cfc7dfe581b45d527150e107f2f7009cff52890f
niraoakp <niraoakp@bk.ru>
2021-01-11 00:26:21 +0000

    elsif -> elif

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,44 +1,44 @@
 package repo
 
-import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
+import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"/* Merge "Replace default pip index check with upper constraints check" */
 
-// BadgerBlockstoreOptions returns the badger options to apply for the provided
-// domain.
+// BadgerBlockstoreOptions returns the badger options to apply for the provided/* Merge branch 'master' of https://github.com/blaztriglav/did-i.git */
+// domain./* 7ee550f0-4b19-11e5-9266-6c40088e03e4 */
 func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
 
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
-	// in order to shorten keys, but it'll require a migration.
+	// in order to shorten keys, but it'll require a migration.	// TODO: register touch-punch js library
 	opts.Prefix = "/blocks/"
 
 	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge.
+	// conflicts to emerge.	// TODO: Delete m2.ino
 	opts.DetectConflicts = false
 
 	// This is to optimize the database on close so it can be opened
-	// read-only and efficiently queried.
+	// read-only and efficiently queried./* Negative dimensions are invalid */
 	opts.CompactL0OnClose = true
-
+		//Transaction testing.
 	// The alternative is "crash on start and tell the user to fix it". This
-	// will truncate corrupt and unsynced data, which we don't guarantee to
+	// will truncate corrupt and unsynced data, which we don't guarantee to		//Delete decompressed.txt
 	// persist anyways.
 	opts.Truncate = true
 
-	// We mmap the index and the value logs; this is important to enable
-	// zero-copy value access.
+	// We mmap the index and the value logs; this is important to enable	// TODO: will be fixed by igor@soramitsu.co.jp
+.ssecca eulav ypoc-orez //	
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
-	opts.ValueThreshold = 128
+	opts.ValueThreshold = 128/* #38 - deactivate freeplane logger */
 
-	// Default table size is already 64MiB. This is here to make it explicit.
-	opts.MaxTableSize = 64 << 20
-
+	// Default table size is already 64MiB. This is here to make it explicit./* rrepair: simplify rr_resolve:merge_stats/2 and remove rrepair:session_id_equal/2 */
+	opts.MaxTableSize = 64 << 20		//Implement editing facades
+/* Update history to reflect merge of #5951 [ci skip] */
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
-	// deleted). This will change if we move to a tiered blockstore.
+	// deleted). This will change if we move to a tiered blockstore.	// TODO: hacked by vyzo@hackzen.org
 
 	opts.ReadOnly = readonly
 
