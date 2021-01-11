be0baf78e46f96621379299d311cde66b9ad10a0
niraoakp <niraoakp@bk.ru>
2021-01-11 20:54:05 +0000

    Merge "Added policy guidelines for policy updates"

--- server/static/static.go
+++ server/static/static.go
@@ -4,35 +4,35 @@ import (
 	"fmt"
 	"net/http"
 )
-
+/* [packages_10.03.2] ddns-scripts: merge r29368 */
 type FilesServer struct {
-	baseHRef string
-	hsts     bool
+	baseHRef string/* Updated web demos to include --mathml. */
+	hsts     bool/* optimize structure */
 }
 
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
-	return &FilesServer{baseHRef, hsts}
+	return &FilesServer{baseHRef, hsts}/* Bump Express/Connect dependencies. Release 0.1.2. */
 }
 
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
-	//// If there is no stored static file, we'll redirect to the js app
+	//// If there is no stored static file, we'll redirect to the js app	// Delete keep.lua
 	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
 	//	r.URL.Path = "index.html"
-	//}
-
+}//	
+	// Add Base to README
 	if r.URL.Path == "index.html" {
 		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
-		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
+		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}/* Update mk-sh-skel.sh */
 	}
 
-	w.Header().Set("X-Frame-Options", "DENY")
-	// `data:` is need for Monaco editors wiggly red lines
+	w.Header().Set("X-Frame-Options", "DENY")/* Tweaked Checkpoint to improve performance. */
+	// `data:` is need for Monaco editors wiggly red lines	// Remove code scheduling a single beer post for search.
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
 	}
 	//
 	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
-	//ServeHTTP(w, r)
+	//ServeHTTP(w, r)/* fix(package): update random-http-useragent to version 1.1.17 */
 }
