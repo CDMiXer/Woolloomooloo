2b0306af85a682249d79c8f8c04d33ab8eb1011b
niraoakp <niraoakp@bk.ru>
2021-01-11 21:47:08 +0000

    Intermediate migration from pictures.car_id to picture_item.item_id

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -5,11 +5,11 @@ import pulumi
 config = pulumi.Config()
 org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
-a = pulumi.StackReference(slug)
+a = pulumi.StackReference(slug)/* Deleted CtrlApp_2.0.5/Release/StdAfx.obj */
 
 oldVal = a.get_output('val')
 
 if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
     raise Exception('Invalid result')
-	// Workaround for uMode precision mismatch
+/* RSS compatibility improvements; now throwing in event of bogus feed. */
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
