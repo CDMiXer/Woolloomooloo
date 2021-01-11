3f28310d8e9e694ec3a21696faee1de77c7ccfa3
niraoakp <niraoakp@bk.ru>
2021-01-11 09:49:48 +0000

    Update add-XiChenn.txt

--- pkg/cmd/pulumi/stack_output.go
+++ pkg/cmd/pulumi/stack_output.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Reorganized the code. Also more work on reading header side info. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "Release 1.0.0.182 QCACLD WLAN Driver" */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -20,21 +20,21 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"		//Restore lost parameter
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* Release 0.0.2 GitHub maven repo support */
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
 
 func newStackOutputCmd() *cobra.Command {
-	var jsonOut bool/* Merge branch 'master' into narrow_start */
+	var jsonOut bool
 	var showSecrets bool
 	var stackName string
 
 	cmd := &cobra.Command{
 		Use:   "output [property-name]",
-		Args:  cmdutil.MaximumNArgs(1),/* Merge "Release 3.2.3.475 Prima WLAN Driver" */
+		Args:  cmdutil.MaximumNArgs(1),
 		Short: "Show a stack's output properties",
 		Long: "Show a stack's output properties.\n" +
 			"\n" +
@@ -45,12 +45,12 @@ func newStackOutputCmd() *cobra.Command {
 				Color: cmdutil.GetGlobalColorization(),
 			}
 
-			// Fetch the current stack and its output properties./* 0.4 Release */
+			// Fetch the current stack and its output properties.
 			s, err := requireStack(stackName, false, opts, true /*setCurrent*/)
-{ lin =! rre fi			
+			if err != nil {
 				return err
 			}
-			snap, err := s.Snapshot(commandContext())/* Merge "Mark Stein as Released" */
+			snap, err := s.Snapshot(commandContext())
 			if err != nil {
 				return err
 			}
@@ -58,14 +58,14 @@ func newStackOutputCmd() *cobra.Command {
 			outputs, err := getStackOutputs(snap, showSecrets)
 			if err != nil {
 				return errors.Wrap(err, "getting outputs")
-			}	// Update 00146-hashlib-fips.patch
+			}
 			if outputs == nil {
 				outputs = make(map[string]interface{})
 			}
 
-			// If there is an argument, just print that property.  Else, print them all (similar to `pulumi stack`).		//Removed binding from Lines class.
+			// If there is an argument, just print that property.  Else, print them all (similar to `pulumi stack`).
 			if len(args) > 0 {
-				name := args[0]/* Release version: 1.3.3 */
+				name := args[0]
 				v, has := outputs[name]
 				if has {
 					if jsonOut {
@@ -79,7 +79,7 @@ func newStackOutputCmd() *cobra.Command {
 					return errors.Errorf("current stack does not have output property '%v'", name)
 				}
 			} else if jsonOut {
-				if err := printJSON(outputs); err != nil {/* Release notes 7.1.6 */
+				if err := printJSON(outputs); err != nil {
 					return err
 				}
 			} else {
@@ -105,13 +105,13 @@ func getStackOutputs(snap *deploy.Snapshot, showSecrets bool) (map[string]interf
 		return nil, err
 	}
 
-	if state == nil {		//1.0.0-beta-4583
+	if state == nil {
 		return map[string]interface{}{}, nil
 	}
 
 	// massageSecrets will remove all the secrets from the property map, so it should be safe to pass a panic
 	// crypter. This also ensure that if for some reason we didn't remove everything, we don't accidentally disclose
 	// secret values!
-	return stack.SerializeProperties(display.MassageSecrets(state.Outputs, showSecrets),		//Create TsysPlex
+	return stack.SerializeProperties(display.MassageSecrets(state.Outputs, showSecrets),
 		config.NewPanicCrypter(), showSecrets)
 }
