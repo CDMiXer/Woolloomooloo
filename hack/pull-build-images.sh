98c241a0dd169849ddb791ea082385e1c96e7847
niraoakp <niraoakp@bk.ru>
2021-01-11 01:38:14 +0000

    [tpm2] nit

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/* Release version 3.6.2.3 */
+#!/bin/bash
 set -eu -o pipefail
-/* remove county mapper, cruft */
+
 grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
