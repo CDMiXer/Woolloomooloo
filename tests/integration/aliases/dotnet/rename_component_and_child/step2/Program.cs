6f142210ffee0ea2488789b32f37607ad9334ea1
niraoakp <niraoakp@bk.ru>
2021-01-11 19:23:04 +0000

    Updates to port / system management to parse netstat output on freebsd

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -1,42 +1,42 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Release v0.2.2. */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* FIX avoid exceptions in error-handler for loading config-file */
 
-using System.Threading.Tasks;	// TODO: Add masquerade module.
+using System.Threading.Tasks;
 using Pulumi;
-		//Fix links to samples
+
 class Resource : ComponentResource
-{/* == Release 0.1.0 for PyPI == */
+{
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)		//fibonacci-modified.py
-{    
+        : base("my:module:Resource", name, options)
+    {
     }
 }
-/* Release 1.04 */
+
 // Scenario #5 - composing #1 and #3
 class ComponentFive : ComponentResource
 {
     private Resource resource;
-
+/* do not interpret default visibility as package or namespace */
     public ComponentFive(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFive", name, options)
+        : base("my:module:ComponentFive", name, options)		//Oops, drop debug code
     {
         this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
         { 
             Parent = this,
-            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },		//Ticket #3025 - Clear cache related to reposts.
-        });
-    }/* bundle-size: 309bebb360d5eb3138f08a12745306334e9ba20d.json */
+            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
+        });	// TODO: will be fixed by ligi@ligi.de
+    }
 }
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)/* Create 02_01.sql */
     {
         return Deployment.RunAsync(() =>
-        {/* Merge "Release resource lock when executing reset_stack_status" */
+        {/* Moved tokens into a package of their own. */
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
             {
-                Aliases = { new Alias { Name = "comp5" } },
-            });
+                Aliases = { new Alias { Name = "comp5" } },/* #14 - tests for behaviourTreeNode */
+            });/* Merge "Release 1.0.0.237 QCACLD WLAN Drive" */
         });
     }
-}
+}	// 67a916cc-2e51-11e5-9284-b827eb9e62be
