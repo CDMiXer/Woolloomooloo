609d9eeada59f2bd5ab9c701d41f81f781f7688f
niraoakp <niraoakp@bk.ru>
2021-01-11 06:18:05 +0000

    Merge branch 'master' into dependabot/npm_and_yarn/frontend/ajv-6.11.0

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fix coversNInputs, additional validation of varargs
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release strict forbiddance in README.md license */
-// +build !oss	// TODO: hacked by alan.shaw@protocol.ai
+/* Release 2.5.8: update sitemap */
+// +build !oss
 
 package machine
