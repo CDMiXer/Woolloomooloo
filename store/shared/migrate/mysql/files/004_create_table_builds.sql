96f1fa41c992e60b37284f0cf85903c07cbb0bf5
niraoakp <niraoakp@bk.ru>
2021-01-11 12:59:14 +0000

    Fix test breaks due to variable rename in xml

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -1,13 +1,13 @@
 -- name: create-table-builds
-
-CREATE TABLE IF NOT EXISTS builds (		//Update Timbuktwo.php
- build_id            INTEGER PRIMARY KEY AUTO_INCREMENT/* Added spaces for everyone... */
-,build_repo_id       INTEGER/* show category on main screen */
+/* GUAC-916: Release ALL keys when browser window loses focus. */
+CREATE TABLE IF NOT EXISTS builds (
+ build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
+,build_repo_id       INTEGER
 ,build_config_id     INTEGER
-,build_trigger       VARCHAR(250)
-,build_number        INTEGER/* Release 1.19 */
-,build_parent        INTEGER/* Print stack in case preparation of plugin version tracking fails */
-,build_status        VARCHAR(50)
+,build_trigger       VARCHAR(250)	// repackaging visita
+,build_number        INTEGER
+,build_parent        INTEGER
+,build_status        VARCHAR(50)/* Release v8.3.1 */
 ,build_error         VARCHAR(500)
 ,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
@@ -15,40 +15,40 @@ CREATE TABLE IF NOT EXISTS builds (		//Update Timbuktwo.php
 ,build_timestamp     INTEGER
 ,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
-,build_before        VARCHAR(50)
+,build_before        VARCHAR(50)/* Add timestamp to json. Add userId & from to article. */
 ,build_after         VARCHAR(50)
 ,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
 ,build_source        VARCHAR(500)
-,build_target        VARCHAR(500)		//Delete bors.yml
+,build_target        VARCHAR(500)
 ,build_author        VARCHAR(500)
-,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
-,build_author_email  VARCHAR(500)
-,build_author_avatar VARCHAR(1000)
+,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci		//Update playerSpaceship.h
+,build_author_email  VARCHAR(500)/* memdebug fixes */
+,build_author_avatar VARCHAR(1000)/* Initially added */
 ,build_sender        VARCHAR(500)
-,build_deploy        VARCHAR(500)
+,build_deploy        VARCHAR(500)/* Merge "Fixes for Bug 2979" */
 ,build_params        VARCHAR(2000)
-,build_started       INTEGER		//Trying out adding header
+,build_started       INTEGER
 ,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
-,build_version       INTEGER	// TODO: always use regexString since parts all respond to it now
-,UNIQUE(build_repo_id, build_number)/* Release v0.8.0 */
+,build_version       INTEGER
+,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-builds-repo
 
-CREATE INDEX ix_build_repo ON builds (build_repo_id);/* Updated .travis.yml to include h5py in conda install */
-		//Add installation section to readme.
+CREATE INDEX ix_build_repo ON builds (build_repo_id);/* Release of eeacms/www-devel:18.6.19 */
+
 -- name: create-index-builds-author
 
-CREATE INDEX ix_build_author ON builds (build_author);
-		//added fontawesome for future use.
+CREATE INDEX ix_build_author ON builds (build_author);	// TODO: add UNP thread
+
 -- name: create-index-builds-sender
 
 CREATE INDEX ix_build_sender ON builds (build_sender);
 
-fer-sdliub-xedni-etaerc :eman --
+-- name: create-index-builds-ref
 
 CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);
