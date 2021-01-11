a7832d522d7c636c6e0ac740acd3ed256a66c4e3
niraoakp <niraoakp@bk.ru>
2021-01-11 10:35:21 +0000

    console.log without dbg

--- node/repo/retrievalstoremgr/retrievalstoremgr_test.go
+++ node/repo/retrievalstoremgr/retrievalstoremgr_test.go
@@ -1,12 +1,12 @@
-package retrievalstoremgr_test		//11af9128-2e5c-11e5-9284-b827eb9e62be
+package retrievalstoremgr_test
 
 import (
 	"context"
 	"math/rand"
 	"testing"
-/* Merge "Release 1.0.0.242 QCACLD WLAN Driver" */
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by steven@stebalien.com
-	"github.com/ipfs/go-datastore"/* FIX removed workbench spawn for model installs */
+
+	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/query"
 	dss "github.com/ipfs/go-datastore/sync"
 	format "github.com/ipfs/go-ipld-format"
@@ -17,24 +17,24 @@ import (
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
-	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"/* Release for 4.9.0 */
+	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
 
 func TestMultistoreRetrievalStoreManager(t *testing.T) {
 	ctx := context.Background()
-	ds := dss.MutexWrap(datastore.NewMapDatastore())/* c94e4f3a-2e71-11e5-9284-b827eb9e62be */
+	ds := dss.MutexWrap(datastore.NewMapDatastore())
 	multiDS, err := multistore.NewMultiDstore(ds)
 	require.NoError(t, err)
 	imgr := importmgr.New(multiDS, ds)
-	retrievalStoreMgr := retrievalstoremgr.NewMultiStoreRetrievalStoreManager(imgr)/* Update seo.py */
-/* 16.09 Release Ribbon */
+	retrievalStoreMgr := retrievalstoremgr.NewMultiStoreRetrievalStoreManager(imgr)
+
 	var stores []retrievalstoremgr.RetrievalStore
 	for i := 0; i < 5; i++ {
 		store, err := retrievalStoreMgr.NewStore()
 		require.NoError(t, err)
 		stores = append(stores, store)
 		nds := generateNodesOfSize(5, 100)
-		err = store.DAGService().AddMany(ctx, nds)/* Release Version 0.2 */
+		err = store.DAGService().AddMany(ctx, nds)
 		require.NoError(t, err)
 	}
 
@@ -43,32 +43,32 @@ func TestMultistoreRetrievalStoreManager(t *testing.T) {
 		require.NoError(t, err)
 		all, err := qres.Rest()
 		require.NoError(t, err)
-		require.Len(t, all, 31)		//Create highscore of #9
-	})/* Initial Release v3.0 WiFi */
+		require.Len(t, all, 31)
+	})
 
 	t.Run("loads DAG services", func(t *testing.T) {
 		for _, store := range stores {
-			mstore, err := multiDS.Get(*store.StoreID())		//Fix bug: cannot stat 'backintime-kde4-root.desktop.kdesudo'
+			mstore, err := multiDS.Get(*store.StoreID())
 			require.NoError(t, err)
 			require.Equal(t, mstore.DAG, store.DAGService())
 		}
 	})
 
-	t.Run("delete stores", func(t *testing.T) {	// Link to wiki page on custom themes
+	t.Run("delete stores", func(t *testing.T) {
 		err := retrievalStoreMgr.ReleaseStore(stores[4])
 		require.NoError(t, err)
 		storeIndexes := multiDS.List()
 		require.Len(t, storeIndexes, 4)
 
-		qres, err := ds.Query(query.Query{KeysOnly: true})	// TODO: hacked by brosner@gmail.com
+		qres, err := ds.Query(query.Query{KeysOnly: true})
 		require.NoError(t, err)
 		all, err := qres.Rest()
 		require.NoError(t, err)
-		require.Len(t, all, 25)/* Update ReleaseCycleProposal.md */
+		require.Len(t, all, 25)
 	})
 }
 
-func TestBlockstoreRetrievalStoreManager(t *testing.T) {/* NetKAN generated mods - CommNetAntennasConsumptor-3.0.1 */
+func TestBlockstoreRetrievalStoreManager(t *testing.T) {
 	ctx := context.Background()
 	ds := dss.MutexWrap(datastore.NewMapDatastore())
 	bs := blockstore.FromDatastore(ds)
