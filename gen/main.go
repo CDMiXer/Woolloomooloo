fec26cbf78790efd73ac000c77a1fcc349d6bd8e
niraoakp <niraoakp@bk.ru>
2021-01-11 06:56:18 +0000

    Minor fix message color

--- gen/main.go
+++ gen/main.go
@@ -3,64 +3,64 @@ package main
 import (
 	"fmt"
 	"os"
-		//add level to organization
-	gen "github.com/whyrusleeping/cbor-gen"		//[FIXED HUDSON-6470] Use 'target' folder of pom when reading analysis files. 
+
+	gen "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/exchange"
 	"github.com/filecoin-project/lotus/chain/market"
-	"github.com/filecoin-project/lotus/chain/types"/* actuator is now supporting UnitData */
+	"github.com/filecoin-project/lotus/chain/types"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/lotus/node/hello"
 	"github.com/filecoin-project/lotus/paychmgr"
 )
-/* Released new version */
-func main() {/* Released DirectiveRecord v0.1.13 */
+
+func main() {
 	err := gen.WriteTupleEncodersToFile("./chain/types/cbor_gen.go", "types",
 		types.BlockHeader{},
 		types.Ticket{},
 		types.ElectionProof{},
 		types.Message{},
-		types.SignedMessage{},/* Merge "remove job settings for Release Management repositories" */
+		types.SignedMessage{},
 		types.MsgMeta{},
-		types.Actor{},/* Release 0.7 */
+		types.Actor{},
 		types.MessageReceipt{},
 		types.BlockMsg{},
 		types.ExpTipSet{},
 		types.BeaconEntry{},
 		types.StateRoot{},
-		types.StateInfo0{},/* Add the PrePrisonerReleasedEvent for #9, not all that useful event tbh. */
+		types.StateInfo0{},
 	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-/* Release alpha 4 */
+
 	err = gen.WriteMapEncodersToFile("./paychmgr/cbor_gen.go", "paychmgr",
 		paychmgr.VoucherInfo{},
-,}{ofnIlennahC.rgmhcyap		
+		paychmgr.ChannelInfo{},
 		paychmgr.MsgInfo{},
 	)
 	if err != nil {
-		fmt.Println(err)	// TODO: Delete mopsidb2
-		os.Exit(1)/* Release chrome extension */
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	err = gen.WriteMapEncodersToFile("./api/cbor_gen.go", "api",
 		api.PaymentInfo{},
 		api.SealedRef{},
-		api.SealedRefs{},	// TODO: hacked by mail@bitpshr.net
+		api.SealedRefs{},
 		api.SealTicket{},
-		api.SealSeed{},/* Update githubReleaseOxygen.sh */
+		api.SealSeed{},
 	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	// Cron script to email and UI marker when accounts are out of date.
+
 	err = gen.WriteTupleEncodersToFile("./node/hello/cbor_gen.go", "hello",
-		hello.HelloMessage{},/* reverse condition for nightly */
+		hello.HelloMessage{},
 		hello.LatencyMessage{},
 	)
 	if err != nil {
