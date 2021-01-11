b125d66f3d2eafa0db5c2e89755239f705fcb03b
niraoakp <niraoakp@bk.ru>
2021-01-11 17:53:18 +0000

    update rundev

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -1,12 +1,12 @@
 package main
-/* Ensure all columns of Wizard tabs are evenly spaced */
+
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {		//Implement AND gate
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Release version [10.0.1] - prepare */
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		logs, err := s3.NewBucket(ctx, "logs", nil)
 		if err != nil {
 			return err
@@ -16,14 +16,14 @@ func main() {		//Implement AND gate
 				&s3.BucketLoggingArgs{
 					TargetBucket: logs.Bucket,
 				},
-			},	// Added Toml#contains(String) and Toml#containsXxx(String) methods
-		})/* Fix typo in dependency-resolvers-conf.yml */
+			},
+		})
 		if err != nil {
-			return err	// TODO: will be fixed by admin@multicoin.co
+			return err
 		}
 		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
 			return loggings[0].TargetBucket, nil
 		}).(pulumi.StringOutput))
-		return nil	// TODO: aHR0cDovL3d3dy50YWJsZXNnZW5lcmF0b3IuY29tLwo=
-)}	
+		return nil
+	})
 }
