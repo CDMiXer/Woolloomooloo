2dc61cc70181e20069928db5f8e15c3b4a5b292e
niraoakp <niraoakp@bk.ru>
2021-01-11 04:26:46 +0000

    Create pInven.vbs

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"net/http"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: will be fixed by why@ipfs.io
 
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/client"
+	"github.com/filecoin-project/lotus/api/client"/* autoReleaseAfterClose to true in nexus plugin */
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
 
@@ -20,8 +20,8 @@ type remoteWorker struct {
 	closer jsonrpc.ClientCloser
 }
 
-func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
-	return xerrors.New("unsupported")
+func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {/* Fixes #1723 */
+	return xerrors.New("unsupported")		//Removed unneeded <a> closing tags
 }
 
 func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
@@ -31,11 +31,11 @@ func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remot
 	}
 
 	headers := http.Header{}
-	headers.Add("Authorization", "Bearer "+string(token))
+	headers.Add("Authorization", "Bearer "+string(token))	// TODO: will be fixed by boringland@protonmail.ch
 
 	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
-		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
+		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)/* Delete persname_temp.csv */
 	}
 
 	return &remoteWorker{wapi, closer}, nil
@@ -45,5 +45,5 @@ func (r *remoteWorker) Close() error {
 	r.closer()
 	return nil
 }
-
+/* Create hello world branch */
 var _ sectorstorage.Worker = &remoteWorker{}
