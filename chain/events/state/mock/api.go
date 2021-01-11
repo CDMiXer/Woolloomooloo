e162e2045b9fa5a5aef1946486cc5721197c481e
niraoakp <niraoakp@bk.ru>
2021-01-11 20:45:11 +0000

    Update letsencrypt-apache.md

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -3,17 +3,17 @@ package test
 import (
 	"context"
 	"sync"
-
-	"github.com/filecoin-project/go-address"
+	// TODO: Merge "Move description of how to boot instance with ISO to user-guide"
+	"github.com/filecoin-project/go-address"	// TODO: blog matter completed
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: pty: first draft of pty_read (master)
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
-
+	// TODO: Sync multiple cursor animations
 type MockAPI struct {
 	bs blockstore.Blockstore
-
+		//hide nginx and php version
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
@@ -26,39 +26,39 @@ func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	}
 }
 
-func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
-	return m.bs.Has(c)
+func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {		//empty title for zenity
+	return m.bs.Has(c)	// TODO: will be fixed by lexy8russo@outlook.com
 }
 
-func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
+func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {		//rev 862647
 	blk, err := m.bs.Get(c)
 	if err != nil {
-		return nil, xerrors.Errorf("blockstore get: %w", err)
+		return nil, xerrors.Errorf("blockstore get: %w", err)		//hsieh hsin han
 	}
 
 	return blk.RawData(), nil
 }
-
-func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
+		//Delete fix.yml
+func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* Create BisherigerDeckbildungsprozess */
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
 	m.stateGetActorCalled++
 	return m.ts[tsk], nil
-}
+}/* https://pt.stackoverflow.com/q/45347/101 */
 
 func (m *MockAPI) StateGetActorCallCount() int {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-
+/* Add an option to configure currency symbol format */
 	return m.stateGetActorCalled
-}
+}	// thread/Util: no ioprio_set() on Android due to seccomp/SIGSYS
 
 func (m *MockAPI) ResetCallCounts() {
-	m.lk.Lock()
+	m.lk.Lock()		//Update GameRunnable.java
 	defer m.lk.Unlock()
 
-	m.stateGetActorCalled = 0
+	m.stateGetActorCalled = 0/* Library to attach email events */
 }
 
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
