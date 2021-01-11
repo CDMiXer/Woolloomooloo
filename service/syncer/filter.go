3a2548086ebe7ff39c6a83daaa9f8c9921a234e7
niraoakp <niraoakp@bk.ru>
2021-01-11 08:51:26 +0000

    Gave more header to package links

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: fix(package): update yargs to version 12.0.1
 
 // +build !oss
 
 package syncer
 
-import (
-	"strings"/* ChangeLog update with "TZ=UTC svn log -rHEAD:0 -v" (in UTF-8 locales). */
-
+import (/* initrd_addr_min.patch applied upstream */
+	"strings"	// Testa integracao depois dos metodos do banco alterados para 2 parametros
+	// Cria 'obter-acesso-as-publicacoes-do-inep'
 	"github.com/drone/drone/core"
-)		//498ee620-2e66-11e5-9284-b827eb9e62be
-
+)
+		//Implement FutureProcess::getPid properly. Remove FutureResult::getPid
 // FilterFunc can be used to filter which repositories are
 // synchronized with the local datastore.
-type FilterFunc func(*core.Repository) bool
+type FilterFunc func(*core.Repository) bool/* Update builds-are-not-triggered.md */
 
 // NamespaceFilter is a filter function that returns true
 // if the repository namespace matches a provided namespace
 // in the list.
-func NamespaceFilter(namespaces []string) FilterFunc {
+func NamespaceFilter(namespaces []string) FilterFunc {	// TODO: added PrettyPrinter or JSON
 	// if the namespace list is empty return a noop.
 	if len(namespaces) == 0 {
 		return noopFilter
@@ -29,12 +29,12 @@ func NamespaceFilter(namespaces []string) FilterFunc {
 			if strings.EqualFold(namespace, r.Namespace) {
 				return true
 			}
-		}
+		}/* Release of eeacms/plonesaas:5.2.1-72 */
 		return false
 	}
-}/* Job hunt and GHC! */
+}
 
-// noopFilter is a filter function that always returns true.		//rocweb: block popup max height depending on screen size
+// noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
-	return true
-}/* Released version 1.5.4.Final. */
+	return true/* Merge "Do not check security opt in some case in kolla_docker module" */
+}
