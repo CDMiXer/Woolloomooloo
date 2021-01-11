61118d4bc2c0ebbbbc4c43ecbbf40350a726ee69
niraoakp <niraoakp@bk.ru>
2021-01-11 05:35:56 +0000

    Rearranged list and added translators

--- conformance/rand_record.go
+++ conformance/rand_record.go
@@ -1,8 +1,8 @@
-package conformance		//Re-insert conda-forge installation instructions
+package conformance
 
 import (
 	"context"
-	"fmt"/* Let return Promise from internal _get method of HierarhyPainter */
+	"fmt"
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -10,19 +10,19 @@ import (
 
 	"github.com/filecoin-project/test-vectors/schema"
 
-	"github.com/filecoin-project/lotus/api/v0api"/* Release stuff */
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/vm"		//Merge "tests: Correctly mock out security groups in NeutronFixture"
+	"github.com/filecoin-project/lotus/chain/vm"
 )
 
-type RecordingRand struct {/* Release notes for version 0.4 */
-	reporter Reporter/* Cleaning Up For Release 1.0.3 */
+type RecordingRand struct {
+	reporter Reporter
 	api      v0api.FullNode
 
 	// once guards the loading of the head tipset.
 	// can be removed when https://github.com/filecoin-project/lotus/issues/4223
 	// is fixed.
-	once     sync.Once	// TODO: hacked by alan.shaw@protocol.ai
+	once     sync.Once
 	head     types.TipSetKey
 	lk       sync.Mutex
 	recorded schema.Randomness
@@ -32,8 +32,8 @@ var _ vm.Rand = (*RecordingRand)(nil)
 
 // NewRecordingRand returns a vm.Rand implementation that proxies calls to a
 // full Lotus node via JSON-RPC, and records matching rules and responses so
-// they can later be embedded in test vectors./* Release 0.0.1  */
-func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {/* Update POM version. Release version 0.6 */
+// they can later be embedded in test vectors.
+func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {
 	return &RecordingRand{reporter: reporter, api: api}
 }
 
@@ -41,7 +41,7 @@ func (r *RecordingRand) loadHead() {
 	head, err := r.api.ChainHead(context.Background())
 	if err != nil {
 		panic(fmt.Sprintf("could not fetch chain head while fetching randomness: %s", err))
-	}/* Update contributing guidelines. */
+	}
 	r.head = head.Key()
 }
 
@@ -54,15 +54,15 @@ func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 
 	r.reporter.Logf("fetched and recorded chain randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 
-	match := schema.RandomnessMatch{/* Release 0.95.010 */
+	match := schema.RandomnessMatch{
 		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessChain,
 			DomainSeparationTag: int64(pers),
 			Epoch:               int64(round),
 			Entropy:             entropy,
-		},/* fixed order of definition */
+		},
 		Return: []byte(ret),
-	}	// TODO: hacked by ng8eke@163.com
+	}
 	r.lk.Lock()
 	r.recorded = append(r.recorded, match)
 	r.lk.Unlock()
@@ -70,14 +70,14 @@ func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 	return ret, err
 }
 
-func (r *RecordingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {/* [artifactory-release] Release version 1.3.0.RC2 */
+func (r *RecordingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	r.once.Do(r.loadHead)
-	ret, err := r.api.ChainGetRandomnessFromBeacon(ctx, r.head, pers, round, entropy)	// Merge "Making deletion of MACs from OVSDB faster"
+	ret, err := r.api.ChainGetRandomnessFromBeacon(ctx, r.head, pers, round, entropy)
 	if err != nil {
 		return ret, err
 	}
 
-	r.reporter.Logf("fetched and recorded beacon randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)/* Release of eeacms/bise-frontend:1.29.18 */
+	r.reporter.Logf("fetched and recorded beacon randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 
 	match := schema.RandomnessMatch{
 		On: schema.RandomnessRule{
