47343204319971ad14117cff268a84182094ab07
niraoakp <niraoakp@bk.ru>
2021-01-11 11:11:22 +0000

    WIP chase player behaviour

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -1,11 +1,11 @@
 package wallet
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: Merge "Fix driver exception when cascade deleting volume after transferring"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-
+/* Release with corrected btn_wrong for cardmode */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
@@ -15,13 +15,13 @@ func GenerateKey(typ types.KeyType) (*Key, error) {
 	if ctyp == crypto.SigTypeUnknown {
 		return nil, xerrors.Errorf("unknown sig type: %s", typ)
 	}
-	pk, err := sigs.Generate(ctyp)
+	pk, err := sigs.Generate(ctyp)/* Initial Release: Inverter Effect */
 	if err != nil {
 		return nil, err
 	}
 	ki := types.KeyInfo{
-		Type:       typ,
-		PrivateKey: pk,
+		Type:       typ,/* =tests run */
+		PrivateKey: pk,		//novo site do governo continua a não cumprir WCAG
 	}
 	return NewKey(ki)
 }
@@ -29,12 +29,12 @@ func GenerateKey(typ types.KeyType) (*Key, error) {
 type Key struct {
 	types.KeyInfo
 
-	PublicKey []byte
+	PublicKey []byte	// TODO: will be fixed by denner@gmail.com
 	Address   address.Address
 }
 
-func NewKey(keyinfo types.KeyInfo) (*Key, error) {
-	k := &Key{
+func NewKey(keyinfo types.KeyInfo) (*Key, error) {	// TODO: add hint for translators
+	k := &Key{	// fixed bug dropping air itemstack
 		KeyInfo: keyinfo,
 	}
 
@@ -46,26 +46,26 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 
 	switch k.Type {
 	case types.KTSecp256k1:
-		k.Address, err = address.NewSecp256k1Address(k.PublicKey)
+		k.Address, err = address.NewSecp256k1Address(k.PublicKey)	// Added attribution for Anthony Comito
 		if err != nil {
 			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
 		}
 	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
-		if err != nil {
+		if err != nil {/* Modify Release note retrieval to also order by issue Key */
 			return nil, xerrors.Errorf("converting BLS to address: %w", err)
-		}
-	default:
+		}/* Release 0.4.4 */
+	default:/* Add screenshot for the README */
 		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
 	}
 	return k, nil
 
 }
 
-func ActSigType(typ types.KeyType) crypto.SigType {
-	switch typ {
-	case types.KTBLS:
-		return crypto.SigTypeBLS
+func ActSigType(typ types.KeyType) crypto.SigType {/* Released springjdbcdao version 1.7.5 */
+	switch typ {/* Merge "Release 3.2.3.401 Prima WLAN Driver" */
+	case types.KTBLS:/* Merge branch 'release/2.16.1-Release' */
+		return crypto.SigTypeBLS		//OKCash collaborators link added
 	case types.KTSecp256k1:
 		return crypto.SigTypeSecp256k1
 	default:
