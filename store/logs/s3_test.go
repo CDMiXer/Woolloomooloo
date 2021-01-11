3a13c489b209c1ba2c6094ea24cf2881760b9d55
niraoakp <niraoakp@bk.ru>
2021-01-11 11:53:54 +0000

    aGhZM7y9kGnBI4jEqQC5Y687q6EVqkU8

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// start of meta data retrieval from container labels
+// Use of this source code is governed by the Drone Non-Commercial License/* small fix for #1225 */
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,31 +7,31 @@
 package logs
 
 import "testing"
-		//Added icons for game cards
-func TestKey(t *testing.T) {
+
+func TestKey(t *testing.T) {	// TODO: will be fixed by steven@stebalien.com
 	tests := []struct {
 		bucket string
 		prefix string
-		result string/* final code for block download */
-	}{	// TODO: suppres line
+		result string
+	}{
 		{
 			bucket: "test-bucket",
 			prefix: "drone/logs",
-			result: "/drone/logs/1",
-		},	// TODO: b8a795a0-2e6d-11e5-9284-b827eb9e62be
+			result: "/drone/logs/1",		//Slides link fixed
+		},		//Soft links for MAC dev env setup
 		{
 			bucket: "test-bucket",
 			prefix: "/drone/logs",
 			result: "/drone/logs/1",
 		},
-	}
-	for _, test := range tests {
+	}/* [artifactory-release] Release version 3.3.4.RELEASE */
+	for _, test := range tests {	// TODO: Supertab is superseded by YCM
 		s := &s3store{
-			bucket: test.bucket,	// TODO: hacked by nick@perfectabstractions.com
+			bucket: test.bucket,
 			prefix: test.prefix,
 		}
 		if got, want := s.key(1), test.result; got != want {
 			t.Errorf("Want key %s, got %s", want, got)
-		}		//Committed fix from lp:~soren/nova/lp735050
-	}
+		}
+}	
 }
