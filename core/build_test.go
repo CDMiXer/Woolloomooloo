19f46edd02717fdcbefc38014f3e303a90ad5f1e
niraoakp <niraoakp@bk.ru>
2021-01-11 17:05:52 +0000

    fix(package): update eslint-plugin-import to version 2.9.0

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Add a test case to see of truezip can recognize ova extention
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Reorganise, Prepare Release. */
-package core	// Updated Links on TwitterMediaClientSpec.scala
+
+package core
