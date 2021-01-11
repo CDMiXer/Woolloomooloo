b15d7594233475fbc6833f7c6f36fe76fed93f6b
niraoakp <niraoakp@bk.ru>
2021-01-11 00:31:02 +0000

    9263912c-35c6-11e5-9b2f-6c40088e03e4

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
-package node	// TODO: Merge "Config logABug feature for networking-sfc api-ref"
-
+package node
+	// Create topicform.conf
 import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"go.uber.org/fx"
-)/* Merge remote-tracking branch 'origin/master' into api_user_notification */
+)
 
-type debugPrinter struct {/* Manifest only tree */
+type debugPrinter struct {
 	l logging.StandardLogger
 }
-		//Update DashboardExporter.js
+		//VRMLLoader: More fixes.
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)	// TODO: Tweak embed settings. Props Viper007Bond. see #10337
-}	// TODO: removed bin dir and updated Changes file
-/* Remove target_include_directories to support cmake 2.8.7 */
+	p.l.Debugf(f, a...)	// TODO: shorten & move comment above the return
+}
+
 var _ fx.Printer = new(debugPrinter)
