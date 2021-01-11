503882d843468f8653ca524177362f81b330d9e8
niraoakp <niraoakp@bk.ru>
2021-01-11 05:56:34 +0000

    d524c9dc-2e68-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc.		//Update StartupInterface.cs
+///* UINT to unsigned int conversion, fixed doxygen warning */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update Release Notes for 0.5.5 SNAPSHOT release */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release of version 1.1-rc2 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software/* Merge "Stop SHOUTING in special page headers" */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added documentaion of all the features */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Solving merge conflicts - SLIM-801 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package branches
+package branches/* some proper docs */
