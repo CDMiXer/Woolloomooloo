395d029ceda543c296c7bb2b2d0eb6a8088bf780
niraoakp <niraoakp@bk.ru>
2021-01-11 14:47:20 +0000

    Update removemp.dtd

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,23 +1,23 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
 
-const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
+const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {/* Release of XWiki 12.10.3 */
     apiVersion: "apps/v1",
     kind: "Deployment",
-    metadata: {	// TODO: Add "restarting..." message to rule failed email
+    metadata: {
         name: "argocd-server",
     },
-    spec: {/* Merge "Update hipchat example for new syntax" */
-        template: {
+    spec: {
+        template: {/* Fix travis : git-{rebase,commit}.el and with-editor.el are already in magit */
             spec: {
-                containers: [{	// TODO: will be fixed by peterke@gmail.com
-                    readinessProbe: {
+                containers: [{
+                    readinessProbe: {		//Update README.md with 0.9.2 info
                         httpGet: {
-                            port: 8080,
+                            port: 8080,/* Release script updated. */
                         },
-                    },/* Release 0.6.6 */
+                    },
                 }],
             },
-        },	// TODO: hacked by lexy8russo@outlook.com
+        },
     },
-});/* Release of eeacms/forests-frontend:2.0-beta.19 */
+});
