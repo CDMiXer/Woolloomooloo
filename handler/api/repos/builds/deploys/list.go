81ddc6b548672a0d763c51f98b1d509287650fae
niraoakp <niraoakp@bk.ru>
2021-01-11 17:20:37 +0000

    Replace deprecated mocking methods for examples for how to rspec mocks

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Added gymControl */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,39 +23,39 @@ import (
 
 	"github.com/go-chi/chi"
 )
-
+/* gossip: removed init delay */
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Replaced BouncyCastle by SpongyCastle. */
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* Reword type safety introduction */
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* Release v0.7.1.1 */
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return
-		}
+			return/* 487c69e8-2e51-11e5-9284-b827eb9e62be */
+		}	// TODO: will be fixed by steven@stebalien.com
 
 		results, err := builds.LatestDeploys(r.Context(), repo.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).		//Refactor player.js & Changed the install maxVersion to 1.2.0pre
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
-		} else {
+		} else {/* WL#7514: More printouts to log for system restart case */
 			render.JSON(w, results, 200)
 		}
 	}
-}
+}	// (#5122) - remove unnecessary code from leveldb/index.js
