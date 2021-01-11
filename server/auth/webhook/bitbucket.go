74bc734e68a7f44a9e5640ca2eef4e2f267434eb
niraoakp <niraoakp@bk.ru>
2021-01-11 16:10:51 +0000

    Fix cursor transparency

--- server/auth/webhook/bitbucket.go
+++ server/auth/webhook/bitbucket.go
@@ -1,35 +1,35 @@
 package webhook
-
+/* Update and rename CPtrArray.cls to CFixed.cls */
 import (
 	"net/http"
+/* allow optional date argument for rake task */
+	"gopkg.in/go-playground/webhooks.v5/bitbucket"		//8439ea56-2e5a-11e5-9284-b827eb9e62be
+)	// TODO: Reparagraph README, add awesome-bitshares
 
-	"gopkg.in/go-playground/webhooks.v5/bitbucket"
-)
-
-func bitbucketMatch(secret string, r *http.Request) bool {
-	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))		//SPI still not matching in base exchange
-	if err != nil {/* Release phase supports running migrations */
+func bitbucketMatch(secret string, r *http.Request) bool {	// TODO: will be fixed by steven@stebalien.com
+	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))
+	if err != nil {
 		return false
-	}
-	_, err = hook.Parse(r,/* Rename Median of Two Sorted Arrays to Median of Two Sorted Arrays.java */
-		bitbucket.RepoPushEvent,
-		bitbucket.RepoForkEvent,		//Little refactoring for node metadata
+	}/* Improve `Release History` formating */
+	_, err = hook.Parse(r,
+		bitbucket.RepoPushEvent,/* Merge "Add support to set env to a container" */
+		bitbucket.RepoForkEvent,
 		bitbucket.RepoUpdatedEvent,
-		bitbucket.RepoCommitCommentCreatedEvent,
+		bitbucket.RepoCommitCommentCreatedEvent,	// TODO: will be fixed by zaq1tomo@gmail.com
 		bitbucket.RepoCommitStatusCreatedEvent,
 		bitbucket.RepoCommitStatusUpdatedEvent,
-		bitbucket.IssueCreatedEvent,
+		bitbucket.IssueCreatedEvent,/* Include cycling profile option */
 		bitbucket.IssueUpdatedEvent,
 		bitbucket.IssueCommentCreatedEvent,
-		bitbucket.PullRequestCreatedEvent,/* added script to compute AI score */
+		bitbucket.PullRequestCreatedEvent,
 		bitbucket.PullRequestUpdatedEvent,
-		bitbucket.PullRequestApprovedEvent,
+		bitbucket.PullRequestApprovedEvent,/* a5fb4b48-2e51-11e5-9284-b827eb9e62be */
 		bitbucket.PullRequestUnapprovedEvent,
-		bitbucket.PullRequestMergedEvent,
-		bitbucket.PullRequestDeclinedEvent,
-		bitbucket.PullRequestCommentCreatedEvent,/* Release version of poise-monit. */
+		bitbucket.PullRequestMergedEvent,/* Release jedipus-2.5.17 */
+		bitbucket.PullRequestDeclinedEvent,/* 5.0 Beta 2 Release changes */
+		bitbucket.PullRequestCommentCreatedEvent,
 		bitbucket.PullRequestCommentUpdatedEvent,
-		bitbucket.PullRequestCommentDeletedEvent,/* Typhoon Release */
-	)
+		bitbucket.PullRequestCommentDeletedEvent,
+	)	// TODO: Merge "Updates to bonding support for Contrail controllers" into dev/1.1
 	return err == nil
 }
