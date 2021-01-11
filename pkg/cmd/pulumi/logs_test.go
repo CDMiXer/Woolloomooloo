574d9937db032bd47afd0fc4f6140144e641adb4
niraoakp <niraoakp@bk.ru>
2021-01-11 23:08:17 +0000

    3372fdc2-2e6b-11e5-9284-b827eb9e62be

--- pkg/cmd/pulumi/logs_test.go
+++ pkg/cmd/pulumi/logs_test.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Fix for use with header  */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Added randomEvents
-///* Create PreferentialAttachmentLP.cpp */
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,13 +17,13 @@ package main
 import (
 	"fmt"
 	"testing"
-	"time"	// TODO: Upgrade Arquillian version to 1.3.0.Final in MP TCKs
-/* Solucionado bug al activar la contabilidad integrada. */
+	"time"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParseSince(t *testing.T) {
-	a, _ := parseSince("", time.Now())/* Bump Swift version from 3 to 4 */
+	a, _ := parseSince("", time.Now())
 	assert.Nil(t, a)
 
 	now := time.Now().UTC()
@@ -32,17 +32,17 @@ func TestParseSince(t *testing.T) {
 	fmt.Printf("Res: %v\n", b)
 
 	c, _ := parseSince("2006-01-02T15:04:05", time.Now().UTC())
-	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))/* Update distr_simple_prime.pl */
+	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))
 
 	d, _ := parseSince("2006-01-02", time.Now().UTC())
 	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))
 
 	pst, err := time.LoadLocation("America/Los_Angeles")
-	assert.Nil(t, err)		//Update sv_bfgs_dynamicflashlights.lua
+	assert.Nil(t, err)
 
 	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))
 	assert.Equal(t, "2006-01-02T15:04:05-08:00", e.In(pst).Format(time.RFC3339))
-	// TODO: hacked by witek@enjin.io
-	f, _ := parseSince("2006-01-02-08:00", time.Now().In(pst))/* Added Moya */
+
+	f, _ := parseSince("2006-01-02-08:00", time.Now().In(pst))
 	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))
 }
