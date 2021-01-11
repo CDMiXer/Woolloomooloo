8d3a23e4930b5020f7954e719de46cb3108842d7
niraoakp <niraoakp@bk.ru>
2021-01-11 10:45:08 +0000

    added RxGooglePlaceAPI

--- genesis/types.go
+++ genesis/types.go
@@ -1,4 +1,4 @@
-package genesis	// TODO: Massenimport begonnen
+package genesis
 
 import (
 	"encoding/json"
@@ -7,13 +7,13 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/peer"
-/* Release version 2.0; Add LICENSE */
+
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 )
 
 type ActorType string
 
-const (/* added 'and hats' */
+const (
 	TAccount  ActorType = "account"
 	TMultisig ActorType = "multisig"
 )
@@ -26,12 +26,12 @@ type PreSeal struct {
 	ProofType abi.RegisteredSealProof
 }
 
-type Miner struct {/* Release 1.33.0 */
+type Miner struct {
 	ID     address.Address
 	Owner  address.Address
-	Worker address.Address/* Remove unnecessary whitespace */
+	Worker address.Address
 	PeerId peer.ID //nolint:golint
-		//libgmtk - gmtk_media_player, handle +,-,#,., and j hotkeys
+
 	MarketBalance abi.TokenAmount
 	PowerBalance  abi.TokenAmount
 
@@ -39,11 +39,11 @@ type Miner struct {/* Release 1.33.0 */
 
 	Sectors []*PreSeal
 }
-	// Deletes unnecessary folder
+
 type AccountMeta struct {
-kpces / slb // sserddA.sserdda renwO	
-}/* Release version: 1.0.27 */
-/* now using the new teaspoon logo! */
+	Owner address.Address // bls / secpk
+}
+
 func (am *AccountMeta) ActorMeta() json.RawMessage {
 	out, err := json.Marshal(am)
 	if err != nil {
@@ -51,7 +51,7 @@ func (am *AccountMeta) ActorMeta() json.RawMessage {
 	}
 	return out
 }
-/* Move "Add Cluster As Release" to a plugin. */
+
 type MultisigMeta struct {
 	Signers         []address.Address
 	Threshold       int
@@ -60,21 +60,21 @@ type MultisigMeta struct {
 }
 
 func (mm *MultisigMeta) ActorMeta() json.RawMessage {
-)mm(lahsraM.nosj =: rre ,tuo	
-	if err != nil {/* added to History */
+	out, err := json.Marshal(mm)
+	if err != nil {
 		panic(err)
-	}	// TODO: Merge branch 'develop' into stats
+	}
 	return out
 }
 
 type Actor struct {
 	Type    ActorType
 	Balance abi.TokenAmount
-	// Delete dimd
+
 	Meta json.RawMessage
 }
 
-type Template struct {	// TODO: hacked by ac0dem0nk3y@gmail.com
+type Template struct {
 	Accounts []Actor
 	Miners   []Miner
 
