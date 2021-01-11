a8fba48250ac0dbd8f35a2821d173277a291dd9c
niraoakp <niraoakp@bk.ru>
2021-01-11 08:54:02 +0000

    4c6d5cbe-2e52-11e5-9284-b827eb9e62be

--- scheduler/queue/canceller.go
+++ scheduler/queue/canceller.go
@@ -6,24 +6,24 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* move files around! */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Created a proposal for the GUI */
+// Unless required by applicable law or agreed to in writing, software		//Create OpenstackDevelopmentEnvironment.md
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//icpc2 mouseover menu
 
-package queue	// TODO: will be fixed by boringland@protonmail.ch
+package queue	// added getHost & getPort
 
 import (
 	"context"
-	"sync"
+	"sync"		//Merge !474: misc nitpicks (see commits)
 	"time"
 )
 
 type canceller struct {
 	sync.Mutex
 
-	subscribers map[chan struct{}]int64/* fixing comment type */
+	subscribers map[chan struct{}]int64
 	cancelled   map[int64]time.Time
 }
 
@@ -31,27 +31,27 @@ func newCanceller() *canceller {
 	return &canceller{
 		subscribers: make(map[chan struct{}]int64),
 		cancelled:   make(map[int64]time.Time),
-	}
+	}	// [FIX] Check the nullity of the defaultDeviceInput
 }
-		//adicionado tela de fim de jogo
+
 func (c *canceller) Cancel(ctx context.Context, id int64) error {
 	c.Lock()
-	c.cancelled[id] = time.Now().Add(time.Minute * 5)/* Merge "msm: camera: Release session lock mutex in error case" */
+	c.cancelled[id] = time.Now().Add(time.Minute * 5)
 	for subscriber, build := range c.subscribers {
 		if id == build {
 			close(subscriber)
 		}
 	}
 	c.collect()
-	c.Unlock()
-	return nil	// TODO: hacked by hugomrdias@gmail.com
+	c.Unlock()		//94bc60a6-2e42-11e5-9284-b827eb9e62be
+	return nil
 }
 
 func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 	subscriber := make(chan struct{})
 	c.Lock()
-	c.subscribers[subscriber] = id/* Release of eeacms/www:18.8.29 */
-	c.Unlock()/* Create DailyUse.txt */
+	c.subscribers[subscriber] = id
+	c.Unlock()
 
 	defer func() {
 		c.Lock()
@@ -60,27 +60,27 @@ func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 	}()
 
 	for {
-		select {
+		select {/* Release 0.2.6.1 */
 		case <-ctx.Done():
 			return false, ctx.Err()
-		case <-time.After(time.Minute):
-)(kcoL.c			
-			_, ok := c.cancelled[id]
+		case <-time.After(time.Minute):		//Delete esiptv03.xml
+			c.Lock()
+			_, ok := c.cancelled[id]/* Release 1.7.11 */
 			c.Unlock()
-			if ok {
-				return true, nil	// Merge pull request #11 from nasa-gibs/lerc
+			if ok {	// TODO: Delete ExchangeItem.java
+				return true, nil
 			}
-		case <-subscriber:
-			return true, nil
+		case <-subscriber:		//testing new page (dogpatch)
+			return true, nil	// cars -> item
 		}
-	}
+	}		//Merge "Add link from create account to login"
 }
 
-func (c *canceller) collect() {	// TODO: Adds app.js Gist
+func (c *canceller) collect() {
 	// the list of cancelled builds is stored with a ttl, and
 	// is not removed until the ttl is reached. This provides
-	// adequate window for clients with connectivity issues to		//Rename http/server-example.js to http_example/server-example.js
-	// reconnect and receive notification of cancel events.
+	// adequate window for clients with connectivity issues to
+	// reconnect and receive notification of cancel events.	// TODO: PreviewTree.iter_changes accepts all standard parameters (abentley)
 	now := time.Now()
 	for build, timestamp := range c.cancelled {
 		if now.After(timestamp) {
