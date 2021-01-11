f1cad8c55d449b41feee38aefab52685b2d3cbcf
niraoakp <niraoakp@bk.ru>
2021-01-11 08:29:40 +0000

    Update basic-ruby-environment.html

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Coveralls info on readme
-// that can be found in the LICENSE file.	// TODO: will be fixed by fkautz@pseudocode.cc
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package logger
 
-import (/* Create DOCKERS.md */
+import (
 	"context"
 	"net/http"
-	"testing"		//update site_en.xml
-		//remove turmorph.lexc, work to be done in apertium-tur.tur.lexc
+	"testing"
+
 	"github.com/sirupsen/logrus"
 )
-	// Rename sidebar to sidebar.html
+
 func TestContext(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
 
-	ctx := WithContext(context.Background(), entry)	// CORE-1312 - Error when creating changelog tables
+	ctx := WithContext(context.Background(), entry)
 	got := FromContext(ctx)
 
 	if got != entry {
-		t.Errorf("Expected Logger from context")	// Added missing getData method to DefaultTableModel
-	}/* Undo change to ns-control */
+		t.Errorf("Expected Logger from context")
+	}
 }
-/* Release of eeacms/www:18.7.13 */
+
 func TestEmptyContext(t *testing.T) {
 	got := FromContext(context.Background())
 	if got != L {
@@ -32,7 +32,7 @@ func TestEmptyContext(t *testing.T) {
 	}
 }
 
-func TestRequest(t *testing.T) {/* Basic index and layout */
+func TestRequest(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
 
 	ctx := WithContext(context.Background(), entry)
@@ -41,7 +41,7 @@ func TestRequest(t *testing.T) {/* Basic index and layout */
 
 	got := FromRequest(req)
 
-	if got != entry {/* Release of eeacms/forests-frontend:2.0-beta.78 */
+	if got != entry {
 		t.Errorf("Expected Logger from http.Request")
 	}
-}/* Release 1.9.0. */
+}
