815f8d4cd770224bb29917b59968f0d885b86d5d
niraoakp <niraoakp@bk.ru>
2021-01-11 18:46:57 +0000

    reduce diff to wine

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -3,12 +3,12 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Rename jqurey.js to js/ jqurey.js */
 package collabs
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"	// TODO: Allow BUILDKITE variables into Docker container for the benefit of logging
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,35 +18,35 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* Release v1.8.1 */
 )
-
-func TestDelete(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+		//41285f56-2e76-11e5-9284-b827eb9e62be
+func TestDelete(t *testing.T) {	// Adding specs
+	controller := gomock.NewController(t)		//Travis: Removed Node 0.9, added 0.11
+	defer controller.Finish()/* Update to sensitivity output for NBN download format. */
+/* Create B827EBFFFE72E7EE.JSON */
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)	// TODO: hacked by ac0dem0nk3y@gmail.com
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
-	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
+	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)	// TODO: will be fixed by nicksavers@gmail.com
 
-	c := new(chi.Context)
+	c := new(chi.Context)	// c0aa7c9a-2e40-11e5-9284-b827eb9e62be
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")/* Forgot to change cabal file, naturally */
 	c.URLParams.Add("member", "octocat")
-
+	// TODO: will be fixed by nagydani@epointsystem.org
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
+	)	// Fixing avatar urls
+	// TODO: Add nl.ts to the .pro file.
 	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: Merge "Stop setting nova.conf settings that mean nothing"
 	}
 }
 
