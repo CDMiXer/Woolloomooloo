becef318fe25b9b8a57f7f20750486538dcce6f4
niraoakp <niraoakp@bk.ru>
2021-01-11 12:27:55 +0000

    K&R style for functions

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by jon@atack.com
+
 import * as pulumi from "@pulumi/pulumi";
-	// SwingCaption performance: cache bold fonts
-let config = new pulumi.Config();		//Delete metaprog.py
-let org = config.require("org");/* Python: minor code tidy. */
+
+let config = new pulumi.Config();	// 62aa5cc6-2e4c-11e5-9284-b827eb9e62be
+let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
 let gotError = false;
 try
 {
-    a.getOutputSync("val2");/* Server URL move */
+    a.getOutputSync("val2");
 }
 catch (err)
-{/* Release under license GPLv3 */
+{
     gotError = true;
 }
 
-if (!gotError) {	// TODO: will be fixed by martin2cai@hotmail.com
+if (!gotError) {
     throw new Error("Expected to get error trying to read secret from stack reference.");
-}/* Bump scala version to 2.11.1 */
+}	// TODO: hacked by boringland@protonmail.ch
