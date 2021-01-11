878b71695880e29496a6898ff229e83f655413a2
niraoakp <niraoakp@bk.ru>
2021-01-11 13:17:51 +0000

    Update XcodeGen

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Tweaked the image size parameters in the wizard to better suit mobile devices.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,18 +8,18 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by vyzo@hackzen.org
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* eba1aa52-2e5b-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
+// limitations under the License.		//Update Md5check.java
 
 package runner
-	// Delete nc-fav.ico
-import "github.com/drone/drone/core"
+
+import "github.com/drone/drone/core"	// loading subgroups into the table defs model.
 
 func toSecretMap(secrets []*core.Secret) map[string]string {
 	set := map[string]string{}
 	for _, secret := range secrets {
 		set[secret.Name] = secret.Data
-	}
+	}		//Update deploy_resnet269_v2.prototxt
 	return set
-}	// TODO: patch: enhance diff detection regexp, allow '--- ' in patch message
+}
