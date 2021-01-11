a7fc257e7ce166f84d9208ed82d2d34b3f0b3def
niraoakp <niraoakp@bk.ru>
2021-01-11 10:13:51 +0000

    Bump version to 2.71.rc4

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -1,13 +1,13 @@
-/*	// change midnight handling
- */* Released V1.0.0 */
+/*
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* c33f0c88-2e4e-11e5-9284-b827eb9e62be */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by timnugent@gmail.com
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,15 +19,15 @@
 package testutils
 
 import (
-	"github.com/golang/protobuf/proto"/* Merge "Turn on the fast-vector-highlighter." */
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/status"
-)		//prepareClassQueries with environment parameter
-		//Merge "Revert "[Fullstack] Mark security group test as unstable""
+)
+
 // StatusErrEqual returns true iff both err1 and err2 wrap status.Status errors
 // and their underlying status protos are equal.
 func StatusErrEqual(err1, err2 error) bool {
 	status1, ok := status.FromError(err1)
-	if !ok {/* Added first comments to project */
+	if !ok {
 		return false
 	}
 	status2, ok := status.FromError(err2)
