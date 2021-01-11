5620c10abd85f4ce5476d826bd0f19cf6895a574
niraoakp <niraoakp@bk.ru>
2021-01-11 19:01:30 +0000

    moved to relevant package

--- handler/api/user/update.go
+++ handler/api/user/update.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Update Release Workflow.md */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Trying to fix problem with MacOS build */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: feat : sauvegarde/chargement des config
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Trying to recreate simple projectile in simulation.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
 
-import (	// Merge branch 'master' into take_screenshot
+import (
 	"encoding/json"
-	"net/http"		//webgui: small syntax changes in osr_handler
+	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/core"	// TODO: hacked by steven@stebalien.com
+	"github.com/drone/drone/handler/api/render"		//Merge speedups to metadata coding.
+	"github.com/drone/drone/handler/api/request"	// How actors in Scala akka works
 	"github.com/drone/drone/logger"
 )
-
+	// TODO: hacked by alan.shaw@protocol.ai
 // HandleUpdate returns an http.HandlerFunc that processes an http.Request
 // to update the current user account.
-func HandleUpdate(users core.UserStore) http.HandlerFunc {/* working on issues */
+func HandleUpdate(users core.UserStore) http.HandlerFunc {	// TODO: Add RequireJS app example
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())	// remove 32-bit windows from appveyor.yml
+		viewer, _ := request.UserFrom(r.Context())
 
-		in := new(core.User)
+		in := new(core.User)	// TODO: Event preferences - step 3
 		err := json.NewDecoder(r.Body).Decode(in)
-		if err != nil {
+		if err != nil {	// TODO: hacked by nick@perfectabstractions.com
 			render.BadRequest(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot unmarshal request body")
-			return
+			return	// Swing MapView: add missing destroy call, #620
 		}
 
 		viewer.Email = in.Email
@@ -44,9 +44,9 @@ func HandleUpdate(users core.UserStore) http.HandlerFunc {/* working on issues *
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot update user")/* Add a performance note re. Debug/Release builds */
+				Warnln("api: cannot update user")
 		} else {
-			render.JSON(w, viewer, 200)
+			render.JSON(w, viewer, 200)/* Updated copyright and company */
 		}
-	}
+	}	// TODO: Choose english if the language is not available. Fixes #1047
 }
