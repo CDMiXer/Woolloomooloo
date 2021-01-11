b1eaf4107ba9b5b34a681fc6937512422ad10b40
niraoakp <niraoakp@bk.ru>
2021-01-11 05:33:29 +0000

    Adding external SAP & IP free floating metrics.

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Update gregorianDekatrian.php
+// Copyright 2019 Drone.IO Inc. All rights reserved./* HDF5 bugfixed */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//57b1ee74-2e47-11e5-9284-b827eb9e62be
+// +build !oss
 
 package metric
