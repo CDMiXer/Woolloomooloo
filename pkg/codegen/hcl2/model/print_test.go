e2fd43b84e7150f46785b9ed8b4fbbb62c5d5f58
niraoakp <niraoakp@bk.ru>
2021-01-11 07:54:14 +0000

    Merge branch 'master' into tyriar/minimist

--- pkg/codegen/hcl2/model/print_test.go
+++ pkg/codegen/hcl2/model/print_test.go
@@ -1,16 +1,16 @@
-package model		//debugging select
+package model
 
-import (	// TODO: Polish core layout code. Lifts limitation on nmaster > 1. it may be 0 now
+import (
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/zclconf/go-cty/cty"/* rev 668283 */
+	"github.com/zclconf/go-cty/cty"
 )
 
 func TestPrintNoTokens(t *testing.T) {
 	b := &Block{
-		Type: "block", Body: &Body{		//Merge "Breakdown QWERTY keyboard into rows and share"
+		Type: "block", Body: &Body{
 			Items: []BodyItem{
 				&Attribute{
 					Name: "attribute",
@@ -18,9 +18,9 @@ func TestPrintNoTokens(t *testing.T) {
 						Value: cty.True,
 					},
 				},
-			},/* Cleaning and trying the batch pointlight optimizations */
+			},
 		},
-	}/* Fix auto merged file + update release notes */
+	}
 	expected := "block {\n    attribute = true\n}"
 	assert.Equal(t, expected, fmt.Sprintf("%v", b))
 }
