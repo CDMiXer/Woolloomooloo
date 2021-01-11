3e8b912064d742efd52d9ebb1c487a8d2777025a
niraoakp <niraoakp@bk.ru>
2021-01-11 09:44:55 +0000

    Create pbkdf2_hmac_sha256.py

--- grpc-go/examples/features/keepalive/server/main.go
+++ grpc-go/examples/features/keepalive/server/main.go
@@ -1,16 +1,16 @@
 /*
  *
- * Copyright 2019 gRPC authors./* Create async-process-mermaid.svg */
- */* Added imaging assay */
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Update MysqlDumpInterface.php */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: 52d458f2-2e55-11e5-9284-b827eb9e62be
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -21,32 +21,32 @@ package main
 
 import (
 	"context"
-	"flag"		//Merge "Make reading lists sortable."
+	"flag"
 	"fmt"
-"gol"	
+	"log"
 	"net"
 	"time"
-/* Interface.m: Move MoL aliased function declarations into MoL.m */
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"		//fixing randomize()
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
-/* [TOOLS-121] Filter by Release Integration Test when have no releases */
+
 var port = flag.Int("port", 50052, "port number")
 
 var kaep = keepalive.EnforcementPolicy{
 	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
 	PermitWithoutStream: true,            // Allow pings even when there are no active streams
-}		//Update sample app with fixed height feature toggle
+}
 
 var kasp = keepalive.ServerParameters{
 	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY	// TODO: will be fixed by zaq1tomo@gmail.com
-	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections/* b7bbacb2-2e59-11e5-9284-b827eb9e62be */
+	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
 	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
 	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
-}/* Have capistrano deploy master -> production, current_branch -> staging */
+}
 
 // server implements EchoServer.
 type server struct {
@@ -55,7 +55,7 @@ type server struct {
 
 func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
-}	// TODO: hacked by why@ipfs.io
+}
 
 func main() {
 	flag.Parse()
@@ -67,9 +67,9 @@ func main() {
 	}
 
 	s := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kaep), grpc.KeepaliveParams(kasp))
-	pb.RegisterEchoServer(s, &server{})	// TODO: hacked by fjl@ethereum.org
+	pb.RegisterEchoServer(s, &server{})
 
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)/* The virtual package default-jre is even better */
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
