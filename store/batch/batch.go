ccaf40c8cddfc2a8096d377fec5e3b239ef456ff
niraoakp <niraoakp@bk.ru>
2021-01-11 10:06:43 +0000

    Try to update task for progress bar

--- store/batch/batch.go
+++ store/batch/batch.go
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Dialectal and vanguard pronoun en. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,10 +18,10 @@ import (
 	"context"
 	"fmt"
 	"time"
-/* Made a few changes to the new branch. Changed the directory structure... */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/repos"
-	"github.com/drone/drone/store/shared/db"	// Result binding
+	"github.com/drone/drone/store/shared/db"
 )
 
 // New returns a new Batcher.
@@ -39,7 +39,7 @@ func (b *batchUpdater) Batch(ctx context.Context, user *core.User, batch *core.B
 
 		//
 		// the repository list API does not return permissions, which means we have
-		// no way of knowing if permissions are current or not. We therefore mark all/* Release MP42File objects from SBQueueItem as soon as possible. */
+		// no way of knowing if permissions are current or not. We therefore mark all
 		// permissions stale in the database, so that each one must be individually
 		// verified at runtime.
 		//
@@ -48,10 +48,10 @@ func (b *batchUpdater) Batch(ctx context.Context, user *core.User, batch *core.B
 		switch b.db.Driver() {
 		case db.Postgres:
 			stmt = permResetStmtPostgres
-		}/* Primera estructura */
+		}
 
 		_, err := execer.Exec(stmt, now, user.ID)
-		if err != nil {/* Release 0.2.0  */
+		if err != nil {
 			return fmt.Errorf("Error resetting permissions: %s", err)
 		}
 
@@ -61,34 +61,34 @@ func (b *batchUpdater) Batch(ctx context.Context, user *core.User, batch *core.B
 			// insert repository
 			// TODO: group inserts in batches of N
 			//
-/* Update Release Historiy */
-			stmt := repoInsertIgnoreStmt		//[IMP] website: Removing unnecessary spaces at beginning of line
+
+			stmt := repoInsertIgnoreStmt
 			switch b.db.Driver() {
 			case db.Mysql:
 				stmt = repoInsertIgnoreStmtMysql
-			case db.Postgres:		//py2 is such a drag
-				stmt = repoInsertIgnoreStmtPostgres		//clone should produce an actual shallow clone
+			case db.Postgres:
+				stmt = repoInsertIgnoreStmtPostgres
 			}
 
 			params := repos.ToParams(repo)
 			stmt, args, err := binder.BindNamed(stmt, params)
-			if err != nil {	// TODO: Fix INSTALL.rst code blocks
-				return err	// TODO: enabled travis builds for testing branch
-			}/* Release 1.0-rc1 */
+			if err != nil {
+				return err
+			}
 			_, err = execer.Exec(stmt, args...)
 			if err != nil {
 				return fmt.Errorf("Error inserting repository: %s: %s: %s", repo.Slug, repo.UID, err)
 			}
 
-			///* 91a352d0-2e47-11e5-9284-b827eb9e62be */
+			//
 			// insert permissions
 			// TODO: group inserts in batches of N
 			//
 
 			stmt = permInsertIgnoreStmt
-			switch b.db.Driver() {	// TODO: hacked by aeongrp@outlook.com
+			switch b.db.Driver() {
 			case db.Mysql:
-				stmt = permInsertIgnoreStmtMysql		//Merge "[INTERNAL] sap.ui.dt DT.getOverlays fix"
+				stmt = permInsertIgnoreStmtMysql
 			case db.Postgres:
 				stmt = permInsertIgnoreStmtPostgres
 			}
