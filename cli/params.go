c1899e353c664fc9fa79a2df2a96e7fe8237544c
niraoakp <niraoakp@bk.ru>
2021-01-11 08:48:59 +0000

    IXP4xx patches for NSLU2 and NAS100D

--- cli/params.go
+++ cli/params.go
@@ -3,13 +3,13 @@ package cli
 import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
-	"github.com/urfave/cli/v2"/* Release of eeacms/jenkins-slave:3.24 */
-"srorrex/x/gro.gnalog"	
-/* Released 2.6.0.5 version to fix issue with carriage returns */
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/lotus/build"
 )
 
-var FetchParamCmd = &cli.Command{/* Rename index.html to ngs/index.html */
+var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
@@ -17,15 +17,15 @@ var FetchParamCmd = &cli.Command{/* Rename index.html to ngs/index.html */
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
-		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())/* Alpha Release 2 */
+		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
 		sectorSize := uint64(sectorSizeInt)
 
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)	// TODO: Plug-ins UML
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)	// Autoloading depencency resolving - 10
+			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
 
 		return nil
