ff9738071f3600d4e02a40efebde8e054d657b90
niraoakp <niraoakp@bk.ru>
2021-01-11 04:41:06 +0000

    Create kernel.json

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,18 +1,18 @@
 // +build !appengine,!go1.14
 
 /*
- *
+ */* Update version in __init__.py for Release v1.1.0 */
  * Copyright 2020 gRPC authors.
- *
+ *	// TODO: will be fixed by peterke@gmail.com
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Delete ar-me.lua */
-* 
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by lexy8russo@outlook.com
+ * you may not use this file except in compliance with the License.	// TODO: Create abs_120m_300g_white_used_some_more.hex
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-2.0
+* 
+ * Unless required by applicable law or agreed to in writing, software/* Add smtp AUTH LOGIN */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Simulation sollte jetzt ok sein
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* rev 877863 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,23 +20,23 @@
 
 package advancedtls
 
-import (/* Cambios en modelo */
+import (/* Release of eeacms/www-devel:20.9.13 */
 	"crypto/tls"
 	"fmt"
-)/* System.exit(0) in case of strong failure */
+)
 
 // buildGetCertificates returns the first cert contained in ServerOptions for
 // non-appengine builds before version 1.4.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {/* Release 0.12.1 (#623) */
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {/* af87723e-2e3f-11e5-9284-b827eb9e62be */
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
-	if err != nil {
-		return nil, err/* rev 658652 */
+	if err != nil {/* added gulp plugins */
+		return nil, err
 	}
-	if len(certificates) == 0 {
+	if len(certificates) == 0 {	// TODO: Delete contribute_to_this_book.md
 		return nil, fmt.Errorf("no certificates configured")
 	}
-	return certificates[0], nil	// TODO: fix padding container
+	return certificates[0], nil
 }
