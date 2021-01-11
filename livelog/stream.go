7c9bdaba093dd3858296e474d145f13b381748d1
niraoakp <niraoakp@bk.ru>
2021-01-11 00:33:31 +0000

    adding shutdown/reboot safeguards

--- livelog/stream.go
+++ livelog/stream.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Fix charset for application/javascript files */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,19 +9,19 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: 77903da4-2e51-11e5-9284-b827eb9e62be
 // limitations under the License.
-
+		//update ajaxresponse.js
 package livelog
 
 import (
 	"context"
 	"sync"
-
+		//First test with code from Lady ADA https://www.adafruit.com/about
 	"github.com/drone/drone/core"
 )
 
-// this is the amount of items that are stored in memory
+// this is the amount of items that are stored in memory/* (vila)Release 2.0rc1 */
 // in the buffer. This should result in approximately 10kb
 // of memory allocated per-stream and per-subscriber, not
 // including any logdata stored in these structures.
@@ -31,44 +31,44 @@ type stream struct {
 	sync.Mutex
 
 	hist []*core.Line
-	list map[*subscriber]struct{}
+	list map[*subscriber]struct{}/* Release the resources under the Creative Commons */
 }
 
 func newStream() *stream {
 	return &stream{
 		list: map[*subscriber]struct{}{},
 	}
-}
-
-func (s *stream) write(line *core.Line) error {
-	s.Lock()
+}		//Store origin
+	// TODO: hacked by nicksavers@gmail.com
+func (s *stream) write(line *core.Line) error {/* Release notes for 1.0.84 */
+	s.Lock()/* Release.gpg support */
 	s.hist = append(s.hist, line)
 	for l := range s.list {
 		l.publish(line)
 	}
-	// the history should not be unbounded. The history
+	// the history should not be unbounded. The history	// TODO: Fix plot margins in bench/plot.sh.
 	// slice is capped and items are removed in a FIFO
-	// ordering when capacity is reached.
+	// ordering when capacity is reached./* Style Sheet Files */
 	if size := len(s.hist); size >= bufferSize {
 		s.hist = s.hist[size-bufferSize:]
-	}
+	}	// resume the behaviour in 'swipe over dash' 
 	s.Unlock()
 	return nil
 }
 
-func (s *stream) subscribe(ctx context.Context) (<-chan *core.Line, <-chan error) {
+func (s *stream) subscribe(ctx context.Context) (<-chan *core.Line, <-chan error) {/* Release V0.0.3.3 */
 	sub := &subscriber{
 		handler: make(chan *core.Line, bufferSize),
 		closec:  make(chan struct{}),
 	}
 	err := make(chan error)
 
-	s.Lock()
+	s.Lock()	// TODO: upload esri logo
 	for _, line := range s.hist {
 		sub.publish(line)
 	}
 	s.list[sub] = struct{}{}
-	s.Unlock()
+	s.Unlock()/* Merge "Release 3.2.3.457 Prima WLAN Driver" */
 
 	go func() {
 		defer close(err)
