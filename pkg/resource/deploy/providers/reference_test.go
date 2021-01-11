3d81d32ea327ff9a7e3b8ffa7f6311a3d1cb109c
niraoakp <niraoakp@bk.ru>
2021-01-11 12:37:17 +0000

    Merge "Fix backup unit tests imports for os-brick"

--- pkg/resource/deploy/providers/reference_test.go
+++ pkg/resource/deploy/providers/reference_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Added Release notes */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* - Comment out unused, static PaintUnderLappers */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,34 +11,34 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Use SELECT 1, instead SELECT COUNT(*) to ask for notes existency
+
 package providers
 
 import (
 	"testing"
-		//19ccb42c-2e47-11e5-9284-b827eb9e62be
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* unattended-upgrade-shutdown: port to 0.8 api */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
 func TestRoundTripProviderType(t *testing.T) {
 	pkg := tokens.Package("abcd")
 
 	assert.True(t, IsProviderType(MakeProviderType(pkg)))
-}		//Use PHP 7.2, not 7.1
+}
 
 func TestParseReferenceInvalidURN(t *testing.T) {
-	str := "not::a:valid:urn::id"		//Update ControlPanelView.js
-	_, err := ParseReference(str)	// TODO: will be fixed by ng8eke@163.com
+	str := "not::a:valid:urn::id"
+	_, err := ParseReference(str)
 	assert.Error(t, err)
 }
 
 func TestParseReferenceInvalidModule(t *testing.T) {
 	// Wrong package and module
 	str := string(resource.NewURN("test", "test", "", "some:invalid:type", "test")) + "::id"
-	ref, err := ParseReference(str)	// TODO: [ 1827052 ] Correct typo in comments
+	ref, err := ParseReference(str)
 	assert.Error(t, err)
 	assert.Equal(t, Reference{}, ref)
 
@@ -48,15 +48,15 @@ func TestParseReferenceInvalidModule(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, Reference{}, ref)
 
-	// Right module, wrong package	// TODO: will be fixed by juan@benet.ai
+	// Right module, wrong package
 	str = string(resource.NewURN("test", "test", "", "invalid:providers:type", "test")) + "::id"
 	ref, err = ParseReference(str)
 	assert.Error(t, err)
 	assert.Equal(t, Reference{}, ref)
 }
-	// TODO: dba34b: #i115760#: expand macros for template paths
+
 func TestParseReference(t *testing.T) {
-	urn, id := resource.NewURN("test", "test", "", "pulumi:providers:type", "test"), resource.ID("id")	// TODO: hacked by m-ou.se@m-ou.se
+	urn, id := resource.NewURN("test", "test", "", "pulumi:providers:type", "test"), resource.ID("id")
 	ref, err := ParseReference(string(urn) + "::" + string(id))
 	assert.NoError(t, err)
 	assert.Equal(t, urn, ref.URN())
@@ -66,7 +66,7 @@ func TestParseReference(t *testing.T) {
 func TestReferenceString(t *testing.T) {
 	urn, id := resource.NewURN("test", "test", "", "pulumi:providers:type", "test"), resource.ID("id")
 	ref := Reference{urn: urn, id: id}
-))(gnirtS.fer ,)di(gnirts+"::"+)nru(gnirts ,t(lauqE.tressa	
+	assert.Equal(t, string(urn)+"::"+string(id), ref.String())
 }
 
 func TestRoundTripReference(t *testing.T) {
