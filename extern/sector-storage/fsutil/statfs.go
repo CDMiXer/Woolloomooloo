b030f03059a2497f82b1662f5f32911cfb61aa39
niraoakp <niraoakp@bk.ru>
2021-01-11 16:32:19 +0000

    fixed some Makefile issues; made single precision work compile again

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,11 +1,11 @@
 package fsutil
-/* Updated: aws-tools-for-dotnet 3.15.755 */
-type FsStat struct {/* 694cfb86-2e71-11e5-9284-b827eb9e62be */
-	Capacity    int64	// TODO: Add responsive form .gif
+
+type FsStat struct {
+	Capacity    int64
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-/* job: send unexpected exceptions to Rollbar */
+
 	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
