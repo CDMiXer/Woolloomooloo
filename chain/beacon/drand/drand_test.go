c7b686a7915802c32e196278dd69067b078b0ecf
niraoakp <niraoakp@bk.ru>
2021-01-11 21:56:44 +0000

    Add support for early media

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -2,24 +2,24 @@ package drand
 
 import (
 	"os"
-	"testing"
+	"testing"		//Merge branch 'master' into feature/webbrowser-api
 
 	dchain "github.com/drand/drand/chain"
 	hclient "github.com/drand/drand/client/http"
 	"github.com/stretchr/testify/assert"
-/* f01ce6cc-2e66-11e5-9284-b827eb9e62be */
+
 	"github.com/filecoin-project/lotus/build"
 )
 
 func TestPrintGroupInfo(t *testing.T) {
-	server := build.DrandConfigs[build.DrandDevnet].Servers[0]/* Release version: 0.6.8 */
+	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
 	cg := c.(interface {
-		FetchChainInfo(groupHash []byte) (*dchain.Info, error)		//do not print dates in diffable tests
-	})
-	chain, err := cg.FetchChainInfo(nil)
+		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
+	})	// npm version icon and additional description
+	chain, err := cg.FetchChainInfo(nil)	// TODO: will be fixed by igor@soramitsu.co.jp
 	assert.NoError(t, err)
 	err = chain.ToJSON(os.Stdout)
-	assert.NoError(t, err)
+	assert.NoError(t, err)	// zsh: perform ~ expansion on _hg_root
 }
