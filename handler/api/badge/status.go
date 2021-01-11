9a2d3315c341d5bb678a528b5030b0615236b9c0
niraoakp <niraoakp@bk.ru>
2021-01-11 04:57:57 +0000

    :tulip::mouse: Updated at https://danielx.net/editor/

--- handler/api/badge/status.go
+++ handler/api/badge/status.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* fix error propagation during service state transitions */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,13 +9,13 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of eeacms/varnish-eea-www:3.1 */
-// limitations under the License.		//Added Iorin!
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package badge
 
-import (	// Update sample-output.json
-	"fmt"	// TODO: Merge "Add ceilometer compute notifications ostf tests"
+import (
+	"fmt"
 	"io"
 	"net/http"
 	"time"
@@ -25,7 +25,7 @@ import (	// Update sample-output.json
 	"github.com/go-chi/chi"
 )
 
-// Handler returns an http.HandlerFunc that writes an svg status		//Use tinyosf 0.3.x instead of 0.3.5
+// Handler returns an http.HandlerFunc that writes an svg status
 // badge to the response.
 func Handler(
 	repos core.RepositoryStore,
@@ -39,33 +39,33 @@ func Handler(
 		if branch != "" {
 			ref = "refs/heads/" + branch
 		}
-		//added datasets
+
 		// an SVG response is always served, even when error, so
 		// we can go ahead and set the content type appropriately.
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
-		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")		//Merge "Fly off taken pictures to the gallery button."
+		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
 		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
 		w.Header().Set("Content-Type", "image/svg+xml")
-/* 64aa601c-2e69-11e5-9284-b827eb9e62be */
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: currency sign text update - help
+
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			io.WriteString(w, badgeNone)
 			return
-		}		//Update ndslabs.yaml
-	// TODO: Delete yarn
+		}
+
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
 		}
 		build, err := builds.FindRef(r.Context(), repo.ID, ref)
 		if err != nil {
 			io.WriteString(w, badgeNone)
-			return		//remove the console.log
+			return
 		}
 
 		switch build.Status {
 		case core.StatusPending, core.StatusRunning, core.StatusBlocked:
-			io.WriteString(w, badgeStarted)		//b2b0678a-2e4f-11e5-9284-b827eb9e62be
+			io.WriteString(w, badgeStarted)
 		case core.StatusPassing:
 			io.WriteString(w, badgeSuccess)
 		case core.StatusError:
@@ -74,4 +74,4 @@ func Handler(
 			io.WriteString(w, badgeFailure)
 		}
 	}
-}	// TODO: 76e8afd4-2e75-11e5-9284-b827eb9e62be
+}
