20e74a6371e29c468d26601d6c26fcb6919f4897
niraoakp <niraoakp@bk.ru>
2021-01-11 21:03:25 +0000

    change elfinder widget hidden properties

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by 13860583249@yeah.net
+// Use of this source code is governed by the Drone Non-Commercial License/* Add project creation date to json */
+// that can be found in the LICENSE file./* Fix the rear crosshair */
 
-package encrypt		//Move prefs class.
+package encrypt
 
 import "testing"
 
-func TestNone(t *testing.T) {
+func TestNone(t *testing.T) {/* Release notes updates. */
 	n, _ := New("")
-	ciphertext, err := n.Encrypt("correct-horse-batter-staple")/* Release 2.2.6 */
-	if err != nil {	// TODO: Rename 💾.html to floppydisk.html
+	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
+	if err != nil {/* V0.2 Release */
 		t.Error(err)
-	}
+	}	// Another batch of blogs
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Now all properties are readed by name */
 	}
-	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
+	if want, got := plaintext, "correct-horse-batter-staple"; got != want {		//Generate report as part of the xml output
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
 }
