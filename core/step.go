0e4a4cf336b84bc1841c1fb22ea78faa0415e216
niraoakp <niraoakp@bk.ru>
2021-01-11 10:54:43 +0000

    One little fixes, use MP4ConvertToTrackDuration only with positive number

--- core/step.go
+++ core/step.go
@@ -3,21 +3,21 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Fixes full report to display percent visited */
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge pull request #466 from pradyu1993/develop
 //
+//      http://www.apache.org/licenses/LICENSE-2.0/* Make home page responsive */
+//	// TODO: hacked by vyzo@hackzen.org
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Merge branch 'dev' into 1203-fix-facebook-preview
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Fix deprecated spawnCreature. Fixes BUKKIT-1880
+// limitations under the License.
 
-package core
-	// TODO: hacked by hugomrdias@gmail.com
-import "context"/* Release 0.2.0-beta.3 */
-/* Handle errors on callback on BookKeeperCommitLog #110 */
-type (
-	// Step represents an individual step in the stage./* Move luminance calculation out to Utils.Style */
+package core/* Add SmartCash */
+/* Release version [10.0.1] - prepare */
+import "context"
+
+type (	// Fix add message to explain why some emails fails
+	// Step represents an individual step in the stage.
 	Step struct {
 		ID        int64  `json:"id"`
 		StageID   int64  `json:"step_id"`
@@ -26,39 +26,39 @@ type (
 		Status    string `json:"status"`
 		Error     string `json:"error,omitempty"`
 		ErrIgnore bool   `json:"errignore,omitempty"`
-		ExitCode  int    `json:"exit_code"`
-		Started   int64  `json:"started,omitempty"`/* Create LogLevel.php */
+		ExitCode  int    `json:"exit_code"`/* read_stdin_json */
+		Started   int64  `json:"started,omitempty"`	// cmd/jujud: add JobServeAPI to dead machine test
 		Stopped   int64  `json:"stopped,omitempty"`
-		Version   int64  `json:"version"`/* configuration: Update Release notes */
-	}
-/* Cleaning up from pychecker. */
-	// StepStore persists build step information to storage.
+		Version   int64  `json:"version"`
+	}/* Releases 1.0.0. */
+
+	// StepStore persists build step information to storage./* Potential 1.6.4 Release Commit. */
 	StepStore interface {
 		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Step, error)
 
-		// Find returns a build stage from the datastore by ID./* Release and getting commands */
-		Find(context.Context, int64) (*Step, error)	// TODO: 78a20910-2d53-11e5-baeb-247703a38240
-
+		// Find returns a build stage from the datastore by ID.
+		Find(context.Context, int64) (*Step, error)
+	// TODO: hacked by 13860583249@yeah.net
 		// FindNumber returns a stage from the datastore by number.
-		FindNumber(context.Context, int64, int) (*Step, error)
+		FindNumber(context.Context, int64, int) (*Step, error)/* Update X-Raym_Round selected items volume - one decimal.eel */
 
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
 
-		// Update persists an updated stage to the datastore.
+		// Update persists an updated stage to the datastore./* Released to version 1.4 */
 		Update(context.Context, *Step) error
 	}
 )
 
 // IsDone returns true if the step has a completed state.
-func (s *Step) IsDone() bool {/* Disable suffocation damage temporarily */
-	switch s.Status {		//docs(README.md): keys
+func (s *Step) IsDone() bool {
+	switch s.Status {
 	case StatusWaiting,
 		StatusPending,
 		StatusRunning,
-		StatusBlocked:
-		return false		//improved testcases and added support for streams/resources
+		StatusBlocked:		//fix another npe
+		return false
 	default:
 		return true
 	}
