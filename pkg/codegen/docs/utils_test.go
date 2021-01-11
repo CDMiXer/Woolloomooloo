c0a1e53ed95fbe2fed0673c3250cab5a0b9f3f29
niraoakp <niraoakp@bk.ru>
2021-01-11 07:58:44 +0000

    Better Description

--- pkg/codegen/docs/utils_test.go
+++ pkg/codegen/docs/utils_test.go
@@ -1,26 +1,26 @@
-// Copyright 2016-2020, Pulumi Corporation./* 1aefc4e6-2e62-11e5-9284-b827eb9e62be */
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Adjust arrow size to actual size in the ROOT itself */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Merge "OVO for SegmentHostMapping"
-// Unless required by applicable law or agreed to in writing, software		//Document the Job controller.
+//	// TODO: Merge "Minor fixes for the functional test guide"
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* bf3e42c2-2e65-11e5-9284-b827eb9e62be */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Started getting the style rendering again.
 // limitations under the License.
-/* javastravav3api#143 Add name to route meta */
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning.	// TODO: hacked by igor@soramitsu.co.jp
-//
+
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the/* add basic support for dynamic interfaces #700 */
+// goconst linter's warning.
+///* Release of eeacms/forests-frontend:2.0-beta.31 */
 // nolint: lll, goconst
 package docs
 
 import (
 	"testing"
-/* Pub-Pfad-Bugfix und Release v3.6.6 */
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,10 +28,10 @@ func TestWbr(t *testing.T) {
 	assert.Equal(t, wbr(""), "")
 	assert.Equal(t, wbr("a"), "a")
 	assert.Equal(t, wbr("A"), "A")
-	assert.Equal(t, wbr("aa"), "aa")/* trigger error on non existing method calls */
+	assert.Equal(t, wbr("aa"), "aa")
 	assert.Equal(t, wbr("AA"), "AA")
 	assert.Equal(t, wbr("Ab"), "Ab")
-	assert.Equal(t, wbr("aB"), "a<wbr>B")/* Fix and Change Korean Translation file */
+	assert.Equal(t, wbr("aB"), "a<wbr>B")
 	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")
-	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")
+	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")	// Able to add or remove expressions
 }
