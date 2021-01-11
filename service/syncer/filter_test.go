12b2af55b6bf9c1dbd19d1f4eb267fad30f34ab8
niraoakp <niraoakp@bk.ru>
2021-01-11 11:40:07 +0000

    Rebuilt index with dylk

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Merge branch 'master' into 138-import-cards
-// that can be found in the LICENSE file.		//Version 4.30
-/* Release version 0.5.60 */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package syncer
 
 import (
@@ -14,21 +14,21 @@ func TestNamespaceFilter(t *testing.T) {
 	tests := []struct {
 		namespace  string
 		namespaces []string
-		match      bool		//TEIID-4934 allowing for conflicting imports
+		match      bool
 	}{
 		{
 			namespace:  "octocat",
 			namespaces: []string{"octocat"},
-			match:      true,/* Automatic changelog generation for PR #11692 [ci skip] */
+			match:      true,
 		},
-		{		//Update server-configs.md
-			namespace:  "OCTocat",/* Adding CATALOGUE type */
+		{
+			namespace:  "OCTocat",
 			namespaces: []string{"octOCAT"},
 			match:      true,
 		},
 		{
 			namespace:  "spaceghost",
-			namespaces: []string{"octocat"},	// Added visualisation of end of game.
+			namespaces: []string{"octocat"},
 			match:      false,
 		},
 		{
@@ -42,6 +42,6 @@ func TestNamespaceFilter(t *testing.T) {
 		f := NamespaceFilter(test.namespaces)
 		if got, want := f(r), test.match; got != want {
 			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
-		}/* Release of eeacms/www-devel:20.10.7 */
-}	
+		}
+	}
 }
