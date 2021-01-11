4b9e6445e679f0c2c1eb63c915ee41414f5939c1
niraoakp <niraoakp@bk.ru>
2021-01-11 04:10:17 +0000

    Large edit of README copy

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//first epub tutorial
 // You may obtain a copy of the License at
-//
+//		//Delete reset_y.css
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added null checks to oldState->Release in OutputMergerWrapper. Fixes issue 536. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//da47feae-2e5c-11e5-9284-b827eb9e62be
 // limitations under the License.
 
 package webhook
@@ -20,6 +20,6 @@ import "github.com/drone/drone/core"
 type Config struct {
 	Events   []string
 	Endpoint []string
-	Secret   string/* Release 3.0.9 */
+	Secret   string
 	System   *core.System
-}	// TODO: hacked by alex.gaynor@gmail.com
+}/* Re #292346 Release Notes */
