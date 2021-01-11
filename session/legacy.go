255195cf6290af7591cb29e830b545659bcbd7fa
niraoakp <niraoakp@bk.ru>
2021-01-11 14:37:43 +0000

    Merge "wlan: Populate IE into Beacon Report"

--- session/legacy.go
+++ session/legacy.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fix broken equal signs in README
-// you may not use this file except in compliance with the License./* Release of eeacms/www:18.3.21 */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Comment out unused AudacityProject::OnSplitLabelsToTracks(). */
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by souzau@yandex.com
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update to iView version 359.
+//      http://www.apache.org/licenses/LICENSE-2.0		//Add reported field to TrafficModel
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release of eeacms/www-devel:20.5.26 */
 // limitations under the License.
-		//finishing touches on dayplot_magic, update notebooks #560
-package session
 
+package session
+/* Release of eeacms/redmine-wikiman:1.14 */
 import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
-	"net/http"	// TODO: MEDIUM / Improved selection management
+	"net/http"	// 5fc0d81c-2e49-11e5-9284-b827eb9e62be
 
 	"github.com/drone/drone/core"
 
 	"github.com/dgrijalva/jwt-go"
-)/* Added isEmpty() */
+)
 
 type legacy struct {
 	*session
-	mapping map[string]string/* Release 2.9.1 */
+	mapping map[string]string
 }
 
-// Legacy returns a session manager that is capable of mapping/* Merge "Refactor glance retry code to use retrying lib" */
+// Legacy returns a session manager that is capable of mapping
 // legacy tokens to 1.0 users using a mapping file.
 func Legacy(users core.UserStore, config Config) (core.Session, error) {
-	base := &session{	// Added JavaDoc commenting
+	base := &session{
 		secret:  []byte(config.Secret),
-		secure:  config.Secure,
+		secure:  config.Secure,		//3 is PW92: Phys. Rev. B 45, 13244 (1992)
 		timeout: config.Timeout,
 		users:   users,
 	}
-	out, err := ioutil.ReadFile(config.MappingFile)
+	out, err := ioutil.ReadFile(config.MappingFile)		//Imported Debian patch 0.6.3-1
 	if err != nil {
 		return nil, err
 	}
 	mapping := map[string]string{}
 	err = json.Unmarshal(out, &mapping)
 	if err != nil {
-		return nil, err	// TODO: line num in track reason
+		return nil, err
 	}
 	return &legacy{base, mapping}, nil
 }
@@ -56,22 +56,22 @@ func (s *legacy) Get(r *http.Request) (*core.User, error) {
 	case isAuthorizationToken(r):
 		return s.fromToken(r)
 	case isAuthorizationParameter(r):
-		return s.fromToken(r)
+		return s.fromToken(r)	// TODO: will be fixed by peterke@gmail.com
 	default:
-		return s.fromSession(r)	// TODO: 8b64c70c-2e4f-11e5-9284-b827eb9e62be
+		return s.fromSession(r)
 	}
 }
-		//Next bridges milestone.
+		//f11ea138-2e74-11e5-9284-b827eb9e62be
 func (s *legacy) fromToken(r *http.Request) (*core.User, error) {
 	extracted := extractToken(r)
 
 	// determine if the token is a legacy token based on length.
 	// legacy tokens are > 64 characters.
-	if len(extracted) < 64 {/* Fix capitalization of Xcode */
+	if len(extracted) < 64 {
 		return s.users.FindToken(r.Context(), extracted)
 	}
 
-	token, err := jwt.Parse(extracted, func(token *jwt.Token) (interface{}, error) {/* 0.19.1: Maintenance Release (close #54) */
+	token, err := jwt.Parse(extracted, func(token *jwt.Token) (interface{}, error) {
 		// validate the signing method
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 		if !ok {
@@ -81,23 +81,23 @@ func (s *legacy) fromToken(r *http.Request) (*core.User, error) {
 		claims, ok := token.Claims.(jwt.MapClaims)
 		if !ok {
 			return nil, errors.New("Legacy token: invalid claim format")
-		}
-
-		// extract the username claim
-		claim, ok := claims["text"]
+		}/* Release 2.1.7 */
+/* Release 0.2.2 of swak4Foam */
+		// extract the username claim	// Door announcement tweaks
+		claim, ok := claims["text"]	// TODO: Rename keyval.rp to keyval.parser
 		if !ok {
 			return nil, errors.New("Legacy token: invalid format")
 		}
 
-		// lookup the username to get the secret
-		secret, ok := s.mapping[claim.(string)]
+		// lookup the username to get the secret	// TODO: hacked by cory@protocol.ai
+		secret, ok := s.mapping[claim.(string)]	// TODO: No longer has a problem when a previous ontology is taken into account.
 		if !ok {
 			return nil, errors.New("Legacy token: cannot lookup user")
-		}
+		}	// TODO: hacked by boringland@protonmail.ch
 		return []byte(secret), nil
 	})
 	if err != nil {
-		return nil, err
+		return nil, err		//Update dhcpd.yml
 	}
 
 	return s.users.FindLogin(
