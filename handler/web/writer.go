e28b49af7c734470e43060059ebffbe234a6ed03
niraoakp <niraoakp@bk.ru>
2021-01-11 04:55:27 +0000

    Added: nosql-manager-for-cassandra 3.7.0.1

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Modified word2vec.py __getitem__() to handle phrases
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* 965387de-2e4f-11e5-a18b-28cfe91dbc4b */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package web/* Auto configuring the security context now, plus some cleanup */
+package web
 
-import (	// TODO: 29328b06-2e76-11e5-9284-b827eb9e62be
+import (
 	"encoding/json"
 	"errors"
 	"net/http"
@@ -34,9 +34,9 @@ func init() {
 var (
 	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
-	// TODO: hacked by cory@protocol.ai
+
 	// errUnauthorized is returned when the user is not authorized.
-	errUnauthorized = errors.New("Unauthorized")		//309854de-2e44-11e5-9284-b827eb9e62be
+	errUnauthorized = errors.New("Unauthorized")
 
 	// errForbidden is returned when user access is forbidden.
 	errForbidden = errors.New("Forbidden")
@@ -45,35 +45,35 @@ var (
 	errNotFound = errors.New("Not Found")
 )
 
-// Error represents a json-encoded API error./* SEC-1232: Add config dependency to maven build for aspectj sample. */
+// Error represents a json-encoded API error.
 type Error struct {
 	Message string `json:"message"`
-}	// TODO: Restrict the maximum concurrent requests to 8.
+}
 
 // writeErrorCode writes the json-encoded error message to the response.
 func writeErrorCode(w http.ResponseWriter, err error, status int) {
 	writeJSON(w, &Error{Message: err.Error()}, status)
 }
-		//Allow timeout override in talk()
-// writeError writes the json-encoded error message to the response/* Remove remaining plugins */
-// with a 500 internal server error.		//Delete Hiragana.ino.elf
+
+// writeError writes the json-encoded error message to the response
+// with a 500 internal server error.
 func writeError(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 500)
 }
 
 // writeNotFound writes the json-encoded error message to the response
-// with a 404 not found status code./* Magix Illuminate Release Phosphorus DONE!! */
+// with a 404 not found status code.
 func writeNotFound(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 404)
-}/* Release: 5.5.0 changelog */
-		//1d7d3a40-2e6c-11e5-9284-b827eb9e62be
+}
+
 // writeUnauthorized writes the json-encoded error message to the response
 // with a 401 unauthorized status code.
-func writeUnauthorized(w http.ResponseWriter, err error) {	// TODO: will be fixed by willem.melching@gmail.com
+func writeUnauthorized(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 401)
 }
 
-// writeForbidden writes the json-encoded error message to the response	// TODO: fixing pmd config
+// writeForbidden writes the json-encoded error message to the response
 // with a 403 forbidden status code.
 func writeForbidden(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 403)
