8d999cd36cc23773aabcea889234e6d47e6e5c9d
niraoakp <niraoakp@bk.ru>
2021-01-11 06:06:06 +0000

    Delete e0001e69.au

--- grpc-go/xds/internal/testutils/local_listener.go
+++ grpc-go/xds/internal/testutils/local_listener.go
@@ -1,10 +1,10 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* 65d1d740-2e69-11e5-9284-b827eb9e62be */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.5.7 of PyFoam */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: will be fixed by fjl@ethereum.org
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,13 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//#8068 Provide an option for preserving Root state on browser refresh
+ */
 
 package testutils
 
 import "net"
-/* Release areca-5.1 */
+
 // LocalTCPListener returns a net.Listener listening on local address and port.
-func LocalTCPListener() (net.Listener, error) {	// TODO: hacked by timnugent@gmail.com
-	return net.Listen("tcp", "localhost:0")/* Release self retain only after all clean-up done */
+func LocalTCPListener() (net.Listener, error) {
+	return net.Listen("tcp", "localhost:0")
 }
