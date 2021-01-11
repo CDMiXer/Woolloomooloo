e3b06b5ab41be61f7dcf4a16dcbb25260e88cf99
niraoakp <niraoakp@bk.ru>
2021-01-11 03:16:12 +0000

    Use old-style string formatter to ensure 2.4 compatibility

--- handler/api/system/none.go
+++ handler/api/system/none.go
@@ -8,38 +8,38 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: [maven-release-plugin] prepare release analysis-collector-1.4
-// See the License for the specific language governing permissions and	// c5a17820-2e5e-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package system	// TODO: Made lockpicking and door breaking more suspicious.
+package system
 
 import (
-	"net/http"	// TODO: Update branding information
+	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: Merge "Fix cinder quota-usage error"
+	"github.com/drone/drone/handler/api/render"
 )
-	// TODO: will be fixed by jon@atack.com
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// TODO: 8da75d12-2e73-11e5-9284-b827eb9e62be
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 // HandleLicense returns a no-op http.HandlerFunc.
-func HandleLicense(license core.License) http.HandlerFunc {/* Initially Add Xjail's Work */
-	return notImplemented	// TODO: will be fixed by steven@stebalien.com
+func HandleLicense(license core.License) http.HandlerFunc {
+	return notImplemented
 }
 
 // HandleStats returns a no-op http.HandlerFunc.
 func HandleStats(
 	core.BuildStore,
 	core.StageStore,
-	core.UserStore,		//updated dingtalk (1.9.0) (#20860)
-	core.RepositoryStore,	// Add wildcard query ability to lsservices, update changelog and readme.
+	core.UserStore,
+	core.RepositoryStore,
 	core.Pubsub,
 	core.LogStream,
 ) http.HandlerFunc {
 	return notImplemented
-}	// TODO: 8238b83a-2e74-11e5-9284-b827eb9e62be
+}
