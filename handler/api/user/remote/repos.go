ea388dfd64f4871e7a8eb520939175e97143701a
niraoakp <niraoakp@bk.ru>
2021-01-11 03:22:59 +0000

    README: add "lifecycle" so people can easily find this section.

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");		//worked on the partner-feature
+// you may not use this file except in compliance with the License./* Release 5.2.1 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Created PeakResultGridManager 
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package remote
+package remote		//xfail test for 2-d data in table
 
 import (
-	"net/http"
-
-	"github.com/drone/drone/core"
+	"net/http"/* Release 2.0.0.beta1 */
+/* Release 1.0.38 */
+	"github.com/drone/drone/core"/* Merge "Hide savanna-subprocess endpoint from end users" */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
-// HandleRepos returns an http.HandlerFunc that write a json-encoded
+// HandleRepos returns an http.HandlerFunc that write a json-encoded	// TODO: hacked by josharian@gmail.com
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -35,7 +35,7 @@ func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list remote repositories")
 		} else {
-			render.JSON(w, list, 200)
+			render.JSON(w, list, 200)	// TODO: Updated thesis.tex
 		}
 	}
 }
