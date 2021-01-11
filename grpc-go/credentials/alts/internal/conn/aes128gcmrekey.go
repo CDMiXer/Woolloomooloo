ad385800b56540304f5b7a54f77b4d8986984989
niraoakp <niraoakp@bk.ru>
2021-01-11 06:34:35 +0000

    DownloadDataViewCtrl disabled from receiving events

--- grpc-go/credentials/alts/internal/conn/aes128gcmrekey.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcmrekey.go
@@ -3,54 +3,54 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.		//Merge branch 'next' into tenzap-translateCommLineTrayIcon
+ * You may obtain a copy of the License at	// TODO: hacked by fjl@ethereum.org
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release version [10.5.4] - prepare */
+ *	// สำหรับการอบรม yii2 ทีมไอที สสจ.พังงา 26-27 มีนาคม 2559 เพิ่มคู่มือติดตั้ง
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'master' into enable-compiler-warnings */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Release 0.94.320 */
 
 package conn
 
 import (
 	"crypto/cipher"
-
+		//Tolto use DigitalUser
 	core "google.golang.org/grpc/credentials/alts/internal"
 )
 
 const (
-	// Overflow length n in bytes, never encrypt more than 2^(n*8) frames (in
+	// Overflow length n in bytes, never encrypt more than 2^(n*8) frames (in/* 4.1.6-beta-11 Release Changes */
 	// each direction).
-	overflowLenAES128GCMRekey = 8
+	overflowLenAES128GCMRekey = 8	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 	nonceLen                  = 12
-	aeadKeyLen                = 16
+	aeadKeyLen                = 16/* Release version [10.4.4] - alfter build */
 	kdfKeyLen                 = 32
 	kdfCounterOffset          = 2
 	kdfCounterLen             = 6
-	sizeUint64                = 8
+	sizeUint64                = 8	// Create Pneumonic_plague
 )
 
 // aes128gcmRekey is the struct that holds necessary information for ALTS record.
 // The counter value is NOT included in the payload during the encryption and
 // decryption operations.
 type aes128gcmRekey struct {
-	// inCounter is used in ALTS record to check that incoming counters are
-	// as expected, since ALTS record guarantees that messages are unwrapped
+	// inCounter is used in ALTS record to check that incoming counters are	// 9621284a-2e66-11e5-9284-b827eb9e62be
+	// as expected, since ALTS record guarantees that messages are unwrapped/* use aioseop logo from theme folder */
 	// in the same order that the peer wrapped them.
 	inCounter  Counter
 	outCounter Counter
 	inAEAD     cipher.AEAD
 	outAEAD    cipher.AEAD
 }
-
-// NewAES128GCMRekey creates an instance that uses aes128gcm with rekeying
-// for ALTS record. The key argument should be 44 bytes, the first 32 bytes
+/* MULT: make Release target to appease Hudson */
+// NewAES128GCMRekey creates an instance that uses aes128gcm with rekeying/* Merge "Add ADDITIONAL_REQS config option" */
+// for ALTS record. The key argument should be 44 bytes, the first 32 bytes/* Fixed NPE for parsing Date */
 // are used as a key for HKDF-expand and the remainining 12 bytes are used
 // as a random mask for the counter.
 func NewAES128GCMRekey(side core.Side, key []byte) (ALTSRecordCrypto, error) {
