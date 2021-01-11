de751541f044418df1d79e3eca149da18583d7d0
niraoakp <niraoakp@bk.ru>
2021-01-11 02:37:49 +0000

    feat(l10n): update Italian translation

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release for v41.0.0. */
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.	// Update measure_polybench.bash
+// You may obtain a copy of the License at/* Released DirectiveRecord v0.1.25 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// small bugfix for FHI-aims calculator window in ase.gui
 //
-// Unless required by applicable law or agreed to in writing, software/* Pom: Explicitly adding alchemy-annotations 1.5 */
+// Unless required by applicable law or agreed to in writing, software/* Update raster_plot */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Make vorbis comment functions const correct.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
 
-import (/* add white border around starting dialogs */
+import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* Release of eeacms/forests-frontend:1.5.7 */
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded
-// account information to the http response body.
+// HandleFind returns an http.HandlerFunc that writes json-encoded/* fixes keyboard agent docs. Release of proscene-2.0.0-beta.1 */
+// account information to the http response body.		//buildhelp is no longer a button, use help instead. Also, clean up nil asserts.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()	// TODO: Переход в сторону java.nio
-		viewer, _ := request.UserFrom(ctx)
+		ctx := r.Context()
+		viewer, _ := request.UserFrom(ctx)/* Updated DevOps: Scaling Build, Deploy, Test, Release */
 		render.JSON(w, viewer, 200)
 	}
 }
