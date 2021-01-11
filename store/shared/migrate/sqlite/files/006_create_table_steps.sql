444050dde774da1052c18e9978a22f844d6d06a2
niraoakp <niraoakp@bk.ru>
2021-01-11 16:05:40 +0000

    Added new files, mostly figures, and some saved states.

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
 -- name: create-table-steps
-		//trigger new build for ruby-head-clang (1587b32)
+
 CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTOINCREMENT	// ConfigManager: add getActionFactoryFactory to allow proxies.
-,step_stage_id    INTEGER
+ step_id          INTEGER PRIMARY KEY AUTOINCREMENT
+,step_stage_id    INTEGER		//Merge "[FIX] Demokit 2.0 TabHeader texts are no longer cut"
 ,step_number      INTEGER
 ,step_name        TEXT
 ,step_status      TEXT
-,step_error       TEXT
-,step_errignore   BOOLEAN
+,step_error       TEXT/* Release version: 1.0.3 */
+,step_errignore   BOOLEAN	// TODO: Fixed the Upgrade instructions
 ,step_exit_code   INTEGER
-,step_started     INTEGER
+,step_started     INTEGER/* Updates kickstart config to include additional required packages */
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE	// no more $apply user model change on vcard & roster list
-);
+,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
+);/* Delete 007.xml */
 
 -- name: create-index-steps-stage
 
