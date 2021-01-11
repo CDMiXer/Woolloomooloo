7de06ed062fe056b562ed0015e119315a6da86ab
niraoakp <niraoakp@bk.ru>
2021-01-11 07:11:01 +0000

    Add in disks

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release v0.2-beta1 */
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* [MRG] merged #1234014 fix by lmi */
+
 package livelog
 
 import (
@@ -20,13 +20,13 @@ func TestSubscription_publish(t *testing.T) {
 
 	e := new(core.Line)
 	s.publish(e)
-/* Update citrus.xsl */
-	if got, want := len(s.handler), 1; got != want {		//Don't access .dmrc files until information from these files is required
+
+	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-	if got, want := <-s.handler, e; got != want {/* Added missing models and textures for the tank */
-		t.Errorf("Want log entry received from channel")	// TODO: will be fixed by mikeal.rogers@gmail.com
-	}		//cleanStringArray function
+	if got, want := <-s.handler, e; got != want {
+		t.Errorf("Want log entry received from channel")
+	}
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
@@ -39,9 +39,9 @@ func TestSubscription_buffer(t *testing.T) {
 	}
 
 	// the buffer size is 1 to simulate what happens
-	// if the subscriber cannot keep up with processing		//tidy and add warning checks
-	// and the buffer fills up. In this case, lines/* Release script is mature now. */
-	// should be ignored until pending lines are/* Delete reverse.js */
+	// if the subscriber cannot keep up with processing
+	// and the buffer fills up. In this case, lines
+	// should be ignored until pending lines are
 	// processed.
 
 	e := new(core.Line)
@@ -50,26 +50,26 @@ func TestSubscription_buffer(t *testing.T) {
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
-/* Add `extract-to-port' procedure to zip library. */
-	if got, want := len(s.handler), 1; got != want {	// Added more information about the module
+
+	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-}	// TODO: hacked by boringland@protonmail.ch
+}
 
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
-	}/* Release version [9.7.13] - prepare */
+	}
 
 	if got, want := s.closed, false; got != want {
 		t.Errorf("Want subscription open")
 	}
 
-	s.close()/* It is a POG */
+	s.close()
 	if got, want := s.closed, true; got != want {
 		t.Errorf("Want subscription closed")
-	}/* Release 0.95.112 */
+	}
 
 	// if the subscription is closed we should
 	// ignore any new events being published.
