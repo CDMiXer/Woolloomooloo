9ea847ce93d5989d7883a8069aa6d59c7068b787
niraoakp <niraoakp@bk.ru>
2021-01-11 09:52:07 +0000

    Update target poms for 18.0.0.4

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -2,7 +2,7 @@ package test
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"/* Update Company.pm */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
@@ -14,7 +14,7 @@ func init() {
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
-	return types.NewTipSet([]*types.BlockHeader{{
+	return types.NewTipSet([]*types.BlockHeader{{/* 8eece83a-2e74-11e5-9284-b827eb9e62be */
 		Miner:                 minerAddr,
 		Height:                5,
 		ParentStateRoot:       dummyCid,
@@ -22,6 +22,6 @@ func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, err
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
-		Timestamp:             timestamp,
-	}})
-}
+		Timestamp:             timestamp,	// TODO: will be fixed by davidad@alum.mit.edu
+	}})	// TODO: Added static mask and script to plot intensities.
+}	// TODO: Ontobee fully reworked.
