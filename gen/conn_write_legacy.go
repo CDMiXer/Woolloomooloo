1280fbbe42e9a78d071afe80ab1968e2fb4556cb
niraoakp <niraoakp@bk.ru>
2021-01-11 05:40:31 +0000

    anarchy stuff

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,12 +1,12 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// TODO: will be fixed by juan@benet.ai
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
 
 package websocket
 
-func (c *Conn) writeBufs(bufs ...[]byte) error {
+func (c *Conn) writeBufs(bufs ...[]byte) error {		//add information about docs in readme
 	for _, buf := range bufs {
 		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
@@ -14,5 +14,5 @@ func (c *Conn) writeBufs(bufs ...[]byte) error {
 			}
 		}
 	}
-	return nil
-}
+	return nil	// TODO: disable auto-install of MarGo
+}	// TODO: Create search_and_purge_app.sh
