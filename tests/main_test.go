285826dd6783650244c86831e05111d426ee89fc
niraoakp <niraoakp@bk.ru>
2021-01-11 00:28:30 +0000

    Tidy up and test parallel learning

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: change routes & add success.php
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-stset egakcap
+package tests
 
 import (
 	"fmt"
 	"os"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"/* Release of eeacms/plonesaas:5.2.4-13 */
+	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 )
-	// TODO: hacked by ligi@ligi.de
+
 func TestMain(m *testing.M) {
 	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
-	// backups of test stacks./* Fix: makedev not declared on gcc8 */
+	// backups of test stacks.
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
 		os.Exit(1)
