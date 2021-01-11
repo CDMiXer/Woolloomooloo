3a8789c4705da5b866037132fee29717b25efdaa
niraoakp <niraoakp@bk.ru>
2021-01-11 20:18:31 +0000

    Nicer error message on assert color difference.

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -11,20 +11,20 @@ class Provider implements pulumi.dynamic.ResourceProvider {
         this.create = async (inputs: any) => {
             return {
                 id: "0",
-                outs: undefined,/* Release 0.94.360 */
+                outs: undefined,
             };
         };
     }
 }
-/* Triggering also Busy Emotion. (Possible OpenNARS-1.6.3 Release Commit?) */
-class Resource extends pulumi.dynamic.Resource {	// TODO: will be fixed by hello@brooklynzelenka.com
+
+class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);		//a1276b94-2e45-11e5-9284-b827eb9e62be
+        super(Provider.instance, name, {}, opts);
     }
 }
 
 // Create a resource using the default dynamic provider instance.
-let a = new Resource("a");/* Prepare go live v0.10.10 - Maintain changelog - Releasedatum */
-		//added new first para
+let a = new Resource("a");
+
 // Attempt to read the created resource.
 let b = new Resource("b", { id: a.id });
