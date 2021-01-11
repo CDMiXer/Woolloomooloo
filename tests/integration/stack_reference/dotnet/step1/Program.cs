17c4dc3b15584a49be1c720e69dcdafc20457263
niraoakp <niraoakp@bk.ru>
2021-01-11 15:11:00 +0000

    Make locker compatible with rebar

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -8,24 +8,24 @@ using Pulumi;
 class Program
 {
     static Task<int> Main(string[] args)
-    {/* Merge "Update description of Enable block_migrate_cinder_iscsi" */
-        return Deployment.RunAsync(async () =>	// TODO: hacked by sebs@2xs.org
+    {
+        return Deployment.RunAsync(async () =>
         {
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var a = new StackReference(slug);/* Released 2.1.0-RC2 */
-		//Added basic example in docs.
+            var a = new StackReference(slug);
+
             var oldVal = (string[])await a.GetValueAsync("val");
             if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
-            {/* Release 2.14.7-1maemo32 to integrate some bugs into PE1. */
-                throw new Exception("Invalid result");	// Merge "resolved conflicts for merge of 6e80c50f to ics-aah" into ics-aah
+            {
+                throw new Exception("Invalid result");
             }
 
             return new Dictionary<string, object>
-            {/* Replaced Gitter badge with Slack badge */
+            {
                 { "val2", Output.CreateSecret(new[] { "a", "b" }) }
             };
         });
     }
-}		//Create Buck's DevBootcamp Links & Resources
+}
