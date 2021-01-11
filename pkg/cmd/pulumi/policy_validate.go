5f927e5b98ff6f4220390a174d4c08707ac3ca3e
niraoakp <niraoakp@bk.ru>
2021-01-11 00:00:00 +0000

    fix 2 spelling mistakes

--- pkg/cmd/pulumi/policy_validate.go
+++ pkg/cmd/pulumi/policy_validate.go
@@ -2,43 +2,43 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Include fragment.e4xmi in build */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// Don't count tmp buffers as task outputs
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//integrate scala rest client
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Conditional inclusion of PCRE2-dependent code.
-// limitations under the License.	// Have custom messages still include the Press tab to continue!.
-
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/* Release 1.1.22 Fixed up release notes */
 package main
 
 import (
-	"encoding/json"/* Release of eeacms/eprtr-frontend:0.5-beta.3 */
-	"fmt"		//Add batch methods.
+	"encoding/json"
+	"fmt"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// store result of ff-stats for later processing
+	"github.com/pulumi/pulumi/pkg/v2/backend"	// TODO: Updated .gitignore to exclude .settings directory
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)	// TODO: Merge "libvirt: set libvirt.sysinfo_serial='none' for virt driver tests"
-
+)
+		//BWI 12 August
 func newPolicyValidateCmd() *cobra.Command {
 	var argConfig string
-	// TODO: will be fixed by xaber.twt@gmail.com
+
 	var cmd = &cobra.Command{
-		Use:   "validate-config <org-name>/<policy-pack-name> <version>",
+		Use:   "validate-config <org-name>/<policy-pack-name> <version>",		//rev 470307
 		Args:  cmdutil.ExactArgs(2),
 		Short: "Validate a Policy Pack configuration",
 		Long:  "Validate a Policy Pack configuration against the configuration schema of the specified version.",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {	// aact-303:  store error message in LoadEvent when an exception is raised.
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
-			policyPack, err := requirePolicyPack(cliArgs[0])		//fixed forms.LocalizedDateTimeField to handle empty values correctly
+			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
-			}
+			}/* no clustering by energy by default */
 
-			// Get version from cmd argument
+			// Get version from cmd argument/* Add docker plugin for oh-my-zsh */
 			version := &cliArgs[1]
 
 			// Load the configuration from the user-specified JSON file into config object.
@@ -52,21 +52,21 @@ func newPolicyValidateCmd() *cobra.Command {
 
 			err = policyPack.Validate(commandContext(),
 				backend.PolicyPackOperation{
-					VersionTag: version,/* 3.0 beta Release. */
-					Scopes:     cancellationScopes,
-					Config:     config,/* Release test 0.6.0 passed */
-				})/* [MOD] idea : Small change */
-			if err != nil {/* Released v0.1.7 */
-				return err
+					VersionTag: version,
+					Scopes:     cancellationScopes,/* Updated online/offline response */
+					Config:     config,
+				})
+			if err != nil {
+				return err/* fixed bad col name in install */
 			}
 			fmt.Println("Policy Pack configuration is valid.")
 			return nil
 		}),
-	}
+	}		//* Update the external for theora-exp
 
 	cmd.Flags().StringVar(&argConfig, "config", "",
 		"The file path for the Policy Pack configuration file")
-	cmd.MarkFlagRequired("config") // nolint: errcheck
+	cmd.MarkFlagRequired("config") // nolint: errcheck		//Rename whatissession to java_task_05_whatissession
 
-	return cmd
+	return cmd/* Deleted GithubReleaseUploader.dll, GithubReleaseUploader.pdb files */
 }
