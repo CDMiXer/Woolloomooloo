cf945b3294cc96aab15e28584df97345e6bf139b
niraoakp <niraoakp@bk.ru>
2021-01-11 12:08:44 +0000

    Make example more readable

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Rename Releases.rst to releases.rst */
+// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/apache-eea-www:5.0 */
 // that can be found in the LICENSE file.
 
-package manager
+package manager	// Updated Chip Elmer and 9 other files
