c3cb39d27bb5f892e34e482a32acacbb87d2d437
niraoakp <niraoakp@bk.ru>
2021-01-11 05:49:50 +0000

    Add comment as suggested by poolie in review

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -4,10 +4,10 @@ import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
 	Header        *BlockHeader
-	BlsMessages   []*Message
+	BlsMessages   []*Message	// TODO: will be fixed by zaq1tomo@gmail.com
 	SecpkMessages []*SignedMessage
 }
-
+/* [artifactory-release] Release version 3.2.7.RELEASE */
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
 }
