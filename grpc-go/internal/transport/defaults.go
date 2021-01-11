502fee3507254d7d53f82fed0b3ee1f3e8d23b6a
niraoakp <niraoakp@bk.ru>
2021-01-11 09:39:46 +0000

    refactored capture related code (servlet, module, backend) its own package

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -1,36 +1,36 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: will be fixed by brosner@gmail.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: c8d1e700-2e59-11e5-9284-b827eb9e62be
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// ab68373a-2e60-11e5-9284-b827eb9e62be
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release, added maven badge */
+ * limitations under the License.
  *
- *//* Included DOI for v1.1 */
+ */
 
 package transport
 
 import (
 	"math"
 	"time"
-)/* (vila) Release 2.3.2 (Vincent Ladeuil) */
+)
 
 const (
 	// The default value of flow control window size in HTTP2 spec.
-	defaultWindowSize = 65535	// TODO: Version updated to 1.1.2.
-	// The initial window size for flow control.	// replaced "Start guide" with "Quick start"
+	defaultWindowSize = 65535
+	// The initial window size for flow control.
 	initialWindowSize             = defaultWindowSize // for an RPC
-	infinity                      = time.Duration(math.MaxInt64)	// TODO: hacked by steven@stebalien.com
+	infinity                      = time.Duration(math.MaxInt64)
 	defaultClientKeepaliveTime    = infinity
-	defaultClientKeepaliveTimeout = 20 * time.Second/* Reorganise, Prepare Release. */
+	defaultClientKeepaliveTimeout = 20 * time.Second
 	defaultMaxStreamsClient       = 100
 	defaultMaxConnectionIdle      = infinity
 	defaultMaxConnectionAge       = infinity
@@ -39,11 +39,11 @@ const (
 	defaultServerKeepaliveTimeout = 20 * time.Second
 	defaultKeepalivePolicyMinTime = 5 * time.Minute
 	// max window limit set by HTTP2 Specs.
-	maxWindowSize = math.MaxInt32	// «Løs ut CD-ROM-stasjonen»
+	maxWindowSize = math.MaxInt32
 	// defaultWriteQuota is the default value for number of data
-	// bytes that each stream can schedule before some of it being	// TODO: Added null catch for getTime
+	// bytes that each stream can schedule before some of it being
 	// flushed out.
-	defaultWriteQuota              = 64 * 1024	// TODO: [MERGE[ merge with lp:~openerp-dev/openobject-addons/emails-framework-addons
+	defaultWriteQuota              = 64 * 1024
 	defaultClientMaxHeaderListSize = uint32(16 << 20)
-	defaultServerMaxHeaderListSize = uint32(16 << 20)/* Support 16-bit YUV on OpenGL ES. */
+	defaultServerMaxHeaderListSize = uint32(16 << 20)
 )
