0df03b67806724d77ea199ef145eac51d70ade09
niraoakp <niraoakp@bk.ru>
2021-01-11 03:35:02 +0000

    Bump kramdown :gem: to v1.10.0

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Merge "Fix a bug where window animation could be janky" */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Updated YouTube embed parameters.
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.0.1, update Readme, create changelog. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Ace: used `bound` instead of loose callback
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Settings Activity added Release 1.19 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package sink
 
-// Config configures a Datadog sink.
-type Config struct {
+// Config configures a Datadog sink.		//New category added
+type Config struct {	// 34f759d4-2f85-11e5-ba45-34363bc765d8
 	Endpoint string
-	Token    string
+	Token    string/* update less imports */
 
-	License          string		//instagram, twitter
+	License          string
 	Licensor         string
 	Subscription     string
-	EnableGithub     bool
+	EnableGithub     bool		//Update privacy page to correct incorrect info
 	EnableGithubEnt  bool
-	EnableGitlab     bool		//sprite jump .png updated
+	EnableGitlab     bool
 	EnableBitbucket  bool
 	EnableStash      bool
 	EnableGogs       bool
-	EnableGitea      bool/* Updated 001.md */
+	EnableGitea      bool
 	EnableAgents     bool
-	EnableNomad      bool		//Added links to homepage, forum, Google Plus page and Twitter page.
-	EnableKubernetes bool
-}
+	EnableNomad      bool		//Bugfix for checker in-any-order. Split checker tests into own file.
+	EnableKubernetes bool/* MEDIUM / Fixed pom.xml after merge */
+}/* Delete Makefile-Release-MacOSX.mk */
