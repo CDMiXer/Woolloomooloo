8cbd2ae0a695a793cc3f8648470e768dfd7d80fe
niraoakp <niraoakp@bk.ru>
2021-01-11 06:53:51 +0000

    Updating Oil to work with the new Package class.

--- metric/stages_test.go
+++ metric/stages_test.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 0.1.8. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* [skip ci] max */
-/* Add FFI_COMPILER preprocessor directive, was missing on Release mode */
+// +build !oss
+
 package metric
 
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* include ui-icons because it seems to be being fetched by jquery regardless */
+	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"		//Merge "Switch from Droid -> Noto for RS fonts."
+	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
-)		//Syntax hotfix
+)
 
 func TestStagePendingCount(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -26,19 +26,19 @@ func TestStagePendingCount(t *testing.T) {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
-/* Merge "Release notes for "evaluate_env"" */
+
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
-	// x5 stage count		//into control
+	// x5 stage count
 	data := []*core.Stage{{}, {}, {}, {}, {}}
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListState(gomock.Any(), core.StatusPending).Return(data, nil)
-	PendingJobCount(stages)	// TODO: will be fixed by boringland@protonmail.ch
+	PendingJobCount(stages)
 
-	metrics, err := registry.Gather()/* Proveedor personalizado, intentos persistir metadata */
+	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
 		return
@@ -49,7 +49,7 @@ func TestStagePendingCount(t *testing.T) {
 	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)	// TODO: Added link to the gatt project.
+		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(len(data)); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
@@ -63,14 +63,14 @@ func TestStageRunningCount(t *testing.T) {
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot/* какая семья живёт в доме с указанным номером. */
+		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
-/* Release 1.9.0 */
-	// creates a blank registry/* Small readme changes. */
-	registry := prometheus.NewRegistry()	// remove environment port check
+
+	// creates a blank registry
+	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
-/* Merge branch 'release/0.2.1-alpha' */
+
 	// x5 stage count
 	data := []*core.Stage{{}, {}, {}, {}, {}}
 
