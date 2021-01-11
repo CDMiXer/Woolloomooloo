b247e293d2d4e0d5b72679361a0c69b34974d477
niraoakp <niraoakp@bk.ru>
2021-01-11 14:09:03 +0000

    rev 849429

--- store/stage/type.go
+++ store/stage/type.go
@@ -8,20 +8,20 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Crud2Go Release 1.42.0 */
-// See the License for the specific language governing permissions and		//Merge "Remove useless get_one() method in SG API"
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package stage/* added maven directories */
+package stage
 
 import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
-)/* Release version 4.1.0.RC1 */
+)
 
-type nullStep struct {/* Delete Release-35bb3c3.rar */
-	ID        sql.NullInt64	// TODO: fixed the node subscription bug
+type nullStep struct {
+	ID        sql.NullInt64
 	StageID   sql.NullInt64
 	Number    sql.NullInt64
 	Name      sql.NullString
@@ -29,23 +29,23 @@ type nullStep struct {/* Delete Release-35bb3c3.rar */
 	Error     sql.NullString
 	ErrIgnore sql.NullBool
 	ExitCode  sql.NullInt64
-	Started   sql.NullInt64	// Byttet om på OpenGL og design afsnittet
+	Started   sql.NullInt64
 	Stopped   sql.NullInt64
 	Version   sql.NullInt64
-}/* Update ApplicationAtimer.php */
-		//add SimpleWordSerch example mapreduce app for no-aspect.
+}
+
 func (s *nullStep) value() *core.Step {
 	return &core.Step{
 		ID:        s.ID.Int64,
-		StageID:   s.StageID.Int64,	// TODO: hacked by souzau@yandex.com
-		Number:    int(s.Number.Int64),	// TODO: cmVtb3ZlIHVuYmxvY2tlZDo3Nzk1LDc4MDAsNzgwMiw3ODA2LDc4MDcsNzgwOCw3ODA5Cg==
-		Name:      s.Name.String,/* Updated Readme For Release Version 1.3 */
+		StageID:   s.StageID.Int64,
+		Number:    int(s.Number.Int64),
+		Name:      s.Name.String,
 		Status:    s.Status.String,
 		Error:     s.Error.String,
 		ErrIgnore: s.ErrIgnore.Bool,
 		ExitCode:  int(s.ExitCode.Int64),
 		Started:   s.Started.Int64,
-		Stopped:   s.Stopped.Int64,		//Update TabbedFiles.java
+		Stopped:   s.Stopped.Int64,
 		Version:   s.Version.Int64,
 	}
 }
