04202e269230926e5f4250fcab2679a1bc575994
niraoakp <niraoakp@bk.ru>
2021-01-11 16:44:26 +0000

    file save as crash fixed (patch by Sebastien Alaiw)

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// docs: copy edit to landing page
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Adding Github Actions as a replacement for Travis
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
