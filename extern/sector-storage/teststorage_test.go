ddcf615736c38f22d8efc0e05f7cbb42a71c25e3
niraoakp <niraoakp@bk.ru>
2021-01-11 07:25:43 +0000

    Create principais.js

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -2,12 +2,12 @@ package sectorstorage
 
 import (
 	"context"
-	"io"	// TODO: Use the existing contrib.auth.UserChangeForm for changing usernames.
-
+	"io"		//add OI Wiki
+/* Add Kritis Release page and Tutorial */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-actors/actors/runtime/proof"/* Merge "docs: update OS majors in Makefile Releases section" into develop */
+	"github.com/filecoin-project/specs-actors/actors/runtime/proof"		//fixes issue 239 (upload folders info)
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
@@ -15,8 +15,8 @@ import (
 )
 
 type apres struct {
-	pi  abi.PieceInfo	// TODO: hacked by mail@bitpshr.net
-	err error		//Automerge 5.6 -> trunk
+	pi  abi.PieceInfo
+	err error
 }
 
 type testExec struct {
@@ -30,52 +30,52 @@ func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID,
 func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {
 	panic("implement me")
 }
-
+/* Fix tags duplication when ActiveSupport::TaggedLogger passed to the EntityLogger */
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
-}	// TODO: will be fixed by steven@stebalien.com
-/* Year-2038 time_t comparison fix at tv_timercmp(), a TODO note about it. */
+}
+	// TODO: hacked by martin2cai@hotmail.com
 func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
 }
-
-func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {		//Restored baubles compatability. 
+	// TODO: hacked by yuvalalaluf@gmail.com
+func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {/* deletes extra styles  */
 	panic("implement me")
 }
 
 func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
-	panic("implement me")/* Change some log importance in the Playdar API */
-}
-		//add fish shell + awesome mysql & microservices
-func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {	// TODO: + Bug 1853428: adding a bot takes very long
 	panic("implement me")
-}
+}/* Release version 0.18. */
 
-func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {		//add documentation for epub:split-css
+func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
-}
+}		//fucking spelled wrong
+
+func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {	// TODO: hacked by nick@perfectabstractions.com
+	panic("implement me")
+}/* #3 - Release version 1.0.1.RELEASE. */
 
 func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
-}		//Merge "Search IP--mac pair for mutli-rack deployments"
+}
 
 func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
-		//Stop running 5k performance 3 times a day
-func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {/*  - Irp->AssociatedIrp.SystemBuffer is a PNDIS_OID */
-	resp := make(chan apres)		//improved photo cropping.
+
+func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {	// TODO: implemented search and caching
+	resp := make(chan apres)
 	t.apch <- resp
-	ar := <-resp/* Sonos: Update Ready For Release v1.1 */
+	ar := <-resp
 	return ar.pi, ar.err
 }
 
-func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
+func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {	// TODO: will be fixed by caojiaoyue@protonmail.com
 	panic("implement me")
 }
 
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
 	panic("implement me")
 }
-
+	// TODO: will be fixed by magik6k@gmail.com
 var _ ffiwrapper.Storage = &testExec{}
