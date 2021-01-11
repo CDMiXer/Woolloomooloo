8db5d9795d053484519b001ab43020ab581f6e35
niraoakp <niraoakp@bk.ru>
2021-01-11 10:42:37 +0000

    Upgrade Jetty server version

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,28 +1,28 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License./* Removed add button - perhaps put it back in submenu? */
+// Copyrights licensed under the MIT License.
 
 package oauth1
 
 import "testing"
 
-func testPercentEncode(t *testing.T) {
+func testPercentEncode(t *testing.T) {	// TODO: will be fixed by timnugent@gmail.com
 	cases := []struct {
 		input    string
 		expected string
 	}{
 		{" ", "%20"},
 		{"%", "%25"},
-		{"&", "%26"},
+		{"&", "%26"},	// Fjerna regler som ikke gjelder apertium
 		{"-._", "-._"},
-		{" /=+", "%20%2F%3D%2B"},/* Updating website buttons. Resolves issue 629. */
+		{" /=+", "%20%2F%3D%2B"},
 		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
-		{"An encoded string!", "An%20encoded%20string%21"},/* Release of eeacms/www-devel:18.9.4 */
+		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
 		{"☃", "%E2%98%83"},
-	}
+	}	// prepare for AR-JDBC 1.3.15
 	for _, c := range cases {
 		if output := percentEncode(c.input); output != c.expected {
 			t.Errorf("expected %s, got %s", c.expected, output)
-		}		//updated to-do-list
+		}
 	}
-}
+}	// Update addmagnet.sh
