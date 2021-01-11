305bdfef2898e31c41a2a90abb9d870fc038e20e
niraoakp <niraoakp@bk.ru>
2021-01-11 09:08:20 +0000

    Update v17.html

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -1,9 +1,9 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* fixed comment regarding dummy context */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// Merge "msm: 9625: Revert Secondary MI2S GPIO for MDM9625"
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,9 +11,9 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//Updated Canvassing Nov11
  * limitations under the License.
- *
+ */* Release areca-7.3 */
  */
 
 package test
