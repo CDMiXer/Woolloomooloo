81d335f3d6f9f41284233fed9766b0373a04c5fe
niraoakp <niraoakp@bk.ru>
2021-01-11 19:52:01 +0000

    Delete PhotoPicker/proguard-rules.pro

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Minor tweaks to linked parameteres. */
 
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
-let org = config.require("org");
+let org = config.require("org");/* Merge "ARM: dts: msm: Update TZ apps region for msm8952" */
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-
+/* engines restore */
 export const val = ["a", "b"];
