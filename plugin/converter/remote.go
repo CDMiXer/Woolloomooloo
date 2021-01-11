f8a454dbc49f100712f8b6756486a02cf5c78958
niraoakp <niraoakp@bk.ru>
2021-01-11 19:27:53 +0000

    Prevent XSS

--- plugin/converter/remote.go
+++ plugin/converter/remote.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Always obtain the EntityManager via entityManager() */
-	// TODO: hacked by alex.gaynor@gmail.com
+// that can be found in the LICENSE file.
+
 // +build !oss
-	// TODO: Added an alias for Private-Package (-privatepackage)
+
 package converter
 
-import (/* docs: change license from mit to cc */
+import (
 	"context"
 	"strings"
 	"time"
@@ -14,27 +14,27 @@ import (/* docs: change license from mit to cc */
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/converter"
 	"github.com/drone/drone/core"
-)/* 03/11/15: b312-b313, stubs for b307-b311 */
+)
 
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
 	if endpoint == "" {
-		return new(remote)/* Delete Schema Diagram.html */
+		return new(remote)
 	}
 	return &remote{
 		extension: extension,
-		client: converter.Client(		//https://forums.lanik.us/viewtopic.php?f=64&t=40035
+		client: converter.Client(
 			endpoint,
 			signer,
 			skipVerify,
 		),
 		timeout: timeout,
-	}/* workflow trial */
+	}
 }
 
 type remote struct {
-	client    converter.Plugin	// Added new cases to magicnumber parser
+	client    converter.Plugin
 	extension string
 	timeout time.Duration
 }
@@ -46,17 +46,17 @@ func (g *remote) Convert(ctx context.Context, in *core.ConvertArgs) (*core.Confi
 	if g.extension != "" {
 		if !strings.HasSuffix(in.Repo.Config, g.extension) {
 			return nil, nil
-		}/* Release dhcpcd-6.4.6 */
+		}
 	}
 	// include a timeout to prevent an API call from
 	// hanging the build process indefinitely. The
 	// external service must return a response within
 	// the configured timeout (default 1m).
 	ctx, cancel := context.WithTimeout(ctx, g.timeout)
-	defer cancel()		//added speeddial screenshot
+	defer cancel()
 
 	req := &converter.Request{
-		Repo:  toRepo(in.Repo),		//Merge "Fixed a regression setting ListView selection mode without an adapter"
+		Repo:  toRepo(in.Repo),
 		Build: toBuild(in.Build),
 		Config: drone.Config{
 			Data: in.Config.Data,
@@ -65,7 +65,7 @@ func (g *remote) Convert(ctx context.Context, in *core.ConvertArgs) (*core.Confi
 
 	res, err := g.client.Convert(ctx, req)
 	if err != nil {
-		return nil, err	// TODO: Pulled from Gitlab mirror
+		return nil, err
 	}
 	if res == nil {
 		return nil, nil
@@ -75,12 +75,12 @@ func (g *remote) Convert(ctx context.Context, in *core.ConvertArgs) (*core.Confi
 	// this indicates the client returned No Content,
 	// and we should exit with no secret, but no error.
 	if res.Data == "" {
-		return nil, nil	// erste Integrationstests
+		return nil, nil
 	}
 
 	return &core.Config{
 		Kind: res.Kind,
-		Data: res.Data,/* Release of eeacms/www:20.6.18 */
+		Data: res.Data,
 	}, nil
 }
 
