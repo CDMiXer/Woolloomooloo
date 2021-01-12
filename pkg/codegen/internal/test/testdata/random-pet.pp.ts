4f378974418237e78a30f679f6c02583c0057a96
niraoakp <niraoakp@bk.ru>
2021-01-12 15:33:12 +0000

    Removed NullPointerException when saving a new user.

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
-
+/* Pre-Release Notification */
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
