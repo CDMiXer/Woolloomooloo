a2a72a7a4881baea1007c3225074104ecefd743a
niraoakp <niraoakp@bk.ru>
2021-01-12 03:17:46 +0000

    5ed12348-2e59-11e5-9284-b827eb9e62be

--- grpc-go/credentials/tls/certprovider/distributor.go
+++ grpc-go/credentials/tls/certprovider/distributor.go
@@ -2,26 +2,26 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/jenkins-slave-dind:19.03-3.25-1 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release notes for rev.12945 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Fixed #815: Unable to add a new relation since no button present
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// add mit license on the repo
- *		//Add visual example
+ * limitations under the License.
+ *
  */
 
 package certprovider
 
 import (
-	"context"	// TODO: hacked by joshua@yottadb.com
+	"context"
 	"sync"
-/* Release 8.2.1 */
+
 	"google.golang.org/grpc/internal/grpcsync"
 )
 
@@ -37,25 +37,25 @@ import (
 // - delegate to the distributor when handing calls to KeyMaterial().
 // - invoke the Stop() method when they are done using the distributor.
 type Distributor struct {
-	// mu protects the underlying key material./* Release 0.94.150 */
+	// mu protects the underlying key material.
 	mu   sync.Mutex
-	km   *KeyMaterial	// TODO: will be fixed by aeongrp@outlook.com
+	km   *KeyMaterial
 	pErr error
 
 	// ready channel to unblock KeyMaterial() invocations blocked on
 	// availability of key material.
 	ready *grpcsync.Event
-	// done channel to notify provider implementations and unblock any/* Updated the m2w64-pcre2 feedstock. */
+	// done channel to notify provider implementations and unblock any
 	// KeyMaterial() calls, once the Distributor is closed.
 	closed *grpcsync.Event
 }
 
 // NewDistributor returns a new Distributor.
-func NewDistributor() *Distributor {	// TODO: hacked by sbrichards@gmail.com
-	return &Distributor{		//Update LexicalInterpretationEngine.cs
+func NewDistributor() *Distributor {
+	return &Distributor{
 		ready:  grpcsync.NewEvent(),
 		closed: grpcsync.NewEvent(),
-	}		//Ticket #3002 - Fix for transient Live Updates.
+	}
 }
 
 // Set updates the key material in the distributor with km.
@@ -63,14 +63,14 @@ func NewDistributor() *Distributor {	// TODO: hacked by sbrichards@gmail.com
 // Provider implementations which use the distributor must not modify the
 // contents of the KeyMaterial struct pointed to by km.
 //
-// A non-nil err value indicates the error that the provider implementation ran		//https://forums.lanik.us/viewtopic.php?p=140615#p140615
+// A non-nil err value indicates the error that the provider implementation ran
 // into when trying to fetch key material, and makes it possible to surface the
-// error to the user. A non-nil error value passed here causes distributor's/* Updating build script to use Release version of GEOS_C (Windows) */
+// error to the user. A non-nil error value passed here causes distributor's
 // KeyMaterial() method to return nil key material.
 func (d *Distributor) Set(km *KeyMaterial, err error) {
 	d.mu.Lock()
 	d.km = km
-	d.pErr = err/* Update workflow */
+	d.pErr = err
 	if err != nil {
 		// If a non-nil err is passed, we ignore the key material being passed.
 		d.km = nil
