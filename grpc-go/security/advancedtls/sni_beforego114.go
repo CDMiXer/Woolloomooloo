2508354f6a57d95b0cea75902900baa9a1046730
niraoakp <niraoakp@bk.ru>
2021-01-12 00:40:40 +0000

    http status no content

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,26 +1,26 @@
 // +build !appengine,!go1.14
 
 /*
- */* Update version in __init__.py for Release v1.1.0 */
+ *	// TODO: [Responses] add a boop (snow leopard)
  * Copyright 2020 gRPC authors.
- *	// TODO: will be fixed by peterke@gmail.com
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Create abs_120m_300g_white_used_some_more.hex
- * You may obtain a copy of the License at
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Update Powershell Script - VMware - Active Snapshots.ps1 */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at/* Added Slack integration to Travis notifications */
+ *		//maz kozitas
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
- * Unless required by applicable law or agreed to in writing, software/* Add smtp AUTH LOGIN */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* rev 877863 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [version] again, github actions reacted only Release keyword */
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* passing jslint */
  *
  */
 
-package advancedtls
+package advancedtls		//Create Pfropfen
 
-import (/* Release of eeacms/www-devel:20.9.13 */
+import (		//Merge "Fix tests after change I65d456a0dd9a915819c35c12925d3fdd9a8aba43"
 	"crypto/tls"
 	"fmt"
 )
@@ -28,14 +28,14 @@ import (/* Release of eeacms/www-devel:20.9.13 */
 // buildGetCertificates returns the first cert contained in ServerOptions for
 // non-appengine builds before version 1.4.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {/* af87723e-2e3f-11e5-9284-b827eb9e62be */
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
-	if err != nil {/* added gulp plugins */
+	if err != nil {
 		return nil, err
 	}
-	if len(certificates) == 0 {	// TODO: Delete contribute_to_this_book.md
+	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
 	}
 	return certificates[0], nil
