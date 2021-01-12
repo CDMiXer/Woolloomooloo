5fe4526184d10b4dac691ab6901398db93e106e7
niraoakp <niraoakp@bk.ru>
2021-01-12 01:48:13 +0000

    Update kontak-kami.md

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -2,56 +2,56 @@
 
 package build
 
-import (/* Gradle Release Plugin - new version commit:  "2.7-SNAPSHOT". */
-	"github.com/filecoin-project/go-address"
+import (
+	"github.com/filecoin-project/go-address"	// Begin cleaning up movment into new scr folder
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: update package information.
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}/* Merge "[INTERNAL][FIX] unified.FileUploader height in toolbar fixed" */
+}
 
 const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
-
+	// TODO: will be fixed by juan@benet.ai
 const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 120		//Build 45a: Add Junit Tests, Removed Client/Server code.
+const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
-const UpgradeRefuelHeight = -4/* GROOVY-2445: Provide an enhanced each() method for subclasses of java.lang.Enum */
-		//Delete Point.h.gch
-var UpgradeActorsV2Height = abi.ChainEpoch(30)
+const UpgradeRefuelHeight = -4
 
-const UpgradeTapeHeight = 60		//pt-osc: Change --quiet back and remove the quietness checks
+var UpgradeActorsV2Height = abi.ChainEpoch(30)	// Added GPL2 license
+
+const UpgradeTapeHeight = 60
 const UpgradeLiftoffHeight = -5
-const UpgradeKumquatHeight = 90
-const UpgradeCalicoHeight = 120/* removing unused indexes */
-const UpgradePersianHeight = 150
+const UpgradeKumquatHeight = 90/* - fixing default for exact numerics forgotten in last committ. */
+const UpgradeCalicoHeight = 120/* Merge "Resign all Release files if necesary" */
+const UpgradePersianHeight = 150/* Add updated version for repoze. Release 0.10.6. */
 const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
-const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)	// Fix NOTICEs files wrt jline
-const UpgradeActorsV4Height = 8922
+const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)	// TODO: hacked by peterke@gmail.com
+const UpgradeActorsV4Height = 8922	// updating ignore with bin and gen.
 
-func init() {		//wall collision 
+func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
-	policy.SetSupportedProofTypes(/* Delete Acuaticas.java */
+	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
 
-	SetAddressNetwork(address.Testnet)
+	SetAddressNetwork(address.Testnet)		//Implemented version check
 
 	Devnet = true
-}	// Merge "[INTERNAL][FIX] uxap.ObjectPage AnchorBar HCW, HCB styling fixed"
+}
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-/* Merge "Release 3.2.3.414 Prima WLAN Driver" */
-const PropagationDelaySecs = uint64(6)
 
-// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start		//Atualiza dados da coleção West Indies
-const BootstrapPeerThreshold = 2/* 686405a0-2e5a-11e5-9284-b827eb9e62be */
+const PropagationDelaySecs = uint64(6)/* Copied latest tokenizer enhancements to main branch */
+
+// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
+const BootstrapPeerThreshold = 2
 
 var WhitelistedBlock = cid.Undef
