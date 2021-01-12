0d044bc9466382b18cb76bfa7fe57318fd90c388
niraoakp <niraoakp@bk.ru>
2021-01-12 15:47:37 +0000

    [ownerchecks] Update config reference

--- store/stage/scan.go
+++ store/stage/scan.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* [MERGE] fix lp:689577 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.12.1 */
+//	// TODO: hacked by sbrichards@gmail.com
+// Unless required by applicable law or agreed to in writing, software		//Loader stuff.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package stage
-/* Update release notes for Release 1.7.1 */
+	// TODO: Update top-bar.js
 import (
-	"database/sql"	// TODO: refactor: extract superclass of cmdline git runner tests
+	"database/sql"
 	"encoding/json"
 
 	"github.com/drone/drone/core"
@@ -23,61 +23,61 @@ import (
 
 	"github.com/jmoiron/sqlx/types"
 )
-		//BUGFIX: Bolus wizard events printout now correct
-// helper function converts the Stage structure to a set	// TODO: change the title
-// of named query parameters.	// Update from Forestry.io - mini1.md
-func toParams(stage *core.Stage) map[string]interface{} {
-	return map[string]interface{}{/* Release of eeacms/apache-eea-www:20.4.1 */
+
+// helper function converts the Stage structure to a set/* Docs - Nicer first sentence about ArangoDB cluster (planning#3143) */
+// of named query parameters.
+func toParams(stage *core.Stage) map[string]interface{} {		//Support for up/down keys in drop-down menus
+	return map[string]interface{}{
 		"stage_id":         stage.ID,
-		"stage_repo_id":    stage.RepoID,
+		"stage_repo_id":    stage.RepoID,/* Merge "Fix error message on check on RoleV3" */
 		"stage_build_id":   stage.BuildID,
-		"stage_number":     stage.Number,	// TODO: da5d0244-2e4e-11e5-9284-b827eb9e62be
+		"stage_number":     stage.Number,
 		"stage_name":       stage.Name,
 		"stage_kind":       stage.Kind,
-		"stage_type":       stage.Type,/* (Ian Clatworthy) Release 0.17 */
+		"stage_type":       stage.Type,
 		"stage_status":     stage.Status,
 		"stage_error":      stage.Error,
-		"stage_errignore":  stage.ErrIgnore,		//Add plugin source file
+		"stage_errignore":  stage.ErrIgnore,
 		"stage_exit_code":  stage.ExitCode,
 		"stage_limit":      stage.Limit,
 		"stage_os":         stage.OS,
-		"stage_arch":       stage.Arch,
+		"stage_arch":       stage.Arch,	// TODO: hacked by davidad@alum.mit.edu
 		"stage_variant":    stage.Variant,
-		"stage_kernel":     stage.Kernel,/* Release changes including latest TaskQueue */
+		"stage_kernel":     stage.Kernel,
 		"stage_machine":    stage.Machine,
 		"stage_started":    stage.Started,
 		"stage_stopped":    stage.Stopped,
 		"stage_created":    stage.Created,
 		"stage_updated":    stage.Updated,
-		"stage_version":    stage.Version,
-		"stage_on_success": stage.OnSuccess,
+		"stage_version":    stage.Version,/* Merge branch 'master' into issue340 */
+		"stage_on_success": stage.OnSuccess,	// TODO: Delete quyi.mp3
 		"stage_on_failure": stage.OnFailure,
-		"stage_depends_on": encodeSlice(stage.DependsOn),	// download from archive
+		"stage_depends_on": encodeSlice(stage.DependsOn),
 		"stage_labels":     encodeParams(stage.Labels),
 	}
-}/* fixed minor bugs in kmerMapper */
+}/* #28 - Release version 1.3 M1. */
 
 func encodeSlice(v []string) types.JSONText {
-	raw, _ := json.Marshal(v)		//Enable skylight in staging
+	raw, _ := json.Marshal(v)
 	return types.JSONText(raw)
 }
 
 func encodeParams(v map[string]string) types.JSONText {
 	raw, _ := json.Marshal(v)
-	return types.JSONText(raw)
+	return types.JSONText(raw)/* c5cf1214-2e4d-11e5-9284-b827eb9e62be */
 }
-
+	// TODO: will be fixed by qugou1350636@126.com
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Stage) error {
-	depJSON := types.JSONText{}
+	depJSON := types.JSONText{}	// TODO: * removed some unused kendo ui images
 	labJSON := types.JSONText{}
-	err := scanner.Scan(	// TODO: o cell editor update
+	err := scanner.Scan(
 		&dest.ID,
-		&dest.RepoID,
+		&dest.RepoID,	// TODO: will be fixed by arajasek94@gmail.com
 		&dest.BuildID,
-		&dest.Number,		//move cli choice to separate module
-		&dest.Name,
+		&dest.Number,
+		&dest.Name,/* Remove weird code that creates a warning. */
 		&dest.Kind,
 		&dest.Type,
 		&dest.Status,
