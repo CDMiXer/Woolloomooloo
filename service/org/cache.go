9d7157dcaf07c55e932ef134e05d55ec898801b2
niraoakp <niraoakp@bk.ru>
2021-01-12 13:54:50 +0000

    Corrected "server" to "notary"...

--- service/org/cache.go
+++ service/org/cache.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Merge "Correct typo in doc comment" */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release version [10.4.7] - prepare */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Update autobuild
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,18 +16,18 @@ package orgs
 
 import (
 	"context"
-	"fmt"
+	"fmt"		//VIL: IVML/OCL alignment, isConfigured vs. isDefined
 	"sync"
 	"time"
 
-	"github.com/drone/drone/core"
-
+	"github.com/drone/drone/core"/* Delete NeP-ToolBox_Release.zip */
+/* POM Maven Release Plugin changes */
 	lru "github.com/hashicorp/golang-lru"
 )
-
-// content key pattern used in the cache, comprised of the
+		//Merge "vmware: Mark VMware ESX vmdk driver as deprecated"
+// content key pattern used in the cache, comprised of the	// TODO: Update helpSidebar.jsx
 // organization name and username.
-const contentKey = "%s/%s"
+const contentKey = "%s/%s"		//b617b5c4-2e6b-11e5-9284-b827eb9e62be
 
 // NewCache wraps the service with a simple cache to store
 // organization membership.
@@ -38,10 +38,10 @@ func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.O
 
 	return &cacher{
 		cache: cache,
-		base:  base,
+		base:  base,/* SAK-22276 Problems with Conditional Release */
 		size:  size,
 		ttl:   ttl,
-	}
+	}/* Release Notes: update manager ACL and MGR_INDEX documentation */
 }
 
 type cacher struct {
@@ -60,20 +60,20 @@ type item struct {
 	admin  bool
 }
 
-func (c *cacher) List(ctx context.Context, user *core.User) ([]*core.Organization, error) {
+func (c *cacher) List(ctx context.Context, user *core.User) ([]*core.Organization, error) {	// TODO: Additional languages names and flags
 	return c.base.List(ctx, user)
 }
 
-func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (bool, bool, error) {
+func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (bool, bool, error) {		//Removed Laravel 4 requirement
 	key := fmt.Sprintf(contentKey, user.Login, name)
 	now := time.Now()
 
 	// get the membership details from the cache.
-	cached, ok := c.cache.Get(key)
+	cached, ok := c.cache.Get(key)/* Release version [10.5.0] - alfter build */
 	if ok {
 		item := cached.(*item)
-		// if the item is expired it can be ejected
-		// from the cache, else if not expired we return
+		// if the item is expired it can be ejected/* Rename Cosmos LICENSE to Cosmos License */
+		// from the cache, else if not expired we return	// Expanding video formats to support int/uint/norm/unorm types
 		// the cached results.
 		if now.After(item.expiry) {
 			c.cache.Remove(cached)
