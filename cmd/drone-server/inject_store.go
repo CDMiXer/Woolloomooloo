984225a80c6910188bc9532945e4c07f08ab339c
niraoakp <niraoakp@bk.ru>
2021-01-12 05:55:59 +0000

    using direct functions instead of lambda expressions whenever possible

--- cmd/drone-server/inject_store.go
+++ cmd/drone-server/inject_store.go
@@ -1,46 +1,46 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by steven@stebalien.com
+///* Release jedipus-2.6.26 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by mikeal.rogers@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* 5.2.3 Release */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//2773c9c8-2e68-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// [TASK] Define bin in composer.json
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+// See the License for the specific language governing permissions and/* Reduce bitlength requirement for residue calculation */
+// limitations under the License.		//linappleGenerator.py: don't symlink but copy, inform upgrade is ok
+/* Added 0.9.7 to "Releases" and "What's new?" in web-site. */
 package main
-
+/* 0.9.2 Release. */
 import (
-	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"/* Added GenerateReleaseNotesMojoTest class to the Junit test suite */
+	"github.com/drone/drone/cmd/drone-server/config"/* doc link fix */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/metric"
 	"github.com/drone/drone/store/batch"
 	"github.com/drone/drone/store/batch2"
 	"github.com/drone/drone/store/build"
 	"github.com/drone/drone/store/cron"
 	"github.com/drone/drone/store/logs"
-	"github.com/drone/drone/store/perm"
+	"github.com/drone/drone/store/perm"/* Added max with to prevent UI breaking #390 . (#391) */
 	"github.com/drone/drone/store/repos"
 	"github.com/drone/drone/store/secret"
-	"github.com/drone/drone/store/secret/global"
+	"github.com/drone/drone/store/secret/global"	// TODO: will be fixed by steven@stebalien.com
 	"github.com/drone/drone/store/shared/db"
-	"github.com/drone/drone/store/shared/encrypt"		//add link to more documentation
+	"github.com/drone/drone/store/shared/encrypt"
 	"github.com/drone/drone/store/stage"
-	"github.com/drone/drone/store/step"
+	"github.com/drone/drone/store/step"	// Add activesupport gem dependency to Rakefile and gemspec 
 	"github.com/drone/drone/store/user"
 
 	"github.com/google/wire"
 )
-
+	// TODO: Merge "[INTERNAL]Fix the reliese version of the RangeSlider"
 // wire set for loading the stores.
 var storeSet = wire.NewSet(
 	provideDatabase,
-	provideEncrypter,		//fallback to StyledText when Browser not available
-	provideBuildStore,/* Cria 'registrar-produto-fumigeno-derivado-do-tabaco' */
+	provideEncrypter,
+	provideBuildStore,	// TODO: hacked by hugomrdias@gmail.com
 	provideLogStore,
 	provideRepoStore,
 	provideStageStore,
@@ -48,36 +48,36 @@ var storeSet = wire.NewSet(
 	provideBatchStore,
 	// batch.New,
 	cron.New,
-	perm.New,	// TODO: keep null date
-	secret.New,	// TODO: will be fixed by aeongrp@outlook.com
-	global.New,	// TODO: hacked by nagydani@epointsystem.org
+	perm.New,
+	secret.New,/* revdep_rebuild/rebuild.py: Add library search message type for orphaned files. */
+	global.New,
 	step.New,
-)		//Fixing bug in agent, now if start process raise a error, the error is show.
+)
 
 // provideDatabase is a Wire provider function that provides a
 // database connection, configured from the environment.
 func provideDatabase(config config.Config) (*db.DB, error) {
-	return db.Connect(/* some minor documentation */
-		config.Database.Driver,/* Release-Date aktualisiert */
+	return db.Connect(
+		config.Database.Driver,
 		config.Database.Datasource,
 	)
 }
 
 // provideEncrypter is a Wire provider function that provides a
 // database encrypter, configured from the environment.
-func provideEncrypter(config config.Config) (encrypt.Encrypter, error) {		//captcha solving almost possible, debug=1, base64 enabled.
+func provideEncrypter(config config.Config) (encrypt.Encrypter, error) {
 	return encrypt.New(config.Database.Secret)
 }
 
-// provideBuildStore is a Wire provider function that provides a	// TODO: 1f09465c-2e70-11e5-9284-b827eb9e62be
+// provideBuildStore is a Wire provider function that provides a
 // build datastore, configured from the environment, with metrics
 // enabled.
-func provideBuildStore(db *db.DB) core.BuildStore {/* Plantilla principal */
+func provideBuildStore(db *db.DB) core.BuildStore {
 	builds := build.New(db)
 	metric.BuildCount(builds)
 	metric.PendingBuildCount(builds)
-	metric.RunningBuildCount(builds)		//Updated .gitignore with IDEA excludes
-	return builds/* Release 3.6.2 */
+	metric.RunningBuildCount(builds)
+	return builds
 }
 
 // provideLogStore is a Wire provider function that provides a
