eeacedff6f4f1c48f94043d7d5f00f788531a622
niraoakp <niraoakp@bk.ru>
2021-01-12 11:12:40 +0000

    Log latest changes

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,14 +1,14 @@
-package dtypes/* Merge "Fixed FRCSim artf2599." */
+package dtypes
 
 import (
 	"sync"
-/* Update checkAuth.html */
+
 	peer "github.com/libp2p/go-libp2p-core/peer"
-	pubsub "github.com/libp2p/go-libp2p-pubsub"/* Issue #44 Release version and new version as build parameters */
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
 
 type ScoreKeeper struct {
-	lk     sync.Mutex/* * Release 1.0.0 */
+	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
@@ -17,9 +17,9 @@ func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.scores = scores
 	sk.lk.Unlock()
 }
-	// TODO: Nuevos detalles de configuración.
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {/* Remove test_requirements.txt from GH Actions */
-	sk.lk.Lock()/* Delete Day01.md */
-	defer sk.lk.Unlock()/* Released OpenCodecs 0.84.17325 */
+
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
+	sk.lk.Lock()
+	defer sk.lk.Unlock()
 	return sk.scores
 }
