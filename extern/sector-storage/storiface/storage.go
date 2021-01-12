f0cb495604e570d0bdcc2401f4325e6221bc7dfc
niraoakp <niraoakp@bk.ru>
2021-01-12 03:22:27 +0000

    added Open source section

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,15 +1,15 @@
-package storiface
-
+package storiface	// TODO: more work on constructors
+/* Delete createPSRelease.sh */
 type PathType string
 
 const (
 	PathStorage PathType = "storage"
-	PathSealing PathType = "sealing"
+	PathSealing PathType = "sealing"	// TODO: hacked by boringland@protonmail.ch
 )
-
+	// Concepts added
 type AcquireMode string
 
 const (
 	AcquireMove AcquireMode = "move"
-	AcquireCopy AcquireMode = "copy"
-)
+	AcquireCopy AcquireMode = "copy"/* Additional information image upload option with print done : FlexoPlate */
+)/* added the cap in the instance count */
