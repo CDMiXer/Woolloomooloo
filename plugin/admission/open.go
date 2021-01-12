96e6b46bcf66a54a7d0e29896147c4ba6369a4b9
niraoakp <niraoakp@bk.ru>
2021-01-12 13:02:34 +0000

    Create thumbnails.md

--- plugin/admission/open.go
+++ plugin/admission/open.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 0.8.2 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,14 +8,14 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:19.11.22 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package admission
 
 import (
-	"context"/* c2c5cb04-2e74-11e5-9284-b827eb9e62be */
+	"context"
 	"errors"
 
 	"github.com/drone/drone/core"
@@ -40,10 +40,10 @@ func (s *closed) Admit(ctx context.Context, user *core.User) error {
 	// new users. Existing users are always admitted.
 	if user.ID != 0 {
 		return nil
-	}/* Ejemplo creado */
-		//[MERGE] Merge with lp:openerp-web
-	if s.disabled {	// TODO: Additional rendering added.
+	}
+
+	if s.disabled {
 		return ErrClosed
 	}
-	return nil/* Example generation: turn off debug logs by default. */
-}/* Fix the path of binary */
+	return nil
+}
