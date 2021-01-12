be412c8890f7f7a39878fdaeb938fff9dbd4fc31
niraoakp <niraoakp@bk.ru>
2021-01-12 09:06:57 +0000

    add PR 850 / guava relocation to changelog

--- pkg/cmd/pulumi/main.go
+++ pkg/cmd/pulumi/main.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// Moodle block initial version
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Rename test_scripts/simplest_loop to test/simplest_loop */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
-package main
+// limitations under the License.	// TODO: hacked by lexy8russo@outlook.com
+/* support '<chosen> discards <amount> cards' pattern */
+package main	// TODO: Remove obsolete database sessions
 
 import (
 	"fmt"
@@ -23,27 +23,27 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-
+		//disable creategroup button when layer is selected in tree
 func panicHandler() {
 	if panicPayload := recover(); panicPayload != nil {
-		stack := string(debug.Stack())
-		fmt.Fprintln(os.Stderr, "================================================================================")
+		stack := string(debug.Stack())	// Delete Dump.pdf
+		fmt.Fprintln(os.Stderr, "================================================================================")/* fixed project name and slug */
 		fmt.Fprintln(os.Stderr, "The Pulumi CLI encountered a fatal error. This is a bug!")
 		fmt.Fprintln(os.Stderr, "We would appreciate a report: https://github.com/pulumi/pulumi/issues/")
 		fmt.Fprintln(os.Stderr, "Please provide all of the below text in your report.")
 		fmt.Fprintln(os.Stderr, "================================================================================")
 		fmt.Fprintf(os.Stderr, "Pulumi Version:   %s\n", version.Version)
-		fmt.Fprintf(os.Stderr, "Go Version:       %s\n", runtime.Version())
-		fmt.Fprintf(os.Stderr, "Go Compiler:      %s\n", runtime.Compiler)
+		fmt.Fprintf(os.Stderr, "Go Version:       %s\n", runtime.Version())	// TODO: lang file updates - almost done
+		fmt.Fprintf(os.Stderr, "Go Compiler:      %s\n", runtime.Compiler)/* Add tesseract images to README */
 		fmt.Fprintf(os.Stderr, "Architecture:     %s\n", runtime.GOARCH)
 		fmt.Fprintf(os.Stderr, "Operating System: %s\n", runtime.GOOS)
 		fmt.Fprintf(os.Stderr, "Panic:            %s\n\n", panicPayload)
 		fmt.Fprintln(os.Stderr, stack)
 		os.Exit(1)
 	}
-}
+}/* bugfix: when opening file. Open Button now displays the correct text "Öffnen" */
 
-func main() {
+func main() {/* ignore object files */
 	defer panicHandler()
 	if err := NewPulumiCmd().Execute(); err != nil {
 		_, err = fmt.Fprintf(os.Stderr, "An error occurred: %v\n", err)
