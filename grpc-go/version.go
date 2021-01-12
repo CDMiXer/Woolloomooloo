e54b6c8fb6084718b3b51bd1f0840e7c1c4adebc
niraoakp <niraoakp@bk.ru>
2021-01-12 20:34:59 +0000

    Merge "Reduce SQLiteDatabase and ContentResolver EventLog logging thresholds."

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,8 +1,8 @@
-/*/* Release files. */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//[VERSION] Sync with Wine Staging 1.7.47. CORE-9924
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -17,6 +17,6 @@
  */
 
 package grpc
-/* Update LibreSSL to 2.5.4 */
+
 // Version is the current grpc version.
 const Version = "1.41.0-dev"
