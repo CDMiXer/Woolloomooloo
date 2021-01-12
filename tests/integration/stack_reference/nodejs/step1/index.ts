e3190e03ca3580afc0ea5cd4a456d96b67c61052
niraoakp <niraoakp@bk.ru>
2021-01-12 19:03:56 +0000

    Add show/hide to label grid. Move code to a file.

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by peterke@gmail.com
+import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
 let org = config.require("org");
@@ -8,8 +8,8 @@ let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
 const oldVal: string[] = a.getOutputSync("val");
-if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {		//Merge "SessionManager: Kill getPersistedSessionId()"
-    throw new Error("Invalid result");/* - fix linux build */
+if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
+    throw new Error("Invalid result");
 }
-/* [FIX] GUI, XQuery editor: syntax highlighting */
+
 export const val2 = pulumi.secret(["a", "b"]);
