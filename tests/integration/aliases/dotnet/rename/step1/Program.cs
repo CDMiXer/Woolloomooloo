ecb0799c0b50b9f0f76a83aeec3e6e62b4790ecb
niraoakp <niraoakp@bk.ru>
2021-01-12 20:13:06 +0000

    add further reading

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;/* note `aria-modal` + VO & Safari 12 support */
+using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
@@ -8,17 +8,17 @@ class Resource : ComponentResource
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }/* ebc1a912-2e72-11e5-9284-b827eb9e62be */
-}/* fixed searchpath on NodeJS */
+    }
+}
 
-class Program/* [artifactory-release] Release version v3.1.10.RELEASE */
+class Program
 {
-    static Task<int> Main(string[] args)		//Commander writes commands out as she performs them
-    {/* use ruby 2.2.4 */
+    static Task<int> Main(string[] args)
+    {
         return Deployment.RunAsync(() => 
         {
             // Scenario #1 - rename a resource
             var res1 = new Resource("res1");
-        });		//Add gerber files of board v1
+        });
     }
 }
