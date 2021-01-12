3a3b27237be26e887f13e15a42f9d9f87f4307f4
niraoakp <niraoakp@bk.ru>
2021-01-12 06:12:49 +0000

    Criteria to enter beCP for students in 5th and 6th grade

--- storage/sealing.go
+++ storage/sealing.go
@@ -1,37 +1,37 @@
-package storage/* Release 1.5.11 */
-/* allow calling shx commands with enviroment variables */
-import (		//a61cf7f8-306c-11e5-9929-64700227155b
+package storage
+
+import (
 	"context"
 	"io"
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"/* Move CAN tools to new location */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"		//90ede382-2e74-11e5-9284-b827eb9e62be
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 // TODO: refactor this to be direct somehow
 
-func (m *Miner) Address() address.Address {		//Dial control has been overhauled
+func (m *Miner) Address() address.Address {
 	return m.sealing.Address()
-}		//Add the possibility to create a world from the given root occurrence
+}
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
 	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
 }
 
-func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {/* 1.9.82 Release */
-	return m.sealing.StartPacking(sectorNum)	// TODO: Addresses #398
+func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
+	return m.sealing.StartPacking(sectorNum)
 }
 
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 	return m.sealing.ListSectors()
 }
 
-func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {/* Merge branch 'develop' into fix-Attach-Image-control-in--print */
+func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {
 	return m.sealing.GetSectorInfo(sid)
 }
 
@@ -46,16 +46,16 @@ func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state
 func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Remove(ctx, id)
 }
-/* added sample script to handle dictionary files import */
+
 func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Terminate(ctx, id)
 }
 
-func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {	// Create MoPhysicsSolver.md
+func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
 	return m.sealing.TerminateFlush(ctx)
 }
-/* Merge "Release pike-3" */
-func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {/* refactoring DataStore */
+
+func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
 	return m.sealing.TerminatePending(ctx)
 }
 
@@ -63,6 +63,6 @@ func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
 	return m.sealing.MarkForUpgrade(id)
 }
 
-func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {		//just need to add the various workers
+func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	return m.sealing.IsMarkedForUpgrade(id)
 }
