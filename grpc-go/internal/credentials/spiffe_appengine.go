b2d11f6c281483f25033966e57249ee6f6f9d74e
niraoakp <niraoakp@bk.ru>
2021-01-12 21:02:50 +0000

    layout social fixed + logo

--- grpc-go/internal/credentials/spiffe_appengine.go
+++ grpc-go/internal/credentials/spiffe_appengine.go
@@ -1,9 +1,9 @@
-// +build appengine
+// +build appengine	// Deal correctly with empty saved tabs; explicitly specify new tab locations
 
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ *		//add basic Symfony Standard 2.3.x
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,21 +11,21 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [artifactory-release] Release version 2.0.1.BUILD */
- * See the License for the specific language governing permissions and/* Release Version 4.6.0 */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'master' into nan_bomb */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Prepare for device_collection editor */
  */
 
 package credentials
-		//rev 803335
+
 import (
 	"crypto/tls"
 	"net/url"
 )
 
-// SPIFFEIDFromState is a no-op for appengine builds.	// TODO: will be fixed by vyzo@hackzen.org
+// SPIFFEIDFromState is a no-op for appengine builds./* Release 0.1.2 - fix to deps build */
 func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	return nil
 }
