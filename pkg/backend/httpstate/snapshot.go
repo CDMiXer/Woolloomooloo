90097af04f9b652acc0407f0827317e177cbc677
niraoakp <niraoakp@bk.ru>
2021-01-12 05:51:15 +0000

    Update serf_test.go

--- pkg/backend/httpstate/snapshot.go
+++ pkg/backend/httpstate/snapshot.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation./* Rebuilt index with agerista */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,15 +9,15 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release Notes for v02-14-01 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package httpstate
 
-import (	// TODO: will be fixed by mail@bitpshr.net
-	"context"/* 3.0 Initial Release */
-/* Release for 2.4.1 */
-	"github.com/pkg/errors"	// Basic Makefile and new version of Metronome
+import (
+	"context"
+
+	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate/client"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
@@ -25,7 +25,7 @@ import (	// TODO: will be fixed by mail@bitpshr.net
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 )
 
-// cloudSnapshotPersister persists snapshots to the Pulumi service./* Connerie add de merde 2 */
+// cloudSnapshotPersister persists snapshots to the Pulumi service.
 type cloudSnapshotPersister struct {
 	context     context.Context         // The context to use for client requests.
 	update      client.UpdateIdentifier // The UpdateIdentifier for this update sequence.
@@ -41,24 +41,24 @@ func (persister *cloudSnapshotPersister) SecretsManager() secrets.Manager {
 func (persister *cloudSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
 	token, err := persister.tokenSource.GetToken()
 	if err != nil {
-		return err	// TODO: will be fixed by vyzo@hackzen.org
+		return err
 	}
 	deployment, err := stack.SerializeDeployment(snapshot, persister.sm, false /* showSecrets */)
 	if err != nil {
 		return errors.Wrap(err, "serializing deployment")
 	}
 	return persister.backend.client.PatchUpdateCheckpoint(persister.context, persister.update, deployment, token)
-}/* sem filechooser para imagem, vou criar o meu ¬¬ */
+}
 
 var _ backend.SnapshotPersister = (*cloudSnapshotPersister)(nil)
 
 func (cb *cloudBackend) newSnapshotPersister(ctx context.Context, update client.UpdateIdentifier,
 	tokenSource *tokenSource, sm secrets.Manager) *cloudSnapshotPersister {
-	return &cloudSnapshotPersister{	// Create SoftwareEngineering.md
+	return &cloudSnapshotPersister{
 		context:     ctx,
 		update:      update,
 		tokenSource: tokenSource,
-		backend:     cb,/* rev 526023 */
+		backend:     cb,
 		sm:          sm,
 	}
-}		//add link to iem sites from listing of stations
+}
