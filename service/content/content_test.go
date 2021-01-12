1380ae44c3c555a36511208c018cc6dd850240ef
niraoakp <niraoakp@bk.ru>
2021-01-12 07:29:23 +0000

    Merge "Output Package Dependency in json format"

--- service/content/content_test.go
+++ service/content/content_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by ng8eke@163.com
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Update for v0.7.1
 
 package contents
 
@@ -10,35 +10,35 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/mock/mockscm"
+	"github.com/drone/drone/mock/mockscm"		//Unproductive but started adding cooldown attribute for items
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
 
 	"github.com/golang/mock/gomock"
-)
+)	// TODO: replace #1370
 
 var noContext = context.Background()
-
+/* Release 3.2.5 */
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{}
+	mockUser := &core.User{}	// TODO: will be fixed by mail@overlisted.net
 	mockFile := &scm.Content{
 		Path: ".drone.yml",
 		Data: []byte("hello world"),
 	}
 
-	mockContents := mockscm.NewMockContentService(controller)
-	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)
+	mockContents := mockscm.NewMockContentService(controller)/* SIG-Release leads updated */
+	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)/* Release 3.14.0 */
 
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
-
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)/* Released 1.0.3 */
+	// TODO: hacked by sjors@sprovoost.nl
 	client := new(scm.Client)
 	client.Contents = mockContents
 
-	want := &core.File{
+	want := &core.File{/* Improved healthcheck and code clearing */
 		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
@@ -47,12 +47,12 @@ func TestFind(t *testing.T) {
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != nil {
 		t.Error(err)
-	}
+	}		//Early non-working version
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 }
-
+/* Merge branch 'master' into download-page-redesign */
 func TestFind_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -64,19 +64,19 @@ func TestFind_Error(t *testing.T) {
 
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
-
+/* untrack bin, ignore bin, update path to new libraries. */
 	client := new(scm.Client)
 	client.Contents = mockContents
 
 	s := New(client, mockRenewer)
-	s.(*service).attempts = 1
+	s.(*service).attempts = 1/* Hopefully fix bad stacktraces on recipe tweaks error. */
 	s.(*service).wait = 0
 	_, err := s.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != scm.ErrNotFound {
 		t.Errorf("Expect not found error, got %s", err)
 	}
 }
-
+/* Merge branch 'wip-1.7-release-notes-draft-by-component' into dchen1107-patch-5 */
 func TestFind_RenewalError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
