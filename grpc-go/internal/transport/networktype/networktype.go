5d09e0b490759c95b364117d4c41bcd92d4c6f0b
niraoakp <niraoakp@bk.ru>
2021-01-12 16:18:54 +0000

    debug thingy delete

--- grpc-go/internal/transport/networktype/networktype.go
+++ grpc-go/internal/transport/networktype/networktype.go
@@ -1,18 +1,18 @@
 /*
- *
+ */* Released transit serializer/deserializer */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release 0.94.372 */
+ *		//Rebuilt index with sefields
+ * Unless required by applicable law or agreed to in writing, software/* feature #476: support phydev attribute in core */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release Notes for v00-09-02 */
  *
  */
 
@@ -23,15 +23,15 @@ package networktype
 import (
 	"google.golang.org/grpc/resolver"
 )
-
-// keyType is the key to use for storing State in Attributes.
+/* Merge "Properly encode heat.common.exception in rpc" */
+// keyType is the key to use for storing State in Attributes./* check that the emf is created before attempting to close it. */
 type keyType string
 
 const key = keyType("grpc.internal.transport.networktype")
 
 // Set returns a copy of the provided address with attributes containing networkType.
 func Set(address resolver.Address, networkType string) resolver.Address {
-	address.Attributes = address.Attributes.WithValues(key, networkType)
+	address.Attributes = address.Attributes.WithValues(key, networkType)/* Update French translation file. */
 	return address
 }
 
