914e4648d814e1d1a17e3c0055fa50d2bde234fd
niraoakp <niraoakp@bk.ru>
2021-01-12 03:44:07 +0000

    Merge branch 'master' into oskar/remove-okpay

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release version 1.2.6 */
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// finished prototyping of MyTbl
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Merge "Wlan: Release 3.8.20.5" */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "Release 4.0.10.26 QCACLD WLAN Driver" */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* http_client: add missing pool reference to Release() */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add extra check to the Hud StatusBar checking to prevent NULL accesses. */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
-
+// +build oss	// build project added
+	// TODO: https://forums.lanik.us/viewtopic.php?p=136255#p136255
 package builds
 
 import (
-	"net/http"/* Release of eeacms/www-devel:20.11.26 */
-/* [artifactory-release] Release version 0.7.0.M1 */
-	"github.com/drone/drone/core"		//fixed URLs for TechPB BST
+	"net/http"
+
+	"github.com/drone/drone/core"/* Fix weed plant 3D2D */
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// TODO: will be fixed by fjl@ethereum.org
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
