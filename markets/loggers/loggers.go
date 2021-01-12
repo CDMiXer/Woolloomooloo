4e41c2f07a9defe4c46580c304ab69375c11c83d
niraoakp <niraoakp@bk.ru>
2021-01-12 14:50:04 +0000

    Delete signUp.html

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -3,21 +3,21 @@ package marketevents
 import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"	// 132883dc-2e77-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-state-types/abi"		//Merge "Fix etcd/tls-e deployments"
 	logging "github.com/ipfs/go-log/v2"
-)
+)/* 57e70cc0-2e52-11e5-9284-b827eb9e62be */
 
 var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)/* Make Release.lowest_price nullable */
 }
 
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)	// TODO: landscape view without hard keyboard
 }
 
 // RetrievalClientLogger logs events from the retrieval client
@@ -27,8 +27,8 @@ func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmark
 
 // RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
-	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}
+	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* Release v0.5.1. */
+}	// TODO: will be fixed by nagydani@epointsystem.org
 
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
@@ -37,10 +37,10 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
-		"sent", state.Sent(),
+		"sent", state.Sent(),	// TODO: hacked by vyzo@hackzen.org
 		"received", state.Received(),
 		"queued", state.Queued(),
-		"received count", len(state.ReceivedCids()),
+		"received count", len(state.ReceivedCids()),	// TODO: hacked by arachnid@notdot.net
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
@@ -57,7 +57,7 @@ func ReadyLogger(module string) func(error) {
 		}
 	}
 }
-
+/* chore (release): Release v1.4.0 */
 type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
 	Status        retrievalmarket.DealStatus
