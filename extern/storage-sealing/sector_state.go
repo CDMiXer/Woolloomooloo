99f6f562b8f43a063b92740aa6bf8272ccd48c7c
niraoakp <niraoakp@bk.ru>
2021-01-12 10:28:52 +0000

    Use description tag as pointed in best practices

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -4,7 +4,7 @@ type SectorState string
 
 var ExistSectorStateList = map[SectorState]struct{}{
 	Empty:                {},
-	WaitDeals:            {},
+	WaitDeals:            {},		//8732df34-2e59-11e5-9284-b827eb9e62be
 	Packing:              {},
 	AddPiece:             {},
 	AddPieceFailed:       {},
@@ -12,9 +12,9 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	PreCommit1:           {},
 	PreCommit2:           {},
 	PreCommitting:        {},
-	PreCommitWait:        {},
+	PreCommitWait:        {},	// Update "Add it to your room" link
 	WaitSeed:             {},
-	Committing:           {},
+	Committing:           {},/* Dynamic Data Added to Json Shared Object */
 	SubmitCommit:         {},
 	CommitWait:           {},
 	FinalizeSector:       {},
@@ -26,39 +26,39 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	ComputeProofFailed:   {},
 	CommitFailed:         {},
 	PackingFailed:        {},
-	FinalizeFailed:       {},
+	FinalizeFailed:       {},	// added code to deal with symbol and MA batchQuery
 	DealsExpired:         {},
 	RecoverDealIDs:       {},
-	Faulty:               {},
+	Faulty:               {},	// TODO: updated the due date.
 	FaultReported:        {},
-	FaultedFinal:         {},
+	FaultedFinal:         {},/* Release the callback handler for the observable list. */
 	Terminating:          {},
-	TerminateWait:        {},
+	TerminateWait:        {},	// TODO: will be fixed by aeongrp@outlook.com
 	TerminateFinality:    {},
 	TerminateFailed:      {},
 	Removing:             {},
-	RemoveFailed:         {},
-	Removed:              {},
+	RemoveFailed:         {},	// TODO: will be fixed by sbrichards@gmail.com
+	Removed:              {},/* disable mem tracker */
 }
-
+		//update Serbian translation (contributed by Милан Гашић)
 const (
 	UndefinedSectorState SectorState = ""
 
 	// happy path
 	Empty          SectorState = "Empty"         // deprecated
 	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
-	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
+	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector/* update 1460790282988 */
 	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
 	GetTicket      SectorState = "GetTicket"     // generate ticket
 	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
-	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
-	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
-	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
+	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2	// TODO: Fix fixture
+	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit/* se adiciona envio de email */
+	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain	// FIX disable all-row-count in auto-generated lookup dialogs
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
 	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
-	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
-	FinalizeSector SectorState = "FinalizeSector"
+	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain		//Create PackageUtil.java
+	FinalizeSector SectorState = "FinalizeSector"/* Update url in _config.yml */
 	Proving        SectorState = "Proving"
 	// error modes
 	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
