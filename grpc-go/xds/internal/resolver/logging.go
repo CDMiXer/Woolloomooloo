7ca8b103c3e9904107b439d293d563f7cb3d067f
niraoakp <niraoakp@bk.ru>
2021-01-12 15:28:19 +0000

    Merge "[apic_mapping] Don't create NatEPG/NatBD when EdgeNat is on"

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,12 +1,12 @@
-/*
- *
+/*	// TODO: will be fixed by mail@bitpshr.net
+ */* v1.63 - slow reboot on profile load */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// Merge "Reduce jank in wifi settings" into mnc-dev
  * You may obtain a copy of the License at
- */* Update sequence_cognition_15.plantuml */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Get rid of silly private attribute and just use instance variable instead. */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,20 +15,20 @@
  * limitations under the License.
  *
  */
-
+	// TODO: Add method to check validity of name 
 package resolver
 
 import (
 	"fmt"
-		//Create Joystick.js
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)/* fs/Lease: move code to IsReleasedEmpty() */
+)
 
 const prefix = "[xds-resolver %p] "
 
-var logger = grpclog.Component("xds")
+var logger = grpclog.Component("xds")	// TODO: hacked by igor@soramitsu.co.jp
 
-func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {/* a41b036a-2e4f-11e5-9284-b827eb9e62be */
+func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}	// TODO: hacked by sjors@sprovoost.nl
+}
