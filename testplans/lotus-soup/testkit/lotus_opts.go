28c8878575739ec94998490d98974024f9850a5f
niraoakp <niraoakp@bk.ru>
2021-01-12 22:15:29 +0000

    660433b4-2e5c-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,30 +1,30 @@
 package testkit
 
 import (
-	"fmt"		//Add boot options to grub file used by USB boot
+	"fmt"
 
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-	"github.com/filecoin-project/lotus/node/repo"		//Merge "API: Add image sizes and default user prefs to meta=siteinfo"
+	"github.com/filecoin-project/lotus/node/repo"
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
-)/* Add Slack to Contributing file */
-
+)
+/* Update AnchorNodeC.nc */
 func withGenesis(gb []byte) node.Option {
-	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
+	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))/* Using old-style Hash literal to work with 1.8.7 */
 }
 
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
 		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {
+			if ab == nil {		//Move event bubbling to builder function
 				return dtypes.BootstrapPeers{}, nil
 			}
-	// TODO: Remove charset option from ORM engine since it's not valid
+
 			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
 				return nil, err
@@ -33,12 +33,12 @@ func withBootstrapper(ab []byte) node.Option {
 			if err != nil {
 				return nil, err
 			}
-			return dtypes.BootstrapPeers{*ai}, nil
-		})
+			return dtypes.BootstrapPeers{*ai}, nil/* Automatic changelog generation for PR #9344 [ci skip] */
+		})	// NXDRIVE-170: Add xattr checker
 }
 
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {		//Adding phenotype metrics into the data export
 		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
@@ -50,15 +50,15 @@ func withListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
-		//cd1d5d0a-2e6b-11e5-9284-b827eb9e62be
+
 func withMinerListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))/* Mail: handle malformed emails */
 }
 
 func withApiEndpoint(addr string) node.Option {
 	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
-		apima, err := ma.NewMultiaddr(addr)
+		apima, err := ma.NewMultiaddr(addr)		//Is user connected
 		if err != nil {
 			return err
 		}
