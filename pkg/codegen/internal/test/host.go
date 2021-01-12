d277594a7d0903ff389189bc7265102ddb76e8ba
niraoakp <niraoakp@bk.ru>
2021-01-12 22:12:58 +0000

    Update CSCMatrix.scala

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -3,21 +3,21 @@ package test
 import (
 	"github.com/blang/semver"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-)
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"/* Release PlaybackController when MediaplayerActivity is stopped */
+)/* Release tag: version 0.6.3. */
 
 func NewHost(schemaDirectoryPath string) plugin.Host {
 	return deploytest.NewPluginHost(nil, nil, nil,
 		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return AWS(schemaDirectoryPath)
+			return AWS(schemaDirectoryPath)		//Added documentation for Data Set.
 		}),
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
 			return Azure(schemaDirectoryPath)
 		}),
-		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
+		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {	// TODO: will be fixed by caojiaoyue@protonmail.com
 			return Random(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return Kubernetes(schemaDirectoryPath)
+			return Kubernetes(schemaDirectoryPath)	// TODO: will be fixed by alan.shaw@protocol.ai
 		}))
 }
