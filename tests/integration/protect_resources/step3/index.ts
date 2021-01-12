1795cf710f937b0cf20c57f3d896bd9b7aa2f404
niraoakp <niraoakp@bk.ru>
2021-01-12 09:09:35 +0000

    popravljen shiny - povečan height na 1100

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* *Update rAthena up to 16854 */
 
 // Attempt to delete the resource; this will fail!
