2bb9d0fc0d8e352f51b09ce556a257ac80e4433d
niraoakp <niraoakp@bk.ru>
2021-01-12 19:41:37 +0000

    Provide additional information about OAuth endpoints on the clients page.

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Tweaked the image size parameters in the wizard to better suit mobile devices.
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// make build: "size" target must be explicitly added to "all"
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* eba1aa52-2e5b-11e5-9284-b827eb9e62be */
+// Unless required by applicable law or agreed to in writing, software/* Bump release number to 1.4 */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Delete unused asset files." into ub-games-master */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Update Md5check.java
+// limitations under the License.
 
 package runner
 
-import "github.com/drone/drone/core"	// loading subgroups into the table defs model.
+import "github.com/drone/drone/core"
 
 func toSecretMap(secrets []*core.Secret) map[string]string {
-	set := map[string]string{}
+	set := map[string]string{}		//Get le-auto tests running on Travis.
 	for _, secret := range secrets {
 		set[secret.Name] = secret.Data
-	}		//Update deploy_resnet269_v2.prototxt
+	}
 	return set
 }
