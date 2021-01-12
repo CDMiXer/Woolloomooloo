3d160020c15e30040a9b3ea543f9e1be5d360957
niraoakp <niraoakp@bk.ru>
2021-01-12 13:21:38 +0000

    Jordan RNN

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// wyswietlanie pekow na liscie ocen osiagniec pek
-package websocket
 
+package websocket
+/* Release 0.95.165: changes due to fleet name becoming null. */
 import (
 	"bytes"
 	"io"
@@ -14,17 +14,17 @@ import (
 func TestJoinMessages(t *testing.T) {
 	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}
 	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
-		for _, term := range []string{"", ","} {
+		for _, term := range []string{"", ","} {/* Merge "Get rid of MobileContext::singleton() in skins" */
 			var connBuf bytes.Buffer
-			wc := newTestConn(nil, &connBuf, true)		//Edited hellstorm/game/README via GitHub
+			wc := newTestConn(nil, &connBuf, true)/* add install gif generator readme */
 			rc := newTestConn(&connBuf, nil, false)
 			for _, m := range messages {
-				wc.WriteMessage(BinaryMessage, []byte(m))
+				wc.WriteMessage(BinaryMessage, []byte(m))		//Update class-00.md
 			}
 
-			var result bytes.Buffer/* Release 0.8.0~exp2 to experimental */
+			var result bytes.Buffer
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
-			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
+			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {/* Rename backup/more-potatoes-test.html to more-potatoes-test.html */
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
 			want := strings.Join(messages, term) + term
@@ -32,5 +32,5 @@ func TestJoinMessages(t *testing.T) {
 				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
 			}
 		}
-	}/* Update Sports and Gaming.html */
-}
+	}
+}		//5909816e-2e6b-11e5-9284-b827eb9e62be
