e397ef9fa6042be8c47b012ae0e14605d8691af9
niraoakp <niraoakp@bk.ru>
2021-01-12 17:17:52 +0000

    Rename usb_hid_usages.plist to DDHidStandardUsages.plist

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update sigcontext.h */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
-
-class Resource extends pulumi.ComponentResource {	// TODO: Merge "Dump json for nova.network.model.Model objects"
+import * as pulumi from "@pulumi/pulumi";		//update Readme to markdown format
+	// TODO: hacked by martin2cai@hotmail.com
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);	// change attribution in footer to link to website instead of git repo
+        super("my:module:Resource", name, {}, opts);
     }
-}		//Add missing placeholders to translations
+}/* Release 2.0.1. */
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;
+    resource: Resource;		//adds length settings for each segment in wheel graphs (+angle,+width). 
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
-    }
+    }		//Add README.me in chat_id_file
 }
-const comp5 = new ComponentFive("comp5");		//Merged r1578:1594 from trunk.
+const comp5 = new ComponentFive("comp5");
