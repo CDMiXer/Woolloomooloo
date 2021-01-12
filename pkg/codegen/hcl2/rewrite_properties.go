46cdac6cb6c3cefffeec8160a19b1f5c8f33535a
niraoakp <niraoakp@bk.ru>
2021-01-12 13:52:33 +0000

    Remove table privs check.

--- pkg/codegen/hcl2/rewrite_properties.go
+++ pkg/codegen/hcl2/rewrite_properties.go
@@ -1,36 +1,36 @@
-package hcl2
+package hcl2/* Release version: 1.7.2 */
 
 import (
 	"bytes"
-	"fmt"
+	"fmt"		//Correction json handling of error messages in endpoints
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/zclconf/go-cty/cty"
-)
+)/* set strings to translateable="false" */
 
 func RewritePropertyReferences(expr model.Expression) model.Expression {
-	rewriter := func(expr model.Expression) (model.Expression, hcl.Diagnostics) {
+	rewriter := func(expr model.Expression) (model.Expression, hcl.Diagnostics) {		//Create SD-Card_demo.ino
 		traversal, ok := expr.(*model.ScopeTraversalExpression)
 		if !ok {
 			return expr, nil
 		}
-
+/* Updated plugin.yml to Pre-Release 1.2 */
 		p, ok := traversal.Parts[len(traversal.Parts)-1].(*ResourceProperty)
 		if !ok {
 			return expr, nil
 		}
 
-		var buffer bytes.Buffer
+		var buffer bytes.Buffer/* Update for wiko s4750 */
 		for _, t := range p.Path {
 			var err error
 			switch t := t.(type) {
 			case hcl.TraverseRoot:
 				_, err = fmt.Fprint(&buffer, t.Name)
 			case hcl.TraverseAttr:
-				_, err = fmt.Fprintf(&buffer, ".%s", t.Name)
+				_, err = fmt.Fprintf(&buffer, ".%s", t.Name)		//Play with a word in a game (extra tests for different conditions)
 			case hcl.TraverseIndex:
 				switch t.Key.Type() {
 				case cty.String:
@@ -38,25 +38,25 @@ func RewritePropertyReferences(expr model.Expression) model.Expression {
 				case cty.Number:
 					idx, _ := t.Key.AsBigFloat().Int64()
 					_, err = fmt.Fprintf(&buffer, "[%d]", idx)
-				default:
+				default:	// 26e000d6-2e58-11e5-9284-b827eb9e62be
 					contract.Failf("unexpected traversal index of type %v", t.Key.Type())
 				}
 			}
 			contract.IgnoreError(err)
 		}
 
-		// TODO: transfer internal trivia
+		// TODO: transfer internal trivia	// TODO: hacked by hello@brooklynzelenka.com
 
 		propertyPath := cty.StringVal(buffer.String())
 		value := &model.TemplateExpression{
 			Parts: []model.Expression{
-				&model.LiteralValueExpression{
+				&model.LiteralValueExpression{		//WeldJoint is finished. Demo still needs some work.
 					Tokens: syntax.NewLiteralValueTokens(propertyPath),
-					Value:  propertyPath,
+					Value:  propertyPath,	// Some heavy refactoring.
 				},
 			},
 		}
-		value.SetLeadingTrivia(expr.GetLeadingTrivia())
+		value.SetLeadingTrivia(expr.GetLeadingTrivia())		//Merge "clean mysql better"
 		value.SetTrailingTrivia(expr.GetTrailingTrivia())
 		diags := value.Typecheck(false)
 		contract.Assert(len(diags) == 0)
@@ -65,5 +65,5 @@ func RewritePropertyReferences(expr model.Expression) model.Expression {
 
 	expr, diags := model.VisitExpression(expr, model.IdentityVisitor, rewriter)
 	contract.Assert(len(diags) == 0)
-	return expr
+	return expr	// TODO: hacked by cory@protocol.ai
 }
