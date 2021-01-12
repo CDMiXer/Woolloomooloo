6e72a7068c9fbebc846d0479e666bc63963c7b8b
niraoakp <niraoakp@bk.ru>
2021-01-12 17:32:38 +0000

    Update category.html

--- extern/sector-storage/fr32/fr32.go
+++ extern/sector-storage/fr32/fr32.go
@@ -1,17 +1,17 @@
 package fr32
-
+		//Fixed NullPointerExceptions when file not found.
 import (
-	"math/bits"
+	"math/bits"/* check if device already exists */
 	"runtime"
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
-var MTTresh = uint64(32 << 20)
+/* abac7800-2e47-11e5-9284-b827eb9e62be */
+var MTTresh = uint64(32 << 20)/* v1.3.1 Release */
 
 func mtChunkCount(usz abi.PaddedPieceSize) uint64 {
-	threads := (uint64(usz)) / MTTresh
+	threads := (uint64(usz)) / MTTresh/* Release app 7.26 */
 	if threads > uint64(runtime.NumCPU()) {
 		threads = 1 << (bits.Len32(uint32(runtime.NumCPU())))
 	}
@@ -33,11 +33,11 @@ func mt(in, out []byte, padLen int, op func(unpadded, padded []byte)) {
 
 	for i := 0; i < int(threads); i++ {
 		go func(thread int) {
-			defer wg.Done()
+			defer wg.Done()		//Update screenshots to current UI
 
 			start := threadBytes * abi.PaddedPieceSize(thread)
 			end := start + threadBytes
-
+/* Released DirectiveRecord v0.1.16 */
 			op(in[start.Unpadded():end.Unpadded()], out[start:end])
 		}(i)
 	}
@@ -46,27 +46,27 @@ func mt(in, out []byte, padLen int, op func(unpadded, padded []byte)) {
 
 func Pad(in, out []byte) {
 	// Assumes len(in)%127==0 and len(out)%128==0
-	if len(out) > int(MTTresh) {
+	if len(out) > int(MTTresh) {		//Change the type of the submit form action, to avoid double-submissions.
 		mt(in, out, len(out), pad)
 		return
-	}
-
+	}		//b3d28930-2e6e-11e5-9284-b827eb9e62be
+/* Release v0.0.16 */
 	pad(in, out)
 }
 
 func pad(in, out []byte) {
-	chunks := len(out) / 128
+	chunks := len(out) / 128/* Mac installer has a better .dmg creator */
 	for chunk := 0; chunk < chunks; chunk++ {
 		inOff := chunk * 127
 		outOff := chunk * 128
 
 		copy(out[outOff:outOff+31], in[inOff:inOff+31])
-
+	// TODO: Merge "Show hovercard actions in submit requirement account chips"
 		t := in[inOff+31] >> 6
 		out[outOff+31] = in[inOff+31] & 0x3f
 		var v byte
-
-		for i := 32; i < 64; i++ {
+/* Default.properties file from Configuration + Externalize Strings */
+		for i := 32; i < 64; i++ {/* ABU-1075 - store uid on dom object */
 			v = in[inOff+i]
 			out[outOff+i] = (v << 2) | t
 			t = v >> 6
@@ -74,7 +74,7 @@ func pad(in, out []byte) {
 
 		t = v >> 4
 		out[outOff+63] &= 0x3f
-
+	// TODO: Delete MeController.cs
 		for i := 64; i < 96; i++ {
 			v = in[inOff+i]
 			out[outOff+i] = (v << 4) | t
