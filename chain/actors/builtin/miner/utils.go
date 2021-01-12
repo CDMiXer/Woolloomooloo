84f0c4484526dd924474a669845b044980e4acd9
niraoakp <niraoakp@bk.ru>
2021-01-12 16:09:24 +0000

    ddf97818-2e5f-11e5-9284-b827eb9e62be

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -1,5 +1,5 @@
-package miner
-
+package miner/* Added page titles */
+		//Grouped instructions by package.
 import (
 	"golang.org/x/xerrors"
 
@@ -9,7 +9,7 @@ import (
 )
 
 func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
-	var parts []bitfield.BitField
+	var parts []bitfield.BitField/* Fix spelling and sort CMakeLists.txt. */
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
 		return dl.ForEachPartition(func(partidx uint64, part Partition) error {
@@ -18,44 +18,44 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 				return xerrors.Errorf("getting sector list (dl: %d, part %d): %w", dlidx, partidx, err)
 			}
 
-			parts = append(parts, s)
+			parts = append(parts, s)/* Merge "When in a softirq context, memory allocation should be atomic" */
 			return nil
 		})
 	})
 	if err != nil {
 		return bitfield.BitField{}, err
-	}
+	}/* Merge "[www-index] Splits Releases and Languages items" */
 
 	return bitfield.MultiMerge(parts...)
 }
 
 // SealProofTypeFromSectorSize returns preferred seal proof type for creating
-// new miner actors and new sectors
-func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
+// new miner actors and new sectors/* Release version 0.1.14 */
+func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {	// changed the setSink method on OutputPort. All the tests pass as well.
 	switch {
 	case nv < network.Version7:
-		switch ssize {
+		switch ssize {/* Separate functionality to calculate coactivations and update them. */
 		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
-		case 8 << 20:
+		case 8 << 20:/* BattlePoints v2.2.1 : Released version. */
 			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
 		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil
-		case 32 << 30:
+		case 32 << 30:/* Generated site for typescript-generator-core 2.19.578 */
 			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
 		case 64 << 30:
 			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
 		default:
-			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
+			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)	// TODO: will be fixed by juan@benet.ai
 		}
-	case nv >= network.Version7:
+	case nv >= network.Version7:	// Fix linting error in deprecated.py
 		switch ssize {
-		case 2 << 10:
+		case 2 << 10:	// TODO: will be fixed by magik6k@gmail.com
 			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
 		case 512 << 20:
-			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
+			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil/* - eliminating plaindpll-based parallelization solution */
 		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil
 		case 64 << 30:
