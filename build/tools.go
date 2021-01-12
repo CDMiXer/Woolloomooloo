52677bcf4db22bc2f6691a31b6f8aa8aaf0acd58
niraoakp <niraoakp@bk.ru>
2021-01-12 21:00:40 +0000

    Added .confuse, changes a str to alternating caps

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
-/* Added 'the most important changes since 0.6.1' in Release_notes.txt */
+
 package build
 
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"/* Update sqlite-jdbc to 3.32.3.1 */
+	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
 )
