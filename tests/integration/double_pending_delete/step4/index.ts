e850ff81a0135a212666922bb6548edc38d1e7c4
niraoakp <niraoakp@bk.ru>
2021-01-12 16:21:26 +0000

    Cleanup stray Windows reference.

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Release LastaTaglib-0.7.0 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Maven environment */
-//		//Removed unused cBlockHayBaleHandler.
+// you may not use this file except in compliance with the License./* IHTSDO Release 4.5.51 */
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//api decorator
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* a21a57bc-2e74-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: Adding space b/w bar
 import { Resource } from "./resource";
 
-// We'll complete our disaster recovery by triggering replacements of A and B again,
+// We'll complete our disaster recovery by triggering replacements of A and B again,		//Clean up indention and line endings
 // but this time the replacement of B will succeed.
 // The engine should generate:
 //
