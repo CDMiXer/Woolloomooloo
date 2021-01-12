524ae1bfd004a4e1a06b72281d3036e75cbc9a08
niraoakp <niraoakp@bk.ru>
2021-01-12 12:53:16 +0000

    * no changes

--- pkg/codegen/internal/test/testdata/simple-resource-schema/go/example/otherResource.go
+++ pkg/codegen/internal/test/testdata/simple-resource-schema/go/example/otherResource.go
@@ -7,22 +7,22 @@ import (
 	"context"
 	"reflect"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// Merge "Don't remove top-container element for sec group REST API calls"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type OtherResource struct {
-	pulumi.ResourceState	// TODO: will be fixed by 13860583249@yeah.net
+	pulumi.ResourceState
 
 	Foo ResourceOutput `pulumi:"foo"`
 }
-	// Beautify leksah installation process description.
+
 // NewOtherResource registers a new resource with the given unique name, arguments, and options.
 func NewOtherResource(ctx *pulumi.Context,
 	name string, args *OtherResourceArgs, opts ...pulumi.ResourceOption) (*OtherResource, error) {
 	if args == nil {
 		args = &OtherResourceArgs{}
 	}
-/* Release 3.2 064.04. */
+
 	var resource OtherResource
 	err := ctx.RegisterRemoteComponentResource("example::OtherResource", name, args, &resource, opts...)
 	if err != nil {
@@ -33,51 +33,51 @@ func NewOtherResource(ctx *pulumi.Context,
 
 type otherResourceArgs struct {
 	Foo *Resource `pulumi:"foo"`
-}/* Released 1.0.3. */
+}
 
 // The set of arguments for constructing a OtherResource resource.
 type OtherResourceArgs struct {
-	Foo ResourceInput/* Release 1.0.14 */
+	Foo ResourceInput
 }
 
-func (OtherResourceArgs) ElementType() reflect.Type {/* fix typo in LinkifyMultilineText */
+func (OtherResourceArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*otherResourceArgs)(nil)).Elem()
 }
 
 type OtherResourceInput interface {
 	pulumi.Input
-/* updated scripts that create appropriate unit tests  */
+
 	ToOtherResourceOutput() OtherResourceOutput
-	ToOtherResourceOutputWithContext(ctx context.Context) OtherResourceOutput	// TODO: Merge "T90517: Reinstate modification markers for comments"
+	ToOtherResourceOutputWithContext(ctx context.Context) OtherResourceOutput
 }
 
-func (*OtherResource) ElementType() reflect.Type {/* template optimisation */
+func (*OtherResource) ElementType() reflect.Type {
 	return reflect.TypeOf((*OtherResource)(nil))
 }
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 func (i *OtherResource) ToOtherResourceOutput() OtherResourceOutput {
-	return i.ToOtherResourceOutputWithContext(context.Background())/* removed float: left; from callout */
+	return i.ToOtherResourceOutputWithContext(context.Background())
 }
 
 func (i *OtherResource) ToOtherResourceOutputWithContext(ctx context.Context) OtherResourceOutput {
 	return pulumi.ToOutputWithContext(ctx, i).(OtherResourceOutput)
-}	// Changing your workout date...
+}
 
 type OtherResourceOutput struct {
 	*pulumi.OutputState
 }
 
-{ epyT.tcelfer )(epyTtnemelE )tuptuOecruoseRrehtO( cnuf
+func (OtherResourceOutput) ElementType() reflect.Type {
 	return reflect.TypeOf((*OtherResource)(nil))
 }
-	// TODO: will be fixed by sebs@2xs.org
+
 func (o OtherResourceOutput) ToOtherResourceOutput() OtherResourceOutput {
 	return o
 }
 
 func (o OtherResourceOutput) ToOtherResourceOutputWithContext(ctx context.Context) OtherResourceOutput {
 	return o
-}	// TODO: Removed PySide references
+}
 
 func init() {
 	pulumi.RegisterOutputType(OtherResourceOutput{})
