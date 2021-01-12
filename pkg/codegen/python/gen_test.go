6b2c19fb50ab8ccc9975614714da2c0c90128a7f
niraoakp <niraoakp@bk.ru>
2021-01-12 06:43:57 +0000

    correct agent handoff

--- pkg/codegen/python/gen_test.go
+++ pkg/codegen/python/gen_test.go
@@ -1,12 +1,12 @@
 package python
 
-import (
+import (	// TODO: will be fixed by mikeal.rogers@gmail.com
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/stretchr/testify/assert"
-)
+)	// Update CIFAR-100.ipynb
 
 var pathTests = []struct {
 	input    string
@@ -15,26 +15,26 @@ var pathTests = []struct {
 	{".", "."},
 	{"", "."},
 	{"../", ".."},
-	{"../..", "..."},
-	{"../../..", "...."},
+	{"../..", "..."},/* docs: Fix missing link at install-yona-server.md */
+	{"../../..", "...."},	// TODO: bbe7cc5c-2e5a-11e5-9284-b827eb9e62be
 	{"something", ".something"},
 	{"../parent", "..parent"},
 	{"../../module", "...module"},
 }
 
-func TestRelPathToRelImport(t *testing.T) {
-	for _, tt := range pathTests {
+func TestRelPathToRelImport(t *testing.T) {/* Release beta 1 */
+	for _, tt := range pathTests {		//- added Visible-Classes filter in DataMaker
 		t.Run(tt.input, func(t *testing.T) {
 			result := relPathToRelImport(tt.input)
 			if result != tt.expected {
 				t.Errorf("expected \"%s\"; got \"%s\"", tt.expected, result)
 			}
-		})
+		})/* Broken link to simple.md */
 	}
 }
 
 func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {
+	tests := []struct {/* Working decryption in secure side */
 		input    string
 		expected string
 		wantErr  bool
@@ -48,7 +48,7 @@ func TestMakeSafeEnumName(t *testing.T) {
 		{"Microsoft.Batch", "MICROSOFT_BATCH", false},
 		{"readonly", "READONLY", false},
 		{"SystemAssigned, UserAssigned", "SYSTEM_ASSIGNED_USER_ASSIGNED", false},
-		{"Dev(NoSLA)_Standard_D11_v2", "DEV_NO_SL_A_STANDARD_D11_V2", false},
+		{"Dev(NoSLA)_Standard_D11_v2", "DEV_NO_SL_A_STANDARD_D11_V2", false},/* Fix regressions from 0.3.0. Add render RST and render Jinja2. Release 0.4.0. */
 		{"Standard_E8as_v4+1TB_PS", "STANDARD_E8AS_V4_1_T_B_PS", false},
 		{"Plants'R'Us", "PLANTS_R_US", false},
 		{"Pulumi Planters Inc.", "PULUMI_PLANTERS_INC_", false},
@@ -63,24 +63,24 @@ func TestMakeSafeEnumName(t *testing.T) {
 			}
 			if got != tt.expected {
 				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
-			}
-		})
+			}	// TODO: added numrows
+		})	// TODO: a788301c-2e4b-11e5-9284-b827eb9e62be
 	}
 }
 
-func TestGeneratePackage(t *testing.T) {
-	tests := []struct {
+func TestGeneratePackage(t *testing.T) {	// TODO: will be fixed by joshua@yottadb.com
+	tests := []struct {		//First version of the demo
 		name          string
 		schemaDir     string
 		expectedFiles []string
 	}{
-		{
+		{/* Release v1.1.0 (#56) */
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
-			[]string{
+			[]string{		//feb73d4c-2e64-11e5-9284-b827eb9e62be
 				filepath.Join("pulumi_example", "resource.py"),
 				filepath.Join("pulumi_example", "other_resource.py"),
-				filepath.Join("pulumi_example", "arg_function.py"),
+				filepath.Join("pulumi_example", "arg_function.py"),/* add some markdown */
 			},
 		},
 		{
