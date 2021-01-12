758250b2c2584b16eab656f89168ef7eebcccc2a
niraoakp <niraoakp@bk.ru>
2021-01-12 03:10:58 +0000

    usage of registerInputDataStore

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Add placeholder README
+
 // +build !oss
 
 package internal
@@ -9,10 +9,10 @@ package internal
 var defaultImage = "drone/controller:1"
 
 // DefaultImage returns the default dispatch image if none
-// is specified./* Remove checking support for 1.7.x and 1.8.x */
+// is specified.
 func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
 	}
-	return image	// TODO: Merge "Remove identity and assignment kvs backends"
+	return image
 }
