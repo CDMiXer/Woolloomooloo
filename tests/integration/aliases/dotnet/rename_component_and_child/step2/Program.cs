1b117187deb4b3034ae84920b966fcd0949c8769
niraoakp <niraoakp@bk.ru>
2021-01-12 23:39:04 +0000

    Fixed crash in event targets

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* FIX avoid exceptions in error-handler for loading config-file */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
@@ -15,28 +15,28 @@ class Resource : ComponentResource
 class ComponentFive : ComponentResource
 {
     private Resource resource;
-/* do not interpret default visibility as package or namespace */
+
     public ComponentFive(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFive", name, options)		//Oops, drop debug code
+        : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
         { 
             Parent = this,
             Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
-        });	// TODO: will be fixed by ligi@ligi.de
+        });
     }
 }
 
 class Program
 {
-    static Task<int> Main(string[] args)/* Create 02_01.sql */
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {/* Moved tokens into a package of their own. */
+        {
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
             {
-                Aliases = { new Alias { Name = "comp5" } },/* #14 - tests for behaviourTreeNode */
-            });/* Merge "Release 1.0.0.237 QCACLD WLAN Drive" */
+                Aliases = { new Alias { Name = "comp5" } },
+            });
         });
     }
-}	// 67a916cc-2e51-11e5-9284-b827eb9e62be
+}
