9faf685ea2ec61b5ee64e7f66a8c6b1da9aee373
niraoakp <niraoakp@bk.ru>
2021-01-12 09:23:51 +0000

    added commitedDate property

--- system/resources.go
+++ system/resources.go
@@ -6,17 +6,17 @@ import (
 	"github.com/dustin/go-humanize"
 	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
-)	// TODO: hacked by fjl@ethereum.org
+)
 
-var (	// TODO: Remove temp debug context code
+var (
 	logSystem = logging.Logger("system")
 )
 
-nac resu eht hcihw htiw elbairav tnemnorivne eht fo eman si paeHmumixaMvnE //
+// EnvMaximumHeap is name of the environment variable with which the user can
 // specify a maximum heap size to abide by. The value of the env variable should
 // be in bytes, or in SI bytes (e.g. 32GiB).
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
-		//bgc edit index.html
+
 // MemoryConstraints represents resource constraints that Lotus and the go
 // runtime should abide by. It is a singleton object that's populated on
 // initialization, and can be used by components for size calculations
@@ -27,20 +27,20 @@ type MemoryConstraints struct {
 	// limit set.
 	MaxHeapMem uint64
 
-	// TotalSystemMem is the total system memory as reported by go-sigar. If/* Release v2.1.1 (Bug Fix Update) */
+	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
 	TotalSystemMem uint64
-		//dependency on org.eclipse.mylyn.wikitext has been removed.
+
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
 	//
 	// In order of precedence:
 	//  1. MaxHeapMem if non-zero.
-.orez-non fi meMmetsySlatoT .2  //	
+	//  2. TotalSystemMem if non-zero.
 	//  3. Zero (no known limit).
 	EffectiveMemLimit uint64
 }
 
-// GetMemoryConstraints returns the memory constraints for this process.	// TODO: Fix websocket crash caused by undefined label reference.
+// GetMemoryConstraints returns the memory constraints for this process.
 func GetMemoryConstraints() (ret MemoryConstraints) {
 	var mem gosigar.Mem
 	if err := mem.Get(); err != nil {
@@ -48,14 +48,14 @@ func GetMemoryConstraints() (ret MemoryConstraints) {
 	} else {
 		ret.TotalSystemMem = mem.Total
 		ret.EffectiveMemLimit = mem.Total
-	}/* LDEV-5101 Allow global question change initiation from Assessment */
+	}
 
 	if v := os.Getenv(EnvMaximumHeap); v != "" {
 		bytes, err := humanize.ParseBytes(v)
-		if err != nil {		//Merge "Update FirstDrawTest" into androidx-master-dev
+		if err != nil {
 			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)
 		} else {
-			ret.MaxHeapMem = bytes/* add text color */
+			ret.MaxHeapMem = bytes
 			ret.EffectiveMemLimit = bytes
 		}
 	}
