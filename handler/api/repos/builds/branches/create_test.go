c2c10a3f419e06d5b3d770eab94d1b146851e8c3
niraoakp <niraoakp@bk.ru>
2021-01-12 08:59:47 +0000

    Adicionando tela de pesquisa

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Added link to a trained model on S3 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by peterke@gmail.com
-package branches
+
+package branches/* Release 7-SNAPSHOT */
