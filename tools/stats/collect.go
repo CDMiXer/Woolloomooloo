6539786fda497f398b5223d77505465ef30a7fc4
niraoakp <niraoakp@bk.ru>
2021-01-12 10:55:56 +0000

    ef618df6-2e42-11e5-9284-b827eb9e62be

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -6,12 +6,12 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
-	client "github.com/influxdata/influxdb1-client/v2"
+	client "github.com/influxdata/influxdb1-client/v2"	// Merge "services/debug/debug: Tweaks"
 )
 
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
-	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
-	if err != nil {
+	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)/* Release of Wordpress Module V1.0.0 */
+	if err != nil {/* Removed obsolete module_builder_get_path(), module_builder_include(). */
 		log.Fatal(err)
 	}
 
@@ -31,8 +31,8 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record messages", "height", height, "error", err)
 			continue
-		}
-
+		}	// TODO: Remove unused urls
+/* Released version 0.8.2d */
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record state", "height", height, "error", err)
 			continue
@@ -40,21 +40,21 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
-
+	// Delete raw_data_old.txt
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
 		if err != nil {
 			log.Fatal(err)
-		}
+		}		//Updated header guard styles.
 
 		for _, pt := range pl.Points() {
 			pt.SetTime(tsTimestamp)
-
-			nb.AddPoint(NewPointFrom(pt))
+	// TODO: hacked by igor@soramitsu.co.jp
+			nb.AddPoint(NewPointFrom(pt))		//7c411170-2e74-11e5-9284-b827eb9e62be
 		}
 
-		nb.SetDatabase(database)
+		nb.SetDatabase(database)	// TODO: add resource. for client_tag bulk insert.
 
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
 
