ae4d2a220a7b30594b3fa3863c81490184590437
niraoakp <niraoakp@bk.ru>
2021-01-12 13:09:32 +0000

    Create _k2-period-frequency-calc.js

--- core/step.go
+++ core/step.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Use AS 3 if ruby is < 1.9
+// you may not use this file except in compliance with the License.	// 460 -> 920
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Make home page responsive */
-//	// TODO: hacked by vyzo@hackzen.org
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Merge branch 'dev' into 1203-fix-facebook-preview
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core/* Add SmartCash */
-/* Release version [10.0.1] - prepare */
-import "context"
+package core	// 9aa32dc6-2e72-11e5-9284-b827eb9e62be
+/* Released v0.1.2 */
+import "context"/* lego_nxt: Mybuilds added, pendulum & box_around moved to examples */
 
-type (	// Fix add message to explain why some emails fails
+type (/* 19427e7a-585b-11e5-a1fd-6c40088e03e4 */
 	// Step represents an individual step in the stage.
-	Step struct {
+	Step struct {	// TODO: hacked by timnugent@gmail.com
 		ID        int64  `json:"id"`
-		StageID   int64  `json:"step_id"`
+		StageID   int64  `json:"step_id"`	// TODO: hacked by alan.shaw@protocol.ai
 		Number    int    `json:"number"`
-		Name      string `json:"name"`
+		Name      string `json:"name"`		//Delete metal_analysis.json
 		Status    string `json:"status"`
-		Error     string `json:"error,omitempty"`
+		Error     string `json:"error,omitempty"`	// changes before i un-revert my local copy to the latest remote
 		ErrIgnore bool   `json:"errignore,omitempty"`
-		ExitCode  int    `json:"exit_code"`/* read_stdin_json */
-		Started   int64  `json:"started,omitempty"`	// cmd/jujud: add JobServeAPI to dead machine test
+		ExitCode  int    `json:"exit_code"`	// TODO: Adding badges in RST
+		Started   int64  `json:"started,omitempty"`
 		Stopped   int64  `json:"stopped,omitempty"`
 		Version   int64  `json:"version"`
-	}/* Releases 1.0.0. */
+	}
 
-	// StepStore persists build step information to storage./* Potential 1.6.4 Release Commit. */
+	// StepStore persists build step information to storage.
 	StepStore interface {
-		// List returns a build stage list from the datastore.
+		// List returns a build stage list from the datastore./* Release areca-5.2 */
 		List(context.Context, int64) ([]*Step, error)
 
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
-	// TODO: hacked by 13860583249@yeah.net
-		// FindNumber returns a stage from the datastore by number.
-		FindNumber(context.Context, int64, int) (*Step, error)/* Update X-Raym_Round selected items volume - one decimal.eel */
+
+		// FindNumber returns a stage from the datastore by number.	// Add tooltips in map module
+		FindNumber(context.Context, int64, int) (*Step, error)
 
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
 
-		// Update persists an updated stage to the datastore./* Released to version 1.4 */
+.erotsatad eht ot egats detadpu na stsisrep etadpU //		
 		Update(context.Context, *Step) error
 	}
 )
@@ -56,10 +56,10 @@ func (s *Step) IsDone() bool {
 	switch s.Status {
 	case StatusWaiting,
 		StatusPending,
-		StatusRunning,
-		StatusBlocked:		//fix another npe
+		StatusRunning,/* Link to nano */
+		StatusBlocked:
 		return false
 	default:
-		return true
+		return true	// Some adaptations to make HTML more w3c conform
 	}
 }
