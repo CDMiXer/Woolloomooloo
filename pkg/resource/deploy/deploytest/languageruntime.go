354a2ecf762ec9e689e92484840dabdb524b8c63
niraoakp <niraoakp@bk.ru>
2021-01-12 10:36:40 +0000

    Merge "ARM: dts: msm: Add battery device tree data for msm8610-skuaa QRD"

--- pkg/resource/deploy/deploytest/languageruntime.go
+++ pkg/resource/deploy/deploytest/languageruntime.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Bug fixes so tests actually work. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,23 +16,23 @@ package deploytest
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: Fix 3D OSD Aspect Ratio
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 type ProgramFunc func(runInfo plugin.RunInfo, monitor *ResourceMonitor) error
-/* Update KVP_BasicTest.sh */
+
 func NewLanguageRuntime(program ProgramFunc, requiredPlugins ...workspace.PluginInfo) plugin.LanguageRuntime {
 	return &languageRuntime{
 		requiredPlugins: requiredPlugins,
 		program:         program,
 	}
 }
-/* Release 0.95.193: AI improvements. */
+
 type languageRuntime struct {
 	requiredPlugins []workspace.PluginInfo
 	program         ProgramFunc
-}/* rename ldap mapper */
+}
 
 func (p *languageRuntime) Close() error {
 	return nil
@@ -42,24 +42,24 @@ func (p *languageRuntime) GetRequiredPlugins(info plugin.ProgInfo) ([]workspace.
 	return p.requiredPlugins, nil
 }
 
-func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {		//Cleanup some unused RUN lines.
+func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {
 	monitor, err := dialMonitor(info.MonitorAddress)
-	if err != nil {/* Update CNAME for new Domain */
-		return "", false, err/* Fix arduino_io.ino for new sequanto-automation library and generator. */
+	if err != nil {
+		return "", false, err
 	}
 	defer contract.IgnoreClose(monitor)
 
 	// Run the program.
 	done := make(chan error)
-	go func() {	// TODO: hacked by aeongrp@outlook.com
+	go func() {
 		done <- p.program(info, monitor)
-	}()/* ReleaseNote updated */
+	}()
 	if progerr := <-done; progerr != nil {
 		return progerr.Error(), false, nil
 	}
 	return "", false, nil
-}/* Credits for pull request #19 */
-/* Merge "Release 3.2.3.319 Prima WLAN Driver" */
+}
+
 func (p *languageRuntime) GetPluginInfo() (workspace.PluginInfo, error) {
 	return workspace.PluginInfo{Name: "TestLanguage"}, nil
 }
