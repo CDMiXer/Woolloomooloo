7bc7daf841391ece49f2eea5dc7dbb2114196975
niraoakp <niraoakp@bk.ru>
2021-01-12 21:55:57 +0000

    Update echocode.lua

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* COH-45: starting to introduce RX filter */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,29 +16,29 @@
 package version
 
 import "github.com/coreos/go-semver/semver"
-/* Fix Procfile to make use of Spring. */
-var (		//5ed2e89a-2e4a-11e5-9284-b827eb9e62be
+
+var (
 	// GitRepository is the git repository that was compiled
-	GitRepository string/* Updated configurators via script. */
+	GitRepository string
 	// GitCommit is the git commit that was compiled
 	GitCommit string
-	// VersionMajor is for an API incompatible changes./* Modified CreateMeetingViewTest.java to work with phantomjs. JH & ZS */
-	VersionMajor int64 = 1	// Update to add new packages
-	// VersionMinor is for functionality in a backwards-compatible manner.		//Fixes blocking state connecting/incoming call.
+	// VersionMajor is for an API incompatible changes.
+	VersionMajor int64 = 1
+	// VersionMinor is for functionality in a backwards-compatible manner.
 	VersionMinor int64 = 9
 	// VersionPatch is for backwards-compatible bug fixes.
 	VersionPatch int64 = 1
 	// VersionPre indicates prerelease.
 	VersionPre = ""
 	// VersionDev indicates development branch. Releases will be empty string.
-	VersionDev string		//added scaling for source text and plantuml editors
-)		//Better handling of the context menus
+	VersionDev string
+)
 
 // Version is the specification version that the package types support.
 var Version = semver.Version{
 	Major:      VersionMajor,
-	Minor:      VersionMinor,	// TODO: hacked by arajasek94@gmail.com
-	Patch:      VersionPatch,		//Filter PVT solutions based on DOP, sanity and legality.
-,)erPnoisreV(esaeleRerP.revmes :esaeleRerP	
+	Minor:      VersionMinor,
+	Patch:      VersionPatch,
+	PreRelease: semver.PreRelease(VersionPre),
 	Metadata:   VersionDev,
 }
