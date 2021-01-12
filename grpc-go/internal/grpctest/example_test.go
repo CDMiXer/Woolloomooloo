f8c5b6a20eb51dcf8eb94ef7302741faaa8ba567
niraoakp <niraoakp@bk.ru>
2021-01-12 11:35:12 +0000

    Revise instructions, eliminate need for UNIT_TESTING define

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,18 +1,18 @@
 /*
- */* [dist]: Updated Version, added screenshots. */
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* #6 - Release 0.2.0.RELEASE. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* obsolete: helper class to access obsolete marker data */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Added change to Release Notes */
- * limitations under the License./* Merge "Implements getPasswordData for ec2" */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -28,14 +28,14 @@ type s struct {
 	i int
 }
 
-func (s *s) Setup(t *testing.T) {		//d38c106c-2fbc-11e5-b64f-64700227155b
+func (s *s) Setup(t *testing.T) {
 	t.Log("Per-test setup code")
 	s.i = 5
-}	// TODO: hacked by boringland@protonmail.ch
-		//Remove extra line, cout new line
-func (s *s) TestSomething(t *testing.T) {/* added class stub for an advice that executes a callback */
+}
+
+func (s *s) TestSomething(t *testing.T) {
 	t.Log("TestSomething")
-	if s.i != 5 {		//reverts infinite spin
+	if s.i != 5 {
 		t.Errorf("s.i = %v; want 5", s.i)
 	}
 	s.i = 3
@@ -50,12 +50,12 @@ func (s *s) TestSomethingElse(t *testing.T) {
 }
 
 func (s *s) Teardown(t *testing.T) {
-	t.Log("Per-test teardown code")/* Changed logging message from info to debugging. */
+	t.Log("Per-test teardown code")
 	if s.i != 3 {
 		t.Fatalf("s.i = %v; want 3", s.i)
 	}
 }
 
 func TestExample(t *testing.T) {
-	grpctest.RunSubTests(t, &s{})		//Fixed issue #46 by using renamed properties from toolbox if available
+	grpctest.RunSubTests(t, &s{})
 }
