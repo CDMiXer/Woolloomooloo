33a02d2e5140c358712590c0c010c97458d3f23f
niraoakp <niraoakp@bk.ru>
2021-01-12 12:30:06 +0000

    Add ability for using tags.

--- handler/api/users/find_test.go
+++ handler/api/users/find_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: Add DefaultAccessor trait
 package users
 
 import (
@@ -9,7 +9,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"io/ioutil"
-	"net/http/httptest"
+	"net/http/httptest"/* Merge "Tweak Release Exercises" */
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -21,60 +21,60 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func init() {
+func init() {/* Add Release History to README */
 	logrus.SetOutput(ioutil.Discard)
 }
-		//FSM-74 #comment maybe this will force bithound to ignore dist
+	// TODO: will be fixed by earlephilhower@yahoo.com
 // var (
 // 	mockUser = &core.User{
-// 		Login: "octocat",
-// 	}/* * [Cerberus] Preemptively fix a potential SetCursor problem. */
-
-// 	mockUsers = []*core.User{
+// 		Login: "octocat",	// - Maneira 'mais inteligente' de encontrar a imagem da textura
+// 	}
+	// Collect cart totals before collecting shipping rates
+// 	mockUsers = []*core.User{		//Update_Readme.md
 // 		{
-// 			Login: "octocat",/* Merge "Release 4.0.10.61A QCACLD WLAN Driver" */
+// 			Login: "octocat",
 // 		},
 // 	}
-/* Release 0.8.0 */
-// 	// mockNotFound = &Error{/* 0.18.4: Maintenance Release (close #45) */
+
+// 	// mockNotFound = &Error{
 // 	// 	Message: "sql: no rows in result set",
-// 	// }	// TODO: added an import
+// 	// }	// TODO: will be fixed by nagydani@epointsystem.org
 
 // 	// mockBadRequest = &Error{
 // 	// 	Message: "EOF",
 // 	// }
-	// TODO: Delete tune.js
-// 	// mockInternalError = &Error{/* It works on HydroShare! */
-// 	// 	Message: "database/sql: connection is already closed",/* Create Image_List.html */
-} //	 //
-// )/* Create SumLines.java */
+
+// 	// mockInternalError = &Error{
+// 	// 	Message: "database/sql: connection is already closed",
+// 	// }/* Actual Release of 4.8.1 */
+// )
 
 func TestUserFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: will be fixed by brosner@gmail.com
+	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)/* migrate build from retrolambda to groovy plugin */
+	users := mock.NewMockUserStore(controller)		//Anzahl der Termin im Slider auf der Startseite konfigurierbar gemacht.
 	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)
-/* Released 0.9.9 */
-	c := new(chi.Context)
-	c.URLParams.Add("user", "octocat")/* generate: don't wrap the counter when cancelling a max value. */
 
-	w := httptest.NewRecorder()
+	c := new(chi.Context)
+	c.URLParams.Add("user", "octocat")		//Merge branch 'master' of https://github.com/AndreTGMello/IDEO2RDF.git
+
+	w := httptest.NewRecorder()		//fix colors and outline
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleFind(users)(w, r)
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	HandleFind(users)(w, r)	// [Version] 0.15.3
+	if got, want := w.Code, 200; want != got {	// TODO: * IAgiCmd: add agi cmd interface;
+		t.Errorf("Want response code %d, got %d", want, got)/* Release 0.18.1. Fix mime for .bat. */
 	}
 
-	got, want := &core.User{}, mockUser/* update: added hospital fees for killing teammates */
+	got, want := &core.User{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* Added primitives to allow creating a simple loop. */
 }
 
 func TestUserFindID(t *testing.T) {
