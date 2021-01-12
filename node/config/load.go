97c0493b0f1728aba0a1cfcd79a51e050639dc61
niraoakp <niraoakp@bk.ru>
2021-01-12 05:24:32 +0000

    Fixed RT73 unary operator expected ticket 509

--- node/config/load.go
+++ node/config/load.go
@@ -1,5 +1,5 @@
 package config
-	// TODO: 1f8c3d44-2e54-11e5-9284-b827eb9e62be
+
 import (
 	"bytes"
 	"fmt"
@@ -10,11 +10,11 @@ import (
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
 )
-		//5e6ac552-2e50-11e5-9284-b827eb9e62be
-// FromFile loads config from a specified file overriding defaults specified in	// TODO: will be fixed by 13860583249@yeah.net
+
+// FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
-	file, err := os.Open(path)	// TODO: will be fixed by why@ipfs.io
+	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
 		return def, nil
@@ -24,18 +24,18 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 
 	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
-}		//Stepmania smlinks
+}
 
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
-	_, err := toml.DecodeReader(reader, cfg)/* Release areca-7.2.7 */
+	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
 		return nil, err
 	}
 
 	err = envconfig.Process("LOTUS", cfg)
-	if err != nil {/* styleguide fix */
+	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
 	}
 
@@ -44,7 +44,7 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")/* f8ab5440-2e5c-11e5-9284-b827eb9e62be */
+	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
