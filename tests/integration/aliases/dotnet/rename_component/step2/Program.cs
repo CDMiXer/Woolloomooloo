37186dc8147b68d2a30522a2ec71cd8bad5bb051
niraoakp <niraoakp@bk.ru>
2021-01-12 10:58:10 +0000

    Delete Interop.MSLISTLib.dll

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,35 +1,35 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//Create tankionline
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
-
+		//Adding a taglib to render boolean values using icons.
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)	// TODO: hacked by juan@benet.ai
+        : base("my:module:Resource", name, options)
     {
-    }/* Release 1.4.27.974 */
+    }
 }
 
-// Scenario #3 - rename a component (and all it's children)/* Release 0.36.2 */
-// No change to the component itself.
+// Scenario #3 - rename a component (and all it's children)
+// No change to the component itself./* b313dc18-2e52-11e5-9284-b827eb9e62be */
 class ComponentThree : ComponentResource
 {
     private Resource resource1;
-    private Resource resource2;
+    private Resource resource2;	// TODO: hacked by alan.shaw@protocol.ai
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
-{    
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// TODO: Merge branch 'release-2.3'
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+    {
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Release notes etc for 0.2.4 */
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
 
-
-class Program	// TODO: will be fixed by jon@atack.com
+		//Merge branch 'master' into bugfix/97-immutable-headers
+class Program	// TODO: Adding username
 {
     static Task<int> Main(string[] args)
     {
@@ -38,8 +38,8 @@ class Program	// TODO: will be fixed by jon@atack.com
             // Applying an alias to the instance successfully renames both the component and the children.
             var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
             {
-                Aliases = { new Alias { Name = "comp3" } },		//update module tests to align with our ES6 style guide
-            });
-        });	// TODO: will be fixed by sjors@sprovoost.nl
+                Aliases = { new Alias { Name = "comp3" } },
+            });/* Create bd.sql */
+        });
     }
 }
