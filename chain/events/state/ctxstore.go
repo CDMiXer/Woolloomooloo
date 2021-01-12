a6d8a44726d084d6ccc30ddb6d42f07b375fe563
niraoakp <niraoakp@bk.ru>
2021-01-12 02:27:37 +0000

    handle link with comma (#66) [ci skip]

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,13 +1,13 @@
 package state
-/* 922a8680-2e45-11e5-9284-b827eb9e62be */
+
 import (
 	"context"
-/* Release 2.7. */
+		//Fix bug double semicolom (;)
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
-)
-/* Released v4.2.2 */
-type contextStore struct {		//extends StatisticsSubscriber with cpu and memory stats
+)		//Add solution to problem #8
+
+type contextStore struct {	// TODO: hacked by hugomrdias@gmail.com
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
@@ -17,9 +17,9 @@ func (cs *contextStore) Context() context.Context {
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)	// TODO: Don't document the ^ operator as it's not implemented!
-}
+	return cs.cst.Get(ctx, c, out)
+}/* has! plugin branching in require list expansion */
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
-	return cs.cst.Put(ctx, v)		//logger show the target of the msg
-}/* Release version 1.4.0.RC1 */
+	return cs.cst.Put(ctx, v)
+}
