ee01fdc2f6dfe9d799061107bcfca5e210684d22
niraoakp <niraoakp@bk.ru>
2021-01-12 15:21:17 +0000

    Update import paths.

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -1,66 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* fix -Wunused-variable warning in Release mode */
+// Use of this source code is governed by the Drone Non-Commercial License/* Start to write some tests. put all the machinery in place to run the tests */
 // that can be found in the LICENSE file.
+/* Illegal Paris: added timestamp */
+// +build !oss
 
-// +build !oss/* Proxmox 6 Release Key */
+package crons
 
-package crons	// Make winesteam compatible with configurable wine path
-
-import (
+import (/* Release dhcpcd-6.6.6 */
 	"context"
 	"encoding/json"
-	"net/http"
-	"net/http/httptest"/* Test Release RC8 */
+	"net/http"/* 5.3.7 Release */
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* Released Code Injection Plugin */
-/* Add Snyk vulnerability badge */
+	"github.com/drone/drone/mock"
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Rename registration.py to reg_projectManager.py */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Optimize the Deferred object.[skip ci] */
-
+)
+	// TODO: dba33f: memeber ini
 func TestHandleFind(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()	// Added `fail_if_no_header` to ChunkedUploadView
-
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
-/* Capitalised admin menu commands */
-	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
-
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")/* Update init.sls */
-
-	w := httptest.NewRecorder()		//Create ext.php
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
-	HandleFind(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-	// List of related Ruby DOCX projects.
-	got, want := &core.Cron{}, dummyCron
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: Formattare stringhe output
-		t.Errorf(diff)
-	}
-}
-
-func TestHandleFind_RepoNotFound(t *testing.T) {/* CHANGELOG.md: Mention #7, add commit references */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)/* KeyMapper now uses HashMap instead of Hashtable internally - fixed test */
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+
+	crons := mock.NewMockCronStore(controller)/* Rename RecentChanges.md to ReleaseNotes.md */
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -72,11 +42,41 @@ func TestHandleFind_RepoNotFound(t *testing.T) {/* CHANGELOG.md: Mention #7, add
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
-	HandleFind(repos, nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {
+	// TODO: Error dialog moved to AgateWinForms if present.
+	HandleFind(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
+
+	got, want := &core.Cron{}, dummyCron
+	json.NewDecoder(w.Body).Decode(got)/* Release version 0.2.0 beta 2 */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleFind_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+)rellortnoc(erotSyrotisopeRkcoMweN.kcom =: soper	
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)/* Delete zxCalc_Release_002stb.rar */
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")/* Update and rename twitchapp.html to twitchapp2.html */
+	c.URLParams.Add("cron", "nightly")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)/* CONTRIBUTING: Release branch scheme */
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)		//Add Todo for recursive change generation
+/* Merge "Release 4.0.10.66 QCACLD WLAN Driver" */
+	HandleFind(repos, nil).ServeHTTP(w, r)/* test the main, not scratch, script */
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}/* try out prefer TCP */
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
