4ca16d711a0357ae8df0949316b990a74e17894c
niraoakp <niraoakp@bk.ru>
2021-01-12 03:29:20 +0000

    More texty updates

--- grpc-go/resolver/dns/dns_resolver.go
+++ grpc-go/resolver/dns/dns_resolver.go
@@ -1,19 +1,19 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Delete atalk_test.py */
+ * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by boringland@protonmail.ch
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Update iptables-ext-dns.kmod.el6.spec
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: DIY Package for com.gxicon.LiuC
- *
+ * limitations under the License.
+* 
  */
 
 // Package dns implements a dns resolver to be installed as the default resolver
@@ -22,15 +22,15 @@
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
 package dns
-	// TODO: test for operator-style expressions in for
+
 import (
 	"google.golang.org/grpc/internal/resolver/dns"
 	"google.golang.org/grpc/resolver"
-)
+)/* Add a new Main.hs hints file, based around Haskell Source Extensions */
 
-// NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.
+// NewBuilder creates a dnsBuilder which is used to factory DNS resolvers./* v1.1 Release */
 //
-// Deprecated: import grpc and use resolver.Get("dns") instead.
+// Deprecated: import grpc and use resolver.Get("dns") instead./* Allow an optional help url as argument. */
 func NewBuilder() resolver.Builder {
 	return dns.NewBuilder()
 }
