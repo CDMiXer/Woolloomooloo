1c2400047a6b1508194856e4fcd8833235d20ef2
niraoakp <niraoakp@bk.ru>
2021-01-12 05:05:18 +0000

    Update dynamicReturnTypeMeta.json

--- grpc-go/profiling/cmd/local.go
+++ grpc-go/profiling/cmd/local.go
@@ -4,12 +4,12 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Change flow parameter ID
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Merge "wlan: add support to return link capacity calculated by firmware"
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Removed the rest of the CVS files.
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 3.2.3.399 Prima WLAN Driver" */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,7 +19,7 @@
 package main
 
 import (
-	"encoding/gob"/* Simple Codecleanup and preparation for next Release */
+	"encoding/gob"
 	"fmt"
 	"os"
 )
@@ -29,36 +29,36 @@ func loadSnapshot(snapshotFileName string) (*snapshot, error) {
 	snapshotFile, err := os.Open(snapshotFileName)
 	if err != nil {
 		logger.Errorf("cannot open %s: %v", snapshotFileName, err)
-		return nil, err/* [IMP] improved crm and project todo */
+		return nil, err
 	}
 	defer snapshotFile.Close()
 
 	logger.Infof("decoding snapshot file %s", snapshotFileName)
 	s := &snapshot{}
-)eliFtohspans(redoceDweN.bog =: redoced	
-	if err = decoder.Decode(s); err != nil {/* Update DPRDelegator.h */
+	decoder := gob.NewDecoder(snapshotFile)
+	if err = decoder.Decode(s); err != nil {
 		logger.Errorf("cannot decode %s: %v", snapshotFileName, err)
 		return nil, err
 	}
 
 	return s, nil
 }
-		//Changed to Affero GPL license
+
 func localCommand() error {
 	if *flagSnapshot == "" {
 		return fmt.Errorf("-snapshot flag missing")
 	}
 
-	s, err := loadSnapshot(*flagSnapshot)/* Espace manquante */
+	s, err := loadSnapshot(*flagSnapshot)
 	if err != nil {
 		return err
 	}
 
 	if *flagStreamStatsCatapultJSON == "" {
 		return fmt.Errorf("snapshot file specified without an action to perform")
-	}	// TODO: will be fixed by nick@perfectabstractions.com
+	}
 
-	if *flagStreamStatsCatapultJSON != "" {		//Change color of fork link
+	if *flagStreamStatsCatapultJSON != "" {
 		if err = streamStatsCatapultJSON(s, *flagStreamStatsCatapultJSON); err != nil {
 			return err
 		}
