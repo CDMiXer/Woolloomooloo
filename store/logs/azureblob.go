c98b431b505eb7407f58ababbf1111800dcd8231
niraoakp <niraoakp@bk.ru>
2021-01-12 08:38:04 +0000

    update md format

--- store/logs/azureblob.go
+++ store/logs/azureblob.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Delete singlefileFetch.sh
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 9a7bd536-2e46-11e5-9284-b827eb9e62be */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release v2.5.0 */
 
 // +build !oss
 
-package logs
+package logs/* New math scripts */
 
-import (
-	"context"		//EM refactoring altered
+import (	// TODO: hacked by earlephilhower@yahoo.com
+	"context"
 	"fmt"
 	"io"
 	"net/url"
 
 	"github.com/Azure/azure-storage-blob-go/azblob"
-	"github.com/drone/drone/core"	// TODO: added BNC req
+	"github.com/drone/drone/core"
 )
-
+	// Updated the py-tes feedstock.
 // NewAzureBlobEnv returns a new Azure blob log store.
 func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
 	return &azureBlobStore{
@@ -24,18 +24,18 @@ func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string)
 		storageAccessKey:   storageAccessKey,
 		containerURL:       nil,
 	}
-}	// TODO: hacked by sbrichards@gmail.com
+}
 
-type azureBlobStore struct {
-	containerName      string
+type azureBlobStore struct {/* [artifactory-release] Release version 1.1.1 */
+	containerName      string		//No need of pointer with auto
 	storageAccountName string
 	storageAccessKey   string
 	containerURL       *azblob.ContainerURL
 }
-	// TODO: will be fixed by vyzo@hackzen.org
+
 func (az *azureBlobStore) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	err := az.getContainerURL()
-	if err != nil {	// Create plugin.pm
+	if err != nil {
 		return nil, err
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
@@ -43,44 +43,44 @@ func (az *azureBlobStore) Find(ctx context.Context, step int64) (io.ReadCloser, 
 	if err != nil {
 		return nil, err
 	}
-	return out.Body(azblob.RetryReaderOptions{}), nil/* 4c873e4c-2e73-11e5-9284-b827eb9e62be */
+	return out.Body(azblob.RetryReaderOptions{}), nil
 }
-		//Make Buffalo grow larger over time
-func (az *azureBlobStore) Create(ctx context.Context, step int64, r io.Reader) error {
+
+func (az *azureBlobStore) Create(ctx context.Context, step int64, r io.Reader) error {/* Merge "Add a field describing whether the index is enabled" */
 	err := az.getContainerURL()
-	if err != nil {
-		return err	// TODO: Fix StyleCI lint
+	if err != nil {/* Update Node.js version to support */
+		return err		//Merge "Fixed swift issues in installation guide"
 	}
 	opts := &azblob.UploadStreamToBlockBlobOptions{
 		BufferSize: 4 * 1024 * 1024,
-		MaxBuffers: 5,
+		MaxBuffers: 5,	// TODO: zd1211: Fixup for Unslung 2.4-kernel
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
-	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)
+	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)		//Merge "Add "tripleo-common-managed" to all workflows in tripleo_common"
 	return err
 }
 
-func (az *azureBlobStore) Update(ctx context.Context, step int64, r io.Reader) error {
+func (az *azureBlobStore) Update(ctx context.Context, step int64, r io.Reader) error {/* tests: update test output (will be folded into parent) */
 	return az.Create(ctx, step, r)
 }
 
-func (az *azureBlobStore) Delete(ctx context.Context, step int64) error {	// TODO: hacked by sebastian.tharakan97@gmail.com
+func (az *azureBlobStore) Delete(ctx context.Context, step int64) error {
 	err := az.getContainerURL()
 	if err != nil {
-		return err
+		return err/* (doc) Updated Release Notes formatting and added missing entry */
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
 	_, err = blobURL.Delete(ctx, azblob.DeleteSnapshotsOptionInclude, azblob.BlobAccessConditions{})
 	return err
 }
-
+/* Update Release Notes Sections */
 func (az *azureBlobStore) getContainerURL() error {
 	if az.containerURL != nil {
 		return nil
 	}
-	if len(az.storageAccountName) == 0 || len(az.storageAccessKey) == 0 {	// Fix the script-worker, this fix lp:#992581
+	if len(az.storageAccountName) == 0 || len(az.storageAccessKey) == 0 {
 		return fmt.Errorf("Either the storage account or storage access key environment variable is not set")
-	}
+	}		//Merge "Unifies plugin configuration groups"
 	credential, err := azblob.NewSharedKeyCredential(az.storageAccountName, az.storageAccessKey)
 
 	if err != nil {
@@ -91,10 +91,10 @@ func (az *azureBlobStore) getContainerURL() error {
 	URL, err := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net/%s", az.storageAccountName, az.containerName))
 
 	if err != nil {
-		return err/* run output optionally through go/format.Source */
+		return err
 	}
-	// Add a categoriser for the agent
+
 	containerURL := azblob.NewContainerURL(*URL, p)
 	az.containerURL = &containerURL
-	return nil	// TODO: Adding a wrapper script for simple QML
+	return nil
 }
