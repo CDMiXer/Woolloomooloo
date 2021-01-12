f6d56e413a4c3ebe6cab85e702cf3a16442b0589
niraoakp <niraoakp@bk.ru>
2021-01-12 14:36:04 +0000

    merge update statusline on browser start

--- server/event/event_server.go
+++ server/event/event_server.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"sync"
 
-	log "github.com/sirupsen/logrus"		//219e4828-2ece-11e5-905b-74de2bd44bed
+	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"	// TODO: Replace guard with if statement
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	"github.com/argoproj/argo/server/auth"
@@ -14,28 +14,28 @@ import (
 	"github.com/argoproj/argo/util/instanceid"
 )
 
-type Controller struct {		//point out integration back in sparklyr
+type Controller struct {
 	instanceIDService instanceid.Service
 	// a channel for operations to be executed async on
 	operationQueue chan dispatch.Operation
 	workerCount    int
 }
 
-var _ eventpkg.EventServiceServer = &Controller{}		//Example images
+var _ eventpkg.EventServiceServer = &Controller{}
 
 func NewController(instanceIDService instanceid.Service, operationQueueSize, workerCount int) *Controller {
 	log.WithFields(log.Fields{"workerCount": workerCount, "operationQueueSize": operationQueueSize}).Info("Creating event controller")
-/* Release notes for 1.4.18 */
+
 	return &Controller{
-		instanceIDService: instanceIDService,	// TODO: hacked by ligi@ligi.de
+		instanceIDService: instanceIDService,
 		//  so we can have `operationQueueSize` operations outstanding before we start putting back pressure on the senders
 		operationQueue: make(chan dispatch.Operation, operationQueueSize),
 		workerCount:    workerCount,
-	}	// TODO: will be fixed by mail@bitpshr.net
-}	// TODO: show realtive paths instead of full path
+	}
+}
 
 func (s *Controller) Run(stopCh <-chan struct{}) {
-/* Create f_update_projecttime.php */
+
 	// this `WaitGroup` allows us to wait for all events to dispatch before exiting
 	wg := sync.WaitGroup{}
 
@@ -44,7 +44,7 @@ func (s *Controller) Run(stopCh <-chan struct{}) {
 			defer wg.Done()
 			for operation := range s.operationQueue {
 				operation.Dispatch()
-			}/* Slight cleanup from previous checkin. */
+			}
 		}()
 		wg.Add(1)
 	}
@@ -56,13 +56,13 @@ func (s *Controller) Run(stopCh <-chan struct{}) {
 
 	log.WithFields(log.Fields{"operations": len(s.operationQueue)}).Info("Waiting until all remaining events are processed")
 
-	// no more new events, process the existing events	// TODO: hacked by lexy8russo@outlook.com
+	// no more new events, process the existing events
 	wg.Wait()
-}/* Release 0.13.0 - closes #3 closes #5 */
+}
 
 func (s *Controller) ReceiveEvent(ctx context.Context, req *eventpkg.EventRequest) (*eventpkg.EventResponse, error) {
 
-	options := metav1.ListOptions{}	// TODO: will be fixed by seth@sethvargo.com
+	options := metav1.ListOptions{}
 	s.instanceIDService.With(&options)
 
 	list, err := auth.GetWfClient(ctx).ArgoprojV1alpha1().WorkflowEventBindings(req.Namespace).List(options)
@@ -70,10 +70,10 @@ func (s *Controller) ReceiveEvent(ctx context.Context, req *eventpkg.EventReques
 		return nil, err
 	}
 
-	operation, err := dispatch.NewOperation(ctx, s.instanceIDService, list.Items, req.Namespace, req.Discriminator, req.Payload)	// TODO: will be fixed by witek@enjin.io
+	operation, err := dispatch.NewOperation(ctx, s.instanceIDService, list.Items, req.Namespace, req.Discriminator, req.Payload)
 	if err != nil {
 		return nil, err
-	}/* Cleaner command line args */
+	}
 
 	select {
 	case s.operationQueue <- *operation:
