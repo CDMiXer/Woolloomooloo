2c5121b4fd30d71aef956723dede11b2b4299f38
niraoakp <niraoakp@bk.ru>
2021-01-12 01:43:30 +0000

    Define raw_input() for Python 3

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -1,35 +1,35 @@
-package event
+package event	// TODO: 6100d6c0-2e4c-11e5-9284-b827eb9e62be
 
-import (/* V0.3 Released */
-	"testing"
+import (
+	"testing"/* chore(deps): update dependency js-given to v0.1.6 */
 
-	"github.com/stretchr/testify/assert"/* The function text_to_html() uses ParsedownExtra class since now. */
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
-/* Release Notes for Squid-3.5 */
-	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
+
+	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"/* update limit on current_user_saved_albums */
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"		//Flipping bits added
+	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"	// TODO: will be fixed by sbrichards@gmail.com
+	"github.com/argoproj/argo/server/auth"/* Release: 6.6.3 changelog */
 	"github.com/argoproj/argo/util/instanceid"
 )
 
-func TestController(t *testing.T) {
+func TestController(t *testing.T) {/* add LaTeX files to .gitignore */
 	clientset := fake.NewSimpleClientset()
 	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
 
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
-	assert.NoError(t, err)
+	assert.NoError(t, err)	// TODO: will be fixed by juan@benet.ai
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
 	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
-	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
+	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")/* Release v0.5.8 */
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
 	s.Run(stopCh)
-
+	// TODO: Merge "Add 'adb dpm' subcommand to set profile owner" into lmp-dev
 	assert.Len(t, s.operationQueue, 0, "all events were processed")
-
-}/* Update exception.golden.txt */
+	// TODO: chore(package): add changelog generation script
+}
