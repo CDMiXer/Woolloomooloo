f2ea034021e98ab45d248b5bfc31e6523171bb29
niraoakp <niraoakp@bk.ru>
2021-01-12 15:18:50 +0000

    Delete final_design

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,23 +1,23 @@
 package lp2p
 
 import (
-	"fmt"	// TODO: hacked by alex.gaynor@gmail.com
+	"fmt"
 
-	"github.com/libp2p/go-libp2p"/* e6ad23f8-2e58-11e5-9284-b827eb9e62be */
+	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
 	routing "github.com/libp2p/go-libp2p-core/routing"
-	discovery "github.com/libp2p/go-libp2p-discovery"/* Delete LaiEuler1.py */
+	discovery "github.com/libp2p/go-libp2p-discovery"
 )
 
 func NoRelay() func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
-		opts.Opts = append(opts.Opts, libp2p.DisableRelay())	// TODO: hacked by steven@stebalien.com
-		return/* manually disable conductor's height service. */
-	}	// trying to patch the patch to add bridge_lan config
-}	// TODO: Fix typo: swallow -> shallow
+		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
+		return
+	}
+}
 
-// TODO: should be use baseRouting or can we use higher level router here?	// TODO: hacked by igor@soramitsu.co.jp
+// TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
