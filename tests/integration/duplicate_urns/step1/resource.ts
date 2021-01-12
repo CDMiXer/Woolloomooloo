87e4e66e0dcdcc33616f501a5d1ca0c3051ff634
niraoakp <niraoakp@bk.ru>
2021-01-12 02:15:26 +0000

    add flake check before coverage

--- tests/integration/duplicate_urns/step1/resource.ts
+++ tests/integration/duplicate_urns/step1/resource.ts
@@ -1,31 +1,31 @@
-// Copyright 2016-2018, Pulumi Corporation.	// fix homepage in pubspec.yaml
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Released 0.1.5 version */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Falla modificacion, Falta parte del gestionar item
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//[ci skip] Separate file folders function out into find and compile
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-/* Update travis build to fix deploy error. */
+
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-	// update readme, not completed yet
+
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {		//central reparent orphan
+        return {
             inputs: news,
         };
     }
-/* Update new-blog-by-github-pages-jekyll-theme.md */
+
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
             return {
@@ -34,13 +34,13 @@ export class Provider implements dynamic.ResourceProvider {
             };
         }
 
-        return {/* Initialize batons to NULL to make debugging easier. */
+        return {
             changes: false,
         };
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {		//Make sure columns never have null values
+        return {
             id: (this.id++).toString(),
             outs: inputs,
         };
@@ -53,10 +53,10 @@ export class Resource extends pulumi.dynamic.Resource {
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }		//Added author to post
+    }
 }
 
 export interface ResourceProps {
     readonly uniqueKey?: pulumi.Input<number>;
     readonly state: pulumi.Input<number>;
-}/* Merge branch 'master' into add_appveyor_button */
+}
