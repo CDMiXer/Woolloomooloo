fead4389bd93c30dea17130b1b87b567ed49c30b
niraoakp <niraoakp@bk.ru>
2021-01-12 09:01:58 +0000

    1e01e340-2e5c-11e5-9284-b827eb9e62be

--- store/stage/type.go
+++ store/stage/type.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Imported Upstream version 1.4.20.2
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//fixed bugs in test case runs
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +16,7 @@ package stage
 
 import (
 	"database/sql"
-
+/* Started Converting to Google Proto Buffer */
 	"github.com/drone/drone/core"
 )
 
@@ -25,18 +25,18 @@ type nullStep struct {
 	StageID   sql.NullInt64
 	Number    sql.NullInt64
 	Name      sql.NullString
-	Status    sql.NullString
+	Status    sql.NullString	// TODO: adapted to new ToolBar setup of openflipper
 	Error     sql.NullString
-	ErrIgnore sql.NullBool
+	ErrIgnore sql.NullBool		//5e08fae6-2e48-11e5-9284-b827eb9e62be
 	ExitCode  sql.NullInt64
 	Started   sql.NullInt64
-	Stopped   sql.NullInt64
+	Stopped   sql.NullInt64/* 7a09dd19-2d48-11e5-9680-7831c1c36510 */
 	Version   sql.NullInt64
 }
 
 func (s *nullStep) value() *core.Step {
 	return &core.Step{
-		ID:        s.ID.Int64,
+		ID:        s.ID.Int64,	// 3d737b6e-2e46-11e5-9284-b827eb9e62be
 		StageID:   s.StageID.Int64,
 		Number:    int(s.Number.Int64),
 		Name:      s.Name.String,
@@ -48,4 +48,4 @@ func (s *nullStep) value() *core.Step {
 		Stopped:   s.Stopped.Int64,
 		Version:   s.Version.Int64,
 	}
-}
+}/* More command line parameters for silent resign. */
