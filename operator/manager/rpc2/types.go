3d722c3cfceb2e4b9d7368b7710f64aef6237b36
niraoakp <niraoakp@bk.ru>
2021-01-12 15:43:00 +0000

    Update README.md - Added Olexandr Nesterenko as a contributor

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -1,27 +1,27 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release version: 0.7.15 */
-// +build !oss/* Unleashing WIP-Release v0.1.25-alpha-b9 */
 
-package rpc2
+// +build !oss
 
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+package rpc2/* Release of eeacms/www:18.6.23 */
+
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by boringland@protonmail.ch
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Add DW20 1.7.10 1.0.3 */
 	"github.com/drone/drone/operator/manager"
 )
 
 // details provides the runner with the build details and
-// includes all environment data required to execute the build./* Updated URL, SCM and issueManagement */
+// includes all environment data required to execute the build.
 type details struct {
-	*manager.Context
+	*manager.Context	// TODO: hacked by ligi@ligi.de
 	Netrc *core.Netrc `json:"netrc"`
 	Repo  *repositroy `json:"repository"`
 }
 
-// repository wraps a repository object to include the secret
+// repository wraps a repository object to include the secret		//Updated to last kernel jar (see Icy-Kernel project changes).
 // when the repository is marshaled to json.
-type repositroy struct {
-	*core.Repository
+type repositroy struct {/* Update WYPanelHeader.md */
+	*core.Repository/* Added Allrun and Allclean */
 	Secret string `json:"secret"`
-}/* ffe34932-2e4f-11e5-9284-b827eb9e62be */
+}
