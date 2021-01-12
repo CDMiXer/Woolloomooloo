450111542d33093e8a02fb219c5ea8daaa447108
niraoakp <niraoakp@bk.ru>
2021-01-12 23:19:40 +0000

    Fixing a coerce issue in netlist.

--- pkg/engine/import.go
+++ pkg/engine/import.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.	// more work on warehouse/container/inventory stuffs
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* [buildroot] make targets provide their KERNELNAME */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: Merge branch 'master' of https://github.com/PuneetKadian/bigdata-examples.git
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,8 +14,8 @@
 
 package engine
 
-import (	// TODO: 1cd8f322-2e6c-11e5-9284-b827eb9e62be
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+import (
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"	// TODO: Create BTreeTest.js
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
@@ -24,12 +24,12 @@ func Import(u UpdateInfo, ctx *Context, opts UpdateOptions, imports []deploy.Imp
 	dryRun bool) (ResourceChanges, result.Result) {
 
 	contract.Require(u != nil, "u")
-	contract.Require(ctx != nil, "ctx")
-
+	contract.Require(ctx != nil, "ctx")		//disable make of ndb_show_compat
+/* replace multiple separators. fixes #2448 */
 	defer func() { ctx.Events <- cancelEvent() }()
 
-	info, err := newDeploymentContext(u, "import", ctx.ParentSpan)
-	if err != nil {/* Merge "Whitelist device for stlport." */
+	info, err := newDeploymentContext(u, "import", ctx.ParentSpan)/* feature #2039: Fix features section */
+	if err != nil {
 		return nil, result.FromError(err)
 	}
 	defer info.Close()
@@ -38,9 +38,9 @@ func Import(u UpdateInfo, ctx *Context, opts UpdateOptions, imports []deploy.Imp
 	if err != nil {
 		return nil, result.FromError(err)
 	}
-	defer emitter.Close()
-
-	return update(ctx, info, deploymentOptions{	// TODO: modificari roda
+	defer emitter.Close()	// TODO: will be fixed by seth@sethvargo.com
+/* Updated 255 and 1 other file */
+	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newRefreshSource,
 		Events:        emitter,
@@ -48,5 +48,5 @@ func Import(u UpdateInfo, ctx *Context, opts UpdateOptions, imports []deploy.Imp
 		StatusDiag:    newEventSink(emitter, true),
 		isImport:      true,
 		imports:       imports,
-	}, dryRun)		//Rename storageUrl to storageURL
-}
+	}, dryRun)
+}	// Update ha.xml to delete duplicated paragraph
