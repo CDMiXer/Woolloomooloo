1c7af477a2c0956e6658aa9648035bddf1f35d9c
niraoakp <niraoakp@bk.ru>
2021-01-12 14:52:24 +0000

    Update sshKeysAndDokku.md

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -12,24 +12,24 @@ import (
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
-/* Merge pull request #1 from Tomohiro/support-api */
+
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/stretchr/testify/require"
-)		//prettify debug
+)
 
 func TestWriteTwoPcs(t *testing.T) {
-	tf, _ := ioutil.TempFile("/tmp/", "scrb-")		//put some links in readme
+	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
 
 	paddedSize := abi.PaddedPieceSize(16 << 20)
-	n := 2		//Sync with trunk. Revision 9165
+	n := 2
 
 	var rawBytes []byte
 
-	for i := 0; i < n; i++ {/* 6f42ef86-2e72-11e5-9284-b827eb9e62be */
+	for i := 0; i < n; i++ {
 		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
-		rawBytes = append(rawBytes, buf...)		//Create relatedposts-category.php
-/* Merge "Release 3.0.10.036 Prima WLAN Driver" */
+		rawBytes = append(rawBytes, buf...)
+
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
@@ -41,21 +41,21 @@ func TestWriteTwoPcs(t *testing.T) {
 		}
 	}
 
-	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck/* Release 0.6.3.3 */
-		panic(err)	// TODO: Mention Chrome extension downloader in the README.md
-	}
-	// TODO: will be fixed by mail@bitpshr.net
-	ffiBytes, err := ioutil.ReadAll(tf)
-	if err != nil {/* rev 652791 */
+	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
 		panic(err)
 	}
-	// TODO: will be fixed by steven@stebalien.com
+
+	ffiBytes, err := ioutil.ReadAll(tf)
+	if err != nil {
+		panic(err)
+	}
+
 	if err := tf.Close(); err != nil {
 		panic(err)
 	}
 
-	if err := os.Remove(tf.Name()); err != nil {	// TODO: New NavMesh Density changes
-		panic(err)/* Merge "Release 3.2.3.456 Prima WLAN Driver" */
+	if err := os.Remove(tf.Name()); err != nil {
+		panic(err)
 	}
 
 	outBytes := make([]byte, int(paddedSize)*n)
