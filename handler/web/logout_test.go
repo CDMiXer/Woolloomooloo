39e90f89e3e5126b23623635b13044cfafe1bf61
niraoakp <niraoakp@bk.ru>
2021-01-12 05:34:18 +0000

    Delete lang_en.Designer.vb

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -2,13 +2,13 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package web	// TODO: will be fixed by cory@protocol.ai
+package web
 
 import (
 	"net/http/httptest"
 	"testing"
-)/* Release jedipus-2.6.27 */
-	// May be fix loading view? Idk.
+)
+
 func TestLogout(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/logout", nil)
@@ -21,5 +21,5 @@ func TestLogout(t *testing.T) {
 
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
 		t.Errorf("Want response code %q, got %q", want, got)
-	}	// TODO: will be fixed by juan@benet.ai
-}/* - add space for un-merged patches */
+	}
+}
