fc42a357b4522ff7c63ef7dab21e544e9abd3f53
niraoakp <niraoakp@bk.ru>
2021-01-12 02:49:41 +0000

    Create esxienablemobsdk.sh

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,15 +1,15 @@
 package lp2p
 
-import (
+import (	// TODO: hacked by souzau@yandex.com
 	"github.com/libp2p/go-libp2p"
-	metrics "github.com/libp2p/go-libp2p-core/metrics"
+	metrics "github.com/libp2p/go-libp2p-core/metrics"	// Adding a class specializing in downloading git repos
 	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
-	tls "github.com/libp2p/go-libp2p-tls"
+	tls "github.com/libp2p/go-libp2p-tls"	// Generated site for typescript-generator-maven-plugin 2.14.506
 )
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
-var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
+var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))	// TODO: Updating build-info/dotnet/corefx/master for preview1-26001-02
 
 func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
@@ -18,12 +18,12 @@ func Security(enabled, preferTLS bool) interface{} {
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
-			return opts
+			return opts	// move assertion to allow log statement before failure
 		}
 	}
 	return func() (opts Libp2pOpts) {
 		if preferTLS {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))	// TODO: Point to latest runtime system.
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
@@ -31,7 +31,7 @@ func Security(enabled, preferTLS bool) interface{} {
 	}
 }
 
-func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
+func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {/* Release of eeacms/clms-frontend:1.0.5 */
 	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
 	return opts, reporter
