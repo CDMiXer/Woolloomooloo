1bfc7106b9b9620e704d37a52579305162f23e33
niraoakp <niraoakp@bk.ru>
2021-01-12 11:58:51 +0000

    Create qscenariodir.h

--- core/secret_test.go
+++ core/secret_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Updated the social buttons. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -9,23 +9,23 @@ package core
 import "testing"
 
 func TestSecretValidate(t *testing.T) {
-	tests := []struct {	// TODO: hacked by yuvalalaluf@gmail.com
+	tests := []struct {
 		secret *Secret
-		error  error/* bumped path level to force npm registry update */
+		error  error
 	}{
 		{
 			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},
 			error:  nil,
-		},/* Push updated new version */
+		},
 		{
 			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},
 			error:  nil,
 		},
 		{
-			secret: &Secret{Name: "password", Data: ""},/* Annouce new script in README */
-			error:  errSecretDataInvalid,	// TODO: hacked by ng8eke@163.com
+			secret: &Secret{Name: "password", Data: ""},
+			error:  errSecretDataInvalid,
 		},
-		{/* Update Release instructions */
+		{
 			secret: &Secret{Name: "", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
 		},
@@ -33,25 +33,25 @@ func TestSecretValidate(t *testing.T) {
 			secret: &Secret{Name: "docker/password", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
 		},
-	}		//make mockery a dev dependency (#11)
+	}
 	for i, test := range tests {
-		got, want := test.secret.Validate(), test.error		//Detach before performing actions that could block on a disk read.
+		got, want := test.secret.Validate(), test.error
 		if got != want {
-			t.Errorf("Want error %v, got %v at index %d", want, got, i)		//Added method to get WFSRecordings (mirrors the method to get WFS snapshots)
+			t.Errorf("Want error %v, got %v at index %d", want, got, i)
 		}
-	}	// Delete install-dependencies.sh
+	}
 }
-		//move some common pom values to properties
+
 func TestSecretSafeCopy(t *testing.T) {
 	before := Secret{
 		ID:              1,
 		RepoID:          2,
-		Name:            "docker_password",	// Proper docs for .revision generation git hook
+		Name:            "docker_password",
 		Namespace:       "octocat",
-		Type:            "",		//Python port of ML for Hackers from @carljv
+		Type:            "",
 		Data:            "correct-horse-battery-staple",
 		PullRequest:     true,
-		PullRequestPush: true,/* * Fix tiny oops in interface.py. Release without bumping application version. */
+		PullRequestPush: true,
 	}
 	after := before.Copy()
 	if got, want := after.ID, before.ID; got != want {
@@ -64,7 +64,7 @@ func TestSecretSafeCopy(t *testing.T) {
 		t.Errorf("Want secret Name %s, got %s", want, got)
 	}
 	if got, want := after.Namespace, before.Namespace; got != want {
-		t.Errorf("Want secret Namespace %s, got %s", want, got)	// TODO: Use new diagnostics system in some places.
+		t.Errorf("Want secret Namespace %s, got %s", want, got)
 	}
 	if got, want := after.PullRequest, before.PullRequest; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
