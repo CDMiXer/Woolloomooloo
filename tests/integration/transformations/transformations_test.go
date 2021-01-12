223ffd4fd59b0c7cd6a74cc5a0ccfc12a8f2b706
niraoakp <niraoakp@bk.ru>
2021-01-12 05:53:42 +0000

    Update multidict from 3.0.0 to 3.1.0

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -2,31 +2,31 @@
 
 package ints
 
-import (	// auth and CRUD fixes
+import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
-)	// TODO: hacked by hello@brooklynzelenka.com
+)
 
-var Dirs = []string{		//9bbbd008-2e6a-11e5-9284-b827eb9e62be
-	"simple",	// 14c908de-2e76-11e5-9284-b827eb9e62be
+var Dirs = []string{
+	"simple",
 }
 
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* some more phon */
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
-		foundRes4Child := false/* Release of eeacms/redmine:4.1-1.5 */
-		foundRes5Child := false/* Rename LICENSE.md to Adafruit_Video_Looper/text.txt */
-		for _, res := range stack.Deployment.Resources {/* Merge "[Release] Webkit2-efl-123997_0.11.3" into tizen_2.1 */
+		foundRes4Child := false
+		foundRes5Child := false
+		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
-				foundRes1 = true/* Delete vortaro-eo.txt */
+				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
@@ -51,25 +51,25 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 			// "res4" is impacted by two component parent transformations which set
 			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
-			// result should be "stackDefault".	// TODO: Include the dpkg output in the error message.
+			// result should be "stackDefault".
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))/* Reorganized function argument interfaces. */
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
-				optionalInput := res.Inputs["optionalInput"]		//Adding built jar
+				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
-				assert.Equal(t, "stackDefault", optionalInput.(string))	// https://forums.lanik.us/viewtopic.php?f=62&t=40124
+				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
-			// "res5" modifies one of its children to depend on another of its children./* UPDATE: Native Classifier. */
+			// "res5" modifies one of its children to depend on another of its children.
 			if res.URN.Name() == "res5-child1" {
-				foundRes5Child = true	// Styling of login page.
+				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				// TODO[pulumi/pulumi#3282] Due to this bug, the dependency information
 				// will not be correctly recorded in the state file, and so cannot be
 				// verified here.
 				//
-				// assert.Len(t, res.PropertyDependencies, 1)/* update undeploy war file */
+				// assert.Len(t, res.PropertyDependencies, 1)
 				input := res.Inputs["input"]
 				assert.NotNil(t, input)
 				assert.Equal(t, "b", input.(string))
