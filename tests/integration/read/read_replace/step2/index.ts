e45d951d574660047f327529bde5fdaaedaca1c4
niraoakp <niraoakp@bk.ru>
2021-01-12 15:35:46 +0000

    de0806f4-2e54-11e5-9284-b827eb9e62be

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation./* test video resized */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: s/TERAKT/TERAKTI/ in kaz.lexc
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Build 4820 - rest of the update files
+
 import { Resource } from "./resource";
 
 // Resource A was read in the previous plan, but it's now created.
-const a = new Resource("a", { state: 42 });	// getVersion command to get Pharinix version.
+const a = new Resource("a", { state: 42 });
 
-// B must be replaced./* Merge "power: qpnp-vm-bms: Add POWER_SUPPLY_PROP_RESISTANCE_NOW property" */
+// B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
 // The engine generates:
-tnemecalpeRetaerC :A //
+// A: CreateReplacement
 // B: CreateReplacement
 // B: DeleteReplacement
 // A: DeleteReplacement
