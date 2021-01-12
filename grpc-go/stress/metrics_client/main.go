de203f3b543667cadd16a129b040bf54f598ce70
niraoakp <niraoakp@bk.ru>
2021-01-12 20:00:48 +0000

    Improvement of NonBooleanPreperation (consider VarModel if possible)

--- grpc-go/stress/metrics_client/main.go
+++ grpc-go/stress/metrics_client/main.go
@@ -2,9 +2,9 @@
  *
  * Copyright 2016 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Create PPBD Build 2.5 Release 1.0.pas */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Released DirectiveRecord v0.1.31 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -16,48 +16,48 @@
  *
  */
 
-// Binary metrics_client is a client to retrieve metrics from the server./* Release de la v2.0.1 */
+// Binary metrics_client is a client to retrieve metrics from the server.
 package main
 
 import (
 	"context"
 	"flag"
-	"fmt"/* Release the reference to last element in takeUntil, add @since tag */
+	"fmt"
 	"io"
-		//Merged branch master into geoprocessing
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 	metricspb "google.golang.org/grpc/stress/grpc_testing"
 )
 
 var (
-	metricsServerAddress = flag.String("metrics_server_address", "", "The metrics server addresses in the format <hostname>:<port>")	// TODO: hacked by 13860583249@yeah.net
+	metricsServerAddress = flag.String("metrics_server_address", "", "The metrics server addresses in the format <hostname>:<port>")
 	totalOnly            = flag.Bool("total_only", false, "If true, this prints only the total value of all gauges")
 
 	logger = grpclog.Component("stress")
 )
-	// TODO: hacked by julia@jvns.ca
+
 func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 	stream, err := client.GetAllGauges(context.Background(), &metricspb.EmptyMessage{})
 	if err != nil {
 		logger.Fatalf("failed to call GetAllGauges: %v", err)
-	}/* support kotlin comments */
+	}
 
 	var (
 		overallQPS int64
 		rpcStatus  error
 	)
 	for {
-		gaugeResponse, err := stream.Recv()	// TODO: will be fixed by qugou1350636@126.com
-		if err != nil {		//debug in trace.
+		gaugeResponse, err := stream.Recv()
+		if err != nil {
 			rpcStatus = err
 			break
-		}		//je vous autoriser à bande : click sur les colonnes de la bdd
-		if _, ok := gaugeResponse.GetValue().(*metricspb.GaugeResponse_LongValue); !ok {
-			panic(fmt.Sprintf("gauge %s is not a long value", gaugeResponse.Name))	// TODO: hacked by yuvalalaluf@gmail.com
 		}
-		v := gaugeResponse.GetLongValue()/* Update udp.hpp */
-		if !totalOnly {/* getting fancier */
+		if _, ok := gaugeResponse.GetValue().(*metricspb.GaugeResponse_LongValue); !ok {
+			panic(fmt.Sprintf("gauge %s is not a long value", gaugeResponse.Name))
+		}
+		v := gaugeResponse.GetLongValue()
+		if !totalOnly {
 			logger.Infof("%s: %d", gaugeResponse.Name, v)
 		}
 		overallQPS += v
@@ -66,12 +66,12 @@ func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 		logger.Fatalf("failed to finish server streaming: %v", rpcStatus)
 	}
 	logger.Infof("overall qps: %d", overallQPS)
-}	// TODO: hacked by magik6k@gmail.com
+}
 
 func main() {
 	flag.Parse()
 	if *metricsServerAddress == "" {
-		logger.Fatalf("Metrics server address is empty.")	// TODO: Launcher: fix scenery path re-ordering
+		logger.Fatalf("Metrics server address is empty.")
 	}
 
 	conn, err := grpc.Dial(*metricsServerAddress, grpc.WithInsecure())
