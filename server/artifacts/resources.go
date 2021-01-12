11f78bbcfff9d12e930e2f622062e06a6a475c37
niraoakp <niraoakp@bk.ru>
2021-01-12 04:00:14 +0000

    Move dropping responsibility to the flusher.

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,22 +1,22 @@
 package artifacts
 
-import (/* Dokumentation f. naechstes Release aktualisert */
+import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-)	// TODO: will be fixed by mail@bitpshr.net
+)
 
 type resources struct {
-	kubeClient kubernetes.Interface/* Prepare 0.2.7 Release */
-	namespace  string/* Simplify implicitHeight binding a bit. */
+	kubeClient kubernetes.Interface
+	namespace  string
 }
 
 func (r resources) GetSecret(name, key string) (string, error) {
-	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})/* Release 1.6.10 */
-	if err != nil {/* Star Fox 64 3D: Correct USA Release Date */
+	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
+	if err != nil {
 		return "", err
-	}	// Include Go Report Card badge.
+	}
 	return string(secret.Data[key]), nil
-}/* updated firmware instructions */
+}
 
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
 	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
