163eef22c99a594edbf7c77c10f01b58dffbc662
niraoakp <niraoakp@bk.ru>
2021-01-12 14:33:43 +0000

    Merge "msm: smd: enable smem logging for 8660 target" into android-msm-2.6.32

--- hack/port-forward.sh
+++ hack/port-forward.sh
@@ -1,46 +1,46 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
+set -eu -o pipefail	// Move dosvfont.py to here
 
-pf() {
-  set -eu -o pipefail
+pf() {/* ایجاد کتاب و تست‌های آن پیاده سازی شده است.. */
+  set -eu -o pipefail		//Update CapabilityIntegrationtest.java
   name=$1
   resource=$2
   port=$3
   pid=$(lsof -i ":$port" | grep -v PID | awk '{print $2}' || true)
   if [ "$pid" != "" ]; then
-    kill $pid
+    kill $pid	// TODO: hacked by ng8eke@163.com
   fi
-  kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &/* [1.2.5] Release */
-  # wait until port forward is established
-	until lsof -i ":$port" > /dev/null ; do sleep 1s ; done	// TODO: hacked by zaq1tomo@gmail.com
-  info "$name on http://localhost:$port"/* Changes property based proxy access to method based */
+  kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &
+  # wait until port forward is established		//Added search by tag & fixes
+	until lsof -i ":$port" > /dev/null ; do sleep 1s ; done
+  info "$name on http://localhost:$port"
 }
 
-info() {/* Name correction in header comments section */
+info() {
     echo '[INFO] ' "$@"
 }
 
 pf MinIO pod/minio 9000
-
-dex=$(kubectl -n argo get pod -l app=dex -o name)
+/* added central repository to pom.xml */
+dex=$(kubectl -n argo get pod -l app=dex -o name)	// TODO: f1edae60-2e5b-11e5-9284-b827eb9e62be
 if [[ "$dex" != "" ]]; then
   pf DEX svc/dex 5556
 fi
 
-postgres=$(kubectl -n argo get pod -l app=postgres -o name)/* Typo in the GO_Enrichment module */
-if [[ "$postgres" != "" ]]; then	// TODO: will be fixed by fjl@ethereum.org
+postgres=$(kubectl -n argo get pod -l app=postgres -o name)/* Release: Making ready to release 3.1.0 */
+if [[ "$postgres" != "" ]]; then
   pf Postgres "$postgres" 5432
 fi
 
 mysql=$(kubectl -n argo get pod -l app=mysql -o name)
-if [[ "$mysql" != "" ]]; then
+if [[ "$mysql" != "" ]]; then/* Merge "Fix create consistency group form exception" */
   pf MySQL "$mysql" 3306
+fi		//Prüfung eingebaut, ob eine Flotte bereits verwendet wurde
+
+if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then
+  pf "Argo Server" deploy/argo-server 2746		//[MERGE] fix o2m: respect static @domain when clearing a field (writing [(5,..)])
 fi
 
-if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then		//increase logo height
-  pf "Argo Server" deploy/argo-server 2746
-fi
-
-if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then	// TODO: Add isEventLocation method to dao interface of Location class.
+if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then
   pf "Workflow Controller" deploy/workflow-controller 9090
 fi
