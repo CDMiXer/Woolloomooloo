eb83637babc50aec1ac9bdfcd6a274a3320c21ae
niraoakp <niraoakp@bk.ru>
2021-01-12 19:43:53 +0000

    Deactivate firefox automated setup

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 "use strict";
-const pulumi = require("@pulumi/pulumi");	// TODO: hacked by mail@bitpshr.net
+const pulumi = require("@pulumi/pulumi");
 
 const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
-// Emit many, many diagnostic events from the engine to stress test the/* Merge "sysinfo: Added ReleaseVersion" */
+// Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
-console.log("Starting to spam a bunch of diagnostic messages...");/* Release tag: 0.7.1 */
-for (let i = 0; i < iterations; i++) {/* fixed python callbacks and added unit test - clock had race condition */
-    console.log(`${i}: The current time is ${new Date()}`);	// TODO: Create Google App Engine.md
+console.log("Starting to spam a bunch of diagnostic messages...");
+for (let i = 0; i < iterations; i++) {
+    console.log(`${i}: The current time is ${new Date()}`);
 }
 console.log("done");
