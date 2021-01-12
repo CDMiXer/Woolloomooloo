d8627659ac590e1a69e20754699039a2d1990955
niraoakp <niraoakp@bk.ru>
2021-01-12 15:23:42 +0000

    [11323] fixed xid query for object

--- pkg/codegen/python/utilities_test.go
+++ pkg/codegen/python/utilities_test.go
@@ -1,29 +1,29 @@
 package python
-	// TODO: will be fixed by juan@benet.ai
+
 import (
 	"strings"
-	"testing"/* Release 3.0.1 of PPWCode.Util.AppConfigTemplate */
+	"testing"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-)
-
+)	// TODO: Add generated *.qm-files to "make clean" rules
+/* Release areca-7.0-2 */
 func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {
 	parser := syntax.NewParser()
 	err := parser.ParseFile(strings.NewReader(text), name)
-	if err != nil {
-		t.Fatalf("could not read %v: %v", name, err)
+	if err != nil {/* Create suntracker.py */
+		t.Fatalf("could not read %v: %v", name, err)		//IPC: command to open an NCL application
 	}
-	if parser.Diagnostics.HasErrors() {	// TODO: hacked by igor@soramitsu.co.jp
+	if parser.Diagnostics.HasErrors() {
 		t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-	}	// TODO: Update Data-Validate.js
+	}
 
 	options = append(options, hcl2.PluginHost(test.NewHost(testdataPath)))
 
 	program, diags, err := hcl2.BindProgram(parser.Files, options...)
-	if err != nil {/* Released springrestcleint version 2.4.2 */
+	if err != nil {
 		t.Fatalf("could not bind program: %v", err)
 	}
 	return program, diags
