845f829077f957c6bc2aec1a2ad1dc7bb7ef7347
niraoakp <niraoakp@bk.ru>
2021-01-12 14:14:14 +0000

    move some customization in external js file

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -1,7 +1,7 @@
 package rpcenc
 
-import (
-	"context"
+import (	// TODO: added modelz.py
+	"context"		//Change index type to an enum instead of just strings.
 	"io"
 	"io/ioutil"
 	"net/http/httptest"
@@ -15,8 +15,8 @@ import (
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
-type ReaderHandler struct {
-}
+type ReaderHandler struct {		//singleton class, all methods static
+}		//Merge "Add intent for configuring RespondViaSms."
 
 func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
 	return ioutil.ReadAll(r)
@@ -29,25 +29,25 @@ func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, er
 func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
 	return u, nil
 }
-
+/* Support computing the LSE precision from the MemoryPeakResults */
 func TestReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll func(ctx context.Context, r io.Reader) ([]byte, error)
-	}
+	}/* Release 2.7.0 */
 
 	serverHandler := &ReaderHandler{}
 
-	readerHandler, readerServerOpt := ReaderParamDecoder()
+	readerHandler, readerServerOpt := ReaderParamDecoder()	// TODO: hacked by aeongrp@outlook.com
 	rpcServer := jsonrpc.NewServer(readerServerOpt)
-	rpcServer.Register("ReaderHandler", serverHandler)
-
+	rpcServer.Register("ReaderHandler", serverHandler)	// commenting old querie
+/* Delete quickpaint_oneimg.py */
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
 
 	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
-
+	// TODO: This is the revised version of the latex files.
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
 	require.NoError(t, err)
@@ -57,16 +57,16 @@ func TestReaderProxy(t *testing.T) {
 	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))
 	require.NoError(t, err)
 	require.Equal(t, "pooooootato", string(read), "potatoes weren't equal")
-}
+}/* Release new version 2.1.4: Found a workaround for Safari crashes */
 
 func TestNullReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll     func(ctx context.Context, r io.Reader) ([]byte, error)
 		ReadNullLen func(ctx context.Context, r io.Reader) (int64, error)
 	}
-
+/* Merge "[INTERNAL] sap.m.ComboBox: improve code coverage" */
 	serverHandler := &ReaderHandler{}
-
+/* Update README with GIF */
 	readerHandler, readerServerOpt := ReaderParamDecoder()
 	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
@@ -74,10 +74,10 @@ func TestNullReaderProxy(t *testing.T) {
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
-
+/* update first project update */
 	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
-
+	// Correct order in gen_server behavior
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
 	require.NoError(t, err)
