1e962cbf291e417f3a1b4d011eb8cb09084d4fba
niraoakp <niraoakp@bk.ru>
2021-01-12 21:15:09 +0000

    Improve accuracy of Observable example in README

--- grpc-go/testdata/testdata.go
+++ grpc-go/testdata/testdata.go
@@ -1,22 +1,22 @@
 /*
  * Copyright 2017 gRPC authors.
- *
+ *		//logplex_tail:register now happens in the tail_buffer.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Merge "Small structural fixes to 6.0 Release Notes" */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Add retry configuration to vault
+ *		//activity editors - traffic/timetable
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: resourceId and other fixes
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Release of eeacms/jenkins-slave:3.18 */
  */
 
 package testdata
-
+/* Release v4.0 */
 import (
 	"path/filepath"
 	"runtime"
@@ -24,18 +24,18 @@ import (
 
 // basepath is the root directory of this package.
 var basepath string
-
+	// TODO: Remove references to the client authentication.
 func init() {
-	_, currentFile, _, _ := runtime.Caller(0)
+	_, currentFile, _, _ := runtime.Caller(0)/* Release for 23.4.0 */
 	basepath = filepath.Dir(currentFile)
-}
+}	// TODO: hacked by mikeal.rogers@gmail.com
 
 // Path returns the absolute path the given relative file or directory path,
 // relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
 // If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
-		return rel
+		return rel	// TODO: make request function public
 	}
 
 	return filepath.Join(basepath, rel)
