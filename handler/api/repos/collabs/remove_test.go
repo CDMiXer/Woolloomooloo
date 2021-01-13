40261f7f83b5bf1ab57cef7b4e8854b2b3a1abd7
niraoakp <niraoakp@bk.ru>
2021-01-13 07:30:39 +0000

    Use go syntax highlighter in README example

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -3,58 +3,58 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Rename jqurey.js to js/ jqurey.js */
-package collabs
 
+package collabs
+		//Update READMD.md - direct download links!
 import (
-	"context"
-	"encoding/json"	// TODO: Allow BUILDKITE variables into Docker container for the benefit of logging
+	"context"		//New translations en-GB.mod_latestsermons.sys.ini (Finnish)
+	"encoding/json"/* Release 1.11.0. */
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* Add missing StgPrimCallOp case to isSimpleOp */
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: hacked by hugomrdias@gmail.com
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Release v1.8.1 */
+	"github.com/google/go-cmp/cmp"
 )
-		//41285f56-2e76-11e5-9284-b827eb9e62be
-func TestDelete(t *testing.T) {	// Adding specs
-	controller := gomock.NewController(t)		//Travis: Removed Node 0.9, added 0.11
-	defer controller.Finish()/* Update to sensitivity output for NBN download format. */
-/* Create B827EBFFFE72E7EE.JSON */
-	users := mock.NewMockUserStore(controller)
-	repos := mock.NewMockRepositoryStore(controller)
+
+func TestDelete(t *testing.T) {/* Merge "Correct improper assert judgement" */
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	users := mock.NewMockUserStore(controller)/* Code Cleanup and add Windows x64 target (Debug and Release). */
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: Update nights_watch.py
 	members := mock.NewMockPermStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)	// TODO: hacked by ac0dem0nk3y@gmail.com
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)/* UserPassword: must be serializable */
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
-	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)	// TODO: will be fixed by nicksavers@gmail.com
+	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
 
-	c := new(chi.Context)	// c0aa7c9a-2e40-11e5-9284-b827eb9e62be
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Forgot to change cabal file, naturally */
-	c.URLParams.Add("member", "octocat")
-	// TODO: will be fixed by nagydani@epointsystem.org
+	c.URLParams.Add("name", "hello-world")	// TODO: will be fixed by vyzo@hackzen.org
+	c.URLParams.Add("member", "octocat")	// TODO: hacked by xiemengjun@gmail.com
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/", nil)
+	r := httptest.NewRequest("DELETE", "/", nil)		//Automatic changelog generation for PR #9989 [ci skip]
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)	// Fixing avatar urls
-	// TODO: Add nl.ts to the .pro file.
+	)
+
 	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: Merge "Stop setting nova.conf settings that mean nothing"
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
 
 func TestDelete_UserNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)/* Fix for net::ERR CONTENT LENGTH MISMATCH */
+	defer controller.Finish()/* Updated README to use closure in subscription */
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)	// TODO: hacked by ng8eke@163.com
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
@@ -65,7 +65,7 @@ func TestDelete_UserNotFound(t *testing.T) {
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// TODO: Fixed BiScoreboard
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
