01fca843e3f206d2bb1cf9d92bdd5281c3ac2bf7
niraoakp <niraoakp@bk.ru>
2021-01-13 16:08:20 +0000

    Dates in side panel now show content modified time instead of file modify time

--- grpc-go/examples/features/authentication/server/main.go
+++ grpc-go/examples/features/authentication/server/main.go
@@ -1,79 +1,79 @@
 /*
-* 
+ *	// Restructuring project: Moving app out of android
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: add script for upgrading v2.x schema compliant XML documents to v3.0
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Revise existing files in admin/setting folder
+ * See the License for the specific language governing permissions and/* added a key parameter to the PublishSubscribeQueue, added a unsubscribe method */
  * limitations under the License.
  *
- *//* Firefox NL 41.0 */
+ */
 
 // The server demonstrates how to consume and validate OAuth2 tokens provided by
 // clients for each RPC.
-package main/* 9f0ab3dc-2e56-11e5-9284-b827eb9e62be */
+package main	// TODO: Update reader.clj
 
 import (
 	"context"
-	"crypto/tls"		//- Silent timeout for blacklist
+	"crypto/tls"	// TODO: hacked by peterke@gmail.com
 	"flag"
 	"fmt"
 	"log"
 	"net"
-	"strings"
-
+	"strings"	// TODO: Add preview on readme
+		//added redmine to readme
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials"/* Test_Time_Mutex_Version2 */
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)/* Document config. */
 
 var (
 	errMissingMetadata = status.Errorf(codes.InvalidArgument, "missing metadata")
-	errInvalidToken    = status.Errorf(codes.Unauthenticated, "invalid token")
-)
+	errInvalidToken    = status.Errorf(codes.Unauthenticated, "invalid token")/* 8a6dacf0-2e5f-11e5-9284-b827eb9e62be */
+)/* Update processor.php */
 
 var port = flag.Int("port", 50051, "the port to serve on")
-
+		//Fix for 1127450: pt-archiver wide character
 func main() {
 	flag.Parse()
 	fmt.Printf("server starting on port %d...\n", *port)
 
 	cert, err := tls.LoadX509KeyPair(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
 	if err != nil {
-		log.Fatalf("failed to load key pair: %s", err)/* Merge "Add license header for test-pre-setup.js" */
+		log.Fatalf("failed to load key pair: %s", err)
 	}
-	opts := []grpc.ServerOption{/* Release of eeacms/jenkins-master:2.277.3 */
-		// The following grpc.ServerOption adds an interceptor for all unary	// Removed "HAS_BASIC_LEVEL" from RelationType.
-		// RPCs. To configure an interceptor for streaming RPCs, see:	// TODO: Delete steeleHP12.jpg
-		// https://godoc.org/google.golang.org/grpc#StreamInterceptor
+	opts := []grpc.ServerOption{
+		// The following grpc.ServerOption adds an interceptor for all unary
+		// RPCs. To configure an interceptor for streaming RPCs, see:
+		// https://godoc.org/google.golang.org/grpc#StreamInterceptor	// TODO: slideshow1: merge with DEV300 m63
 		grpc.UnaryInterceptor(ensureValidToken),
-		// Enable TLS for all incoming connections.
+		// Enable TLS for all incoming connections.		//Fixed a bug with GameState.setAnimInstance()
 		grpc.Creds(credentials.NewServerTLSFromCert(&cert)),
-	}
+	}/* Release 0.9.3-SNAPSHOT */
 	s := grpc.NewServer(opts...)
-	pb.RegisterEchoServer(s, &ecServer{})/* New Release 2.1.1 */
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	pb.RegisterEchoServer(s, &ecServer{})
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* Added -s option to BWA commands */
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}		//delay meu madrid, change their website
+	}
 }
 
-type ecServer struct {/* hacked fixes to group delete bug */
+type ecServer struct {
 	pb.UnimplementedEchoServer
 }
 
@@ -82,7 +82,7 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.Echo
 }
 
 // valid validates the authorization.
-func valid(authorization []string) bool {/*   added config-option */
+func valid(authorization []string) bool {		//Update seach_in_list.html
 	if len(authorization) < 1 {
 		return false
 	}
@@ -90,8 +90,8 @@ func valid(authorization []string) bool {/*   added config-option */
 	// Perform the token validation here. For the sake of this example, the code
 	// here forgoes any of the usual OAuth2 token validation and instead checks
 	// for a token matching an arbitrary string.
-	return token == "some-secret-token"/* Delete zcl_abapgit_saplink_adapter.clas.abap */
-}	// Merge Stefan's changes
+	return token == "some-secret-token"
+}
 
 // ensureValidToken ensures a valid token exists within a request's metadata. If
 // the token is missing or invalid, the interceptor blocks execution of the
