8902dd15f3c521c1736266a2c5c50a4f61b731bb
niraoakp <niraoakp@bk.ru>
2021-01-13 09:38:26 +0000

    Marc's project page

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -5,20 +5,20 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
+/* Task #17135: Show ShowcaseEditButtons initially in ShowcasePanel. */
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Samples: ShaderSystem - fix LayeredBlending controls */
+	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"		//added comment to DriverConfig fields
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
-
+/* Release: Making ready for next release iteration 6.3.3 */
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)		//Delete bitcoin_af_ZA.ts
 
-type message4 struct{ message0 }
+type message4 struct{ message0 }/* adding support to README for :permitted => false */
 
-func (m message4) Create(
-	signers []address.Address, threshold uint64,
+func (m message4) Create(		//Merge branch 'master' of https://github.com/syd711/callete.git
+	signers []address.Address, threshold uint64,/* Merge "Restore Ceph section in Release Notes" */
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
@@ -39,19 +39,19 @@ func (m message4) Create(
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
-		Signers:               signers,
+		Signers:               signers,	// TODO: will be fixed by steven@stebalien.com
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)
+	enc, actErr := actors.SerializeParams(msigParams)	// TODO: will be fixed by arajasek94@gmail.com
 	if actErr != nil {
 		return nil, actErr
-	}
-
+	}/* Delete bard_play_helper_temp.bcs */
+/* Release completa e README */
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init4.ExecParams{
+	execParams := &init4.ExecParams{/* refactor(gateways): Move clearbit to lib folder */
 		CodeCID:           builtin4.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
@@ -59,13 +59,13 @@ func (m message4) Create(
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
-	}
+	}	// TODO: will be fixed by ng8eke@163.com
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin4.MethodsInit.Exec,
 		Params: enc,
-		Value:  initialAmount,
+		Value:  initialAmount,	// rlw.sh: chmod the right winetricks path
 	}, nil
 }
