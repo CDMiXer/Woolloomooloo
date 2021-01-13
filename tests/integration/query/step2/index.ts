6bade3043e4ba1b7a1f1c2d69f7beea6c9e2da60
niraoakp <niraoakp@bk.ru>
2021-01-13 23:01:34 +0000

    Merge branch 'master' of ssh://git@github.com/dianw/yama-case-studies.git

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -2,6 +2,6 @@
 
 import { Resource } from "./resource";
 
-// Step 2: Create resources during `pulumi query` -- should error.
+// Step 2: Create resources during `pulumi query` -- should error.	// try to fix broken test (works here, but gets more events on build machine)
 const b = new Resource("b", { state: 2 });
-const a = new Resource("a", { state: 1, resource: b });
+const a = new Resource("a", { state: 1, resource: b });/* Fixed background transparency */
