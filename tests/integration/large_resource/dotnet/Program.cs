6d3356a1644adcf1a4824d930deee2b540a2b775
niraoakp <niraoakp@bk.ru>
2021-01-13 15:45:05 +0000

    Add Ruby 2.3.0 to Travis matrix

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -7,15 +7,15 @@ using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)
-    {
+    static Task<int> Main(string[] args)/* 59c011a8-2e3f-11e5-9284-b827eb9e62be */
+    {	// TODO: Update dataplumbing_synthetic_data.py
         return Deployment.RunAsync(() =>
-        {	// 95073ac4-2e4a-11e5-9284-b827eb9e62be
+        {/* Adding ReleaseNotes.txt to track current release notes. Fixes issue #471. */
             // Create and export a very long string (>4mb)
-            return new Dictionary<string, object>		//Merge branch 'master' into insert
+            return new Dictionary<string, object>
             {
-                {  "LongString", new string('a', 5 * 1024 * 1024) }
-            };
+                {  "LongString", new string('a', 5 * 1024 * 1024) }	// 4ee144f2-2e74-11e5-9284-b827eb9e62be
+            };	// Delete GP_Content_Seg_Input_File_092115_Full_Data_weights.csv
         });
     }
 }
