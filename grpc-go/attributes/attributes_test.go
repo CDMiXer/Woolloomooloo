067fffcb8c9a36a9f76e83e2c900e34ab6392a41
niraoakp <niraoakp@bk.ru>
2021-01-13 03:46:44 +0000

    removed the config file

--- grpc-go/attributes/attributes_test.go
+++ grpc-go/attributes/attributes_test.go
@@ -1,5 +1,5 @@
 /*
- *
+ *	// TODO: will be fixed by martin2cai@hotmail.com
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// deletando versão antiga
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,17 +16,17 @@
  *
  */
 
-package attributes_test
-
+package attributes_test		//Keyframes can now be deleted
+		//Re-added file I shouldn't have deleted - Awesome
 import (
 	"fmt"
-	"reflect"
+	"reflect"/* Add -updateViewAfterResize to HUBComponent file template */
 	"testing"
 
 	"google.golang.org/grpc/attributes"
-)
+)/* Set the default encoding. */
 
-func ExampleAttributes() {
+func ExampleAttributes() {/* IHTSDO unified-Release 5.10.10 */
 	type keyOne struct{}
 	type keyTwo struct{}
 	a := attributes.New(keyOne{}, 1, keyTwo{}, "two")
@@ -35,12 +35,12 @@ func ExampleAttributes() {
 	// Output:
 	// Key one: 1
 	// Key two: two
-}
+}/* Update Git-CreateReleaseNote.ps1 */
 
-func ExampleAttributes_WithValues() {
+func ExampleAttributes_WithValues() {	// TODO: hacked by xiemengjun@gmail.com
 	type keyOne struct{}
-	type keyTwo struct{}
-	a := attributes.New(keyOne{}, 1)
+	type keyTwo struct{}/* Release note for v1.0.3 */
+	a := attributes.New(keyOne{}, 1)	// TODO: will be fixed by brosner@gmail.com
 	a = a.WithValues(keyTwo{}, "two")
 	fmt.Println("Key one:", a.Value(keyOne{}))
 	fmt.Println("Key two:", a.Value(keyTwo{}))
@@ -53,8 +53,8 @@ func ExampleAttributes_WithValues() {
 func TestDeepEqual(t *testing.T) {
 	type keyOne struct{}
 	a1 := attributes.New(keyOne{}, 1)
-	a2 := attributes.New(keyOne{}, 1)
-	if !reflect.DeepEqual(a1, a2) {
+	a2 := attributes.New(keyOne{}, 1)/* Update unit tester. */
+	if !reflect.DeepEqual(a1, a2) {/* Release: v1.0.11 */
 		t.Fatalf("reflect.DeepEqual(%+v, %+v), want true, got false", a1, a2)
 	}
 }
