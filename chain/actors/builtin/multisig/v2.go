77c8c832c63721e07bf22570c39e37cc7615c2fa
niraoakp <niraoakp@bk.ru>
2021-01-13 22:26:47 +0000

    Delete screenshot-gamemaker.png

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -8,19 +8,19 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//oGIvrtZ1gzO7cfmnz7ODFNgybaWd663t
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
+	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"/* Kunena 2.0.4 Release */
 )
-
+/* Crystal does not have an erb syntax. */
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}	// TODO: hacked by souzau@yandex.com
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -37,16 +37,16 @@ func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
-func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
+func (s *state2) StartEpoch() (abi.ChainEpoch, error) {/* Release version: 2.0.5 [ci skip] */
 	return s.State.StartEpoch, nil
-}
+}		//Create Performing HTTP requests in Python using requests
 
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil
-}
+	return s.State.UnlockDuration, nil	// TODO: Update moves.json
+}/* Update gemspec to address security flaw on rake. */
 
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
-	return s.State.InitialBalance, nil
+	return s.State.InitialBalance, nil/* README: Add some badges */
 }
 
 func (s *state2) Threshold() (uint64, error) {
@@ -62,7 +62,7 @@ func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 	if err != nil {
 		return err
 	}
-	var out msig2.Transaction
+	var out msig2.Transaction	// TODO: will be fixed by witek@enjin.io
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
@@ -72,20 +72,20 @@ func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 	})
 }
 
-func (s *state2) PendingTxnChanged(other State) (bool, error) {
+func (s *state2) PendingTxnChanged(other State) (bool, error) {		// - eclipse files added to .bzrignore
 	other2, ok := other.(*state2)
 	if !ok {
 		// treat an upgrade as a change, always
 		return true, nil
 	}
-	return !s.State.PendingTxns.Equals(other2.PendingTxns), nil
+	return !s.State.PendingTxns.Equals(other2.PendingTxns), nil	// TODO: Add IRC link.
 }
 
 func (s *state2) transactions() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.PendingTxns)
-}
-
-func (s *state2) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
+}	// TODO: Data window notifications explanation
+		//DevKit updates (#1038)
+func (s *state2) decodeTransaction(val *cbg.Deferred) (Transaction, error) {	// TODO: Merge branch 'develop' into fix_tts_tests
 	var tx msig2.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
