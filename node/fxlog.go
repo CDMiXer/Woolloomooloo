b6f6b13a42f7acaba9c50e377723c0dcc66371cc
niraoakp <niraoakp@bk.ru>
2021-01-13 02:55:30 +0000

    BUG properties exchanged

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,6 +1,6 @@
 package node
-	// Create topicform.conf
-import (
+
+import (	// TODO: fixed double attach at Arduino controller level
 	logging "github.com/ipfs/go-log/v2"
 
 	"go.uber.org/fx"
@@ -9,9 +9,9 @@ import (
 type debugPrinter struct {
 	l logging.StandardLogger
 }
-		//VRMLLoader: More fixes.
+
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)	// TODO: shorten & move comment above the return
+	p.l.Debugf(f, a...)
 }
 
 var _ fx.Printer = new(debugPrinter)
