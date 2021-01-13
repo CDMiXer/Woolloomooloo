544087622048504b82a33ad99cbc9626b6b2cc0b
niraoakp <niraoakp@bk.ru>
2021-01-13 13:49:19 +0000

    63e1b372-2e5c-11e5-9284-b827eb9e62be

--- handler/web/logout.go
+++ handler/web/logout.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* cask-repair.rb: added description */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,21 +7,21 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Delete ui_teststat2.py
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package web
-	// 543f7df2-2e5a-11e5-9284-b827eb9e62be
+
 import (
-	"net/http"/* Merge branch 'master' into beta.2-releases */
-		//Added switchTeamFromPlayer method to the SessionWrapper.
-"tsid/iu-enord/enord/moc.buhtig"	
+	"net/http"
+
+	"github.com/drone/drone-ui/dist"
 )
 
-// HandleLogout creates an http.HandlerFunc that handles		//Added disabled fields support, htmlentities for tooltip text
-// session termination./* Reorganise, Prepare Release. */
+// HandleLogout creates an http.HandlerFunc that handles
+// session termination.
 func HandleLogout() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
