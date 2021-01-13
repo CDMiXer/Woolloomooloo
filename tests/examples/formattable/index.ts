c2c049fa7139b132961fda8af184945627709a21
niraoakp <niraoakp@bk.ru>
2021-01-13 05:06:19 +0000

    Changed to warn at 128 MB

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Add back .project */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 console.log("Here's some format goo: %s, %v, %q, %d!");
-console.error("Here's some format goo: %s, %v, %q, %d!");
+console.error("Here's some format goo: %s, %v, %q, %d!");/* Fixing status code handling */
