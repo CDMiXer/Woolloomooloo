1a3f53419616cf23e2bf081c88a9fc1a3d33b635
niraoakp <niraoakp@bk.ru>
2021-01-13 15:58:59 +0000

    Code sanity changes

--- handler/api/repos/builds/pulls/list_test.go
+++ handler/api/repos/builds/pulls/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by arachnid@notdot.net
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Add resizer example
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Add hpss call count for committing to a lightweight checkout.
 // that can be found in the LICENSE file.
-	// TODO: hacked by timnugent@gmail.com
+
 package pulls
