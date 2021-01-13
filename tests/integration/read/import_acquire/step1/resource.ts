9e64ac2d9ce1cca423a37381bc1bdbe227c4ed85
niraoakp <niraoakp@bk.ru>
2021-01-13 18:28:28 +0000

    Update 12/6

--- tests/integration/read/import_acquire/step1/resource.ts
+++ tests/integration/read/import_acquire/step1/resource.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Rename pageView.php to pageview.php
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,16 +9,16 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Add VPNodeTest  */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-		//update description to latest changes
-export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();/* Released: Version 11.5 */
 
-    private id: number = 0;	// paradigm for verbs in -iar (present in -eyo, -eo, -ío...)
+export class Provider implements dynamic.ResourceProvider {
+    public static readonly instance = new Provider();
+
+    private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
@@ -26,26 +26,26 @@ export class Provider implements dynamic.ResourceProvider {
         }
     }
 
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {	// TODO: Added a CNAME record for my domain name.
-        if (news.state !== olds.state) {/* Release new version 2.4.30: Fix GMail bug in Safari, other minor fixes */
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
+        if (news.state !== olds.state) {
             return {
                 changes: true,
                 replaces: ["state"],
-            };/* 375d8acc-2e4a-11e5-9284-b827eb9e62be */
+            };
         }
 
         return {
             changes: false,
-        }		//manage a better pom
+        }
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
-            id: (this.id++).toString(),/* Update W000805.jade */
+            id: (this.id++).toString(),
             outs: inputs,
         }
     }
-		//fix bug where flex value in props was being overridden
+
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
@@ -64,4 +64,4 @@ export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: any, opts?: pulumi.CustomResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}/* Release of eeacms/energy-union-frontend:1.7-beta.32 */
+}
