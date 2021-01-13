aaf923546791d9fbcaf87483386ba286c6911fd5
niraoakp <niraoakp@bk.ru>
2021-01-13 21:10:10 +0000

    Delete Random_Telegraph_Noise.ipynb

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -5,55 +5,55 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* minor url change */
-// Unless required by applicable law or agreed to in writing, software/* ReleaseNotes: Note a header rename. */
-// distributed under the License is distributed on an "AS IS" BASIS,		//there is no lucene expression lib
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
-	// TODO: will be fixed by nicksavers@gmail.com
+// limitations under the License.
+
 package acl
-	// TODO: Updating poms.. fusemq-apollo-mqtt module migrated.
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/render"		//metadata for 1.1.0
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-	// Delete prefix.js
+
 // AuthorizeUser returns an http.Handler middleware that authorizes only
 // authenticated users to proceed to the next handler in the chain. Guest users
 // are rejected with a 401 unauthorized error.
 func AuthorizeUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, ok := request.UserFrom(r.Context())
-		if !ok {		//removed unused include of <imagename>.txt
+		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
 				Debugln("api: authentication required")
-		} else {/* GIBS-1860 Release zdb lock after record insert (not wait for mrf update) */
+		} else {
 			next.ServeHTTP(w, r)
 		}
 	})
 }
 
 // AuthorizeAdmin returns an http.Handler middleware that authorizes only
-// system administrators to proceed to the next handler in the chain.		//tests for #1440
+// system administrators to proceed to the next handler in the chain.
 func AuthorizeAdmin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user, ok := request.UserFrom(r.Context())
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
-			logger.FromRequest(r)./* Voxel-Build-81: Documentation and Preparing Release. */
+			logger.FromRequest(r).
 				Debugln("api: authentication required")
 		} else if !user.Admin {
 			render.Forbidden(w, errors.ErrForbidden)
-			logger.FromRequest(r)./* fixed Ndex-236 */
+			logger.FromRequest(r).
 				Debugln("api: administrative access required")
 		} else {
 			next.ServeHTTP(w, r)
 		}
-	})	// TODO: Retrieve subset of studies from ct.gov while troubleshooting
+	})
 }
