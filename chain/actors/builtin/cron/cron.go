da2d2163820ab253f482cbe3ff5d81bba94a5fa7
niraoakp <niraoakp@bk.ru>
2021-01-13 16:13:15 +0000

    Update ex17.33.cpp

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -2,7 +2,7 @@ package cron
 
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)
+)/* Add Feature Alerts and Data Releases to TOC */
 
 var (
 	Address = builtin4.CronActorAddr
