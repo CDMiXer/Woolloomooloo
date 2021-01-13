f6a63af4e13d935130854d682449c57b1fa9caf0
niraoakp <niraoakp@bk.ru>
2021-01-13 22:07:49 +0000

    Update Infinity.podspec

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//ca3c9d02-2e4e-11e5-a98a-28cfe91dbc4b
-import { Resource } from "./resource";
 
-// Step 1: Populate our dependency graph.		//Clean up the strategy
-const a = new Resource("a", { state: 1 });/* Update cahier_des_charges.txt */
+import { Resource } from "./resource";	// Updated the pyuoi feedstock.
+
+// Step 1: Populate our dependency graph.
+const a = new Resource("a", { state: 1 });
 const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
