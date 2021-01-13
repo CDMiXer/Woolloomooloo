7438f8f4a907392fc2308dcddb1c5e90b5c14ea7
niraoakp <niraoakp@bk.ru>
2021-01-13 17:23:32 +0000

    PreferenceForm: Hinweis korrekt darstellen

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.		//Delete sprite_seq_simple.py
-//		//Rename to reflect the name change of the proxy
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Merge "Add portsecurity extension support"
-///* Release areca-7.3.1 */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Create LICENSE.adoc
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// more cleanup of includes and settings
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Remove possible double close of audio context */
+
 // Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
 // multiple subscribers.
-package pubsub	// TODO: will be fixed by peterke@gmail.com
+package pubsub
