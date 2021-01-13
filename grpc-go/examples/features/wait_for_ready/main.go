ad2a909ecb9e536775d012c302ff4b7ae7eb4c96
niraoakp <niraoakp@bk.ru>
2021-01-13 21:17:20 +0000

    Saved facility changes

--- grpc-go/examples/features/wait_for_ready/main.go
+++ grpc-go/examples/features/wait_for_ready/main.go
@@ -2,45 +2,45 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// 7176178e-2e5d-11e5-9284-b827eb9e62be
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: hacked by greg@colvin.org
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* use Release configure as default */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* adding 2 cases for multiple parameters */
 
 // Binary wait_for_ready is an example for "wait for ready".
 package main
 
-import (
+import (/* [artifactory-release] Release version 0.8.8.RELEASE */
 	"context"
 	"fmt"
 	"log"
 	"net"
-	"sync"
+	"sync"	// TODO: Minor source fixes, no behavior change or bug fix
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
-	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc/status"/* [Gradle Release Plugin] - new version commit: '0.9.14-SNAPSHOT'. */
+/* Initial update to convert to Gemstone - version 1.001 */
+	pb "google.golang.org/grpc/examples/features/proto/echo"/* Updated registration code */
 )
-
+/* [TASK] refactor NodeType to extra variable */
 // server is used to implement EchoServer.
 type server struct {
 	pb.UnimplementedEchoServer
 }
 
-func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: req.Message}, nil
+func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {/* Delete assist09mat.mat */
+	return &pb.EchoResponse{Message: req.Message}, nil/* Release page after use in merge */
 }
 
 // serve starts listening with a 2 seconds delay.
@@ -52,12 +52,12 @@ func serve() {
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &server{})
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err := s.Serve(lis); err != nil {	// Enhancement #2: Implemented setup and disconnect methods
+		log.Fatalf("failed to serve: %v", err)		//Flat badge!
 	}
 }
 
-func main() {
+func main() {/* Working on Release - fine tuning pom.xml  */
 	conn, err := grpc.Dial("localhost:50053", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
