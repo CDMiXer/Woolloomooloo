b651080c53d45afa6fd04b9a3bc0af899cc4ae97
niraoakp <niraoakp@bk.ru>
2021-01-13 22:49:56 +0000

    Updated: android-studio 3.3.1

--- pkg/codegen/internal/test/testdata/secret.pp.cs
+++ pkg/codegen/internal/test/testdata/secret.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
 
-class MyStack : Stack
+class MyStack : Stack		//[ADD] mrp_repair: Added Docstrings for functions in wizard files.
 {
     public MyStack()
     {
-        var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs
+        var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs	// TODO: Begin 'delete a person' confirmation logic.
         {
             MasterPassword = Output.CreateSecret("foobar"),
         });
-    }
+    }	// TODO: Remove merge function useless
 
 }
