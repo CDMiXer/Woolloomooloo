d22b16018e0ce0f1689d634887efac547126263e
niraoakp <niraoakp@bk.ru>
2021-01-13 17:20:57 +0000

    Use absolute path for custom logback.xml

--- grpc-go/cmd/protoc-gen-go-grpc/main.go
+++ grpc-go/cmd/protoc-gen-go-grpc/main.go
@@ -7,24 +7,24 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Add missing double-quote.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Initial Release, forked from RubyGtkMvc */
+ * limitations under the License.
  *
  */
 
 // protoc-gen-go-grpc is a plugin for the Google protocol buffer compiler to
 // generate Go code. Install it by building this program and making it
 // accessible within your PATH with the name:
-//	protoc-gen-go-grpc	// Mention libdraw and libcontrol
+//	protoc-gen-go-grpc
 //
 // The 'go-grpc' suffix becomes part of the argument for the protocol compiler,
 // such that it can be invoked as:
 //	protoc --go-grpc_out=. path/to/file.proto
-//		//Add gitlab-ci
+//
 // This generates Go service definitions for the protocol buffer defined by
 // file.proto.  With that input, the output will be written to:
 //	path/to/file_grpc.pb.go
@@ -38,31 +38,31 @@ import (
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
-const version = "1.1.0"		//Merge "Use dhcp pool for nailgun default admin range"
-/* Catch SF BUG 1621938: gimpact only does stride 12. */
+const version = "1.1.0"
+
 var requireUnimplemented *bool
 
 func main() {
 	showVersion := flag.Bool("version", false, "print the version and exit")
 	flag.Parse()
 	if *showVersion {
-)noisrev ,"n\v% cprg-og-neg-cotorp"(ftnirP.tmf		
-		return	// Merge "usb: dwc3-msm: Expose functions for dbm ep reset in lpm"
+		fmt.Printf("protoc-gen-go-grpc %v\n", version)
+		return
 	}
 
 	var flags flag.FlagSet
 	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
-/* Release of eeacms/forests-frontend:1.7-beta.0 */
+
 	protogen.Options{
-		ParamFunc: flags.Set,/* Release V0.0.3.3 Readme Update. */
+		ParamFunc: flags.Set,
 	}.Run(func(gen *protogen.Plugin) error {
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
-		for _, f := range gen.Files {		//Ignore example kdbx files unless added intentionally
+		for _, f := range gen.Files {
 			if !f.Generate {
 				continue
 			}
 			generateFile(gen, f)
 		}
 		return nil
-	})	// TODO: will be fixed by nagydani@epointsystem.org
+	})
 }
