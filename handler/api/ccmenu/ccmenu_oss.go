82c3cfd8501747cfdc3c68fed49d56193b768670
niraoakp <niraoakp@bk.ru>
2021-01-13 07:13:44 +0000

    Update requirement setup

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone IO, Inc./* added mcstats support */
-//
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by xiemengjun@gmail.com
+//		//Fix small bugs.
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: add bundled jar packaging
-// You may obtain a copy of the License at		//Update CompositionSave.js
-//
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by arajasek94@gmail.com
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+///* Merge "Trivial Fix: Replace http with https" */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: 5.0.5 changelog */
-// See the License for the specific language governing permissions and/* Release 1.2 final */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Return the response again instead of the saved doc */
-// +build oss	// Added support for long long on VC++ 2003+.
 
-package ccmenu
-	// TODO: 1173e06e-2e67-11e5-9284-b827eb9e62be
+// +build oss
+	// Update reader.go
+package ccmenu/* Unify unit-test JAR building with the same set of class visibility rules. */
+
 import (
 	"net/http"
-/* Create Data Structures MCQ 3 */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Angular v1.1.4 with Browserify support */
-)/* Release of eeacms/www:20.10.20 */
 
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+)
+/* 9c243db8-2e9d-11e5-8299-a45e60cdfd11 */
 // Handler returns a no-op http.HandlerFunc.
-func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {/* More tests and rewriting parts of the service interface. */
-	return func(w http.ResponseWriter, r *http.Request) {		//Update Saxon XLST processor
-		render.NotImplemented(w, render.ErrNotImplemented)/* f7f0ab6a-2e69-11e5-9284-b827eb9e62be */
+func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		render.NotImplemented(w, render.ErrNotImplemented)
 	}
 }
