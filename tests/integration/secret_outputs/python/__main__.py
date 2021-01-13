5c5b238120f62c387afa31f9c8e196886ee9a729
niraoakp <niraoakp@bk.ru>
2021-01-13 21:43:13 +0000

    Merge "soc: qcom: smem: Rename SMEM_CLKREGIM_BSP to SMEM_VSENSE_DATA"

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// 978699e2-2e55-11e5-9284-b827eb9e62be
+# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 from pulumi import export, Input, Output, ResourceOptions
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-class Provider(ResourceProvider):	// añadido un feature aunque no funciona bien
-    def create(self, props):	// TODO: hacked by hello@brooklynzelenka.com
+class Provider(ResourceProvider):
+    def create(self, props):
         return CreateResult("1", {"prefix": props["prefix"]})
 
 class R(Resource):
@@ -13,11 +13,11 @@ class R(Resource):
         super().__init__(Provider(), name, {"prefix": prefix}, opts)
 
 without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
-with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))/* Fixed compiler warning about unused variable, when running Release */
+with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
     prefix=Output.from_input("it's a secret to everybody"),
-    opts=ResourceOptions(additional_secret_outputs=["prefix"]))	// Grammar fix, and link to Orbiter in README
+    opts=ResourceOptions(additional_secret_outputs=["prefix"]))
 
-export("withoutSecret", without_secret)	// setup assistant: added control to drag the public key into the mail
+export("withoutSecret", without_secret)
 export("withSecret", with_secret)
 export("withSecretAdditional", with_secret_additional)
