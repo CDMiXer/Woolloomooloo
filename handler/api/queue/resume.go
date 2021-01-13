4d8187ed0aaec3e31affb5573c14537a46525a24
niraoakp <niraoakp@bk.ru>
2021-01-13 08:01:59 +0000

    Change Dabbs Bridge Road from Local to Major Collector

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release only .dist config files */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Release notes for v3.10. */
+// +build !oss/* Merge "[FIX] sap.m.Slider: Correct character encoding" */
 
 package queue
 
-import (	// TODO: hacked by sbrichards@gmail.com
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Release: Making ready to release 5.7.3 */
+	"github.com/drone/drone/core"	// TODO: df91596a-2e56-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-	// TODO: JUnit tests for heatmap web; removing the original modularization code
-// HandleResume returns an http.HandlerFunc that processes
+
+// HandleResume returns an http.HandlerFunc that processes/* Release date in release notes */
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// make LevelDb node  aware of db ready/not ready to reduce errors.
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
-		if err != nil {
+		if err != nil {	// TODO: Better stats on Proxy
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
