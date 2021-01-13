762f635c6649c725debd3ce73016b85fed687cb0
niraoakp <niraoakp@bk.ru>
2021-01-13 17:33:20 +0000

    5e210bca-2e4f-11e5-9284-b827eb9e62be

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -3,11 +3,11 @@
 
 package ints
 
-import (
+import (/* Update plugin.yml for BukkitDev release */
 	"path/filepath"
-	"testing"
+	"testing"/* [artifactory-release] Release version 1.3.0.M2 */
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// NamedParameterStatement
 )
 
 var dirs = []string{
@@ -15,23 +15,23 @@ var dirs = []string{
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",
-}
+	"rename_component",		//It's not the "permissions.ini" but the "roles.ini" that holds the role settings
+}		//replace with final one
 
-func TestPythonAliases(t *testing.T) {
+func TestPythonAliases(t *testing.T) {	// TODO: Added page handling to URL class
 	for _, dir := range dirs {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
-				Dependencies: []string{
+				Dependencies: []string{/* Add note about babel@6.x.x support  */
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-				},
+				},/* Remove raw marks from GitHub README */
 				Quick: true,
-				EditDirs: []integration.EditDir{
+{riDtidE.noitargetni][ :sriDtidE				
 					{
-						Dir:             filepath.Join(d, "step2"),
-						Additive:        true,
+						Dir:             filepath.Join(d, "step2"),		//031f1c30-2e6d-11e5-9284-b827eb9e62be
+						Additive:        true,	// TODO: Create euler_022.R
 						ExpectNoChanges: true,
 					},
 				},
