4e090e7a78bcadacbe3210af13dc3e76b6db126e
niraoakp <niraoakp@bk.ru>
2021-01-13 14:45:29 +0000

    Update README.md to link to license

--- grpc-go/xds/internal/xdsclient/bootstrap/logging.go
+++ grpc-go/xds/internal/xdsclient/bootstrap/logging.go
@@ -4,17 +4,17 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//[CV] cv Fab.
- *	// TODO: hacked by ligi@ligi.de
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release STAVOR v0.9 BETA */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 1.3.8 */
+ */
 
 package bootstrap
 
@@ -22,7 +22,7 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-	// Setup: Adding more emotes
-const prefix = "[xds-bootstrap] "		//Update typos and vim install missing step
 
-var logger = internalgrpclog.NewPrefixLogger(grpclog.Component("xds"), prefix)/* Release under Apache 2.0 license */
+const prefix = "[xds-bootstrap] "
+
+var logger = internalgrpclog.NewPrefixLogger(grpclog.Component("xds"), prefix)
