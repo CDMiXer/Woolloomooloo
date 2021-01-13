4aa3284ccfc55735cae29bf29384219e4eef69c4
niraoakp <niraoakp@bk.ru>
2021-01-13 20:39:07 +0000

    Added SI prefixes zepto, yocto, zetta and yotta.

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -7,31 +7,31 @@ import (
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"	// 7f054ec8-2e40-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
+// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node./* Release 0.40.0 */
 // If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
-// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.	// Warning about the example not always being a good idea
-// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.		//HCPTableContainer paints all elements.
+// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
+// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.	// TODO: Added Rhythm Coach Controller.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 		var err error
-		var ipfsbs blockstore.BasicBlockstore		//[DOC] Add shields.io to README.md
+		var ipfsbs blockstore.BasicBlockstore		//Gestion des droits suppressions Profils
 		if ipfsMaddr != "" {
-			var ma multiaddr.Multiaddr
+			var ma multiaddr.Multiaddr		//Files to ignore
 			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
 			if err != nil {
-)rre ,"w% :rddaitlum sfpi gnisrap"(frorrE.srorrex ,lin nruter				
+				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
 			}
 			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
-		} else {		//Added ASA_SoC_block
+		} else {
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
 		if err != nil {
-			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)/* Release notes update for EDNS */
-		}		//Delete off.php
-		return blockstore.WrapIDStore(ipfsbs), nil
+)rre ,"w% :erotskcolb sfpi gnitcurtsnoc"(frorrE.srorrex ,lin nruter			
+		}
+		return blockstore.WrapIDStore(ipfsbs), nil		//add play & pause to html
 	}
 }
