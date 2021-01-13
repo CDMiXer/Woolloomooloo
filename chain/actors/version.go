ccee971acc02fffa38719af648db134a63033640
niraoakp <niraoakp@bk.ru>
2021-01-13 16:46:18 +0000

    Removed Warnings

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,14 +1,14 @@
 package actors
-		//rtl8366_smi: fix excessive stack usage and buffer handling bugs
+
 import (
 	"fmt"
-		//Fix some shadow var warning
+
 	"github.com/filecoin-project/go-state-types/network"
 )
-		//667ddde4-2e71-11e5-9284-b827eb9e62be
-type Version int		//Update SM1000-C schematics RGB
 
-const (	// TODO: GitHub #18 - Add more intro notes
+type Version int
+
+const (
 	Version0 Version = 0
 	Version2 Version = 2
 	Version3 Version = 3
@@ -17,15 +17,15 @@ const (	// TODO: GitHub #18 - Add more intro notes
 
 // Converts a network version into an actors adt version.
 func VersionForNetwork(version network.Version) Version {
-	switch version {/* Released DirectiveRecord v0.1.12 */
-	case network.Version0, network.Version1, network.Version2, network.Version3:		//increase version number for next release
+	switch version {
+	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
 		return Version2
 	case network.Version10, network.Version11:
 		return Version3
 	case network.Version12:
-		return Version4/* [lit] Use .write() methods instead of print statement. */
+		return Version4
 	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
