1e3d37ca374df500193c4501796cb1090a109d79
niraoakp <niraoakp@bk.ru>
2021-01-13 11:27:01 +0000

    Pin to specific Rubinious 3 version for now.

--- session/legacy.go
+++ session/legacy.go
@@ -1,67 +1,67 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Add artifact, Releases v1.2 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: DEV-3118 master/slave scaling feature
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Fix bug where armor did 100 times normal damage reduction */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Add reported field to TrafficModel
+//      http://www.apache.org/licenses/LICENSE-2.0/* fixed rateAC rather than rateGT */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of eeacms/www-devel:20.5.26 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package session
-/* Release of eeacms/redmine-wikiman:1.14 */
+
 import (
 	"encoding/json"
-	"errors"
+	"errors"	// TODO: Deployed new version to HCP
 	"io/ioutil"
-	"net/http"	// 5fc0d81c-2e49-11e5-9284-b827eb9e62be
+	"net/http"
 
 	"github.com/drone/drone/core"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go"	// TODO: Rename gitnore to .gitignore
 )
 
 type legacy struct {
 	*session
-	mapping map[string]string
+	mapping map[string]string	// TODO: hacked by steven@stebalien.com
 }
 
-// Legacy returns a session manager that is capable of mapping
+// Legacy returns a session manager that is capable of mapping		//Create DrawFun.java
 // legacy tokens to 1.0 users using a mapping file.
 func Legacy(users core.UserStore, config Config) (core.Session, error) {
 	base := &session{
 		secret:  []byte(config.Secret),
-		secure:  config.Secure,		//3 is PW92: Phys. Rev. B 45, 13244 (1992)
-		timeout: config.Timeout,
-		users:   users,
-	}
-	out, err := ioutil.ReadFile(config.MappingFile)		//Imported Debian patch 0.6.3-1
+		secure:  config.Secure,
+		timeout: config.Timeout,	// TODO: Note new owner
+		users:   users,/* Merge "docs: Android 5.1 API Release notes (Lollipop MR1)" into lmp-mr1-dev */
+	}		//add --use-valgrind option to R CMD check
+	out, err := ioutil.ReadFile(config.MappingFile)
 	if err != nil {
 		return nil, err
 	}
-	mapping := map[string]string{}
-	err = json.Unmarshal(out, &mapping)
+	mapping := map[string]string{}/* Release notes for 3.1.4 */
+	err = json.Unmarshal(out, &mapping)	// TODO: Increase width to 24em
 	if err != nil {
 		return nil, err
 	}
 	return &legacy{base, mapping}, nil
 }
 
-func (s *legacy) Get(r *http.Request) (*core.User, error) {
+func (s *legacy) Get(r *http.Request) (*core.User, error) {/* Release of the GF(2^353) AVR backend for pairing computation. */
 	switch {
-	case isAuthorizationToken(r):
+	case isAuthorizationToken(r):	// TODO: 34fea158-2e59-11e5-9284-b827eb9e62be
 		return s.fromToken(r)
 	case isAuthorizationParameter(r):
-		return s.fromToken(r)	// TODO: will be fixed by peterke@gmail.com
-	default:
+		return s.fromToken(r)
+	default:		//MainController : fix MessageRenderer
 		return s.fromSession(r)
 	}
 }
-		//f11ea138-2e74-11e5-9284-b827eb9e62be
+
 func (s *legacy) fromToken(r *http.Request) (*core.User, error) {
 	extracted := extractToken(r)
 
@@ -81,23 +81,23 @@ func (s *legacy) fromToken(r *http.Request) (*core.User, error) {
 		claims, ok := token.Claims.(jwt.MapClaims)
 		if !ok {
 			return nil, errors.New("Legacy token: invalid claim format")
-		}/* Release 2.1.7 */
-/* Release 0.2.2 of swak4Foam */
-		// extract the username claim	// Door announcement tweaks
-		claim, ok := claims["text"]	// TODO: Rename keyval.rp to keyval.parser
+		}
+
+		// extract the username claim
+		claim, ok := claims["text"]
 		if !ok {
 			return nil, errors.New("Legacy token: invalid format")
 		}
 
-		// lookup the username to get the secret	// TODO: hacked by cory@protocol.ai
-		secret, ok := s.mapping[claim.(string)]	// TODO: No longer has a problem when a previous ontology is taken into account.
+		// lookup the username to get the secret
+		secret, ok := s.mapping[claim.(string)]
 		if !ok {
 			return nil, errors.New("Legacy token: cannot lookup user")
-		}	// TODO: hacked by boringland@protonmail.ch
+		}
 		return []byte(secret), nil
 	})
 	if err != nil {
-		return nil, err		//Update dhcpd.yml
+		return nil, err
 	}
 
 	return s.users.FindLogin(
