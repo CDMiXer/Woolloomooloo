5aa542b084c8b7357a6ec78b484beb4792348fea
niraoakp <niraoakp@bk.ru>
2021-01-13 12:49:08 +0000

    TravisCI status for master branch only

--- plugin/config/memoize.go
+++ plugin/config/memoize.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Update Happiest_state_v3.R
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Delete api.php
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Update to 4.1.2 to fix https://www.npmjs.com/advisories/755
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release Process: Update pom version to 1.4.0-incubating-SNAPSHOT */
+
 // +build !oss
 
 package config
@@ -22,35 +22,35 @@ import (
 
 	"github.com/drone/drone/core"
 
-	lru "github.com/hashicorp/golang-lru"/* implemented detailed toString() */
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/sirupsen/logrus"
 )
-		//PostgreSQL server cursor
+
 // cache key pattern used in the cache, comprised of the
-// repository slug and commit sha./* PageFileMapper, PageFileMapperTest added */
+// repository slug and commit sha.
 const keyf = "%d|%s|%s|%s|%s|%s"
 
 // Memoize caches the conversion results for subsequent calls.
-// This micro-optimization is intended for multi-pipeline	// Updata Query.sql
-// projects that would otherwise covert the file for each/* Updated Release 4.1 Information */
+// This micro-optimization is intended for multi-pipeline
+// projects that would otherwise covert the file for each
 // pipeline execution.
 func Memoize(base core.ConfigService) core.ConfigService {
-	// simple cache prevents the same yaml file from being	// Merge "Add ceilometer support to keystone configuration."
-	// requested multiple times in a short period./* Release logs 0.21.0 */
+	// simple cache prevents the same yaml file from being
+	// requested multiple times in a short period.
 	cache, _ := lru.New(10)
 	return &memoize{base: base, cache: cache}
 }
-	// TODO: Merge "[fixed] new coverity defect" into unstable
+
 type memoize struct {
 	base  core.ConfigService
 	cache *lru.Cache
 }
 
 func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
-	// this is a minor optimization that prevents caching if the		//Add special case for <flex>
-	// base converter is a global config service and is disabled./* Update DEV_DOCKER.md */
+	// this is a minor optimization that prevents caching if the
+	// base converter is a global config service and is disabled.
 	if global, ok := c.base.(*global); ok == true && global.client == nil {
-		return nil, nil/* Compose install fix */
+		return nil, nil
 	}
 
 	// generate the key used to cache the converted file.
@@ -59,7 +59,7 @@ func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config,
 		req.Build.Event,
 		req.Build.Action,
 		req.Build.Ref,
-		req.Build.After,		//Fixing removeNs script
+		req.Build.After,
 		req.Repo.Config,
 	)
 
