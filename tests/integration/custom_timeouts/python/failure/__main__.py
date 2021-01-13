be5847ab34e0d9ee591cdddc8d57bd77005defb1
niraoakp <niraoakp@bk.ru>
2021-01-13 14:38:23 +0000

    change logo and favicon (kkandpwiki) T1575

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,12 +1,12 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Create SuperSweetTildeSuite
+	// TODO: add tweets in db
+from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions/* Release new version 2.5.27: Fix some websites broken by injecting a <link> tag */
 
-from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
-
-class Resource1(ComponentResource):	// TODO: hacked by xiemengjun@gmail.com
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+	// TODO: Changed dictionary keys from unicode to strings.
 # Attempt to create a resource with a CustomTimeout that should fail
 res5 = Resource1("res5",
-    opts=ResourceOptions(custom_timeouts='asdf')	// TODO: will be fixed by nicksavers@gmail.com
-)	// TODO: will be fixed by arajasek94@gmail.com
+    opts=ResourceOptions(custom_timeouts='asdf')
+)
