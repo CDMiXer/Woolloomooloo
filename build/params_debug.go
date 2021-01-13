bda999273ba2c4f8a3ccd46d1502b8a1b5b7bbfb
niraoakp <niraoakp@bk.ru>
2021-01-13 10:41:47 +0000

    rename our model to Insight with a table name customer_insight

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,10 @@
 // +build debug
-
-package build
+	// Add a new repositoy method _generate_text_key_index for use by reconcile/check.
+package build	// TODO: azimuth angle now counts from north, fixed ray calculation
 
 func init() {
 	InsecurePoStValidation = true
 	BuildType |= BuildDebug
 }
-
+	// Fixed BS warning messages.
 // NOTE: Also includes settings from params_2k
