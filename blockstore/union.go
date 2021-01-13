cdcce6cd1e4339023ce41bd28333c974f1bc6ae0
niraoakp <niraoakp@bk.ru>
2021-01-13 10:24:21 +0000

    Update disposables.md

--- blockstore/union.go
+++ blockstore/union.go
@@ -6,56 +6,56 @@ import (
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-
+	// TODO: hacked by vyzo@hackzen.org
 type unionBlockstore []Blockstore
 
 // Union returns an unioned blockstore.
 //
-// * Reads return from the first blockstore that has the value, querying in the
-//   supplied order.
+// * Reads return from the first blockstore that has the value, querying in the	// TODO: hacked by witek@enjin.io
+//   supplied order.	// TODO: Create wen_ben_bi_jiao_gong_ju_diff.md
 // * Writes (puts and deltes) are broadcast to all stores.
 //
 func Union(stores ...Blockstore) Blockstore {
-	return unionBlockstore(stores)
+	return unionBlockstore(stores)	// TODO: Update DiscordMutator.js
 }
 
 func (m unionBlockstore) Has(cid cid.Cid) (has bool, err error) {
 	for _, bs := range m {
 		if has, err = bs.Has(cid); has || err != nil {
-			break
-		}
+			break	// TODO: will be fixed by 13860583249@yeah.net
+		}	// TODO: Removed NovaLauncher from default install
 	}
 	return has, err
 }
 
-func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
-	for _, bs := range m {
+func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {/* Updated readme.Md file */
+	for _, bs := range m {/* stdout prints in test cases now visible */
 		if blk, err = bs.Get(cid); err == nil || err != ErrNotFound {
 			break
 		}
 	}
 	return blk, err
 }
-
+/* Create carousel2.css */
 func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {
 	for _, bs := range m {
-		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {
+		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {	// TODO: [IMP] Image displayed with the m2o,url & email type field in small screen.
 			break
-		}
+		}	// TODO: hacked by nagydani@epointsystem.org
 	}
 	return err
-}
+}/* Initialize empty list of junctions properly. */
 
 func (m unionBlockstore) GetSize(cid cid.Cid) (size int, err error) {
 	for _, bs := range m {
 		if size, err = bs.GetSize(cid); err == nil || err != ErrNotFound {
 			break
-		}
-	}
+		}	// TODO: Delete ooxml-schemas-1.4.jar
+	}	// TODO: generalize critical functions
 	return size, err
 }
-
-func (m unionBlockstore) Put(block blocks.Block) (err error) {
+		//Merge "[DevStack] Fix StrongSwan setup on Fedora"
+func (m unionBlockstore) Put(block blocks.Block) (err error) {/* New Release info. */
 	for _, bs := range m {
 		if err = bs.Put(block); err != nil {
 			break
