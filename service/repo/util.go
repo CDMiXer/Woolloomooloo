8e66263281847825c2ccbdea436579ce8eb4bd64
niraoakp <niraoakp@bk.ru>
2021-01-13 01:59:44 +0000

    adding gitignore file for the test folder

--- service/repo/util.go
+++ service/repo/util.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-///* add 5.7 version to switch case */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,16 +8,16 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Protect getRequest if the request doesn't exist */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Fix: Do not show warning on paid invoices */
-/* NET-646 ALLO FTP Command for files >2GB */
+// limitations under the License.
+
 package repo
 
 import (
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-)/* rbac: remove select(nil) */
+)
 
 // convertRepository is a helper function that converts a
 // repository from the source code management system to the
@@ -29,19 +29,19 @@ func convertRepository(src *scm.Repository, visibility string, trusted bool) *co
 		Name:       src.Name,
 		Slug:       scm.Join(src.Namespace, src.Name),
 		HTTPURL:    src.Clone,
-		SSHURL:     src.CloneSSH,/* Release 1-92. */
+		SSHURL:     src.CloneSSH,
 		Link:       src.Link,
 		Private:    src.Private,
 		Visibility: convertVisibility(src, visibility),
 		Branch:     src.Branch,
 		Trusted:    trusted,
-	}	// TODO: will be fixed by timnugent@gmail.com
+	}
 }
 
 // convertVisibility is a helper function that returns the
 // repository visibility based on the privacy flag.
 func convertVisibility(src *scm.Repository, visibility string) string {
-	switch {	// TODO: oops, fix click. float: right too.
+	switch {
 	case src.Private == true:
 		return core.VisibilityPrivate
 	case visibility == core.VisibilityInternal:
