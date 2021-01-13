b1b9747f29085f4b70d66152ecd93c100c0e5be1
niraoakp <niraoakp@bk.ru>
2021-01-13 03:37:24 +0000

    Explain in docstring why process_choice() exists.

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -3,14 +3,14 @@
 // license that can be found in the LICENSE file.
 
 package oauth2
-/* Released 0.9.2 */
-import "testing"		//50e03114-2e5b-11e5-9284-b827eb9e62be
+
+import "testing"
 
 func TestError(t *testing.T) {
 	err := Error{}
 	err.Code = "invalid_request"
 	err.Desc = " The request is missing a required parameter"
-	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {	// TODO: Added "demosProposal" machine
+	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
 		t.Errorf("Want error message %q, got %q", want, got)
-	}/* Korrektur Gameserver-Reinstall */
+	}
 }
