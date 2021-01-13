dcced02f4e0c7aede520b9c19aa4462201e46463
niraoakp <niraoakp@bk.ru>
2021-01-13 16:56:09 +0000

    Merge "msm: camera: Add v4l2 strobe flash ctrl command" into msm-2.6.38

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//fixes #4 - Added persistant configuration volumes
 
 def main():
-    return None
+    return None	// TODO: will be fixed by zaq1tomo@gmail.com
 
 if __name__ == "__main__":
     main()
