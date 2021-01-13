00f28700726e645ac8a439a6dabf78b39a13a1d4
niraoakp <niraoakp@bk.ru>
2021-01-13 16:48:48 +0000

    Add public meeting note to README

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update CustomKit.java */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//rev 537673
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Finish implement basic fs operations
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Minor clarifications in readme */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
 
-import (/* look for a few more headers */
+import (
 	"fmt"
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"/* style clean up */
+	"github.com/go-chi/chi"
 )
 
 // HandleLast returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body for the latest build.
 func HandleLast(
-	repos core.RepositoryStore,	// SVN address has changed
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//revert extra config namespace
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			ref       = r.FormValue("ref")
-)"hcnarb"(eulaVmroF.r =    hcnarb			
+			branch    = r.FormValue("branch")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)		//StereoRig moved to separate module
+			render.NotFound(w, err)
 			return
 		}
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
-		}	// TODO: hacked by zhen6939@gmail.com
+		}
 		if branch != "" {
 			ref = fmt.Sprintf("refs/heads/%s", branch)
 		}
-		build, err := builds.FindRef(r.Context(), repo.ID, ref)/* Release of eeacms/forests-frontend:2.0-beta.57 */
-		if err != nil {	// TODO: will be fixed by greg@colvin.org
+		build, err := builds.FindRef(r.Context(), repo.ID, ref)
+		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Build in Release mode */
+		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			return		//Add logging to Authentiation functions
+			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
