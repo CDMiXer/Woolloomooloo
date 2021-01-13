4cba0e9c407cc1ea5f0462c506e2e5ddb1212abf
niraoakp <niraoakp@bk.ru>
2021-01-13 02:16:23 +0000

    Global reconstruction done.

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,42 +1,42 @@
-// Copyright 2016-2018, Pulumi Corporation./* bundle-size: 416f2b202c06ba6b33ed3637105f63aa43549895 (86.38KB) */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* move Xlib stuff to docky.services */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//		//Add utility method getParameter to SwaggerUtil
+// Unless required by applicable law or agreed to in writing, software/* cleanup & synchronization between all languages */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* New Release Note. */
+// limitations under the License.
 
 package stack
-/* added Ws2_32.lib to "Release" library dependencies */
+
 import (
 	"io/ioutil"
-	"testing"/* Release version 0.5.3 */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadV0Checkpoint(t *testing.T) {
-	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")/* Release 0.95.169 */
+func TestLoadV0Checkpoint(t *testing.T) {		//Updating build-info/dotnet/coreclr/master for preview-27109-03
+	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
 	assert.NoError(t, err)
-/* Merge branch 'master' into feature/split-mitigation-check */
-	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
-	assert.NoError(t, err)
-	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)/* Release 0.2.1rc1 */
-}		//18806f8e-2e5e-11e5-9284-b827eb9e62be
 
-func TestLoadV1Checkpoint(t *testing.T) {/* bumped to version 10.1.24 */
+	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
+	assert.NoError(t, err)/* Merge "Bug 1829943: Release submitted portfolios when deleting an institution" */
+	assert.NotNil(t, chk.Latest)
+	assert.Len(t, chk.Latest.Resources, 30)
+}
+	// TODO: Merge "Remove neutronclient mocks from network delete tests"
+func TestLoadV1Checkpoint(t *testing.T) {	// Merge "Add service of ns scale"
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
 	assert.NoError(t, err)
 
-	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
+	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)/* Release 1.0.1 final */
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)		//Version bump v0.5.5
+	assert.Len(t, chk.Latest.Resources, 30)		//3c21a214-2e40-11e5-9284-b827eb9e62be
 }
