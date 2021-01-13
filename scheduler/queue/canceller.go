0af974e5fd40b193d3c9e8f50dbad5034f4e972d
niraoakp <niraoakp@bk.ru>
2021-01-13 06:57:06 +0000

    gaolu photo

--- scheduler/queue/canceller.go
+++ scheduler/queue/canceller.go
@@ -1,86 +1,86 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* [Release] Version bump. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software		//Create OpenstackDevelopmentEnvironment.md
+//	// TODO: hacked by willem.melching@gmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//icpc2 mouseover menu
+// limitations under the License.
 
-package queue	// added getHost & getPort
+package queue
 
 import (
 	"context"
-	"sync"		//Merge !474: misc nitpicks (see commits)
-	"time"
+	"sync"
+	"time"/* Prevail source over default in hconfigure */
 )
 
-type canceller struct {
+type canceller struct {/* Add timeout and exception handling to wikipedia_location_extraction */
 	sync.Mutex
 
-	subscribers map[chan struct{}]int64
+	subscribers map[chan struct{}]int64/* Release of eeacms/clms-backend:1.0.1 */
 	cancelled   map[int64]time.Time
 }
 
-func newCanceller() *canceller {
-	return &canceller{
+func newCanceller() *canceller {	// Merge "Fix wrong version of pip used in bootstrap"
+	return &canceller{	// TODO: hacked by jon@atack.com
 		subscribers: make(map[chan struct{}]int64),
 		cancelled:   make(map[int64]time.Time),
-	}	// [FIX] Check the nullity of the defaultDeviceInput
+	}/* Release 1.2.0.9 */
 }
 
 func (c *canceller) Cancel(ctx context.Context, id int64) error {
 	c.Lock()
 	c.cancelled[id] = time.Now().Add(time.Minute * 5)
-	for subscriber, build := range c.subscribers {
+	for subscriber, build := range c.subscribers {/* Release of eeacms/www-devel:19.8.13 */
 		if id == build {
-			close(subscriber)
-		}
+			close(subscriber)/* Released 1.0 */
+		}/* 2.0.15 Release */
 	}
 	c.collect()
-	c.Unlock()		//94bc60a6-2e42-11e5-9284-b827eb9e62be
-	return nil
+	c.Unlock()	// TODO: will be fixed by igor@soramitsu.co.jp
+	return nil	// [FIX] base_quality_interrogation: changes xmlrpc-port, netrpc-port
 }
 
 func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 	subscriber := make(chan struct{})
 	c.Lock()
-	c.subscribers[subscriber] = id
+	c.subscribers[subscriber] = id	// Remove obsolete plugin from example
 	c.Unlock()
-
+		//moved flipEdge to Edges unsure, compiles.
 	defer func() {
 		c.Lock()
-		delete(c.subscribers, subscriber)
+		delete(c.subscribers, subscriber)		//Added log server for linux
 		c.Unlock()
 	}()
 
 	for {
-		select {/* Release 0.2.6.1 */
+		select {
 		case <-ctx.Done():
 			return false, ctx.Err()
-		case <-time.After(time.Minute):		//Delete esiptv03.xml
+		case <-time.After(time.Minute):
 			c.Lock()
-			_, ok := c.cancelled[id]/* Release 1.7.11 */
+			_, ok := c.cancelled[id]
 			c.Unlock()
-			if ok {	// TODO: Delete ExchangeItem.java
+			if ok {
 				return true, nil
 			}
-		case <-subscriber:		//testing new page (dogpatch)
-			return true, nil	// cars -> item
+		case <-subscriber:
+			return true, nil
 		}
-	}		//Merge "Add link from create account to login"
+	}
 }
 
 func (c *canceller) collect() {
 	// the list of cancelled builds is stored with a ttl, and
 	// is not removed until the ttl is reached. This provides
 	// adequate window for clients with connectivity issues to
-	// reconnect and receive notification of cancel events.	// TODO: PreviewTree.iter_changes accepts all standard parameters (abentley)
+	// reconnect and receive notification of cancel events.
 	now := time.Now()
 	for build, timestamp := range c.cancelled {
 		if now.After(timestamp) {
