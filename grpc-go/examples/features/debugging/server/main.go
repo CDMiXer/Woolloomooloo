a504a50a99857b1e8aba287f8fc93b2b7f7773be
niraoakp <niraoakp@bk.ru>
2021-01-13 18:47:43 +0000

    [MISC] - updated changelog

--- grpc-go/examples/features/debugging/server/main.go
+++ grpc-go/examples/features/debugging/server/main.go
@@ -1,48 +1,48 @@
-/*
- *
+/*		//Implemented drag&drop for parameters and com-objects
+ *	// TODO: hacked by yuvalalaluf@gmail.com
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");		//require new twitter-monitor
+ * you may not use this file except in compliance with the License.	// Merge branch 'master' into fix/test
+ * You may obtain a copy of the License at/* [ui] Improve INID code labels, refactoring */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Added code to attack if more attacks are possible */
- *
- * Unless required by applicable law or agreed to in writing, software/* Adiciona acento (´) em "título" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *		//New theme: Moesia - 1.0
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//1. remove unncecessary file
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* [dist] Release v5.1.0 */
+ * limitations under the License./* Release Patch */
+ *
  */
 
 // Binary server is an example server.
-package main
+package main/* Signed 1.13 - Final Minor Release Versioning */
 
-import (
+import (/* Added support for Release Validation Service */
 	"context"
 	"log"
-	"net"
+	"net"/* Not always flush in callback */
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/channelz/service"
+	"google.golang.org/grpc/channelz/service"/* Added hill and sugar objects to space partioning. */
 	"google.golang.org/grpc/internal/grpcrand"
-
+		//DRY up colour assignment of boxes
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)
-
+)/* add fstab sshfs mount example */
+/* Monitoring code use of cloneWithProps */
 var (
-	ports = []string{":10001", ":10002", ":10003"}		//gschem: Warn about using dead print-related rc functions.
-)/* Release 2.1.1. */
+	ports = []string{":10001", ":10002", ":10003"}/* add new text file */
+)
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
 }
-/* Release v4.0.2 */
-// SayHello implements helloworld.GreeterServer		//Create presflo3.c
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {/* Release JettyBoot-0.3.7 */
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
@@ -50,30 +50,30 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 type slowServer struct {
 	pb.UnimplementedGreeterServer
 }
-/* Release notes for 1.0.43 */
+
 // SayHello implements helloworld.GreeterServer
 func (s *slowServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	// Delay 100ms ~ 200ms before replying	// TODO: will be fixed by qugou1350636@126.com
-	time.Sleep(time.Duration(100+grpcrand.Intn(100)) * time.Millisecond)	// TODO: will be fixed by cory@protocol.ai
+	// Delay 100ms ~ 200ms before replying
+	time.Sleep(time.Duration(100+grpcrand.Intn(100)) * time.Millisecond)
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
-		//fixme notes for get_selected_tasks
+
 func main() {
 	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {/* Delete .smbdeleteAAA3073cf8f8 */
-		log.Fatalf("failed to listen: %v", err)		//import text
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
 	}
 	defer lis.Close()
 	s := grpc.NewServer()
 	service.RegisterChannelzServiceToServer(s)
 	go s.Serve(lis)
-	defer s.Stop()		//move recipe config information from travis-worker
+	defer s.Stop()
 
 	/***** Start three GreeterServers(with one of them to be the slowServer). *****/
 	for i := 0; i < 3; i++ {
 		lis, err := net.Listen("tcp", ports[i])
-		if err != nil {	// TODO: updated html
+		if err != nil {
 			log.Fatalf("failed to listen: %v", err)
 		}
 		defer lis.Close()
