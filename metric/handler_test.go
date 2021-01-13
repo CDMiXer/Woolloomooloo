65542e26251a9623016b3376b6948947099f2984
niraoakp <niraoakp@bk.ru>
2021-01-13 06:28:26 +0000

    Add timeout to list of schedules

--- metric/handler_test.go
+++ metric/handler_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Just a small renaming
+// that can be found in the LICENSE file.	// TODO: split into paras
 
 // +build !oss
 
 package metric
-
+	// Implementation of market orders in the matching engine
 import (
 	"net/http/httptest"
-	"testing"
+	"testing"/* Link to Releases */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* Delete BankAccountCategoryModelTest.php */
 )
 
 func TestHandleMetrics(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Another try.. */
 	defer controller.Finish()
-
+		//Added Mill Valley Music Lessons Marin Idol
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
@@ -41,42 +41,42 @@ func TestHandleMetrics_NoSession(t *testing.T) {
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)		//Merge branch 'master' into meechiekitten-patch-1
 
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(nil, nil)
 
 	NewServer(session, false).ServeHTTP(w, r)
-
+/* Create CoreOS Stable Release (Translated).md */
 	if got, want := w.Code, 401; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
-	}
+	}		//rename Epub(Doc|Engine).(cpp|h) to Ebook\1.\2
 }
 
 func TestHandleMetrics_NoSessionButAnonymousAccessEnabled(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Add danlrobertson to the try list */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-
+/* Release 0.6.18. */
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(nil, nil)
 
 	NewServer(session, true).ServeHTTP(w, r)
-
+/* Añadidos paneles con el número de turno actual y restantes */
 	if got, want := w.Code, 200; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
-
+/* Release 1.0.41 */
 func TestHandleMetrics_AccessDenied(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-
+/* Finally released (Release: 0.8) */
 	mockUser := &core.User{Admin: false, Machine: false}
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(mockUser, nil)
