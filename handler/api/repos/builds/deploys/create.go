7a8910c2295661a3b9a042eb94b303d557719c98
niraoakp <niraoakp@bk.ru>
2021-01-13 09:57:10 +0000

    Implemented Matches Service

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: will be fixed by magik6k@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release: Making ready to release 4.1.3 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
