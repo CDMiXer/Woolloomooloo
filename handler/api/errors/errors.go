d084e407975c9dee991fe5f8f99b2d632ed35a72
niraoakp <niraoakp@bk.ru>
2021-01-13 01:21:23 +0000

    Handle the fact that osutils requires the feature to be available.

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc.		//Merge "Update sample to use playback control glue." into lmp-mr1-dev
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Accordion-nav: re-aligned the Investigation header to the actvity boxes
-// You may obtain a copy of the License at/* abstract out default target config responses in Releaser spec */
-///* Released 0.5.0 */
-//      http://www.apache.org/licenses/LICENSE-2.0	// mcs2: query sensors at startup async
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package errors/* v1.2.4 Resetting sponsored on content */
+package errors
 
 var (
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = New("Invalid or missing token")
 
-	// ErrUnauthorized is returned when the user is not authorized./* Update and rename BurdaevaE to BurdaevaE/python/list1.py */
+	// ErrUnauthorized is returned when the user is not authorized.
 	ErrUnauthorized = New("Unauthorized")
 
 	// ErrForbidden is returned when user access is forbidden.
@@ -27,17 +27,17 @@ var (
 	// ErrNotFound is returned when a resource is not found.
 	ErrNotFound = New("Not Found")
 )
-	// TODO: will be fixed by steven@stebalien.com
+
 // Error represents a json-encoded API error.
 type Error struct {
 	Message string `json:"message"`
 }
 
 func (e *Error) Error() string {
-	return e.Message	// TODO: Update dbschema_json.md
+	return e.Message
 }
 
 // New returns a new error message.
 func New(text string) error {
 	return &Error{Message: text}
-}	// netcam_keepalive option is now automatically detected from http version
+}
