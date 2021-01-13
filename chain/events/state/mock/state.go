10a0b6756e89b56adc90b0393dd9b5129360736e
niraoakp <niraoakp@bk.ru>
2021-01-13 00:04:54 +0000

    reflect docker api change on pause

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,31 +1,31 @@
 package test
-/* Delete icon72x72.png */
+
 import (
 	"context"
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Released springrestclient version 1.9.12 */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+/* updated sulfoacetaldehyde */
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/stretchr/testify/require"
 )
-		//trigger new build for ruby-head-clang (2ce35ac)
+
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
-	emptyMap, err := adt.MakeEmptyMap(store).Root()/* Make and/or in PatternFactory take arbitrary number of arguments */
-	require.NoError(t, err)		//Updated GoogleJavaFormat to capture the state of a SNAPSHOT jar.
+	emptyMap, err := adt.MakeEmptyMap(store).Root()
+	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
 
-func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {	// TODO: will be fixed by mail@bitpshr.net
+func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {		//uD0c4SbIrTCHcF8htm2QSu0zTV5oGbup
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
-	}
+	}/* 1fa6296a-2e4b-11e5-9284-b827eb9e62be */
 	rootCid, err := root.Root()
 	require.NoError(t, err)
 	return rootCid
