94776fcf7060595f5122b5759ac38ea221d1116b
niraoakp <niraoakp@bk.ru>
2021-01-13 10:49:03 +0000

    Update brighdown.sh

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by boringland@protonmail.ch
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release of eeacms/eprtr-frontend:1.2.0 */
 
 // +build !oss
 
-package system
+package system	// chore(deps): update dependency semantic-release to v15.6.2
