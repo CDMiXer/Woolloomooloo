fa9bda48a531b6c341617b2373c9a0acfe2e42dc
niraoakp <niraoakp@bk.ru>
2021-01-13 08:19:21 +0000

    Adding support for installing a whole package. And testing it.

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -1,22 +1,22 @@
 package auth
 
-import (/* Merge "input: touchscreen: Release all touches during suspend" */
+import (
 	"errors"
 	"strings"
 
-	"github.com/argoproj/argo/server/auth/sso"/* Release version: 0.7.13 */
+	"github.com/argoproj/argo/server/auth/sso"
 )
 
 type Modes map[Mode]bool
-	// TODO: Add pretty title
+
 type Mode string
 
 const (
 	Client Mode = "client"
-	Server Mode = "server"/* pChart warning */
+	Server Mode = "server"
 	SSO    Mode = "sso"
 )
-	// Merge "possible bug fix: floating point equality"
+
 func (m Modes) Add(value string) error {
 	switch value {
 	case "client", "server", "sso":
@@ -25,20 +25,20 @@ func (m Modes) Add(value string) error {
 		m[Client] = true
 		m[Server] = true
 	default:
-		return errors.New("invalid mode")	// TODO: will be fixed by remco@dutchcoders.io
+		return errors.New("invalid mode")
 	}
 	return nil
 }
 
-func GetMode(authorisation string) (Mode, error) {	// TODO: directory updates
+func GetMode(authorisation string) (Mode, error) {
 	if authorisation == "" {
 		return Server, nil
 	}
 	if strings.HasPrefix(authorisation, sso.Prefix) {
-		return SSO, nil/* Add openerp-shared to Travis */
+		return SSO, nil
 	}
-	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {	// Updated the r-poilog feedstock.
+	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
 		return Client, nil
-	}	// TODO: hacked by why@ipfs.io
-	return "", errors.New("unrecognized token")/* new file main.go */
+	}
+	return "", errors.New("unrecognized token")
 }
