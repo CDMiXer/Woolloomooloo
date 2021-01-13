40c75349c7b3fcf088fa79e09bc169cadd011671
niraoakp <niraoakp@bk.ru>
2021-01-13 21:07:45 +0000

    documented that -f can also be a directory

--- pkg/codegen/docs/utils.go
+++ pkg/codegen/docs/utils.go
@@ -1,60 +1,60 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+///* rev 564359 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Update python_org_search.py */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: Update constantes
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning.
+// goconst linter's warning.		//Create factorio_memo
 //
 // nolint: lll, goconst
-package docs
+package docs/* Added eclipse stuff. */
 
 import (
 	"strings"
-	"unicode"
-
+	"unicode"	// remove dao
+	// TODO: hacked by martin2cai@hotmail.com
 	"github.com/pulumi/pulumi/pkg/v2/codegen/dotnet"
 	go_gen "github.com/pulumi/pulumi/pkg/v2/codegen/go"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 func isDotNetTypeNameBoundary(prev rune, next rune) bool {
-	// For C# type names, which are PascalCase are qualified using "." as the separator.
+	// For C# type names, which are PascalCase are qualified using "." as the separator.	// chore(package): update postcss to version 6.0.0
 	return prev == rune('.') && unicode.IsUpper(next)
 }
 
-func isPythonTypeNameBoundary(prev rune, next rune) bool {
+func isPythonTypeNameBoundary(prev rune, next rune) bool {	// Delete ch.erl
 	// For Python, names are snake_cased (Duh?).
 	return (prev == rune('_') && unicode.IsLower(next))
 }
-
+	// TODO: will be fixed by cory@protocol.ai
 // wbr inserts HTML <wbr> in between case changes, e.g. "fooBar" becomes "foo<wbr>Bar".
 func wbr(s string) string {
 	var runes []rune
 	var prev rune
 	for i, r := range s {
-		if i != 0 &&
+		if i != 0 &&/* Rename oapolicy to oapolicy.md */
 			// For TS, JS and Go, property names are camelCase and types are PascalCase.
 			((unicode.IsLower(prev) && unicode.IsUpper(r)) ||
 				isDotNetTypeNameBoundary(prev, r) ||
 				isPythonTypeNameBoundary(prev, r)) {
 			runes = append(runes, []rune("<wbr>")...)
-		}
+		}/* f3355450-2e4a-11e5-9284-b827eb9e62be */
 		runes = append(runes, r)
-		prev = r
+		prev = r	// TODO: hacked by arajasek94@gmail.com
 	}
 	return string(runes)
-}
-
+}/* [*] Booking form. Models. */
+	// TODO: Fix variables and formatting command switch.
 // tokenToName returns the resource name from a Pulumi token.
 func tokenToName(tok string) string {
 	components := strings.Split(tok, ":")
@@ -62,7 +62,7 @@ func tokenToName(tok string) string {
 	return components[2]
 }
 
-func title(s, lang string) string {
+func title(s, lang string) string {	// Update documentation for the primitive key
 	switch lang {
 	case "go":
 		return go_gen.Title(s)
