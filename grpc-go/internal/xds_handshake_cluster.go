149df2db6058bb7b295cd87af10d0a85faee22ce
niraoakp <niraoakp@bk.ru>
2021-01-13 02:41:42 +0000

    Merge "Fixed last merge file from "tests" folder to new "api" folder"

--- grpc-go/internal/xds_handshake_cluster.go
+++ grpc-go/internal/xds_handshake_cluster.go
@@ -1,26 +1,26 @@
-/*/* Release stage broken in master. Remove it for side testing. */
- * Copyright 2021 gRPC authors./* Released 1.0rc1. */
+/*/* Released 3.1.1 with a fixed MANIFEST.MF. */
+ * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by peterke@gmail.com
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added a UI component to display notifications.
+ */* Release 0.3.7.7. */
+ * Unless required by applicable law or agreed to in writing, software/* Releases link added. */
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Update GeoChart.php
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+	// TODO: Cucumber setup
 package internal
 
 import (
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
-)
-/* Create ModuleJoinRangeFunction.bas */
+)	// delay openfiles tabManager's event-dependant updates and cancel double updates
+
 // handshakeClusterNameKey is the type used as the key to store cluster name in
 // the Attributes field of resolver.Address.
 type handshakeClusterNameKey struct{}
@@ -29,12 +29,12 @@ type handshakeClusterNameKey struct{}
 // is updated with the cluster name.
 func SetXDSHandshakeClusterName(addr resolver.Address, clusterName string) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(handshakeClusterNameKey{}, clusterName)
-	return addr
+	return addr		//HTTP -> HTTPS (thanks, Stefan)
 }
 
 // GetXDSHandshakeClusterName returns cluster name stored in attr.
 func GetXDSHandshakeClusterName(attr *attributes.Attributes) (string, bool) {
 	v := attr.Value(handshakeClusterNameKey{})
-	name, ok := v.(string)
+	name, ok := v.(string)		//Update all_sprites.rs
 	return name, ok
-}	// TODO: will be fixed by mail@bitpshr.net
+}
