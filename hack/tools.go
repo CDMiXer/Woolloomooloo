aa8f0f61ba11640c500bfa71b01a61cbd7b4e4de
niraoakp <niraoakp@bk.ru>
2021-01-13 02:09:14 +0000

    PHPDoc (davux)

--- hack/tools.go
+++ hack/tools.go
@@ -1,31 +1,31 @@
 // +build tools
-		//Fixed some command output
+/* Añadido servicios de Cita y Horario iniciales */
 // This package contains code generation utilities
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
 
-import (		//Actual README
-"selifcitats/ek.uob" _	
+import (
+	_ "bou.ke/staticfiles"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "github.com/jstemmer/go-junit-report"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"/* Released 1.11,add tag. */
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"		//new option added for passing oechem license as argument.
+	_ "github.com/jstemmer/go-junit-report"	// TODO: [DDW-81] fix ada redemption menu logic
 	_ "github.com/mattn/goreman"
-"stropmiog/dmc/sloot/x/gro.gnalog" _	
+	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/code-generator/cmd/defaulter-gen"/* Release 0.7.1.2 */
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/openapi-gen"
 	_ "k8s.io/code-generator/cmd/register-gen"
-	_ "k8s.io/code-generator/cmd/set-gen"
+	_ "k8s.io/code-generator/cmd/set-gen"	// TODO: MEDIUM / Fixed AddExcelCell inspector
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
