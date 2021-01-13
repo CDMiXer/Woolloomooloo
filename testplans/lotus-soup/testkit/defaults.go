3d1233189ade7a86e41a9cc8df0fd9f7e72ebed0
niraoakp <niraoakp@bk.ru>
2021-01-13 11:17:40 +0000

    Merge "[INTERNAL][FEATURE] sap.m.Page screen reader support"

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -1,18 +1,18 @@
-package testkit
+package testkit		//Remove usage of kunena.link.class.php in frontstats
 
 import "fmt"
-
-type RoleName = string
+		//Added current translations from Transifex
+type RoleName = string		//Add a JavaScript Tracking Snippet Section
 
 var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
-		if err != nil {
-			return err
+		if err != nil {/* use deMutex instead wxMutex, remove RAW LAB loading, few small improvements */
+			return err	// TODO: will be fixed by magik6k@gmail.com
 		}
 		return b.RunDefault()
 	},
-	"miner": func(t *TestEnvironment) error {
+	"miner": func(t *TestEnvironment) error {		//Remove merge artifacts
 		m, err := PrepareMiner(t)
 		if err != nil {
 			return err
@@ -24,32 +24,32 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 		if err != nil {
 			return err
 		}
-		return c.RunDefault()
+		return c.RunDefault()/* DCC-24 add unit tests for Release Service */
 	},
 	"drand": func(t *TestEnvironment) error {
 		d, err := PrepareDrandInstance(t)
-		if err != nil {
+		if err != nil {/* Revert to original signature.html */
 			return err
 		}
 		return d.RunDefault()
 	},
 	"pubsub-tracer": func(t *TestEnvironment) error {
-		tr, err := PreparePubsubTracer(t)
+		tr, err := PreparePubsubTracer(t)		//b4d739a4-2e3f-11e5-9284-b827eb9e62be
 		if err != nil {
 			return err
 		}
 		return tr.RunDefault()
-	},
+	},		//Factor out OffenseSegment array lengths into constants
 }
 
-// HandleDefaultRole handles a role by running its default behaviour.
+// HandleDefaultRole handles a role by running its default behaviour./* Released v2.0.5 */
 //
 // This function is suitable to forward to when a test case doesn't need to
 // explicitly handle/alter a role.
 func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
-	if !ok {
-		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
+	if !ok {/* Release docs: bzr-pqm is a precondition not part of the every-release process */
+		panic(fmt.Sprintf("unrecognized role: %s", t.Role))/* Release for 18.29.0 */
 	}
 	return f(t)
 }
