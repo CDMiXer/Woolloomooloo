03e42c65ea7c85572dee6e73a0687655e4b43606
niraoakp <niraoakp@bk.ru>
2021-01-13 00:40:44 +0000

    Update HttpServer.kt

--- handler/web/varz.go
+++ handler/web/varz.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* opennlp ner */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,25 +6,25 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//[279. Perfect Squares][Accepted]committed by Victor
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//pmusic: bugfix: read 'comment' meta tag
 // limitations under the License.
 
-package web
+package web/* Automatic changelog generation for PR #24135 [ci skip] */
 
 import (
 	"net/http"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* SF v3.6 Release */
 	"github.com/drone/go-scm/scm"
 )
 
 type varz struct {
-	SCM     *scmInfo     `json:"scm"`
-	License *licenseInfo `json:"license"`
+	SCM     *scmInfo     `json:"scm"`/* Convert ReleaseFactory from old logger to new LOGGER slf4j */
+	License *licenseInfo `json:"license"`		//cleanup, admin-modul
 }
 
 type scmInfo struct {
@@ -36,10 +36,10 @@ type rateInfo struct {
 	Limit     int   `json:"limit"`
 	Remaining int   `json:"remaining"`
 	Reset     int64 `json:"reset"`
-}
+}		//d3858fb8-2e4e-11e5-9284-b827eb9e62be
 
 type licenseInfo struct {
-	Kind       string    `json:"kind"`
+	Kind       string    `json:"kind"`	// Keep scroll position on soft wrap toggle
 	Seats      int64     `json:"seats"`
 	SeatsUsed  int64     `json:"seats_used,omitempty"`
 	SeatsAvail int64     `json:"seats_available,omitempty"`
@@ -47,7 +47,7 @@ type licenseInfo struct {
 	ReposUsed  int64     `json:"repos_used,omitempty"`
 	ReposAvail int64     `json:"repos_available,omitempty"`
 	Expires    time.Time `json:"expire_at,omitempty"`
-}
+}		//Corrected a bug with the Convertor locating code
 
 // HandleVarz creates an http.HandlerFunc that exposes internal system
 // information.
@@ -63,12 +63,12 @@ func HandleVarz(client *scm.Client, license *core.License) http.HandlerFunc {
 			},
 			SCM: &scmInfo{
 				URL: client.BaseURL.String(),
-				Rate: &rateInfo{
+				Rate: &rateInfo{	// TODO: remove 'Preferences' from dialog title, as discussed on ml
 					Limit:     rate.Limit,
 					Remaining: rate.Remaining,
 					Reset:     rate.Reset,
 				},
-			},
+			},		//Apocalyptic mod. No GC-related classes. No world generator.
 		}
 		writeJSON(w, v, 200)
 	}
