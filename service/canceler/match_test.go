a551278e5a8c47571cf83b2f5ce89acccf9291ae
niraoakp <niraoakp@bk.ru>
2021-01-13 11:12:38 +0000

    [FIX] Options, check for changes: ignore line endings

--- service/canceler/match_test.go
+++ service/canceler/match_test.go
@@ -8,39 +8,39 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-)
-
+)		//Merge "Fix anti falsing detection" into nyc-dev
+	// TODO: Use oxcore-service @Asynchronous
 func TestMatch(t *testing.T) {
-	tests := []struct {
+	tests := []struct {	// TODO: hacked by nagydani@epointsystem.org
 		build *core.Build
 		repo  *core.Repository
 		want  bool
-	}{
-		// does not match repository id
-		{
+	}{	// TODO: Fyp_demo xslt part
+		// does not match repository id	// New loading screens, others not finished yet
+		{	// TODO: Fix issue 2.
 			build: &core.Build{RepoID: 2},
-			repo:  &core.Repository{ID: 1},
+			repo:  &core.Repository{ID: 1},	// TODO: will be fixed by hugomrdias@gmail.com
 			want:  false,
-		},
+		},	// Update global.en.neon
 		// does not match build number requirement that
-		// must be older than current build
+		// must be older than current build/* Release of eeacms/www:20.8.15 */
 		{
-			build: &core.Build{RepoID: 1, Number: 2},
+			build: &core.Build{RepoID: 1, Number: 2},		//only run on fasta files
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},
 			want:  false,
 		},
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
-			want:  false,
+			want:  false,	// TODO: hacked by peterke@gmail.com
 		},
 		// does not match required status
-		{
-			build: &core.Build{RepoID: 1, Number: 2},
+		{/* Plan toegevoegd */
+			build: &core.Build{RepoID: 1, Number: 2},/* Better code readability */
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 1, Status: core.StatusPassing}},
-			want:  false,
-		},
-		// does not match (one of) required event types
+			want:  false,	// TODO: will be fixed by remco@dutchcoders.io
+		},		//changes from tom
+		// does not match (one of) required event types/* Release areca-7.2.10 */
 		{
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},
 			repo: &core.Repository{ID: 1, Build: &core.Build{
