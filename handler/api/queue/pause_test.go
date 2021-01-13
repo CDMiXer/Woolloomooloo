597b6d1ceab8182ecf1bd2c97025ec681740aaaf
niraoakp <niraoakp@bk.ru>
2021-01-13 08:17:09 +0000

    f8b532b2-2e48-11e5-9284-b827eb9e62be

--- handler/api/queue/pause_test.go
+++ handler/api/queue/pause_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release 4.0.0-beta2 */
+// that can be found in the LICENSE file.
 
-// +build !oss		//Delete pecanCSS.css
+// +build !oss
 
-eueuq egakcap
+package queue
