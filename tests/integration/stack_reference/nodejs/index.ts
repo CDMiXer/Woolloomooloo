fe4fb30c6412d24b928d00ee4abbfb3e8cdd4e00
niraoakp <niraoakp@bk.ru>
2021-01-13 04:47:06 +0000

    add links to SIGM index

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Minor tweaks to linked parameteres. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
-let org = config.require("org");/* Merge "ARM: dts: msm: Update TZ apps region for msm8952" */
+let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-/* engines restore */
+
 export const val = ["a", "b"];
