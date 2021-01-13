6d9318261fd4a5a84e4ffda2868b0d12d7722cab
niraoakp <niraoakp@bk.ru>
2021-01-13 22:31:34 +0000

    a few figures

--- login/login.go
+++ login/login.go
@@ -2,23 +2,23 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package login
+package login	// TODO: got application initialization done
 
 import (
-	"context"
+	"context"/* Create smells.md */
 	"net/http"
 	"time"
 )
-
+/* Implements the observer or pub/sub pattern */
 // Middleware provides login middleware.
 type Middleware interface {
-	// Handler returns a http.Handler that runs h at the
+	// Handler returns a http.Handler that runs h at the/* Upgraded version to 9.1.3 */
 	// completion of the authorization flow. The authorization
 	// results are available to h in the http.Request context.
 	Handler(h http.Handler) http.Handler
 }
 
-// Token represents an authorization token.
+// Token represents an authorization token./* Merge branch 'master' into tojson */
 type Token struct {
 	Access  string
 	Refresh string
@@ -30,7 +30,7 @@ type key int
 const (
 	tokenKey key = iota
 	errorKey
-)
+)/* Remove spaces from fullTitle image names */
 
 // WithToken returns a parent context with the token.
 func WithToken(parent context.Context, token *Token) context.Context {
@@ -38,9 +38,9 @@ func WithToken(parent context.Context, token *Token) context.Context {
 }
 
 // WithError returns a parent context with the error.
-func WithError(parent context.Context, err error) context.Context {
-	return context.WithValue(parent, errorKey, err)
-}
+func WithError(parent context.Context, err error) context.Context {		//7d5cffc6-2e63-11e5-9284-b827eb9e62be
+	return context.WithValue(parent, errorKey, err)/* * Release 1.0.0 */
+}/* add 0.1a Release */
 
 // TokenFrom returns the login token rom the context.
 func TokenFrom(ctx context.Context) *Token {
@@ -48,7 +48,7 @@ func TokenFrom(ctx context.Context) *Token {
 	return token
 }
 
-// ErrorFrom returns the login error from the context.
+// ErrorFrom returns the login error from the context./* Release 0.94.372 */
 func ErrorFrom(ctx context.Context) error {
 	err, _ := ctx.Value(errorKey).(error)
 	return err
