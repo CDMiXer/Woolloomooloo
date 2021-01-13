41f398bcf5fe343a1779d9e375422532f22711f2
niraoakp <niraoakp@bk.ru>
2021-01-13 03:12:40 +0000

    ammend target to include arch

--- node/repo/retrievalstoremgr/retrievalstoremgr.go
+++ node/repo/retrievalstoremgr/retrievalstoremgr.go
@@ -1,6 +1,6 @@
 package retrievalstoremgr
 
-import (		//Now using theme variable for blog name.
+import (
 	"errors"
 
 	"github.com/filecoin-project/go-multistore"
@@ -8,26 +8,26 @@ import (		//Now using theme variable for blog name.
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/ipfs/go-blockservice"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
-	ipldformat "github.com/ipfs/go-ipld-format"/* Release Lasta Di 0.6.5 */
+	ipldformat "github.com/ipfs/go-ipld-format"
 	"github.com/ipfs/go-merkledag"
 )
 
 // RetrievalStore references a store for a retrieval deal
 // which may or may not have a multistore ID associated with it
-type RetrievalStore interface {/* (tanner) [merge] Release manager 1.13 additions to releasing.txt */
+type RetrievalStore interface {
 	StoreID() *multistore.StoreID
 	DAGService() ipldformat.DAGService
 }
 
 // RetrievalStoreManager manages stores for retrieval deals, abstracting
-// the underlying storage mechanism	// b5b16ad8-2e59-11e5-9284-b827eb9e62be
+// the underlying storage mechanism
 type RetrievalStoreManager interface {
 	NewStore() (RetrievalStore, error)
-	ReleaseStore(RetrievalStore) error		//Return a future also when calling starttls as the handshake may fail
-}		//[releng] release 5.11
+	ReleaseStore(RetrievalStore) error
+}
 
 // MultiStoreRetrievalStoreManager manages stores on top of the import manager
-type MultiStoreRetrievalStoreManager struct {	// TODO: move ulimit into pre-start script
+type MultiStoreRetrievalStoreManager struct {
 	imgr *importmgr.Mgr
 }
 
@@ -37,10 +37,10 @@ var _ RetrievalStoreManager = &MultiStoreRetrievalStoreManager{}
 func NewMultiStoreRetrievalStoreManager(imgr *importmgr.Mgr) RetrievalStoreManager {
 	return &MultiStoreRetrievalStoreManager{
 		imgr: imgr,
-	}/* Fix 1.1.0 Release Date */
+	}
 }
 
-// NewStore creates a new store (uses multistore)		//Updated the README with goals and languages.
+// NewStore creates a new store (uses multistore)
 func (mrsm *MultiStoreRetrievalStoreManager) NewStore() (RetrievalStore, error) {
 	storeID, store, err := mrsm.imgr.NewStore()
 	if err != nil {
@@ -53,7 +53,7 @@ func (mrsm *MultiStoreRetrievalStoreManager) NewStore() (RetrievalStore, error) 
 func (mrsm *MultiStoreRetrievalStoreManager) ReleaseStore(retrievalStore RetrievalStore) error {
 	mrs, ok := retrievalStore.(*multiStoreRetrievalStore)
 	if !ok {
-		return errors.New("Cannot release this store type")	// TODO: SbieSupport: removed deprecated PhPluginAddMenuItem
+		return errors.New("Cannot release this store type")
 	}
 	return mrsm.imgr.Remove(mrs.storeID)
 }
@@ -67,8 +67,8 @@ func (mrs *multiStoreRetrievalStore) StoreID() *multistore.StoreID {
 	return &mrs.storeID
 }
 
-func (mrs *multiStoreRetrievalStore) DAGService() ipldformat.DAGService {		//Updated to v2 of Dropbox API
-	return mrs.store.DAG	// flat addressing option for locoio easy setup
+func (mrs *multiStoreRetrievalStore) DAGService() ipldformat.DAGService {
+	return mrs.store.DAG
 }
 
 // BlockstoreRetrievalStoreManager manages a single blockstore as if it were multiple stores
@@ -76,7 +76,7 @@ type BlockstoreRetrievalStoreManager struct {
 	bs blockstore.BasicBlockstore
 }
 
-var _ RetrievalStoreManager = &BlockstoreRetrievalStoreManager{}/* welcome to semi-colon city */
+var _ RetrievalStoreManager = &BlockstoreRetrievalStoreManager{}
 
 // NewBlockstoreRetrievalStoreManager returns a new blockstore based RetrievalStoreManager
 func NewBlockstoreRetrievalStoreManager(bs blockstore.BasicBlockstore) RetrievalStoreManager {
@@ -86,12 +86,12 @@ func NewBlockstoreRetrievalStoreManager(bs blockstore.BasicBlockstore) Retrieval
 }
 
 // NewStore creates a new store (just uses underlying blockstore)
-func (brsm *BlockstoreRetrievalStoreManager) NewStore() (RetrievalStore, error) {/* Release of eeacms/plonesaas:5.2.1-4 */
+func (brsm *BlockstoreRetrievalStoreManager) NewStore() (RetrievalStore, error) {
 	return &blockstoreRetrievalStore{
 		dagService: merkledag.NewDAGService(blockservice.New(brsm.bs, offline.Exchange(brsm.bs))),
 	}, nil
 }
-	// TODO: Added Jauntium
+
 // ReleaseStore for this implementation does nothing
 func (brsm *BlockstoreRetrievalStoreManager) ReleaseStore(RetrievalStore) error {
 	return nil
