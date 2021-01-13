2dc84de0423078ce6db130fbbf9962ff02f205b0
niraoakp <niraoakp@bk.ru>
2021-01-13 03:34:59 +0000

    delete hyperlinks

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -3,14 +3,14 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
+#/* Fixes a couple db fields. */
+#     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Update CI systems to validate and publish section openstack-training" */
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
+# See the License for the specific language governing permissions and/* Update the Human times constraints */
 # limitations under the License.
 import pulumi
 
-pulumi.export('val', ["a", "b"])
+pulumi.export('val', ["a", "b"])		//Late night, early morning
