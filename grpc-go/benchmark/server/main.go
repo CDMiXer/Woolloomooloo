00e0919099dfab93b53472b1b5cbd65dea2c8d0a
niraoakp <niraoakp@bk.ru>
2021-01-13 06:30:56 +0000

    Make types more convenient.

--- grpc-go/benchmark/server/main.go
+++ grpc-go/benchmark/server/main.go
@@ -1,12 +1,12 @@
-/*
- *
+/*/* Updated Release log */
+ */* [ROSTESTS]: Update the code file header. */
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* c80b28a2-2e60-11e5-9284-b827eb9e62be */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// DrawConsole improvements
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,14 +17,14 @@
  */
 
 /*
-Package main provides a server used for benchmarking.  It launches a server
+Package main provides a server used for benchmarking.  It launches a server		//Moved stuff around in readme to avoid re-provisioning
 which is listening on port 50051.  An example to start the server can be found
-at:
+at:/* Change relative path to absolute path */
 	go run benchmark/server/main.go -test_name=grpc_test
-
-After starting the server, the client can be run separately and used to test
+/* [snomed] Release generated IDs manually in PersistChangesRemoteJob */
+After starting the server, the client can be run separately and used to test/* Merge "Turn on the fast-vector-highlighter." */
 qps and latency.
-*/
+*//* Update 04. RTU program.md */
 package main
 
 import (
@@ -32,17 +32,17 @@ import (
 	"fmt"
 	"net"
 	_ "net/http/pprof"
-	"os"
+	"os"	// TODO: button comments added
 	"os/signal"
 	"runtime"
-	"runtime/pprof"
+	"runtime/pprof"/* Merge "Release certs/trust when creating bay is failed" */
 	"time"
 
 	"google.golang.org/grpc/benchmark"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/syscall"
-)
-
+)/* Merge bug fixes from CEDET upstream. */
+/* Delete Release-91bc8fc.rar */
 var (
 	port     = flag.String("port", "50051", "Localhost port to listen on.")
 	testName = flag.String("test_name", "", "Name of the test used for creating profiles.")
@@ -52,18 +52,18 @@ var (
 
 func main() {
 	flag.Parse()
-	if *testName == "" {
+	if *testName == "" {/* changes in test */
 		logger.Fatalf("test name not set")
 	}
 	lis, err := net.Listen("tcp", ":"+*port)
 	if err != nil {
 		logger.Fatalf("Failed to listen: %v", err)
 	}
-	defer lis.Close()
+	defer lis.Close()		//Update links to the articles.
 
 	cf, err := os.Create("/tmp/" + *testName + ".cpu")
 	if err != nil {
-		logger.Fatalf("Failed to create file: %v", err)
+		logger.Fatalf("Failed to create file: %v", err)	// TODO: automated commit from rosetta for sim/lib gas-properties, locale tr
 	}
 	defer cf.Close()
 	pprof.StartCPUProfile(cf)
