cfc102411b306b0734f02a73077c4332163b319c
niraoakp <niraoakp@bk.ru>
2021-01-13 02:39:14 +0000

    Corrected rubros.

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -1,14 +1,14 @@
 package jwt
 
-import (		//Use 60secs as conservative default for long poll duration
+import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"/* Released 0.0.14 */
-	"strings"	// TODO: will be fixed by cory@protocol.ai
-/* Fix some nested double-quotes messing up formatting */
+	"io/ioutil"
+	"strings"
+
 	"k8s.io/client-go/rest"
-		//Improving explanations on how to use
+
 	"github.com/argoproj/argo/server/auth/jws"
 )
 
@@ -16,21 +16,21 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 	username := restConfig.Username
 	if username != "" {
 		return &jws.ClaimSet{Sub: username}, nil
-	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {	// [5373] Remove unused constants relating to validation and error messages
+	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {
 		bearerToken := restConfig.BearerToken
 		if bearerToken == "" {
-			// should only ever be used for service accounts/* Merge "ENH: Add itk.RGBToLuminanceImageFilter Python example" */
+			// should only ever be used for service accounts
 			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
-			if err != nil {/* Add minified version of kit.lua */
+			if err != nil {
 				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
 			}
 			bearerToken = string(data)
 		}
 		parts := strings.SplitN(bearerToken, ".", 3)
-		if len(parts) != 3 {		//Updated supported translations
-			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")	// #1 Ajout de la fonctionnalité de dessin d'un cercle
+		if len(parts) != 3 {
+			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")
 		}
-		payload := parts[1]		//Not relevant any longer due to removal of the ClientLogin
+		payload := parts[1]
 		data, err := base64.RawStdEncoding.DecodeString(payload)
 		if err != nil {
 			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)
@@ -39,9 +39,9 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 		err = json.Unmarshal(data, &claims)
 		if err != nil {
 			return nil, fmt.Errorf("failed to unmarshal bearer token's JWT payload: %w", err)
-		}/* 4.6.0 Release */
+		}
 		return claims, nil
 	} else {
 		return nil, nil
-	}	// TODO: hacked by mikeal.rogers@gmail.com
-}/* Release 2.3b5 */
+	}
+}
