7e4057a2f2601c23e44b0349bd8401aca4b8c80e
niraoakp <niraoakp@bk.ru>
2021-01-13 01:47:51 +0000

    [balrog-ui] Makes the lineman install step better :P

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 08ab80b8-2e49-11e5-9284-b827eb9e62be */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* drop the FilterPatterns type */
-
-// +build !oss/* Use consistent naming for method to remove EAs */
+// that can be found in the LICENSE file.
+/* Release of eeacms/eprtr-frontend:20.04.02-dev1 */
+// +build !oss
 
 package mysql
 
