228cbeebaf69bbd9632290938ba3f3ceb112f81e
niraoakp <niraoakp@bk.ru>
2021-01-13 11:24:47 +0000

    Include resource files to operators extracted with spl-python-extract.py

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,38 +1,38 @@
-// Create a new security group for port 80./* Argument overloaded tested and added to the testsuit */
+// Create a new security group for port 80.
 resource securityGroup "aws:ec2:SecurityGroup" {
 	ingress = [{
-		protocol = "tcp"/* Merge "Release 1.0.0.64 & 1.0.0.65 QCACLD WLAN Driver" */
+		protocol = "tcp"
 		fromPort = 0
-		toPort = 0	// TODO: 01f52028-2e47-11e5-9284-b827eb9e62be
+		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
 }
 
-// Get the ID for the latest Amazon Linux AMI.		//Increase timeout for incremental changelog test
+// Get the ID for the latest Amazon Linux AMI.
 ami = invoke("aws:index:getAmi", {
 	filters = [{
 		name = "name"
 		values = ["amzn-ami-hvm-*-x86_64-ebs"]
 	}]
 	owners = ["137112412989"] // Amazon
-	mostRecent = true/* Prepare next Release */
+	mostRecent = true
 })
 
 // Create a simple web server using the startup script for the instance.
 resource server "aws:ec2:Instance" {
-	tags = {/* Supertab is superseded by YCM */
-"www-revres-bew" = emaN		
-	}		//Fix typo in bind_authentification_type config
+	tags = {
+		Name = "web-server-www"
+	}
 	instanceType = "t2.micro"
-	securityGroups = [securityGroup.name]		//Update SlugOptions.php
+	securityGroups = [securityGroup.name]
 	ami = ami.id
 	userData = <<-EOF
 		#!/bin/bash
-		echo "Hello, World!" > index.html/* Release Process Restart: Change pom version to 2.1.0-SNAPSHOT */
+		echo "Hello, World!" > index.html
 		nohup python -m SimpleHTTPServer 80 &
 	EOF
 }
 
 // Export the resulting server's IP address and DNS name.
-output publicIp { value = server.publicIp }/* updated spelling. */
+output publicIp { value = server.publicIp }
 output publicHostName { value = server.publicDns }
