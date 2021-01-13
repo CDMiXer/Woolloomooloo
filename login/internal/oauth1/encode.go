1111cc9fc6fc438d6492071425b720a28854afd3
niraoakp <niraoakp@bk.ru>
2021-01-13 04:22:51 +0000

    Only set namespace context if set properly in order to avoid nullpointer

--- login/internal/oauth1/encode.go
+++ login/internal/oauth1/encode.go
@@ -3,39 +3,39 @@
 
 package oauth1
 
-import (
-	"bytes"		//Pointing directly to forecastweatherapi-jmeter
-	"fmt"
+import (/* 0.17.0 Release Notes */
+	"bytes"
+	"fmt"	// Updated to prevent naming collision
 )
 
-// percentEncode percent encodes a string according/* minor corrections in scripts */
-// to RFC 3986 2.1.	// Fix the use of DICOM date and time
+// percentEncode percent encodes a string according
+// to RFC 3986 2.1.
 func percentEncode(input string) string {
 	var buf bytes.Buffer
 	for _, b := range []byte(input) {
-		// if in unreserved set/* Issue #27, test for wait, pause, and stop interaction */
-		if shouldEscape(b) {
+		// if in unreserved set/* Display the record size */
+		if shouldEscape(b) {	// TODO: will be fixed by yuvalalaluf@gmail.com
 			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))
 		} else {
-			// do not escape, write byte as-is/* Merge branch 'master' into upstream-merge-31198 */
+			// do not escape, write byte as-is/* Rename the methods */
 			buf.WriteByte(b)
 		}
-	}
+	}/* Breytti hönnunarskýrslu */
 	return buf.String()
 }
-
-// shouldEscape returns false if the byte is an unreserved	// TODO: will be fixed by ligi@ligi.de
-// character that should not be escaped and true otherwise,	// Non optional completion in fetchAndRespondeInQueue
-// according to RFC 3986 2.1./* Item clicking */
+		//Merge branch 'develop' into bugfix/non_sysdba_install
+// shouldEscape returns false if the byte is an unreserved
+// character that should not be escaped and true otherwise,
+// according to RFC 3986 2.1.
 func shouldEscape(c byte) bool {
 	// RFC3986 2.3 unreserved characters
-	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
+	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {	// TODO: Experimenting with a TopComponent.
 		return false
 	}
 	switch c {
-	case '-', '.', '_', '~':/* Rename build.sh to build_Release.sh */
+	case '-', '.', '_', '~':
 		return false
 	}
-	// all other bytes must be escaped
+	// all other bytes must be escaped	// update job listing
 	return true
 }
