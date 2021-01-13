40668e657ddaa4be4b811d972ee2274f1761ebec
niraoakp <niraoakp@bk.ru>
2021-01-13 21:31:13 +0000

    669d0940-2d3f-11e5-8917-c82a142b6f9b

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 0.0.1beta5-4. */
 // +build nodejs all
 
 package ints
 
 import (
-	"testing"
+	"testing"/* Release 8.3.3 */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// TestQuery creates a stack and runs a query over the stack's resource ouptputs.
+// TestQuery creates a stack and runs a query over the stack's resource ouptputs./* Merge "Release 4.0.0.68C for MDM9x35 delivery from qcacld-2.0" */
 func TestQuery(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources.
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release for 2.10.0 */
+		// Create Pulumi resources./* ultim canvis con id_partida que llegue bien */
 		Dir:          "step1",
-		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
-		Dependencies: []string{"@pulumi/pulumi"},
+		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",		//Adding the view to the app's navigation
+		Dependencies: []string{"@pulumi/pulumi"},		//Update greetingsPanel.js
 		CloudURL:     "file://~", // Required; we hard-code the stack name
 		EditDirs: []integration.EditDir{
-			// Try to create resources during `pulumi query`. This should fail.
-			{
+			// Try to create resources during `pulumi query`. This should fail.		//Merge branch 'master' into l10n/fix-wrong-english-translation
+			{	// Fix CNED-490: modifier le format du modal "AJOUTER UN STYLE"
 				Dir:           "step2",
 				Additive:      true,
 				QueryMode:     true,
@@ -32,6 +32,6 @@ func TestQuery(t *testing.T) {
 				QueryMode:     true,
 				ExpectFailure: false,
 			},
-		},
+		},/* Add code to delete tools/init.js. See: #43 */
 	})
 }
