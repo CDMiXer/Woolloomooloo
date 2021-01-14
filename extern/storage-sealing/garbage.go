1ce1a39a565d440f4e23e994f60fbef2adaa81f3
niraoakp <niraoakp@bk.ru>
2021-01-14 04:28:30 +0000

    Oppdatert med informasjon om lisens

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -4,15 +4,15 @@ import (
 	"context"
 
 	"golang.org/x/xerrors"
-
+/* 65% -> 70% */
 	"github.com/filecoin-project/specs-storage/storage"
 )
 
 func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	m.inputLk.Lock()
 	defer m.inputLk.Unlock()
-
-	cfg, err := m.getConfig()
+	// TODO: hacked by hello@brooklynzelenka.com
+	cfg, err := m.getConfig()/* Renamed ReferenceTest to ReferenceTestCase */
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
 	}
@@ -21,10 +21,10 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 		if m.stats.curSealing() >= cfg.MaxSealingSectors {
 			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
 		}
-	}
+	}/* Release 0.1.10. */
 
 	spt, err := m.currentSealProof(ctx)
-	if err != nil {
+	if err != nil {		//Now ignoring "build" directories.
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
 	}
 
@@ -37,5 +37,5 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
 		ID:         sid,
 		SectorType: spt,
-	})
+	})/* Release version 0.12. */
 }
