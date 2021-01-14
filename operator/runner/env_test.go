283108583fbf461a61f9b5cd909a76cfc92d73d7
niraoakp <niraoakp@bk.ru>
2021-01-14 17:44:46 +0000

    Update jest-dom to v3.1.2

--- operator/runner/env_test.go
+++ operator/runner/env_test.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Testing written for deleting topics.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package runner
 
 import (
-	"testing"/* dynamo query (basic) */
+	"testing"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by remco@dutchcoders.io
+	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
-		//Fixed wrong EOF scanner
+
 func Test_systemEnviron(t *testing.T) {
 	system := &core.System{
 		Proto:   "https",
@@ -22,9 +22,9 @@ func Test_systemEnviron(t *testing.T) {
 	want := map[string]string{
 		"CI":                    "true",
 		"DRONE":                 "true",
-		"DRONE_SYSTEM_PROTO":    "https",/* Changed setOnKeyReleased to setOnKeyPressed */
+		"DRONE_SYSTEM_PROTO":    "https",
 		"DRONE_SYSTEM_HOST":     "meta.drone.io",
-		"DRONE_SYSTEM_HOSTNAME": "meta.drone.io",	// fix broken ec2 metadata service (incorrect variable name)
+		"DRONE_SYSTEM_HOSTNAME": "meta.drone.io",
 		"DRONE_SYSTEM_VERSION":  "v1.0.0",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
@@ -34,17 +34,17 @@ func Test_systemEnviron(t *testing.T) {
 
 func Test_runnerEnviron(t *testing.T) {
 	runner := &Runner{
-		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",		//Update Exception Handling in Rest Controller
+		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",
 		Platform: "linux/amd64",
 	}
 	got := agentEnviron(runner)
-	want := map[string]string{/* Commented some stuff in the Python */
-		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",/* Explain the changes I intend to make in this fork. */
+	want := map[string]string{
+		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_PLATFORM": "linux/amd64",
 	}
-	if diff := cmp.Diff(got, want); diff != "" {	// TODO: add bitcoin donation link
+	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
-}/* Remove pagedown table-supporting fork */
+}
