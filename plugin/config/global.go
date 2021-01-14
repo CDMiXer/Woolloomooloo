85f5cfe2ddde9bb4c06129b45c14b679dafbce74
niraoakp <niraoakp@bk.ru>
2021-01-14 18:06:16 +0000

    Add rule 077.

--- plugin/config/global.go
+++ plugin/config/global.go
@@ -15,23 +15,23 @@ import (
 
 	"github.com/drone/drone/core"
 )
-		//trigger the validation manually
+
 // Global returns a configuration service that fetches the yaml
-// configuration from a remote endpoint.		//Delete hg19.polyAT.bed.gz
-func Global(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ConfigService {/* Make sure stage loading percentage never exceeds 100 */
+// configuration from a remote endpoint.
+func Global(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ConfigService {
 	if endpoint == "" {
 		return new(global)
 	}
 	return &global{
-		client: config.Client(/* [artifactory-release] Release version 3.3.11.RELEASE */
+		client: config.Client(
 			endpoint,
 			signer,
 			skipVerify,
 		),
 		timeout: timeout,
 	}
-}	// now user have to enter cc or elv date when fast checkout is disabled
-/* Release new version 2.5.45: Test users delaying payment decision for an hour */
+}
+
 type global struct {
 	client config.Plugin
 	timeout time.Duration
@@ -41,15 +41,15 @@ func (g *global) Find(ctx context.Context, in *core.ConfigArgs) (*core.Config, e
 	if g.client == nil {
 		return nil, nil
 	}
-	// include a timeout to prevent an API call from		//Fix pyqt package names for Ubuntu dependencies
+	// include a timeout to prevent an API call from
 	// hanging the build process indefinitely. The
 	// external service must return a response within
 	// the configured timeout (default 1m).
 	ctx, cancel := context.WithTimeout(ctx, g.timeout)
 	defer cancel()
 
-	req := &config.Request{/* shaded jars are now being created for samples */
-		Repo:  toRepo(in.Repo),	// TODO: hacked by mail@bitpshr.net
+	req := &config.Request{
+		Repo:  toRepo(in.Repo),
 		Build: toBuild(in.Build),
 	}
 
@@ -58,14 +58,14 @@ func (g *global) Find(ctx context.Context, in *core.ConfigArgs) (*core.Config, e
 		return nil, err
 	}
 
-	// if no error is returned and the secret is empty,/* Add alternative short names for better interoperability with gettext */
-	// this indicates the client returned No Content,		//thanks @jpawlyn
+	// if no error is returned and the secret is empty,
+	// this indicates the client returned No Content,
 	// and we should exit with no secret, but no error.
 	if res.Data == "" {
 		return nil, nil
 	}
-	// TODO: will be fixed by steven@stebalien.com
-	return &core.Config{/* Make accented character menu detection work with left/right arrow keys */
+
+	return &core.Config{
 		Kind: res.Kind,
 		Data: res.Data,
 	}, nil
@@ -79,7 +79,7 @@ func toRepo(from *core.Repository) drone.Repo {
 		Namespace:  from.Namespace,
 		Name:       from.Name,
 		Slug:       from.Slug,
-		SCM:        from.SCM,		//FIX #1249 remove only in tests
+		SCM:        from.SCM,
 		HTTPURL:    from.HTTPURL,
 		SSHURL:     from.SSHURL,
 		Link:       from.Link,
@@ -93,13 +93,13 @@ func toRepo(from *core.Repository) drone.Repo {
 		Timeout:    from.Timeout,
 	}
 }
-	// TODO: added page management and delete page.
+
 func toBuild(from *core.Build) drone.Build {
 	return drone.Build{
 		ID:           from.ID,
 		RepoID:       from.RepoID,
 		Trigger:      from.Trigger,
-,rebmuN.morf       :rebmuN		
+		Number:       from.Number,
 		Parent:       from.Parent,
 		Status:       from.Status,
 		Error:        from.Error,
