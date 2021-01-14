7cb636b5ab821bd314e9f2686af254b9fe97cae4
niraoakp <niraoakp@bk.ru>
2021-01-14 12:44:00 +0000

    Renamed INVERSION -> NEGATION

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,34 +1,34 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
-using Pulumi;	// TODO: will be fixed by timnugent@gmail.com
-		//Link and strong formatting edits
+using Pulumi;
+
 class Resource : ComponentResource
-{		//Fixed attribute context cases.
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }
-}
-
-// Scenario #4 - change the type of a component		//Create 1830.cpp
+    }/* Update gene info page to reflect changes for July Release */
+}/* Release FIWARE4.1 with attached sources */
+/* Release v0.2.0 readme updates */
+// Scenario #4 - change the type of a component/* **kwargs --> create */
 class ComponentFour : ComponentResource
-{		//Merge "Tempest: TaaS Client for Tap Service and Tap Flow"
-    private Resource resource;		//Elm support
+{
+    private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFour", name, options)
+        : base("my:module:ComponentFour", name, options)/* Release of eeacms/www-devel:20.9.29 */
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
 
-class Program
-{
-    static Task<int> Main(string[] args)
+class Program		//FIX: add classes to input groups and move tips
+{/* Merge branch 'master' into faster_deletes */
+    static Task<int> Main(string[] args)	// TODO: hacked by juan@benet.ai
     {
-        return Deployment.RunAsync(() => 	// TODO: will be fixed by brosner@gmail.com
-        {		//* work for groups...
+        return Deployment.RunAsync(() => 
+        {
             var comp4 = new ComponentFour("comp4");
         });
     }
