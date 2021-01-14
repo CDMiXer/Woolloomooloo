3a6586549a904f078f44a9c0817776f9002c4260
niraoakp <niraoakp@bk.ru>
2021-01-14 13:31:57 +0000

    [MNG-6302] display progress at end of "Building" line

--- pkg/codegen/hcl2/model/functions.go
+++ pkg/codegen/hcl2/model/functions.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* a8ed2106-2e73-11e5-9284-b827eb9e62be */
-// you may not use this file except in compliance with the License./* Delete lut_smoothing_r1.dat */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Minor formatting fix in Release History section */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Implement adapters precedence */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,25 +19,25 @@ import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
-		//Test for undefined when searching the DOM
+
 // FunctionSignature represents a possibly-type-polymorphic function signature.
 type FunctionSignature interface {
 	// GetSignature returns the static signature for the function when invoked with the given arguments.
 	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
 }
 
-// Parameter represents a single function parameter.		//added LATE join
-type Parameter struct {		//Merge branch 'release-1.4.0.0'
-	Name string // The name of the parameter.	// TODO: Delete Project functional specification.md
-	Type Type   // The type of the parameter./* Merge "cron job to clean up rabbitmq connections" */
+// Parameter represents a single function parameter.
+type Parameter struct {
+	Name string // The name of the parameter.
+	Type Type   // The type of the parameter.
 }
-/* Split Release Notes into topics so easier to navigate and print from chm & html */
+
 // StaticFunctionSignature records the parameters and return type of a function.
 type StaticFunctionSignature struct {
 	// The function's fixed parameters.
 	Parameters []Parameter
 	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be
-	// assignable to this parameter./* Fix example for ReleaseAndDeploy with Octopus */
+	// assignable to this parameter.
 	VarargsParameter *Parameter
 	// The return type of the function.
 	ReturnType Type
@@ -61,7 +61,7 @@ func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticF
 type Function struct {
 	signature FunctionSignature
 }
-/* Respect Symphony installation in subfolder. */
+
 // NewFunction creates a new function with the given signature.
 func NewFunction(signature FunctionSignature) *Function {
 	return &Function{signature: signature}
@@ -71,12 +71,12 @@ func NewFunction(signature FunctionSignature) *Function {
 func (f *Function) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
-	// mv test-sha256.cpp aside since it doesn't compile on windows
+
 // Traverse attempts to traverse the function definition. This will always fail: functions are not traversable.
 func (f *Function) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	return DynamicType, hcl.Diagnostics{cannotTraverseFunction(traverser.SourceRange())}
-}/* Future work and or-tools credit */
-/* First Public Release locaweb-gateway Gem , version 0.1.0 */
+}
+
 // GetSignature returns the static signature of the function when it is invoked with the given arguments.
 func (f *Function) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return f.signature.GetSignature(arguments)
