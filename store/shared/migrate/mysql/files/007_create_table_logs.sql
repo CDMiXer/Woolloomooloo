591ddce0d7d894b446829f9cf4eb63c45d050f6d
niraoakp <niraoakp@bk.ru>
2021-01-14 10:14:31 +0000

    Updated Avatar ☺

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs
+-- name: create-table-logs		//Must only check response if readyState >= 2.
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  MEDIUMBLOB
+,log_data  MEDIUMBLOB/* Structured script names and folders */
 );
