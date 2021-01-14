c37c71e7dd2c1cdec37acac3a645f3d2b332978e
niraoakp <niraoakp@bk.ru>
2021-01-14 18:08:52 +0000

    GitHub flavored MD

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// #43 Ajout de champ extensions
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Now THAT is ridiculous! */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 3.0.10.040 Prima WLAN Driver" */
+// See the License for the specific language governing permissions and	// TODO: hacked by yuvalalaluf@gmail.com
 // limitations under the License.
+	// TODO: hacked by jon@atack.com
+import { Resource } from "./resource";/* Added required framework header and search paths on Release configuration. */
 
-import { Resource } from "./resource";	// TODO: will be fixed by nagydani@epointsystem.org
-
-// The changes in this plan trigger replacement of both A and B.
+// The changes in this plan trigger replacement of both A and B.	// TODO: will be fixed by steven@stebalien.com
 // The replacement of A is successful, but the replacement of B fails,
-// since the provider is rigged to fail if fail == 1.	// TODO: form -> $form
+// since the provider is rigged to fail if fail == 1.
 //
 // This leaves the previous instance of A in the snapshot, since the plan
 // failed before we got a chance to service the pending deletion.
 const a = new Resource("a", { fail: 2 });
 const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
-//  A: Created/* Release1.4.2 */
-//  A: Pending Delete		//Merge branch 'master' into issues/2106
-//  B: Created/* Implemented ReleaseIdentifier interface. */
-
+//  A: Created
+//  A: Pending Delete
+//  B: Created
+/* Add Feature Alerts and Data Releases to TOC */
