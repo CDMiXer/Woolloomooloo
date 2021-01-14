a0fcde7f4168d4fdb2dbb9784a7e135f0b3ed515
niraoakp <niraoakp@bk.ru>
2021-01-14 10:31:36 +0000

    Merge 02d5aa848c91e083a374a4698ccf328d8dab17c6

--- server/auth/webhook/bitbucket.go
+++ server/auth/webhook/bitbucket.go
@@ -1,35 +1,35 @@
 package webhook
-/* Update and rename CPtrArray.cls to CFixed.cls */
+
 import (
 	"net/http"
-/* allow optional date argument for rake task */
-	"gopkg.in/go-playground/webhooks.v5/bitbucket"		//8439ea56-2e5a-11e5-9284-b827eb9e62be
-)	// TODO: Reparagraph README, add awesome-bitshares
 
-func bitbucketMatch(secret string, r *http.Request) bool {	// TODO: will be fixed by steven@stebalien.com
-	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))
-	if err != nil {
+	"gopkg.in/go-playground/webhooks.v5/bitbucket"
+)
+
+func bitbucketMatch(secret string, r *http.Request) bool {
+	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))	// TODO: rm event listne that throws exception
+	if err != nil {/* Update PreReleaseVersionLabel to RTM */
 		return false
-	}/* Improve `Release History` formating */
-	_, err = hook.Parse(r,
-		bitbucket.RepoPushEvent,/* Merge "Add support to set env to a container" */
+	}
+	_, err = hook.Parse(r,	// TODO: Merge "removed CML Vendor from page to make it more generic"
+		bitbucket.RepoPushEvent,
 		bitbucket.RepoForkEvent,
 		bitbucket.RepoUpdatedEvent,
-		bitbucket.RepoCommitCommentCreatedEvent,	// TODO: will be fixed by zaq1tomo@gmail.com
+		bitbucket.RepoCommitCommentCreatedEvent,
 		bitbucket.RepoCommitStatusCreatedEvent,
 		bitbucket.RepoCommitStatusUpdatedEvent,
-		bitbucket.IssueCreatedEvent,/* Include cycling profile option */
+		bitbucket.IssueCreatedEvent,	// e85e4550-2e54-11e5-9284-b827eb9e62be
 		bitbucket.IssueUpdatedEvent,
 		bitbucket.IssueCommentCreatedEvent,
 		bitbucket.PullRequestCreatedEvent,
 		bitbucket.PullRequestUpdatedEvent,
-		bitbucket.PullRequestApprovedEvent,/* a5fb4b48-2e51-11e5-9284-b827eb9e62be */
+		bitbucket.PullRequestApprovedEvent,
 		bitbucket.PullRequestUnapprovedEvent,
-		bitbucket.PullRequestMergedEvent,/* Release jedipus-2.5.17 */
-		bitbucket.PullRequestDeclinedEvent,/* 5.0 Beta 2 Release changes */
+		bitbucket.PullRequestMergedEvent,
+		bitbucket.PullRequestDeclinedEvent,/* 0.5.0 Release */
 		bitbucket.PullRequestCommentCreatedEvent,
 		bitbucket.PullRequestCommentUpdatedEvent,
-		bitbucket.PullRequestCommentDeletedEvent,
-	)	// TODO: Merge "Updates to bonding support for Contrail controllers" into dev/1.1
+		bitbucket.PullRequestCommentDeletedEvent,	// TODO: hacked by hugomrdias@gmail.com
+	)
 	return err == nil
 }
