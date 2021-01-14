cdee78225af439e9e59bde392beba21d646c6581
niraoakp <niraoakp@bk.ru>
2021-01-14 13:29:45 +0000

    Added Ken's restore window buttons.

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -5,11 +5,11 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Comment added by Pvlerick
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Added information about future and maintenance
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
@@ -17,10 +17,10 @@
 package converter
 
 import (
-	"time"	// TODO: BT: Reduced waitBetweenSends and removed sleep duplicates
+	"time"
 
-	"github.com/drone/drone/core"/* Added http urls */
-)/* Merge "Fix dereference of null state pointer in ifmap introspect code." */
+	"github.com/drone/drone/core"
+)
 
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
