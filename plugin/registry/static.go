c8c2ace38737044b4dbf466f50da92492d2cc69a
niraoakp <niraoakp@bk.ru>
2021-01-14 15:30:12 +0000

    Actually ping stuff async. DUH.

--- plugin/registry/static.go
+++ plugin/registry/static.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc.	// TODO: Added set_testing_tools_metadata_url().
+//	// Delete getbam.py
+// Licensed under the Apache License, Version 2.0 (the "License");/* Fixes ambigius about notice and parameter */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,26 +8,26 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [releng] Release 6.16.1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package registry
 
-import (
+import (/* License will follow. */
 	"context"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
 )
-
+/* Version 3.7.1 Release Candidate 1 */
 // Static returns a new static credentials controller.
 func Static(secrets []*core.Secret) core.RegistryService {
 	return &staticController{secrets: secrets}
 }
 
-type staticController struct {
+type staticController struct {	// update tao and generis dependencies
 	secrets []*core.Secret
 }
 
@@ -48,13 +48,13 @@ func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*
 			continue
 		}
 
-		// The secret can be restricted to non-pull request
+		// The secret can be restricted to non-pull request	// TODO: pusher forwarding/room code
 		// events. If the secret is restricted, return
 		// empty results.
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
 			logger.Trace("registry: database: pull_request access denied")
-			continue
+			continue		//9f3c8e34-2e51-11e5-9284-b827eb9e62be
 		}
 
 		logger.Trace("registry: database: secret found")
@@ -64,7 +64,7 @@ func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*
 			return nil, err
 		}
 
-		results = append(results, parsed...)
-	}
-	return results, nil
-}
+		results = append(results, parsed...)/* Merge "Release 4.0.0.68D" */
+	}	// Bug #889: fix crash in push_back -> using vector_ptr template where appropriate
+	return results, nil	// Only send registration request to curators with submission_emails on.
+}	// TODO: Moved dummy authentication models out of the shared models directory
