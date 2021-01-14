27876d8c4ea63ab1a8c1f71e35409e01b8d9d41f
niraoakp <niraoakp@bk.ru>
2021-01-14 01:03:20 +0000

    Adjust for new locations of base package vignettes.

--- handler/api/repos/secrets/update_test.go
+++ handler/api/repos/secrets/update_test.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by sjors@sprovoost.nl
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
-/* Updated .pom to 0.5.0-SNAPSHOT */
+
 import (
 	"bytes"
-	"context"/* GitHub Releases in README */
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+/* Added 'View Release' to ProjectBuildPage */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-/* First thoughts on a REST API */
+		//4ffcd790-2e6d-11e5-9284-b827eb9e62be
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
-
+)	// TODO: will be fixed by willem.melching@gmail.com
+/* Make the captured page nicer, make the iframe full page */
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* (@:print @.value) */
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
-	secrets := mock.NewMockSecretStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)		//remove duplicate instanciation of user AuthProfile
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)		//Create R_codes_for_CaRcone
+
+	secrets := mock.NewMockSecretStore(controller)/* Link to the Release Notes */
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
-	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)	// new Dribbble API
+	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
-	c := new(chi.Context)/* Post deleted: Second Post */
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(dummySecret)
+	json.NewEncoder(in).Encode(dummySecret)	// TODO: 153. Find Minimum in Rotated Sorted Array
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(		//Adding in Travis test skipping.
+	r = r.WithContext(		//Merge del buildservice
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleUpdate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//Mention DEBUG_TIME in Simple Tutorial
+		t.Errorf("Want response code %d, got %d", want, got)		//Meilleur découpage du code
 	}
-/* Don't animate to the new position when re-initialising (see issue 36)... */
+/* 21da3472-2e61-11e5-9284-b827eb9e62be */
 	got, want := new(core.Secret), dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Update join.adoc (bad link) */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}	// TODO: will be fixed by jon@atack.com
 }
 
 func TestHandleUpdate_ValidationError(t *testing.T) {
@@ -66,23 +66,23 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-
+/* Updated Release URL */
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
-		//open pictures in _blank, kind of repairs clicking "back" after viewing image
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
-	in := new(bytes.Buffer)
+	in := new(bytes.Buffer)	// TODO: Stop testing on node 5
 	json.NewEncoder(in).Encode(&core.Secret{Data: ""})
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Delete 16.JPG */
+	)
 
 	HandleUpdate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusBadRequest; want != got {
@@ -91,24 +91,24 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 
 	got, want := new(errors.Error), &errors.Error{Message: "Invalid Secret Value"}
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Released version 0.2.3 */
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)/* better menu indentation pt2 */
 	}
 }
 
 func TestHandleUpdate_BadRequest(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//Score on top of entry name
 	defer controller.Finish()
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")		//Add to $scope abribute urlLogo 
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: 334db74a-2e4f-11e5-9284-b827eb9e62be
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleUpdate(nil, nil).ServeHTTP(w, r)
