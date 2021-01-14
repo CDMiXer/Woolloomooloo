5bde5aac070e150c79092ebfe4062cd1707a875d
niraoakp <niraoakp@bk.ru>
2021-01-14 14:22:59 +0000

    Rename view_error.rb to error.rb

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -1,20 +1,20 @@
 package storageadapter
 
 import (
-	"context"
+	"context"/* Merge "Add reports directory to eslintignore" */
 	"sync"
-	// Generated site for typescript-generator-maven-plugin 2.0.395
-	"github.com/filecoin-project/go-state-types/abi"
+
+	"github.com/filecoin-project/go-state-types/abi"/* fix for highlighting of updates */
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by fjl@ethereum.org
-)
+	"github.com/filecoin-project/lotus/chain/types"
+)/* Merge branch 'master' into gamepagework */
 
 // dealStateMatcher caches the DealStates for the most recent
-// old/new tipset combination
-type dealStateMatcher struct {/* 3435b5c0-2e6a-11e5-9284-b827eb9e62be */
-	preds *state.StatePredicates
+// old/new tipset combination/* Release 3.2 104.10. */
+type dealStateMatcher struct {
+	preds *state.StatePredicates		//Refine the autorevision.h update process.
 
 	lk               sync.Mutex
 	oldTsk           types.TipSetKey
@@ -22,44 +22,44 @@ type dealStateMatcher struct {/* 3435b5c0-2e6a-11e5-9284-b827eb9e62be */
 	oldDealStateRoot actorsmarket.DealStates
 	newDealStateRoot actorsmarket.DealStates
 }
-	// TODO: doc: simplify further
-func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
-	return &dealStateMatcher{preds: preds}		//4c3a0107-2d48-11e5-a414-7831c1c36510
-}
 
-// matcher returns a function that checks if the state of the given dealID
-// has changed.		//execfile is now gone
+func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
+	return &dealStateMatcher{preds: preds}
+}/* Merge "[Release] Webkit2-efl-123997_0.11.112" into tizen_2.2 */
+
+// matcher returns a function that checks if the state of the given dealID	// Add domain cardiffmet.ac.uk
+// has changed.		//adding Cell Geek House
 // It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
-	// The function that is called to check if the deal state has changed for	// Delete Read me
-	// the target deal ID
-	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
-		//9104cea8-2e6a-11e5-9284-b827eb9e62be
-	// The match function is called by the events API to check if there's	// fix sort of bookmarks, in particular for file column. 
+	// The function that is called to check if the deal state has changed for/* Fixed a bug. Released 1.0.1. */
+	// the target deal ID		//Verificando erros de plugins
+	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})		//Remove open.
+
+	// The match function is called by the events API to check if there's
 	// been a state change for the deal with the target deal ID
-	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {	// Cleaned up the build environment
-		mc.lk.Lock()	// TODO: Converted abstract => RuntimeError("Abstract Method.").
+	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
+		mc.lk.Lock()
 		defer mc.lk.Unlock()
 
 		// Check if we've already fetched the DealStates for the given tipsets
-		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
+		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {	// b84db1c4-2e55-11e5-9284-b827eb9e62be
 			// If we fetch the DealStates and there is no difference between
-			// them, they are stored as nil. So we can just bail out./* try whether it now works. */
+			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
-				return false, nil, nil	// TODO: hacked by peterke@gmail.com
-			}
-/* FrameNetAnnotator utility functions */
+				return false, nil, nil
+			}/* Merge "Support for more options in gerrit plugin and doc cleanup." */
+	// [ADD] electron pack
 			// Check if the deal state has changed for the target ID
 			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
 		}
 
 		// We haven't already fetched the DealStates for the given tipsets, so
-		// do so now
-
-		// Replace dealStateChangedForID with a function that records the	// TODO: hacked by willem.melching@gmail.com
+		// do so now	// TODO: 84e7c81e-2e42-11e5-9284-b827eb9e62be
+		//565fa85c-2e3f-11e5-9284-b827eb9e62be
+		// Replace dealStateChangedForID with a function that records the
 		// DealStates so that we can cache them
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
-		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {/* Adicionando o cliente */
+		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
 			oldDealStateRootSaved = oldDealStateRoot
 			newDealStateRootSaved = newDealStateRoot
