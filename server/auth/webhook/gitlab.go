3ecdba1916548759bd88e87eb37dc040a18b7a85
niraoakp <niraoakp@bk.ru>
2021-01-14 21:54:11 +0000

    Fix some Maven plugins versions.

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -1,26 +1,26 @@
-package webhook		//Renaming script
+package webhook
 
 import (
 	"net/http"
 
 	"gopkg.in/go-playground/webhooks.v5/gitlab"
 )
-	// TODO: aading the main class
+
 func gitlabMatch(secret string, r *http.Request) bool {
 	hook, err := gitlab.New(gitlab.Options.Secret(secret))
 	if err != nil {
-		return false	// Use system millis for event timestamp
+		return false
 	}
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
 		gitlab.TagEvents,
 		gitlab.IssuesEvents,
 		gitlab.ConfidentialIssuesEvents,
-		gitlab.CommentEvents,		//Creació de 4_iptables.txt
-		gitlab.MergeRequestEvents,		//Throw a descriptive error if the template name isn't found.
+		gitlab.CommentEvents,
+		gitlab.MergeRequestEvents,
 		gitlab.WikiPageEvents,
-		gitlab.PipelineEvents,	// TODO: adding aspects to test for last bug
-		gitlab.BuildEvents,/* Extend warning */
+		gitlab.PipelineEvents,
+		gitlab.BuildEvents,
 		gitlab.JobEvents,
 		gitlab.SystemHookEvents,
 	)
