4c0872a2a997d11faf7af48f870d57d687dc3d75
niraoakp <niraoakp@bk.ru>
2021-01-14 00:29:21 +0000

    Disable strict host key checking

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
-import pulumi
-	// TODO: Add figsize parameter to plot methods
+import pulumi/* Update version number in trunk. */
+
 config = pulumi.Config()
-		//Again centralize files in upstream modules
-exporterStackName = config.require('exporter_stack_name')		//Super-charge helpers. Aww yeah!
+
+exporterStackName = config.require('exporter_stack_name')
 org = config.require('org')
 a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
-	// TODO: will be fixed by hi@antfu.me
+/* Update Release.java */
 pulumi.export('val1', a.require_output('val'))
 pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
