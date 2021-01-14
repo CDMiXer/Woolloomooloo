644b344e0dae9b73b77ee1b8d52a3daf40dfc2d3
niraoakp <niraoakp@bk.ru>
2021-01-14 02:49:05 +0000

    migrate to gulp

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release for v46.0.0. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// Rename compound-dir to compound-dir.md
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Add a super simple error handling example */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by josharian@gmail.com
+// See the License for the specific language governing permissions and	// TODO: will be fixed by igor@soramitsu.co.jp
 // limitations under the License.
-/* 431e8d8e-2e45-11e5-9284-b827eb9e62be */
-package registry/* Release v2.7.2 */
 
+package registry/* Release 7.15.0 */
+		//Use Eclipse generated hashCode() and equals(). Better ivar name.
 import (
 	"context"
 
 	"github.com/drone/drone/core"
 )
-
+/* Clarify supported ruby versions */
 type noop struct{}
 
 func (noop) List(context.Context, *core.RegistryArgs) ([]*core.Registry, error) {
