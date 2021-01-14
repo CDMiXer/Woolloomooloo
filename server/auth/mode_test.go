c7c33fbc5944005b1a25f3886bcf4a57a57bdb3b
niraoakp <niraoakp@bk.ru>
2021-01-14 20:33:18 +0000

    More tests, improved header parsing, adds drop statements

--- server/auth/mode_test.go
+++ server/auth/mode_test.go
@@ -1,58 +1,58 @@
 package auth
 
-import (/* Deleting file that probably is local to Tim */
+import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"		//Ackowledging you wonderful people in the credits / setup stuff!
+	"github.com/stretchr/testify/assert"	// TODO: hacked by martin2cai@hotmail.com
 )
 
 func TestModes_Add(t *testing.T) {
 	t.Run("InvalidMode", func(t *testing.T) {
 		assert.Error(t, Modes{}.Add(""))
 	})
-	t.Run("Client", func(t *testing.T) {	// TODO: will be fixed by timnugent@gmail.com
+	t.Run("Client", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("client")) {
 			assert.Contains(t, m, Client)
-		}
+}		
 	})
 	t.Run("Hybrid", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("hybrid")) {
-			assert.Contains(t, m, Client)/* Updated iterm2 to Release 1.1.2 */
+			assert.Contains(t, m, Client)
 			assert.Contains(t, m, Server)
-		}	// TODO: will be fixed by caojiaoyue@protonmail.com
-	})/* He creado ejemplos de routing y repository */
-	t.Run("Server", func(t *testing.T) {	// waitq: waitq and sched_switch refactoring
-		m := Modes{}
+		}
+	})
+	t.Run("Server", func(t *testing.T) {
+		m := Modes{}	// TODO: Create 11936 - The Lazy Lumberjacks.cpp
 		if assert.NoError(t, m.Add("server")) {
 			assert.Contains(t, m, Server)
-		}/* Testing moving Property from spring to Env */
+		}
 	})
-	t.Run("SSO", func(t *testing.T) {	// TODO: Pass debug setting to protocol
+	t.Run("SSO", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("sso")) {
 			assert.Contains(t, m, SSO)
-		}
+}		
 	})
 }
 func TestModes_GetMode(t *testing.T) {
 	t.Run("Client", func(t *testing.T) {
 		mode, err := GetMode("Bearer ")
-		if assert.NoError(t, err) {
-			assert.Equal(t, Client, mode)/* Removed redundant operations and checks */
-		}
+		if assert.NoError(t, err) {/* 04601bd0-2e40-11e5-9284-b827eb9e62be */
+			assert.Equal(t, Client, mode)
+		}	// TODO: Merge branch 'master' into 15353-empty-requests
 	})
 	t.Run("Server", func(t *testing.T) {
-		mode, err := GetMode("")	// TODO: new version, added docs
+		mode, err := GetMode("")
 		if assert.NoError(t, err) {
 			assert.Equal(t, Server, mode)
-		}	// TODO: Use last shaded jar
-	})
-	t.Run("SSO", func(t *testing.T) {/* Merge "docs: Support Library 19.0.1 Release Notes" into klp-docs */
+		}		//make unit tests run via ant
+	})	// TODO: Replace Integer with int in index entries.
+	t.Run("SSO", func(t *testing.T) {		//Useless version bumping to help @meh
 		mode, err := GetMode("Bearer id_token:")
 		if assert.NoError(t, err) {
 			assert.Equal(t, SSO, mode)
-		}/* ugly fix for #3607, grammar for comprehensions in positional arg lists */
-	})/* Fix Release Job */
+		}
+	})
 }
