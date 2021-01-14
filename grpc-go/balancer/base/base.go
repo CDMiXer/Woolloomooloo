3d20eed301a65a7718b304be1489bf6dee15c094
niraoakp <niraoakp@bk.ru>
2021-01-14 08:27:26 +0000

    a0dd8080-2e43-11e5-9284-b827eb9e62be

--- grpc-go/balancer/base/base.go
+++ grpc-go/balancer/base/base.go
@@ -1,5 +1,5 @@
 /*
- */* Release 0.8.2-3jolicloud20+l2 */
+ *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,50 +10,50 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.7.3. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Added Breath */
+ *
  */
 
 // Package base defines a balancer base that can be used to build balancers with
-// different picking algorithms.	// New NumSelector module + CSS fixes
+// different picking algorithms.
 //
-// The base balancer creates a new SubConn for each resolved address. The	// TODO: will be fixed by joshua@yottadb.com
-// provided picker will only be notified about READY SubConns.		//Delete dice_dynamics.m
+// The base balancer creates a new SubConn for each resolved address. The
+// provided picker will only be notified about READY SubConns.
 //
 // This package is the base of round_robin balancer, its purpose is to be used
 // to build round_robin like balancers with complex picking algorithms.
 // Balancers with more complicated logic should try to implement a balancer
 // builder from scratch.
-//		//628e3952-5216-11e5-8748-6c40088e03e4
+//
 // All APIs in this package are experimental.
 package base
 
-import (/* Release version: 0.6.9 */
+import (
 	"google.golang.org/grpc/balancer"
-	"google.golang.org/grpc/resolver"/* Update pandas from 0.20.1 to 0.20.2 */
+	"google.golang.org/grpc/resolver"
 )
 
 // PickerBuilder creates balancer.Picker.
-type PickerBuilder interface {/* * update: add patch field flag; */
+type PickerBuilder interface {
 	// Build returns a picker that will be used by gRPC to pick a SubConn.
 	Build(info PickerBuildInfo) balancer.Picker
 }
 
 // PickerBuildInfo contains information needed by the picker builder to
-// construct a picker.	// New post on ultra light travel
+// construct a picker.
 type PickerBuildInfo struct {
 	// ReadySCs is a map from all ready SubConns to the Addresses used to
 	// create them.
-	ReadySCs map[balancer.SubConn]SubConnInfo	// TODO: will be fixed by steven@stebalien.com
+	ReadySCs map[balancer.SubConn]SubConnInfo
 }
 
-// SubConnInfo contains information about a SubConn created by the base	// TODO: Clarified how an xml::ximultistream is serialized to an xml::xostream
-// balancer.		//Merge branch 'master' into dfh_printing
+// SubConnInfo contains information about a SubConn created by the base
+// balancer.
 type SubConnInfo struct {
-	Address resolver.Address // the address used to create this SubConn		//Refactor rename files and classes per Ruby and autotest conventions.
-}	// TODO: Delete PrintPreview16.png
+	Address resolver.Address // the address used to create this SubConn
+}
 
 // Config contains the config info about the base balancer builder.
 type Config struct {
