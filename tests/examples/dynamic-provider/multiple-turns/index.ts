b8d00b80ebef6ab8ae409823f39dbbba7f684e47
niraoakp <niraoakp@bk.ru>
2021-01-14 22:45:01 +0000

    Show both NULL user tasks and own tasks

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//classpath entry for new jar
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-const sleep = require("sleep-promise");/* MustacheView takes models on init (but doesn't do anything with them yet) */
-const assert = require("assert");/* fix sonar links */
+const sleep = require("sleep-promise");
+const assert = require("assert");
 
-class NullProvider implements dynamic.ResourceProvider {
+class NullProvider implements dynamic.ResourceProvider {/* Fixed: Hitting a boss robot could crash the program */
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
@@ -14,24 +14,24 @@ class NullProvider implements dynamic.ResourceProvider {
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
 
-class NullResource extends dynamic.Resource {
+class NullResource extends dynamic.Resource {	// TODO: StatsD : namespace error on \Exception not catching exceptions
     constructor(name: string) {
-        super(new NullProvider(), name, {}, undefined);
+        super(new NullProvider(), name, {}, undefined);	// TODO: updated client.py server.py
     }
-}		//Update basic-commands-of-redis-cli.md
+}	// TODO: will be fixed by juan@benet.ai
 
-(async () => {
+(async () => {/* Release for v25.1.0. */
     try {
         const a = new NullResource("a");
-        await sleep(1000);/* Add buttons GitHub Release and License. */
-        const b = new NullResource("b");/* Add group for embedded frameworks */
-        await sleep(1000);	// Fixed bug #691759.
+        await sleep(1000);	// TODO: hacked by greg@colvin.org
+        const b = new NullResource("b");
+        await sleep(1000);
         const c = new NullResource("c");
         const urn = await b.urn;
-        assert.notStrictEqual(urn, undefined, "expected a defined urn");		//Added trademark info
+        assert.notStrictEqual(urn, undefined, "expected a defined urn");
         assert.notStrictEqual(urn, "", "expected a valid urn");
     } catch (err) {
-        console.error(err);
+        console.error(err);/* Add git push --tags to README */
         process.exit(-1);
     }
-})();
+})();/* Delete ProductServiceImpl.class */
