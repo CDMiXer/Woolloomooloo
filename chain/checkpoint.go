e60fc0e79643f647853c97dcf84c8f67cd91eedd
niraoakp <niraoakp@bk.ru>
2021-01-14 06:13:52 +0000

    Software Engineer (XCode)

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,21 +1,21 @@
-package chain
+package chain	// TODO: Get rid of spaces and ;
 
 import (
 	"context"
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Delete life.out
 
-	"golang.org/x/xerrors"
-)
+	"golang.org/x/xerrors"	// skip highlight of function definition
+)/* ReleaseNotes: mention basic debug info and ASan support in the Windows blurb */
 
-func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
+func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {	// TODO: hacked by xaber.twt@gmail.com
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
-	}
+	}/* Release Django-Evolution 0.5. */
 
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
-	if err != nil {
-		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
+	if err != nil {	// TODO: hacked by cory@protocol.ai
+		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)	// Disable all builds on AppVeyor except release for Qt 5.7 (#1828)
 		if err != nil {
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
 		} else if len(tss) != 1 {
@@ -25,9 +25,9 @@ func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) e
 	}
 
 	if err := syncer.switchChain(ctx, ts); err != nil {
-		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
+		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)	// TODO: adding staging plugin
 	}
-
+/* Release version 3.2.1.RELEASE */
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
 	}
@@ -46,12 +46,12 @@ func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	}
 
 	// Otherwise, sync the chain and set the head.
-	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
+	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {	// Fix selected attributes visibility.
 		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
 	}
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain head: %w", err)
+		return xerrors.Errorf("failed to set the chain head: %w", err)/* Release for 18.12.0 */
 	}
 	return nil
 }
