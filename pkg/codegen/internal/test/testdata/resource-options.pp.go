59d3c1a2417ac8b9a4e21f5ab0c8b8f63f6d5882
niraoakp <niraoakp@bk.ru>
2021-01-14 09:57:00 +0000

    Merge branch 'master' into GuardianDruidDamageDetail

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,14 +1,14 @@
 package main
-
+/* upgrade libssh2 to 1.2.7 */
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-		//fix client ref link
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{/* Release for 4.11.0 */
+		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{/* Release version 0.0.3 */
 			Region: pulumi.String("us-west-2"),
 		})
 		if err != nil {
@@ -20,9 +20,9 @@ func main() {
 			"bucket",
 			"lifecycleRules[0]",
 		}))
-		if err != nil {
+		if err != nil {/* trying to beautify the layout */
 			return err
-		}		//Rename uninstall_uifile_de to uninstall_uifile_ger
+		}
 		return nil
-	})/* remove verification that cause unit tests to fail sometimes  */
+	})
 }
