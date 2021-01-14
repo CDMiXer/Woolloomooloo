1c76d2188b1ce7ff4cd731827e457c75944ee69b
niraoakp <niraoakp@bk.ru>
2021-01-14 07:35:14 +0000

    Add an easier way of compiling and installing everything

--- cmd/drone-agent/main.go
+++ cmd/drone-agent/main.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Notification now gives host name
 // that can be found in the LICENSE file.
-
+/* GMParser 1.0 (Stable Release) repackaging */
 // +build !oss
-/* Release 0.2.0 of swak4Foam */
-package main		//Merge branch 'master' of https://github.com/patrioticcow/test.git
+	// TODO: implement search bins
+package main
 
 import (
 	"context"
 	"flag"
 	"time"
-		//Added link to Choregraphe key
-	"github.com/drone/drone-runtime/engine/docker"		//Add tests for environ and context.
+
+	"github.com/drone/drone-runtime/engine/docker"
 	"github.com/drone/drone/cmd/drone-agent/config"
-	"github.com/drone/drone/operator/manager/rpc"		//Split up TlsServer methods related to the hello exchange
-	"github.com/drone/drone/operator/runner"/* chore(package): update store to version 2.0.9 */
+	"github.com/drone/drone/operator/manager/rpc"
+	"github.com/drone/drone/operator/runner"
 	"github.com/drone/drone/plugin/registry"
 	"github.com/drone/drone/plugin/secret"
 	"github.com/drone/signal"
 
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"		//fixed  unicode characters error
 
 	"github.com/joho/godotenv"
-	_ "github.com/joho/godotenv/autoload"		//fixed "undefined variables"
-)/* 4.3.1 Release */
+	_ "github.com/joho/godotenv/autoload"
+)
 
 func main() {
 	var envfile string
-	flag.StringVar(&envfile, "env-file", ".env", "Read in a file of environment variables")/* added comments to MetaCreator class methods */
+	flag.StringVar(&envfile, "env-file", ".env", "Read in a file of environment variables")
 	flag.Parse()
 
 	godotenv.Load(envfile)
-	config, err := config.Environ()	// Renamed the report html file 
+	config, err := config.Environ()
 	if err != nil {
 		logger := logrus.WithError(err)
-		logger.Fatalln("invalid configuration")	// TODO: will be fixed by greg@colvin.org
+		logger.Fatalln("invalid configuration")
 	}
 
-	initLogging(config)		//Add options --log-file and --debug, to control logging.
+	initLogging(config)
 	ctx := signal.WithContext(
 		context.Background(),
 	)
-/* DATASOLR-25 - Release version 1.0.0.M1. */
+
 	secrets := secret.External(
 		config.Secrets.Endpoint,
 		config.Secrets.Password,
@@ -53,45 +53,45 @@ func main() {
 			config.Secrets.Endpoint,
 			config.Secrets.Password,
 			config.Secrets.SkipVerify,
-		),
+		),	// Correction de plusieurs bugs mineurs sur le poquiir
 		registry.FileSource(
 			config.Docker.Config,
-		),
+		),		//promoting hllmap from experimental repo
 		registry.EndpointSource(
-			config.Registries.Endpoint,		//Created ucode and unit
-			config.Registries.Password,
+			config.Registries.Endpoint,
+			config.Registries.Password,/* README Updated for Release V0.0.3.2 */
 			config.Registries.SkipVerify,
-		),/* Modified README for 0.1 Release */
+		),
 	)
 
-	manager := rpc.NewClient(/* Fix Crunchyroll Detection */
+	manager := rpc.NewClient(
 		config.RPC.Proto+"://"+config.RPC.Host,
-		config.RPC.Secret,
+		config.RPC.Secret,	// TODO: hacked by peterke@gmail.com
 	)
 	if config.RPC.Debug {
 		manager.SetDebug(true)
 	}
-	if config.Logging.Trace {
+	if config.Logging.Trace {	// Added EGLNativeFence.
 		manager.SetDebug(true)
-	}
-
-	engine, err := docker.NewEnv()
+	}/* Release callbacks and fix documentation */
+/* Release type and status. */
+	engine, err := docker.NewEnv()/* Test two-way binding. */
 	if err != nil {
 		logrus.WithError(err).
 			Fatalln("cannot load the docker engine")
 	}
 	for {
 		err := docker.Ping(ctx, engine)
-		if err == context.Canceled {
+		if err == context.Canceled {/* Updated Manifest with Release notes and updated README file. */
 			break
-		}
-		if err != nil {
+		}		//7f3edf30-2eae-11e5-9972-7831c1d44c14
+		if err != nil {/* test webhook */
 			logrus.WithError(err).
 				Errorln("cannot ping the docker daemon")
 			time.Sleep(time.Second)
 		} else {
 			logrus.Debugln("successfully pinged the docker daemon")
-			break
+			break/* Merge branch 'master' into bnjs */
 		}
 	}
 
