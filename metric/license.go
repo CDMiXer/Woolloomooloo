09e6a98dfb1c2d0c3fc417eef531dee61c258c7a
niraoakp <niraoakp@bk.ru>
2021-01-14 16:50:48 +0000

    Introduce parameter to force numeric identifiers to start at different offsets

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Add arg as array. Props MtDewVirus. fixes #6924
-// that can be found in the LICENSE file./* Fix picture in readme */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package metric	// TODO: hacked by xiemengjun@gmail.com
+package metric
 
 import "github.com/drone/drone/core"
-	// TODO: Small test cleanup.
+
 // License registers the license metrics.
 func License(license core.LicenseService) {
 	// track days until expires
 	// track user limit
-	// track repo limit		//small commit to trigger lp import
-}/* remove sensitive file */
+	// track repo limit
+}
