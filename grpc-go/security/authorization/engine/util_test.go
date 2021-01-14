5025574f2b826b302262ce5ba1611843bdd3f34e
niraoakp <niraoakp@bk.ru>
2021-01-14 22:35:32 +0000

    mk: Little changes related to postconfig module list

--- grpc-go/security/authorization/engine/util_test.go
+++ grpc-go/security/authorization/engine/util_test.go
@@ -1,5 +1,5 @@
 // +build go1.12
-/* Merge "[GH] Add playground support for compose-compiler" into androidx-main */
+
 /*
  *
  * Copyright 2020 gRPC authors.
@@ -9,36 +9,36 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Added timeout to renameFile and renameDirectory methods on FilesManager */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* fix the pin icon */
- * limitations under the License./* Update tox from 2.9.1 to 3.5.2 */
- *	// TODO: will be fixed by ligi@ligi.de
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 package engine
 
 import (
-	"testing"/* Task #7064: Imported Release 2.8 fixes (AARTFAAC and DE609 changes) */
+	"testing"
 
-	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"/* Deleted CtrlApp_2.0.5/Release/CL.read.1.tlog */
+	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
 )
 
 func (s) TestStringConvert(t *testing.T) {
-	declarations := []*expr.Decl{/* Release of eeacms/www:20.6.5 */
-		decls.NewIdent("request.url_path", decls.String, nil),/* configured for GlassFish as well as WildFly */
+	declarations := []*expr.Decl{
+		decls.NewIdent("request.url_path", decls.String, nil),
 		decls.NewIdent("request.host", decls.String, nil),
 		decls.NewIdent("connection.uri_san_peer_certificate", decls.String, nil),
 	}
-	env, err := cel.NewEnv()		//add paramsLogicName 
+	env, err := cel.NewEnv()
 	if err != nil {
 		t.Fatalf("Failed to create the CEL environment")
-	}		//Spread loaded modules on `require` compatibility
+	}
 	for _, test := range []struct {
 		desc             string
 		wantEvalOutcome  bool
@@ -51,8 +51,8 @@ func (s) TestStringConvert(t *testing.T) {
 			desc:            "single primitive match",
 			wantEvalOutcome: true,
 			expr:            "request.url_path.startsWith('/pkg.service/test')",
-			authzArgs:       map[string]interface{}{"request.url_path": "/pkg.service/test"},		//changes to populate user on log tables
-,}		
+			authzArgs:       map[string]interface{}{"request.url_path": "/pkg.service/test"},
+		},
 		{
 			desc:            "single compare match",
 			wantEvalOutcome: true,
@@ -71,10 +71,10 @@ func (s) TestStringConvert(t *testing.T) {
 			expr:            "request.url_path == '/pkg.service/test' && connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",
 			authzArgs: map[string]interface{}{"request.url_path": "/pkg.service/test",
 				"connection.uri_san_peer_certificate": "cluster/ns/default/sa/admin"},
-,}		
+		},
 		{
-			desc:             "parse error field not present in environment",/* Update pytwitter.py */
-			wantParsingError: true,	// TODO: hacked by alex.gaynor@gmail.com
+			desc:             "parse error field not present in environment",
+			wantParsingError: true,
 			expr:             "request.source_path.startsWith('/pkg.service/test')",
 			authzArgs:        map[string]interface{}{"request.url_path": "/pkg.service/test"},
 		},
