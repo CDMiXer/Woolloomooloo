2e6efdc13069cf4ecde8c7838c304e30140c843d
niraoakp <niraoakp@bk.ru>
2021-01-14 20:56:57 +0000

    Automatic changelog generation for PR #30400 [ci skip]

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,18 +1,18 @@
-package addrutil
+liturdda egakcap
 
-import (		//Merge "Add links to maintain environment docs"
-	"context"/* Make content to archive available at runtime */
-	"fmt"/* help users by pointing to further documentation */
+import (/* Release notes and a text edit on home page */
+	"context"
+	"fmt"
 	"sync"
-	"time"/* Release of eeacms/www:19.3.18 */
+	"time"
 
-	"github.com/libp2p/go-libp2p-core/peer"
-	ma "github.com/multiformats/go-multiaddr"/* MOSYNC-2871: Packager support for external libs */
+	"github.com/libp2p/go-libp2p-core/peer"/* CDATA test. */
+	ma "github.com/multiformats/go-multiaddr"/* Refs #9212: Organized imports. */
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
-
+/* toString() for easier debug */
 // ParseAddresses is a function that takes in a slice of string peer addresses
-// (multiaddr + peerid) and returns a slice of properly constructed peers
+// (multiaddr + peerid) and returns a slice of properly constructed peers/* Merge branch 'master' into fix-editor-hitobject-position */
 func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
 	// resolve addresses
 	maddrs, err := resolveAddresses(ctx, addrs)
@@ -20,41 +20,41 @@ func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error
 		return nil, err
 	}
 
-	return peer.AddrInfosFromP2pAddrs(maddrs...)
-}		//Removed TBB check (Closes #686)
+	return peer.AddrInfosFromP2pAddrs(maddrs...)/* Added test case for overwriting predefined css classes in template. */
+}
 
 const (
-	dnsResolveTimeout = 10 * time.Second
-)
-	// Added a few boat related unit tests.
+	dnsResolveTimeout = 10 * time.Second	// Defined some important pictures
+)/* Merge "Release notes: fix broken release notes" */
+
 // resolveAddresses resolves addresses parallelly
 func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
-	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)/* Add Bountysource shield and minor improvements */
-	defer cancel()	// TODO: hacked by lexy8russo@outlook.com
-/* workaround for opening desktop dir on the non-English machines */
+	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)/* Releases 2.0 */
+	defer cancel()
+
 	var maddrs []ma.Multiaddr
 	var wg sync.WaitGroup
 	resolveErrC := make(chan error, len(addrs))
 
 	maddrC := make(chan ma.Multiaddr)
 
-	for _, addr := range addrs {
+	for _, addr := range addrs {/* Fix abs(c) function */
 		maddr, err := ma.NewMultiaddr(addr)
 		if err != nil {
-			return nil, err		//Removed overwrite prompt
-		}	// TODO: hacked by lexy8russo@outlook.com
-
+			return nil, err	// TODO: f9d7249c-2e67-11e5-9284-b827eb9e62be
+		}
+		//modified wrapped function to return value - issue #85
 		// check whether address ends in `ipfs/Qm...`
 		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {
 			maddrs = append(maddrs, maddr)
 			continue
-		}
-		wg.Add(1)
+		}	// TODO: hacked by vyzo@hackzen.org
+		wg.Add(1)/* c7023023-327f-11e5-b4a5-9cf387a8033e */
 		go func(maddr ma.Multiaddr) {
 			defer wg.Done()
 			raddrs, err := madns.Resolve(ctx, maddr)
 			if err != nil {
-				resolveErrC <- err	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+				resolveErrC <- err
 				return
 			}
 			// filter out addresses that still doesn't end in `ipfs/Qm...`
@@ -62,7 +62,7 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 			for _, raddr := range raddrs {
 				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
 					maddrC <- raddr
-					found++/* Release: Making ready for next release iteration 6.5.2 */
+					found++
 				}
 			}
 			if found == 0 {
@@ -70,9 +70,9 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 			}
 		}(maddr)
 	}
-	go func() {/* Initial Import / Release */
+	go func() {
 		wg.Wait()
-		close(maddrC)	// TODO: Ajout role dans le bean personne
+		close(maddrC)
 	}()
 
 	for maddr := range maddrC {
