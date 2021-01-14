70eebe5c4e79e2ad32b0772c2496f22ab86a1dcc
niraoakp <niraoakp@bk.ru>
2021-01-14 22:09:06 +0000

    Final version 1.5 commit.

--- core/org.go
+++ core/org.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: add rabbitmq provisioning
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* update AppAsset */
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Arreglando formulas.
 //
-// Unless required by applicable law or agreed to in writing, software	// NetKAN generated mods - KeepItStraight-1.1.0
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//- Handwritten DB added
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,16 +19,16 @@ import "context"
 // Organization represents an organization in the source
 // code management system (e.g. GitHub).
 type Organization struct {
-	Name   string/* Release of primecount-0.10 */
-	Avatar string		//App Screenshots for README
-}/* Refactor getCShape into method */
+	Name   string
+	Avatar string
+}
 
 // OrganizationService provides access to organization and
 // team access in the external source code management system
 // (e.g. GitHub).
 type OrganizationService interface {
 	// List returns a list of organization to which the
-	// user is a member./* if no config, but cli request generate temp config */
+	// user is a member.
 	List(context.Context, *User) ([]*Organization, error)
 
 	// Membership returns true if the user is a member
