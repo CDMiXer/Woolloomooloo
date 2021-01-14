5b70a088c4756e0bff80e3bec40e8d10ccf10f36
niraoakp <niraoakp@bk.ru>
2021-01-14 01:18:02 +0000

    Merge "Disable docs generation for android-view-demos" into androidx-master-dev

--- server/static/static.go
+++ server/static/static.go
@@ -1,38 +1,38 @@
 package static
-
+/* Delete Release */
 import (
 	"fmt"
-	"net/http"
+	"net/http"	// sinamo:mac5
 )
-/* [packages_10.03.2] ddns-scripts: merge r29368 */
-type FilesServer struct {
-	baseHRef string/* Updated web demos to include --mathml. */
-	hsts     bool/* optimize structure */
-}
 
-func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
-	return &FilesServer{baseHRef, hsts}/* Bump Express/Connect dependencies. Release 0.1.2. */
+type FilesServer struct {
+	baseHRef string
+	hsts     bool
+}
+		//Merge "Ignore deleted services in minimum version calculation"
+func NewFilesServer(baseHRef string, hsts bool) *FilesServer {/* smartctl: Always print sector size in '-i' output (ticket #166). */
+	return &FilesServer{baseHRef, hsts}
 }
 
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
-	//// If there is no stored static file, we'll redirect to the js app	// Delete keep.lua
+	//// If there is no stored static file, we'll redirect to the js app	// Use the typeFactory instead
 	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
 	//	r.URL.Path = "index.html"
-}//	
-	// Add Base to README
-	if r.URL.Path == "index.html" {
+	//}
+
+	if r.URL.Path == "index.html" {		//Delete MooxSelectContent.html
 		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
-		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}/* Update mk-sh-skel.sh */
+		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
 	}
 
-	w.Header().Set("X-Frame-Options", "DENY")/* Tweaked Checkpoint to improve performance. */
-	// `data:` is need for Monaco editors wiggly red lines	// Remove code scheduling a single beer post for search.
+	w.Header().Set("X-Frame-Options", "DENY")	// Rename geo-page to geo-page.html
+	// `data:` is need for Monaco editors wiggly red lines		//simplify block chaining
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
-	if s.hsts {
+	if s.hsts {	// TODO: hacked by alan.shaw@protocol.ai
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
 	}
 	//
 	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
-	//ServeHTTP(w, r)/* fix(package): update random-http-useragent to version 1.1.17 */
-}
+	//ServeHTTP(w, r)
+}/* trying to fix release issue with newer versino of gpg plugin */
