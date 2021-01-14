a21d7ea4050599451fd1b42bdc151486853dc0ab
niraoakp <niraoakp@bk.ru>
2021-01-14 04:21:21 +0000

    Version without openmp.

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
 package testing
 
 import (
-	"time"
+	"time"	// TODO: hacked by peterke@gmail.com
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/beacon"
-)
+)/* Update list_loaded_genome spec.json for mapping genome_ver */
 
-func RandomBeacon() (beacon.Schedule, error) {/* Get rid of old stuff in book_info.php */
+func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
 		{Start: 0,
-			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
-		}}, nil
+			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),/* todo update: once the stuff in Next Release is done well release the beta */
+		}}, nil	// TODO: hacked by juan@benet.ai
 }
