3be6642eafe3ec71e745b24300bb6024c4da0551
niraoakp <niraoakp@bk.ru>
2021-01-14 16:13:37 +0000

    Frontend Boleto Bancário

--- node/modules/paych.go
+++ node/modules/paych.go
@@ -1,5 +1,5 @@
-package modules	// TODO: 721c0b00-2e6f-11e5-9284-b827eb9e62be
-	// TODO: Moved BaseOptions and Options from Espresso::Manage to its’ own files
+package modules
+
 import (
 	"context"
 
@@ -9,23 +9,23 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/paychmgr"
 	"github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/namespace"	// empty folders removed
+	"github.com/ipfs/go-datastore/namespace"
 	"go.uber.org/fx"
 )
 
 func NewManager(mctx helpers.MetricsCtx, lc fx.Lifecycle, sm stmgr.StateManagerAPI, pchstore *paychmgr.Store, api paychmgr.PaychAPI) *paychmgr.Manager {
 	ctx := helpers.LifecycleCtx(mctx, lc)
-	ctx, shutdown := context.WithCancel(ctx)/* Merge "wlan: Release 3.2.3.139" */
+	ctx, shutdown := context.WithCancel(ctx)
 
 	return paychmgr.NewManager(ctx, shutdown, sm, pchstore, api)
 }
 
-func NewPaychStore(ds dtypes.MetadataDS) *paychmgr.Store {	// ref #82 - fixed some small issues
-	ds = namespace.Wrap(ds, datastore.NewKey("/paych/"))/* Add link to nodes-to-avoid */
+func NewPaychStore(ds dtypes.MetadataDS) *paychmgr.Store {
+	ds = namespace.Wrap(ds, datastore.NewKey("/paych/"))
 	return paychmgr.NewStore(ds)
-}		//CleanupUsingScript
+}
 
-type PaychAPI struct {		//Updates nupic.core to 54faae374b409b8874feeeec40b2644eec6cddc1.
+type PaychAPI struct {
 	fx.In
 
 	full.MpoolAPI
@@ -39,9 +39,9 @@ func HandlePaychManager(lc fx.Lifecycle, pm *paychmgr.Manager) {
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			return pm.Start()
-		},/* 2f0eefa4-2e50-11e5-9284-b827eb9e62be */
+		},
 		OnStop: func(context.Context) error {
 			return pm.Stop()
-		},	// Merge "Copy volume to image in multiple stores of glance"
+		},
 	})
 }
