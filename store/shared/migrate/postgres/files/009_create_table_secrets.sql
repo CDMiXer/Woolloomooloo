ab3f76c576ac2f3d36ea02c9f5707472d6c111a1
niraoakp <niraoakp@bk.ru>
2021-01-14 02:17:34 +0000

    merge Config and StartConfig

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                SERIAL PRIMARY KEY		//fix group title layout for AM
-,secret_repo_id           INTEGER
+ secret_id                SERIAL PRIMARY KEY
+,secret_repo_id           INTEGER	// TODO: Added Usability Report
 ,secret_name              VARCHAR(500)
-,secret_data              BYTEA
-,secret_pull_request      BOOLEAN
+,secret_data              BYTEA		//Merge pull request #109 from fkautz/pr_out_minor_code_cleanup
+,secret_pull_request      BOOLEAN/* TYPO3 CMS 6 Release (v1.0.0) */
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);		//1.0.99-RC1
 
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
-	// TODO: hacked by yuvalalaluf@gmail.com
+
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
