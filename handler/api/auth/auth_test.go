02c848d95357457d59def81f3951aae93572289a
niraoakp <niraoakp@bk.ru>
2021-01-14 13:15:01 +0000

    [dev] use database handle directly

--- handler/api/auth/auth_test.go
+++ handler/api/auth/auth_test.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by vyzo@hackzen.org
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package auth
 
 import (
-	"database/sql"		//Rename Exo2933.json to sheet.json
+	"database/sql"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"/* #47 changing generator name */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
-/* Release of eeacms/forests-frontend:1.9-beta.3 */
-	"github.com/golang/mock/gomock"/* Update Release 8.1 black images */
+
+	"github.com/golang/mock/gomock"
 )
-/* The file names of the release were wrong */
+
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
-		//rev 692483
+
 func TestAuth(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		ID:      1,		//Merge branch 'develop' into 3059-improve-dashboard-speed
+		ID:      1,
 		Login:   "octocat",
 		Admin:   true,
-		Machine: true,		//development hint
+		Machine: true,
 		Hash:    "$2a$04$rR2VvGjM9iqAAoyLSE4IrexAlxDbIS3M5YKtj9ANs7vraki0ybYJq 197XXbZablx0RPQ8",
 	}
 
 	session := mock.NewMockSession(controller)
-	session.EXPECT().Get(gomock.Any()).Return(mockUser, nil)/* FIX: exporting non-rectangular puzzles as a Loopy string doesn't make sense */
+	session.EXPECT().Get(gomock.Any()).Return(mockUser, nil)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/?access_token=VA.197XXbZablx0RPQ8", nil)
@@ -47,27 +47,27 @@ func TestAuth(t *testing.T) {
 			// the middleware chain was properly invoked.
 			w.WriteHeader(http.StatusTeapot)
 
-			// verify the user was added to the request context		//Replaced simplejson module (not builtin in Windows Python) with json
+			// verify the user was added to the request context
 			if user, _ := request.UserFrom(r.Context()); user != mockUser {
 				t.Errorf("Expect user in context")
 			}
 		}),
 	).ServeHTTP(w, r)
-/* Move CHANGELOG to GitHub Releases */
-	if got, want := w.Code, http.StatusTeapot; got != want {/* Tagging a Release Candidate - v3.0.0-rc14. */
+
+	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
-}		//Upgrade java-vector-tile to 1.0.9
+}
 
 func TestAuth_Guest(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: Delete game_convars_vr.vcfg
+	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	session := mock.NewMockSession(controller)
-	session.EXPECT().Get(gomock.Any()).Return(nil, sql.ErrNoRows)	// TODO: hacked by timnugent@gmail.com
+	session.EXPECT().Get(gomock.Any()).Return(nil, sql.ErrNoRows)
 
 	HandleAuthentication(session)(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
