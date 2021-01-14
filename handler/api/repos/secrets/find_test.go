ae1b7191965007fac79d351ec09c42f182815df2
niraoakp <niraoakp@bk.ru>
2021-01-14 12:27:18 +0000

    Merge "Kconfig: Enable config to allow vmalloc savings"

--- handler/api/repos/secrets/find_test.go
+++ handler/api/repos/secrets/find_test.go
@@ -1,68 +1,38 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by brosner@gmail.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* app-i18n/ibus-anthy: ~amd64 keywording+cleanup. */
 package secrets
 
-import (	// Replace tr("KDocker") with qApp->applicationName().
+import (		//Allow use of Helix queues for rest of OSX machines
 	"context"
 	"encoding/json"
-	"net/http"	// TODO: will be fixed by timnugent@gmail.com
+	"net/http"
 	"net/http/httptest"
 	"testing"
-/* install only for Release build */
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"		//Changed name to connect-rewrite
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-/* Release version 0.2.0 beta 2 */
-	"github.com/go-chi/chi"	// TODO: hacked by xiemengjun@gmail.com
+
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Added match-statement test
-)		//Update codebook_routes.csv
+	"github.com/google/go-cmp/cmp"
+)	// Update buttons in PasswordDialog to be called "Login" instead of OK
 
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// comment out demo until I can fix it
-
-	repos := mock.NewMockRepositoryStore(controller)		//Added TM1829
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-
-	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
-
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")		//No changes, just some renaming.
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Reference GitHub Releases from the changelog */
-	)
-
-	HandleFind(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//hackSchema
-	}
-
-	got, want := &core.Secret{}, dummySecretScrubbed
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Open GitHub in new tab */
-	}
-}
-
-func TestHandleFind_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)	// Removed instructions from the source, moved to readme
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
-	c := new(chi.Context)
+	secrets := mock.NewMockSecretStore(controller)/* add 'аударушы' (translator) as a noun */
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
+
+	c := new(chi.Context)		//Refactor output into options flag
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
@@ -73,23 +43,53 @@ func TestHandleFind_RepoNotFound(t *testing.T) {
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
+	HandleFind(repos, secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &core.Secret{}, dummySecretScrubbed
+	json.NewDecoder(w.Body).Decode(got)/* job #11437 - updated Release Notes and What's New */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}/* Fix bug #577792 */
+
+func TestHandleFind_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+/* Release 6.2.1 */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)	// use wayf as virtual slot label.
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//Remove long dead code for handling vector shift by immediate builtins.
+	)
+/* Deleted msmeter2.0.1/Release/mt.command.1.tlog */
 	HandleFind(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)/* Release of eeacms/ims-frontend:0.6.4 */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}
+}		//Add Brotli compatibility based on issue #48
 
-func TestHandleFind_SecretNotFound(t *testing.T) {
+func TestHandleFind_SecretNotFound(t *testing.T) {		//Brought API back to the repo. abstracted into SlimevoidLib
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* fixed Release build */
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
