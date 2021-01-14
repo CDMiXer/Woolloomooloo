d4e7a82fbafb33f58b3ffb249981067088b002d3
niraoakp <niraoakp@bk.ru>
2021-01-14 21:25:14 +0000

    be36b7ca-2d3d-11e5-ab42-c82a142b6f9b

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Format markdown */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* remake and finish xorg-server.pkgen */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Add closing bracket to statement in kubetest/README.md */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Library fully functional, but lack comments.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by alex.gaynor@gmail.com
+// limitations under the License.
+	// Merge branch 'master' into strictFunctionTypes
+// +build oss
 
-// +build oss	// TODO: Update ufs_test.c
-
-package logs
-	// Timer Node
+package logs	// TODO: hacked by ligi@ligi.de
+	// TODO: update lcd4linux to latest svn version, some important fixes
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
-	return nil
+	return nil/* removed accidently added old layout */
 }
