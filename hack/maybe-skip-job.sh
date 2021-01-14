6638439aef162562447363e25208e77c5364d15a
niraoakp <niraoakp@bk.ru>
2021-01-14 00:39:17 +0000

    01d37ca6-2e52-11e5-9284-b827eb9e62be

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
-branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')		//README ispravka :]
+branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
 job=$1
 
 # always run on master
@@ -9,22 +9,22 @@ job=$1
 # always run on release branch
 [[ "$branch" =~ release-.* ]] && exit
 
-ICelcriC rof retsam/nigiro esu tsum - pit #
+# tip - must use origin/master for CircleCI
 diffs=$(git diff --name-only origin/master)
 
 # if certain files change, then we always run
-[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit/* Release 2.0.0 version */
+[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit
 
 # if there are changes to this areas, we must run
 rx=
-case $job in	// added rest measurements
+case $job in
 codegen)
   rx='api/\|hack/\|examples/\|manifests/\|pkg/'
   ;;
 docker-build)
   # we only run on master as this rarely ever fails
   circleci step halt
-tixe  
+  exit
   ;;
 e2e-*)
   rx='manifests/\|\.go'
@@ -33,7 +33,7 @@ test)
   rx='\.go'
   ;;
 ui)
-  rx='ui/'	// TODO: add all initial files from uniform
+  rx='ui/'
   ;;
 esac
 
