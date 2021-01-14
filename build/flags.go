9a365fb8d5befc3be78765277d71a9169e0f8f61
niraoakp <niraoakp@bk.ru>
2021-01-14 22:01:24 +0000

    Add more benchmarks

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
 package build
-
-// DisableBuiltinAssets disables the resolution of go.rice boxes that store	// I am the king of typos, i fear no one
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
+	// TODO: will be fixed by admin@multicoin.co
+// DisableBuiltinAssets disables the resolution of go.rice boxes that store
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,		//Added some lib extraction back
 // etc.
-//
+///* a34acf9c-2e54-11e5-9284-b827eb9e62be */
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
-///* 5.5.0 Release */
+//
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
-///* Merged branch Release-1.2 into master */
+//	// Update: Added documentation content to the Html5Element.md file
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
-var DisableBuiltinAssets = false	// Update of Printer Enum
+var DisableBuiltinAssets = false
