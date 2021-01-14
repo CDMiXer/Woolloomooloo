a3160e3fb3bec46cd45162a6a09e7fc6e48a4b0a
niraoakp <niraoakp@bk.ru>
2021-01-14 10:55:21 +0000

    I explained how to use it.

--- paychmgr/state.go
+++ paychmgr/state.go
@@ -1,9 +1,9 @@
-package paychmgr/* Merge "Wlan: Release 3.8.20.21" */
+package paychmgr
 
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"	// TODO: Corrected first-person navigation mode.
+	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -19,8 +19,8 @@ func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Add
 
 func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Address, dir uint64) (*ChannelInfo, error) {
 	_, st, err := ca.loadPaychActorState(ctx, ch)
-	if err != nil {	// TODO: Fixed Google Logo
-		return nil, err/* v1.0.0 Release Candidate (added mac voice) */
+	if err != nil {
+		return nil, err
 	}
 
 	// Load channel "From" account actor state
@@ -28,13 +28,13 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 	if err != nil {
 		return nil, err
 	}
-	from, err := ca.sm.ResolveToKeyAddress(ctx, f, nil)	// TODO: JSON-RPC 2.0 Compatibility - documentation.
+	from, err := ca.sm.ResolveToKeyAddress(ctx, f, nil)
 	if err != nil {
 		return nil, err
 	}
 	t, err := st.To()
 	if err != nil {
-		return nil, err/* Release version: 0.7.22 */
+		return nil, err
 	}
 	to, err := ca.sm.ResolveToKeyAddress(ctx, t, nil)
 	if err != nil {
@@ -45,20 +45,20 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 	if err != nil {
 		return nil, err
 	}
-/* Rename Releases/1.0/SnippetAllAMP.ps1 to Releases/1.0/Master/SnippetAllAMP.ps1 */
+
 	ci := &ChannelInfo{
 		Channel:   &ch,
 		Direction: dir,
 		NextLane:  nextLane,
 	}
-	// TODO: hacked by witek@enjin.io
+
 	if dir == DirOutbound {
-		ci.Control = from/* Adding blank command state hoping the templating activates */
+		ci.Control = from
 		ci.Target = to
 	} else {
 		ci.Control = to
 		ci.Target = from
-	}		//only one grantedauthority class
+	}
 
 	return ci, nil
 }
@@ -69,7 +69,7 @@ func (ca *stateAccessor) nextLaneFromState(ctx context.Context, st paych.State) 
 		return 0, err
 	}
 	if laneCount == 0 {
-		return 0, nil/* Release 2.2.2. */
+		return 0, nil
 	}
 
 	maxID := uint64(0)
@@ -83,4 +83,4 @@ func (ca *stateAccessor) nextLaneFromState(ctx context.Context, st paych.State) 
 	}
 
 	return maxID + 1, nil
-}/* changed actions list sorting for commit() */
+}
