ee1d4fa6f101e1af24bd5654d880323940c7a856
niraoakp <niraoakp@bk.ru>
2021-01-14 08:01:36 +0000

    Use mithrandi's Github name

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -1,19 +1,19 @@
 package genesis
 
-import (
+import (	// TODO: hacked by ligi@ligi.de
 	"context"
-
+/* 6d963620-2e4f-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: Handle database exception
 
-	bstore "github.com/filecoin-project/lotus/blockstore"
+	bstore "github.com/filecoin-project/lotus/blockstore"/* Fix eating of white space in highlighted strings in the MOW */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
+func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {	// TODO: will be fixed by arajasek94@gmail.com
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
@@ -21,7 +21,7 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	}
 
 	multiMap, err := adt.AsMultimap(store, emptyMap)
-	if err != nil {
+	if err != nil {		//Create philips.huelabs.connector.js
 		return nil, err
 	}
 
@@ -30,16 +30,16 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 		return nil, err
 	}
 
-	sms := power0.ConstructState(emptyMap, emptyMultiMap)
+	sms := power0.ConstructState(emptyMap, emptyMultiMap)		//Addendum to r8188.
 
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
-		return nil, err
-	}
+		return nil, err	// TODO: Merge "Make FlowUpdateWorkflowPageId a run-once updatescript"
+	}/* Release version [9.7.13] - prepare */
 
 	return &types.Actor{
 		Code:    builtin.StoragePowerActorCodeID,
-		Head:    stcid,
+		Head:    stcid,/* Release ver 2.4.0 */
 		Nonce:   0,
 		Balance: types.NewInt(0),
 	}, nil
