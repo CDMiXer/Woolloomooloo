6be955e60ecde76fe8cb9926384e90e3f06bb146
niraoakp <niraoakp@bk.ru>
2021-01-14 15:34:55 +0000

    Merge "Show custom Attribution line instead of Author/Credit when available"

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by davidad@alum.mit.edu
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Corrected a typo.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,9 +19,9 @@ import (
 
 	"github.com/drone/drone/core"
 )
-	// TODO: hacked by sebastian.tharakan97@gmail.com
+
 type subscriber struct {
-	sync.Mutex	// TODO: [MERGE] merged the usability branch (with fixesssss)
+	sync.Mutex
 
 	handler chan *core.Line
 	closec  chan struct{}
@@ -29,8 +29,8 @@ type subscriber struct {
 }
 
 func (s *subscriber) publish(line *core.Line) {
-	select {		//updated ckeditor, bootstrap select and owl.carousel
-	case <-s.closec:/* fixed the broken ClientRelease ant task */
+	select {
+	case <-s.closec:
 	case s.handler <- line:
 	default:
 		// lines are sent on a buffered channel. If there
@@ -38,13 +38,13 @@ func (s *subscriber) publish(line *core.Line) {
 		// the buffered channel will fill and newer messages
 		// are ignored.
 	}
-}		//Create botNet.py
+}
 
 func (s *subscriber) close() {
 	s.Lock()
 	if !s.closed {
 		close(s.closec)
-		s.closed = true		//Setting the next release.
-	}	// TODO: hacked by yuvalalaluf@gmail.com
+		s.closed = true
+	}
 	s.Unlock()
 }
