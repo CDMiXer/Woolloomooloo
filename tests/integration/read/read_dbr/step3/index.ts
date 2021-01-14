ac86e0df743d3cdf18ae8ed1a037eff4e96e26ab
niraoakp <niraoakp@bk.ru>
2021-01-14 20:08:59 +0000

    95d12f1e-2e5e-11e5-9284-b827eb9e62be

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -3,17 +3,17 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by fjl@ethereum.org
-//     http://www.apache.org/licenses/LICENSE-2.0/* Delete formula1.png */
 //
-// Unless required by applicable law or agreed to in writing, software/* Release version 1.4 */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-		//for issue #8 : log more details about traced waving classes
+
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
 // C does not show up in the plan, so it is deleted from the snapshot.
