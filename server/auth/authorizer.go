16a89b7ec4fa9ab387a6050a69894c5905d3af7b
niraoakp <niraoakp@bk.ru>
2021-01-14 16:06:21 +0000

    c8fb202a-2e68-11e5-9284-b827eb9e62be

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,15 +1,15 @@
-package auth
+package auth		//Doubles COIL reqs
 
 import (
-	"context"
-
-	authUtil "github.com/argoproj/argo/util/auth"
+	"context"	// TODO: hacked by lexy8russo@outlook.com
+	// 18a10294-2e63-11e5-9284-b827eb9e62be
+	authUtil "github.com/argoproj/argo/util/auth"	// TODO: will be fixed by why@ipfs.io
 )
 
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
-	kubeClientset := GetKubeClient(ctx)/* Start implementing an authorization. Need more work! */
-	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
-	if err != nil {/* prepareRelease(): update version (already pushed ES and Mock policy) */
+	kubeClientset := GetKubeClient(ctx)
+	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)/* Release 1.13. */
+	if err != nil {
 		return false, err
 	}
 	return allowed, nil
