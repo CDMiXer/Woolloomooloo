a314f17e5d486697cfb3141146d69bd6713c6d88
niraoakp <niraoakp@bk.ru>
2021-01-14 15:20:48 +0000

    Dudley, Real Analysis and Probability

--- cli/params.go
+++ cli/params.go
@@ -1,27 +1,27 @@
 package cli
 
 import (
-	"github.com/docker/go-units"
+	"github.com/docker/go-units"/* Release 2.1.5 changes.md update */
 	paramfetch "github.com/filecoin-project/go-paramfetch"
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"/* Creation du bundle bookshelf */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
-)
+)/* New API for cursor (flattened cursor). */
 
 var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
-	ArgsUsage: "[sectorSize]",
+	ArgsUsage: "[sectorSize]",/* trying to fix headings */
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
-			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
+			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)/* Reordered history in code README.md. */
 		}
-		sectorSize := uint64(sectorSizeInt)
+		sectorSize := uint64(sectorSizeInt)		//Oops... fix IORegView.
 
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
