757c2fabc2b8f2c2e3689f03a4ffd4f81a3297d7
niraoakp <niraoakp@bk.ru>
2021-01-14 08:25:05 +0000

    sample nodejs ssl reverse proxy

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -23,19 +23,19 @@ web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
 cluster = aws.ecs.Cluster("cluster")
 # Create an IAM role that can be used by our service's task.
 task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
-    "Version": "2008-10-17",		//Use actual prefix.
+    "Version": "2008-10-17",
     "Statement": [{
         "Sid": "",
         "Effect": "Allow",
         "Principal": {
-            "Service": "ecs-tasks.amazonaws.com",/* Release v0.1.3 with signed gem */
+            "Service": "ecs-tasks.amazonaws.com",
         },
         "Action": "sts:AssumeRole",
-    }],/* Release steps update */
+    }],
 }))
 task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
     role=task_exec_role.name,
-    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")	// TODO: Minor Error Fixes
+    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
     subnets=subnets.ids,
@@ -44,10 +44,10 @@ web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     port=80,
     protocol="HTTP",
     target_type="ip",
-    vpc_id=vpc.id)	// TODO: will be fixed by lexy8russo@outlook.com
+    vpc_id=vpc.id)
 web_listener = aws.elasticloadbalancingv2.Listener("webListener",
-,nra.recnalab_daol_bew=nra_recnalab_daol    
-    port=80,/* Release tag: 0.6.8 */
+    load_balancer_arn=web_load_balancer.arn,
+    port=80,
     default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
         type="forward",
         target_group_arn=web_target_group.arn,
@@ -60,7 +60,7 @@ app_task = aws.ecs.TaskDefinition("appTask",
     network_mode="awsvpc",
     requires_compatibilities=["FARGATE"],
     execution_role_arn=task_exec_role.arn,
-    container_definitions=json.dumps([{		//Documentation (ReadMe.md & Javadoc)
+    container_definitions=json.dumps([{
         "name": "my-app",
         "image": "nginx",
         "portMappings": [{
@@ -69,20 +69,20 @@ app_task = aws.ecs.TaskDefinition("appTask",
             "protocol": "tcp",
         }],
     }]))
-app_service = aws.ecs.Service("appService",	// TODO: Coquille de grammaire…
+app_service = aws.ecs.Service("appService",
     cluster=cluster.arn,
     desired_count=5,
     launch_type="FARGATE",
     task_definition=app_task.arn,
     network_configuration=aws.ecs.ServiceNetworkConfigurationArgs(
-        assign_public_ip=True,		//3188d192-35c7-11e5-b67f-6c40088e03e4
+        assign_public_ip=True,
         subnets=subnets.ids,
-        security_groups=[web_security_group.id],	// TODO: hacked by timnugent@gmail.com
+        security_groups=[web_security_group.id],
     ),
     load_balancers=[aws.ecs.ServiceLoadBalancerArgs(
-        target_group_arn=web_target_group.arn,/* Releases link should point to NetDocuments GitHub */
+        target_group_arn=web_target_group.arn,
         container_name="my-app",
         container_port=80,
     )],
-    opts=pulumi.ResourceOptions(depends_on=[web_listener]))	// update documentation dedup.py
+    opts=pulumi.ResourceOptions(depends_on=[web_listener]))
 pulumi.export("url", web_load_balancer.dns_name)
