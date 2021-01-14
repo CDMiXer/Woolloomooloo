9e247335968ff92752f87feaa71c3eeadc34fd0c
niraoakp <niraoakp@bk.ru>
2021-01-14 23:08:26 +0000

    MPI tmp fold problem for search workflow

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -4,29 +4,29 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* 9485ffc0-2e65-11e5-9284-b827eb9e62be */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'master' into mobileColumns */
+// See the License for the specific language governing permissions and		//Merge "[FIX] JSONModel: Fixed issue with property binding on root"
 // limitations under the License.
-
-package operations
-
+/* Model: Release more data in clear() */
+package operations		//fix one bug, the begin and the end in a row, show the wrong number
+/* Merge "Release Japanese networking guide" */
 import (
 	"time"
-)
+)/* Release 1.0.0 bug fixing and maintenance branch */
 
 // LogEntry is a row in the logs for a running compute service
 type LogEntry struct {
 	ID string
-	// Timestamp is a Unix timestamp, in milliseconds
+	// Timestamp is a Unix timestamp, in milliseconds/* Re #26643 Release Notes */
 	Timestamp int64
 	Message   string
 }
-
-// ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:
+/* Laravel 7.x Released */
+// ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:	// TODO: Closes #112: Rewrite document-classification transformer to spark
 // - Full URN: "<namespace>::<alloc>::<type>::<name>"
 // - Type + Name: "<type>::<name>"
 // - Name: "<name>"
@@ -43,7 +43,7 @@ type LogQuery struct {
 	StartTime *time.Time `url:"startTime,unix"`
 	// EndTime is an optional time indiciating that only logs from before this time should be produced.
 	EndTime *time.Time `url:"endTime,unix"`
-	// ResourceFilter is a string indicating that logs should be limited to a resource or resources
+	// ResourceFilter is a string indicating that logs should be limited to a resource or resources/* Release note for #721 */
 	ResourceFilter *ResourceFilter `url:"resourceFilter"`
 }
 
