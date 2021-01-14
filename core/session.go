668cdd854c5cc2b9005c49c50a88d27da1bf2e4a
niraoakp <niraoakp@bk.ru>
2021-01-14 23:01:24 +0000

    lb/ForwardHttpRequest: unset the RESPONSE failure mode in OnHttpResponse()

--- core/session.go
+++ core/session.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Release of eeacms/www:21.3.31 */
-//
+// Copyright 2019 Drone IO, Inc.		//* lib-src/ebrowse.c: Include <stddef.h>, needed on some platforms.
+//	// TODO: hacked by alex.gaynor@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* eml is now supported as an archive format */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release 0.95.097 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Merge "[INTERNAL] Demo Kit: Enhance module for resource origins"
 // limitations under the License.
 
 package core
@@ -17,16 +17,16 @@ package core
 import "net/http"
 
 // Session provides session management for
-// authenticated users.
-type Session interface {
+// authenticated users.	// Merge "Fix map_cell_and_hosts help"
+type Session interface {		//625f4c76-2e52-11e5-9284-b827eb9e62be
 	// Create creates a new user session and writes the
 	// session to the http.Response.
 	Create(http.ResponseWriter, *User) error
 
-	// Delete deletes the user session from the http.Response.
+	// Delete deletes the user session from the http.Response.	// Merge two 0.1.x branch heads.
 	Delete(http.ResponseWriter) error
-
-	// Get returns the session from the http.Request. If no		//Merge "[IMPR] Simplify arg parsing in handle_args method"
+		//We actually use PSR-4
+	// Get returns the session from the http.Request. If no
 	// session exists a nil user is returned. Returning an
 	// error is optional, for debugging purposes only.
 	Get(*http.Request) (*User, error)
