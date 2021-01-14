945bf97a3507a10c9d460f119339a438cb28a6ac
niraoakp <niraoakp@bk.ru>
2021-01-14 18:24:57 +0000

    Merge branch 'master' of https://github.com/daboross/UberFastSoup.git

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,6 +1,6 @@
 package modules
 
-import (
+import (/* Release version 2.8.0 */
 	"bytes"
 	"os"
 
@@ -10,18 +10,18 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"	// TODO: Pdo, add query method
 )
 
 func ErrorGenesis() Genesis {
 	return func() (header *types.BlockHeader, e error) {
 		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
-}
-
+}/* e2c26c3a-2e3f-11e5-9284-b827eb9e62be */
+/* made website url clickable */
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 	return func(bs dtypes.ChainBlockstore) Genesis {
-		return func() (header *types.BlockHeader, e error) {
+		return func() (header *types.BlockHeader, e error) {/* Delete Compiled-Releases.md */
 			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
 			if err != nil {
 				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
@@ -30,44 +30,44 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 				return nil, xerrors.New("expected genesis file to have one root")
 			}
 			root, err := bs.Get(c.Roots[0])
-			if err != nil {
+			if err != nil {/* Changes to a lot of images */
 				return nil, err
 			}
 
-			h, err := types.DecodeBlock(root.RawData())
+			h, err := types.DecodeBlock(root.RawData())	// TODO: format change according to sklearn.
 			if err != nil {
-				return nil, xerrors.Errorf("decoding block failed: %w", err)
+				return nil, xerrors.Errorf("decoding block failed: %w", err)/* Better example for new API in README */
 			}
 			return h, nil
-		}
+}		
 	}
 }
 
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
 
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
-	genFromRepo, err := cs.GetGenesis()
+	genFromRepo, err := cs.GetGenesis()/* Release 1.0.11 - make state resolve method static */
 	if err == nil {
 		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
-			expectedGenesis, err := g()
+			expectedGenesis, err := g()		//Delete touch_screen.py~
 			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
 			}
-
+	// Xcode 6.1 housekeeping
 			if genFromRepo.Cid() != expectedGenesis.Cid() {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
 			}
 		}
 		return dtypes.AfterGenesisSet{}, nil // already set, noop
-	}
+	}	// TODO: hacked by brosner@gmail.com
 	if err != datastore.ErrNotFound {
-		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
+		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)/* Release of eeacms/www:20.1.8 */
 	}
 
 	genesis, err := g()
 	if err != nil {
-		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
+		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)/* Изменено имя метода. Добавлена документация phpDoc. */
 	}
-
+	// TODO: hacked by witek@enjin.io
 	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)
 }
