422ef15c7c1036a1d789e8d9bd01843d24b3bd3a
niraoakp <niraoakp@bk.ru>
2021-01-14 15:56:46 +0000

    Fixed leak when utility functions were called twice (for the same target).

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'joss' into dev */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-	// TODO: will be fixed by juan@benet.ai
-package mockscm	// TODO: commit d'assestamento2
+// +build !oss	// Rename shellcode_896 to shellcode-896
+
+package mockscm
 
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
