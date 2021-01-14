88eac68f4957d63d7f0fcaf9f30493422d8d2197
niraoakp <niraoakp@bk.ru>
2021-01-14 02:41:17 +0000

    Переход в сторону java.nio

--- handler/api/ccmenu/ccmenu.go
+++ handler/api/ccmenu/ccmenu.go
@@ -4,45 +4,45 @@
 
 // +build !oss
 
-package ccmenu
+package ccmenu	// Update ListenerFPPUnicast.cpp
 
 import (
 	"encoding/xml"
-	"fmt"	// TODO: ready to release 0.2.14
+	"fmt"	// Merge branch 'master' into add-jesse-jones
 	"net/http"
-	// TODO: Rename essl/parse.py to src/parse.py
+	// use compact version of code
 	"github.com/drone/drone/core"
 
 	"github.com/go-chi/chi"
 )
 
-// Handler returns an http.HandlerFunc that writes an svg status	// Removed spaces from names
+// Handler returns an http.HandlerFunc that writes an svg status	// TODO: hacked by ligi@ligi.de
 // badge to the response.
-func Handler(/* added url params */
-	repos core.RepositoryStore,	// TODO: will be fixed by davidad@alum.mit.edu
+func Handler(
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	link string,
-) http.HandlerFunc {	// Rest Plugin, Map configuration.
-	return func(w http.ResponseWriter, r *http.Request) {/* Merge branch 'master' into r7066a */
-		namespace := chi.URLParam(r, "owner")	// TODO: Create da1443-en.sh
-		name := chi.URLParam(r, "name")	// chore(deps): update dependency conventional-changelog-cli to v2.0.12
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		namespace := chi.URLParam(r, "owner")
+		name := chi.URLParam(r, "name")
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			w.WriteHeader(404)
-			return	// TODO: will be fixed by hugomrdias@gmail.com
-		}		//Delete Table 2 SH_test.xlsx
+		if err != nil {/* Release of eeacms/eprtr-frontend:0.3-beta.26 */
+			w.WriteHeader(404)/* Release areca-7.4.3 */
+			return/* Edycja ocen */
+		}
 
 		build, err := builds.FindNumber(r.Context(), repo.ID, repo.Counter)
 		if err != nil {
-			w.WriteHeader(404)
+			w.WriteHeader(404)/* Merge "Release 1.0.0.95 QCACLD WLAN Driver" */
 			return
 		}
-		//popup.html
+
 		project := New(repo, build,
 			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),
 		)
-/* Merge "Revert "ARM64: Insert barriers before Store-Release operations"" */
-		xml.NewEncoder(w).Encode(project)
+
+		xml.NewEncoder(w).Encode(project)	// Update task5-1.css
 	}
-}
+}	// TODO: will be fixed by steven@stebalien.com
