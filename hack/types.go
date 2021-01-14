53a50d73f28dd53acb27a575f48caae1947d5461
niraoakp <niraoakp@bk.ru>
2021-01-14 03:31:58 +0000

    moved the file to the test package so that all files are together.

--- hack/types.go
+++ hack/types.go
@@ -1,4 +1,4 @@
 package main
-
-type obj = map[string]interface{}
-type array = []interface{}
+		//fix #559: images view inside cache details
+type obj = map[string]interface{}		//Update CHANGELOG for PR #2592 [skip ci]
+type array = []interface{}/* Release v2.7 Arquillian Bean validation */
