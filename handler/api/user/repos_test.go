653e3de90de3925e909efd55397d66be39180cf3
niraoakp <niraoakp@bk.ru>
2021-01-14 23:43:51 +0000

    add physics functions

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Delete ._data_cleaning.R */
+
 package user
-/* Bump version, add support for keyboard sidedness. */
+
 import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
-	"testing"	// TODO: will be fixed by aeongrp@outlook.com
+	"net/http/httptest"/* Added compressed version. */
+	"testing"/* Delete prim_conv.cpp */
 
-"srorre/ipa/reldnah/enord/enord/moc.buhtig"	
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/request"/* Create new file TODO Release_v0.1.3.txt, which contains the tasks for v0.1.3. */
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-	// TODO: 1a44f1e0-2e55-11e5-9284-b827eb9e62be
+
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
 )
-
+/* mac80211: fix WPA auth on WDS station interfaces (#9227) */
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)/* templatefilters: add parameterized fill function */
 }
-
+	// TODO: fix repo url in git clone
 func TestResitoryList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -36,38 +36,38 @@ func TestResitoryList(t *testing.T) {
 
 	mockRepos := []*core.Repository{
 		{
-			Namespace: "octocat",
-			Name:      "hello-world",
+			Namespace: "octocat",		//Merge "Fix undefined $project"
+			Name:      "hello-world",/* Merge branch 'release/2.0.0-SM3' */
 			Slug:      "octocat/hello-world",
 		},
-	}/* Release 0.95.200: Crash & balance fixes. */
-
+	}/*  - Release all adapter IP addresses when using /release */
+/* Added API to retrieve device data */
 	repos := mock.NewMockRepositoryStore(controller)
-)lin ,sopeRkcom(nruteR.)DI.resUkcom ,)(ynA.kcomog(tsiL.)(TCEPXE.soper	
-
-	w := httptest.NewRecorder()
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
+/* Project Bitmark Release Schedule Image */
+	w := httptest.NewRecorder()	// Merge branch 'master' into relocate_rotate
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)
+	)/* rev 795028 */
 
-	HandleRepos(repos)(w, r)
+	HandleRepos(repos)(w, r)/* Release 0.10.7. Update repoze. */
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Repository{}, mockRepos
-	json.NewDecoder(w.Body).Decode(&got)	// TODO: hacked by yuvalalaluf@gmail.com
+	json.NewDecoder(w.Body).Decode(&got)	// SLTS-104 Add API to get properties point base on xid
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)/* Release Version 0.4 */
-	}	// TODO: will be fixed by witek@enjin.io
-}/* #105 - Release 1.5.0.RELEASE (Evans GA). */
+		t.Errorf(diff)
+	}
+}
 
 func TestResitoryListErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{	// Add support for send redirect
+	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
 	}
@@ -77,18 +77,18 @@ func TestResitoryListErr(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* Release 0.7.1 */
+	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
 
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusInternalServerError; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Color usernames! */
+	}
 
 	got, want := &errors.Error{}, errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)	// TODO: will be fixed by why@ipfs.io
-	if diff := cmp.Diff(got, want); len(diff) > 0 {/* 3.4.0 Release */
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
 }
