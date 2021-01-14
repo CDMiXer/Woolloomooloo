0f67e4c4bc7227c6c0d5b7844085b76b47c1dc8b
niraoakp <niraoakp@bk.ru>
2021-01-14 21:37:20 +0000

    Control Button

--- tests/examples/dynamic-provider/simple/index.ts
+++ tests/examples/dynamic-provider/simple/index.ts
@@ -1,31 +1,31 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by juan@benet.ai
+
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-/* UndineMailer v0.1.0 : Added license description. */
+
 class OperatorProvider implements dynamic.ResourceProvider {
     private op: (l: number, r: number) => any;
-	// TODO: hacked by nagydani@epointsystem.org
+
     constructor(op: (l: number, r: number) => any) {
         this.op = op;
     }
 
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
-    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }		//Some minor wording
+    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
-    public create(inputs: any) { return Promise.resolve({ id: "0", outs: this.op(Number(inputs.left), Number(inputs.right)) }); }		//refactor ls command to use new APIs
+    public create(inputs: any) { return Promise.resolve({ id: "0", outs: this.op(Number(inputs.left), Number(inputs.right)) }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: this.op(Number(news.left), Number(news.right)) }); }
 }
 
-class DivProvider extends OperatorProvider {	// TODO: hacked by mikeal.rogers@gmail.com
-    constructor() {/* Rename Release.md to release.md */
+class DivProvider extends OperatorProvider {
+    constructor() {
         super((left: number, right: number) => <any>{ quotient: Math.floor(left / right), remainder: left % right });
     }
 
     public async check(olds: any, news: any) {
         return {
             inputs: news,
-            failures: news.right == 0 ? [ { property: "right", reason: "divisor must be non-zero" } ] : [],	// TODO: will be fixed by alex.gaynor@gmail.com
+            failures: news.right == 0 ? [ { property: "right", reason: "divisor must be non-zero" } ] : [],
         }
     }
 }
@@ -39,29 +39,29 @@ class Add extends dynamic.Resource {
         super(Add.provider, name, {left: left, right: right, sum: undefined}, undefined);
     }
 }
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 class Mul extends dynamic.Resource {
-    public readonly product: pulumi.Output<number>;/* added factory method to convert an array to a request */
+    public readonly product: pulumi.Output<number>;
 
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ product: left * right });
-	// TODO: will be fixed by aeongrp@outlook.com
+
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Mul.provider, name, {left: left, right: right, product: undefined}, undefined);
-    }		//Ignore stderr message
+    }
 }
 
 class Sub extends dynamic.Resource {
     public readonly difference: pulumi.Output<number>;
-/* Update lesson-9.md */
+
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ difference: left - right });
 
-    constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {/* travis-ci.org provides 1.9.3[-preview1]; also, test against rbx-2.0 */
+    constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Sub.provider, name, {left: left, right: right, difference: undefined}, undefined);
-    }	// TODO: will be fixed by vyzo@hackzen.org
+    }
 }
 
 class Div extends dynamic.Resource {
-    public readonly quotient: pulumi.Output<number>;/* add Codeclimate Maintainability */
+    public readonly quotient: pulumi.Output<number>;
     public readonly remainder: pulumi.Output<number>;
 
     private static provider = new DivProvider();
