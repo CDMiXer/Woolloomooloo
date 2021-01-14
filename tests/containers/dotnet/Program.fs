ad6ad5a3be04cb7b4bf4ab58a151944b8a0aa6bb
niraoakp <niraoakp@bk.ru>
2021-01-14 04:33:30 +0000

    Version 2.3.12

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
 ﻿module Program
-/* Release v2.4.2 */
+
 open System
 open Pulumi.FSharp
 
-let infra () =
+let infra () =	// TODO: Merge "Normalize image when using PUT on Glance v2"
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
-  Console.WriteLine("Hello from {0}", runtime)
+  Console.WriteLine("Hello from {0}", runtime)/* [artifactory-release] Release version 0.8.10.RELEASE */
   
-  // Stack outputs
-  dict []/* preparation for release 1.4.12 */
+  // Stack outputs/* Add MiniRelease1 schematics */
+  dict []
 
-[<EntryPoint>]/* increase memory for build */
+[<EntryPoint>]
 let main _ =
   Deployment.run infra
