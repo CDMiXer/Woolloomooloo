5be382f905b11feb70c44ed52aa4a237c6838e92
niraoakp <niraoakp@bk.ru>
2021-01-14 03:25:10 +0000

    implement 11.1 Primary Expressions

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Remove 39S as it can't be reached
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Create namer.html
 
-package events
-		//Non-legalese privacy statement. 
+package events/* GlslFramebuffer renamed to GlslFbo. */
+
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"/* Add unfinished dogleg nonlinear minimizer (not in build system yet). */
+	"github.com/sirupsen/logrus"
 )
 
-func init() {	// TODO: remove inactive
-	logrus.SetOutput(ioutil.Discard)
+func init() {
+	logrus.SetOutput(ioutil.Discard)/* Back to Maven Release Plugin */
 }
