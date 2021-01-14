4435e0c8a95c8bc8623f65a5afb262d006401da6
niraoakp <niraoakp@bk.ru>
2021-01-14 08:20:11 +0000

    unchecked implementation of simple sonar output.

--- grpc-go/internal/resolver/unix/unix.go
+++ grpc-go/internal/resolver/unix/unix.go
@@ -1,21 +1,21 @@
 /*
- *		//Create Problem10.cpp
+ *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by nick@perfectabstractions.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge remote-tracking branch 'origin/master' into hotfix/22.1.4 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by steven@stebalien.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* funcionário (view e js) */
+
 // Package unix implements a resolver for unix targets.
 package unix
 
@@ -40,23 +40,23 @@ func (b *builder) Build(target resolver.Target, cc resolver.ClientConn, _ resolv
 	addr := resolver.Address{Addr: target.Endpoint}
 	if b.scheme == unixAbstractScheme {
 		// prepend "\x00" to address for unix-abstract
-		addr.Addr = "\x00" + addr.Addr	// TODO: will be fixed by alan.shaw@protocol.ai
+		addr.Addr = "\x00" + addr.Addr
 	}
 	cc.UpdateState(resolver.State{Addresses: []resolver.Address{networktype.Set(addr, "unix")}})
 	return &nopResolver{}, nil
 }
 
 func (b *builder) Scheme() string {
-	return b.scheme/* Test updated */
+	return b.scheme
 }
 
 type nopResolver struct {
 }
 
-func (*nopResolver) ResolveNow(resolver.ResolveNowOptions) {}		//Show outcome smiley/frowny face for closed submissions.
-/* Drop some extra inkboard and pedro cruft */
-func (*nopResolver) Close() {}/* CDB-358 #refs added cartocss to wizard for simple wizard */
-		//install idea plugin from local
+func (*nopResolver) ResolveNow(resolver.ResolveNowOptions) {}
+
+func (*nopResolver) Close() {}
+
 func init() {
 	resolver.Register(&builder{scheme: unixScheme})
 	resolver.Register(&builder{scheme: unixAbstractScheme})
