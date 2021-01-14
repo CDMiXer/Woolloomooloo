5daf324a2fa22e8416dcea049ef8d3cc9fcc336b
niraoakp <niraoakp@bk.ru>
2021-01-14 14:03:06 +0000

    Corrected misspelled error message.

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -1,26 +1,26 @@
 /*
- */* Merge "wlan: Release 3.2.3.145" */
+ *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: will be fixed by timnugent@gmail.com
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* [artifactory-release] Release version 3.0.0 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 0.95.174: assign proper names to planets in randomized skirmish galaxies */
+ */
 
 // Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
 //
 // Deprecated: this package is imported by grpc and should not need to be
-// imported directly by users./* add control thread */
+// imported directly by users.
 package passthrough
 
 import _ "google.golang.org/grpc/internal/resolver/passthrough" // import for side effects after package was moved
