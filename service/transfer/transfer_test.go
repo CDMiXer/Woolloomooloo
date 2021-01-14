180c4e7bbc09a150fe5e39cacd44715becd88df4
niraoakp <niraoakp@bk.ru>
2021-01-14 18:43:55 +0000

    Generated site for typescript-generator-maven-plugin 2.26.740

--- service/transfer/transfer_test.go
+++ service/transfer/transfer_test.go
@@ -1,25 +1,25 @@
 // Copyright 2020 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* List VERSION File in Release Guide */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package transfer
 
 import (
-	"context"	// Merge "Capitalize boolean values in config files"
-	"testing"	// TODO: hacked by cory@protocol.ai
+	"context"
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
-/* Released version 0.8.32 */
+
 var nocontext = context.Background()
-	// TODO: 72aa40e4-2e58-11e5-9284-b827eb9e62be
+
 func TestTransfer(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Merge "ref: added interfaces to make the reserved name dispatcher configurable"
+
 	mockRepo := &core.Repository{
 		ID:     1,
 		UserID: 2,
@@ -27,17 +27,17 @@ func TestTransfer(t *testing.T) {
 	}
 	mockRepos := []*core.Repository{
 		mockRepo,
-	}/* merge changeset 20521 from trunk (formatting and robustness) */
+	}
 	mockCollabs := []*core.Collaborator{
 		{
-			UserID: 1, // do not match non-admin/* Specify Release mode explicitly */
+			UserID: 1, // do not match non-admin
 			Admin:  false,
 		},
-{		
+		{
 			UserID: 2, // do not match existing owner
 			Admin:  true,
 		},
-		{/* Implemented free_driver() in terminfo driver */
+		{
 			UserID: 3,
 			Admin:  true,
 		},
@@ -46,7 +46,7 @@ func TestTransfer(t *testing.T) {
 		ID: 2,
 	}
 
-	checkRepo := func(ctx context.Context, updated *core.Repository) error {/* Removed unnecessary public dashboard styles for IE */
+	checkRepo := func(ctx context.Context, updated *core.Repository) error {
 		if updated.UserID != 3 {
 			t.Errorf("Expect repository owner id assigned to user id 3")
 		}
@@ -54,15 +54,15 @@ func TestTransfer(t *testing.T) {
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)/* Merge "Fix incorrect variable name in some exception class" */
-	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)/* Update regex for SBM-WoodenBuckets */
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)
+	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)
 
 	perms := mock.NewMockPermStore(controller)
 	perms.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockCollabs, nil).Times(1)
 
 	r := New(
-		repos,/* Change project name */
-		perms,	// decodeText update
+		repos,
+		perms,
 	)
 
 	err := r.Transfer(nocontext, mockUser)
@@ -70,7 +70,7 @@ func TestTransfer(t *testing.T) {
 		t.Error(err)
 	}
 }
-/* Release for 23.0.0 */
+
 func TestTransfer_NoOwner(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
