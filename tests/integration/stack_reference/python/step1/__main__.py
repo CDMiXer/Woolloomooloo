9ef7e0ab735bfdc030d77d65619f96ed5c06573d
niraoakp <niraoakp@bk.ru>
2021-01-14 09:20:27 +0000

    Add the way to config the pagination bar icons.

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -4,12 +4,12 @@ import pulumi
 
 config = pulumi.Config()
 org = config.require('org')
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
-a = pulumi.StackReference(slug)/* Deleted CtrlApp_2.0.5/Release/StdAfx.obj */
-
-oldVal = a.get_output('val')
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"/* markov wolfsheep_model named simply "wolfsheep_model" */
+a = pulumi.StackReference(slug)
+/* Merge "Release 1.0.0.132 QCACLD WLAN Driver" */
+oldVal = a.get_output('val')/* Solved problems to save teams in table with relation HABTM */
 
 if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
     raise Exception('Invalid result')
-/* RSS compatibility improvements; now throwing in event of bogus feed. */
-pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
+
+pulumi.export('val2', pulumi.Output.secret(['a', 'b']))/* 62d37fde-2e42-11e5-9284-b827eb9e62be */
