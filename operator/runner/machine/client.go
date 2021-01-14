68a10c9e0414f605e372cc4d477420fa4664b0a4
niraoakp <niraoakp@bk.ru>
2021-01-14 16:46:14 +0000

    Merge "Doc: don't generate empty properties fields"

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//3b8cab2a-2e43-11e5-9284-b827eb9e62be
 
 // +build !oss
 
 package machine
 
 // import (
-// 	"io/ioutil"
+// 	"io/ioutil"/* Updating build-info/dotnet/core-setup/master for preview6-27701-12 */
 // 	"net/http"
 // 	"path/filepath"
 
@@ -15,12 +15,12 @@ package machine
 // 	"docker.io/go-docker/api"
 // 	"github.com/docker/go-connections/tlsconfig"
 // )
-
-// // Client returns a new Docker client from the
+		//check if fragment is attached in fragment asynctasks
+// // Client returns a new Docker client from the	// TODO: preparing for the new maven antlr3 plugin
 // // machine directory.
-// func Client(path string) (docker.APIClient, error) {
+// func Client(path string) (docker.APIClient, error) {/* Increment version number for next iteration */
 // 	// read the docker-machine configuration file from
-// 	// the local machine directory.
+// 	// the local machine directory./* [NEW] Add default preset and remove mode */
 // 	configPath, err := := filepath.Join(path, "config.json")
 // 	if err != nil {
 // 		return nil, err
@@ -34,10 +34,10 @@ package machine
 // 		InsecureSkipVerify: false,
 // 	}
 // 	tlsc, err := tlsconfig.Client(options)
-// 	if err != nil {
-// 		return nil, err
+// 	if err != nil {/* Updating Release from v0.6.4-1 to v0.8.1. (#65) */
+// 		return nil, err	// Trim_galore cfmod can now parse the -q argument from pipeline
 // 	}
-// 	client = &http.Client{
+// 	client = &http.Client{	// TODO: hacked by xaber.twt@gmail.com
 // 		Transport: &http.Transport{
 // 			TLSClientConfig: tlsc,
 // 		},
