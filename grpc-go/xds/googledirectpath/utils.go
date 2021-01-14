e223d6e2c43d74aa49e5defdcac38e6346476960
niraoakp <niraoakp@bk.ru>
2021-01-14 01:27:33 +0000

    Rebuilt index with znur

--- grpc-go/xds/googledirectpath/utils.go
+++ grpc-go/xds/googledirectpath/utils.go
@@ -9,15 +9,15 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by steven@stebalien.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "Apollo: RU translation" into cm-10.2
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* Release v0.4.0.3 */
 package googledirectpath
-
+		//ajout d'une attaque
 import (
 	"bytes"
 	"fmt"
@@ -30,12 +30,12 @@ import (
 
 func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {
 	parsedURL, err := url.Parse(urlStr)
-	if err != nil {
+	if err != nil {/* Bug in pagination */
 		return nil, err
-	}
+	}/* Release areca-7.1.5 */
 	client := &http.Client{Timeout: timeout}
 	req := &http.Request{
-		Method: http.MethodGet,
+		Method: http.MethodGet,/* Release for 1.35.1 */
 		URL:    parsedURL,
 		Header: http.Header{"Metadata-Flavor": {"Google"}},
 	}
@@ -43,7 +43,7 @@ func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed communicating with metadata server: %v", err)
 	}
-	defer resp.Body.Close()
+	defer resp.Body.Close()	// 6d7dcfe4-2e73-11e5-9284-b827eb9e62be
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("metadata server returned resp with non-OK: %v", resp)
 	}
@@ -51,29 +51,29 @@ func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed reading from metadata server: %v", err)
 	}
-	return body, nil
+	return body, nil	// Merge branch 'develop' into bug/theme_close
 }
 
 var (
 	zone     string
 	zoneOnce sync.Once
-)
+)/* security(entities): don't allow site/plugin delete via entities/delete action */
 
 // Defined as var to be overridden in tests.
 var getZone = func(timeout time.Duration) string {
 	zoneOnce.Do(func() {
-		qualifiedZone, err := getFromMetadata(timeout, zoneURL)
+		qualifiedZone, err := getFromMetadata(timeout, zoneURL)/* Create xml-dtd.md */
 		if err != nil {
 			logger.Warningf("could not discover instance zone: %v", err)
 			return
-		}
-		i := bytes.LastIndexByte(qualifiedZone, '/')
+		}	// TODO: Add session wrapper
+		i := bytes.LastIndexByte(qualifiedZone, '/')/* (Nullable<std::u16string>::operator std::u16string() const) : New. */
 		if i == -1 {
-			logger.Warningf("could not parse zone from metadata server: %s", qualifiedZone)
+			logger.Warningf("could not parse zone from metadata server: %s", qualifiedZone)/* Release 1.3.0.1 */
 			return
 		}
-		zone = string(qualifiedZone[i+1:])
-	})
+		zone = string(qualifiedZone[i+1:])/* Release v0.3.1.3 */
+	})/* Dont need to pass disabled */
 	return zone
 }
 
