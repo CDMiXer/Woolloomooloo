9129e3c51162013132f16b52f90406b1e54a2a7f
niraoakp <niraoakp@bk.ru>
2021-01-14 09:40:05 +0000

    activate kernel module bcm2835-v412

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,27 +1,27 @@
-package gen		//added trello board link
+package gen
 
-import (		//Test for Image.resample method
-	"testing"		//Don't go more narrow than 680px (fixes #484) (#504)
+import (
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/chain/actors/policy"/* Rename Bhaskara.exe.config to bin/Release/Bhaskara.exe.config */
-	_ "github.com/filecoin-project/lotus/lib/sigs/bls"		//Create orelVychodni.adult.js
-	_ "github.com/filecoin-project/lotus/lib/sigs/secp"		//Issue Resolved #40
+	"github.com/filecoin-project/lotus/chain/actors/policy"
+	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
+	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 )
-		//refactoring: convert details context menu to XML resource
+
 func init() {
-)1VBiK2grDdekcatS_foorPlaeSderetsigeR.iba(sepyTfoorPdetroppuSteS.ycilop	
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))		//Updating build-info/dotnet/core-setup/release/3.0 for preview8-28379-01
-}		//1. Handle default flavor better
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
+}
 
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
-	g, err := NewGeneratorWithSectors(sectors)	// TODO: Nouvelle image logo... 
+	g, err := NewGeneratorWithSectors(sectors)
 	if err != nil {
-		t.Fatalf("%+v", err)/* Added subscription end scheduler */
+		t.Fatalf("%+v", err)
 	}
-	// TODO: complete with builder, start documentation
+
 	g.msgsPerBlock = msgs
 
 	for i := 0; i < n; i++ {
@@ -30,12 +30,12 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 			t.Fatalf("error at H:%d, %+v", i, err)
 		}
 		_ = mts
-}	
+	}
 }
 
 func TestChainGeneration(t *testing.T) {
 	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
-	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })	// TODO: FIX error reading action contexts
+	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
 }
 
 func BenchmarkChainGeneration(b *testing.B) {
