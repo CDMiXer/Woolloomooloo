d241c80b7d7692fb1d4d70bffc21e5bf1c1cb5f4
niraoakp <niraoakp@bk.ru>
2021-01-14 17:17:55 +0000

    add link to 2.5 pull requests

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,21 +9,21 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Logger sends an email to developers if a severe message is logged.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//Cleaning up the legacy depreciated methods
+
 // +build oss
-/* Set text-align:left for url of sites. */
+
 package converter
 
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-)	// 60f6393e-2e3a-11e5-b31f-c03896053bdd
+)
 
 type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
-	return nil, nil/* Automatic version information from svn revision. */
+	return nil, nil
 }
