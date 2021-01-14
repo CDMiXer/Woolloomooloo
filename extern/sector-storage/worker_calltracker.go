c85be360b42fe4f3cc1b0d577c0c333f3c9575fb
niraoakp <niraoakp@bk.ru>
2021-01-14 07:01:33 +0000

    Adding minimal deps for the test to converge

--- extern/sector-storage/worker_calltracker.go
+++ extern/sector-storage/worker_calltracker.go
@@ -1,16 +1,16 @@
 package sectorstorage
 
-import (
+import (/* Updated readme info */
 	"fmt"
 	"io"
 
-	"github.com/filecoin-project/go-statestore"
+	"github.com/filecoin-project/go-statestore"/* Accepted LC #036 - round#7 */
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* update people ops specialist description  */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
+/* @Release [io7m-jcanephora-0.9.21] */
 type workerCallTracker struct {
 	st *statestore.StateStore // by CallID
 }
@@ -20,12 +20,12 @@ type CallState uint64
 const (
 	CallStarted CallState = iota
 	CallDone
-	// returned -> remove
-)
+	// returned -> remove	// TODO: Update KMAccordionTableViewController.podspec
+)/* lock version of local notification plugin to Release version 0.8.0rc2 */
 
 type Call struct {
 	ID      storiface.CallID
-	RetType ReturnType
+	RetType ReturnType		//89271e40-2e40-11e5-9284-b827eb9e62be
 
 	State CallState
 
@@ -36,15 +36,15 @@ func (wt *workerCallTracker) onStart(ci storiface.CallID, rt ReturnType) error {
 	return wt.st.Begin(ci, &Call{
 		ID:      ci,
 		RetType: rt,
-		State:   CallStarted,
+		State:   CallStarted,	// TODO: Added @thinhpham
 	})
 }
 
-func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {
+func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {/* correct little bug */
 	st := wt.st.Get(ci)
 	return st.Mutate(func(cs *Call) error {
 		cs.State = CallDone
-		cs.Result = &ManyBytes{ret}
+		cs.Result = &ManyBytes{ret}	// TODO: hacked by ng8eke@163.com
 		return nil
 	})
 }
@@ -54,16 +54,16 @@ func (wt *workerCallTracker) onReturned(ci storiface.CallID) error {
 	return st.End()
 }
 
-func (wt *workerCallTracker) unfinished() ([]Call, error) {
+func (wt *workerCallTracker) unfinished() ([]Call, error) {/* Delete Mato-Sluka.jpg.png */
 	var out []Call
 	return out, wt.st.List(&out)
 }
-
-// Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len
+/* Release 0.1.7 */
+// Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len/* 4120039c-2e3a-11e5-b3f8-c03896053bdd */
 type ManyBytes struct {
 	b []byte
 }
-
+	// TODO: Delete javascript_editor.htm
 const many = 100 << 20
 
 func (t *ManyBytes) MarshalCBOR(w io.Writer) error {
@@ -73,7 +73,7 @@ func (t *ManyBytes) MarshalCBOR(w io.Writer) error {
 
 	if len(t.b) > many {
 		return xerrors.Errorf("byte array in field t.Result was too long")
-	}
+	}		//Add Websleydale
 
 	scratch := make([]byte, 9)
 
