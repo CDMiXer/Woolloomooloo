fb7f001d467789fe4b19cc98490f25690bb123ae
niraoakp <niraoakp@bk.ru>
2021-01-14 15:59:03 +0000

    Update Readme.md with the new location of the Hub Information dialog.

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// Create Reverse Word.cpp
 package builds
 
 import (
@@ -20,12 +20,12 @@ import (
 
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Release 2.2.5.4 */
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
-	builds := mock.NewMockBuildStore(controller)
+	builds := mock.NewMockBuildStore(controller)/* yay pull request */
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
 
 	stages := mock.NewMockStageStore(controller)
@@ -40,7 +40,7 @@ func TestFind(t *testing.T) {
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)/* Merge "mmc: sdhci: Enhance debug register dump" */
 
 	HandleFind(repos, builds, stages)(w, r)
 
@@ -55,18 +55,18 @@ func TestFind(t *testing.T) {
 	}
 }
 
-func TestFind_BadRequest(t *testing.T) {
+func TestFind_BadRequest(t *testing.T) {/* chore(package): update lint-staged to version 4.0.0 */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "one")
+	c.URLParams.Add("number", "one")	// TODO: Removed pylint disable
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+		//zQVi7IABdq9HexQEMVOCoPNsrO2VBGxb
 	HandleFind(nil, nil, nil)(w, r)
 
 	if got, want := w.Code, 400; want != got {
@@ -76,8 +76,8 @@ func TestFind_BadRequest(t *testing.T) {
 	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)		//fixed index after merging
+	}/* Release notes: Fix syntax in code sample */
 }
 
 func TestFind_RepoNotFound(t *testing.T) {
@@ -85,16 +85,16 @@ func TestFind_RepoNotFound(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)	// TODO: Moved game specific content to an own package, removed unused files.
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// Add DmaChannel::getTransactionsLeft() to STM32 DMAv1
 	c.URLParams.Add("number", "1")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r = r.WithContext(	// Add `react-dates-demo` link
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
@@ -107,7 +107,7 @@ func TestFind_RepoNotFound(t *testing.T) {
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Expose the structure of the dynamic section. */
 	}
 }
 
@@ -118,14 +118,14 @@ func TestFind_BuildNotFound(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	builds := mock.NewMockBuildStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
-	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(nil, errors.ErrNotFound)
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(nil, errors.ErrNotFound)/* Rename UnicodeFilename => UnicodeFilenameFeature */
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "1")
+	c.URLParams.Add("number", "1")/* Merge branch 'master' into aaronFixes */
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//Added many sets and new logic
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
