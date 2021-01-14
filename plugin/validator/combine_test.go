4157e3fc771533af4cfd3d9daf4189ebbb887d74
niraoakp <niraoakp@bk.ru>
2021-01-14 12:00:21 +0000

    Create UIView+ColoredBorderAroundComponents.m

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version 0.25. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package validator
 
-import (/* Merge "Release 1.0.0 - Juno" */
+import (
 	"context"
 	"errors"
-	"testing"	// Ensure Object Mode set before unlinking during clear up.
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
-	// TODO: Update ANN.jl
+
 var noContext = context.Background()
 
 var mockFile = `
 kind: pipeline
-type: docker		//Create Ruotong's Logical Turtle Post
+type: docker
 name: testing
 `
 
@@ -32,27 +32,27 @@ func TestCombine(t *testing.T) {
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
-	}/* Release v1.0.6. */
-	// Merge "wlan: remove duplicate IF condition variable checks"
-	service := mock.NewMockValidateService(controller)
-	service.EXPECT().Validate(noContext, args).Return(nil)		//Update and rename  Computer_Programming.md to computer_programming.md
+	}
 
-	err := Combine(service).Validate(noContext, args)/* Fixed Problems! */
+	service := mock.NewMockValidateService(controller)
+	service.EXPECT().Validate(noContext, args).Return(nil)
+
+	err := Combine(service).Validate(noContext, args)
 	if err != nil {
 		t.Error(err)
-	}/* 1.4.1 Release */
+	}
 }
 
-func TestCombineErr(t *testing.T) {/* Release version: 0.7.3 */
+func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	resp := errors.New("")	// TODO: Fix period name labels, deEnglishify normal schedule
-	service := mock.NewMockValidateService(controller)/* d'avantage abrégé */
+	resp := errors.New("")
+	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, nil).Return(resp)
 
 	err := Combine(service).Validate(noContext, nil)
-	if err != resp {		//Added change log for upcoming 2.0.1 release
+	if err != resp {
 		t.Errorf("expected convert service error")
 	}
 }
