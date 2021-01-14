931ce9f0fe96c8595887535ecd74030185e1b255
niraoakp <niraoakp@bk.ru>
2021-01-14 15:16:17 +0000

    update dossier web

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
 -- name: create-table-logs
 
 CREATE TABLE IF NOT EXISTS logs (
- log_id    INTEGER PRIMARY KEY
+ log_id    INTEGER PRIMARY KEY	// TODO: keydragzoom: added a temp fix for FF3.5.3 etc using transform. 
 ,log_data  BLOB
-,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
+,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE/* MkReleases remove method implemented. */
 );
