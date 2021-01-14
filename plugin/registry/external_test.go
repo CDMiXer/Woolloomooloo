9b306086a8589028e28a4795a97424903f662bb7
niraoakp <niraoakp@bk.ru>
2021-01-14 19:28:30 +0000

    make Grid the owner of Controls that are part of its cells

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Release of eeacms/www-devel:20.8.11
 // +build !oss
-
+/* Release of eeacms/eprtr-frontend:0.2-beta.41 */
 package registry
