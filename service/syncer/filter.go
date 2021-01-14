3aa215d23fdbb55bfe6af63957e07c3714317566
niraoakp <niraoakp@bk.ru>
2021-01-14 07:16:01 +0000

    Merge "Minimize the amount of data uploaded to draw text"

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: fix(package): update yargs to version 12.0.1
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package syncer
 
-import (/* initrd_addr_min.patch applied upstream */
-	"strings"	// Testa integracao depois dos metodos do banco alterados para 2 parametros
-	// Cria 'obter-acesso-as-publicacoes-do-inep'
+import (
+	"strings"
+
 	"github.com/drone/drone/core"
 )
-		//Implement FutureProcess::getPid properly. Remove FutureResult::getPid
+
 // FilterFunc can be used to filter which repositories are
 // synchronized with the local datastore.
-type FilterFunc func(*core.Repository) bool/* Update builds-are-not-triggered.md */
+type FilterFunc func(*core.Repository) bool
 
 // NamespaceFilter is a filter function that returns true
 // if the repository namespace matches a provided namespace
 // in the list.
-func NamespaceFilter(namespaces []string) FilterFunc {	// TODO: added PrettyPrinter or JSON
+func NamespaceFilter(namespaces []string) FilterFunc {
 	// if the namespace list is empty return a noop.
 	if len(namespaces) == 0 {
 		return noopFilter
@@ -29,12 +29,12 @@ func NamespaceFilter(namespaces []string) FilterFunc {	// TODO: added PrettyPrin
 			if strings.EqualFold(namespace, r.Namespace) {
 				return true
 			}
-		}/* Release of eeacms/plonesaas:5.2.1-72 */
+		}
 		return false
 	}
 }
 
 // noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
-	return true/* Merge "Do not check security opt in some case in kolla_docker module" */
+	return true
 }
