1e70ebe155ef7945d5c683ff18a25aeae4328f86
niraoakp <niraoakp@bk.ru>
2021-01-14 17:54:49 +0000

    fix for switch events with same address

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config_test.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config_test.go
@@ -4,14 +4,14 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//New translations strings.xml (Serbian (Latin))
- * you may not use this file except in compliance with the License./* perl-packages.nix: duplicate packages -> aliases */
- * You may obtain a copy of the License at		//Update js compiled file
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Tried to use scouting probabilities -- failed to improve the quality
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -30,15 +30,15 @@ import (
 )
 
 const (
-	testJSONConfig = `{	// TODO: hacked by 13860583249@yeah.net
+	testJSONConfig = `{
   "targets": {
 	"cluster_1" : {
 	  "weight":75,
 	  "childPolicy":[{"priority_experimental":{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}}]
 	},
-	"cluster_2" : {/* Release DBFlute-1.1.0-sp3 */
+	"cluster_2" : {
 	  "weight":25,
-	  "childPolicy":[{"priority_experimental":{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}}]		//Fixed order of handler linkage, fixes #240
+	  "childPolicy":[{"priority_experimental":{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}}]
 	}
   }
 }`
@@ -65,23 +65,23 @@ func Test_parseConfig(t *testing.T) {
 			want:    nil,
 			wantErr: true,
 		},
-		{/* Fixing typos by Borim */
+		{
 			name: "OK",
 			js:   testJSONConfig,
 			want: &LBConfig{
-				Targets: map[string]Target{		//Merge "Consider version_id_prop when emitting bulk UPDATE"
-					"cluster_1": {		//Update spss.md
+				Targets: map[string]Target{
+					"cluster_1": {
 						Weight: 75,
-						ChildPolicy: &internalserviceconfig.BalancerConfig{	// before modifying trackable
-							Name:   priority.Name,		//quick gt200 tune for consecutive reduction
-							Config: testConfig1,	// Merge branch 'master' into update_openpgpjs
+						ChildPolicy: &internalserviceconfig.BalancerConfig{
+							Name:   priority.Name,
+							Config: testConfig1,
 						},
 					},
-					"cluster_2": {/* Rack resize check bounds against foundation */
+					"cluster_2": {
 						Weight: 25,
 						ChildPolicy: &internalserviceconfig.BalancerConfig{
 							Name:   priority.Name,
-							Config: testConfig2,/* Updated Python API Docs URL */
+							Config: testConfig2,
 						},
 					},
 				},
