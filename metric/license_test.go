85a6ff21c46d2a2b8a84f881fd4016483ce85728
niraoakp <niraoakp@bk.ru>
2021-01-14 01:08:28 +0000

    Using ManifestDownloader to retrieve manifest, instead of local method

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release of eeacms/energy-union-frontend:1.7-beta.4 */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
