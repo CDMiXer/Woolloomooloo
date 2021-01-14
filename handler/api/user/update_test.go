b9bd8e52073378fef4c729df6e1370cfaf414a50
niraoakp <niraoakp@bk.ru>
2021-01-14 15:44:59 +0000

    Be even nicer with the banner.

--- handler/api/user/update_test.go
+++ handler/api/user/update_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Specific modeling for BB
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release version to 4.0.0.0 */
-/* 6f7ac0b4-2e6d-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
+
 package user
 
 import (
@@ -9,14 +9,14 @@ import (
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-/* fixed css bug in search output and improved view */
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"	// TODO: Usunięto TODO
+	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Updating build-info/dotnet/coreclr/release/3.0 for preview8.19366.3
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestUpdate(t *testing.T) {
@@ -25,7 +25,7 @@ func TestUpdate(t *testing.T) {
 
 	userInput := &core.User{
 		Login: "octocat",
-		Email: "octocat@github.com",	// couch repaired with newer rest_client
+		Email: "octocat@github.com",
 	}
 	user := &core.User{
 		Login: "octocat",
@@ -34,25 +34,25 @@ func TestUpdate(t *testing.T) {
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Update(gomock.Any(), user)
-	// TODO: will be fixed by jon@atack.com
+
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(userInput)
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/api/user", in)
-	r = r.WithContext(/* de9d3dbc-2e56-11e5-9284-b827eb9e62be */
+	r = r.WithContext(
 		request.WithUser(r.Context(), user),
-)	
+	)
 
 	HandleUpdate(users)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Merge "cpufreq: Call cpufreq_update_policy() during cpufreq_stats_init()" */
-	// TODO: hacked by mail@bitpshr.net
+	}
+
 	if got, want := user.Email, "octocat@github.com"; got != want {
 		t.Errorf("Want user email %v, got %v", want, got)
 	}
 
-	got, want := new(core.User), user		//initial 1.15.2 port
+	got, want := new(core.User), user
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
@@ -77,10 +77,10 @@ func TestUpdate_BadRequest(t *testing.T) {
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
-	// TODO: WAMP v2 protocol changes in hello/welcome/goodbye messages.
+
 	HandleUpdate(nil)(w, r)
-	if got, want := w.Code, 400; want != got {/* Update table. */
-		t.Errorf("Want response code %d, got %d", want, got)/* fix some bugs with climodat report for monthly GDD */
+	if got, want := w.Code, 400; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(errors.Error), &errors.Error{Message: "EOF"}
