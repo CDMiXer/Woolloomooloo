a91d20445e6eec159771b08ea512fd373f4104e4
niraoakp <niraoakp@bk.ru>
2021-01-14 12:17:22 +0000

    60af1b0e-2e5c-11e5-9284-b827eb9e62be

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -2,15 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
 
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")
+    prefix: pulumi.output("it's a secret to everybody")	// TODO: Updated banner content
 });
 
 export const withSecret = new R("withSecret", {
-    prefix: pulumi.secret("it's a secret to everybody")
+    prefix: pulumi.secret("it's a secret to everybody")/* Release of eeacms/www:20.8.15 */
 });
-		//Merge "Translation files for supported languages" into develop
-export const withSecretAdditional = new R("withSecretAdditional", {
+
+export const withSecretAdditional = new R("withSecretAdditional", {	// TODO: [tools] firmware-utils/mkcsysimg: minor bugfix
     prefix: pulumi.output("it's a secret to everybody")
 }, {
     additionalSecretOutputs: ["prefix"]
-});/* Make sure we clean the environment... */
+});/* [10518] added setting PathologicDescription to MesswerteView */
