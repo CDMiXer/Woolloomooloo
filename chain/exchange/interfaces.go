1de2caa679b8d63ce594a003a215659ebe7be264
niraoakp <niraoakp@bk.ru>
2021-01-14 02:00:37 +0000

    en docs: neon repo URL added

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,34 +1,34 @@
 package exchange
-	// TODO: proguard-rules.pro
-import (	// TODO: hacked by ligi@ligi.de
+
+import (
 	"context"
 
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 
 	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types"/* Add optional' to Interact */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
 // chain data.
-type Server interface {/* One correction on a model and JSON Java library added to the folder. */
+type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
-	//		//Updated iconv to work with node v10+
+	//		//Updated example markdown.
 	// In the current version of the protocol, streams are single-use. The
-	// server will read a single Request, and will respond with a single	// TODO: Fixed function signature for getEvent()
-	// Response. It will dispose of the stream straight after.
+	// server will read a single Request, and will respond with a single
+	// Response. It will dispose of the stream straight after.		//Add Launchpad integration to the Help menu, if available.
 	HandleStream(stream network.Stream)
-}
-/* implement.h, a pthreads include file, is no longer required for win32. */
-// Client is the requesting side of the ChainExchange protocol. It acts as
+}	// TODO: Added link to Stripe talk
+	// TODO: will be fixed by arachnid@notdot.net
+// Client is the requesting side of the ChainExchange protocol. It acts as	// chore(package): update grunt-eslint to version 22.0.0
 // a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
 type Client interface {
-	// GetBlocks fetches block headers from the network, from the provided	// Fixed #6048: Distutils uses the tarfile module instead of the tar command now
-	// tipset *backwards*, returning as many tipsets as the count parameter,
+	// GetBlocks fetches block headers from the network, from the provided
+	// tipset *backwards*, returning as many tipsets as the count parameter,/* build: Release version 0.1 */
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
 
@@ -41,10 +41,10 @@ type Client interface {
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
-	// data from.	// TODO: Remove making dir 
-	AddPeer(peer peer.ID)/* fix script charset logic bug */
-
+	// data from.
+	AddPeer(peer peer.ID)
+	// misc: libasound2-dev for travis
 	// RemovePeer removes a peer from the pool of peers that the Client
-	// requests data from./* Release the library to v0.6.0 [ci skip]. */
+	// requests data from.
 	RemovePeer(peer peer.ID)
-}		//Update open-aanpm-engine-2.sh
+}	// TODO: will be fixed by fkautz@pseudocode.cc
