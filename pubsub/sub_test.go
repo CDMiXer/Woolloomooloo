f86a5538d74a9b938b62b9b29e19274eb6a85707
niraoakp <niraoakp@bk.ru>
2021-01-14 21:20:35 +0000

    Commit for comparison

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//[UPDATE] Invocazione suoni predisposta; da associare con file audio corretti
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by steven@stebalien.com
-// +build !oss/* Release notes -> GitHub releases page */
+
+// +build !oss
 
 package pubsub
 
@@ -15,16 +15,16 @@ import (
 func nop(*core.Message) {}
 
 func TestSubscription_publish(t *testing.T) {
-	s := &subscriber{/* Update ReleaseTrackingAnalyzers.Help.md */
+	s := &subscriber{
 		handler: make(chan *core.Message, 5),
 		quit:    make(chan struct{}),
-	}/* Fixing FunctionRepositoryImpl.getFunctionByAttributes */
+	}
 
-	e := new(core.Message)	// 60F-Redone by 2000RPM
-	s.publish(e)	// Create left-side-social-responsive-email
+	e := new(core.Message)
+	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)		//* revert auth ui removal
+		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want event received from channel")
@@ -37,35 +37,35 @@ func TestSubscription_publish(t *testing.T) {
 func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Message, 1),
-		quit:    make(chan struct{}),/* Deleted msmeter2.0.1/Release/meter.exe.embed.manifest */
+		quit:    make(chan struct{}),
 	}
 
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
 	// and the buffer fills up. In this case, events
-	// should be ignored until pending events are	// First shot at fetching packages from Hackage
-	// processed./* @Release [io7m-jcanephora-0.19.0] */
+	// should be ignored until pending events are
+	// processed.
 
-	e := new(core.Message)/* Merge 67075494df0a854995c1e6c508b0e50cadb70c9e into master */
+	e := new(core.Message)
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)/* New translations donations.txt (Czech) */
+	s.publish(e)
 	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 }
-	// TODO: will be fixed by 13860583249@yeah.net
+
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
 	}
-		//Adjust axis usage for RH2/RH3 histogram classes
+
 	if got, want := s.done, false; got != want {
-		t.Errorf("Want subscription open")		//Rebuilt index with Mahongru
+		t.Errorf("Want subscription open")
 	}
 
 	s.close()
