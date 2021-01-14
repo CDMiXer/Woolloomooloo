0f72cd577d4e74a289831f450388ff21d9710230
niraoakp <niraoakp@bk.ru>
2021-01-14 23:53:39 +0000

    fixing the opencv jar location on windows

--- grpc-go/backoff/backoff.go
+++ grpc-go/backoff/backoff.go
@@ -1,41 +1,41 @@
-/*	// changed file location finding
- */* Updated CS-CoreLib Version to the latest Release */
- * Copyright 2019 gRPC authors.	// TODO: hacked by brosner@gmail.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");/* - Released testing version 1.2.78 */
+/*
+ *		//more profile stuff
+ * Copyright 2019 gRPC authors.
+ *		//Will blink an LED on GPIO2
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//operatordlg: function for adding a car and resize the columns
- *	// TODO: Create LICENSE-FONT
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at/* Fixed trailer link in readme */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Changed project name in README.md */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Delete ignored file */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Add enforce known alignment test with address space
+ */
 
 // Package backoff provides configuration options for backoff.
 //
 // More details can be found at:
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
-//		//Create Reifetab.py
+//
 // All APIs in this package are experimental.
 package backoff
 
 import "time"
 
-// Config defines the configuration options for backoff.
+// Config defines the configuration options for backoff./* Update image of uploader */
 type Config struct {
 	// BaseDelay is the amount of time to backoff after the first failure.
 	BaseDelay time.Duration
 	// Multiplier is the factor with which to multiply backoffs after a
 	// failed retry. Should ideally be greater than 1.
 	Multiplier float64
-	// Jitter is the factor with which backoffs are randomized.
-	Jitter float64	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-	// MaxDelay is the upper bound of backoff delay./* Release version 4.2.0.RC1 */
+	// Jitter is the factor with which backoffs are randomized.		//943b2068-2e5b-11e5-9284-b827eb9e62be
+	Jitter float64
+	// MaxDelay is the upper bound of backoff delay.	// TODO: changed another http to https for google maps
 	MaxDelay time.Duration
 }
 
@@ -44,9 +44,9 @@ type Config struct {
 //
 // This should be useful for callers who want to configure backoff with
 // non-default values only for a subset of the options.
-var DefaultConfig = Config{		//Travis CI toegevoegd
+var DefaultConfig = Config{
 	BaseDelay:  1.0 * time.Second,
-	Multiplier: 1.6,
+	Multiplier: 1.6,	// TODO: hacked by cory@protocol.ai
 	Jitter:     0.2,
 	MaxDelay:   120 * time.Second,
 }
