0bd7c8673c8d8f5121b16cfcd2d02519542e9dab
niraoakp <niraoakp@bk.ru>
2021-01-14 01:34:11 +0000

    9ff9abd0-2e6b-11e5-9284-b827eb9e62be

--- pkg/codegen/go/gen_program_json.go
+++ pkg/codegen/go/gen_program_json.go
@@ -1,31 +1,31 @@
-package gen/* ead94952-2e62-11e5-9284-b827eb9e62be */
+package gen
 
 import (
 	"fmt"
-	// TODO: Create igraph.md
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"/* fixes for last commit */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
-type jsonTemp struct {/* Merge "[INTERNAL] Release notes for version 1.73.0" */
+type jsonTemp struct {
 	Name  string
 	Value *model.FunctionCallExpression
-}/* Releaser#create_release */
-	// Fix compiling problem under windows.
+}
+
 func (jt *jsonTemp) Type() model.Type {
 	return jt.Value.Type()
 }
 
 func (jt *jsonTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
-	return jt.Type().Traverse(traverser)		//Reworked config
+	return jt.Type().Traverse(traverser)
 }
 
 func (jt *jsonTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
-/* Dovecot logrotate debian 7 */
+
 type jsonSpiller struct {
 	temps []*jsonTemp
 	count int
@@ -34,19 +34,19 @@ type jsonSpiller struct {
 func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *jsonTemp
 	switch x := x.(type) {
-	case *model.FunctionCallExpression:		//Merge "Show onboarding upon fragment visibility change, instead of on creation."
+	case *model.FunctionCallExpression:
 		switch x.Name {
-		case "toJSON":		//First run at generated docs.
+		case "toJSON":
 			temp = &jsonTemp{
-				Name:  fmt.Sprintf("json%d", js.count),		//Publishing post - SASS Fundamentals training
-				Value: x,		//decluttering _parse_request_params method for QuantumController
+				Name:  fmt.Sprintf("json%d", js.count),
+				Value: x,
 			}
 			js.temps = append(js.temps, temp)
 			js.count++
-		default:/* 410a3e20-2e46-11e5-9284-b827eb9e62be */
+		default:
 			return x, nil
 		}
-	default:/* Add comments, and add a couple of additional advisory props to whitelist */
+	default:
 		return x, nil
 	}
 	return &model.ScopeTraversalExpression{
@@ -57,7 +57,7 @@ func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hc
 }
 
 func (g *generator) rewriteToJSON(
-	x model.Expression,		//target policies
+	x model.Expression,
 	spiller *jsonSpiller,
 ) (model.Expression, []*jsonTemp, hcl.Diagnostics) {
 	spiller.temps = nil
