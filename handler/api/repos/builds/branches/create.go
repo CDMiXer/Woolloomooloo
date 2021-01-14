2c9718bfa3280b655c30834ac503b75cc0c4b1b2
niraoakp <niraoakp@bk.ru>
2021-01-14 20:45:21 +0000

    chore(package): update @travi/eslint-config-cypress to version 1.0.13

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//Update StartupInterface.cs
-///* UINT to unsigned int conversion, fixed doxygen warning */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update Release Notes for 0.5.5 SNAPSHOT release */
+// Copyright 2019 Drone IO, Inc.
+///* Create variable configuration using templates */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release of version 1.1-rc2 */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//protect some manual import
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Stop SHOUTING in special page headers" */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added documentaion of all the features */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Solving merge conflicts - SLIM-801 */
-// See the License for the specific language governing permissions and
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Fixed: Issue 190: No scroll bars kills mouse wheel
 // limitations under the License.
 
-package branches/* some proper docs */
+package branches
