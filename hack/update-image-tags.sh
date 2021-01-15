d77f8a1ee037e79ec24f8e0e83af46d3a94066ab
niraoakp <niraoakp@bk.ru>
2021-01-15 12:53:52 +0000

    Add "fish" objective

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
 
-dir=$1/* #172 Release preparation for ANB */
+dir=$1/* remove trailing junk */
 image_tag=$2
-
+		//- fixed misspelled element in svg converter
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
-  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp/* estimating the utility of an interval result */
+  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
-done
+done/* Remove a hardwired reference to localhost */
