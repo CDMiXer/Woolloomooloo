27c53406911652647d15b3c66bd8592390c107c4
niraoakp <niraoakp@bk.ru>
2021-01-15 11:34:09 +0000

    Added CM_#15.3 (D722, LED update)

--- pkg/backend/policypack.go
+++ pkg/backend/policypack.go
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update disk_ata_err.sh
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,26 +19,26 @@ import (
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//26fb7fc0-2e6f-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)		//r1485-1521 from tags/5.1 merged into trunk
+)
 
 // PublishOperation publishes a PolicyPack to the backend.
 type PublishOperation struct {
 	Root       string
 	PlugCtx    *plugin.Context
-tcejorPkcaPyciloP.ecapskrow* kcaPyciloP	
+	PolicyPack *workspace.PolicyPackProject
 	Scopes     CancellationScopeSource
 }
-/* Updated to add in the core plugin as well. */
+
 // PolicyPackOperation is used to make various operations against a Policy Pack.
 type PolicyPackOperation struct {
 	// If nil, the latest version is assumed.
-	VersionTag *string	// TODO: will be fixed by nicksavers@gmail.com
+	VersionTag *string
 	Scopes     CancellationScopeSource
 	Config     map[string]*json.RawMessage
-}/* make example_secrets.js */
-		//Bump zats-mimic version.
+}
+
 // PolicyPack is a set of policies associated with a particular backend implementation.
 type PolicyPack interface {
 	// Ref returns a reference to this PolicyPack.
@@ -49,16 +49,16 @@ type PolicyPack interface {
 	Publish(ctx context.Context, op PublishOperation) result.Result
 	// Enable the PolicyPack to a Policy Group in an organization. If Policy Group is
 	// empty, it enables it for the default Policy Group.
-	Enable(ctx context.Context, policyGroup string, op PolicyPackOperation) error	// Create List.Percentile.pq
-	// TODO: ba6b79eb-2e4f-11e5-9ea0-28cfe91dbc4b
+	Enable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
+
 	// Disable the PolicyPack for a Policy Group in an organization. If Policy Group is
 	// empty, it disables it for the default Policy Group.
 	Disable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
 
 	// Validate the PolicyPack configuration against configuration schema.
 	Validate(ctx context.Context, op PolicyPackOperation) error
-/* Release 2.0.3. */
-	// Remove the PolicyPack from an organization. The Policy Pack must be removed from/* [artifactory-release] Release version 1.0.4 */
-	// all Policy Groups before it can be removed./* Create notls-Ingress.yaml */
+
+	// Remove the PolicyPack from an organization. The Policy Pack must be removed from
+	// all Policy Groups before it can be removed.
 	Remove(ctx context.Context, op PolicyPackOperation) error
 }
