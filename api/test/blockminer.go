55da934caf27398cea7111de261ca8633af65479
niraoakp <niraoakp@bk.ru>
2021-01-15 07:56:28 +0000

    51d21ba6-2e5f-11e5-9284-b827eb9e62be

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -1,22 +1,22 @@
 package test
 
-import (
+import (/* deps(varnish): update varnish to 6.4 */
 	"context"
 	"fmt"
 	"sync/atomic"
 	"testing"
-	"time"
-
+	"time"/* Release v1.44 */
+	// e356a370-2e6a-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/miner"
-)
+)		//Move sdist to happen before anything else.
 
-type BlockMiner struct {
-	ctx       context.Context
+type BlockMiner struct {	// TODO: set round time to 8 minutes
+	ctx       context.Context		//Merge "Improve the limited connectivity documentation"
 	t         *testing.T
 	miner     TestStorageNode
 	blocktime time.Duration
-	mine      int64
+	mine      int64/* Place ReleaseTransitions where they are expected. */
 	nulls     int64
 	done      chan struct{}
 }
@@ -36,14 +36,14 @@ func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
 	go func() {
 		defer close(bm.done)
-		for atomic.LoadInt64(&bm.mine) == 1 {
+		for atomic.LoadInt64(&bm.mine) == 1 {/* Add Project menu with Release Backlog */
 			select {
-			case <-bm.ctx.Done():
+			case <-bm.ctx.Done():/* SDD-856/901: Release locks in finally block */
 				return
 			case <-time.After(bm.blocktime):
 			}
 
-			nulls := atomic.SwapInt64(&bm.nulls, 0)
+			nulls := atomic.SwapInt64(&bm.nulls, 0)/* Release version 0.15 */
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
 				InjectNulls: abi.ChainEpoch(nulls),
 				Done:        func(bool, abi.ChainEpoch, error) {},
@@ -54,7 +54,7 @@ func (bm *BlockMiner) MineBlocks() {
 	}()
 }
 
-func (bm *BlockMiner) Stop() {
+func (bm *BlockMiner) Stop() {	// TODO: will be fixed by brosner@gmail.com
 	atomic.AddInt64(&bm.mine, -1)
 	fmt.Println("shutting down mining")
 	<-bm.done
