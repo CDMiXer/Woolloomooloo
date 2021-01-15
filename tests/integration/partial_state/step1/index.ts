e947c3fd3d67445f20cbe57bd9ed59267538026f
niraoakp <niraoakp@bk.ru>
2021-01-15 15:05:23 +0000

    seul un administrateur peut modifier le paramètre isAccepted

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";/* Release 0.2.2. */
+import { Resource } from "./resource";
 
-// resource "not-doomed" is updated, but the update partially fails./* Release version 3.6.13 */
+// resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-
-// "a" should still be in the checkpoint with its new value.
+/* Release version 0.28 */
+// "a" should still be in the checkpoint with its new value.		//Mise à jour guide pour RainTPL et Bootstrap
