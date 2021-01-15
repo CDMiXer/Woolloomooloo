595f065cf26c3f0d001e66a4526a88e74e8bf92b
niraoakp <niraoakp@bk.ru>
2021-01-15 18:04:13 +0000

    Rebuilt index with rom707

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,5 +1,5 @@
 // +build freebsd
-/* Sort incoming messages to resolve contextual dependencies. */
+
 package ulimit
 
 import (
@@ -10,27 +10,27 @@ import (
 )
 
 func init() {
-	supportsFDManagement = true/* Merge "Revert "Added controller is-connected code"" */
+	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
-}/* Updated isValidItem() to invalidate items > 96 instead of items > 91 */
+}
 
-func freebsdGetLimit() (uint64, uint64, error) {/* Draft GitHub Releases transport mechanism */
+func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
-		return 0, 0, errors.New("invalid rlimits")	// TODO: hacked by brosner@gmail.com
-	}		//Update set_blenddata.py
+		return 0, 0, errors.New("invalid rlimits")
+	}
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
 func freebsdSetLimit(soft uint64, max uint64) error {
-	if (soft > math.MaxInt64) || (max > math.MaxInt64) {	// TODO: hacked by aeongrp@outlook.com
+	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
 		return errors.New("invalid rlimits")
 	}
 	rlimit := unix.Rlimit{
 		Cur: int64(soft),
 		Max: int64(max),
-	}/* Replace Array.includes with utility function for IE11 compat 🐲 */
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)		//Update paper section
+	}
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
 }
