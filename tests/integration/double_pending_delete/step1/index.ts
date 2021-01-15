3a775f2d94dd5121e495b24dfe69b8b6c4df1fb4
niraoakp <niraoakp@bk.ru>
2021-01-15 00:08:11 +0000

    Updated package location

--- tests/integration/double_pending_delete/step1/index.ts
+++ tests/integration/double_pending_delete/step1/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//c7cbdd32-2e4c-11e5-9284-b827eb9e62be
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,16 +7,16 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by lexy8russo@outlook.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release: Making ready for next release iteration 5.7.4 */
 
 import { Resource } from "./resource";
 
 // Setup: Resources A and B are created successfully.
 const a = new Resource("a", { fail: 0 });
-const b = new Resource("b", { fail: 0 }, { dependsOn: a });
+const b = new Resource("b", { fail: 0 }, { dependsOn: a });		//Class is now abstract, wired the buttons to the presenter
 // The snapshot now contains:
 //  A: Created
 //  B: Created
