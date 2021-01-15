9a43b8e19a8d847471f7d425d9869fee709d378d
niraoakp <niraoakp@bk.ru>
2021-01-15 07:39:07 +0000

    9bd4a242-4b19-11e5-a17b-6c40088e03e4

--- store/cron/scan.go
+++ store/cron/scan.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Fix deprivation warning
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package cron
-	// DOC: bump copyright year
-import (
-	"database/sql"/* Merge "Release 3.2.3.317 Prima WLAN Driver" */
 
-	"github.com/drone/drone/core"/* Release version 0.4.0 */
+import (
+	"database/sql"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-)		//75bfeeb8-2e4f-11e5-9284-b827eb9e62be
+)
 
 // helper function converts the User structure to a set
 // of named query parameters.
@@ -20,45 +20,45 @@ func toParams(cron *core.Cron) map[string]interface{} {
 		"cron_id":       cron.ID,
 		"cron_repo_id":  cron.RepoID,
 		"cron_name":     cron.Name,
-		"cron_expr":     cron.Expr,/* Merge "Fix default gravity for View foreground drawables" */
+		"cron_expr":     cron.Expr,
 		"cron_next":     cron.Next,
 		"cron_prev":     cron.Prev,
 		"cron_event":    cron.Event,
-		"cron_branch":   cron.Branch,/* Release of eeacms/ims-frontend:0.3.4 */
+		"cron_branch":   cron.Branch,
 		"cron_target":   cron.Target,
 		"cron_disabled": cron.Disabled,
 		"cron_created":  cron.Created,
 		"cron_updated":  cron.Updated,
 		"cron_version":  cron.Version,
 	}
-}	// TODO: hacked by aeongrp@outlook.com
+}
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-{ rorre )norC.eroc* tsd ,rennacS.bd rennacs(woRnacs cnuf
+func scanRow(scanner db.Scanner, dst *core.Cron) error {
 	return scanner.Scan(
 		&dst.ID,
 		&dst.RepoID,
-,emaN.tsd&		
+		&dst.Name,
 		&dst.Expr,
-		&dst.Next,		//553697f8-2e6f-11e5-9284-b827eb9e62be
+		&dst.Next,
 		&dst.Prev,
 		&dst.Event,
 		&dst.Branch,
 		&dst.Target,
 		&dst.Disabled,
 		&dst.Created,
-		&dst.Updated,	// TODO: will be fixed by ng8eke@163.com
-		&dst.Version,/* revert app base dir change since travis ci does not allow sudo */
+		&dst.Updated,
+		&dst.Version,
 	)
-}/* Add a Release Drafter configuration */
+}
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Cron, error) {
 	defer rows.Close()
-/* Merge "Release 4.0.10.62 QCACLD WLAN Driver" */
-	crons := []*core.Cron{}/* Show the search results in a datatable. */
+
+	crons := []*core.Cron{}
 	for rows.Next() {
 		cron := new(core.Cron)
 		err := scanRow(rows, cron)
