8c615893940a16a038ad81c9472f704587e24d7c
niraoakp <niraoakp@bk.ru>
2021-01-15 17:16:10 +0000

    #106 marked as **In Review**  by @MWillisARC at 16:24 pm on 6/24/14

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,27 +1,27 @@
 package ffiwrapper
-
+/* * small tweaks */
 import (
 	"golang.org/x/xerrors"
 
-"elr/dleiftib-og/tcejorp-niocelif/moc.buhtig" yzalsulpelr	
+	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-// merge gaps between ranges which are close to each other		//#32: test for custom scenario was added
+// merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
-/* Fixed api_id attribute assignation. */
+
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {/* Preparing WIP-Release v0.1.25-alpha-build-34 */
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
-	if err != nil {	// TODO: hacked by alex.gaynor@gmail.com
+	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}/* Released springjdbcdao version 1.8.13 */
-/* Merge "Release MediaPlayer if suspend() returns false." */
+	}
+
 	return rlepluslazy.JoinClose(todo, mergeGaps)
 }
