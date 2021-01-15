8ad663c2ba9381f2a75d5677118ebab8211c6531
niraoakp <niraoakp@bk.ru>
2021-01-15 17:25:26 +0000

    History graph fix to make it readable

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by nick@perfectabstractions.com
-// Use of this source code is governed by the Drone Non-Commercial License		//[GITFLOW]merging 'master' into 'develop'
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// Create make.py
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Added EncodeDecodeTest.java */
 package queue
 
 import (
-	"net/http"
+	"net/http"/* Merge "Updated Release Notes for 7.0.0.rc1. For #10651." */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Release '0.1~ppa18~loms~lucid'. */
+	"github.com/drone/drone/logger"
 )
-
+		//5e5894a5-2d16-11e5-af21-0401358ea401
 // HandlePause returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		err := scheduler.Pause(ctx)/* Merge "Upate versions after Dec 4th Release" into androidx-master-dev */
+		err := scheduler.Pause(ctx)
 		if err != nil {
-			render.InternalError(w, err)/* Pequena correção em 'Exemplo'. :coffee: */
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot pause scheduler")
-			return		//Merge branch 'master' into issue464
+			return
 		}
 		w.WriteHeader(http.StatusNoContent)
-	}	// TODO: hacked by steven@stebalien.com
-}/* Release 1.16.14 */
+	}
+}/* chore(package): update eslint to version 2.11.0 (#150) */
