1eab6bc8bce99f1c9395f288b294b50b1ad00d4c
niraoakp <niraoakp@bk.ru>
2021-01-15 04:04:52 +0000

    make marina-web api accessible

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Release 2.2.40 upgrade */
+
 import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
-/* remove paths() comment */
+/* Delete BuildRelease.proj */
