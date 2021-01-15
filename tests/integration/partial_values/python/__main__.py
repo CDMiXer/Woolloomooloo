94fc9262aeb3b715a19f78112c45e1b30ddc3d47
niraoakp <niraoakp@bk.ru>
2021-01-15 06:20:24 +0000

    Ajout mail lorsqu'un utilisateur créer un post + petits correctifs

--- tests/integration/partial_values/python/__main__.py
+++ tests/integration/partial_values/python/__main__.py
@@ -1,36 +1,36 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Merge "Fix broken actions with MessagingStyle" into nyc-dev
+
 import asyncio
 from pulumi import Output, export, UNKNOWN
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from pulumi.runtime import is_dry_run
-/* [checkstyle][fix 9eb779b05c585a] Import order */
-:)redivorPecruoseR(redivorPyM ssalc
-    def create(self, props):	// TODO: optimisation du cas si un document existe deja
-        return CreateResult("0", props)/* clang-tidy: fix performance-faster-string-find */
 
-class MyResource(Resource):/* [artifactory-release] Release version 1.3.0.M5 */
+class MyProvider(ResourceProvider):
+    def create(self, props):
+        return CreateResult("0", props)
+
+class MyResource(Resource):
     foo: Output
     bar: Output
     baz: Output
 
     def __init__(self, name, props, opts = None):
-        super().__init__(MyProvider(), name, props, opts)	// TODO: hacked by boringland@protonmail.ch
+        super().__init__(MyProvider(), name, props, opts)
 
-unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")		//f5a00585-2e9b-11e5-a6b1-a45e60cdfd11
-	// Catch SF BUG 1621938: gimpact only does stride 12.
-a = MyResource("a", {		//Stop waiting for a lock on the IOQueue before doing serialization, etc. 
+unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")
+
+a = MyResource("a", {
     "foo": "foo",
     "bar": { "value": "foo", "unknown": unknown },
-    "baz": [ "foo", unknown ],		//Fix a few typos in the "How to see into the future at GitLab" blog post
+    "baz": [ "foo", unknown ],
 })
-/* More protected area categories */
+
 async def check_knowns():
     assert await a.foo.is_known()
     assert await a.bar["value"].is_known()
-    assert await a.bar["unknown"].is_known() != is_dry_run()	// TODO: Update ConjurersGarb.cs
+    assert await a.bar["unknown"].is_known() != is_dry_run()
     assert await a.baz[0].is_known()
     assert await a.baz[1].is_known() != is_dry_run()
-    print("ok")/* Release of eeacms/ims-frontend:0.5.0 */
+    print("ok")
 
 export("o", check_knowns())
