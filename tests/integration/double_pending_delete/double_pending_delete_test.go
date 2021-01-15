6612012a7a74cf266f7cdc1a141c76d5840f06ca
niraoakp <niraoakp@bk.ru>
2021-01-15 20:07:21 +0000

    yarn rm: vcores

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -2,11 +2,11 @@
 // +build nodejs all
 
 package ints
-		//Added make data for xcmaster.
-import (	// TODO: no overlay
-	"testing"		//Merge "Add concrete example for stack creation and update."
 
-"sredivorp/yolped/ecruoser/2v/gkp/imulup/imulup/moc.buhtig"	
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
@@ -15,7 +15,7 @@ import (	// TODO: no overlay
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestDoublePendingDelete(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",	// 91929762-2e5e-11e5-9284-b827eb9e62be
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
@@ -29,7 +29,7 @@ func TestDoublePendingDelete(t *testing.T) {
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// TODO: ee160484-2e5d-11e5-9284-b827eb9e62be
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
@@ -42,30 +42,30 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.True(t, aCondemned.Delete)
 
 					b := stackInfo.Deployment.Resources[4]
-					assert.Equal(t, "b", string(b.URN.Name()))		//Update Turning_in_code.md
-					assert.False(t, b.Delete)/* factor out sizing issues */
+					assert.Equal(t, "b", string(b.URN.Name()))
+					assert.False(t, b.Delete)
 
 				},
-			},/* Merge "Release 1.0.0 - Juno" */
+			},
 			{
 				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* Delete CodeSkulptor.Release.bat */
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// There is still only one pending delete resource in this snapshot.
 					assert.NotNil(t, stackInfo.Deployment)
 
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))/* Release 1.0 - a minor correction within README.md. */
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
-					a := stackInfo.Deployment.Resources[2]		//381c374e-2e61-11e5-9284-b827eb9e62be
+					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
 
-					aCondemned := stackInfo.Deployment.Resources[3]	// TODO: Merge branch 'master' into T225635-update-string
+					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
 
@@ -78,9 +78,9 @@ func TestDoublePendingDelete(t *testing.T) {
 				Dir:      "step4",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// We should have cleared out all of the pending deletes now.	// TODO: rev 511404
-					assert.NotNil(t, stackInfo.Deployment)		//Merge pull request #11 from kevin-jueni/master
-/* Use single quotes. */
+					// We should have cleared out all of the pending deletes now.
+					assert.NotNil(t, stackInfo.Deployment)
+
 					assert.Equal(t, 4, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
