c06c2378e3e77835dfcd1309e78d4dc78cf64cbb
niraoakp <niraoakp@bk.ru>
2021-01-15 02:12:11 +0000

    Eclipse mess up?

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -1,39 +1,39 @@
-using Pulumi;	// TODO: hacked by steven@stebalien.com
+using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
 
-class MyStack : Stack		//Rename toggle-off.svg to toggle-off.svg.bak
-{		//Delete leaf2.png
-    public MyStack()
-    {/* Pedantic fixes, really fixing stupid bugs! */
+class MyStack : Stack	// Update lucia-manzano-girlsintech.html
+{
+    public MyStack()/* Delete mystery-aton.html */
+    {
         var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
         {
             ApiVersion = "v1",
             Kind = "Pod",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
-            {/* Merge branch 'master' into update_02 */
+            {
                 Namespace = "foo",
                 Name = "bar",
             },
-            Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs		//Merge "Default guideline to latest approved guideline"
+            Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
             {
                 Containers = 
                 {
-                    new Kubernetes.Types.Inputs.Core.V1.ContainerArgs		//Fixes a typo in README where joda was misspelled
+                    new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
                     {
                         Name = "nginx",
                         Image = "nginx:1.14-alpine",
                         Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
                         {
                             Limits = 
-                            {	// Thrift example
+                            {
                                 { "memory", "20Mi" },
-                                { "cpu", "0.2" },
-                            },/* fix: force new version test w/ CircleCI + Semantic Release */
-                        },
-                    },
-                },/* Release web view properly in preview */
+                                { "cpu", "0.2" },/* minimOS-63 ABI stub */
+                            },
+                        },/* Release 1-78. */
+                    },	// TODO: Moved all tests into a central folder. Moved public ct list to settings.
+                },
             },
         });
-}    
-	// TODO: will be fixed by davidad@alum.mit.edu
+    }
+
 }
