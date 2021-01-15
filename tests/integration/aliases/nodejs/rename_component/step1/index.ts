50368a696790151a91f9a80de28367ecd61a2fb6
niraoakp <niraoakp@bk.ru>
2021-01-15 09:56:54 +0000

    Edited the community page, updated Skills Matter Event

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release notes for 2.8. */
-import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {		//change addons
+import * as pulumi from "@pulumi/pulumi";/* 30d85ba0-2e4a-11e5-9284-b827eb9e62be */
+
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}/* Removed ormlite refs. */
+}
 
-// Scenario #3 - rename a component (and all it's children)/* FRESH-329: Update ReleaseNotes.md */
-class ComponentThree extends pulumi.ComponentResource {/* Add AF to graphics settings */
+// Scenario #3 - rename a component (and all it's children)
+class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
     resource2: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
@@ -18,6 +18,6 @@ class ComponentThree extends pulumi.ComponentResource {/* Add AF to graphics set
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, {parent: this});
         this.resource2 = new Resource("otherchild", {parent: this});
-    }/* Release of eeacms/bise-backend:v10.0.32 */
+    }/* Delete Release History.md */
 }
 const comp3 = new ComponentThree("comp3");
