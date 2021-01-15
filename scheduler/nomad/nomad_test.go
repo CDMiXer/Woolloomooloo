941c5ffcd45cfa12046c13ae6383374f6f2e0bae
niraoakp <niraoakp@bk.ru>
2021-01-15 10:14:37 +0000

    Rename css/font-awesome.css to font-awesome.css

--- scheduler/nomad/nomad_test.go
+++ scheduler/nomad/nomad_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Create ItensAProduzir.md
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Update Advanced SPC Mod 0.14.x Release version */
-// +build !oss
+// that can be found in the LICENSE file.	// TODO: change AMI ID for HelloWorld; temporarily block other workflows
 
-package nomad
+// +build !oss/* Merge "Release 1.0.0 with all backwards-compatibility dropped" */
+
+package nomad/* trigger new build for ruby-head (339efd1) */
