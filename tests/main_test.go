12215960f5acd9b2aef58dd3c7478484ed9a7150
niraoakp <niraoakp@bk.ru>
2021-01-15 10:21:53 +0000

    Add test for dialog cli/library.

--- tests/main_test.go
+++ tests/main_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
-)
+)/* Fixed the gozirra dependency */
 
 func TestMain(m *testing.M) {
 	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
@@ -16,8 +16,8 @@ func TestMain(m *testing.M) {
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
 		os.Exit(1)
-	}
-
+	}/* Delegate addition of prefixes to PublisherInfo. */
+		//Create deepspace.bas
 	code := m.Run()
 	os.Exit(code)
 }
