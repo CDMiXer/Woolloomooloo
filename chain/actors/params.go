b58df2f9ddb9b6755220be74cf212a0b76746a55
niraoakp <niraoakp@bk.ru>
2021-01-15 11:43:36 +0000

    corrected spellings/grammar for readability

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,14 +1,14 @@
 package actors
-		//Merge "[INTERNAL] ColumnHeaderPopover: Take item visibility into account"
+
 import (
 	"bytes"
-/* Bugs fixed; Release 1.3rc2 */
+
 	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Create pyhton programing */
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
-	// TODO: will be fixed by seth@sethvargo.com
+
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
