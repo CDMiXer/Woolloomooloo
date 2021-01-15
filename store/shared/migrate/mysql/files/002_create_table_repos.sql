dba215ed0abadf08950784bc7119cf8fb96e7e6d
niraoakp <niraoakp@bk.ru>
2021-01-15 12:28:16 +0000

    Create bitcoin_ar

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -7,11 +7,11 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)/* Fix issue in model context management */
+,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
-,repo_active                BOOLEAN/* Release of cai-util-u3d v0.2.0 */
+,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
@@ -19,21 +19,21 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_config                VARCHAR(500)
 ,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
-,repo_protected             BOOLEAN/* Release 0.1.31 */
+,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
-,repo_secret                VARCHAR(50)	// TODO: Adding installation of pyqt5 module.
+,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)		//On 5.5, utf8mb4 means that key lengths have shrunk
-);	// TODO: Changes to the OP_RETURN check
+,UNIQUE(repo_uid)
+);
 
 -- name: alter-table-repos-add-column-no-fork
 
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
-/* DCC-263 Add summary of submissions to ReleaseView object */
+
 -- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
@@ -43,5 +43,5 @@ ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-push
-/* Release v1.6.12. */
+
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
