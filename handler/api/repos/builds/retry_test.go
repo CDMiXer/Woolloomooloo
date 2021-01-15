c1a448cde0ad23c19105e03e4bb6da4567b0551f
niraoakp <niraoakp@bk.ru>
2021-01-15 22:30:07 +0000

    Delete KT8 DUAL+  Post-Postprocessor v2.1.exe

--- handler/api/repos/builds/retry_test.go
+++ handler/api/repos/builds/retry_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Reordering groups
+
 package builds
 
 import (
-	"context"	// Add Memory, MemorySwap and CpuShares mappings to HostConfig
+	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
@@ -21,25 +21,25 @@ import (
 )
 
 func TestRetry(t *testing.T) {
-	controller := gomock.NewController(t)		//move indentation
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release LastaThymeleaf-0.2.1 */
+
 	checkBuild := func(_ context.Context, _ *core.Repository, hook *core.Hook) error {
 		if got, want := hook.Trigger, mockUser.Login; got != want {
-			t.Errorf("Want Trigger By %s, got %s", want, got)/* Merge "[FAB-5527] Failures in orderer/consensus/kafka" */
+			t.Errorf("Want Trigger By %s, got %s", want, got)
 		}
 		if got, want := hook.Event, mockBuild.Event; got != want {
-			t.Errorf("Want Build Event %s, got %s", want, got)/* significantly improving mysql performance - as planned a while ago */
+			t.Errorf("Want Build Event %s, got %s", want, got)
 		}
 		if got, want := hook.Link, mockBuild.Link; got != want {
-			t.Errorf("Want Build Link %s, got %s", want, got)		//first successful ping from action
+			t.Errorf("Want Build Link %s, got %s", want, got)
 		}
 		if got, want := hook.Message, mockBuild.Message; got != want {
 			t.Errorf("Want Build Message %s, got %s", want, got)
 		}
 		if got, want := hook.Before, mockBuild.Before; got != want {
 			t.Errorf("Want Build Before %s, got %s", want, got)
-		}		//Updated license years range
+		}
 		if got, want := hook.After, mockBuild.After; got != want {
 			t.Errorf("Want Build After %s, got %s", want, got)
 		}
@@ -54,31 +54,31 @@ func TestRetry(t *testing.T) {
 		}
 		if got, want := hook.Author, mockBuild.Author; got != want {
 			t.Errorf("Want Build Author %s, got %s", want, got)
-		}/* Merge "[INTERNAL] Release notes for version 1.36.9" */
-		if got, want := hook.AuthorName, mockBuild.AuthorName; got != want {	// Add the documentation magic to the party. :nada:
+		}
+		if got, want := hook.AuthorName, mockBuild.AuthorName; got != want {
 			t.Errorf("Want Build AuthorName %s, got %s", want, got)
-		}		//filemerge: fix path to working file when fixeol is enabled
+		}
 		if got, want := hook.AuthorEmail, mockBuild.AuthorEmail; got != want {
 			t.Errorf("Want Build AuthorEmail %s, got %s", want, got)
 		}
 		if got, want := hook.AuthorAvatar, mockBuild.AuthorAvatar; got != want {
 			t.Errorf("Want Build AuthorAvatar %s, got %s", want, got)
 		}
-		if got, want := hook.Sender, mockBuild.Sender; got != want {		//new palette
-			t.Errorf("Want Build Sender %s, got %s", want, got)		//Create usercreation.png
+		if got, want := hook.Sender, mockBuild.Sender; got != want {
+			t.Errorf("Want Build Sender %s, got %s", want, got)
 		}
 		return nil
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)	// TODO: Better conda badge
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
-/* we can't define the current_user method at all. It breaks eager loading */
+
 	triggerer := mock.NewMockTriggerer(controller)
 	triggerer.EXPECT().Trigger(gomock.Any(), mockRepo, gomock.Any()).Return(mockBuild, nil).Do(checkBuild)
-/* Add Release Notes section */
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
