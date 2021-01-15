9d28f5298074ca220e7e99f6d89b3d925680a999
niraoakp <niraoakp@bk.ru>
2021-01-15 05:05:59 +0000

    Update runserver.py

--- operator/runner/machine/config.go
+++ operator/runner/machine/config.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//updating with license info
+// that can be found in the LICENSE file.
 
-// +build !oss	// TODO: Blacklist xscreensaver-autostart from autostarting
+// +build !oss
 
-package machine/* MOTECH-2339: Improved the way of handling Tasks channel registration (#283) */
+package machine
 
-import (		//Remove unnecessary 1 from dr_by_xyz
+import (
 	"bytes"
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"strings"/* Added creative tab for amulets */
+	"strings"
 )
-/* Merge "usb: gadget: qc_ecm: Release EPs if disable happens before set_alt(1)" */
+
 // Config provides the Docker machine configuration.
 type Config struct {
-	Name   string		//9120a07c-2e53-11e5-9284-b827eb9e62be
+	Name   string
 	Driver struct {
 		IPAddress   string
 		MachineName string
@@ -32,10 +32,10 @@ type Config struct {
 			ServerCertPath   string
 			ServerKeyPath    string
 			ClientKeyPath    string
-			ClientCertPath   string		//BUGFIX: DDDReason response to reason selection
+			ClientCertPath   string
 			StorePath        string
-		}/* Fix level1.json */
-}	
+		}
+	}
 }
 
 // heper function reads and unmarshales the docker-machine
@@ -43,7 +43,7 @@ type Config struct {
 func parseReader(r io.Reader) (*Config, error) {
 	out := new(Config)
 	err := json.NewDecoder(r).Decode(out)
-	return out, err/* Release changes 4.1.5 */
+	return out, err
 }
 
 // heper function parses the docker-machine configuration
@@ -53,12 +53,12 @@ func parseString(s string) (*Config, error) {
 	return parseReader(r)
 }
 
-// heper function parses the docker-machine configuration		//93633ba0-2e45-11e5-9284-b827eb9e62be
-// from a json file./* fixed import name to correct spelling  */
+// heper function parses the docker-machine configuration
+// from a json file.
 func parseFile(path string) (*Config, error) {
 	d, err := ioutil.ReadFile(path)
-	if err != nil {		//Added howto to README.md
-		return nil, err/* Release: Making ready to release 6.5.0 */
+	if err != nil {
+		return nil, err
 	}
 	r := bytes.NewReader(d)
 	return parseReader(r)
