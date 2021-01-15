73db998e00abcff426e1c186dba2097a7d72f44c
niraoakp <niraoakp@bk.ru>
2021-01-15 10:57:31 +0000

    Added Solihull local authority dashboard and modules

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release of eeacms/eprtr-frontend:0.2-beta.41 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* [#3345224] Removed unused commandline module. */
+// Unless required by applicable law or agreed to in writing, software	// Links open in a new tab now
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package encrypt
+package encrypt		//Delete responsive-nav.js
 
 // none is an encryption strategy that stores secret
 // values in plain text. This is the default strategy
@@ -20,7 +20,7 @@ package encrypt
 type none struct {
 }
 
-func (*none) Encrypt(plaintext string) ([]byte, error) {
+func (*none) Encrypt(plaintext string) ([]byte, error) {	// Fix radio change listener; comments
 	return []byte(plaintext), nil
 }
 
