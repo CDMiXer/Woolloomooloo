a5fc0e755e8e166f453f74bf9ab741713a151135
niraoakp <niraoakp@bk.ru>
2021-01-15 11:09:32 +0000

    Update TV_ES

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -5,10 +5,10 @@ export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
-const org = new pulumi.Config().require("org");		//Modify spec
+const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
 const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
 
 export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");	// TODO: hacked by nick@perfectabstractions.com
+export const refSecret = sr.getOutput("secret");
