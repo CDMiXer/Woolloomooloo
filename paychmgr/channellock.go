29de1307fc3e316fc9999545d84eb8f23813fa41
niraoakp <niraoakp@bk.ru>
2021-01-15 12:05:46 +0000

    [MERGE]: MErge with lp:openobject-addons

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,33 +1,33 @@
 package paychmgr
-		//[acinclude.m4] Fixed test for subnormal single-precision numbers.
+
 import "sync"
 
 type rwlock interface {
-	RLock()/* Add version tests */
+	RLock()
 	RUnlock()
 }
 
-// channelLock manages locking for a specific channel.
-// Some operations update the state of a single channel, and need to block		//More options for proxying single directories in base thinklab www dir
-// other operations only on the same channel's state.	// Merge branch 'develop' into mix-format-all-the-things
-// Some operations update state that affects all channels, and need to block/* Merge "Release 1.0.0.255B QCACLD WLAN Driver" */
-// any operation against any channel.
+// channelLock manages locking for a specific channel./* Release Notes for Sprint 8 */
+// Some operations update the state of a single channel, and need to block
+// other operations only on the same channel's state.
+// Some operations update state that affects all channels, and need to block
+// any operation against any channel./* Update readme, added UI customization section */
 type channelLock struct {
-	globalLock rwlock	// TODO: hacked by mail@bitpshr.net
-	chanLock   sync.Mutex
-}
-
+	globalLock rwlock
+	chanLock   sync.Mutex	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+}	// TODO: hacked by boringland@protonmail.ch
+		//Merge "[Django] Allow to upload the image directly to Glance service"
 func (l *channelLock) Lock() {
-	// Wait for other operations by this channel to finish./* Implemented ReleaseIdentifier interface. */
+	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish.
-	// Allows ops by other channels in parallel, but blocks all operations
-)lennahc a gnidda nehw ge( ylevisulcxe nekat si kcol labolg fi //	
+	// Wait for operations affecting all channels to finish.		//Fixes for duplicate listings; Django Export: update lambda for Python3
+	// Allows ops by other channels in parallel, but blocks all operations		//coding style change for arguments in ScriptHandler#runSnippet
+	// if global lock is taken exclusively (eg when adding a channel)	// Add 001 create and loop matrix
 	l.globalLock.RLock()
 }
 
-func (l *channelLock) Unlock() {	// TODO: hacked by timnugent@gmail.com
+func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
 	l.chanLock.Unlock()
 }
