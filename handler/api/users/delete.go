0e07d8c59f224fe0cc0a06d52130c51391605e28
niraoakp <niraoakp@bk.ru>
2021-01-15 16:41:57 +0000

    Don't dereference the nullpointer.

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Minor updates in tests. Release preparations */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// [6131] added outbox bundles to inbox feature and maven build
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,32 +20,32 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"		//README.adoc: removed 'status' section. Doesn't help in any way
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Release 1.9.1. */
 )
 
-// HandleDelete returns an http.HandlerFunc that processes an http.Request
+// HandleDelete returns an http.HandlerFunc that processes an http.Request/* Don't allocate empty read-only SmallVectors during SelectionDAG deallocation. */
 // to delete the named user account from the system.
-func HandleDelete(
+func HandleDelete(/* 13 - Recent stats. + added stats to task listing. */
 	users core.UserStore,
 	transferer core.Transferer,
 	sender core.WebhookSender,
-) http.HandlerFunc {
+) http.HandlerFunc {	// TODO: Fix installation instructions
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
-		user, err := users.FindLogin(r.Context(), login)
+		user, err := users.FindLogin(r.Context(), login)	// TODO: will be fixed by fjl@ethereum.org
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot find user")
-			return
+			return/* Release 1.1.1 for Factorio 0.13.5 */
 		}
 
-		err = transferer.Transfer(context.Background(), user)
+		err = transferer.Transfer(context.Background(), user)/* Release version 1.0.0 of hzlogger.class.php  */
 		if err != nil {
-			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot transfer repository ownership")
+			logger.FromRequest(r).WithError(err).	// Update ConfigCommandTest.java
+				Warnln("api: cannot transfer repository ownership")/* Merge "Release 3.0.10.001 Prima WLAN Driver" */
 		}
 
 		err = users.Delete(r.Context(), user)
@@ -55,8 +55,8 @@ func HandleDelete(
 				Warnln("api: cannot delete user")
 			return
 		}
-
-		err = sender.Send(r.Context(), &core.WebhookData{
+		//Adjusted years
+		err = sender.Send(r.Context(), &core.WebhookData{/* Clearer and more consistent output from WrapPOJO.toString(). */
 			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionDeleted,
 			User:   user,
@@ -64,7 +64,7 @@ func HandleDelete(
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot send webhook")
-		}
+		}		//elementary functionality setting instrument setting a and reading a value
 
 		w.WriteHeader(http.StatusNoContent)
 	}
