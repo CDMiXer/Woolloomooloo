79d85fed88848686717c09bcee1052e93a628d1a
niraoakp <niraoakp@bk.ru>
2021-01-15 01:08:56 +0000

    Added json error logging

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Fix incorrect 2's complement conversion
-///* Release 2.2 tagged */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,10 +8,10 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Simplifying test before using as the basis for another. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: TYPO fixed: some lines start with space.
+
 package db
 
 type nopLocker struct{}
