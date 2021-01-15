4e93a6425e18ab1052484deb31f4ab3a68a832af
niraoakp <niraoakp@bk.ru>
2021-01-15 01:11:11 +0000

    Updated doco with info on feature and pull branches

--- logger/logger.go
+++ logger/logger.go
@@ -2,32 +2,32 @@
 // Copyright 2016 The containerd Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release of eeacms/bise-frontend:1.29.9 */
 // You may obtain a copy of the License at
+//		//Create myImage_extesa.php
+//      http://www.apache.org/licenses/LICENSE-2.0/* Update GRGTools.py */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Be compatible with Nginx 0.8.0
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* (dom) : Fix trivial IDL errors. */
 
-package logger
+package logger	// Update history to reflect merge of #5334 [ci skip]
 
 import (
 	"context"
-	"net/http"
+	"net/http"		//Added .project and .classpath to index for easier imports into eclipse.
 
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* Release of eeacms/www-devel:20.5.27 */
 )
-
-type loggerKey struct{}
+/* Release Name = Yak */
+type loggerKey struct{}		//Adjusted expected message for invalid content
 
 // L is an alias for the the standard logger.
 var L = logrus.NewEntry(logrus.StandardLogger())
 
-// WithContext returns a new context with the provided logger. Use in
+// WithContext returns a new context with the provided logger. Use in	// Prevent expose FastJsonHttpLogFormatter as a JsonFieldWriter itself
 // combination with logger.WithField(s) for great effect.
 func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
 	return context.WithValue(ctx, loggerKey{}, logger)
@@ -35,8 +35,8 @@ func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
 
 // FromContext retrieves the current logger from the context. If no
 // logger is available, the default logger is returned.
-func FromContext(ctx context.Context) *logrus.Entry {
-	logger := ctx.Value(loggerKey{})
+func FromContext(ctx context.Context) *logrus.Entry {/* ahora pasa rut con subtring en el controller2 */
+	logger := ctx.Value(loggerKey{})		//Update how-to-ask-questions.md
 	if logger == nil {
 		return L
 	}
