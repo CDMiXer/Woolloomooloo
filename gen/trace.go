46e594c0e928d40f6ea0158e842a5b32a1c6e1d4
niraoakp <niraoakp@bk.ru>
2021-01-15 18:29:37 +0000

    Update Dungeon.py

--- gen/trace.go
+++ gen/trace.go
@@ -2,18 +2,18 @@
 
 package websocket
 
-import (
+import (/* Merge "[INTERNAL] Release notes for version 1.32.0" */
 	"crypto/tls"
 	"net/http/httptrace"
-)
-
+)/* [Release 0.8.2] Update change log */
+/* Release 1-136. */
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
 	}
-	err := doHandshake(tlsConn, cfg)
+	err := doHandshake(tlsConn, cfg)/* Release v3.7.0 */
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
-	return err
+	return err	// TODO: Update and rename Documentacion to Documentacion/Stakeholders-final
 }
