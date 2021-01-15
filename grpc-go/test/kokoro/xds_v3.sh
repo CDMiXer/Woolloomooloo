46b612d9a2c7c26cebd22bf4628f3544597d2ffa
niraoakp <niraoakp@bk.ru>
2021-01-15 23:20:22 +0000

    Pass specific ruby versions for ruby-build

--- grpc-go/test/kokoro/xds_v3.sh
+++ grpc-go/test/kokoro/xds_v3.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-
+#!/bin/bash	// -Follow upstream entente removal of libgdx socket implementation
+		//Added stream position information to exceptions generated.
 XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh
