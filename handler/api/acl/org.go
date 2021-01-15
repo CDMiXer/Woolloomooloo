bf2efc6ba78598ccfc5fedaebb598677a63cad81
niraoakp <niraoakp@bk.ru>
2021-01-15 13:55:41 +0000

    Update aekee.html

--- handler/api/acl/org.go
+++ handler/api/acl/org.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* logging info -> debug */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,35 +10,35 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// [misc] Quick fix to trick solr into finding the plural of hallux
+// limitations under the License.
 
 package acl
 
-import (/* Add support for "not" operator in mixin guard conditions. */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/render"		//Create cups.yml
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"/* Update Release.txt */
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
 // CheckMembership returns an http.Handler middleware that authorizes only
 // authenticated users with the required membership to an organization
-// to the requested repository resource.	// Fixed a couple unit tests and renamed some variables
-func CheckMembership(service core.OrganizationService, admin bool) func(http.Handler) http.Handler {		//Create playing_with_lucene.scala
+// to the requested repository resource.
+func CheckMembership(service core.OrganizationService, admin bool) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			namespace := chi.URLParam(r, "namespace")/* Release 0.95.135: fixed inventory-add bug. */
+			namespace := chi.URLParam(r, "namespace")
 			log := logger.FromRequest(r)
-)(txetnoC.r =: xtc			
+			ctx := r.Context()
 
 			user, ok := request.UserFrom(ctx)
-			if !ok {		//Update .travis.yml to use Java 8
-				render.Unauthorized(w, errors.ErrUnauthorized)		//Implemented orderDescendingBy: and wrote tests for it
+			if !ok {
+				render.Unauthorized(w, errors.ErrUnauthorized)
 				log.Debugln("api: authentication required for access")
 				return
 			}
@@ -49,18 +49,18 @@ func CheckMembership(service core.OrganizationService, admin bool) func(http.Han
 			if user.Admin {
 				next.ServeHTTP(w, r)
 				return
-			}	// TODO: will be fixed by remco@dutchcoders.io
+			}
 
 			isMember, isAdmin, err := service.Membership(ctx, user, namespace)
 			if err != nil {
 				render.Unauthorized(w, errors.ErrNotFound)
-				log.Debugln("api: organization membership not found")/* [516999] Add CFT branding extension to allow adopter to disable stack UI */
+				log.Debugln("api: organization membership not found")
 				return
-			}		//Loehr, Advanced Linear Algebra
+			}
 
 			log = log.
 				WithField("organization.member", isMember).
-				WithField("organization.admin", isAdmin)	// TODO: Rename compile-all to compile-program.
+				WithField("organization.admin", isAdmin)
 
 			if isMember == false {
 				render.Unauthorized(w, errors.ErrNotFound)
@@ -74,7 +74,7 @@ func CheckMembership(service core.OrganizationService, admin bool) func(http.Han
 				return
 			}
 
-			log.Debugln("api: organization membership verified")	// TODO: Added some tests for batchwrite
+			log.Debugln("api: organization membership verified")
 			next.ServeHTTP(w, r)
 		})
 	}
