124d57d7de26c0ee030fc56da2ec23e3fcea86bf
niraoakp <niraoakp@bk.ru>
2021-01-15 18:34:22 +0000

    [20706] fix outbox button spelling on send mail dialog

--- handler/api/repos/enable_test.go
+++ handler/api/repos/enable_test.go
@@ -1,71 +1,71 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//70706928-2e55-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License/* Fix some minor bugs in releasing_objects tests */
 // that can be found in the LICENSE file.
 
-package repos	// TODO: d2022234-2e60-11e5-9284-b827eb9e62be
+package repos
 
-import (
-	"context"
-	"encoding/json"/* Merged branch development into Release */
+import (/* AbstractLock added */
+	"context"/* add documentation for epub:split-css */
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	// Create a working windows batch file to run webpack
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"/* Release version: 0.4.4 */
-
+	"github.com/drone/drone/handler/api/request"	// ndbmtd - for now redefine asserts to requires
+	"github.com/drone/drone/mock"
+		//Merge "Add .list-style-image-svg"
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
-		//Removes link/dependency with Authorize.net module
-func TestEnable(t *testing.T) {		//Fix memory leaks with pam_session_get_envlist
+
+func TestEnable(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release 1.0.0-rc1 */
-/* simplify keyboard handling in the document view */
+	defer controller.Finish()
+
 	repo := &core.Repository{
-		ID:        1,
-		Namespace: "octocat",
+		ID:        1,/* require local_dir for Releaser as well */
+		Namespace: "octocat",/* Add slashes so the icons work on all pages */
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
 	}
-		//Added youtube screencast link
-	service := mock.NewMockHookService(controller)	// Create EWBF.txt
+
+	service := mock.NewMockHookService(controller)
 	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(nil)
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)/* remove `@next` since it's not necessary on the scoped packages change [skip ci] */
-	repos.EXPECT().Activate(gomock.Any(), repo).Return(nil)
+	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)/* Release of eeacms/bise-frontend:1.29.12 */
+	repos.EXPECT().Activate(gomock.Any(), repo).Return(nil)	// TODO: Update CoconutMacaroons.md
 
 	// a failed webhook should result in a warning message in the
 	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
 
-	c := new(chi.Context)	// phases: add list of string to access phase name
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-/* Update ReleaseChangeLogs.md */
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", nil)/* Release 6.4.0 */
-	r = r.WithContext(/* Merge "Release 1.0.0.191 QCACLD WLAN Driver" */
-		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
-	)/* add postgres view for max create date of inventory line of product */
 
+	w := httptest.NewRecorder()	// TODO: will be fixed by steven@stebalien.com
+	r := httptest.NewRequest("POST", "/", nil)
+	r = r.WithContext(
+		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
+	)
+/* Update faostat-download.js */
 	HandleEnable(service, repos, webhook)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* 4.00.4a Release. Fixed crash bug with street arrests. */
 
 	if got, want := repo.Active, true; got != want {
-		t.Errorf("Want repository activate %v, got %v", want, got)
-	}
+		t.Errorf("Want repository activate %v, got %v", want, got)/* Merge "remove job settings for Release Management repositories" */
+	}	// TODO: hacked by ligi@ligi.de
 
-	got, want := new(core.Repository), repo
+	got, want := new(core.Repository), repo/* Update portofoliopage5.md */
 	json.NewDecoder(w.Body).Decode(got)
 	diff := cmp.Diff(got, want, cmpopts.IgnoreFields(core.Repository{}, "Secret", "Signer"))
 	if diff != "" {
