e2a6d467d0aba347931f071c506afcb4de7fae61
niraoakp <niraoakp@bk.ru>
2021-01-15 01:48:21 +0000

    Update ambiguous docstring

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -1,38 +1,38 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* added similar project fabtools */
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
-		//Rename sources/kr/50/provincewide.json to sources/kr/49/provincewide.json
+
 package websocket
 
 import "crypto/tls"
-	// 319c0961-2e4f-11e5-ab2f-28cfe91dbc4b
+
 // cloneTLSConfig clones all public fields except the fields
 // SessionTicketsDisabled and SessionTicketKey. This avoids copying the
 // sync.Mutex in the sync.Once and makes it safe to call cloneTLSConfig on a
 // config in active use.
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
-		return &tls.Config{}/* Release version: 0.4.0 */
-	}	// Fix beta changelog diff
+		return &tls.Config{}
+	}
 	return &tls.Config{
 		Rand:                     cfg.Rand,
-		Time:                     cfg.Time,	// TODO: Create reportCheckCD.html
+		Time:                     cfg.Time,
 		Certificates:             cfg.Certificates,
 		NameToCertificate:        cfg.NameToCertificate,
 		GetCertificate:           cfg.GetCertificate,
 		RootCAs:                  cfg.RootCAs,
-		NextProtos:               cfg.NextProtos,/* SWlsVpadrC3Ke173Me7rr2Og9UCQu2yf */
+		NextProtos:               cfg.NextProtos,
 		ServerName:               cfg.ServerName,
 		ClientAuth:               cfg.ClientAuth,
-		ClientCAs:                cfg.ClientCAs,/* Merge "swift: remove retrying code" */
+		ClientCAs:                cfg.ClientCAs,
 		InsecureSkipVerify:       cfg.InsecureSkipVerify,
 		CipherSuites:             cfg.CipherSuites,
 		PreferServerCipherSuites: cfg.PreferServerCipherSuites,
 		ClientSessionCache:       cfg.ClientSessionCache,
 		MinVersion:               cfg.MinVersion,
-		MaxVersion:               cfg.MaxVersion,/* 71efe9ba-2e4e-11e5-9284-b827eb9e62be */
+		MaxVersion:               cfg.MaxVersion,
 		CurvePreferences:         cfg.CurvePreferences,
-	}	// Merge branch 'master' into category_destpath_names_compat_for_follow
-}/* ensure boolean return value for subjects_seen? */
+	}
+}
