6309e9e866e6dae58417e357c460290079e6a335
niraoakp <niraoakp@bk.ru>
2021-01-15 19:28:48 +0000

    Fixed the back button on profile columns

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -2,10 +2,10 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-let config = new pulumi.Config();	// 62aa5cc6-2e4c-11e5-9284-b827eb9e62be
+let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);
+let a = new pulumi.StackReference(slug);		//hideOnClosest
 
 let gotError = false;
 try
@@ -19,4 +19,4 @@ catch (err)
 
 if (!gotError) {
     throw new Error("Expected to get error trying to read secret from stack reference.");
-}	// TODO: hacked by boringland@protonmail.ch
+}
