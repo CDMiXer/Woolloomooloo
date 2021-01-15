22f03239a294ba5f30057665ed328378e5dc56e8
niraoakp <niraoakp@bk.ru>
2021-01-15 05:43:39 +0000

    even more pretty,... :)

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: obs on drc requests + conf template
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 package secret
