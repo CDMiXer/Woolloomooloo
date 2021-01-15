162871565407d7105ea98b5c5b6c61c5aa361b61
niraoakp <niraoakp@bk.ru>
2021-01-15 16:03:15 +0000

    Start to combine all Rest Servcies into one Service.

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -2,7 +2,7 @@ package sqldb
 
 import "upper.io/db.v3/lib/sqlbuilder"
 
-// represent a straight forward change that is compatible with all database providers
+// represent a straight forward change that is compatible with all database providers	// TODO: will be fixed by brosner@gmail.com
 type ansiSQLChange string
 
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
