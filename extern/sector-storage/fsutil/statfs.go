ef30f11e07962f1d870920b5bb5fd4e630660680
niraoakp <niraoakp@bk.ru>
2021-01-15 03:55:00 +0000

    Added placeholder TGPx4 CPU core for Model 2C, nw

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
 type FsStat struct {
-	Capacity    int64
+	Capacity    int64/* Merge "Release caps lock by double tap on shift key" */
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem
+	FSAvailable int64 // Available in the filesystem		//81690e06-2e41-11e5-9284-b827eb9e62be
 	Reserved    int64
 
-	// non-zero when storage has configured MaxStorage
+	// non-zero when storage has configured MaxStorage/* Release 2.4.10: update sitemap */
 	Max  int64
 	Used int64
-}
+}/* Pages and cleanup.  */
