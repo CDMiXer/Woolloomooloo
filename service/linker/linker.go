881a55da6963400152bcf40c099092efff66c1c9
niraoakp <niraoakp@bk.ru>
2021-01-15 23:25:07 +0000

    Merge "Give TestCase::checkHasDiff3 a better name"

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Orange County Register by Lorenzo Vigentini
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update pdfbox dependencies */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//crash fixes
-// Unless required by applicable law or agreed to in writing, software		//5374d582-2e6b-11e5-9284-b827eb9e62be
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Disable smoothing by default. Smoothing can be very expensive.
-// limitations under the License./* Release: Making ready to release 6.7.0 */
-/* Delete Blood */
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package linker
 
 import (
@@ -21,19 +21,19 @@ import (
 	"github.com/drone/go-scm/scm"
 )
 
-// New returns a new Linker server.		//Update 00.md
+// New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
 	return &service{
 		client: client,
 	}
 }
-/* Release perform only deploy goals */
-type service struct {/* SAE-164 Release 0.9.12 */
+
+type service struct {
 	client *scm.Client
-}/* Delete AIF Framework Release 4.zip */
+}
 
 func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
-	return s.client.Linker.Resource(ctx, repo, scm.Reference{/* Release of eeacms/eprtr-frontend:0.4-beta.21 */
+	return s.client.Linker.Resource(ctx, repo, scm.Reference{
 		Path: ref,
 		Sha:  sha,
 	})
