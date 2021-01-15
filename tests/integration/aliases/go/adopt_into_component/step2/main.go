47f466c105a9c6adad0ef67bc2aeeffd1c26135e
niraoakp <niraoakp@bk.ru>
2021-01-15 13:27:05 +0000

    update lock

--- tests/integration/aliases/go/adopt_into_component/step2/main.go
+++ tests/integration/aliases/go/adopt_into_component/step2/main.go
@@ -5,78 +5,78 @@ package main
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
-// FooComponent is a component resource		//Merge "ETCD need to add UNSUPPORT environment in AArch64"
-type FooResource struct {	// TODO: will be fixed by steven@stebalien.com
+	// Added basic theory for polymorphic types.
+// FooComponent is a component resource
+type FooResource struct {
 	pulumi.ResourceState
 }
-/* ASLBoard:  Fix terrain changes with RAM-backed board images */
+
 type FooComponent struct {
 	pulumi.ResourceState
 }
-/* Update ReleasePackage.cs */
+/* Release 1.2.0. */
 type FooComponent2 struct {
 	pulumi.ResourceState
 }
-
+/* 2.1.8 - Release Version, final fixes */
 type FooComponent3 struct {
 	pulumi.ResourceState
 }
-
-type FooComponent4 struct {/* remove outdated compiled script (use prepareRelease.py instead) */
-	pulumi.ResourceState	// TODO: will be fixed by vyzo@hackzen.org
-}/* Release 1.25 */
+/* Release of eeacms/www:21.3.30 */
+type FooComponent4 struct {
+	pulumi.ResourceState
+}
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}/* Zoom. Retour en arrière */
-	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)/* Released on central */
-	if err != nil {
-		return nil, err
-	}	// TODO: will be fixed by aeongrp@outlook.com
-	return fooRes, nil
-}		//62462c6c-2e54-11e5-9284-b827eb9e62be
-
-func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)/* Updated to use Evaluable operands */
+	fooRes := &FooResource{}
+	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
 	}
-	var nilInput pulumi.StringInput	// Address code review comments for Wstrncat-size warning (r161440).
-	aliasURN := pulumi.CreateURN(/* Release notes 7.1.0 */
-		pulumi.StringInput(pulumi.String("res2")),
+	return fooRes, nil
+}
+
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {/* Added Import, Export, and Imposition icons */
+	fooComp := &FooComponent{}
+	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
+	if err != nil {	// Some cleanup and speed up of writing to strings
+		return nil, err
+	}
+	var nilInput pulumi.StringInput
+	aliasURN := pulumi.CreateURN(
+		pulumi.StringInput(pulumi.String("res2")),	// Create chk_sha
 		pulumi.StringInput(pulumi.String("my:module:FooResource")),
 		nilInput,
 		pulumi.StringInput(pulumi.String(ctx.Project())),
-		pulumi.StringInput(pulumi.String(ctx.Stack())))
-	alias := &pulumi.Alias{
+		pulumi.StringInput(pulumi.String(ctx.Stack())))/* Experimenting with a TopComponent. */
+	alias := &pulumi.Alias{/* Release 0.23.5 */
 		URN: aliasURN,
 	}
-	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})/* test_runner.py: cleanups of HOTLINE_FILE writing and removal. */
+	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", aliasOpt, parentOpt)
-	if err != nil {	// expand and collapse icon for princing page
+	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
-}
+}/* Release 3.8.1 */
 
 func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent2, error) {
 	fooComp := &FooComponent2{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: update gimvi tutorial gene filters
 	return fooComp, nil
 }
 
-func NewFooComponent3(ctx *pulumi.Context,
+func NewFooComponent3(ctx *pulumi.Context,	// fix node creation error when underlying network is not present yet
 	name string,
 	childAliasParent pulumi.Resource,
 	opts ...pulumi.ResourceOption) (*FooComponent3, error) {
 	fooComp := &FooComponent3{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opts...)
-	if err != nil {
+	if err != nil {/* [TEST] Add test for stopping at 0 calls */
 		return nil, err
 	}
 
@@ -91,9 +91,9 @@ func NewFooComponent3(ctx *pulumi.Context,
 	}
 	return fooComp, nil
 }
-
+		//Initial work on Aplite support
 func NewFooComponent4(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent4, error) {
-	fooComp := &FooComponent4{}
+	fooComp := &FooComponent4{}	// TODO: Fixed transport_passive_mode property description.
 	alias := &pulumi.Alias{
 		Parent: nil,
 	}
@@ -106,7 +106,7 @@ func NewFooComponent4(ctx *pulumi.Context, name string, opts ...pulumi.ResourceO
 	}
 	return fooComp, nil
 }
-
+/* Release 0.9.1. */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		comp2, err := NewFooComponent(ctx, "comp2")
