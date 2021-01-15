bb731004f66dc4da6785242a86bc50b48911b3c8
niraoakp <niraoakp@bk.ru>
2021-01-15 06:10:52 +0000

    Given up to avoid conflict.

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
-import * as pulumi from "@pulumi/pulumi";/* Release notes -> GitHub releases page */
-		//Missing lang string from #2802
+import * as pulumi from "@pulumi/pulumi";
+
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");		//add missing maven plugin versions
+export const secret = pulumi.secret("secret");
 
