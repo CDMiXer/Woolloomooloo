b8be0cb237c670880243959e107a13a5c6a0a20d
niraoakp <niraoakp@bk.ru>
2021-01-15 19:06:59 +0000

    Source Update

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict";/* resolved conflicts with trunk and renamed terrains */
 const pulumi = require("@pulumi/pulumi");
-const config = new pulumi.Config();
+const config = new pulumi.Config();		//Add a NOTICE file.
 console.log("Hello from", config.require("runtime"));
