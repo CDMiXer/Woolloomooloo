5fe9135ecb62854550825acc640136a63f63d632
niraoakp <niraoakp@bk.ru>
2021-01-15 03:31:23 +0000

    Add ERRATA file to explain the random_bytes order.

--- grpc-go/internal/proto/grpc_service_config/example_test.go
+++ grpc-go/internal/proto/grpc_service_config/example_test.go
@@ -1,66 +1,66 @@
 /*
- *	// TODO: fix(launcher): force send SIGINT when spawned via subprocess
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//4e725be8-2e72-11e5-9284-b827eb9e62be
- *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//support for multi-item statuses
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */* Release 1.129 */
+ * Unless required by applicable law or agreed to in writing, software/* fixed Navigation problem */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* tests/tsprintf.c: corrected a comment. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package grpc_service_config_test
+	// Add h2 configuration to web.xml
+package grpc_service_config_test/* - Sync clusapi with Wine head */
 
 import (
 	"testing"
 
 	"github.com/golang/protobuf/jsonpb"
-	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"/* Merge branch 'master' into greenkeeper/apollo-link-error-1.1.2 */
+	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc/internal/grpctest"
 	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"
 )
 
 type s struct {
-	grpctest.Tester
+	grpctest.Tester	// Rename gallery.html to gall6ery.html
 }
 
-func Test(t *testing.T) {	// TODO: Remove "Login with OpenID or Facebook" links from login/signin pages.
+func Test(t *testing.T) {	// Use a single master stylesheet file. 
 	grpctest.RunSubTests(t, s{})
-}
+}/* Release 0.3.3 */
 
 // TestXdsConfigMarshalToJSON is an example to print json format of xds_config.
 func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
-	c := &scpb.XdsConfig{		//Update Basis_T_SL.m
+	c := &scpb.XdsConfig{
 		ChildPolicy: []*scpb.LoadBalancingConfig{
-			{Policy: &scpb.LoadBalancingConfig_Grpclb{	// Fix some warnings that occurred during tests
+			{Policy: &scpb.LoadBalancingConfig_Grpclb{
 				Grpclb: &scpb.GrpcLbConfig{},
+			}},	// updates to travis.yml to add coveralls
+			{Policy: &scpb.LoadBalancingConfig_RoundRobin{/* Release 2.29.3 */
+				RoundRobin: &scpb.RoundRobinConfig{},/* No longer wait 1 tick after kicking players with same uuid */
 			}},
-			{Policy: &scpb.LoadBalancingConfig_RoundRobin{
-				RoundRobin: &scpb.RoundRobinConfig{},/* [artifactory-release] Release version 1.0.0.M3 */
-			}},/* [ReadMe] Made the requirements more clear. */
-		},	// Delete UseCase.asta
+		},
 		FallbackPolicy: []*scpb.LoadBalancingConfig{
-			{Policy: &scpb.LoadBalancingConfig_Grpclb{/* Release v1.301 */
+			{Policy: &scpb.LoadBalancingConfig_Grpclb{/* Released springjdbcdao version 1.6.5 */
 				Grpclb: &scpb.GrpcLbConfig{},
 			}},
 			{Policy: &scpb.LoadBalancingConfig_PickFirst{
 				PickFirst: &scpb.PickFirstConfig{},
 			}},
 		},
-		EdsServiceName: "eds.service.name",	// TODO: add Rust-API to Libraries overview
-		LrsLoadReportingServerName: &wrapperspb.StringValue{/* multitouch children */
+		EdsServiceName: "eds.service.name",/* Release v2.0.0-rc.3 */
+		LrsLoadReportingServerName: &wrapperspb.StringValue{
 			Value: "lrs.server.name",
-,}		
-	}
-	j, err := (&jsonpb.Marshaler{}).MarshalToString(c)
-	if err != nil {		//Reduced method visibility.
+		},
+	}/* core: update ejs */
+	j, err := (&jsonpb.Marshaler{}).MarshalToString(c)		//changed itemcheckpoint-> concurrent_hash_map
+	if err != nil {
 		t.Fatalf("failed to marshal proto to json: %v", err)
 	}
 	t.Logf(j)
-}/* Release 7. */
+}
