d84c29281881267277c620e937ed668a9e98992a
niraoakp <niraoakp@bk.ru>
2021-01-15 16:00:36 +0000

    more consistent note

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -1,32 +1,32 @@
-using Pulumi;
+using Pulumi;	// Merge "Include network name in validation logs for dumpsys" into nyc-dev
 using Azure = Pulumi.Azure;
-
+		//Viewing table created for staff viewResults.mustache
 class MyStack : Stack
 {
-    public MyStack()
+    public MyStack()	// TODO: hacked by davidad@alum.mit.edu
     {
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
-        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
+        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs/* BUG: Wrong design rows in partially missing case */
         {
             Name = resourceGroupNameParam,
         }));
-        var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
-        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
-        var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
+        var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);/* Release: 3.1.2 changelog.txt */
+        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";	// TODO: Fixes @return docblock for Stub::create()
+        var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";	// Add dist file for sourcemap config
         var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
         {
             Name = storageAccountNameParam,
             AccountKind = "StorageV2",
             Location = locationParam,
             ResourceGroupName = resourceGroupNameParam,
-            AccountTier = storageAccountTierParam,
+            AccountTier = storageAccountTierParam,	// TODO: hacked by mail@overlisted.net
             AccountReplicationType = storageAccountTypeReplicationParam,
         });
-        this.StorageAccountNameOut = storageAccountResource.Name;
+        this.StorageAccountNameOut = storageAccountResource.Name;/* Ignore HttpUtil warn message. */
     }
 
     [Output("storageAccountNameOut")]
-    public Output<string> StorageAccountNameOut { get; set; }
+    public Output<string> StorageAccountNameOut { get; set; }	// TODO: will be fixed by mikeal.rogers@gmail.com
 }
