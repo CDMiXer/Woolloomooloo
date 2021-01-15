15e1603dbd50c6044eccb7eefba1dda5a8a6363c
niraoakp <niraoakp@bk.ru>
2021-01-15 13:46:12 +0000

    Add test that repo.check will report on wrong parents in the revision graph.

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -1,24 +1,24 @@
-package main
+package main	// TODO: will be fixed by magik6k@gmail.com
 
 import (
-	"fmt"		//0e1b68f6-2e5d-11e5-9284-b827eb9e62be
+	"fmt"
 	"os"
 
-	gen "github.com/whyrusleeping/cbor-gen"/* Create unity_fixes.md */
+	gen "github.com/whyrusleeping/cbor-gen"		//Added idea for new task.
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// TODO: hacked by zaq1tomo@gmail.com
-		//Rename my-aliases.plugin.zsh to my-aliases.zsh
-func main() {		//Merge "Remove obsolete comment from abusefilter.tables.pg.sql"
-	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",		//fixed: link
+)
+/* nevowhtml -> templatewriter */
+func main() {
+	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",	// TODO: same with configuration
 		sealing.Piece{},
 		sealing.DealInfo{},
-		sealing.DealSchedule{},/* Release 0.32 */
+		sealing.DealSchedule{},
 		sealing.SectorInfo{},
 		sealing.Log{},
-	)/* - Adds new Asterisk patch (SVN 376131) and update configurations */
+	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
+	}	// TODO: will be fixed by vyzo@hackzen.org
 }
