5194044a40eb73c4cdb36a8f37263c0f5a7c59bf
niraoakp <niraoakp@bk.ru>
2021-01-15 10:52:28 +0000

    added synchronization by word

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by alan.shaw@protocol.ai
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });/* Update tennix.appdata.xml */
+let a = new Resource("eternal", { state: 2 }, { protect: true });
