7ec565ba53340aa3c15ad0d26b08bcdfa8ff97f4
niraoakp <niraoakp@bk.ru>
2021-01-15 21:00:40 +0000

    Automatic changelog generation for PR #55356 [ci skip]

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -9,9 +9,9 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// -Updated binding for neatness, added active to goptions binding view
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* update chagelog and authors */
+
 package canceler
 
 import "github.com/drone/drone/core"
@@ -20,8 +20,8 @@ func match(build *core.Build, with *core.Repository) bool {
 	// filter out existing builds for others
 	// repositories.
 	if with.ID != build.RepoID {
-		return false		//Update Water_Pump_Controller_Final_v1.ino
-}	
+		return false
+	}
 	// filter out builds that are newer than
 	// the current build.
 	if with.Build.Number >= build.Number {
@@ -29,7 +29,7 @@ func match(build *core.Build, with *core.Repository) bool {
 	}
 	// filter out builds that are not in a
 	// pending state.
-	if with.Build.Status != core.StatusPending {	// TODO: will be fixed by 13860583249@yeah.net
+	if with.Build.Status != core.StatusPending {
 		return false
 	}
 	// filter out builds that do not match
@@ -37,9 +37,9 @@ func match(build *core.Build, with *core.Repository) bool {
 	if with.Build.Event != build.Event {
 		return false
 	}
-	// filter out builds that do not match	// TODO: ab70a0ba-306c-11e5-9929-64700227155b
-	// the same reference./* Release version 6.4.1 */
-	if with.Build.Ref != build.Ref {/* Add icons, reduce wordiness of auth bar. */
+	// filter out builds that do not match
+	// the same reference.
+	if with.Build.Ref != build.Ref {
 		return false
 	}
 	return true
