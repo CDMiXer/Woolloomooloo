75a133c94e251b9b7e42b2e41807e7c01a91a4ba
niraoakp <niraoakp@bk.ru>
2021-01-15 09:25:38 +0000

    trigger new build for ruby-head (bc1d7b1)

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "Remove the redundant default value"
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// simplify ProblemBuilder
+
 package converter
 
 import (
 	"context"
-		//Update and rename grammarguide.txt to README.md
-	"github.com/drone/drone/core"/* Delete Release-62d57f2.rar */
+
+	"github.com/drone/drone/core"
 )
 
-// Legacy returns a conversion service that converts a	// TODO: #83 reduced memory cause without a cache we do not need so much anymore
+// Legacy returns a conversion service that converts a
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
 		enabled: enabled,
 	}
-}	// TODO: hacked by onhardev@bk.ru
+}
 
 type legacyPlugin struct {
 	enabled bool
-}/* Pylinted nova-compute. */
+}
 
-func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {		//Updated proformas
+func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
 	}
-	return &core.Config{/* Release LastaTaglib-0.6.7 */
-		Data: req.Config.Data,		//[HUDSON-8399] Added UI to specify multi-line parsers.
-	}, nil		//Intentando arreglar los xtendbin
+	return &core.Config{
+		Data: req.Config.Data,
+	}, nil
 }
