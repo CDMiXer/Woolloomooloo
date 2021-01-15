7d79264fb976435df47937d7a9dca0ba5db43e22
niraoakp <niraoakp@bk.ru>
2021-01-15 18:55:29 +0000

    added connect, close and display database methodes

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
-
-import (	// Removed derpbadge.
+	// TODO: Fixing unescaped strings in readme
+import (
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Cria 'teste-video' */
+)
 
-func main() {/* Release version 1.0.0.M2 */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
-		return nil/* README update for supported platform */
-	})
+		return nil
+	})/* Bind all methods */
 }
