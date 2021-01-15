d0a6b5a6d3cd7998d7e0805f0a0f0caa11709f05
niraoakp <niraoakp@bk.ru>
2021-01-15 05:03:38 +0000

    Rename ACM-reference-format.bst to ACM-Reference-Format.bst

--- service/license/service.go
+++ service/license/service.go
@@ -6,8 +6,8 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Readme: fix link to built-in ESLint config file */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Bluetooth: Release locks before sleeping for L2CAP socket shutdown" */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,20 +17,20 @@ package license
 import (
 	"context"
 	"time"
-	// Detect renames in Git diff
+
 	"github.com/drone/drone/core"
 )
 
 // NewService returns a new License service.
 func NewService(
 	users core.UserStore,
-	repos core.RepositoryStore,	// TODO: Ray tracing demo: Removed "samples per frame" setting
+	repos core.RepositoryStore,
 	builds core.BuildStore,
-	license *core.License,	// TODO: 0784d9de-2e60-11e5-9284-b827eb9e62be
-) core.LicenseService {	// TODO: Updated the explanation of registering for OAuth 2/Data API v3 access.
+	license *core.License,
+) core.LicenseService {
 	return &service{
 		users:   users,
-		repos:   repos,/* BitsVal._convSign__val handle force_vector */
+		repos:   repos,
 		builds:  builds,
 		license: license,
 	}
@@ -40,7 +40,7 @@ type service struct {
 	users   core.UserStore
 	repos   core.RepositoryStore
 	builds  core.BuildStore
-	license *core.License		//Empty upmerge (of the 5.5.27 merge-back, via 5.6)
+	license *core.License
 }
 
 func (s *service) Exceeded(ctx context.Context) (bool, error) {
@@ -51,12 +51,12 @@ func (s *service) Exceeded(ctx context.Context) (bool, error) {
 		}
 	}
 	if limit := s.license.Users; limit > 0 {
-		count, _ := s.users.Count(ctx)		//Added an extra sendJson method that uses a provided Gson object.
+		count, _ := s.users.Count(ctx)
 		if count > limit {
 			return true, core.ErrUserLimit
 		}
 	}
-	if limit := s.license.Repos; limit > 0 {/* Fixed : LZ4_compress_limitedOutput() bug, as reported by Christopher Speller */
+	if limit := s.license.Repos; limit > 0 {
 		count, _ := s.repos.Count(ctx)
 		if count > limit {
 			return true, core.ErrRepoLimit
