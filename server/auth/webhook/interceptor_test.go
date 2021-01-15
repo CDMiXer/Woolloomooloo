8db8642972caf8eb59d6e0f92f1410a0f1df73b6
niraoakp <niraoakp@bk.ru>
2021-01-15 08:59:14 +0000

    changed generator to c

--- server/auth/webhook/interceptor_test.go
+++ server/auth/webhook/interceptor_test.go
@@ -1,24 +1,24 @@
 package webhook
-/* update ProRelease2 hardware */
+
 import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
-	"testing"		//fixed scenario based test
-/* add begining of user specified RULES */
-	"github.com/stretchr/testify/assert"/* 4f4c7f80-2e40-11e5-9284-b827eb9e62be */
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* Updated doc string for do_size */
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
 type testHTTPHandler struct{}
-/* README Release update #2 */
-func (t testHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by witek@enjin.io
+
+func (t testHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func TestInterceptor(t *testing.T) {
-	// we ignore these/* Release 1.1 */
+	// we ignore these
 	t.Run("WrongMethod", func(t *testing.T) {
 		r, _ := intercept("GET", "/api/v1/events/", nil)
 		assert.Empty(t, r.Header["Authorization"])
@@ -43,27 +43,27 @@ func TestInterceptor(t *testing.T) {
 		assert.Empty(t, r.Header["Authorization"])
 	})
 	// we accept these
-	t.Run("Bitbucket", func(t *testing.T) {/* Merge "Extend root device hints to support device name" */
+	t.Run("Bitbucket", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Event-Key": "repo:push",
-			"X-Hook-UUID": "sh!",	// TODO: hacked by nicksavers@gmail.com
+			"X-Hook-UUID": "sh!",
 		})
 		assert.Equal(t, []string{"Bearer my-bitbucket-token"}, r.Header["Authorization"])
 	})
-	t.Run("Bitbucketserver", func(t *testing.T) {/* Release for v9.1.0. */
+	t.Run("Bitbucketserver", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Event-Key":     "pr:modified",
-,"36953fc7f609649ecc8d2b6df7f022d6fa5093e627d7df9795d76dcd8beec6290000000" :"erutangiS-buH-X"			
+			"X-Hub-Signature": "0000000926ceeb8dcd67d5979fd7d726e3905af6d220f7fd6b2d8cce946906f7cf35963",
 		})
 		assert.Equal(t, []string{"Bearer my-bitbucketserver-token"}, r.Header["Authorization"])
 	})
 	t.Run("Github", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
-			"X-Github-Event":  "push",		//save Status Planned Outcomes and milestones many to many relationships
+			"X-Github-Event":  "push",
 			"X-Hub-Signature": "00000ba880174336fbe22d4723a67ba5c4c356ec9c696",
 		})
 		assert.Equal(t, []string{"Bearer my-github-token"}, r.Header["Authorization"])
-	})/* remove false positives from Esperanza/Kedsum */
+	})
 	t.Run("Gitlab", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Gitlab-Event": "Push Hook",
@@ -83,10 +83,10 @@ func intercept(method string, target string, headers map[string]string) (*http.R
 				"bitbucketserver": []byte("type: bitbucketserver\nsecret: sh!"),
 				"github":          []byte("type: github\nsecret: sh!"),
 				"gitlab":          []byte("type: gitlab\nsecret: sh!"),
-			},	// TODO: 2LgmNf3dg6kCr3KonxXfyhBeHYTZzxQr
+			},
 		},
 		// bitbucket
-		&corev1.ServiceAccount{/* Do not remove semtags from propernouns. */
+		&corev1.ServiceAccount{
 			ObjectMeta: metav1.ObjectMeta{Name: "bitbucket", Namespace: "my-ns"},
 			Secrets:    []corev1.ObjectReference{{Name: "bitbucket-token"}},
 		},
