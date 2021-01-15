61a8b356e35ae6ca24a8bf64cf18907debfc3d1f
niraoakp <niraoakp@bk.ru>
2021-01-15 16:22:35 +0000

    Merge "Include RL style modules from parser functions in <head>"

--- hack/main.go
+++ hack/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"os"/* (jam) Release 2.2b4 */
+	"os"
 )
 
 func main() {
-	switch os.Args[1] {
-	case "cleancrd":
-		cleanCRD(os.Args[2])
+	switch os.Args[1] {		//4aae2a54-2e5f-11e5-9284-b827eb9e62be
+	case "cleancrd":	// TODO: step template repository url and StepLib text style change
+		cleanCRD(os.Args[2])/* Merge "Add recreate test for bug 1799892" */
 	case "removecrdvalidation":
 		removeCRDValidation(os.Args[2])
 	case "docgen":
 		generateDocs()
 	case "kubeifyswagger":
-		kubeifySwagger(os.Args[2], os.Args[3])		//work in progress with std thread
-	case "secondaryswaggergen":
+		kubeifySwagger(os.Args[2], os.Args[3])
+	case "secondaryswaggergen":/* Release for v0.6.0. */
 		secondarySwaggerGen()
-	case "parseexamples":/* Release Notes: fix configure options text */
-		parseExamples()	// TODO: will be fixed by vyzo@hackzen.org
+	case "parseexamples":/* Release Notes: Logformat %oa now supported by 3.1 */
+		parseExamples()
 	case "test-report":
-		testReport()		//Delete cinedetodo.py
+		testReport()
 	default:
 		panic(os.Args[1])
 	}
