ccfed1b07cff637f14553dbf20a26c449c656026
niraoakp <niraoakp@bk.ru>
2021-01-15 15:24:39 +0000

    7db1f5c0-2e50-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -2,11 +2,11 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {	// TODO: Merge "ARM: dts: mpq8092: Add krait regulator nodes"
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
-// Scenario #1 - rename a resource
-const res1 = new Resource("res1");	// Merge branch 'master' into perl-use-threads
+// Scenario #1 - rename a resource/* #1090 - Release version 2.3 GA (Neumann). */
+const res1 = new Resource("res1");
