e7c967e87fd14c562faa2a3c5e657ad81efeb928
niraoakp <niraoakp@bk.ru>
2021-01-15 15:00:21 +0000

    Update flycheck.el

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,10 +1,10 @@
-import * as pulumi from "@pulumi/pulumi";		//fixed date, time, and timestamp mappings
+import * as pulumi from "@pulumi/pulumi";
 
 // Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
-// with secret configuration values, so long as these values are themselves strings.		//update the list
-class DynamicProvider extends pulumi.ProviderResource {
+// with secret configuration values, so long as these values are themselves strings.
+class DynamicProvider extends pulumi.ProviderResource {/* bring in lodash dependency */
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);/* Release 1.129 */
+        super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
     }
 }
 
