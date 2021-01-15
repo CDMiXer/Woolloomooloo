a32d2a50d54cb8be90a4b40e4d2f4c33e0001c27
niraoakp <niraoakp@bk.ru>
2021-01-15 16:59:16 +0000

    Fixed formatting in users guide. Updated credits with recent history.

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -2,23 +2,23 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Released springjdbcdao version 1.7.2 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Merge "Copy volume to image in multiple stores of glance"
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Typo in project name in pom */
 
 package branches
-
+/* Update home.spec.js */
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	// TODO: hacked by yuvalalaluf@gmail.com
+	"github.com/drone/drone/core"/* Update makebackup.sh */
+	"github.com/drone/drone/handler/api/render"/* Release 7.0.1 */
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -33,26 +33,26 @@ func HandleList(
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* Fix metadata information */
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		repo, err := repos.FindName(r.Context(), namespace, name)/* abaafa5c-2e6f-11e5-9284-b827eb9e62be */
+		if err != nil {		//haddockizing some comments from Make.hs
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace)./* b5ec2c68-2e4f-11e5-9284-b827eb9e62be */
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
 
-		results, err := builds.LatestBranches(r.Context(), repo.ID)
-		if err != nil {
+		results, err := builds.LatestBranches(r.Context(), repo.ID)	// Added Warning notes for third-party library
+		if err != nil {/* New Release 2.1.6 */
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r)./* last Glib::Dispatcher example before I munge it for BP */
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name).	// Anpassen der Texte in deutsch
 				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
