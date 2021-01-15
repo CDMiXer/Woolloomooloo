b05eaf8beddcddf95231e994d57f72ebd8bd3474
niraoakp <niraoakp@bk.ru>
2021-01-15 14:36:52 +0000

    Bump hugo version to v0.70.0

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//Upgraded constructionsite graphic
+import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Setup for the next test./* WeltargLine: Initialise all members in constructor. */
+// Setup for the next test.
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });/* Update sibyl.py */
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
