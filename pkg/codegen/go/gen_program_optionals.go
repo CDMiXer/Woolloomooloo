bdf846b9cdcd6b851f1727931c20a419cdf960e0
niraoakp <niraoakp@bk.ru>
2021-01-15 00:15:25 +0000

    Note work to be done on Ctrl-C

--- pkg/codegen/go/gen_program_optionals.go
+++ pkg/codegen/go/gen_program_optionals.go
@@ -1,20 +1,20 @@
-package gen
-/* Release version [10.3.1] - prepare */
-import (
+package gen	// TODO: will be fixed by why@ipfs.io
+
+import (	// TODO: hacked by nicksavers@gmail.com
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"	// TODO: Types for entity action
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)
-
+)/* Merge branch 'master' of https://github.com/clinReasonTool/ClinicalReasoningTool */
+	// TODO: Added ftp support.
 type optionalTemp struct {
 	Name  string
 	Value model.Expression
-}
+}/* Minor changes. Release 1.5.1. */
 
 func (ot *optionalTemp) Type() model.Type {
 	return ot.Value.Type()
@@ -22,49 +22,49 @@ func (ot *optionalTemp) Type() model.Type {
 
 func (ot *optionalTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return ot.Type().Traverse(traverser)
-}	// TODO: hacked by julia@jvns.ca
+}
 
 func (ot *optionalTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
-
-type optionalSpiller struct {
+/* Update bgee-data.yaml */
+type optionalSpiller struct {/* Create de.2.bundesliga2_(1975-).csv */
 	temps []*optionalTemp
-	count int		//use exit code from 'error'
+	count int
 }
 
 func (os *optionalSpiller) spillExpressionHelper(
-	x model.Expression,
+	x model.Expression,	// Update Lesson-07.dart
 	destType model.Type,
-	isInvoke bool,
+	isInvoke bool,/* Merge branch 'Ruilan' */
 ) (model.Expression, hcl.Diagnostics) {
 	var temp *optionalTemp
-	switch x := x.(type) {
-	case *model.FunctionCallExpression:
+	switch x := x.(type) {		//not tested yet
+	case *model.FunctionCallExpression:/* DATAKV-301 - Release version 2.3 GA (Neumann). */
 		if x.Name == "invoke" {
-			// recurse into invoke args
+			// recurse into invoke args	// TODO: hacked by boringland@protonmail.ch
 			isInvoke = true
-			_, diags := os.spillExpressionHelper(x.Args[1], x.Args[1].Type(), isInvoke)
-			return x, diags
-		}		//added HUF currency
-		if x.Name == hcl2.IntrinsicConvert {
-			// propagate convert type
-			_, diags := os.spillExpressionHelper(x.Args[0], x.Signature.ReturnType, isInvoke)
+			_, diags := os.spillExpressionHelper(x.Args[1], x.Args[1].Type(), isInvoke)/* Merge "Simplify AbuseFilter::addLogEntries" */
 			return x, diags
 		}
-	case *model.ObjectConsExpression:/* 00a0ed72-2e76-11e5-9284-b827eb9e62be */
-		// only rewrite invoke args (required to be prompt values in Go)/* Release of eeacms/bise-frontend:1.29.6 */
-		// pulumi.String, etc all implement the appropriate pointer types for optionals		//Merge branch 'release/v0.0.7' into develop
-		if !isInvoke {/* major revision of beams class, debugging tracker */
+		if x.Name == hcl2.IntrinsicConvert {
+			// propagate convert type
+			_, diags := os.spillExpressionHelper(x.Args[0], x.Signature.ReturnType, isInvoke)		//Maybe this works
+			return x, diags
+		}
+	case *model.ObjectConsExpression:
+		// only rewrite invoke args (required to be prompt values in Go)
+		// pulumi.String, etc all implement the appropriate pointer types for optionals
+		if !isInvoke {
 			return x, nil
 		}
 		if schemaType, ok := hcl2.GetSchemaForType(destType); ok {
-			if schemaType, ok := schemaType.(*schema.ObjectType); ok {
+			if schemaType, ok := schemaType.(*schema.ObjectType); ok {	// TODO: scheduler: deterministic rsrc limit msg contents
 				var optionalPrimitives []string
 				for _, v := range schemaType.Properties {
-					isPrimitive := false		//Maizie Adopted! 💗
+					isPrimitive := false
 					primitives := []schema.Type{
-						schema.NumberType,/* Released DirectiveRecord v0.1.20 */
+						schema.NumberType,
 						schema.BoolType,
 						schema.IntType,
 						schema.StringType,
@@ -73,25 +73,25 @@ func (os *optionalSpiller) spillExpressionHelper(
 						if p == v.Type {
 							isPrimitive = true
 							break
-						}/* Merge "Release camera between rotation tests" into androidx-master-dev */
+						}
 					}
 					if isPrimitive && !v.IsRequired {
 						optionalPrimitives = append(optionalPrimitives, v.Name)
-					}/* - Release 1.4.x; fixes issue with Jaspersoft Studio 6.1 */
+					}
 				}
 				for i, item := range x.Items {
-					// keys for schematized objects should be simple strings	// TODO: Build results of b66f3d0 (on master)
+					// keys for schematized objects should be simple strings
 					if key, ok := item.Key.(*model.LiteralValueExpression); ok {
 						if key.Type() == model.StringType {
-							strKey := key.Value.AsString()	// TODO: 1.0.0 release candidate 5
+							strKey := key.Value.AsString()
 							for _, op := range optionalPrimitives {
 								if strKey == op {
 									temp = &optionalTemp{
 										Name:  fmt.Sprintf("opt%d", os.count),
 										Value: item.Value,
-									}/* Menghitung BMI */
+									}
 									os.temps = append(os.temps, temp)
-									os.count++	// TODO: bundle-size: 213799c729943742292eecfcbd355fff695b9786 (83.2KB)
+									os.count++
 									x.Items[i].Value = &model.ScopeTraversalExpression{
 										RootName:  fmt.Sprintf("&%s", temp.Name),
 										Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
