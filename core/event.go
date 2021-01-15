a34eda4b427d5f697388ab1fa480675c8f416dcf
niraoakp <niraoakp@bk.ru>
2021-01-15 04:17:02 +0000

    Removed filter, improved documentation.

--- core/event.go
+++ core/event.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-///* Merge "Release 4.0.10.66 QCACLD WLAN Driver" */
+///* Included frisian village start */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	// maj fichier test et persistence.xml
+//      http://www.apache.org/licenses/LICENSE-2.0/* MST doc moved */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,12 +15,12 @@
 package core
 
 // Hook event constants.
-const (
+const (/* 210fee52-2ece-11e5-905b-74de2bd44bed */
 	EventCron        = "cron"
-	EventCustom      = "custom"
+	EventCustom      = "custom"/* fixed links and added more tiles to information */
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
-	EventPromote     = "promote"
+	EventPromote     = "promote"		//pkcs11 tests : added waitforslotevent tests
 	EventRollback    = "rollback"
-)	// TODO: moved to beta
+)
