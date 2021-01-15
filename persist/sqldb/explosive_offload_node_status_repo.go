474ecbe4025f2355d240355900f0fe07394fc476
niraoakp <niraoakp@bk.ru>
2021-01-15 22:06:16 +0000

    Update README.md with ETT/Figo/Cincinnati

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,35 +1,35 @@
-package sqldb	// Make password hash url-safe
-/* Added tests for absoluteBearing. */
+package sqldb		//Added note about deadlock
+
 import (
 	"fmt"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+)/* Delete model_generator/point_dispersion/text.csv */
 
 var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
-var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
+var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")	// TextComponentValue is now replaced by DocumentTextProperty.
 
 type explosiveOffloadNodeStatusRepo struct {
 }
 
-func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
+func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {	// TODO: will be fixed by martin2cai@hotmail.com
 	return false
-}/* Remove unnecessary quotes from the config file */
+}	// TODO: hacked by joshua@yottadb.com
 
 func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
-	return "", OffloadNotSupportedError
+	return "", OffloadNotSupportedError	// TODO: will be fixed by timnugent@gmail.com
 }
 
 func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
-}
-/* Release 1.0.0 version */
-func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
+}/* fixed a couple triggerEvent calls that were passing target as first param */
+
+func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {	// Release 0.64
 	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
-	return OffloadNotSupportedError/* Test if optics can be repeated */
+func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {/* 47dde8f6-2e59-11e5-9284-b827eb9e62be */
+	return OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
