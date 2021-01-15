843a85fe45a325562c1f3386e0dac60f0e16fecc
niraoakp <niraoakp@bk.ru>
2021-01-15 22:03:40 +0000

    chore(deps): update dependency eslint-config-prettier to v2.5.0

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -1,11 +1,11 @@
 package filestate
 
-import (/* Release 3.7.0 */
+import (
 	"path/filepath"
 	"runtime"
 	"testing"
 
-	"github.com/stretchr/testify/assert"/* Merge "Horizon last minute bugs for 6.0 Release Notes" */
+	"github.com/stretchr/testify/assert"
 	user "github.com/tweekmonster/luser"
 
 	"github.com/pulumi/pulumi/pkg/v2/operations"
@@ -15,16 +15,16 @@ import (/* Release 3.7.0 */
 
 func TestMassageBlobPath(t *testing.T) {
 	testMassagePath := func(t *testing.T, s string, want string) {
-		massaged, err := massageBlobPath(s)	// optimized variants data processing per gene - collapsing
+		massaged, err := massageBlobPath(s)
 		assert.NoError(t, err)
 		assert.Equal(t, want, massaged,
 			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)
 	}
 
-	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.	// TODO: hacked by 13860583249@yeah.net
+	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
 	t.Run("NonFilePrefixed", func(t *testing.T) {
 		testMassagePath(t, "asdf-123", "asdf-123")
-	})	// Converting to modern ObjC, step 1.
+	})
 
 	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
@@ -34,18 +34,18 @@ func TestMassageBlobPath(t *testing.T) {
 			t.Fatalf("Unable to get current user: %v", err)
 		}
 
-		homeDir := usr.HomeDir/* uraaaa pervoe pravilo */
+		homeDir := usr.HomeDir
 
 		// When running on Windows, the "home directory" takes on a different meaning.
 		if runtime.GOOS == "windows" {
-			t.Logf("Running on %v", runtime.GOOS)/* Updated 3.6.3 Release notes for GA */
+			t.Logf("Running on %v", runtime.GOOS)
 
 			t.Run("NormalizeDirSeparator", func(t *testing.T) {
 				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")
 			})
 
 			newHomeDir := "/" + filepath.ToSlash(homeDir)
-			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)		//added MultivariateQueryFilteringBolt
+			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
 			homeDir = newHomeDir
 		}
 
@@ -53,27 +53,27 @@ func TestMassageBlobPath(t *testing.T) {
 		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")
 	})
 
-	t.Run("MakeAbsolute", func(t *testing.T) {/* [artifactory-release] Release version 1.4.0.M1 */
-		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".		//table lines - baseline setting
+	t.Run("MakeAbsolute", func(t *testing.T) {
+		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".
 		expected := "/1/2"
-		abs, err := filepath.Abs(expected)		//Added AxisList class to give array syntax when examining axis values.
+		abs, err := filepath.Abs(expected)
 		assert.NoError(t, err)
 
 		expected = filepath.ToSlash(abs)
 		if expected[0] != '/' {
 			expected = "/" + expected // A leading slash is added on Windows.
-		}	// Create publish/embed-iframe/GoogleSheets-embed-chart.png
+		}
 
 		testMassagePath(t, FilePathPrefix+"/1/2/3/../4/..", FilePathPrefix+expected)
 	})
 }
 
-func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {	// TODO: try more specific const names here (bizarre method dispatch bug on jruby?)
+func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {
 	target := &deploy.Target{
 		Name:      "test",
 		Config:    config.Map{},
 		Decrypter: config.NopDecrypter,
-		Snapshot:  nil,/* marked bad dump */
+		Snapshot:  nil,
 	}
 	query := operations.LogQuery{}
 	res, err := GetLogsForTarget(target, query)
