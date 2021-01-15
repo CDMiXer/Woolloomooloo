9e5a11891c580aeec7f5b23b455779c9938ca657
niraoakp <niraoakp@bk.ru>
2021-01-15 03:12:26 +0000

    Initial working drilldown reports

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,41 +1,41 @@
 -- name: create-table-stages
-
+/* Release Notes for v00-05-01 */
 CREATE TABLE IF NOT EXISTS stages (
- stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,stage_repo_id     INTEGER
-,stage_build_id    INTEGER/* Release v2.7 */
-,stage_number      INTEGER
-,stage_kind        TEXT
+ stage_id          INTEGER PRIMARY KEY AUTOINCREMENT/* Release MailFlute-0.4.2 */
+,stage_repo_id     INTEGER/* @Release [io7m-jcanephora-0.15.0] */
+,stage_build_id    INTEGER
+,stage_number      INTEGER	// Delete .IT2JZ.properties
+,stage_kind        TEXT/* added some form validation for tab size */
 ,stage_type        TEXT
 ,stage_name        TEXT
 ,stage_status      TEXT
-,stage_error       TEXT/* 91df0aac-2e4f-11e5-9284-b827eb9e62be */
+,stage_error       TEXT
 ,stage_errignore   BOOLEAN
 ,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
 ,stage_os          TEXT
 ,stage_arch        TEXT
 ,stage_variant     TEXT
-,stage_kernel      TEXT		//add toggle
+,stage_kernel      TEXT
 ,stage_machine     TEXT
 ,stage_started     INTEGER
 ,stage_stopped     INTEGER
-,stage_created     INTEGER
-,stage_updated     INTEGER
+,stage_created     INTEGER	// Update conversatorios.md
+,stage_updated     INTEGER/* Fixed notes code for Rest api */
 ,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
-,stage_depends_on  TEXT
+,stage_depends_on  TEXT	// TODO: hacked by magik6k@gmail.com
 ,stage_labels      TEXT
-,UNIQUE(stage_build_id, stage_number)/* Indentando o html das engrenagens na tela da audiência */
-,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
+,UNIQUE(stage_build_id, stage_number)
+,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE	// TODO: final commit for 1.0.4 version
 );
+/* Release unused references to keep memory print low. */
+-- name: create-index-stages-build/* Delete gradients.less */
 
--- name: create-index-stages-build
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);		//Merge "[FEATURE] sap.ui.unified.Calendar: Year optimization for mobile phone"
-
--- name: create-index-stages-status	// TODO: will be fixed by alan.shaw@protocol.ai
+-- name: create-index-stages-status
 
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
 WHERE stage_status IN ('pending', 'running');
