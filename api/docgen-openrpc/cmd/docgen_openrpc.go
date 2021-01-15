bd76c90028cb85115e11b0224053c4f2465e09a8
niraoakp <niraoakp@bk.ru>
2021-01-15 10:24:05 +0000

    use context as this, instead of script args

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,55 +1,55 @@
-package main
+package main/* Update 5_responsecodes.markdown */
 
 import (
-	"compress/gzip"		//Merge "Fix uses of all-subdir-makefiles"
-	"encoding/json"	// Fix conformance tests to use new package
-	"io"/* Release of eeacms/www:20.2.13 */
-	"log"		//All done except documentation and tests
+	"compress/gzip"
+	"encoding/json"/* Release 3.5.0 */
+	"io"	// Bottom action does not works correctly (Bug #119)
+	"log"
 	"os"
-/* Merge "Release 3.2.3.261 Prima WLAN Driver" */
-	"github.com/filecoin-project/lotus/api/docgen"
 
+	"github.com/filecoin-project/lotus/api/docgen"/* v0.2.4 Release information */
+/* devops-edit --pipeline=maven/CanaryReleaseAndStage/Jenkinsfile */
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
-/* Use a somewhat more interesting mirror */
-/*
+/* Add a new 31x31 Alfaerie Western Piece Set. */
+/*	// TODO: NetKAN generated mods - Kethane-0.10.2
 main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
-	// Added convolution method
+/* Zeile 187: Frankfurt am Main */
 If the first argument is "miner", the document will describe the StorageMiner API.
-If not (no, or any other args), the document will describe the Full API.		//Update Affero_GPL.svg
-		//FROM dockerfile/nodejs -> FROM node
-Use:
+If not (no, or any other args), the document will describe the Full API.
 
-		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
+Use:	// TODO: Aggiunto controllo sul publish_code in entrata da chi trasmette.
+		//3.46 begins
+		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]/* Merge branch 'feature--polymer2-migration' into update-for-piwik-element */
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
-/* Earlier injection of servicelocator in transferagent */
+
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
-*/	// must be in ignore, but dont want to google now how it works.
+*/
 
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+	// TODO: 57e63210-2e6a-11e5-9284-b827eb9e62be
+	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
-	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)/* Release Notes: update squid.conf directive status */
-
-	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])		//52929be4-2e76-11e5-9284-b827eb9e62be
-	doc.RegisterReceiverName("Filecoin", i)
+	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
+	doc.RegisterReceiverName("Filecoin", i)/* Release for 3.6.0 */
 
 	out, err := doc.Discover()
-	if err != nil {
+	if err != nil {/* Prepare for Release 0.5.4 */
 		log.Fatalln(err)
 	}
 
-	var jsonOut []byte	// Version file update to v3
+	var jsonOut []byte
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
-	// Could use flags package to handle this more cleanly, but that requires changes elsewhere	// Update, sized aliases
+	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
-	if len(os.Args) > 5 && os.Args[5] == "-gzip" {/* pelisplus: fix */
+	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
 		jsonOut, err = json.Marshal(out)
 		if err != nil {
 			log.Fatalln(err)
@@ -59,9 +59,9 @@ func main() {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
 			log.Fatalln(err)
-		}
+		}/* CleanupWorklistBot - Release all db stuff */
 		writer = os.Stdout
-	}
+	}/*  - Release all adapter IP addresses when using /release */
 
 	_, err = writer.Write(jsonOut)
 	if err != nil {
