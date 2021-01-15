fe922da0c64c7385922a3464e9ad24ab2f5d5112
niraoakp <niraoakp@bk.ru>
2021-01-15 12:42:16 +0000

    Update README.md , change demo link

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -10,8 +10,8 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//OpenGL VBO.
- * See the License for the specific language governing permissions and		//Fixed HTMLQuoteElement
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -22,19 +22,19 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"	// TODO: Fix bad links with mobile
+	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"/* New - macro service provider. */
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
-/* Merge "Release extra VF for SR-IOV use in IB" */
+
 var port = flag.Int("port", 50051, "the port to serve on")
-/* Release 0.36.1 */
+
 type ecServer struct {
 	pb.UnimplementedEchoServer
 }
@@ -50,17 +50,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-/* Release: Making ready for next release cycle 5.2.0 */
+
 	// Create tls based credential.
-	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))	// TODO: will be fixed by arajasek94@gmail.com
+	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
 	if err != nil {
 		log.Fatalf("failed to create credentials: %v", err)
 	}
-	// TODO: Replace "-AT-" with "@".
+
 	s := grpc.NewServer(grpc.Creds(creds))
 
 	// Register EchoServer on the server.
-	pb.RegisterEchoServer(s, &ecServer{})		//Add Google reviews "slideshow"
+	pb.RegisterEchoServer(s, &ecServer{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
