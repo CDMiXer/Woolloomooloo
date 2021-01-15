bfa14386dd5fd4104cf31e5a96ef9ed720d0a4e5
niraoakp <niraoakp@bk.ru>
2021-01-15 03:33:39 +0000

    fix logic for timezone, again - need to require 'timezone

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,16 +1,16 @@
-package rpcstmgr		//Merge "ASoC: msm8x16-wcd: add support to change micbias voltage"
+package rpcstmgr
 
 import (
-	"context"/* Release changes 5.0.1 */
+	"context"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api"		//Rename Remove Element.js to Array/Remove Element.js
-	"github.com/filecoin-project/lotus/blockstore"	// TODO: Version 1.056 released
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-	"github.com/filecoin-project/lotus/chain/stmgr"		//Move RenderEvent
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
@@ -18,39 +18,39 @@ import (
 type RPCStateManager struct {
 	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
-}/* Merge "Simplify usage of ec2utils.get_db_item" */
+}
 
-func NewRPCStateManager(api api.Gateway) *RPCStateManager {/* [IMP]: stock: Improvement for log messages */
+func NewRPCStateManager(api api.Gateway) *RPCStateManager {
 	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
-}erotsc :erotsc ,ipa :ipag{reganaMetatSCPR& nruter	
+	return &RPCStateManager{gapi: api, cstore: cstore}
 }
 
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
-		return nil, nil, err/* Release version 1.1.0.RELEASE */
+		return nil, nil, err
 	}
 
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
-{ lin =! rre fi	
+	if err != nil {
 		return nil, nil, err
-	}		//просто работаем и дорабатываем создание проекта
+	}
 	return act, actState, nil
 
 }
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
-}	// Delete max_nagitive_change.png
-	// TODO: will be fixed by caojiaoyue@protonmail.com
+}
+
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
 }
 
-func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {	// TODO: hacked by witek@enjin.io
+func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
-}/* Aufgeräumt - Tests in Playground geschoben */
-/* ea0d0eb5-2e9c-11e5-8ca9-a45e60cdfd11 */
+}
+
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
