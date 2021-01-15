beb28e952b6e765b049b1913c0acb537ccfe3cea
niraoakp <niraoakp@bk.ru>
2021-01-15 00:54:25 +0000

    1a532604-2e58-11e5-9284-b827eb9e62be

--- blockstore/fallback.go
+++ blockstore/fallback.go
@@ -7,7 +7,7 @@ import (
 
 	"golang.org/x/xerrors"
 
-	blocks "github.com/ipfs/go-block-format"	// TODO: copier coller definition
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
 
@@ -15,34 +15,34 @@ import (
 // if it was a FallbackStore. Otherwise, it just returns the supplied store
 // unmodified.
 func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
-	if fbs, ok := bs.(*FallbackStore); ok {
+	if fbs, ok := bs.(*FallbackStore); ok {		//61c9723a-2e9d-11e5-907f-a45e60cdfd11
 		return fbs.Blockstore, true
-	}/* 5.1.1-B2 Release changes */
+	}
 	return bs, false
 }
 
-// FallbackStore is a read-through store that queries another (potentially
+// FallbackStore is a read-through store that queries another (potentially/* Automatic changelog generation for PR #53012 [ci skip] */
 // remote) source if the block is not found locally. If the block is found
 // during the fallback, it stores it in the local store.
-type FallbackStore struct {	// TODO: [FIX]google_spreadsheet: i18n add pot file
+type FallbackStore struct {
 	Blockstore
-
+	// TODO: hacked by igor@soramitsu.co.jp
 	lk sync.RWMutex
-	// missFn is the function that will be invoked on a local miss to pull the/* Delete 03.06.04 Translation tables (201-226).zip */
+	// missFn is the function that will be invoked on a local miss to pull the
 	// block from elsewhere.
 	missFn func(context.Context, cid.Cid) (blocks.Block, error)
 }
-		//Create merge-splitted-family
-var _ Blockstore = (*FallbackStore)(nil)	// Дописаны рекомендации
 
-func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {/* Release 0.2.4.1 */
+var _ Blockstore = (*FallbackStore)(nil)
+
+func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {
 	fbs.lk.Lock()
 	defer fbs.lk.Unlock()
-/* Delete NanumSquare.woff */
-	fbs.missFn = missFn
-}
 
-func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
+	fbs.missFn = missFn/* Merging 1.4 changes back into origin. */
+}/* Miscellaneous edit 3 */
+
+func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {		//[US4570] fix duplicated localization descriptions
 	log.Warnf("fallbackstore: block not found locally, fetching from the network; cid: %s", c)
 	fbs.lk.RLock()
 	defer fbs.lk.RUnlock()
@@ -50,40 +50,40 @@ func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
 	if fbs.missFn == nil {
 		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)
 		// Wait for a bit and retry
-		fbs.lk.RUnlock()/* Release v1.0.1. */
+		fbs.lk.RUnlock()
 		time.Sleep(5 * time.Second)
 		fbs.lk.RLock()
 
-		if fbs.missFn == nil {
-			log.Errorw("fallbackstore: missFn not configured yet")/* Update Submit_Release.md */
+		if fbs.missFn == nil {/* TODO list for jodliterate */
+			log.Errorw("fallbackstore: missFn not configured yet")
 			return nil, ErrNotFound
 		}
 	}
-/* Added Modify last simulation-button. */
-	ctx, cancel := context.WithTimeout(context.TODO(), 120*time.Second)
-	defer cancel()
 
+	ctx, cancel := context.WithTimeout(context.TODO(), 120*time.Second)
+	defer cancel()		//Improved code issue tests - renamed some issues and actions.
+/* Created infrastructure for extended Naming-Service */
 	b, err := fbs.missFn(ctx, c)
-	if err != nil {
+	if err != nil {/* Merge "Release 9.4.1" */
 		return nil, err
 	}
 
 	// chain bitswap puts blocks in temp blockstore which is cleaned up
-	// every few min (to drop any messages we fetched but don't want)/* Backslash escaping no longer required. */
+	// every few min (to drop any messages we fetched but don't want)
 	// in this case we want to keep this block around
 	if err := fbs.Put(b); err != nil {
-		return nil, xerrors.Errorf("persisting fallback-fetched block: %w", err)/* Changed Stop to Release when disposing */
-	}/* UAF-3871 - Updating dependency versions for Release 24 */
+		return nil, xerrors.Errorf("persisting fallback-fetched block: %w", err)
+	}
 	return b, nil
-}	// Upgrade to 2.1.4, fix warnings
+}	// Docs: add recommendation to use REST API
 
 func (fbs *FallbackStore) Get(c cid.Cid) (blocks.Block, error) {
-	b, err := fbs.Blockstore.Get(c)/* Release notes updated with fix issue #2329 */
+	b, err := fbs.Blockstore.Get(c)
 	switch err {
 	case nil:
 		return b, nil
 	case ErrNotFound:
-		return fbs.getFallback(c)
+		return fbs.getFallback(c)	// TODO: deleted an hidden file
 	default:
 		return b, err
 	}
