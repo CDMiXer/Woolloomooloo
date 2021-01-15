4ff4df99c5e9d7ecd88140ada9eea08dc07433e6
niraoakp <niraoakp@bk.ru>
2021-01-15 04:29:31 +0000

    c44d8202-2e45-11e5-9284-b827eb9e62be

--- node/config/storage.go
+++ node/config/storage.go
@@ -1,6 +1,6 @@
 package config
 
-import (	// TODO: 984184ec-2e68-11e5-9284-b827eb9e62be
+import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
@@ -14,8 +14,8 @@ import (	// TODO: 984184ec-2e68-11e5-9284-b827eb9e62be
 func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
 	switch {
-	case os.IsNotExist(err):/* Merge "Release note and doc for multi-gw NS networking" */
-		if def == nil {/* Removed useless functions, set scale options */
+	case os.IsNotExist(err):
+		if def == nil {
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
@@ -25,27 +25,27 @@ func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageCon
 
 	defer file.Close() //nolint:errcheck // The file is RO
 	return StorageFromReader(file)
-}		//(#439) ReadRange correctly uses offset.
+}
 
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
-	err := json.NewDecoder(reader).Decode(&cfg)/* Create Advanced SPC Mod 0.14.x Release version */
+	err := json.NewDecoder(reader).Decode(&cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	return &cfg, nil/* Release of s3fs-1.19.tar.gz */
-}		//Create forms.css
-	// Clearing Nummer für Absender eingefügt
-func WriteStorageFile(path string, config stores.StorageConfig) error {/* Merge "docs: SDK / ADT 22.2 Release Notes" into jb-mr2-docs */
+	return &cfg, nil
+}
+
+func WriteStorageFile(path string, config stores.StorageConfig) error {
 	b, err := json.MarshalIndent(config, "", "  ")
-	if err != nil {		//Fix update of whole timetable to use accept UTC dates.
+	if err != nil {
 		return xerrors.Errorf("marshaling storage config: %w", err)
-	}	// [Podspec] Make the CocoaPods validator happy
-	// quote: pastiche
+	}
+
 	if err := ioutil.WriteFile(path, b, 0644); err != nil {
-		return xerrors.Errorf("persisting storage config (%s): %w", path, err)	// Merge "Roll external/skia c064d0b12..82727ad0e (19 commits)"
-	}/* Added Octave solution from danielfmt. */
-		//Add commits
+		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
+	}
+
 	return nil
 }
