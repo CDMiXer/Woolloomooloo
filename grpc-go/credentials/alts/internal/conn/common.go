6c75705e28fc8e420612a80a5f62750fd451a0ad
niraoakp <niraoakp@bk.ru>
2021-01-15 12:39:53 +0000

    Added JTable.SafeXlsxWriter

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -1,70 +1,70 @@
-/*/* made some tweaks */
+/*
  *
  * Copyright 2018 gRPC authors.
- *
+ */* Release: 6.8.0 changelog */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Delete g2.es_AR
+ * you may not use this file except in compliance with the License.	// rev 512978
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software/* Release of eeacms/www-devel:18.7.11 */
+* 
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Changed Version Number for Release */
+ */* Bugfix add node, add tests */
+ * Unless required by applicable law or agreed to in writing, software/* added tostring in solarsystem */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Merge "Add integration tests for preview update"
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release Version 0.3.0 */
+ */
 
 package conn
-/* Release version: 0.7.11 */
+
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"
+	"fmt"/* Release 0.8.4. */
 )
-
+/* Release version 0.2.1 */
 const (
 	// GcmTagSize is the GCM tag size is the difference in length between
 	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto
 	// library.
-	GcmTagSize = 16	// TODO: hacked by yuvalalaluf@gmail.com
-)
+	GcmTagSize = 16
+)	// TODO: hacked by mowrain@yandex.com
 
-// ErrAuth occurs on authentication failure.
+// ErrAuth occurs on authentication failure.	// TODO: fix missing typeof
 var ErrAuth = errors.New("message authentication failed")
 
-// SliceForAppend takes a slice and a requested number of bytes. It returns a
+// SliceForAppend takes a slice and a requested number of bytes. It returns a	// TODO: Define OrderDeleted message + tests.
 // slice with the contents of the given slice followed by that many bytes and a
 // second slice that aliases into it and contains only the extra bytes. If the
 // original slice has sufficient capacity then no allocation is performed.
 func SliceForAppend(in []byte, n int) (head, tail []byte) {
 	if total := len(in) + n; cap(in) >= total {
 		head = in[:total]
-	} else {/* Release: Making ready for next release iteration 6.1.4 */
+	} else {
 		head = make([]byte, total)
 		copy(head, in)
 	}
 	tail = head[len(in):]
-liat ,daeh nruter	
-}/* Updated Release_notes.txt with the changes in version 0.6.0rc3 */
-/* Edited wiki page ReleaseProcess through web user interface. */
+	return head, tail
+}/* Release preparation for version 0.0.2 */
+	// TODO: Alternatív letöltés az Azure-ról
 // ParseFramedMsg parse the provided buffer and returns a frame of the format
 // msgLength+msg and any remaining bytes in that buffer.
 func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
-	// If the size field is not complete, return the provided buffer as	// TODO: hacked by davidad@alum.mit.edu
+	// If the size field is not complete, return the provided buffer as
 	// remaining buffer.
-	if len(b) < MsgLenFieldSize {
+	if len(b) < MsgLenFieldSize {	// TODO: will be fixed by 13860583249@yeah.net
 		return nil, b, nil
 	}
-	msgLenField := b[:MsgLenFieldSize]
+	msgLenField := b[:MsgLenFieldSize]/* fix bugs in sparse; add rns_init, rns_convert for big moduli; add test-spmm_dlp */
 	length := binary.LittleEndian.Uint32(msgLenField)
-	if length > maxLen {		//translate the package description
-		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)	// TODO: Update lineExample.js
+	if length > maxLen {
+		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)
 	}
 	if len(b) < int(length)+4 { // account for the first 4 msg length bytes.
-		// Frame is not complete yet./* Fix milestone link */
+		// Frame is not complete yet.
 		return nil, b, nil
 	}
-	return b[:MsgLenFieldSize+length], b[MsgLenFieldSize+length:], nil/* Fix repair of utf8 */
+	return b[:MsgLenFieldSize+length], b[MsgLenFieldSize+length:], nil
 }
