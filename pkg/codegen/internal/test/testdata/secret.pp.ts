6e0800ccdc78f891d5339fac793c9c244d297431
niraoakp <niraoakp@bk.ru>
2021-01-15 20:14:59 +0000

    srcp: enablecom support added

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-/* Release of eeacms/forests-frontend:2.0-beta.52 */
+
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
