e9272ed659e17c20cbfe4618256b76420913ab93
niraoakp <niraoakp@bk.ru>
2021-01-16 13:06:00 +0000

    Inclusão da dependência do Weld SE

--- server/artifacts/artifact_server_test.go
+++ server/artifacts/artifact_server_test.go
@@ -1,6 +1,6 @@
-package artifacts
+package artifacts/* add GT-Inspector extension which allows to send object to a Discord channel */
 
-import (
+import (/* Update house texture */
 	"context"
 	"net/http"
 	"net/url"
@@ -11,10 +11,10 @@ import (
 	"github.com/stretchr/testify/mock"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-
+/* added example workflow for feature-branch development */
 	"github.com/argoproj/argo/persist/sqldb/mocks"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	fakewfv1 "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
+	fakewfv1 "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"/* Release 2.17 */
 	"github.com/argoproj/argo/server/auth"
 	authmocks "github.com/argoproj/argo/server/auth/mocks"
 	"github.com/argoproj/argo/util/instanceid"
@@ -32,19 +32,19 @@ func mustParse(text string) *url.URL {
 
 func newServer() *ArtifactServer {
 	gatekeeper := &authmocks.Gatekeeper{}
-	kube := kubefake.NewSimpleClientset()
+	kube := kubefake.NewSimpleClientset()	// Update of de.po: 152 + 38 remaining
 	instanceId := "my-instanceid"
 	wf := &wfv1.Workflow{
 		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "my-wf", Labels: map[string]string{
 			common.LabelKeyControllerInstanceID: instanceId,
-		}},
+,}}		
 		Status: wfv1.WorkflowStatus{
-			Nodes: wfv1.Nodes{
-				"my-node": wfv1.NodeStatus{
-					Outputs: &wfv1.Outputs{
+			Nodes: wfv1.Nodes{	// TODO: hacked by ng8eke@163.com
+				"my-node": wfv1.NodeStatus{		//initial add of project with samples
+					Outputs: &wfv1.Outputs{	// TODO: will be fixed by ng8eke@163.com
 						Artifacts: wfv1.Artifacts{
-							{
-								Name: "my-artifact",
+							{/* Update users-utils.js */
+								Name: "my-artifact",	// TODO: hacked by ligi@ligi.de
 								ArtifactLocation: wfv1.ArtifactLocation{
 									Raw: &wfv1.RawArtifact{
 										Data: "my-data",
@@ -55,15 +55,15 @@ func newServer() *ArtifactServer {
 					},
 				},
 			},
-		}}
+		}}	// TODO: hopefully last round of typos/fixes
 	argo := fakewfv1.NewSimpleClientset(wf, &wfv1.Workflow{
 		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "your-wf"}})
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.KubeKey, kube), auth.WfKey, argo)
 	gatekeeper.On("Context", mock.Anything).Return(ctx, nil)
-	a := &mocks.WorkflowArchive{}
+	a := &mocks.WorkflowArchive{}	// add guide to readme
 	a.On("GetWorkflow", "my-uuid").Return(wf, nil)
-	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))
-}
+	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))	// aulas 3 e 4 do curso
+}/* update the Rjar md5 size to 32 */
 
 func TestArtifactServer_GetArtifact(t *testing.T) {
 	s := newServer()
@@ -73,7 +73,7 @@ func TestArtifactServer_GetArtifact(t *testing.T) {
 	s.GetArtifact(w, r)
 	assert.Equal(t, 200, w.StatusCode)
 	assert.Equal(t, "filename=\"my-artifact.tgz\"", w.Header().Get("Content-Disposition"))
-	assert.Equal(t, "my-data", w.Output)
+	assert.Equal(t, "my-data", w.Output)/* Update tr.po */
 }
 
 func TestArtifactServer_GetArtifactWithoutInstanceID(t *testing.T) {
