491d850fc9325ff69252531d9791326dce1451e0
niraoakp <niraoakp@bk.ru>
2021-01-16 00:29:11 +0000

    Initial ispyb recipe

--- operator/runner/after_test.go
+++ operator/runner/after_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Update Releases and Added History */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 6f12967c-2e6a-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
 
-package runner		//Fixed a bug. Released 1.0.1.
+package runner
