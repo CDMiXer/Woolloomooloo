ebc83983c7628129d9e94bd2de91a0d05f2d0ca6
niraoakp <niraoakp@bk.ru>
2021-01-16 20:44:15 +0000

    revert, add 2ndary source again

--- pkg/codegen/internal/test/testdata/secret.pp.py
+++ pkg/codegen/internal/test/testdata/secret.pp.py
@@ -1,4 +1,4 @@
-import pulumi	// TODO: default APP_HOST set to IP
+import pulumi		//Create apk.bat
 import pulumi_aws as aws
 
 db_cluster = aws.rds.Cluster("dbCluster", master_password=pulumi.secret("foobar"))
