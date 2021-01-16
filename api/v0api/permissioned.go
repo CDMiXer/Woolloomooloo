b101c5b8d3094037d4f2e815894515af48b49338
niraoakp <niraoakp@bk.ru>
2021-01-16 12:12:08 +0000

    Progress bar to use #56FF9B.

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
-package v0api
-
+package v0api/* Merge "Release 1.0.0.113 QCACLD WLAN Driver" */
+		//Create bot.txt
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// Added progress callback to httpconnection
 )
-	// add block macro
-func PermissionedFullAPI(a FullNode) FullNode {
+
+func PermissionedFullAPI(a FullNode) FullNode {		//Fixed Class name renaming
 	var out FullNodeStruct
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)/* Released springrestcleint version 2.4.9 */
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)	// TODO: will be fixed by mail@bitpshr.net
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)/* Released 0.3.0 */
 	return &out
 }
