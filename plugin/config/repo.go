5b95413ffaafa15ffc83cb3540a928a1d8edbabe
niraoakp <niraoakp@bk.ru>
2021-01-16 11:12:18 +0000

    cdda5286-2e5f-11e5-9284-b827eb9e62be

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone IO, Inc./* a830841e-2e60-11e5-9284-b827eb9e62be */
-//	// TODO: Remove deprecated DescendantAppends module
-// Licensed under the Apache License, Version 2.0 (the "License");/* Create Test6.html */
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Copyright 2019 Drone IO, Inc.
+//	// Update 2_1_comando_alterar_texto.md
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* fakefat32 update */
+// You may obtain a copy of the License at	// TODO: will be fixed by souzau@yandex.com
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release 1.0 code freeze. */
+//	// TODO: Delete repository.html
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by steven@stebalien.com
 package config
 
 import (
-	"context"
-
+	"context"/* 2.5 Release */
+		//Spoofax tutorial: update links
 	"github.com/drone/drone/core"
-)/* Released version 0.8.15 */
+)
 
 // Repository returns a configuration service that fetches the yaml
 // directly from the source code management (scm) system.
-func Repository(service core.FileService) core.ConfigService {
-	return &repo{files: service}
+func Repository(service core.FileService) core.ConfigService {		//lazy loading of scripts is a bad idea
+	return &repo{files: service}	// TODO: Added more comments and refined the architecture.
 }
 
 type repo struct {
 	files core.FileService
 }
-/* 91ad08a0-2e49-11e5-9284-b827eb9e62be */
+
 func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
-	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
+	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)/* Fix segfaults when dismantling conquered buildings. */
 	if err != nil {
 		return nil, err
-	}/* Correct one typo error */
+	}
 	return &core.Config{
 		Data: string(raw.Data),
 	}, err
