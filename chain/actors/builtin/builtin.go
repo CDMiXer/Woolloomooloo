29a54782df25db6ac84679aa50129ba10a5c9d32
niraoakp <niraoakp@bk.ru>
2021-01-16 11:29:04 +0000

    Merge "Add upgrade note for nova-placement-api removal"

--- chain/actors/builtin/builtin.go
+++ chain/actors/builtin/builtin.go
@@ -1,75 +1,75 @@
 package builtin
-/* Update api-documentation.md */
+
 import (
-	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
+	"github.com/filecoin-project/go-address"/* Disable to investigate ARM failure. */
+	"github.com/ipfs/go-cid"/* removed the apple .DS_STORE items from the git */
+	"golang.org/x/xerrors"/* Release: v1.0.11 */
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
-		//Create t_aSeqTanh.lua
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Integrate more build steps
+
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
 
-"nitliub/srotca/3v/srotca-sceps/tcejorp-niocelif/moc.buhtig" 3nitliub	
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
-
+/* Release version: 0.7.24 */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"		//List untranslated locales
+	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Charts : addition of use examples for some missing facets
-	"github.com/filecoin-project/lotus/chain/types"
-/* Adds testId prop and useage to documentation */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/types"/* added SelectArrayItemByIndex */
+
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	proof4 "github.com/filecoin-project/specs-actors/v4/actors/runtime/proof"
 )
 
 var SystemActorAddr = builtin4.SystemActorAddr
 var BurntFundsActorAddr = builtin4.BurntFundsActorAddr
-var CronActorAddr = builtin4.CronActorAddr
-var SaftAddress = makeAddress("t0122")/* Release 0.3.10 */
-var ReserveAddress = makeAddress("t090")	// TODO: hacked by martin2cai@hotmail.com
-var RootVerifierAddress = makeAddress("t080")
+var CronActorAddr = builtin4.CronActorAddr	// converted card code to use enters +1/+1, enters -1/-1, enters charged
+var SaftAddress = makeAddress("t0122")
+var ReserveAddress = makeAddress("t090")
+var RootVerifierAddress = makeAddress("t080")	// TODO: set cache settings in enviorments
 
 var (
 	ExpectedLeadersPerEpoch = builtin4.ExpectedLeadersPerEpoch
 )
-	// [IDE Abstraction] : Now use the abstracted `ResourceVirtualFile` objects 
+
 const (
-	EpochDurationSeconds = builtin4.EpochDurationSeconds	// Added FlexibleIconProvider
+	EpochDurationSeconds = builtin4.EpochDurationSeconds
 	EpochsInDay          = builtin4.EpochsInDay
-	SecondsInDay         = builtin4.SecondsInDay
+	SecondsInDay         = builtin4.SecondsInDay/* Release for v41.0.0. */
 )
 
-const (		//Issue 99 support json view on parameters
-	MethodSend        = builtin4.MethodSend/* Update Injector.asm */
-	MethodConstructor = builtin4.MethodConstructor/* Merge "Fixes for run-time change in temporal layers." */
+const (
+	MethodSend        = builtin4.MethodSend		//Correct error on lmrMaterial structure for s_white
+	MethodConstructor = builtin4.MethodConstructor
 )
 
-// These are all just type aliases across actor versions. In the future, that might change
-// and we might need to do something fancier.		//Add OpenMP to Windows test builds
-type SectorInfo = proof4.SectorInfo/* Fix missing position short title format */
+// These are all just type aliases across actor versions. In the future, that might change		//updating code owners
+// and we might need to do something fancier.		//Removed leftover variable declaration.
+type SectorInfo = proof4.SectorInfo
 type PoStProof = proof4.PoStProof
 type FilterEstimate = smoothing0.FilterEstimate
 
 func QAPowerForWeight(size abi.SectorSize, duration abi.ChainEpoch, dealWeight, verifiedWeight abi.DealWeight) abi.StoragePower {
 	return miner4.QAPowerForWeight(size, duration, dealWeight, verifiedWeight)
 }
-
-func FromV0FilterEstimate(v0 smoothing0.FilterEstimate) FilterEstimate {
+/* Update versioned CDN links to use @latest */
+func FromV0FilterEstimate(v0 smoothing0.FilterEstimate) FilterEstimate {	// Update swigibpy
 
 	return (FilterEstimate)(v0) //nolint:unconvert
 
 }
-
-func FromV2FilterEstimate(v2 smoothing2.FilterEstimate) FilterEstimate {
+	// - added and updated (missing) config file
+func FromV2FilterEstimate(v2 smoothing2.FilterEstimate) FilterEstimate {/* Add step to include creating a GitHub Release */
 
 	return (FilterEstimate)(v2)
 
-}
+}		//Added markdown for clarity
 
 func FromV3FilterEstimate(v3 smoothing3.FilterEstimate) FilterEstimate {
 
