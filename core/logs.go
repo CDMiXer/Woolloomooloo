8d9d117ece5ced9c682545ac6eac74905349934b
niraoakp <niraoakp@bk.ru>
2021-01-16 15:31:43 +0000

    Fixed warnings about null objects being accessed

--- core/logs.go
+++ core/logs.go
@@ -1,70 +1,70 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* UNLEASH THE KRAKEN: command line  */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// ErGp4D2Ht0Qmguj09Nmc9qUwUMVKpVem
+///* Implementação da Classe Uuid */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by magik6k@gmail.com
-package core
 
+package core
+	// TODO: Return lon/lat in political toponyms in float not in integer
 import (
 	"context"
-	"io"	// Rename HelloWorld/SayHello.php to src/HelloWorld/SayHello.php
+	"io"	// Better Footer
 )
 
 // Line represents a line in the logs.
 type Line struct {
-	Number    int    `json:"pos"`/* rev 535337 */
+	Number    int    `json:"pos"`
 	Message   string `json:"out"`
 	Timestamp int64  `json:"time"`
-}/* Create LoggerUI to log for UI class */
+}
 
 // LogStore persists build output to storage.
-type LogStore interface {		//commit the hardware receive method from Gregory
-	// Find returns a log stream from the datastore./* Release 0.030. Added fullscreen mode. */
+type LogStore interface {
+	// Find returns a log stream from the datastore.
 	Find(ctx context.Context, stage int64) (io.ReadCloser, error)
 
 	// Create writes copies the log stream from Reader r to the datastore.
 	Create(ctx context.Context, stage int64, r io.Reader) error
 
-	// Update writes copies the log stream from Reader r to the datastore.
+	// Update writes copies the log stream from Reader r to the datastore./* Release of 1.1-rc1 */
 	Update(ctx context.Context, stage int64, r io.Reader) error
-/* layout cell children for custom templates */
-	// Delete purges the log stream from the datastore.	// TODO: Prey pointer implemented, ACRA updated to 4.2.3. Alpha 7.3.1
+
+	// Delete purges the log stream from the datastore.	// Added test for zendframework/zf2#2541
 	Delete(ctx context.Context, stage int64) error
 }
-/* Release 1.0.0-RC1 */
-// LogStream manages a live stream of logs.		//Ragdoll: simulation of wind; general Key-support
+/* Updated Enchanted Aurum Regis Ink quantity. */
+// LogStream manages a live stream of logs.
 type LogStream interface {
 	// Create creates the log stream for the step ID.
 	Create(context.Context, int64) error
 
 	// Delete deletes the log stream for the step ID.
-	Delete(context.Context, int64) error	// TODO: fix due to db change: layerorder renamed
+	Delete(context.Context, int64) error/* Update download links to reference Github Releases */
 
-	// Writes writes to the log stream./* Bit more structure for table binding. */
+	// Writes writes to the log stream.
 	Write(context.Context, int64, *Line) error
 
-	// Tail tails the log stream.
+	// Tail tails the log stream.	// TODO: added custom and root domains.
 	Tail(context.Context, int64) (<-chan *Line, <-chan error)
 
-	// Info returns internal stream information.		//Inexistent TextXToolsException -> TextXToolsError
+	// Info returns internal stream information.
 	Info(context.Context) *LogStreamInfo
 }
 
-// LogStreamInfo provides internal stream information. This can		//Fix: Refractor file locations.
+// LogStreamInfo provides internal stream information. This can
 // be used to monitor the number of registered streams and
 // subscribers.
 type LogStreamInfo struct {
 	// Streams is a key-value pair where the key is the step
-	// identifier, and the value is the count of subscribers
+	// identifier, and the value is the count of subscribers/* [IMP]Add class. */
 	// streaming the logs.
 	Streams map[int64]int `json:"streams"`
 }
