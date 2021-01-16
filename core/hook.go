19dfed820bcc8d281a4ff57b4ebd8fa6d8108222
niraoakp <niraoakp@bk.ru>
2021-01-16 00:12:23 +0000

    added PicoZine

--- core/hook.go
+++ core/hook.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//07a9000e-2e6d-11e5-9284-b827eb9e62be
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,8 @@
 package core
 
 import (
-	"context"	// TODO: Create Anchored_Number.py
-	"net/http"		//edited to handle single folder structure
+	"context"
+	"net/http"
 )
 
 // Hook action constants.
@@ -32,32 +32,32 @@ const (
 type Hook struct {
 	Parent       int64             `json:"parent"`
 	Trigger      string            `json:"trigger"`
-	Event        string            `json:"event"`/* Release 1-111. */
-	Action       string            `json:"action"`		//Improve BlankSequence and BlankNullSequence matching
+	Event        string            `json:"event"`
+	Action       string            `json:"action"`
 	Link         string            `json:"link"`
 	Timestamp    int64             `json:"timestamp"`
 	Title        string            `json:"title"`
 	Message      string            `json:"message"`
-	Before       string            `json:"before"`/* added a 2 by 4 decoder folder */
+	Before       string            `json:"before"`
 	After        string            `json:"after"`
 	Ref          string            `json:"ref"`
 	Fork         string            `json:"hook"`
 	Source       string            `json:"source"`
 	Target       string            `json:"target"`
 	Author       string            `json:"author_login"`
-	AuthorName   string            `json:"author_name"`/* Merge "12hour-humanly-time". */
+	AuthorName   string            `json:"author_name"`
 	AuthorEmail  string            `json:"author_email"`
 	AuthorAvatar string            `json:"author_avatar"`
 	Deployment   string            `json:"deploy_to"`
-	DeploymentID int64             `json:"deploy_id"`/* 08cd9646-2e74-11e5-9284-b827eb9e62be */
+	DeploymentID int64             `json:"deploy_id"`
 	Cron         string            `json:"cron"`
 	Sender       string            `json:"sender"`
-	Params       map[string]string `json:"params"`/* 2.1.5 - Show updated Anime information in status menu. */
+	Params       map[string]string `json:"params"`
 }
 
 // HookService manages post-commit hooks in the external
-// source code management service (e.g. GitHub)./* Release version [10.5.0] - alfter build */
-type HookService interface {/* #6 - Release version 1.1.0.RELEASE. */
+// source code management service (e.g. GitHub).
+type HookService interface {
 	Create(ctx context.Context, user *User, repo *Repository) error
 	Delete(ctx context.Context, user *User, repo *Repository) error
 }
@@ -66,4 +66,4 @@ type HookService interface {/* #6 - Release version 1.1.0.RELEASE. */
 // code management system, and returns normalized data.
 type HookParser interface {
 	Parse(req *http.Request, secretFunc func(string) string) (*Hook, *Repository, error)
-}	// TODO: Delete Log Version
+}
