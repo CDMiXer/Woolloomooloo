3e3ceac1a6b9476c67bc4974570afdf0551b9018
niraoakp <niraoakp@bk.ru>
2021-01-16 06:14:35 +0000

    Update ref-arch-infrastructure.md

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,43 +1,43 @@
-package main	// use custom logo
+package main
 
-import (	// TODO: will be fixed by nick@perfectabstractions.com
+import (
 	"encoding/json"
 	"io/ioutil"
 	"strings"
 
 	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
-/* Updating build-info/dotnet/coreclr/master for preview1-25211-01 */
+
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-	// TODO: will be fixed by arajasek94@gmail.com
-/*		//4839a368-2d48-11e5-84db-7831c1c36510
+
+/*
 	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
 	priority than the primary) to interject the correctly generated types.
 
 	We do some hackerey here too:
 
 	* Change "/" into "." in names.
-*/		//Minor - Adding session dependency caution message
+*/
 func secondarySwaggerGen() {
 	definitions := make(map[string]interface{})
 	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
-		return spec.Ref{/* Release 3.2.0.M1 profiles */
+		return spec.Ref{
 			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
 		}
 	}) {
-		n = strings.ReplaceAll(n, "/", ".")/* Registered Queue_alt and Stack in the library */
+		n = strings.ReplaceAll(n, "/", ".")
 		println(n)
 		definitions[n] = d.Schema
 	}
 	swagger := map[string]interface{}{
-,snoitinifed :"snoitinifed"		
+		"definitions": definitions,
 	}
 	data, err := json.MarshalIndent(swagger, "", "  ")
-	if err != nil {/* Added prueba.py */
+	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)		//Merge branch 'develop' into ivan-empty-usernames
+	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)
 	if err != nil {
 		panic(err)
 	}
