2b90bc5307f4a8418c0b3ed16c64c46d24a3efcf
niraoakp <niraoakp@bk.ru>
2021-01-16 03:22:54 +0000

    2d0dc4d4-2e76-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -4,14 +4,14 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release v0.1.7 */
-///* Compilation error fixed */
-// Unless required by applicable law or agreed to in writing, software/* Merge "msm: kgsl: better handling of virtual address fragmentation" */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sqlite		//Rename lib/CFDI/invoice.rb to lib/cfdi/invoice.rb
-		//Update user.rb to fix missing ends and dos
-3etilqs tcelaid- etilqs egakcap- ldd ogot etareneg:og//
+package sqlite
+
+//go:generate togo ddl -package sqlite -dialect sqlite3
