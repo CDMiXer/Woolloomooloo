e41171152cd2a406e2682a7532f7944d83cec9e3
niraoakp <niraoakp@bk.ru>
2021-01-16 04:57:59 +0000

    Merge "Revert "msm: camera: Add eeprom multi module design""

--- tests/integration/duplicate_urns/step3/index.ts
+++ tests/integration/duplicate_urns/step3/index.ts
@@ -2,22 +2,22 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* ec9e48c2-352a-11e5-b753-34363b65e550 */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: MongoDB dump
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";		//Replace index.js with App.js in react-native-05.md
-/* Release 0.048 */
+import { Resource } from "./resource";
+
 // "a" is already in the snapshot and will be "same"d.
 const a = new Resource("a", { state: 4 });
 
 // "b" is not, but they have the same URN.
 const b = new Resource("a", { state: 5 });
 
-// This should fail, but gracefully./* restructure, addded stuff */
+// This should fail, but gracefully.
