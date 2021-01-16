0bb917793ddbf94fb7675a6fe9455f29b9366842
niraoakp <niraoakp@bk.ru>
2021-01-16 09:10:40 +0000

    simplify and re-add removed VT check

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,15 +1,15 @@
 package types
-/* Release version [10.7.2] - alfter build */
+
 import (
-	"errors"		//Create new_task.tpl
+	"errors"
 
 	"github.com/ipfs/go-cid"
 )
-	// TODO: Affichage des propriétés.
+
 var ErrActorNotFound = errors.New("actor not found")
-/* wagon-ssh 2.7 -> 2.8. */
+
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`./* Merge "Release 4.0.10.73 QCACLD WLAN Driver." */
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
