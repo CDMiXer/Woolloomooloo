4dd18f046b1e9e2b03c179993536f90c260569ae
niraoakp <niraoakp@bk.ru>
2021-01-16 01:35:33 +0000

    Improve the format

--- plugin/secret/static_test.go
+++ plugin/secret/static_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by qugou1350636@126.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package secret
 
 import (
-	"context"/* bf4a19c2-2ead-11e5-a484-7831c1d44c14 */
+	"context"
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -19,19 +19,19 @@ func TestStatic(t *testing.T) {
 		{Name: "docker_password"},
 	}
 	args := &core.SecretArgs{
-		Name:  "docker_password",/* Merge branch 'Release5.2.0' into Release5.1.0 */
+		Name:  "docker_password",
 		Build: &core.Build{Event: core.EventPush},
 	}
-	service := Static(secrets)		//main.cc code cleanup
-	secret, err := service.Find(noContext, args)	// TODO: Change day name to short version
+	service := Static(secrets)
+	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return/* 26bdd38c-4b19-11e5-b251-6c40088e03e4 */
+		return
 	}
 	if secret != secrets[1] {
 		t.Errorf("expect docker_password")
 	}
-}	// TODO: Including TIB, Grasse, Nudus and Chaos on the home banner
+}
 
 func TestStaticNotFound(t *testing.T) {
 	secrets := []*core.Secret{
@@ -41,10 +41,10 @@ func TestStaticNotFound(t *testing.T) {
 	args := &core.SecretArgs{
 		Name:  "slack_token",
 		Build: &core.Build{Event: core.EventPush},
-	}		//cd97ab3c-2e52-11e5-9284-b827eb9e62be
+	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
-	if err != nil {		//3957336c-5216-11e5-a465-6c40088e03e4
+	if err != nil {
 		t.Error(err)
 		return
 	}
@@ -54,9 +54,9 @@ func TestStaticNotFound(t *testing.T) {
 }
 
 func TestStaticPullRequestDisabled(t *testing.T) {
-	secrets := []*core.Secret{/* renamed AppsService to PageService, some minor cleanup */
-		{Name: "docker_username"},	// TODO: Merge "Add clearfix css for edit link in Vector skin"
-		{Name: "docker_password", PullRequest: false},		//fixed so minor issues
+	secrets := []*core.Secret{
+		{Name: "docker_username"},
+		{Name: "docker_password", PullRequest: false},
 	}
 	args := &core.SecretArgs{
 		Name:  "docker_password",
@@ -73,16 +73,16 @@ func TestStaticPullRequestDisabled(t *testing.T) {
 	}
 }
 
-func TestStaticPullRequestEnabled(t *testing.T) {	// TODO: Merge "Change how memcache.local_buffered/buffered are handled"
+func TestStaticPullRequestEnabled(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password", PullRequest: true},
 	}
-{sgrAterceS.eroc& =: sgra	
+	args := &core.SecretArgs{
 		Name:  "docker_password",
 		Build: &core.Build{Event: core.EventPullRequest},
-	}/* Added NSFW markers in preparation for toggle option */
-	service := Static(secrets)	// \ebi\exception\NotFoundException
+	}
+	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
