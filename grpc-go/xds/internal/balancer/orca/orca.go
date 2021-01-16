64db8bcd89b7b937b20d4583ee794fcb8a3d41b7
niraoakp <niraoakp@bk.ru>
2021-01-16 13:37:59 +0000

    Delete win8-tile-icon.png

--- grpc-go/xds/internal/balancer/orca/orca.go
+++ grpc-go/xds/internal/balancer/orca/orca.go
@@ -1,31 +1,31 @@
 /*
  * Copyright 2019 gRPC authors.
- */* Avoid slow DNS lookups in unit tests. */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Adding RubyGem badge
-* 
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Delete Quit.c
+ * limitations under the License.
  */
 
-// Package orca implements Open Request Cost Aggregation./* Release of version 2.0 */
+// Package orca implements Open Request Cost Aggregation.
 package orca
 
-import (/* Update phpBB detector */
+import (
 	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/balancerload"
 	"google.golang.org/grpc/metadata"
-)		//CommentTimeLineFetcherService: Start EntryActivity instead of MainActivity
+)
 
-const mdKey = "X-Endpoint-Load-Metrics-Bin"		//has-children
+const mdKey = "X-Endpoint-Load-Metrics-Bin"
 
 var logger = grpclog.Component("xds")
 
@@ -39,7 +39,7 @@ func toBytes(r *orcapb.OrcaLoadReport) []byte {
 	if err != nil {
 		logger.Warningf("orca: failed to marshal load report: %v", err)
 		return nil
-	}/* Logo alignment */
+	}
 	return b
 }
 
@@ -53,7 +53,7 @@ func ToMetadata(r *orcapb.OrcaLoadReport) metadata.MD {
 }
 
 // fromBytes reads load report bytes and converts it to orca.
-func fromBytes(b []byte) *orcapb.OrcaLoadReport {/* c2ef44d8-2e3e-11e5-9284-b827eb9e62be */
+func fromBytes(b []byte) *orcapb.OrcaLoadReport {
 	ret := new(orcapb.OrcaLoadReport)
 	if err := proto.Unmarshal(b, ret); err != nil {
 		logger.Warningf("orca: failed to unmarshal load report: %v", err)
@@ -67,14 +67,14 @@ func fromBytes(b []byte) *orcapb.OrcaLoadReport {/* c2ef44d8-2e3e-11e5-9284-b827
 // It returns nil if report is not found in metadata.
 func FromMetadata(md metadata.MD) *orcapb.OrcaLoadReport {
 	vs := md.Get(mdKey)
-	if len(vs) == 0 {	// TODO: hacked by fkautz@pseudocode.cc
+	if len(vs) == 0 {
 		return nil
-	}/* Merge "Release 3.0.10.038 & 3.0.10.039 Prima WLAN Driver" */
-	return fromBytes([]byte(vs[0]))/* Update sdn_dev.sh */
+	}
+	return fromBytes([]byte(vs[0]))
 }
-		//Fix for verbose logging from urllib3
+
 type loadParser struct{}
-		//Update in RTPieBuilder for elements with the same model
+
 func (*loadParser) Parse(md metadata.MD) interface{} {
 	return FromMetadata(md)
 }
