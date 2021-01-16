60693a5b752f36408180f4915512fb3444fe4f13
niraoakp <niraoakp@bk.ru>
2021-01-16 15:16:51 +0000

    Disable input processor validation on unique fields

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Config } from "@pulumi/pulumi";	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-import * as runtime from "@pulumi/pulumi/runtime"/* Press Release Naranja */
+/* Old PFP Removal */
+import { Config } from "@pulumi/pulumi";
+import * as runtime from "@pulumi/pulumi/runtime"		//JZ: Rename common.pip to base.pip
 
 (async function() {
     const config = new Config();
-
+/* Zuhause -  refresh angepasst; kein Timer bisher; Logdatei nun mit Jahresangabe */
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
-    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that		//Remove two fixes that were backported to RC
     // behavior.
-    const deps = await runtime.computeCodePaths();
+    const deps = await runtime.computeCodePaths();		//Translation of option values. part 2
 
     const actual = JSON.stringify([...deps.keys()].sort());
     const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
