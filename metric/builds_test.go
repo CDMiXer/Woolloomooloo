acdd1b5e906e4ad6d3126120657fb90bdceaa2c1
niraoakp <niraoakp@bk.ru>
2021-01-16 18:04:27 +0000

    Initial directory structure and eclipse project

--- metric/builds_test.go
+++ metric/builds_test.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Update DockerfileRelease */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// [IMP] revision de version dia anterior
 
 package metric
 
-import (
+import (	// TODO: will be fixed by mikeal.rogers@gmail.com
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
-
+	"github.com/drone/drone/mock"/* Finalising R2 PETA Release */
+/* Update mmf.rb */
 	"github.com/golang/mock/gomock"
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"/* Updating Release Workflow */
 )
-
+/* Switch to newest closure compiler snapshot */
 func TestBuildCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
-	// restore the default prometheus registerer
+	// restore the default prometheus registerer/* Document 'module' key */
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot
+		prometheus.DefaultRegisterer = snapshot/* Release 0.5.1. Update to PQM brink. */
 		controller.Finish()
-	}()
+	}()/* Merge branch '5.0' into ch-5.0-1399025032544 */
 
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
@@ -33,21 +33,21 @@ func TestBuildCount(t *testing.T) {
 
 	// x2 repository count
 	count := int64(5)
-
+		//CID display added here and there (in frames, when nick missing)
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Count(gomock.Any()).Return(count, nil)
-	BuildCount(builds)
-
+	BuildCount(builds)		//[ci fw-only Java/officefloor]
+		//Require xenial-arm64 for passing
 	metrics, err := registry.Gather()
-	if err != nil {
+	if err != nil {/* wiki link changes */
 		t.Error(err)
-		return
+		return/* Release version 1.3 */
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
 	}
-	metric := metrics[0]
+	metric := metrics[0]/* Release version 0.0.37 */
 	if want, got := metric.GetName(), "drone_build_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
