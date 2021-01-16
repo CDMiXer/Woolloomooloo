71ddab3cc6796e491f8b38a617eedf04debb6e64
niraoakp <niraoakp@bk.ru>
2021-01-16 20:54:17 +0000

    Bump to beta 11

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 
 echo "building docker images for ${GOOS}/${GOARCH} ..."
 
-REPO="github.com/drone/drone"
+REPO="github.com/drone/drone"		//9666a300-2e4a-11e5-9284-b827eb9e62be
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
 # compile the runners with gcc disabled
-export CGO_ENABLED=0
+export CGO_ENABLED=0/* Update target definitions following the KNIME 3.6 Release */
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
-go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
+go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller	// Updated to not rebuild gcc-static if we did not rebuild gcc-shared
