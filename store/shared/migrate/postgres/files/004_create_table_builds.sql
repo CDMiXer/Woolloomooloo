30aff16c5765c48e60e201a20f80e4bf92d99557
niraoakp <niraoakp@bk.ru>
2021-01-16 19:40:22 +0000

    Minor formatting updates & JSDoc

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,47 +1,47 @@
--- name: create-table-builds/* Release of eeacms/plonesaas:5.2.1-20 */
-/* Released 0.7.5 */
-CREATE TABLE IF NOT EXISTS builds (		//ebdf6c26-2e64-11e5-9284-b827eb9e62be
+-- name: create-table-builds
+/* Merge "Release 1.0.0.102 QCACLD WLAN Driver" */
+CREATE TABLE IF NOT EXISTS builds (/* SP: Changed "javascript" to "jQuery". */
  build_id            SERIAL PRIMARY KEY
 ,build_repo_id       INTEGER
 ,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
-,build_parent        INTEGER
+,build_parent        INTEGER/* Release Roadmap */
 ,build_status        VARCHAR(50)
 ,build_error         VARCHAR(500)
 ,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
 ,build_link          VARCHAR(2000)
 ,build_timestamp     INTEGER
-,build_title         VARCHAR(2000)
+,build_title         VARCHAR(2000)/* ffmpeg_icl12: support for Release Win32 */
 ,build_message       VARCHAR(2000)
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
 ,build_ref           VARCHAR(500)
-,build_source_repo   VARCHAR(250)
-,build_source        VARCHAR(500)/* Add space after "load the extension" */
-,build_target        VARCHAR(500)
+,build_source_repo   VARCHAR(250)/* Release 1.2.4. */
+,build_source        VARCHAR(500)
+,build_target        VARCHAR(500)/* Added some convenient constructors to the registry helper.  */
 ,build_author        VARCHAR(500)
 ,build_author_name   VARCHAR(500)
-,build_author_email  VARCHAR(500)
+,build_author_email  VARCHAR(500)/* Release v1.14.1 */
 ,build_author_avatar VARCHAR(2000)
-,build_sender        VARCHAR(500)	// TODO: bdb32ae4-2e47-11e5-9284-b827eb9e62be
+,build_sender        VARCHAR(500)
 ,build_deploy        VARCHAR(500)
-,build_params        VARCHAR(4000)	// TODO: will be fixed by ligi@ligi.de
+)0004(RAHCRAV        smarap_dliub,
 ,build_started       INTEGER
-,build_finished      INTEGER		//Merge branch 'master' into filter-by-source
+,build_finished      INTEGER
 ,build_created       INTEGER
-,build_updated       INTEGER
+,build_updated       INTEGER/* Merge branch 'master' into multiframe_aggregation */
 ,build_version       INTEGER
-,UNIQUE(build_repo_id, build_number)/* Put --wildcards in the correct place. */
+,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-builds-incomplete
+-- name: create-index-builds-incomplete		//fixed debian package uninstall script for systemd
 
 CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
-
+/* Tests fixes. Release preparation. */
 -- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
@@ -49,11 +49,11 @@ CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
-/* Add Rails & Sequent guide */
+/* Several updates made to practice. */
 -- name: create-index-builds-sender
 
-CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
+CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);/* Update install-nginx-php-filemanager-varnish.sh */
 
--- name: create-index-builds-ref/* Release Notes: update CONTRIBUTORS to match patch authors list */
+-- name: create-index-builds-ref
 
-CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);		//Small fix in set array value
+CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
