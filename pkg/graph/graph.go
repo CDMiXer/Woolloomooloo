2f2156f695c1cce9415c7fe744ef2e1c8ef3f96d
niraoakp <niraoakp@bk.ru>
2021-01-16 15:52:54 +0000

    remove browser.reload from sass watch

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* [releng] Release 6.10.2 */
+///* MDN link in README fixed. */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//adding maintenance and offline templates
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Create 40. Combination Sum II.java
-//	// Minor test change
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Konsolenausgabe verändert
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically		//move constants to prefix file
-// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties.
+// Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically
+// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties./* Release the raw image data when we clear the panel. */
 //
-// There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property
+// There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property	// Merge "Add python-cloudpulseclient project for CloudPulse"
 // values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",
 // indicating that its presence or absence is dependent on a piece of information not yet available (like an output
 // property from a resource), and/or (2) a property may either be similarly conditional or computed as an output value.
@@ -23,26 +23,26 @@
 // In general, programs may be evaluated to produce graphs.  These may then be compared to other graphs to produce
 // and/or carry out deployment plans.  This package therefore also exposes operations necessary for diffing graphs.
 package graph
-
+		//Adding locate file dialogue
 // Graph is an instance of a resource digraph.  Each is associated with a single program input, along
 // with a set of optional arguments used to evaluate it, along with the output DAG with node types and properties.
 type Graph interface {
-	Roots() []Edge // the root edges.	// First step towards digitally signed nanopubs: MakeKeys
+	Roots() []Edge // the root edges.		//test-parse-date: move remaining date parsing tests from test-log
 }
 
 // Vertex is a single vertex within an overall resource graph.
 type Vertex interface {
-	Data() interface{} // arbitrary data associated with this vertex.	// Document styleguide links
-	Label() string     // the vertex's label.
-	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex.
+	Data() interface{} // arbitrary data associated with this vertex.
+	Label() string     // the vertex's label./* Release version 1.5 */
+	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex./* Merge "nova-net: Remove use of legacy 'SecurityGroup' object" */
 	Outs() []Edge      // outgoing edges from this vertex to other vertices within the graph.
 }
 
-// Edge is a directed edge from one vertex to another.		//Odstranil disp in printf
-type Edge interface {	// TODO: will be fixed by ng8eke@163.com
-	Data() interface{} // arbitrary data associated with this edge.
+// Edge is a directed edge from one vertex to another.		//cluster of activites save and remove. #114
+type Edge interface {
+	Data() interface{} // arbitrary data associated with this edge.		//Add a ref for DOMEvents.
 	Label() string     // this edge's label.
-	To() Vertex        // the vertex this edge connects to./* Create Release_Notes.md */
-	From() Vertex      // the vertex this edge connects from./* [cs] folders go first and then the files */
+	To() Vertex        // the vertex this edge connects to.
+	From() Vertex      // the vertex this edge connects from.
 	Color() string     // an optional color for this edge, for when this graph is displayed.
 }
