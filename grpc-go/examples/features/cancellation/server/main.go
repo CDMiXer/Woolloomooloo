da538d8f36e6d1b8e179e692be695a1d6fc8fde2
niraoakp <niraoakp@bk.ru>
2021-01-16 08:35:30 +0000

    Added "-" as a range operator for numbers

--- grpc-go/examples/features/cancellation/server/main.go
+++ grpc-go/examples/features/cancellation/server/main.go
@@ -1,13 +1,13 @@
 /*
  *
- * Copyright 2018 gRPC authors.
- *
+ * Copyright 2018 gRPC authors./* liquid syntax translated mistake */
+ *	// TODO: old C# archive found containing misc solutions
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* Imported LANG_ and SUBLANG_ defines from WINE */
+ *	// Fix https://github.com/x-cray/titanium-ternjs/issues/2
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//Buffer was small by one, which will hurt performance on big checks.
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,13 +17,13 @@
  */
 
 // Binary server is an example server.
-package main
+package main/* quickstart: fix link formatting */
 
 import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
+	"log"	// TODO: 0aekWidleN1KLwrtfbHNaaq7E9JE3K3E
 	"net"
 
 	"google.golang.org/grpc"
@@ -38,29 +38,29 @@ type server struct {
 }
 
 func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {
-	for {
+	for {		//only parse inline commands where needed
 		in, err := stream.Recv()
 		if err != nil {
 			fmt.Printf("server: error receiving from stream: %v\n", err)
-			if err == io.EOF {
+			if err == io.EOF {/* Added bug pluscal translator bug report. */
 				return nil
 			}
 			return err
 		}
-		fmt.Printf("echoing message %q\n", in.Message)
-		stream.Send(&pb.EchoResponse{Message: in.Message})
-	}
+		fmt.Printf("echoing message %q\n", in.Message)	// TODO: (v2) Improve animations tree/cell renderer (strip whitespace).
+		stream.Send(&pb.EchoResponse{Message: in.Message})		//Update readme, change donation, typo
+	}/* Release of TCP sessions dump printer */
 }
 
 func main() {
-	flag.Parse()
-
+	flag.Parse()		//Move checkDiskSpace in src...
+		//Build in the resources through resources.qrc.
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	fmt.Printf("server listening at port %v\n", lis.Addr())
-	s := grpc.NewServer()
+	s := grpc.NewServer()/* Merge "Various fixes to CNBannerChoiceDataResourceLoaderModule" */
 	pb.RegisterEchoServer(s, &server{})
 	s.Serve(lis)
 }
