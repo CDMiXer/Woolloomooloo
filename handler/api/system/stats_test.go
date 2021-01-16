4348d906def09108dda5960d0065e4f816f6755f
niraoakp <niraoakp@bk.ru>
2021-01-16 17:45:07 +0000

    Update new-memory javadoc

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Test helper exports both sinon and sinon.sandbox
-
+// that can be found in the LICENSE file.
+		//Added the prettyname of the entities in the admin tab
 // +build !oss
-/* Merge "Update Pylint score (10/10) in Release notes" */
-package system
 
+package system/* 4.0.2 Release Notes. */
+	// TODO: for linux: exit immediately when error occurs
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"/* Added research topic 1 */
+	"github.com/sirupsen/logrus"
 )
-
-func init() {		//SDL_mixer: Add depends on FLAC codec
-	logrus.SetOutput(ioutil.Discard)/* Release DBFlute-1.1.0-RC2 */
+/* Release notes: Document spoof_client_ip */
+func init() {
+	logrus.SetOutput(ioutil.Discard)
 }
