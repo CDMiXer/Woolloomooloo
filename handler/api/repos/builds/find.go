2db49c58ae1718ec99a6db58b86baf2e4a09fc16
niraoakp <niraoakp@bk.ru>
2021-01-16 21:20:48 +0000

    Fix nested transactions when processing documents

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,60 +1,60 @@
-// Copyright 2019 Drone IO, Inc./* Releases 0.2.1 */
+// Copyright 2019 Drone IO, Inc./* Merge "Release Notes 6.1 - New Features (Partner)" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 2.1 */
-// You may obtain a copy of the License at/* Delete test.min.js.map */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// 64c2e4e6-2e9d-11e5-b2f4-a45e60cdfd11
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Updated blog model for hook test cases.
+erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// distributed under the License is distributed on an "AS IS" BASIS,		//Update structures.go
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release 1.12. */
+// limitations under the License.
 
-package builds/* 2f7c1ed4-2e6f-11e5-9284-b827eb9e62be */
+package builds
 
 import (
-	"net/http"
-	"strconv"
-		//Change UI Layout and modify setup and cpp stuff
+	"net/http"/* 0.5.0 Release. */
+	"strconv"		//upgrade scala version
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// make the current worker payload a link
+	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"/* Rename main.py to script.py */
+	"github.com/go-chi/chi"
 )
-
-// HandleFind returns an http.HandlerFunc that writes json-encoded
-// build details to the the response body.
+	// TODO: 240832f4-2e60-11e5-9284-b827eb9e62be
+// HandleFind returns an http.HandlerFunc that writes json-encoded	// TODO: autocompleting text
+// build details to the the response body.	// TODO: hacked by witek@enjin.io
 func HandleFind(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-) http.HandlerFunc {		//cambios en cerrar fase
-	return func(w http.ResponseWriter, r *http.Request) {
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {		//Updated da translationfile
 		var (
-			namespace = chi.URLParam(r, "owner")		//EditableLabel Component
-			name      = chi.URLParam(r, "name")		//Updating contributors
-		)	// TODO: Create modalContent.html
+			namespace = chi.URLParam(r, "owner")/* Fixes for Data18 Web Content split scenes - Studio & Release date. */
+			name      = chi.URLParam(r, "name")		//Update DialogView.axaml
+		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: do not compress css and js files in trunk folder
-			render.NotFound(w, err)
-			return
-		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)/* WIP: Work on ImageMotionKernel */
 		if err != nil {
 			render.NotFound(w, err)
 			return
+		}
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)		//-- correcting typo in the yml
+		if err != nil {
+			render.NotFound(w, err)
+			return/* Merge "Release 3.2.3.354 Prima WLAN Driver" */
 		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			return	// TODO: Merge "Handle call list in CallManager."
+			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
@@ -62,5 +62,5 @@ func HandleFind(
 
 type buildWithStages struct {
 	*core.Build
-	Stages []*core.Stage `json:"stages,omitempty"`
+	Stages []*core.Stage `json:"stages,omitempty"`		//Change presenter from Philip Craig to Wayne Palmer
 }
