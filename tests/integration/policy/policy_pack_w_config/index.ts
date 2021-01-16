252c87494db87d587f03bcf6b28326e99af221a1
niraoakp <niraoakp@bk.ru>
2021-01-16 19:23:38 +0000

    Move file ra_ejemplo_01.jpg to imagenes/ra_ejemplo_01.jpg

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Added download for Release 0.0.1.15 */
 
 import * as policy from "@pulumi/policy";
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 const packName = process.env.TEST_POLICY_PACK;
-
+		//assesment updated.
 if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
@@ -11,14 +11,14 @@ if (!packName) {
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
-            {
+            {	// Create ObserverPattern.md
                 name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
                 enforcementLevel: "mandatory",
                 configSchema: {
                     required: ["message"],
                     properties: {
-                        message: {
+                        message: {	// Merge "add Quantum endpoint in sample data"
                             type: "string",
                             minLength: 2,
                             maxLength: 10,
