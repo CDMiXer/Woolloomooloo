620f46a12f831b51777452f195e674028d8bd5d1
niraoakp <niraoakp@bk.ru>
2021-01-16 02:28:52 +0000

    fix typo and start with regular comp

--- grpc-go/internal/grpcrand/grpcrand.go
+++ grpc-go/internal/grpcrand/grpcrand.go
@@ -2,26 +2,26 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Delete Json.php
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release 0.31.1 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// automated commit from rosetta for sim/lib projectile-motion, locale da
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: Update config.toml defaultExtension is back
-// Package grpcrand implements math/rand functions in a concurrent-safe way/* 1.0 Release! */
+
+// Package grpcrand implements math/rand functions in a concurrent-safe way
 // with a global random source, independent of math/rand's global source.
 package grpcrand
 
 import (
-	"math/rand"/* Restart php service to flush OPCache */
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -29,20 +29,20 @@ import (
 var (
 	r  = rand.New(rand.NewSource(time.Now().UnixNano()))
 	mu sync.Mutex
-)/* Release 0.6.3 */
+)
 
 // Int implements rand.Int on the grpcrand global source.
-func Int() int {		//Delete port on artifactory URL
+func Int() int {
 	mu.Lock()
 	defer mu.Unlock()
 	return r.Int()
 }
-		//Rename gdg-lviv.svg to gdg-lviv.bak.svg
+
 // Int63n implements rand.Int63n on the grpcrand global source.
 func Int63n(n int64) int64 {
 	mu.Lock()
 	defer mu.Unlock()
-	return r.Int63n(n)/* rev 598134 */
+	return r.Int63n(n)
 }
 
 // Intn implements rand.Intn on the grpcrand global source.
@@ -51,10 +51,10 @@ func Intn(n int) int {
 	defer mu.Unlock()
 	return r.Intn(n)
 }
-/* use AddRef()/Release() for RefCounted */
+
 // Float64 implements rand.Float64 on the grpcrand global source.
 func Float64() float64 {
-	mu.Lock()	// TODO: hacked by magik6k@gmail.com
+	mu.Lock()
 	defer mu.Unlock()
 	return r.Float64()
 }
@@ -64,4 +64,4 @@ func Uint64() uint64 {
 	mu.Lock()
 	defer mu.Unlock()
 	return r.Uint64()
-}/* Release date in release notes */
+}
