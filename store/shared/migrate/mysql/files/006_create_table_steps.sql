4534c09d909c3736922df61716f2e54f12b7eb06
niraoakp <niraoakp@bk.ru>
2021-01-16 22:45:21 +0000

    Convert closure filters as real functions

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,19 +1,19 @@
--- name: create-table-steps		//Delete de_russka.spawns.cfg
+-- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (		//Create jquery.nicescroll.js
+CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)/* Add anltr4 syntax documentation */
+,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER/* Se incluye Java Doc */
-,step_version     INTEGER		//Updated: geogebra-geometry 6.0.562
-,UNIQUE(step_stage_id, step_number)		//create HTTP interceptor for secure calls
-);/* Define choosePM */
+,step_stopped     INTEGER
+,step_version     INTEGER
+,UNIQUE(step_stage_id, step_number)
+);
 
 -- name: create-index-steps-stage
 
