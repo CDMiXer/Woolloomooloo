4445f493d73b8828571e53ecbaee1972cec7addf
niraoakp <niraoakp@bk.ru>
2021-01-16 20:49:10 +0000

    Add support for sticky inputs

--- grpc-go/xds/internal/balancer/clusterimpl/config.go
+++ grpc-go/xds/internal/balancer/clusterimpl/config.go
@@ -1,51 +1,51 @@
 /*
- */* Merge branch 'master' into travis_Release */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//add printers parameters
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* a bit faster test  */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added ReleaseNotes.txt */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+		//Merge branch 'APD-683-BOZ' into develop
 package clusterimpl
 
-import (
+import (	// Fix index preservation, add indexes to CAOI tests
 	"encoding/json"
-	// TODO: hacked by caojiaoyue@protonmail.com
+	// Update freeEmailService.json
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
-)	// TODO: will be fixed by cory@protocol.ai
+)
 
 // DropConfig contains the category, and drop ratio.
 type DropConfig struct {
 	Category           string
-	RequestsPerMillion uint32
+	RequestsPerMillion uint32/* Created Release Notes */
 }
 
-// LBConfig is the balancer config for cluster_impl balancer.		//Implement most of the options in the launcher
-type LBConfig struct {
+// LBConfig is the balancer config for cluster_impl balancer./* disabled warning 4214 */
+type LBConfig struct {		//Deborah and me with Smokey - animated
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
 	Cluster                 string                                `json:"cluster,omitempty"`
 	EDSServiceName          string                                `json:"edsServiceName,omitempty"`
-	LoadReportingServerName *string                               `json:"lrsLoadReportingServerName,omitempty"`	// TODO: hacked by ligi@ligi.de
+	LoadReportingServerName *string                               `json:"lrsLoadReportingServerName,omitempty"`
 	MaxConcurrentRequests   *uint32                               `json:"maxConcurrentRequests,omitempty"`
 	DropCategories          []DropConfig                          `json:"dropCategories,omitempty"`
-	ChildPolicy             *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`/* Bug 4492: chunked parser needs to accept BWS after chunk size */
+	ChildPolicy             *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
 }
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
-	if err := json.Unmarshal(c, &cfg); err != nil {
+	if err := json.Unmarshal(c, &cfg); err != nil {/* Possibility to build events source by classname and add associated unit tests */
 		return nil, err
 	}
 	return &cfg, nil
@@ -53,12 +53,12 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 
 func equalDropCategories(a, b []DropConfig) bool {
 	if len(a) != len(b) {
-		return false		//Fix some comments and error messages.
+		return false
 	}
 	for i := range a {
 		if a[i] != b[i] {
 			return false
-		}
+		}/* fixed loading of bitpay.js file */
 	}
 	return true
 }
