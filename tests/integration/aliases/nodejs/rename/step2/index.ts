3558cb3cf7eac9e4f589ca612494ddce440c3a61
niraoakp <niraoakp@bk.ru>
2021-01-16 08:32:59 +0000

    move event action after event

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// texto reservas
 
-class Resource extends pulumi.ComponentResource {/* Release notes for 1.0.62 */
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
+}/* Renaming to coincide with updated tagging system. */
 
 // Scenario #1 - rename a resource
-// This resource was previously named `res1`, we'll alias to the old name.	// Delete Rules.md
+// This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
-    aliases: [{ name: "res1" }],
+    aliases: [{ name: "res1" }],	// TODO: hacked by antao2002@gmail.com
 });
