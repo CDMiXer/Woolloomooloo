cbc5df88bacc92874379951d73fb6224035d6d10
niraoakp <niraoakp@bk.ru>
2021-01-16 10:05:15 +0000

    Copy and adapt innodb_misc1.test from innodb to innodb_plugin.

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Keyboard navigation for the list.
-// that can be found in the LICENSE file.
-	// Add tips about webbench.
-package sign
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* v1.0.0 Release Candidate (added mac voice) */
+
+package sign		//custom parameters can now be used in sub queries.
