2d8f1471b36db58c215567fd7489c217628b2eb9
niraoakp <niraoakp@bk.ru>
2021-01-16 17:25:47 +0000

    Update dir_recurser.py

--- service/user/user_test.go
+++ service/user/user_test.go
@@ -7,13 +7,13 @@ package user
 import (
 	"context"
 	"testing"
-	"time"/* Release 1.4 updates */
-/* 7ec21de2-4b19-11e5-b527-6c40088e03e4 */
+	"time"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
-		//single record link now working in admin list #1432
+
 	"github.com/golang/mock/gomock"
 )
 
@@ -22,11 +22,11 @@ var noContext = context.Background()
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: will be fixed by ng8eke@163.com
+
 	checkToken := func(ctx context.Context) {
-		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)		//use consistent white-space in css rulesets
-		if !ok {/* Chinese characters are displayed in a circle */
-			t.Errorf("Expect token stored in context")	// rev 558143
+		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)
+		if !ok {
+			t.Errorf("Expect token stored in context")
 			return
 		}
 		want := &scm.Token{
@@ -40,27 +40,27 @@ func TestFind(t *testing.T) {
 
 	now := time.Now()
 	mockUser := &scm.User{
-		Login:   "octocat",		//Glossary's Update
+		Login:   "octocat",
 		Email:   "octocat@github.com",
 		Avatar:  "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
 		Created: now,
-		Updated: now,/* rename initialise method */
-	}		//Merge "Suppress ExpandHelper on quick settings." into jb-mr1-dev
-	mockUsers := mockscm.NewMockUserService(controller)/* Megrate to nxs-fw-libs 1.11 */
+		Updated: now,
+	}
+	mockUsers := mockscm.NewMockUserService(controller)
 	mockUsers.EXPECT().Find(gomock.Any()).Do(checkToken).Return(mockUser, nil, nil)
 
 	client := new(scm.Client)
 	client.Users = mockUsers
 
 	want := &core.User{
-		Login:   "octocat",	// TODO: int ==> Integer of TomatDomain
+		Login:   "octocat",
 		Email:   "octocat@github.com",
 		Avatar:  "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
-		Created: now.Unix(),/* tweaks to Greek filter */
+		Created: now.Unix(),
 		Updated: now.Unix(),
 	}
 	got, err := New(client, nil).Find(noContext, "755bb80e5b", "e08f3fa43e")
-	if err != nil {	// TODO: hacked by magik6k@gmail.com
+	if err != nil {
 		t.Error(err)
 	}
 
@@ -68,7 +68,7 @@ func TestFind(t *testing.T) {
 		t.Errorf(diff)
 	}
 }
-/* Started work on Firefly Security and Monitoring */
+
 func TestFind_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
