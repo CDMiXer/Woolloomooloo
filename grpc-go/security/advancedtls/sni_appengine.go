be603753553c222197712b01bbb0473bddfc8caa
niraoakp <niraoakp@bk.ru>
2021-01-16 04:38:23 +0000

    Create AdvancedSettingsActivity$5.smali

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,14 +1,14 @@
 // +build appengine
-/* [Cleanup] Remove CConnman::Copy(Release)NodeVector, now unused */
+
 /*
- *	// TODO: regenerate po/software-center.pot
+ *
  * Copyright 2020 gRPC authors.
- *		//Added authentication functions
- * Licensed under the Apache License, Version 2.0 (the "License");/* (#5206) - fix Buffer being included in bundle */
- * you may not use this file except in compliance with the License.	// TODO: hacked by magik6k@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Add Squirrel Release Server to the update server list. */
- *     http://www.apache.org/licenses/LICENSE-2.0/* a6b5bf18-2e4d-11e5-9284-b827eb9e62be */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,8 +19,8 @@
  */
 
 package advancedtls
-/* Add DPH dotp test */
-import (	// TODO: fd05461a-2e64-11e5-9284-b827eb9e62be
+
+import (
 	"crypto/tls"
 )
 
