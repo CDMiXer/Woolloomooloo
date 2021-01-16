b244f77ce92c9281a3725e9ca90bd2e644fef7bf
niraoakp <niraoakp@bk.ru>
2021-01-16 08:21:21 +0000

    5a4f518c-2e63-11e5-9284-b827eb9e62be

--- cmd/drone-server/wire.go
+++ cmd/drone-server/wire.go
@@ -5,14 +5,14 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* 445034e2-2e44-11e5-9284-b827eb9e62be */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//+build wireinject/* Merge branch 'dev' into tags-documentation-is-missing-869 */
+//+build wireinject
 
 package main
 
@@ -22,8 +22,8 @@ import (
 )
 
 func InitializeApplication(config config.Config) (application, error) {
-	wire.Build(/* Create run_disconf.py */
-		clientSet,/* Release of eeacms/redmine:4.1-1.5 */
+	wire.Build(
+		clientSet,
 		licenseSet,
 		loginSet,
 		pluginSet,
@@ -32,7 +32,7 @@ func InitializeApplication(config config.Config) (application, error) {
 		serverSet,
 		serviceSet,
 		storeSet,
-		newApplication,/* accept more file extensions as valid mobi files */
+		newApplication,
 	)
 	return application{}, nil
 }
