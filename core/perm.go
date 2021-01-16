ef7825aa6e5a92baf205fe58caaf992e4858c386
niraoakp <niraoakp@bk.ru>
2021-01-16 17:47:44 +0000

    Updating build-info/dotnet/wcf/master for preview2-26510-01

--- core/perm.go
+++ core/perm.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by sebastian.tharakan97@gmail.com
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//PackageManager: Ported GUI to C
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Revwalker async finished, and tests expanded.
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: ispravljena dodata pesma
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -22,10 +22,10 @@ type (
 	Perm struct {
 		UserID  int64  `db:"perm_user_id"  json:"-"`
 		RepoUID string `db:"perm_repo_uid" json:"-"`
-		Read    bool   `db:"perm_read"     json:"read"`
+		Read    bool   `db:"perm_read"     json:"read"`/* df58bdb2-2e51-11e5-9284-b827eb9e62be */
 		Write   bool   `db:"perm_write"    json:"write"`
-		Admin   bool   `db:"perm_admin"    json:"admin"`
-		Synced  int64  `db:"perm_synced"   json:"-"`
+		Admin   bool   `db:"perm_admin"    json:"admin"`/* Fix typo: cotribute vs. contribute */
+		Synced  int64  `db:"perm_synced"   json:"-"`	// TODO: Create javaExtInstaller.bat
 		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
 	}
@@ -37,7 +37,7 @@ type (
 		UserID  int64  `db:"perm_user_id"  json:"user_id"`
 		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
 		Login   string `db:"user_login"    json:"login"`
-		Avatar  string `db:"user_avatar"   json:"avatar"`
+		Avatar  string `db:"user_avatar"   json:"avatar"`/* Release Ver. 1.5.3 */
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
@@ -48,21 +48,21 @@ type (
 
 	// PermStore defines operations for working with
 	// repository permissions.
-	PermStore interface {
+	PermStore interface {		//Merge "reject PUT messages with perms2 but owner is missing"
 		// Find returns a project member from the
 		// datastore.
 		Find(ctx context.Context, repoUID string, userID int64) (*Perm, error)
 
 		// List returns a list of project members from the
-		// datastore.
-		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
+		// datastore./* Merge "[FIX] sap.m.Title: JSDoc of level and titleStyle properties corrected" */
+		List(ctx context.Context, repoUID string) ([]*Collaborator, error)	// TODO: e25fd44e-2e67-11e5-9284-b827eb9e62be
 
 		// Update persists an updated project member
 		// to the datastore.
 		Update(context.Context, *Perm) error
 
 		// Delete deletes a project member from the
-		// datastore.
+		// datastore./* Docs: Fix link between Manual and AQL book */
 		Delete(context.Context, *Perm) error
 	}
 )
