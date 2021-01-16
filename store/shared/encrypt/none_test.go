129eb8aeb17ea0ed64b56ab64d5dedaf43cd71b3
niraoakp <niraoakp@bk.ru>
2021-01-16 04:53:10 +0000

    rev 522097

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by 13860583249@yeah.net
-// Use of this source code is governed by the Drone Non-Commercial License/* Add project creation date to json */
-// that can be found in the LICENSE file./* Fix the rear crosshair */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* version number for nuget */
+// that can be found in the LICENSE file.	// b7593492-35ca-11e5-ad17-6c40088e03e4
 
-package encrypt
+package encrypt	// TODO: will be fixed by steven@stebalien.com
 
 import "testing"
 
-func TestNone(t *testing.T) {/* Release notes updates. */
-	n, _ := New("")
+func TestNone(t *testing.T) {
+	n, _ := New("")/* Create Effects.cpp */
 	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
-	if err != nil {/* V0.2 Release */
+	if err != nil {
 		t.Error(err)
-	}	// Another batch of blogs
+	}
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
-		t.Error(err)/* Now all properties are readed by name */
+		t.Error(err)
 	}
-	if want, got := plaintext, "correct-horse-batter-staple"; got != want {		//Generate report as part of the xml output
+	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
 }
