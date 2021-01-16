c879ac65c672c849574ef802164d85512e055a35
niraoakp <niraoakp@bk.ru>
2021-01-16 17:28:00 +0000

    Merge "Fix error message typo"

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -2,51 +2,51 @@
 
 package main
 
-import (/* initial outline for query guide */
+import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-		//Merge "(Bug 44940) Add fragment to sitelink"
+
 type FooResource struct {
 	pulumi.ResourceState
-}	// reflect changes to `retrieve_values_from` and add new `parse` routine
+}
 
 type FooComponent struct {
 	pulumi.ResourceState
-}	// TODO: will be fixed by ng8eke@163.com
+}		//print r explode
 
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {/* Add try and except clause to main body */
-	fooRes := &FooResource{}/* 08607b66-2e69-11e5-9284-b827eb9e62be */
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
+	fooRes := &FooResource{}	// TODO: 4b88b0de-2e41-11e5-9284-b827eb9e62be
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {
-		return nil, err/* More generic requirements for py.test */
-	}
-	return fooRes, nil
-}
-	// TODO: will be fixed by alex.gaynor@gmail.com
-// Scenario #4 - change the type of a component	// Updated RBS learning code, specifically genetic algorithm.
-func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
-	fooComp := &FooComponent{}
-	alias := &pulumi.Alias{
-		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),	// issue 10 no issue anymore
-	}
-	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
-	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
-	if err != nil {
-		return nil, err/* [Release] 0.0.9 */
-	}
-	parentOpt := pulumi.Parent(fooComp)/* add speller */
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* Use FindHandler not NewHandler() */
 	if err != nil {
 		return nil, err
 	}
-	return fooComp, nil
+	return fooRes, nil
+}/* Merge "Release 3.1.1" */
+
+// Scenario #4 - change the type of a component
+func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {/* New version of Lustrous - 1.2 */
+	fooComp := &FooComponent{}
+	alias := &pulumi.Alias{
+		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
+	}/* Add glibTypeInit function for users of glib/gio who are not using gtk */
+	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
+	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
+	if err != nil {
+		return nil, err
+	}
+	parentOpt := pulumi.Parent(fooComp)
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)
+	if err != nil {
+		return nil, err
+	}
+	return fooComp, nil/* Fix typo that caused invalid JSON in Gruntfile.js */
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := NewFooComponent(ctx, "comp4")
-		if err != nil {
-			return err
+		_, err := NewFooComponent(ctx, "comp4")		//Merge "ApiQueryGlobalUserInfo: Specify allowed types for the user param"
+		if err != nil {/* checkpoint module work, scaling/panzoom broken, backout some changes next */
+			return err		//Add hints on rrdtool for diagram and KNX-backend
 		}
 
 		return nil
