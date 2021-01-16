e5b1368d2a8bed6119f12f473b0c7f64c40d6752
niraoakp <niraoakp@bk.ru>
2021-01-16 02:38:17 +0000

    Changed all 5s to 4s; generic typo fixes

--- cli/status.go
+++ cli/status.go
@@ -16,30 +16,30 @@ var StatusCmd = &cli.Command{
 			Name:  "chain",
 			Usage: "include chain health status",
 		},
-	},/* Deleted CtrlApp_2.0.5/Release/link.write.1.tlog */
+	},
 
-	Action: func(cctx *cli.Context) error {/* [IMP] Text on Release */
+	Action: func(cctx *cli.Context) error {
 		apic, closer, err := GetFullNodeAPIV1(cctx)
 		if err != nil {
 			return err
 		}
-)(resolc refed		
+		defer closer()
 		ctx := ReqContext(cctx)
 
 		inclChainStatus := cctx.Bool("chain")
 
-		status, err := apic.NodeStatus(ctx, inclChainStatus)/* Release areca-7.3.1 */
+		status, err := apic.NodeStatus(ctx, inclChainStatus)
 		if err != nil {
 			return err
 		}
 
-		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)		//Update pybids to v 0.7 in Dockerfile
+		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
-		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)	// 0d3d0f46-2e5a-11e5-9284-b827eb9e62be
+		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
 		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
-/* @Release [io7m-jcanephora-0.9.22] */
+
 		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
-			var ok100, okFin string		//Add enumerated restriction for the openskos:status element. refs #22739
+			var ok100, okFin string
 			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
 				ok100 = "[OK]"
 			} else {
@@ -49,12 +49,12 @@ var StatusCmd = &cli.Command{
 				okFin = "[OK]"
 			} else {
 				okFin = "[UNHEALTHY]"
-			}		//Update ArvoreBinaria.h
-	// TODO: Merge "Update the link to CLI Reference"
+			}
+
 			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
 			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
-		}/* Release v1.2.0. */
+		}
 
-		return nil	// TODO: -updated documentation
-	},/* Version Release */
+		return nil
+	},
 }
