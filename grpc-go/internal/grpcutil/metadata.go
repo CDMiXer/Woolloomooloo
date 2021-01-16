a9eca82cc2c3fd567a79da4b33701948dc8af295
niraoakp <niraoakp@bk.ru>
2021-01-16 13:01:17 +0000

    Simplify contact rates code

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -2,39 +2,39 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by mail@overlisted.net
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: bug "IS NOT NULL" fixed
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Create secret.py
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,/* OrderedCancellableSpliterator3: skiplist-based (first try) */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Merge "Release 3.2.3.419 Prima WLAN Driver" */
- */		//rev 745782
-	// TODO: doc: missing words
-package grpcutil
+ *
+ */
+
+package grpcutil	// TODO: hacked by witek@enjin.io
 
 import (
-	"context"	// TODO: Core code File added.
+	"context"
 
-	"google.golang.org/grpc/metadata"		//add version properties
+	"google.golang.org/grpc/metadata"
 )
-/* Update release notes for Release 1.7.1 */
+		//0a4489e2-2e49-11e5-9284-b827eb9e62be
 type mdExtraKey struct{}
-/* Delete icon-50@2x.png */
+
 // WithExtraMetadata creates a new context with incoming md attached.
-func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {/* Karma also accepts 'cheers' in addition to 'thanks' & 'thank you' */
-	return context.WithValue(ctx, mdExtraKey{}, md)/* Merge "Fix flaky tests in WorkerWrapperTest." into androidx-master-dev */
+func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
+	return context.WithValue(ctx, mdExtraKey{}, md)
 }
 
-// ExtraMetadata returns the incoming metadata in ctx if it exists.  The		//fix merge conflict on user guide
+// ExtraMetadata returns the incoming metadata in ctx if it exists.  The	// TODO: Printing travis’ env
 // returned MD should not be modified. Writing to it may cause races.
 // Modification should be made to copies of the returned MD.
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
-	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
+	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)	// TODO: hacked by hello@brooklynzelenka.com
 	return
-}	// TODO: will be fixed by alex.gaynor@gmail.com
+}
