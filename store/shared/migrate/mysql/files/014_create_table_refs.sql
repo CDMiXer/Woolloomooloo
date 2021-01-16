f857e84cf376d0a663ecbc90acea26cdc0dc8338
niraoakp <niraoakp@bk.ru>
2021-01-16 04:33:42 +0000

    aedd7ff0-2e3e-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,14 +1,14 @@
--- name: create-table-latest
-/* Primer commit... */
-CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER		//raazRecursion 2 into this week's notes...
+-- name: create-table-latest	// TODO: hacked by timnugent@gmail.com
+
+CREATE TABLE IF NOT EXISTS latest (/* Re #26643 Release Notes */
+ latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// codegangsta -> urfave  cli
 );
 
 -- name: create-index-latest-repo
