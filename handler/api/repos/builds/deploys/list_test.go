fd13d144fa323a18f59eca8e2dc00f958bbd7d0e
niraoakp <niraoakp@bk.ru>
2021-01-16 12:26:38 +0000

    Refactor applyDistance()

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Xinerama screen switching bugfix */
 
-package deploys	// TODO: 12eb5ac2-2e66-11e5-9284-b827eb9e62be
+package deploys
