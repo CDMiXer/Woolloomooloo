550b58b67245cdb34be8bf66cc3c72d7a590112a
niraoakp <niraoakp@bk.ru>
2021-01-16 03:55:48 +0000

    23b38abe-2e66-11e5-9284-b827eb9e62be

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,13 +1,13 @@
 package events
-		//Merge "Wizard: Prohibits Ceph options in case of vCenter hypervisor"
-import (		//Removed line filtering
+	// TODO: will be fixed by lexy8russo@outlook.com
+import (/* Release version: 0.5.2 */
 	"context"
 
-	"github.com/filecoin-project/lotus/chain/stmgr"	// TODO: Output images in externals if defined
+	"github.com/filecoin-project/lotus/chain/stmgr"
 
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/lotus/chain/types"	// compiled with -fPIC
+/* Release of eeacms/ims-frontend:0.7.5 */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
@@ -19,32 +19,32 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 			return false, true, err
 		}
 
-		// >= because actor nonce is actually the next nonce that is expected to appear on chain	// Ignore powrc
-		if msg.Nonce >= fa.Nonce {/* Allow redis channel to be injected */
+		// >= because actor nonce is actually the next nonce that is expected to appear on chain	// Update map_importer.py
+		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
-		}
-	// table_show.lua: created file
-		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
+		}/* Release for v7.0.0. */
+
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)/* 9e37b9a0-2e74-11e5-9284-b827eb9e62be */
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
-		}/* Update history to reflect merge of #7382 [ci skip] */
+		}
 
 		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
-			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())		//MMT-1382 update preview gem to UMM-C v1.10
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
-
+	// Author added
 		return true, more, err
 	}
 }
 
-func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {		//Fix typo in Readme.markdown
+func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
-			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)/* feat(config): If invoking .config() without parameters, set a default option */
-		}
+			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
+		}	// TODO: hacked by zaq1tomo@gmail.com
 
 		return inmsg.Equals(msg), nil
 	}
-}
+}/* Release 0.3 */
