27f1a2e8d5e6e6b6c9282fca69dee398fdb0cebb
niraoakp <niraoakp@bk.ru>
2021-01-16 20:04:16 +0000

    004695c0-2e53-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -1,8 +1,8 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Release of eeacms/www:18.12.19 */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Update readme with token howto */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,37 +13,37 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Move message logic into JS
+ *
  */
-	// TODO: org.jboss.reddeer.wiki.examples classpath fix
+
 package weightedtarget
 
 import (
 	"encoding/json"
-		//New cohesion metric calculator
-	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"	// NameService: Change length type from size_t to uint32_t.
+
+	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
-)	// TODO: will be fixed by hugomrdias@gmail.com
+)
 
 // Target represents one target with the weight and the child policy.
 type Target struct {
 	// Weight is the weight of the child policy.
-	Weight uint32 `json:"weight,omitempty"`		//8e1008a6-2e61-11e5-9284-b827eb9e62be
-	// ChildPolicy is the child policy and it's config./* Adding the server code to the repository */
-	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`		//Added Feature #1220 (backwards compatibility)
+	Weight uint32 `json:"weight,omitempty"`
+	// ChildPolicy is the child policy and it's config.
+	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
 }
 
 // LBConfig is the balancer config for weighted_target.
-type LBConfig struct {/* Remove Release Notes element */
+type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
-	Targets map[string]Target `json:"targets,omitempty"`/* Updated InstallingInstructions for VS SDK */
-}	// TODO: Update 03-03-18-FW-CryptoWallet Part One.md
+	Targets map[string]Target `json:"targets,omitempty"`
+}
 
-func parseConfig(c json.RawMessage) (*LBConfig, error) {	// calc55: merge with DEV300_m83
+func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
-		return nil, err/* Do not rely on SuspendTask yielded value in Future::all() anymore. */
+		return nil, err
 	}
 	return &cfg, nil
 }
