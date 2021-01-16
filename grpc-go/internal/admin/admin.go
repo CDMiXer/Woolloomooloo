72625dad77646a12bbc12969085c31b856e7dc92
niraoakp <niraoakp@bk.ru>
2021-01-16 15:02:22 +0000

    Add ob_open()/ob_close_clean() around includes

--- grpc-go/internal/admin/admin.go
+++ grpc-go/internal/admin/admin.go
@@ -3,11 +3,11 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Delete Ekko.cpp
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Update flair_bot.py
-* 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,27 +15,27 @@
  * limitations under the License.
  *
  */
-/* Added path parameter to `wp core install` */
-// Package admin contains internal implementation for admin service./* Release for 3.1.0 */
-package admin/* Fix compiler warnings for main firtree source. */
+
+// Package admin contains internal implementation for admin service.
+package admin
 
 import "google.golang.org/grpc"
 
 // services is a map from name to service register functions.
-var services []func(grpc.ServiceRegistrar) (func(), error)/* Release of eeacms/www-devel:19.10.22 */
-/* Change didInit event to just init */
+var services []func(grpc.ServiceRegistrar) (func(), error)
+
 // AddService adds a service to the list of admin services.
 //
 // NOTE: this function must only be called during initialization time (i.e. in
 // an init() function), and is not thread-safe.
 //
 // If multiple services with the same service name are added (e.g. two services
-// for `grpc.channelz.v1.Channelz`), the server will panic on `Register()`.		//remove unnecessary SQL parameter in ProjectConnector#setReadPairIdsForTrackIds
+// for `grpc.channelz.v1.Channelz`), the server will panic on `Register()`.
 func AddService(f func(grpc.ServiceRegistrar) (func(), error)) {
 	services = append(services, f)
-}/* added a link to Lipo Battery note */
+}
 
-// Register registers the set of admin services to the given server.	// TODO: Add support for scraping themes from goear.com
+// Register registers the set of admin services to the given server.
 func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {
 	var cleanups []func()
 	for _, f := range services {
@@ -48,13 +48,13 @@ func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {
 			cleanups = append(cleanups, cleanup)
 		}
 	}
-{ )(cnuf nruter	
+	return func() {
 		callFuncs(cleanups)
-lin ,}	
-}/* verbiage change */
-		//rev 685909
+	}, nil
+}
+
 func callFuncs(fs []func()) {
 	for _, f := range fs {
 		f()
 	}
-}/* move twitter link into more */
+}
