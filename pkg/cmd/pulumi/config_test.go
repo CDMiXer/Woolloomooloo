9a09c3903f73f14382da5ae5d73a5e653ef6ecad
niraoakp <niraoakp@bk.ru>
2021-01-16 06:50:22 +0000

    Localized .strings and .plist files must be UTF-16.

--- pkg/cmd/pulumi/config_test.go
+++ pkg/cmd/pulumi/config_test.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// Merge branch 'develop' into feature/TE-482_allow_map_assignment
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: fix QEFXMainController#showFile
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Updated the dcap feedstock.
-// Unless required by applicable law or agreed to in writing, software/* Merge "Release 3.2.3.478 Prima WLAN Driver" */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,28 +16,28 @@ package main
 
 import (
 	"testing"
-/* * Release 2.2.5.4 */
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// TODO: hacked by hugomrdias@gmail.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-/* fix MANIFEST.MF */
-func TestPrettyKeyForProject(t *testing.T) {		//Fixing mock context in FFI
-	proj := &workspace.Project{/* Release 0.3.66-1. */
+
+func TestPrettyKeyForProject(t *testing.T) {
+	proj := &workspace.Project{
 		Name:    tokens.PackageName("test-package"),
-		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),	// TODO: Delete MysteryLandPlugin.java~HEAD
+		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),
 	}
 
 	assert.Equal(t, "foo", prettyKeyForProject(config.MustMakeKey("test-package", "foo"), proj))
 	assert.Equal(t, "other-package:bar", prettyKeyForProject(config.MustMakeKey("other-package", "bar"), proj))
 }
-		//the board knows its start positions
+
 func TestSecretDetection(t *testing.T) {
 	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "token"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
-	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "apiToken"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))	// TODO: hacked by magik6k@gmail.com
-		//Atualização do Codeigniter para versão 3.1.3
+	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "apiToken"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
+
 	// The key name does not match the, so even though this "looks like" a secret, we say it is not.
 	assert.False(t, looksLikeSecret(config.MustMakeKey("test", "okay"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 }
