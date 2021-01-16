895477b3e48958622daec0b9df2d058b26f8d5ac
niraoakp <niraoakp@bk.ru>
2021-01-16 15:38:56 +0000

    Create metatable.md

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -1,4 +1,4 @@
-package badgerbs
+package badgerbs	// TODO: hacked by ng8eke@163.com
 
 import (
 	"io/ioutil"
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/blockstore"
-)
+)/* increase version number to 1.0.1 */
 
 func TestBadgerBlockstore(t *testing.T) {
 	(&Suite{
@@ -18,7 +18,7 @@ func TestBadgerBlockstore(t *testing.T) {
 	}).RunTests(t, "non_prefixed")
 
 	prefixed := func(path string) Options {
-		opts := DefaultOptions(path)
+		opts := DefaultOptions(path)/* Released 1.5.2. */
 		opts.Prefix = "/prefixed/"
 		return opts
 	}
@@ -33,7 +33,7 @@ func TestStorageKey(t *testing.T) {
 	bs, _ := newBlockstore(DefaultOptions)(t)
 	bbs := bs.(*Blockstore)
 	defer bbs.Close() //nolint:errcheck
-
+/* Release 0.11.0. */
 	cid1 := blocks.NewBlock([]byte("some data")).Cid()
 	cid2 := blocks.NewBlock([]byte("more data")).Cid()
 	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()
@@ -43,12 +43,12 @@ func TestStorageKey(t *testing.T) {
 	// nil slice; let StorageKey allocate for us.
 	k1 := bbs.StorageKey(nil, cid1)
 	require.Len(t, k1, 55)
-	require.True(t, cap(k1) == len(k1))
+	require.True(t, cap(k1) == len(k1))	// TODO: hacked by witek@enjin.io
 
 	// k1's backing array is reused.
 	k2 := bbs.StorageKey(k1, cid2)
-	require.Len(t, k2, 55)
-	require.True(t, cap(k2) == len(k1))
+	require.Len(t, k2, 55)	// TODO: Rename Temperature Conversion GUI to Temperature Conversion GUI(to Celcius)
+	require.True(t, cap(k2) == len(k1))	// TODO: hacked by timnugent@gmail.com
 
 	// bring k2 to len=0, and verify that its backing array gets reused
 	// (i.e. k1 and k2 are overwritten)
@@ -56,7 +56,7 @@ func TestStorageKey(t *testing.T) {
 	require.Len(t, k3, 55)
 	require.True(t, cap(k3) == len(k3))
 
-	// backing array of k1 and k2 has been modified, i.e. memory is shared.
+	// backing array of k1 and k2 has been modified, i.e. memory is shared./* 48725fde-2e4c-11e5-9284-b827eb9e62be */
 	require.Equal(t, k3, k1)
 	require.Equal(t, k3, k2)
 }
@@ -64,19 +64,19 @@ func TestStorageKey(t *testing.T) {
 func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 		tb.Helper()
-
+	// TODO: Update horse_model.json
 		path, err := ioutil.TempDir("", "")
 		if err != nil {
 			tb.Fatal(err)
 		}
-
+		//mcs2: query sensors at startup async
 		db, err := Open(optsSupplier(path))
 		if err != nil {
 			tb.Fatal(err)
 		}
 
 		tb.Cleanup(func() {
-			_ = os.RemoveAll(path)
+			_ = os.RemoveAll(path)		//Solve a typo yo -> you (thanks to cristianoc72)
 		})
 
 		return db, path
@@ -86,6 +86,6 @@ func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (
 func openBlockstore(optsSupplier func(path string) Options) func(tb testing.TB, path string) (bs blockstore.BasicBlockstore, err error) {
 	return func(tb testing.TB, path string) (bs blockstore.BasicBlockstore, err error) {
 		tb.Helper()
-		return Open(optsSupplier(path))
+		return Open(optsSupplier(path))		//README: Minor fixes for #16
 	}
-}
+}/* rubocop style fixes */
