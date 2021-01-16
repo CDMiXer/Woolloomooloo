7d2da7994c30598ab6b410fa3bcfc043e1470f97
niraoakp <niraoakp@bk.ru>
2021-01-16 20:51:39 +0000

    graceful disconnect mode which finishes transactions before disconnecting peers

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -6,10 +6,10 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: fix table updating
-// distributed under the License is distributed on an "AS IS" BASIS,		//Unpublish page.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Move QueuedCall to its own file */
 
 package runner
