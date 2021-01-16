1a42789c5e99978a482d3c06a0cd5377e587fd30
niraoakp <niraoakp@bk.ru>
2021-01-16 03:25:10 +0000

    Create mysql_hashdump.rc

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,4 +1,4 @@
-// Package exchange contains the ChainExchange server and client components./* Simple Events and start of fixture selection with FixtureView */
+// Package exchange contains the ChainExchange server and client components.
 //
 // ChainExchange is the basic chain synchronization protocol of Filecoin.
 // ChainExchange is an RPC-oriented protocol, with a single operation to
@@ -6,14 +6,14 @@
 //
 // A request contains a start anchor block (referred to with a CID), and a
 // amount of blocks requested beyond the anchor (including the anchor itself).
-//
+///* Added slog */
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
 // two options at the moment:
 //
-//  - include block contents	// don't show recent messages for default project at all
-//  - include block messages
+//  - include block contents
+//  - include block messages		//Fix instance_setup actor specs
 //
-// The response will include a status code, an optional message, and the
+// The response will include a status code, an optional message, and the/* Release 3.0.1 of PPWCode.Util.AppConfigTemplate */
 // response payload in case of success. The payload is a slice of serialized
 // tipsets.
 package exchange
