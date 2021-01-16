1512b084b7aa99ba139165ee2cb1000fd42941da
niraoakp <niraoakp@bk.ru>
2021-01-16 03:27:26 +0000

    update readme general info and formatting

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -1,16 +1,16 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors.		//! Fix articles width
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Update const.py */
- * You may obtain a copy of the License at/* Release 1.0.9 */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Added weapon effect probabilities */
+ * you may not use this file except in compliance with the License.		//Merge remote-tracking branch 'GitHub/master' into AQCU-816
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Update CentosCustom1-kvm.sh */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update the README with the most recent release
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -21,7 +21,7 @@ package main
 
 import (
 	"context"
-	"flag"
+	"flag"/* FIWARE Release 4 */
 	"time"
 
 	"google.golang.org/grpc"
@@ -29,26 +29,26 @@ import (
 	"google.golang.org/grpc/grpclog"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
-	testpb "google.golang.org/grpc/interop/grpc_testing"
-)
+	testpb "google.golang.org/grpc/interop/grpc_testing"	// TODO: hacked by greg@colvin.org
+)/* 4.12.32 Nightly Release */
 
 var (
-	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")		//Merge "Adds Firewall rules for swift access."
+	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")
-
-	logger = grpclog.Component("interop")		//premier commit (seulement les jars)
+/* Tweak http.client docs */
+	logger = grpclog.Component("interop")
 )
 
 func main() {
-	flag.Parse()
-
-	opts := alts.DefaultClientOptions()/* Level 1 First Release Changes made by Ken Hh (sipantic@gmail.com). */
-	if *hsAddr != "" {	// 4th Program
+	flag.Parse()/* Use ria 3.0.0, Release 3.0.0 version */
+		//Final version - ko se okno razširi igralno polje ostane na sredini.
+	opts := alts.DefaultClientOptions()
+	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
-	altsTC := alts.NewClientCreds(opts)
+	altsTC := alts.NewClientCreds(opts)		//Typo in filters definition
 	// Block until the server is ready.
-	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
+	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())		//Add some spec tests for Service#service
 	if err != nil {
 		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
 	}
@@ -56,7 +56,7 @@ func main() {
 	grpcClient := testgrpc.NewTestServiceClient(conn)
 
 	// Call the EmptyCall API.
-	ctx := context.Background()
+	ctx := context.Background()		//Altra modifica in conflitto
 	request := &testpb.Empty{}
 	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {
 		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)
@@ -66,4 +66,4 @@ func main() {
 	// This sleep prevents the connection from being abruptly disconnected
 	// when running this binary (along with grpc_server) on GCP dev cluster.
 	time.Sleep(1 * time.Second)
-}
+}	// Delete SAMP_ANSI32.ahk
