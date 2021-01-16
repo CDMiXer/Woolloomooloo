e8f97e19e852f7a2215af6c8b7e76a421b58d69c
niraoakp <niraoakp@bk.ru>
2021-01-16 23:11:58 +0000

    Refactoring: Better naming of test entities

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,19 +1,19 @@
 package test
 
-import (
-	"context"
+import (/* Release v0.5.1 -- Bug fixes */
+	"context"	// fix crash when email is undefined
 	"sync"
-	// TODO: Merge "Move description of how to boot instance with ISO to user-guide"
-	"github.com/filecoin-project/go-address"	// TODO: blog matter completed
+/* silly things preventing builds on newer PIC */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: pty: first draft of pty_read (master)
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
-	// TODO: Sync multiple cursor animations
-type MockAPI struct {
-	bs blockstore.Blockstore
-		//hide nginx and php version
+	// Remove IntelliJ files
+type MockAPI struct {		//Windows binary  created by pyinstaller
+	bs blockstore.Blockstore/* Remove as requested */
+
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
@@ -26,39 +26,39 @@ func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	}
 }
 
-func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {		//empty title for zenity
-	return m.bs.Has(c)	// TODO: will be fixed by lexy8russo@outlook.com
+func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
+	return m.bs.Has(c)
 }
 
-func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {		//rev 862647
+func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
 	if err != nil {
-		return nil, xerrors.Errorf("blockstore get: %w", err)		//hsieh hsin han
+		return nil, xerrors.Errorf("blockstore get: %w", err)
 	}
 
 	return blk.RawData(), nil
 }
-		//Delete fix.yml
-func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* Create BisherigerDeckbildungsprozess */
+/* adapt readme.md */
+func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
 	m.stateGetActorCalled++
 	return m.ts[tsk], nil
-}/* https://pt.stackoverflow.com/q/45347/101 */
-
+}
+/* b92a7da6-2e51-11e5-9284-b827eb9e62be */
 func (m *MockAPI) StateGetActorCallCount() int {
+	m.lk.Lock()/* Merge "[FEATURE] Glob-184 - String.prototype.normalize() for Filter" */
+	defer m.lk.Unlock()
+		//Add site plugin
+	return m.stateGetActorCalled/* FXML updates for hash view */
+}
+	// Update URL for "Need your API key?" link
+func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-/* Add an option to configure currency symbol format */
-	return m.stateGetActorCalled
-}	// thread/Util: no ioprio_set() on Android due to seccomp/SIGSYS
 
-func (m *MockAPI) ResetCallCounts() {
-	m.lk.Lock()		//Update GameRunnable.java
-	defer m.lk.Unlock()
-
-	m.stateGetActorCalled = 0/* Library to attach email events */
+	m.stateGetActorCalled = 0
 }
 
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
