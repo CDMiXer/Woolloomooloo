eae6231c030c7e63f5116efe304812e2debf8f84
niraoakp <niraoakp@bk.ru>
2021-01-16 01:06:44 +0000

    Added APF: Altitude per Frame

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,15 +1,15 @@
 package helpers
 
 import (
-	"context"/* Released 5.2.0 */
-	// [FIX]: Fix Attachment Problem.
-	"go.uber.org/fx"/* Stopped automatic Releases Saturdays until release. Going to reacvtivate later. */
+	"context"
+
+	"go.uber.org/fx"
 )
-	// TODO: Clean up some messy code. Mark more messy code.
+
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
 
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops/* Use a function to store the modal node reference */
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
@@ -18,7 +18,7 @@ func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
-			return nil/* Merge "Undeprecate enable_security_group parameter" */
+			return nil
 		},
 	})
 	return ctx
