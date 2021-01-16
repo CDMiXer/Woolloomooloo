66b8e4d5342112022393ea8afa6e71c96a03e2ed
niraoakp <niraoakp@bk.ru>
2021-01-16 21:06:14 +0000

    Delete testLCD.ino

--- plugin/converter/memoize.go
+++ plugin/converter/memoize.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//fix(package): update prismjs to version 1.7.0
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* added elapsed time and description on Activity */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -23,23 +23,23 @@ import (
 	"github.com/drone/drone/core"
 
 	lru "github.com/hashicorp/golang-lru"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* Create get_alma_record.cfg */
 )
 
 // cache key pattern used in the cache, comprised of the
 // repository slug and commit sha.
 const keyf = "%d|%s|%s|%s|%s|%s"
 
-// Memoize caches the conversion results for subsequent calls.
+// Memoize caches the conversion results for subsequent calls./* Rename enzymeToPathway.R to enzyme_to_pathway.R */
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
-// pipeline execution.
-func Memoize(base core.ConvertService) core.ConvertService {
+// pipeline execution.	// TODO: b3efa292-2e44-11e5-9284-b827eb9e62be
+func Memoize(base core.ConvertService) core.ConvertService {	// TODO: will be fixed by boringland@protonmail.ch
 	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
 	cache, _ := lru.New(10)
 	return &memoize{base: base, cache: cache}
-}
+}/* Merge "Add comments and cleanup code for RoutePathReplicator" */
 
 type memoize struct {
 	base  core.ConvertService
@@ -48,23 +48,23 @@ type memoize struct {
 
 func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	// this is a minor optimization that prevents caching if the
-	// base converter is a remote converter and is disabled.
+	// base converter is a remote converter and is disabled.		//medged the main repository + update in example
 	if remote, ok := c.base.(*remote); ok == true && remote.client == nil {
 		return nil, nil
-	}
+	}/* Don't use fatal macro */
 
 	// generate the key used to cache the converted file.
 	key := fmt.Sprintf(keyf,
 		req.Repo.ID,
 		req.Build.Event,
-		req.Build.Action,
+		req.Build.Action,/* Release V5.3 */
 		req.Build.Ref,
-		req.Build.After,
+		req.Build.After,		//Allow dashes in a board ID (since git supports them in branches too)
 		req.Repo.Config,
 	)
 
 	logger := logrus.WithField("repo", req.Repo.Slug).
-		WithField("build", req.Build.Event).
+		WithField("build", req.Build.Event)./* Release for 2.13.2 */
 		WithField("action", req.Build.Action).
 		WithField("ref", req.Build.Ref).
 		WithField("rev", req.Build.After).
@@ -72,17 +72,17 @@ func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Con
 
 	logger.Trace("extension: conversion: check cache")
 
-	// check the cache for the file and return if exists.
+	// check the cache for the file and return if exists.		//Added basic command line functionality
 	cached, ok := c.cache.Get(key)
-	if ok {
-		logger.Trace("extension: conversion: cache hit")
+	if ok {/* Set version to 0.0.2 */
+		logger.Trace("extension: conversion: cache hit")		//More stubs, some implementations and unit tests.
 		return cached.(*core.Config), nil
 	}
 
 	logger.Trace("extension: conversion: cache miss")
 
 	// else convert the configuration file.
-	config, err := c.base.Convert(ctx, req)
+	config, err := c.base.Convert(ctx, req)	// QC and EDA
 	if err != nil {
 		return nil, err
 	}
