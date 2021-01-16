6f6627632e06bc9604379f09630b8fc5e6698f67
niraoakp <niraoakp@bk.ru>
2021-01-16 23:30:20 +0000

    Fix multiworld

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Releases 1.2.0 */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update layer-name.md
-// You may obtain a copy of the License at/* Released DirectiveRecord v0.1.10 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// TODO: cleaned up variable names
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by why@ipfs.io
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by alex.gaynor@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* send count display */
-// +build oss
-	// Fixed issue #680.
-package metric
 
+// +build oss
+
+package metric
+/* Base para clase Boton */
 import "github.com/drone/drone/core"
 
-func BuildCount(core.BuildStore)        {}	// TODO: will be fixed by 13860583249@yeah.net
-func PendingBuildCount(core.BuildStore) {}		//starting to move to a 50,50 center
+func BuildCount(core.BuildStore)        {}	// TODO: will be fixed by greg@colvin.org
+func PendingBuildCount(core.BuildStore) {}
 func RunningBuildCount(core.BuildStore) {}
-func RunningJobCount(core.StageStore)   {}/* reorder access to pages during scans */
+func RunningJobCount(core.StageStore)   {}
 func PendingJobCount(core.StageStore)   {}
 func RepoCount(core.RepositoryStore)    {}
 func UserCount(core.UserStore)          {}
