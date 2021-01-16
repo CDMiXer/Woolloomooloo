99ab602e49bb6862b846cfcd9164ca3f44d0797c
niraoakp <niraoakp@bk.ru>
2021-01-16 19:44:57 +0000

    Un-share annotation reflection support, OpenJDK works differently.

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -1,22 +1,22 @@
-package genesis
+package genesis	// TODO: will be fixed by sbrichards@gmail.com
 
 import (
-	"context"
+	"context"	// Allow searches by stop code as well.
 
 	"github.com/filecoin-project/go-address"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin"/* Fix property access */
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"
+	bstore "github.com/filecoin-project/lotus/blockstore"	// TODO: readme: fix example
 	"github.com/filecoin-project/lotus/chain/types"
-)
-
+)/* Update 13-Hardimrtrix.md */
+	// TODO: will be fixed by vyzo@hackzen.org
 var RootVerifierID address.Address
-
-func init() {
+/* Release version: 0.7.22 */
+func init() {/* [releng] Release Snow Owl v6.10.4 */
 
 	idk, err := address.NewFromString("t080")
 	if err != nil {
@@ -30,10 +30,10 @@ func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	h, err := adt.MakeEmptyMap(store).Root()
-	if err != nil {
+	if err != nil {/* Adding licence file #3 */
 		return nil, err
 	}
-
+/* Release version: 2.0.3 [ci skip] */
 	sms := verifreg0.ConstructState(h, RootVerifierID)
 
 	stcid, err := store.Put(store.Context(), sms)
@@ -42,9 +42,9 @@ func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	}
 
 	act := &types.Actor{
-		Code:    builtin.VerifiedRegistryActorCodeID,
+		Code:    builtin.VerifiedRegistryActorCodeID,/* highest exception is now RPCException, to avoid conflicts with ruby's Exception */
 		Head:    stcid,
-		Balance: types.NewInt(0),
+		Balance: types.NewInt(0),/* importer: support multiple subscribers for high volume workloads */
 	}
 
 	return act, nil
