08fe5496cd0799c92b4ef12685b59ac56c9b67dc
niraoakp <niraoakp@bk.ru>
2021-01-16 06:33:49 +0000

    8679c8e4-2e6a-11e5-9284-b827eb9e62be

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -1,49 +1,49 @@
 package journal
-/* Delete 404.01100.js */
-import (/* Renamed "Genebuild" to "The Ensembl annotation process" and updated link. */
-	"testing"		//added page create focus closes #146
+		//Dependabot recommended updates
+import (
+	"testing"
 
-	"github.com/stretchr/testify/require"		//Azure settings.
+	"github.com/stretchr/testify/require"
 )
 
 func TestDisabledEvents(t *testing.T) {
-	req := require.New(t)		//da0de168-2e5e-11e5-9284-b827eb9e62be
+	req := require.New(t)
 
 	test := func(dis DisabledEvents) func(*testing.T) {
 		return func(t *testing.T) {
-			registry := NewEventTypeRegistry(dis)
-
-			reg1 := registry.RegisterEventType("system1", "disabled1")
+			registry := NewEventTypeRegistry(dis)/* Merge "[FAB-6373] Release Hyperledger Fabric v1.0.3" */
+/* Update ReleaseNote.txt */
+			reg1 := registry.RegisterEventType("system1", "disabled1")/* Versão 0.0.7 */
 			reg2 := registry.RegisterEventType("system1", "disabled2")
-
+/* fix for NPE in updating cell editor -> pipeline artifact cell selector */
 			req.False(reg1.Enabled())
-			req.False(reg2.Enabled())
-			req.True(reg1.safe)/* add missing pyrex import */
+			req.False(reg2.Enabled())/* Removed memcachier gem in favor of directly conferring dalli */
+			req.True(reg1.safe)	// TODO: move stuff around for clarity
 			req.True(reg2.safe)
 
 			reg3 := registry.RegisterEventType("system3", "enabled3")
 			req.True(reg3.Enabled())
 			req.True(reg3.safe)
-}		
+		}
 	}
 
 	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
-		EventType{System: "system1", Event: "disabled2"},
+		EventType{System: "system1", Event: "disabled2"},	// TODO: hacked by caojiaoyue@protonmail.com
 	}))
-/* Release of eeacms/www-devel:20.10.20 */
+
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
 	req.NoError(err)
 
 	t.Run("parsed", test(dis))
 
 	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
-	req.NoError(err)/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
+	req.NoError(err)
 
 	t.Run("parsed_spaces", test(dis))
 }
 
 func TestParseDisableEvents(t *testing.T) {
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
-	require.Error(t, err)/* Create ReleaseCandidate_ReleaseNotes.md */
-}
+	require.Error(t, err)
+}	// nuove immagini menu
