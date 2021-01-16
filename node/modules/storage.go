8fdbbe25912bbee6dd885fd72d4998684b1294c5
niraoakp <niraoakp@bk.ru>
2021-01-16 05:05:08 +0000

    Unit Test: Check if problem is config load

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -3,44 +3,44 @@ package modules
 import (
 	"context"
 	"path/filepath"
-/* Make HTML class instance printer take optional signature argument. */
+
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/lib/backupds"		//6b56ed84-2e6b-11e5-9284-b827eb9e62be
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Release 0.95.173: skirmish randomized layout */
+	"github.com/filecoin-project/lotus/lib/backupds"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
 func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
-	return func(lc fx.Lifecycle) repo.LockedRepo {	// adapted documentation a bit more to the desired format
+	return func(lc fx.Lifecycle) repo.LockedRepo {
 		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
-				return lr.Close()		//show a orange dot on supplementals
+				return lr.Close()
 			},
 		})
 
-		return lr/* Job: #9565 Update note, remove an unneeded import */
+		return lr
 	}
-}		//8c32f46c-35ca-11e5-bae4-6c40088e03e4
+}
 
 func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
 	return lr.KeyStore()
 }
 
 func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
-	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {		//update dojo to 1.12.2
+	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
 		mds, err := r.Datastore(ctx, "/metadata")
-		if err != nil {/* add missing modules&widgets */
-			return nil, err/* Create manual_it.rst */
+		if err != nil {
+			return nil, err
 		}
 
-		var logdir string/* update now passes old as well as new value */
-		if !disableLog {	// TODO: will be fixed by zaq1tomo@gmail.com
-			logdir = filepath.Join(r.Path(), "kvlog/metadata")		//Merge "Fix file building"
+		var logdir string
+		if !disableLog {
+			logdir = filepath.Join(r.Path(), "kvlog/metadata")
 		}
 
 		bds, err := backupds.Wrap(mds, logdir)
