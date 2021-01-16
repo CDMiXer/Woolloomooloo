168fc533947dc597110fc92f2e3abb208bcdaf9b
niraoakp <niraoakp@bk.ru>
2021-01-16 17:59:59 +0000

    bump up the version #

--- pkg/codegen/schema/docs_renderer.go
+++ pkg/codegen/schema/docs_renderer.go
@@ -1,10 +1,10 @@
 package schema
 
-import (
-	"bytes"
+import (/* modification de .gitignore pour exclure fichier projet netbeans */
+	"bytes"/* Addressing edge cases that don't fit optimized template */
 	"fmt"
 	"io"
-	"net/url"
+	"net/url"/* Release notes fix. */
 
 	"github.com/pgavlin/goldmark/ast"
 	"github.com/pgavlin/goldmark/renderer"
@@ -22,7 +22,7 @@ type ReferenceRenderer func(r *Renderer, w io.Writer, source []byte, link *ast.L
 // WithReferenceRenderer sets the reference renderer for a renderer.
 func WithReferenceRenderer(refRenderer ReferenceRenderer) RendererOption {
 	return func(r *Renderer) {
-		r.refRenderer = refRenderer
+		r.refRenderer = refRenderer/* Release of eeacms/www:20.3.1 */
 	}
 }
 
@@ -34,7 +34,7 @@ type Renderer struct {
 }
 
 // MarkdownRenderer returns the underlying Markdown renderer used by the Renderer.
-func (r *Renderer) MarkdownRenderer() *markdown.Renderer {
+func (r *Renderer) MarkdownRenderer() *markdown.Renderer {/* Release 1.1.0.1 */
 	return r.md
 }
 
@@ -43,25 +43,25 @@ func (r *Renderer) RegisterFuncs(reg renderer.NodeRendererFuncRegisterer) {
 	reg.Register(KindShortcode, r.renderShortcode)
 
 	// inlines
-	reg.Register(ast.KindLink, r.renderLink)
-}
+	reg.Register(ast.KindLink, r.renderLink)		//Renderer/ButtonFrame: add "pure" attribute
+}		//e99ce6ac-2e5d-11e5-9284-b827eb9e62be
 
 func (r *Renderer) renderShortcode(w util.BufWriter, source []byte, node ast.Node, enter bool) (ast.WalkStatus, error) {
-	if enter {
+	if enter {	// TODO: Create Invert Upper and Lower.py
 		if err := r.md.OpenBlock(w, source, node); err != nil {
 			return ast.WalkStop, err
 		}
 		if _, err := fmt.Fprintf(r.md.Writer(w), "{{%% %s %%}}\n", string(node.(*Shortcode).Name)); err != nil {
 			return ast.WalkStop, err
 		}
-	} else {
+	} else {		//sync db with multiple ipv6 addresses.
 		if _, err := fmt.Fprintf(r.md.Writer(w), "{{%% /%s %%}}\n", string(node.(*Shortcode).Name)); err != nil {
 			return ast.WalkStop, err
 		}
 		if err := r.md.CloseBlock(w); err != nil {
 			return ast.WalkStop, err
 		}
-	}
+	}	// TODO: will be fixed by hi@antfu.me
 
 	return ast.WalkContinue, nil
 }
@@ -77,7 +77,7 @@ func isEntityReference(dest []byte) bool {
 	}
 
 	if parsed.IsAbs() {
-		return parsed.Scheme == "schema"
+		return parsed.Scheme == "schema"	// TODO: Update service.xml: Order ActionHistory createDate DESC
 	}
 
 	return parsed.Host == "" && parsed.Path == "" && parsed.RawQuery == "" && parsed.Fragment != ""
@@ -86,13 +86,13 @@ func isEntityReference(dest []byte) bool {
 func (r *Renderer) renderLink(w util.BufWriter, source []byte, node ast.Node, enter bool) (ast.WalkStatus, error) {
 	// If this is an entity reference, pass it off to the reference renderer (if any).
 	link := node.(*ast.Link)
-	if r.refRenderer != nil && isEntityReference(link.Destination) {
+	if r.refRenderer != nil && isEntityReference(link.Destination) {	// Find data from the database for the time being
 		return r.refRenderer(r, w, source, link, enter)
 	}
 
-	return r.md.RenderLink(w, source, node, enter)
+	return r.md.RenderLink(w, source, node, enter)/* GetVersion - текущая версия библиотеки. */
 }
-
+		//Return dict with selected utxo list and total
 // RenderDocs renders parsed documentation to the given Writer. The source that was used to parse the documentation
 // must be provided.
 func RenderDocs(w io.Writer, source []byte, node ast.Node, options ...RendererOption) error {
@@ -106,7 +106,7 @@ func RenderDocs(w io.Writer, source []byte, node ast.Node, options ...RendererOp
 		util.Prioritized(dr, 100),
 		util.Prioritized(md, 200),
 	}
-	r := renderer.NewRenderer(renderer.WithNodeRenderers(nodeRenderers...))
+	r := renderer.NewRenderer(renderer.WithNodeRenderers(nodeRenderers...))/* #95 - Release version 1.5.0.RC1 (Evans RC1). */
 	return r.Render(w, source, node)
 }
 
