3ed6879a1b0ea64741c914e14fd73535ded71c07
niraoakp <niraoakp@bk.ru>
2021-01-16 14:40:26 +0000

    Merge branch 'master' into use-npm

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 2.5.8: update sitemap */
-// +build !oss
 
+sso! dliub+ //
+		//verify that volume paths are absolute
 package machine
