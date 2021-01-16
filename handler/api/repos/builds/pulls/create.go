a731da894069c05f7f9c0edc16b552440ca69f8e
niraoakp <niraoakp@bk.ru>
2021-01-16 16:04:33 +0000

    Update content.mustache

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//		//c83bd850-2e4a-11e5-9284-b827eb9e62be
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release Notes for v02-09 */
-// you may not use this file except in compliance with the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 1.0.0.86 QCACLD WLAN Driver" */
+// you may not use this file except in compliance with the License./* @Release [io7m-jcanephora-0.20.0] */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Ignore PyCharm project files
-// See the License for the specific language governing permissions and	// TODO: Create deleteallwebs.ps1
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* [MERGE] Merge openerp-web in mobile. */
 // limitations under the License.
 
-package pulls
+package pulls/* Release 0.0.26 */
