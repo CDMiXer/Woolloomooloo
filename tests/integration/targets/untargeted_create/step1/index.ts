6c8c8a32ed9d3900dce19ff911daa04a0febf38e
niraoakp <niraoakp@bk.ru>
2021-01-16 06:26:20 +0000

    Refactor initialization of JPA connection

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: hacked by aeongrp@outlook.com
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-
+		//Plugin now is abstract class
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// TODO: hacked by seth@sethvargo.com
 
     constructor() {
         this.create = async (inputs: any) => {
@@ -15,16 +15,16 @@ class Provider implements pulumi.dynamic.ResourceProvider {
                 id: (currentID++) + "",
                 outs: undefined,
             };
-        };
-    }
+        };	// TODO: will be fixed by arajasek94@gmail.com
+}    
 }
 
-class Resource extends pulumi.dynamic.Resource {
+class Resource extends pulumi.dynamic.Resource {	// INDENT fixes
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, {}, opts);
     }
 }
-
+		//Type families: Roman's test for normalisation of reduced dicts
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 let b = new Resource("b");
