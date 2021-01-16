c486d49c8657e908f4c307bcc062fe6293d94fef
niraoakp <niraoakp@bk.ru>
2021-01-16 19:38:02 +0000

    b7c9462e-2e4a-11e5-9284-b827eb9e62be

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -3,28 +3,28 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Added overflow handling to highlighter
-//     http://www.apache.org/licenses/LICENSE-2.0
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: Added js files
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Debug code. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// [#43265783] Add the project create form
-package filestate/* Release of eeacms/forests-frontend:1.9-prod.0 */
 
-import (		//Create delayed_job.markdown
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"		//Updating GBP from PR #57849 [ci skip]
+package filestate
+
+import (		//Memory leaks fix / code cleanup
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
-// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
+// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots/* DATASOLR-157 - Release version 1.2.0.RC1. */
 // to disk on the local machine.
 type localSnapshotPersister struct {
 	name    tokens.QName
-	backend *localBackend		//[hotfix][build] Remove reference to scala-2.11 profile
+	backend *localBackend
 	sm      secrets.Manager
 }
 
@@ -33,8 +33,8 @@ func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 }
 
 func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
-	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)/* Purge UPDATE, HungerGames UPDATE (*EntityCombustByBlockEvent Added*) */
-	return err
+	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)
+	return err		//Delete ListaEchipamente.txt
 
 }
 
