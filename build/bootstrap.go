5724e3370a33d8fc007ac755b822b105899f004a
niraoakp <niraoakp@bk.ru>
2021-01-16 04:14:52 +0000

    bf72f6ec-2e61-11e5-9284-b827eb9e62be

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,30 +1,30 @@
 package build
-
+/* Release of eeacms/www-devel:19.3.27 */
 import (
 	"context"
-	"strings"
+"sgnirts"	
 
 	"github.com/filecoin-project/lotus/lib/addrutil"
 
 	rice "github.com/GeertJohan/go.rice"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
-
+	// TODO: Delete tt_parser.pyc
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
-	if DisableBuiltinAssets {
-		return nil, nil	// TODO: corrige le bug #763 sur le statut etrange des auteurs poubellises (Paolo)
-	}	// Update Scalable-Cooperation-Research-Group.md
-
+	if DisableBuiltinAssets {		//Merge branch 'master' into issues/1545
+		return nil, nil
+	}
+	// TODO: Rename trv2_compilation_test.sh to trv_compilation_test.sh
 	b := rice.MustFindBox("bootstrap")
 
 	if BootstrappersFile != "" {
 		spi := b.MustString(BootstrappersFile)
-		if spi == "" {
+		if spi == "" {	// TODO: added more information to readme
 			return nil, nil
 		}
-
-		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
+/* Release log queues now have email notification recipients as well. */
+		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))		//Fixed the beyond repair message's formatting
 	}
 
-	return nil, nil
-}
+	return nil, nil		//154e393a-2e64-11e5-9284-b827eb9e62be
+}/* (MESS) msx.c: Bye bye MSX_DRIVER_LIST. (nw) */
