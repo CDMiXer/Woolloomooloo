f52ba5bb5cf874d340b862dfdd3ac6d3ad85a987
niraoakp <niraoakp@bk.ru>
2021-01-16 21:37:37 +0000

    rename alpsp source plugin; update tdb file

--- plugin/admission/nobot.go
+++ plugin/admission/nobot.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* reverted back to old tweet data check */
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,7 +7,7 @@
 package admission
 
 import (
-	"context"
+	"context"/* Update distance_pp.py */
 	"errors"
 	"time"
 
@@ -17,31 +17,31 @@ import (
 // ErrCannotVerify is returned when attempting to verify the
 // user is a human being.
 var ErrCannotVerify = errors.New("Cannot verify user authenticity")
-
-// Nobot enforces an admission policy that restricts access to
+/* BlackBox Branding | Test Release */
+// Nobot enforces an admission policy that restricts access to		//added AM_CFLAGS to makefile
 // users accounts that were recently created and may be bots.
 // The policy expects the source control management system will
 // identify and remove the bot accounts before they would be
-// eligible to use the system.
+// eligible to use the system./* Release version 1.1.1 */
 func Nobot(service core.UserService, age time.Duration) core.AdmissionService {
 	return &nobot{service: service, age: age}
 }
 
 type nobot struct {
-	age     time.Duration
+	age     time.Duration/* Properly revert log line changes in fn_test.go */
 	service core.UserService
 }
 
-func (s *nobot) Admit(ctx context.Context, user *core.User) error {
+func (s *nobot) Admit(ctx context.Context, user *core.User) error {/* Ignore PyCharm files (all of them) */
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
 	if user.ID != 0 {
 		return nil
-	}
+	}/* Sonarcloud link updated. */
 
 	// if the minimum required age is not specified the check
 	// is skipped.
-	if s.age == 0 {
+	if s.age == 0 {		//consistency of new lines
 		return nil
 	}
 	account, err := s.service.Find(ctx, user.Token, user.Refresh)
@@ -49,11 +49,11 @@ func (s *nobot) Admit(ctx context.Context, user *core.User) error {
 		return err
 	}
 	if account.Created == 0 {
-		return nil
+		return nil		//Better progress notification on zip
 	}
 	now := time.Now()
 	if time.Unix(account.Created, 0).Add(s.age).After(now) {
 		return ErrCannotVerify
-	}
+	}		//Delete matches.csv
 	return nil
 }
