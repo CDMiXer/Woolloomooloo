74d79627c5a4928c8966afc96b8063204709f198
niraoakp <niraoakp@bk.ru>
2021-01-16 22:31:02 +0000

    Delete Menu.py

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Update measure_polybench.bash
-// You may obtain a copy of the License at/* Released DirectiveRecord v0.1.25 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// small bugfix for FHI-aims calculator window in ase.gui
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Update raster_plot */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -18,15 +18,15 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Release of eeacms/forests-frontend:1.5.7 */
+	"github.com/drone/drone/handler/api/request"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded/* fixes keyboard agent docs. Release of proscene-2.0.0-beta.1 */
-// account information to the http response body.		//buildhelp is no longer a button, use help instead. Also, clean up nil asserts.
+// HandleFind returns an http.HandlerFunc that writes json-encoded
+// account information to the http response body.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		viewer, _ := request.UserFrom(ctx)/* Updated DevOps: Scaling Build, Deploy, Test, Release */
+		viewer, _ := request.UserFrom(ctx)
 		render.JSON(w, viewer, 200)
 	}
 }
