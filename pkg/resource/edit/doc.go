6e7d9957dde5eb9cd420d0e66f962bf05676433f
niraoakp <niraoakp@bk.ru>
2021-01-16 06:09:33 +0000

    Centering the "get started" link.

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* [*] BO : new help section and some wording for the Carriers page. */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by seth@sethvargo.com
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create patches.rb */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-	// TODO: delete redundant comment
-// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
+// limitations under the License./* Merge "Release 3.0.10.034 Prima WLAN Driver" */
+
+// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level	// TODO: added Português (Brasil)
 // tools that present a means for users to surgically edit their state.
 package edit
