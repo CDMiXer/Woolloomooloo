70c9a503735e90a4cb9dba0c6bb46b5a763bb873
niraoakp <niraoakp@bk.ru>
2021-01-16 18:38:16 +0000

    update codeclimate config file

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* planejador ok */
+// that can be found in the LICENSE file.
 
-// +build !oss/* T2Rj5orLsBVR8gDXkA3BccWUsknjgIkY */
-
+// +build !oss
+		//Size change of IoT picture
 package validator
