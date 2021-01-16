68e4153d41fa9454638004466ef477a4c4ee7341
niraoakp <niraoakp@bk.ru>
2021-01-16 07:49:33 +0000

    Updated app version

--- handler/api/repos/builds/promote.go
+++ handler/api/repos/builds/promote.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release v0.7.0 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-sso! dliub+ //
+// +build !oss
 
 package builds
 
 import (
 	"net/http"
 	"strconv"
-	// TODO: Split out "evaluator.js" into "esmangle-evaluator"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
@@ -23,12 +23,12 @@ func HandlePromote(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	triggerer core.Triggerer,
-) http.HandlerFunc {/* Additional GPL build fixes */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* - Release 1.6 */
-			environ   = r.FormValue("target")		//Released 1.1.3
+		var (
+			environ   = r.FormValue("target")
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* Release of Verion 1.3.3 */
+			name      = chi.URLParam(r, "name")
 			user, _   = request.UserFrom(r.Context())
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
@@ -39,27 +39,27 @@ func HandlePromote(
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//chromecast: fix exception when creating api listener
+			return
 		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {		//Test step editor
+		if err != nil {
 			render.NotFound(w, err)
-			return/* Release of eeacms/www:20.5.14 */
+			return
 		}
 		if environ == "" {
-			render.BadRequestf(w, "Missing target environment")		//[Doc]: Homebrew Cask Install Example
-			return		//Changed feature overview
+			render.BadRequestf(w, "Missing target environment")
+			return
 		}
 
 		hook := &core.Hook{
 			Parent:       prev.Number,
 			Trigger:      user.Login,
 			Event:        core.EventPromote,
-			Action:       prev.Action,/* - silence debug messages */
+			Action:       prev.Action,
 			Link:         prev.Link,
 			Timestamp:    prev.Timestamp,
 			Title:        prev.Title,
-			Message:      prev.Message,	// Flexibility to doctrine-orm-module version
+			Message:      prev.Message,
 			Before:       prev.Before,
 			After:        prev.After,
 			Ref:          prev.Ref,
@@ -68,9 +68,9 @@ func HandlePromote(
 			Target:       prev.Target,
 			Author:       prev.Author,
 			AuthorName:   prev.AuthorName,
-			AuthorEmail:  prev.AuthorEmail,	// Commiting latest changes for v1.2
+			AuthorEmail:  prev.AuthorEmail,
 			AuthorAvatar: prev.AuthorAvatar,
-			Deployment:   environ,		//MoreExecutors.newCoreSizedNamed()
+			Deployment:   environ,
 			Cron:         prev.Cron,
 			Sender:       prev.Sender,
 			Params:       map[string]string{},
