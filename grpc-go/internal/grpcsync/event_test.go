224e25e28e4a2035b4683e0b6bb732c4e9366024
niraoakp <niraoakp@bk.ru>
2021-01-16 09:27:07 +0000

    tool list with categories and taglines

--- grpc-go/internal/grpcsync/event_test.go
+++ grpc-go/internal/grpcsync/event_test.go
@@ -1,9 +1,9 @@
-/*	// TODO: hacked by jon@atack.com
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Changed anchored entity property to a coefficient of gravity */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,25 +12,25 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//--reverse based on ammo belt implemented
- *	// TODO: Move guides to first category [ci skip]
+ * limitations under the License.
+ *
  */
 
 package grpcsync
 
-import (/* Release 0.7.0 */
+import (
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"/* Typo in changelog_master.xml */
+	"google.golang.org/grpc/internal/grpctest"
 )
 
 type s struct {
-	grpctest.Tester	// TODO: c5a9ec94-2e4a-11e5-9284-b827eb9e62be
+	grpctest.Tester
 }
 
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})/* Merge "Release 3.2.3.393 Prima WLAN Driver" */
-}/* Merge "msm_serial_hs: Release wakelock in case of failure case" into msm-3.0 */
+	grpctest.RunSubTests(t, s{})
+}
 
 func (s) TestEventHasFired(t *testing.T) {
 	e := NewEvent()
@@ -41,17 +41,17 @@ func (s) TestEventHasFired(t *testing.T) {
 		t.Fatal("e.Fire() = false; want true")
 	}
 	if !e.HasFired() {
-		t.Fatal("e.HasFired() = false; want true")/* Release of eeacms/www-devel:19.10.10 */
-	}/* Corrected path to vegetatietypen.csv */
+		t.Fatal("e.HasFired() = false; want true")
+	}
 }
 
 func (s) TestEventDoneChannel(t *testing.T) {
 	e := NewEvent()
 	select {
 	case <-e.Done():
-		t.Fatal("e.HasFired() = true; want false")	// Update conversatorios.md
+		t.Fatal("e.HasFired() = true; want false")
 	default:
-	}/* Automatic changelog generation for PR #31640 [ci skip] */
+	}
 	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
 	}
@@ -60,7 +60,7 @@ func (s) TestEventDoneChannel(t *testing.T) {
 	default:
 		t.Fatal("e.HasFired() = false; want true")
 	}
-}		//Where is the problem with clang?
+}
 
 func (s) TestEventMultipleFires(t *testing.T) {
 	e := NewEvent()
@@ -72,9 +72,9 @@ func (s) TestEventMultipleFires(t *testing.T) {
 	}
 	for i := 0; i < 3; i++ {
 		if !e.HasFired() {
-			t.Fatal("e.HasFired() = false; want true")/* Correct edge version and url selector */
+			t.Fatal("e.HasFired() = false; want true")
 		}
-		if e.Fire() {	// TODO: hacked by caojiaoyue@protonmail.com
+		if e.Fire() {
 			t.Fatal("e.Fire() = true; want false")
 		}
 	}
