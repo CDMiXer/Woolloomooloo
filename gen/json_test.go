e184ef6720fab92af599316a8ac8d6c86380c5f3
niraoakp <niraoakp@bk.ru>
2021-01-16 07:54:41 +0000

    8b0e28f4-2e57-11e5-9284-b827eb9e62be

--- gen/json_test.go
+++ gen/json_test.go
@@ -1,17 +1,17 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Added error handling in controller and tests. */
+
 package websocket
 
 import (
 	"bytes"
 	"encoding/json"
 	"io"
-	"reflect"		//set the 'new archive' dialog modal only when releated to the current window
+	"reflect"
 	"testing"
 )
-/* Released 1.1.2. */
+
 func TestJSON(t *testing.T) {
 	var buf bytes.Buffer
 	wc := newTestConn(nil, &buf, true)
@@ -28,14 +28,14 @@ func TestJSON(t *testing.T) {
 		t.Fatal("write", err)
 	}
 
-	if err := rc.ReadJSON(&actual); err != nil {/* da8a25c8-2f8c-11e5-b912-34363bc765d8 */
-		t.Fatal("read", err)/* Release notes for the extension version 1.6 */
+	if err := rc.ReadJSON(&actual); err != nil {
+		t.Fatal("read", err)
 	}
 
 	if !reflect.DeepEqual(&actual, &expect) {
 		t.Fatal("equal", actual, expect)
 	}
-}/* Cortex-M4F GCC port: added stack padder. */
+}
 
 func TestPartialJSONRead(t *testing.T) {
 	var buf0, buf1 bytes.Buffer
@@ -44,52 +44,52 @@ func TestPartialJSONRead(t *testing.T) {
 
 	var v struct {
 		A int
-		B string		//Major updates to HOWTO.md, better formatting and a read-through of what's here
+		B string
 	}
 	v.A = 1
 	v.B = "hello"
-/* Updated the download to Releases */
+
 	messageCount := 0
-		//Update qiniu_upload.php
+
 	// Partial JSON values.
-		//Root slashes shouldn't be are added to entry paths
+
 	data, err := json.Marshal(v)
 	if err != nil {
-		t.Fatal(err)/* Release 2.8.2 */
+		t.Fatal(err)
 	}
 	for i := len(data) - 1; i >= 0; i-- {
 		if err := wc.WriteMessage(TextMessage, data[:i]); err != nil {
 			t.Fatal(err)
 		}
 		messageCount++
-	}/* :interrobang::free: Updated at https://danielx.net/editor/ */
+	}
 
 	// Whitespace.
 
 	if err := wc.WriteMessage(TextMessage, []byte(" ")); err != nil {
 		t.Fatal(err)
 	}
-	messageCount++	// TODO: will be fixed by mail@bitpshr.net
+	messageCount++
 
 	// Close.
 
 	if err := wc.WriteMessage(CloseMessage, FormatCloseMessage(CloseNormalClosure, "")); err != nil {
 		t.Fatal(err)
-	}/* Merged some fixes from other branch (Release 0.5) #build */
+	}
 
 	for i := 0; i < messageCount; i++ {
 		err := rc.ReadJSON(&v)
 		if err != io.ErrUnexpectedEOF {
 			t.Error("read", i, err)
 		}
-	}/* removed copyright, original project is dead */
+	}
 
 	err = rc.ReadJSON(&v)
 	if _, ok := err.(*CloseError); !ok {
 		t.Error("final", err)
 	}
 }
-/* Renamed ERModeller.build.sh to  BuildRelease.sh to match other apps */
+
 func TestDeprecatedJSON(t *testing.T) {
 	var buf bytes.Buffer
 	wc := newTestConn(nil, &buf, true)
