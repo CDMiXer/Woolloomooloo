e329d12f60d7df7613b58ea4223c65c5fffee2d3
niraoakp <niraoakp@bk.ru>
2021-01-16 02:54:40 +0000

    Create command_remote.md

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -2,51 +2,51 @@ package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// TODO: More updated work on GPS.  Not ready yet.
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"/* Merge "[Release notes] Small changes in mitaka release notes" */
-	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"/* fixed tthe previous test case */
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
 )
 
-var _ State = (*state0)(nil)		//add enumerable examples
+var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* in examples, replace deprecated methods and classes */
-		return nil, err		//Add simple Election recipe and tests
+	err := store.Get(store.Context(), root, &out)/* NetKAN generated mods - BetterSRBs-1.2.3 */
+	if err != nil {
+		return nil, err
 	}
-	return &out, nil/* README update (Bold Font for Release 1.3) */
-}
-	// TODO: hacked by mikeal.rogers@gmail.com
-type state0 struct {
+	return &out, nil
+}		//[InstallerBundle] Fix setup command
+
+type state0 struct {/* Removed unnecessary dir commands */
 	reward0.State
-	store adt.Store
+	store adt.Store/* Release Notes: remove 3.3 HTML notes from 3.HEAD */
 }
 
-func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil/* [artifactory-release] Release version 2.0.0.M1 */
+func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {/* a new release.notes to summarize changes. */
+	return s.State.ThisEpochReward, nil
 }
 
 func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
-/* bump dependencies to latest */
+
 	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
 }
 
 func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
-	return s.State.ThisEpochBaselinePower, nil		//Fix NPE reference regarding config.
+	return s.State.ThisEpochBaselinePower, nil
 }
 
 func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
-	return s.State.TotalMined, nil	// TODO: LIB: Remove the concept of "generated" profiles
-}/* A quick revision for Release 4a, version 0.4a. */
+	return s.State.TotalMined, nil	// Update model
+}
 
-func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {		//Recoded to support platform factories
+func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
@@ -56,30 +56,30 @@ func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
-}	// 210d132c-2e5a-11e5-9284-b827eb9e62be
+}/* Release: 6.6.2 changelog */
 
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
-
+/* Fixed speedhack check and minor other stuff */
 func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner0.InitialPledgeForPower(
-		sectorWeight,
-		s.State.ThisEpochBaselinePower,
+		sectorWeight,/* No magic numbers */
+		s.State.ThisEpochBaselinePower,	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 		networkTotalPledge,
 		s.State.ThisEpochRewardSmoothed,
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},
+		},/* e9dfa80a-2e64-11e5-9284-b827eb9e62be */
 		circSupply), nil
 }
 
 func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
-	return miner0.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
+	return miner0.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,		//fixed concurrent puts to the same key.
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},
+		},/* Deleted CtrlApp_2.0.5/Release/ctrl_app.exe.intermediate.manifest */
 		sectorWeight), nil
 }
