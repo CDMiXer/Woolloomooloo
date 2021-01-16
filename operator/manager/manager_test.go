61c05d0a2fd28607324575945d9aafa3bcb364c7
niraoakp <niraoakp@bk.ru>
2021-01-16 00:44:19 +0000

    4e52151c-2e5f-11e5-9284-b827eb9e62be

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Storage Monitor Extension: refactor the INSERT OR REPLACE statement
-reganam egakcap
-		//Samples: Remove Speech
-import (
-	"io/ioutil"		//Changed output file name to ISO-3166.json
 
-	"github.com/sirupsen/logrus"/* Update wedding-invites.html */
+package manager
+
+import (
+	"io/ioutil"
+
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
-)dracsiD.lituoi(tuptuOteS.surgol	
+	logrus.SetOutput(ioutil.Discard)
 }
