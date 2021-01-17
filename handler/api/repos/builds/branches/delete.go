8150535b1e0b1e1371299e1f38e8eea3f14bdb72
niraoakp <niraoakp@bk.ru>
2021-01-17 11:45:59 +0000

    removed unused val

--- handler/api/repos/builds/branches/delete.go
+++ handler/api/repos/builds/branches/delete.go
@@ -1,62 +1,62 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Small fix to import statement
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Fixed ScrollTo script to work with # parameters
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Add Release Notes to the README */
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by why@ipfs.io
+//	// TODO: Remembering deps
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by arachnid@notdot.net
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package branches
 
 import (
-	"net/http"/* update doc for issue #4 */
-		//release v8.0.13
-	"github.com/drone/drone/core"
+	"net/http"
+
+	"github.com/drone/drone/core"		//i18n: jca and several small bugfixes
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
-/* 0.9.7 Release. */
-// HandleDelete returns an http.HandlerFunc that handles an
+
+// HandleDelete returns an http.HandlerFunc that handles an/* Update Arduino_ESP32.yml */
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Release of eeacms/www-devel:18.9.2 */
+	builds core.BuildStore,	// verify that volume paths are absolute
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			namespace = chi.URLParam(r, "owner")/* Primer Release */
+			name      = chi.URLParam(r, "name")		//Delete PaintTest.java
 			branch    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {/* service and test refactoring */
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace)./* Release RedDog demo 1.1.0 */
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return
+			return/* 4.1.6-beta 5 Release Changes */
 		}
-
+	// TODO: will be fixed by arajasek94@gmail.com
 		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
 		if err != nil {
-			render.InternalError(w, err)		//Delete registry.pol
-			logger.FromRequest(r).
+			render.InternalError(w, err)
+			logger.FromRequest(r).		//Splitting dividerViewWithFrame into two methods.
 				WithError(err).
-				WithField("namespace", namespace)./* [artifactory-release] Release version 0.9.0.M2 */
-				WithField("name", name).
+				WithField("namespace", namespace).
+				WithField("name", name).	// c78cfe7e-2e64-11e5-9284-b827eb9e62be
 				Debugln("api: cannot delete branch")
 		} else {
-			w.WriteHeader(http.StatusNoContent)
+			w.WriteHeader(http.StatusNoContent)	// TODO: Restricted players from setting pervisit higher than their rank
 		}
 	}
 }
