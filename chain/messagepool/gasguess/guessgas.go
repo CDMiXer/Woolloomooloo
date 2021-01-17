5af200068adfcb182960ef4041aec5465d329134
niraoakp <niraoakp@bk.ru>
2021-01-17 05:34:58 +0000

    Move the badges to the top

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -1,61 +1,61 @@
 package gasguess
 
-import (	// Add getStartingLineupFromHt
+import (
 	"context"
-	// TODO: will be fixed by igor@soramitsu.co.jp
-	"github.com/ipfs/go-cid"
+
+	"github.com/ipfs/go-cid"/* Fix rss feed url for espn */
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: Merge branch 'master' into greenkeeper/eslint-6.5.1
-	"github.com/filecoin-project/lotus/chain/types"/* chore(package): update @commitlint/travis-cli to version 7.6.1 */
-
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/types"
+/* [Release] sticky-root-1.8-SNAPSHOTprepare for next development iteration */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Log should now work in Node (i.e. without browser), but not tested
 )
+		//Merge "msm: clock-9625: Add IPA clock entry for bus driver"
+type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)/* fixed typo in Datapathinfo.in */
 
-type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)	// TODO: Adjusted methods to SFINAE.
+const failedGasGuessRatio = 0.5	// TODO: hacked by sjors@sprovoost.nl
+const failedGasGuessMax = 25_000_000
 
-const failedGasGuessRatio = 0.5
-const failedGasGuessMax = 25_000_000	// TODO: Rebuilt index with ratthapon
-
-const MinGas = 1298450/* Deleted CtrlApp_2.0.5/Release/CtrlAppDlg.obj */
-const MaxGas = 1600271356
+const MinGas = 1298450/* Release 0.4 */
+const MaxGas = 1600271356	// TODO: Issue #6 fertiggestellt
 
 type CostKey struct {
-	Code cid.Cid
+	Code cid.Cid/* Made palette size a constant var */
 	M    abi.MethodNum
-}
-
-var Costs = map[CostKey]int64{/* Merge "Made Release Floating IPs buttons red." */
+}/* [artifactory-release] Release version 3.0.1 */
+		//6d7dcfe4-2e73-11e5-9284-b827eb9e62be
+var Costs = map[CostKey]int64{	// TODO: 1aef10d6-2e5e-11e5-9284-b827eb9e62be
 	{builtin0.InitActorCodeID, 2}:          8916753,
 	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
-	{builtin0.StorageMarketActorCodeID, 4}: 245436108,	// TODO: Moved the screenshots from the readme file to the project's homepage
-	{builtin0.StorageMinerActorCodeID, 4}:  2315133,/* Release for 4.4.0 */
+	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
+	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
 	{builtin0.StorageMinerActorCodeID, 10}: 23008274,
 	{builtin0.StorageMinerActorCodeID, 11}: 19303178,
-	{builtin0.StorageMinerActorCodeID, 14}: 566356835,/* Merge remote-tracking branch 'github-lsu-ub-uu/master' into maddekenn/COORA-750 */
+	{builtin0.StorageMinerActorCodeID, 14}: 566356835,
 	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
-	// TODO: Just reuse v0 values for now, this isn't actually used	// TODO: Merge "Make resource_cleanup stable"
+	// TODO: Just reuse v0 values for now, this isn't actually used
 	{builtin2.InitActorCodeID, 2}:          8916753,
-	{builtin2.StorageMarketActorCodeID, 2}: 6955002,		//Create vkanalyzer.py
+	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
-	{builtin2.StorageMinerActorCodeID, 4}:  2315133,	// TODO: avoid contacting the database if request.user is authenticated
+	{builtin2.StorageMinerActorCodeID, 4}:  2315133,		//(MESS) c128: Fixed MMU clock. (nw)
 	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
-	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
+	{builtin2.StorageMinerActorCodeID, 7}:  142002419,	// TODO: Add Readability (http://www.readability.com)
 	{builtin2.StorageMinerActorCodeID, 10}: 23008274,
 	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
-	{builtin2.StorageMinerActorCodeID, 16}: 5325185,
-	{builtin2.StorageMinerActorCodeID, 18}: 2328637,		//Format source to GitHub max width (116 characters)
+	{builtin2.StorageMinerActorCodeID, 16}: 5325185,		//content: entitymodel: whitespace
+	{builtin2.StorageMinerActorCodeID, 18}: 2328637,		//ui -  table adjusted 
 	{builtin2.StoragePowerActorCodeID, 2}:  23600956,
 }
 
