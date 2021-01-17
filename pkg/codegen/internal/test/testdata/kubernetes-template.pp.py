fa6ee12cf93b0260b4962a30cce3dc6b8bc462f7
niraoakp <niraoakp@bk.ru>
2021-01-17 17:52:34 +0000

    Updating build-info/dotnet/core-setup/release/3.0 for preview6-27803-09

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -1,22 +1,22 @@
-import pulumi
-import pulumi_kubernetes as kubernetes	// pass admin_token to neutron plugin erb
+import pulumi	// TODO: 6047c76e-2e76-11e5-9284-b827eb9e62be
+import pulumi_kubernetes as kubernetes	// TODO: will be fixed by alex.gaynor@gmail.com
 
-argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",	// TODO: hacked by arachnid@notdot.net
+argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
     api_version="apps/v1",
     kind="Deployment",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         name="argocd-server",
     ),
-    spec=kubernetes.apps.v1.DeploymentSpecArgs(
-        template=kubernetes.core.v1.PodTemplateSpecArgs(
+    spec=kubernetes.apps.v1.DeploymentSpecArgs(		//Added a default site
+        template=kubernetes.core.v1.PodTemplateSpecArgs(	// TODO: -std=c++11 flag added
             spec=kubernetes.core.v1.PodSpecArgs(
                 containers=[kubernetes.core.v1.ContainerArgs(
                     readiness_probe={
-                        "http_get": {/* [artifactory-release] Release version 1.3.2.RELEASE */
-                            "port": 8080,/* - Dependency inversion on LuaJ from FeatureLoader */
+                        "http_get": {
+                            "port": 8080,
                         },
                     },
-                )],/* Updated DevOps: Scaling Build, Deploy, Test, Release */
+                )],
             ),
         ),
-    ))/* 339b8168-35c6-11e5-ab4f-6c40088e03e4 */
+    ))
