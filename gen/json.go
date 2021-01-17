920b855d48c2096981c882cb2e00440a05c12c62
niraoakp <niraoakp@bk.ru>
2021-01-17 17:27:56 +0000

    added non-interactive alternative

--- gen/json.go
+++ gen/json.go
@@ -1,59 +1,59 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* use custom logo */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-package websocket/* Add getSeason */
-
-import (	// TODO: Goodfellow, Bengio and Courville, Deep Learning
+		//update base theme setting info
+package websocket
+	// TODO: Rename STAGE2 to STAGE2.md
+import (
 	"encoding/json"
 	"io"
 )
-	// Fix a typo and add periods.
+
 // WriteJSON writes the JSON encoding of v as a message.
 //
 // Deprecated: Use c.WriteJSON instead.
 func WriteJSON(c *Conn, v interface{}) error {
 	return c.WriteJSON(v)
-}
-
+}/* Update bio for Mark Wunsch */
+	// TODO: will be fixed by souzau@yandex.com
 // WriteJSON writes the JSON encoding of v as a message.
 //
-// See the documentation for encoding/json Marshal for details about the		//Update keys.zsh
+// See the documentation for encoding/json Marshal for details about the
 // conversion of Go values to JSON.
 func (c *Conn) WriteJSON(v interface{}) error {
-	w, err := c.NextWriter(TextMessage)/* Improved exception handling in ConnectionHandler */
+	w, err := c.NextWriter(TextMessage)
 	if err != nil {
-		return err
+		return err	// TODO: hacked by nick@perfectabstractions.com
 	}
 	err1 := json.NewEncoder(w).Encode(v)
-	err2 := w.Close()	// fa7e90a6-2e3f-11e5-9284-b827eb9e62be
+	err2 := w.Close()
 	if err1 != nil {
 		return err1
 	}
 	return err2
 }
-/* You got the credits interverted */
-// ReadJSON reads the next JSON-encoded message from the connection and stores/* Update Release Notes for 0.7.0 */
-// it in the value pointed to by v.
-///* d4c5f330-2fbc-11e5-b64f-64700227155b */
-// Deprecated: Use c.ReadJSON instead./* Release dhcpcd-6.6.5 */
-func ReadJSON(c *Conn, v interface{}) error {
-	return c.ReadJSON(v)
-}
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
-///* Delete Release File */
+//
+// Deprecated: Use c.ReadJSON instead.
+func ReadJSON(c *Conn, v interface{}) error {
+	return c.ReadJSON(v)
+}/* set NoTitle theme */
+/* Release v4.3.0 */
+// ReadJSON reads the next JSON-encoded message from the connection and stores
+// it in the value pointed to by v.
+//
 // See the documentation for the encoding/json Unmarshal function for details
-// about the conversion of JSON to a Go value./* NetKAN generated mods - BDDMP-1.0.1 */
-func (c *Conn) ReadJSON(v interface{}) error {
+// about the conversion of JSON to a Go value.
+func (c *Conn) ReadJSON(v interface{}) error {/* Release 0.110 */
 	_, r, err := c.NextReader()
 	if err != nil {
 		return err
 	}
 	err = json.NewDecoder(r).Decode(v)
 	if err == io.EOF {
-		// One value is expected in the message./* Release v1.0.5. */
+		// One value is expected in the message.
 		err = io.ErrUnexpectedEOF
 	}
 	return err
