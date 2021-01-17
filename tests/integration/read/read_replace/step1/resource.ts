6f1828edb12a3310e2aa7373a478c0fca21dbe58
niraoakp <niraoakp@bk.ru>
2021-01-17 16:38:16 +0000

    Added context menu to BrowsingActivity

--- tests/integration/read/read_replace/step1/resource.ts
+++ tests/integration/read/read_replace/step1/resource.ts
@@ -1,49 +1,49 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release: 6.0.3 changelog */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Removal of Tracker&Live, Bukkit Build Update.
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: merge typo to its own branch
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//WIP - Removed AES & incorrect secrecy claim.
-// See the License for the specific language governing permissions and/* first loop test */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();	// TODO: will be fixed by remco@dutchcoders.io
+    public static readonly instance = new Provider();
 
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,	// Fixed some Fortify findings.
+            inputs: news,
         }
     }
 
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {/* Release v0.3.3.1 */
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
                 changes: true,
                 replaces: ["state"],
             };
-        }		//Update 13-08-2006 17:30
+        }
 
         return {
             changes: false,
         }
     }
 
-    public async create(inputs: any): Promise<dynamic.CreateResult> {/* Release osso-gnomevfs-extra 1.7.1. */
+    public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: (this.id++).toString(),
-            outs: inputs,	// PDDP parameters are now parameterizable
-        }/* Release 1.8.5 */
+            outs: inputs,
+        }
     }
 
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
@@ -51,7 +51,7 @@ export class Provider implements dynamic.ResourceProvider {
     }
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
-        return {		//Update Travis Go versions.
+        return {
             id: id,
             props: props,
         }
@@ -60,7 +60,7 @@ export class Provider implements dynamic.ResourceProvider {
 
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
-	// TODO: hacked by lexy8russo@outlook.com
+
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
