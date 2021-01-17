ebb0a483ea2ff7631e1f314ece3b736249775395
niraoakp <niraoakp@bk.ru>
2021-01-17 06:04:14 +0000

    namespace edits

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,7 +1,7 @@
--- name: create-table-secrets
+-- name: create-table-secrets	// Add info about data
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT	// Create mosaik_calc_distance.user.js
 ,secret_repo_id           INTEGER
 ,secret_name              TEXT
 ,secret_data              BLOB
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS secrets (
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-secrets-repo
+-- name: create-index-secrets-repo		//:memo: [skip ci] remove ! from Yahoo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name
+-- name: create-index-secrets-repo-name		//Доработать постер
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
