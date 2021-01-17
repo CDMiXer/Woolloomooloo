9ff9e9f2bea604a6256be72d9a98bb14b01446e7
niraoakp <niraoakp@bk.ru>
2021-01-17 18:20:32 +0000

    Create cvr_qualifiers_array_declaration.c

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,15 +1,15 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-
-import (
+		//new demo version
+import (/* Initial Release!! */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// FooComponent is a component resource
-type FooComponent struct {
+// FooComponent is a component resource/* rev 516728 */
+type FooComponent struct {/* incorporate patches from ccp4 version 6.1.3 */
 	pulumi.ResourceState
-}
+}	// TODO: hacked by onhardev@bk.ru
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
