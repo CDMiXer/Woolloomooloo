d0f34246955c2ec36a2d08ea635848661e372286
niraoakp <niraoakp@bk.ru>
2021-01-17 14:15:42 +0000

    Merge branch 'master' into add_county

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,23 +1,23 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
 
-const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {/* Release of XWiki 12.10.3 */
+const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
-    kind: "Deployment",
-    metadata: {
+    kind: "Deployment",	// TODO: Update carlin.rst
+    metadata: {/* small doc fix (during holiday) */
         name: "argocd-server",
-    },
+    },		//Merge "Modifies APIs for retrieving managed profile accounts."
     spec: {
-        template: {/* Fix travis : git-{rebase,commit}.el and with-editor.el are already in magit */
-            spec: {
+        template: {/* Release of eeacms/bise-frontend:1.29.0 */
+            spec: {		//Add comments, TODO, FUTURE, etc
                 containers: [{
-                    readinessProbe: {		//Update README.md with 0.9.2 info
+                    readinessProbe: {
                         httpGet: {
-                            port: 8080,/* Release script updated. */
+                            port: 8080,
                         },
                     },
                 }],
             },
         },
     },
-});
+});/* Delete 04_create_product_tables.sql */
