fc5cc75405205b4b971050feedef17d101ab0bb2
niraoakp <niraoakp@bk.ru>
2021-01-17 22:53:06 +0000

    Merge "Update capabilities-map.yaml"

--- handler/api/repos/builds/retry.go
+++ handler/api/repos/builds/retry.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
-// You may obtain a copy of the License at	// TODO: HTML cleanup, added URL for jQuery 2.1.4
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by vyzo@hackzen.org
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Changed order functionality; now working */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* LAD Release 3.0.121 */
+
 package builds
 
 import (
-	"net/http"/* Release 1.0.29 */
+	"net/http"
 	"strconv"
-/* Release of eeacms/energy-union-frontend:1.7-beta.0 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"	// Merge pull request #9403 from Memphiz/fix_json_stack_overflow
+	"github.com/drone/drone/handler/api/request"
 
 	"github.com/go-chi/chi"
 )
@@ -28,13 +28,13 @@ import (
 // HandleRetry returns an http.HandlerFunc that processes http
 // requests to retry and re-execute a build.
 func HandleRetry(
-	repos core.RepositoryStore,	// TODO: Work in progress #14
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Release for v47.0.0. */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			user, _   = request.UserFrom(r.Context())
 		)
@@ -43,14 +43,14 @@ func HandleRetry(
 			render.BadRequest(w, err)
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: Remove now useless LD script
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+			render.NotFound(w, err)
 			return
 		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: will be fixed by witek@enjin.io
+			render.NotFound(w, err)
 			return
 		}
 
@@ -63,9 +63,9 @@ func HandleRetry(
 			return
 		}
 
-		hook := &core.Hook{/* Release of eeacms/plonesaas:5.2.1-24 */
+		hook := &core.Hook{
 			Trigger:      user.Login,
-			Event:        prev.Event,/* support for add to screen on mobile devices */
+			Event:        prev.Event,
 			Action:       prev.Action,
 			Link:         prev.Link,
 			Timestamp:    prev.Timestamp,
@@ -77,7 +77,7 @@ func HandleRetry(
 			Fork:         prev.Fork,
 			Source:       prev.Source,
 			Target:       prev.Target,
-			Author:       prev.Author,	// Update upcoming.md
+			Author:       prev.Author,
 			AuthorName:   prev.AuthorName,
 			AuthorEmail:  prev.AuthorEmail,
 			AuthorAvatar: prev.AuthorAvatar,
