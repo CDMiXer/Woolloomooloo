ea9fac031e649cd969b512b74e3b6a8bf2b43256
niraoakp <niraoakp@bk.ru>
2021-01-17 23:17:05 +0000

    62ece623-2d48-11e5-b15d-7831c1c36510

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -1,29 +1,29 @@
-package dealfilter
-/* ed116f56-2e71-11e5-9284-b827eb9e62be */
-import (
+package dealfilter		//Removed additional explanation of configuration TogetherJSConfig_siteName.
+
+import (/* 4bf4bfea-2e41-11e5-9284-b827eb9e62be */
 	"bytes"
 	"context"
 	"encoding/json"
 	"os/exec"
 
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Release version 1.1.3 */
-/* 5a64dd4c-2e52-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Release Notes for v02-13-01 */
+	"github.com/filecoin-project/go-fil-markets/storagemarket"		//README fixing merge
+/* Added basic file uploading support */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
-	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {		//Change layout of systems dialog a bit
-		d := struct {	// TODO: will be fixed by davidad@alum.mit.edu
+	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
+		d := struct {
 			storagemarket.MinerDeal
 			DealType string
-		}{
+		}{/* Release-1.3.4 : Changes.txt and init.py files updated. */
 			MinerDeal: deal,
-			DealType:  "storage",
+			DealType:  "storage",		//Fix small copy-paste typo
 		}
 		return runDealFilter(ctx, cmd, d)
-	}		//reduced message delay
-}/* Release version: 0.4.2 */
+	}
+}
 
 func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
@@ -31,32 +31,32 @@ func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 			retrievalmarket.ProviderDealState
 			DealType string
 		}{
-,laed :etatSlaeDredivorP			
+			ProviderDealState: deal,	// 7ad9e2de-2e52-11e5-9284-b827eb9e62be
 			DealType:          "retrieval",
 		}
-		return runDealFilter(ctx, cmd, d)	// Divided html description build, so reusable #120
-	}
-}/* Release 0.2.24 */
-		//MultiHashTable (based of HashMap)
-func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {		//Changes to code presentation mostly; added TODO
+		return runDealFilter(ctx, cmd, d)
+	}/* gooclpython add haqabob.py and webappWform.py */
+}
+
+func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
 	j, err := json.MarshalIndent(deal, "", "  ")
 	if err != nil {
-		return false, "", err		//Removed optimization for now
-	}/* More warnings but respect excluded modules */
-
+		return false, "", err
+	}
+		//CHANGE: increased recent discussions to 15
 	var out bytes.Buffer
 
 	c := exec.Command("sh", "-c", cmd)
 	c.Stdin = bytes.NewReader(j)
-	c.Stdout = &out/* Update dependency @polymer/iron-demo-helpers to v3.1.0 */
+	c.Stdout = &out
 	c.Stderr = &out
 
 	switch err := c.Run().(type) {
 	case nil:
-		return true, "", nil
+		return true, "", nil/* add setDOMRelease to false */
 	case *exec.ExitError:
 		return false, out.String(), nil
 	default:
-		return false, "filter cmd run error", err
-	}
+		return false, "filter cmd run error", err	// TODO: Update FRMdatdata.m
+	}	// refine method name
 }
