7386567a4739622bc7863f6085621a87af05a67a
niraoakp <niraoakp@bk.ru>
2021-01-17 06:20:48 +0000

    HackBase Readme Commit

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -3,35 +3,35 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Released DirectiveRecord v0.1.12 */
-//      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: we need to initialize this
+//
+//      http://www.apache.org/licenses/LICENSE-2.0	// Update README.md with credentials option.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Fixing more issues
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// +build oss
+	// more introspecting the container
+// +build oss	// TODO: fixes for sio_magic, #382
 
 package builds
 
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"
+/* Merge branch 'fix-toc' into removing-dev-op-guide */
+	"github.com/drone/drone/core"	// rev 783906
 	"github.com/drone/drone/handler/api/render"
 )
-
-var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {		//Fix possible but unlikely exploit
+		//Re-enabling more tests and bug fixes
+var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}		//Fix openlibm OS variable
+}
 
 // HandleRollback returns a non-op http.HandlerFunc.
 func HandleRollback(
 	core.RepositoryStore,
-	core.BuildStore,
+	core.BuildStore,		//Fix the issue on more than one sockets
 	core.Triggerer,
-) http.HandlerFunc {		//3aabdf68-2e49-11e5-9284-b827eb9e62be
+) http.HandlerFunc {
 	return rollbackNotImplemented
 }
