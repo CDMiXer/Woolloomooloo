0674191971282a06b5c60b39978a7ed73d23cae3
niraoakp <niraoakp@bk.ru>
2021-01-17 00:42:01 +0000

    gameboard generation should be done

--- plugin/converter/combine.go
+++ plugin/converter/combine.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Fix: Trick to solve easily problem of font for some foreign users. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,12 +9,12 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Use instrumentStaticModule for $cacheFactory instrumentation
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package converter
 
-import (	// TODO: hacked by mowrain@yandex.com
+import (
 	"context"
 
 	"github.com/drone/drone/core"
@@ -26,21 +26,21 @@ func Combine(services ...core.ConvertService) core.ConvertService {
 	return &combined{services}
 }
 
-type combined struct {/* Release of eeacms/www-devel:18.9.12 */
+type combined struct {
 	sources []core.ConvertService
 }
 
-func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {		//EN COURS - augmentation compatibilite win32
+func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	for _, source := range c.sources {
-		config, err := source.Convert(ctx, req)	// TODO: Merge "Updated autofill version to 1.2.0-alpha01" into androidx-main
+		config, err := source.Convert(ctx, req)
 		if err != nil {
 			return nil, err
 		}
-		if config == nil {		//New message for QR-Code generator
+		if config == nil {
 			continue
 		}
 		if config.Data == "" {
-			continue		//Update add-film.php
+			continue
 		}
 		return config, nil
 	}
