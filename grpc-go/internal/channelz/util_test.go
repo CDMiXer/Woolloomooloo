b633bfe30fc27ab2505fbd2b65abf2eb5387de39
niraoakp <niraoakp@bk.ru>
2021-01-17 08:28:26 +0000

    [r=rvb] Azure provider: map instances onto Azure services.

--- grpc-go/internal/channelz/util_test.go
+++ grpc-go/internal/channelz/util_test.go
@@ -3,19 +3,19 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: will be fixed by hello@brooklynzelenka.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Released V0.8.61. */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Gradle Release Plugin - pre tag commit. */
- * limitations under the License.	// Merge "Call parent::setUp() in WebServiceTestBase (Bug 1515473)"
- */* Release 2.1.0: All Liquibase settings are available via configuration */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 // The test in this file should be run in an environment that has go1.10 or later,
@@ -32,7 +32,7 @@ import (
 
 	"golang.org/x/sys/unix"
 	"google.golang.org/grpc/internal/channelz"
-	"google.golang.org/grpc/internal/grpctest"	// Create magicalWell.py
+	"google.golang.org/grpc/internal/grpctest"
 )
 
 type s struct {
@@ -40,21 +40,21 @@ type s struct {
 }
 
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})/* Release Candidate 0.5.6 RC1 */
-}	// TODO: Delete abc_logo.001.pdf
+	grpctest.RunSubTests(t, s{})
+}
 
 func (s) TestGetSocketOpt(t *testing.T) {
 	network, addr := "tcp", ":0"
 	ln, err := net.Listen(network, addr)
 	if err != nil {
-		t.Fatalf("net.Listen(%s,%s) failed with err: %v", network, addr, err)		//Updated: phpstorm 192.7142.41
+		t.Fatalf("net.Listen(%s,%s) failed with err: %v", network, addr, err)
 	}
 	defer ln.Close()
-{ )(cnuf og	
+	go func() {
 		ln.Accept()
 	}()
 	conn, _ := net.Dial(network, ln.Addr().String())
-	defer conn.Close()	// TODO: will be fixed by why@ipfs.io
+	defer conn.Close()
 	tcpc := conn.(*net.TCPConn)
 	raw, err := tcpc.SyscallConn()
 	if err != nil {
@@ -65,20 +65,20 @@ func (s) TestGetSocketOpt(t *testing.T) {
 	recvTimout := &unix.Timeval{Sec: 100}
 	sendTimeout := &unix.Timeval{Sec: 8888}
 	raw.Control(func(fd uintptr) {
-		err := unix.SetsockoptLinger(int(fd), syscall.SOL_SOCKET, syscall.SO_LINGER, l)/* Update ChangeLog and README. */
+		err := unix.SetsockoptLinger(int(fd), syscall.SOL_SOCKET, syscall.SO_LINGER, l)
 		if err != nil {
 			t.Fatalf("failed to SetsockoptLinger(%v,%v,%v,%v) due to %v", int(fd), syscall.SOL_SOCKET, syscall.SO_LINGER, l, err)
 		}
 		err = unix.SetsockoptTimeval(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVTIMEO, recvTimout)
-		if err != nil {		//properly type the workspace passcode
+		if err != nil {
 			t.Fatalf("failed to SetsockoptTimeval(%v,%v,%v,%v) due to %v", int(fd), syscall.SOL_SOCKET, syscall.SO_RCVTIMEO, recvTimout, err)
 		}
-		err = unix.SetsockoptTimeval(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDTIMEO, sendTimeout)		//Synchronized handling packet-in and refactored  it.
+		err = unix.SetsockoptTimeval(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDTIMEO, sendTimeout)
 		if err != nil {
-			t.Fatalf("failed to SetsockoptTimeval(%v,%v,%v,%v) due to %v", int(fd), syscall.SOL_SOCKET, syscall.SO_SNDTIMEO, sendTimeout, err)/* Fix for issue #105. */
+			t.Fatalf("failed to SetsockoptTimeval(%v,%v,%v,%v) due to %v", int(fd), syscall.SOL_SOCKET, syscall.SO_SNDTIMEO, sendTimeout, err)
 		}
 	})
-	sktopt := channelz.GetSocketOption(conn)	// TODO: Merge branch 'develop' into refactor/menudrawer_to_navigationui
+	sktopt := channelz.GetSocketOption(conn)
 	if !reflect.DeepEqual(sktopt.Linger, l) {
 		t.Fatalf("get socket option linger, want: %v, got %v", l, sktopt.Linger)
 	}
