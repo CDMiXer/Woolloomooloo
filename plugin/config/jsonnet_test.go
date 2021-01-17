75a0c35f95c89e7d393bd2c600e55d673c3ae5e3
niraoakp <niraoakp@bk.ru>
2021-01-17 18:59:04 +0000

    Delete imported framework on cancel CA-604

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* DATASOLR-234 - Release version 1.4.0.RELEASE. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package config
+	// TODO: Car now turns 2 wheels at once
+package config	// TODO: hacked by lexy8russo@outlook.com
