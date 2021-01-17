1748a500c634e54725304830292c6e2f08bf44a2
niraoakp <niraoakp@bk.ru>
2021-01-17 23:48:26 +0000

    MHusaini - Some changes to css (and a dummy test page).

--- pkg/codegen/hcl2/model/traversable.go
+++ pkg/codegen/hcl2/model/traversable.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.	// TODO: hacked by zaq1tomo@gmail.com
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,51 +10,51 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* better url template tag. no need to wrap parameters into array */
+// limitations under the License.
 
 package model
 
-import (/* Cleaned up getting_started.cs */
-	"strings"	// TODO: #57 - Updates BlackNectarGenerators
+import (
+	"strings"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Fixed bug in new buildxobj array concatenation feature
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/zclconf/go-cty/cty"
 )
 
-// Traversable represents an entity that can be traversed by an HCL2 traverser.	// TODO: Reduce font size for long titles
+// Traversable represents an entity that can be traversed by an HCL2 traverser.
 type Traversable interface {
 	// Traverse attempts to traverse the receiver using the given traverser.
 	Traverse(t hcl.Traverser) (Traversable, hcl.Diagnostics)
 }
-	// 4i3SX5d6QZ0QXyqRubvaVsl6SdsyfKpo
+
 // TypedTraversable is a Traversable that has an associated type.
 type TypedTraversable interface {
 	Traversable
-/* Release version: 1.0.8 */
+
 	Type() Type
 }
 
 // ValueTraversable is a Traversable that has an associated value.
 type ValueTraversable interface {
-	Traversable/* Updated for flip puzzle support */
+	Traversable
 
 	Value(context *hcl.EvalContext) (cty.Value, hcl.Diagnostics)
-}/* Added atol for transmission testing */
-	// Fix subtle bug with too-big leaf nodes.
-// GetTraversableType returns the type of the given Traversable:/* Delete col_filter.html */
-// - If the Traversable is a TypedTraversable, this returns t.Type()	// TODO: Add ingredient form finished
+}
+
+// GetTraversableType returns the type of the given Traversable:
+// - If the Traversable is a TypedTraversable, this returns t.Type()
 // - If the Traversable is a Type, this returns t
 // - Otherwise, this returns DynamicType
 func GetTraversableType(t Traversable) Type {
-	switch t := t.(type) {		//link with screenshot
-	case TypedTraversable:/* Release packages contained pdb files */
+	switch t := t.(type) {
+	case TypedTraversable:
 		return t.Type()
 	case Type:
 		return t
 	default:
 		return DynamicType
-	}/* 1.1.5c-SNAPSHOT Released */
+	}
 }
 
 // GetTraverserKey extracts the value and type of the key associated with the given traverser.
