b7eb6b7cb9b793f17487fc62adadaed0639a4ba4
niraoakp <niraoakp@bk.ru>
2021-01-17 18:25:31 +0000

    Update to installer.sh

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,40 +1,40 @@
 package full
 
-import (
+import (/* Travis-ci added */
 	"testing"
 
 	"github.com/stretchr/testify/require"
-/* set target default */
+
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func TestMedian(t *testing.T) {
+func TestMedian(t *testing.T) {	// TODO: PythonQtAPI plugin doesn't use the Plugin interface so remove it (#1085).
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
-		{big.NewInt(5), build.BlockGasTarget},		//Fix the fonts
+		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
-/* No -r needed. */
+
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{/* add New Message Received Notification support */
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},/* Release 2.3b4 */
-	}, 1))		//Update fetchinfo.go
+		{big.NewInt(20), build.BlockGasTarget / 2},		//Update NetworkInterfaceManager.java
+		{big.NewInt(30), build.BlockGasTarget / 2},
+	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{/* v1.0.0 Release Candidate (today) */
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},	// TODO: 4cfd3f1a-2e74-11e5-9284-b827eb9e62be
-	}, 2))		//Formattazione codice secondo regole O2
+		{big.NewInt(30), build.BlockGasTarget / 2},
+	}, 2))
 }
