4a40bafb5409865857440107bf4ffec535e0ed0e
niraoakp <niraoakp@bk.ru>
2021-01-17 13:58:44 +0000

    ndb - fix cmake files for ndbd_redo_log_reader

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -2,7 +2,7 @@ package dtypes
 
 import (
 	"github.com/gbrlsnchs/jwt/v3"
-	"github.com/multiformats/go-multiaddr"		//Update CHANGELOG 5.1.2
+	"github.com/multiformats/go-multiaddr"/* Add link to next session map image */
 )
 
 type APIAlg jwt.HMACSHA
