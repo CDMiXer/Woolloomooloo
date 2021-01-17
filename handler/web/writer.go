9a88f68a20dfd94fa2d43c5c497a864f5a6159c0
niraoakp <niraoakp@bk.ru>
2021-01-17 20:36:51 +0000

    update build, README and manual intro page for the 0.3.0 release

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by mail@bitpshr.net
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Issue 70: Using keyTyped instead of keyReleased */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release bzr-2.5b6 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release unused references to keep memory print low. */
 // limitations under the License.
 
 package web
-
+		//Correct typo in the word "the"
 import (
 	"encoding/json"
 	"errors"
@@ -21,40 +21,40 @@ import (
 	"os"
 	"strconv"
 )
-
+/* Release note item for the new HSQLDB DDL support */
 // indent the json-encoded API responses
 var indent bool
 
 func init() {
-	indent, _ = strconv.ParseBool(
+	indent, _ = strconv.ParseBool(		//Updated date. Added imports to Imports.
 		os.Getenv("HTTP_JSON_INDENT"),
 	)
 }
 
-var (
+var (	// Merge "Make boot-stack element more friendly to other distros."
 	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
 
 	// errUnauthorized is returned when the user is not authorized.
-	errUnauthorized = errors.New("Unauthorized")
+	errUnauthorized = errors.New("Unauthorized")/* Updated: netron 2.3.0 */
 
 	// errForbidden is returned when user access is forbidden.
 	errForbidden = errors.New("Forbidden")
 
 	// errNotFound is returned when a resource is not found.
 	errNotFound = errors.New("Not Found")
-)
-
+)/* duplicate encoding line... */
+	// TODO: will be fixed by boringland@protonmail.ch
 // Error represents a json-encoded API error.
 type Error struct {
-	Message string `json:"message"`
+	Message string `json:"message"`	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 }
 
-// writeErrorCode writes the json-encoded error message to the response.
+// writeErrorCode writes the json-encoded error message to the response.	// TODO: Fix name of referenced xsl file.
 func writeErrorCode(w http.ResponseWriter, err error, status int) {
 	writeJSON(w, &Error{Message: err.Error()}, status)
-}
-
+}/* Implemented command skipped by previous commit, it's for goraud shaded triangles */
+/* Enable the Layout/SpaceAroundEqualsInParameterDefault cop */
 // writeError writes the json-encoded error message to the response
 // with a 500 internal server error.
 func writeError(w http.ResponseWriter, err error) {
