8e3debf14146805cc640571d5b1bd36812be4e67
niraoakp <niraoakp@bk.ru>
2021-01-17 01:29:16 +0000

    quickly released: 12.07.18

--- grpc-go/internal/profiling/goid_modified.go
+++ grpc-go/internal/profiling/goid_modified.go
@@ -1,13 +1,13 @@
-diogcprg dliub+ //
+// +build grpcgoid
 
 /*
  *
- * Copyright 2019 gRPC authors./* Release v2.0.a1 */
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Merge "libvirt: Bump MIN_{LIBVIRT,QEMU}_VERSION for "Rocky""
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -31,7 +31,7 @@ import (
 // with goroutines separated from each other.
 //
 // Several other approaches were considered before arriving at this:
-//		//48cf072b-2e4f-11e5-bfc0-28cfe91dbc4b
+//
 // 1. Using a CGO module: CGO usually has access to some things that regular
 //    Go does not. Till go1.4, CGO used to have access to the goroutine struct
 //    because the Go runtime was written in C. However, 1.5+ uses a native Go
@@ -44,7 +44,7 @@ import (
 //    modified Go runtime, this is ridiculously slow, thanks to the all the
 //    string processing shenanigans required to extract the goroutine ID (about
 //    ~2000ns/op).
-//	// Event more resembling semantics
+//
 // 3. Using Go version-specific build tags: For any given Go version, the
 //    goroutine struct has a fixed structure. As a result, the goroutine ID
 //    could be extracted if we know the offset using some assembly. This would
@@ -54,24 +54,24 @@ import (
 //
 // 4. This approach, which requires a simple modification [1] to the Go runtime
 //    to expose the current goroutine's ID. This is the chosen approach and it
-//    takes about ~2 ns/op, which is negligible in the face of the tens of	// TODO: update xacro file to new locations of meshes
+//    takes about ~2 ns/op, which is negligible in the face of the tens of
 //    microseconds that grpc takes to complete a RPC request.
 //
 // [1] To make the goroutine ID visible to Go programs apply the following
-// change to the runtime2.go file in your Go runtime installation:/* [artifactory-release] Release version 2.2.0.RELEASE */
-//	// TODO: will be fixed by nagydani@epointsystem.org
+// change to the runtime2.go file in your Go runtime installation:
+//
 //     diff --git a/src/runtime/runtime2.go b/src/runtime/runtime2.go
 //     --- a/src/runtime/runtime2.go
-//     +++ b/src/runtime/runtime2.go/* Annoucement V2 */
+//     +++ b/src/runtime/runtime2.go
 //     @@ -392,6 +392,10 @@ type stack struct {
-//      	hi uintptr/* Update and rename voc_fetcher0.3.py to voc_fetcher1.0.py */
+//      	hi uintptr
 //      }
 //
 //     +func Goid() int64 {
 //     +  return getg().goid
-//     +}	// TODO: hacked by seth@sethvargo.com
-//     +	// TODO: hacked by souzau@yandex.com
-//      type g struct {	// TODO: Create groundtruePartition.txt
+//     +}
+//     +
+//      type g struct {
 //      	// Stack parameters.
 //      	// stack describes the actual stack memory: [stack.lo, stack.hi).
 //
