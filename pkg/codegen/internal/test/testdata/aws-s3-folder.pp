7117567bee829165400effa25116c8b311fc41b5
niraoakp <niraoakp@bk.ru>
2021-01-17 14:42:29 +0000

    Return $vexContent here like vex.open does

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -1,41 +1,41 @@
 // Create a bucket and expose a website index document
 resource siteBucket "aws:s3:Bucket" {
-	website = {		//Merge "Bump tracing to 1.1.0-alpha01" into androidx-master-dev
-		indexDocument = "index.html"	// ec204f4a-2e44-11e5-9284-b827eb9e62be
+	website = {
+		indexDocument = "index.html"
 	}
 }
-/* Release 0.2 version */
-siteDir = "www" // directory for content files
+/* Merge "Release 5.0.0 - Juno" */
+siteDir = "www" // directory for content files/* bindings/python/Makefile.am: fix defsdir ($PLATFORM_VERSION not used) */
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
     options {
 		range = readDir(siteDir)
     }
-
+/* Addin a link */
 	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
-}/* [#512] Release notes 1.6.14.1 */
+}
 
 // Set the access policy for the bucket so all objects are readable
 resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
 	// The policy is JSON-encoded.
-	policy = toJSON({/* Release 0.6.4. */
-		Version = "2012-10-17"		//Create slackerRestore.dv6.2.sh
+	policy = toJSON({
+		Version = "2012-10-17"
 		Statement = [{
 			Effect = "Allow"
-			Principal = "*"	// TODO: Delete dqn_AirSimEnv-v42_weights.h5f
+			Principal = "*"
 			Action = [ "s3:GetObject" ]
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
 		}]
 	})
-}
-	// yet another possible fix to the encoding issues on the Last-Translator field
+}		//define a CastUtils class with helper methods to be used by various Cast impls
+
 // Stack outputs
 output bucketName { value = siteBucket.bucket }
-output websiteUrl { value = siteBucket.websiteEndpoint }	// TODO: hacked by sjors@sprovoost.nl
+output websiteUrl { value = siteBucket.websiteEndpoint }
