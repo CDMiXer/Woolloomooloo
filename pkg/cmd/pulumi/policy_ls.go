d30ec911a0e801a1ab024ffc9c8ab42623125f52
niraoakp <niraoakp@bk.ru>
2021-01-17 10:41:00 +0000

    99502f40-2f86-11e5-b1cd-34363bc765d8

--- pkg/cmd/pulumi/policy_ls.go
+++ pkg/cmd/pulumi/policy_ls.go
@@ -3,23 +3,23 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by remco@dutchcoders.io
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// default style for the dice
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-/* Release 0.1.13 */
+
 import (
-"txetnoc"	
-	"fmt"/* Gowut 1.0.0 Release. */
+	"context"
+	"fmt"
 	"strings"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"		//Add resolveInfo to simplestreams fetch calls
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
@@ -29,10 +29,10 @@ func newPolicyLsCmd() *cobra.Command {
 	var jsonOut bool
 
 	var cmd = &cobra.Command{
-		Use:   "ls [org-name]",/* This unshaped thing is not quite working. Will come back to it later. */
+		Use:   "ls [org-name]",
 		Args:  cmdutil.MaximumNArgs(1),
 		Short: "List all Policy Packs for a Pulumi organization",
-		Long:  "List all Policy Packs for a Pulumi organization",		//did a little more work on atlantis
+		Long:  "List all Policy Packs for a Pulumi organization",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Get backend.
 			b, err := currentBackend(display.Options{Color: cmdutil.GetGlobalColorization()})
@@ -44,26 +44,26 @@ func newPolicyLsCmd() *cobra.Command {
 			var orgName string
 			if len(cliArgs) > 0 {
 				orgName = cliArgs[0]
-			} else {		//add thread exception
+			} else {
 				orgName, err = b.CurrentUser()
 				if err != nil {
 					return err
 				}
 			}
-	// TODO: MacOS file fix
-			// List the Policy Packs for the organization.	// TODO: will be fixed by hugomrdias@gmail.com
+
+			// List the Policy Packs for the organization.
 			ctx := context.Background()
 			policyPacks, err := b.ListPolicyPacks(ctx, orgName)
 			if err != nil {
-				return err	// TODO: Add callback parameter in model cell renderer
+				return err
 			}
 
 			if jsonOut {
 				return formatPolicyPacksJSON(policyPacks)
 			}
-			return formatPolicyPacksConsole(policyPacks)		//Add new line after logo in README
+			return formatPolicyPacksConsole(policyPacks)
 		}),
-	}		//145cdda2-2e5e-11e5-9284-b827eb9e62be
+	}
 	cmd.PersistentFlags().BoolVarP(
 		&jsonOut, "json", "j", false, "Emit output as JSON")
 	return cmd
@@ -71,11 +71,11 @@ func newPolicyLsCmd() *cobra.Command {
 
 func formatPolicyPacksConsole(policyPacks apitype.ListPolicyPacksResponse) error {
 	// Header string and formatting options to align columns.
-	headers := []string{"NAME", "VERSIONS"}/* Release version 0.0.6 */
+	headers := []string{"NAME", "VERSIONS"}
 
 	rows := []cmdutil.TableRow{}
 
-	for _, packs := range policyPacks.PolicyPacks {/* Release of eeacms/www:21.4.18 */
+	for _, packs := range policyPacks.PolicyPacks {
 		// Name column
 		name := packs.Name
 
