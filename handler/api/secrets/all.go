79dbee2b8b27ada43036f14a92cebe82df8f8860
niraoakp <niraoakp@bk.ru>
2021-01-17 10:09:11 +0000

    Définition du mode d'inscription par défaut pour #FORMULAIRE_INSCRIPTION (test)

--- handler/api/secrets/all.go
+++ handler/api/secrets/all.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release1.4.1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-		//Merge "fix a typo of requirements"
+// that can be found in the LICENSE file.	// TODO: will be fixed by nick@perfectabstractions.com
+
 // +build !oss
 
-package secrets
-
+package secrets	// TODO: Merge "NSXv3: Add new tags for LBaaS resources"
+	// TODO: CLI: add empty 'amber create' command
 import (
 	"net/http"
 
@@ -16,18 +16,18 @@ import (
 // HandleAll returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
 func HandleAll(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Released springjdbcdao version 1.9.15 */
-		list, err := secrets.ListAll(r.Context())
+	return func(w http.ResponseWriter, r *http.Request) {/* Released springjdbcdao version 1.8.3 */
+		list, err := secrets.ListAll(r.Context())		//Update PointsToGraph.java
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		// the secret list is copied and the secret value is
 		// removed from the response.
-		secrets := []*core.Secret{}/* Release 0.9.0.rc1 */
+		secrets := []*core.Secret{}
 		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
 		}
-		render.JSON(w, secrets, 200)	// Privacy update w/ GDPR
+		render.JSON(w, secrets, 200)
 	}
-}
+}	// Hopefully fix the failing include/exclude match in sed
