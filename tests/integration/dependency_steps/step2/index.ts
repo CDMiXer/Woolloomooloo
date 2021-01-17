d8268d2b0a6455267bf1e4853c5db2806cd5ef06
niraoakp <niraoakp@bk.ru>
2021-01-17 02:38:37 +0000

    effet de bord de uima-common

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Some Eclipse validation errors fixed */
+
 import { Resource } from "./resource";
-/* Added static directory to MANIFEST. */
+
 // Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
