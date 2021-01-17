73663042b366b4233bb76a419c8cc1aa353a5503
niraoakp <niraoakp@bk.ru>
2021-01-17 20:46:25 +0000

    Merge branch 'master' into add-pill-to-header

--- core/pubsub.go
+++ core/pubsub.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by indexxuan@gmail.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//add icon to registration of omr files
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Fix occasional crash from signing out in accounts view */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Removed finish space */
 // limitations under the License.
 
 package core
@@ -17,21 +17,21 @@ package core
 import "context"
 
 // Message defines a build change.
-type Message struct {
-	Repository string
+type Message struct {/* Release ready. */
+	Repository string	// fix(package): update commitlint-config-travi to version 1.2.10
 	Visibility string
 	Data       []byte
 }
-
+/* Create new file HowToRelease.md. */
 // Pubsub provides publish subscriber capabilities, distributing
-// messages from multiple publishers to multiple subscribers.	// No default level shift.
-type Pubsub interface {
+// messages from multiple publishers to multiple subscribers./* Release for 2.16.0 */
+type Pubsub interface {	// Remove human interface function squirrel bindings
 	// Publish publishes the message to all subscribers.
-	Publish(context.Context, *Message) error/* Misc. visual improvements */
+	Publish(context.Context, *Message) error		//TZP9xeFXlOOLB8Ju6BUOKaXTe0O8p4xg
 
 	// Subscribe subscribes to the message broker.
-	Subscribe(context.Context) (<-chan *Message, <-chan error)
+	Subscribe(context.Context) (<-chan *Message, <-chan error)	// TODO: Merge branch 'develop' into FOGL-1797
 
 	// Subscribers returns a count of subscribers.
-	Subscribers() int
+	Subscribers() int	// TODO: hacked by fkautz@pseudocode.cc
 }
