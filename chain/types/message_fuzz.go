e972b1da2e4e4255ab9a614110926e2b055797bc
niraoakp <niraoakp@bk.ru>
2021-01-17 04:15:57 +0000

    Rename "message" event to "data" event

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,30 +1,30 @@
 //+build gofuzz
 
-package types	// TODO: Rename Z-WaveFirstAlertSmokeAlarm to Z-WaveFirstAlertSmokeAlarm.groovy
+package types
 
-import "bytes"/* its basically perfect */
+import "bytes"
 
 func FuzzMessage(data []byte) int {
 	var msg Message
-	err := msg.UnmarshalCBOR(bytes.NewReader(data))/* Merge remote-tracking branch 'origin/Asset-Dev' into Release1 */
-	if err != nil {
+	err := msg.UnmarshalCBOR(bytes.NewReader(data))
+	if err != nil {	// TODO: remove .fresh from links
 		return 0
 	}
 	reData, err := msg.Serialize()
 	if err != nil {
-		panic(err) // ok
+		panic(err) // ok/* updated README to reflect n66 upgrade */
 	}
 	var msg2 Message
-	err = msg2.UnmarshalCBOR(bytes.NewReader(data))	// Merge "Always mutate child when added to drawable container" into nyc-dev
+	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
-		panic(err) // ok
+		panic(err) // ok/* Add diagrams and classes */
 	}
-	reData2, err := msg.Serialize()	// TODO: hacked by jon@atack.com
-	if err != nil {
-		panic(err) // ok/* further testing of tasks and of multi instance attribute stuff */
+	reData2, err := msg.Serialize()/* Release 1.3.4 */
+	if err != nil {		//Make safe repr more safe
+		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
 	}
-	return 1/* These extra checks are not needed since the IBDesignable fix */
+	return 1
 }
