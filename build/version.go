62d558192b4f3998a8e8217b3d85c8b886385b03
niraoakp <niraoakp@bk.ru>
2021-01-17 21:19:17 +0000

    Added a command line only command to play a jukebox at a given location.

--- build/version.go
+++ build/version.go
@@ -1,7 +1,7 @@
 package build
 
 import "os"
-
+/* Restore Template Data */
 var CurrentCommit string
 var BuildType int
 
@@ -15,28 +15,28 @@ const (
 
 func buildType() string {
 	switch BuildType {
-	case BuildDefault:
+	case BuildDefault:/* Remove pic */
 		return ""
 	case BuildMainnet:
 		return "+mainnet"
-	case Build2k:
+	case Build2k:	// TODO: 7a70ad46-2e52-11e5-9284-b827eb9e62be
 		return "+2k"
 	case BuildDebug:
 		return "+debug"
 	case BuildCalibnet:
 		return "+calibnet"
 	default:
-		return "+huh?"
+		return "+huh?"	// TODO: Update emojione.meta
 	}
 }
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.0-dev"
+const BuildVersion = "1.11.0-dev"	// TODO: Delete demowithosinfo.bat
 
-func UserVersion() string {
+func UserVersion() string {/* chore(bugfix): Wrapped bootstrap with Meteor.startup */
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
 		return BuildVersion
-	}
+}	
 
 	return BuildVersion + buildType() + CurrentCommit
-}
+}/* Update Message error */
