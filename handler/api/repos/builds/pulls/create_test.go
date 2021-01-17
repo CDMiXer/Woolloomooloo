34d117ee7886582a417fb27329a2ab72852815a3
niraoakp <niraoakp@bk.ru>
2021-01-17 08:47:29 +0000

    780fff2c-2e55-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/pulls/create_test.go
+++ handler/api/repos/builds/pulls/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package pulls	// Weitere xsd. Noch nicht angewandt im Code
+		//Merge "tools: update sca and cpi requirements file"
+package pulls
