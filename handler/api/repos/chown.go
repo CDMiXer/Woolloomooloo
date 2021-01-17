c85b77df0368561df5ef6a15268e9b80959f6c66
niraoakp <niraoakp@bk.ru>
2021-01-17 19:47:01 +0000

    DDBNEXT-1079: social media breaks in compare page

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* rt_frtchord: revert get_size/1 behaviour, add new function for filtered RT size */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Dialed down logging level. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Eggdrop v1.8.4 Release Candidate 2 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
-/* Release 2.0.24 - ensure 'required' parameter is included */
-import (
+
+import (/* Deleted CtrlApp_2.0.5/Release/CL.read.1.tlog */
 	"net/http"
-/* Merge branch 'develop' into SELX-155-Release-1.0 */
+/* [artifactory-release] Release version 1.1.1.M1 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* 3.4.0 Release */
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-
+/* Merge "[INTERNAL] Release notes for version 1.71.0" */
 	"github.com/go-chi/chi"
 )
 
-// HandleChown returns an http.HandlerFunc that processes http
+// HandleChown returns an http.HandlerFunc that processes http/* Merge "Fixing duplicate lang string (Bug #1393622)" */
 // requests to chown the repository to the currently authenticated user.
 func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (	// TODO: Update openshift/docker/backup-pvc/entrypoint.sh
 			owner = chi.URLParam(r, "owner")
-			name  = chi.URLParam(r, "name")/* - Moving complete, world gets skewed as camera changes direction. */
-		)/* Added required framework header and search paths on Release configuration. */
-
+			name  = chi.URLParam(r, "name")/* Some refactoring on the detect dawn script */
+		)
+		//python-pygments: update to 2.9.0
 		repo, err := repos.FindName(r.Context(), owner, name)
-		if err != nil {/* Release version: 1.0.3 [ci skip] */
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* Release 2.0.0-alpha */
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
 		}
 
-		user, _ := request.UserFrom(r.Context())	// 127d14ce-2e4b-11e5-9284-b827eb9e62be
+		user, _ := request.UserFrom(r.Context())
 		repo.UserID = user.ID
-/* Added idea to create offline JSON */
-		err = repos.Update(r.Context(), repo)/* Add another device driver plugin call point for Apple devices */
-		if err != nil {
+
+		err = repos.Update(r.Context(), repo)
+		if err != nil {/* 762d10e4-2d53-11e5-baeb-247703a38240 */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).	// TODO: hacked by lexy8russo@outlook.com
+				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: cannot chown repository")
 		} else {
-			render.JSON(w, repo, 200)
-		}/* update the profile view  */
+			render.JSON(w, repo, 200)/* Release 0.9.1 */
+		}
 	}
 }
