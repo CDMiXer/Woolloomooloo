36768a03c6c152769a233da6fa7bb4c7dbfb5a0a
niraoakp <niraoakp@bk.ru>
2021-01-17 19:51:44 +0000

    Create showget.html

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Added support to UI */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -10,10 +10,10 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Create cherry blossoms.html */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-/* Store and get connection from channel attrs when possible */
+
 // HandleIncomplete returns an http.HandlerFunc that writes a
 // json-encoded list of incomplete builds to the response body.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
@@ -21,10 +21,10 @@ func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 		list, err := repos.ListIncomplete(r.Context())
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* timeit library */
+			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list incomplete builds")
 		} else {
 			render.JSON(w, list, 200)
-		}	// TODO: will be fixed by witek@enjin.io
+		}
 	}
 }
