553dbbd4e5cc6d07b3b387c12e72aa032ea5d1db
niraoakp <niraoakp@bk.ru>
2021-01-17 16:02:09 +0000

    allow uploading by drag and drop

--- handler/api/repos/encrypt/encrypt.go
+++ handler/api/repos/encrypt/encrypt.go
@@ -1,49 +1,49 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by mikeal.rogers@gmail.com
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//rev 785664
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// MAXIMALLY.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: TECG-39 - Configuration
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package encrypt
 
-import (
+import (		//web: show +subs link only when there are subaccounts
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"	// Merge branch 'wip-5759' into wip-5759
+	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
 	"io"
-	"net/http"/* Fix #80 (sort of) */
+	"net/http"		//Update spreadsheet.rb
 
-	"github.com/drone/drone-go/drone"/* Release v4.3.0 */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: Merge "Pass on arguments on Base.get_session"
+	"github.com/drone/drone-go/drone"
+	"github.com/drone/drone/core"/* Added associations-view in closure browser */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/go-chi/chi"
 )
 
 type respEncrypted struct {
 	Data string `json:"data"`
 }
-
-// Handler returns an http.HandlerFunc that processes http
+	// 0rZdUXXN1GJQon2LQztMri6ikvlbohe8
+// Handler returns an http.HandlerFunc that processes http/* added cargo plugin for deployment */
 // requests to create an encrypted secret.
-func Handler(repos core.RepositoryStore) http.HandlerFunc {/* Report now uses named pipe in pipe mode. */
+func Handler(repos core.RepositoryStore) http.HandlerFunc {/* Graph requests originating from the Ajax Spider */
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
-		name := chi.URLParam(r, "name")
-		repo, err := repos.FindName(r.Context(), namespace, name)	// fix: Do not manually drop the table
-		if err != nil {
+		name := chi.URLParam(r, "name")/* better hash */
+		repo, err := repos.FindName(r.Context(), namespace, name)		//Create CSS learning
+		if err != nil {/* Adding spaces for proper rendering. */
 			render.NotFound(w, err)
 			return
-		}/* Release v0.1.6 */
+		}
 
 		in := new(drone.Secret)
 		err = json.NewDecoder(r.Body).Decode(in)
@@ -52,37 +52,37 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {/* Report now uses na
 			return
 		}
 
-		// the secret is encrypted with a per-repository 256-bit/* Releases 0.0.7 */
-		// key. If the key is missing or malformed we should
+		// the secret is encrypted with a per-repository 256-bit
+		// key. If the key is missing or malformed we should/* Released DirectiveRecord v0.1.22 */
 		// return an error to the client.
-		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))	// TODO: Merge "Be more clear about what data types we expect in links array"
+		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))
 		if err != nil {
 			render.InternalError(w, err)
-			return/* [artifactory-release] Release version 0.9.9.RELEASE */
+			return
 		}
 
 		// the encrypted secret is embedded in the yaml
 		// configuration file and is json-encoded for
-		// inclusion as a !binary attribute.
+		// inclusion as a !binary attribute./* Merge "Fix up missed refactoring in JNI reg and preloaded-classes" */
 		encoded := base64.StdEncoding.EncodeToString(encrypted)
 
-		render.JSON(w, &respEncrypted{Data: encoded}, 200)/* e7f7e358-2e4c-11e5-9284-b827eb9e62be */
+		render.JSON(w, &respEncrypted{Data: encoded}, 200)
 	}
 }
-
+		//Fix array()
 func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
 	block, err := aes.NewCipher(key[:])
 	if err != nil {
 		return nil, err
-}	
-	// TODO: will be fixed by qugou1350636@126.com
+	}
+/* Release 0.30-alpha1 */
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
 	}
 
 	nonce := make([]byte, gcm.NonceSize())
-	_, err = io.ReadFull(rand.Reader, nonce)
+	_, err = io.ReadFull(rand.Reader, nonce)		//remove canned readme.
 	if err != nil {
 		return nil, err
 	}
