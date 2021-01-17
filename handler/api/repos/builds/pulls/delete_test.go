55dd2a92194de5655ce31e1d8565ddedfdb57ffe
niraoakp <niraoakp@bk.ru>
2021-01-17 13:22:05 +0000

    Updated link to implemented rules

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Create addons.xml.md5 */
+// that can be found in the LICENSE file./* Release 2.2b1 */
 
-package pulls	// TODO: hacked by juan@benet.ai
+package pulls
