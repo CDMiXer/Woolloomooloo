f6d925d13b2cc75bb749cdffba852a8b7d241f75
niraoakp <niraoakp@bk.ru>
2021-01-17 19:06:11 +0000

    Merge "Deprecate resources_prefix and change rand_name()"

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,14 +1,14 @@
-package reward
-
+package reward/* Added converters between normalized/un-normalized fields. */
+/* widget editor start */
 import (
-	"github.com/filecoin-project/go-state-types/abi"/* Deleted CtrlApp_2.0.5/Release/ctrl_app.lastbuildstate */
+	"github.com/filecoin-project/go-state-types/abi"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
-	"github.com/ipfs/go-cid"/* Remove extraneous comma. */
+	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/cbor"		//Fix portlet 18: Show Dossier By govAgencyCode
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* Release builds in \output */
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
@@ -16,31 +16,31 @@ import (
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: hacked by steven@stebalien.com
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* fixed problem with jar adding when writing yaml */
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release 7-SNAPSHOT */
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Reorganize a bit. No functional change, just moving patterns up. */
 func init() {
-
+		//* updated traditional chinese and italian language files
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
-	})
+	})/* refactored factories; deletables */
 
-	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)	// TODO: will be fixed by jon@atack.com
-	})	// TODO: update of silly-collections dependency version
+	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Delete InstanceControllerTest.php
+		return load2(store, root)
+	})
 
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)		//Update sessions_helper.rb
-	})	// TODO: hacked by aeongrp@outlook.com
-
-	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)/* fix Grid redraw */
+		return load3(store, root)		//7190f620-2e5e-11e5-9284-b827eb9e62be
 	})
+
+	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Fix CHINATELECOM name
+		return load4(store, root)
+	})		//Update CHANGELOG for #16412
 }
-/* Release of eeacms/forests-frontend:1.8.6 */
-var (	// TODO: missing part in SequenceVar
+
+var (
 	Address = builtin4.RewardActorAddr
 	Methods = builtin4.MethodsReward
 )
@@ -51,21 +51,21 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
 
-	case builtin2.RewardActorCodeID:
+	case builtin2.RewardActorCodeID:/* Use UTF8 for advances too */
 		return load2(store, act.Head)
 
 	case builtin3.RewardActorCodeID:
 		return load3(store, act.Head)
 
 	case builtin4.RewardActorCodeID:
-		return load4(store, act.Head)
+		return load4(store, act.Head)		//Test and Bug Fixes
 
-	}		//First draft of activity diagram
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}
-		//Updating journey/complete/reporting-dashboard.html via Laneworks CMS Publish
-type State interface {/* Added regex and validationMessage to UserNameTextBox */
-	cbor.Marshaler/* renamed for consistency (nw) */
+}		//Delete IctpMeeting.css
+
+type State interface {/* Released 0.0.14 */
+	cbor.Marshaler
 
 	ThisEpochBaselinePower() (abi.StoragePower, error)
 	ThisEpochReward() (abi.StoragePower, error)
