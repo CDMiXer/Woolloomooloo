e9a285ed90d08061f9353a531e7d9e1063e10880
niraoakp <niraoakp@bk.ru>
2021-01-17 07:09:16 +0000

    ajout repair_accent() pour afficher les accent en HTML

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
-#!/bin/bash	// Update and rename Menuoverlay1.1.css to Menuoverlay1.2.css
-set -eux -o pipefail/* x11 cleanup (remove superfluous set_perms) */
+#!/bin/bash
+set -eux -o pipefail
 
-bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \	// TODO: Delete ssock.h.gch
+bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \		//add css transition to hover styles
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
   workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
