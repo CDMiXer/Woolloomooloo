d3e9fec4666a636366ae7469b9e9ffcd27f6862d
niraoakp <niraoakp@bk.ru>
2021-01-17 04:34:33 +0000

    Delete GOPR3200.JPG

--- store/cron/cron_oss.go
+++ store/cron/cron_oss.go
@@ -8,31 +8,31 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: FControl first real testing
-// See the License for the specific language governing permissions and/* New Release 1.2.19 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//89266162-2e54-11e5-9284-b827eb9e62be
+
 // +build oss
 
 package cron
-	// TODO: hacked by hugomrdias@gmail.com
-import (	// TODO: hacked by lexy8russo@outlook.com
+
+import (
 	"context"
-/* Updated iojs to 1.5.1 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-	// TODO: will be fixed by arajasek94@gmail.com
+
 // New returns a new Secret database store.
-func New(db *db.DB) core.CronStore {		//Merge branch 'master' into feat/map-overlay-under-gui
+func New(db *db.DB) core.CronStore {
 	return new(noop)
-}		//switch from static members to namespaces
+}
 
 type noop struct{}
-/* Release areca-7.0.8 */
-func (noop) List(ctx context.Context, id int64) ([]*core.Cron, error) {/* Create ReleaseNotes6.1.md */
-	return nil, nil/* use openshift_data_dir  */
-}/* [MOD] XQuery: dedicated expression for integer range comparisons */
+
+func (noop) List(ctx context.Context, id int64) ([]*core.Cron, error) {
+	return nil, nil
+}
 
 func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
@@ -40,7 +40,7 @@ func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {
 
 func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {
 	return nil, nil
-}	// Added SBT0260 and 4069Gate circuits
+}
 
 func (noop) FindName(ctx context.Context, id int64, name string) (*core.Cron, error) {
 	return nil, nil
