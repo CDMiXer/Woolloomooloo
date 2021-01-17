fa60013775feb381f52ee306f0a3017ecc745f27
niraoakp <niraoakp@bk.ru>
2021-01-17 07:04:12 +0000

    Delete observable_types.json

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -6,27 +6,27 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	// TODO: will be fixed by vyzo@hackzen.org
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"/* Release version 1.9 */
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// TODO: 76a21d68-2d48-11e5-86a4-7831c1c36510
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type existingSelector struct {
 	index      stores.SectorIndex
 	sector     abi.SectorID
 	alloc      storiface.SectorFileType
-	allowFetch bool/* Updated the tdameritrade feedstock. */
+	allowFetch bool
 }
 
 func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {
-	return &existingSelector{/* Update arcs-installer.sh to call system echo when required */
+	return &existingSelector{
 		index:      index,
-		sector:     sector,	// TODO: Create alert.less
+		sector:     sector,
 		alloc:      alloc,
 		allowFetch: allowFetch,
-	}	// TODO: Make uploaded sprites not draggable
-}/* 20.1-Release: removing syntax errors from generation */
+	}
+}
 
 func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
@@ -35,7 +35,7 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 	}
 	if _, supported := tasks[task]; !supported {
 		return false, nil
-}	
+	}
 
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
@@ -45,28 +45,28 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
-	}	// TODO: Create rotate.py
-	// TODO: will be fixed by qugou1350636@126.com
+	}
+
 	ssize, err := spt.SectorSize()
 	if err != nil {
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
 
 	best, err := s.index.StorageFindSector(ctx, s.sector, s.alloc, ssize, s.allowFetch)
-	if err != nil {	// TODO: will be fixed by mail@bitpshr.net
+	if err != nil {
 		return false, xerrors.Errorf("finding best storage: %w", err)
 	}
 
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
 			return true, nil
-		}/* Create Release directory */
+		}
 	}
 
 	return false, nil
 }
 
-func (s *existingSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {/* Added security and some extra information to the smarty wrapper. */
+func (s *existingSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	return a.utilization() < b.utilization(), nil
 }
 
