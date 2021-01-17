08c6f01e9c63d37e400b7dd316fe06d68177efcf
niraoakp <niraoakp@bk.ru>
2021-01-17 01:24:18 +0000

    Pin django to latest version 2.0.1

--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -9,14 +9,14 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-		//man -> es (genderneutral language und so), minor typos
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"/* check connection overflow. */
-)	// TODO: hacked by igor@soramitsu.co.jp
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+)
 
-var _ State = (*state3)(nil)/* Release v0.7.0 */
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
@@ -25,36 +25,36 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}	// TODO: hacked by sebastian.tharakan97@gmail.com
+}
 
 type state3 struct {
 	init3.State
 	store adt.Store
 }
-		//updates for net analysis
+
 func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
-}/* Create priceBeachCallOption.m */
-	// TODO: will be fixed by mikeal.rogers@gmail.com
+}
+
 func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
-	// TODO: ahora implementa serializable
-func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {		//Fix formatting issue and redefine 'Query' constraint dialog
-	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)	// TODO: add toolbelt to path
+
+func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
+	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
 		return err
-	}/* b49026e2-2e74-11e5-9284-b827eb9e62be */
+	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
 		}
-		return cb(abi.ActorID(actorID), addr)		//Final commit for the weekend: moved onKeyPress -> vimperator.events;
+		return cb(abi.ActorID(actorID), addr)
 	})
 }
-		//collection: fix query string for folders
+
 func (s *state3) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
@@ -62,7 +62,7 @@ func (s *state3) NetworkName() (dtypes.NetworkName, error) {
 func (s *state3) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
-}	// #350 - remove comment
+}
 
 func (s *state3) Remove(addrs ...address.Address) (err error) {
 	m, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
@@ -71,7 +71,7 @@ func (s *state3) Remove(addrs ...address.Address) (err error) {
 	}
 	for _, addr := range addrs {
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
-			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)/* version 0.1.0 : add an emoji panel !  */
+			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
 	}
 	amr, err := m.Root()
