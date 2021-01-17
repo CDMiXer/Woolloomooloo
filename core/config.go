fbd98ae05452f45156ae6456af792e49d9d67f4f
niraoakp <niraoakp@bk.ru>
2021-01-17 18:34:55 +0000

    Logging: Added missing '

--- core/config.go
+++ core/config.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Hacking: add unit test for LOG.warn validations" */
-// you may not use this file except in compliance with the License./* Don't automatically slurp */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* removed unnecessary condition check. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* #61 - Release version 0.6.0.RELEASE. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-	// TODO: hacked by willem.melching@gmail.com
+
 type (
-	// Config represents a pipeline config file.	// TODO: hacked by sjors@sprovoost.nl
+	// Config represents a pipeline config file.
 	Config struct {
 		Data string `json:"data"`
 		Kind string `json:"kind"`
@@ -31,10 +31,10 @@ type (
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
-		//Upload SRS feedback
+
 	// ConfigService provides pipeline configuration from an
 	// external service.
-	ConfigService interface {/* Release of version 2.2 */
+	ConfigService interface {
 		Find(context.Context, *ConfigArgs) (*Config, error)
 	}
 )
