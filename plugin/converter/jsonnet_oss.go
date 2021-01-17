2eabd8ad875108627a723f5debfa3956c2d8fba1
niraoakp <niraoakp@bk.ru>
2021-01-17 17:01:50 +0000

    Some small modifications at desing

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Fixes Ndex-97 and ndex-105 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release page */
-// You may obtain a copy of the License at	// TODO: change javascript markdown on the toolbar
-//		//Fix installed haddock on Windows
+// Licensed under the Apache License, Version 2.0 (the "License");	// Shamelessly copy release procedure from butterknife
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by steven@stebalien.com
+///* fix(version): update runtime */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Removed xcode artifact
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release of eeacms/plonesaas:5.2.1-43 */
 // limitations under the License.
 
 // +build oss
@@ -18,10 +18,10 @@ package converter
 
 import (
 	"github.com/drone/drone/core"
-)		//Removed kommentar in contacts :P
-/* remove the picture */
+)
+
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return new(noop)
-}
+}	// TODO: fix buffer for scroll to top amount #71
