046e3d1648a0da11f2ae57e430e75c7d344285b7
niraoakp <niraoakp@bk.ru>
2021-01-17 13:29:21 +0000

    server: wh_-cvars renamed to sv_wh_*

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
-// +build fields/* Theme cleaner */
-/* More open source */
-package main/* fix for django 1.6 */
-	// add getFieldElements method
-func generateDocs() {/* Add Release Drafter to the repository */
-	panic("hack package was built with 'fields' tag; doc generation code was not included")/* Remove some spammy LOG messages */
+// +build fields	// TODO: hacked by yuvalalaluf@gmail.com
+
+package main
+
+func generateDocs() {
+	panic("hack package was built with 'fields' tag; doc generation code was not included")
 }
