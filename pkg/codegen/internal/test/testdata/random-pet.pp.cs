53a8f379d808ac8ffd9c7f319d0b5c993dd2472b
niraoakp <niraoakp@bk.ru>
2021-01-17 00:05:57 +0000

    bf45766e-2e60-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,14 +1,14 @@
-using Pulumi;
+using Pulumi;	// TODO: hacked by admin@multicoin.co
 using Random = Pulumi.Random;
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs/* Merge "Cache the result of getUserLanguageInfo()" */
+        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
         {
             Prefix = "doggo",
-        });
+        });		//Merge "Fix network segment range "_get_ranges" function"
     }
 
 }
