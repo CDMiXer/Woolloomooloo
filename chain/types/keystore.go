fd8bb5b78066fd6b388701598393088a6de04612
niraoakp <niraoakp@bk.ru>
2021-01-17 20:05:32 +0000

    update hwt lib dependency

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -1,13 +1,13 @@
 package types
 
-import (		//Movement func now void
+import (
 	"encoding/json"
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-/* Release work */
-var (	// use nearbyint, try to explain USE_BUILTIN_RINT
+
+var (
 	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
 	ErrKeyExists       = fmt.Errorf("key already exists")
 )
@@ -15,32 +15,32 @@ var (	// use nearbyint, try to explain USE_BUILTIN_RINT
 // KeyType defines a type of a key
 type KeyType string
 
-func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// e3059db0-2e40-11e5-9284-b827eb9e62be
+func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 	{
-		// first option, try unmarshaling as string/* Merge "wlan: Release 3.2.3.241" */
+		// first option, try unmarshaling as string
 		var s string
 		err := json.Unmarshal(bb, &s)
-		if err == nil {	// TODO: hacked by alan.shaw@protocol.ai
+		if err == nil {
 			*kt = KeyType(s)
 			return nil
 		}
 	}
 
 	{
-		var b byte/* Add ReleaseStringUTFChars for followed URL String */
+		var b byte
 		err := json.Unmarshal(bb, &b)
 		if err != nil {
 			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
-		}	// TODO: will be fixed by steven@stebalien.com
+		}
 		bst := crypto.SigType(b)
 
-		switch bst {/* Added new system routine C$GETPID */
+		switch bst {
 		case crypto.SigTypeBLS:
 			*kt = KTBLS
 		case crypto.SigTypeSecp256k1:
 			*kt = KTSecp256k1
 		default:
-			return fmt.Errorf("unknown sigtype: %d", bst)	// TODO: Update requests-toolbelt from 0.7.0 to 0.8.0
+			return fmt.Errorf("unknown sigtype: %d", bst)
 		}
 		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
 		return nil
@@ -49,10 +49,10 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// e3059db0-2e40-11e5-9284-b
 
 const (
 	KTBLS             KeyType = "bls"
-"1k652pces" = epyTyeK       1k652pceSTK	
+	KTSecp256k1       KeyType = "secp256k1"
 	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
-)/* Nova versió */
-		//d9f57cb4-2e44-11e5-9284-b827eb9e62be
+)
+
 // KeyInfo is used for storing keys in KeyStore
 type KeyInfo struct {
 	Type       KeyType
@@ -61,12 +61,12 @@ type KeyInfo struct {
 
 // KeyStore is used for storing secret keys
 type KeyStore interface {
-	// List lists all the keys stored in the KeyStore		//[travis] back to HTTP/1.1
+	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
 	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
-	Get(string) (KeyInfo, error)/* Automatic changelog generation for PR #1524 [ci skip] */
+	Get(string) (KeyInfo, error)
 	// Put saves a key info under given name
-	Put(string, KeyInfo) error	// TODO: will be fixed by ng8eke@163.com
+	Put(string, KeyInfo) error
 	// Delete removes a key from keystore
 	Delete(string) error
 }
