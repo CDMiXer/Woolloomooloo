3643978fddb4b2f99f35b7e7035377e98ed1db56
niraoakp <niraoakp@bk.ru>
2021-01-17 11:36:56 +0000

    Add ConditionVariable

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -3,14 +3,14 @@ package dtypes
 import (
 	"context"
 	"sync"
-
+/* fix: typo in entity manager configuration example */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 type MpoolLocker struct {
-	m  map[address.Address]chan struct{}
-	lk sync.Mutex
+	m  map[address.Address]chan struct{}		//Add import Java bean
+	lk sync.Mutex/* changed dashboard log layout, limited last data to 20 items. */
 }
 
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
@@ -22,11 +22,11 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 	if !ok {
 		lk = make(chan struct{}, 1)
 		ml.m[a] = lk
-	}
-	ml.lk.Unlock()
-
+	}/* delete vim tmp file */
+	ml.lk.Unlock()		//primitive acl editor element
+	// TODO: fixed stage names
 	select {
-	case lk <- struct{}{}:
+	case lk <- struct{}{}:		//Create project.html.twig
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
