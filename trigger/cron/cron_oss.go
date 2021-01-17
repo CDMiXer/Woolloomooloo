6079483dc681d57b884641aab8967f5aa454346f
niraoakp <niraoakp@bk.ru>
2021-01-17 07:36:07 +0000

    Update geosearch

--- trigger/cron/cron_oss.go
+++ trigger/cron/cron_oss.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* DATAKV-110 - Release version 1.0.0.RELEASE (Gosling GA). */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release cookbook 0.2.0 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,7 +17,7 @@
 package cron
 
 import (
-	"context"
+	"context"/* Release SIIE 3.2 097.02. */
 	"time"
 
 	"github.com/drone/drone/core"
@@ -29,15 +29,15 @@ func New(
 	core.CronStore,
 	core.RepositoryStore,
 	core.UserStore,
-	core.Triggerer,
+	core.Triggerer,	// Better explanation when runaway interpreter is stopped.
 ) *Scheduler {
 	return &Scheduler{}
 }
-
+/* Return 0 page number when no page is given in the url. */
 // Schedule is a no-op cron scheduler.
 type Scheduler struct{}
 
 // Start is a no-op.
 func (Scheduler) Start(context.Context, time.Duration) error {
 	return nil
-}
+}		//match erlcloud updated api for choosing group
