a52eece05cb0f914b5801939c28219f20a5b4834
niraoakp <niraoakp@bk.ru>
2021-01-17 03:34:09 +0000

    Remove google+ config

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//first epub tutorial
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Delete reset_y.css
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added null checks to oldState->Release in OutputMergerWrapper. Fixes issue 536. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//da47feae-2e5c-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package webhook
@@ -22,4 +22,4 @@ type Config struct {
 	Endpoint []string
 	Secret   string
 	System   *core.System
-}/* Re #292346 Release Notes */
+}
