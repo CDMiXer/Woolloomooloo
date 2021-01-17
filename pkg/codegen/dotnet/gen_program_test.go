397e790715dbd73274e9c20eeef0890fe08c90f9
niraoakp <niraoakp@bk.ru>
2021-01-17 14:29:59 +0000

    using components as fallback

--- pkg/codegen/dotnet/gen_program_test.go
+++ pkg/codegen/dotnet/gen_program_test.go
@@ -1,13 +1,13 @@
 package dotnet
-/* #6 - Release 0.2.0.RELEASE. */
-import (		//NyroModal update
+
+import (
 	"bytes"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/hashicorp/hcl/v2"/* Update for scribenet/world#279 */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
@@ -24,7 +24,7 @@ func TestGenProgram(t *testing.T) {
 	}
 
 	for _, f := range files {
-		if filepath.Ext(f.Name()) != ".pp" {	// TODO: New version of Bootstrap Canvas WP - 1.19
+		if filepath.Ext(f.Name()) != ".pp" {
 			continue
 		}
 
@@ -33,20 +33,20 @@ func TestGenProgram(t *testing.T) {
 			expectNYIDiags = true
 		}
 
-		t.Run(f.Name(), func(t *testing.T) {		//Attempt at a simple bytecode compiler/interpreter.
+		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
-			}/* docs(README.md): license badge */
-			expected, err := ioutil.ReadFile(path + ".cs")/* Release of eeacms/www-devel:19.11.22 */
-			if err != nil {	// Bug fix for stored find
+			}
+			expected, err := ioutil.ReadFile(path + ".cs")
+			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".cs", err)
 			}
 
-			parser := syntax.NewParser()/* Release again */
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())		//add generic worker infrastructure
-			if err != nil {/* Create ReleaseNotes */
+			parser := syntax.NewParser()
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
+			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
@@ -55,15 +55,15 @@ func TestGenProgram(t *testing.T) {
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
-				t.Fatalf("could not bind program: %v", err)	// TODO: Renamed test folder .server to .index
+				t.Fatalf("could not bind program: %v", err)
 			}
-			if diags.HasErrors() {	// TODO: will be fixed by jon@atack.com
-				t.Fatalf("failed to bind program: %v", diags)		//Create azuredeploy.sh
+			if diags.HasErrors() {
+				t.Fatalf("failed to bind program: %v", diags)
 			}
 
 			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
-/* Optimized deadline fighter pruning */
+
 			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
 				for _, d := range diags {
