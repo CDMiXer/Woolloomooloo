0e5d23d731db96f4eb875cfdd25145935a65f8e1
niraoakp <niraoakp@bk.ru>
2021-01-17 01:26:56 +0000

    Moving icons paths from map to DatastoreDescriptor.

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -eu -o pipefail	// TODO: will be fixed by alan.shaw@protocol.ai
+set -eu -o pipefail
 
 for m in $*; do
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)	// TODO: hacked by arachnid@notdot.net
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
   cd -
-done	// TODO: 20d94ee8-2e6e-11e5-9284-b827eb9e62be
+done
