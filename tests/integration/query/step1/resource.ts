f5176a85574277743ccbe13560d177883276d21f
niraoakp <niraoakp@bk.ru>
2021-01-17 13:24:36 +0000

    added TestHelp class

--- tests/integration/query/step1/resource.ts
+++ tests/integration/query/step1/resource.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
+/* Release areca-7.2 */
+import * as pulumi from "@pulumi/pulumi";/* Release 0.1.6. */
 
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
+;)(redivorP wen = ecnatsni ylnodaer citats cilbup    
 
-    public async create(inputs: any) {
+    public async create(inputs: any) {	// TODO: MassBuild#289: Increase release tag
         return {
             id: (currentID++).toString(),
             outs: undefined,
@@ -15,12 +15,12 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
     }
 }
 
-export class Resource extends pulumi.dynamic.Resource {
-    public isInstance(o: any): o is Resource {
+export class Resource extends pulumi.dynamic.Resource {		//docs: draft release notes
+    public isInstance(o: any): o is Resource {	// removed .temp test file
         return o.__pulumiType === "pulumi-nodejs:dynamic:Resource";
     }
 
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);
-    }
+        super(Provider.instance, name, props, opts);/* Delete 02.Square of Stars.js */
+    }/* Added Release Notes for 1.11.3 release */
 }
