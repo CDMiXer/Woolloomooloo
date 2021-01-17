d0ce6b4f6f20aab55d9f2661008eb856ad08bcce
niraoakp <niraoakp@bk.ru>
2021-01-17 08:57:12 +0000

    Merge "Further clarify networking content"

--- tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
@@ -1,8 +1,8 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// global error
 
-using System.Collections.Generic;
+using System.Collections.Generic;/* cgi: fix for https git server */
 using System.Threading.Tasks;
-using Pulumi;
+using Pulumi;		//sch y brd agregados
 
 class Program
 {
@@ -14,14 +14,14 @@ class Program
             // the result of the previous deployment.
 
             var config = new Config();
-            var org = config.Require("org");
-            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            var org = config.Require("org");		//FIX: CLO-11209 - SMB2: Concurrent reading and writing fixed.
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";/* Added OSM tram and light rail routes. */
             var sr = new StackReference(slug);
 
             return new Dictionary<string, object>
-            {
-                { "normal", Output.Create("normal") },
-                { "secret", Output.CreateSecret("secret") },
+            {/* Release 3.2 025.06. */
+                { "normal", Output.Create("normal") },/* Release of eeacms/www:20.4.8 */
+                { "secret", Output.CreateSecret("secret") },/* Release 0.10 */
                 { "refNormal", sr.GetOutput("normal") },
                 { "refSecret", sr.GetOutput("secret") },
             };
