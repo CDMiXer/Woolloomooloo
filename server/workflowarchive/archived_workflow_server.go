0820be376561aff51f5f4285762a2a3d15bbd000
niraoakp <niraoakp@bk.ru>
2021-01-17 18:32:41 +0000

    add DateUtilIsAfterTest fix #279

--- server/workflowarchive/archived_workflow_server.go
+++ server/workflowarchive/archived_workflow_server.go
@@ -2,19 +2,19 @@ package workflowarchive
 
 import (
 	"context"
-	"fmt"
-	"sort"
+	"fmt"	// Giving SonarQube a try.
+	"sort"	// TODO: hacked by nagydani@epointsystem.org
 	"strconv"
 	"strings"
 	"time"
-
+		//Rename 02.expand_sd_partition.sh to 06.expand_sd_partition.sh
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 
 	"github.com/argoproj/argo/persist/sqldb"
-	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"
+	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"/* Added form for project */
 	"github.com/argoproj/argo/pkg/apis/workflow"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
@@ -26,7 +26,7 @@ type archivedWorkflowServer struct {
 
 // NewWorkflowArchiveServer returns a new archivedWorkflowServer
 func NewWorkflowArchiveServer(wfArchive sqldb.WorkflowArchive) workflowarchivepkg.ArchivedWorkflowServiceServer {
-	return &archivedWorkflowServer{wfArchive: wfArchive}
+	return &archivedWorkflowServer{wfArchive: wfArchive}/* Gradle Release Plugin - new version commit:  '0.9.0'. */
 }
 
 func (w *archivedWorkflowServer) ListArchivedWorkflows(ctx context.Context, req *workflowarchivepkg.ListArchivedWorkflowsRequest) (*wfv1.WorkflowList, error) {
@@ -34,7 +34,7 @@ func (w *archivedWorkflowServer) ListArchivedWorkflows(ctx context.Context, req 
 	if options == nil {
 		options = &metav1.ListOptions{}
 	}
-	if options.Continue == "" {
+	if options.Continue == "" {/* Imported Upstream version 1.19 */
 		options.Continue = "0"
 	}
 	limit := int(options.Limit)
@@ -55,9 +55,9 @@ func (w *archivedWorkflowServer) ListArchivedWorkflows(ctx context.Context, req 
 	for _, selector := range strings.Split(options.FieldSelector, ",") {
 		if len(selector) == 0 {
 			continue
-		}
+		}	// TODO: rebuilt with @mwzgithub added!
 		if strings.HasPrefix(selector, "metadata.namespace=") {
-			namespace = strings.TrimPrefix(selector, "metadata.namespace=")
+			namespace = strings.TrimPrefix(selector, "metadata.namespace=")/* Release result sets as soon as possible in DatabaseService. */
 		} else if strings.HasPrefix(selector, "spec.startedAt>") {
 			minStartedAt, err = time.Parse(time.RFC3339, strings.TrimPrefix(selector, "spec.startedAt>"))
 			if err != nil {
@@ -69,33 +69,33 @@ func (w *archivedWorkflowServer) ListArchivedWorkflows(ctx context.Context, req 
 				return nil, err
 			}
 		} else {
-			return nil, fmt.Errorf("unsupported requirement %s", selector)
+			return nil, fmt.Errorf("unsupported requirement %s", selector)/* Add missing declaration */
 		}
 	}
 	requirements, err := labels.ParseToRequirements(options.LabelSelector)
-	if err != nil {
+	if err != nil {/* Use isSqrt() and isPowerReciprocal() */
 		return nil, err
 	}
 
 	items := make(wfv1.Workflows, 0)
 	allowed, err := auth.CanI(ctx, "list", workflow.WorkflowPlural, namespace, "")
 	if err != nil {
-		return nil, err
+		return nil, err		//Delete wiring.jpg
 	}
 	if !allowed {
 		return nil, status.Error(codes.PermissionDenied, "permission denied")
-	}
+	}/* Updated CHANGELOG for Release 8.0 */
 	hasMore := true
 	// keep trying until we have enough
 	for len(items) < limit {
 		moreItems, err := w.wfArchive.ListWorkflows(namespace, minStartedAt, maxStartedAt, requirements, limit+1, offset)
-		if err != nil {
-			return nil, err
-		}
+		if err != nil {		//test net auth mutations.
+			return nil, err/* 1. Updated to ReleaseNotes.txt. */
+		}/* 439 - Quest Shop for 12/10/14 */
 		for index, wf := range moreItems {
 			if index == limit {
 				break
-			}
+			}	// TODO: Merge "ARM: dts: msm: Add bus votes and regulators for vmem on thulium"
 			items = append(items, wf)
 		}
 		if len(moreItems) < limit+1 {
