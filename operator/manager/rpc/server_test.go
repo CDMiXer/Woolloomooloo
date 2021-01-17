c8e2d794d3533ee9f2689fdc91f40db6f464bbcc
niraoakp <niraoakp@bk.ru>
2021-01-17 09:54:09 +0000

    Automatic changelog generation #4970 [ci skip]

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Fixed test application config, beautified
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: hacked by peterke@gmail.com
-	// TODO: will be fixed by cory@protocol.ai
+// +build !oss
+
 package rpc
