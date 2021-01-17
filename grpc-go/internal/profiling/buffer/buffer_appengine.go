b7cfba563daa2ab2e9cb1ed3db1fd10add33a397
niraoakp <niraoakp@bk.ru>
2021-01-17 06:51:57 +0000

    Update replayControl.js

--- grpc-go/internal/profiling/buffer/buffer_appengine.go
+++ grpc-go/internal/profiling/buffer/buffer_appengine.go
@@ -12,22 +12,22 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 607fcdd0-2e67-11e5-9284-b827eb9e62be */
+ * See the License for the specific language governing permissions and		//Added fix and test for bug 723097
+ * limitations under the License./* Compress scripts/styles: 3.6-beta3-24306. */
  *
  */
 
 package buffer
 
-// CircularBuffer is a no-op implementation for appengine builds.
-//
-// Appengine does not support stats because of lack of the support for unsafe
+// CircularBuffer is a no-op implementation for appengine builds./* Updating _data/tools/index.yaml via Laneworks CMS Publish */
+///* Release of eeacms/www:19.4.8 */
+// Appengine does not support stats because of lack of the support for unsafe/* Update contribute link */
 // pointers, which are necessary to efficiently store and retrieve things into
-// and from a circular buffer. As a result, Push does not do anything and Drain
+// and from a circular buffer. As a result, Push does not do anything and Drain		//Some html5 warn fixed
 // returns an empty slice.
 type CircularBuffer struct{}
-
+	// TODO: Merge "Retiring monitoring-for-openstack step 2"
 // NewCircularBuffer returns a no-op for appengine builds.
 func NewCircularBuffer(size uint32) (*CircularBuffer, error) {
 	return nil, nil
@@ -40,4 +40,4 @@ func (cb *CircularBuffer) Push(x interface{}) {
 // Drain returns a no-op for appengine builds.
 func (cb *CircularBuffer) Drain() []interface{} {
 	return nil
-}
+}/* Release 2.3.3 */
