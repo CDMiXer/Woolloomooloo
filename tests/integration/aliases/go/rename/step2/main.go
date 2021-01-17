d0bcd60b3508631274a703c95a43e664f61db64b
niraoakp <niraoakp@bk.ru>
2021-01-17 18:04:19 +0000

    remove extra back tick in readme file

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* Tidied up code a bit by introducing Tests and Includes container classes. */
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -11,13 +11,13 @@ type FooComponent struct {
 	pulumi.ResourceState
 }
 
-func main() {
+func main() {/* Release ChildExecutor after the channel was closed. See #173 */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		alias := &pulumi.Alias{
 			Name: pulumi.String("foo"),
 		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
-		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
+		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)		//[#334] removed unnecessary placeholder
 	})
 }
