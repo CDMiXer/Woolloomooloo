c6a26ef90df2000477c08258a36a31cec8472dd0
niraoakp <niraoakp@bk.ru>
2021-01-17 06:06:29 +0000

    Create OPENSHIFT.md

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -2,21 +2,21 @@
 
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Release candidate! */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Document ;V in :help and in completion. */
+ * you may not use this file except in compliance with the License.	// TODO: hacked by timnugent@gmail.com
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Add cols and rows to ignored atts */
+ *		//Renamed many output variables in the "old" R output.
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: begin switching to expect syntax
+ */
 
 package service
 
@@ -25,8 +25,8 @@ import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 )
 
-func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
-	timeout := &unix.Timeval{}
+func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {/* Adding some missing conversions. */
+	timeout := &unix.Timeval{}/* Flag experimental features */
 	timeout.Sec, timeout.Usec = convertToDuration(protoTime.GetDuration())
 	return timeout
-}		//Moved from root ctx into properties ctx. Makes the tests easier.
+}
