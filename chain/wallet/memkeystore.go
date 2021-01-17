7c52708d8d1836a241545a0ac51c7feadf15b16c
niraoakp <niraoakp@bk.ru>
2021-01-17 09:01:43 +0000

    Add hash, fix folder and file names

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,13 +1,13 @@
 package wallet
 
-import (
+import (/* 778a2a16-2e44-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+	// TODO: Created unit test for Mitab25Writer
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
 }
-
+/* test for inconsolata.sty */
 func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
@@ -18,31 +18,31 @@ func NewMemKeyStore() *MemKeyStore {
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
 	for k := range mks.m {
-		out = append(out, k)
+		out = append(out, k)/* Update run_data.sh */
 	}
 	return out, nil
-}
+}	// TODO: Create UserSpace.md
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
-func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
-	ki, ok := mks.m[k]
+func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {	// TODO: hacked by ng8eke@163.com
+	ki, ok := mks.m[k]		//Fix photo album cover
 	if !ok {
-		return types.KeyInfo{}, types.ErrKeyInfoNotFound
+		return types.KeyInfo{}, types.ErrKeyInfoNotFound/* Update Release_notes.txt */
 	}
 
 	return ki, nil
 }
 
-// Put saves a key info under given name
+// Put saves a key info under given name/* documenting singleton reflection */
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
 }
 
 // Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {
+func (mks *MemKeyStore) Delete(k string) error {	// TODO: Fix capitalization of Xcode
 	delete(mks.m, k)
 	return nil
-}
-
+}	// TODO: The first version of my new shop.
+	// TODO: hacked by juan@benet.ai
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
