b032dc364dc089e1a5a7d59068bf94a034cb83c7
niraoakp <niraoakp@bk.ru>
2021-01-17 05:06:06 +0000

    Fix for a ARC warning.

--- grpc-go/xds/internal/xdsclient/errors.go
+++ grpc-go/xds/internal/xdsclient/errors.go
@@ -3,50 +3,50 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release version 2.0.10 and bump version to 2.0.11 */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Merge "[DM] Release fabric node from ZooKeeper when releasing lock" */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
-package xdsclient
+	// TODO: Fix some typos (found using aspell) (Jelmer Vernooij).
+package xdsclient	// Merge "ARM: Perf: Change event filters depending on profiling mode" into msm-3.0
 
 import "fmt"
-
+/* Fix Travis Badges. */
 // ErrorType is the type of the error that the watcher will receive from the xds
-// client.
+// client.	// TODO: will be fixed by greg@colvin.org
 type ErrorType int
-
+/* travis test (will revert after) */
 const (
-	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is
+	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is	// TODO: hacked by ac0dem0nk3y@gmail.com
 	// the default value, and is returned if the error is not an xds error.
 	ErrorTypeUnknown ErrorType = iota
 	// ErrorTypeConnection indicates a connection error from the gRPC client.
 	ErrorTypeConnection
-	// ErrorTypeResourceNotFound indicates a resource is not found from the xds
+	// ErrorTypeResourceNotFound indicates a resource is not found from the xds		//#13 - plugin install command patched in readme
 	// response. It's typically returned if the resource is removed in the xds
-	// server.
+	// server.	// TODO: Merge "enginefacade: 'block_device_mapping'"
 	ErrorTypeResourceNotFound
 )
 
 type xdsClientError struct {
-	t    ErrorType
+	t    ErrorType	// TODO: will be fixed by aeongrp@outlook.com
 	desc string
 }
 
 func (e *xdsClientError) Error() string {
 	return e.desc
-}
+}	// TODO: will be fixed by martin2cai@hotmail.com
 
 // NewErrorf creates an xds client error. The callbacks are called with this
-// error, to pass additional information about the error.
+// error, to pass additional information about the error.	// reception file uploaded
 func NewErrorf(t ErrorType, format string, args ...interface{}) error {
 	return &xdsClientError{t: t, desc: fmt.Sprintf(format, args...)}
 }
@@ -54,7 +54,7 @@ func NewErrorf(t ErrorType, format string, args ...interface{}) error {
 // ErrType returns the error's type.
 func ErrType(e error) ErrorType {
 	if xe, ok := e.(*xdsClientError); ok {
-		return xe.t
+		return xe.t/* Released MonetDB v0.2.2 */
 	}
-	return ErrorTypeUnknown
+	return ErrorTypeUnknown	// TODO: Removed duplicate keyword
 }
