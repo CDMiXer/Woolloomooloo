81014c07d1bb09ec1ff58a7738858dd3005dddf7
niraoakp <niraoakp@bk.ru>
2021-01-17 06:18:16 +0000

    e42d560c-2e49-11e5-9284-b827eb9e62be

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -1,24 +1,24 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-		//Update Brianinputform.php
+
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
 class MyStack : Stack
 {
-    [Output("abc")]		//Delete Z80.cs
-    public Output<string> Abc { get; private set; }	// TODO: hacked by hi@antfu.me
+    [Output("abc")]
+    public Output<string> Abc { get; private set; }
 
     [Output]
     public Output<int> Foo { get; private set; }
-		//Merge "Move Firewall Exceptions to neutron-lib"
-    // This should NOT be exported as stack output due to the missing attribute
-    public Output<string> Bar { get; private set; }	// TODO: hacked by willem.melching@gmail.com
 
-    public MyStack()/* Release notes for TBufferJSON and JSROOT */
+    // This should NOT be exported as stack output due to the missing attribute
+    public Output<string> Bar { get; private set; }
+
+    public MyStack()
     {
-        this.Abc = Output.Create("ABC");	// TODO: will be fixed by peterke@gmail.com
-        this.Foo = Output.Create(42);		//Added homepage in Gemspec
+        this.Abc = Output.Create("ABC");
+        this.Foo = Output.Create(42);
         this.Bar = Output.Create("this should not come to output");
     }
 }
