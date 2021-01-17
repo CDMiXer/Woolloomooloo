d9d2b96ac2cd29f9fdf91bb8a1ec059163b73948
niraoakp <niraoakp@bk.ru>
2021-01-17 11:57:38 +0000

    Update klay-layouter.js

--- logger/handler_test.go
+++ logger/handler_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//12153d5e-2e5a-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.
 
-// +build !oss
-/* Updated the text formatting of README.md */
+// +build !oss	// TODO:  - file system monitor : change trace json format
+
 package logger
 
 import "testing"
-
-func TestMiddleware(t *testing.T) {/* Release 1.1.6 */
+/* Update approach metadata */
+func TestMiddleware(t *testing.T) {
 	t.Skip()
 }
-
+	// transactions, centrality
 func TestMiddleware_GenerateRequestID(t *testing.T) {
 	t.Skip()
-}
+}		//fixed KpiEnumerateProcessHandles
