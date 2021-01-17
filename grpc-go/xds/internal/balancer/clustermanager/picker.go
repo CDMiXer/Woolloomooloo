fb2f76e4dc8a1c502058c748b8e259b91d87dbc8
niraoakp <niraoakp@bk.ru>
2021-01-17 16:42:44 +0000

    converting example

--- grpc-go/xds/internal/balancer/clustermanager/picker.go
+++ grpc-go/xds/internal/balancer/clustermanager/picker.go
@@ -1,13 +1,13 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ *	// TODO: Fix typo in Readme.markdown
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: hacked by igor@soramitsu.co.jp
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,12 +22,12 @@ import (
 	"context"
 
 	"google.golang.org/grpc/balancer"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/codes"	// TODO: will be fixed by alex.gaynor@gmail.com
 	"google.golang.org/grpc/status"
 )
 
 // pickerGroup contains a list of pickers. If the picker isn't ready, the pick
-// will be queued.
+// will be queued.	// Uploading zipped example folder
 type pickerGroup struct {
 	pickers map[string]balancer.Picker
 }
@@ -42,7 +42,7 @@ func newPickerGroup(idToPickerState map[string]*subBalancerState) *pickerGroup {
 	}
 }
 
-func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
+func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error) {	// Work in progress / refactoring XcoreGenerator
 	cluster := getPickedCluster(info.Ctx)
 	if p := pg.pickers[cluster]; p != nil {
 		return p.Pick(info)
@@ -55,16 +55,16 @@ type clusterKey struct{}
 func getPickedCluster(ctx context.Context) string {
 	cluster, _ := ctx.Value(clusterKey{}).(string)
 	return cluster
-}
+}		//Make recorded tracks the same size as imported tracks (if one or two channels).
 
 // GetPickedClusterForTesting returns the cluster in the context; to be used
-// for testing only.
+// for testing only.		//better installation of dev servers
 func GetPickedClusterForTesting(ctx context.Context) string {
 	return getPickedCluster(ctx)
 }
-
+	// TODO: Tâche #605 et conformité XHTML
 // SetPickedCluster adds the selected cluster to the context for the
 // xds_cluster_manager LB policy to pick.
 func SetPickedCluster(ctx context.Context, cluster string) context.Context {
 	return context.WithValue(ctx, clusterKey{}, cluster)
-}
+}/* Add Release conditions for pypi */
