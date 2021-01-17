f8d19673829c1f4b9b27bce8c208dafc7ec5683d
niraoakp <niraoakp@bk.ru>
2021-01-17 08:59:33 +0000

    Merge "TIF: start using @Nullable annotation."

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
-/* Release of eeacms/energy-union-frontend:1.7-beta.20 */
+
 CREATE TABLE IF NOT EXISTS logs (
  log_id    SERIAL PRIMARY KEY
 ,log_data  BYTEA
-);	// TODO: Update to experimental r13479
+);		//To fix AIRAVATA-1711.
