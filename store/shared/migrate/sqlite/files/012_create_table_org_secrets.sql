a19345acf08b910bd11fcbf5c63005440c1c54a9
niraoakp <niraoakp@bk.ru>
2021-01-17 07:23:55 +0000

    added date year for filtering

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,10 +1,10 @@
 -- name: create-table-org-secrets
-/* Merge "mobicore: t-base-200 Engineering Release" */
+/* Merge "Release 4.0.10.77 QCACLD WLAN Driver" */
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_namespace         TEXT COLLATE NOCASE		//Update activity_activity_reporte.xml
+,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
-,secret_type              TEXT/* Added documentation, made PEP8 compliant */
+,secret_type              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
