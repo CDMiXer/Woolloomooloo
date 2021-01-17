49dfe6dae04e48bbecc0cba6e3ba159aa9ff5159
niraoakp <niraoakp@bk.ru>
2021-01-17 07:33:41 +0000

    [FIX] Fix 'installable' syntax in manifest file

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -4,6 +4,6 @@ func CommDR(in []byte) (out [32]byte) {
 	for i, b := range in {
 		out[i] = ^b
 	}
-	// Clarify that parse_timestamp is for machine timestamps
-	return out	// TODO: working on the date editor
-}	// TODO: Updated the dcap feedstock.
+
+	return out
+}
