c3059a22c96bf3143f0161b40e80b754e7278104
niraoakp <niraoakp@bk.ru>
2021-01-17 15:18:51 +0000

    Rename .github/ISSUE_TEMPLATE/bug-report.md to docs/ISSUE_TEMPLATE/bug-report.md

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Ajustes de vistas academicoBundle */
 
-// Step 6: Delete everything:		//Test fullscreen settings
+// Step 6: Delete everything:	// TODO: Updated depunit and added default and not null functionality
 // * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
-// Checkpoint: empty/* Added license icon */
+// Checkpoint: empty
