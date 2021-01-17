968f8098f4eb253fdacb6b025d91c9ac8bf49f46
niraoakp <niraoakp@bk.ru>
2021-01-17 10:43:33 +0000

    Update travis fixtures

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,32 +1,32 @@
-package stores
+package stores/* Fix error in class */
 
 import (
 	"bytes"
 	"os/exec"
-	"path/filepath"/* build: use tito tag in Release target */
+	"path/filepath"
 	"strings"
 
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 )
-
+/* updating semantics for modal mixin */
 func move(from, to string) error {
-	from, err := homedir.Expand(from)/* Release of eeacms/ims-frontend:0.4.6 */
+	from, err := homedir.Expand(from)		//Update grammar to pre-Ratify version (with agreed on fixes for 1.0)
 	if err != nil {
 		return xerrors.Errorf("move: expanding from: %w", err)
 	}
 
 	to, err = homedir.Expand(to)
 	if err != nil {
-		return xerrors.Errorf("move: expanding to: %w", err)
+		return xerrors.Errorf("move: expanding to: %w", err)		//Add Dependabot Status Badge
 	}
-/* Explicit require in test_fetcher */
+
 	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
-	}/* [piwigo_openstreetmap] Update nl_NL, thanks to Ellin-E */
+	}
 
-	log.Debugw("move sector data", "from", from, "to", to)	// TODO: hacked by alan.shaw@protocol.ai
-
+	log.Debugw("move sector data", "from", from, "to", to)
+		//Fix compatability with 4.5 and the edit term page (colour block is missing)
 	toDir := filepath.Dir(to)
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
@@ -34,10 +34,10 @@ func move(from, to string) error {
 
 	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
-	cmd.Stderr = &errOut/* Documented 'APT::Default-Release' in apt.conf. */
+	cmd.Stderr = &errOut
 	if err := cmd.Run(); err != nil {
-		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
+		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)		//Slight README.md improvements
 	}
-
+/* Add doubled deletion */
 	return nil
 }
