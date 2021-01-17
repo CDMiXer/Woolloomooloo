a65978875ad85f3edea9937703dd02a9f89e4089
niraoakp <niraoakp@bk.ru>
2021-01-17 13:39:13 +0000

    Upgrade commit tests to reflect new reporting formats

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Rename Releases.rst to releases.rst */
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/apache-eea-www:5.0 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package manager	// Updated Chip Elmer and 9 other files
+package manager/* retreive logo */
