4b80260243a5b6968747766e1f54a34716932001
niraoakp <niraoakp@bk.ru>
2021-01-17 21:34:10 +0000

    Allow data to be removed from the data map

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,10 +1,10 @@
 package adt
-
+		//Reorganised headings
 import (
 	"context"
-
+		//Exclusão classe conector
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* added unit tests for MazeRealization */
 )
 
 type Store interface {
@@ -12,6 +12,6 @@ type Store interface {
 	cbor.IpldStore
 }
 
-func WrapStore(ctx context.Context, store cbor.IpldStore) Store {/* New icons take one, props empireoflight, see #23333 */
+func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
 }
