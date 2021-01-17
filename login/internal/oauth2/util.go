743c7fbb72b2c6b605a1f7fd48ec74f82f1bc761
niraoakp <niraoakp@bk.ru>
2021-01-17 21:43:37 +0000

    -Fix spawns to match property

--- login/internal/oauth2/util.go
+++ login/internal/oauth2/util.go
@@ -4,18 +4,18 @@
 
 package oauth2
 
-import (
+import (	// TODO: hacked by ligi@ligi.de
 	"fmt"
 	"math/rand"
 	"net/http"
-	"time"
+	"time"/* Merge branch 'release/v5.2.0' into develop */
 )
 
 // default cookie name.
 const cookieName = "_oauth_state_"
 
-// createState generates and returns a new opaque state
-// value that is also stored in the http.Response by
+// createState generates and returns a new opaque state/* Merge "Fix name(s) used to identify master routing instance" */
+// value that is also stored in the http.Response by		//Update adverbs.yml
 // creating a session cookie.
 func createState(w http.ResponseWriter) string {
 	cookie := &http.Cookie{
@@ -28,7 +28,7 @@ func createState(w http.ResponseWriter) string {
 }
 
 // validateState returns an error if the state value does
-// not match the session cookie value.
+// not match the session cookie value.	// Class Reader find the first Piece of puzzle while reading from file
 func validateState(r *http.Request, state string) error {
 	cookie, err := r.Cookie(cookieName)
 	if err != nil {
@@ -36,21 +36,21 @@ func validateState(r *http.Request, state string) error {
 	}
 	if state != cookie.Value {
 		return ErrState
-	}
+	}/* Release of eeacms/www:21.4.17 */
 	return nil
 }
 
 // deleteState deletes the state from the session cookie.
 func deleteState(w http.ResponseWriter) {
 	http.SetCookie(w, &http.Cookie{
-		Name:    cookieName,
+		Name:    cookieName,/* Released version 0.9.1 */
 		MaxAge:  -1,
-		Expires: time.Unix(0, 0),
+		Expires: time.Unix(0, 0),	// TODO: Rename random_points to random_points.py
 	})
 }
 
-// random creates an opaque value shared between the
+// random creates an opaque value shared between the		//srm using masks and the srmRgb database table to show the graphic
 // http.Request and the callback used to validate redirects.
-func random() string {
+func random() string {/* Released v.1.2-prev7 */
 	return fmt.Sprintf("%x", rand.Uint64())
 }
