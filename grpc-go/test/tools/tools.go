64dfbeb096d527082575805b56d354f32d87a51e
niraoakp <niraoakp@bk.ru>
2021-01-17 10:28:13 +0000

    Fix hyperlink to `find_library` documentation

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -1,21 +1,21 @@
 // +build tools
-
+	// TODO: will be fixed by nick@perfectabstractions.com
 /*
  *
- * Copyright 2018 gRPC authors.
- *
+ * Copyright 2018 gRPC authors.	// TODO: widget usage download image sizes smaller popover refs #17535
+ *	// TODO: Fixed insertion of valid dates into concept_stage
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License./* Update Release_Notes.txt */
+ * You may obtain a copy of the License at/* Release 1.0.0: Initial release documentation. */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Release: Making ready to release 5.4.0 */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// fixed unittest for 5.3
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* adding mission control */
  */
 
 // This file is not intended to be compiled.  Because some of these imports are
@@ -23,7 +23,7 @@
 // prevent tools from inspecting the imports.
 
 package tools
-
+	// TODO: hacked by nick@perfectabstractions.com
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golang/protobuf/protoc-gen-go"
