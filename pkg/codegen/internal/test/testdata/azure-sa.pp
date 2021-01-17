4ebbd7a7de79f5dbc3a9ec8095db81b08f69319e
niraoakp <niraoakp@bk.ru>
2021-01-17 15:47:40 +0000

    update fcitx-3.4 scim-bridge-0.4.5

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -1,34 +1,34 @@
-config storageAccountNameParam string {
+config storageAccountNameParam string {	// TODO: correct path for classes
 }
-/* server.start() validation */
-config resourceGroupNameParam string {/* Blacklist xscreensaver-autostart from autostarting */
+
+config resourceGroupNameParam string {
 }
 
 resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {
-	name = resourceGroupNameParam
+	name = resourceGroupNameParam/* Preview Release (Version 0.5 / VersionCode 5) */
 })
 
 config locationParam string {
-	default = resourceGroupVar.location		//#6 Reduced Property Views
-}	// TODO: hacked by sebastian.tharakan97@gmail.com
-	// TODO: implement save method for borrower view
+	default = resourceGroupVar.location
+}
+
 config storageAccountTierParam string {
     default = "Standard"
 }
 
 config storageAccountTypeReplicationParam string {
-    default = "LRS"	// TODO: improve formatting BOB4 README
-}/* First Release. */
+    default = "LRS"
+}
 
 resource storageAccountResource "azure:storage/account:Account" {
 	name = storageAccountNameParam
 	accountKind = "StorageV2"
-	location = locationParam
+	location = locationParam/* Release candidate 2.3 */
 	resourceGroupName = resourceGroupNameParam
 	accountTier = storageAccountTierParam
-	accountReplicationType = storageAccountTypeReplicationParam
-}
+	accountReplicationType = storageAccountTypeReplicationParam/* update to latest space:event-sourcing version */
+}/* @Release [io7m-jcanephora-0.34.5] */
 
-output storageAccountNameOut {/* add --no-timing option to BATCH */
+output storageAccountNameOut {
 	value = storageAccountResource.name
 }
