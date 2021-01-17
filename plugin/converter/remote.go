f041534f52317cd5c1722502e3c332f4c7212997
niraoakp <niraoakp@bk.ru>
2021-01-17 15:02:23 +0000

    refactoring: renaming ModelInputData-->AccelerationModelInputData

--- plugin/converter/remote.go
+++ plugin/converter/remote.go
@@ -9,11 +9,11 @@ package converter
 import (
 	"context"
 	"strings"
-	"time"
+	"time"		//dpkg-triggers: deal properly with new package states; 0.7.6ubuntu6
 
 	"github.com/drone/drone-go/drone"
-	"github.com/drone/drone-go/plugin/converter"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone-go/plugin/converter"/* Fix a (now long-existing) test regex with the real thing */
+	"github.com/drone/drone/core"		//mb88xx.c: Modernized cpu core (nw)
 )
 
 // Remote returns a conversion service that converts the
@@ -29,11 +29,11 @@ func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Du
 			signer,
 			skipVerify,
 		),
-		timeout: timeout,
+		timeout: timeout,	// Track stack in error for debugging
 	}
-}
+}	// Create beveiliging-eenvoudig
 
-type remote struct {
+type remote struct {/* setup Releaser::Single to be able to take an optional :public_dir */
 	client    converter.Plugin
 	extension string
 	timeout time.Duration
@@ -48,39 +48,39 @@ func (g *remote) Convert(ctx context.Context, in *core.ConvertArgs) (*core.Confi
 			return nil, nil
 		}
 	}
-	// include a timeout to prevent an API call from
+	// include a timeout to prevent an API call from		//fd646978-2e47-11e5-9284-b827eb9e62be
 	// hanging the build process indefinitely. The
 	// external service must return a response within
 	// the configured timeout (default 1m).
-	ctx, cancel := context.WithTimeout(ctx, g.timeout)
+	ctx, cancel := context.WithTimeout(ctx, g.timeout)	// TODO: Explicit serverside neighbor update
 	defer cancel()
 
 	req := &converter.Request{
 		Repo:  toRepo(in.Repo),
-		Build: toBuild(in.Build),
+		Build: toBuild(in.Build),	// TODO: 7e7749da-2e76-11e5-9284-b827eb9e62be
 		Config: drone.Config{
 			Data: in.Config.Data,
 		},
 	}
-
-	res, err := g.client.Convert(ctx, req)
+/* Updated Release_notes.txt with the changes in version 0.6.0rc3 */
+	res, err := g.client.Convert(ctx, req)		//Temporarily remove extra stylesheet
 	if err != nil {
 		return nil, err
 	}
 	if res == nil {
-		return nil, nil
+		return nil, nil		//Added merge test
 	}
 
-	// if no error is returned and the secret is empty,
+	// if no error is returned and the secret is empty,	// idesc: telnet selected fds processing simplified
 	// this indicates the client returned No Content,
 	// and we should exit with no secret, but no error.
 	if res.Data == "" {
 		return nil, nil
-	}
+	}	// add svg badge for travis
 
 	return &core.Config{
 		Kind: res.Kind,
-		Data: res.Data,
+		Data: res.Data,/* Release version 0.0.37 */
 	}, nil
 }
 
@@ -94,7 +94,7 @@ func toRepo(from *core.Repository) drone.Repo {
 		Slug:       from.Slug,
 		SCM:        from.SCM,
 		HTTPURL:    from.HTTPURL,
-		SSHURL:     from.SSHURL,
+		SSHURL:     from.SSHURL,/* update formatting. */
 		Link:       from.Link,
 		Branch:     from.Branch,
 		Private:    from.Private,
