b906a98c598987f3a2af45904109453a6de42dd3
niraoakp <niraoakp@bk.ru>
2021-01-17 20:41:36 +0000

    Moved map files to common game directory

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
-///* this is the university control project */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//fixes #295: crash using traits with PHP 5.4.8+
+// Copyright 2019 Drone IO, Inc./* Release of eeacms/clms-frontend:1.0.4 */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Merge pull request #2 from BennyHallett/commands */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Merge "msm: iommu: Use iommu_map_range for 4K mappings" into ics_strawberry
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//  added: value-mapping for request
+// See the License for the specific language governing permissions and	// Create Services.md
 // limitations under the License.
 
 // +build oss
 
 package registry
-	// Delete GetProgress_LameDec.progress
+
 import "github.com/drone/drone/core"
 
 // External returns a no-op registry credential provider.
-func External(string, string, bool) core.RegistryService {/* Add a missing id to the object */
+func External(string, string, bool) core.RegistryService {
 	return new(noop)
 }
