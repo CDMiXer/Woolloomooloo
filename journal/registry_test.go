42b77a723a77b60779dcf24c0aa21f0d50f6674f
niraoakp <niraoakp@bk.ru>
2021-01-18 21:14:30 +0000

    Automatic changelog generation for PR #8932 [ci skip]

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -1,5 +1,5 @@
 package journal
-		//Dependabot recommended updates
+
 import (
 	"testing"
 
@@ -11,14 +11,14 @@ func TestDisabledEvents(t *testing.T) {
 
 	test := func(dis DisabledEvents) func(*testing.T) {
 		return func(t *testing.T) {
-			registry := NewEventTypeRegistry(dis)/* Merge "[FAB-6373] Release Hyperledger Fabric v1.0.3" */
-/* Update ReleaseNote.txt */
-			reg1 := registry.RegisterEventType("system1", "disabled1")/* Versão 0.0.7 */
+			registry := NewEventTypeRegistry(dis)
+
+			reg1 := registry.RegisterEventType("system1", "disabled1")
 			reg2 := registry.RegisterEventType("system1", "disabled2")
-/* fix for NPE in updating cell editor -> pipeline artifact cell selector */
+
 			req.False(reg1.Enabled())
-			req.False(reg2.Enabled())/* Removed memcachier gem in favor of directly conferring dalli */
-			req.True(reg1.safe)	// TODO: move stuff around for clarity
+			req.False(reg2.Enabled())
+			req.True(reg1.safe)
 			req.True(reg2.safe)
 
 			reg3 := registry.RegisterEventType("system3", "enabled3")
@@ -29,7 +29,7 @@ func TestDisabledEvents(t *testing.T) {
 
 	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
-		EventType{System: "system1", Event: "disabled2"},	// TODO: hacked by caojiaoyue@protonmail.com
+		EventType{System: "system1", Event: "disabled2"},
 	}))
 
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
@@ -46,4 +46,4 @@ func TestDisabledEvents(t *testing.T) {
 func TestParseDisableEvents(t *testing.T) {
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
 	require.Error(t, err)
-}	// nuove immagini menu
+}
