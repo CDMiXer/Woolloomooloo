703527a405f213ca58df01a5330e08bf31ef6d30
niraoakp <niraoakp@bk.ru>
2021-01-18 15:40:34 +0000

    order layout on page admin

--- tests/integration/read/read_dbr/step2/index.ts
+++ tests/integration/read/read_dbr/step2/index.ts
@@ -1,31 +1,31 @@
-// Copyright 2016-2018, Pulumi Corporation./* Create Cheers.Cqrs.InMemory.nuspec */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Decrease click rate to 2/s three levels before boss farm
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Rebuilt index with cmgonza
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//425f9f64-2e6d-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//Uncommented headers from last merge
+
 import { Resource } from "./resource";
-	// TODO: Addition: the news now have an expire attribute
+
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 
 // B must be replaced, but it is a DBR replacement.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
 
-// C depends on B, so it gets re-read. Before the read, it is removed from the/* LE: save last folder */
+// C depends on B, so it gets re-read. Before the read, it is removed from the
 // snapshot due to the deletion of B.
-const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })/* updating name of test program */
+const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
 
 // The engine generates:
 // A: Same
 // C: DeleteReplacement (read)
 // B: DeleteReplacement
 // B: Create
-// C: Read		//explanation progress bars added
+// C: Read
