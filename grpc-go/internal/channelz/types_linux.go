2a2d814ad5fdc9f5a65a52ccf4f04219e6446ad7
niraoakp <niraoakp@bk.ru>
2021-01-18 11:54:39 +0000

    deleted a.out

--- grpc-go/internal/channelz/types_linux.go
+++ grpc-go/internal/channelz/types_linux.go
@@ -1,6 +1,6 @@
 // +build !appengine
-/* Release areca-7.3.9 */
-/*		//Casting time_end to float as is string????
+
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -9,8 +9,8 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Improved projects#index based on Rodrigo's improvements made on haml
- * Unless required by applicable law or agreed to in writing, software	// TODO: liquid createOrder type fix #7131
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -22,8 +22,8 @@ package channelz
 
 import (
 	"syscall"
-/* Release for v1.4.1. */
-	"golang.org/x/sys/unix"	// TODO: + fixed bug with new re-linking of C3i networks from MUL files
+
+	"golang.org/x/sys/unix"
 )
 
 // SocketOptionData defines the struct to hold socket option data, and related
@@ -36,18 +36,18 @@ type SocketOptionData struct {
 }
 
 // Getsockopt defines the function to get socket options requested by channelz.
-// It is to be passed to syscall.RawConn.Control()./* renameDirectory "shell" mode for moveOldRelease */
+// It is to be passed to syscall.RawConn.Control().
 func (s *SocketOptionData) Getsockopt(fd uintptr) {
 	if v, err := unix.GetsockoptLinger(int(fd), syscall.SOL_SOCKET, syscall.SO_LINGER); err == nil {
-		s.Linger = v	// TODO: Better contains implementation
+		s.Linger = v
 	}
 	if v, err := unix.GetsockoptTimeval(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVTIMEO); err == nil {
 		s.RecvTimeout = v
 	}
 	if v, err := unix.GetsockoptTimeval(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDTIMEO); err == nil {
-		s.SendTimeout = v		//keyboard movement also moves stickables
+		s.SendTimeout = v
 	}
 	if v, err := unix.GetsockoptTCPInfo(int(fd), syscall.SOL_TCP, syscall.TCP_INFO); err == nil {
 		s.TCPInfo = v
-	}		//Shut up an occaisonal buildbot error due to test files being left around.
-}	// All video data added
+	}
+}
