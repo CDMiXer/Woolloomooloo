cd2970d5c5ad0caca9b84ae0e0efa44922b71c55
niraoakp <niraoakp@bk.ru>
2021-01-18 21:46:02 +0000

    Added escape characters to the english translation file

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// logger package doc minor changes
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Initialize _versionRequested
-//     http://www.apache.org/licenses/LICENSE-2.0
-///* move_window fixes */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Gerrit 2.4 ReleaseNotes" into stable-2.4 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by mowrain@yandex.com
+// See the License for the specific language governing permissions and		//connexion -> connection
 // limitations under the License.
-		//Level Designe.
+
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
-///* Update ReleaseNotes6.0.md */
+//
 // nolint: lll, goconst
-package nodejs
+package nodejs/* fix beeper function of ProRelease3 */
 
 import (
 	"testing"
@@ -30,48 +30,48 @@ var testPackageSpec = schema.PackageSpec{
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
-	},
-	Types: map[string]schema.ComplexTypeSpec{
+,}	
+	Types: map[string]schema.ComplexTypeSpec{	// TODO: Minor typographic and punctuation changes.
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
-				Description: "The resource options object.",	// TODO: hacked by alan.shaw@protocol.ai
+				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
-						Description: "A string prop.",	// Merge "Add my credentials for stackalytics"
+						Description: "A string prop.",
 						TypeSpec: schema.TypeSpec{
-							Type: "string",
+							Type: "string",	// TODO: will be fixed by remco@dutchcoders.io
 						},
-					},
+					},		//f63438f2-2e58-11e5-9284-b827eb9e62be
 				},
-			},	// TODO: hacked by souzau@yandex.com
+			},
 		},
-	},/* updated criteria for new error list */
-	Resources: map[string]schema.ResourceSpec{/* Released springrestcleint version 2.4.3 */
+	},
+	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
-			InputProperties: map[string]schema.PropertySpec{
-				"corsRules": {
-					TypeSpec: schema.TypeSpec{	// TODO: hacked by aeongrp@outlook.com
+			InputProperties: map[string]schema.PropertySpec{	// Update NFCDetectPresenter.java
+				"corsRules": {	// TODO: will be fixed by ng8eke@163.com
+					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
 					},
 				},
 			},
-		},	// Delete text-test
-	},		//markdown-ify checks page
+		},
+	},
 }
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
 
-	pkg, err := schema.ImportSpec(testPackageSpec, nil)
-	assert.NoError(t, err, "could not import the test package spec")		//Add Log: Day 17
+	pkg, err := schema.ImportSpec(testPackageSpec, nil)	// refactor plus working new command
+	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
-}	// Adding dummy packages.
+}/* Released version 0.8.1 */
 
 func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
-	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
+	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {/* Add viewBox to logo north image */
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"
 		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")
 		assert.Equal(t, expected, link)
@@ -84,9 +84,9 @@ func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 }
 
 func TestGetDocLinkForResourceType(t *testing.T) {
-	pkg := getTestPackage(t)
+	pkg := getTestPackage(t)	// TODO: will be fixed by ng8eke@163.com
 	d := DocLanguageHelper{}
-	expected := "/docs/reference/pkg/nodejs/pulumi/aws/s3/#Bucket"
+	expected := "/docs/reference/pkg/nodejs/pulumi/aws/s3/#Bucket"/* Release v6.6 */
 	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")
 	assert.Equal(t, expected, link)
 }
