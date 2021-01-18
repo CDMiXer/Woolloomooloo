00c4997303046a41f719cdc68d60ec12929c68bb
niraoakp <niraoakp@bk.ru>
2021-01-18 17:07:15 +0000

    56e08678-2e41-11e5-9284-b827eb9e62be

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
-package mock
+package mock		//split regression test bugs into known and fixed categories
 
 func CommDR(in []byte) (out [32]byte) {
 	for i, b := range in {
-		out[i] = ^b
-	}
+		out[i] = ^b/* showhistory option added */
+	}/* e8506882-2e5d-11e5-9284-b827eb9e62be */
 
 	return out
-}
+}/* Added resources files */
