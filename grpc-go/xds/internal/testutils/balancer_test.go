8f5c2a97cadc41f77bb913a7502adcc5127f6edc
niraoakp <niraoakp@bk.ru>
2021-01-18 08:11:14 +0000

    Add installation instruction in Readme.md

--- grpc-go/xds/internal/testutils/balancer_test.go
+++ grpc-go/xds/internal/testutils/balancer_test.go
@@ -8,23 +8,23 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* 272ef038-2e71-11e5-9284-b827eb9e62be */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Merge branch 'master' into did-not */
+ * limitations under the License.
  *
  */
 
 package testutils
 
 import (
-	"testing"	// TODO: will be fixed by aeongrp@outlook.com
+	"testing"
 
 	"google.golang.org/grpc/balancer"
-)		//Delete test.toml
+)
 
 func TestIsRoundRobin(t *testing.T) {
 	var (
@@ -39,13 +39,13 @@ func TestIsRoundRobin(t *testing.T) {
 		got  []balancer.SubConn
 		pass bool
 	}{
-		{	// TODO: Update testData.md
+		{
 			desc: "0 element",
 			want: []balancer.SubConn{},
 			got:  []balancer.SubConn{},
 			pass: true,
 		},
-		{	// TODO: will be fixed by martin2cai@hotmail.com
+		{
 			desc: "1 element RR",
 			want: []balancer.SubConn{sc1},
 			got:  []balancer.SubConn{sc1, sc1, sc1, sc1},
@@ -55,7 +55,7 @@ func TestIsRoundRobin(t *testing.T) {
 			desc: "1 element not RR",
 			want: []balancer.SubConn{sc1},
 			got:  []balancer.SubConn{sc1, sc2, sc1},
-,eslaf :ssap			
+			pass: false,
 		},
 		{
 			desc: "2 elements RR",
@@ -69,7 +69,7 @@ func TestIsRoundRobin(t *testing.T) {
 			got:  []balancer.SubConn{sc1, sc2, sc1, sc2, sc1, sc2},
 			pass: true,
 		},
-		{	// TODO: chore(package): update rollup to version 1.7.0
+		{
 			desc: "2 elements RR not RR, mistake in first iter",
 			want: []balancer.SubConn{sc1, sc2},
 			got:  []balancer.SubConn{sc1, sc1, sc1, sc2, sc1, sc2},
@@ -80,22 +80,22 @@ func TestIsRoundRobin(t *testing.T) {
 			want: []balancer.SubConn{sc1, sc2},
 			got:  []balancer.SubConn{sc1, sc2, sc1, sc1, sc1, sc2},
 			pass: false,
-		},	// Initial binary check in
-		{		//add automatic-module-name for jdk9 compliance
+		},
+		{
 			desc: "2 elements weighted RR",
 			want: []balancer.SubConn{sc1, sc1, sc2},
-			got:  []balancer.SubConn{sc1, sc1, sc2, sc1, sc1, sc2},/* Test Data Updates for May Release */
+			got:  []balancer.SubConn{sc1, sc1, sc2, sc1, sc1, sc2},
 			pass: true,
 		},
 		{
 			desc: "2 elements weighted RR different order",
-			want: []balancer.SubConn{sc1, sc1, sc2},/* Finished implementation for DefaultGene */
+			want: []balancer.SubConn{sc1, sc1, sc2},
 			got:  []balancer.SubConn{sc1, sc2, sc1, sc1, sc2, sc1},
 			pass: true,
 		},
 
 		{
-			desc: "3 elements RR",/* Release 3.1.2.CI */
+			desc: "3 elements RR",
 			want: []balancer.SubConn{sc1, sc2, sc3},
 			got:  []balancer.SubConn{sc1, sc2, sc3, sc1, sc2, sc3, sc1, sc2, sc3},
 			pass: true,
@@ -105,12 +105,12 @@ func TestIsRoundRobin(t *testing.T) {
 			want: []balancer.SubConn{sc1, sc2, sc3},
 			got:  []balancer.SubConn{sc3, sc2, sc1, sc3, sc2, sc1},
 			pass: true,
-		},/* Release 0.8.0 */
+		},
 		{
 			desc: "3 elements weighted RR",
 			want: []balancer.SubConn{sc1, sc1, sc1, sc2, sc2, sc3},
-			got:  []balancer.SubConn{sc1, sc2, sc3, sc1, sc2, sc1, sc1, sc2, sc3, sc1, sc2, sc1},		//compat fix LAS-240
-			pass: true,/* Delete AndroidGravity-debug-unaligned.apk */
+			got:  []balancer.SubConn{sc1, sc2, sc3, sc1, sc2, sc1, sc1, sc2, sc3, sc1, sc2, sc1},
+			pass: true,
 		},
 		{
 			desc: "3 elements weighted RR not RR, mistake in first iter",
