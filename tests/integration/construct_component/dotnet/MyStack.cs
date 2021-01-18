9edd6b79226b5d069b062f2a6b38051bb3cbac1b
niraoakp <niraoakp@bk.ru>
2021-01-18 12:13:35 +0000

    fixing the project file

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;
+using Pulumi;/* Added RestingControl (thank you Teemu Salminen) */
 
 class MyStack : Stack
 {
-    public MyStack()
+    public MyStack()	// Break pane API into sections
     {
-        var componentA = new Component("a", new ComponentArgs { Echo = 42 });
-        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });	// TODO: will be fixed by nagydani@epointsystem.org
+        var componentA = new Component("a", new ComponentArgs { Echo = 42 });/* -Corrección de modelos */
+        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });		//Fix bug [ 1884368 ] festatus command doesn't work on DVB-S (patch supplied)
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
     }
 }
