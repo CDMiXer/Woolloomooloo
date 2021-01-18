e09e666d43f38124d631cac3e20a5bd9a5f857d7
niraoakp <niraoakp@bk.ru>
2021-01-18 04:32:58 +0000

    add fastutil classes to release jar

--- tests/integration/partial_state/partial_state_test.go
+++ tests/integration/partial_state/partial_state_test.go
@@ -1,64 +1,64 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 3.2 070.01. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
-package ints/* Create graphics/image processing section, add libs */
+		//Fix DomainMessage metadata bottleneck
+package ints
 
 import (
-	"testing"/* Release version: 1.0.12 */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Release 3.4.0. */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
-)/* Added full reference to THINCARB paper and added Release Notes */
-/* Added some rudimentary slope stability and trig functions to LSDRaster */
+)		//Updated version to 1.4.0
+
 // TestPartialState tests that the engine persists partial state of a resource if a provider
 // provides partial state alongside a resource creation or update error.
 //
-// The setup of this test uses a dynamic provider that will partially fail if a resource's state
+// The setup of this test uses a dynamic provider that will partially fail if a resource's state		//Fixed init variables
 // value is the number 4.
-func TestPartialState(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: rev 873734
-		Dir:           "step1",
+func TestPartialState(t *testing.T) {/* raising oracle java version up to 8 as this is the supported version now */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Make diff() ref checks support hashes */
+		Dir:           "step1",/* Update omni_socket.c */
 		Dependencies:  []string{"@pulumi/pulumi"},
 		Quick:         true,
 		ExpectFailure: true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// The first update tries to create a resource with state 4. This fails partially.
 			assert.NotNil(t, stackInfo.Deployment)
-			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))		//Updated the pysmbclient feedstock.
-			stackRes := stackInfo.Deployment.Resources[0]		//élimine les doublons sur les forums
-			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))	// TODO: will be fixed by josharian@gmail.com
+			stackRes := stackInfo.Deployment.Resources[0]
+			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* Merge "Release 3.2.3.398 Prima WLAN Driver" */
 			providerRes := stackInfo.Deployment.Resources[1]
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-
-			a := stackInfo.Deployment.Resources[2]
-
+	// 6532ccf8-2e6f-11e5-9284-b827eb9e62be
+			a := stackInfo.Deployment.Resources[2]/* newInstance() not needed to load JDBC drivers. */
+/* Release-Version inkl. Tests und Testüberdeckungsprotokoll */
 			// We should still have persisted the resource and its outputs to the snapshot
-			assert.Equal(t, "doomed", string(a.URN.Name()))		//Fix #922969 (Add another Pont-of-View PlayTab Pro driver)
-			assert.Equal(t, 4.0, a.Outputs["state"].(float64))
-			assert.Equal(t, []string{"state can't be 4"}, a.InitErrors)/* Release areca-5.5.6 */
+			assert.Equal(t, "doomed", string(a.URN.Name()))
+			assert.Equal(t, 4.0, a.Outputs["state"].(float64))/* .war and .jar ignored */
+			assert.Equal(t, []string{"state can't be 4"}, a.InitErrors)
 		},
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,		//Create D. Fractiles Large.cpp
+				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The next update deletes the resource. We should successfully delete it.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]	// TODO: Feedback with à mail Resource in Tomee.xml, modify feedback button css.
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 				},
 			},
 			{
 				Dir:      "step3",
-				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// Merge branch 'staging' into borked_ci
-					// Step 3 creates a resource with state 5, which succeeds.	// TODO: will be fixed by lexy8russo@outlook.com
+				Additive: true,/* Release 4.2.0-SNAPSHOT */
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+					// Step 3 creates a resource with state 5, which succeeds.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]/* running in stageblock (WIP) */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
@@ -76,14 +76,14 @@ func TestPartialState(t *testing.T) {
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// Step 4 updates the resource to have state 4, which fails partially.
 					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))		//[ID] Items updated
+					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
-		//Update GiftedListView.js
+
 					// We should have persisted the updated resource's new outputs
 					// to the snapshot.
 					assert.Equal(t, "not-doomed", string(a.URN.Name()))
