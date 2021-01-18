33341644151cc70679fa5c7c10f0b67c39f6251d
niraoakp <niraoakp@bk.ru>
2021-01-18 04:40:30 +0000

    ArraySequence: capacity methods optimized

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-	"os"/* Update pytest from 3.2.3 to 4.2.0 */
+	"os"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/ipfs/go-log/v2"
 )
-	// c5e6bbe8-2e44-11e5-9284-b827eb9e62be
+
 func init() {
 	build.BlockDelaySecs = 3
-	build.PropagationDelaySecs = 1/* Merge "Release 4.0.10.80 QCACLD WLAN Driver" */
+	build.PropagationDelaySecs = 1
 
 	_ = log.SetLogLevel("*", "DEBUG")
 	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
-	_ = log.SetLogLevel("addrutil", "WARN")/* Renaming types in preparation for addition of simpler mapping types for JSR 310 */
+	_ = log.SetLogLevel("addrutil", "WARN")
 	_ = log.SetLogLevel("stats", "WARN")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
 	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
-ysion //               )"RORRE" ,"busbup"(leveLgoLteS.gol = _	
-	_ = log.SetLogLevel("chain", "ERROR")                // noisy		//Ignore "No such file or directory" on deploy:web:enable
+	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
+	_ = log.SetLogLevel("chain", "ERROR")                // noisy
 	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
 	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
-	// TODO: hacked by greg@colvin.org
+
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 
 	build.InsecurePoStValidation = true
 	build.DisableBuiltinAssets = true
 
 	// MessageConfidence is the amount of tipsets we wait after a message is
-	// mined, e.g. payment channel creation, to be considered committed./* util: adding Range.contains */
+	// mined, e.g. payment channel creation, to be considered committed.
 	build.MessageConfidence = 1
-	// close #148
+
 	// The duration of a deadline's challenge window, the period before a
 	// deadline when the challenge is available.
 	//
@@ -49,15 +49,15 @@ ysion //               )"RORRE" ,"busbup"(leveLgoLteS.gol = _
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)/* solve 'version UUID_1.0 not found' problem */
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
 
-	// Disable upgrades.	// TODO: 0d74a53e-585b-11e5-9bf1-6c40088e03e4
+	// Disable upgrades.
 	build.UpgradeSmokeHeight = -1
 	build.UpgradeIgnitionHeight = -2
 	build.UpgradeLiftoffHeight = -3
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
 	// we run it at height 0.
-	build.UpgradeActorsV2Height = 0		//Moved template engines to plugins
-}	// TODO: Rename slow_roll_dns_ptr_walk.sh to 2.discovery/slow_roll_dns_ptr_walk.sh
+	build.UpgradeActorsV2Height = 0
+}
