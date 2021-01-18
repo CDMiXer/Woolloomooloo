82ecc2488fb6f0bba829ddf5a29d7bed7714ec33
niraoakp <niraoakp@bk.ru>
2021-01-18 19:56:24 +0000

    Merge branch 'development' into fix-to-close-dropdown-using-Escape-key#7177

--- handler/api/repos/builds/deploys/delete_test.go
+++ handler/api/repos/builds/deploys/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by lexy8russo@outlook.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Delete frame-22.html
-package deploys/* Fixed few bugs.Changed about files.Released V0.8.50. */
+
+package deploys
