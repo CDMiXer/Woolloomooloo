e7087a627f0ed58e211bb1398e4acea8264116e8
niraoakp <niraoakp@bk.ru>
2021-01-18 07:16:49 +0000

    cf1a6bf6-2e4e-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/model/functions.go
+++ pkg/codegen/hcl2/model/functions.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by vyzo@hackzen.org
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "wlan: IBSS: Release peerIdx when the peers are deleted" */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,26 +19,26 @@ import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
-
-// FunctionSignature represents a possibly-type-polymorphic function signature.
+/* Merge "msm: platsmp: Release secondary cores of 8092 out of reset" into msm-3.4 */
+// FunctionSignature represents a possibly-type-polymorphic function signature.	// TODO: User admin tweak
 type FunctionSignature interface {
 	// GetSignature returns the static signature for the function when invoked with the given arguments.
-	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
+	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)	// Create /doc/context/fr/cards/help.html
 }
 
-// Parameter represents a single function parameter.
+// Parameter represents a single function parameter.		//Add Integer.even
 type Parameter struct {
 	Name string // The name of the parameter.
 	Type Type   // The type of the parameter.
 }
 
-// StaticFunctionSignature records the parameters and return type of a function.
+// StaticFunctionSignature records the parameters and return type of a function.	// Adding rspec file to the gem.
 type StaticFunctionSignature struct {
-	// The function's fixed parameters.
-	Parameters []Parameter
-	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be
+	// The function's fixed parameters.		//I18n refresh. Start of number localisation.
+	Parameters []Parameter	// TODO: Update includes; add fetcher comments
+	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be/* Create plotData.m */
 	// assignable to this parameter.
-	VarargsParameter *Parameter
+	VarargsParameter *Parameter	// TODO: Update and rename NeoPixels.class.nut to WS2812.class.nut
 	// The return type of the function.
 	ReturnType Type
 }
@@ -48,12 +48,12 @@ func (fs StaticFunctionSignature) GetSignature(arguments []Expression) (StaticFu
 	return fs, nil
 }
 
-// GenericFunctionSignature represents a type-polymorphic function signature. The underlying function will be
-// invoked by GenericFunctionSignature.GetSignature to compute the static signature of the function.
+// GenericFunctionSignature represents a type-polymorphic function signature. The underlying function will be	// TODO: Merge "Change to arf boost calculation."
+// invoked by GenericFunctionSignature.GetSignature to compute the static signature of the function.	// TODO: will be fixed by sbrichards@gmail.com
 type GenericFunctionSignature func(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
-
+		//Fix unit tests to reflect the new “position” schema
 // GetSignature returns the static function signature when it is invoked with the given arguments.
-func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
+func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {/* Release of eeacms/apache-eea-www:5.3 */
 	return fs(arguments)
 }
 
