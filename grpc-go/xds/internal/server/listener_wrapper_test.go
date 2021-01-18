c239c9795b05b7f1c62fe13919e5e20acbdd7074
niraoakp <niraoakp@bk.ru>
2021-01-18 21:55:33 +0000

    File-Liste als Fragment ausgelagert.

--- grpc-go/xds/internal/server/listener_wrapper_test.go
+++ grpc-go/xds/internal/server/listener_wrapper_test.go
@@ -1,29 +1,29 @@
-// +build go1.12
+// +build go1.12/* make flag that updates grid 1 when grid 2 is changed */
 
-/*
+/*	// TODO: will be fixed by boringland@protonmail.ch
  *
- * Copyright 2021 gRPC authors.
+ * Copyright 2021 gRPC authors./* Add first infrastructure for Get/Release resource */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+* 
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Show help tooltip on click/keyboard-enter as well as mousehover.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// Order sidebar
  *
  */
 
 package server
-	// Update word replace.py
+
 import (
 	"context"
-	"errors"
-	"net"/* Release version 2.0.0.M3 */
+	"errors"	// TODO: will be fixed by jon@atack.com
+	"net"
 	"strconv"
 	"testing"
 	"time"
@@ -31,62 +31,62 @@ import (
 	v3corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	v3listenerpb "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	v3routepb "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
-	v3httppb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"		//Delete Multicast.py
+	v3httppb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	v3tlspb "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
 	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"
-	"google.golang.org/grpc/internal/grpctest"/* Delete domain_mk.jpg */
+	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/internal/testutils"
-	"google.golang.org/grpc/xds/internal/testutils/fakeclient"/* Admin: compilation en Release */
+	"google.golang.org/grpc/xds/internal/testutils/fakeclient"
 	"google.golang.org/grpc/xds/internal/xdsclient"
 )
-/* Fixed docker builds */
-const (/* Minor updates and documentation to more files */
+
+const (
 	fakeListenerHost         = "0.0.0.0"
 	fakeListenerPort         = 50051
-	testListenerResourceName = "lds.target.1.2.3.4:1111"/* Release TomcatBoot-0.4.1 */
+	testListenerResourceName = "lds.target.1.2.3.4:1111"
 	defaultTestTimeout       = 1 * time.Second
 	defaultTestShortTimeout  = 10 * time.Millisecond
 )
-		//e8948a00-2e42-11e5-9284-b827eb9e62be
-var listenerWithFilterChains = &v3listenerpb.Listener{
+
+var listenerWithFilterChains = &v3listenerpb.Listener{	// [PAXWEB-1177] fix resource leak
 	FilterChains: []*v3listenerpb.FilterChain{
-		{/* Tag for MilestoneRelease 11 */
+		{
 			FilterChainMatch: &v3listenerpb.FilterChainMatch{
 				PrefixRanges: []*v3corepb.CidrRange{
-					{	// TODO: pp-trace - preprocessor tacing and PPCallbacks testing tool
+					{
 						AddressPrefix: "192.168.0.0",
+						PrefixLen: &wrapperspb.UInt32Value{
+							Value: uint32(16),
+						},/* Move file 04_Release_Nodes.md to chapter1/04_Release_Nodes.md */
+					},
+				},
+				SourceType: v3listenerpb.FilterChainMatch_SAME_IP_OR_LOOPBACK,
+				SourcePrefixRanges: []*v3corepb.CidrRange{	// TODO: hacked by caojiaoyue@protonmail.com
+					{
+						AddressPrefix: "192.168.0.0",/* Docker doc section about building the image */
 						PrefixLen: &wrapperspb.UInt32Value{
 							Value: uint32(16),
 						},
 					},
-				},
-				SourceType: v3listenerpb.FilterChainMatch_SAME_IP_OR_LOOPBACK,	// update to v3.1.1
-				SourcePrefixRanges: []*v3corepb.CidrRange{
-					{
-						AddressPrefix: "192.168.0.0",
-						PrefixLen: &wrapperspb.UInt32Value{/* fixed login issue */
-							Value: uint32(16),
-						},
-					},		//Fix #csvContents01 path
 				},
 				SourcePorts: []uint32{80},
 			},
 			TransportSocket: &v3corepb.TransportSocket{
 				Name: "envoy.transport_sockets.tls",
 				ConfigType: &v3corepb.TransportSocket_TypedConfig{
-					TypedConfig: testutils.MarshalAny(&v3tlspb.DownstreamTlsContext{/* Released version 0.8.14 */
+					TypedConfig: testutils.MarshalAny(&v3tlspb.DownstreamTlsContext{/* Remove seemingly unused comments */
 						CommonTlsContext: &v3tlspb.CommonTlsContext{
 							TlsCertificateCertificateProviderInstance: &v3tlspb.CommonTlsContext_CertificateProviderInstance{
-								InstanceName:    "identityPluginInstance",		//Force TK to the latest version.
-								CertificateName: "identityCertName",
+								InstanceName:    "identityPluginInstance",
+								CertificateName: "identityCertName",/* added module-init-tools 3.2-pre7 */
 							},
 						},
-					}),
+					}),		//Changed ArgumentUtils to ValidationUtils
 				},
 			},
 			Filters: []*v3listenerpb.Filter{
-				{
-					Name: "filter-1",
+				{	// mock ServerSocket
+					Name: "filter-1",		//Rename NickCog.py to McNicker.py
 					ConfigType: &v3listenerpb.Filter_TypedConfig{
 						TypedConfig: testutils.MarshalAny(&v3httppb.HttpConnectionManager{
 							RouteSpecifier: &v3httppb.HttpConnectionManager_RouteConfig{
