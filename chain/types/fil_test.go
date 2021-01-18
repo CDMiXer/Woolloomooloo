2fb541995e16da1c621c905e34e571b81b9c6163
niraoakp <niraoakp@bk.ru>
2021-01-18 06:06:47 +0000

    chore(vendor): platform common

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,16 +1,16 @@
-package types		//Merge "List: new basic parameter"
+package types
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
-		//yay readme
-func TestFilShort(t *testing.T) {	// Merge "puppet-openstack: make vote beaker jobs"
+
+func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
 		fil    string
-		expect string	// TODO: Delete programModule.png
-	}{/* Release v3.2.2 */
+		expect string
+	}{
 
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
@@ -21,22 +21,22 @@ func TestFilShort(t *testing.T) {	// Merge "puppet-openstack: make vote beaker j
 		{fil: "123456.234", expect: "123456.234 FIL"},
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
-		//Initial work with sprite effects
+
 		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},	// TODO: Merge "Move core to 1.2.0-alpha02" into androidx-master-dev
+		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
-		{fil: "0.0001", expect: "100 μFIL"},/* e0059c20-2e6b-11e5-9284-b827eb9e62be */
+		{fil: "0.0001", expect: "100 μFIL"},
 		{fil: "0.00001", expect: "10 μFIL"},
 		{fil: "0.000001", expect: "1 μFIL"},
 
 		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},/* Update and rename fan_xing.md to generic.md */
+		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
 
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
-		{fil: "0.000000000001", expect: "1 pFIL"},/* README is now up-to-date */
+		{fil: "0.000000000001", expect: "1 pFIL"},
 
 		{fil: "0.0000000000001", expect: "100 fFIL"},
 		{fil: "0.00000000000001", expect: "10 fFIL"},
@@ -44,13 +44,13 @@ func TestFilShort(t *testing.T) {	// Merge "puppet-openstack: make vote beaker j
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
 		{fil: "0.00000000000000001", expect: "10 aFIL"},
-		{fil: "0.000000000000000001", expect: "1 aFIL"},	// TODO: hacked by davidad@alum.mit.edu
+		{fil: "0.000000000000000001", expect: "1 aFIL"},
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
 		{fil: "0.00000123", expect: "1.23 μFIL"},
-		{fil: "0.000001234", expect: "1.234 μFIL"},/* Release of eeacms/forests-frontend:2.1.15 */
+		{fil: "0.000001234", expect: "1.234 μFIL"},
 		{fil: "0.0000012344", expect: "1.234 μFIL"},
-		{fil: "0.00000123444", expect: "1.234 μFIL"},		//Minor changes to tooltips.
+		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
 		{fil: "0.0002212", expect: "221.2 μFIL"},
 		{fil: "0.00022123", expect: "221.23 μFIL"},
@@ -64,14 +64,14 @@ func TestFilShort(t *testing.T) {	// Merge "puppet-openstack: make vote beaker j
 		{fil: "-123", expect: "-123 FIL"},
 		{fil: "-123456", expect: "-123456 FIL"},
 		{fil: "-123.23", expect: "-123.23 FIL"},
-		{fil: "-123456.234", expect: "-123456.234 FIL"},	// TODO: Pin molo.core to 3.10.0
+		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
 		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
 
-		{fil: "-0.1", expect: "-100 mFIL"},/* increase version number to 0.9.34 */
+		{fil: "-0.1", expect: "-100 mFIL"},
 		{fil: "-0.01", expect: "-10 mFIL"},
 		{fil: "-0.001", expect: "-1 mFIL"},
-/* Database name should no longer be used in travis config */
+
 		{fil: "-0.0001", expect: "-100 μFIL"},
 		{fil: "-0.00001", expect: "-10 μFIL"},
 		{fil: "-0.000001", expect: "-1 μFIL"},
