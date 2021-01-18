d957bdfc92d53d99981c4ff65a1e391cdb5fb22e
niraoakp <niraoakp@bk.ru>
2021-01-18 09:48:38 +0000

    factor into one if

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update README for cocoapods */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* f6679dc6-2e69-11e5-9284-b827eb9e62be */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release version [10.6.4] - alfter build */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
 
-import (
-	"fmt"
+import (/* Create strictor.txt */
+	"fmt"		//cleanup dead code
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Release 4.1 */
 
 	"github.com/go-chi/chi"
 )
 
 // HandleLast returns an http.HandlerFunc that writes json-encoded
-// build details to the the response body for the latest build.
+// build details to the the response body for the latest build./* Merge "ARM: dt: apq8084: Change the gpu vote for the bus bandwidth" */
 func HandleLast(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-) http.HandlerFunc {
+) http.HandlerFunc {		//Update windows-regtest-reset.bat
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			ref       = r.FormValue("ref")
+			ref       = r.FormValue("ref")/* Fix dependency groupId */
 			branch    = r.FormValue("branch")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//1ccf9b90-2e4b-11e5-9284-b827eb9e62be
 			return
 		}
 		if ref == "" {
@@ -49,14 +49,14 @@ func HandleLast(
 		if branch != "" {
 			ref = fmt.Sprintf("refs/heads/%s", branch)
 		}
-		build, err := builds.FindRef(r.Context(), repo.ID, ref)
+		build, err := builds.FindRef(r.Context(), repo.ID, ref)/* Add link to PerfSim tasks [skip ci] */
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)		//fix available nodes procedures
 			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
