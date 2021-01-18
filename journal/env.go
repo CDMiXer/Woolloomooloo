33c4763004eed3a63df824c15f44f0bbd9216669
niraoakp <niraoakp@bk.ru>
2021-01-18 21:28:50 +0000

    Delete plugin.video.digi-online.zip

--- journal/env.go
+++ journal/env.go
@@ -1,18 +1,18 @@
 package journal
 
-import (
+import (/* Factorize optional homomorphism type. */
 	"os"
 )
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
-
+/* Release changes */
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
-		}
+		}		//NEW Introduce function dolGetFirstLineOfText
 	}
 	// fallback if env variable is not set, or if it failed to parse.
 	return DefaultDisabledEvents
