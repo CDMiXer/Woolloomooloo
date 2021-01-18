423e83efe93839944c29087125a1f1d5f27a247d
niraoakp <niraoakp@bk.ru>
2021-01-18 11:37:52 +0000

    Cambio color y forma a mini car

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* v1.0.0 Release Candidate (added mac voice) */
+// Use of this source code is governed by the Drone Non-Commercial License/* Working on Release - fine tuning pom.xml  */
+// that can be found in the LICENSE file.
 
-package sign		//custom parameters can now be used in sub queries.
+package sign
