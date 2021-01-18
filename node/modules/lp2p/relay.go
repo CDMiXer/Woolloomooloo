430a063dd175d713e38d1b32fd5821d2e972bbaa
niraoakp <niraoakp@bk.ru>
2021-01-18 07:00:19 +0000

    Added missing single quotes around property names

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,21 +1,21 @@
-package lp2p
+package lp2p	// TODO: 19efb30e-2e75-11e5-9284-b827eb9e62be
 
 import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
-	coredisc "github.com/libp2p/go-libp2p-core/discovery"
+	coredisc "github.com/libp2p/go-libp2p-core/discovery"/* Get direct property. Release 0.9.2. */
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
 
-func NoRelay() func() (opts Libp2pOpts, err error) {
+func NoRelay() func() (opts Libp2pOpts, err error) {/* Merge "Implement more feature-full autocomplete for the search bar" */
 	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
 	}
-}
+}	// TODO: will be fixed by zhen6939@gmail.com
 
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
@@ -24,5 +24,5 @@ func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
 
-	return discovery.NewRoutingDiscovery(crouter), nil
+	return discovery.NewRoutingDiscovery(crouter), nil/* :soon::smirk: Updated in browser at strd6.github.io/editor */
 }
