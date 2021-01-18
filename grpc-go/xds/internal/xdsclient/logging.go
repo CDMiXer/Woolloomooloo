c0a8084826e5f682e9d65a2179e658bed2c62839
niraoakp <niraoakp@bk.ru>
2021-01-18 15:59:38 +0000

    A working version of the new-style repl tools.

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,20 +1,20 @@
-/*		//Removed ratio option as not implemented 
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release 0.6.8 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by joshua@yottadb.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release of eeacms/jenkins-slave:3.12 */
- *//* FontCache: Release all entries if app is destroyed. */
+ *
+ */
 
 package xdsclient
 
