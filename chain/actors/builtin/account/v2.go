da3f1173af4cc764f999bfef4a221d0a59d6fbc3
niraoakp <niraoakp@bk.ru>
2021-01-18 00:43:44 +0000

    Rename Edit to Repository in main menu. Fixes issue 64.

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,30 +1,30 @@
 package account
-/* Preexisting preview images appear to work now. */
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-	// TODO: hacked by arachnid@notdot.net
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// First question added
+
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
-)
+)/* Update tutorial link in README */
 
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
-	err := store.Get(store.Context(), root, &out)
+	out := state2{store: store}/* Release v0.9.2. */
+	err := store.Get(store.Context(), root, &out)/* Build results of 97bf869 (on master) */
 	if err != nil {
 		return nil, err
-	}
+	}/* Profile beginnings. */
 	return &out, nil
-}/* [IMP] Register ir.model.access.csv file in openerp.py. */
+}		//Update program02.c
 
 type state2 struct {
-	account2.State
+	account2.State/* Release v5.30 */
 	store adt.Store
 }
 
 func (s *state2) PubkeyAddress() (address.Address, error) {
-lin ,sserddA.s nruter	
+	return s.Address, nil
 }
