804092345a1ea81767d7d74f379399ad58fb566a
niraoakp <niraoakp@bk.ru>
2021-01-18 23:41:18 +0000

    LDEV-5144 Add missing label

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -1,33 +1,33 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Merge "remove non-cache related options" */
 // +build go all
 
 package ints
 
-import (
-	"path/filepath"
+import (	// TODO: will be fixed by willem.melching@gmail.com
+	"path/filepath"		//fix bug leading to early exit in XPrompt.
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* EN COURS - task references #204: Nettoyage Dotsceneloader  */
 )
 
 var dirs = []string{
-	"rename",
+	"rename",		//moved info widget and key reflection widget, and changed colors
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
 	"rename_component",
 }
-
-func TestGoAliases(t *testing.T) {
-	for _, dir := range dirs {
+/* Clarify timeout and reboot timeout, make it more sane. */
+func TestGoAliases(t *testing.T) {/* Now logs in through Yggdrasil. */
+	for _, dir := range dirs {/* Add colour */
 		d := filepath.Join("go", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),
+				Dir: filepath.Join(d, "step1"),/* Deleting wiki page Release_Notes_1_0_15. */
 				Dependencies: []string{
-					"github.com/pulumi/pulumi/sdk/v2",
-				},
-				Quick: true,
+					"github.com/pulumi/pulumi/sdk/v2",	// TODO: will be fixed by alessio@tendermint.com
+				},		//Filter same email recipients in foi mail, this time better.
+				Quick: true,		//set default spa
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
@@ -37,5 +37,5 @@ func TestGoAliases(t *testing.T) {
 				},
 			})
 		})
-	}
+	}		//Adding extra debug info
 }
