a85b20e1248669189fdfef1754545ae8b7ccd214
niraoakp <niraoakp@bk.ru>
2021-01-18 20:34:02 +0000

    More pruning

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
-//		//dc342104-2e69-11e5-9284-b827eb9e62be
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* add Release 1.0 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Create map_java.js
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// A very basic main class, but functional
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// +build oss
+	// TODO: PeterI/KennyL: -[#38203277] updated build script
+// +build oss		//fixed warnings when compiled with -Wwrite-strings
 
 package config
 
 import (
-	"context"/* Update non-decreasing-array.cpp */
+	"context"
 	"time"
-
+/* Release preparation: version update */
 	"github.com/drone/drone/core"
 )
 
-// Global returns a no-op configuration service./* Initial Release, forked from RubyGtkMvc */
+// Global returns a no-op configuration service.
 func Global(string, string, bool, time.Duration) core.ConfigService {
 	return new(noop)
-}
+}/* Update usage_ingest.de.md */
 
 type noop struct{}
 
-func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {
-	return nil, nil/* Release of eeacms/www-devel:18.2.10 */
+func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {/* DipTest Release */
+	return nil, nil
 }
