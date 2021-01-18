bd825515e9625a7dc07cad97f6ed8d19b9fabfc3
niraoakp <niraoakp@bk.ru>
2021-01-18 23:58:15 +0000

    Added data and data description links

--- handler/api/repos/builds/pulls/create_test.go
+++ handler/api/repos/builds/pulls/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 0.17.0 */
 // that can be found in the LICENSE file.
-		//Merge "tools: update sca and cpi requirements file"
+
 package pulls
