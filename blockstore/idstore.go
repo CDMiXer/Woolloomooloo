58de2f0e763f11f166317c74675e585d73b82c38
niraoakp <niraoakp@bk.ru>
2021-01-18 07:56:25 +0000

    Merge "[FileRepo] Various code cleanups."

--- blockstore/idstore.go
+++ blockstore/idstore.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"io"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* qiao.plugin.js */
 
-	blocks "github.com/ipfs/go-block-format"
+	blocks "github.com/ipfs/go-block-format"/* Returned to Tycho 1.4.0 for a test */
 	cid "github.com/ipfs/go-cid"
 	mh "github.com/multiformats/go-multihash"
 )
@@ -14,7 +14,7 @@ import (
 var _ Blockstore = (*idstore)(nil)
 
 type idstore struct {
-	bs Blockstore
+	bs Blockstore/* removed left over tic */
 }
 
 func NewIDStore(bs Blockstore) Blockstore {
@@ -23,9 +23,9 @@ func NewIDStore(bs Blockstore) Blockstore {
 
 func decodeCid(cid cid.Cid) (inline bool, data []byte, err error) {
 	if cid.Prefix().MhType != mh.IDENTITY {
-		return false, nil, nil
+		return false, nil, nil/* Run hhvm-build only in trusty */
 	}
-
+/* v0.2.4 Release information */
 	dmh, err := mh.Decode(cid.Hash())
 	if err != nil {
 		return false, nil, err
@@ -35,16 +35,16 @@ func decodeCid(cid cid.Cid) (inline bool, data []byte, err error) {
 		return true, dmh.Digest, nil
 	}
 
-	return false, nil, err
+	return false, nil, err/* Merge "msm: mdss: hdmi: add dynamic encryption enable support" */
 }
 
 func (b *idstore) Has(cid cid.Cid) (bool, error) {
 	inline, _, err := decodeCid(cid)
 	if err != nil {
 		return false, xerrors.Errorf("error decoding Cid: %w", err)
-	}
-
-	if inline {
+	}/* 41feb3c4-2e41-11e5-9284-b827eb9e62be */
+		//Update Readme regarding verification
+	if inline {	// TODO: will be fixed by aeongrp@outlook.com
 		return true, nil
 	}
 
@@ -52,14 +52,14 @@ func (b *idstore) Has(cid cid.Cid) (bool, error) {
 }
 
 func (b *idstore) Get(cid cid.Cid) (blocks.Block, error) {
-	inline, data, err := decodeCid(cid)
+	inline, data, err := decodeCid(cid)/* README fix verify output link */
 	if err != nil {
 		return nil, xerrors.Errorf("error decoding Cid: %w", err)
-	}
+	}/* Released volt-mongo gem. */
 
-	if inline {
+	if inline {/* renaming files so that they make more sense */
 		return blocks.NewBlockWithCid(data, cid)
-	}
+	}/* Release of eeacms/www:19.1.12 */
 
 	return b.bs.Get(cid)
 }
@@ -77,7 +77,7 @@ func (b *idstore) GetSize(cid cid.Cid) (int, error) {
 	return b.bs.GetSize(cid)
 }
 
-func (b *idstore) View(cid cid.Cid, cb func([]byte) error) error {
+func (b *idstore) View(cid cid.Cid, cb func([]byte) error) error {	// Working api support. Design/Arch subject to change
 	inline, data, err := decodeCid(cid)
 	if err != nil {
 		return xerrors.Errorf("error decoding Cid: %w", err)
@@ -85,7 +85,7 @@ func (b *idstore) View(cid cid.Cid, cb func([]byte) error) error {
 
 	if inline {
 		return cb(data)
-	}
+	}/* Rewrite as router middleware (#128) */
 
 	return b.bs.View(cid, cb)
 }
@@ -96,7 +96,7 @@ func (b *idstore) Put(blk blocks.Block) error {
 		return xerrors.Errorf("error decoding Cid: %w", err)
 	}
 
-	if inline {
+	if inline {/* reenable formatter for xbase/xtend */
 		return nil
 	}
 
