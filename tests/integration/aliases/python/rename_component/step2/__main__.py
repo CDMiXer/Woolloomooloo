ed5cf190a5bf4db09c33539aa63b43d467120955
niraoakp <niraoakp@bk.ru>
2021-01-18 11:52:18 +0000

    Fixes +449. question dialog when no merge is necessary

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -2,21 +2,21 @@
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+class Resource1(ComponentResource):/* [artifactory-release] Release version 3.3.7.RELEASE */
+    def __init__(self, name, opts=None):	// TODO: Log latest changes
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #3 - rename a component (and all it's children)
 # No change to the component...
-class ComponentThree(ComponentResource):
+class ComponentThree(ComponentResource):	// Changes in send_email method for report generation
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentThree", name, None, opts)
-        # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
+        # Note that both un-prefixed and parent-name-prefixed child names are supported. For the/* Hook up warning for an incomplete scanlist in scanf format strings. */
         # later, the implicit alias inherited from the parent alias will include replacing the name
         # prefix to match the parent alias name.
         resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
 
 # ...but applying an alias to the instance successfully renames both the component and the children.
-comp3 = ComponentThree("newcomp3", ResourceOptions(
+comp3 = ComponentThree("newcomp3", ResourceOptions(	// TODO: hacked by mowrain@yandex.com
     aliases=[Alias(name="comp3")]))
