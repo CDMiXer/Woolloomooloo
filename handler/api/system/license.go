b630bcf603facd760028a909001b742e5a434a67
niraoakp <niraoakp@bk.ru>
2021-01-18 17:24:08 +0000

    Load CF7 - Google API v3 only specified pages

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//debug and release build using makefile
 // that can be found in the LICENSE file.
+		//Adding stalebot conf file to the repository
+// +build !oss
 
-// +build !oss	// TODO: will be fixed by alan.shaw@protocol.ai
+package system/* [RELEASE] Release of pagenotfoundhandling 2.3.0 */
 
-package system
-
-import (
+import (/* Added the Line class and some others. */
 	"net/http"
 
-	"github.com/drone/drone/core"/* Merge "Release 3.2.3.338 Prima WLAN Driver" */
-	"github.com/drone/drone/handler/api/render"
-)/* Ignore errors from Sentry when the application is offline */
-
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"/* Fix transaction/sql error */
+)
+/* Update pocketlint. Release 0.6.0. */
 // HandleLicense returns an http.HandlerFunc that writes
-// json-encoded license details to the response body.		//Remove remains from the old bookmarklet code
+// json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, license, 200)
