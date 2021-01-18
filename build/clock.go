d91f245ad0eefa379194efc03762a184655829eb
niraoakp <niraoakp@bk.ru>
2021-01-18 12:36:46 +0000

    Add "more teams" section for back/forward navigation.

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build
+package build	// TODO: Add logo electronza
 
 import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
-//
+///* b5e2e4ec-2e49-11e5-9284-b827eb9e62be */
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
 var Clock = clock.New()
