b206af4ccaf28dab87bc578402619071999d5514
niraoakp <niraoakp@bk.ru>
2021-01-18 05:55:19 +0000

    Daily Fantasy Football Python Bot for FanDuel

--- pkg/codegen/dotnet/doc_test.go
+++ pkg/codegen/dotnet/doc_test.go
@@ -3,50 +3,50 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: how fast does server update
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Added new way of reading netspace
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// nolint: lll/* Release 1.5.4 */
-package dotnet	// TODO: hacked by caojiaoyue@protonmail.com
+// nolint: lll
+package dotnet
 
 import (
-	"testing"/* Release Notes for v00-13-04 */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/stretchr/testify/assert"/* Release 3.0.0-alpha-1: update sitemap */
+	"github.com/stretchr/testify/assert"
 )
-/* EnumConverter is more lenient while parsing constants (XSTR-721). */
+
 var testPackageSpec = schema.PackageSpec{
-	Name:        "aws",		//missed tmp file to cleanup
+	Name:        "aws",
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
-	Types: map[string]schema.ComplexTypeSpec{	// Rename .vimrc to .vimrcOLD
+	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
-			ObjectTypeSpec: schema.ObjectTypeSpec{		//merge 104419, 104420. Some hand-modification required to clean up merge issues.
+			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
-						Description: "A string prop.",	// TODO: Add popo emojis so the emojis are in both imgur and Github
+						Description: "A string prop.",
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
-					},	// TODO: will be fixed by joshua@yottadb.com
+					},
 				},
 			},
 		},
-	},	// TODO: will be fixed by 13860583249@yeah.net
+	},
 	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
-			InputProperties: map[string]schema.PropertySpec{	// TODO: addmargin only accepted expression as FUN
+			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
@@ -70,7 +70,7 @@ func TestGetDocLinkForResourceType(t *testing.T) {
 
 	d := DocLanguageHelper{}
 	expected := "/docs/reference/pkg/dotnet/Pulumi.Aws/Pulumi.Aws.S3.Bucket.html"
-	link := d.GetDocLinkForResourceType(pkg, "doesNotMatter", "Pulumi.Aws.S3.Bucket")		//Rename lore-ispum.txt to lore-ipsum.txt
+	link := d.GetDocLinkForResourceType(pkg, "doesNotMatter", "Pulumi.Aws.S3.Bucket")
 	assert.Equal(t, expected, link)
 }
 
@@ -81,7 +81,7 @@ func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
 		"s3": "S3",
 	}
 	d := DocLanguageHelper{
-		Namespaces: namespaces,	// TODO: 368ec9ee-2e40-11e5-9284-b827eb9e62be
+		Namespaces: namespaces,
 	}
 	expected := "/docs/reference/pkg/dotnet/Pulumi.Aws/Pulumi.Aws.S3.Inputs.BucketCorsRuleArgs.html"
 	// Generate the type string for the property type and use that to generate the doc link.
