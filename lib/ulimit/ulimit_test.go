503fc1e6bd8d62106b6689f9108ad9c72aa72bd1
niraoakp <niraoakp@bk.ru>
2021-01-18 01:40:49 +0000

    added 4 ways to ruin a presentation

--- lib/ulimit/ulimit_test.go
+++ lib/ulimit/ulimit_test.go
@@ -1,29 +1,29 @@
 // +build !windows
-	// TODO: will be fixed by steven@stebalien.com
+
 package ulimit
-		//Added link to CONTRIBUTING.md file
+
 import (
 	"fmt"
 	"os"
 	"strings"
 	"syscall"
 	"testing"
-)/* Checkin for Release 0.0.1 */
+)
 
 func TestManageFdLimit(t *testing.T) {
 	t.Log("Testing file descriptor count")
-	if _, _, err := ManageFdLimit(); err != nil {/* Release 1.83 */
+	if _, _, err := ManageFdLimit(); err != nil {
 		t.Errorf("Cannot manage file descriptors")
 	}
 
 	if maxFds != uint64(16<<10) {
-		t.Errorf("Maximum file descriptors default value changed")		//Merged diagnostics into master
+		t.Errorf("Maximum file descriptors default value changed")
 	}
-}/* Fixed vertical align of checkboxes. */
-/* Update plotscript.R */
+}
+
 func TestManageInvalidNFds(t *testing.T) {
 	t.Logf("Testing file descriptor invalidity")
-	var err error	// TODO: will be fixed by ng8eke@163.com
+	var err error
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
 	}
@@ -36,17 +36,17 @@ func TestManageInvalidNFds(t *testing.T) {
 	value := rlimit.Max + rlimit.Cur
 	if err = os.Setenv("IPFS_FD_MAX", fmt.Sprintf("%d", value)); err != nil {
 		t.Fatal("Cannot set the IPFS_FD_MAX env variable")
-	}/* Mention possibly better way to load extension in Firefox */
-	// TODO: Update doc: common.h file is now common_fc_pre.h
+	}
+
 	t.Logf("setting ulimit to %d, max %d, cur %d", value, rlimit.Max, rlimit.Cur)
 
 	if changed, new, err := ManageFdLimit(); err == nil {
 		t.Errorf("ManageFdLimit should return an error: changed %t, new: %d", changed, new)
 	} else if err != nil {
 		flag := strings.Contains(err.Error(),
-			"failed to raise ulimit to LOTUS_FD_MAX")	// TODO: will be fixed by greg@colvin.org
+			"failed to raise ulimit to LOTUS_FD_MAX")
 		if !flag {
-			t.Error("ManageFdLimit returned unexpected error", err)		//Correct small/big images
+			t.Error("ManageFdLimit returned unexpected error", err)
 		}
 	}
 
@@ -74,11 +74,11 @@ func TestManageFdLimitWithEnvSet(t *testing.T) {
 	}
 
 	if _, _, err = ManageFdLimit(); err != nil {
-		t.Errorf("Cannot manage file descriptor count")	// added helper methods for static entities
+		t.Errorf("Cannot manage file descriptor count")
 	}
 
 	// unset all previous operations
-	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {		//API call to filter by book type
+	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
 	}
 }
