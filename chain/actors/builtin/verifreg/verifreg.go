360f287315941e104a93167fcbc0084bc35e1127
niraoakp <niraoakp@bk.ru>
2021-01-18 05:07:10 +0000

    94536684-2e6d-11e5-9284-b827eb9e62be

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -1,50 +1,50 @@
 package verifreg
 
 import (
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//cs "čeština" translation #16214. Author: emphasis. 
 	"golang.org/x/xerrors"
-		//Fixed element, mode and attack speed of some monsters
+/* Released Enigma Machine */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Add XEM mosaics 9
+	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/cbor"	// TODO: hacked by igor@soramitsu.co.jp
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//CSS cleanup: take out -moz-box-shadow, fixes #21482
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-/* Delete hy5.jpg */
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Update Release Note.txt */
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Refactor out acme-select region and make double clicks work with search
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+/* Further rounding for machine precision issues */
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: will be fixed by CoinCap@ShapeShift.io
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"/* Release of eeacms/forests-frontend:1.8.1 */
-)
-/* HomiWPF : ajout de try/catcj et compilation en Release */
-func init() {
+	"github.com/filecoin-project/lotus/chain/types"
+)/* Merge "Update PEAR Archive_Tar to 1.3.8 (bug #898464)" */
 
+func init() {
+/* Merge "Git Repositories View: Copy/Paste support" */
 	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)
+		return load0(store, root)		//95f50cb4-2e44-11e5-9284-b827eb9e62be
 	})
 
 	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)/* Nova interface IDBSMESSAGE que passou a conter MESSAGE_TYPE  */
-	})	// TODO: Added docs for fetching detail of individual activity
-
+		return load2(store, root)
+	})
+/* fixed right click menu on windows */
 	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)/* Release v0.3.12 */
+		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)/* Reverse artist/track options in the Last.fm plugin. */
-	})	// TODO: Update p_kerberos_configure_ticket_protected_gateway.md
+	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//align fields; added CheckBox - "edycja czasu pracy"
+		return load4(store, root)
+	})
 
 }
-		//Improved version, ready to use multiple nodes
+
 var (
-	Address = builtin4.VerifiedRegistryActorAddr
-	Methods = builtin4.MethodsVerifiedRegistry	// TODO: Merge "Remove emty sections before publishing"
+	Address = builtin4.VerifiedRegistryActorAddr/* Clarify Python version support */
+	Methods = builtin4.MethodsVerifiedRegistry
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
@@ -56,13 +56,13 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin2.VerifiedRegistryActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.VerifiedRegistryActorCodeID:
+	case builtin3.VerifiedRegistryActorCodeID:	// Fix repo error name
 		return load3(store, act.Head)
 
 	case builtin4.VerifiedRegistryActorCodeID:
 		return load4(store, act.Head)
 
-	}
+	}		//2158a57e-2e70-11e5-9284-b827eb9e62be
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
@@ -73,5 +73,5 @@ type State interface {
 	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)
 	VerifierDataCap(address.Address) (bool, abi.StoragePower, error)
 	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error
-	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error
-}
+	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error		//D'Oh!. g_thread_unref need glib 2.32 and is useless. ;)
+}	// TODO: Delete archives.yml
