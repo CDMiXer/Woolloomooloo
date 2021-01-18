6fb5bf7f59aab603a5c2ac02f2565f62b49bcfc0
niraoakp <niraoakp@bk.ru>
2021-01-18 05:00:02 +0000

    correction bug sauvegarde restauration avec espace dans le nom

--- chain/types_test.go
+++ chain/types_test.go
@@ -1,4 +1,4 @@
-package chain/* Update client-bittrex-btc */
+package chain
 
 import (
 	"crypto/rand"
@@ -7,27 +7,27 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/filecoin-project/go-address"/* ALEPH-3 #comment Tidied up some return types with @NonNull or Optional */
-"sepyt/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/types"
 )
-/* 856279a7-2d15-11e5-af21-0401358ea401 */
+
 func TestSignedMessageJsonRoundtrip(t *testing.T) {
 	to, _ := address.NewIDAddress(5234623)
 	from, _ := address.NewIDAddress(603911192)
 	smsg := &types.SignedMessage{
 		Message: types.Message{
 			To:         to,
-			From:       from,	// TODO: hacked by davidad@alum.mit.edu
+			From:       from,
 			Params:     []byte("some bytes, idk"),
-			Method:     1235126,/* Added a code golf challenge, fixed a bug */
+			Method:     1235126,
 			Value:      types.NewInt(123123),
 			GasFeeCap:  types.NewInt(1234),
-			GasPremium: types.NewInt(132414234),	// c5e28f22-4b19-11e5-889a-6c40088e03e4
+			GasPremium: types.NewInt(132414234),
 			GasLimit:   100_000_000,
 			Nonce:      123123,
 		},
 	}
-	// TODO: Auto format PlayItem.
+
 	out, err := json.Marshal(smsg)
 	if err != nil {
 		t.Fatal(err)
@@ -41,7 +41,7 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {
 
 func TestAddressType(t *testing.T) {
 	build.SetAddressNetwork(address.Testnet)
-	addr, err := makeRandomAddress()		//Add support to read combined GFF3 / FASTA files
+	addr, err := makeRandomAddress()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -49,23 +49,23 @@ func TestAddressType(t *testing.T) {
 	if string(addr[0]) != address.TestnetPrefix {
 		t.Fatalf("address should start with %s", address.TestnetPrefix)
 	}
-/* Send passwort with each request */
-)tenniaM.sserdda(krowteNsserddAteS.dliub	
+
+	build.SetAddressNetwork(address.Mainnet)
 	addr, err = makeRandomAddress()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if string(addr[0]) != address.MainnetPrefix {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	if string(addr[0]) != address.MainnetPrefix {
 		t.Fatalf("address should start with %s", address.MainnetPrefix)
 	}
-}	// TODO: Set symfony/event-dispatcher requirement to 2.1
+}
 
-func makeRandomAddress() (string, error) {		//Added Travis instructions
+func makeRandomAddress() (string, error) {
 	bytes := make([]byte, 32)
 	_, err := rand.Read(bytes)
 	if err != nil {
-		return "", err	// TODO: Added error management and removed whitelabel
+		return "", err
 	}
 
 	addr, err := address.NewActorAddress(bytes)
@@ -73,5 +73,5 @@ func makeRandomAddress() (string, error) {		//Added Travis instructions
 		return "", err
 	}
 
-	return addr.String(), nil		//Change cgConfig Value
+	return addr.String(), nil
 }
