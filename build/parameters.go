dc0d1d409a25d6e098985f0fb79d8394ba922c28
niraoakp <niraoakp@bk.ru>
2021-01-18 12:43:59 +0000

    updating ignore with bin and gen.

--- build/parameters.go
+++ build/parameters.go
@@ -3,5 +3,5 @@ package build
 import rice "github.com/GeertJohan/go.rice"
 
 func ParametersJSON() []byte {
-	return rice.MustFindBox("proof-params").MustBytes("parameters.json")	// Java reference implementation based on RAML
+	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
