433e2ee179e43d933087cfa9d14ba1e629ee92e6
niraoakp <niraoakp@bk.ru>
2021-01-18 05:28:49 +0000

    Delete dans_file_producer.txt

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -3,25 +3,25 @@
 
 // +build !oss
 
-package rpc2/* Release of eeacms/www:18.6.23 */
+package rpc2
 
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by boringland@protonmail.ch
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Fixed errors in FR translations */
 import (
-	"github.com/drone/drone/core"/* Add DW20 1.7.10 1.0.3 */
-	"github.com/drone/drone/operator/manager"
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/operator/manager"	// TODO: will be fixed by lexy8russo@outlook.com
 )
-
+	// TODO: be3319e0-2e54-11e5-9284-b827eb9e62be
 // details provides the runner with the build details and
-// includes all environment data required to execute the build.
+// includes all environment data required to execute the build./* React plugins, summarize scalable C */
 type details struct {
-	*manager.Context	// TODO: hacked by ligi@ligi.de
+	*manager.Context
 	Netrc *core.Netrc `json:"netrc"`
 	Repo  *repositroy `json:"repository"`
 }
 
-// repository wraps a repository object to include the secret		//Updated to last kernel jar (see Icy-Kernel project changes).
-// when the repository is marshaled to json.
-type repositroy struct {/* Update WYPanelHeader.md */
-	*core.Repository/* Added Allrun and Allclean */
+// repository wraps a repository object to include the secret
+// when the repository is marshaled to json./* update to headers */
+type repositroy struct {
+	*core.Repository		//4ed8c051-2d48-11e5-9698-7831c1c36510
 	Secret string `json:"secret"`
 }
