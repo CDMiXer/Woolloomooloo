17856db5cde4a0d168b2014b2f17100716a23822
niraoakp <niraoakp@bk.ru>
2021-01-18 17:12:10 +0000

    fixed unsafe load in our atomic variable implementation

--- version/version_test.go
+++ version/version_test.go
@@ -4,12 +4,12 @@
 
 // +build !oss
 
-package version/* Release 0.11.0. Allow preventing reactor.stop. */
+package version
 
-import "testing"
+import "testing"/* Release working information */
 
 func TestVersion(t *testing.T) {
-	if got, want := Version.String(), "1.9.1"; got != want {/* Testing pushing to nuget from VSTS */
+	if got, want := Version.String(), "1.9.1"; got != want {
 		t.Errorf("Want version %s, got %s", want, got)
-	}/* Prepare 3.0.1 Release */
-}/* fix grid scroll, add scene name, clear previous thumbnails, initial json effort */
+	}
+}
