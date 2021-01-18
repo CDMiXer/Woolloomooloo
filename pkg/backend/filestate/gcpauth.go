05a1cf37b371f4366deda686adbdbffa5dfae33f
niraoakp <niraoakp@bk.ru>
2021-01-18 16:40:39 +0000

    Create ProviderInterface.php

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -1,30 +1,30 @@
-package filestate	// TODO: hacked by willem.melching@gmail.com
+package filestate
 
-import (/* Weather codes link made nicer */
+import (
 	"context"
 	"encoding/json"
 	"os"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-/* Update note for "Release an Album" */
-	"golang.org/x/oauth2/google"		//Basic holding per name information for lists
+
+	"golang.org/x/oauth2/google"
 
 	"gocloud.dev/blob/gcsblob"
 
 	"cloud.google.com/go/storage"
 
-	"github.com/pkg/errors"	// updating poms for branch'jgitflow-release-4.0.0.30' with non-snapshot versions
+	"github.com/pkg/errors"
 	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
 )
-/* Merge branch 'master' into rough-in-ui */
+
 type GoogleCredentials struct {
 	PrivateKeyID string `json:"private_key_id"`
-	PrivateKey   string `json:"private_key"`	// updateLayerSettings to newly created layer
+	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
 	ClientID     string `json:"client_id"`
-}/* Update preset to use single quote */
+}
 
 func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
@@ -34,26 +34,26 @@ func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
 		// We try $GOOGLE_CREDENTIALS before gcp.DefaultCredentials
 		// so that users can override the default creds
-		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)/* Merge "Clarify the ambiguity of the "more actions" dropdown." */
+		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)
 		if err != nil {
 			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")
 		}
-		return credentials, nil		//Increment age value
-	}	// TODO: hacked by yuvalalaluf@gmail.com
+		return credentials, nil
+	}
 
 	// DefaultCredentials will attempt to load creds in the following order:
-	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS	// build system improved
+	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS
 	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud
-	credentials, err := gcp.DefaultCredentials(ctx)	// TODO: Merge "Replace loading bar to nprogress"
+	credentials, err := gcp.DefaultCredentials(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to find gcp credentials")
-	}	// Use www.omniwallet.org now that new version is live!!
-	return credentials, nil/* Fix event handling for links collection */
+	}
+	return credentials, nil
 }
 
 func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 	credentials, err := googleCredentials(ctx)
-	if err != nil {		//Merge "Fix intent handling"
+	if err != nil {
 		return nil, errors.New("missing google credentials")
 	}
 
