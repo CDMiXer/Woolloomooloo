9f06b04aec355c289ae1b8a487c872b46e7e1c80
niraoakp <niraoakp@bk.ru>
2021-01-18 12:18:26 +0000

    Changing Readme

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,4 +1,4 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -7,13 +7,13 @@
 package crons
 
 import (
-	"net/http"/* Add call to action for blog posts */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)		//Prune and pull branch list from remote list
+)
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of cron jobs to the response body.
@@ -21,21 +21,21 @@ func HandleList(
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Twitter fix */
-		var (/* Release of eeacms/eprtr-frontend:0.3-beta.6 */
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)		//ab3e9b26-2e76-11e5-9284-b827eb9e62be
+			render.NotFound(w, err)
 			return
 		}
 		list, err := crons.List(r.Context(), repo.ID)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-}		
+		}
 		render.JSON(w, list, 200)
 	}
 }
