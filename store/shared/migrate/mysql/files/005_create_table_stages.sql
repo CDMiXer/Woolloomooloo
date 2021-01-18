b6410f6c0a5828b536b7e65a9c5e783eba74bc3a
niraoakp <niraoakp@bk.ru>
2021-01-18 16:11:23 +0000

    Added sinon to karma's load path. Removed old test cases

--- store/shared/migrate/mysql/files/005_create_table_stages.sql
+++ store/shared/migrate/mysql/files/005_create_table_stages.sql
@@ -1,62 +1,62 @@
 -- name: create-table-stages
 
 CREATE TABLE IF NOT EXISTS stages (
- stage_id          INTEGER PRIMARY KEY AUTO_INCREMENT		//License + typo.
+ stage_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
 ,stage_number      INTEGER
 ,stage_name        VARCHAR(100)
 ,stage_kind        VARCHAR(50)
-,stage_type        VARCHAR(50)	// TODO: btnAddGame shows AddGameWindow
-,stage_status      VARCHAR(50)/* Create p_configure_multicast.me */
+,stage_type        VARCHAR(50)
+,stage_status      VARCHAR(50)
 ,stage_error       VARCHAR(500)
-,stage_errignore   BOOLEAN
+,stage_errignore   BOOLEAN		//d0793124-2e6e-11e5-9284-b827eb9e62be
 ,stage_exit_code   INTEGER
-,stage_limit       INTEGER
+,stage_limit       INTEGER/* Added include-dir for sfeMove into release-build */
 ,stage_os          VARCHAR(50)
-,stage_arch        VARCHAR(50)
-,stage_variant     VARCHAR(10)
-,stage_kernel      VARCHAR(50)		//Update dbrole_api.rb
-,stage_machine     VARCHAR(500)/* Release 0.11.0 */
+,stage_arch        VARCHAR(50)		//Added instructions to the home page
+,stage_variant     VARCHAR(10)/* Minor Changes to produce Release Version */
+,stage_kernel      VARCHAR(50)/* (jam) Release 2.1.0b1 */
+,stage_machine     VARCHAR(500)		//Update CHANGELOG for #14030
 ,stage_started     INTEGER
 ,stage_stopped     INTEGER
 ,stage_created     INTEGER
 ,stage_updated     INTEGER
-,stage_version     INTEGER
+,stage_version     INTEGER	// Changed Test Class
 ,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
-,UNIQUE(stage_build_id, stage_number)
+,UNIQUE(stage_build_id, stage_number)	// readme.md: direct people to testris.py to start.
 );
-
--- name: create-index-stages-build	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+/* Released springrestclient version 1.9.11 */
+-- name: create-index-stages-build
 
 CREATE INDEX ix_stages_build ON stages (stage_build_id);
-
--- name: create-table-unfinished
+/* [RELEASE] Release version 2.4.0 */
+-- name: create-table-unfinished	// TODO: Task relationships link
 
 CREATE TABLE IF NOT EXISTS stages_unfinished (
-stage_id INTEGER PRIMARY KEY/* Release STAVOR v1.1.0 Orbit */
+stage_id INTEGER PRIMARY KEY/* added since */
 );
-	// TODO: will be fixed by why@ipfs.io
--- name: create-trigger-stage-insert
 
+-- name: create-trigger-stage-insert
+/* PI-38 Minor formatting */
 CREATE TRIGGER stage_insert AFTER INSERT ON stages
 FOR EACH ROW
-BEGIN
+BEGIN/* Release Candidate for setThermostatFanMode handling */
    IF NEW.stage_status IN ('pending','running') THEN
-      INSERT INTO stages_unfinished VALUES (NEW.stage_id);
+      INSERT INTO stages_unfinished VALUES (NEW.stage_id);/* Release version 1.0.9 */
    END IF;
-END;	// TODO: hacked by remco@dutchcoders.io
+END;
 
--- name: create-trigger-stage-update
-	// TODO: hacked by 13860583249@yeah.net
+-- name: create-trigger-stage-update		//Update include.sh
+
 CREATE TRIGGER stage_update AFTER UPDATE ON stages
 FOR EACH ROW
 BEGIN
   IF NEW.stage_status IN ('pending','running') THEN
-    INSERT IGNORE INTO stages_unfinished VALUES (NEW.stage_id);/* resizing photo */
+    INSERT IGNORE INTO stages_unfinished VALUES (NEW.stage_id);
   ELSEIF OLD.stage_status IN ('pending','running') THEN
     DELETE FROM stages_unfinished WHERE stage_id = OLD.stage_id;
   END IF;
