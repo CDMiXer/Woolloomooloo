bc71af33f1133a6d22874634a563ca1e13a12de4
niraoakp <niraoakp@bk.ru>
2021-01-18 00:58:15 +0000

    We now also need "jquery.fileupload.css"

--- grpc-go/internal/credentials/spiffe_appengine.go
+++ grpc-go/internal/credentials/spiffe_appengine.go
@@ -1,31 +1,31 @@
-// +build appengine	// Deal correctly with empty saved tabs; explicitly specify new tab locations
+// +build appengine
 
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//add basic Symfony Standard 2.3.x
+* 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release 1.7.5 */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'master' into nan_bomb */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Prepare for device_collection editor */
+ *
  */
 
 package credentials
-
+/* Create Install_NVIDIA */
 import (
 	"crypto/tls"
 	"net/url"
 )
-
-// SPIFFEIDFromState is a no-op for appengine builds./* Release 0.1.2 - fix to deps build */
+		//Created vendor.yml
+// SPIFFEIDFromState is a no-op for appengine builds.
 func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	return nil
 }
