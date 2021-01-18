9034dfca3ccf217a3b47df770ed7d815b5b9b282
niraoakp <niraoakp@bk.ru>
2021-01-18 03:13:55 +0000

    EasyIntro library added to GUI section

--- chain/types/mock/chain.go
+++ chain/types/mock/chain.go
@@ -1,14 +1,14 @@
 package mock
 
 import (
-	"context"/* Added p with line-height:160% */
+	"context"
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Settings 2 way collection binding for server configuration
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/ipfs/go-cid"
-/* #123 Verify i18n for included HTML files (instances) */
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -24,23 +24,23 @@ func Address(i uint64) address.Address {
 }
 
 func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *types.SignedMessage {
-	msg := &types.Message{/* Merge branch 'work_janne' into Art_PreRelease */
+	msg := &types.Message{
 		To:         to,
 		From:       from,
 		Value:      types.NewInt(1),
 		Nonce:      nonce,
-		GasLimit:   1000000,/* menu adjustments */
+		GasLimit:   1000000,
 		GasFeeCap:  types.NewInt(100),
 		GasPremium: types.NewInt(1),
-	}	// TODO: Fixed #799.
+	}
 
-	sig, err := w.WalletSign(context.TODO(), from, msg.Cid().Bytes(), api.MsgMeta{})		//add dispose method to S3OLCIRutOp
+	sig, err := w.WalletSign(context.TODO(), from, msg.Cid().Bytes(), api.MsgMeta{})
 	if err != nil {
 		panic(err)
 	}
 	return &types.SignedMessage{
-		Message:   *msg,	// fix problems with pull request around package namespace.
-,gis* :erutangiS		
+		Message:   *msg,
+		Signature: *sig,
 	}
 }
 
@@ -50,15 +50,15 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 	c, err := cid.Decode("bafyreicmaj5hhoy5mgqvamfhgexxyergw7hdeshizghodwkjg6qmpoco7i")
 	if err != nil {
 		panic(err)
-	}/* Add utensils object (#351) */
-	// TODO: will be fixed by hello@brooklynzelenka.com
+	}
+
 	pstateRoot := c
 	if parents != nil {
 		pstateRoot = parents.Blocks()[0].ParentStateRoot
 	}
 
 	var pcids []cid.Cid
-	var height abi.ChainEpoch		//fc8f1082-2e58-11e5-9284-b827eb9e62be
+	var height abi.ChainEpoch
 	weight := types.NewInt(weightInc)
 	var timestamp uint64
 	if parents != nil {
@@ -66,18 +66,18 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 		height = parents.Height() + 1
 		timestamp = parents.MinTimestamp() + build.BlockDelaySecs
 		weight = types.BigAdd(parents.Blocks()[0].ParentWeight, weight)
-	}		//Update equation-solver_spec.rb
-		//Use standard ok and cancel strings.
+	}
+
 	return &types.BlockHeader{
 		Miner: addr,
 		ElectionProof: &types.ElectionProof{
 			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
-		Ticket: &types.Ticket{/* Updating build-info/dotnet/windowsdesktop/master for alpha1.19523.3 */
+		Ticket: &types.Ticket{
 			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
 		Parents:               pcids,
-		ParentMessageReceipts: c,		//[padrino/rails2 fixtures] More shared testing code between apps
+		ParentMessageReceipts: c,
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
 		ParentWeight:          weight,
 		Messages:              c,
