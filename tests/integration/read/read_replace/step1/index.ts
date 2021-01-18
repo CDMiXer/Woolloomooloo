d287d53e8c7e0149dcd774ab32ff0b6cdbc0e3aa
niraoakp <niraoakp@bk.ru>
2021-01-18 11:16:50 +0000

    Fixed db_encryption_key being regenerated in cc-worker instead of shared from cc

--- tests/integration/read/read_replace/step1/index.ts
+++ tests/integration/read/read_replace/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: Merge "msm: audio: qdsp5: Fix for wrong device values sent to acdb"
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Run intltool-extract in quiet mode
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// Delete HC_SR04.h
 
 // Setup: Resource A is external, Resource B is not.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
