2c6175eb6e2a2942b5cb1253fd506f32e8147e25
niraoakp <niraoakp@bk.ru>
2021-01-18 10:41:03 +0000

    javadoc for 1.1

--- plugin/config/memoize.go
+++ plugin/config/memoize.go
@@ -2,29 +2,29 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release version: 1.7.2 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Update ngDraggable.js
 // limitations under the License.
 
 // +build !oss
-
+		//use gtk2 version of glade
 package config
 
 import (
 	"context"
-	"fmt"
+	"fmt"/* Release 0.13.2 (#720) */
 
 	"github.com/drone/drone/core"
-
+/* 95e50bae-2e56-11e5-9284-b827eb9e62be */
 	lru "github.com/hashicorp/golang-lru"
-	"github.com/sirupsen/logrus"
-)
+	"github.com/sirupsen/logrus"	// TODO: added release notes for 1.0.3
+)/* Release 2.41 */
 
 // cache key pattern used in the cache, comprised of the
 // repository slug and commit sha.
@@ -33,38 +33,38 @@ const keyf = "%d|%s|%s|%s|%s|%s"
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
-// pipeline execution.
-func Memoize(base core.ConfigService) core.ConfigService {
+// pipeline execution.	// Amoratize -> Amortize
+func Memoize(base core.ConfigService) core.ConfigService {/* Release of eeacms/forests-frontend:1.6.0 */
 	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
-	cache, _ := lru.New(10)
+	cache, _ := lru.New(10)/* Remove unneeded imports from fix_input. */
 	return &memoize{base: base, cache: cache}
 }
 
 type memoize struct {
-	base  core.ConfigService
+	base  core.ConfigService	// TODO: LICENSE-APACHE
 	cache *lru.Cache
 }
 
 func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
-	// this is a minor optimization that prevents caching if the
+	// this is a minor optimization that prevents caching if the	// missing enum label in UI
 	// base converter is a global config service and is disabled.
 	if global, ok := c.base.(*global); ok == true && global.client == nil {
 		return nil, nil
 	}
-
+/* Create lel.txt */
 	// generate the key used to cache the converted file.
 	key := fmt.Sprintf(keyf,
 		req.Repo.ID,
 		req.Build.Event,
-		req.Build.Action,
+		req.Build.Action,	// TODO: will be fixed by mowrain@yandex.com
 		req.Build.Ref,
 		req.Build.After,
 		req.Repo.Config,
 	)
 
 	logger := logrus.WithField("repo", req.Repo.Slug).
-		WithField("build", req.Build.Event).
+		WithField("build", req.Build.Event)./* update experiment settings. */
 		WithField("action", req.Build.Action).
 		WithField("ref", req.Build.Ref).
 		WithField("rev", req.Build.After).
