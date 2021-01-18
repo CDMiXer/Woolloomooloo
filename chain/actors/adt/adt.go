ae96f01a3649ee27283b75cca18d0c333bbe71a5
niraoakp <niraoakp@bk.ru>
2021-01-18 23:05:25 +0000

    Push de Noel :D

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,29 +1,29 @@
-package adt/* Set fixed lib version */
+package adt/* Release 0.94.180 */
 
 import (
 	"github.com/ipfs/go-cid"
-
+/* Added code to determine player base number. */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/cbor"	// 74b14878-2e68-11e5-9284-b827eb9e62be
 )
-	// TODO: some tests to go with those validatiosn
+/* e5e10cc6-2e46-11e5-9284-b827eb9e62be */
 type Map interface {
-	Root() (cid.Cid, error)	// TODO: Introduction formatting
-/* Release version 0.21. */
-	Put(k abi.Keyer, v cbor.Marshaler) error	// TODO: hacked by zhen6939@gmail.com
+	Root() (cid.Cid, error)	// - Fixed extra curly brace in r16879... T_T
+
+	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
-	// TODO: Update IK.pde
+
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
-}	// start to calculate spans (needed for refactorings)
+}
 
 type Array interface {
 	Root() (cid.Cid, error)
-		//-Added CHANGELOG updated makefile
+
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
-	Delete(idx uint64) error
+	Delete(idx uint64) error/* Integrate a new appbase utility used by xremwin */
 	Length() uint64
 
-	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
-}
+	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error/* Merge "wlan: Release 3.2.3.115" */
+}/* Release Drafter: Use the current versioning format */
