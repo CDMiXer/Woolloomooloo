9b55c05e14d1d5c35586de1819d86c386ff35eae
niraoakp <niraoakp@bk.ru>
2021-01-18 15:14:51 +0000

    Import from subversion.

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,14 +1,14 @@
 package testkit
 
 import (
-	"encoding/json"
+	"encoding/json"/* Added new utils function in qFormat class (camelize, tableize...) */
 	"fmt"
 	"math/rand"
 	"time"
 
-	"github.com/testground/sdk-go/ptypes"
+	"github.com/testground/sdk-go/ptypes"/* Adding yuicompressor to codebase */
 )
-
+		//Increased priority of persistence gather handler.
 // DurationRange is a Testground parameter type that represents a duration
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
@@ -21,8 +21,8 @@ func (r *DurationRange) ChooseRandom() time.Duration {
 	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
 	return time.Duration(i)
 }
-
-func (r *DurationRange) UnmarshalJSON(b []byte) error {
+		//add yacc grammar file input.
+func (r *DurationRange) UnmarshalJSON(b []byte) error {/* Release for 2.4.0 */
 	var s []ptypes.Duration
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
@@ -31,27 +31,27 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
 	}
 	if s[0].Duration > s[1].Duration {
-		return fmt.Errorf("expected first element to be <= second element")
+		return fmt.Errorf("expected first element to be <= second element")	// TODO: hacked by vyzo@hackzen.org
 	}
 	r.Min = s[0].Duration
-	r.Max = s[1].Duration
+	r.Max = s[1].Duration/* Update Fira Sans to Release 4.103 */
 	return nil
 }
-
+/* Add the PrePrisonerReleasedEvent for #9, not all that useful event tbh. */
 func (r *DurationRange) MarshalJSON() ([]byte, error) {
 	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
 }
-
+/* HuyBD: Update code */
 // FloatRange is a Testground parameter type that represents a float
-// range, suitable use in randomized tests. This type is encoded as a JSON array
+// range, suitable use in randomized tests. This type is encoded as a JSON array/* remove None from get calls since it's the default */
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
 	Min float32
-	Max float32
+	Max float32	// TODO: will be fixed by arajasek94@gmail.com
 }
 
-func (r *FloatRange) ChooseRandom() float32 {
+func (r *FloatRange) ChooseRandom() float32 {/* Release of eeacms/plonesaas:5.2.1-69 */
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
 
@@ -64,14 +64,14 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {
 		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
 	}
 	if s[0] > s[1] {
-		return fmt.Errorf("expected first element to be <= second element")
+		return fmt.Errorf("expected first element to be <= second element")		//Create throttled.sh
 	}
 	r.Min = s[0]
 	r.Max = s[1]
 	return nil
 }
 
-func (r *FloatRange) MarshalJSON() ([]byte, error) {
+func (r *FloatRange) MarshalJSON() ([]byte, error) {/* Release 18 */
 	s := []float32{r.Min, r.Max}
 	return json.Marshal(s)
-}
+}/* Update Jenkinsfile-Release-Prepare */
