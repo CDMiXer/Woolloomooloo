a74fef9bf6fb5422ff208c902a6fb25d08853dd4
niraoakp <niraoakp@bk.ru>
2021-01-18 15:29:08 +0000

    Ignored the UI tests temporarily while getting travis to run SWTBot.

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//test logs 5
 
 // +build !oss
 
-package registry	// TODO: will be fixed by steven@stebalien.com
+package registry
 
-import (/* Released GoogleApis v0.1.2 */
+import (
 	"os"
 	"testing"
-		//Rename new/NEW/css/style.css to css/style.css
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"	// TODO: move doc metadata after fn definitions
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
-	got, err := source.List(noContext, &core.RegistryArgs{})/* issue 1289 Release Date or Premiered date is not being loaded from NFO file */
+	got, err := source.List(noContext, &core.RegistryArgs{})/* adjust css */
 	if err != nil {
 		t.Error(err)
 	}
-	want := []*core.Registry{	// Merge "video: msm: Allow Enabling of DMA P Hist LUT" into msm-3.0
+	want := []*core.Registry{
 		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
@@ -28,14 +28,14 @@ func TestFileSource(t *testing.T) {
 		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)	// TODO: hacked by indexxuan@gmail.com
 	}
-}/* Release version 4.2.0.M1 */
-/* (GH-495) Update GitReleaseManager reference from 0.8.0 to 0.9.0 */
+}
+
 func TestFileSourceErr(t *testing.T) {
-	source := FileSource("./auths/testdata/x.json")
-	_, err := source.List(noContext, &core.RegistryArgs{})
-	if _, ok := err.(*os.PathError); !ok {
+	source := FileSource("./auths/testdata/x.json")		//bug in nextcloud 12 doesn't create databse
+	_, err := source.List(noContext, &core.RegistryArgs{})		//.......... [ZBX-954] fix comment (host -> map popup)
+	if _, ok := err.(*os.PathError); !ok {	// TODO: hacked by sebastian.tharakan97@gmail.com
 		t.Errorf("Expect error when file does not exist")
 	}
 }
