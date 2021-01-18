28ddee91e449cb6f269182d9ceae05ce86cd92b9
niraoakp <niraoakp@bk.ru>
2021-01-18 12:25:14 +0000

    Remove more YiM-level buffer stuff

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,6 +1,6 @@
--- name: create-table-latest	// TODO: hacked by timnugent@gmail.com
+-- name: create-table-latest
 
-CREATE TABLE IF NOT EXISTS latest (/* Re #26643 Release Notes */
+CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS latest (/* Re #26643 Release Notes */
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// codegangsta -> urfave  cli
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
 -- name: create-index-latest-repo
