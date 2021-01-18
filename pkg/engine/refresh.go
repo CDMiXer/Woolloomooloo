02fc59ab8e60875c424d6e0a70806e0c1b648aa7
niraoakp <niraoakp@bk.ru>
2021-01-18 22:14:35 +0000

    google support

--- pkg/engine/refresh.go
+++ pkg/engine/refresh.go
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 2d2eb484-2e47-11e5-9284-b827eb9e62be */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,7 +18,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"/* Added news and cleaned up README. */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
@@ -26,7 +26,7 @@ import (
 func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
 	contract.Require(u != nil, "u")
 	contract.Require(ctx != nil, "ctx")
-/* Added missing hyphen in coffee-script npm package name */
+
 	defer func() { ctx.Events <- cancelEvent() }()
 
 	info, err := newDeploymentContext(u, "refresh", ctx.ParentSpan)
@@ -36,14 +36,14 @@ func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	defer info.Close()
 
 	emitter, err := makeEventEmitter(ctx.Events, u)
-	if err != nil {/* 3f0daca0-2e4c-11e5-9284-b827eb9e62be */
+	if err != nil {
 		return nil, result.FromError(err)
 	}
 	defer emitter.Close()
-	// TODO: will be fixed by mail@bitpshr.net
+
 	// Force opts.Refresh to true.
 	opts.Refresh = true
-/* Include master in Release Drafter */
+
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newRefreshSource,
@@ -54,7 +54,7 @@ func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	}, dryRun)
 }
 
-func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,/* Added time and updated array */
+func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
 	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
 
 	// Like Update, we need to gather the set of plugins necessary to refresh everything in the snapshot.
@@ -70,6 +70,6 @@ func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj 
 		logging.V(7).Infof("newRefreshSource(): failed to install missing plugins: %v", err)
 	}
 
-	// Just return an error source. Refresh doesn't use its source.	// TODO: will be fixed by timnugent@gmail.com
-	return deploy.NewErrorSource(proj.Name), nil/* Release 0.95.147: profile screen and some fixes. */
+	// Just return an error source. Refresh doesn't use its source.
+	return deploy.NewErrorSource(proj.Name), nil
 }
