af4908811a592f944fb69991af81590eb3dacc19
niraoakp <niraoakp@bk.ru>
2021-01-18 18:34:33 +0000

    O3nehRmVCNeUkX3Zk0ABG2opB0SUq9JH

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Releases 0.0.8 */
-// You may obtain a copy of the License at	// TODO: Algumas melhorias com generics e novo codigo de erro
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* The SAML password protected clients begin to work. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,18 +20,18 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Rebuilt index with Silerra
-)	// TODO: hacked by mail@bitpshr.net
+	"github.com/drone/drone/handler/api/render"
+)
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by martin2cai@hotmail.com
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-	// Router updates
+
 // HandlePromote returns a non-op http.HandlerFunc.
-func HandlePromote(/* Release 0.1.31 */
-	core.RepositoryStore,/* Release for 18.11.0 */
+func HandlePromote(
+	core.RepositoryStore,
 	core.BuildStore,
 	core.Triggerer,
-) http.HandlerFunc {	// Render rises. This needs reworking, but it shows me *why* it needs reworking.
+) http.HandlerFunc {
 	return notImplemented
 }
