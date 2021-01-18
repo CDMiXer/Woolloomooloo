091a29e75d950d8673d04bab17d08486198295c8
niraoakp <niraoakp@bk.ru>
2021-01-18 21:33:40 +0000

    Slight update to package.json

--- handler/api/events/build_test.go
+++ handler/api/events/build_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release of eeacms/www-devel:18.3.6 */
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package events
