19c4881fa5e3148aacafce62e0c9e4e947402c84
niraoakp <niraoakp@bk.ru>
2021-01-18 00:24:07 +0000

    047f541e-2e45-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/list.go
+++ handler/api/repos/builds/list.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* SemBBS: new gender for the people! */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,18 +9,18 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 4.0.0-beta2 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
 
-import (	// TODO: hacked by jon@atack.com
+import (
 	"fmt"
-	"net/http"		//Added blog post
+	"net/http"
 	"strconv"
-		//Some text correction in cluster disaster recovery design
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: hacked by 13860583249@yeah.net
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -28,7 +28,7 @@ import (	// TODO: hacked by jon@atack.com
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
-func HandleList(/* Release script: added Dockerfile(s) */
+func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
@@ -46,37 +46,37 @@ func HandleList(/* Release script: added Dockerfile(s) */
 			limit = 25
 		}
 		switch offset {
-		case 0, 1:	// Send an array of email domains
+		case 0, 1:
 			offset = 0
 		default:
 			offset = (offset - 1) * limit
-		}/* f9dc5bfa-2e40-11e5-9284-b827eb9e62be */
+		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Release of eeacms/forests-frontend:2.0-beta.39 */
+			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).		//jenkins-promote-staging-trunk-libmemcached-1
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return	// TODO: hacked by sebastian.tharakan97@gmail.com
+			return
 		}
 
 		var results []*core.Build
 		if branch != "" {
-			ref := fmt.Sprintf("refs/heads/%s", branch)	// Merge branch 'master' into brian/password-show-clear
+			ref := fmt.Sprintf("refs/heads/%s", branch)
 			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)
 		} else {
 			results, err = builds.List(r.Context(), repo.ID, limit, offset)
-		}	// TODO: will be fixed by boringland@protonmail.ch
+		}
 
-		if err != nil {/* Released jujiboutils 2.0 */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot list builds")/* Delete univie-SDM_K-means.zip */
+				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
 		}
