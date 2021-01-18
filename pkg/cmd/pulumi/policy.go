9f5a48c0459d471ad565f49560a64dc7fffa3526
niraoakp <niraoakp@bk.ru>
2021-01-18 00:48:35 +0000

    Merge branch 'master' into fix-right-mouse-drag-large-view

--- pkg/cmd/pulumi/policy.go
+++ pkg/cmd/pulumi/policy.go
@@ -1,39 +1,39 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release ProcessPuzzleUI-0.8.0 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Add owners informations */
-//     http://www.apache.org/licenses/LICENSE-2.0
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+///* Release version [10.4.4] - prepare */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release version 1.1. */
+// limitations under the License.
 
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)
+)/* Release for v47.0.0. */
 
-func newPolicyCmd() *cobra.Command {		//Fix capitalization for endShape in docs
+func newPolicyCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "policy",/* 86936fc9-2d15-11e5-af21-0401358ea401 */
+		Use:   "policy",
 		Short: "Manage resource policies",
-		Args:  cmdutil.NoArgs,
+		Args:  cmdutil.NoArgs,/* Update DatabaseHandler.php */
 	}
 
-	cmd.AddCommand(newPolicyDisableCmd())
+	cmd.AddCommand(newPolicyDisableCmd())/* Rename ReceivingHTTPS to ReceivingHTTPS.md */
 	cmd.AddCommand(newPolicyEnableCmd())
 	cmd.AddCommand(newPolicyGroupCmd())
-	cmd.AddCommand(newPolicyLsCmd())/* Potential 1.6.4 Release Commit. */
+	cmd.AddCommand(newPolicyLsCmd())
 	cmd.AddCommand(newPolicyNewCmd())
 	cmd.AddCommand(newPolicyPublishCmd())
 	cmd.AddCommand(newPolicyRmCmd())
 	cmd.AddCommand(newPolicyValidateCmd())
 
-	return cmd
+	return cmd/* Update README.md (add reference to Releases) */
 }
