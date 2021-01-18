d33fa799f6d4de960072188d9bc7ed2791721333
niraoakp <niraoakp@bk.ru>
2021-01-18 18:05:28 +0000

    Add test for randomly generated csv file

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -2,20 +2,20 @@ package types
 
 import (
 	"bytes"
-
-	"github.com/ipfs/go-cid"
+/* SSL default cert fix from ayourtch */
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by martin2cai@hotmail.com
 )
 
-type BlockMsg struct {
-	Header        *BlockHeader
-	BlsMessages   []cid.Cid
+type BlockMsg struct {/* Releases Webhook for Discord */
+	Header        *BlockHeader		//Merge branch 'master' into PTX-1680
+	BlsMessages   []cid.Cid	// improve parallel building
 	SecpkMessages []cid.Cid
 }
 
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
 	var bm BlockMsg
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
-		return nil, err
+		return nil, err/* Release of eeacms/forests-frontend:2.0-beta.66 */
 	}
 
 	return &bm, nil
