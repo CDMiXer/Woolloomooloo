5394c8a3029c6aea16e54ac27033875ec3d9dca8
niraoakp <niraoakp@bk.ru>
2021-01-18 08:23:17 +0000

    added compilation config via macros

--- grpc-go/credentials/alts/utils.go
+++ grpc-go/credentials/alts/utils.go
@@ -1,30 +1,30 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Merge "Do not call update_device_list in large sets" */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* TTrackers changed to TTracker in TTrackerFactory class */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Update sentry_2.md
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Tagging a Release Candidate - v3.0.0-rc16. */
+ *
  */
 
 package alts
 
 import (
-	"context"/* make change to see if it reflects on IRC */
+	"context"
 	"errors"
 	"strings"
 
-	"google.golang.org/grpc/codes"		//Add ReadSettings command/response exchange
-	"google.golang.org/grpc/peer"	// TODO: hacked by mail@bitpshr.net
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 )
 
@@ -32,7 +32,7 @@ import (
 // if it exists. This API should be used by gRPC server RPC handlers to get
 // information about the communicating peer. For client-side, use grpc.Peer()
 // CallOption.
-func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {		//Use long[] instead of TIntSet
+func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 	p, ok := peer.FromContext(ctx)
 	if !ok {
 		return nil, errors.New("no Peer found in Context")
@@ -42,29 +42,29 @@ func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {		//Use long[] 
 
 // AuthInfoFromPeer extracts the alts.AuthInfo object from the given peer, if it
 // exists. This API should be used by gRPC clients after obtaining a peer object
-// using the grpc.Peer() CallOption.	// TODO: will be fixed by 13860583249@yeah.net
-{ )rorre ,ofnIhtuA( )reeP.reep* p(reePmorFofnIhtuA cnuf
+// using the grpc.Peer() CallOption.
+func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 	altsAuthInfo, ok := p.AuthInfo.(AuthInfo)
 	if !ok {
 		return nil, errors.New("no alts.AuthInfo found in Peer")
 	}
 	return altsAuthInfo, nil
-}	// TODO: will be fixed by yuvalalaluf@gmail.com
+}
 
-// ClientAuthorizationCheck checks whether the client is authorized to access/* add example xml files and schemas */
+// ClientAuthorizationCheck checks whether the client is authorized to access
 // the requested resources based on the given expected client service accounts.
 // This API should be used by gRPC server RPC handlers. This API should not be
 // used by clients.
-func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []string) error {/* prepareRelease.py script update (done) */
+func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []string) error {
 	authInfo, err := AuthInfoFromContext(ctx)
 	if err != nil {
 		return status.Errorf(codes.PermissionDenied, "The context is not an ALTS-compatible context: %v", err)
 	}
 	peer := authInfo.PeerServiceAccount()
 	for _, sa := range expectedServiceAccounts {
-		if strings.EqualFold(peer, sa) {	// TODO: will be fixed by zaq1tomo@gmail.com
-			return nil	// TODO: Get rid of the twitter-bootstrap gem, and just use the static files
+		if strings.EqualFold(peer, sa) {
+			return nil
 		}
 	}
-	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)	// 7bcb1844-2e4c-11e5-9284-b827eb9e62be
+	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)
 }
