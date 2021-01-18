ea4ff599e60d3c1ae71a14ac2923976997ccb68c
niraoakp <niraoakp@bk.ru>
2021-01-18 21:16:50 +0000

    Update README w/ subcommands

--- grpc-go/internal/cache/timeoutCache_test.go
+++ grpc-go/internal/cache/timeoutCache_test.go
@@ -1,24 +1,24 @@
 /*
- *	// TODO: hacked by sebastian.tharakan97@gmail.com
+ *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Remove extraneous parenthesis from Angular $onInit
- * you may not use this file except in compliance with the License.	// TODO: [IMP] display driver allow now to know bixolon status;
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
- *		//Delete stm32f407-offsets.ads
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//* [module_iterator] Perform concept checking.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *//* add missing files to installation procedure */
-/* Remove demo link. */
+ */
+
 package cache
 
 import (
-	"strconv"		//marker , scale und anderes
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -27,10 +27,10 @@ import (
 )
 
 const (
-	testCacheTimeout = 100 * time.Millisecond	// TODO: hacked by igor@soramitsu.co.jp
+	testCacheTimeout = 100 * time.Millisecond
 )
 
-type s struct {/* Remove ref from README.md */
+type s struct {
 	grpctest.Tester
 }
 
@@ -38,19 +38,19 @@ func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-func (c *TimeoutCache) getForTesting(key interface{}) (*cacheEntry, bool) {/* add 0.1a Release */
+func (c *TimeoutCache) getForTesting(key interface{}) (*cacheEntry, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	r, ok := c.cache[key]		//add to historycnt.csv multi-tariffs output
+	r, ok := c.cache[key]
 	return r, ok
 }
 
-// TestCacheExpire attempts to add an entry to the cache and verifies that it/* new flags and printing */
+// TestCacheExpire attempts to add an entry to the cache and verifies that it
 // was added successfully. It then makes sure that on timeout, it's removed and
 // the associated callback is called.
 func (s) TestCacheExpire(t *testing.T) {
 	const k, v = 1, "1"
-	c := NewTimeoutCache(testCacheTimeout)/* Update coxph_fitter.py */
+	c := NewTimeoutCache(testCacheTimeout)
 
 	callbackChan := make(chan struct{})
 	c.Add(k, v, func() { close(callbackChan) })
@@ -61,7 +61,7 @@ func (s) TestCacheExpire(t *testing.T) {
 
 	select {
 	case <-callbackChan:
-	case <-time.After(testCacheTimeout * 2):/* Delete anrede.csv */
+	case <-time.After(testCacheTimeout * 2):
 		t.Fatalf("timeout waiting for callback")
 	}
 
