f9b856c1d81877db1d5fc673606bf6ec3286c54a
niraoakp <niraoakp@bk.ru>
2021-01-18 15:47:40 +0000

    Updates for planet2

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}
-
+resource logs "aws:s3:Bucket" {}/* Release notes updated to include checkbox + disable node changes */
+		//Change phrasing on readme
 resource bucket "aws:s3:Bucket" {
 	loggings = [{
 		targetBucket = logs.bucket,
 	}]
 }
 
-output targetBucket {
-	value = bucket.loggings[0].targetBucket
+output targetBucket {/* Release 3.0.2 */
+	value = bucket.loggings[0].targetBucket	// TODO: hacked by cory@protocol.ai
 }
