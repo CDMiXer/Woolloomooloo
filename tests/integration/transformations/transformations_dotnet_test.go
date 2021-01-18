c4d254bd02311586c66515840bb35722d99fb2e4
niraoakp <niraoakp@bk.ru>
2021-01-18 17:43:30 +0000

    Delete lxde-minimal-post-install.txt

--- tests/integration/transformations/transformations_dotnet_test.go
+++ tests/integration/transformations/transformations_dotnet_test.go
@@ -1,64 +1,64 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build dotnet all
-
-package ints
-
-import (
+/* Release areca-5.3.4 */
+package ints/* Launch Upd */
+/* Release version */
+import (/* 1.x: Release 1.1.2 CHANGES.md update */
 	"path/filepath"
-	"testing"	// TODO: Implements Recurring::RecurringResponse
+	"testing"/* Modified to make filename hardcoded */
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: little progress.
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Released array constraint on payload */
 )
 
 func TestDotNetTransformations(t *testing.T) {
-	for _, dir := range Dirs {	// 40820512-2e52-11e5-9284-b827eb9e62be
+	for _, dir := range Dirs {/* 1.5.0 Release */
 		d := filepath.Join("dotnet", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release Notes updates */
+			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Merge "Slightly better translation friendly formatting" */
 				Dir:                    d,
 				Dependencies:           []string{"Pulumi"},
 				Quick:                  true,
 				ExtraRuntimeValidation: dotNetValidator(),
 			})
-		})	// Merge "Move router advertisement daemon restarts to privsep."
-	}
+		})
+	}	// licence change/update/correct (4/n)
 }
 
-// .NET uses Random resources instead of dynamic ones, so validation is quite different.
-func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-	resName := "random:index/randomString:RandomString"		//don't need this to be public
-	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {	// provide basic documentation
+// .NET uses Random resources instead of dynamic ones, so validation is quite different./* Release of eeacms/www-devel:20.8.7 */
+func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {		//Rwoverdijk assetmanager added to composer.
+	resName := "random:index/randomString:RandomString"
+	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 		foundRes1 := false
-		foundRes2Child := false/* Merge "wlan: Release 3.2.3.110c" */
-		foundRes3 := false/* 0.20.2: Maintenance Release (close #78) */
+		foundRes2Child := false	// TODO: Add newline character to stack allocation error
+		foundRes3 := false
 		foundRes4Child := false
-		foundRes5Child := false
+		foundRes5Child := false		//fixed io.run() for windows.
 		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
 				foundRes1 = true
-				assert.Equal(t, res.Type, tokens.Type(resName))		//file uploads are now chunked.
+				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child" and sets minUpper to 2
-			if res.URN.Name() == "res2-child" {		//add TeamManage html
-				foundRes2Child = true
+			// "child" and sets minUpper to 2/* Added 0.9.5 Release Notes */
+			if res.URN.Name() == "res2-child" {
+				foundRes2Child = true/* updated to check for null on specific dates */
 				assert.Equal(t, res.Type, tokens.Type(resName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))/* Release War file */
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))
 				minUpper := res.Inputs["minUpper"]
 				assert.NotNil(t, minUpper)
-				assert.Equal(t, 2.0, minUpper.(float64))/* Updated SNAPSHOT version */
+				assert.Equal(t, 2.0, minUpper.(float64))
 			}
 			// "res3" is impacted by a global stack transformation which sets
-			// overrideSpecial to "stackvalue"/* Reintroduce variable flag that stores pacing settings in local storage. */
+			// overrideSpecial to "stackvalue"
 			if res.URN.Name() == "res3" {
-				foundRes3 = true	// TODO: hacked by ligi@ligi.de
+				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				overrideSpecial := res.Inputs["overrideSpecial"]
 				assert.NotNil(t, overrideSpecial)
@@ -68,7 +68,7 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 			// to overrideSpecial "value1" and then "value2" and also a global stack
 			// transformation which appends "stackvalue" to overrideSpecial.  The end
 			// result should be "value1value2stackvalue".
-			if res.URN.Name() == "res4-child" {/* Update collegelife.html */
+			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
@@ -81,7 +81,7 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
-				length := res.Inputs["length"]		//Rename autoSaveIGOR.ipf to IGORpro/autoSaveIGOR.ipf
+				length := res.Inputs["length"]
 				assert.NotNil(t, length)
 				assert.Equal(t, 6.0, length.(float64))
 			}
