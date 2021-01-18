9c7d92fbf50ffa571173ef2ffc86486ee157ad20
niraoakp <niraoakp@bk.ru>
2021-01-18 16:47:58 +0000

    Added Travis CI and CodeClimate Badges in README

--- tests/integration/dynamic/python/__main__.py
+++ tests/integration/dynamic/python/__main__.py
@@ -1,21 +1,21 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import binascii	// Update TimeMenu.java
+import binascii
 import os
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-	// update Virtual Tripwire
+
 class RandomResourceProvider(ResourceProvider):
     def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
 
-class Random(Resource):/* Release 7.6.0 */
-    val: str	// TODO: removed EDGE_MIDDLE & BOX_MIDDLE axis styles, they were broken & useless
+class Random(Resource):
+    val: str
     def __init__(self, name, opts = None):
-        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)/* Create InstallSophos.sh */
+        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
-r = Random("foo")/* Release of eeacms/eprtr-frontend:0.4-beta.10 */
+r = Random("foo")
 
-export("random_id", r.id)		//Add metadata to TypeModule and TypeDeclaration
-export("random_val", r.val)/* 1d2758ba-2e48-11e5-9284-b827eb9e62be */
+export("random_id", r.id)
+export("random_val", r.val)
