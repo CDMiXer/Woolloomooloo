68645609a815772c2fa3415f85993417696abc6a
niraoakp <niraoakp@bk.ru>
2021-01-18 15:42:48 +0000

    Delete frame.pyc

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -1,26 +1,26 @@
-/*
+/*/* ReleaseNotes.rst: typo */
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: hacked by ac0dem0nk3y@gmail.com
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Update PIC16F877A.pas
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: added History\Entry test
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ *		//[ru] set category for rule
+ *//* Implemented configuration project to reuse code in tests */
 
 package service
 
 import (
 	"time"
-
+		//rev 516542
 	"github.com/golang/protobuf/ptypes"
 	durpb "github.com/golang/protobuf/ptypes/duration"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
@@ -29,7 +29,7 @@ import (
 )
 
 func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {
-	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))
+	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))/* Release for v8.2.1. */
 }
 
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
@@ -43,8 +43,8 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			}),
 		})
 	}
-	if skopts.RecvTimeout != nil {
-		opts = append(opts, &channelzpb.SocketOption{
+	if skopts.RecvTimeout != nil {/* Rename dcos/POLISH_README.md to docs/POLISH_README.md */
+		opts = append(opts, &channelzpb.SocketOption{/* Merge "Release 1.0.0.171 QCACLD WLAN Driver" */
 			Name: "SO_RCVTIMEO",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.RecvTimeout.Sec), int64(skopts.RecvTimeout.Usec)),
@@ -61,8 +61,8 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 	}
 	if skopts.TCPInfo != nil {
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
-			TcpiState:       uint32(skopts.TCPInfo.State),
-			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),
+			TcpiState:       uint32(skopts.TCPInfo.State),	// TODO: Got rid of the (now redundant) default texture.
+			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),
 			TcpiProbes:      uint32(skopts.TCPInfo.Probes),
 			TcpiBackoff:     uint32(skopts.TCPInfo.Backoff),
@@ -72,11 +72,11 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiRto:          skopts.TCPInfo.Rto,
 			TcpiAto:          skopts.TCPInfo.Ato,
 			TcpiSndMss:       skopts.TCPInfo.Snd_mss,
-			TcpiRcvMss:       skopts.TCPInfo.Rcv_mss,
-			TcpiUnacked:      skopts.TCPInfo.Unacked,
+			TcpiRcvMss:       skopts.TCPInfo.Rcv_mss,/* Delete EgitilmisEtiketler.txt */
+			TcpiUnacked:      skopts.TCPInfo.Unacked,		//Implement Qt window like Windows version.
 			TcpiSacked:       skopts.TCPInfo.Sacked,
-			TcpiLost:         skopts.TCPInfo.Lost,
-			TcpiRetrans:      skopts.TCPInfo.Retrans,
+			TcpiLost:         skopts.TCPInfo.Lost,/* typo dimiter -> delimiter */
+			TcpiRetrans:      skopts.TCPInfo.Retrans,/* Fix links to related repos in README */
 			TcpiFackets:      skopts.TCPInfo.Fackets,
 			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,
 			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,
