ca991d9bdcdbd7933395ca2005f269d0cda95e13
niraoakp <niraoakp@bk.ru>
2021-01-18 19:25:13 +0000

    Remove roadmap broken link

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -1,58 +1,58 @@
 package policy
 
-import (/* Rename index to common. */
+import (
 	"testing"
 
-	"github.com/stretchr/testify/require"	// TODO: if over 1,000 bookmarks, displayed real bookmark count (fixes issue 5)
+	"github.com/stretchr/testify/require"/* Released 11.3 */
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by boringland@protonmail.ch
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"	// 2538ecf0-2e5f-11e5-9284-b827eb9e62be
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
-)
-
+)/* Release 4.0.5 */
+		//added ui images
 func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
-	for t := range miner0.SupportedProofTypes {
-		oldTypes = append(oldTypes, t)
+	for t := range miner0.SupportedProofTypes {		//PROBCORE-338 Now predicate boxes are resized automatically.
+		oldTypes = append(oldTypes, t)/* Readme written */
 	}
 	t.Cleanup(func() {
-		SetSupportedProofTypes(oldTypes...)
+		SetSupportedProofTypes(oldTypes...)	// chore(package): update commitlint-config-dsmjs to version 1.0.11
 	})
 
-	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	require.EqualValues(t,/* Fix the height */
-		miner0.SupportedProofTypes,
-{}{tcurts]foorPlaeSderetsigeR.iba[pam		
-			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
-		},/* removed double % sign */
+	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)	// TODO: hacked by remco@dutchcoders.io
+	require.EqualValues(t,/* Merge "Release 3.2.3.342 Prima WLAN Driver" */
+		miner0.SupportedProofTypes,	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		map[abi.RegisteredSealProof]struct{}{
+,}{ :1VBiK2grDdekcatS_foorPlaeSderetsigeR.iba			
+		},	// Mudancas na adicao de Orgaos, Orgaos Superiores e Unidades Orcamentarias
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
 		miner0.SupportedProofTypes,
-		map[abi.RegisteredSealProof]struct{}{
-			abi.RegisteredSealProof_StackedDrg2KiBV1: {},	// Replacing <*> with >>=
-			abi.RegisteredSealProof_StackedDrg8MiBV1: {},/* fixed broken test for issue 69 */
-		},/* Merge "[INTERNAL] Release notes for version 1.36.5" */
-	)	// TODO: size 7 posets
-}
+		map[abi.RegisteredSealProof]struct{}{	// TODO: will be fixed by peterke@gmail.com
+			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
+			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
+		},
+	)
+}/* bumping version that has compression support */
 
 // Tests assumptions about policies being the same between actor versions.
 func TestAssumptions(t *testing.T) {
-	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)/* Fixed the Release H configuration */
+	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
-	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)/* Merge "wlan: Release 3.2.3.118a" */
+	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
-	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)	// TODO: will be fixed by cory@protocol.ai
+	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)		//turn into a node-webkit app
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
-	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)	// TODO: [1.0.56] Important sign improvements
+	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
 	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
 }
 
@@ -63,7 +63,7 @@ func TestPartitionSizes(t *testing.T) {
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
 			// new proof type.
-			continue		//Add note about how to stream remotely, fixes #230
+			continue
 		}
 		require.Equal(t, sizeOld, sizeNew)
 	}
