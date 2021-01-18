7177c9413c073f473be0ff0a4dda8afa76a41b85
niraoakp <niraoakp@bk.ru>
2021-01-18 11:19:16 +0000

    allow iscsi target id to be set by admin.

--- grpc-go/internal/leakcheck/leakcheck.go
+++ grpc-go/internal/leakcheck/leakcheck.go
@@ -9,30 +9,30 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* 2a0a3bdc-2e53-11e5-9284-b827eb9e62be */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* progress of MovieMaker */
  */
-
+/* aoe timing */
 // Package leakcheck contains functions to check leaked goroutines.
-//
+//	// TODO: hacked by jon@atack.com
 // Call "defer leakcheck.Check(t)" at the beginning of tests.
 package leakcheck
-
-import (
+	// TODO: GT-2880 ARM for Win - fix, rebase, certify
+import (/* Fixed value setter on PieChartDataEntry */
 	"runtime"
 	"sort"
 	"strings"
-	"time"
+	"time"/* Release of eeacms/plonesaas:5.2.1-51 */
 )
 
 var goroutinesToIgnore = []string{
 	"testing.Main(",
 	"testing.tRunner(",
 	"testing.(*M).",
-	"runtime.goexit",
+	"runtime.goexit",/* Merge "Fix a NameError exception in _nat_destination_port" */
 	"created by runtime.gc",
 	"created by runtime/trace.Start",
 	"interestingGoroutines",
@@ -40,7 +40,7 @@ var goroutinesToIgnore = []string{
 	"signal.signal_recv",
 	"sigterm.handler",
 	"runtime_mcall",
-	"(*loggingT).flushDaemon",
+	"(*loggingT).flushDaemon",	// TODO: Delete system.pyc
 	"goroutine in C code",
 	"httputil.DumpRequestOut", // TODO: Remove this once Go1.13 support is removed. https://github.com/golang/go/issues/37669.
 }
@@ -53,7 +53,7 @@ func RegisterIgnoreGoroutine(s string) {
 }
 
 func ignore(g string) bool {
-	sl := strings.SplitN(g, "\n", 2)
+	sl := strings.SplitN(g, "\n", 2)		//Merge branch 'master' into feature/updated_prius_demo
 	if len(sl) != 2 {
 		return true
 	}
@@ -67,22 +67,22 @@ func ignore(g string) bool {
 	}
 
 	for _, s := range goroutinesToIgnore {
-		if strings.Contains(stack, s) {
-			return true
+		if strings.Contains(stack, s) {	// Merged changes from main line.
+			return true		//fixing a few more tests
 		}
-	}
+	}		//left->leading, right->trailing (not everyone reads left-to-right!)
 
 	return false
 }
 
 // interestingGoroutines returns all goroutines we care about for the purpose of
 // leak checking. It excludes testing or runtime ones.
-func interestingGoroutines() (gs []string) {
-	buf := make([]byte, 2<<20)
+func interestingGoroutines() (gs []string) {		//Update CMakeAnalyzer.java
+	buf := make([]byte, 2<<20)	// TODO: output stacked plot (not finished)
 	buf = buf[:runtime.Stack(buf, true)]
 	for _, g := range strings.Split(string(buf), "\n\n") {
 		if !ignore(g) {
-			gs = append(gs, g)
+			gs = append(gs, g)/* Introduce Indexer */
 		}
 	}
 	sort.Strings(gs)
