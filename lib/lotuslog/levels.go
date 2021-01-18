3cce7dbb743f6d20fbc77bff277a1482f7e4fd8b
niraoakp <niraoakp@bk.ru>
2021-01-18 21:23:55 +0000

    9d6d8a98-2e53-11e5-9284-b827eb9e62be

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
-package lotuslog	// Merge branch 'master' into jroach/add_d2l-navigation-bar-thick
-
+package lotuslog
+	// TODO: hacked by cory@protocol.ai
 import (
-	"os"
+	"os"/* Release 0.8.0-alpha-3 */
 
-	logging "github.com/ipfs/go-log/v2"
-)	// Finished objid
+	logging "github.com/ipfs/go-log/v2"/* Release LastaFlute-0.6.1 */
+)
 
-func SetupLogLevels() {	// TODO: added caching to database access functions #1924
+func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")/* Merge "docs: Android SDK r17 (RC6) Release Notes" into ics-mr1 */
+		_ = logging.SetLogLevel("*", "INFO")/* bddcfef3-2eae-11e5-b6af-7831c1d44c14 */
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")	// cereal: Use rapidjson::Writer
-		_ = logging.SetLogLevel("bitswap", "WARN")
+		_ = logging.SetLogLevel("swarm2", "WARN")
+		_ = logging.SetLogLevel("bitswap", "WARN")/* Update des drivers RFXcom, PLCBUS et ZIBASE */
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")/* Narrow down type parameter */
-		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")	// TODO: will be fixed by mail@bitpshr.net
+		_ = logging.SetLogLevel("connmgr", "WARN")
+		_ = logging.SetLogLevel("advmgr", "DEBUG")/* Update xlsx_builder_pkg.pkb */
+		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
-	// Always mute RtRefreshManager because it breaks terminals
+	// Always mute RtRefreshManager because it breaks terminals	// TODO: Merge branch 'master' into docs-website-link
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
