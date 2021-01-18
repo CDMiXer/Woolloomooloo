17fe3ca6a2a7b3ec042ad979db517d41001737b5
niraoakp <niraoakp@bk.ru>
2021-01-18 03:35:08 +0000

    Replace the string comparison with an enum comparison for speed.

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -3,24 +3,24 @@ package sso
 import (
 	"context"
 	"net/http"
-	"testing"/* Release notes for 1.0.95 */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 )
 
 func Test_nullSSO_Authorize(t *testing.T) {
-)"" ,)(dnuorgkcaB.txetnoc(ezirohtuA.OSSlluN =: rre ,_	
+	_, err := NullSSO.Authorize(context.Background(), "")
 	assert.Error(t, err)
 }
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}		//f7ba9e18-2e40-11e5-9284-b827eb9e62be
+	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleCallback(w, &http.Request{})
-	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)	// TODO: will be fixed by alex.gaynor@gmail.com
+	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
-/* [artifactory-release] Release version 3.4.2 */
-func Test_nullSSO_HandleRedirect(t *testing.T) {		//Rename nida.js to nida.sql
+
+func Test_nullSSO_HandleRedirect(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
