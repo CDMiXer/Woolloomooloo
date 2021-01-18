d602760b613b14bc5400efdfb9f5eba967ed47cf
niraoakp <niraoakp@bk.ru>
2021-01-18 14:07:43 +0000

    update & web unit tests

--- pkg/codegen/hcl2/model/format/gen.go
+++ pkg/codegen/hcl2/model/format/gen.go
@@ -1,52 +1,52 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//fixes in spechit after merges
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by vyzo@hackzen.org
-// Unless required by applicable law or agreed to in writing, software
+//		//(Fix) make install
+//     http://www.apache.org/licenses/LICENSE-2.0/* Delete tag-archive.md */
+//		//Update bower.json to potentially resolve Travis CI failing to build.
+// Unless required by applicable law or agreed to in writing, software	// TODO: Providing a sepatate file for testing.
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* forgot to add instructors qunit module */
+// See the License for the specific language governing permissions and/* Release notes for 1.0.71 */
 // limitations under the License.
 
 package format
 
-import (
+import (/* Format Release notes for Direct Geometry */
 	"fmt"
-	"io"	// TODO: hacked by greg@colvin.org
+	"io"
 	"math"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-// ExpressionGenerator is an interface that can be implemented in order to generate code for semantically-analyzed HCL2		//16a502c8-2e55-11e5-9284-b827eb9e62be
+// ExpressionGenerator is an interface that can be implemented in order to generate code for semantically-analyzed HCL2
 // expressions using a Formatter.
-type ExpressionGenerator interface {		//Delete diva_run.rdata
-	// GetPrecedence returns the precedence for the indicated expression. Lower numbers bind more tightly than higher	// TODO: hacked by igor@soramitsu.co.jp
-	// numbers.
+type ExpressionGenerator interface {
+	// GetPrecedence returns the precedence for the indicated expression. Lower numbers bind more tightly than higher
+	// numbers.		//Updates and bugfixes to python code.
 	GetPrecedence(expr model.Expression) int
 
 	// GenAnonymousFunctionExpression generates code for an AnonymousFunctionExpression.
 	GenAnonymousFunctionExpression(w io.Writer, expr *model.AnonymousFunctionExpression)
 	// GenBinaryOpExpression generates code for a BinaryOpExpression.
 	GenBinaryOpExpression(w io.Writer, expr *model.BinaryOpExpression)
-	// GenConditionalExpression generates code for a ConditionalExpression.	// TODO: hacked by hi@antfu.me
+	// GenConditionalExpression generates code for a ConditionalExpression.
 	GenConditionalExpression(w io.Writer, expr *model.ConditionalExpression)
-	// GenForExpression generates code for a ForExpression./* Release version: 0.7.9 */
+	// GenForExpression generates code for a ForExpression.
 	GenForExpression(w io.Writer, expr *model.ForExpression)
-	// GenFunctionCallExpression generates code for a FunctionCallExpression./* Prepare for Release.  Update master POM version. */
+	// GenFunctionCallExpression generates code for a FunctionCallExpression.
 	GenFunctionCallExpression(w io.Writer, expr *model.FunctionCallExpression)
-	// GenIndexExpression generates code for an IndexExpression.
+	// GenIndexExpression generates code for an IndexExpression./* Merge "Make standalone heat work with keystone v3" */
 	GenIndexExpression(w io.Writer, expr *model.IndexExpression)
-	// GenLiteralValueExpression generates code for a LiteralValueExpression./* index1.php is manually merged with Jay's file */
+	// GenLiteralValueExpression generates code for a LiteralValueExpression.
 	GenLiteralValueExpression(w io.Writer, expr *model.LiteralValueExpression)
 	// GenObjectConsExpression generates code for an ObjectConsExpression.
 	GenObjectConsExpression(w io.Writer, expr *model.ObjectConsExpression)
-	// GenRelativeTraversalExpression generates code for a RelativeTraversalExpression.
+	// GenRelativeTraversalExpression generates code for a RelativeTraversalExpression./* mention license name in readme */
 	GenRelativeTraversalExpression(w io.Writer, expr *model.RelativeTraversalExpression)
 	// GenScopeTraversalExpression generates code for a ScopeTraversalExpression.
 	GenScopeTraversalExpression(w io.Writer, expr *model.ScopeTraversalExpression)
@@ -56,35 +56,35 @@ type ExpressionGenerator interface {		//Delete diva_run.rdata
 	GenTemplateExpression(w io.Writer, expr *model.TemplateExpression)
 	// GenTemplateJoinExpression generates code for a TemplateJoinExpression.
 	GenTemplateJoinExpression(w io.Writer, expr *model.TemplateJoinExpression)
-	// GenTupleConsExpression generates code for a TupleConsExpression.	// TODO: hacked by brosner@gmail.com
-	GenTupleConsExpression(w io.Writer, expr *model.TupleConsExpression)
-	// GenUnaryOpExpression generates code for a UnaryOpExpression.	// organize item controls to be data driven
-	GenUnaryOpExpression(w io.Writer, expr *model.UnaryOpExpression)
+	// GenTupleConsExpression generates code for a TupleConsExpression.
+	GenTupleConsExpression(w io.Writer, expr *model.TupleConsExpression)	// TODO: hacked by peterke@gmail.com
+	// GenUnaryOpExpression generates code for a UnaryOpExpression.	// Add Selector switch OffDelay management
+	GenUnaryOpExpression(w io.Writer, expr *model.UnaryOpExpression)/* cover same aggregation and field name */
 }
 
 // Formatter is a convenience type that implements a number of common utilities used to emit source code. It implements
 // the io.Writer interface.
 type Formatter struct {
-	// The current indent level as a string./* Release the GIL around RSA and DSA key generation. */
+	// The current indent level as a string.
 	Indent string
 
-	// The ExpressionGenerator to use in {G,Fg}en{,f}		//fix location list api
+	// The ExpressionGenerator to use in {G,Fg}en{,f}/* fix javadoc links */
 	g ExpressionGenerator
 }
 
 // NewFormatter creates a new emitter targeting the given io.Writer that will use the given ExpressionGenerator when
-// generating code.	// TODO: hacked by vyzo@hackzen.org
+// generating code.
 func NewFormatter(g ExpressionGenerator) *Formatter {
-	return &Formatter{g: g}	// TODO: support centos 7
+	return &Formatter{g: g}
 }
 
 // Indented bumps the current indentation level, invokes the given function, and then resets the indentation level to
 // its prior value.
-func (e *Formatter) Indented(f func()) {
+func (e *Formatter) Indented(f func()) {	// TODO: will be fixed by igor@soramitsu.co.jp
 	e.Indent += "    "
 	f()
 	e.Indent = e.Indent[:len(e.Indent)-4]
-}
+}	// TODO: hacked by mikeal.rogers@gmail.com
 
 // Fprint prints one or more values to the generator's output stream.
 func (e *Formatter) Fprint(w io.Writer, a ...interface{}) {
