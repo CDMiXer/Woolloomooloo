1380af0e0ea47cc4b5261fff4de22dbc84985fcf
niraoakp <niraoakp@bk.ru>
2021-01-19 01:11:55 +0000

    Update Constants.java

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Fixed test application config, beautified
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//Update cola_data_as_python_dict.md
 
 package rpc
