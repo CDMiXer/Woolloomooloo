0493733dece698ad65bb21ac902533bf2b78585a
niraoakp <niraoakp@bk.ru>
2021-01-19 11:10:31 +0000

    upgrade support annotations to 23.3.0

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: f8128db6-2e50-11e5-9284-b827eb9e62be
-// you may not use this file except in compliance with the License./* Update examples with new way of working */
+// Licensed under the Apache License, Version 2.0 (the "License");	// Merge "Fix a bug in ControllerActivityCounter" into nyc-dev
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Merge branch 'master' into 179_normalize_attribute_values
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Released version 0.8.32 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Fix Releases link */
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by zhen6939@gmail.com
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Fixed read function
 package logs
 
 import "github.com/drone/drone/store/shared/db"
-
+		//MBug#698132: Fix wrong buffer calculation in send_change_user_packet()
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
 	return scanner.Scan(
-		&dst.ID,
+		&dst.ID,/* Release v1.21 */
 		&dst.Data,
 	)
-}
+}/* Fix potential notice in wp_handle_sideload(). */
