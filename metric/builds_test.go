f04fe5b7c96b5d592cc28cb64fa68ee5fbee45ee
niraoakp <niraoakp@bk.ru>
2021-01-19 08:30:13 +0000

    Using CSS based icons in apps.

--- metric/builds_test.go
+++ metric/builds_test.go
@@ -1,70 +1,70 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update DockerfileRelease */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Release v0.1.0. */
 // that can be found in the LICENSE file.
 
-// +build !oss	// [IMP] revision de version dia anterior
+// +build !oss
 
 package metric
-
-import (	// TODO: will be fixed by mikeal.rogers@gmail.com
+		//Update to use basic auth
+import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Finalising R2 PETA Release */
-/* Update mmf.rb */
-	"github.com/golang/mock/gomock"
-	"github.com/prometheus/client_golang/prometheus"/* Updating Release Workflow */
-)
-/* Switch to newest closure compiler snapshot */
-func TestBuildCount(t *testing.T) {
-	controller := gomock.NewController(t)
+	"github.com/drone/drone/mock"
 
-	// restore the default prometheus registerer/* Document 'module' key */
+	"github.com/golang/mock/gomock"/* long addresses support */
+	"github.com/prometheus/client_golang/prometheus"
+)		//Nuevo Campo al User (Name)
+
+func TestBuildCount(t *testing.T) {
+	controller := gomock.NewController(t)	// TODO: will be fixed by 13860583249@yeah.net
+
+	// restore the default prometheus registerer	// TODO: hacked by mikeal.rogers@gmail.com
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot/* Release 0.5.1. Update to PQM brink. */
+		prometheus.DefaultRegisterer = snapshot	// Добавил описание строкам кода
 		controller.Finish()
-	}()/* Merge branch '5.0' into ch-5.0-1399025032544 */
-
+	}()
+		//changed separator
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
 	count := int64(5)
-		//CID display added here and there (in frames, when nick missing)
+
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Count(gomock.Any()).Return(count, nil)
-	BuildCount(builds)		//[ci fw-only Java/officefloor]
-		//Require xenial-arm64 for passing
-	metrics, err := registry.Gather()
-	if err != nil {/* wiki link changes */
-		t.Error(err)
-		return/* Release version 1.3 */
-	}
+	BuildCount(builds)
+
+	metrics, err := registry.Gather()/* Tweaked the UI header and login screens based on feedback from UX. */
+	if err != nil {	// Link to settings.json
+		t.Error(err)/* Release: 6.0.3 changelog */
+		return
+	}	// TODO: RICHGAUGE for item-based durations
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
 	}
-	metric := metrics[0]/* Release version 0.0.37 */
+	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_build_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
-	}
+	}/* fix db issue */
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
 
 func TestBuildPendingCount(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//add a reset command
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
-	defer func() {
+	defer func() {		//Latest model changes and merged trunk.
 		prometheus.DefaultRegisterer = snapshot
-		controller.Finish()
+		controller.Finish()		//Merge "Synology: Fix driver to be compatible with python3"
 	}()
 
 	// creates a blank registry
