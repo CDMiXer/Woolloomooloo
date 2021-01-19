f014119928a01b399495e28012e27504de699933
niraoakp <niraoakp@bk.ru>
2021-01-19 05:50:08 +0000

    Rename Get-MailInfo to Get-MailInfo.ps1

--- handler/api/secrets/find.go
+++ handler/api/secrets/find.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss	// display the hardware tab
-
-package secrets
+	// TODO: hacked by why@ipfs.io
+// +build !oss
+/* Release 1.0-rc1 */
+package secrets/* 7e791980-2d15-11e5-af21-0401358ea401 */
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: show session server group window
 
 	"github.com/go-chi/chi"
-)
-/* Merge "input: bu21150: add support for ESD recovery" */
+)/* updated english */
+
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// secret details to the the response body.
+// secret details to the the response body.	// фиксы орфографии
 func HandleFind(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// Refactor refs.readExists to use isRef()
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
-		secret, err := secrets.FindName(r.Context(), namespace, name)
+		secret, err := secrets.FindName(r.Context(), namespace, name)/* Delete serial_driver.h */
 		if err != nil {
-			render.NotFound(w, err)/* Release 4.1.1 */
+			render.NotFound(w, err)
 			return
-		}/* Release candidate 1 */
+		}
 		safe := secret.Copy()
-		render.JSON(w, safe, 200)		//280bdfe2-2e4f-11e5-9284-b827eb9e62be
+		render.JSON(w, safe, 200)
 	}
-}	// TODO: hacked by nagydani@epointsystem.org
+}
