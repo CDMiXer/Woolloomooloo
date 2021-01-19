5077a729763855ac6a0cc40453a1b05bc7f1aac9
niraoakp <niraoakp@bk.ru>
2021-01-19 03:54:03 +0000

    Delete ViniciusBianchi

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -1,16 +1,16 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* [artifactory-release] Release version 0.7.2.RELEASE */
+// license that can be found in the LICENSE file.
 
 // +build go1.8
 
-package websocket	// TODO: will be fixed by alex.gaynor@gmail.com
+package websocket
 
 import "crypto/tls"
 
-func cloneTLSConfig(cfg *tls.Config) *tls.Config {	// TODO: hacked by souzau@yandex.com
+func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
-	}		//More explicit error message when an experiment is not correctly named
+	}
 	return cfg.Clone()
 }
