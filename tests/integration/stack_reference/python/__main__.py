b57cf7d79a7f151831c1461037c3fb3d2404eb86
niraoakp <niraoakp@bk.ru>
2021-01-19 15:05:54 +0000

    300cbc8f-2d3f-11e5-a749-c82a142b6f9b

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -3,14 +3,14 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#/* Fixes a couple db fields. */
-#     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Update CI systems to validate and publish section openstack-training" */
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and/* Update the Human times constraints */
+# See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
 
-pulumi.export('val', ["a", "b"])		//Late night, early morning
+pulumi.export('val', ["a", "b"])/* Allow PHP 5.3 to fail --  unable to set Precise on Travis. */
