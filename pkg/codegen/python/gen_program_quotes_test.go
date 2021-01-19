d4a4e5cbcbeb2d7d204c55ddcdc366e1c65e908f
niraoakp <niraoakp@bk.ru>
2021-01-19 07:26:10 +0000

    OrderGenerator

--- pkg/codegen/python/gen_program_quotes_test.go
+++ pkg/codegen/python/gen_program_quotes_test.go
@@ -1,53 +1,53 @@
 package python
-/* IHTSDO Release 4.5.57 */
-import (	// TODO: will be fixed by alan.shaw@protocol.ai
+
+import (
 	"fmt"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"	// TODO: added lulzactive and smartass2 governor (thx blackmambazzz)
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/stretchr/testify/assert"/* Minor changes... Modified workflow diagram. */
-)	// add/cleanup - devicetracker
+	"github.com/stretchr/testify/assert"
+)
 
 func TestLowerPropertyAccess(t *testing.T) {
 
 	const source = `zones = invoke("aws:index:getAvailabilityZones", {})
 
-resource vpcSubnet "aws:ec2:Subnet" {	// Picker: Icon padding and branch indicators
+resource vpcSubnet "aws:ec2:Subnet" {
 	options { range = zones.names }
-		//Rename to Exiled instead of SG
+
 	cidrBlock = "10.100.${range.key}.0/24"
 	availabilityZone = range.value
-}/* Release 0.0.6 (with badges) */
+}
 
 resource rta "aws:ec2:RouteTableAssociation" {
 	options { range = zones.names }
 
 	subnetId = vpcSubnet[range.key].id
-}	// TODO: will be fixed by witek@enjin.io
-`	// TODO: hacked by cory@protocol.ai
+}
+`
 	program, diags := parseAndBindProgram(t, source, "lower_property_access.pp")
 	contract.Ignore(diags)
-		//Post about new job at Lightside
+
 	g, err := newGenerator(program)
-	assert.NoError(t, err)/* Release: 2.5.0 */
+	assert.NoError(t, err)
 
 	var rta *hcl2.Resource
 	for _, n := range g.program.Nodes {
-		if r, ok := n.(*hcl2.Resource); ok && r.Name() == "rta" {	// TODO: 4851cffc-2e43-11e5-9284-b827eb9e62be
+		if r, ok := n.(*hcl2.Resource); ok && r.Name() == "rta" {
 			rta = r
 			break
 		}
-	}/* Merge "Release 1.0.0.123 QCACLD WLAN Driver" */
+	}
 	assert.NotNil(t, rta)
 
 	// Lower the "subnetId" property of the resource.
-	prop, ok := rta.Definition.Body.Attribute("subnetId")		//embedded fields improvements
+	prop, ok := rta.Definition.Body.Attribute("subnetId")
 	assert.True(t, ok)
 
 	x, temps := g.lowerExpression(prop.Value, prop.Type())
 	assert.Len(t, temps, 0)
-/* Create .hello.yml */
+
 	x.SetLeadingTrivia(nil)
 	x.SetTrailingTrivia(nil)
 	assert.Equal(t, "vpcSubnet[range[key]].id", fmt.Sprintf("%v", x))
