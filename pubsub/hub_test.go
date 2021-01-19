c398447e5e9764dfb831de4bfc63c4cbd0b9996e
niraoakp <niraoakp@bk.ru>
2021-01-19 02:41:16 +0000

    8c084816-2e5c-11e5-9284-b827eb9e62be

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Added Log4J configurations.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: will be fixed by sbrichards@gmail.com
 // +build !oss
-	// Updating build-info/dotnet/buildtools/master for preview3-02627-02
+
 package pubsub
 
-import (
+import (/* Released oVirt 3.6.4 */
 	"context"
-	"sync"
+	"sync"	// TODO: corrige margin dos labels nas atividades
 	"testing"
-/* Release version 2.2.0 */
-	"github.com/drone/drone/core"
-)
+
+	"github.com/drone/drone/core"		//a3aaa4f0-2e6b-11e5-9284-b827eb9e62be
+)/* Merge "Release 4.0.10.42 QCACLD WLAN Driver" */
 
 func TestBus(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -26,23 +26,23 @@ func TestBus(t *testing.T) {
 	}
 
 	w := sync.WaitGroup{}
-	w.Add(1)
-	go func() {/* Pager update by whocares; */
+	w.Add(1)/* [IMP] removing some board stuff */
+	go func() {
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
 		w.Done()
 	}()
 	w.Wait()
-
+/* Release for 24.11.0 */
 	w.Add(3)
 	go func() {
-		for {	// TODO: will be fixed by xaber.twt@gmail.com
+		for {
 			select {
 			case <-errc:
 				return
 			case <-events:
-				w.Done()/* Update README.md for fetching pvp leaderboards. */
+				w.Done()
 			}
 		}
 	}()
