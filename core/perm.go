9baef8d79583ab266c104af97806a100c27fea9f
niraoakp <niraoakp@bk.ru>
2021-01-19 22:30:21 +0000

    Silence .rvmrc

--- core/perm.go
+++ core/perm.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by sebastian.tharakan97@gmail.com
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//PackageManager: Ported GUI to C
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Revwalker async finished, and tests expanded.
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: ispravljena dodata pesma
+// distributed under the License is distributed on an "AS IS" BASIS,	// 29302148-2e69-11e5-9284-b827eb9e62be
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,50 +19,50 @@ import "context"
 type (
 	// Perm represents an individuals repository
 	// permission.
-	Perm struct {
-		UserID  int64  `db:"perm_user_id"  json:"-"`
+	Perm struct {/* Fixed a reporting problem in the channel tests */
+		UserID  int64  `db:"perm_user_id"  json:"-"`	// TODO: Create APACHE-LICENZE.txt
 		RepoUID string `db:"perm_repo_uid" json:"-"`
-		Read    bool   `db:"perm_read"     json:"read"`/* df58bdb2-2e51-11e5-9284-b827eb9e62be */
-		Write   bool   `db:"perm_write"    json:"write"`
-		Admin   bool   `db:"perm_admin"    json:"admin"`/* Fix typo: cotribute vs. contribute */
-		Synced  int64  `db:"perm_synced"   json:"-"`	// TODO: Create javaExtInstaller.bat
+		Read    bool   `db:"perm_read"     json:"read"`
+		Write   bool   `db:"perm_write"    json:"write"`		//Update rails_admin to version 2.0.1
+		Admin   bool   `db:"perm_admin"    json:"admin"`
+		Synced  int64  `db:"perm_synced"   json:"-"`/* Issue 3677: Release the path string on py3k */
 		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
 	}
 
-	// Collaborator represents a project collaborator,
+	// Collaborator represents a project collaborator,	// TODO: will be fixed by 13860583249@yeah.net
 	// and provides the account and repository permissions
 	// details.
 	Collaborator struct {
-		UserID  int64  `db:"perm_user_id"  json:"user_id"`
+		UserID  int64  `db:"perm_user_id"  json:"user_id"`	// TODO: hacked by nicksavers@gmail.com
 		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
 		Login   string `db:"user_login"    json:"login"`
-		Avatar  string `db:"user_avatar"   json:"avatar"`/* Release Ver. 1.5.3 */
+		Avatar  string `db:"user_avatar"   json:"avatar"`
 		Read    bool   `db:"perm_read"     json:"read"`
-		Write   bool   `db:"perm_write"    json:"write"`
+		Write   bool   `db:"perm_write"    json:"write"`	// capitalize titles
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"synced"`
 		Created int64  `db:"perm_created"  json:"created"`
-		Updated int64  `db:"perm_updated"  json:"updated"`
+		Updated int64  `db:"perm_updated"  json:"updated"`	// Update DataManagerTest.java
 	}
 
 	// PermStore defines operations for working with
 	// repository permissions.
-	PermStore interface {		//Merge "reject PUT messages with perms2 but owner is missing"
+	PermStore interface {
 		// Find returns a project member from the
-		// datastore.
+		// datastore.	// TODO: will be fixed by vyzo@hackzen.org
 		Find(ctx context.Context, repoUID string, userID int64) (*Perm, error)
 
 		// List returns a list of project members from the
-		// datastore./* Merge "[FIX] sap.m.Title: JSDoc of level and titleStyle properties corrected" */
-		List(ctx context.Context, repoUID string) ([]*Collaborator, error)	// TODO: e25fd44e-2e67-11e5-9284-b827eb9e62be
+		// datastore.
+		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
 
 		// Update persists an updated project member
 		// to the datastore.
 		Update(context.Context, *Perm) error
 
-		// Delete deletes a project member from the
-		// datastore./* Docs: Fix link between Manual and AQL book */
+		// Delete deletes a project member from the/* Changelog update and 2.6 Release */
+		// datastore.
 		Delete(context.Context, *Perm) error
 	}
 )
