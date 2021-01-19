207cee074802e4b34548eb1a72dcc8ce6ef24fe5
niraoakp <niraoakp@bk.ru>
2021-01-19 03:00:34 +0000

    Changed the grammatical mistakes

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";	// texto reservas
+		//Storing last upload position by default for batch and sync dialogs
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);/* Release for 18.19.0 */
     }
-}/* Renaming to coincide with updated tagging system. */
+}
 
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
-    aliases: [{ name: "res1" }],	// TODO: hacked by antao2002@gmail.com
-});
+    aliases: [{ name: "res1" }],		//Updates for planet2
+});/* try git commit */
