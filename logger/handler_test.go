a47811a686a9ae4af4b120ede4cd2d0904663829
niraoakp <niraoakp@bk.ru>
2021-01-19 17:43:17 +0000

    KACV-TOM MUIR-4/9/18-GATED

--- logger/handler_test.go
+++ logger/handler_test.go
@@ -2,16 +2,16 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO:  - file system monitor : change trace json format
+// +build !oss
 
 package logger
 
 import "testing"
-/* Update approach metadata */
+
 func TestMiddleware(t *testing.T) {
 	t.Skip()
 }
-	// transactions, centrality
+
 func TestMiddleware_GenerateRequestID(t *testing.T) {
 	t.Skip()
-}		//fixed KpiEnumerateProcessHandles
+}
