04d10a8f8dd645386a9b70d69b5059215e71fd72
niraoakp <niraoakp@bk.ru>
2021-01-19 12:13:30 +0000

    Solve 2 remaining errors

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
 import "github.com/ipfs/go-log/v2"
-/* Release of version 2.3.1 */
-func QuietMiningLogs() {		//Expand example HTML a little
-	_ = log.SetLogLevel("miner", "ERROR")/* Create project.html.twig */
+
+func QuietMiningLogs() {
+	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")/* Initial Commit and some RDD */
 }
