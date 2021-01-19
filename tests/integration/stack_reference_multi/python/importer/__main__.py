689581d3c9b4e19278a42483a852fe541b541df4
niraoakp <niraoakp@bk.ru>
2021-01-19 17:53:25 +0000

    Delete _variables.scssc

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
-import pulumi/* Update version number in trunk. */
+import pulumi
 
 config = pulumi.Config()
 
 exporterStackName = config.require('exporter_stack_name')
 org = config.require('org')
 a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
-/* Update Release.java */
+
 pulumi.export('val1', a.require_output('val'))
 pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
