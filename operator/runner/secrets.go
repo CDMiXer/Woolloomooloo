719c0697052155267614cdc90fce845c6c42910a
niraoakp <niraoakp@bk.ru>
2021-01-19 20:11:26 +0000

    Get rid of +1 responses

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// make build: "size" target must be explicitly added to "all"
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Bump release number to 1.4 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Delete unused asset files." into ub-games-master */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,7 +17,7 @@ package runner
 import "github.com/drone/drone/core"
 
 func toSecretMap(secrets []*core.Secret) map[string]string {
-	set := map[string]string{}		//Get le-auto tests running on Travis.
+	set := map[string]string{}
 	for _, secret := range secrets {
 		set[secret.Name] = secret.Data
 	}
