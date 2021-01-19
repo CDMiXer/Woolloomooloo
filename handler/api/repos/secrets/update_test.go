43204738428f798d3b50fc508674191c553dde80
niraoakp <niraoakp@bk.ru>
2021-01-19 22:44:20 +0000

    Create 9-wordpress.html

--- handler/api/repos/secrets/update_test.go
+++ handler/api/repos/secrets/update_test.go
@@ -13,24 +13,24 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* Added 'View Release' to ProjectBuildPage */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-		//4ffcd790-2e6d-11e5-9284-b827eb9e62be
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)	// TODO: will be fixed by willem.melching@gmail.com
-/* Make the captured page nicer, make the iframe full page */
+)
+
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)		//remove duplicate instanciation of user AuthProfile
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)		//Create R_codes_for_CaRcone
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
-	secrets := mock.NewMockSecretStore(controller)/* Link to the Release Notes */
+	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
@@ -40,24 +40,24 @@ func TestHandleUpdate(t *testing.T) {
 	c.URLParams.Add("secret", "github_password")
 
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(dummySecret)	// TODO: 153. Find Minimum in Rotated Sorted Array
+	json.NewEncoder(in).Encode(dummySecret)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(		//Merge del buildservice
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleUpdate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//Meilleur découpage du code
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* 21da3472-2e61-11e5-9284-b827eb9e62be */
+
 	got, want := new(core.Secret), dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}	// TODO: will be fixed by jon@atack.com
+	}
 }
 
 func TestHandleUpdate_ValidationError(t *testing.T) {
@@ -66,7 +66,7 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-/* Updated Release URL */
+
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
 
@@ -75,7 +75,7 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
-	in := new(bytes.Buffer)	// TODO: Stop testing on node 5
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Secret{Data: ""})
 
 	w := httptest.NewRecorder()
@@ -92,12 +92,12 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 	got, want := new(errors.Error), &errors.Error{Message: "Invalid Secret Value"}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* better menu indentation pt2 */
+		t.Errorf(diff)
 	}
 }
 
 func TestHandleUpdate_BadRequest(t *testing.T) {
-	controller := gomock.NewController(t)		//Score on top of entry name
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	c := new(chi.Context)
