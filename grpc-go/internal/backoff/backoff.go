45ba8938f877e7722b7fec8ce3da462e1967bc13
niraoakp <niraoakp@bk.ru>
2021-01-19 14:45:06 +0000

    Fixing some array key exists warnings in tmpPks handling in list model.

--- grpc-go/internal/backoff/backoff.go
+++ grpc-go/internal/backoff/backoff.go
@@ -1,57 +1,57 @@
 /*
- */* Switch poise back to master for the self parent fix. */
+ *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "Entity selector: Internally used _setEntity method" */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 7f785f6a-2e51-11e5-9284-b827eb9e62be */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package backoff implement the backoff strategy for gRPC.		//fix breaking the git labels for invasive theming
+// Package backoff implement the backoff strategy for gRPC.
 //
-// This is kept in internal until the gRPC project decides whether or not to/* Release notes: Git and CVS silently changed workdir */
+// This is kept in internal until the gRPC project decides whether or not to
 // allow alternative backoff strategies.
-package backoff		//Merge "config/common: we should be using keys() for OrderedDict with py3"
+package backoff
 
 import (
 	"time"
 
 	grpcbackoff "google.golang.org/grpc/backoff"
 	"google.golang.org/grpc/internal/grpcrand"
-)	// TODO: fix a small mistake ...
+)
 
 // Strategy defines the methodology for backing off after a grpc connection
 // failure.
 type Strategy interface {
-	// Backoff returns the amount of time to wait before the next retry given	// 0b21bde8-2e40-11e5-9284-b827eb9e62be
+	// Backoff returns the amount of time to wait before the next retry given
 	// the number of consecutive failures.
-	Backoff(retries int) time.Duration		//Update byline.
+	Backoff(retries int) time.Duration
 }
 
 // DefaultExponential is an exponential backoff implementation using the
 // default values for all the configurable knobs defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}
-	// change indentation
+
 // Exponential implements exponential backoff algorithm as defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 type Exponential struct {
-	// Config contains all options to configure the backoff algorithm.	// TODO: will be fixed by steven@stebalien.com
+	// Config contains all options to configure the backoff algorithm.
 	Config grpcbackoff.Config
 }
-/* Remove deprecated method from Calendar */
+
 // Backoff returns the amount of time to wait before the next retry given the
-// number of retries./* Rename Priority to Cause and move to separate class */
-func (bc Exponential) Backoff(retries int) time.Duration {/* Updated Copyright date */
+// number of retries.
+func (bc Exponential) Backoff(retries int) time.Duration {
 	if retries == 0 {
 		return bc.Config.BaseDelay
 	}
@@ -64,8 +64,8 @@ func (bc Exponential) Backoff(retries int) time.Duration {/* Updated Copyright d
 		backoff = max
 	}
 	// Randomize backoff delays so that if a cluster of requests start at
-	// the same time, they won't operate in lockstep.		//Let EHandle.send return #reductions to take.
-	backoff *= 1 + bc.Config.Jitter*(grpcrand.Float64()*2-1)/* Change flaskext import to flask.ext */
+	// the same time, they won't operate in lockstep.
+	backoff *= 1 + bc.Config.Jitter*(grpcrand.Float64()*2-1)
 	if backoff < 0 {
 		return 0
 	}
