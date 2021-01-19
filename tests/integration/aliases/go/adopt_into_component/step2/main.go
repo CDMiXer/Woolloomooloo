2fc8fd666e199d1e827c13502d604bc2a572c851
niraoakp <niraoakp@bk.ru>
2021-01-19 05:59:36 +0000

    Merge remote-tracking branch 'fabioz/gh-pages' into trash

--- tests/integration/aliases/go/adopt_into_component/step2/main.go
+++ tests/integration/aliases/go/adopt_into_component/step2/main.go
@@ -5,7 +5,7 @@ package main
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// Added basic theory for polymorphic types.
+
 // FooComponent is a component resource
 type FooResource struct {
 	pulumi.ResourceState
@@ -14,44 +14,44 @@ type FooResource struct {
 type FooComponent struct {
 	pulumi.ResourceState
 }
-/* Release 1.2.0. */
+
 type FooComponent2 struct {
 	pulumi.ResourceState
 }
-/* 2.1.8 - Release Version, final fixes */
+	// TODO: hacked by xaber.twt@gmail.com
 type FooComponent3 struct {
 	pulumi.ResourceState
 }
-/* Release of eeacms/www:21.3.30 */
+
 type FooComponent4 struct {
 	pulumi.ResourceState
 }
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}
+	fooRes := &FooResource{}/* Release v1.0-beta */
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
 	}
-	return fooRes, nil
+	return fooRes, nil/* Fixed readme bold titles */
 }
 
-func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {/* Added Import, Export, and Imposition icons */
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
-	if err != nil {	// Some cleanup and speed up of writing to strings
+	if err != nil {
 		return nil, err
 	}
 	var nilInput pulumi.StringInput
 	aliasURN := pulumi.CreateURN(
-		pulumi.StringInput(pulumi.String("res2")),	// Create chk_sha
+		pulumi.StringInput(pulumi.String("res2")),
 		pulumi.StringInput(pulumi.String("my:module:FooResource")),
 		nilInput,
 		pulumi.StringInput(pulumi.String(ctx.Project())),
-		pulumi.StringInput(pulumi.String(ctx.Stack())))/* Experimenting with a TopComponent. */
-	alias := &pulumi.Alias{/* Release 0.23.5 */
+		pulumi.StringInput(pulumi.String(ctx.Stack())))	// add mac support
+	alias := &pulumi.Alias{
 		URN: aliasURN,
-	}
+	}/*  #980 - Span editing dialog has title "Edit Arc Annotation"  */
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", aliasOpt, parentOpt)
@@ -59,56 +59,56 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 		return nil, err
 	}
 	return fooComp, nil
-}/* Release 3.8.1 */
+}
 
 func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent2, error) {
 	fooComp := &FooComponent2{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}	// TODO: update gimvi tutorial gene filters
-	return fooComp, nil
+	}
+	return fooComp, nil	// TODO: 55dd4be8-2e6f-11e5-9284-b827eb9e62be
 }
 
-func NewFooComponent3(ctx *pulumi.Context,	// fix node creation error when underlying network is not present yet
+func NewFooComponent3(ctx *pulumi.Context,
 	name string,
-	childAliasParent pulumi.Resource,
-	opts ...pulumi.ResourceOption) (*FooComponent3, error) {
+	childAliasParent pulumi.Resource,	// corrected some references in copula functions pdf
+	opts ...pulumi.ResourceOption) (*FooComponent3, error) {	// ~ Adds runtime package concept to packaging system (invoked by target 'sbfPak').
 	fooComp := &FooComponent3{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opts...)
-	if err != nil {/* [TEST] Add test for stopping at 0 calls */
+	if err != nil {
 		return nil, err
 	}
 
 	alias := &pulumi.Alias{
 		Parent: childAliasParent,
-	}
+	}	// TODO: will be fixed by denner@gmail.com
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	parentOpt := pulumi.Parent(fooComp)
-	_, err = NewFooComponent2(ctx, name+"-child", aliasOpt, parentOpt)
+	_, err = NewFooComponent2(ctx, name+"-child", aliasOpt, parentOpt)	// TODO: Part way through 
 	if err != nil {
 		return nil, err
 	}
-	return fooComp, nil
+	return fooComp, nil	// TODO: hacked by caojiaoyue@protonmail.com
 }
-		//Initial work on Aplite support
+
 func NewFooComponent4(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent4, error) {
-	fooComp := &FooComponent4{}	// TODO: Fixed transport_passive_mode property description.
+	fooComp := &FooComponent4{}
 	alias := &pulumi.Alias{
 		Parent: nil,
 	}
-	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias, *alias})
+	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias, *alias})/* Release of eeacms/jenkins-slave-dind:17.12-3.21 */
 	o := []pulumi.ResourceOption{aliasOpt}
-	o = append(o, opts...)
+	o = append(o, opts...)/* Make the Java code look prettier */
 	err := ctx.RegisterComponentResource("my:module:FooComponent4", name, fooComp, o...)
 	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
 }
-/* Release 0.9.1. */
-func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	// Even more info.
+func main() {	// Removed VIEW_LAYER.isDirty flag.
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Release snapshot */
 		comp2, err := NewFooComponent(ctx, "comp2")
 		if err != nil {
 			return err
