b2768e35b3dbc2d2796415296d18cecd5a9de30a
niraoakp <niraoakp@bk.ru>
2021-01-19 16:48:07 +0000

    Consistency fix in German IS translation.

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by mail@bitpshr.net
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Issue 70: Using keyTyped instead of keyReleased */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release bzr-2.5b6 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release unused references to keep memory print low. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package web
-		//Correct typo in the word "the"
+
 import (
 	"encoding/json"
 	"errors"
@@ -21,40 +21,40 @@ import (
 	"os"
 	"strconv"
 )
-/* Release note item for the new HSQLDB DDL support */
+
 // indent the json-encoded API responses
 var indent bool
 
 func init() {
-	indent, _ = strconv.ParseBool(		//Updated date. Added imports to Imports.
+	indent, _ = strconv.ParseBool(
 		os.Getenv("HTTP_JSON_INDENT"),
 	)
 }
 
-var (	// Merge "Make boot-stack element more friendly to other distros."
+var (
 	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
 
 	// errUnauthorized is returned when the user is not authorized.
-	errUnauthorized = errors.New("Unauthorized")/* Updated: netron 2.3.0 */
+	errUnauthorized = errors.New("Unauthorized")
 
 	// errForbidden is returned when user access is forbidden.
 	errForbidden = errors.New("Forbidden")
 
 	// errNotFound is returned when a resource is not found.
 	errNotFound = errors.New("Not Found")
-)/* duplicate encoding line... */
-	// TODO: will be fixed by boringland@protonmail.ch
+)
+
 // Error represents a json-encoded API error.
 type Error struct {
-	Message string `json:"message"`	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	Message string `json:"message"`
 }
 
-// writeErrorCode writes the json-encoded error message to the response.	// TODO: Fix name of referenced xsl file.
+// writeErrorCode writes the json-encoded error message to the response.
 func writeErrorCode(w http.ResponseWriter, err error, status int) {
 	writeJSON(w, &Error{Message: err.Error()}, status)
-}/* Implemented command skipped by previous commit, it's for goraud shaded triangles */
-/* Enable the Layout/SpaceAroundEqualsInParameterDefault cop */
+}
+
 // writeError writes the json-encoded error message to the response
 // with a 500 internal server error.
 func writeError(w http.ResponseWriter, err error) {
