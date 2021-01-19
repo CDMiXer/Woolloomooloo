1c1ee3e027d8da14cdd19b3330e138289151f3dc
niraoakp <niraoakp@bk.ru>
2021-01-19 02:07:09 +0000

    Added link to API mailing list

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//update Readme to markdown format
-	// TODO: hacked by martin2cai@hotmail.com
+import * as pulumi from "@pulumi/pulumi";
+
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}/* Release 2.0.1. */
+}
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;		//adds length settings for each segment in wheel graphs (+angle,+width). 
+    resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
-    }		//Add README.me in chat_id_file
+    }
 }
 const comp5 = new ComponentFive("comp5");
