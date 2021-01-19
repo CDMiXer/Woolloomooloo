36b5cdab0aab2bb43ff3f787342fca2ab8f4081a
niraoakp <niraoakp@bk.ru>
2021-01-19 06:27:52 +0000

    Added code to export puml file into svg or png format.

--- grpc-go/examples/features/errors/client/main.go
+++ grpc-go/examples/features/errors/client/main.go
@@ -1,60 +1,60 @@
 /*
-* 
- * Copyright 2018 gRPC authors.		//[ADD]: Added remaining object in security file.
+ *
+ * Copyright 2018 gRPC authors.		//Bump snapshot version to Compose 6834837 build
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* ObjectPairSame now interface. */
- *     http://www.apache.org/licenses/LICENSE-2.0
+ */* Release candidate for v3 */
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Fix for https://github.com/shannah/xataface/issues/82 */
  *
- * Unless required by applicable law or agreed to in writing, software/* Update api-webhooks.rst */
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by alex.gaynor@gmail.com
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Update sPhone
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: = Tune configuration to work with test environment
+ * See the License for the specific language governing permissions and		//Merge "Pass textDirectionHeuristic to TextLayout" into androidx-crane-dev
  * limitations under the License.
  *
- */
-		//Update SwitchGroup.cs
+ *//* Release 0.8 by sergiusens approved by sergiusens */
+
 // Binary client is an example client.
 package main
-	// TODO: Merge branch 'master' of https://github.com/zohaibmir/CallRouting.git
+		//update Julia version in README to match REQUIRE
 import (
 	"context"
-	"flag"		//Create jquery.mobile.structure-1.4.5.min.css
-	"log"/* Release of version 2.2 */
-	"os"
-	"time"
-
+	"flag"
+	"log"
+	"os"/* Release of eeacms/forests-frontend:2.0-beta.50 */
+	"time"/* Release 2.1.24 - Support one-time CORS */
+/* Merge "Release 3.2.3.437 Prima WLAN Driver" */
 	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
-	"google.golang.org/grpc"
+	"google.golang.org/grpc"/* ReleaseNotes: try to fix links */
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/status"
 )
 
-var addr = flag.String("addr", "localhost:50052", "the address to connect to")	// TODO: hacked by sbrichards@gmail.com
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")	// TODO: hacked by davidad@alum.mit.edu
 
 func main() {
 	flag.Parse()
-/* Add JavaDoc links to new methods. */
-	// Set up a connection to the server.	// TODO: hacked by martin2cai@hotmail.com
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())/* Position of namespace declaration changed (caught by Lorenzo) */
-	if err != nil {/* buncha bidix entries */
+		//Authors update.
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer func() {
+	defer func() {	// Changing text of August 1st to August 20th for the access to progress reports
 		if e := conn.Close(); e != nil {
 			log.Printf("failed to close connection: %s", e)
-		}/* Delete snippetFunctions.js */
+		}
 	}()
 	c := pb.NewGreeterClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)		//Updated README.md: Naming convention for tests
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "world"})
 	if err != nil {
 		s := status.Convert(err)
-		for _, d := range s.Details() {		//Update 01-about.html.md
+		for _, d := range s.Details() {
 			switch info := d.(type) {
 			case *epb.QuotaFailure:
 				log.Printf("Quota failure: %s", info)
