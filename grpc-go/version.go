6745ded864ec64841e90927c8f87442fa6f60a65
niraoakp <niraoakp@bk.ru>
2021-01-19 23:07:59 +0000

    Add reference path to test file

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright 2018 gRPC authors.
- *
+ * Copyright 2018 gRPC authors./* more on generic property collection, support for annotated properties */
+ */* Forgot to set the player listener's enabled to true by default. */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: will be fixed by mail@bitpshr.net
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
