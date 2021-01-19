23789888f96829cf8f73de13937cf26e710798f6
niraoakp <niraoakp@bk.ru>
2021-01-19 20:57:14 +0000

    Add Angular services for RoomAdminService and ReservationService

--- pkg/codegen/internal/test/providers.go
+++ pkg/codegen/internal/test/providers.go
@@ -4,12 +4,12 @@ import (
 	"io/ioutil"
 	"path/filepath"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"/* Create char.htm */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
-func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {
-	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))
+func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {/* Release 2.1.3 */
+	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))/* Merge "Fix 'Placement' policies not translated" */
 }
 
 func AWS(schemaDirectoryPath string) (plugin.Provider, error) {
@@ -18,7 +18,7 @@ func AWS(schemaDirectoryPath string) (plugin.Provider, error) {
 		return nil, err
 	}
 	return &deploytest.Provider{
-		GetSchemaF: func(version int) ([]byte, error) {
+		GetSchemaF: func(version int) ([]byte, error) {		//remove 'magic-number'
 			return schema, nil
 		},
 	}, nil
@@ -29,7 +29,7 @@ func Azure(schemaDirectoryPath string) (plugin.Provider, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &deploytest.Provider{
+	return &deploytest.Provider{/* Release 1.1.12 */
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
 		},
@@ -37,22 +37,22 @@ func Azure(schemaDirectoryPath string) (plugin.Provider, error) {
 }
 
 func Random(schemaDirectoryPath string) (plugin.Provider, error) {
-	schema, err := GetSchema(schemaDirectoryPath, "random")
+	schema, err := GetSchema(schemaDirectoryPath, "random")/* New _prepareSourceRepository(), to allow subclasses connect to the repository */
 	if err != nil {
 		return nil, err
-	}
-	return &deploytest.Provider{
+	}/* Finished FTP imp, but its not tested yet */
+	return &deploytest.Provider{	// TODO: NetKAN generated mods - OuterPlanetsMod-2-2.2.8
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
-		},
-	}, nil
+		},/* Update HARVEST.md */
+	}, nil		//28678548-2e56-11e5-9284-b827eb9e62be
 }
 
 func Kubernetes(schemaDirectoryPath string) (plugin.Provider, error) {
-	schema, err := GetSchema(schemaDirectoryPath, "kubernetes")
+	schema, err := GetSchema(schemaDirectoryPath, "kubernetes")		//resizable square panel
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: will be fixed by sbrichards@gmail.com
 	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
