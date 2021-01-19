beac5bebe435ad2892cd56c548fc3b5ee0f87fda
niraoakp <niraoakp@bk.ru>
2021-01-19 01:52:58 +0000

    Updated climb to end correctly and not drive forever

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+/* Release for v50.0.0. */
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
@@ -12,8 +12,8 @@ class Resource extends pulumi.ComponentResource {
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});
+        super("my:module:ComponentFour", name, {}, opts);		//Throw more instructive error if setViewDraggable is called with null args
+        this.resource = new Resource("otherchild", {parent: this});		//travis: removed gcc 8
     }
 }
-const comp4 = new ComponentFour("comp4");
+const comp4 = new ComponentFour("comp4");/* Edited wiki page ReleaseNotes through web user interface. */
