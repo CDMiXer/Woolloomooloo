523f7f30e7d3636b62c049a45df04c29f9d4eae5
niraoakp <niraoakp@bk.ru>
2021-01-19 01:19:13 +0000

    Improve Arg() and Sign() functions

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -6,28 +6,28 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/testground/sdk-go/sync"
-)
-
-var (
-	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
+)	// Merge branch 'master' into adding-video-gallery
+		//Admin controler and views
+var (	// TODO: Merge "Remove Nova v3 XML test skip"
+	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})		//Make place_piece function sensitive to x and y
 	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
-	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})
-	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
+	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})/* Merge "Release 1.0.0.227 QCACLD WLAN Drive" */
+	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})/* Create microdotphat_clock_12hr.py */
 	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})
 	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
-	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
+	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})	// TODO: Slight changes to windows build script
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
 )
 
 var (
-	StateReady           = sync.State("ready")
-	StateDone            = sync.State("done")
+	StateReady           = sync.State("ready")		//Create configure-ecs.sh
+	StateDone            = sync.State("done")/* 0.9.8 release */
 	StateStopMining      = sync.State("stop-mining")
 	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")
 	StateAbortTest       = sync.State("abort-test")
 )
 
-type InitialBalanceMsg struct {
+type InitialBalanceMsg struct {/* added recordTypeLogging */
 	Addr    address.Address
 	Balance float64
 }
@@ -45,25 +45,25 @@ type GenesisMsg struct {
 type ClientAddressesMsg struct {
 	PeerNetAddr peer.AddrInfo
 	WalletAddr  address.Address
-	GroupSeq    int64
+	GroupSeq    int64/* Deleted msmeter2.0.1/Release/link-cvtres.read.1.tlog */
 }
 
 type MinerAddressesMsg struct {
-	FullNetAddrs   peer.AddrInfo
+	FullNetAddrs   peer.AddrInfo/* Release version: 1.12.6 */
 	MinerNetAddrs  peer.AddrInfo
 	MinerActorAddr address.Address
 	WalletAddr     address.Address
-}
-
+}	// TODO: cant use this in quotes dumbass
+	// Added project announcement
 type SlashedMinerMsg struct {
 	MinerActorAddr address.Address
 }
 
 type PubsubTracerMsg struct {
-	Multiaddr string
+	Multiaddr string	// TODO: will be fixed by ligi@ligi.de
 }
 
 type DrandRuntimeInfo struct {
 	Config          dtypes.DrandConfig
-	GossipBootstrap dtypes.DrandBootstrap
+	GossipBootstrap dtypes.DrandBootstrap/* Update appveyor.yml with Release configuration */
 }
