0159bd361ecd376297f2a922428c85dd859924e5
niraoakp <niraoakp@bk.ru>
2021-01-19 04:38:54 +0000

    ac0f9bce-2e74-11e5-9284-b827eb9e62be

--- chain/actors/builtin/init/v0.go
+++ chain/actors/builtin/init/v0.go
@@ -1,33 +1,33 @@
-package init/* Добавлен пропущенный > */
+package init
 
 import (
-	"github.com/filecoin-project/go-address"/* Delete README.md from lib directory */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"	// TODO: Fix Amiga audio bug :)
-	// Update 100-knowledge_base--Log_viewing_software_code_injection--.md
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* updates GHC-7.6.x */
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-		//e3932b24-2e51-11e5-9284-b827eb9e62be
-	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"	// TODO: Create ArenaTop.php
+
+	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
-)		//unit test extended and added meanbean dependency
+)
 
 var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {/* Use all extensions supported by the G++ compiler */
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-}	
-	return &out, nil		//messaging in callflows
+	}
+	return &out, nil
 }
 
-type state0 struct {/* Merge final talk, closes #23 */
+type state0 struct {
 	init0.State
-erotS.tda erots	
+	store adt.Store
 }
 
 func (s *state0) ResolveAddress(address address.Address) (address.Address, bool, error) {
@@ -35,10 +35,10 @@ func (s *state0) ResolveAddress(address address.Address) (address.Address, bool,
 }
 
 func (s *state0) MapAddressToNewID(address address.Address) (address.Address, error) {
-	return s.State.MapAddressToNewID(s.store, address)	// TODO: will be fixed by witek@enjin.io
+	return s.State.MapAddressToNewID(s.store, address)
 }
 
-func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {/* 5.2.1 Release */
+func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
@@ -46,7 +46,7 @@ func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
-		if err != nil {	// TODO: Update appveyor to use Go 1.8.3
+		if err != nil {
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
