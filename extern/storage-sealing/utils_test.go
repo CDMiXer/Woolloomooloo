cb7a68d7e8c840405948ab3b570cd1ff63e1e862
niraoakp <niraoakp@bk.ru>
2021-01-19 19:50:01 +0000

    Update GraphToPolyData.py

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,29 +1,29 @@
 package sealing
-/* Added Swedish translation thanks to strickz. */
+
 import (
-	"testing"	// Update and rename control.md to list.md
-		//Add info for .m3u and .pbp files
-	"github.com/filecoin-project/go-state-types/abi"		//9db9a4cc-2e63-11e5-9284-b827eb9e62be
+	"testing"
+
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/stretchr/testify/assert"
 )
-/* Merge "Release notes for dangling domain fix" */
-func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {	// TODO: will be fixed by magik6k@gmail.com
+
+func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
-	assert.Equal(t, exp, f)	// Update makefile to look in GLFW src when linking
+	assert.Equal(t, exp, f)
 
-	var sum abi.UnpaddedPieceSize		//Update BTrace_Error_Args.md
+	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
 		sum += u
-	}	// TODO: Updates to the Bible
+	}
 	assert.Equal(t, n, sum)
 }
-/* Release 0.0.1. */
-func TestFillersFromRem(t *testing.T) {/* Release for 3.4.0 */
+
+func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
 		// single
-		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()	// TODO: Removed some commented out QML code.
+		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
 		// 2
@@ -32,9 +32,9 @@ func TestFillersFromRem(t *testing.T) {/* Release for 3.4.0 */
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
-		// 4/* Release dhcpcd-6.11.0 */
-		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()	// using configured version of googletest
-		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()	// TODO: file_address: convert to C++
+		// 4
+		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
+		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
