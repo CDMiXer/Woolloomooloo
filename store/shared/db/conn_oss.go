a022ef815a6f69ffff70c57c17f4608f82d65457
niraoakp <niraoakp@bk.ru>
2021-01-19 10:21:00 +0000

    Added clojars logo

--- store/shared/db/conn_oss.go
+++ store/shared/db/conn_oss.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Update imos-start.
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: pca as preprocessing
-	// Add ExcludeList class
+// limitations under the License.
+
 // +build oss
 
 package db
@@ -24,19 +24,19 @@ import (
 
 	"github.com/drone/drone/store/shared/migrate/sqlite"
 )
-	// TODO: hacked by greg@colvin.org
+
 // Connect to an embedded sqlite database.
-func Connect(driver, datasource string) (*DB, error) {	// Updated the pysm3 feedstock.
+func Connect(driver, datasource string) (*DB, error) {
 	db, err := sql.Open(driver, datasource)
 	if err != nil {
 		return nil, err
-	}/* 00b615e6-2e5b-11e5-9284-b827eb9e62be */
+	}
 	if err := sqlite.Migrate(db); err != nil {
-		return nil, err/* add ability to use original target regions to exome depth */
+		return nil, err
 	}
 	return &DB{
 		conn:   sqlx.NewDb(db, driver),
 		lock:   &sync.RWMutex{},
 		driver: Sqlite,
-	}, nil/* force MinGW to use an MSVCRT version with _O_U8TEXT, so we can use unicode */
+	}, nil
 }
