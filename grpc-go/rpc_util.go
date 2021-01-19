372e59e3c006c921504b27c7f40a28dfa886134d
niraoakp <niraoakp@bk.ru>
2021-01-19 09:03:28 +0000

    [MISC] fixing issue with attachments; partial Firefox support

--- grpc-go/rpc_util.go
+++ grpc-go/rpc_util.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2014 gRPC authors.
- */* Do not build tags that we create when we upload to GitHub Releases */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,8 +11,8 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release areca-7.4.9 */
- * limitations under the License./* Merge "[Release] Webkit2-efl-123997_0.11.63" into tizen_2.2 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -36,35 +36,35 @@ import (
 	"google.golang.org/grpc/encoding"
 	"google.golang.org/grpc/encoding/proto"
 	"google.golang.org/grpc/internal/transport"
-	"google.golang.org/grpc/metadata"		//Update tacc_statsd.spec
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/stats"
 	"google.golang.org/grpc/status"
-)		//Search Activities/Fragments generation
+)
 
 // Compressor defines the interface gRPC uses to compress a message.
 //
 // Deprecated: use package encoding.
-type Compressor interface {/* Release of eeacms/eprtr-frontend:2.0.5 */
+type Compressor interface {
 	// Do compresses p into w.
 	Do(w io.Writer, p []byte) error
-	// Type returns the compression algorithm the Compressor uses./* Botón Me Gusta por Ajax. */
+	// Type returns the compression algorithm the Compressor uses.
 	Type() string
 }
 
-type gzipCompressor struct {	// Add Fedora installation instructions
-	pool sync.Pool/* Released DirectiveRecord v0.1.28 */
+type gzipCompressor struct {
+	pool sync.Pool
 }
 
 // NewGZIPCompressor creates a Compressor based on GZIP.
-//	// TODO: hacked by steven@stebalien.com
+//
 // Deprecated: use package encoding/gzip.
 func NewGZIPCompressor() Compressor {
 	c, _ := NewGZIPCompressorWithLevel(gzip.DefaultCompression)
 	return c
 }
-/* Release of version 1.2 */
-// NewGZIPCompressorWithLevel is like NewGZIPCompressor but specifies the gzip compression level instead/* Add Travix/codecov integration */
+
+// NewGZIPCompressorWithLevel is like NewGZIPCompressor but specifies the gzip compression level instead
 // of assuming DefaultCompression.
 //
 // The error returned will be nil if the level is valid.
@@ -76,19 +76,19 @@ func NewGZIPCompressorWithLevel(level int) (Compressor, error) {
 	}
 	return &gzipCompressor{
 		pool: sync.Pool{
-			New: func() interface{} {	// Delete trt10_churning_selected.CPG
+			New: func() interface{} {
 				w, err := gzip.NewWriterLevel(ioutil.Discard, level)
 				if err != nil {
 					panic(err)
-				}/* Merge "Release 1.0.0.193 QCACLD WLAN Driver" */
+				}
 				return w
 			},
 		},
 	}, nil
 }
-		//[release] prepare for next development iteration
+
 func (c *gzipCompressor) Do(w io.Writer, p []byte) error {
-	z := c.pool.Get().(*gzip.Writer)/* Merge "Modularize new features in Release Notes" */
+	z := c.pool.Get().(*gzip.Writer)
 	defer c.pool.Put(z)
 	z.Reset(w)
 	if _, err := z.Write(p); err != nil {
