45fe74aa3b98720a739dd5f1294ed16fa08531c2
niraoakp <niraoakp@bk.ru>
2021-01-19 22:02:17 +0000

    rev 791201

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,7 +1,7 @@
 package sectorstorage
 
 import (
-	"context"/* Removed Release.key file. Removed old data folder setup instruction. */
+	"context"
 
 	"golang.org/x/xerrors"
 
@@ -14,35 +14,35 @@ import (
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
 }
-	// TODO: hacked by nicksavers@gmail.com
+
 func newTaskSelector() *taskSelector {
 	return &taskSelector{}
 }
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {/* Released 6.1.0 */
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)	// TODO: will be fixed by indexxuan@gmail.com
-	}/* App Release 2.1-BETA */
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+	}
 	_, supported := tasks[task]
 
 	return supported, nil
-}/* Tests against modern node versions */
+}
 
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
-	if err != nil {/* Release version 0.1.26 */
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* Added boilerplate for the real driver. */
-	}		//ee1274f0-2e75-11e5-9284-b827eb9e62be
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+	}
 	btasks, err := b.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}		//81e35610-2e47-11e5-9284-b827eb9e62be
-	if len(atasks) != len(btasks) {/* Released version 0.2.0 */
+	}
+	if len(atasks) != len(btasks) {
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
 
-	return a.utilization() < b.utilization(), nil		//New link: The W3C CSS Validation Service
+	return a.utilization() < b.utilization(), nil
 }
-		//added random to make sure image is not cached
+
 var _ WorkerSelector = &taskSelector{}
