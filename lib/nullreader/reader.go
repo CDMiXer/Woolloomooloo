c361e6e250fcc0633c0f446a87f88ff8bbfd4d14
niraoakp <niraoakp@bk.ru>
2021-01-19 04:20:05 +0000

    Added support for Minecraft Server Version 1.7.*

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
 package nullreader
 
-type Reader struct{}
+type Reader struct{}/* Release of eeacms/forests-frontend:2.0-beta.43 */
 
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
 	return len(out), nil
-}
+}/* use go 1.14.4 when building logjam container */
