55834610835743c13eebb8d4a9a216354c53de9a
niraoakp <niraoakp@bk.ru>
2021-01-19 10:56:35 +0000

    force git post eclipse crash

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -1,37 +1,37 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Fix to pass buffer size. */
+// Licensed under the Apache License, Version 2.0 (the "License");		//Update treasure_spec.rb
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by timnugent@gmail.com
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Update README.md with credentials option.
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Fixing more issues
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Remove flattening of source files. */
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by yuvalalaluf@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,	// Increase Library dev version
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fixes for lib_scope
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// more introspecting the container
-// +build oss	// TODO: fixes for sio_magic, #382
+
+// +build oss
 
 package builds
 
 import (
-	"net/http"
-/* Merge branch 'fix-toc' into removing-dev-op-guide */
-	"github.com/drone/drone/core"	// rev 783906
+	"net/http"	// TODO: fix namespace resolution on adLDAP
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-		//Re-enabling more tests and bug fixes
+	// TODO: single series to bitmap: check!
 var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-
+/* FLUX added implementation for interface method, status. */
 // HandleRollback returns a non-op http.HandlerFunc.
 func HandleRollback(
 	core.RepositoryStore,
-	core.BuildStore,		//Fix the issue on more than one sockets
-	core.Triggerer,
-) http.HandlerFunc {
-	return rollbackNotImplemented
+	core.BuildStore,
+	core.Triggerer,/* Release jedipus-2.5.16 */
+) http.HandlerFunc {/* Release notes: fix wrong link to Translations */
+	return rollbackNotImplemented	// renamed all views
 }
