50cb24f2558876cc2c739e6104601328d883a8aa
niraoakp <niraoakp@bk.ru>
2021-01-19 20:16:02 +0000

    Added edit function in Material Manager

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -1,39 +1,39 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// Make Digest benchmarks configurable. Exercise different hash methods.
+import * as pulumi from "@pulumi/pulumi";
 
-class DynamicProvider extends pulumi.ProviderResource {	// TODO: hacked by juan@benet.ai
+class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name, {}, opts);
-}    
+    }
 }
 
 class Provider implements pulumi.dynamic.ResourceProvider {
-    public static instance = new Provider();/* fix custom build bug */
-/* use a constant for the main logo just in case i want to change the logo. */
+    public static instance = new Provider();
+
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
-            return {	// TODO: Space-time maps working: Themeless, Hinge 1.5, Hinge 3.0
+            return {
                 id: "0",
                 outs: undefined,
-            };/* [artifactory-release] Release version 0.8.13.RELEASE */
+            };
         };
     }
-}/* Merge "Release 3.2.3.456 Prima WLAN Driver" */
+}
 
 class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, provider?: pulumi.ProviderResource) {/* rev 861099 */
+    constructor(name: string, provider?: pulumi.ProviderResource) {
         super(Provider.instance, name, {}, { provider: provider});
     }
 }
 
 // Create a resource using the default dynamic provider instance.
-let a = new Resource("a");/* Now marshal's objects so more than strings can be stored. */
+let a = new Resource("a");
 
 // Create an explicit instance of the dynamic provider.
-let p = new DynamicProvider("p");/* Release version 4.0.0.12. */
+let p = new DynamicProvider("p");
 
 // Create a resource using the explicit dynamic provider instance.
 let b = new Resource("b", p);
