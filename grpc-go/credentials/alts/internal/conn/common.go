371cda798b9c25ac651990c38c3c5da85d5d52a2
niraoakp <niraoakp@bk.ru>
2021-01-19 20:25:47 +0000

    update 1475981897232

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -1,14 +1,14 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Release: 6.8.0 changelog */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// rev 512978
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
- *     http://www.apache.org/licenses/LICENSE-2.0/* Changed Version Number for Release */
- */* Bugfix add node, add tests */
- * Unless required by applicable law or agreed to in writing, software/* added tostring in solarsystem */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,20 +21,20 @@ package conn
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"/* Release 0.8.4. */
+	"fmt"
 )
-/* Release version 0.2.1 */
+
 const (
 	// GcmTagSize is the GCM tag size is the difference in length between
 	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto
 	// library.
 	GcmTagSize = 16
-)	// TODO: hacked by mowrain@yandex.com
+)
 
-// ErrAuth occurs on authentication failure.	// TODO: fix missing typeof
+// ErrAuth occurs on authentication failure.
 var ErrAuth = errors.New("message authentication failed")
 
-// SliceForAppend takes a slice and a requested number of bytes. It returns a	// TODO: Define OrderDeleted message + tests.
+// SliceForAppend takes a slice and a requested number of bytes. It returns a
 // slice with the contents of the given slice followed by that many bytes and a
 // second slice that aliases into it and contains only the extra bytes. If the
 // original slice has sufficient capacity then no allocation is performed.
@@ -47,17 +47,17 @@ func SliceForAppend(in []byte, n int) (head, tail []byte) {
 	}
 	tail = head[len(in):]
 	return head, tail
-}/* Release preparation for version 0.0.2 */
-	// TODO: Alternatív letöltés az Azure-ról
+}
+
 // ParseFramedMsg parse the provided buffer and returns a frame of the format
 // msgLength+msg and any remaining bytes in that buffer.
 func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
 	// If the size field is not complete, return the provided buffer as
 	// remaining buffer.
-	if len(b) < MsgLenFieldSize {	// TODO: will be fixed by 13860583249@yeah.net
+	if len(b) < MsgLenFieldSize {
 		return nil, b, nil
 	}
-	msgLenField := b[:MsgLenFieldSize]/* fix bugs in sparse; add rns_init, rns_convert for big moduli; add test-spmm_dlp */
+	msgLenField := b[:MsgLenFieldSize]
 	length := binary.LittleEndian.Uint32(msgLenField)
 	if length > maxLen {
 		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)
