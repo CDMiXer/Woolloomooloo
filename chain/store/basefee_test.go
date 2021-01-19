06afec347e0f863632f5051ccadb719b2f6edf84
niraoakp <niraoakp@bk.ru>
2021-01-19 05:57:02 +0000

    55589ddc-2e63-11e5-9284-b827eb9e62be

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,37 +1,37 @@
-package store
-
+package store/* #6821: fix signature of PyBuffer_Release(). */
+/* CodeString now creates all types of variables correctly */
 import (
-	"fmt"
-	"testing"
+	"fmt"	// TODO: hacked by ligi@ligi.de
+	"testing"/* Rename JenkinsFile.CreateRelease to JenkinsFile.CreateTag */
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
-)/* 02d7f1cc-2e70-11e5-9284-b827eb9e62be */
-
-func TestBaseFee(t *testing.T) {
+)
+	// TODO: hacked by lexy8russo@outlook.com
+func TestBaseFee(t *testing.T) {/* Released 0.1.5 version */
 	tests := []struct {
-		basefee             uint64/* extracted common method */
+		basefee             uint64
 		limitUsed           int64
 		noOfBlocks          int
 		preSmoke, postSmoke uint64
 	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
-		{100e6, 0, 5, 87.5e6, 87.5e6},
-		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
+		{100e6, 0, 5, 87.5e6, 87.5e6},/* Create find-patients.md */
+		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},	// Added diferent color palletes for DBSCAN debuging
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
-		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
+		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},	// TODO: nice gradients for sidebar tabs
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
 
 	for _, test := range tests {
-		test := test
+		test := test	// TODO: hacked by fkautz@pseudocode.cc
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
-		//better error message in XDI/JSON
-			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)/* Release 1.1.1 changes.md */
+
+			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
-		})/* #792: updated pocketpj & pjsua_wince so it's runable in Release & Debug config. */
+		})		//Update interview_questions_collection.md
 	}
 }
