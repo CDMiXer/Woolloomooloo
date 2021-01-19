0c25a28e00f3d765e5fe665da2fc344784b4b4a3
niraoakp <niraoakp@bk.ru>
2021-01-19 19:25:57 +0000

    Bug fix: need to save/restore size of partial_calls with rescue, too.

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -1,19 +1,19 @@
 package genesis
 
-import (	// TODO: hacked by ligi@ligi.de
+import (
 	"context"
-/* 6d963620-2e4f-11e5-9284-b827eb9e62be */
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
-	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: Handle database exception
+	cbor "github.com/ipfs/go-ipld-cbor"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"/* Fix eating of white space in highlighted strings in the MOW */
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {	// TODO: will be fixed by arajasek94@gmail.com
+func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
@@ -21,7 +21,7 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {	// TOD
 	}
 
 	multiMap, err := adt.AsMultimap(store, emptyMap)
-	if err != nil {		//Create philips.huelabs.connector.js
+	if err != nil {
 		return nil, err
 	}
 
@@ -30,16 +30,16 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {	// TOD
 		return nil, err
 	}
 
-	sms := power0.ConstructState(emptyMap, emptyMultiMap)		//Addendum to r8188.
+	sms := power0.ConstructState(emptyMap, emptyMultiMap)
 
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
-		return nil, err	// TODO: Merge "Make FlowUpdateWorkflowPageId a run-once updatescript"
-	}/* Release version [9.7.13] - prepare */
+		return nil, err
+	}
 
 	return &types.Actor{
 		Code:    builtin.StoragePowerActorCodeID,
-		Head:    stcid,/* Release ver 2.4.0 */
+		Head:    stcid,
 		Nonce:   0,
 		Balance: types.NewInt(0),
 	}, nil
