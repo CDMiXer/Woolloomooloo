227de1e859258e4e09fbd0da0d44953993409268
niraoakp <niraoakp@bk.ru>
2021-01-19 14:42:29 +0000

    Removed metadata from lenna. Sorry, lady.

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -2,6 +2,6 @@
 
 import { Resource } from "./resource";
 
-// Step 2: Create resources during `pulumi query` -- should error.	// try to fix broken test (works here, but gets more events on build machine)
+// Step 2: Create resources during `pulumi query` -- should error.
 const b = new Resource("b", { state: 2 });
-const a = new Resource("a", { state: 1, resource: b });/* Fixed background transparency */
+const a = new Resource("a", { state: 1, resource: b });
