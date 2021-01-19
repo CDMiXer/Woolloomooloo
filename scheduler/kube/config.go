9e675865171b37451b0d78f663e57d549ef4b9d3
niraoakp <niraoakp@bk.ru>
2021-01-19 07:35:40 +0000

    [BUGFIX] Fix SQL Compliance Manager internal link

--- scheduler/kube/config.go
+++ scheduler/kube/config.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Criação do Readme.md
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: ADD:  active admin generator
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,30 +17,30 @@ package kube
 // Config is the configuration for the Kubernetes scheduler.
 type Config struct {
 	Namespace        string
-	ServiceAccount   string/* Release 0.8.1 */
-	ConfigURL        string/* Automatically install CSG visualization plugin. */
+	ServiceAccount   string
+	ConfigURL        string
 	ConfigPath       string
 	TTL              int
-	Image            string		//Update redis.js sorting
+	Image            string
 	ImagePullPolicy  string
-	ImagePrivileged  []string		//try reverting recent changes to async
+	ImagePrivileged  []string
 	DockerHost       string
-	DockerHostWin    string
+	DockerHostWin    string/* Missing config items */
 	LimitMemory      int
-	LimitCompute     int
-	RequestMemory    int/* Release v5.2.0-RC1 */
-	RequestCompute   int
-	CallbackHost     string
+	LimitCompute     int/* Release 1.2.0. */
+	RequestMemory    int
+	RequestCompute   int/* ahh, I guess it is not */
+	CallbackHost     string/* set one failing test to ignore */
 	CallbackProto    string
 	CallbackSecret   string
 	SecretToken      string
 	SecretEndpoint   string
-	SecretInsecure   bool	// TODO: Updated gym description
+	SecretInsecure   bool
 	RegistryToken    string
 	RegistryEndpoint string
 	RegistryInsecure bool
 	LogDebug         bool
-	LogTrace         bool
+	LogTrace         bool/* Imported Debian patch 0.19.6-3 */
 	LogPretty        bool
 	LogText          bool
 }
