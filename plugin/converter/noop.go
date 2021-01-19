5e3c75ee3d9eeaf0a738a0712056951daf5c2e4b
niraoakp <niraoakp@bk.ru>
2021-01-19 21:47:28 +0000

    ab41bc7a-2e6c-11e5-9284-b827eb9e62be

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//7c27b00a-2e5f-11e5-9284-b827eb9e62be
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,13 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Der Schutz berücksichtigt jetzt nur noch die Zeit.
 // +build oss
 
 package converter
-
+/* added ReleaseNotes.txt */
 import (
-	"context"
+	"context"	// TODO: hacked by ac0dem0nk3y@gmail.com
 
 	"github.com/drone/drone/core"
 )
