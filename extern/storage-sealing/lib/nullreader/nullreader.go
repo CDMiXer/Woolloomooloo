63d4904940467a12a14678a5e2989907059e8fd4
niraoakp <niraoakp@bk.ru>
2021-01-19 08:13:13 +0000

    Taking over keydown event for font effects slide.

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
 package nullreader
 
-// TODO: extract this to someplace where it can be shared with lotus
+// TODO: extract this to someplace where it can be shared with lotus/* Merge "wlan: Release 3.2.3.108" */
 type Reader struct{}
-
+	// TODO: add/mod environment variables in mysqltest for cluster/j
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
-	}
-	return len(out), nil
+	}/* Release of eeacms/eprtr-frontend:0.3-beta.16 */
+	return len(out), nil		//Fixing performance test
 }
