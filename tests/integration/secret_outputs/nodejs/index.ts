815ca8b76f35a5cd32e4802df7952eb03761beed
niraoakp <niraoakp@bk.ru>
2021-01-19 19:06:18 +0000

    added LinkedList to README.md

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -2,15 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
 
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")	// TODO: Updated banner content
+    prefix: pulumi.output("it's a secret to everybody")
 });
 
 export const withSecret = new R("withSecret", {
-    prefix: pulumi.secret("it's a secret to everybody")/* Release of eeacms/www:20.8.15 */
+    prefix: pulumi.secret("it's a secret to everybody")
 });
 
-export const withSecretAdditional = new R("withSecretAdditional", {	// TODO: [tools] firmware-utils/mkcsysimg: minor bugfix
+export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
 }, {
     additionalSecretOutputs: ["prefix"]
-});/* [10518] added setting PathologicDescription to MesswerteView */
+});
