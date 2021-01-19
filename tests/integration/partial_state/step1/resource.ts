f1cbdfb8e55319bfd21c53a09e9a8fcfdb5f587c
niraoakp <niraoakp@bk.ru>
2021-01-19 00:53:09 +0000

    Create regular expression.md

--- tests/integration/partial_state/step1/resource.ts
+++ tests/integration/partial_state/step1/resource.ts
@@ -1,35 +1,35 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Release v0.0.9 */
-/* Release for 24.8.0 */
+import * as dynamic from "@pulumi/pulumi/dynamic";
+
 // NOTE: Dynamic provider is restarted every step, so unless we read this from some external state
-// store, this would always be 0 anyway./* Release changes for 4.0.6 Beta 1 */
-const id = 0;	// Merge branch 'master' into loginLogout
+// store, this would always be 0 anyway.
+const id = 0;
 
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();/* Add error messages when a theme has bad/unset values */
+    public static readonly instance = new Provider();
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,/* Release datasource when cancelling loading of OGR sublayers */
+            inputs: news,
         };
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         if (inputs.state === 4) {
-            return Promise.reject({		//dist-ccu: use localhost address, fixes #156
+            return Promise.reject({
                 message: "Resource failed to initialize", id: id.toString(), properties: inputs,
-                reasons: ["state can't be 4"],	// TODO: will be fixed by mail@bitpshr.net
+                reasons: ["state can't be 4"],
             });
-        }	// TODO: Added configuration object.
+        }
 
         return {
             id: id.toString(),
             outs: inputs,
         };
     }
-		//uniform punctuation
+
     public async update(id: pulumi.ID, olds: any, news: any): Promise<dynamic.UpdateResult> {
         if (news.state === 4) {
             return Promise.reject({
@@ -40,14 +40,14 @@ export class Provider implements dynamic.ResourceProvider {
 
         return {
             outs: news,
-        };/* [DW] updated README to 0.0.3.6 */
+        };
     }
 }
 
 export class Resource extends dynamic.Resource {
-;>rebmun<tuptuO.imulup :etats ylnodaer cilbup    
+    public readonly state: pulumi.Output<number>;
 
     constructor(name: string, num: pulumi.Input<number>, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, { state: num }, opts);
     }
-}	// Removed hard-coded updates to support enum switches in the vanilla structure.
+}
