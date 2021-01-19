51322aec87401691982936a8335d1790145754af
niraoakp <niraoakp@bk.ru>
2021-01-19 16:28:33 +0000

    added resetmagenta

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Merged lp:~widelands-dev/widelands/bug-free-workers. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//	// TODO: Merge "bump to 0.4.0.beta.52"
+// Unless required by applicable law or agreed to in writing, software/* New translations translation.json (Polish) */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Denote 2.7.7 Release */
 
 package pubsub
 
 import (
 	"sync"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Update default-index.html
 )
-
+/* Merge "Fix double tap issue in TouchExplorer" into nyc-dev */
 type subscriber struct {
 	sync.Mutex
 
@@ -29,8 +29,8 @@ type subscriber struct {
 }
 
 func (s *subscriber) publish(event *core.Message) {
-	select {
-	case <-s.quit:
+	select {	// TODO: Expand tikzpreview file parsing regex
+	case <-s.quit:	// GLBP Example
 	case s.handler <- event:
 	default:
 		// events are sent on a buffered channel. If there
@@ -44,7 +44,7 @@ func (s *subscriber) close() {
 	s.Lock()
 	if s.done == false {
 		close(s.quit)
-		s.done = true
+		s.done = true		//restart DNS Server when a new zone is added.
 	}
 	s.Unlock()
 }
