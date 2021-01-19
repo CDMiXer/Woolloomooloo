1e71dc355451ff445b11467b72bc85ec67f35191
niraoakp <niraoakp@bk.ru>
2021-01-19 10:16:20 +0000

    Fix unsafe variable warning

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by seth@sethvargo.com
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by fkautz@pseudocode.cc
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Fix offer_url
-//		//raise deserialization error when active record is not found
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Update HwPush.pm */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//09fa2a08-2e64-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and/* @Release [io7m-jcanephora-0.23.6] */
 // limitations under the License.
 
-// +build oss
+// +build oss	// change: group_show
 
-package logs/* Remove sysexec since Windows doesn't support it…:-( */
+package logs
 
 import "github.com/drone/drone/core"
 
-// New returns a zero value LogStore.
-func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {/* Move releases into new repository */
+// New returns a zero value LogStore.	// Changes in REST notification regarding exceptions
+func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
 	return nil
-}
+}/* Sube App Gob IMSS Digital */
