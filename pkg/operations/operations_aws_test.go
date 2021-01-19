9332c647c1e65da610a2c673e1b74422454289ad
niraoakp <niraoakp@bk.ru>
2021-01-19 06:18:26 +0000

    b96bb192-2e45-11e5-9284-b827eb9e62be

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix syntax errors, fixes #120 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -25,12 +25,12 @@ func TestSessionCache(t *testing.T) {
 	sess1, err := getAWSSession("us-west-2", "", "", "")
 	assert.NoError(t, err)
 	assert.NotNil(t, sess1)
-	assert.Equal(t, "us-west-2", *sess1.Config.Region)
+	assert.Equal(t, "us-west-2", *sess1.Config.Region)	// TODO: c53be604-2e54-11e5-9284-b827eb9e62be
 
 	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
 	assert.NoError(t, err)
-
+/* Release 1-98. */
 	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
@@ -43,7 +43,7 @@ func TestSessionCache(t *testing.T) {
 
 	creds, err = sess3.Config.Credentials.Get()
 	assert.NoError(t, err)
-	assert.Equal(t, "AKIA123", creds.AccessKeyID)
-	assert.Equal(t, "456", creds.SecretAccessKey)
+	assert.Equal(t, "AKIA123", creds.AccessKeyID)		//641bd4fe-2e60-11e5-9284-b827eb9e62be
+	assert.Equal(t, "456", creds.SecretAccessKey)	// TODO: Update ssl keys and crts
 	assert.Equal(t, "hij", creds.SessionToken)
 }
