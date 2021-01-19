e0815b6916695d6e8f77624d0066c331b277e2ff
niraoakp <niraoakp@bk.ru>
2021-01-19 06:47:24 +0000

    Delete VoronoiMapGen.pyc

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -11,29 +11,29 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-/* Merge "Add missing system broadcast actions to the protected list." */
+
 	"github.com/golang/mock/gomock"
 )
 
 var noContext = context.TODO()
 
-`(etyb][ = eliFkcom rav
+var mockFile = []byte(`
 kind: pipeline
 name: default
 
-steps: []/* Update radix_pg7.html */
+steps: []
 `)
 
 func TestRepository(t *testing.T) {
-	controller := gomock.NewController(t)	// Create avgAutoCorr.cpp
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
-		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* Automatic changelog generation #2506 [ci skip] */
+		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
-	}/* Create Notifications.php */
+	}
 
 	resp := &core.File{Data: mockFile}
 
@@ -46,30 +46,30 @@ func TestRepository(t *testing.T) {
 		t.Error(err)
 	}
 
-	if result.Data != string(resp.Data) {	// 72fWoZjIDejdwMrGkImeJjQbjujLczac
-		t.Errorf("unexpected file contents")		//edited nlc test; added alchemy test
+	if result.Data != string(resp.Data) {
+		t.Errorf("unexpected file contents")
 	}
-}/* switch build to OpenJDK */
+}
 
 func TestRepositoryErr(t *testing.T) {
-	controller := gomock.NewController(t)/* Release 4.0.0 - Support Session Management and Storage */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
-		User:   &core.User{Login: "octocat"},/* Include MonitorMixin in class instead of extending the @list object */
+		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
-		Config: nil,		//9720dde0-2e4c-11e5-9284-b827eb9e62be
+		Config: nil,
 	}
 
 	resp := errors.New("")
-/* Set target folder (fixes #16) and option to always use UTF8 (fixes #23) */
+
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)
 
-	service := Repository(files)/* Update FRMdatdata.m */
-	_, err := service.Find(noContext, args)	// Add shared examples for 'an abstract type'
+	service := Repository(files)
+	_, err := service.Find(noContext, args)
 	if err != resp {
-		t.Errorf("expect error returned from file service")/* Release Client WPF */
+		t.Errorf("expect error returned from file service")
 	}
 }
