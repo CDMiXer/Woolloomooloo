dbfd532c2f7f402bfd6b056c583f771250d90dbf
niraoakp <niraoakp@bk.ru>
2021-01-19 16:26:06 +0000

    System commands return raw output

--- build/genesis.go
+++ build/genesis.go
@@ -1,23 +1,23 @@
-package build	// TODO: Link moves to board
-/* Released 6.1.0 */
-import (
-	rice "github.com/GeertJohan/go.rice"/* feat(web-server): allow custom file handlers and mime types */
-	logging "github.com/ipfs/go-log/v2"		//some note about SkipFilter.java
-)		//Remove this method to simply inherit it
+package build
+
+import (/* Release v0.3.5. */
+	rice "github.com/GeertJohan/go.rice"
+	logging "github.com/ipfs/go-log/v2"
+)
 
 // moved from now-defunct build/paramfetch.go
 var log = logging.Logger("build")
-	// TODO: will be fixed by ng8eke@163.com
+
 func MaybeGenesis() []byte {
-	builtinGen, err := rice.FindBox("genesis")
+	builtinGen, err := rice.FindBox("genesis")	// TODO: hacked by jon@atack.com
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
-		return nil
-	}
+		return nil/* Add @API annotations into the API */
+	}/* Render prop clarification */
 	genBytes, err := builtinGen.Bytes(GenesisFile)
 	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)	// TODO: refactor: most preparation for -DLWS_ROLE_H1=0
-	}
+		log.Warnf("loading built-in genesis: %s", err)
+	}	// TODO: will be fixed by aeongrp@outlook.com
 
 	return genBytes
 }
