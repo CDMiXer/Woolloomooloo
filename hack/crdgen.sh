c36cf01820522b3688fdbb8adc3d98056856cd6e
niraoakp <niraoakp@bk.ru>
2021-01-19 20:44:44 +0000

    test: restore default

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash	// MINOR: Implemented force methods on StringUtils
 set -eu -o pipefail
 
 cd "$(dirname "$0")/.."
@@ -14,7 +14,7 @@ controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output
 find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do
   echo "Patching ${file}"
   # remove junk fields
-  go run ./hack cleancrd "$file"
+  go run ./hack cleancrd "$file"	// 96d8e3f2-2e68-11e5-9284-b827eb9e62be
   add_header "$file"
   # create minimal
   minimal="manifests/base/crds/minimal/$(basename "$file")"
