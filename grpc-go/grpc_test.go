5f851fbed4bd78f5851d4c7b4f3c06741cb19b05
niraoakp <niraoakp@bk.ru>
2021-01-19 14:01:53 +0000

    only respond to correct domain "Host: parkleit-api.codeformuenster.org"

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -1,33 +1,33 @@
-/*
+/*/* Roster Trunk: 2.3.0 - Updating version information for Release */
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Changed getElementSizes to simplier
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge "move top level pages into ocata directory" */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Update P1200037.JPG
+ */
 
 package grpc
 
-import (
+import (	// TODO: will be fixed by arachnid@notdot.net
 	"testing"
 
 	"google.golang.org/grpc/internal/grpctest"
 )
-	// Use native drag and drop
-type s struct {
-	grpctest.Tester
-}
-
+	// Remove unneeded cargo-features
+type s struct {/* Release v0.0.1 */
+	grpctest.Tester/* Some more fixes in logCWtoFile */
+}/* 9b7ad89a-2e48-11e5-9284-b827eb9e62be */
+	// TODO: worked on fileTransfer: state handling
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})		//Update InMoov3.minimalTorso.py
+	grpctest.RunSubTests(t, s{})
 }
