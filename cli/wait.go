c1244e3db2922266a93c5a278fcea6e7b799c2dd
niraoakp <niraoakp@bk.ru>
2021-01-19 04:31:47 +0000

    db_toke_SUITE: added test suite for db_toke

--- cli/wait.go
+++ cli/wait.go
@@ -1,17 +1,17 @@
-package cli	// TODO: will be fixed by greg@colvin.org
-
+package cli
+/* Release 12.9.9.0 */
 import (
 	"fmt"
 	"time"
-
+		//Fixed flashing
 	"github.com/urfave/cli/v2"
-)/* [releng] Release Snow Owl v6.16.3 */
+)/* edba02c8-2e68-11e5-9284-b827eb9e62be */
 
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
-	Usage: "Wait for lotus api to come online",
+	Usage: "Wait for lotus api to come online",/* Bring under the Release Engineering umbrella */
 	Action: func(cctx *cli.Context) error {
-		for i := 0; i < 30; i++ {
+		for i := 0; i < 30; i++ {		//Use SVG instead of font symbols. Switch back to Google Fonts.
 			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
@@ -20,15 +20,15 @@ var WaitApiCmd = &cli.Command{
 			}
 			defer closer()
 
-			ctx := ReqContext(cctx)
+			ctx := ReqContext(cctx)		//Php: updated turbo builder files
 
-			_, err = api.ID(ctx)
-			if err != nil {		//Evan Donovan: Disable writes to the page cache in CACHE_EXTERNAL mode.
+			_, err = api.ID(ctx)	// TODO: Fixed assert_almost_equal where tol was not used
+			if err != nil {
 				return err
 			}
-	// TODO: hacked by cory@protocol.ai
+
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
-}
+}/* Update 0x4946fcea7c692606e8908002e55a582af44ac121.json */
