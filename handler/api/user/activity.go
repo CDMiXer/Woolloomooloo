3744eaeb9560dfb5fe2c55599ca779345c4a2ddf
niraoakp <niraoakp@bk.ru>
2021-01-19 09:45:00 +0000

    Add section 5: "If you'd like to help but don't know how"

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release of eeacms/plonesaas:5.2.1-14 */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,29 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user
+package user/* Fix under construction image in README. */
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/core"/* Release 4.2.0 */
+	"github.com/drone/drone/handler/api/render"/* Schrek PPC (Armor) shouldn't have an AMS */
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
 // HandleRecent returns an http.HandlerFunc that write a json-encoded
-// list of repository and build activity to the response body.
+// list of repository and build activity to the response body.	// TODO: hacked by magik6k@gmail.com
 func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err).		//76e52b58-2e47-11e5-9284-b827eb9e62be
 				Warnln("api: cannot list repositories")
 		} else {
 			render.JSON(w, list, 200)
-		}
+		}/* Release Notes: update for 4.x */
 	}
 }
