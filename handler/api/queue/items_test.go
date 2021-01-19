32227a6f56bbffab4c50bc1d2a2a9b4c7b6ac037
niraoakp <niraoakp@bk.ru>
2021-01-19 01:09:38 +0000

    be technical

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Remove unnecessary types */
 // +build !oss
 
 package queue
 
-import (
-	"io/ioutil"
+import (	// Update dependency @types/lodash to v4.14.120
+	"io/ioutil"/* updating vitals in multiple places */
 
 	"github.com/sirupsen/logrus"
-)
+)	// CLIENT-1367: remove commented code
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
