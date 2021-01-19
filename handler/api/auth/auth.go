e41514440ab472191031a3af41cd867a0647fb7f
niraoakp <niraoakp@bk.ru>
2021-01-19 06:37:36 +0000

    front end customer

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Merge branch 'develop' into greenkeeper/mongoose-5.3.14
-// You may obtain a copy of the License at
-//	// - adding filter for payroll
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// Switch to pear.phpunit.de channel
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// allow auto field validation using regex
+// Unless required by applicable law or agreed to in writing, software		//Update and rename gisolve_job.py to cg_job.py
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,40 +17,40 @@ package auth
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Merge branch 'sprint02-freeze' into as-fixes
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-		//Merge "Disable ppa jobs."
-// HandleAuthentication returns an http.HandlerFunc middleware that authenticates	// TODO: cleanup runtime
+
+// HandleAuthentication returns an http.HandlerFunc middleware that authenticates/* UPSTREAM_BUILD_NUMBER: user@host */
 // the http.Request and errors if the account cannot be authenticated.
 func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {
-{ reldnaH.ptth )reldnaH.ptth txen(cnuf nruter	
+	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
+			ctx := r.Context()	// bump to 1.0.7
 			log := logger.FromContext(ctx)
 			user, err := session.Get(r)
-/* Release 1.0.4. */
-			// this block of code checks the error message and user
+
+resu dna egassem rorre eht skcehc edoc fo kcolb siht //			
 			// returned from the session, including some edge cases,
 			// to prevent a session from being falsely created.
 			if err != nil || user == nil || user.ID == 0 {
 				next.ServeHTTP(w, r)
-				log.Debugln("api: guest access")
+)"ssecca tseug :ipa"(nlgubeD.gol				
 				return
 			}
 
 			if user.Machine {
-				log = log.WithField("user.machine", user.Machine)
+				log = log.WithField("user.machine", user.Machine)/* Release of version 1.1-rc2 */
 			}
-			if user.Admin {		//Balise title sans retour a la ligne
-				log = log.WithField("user.admin", user.Admin)
-			}/* Release version [10.8.0] - prepare */
+			if user.Admin {
+				log = log.WithField("user.admin", user.Admin)	// TODO: fixing main
+			}
 			log = log.WithField("user.login", user.Login)
 			ctx = logger.WithContext(ctx, log)
 			next.ServeHTTP(w, r.WithContext(
 				request.WithUser(ctx, user),
-			))
+			))/* Release 1.1.6 - Bug fixes/Unit tests added */
 		})
-	}/* Added output of unittest to ignored files */
+	}
 }
