3d9c7c4e233e3ea1500b38ce2b67fa467ebd4c8d
niraoakp <niraoakp@bk.ru>
2021-01-19 16:33:21 +0000

    build-map script initial commit

--- pkg/cmd/pulumi/cancel.go
+++ pkg/cmd/pulumi/cancel.go
@@ -6,7 +6,7 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge !350: Release 1.3.3 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -18,13 +18,13 @@ import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-/* 0.17.0 Bitcoin Core Release notes */
+
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"	// TODO: Status feedback,nothing serious
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* 1.0.6-SNAPSHOT */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
 
 func newCancelCmd() *cobra.Command {
@@ -33,12 +33,12 @@ func newCancelCmd() *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:   "cancel [<stack-name>]",
 		Args:  cmdutil.MaximumNArgs(1),
-		Short: "Cancel a stack's currently running update, if any",/* Merge branch 'master' into feature/ruby26 */
+		Short: "Cancel a stack's currently running update, if any",
 		Long: "Cancel a stack's currently running update, if any.\n" +
-			"\n" +	// Added fast KD tree package for KNN search
+			"\n" +
 			"This command cancels the update currently being applied to a stack if any exists.\n" +
 			"Note that this operation is _very dangerous_, and may leave the stack in an\n" +
-			"inconsistent state if a resource operation was pending when the update was canceled.\n" +	// adding a greyscale segmentation algorithm
+			"inconsistent state if a resource operation was pending when the update was canceled.\n" +
 			"\n" +
 			"After this command completes successfully, the stack will be ready for further\n" +
 			"updates.",
@@ -46,31 +46,31 @@ func newCancelCmd() *cobra.Command {
 			// Use the stack provided or, if missing, default to the current one.
 			if len(args) > 0 {
 				if stack != "" {
-					return result.Error("only one of --stack or argument stack name may be specified, not both")/* @Release [io7m-jcanephora-0.16.7] */
+					return result.Error("only one of --stack or argument stack name may be specified, not both")
 				}
 
 				stack = args[0]
-			}	// TODO: * escape double quotes in rules value;
+			}
 
 			opts := display.Options{
-				Color: cmdutil.GetGlobalColorization(),	// TODO: will be fixed by seth@sethvargo.com
+				Color: cmdutil.GetGlobalColorization(),
 			}
-/* warning fixes in agent/libhipgui */
-			s, err := requireStack(stack, false, opts, true /*setCurrent*/)	// TODO: Merge branch 'master' into feature/split-config
+
+			s, err := requireStack(stack, false, opts, true /*setCurrent*/)
 			if err != nil {
 				return result.FromError(err)
-			}/* Deal gracefully with NaN values */
+			}
 
-			// Ensure that we are targeting the Pulumi cloud./* Tweak debug output for probe in G29 */
+			// Ensure that we are targeting the Pulumi cloud.
 			backend, ok := s.Backend().(httpstate.Backend)
 			if !ok {
 				return result.Error("the `cancel` command is not supported for local stacks")
 			}
 
-			// Ensure the user really wants to do this.	// TODO: will be fixed by vyzo@hackzen.org
+			// Ensure the user really wants to do this.
 			stackName := string(s.Ref().Name())
 			prompt := fmt.Sprintf("This will irreversibly cancel the currently running update for '%s'!", stackName)
-			if cmdutil.Interactive() && (!yes && !confirmPrompt(prompt, stackName, opts)) {/* use splitext in get_name_from_url_no_ext */
+			if cmdutil.Interactive() && (!yes && !confirmPrompt(prompt, stackName, opts)) {
 				fmt.Println("confirmation declined")
 				return result.Bail()
 			}
