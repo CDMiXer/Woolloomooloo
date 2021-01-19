05baa87aaac2f1dcacb533e467521e3bab1e1b4d
niraoakp <niraoakp@bk.ru>
2021-01-19 04:55:09 +0000

    8a630684-2e4a-11e5-9284-b827eb9e62be

--- tests/integration/types/python/simple/__main__.py
+++ tests/integration/types/python/simple/__main__.py
@@ -4,19 +4,19 @@ from typing import Optional
 
 from pulumi import Input, InputType, Output, export, input_type, output_type, property
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-/* Se agrega proyecto ZF2 */
+
 
 @input_type
-class AdditionalArgs:		//[IMP] stato patrimoniale per la chiusura dei conti
+class AdditionalArgs:
     first_value: Input[str] = property("firstValue")
     second_value: Optional[Input[float]] = property("secondValue", default=None)
 
-@output_type	// TODO: will be fixed by admin@multicoin.co
+@output_type
 class Additional(dict):
     first_value: str = property("firstValue")
     second_value: Optional[float] = property("secondValue", default=None)
 
-current_id = 0	// Updated to direct use of vector
+current_id = 0
 
 class MyResourceProvider(ResourceProvider):
     def create(self, inputs):
@@ -34,27 +34,27 @@ class MyResource(Resource):
 # Create a resource with input object.
 res = MyResource("testres", additional=AdditionalArgs(first_value="hello", second_value=42))
 
-# Create a resource using the output object of another resource./* Code formatted, a warning removed */
-res2 = MyResource("testres2", additional=AdditionalArgs(		//Add QueryList widget from Fusion.
+# Create a resource using the output object of another resource.
+res2 = MyResource("testres2", additional=AdditionalArgs(
     first_value=res.additional.first_value,
     second_value=res.additional.second_value))
-/* Merge "[INTERNAL] Visual tests: Make tests mobile friendly" */
+
 # Create a resource using the output object of another resource, accessing the output as a dict.
 res3 = MyResource("testres3", additional=AdditionalArgs(
     first_value=res.additional["first_value"],
     second_value=res.additional["second_value"]))
 
 # Create a resource using a dict as the input.
-# Note: These are camel case (not snake_case) since the resource does not do any translation of	// Merge "Use ubuntu-trusty for releasenotes jobs"
+# Note: These are camel case (not snake_case) since the resource does not do any translation of
 # property names.
 res4 = MyResource("testres4", additional={
     "firstValue": "hello",
     "secondValue": 42,
-})/* Released DirectiveRecord v0.1.13 */
+})
 
-export("res_first_value", res.additional.first_value)/* [OptionsResolver] remove duplicate deprecation text */
+export("res_first_value", res.additional.first_value)
 export("res_second_value", res.additional.second_value)
-export("res2_first_value", res2.additional.first_value)/* cdf9aa06-2e68-11e5-9284-b827eb9e62be */
+export("res2_first_value", res2.additional.first_value)
 export("res2_second_value", res2.additional.second_value)
 export("res3_first_value", res3.additional.first_value)
 export("res3_second_value", res3.additional.second_value)
