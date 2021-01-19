f7063ce30ee7a55e3cc4d9a703a40113cc67ce14
niraoakp <niraoakp@bk.ru>
2021-01-19 05:52:32 +0000

    Merge "auto-generate object docs"

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
 
 import "github.com/libp2p/go-libp2p-core/peer"
-	// TODO: hacked by arachnid@notdot.net
-type BootstrapPeers []peer.AddrInfo/* Update spamemails.txt */
-type DrandBootstrap []peer.AddrInfo
-
+/* Release areca-7.0.5 */
+type BootstrapPeers []peer.AddrInfo
+type DrandBootstrap []peer.AddrInfo/* Release tag: 0.7.2. */
+/* Release 11.1 */
 type Bootstrapper bool
