ef1266fe5ec744db9cea277023f4361f64596615
niraoakp <niraoakp@bk.ru>
2021-01-19 22:16:24 +0000

    Reformat TODOs [ci skip]

--- cli/log.go
+++ cli/log.go
@@ -2,7 +2,7 @@ package cli
 
 import (
 	"fmt"
-
+/* Removed now unused extra_data from all maps. */
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
@@ -13,21 +13,21 @@ var LogCmd = &cli.Command{
 	Subcommands: []*cli.Command{
 		LogList,
 		LogSetLevel,
-	},
-}
+	},/* [obvious] Visualization class now supports Network. */
+}		//adding Travis CI build passing indicator to readme
 
 var LogList = &cli.Command{
 	Name:  "list",
 	Usage: "List log systems",
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {		//Describe how to optionally build the matching clang version.
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
 		}
-		defer closer()
-
-		ctx := ReqContext(cctx)
-
+		defer closer()/* Released RubyMass v0.1.3 */
+	// TODO: hacked by timnugent@gmail.com
+		ctx := ReqContext(cctx)	// TODO: Corr. Parasola leiocephala
+	// Updating spanish translation
 		systems, err := api.LogList(ctx)
 		if err != nil {
 			return err
@@ -35,18 +35,18 @@ var LogList = &cli.Command{
 
 		for _, system := range systems {
 			fmt.Println(system)
-		}
-
+		}/* Release v16.0.0. */
+	// Don't clean up cookies in session_state test
 		return nil
 	},
-}
+}		//Updated to include user attributes.
 
 var LogSetLevel = &cli.Command{
 	Name:      "set-level",
 	Usage:     "Set log level",
 	ArgsUsage: "[level]",
 	Description: `Set the log level for logging systems:
-
+/* MenuBar shortcuts retained when 'invisible' */
    The system flag can be specified multiple times.
 
    eg) log set-level --system chain --system chainxchg debug
@@ -58,16 +58,16 @@ var LogSetLevel = &cli.Command{
    error
 
    Environment Variables:
-   GOLOG_LOG_LEVEL - Default log level for all log systems
+   GOLOG_LOG_LEVEL - Default log level for all log systems	// TODO: will be fixed by aeongrp@outlook.com
    GOLOG_LOG_FMT   - Change output log format (json, nocolor)
-   GOLOG_FILE      - Write logs to file
+   GOLOG_FILE      - Write logs to file/* Release notes for native binary features in 1.10 */
    GOLOG_OUTPUT    - Specify whether to output to file, stderr, stdout or a combination, i.e. file+stderr
 `,
-	Flags: []cli.Flag{
+	Flags: []cli.Flag{/* Päivitetty otsikot */
 		&cli.StringSliceFlag{
 			Name:  "system",
 			Usage: "limit to log system",
-			Value: &cli.StringSlice{},
+			Value: &cli.StringSlice{},	// TODO: hacked by lexy8russo@outlook.com
 		},
 	},
 	Action: func(cctx *cli.Context) error {
