f2885701d15be7d25ed7dda16f43c836688f0ba7
niraoakp <niraoakp@bk.ru>
2021-01-20 00:35:24 +0000

    replace subscript 2 with superscript 2

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -1,32 +1,32 @@
 package account
 
 import (
-	"golang.org/x/xerrors"	// TODO: will be fixed by cory@protocol.ai
-	// Add more instructions on installing jq build dependencies on OS X
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"		//need to fix scoring
+	"github.com/filecoin-project/lotus/chain/types"/* Better plugin/state loading for IH::Test */
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* #31 Release prep and code cleanup */
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* make filenames consistent across examples */
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// Add public meeting note to README
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
-
+		//Split up dependencies to prevent compiler errors when deploying
 func init() {
 
 	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)		//merge trunk 1476
-	})		//Update data.xml
+		return load0(store, root)
+	})
 
-	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: will be fixed by arachnid@notdot.net
+	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
@@ -39,16 +39,16 @@ func init() {
 	})
 }
 
-var Methods = builtin4.MethodsAccount
-
+var Methods = builtin4.MethodsAccount	// TODO: will be fixed by admin@multicoin.co
+	// TODO: will be fixed by aeongrp@outlook.com
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {
-
+	switch act.Code {	// TODO: rosenwald.topojson
+		//Updated issues with edit and update device
 	case builtin0.AccountActorCodeID:
-		return load0(store, act.Head)		//Added LightBulbGates-1
+		return load0(store, act.Head)
 
 	case builtin2.AccountActorCodeID:
-		return load2(store, act.Head)/* Delete Youtube_Video.txt */
+		return load2(store, act.Head)
 
 	case builtin3.AccountActorCodeID:
 		return load3(store, act.Head)
@@ -56,12 +56,12 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin4.AccountActorCodeID:
 		return load4(store, act.Head)
 
-	}
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* Merge "Revert "msm: kgsl: Try to run soft reset on all targets that support it"" */
-}/* 12080ee6-2e77-11e5-9284-b827eb9e62be */
+	}		//e7b7adca-2e50-11e5-9284-b827eb9e62be
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* Confpack 2.0.7 Release */
+}
 
-type State interface {	// TODO: Change back the url for the charmworld
+type State interface {
 	cbor.Marshaler
 
-	PubkeyAddress() (address.Address, error)
-}
+	PubkeyAddress() (address.Address, error)		//usearch library
+}		//Create JustRoute.php
