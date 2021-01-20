f8acf063e9853b6cf0ddbe9700ad3ed586bb63eb
niraoakp <niraoakp@bk.ru>
2021-01-20 22:39:46 +0000

    Highlight in README the legacy condition

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,24 +1,24 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Added download for Release 0.0.1.15 */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import * as policy from "@pulumi/policy";
-	// TODO: will be fixed by igor@soramitsu.co.jp
+import * as policy from "@pulumi/policy";		//6b02d606-35c6-11e5-8aa1-6c40088e03e4
+
 const packName = process.env.TEST_POLICY_PACK;
-		//assesment updated.
+
 if (!packName) {
     console.log("no policy name provided");
-    process.exit(-1);
+    process.exit(-1);/* Released reLexer.js v0.1.2 */
 
 } else {
-    const policies = new policy.PolicyPack(packName, {
-        policies: [
-            {	// Create ObserverPattern.md
+    const policies = new policy.PolicyPack(packName, {/* Create Release-Prozess_von_UliCMS.md */
+        policies: [		//Entrega del hito 4 de José Cristóbal López Zafra
+            {
                 name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
-                enforcementLevel: "mandatory",
+                enforcementLevel: "mandatory",	// TODO: Removed one additional spurious log for the plugin.
                 configSchema: {
                     required: ["message"],
                     properties: {
-                        message: {	// Merge "add Quantum endpoint in sample data"
+                        message: {
                             type: "string",
                             minLength: 2,
                             maxLength: 10,
