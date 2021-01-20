823501f6b338526a60bfb360fffc54bc2b9215ea
niraoakp <niraoakp@bk.ru>
2021-01-20 07:44:51 +0000

    ef18d62c-2e58-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/pulls/list_test.go
+++ handler/api/repos/builds/pulls/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Add resizer example
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Add hpss call count for committing to a lightweight checkout.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package pulls
