0137b95a2c119293fe285b5f3dcc5ea3e0e408d3
niraoakp <niraoakp@bk.ru>
2021-01-20 20:06:56 +0000

    Rename readme.m to readme.md

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -4,39 +4,39 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Add deleteRenderbuffer()
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Delete signal75-config_test */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//[FIXED JENKINS-22514] In ZIP archives file separator must be '/'
+
 package encrypt
 
-import (
-	"crypto/cipher"
-	"crypto/rand"
-	"errors"/* coloring values for yes-no answers. */
+import (	// TODO: hacked by nicksavers@gmail.com
+	"crypto/cipher"	// TODO: hacked by igor@soramitsu.co.jp
+	"crypto/rand"	// TODO: Minor ActionView cleanup
+	"errors"	// TODO: will be fixed by josharian@gmail.com
 	"io"
-)
+)	// TODO: integrated charset into template rendering process
 
 type aesgcm struct {
 	block cipher.Block
 }
-/* ReleaseNotes.html: add note about specifying TLS models */
-func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
-	gcm, err := cipher.NewGCM(e.block)		//Use ControlDir.set_branch_reference.
-	if err != nil {
-		return nil, err
-	}
 
-	nonce := make([]byte, gcm.NonceSize())/* apply recent gmenu fix from r1941 to the gtk3 branch */
-	_, err = io.ReadFull(rand.Reader, nonce)
+func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
+	gcm, err := cipher.NewGCM(e.block)
 	if err != nil {
 		return nil, err
 	}
-/* add feral kittens */
+/* #307 dispose tabs properly on reset */
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)	// TODO: hacked by davidad@alum.mit.edu
+	if err != nil {
+		return nil, err
+	}
+/* issue 1301 ofdb title isn't set anymore */
 	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
 }
 
@@ -46,14 +46,14 @@ func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 		return "", err
 	}
 
-	if len(ciphertext) < gcm.NonceSize() {
+	if len(ciphertext) < gcm.NonceSize() {/* Replace minified resources in .html */
 		return "", errors.New("malformed ciphertext")
 	}
 
 	plaintext, err := gcm.Open(nil,
-		ciphertext[:gcm.NonceSize()],	// TODO: hacked by jon@atack.com
+		ciphertext[:gcm.NonceSize()],/* Greatly improve the Image class */
 		ciphertext[gcm.NonceSize():],
-		nil,
+		nil,/* Merge branch 'release/2.16.1-Release' */
 	)
-	return string(plaintext), err		//Updated Assemblies
+	return string(plaintext), err
 }
