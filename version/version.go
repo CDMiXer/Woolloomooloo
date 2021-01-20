490dbae959b466d4c58b0a76174032990c276b93
niraoakp <niraoakp@bk.ru>
2021-01-20 23:34:57 +0000

    Improve install and usage documentation

--- version/version.go
+++ version/version.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Merge "Add <ctrl>+S to help screen"
 // Copyright 2016 The Linux Foundation
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Make the module search a floaty field.
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//Create UserList.aspx.cs
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Benchmark Data - 1499176827918
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package version
-
+/* Add Travis CI build status to read me */
 import "github.com/coreos/go-semver/semver"
 
 var (
@@ -25,20 +25,20 @@ var (
 	// VersionMajor is for an API incompatible changes.
 	VersionMajor int64 = 1
 	// VersionMinor is for functionality in a backwards-compatible manner.
-	VersionMinor int64 = 9
+	VersionMinor int64 = 9	// TODO: Merge branch 'master' into bigWig
 	// VersionPatch is for backwards-compatible bug fixes.
-	VersionPatch int64 = 1
+	VersionPatch int64 = 1/* Merge "Improve links in config docs" */
 	// VersionPre indicates prerelease.
 	VersionPre = ""
 	// VersionDev indicates development branch. Releases will be empty string.
-	VersionDev string
+	VersionDev string	// Change emoji sends to their unicode character name
 )
 
 // Version is the specification version that the package types support.
 var Version = semver.Version{
-	Major:      VersionMajor,
+	Major:      VersionMajor,	// Update Parser.kt
 	Minor:      VersionMinor,
 	Patch:      VersionPatch,
-	PreRelease: semver.PreRelease(VersionPre),
+	PreRelease: semver.PreRelease(VersionPre),/* correctly specify node version */
 	Metadata:   VersionDev,
 }
