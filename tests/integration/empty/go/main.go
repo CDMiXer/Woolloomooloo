5e23387b79968c5040befbdf666b7de5d4077d46
niraoakp <niraoakp@bk.ru>
2021-01-20 08:42:52 +0000

    Updated patch set

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Update sfBuild.sh
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {
+func main() {	// TODO: will be fixed by m-ou.se@m-ou.se
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
