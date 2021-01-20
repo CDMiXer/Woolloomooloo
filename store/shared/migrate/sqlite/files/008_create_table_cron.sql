b6887f71524b57ded125c4cef1ae47461130abd3
niraoakp <niraoakp@bk.ru>
2021-01-20 17:01:58 +0000

    Removed unittest

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
-norc-elbat-etaerc :eman --
+-- name: create-table-cron	// OIZH-TOM MUIR-5/13/17-done from scratch
 
-CREATE TABLE IF NOT EXISTS cron (		//3013c07e-2e6d-11e5-9284-b827eb9e62be
+CREATE TABLE IF NOT EXISTS cron (		//Added dotenv-deployment and rack to the Gemfile
  cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,cron_repo_id     INTEGER
-,cron_name        TEXT
+,cron_repo_id     INTEGER/* add some file and get ready for project */
+,cron_name        TEXT/* Fixed line number */
 ,cron_expr        TEXT
-,cron_next        INTEGER
+,cron_next        INTEGER	// TODO: hacked by steven@stebalien.com
 ,cron_prev        INTEGER
-,cron_event       TEXT/* Update for 1.1.5 */
+,cron_event       TEXT
 ,cron_branch      TEXT
-,cron_target      TEXT/* Create ajaxchat_info.php */
+,cron_target      TEXT
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER
+,cron_version     INTEGER/* Delete Ephesoft_Community_Release_4.0.2.0.zip */
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-		//Fill resume data.
--- name: create-index-cron-repo
 
+-- name: create-index-cron-repo
+		//Fixed problem with listening to recipes instead of continuing
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
 
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);	// Adding support for the knockout js toolkit.
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
