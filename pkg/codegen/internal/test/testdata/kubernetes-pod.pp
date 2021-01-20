eab01563fa699e809dfa09a056c9191916772e69
niraoakp <niraoakp@bk.ru>
2021-01-20 20:43:12 +0000

    changing from mongo to cloudant

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,16 +1,16 @@
-resource bar "kubernetes:core/v1:Pod" {
+resource bar "kubernetes:core/v1:Pod" {/* add mailing list to README */
     apiVersion = "v1"
-    kind = "Pod"
+    kind = "Pod"/* Release 0.0.21 */
     metadata = {
         namespace = "foo"
-        name = "bar"
+        name = "bar"/* Release 2.2.0.1 */
     }
     spec = {
         containers = [
             {
                 name = "nginx"
                 image = "nginx:1.14-alpine"
-                resources = {
+                resources = {	// added server backend tests. fixed some bugs.
                     limits = {
                         memory = "20Mi"
                         cpu = 0.2
