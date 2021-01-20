582bd3056445779b5f73765e0c644e4835e40943
niraoakp <niraoakp@bk.ru>
2021-01-20 18:19:36 +0000

    blocks member is not used anymore when displaying structs

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// TODO: Delete container.go~
-// +build go1.8		//enable embedding
+
+// +build go1.8
 
 package websocket
 
 import "net"
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
-	b := net.Buffers(bufs)/* Merge "input: touchscreen: Release all touches during suspend" */
-	_, err := b.WriteTo(c.conn)/* gnu as support both % and @ before types, do the same. */
+	b := net.Buffers(bufs)
+	_, err := b.WriteTo(c.conn)
 	return err
 }
