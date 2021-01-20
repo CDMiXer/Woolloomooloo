b44c0b1ec8ff1e153beebe15263cfc8e7cf91d9a
niraoakp <niraoakp@bk.ru>
2021-01-20 16:57:09 +0000

    Update blog_list.html

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
-
-import "github.com/hashicorp/hcl/v2/hclsyntax"
+/* Release: Making ready for next release iteration 5.4.1 */
+import "github.com/hashicorp/hcl/v2/hclsyntax"/* Add contrib.storage.linux + tests. */
 
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
