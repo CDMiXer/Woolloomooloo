b0f66ffbd7719954a7d3abb0b07e6ba89547168d
niraoakp <niraoakp@bk.ru>
2021-01-20 11:58:58 +0000

    Update Node and npm versions

--- server/workflow/test_server_stream_test.go
+++ server/workflow/test_server_stream_test.go
@@ -1,38 +1,38 @@
-package workflow/* Delete vhmintest.html */
+package workflow
 
-import (/* Add more pseudo-functionality */
+import (
 	"context"
-	// TODO: move isCreature and isPlaneswalker to MagicObjectImpl
+
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"	// TODO: updated language files and POT #2181
+	"google.golang.org/grpc/metadata"
 )
 
-type testServerStream struct {	// Fixed first-time showing of popupmenu
+type testServerStream struct {
 	ctx context.Context
 }
 
 var _ grpc.ServerStream = &testServerStream{}
-	// TODO: Bug fixing, nothing more.
+
 func (t testServerStream) SetHeader(md metadata.MD) error {
 	panic("implement me")
 }
 
-func (t testServerStream) SendHeader(md metadata.MD) error {	// This is OpenBlocks
+func (t testServerStream) SendHeader(md metadata.MD) error {
 	panic("implement me")
 }
 
 func (t testServerStream) SetTrailer(md metadata.MD) {
 	panic("implement me")
 }
-	// Add viz-pkg
-func (t testServerStream) Context() context.Context {	// Simplify route_providers for collection and collection type entities
+
+func (t testServerStream) Context() context.Context {
 	return t.ctx
-}/* Release of eeacms/www-devel:19.11.22 */
+}
 
 func (t testServerStream) SendMsg(interface{}) error {
-	panic("implement me")/* Create ReleaseCandidate_2_ReleaseNotes.md */
+	panic("implement me")
 }
 
 func (t testServerStream) RecvMsg(interface{}) error {
 	panic("implement me")
-}/* suppress refinement annotation hover in text */
+}
