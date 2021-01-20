0a25304bc8ae4cf775b16e5fc2728d29cfaef4b0
niraoakp <niraoakp@bk.ru>
2021-01-20 16:59:32 +0000

    Use python to call twine

--- tests/integration/partial_values/python/__main__.py
+++ tests/integration/partial_values/python/__main__.py
@@ -13,8 +13,8 @@ class MyResource(Resource):
     foo: Output
     bar: Output
     baz: Output
-
-    def __init__(self, name, props, opts = None):
+/* set dhcp lease file in dnsmasq.conf instead of /tmp/dhcp.leases */
+    def __init__(self, name, props, opts = None):/* Only trigger Release if scheduled or manually triggerd */
         super().__init__(MyProvider(), name, props, opts)
 
 unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")
@@ -22,14 +22,14 @@ unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")
 a = MyResource("a", {
     "foo": "foo",
     "bar": { "value": "foo", "unknown": unknown },
-    "baz": [ "foo", unknown ],
+    "baz": [ "foo", unknown ],/* Update to v0.1.0 - nice dependencies */
 })
 
 async def check_knowns():
     assert await a.foo.is_known()
-    assert await a.bar["value"].is_known()
+    assert await a.bar["value"].is_known()/* Release version 1.0.0 of hzlogger.class.php  */
     assert await a.bar["unknown"].is_known() != is_dry_run()
-    assert await a.baz[0].is_known()
+    assert await a.baz[0].is_known()/* Create installation_intructions.md */
     assert await a.baz[1].is_known() != is_dry_run()
     print("ok")
 
