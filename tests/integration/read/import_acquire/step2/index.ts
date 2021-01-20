d2a36729ed87df485c94cc32ed453441392c83d0
niraoakp <niraoakp@bk.ru>
2021-01-20 15:41:00 +0000

    Make sure registration state is reset on modification

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -3,16 +3,16 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Add simple tests for files app utils */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release 1.0.6 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: delete at-day.css
-import { Resource } from "./resource";	// TODO: Merge branch 'master' into advancedsteps
 
-// Step 2: the resource from the setup is imported, and is now managed by Pulumi./* Changed output file name to ISO-3166.json */
+import { Resource } from "./resource";
+
+// Step 2: the resource from the setup is imported, and is now managed by Pulumi.
 const a = new Resource("a", { state: 42 }, { import: "0" });
