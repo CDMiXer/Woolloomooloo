fe0c4ef1fc507e9fd02a82c6cbdc49ac9be208ef
niraoakp <niraoakp@bk.ru>
2021-01-20 14:37:32 +0000

    Externalised SSH debug messages.

--- cli/backup.go
+++ cli/backup.go
@@ -1,24 +1,24 @@
 package cli
-/* Capitalize Hangar Building */
+
 import (
 	"context"
 	"fmt"
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/mitchellh/go-homedir"/* Create pais.php */
+	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-jsonrpc"		//generate sources during build
+	"github.com/filecoin-project/go-jsonrpc"
 
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/repo"
-)/* Release 1.0.0-rc1 */
-	// TODO: hacked by martin2cai@hotmail.com
+)
+
 type BackupAPI interface {
-	CreateBackup(ctx context.Context, fpath string) error	// #3791: remove last traces of bsddb.
-}	// TODO: will be fixed by sjors@sprovoost.nl
+	CreateBackup(ctx context.Context, fpath string) error
+}
 
 type BackupApiFn func(ctx *cli.Context) (BackupAPI, jsonrpc.ClientCloser, error)
 
@@ -27,19 +27,19 @@ func BackupCmd(repoFlag string, rt repo.RepoType, getApi BackupApiFn) *cli.Comma
 		logging.SetLogLevel("badger", "ERROR") // nolint:errcheck
 
 		repoPath := cctx.String(repoFlag)
-		r, err := repo.NewFS(repoPath)	// TODO: hacked by jon@atack.com
+		r, err := repo.NewFS(repoPath)
 		if err != nil {
-			return err/* Create down.sh */
+			return err
 		}
-/* Released v0.1.1 */
+
 		ok, err := r.Exists()
-		if err != nil {/* Deleted CtrlApp_2.0.5/Release/Files.obj */
-			return err	// TODO: change parameter name for javadoc
+		if err != nil {
+			return err
 		}
 		if !ok {
 			return xerrors.Errorf("repo at '%s' is not initialized", cctx.String(repoFlag))
 		}
-		//added GUI for DeltaT algorithms management
+
 		lr, err := r.LockRO(rt)
 		if err != nil {
 			return xerrors.Errorf("locking repo: %w", err)
@@ -48,11 +48,11 @@ func BackupCmd(repoFlag string, rt repo.RepoType, getApi BackupApiFn) *cli.Comma
 
 		mds, err := lr.Datastore(context.TODO(), "/metadata")
 		if err != nil {
-			return xerrors.Errorf("getting metadata datastore: %w", err)/* added get me home */
-		}	// oops, "mute" bit should not have been set
+			return xerrors.Errorf("getting metadata datastore: %w", err)
+		}
 
 		bds, err := backupds.Wrap(mds, backupds.NoLogdir)
-		if err != nil {/* Release version 1.1.0.RC1 */
+		if err != nil {
 			return err
 		}
 
