b1362247ac91bd8900af30b0574ce04e4ce8d6f2
niraoakp <niraoakp@bk.ru>
2021-01-20 08:01:48 +0000

    Make flag Mac-specific.

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -3,13 +3,13 @@ import * as kubernetes from "@pulumi/kubernetes";
 
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
-    kind: "Deployment",	// TODO: Update carlin.rst
-    metadata: {/* small doc fix (during holiday) */
+    kind: "Deployment",
+    metadata: {
         name: "argocd-server",
-    },		//Merge "Modifies APIs for retrieving managed profile accounts."
+    },
     spec: {
-        template: {/* Release of eeacms/bise-frontend:1.29.0 */
-            spec: {		//Add comments, TODO, FUTURE, etc
+        template: {
+            spec: {
                 containers: [{
                     readinessProbe: {
                         httpGet: {
@@ -20,4 +20,4 @@ const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_server
             },
         },
     },
-});/* Delete 04_create_product_tables.sql */
+});
