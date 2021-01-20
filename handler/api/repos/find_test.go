294d9e0c8e13d3c5f328db0e3ffc3f3b5697daea
niraoakp <niraoakp@bk.ru>
2021-01-20 13:45:06 +0000

    Fix typo in README.MD

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by mail@bitpshr.net
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Field names for cycles and boost cycles in portal frames
 
 package repos
 
@@ -16,7 +16,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* [artifactory-release] Release version 1.2.0.BUILD-SNAPSHOT */
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -24,9 +24,9 @@ func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
-var (
+var (/* Delete AIF Framework Release 4.zip */
 	mockRepo = &core.Repository{
-		ID:        1,
+		ID:        1,/* Updated ReleaseNotes. */
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
@@ -34,7 +34,7 @@ var (
 		Branch:    "master",
 	}
 
-	mockRepos = []*core.Repository{
+	mockRepos = []*core.Repository{	// TODO: Système de calcul "Phase de Thal" (script) 
 		{
 			ID:        1,
 			Namespace: "octocat",
@@ -44,7 +44,7 @@ var (
 		{
 			ID:        1,
 			Namespace: "octocat",
-			Name:      "spoon-knife",
+			Name:      "spoon-knife",	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 			Slug:      "octocat/spoon-knife",
 		},
 	}
@@ -53,24 +53,24 @@ var (
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Lots of work done - tested script running and file reading remote  */
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
-	r = r.WithContext(request.WithRepo(
-		context.Background(), mockRepo,
+	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)		//Merge "Change from unittest to testtools"
+	r = r.WithContext(request.WithRepo(	// TODO: Fonts, Includes: Added cleanup() to directly call this process.
+		context.Background(), mockRepo,/* 726a5bb4-2e49-11e5-9284-b827eb9e62be */
 	))
 
 	router := chi.NewRouter()
 	router.Get("/api/repos/{owner}/{name}", HandleFind())
 	router.ServeHTTP(w, r)
-
+		//rev 569952
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(core.Repository), mockRepo
+	got, want := new(core.Repository), mockRepo/* better logger, with file handle. */
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)/* b31e549c-2e46-11e5-9284-b827eb9e62be */
+	}	// Add userinfo
 }
