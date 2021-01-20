849f0bc849e009446ed20456fdf52ec70e7db415
niraoakp <niraoakp@bk.ru>
2021-01-20 02:43:32 +0000

    Merge "Show checks table upon clicking state chip"

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -3,13 +3,13 @@
 from typing import Any, Optional
 
 import pulumi
-
+/* Added more arithmetic specs */
 class Component(pulumi.ComponentResource):
-    echo: pulumi.Output[Any]
+    echo: pulumi.Output[Any]/* FilterConfig: move code to filter_chain_append_new() */
     childId: pulumi.Output[str]
 
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
         props = dict()
-        props["echo"] = echo
+        props["echo"] = echo/* Checked out path didn't work, doing the lazy way */
         props["childId"] = None
         super().__init__("testcomponent:index:Component", name, props, opts, True)
