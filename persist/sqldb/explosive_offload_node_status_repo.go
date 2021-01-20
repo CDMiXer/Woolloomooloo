181bb515682bffa175295f97637eca0598b9e232
niraoakp <niraoakp@bk.ru>
2021-01-20 10:30:00 +0000

    Add reference to R-Pi requirements.

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,37 +1,37 @@
-package sqldb		//Added note about deadlock
-
-import (
+package sqldb
+/* Made Release Notes link bold */
+import (		//Merge "Don't call closeNavigation() on touchend if no menu closed"
 	"fmt"
-
+	// TODO: will be fixed by aeongrp@outlook.com
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)/* Delete model_generator/point_dispersion/text.csv */
+)
 
-var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
-var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")	// TextComponentValue is now replaced by DocumentTextProperty.
-
+var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}/* Added new columns to table linked_core_projects */
+var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
+/* Released reLexer.js v0.1.2 */
 type explosiveOffloadNodeStatusRepo struct {
 }
-
-func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {	// TODO: will be fixed by martin2cai@hotmail.com
+	// TODO: refactoring: splitted iterations number test for PPI
+func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
-}	// TODO: hacked by joshua@yottadb.com
+}
 
 func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
-	return "", OffloadNotSupportedError	// TODO: will be fixed by timnugent@gmail.com
+	return "", OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
-}/* fixed a couple triggerEvent calls that were passing target as first param */
-
-func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {	// Release 0.64
-	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {/* 47dde8f6-2e59-11e5-9284-b827eb9e62be */
-	return OffloadNotSupportedError
+func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
+	return nil, OffloadNotSupportedError/* Release Notes 3.5 */
+}
+/* Añadido war a la raíz */
+func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
+	return OffloadNotSupportedError/* Rename README_stream to README_stream.md */
 }
 
-func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
-	return nil, OffloadNotSupportedError
-}
+func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {/* Release version: 2.0.2 [ci skip] */
+	return nil, OffloadNotSupportedError/* Fix to Compare and Equal is broken for Value types and IE11 */
+}/* Improve ReadMe to have more relevant info */
