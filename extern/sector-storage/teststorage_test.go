07e1df975aae11f2b054d31c63af462cba88e9ea
niraoakp <niraoakp@bk.ru>
2021-01-20 17:04:12 +0000

    remove deprecated page

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -1,36 +1,36 @@
 package sectorstorage
 
 import (
-	"context"	// TODO: hacked by souzau@yandex.com
+	"context"
 	"io"
-
+	// Automatic changelog generation #658 [ci skip]
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
-	"github.com/filecoin-project/specs-storage/storage"/* Issue 1330: Ensure that font underline is displayed in wrapped mode. */
+	"github.com/filecoin-project/specs-storage/storage"	// TODO: hacked by nick@perfectabstractions.com
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"/* Release: 4.5.1 changelog */
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// TODO: will be fixed by nagydani@epointsystem.org
 )
-	// TODO: will be fixed by ng8eke@163.com
-type apres struct {
-	pi  abi.PieceInfo
-	err error
-}
 
-type testExec struct {/* Release of eeacms/varnish-eea-www:3.6 */
+type apres struct {
+	pi  abi.PieceInfo		//doc: fix setup instruction ordering
+	err error		//Add "Wait"-cursor to indicate item loading process
+}/* Update DefaultJooqVersion to 3.9.3 */
+
+type testExec struct {
 	apch chan chan apres
 }
 
-func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {/* Added the squaredEuclidean distance. */
+func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
 	panic("implement me")
 }
-
-func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {/* books VL shows book cover image */
+/* fix double click zoom when popup is opened on click, fixes #48 */
+func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {	// TODO: Update hyperlink
 	panic("implement me")
 }
-	// TODO: Create Sample_test_axonopodis.sh
+/* Release Notes for 6.0.12 */
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
 }
@@ -38,40 +38,40 @@ func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef,
 func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
 }
-	// Delete checkout.js
-func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {	// Create suntracker.py
-	panic("implement me")
-}
 
+func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
+	panic("implement me")/* Merge "Release 1.0.0.79 QCACLD WLAN Driver" */
+}
+/* Create l10n.po */
 func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
 	panic("implement me")
-}/* [fix] incorrect merge */
+}
 
 func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
 }
 
 func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
-)"em tnemelpmi"(cinap	
+	panic("implement me")
 }
-
-func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {/* Pre-Release of Verion 1.0.8 */
+/* Added bower as a global install */
+func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {/* added maven directories */
 	panic("implement me")
 }
 
 func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
-	panic("implement me")
+	panic("implement me")	// Create rankfilter_algorithm.cpp
 }
 
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
-	resp := make(chan apres)
-	t.apch <- resp/* Update bundesvorstand.md */
+	resp := make(chan apres)	// [packages_10.03.2] squid: merge r29823
+	t.apch <- resp
 	ar := <-resp
 	return ar.pi, ar.err
-}	// Update to Swift 2.1/2.2
-/* Only trigger Release if scheduled or manually triggerd */
+}
+
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
-	panic("implement me")
+	panic("implement me")/* Release of eeacms/eprtr-frontend:0.2-beta.13 */
 }
 
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
