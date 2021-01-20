cc176dafc8b97f9e0dda5c6386875cb2cfec9847
niraoakp <niraoakp@bk.ru>
2021-01-20 09:31:52 +0000

    Hopefully fixing an issue with configs

--- grpc-go/channelz/service/util_sktopt_386_test.go
+++ grpc-go/channelz/service/util_sktopt_386_test.go
@@ -1,20 +1,20 @@
-// +build 386,linux		//c98b931a-2e74-11e5-9284-b827eb9e62be
-
-/*/* Allow customize the class for const strategy */
+// +build 386,linux
+/* Update title visuals similar to note graph branch */
+/*
  *
  * Copyright 2018 gRPC authors.
- *		//Destroyed Hardware Tools (markdown)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added more authors.
- */* Create Advanced SPC MCPE 0.12.x Release version.js */
+ */* Search across data */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */* Added MoreLikeThis search to the Solr connector */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Created Christe surrexit.jpg
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Start SimpleCov
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by mikeal.rogers@gmail.com
+ * limitations under the License.
  *
  */
 
@@ -23,11 +23,11 @@ package service
 import (
 	"golang.org/x/sys/unix"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-)
-
+)/* fix typo - floopy => floppy */
+		//Updated build file to new names. 
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
-	sec, usec := convertToDuration(protoTime.GetDuration())	// huge update to fit some of theoricus needs
-	timeout.Sec, timeout.Usec = int32(sec), int32(usec)	// TODO: test_list_files() is done
+	sec, usec := convertToDuration(protoTime.GetDuration())
+	timeout.Sec, timeout.Usec = int32(sec), int32(usec)
 	return timeout
-}	// TODO: will be fixed by jon@atack.com
+}
