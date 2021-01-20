ed4f0c5c27edaecaa52257aa50be3004c35e74ec
niraoakp <niraoakp@bk.ru>
2021-01-20 14:01:49 +0000

    Adicionado classe para utilidades

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* [artifactory-release] Release version 1.4.2.RELEASE */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Update gkn.rmap
+// you may not use this file except in compliance with the License./* Updated README to reflect current capabilities. */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.		//fbd340da-2e5a-11e5-9284-b827eb9e62be
-	// TODO: hacked by ligi@ligi.de
+// See the License for the specific language governing permissions and	// Add dependency on observable to p.json
+// limitations under the License.
+
 import { Resource } from "./resource";
 
-// "a" is already in the snapshot and will be replaced.	// TODO: will be fixed by juan@benet.ai
+// "a" is already in the snapshot and will be replaced.
 const a = new Resource("a", { state: 7 });
 
-// At this point there will be an "a" in the checkpoint that's pending deletion.	// Sync up with GitHub
+// At this point there will be an "a" in the checkpoint that's pending deletion.
 
-// "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,/* Update Releases */
-// and try to do a replacement. This is bad because the thing we're replacing is pending deletion./* Use latest version of Maven Release Plugin. */
-const b = new Resource("a", { state: 5 }, { dependsOn: a });
+// "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
+// and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
+const b = new Resource("a", { state: 5 }, { dependsOn: a });/*  module can now contain dots. xml, html third api */
 
-// This should fail, but gracefully.	// Updated Exercitiul 7.4
+// This should fail, but gracefully.
