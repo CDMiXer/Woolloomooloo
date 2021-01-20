d31041140ab328117cfccdfdd0d8720363c1de71
niraoakp <niraoakp@bk.ru>
2021-01-20 12:10:33 +0000

    added missing * in cache ignores

--- cmd/drone-server/inject_store.go
+++ cmd/drone-server/inject_store.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by steven@stebalien.com
-///* Release jedipus-2.6.26 */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* 5.2.3 Release */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//2773c9c8-2e68-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// [TASK] Define bin in composer.json
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Reduce bitlength requirement for residue calculation */
-// limitations under the License.		//linappleGenerator.py: don't symlink but copy, inform upgrade is ok
-/* Added 0.9.7 to "Releases" and "What's new?" in web-site. */
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
-/* 0.9.2 Release. */
+
 import (
-	"github.com/drone/drone/cmd/drone-server/config"/* doc link fix */
+	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/metric"
 	"github.com/drone/drone/store/batch"
@@ -23,24 +23,24 @@ import (
 	"github.com/drone/drone/store/build"
 	"github.com/drone/drone/store/cron"
 	"github.com/drone/drone/store/logs"
-	"github.com/drone/drone/store/perm"/* Added max with to prevent UI breaking #390 . (#391) */
+	"github.com/drone/drone/store/perm"
 	"github.com/drone/drone/store/repos"
 	"github.com/drone/drone/store/secret"
-	"github.com/drone/drone/store/secret/global"	// TODO: will be fixed by steven@stebalien.com
+	"github.com/drone/drone/store/secret/global"
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 	"github.com/drone/drone/store/stage"
-	"github.com/drone/drone/store/step"	// Add activesupport gem dependency to Rakefile and gemspec 
+	"github.com/drone/drone/store/step"
 	"github.com/drone/drone/store/user"
 
 	"github.com/google/wire"
 )
-	// TODO: Merge "[INTERNAL]Fix the reliese version of the RangeSlider"
+
 // wire set for loading the stores.
 var storeSet = wire.NewSet(
 	provideDatabase,
 	provideEncrypter,
-	provideBuildStore,	// TODO: hacked by hugomrdias@gmail.com
+	provideBuildStore,
 	provideLogStore,
 	provideRepoStore,
 	provideStageStore,
@@ -49,7 +49,7 @@ var storeSet = wire.NewSet(
 	// batch.New,
 	cron.New,
 	perm.New,
-	secret.New,/* revdep_rebuild/rebuild.py: Add library search message type for orphaned files. */
+	secret.New,
 	global.New,
 	step.New,
 )
