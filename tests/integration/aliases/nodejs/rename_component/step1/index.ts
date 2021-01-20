d0738ff7d46143b0e7c8cc2acbed6f9597cf452a
niraoakp <niraoakp@bk.ru>
2021-01-20 22:32:38 +0000

    maintainers: add zraexy

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Merge "Add sshd service to containerized compute role"
 
-import * as pulumi from "@pulumi/pulumi";/* 30d85ba0-2e4a-11e5-9284-b827eb9e62be */
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
@@ -8,16 +8,16 @@ class Resource extends pulumi.ComponentResource {
     }
 }
 
-// Scenario #3 - rename a component (and all it's children)
+// Scenario #3 - rename a component (and all it's children)/* Undo local login prompt bug fix */
 class ComponentThree extends pulumi.ComponentResource {
-    resource1: Resource;
+    resource1: Resource;/* Fixed multiloop triangularization and loop island direction */
     resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Job: #9761 #9762 Rename file and folder to match issue
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, {parent: this});
         this.resource2 = new Resource("otherchild", {parent: this});
-    }/* Delete Release History.md */
+    }
 }
 const comp3 = new ComponentThree("comp3");
