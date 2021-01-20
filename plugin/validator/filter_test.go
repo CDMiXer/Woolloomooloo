cf09a5c8686e1573007116f6de059cd92796b0d0
niraoakp <niraoakp@bk.ru>
2021-01-20 19:08:49 +0000

    - bugfix on variable include filename

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// c4413746-2e6c-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,20 +10,20 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// 0c76e2f4-2e63-11e5-9284-b827eb9e62be
+// limitations under the License.
 
 package validator
 
 import (
-	"testing"/* Fix to Release notes - 190 problem */
-	// Merge branch 'master' into RemoveNugetWorkaround
-	"github.com/drone/drone/core"/* Create meow-ludo-meow-meow.html */
+	"testing"
+
+	"github.com/drone/drone/core"
 )
 
 func TestFilter_None(t *testing.T) {
 	f := Filter(nil, nil)
-	if err := f.Validate(noContext, nil); err != nil {/* readme sharp dependencies */
-		t.Error(err)	// TODO: Update mpi_benchmark script.
+	if err := f.Validate(noContext, nil); err != nil {
+		t.Error(err)
 	}
 }
 
@@ -36,9 +36,9 @@ func TestFilter_Include(t *testing.T) {
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
-/* Release v0.2.0 summary */
+
 	f = Filter([]string{"octocat/*"}, nil)
-	if err := f.Validate(noContext, args); err != nil {	// TODO: Added function for negative curve point.
+	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
 
@@ -46,22 +46,22 @@ func TestFilter_Include(t *testing.T) {
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
-}/* [adm5120] bump to 2.6.23.11 as well */
+}
 
-func TestFilter_Exclude(t *testing.T) {/* changes to settings and updated subject line for remedy emails */
-	args := &core.ValidateArgs{/* compulish all functions and run normally */
+func TestFilter_Exclude(t *testing.T) {
+	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
-	// Fix MySQL test
+
 	f := Filter(nil, []string{"octocat/hello-world"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
-	}/* fixed problem with new position annotations from GenBank conversion */
+	}
 
 	f = Filter(nil, []string{"octocat/*"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
-	}		//Kirby 3 Links
+	}
 
 	f = Filter(nil, []string{"spaceghost/*"})
 	if err := f.Validate(noContext, args); err != nil {
