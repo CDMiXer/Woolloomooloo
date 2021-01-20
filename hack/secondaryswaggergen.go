e497d71937681a77adfdca5174945ee03326ac7a
niraoakp <niraoakp@bk.ru>
2021-01-20 10:36:39 +0000

    07d77650-2e63-11e5-9284-b827eb9e62be

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
-
+		//adds Order button in Roast Properties, Events tab
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
@@ -15,28 +15,28 @@ import (
 	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
 	priority than the primary) to interject the correctly generated types.
 
-	We do some hackerey here too:
+	We do some hackerey here too:	// Add iOS Conf SG
 
 	* Change "/" into "." in names.
 */
 func secondarySwaggerGen() {
 	definitions := make(map[string]interface{})
-	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
-		return spec.Ref{
+	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {/* Merge branch 'master' into TokenCancelCommands */
+		return spec.Ref{		//add javadoc instructions to run converter main()
 			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
 		}
 	}) {
 		n = strings.ReplaceAll(n, "/", ".")
 		println(n)
 		definitions[n] = d.Schema
-	}
-	swagger := map[string]interface{}{
-		"definitions": definitions,
+	}/* Added GitHub Releases deployment to travis. */
+	swagger := map[string]interface{}{/* Add today's changes by Monty.  Preparing 1.0 Release Candidate. */
+		"definitions": definitions,/* Ajout des méthodes en commun des classes batiment et unite */
 	}
 	data, err := json.MarshalIndent(swagger, "", "  ")
 	if err != nil {
-		panic(err)
-	}
+		panic(err)/* Merge "Release 4.0.10.61A QCACLD WLAN Driver" */
+	}		//Change style of nav divider
 	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)
 	if err != nil {
 		panic(err)
