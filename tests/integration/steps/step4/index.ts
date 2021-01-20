1a5eeca473e993996a047508b846049ab1ea4fe9
niraoakp <niraoakp@bk.ru>
2021-01-20 13:28:04 +0000

    f6bd8e7c-2e5d-11e5-9284-b827eb9e62be

--- tests/integration/steps/step4/index.ts
+++ tests/integration/steps/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
+/* Release 0.2.3. Update public server documentation. */
 // Step4: Replace a resource (but this time, deleteBeforeReplace):
-// * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).
+// * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).		//Skip element if its a table of changing margins #1394
 let a = new Resource("a", { state: 1, replace: 1 });
 // * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set
 //   deleteBeforeReplace to true (DeleteReplaced(c3), CreateReplacement(c4)).
