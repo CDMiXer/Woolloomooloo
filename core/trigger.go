4d67e6d3bc08def85fcd7f9453911e2a47fdef8d
niraoakp <niraoakp@bk.ru>
2021-01-20 20:02:00 +0000

    Merge "quota: remove default kwarg on get_class_quotas()"

--- core/trigger.go
+++ core/trigger.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Switched to try ressource management
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge "Add support for Trove create database"
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Add support for embedded CSV files
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: Se agrega mensaje descriptivo para error al validar Email
+// You may obtain a copy of the License at
+//	// TODO: will be fixed by igor@soramitsu.co.jp
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge branch 'master' into remove-file-safety */
-// Unless required by applicable law or agreed to in writing, software		//Merge branch 'master' into playlist-item-paragraph
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Added @Deprecated annotation to a deprecated method (through JavaDoc). */
-// limitations under the License.		//aba6e0dc-2e5d-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
+// limitations under the License./* Merge "wlan: Release 3.2.3.132" */
 
 package core
-		//Progress towards level creation
+	// TODO: Change schicksalswiki logo
 import "context"
-
+		//Refactored context menu to allow special item widgets.
 // Trigger types
 const (
-	TriggerHook = "@hook"
-	TriggerCron = "@cron"/* Merge "soc: qcom: glink: Add list of available logical Channel IDs" */
+	TriggerHook = "@hook"	// Update home page
+	TriggerCron = "@cron"
 )
-	// enable autogen again
-// Triggerer is responsible for triggering a Build from an		//refactor(general): move code to lib/, add opts.js
-// incoming drone. If a build is skipped a nil value is	// TODO: hacked by juan@benet.ai
+
+// Triggerer is responsible for triggering a Build from an
+// incoming drone. If a build is skipped a nil value is	// Fix indent error in Mobi writer
 // returned.
 type Triggerer interface {
-	Trigger(context.Context, *Repository, *Hook) (*Build, error)
+	Trigger(context.Context, *Repository, *Hook) (*Build, error)	// TODO: will be fixed by aeongrp@outlook.com
 }
