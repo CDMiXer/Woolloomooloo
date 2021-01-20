0299150069766e0856d0324c519c7492b5e3d1bb
niraoakp <niraoakp@bk.ru>
2021-01-20 02:22:03 +0000

    chore(travis): support for osx

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -1,16 +1,16 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
 
-import pulumi	// Fix code system creation for test.
+import pulumi
 
-config = pulumi.Config()		//Revert changes to plot/python/demo used in debugging
+config = pulumi.Config()
 org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
-/* Release preparations for 0.2 Alpha */
+
 got_err = False
-/* Release version [10.5.0] - prepare */
-try:		//Pass response as a controller argument to allow direct manipulation if needed.
-    a.get_output('val2')/* Merge "Add Release Notes and Architecture Docs" */
+
+try:
+    a.get_output('val2')
 except Exception:
     got_err = True
 
