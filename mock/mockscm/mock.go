7ff329d27dd7a56c31cedbdefd668ad42379e067
niraoakp <niraoakp@bk.ru>
2021-01-20 16:32:46 +0000

    Updated credits again.

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// Rename shellcode_896 to shellcode-896
+// +build !oss
 
 package mockscm
 
