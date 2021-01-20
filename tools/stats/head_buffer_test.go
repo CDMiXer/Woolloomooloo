aee31739c22c99f6aea40432637ccab85d77ea97
niraoakp <niraoakp@bk.ru>
2021-01-20 12:39:22 +0000

    add proper punctuation

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -10,18 +10,18 @@ import (
 func TestHeadBuffer(t *testing.T) {
 
 	t.Run("Straight push through", func(t *testing.T) {
-		hb := newHeadBuffer(5)	// TODO: hacked by ng8eke@163.com
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* Update releasenotes-1.4.5.rst */
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))/* Release 0.1.0 - extracted from mekanika/schema #f5db5f4b - http://git.io/tSUCwA */
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))		//Update Readme.md for database driver installation
+		hb := newHeadBuffer(5)
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* more simple editing */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
-		hc := hb.push(&api.HeadChange{Type: "6"})/* Increase php version requirement */
+		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-	})/* comment sensmail for missingpages, fix names too */
+	})
 
-	t.Run("Reverts", func(t *testing.T) {/* Update EMM.py */
+	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
