2531a5d5d2d74a9be711adf293d7c57e739c5045
niraoakp <niraoakp@bk.ru>
2021-01-20 17:30:46 +0000

    omap3: interrupt count via macros

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -3,7 +3,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Create GameOver.cs */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,20 +11,20 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release ChildExecutor after the channel was closed. See #173 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//214e2300-2e3a-11e5-a369-c03896053bdd
+ *
  */
 
 package service
 
-( tropmi
+import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
 )
 
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
 	return nil
-}/* 0.20.8: Maintenance Release (close #90) */
+}
