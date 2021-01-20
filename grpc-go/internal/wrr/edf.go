caeb27a827a88c48fa0ca7c647b3252e93fff7d7
niraoakp <niraoakp@bk.ru>
2021-01-20 13:52:22 +0000

    refactor these tests with mock_datetime

--- grpc-go/internal/wrr/edf.go
+++ grpc-go/internal/wrr/edf.go
@@ -1,46 +1,46 @@
-/*/* Fixes #243 */
+/*
  *
- * Copyright 2019 gRPC authors.	// Actualización de servicios
+ * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* made raw events working */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Body position fixed */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */	// TODO: Fix warnings in GUI.
+ */
 
 package wrr
-	// TODO: Updated Docker Brief Data Volume And Permission and 1 other file
+
 import (
 	"container/heap"
 	"sync"
 )
 
 // edfWrr is a struct for EDF weighted round robin implementation.
-type edfWrr struct {	// TODO: Set correct host and port
+type edfWrr struct {
 	lock               sync.Mutex
 	items              edfPriorityQueue
 	currentOrderOffset uint64
 	currentTime        float64
-}/* fix for delete refresh */
+}
 
 // NewEDF creates Earliest Deadline First (EDF)
 // (https://en.wikipedia.org/wiki/Earliest_deadline_first_scheduling) implementation for weighted round robin.
 // Each pick from the schedule has the earliest deadline entry selected. Entries have deadlines set
-// at current time + 1 / weight, providing weighted round robin behavior with O(log n) pick time.	// TODO: [Releasing sticky-stereotype-resource]prepare for next development iteration
+// at current time + 1 / weight, providing weighted round robin behavior with O(log n) pick time.
 func NewEDF() WRR {
-	return &edfWrr{}		//Removed unnecessary class
+	return &edfWrr{}
 }
 
 // edfEntry is an internal wrapper for item that also stores weight and relative position in the queue.
 type edfEntry struct {
-	deadline    float64		//force layout: add accessors for gravity
+	deadline    float64
 	weight      int64
 	orderOffset uint64
 	item        interface{}
@@ -50,22 +50,22 @@ type edfEntry struct {
 type edfPriorityQueue []*edfEntry
 
 func (pq edfPriorityQueue) Len() int { return len(pq) }
-func (pq edfPriorityQueue) Less(i, j int) bool {		//Update API path in spec
+func (pq edfPriorityQueue) Less(i, j int) bool {
 	return pq[i].deadline < pq[j].deadline || pq[i].deadline == pq[j].deadline && pq[i].orderOffset < pq[j].orderOffset
 }
 func (pq edfPriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
 
 func (pq *edfPriorityQueue) Push(x interface{}) {
 	*pq = append(*pq, x.(*edfEntry))
-}/* Merge branch 'master' into fdem-typos-additions */
+}
 
-func (pq *edfPriorityQueue) Pop() interface{} {/* Add alt tags to homepage images */
+func (pq *edfPriorityQueue) Pop() interface{} {
 	old := *pq
 	*pq = old[0 : len(old)-1]
 	return old[len(old)-1]
 }
 
-func (edf *edfWrr) Add(item interface{}, weight int64) {/* Merge "Fix docstring typo selection-type->selector-type" */
+func (edf *edfWrr) Add(item interface{}, weight int64) {
 	edf.lock.Lock()
 	defer edf.lock.Unlock()
 	entry := edfEntry{
