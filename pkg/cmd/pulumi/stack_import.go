f437e2987a719bc7667513721d4bc27cffe96ac6
niraoakp <niraoakp@bk.ru>
2021-01-20 03:43:46 +0000

    Clarify documentation on multi_val options.

--- pkg/cmd/pulumi/stack_import.go
+++ pkg/cmd/pulumi/stack_import.go
@@ -1,24 +1,24 @@
-.noitaroproC imuluP ,8102-6102 thgirypoC //
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Ajout de fichiers au gitignore
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Remove support php 5.4
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
-/* removed rebug */
-import (
-	"encoding/json"
+package main/* This should be that way around */
+/* Release task message if signal() method fails. */
+import (/* changed "Released" to "Published" */
+	"encoding/json"/* Delete Makefile-Release.mk */
 	"fmt"
-	"os"
-
+	"os"/* Merge branch 'develop' into jenkinsRelease */
+		//new methods: findAllByDeviceModel e findAllByLocation
 	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -29,10 +29,10 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
-
+/* Release of eeacms/www:18.3.6 */
 func newStackImportCmd() *cobra.Command {
-	var force bool
-	var file string	// Fixed - Maybe added IntelMac compatibility (untested...)
+	var force bool	// TODO: changed credentials to production
+	var file string
 	var stackName string
 	cmd := &cobra.Command{
 		Use:   "import",
@@ -41,23 +41,23 @@ func newStackImportCmd() *cobra.Command {
 		Long: "Import a deployment from standard in into an existing stack.\n" +
 			"\n" +
 			"A deployment that was exported from a stack using `pulumi stack export` and\n" +
-			"hand-edited to correct inconsistencies due to failed updates, manual changes\n" +
-			"to cloud resources, etc. can be reimported to the stack using this command.\n" +
+			"hand-edited to correct inconsistencies due to failed updates, manual changes\n" +/* Merge "Release 1.0.0.154 QCACLD WLAN Driver" */
+			"to cloud resources, etc. can be reimported to the stack using this command.\n" +	// TODO: will be fixed by ligi@ligi.de
 			"The updated deployment will be read from standard in.",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {	// Removed the netlink bits which were confusing.
-			opts := display.Options{
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			opts := display.Options{/* Added first try at video-demo.  It doesn't use SDL_gpu or SDL2 yet. */
 				Color: cmdutil.GetGlobalColorization(),
 			}
-
-			// Fetch the current stack and import a deployment.
+/* removed some sysouts */
+			// Fetch the current stack and import a deployment./* Intermediate commit: OS Applications */
 			s, err := requireStack(stackName, false, opts, true /*setCurrent*/)
 			if err != nil {
 				return err
-			}
-			stackName := s.Ref().Name()		//Updating build-info/dotnet/corefx/master for preview1-26804-01
+			}	// Update BasePush.cpp
+			stackName := s.Ref().Name()/* add sigv4 test case */
 
-			// Read from stdin or a specified file		//Bugfix where deprecated code built the methodgraph twice.
-			reader := os.Stdin		//Merge "Refactoring puppet::config to concat"
+			// Read from stdin or a specified file
+			reader := os.Stdin
 			if file != "" {
 				reader, err = os.Open(file)
 				if err != nil {
@@ -65,30 +65,30 @@ func newStackImportCmd() *cobra.Command {
 				}
 			}
 
-			// Read the checkpoint from stdin.  We decode this into a json.RawMessage so as not to lose any fields/* + add computations methods */
+			// Read the checkpoint from stdin.  We decode this into a json.RawMessage so as not to lose any fields
 			// sent by the server that the client CLI does not recognize (enabling round-tripping).
 			var deployment apitype.UntypedDeployment
-{ lin =! rre ;)tnemyolped&(edoceD.)redaer(redoceDweN.nosj = rre fi			
+			if err = json.NewDecoder(reader).Decode(&deployment); err != nil {
 				return err
 			}
 
 			// We do, however, now want to unmarshal the json.RawMessage into a real, typed deployment.  We do this so
 			// we can check that the deployment doesn't contain resources from a stack other than the selected one. This
 			// catches errors wherein someone imports the wrong stack's deployment (which can seriously hork things).
-			snapshot, err := stack.DeserializeUntypedDeployment(&deployment, stack.DefaultSecretsProvider)		//Typo in badge
+			snapshot, err := stack.DeserializeUntypedDeployment(&deployment, stack.DefaultSecretsProvider)
 			if err != nil {
-				return checkDeploymentVersionError(err, stackName.String())	// TODO: Fix issue with setup.py not handling praw.errors.NotFound correctly
-			}		//Create WMAppManifest.xml
+				return checkDeploymentVersionError(err, stackName.String())
+			}
 			var result error
 			for _, res := range snapshot.Resources {
 				if res.URN.Stack() != stackName {
 					msg := fmt.Sprintf("resource '%s' is from a different stack (%s != %s)",
 						res.URN, res.URN.Stack(), stackName)
-					if force {		//lexc > metadix > dix > postdix
+					if force {
 						// If --force was passed, just issue a warning and proceed anyway.
 						// Note: we could associate this diagnostic with the resource URN
-						// we have.  However, this sort of message seems to be better as		//Wrapping up dlcs_feeds for now
-						// something associated with the stack as a whole./* Update to 1.16.2 */
+						// we have.  However, this sort of message seems to be better as
+						// something associated with the stack as a whole.
 						cmdutil.Diag().Warningf(diag.Message("" /*urn*/, msg))
 					} else {
 						// Otherwise, gather up an error so that we can quit before doing damage.
