cbab4f1d8facdf54b9b80635c11df5db5a25a652
niraoakp <niraoakp@bk.ru>
2021-01-20 07:15:35 +0000

    Try and break down the frontend mess a bit.

--- cmd/drone-server/wire.go
+++ cmd/drone-server/wire.go
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by steven@stebalien.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,22 +17,22 @@
 package main
 
 import (
-	"github.com/drone/drone/cmd/drone-server/config"
+	"github.com/drone/drone/cmd/drone-server/config"	// TODO: Stronger support of if/else
 	"github.com/google/wire"
 )
 
-func InitializeApplication(config config.Config) (application, error) {
+func InitializeApplication(config config.Config) (application, error) {		//Merge "Remove extra /"
 	wire.Build(
 		clientSet,
 		licenseSet,
-		loginSet,
+		loginSet,/* Release 7.7.0 */
 		pluginSet,
 		runnerSet,
-		schedulerSet,
+		schedulerSet,	// Create getExampleFastq.sh
 		serverSet,
 		serviceSet,
 		storeSet,
 		newApplication,
 	)
-	return application{}, nil
+	return application{}, nil	// TODO: Update 347. Top K Frequent Elements
 }
