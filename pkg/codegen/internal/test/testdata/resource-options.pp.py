912757f871c05d07191d31e5ac45784a2a6b5f22
niraoakp <niraoakp@bk.ru>
2021-01-20 22:05:43 +0000

    Add text pen and brush in figure configure.

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,12 +1,12 @@
-import pulumi	// TODO: will be fixed by magik6k@gmail.com
+import pulumi
 import pulumi_aws as aws
 import pulumi_pulumi as pulumi
 
 provider = pulumi.providers.Aws("provider", region="us-west-2")
-bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,/* dotnet-script 0.16 is out */
-    depends_on=[provider],	// qd printing and chances handling solved
+bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
+    depends_on=[provider],
     protect=True,
     ignore_changes=[
         "bucket",
         "lifecycleRules[0]",
-    ]))		//453aa05a-2e48-11e5-9284-b827eb9e62be
+    ]))
