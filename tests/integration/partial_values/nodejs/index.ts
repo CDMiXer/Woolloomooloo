2c4df494f8739ba9507cb293e08ed225aea83f1f
niraoakp <niraoakp@bk.ru>
2021-01-20 20:16:06 +0000

    Fixed Task #14318.

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as assert from "assert";
+import * as assert from "assert";/* Version 0.10.1 Release */
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: hacked by nagydani@epointsystem.org
 
 const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
-	// TODO: will be fixed by vyzo@hackzen.org
+
 let a = new Resource("res", {
     foo: "foo",
     bar: { value: "foo", unknown },
     baz: [ "foo", unknown ],
-});/* Release for 3.4.0 */
-/* Update net_protocol.md */
-export let o = Promise.all([
+});		//Added new rules to naming convention
+		//Don’t init if platform isn’t supported.
+export let o = Promise.all([/* add libasound-dev to travis */
     (<any>a.foo).isKnown,
     (<any>a.bar.value).isKnown,
-    (<any>a.bar.unknown).isKnown,
+    (<any>a.bar.unknown).isKnown,/* Fix bad url generation. */
     (<any>a.baz[0]).isKnown,
     (<any>a.baz[1]).isKnown,
 ]).then(([r1, r2, r3, r4, r5]) => {
     assert.equal(r1, true);
-    assert.equal(r2, true);
-    assert.equal(r3, !pulumi.runtime.isDryRun());		//Create ImagesPreviewer.js
+    assert.equal(r2, true);/* upping to support UserEmailAlreadyExists */
+    assert.equal(r3, !pulumi.runtime.isDryRun());
     assert.equal(r4, true);
-    assert.equal(r5, !pulumi.runtime.isDryRun());		//Fix SnapshotEngine closest version computation.
-	// Bump to version 4.0.1.
-    console.log("ok");
-    return "checked";/* Create vm-contents */
+    assert.equal(r5, !pulumi.runtime.isDryRun());		//Fix compute homepage URL (#927)
+		//extended readme file and added simple usage example
+    console.log("ok");	// TODO: hacked by nick@perfectabstractions.com
+    return "checked";
 });
