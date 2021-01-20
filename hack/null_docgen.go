8864464c08ba5157b21cd86d6ae2954a12aeee77
niraoakp <niraoakp@bk.ru>
2021-01-20 09:46:04 +0000

    Rename AIULogin Converted to Trees to AIULogin Converted to Trees (Adam)

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
-// +build fields	// TODO: hacked by yuvalalaluf@gmail.com
+// +build fields
 
-package main
-
-func generateDocs() {
+package main/* Further refactoring to support linked datasets */
+		//Second Commit.
+func generateDocs() {/* Review dependencies */
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
 }
