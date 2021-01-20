454204d53492f7a5d7f84c40bde57a74a5a62e19
niraoakp <niraoakp@bk.ru>
2021-01-20 03:21:52 +0000

    Delete License.docx

--- tests/examples/dynamic-provider/multiple-turns-2/index.ts
+++ tests/examples/dynamic-provider/multiple-turns-2/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by ac0dem0nk3y@gmail.com
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-
+/* add a sample team table */
 const sleep = require("sleep-promise");
-const assert = require("assert");		//Fix the first slider display for mobile.
-	// Fixed comments and author tag.
+const assert = require("assert");
+
 class NullProvider implements dynamic.ResourceProvider {
-    check = (olds: any, news: any) => Promise.resolve({ inputs: news });/* Release 6.0.0.RC1 take 3 */
+    check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
@@ -15,15 +15,15 @@ class NullProvider implements dynamic.ResourceProvider {
 }
 
 class NullResource extends dynamic.Resource {
-    constructor(name: string, input: any) {/* Merge "Make RPCFixture support multiple connections" */
+    constructor(name: string, input: any) {
         super(new NullProvider(), name, {input: input}, undefined);
     }
-}		//Finalisation du Loader
+}
 
 async function getInput(): Promise<pulumi.Output<string>> {
     await sleep(1000);
 
     return (new NullResource("a", "")).urn;
 }
-/* Added test for chained maps */
+		//terrain transitions for all terrains, new cave terain other terrain tweaks.
 const b = new NullResource("b", getInput());
