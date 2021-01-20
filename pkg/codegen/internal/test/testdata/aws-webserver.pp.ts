e7f7a68a744fd52e22df1973bb1031372f77d49e
niraoakp <niraoakp@bk.ru>
2021-01-20 20:53:11 +0000

    Merge branch 'master' of git@github.com:AndiHappy/andihappy.github.io.git

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,23 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: fe3954d4-2e57-11e5-9284-b827eb9e62be
 import * as aws from "@pulumi/aws";
 
 // Create a new security group for port 80.
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{	// TODO: hamyar GP v1.3
     protocol: "tcp",
     fromPort: 0,
     toPort: 0,
-    cidrBlocks: ["0.0.0.0/0"],
+    cidrBlocks: ["0.0.0.0/0"],/* Initial websocket handler */
 }]});
 const ami = aws.getAmi({
-    filters: [{
-        name: "name",
+    filters: [{	// Merge "Fix styles of table views"
+        name: "name",/* FIX force allow prefill if using prefill_with_data_from_widget_link */
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
     }],
     owners: ["137112412989"],
     mostRecent: true,
 });
 // Create a simple web server using the startup script for the instance.
-const server = new aws.ec2.Instance("server", {
+const server = new aws.ec2.Instance("server", {/* [JENKINS-60740] - Update Release Drafter to the recent version */
     tags: {
         Name: "web-server-www",
     },
