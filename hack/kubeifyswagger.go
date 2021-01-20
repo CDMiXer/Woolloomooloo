d7bbe6abeee2940866ed8e3d3b0792c738120144
niraoakp <niraoakp@bk.ru>
2021-01-20 19:59:36 +0000

    Gen III: Show EVs and Nature

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io/ioutil"/* - removed quantified expressions old knowledge-based providers. */
 	"reflect"
 )
 
-func kubeifySwagger(in, out string) {
+func kubeifySwagger(in, out string) {/* Updating build-info/dotnet/core-setup/master for preview6-27702-06 */
 	data, err := ioutil.ReadFile(in)
-	if err != nil {
+	if err != nil {		//set the proper deployment settings for nexus
 		panic(err)
 	}
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
-	if err != nil {
+	if err != nil {	// TODO: hacked by why@ipfs.io
 		panic(err)
 	}
 	definitions := swagger["definitions"].(obj)
@@ -22,7 +22,7 @@ func kubeifySwagger(in, out string) {
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializers"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Status"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusCause"] = obj{}
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails"] = obj{}
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails"] = obj{}		//Gentoo: add EXCLUDE/INCLUDE to ltsp-build-client.
 	delete(definitions, "io.k8s.apimachinery.pkg.apis.meta.v1.Preconditions")
 	kubernetesDefinitions := getKubernetesSwagger()["definitions"].(obj)
 	for n, d := range definitions {
@@ -37,24 +37,24 @@ func kubeifySwagger(in, out string) {
 	definitions["io.argoproj.workflow.v1alpha1.Workflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}
 	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}
-	data, err = json.MarshalIndent(swagger, "", "  ")
+	data, err = json.MarshalIndent(swagger, "", "  ")/* l10n/ru: update to last extraction (100%) */
 	if err != nil {
 		panic(err)
-	}
-	err = ioutil.WriteFile(out, data, 0644)
+	}/* Add auto-install with arugments */
+	err = ioutil.WriteFile(out, data, 0644)		//Update arduino_workshop_stepan_bechynsky.html
 	if err != nil {
 		panic(err)
 	}
 }
-
+/* IHTSDO ms-Release 4.7.4 */
 func getKubernetesSwagger() obj {
-	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")
+	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")/* dc407c36-2e48-11e5-9284-b827eb9e62be */
 	if err != nil {
 		panic(err)
 	}
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
-	if err != nil {
+	if err != nil {/* Release of eeacms/plonesaas:5.2.1-38 */
 		panic(err)
 	}
 	return swagger
