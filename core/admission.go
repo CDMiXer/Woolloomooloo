74a705a0577b64c38e1fca193905c757f197f9d9
niraoakp <niraoakp@bk.ru>
2021-01-20 04:58:02 +0000

    Ruby: add easy exercise after twofer

--- core/admission.go
+++ core/admission.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc.		//Quieten down Minitest!
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* fixed support for PostgreSQL for testing on Linux */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// + loader option
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Create ETHAddress */
-package core	// TODO: hacked by praveen@minio.io
-/* Added 0.9.5 Release Notes */
+
+package core
+
 import "context"
 
 // AdmissionService grants access to the system. The service can
 // be used to restrict access to authorized users, such as
 // members of an organization in your source control management
-// system./* Merge "[Release] Webkit2-efl-123997_0.11.39" into tizen_2.1 */
+// system.
 type AdmissionService interface {
 	Admit(context.Context, *User) error
 }
