54c195c3ff3c07839d3bb64bab2e71209afcec7e
niraoakp <niraoakp@bk.ru>
2021-01-20 12:08:19 +0000

    Add debug statements.

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Wlan: Release 3.8.20.5" */
+// Licensed under the Apache License, Version 2.0 (the "License");/* - оффсет времени */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: 812d8bb8-2e57-11e5-9284-b827eb9e62be
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "Release 4.0.10.26 QCACLD WLAN Driver" */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss	// build project added
-	// TODO: https://forums.lanik.us/viewtopic.php?p=136255#p136255
+// +build oss
+
 package builds
 
-import (
+import (	// TODO: hacked by witek@enjin.io
 	"net/http"
 
-	"github.com/drone/drone/core"/* Fix weed plant 3D2D */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: Translated nautilus.ini -- partial
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-// HandleIncomplete returns a no-op http.HandlerFunc.
+// HandleIncomplete returns a no-op http.HandlerFunc.		//trigger new build for ruby-head-clang (919587e)
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return notImplemented
 }
