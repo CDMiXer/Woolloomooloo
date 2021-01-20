feeb4e7ee081895804a006f0320012587d445761
niraoakp <niraoakp@bk.ru>
2021-01-20 14:42:05 +0000

    Fixed player info message.

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -1,32 +1,32 @@
-// Copyright 2019 Drone IO, Inc.		//64 is enough
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Added DBUS_CFLAGS to docs build
-// you may not use this file except in compliance with the License./* aggiornato readme */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* changed my mind, version 1.0.4 it is */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* run_test now uses Release+Asserts */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* prepared Release 7.0.0 */
+
 // +build oss
 
 package queue
 
 import (
-	"net/http"	// TODO: Added hama PiEstimatorBenchmark caliper results
+	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: Reenable timber-logback-support, reformat with Idea, purge OSGi
+	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}	// * Updated apf_release
-/* Update VRAnimator.swift */
+}
+
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return notImplemented
 }
@@ -34,7 +34,7 @@ func HandleItems(store core.StageStore) http.HandlerFunc {
 func HandlePause(core.Scheduler) http.HandlerFunc {
 	return notImplemented
 }
-		//Optimized XLS generation.
+
 func HandleResume(core.Scheduler) http.HandlerFunc {
 	return notImplemented
 }
