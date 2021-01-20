c0e723d5cec14eedeb334e70cfd298620ac39c37
niraoakp <niraoakp@bk.ru>
2021-01-20 02:07:11 +0000

    Merge "ASoC: msm: Fix wrong wait_event_timeout timeout checks"

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* DATASOLR-234 - Release version 1.4.0.RELEASE. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* 0.2.2 Release */
 // +build !oss
-	// TODO: Car now turns 2 wheels at once
-package config	// TODO: hacked by lexy8russo@outlook.com
+
+package config
