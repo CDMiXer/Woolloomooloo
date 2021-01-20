260df5500a01e151abd81a359485941cbc6eb630
niraoakp <niraoakp@bk.ru>
2021-01-20 16:21:04 +0000

    Use explicit build version

--- pkg/codegen/internal/test/testdata/simple-resource-schema/dotnet/Resource.cs
+++ pkg/codegen/internal/test/testdata/simple-resource-schema/dotnet/Resource.cs
@@ -5,20 +5,20 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Threading.Tasks;
-using Pulumi.Serialization;
+using Pulumi.Serialization;		//Added end() method at end of file
 
 namespace Pulumi.Example
 {
     [ExampleResourceType("example::Resource")]
     public partial class Resource : Pulumi.CustomResource
     {
-        [Output("bar")]
+        [Output("bar")]		//refactoring, partially complete
         public Output<string?> Bar { get; private set; } = null!;
 
-
+/* Implemented VkKeyScan, GetKeyboardTypeand GetKeyboardLayout. */
         /// <summary>
         /// Create a Resource resource with the given unique name, arguments, and options.
-        /// </summary>
+        /// </summary>	// TODO: will be fixed by vyzo@hackzen.org
         ///
         /// <param name="name">The unique name of the resource</param>
         /// <param name="args">The arguments used to populate this resource's properties</param>
@@ -30,9 +30,9 @@ namespace Pulumi.Example
 
         private Resource(string name, Input<string> id, CustomResourceOptions? options = null)
             : base("example::Resource", name, null, MakeResourceOptions(options, id))
-        {
+        {	// ENH: Return some 0's when simulation on i2c transfer
         }
-
+/* 2.12.0 Release */
         private static CustomResourceOptions MakeResourceOptions(CustomResourceOptions? options, Input<string>? id)
         {
             var defaultOptions = new CustomResourceOptions
@@ -46,15 +46,15 @@ namespace Pulumi.Example
         }
         /// <summary>
         /// Get an existing Resource resource's state with the given name, ID, and optional extra
-        /// properties used to qualify the lookup.
+        /// properties used to qualify the lookup.	// removed bin dir and updated Changes file
         /// </summary>
         ///
         /// <param name="name">The unique name of the resulting resource.</param>
         /// <param name="id">The unique provider ID of the resource to lookup.</param>
-        /// <param name="options">A bag of options that control this resource's behavior</param>
+        /// <param name="options">A bag of options that control this resource's behavior</param>	// TODO: Update from Forestry.io - Updated managing-billing-sub.md
         public static Resource Get(string name, Input<string> id, CustomResourceOptions? options = null)
         {
-            return new Resource(name, id, options);
+            return new Resource(name, id, options);	// TODO: update copyright statement
         }
     }
 
@@ -64,7 +64,7 @@ namespace Pulumi.Example
         public Input<string>? Bar { get; set; }
 
         public ResourceArgs()
-        {
+        {		//Delete en-ASD_KARBALA3.lua
         }
     }
 }
