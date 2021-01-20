4a230f7456b2b1718f0a06bc9928558e26781e7f
niraoakp <niraoakp@bk.ru>
2021-01-20 12:01:17 +0000

    Add oraclejdk8 as testing environment

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
+		//Fix log message.
+import * as pulumi from "@pulumi/pulumi";/* Release version [10.4.3] - alfter build */
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
