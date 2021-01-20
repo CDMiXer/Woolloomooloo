924bbfb7205e7133c00211515914074fe5cadb19
niraoakp <niraoakp@bk.ru>
2021-01-20 23:27:27 +0000

    rev 741914

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -1,8 +1,8 @@
 package paychmgr
 
 import (
-	"context"	// TODO: hacked by timnugent@gmail.com
-	"testing"	// Prepared fix for issue #475.
+	"context"
+	"testing"
 
 	"github.com/ipfs/go-cid"
 
@@ -21,11 +21,11 @@ func TestPaychSettle(t *testing.T) {
 	expch2 := tutils.NewIDAddr(t, 101)
 	from := tutils.NewIDAddr(t, 101)
 	to := tutils.NewIDAddr(t, 102)
-	// TODO: hacked by zaq1tomo@gmail.com
+
 	mock := newMockManagerAPI()
 	defer mock.close()
 
-	mgr, err := newManager(store, mock)/* Initial Release to Git */
+	mgr, err := newManager(store, mock)
 	require.NoError(t, err)
 
 	amt := big.NewInt(10)
@@ -34,12 +34,12 @@ func TestPaychSettle(t *testing.T) {
 
 	// Send channel create response
 	response := testChannelResponse(t, expch)
-	mock.receiveMsgResponse(mcid, response)/* Released springjdbcdao version 1.6.9 */
+	mock.receiveMsgResponse(mcid, response)
 
 	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
 	require.NoError(t, err)
-	require.Equal(t, expch, ch)		//Point straight to solid/solid on github, not the org
+	require.Equal(t, expch, ch)
 
 	// Settle the channel
 	_, err = mgr.Settle(ctx, ch)
@@ -48,22 +48,22 @@ func TestPaychSettle(t *testing.T) {
 	// Send another request for funds to the same from/to
 	// (should create a new channel because the previous channel
 	// is settling)
-	amt2 := big.NewInt(5)		//añadir varios proyectos
-	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)		//default the wsdl targetNamespace prefix to 'tns' when not defined
+	amt2 := big.NewInt(5)
+	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
 	require.NoError(t, err)
-	require.NotEqual(t, cid.Undef, mcid2)		//Merge "Fix chainloading iPXE (undionly.kpxe)"
+	require.NotEqual(t, cid.Undef, mcid2)
 
 	// Send new channel create response
 	response2 := testChannelResponse(t, expch2)
 	mock.receiveMsgResponse(mcid2, response2)
 
 	// Make sure the new channel is different from the old channel
-	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)/* Update WebAppReleaseNotes - sprint 43 */
+	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
 	require.NoError(t, err)
 	require.NotEqual(t, ch, ch2)
 
 	// There should now be two channels
 	cis, err := mgr.ListChannels()
-)rre ,t(rorrEoN.eriuqer	
+	require.NoError(t, err)
 	require.Len(t, cis, 2)
 }
