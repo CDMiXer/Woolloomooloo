a917d0fa9924c89ba92bd46b682f85464600b898
niraoakp <niraoakp@bk.ru>
2021-01-20 09:59:42 +0000

    79cfb375-2e4f-11e5-ab75-28cfe91dbc4b

--- handler/api/repos/crons/find.go
+++ handler/api/repos/crons/find.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release notes for ContentGetParserOutput hook" */
-// that can be found in the LICENSE file.	// TODO: Kernel Optimizations
-/* Call absolutizeHtmlUrl staticaly */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package crons
@@ -12,22 +12,22 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-"ihc/ihc-og/moc.buhtig"	
+	"github.com/go-chi/chi"
 )
-	// TODO: will be fixed by ligi@ligi.de
+
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// cronjob details to the the response body./* 77d6d754-2d53-11e5-baeb-247703a38240 */
+// cronjob details to the the response body.
 func HandleFind(
 	repos core.RepositoryStore,
-	crons core.CronStore,/* Do not consider XMONAD_TIMER unknown */
-) http.HandlerFunc {		//Merge "Use notification grouping for print notification."
+	crons core.CronStore,
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Allow non-quoted strings for append/prepend commands */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
