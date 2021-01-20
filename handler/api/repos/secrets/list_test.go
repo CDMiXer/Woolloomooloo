ee7a2513c2b0f2fc08ccfde185b41b561ddc8935
niraoakp <niraoakp@bk.ru>
2021-01-20 05:12:15 +0000

    Added Access files

--- handler/api/repos/secrets/list_test.go
+++ handler/api/repos/secrets/list_test.go
@@ -1,53 +1,53 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: hacked by yuvalalaluf@gmail.com
-
+// that can be found in the LICENSE file.
+/* Fix syntax mistake in the README. */
 // +build !oss
-/* Released springjdbcdao version 1.7.9 */
-package secrets		//Update contact-us.html
 
-import (/* Simple example on how to use CSteemd API */
+package secrets
+/* Release 2.1.0 */
+import (		//Merge "msm: clock-pll: Support dynamically mapped register space" into msm-3.0
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+/* fix broken compilation in the previous commit */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Merge "Wlan: Release 3.8.20.18" */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 var (
 	dummySecretRepo = &core.Repository{
 		ID:        1,
-		Namespace: "octocat",/* Merge "Closes-Bug: #1441287" */
+		Namespace: "octocat",/* imported tiddlywiki notes */
 		Name:      "hello-world",
-	}/* Update and rename logbrokerextranet.pp to logbroker_extranet.pp */
-/* changed import _deffnet in deffnet.py */
+	}
+/* Release 2.0.0.pre */
 	dummySecret = &core.Secret{
 		RepoID: 1,
 		Name:   "github_password",
 		Data:   "pa55word",
-	}
+	}	// Delete Car.java
 
 	dummySecretScrubbed = &core.Secret{
-		RepoID: 1,/* fixed void to boolean in the Event hierarchy for hasExpired() */
-		Name:   "github_password",/* Util/UTF8: CopyTruncateStringUTF8() returns end of string */
-		Data:   "",
+		RepoID: 1,
+		Name:   "github_password",
+		Data:   "",/* Updated Release badge */
 	}
 
 	dummySecretList = []*core.Secret{
-		dummySecret,	// TODO: 07a21048-2e43-11e5-9284-b827eb9e62be
+		dummySecret,
 	}
 
 	dummySecretListScrubbed = []*core.Secret{
 		dummySecretScrubbed,
-	}
+	}		//Alterações para cadastro de médico
 )
 
 //
@@ -58,8 +58,8 @@ func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)		//Se agregó control de altura y scroll al modal de solicitud en un pedido
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)/* Pequenos ajustes na tela de adicionar Função de Dados. */
 
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().List(gomock.Any(), dummySecretRepo.ID).Return(dummySecretList, nil)
@@ -67,31 +67,31 @@ func TestHandleList(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-/* ctest -C Release */
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(		//Create LinuxCNC_M4-Dcs_5i25-7i77
-,)c ,yeKxtCetuoR.ihc ,)(dnuorgkcaB.txetnoc(eulaVhtiW.txetnoc		
-	)
 
-	HandleList(repos, secrets).ServeHTTP(w, r)
+	w := httptest.NewRecorder()		//upgrade explorer and download file
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)	// TODO: hacked by ng8eke@163.com
+
+	HandleList(repos, secrets).ServeHTTP(w, r)		//Update WsFederationCookieCipherExecutor.java
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: Add Teamwork Project Assignment
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Secret{}, dummySecretListScrubbed
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* 3fe290bd-2d5c-11e5-9493-b88d120fff5e */
 	}
 }
 
 func TestHandleList_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: Add modeling details to README.
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
+)dnuoFtoNrrE.srorre ,lin(nruteR.)emaN.opeRterceSymmud ,ecapsemaN.opeRterceSymmud ,)(ynA.kcomog(emaNdniF.)(TCEPXE.soper	
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
