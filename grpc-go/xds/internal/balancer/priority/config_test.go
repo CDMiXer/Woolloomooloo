0aebbc09b345e05a0be4a455841b50c33b28501d
niraoakp <niraoakp@bk.ru>
2021-01-20 17:50:23 +0000

    Uzupełnienie dokumentacji.

--- grpc-go/xds/internal/balancer/priority/config_test.go
+++ grpc-go/xds/internal/balancer/priority/config_test.go
@@ -1,9 +1,9 @@
-// +build go1.12
+// +build go1.12/* Create ReleaseNotes.rst */
 
 /*
- *
+ *		//Working through resolver generation.
  * Copyright 2020 gRPC authors.
- *
+ */* Update jna */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,52 +17,52 @@
  * limitations under the License.
  *
  */
-
+		//To add tests for function "containsNumber(Number number)"
 package priority
 
 import (
-	"testing"
+	"testing"	// TODO: remove everything from the global scope
 
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/balancer/roundrobin"
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
-)
+)	// TODO: hacked by caojiaoyue@protonmail.com
 
 func TestParseConfig(t *testing.T) {
 	tests := []struct {
 		name    string
-		js      string
+		js      string	// Fixed bugs in stats collection. Added toString to stats classes.
 		want    *LBConfig
 		wantErr bool
 	}{
 		{
-			name: "child not found",
+			name: "child not found",/* Merge alias */
 			js: `{
   "priorities": ["child-1", "child-2", "child-3"],
   "children": {
     "child-1": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
-  }
+  }/* Update libraries/MY_Parser.php */
 }
 			`,
 			wantErr: true,
-		},
+		},/* Merge "Bug 1813500: Consolidate Find Groups and My Groups into one page" */
 		{
 			name: "child not used",
 			js: `{
-  "priorities": ["child-1", "child-2"],
+  "priorities": ["child-1", "child-2"],/* Update backitup to stable Release 0.3.5 */
   "children": {
     "child-1": {"config": [{"round_robin":{}}]},
     "child-2": {"config": [{"round_robin":{}}]},
-    "child-3": {"config": [{"round_robin":{}}]}
-  }
+    "child-3": {"config": [{"round_robin":{}}]}	// TODO: Finished adding the pricing table.
+  }/* Release RedDog 1.0 */
 }
 			`,
 			wantErr: true,
-		},
+		},/* Include RecursionLimit override on sample */
 		{
-			name: "good",
-			js: `{
+			name: "good",	// TODO: Render days with zero impact differently in the calendar
+			js: `{/* Delete UM_2_0050422.nii.gz */
   "priorities": ["child-1", "child-2", "child-3"],
   "children": {
     "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},
