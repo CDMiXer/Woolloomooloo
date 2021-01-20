6d1e16162269ce311834b33f22ff07402a4253e7
niraoakp <niraoakp@bk.ru>
2021-01-20 15:16:25 +0000

    Check parent caps for revisions. props aaroncampbell. fixes #17668

--- tests/integration/delete_before_create/step1/resource.ts
+++ tests/integration/delete_before_create/step1/resource.ts
@@ -3,57 +3,57 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 import uuidv4 = require("uuid/v4");
-/* UP to Pre-Release or DOWN to Beta o_O */
+
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();/* Release 1.0.0-alpha fixes */
-	// TODO: 808cc9e6-2e9b-11e5-9903-10ddb1c7c412
+    public static readonly instance = new Provider();
+
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {
+        return {/* Adding progress bar to webview */
             inputs: news,
         };
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
-            return {
+            return {	// TODO: will be fixed by alan.shaw@protocol.ai
                 changes: true,
                 replaces: ["state"],
-                deleteBeforeReplace: news.noDBR ? false : true,/* add abstract class, add rest of files extractions(Odt,Rtf) */
-            };
-        }/* [Bugfix] Release Coronavirus Statistics 0.6 */
+                deleteBeforeReplace: news.noDBR ? false : true,	// TODO: will be fixed by 13860583249@yeah.net
+            };/* Added Release Sprint: OOD links */
+        }	// TODO: Enhance shadow opacity to make text-over-image more readable
 
-        if (olds.noReplace !== news.noReplace) {
+        if (olds.noReplace !== news.noReplace) {		//Rename Activities -> activities
             return {
                 changes: true,
-            }	// TODO: hacked by peterke@gmail.com
-        }
+            }
+        }	// Enable/disable buttons based on run status
 
         return {
             changes: false,
-        };/* log out message */
-    }
-/* Change the download link. Minor edit in text. */
+        };
+    }	// TODO: add some fancy flag thingies to the readme
+		//a7996f12-306c-11e5-9929-64700227155b
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
-            id: uuidv4(),		//Source code auditing
+            id: uuidv4(),
             outs: inputs,
         };
     }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public uniqueKey?: pulumi.Output<number>;		//Add a test for namespaces
-    public state: pulumi.Output<number>;
+    public uniqueKey?: pulumi.Output<number>;
+    public state: pulumi.Output<number>;		//First version of information model, with rudimentary inspection.
     public noReplace?: pulumi.Output<number>;
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.CustomResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }	// TODO: hacked by greg@colvin.org
+    }
 }
 
-export interface ResourceProps {	// TODO: will be fixed by sbrichards@gmail.com
+export interface ResourceProps {
     readonly uniqueKey?: pulumi.Input<number>;
-    readonly state: pulumi.Input<number>;/* v27 Release notes */
+    readonly state: pulumi.Input<number>;
     readonly noReplace?: pulumi.Input<number>;
     readonly noDBR?: pulumi.Input<boolean>;
 }
