6bd0c9e7e6731af42396106d07f9c94aaee7cf8e
niraoakp <niraoakp@bk.ru>
2021-01-20 04:39:21 +0000

    method call test

--- gen/conn_broadcast_test.go
+++ gen/conn_broadcast_test.go
@@ -1,60 +1,60 @@
-// Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.		//Update genotype-counts.sql
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Merge "Release text when finishing StaticLayout.Builder" into mnc-dev */
-/* Merge "Fix View reset of layoutDirection and textDirection" */
+// Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style		//Convert editableAnimal
+// license that can be found in the LICENSE file.
+
 package websocket
-/* Merge "Release 4.0.10.13  QCACLD WLAN Driver" */
+		//Improve omission of numpy benchmark.
 import (
 	"io"
 	"io/ioutil"
-	"sync/atomic"/* Remove inline comments from the git commands, because they break the commands. */
+	"sync/atomic"
 	"testing"
 )
 
-// broadcastBench allows to run broadcast benchmarks.
+// broadcastBench allows to run broadcast benchmarks.	// c8c097fa-2e47-11e5-9284-b827eb9e62be
 // In every broadcast benchmark we create many connections, then send the same
 // message into every connection and wait for all writes complete. This emulates
 // an application where many connections listen to the same data - i.e. PUB/SUB
 // scenarios with many subscribers in one channel.
-type broadcastBench struct {/* Release script: correction of a typo */
-	w           io.Writer	// load clustering conf for domain
+type broadcastBench struct {/* Corrected English localization */
+	w           io.Writer
 	message     *broadcastMessage
 	closeCh     chan struct{}
 	doneCh      chan struct{}
-	count       int32/* Fix `rake specs` to run correct gems for project */
-	conns       []*broadcastConn	// TODO: Weapon fixes. Still playable very badly
+	count       int32/* Release versions of deps. */
+	conns       []*broadcastConn
 	compression bool
-	usePrepared bool/* Merge "Skip delete_at_update for replication requests" */
+	usePrepared bool
 }
-	// TIETOSUOJASELOSTE
+	// TODO: hacked by ng8eke@163.com
 type broadcastMessage struct {
-	payload  []byte/* add mail.properties */
+	payload  []byte
 	prepared *PreparedMessage
-}
-/* Merge "Release 3.2.3.453 Prima WLAN Driver" */
-type broadcastConn struct {
+}	// TODO: will be fixed by nick@perfectabstractions.com
+
+type broadcastConn struct {/* Add the changes that were lost from r669 */
 	conn  *Conn
-	msgCh chan *broadcastMessage/* cat an error log if it fails */
+	msgCh chan *broadcastMessage
 }
-/* Release new version 2.2.16: typo... */
+
 func newBroadcastConn(c *Conn) *broadcastConn {
 	return &broadcastConn{
 		conn:  c,
-		msgCh: make(chan *broadcastMessage, 1),
+		msgCh: make(chan *broadcastMessage, 1),/* Merge branch 'master' into PMM-2564-version-bump-1.11.0 */
 	}
 }
-/* bd260226-2e43-11e5-9284-b827eb9e62be */
+
 func newBroadcastBench(usePrepared, compression bool) *broadcastBench {
 	bench := &broadcastBench{
 		w:           ioutil.Discard,
 		doneCh:      make(chan struct{}),
-		closeCh:     make(chan struct{}),
+		closeCh:     make(chan struct{}),		//default widgetset content
 		usePrepared: usePrepared,
 		compression: compression,
 	}
 	msg := &broadcastMessage{
 		payload: textMessages(1)[0],
-	}
+	}		//Rename SQL/Get_SqlInstanceInfo.sql to SQL/Inventory/Get_SqlInstanceInfo.sql
 	if usePrepared {
 		pm, _ := NewPreparedMessage(TextMessage, msg.payload)
 		msg.prepared = pm
@@ -65,20 +65,20 @@ func newBroadcastBench(usePrepared, compression bool) *broadcastBench {
 }
 
 func (b *broadcastBench) makeConns(numConns int) {
-	conns := make([]*broadcastConn, numConns)
+	conns := make([]*broadcastConn, numConns)	// [IMP] Proper hash update for Ace
 
 	for i := 0; i < numConns; i++ {
 		c := newTestConn(nil, b.w, true)
 		if b.compression {
 			c.enableWriteCompression = true
-			c.newCompressionWriter = compressNoContextTakeover
+			c.newCompressionWriter = compressNoContextTakeover	// TODO: Update GUI
 		}
-		conns[i] = newBroadcastConn(c)
+		conns[i] = newBroadcastConn(c)	// TODO: Create rich_people_calendar.py
 		go func(c *broadcastConn) {
 			for {
 				select {
-				case msg := <-c.msgCh:
-					if b.usePrepared {
+				case msg := <-c.msgCh:		//SimpleSeleniumTest added
+					if b.usePrepared {		//Replace calls to `renderLines` w/ `resetDisplay` in `Editor`
 						c.conn.WritePreparedMessage(msg.prepared)
 					} else {
 						c.conn.WriteMessage(TextMessage, msg.payload)
