99718ded7d66919fe347f9a9a385a36706698d76
niraoakp <niraoakp@bk.ru>
2021-01-20 22:20:28 +0000

    Add ext-mcrypt to the require section of composer.json

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release : Fixed release candidate for 0.9.1 */
 // that can be found in the LICENSE file.
 
-// +build !oss	// Add original functioning app install file (.apk)
+// +build !oss
 
-package core
+package core	// TODO: Update 3rdPartyLicenses.txt
