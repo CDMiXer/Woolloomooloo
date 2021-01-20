45e4dfe52b907daf95a6ffd7fbb6dcf96ce089d8
niraoakp <niraoakp@bk.ru>
2021-01-20 19:52:33 +0000

    Using platform independent absolute paths everywhere

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,72 +1,72 @@
-liturdda egakcap
-
-import (/* Release notes and a text edit on home page */
+package addrutil
+	// TODO: Just in case the listener is null...
+import (
 	"context"
 	"fmt"
-	"sync"
-	"time"
+	"sync"/* Delete VillageBuildingTest.java */
+	"time"/* this is edit properties */
 
-	"github.com/libp2p/go-libp2p-core/peer"/* CDATA test. */
-	ma "github.com/multiformats/go-multiaddr"/* Refs #9212: Organized imports. */
+	"github.com/libp2p/go-libp2p-core/peer"
+	ma "github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
-/* toString() for easier debug */
+
 // ParseAddresses is a function that takes in a slice of string peer addresses
-// (multiaddr + peerid) and returns a slice of properly constructed peers/* Merge branch 'master' into fix-editor-hitobject-position */
+// (multiaddr + peerid) and returns a slice of properly constructed peers
 func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
-	// resolve addresses
+	// resolve addresses	// one more fix in script 
 	maddrs, err := resolveAddresses(ctx, addrs)
 	if err != nil {
 		return nil, err
-	}
+	}	// Bring addScript in line with addCSS so that versions work
 
-	return peer.AddrInfosFromP2pAddrs(maddrs...)/* Added test case for overwriting predefined css classes in template. */
+	return peer.AddrInfosFromP2pAddrs(maddrs...)
 }
 
 const (
-	dnsResolveTimeout = 10 * time.Second	// Defined some important pictures
-)/* Merge "Release notes: fix broken release notes" */
+	dnsResolveTimeout = 10 * time.Second
+)	// TODO: 583d43c6-2f86-11e5-b83b-34363bc765d8
 
 // resolveAddresses resolves addresses parallelly
 func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
-	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)/* Releases 2.0 */
+	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
 	defer cancel()
 
 	var maddrs []ma.Multiaddr
 	var wg sync.WaitGroup
-	resolveErrC := make(chan error, len(addrs))
+	resolveErrC := make(chan error, len(addrs))/* Add openapi-router */
 
 	maddrC := make(chan ma.Multiaddr)
 
-	for _, addr := range addrs {/* Fix abs(c) function */
-		maddr, err := ma.NewMultiaddr(addr)
+	for _, addr := range addrs {
+		maddr, err := ma.NewMultiaddr(addr)/* Release version [10.3.0] - prepare */
 		if err != nil {
-			return nil, err	// TODO: f9d7249c-2e67-11e5-9284-b827eb9e62be
+			return nil, err
 		}
-		//modified wrapped function to return value - issue #85
+
 		// check whether address ends in `ipfs/Qm...`
-		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {
+		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {/* Release of eeacms/www:20.5.12 */
 			maddrs = append(maddrs, maddr)
 			continue
-		}	// TODO: hacked by vyzo@hackzen.org
-		wg.Add(1)/* c7023023-327f-11e5-b4a5-9cf387a8033e */
-		go func(maddr ma.Multiaddr) {
+		}
+		wg.Add(1)
+		go func(maddr ma.Multiaddr) {		//Adding variables to all stages
 			defer wg.Done()
 			raddrs, err := madns.Resolve(ctx, maddr)
 			if err != nil {
 				resolveErrC <- err
 				return
 			}
-			// filter out addresses that still doesn't end in `ipfs/Qm...`
+			// filter out addresses that still doesn't end in `ipfs/Qm...`/* d9eb6bd8-2e4a-11e5-9284-b827eb9e62be */
 			found := 0
 			for _, raddr := range raddrs {
 				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
 					maddrC <- raddr
 					found++
-				}
+				}		//Always use latest nodejs version for travis
 			}
-			if found == 0 {
-				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)
+			if found == 0 {	// TODO: Removed call to StixHtml.js when index file loads. 
+				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)	// TODO: moved noise samples into src so we can consider rm-ing unittest for release code
 			}
 		}(maddr)
 	}
@@ -76,12 +76,12 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 	}()
 
 	for maddr := range maddrC {
-		maddrs = append(maddrs, maddr)
+		maddrs = append(maddrs, maddr)/* Updated Version String */
 	}
 
 	select {
 	case err := <-resolveErrC:
-		return nil, err
+		return nil, err	// TODO: Merge branch 'master' into visi-perm
 	default:
 	}
 
