14e0499d8e5a7698629fb02d37299e04ac70dfef
niraoakp <niraoakp@bk.ru>
2021-01-20 10:56:01 +0000

    chore: remove leftover slot from card package.json

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
 -- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (
- log_id    INTEGER PRIMARY KEY	// TODO: keydragzoom: added a temp fix for FF3.5.3 etc using transform. 
+CREATE TABLE IF NOT EXISTS logs (		//Update readFormFields.js
+ log_id    INTEGER PRIMARY KEY
 ,log_data  BLOB
-,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE/* MkReleases remove method implemented. */
-);
+,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
+);		//7jg1B3oJy0jzuDdMb62TAYU1tua17Vw6
