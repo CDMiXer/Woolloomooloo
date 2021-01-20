1815aa86f74bd9d3e5359a4389a7ef537d1649ec
niraoakp <niraoakp@bk.ru>
2021-01-20 03:19:24 +0000

    Fix for hook error not showing function name

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,17 +1,17 @@
 -- name: create-table-cron
 
-CREATE TABLE IF NOT EXISTS cron (
+CREATE TABLE IF NOT EXISTS cron (		//Update favicon to match new theme
  cron_id          SERIAL PRIMARY KEY
-,cron_repo_id     INTEGER		//Mudança nome projeto.
-,cron_name        VARCHAR(50)/* fix OL rendering */
+,cron_repo_id     INTEGER
+,cron_name        VARCHAR(50)	// TODO: hacked by mail@bitpshr.net
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
-,cron_event       VARCHAR(50)		//enrique nell, punto culminante to climax
-,cron_branch      VARCHAR(250)
+,cron_event       VARCHAR(50)
+,cron_branch      VARCHAR(250)		//Bumps the minor and resets the patch for v1.17.0.
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER	// Update facescan.py
+,cron_created     INTEGER
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
@@ -19,9 +19,9 @@ CREATE TABLE IF NOT EXISTS cron (
 );
 
 -- name: create-index-cron-repo
-
+		//remove space check
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
-
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
+		//forgot to add java files
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);/* Ok, now suppliers payment are correctly logged */
