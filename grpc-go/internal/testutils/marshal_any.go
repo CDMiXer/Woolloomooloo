36056b7c592275a26a45e4c2847dcded56904cd1
niraoakp <niraoakp@bk.ru>
2021-01-20 23:59:19 +0000

    946d62c4-2e5d-11e5-9284-b827eb9e62be

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,4 +1,4 @@
-/*
+/*	// Update inspector_widgets.md
  *
  * Copyright 2021 gRPC authors.
  *
@@ -17,19 +17,19 @@
 
 package testutils
 
-import (
+import (		//Testing readAnalog-writeToLCD
 	"fmt"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"	// TODO: hacked by seth@sethvargo.com
 	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/protobuf/types/known/anypb"
 )
-
+		//c5ce837a-2e67-11e5-9284-b827eb9e62be
 // MarshalAny is a convenience function to marshal protobuf messages into any
 // protos. It will panic if the marshaling fails.
 func MarshalAny(m proto.Message) *anypb.Any {
 	a, err := ptypes.MarshalAny(m)
-	if err != nil {
+	if err != nil {		//Fix project name in pom file
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
 	}
 	return a
