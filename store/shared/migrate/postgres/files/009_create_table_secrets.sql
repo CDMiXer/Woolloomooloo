17a1f388770c9b922c8b65f03e2f27ec1fef49c5
niraoakp <niraoakp@bk.ru>
2021-01-20 14:08:45 +0000

    Fixed bug with tile placement.

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -2,14 +2,14 @@
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                SERIAL PRIMARY KEY
-,secret_repo_id           INTEGER	// TODO: Added Usability Report
+,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
-,secret_data              BYTEA		//Merge pull request #109 from fkautz/pr_out_minor_code_cleanup
-,secret_pull_request      BOOLEAN/* TYPO3 CMS 6 Release (v1.0.0) */
+,secret_data              BYTEA
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);		//1.0.99-RC1
+);
 
 -- name: create-index-secrets-repo
 
