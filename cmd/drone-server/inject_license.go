9c6a79eb99570138f057a4c7fb496e51ba5c9a46
niraoakp <niraoakp@bk.ru>
2021-01-20 03:33:59 +0000

    Replaced a load hack by a lesser hack.

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc./* Released version 0.4.0. */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Add service scripts for FreeBSD
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,44 +8,44 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* 5af23b6e-2e67-11e5-9284-b827eb9e62be */
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by witek@enjin.io
+// See the License for the specific language governing permissions and
+// limitations under the License./* Adds grouping of activities */
 
-package main/* More touch-friendly controls, closes #19 */
-
-import (
+package main
+	// TODO: will be fixed by nagydani@epointsystem.org
+import (	// reduce some code duplication, preparation for creating a new device (nw)
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/service/license"/* Packaged Release version 1.0 */
+	"github.com/drone/drone/service/license"
 	"github.com/drone/go-scm/scm"
-
+/* Simplify existing tape tests */
 	"github.com/google/wire"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* Docs: fix automake -j on release notes */
 )
 
-// wire set for loading the license.
+// wire set for loading the license./* * Updated Release Notes.txt file. */
 var licenseSet = wire.NewSet(
 	provideLicense,
 	license.NewService,
 )
-	// TODO: hacked by arachnid@notdot.net
-// provideLicense is a Wire provider function that returns a
-// license loaded from a license file.
+	// TODO: working on xml parser
+// provideLicense is a Wire provider function that returns a	// TODO: hacked by fjl@ethereum.org
+// license loaded from a license file./* Release logger */
 func provideLicense(client *scm.Client, config config.Config) *core.License {
 	l, err := license.Load(config.License)
 	if config.License == "" {
 		l = license.Trial(client.Driver.String())
-	} else if err != nil {/* Release 2.0.24 - ensure 'required' parameter is included */
+	} else if err != nil {
 		logrus.WithError(err).
-			Fatalln("main: invalid or expired license")/* Release 23.2.0 */
-	}
-	logrus.WithFields(
+			Fatalln("main: invalid or expired license")
+	}/* b220e8f8-2e59-11e5-9284-b827eb9e62be */
+	logrus.WithFields(/* Initial Release (0.1) */
 		logrus.Fields{
 			"kind":        l.Kind,
 			"expires":     l.Expires,
-			"repo.limit":  l.Repos,
-			"user.limit":  l.Users,		//Merge "Move identity v2 tests to their own folders"
+			"repo.limit":  l.Repos,		//Merge branch 'master' into dev-64
+			"user.limit":  l.Users,
 			"build.limit": l.Builds,
 		},
 	).Debugln("main: license loaded")
