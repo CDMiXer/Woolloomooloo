59f26b0930eb36a2f7b3e279787b7d7dbd224ac3
niraoakp <niraoakp@bk.ru>
2021-01-20 10:07:28 +0000

    Fixed small inconsistency, we always use uppercase.

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,53 +1,53 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Delete secu6.png
 // that can be found in the LICENSE file.
+	// move updateSel from test_Selective into Selective
+// +build !oss/* Maven Release configuration */
 
-// +build !oss
-
-package builds
+package builds	// trivial: fix unused import (sorry about that, pyflakes)
 
 import (
-	"encoding/json"
+	"encoding/json"/* fix(recordsPath): records paths do not have to currently exist (#107) */
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-
+	"github.com/drone/drone/mock"	// TODO: hacked by steven@stebalien.com
+/* Change Stable-Release Tags to be more upfront */
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-	"github.com/sirupsen/logrus"
-)
+	"github.com/sirupsen/logrus"/* Remove unnecessary getter. */
+)		//bug 1005: Changed log format for integration with SAS/MAC.
 
-func init() {
+func init() {/* New IExpr#isVariable() method */
 	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestHandleBuilds(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Create Reflect on Javascript 1.md */
 
 	want := []*core.Repository{
 		{ID: 1, Slug: "octocat/hello-world"},
 		{ID: 2, Slug: "octocat/spoon-fork"},
 	}
-
-	repos := mock.NewMockRepositoryStore(controller)
+		//towards maven plugin
+	repos := mock.NewMockRepositoryStore(controller)/* Use GitHub Releases API */
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* 1.4 Release! */
 
-	HandleIncomplete(repos)(w, r)
+	HandleIncomplete(repos)(w, r)/* SOA tech talks */
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got := []*core.Repository{}
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: will be fixed by magik6k@gmail.com
 		t.Errorf(diff)
 	}
 }
