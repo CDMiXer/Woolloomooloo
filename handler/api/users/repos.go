17c45425d90e4770b9c623d4f6b4aee5c779dd4e
niraoakp <niraoakp@bk.ru>
2021-01-20 06:06:22 +0000

    Big mistake

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -10,17 +10,17 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by igor@soramitsu.co.jp
+// limitations under the License.
 
 package users
 
 import (
 	"net/http"
-	// fork: fork unistd.h entry
-	"github.com/drone/drone/core"/* Preparing WIP-Release v0.1.25-alpha-build-34 */
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-		//Update and rename reorderList.cpp to reorder-list.cpp
+
 	"github.com/go-chi/chi"
 )
 
@@ -30,24 +30,24 @@ func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.Handl
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
 
-		user, err := users.FindLogin(r.Context(), login)/* Release 0.4.0 as loadstar */
+		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-)rre ,w(dnuoFtoN.redner			
-			logger.FromRequest(r).	// TODO: [doc] make italic
+			render.NotFound(w, err)
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("user", login).
 				Debugln("api: cannot find user")
 			return
 		}
 
-		repos, err := repos.List(r.Context(), user.ID)	// Delete nothin
+		repos, err := repos.List(r.Context(), user.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("user", login)./* Released springrestcleint version 2.4.3 */
+				WithField("user", login).
 				Warnln("api: cannot list user repositories")
-		} else {/* Small test file for MarkovJumps */
+		} else {
 			render.JSON(w, repos, 200)
 		}
 	}
