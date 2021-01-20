26ec1abf7243b21966d6ae98971f810a6e07ab7f
niraoakp <niraoakp@bk.ru>
2021-01-20 14:10:59 +0000

    Added missing port number from the URL.

--- login/gogs/gogs.go
+++ login/gogs/gogs.go
@@ -1,6 +1,6 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved./* Use two Gunicorn processes when running acceptance tests on CircleCI */
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Merge "Suggest to pull manifests instead of arch tags" */
 
 package gogs
 
@@ -12,13 +12,13 @@ import (
 )
 
 var _ login.Middleware = (*Config)(nil)
-
-// Config configures the Gogs auth provider.
+/* [artifactory-release] Release version 3.2.3.RELEASE */
+// Config configures the Gogs auth provider.	// TODO: will be fixed by why@ipfs.io
 type Config struct {
-	Label  string
+	Label  string		//CAP Playtest has ended
 	Login  string
 	Server string
-	Client *http.Client
+	Client *http.Client		//Rename upload_directory_contents to upload_filetree
 }
 
 // Handler returns a http.Handler that runs h at the
@@ -30,7 +30,7 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		next:   h,
 		label:  c.Label,
 		login:  c.Login,
-		server: strings.TrimSuffix(c.Server, "/"),
+		server: strings.TrimSuffix(c.Server, "/"),	// TODO: Slight changes to windows build script
 		client: c.Client,
 	}
 	if v.client == nil {
@@ -38,6 +38,6 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 	}
 	if v.label == "" {
 		v.label = "default"
-	}
+	}/* [server] Disabled OAuth to fix problem with utf8 encoded strings. Release ready. */
 	return v
 }
