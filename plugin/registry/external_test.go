be0e95fd18022ee4ca7472a9b633e86290c3b396
niraoakp <niraoakp@bk.ru>
2021-01-20 15:36:23 +0000

    Rebuilt index with AmatsuZero

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 5b951350-2e46-11e5-9284-b827eb9e62be */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by why@ipfs.io
 // that can be found in the LICENSE file.
-		//Release of eeacms/www-devel:20.8.11
+
 // +build !oss
-/* Release of eeacms/eprtr-frontend:0.2-beta.41 */
+/* server files added */
 package registry
