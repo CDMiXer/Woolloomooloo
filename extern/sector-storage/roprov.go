ccb28a39fb8526d963959a4e8a199ff4ce7bc3c0
niraoakp <niraoakp@bk.ru>
2021-01-20 18:02:38 +0000

    Merge "T86902: Add new local setting for CSS modules load URI"

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,16 +1,16 @@
 package sectorstorage
 
-import (/* Release 0.5.7 */
+import (
 	"context"
 
-	"golang.org/x/xerrors"	// TODO: Update 40.1. Customizing endpoints.md
-		//Changed http to https in ROOT_URL
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* Adding Release instructions */
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-/* Improved example speed */
+
 type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
@@ -18,7 +18,7 @@ type readonlyProvider struct {
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
-		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")		//Delete main.52efa4a9.js.map
+		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
 
 	ctx, cancel := context.WithCancel(ctx)
@@ -28,12 +28,12 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
-	}		//Use :speak instead of :tts for consistency with method name
+	}
 	if !locked {
-		cancel()		//Add support for Ubuntu logs.
+		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
-		//streamlined version for debugging
+
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
 	return p, cancel, err
