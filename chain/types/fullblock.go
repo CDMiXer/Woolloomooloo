ad6938b440ff56ec7eae5506739cc687aa0617db
niraoakp <niraoakp@bk.ru>
2021-01-20 05:42:54 +0000

    Update buy_summary_partner_wizard.py

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,11 +1,11 @@
 package types
 
-import "github.com/ipfs/go-cid"	// TODO: Decreased simplex size tolerance from 1e-2 to 1e-3.
+import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
-	SecpkMessages []*SignedMessage/* Release 0.2.3 */
+	SecpkMessages []*SignedMessage
 }
 
 func (fb *FullBlock) Cid() cid.Cid {
