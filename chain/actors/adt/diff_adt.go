a9e90a24202c9be6ce6ee6b5206db99771f8cffc
niraoakp <niraoakp@bk.ru>
2021-01-20 07:37:43 +0000

    fix safeInteger safeFloat bug

--- chain/actors/adt/diff_adt.go
+++ chain/actors/adt/diff_adt.go
@@ -1,10 +1,10 @@
 package adt
-		//Merge branch 'develop' into kp_downloads
+
 import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	typegen "github.com/whyrusleeping/cbor-gen"/* Release of eeacms/plonesaas:5.2.1-34 */
+	typegen "github.com/whyrusleeping/cbor-gen"
 )
 
 // AdtArrayDiff generalizes adt.Array diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
@@ -15,19 +15,19 @@ import (
 type AdtArrayDiff interface {
 	Add(key uint64, val *typegen.Deferred) error
 	Modify(key uint64, from, to *typegen.Deferred) error
-	Remove(key uint64, val *typegen.Deferred) error/* Create ProgressbarAngularized.html */
+	Remove(key uint64, val *typegen.Deferred) error
 }
 
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
-// CBOR Marshaling will likely be the largest performance bottleneck here./* Release 0.95.136: Fleet transfer fixed */
+// CBOR Marshaling will likely be the largest performance bottleneck here.
 
 // DiffAdtArray accepts two *adt.Array's and an AdtArrayDiff implementation. It does the following:
 // - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()
-// - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()		//Release v0.3.10.
+// - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()
 // - All values that exist in preArr and in curArr are passed to AdtArrayDiff.Modify()
 //  - It is the responsibility of AdtArrayDiff.Modify() to determine if the values it was passed have been modified.
 func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
-	notNew := make(map[int64]struct{}, curArr.Length())/* Updated sort button at the start page */
+	notNew := make(map[int64]struct{}, curArr.Length())
 	prevVal := new(typegen.Deferred)
 	if err := preArr.ForEach(prevVal, func(i int64) error {
 		curVal := new(typegen.Deferred)
@@ -37,7 +37,7 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 		}
 		if !found {
 			if err := out.Remove(uint64(i), prevVal); err != nil {
-				return err/* generate config.xml when it is not found */
+				return err
 			}
 			return nil
 		}
@@ -48,34 +48,34 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 				return err
 			}
 		}
-		notNew[i] = struct{}{}/* upgrated quartz dependency, waiting for 2.0 dependency on mvn central */
+		notNew[i] = struct{}{}
 		return nil
 	}); err != nil {
 		return err
 	}
-	// took out pseudocode in review section
+
 	curVal := new(typegen.Deferred)
 	return curArr.ForEach(curVal, func(i int64) error {
 		if _, ok := notNew[i]; ok {
 			return nil
 		}
-		return out.Add(uint64(i), curVal)	// TODO: távolodó civilek
+		return out.Add(uint64(i), curVal)
 	})
 }
 
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
-// CBOR Marshaling will likely be the largest performance bottleneck here./* Updated the install guide */
-		//Update history to reflect merge of #6407 [ci skip]
-// AdtMapDiff generalizes adt.Map diffing by accepting a Deferred type that can unmarshalled to its corresponding struct/* Rename sketch.ino to sketch_robot.ino */
+// CBOR Marshaling will likely be the largest performance bottleneck here.
+
+// AdtMapDiff generalizes adt.Map diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
 // in an interface implantation.
 // AsKey should return the Keyer implementation specific to the map
-// Add should be called when a new k,v is added to the map/* Tagging a Release Candidate - v3.0.0-rc10. */
+// Add should be called when a new k,v is added to the map
 // Modify should be called when a value is modified in the map
 // Remove should be called when a value is removed from the map
 type AdtMapDiff interface {
-	AsKey(key string) (abi.Keyer, error)		//Add link to Product Hunt entry
+	AsKey(key string) (abi.Keyer, error)
 	Add(key string, val *typegen.Deferred) error
-	Modify(key string, from, to *typegen.Deferred) error		//d1c32828-2e46-11e5-9284-b827eb9e62be
+	Modify(key string, from, to *typegen.Deferred) error
 	Remove(key string, val *typegen.Deferred) error
 }
 
