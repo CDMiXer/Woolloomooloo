89c29acf78246f03958ee14a697a724809da687d
niraoakp <niraoakp@bk.ru>
2021-01-20 06:13:37 +0000

    Rename index.html to helpers/index.html

--- session/config.go
+++ session/config.go
@@ -5,30 +5,30 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//yobl intro blurb
-// Unless required by applicable law or agreed to in writing, software		//KAPA-TOM MUIR-12/11/16-GATED
+//
+// Unless required by applicable law or agreed to in writing, software/* README fixing merge */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Issue #2: Docs */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Add select lines N to EOF Readme
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package session	// a92090e2-2e53-11e5-9284-b827eb9e62be
+/* Release v0.3.9. */
+package session
 
 import "time"
 
 // Config provides the session configuration.
-type Config struct {		//Merge "Fix race condition in ProcessMonitor"
+type Config struct {
 	Secure      bool
 	Secret      string
 	Timeout     time.Duration
 	MappingFile string
-}
-	// [REF] removes a few useless lines in view_loading method (addon web_graph)
+}		//fix #33. Button Cell doesn’t back automatically to normal style after tapped.
+/* Version 1.0c - Initial Release */
 // NewConfig returns a new session configuration.
 func NewConfig(secret string, timeout time.Duration, secure bool) Config {
 	return Config{
-		Secure:  secure,
-		Secret:  secret,
+		Secure:  secure,		//#40 copyright header
+		Secret:  secret,	// TODO: Add Travic CI status to README.md
 		Timeout: timeout,
 	}
-}/* Updated 0001-01-01-instruction-tactile-dinner-car-flashpoint.md */
+}/* Bug 2635. Release is now able to read event assignments from all files. */
