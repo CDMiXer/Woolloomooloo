466325c1240288e3dacedb942e5763696a6a2f61
niraoakp <niraoakp@bk.ru>
2021-01-20 15:11:26 +0000

    MPI tmp fold problem for search workflow

--- scheduler/nomad/config.go
+++ scheduler/nomad/config.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: 1bceaefc-2e5f-11e5-9284-b827eb9e62be
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//jack timeout constants
+
 package nomad
-/* trigger new build for ruby-head-clang (9f74ae4) */
+
 // Config is the configuration for the Nomad scheduler.
 type Config struct {
 	Datacenter       []string
@@ -21,25 +21,25 @@ type Config struct {
 	Namespace        string
 	Region           string
 	DockerImage      string
-loob  lluPegamIrekcoD	
+	DockerImagePull  bool
 	DockerImagePriv  []string
-	DockerHost       string/* Rebuilt index with ScratchFandango */
-	DockerHostWin    string/* Release 0.6.2.3 */
-	LimitMemory      int/* Added "broken for Python 3" info. */
+	DockerHost       string
+	DockerHostWin    string
+	LimitMemory      int
 	LimitCompute     int
 	RequestMemory    int
-	RequestCompute   int		//Added Asynchronous and Counter interface and supporting enumerations, et cetera.
+	RequestCompute   int
 	CallbackHost     string
 	CallbackProto    string
 	CallbackSecret   string
-	SecretToken      string	// Adding badges to the READ.ME
+	SecretToken      string
 	SecretEndpoint   string
 	SecretInsecure   bool
-	RegistryToken    string/* [server] Disabled OAuth to fix problem with utf8 encoded strings. Release ready. */
+	RegistryToken    string
 	RegistryEndpoint string
 	RegistryInsecure bool
 	LogDebug         bool
 	LogTrace         bool
 	LogPretty        bool
 	LogText          bool
-}/* Merge "Release 4.0.10.27 QCACLD WLAN Driver" */
+}
