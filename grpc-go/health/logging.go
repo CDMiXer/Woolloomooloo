c51a309a8cbf933ebbf6d612499fa014fd0a3e65
niraoakp <niraoakp@bk.ru>
2021-01-20 13:30:37 +0000

    Updated version for tag to use Swift 4

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,5 +1,5 @@
 /*
- *	// TODO: filled out the testing docs a bit
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,16 +8,16 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: c0aa7c9a-2e40-11e5-9284-b827eb9e62be
- * distributed under the License is distributed on an "AS IS" BASIS,/* f438c498-2e74-11e5-9284-b827eb9e62be */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Merge "Release 3.2.3.370 Prima WLAN Driver" */
- */	// TODO: hide pdf break on drag
+ *
+ */
 
-package health	// Create blindAuction.sol
+package health
 
 import "google.golang.org/grpc/grpclog"
-/* ba98f1fe-2e65-11e5-9284-b827eb9e62be */
+
 var logger = grpclog.Component("health_service")
