0a90d6d75f2ed707a997e8be261bf3d0178378f1
niraoakp <niraoakp@bk.ru>
2021-01-20 09:13:10 +0000

    Make silence

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -8,24 +8,24 @@ class Resource : ComponentResource
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }/* Update gene info page to reflect changes for July Release */
-}/* Release FIWARE4.1 with attached sources */
-/* Release v0.2.0 readme updates */
-// Scenario #4 - change the type of a component/* **kwargs --> create */
+    }
+}
+
+// Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
 {
     private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFour", name, options)/* Release of eeacms/www-devel:20.9.29 */
+        : base("my:module:ComponentFour", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
 
-class Program		//FIX: add classes to input groups and move tips
-{/* Merge branch 'master' into faster_deletes */
-    static Task<int> Main(string[] args)	// TODO: hacked by juan@benet.ai
+class Program
+{
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
         {
