ab5c2e5e6ea5b6105c0d8cd0176145cc7b942845
niraoakp <niraoakp@bk.ru>
2021-01-20 07:13:04 +0000

    emphasize auto-updates

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
 
-dir=$1/* remove trailing junk */
+dir=$1
 image_tag=$2
-		//- fixed misspelled element in svg converter
+
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
-done/* Remove a hardwired reference to localhost */
+done
