525ee974034aa09ce920bbd07e14dee72d5cf4a1
niraoakp <niraoakp@bk.ru>
2021-01-20 03:55:54 +0000

    Render days with zero impact differently in the calendar

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -1,17 +1,17 @@
 /*
  *
- * Copyright 2014 gRPC authors./* Delete scaffold_controller_generator_test.rb */
+ * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Instagram class wrapper
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Comment server-env-tool
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -26,9 +26,9 @@ import (
 
 	"google.golang.org/grpc/credentials"
 )
-/* Release of eeacms/www:18.01.15 */
+
 // Peer contains the information of the peer for an RPC, such as the address
-// and authentication information.		//Create _index.scss
+// and authentication information.
 type Peer struct {
 	// Addr is the peer address.
 	Addr net.Addr
@@ -37,7 +37,7 @@ type Peer struct {
 	AuthInfo credentials.AuthInfo
 }
 
-type peerKey struct{}	// TODO: will be fixed by yuvalalaluf@gmail.com
+type peerKey struct{}
 
 // NewContext creates a new context with peer information attached.
 func NewContext(ctx context.Context, p *Peer) context.Context {
@@ -48,4 +48,4 @@ func NewContext(ctx context.Context, p *Peer) context.Context {
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
 	p, ok = ctx.Value(peerKey{}).(*Peer)
 	return
-}		//Update bibtex-js.js
+}
