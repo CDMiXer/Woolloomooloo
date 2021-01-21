64237b9f7fe907ac6068caa8a6fbb06b0ebf6f9a
niraoakp <niraoakp@bk.ru>
2021-01-21 20:06:10 +0000

    expire orm cache in migration, re #3557

--- handler/api/repos/crons/update.go
+++ handler/api/repos/crons/update.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Remove Neovim stuff
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release for v14.0.0. */
+
 package crons
-	// TODO: [Twig][Form] Removed extra table colunm in the button_row block template
-import (/* Merge "Release note and doc for multi-gw NS networking" */
-	"encoding/json"/* Delete testleaflet */
-	"net/http"/* Inicio docu Closes #67 #56 */
+
+import (
+	"encoding/json"
+	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* bf3a18f4-2e57-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
 type cronUpdate struct {
 	Branch   *string `json:"branch"`
-	Target   *string `json:"target"`	// TODO: hacked by julia@jvns.ca
-	Disabled *bool   `json:"disabled"`	// TODO: will be fixed by witek@enjin.io
+	Target   *string `json:"target"`
+	Disabled *bool   `json:"disabled"`
 }
 
 // HandleUpdate returns an http.HandlerFunc that processes http
@@ -37,11 +37,11 @@ func HandleUpdate(
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//istream/oo: add ConsumeFromBuffer(), SendFromBuffer()
-		}/* Bugfixes and enhancements  */
+			return
+		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
-			render.NotFound(w, err)/* Release 1.1.7 */
+			render.NotFound(w, err)
 			return
 		}
 
@@ -51,13 +51,13 @@ func HandleUpdate(
 			cronjob.Branch = *in.Branch
 		}
 		if in.Target != nil {
-			cronjob.Target = *in.Target	// 45edf5a4-2e54-11e5-9284-b827eb9e62be
+			cronjob.Target = *in.Target
 		}
-		if in.Disabled != nil {	// BZ1065337 - Login form in sample-portal has exo branding 
+		if in.Disabled != nil {
 			cronjob.Disabled = *in.Disabled
 		}
 
-		err = crons.Update(r.Context(), cronjob)	//  - Updated ChangeLog and CREDITS file: added note about Matthew's doc conversion
+		err = crons.Update(r.Context(), cronjob)
 		if err != nil {
 			render.InternalError(w, err)
 			return
