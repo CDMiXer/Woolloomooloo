f18db65c28a24644e385e92d6134674dc23a98f1
niraoakp <niraoakp@bk.ru>
2021-01-21 01:12:42 +0000

    Added "set hidden" and buffer shortcuts to vimrc.

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -1,11 +1,11 @@
-config storageAccountNameParam string {	// TODO: correct path for classes
-}
+config storageAccountNameParam string {
+}	// Version bump and gemspec
 
 config resourceGroupNameParam string {
-}
+}/* Dictionary exclude col should be dimension not measure (#503) */
 
-resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {
-	name = resourceGroupNameParam/* Preview Release (Version 0.5 / VersionCode 5) */
+resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {/* Document `loadAddon` and add guide for using addons */
+	name = resourceGroupNameParam
 })
 
 config locationParam string {
@@ -14,21 +14,21 @@ config locationParam string {
 
 config storageAccountTierParam string {
     default = "Standard"
-}
+}/* Merged release/Inital_Release into master */
 
 config storageAccountTypeReplicationParam string {
-    default = "LRS"
+    default = "LRS"	// TODO: Amended /ToS-Load/jagex.json
 }
 
 resource storageAccountResource "azure:storage/account:Account" {
 	name = storageAccountNameParam
 	accountKind = "StorageV2"
-	location = locationParam/* Release candidate 2.3 */
-	resourceGroupName = resourceGroupNameParam
-	accountTier = storageAccountTierParam
-	accountReplicationType = storageAccountTypeReplicationParam/* update to latest space:event-sourcing version */
-}/* @Release [io7m-jcanephora-0.34.5] */
-
+	location = locationParam
+	resourceGroupName = resourceGroupNameParam/* uaa.port no longer used */
+	accountTier = storageAccountTierParam	// -Mise en place de plusieurs controller
+	accountReplicationType = storageAccountTypeReplicationParam
+}
+/* Updated Releases section */
 output storageAccountNameOut {
-	value = storageAccountResource.name
+	value = storageAccountResource.name	// TODO: will be fixed by ligi@ligi.de
 }
