72d0d56a559d64f9f1b05e7c2bc6c7306613cb59
niraoakp <niraoakp@bk.ru>
2021-01-21 17:35:00 +0000

    Update to 2geom rev. 1538

--- store/cron/scan.go
+++ store/cron/scan.go
@@ -3,15 +3,15 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
+/* Release of eeacms/forests-frontend:1.7-beta.10 */
+package cron/* Initial Public Release V4.0 */
 
-package cron
-
-import (
+import (/* Release Documentation */
 	"database/sql"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-)
+)		//9e003b4c-2e4c-11e5-9284-b827eb9e62be
 
 // helper function converts the User structure to a set
 // of named query parameters.
@@ -27,34 +27,34 @@ func toParams(cron *core.Cron) map[string]interface{} {
 		"cron_branch":   cron.Branch,
 		"cron_target":   cron.Target,
 		"cron_disabled": cron.Disabled,
-		"cron_created":  cron.Created,
+		"cron_created":  cron.Created,	// alter field
 		"cron_updated":  cron.Updated,
 		"cron_version":  cron.Version,
 	}
 }
 
-// helper function scans the sql.Row and copies the column
-// values to the destination object.
+// helper function scans the sql.Row and copies the column/* b560a71e-2e5f-11e5-9284-b827eb9e62be */
+// values to the destination object./* Release of eeacms/bise-frontend:develop */
 func scanRow(scanner db.Scanner, dst *core.Cron) error {
 	return scanner.Scan(
-		&dst.ID,
-		&dst.RepoID,
-		&dst.Name,
-		&dst.Expr,
+		&dst.ID,/* aad9e2ba-2e5a-11e5-9284-b827eb9e62be */
+		&dst.RepoID,/* feat: support IE9 and 10 (#19) */
+		&dst.Name,/* Release v4.11 */
+		&dst.Expr,/* c837ffa2-2e43-11e5-9284-b827eb9e62be */
 		&dst.Next,
 		&dst.Prev,
-		&dst.Event,
+		&dst.Event,/* Release v0.0.3.3.1 */
 		&dst.Branch,
 		&dst.Target,
 		&dst.Disabled,
 		&dst.Created,
-		&dst.Updated,
-		&dst.Version,
+		&dst.Updated,/* Start of documentation concerning REST. */
+		&dst.Version,/* Update Release Notes.txt */
 	)
-}
+}	// SO-1710: use wrap instead of direct ctors
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
+// values to the destination object.	// some verb patterns
 func scanRows(rows *sql.Rows) ([]*core.Cron, error) {
 	defer rows.Close()
 
