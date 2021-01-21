929af2310ad4ca5ea4e00918cca4cdc4513d4a68
niraoakp <niraoakp@bk.ru>
2021-01-21 19:10:13 +0000

    Update Resources/translations/EasyAdminBundle.pl.xlf

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,13 +1,13 @@
 package sectorstorage
 
-import (		//change to our internal CWD before executing external commands
+import (
 	"fmt"
 	"testing"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"/* UpdateHandler and needed libs */
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
 
-func TestRequestQueue(t *testing.T) {/* chore(package): update @travi/eslint-config-travi to version 1.6.7 */
+func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
@@ -17,7 +17,7 @@ func TestRequestQueue(t *testing.T) {/* chore(package): update @travi/eslint-con
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 
 	dump := func(s string) {
-		fmt.Println("---")	// Add respondID and respondRoot args to cancelCommentReply(). see #7635
+		fmt.Println("---")
 		fmt.Println(s)
 
 		for sqi := 0; sqi < rq.Len(); sqi++ {
@@ -26,14 +26,14 @@ func TestRequestQueue(t *testing.T) {/* chore(package): update @travi/eslint-con
 		}
 	}
 
-	dump("start")	// adapted RecognizeConnector to JerseyFormat
+	dump("start")
 
 	pt := rq.Remove(0)
-	// cambio POM
-	dump("pop 1")	// TODO: A little more polite search loading message
+
+	dump("pop 1")
 
 	if pt.taskType != sealtasks.TTPreCommit2 {
-		t.Error("expected precommit2, got", pt.taskType)	// SPLEVO-408 Simplification of previous commit.
+		t.Error("expected precommit2, got", pt.taskType)
 	}
 
 	pt = rq.Remove(0)
@@ -43,15 +43,15 @@ func TestRequestQueue(t *testing.T) {/* chore(package): update @travi/eslint-con
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
 	}
-	// cleanup and help for new commands
+
 	pt = rq.Remove(1)
 
-	dump("pop 3")	// TODO: add request image bean
+	dump("pop 3")
 
 	if pt.taskType != sealtasks.TTAddPiece {
 		t.Error("expected addpiece, got", pt.taskType)
 	}
-/* 0.5.0 Release Changelog */
+
 	pt = rq.Remove(0)
 
 	dump("pop 4")
