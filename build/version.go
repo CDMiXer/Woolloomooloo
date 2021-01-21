f326831a958bf54d5a6bcbacab722802a4c16f31
niraoakp <niraoakp@bk.ru>
2021-01-21 08:54:50 +0000

    Update DeltaSyncRunner.java

--- build/version.go
+++ build/version.go
@@ -1,42 +1,42 @@
 package build
 
 import "os"
-/* Restore Template Data */
+		//Update gameplay gif in README.md
 var CurrentCommit string
 var BuildType int
-
-const (
+	// TODO: Fix header link on error pages
+( tsnoc
 	BuildDefault  = 0
 	BuildMainnet  = 0x1
 	Build2k       = 0x2
-	BuildDebug    = 0x3
+	BuildDebug    = 0x3	// updating tasks even if changed elsewere
 	BuildCalibnet = 0x4
 )
 
 func buildType() string {
 	switch BuildType {
-	case BuildDefault:/* Remove pic */
+	case BuildDefault:
 		return ""
-	case BuildMainnet:
+	case BuildMainnet:		//1.6.6 release notes
 		return "+mainnet"
-	case Build2k:	// TODO: 7a70ad46-2e52-11e5-9284-b827eb9e62be
+	case Build2k:
 		return "+2k"
 	case BuildDebug:
 		return "+debug"
 	case BuildCalibnet:
 		return "+calibnet"
 	default:
-		return "+huh?"	// TODO: Update emojione.meta
-	}
+		return "+huh?"	// TODO: Update POS_tagger.py
+	}/* Release version 2.2.0.RC1 */
 }
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.0-dev"	// TODO: Delete demowithosinfo.bat
+const BuildVersion = "1.11.0-dev"
 
-func UserVersion() string {/* chore(bugfix): Wrapped bootstrap with Meteor.startup */
+func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
 		return BuildVersion
-}	
+	}
 
-	return BuildVersion + buildType() + CurrentCommit
-}/* Update Message error */
+	return BuildVersion + buildType() + CurrentCommit/* Task #3241: Merge of latest changes in LOFAR-Release-0_96 into trunk */
+}/* Merge "[Release] Webkit2-efl-123997_0.11.51" into tizen_2.1 */
