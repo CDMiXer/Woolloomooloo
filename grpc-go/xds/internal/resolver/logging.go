9dcd912d66bd377683073d4696bc474de1720f10
niraoakp <niraoakp@bk.ru>
2021-01-21 01:42:30 +0000

    Fix bug #768

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,9 +1,9 @@
-/*	// TODO: will be fixed by mail@bitpshr.net
- */* v1.63 - slow reboot on profile load */
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Merge "Reduce jank in wifi settings" into mnc-dev
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-	// TODO: Add method to check validity of name 
+
 package resolver
 
 import (
@@ -27,7 +27,7 @@ import (
 
 const prefix = "[xds-resolver %p] "
 
-var logger = grpclog.Component("xds")	// TODO: hacked by igor@soramitsu.co.jp
+var logger = grpclog.Component("xds")
 
 func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
