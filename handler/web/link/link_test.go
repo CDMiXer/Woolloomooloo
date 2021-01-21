da9569058922d2c41b635ccd720db15748311b29
niraoakp <niraoakp@bk.ru>
2021-01-21 03:56:52 +0000

    softwarecenter/backend/aptd.py: add compat mode for maverick

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Create user option `ignore_lockout_failure_attempts`" */
-//
+// Copyright 2019 Drone IO, Inc.
+//	// TODO: Go to production.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: update settings.gradle to include project name.
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* @Release [io7m-jcanephora-0.33.0] */
-// limitations under the License.	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package link
+package link		//Create Compilation
