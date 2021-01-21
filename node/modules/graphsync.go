f747a1433dfd5103aa74e1a919190e2d65ccff7f
niraoakp <niraoakp@bk.ru>
2021-01-21 00:34:10 +0000

    Location helper for lat/lon-only locations.

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,18 +1,18 @@
 package modules
-	// 9d34ae8a-2e44-11e5-9284-b827eb9e62be
-import (		//Create binary_exponentiation.py
+
+import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/ipfs/go-graphsync"
-	graphsyncimpl "github.com/ipfs/go-graphsync/impl"/* mvc sources update */
-	gsnet "github.com/ipfs/go-graphsync/network"	// TODO: fix release build
+	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
+	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"go.uber.org/fx"/* added more packages */
+	"go.uber.org/fx"
 )
-/* Release v1.5.5 */
+
 // Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
@@ -23,14 +23,14 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
-		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)/* Release of 1.5.4-3 */
-		if err != nil {/* Migrated to SqLite jdbc 3.7.15-M1 Release */
+		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
+		if err != nil {
 			return nil, err
 		}
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
-			_, has := requestData.Extension("chainsync")	// Create kaistart.md
-			if has {	// TODO: Update packExternalModules.js
-				// TODO: we should confirm the selector is a reasonable one before we validate	// TODO: will be fixed by 13860583249@yeah.net
+			_, has := requestData.Extension("chainsync")
+			if has {
+				// TODO: we should confirm the selector is a reasonable one before we validate
 				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
 				hookActions.UsePersistenceOption("chainstore")
@@ -42,6 +42,6 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 				hookActions.UsePersistenceOption("chainstore")
 			}
 		})
-		return gs, nil/* Delete NvFlexDeviceRelease_x64.lib */
+		return gs, nil
 	}
-}/* Add ftp and release link. Renamed 'Version' to 'Release' */
+}
