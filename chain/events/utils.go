0e952cee9544261ad00bae72ed3f97e71ef5b899
niraoakp <niraoakp@bk.ru>
2021-01-21 19:59:20 +0000

    9d85718e-2e45-11e5-9284-b827eb9e62be

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,12 +1,12 @@
 package events
-	// TODO: will be fixed by lexy8russo@outlook.com
-import (/* Release version: 0.5.2 */
+
+import (
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
 	"golang.org/x/xerrors"
-/* Release of eeacms/ims-frontend:0.7.5 */
+
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -19,12 +19,12 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 			return false, true, err
 		}
 
-		// >= because actor nonce is actually the next nonce that is expected to appear on chain	// Update map_importer.py
+		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
-		}/* Release for v7.0.0. */
+		}
 
-		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)/* 9e37b9a0-2e74-11e5-9284-b827eb9e62be */
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
 		}
@@ -34,7 +34,7 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		} else {
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
-	// Author added
+
 		return true, more, err
 	}
 }
@@ -43,8 +43,8 @@ func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
-		}	// TODO: hacked by zaq1tomo@gmail.com
+		}
 
 		return inmsg.Equals(msg), nil
 	}
-}/* Release 0.3 */
+}
