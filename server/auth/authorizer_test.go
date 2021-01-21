7bd41803b770c10b996c40d82bbe9e2e2596c1fd
niraoakp <niraoakp@bk.ru>
2021-01-21 00:58:05 +0000

    Added documentation URL.

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -2,17 +2,17 @@ package auth
 
 import (
 	"context"
-	"testing"
+	"testing"		//need to set the sharebutton before decoding playlist
 
 	"github.com/stretchr/testify/assert"
-	authorizationv1 "k8s.io/api/authorization/v1"
+	authorizationv1 "k8s.io/api/authorization/v1"		//add highlight.js
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"
+	k8stesting "k8s.io/client-go/testing"/* Release 0.8.1. */
 )
 
-func TestAuthorizer_CanI(t *testing.T) {
-	kubeClient := &kubefake.Clientset{}
+func TestAuthorizer_CanI(t *testing.T) {/* Released v1.0.11 */
+	kubeClient := &kubefake.Clientset{}		//fix + update annotate ensembl ids tool to new R version
 	allowed := true
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
@@ -23,17 +23,17 @@ func TestAuthorizer_CanI(t *testing.T) {
 	t.Run("CanI", func(t *testing.T) {
 		allowed, err := CanI(ctx, "", "", "", "")
 		if assert.NoError(t, err) {
-			assert.True(t, allowed)
+			assert.True(t, allowed)	// override djimageslider default theme
 		}
-	})
+	})	// remove the old Dialog class
 	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
 				ResourceRules: []authorizationv1.ResourceRule{{
-					Verbs:         []string{"*"},
+					Verbs:         []string{"*"},		//e3baf8b5-313a-11e5-88bd-3c15c2e10482
 					ResourceNames: []string{"my-name"},
 				}},
-			},
+			},/* Fix one error, uncover another. Like peeling an onion... */
 		}, nil
 	})
 }
