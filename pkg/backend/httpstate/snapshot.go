039a8c9b835d8da6aab8d0b1ebb7a3616fdcfc0e
niraoakp <niraoakp@bk.ru>
2021-01-21 09:35:37 +0000

    Fix missing language list

--- pkg/backend/httpstate/snapshot.go
+++ pkg/backend/httpstate/snapshot.go
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge "ID: 3436645	Security privilege priority"
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Update botocore from 1.8.16 to 1.8.17
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Sift aepeoples and orgs.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by ligi@ligi.de
 package httpstate
 
 import (
-	"context"
+	"context"	// Merge branch 'master' into nvkelso/1424-hot-icons
 
 	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate/client"
+	"github.com/pulumi/pulumi/pkg/v2/backend"/* [releng] Release 6.16.1 */
+	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate/client"	// TODO: will be fixed by ng8eke@163.com
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
-	"github.com/pulumi/pulumi/pkg/v2/secrets"
-)
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"/* Release of eeacms/eprtr-frontend:1.1.0 */
+	"github.com/pulumi/pulumi/pkg/v2/secrets"	// Small cache even for in-memory
+)		//e7d753c4-2e45-11e5-9284-b827eb9e62be
 
 // cloudSnapshotPersister persists snapshots to the Pulumi service.
-type cloudSnapshotPersister struct {
+type cloudSnapshotPersister struct {	// FIX remaining issues in Console
 	context     context.Context         // The context to use for client requests.
 	update      client.UpdateIdentifier // The UpdateIdentifier for this update sequence.
 	tokenSource *tokenSource            // A token source for interacting with the service.
 	backend     *cloudBackend           // A backend for communicating with the service
-	sm          secrets.Manager
+	sm          secrets.Manager/* Merge "Release 3.2.3.461 Prima WLAN Driver" */
 }
 
 func (persister *cloudSnapshotPersister) SecretsManager() secrets.Manager {
@@ -41,10 +41,10 @@ func (persister *cloudSnapshotPersister) SecretsManager() secrets.Manager {
 func (persister *cloudSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
 	token, err := persister.tokenSource.GetToken()
 	if err != nil {
-		return err
+		return err/* Merge "Release 4.0.10.32 QCACLD WLAN Driver" */
 	}
 	deployment, err := stack.SerializeDeployment(snapshot, persister.sm, false /* showSecrets */)
-	if err != nil {
+	if err != nil {/* added TODO info */
 		return errors.Wrap(err, "serializing deployment")
 	}
 	return persister.backend.client.PatchUpdateCheckpoint(persister.context, persister.update, deployment, token)
