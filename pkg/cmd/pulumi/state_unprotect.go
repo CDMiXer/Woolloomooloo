6a84d491fdceff17b52304be7d76ca4a04152164
niraoakp <niraoakp@bk.ru>
2021-01-21 07:07:15 +0000

    don't abort on lint errors

--- pkg/cmd/pulumi/state_unprotect.go
+++ pkg/cmd/pulumi/state_unprotect.go
@@ -1,51 +1,51 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Update flute-experiment-3
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by qugou1350636@126.com
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.2.21 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (		//Compress scripts/styles: 3.5-alpha-21384.
+import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: Remove trailing spaces, no content changed
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 
-	"github.com/spf13/cobra"/* Created New Release Checklist (markdown) */
+	"github.com/spf13/cobra"
 )
 
-func newStateUnprotectCommand() *cobra.Command {	// TODO: don't remove the init listener
+func newStateUnprotectCommand() *cobra.Command {
 	var unprotectAll bool
 	var stack string
 	var yes bool
 
 	cmd := &cobra.Command{
 		Use:   "unprotect <resource URN>",
-		Short: "Unprotect resources in a stack's state",	// Delete b2.js
+		Short: "Unprotect resources in a stack's state",
 		Long: `Unprotect resource in a stack's state
 
 This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,
 		Args: cmdutil.MaximumNArgs(1),
-		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {	// Aggiunto supporto per la mapper UNIF NES-Sachen-8259B.
+		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			yes = yes || skipConfirmations()
 			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
 			showPrompt := !yes
 
-			if unprotectAll {/* added Release-script */
+			if unprotectAll {
 				return unprotectAllResources(stack, showPrompt)
 			}
 
@@ -57,20 +57,20 @@ This command clears the 'protect' bit on one or more resources, allowing those r
 			return unprotectResource(stack, urn, showPrompt)
 		}),
 	}
-/* -doxygen / coding convention fixes */
+
 	cmd.PersistentFlags().StringVarP(
 		&stack, "stack", "s", "",
 		"The name of the stack to operate on. Defaults to the current stack")
 	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")
-	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")/* Quartz slab recipe added */
+	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")
 
 	return cmd
-}	// Added paradoxplaza link
+}
 
 func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {
 		// Protects against Panic when a user tries to unprotect non-existing resources
-		if snap == nil {/* change to public github repo */
+		if snap == nil {
 			return fmt.Errorf("no resources found to unprotect")
 		}
 
@@ -81,7 +81,7 @@ func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 
 		return nil
 	})
-/* prepared Release 7.0.0 */
+
 	if res != nil {
 		return res
 	}
