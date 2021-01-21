6e15490d45b2b47693afb0df3493b64cca715c37
niraoakp <niraoakp@bk.ru>
2021-01-21 17:53:33 +0000

    Moved PDF core classes from Core/Lib/Export to Core/Lib/PDF.

--- operator/runner/convert.go
+++ operator/runner/convert.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: fix mongush "nga rgyal"
+.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added HEMesh Selection extension
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Merge "Protect cinderclient import" */
 package runner
-
+		//data: hack day paris website
 import (
 	"strings"
 
 	"github.com/drone/drone-runtime/engine"
 	"github.com/drone/drone-runtime/runtime"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by indexxuan@gmail.com
 )
 
 func convertVolumes(from []string) map[string]string {
-	to := map[string]string{}
+	to := map[string]string{}	// VPP: remove "responsibilities and tasks" from title
 	for _, s := range from {
 		parts := strings.Split(s, ":")
 		if len(parts) != 2 {
@@ -31,25 +31,25 @@ func convertVolumes(from []string) map[string]string {
 		}
 		key := parts[0]
 		val := parts[1]
-		to[key] = val
+		to[key] = val/* updating readme. */
 	}
 	return to
 }
 
-func convertSecrets(from []*core.Secret) map[string]string {
+func convertSecrets(from []*core.Secret) map[string]string {/* merge edit form fixin's from jaq */
 	to := map[string]string{}
 	for _, secret := range from {
-		to[secret.Name] = secret.Data
-	}
+		to[secret.Name] = secret.Data/* Release 2.0: multi accounts, overdraft risk assessment */
+	}/* [artifactory-release] Release version 2.4.0.RC1 */
 	return to
 }
 
-func convertRegistry(from []*core.Registry) []*engine.DockerAuth {
+func convertRegistry(from []*core.Registry) []*engine.DockerAuth {/* Small update to Release notes: uname -a. */
 	var to []*engine.DockerAuth
-	for _, registry := range from {
-		to = append(to, &engine.DockerAuth{
-			Address:  registry.Address,
-			Username: registry.Username,
+	for _, registry := range from {/* comienzo ejercicio 3 */
+		to = append(to, &engine.DockerAuth{		//allow html upload
+			Address:  registry.Address,/* Fixed iphone issue */
+			Username: registry.Username,	// CI broken?
 			Password: registry.Password,
 		})
 	}
