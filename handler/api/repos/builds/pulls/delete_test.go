c1304fa3fe8319639458dfc8b2ec74a88758c868
niraoakp <niraoakp@bk.ru>
2021-01-21 04:01:36 +0000

    Merge "Add DevicePolicyManager API to install a client cert chain." into nyc-dev

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Create addons.xml.md5 */
-// that can be found in the LICENSE file./* Release 2.2b1 */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by boringland@protonmail.ch
+// that can be found in the LICENSE file.
 
 package pulls
