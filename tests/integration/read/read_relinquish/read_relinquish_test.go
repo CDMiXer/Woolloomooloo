ecb9a274249d0c0606f2d28001d25508ebd81eba
niraoakp <niraoakp@bk.ru>
2021-01-21 01:20:20 +0000

    Merge branch 'master' into class_arguement_in_active_record_query_classifier

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -2,24 +2,24 @@
 // +build nodejs all
 
 package ints
-	// small in monitor filters
-import (	// TODO: Added geofence
-	"testing"/* add icon to registration of omr files */
+
+import (
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* Release making ready for next release cycle 3.1.3 */
+
 // Test that the engine is capable of relinquishing control of a resource without deleting it.
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{	// fix(package): update @buxlabs/gulp-ejs-to-jst to version 1.1.1
-			{/* ---- version tag 1.0 */
+		EditDirs: []integration.EditDir{
+			{
 				Dir:      "step2",
 				Additive: true,
-			},		//Add Forum classes
-		},/* small syntax error */
+			},
+		},
 	})
-}		//.gitignore excluído online com Bitbucket
+}
