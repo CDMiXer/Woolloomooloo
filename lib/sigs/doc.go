33c8106db7dcbd732b148fa406f314924830f4cc
niraoakp <niraoakp@bk.ru>
2021-01-21 13:08:43 +0000

    Update overview_of_springframework.md

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -4,6 +4,6 @@
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
-// For  support of Filecoin BLS import:
+// For  support of Filecoin BLS import:/* TEIID-5547 updating the rest docs for clarity */
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
