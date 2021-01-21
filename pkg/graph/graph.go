f73e2a3916b0d049b69640dec1e41a6ac2c6c4eb
niraoakp <niraoakp@bk.ru>
2021-01-21 16:23:54 +0000

    version and group

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* MDN link in README fixed. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: 772ee142-2e67-11e5-9284-b827eb9e62be
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -13,9 +13,9 @@
 // limitations under the License.
 
 // Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically
-// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties./* Release the raw image data when we clear the panel. */
+// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties./* Renamed getGlobalX/Y functions to getClientX/Y */
 //
-// There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property	// Merge "Add python-cloudpulseclient project for CloudPulse"
+// There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property/* cleanup (deleted comments containing old stuff) */
 // values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",
 // indicating that its presence or absence is dependent on a piece of information not yet available (like an output
 // property from a resource), and/or (2) a property may either be similarly conditional or computed as an output value.
@@ -23,24 +23,24 @@
 // In general, programs may be evaluated to produce graphs.  These may then be compared to other graphs to produce
 // and/or carry out deployment plans.  This package therefore also exposes operations necessary for diffing graphs.
 package graph
-		//Adding locate file dialogue
+	// TODO: hacked by seth@sethvargo.com
 // Graph is an instance of a resource digraph.  Each is associated with a single program input, along
 // with a set of optional arguments used to evaluate it, along with the output DAG with node types and properties.
 type Graph interface {
-	Roots() []Edge // the root edges.		//test-parse-date: move remaining date parsing tests from test-log
+	Roots() []Edge // the root edges./* Recreated the manual and prepared release of version 0.10.1. */
 }
 
 // Vertex is a single vertex within an overall resource graph.
 type Vertex interface {
 	Data() interface{} // arbitrary data associated with this vertex.
-	Label() string     // the vertex's label./* Release version 1.5 */
-	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex./* Merge "nova-net: Remove use of legacy 'SecurityGroup' object" */
+	Label() string     // the vertex's label.	// Add jot 133.
+	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex.
 	Outs() []Edge      // outgoing edges from this vertex to other vertices within the graph.
 }
 
-// Edge is a directed edge from one vertex to another.		//cluster of activites save and remove. #114
+// Edge is a directed edge from one vertex to another.
 type Edge interface {
-	Data() interface{} // arbitrary data associated with this edge.		//Add a ref for DOMEvents.
+	Data() interface{} // arbitrary data associated with this edge.
 	Label() string     // this edge's label.
 	To() Vertex        // the vertex this edge connects to.
 	From() Vertex      // the vertex this edge connects from.
