b3cfacd2fdc77e2385b6dcde40ba54ec751700be
niraoakp <niraoakp@bk.ru>
2021-01-21 14:44:20 +0000

    Make it more stable

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//remove shadow so computers don’t take off due to their fans
-		//Remove unneeded status column
+// Use of this source code is governed by the Drone Non-Commercial License/* Switch to Windows agent */
+// that can be found in the LICENSE file.
+
 // +build !oss
-	// storing the current content size for art.window
-package core/* finally got some good results with the clustering after these fixes */
+
+package core
