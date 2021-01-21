b7af6bd738957d16242d469b07e424467751d675
niraoakp <niraoakp@bk.ru>
2021-01-21 23:04:39 +0000

    use sublime as editor, add NODE_PATH

--- session/legacy.go
+++ session/legacy.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc./* Add artifact, Releases v1.2 */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: DEV-3118 master/slave scaling feature
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fix bug where armor did 100 times normal damage reduction */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* fixed rateAC rather than rateGT */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,48 +16,48 @@ package session
 
 import (
 	"encoding/json"
-	"errors"	// TODO: Deployed new version to HCP
+	"errors"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/drone/drone/core"
 
-	"github.com/dgrijalva/jwt-go"	// TODO: Rename gitnore to .gitignore
+	"github.com/dgrijalva/jwt-go"
 )
 
 type legacy struct {
 	*session
-	mapping map[string]string	// TODO: hacked by steven@stebalien.com
+	mapping map[string]string
 }
 
-// Legacy returns a session manager that is capable of mapping		//Create DrawFun.java
+// Legacy returns a session manager that is capable of mapping
 // legacy tokens to 1.0 users using a mapping file.
 func Legacy(users core.UserStore, config Config) (core.Session, error) {
 	base := &session{
 		secret:  []byte(config.Secret),
 		secure:  config.Secure,
-		timeout: config.Timeout,	// TODO: Note new owner
-		users:   users,/* Merge "docs: Android 5.1 API Release notes (Lollipop MR1)" into lmp-mr1-dev */
-	}		//add --use-valgrind option to R CMD check
+		timeout: config.Timeout,
+		users:   users,
+	}
 	out, err := ioutil.ReadFile(config.MappingFile)
 	if err != nil {
 		return nil, err
 	}
-	mapping := map[string]string{}/* Release notes for 3.1.4 */
-	err = json.Unmarshal(out, &mapping)	// TODO: Increase width to 24em
+	mapping := map[string]string{}
+	err = json.Unmarshal(out, &mapping)
 	if err != nil {
 		return nil, err
 	}
 	return &legacy{base, mapping}, nil
 }
 
-func (s *legacy) Get(r *http.Request) (*core.User, error) {/* Release of the GF(2^353) AVR backend for pairing computation. */
+func (s *legacy) Get(r *http.Request) (*core.User, error) {
 	switch {
-	case isAuthorizationToken(r):	// TODO: 34fea158-2e59-11e5-9284-b827eb9e62be
+	case isAuthorizationToken(r):
 		return s.fromToken(r)
 	case isAuthorizationParameter(r):
 		return s.fromToken(r)
-	default:		//MainController : fix MessageRenderer
+	default:
 		return s.fromSession(r)
 	}
 }
