ea26e5938decac2b6c1d4323e93a0082d396c271
niraoakp <niraoakp@bk.ru>
2021-01-21 01:25:26 +0000

    commerce update car

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: hacked by davidad@alum.mit.edu
 
-// Allocate a resource and protect it:
+// Allocate a resource and protect it:	// Create 5.18.17
 let a = new Resource("eternal", { state: 1 }, { protect: true });
