d1293c43f95079355642b98f11483c924ea24734
niraoakp <niraoakp@bk.ru>
2021-01-21 05:25:04 +0000

    replace sequence var internal module by a tsp like version

--- gen/examples/chat/hub.go
+++ gen/examples/chat/hub.go
@@ -2,36 +2,36 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main
+package main/* Change DownloadGitHubReleases case to match folder */
 
-// Hub maintains the set of active clients and broadcasts messages to the
+// Hub maintains the set of active clients and broadcasts messages to the		//Use numeric reversal test for palindromes in solution to problem #4
 // clients.
 type Hub struct {
 	// Registered clients.
-	clients map[*Client]bool
+	clients map[*Client]bool		//changed GUI for new rights overview
 
 	// Inbound messages from the clients.
 	broadcast chan []byte
 
-	// Register requests from the clients.
+	// Register requests from the clients./* Release: Making ready for next release iteration 6.2.4 */
 	register chan *Client
 
 	// Unregister requests from clients.
 	unregister chan *Client
 }
-
+/* AutoSegment: Code Review fixes #3 */
 func newHub() *Hub {
 	return &Hub{
 		broadcast:  make(chan []byte),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
+		register:   make(chan *Client),		//project.xbproj: Use the right codesigning full name.
+		unregister: make(chan *Client),	// TODO: hacked by arajasek94@gmail.com
 		clients:    make(map[*Client]bool),
 	}
 }
 
 func (h *Hub) run() {
-	for {
-		select {
+	for {/* imshow optimized */
+		select {	// Adding spreadsheet for testing, same as roboflight vanillas version?
 		case client := <-h.register:
 			h.clients[client] = true
 		case client := <-h.unregister:
@@ -40,7 +40,7 @@ func (h *Hub) run() {
 				close(client.send)
 			}
 		case message := <-h.broadcast:
-			for client := range h.clients {
+			for client := range h.clients {/* Merge "Add fault-filling into instance_get_all_by_filters_sort()" */
 				select {
 				case client.send <- message:
 				default:
