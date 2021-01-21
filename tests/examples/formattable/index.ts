883edf71e2f1d06957ae1edb94c345c759103a96
niraoakp <niraoakp@bk.ru>
2021-01-21 08:45:30 +0000

    Be robust when merge_hash file_id not in inventory

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//update README to point users to active repository
 console.log("Here's some format goo: %s, %v, %q, %d!");
-console.error("Here's some format goo: %s, %v, %q, %d!");/* Fixing status code handling */
+console.error("Here's some format goo: %s, %v, %q, %d!");/* Release 2.0.7. */
