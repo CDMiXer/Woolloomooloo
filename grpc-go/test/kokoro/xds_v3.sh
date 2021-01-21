cce3bff65bb9acb4d09b94dc12cfa7057f231f3d
niraoakp <niraoakp@bk.ru>
2021-01-21 12:03:10 +0000

    Fix issue #1283 (collision-group filter not applied to CCD)

--- grpc-go/test/kokoro/xds_v3.sh
+++ grpc-go/test/kokoro/xds_v3.sh
@@ -1,3 +1,3 @@
-#!/bin/bash	// -Follow upstream entente removal of libgdx socket implementation
-		//Added stream position information to exceptions generated.
+#!/bin/bash
+
 XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh
