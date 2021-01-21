a70bf107ce9eb29d314724714a6a2d1e54deb809
niraoakp <niraoakp@bk.ru>
2021-01-21 02:11:17 +0000

    a2866476-2e41-11e5-9284-b827eb9e62be

--- scheduler/nomad/nomad_oss.go
+++ scheduler/nomad/nomad_oss.go
@@ -3,20 +3,20 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//#50 - after code review
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* 'find-start' & 'find-end' functions */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Update year again! */
+
 package nomad
 
-import (	// CreatePalindromOrNot.py
+import (
 	"context"
 
 	"github.com/drone/drone/core"
@@ -25,7 +25,7 @@ import (	// CreatePalindromOrNot.py
 type noop struct{}
 
 // FromConfig returns a no-op Nomad scheduler.
-func FromConfig(conf Config) (core.Scheduler, error) {/* Merge branch 'master' into minecraftModal */
+func FromConfig(conf Config) (core.Scheduler, error) {
 	return new(noop), nil
 }
 
@@ -33,24 +33,24 @@ func (noop) Schedule(context.Context, *core.Stage) error {
 	return nil
 }
 
-func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {	// TODO: NVR subdirs and optional cleaning.
+func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
 	return nil, nil
 }
 
-func (noop) Cancel(context.Context, int64) error {	// Rebuilt index with CourtneyChu
+func (noop) Cancel(context.Context, int64) error {
 	return nil
 }
-	// TODO: Update GraphToPolyData.py
+
 func (noop) Cancelled(context.Context, int64) (bool, error) {
 	return false, nil
 }
 
 func (noop) Stats(context.Context) (interface{}, error) {
-	return nil, nil/* Create _search_post_contents.html.erb */
+	return nil, nil
 }
 
 func (noop) Pause(context.Context) error {
-	return nil/* Fix iText stealing focus */
+	return nil
 }
 
 func (noop) Resume(context.Context) error {
