1a009b7428b8b25ec09292fa47ac0de97f80025c
niraoakp <niraoakp@bk.ru>
2021-01-21 21:13:37 +0000

    z6 contrib

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* change loging to debug so test run is less verbose. */
+
 using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{	// TODO: hacked by hello@brooklynzelenka.com
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
@@ -12,30 +12,30 @@ class Resource : ComponentResource
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour : ComponentResource
+class ComponentFour : ComponentResource/* gnunet-service-dns uses the new mesh */
 {
     private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
-        : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions		//Merge pull request #2817 from rusikf/patch-2
+        : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
         {
-            // Add an alias that references the old type of this resource/* #661 - Upgraded Kotlin version to 1.3.0. */
+            // Add an alias that references the old type of this resource		//CODENVY-27; adopt token service to workspace sharing (#245)
             // and then make the base() call with the new type of this resource and the added alias.
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
-        }))
-    {/* [FIX] tcp: correct initialization of TCPConnection */
-        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to./* AUTOMATIC UPDATE BY DSC Project BUILD ENVIRONMENT - DSC_SCXDEV_1.0.0-578 */
-        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });	// Updated README with NPM info.
+        }))		//added functionality to manually load patient from csv file
+    {
+        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
+        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
 
 class Program
-{
-    static Task<int> Main(string[] args)/* Updated VB.NET Examples for Release 3.2.0 */
+{	// TODO: will be fixed by vyzo@hackzen.org
+    static Task<int> Main(string[] args)/* Release of eeacms/eprtr-frontend:0.4-beta.23 */
     {
         return Deployment.RunAsync(() =>
-        {/* Merge "ASoC: wcd9335: Give more headroom for headphone PA ramp" */
-            var comp4 = new ComponentFour("comp4");
-;)}        
+        {
+            var comp4 = new ComponentFour("comp4");	// TODO: hacked by mail@bitpshr.net
+        });
     }
 }
