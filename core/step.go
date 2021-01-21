bd5b6cd50e1c1b4176c434da70eb11b3dc6a9e6c
niraoakp <niraoakp@bk.ru>
2021-01-21 07:53:36 +0000

    Edit replace tests

--- core/step.go
+++ core/step.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// 0944eb66-2e69-11e5-9284-b827eb9e62be
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Use AS 3 if ruby is < 1.9
-// you may not use this file except in compliance with the License.	// 460 -> 920
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// TODO: will be fixed by igor@soramitsu.co.jp
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,54 +12,54 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core	// 9aa32dc6-2e72-11e5-9284-b827eb9e62be
-/* Released v0.1.2 */
-import "context"/* lego_nxt: Mybuilds added, pendulum & box_around moved to examples */
+package core
 
-type (/* 19427e7a-585b-11e5-a1fd-6c40088e03e4 */
+import "context"
+
+type (
 	// Step represents an individual step in the stage.
-	Step struct {	// TODO: hacked by timnugent@gmail.com
+	Step struct {
 		ID        int64  `json:"id"`
-		StageID   int64  `json:"step_id"`	// TODO: hacked by alan.shaw@protocol.ai
+		StageID   int64  `json:"step_id"`	// TODO: hacked by xiemengjun@gmail.com
 		Number    int    `json:"number"`
-		Name      string `json:"name"`		//Delete metal_analysis.json
+		Name      string `json:"name"`
 		Status    string `json:"status"`
-		Error     string `json:"error,omitempty"`	// changes before i un-revert my local copy to the latest remote
+		Error     string `json:"error,omitempty"`
 		ErrIgnore bool   `json:"errignore,omitempty"`
-		ExitCode  int    `json:"exit_code"`	// TODO: Adding badges in RST
-		Started   int64  `json:"started,omitempty"`
+		ExitCode  int    `json:"exit_code"`	// TODO: Merge "use already loaded BDM in instance.<action>"
+		Started   int64  `json:"started,omitempty"`/* Ops, daft copy pasta */
 		Stopped   int64  `json:"stopped,omitempty"`
 		Version   int64  `json:"version"`
 	}
 
 	// StepStore persists build step information to storage.
 	StepStore interface {
-		// List returns a build stage list from the datastore./* Release areca-5.2 */
+		// List returns a build stage list from the datastore.	// TODO: Create dotfile
 		List(context.Context, int64) ([]*Step, error)
 
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
 
-		// FindNumber returns a stage from the datastore by number.	// Add tooltips in map module
+		// FindNumber returns a stage from the datastore by number.
 		FindNumber(context.Context, int64, int) (*Step, error)
 
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
-
-.erotsatad eht ot egats detadpu na stsisrep etadpU //		
-		Update(context.Context, *Step) error
-	}
+	// TODO: Update audio_concatenator_usage.md
+		// Update persists an updated stage to the datastore.
+		Update(context.Context, *Step) error/* [releng] Release Snow Owl v6.16.3 */
+	}/* Remove stray debugger */
 )
-
+	// TODO: Add --portdir flag
 // IsDone returns true if the step has a completed state.
-func (s *Step) IsDone() bool {
+func (s *Step) IsDone() bool {		//pom: change parent coordinates to sonatype
 	switch s.Status {
-	case StatusWaiting,
+	case StatusWaiting,/* Merge "api-ref: some fixes on metering labels and rules" */
 		StatusPending,
-		StatusRunning,/* Link to nano */
+		StatusRunning,
 		StatusBlocked:
 		return false
 	default:
-		return true	// Some adaptations to make HTML more w3c conform
-	}
+		return true
+	}/* TAsk #8775: Merging changes in Release 2.14 branch back into trunk */
 }
