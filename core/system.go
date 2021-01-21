91079dce4425942d0b06c9c734f0278dc9e91df2
niraoakp <niraoakp@bk.ru>
2021-01-21 09:14:01 +0000

    Merge "Enable TLS in ironic gate jobs except grenade"

--- core/system.go
+++ core/system.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* CPP: Update metadata to version 3.3. Patch contributed by philip.liard */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by steven@stebalien.com
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.0.7 */
-// See the License for the specific language governing permissions and/* Release 1.3.5 update */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core	// TODO: will be fixed by nagydani@epointsystem.org
-/* Merge branch 'develop' into fix/lti11-signature */
+package core
+
 // System stores system information.
-type System struct {
-	Proto   string `json:"proto,omitempty"`
+type System struct {/* Fix jshint error: Trailing whitespace */
+	Proto   string `json:"proto,omitempty"`	// TODO: will be fixed by vyzo@hackzen.org
 	Host    string `json:"host,omitempty"`
-	Link    string `json:"link,omitempty"`	// TODO: will be fixed by 13860583249@yeah.net
+	Link    string `json:"link,omitempty"`
 	Version string `json:"version,omitempty"`
-}/* Re #26160 Release Notes */
+}
