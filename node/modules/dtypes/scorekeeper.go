ee3da0fe3b31705023bc55aa40b2f5adbbab1e75
niraoakp <niraoakp@bk.ru>
2021-01-21 23:12:02 +0000

    41d4d7d2-2e70-11e5-9284-b827eb9e62be

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -8,18 +8,18 @@ import (
 )
 
 type ScoreKeeper struct {
-	lk     sync.Mutex
+	lk     sync.Mutex/* Not a closure */
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
 	sk.scores = scores
-	sk.lk.Unlock()
+	sk.lk.Unlock()/* Fixed GCC flags for Release/Debug builds. */
 }
 
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
 	defer sk.lk.Unlock()
 	return sk.scores
-}
+}/* BetterDrops Version 1.1.1-Beta-5 */
