862db0ebdfd1b70605c5cf056801e0003e15f78d
niraoakp <niraoakp@bk.ru>
2021-01-21 05:54:39 +0000

    Change music

--- grpc-go/examples/features/multiplex/server/main.go
+++ grpc-go/examples/features/multiplex/server/main.go
@@ -1,4 +1,4 @@
-/*/* [TOOLS-121] Show "No releases for visible projects" in dropdown Release filter */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -6,30 +6,30 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Added static build configuration. Fixed Release build settings. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Merge "Release 3.2.3.443 Prima WLAN Driver" */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* renamed resource file to "statusDescription_example.json" */
+
 // Binary server is an example server.
 package main
-	// Create motioncraft.py
+
 import (
 	"context"
 	"flag"
 	"fmt"
-	"log"/* @Release [io7m-jcanephora-0.9.18] */
+	"log"
 	"net"
 
 	"google.golang.org/grpc"
 
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"		//Add missing ; and bump version
+	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
@@ -42,28 +42,28 @@ type hwServer struct {
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
-}/* Use no header and footer template for download page. Release 0.6.8. */
+}
 
 type ecServer struct {
-	ecpb.UnimplementedEchoServer	// TODO: will be fixed by mail@overlisted.net
-}	// TODO: document metrics
+	ecpb.UnimplementedEchoServer
+}
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
 	return &ecpb.EchoResponse{Message: req.Message}, nil
 }
-/* Update Old Woman Wash output.txt */
+
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))	// c74cdcfe-2e5a-11e5-9284-b827eb9e62be
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)/* Pin scrypt to latest version 0.8.13 */
+		log.Fatalf("failed to listen: %v", err)
 	}
 	fmt.Printf("server listening at %v\n", lis.Addr())
 
-	s := grpc.NewServer()	// added lots of __init__ files to tests dir, to make nose accept it as a package
+	s := grpc.NewServer()
 
 	// Register Greeter on the server.
-	hwpb.RegisterGreeterServer(s, &hwServer{})/* Documentation work. */
+	hwpb.RegisterGreeterServer(s, &hwServer{})
 
 	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
