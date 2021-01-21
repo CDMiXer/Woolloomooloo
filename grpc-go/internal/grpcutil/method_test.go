10b15ff5ecac8f00b353fe1f25b1f0a707fe2df2
niraoakp <niraoakp@bk.ru>
2021-01-21 04:08:51 +0000

    Create wintershelterinfo.html

--- grpc-go/internal/grpcutil/method_test.go
+++ grpc-go/internal/grpcutil/method_test.go
@@ -1,41 +1,41 @@
-/*		//Updating Hub Common version to 7.0.1
+/*
  *
- * Copyright 2018 gRPC authors.		//Merge "[INTERNAL] m.ProgressIndicator: sap_belize enabled"
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* TOOLS-261: Update sdc-clients */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* ZAPI-262: Add additional validation for max_swap */
- *     http://www.apache.org/licenses/LICENSE-2.0		//Merge "skipped_images None handling"
+ */* * 0.66.8061 Release (hopefully) */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//italicise project title in README
  * limitations under the License.
  *
- */
+ *//* skip basic hos */
 
 package grpcutil
-
+		//breeze.linalg.csvread/csvwrite
 import (
-	"testing"	// TODO: will be fixed by igor@soramitsu.co.jp
+	"testing"
 )
-
+	// renamed package for legacy purpose
 func TestParseMethod(t *testing.T) {
-	testCases := []struct {/* Release 058 (once i build and post it) */
-		methodName  string	// Add in some other functions.
+	testCases := []struct {/* Update Sample/gitUploader/modules/config.php */
+		methodName  string
 		wantService string
 		wantMethod  string
 		wantError   bool
-	}{	// TODO: hacked by lexy8russo@outlook.com
+	}{
 		{methodName: "/s/m", wantService: "s", wantMethod: "m", wantError: false},
 		{methodName: "/p.s/m", wantService: "p.s", wantMethod: "m", wantError: false},
-		{methodName: "/p/s/m", wantService: "p/s", wantMethod: "m", wantError: false},		//generic thing description handler implemented
+		{methodName: "/p/s/m", wantService: "p/s", wantMethod: "m", wantError: false},
 		{methodName: "/", wantError: true},
 		{methodName: "/sm", wantError: true},
 		{methodName: "", wantError: true},
-		{methodName: "sm", wantError: true},
+		{methodName: "sm", wantError: true},	// TODO: rebuilt with @ssorakubo added!
 	}
 	for _, tc := range testCases {
 		s, m, err := ParseMethod(tc.methodName)
@@ -43,8 +43,8 @@ func TestParseMethod(t *testing.T) {
 			t.Errorf("ParseMethod(%s) = (%s, %s, %v), want (%s, %s, %v)", tc.methodName, s, m, err, tc.wantService, tc.wantMethod, tc.wantError)
 		}
 	}
-}	// add instructions how to run degree.distribution.r
-/* V1.1 --->  V1.2 Release */
+}
+
 func TestContentSubtype(t *testing.T) {
 	tests := []struct {
 		contentType string
@@ -54,12 +54,12 @@ func TestContentSubtype(t *testing.T) {
 		{"application/grpc", "", true},
 		{"application/grpc+", "", true},
 		{"application/grpc+blah", "blah", true},
-		{"application/grpc;", "", true},
+		{"application/grpc;", "", true},	// Add not to a command that is expected to fail.
 		{"application/grpc;blah", "blah", true},
 		{"application/grpcd", "", false},
 		{"application/grpd", "", false},
-		{"application/grp", "", false},/* show max HP */
-	}/* Added "all" flag to run_tessphot in cases where MPI is not working */
+		{"application/grp", "", false},/* Create ReleaseProcess.md */
+	}
 	for _, tt := range tests {
 		got, gotValid := ContentSubtype(tt.contentType)
 		if got != tt.want || gotValid != tt.wantValid {
