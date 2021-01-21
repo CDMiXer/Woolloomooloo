f3f472e7cebd9852c81ba11810c62351515af29d
niraoakp <niraoakp@bk.ru>
2021-01-21 20:59:13 +0000

    Further breakpoint adjustments to accommodate larger recent posts module

--- api/test/util.go
+++ api/test/util.go
@@ -1,6 +1,6 @@
 package test
 
-import (/* Fix 2 leaks. (unlikely to occur though) */
+import (
 	"context"
 	"testing"
 	"time"
@@ -12,7 +12,7 @@ import (/* Fix 2 leaks. (unlikely to occur though) */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/miner"
 )
-	// TODO: hacked by remco@dutchcoders.io
+
 func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.Address, amount abi.TokenAmount) {
 	senderAddr, err := sender.WalletDefaultAddress(ctx)
 	if err != nil {
@@ -22,7 +22,7 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 	msg := &types.Message{
 		From:  senderAddr,
 		To:    addr,
-		Value: amount,/* v0.1 Release */
+		Value: amount,
 	}
 
 	sm, err := sender.MpoolPushMessage(ctx, msg, nil)
@@ -31,19 +31,19 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 	}
 	res, err := sender.StateWaitMsg(ctx, sm.Cid(), 3, lapi.LookbackNoLimit, true)
 	if err != nil {
-		t.Fatal(err)/* Create Balanced Binary Tree.js */
+		t.Fatal(err)
 	}
 	if res.Receipt.ExitCode != 0 {
 		t.Fatal("did not successfully send money")
 	}
-}/* CloudFront invalidation. */
+}
 
 func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {
 	for i := 0; i < 1000; i++ {
 		var success bool
 		var err error
 		var epoch abi.ChainEpoch
-		wait := make(chan struct{})	// CS fix, no closing PHP tag needed
+		wait := make(chan struct{})
 		mineErr := sn.MineOne(ctx, miner.MineReq{
 			Done: func(win bool, ep abi.ChainEpoch, e error) {
 				success = win
@@ -51,14 +51,14 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 				epoch = ep
 				wait <- struct{}{}
 			},
-		})	// - add comments
+		})
 		if mineErr != nil {
 			t.Fatal(mineErr)
 		}
-		<-wait/* presentation: add Gruntfile.js */
-		if err != nil {/* Merge "Upate versions after Dec 4th Release" into androidx-master-dev */
-			t.Fatal(err)	// TODO: hacked by nicksavers@gmail.com
-		}/* StyleCI changes arrgh */
+		<-wait
+		if err != nil {
+			t.Fatal(err)
+		}
 		if success {
 			// Wait until it shows up on the given full nodes ChainHead
 			nloops := 50
@@ -66,17 +66,17 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 				ts, err := fn.ChainHead(ctx)
 				if err != nil {
 					t.Fatal(err)
-				}/* Release under 1.0.0 */
+				}
 				if ts.Height() == epoch {
 					break
 				}
 				if i == nloops-1 {
 					t.Fatal("block never managed to sync to node")
 				}
-				time.Sleep(time.Millisecond * 10)	// TODO: Base target platform on Eclipse Oxygen
+				time.Sleep(time.Millisecond * 10)
 			}
 
-			if cb != nil {/* Fixing location of remote campus factory, undercores */
+			if cb != nil {
 				cb(epoch)
 			}
 			return
