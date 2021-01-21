15c2a1785ba6e73bc58fee6961ed2a5858f5c0a0
niraoakp <niraoakp@bk.ru>
2021-01-21 00:04:08 +0000

    update manifest files

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by alex.gaynor@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Merge "Add tool for reporting Bandit OpenStack coverage"
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software/* Use double-buffering on Linux */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added new blockstates. #Release */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package logs
 
-import (
+import (	// Added WL_RELEASE file for build 17
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"		//Update digest.jade
 
 	"github.com/go-chi/chi"
 )
@@ -29,34 +29,34 @@ import (
 func HandleDelete(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,
+	stages core.StageStore,	// Merge "Set AuthPlugin in __init__()"
 	steps core.StepStore,
 	logs core.LogStore,
-) http.HandlerFunc {
+) http.HandlerFunc {/* Release 2.0.0: Upgrade to ECM 3 */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* Merge "Removing PARAMS macro for consistency" */
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* Add NtGdiTransformPoints stub. */
 			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))/* Release: Making ready for next release cycle 3.1.4 */
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)		//949c3932-2e3f-11e5-9284-b827eb9e62be
 			return
-		}
+		}/* Release 1.0.64 */
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: by joachim: Fixed api.php docs.
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return		//f51b9328-2e69-11e5-9284-b827eb9e62be
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
@@ -73,7 +73,7 @@ func HandleDelete(
 			render.NotFound(w, err)
 			return
 		}
-		err = logs.Delete(r.Context(), step.ID)
+		err = logs.Delete(r.Context(), step.ID)/* Merge "Release 4.0.10.004  QCACLD WLAN Driver" */
 		if err != nil {
 			render.InternalError(w, err)
 			return
