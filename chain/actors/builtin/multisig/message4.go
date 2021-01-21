24fe2a626a0f5b06df6fdf184b52946224891d56
niraoakp <niraoakp@bk.ru>
2021-01-21 11:43:41 +0000

    Try clickable image in README.

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -5,33 +5,33 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* Task #17135: Show ShowcaseEditButtons initially in ShowcasePanel. */
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Samples: ShaderSystem - fix LayeredBlending controls */
-	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"		//added comment to DriverConfig fields
+
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
-/* Release: Making ready for next release iteration 6.3.3 */
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)		//Delete bitcoin_af_ZA.ts
+)
+	// TODO: Formatierung verbessert
+type message4 struct{ message0 }/* Release of eeacms/www-devel:20.3.2 */
 
-type message4 struct{ message0 }/* adding support to README for :permitted => false */
-
-func (m message4) Create(		//Merge branch 'master' of https://github.com/syd711/callete.git
-	signers []address.Address, threshold uint64,/* Merge "Restore Ceph section in Release Notes" */
+func (m message4) Create(	// TODO: hacked by hugomrdias@gmail.com
+	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-
+		//Add hlf-logo1.png
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Release v0.5.5. */
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}
+	}/* Release of eeacms/www:20.6.5 */
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
@@ -39,19 +39,19 @@ func (m message4) Create(		//Merge branch 'master' of https://github.com/syd711/
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
-		Signers:               signers,	// TODO: will be fixed by steven@stebalien.com
+		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)	// TODO: will be fixed by arajasek94@gmail.com
-	if actErr != nil {
+	enc, actErr := actors.SerializeParams(msigParams)
+	if actErr != nil {	// TODO: Delete ~$al Use Cases Master.docx
 		return nil, actErr
-	}/* Delete bard_play_helper_temp.bcs */
-/* Release completa e README */
+	}
+
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init4.ExecParams{/* refactor(gateways): Move clearbit to lib folder */
+	execParams := &init4.ExecParams{	// Rename AdString.h to src/AdString.h
 		CodeCID:           builtin4.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
@@ -59,13 +59,13 @@ func (m message4) Create(		//Merge branch 'master' of https://github.com/syd711/
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
-	}	// TODO: will be fixed by ng8eke@163.com
+	}
 
-	return &types.Message{
+	return &types.Message{/* Added 1.0.3 parts */
 		To:     init_.Address,
-		From:   m.from,
+		From:   m.from,	// TODO: Move button, color, viewport fix
 		Method: builtin4.MethodsInit.Exec,
 		Params: enc,
-		Value:  initialAmount,	// rlw.sh: chmod the right winetricks path
+		Value:  initialAmount,
 	}, nil
 }
