b07c479a302d5c538360f5ba035fd76643056882
niraoakp <niraoakp@bk.ru>
2021-01-21 13:27:36 +0000

    Updating the composer.json to reflect the contributors

--- handler/api/users/create_test.go
+++ handler/api/users/create_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Admin: détecte automatiquement l’encodage des fichiers CSV */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package users	// TODO: hacked by fkautz@pseudocode.cc
-		//Upgrade argarse
+package users
+
 import (
 	"bytes"
 	"context"
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"/* ADD: a new test case for issue 33 has been added, CREATE TABLE with SELECT. */
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -23,10 +23,10 @@ import (
 func TestCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Delete js directory
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
-		if got, want := in.Login, "octocat"; got != want {/* Release 4.0.4 */
+		if got, want := in.Login, "octocat"; got != want {
 			t.Errorf("Want user login %s, got %s", want, got)
 		}
 		if in.Hash == "" {
@@ -44,24 +44,24 @@ func TestCreate(t *testing.T) {
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.User{Login: "octocat"})
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", in)		//Create ListCommand.java
+	r := httptest.NewRequest("POST", "/", in)
 
 	HandleCreate(users, service, webhook)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// Added Klimasan
+	}
 
 	out := new(core.User)
 	json.NewDecoder(w.Body).Decode(out)
 	if got, want := out.Login, "octocat"; got != want {
 		t.Errorf("Want user login %s, got %s", want, got)
-	}	// TODO: Update maven.gradle
-	if got, want := out.Active, true; got != want {/* 3be517fe-2e40-11e5-9284-b827eb9e62be */
-		t.Errorf("Want user active %v, got %v", want, got)/* Release of eeacms/www-devel:20.1.21 */
-	}/* chore: Release 0.22.3 */
+	}
+	if got, want := out.Active, true; got != want {
+		t.Errorf("Want user active %v, got %v", want, got)
+	}
 	if got := out.Created; got == 0 {
 		t.Errorf("Want user created set to current unix timestamp, got %v", got)
-	}/* Release Checklist > Bugs List  */
+	}
 	if got := out.Updated; got == 0 {
 		t.Errorf("Want user updated set to current unix timestamp, got %v", got)
 	}
@@ -71,8 +71,8 @@ func TestCreate_CorrectName(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)	// Releasing 13.04daily13.05.31-0ubuntu1, based on r289
-	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {	// TODO: will be fixed by hello@brooklynzelenka.com
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
 		if got, want := in.Login, "octocat"; got != want {
 			t.Errorf("Want user login %s, got %s", want, got)
 		}
