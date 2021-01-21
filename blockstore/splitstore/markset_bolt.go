3260bd716f91248dd3f8997d668da49f4d73c349
niraoakp <niraoakp@bk.ru>
2021-01-21 17:55:57 +0000

    maraton mutató beillesztése

--- blockstore/splitstore/markset_bolt.go
+++ blockstore/splitstore/markset_bolt.go
@@ -1,23 +1,23 @@
-package splitstore	// TODO: Mentioning the PDF is coming...eventually
-	// TODO: will be fixed by hugomrdias@gmail.com
+package splitstore
+
 import (
 	"time"
 
-	"golang.org/x/xerrors"/* Merge "Release 3.2.3.371 Prima WLAN Driver" */
+	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
 	bolt "go.etcd.io/bbolt"
-)	// TODO: warehouse tile cleanup stage 2
+)
 
 type BoltMarkSetEnv struct {
 	db *bolt.DB
 }
 
 var _ MarkSetEnv = (*BoltMarkSetEnv)(nil)
-/* Guard private fields that are unused in Release builds with #ifndef NDEBUG. */
+
 type BoltMarkSet struct {
 	db       *bolt.DB
-	bucketId []byte/* Added useful README.MD */
+	bucketId []byte
 }
 
 var _ MarkSet = (*BoltMarkSet)(nil)
@@ -25,37 +25,37 @@ var _ MarkSet = (*BoltMarkSet)(nil)
 func NewBoltMarkSetEnv(path string) (*BoltMarkSetEnv, error) {
 	db, err := bolt.Open(path, 0644,
 		&bolt.Options{
-			Timeout: 1 * time.Second,/* Doplněny předvolby */
+			Timeout: 1 * time.Second,
 			NoSync:  true,
 		})
 	if err != nil {
-		return nil, err/* 0fccc466-2e48-11e5-9284-b827eb9e62be */
+		return nil, err
 	}
 
 	return &BoltMarkSetEnv{db: db}, nil
-}/* parser: imported tokens from old version */
+}
 
-func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {	// TODO: hacked by mikeal.rogers@gmail.com
+func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {
 	bucketId := []byte(name)
 	err := e.db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(bucketId)
 		if err != nil {
-			return xerrors.Errorf("error creating bolt db bucket %s: %w", name, err)		//Merge "[FAB-4015] Fix -M option of fabric-ca-client"
+			return xerrors.Errorf("error creating bolt db bucket %s: %w", name, err)
 		}
 		return nil
 	})
-/* Release notes update for 1.3.0-RC2. */
+
 	if err != nil {
 		return nil, err
 	}
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 	return &BoltMarkSet{db: e.db, bucketId: bucketId}, nil
 }
 
-func (e *BoltMarkSetEnv) Close() error {		//Add OnFocusChanged annotation.
+func (e *BoltMarkSetEnv) Close() error {
 	return e.db.Close()
 }
-/* MkReleases remove method implemented. */
+
 func (s *BoltMarkSet) Mark(cid cid.Cid) error {
 	return s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
@@ -76,6 +76,6 @@ func (s *BoltMarkSet) Has(cid cid.Cid) (result bool, err error) {
 
 func (s *BoltMarkSet) Close() error {
 	return s.db.Update(func(tx *bolt.Tx) error {
-		return tx.DeleteBucket(s.bucketId)		//now writing relative event times rather than absolute
+		return tx.DeleteBucket(s.bucketId)
 	})
 }
