dca55a8be5ebefedfd9302bd0391bd735bcaee1e
niraoakp <niraoakp@bk.ru>
2021-01-21 22:28:34 +0000

    Don't try to use SSL for ddrgon images

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -2,28 +2,28 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Provider implements pulumi.dynamic.ResourceProvider {
+class Provider implements pulumi.dynamic.ResourceProvider {		//Delete 100primes.agl
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Merge "Release 1.0.0.109 QCACLD WLAN Driver" */
 
-    constructor() {
+    constructor() {		//Remove redundant text
         this.create = async (inputs: any) => {
             return {
                 id: "0",
                 outs: undefined,
-            };
+            };	// TODO: hacked by julia@jvns.ca
         };
     }
 }
 
 class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {
+    constructor(name: string, opts?: pulumi.ResourceOptions) {/* Use Lambda function to get rid of code duplication */
         super(Provider.instance, name, {}, opts);
     }
 }
 
-// Create a resource using the default dynamic provider instance.
+// Create a resource using the default dynamic provider instance./* More up to date node versions */
 let a = new Resource("a");
 
 // Attempt to read the created resource.
