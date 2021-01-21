f5c7c20566e236f2fce038f4496cd49a739f91c2
niraoakp <niraoakp@bk.ru>
2021-01-21 21:37:52 +0000

    cleaned up simulation end and debug_print

--- api/api_gateway.go
+++ api/api_gateway.go
@@ -1,55 +1,55 @@
-package api/* okToInteractWithUser and such for FileAlterer/ProcessFiles */
+package api
 
-import (	// TODO: will be fixed by brosner@gmail.com
+import (
 	"context"
-
+	// TODO: will be fixed by fjl@ethereum.org
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"/* Release 4.3.0 - SPI */
+	"github.com/filecoin-project/go-address"	// use gThumb in the window title
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
-		//zu früh gefreut, weiterer Fix
-	apitypes "github.com/filecoin-project/lotus/api/types"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+
+	apitypes "github.com/filecoin-project/lotus/api/types"		//Kleine update
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: will be fixed by mikeal.rogers@gmail.com
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-//                       MODIFYING THE API INTERFACE
+//                       MODIFYING THE API INTERFACE/* Shin Megami Tensei IV: Add Taiwanese Release */
 //
 // NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API
 // you'll have to add those methods to interfaces in `api/v0api`
 //
 // When adding / changing methods in this file:
-// * Do the change here	// TODO: will be fixed by nick@perfectabstractions.com
+// * Do the change here
 // * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
-//  * Generate proxy structs	// TODO: hacked by ligi@ligi.de
-//  * Generate mocks
+//  * Generate proxy structs
+//  * Generate mocks/* Release black borders fix */
 //  * Generate markdown docs
 //  * Generate openrpc blobs
-	// TODO: will be fixed by caojiaoyue@protonmail.com
+	// TODO: hacked by arajasek94@gmail.com
 type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
-	ChainGetBlockMessages(context.Context, cid.Cid) (*BlockMessages, error)
+	ChainGetBlockMessages(context.Context, cid.Cid) (*BlockMessages, error)		//Merge branch 'master' into add_ico_banner
 	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
 	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainNotify(context.Context) (<-chan []*HeadChange, error)
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
-	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)/* New App: NotificationLog */
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)	// TODO: Have ImageEnhance range be in keeping with AA
+	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)		//Added missing closing HTML tag
+	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
 	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
-	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*MsigTransaction, error)
+	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*MsigTransaction, error)/* Re #26637 Release notes added */
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
-	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (DealCollateralBounds, error)
+	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (DealCollateralBounds, error)/* SAE-332 Release 1.0.1 */
 	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
-	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)/* Release of V1.5.2 */
-	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (MarketBalance, error)
-	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*MarketDeal, error)
-	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
+	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
+	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (MarketBalance, error)		//Re #1166 (SDP offer/answer glare): added SIPp scenario file to reproduce this
+	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*MarketDeal, error)		//Added error check for missing species in the java learn.
+	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)	// further work on md
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*MinerPower, error)
 	StateNetworkVersion(context.Context, types.TipSetKey) (apitypes.NetworkVersion, error)
@@ -58,4 +58,4 @@ type Gateway interface {
 	StateSearchMsg(ctx context.Context, from types.TipSetKey, msg cid.Cid, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
 	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
-}
+}		//Script fuer Netzwerkuebersicht
