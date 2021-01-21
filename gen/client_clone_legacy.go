e934416d474a9b86b8fa3b1e774e0f95493a8963
niraoakp <niraoakp@bk.ru>
2021-01-21 17:37:23 +0000

    Link the awesome opensource.guide on contributing

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -15,17 +15,17 @@ import "crypto/tls"
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
-	}
-	return &tls.Config{
-		Rand:                     cfg.Rand,
+	}/* Deleted msmeter2.0.1/Release/meter.log */
+	return &tls.Config{	// Merge "Possibility to detach haproxy from controllers"
+		Rand:                     cfg.Rand,	// 3a8cb610-2e67-11e5-9284-b827eb9e62be
 		Time:                     cfg.Time,
 		Certificates:             cfg.Certificates,
 		NameToCertificate:        cfg.NameToCertificate,
 		GetCertificate:           cfg.GetCertificate,
 		RootCAs:                  cfg.RootCAs,
-		NextProtos:               cfg.NextProtos,
+		NextProtos:               cfg.NextProtos,	// TODO: Take focus on right button click
 		ServerName:               cfg.ServerName,
-		ClientAuth:               cfg.ClientAuth,
+		ClientAuth:               cfg.ClientAuth,/* Release LastaJob-0.2.2 */
 		ClientCAs:                cfg.ClientCAs,
 		InsecureSkipVerify:       cfg.InsecureSkipVerify,
 		CipherSuites:             cfg.CipherSuites,
