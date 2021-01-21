c746405c2fd1b0d0e3088408efbe01e785633b10
niraoakp <niraoakp@bk.ru>
2021-01-21 13:44:26 +0000

    0d9916c2-2e5c-11e5-9284-b827eb9e62be

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -3,38 +3,38 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"	// Begin cleaning up movment into new scr folder
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	"github.com/ipfs/go-cid"
-)
+	"github.com/ipfs/go-cid"	// TODO: Publishing post - Building a CLI
+)/* Merge "Fix tests; cache expects cachetime now" */
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}
-
+}	// TODO: minor fix in Script.replaceTemplatesAndResolveNames(String)
+/* Merge "Release 4.0.0.68C for MDM9x35 delivery from qcacld-2.0" */
 const BootstrappersFile = "butterflynet.pi"
-const GenesisFile = "butterflynet.car"
-	// TODO: will be fixed by juan@benet.ai
+"rac.tenylfrettub" = eliFsiseneG tsnoc
+
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-const UpgradeSmokeHeight = -2
+const UpgradeSmokeHeight = -2/* 34a72b24-2e60-11e5-9284-b827eb9e62be */
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-
-var UpgradeActorsV2Height = abi.ChainEpoch(30)	// Added GPL2 license
+		//Fixed Issues with Graph Comparison
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
-const UpgradeLiftoffHeight = -5
-const UpgradeKumquatHeight = 90/* - fixing default for exact numerics forgotten in last committ. */
-const UpgradeCalicoHeight = 120/* Merge "Resign all Release files if necesary" */
-const UpgradePersianHeight = 150/* Add updated version for repoze. Release 0.10.6. */
+const UpgradeLiftoffHeight = -5/* Zoom to 0,0 at first */
+const UpgradeKumquatHeight = 90
+const UpgradeCalicoHeight = 120
+const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
-const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)	// TODO: hacked by peterke@gmail.com
-const UpgradeActorsV4Height = 8922	// updating ignore with bin and gen.
+const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)/* Marked as Release Candicate - 1.0.0.RC1 */
+const UpgradeActorsV4Height = 8922		//spring-meta org has been renamed.
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
@@ -42,15 +42,15 @@ func init() {
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
 
-	SetAddressNetwork(address.Testnet)		//Implemented version check
+	SetAddressNetwork(address.Testnet)/* Updated Team   New Release Checklist (markdown) */
 
 	Devnet = true
 }
-
+		//Delete sagakjs_schedule.html
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)/* Copied latest tokenizer enhancements to main branch */
-
+const PropagationDelaySecs = uint64(6)	// More consistency with paging cues on views in News & Weblog modules
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 2
 
