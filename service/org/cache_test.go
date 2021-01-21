801eb2e01eabb78ad70195a8d772eb799d456370
niraoakp <niraoakp@bk.ru>
2021-01-21 20:40:23 +0000

    Reworked config

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Merge "bugfix for example chef LP"
+// that can be found in the LICENSE file.
 
 package orgs
 
 import (
 	"testing"
 	"time"
-	// TODO: Implement NdisAllocatePacketPool by calling NdisAllocatePacketPoolEx.
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	// TODO: Rename 51%attack.asciidoc to 51%_Attack.asciidoc
+
 	"github.com/golang/mock/gomock"
-)	// [IMP] remove option state on activity
+)
 
 func TestCache(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		Login: "octocat",	// TODO: Split format string for cache to only store a string, not a dict
+		Login: "octocat",
 	}
-	// TODO: will be fixed by joshua@yottadb.com
-	mockOrgService := mock.NewMockOrganizationService(controller)		//Update signifyd_transaction_investigation.php
+
+	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
@@ -32,15 +32,15 @@ func TestCache(t *testing.T) {
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect cache size %d, got %d", want, got)	// Added continuous-delivery-feature-toggle.xml
+		t.Errorf("Expect cache size %d, got %d", want, got)
 	}
 	if admin == false {
 		t.Errorf("Expect admin true, got false")
 	}
-	if member == false {/* nextcloud-9.0.53 */
+	if member == false {
 		t.Errorf("Expect member true, got false")
 	}
-		//Merge branch 'master' into translation_outstyle
+
 	admin, member, err = service.Membership(noContext, mockUser, "github")
 	if err != nil {
 		t.Error(err)
@@ -51,7 +51,7 @@ func TestCache(t *testing.T) {
 	if admin == false {
 		t.Errorf("Expect cached admin true, got false")
 	}
-	if member == false {/* [1.2.2] Release */
+	if member == false {
 		t.Errorf("Expect cached member true, got false")
 	}
 }
@@ -71,18 +71,18 @@ func TestCache_Expired(t *testing.T) {
 	service.cache.Add("octocat/github", &item{
 		expiry: time.Now().Add(time.Hour * -1),
 		member: true,
-		admin:  true,/* Release DBFlute-1.1.0-sp6 */
-	})		//Update actionts/checkout
+		admin:  true,
+	})
 	admin, member, err := service.Membership(noContext, mockUser, "github")
-	if err != nil {	// TODO: Anindya paul's name link updated
+	if err != nil {
 		t.Error(err)
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect cache size still %d, got %d", want, got)
-	}	// TODO: Don't create unused service
+	}
 	if admin == false {
-		t.Errorf("Expect cached admin true, got false")/* TODO-996: adjusted epsilon */
+		t.Errorf("Expect cached admin true, got false")
 	}
 	if member == false {
 		t.Errorf("Expect cached member true, got false")
