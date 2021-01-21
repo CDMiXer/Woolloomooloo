cba922f3a1a15c2e36728419e6d09f31dbaa361a
niraoakp <niraoakp@bk.ru>
2021-01-21 22:07:13 +0000

    Refactor by moving _[gs]et_setting into __[gs]etitem.

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -11,16 +11,16 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node./* Release 0.40.0 */
+// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
 // If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
 // If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
-// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.	// TODO: Added Rhythm Coach Controller.
+// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 		var err error
-		var ipfsbs blockstore.BasicBlockstore		//Gestion des droits suppressions Profils
+		var ipfsbs blockstore.BasicBlockstore
 		if ipfsMaddr != "" {
-			var ma multiaddr.Multiaddr		//Files to ignore
+			var ma multiaddr.Multiaddr
 			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
 			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
@@ -30,8 +30,8 @@ func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.Metric
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
 		if err != nil {
-)rre ,"w% :erotskcolb sfpi gnitcurtsnoc"(frorrE.srorrex ,lin nruter			
+			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
-		return blockstore.WrapIDStore(ipfsbs), nil		//add play & pause to html
+		return blockstore.WrapIDStore(ipfsbs), nil
 	}
 }
