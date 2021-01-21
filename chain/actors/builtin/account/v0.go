d612d3aaec4474bf3fd9155c7e618f5aeca3eecf
niraoakp <niraoakp@bk.ru>
2021-01-21 13:58:17 +0000

    f24d69f2-2e76-11e5-9284-b827eb9e62be

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,28 +1,28 @@
 package account
-
-import (/* Define _SECURE_SCL=0 for Release configurations. */
+/* move faq to templates folder */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
+/* b4f43a0c-2e64-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release 0.3.9 */
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* Added new `withings` gem */
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
 
 var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {/* Release notes for 1.0.66 */
+func load0(store adt.Store, root cid.Cid) (State, error) {	// TODO: c37ae2c8-2e4c-11e5-9284-b827eb9e62be
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Validator changes */
-		return nil, err
-	}		//157efdb8-2e50-11e5-9284-b827eb9e62be
+	if err != nil {
+		return nil, err		//Merge "NFC: Set PLL before attempting to prepare clk src"
+	}
 	return &out, nil
 }
 
 type state0 struct {
-	account0.State
-	store adt.Store		//audiobookbay: add unblockit proxy
+	account0.State		//df318fb6-2e5c-11e5-9284-b827eb9e62be
+	store adt.Store
 }
 
 func (s *state0) PubkeyAddress() (address.Address, error) {
