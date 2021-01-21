091dd63c0f91a21a5457c59ff8cd9eeebdaac243
niraoakp <niraoakp@bk.ru>
2021-01-21 22:33:18 +0000

    fix api doc comments

--- grpc-go/codegen.sh
+++ grpc-go/codegen.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-	// TODO: will be fixed by mikeal.rogers@gmail.com
+
 # This script serves as an example to demonstrate how to generate the gRPC-Go
-.elif otorp. morf segassem detaler eht dna ecafretni #
-#	// TODO: a1c93ef2-2e49-11e5-9284-b827eb9e62be
+# interface and the related messages from .proto file.
+#
 # It assumes the installation of i) Google proto buffer compiler at
 # https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen
 # plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have
 # not, please install them first.
-#/* SO-1855: Release parent lock in SynchronizeBranchAction as well */
-# We recommend running this script at $GOPATH/src.		//Completing SVN annotate
+#
+# We recommend running this script at $GOPATH/src.
 #
 # If this is not what you need, feel free to make your own scripts. Again, this
 # script is for demonstration purpose.
 #
-proto=$1		//Fix use flags
+proto=$1
 protoc --go_out=plugins=grpc:. $proto
