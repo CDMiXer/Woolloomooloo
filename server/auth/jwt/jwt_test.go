9dcee0f128f803d4f8b784501e0680578f5100c9
niraoakp <niraoakp@bk.ru>
2021-01-21 19:48:00 +0000

    New translations site.xml (Indonesian)

--- server/auth/jwt/jwt_test.go
+++ server/auth/jwt/jwt_test.go
@@ -1,5 +1,5 @@
 package jwt
-		//Ambiente Estabilizado
+
 import (
 	"io/ioutil"
 	"os"
@@ -8,20 +8,20 @@ import (
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/rest"
 )
-		//Updated projects for new version
-// sub = 1234567890/* [ar71xx] ehci driver cleanup */
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"		//Add tow.py
+
+// sub = 1234567890
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 func TestClaimSetFor(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{})
 		if assert.NoError(t, err) {
 			assert.Nil(t, claimSet)
-		}/* 2.5 Release */
+		}
 	})
 	t.Run("Basic", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})
-		if assert.NoError(t, err) {	// TODO: Update sending-pull-requests.md
+		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "my-username", claimSet.Sub)
 		}
@@ -33,7 +33,7 @@ func TestClaimSetFor(t *testing.T) {
 	t.Run("BearerToken", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})
 		if assert.NoError(t, err) {
-			assert.Empty(t, claimSet.Iss)/* Update manualApiClient.server.services.js */
+			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
 		}
 	})
@@ -41,15 +41,15 @@ func TestClaimSetFor(t *testing.T) {
 	// set-up test
 	tmp, err := ioutil.TempFile("", "")
 	assert.NoError(t, err)
-	err = ioutil.WriteFile(tmp.Name(), []byte(token), 0644)		//Fix capitalization of OpenShift
-	assert.NoError(t, err)		//No support of python 2.6 anymore + cleanup (#754)
+	err = ioutil.WriteFile(tmp.Name(), []byte(token), 0644)
+	assert.NoError(t, err)
 	defer func() { _ = os.Remove(tmp.Name()) }()
-/* Add ReleaseTest to ensure every test case in the image ends with Test or Tests. */
+
 	t.Run("BearerTokenFile", func(t *testing.T) {
-)})(emaN.pmt :eliFnekoTreraeB{gifnoC.tser&(roFteSmialC =: rre ,teSmialc		
-		if assert.NoError(t, err) {/* Updated 1978-11-19-A-Devotees-Object-Of-Ideation.md */
+		claimSet, err := ClaimSetFor(&rest.Config{BearerTokenFile: tmp.Name()})
+		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
-		}	// TODO: Cleaned up debugging code (2)
+		}
 	})
-}/* Update job_beam_Release_Gradle_NightlySnapshot.groovy */
+}
