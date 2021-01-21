8db66fe2f83debb9115d629bbf73ee6000f50a3b
niraoakp <niraoakp@bk.ru>
2021-01-21 10:57:58 +0000

    Some tests pass interactively only

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//add LICENSE to publishConfig - ref #11
+
 // +build !oss
 
 package rpc2
