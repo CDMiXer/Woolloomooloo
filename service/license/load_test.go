fe773bfd3ba59aafa2bd55c553134e058534ab3c
niraoakp <niraoakp@bk.ru>
2021-01-21 09:59:23 +0000

    F.symjify()

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//MINOR: mostrar version
-// you may not use this file except in compliance with the License./* Delete accordionList.png */
-// You may obtain a copy of the License at/* Merge "Release 1.0.0.153 QCACLD WLAN Driver" */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of eeacms/www:19.1.12 */
-// limitations under the License.		//Delete Input_Var_Enum_List.h
-	// TODO: will be fixed by steven@stebalien.com
-// +build !nolimit
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !nolimit	// TODO: hacked by julia@jvns.ca
 // +build !oss
 
 package license
