1de1fe967307bfd4db5ebda632b873cb14cee420
niraoakp <niraoakp@bk.ru>
2021-01-21 17:39:43 +0000

    Create PureDataToArduino.pd

--- core/event.go
+++ core/event.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-///* Included frisian village start */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// maj fichier test et persistence.xml
-//      http://www.apache.org/licenses/LICENSE-2.0/* MST doc moved */
-//
+///* 15dd597a-2e47-11e5-9284-b827eb9e62be */
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* SoundValueDisplay edited */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Update PlyReader.cs */
 
 package core
 
-// Hook event constants.
-const (/* 210fee52-2ece-11e5-905b-74de2bd44bed */
+// Hook event constants.		//ae229554-2e71-11e5-9284-b827eb9e62be
+const (
 	EventCron        = "cron"
-	EventCustom      = "custom"/* fixed links and added more tiles to information */
+	EventCustom      = "custom"
 	EventPush        = "push"
-	EventPullRequest = "pull_request"
+	EventPullRequest = "pull_request"		//Travis CI integration added.
 	EventTag         = "tag"
-	EventPromote     = "promote"		//pkcs11 tests : added waitforslotevent tests
+	EventPromote     = "promote"
 	EventRollback    = "rollback"
 )
