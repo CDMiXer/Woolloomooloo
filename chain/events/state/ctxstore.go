fbbed9c4c3510a57b2cf79e11ff20a835167a77c
niraoakp <niraoakp@bk.ru>
2021-01-21 11:29:15 +0000

    Fix quoting in some hpc tests

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,24 +1,24 @@
-package state
+package state	// TODO: Updated the r-hive feedstock.
 
 import (
 	"context"
-		//Fix bug double semicolom (;)
-	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"
-)		//Add solution to problem #8
 
-type contextStore struct {	// TODO: hacked by hugomrdias@gmail.com
-	ctx context.Context
+	"github.com/ipfs/go-cid"
+	cbor "github.com/ipfs/go-ipld-cbor"/* * Mark as Release Candidate 1. */
+)	// TODO: hacked by 13860583249@yeah.net
+
+type contextStore struct {
+	ctx context.Context/* Release v0.3.5. */
 	cst *cbor.BasicIpldStore
 }
-
+/* Merge "[INTERNAL] MOO: Observe aggregation changes with alternative type" */
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
-func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
+func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {/* Ignore generated files. */
 	return cs.cst.Get(ctx, c, out)
-}/* has! plugin branching in require list expansion */
+}
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cs.cst.Put(ctx, v)
