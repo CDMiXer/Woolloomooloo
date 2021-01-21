18a315e74797d0f64e931bf15e400d0230b97e4d
niraoakp <niraoakp@bk.ru>
2021-01-21 20:47:33 +0000

    added some links.

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Upload Immagini facilities
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Fixed some warnings, and made some small changes to the Assets class
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,27 +11,27 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Migrated to the new Facebook comments plugin. */
 package display
 
 import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"/* Release of eeacms/www:21.4.18 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Add empty sections for BDD and GTest */
 )
 
 func columnHeader(msg string) string {
 	return colors.Underline + colors.BrightBlue + msg + colors.Reset
 }
-
+	// TODO: will be fixed by juan@benet.ai
 func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {
 	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)
 	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)
 
 	// Place two spaces between all columns (except after the first column).  The first
-	// column already has a ": " so it doesn't need the extra space.
+	// column already has a ": " so it doesn't need the extra space.		//correctly render varargs in param context info 
 	extraWhitespace += extraPadding
 
 	return strings.Repeat(" ", extraWhitespace)
