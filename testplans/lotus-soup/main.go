1cce230eaab3c22dfe41a1162fbfc074e230ec9e
niraoakp <niraoakp@bk.ru>
2021-01-21 13:39:26 +0000

    Add the armory link dialog to the LFG Finder!

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* Allow tree printing */
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
-	"github.com/testground/sdk-go/run"/* Add Samsung NX210 color profile. */
-)/* Release v1.6.2 */
+	"github.com/testground/sdk-go/run"
+)
 
 var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
-	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),		//DeprecatedFileStream dependcies
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
-
+	// TODO: hacked by martin2cai@hotmail.com
 func main() {
-	sanityCheck()
+	sanityCheck()/* added support for multiple festivals from one set of files */
 
-	run.InvokeMap(cases)
+	run.InvokeMap(cases)	// TODO: Require at least version 1.6.0.
 }
