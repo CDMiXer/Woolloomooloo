3fdf4e23b8be6dc8e00f5146e645f7931fed8cf1
niraoakp <niraoakp@bk.ru>
2021-01-21 07:44:11 +0000

    Optimised query for getNeighbours().

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -1,29 +1,29 @@
-package sqldb
+package sqldb	// TODO: bundle-size: fa7bd5a97a72cd03e239a244bb75bc7637c0d726 (85.67KB)
 
-import (		//Merge branch 'master' of https://github.com/fdreyfs/vaadin-tuning-datefield.git
+import (
 	"database/sql"
 
-	"github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"		//10ad415a-2e5a-11e5-9284-b827eb9e62be
 	"upper.io/db.v3"
 )
 
 type dbType string
 
 const (
-	MySQL    dbType = "mysql"
+	MySQL    dbType = "mysql"/* Release areca-6.1 */
 	Postgres dbType = "postgres"
 )
 
-func dbTypeFor(session db.Database) dbType {	// TODO: will be fixed by caojiaoyue@protonmail.com
+func dbTypeFor(session db.Database) dbType {
 	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
-		return MySQL/* Release 1.0.0.M9 */
+		return MySQL/* Refactoring; Simple persistent cache provider added */
 	}
-	return Postgres
+	return Postgres/* Create Update-Release */
 }
 
 func (t dbType) intType() string {
-	if t == MySQL {
+	if t == MySQL {/* Fixed argstream ReadString not working with binary strings */
 		return "signed"
 	}
 	return "int"
