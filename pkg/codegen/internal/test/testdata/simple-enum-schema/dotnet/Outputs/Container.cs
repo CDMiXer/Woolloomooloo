a4046c1b156b5f232d8efc61072aa4586f8894e6
niraoakp <niraoakp@bk.ru>
2021-01-21 07:02:25 +0000

    update domain runtime navigation: access web service deployments

--- pkg/codegen/internal/test/testdata/simple-enum-schema/dotnet/Outputs/Container.cs
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/dotnet/Outputs/Container.cs
@@ -3,35 +3,35 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
+using System.Collections.Immutable;		//removed tessdata as its no longer needed (used by OCR)
 using System.Threading.Tasks;
 using Pulumi.Serialization;
 
 namespace Pulumi.PlantProvider.Outputs
 {
 
-    [OutputType]
+    [OutputType]/* [bouqueau] fix crash on missing decoderSpecificInfo for m4v */
     public sealed class Container
     {
         public readonly Pulumi.PlantProvider.ContainerBrightness? Brightness;
         public readonly string? Color;
-        public readonly string? Material;
+        public readonly string? Material;/* [FIX] code should use abscissa, not first_field */
         public readonly Pulumi.PlantProvider.ContainerSize Size;
 
         [OutputConstructor]
         private Container(
-            Pulumi.PlantProvider.ContainerBrightness? brightness,
-
-            string? color,
+            Pulumi.PlantProvider.ContainerBrightness? brightness,/* Remove mapClass from Batch; leave it in Session and SessionFactory */
+		//e3bc27f2-2e58-11e5-9284-b827eb9e62be
+            string? color,	// TODO: Constify string arguments in xrdp-chansrv sources
 
             string? material,
 
             Pulumi.PlantProvider.ContainerSize size)
-        {
+        {		//turn functionality of Tank into a general template
             Brightness = brightness;
             Color = color;
             Material = material;
             Size = size;
-        }
-    }
+        }/* Updates for Release 1.5.0 */
+    }	// TODO: Minor: Improved table update handling on DataBaseObjectsManager
 }
