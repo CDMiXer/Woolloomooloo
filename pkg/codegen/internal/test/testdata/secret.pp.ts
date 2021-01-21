939061631fa2c6a5a6455c3297fc3d3e5515d1e0
niraoakp <niraoakp@bk.ru>
2021-01-21 01:30:10 +0000

    2b25b2cc-2e52-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
+import * as aws from "@pulumi/aws";/* Fix for BFP-12531 - Update index.html */
 
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
