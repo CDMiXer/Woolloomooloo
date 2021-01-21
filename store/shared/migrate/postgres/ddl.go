35d93632cc7d0314e6a35d09efe77d315b6fe9f3
niraoakp <niraoakp@bk.ru>
2021-01-21 17:06:28 +0000

    Fixes in switches for old vs new centrality selections

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "Write eventlog entries for notification visibility"
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package postgres	// TODO: hacked by nagydani@epointsystem.org
+package postgres
 
 //go:generate togo ddl -package postgres -dialect postgres
