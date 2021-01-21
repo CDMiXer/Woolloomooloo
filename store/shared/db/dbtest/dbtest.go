3e6c0b36071ec357b7ea34604871684810954bb2
niraoakp <niraoakp@bk.ru>
2021-01-21 11:21:45 +0000

    repo url changed

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc.	// Merge "Switch to absolute paths in Dockerfiles"
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release Candidate 3. */
-///* Add changelog convention to README */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,27 +14,27 @@
 
 package dbtest
 
-import (	// Wrong URLs
+import (
 	"os"
 
-	"github.com/drone/drone/store/shared/db"	// TODO: Update the-transformation-of-the-business.md
-/* Release of eeacms/plonesaas:5.2.4-5 */
-	// blank imports are used to load database drivers/* Release version 0.01 */
-	// for unit tests. Only unit tests should be importing/* Added link to package on NPM */
-	// this package.
-	_ "github.com/go-sql-driver/mysql"		//latin extended font
-	_ "github.com/lib/pq"
-"3etilqs-og/nttam/moc.buhtig" _	
-)/* Merge "[api-ref]Add volumes/summary API doc" */
+	"github.com/drone/drone/store/shared/db"
 
-// Connect opens a new test database connection./* Update GenesisCoin.sol */
-func Connect() (*db.DB, error) {	// TODO: will be fixed by steven@stebalien.com
-	var (/* modified Makefiles to compile under OsX */
+	// blank imports are used to load database drivers
+	// for unit tests. Only unit tests should be importing
+	// this package.
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Connect opens a new test database connection.
+func Connect() (*db.DB, error) {
+	var (
 		driver = "sqlite3"
-		config = ":memory:?_foreign_keys=1"/* Update _test_suite.py */
+		config = ":memory:?_foreign_keys=1"
 	)
 	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {
-		driver = os.Getenv("DRONE_DATABASE_DRIVER")	// TODO: 5d52cd2e-2e67-11e5-9284-b827eb9e62be
+		driver = os.Getenv("DRONE_DATABASE_DRIVER")
 		config = os.Getenv("DRONE_DATABASE_DATASOURCE")
 	}
 	return db.Connect(driver, config)
