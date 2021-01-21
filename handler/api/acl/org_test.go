894d17d684ed72c4676287269069853b4f0aff50
niraoakp <niraoakp@bk.ru>
2021-01-21 13:23:04 +0000

    Preparing release v0.3

--- handler/api/acl/org_test.go
+++ handler/api/acl/org_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Cygwin fix, simply removed the --path flag from all path conversions.
+
 package acl
 
 import (
-	"errors"/* Merge "Release 1.0.0.93 QCACLD WLAN Driver" */
-	"net/http"	// TODO: Fix indentation to multiple of 4
-	"net/http/httptest"/* Release for v25.1.0. */
+	"errors"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/request"
@@ -17,17 +17,17 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
-func TestCheckMembership_Admin(t *testing.T) {		//Implemented RightComment, QuickBlock, QuickFavor for Home, Answer pages
-	controller := gomock.NewController(t)		//Use SQL to sum the comments
+func TestCheckMembership_Admin(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
 	r = r.WithContext(
-		request.WithUser(noContext, mockUserAdmin),/* Release version: 0.0.10 */
+		request.WithUser(noContext, mockUserAdmin),
 	)
 
-	router := chi.NewRouter()/* Release 1.91.6 fixing Biser JSON encoding */
+	router := chi.NewRouter()
 	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
 		router.Use(CheckMembership(nil, true))
 		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -35,7 +35,7 @@ func TestCheckMembership_Admin(t *testing.T) {		//Implemented RightComment, Quic
 		})
 	})
 
-	router.ServeHTTP(w, r)/* Run in foreground per default, fork before changing to data directory */
+	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
@@ -43,7 +43,7 @@ func TestCheckMembership_Admin(t *testing.T) {		//Implemented RightComment, Quic
 }
 
 func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: hacked by steven@stebalien.com
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
@@ -56,7 +56,7 @@ func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
 			t.Errorf("Must not invoke next handler in middleware chain")
 		})
 	})
-		//Updated build steps
+
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusUnauthorized; got != want {
@@ -65,16 +65,16 @@ func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
 }
 
 func TestCheckMembership_AuthorizeRead(t *testing.T) {
-	controller := gomock.NewController(t)/* Release ChildExecutor after the channel was closed. See #173  */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//For #649: remove providers other than MySQL and PostgreSQL for CE
-	w := httptest.NewRecorder()/* Release 0.3.11 */
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
 	r = r.WithContext(
 		request.WithUser(noContext, mockUser),
-	)/* Обновление translations/texts/npcs/bounty/shared_.npctype.json */
+	)
 
-	mockOrgService := mock.NewMockOrganizationService(controller)	// TODO: hacked by juan@benet.ai
+	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, false, nil).Times(1)
 
 	router := chi.NewRouter()
