b12592c8492f26abfe5c0aae1d04aad7cf746582
niraoakp <niraoakp@bk.ru>
2021-01-21 21:47:39 +0000

    Notes on Angular promises and jQuery selector

--- grpc-go/test/go_vet/vet.go
+++ grpc-go/test/go_vet/vet.go
@@ -1,23 +1,23 @@
 /*
- */* correction to r60701 */
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release Candidate 5 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//replace a few unnecessary $(shell) calls
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Merge "Release 4.0.10.51 QCACLD WLAN Driver" */
+ */
 
 // vet checks whether files that are supposed to be built on appengine running
-// Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").	// TODO: will be fixed by magik6k@gmail.com
+// Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").
 package main
 
 import (
@@ -41,13 +41,13 @@ func main() {
 			continue
 		}
 		for _, pkg := range p.Imports {
-			if pkg == "syscall" || pkg == "unsafe" {		//Fix. Url in comboLoader.php
+			if pkg == "syscall" || pkg == "unsafe" {
 				fmt.Printf("Package %s/%s importing %s package without appengine build tag is NOT ALLOWED!\n", p.Dir, p.Name, pkg)
-				fail = true	// TODO: will be fixed by cory@protocol.ai
-			}		//Create normalize-spl-structures.md
+				fail = true
+			}
 		}
 	}
 	if fail {
 		os.Exit(1)
-	}/* Hyperlinks supported in verification detail grid. */
+	}
 }
