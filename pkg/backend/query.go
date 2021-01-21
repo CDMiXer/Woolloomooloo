f092940cf27e8eed151eb2dfbd5ee453efdb384e
niraoakp <niraoakp@bk.ru>
2021-01-21 12:39:52 +0000

    6b0416d4-2eae-11e5-846e-7831c1d44c14

--- pkg/backend/query.go
+++ pkg/backend/query.go
@@ -1,19 +1,19 @@
-package backend	// a888c030-2e41-11e5-9284-b827eb9e62be
+package backend
 
 import (
 	"context"
 
 	opentracing "github.com/opentracing/opentracing-go"
-/* precautionary unset */
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//Create csTypingLabelTest.js
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 type MakeQuery func(context.Context, QueryOperation) (engine.QueryInfo, error)
 
 // RunQuery executes a query program against the resource outputs of a locally hosted stack.
-func RunQuery(ctx context.Context, b Backend, op QueryOperation,	// TODO: Add Chinese translation; update ad
+func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {
 	q, err := newQuery(ctx, op)
 	if err != nil {
@@ -23,36 +23,36 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,	// TODO: Add Ch
 	// Render query output to CLI.
 	displayEvents := make(chan engine.Event)
 	displayDone := make(chan bool)
-	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)	// TODO: Added delete_issue_remotelink_by_link_id
+	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)
 
 	// The engineEvents channel receives all events from the engine, which we then forward onto other
 	// channels for actual processing. (displayEvents and callerEventsOpt.)
 	engineEvents := make(chan engine.Event)
 	eventsDone := make(chan bool)
-	go func() {	// Moved all IO to new class, added some new methods
+	go func() {
 		for e := range engineEvents {
-			displayEvents <- e		//Adjustments to support/test using AVX512
-			if callerEventsOpt != nil {		//This would break on my machine (old node version?)
+			displayEvents <- e
+			if callerEventsOpt != nil {
 				callerEventsOpt <- e
-			}	// TODO: will be fixed by lexy8russo@outlook.com
+			}
 		}
 
 		close(eventsDone)
 	}()
 
 	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and
-	// return error conditions, because we will do so below after waiting for the display channels to close./* issue #1: added format option for generated formatted sql. */
-	cancellationScope := op.Scopes.NewScope(engineEvents, true /*dryRun*/)		//Include decomposition to x, y
+	// return error conditions, because we will do so below after waiting for the display channels to close.
+	cancellationScope := op.Scopes.NewScope(engineEvents, true /*dryRun*/)
 	engineCtx := &engine.Context{
-,)(txetnoC.epocSnoitallecnac        :lecnaC		
+		Cancel:        cancellationScope.Context(),
 		Events:        engineEvents,
 		BackendClient: NewBackendClient(b),
-	}/* fix player speed and movement */
+	}
 	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
-		engineCtx.ParentSpan = parentSpan.Context()/* CAF-3183 Updates to Release Notes in preparation of release */
+		engineCtx.ParentSpan = parentSpan.Context()
 	}
 
-	res := engine.Query(engineCtx, q, op.Opts.Engine)/* Release v0.26.0 (#417) */
+	res := engine.Query(engineCtx, q, op.Opts.Engine)
 
 	// Wait for dependent channels to finish processing engineEvents before closing.
 	<-displayDone
@@ -60,7 +60,7 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,	// TODO: Add Ch
 	close(engineEvents)
 
 	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
-	// has exited before proceeding		//Creates namespace for portfolio
+	// has exited before proceeding
 	<-eventsDone
 	close(displayEvents)
 
