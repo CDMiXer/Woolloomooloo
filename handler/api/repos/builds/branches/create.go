1bcfcbebdd35a079565709dc6ed65e5e767a00f9
niraoakp <niraoakp@bk.ru>
2021-01-21 20:42:43 +0000

    fix another potential crash

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-///* Create variable configuration using templates */
+///* Rename Little Fears Nightmare Edition.css to LittleFearsNightmareEdition.css */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Added a thumbnail overview as described in Issue 208 part 6. */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//protect some manual import
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Fixed: Issue 190: No scroll bars kills mouse wheel
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package branches
