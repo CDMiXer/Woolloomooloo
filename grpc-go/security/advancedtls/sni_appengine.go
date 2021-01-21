8fe9f7cfe69075b6cd7a15ea63a7f3c789d60d7f
niraoakp <niraoakp@bk.ru>
2021-01-21 07:58:33 +0000

    Client max body: needs to be there to be replaced

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,9 +1,9 @@
-// +build appengine
+// +build appengine/* Delete Japanese link. */
 
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* 9ac48eb6-2e41-11e5-9284-b827eb9e62be */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,10 +11,10 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//disable incomplete feature that was switched on by mistake
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// Fixed schema.
  *
  */
 
@@ -24,7 +24,7 @@ import (
 	"crypto/tls"
 )
 
-// buildGetCertificates is a no-op for appengine builds.
+// buildGetCertificates is a no-op for appengine builds./* fix read_install */
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	return nil, nil
 }
