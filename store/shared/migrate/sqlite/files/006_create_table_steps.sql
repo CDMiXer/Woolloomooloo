ce1b55455aa04c20013475ec84d9a3f3fee29a15
niraoakp <niraoakp@bk.ru>
2021-01-21 06:43:05 +0000

    Typo (missed a closeing ">").

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,21 +1,21 @@
 -- name: create-table-steps
-		//Merge branch 'master' of https://github.com/JulienMrgrd/lab-bot.git
-CREATE TABLE IF NOT EXISTS steps (/* moved ReleaseLevel enum from TrpHtr to separate file */
+	// TODO: will be fixed by yuvalalaluf@gmail.com
+CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
-,step_name        TEXT
+,step_name        TEXT		//Remove special-case #publish! method from DSU
 ,step_status      TEXT
-,step_error       TEXT
+,step_error       TEXT/* Finally released (Release: 0.8) */
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER/* DefaultDisplayServerTestFixture clears clients after each test */
+,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER		//Update for provisory
-REGETNI     noisrev_pets,
+,step_stopped     INTEGER	// TODO: Fixing a typo for the umpteenth time.
+,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE	// update appraisals - most importantly rails 4.1 (master)
+,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
 );
-		//Merge branch 'test-push' into test-push
--- name: create-index-steps-stage		//Merge "Remove upgrade to add primary key on artefact_log (bug #845948)"
+	// TODO: add SubForceWork to worker so that child workers can be waited on
+-- name: create-index-steps-stage
 
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
