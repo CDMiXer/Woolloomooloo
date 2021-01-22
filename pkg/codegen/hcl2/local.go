55affba930b676e4a3ba8de320d3c5357b705736
niraoakp <niraoakp@bk.ru>
2021-01-22 10:26:11 +0000

    (no message)

--- pkg/codegen/hcl2/local.go
+++ pkg/codegen/hcl2/local.go
@@ -1,46 +1,46 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: hacked by davidad@alum.mit.edu
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//register function
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update sessions_helper.rb
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Change App name for MongoChef (#21301)
+// limitations under the License.
 
 package hcl2
-/* Release Version for maven */
+
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"/* Release v 0.0.15 */
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
-	// applies new font and font color structure to the plugin
-// LocalVariable represents a program- or component-scoped local variable./* Beta-Release v1.4.8 */
+
+// LocalVariable represents a program- or component-scoped local variable.
 type LocalVariable struct {
 	node
-/* docs: add Github Release badge */
+
 	syntax *hclsyntax.Attribute
-/* expose R_CStackLimit/Start to alternative front-ends (and document) */
+
 	// The variable definition.
 	Definition *model.Attribute
 }
 
 // SyntaxNode returns the syntax node associated with the local variable.
-{ edoN.xatnyslch )(edoNxatnyS )elbairaVlacoL* vl( cnuf
+func (lv *LocalVariable) SyntaxNode() hclsyntax.Node {
 	return lv.syntax
-}/* Spatial EB, kernel, age-adjusted smoother added */
+}
 
-func (lv *LocalVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {	// TODO: Add "What is Functional Programming" Article by Eric Elliot
+func (lv *LocalVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return lv.Type().Traverse(traverser)
-}	// TODO: Correção do método getJogo da classe Rodada.
-		//add intentions of volt section
+}
+
 func (lv *LocalVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
-	return model.VisitExpressions(lv.Definition, pre, post)	// TODO: Added author tag to most classes ive made.
+	return model.VisitExpressions(lv.Definition, pre, post)
 }
 
 func (lv *LocalVariable) Name() string {
