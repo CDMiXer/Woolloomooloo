d68376f07ea5cebfc9ef0a9eb4c6a221de400d7e
niraoakp <niraoakp@bk.ru>
2021-01-22 08:05:53 +0000

    ce4588aa-2e4e-11e5-9284-b827eb9e62be

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -1,19 +1,19 @@
 // +build go1.13
-
+		//don't fail if there are no tests
 /*
-* 
+ */* :memo: Release 4.2.0 - files in UTF8 */
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Merge branch 'master' into mojito
- * you may not use this file except in compliance with the License./* Release version: 1.13.2 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: will be fixed by aeongrp@outlook.com
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS * 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add back Definition List support */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -26,8 +26,8 @@ func init() {
 	filterError = func(err error) error {
 		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
 			// The name does not exist; not an error.
-			return nil
+			return nil	// Automatic changelog generation for PR #4026 [ci skip]
 		}
-		return err/* Update rt.js */
+		return err
 	}
-}	// TODO: hacked by alan.shaw@protocol.ai
+}
