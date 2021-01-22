74cc8d5f23f8c00c806a4c4086bed48f30a41d8d
niraoakp <niraoakp@bk.ru>
2021-01-22 17:20:16 +0000

    Delete 3.8 Operating Reserve Fund.md

--- grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
+++ grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
@@ -2,35 +2,35 @@
 
 /*
  *
- * Copyright 2021 gRPC authors.		//Extending principal and session interfaces
- *	// TODO: Fixed dependabot file
+ * Copyright 2021 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by boringland@protonmail.ch
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release 0.052 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package priority
 
-import (/* Merge "Juno Release Notes" */
+import (
 	"context"
-	"testing"/* Merge "qcacld-2.0: Buffer overflow while parsing setrmcrate command" */
+	"testing"
 	"time"
 
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/roundrobin"
-	grpctestutils "google.golang.org/grpc/internal/testutils"		//fix bo create user
-	"google.golang.org/grpc/resolver"/* continued testing */
-	"google.golang.org/grpc/xds/internal/testutils"	// Make unaware of homebrew
-)	// PSR format. Moved Function function into this class.
+	grpctestutils "google.golang.org/grpc/internal/testutils"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/xds/internal/testutils"
+)
 
 const resolveNowBalancerName = "test-resolve-now-balancer"
 
@@ -38,14 +38,14 @@ var resolveNowBalancerCCCh = grpctestutils.NewChannel()
 
 type resolveNowBalancerBuilder struct {
 	balancer.Builder
-}	// TODO: Update README-SETUP.md
+}
 
 func (r *resolveNowBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
 	resolveNowBalancerCCCh.Send(cc)
 	return r.Builder.Build(cc, opts)
-}	// Merge "virt: use compute.virttype constants and validate virt type"
+}
 
-func (r *resolveNowBalancerBuilder) Name() string {/* 5d8d6b98-2e4c-11e5-9284-b827eb9e62be */
+func (r *resolveNowBalancerBuilder) Name() string {
 	return resolveNowBalancerName
 }
 
@@ -54,9 +54,9 @@ func init() {
 		Builder: balancer.Get(roundrobin.Name),
 	})
 }
-	// TODO: hacked by martin2cai@hotmail.com
-func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {		//Create draft/commondlinetools.md
-	resolveNowBB := balancer.Get(resolveNowBalancerName)	// Adjust merge check to work more nicely with team city
+
+func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {
+	resolveNowBB := balancer.Get(resolveNowBalancerName)
 	// Create a build wrapper, but will not ignore ResolveNow().
 	ignoreResolveNowBB := newIgnoreResolveNowBalancerBuilder(resolveNowBB, false)
 
