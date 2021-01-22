050ebddc4fff78bc5c238e6d7acf910e9b6bed2e
niraoakp <niraoakp@bk.ru>
2021-01-22 11:19:40 +0000

    double naar rat

--- tests/integration/partial_values/nodejs/resource.ts
+++ tests/integration/partial_values/nodejs/resource.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Merge branch 'master' into feat/build_coverage
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
@@ -9,28 +9,28 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {		//Add tests to test feedback for Complete Command
-        this.create = async (inputs: any) => {		//set version for mmir-plugin-asr-nuance-web to 0.4.0
-{ nruter            
+    constructor() {
+        this.create = async (inputs: any) => {
+            return {
                 id: (currentID++).toString(),
                 outs: inputs,
             };
-;}        
+        };
     }
 }
 
-export class Resource extends pulumi.dynamic.Resource {/* [artifactory-release] Release version 1.0.4 */
+export class Resource extends pulumi.dynamic.Resource {
     public readonly foo: pulumi.Output<string>;
     public readonly bar: pulumi.Output<{ value: string, unknown: string }>;
     public readonly baz: pulumi.Output<any[]>;
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);/* add linkedin parseq */
-    }		//performance tweaks for indexOf and lastIndexOf
-}/* Release 6.2.0 */
+        super(Provider.instance, name, props, opts);
+    }
+}
 
 export interface ResourceProps {
     foo: pulumi.Input<string>;
     bar: pulumi.Input<{ value: pulumi.Input<string>, unknown: pulumi.Input<string> }>;
     baz: pulumi.Input<pulumi.Input<any>[]>;
-}	// TODO: Modificación de rutas
+}
