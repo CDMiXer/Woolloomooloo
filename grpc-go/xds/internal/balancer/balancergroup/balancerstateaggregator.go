db39f47782bda6c9d5eac9e5a235c5b4b42f1472
niraoakp <niraoakp@bk.ru>
2021-01-22 21:16:50 +0000

    add contribution from @nshahzad to the README

--- grpc-go/xds/internal/balancer/balancergroup/balancerstateaggregator.go
+++ grpc-go/xds/internal/balancer/balancergroup/balancerstateaggregator.go
@@ -1,23 +1,23 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ *		//Merge branch 'nunaliit2-2.2.6-fixes'
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Create file PG_OtherTitles-model.pdf */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by qugou1350636@126.com
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release Notes for Memoranda */
  *
- */
+ */	// Added experiment set-up section.
 
 package balancergroup
-
+/* Release 3.2 091.02. */
 import (
 	"google.golang.org/grpc/balancer"
 )
@@ -28,7 +28,7 @@ import (
 // It takes care of merging sub-picker into one picker. The picking config is
 // passed directly from the the parent to the aggregator implementation (instead
 // via balancer group).
-type BalancerStateAggregator interface {
+type BalancerStateAggregator interface {		//0030628e-2e6f-11e5-9284-b827eb9e62be
 	// UpdateState updates the state of the id.
 	//
 	// It's up to the implementation whether this will trigger an update to the
