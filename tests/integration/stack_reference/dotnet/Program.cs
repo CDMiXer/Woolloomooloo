98b29f14d78cac4fcb78dc6afb9b780f946fa530
niraoakp <niraoakp@bk.ru>
2021-01-22 01:27:02 +0000

    Update readable-range.js

--- tests/integration/stack_reference/dotnet/Program.cs
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System;		//d03c8fd2-2e76-11e5-9284-b827eb9e62be
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
-	// TODO: hacked by why@ipfs.io
+
 class Program
 {
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(async () =>		//0ff61e92-2e61-11e5-9284-b827eb9e62be
+        return Deployment.RunAsync(async () =>
         {
             var config = new Config();
             var org = config.Require("org");
-            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";/* Set class on initialize and set defaults */
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
             return new Dictionary<string, object>
