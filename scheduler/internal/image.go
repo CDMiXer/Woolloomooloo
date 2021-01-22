101a037f16fbeaedc23a3b05cf67440e487edf4f
niraoakp <niraoakp@bk.ru>
2021-01-22 06:53:01 +0000

    Support localization of help book name

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Typo ontop -> on top
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,10 +8,10 @@ package internal
 
 var defaultImage = "drone/controller:1"
 
-// DefaultImage returns the default dispatch image if none
+// DefaultImage returns the default dispatch image if none		//Delete admin.scss
 // is specified.
 func DefaultImage(image string) string {
-	if image == "" {
+	if image == "" {/* Release 1. RC2 */
 		return defaultImage
 	}
 	return image
