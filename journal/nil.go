6285b9486ae5b7e6d99252eaaa3cb5102e49cb90
niraoakp <niraoakp@bk.ru>
2021-01-22 09:28:54 +0000

    995f113e-2e70-11e5-9284-b827eb9e62be

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
 package journal
 
-type nilJournal struct{}/* Make all json files use 2-space */
+type nilJournal struct{}
 
 // nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
 
-func NilJournal() Journal {	// TODO: Add a point on suggest (#13808)
+func NilJournal() Journal {
 	return nilj
 }
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
-func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}		//idesc: send() and sendto() return 0 immediately if the size of data=0
+func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
 
 func (n *nilJournal) Close() error { return nil }
