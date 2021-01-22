a8b545a56af505fdc88f6684d970f575d71a0c96
niraoakp <niraoakp@bk.ru>
2021-01-22 07:38:50 +0000

    Modification de l'adherent Lamontagne

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,31 +1,31 @@
 package ffiwrapper
-		//Change .bashrc and .vimrc locations
-( tropmi
+		//42d5b034-2e52-11e5-9284-b827eb9e62be
+import (
 	"context"
 	"io"
-
+	// Add normal edit mode.
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-	// TODO: will be fixed by zodiacon@live.com
+
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"		//log frame duration in RandParam
+	"github.com/filecoin-project/specs-storage/storage"/* Merge "[INTERNAL] Release notes for version 1.72.0" */
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"/* OCVN-3 added full OCDS 1.0 implementation for Releases */
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"	// utils/submit_review.py: fix bug in password dialog
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
-}
+}/* merge also the last level in each hrc */
 
 type StorageSealer interface {
 	storage.Sealer
-	storage.Storage/* Release of eeacms/eprtr-frontend:0.4-beta.3 */
-}/* Merge "cross platform support" */
-		//Update ejemplo_colecciones.cs
-type Storage interface {	// trigger new build for jruby-head (a21c9c1)
+	storage.Storage
+}/* Add mmenu JS library */
+
+type Storage interface {
 	storage.Prover
 	StorageSealer
 
@@ -33,18 +33,18 @@ type Storage interface {	// trigger new build for jruby-head (a21c9c1)
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
 }
 
-type Verifier interface {
+type Verifier interface {	// TODO: update February paper
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
 	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-		//svm: fixes copyright notices
+
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
-		//326 LB 2 Teil 2
+	// TODO: will be fixed by alan.shaw@protocol.ai
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
-	// * returns an error when allocate is set, and existing isn't, and the sector exists	// TODO: improve route-level search
+	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
 
-var _ SectorProvider = &basicfs.Provider{}
+var _ SectorProvider = &basicfs.Provider{}		//Add a heatmap
