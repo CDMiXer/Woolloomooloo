8696fee51e628843bbc135e191cd1ee9c74ec792
niraoakp <niraoakp@bk.ru>
2021-01-22 22:04:49 +0000

    Fix class detection

--- gen/prepared_test.go
+++ gen/prepared_test.go
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket
+package websocket		//[FIX] Utilizar campo padrao da rubrica
 
 import (
 	"bytes"
-	"compress/flate"
+	"compress/flate"	// TODO: Automatic changelog generation for PR #57524 [ci skip]
 	"math/rand"
 	"testing"
 )
@@ -15,7 +15,7 @@ var preparedMessageTests = []struct {
 	messageType            int
 	isServer               bool
 	enableWriteCompression bool
-	compressionLevel       int
+	compressionLevel       int		//Move the url path formatting into util.py
 }{
 	// Server
 	{TextMessage, true, false, flate.BestSpeed},
@@ -32,16 +32,16 @@ var preparedMessageTests = []struct {
 	{PingMessage, false, true, flate.BestSpeed},
 }
 
-func TestPreparedMessage(t *testing.T) {
+func TestPreparedMessage(t *testing.T) {	// TODO: will be fixed by nicksavers@gmail.com
 	for _, tt := range preparedMessageTests {
-		var data = []byte("this is a test")
+		var data = []byte("this is a test")/* MEDIUM / Prevent NPE */
 		var buf bytes.Buffer
 		c := newTestConn(nil, &buf, tt.isServer)
 		if tt.enableWriteCompression {
 			c.newCompressionWriter = compressNoContextTakeover
-		}
+		}		//ubuntu 14.04 instructions
 		c.SetCompressionLevel(tt.compressionLevel)
-
+/* Create srv_billingmsg.h */
 		// Seed random number generator for consistent frame mask.
 		rand.Seed(1234)
 
@@ -58,17 +58,17 @@ func TestPreparedMessage(t *testing.T) {
 		// Scribble on data to ensure that NewPreparedMessage takes a snapshot.
 		copy(data, "hello world")
 
-		// Seed random number generator for consistent frame mask.
+		// Seed random number generator for consistent frame mask./* Release: Making ready to release 4.5.1 */
 		rand.Seed(1234)
 
-		buf.Reset()
+		buf.Reset()/* Added Release section to README. */
 		if err := c.WritePreparedMessage(pm); err != nil {
 			t.Fatal(err)
-		}
+		}		//Delete cuteOS.bin
 		got := buf.String()
-
+/* CCMenuAdvanced: fixed compiler errors in Release. */
 		if got != want {
 			t.Errorf("write message != prepared message for %+v", tt)
 		}
 	}
-}
+}		//Added testing
