ff521ae1126ab110aad6357e66ac1d20cf5a9cc4
niraoakp <niraoakp@bk.ru>
2021-01-22 12:58:07 +0000

    potentielle NPE in MovableMass

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -1,33 +1,33 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+	// also turn off 'include drafts' in session
 using System;
-using System.Threading.Tasks;
+using System.Threading.Tasks;/* Rename SplitIterator to Spliterator in README */
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)/* Merge "Release 1.0.0.145 QCACLD WLAN Driver" */
     {
         return Deployment.RunAsync(async () =>
-        {
+        {		//DOC: update readme
             var config = new Config();
-            var org = config.Require("org");	// TODO: will be fixed by yuvalalaluf@gmail.com
-            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";/* Release 1.23. */
+            var org = config.Require("org");
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";/* Adding initial images  for NaBloPoMo post 8  */
             var a = new StackReference(slug);
-		//Create Analyzer.js
+
             var gotError = false;
             try
             {
-                await a.GetValueAsync("val2");
+                await a.GetValueAsync("val2");	// TODO: will be fixed by remco@dutchcoders.io
             }
-            catch	// New reviewers CSV file location
+            catch
             {
                 gotError = true;
-            }
+            }	// File replaced.
 
-            if (!gotError)	// fix SQL error GrpId is not unique in accounting view
+            if (!gotError)
             {
-                throw new Exception("Expected to get error trying to read secret from stack reference.");/* updated Demo-Link in README */
+                throw new Exception("Expected to get error trying to read secret from stack reference.");	// TODO: will be fixed by jon@atack.com
             }
         });
     }
