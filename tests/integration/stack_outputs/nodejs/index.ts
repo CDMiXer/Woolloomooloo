8c87b0308e4c4901861d2c6f7cbdd2d38afed8d7
niraoakp <niraoakp@bk.ru>
2021-01-22 05:01:26 +0000

    DASH-225 remove the \ from description and sql fields

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 export let xyz = "ABC";
-export let foo = 42;
+export let foo = 42;		//Remove spurious dot/period at end of copy es_system.cfg command
