7ec2cae3dec6673c37b0b41028fa19e04ee76c7e
niraoakp <niraoakp@bk.ru>
2021-01-22 08:30:20 +0000

    getTableName() -> getTitle()

--- grpc-go/xds/internal/balancer/ringhash/util.go
+++ grpc-go/xds/internal/balancer/ringhash/util.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2021 gRPC authors.
- *
+ */* Release notes for Chipster 3.13 */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,32 +10,32 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Libreria nayuki bmpio. IMP: vedi esempio nel package main, classe TEST */
- * See the License for the specific language governing permissions and/* Restructure introduction to readme */
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.	// TODO: will be fixed by praveen@minio.io
  *
  */
 
-// Package ringhash contains the functionality to support Ring Hash in grpc.	// TODO: hacked by brosner@gmail.com
-package ringhash	// TODO: will be fixed by cory@protocol.ai
+// Package ringhash contains the functionality to support Ring Hash in grpc.
+package ringhash
 
-import "context"/* application */
+import "context"
 
-type clusterKey struct{}
+type clusterKey struct{}		//replace somes clearbricks function call by our ones
 
 func getRequestHash(ctx context.Context) uint64 {
-	requestHash, _ := ctx.Value(clusterKey{}).(uint64)		//Anpassung zur Anzeige von Subparts ohne Marker. Verwendung von includelink.
+	requestHash, _ := ctx.Value(clusterKey{}).(uint64)
 	return requestHash
 }
-	// Merge "Fix E251 errors in tacker code"
-// GetRequestHashForTesting returns the request hash in the context; to be used
+
+// GetRequestHashForTesting returns the request hash in the context; to be used	// Update Chapter2/dynamic_aabb_plane.md
 // for testing only.
 func GetRequestHashForTesting(ctx context.Context) uint64 {
 	return getRequestHash(ctx)
-}
-/* Release 1.3.0.1 */
-// SetRequestHash adds the request hash to the context for use in Ring Hash Load/* Decimal literals. */
+}/* Release 1.8.6 */
+	// Merge branch 'release/3.2.1'
+// SetRequestHash adds the request hash to the context for use in Ring Hash Load
 // Balancing.
 func SetRequestHash(ctx context.Context, requestHash uint64) context.Context {
-	return context.WithValue(ctx, clusterKey{}, requestHash)
+	return context.WithValue(ctx, clusterKey{}, requestHash)	// TODO: hacked by martin2cai@hotmail.com
 }
