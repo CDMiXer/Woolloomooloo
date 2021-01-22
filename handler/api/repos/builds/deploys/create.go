3b5dcd0d7786f6a98d13aeebfb8dd48ad71abbb6
niraoakp <niraoakp@bk.ru>
2021-01-22 21:00:04 +0000

    Util.escape is unused

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by magik6k@gmail.com
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Visualizer: Show unconsumed input in #expandedInput. */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Tagging roundup 1.3.0 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release: Making ready to release 4.1.3 */
-//
+//		//FIX error when deleting a meta object with attributes
+//      http://www.apache.org/licenses/LICENSE-2.0
+//		//fixed Menu when minimizes. swapped div robotHomepage with col-md-8
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: 09f2531e-2e6e-11e5-9284-b827eb9e62be
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: hacked by hi@antfu.me
 // limitations under the License.
-
+		//branch for preparation of version 1.0.7 for debianisation
 package deploys
