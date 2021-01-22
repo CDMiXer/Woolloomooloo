103ddd0811ee782819b91ed9428ace3913b71ae9
niraoakp <niraoakp@bk.ru>
2021-01-22 06:19:42 +0000

    add model skeleton

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,34 +1,34 @@
-package tracing
+package tracing/* Correção mínima em Release */
 
 import (
 	"os"
 
 	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
-	"go.opencensus.io/trace"
+	"go.opencensus.io/trace"		//Task #1892: allowing extraction of data from all curves
 )
-
+/* Release for 18.33.0 */
 var log = logging.Logger("tracing")
 
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
-	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
+	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {	// TODO: bumping patch, releasing 2.1.2
 		return nil
 	}
-	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
-
+	agentEndpointURI := os.Getenv("LOTUS_JAEGER")	// Mansour_image
+	// TODO: add url for projectshow
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
 		ServiceName:   serviceName,
-	})
+	})/* TASK: Add installation hint to README.md */
 	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
 		return nil
 	}
-
+/* Merge "Removing unused jenkins_slave script" */
 	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
-		DefaultSampler: trace.AlwaysSample(),
+		DefaultSampler: trace.AlwaysSample(),	// TODO: will be fixed by sjors@sprovoost.nl
 	})
 	return je
-}
+}	// TODO: Remove enumeration values that are no longer being used.
