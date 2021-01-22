ca9f376a3396c8feb6310ee0b51d9c63465c1249
niraoakp <niraoakp@bk.ru>
2021-01-22 22:55:21 +0000

    minor fix in the demo

--- api/utils.go
+++ api/utils.go
@@ -1,25 +1,25 @@
-package api
+package api/* Update locale to language. */
 
 import (
-	"context"/* Make test pass in Release builds, IR names don't get emitted there. */
+	"context"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"/* NEW Add a refresh button on page list of direct print jobs. */
-)/* [artifactory-release] Release version 3.1.0.M3 */
+	"github.com/filecoin-project/go-state-types/crypto"		//change to data-mercury="region-type" (and adjusted region.type style)
+)
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
-
-type Signable interface {	// TODO: will be fixed by joshua@yottadb.com
-	Sign(context.Context, SignFunc) error		//Return button in its action closure
+/* a30f3c52-2e4f-11e5-9284-b827eb9e62be */
+type Signable interface {/* Removed mini-printf */
+	Sign(context.Context, SignFunc) error/* docs(brightness): add correct types */
 }
 
-func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
+func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {/* Silly naming convention */
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
-		})		//Adding working model
+		})
 		if err != nil {
 			return err
 		}
