9d851dfe5062b7c0224752d97fe9423d0a86ca94
niraoakp <niraoakp@bk.ru>
2021-01-22 13:56:38 +0000

    Better behavior for custom resolutions

--- handler/api/secrets/create_test.go
+++ handler/api/secrets/create_test.go
@@ -1,62 +1,62 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//FIX - final grid
-		//removed diff raw parse's debugging line
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package secrets
 
-( tropmi
+import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"net/http"/* enemy movement */
+	"net/http"
 	"net/http/httptest"
 	"testing"
-	// TODO: Refactor tagging to start on 'start' event.
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// TODO: hacked by seth@sethvargo.com
+	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"	// TODO: ^ addition
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"	// 6ce14fbe-2e6b-11e5-9284-b827eb9e62be
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleCreate(t *testing.T) {		//Update xmpp-config-schema.coffee
+func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
-)txetnoC.ihc(wen =: c	
+	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
 
-	w := httptest.NewRecorder()	// TODO: [skip netlify]
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleCreate(secrets).ServeHTTP(w, r)/* Fixing non-escaped quotes */
-	if got, want := w.Code, http.StatusOK; want != got {/* Release of eeacms/plonesaas:5.2.1-54 */
+	HandleCreate(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-)ffid(frorrE.t		
+		t.Errorf(diff)
 	}
 }
-	// TODO: hacked by joshua@yottadb.com
+
 func TestHandleCreate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
-)(hsiniF.rellortnoc refed	
+	defer controller.Finish()
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
