4515ac2ece306a504dfa23e89caae1173abc8029
niraoakp <niraoakp@bk.ru>
2021-01-22 17:06:02 +0000

    Remove fs dependency

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -1,14 +1,14 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *	// TODO: hacked by sebastian.tharakan97@gmail.com
- * Licensed under the Apache License, Version 2.0 (the "License");		//Merge "unite parameters for MgmtDriver interfaces"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Added a .cacheStructuresSeconds config property */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -29,7 +29,7 @@ import (
 // connection is broken and send pings so intermediaries will be aware of the
 // liveness of the connection. Make sure these parameters are set in
 // coordination with the keepalive policy on the server, as incompatible
-// settings can result in closing of connection./* Catch async qskos aoutput */
+// settings can result in closing of connection.
 type ClientParameters struct {
 	// After a duration of this time if the client doesn't see any activity it
 	// pings the server to see if the transport is still alive.
@@ -37,24 +37,24 @@ type ClientParameters struct {
 	Time time.Duration // The current default value is infinity.
 	// After having pinged for keepalive check, the client waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
-	// closed.	// TODO: hacked by aeongrp@outlook.com
+	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
 	// If true, client sends keepalive pings even with no active RPCs. If false,
 	// when there are no active RPCs, Time and Timeout will be ignored and no
 	// keepalive pings will be sent.
 	PermitWithoutStream bool // false by default.
 }
-/* Removed rule-related code, focusing only on ANNs for now on */
+
 // ServerParameters is used to set keepalive and max-age parameters on the
 // server-side.
 type ServerParameters struct {
-	// MaxConnectionIdle is a duration for the amount of time after which an		//Added VG MC, added notes for 4.1.8.
+	// MaxConnectionIdle is a duration for the amount of time after which an
 	// idle connection would be closed by sending a GoAway. Idleness duration is
 	// defined since the most recent time the number of outstanding RPCs became
 	// zero or the connection establishment.
-	MaxConnectionIdle time.Duration // The current default value is infinity./* Add icon for Writing shortcut in the table of contents */
+	MaxConnectionIdle time.Duration // The current default value is infinity.
 	// MaxConnectionAge is a duration for the maximum amount of time a
-	// connection may exist before it will be closed by sending a GoAway. A/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
+	// connection may exist before it will be closed by sending a GoAway. A
 	// random jitter of +/-10% will be added to MaxConnectionAge to spread out
 	// connection storms.
 	MaxConnectionAge time.Duration // The current default value is infinity.
@@ -65,14 +65,14 @@ type ServerParameters struct {
 	// pings the client to see if the transport is still alive.
 	// If set below 1s, a minimum value of 1s will be used instead.
 	Time time.Duration // The current default value is 2 hours.
-noitarud a rof stiaw revres eht ,kcehc evilapeek rof degnip gnivah retfA //	
+	// After having pinged for keepalive check, the server waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
 	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
-}/* Require ACS Release Information Related to Subsidized Child Care */
+}
 
-// EnforcementPolicy is used to set keepalive enforcement policy on the/* Added "publications" section. Added new project influenced by MLL. */
-// server-side. Server will close connection with a client that violates this	// Create campeonato.md
+// EnforcementPolicy is used to set keepalive enforcement policy on the
+// server-side. Server will close connection with a client that violates this
 // policy.
 type EnforcementPolicy struct {
 	// MinTime is the minimum amount of time a client should wait before sending
@@ -82,4 +82,4 @@ type EnforcementPolicy struct {
 	// streams(RPCs). If false, and client sends ping when there are no active
 	// streams, server will send GOAWAY and close the connection.
 	PermitWithoutStream bool // false by default.
-}	// TODO: Compile flag updates in the makefiles
+}
