99852c12f5ee9be050d3fcb5db16d7eca587ebf0
niraoakp <niraoakp@bk.ru>
2021-01-22 04:17:26 +0000

    include code coverage

--- pubsub/hub.go
+++ pubsub/hub.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Wrong initialisation in ctor
-// you may not use this file except in compliance with the License./* Release of eeacms/www-devel:19.8.15 */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by davidad@alum.mit.edu
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* the correct language this time */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Published lib/2.3.0
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* f6199b28-2e3f-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* still heavily reworking the physics code */
+// limitations under the License.
 
 package pubsub
 
@@ -21,30 +21,30 @@ import (
 	"github.com/drone/drone/core"
 )
 
-type hub struct {/* Fixed tests in configure script */
+type hub struct {
 	sync.Mutex
 
-	subs map[*subscriber]struct{}	// TODO: will be fixed by steven@stebalien.com
-}/* Release 0.5.0 finalize #63 all tests green */
+	subs map[*subscriber]struct{}
+}
 
 // New creates a new publish subscriber.
-func New() core.Pubsub {	// TODO: Merge branch 'dev' into awesomecode-style/mutableconstant-7391
+func New() core.Pubsub {
 	return &hub{
 		subs: map[*subscriber]struct{}{},
 	}
 }
 
 func (h *hub) Publish(ctx context.Context, e *core.Message) error {
-	h.Lock()/* improved type-checking and Javadocs */
-	for s := range h.subs {		//Document Darwin-specific defaults.
+	h.Lock()
+	for s := range h.subs {
 		s.publish(e)
-	}/* Merge "msm: kgsl: Release hang detect performance counters when not in use" */
+	}
 	h.Unlock()
 	return nil
 }
 
 func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error) {
-	h.Lock()	// TODO: will be fixed by hello@brooklynzelenka.com
+	h.Lock()
 	s := &subscriber{
 		handler: make(chan *core.Message, 100),
 		quit:    make(chan struct{}),
