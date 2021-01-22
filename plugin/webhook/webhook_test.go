4c473f1aed3da396cb85eb5d2767e16008f35c67
niraoakp <niraoakp@bk.ru>
2021-01-22 01:36:23 +0000

    Merge branch 'master' into chromecast

--- plugin/webhook/webhook_test.go
+++ plugin/webhook/webhook_test.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* fixed error alert */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+		//practice github flow in web ui
+// +build !oss/* Updated Release_notes.txt for 0.6.3.1 */
 
 package webhook
 
-import (
+import (	// TODO: Add couple of solutions for forms
 	"context"
-	"net/http"
-	"testing"
+	"net/http"/* Release areca-7.3.9 */
+	"testing"/* Rename Test to test.py */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* switch Calibre download to GitHubReleasesInfoProvider to ensure https */
 
 	"github.com/99designs/httpsignatures-go"
 	"github.com/h2non/gock"
 )
 
 var noContext = context.Background()
-
+	// TODO: hacked by juan@benet.ai
 func TestWebhook(t *testing.T) {
 	defer gock.Off()
 
@@ -28,7 +28,7 @@ func TestWebhook(t *testing.T) {
 		User:   &core.User{Login: "octocat"},
 	}
 
-	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {
+	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {	// TODO: Fix geometry type for "Convert geometry type" algorithm
 		signature, err := httpsignatures.FromRequest(r)
 		if err != nil {
 			return false, err
@@ -43,7 +43,7 @@ func TestWebhook(t *testing.T) {
 		MatchHeader("Content-Type", "application/json").
 		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").
 		JSON(webhook).
-		Reply(200).
+		Reply(200)./* [artifactory-release] Release version 0.8.2.RELEASE */
 		Type("application/json")
 
 	config := Config{
@@ -53,15 +53,15 @@ func TestWebhook(t *testing.T) {
 	sender := New(config)
 	err := sender.Send(noContext, webhook)
 	if err != nil {
-		t.Error(err)
-	}
+		t.Error(err)/* Release notes generator */
+}	
 
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")
-	}
-}
+		t.Errorf("Unfinished requests")		//Merged with trunk and fixed broken unit testcases
+}	
+}	// TODO: hacked by greg@colvin.org
 
-func TestWebhook_CustomClient(t *testing.T) {
+func TestWebhook_CustomClient(t *testing.T) {		//1163]: Please put summary nodes options back in the context menu
 	sender := new(sender)
 	if sender.client() != http.DefaultClient {
 		t.Errorf("Expect default http client")
