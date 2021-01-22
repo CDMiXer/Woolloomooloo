eda4605b8eb18ed79a2509b2a5b9babc88e184eb
niraoakp <niraoakp@bk.ru>
2021-01-22 22:16:18 +0000

    * Some code revert. OpcodeDispatcher good for debugging but bad for speed.

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,12 +1,12 @@
 package store
-		//document part of r56359
+
 import (
 	"context"
 	"math/big"
-		//optimization for the Item::add/removeReference methods
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
 
-	big2 "github.com/filecoin-project/go-state-types/big"/* ADD: maven deploy plugin - updateReleaseInfo=true */
+	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -19,22 +19,22 @@ var zero = types.NewInt(0)
 func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
 	if ts == nil {
 		return types.NewInt(0), nil
-	}		//Задел под перенос MCCreateAcc в Activity
-	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)	// TODO: Replace AlterKills script with "/alterkill" slash action
+	}
+	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-	var out = new(big.Int).Set(ts.ParentWeight().Int)/* theme : removing mdb-* theme files */
+	var out = new(big.Int).Set(ts.ParentWeight().Int)
 
 	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	tpow := big2.Zero()
 	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
-		state, err := state.LoadStateTree(cst, ts.ParentState())/* Create ben-jij-de-vuurvreter.md */
+		state, err := state.LoadStateTree(cst, ts.ParentState())
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)/* BISERVER-6714 - Adding a combo button for adding a datasource */
-		}	// Model: Add line_color and line_width
+			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
+		}
 
-		act, err := state.GetActor(power.Address)	// TODO: Create mat_rnn_run.py
+		act, err := state.GetActor(power.Address)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
 		}
@@ -46,17 +46,17 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 
 		claim, err := powState.TotalPower()
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)/* Updates Release Link to Point to Releases Page */
+			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
 		}
-/* Author Information Updated */
+
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
 	}
-/* Update changelog typo */
+
 	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
-	} else {/* Merge "Release 1.0.0.194 QCACLD WLAN Driver" */
-		// Not really expect to be here .../* Fix #405 in Joomla 1.5 */
+	} else {
+		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
 
@@ -69,7 +69,7 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 		totalJ += b.ElectionProof.WinCount
 	}
 
-	eWeight := big.NewInt((log2P * build.WRatioNum))		//20951152-2e4a-11e5-9284-b827eb9e62be
+	eWeight := big.NewInt((log2P * build.WRatioNum))
 	eWeight = eWeight.Lsh(eWeight, 8)
 	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))
 	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))
