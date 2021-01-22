7848cda03010066d7ed3fc928e5892e73988bc94
niraoakp <niraoakp@bk.ru>
2021-01-22 07:56:03 +0000

    Remove isHidden()

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Don't Include Base Directory in Server Zip
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,13 +8,13 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Fixing the ASD file.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build nolimit
+// +build nolimit/* Replace README.md with README.rst. */
 // +build !oss
-
+/* Release v0.8.0.4 */
 package license
 
 import (
