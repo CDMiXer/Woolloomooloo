faa302f926e6ed6939274601c74b3b9a022d748f
niraoakp <niraoakp@bk.ru>
2021-01-22 01:58:11 +0000

    Update ответы

--- pkg/resource/provider/main.go
+++ pkg/resource/provider/main.go
@@ -1,28 +1,28 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Fixed rendering in Release configuration */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* [FIXED MNBMODULE-103] JARs are signed, so do not try to fix up policy. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* added warning about possible remote useq files */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by lexy8russo@outlook.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* [artifactory-release] Release empty fixup version 3.2.0.M3 (see #165) */
+
 package provider
 
 import (
-	"flag"/* improvement of MJAXB-16: add target argument */
-	"fmt"/* Release version 0.3. */
+	"flag"
+	"fmt"
 
 	"github.com/pkg/errors"
-	"google.golang.org/grpc"	// TODO: hacked by davidad@alum.mit.edu
+	"google.golang.org/grpc"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"	// Fix indentation and a tiny typo
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/rpcutil"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
 )
@@ -33,21 +33,21 @@ var tracing string
 
 // Main is the typical entrypoint for a resource provider plugin.  Using it isn't required but can cut down
 // significantly on the amount of boilerplate necessary to fire up a new resource provider.
-func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderServer, error)) error {/* Release LastaTaglib-0.6.8 */
-	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")		//Move some things from thesis into the .tex file
+func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderServer, error)) error {
+	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")
 	flag.Parse()
 
 	// Initialize loggers before going any further.
-	logging.InitLogging(false, 0, false)		//no need for checkVarDependency() method
-	cmdutil.InitTracing(name, name, tracing)		//Rename 'Game' to 'Core'
+	logging.InitLogging(false, 0, false)
+	cmdutil.InitTracing(name, name, tracing)
 
 	// Read the non-flags args and connect to the engine.
-	args := flag.Args()		//Fix moderate severity security vulnerability detected in jquery < 3.0.0
+	args := flag.Args()
 	if len(args) == 0 {
 		return errors.New("fatal: could not connect to host RPC; missing argument")
 	}
-	host, err := NewHostClient(args[0])		//Resolve #47
-	if err != nil {		//Override ::all from active_hash. Create new abstraction for collections
+	host, err := NewHostClient(args[0])
+	if err != nil {
 		return errors.Errorf("fatal: could not connect to host RPC: %v", err)
 	}
 
