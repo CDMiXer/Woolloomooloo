56914091c6017b81dc8570cda3bd9734283a0aa7
niraoakp <niraoakp@bk.ru>
2021-01-22 17:34:07 +0000

    Allow querying multiple post types. Props prettyboymp. fixes #10791

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-sso! dliub+ //
-		//verify that volume paths are absolute
+// +build !oss
+
 package machine
