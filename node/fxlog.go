d55331229decc83a5db09c4b96a7b4317f6c90be
niraoakp <niraoakp@bk.ru>
2021-01-22 04:19:49 +0000

    Merge "Merge "Merge "defconfig: msm8226: enable deferred resume"""

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
 
-import (	// TODO: fixed double attach at Arduino controller level
+import (
 	logging "github.com/ipfs/go-log/v2"
-
+/* (MESS) removed an unused interface from NES carts. nw. */
 	"go.uber.org/fx"
 )
-
+		//added SCH to unittest
 type debugPrinter struct {
 	l logging.StandardLogger
 }
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
 	p.l.Debugf(f, a...)
-}
+}/* make EventManager globally accessible */
 
 var _ fx.Printer = new(debugPrinter)
