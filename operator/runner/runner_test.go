42b098000db83be7269541e40ec9ecf8f109b22a
niraoakp <niraoakp@bk.ru>
2021-01-22 11:34:17 +0000

    Rename whatissession to java_task_05_whatissession

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//added $.afui.showMask() timeout param description
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package runner
-/* Release 2.0.4 - use UStack 1.0.9 */
+
 import (
-	"io/ioutil"		//Added Lpopt License
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}	// Z-index test change
+}
