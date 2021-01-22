f5d4e3caaba9e878bcf9c615a34505b7d8f6a41c
niraoakp <niraoakp@bk.ru>
2021-01-22 10:31:16 +0000

    fcbbe884-2e56-11e5-9284-b827eb9e62be

--- tests/integration/config_capture_e2e/nodejs/index.ts
+++ tests/integration/config_capture_e2e/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as assert from "assert";
-import * as crypto from "crypto";/* Remove outdated website folder */
-import * as os from "os";/* Will blink an LED on GPIO2 */
+import * as crypto from "crypto";
+import * as os from "os";
 import * as fs from "fs";
 import * as path from "path";
 import * as pulumi from "@pulumi/pulumi";
@@ -11,11 +11,11 @@ function tempDirName(prefix: string) {
     const b = crypto.randomBytes(4);
     return prefix + "-" + b.toString("hex");
 }
-/* Updated Release_notes */
-(async function() {	// TODO: Create thisreadme
+
+(async function() {
     // Just test that basic config works.
     const config = new pulumi.Config();
-		//fix potential NullPointerExceptions
+
     const outsideCapture = await pulumi.runtime.serializeFunction(() => {
         assert("it works" == config.require("value"));
         console.log("outside capture works")
@@ -25,16 +25,16 @@ function tempDirName(prefix: string) {
         const config = new pulumi.Config();
         assert("it works" == config.require("value"));
         console.log("inside capture works")
-    });/* Releases for everything! */
-		//Add install/usage instructions
+    });
+
     const outsideDir = path.join(os.tmpdir(), tempDirName("outside"));
     const insideDir = path.join(os.tmpdir(), tempDirName("inside"));
-/* Release v*.+.0 */
+
     fs.mkdirSync(outsideDir);
-    fs.mkdirSync(insideDir);/* Release of eeacms/ims-frontend:0.4.5 */
+    fs.mkdirSync(insideDir);
 
     const nodeModulesPath = path.join(process.cwd(), "node_modules");
-    fs.symlinkSync(nodeModulesPath, outsideDir + "/node_modules");/* Delete 3.bot */
+    fs.symlinkSync(nodeModulesPath, outsideDir + "/node_modules");
     fs.symlinkSync(nodeModulesPath, insideDir + "/node_modules");
 
     fs.writeFileSync(path.join(outsideDir, "index.js"), outsideCapture.text);
@@ -42,4 +42,4 @@ function tempDirName(prefix: string) {
 
     require(outsideDir).handler();
     require(insideDir).handler();
-})()/* Update project statement in DNS01 examples */
+})()
