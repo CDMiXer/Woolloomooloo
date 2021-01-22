2a4bdd42d75029518b2a1ca7bba602d4a76a25f0
niraoakp <niraoakp@bk.ru>
2021-01-22 13:22:08 +0000

    Animations for Interlocked Rally and Interlocked Ramble

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -2,21 +2,21 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-	// TODO: will be fixed by arachnid@notdot.net
-// The DBR deletion of A triggers the deletion of C due to dependency.	// Add possibility to write summary to file.
-// The planner should execute these steps (in this exact order):	// TODO: Assert macros added to 'PS_rosesegment' function - tests passed.
-//   1. DeleteReplacement Dependent/* Delete ima5.jpg */
-//   2. DeleteReplacement Base	// TODO: Merge branch 'master' into product300-disable-new-dataset
-//   3. Replace Base	// Added no activity restart interval to the inputs spec
-//   4. CreateReplacement Base
+/* Release v0.1.0-beta.13 */
+// The DBR deletion of A triggers the deletion of C due to dependency.
+// The planner should execute these steps (in this exact order):
+//   1. DeleteReplacement Dependent	// Possible issue fix up
+//   2. DeleteReplacement Base
+//   3. Replace Base/* Version 2.0 Release Notes Updated */
+//   4. CreateReplacement Base	// Refactor by renaming variables (Refs #11199).
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
 
-//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)/* Updating README for Release */
+//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)	// TODO: Statement compilation now uses shadowed infos
 //   5. DeleteReplacement Base-2
 //   6. Replace Base-2
-//   7. CreateReplacement Base-2	// TODO: hacked by juan@benet.ai
+//   7. CreateReplacement Base-2
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
 
-//   8. Replace Dependent/* Update CollectionsExercises.java */
-//   9. CreateReplacement Dependent		//963dda56-2e40-11e5-9284-b827eb9e62be
+//   8. Replace Dependent
+//   9. CreateReplacement Dependent
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
