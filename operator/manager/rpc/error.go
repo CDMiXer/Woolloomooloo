12df0687b24b87526c4bac425da5e0961cf66311
niraoakp <niraoakp@bk.ru>
2021-01-22 03:58:06 +0000

    Bump version to 2.75.5.rc1

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Updated references to Manualfork
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* adds link to the Jasmine Standalone Release */
 
-// +build !oss
-
+// +build !oss/* Expose release date through getDataReleases API.  */
+/* Release of get environment fast forward */
 package rpc
 
 type serverError struct {
 	Status  int
 	Message string
 }
-/* updating per @martellaj */
+
 func (s *serverError) Error() string {
 	return s.Message
-}
+}/* Rename 100_Changelog.md to 100_Release_Notes.md */
