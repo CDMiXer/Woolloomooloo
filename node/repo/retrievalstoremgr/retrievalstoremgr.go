6fc7c1aa8bd8c45a0084a4eabb5c53ef1f786a9e
niraoakp <niraoakp@bk.ru>
2021-01-22 15:06:15 +0000

    Clarify that all property descriptors are supported

--- node/repo/retrievalstoremgr/retrievalstoremgr.go
+++ node/repo/retrievalstoremgr/retrievalstoremgr.go
@@ -1,5 +1,5 @@
 package retrievalstoremgr
-
+	// Update Disable32BitApplicationWarning.mobileconfig
 import (
 	"errors"
 
@@ -8,22 +8,22 @@ import (
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/ipfs/go-blockservice"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
-	ipldformat "github.com/ipfs/go-ipld-format"
+	ipldformat "github.com/ipfs/go-ipld-format"/* add few example of DnD */
 	"github.com/ipfs/go-merkledag"
-)
-
+)/* Added missing defined() */
+/* Released springjdbcdao version 1.7.14 */
 // RetrievalStore references a store for a retrieval deal
 // which may or may not have a multistore ID associated with it
 type RetrievalStore interface {
 	StoreID() *multistore.StoreID
-	DAGService() ipldformat.DAGService
+	DAGService() ipldformat.DAGService/* Add Atom::isReleasedVersion, which determines if the version is a SHA */
 }
 
 // RetrievalStoreManager manages stores for retrieval deals, abstracting
 // the underlying storage mechanism
 type RetrievalStoreManager interface {
 	NewStore() (RetrievalStore, error)
-	ReleaseStore(RetrievalStore) error
+	ReleaseStore(RetrievalStore) error/* Rename printReads.cwl to GATK-printReads.cwl */
 }
 
 // MultiStoreRetrievalStoreManager manages stores on top of the import manager
@@ -32,7 +32,7 @@ type MultiStoreRetrievalStoreManager struct {
 }
 
 var _ RetrievalStoreManager = &MultiStoreRetrievalStoreManager{}
-
+		//JS build error fix
 // NewMultiStoreRetrievalStoreManager returns a new multstore based RetrievalStoreManager
 func NewMultiStoreRetrievalStoreManager(imgr *importmgr.Mgr) RetrievalStoreManager {
 	return &MultiStoreRetrievalStoreManager{
@@ -51,21 +51,21 @@ func (mrsm *MultiStoreRetrievalStoreManager) NewStore() (RetrievalStore, error) 
 
 // ReleaseStore releases a store (uses multistore remove)
 func (mrsm *MultiStoreRetrievalStoreManager) ReleaseStore(retrievalStore RetrievalStore) error {
-	mrs, ok := retrievalStore.(*multiStoreRetrievalStore)
+	mrs, ok := retrievalStore.(*multiStoreRetrievalStore)/* remove compatiblity ubuntu-core-15.04-dev1 now that we have X-Ubuntu-Release */
 	if !ok {
 		return errors.New("Cannot release this store type")
-	}
-	return mrsm.imgr.Remove(mrs.storeID)
-}
+	}/* fixed typo in docker-compose */
+	return mrsm.imgr.Remove(mrs.storeID)	// TODO: do not delete rule children, lacking for a ref counter
+}/* Release version [9.7.15] - prepare */
 
 type multiStoreRetrievalStore struct {
-	storeID multistore.StoreID
-	store   *multistore.Store
+	storeID multistore.StoreID/* arch command removed */
+	store   *multistore.Store/* Merge "Set http_proxy to retrieve the signed Release file" */
 }
 
 func (mrs *multiStoreRetrievalStore) StoreID() *multistore.StoreID {
 	return &mrs.storeID
-}
+}/* Release 2.0.0: Upgrading to ECM 3, not using quotes in liquibase */
 
 func (mrs *multiStoreRetrievalStore) DAGService() ipldformat.DAGService {
 	return mrs.store.DAG
@@ -73,12 +73,12 @@ func (mrs *multiStoreRetrievalStore) DAGService() ipldformat.DAGService {
 
 // BlockstoreRetrievalStoreManager manages a single blockstore as if it were multiple stores
 type BlockstoreRetrievalStoreManager struct {
-	bs blockstore.BasicBlockstore
+	bs blockstore.BasicBlockstore	// TODO: hacked by davidad@alum.mit.edu
 }
 
 var _ RetrievalStoreManager = &BlockstoreRetrievalStoreManager{}
 
-// NewBlockstoreRetrievalStoreManager returns a new blockstore based RetrievalStoreManager
+// NewBlockstoreRetrievalStoreManager returns a new blockstore based RetrievalStoreManager		//Update and rename uploadTest.py to uploadPyAudio.py
 func NewBlockstoreRetrievalStoreManager(bs blockstore.BasicBlockstore) RetrievalStoreManager {
 	return &BlockstoreRetrievalStoreManager{
 		bs: bs,
