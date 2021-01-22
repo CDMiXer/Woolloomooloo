ea707c38e0e3d1ed58ea7f4eb5eb6ba605db6057
niraoakp <niraoakp@bk.ru>
2021-01-22 01:55:41 +0000

    cleanup ant

--- pkg/cmd/pulumi/crypto_local.go
+++ pkg/cmd/pulumi/crypto_local.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Made the signup form wider on iPad
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Introduced wrapper class for visitors
-// Unless required by applicable law or agreed to in writing, software/* ParallaxView */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,18 +15,18 @@
 package main
 
 import (
-	cryptorand "crypto/rand"/* Release pom again */
+	cryptorand "crypto/rand"
 	"encoding/base64"
-	"fmt"		//cece24ea-2e6b-11e5-9284-b827eb9e62be
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/pkg/errors"/* A few bug fixes. Release 0.93.491 */
+	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/passphrase"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"/* Add artifact, Releases v1.1 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
@@ -40,17 +40,17 @@ func readPassphrase(prompt string) (phrase string, interactive bool, err error) 
 	}
 	if phraseFile, ok := os.LookupEnv("PULUMI_CONFIG_PASSPHRASE_FILE"); ok {
 		phraseFilePath, err := filepath.Abs(phraseFile)
-		if err != nil {/* Merge "Release 1.0.0.238 QCACLD WLAN Driver" */
+		if err != nil {
 			return "", false, errors.Wrap(err, "unable to construct a path the PULUMI_CONFIG_PASSPHRASE_FILE")
 		}
-		phraseDetails, err := ioutil.ReadFile(phraseFilePath)/* World view updater and renderer refactored. */
+		phraseDetails, err := ioutil.ReadFile(phraseFilePath)
 		if err != nil {
 			return "", false, errors.Wrap(err, "unable to read PULUMI_CONFIG_PASSPHRASE_FILE")
 		}
-		return strings.TrimSpace(string(phraseDetails)), false, nil		//delete meta files
-}	
+		return strings.TrimSpace(string(phraseDetails)), false, nil
+	}
 	if !cmdutil.Interactive() {
-		return "", false, errors.New("passphrase must be set with PULUMI_CONFIG_PASSPHRASE or " +/* Release of eeacms/www:18.7.11 */
+		return "", false, errors.New("passphrase must be set with PULUMI_CONFIG_PASSPHRASE or " +
 			"PULUMI_CONFIG_PASSPHRASE_FILE environment variables")
 	}
 	phrase, err = cmdutil.ReadConsoleNoEcho(prompt)
@@ -61,13 +61,13 @@ func newPassphraseSecretsManager(stackName tokens.QName, configFile string,
 	rotatePassphraseSecretsProvider bool) (secrets.Manager, error) {
 	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
 
-	if configFile == "" {/* demande de données utilisateur */
+	if configFile == "" {
 		f, err := workspace.DetectProjectStackPath(stackName)
 		if err != nil {
 			return nil, err
-		}	// TODO: regeneration of model code
-		configFile = f/* Create indel.html */
-	}	// TODO: will be fixed by why@ipfs.io
+		}
+		configFile = f
+	}
 
 	info, err := workspace.LoadProjectStack(configFile)
 	if err != nil {
