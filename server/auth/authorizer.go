b9ac7d265c03ced19de73436508c4b232314061f
niraoakp <niraoakp@bk.ru>
2021-01-22 05:46:37 +0000

    Use getopts for ynh_setup_source helper

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
-package auth		//Doubles COIL reqs
+package auth/* Release documentation updates. */
 
 import (
-	"context"	// TODO: hacked by lexy8russo@outlook.com
-	// 18a10294-2e63-11e5-9284-b827eb9e62be
-	authUtil "github.com/argoproj/argo/util/auth"	// TODO: will be fixed by why@ipfs.io
+	"context"
+
+	authUtil "github.com/argoproj/argo/util/auth"
 )
 
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
-	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)/* Release 1.13. */
+	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
-		return false, err
+		return false, err/* 2.6 Release */
 	}
 	return allowed, nil
-}
+}		//Merge "[TrivialFix] Fix typo error"
