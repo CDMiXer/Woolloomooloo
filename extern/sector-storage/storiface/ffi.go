3dd4d0642a12340bd13424b61ec0ff46de279004
niraoakp <niraoakp@bk.ru>
2021-01-22 11:53:36 +0000

    Add comment for SSL cert verification (disabled)

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -5,16 +5,16 @@ import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
-
+/* ass setReleaseDOM to false so spring doesnt change the message  */
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 var ErrSectorNotFound = errors.New("sector not found")
-
+/*  Arch: import ifc, fix typo in material colors */
 type UnpaddedByteIndex uint64
 
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())/* Release note updated */
 }
 
 type PaddedByteIndex uint64
