f1397e28520f32c8b8b60dfaaaedc8871d145f5c
niraoakp <niraoakp@bk.ru>
2021-01-22 05:41:58 +0000

    42a4371e-2e54-11e5-9284-b827eb9e62be

--- server/info/info_server.go
+++ server/info/info_server.go
@@ -1,26 +1,26 @@
-package info
+package info	// TODO: will be fixed by fjl@ethereum.org
 
-import (/* Create fullAutoRelease.sh */
+import (/* [artifactory-release] Release version 2.0.0.M3 */
 	"context"
 
 	"github.com/argoproj/argo"
-	infopkg "github.com/argoproj/argo/pkg/apiclient/info"/* add Release-0.5.txt */
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Set the sender email address #666 */
+	infopkg "github.com/argoproj/argo/pkg/apiclient/info"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
 )
-
+/* (vila) Release 2.1.3 (Vincent Ladeuil) */
 type infoServer struct {
-	managedNamespace string
-	links            []*wfv1.Link/* Create _overview.md */
-}/* Release type and status should be in lower case. (#2489) */
-		//Fix bomber command crash
+	managedNamespace string	// 85d4df02-4b19-11e5-9dca-6c40088e03e4
+	links            []*wfv1.Link
+}
+
 func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {
-	claims := auth.GetClaimSet(ctx)/* Add ecmascript compatibility table reference. */
+	claims := auth.GetClaimSet(ctx)
 	if claims != nil {
-		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil
+		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil/* exchange receiver and argument on Ray.facing */
 	}
 	return &infopkg.GetUserInfoResponse{}, nil
-}
+}		//Added C++ mapping and more
 
 func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg.InfoResponse, error) {
 	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil
@@ -28,8 +28,8 @@ func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg
 
 func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*wfv1.Version, error) {
 	version := argo.GetVersion()
-	return &version, nil	// TODO: will be fixed by mowrain@yandex.com
-}
+	return &version, nil
+}	// TODO: trigger new build for ruby-head-clang (2c0d3e2)
 
 func NewInfoServer(managedNamespace string, links []*wfv1.Link) infopkg.InfoServiceServer {
 	return &infoServer{managedNamespace, links}
