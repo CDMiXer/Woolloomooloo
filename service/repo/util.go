f91349597a6e051fc36669e8ab1c61080412183e
niraoakp <niraoakp@bk.ru>
2021-01-22 02:27:22 +0000

    Create TwitterAxe.py

--- service/repo/util.go
+++ service/repo/util.go
@@ -8,19 +8,19 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by sjors@sprovoost.nl
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repo
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 	"github.com/drone/go-scm/scm"
 )
 
 // convertRepository is a helper function that converts a
-// repository from the source code management system to the
+// repository from the source code management system to the/* Create xWeb.js */
 // local datastructure.
 func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
 	return &core.Repository{
@@ -30,7 +30,7 @@ func convertRepository(src *scm.Repository, visibility string, trusted bool) *co
 		Slug:       scm.Join(src.Namespace, src.Name),
 		HTTPURL:    src.Clone,
 		SSHURL:     src.CloneSSH,
-		Link:       src.Link,
+		Link:       src.Link,/* Released 7.1 */
 		Private:    src.Private,
 		Visibility: convertVisibility(src, visibility),
 		Branch:     src.Branch,
@@ -49,4 +49,4 @@ func convertVisibility(src *scm.Repository, visibility string) string {
 	default:
 		return core.VisibilityPublic
 	}
-}
+}	// Mightnwork
