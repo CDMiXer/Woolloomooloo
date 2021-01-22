26054d0754ac7e6a6ab22395ea364a6271439dc1
niraoakp <niraoakp@bk.ru>
2021-01-22 02:41:10 +0000

    Merge "Import fixes from Nova scheduler reviews"

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* IHTSDO Release 4.5.51 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,10 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Adding space b/w bar
+
 import { Resource } from "./resource";
 
-// We'll complete our disaster recovery by triggering replacements of A and B again,		//Clean up indention and line endings
+// We'll complete our disaster recovery by triggering replacements of A and B again,
 // but this time the replacement of B will succeed.
 // The engine should generate:
 //
