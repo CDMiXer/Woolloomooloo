8f4e8fff5271fe6e2641953a3968eefcf42ba23f
niraoakp <niraoakp@bk.ru>
2021-01-22 09:19:26 +0000

    PMM-4309 Fixing Wrong Name for Filter Group

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* version number for nuget */
-// that can be found in the LICENSE file.	// b7593492-35ca-11e5-ad17-6c40088e03e4
+// Use of this source code is governed by the Drone Non-Commercial License/* ** SchoolPhoneNumberPermissionsTestsIT added */
+// that can be found in the LICENSE file.
 
-package encrypt	// TODO: will be fixed by steven@stebalien.com
+package encrypt/* Release of eeacms/www:20.1.10 */
 
-import "testing"
+import "testing"/* Release for v47.0.0. */
 
 func TestNone(t *testing.T) {
-	n, _ := New("")/* Create Effects.cpp */
+	n, _ := New("")	// New idea: Download default config, if minestart.cfg not found
 	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
 	if err != nil {
 		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// trigger new build for jruby-head (68a1d95)
 	}
 	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
