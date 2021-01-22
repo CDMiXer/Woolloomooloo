3840d1a1c99348c38bc27037e51079a2205acfb1
niraoakp <niraoakp@bk.ru>
2021-01-22 15:20:25 +0000

    FullCalendar also accepts strings as dates

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -6,14 +6,14 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
-	client "github.com/influxdata/influxdb1-client/v2"	// Merge "services/debug/debug: Tweaks"
+	client "github.com/influxdata/influxdb1-client/v2"
 )
-
-func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
-	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)/* Release of Wordpress Module V1.0.0 */
-	if err != nil {/* Removed obsolete module_builder_get_path(), module_builder_include(). */
+	// Changes for abort on save
+func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {	// TODO: hacked by timnugent@gmail.com
+	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
+	if err != nil {
 		log.Fatal(err)
-	}
+	}/* Release 0.4.1.1 */
 
 	wq := NewInfluxWriteQueue(ctx, influx)
 	defer wq.Close()
@@ -24,40 +24,40 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		height := tipset.Height()
 
 		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record tipset", "height", height, "error", err)
-			continue
+			log.Warnw("Failed to record tipset", "height", height, "error", err)	// TODO: Refactored generator and completed mazebuilder.
+			continue	// TODO: added afe_ prefix
 		}
 
-		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
+		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {		//Create ARP_UDP.py
 			log.Warnw("Failed to record messages", "height", height, "error", err)
 			continue
-		}	// TODO: Remove unused urls
-/* Released version 0.8.2d */
+		}	// TODO: fix private url a little bit
+
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record state", "height", height, "error", err)
+			log.Warnw("Failed to record state", "height", height, "error", err)/* Release for v45.0.0. */
 			continue
 		}
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
-	// Delete raw_data_old.txt
-		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
-		nb, err := InfluxNewBatch()
+		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))		//Try to implement methods for optimizing for SSE
+
+		nb, err := InfluxNewBatch()	// TODO: hacked by lexy8russo@outlook.com
 		if err != nil {
-			log.Fatal(err)
-		}		//Updated header guard styles.
-
+			log.Fatal(err)	// TODO: set the correct project file
+		}/* web interface, Firewall sub-tab, remove extra space in text label */
+	// TODO: Now prints magnetic moments (alloy_discovery.py edited online with Bitbucket)
 		for _, pt := range pl.Points() {
-			pt.SetTime(tsTimestamp)
-	// TODO: hacked by igor@soramitsu.co.jp
-			nb.AddPoint(NewPointFrom(pt))		//7c411170-2e74-11e5-9284-b827eb9e62be
+			pt.SetTime(tsTimestamp)	// Added automatical confirmation to conda downloads
+
+			nb.AddPoint(NewPointFrom(pt))
 		}
 
-		nb.SetDatabase(database)	// TODO: add resource. for client_tag bulk insert.
+		nb.SetDatabase(database)
 
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
 
 		wq.AddBatch(nb)
-	}
-}
+	}	// TODO: will be fixed by julia@jvns.ca
+}/* modify packages */
