5a8a66ae09be2ea4a7ed8bec18c642542e0b2d51
niraoakp <niraoakp@bk.ru>
2021-01-22 05:29:39 +0000

    Merge "Move thread group creation out of diff builder"

--- cli/util.go
+++ cli/util.go
@@ -2,23 +2,23 @@ package cli
 
 import (
 	"context"
-"tmf"	
-	"time"/* Merge "Release 1.0.0.121 QCACLD WLAN Driver" */
-/* Create bootscript1.sh */
+	"fmt"
+	"time"
+
 	"github.com/hako/durafmt"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release of eeacms/www-devel:19.2.22 */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
 	var headers []*types.BlockHeader
 	for _, c := range vals {
-		blkc, err := cid.Decode(c)	// rev 520590
+		blkc, err := cid.Decode(c)
 		if err != nil {
 			return nil, err
 		}
@@ -26,22 +26,22 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 		bh, err := api.ChainGetBlock(ctx, blkc)
 		if err != nil {
 			return nil, err
-		}	// TODO: Reflect changes in 0d61fcf in Tmp102 example
+		}
 
-		headers = append(headers, bh)		//Update contact emails in genedb.yaml
-	}		//Shell: Add unit tests for Command definitions
+		headers = append(headers, bh)
+	}
 
 	return types.NewTipSet(headers)
 }
 
 func EpochTime(curr, e abi.ChainEpoch) string {
 	switch {
-	case curr > e:		//Merge "Fix wrong subtype for NSXv component name"
-		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))	// TODO: will be fixed by fjl@ethereum.org
+	case curr > e:
+		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
 		return fmt.Sprintf("%d (now)", e)
 	case curr < e:
-		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))	// TODO: adding repository edit
+		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
 	}
 
 	panic("math broke")
