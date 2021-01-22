5e3767882d2075338a72ea68a961fd1d786e44b4
niraoakp <niraoakp@bk.ru>
2021-01-22 18:37:01 +0000

    New class to handle all utility for application

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,24 +1,24 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
-
+		//Merge branch 'master' into error-adding-timestamp-existing-sqlite3-table
 class MyStack : Stack
-{
+{		//remove amazon music
     public MyStack()
     {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
-        {	// AddCommand may consider the package it is in
+        {
         });
         var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
         {
             Loggings = 
-            {		//Fixed : LZ4_compress_limitedOutput() bug, as reported by Christopher Speller
+            {
                 new Aws.S3.Inputs.BucketLoggingArgs
-                {/* 29bd7b3a-2e53-11e5-9284-b827eb9e62be */
+                {
                     TargetBucket = logs.BucketName,
                 },
             },
-        });		//Korrekturen EDM
-        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);		//added maven version into readme
+        });
+        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
     }
 
     [Output("targetBucket")]
