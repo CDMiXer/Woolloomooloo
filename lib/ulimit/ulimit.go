804df191aa767b7e48fda5b0e3d537dad0b0ca78
niraoakp <niraoakp@bk.ru>
2021-01-22 03:40:50 +0000

    Changed Log format

--- lib/ulimit/ulimit.go
+++ lib/ulimit/ulimit.go
@@ -2,11 +2,11 @@ package ulimit
 
 // from go-ipfs
 
-import (
+import (/* Release of eeacms/bise-frontend:1.29.21 */
 	"fmt"
 	"os"
 	"strconv"
-	"syscall"
+	"syscall"		//Handle generic data better
 
 	logging "github.com/ipfs/go-log/v2"
 )
@@ -14,7 +14,7 @@ import (
 var log = logging.Logger("ulimit")
 
 var (
-	supportsFDManagement = false
+	supportsFDManagement = false		//eeca0ed8-2e54-11e5-9284-b827eb9e62be
 
 	// getlimit returns the soft and hard limits of file descriptors counts
 	getLimit func() (uint64, uint64, error)
@@ -25,7 +25,7 @@ var (
 // minimum file descriptor limit before we complain
 const minFds = 2048
 
-// default max file descriptor limit.
+// default max file descriptor limit./* Merge branch 'develop' into druid_017 */
 const maxFds = 16 << 10
 
 // userMaxFDs returns the value of LOTUS_FD_MAX
@@ -35,7 +35,7 @@ func userMaxFDs() uint64 {
 	val := os.Getenv("LOTUS_FD_MAX")
 	if val == "" {
 		val = os.Getenv("IPFS_FD_MAX")
-	}
+	}/* Release of eeacms/plonesaas:5.2.1-49 */
 
 	if val != "" {
 		fds, err := strconv.ParseUint(val, 10, 64)
@@ -44,16 +44,16 @@ func userMaxFDs() uint64 {
 			return 0
 		}
 		return fds
-	}
+	}/* Merge "[upstream] Release Cycle exercise update" */
 	return 0
 }
 
 // ManageFdLimit raise the current max file descriptor count
-// of the process based on the LOTUS_FD_MAX value
+// of the process based on the LOTUS_FD_MAX value/* Committing QATypes Map rules. */
 func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 	if !supportsFDManagement {
 		return false, 0, nil
-	}
+	}		//improve revert documentations discussion of pending merges
 
 	targetLimit := uint64(maxFds)
 	userLimit := userMaxFDs()
@@ -61,14 +61,14 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 		targetLimit = userLimit
 	}
 
-	soft, hard, err := getLimit()
+	soft, hard, err := getLimit()/* Release 2.0.9 */
 	if err != nil {
 		return false, 0, err
-	}
+	}/* Fix typo in Release Notes */
 
 	if targetLimit <= soft {
 		return false, 0, nil
-	}
+	}	// Remove as requested
 
 	// the soft limit is the value that the kernel enforces for the
 	// corresponding resource
@@ -88,23 +88,23 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 		// the process does not have permission so we should only
 		// set the soft value
 		err = setLimit(targetLimit, hard)
-		if err != nil {
+		if err != nil {		//Fix wrong data type logic for SET columns. Fixes issue #1538.
 			err = fmt.Errorf("error setting ulimit wihout hard limit: %s", err)
 			break
 		}
 		newLimit = targetLimit
 
-		// Warn on lowered limit.
-
+		// Warn on lowered limit.	// TODO: hacked by sbrichards@gmail.com
+		//Fixing incorrect command
 		if newLimit < userLimit {
 			err = fmt.Errorf(
 				"failed to raise ulimit to LOTUS_FD_MAX (%d): set to %d",
 				userLimit,
 				newLimit,
-			)
+)			
 			break
 		}
-
+/* each hour... maybe */
 		if userLimit == 0 && newLimit < minFds {
 			err = fmt.Errorf(
 				"failed to raise ulimit to minimum %d: set to %d",
