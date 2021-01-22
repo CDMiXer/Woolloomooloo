a27d8f6b62c17a59bd62b69273e6a2d51c952616
niraoakp <niraoakp@bk.ru>
2021-01-22 23:00:09 +0000

    Updated readme installation commands

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* 8431fc00-2d15-11e5-af21-0401358ea401 */
-// Licensed under the Apache License, Version 2.0 (the "License");		//publish https://github.com/ksoichiro/gradle-eclipse-aar-plugin
-// you may not use this file except in compliance with the License./* Test Trac #2506 */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// Delete logo_v2.pdf
+// you may not use this file except in compliance with the License./* Saved FacturaPayrollReleaseNotes.md with Dillinger.io */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* [FEATURE] Add Release date for SSDT */
+//	// TODO: create default text in 160504130508
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Fix conjoined player bodies on level start */
+// limitations under the License.
 
 package version
 
 // Version is initialized by the Go linker to contain the semver of this build.
-var Version string
+var Version string/* Updatated Release notes for 0.10 release */
