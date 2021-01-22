1e1fbc88e73f49290b14bcf52055de68b59961b8
niraoakp <niraoakp@bk.ru>
2021-01-22 05:27:19 +0000

    [doc] add some inline explanation of t2

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -2,76 +2,76 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos/* make 'setPredicateValue' support none parameters */
-
+package repos
+		//remove temporary solution with plain canvas menu
 import (
 	"encoding/json"
 	"io"
-	"net/http"/* [MT05109] fixed amstrad plus out of line drawing [Oliver Stöneberg] */
-	"net/http/httptest"
+	"net/http"
+	"net/http/httptest"	// TODO: will be fixed by boringland@protonmail.ch
 	"testing"
-	// TODO: correct deployment command docs
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
+	// don't test autotune
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/golang/mock/gomock"		//license Delete
+	"github.com/google/go-cmp/cmp"/* Release v2.0.1 */
 )
 
 func TestDisable(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-	// Update SNAPSHOT to 2.0.0.M9
+	defer controller.Finish()/* Merge "Convergence: Make SyncPoint.update_input_data actually atomic." */
+
 	repo := &core.Repository{
 		ID:        1,
-		Namespace: "octocat",/* a little comment */
-		Name:      "hello-world",
-		Slug:      "octocat/hello-world",
+		Namespace: "octocat",
+		Name:      "hello-world",	// TODO: hacked by arajasek94@gmail.com
+		Slug:      "octocat/hello-world",		//Updating build-info/dotnet/corefx/release/3.0 for preview5.19224.8
 		Active:    true,
 	}
-
-	repos := mock.NewMockRepositoryStore(controller)	// fix AutoComplete display value
+	// TODO: messed up?
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)
-	// Fixed the &only= in the script url.
-	// a failed webhook should result in a warning message in the
+
+	// a failed webhook should result in a warning message in the		//705f2516-2e49-11e5-9284-b827eb9e62be
 	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
-/* Update README with breaking change notice */
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
-	router := chi.NewRouter()/* 95d20cbe-2e3f-11e5-9284-b827eb9e62be */
+	w := httptest.NewRecorder()	// TODO: NEW Default MODx templates
+	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+
+	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
 	router.ServeHTTP(w, r)
-		//Move some braces around
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}		//Make sure service info commands use ServiceFind to allow finding by id and name
 
-	if got, want := repo.Active, false; got != want {
-		t.Errorf("Want repository activate %v, got %v", want, got)
+	if got, want := w.Code, 200; want != got {/* Typo asssumes - assumes */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
+/* Also update Lamy 2000's URL. */
+	if got, want := repo.Active, false; got != want {/* email send retry (to be improved). */
+		t.Errorf("Want repository activate %v, got %v", want, got)
+	}		//Create NStats
 
 	got, want := new(core.Repository), repo
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}/* SAE-332 Release 1.0.1 */
-	// TODO: 14322730-2e48-11e5-9284-b827eb9e62be
+}
+
 func TestDisable_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)		//Threshold changes and additional statistics values
+	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)/* Create hashing */
+	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))
