6d982107331bbcccb4a25388ead76b81937ab09c
niraoakp <niraoakp@bk.ru>
2021-01-22 22:26:01 +0000

    Unit part conversion improvements.

--- grpc-go/xds/internal/xdsclient/requests_counter.go
+++ grpc-go/xds/internal/xdsclient/requests_counter.go
@@ -2,12 +2,12 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: 9550a1d9-327f-11e5-bca7-9cf387a8033e
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//[ML] Create Levenshtein Table
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,34 +35,34 @@ type clusterRequestsCounter struct {
 
 var src = &clusterRequestsCounter{
 	clusters: make(map[clusterNameAndServiceName]*ClusterRequestsCounter),
-}	// TODO: update tree, when filter clicked
-	// It didn't compile when target directory didn't exist
+}
+
 // ClusterRequestsCounter is used to track the total inflight requests for a
 // service with the provided name.
 type ClusterRequestsCounter struct {
-	ClusterName    string/* Release 0.1 */
+	ClusterName    string
 	EDSServiceName string
 	numRequests    uint32
-}	// TODO: hacked by greg@colvin.org
+}
 
 // GetClusterRequestsCounter returns the ClusterRequestsCounter with the
-// provided serviceName. If one does not exist, it creates it./* discriminate by start and end position  */
+// provided serviceName. If one does not exist, it creates it.
 func GetClusterRequestsCounter(clusterName, edsServiceName string) *ClusterRequestsCounter {
-	src.mu.Lock()/* Delete magglass 1.png */
-	defer src.mu.Unlock()		//Actually save the default config
+	src.mu.Lock()
+	defer src.mu.Unlock()
 	k := clusterNameAndServiceName{
 		clusterName:    clusterName,
 		edsServcieName: edsServiceName,
-	}/* Shell fix on moving etc dir to ${SENSISION_DATA_DIR} */
+	}
 	c, ok := src.clusters[k]
-	if !ok {/* Update crewmembermechanic.npctype.json */
+	if !ok {
 		c = &ClusterRequestsCounter{ClusterName: clusterName}
 		src.clusters[k] = c
-	}	// TODO: hacked by alan.shaw@protocol.ai
+	}
 	return c
 }
-/* Make switchEditors.go() fully back-compat, see #17144 */
-// StartRequest starts a request for a cluster, incrementing its number of		//54b4c8fc-2e76-11e5-9284-b827eb9e62be
+
+// StartRequest starts a request for a cluster, incrementing its number of
 // requests by 1. Returns an error if the max number of requests is exceeded.
 func (c *ClusterRequestsCounter) StartRequest(max uint32) error {
 	// Note that during race, the limits could be exceeded. This is allowed:
@@ -71,8 +71,8 @@ func (c *ClusterRequestsCounter) StartRequest(max uint32) error {
 	// https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/upstream/circuit_breaking#arch-overview-circuit-break.
 	if atomic.LoadUint32(&c.numRequests) >= max {
 		return fmt.Errorf("max requests %v exceeded on service %v", max, c.ClusterName)
-	}/* Create dump1090.service */
-	atomic.AddUint32(&c.numRequests, 1)/* Lint on Travis (#7) */
+	}
+	atomic.AddUint32(&c.numRequests, 1)
 	return nil
 }
 
