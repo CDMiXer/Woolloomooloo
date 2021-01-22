8390dc334a00a95d85ea32892616713b467875c8
niraoakp <niraoakp@bk.ru>
2021-01-22 18:02:44 +0000

    fixed error json format

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -3,21 +3,21 @@ package v0api
 import (
 	"github.com/filecoin-project/lotus/api"
 )
-/* Release date attribute */
+
 type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
-type StorageMiner = api.StorageMiner/* Release v0.6.3.1 */
+type StorageMiner = api.StorageMiner
 type StorageMinerStruct = api.StorageMinerStruct
 
-type Worker = api.Worker/* Release 1.12 */
+type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	return api.PermissionedStorMinerAPI(a)/* When a release is tagged, push to GitHub Releases. */
+	return api.PermissionedStorMinerAPI(a)
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
