75b097d7456b704d08e44073956e853126b4d15e
niraoakp <niraoakp@bk.ru>
2021-01-22 14:37:28 +0000

    Delete CMSC250_22846_LinuxStudentsAccountsFall16.xlsx

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//Merge "msm: mdss: swap flags for LP1 and LP2 modes"
+import { Resource } from "./resource";
 
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
-// Dependent depends on Base with state 99.
-const b = new Resource("dependent", { state: a.state });		//Merge "Do not access connection from cleared session"
+// Dependent depends on Base with state 99./* Release procedure */
+const b = new Resource("dependent", { state: a.state });
