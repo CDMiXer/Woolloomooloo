fcec1c738ab36281011a93abd8a8e6c2253f3555
niraoakp <niraoakp@bk.ru>
2021-01-22 07:05:02 +0000

    64cdee7e-2e6e-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/create.go
+++ handler/api/repos/builds/create.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: glade/griffith.glade
-// Licensed under the Apache License, Version 2.0 (the "License");/* Extract patch process actions from PatchReleaseController; */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by 13860583249@yeah.net
-//      http://www.apache.org/licenses/LICENSE-2.0	// Correct typo in XML datatypes
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by alex.gaynor@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Add probes to the deployment template. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
-		//a hakyll-based website, build script updates
+
 import (
 	"net/http"
-	// TODO: Add bugs description to docs
-	"github.com/drone/drone/core"/* Automatic changelog generation for PR #2949 [ci skip] */
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/go-scm/scm"	// TODO: f379077e-2e70-11e5-9284-b827eb9e62be
-/* fix rendering in grid */
+	"github.com/drone/go-scm/scm"
+
 	"github.com/go-chi/chi"
 )
 
 // HandleCreate returns an http.HandlerFunc that processes http
-// requests to create a build for the specified commit.		//Removed the access transformers. 
+// requests to create a build for the specified commit.
 func HandleCreate(
-	users core.UserStore,	// TODO: will be fixed by josharian@gmail.com
-	repos core.RepositoryStore,/* Release naming update to 5.1.5 */
+	users core.UserStore,
+	repos core.RepositoryStore,
 	commits core.CommitService,
-	triggerer core.Triggerer,/* build.xml now copies web service common library at build time */
+	triggerer core.Triggerer,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			ctx       = r.Context()
-			namespace = chi.URLParam(r, "owner")		//Slight renaming
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			sha       = r.FormValue("commit")
 			branch    = r.FormValue("branch")
