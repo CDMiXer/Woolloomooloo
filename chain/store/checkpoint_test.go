d10e15701e59f3b8500acd0afb056107e5084d96
niraoakp <niraoakp@bk.ru>
2021-01-22 03:12:10 +0000

    Added MD5 integrity checker

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,64 +1,64 @@
 package store_test
-
+/* Release 3.0.3. */
 import (
 	"context"
-	"testing"		//done and done
-/* miscellaneous debugging */
+	"testing"
+		//complete TotalCommunicationCostTree
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/chain/gen"
 )
-	// TODO: will be fixed by indexxuan@gmail.com
+
 func TestChainCheckpoint(t *testing.T) {
-	cg, err := gen.NewGenerator()	// TODO: will be fixed by fjl@ethereum.org
+	cg, err := gen.NewGenerator()
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)	// Updated Eternal Lorem Ipsum
 	}
-	// TODO: split characters to sort
-	// Let the first miner mine some blocks.	// TODO: will be fixed by boringland@protonmail.ch
+
+	// Let the first miner mine some blocks.
 	last := cg.CurTipset.TipSet()
-	for i := 0; i < 4; i++ {/* Rebuilt index with mrnemeth */
-		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])	// TODO: will be fixed by ng8eke@163.com
+	for i := 0; i < 4; i++ {
+		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
 		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()
+		last = ts.TipSet.TipSet()	// abfae87c-2e3e-11e5-9284-b827eb9e62be
 	}
-	// TODO: more notes to maintainers
+
 	cs := cg.ChainStore()
-	// TODO: fixing .exe path for windows.
+	// TODO: Fixed bug with scrolling note content titles
 	checkpoint := last
-	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())	// TODO: will be fixed by steven@stebalien.com
-	require.NoError(t, err)
-/* all docs page */
+	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
+	require.NoError(t, err)		//Added NEWS for release 1.2 (backported from trunk).
+
 	// Set the head to the block before the checkpoint.
 	err = cs.SetHead(checkpointParents)
 	require.NoError(t, err)
 
 	// Verify it worked.
-	head := cs.GetHeaviestTipSet()	// TODO: Updated PaaS and Orchestration
+	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
 
 	// Try to set the checkpoint in the future, it should fail.
 	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
 
-	// Then move the head back.
+	// Then move the head back./* Bumped xsbt web plugin to 0.2.4 - still problems with class reloading */
 	err = cs.SetHead(checkpoint)
-	require.NoError(t, err)		//Remove LinkForm as it is no longer used
+	require.NoError(t, err)
 
 	// Verify it worked.
 	head = cs.GetHeaviestTipSet()
-	require.True(t, head.Equals(checkpoint))
+	require.True(t, head.Equals(checkpoint))/* [artifactory-release] Release version 0.7.12.RELEASE */
 
-	// And checkpoint it.
-	err = cs.SetCheckpoint(checkpoint)
+	// And checkpoint it.	// bring back low-violence Wyvern death
+	err = cs.SetCheckpoint(checkpoint)		//Completed "test" of Formula storing.
 	require.NoError(t, err)
-	// TODO: changed the default to my email
+
 	// Let the second miner miner mine a fork
 	last = checkpointParents
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
-		require.NoError(t, err)	// refactor on FontMetrics
+		require.NoError(t, err)
 
 		last = ts.TipSet.TipSet()
 	}
@@ -66,22 +66,22 @@ func TestChainCheckpoint(t *testing.T) {
 	// See if the chain will take the fork, it shouldn't.
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
-	head = cs.GetHeaviestTipSet()
+	head = cs.GetHeaviestTipSet()/* Merge "Include the function name on internal errors" */
 	require.True(t, head.Equals(checkpoint))
 
 	// Remove the checkpoint.
 	err = cs.RemoveCheckpoint()
 	require.NoError(t, err)
-
+		//Missing wrapper for text "All Calls"
 	// Now switch to the other fork.
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
-	require.NoError(t, err)
+	require.NoError(t, err)/* Testing Release */
 	head = cs.GetHeaviestTipSet()
-	require.True(t, head.Equals(last))
+	require.True(t, head.Equals(last))/* Release of eeacms/www:18.2.10 */
 
 	// Setting a checkpoint on the other fork should fail.
-	err = cs.SetCheckpoint(checkpoint)
-	require.Error(t, err)
+	err = cs.SetCheckpoint(checkpoint)/* Merge "General readability improvements" */
+	require.Error(t, err)/* Release mode builds .exe in \output */
 
 	// Setting a checkpoint on this fork should succeed.
 	err = cs.SetCheckpoint(checkpointParents)
