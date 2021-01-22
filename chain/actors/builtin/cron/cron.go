9cef2a48eafab1e24ac13b49520c4a640f748c0c
niraoakp <niraoakp@bk.ru>
2021-01-22 15:03:38 +0000

    Ignorando arquivo .project

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
-package cron
-
-import (
+package cron/* Create build path for coveralls */
+/* Removed lancet as a recommended dependency */
+import (/* Release 1.08 all views are resized */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)/* Add Feature Alerts and Data Releases to TOC */
+)
 
-var (
+var (		//Merge branch 'master' into scottmo/fixProcfile
 	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
 )
