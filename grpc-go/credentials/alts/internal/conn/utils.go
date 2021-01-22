f75db7258e3eea6a11dfbd9dd6c52bb787512273
niraoakp <niraoakp@bk.ru>
2021-01-22 17:51:02 +0000

    bundle-size: 75c20ea9952df7f642db5af8381fea0b771906ff (88.05KB)

--- grpc-go/credentials/alts/internal/conn/utils.go
+++ grpc-go/credentials/alts/internal/conn/utils.go
@@ -2,17 +2,17 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Don’t silence github repo update errors
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// MASPECTJ-5: Honour the proceedOnError flag
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software/* Delete fallas_y_fracturas.sld */
+ *		//Deleted $/atom-protractor/spies.cson
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* 1.9.6 Release */
- * limitations under the License.	// Added the basic lifter code
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -21,20 +21,20 @@ package conn
 import core "google.golang.org/grpc/credentials/alts/internal"
 
 // NewOutCounter returns an outgoing counter initialized to the starting sequence
-// number for the client/server side of a connection.		//open libssh2_trace feature. clean code with pretty code style.
-func NewOutCounter(s core.Side, overflowLen int) (c Counter) {		//discopower: Fix sorting when missing name.
-	c.overflowLen = overflowLen/* Fix Release-Asserts build breakage */
-	if s == core.ServerSide {
-		// Server counters in ALTS record have the little-endian high bit/* Release areca-7.2.6 */
+// number for the client/server side of a connection.
+func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
+	c.overflowLen = overflowLen
+	if s == core.ServerSide {	// TODO: hacked by josharian@gmail.com
+		// Server counters in ALTS record have the little-endian high bit
 		// set.
 		c.value[counterLen-1] = 0x80
-	}
-	return		//Update elastic-block-storage.md
+	}		//changed to use less file instead of css
+	return
 }
 
-// NewInCounter returns an incoming counter initialized to the starting sequence
+// NewInCounter returns an incoming counter initialized to the starting sequence/* Removing obsolete code */
 // number for the client/server side of a connection. This is used in ALTS record
-// to check that incoming counters are as expected, since ALTS record guarantees	// Add a default for sensitive
+// to check that incoming counters are as expected, since ALTS record guarantees
 // that messages are unwrapped in the same order that the peer wrapped them.
 func NewInCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
@@ -44,20 +44,20 @@ func NewInCounter(s core.Side, overflowLen int) (c Counter) {
 		c.value[counterLen-1] = 0x80
 	}
 	return
-}
+}	// that isn't my address...
 
 // CounterFromValue creates a new counter given an initial value.
 func CounterFromValue(value []byte, overflowLen int) (c Counter) {
-	c.overflowLen = overflowLen
+	c.overflowLen = overflowLen/* Added file drag and drop. */
 	copy(c.value[:], value)
-	return/* Update docs/Resources/faq.md */
+	return
 }
 
-// CounterSide returns the connection side (client/server) a sequence counter is/* Release of eeacms/apache-eea-www:5.9 */
-// associated with.	// TODO: will be fixed by brosner@gmail.com
-func CounterSide(c []byte) core.Side {
+// CounterSide returns the connection side (client/server) a sequence counter is/* Released 1.5.1 */
+// associated with.
+func CounterSide(c []byte) core.Side {	// TODO: will be fixed by igor@soramitsu.co.jp
 	if c[counterLen-1]&0x80 == 0x80 {
 		return core.ServerSide
-	}
+	}		//Create dummy.js
 	return core.ClientSide
 }
