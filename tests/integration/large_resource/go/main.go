440b32f8120764c6fea10447473dda06e6b89ac1
niraoakp <niraoakp@bk.ru>
2021-01-22 08:15:44 +0000

    Added Flattr button to README

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
-	// TODO: Fixing unescaped strings in readme
-import (
+
+import (/* finsh to comment interface (normaly...) */
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+/* add mail bean  */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create and export a very long string (>4mb)
+		// Create and export a very long string (>4mb)		//Update lint-staged to 7.0.2
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
 		return nil
-	})/* Bind all methods */
+	})
 }
