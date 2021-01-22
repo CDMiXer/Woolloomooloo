5b993dc537902a6a28df6a7eee16275b6c7ef64e
niraoakp <niraoakp@bk.ru>
2021-01-22 20:04:41 +0000

    Correction encodage BCRYPT + intégration de "clef".

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-	// TODO: will be fixed by hello@brooklynzelenka.com
-package ints/* mise à jour commentaires pour documentation doxy */
 
-import (
+package ints
+
+import (/* Create helpURLs.txt */
 	"path/filepath"
 	"testing"
-
+/* Release shall be 0.1.0 */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-func TestNodejsTransformations(t *testing.T) {		//Create TodoItem.js
+func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
-		d := filepath.Join("nodejs", dir)
-		t.Run(d, func(t *testing.T) {/* PDFBOX-2340: rendering */
+		d := filepath.Join("nodejs", dir)/* d179a186-2e72-11e5-9284-b827eb9e62be */
+		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
 				Quick:                  true,
 				ExtraRuntimeValidation: Validator("nodejs"),
 			})
-		})
+		})		//update of notes
 	}
 }
