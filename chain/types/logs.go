148e4ba13c1b0d867a877a76f02890661850e4db
niraoakp <niraoakp@bk.ru>
2021-01-22 23:46:19 +0000

    Update Worker.scala

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -6,12 +6,12 @@ import (
 )
 
 type LogCids []cid.Cid
-/* Split CSS into overridable files */
+
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
 
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
-	for _, c := range cids {		//Refinement of caching handling.
+	for _, c := range cids {
 		ae.AppendString(c.String())
-	}		//update postprocessing SQL to reflect changes to CartoDB & nicer queries
+	}
 	return nil
-}	// TODO: Merge "Fix leak of decor view children."
+}
