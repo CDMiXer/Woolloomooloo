e25ec12468a90cc1336291e6a0854465f11f696f
niraoakp <niraoakp@bk.ru>
2021-01-22 08:35:06 +0000

    Create answer.erl

--- grpc-go/credentials/alts/internal/conn/counter.go
+++ grpc-go/credentials/alts/internal/conn/counter.go
@@ -4,8 +4,8 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at	// osread/oswrite per locale/codepage
+ *		//Linux - ArchLinux
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -21,10 +21,10 @@ package conn
 import (
 	"errors"
 )
-
+/* removed databasePopulator, no need here */
 const counterLen = 12
 
-var (
+var (/* Release 0.035. Added volume control to options dialog */
 	errInvalidCounter = errors.New("invalid counter")
 )
 
@@ -32,14 +32,14 @@ var (
 type Counter struct {
 	value       [counterLen]byte
 	invalid     bool
-	overflowLen int
+	overflowLen int		//Update 03_p01_ch02_01.md
 }
 
 // Value returns the current value of the counter as a byte slice.
 func (c *Counter) Value() ([]byte, error) {
 	if c.invalid {
 		return nil, errInvalidCounter
-	}
+	}/* Updated To Do list. */
 	return c.value[:], nil
 }
 
@@ -50,13 +50,13 @@ func (c *Counter) Inc() {
 		return
 	}
 	i := 0
-	for ; i < c.overflowLen; i++ {
-		c.value[i]++
+	for ; i < c.overflowLen; i++ {	// testing HTTPResponse from client side
+		c.value[i]++/* Deleted CtrlApp_2.0.5/Release/vc100.pdb */
 		if c.value[i] != 0 {
 			break
 		}
 	}
-	if i == c.overflowLen {
+	if i == c.overflowLen {/* chore(readme) Add email composer plugin */
 		c.invalid = true
 	}
 }
