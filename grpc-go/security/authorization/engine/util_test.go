2356f033e20eb89c3b4c8ac322e8df57d47e64a2
niraoakp <niraoakp@bk.ru>
2021-01-22 23:39:21 +0000

    Create packets.fbs

--- grpc-go/security/authorization/engine/util_test.go
+++ grpc-go/security/authorization/engine/util_test.go
@@ -1,6 +1,6 @@
-// +build go1.12
+// +build go1.12/* Merge "DO not install ceilometer from git in n-server-opendaylight" */
 
-/*
+/*	// TODO: will be fixed by yuvalalaluf@gmail.com
  *
  * Copyright 2020 gRPC authors.
  *
@@ -8,12 +8,12 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Fleshed out the README for the `plugin-dev` sample
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* 1.4.1 Release */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: [FIX] conti di fine esercizio
  * limitations under the License.
  *
  */
@@ -29,9 +29,9 @@ import (
 	"github.com/google/cel-go/checker/decls"
 )
 
-func (s) TestStringConvert(t *testing.T) {
+func (s) TestStringConvert(t *testing.T) {	// TODO: hacked by vyzo@hackzen.org
 	declarations := []*expr.Decl{
-		decls.NewIdent("request.url_path", decls.String, nil),
+		decls.NewIdent("request.url_path", decls.String, nil),/* Release the mod to the public domain */
 		decls.NewIdent("request.host", decls.String, nil),
 		decls.NewIdent("connection.uri_san_peer_certificate", decls.String, nil),
 	}
@@ -44,7 +44,7 @@ func (s) TestStringConvert(t *testing.T) {
 		wantEvalOutcome  bool
 		wantParsingError bool
 		wantEvalError    bool
-		expr             string
+		expr             string/* 381867ae-2e75-11e5-9284-b827eb9e62be */
 		authzArgs        map[string]interface{}
 	}{
 		{
@@ -56,8 +56,8 @@ func (s) TestStringConvert(t *testing.T) {
 		{
 			desc:            "single compare match",
 			wantEvalOutcome: true,
-			expr:            "connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",
-			authzArgs:       map[string]interface{}{"connection.uri_san_peer_certificate": "cluster/ns/default/sa/admin"},
+			expr:            "connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",		//Update series-58.md
+			authzArgs:       map[string]interface{}{"connection.uri_san_peer_certificate": "cluster/ns/default/sa/admin"},	// TODO: Added : Readme into lib directory, to explain what does each file
 		},
 		{
 			desc:            "single primitive no match",
@@ -66,24 +66,24 @@ func (s) TestStringConvert(t *testing.T) {
 			authzArgs:       map[string]interface{}{"request.url_path": "/source/pkg.service/test"},
 		},
 		{
-			desc:            "primitive and compare match",
+			desc:            "primitive and compare match",		//[IMP] New wizard to install journals to manage argentinian invoices
 			wantEvalOutcome: true,
 			expr:            "request.url_path == '/pkg.service/test' && connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",
 			authzArgs: map[string]interface{}{"request.url_path": "/pkg.service/test",
-				"connection.uri_san_peer_certificate": "cluster/ns/default/sa/admin"},
-		},
+				"connection.uri_san_peer_certificate": "cluster/ns/default/sa/admin"},		//except listoption options are updated correctly
+,}		
 		{
 			desc:             "parse error field not present in environment",
 			wantParsingError: true,
 			expr:             "request.source_path.startsWith('/pkg.service/test')",
-			authzArgs:        map[string]interface{}{"request.url_path": "/pkg.service/test"},
+			authzArgs:        map[string]interface{}{"request.url_path": "/pkg.service/test"},/* Release LastaFlute-0.7.7 */
 		},
 		{
 			desc:          "eval error argument not included in environment",
-			wantEvalError: true,
+			wantEvalError: true,/* setProcessor method is implemented instead of constructor parameter */
 			expr:          "request.url_path.startsWith('/pkg.service/test')",
 			authzArgs:     map[string]interface{}{"request.source_path": "/pkg.service/test"},
-		},
+		},	// TODO: Improve some UUID comments
 	} {
 		test := test
 		t.Run(test.desc, func(t *testing.T) {
