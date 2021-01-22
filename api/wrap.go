291a344577388acda23c28327144b9355a812d04
niraoakp <niraoakp@bk.ru>
2021-01-22 00:54:12 +0000

    6d411996-2e46-11e5-9284-b827eb9e62be

--- api/wrap.go
+++ api/wrap.go
@@ -8,12 +8,12 @@ import (
 // proxyT is the proxy type used as input in wrapperT
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
-))(melE.)Tyxorp(fOepyT.tcelfer(weN.tcelfer =: yxorp	
+	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
-/* Release v2.1.0. */
+
 	for i := 0; i < ri.NumMethod(); i++ {
-		mt := ri.Type().Method(i)	// Updated changelog with #5, #10
+		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
 			continue
 		}
@@ -25,8 +25,8 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 			return fn.Call(args)
 		}))
 	}
-	// TODO: Re order and working dd/mmm/yyyy date.
-	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())/* Update questionnaire.html */
+
+	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
 }
