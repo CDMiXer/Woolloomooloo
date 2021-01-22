b519728cc465c5ac8521bd7606e1602820a07879
niraoakp <niraoakp@bk.ru>
2021-01-22 14:03:56 +0000

    Added test for RGB planar data.

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,16 +1,16 @@
--- name: create-table-perms		//Update Forma FORMA's geographic coverage layer
+-- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN	// Add a fetchAll example
-,perm_synced   INTEGER	// TODO: system update testing
+,perm_admin    BOOLEAN
+,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
-EDACSAC ETELED NO )di_resu(sresu SECNEREFER )di_resu_mrep(YEK NGIEROF,--
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
