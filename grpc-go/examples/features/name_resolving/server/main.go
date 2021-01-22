1510ea2a1efd0acd342ff1253c07e29142e1c8cf
niraoakp <niraoakp@bk.ru>
2021-01-22 09:01:47 +0000

    reduce opacity of inactive nodes in debugger

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -1,26 +1,26 @@
-/*		//Merge "Provide locking around NPIV wwpns method"
+/*
  *
- * Copyright 2018 gRPC authors./* Release 1.9.0 */
+ * Copyright 2018 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge "Add extension_manager and support for extensions in linuxbridge agent" */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// minor update of file name
- * See the License for the specific language governing permissions and		//Tagged Iteration M06 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//gradle wrapper init. renamed gradlew script to program name
+ */
 
 // Binary server is an example server.
 package main
 
-import (	// TODO: hacked by steven@stebalien.com
-	"context"/* Release 0.18.0. Update to new configuration file format. */
+import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -32,11 +32,11 @@ import (	// TODO: hacked by steven@stebalien.com
 
 const addr = "localhost:50051"
 
-type ecServer struct {		//ca494b3c-2e45-11e5-9284-b827eb9e62be
+type ecServer struct {
 	pb.UnimplementedEchoServer
 	addr string
-}	// Merge 55326d28713fc9598f451e39213b3ba3cbd98d8b
-		//GIS-View and GIS-Graph-View removed
+}
+
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
@@ -51,5 +51,5 @@ func main() {
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}	// Присвоена версия 0.2.0
+	}
 }
