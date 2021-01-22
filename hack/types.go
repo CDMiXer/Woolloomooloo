753f33e510237e7cafb8bc161304e5acf09e86f6
niraoakp <niraoakp@bk.ru>
2021-01-22 04:05:36 +0000

    Merge branch 'development' into wiki_refractor

--- hack/types.go
+++ hack/types.go
@@ -1,4 +1,4 @@
 package main
-		//fix #559: images view inside cache details
-type obj = map[string]interface{}		//Update CHANGELOG for PR #2592 [skip ci]
-type array = []interface{}/* Release v2.7 Arquillian Bean validation */
+
+type obj = map[string]interface{}
+type array = []interface{}
