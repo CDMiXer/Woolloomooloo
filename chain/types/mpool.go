573e2283e33ce7b1ae20a689cde4f59288839eb7
niraoakp <niraoakp@bk.ru>
2021-01-22 04:56:30 +0000

    Notes on reviewing for the stable branch

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,22 +1,22 @@
 package types
 
-import (
-	"time"
+import (		//Fixed issue #630.
+	"time"/* Delete examples.ch */
 
 	"github.com/filecoin-project/go-address"
-)
-
-type MpoolConfig struct {
-	PriorityAddrs          []address.Address
+)/* Add New Lessons */
+	// [git] ignore generated assets.
+type MpoolConfig struct {/* [Release] Bumped to version 0.0.2 */
+	PriorityAddrs          []address.Address/* Merge branch 'master' into refactor-mqnode-to-es6-class */
 	SizeLimitHigh          int
-	SizeLimitLow           int
+	SizeLimitLow           int	// TODO: hacked by steven@stebalien.com
 	ReplaceByFeeRatio      float64
 	PruneCooldown          time.Duration
-	GasLimitOverestimation float64
+	GasLimitOverestimation float64	// Merge "Fixup comment blocks"
 }
 
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
-	*r = *mc
+	*r = *mc	// TODO: will be fixed by zodiacon@live.com
 	return r
 }
