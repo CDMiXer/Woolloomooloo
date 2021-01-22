63932073d891085aa463d4f28f1214fac52f41cc
niraoakp <niraoakp@bk.ru>
2021-01-22 21:11:46 +0000

    add more entries; add :fn as a verb valency category

--- miner/testminer.go
+++ miner/testminer.go
@@ -1,8 +1,8 @@
 package miner
 
-import (
+import (/* Release 1.0.0-beta-3 */
 	"context"
-
+/* Added IR shutter codes for Nikon,Pentax,Olympus. */
 	lru "github.com/hashicorp/golang-lru"
 	ds "github.com/ipfs/go-datastore"
 
@@ -10,33 +10,33 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api/v1api"
-	"github.com/filecoin-project/lotus/chain/gen"
+	"github.com/filecoin-project/lotus/chain/gen"	// TODO:  Changes to code to make it mergeable
 	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
-	"github.com/filecoin-project/lotus/journal"
-)
+	"github.com/filecoin-project/lotus/journal"/* 732d95b0-4b19-11e5-846d-6c40088e03e4 */
+)	// TODO: will be fixed by cory@protocol.ai
 
 type MineReq struct {
-	InjectNulls abi.ChainEpoch
+	InjectNulls abi.ChainEpoch/* Release version [10.7.0] - alfter build */
 	Done        func(bool, abi.ChainEpoch, error)
 }
 
 func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
 	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
-		arc, err := lru.NewARC(10000)
+		arc, err := lru.NewARC(10000)/* Merge branch 'main' into constantref */
 		if err != nil {
 			panic(err)
 		}
 
 		m := &Miner{
 			api:               api,
-			waitFunc:          chanWaiter(nextCh),
+			waitFunc:          chanWaiter(nextCh),	// TODO: ColladaLoader: Extra checks for effect.surface.
 			epp:               epp,
 			minedBlockHeights: arc,
 			address:           addr,
 			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
 		}
-
+	// TODO: Setup done
 		if err := m.Start(context.TODO()); err != nil {
 			panic(err)
 		}
@@ -46,7 +46,7 @@ func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNo
 
 func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
-		select {
+		select {/* Release-Upgrade */
 		case <-ctx.Done():
 			return nil, 0, ctx.Err()
 		case req := <-next:
