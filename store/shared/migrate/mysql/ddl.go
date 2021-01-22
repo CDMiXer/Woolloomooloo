adddabc6773d02f21d5e97081029f4a751a03c67
niraoakp <niraoakp@bk.ru>
2021-01-22 20:43:21 +0000

    Help dialog: Docu for JSON

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 08ab80b8-2e49-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release of eeacms/eprtr-frontend:20.04.02-dev1 */
+
 // +build !oss
 
 package mysql
