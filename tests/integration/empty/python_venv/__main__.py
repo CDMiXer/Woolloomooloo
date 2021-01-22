8afc7f93c5f4c409cb48d10274be8b2c7f1b2289
niraoakp <niraoakp@bk.ru>
2021-01-22 14:18:05 +0000

    Update recruit page

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//fixes #4 - Added persistant configuration volumes
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None	// TODO: will be fixed by zaq1tomo@gmail.com
+    return None
 
 if __name__ == "__main__":
     main()
