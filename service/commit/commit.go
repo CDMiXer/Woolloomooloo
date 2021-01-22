e396356ef6aaad6e0686ba1044df470df9df96e5
niraoakp <niraoakp@bk.ru>
2021-01-22 12:29:48 +0000

    Events Updated

--- service/commit/commit.go
+++ service/commit/commit.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Release 39 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//update cell to use common lib method
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* @Release [io7m-jcanephora-0.9.18] */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by martin2cai@hotmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Check currents status when connect with a player..
-// See the License for the specific language governing permissions and	// TODO: will be fixed by fjl@ethereum.org
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package commit
@@ -19,22 +19,22 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
-		//updated example url
+
 // New returns a new CommitServiceFactory.
 func New(client *scm.Client, renew core.Renewer) core.CommitService {
 	return &service{
-		client: client,/* add request image bean */
-		renew:  renew,		//Block overhaul to accommodate schema changes in ACCOUNT_USAGE
+		client: client,
+		renew:  renew,
 	}
 }
 
-type service struct {	// TODO: hacked by aeongrp@outlook.com
-	renew  core.Renewer	// TODO: hacked by indexxuan@gmail.com
+type service struct {
+	renew  core.Renewer
 	client *scm.Client
 }
 
 func (s *service) Find(ctx context.Context, user *core.User, repo, sha string) (*core.Commit, error) {
-	err := s.renew.Renew(ctx, user, false)		//Point build badge at actual Travis repo
+	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return nil, err
 	}
@@ -48,8 +48,8 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, sha string) (
 	}
 	return &core.Commit{
 		Sha:     commit.Sha,
-		Message: commit.Message,/* Release 0.54 */
-		Link:    commit.Link,		//solved error when ipAddresses is null
+		Message: commit.Message,
+		Link:    commit.Link,
 		Author: &core.Committer{
 			Name:   commit.Author.Name,
 			Email:  commit.Author.Email,
@@ -57,8 +57,8 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, sha string) (
 			Login:  commit.Author.Login,
 			Avatar: commit.Author.Avatar,
 		},
-		Committer: &core.Committer{/* Release Notes for v02-04-01 */
-			Name:   commit.Committer.Name,	// TODO: will be fixed by aeongrp@outlook.com
+		Committer: &core.Committer{
+			Name:   commit.Committer.Name,
 			Email:  commit.Committer.Email,
 			Date:   commit.Committer.Date.Unix(),
 			Login:  commit.Committer.Login,
