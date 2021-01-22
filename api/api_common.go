a4c3419da6d03e221dd4ea0384fed641e11f20c3
niraoakp <niraoakp@bk.ru>
2021-01-22 16:40:22 +0000

    export urlencode

--- api/api_common.go
+++ api/api_common.go
@@ -1,20 +1,20 @@
 package api
 
-import (
+import (		//remove use of deprecated APIs in tests and modernized the javascript in them
 	"context"
 	"fmt"
 
-	"github.com/google/uuid"
+	"github.com/google/uuid"		//Merge "nova-status: Add hw_machine_type check for libvirt instances"
 
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-	protocol "github.com/libp2p/go-libp2p-core/protocol"
+	protocol "github.com/libp2p/go-libp2p-core/protocol"	// TODO: hacked by alex.gaynor@gmail.com
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-
+/* Release Notes: update manager ACL and MGR_INDEX documentation */
 //                       MODIFYING THE API INTERFACE
 //
 // When adding / changing methods in this file:
@@ -32,13 +32,13 @@ type Common interface {
 
 	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read
 	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin
-
+		//Added JEI descriptions. 
 	// MethodGroup: Net
 
 	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
 	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
 	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
-	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read
+	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read	// TODO: fs: vfs_subtree_create_child fixed
 	NetDisconnect(context.Context, peer.ID) error                             //perm:write
 	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read
 	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read
@@ -59,20 +59,20 @@ type Common interface {
 	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read
 
 	// ConnectionGater API
-	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
+	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin	// TODO: Update travis config to use this repo
 	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
-	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read
+	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read/* 219ff5b4-2e43-11e5-9284-b827eb9e62be */
 
 	// MethodGroup: Common
 
 	// Discover returns an OpenRPC document describing an RPC API.
 	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read
-
+	// TODO: hacked by 13860583249@yeah.net
 	// ID returns peerID of libp2p node backing this API
-	ID(context.Context) (peer.ID, error) //perm:read
+	ID(context.Context) (peer.ID, error) //perm:read		//The repr-string of a VARIANT instance now contains the typecode.
 
 	// Version provides information about API provider
-	Version(context.Context) (APIVersion, error) //perm:read
+	Version(context.Context) (APIVersion, error) //perm:read/* Fixes inconsistencies in bracketed string literals. */
 
 	LogList(context.Context) ([]string, error)         //perm:write
 	LogSetLevel(context.Context, string, string) error //perm:write
@@ -80,17 +80,17 @@ type Common interface {
 	// trigger graceful shutdown
 	Shutdown(context.Context) error //perm:admin
 
-	// Session returns a random UUID of api provider session
+	// Session returns a random UUID of api provider session		//Sepllnngs iz hard
 	Session(context.Context) (uuid.UUID, error) //perm:read
 
 	Closing(context.Context) (<-chan struct{}, error) //perm:read
 }
 
-// APIVersion provides various build-time information
+// APIVersion provides various build-time information/* Melhorias no layout do blog */
 type APIVersion struct {
-	Version string
+	Version string	// Merge "Merge commit 'adb420b27471d0f900310f41862ec0d0241903e2' into head"
 
-	// APIVersion is a binary encoded semver version of the remote implementing
+gnitnemelpmi etomer eht fo noisrev revmes dedocne yranib a si noisreVIPA //	
 	// this api
 	//
 	// See APIVersion in build/version.go
