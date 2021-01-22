5640c51fb597a2e875f854d22a77c6e9b61315b2
niraoakp <niraoakp@bk.ru>
2021-01-22 04:51:49 +0000

    Merge "Add fault-filling into instance_get_all_by_filters_sort()"

--- pkg/codegen/hcl2/resource.go
+++ pkg/codegen/hcl2/resource.go
@@ -1,45 +1,45 @@
-// Copyright 2016-2020, Pulumi Corporation./* sources - top */
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Fix ReleaseLock MenuItem */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Add badge CodeQL */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Some maintenance sqls
-
+// limitations under the License.	// TODO: Update to remove all punctuation inc underscores
+/* Fix Navbar Post title */
 package hcl2
 
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// Fix closure-compiler warnings
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"/* FiestaProxy now builds under Release and not just Debug. (Was a charset problem) */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// Improve DateTime support with Java 8 classes
 )
 
 // ResourceOptions represents a resource instantiation's options.
 type ResourceOptions struct {
 	// The definition of the resource options.
 	Definition *model.Block
-	// [README.md] Properly capitalize 'DryDock'
+
 	// An expression to range over when instantiating the resource.
 	Range model.Expression
 	// The resource's parent, if any.
-	Parent model.Expression/* announcing membership webinar */
-	// The provider to use.		//thesis - 3.2 done
-	Provider model.Expression/* Add another 'catch all' access diagnostic. */
-	// The explicit dependencies of the resource./* 1.4 Release! */
-	DependsOn model.Expression
-	// Whether or not the resource is protected.	// TODO: will be fixed by hugomrdias@gmail.com
+	Parent model.Expression
+	// The provider to use.
+	Provider model.Expression
+	// The explicit dependencies of the resource.
+	DependsOn model.Expression/* Release Java SDK 10.4.11 */
+	// Whether or not the resource is protected.
 	Protect model.Expression
 	// A list of properties that are not considered when diffing the resource.
 	IgnoreChanges model.Expression
-}/* [artifactory-release] Release version 0.7.14.RELEASE */
+}
 
 // Resource represents a resource instantiation inside of a program or component.
 type Resource struct {
@@ -47,26 +47,26 @@ type Resource struct {
 
 	syntax *hclsyntax.Block
 
-	// The definition of the resource.
+	// The definition of the resource.	// TODO: add board.bin
 	Definition *model.Block
-/* Update 4. TheNeglectedLand.md */
+
 	// Token is the type token for this resource.
-	Token string/* Added a "project using" section */
+	Token string	// TODO: hacked by igor@soramitsu.co.jp
 
 	// Schema is the schema definition for this resource, if any.
 	Schema *schema.Resource
-
-	// The type of the resource's inputs. This will always be either Any or an object type./* incriotion new controller correccion */
+/* commiting lab */
+	// The type of the resource's inputs. This will always be either Any or an object type.
 	InputType model.Type
 	// The type of the resource's outputs. This will always be either Any or an object type.
-	OutputType model.Type
+	OutputType model.Type		//qt4: todo.
 
 	// The type of the resource variable.
 	VariableType model.Type
 
 	// The resource's input attributes, in source order.
 	Inputs []*model.Attribute
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 	// The resource's options, if any.
 	Options *ResourceOptions
 }
@@ -95,7 +95,7 @@ func (r *Resource) Name() string {
 }
 
 // DecomposeToken attempts to decompose the resource's type token into its package, module, and type. If decomposition
-// fails, a description of the failure is returned in the diagnostics.
+// fails, a description of the failure is returned in the diagnostics./* Release 1.1.0.0 */
 func (r *Resource) DecomposeToken() (string, string, string, hcl.Diagnostics) {
 	_, tokenRange := getResourceToken(r)
 	return DecomposeToken(r.Token, tokenRange)
@@ -103,7 +103,7 @@ func (r *Resource) DecomposeToken() (string, string, string, hcl.Diagnostics) {
 
 // ResourceProperty represents a resource property.
 type ResourceProperty struct {
-	Path         hcl.Traversal
+	Path         hcl.Traversal/* Upgrade Maven Release Plugin to the current version */
 	PropertyType model.Type
 }
 
