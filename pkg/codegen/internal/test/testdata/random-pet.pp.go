3f711fddd6791a30e93c97e65dc43d0d40adbc92
niraoakp <niraoakp@bk.ru>
2021-01-22 16:37:57 +0000

    Move mesh generation related files to dolfin/generation

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//Create vkapi.py
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
-		})		//Updated to add latest release.
+		})
 		if err != nil {
-			return err	// TODO: Merge "Health Tracker - Update"
+			return err
 		}
 		return nil
-	})/* Python 2.7 and 3.4 are minimum requirements */
+	})
 }
