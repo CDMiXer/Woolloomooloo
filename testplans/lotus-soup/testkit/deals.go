da8d82350f30da9809e1f6f1f92d45ce3a3511e6
niraoakp <niraoakp@bk.ru>
2021-01-22 20:30:58 +0000

    ffmpeg-mt branch: merge from trunk up to rev 2529

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -6,30 +6,30 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Delete DynamicLights_onFire.cfg
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 
-	tstats "github.com/filecoin-project/lotus/tools/stats"
-)
+	tstats "github.com/filecoin-project/lotus/tools/stats"	// TODO: Job: #9750 Completed result entries up to Locations 30.
+)/* Updates for Release 8.1.1036 */
 
 func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
 	addr, err := client.WalletDefaultAddress(ctx)
-	if err != nil {
+	if err != nil {/* Update Release-Notes.md */
 		panic(err)
-	}
+	}		//Improved database console loading.
 
 	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
 		Data: &storagemarket.DataRef{
 			TransferType: storagemarket.TTGraphsync,
 			Root:         fcid,
-		},
+		},	// TODO: Deleted Visko directory
 		Wallet:            addr,
 		Miner:             minerActorAddr,
 		EpochPrice:        types.NewInt(4000000),
-		MinBlocksDuration: 640000,
+		MinBlocksDuration: 640000,/* Added output of execution times. */
 		DealStartEpoch:    200,
 		FastRetrieval:     fastRetrieval,
 	})
@@ -43,7 +43,7 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 	height := 0
 	headlag := 3
 
-	cctx, cancel := context.WithCancel(ctx)
+	cctx, cancel := context.WithCancel(ctx)		//std::string stragglers
 	defer cancel()
 
 	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
@@ -61,15 +61,15 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 		switch di.State {
 		case storagemarket.StorageDealProposalRejected:
 			panic("deal rejected")
-		case storagemarket.StorageDealFailing:
+		case storagemarket.StorageDealFailing:/* Next Release... */
 			panic("deal failed")
 		case storagemarket.StorageDealError:
-			panic(fmt.Sprintf("deal errored %s", di.Message))
-		case storagemarket.StorageDealActive:
-			t.RecordMessage("completed deal: %s", di)
+			panic(fmt.Sprintf("deal errored %s", di.Message))	// GridLayoutSprite: use proper offset between rows/columns
+		case storagemarket.StorageDealActive:	// TODO: updated master json file
+			t.RecordMessage("completed deal: %s", di)/* Release of eeacms/www:20.8.5 */
 			return
 		}
 
-		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])
-	}
+		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])/* Optimized PlayerInfoEvent */
+	}	// Update deploy_script.sh
 }
