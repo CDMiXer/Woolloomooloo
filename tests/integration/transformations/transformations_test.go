374c10f379b1c3d835d0fd56fc7892b6e71e3a1f
niraoakp <niraoakp@bk.ru>
2021-01-22 16:28:14 +0000

    Fixed #4503 (Glitch for shooting bullets remotely while reloading).

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Updated field names. Added new script.  */
 
-package ints
+package ints		//Remoção de tela antiga de cadastro de projetos
 
-import (
+import (		//fix NPE in SecurityFilter
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Simplified specs by version table */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
 )
 
 var Dirs = []string{
-	"simple",
+	"simple",/* Update README.md to add NiftyNet paper */
 }
 
 func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
@@ -25,22 +25,22 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
-			if res.URN.Name() == "res1" {
+			if res.URN.Name() == "res1" {	// TODO: add link to show payment summary
 				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child"
+			// "child"/* Release 2.0.0: Upgrade to ECM 3.0 */
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))	// disable sourcemaps in production
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
 			// "res3" is impacted by a global stack transformation which sets
-			// optionalDefault to "stackDefault"
+			// optionalDefault to "stackDefault"/* Reflect actual code contributors */
 			if res.URN.Name() == "res3" {
 				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
@@ -52,12 +52,12 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
 			// result should be "stackDefault".
-			if res.URN.Name() == "res4-child" {
-				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
+			if res.URN.Name() == "res4-child" {		//Create RNASeq_lab_II-DESeq_analysis.md
+				foundRes4Child = true		//Delete Match.js
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))	// TODO: 19577b62-2e5b-11e5-9284-b827eb9e62be
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))/* Release version: 0.6.1 */
 				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)
+				assert.NotNil(t, optionalInput)		//vcf format
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res5" modifies one of its children to depend on another of its children.
@@ -69,7 +69,7 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				// will not be correctly recorded in the state file, and so cannot be
 				// verified here.
 				//
-				// assert.Len(t, res.PropertyDependencies, 1)
+				// assert.Len(t, res.PropertyDependencies, 1)/* Release maintenance v1.1.4 */
 				input := res.Inputs["input"]
 				assert.NotNil(t, input)
 				assert.Equal(t, "b", input.(string))
@@ -78,7 +78,7 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 		assert.True(t, foundRes1)
 		assert.True(t, foundRes2Child)
 		assert.True(t, foundRes3)
-		assert.True(t, foundRes4Child)
+		assert.True(t, foundRes4Child)	// Automatic changelog generation for PR #12812 [ci skip]
 		assert.True(t, foundRes5Child)
 	}
 }
