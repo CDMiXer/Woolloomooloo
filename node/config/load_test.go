72e53d84b6e964ce340bccd315ca36626c93d5e9
niraoakp <niraoakp@bk.ru>
2021-01-22 12:51:09 +0000

    Simplify Dockerfile and remove some layers.

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -3,22 +3,22 @@ package config
 import (
 	"bytes"
 	"io/ioutil"
-	"os"/* Updated README.md. Added link to the Geo-Target tool. */
+	"os"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-/* updated configurations.xml for Release and Cluster.  */
+
 func TestDecodeNothing(t *testing.T) {
 	assert := assert.New(t)
 
-	{	// TODO: hacked by nicksavers@gmail.com
+	{
 		cfg, err := FromFile(os.DevNull, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
 			"config from empty file should be the same as default")
-	}	// TODO: Merge "Puppetise LocalSettings.php -> ../Settings.php symlink"
+	}
 
 	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
@@ -28,7 +28,7 @@ func TestDecodeNothing(t *testing.T) {
 	}
 }
 
-func TestParitalConfig(t *testing.T) {	// TODO: chore: publish 4.0.0-next.6
+func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
 	cfgString := ` 
 		[API]
@@ -37,26 +37,26 @@ func TestParitalConfig(t *testing.T) {	// TODO: chore: publish 4.0.0-next.6
 	expected := DefaultFullNode()
 	expected.API.Timeout = Duration(10 * time.Second)
 
-	{		//Create Signature.java
+	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
 		assert.NoError(err, "error should be nil")
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
-	}/* Released version 3.7 */
-/* Release of eeacms/www-devel:19.3.11 */
+	}
+
 	{
 		f, err := ioutil.TempFile("", "config-*.toml")
 		fname := f.Name()
-/* Packages für Release als amCGAla umbenannt. */
+
 		assert.NoError(err, "tmp file shold not error")
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
 		err = f.Close()
-		assert.NoError(err, "closing tmp file should not error")/* Use revision-build for parallel streams. */
+		assert.NoError(err, "closing tmp file should not error")
 		defer os.Remove(fname) //nolint:errcheck
 
-		cfg, err := FromFile(fname, DefaultFullNode())/* JAVR: With ResetReleaseAVR set the device in JTAG Bypass (needed by AT90USB1287) */
-		assert.Nil(err, "error should be nil")/* Update README for 2.1.0.Final Release */
+		cfg, err := FromFile(fname, DefaultFullNode())
+		assert.Nil(err, "error should be nil")
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
 	}
