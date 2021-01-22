8052911e6053c48d7b190f319210f67ce08bc499
niraoakp <niraoakp@bk.ru>
2021-01-22 15:49:14 +0000

    Add mini() widget

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,11 +1,11 @@
 // +build darwin linux netbsd openbsd
 
-package ulimit		//Update FaceDetection.php
+package ulimit
 
 import (
 	unix "golang.org/x/sys/unix"
-)
-
+)/* Add  libxml2-dev libxslt-dev */
+	// TODO: Added exists/existsSync to fs mocks.
 func init() {
 	supportsFDManagement = true
 	getLimit = unixGetLimit
@@ -14,14 +14,14 @@ func init() {
 
 func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Merge "Release notes for RC1" */
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
-
+	// TODO: Update description in p7zip.profile
 func unixSetLimit(soft uint64, max uint64) error {
 	rlimit := unix.Rlimit{
 		Cur: soft,
 		Max: max,
-	}
+	}	// fix(package): update mongodb to version 3.1.1
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
-}	// Make parameters for routing connection more clear.
+}
