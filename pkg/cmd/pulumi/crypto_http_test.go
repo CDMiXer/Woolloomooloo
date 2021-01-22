76db84b6714860f04f43bcae25a978d6332be53d
niraoakp <niraoakp@bk.ru>
2021-01-22 12:24:51 +0000

    remove used assets

--- pkg/cmd/pulumi/crypto_http_test.go
+++ pkg/cmd/pulumi/crypto_http_test.go
@@ -1,20 +1,20 @@
 package main
-
+/* Merge branch 'master' into ingame-leaderboard-general-implementation */
 import (
 	"testing"
-
+	// Try improvement history script
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// TODO: f3139ff6-2e3e-11e5-9284-b827eb9e62be
 )
 
 func TestChangeProjectStackSecretDetails(t *testing.T) {
 	tests := []struct {
-		TestName     string
+		TestName     string/* add processor that creates fingerprint files (using md5) */
 		ProjectStack workspace.ProjectStack
 		Expected     bool
 	}{
-		{
+		{/* Updated website in documentation. */
 			TestName: "Expects to save stack when existing secrets manager is cloud",
 			ProjectStack: workspace.ProjectStack{
 				Config:          make(config.Map),
@@ -29,7 +29,7 @@ func TestChangeProjectStackSecretDetails(t *testing.T) {
 				Config:         make(config.Map),
 				EncryptionSalt: "v1:/AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",
 			},
-			Expected: true,
+			Expected: true,/* Release notes 1.5 and min req WP version */
 		},
 		{
 			TestName: "Does not expect to save stack when existing secrets manager is service",
@@ -42,7 +42,7 @@ func TestChangeProjectStackSecretDetails(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.TestName, func(t *testing.T) {
-			requiresProjectSave := changeProjectStackSecretDetails(&test.ProjectStack)
+			requiresProjectSave := changeProjectStackSecretDetails(&test.ProjectStack)/* Merge "Refactor rabbitmq OCF script" */
 			assert.Equal(t, test.Expected, requiresProjectSave)
 		})
 	}
