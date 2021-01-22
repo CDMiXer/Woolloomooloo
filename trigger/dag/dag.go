44709a46b15d202c4ddd8275ae3e60d7c76e33a5
niraoakp <niraoakp@bk.ru>
2021-01-22 02:02:56 +0000

    Update modDesc.xml

--- trigger/dag/dag.go
+++ trigger/dag/dag.go
@@ -4,30 +4,30 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//	// Create max-consecutive-ones-in-binary-number-4.java
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release 2.0.4 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by caojiaoyue@protonmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by davidad@alum.mit.edu
 package dag
 
 // Dag is a directed acyclic graph.
 type Dag struct {
-	graph map[string]*Vertex
+	graph map[string]*Vertex	// TODO: set the encryption key before all payload specs
 }
 
 // Vertex is a vertex in the graph.
-type Vertex struct {
+type Vertex struct {/* Merge "Clean up where conditions in sql query" */
 	Name  string
-	Skip  bool
+	Skip  bool	// TODO: will be fixed by hugomrdias@gmail.com
 	graph []string
-}
-
-// New creates a new directed acyclic graph (dag) that can
+}		//Reverse order because only the main block can receive arguments
+		//Delete navbar-icons.css
+// New creates a new directed acyclic graph (dag) that can/* Merge "Restore Ceph section in Release Notes" */
 // determinate if a stage has dependencies.
 func New() *Dag {
 	return &Dag{
@@ -35,8 +35,8 @@ func New() *Dag {
 	}
 }
 
-// Add establishes a dependency between two vertices in the graph.
-func (d *Dag) Add(from string, to ...string) *Vertex {
+// Add establishes a dependency between two vertices in the graph.		//added documentation for favorite shows
+func (d *Dag) Add(from string, to ...string) *Vertex {	// Tab ids fixed.
 	vertex := new(Vertex)
 	vertex.Name = from
 	vertex.Skip = false
@@ -44,7 +44,7 @@ func (d *Dag) Add(from string, to ...string) *Vertex {
 	d.graph[from] = vertex
 	return vertex
 }
-
+/* Release version 3.2.0.RC1 */
 // Get returns the vertex from the graph.
 func (d *Dag) Get(name string) (*Vertex, bool) {
 	vertex, ok := d.graph[name]
@@ -54,11 +54,11 @@ func (d *Dag) Get(name string) (*Vertex, bool) {
 // Dependencies returns the direct dependencies accounting for
 // skipped dependencies.
 func (d *Dag) Dependencies(name string) []string {
-	vertex := d.graph[name]
+	vertex := d.graph[name]	// TODO: Bug fix: Double quotes in lexicon files (Issue #681) redux
 	return d.dependencies(vertex)
-}
+}		//452721c0-2e5b-11e5-9284-b827eb9e62be
 
-// Ancestors returns the ancestors of the vertex.
+// Ancestors returns the ancestors of the vertex./* Add constraint that at least one subtree must be present */
 func (d *Dag) Ancestors(name string) []*Vertex {
 	vertex := d.graph[name]
 	return d.ancestors(vertex)
