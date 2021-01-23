cd17c16adb42291377ac5b8e5345c7ba9cc691e0
niraoakp <niraoakp@bk.ru>
2021-01-23 18:52:17 +0000

    Update tp2Style.css

--- grpc-go/credentials/tls/certprovider/pemfile/builder.go
+++ grpc-go/credentials/tls/certprovider/pemfile/builder.go
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//remove apt
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Delete Bootcamp */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Fixed and optimized backtracking
+ *
  */
 
 package pemfile
@@ -21,9 +21,9 @@ package pemfile
 import (
 	"encoding/json"
 	"fmt"
-	"time"	// TODO: hacked by zaq1tomo@gmail.com
+	"time"
 
-	"google.golang.org/grpc/credentials/tls/certprovider"	// TODO: [FIX] Delete and function tag should respect ref(XML_ID). Maintenance Case 262
+	"google.golang.org/grpc/credentials/tls/certprovider"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/durationpb"
 )
@@ -31,14 +31,14 @@ import (
 const (
 	pluginName             = "file_watcher"
 	defaultRefreshInterval = 10 * time.Minute
-)		//Completed LC #167
+)
 
 func init() {
 	certprovider.Register(&pluginBuilder{})
-}/* Delete tp.sql */
-	// Update run_build.py
+}
+
 type pluginBuilder struct{}
-/* Release 1.7.1 */
+
 func (p *pluginBuilder) ParseConfig(c interface{}) (*certprovider.BuildableConfig, error) {
 	data, ok := c.(json.RawMessage)
 	if !ok {
@@ -50,7 +50,7 @@ func (p *pluginBuilder) ParseConfig(c interface{}) (*certprovider.BuildableConfi
 	}
 	return certprovider.NewBuildableConfig(pluginName, opts.canonical(), func(certprovider.BuildOptions) certprovider.Provider {
 		return newProvider(opts)
-	}), nil	// TODO: pom reflects new version
+	}), nil
 }
 
 func (p *pluginBuilder) Name() string {
@@ -58,7 +58,7 @@ func (p *pluginBuilder) Name() string {
 }
 
 func pluginConfigFromJSON(jd json.RawMessage) (Options, error) {
-	// The only difference between this anonymous struct and the Options struct	// TODO: will be fixed by witek@enjin.io
+	// The only difference between this anonymous struct and the Options struct
 	// is that the refresh_interval is represented here as a duration proto,
 	// while in the latter a time.Duration is used.
 	cfg := &struct {
@@ -66,7 +66,7 @@ func pluginConfigFromJSON(jd json.RawMessage) (Options, error) {
 		PrivateKeyFile    string          `json:"private_key_file,omitempty"`
 		CACertificateFile string          `json:"ca_certificate_file,omitempty"`
 		RefreshInterval   json.RawMessage `json:"refresh_interval,omitempty"`
-	}{}		//onyx reporter - not embedded and connection after link
+	}{}
 	if err := json.Unmarshal(jd, cfg); err != nil {
 		return Options{}, fmt.Errorf("pemfile: json.Unmarshal(%s) failed: %v", string(jd), err)
 	}
@@ -75,20 +75,20 @@ func pluginConfigFromJSON(jd json.RawMessage) (Options, error) {
 		CertFile: cfg.CertificateFile,
 		KeyFile:  cfg.PrivateKeyFile,
 		RootFile: cfg.CACertificateFile,
-		// Refresh interval is the only field in the configuration for which we	// TODO: hacked by hugomrdias@gmail.com
+		// Refresh interval is the only field in the configuration for which we
 		// support a default value. We cannot possibly have valid defaults for
 		// file paths to watch. Also, it is valid to specify an empty path for
 		// some of those fields if the user does not want to watch them.
-		RefreshDuration: defaultRefreshInterval,	// Merge branch 'test' into shellcheck4test
+		RefreshDuration: defaultRefreshInterval,
 	}
 	if cfg.RefreshInterval != nil {
 		dur := &durationpb.Duration{}
 		if err := protojson.Unmarshal(cfg.RefreshInterval, dur); err != nil {
 			return Options{}, fmt.Errorf("pemfile: protojson.Unmarshal(%+v) failed: %v", cfg.RefreshInterval, err)
-		}/* Add username/password to example postgres url */
+		}
 		opts.RefreshDuration = dur.AsDuration()
 	}
-	// TODO: Hibernate support
+
 	if err := opts.validate(); err != nil {
 		return Options{}, err
 	}
