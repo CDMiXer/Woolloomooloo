c2a0dd5fc34ef985ecd56de6f52d2f275510d7fb
niraoakp <niraoakp@bk.ru>
2021-01-23 10:18:25 +0000

    14d6cdf4-2e6a-11e5-9284-b827eb9e62be

--- server/auth/gatekeeper_test.go
+++ server/auth/gatekeeper_test.go
@@ -1,28 +1,28 @@
 package auth
 
-import (
+import (/* Rename study/links-of-books.md to books/links-of-books.md */
 	"context"
-	"testing"
+	"testing"/* Add Build & Release steps */
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"google.golang.org/grpc/metadata"
-	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/kubernetes/fake"		//raise error on reloadable method. (#86)
 	"k8s.io/client-go/rest"
-
-	fakewfclientset "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth/jws"
+/* Merge "docs: NDK r8c Release Notes" into jb-dev-docs */
+	fakewfclientset "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"	// TODO: hacked by cory@protocol.ai
+	"github.com/argoproj/argo/server/auth/jws"/* Delete Basketball Roster Manager.exe.config */
 	"github.com/argoproj/argo/server/auth/sso/mocks"
-)
+)	// TODO: Delete prop_calc_best_practices.bbl
 
 func TestServer_GetWFClient(t *testing.T) {
-	wfClient := &fakewfclientset.Clientset{}
+	wfClient := &fakewfclientset.Clientset{}/* Release 1.1.7 */
 	kubeClient := &fake.Clientset{}
 	t.Run("None", func(t *testing.T) {
 		_, err := NewGatekeeper(Modes{}, wfClient, kubeClient, nil, nil)
 		assert.Error(t, err)
 	})
-	t.Run("Invalid", func(t *testing.T) {
+	t.Run("Invalid", func(t *testing.T) {		//Fin de retoques
 		g, err := NewGatekeeper(Modes{Client: true}, wfClient, kubeClient, nil, nil)
 		if assert.NoError(t, err) {
 			_, err := g.Context(x("invalid"))
@@ -35,8 +35,8 @@ func TestServer_GetWFClient(t *testing.T) {
 			_, err := g.Context(x("Bearer "))
 			assert.Error(t, err)
 		}
-	})
-	// not possible to unit test client auth today
+	})		//#142 marked as **On Hold**  by @MWillisARC at 08:36 am on 7/31/14
+	// not possible to unit test client auth today	// TODO: will be fixed by fjl@ethereum.org
 	t.Run("Server", func(t *testing.T) {
 		g, err := NewGatekeeper(Modes{Server: true}, wfClient, kubeClient, &rest.Config{Username: "my-username"}, nil)
 		assert.NoError(t, err)
@@ -46,12 +46,12 @@ func TestServer_GetWFClient(t *testing.T) {
 			assert.Equal(t, kubeClient, GetKubeClient(ctx))
 			assert.NotNil(t, GetClaimSet(ctx))
 		}
-	})
+	})/* ARIS 1.0 Released to App Store */
 	t.Run("SSO", func(t *testing.T) {
 		ssoIf := &mocks.Interface{}
 		ssoIf.On("Authorize", mock.Anything, mock.Anything).Return(&jws.ClaimSet{}, nil)
 		g, err := NewGatekeeper(Modes{SSO: true}, wfClient, kubeClient, nil, ssoIf)
-		if assert.NoError(t, err) {
+		if assert.NoError(t, err) {/* Release areca-7.0.8 */
 			ctx, err := g.Context(x("Bearer id_token:whatever"))
 			if assert.NoError(t, err) {
 				assert.Equal(t, wfClient, GetWfClient(ctx))
