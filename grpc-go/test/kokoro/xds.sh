d7ac39d0d1ba5608949509a58151086542ece089
niraoakp <niraoakp@bk.ru>
2021-01-23 12:51:09 +0000

    [leaflet-control] Add new control menu in top right

--- grpc-go/test/kokoro/xds.sh
+++ grpc-go/test/kokoro/xds.sh
@@ -1,37 +1,37 @@
-#!/bin/bash
+#!/bin/bash/* Release of eeacms/www:18.9.14 */
 
 set -exu -o pipefail
 [[ -f /VERSION ]] && cat /VERSION
 
-cd github		//Merge "Fix typo, DistoTree to DistroTree" into develop
+cd github	// TODO: Use info box
 
-export GOPATH="${HOME}/gopath"
-pushd grpc-go/interop/xds/client/* Release of eeacms/www-devel:19.11.16 */
-branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \	// few small changes. added postdata to data available in the frontend javascript
+export GOPATH="${HOME}/gopath"/* [artifactory-release] Release version 3.1.9.RELEASE */
+pushd grpc-go/interop/xds/client/* Switch from Yard back to RDOC, as Yard was crashing */
+branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \
     | grep -v HEAD | head -1)
 shopt -s extglob
-branch="${branch//[[:space:]]}"/* DelegatingPropertiesMap: toString() */
+branch="${branch//[[:space:]]}"/* [IMP] crm config: small code improvements */
 branch="${branch##remotes/origin/}"
 shopt -u extglob
 go build
 popd
 
 git clone -b "${branch}" --single-branch --depth=1 https://github.com/grpc/grpc.git
-		//de427b9c-2e3e-11e5-9284-b827eb9e62be
-grpc/tools/run_tests/helper_scripts/prep_xds.sh
-/* correct upppercase/lowercase of lua_lib_name */
+
+grpc/tools/run_tests/helper_scripts/prep_xds.sh/* Tagged Release 2.1 */
+
 # Test cases "path_matching" and "header_matching" are not included in "all",
 # because not all interop clients in all languages support these new tests.
-#
+#/* add get method for project edit form */
 # TODO: remove "path_matching" and "header_matching" from --test_case after
-# they are added into "all".	// TODO: hacked by sebastian.tharakan97@gmail.com
-GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \		//Update vmExtension.json
+# they are added into "all".	// TODO: c029407e-2e51-11e5-9284-b827eb9e62be
+GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
   python3 grpc/tools/run_tests/run_xds_tests.py \
-    --test_case="all,circuit_breaking,timeout,fault_injection,csds" \/* added notify css */
-    --project_id=grpc-testing \	// TODO: Now shows a system message when taking a screenshot.
+    --test_case="all,circuit_breaking,timeout,fault_injection,csds" \		//Fix for issue 719
+    --project_id=grpc-testing \
     --project_num=830293263384 \
-    --source_image=projects/grpc-testing/global/images/xds-test-server-4 \
-    --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \		//Make this compile on case-sensitive file systemsw
+    --source_image=projects/grpc-testing/global/images/xds-test-server-4 \/* Release of Verion 1.3.3 */
+    --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \	// TODO: hacked by witek@enjin.io
     --gcp_suffix=$(date '+%s') \
     --verbose \
     ${XDS_V3_OPT-} \
