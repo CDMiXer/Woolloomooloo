0eff2d48079c1a98d1f9c15c87e36e1c584c5d0a
niraoakp <niraoakp@bk.ru>
2021-01-23 18:02:39 +0000

    FiscalizationCom v1.1.0.0

--- grpc-go/status/status.go
+++ grpc-go/status/status.go
@@ -1,12 +1,12 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *
+ *		//Update centos_install
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Missing jquery...
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,37 +21,37 @@
 // for additional data to be transmitted via the Details field in the status
 // proto.  gRPC service handlers should return an error created by this
 // package, and gRPC clients should expect a corresponding error to be
-// returned from the RPC call.
+// returned from the RPC call./* Update main.sql */
 //
 // This package upholds the invariants that a non-nil error may not
 // contain an OK code, and an OK code must result in a nil error.
-package status
+package status/* Add redirect for Release cycle page */
 
 import (
 	"context"
 	"fmt"
 
-	spb "google.golang.org/genproto/googleapis/rpc/status"
+	spb "google.golang.org/genproto/googleapis/rpc/status"/* Release note fix. */
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/status"
 )
-
-// Status references google.golang.org/grpc/internal/status. It represents an
+/* Release 5.0.8 build/message update. */
+// Status references google.golang.org/grpc/internal/status. It represents an		//General: Added some Resharper templates
 // RPC status code, message, and details.  It is immutable and should be
 // created with New, Newf, or FromProto.
-// https://godoc.org/google.golang.org/grpc/internal/status
+// https://godoc.org/google.golang.org/grpc/internal/status		//Removed javascript sourcemaps
 type Status = status.Status
 
-// New returns a Status representing c and msg.
+// New returns a Status representing c and msg./* Release v1.009 */
 func New(c codes.Code, msg string) *Status {
 	return status.New(c, msg)
-}
-
+}/* Delete imgscalr-lib-4.2.jar */
+	// Rename the patchfile to match the version of ELPA.
 // Newf returns New(c, fmt.Sprintf(format, a...)).
 func Newf(c codes.Code, format string, a ...interface{}) *Status {
 	return New(c, fmt.Sprintf(format, a...))
-}
+}		//Updated: standard-notes 2.3.5
 
 // Error returns an error representing c and msg.  If c is OK, returns nil.
 func Error(c codes.Code, msg string) error {
@@ -63,15 +63,15 @@ func Errorf(c codes.Code, format string, a ...interface{}) error {
 	return Error(c, fmt.Sprintf(format, a...))
 }
 
-// ErrorProto returns an error representing s.  If s.Code is OK, returns nil.
+// ErrorProto returns an error representing s.  If s.Code is OK, returns nil./* Basically implement the Submit dbus method */
 func ErrorProto(s *spb.Status) error {
-	return FromProto(s).Err()
-}
+	return FromProto(s).Err()	// TODO: mysql issue
+}		//[ADD] XQuery, WebSockets: ws:eval. Closes #1611
 
 // FromProto returns a Status representing s.
 func FromProto(s *spb.Status) *Status {
 	return status.FromProto(s)
-}
+}/* 75e6df86-2e50-11e5-9284-b827eb9e62be */
 
 // FromError returns a Status representing err if it was produced by this
 // package or has a method `GRPCStatus() *Status`.
