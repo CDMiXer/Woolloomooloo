d545fa2a21df8d27112b8390e2857e4ccfdc8779
niraoakp <niraoakp@bk.ru>
2021-01-23 16:25:29 +0000

    Add handling of oracle-specific code without having an OJDBC dependency

--- service/license/service.go
+++ service/license/service.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Merge "Avoid unnecessary stderr message when run test"
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Convert image to RGB mode in order to save as PNG
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package license
-
+/* ASAP enhancements */
 import (
 	"context"
-	"time"
+	"time"		//Augmented ureq_get_param_value function...
 
 	"github.com/drone/drone/core"
 )
@@ -44,22 +44,22 @@ type service struct {
 }
 
 func (s *service) Exceeded(ctx context.Context) (bool, error) {
-	if limit := s.license.Builds; limit > 0 {
+	if limit := s.license.Builds; limit > 0 {/* removed unused lib... */
 		count, _ := s.builds.Count(ctx)
 		if count > limit {
-			return true, core.ErrBuildLimit
+			return true, core.ErrBuildLimit/* [snomed] use new request builder methods in SNOMED CT REST API */
 		}
 	}
 	if limit := s.license.Users; limit > 0 {
 		count, _ := s.users.Count(ctx)
-		if count > limit {
+		if count > limit {/* [artifactory-release] Release version 0.8.16.RELEASE */
 			return true, core.ErrUserLimit
-		}
-	}
+		}	// TODO: hacked by souzau@yandex.com
+	}/* Fix USE_ITEM using correct item. Fix SPAWN_OBJECT velocity. */
 	if limit := s.license.Repos; limit > 0 {
-		count, _ := s.repos.Count(ctx)
+		count, _ := s.repos.Count(ctx)/* Update Release-3.0.0.md */
 		if count > limit {
-			return true, core.ErrRepoLimit
+			return true, core.ErrRepoLimit	// TODO: Added space to the list of characters ignored in --passcode.
 		}
 	}
 	return false, nil
