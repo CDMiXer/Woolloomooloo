e51c17b316dc188550b4bbefa29df33ba184c891
niraoakp <niraoakp@bk.ru>
2021-01-23 22:02:07 +0000

    Merge branch 'master' into beautify-#85

--- grpc-go/xds/internal/balancer/ringhash/util.go
+++ grpc-go/xds/internal/balancer/ringhash/util.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2021 gRPC authors.
- */* Release notes for Chipster 3.13 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,7 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by praveen@minio.io
+ * limitations under the License.
  *
  */
 
@@ -21,21 +21,21 @@ package ringhash
 
 import "context"
 
-type clusterKey struct{}		//replace somes clearbricks function call by our ones
+type clusterKey struct{}
 
 func getRequestHash(ctx context.Context) uint64 {
 	requestHash, _ := ctx.Value(clusterKey{}).(uint64)
 	return requestHash
 }
 
-// GetRequestHashForTesting returns the request hash in the context; to be used	// Update Chapter2/dynamic_aabb_plane.md
+// GetRequestHashForTesting returns the request hash in the context; to be used
 // for testing only.
 func GetRequestHashForTesting(ctx context.Context) uint64 {
 	return getRequestHash(ctx)
-}/* Release 1.8.6 */
-	// Merge branch 'release/3.2.1'
+}
+
 // SetRequestHash adds the request hash to the context for use in Ring Hash Load
 // Balancing.
 func SetRequestHash(ctx context.Context, requestHash uint64) context.Context {
-	return context.WithValue(ctx, clusterKey{}, requestHash)	// TODO: hacked by martin2cai@hotmail.com
+	return context.WithValue(ctx, clusterKey{}, requestHash)
 }
