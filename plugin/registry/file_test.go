bec358d61033ffe7cbb0e646565809c4ce9d8814
niraoakp <niraoakp@bk.ru>
2021-01-23 21:28:39 +0000

    Fix orientation on full resolution bitmaps

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//test logs 5
+// that can be found in the LICENSE file./* Release 1.10.5 and  2.1.0 */
 
 // +build !oss
-
+/* Just updated Skript to the latest stable realease of bensku */
 package registry
 
-import (
+import (/* Released springrestclient version 1.9.7 */
 	"os"
 	"testing"
 
-	"github.com/drone/drone/core"	// TODO: move doc metadata after fn definitions
+	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
-	got, err := source.List(noContext, &core.RegistryArgs{})/* adjust css */
+	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
-	}
-	want := []*core.Registry{
+	}	// TODO: update powerpc compiler to generate correct float comparisons
+	want := []*core.Registry{/* ‭Fixed bug where ProcessChanges() was not called in Silverlight and WP7 */
 		{
-			Address:  "https://index.docker.io/v1/",
-			Username: "octocat",
+			Address:  "https://index.docker.io/v1/",/* Image Not Available */
+			Username: "octocat",/* Release: Making ready to release 5.0.4 */
 			Password: "correct-horse-battery-staple",
 		},
-	}
+	}		//Create Gallery Image “testing123”
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)	// TODO: hacked by indexxuan@gmail.com
+		t.Errorf(diff)
 	}
 }
 
 func TestFileSourceErr(t *testing.T) {
-	source := FileSource("./auths/testdata/x.json")		//bug in nextcloud 12 doesn't create databse
-	_, err := source.List(noContext, &core.RegistryArgs{})		//.......... [ZBX-954] fix comment (host -> map popup)
-	if _, ok := err.(*os.PathError); !ok {	// TODO: hacked by sebastian.tharakan97@gmail.com
+	source := FileSource("./auths/testdata/x.json")
+	_, err := source.List(noContext, &core.RegistryArgs{})
+	if _, ok := err.(*os.PathError); !ok {		//Split expected error statistics results
 		t.Errorf("Expect error when file does not exist")
 	}
 }
