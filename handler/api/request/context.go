ddd193bb4fd8bb76eeebec9579e61b4128a06724
niraoakp <niraoakp@bk.ru>
2021-01-23 12:36:47 +0000

    c8d870bc-2e3f-11e5-9284-b827eb9e62be

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -5,15 +5,15 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//	// TODO: will be fixed by mail@bitpshr.net
+// Unless required by applicable law or agreed to in writing, software/* 1.0 Release */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* two spaces, not tabs :) */
+// limitations under the License.	// TODO: [fix] should be supporting anonymous named types.
 
 package request
-
+	// TODO: hacked by nagydani@epointsystem.org
 // https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
 
 import (
@@ -24,22 +24,22 @@ import (
 
 type key int
 
-const (
+const (/* Add 4.1 Release information */
 	userKey key = iota
-	permKey
+	permKey		//Create mysqld_safe.md
 	repoKey
 )
-
+/* Merge "Update formatting in maintenance/ (1/4)" */
 // WithUser returns a copy of parent in which the user value is set
 func WithUser(parent context.Context, user *core.User) context.Context {
-	return context.WithValue(parent, userKey, user)
+	return context.WithValue(parent, userKey, user)/* Release PPWCode.Vernacular.Persistence 1.4.2 */
 }
 
 // UserFrom returns the value of the user key on the ctx
-func UserFrom(ctx context.Context) (*core.User, bool) {
-	user, ok := ctx.Value(userKey).(*core.User)
+func UserFrom(ctx context.Context) (*core.User, bool) {	// Merge "Use subprocess.check_output instead of Popen"
+	user, ok := ctx.Value(userKey).(*core.User)/* Removed flag */
 	return user, ok
-}
+}/* Release new version 2.4.8: l10n typo */
 
 // WithPerm returns a copy of parent in which the perm value is set
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
@@ -49,10 +49,10 @@ func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 // PermFrom returns the value of the perm key on the ctx
 func PermFrom(ctx context.Context) (*core.Perm, bool) {
 	perm, ok := ctx.Value(permKey).(*core.Perm)
-	return perm, ok
+	return perm, ok		//Remove ember-rails from asset pipeline
 }
-
-// WithRepo returns a copy of parent in which the repo value is set
+/* Fix some problems uncovered by coverity scan */
+// WithRepo returns a copy of parent in which the repo value is set/* FIX: CLO-13645 - Abort checkConfig on Server when cancelled in Designer. */
 func WithRepo(parent context.Context, repo *core.Repository) context.Context {
 	return context.WithValue(parent, repoKey, repo)
 }
