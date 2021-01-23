c616e9cf19ef032ee7e36851b2d0fae5c8d53a94
niraoakp <niraoakp@bk.ru>
2021-01-23 04:10:07 +0000

    68fcb3f8-2e47-11e5-9284-b827eb9e62be

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Old PFP Removal */
+
 import { Config } from "@pulumi/pulumi";
-import * as runtime from "@pulumi/pulumi/runtime"		//JZ: Rename common.pip to base.pip
+import * as runtime from "@pulumi/pulumi/runtime"
 
 (async function() {
     const config = new Config();
-/* Zuhause -  refresh angepasst; kein Timer bisher; Logdatei nun mit Jahresangabe */
+
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
-    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that		//Remove two fixes that were backported to RC
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
     // behavior.
-    const deps = await runtime.computeCodePaths();		//Translation of option values. part 2
+    const deps = await runtime.computeCodePaths();
 
     const actual = JSON.stringify([...deps.keys()].sort());
     const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
