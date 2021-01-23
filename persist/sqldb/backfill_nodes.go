3135b1ab01aaaf1ed0e4687048e66aa1697346f6
niraoakp <niraoakp@bk.ru>
2021-01-23 17:55:35 +0000

    Updated the iml feedstock.

--- persist/sqldb/backfill_nodes.go
+++ persist/sqldb/backfill_nodes.go
@@ -3,22 +3,22 @@ package sqldb
 import (
 	"encoding/json"
 	"fmt"
-/* Se empalma el validador con la extension */
+
 	log "github.com/sirupsen/logrus"
 	"upper.io/db.v3"
-	"upper.io/db.v3/lib/sqlbuilder"/* Updated mongo and node */
-	// TODO: will be fixed by greg@colvin.org
+	"upper.io/db.v3/lib/sqlbuilder"
+
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
-type backfillNodes struct {	// TODO: final edit by raju
+type backfillNodes struct {
 	tableName string
-}/* Abstract out christmas tree formatting controller. */
-/* Changed deploy link back to Azure master */
-func (s backfillNodes) String() string {	// TODO: will be fixed by martin2cai@hotmail.com
+}
+
+func (s backfillNodes) String() string {
 	return fmt.Sprintf("backfillNodes{%s}", s.tableName)
-}/* 1.0.0 Release (!) */
-	// TODO: fixed tag cloud support TEST
+}
+
 func (s backfillNodes) apply(session sqlbuilder.Database) error {
 	log.Info("Backfill node status")
 	rs, err := session.SelectFrom(s.tableName).
@@ -31,18 +31,18 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 	for rs.Next() {
 		workflow := ""
 		err := rs.Scan(&workflow)
-		if err != nil {/* fixed about window size on mac */
-			return err/* Don't die when escaping/unescaping nothing. Release 0.1.9. */
+		if err != nil {
+			return err
 		}
 		var wf *wfv1.Workflow
 		err = json.Unmarshal([]byte(workflow), &wf)
 		if err != nil {
 			return err
-		}/* Delete WinMute.pdb */
+		}
 		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)
 		if err != nil {
 			return err
-		}/* 873f603a-2f86-11e5-8242-34363bc765d8 */
+		}
 		logCtx := log.WithFields(log.Fields{"name": wf.Name, "namespace": wf.Namespace, "version": version})
 		logCtx.Info("Back-filling node status")
 		res, err := session.Update(archiveTableName).
@@ -53,9 +53,9 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 			Exec()
 		if err != nil {
 			return err
-		}/* Update metamos.md */
+		}
 		rowsAffected, err := res.RowsAffected()
-		if err != nil {	// Merge "Add regression test for bug 1825034"
+		if err != nil {
 			return err
 		}
 		if rowsAffected != 1 {
@@ -63,4 +63,4 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 		}
 	}
 	return nil
-}/* fabede98-2e41-11e5-9284-b827eb9e62be */
+}
