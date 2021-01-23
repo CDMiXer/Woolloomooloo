c17d50934b2e020bbdd0b2298989663d6b73decb
niraoakp <niraoakp@bk.ru>
2021-01-23 14:39:22 +0000

    Update docs to reflect changes in dev oref0-setup

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
@@ -1,36 +1,36 @@
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;	// Add loading.
+using System.IO;	// Split lib modules into separate packages.
+using System.Linq;
 using System.Text.Json;
 using Pulumi;
 using Aws = Pulumi.Aws;
 
-class MyStack : Stack/* Release Version 1.1.0 */
+class MyStack : Stack
 {
     public MyStack()
     {
         // Create a bucket and expose a website index document
         var siteBucket = new Aws.S3.Bucket("siteBucket", new Aws.S3.BucketArgs
-        {
+        {		//updating ref to wiki
             Website = new Aws.S3.Inputs.BucketWebsiteArgs
             {
                 IndexDocument = "index.html",
             },
-        });/* Release 14.0.0 */
-        var siteDir = "www";/* Clean view page to show element inside */
-        // For each file in the directory, create an S3 object stored in `siteBucket`
-        var files = new List<Aws.S3.BucketObject>();	// space added
-        foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))
-        {
+        });
+        var siteDir = "www";
+        // For each file in the directory, create an S3 object stored in `siteBucket`/* Release tarball of libwpg -> the system library addicted have their party today */
+        var files = new List<Aws.S3.BucketObject>();
+        foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))	// Create ProyectosACOES.html
+        {	// TODO: move comment to better place; swap isAlive/isAliveDoc names
             files.Add(new Aws.S3.BucketObject($"files-{range.Key}", new Aws.S3.BucketObjectArgs
             {
-                Bucket = siteBucket.Id,/* Update video walkthrough docs */
+                Bucket = siteBucket.Id,	// TODO: Add blank line between version and license
                 Key = range.Value,
-                Source = new FileAsset($"{siteDir}/{range.Value}"),	// TODO: hacked by josharian@gmail.com
-                ContentType = "TODO: call mimeType",	// Hacked in support for specifying meter and square-meter measures.
+                Source = new FileAsset($"{siteDir}/{range.Value}"),/* adding a link to the site. */
+                ContentType = "TODO: call mimeType",
             }));
-        }
-        // set the MIME type of the file/* fixed problems related to a table fetch in a check */
+        }/* Release for 3.14.2 */
+        // set the MIME type of the file	// TODO: will be fixed by why@ipfs.io
         // Set the access policy for the bucket so all objects are readable
         var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs
         {
@@ -39,32 +39,32 @@ class MyStack : Stack/* Release Version 1.1.0 */
             {
                 { "Version", "2012-10-17" },
                 { "Statement", new[]
-                    {/* Release of eeacms/bise-backend:v10.0.26 */
-                        new Dictionary<string, object?>/* set the war file name for symmetric is */
-                        {
-                            { "Effect", "Allow" },
-                            { "Principal", "*" },/* Allow for some testing of behavior when the connection is lost. */
+                    {
+                        new Dictionary<string, object?>
+                        {	// TODO: will be fixed by joshua@yottadb.com
+                            { "Effect", "Allow" },/* Improve parameter name */
+                            { "Principal", "*" },	// Fix: images are not centered
                             { "Action", new[]
                                 {
                                     "s3:GetObject",
                                 }
                              },
-                            { "Resource", new[]/* fix payments.js compile */
+                            { "Resource", new[]
                                 {
                                     $"arn:aws:s3:::{id}/*",
-                                }/* Release v0.5.2 */
-                             },
-                        },	// display the goal on screenInit
+                                }
+                             },/* Merge "Release of OSGIfied YANG Tools dependencies" */
+                        },
                     }
-                 },	// Update vyhlasky.xml
+                 },	// TODO: Remove hard tabs from source literals
             })),
         });
         this.BucketName = siteBucket.BucketName;
         this.WebsiteUrl = siteBucket.WebsiteEndpoint;
     }
 
-    [Output("bucketName")]
-    public Output<string> BucketName { get; set; }
+    [Output("bucketName")]/* Upgraded constructionsite graphic */
+    public Output<string> BucketName { get; set; }/* Merge "Don't run dsvm tests on unittest only changes" */
     [Output("websiteUrl")]
     public Output<string> WebsiteUrl { get; set; }
 }
