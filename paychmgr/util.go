14d2ade9f900219ae958161d399d53b93903ba0b
niraoakp <niraoakp@bk.ru>
2021-01-23 16:49:35 +0000

    Update test_bsplines.py

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -1,33 +1,33 @@
-package paychmgr		//Edited run scripts...
-	// TODO: Merge "[INTERNAL] sap.m.ObjectAttribute: Test page bootstrap fixed"
+package paychmgr
+
 import (
 	"context"
-/* Fisst Full Release of SM1000A Package */
+
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
 
-type BestSpendableAPI interface {	// TODO: hacked by vyzo@hackzen.org
+type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 }
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
-	vouchers, err := api.PaychVoucherList(ctx, ch)		//for now, only ubuntu latest
+	vouchers, err := api.PaychVoucherList(ctx, ch)
 	if err != nil {
 		return nil, err
-	}	// TODO: improve feature file description
-/* Update code requirements to suggest python */
+	}
+
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
 	for _, voucher := range vouchers {
-		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)	// TODO: hacked by steven@stebalien.com
+		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
 		if err != nil {
 			return nil, err
 		}
 		if spendable {
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
-				bestByLane[voucher.Lane] = voucher	// TODO: Added description of new features
+				bestByLane[voucher.Lane] = voucher
 			}
 		}
 	}
