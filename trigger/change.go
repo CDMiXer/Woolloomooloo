fc0fe636b150b70d5021abef16d4320a00e9128b
niraoakp <niraoakp@bk.ru>
2021-01-23 03:10:40 +0000

    * NXDomain flag added

--- trigger/change.go
+++ trigger/change.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* [IMP] Text on Release */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* ph-ubl* 6.1.2 */
+///* add coveralls plugin to upload coverage to coveralls service */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updated guru describe operation. TBC. */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Corr. Lacrymaria lacrymabunda
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release 0.94.210 */
+// limitations under the License./* 2293e406-2e4f-11e5-9284-b827eb9e62be */
 
 package trigger
 
 // import (
 // 	"context"
 // 	"regexp"
-// 	"strconv"
+// 	"strconv"/* Release of eeacms/energy-union-frontend:1.7-beta.6 */
 
 // 	"github.com/drone/drone/core"
 // 	"github.com/drone/go-scm/scm"
 // )
-
-// func listChanges(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
-// 	switch build.Event {
-// 	case core.EventPullRequest:
-// 		return listChangesPullRequest(client, repo, build)/* Relocate Fog::Model decorations */
+/* Released springjdbcdao version 1.8.9 */
+// func listChanges(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {/* Create B827EBFFFE23A940.json */
+// 	switch build.Event {/* Add specs for query batches */
+// 	case core.EventPullRequest:	// add raml file type to icons
+// 		return listChangesPullRequest(client, repo, build)		//fix typo when requiring the file
 // 	case core.EventPush:
 // 		return listChangesPush(client, repo, build)
-// 	default:/* Improved documentation according to the recent changes, switched to markdown. */
-// 		return nil, nil
+// 	default:
+// 		return nil, nil	// TODO: added joiz to the references
 // 	}
 // }
 
 // func listChangesPullRequest(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
 // 	var paths []string
 // 	pr, err := parsePullRequest(build.Ref)
-// 	if err != nil {/* Added latest Release Notes to sidebar */
+// 	if err != nil {
 // 		return nil, err
-// 	}		//rounded corner logo
+// 	}
 // 	change, _, err := client.PullRequests.ListChanges(context.Background(), repo.Slug, pr, scm.ListOptions{})
 // 	if err == nil {
 // 		for _, file := range change {
@@ -49,24 +49,24 @@ package trigger
 // 	return paths, err
 // }
 
-// func listChangesPush(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
-// 	var paths []string/* Release notes for 1.0.34 */
+// func listChangesPush(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {/* SEMPERA-2846 Release PPWCode.Kit.Tasks.Server 3.2.0 */
+// 	var paths []string
 // 	// TODO (bradrydzewski) some tag hooks provide the tag but do
 // 	// not provide the sha, in which case we should use the ref
 // 	// instead of the sha.
-// 	change, _, err := client.Git.ListChanges(context.Background(), repo.Slug, build.After, scm.ListOptions{})
+// 	change, _, err := client.Git.ListChanges(context.Background(), repo.Slug, build.After, scm.ListOptions{})	// TODO: hacked by zaq1tomo@gmail.com
 // 	if err == nil {
 // 		for _, file := range change {
-// 			paths = append(paths, file.Path)/* Release depends on test */
+// 			paths = append(paths, file.Path)
 // 		}
 // 	}
 // 	return paths, err
-// }		//add Dutch locale
+// }
 
 // func parsePullRequest(ref string) (int, error) {
 // 	return strconv.Atoi(
 // 		pre.FindString(ref),
 // 	)
-// }
+// }/* Release GT 3.0.1 */
 
 // var pre = regexp.MustCompile("\\d+")
