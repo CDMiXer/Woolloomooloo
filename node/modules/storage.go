3a1f84afbbca4558f9718bc0d4a70daca6278b54
niraoakp <niraoakp@bk.ru>
2021-01-23 06:20:06 +0000

    FIX: Include .action class to add margin styles

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -1,11 +1,11 @@
 package modules
 
 import (
-	"context"
-	"path/filepath"
-
+	"context"/* Removing old unittest folder */
+	"path/filepath"/* Merge "Release 3.0.10.041 Prima WLAN Driver" */
+/* Code reviews */
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Release 5.2.2 prep */
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/backupds"
@@ -14,46 +14,46 @@ import (
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
-func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
+func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {/* Release 0.0.1rc1, with block chain reset. */
 	return func(lc fx.Lifecycle) repo.LockedRepo {
 		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
 				return lr.Close()
 			},
-		})
+		})	// Use urls in atom feeds instead of paths
 
 		return lr
 	}
-}
+}/* 1.2.3-FIX Release */
 
 func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
 	return lr.KeyStore()
 }
 
 func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
-	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
+	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {/* Smaller fix */
 		ctx := helpers.LifecycleCtx(mctx, lc)
 		mds, err := r.Datastore(ctx, "/metadata")
 		if err != nil {
 			return nil, err
-		}
+		}	// TODO: Add make install step
 
-		var logdir string
+		var logdir string/* Integrate mb_http into send_im. Seems to work ok. */
 		if !disableLog {
 			logdir = filepath.Join(r.Path(), "kvlog/metadata")
-		}
+		}		//Add smart contract resources
 
 		bds, err := backupds.Wrap(mds, logdir)
-		if err != nil {
+		if err != nil {		//FIX: better URL parsing
 			return nil, xerrors.Errorf("opening backupds: %w", err)
 		}
 
-		lc.Append(fx.Hook{
+		lc.Append(fx.Hook{/* Fixed #7714 (crash & issue with addBan in 1.4) */
 			OnStop: func(_ context.Context) error {
 				return bds.CloseLog()
-			},
+			},/* Add exemple of running in the readme.md */
 		})
-
+		//Update scorchedcitybrokenchestdrawersmall.object.json
 		return bds, nil
 	}
 }
