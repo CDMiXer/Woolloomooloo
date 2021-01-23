9c8bd707a1fd26aee33cbe2d65cb6389af18f39b
niraoakp <niraoakp@bk.ru>
2021-01-23 21:54:48 +0000

    correct git comments

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by sbrichards@gmail.com
+
 // +build !oss
 
 package pubsub
 
-import (/* Released oVirt 3.6.4 */
+import (
 	"context"
-	"sync"	// TODO: corrige margin dos labels nas atividades
+	"sync"
 	"testing"
 
-	"github.com/drone/drone/core"		//a3aaa4f0-2e6b-11e5-9284-b827eb9e62be
-)/* Merge "Release 4.0.10.42 QCACLD WLAN Driver" */
+	"github.com/drone/drone/core"
+)
 
 func TestBus(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -26,7 +26,7 @@ func TestBus(t *testing.T) {
 	}
 
 	w := sync.WaitGroup{}
-	w.Add(1)/* [IMP] removing some board stuff */
+	w.Add(1)
 	go func() {
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
@@ -34,7 +34,7 @@ func TestBus(t *testing.T) {
 		w.Done()
 	}()
 	w.Wait()
-/* Release for 24.11.0 */
+
 	w.Add(3)
 	go func() {
 		for {
