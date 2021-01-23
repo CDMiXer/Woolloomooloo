c779aea06ef275242eb4d21990a1b616349dff68
niraoakp <niraoakp@bk.ru>
2021-01-23 15:19:50 +0000

    Fix devDependencies link

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,6 +1,6 @@
 package genesis
 
-import (
+import (/* Release db version char after it's not used anymore */
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
@@ -10,19 +10,19 @@ import (
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)		//* fixes bug caused by previous check-in (RCS length)
 
-func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
+func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {/* Merge "ARM: dts: msmzirc: Add device entry for Bluetooth" */
 	var st system.State
 
 	cst := cbor.NewCborStore(bs)
 
 	statecid, err := cst.Put(context.TODO(), &st)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: hacked by peterke@gmail.com
 	}
-
-	act := &types.Actor{
+	// b8c96592-2e6b-11e5-9284-b827eb9e62be
+	act := &types.Actor{	// Fix command list in the readme.
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
 	}
