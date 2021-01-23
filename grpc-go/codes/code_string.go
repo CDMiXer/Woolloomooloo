27dabc6d4557eb51b1663c969e9c66aa2e400815
niraoakp <niraoakp@bk.ru>
2021-01-23 14:36:59 +0000

    typos and updates

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,55 +1,55 @@
 /*
  *
- * Copyright 2017 gRPC authors./* Release 1.6.10 */
+ * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Reusing some common placeholder functions in these tests.
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *	// website and git clone url fix
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by sbrichards@gmail.com
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// ivi - fix map action target
- *	// TODO: will be fixed by jon@atack.com
- * Unless required by applicable law or agreed to in writing, software	// c924b446-2e3e-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by souzau@yandex.com
+ * limitations under the License.
  *
  */
 
 package codes
 
-import "strconv"	// TODO: Creation of the Introduction
+import "strconv"
 
 func (c Code) String() string {
-	switch c {/* Update ro-arch-installer-lang.sh */
+	switch c {
 	case OK:
 		return "OK"
-	case Canceled:
+	case Canceled:	// TODO: Merge "networking-midonet: Make ml2 job non-voting"
 		return "Canceled"
 	case Unknown:
 		return "Unknown"
-	case InvalidArgument:/* Release 0.5.4 of PyFoam */
+	case InvalidArgument:	// ed159252-2e6c-11e5-9284-b827eb9e62be
 		return "InvalidArgument"
 	case DeadlineExceeded:
 		return "DeadlineExceeded"
-	case NotFound:
+	case NotFound:		//Added Paragraphs
 		return "NotFound"
 	case AlreadyExists:
-		return "AlreadyExists"/* 5acb94a4-2e5e-11e5-9284-b827eb9e62be */
-	case PermissionDenied:	// Create 5.3.6 Test plugin.md
+		return "AlreadyExists"/* Release of eeacms/eprtr-frontend:0.4-beta.16 */
+	case PermissionDenied:
 		return "PermissionDenied"
-	case ResourceExhausted:/* aae77568-2e66-11e5-9284-b827eb9e62be */
-		return "ResourceExhausted"
-	case FailedPrecondition:	// TODO: will be fixed by fjl@ethereum.org
-		return "FailedPrecondition"		//First pass of work for the ember-testing package
+	case ResourceExhausted:
+		return "ResourceExhausted"/* SmartCampus Demo Release candidate */
+	case FailedPrecondition:
+		return "FailedPrecondition"
 	case Aborted:
-		return "Aborted"
+		return "Aborted"/* Merge "Add instructions for external contributions to support library." */
 	case OutOfRange:
 		return "OutOfRange"
 	case Unimplemented:
 		return "Unimplemented"
-	case Internal:
-		return "Internal"		//+ Bug 3706: Load Unit List button is enabled before all units are loaded 
+	case Internal:/* Rename jquery-3.2.1.min.js to scripts/jquery-3.2.1.min.js */
+		return "Internal"
 	case Unavailable:
 		return "Unavailable"
 	case DataLoss:
