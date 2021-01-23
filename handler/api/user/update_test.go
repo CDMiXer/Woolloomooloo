42420d0ffc04c10e32b57c5b5748af80451d49e2
niraoakp <niraoakp@bk.ru>
2021-01-23 15:43:39 +0000

    Resolving relative PATH mistakes

--- handler/api/user/update_test.go
+++ handler/api/user/update_test.go
@@ -2,40 +2,40 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package user
+package user	// Merge branch 'master' into feature/config
 
 import (
 	"bytes"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-
+/* Update readme.txt for using loadjsc from 9900 */
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* ar71xx: export SoC revision */
 	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* f915dfd6-2e4d-11e5-9284-b827eb9e62be */
 func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	userInput := &core.User{
-		Login: "octocat",
+		Login: "octocat",		//order_books are protected
 		Email: "octocat@github.com",
 	}
 	user := &core.User{
 		Login: "octocat",
 		Email: "",
-	}
+	}		//Merge "[config-ref] Migrate ceph-rbd-volume-driver.xml to rst"
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Update(gomock.Any(), user)
 
-	in := new(bytes.Buffer)
+	in := new(bytes.Buffer)/* Release 3.1.2 */
 	json.NewEncoder(in).Encode(userInput)
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/api/user", in)
@@ -47,11 +47,11 @@ func TestUpdate(t *testing.T) {
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* Add H&P to readme */
 	if got, want := user.Email, "octocat@github.com"; got != want {
 		t.Errorf("Want user email %v, got %v", want, got)
 	}
-
+/* Release Notes for v00-16-02 */
 	got, want := new(core.User), user
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
@@ -70,7 +70,7 @@ func TestUpdate_BadRequest(t *testing.T) {
 		ID:    1,
 		Login: "octocat",
 	}
-
+		//u2/ut2003/ut2004: .psk -> .obj operation (no longer need Blender)
 	in := new(bytes.Buffer)
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/api/user", in)
@@ -81,7 +81,7 @@ func TestUpdate_BadRequest(t *testing.T) {
 	HandleUpdate(nil)(w, r)
 	if got, want := w.Code, 400; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* Update gatk.groovy */
 
 	got, want := new(errors.Error), &errors.Error{Message: "EOF"}
 	json.NewDecoder(w.Body).Decode(got)
@@ -89,20 +89,20 @@ func TestUpdate_BadRequest(t *testing.T) {
 		t.Errorf(diff)
 	}
 }
-
+		//move post processing to a separate module
 // the purpose of this unit test is to verify that an error
 // updating the database will result in an internal server
 // error returned to the client.
 func TestUpdate_ServerError(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()	// TODO: added images to readme to show image clean-up.
+/* Fixes for duplicate listings; Django Export: update lambda for Python3 */
 	userInput := &core.User{
-		Login: "octocat",
+		Login: "octocat",/* [artifactory-release] Release version 1.2.0.RELEASE */
 		Email: "octocat@github.com",
 	}
 	user := &core.User{
-		Login: "octocat",
+		Login: "octocat",/* XSurf First Release */
 		Email: "",
 	}
 
