117c1a3af4cd59ed6240d6292e66bf5ab7f8a820
niraoakp <niraoakp@bk.ru>
2021-01-23 19:52:27 +0000

    fix for maps.getKeys + test

--- tests/integration/duplicate_urns/duplicate_urns_test.go
+++ tests/integration/duplicate_urns/duplicate_urns_test.go
@@ -1,48 +1,48 @@
 // Copyright 2016-2018, Pulumi Corporation.
-// +build nodejs all/* Bugfix-Release 3.3.1 */
+// +build nodejs all
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Fixed some visiblity errors!
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release 2.14 */
 //
-// Unless required by applicable law or agreed to in writing, software/* Release version [10.4.5] - alfter build */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Bring more out from the view */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* 901b5166-2e46-11e5-9284-b827eb9e62be */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package ints
-	// TODO: Merge branch 'develop' into feature/product-page--fresh-branch
+
 import (
-	"testing"
+	"testing"		//Create About.pdf
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* Merge "Fedora link is too old and so updated with newer version" */
+
 // Test that the engine does not tolerate duplicate URNs in the same plan.
 func TestDuplicateURNs(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Reformat in github style */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
 		Dependencies:  []string{"@pulumi/pulumi"},
 		Quick:         true,
-		ExpectFailure: true,/* Fix typo in PointerReleasedEventMessage */
+		ExpectFailure: true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,/* 2940db94-2e48-11e5-9284-b827eb9e62be */
-			},
+				Additive: true,/* adding argentina and portugal to the list of supported countries */
+			},	// TODO: Some tests, not all implemented yet
 			{
 				Dir:           "step3",
-				Additive:      true,
+				Additive:      true,	// updated README and CHANGES file, and other files added
 				ExpectFailure: true,
 			},
 			{
-				Dir:           "step4",/* Finish hors forfait */
-				Additive:      true,		//Merge branch 'master' into fix/r-undefined
+				Dir:           "step4",
+				Additive:      true,
 				ExpectFailure: true,
 			},
 		},
 	})
-}		//Creating a procedure goes to edit page
+}
