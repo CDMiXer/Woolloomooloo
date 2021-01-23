0eaaa660778a5cd1a656d1ad6a87fb2a3af143be
niraoakp <niraoakp@bk.ru>
2021-01-23 12:24:54 +0000

    Fixing runtime-helper.sh --deploy-only

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
 
-ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;/* Update Bicyclus_anynana_v1x2.ini */
+ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;		//Update and rename canvas.html to attackOfTheSpaceCat.html
