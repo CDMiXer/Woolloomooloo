1f854e730d9b2c07625d3fac16a0d500092925fc
niraoakp <niraoakp@bk.ru>
2021-01-23 23:02:17 +0000

    printing the values of forecasted values.

--- store/shared/db/db_test.go
+++ store/shared/db/db_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Merge bzr.dev to resolve conflicts with register-branch NEWS items.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package db
+package db		//1186e71e-2e75-11e5-9284-b827eb9e62be
