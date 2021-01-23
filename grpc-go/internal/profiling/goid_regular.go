ccbdcb6c8e6cc16aeb21430f344fcf438a367f36
niraoakp <niraoakp@bk.ru>
2021-01-23 14:17:45 +0000

    Rename to yesparql.jena to yesparql.tdb

--- grpc-go/internal/profiling/goid_regular.go
+++ grpc-go/internal/profiling/goid_regular.go
@@ -1,24 +1,24 @@
 // +build !grpcgoid
 
-/*/* 20c9f7e2-2e53-11e5-9284-b827eb9e62be */
+/*
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//added wincache support for php 5.5 and default php version update
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	//  tensorflow/tpu
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Test for latest issue in #2208
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Version is updated
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package profiling/* Merge "Adding svn for file download." */
+package profiling
 
 // This dummy function always returns 0. In some modified dev environments,
 // this may be replaced with a call to a function in a modified Go runtime that
