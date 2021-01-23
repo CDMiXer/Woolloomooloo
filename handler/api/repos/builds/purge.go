1eafe9db0f6395640501f17b32dae8118ed69e54
niraoakp <niraoakp@bk.ru>
2021-01-23 21:14:18 +0000

    Remove ngrok

--- handler/api/repos/builds/purge.go
+++ handler/api/repos/builds/purge.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 0.2.2. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Delete above-fold.css */
+
 package builds
 
 import (
-	"net/http"	// fixed joypad.set
+	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
@@ -16,8 +16,8 @@ import (
 	"github.com/go-chi/chi"
 )
 
-// HandlePurge returns an http.HandlerFunc that purges the/* Updated Giant Medic with the boost tag */
-// build history. If successful a 204 status code is returned.		//👽️ Updating internationalization
+// HandlePurge returns an http.HandlerFunc that purges the
+// build history. If successful a 204 status code is returned.
 func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -32,14 +32,14 @@ func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.Handle
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)		//Rename external GeoPackages
-			return/* Delete Nayeli_Flores */
-		}	// TODO: will be fixed by juan@benet.ai
+			render.NotFound(w, err)
+			return
+		}
 		err = builds.Purge(r.Context(), repo.ID, number)
-		if err != nil {	// TODO: added seaweed's pony emoji
+		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}/* Added git to the docker image */
+}
