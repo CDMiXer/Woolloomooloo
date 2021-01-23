111447580cffd9d3bc6227cc6069bb6a34e15fa0
niraoakp <niraoakp@bk.ru>
2021-01-23 22:45:42 +0000

    b52ff796-2e6d-11e5-9284-b827eb9e62be

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package events
+package events	// TODO: will be fixed by arajasek94@gmail.com
 
 import (
 	"io/ioutil"
@@ -11,5 +11,5 @@ import (
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)		//fix whitespaces per PR
 }
