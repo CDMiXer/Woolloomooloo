7804e9a02928971efb10999cc8d8446a7ad39f14
niraoakp <niraoakp@bk.ru>
2021-01-23 14:13:13 +0000

    New translations en-GB.plg_finder_sermonspeaker.ini (Chinese Traditional)

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -1,14 +1,14 @@
 // +build !appengine,go1.14
 
 /*
- */* Add Releases */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* V1.1 --->  V1.2 Release */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by nick@perfectabstractions.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,37 +16,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-	// Delete env_cube_nx.png
+ *//* Added acknowledgments to the NSF */
+
 package advancedtls
 
 import (
 	"crypto/tls"
 	"fmt"
-)	// TODO: hacked by nicksavers@gmail.com
-/* Print correct std theta  */
+)
+
 // buildGetCertificates returns the certificate that matches the SNI field
-// for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.		//FreenetKnowledge - Libraries upgrades
+// for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {/* Namespacing specs */
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
-		return nil, err/* Release notes and JMA User Guide */
-	}	// TODO: install tasks created. cleanedup events to get more control.
-	if len(certificates) == 0 {	// Small fixes for bloom.patch.patch_main
+		return nil, err/* 893fdb84-2e5d-11e5-9284-b827eb9e62be */
+	}
+	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
 	}
 	// If users pass in only one certificate, return that certificate.
 	if len(certificates) == 1 {
 		return certificates[0], nil
 	}
-	// Choose the SNI certificate using SupportsCertificate.	// TODO: Removed duplicate createTeam method
-	for _, cert := range certificates {	// branch copying, partially working
+	// Choose the SNI certificate using SupportsCertificate.
+	for _, cert := range certificates {
 		if err := clientHello.SupportsCertificate(cert); err == nil {
 			return cert, nil
-		}
+		}/* Release v0.1.3 with signed gem */
 	}
 	// If nothing matches, return the first certificate.
 	return certificates[0], nil
