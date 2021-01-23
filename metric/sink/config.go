137a47379272375d75b2d77f9e548dc9bab80a22
niraoakp <niraoakp@bk.ru>
2021-01-23 13:21:11 +0000

    added appveyor

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* #195 fix url and click selector */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Make the Xml config split to an extension, stage 05 - move the DAOs
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Add converters for remaining classes in java.time. */
-///* [ASan] Use less shadow on Win 32-bit */
+// You may obtain a copy of the License at/* Add some Release Notes for upcoming version */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by davidad@alum.mit.edu
+// Unless required by applicable law or agreed to in writing, software/* Update URLHelper.php */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by witek@enjin.io
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package sink
 
-// Config configures a Datadog sink./* Added 18367789543 8ac09ffaee O */
-type Config struct {
-	Endpoint string	// 66a6fb02-2fbb-11e5-9f8c-64700227155b
+// Config configures a Datadog sink.
+type Config struct {		//add sqrt, rational_number, e2_1
+	Endpoint string		//Renamed LCOMPreferencePage to CohesionPreferencePage
 	Token    string
-
+		//#12 font-size: 14px;  #126 @media(min-width:650);
 	License          string
 	Licensor         string
 	Subscription     string
@@ -29,7 +29,7 @@ type Config struct {
 	EnableStash      bool
 	EnableGogs       bool
 	EnableGitea      bool
-	EnableAgents     bool/* Checkin for Release 0.0.1 */
+	EnableAgents     bool
 	EnableNomad      bool
-	EnableKubernetes bool/*  DirectXTK: Fix for EffectFactory::ReleaseCache() */
+	EnableKubernetes bool
 }
