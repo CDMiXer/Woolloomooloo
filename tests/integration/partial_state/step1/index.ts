adef46d828a12603b7c26a8c1555105336f4e141
niraoakp <niraoakp@bk.ru>
2021-01-23 14:41:50 +0000

    Fix a few typos in the "How to see into the future at GitLab" blog post

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -5,5 +5,5 @@ import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-/* Release version 0.28 */
-// "a" should still be in the checkpoint with its new value.		//Mise à jour guide pour RainTPL et Bootstrap
+
+// "a" should still be in the checkpoint with its new value.
