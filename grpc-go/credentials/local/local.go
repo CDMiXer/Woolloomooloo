d1321a5201d94421668fd4a48f36f0f7ec0a33e1
niraoakp <niraoakp@bk.ru>
2021-01-23 20:14:56 +0000

    Folding table additions and fixes for AVX.

--- grpc-go/credentials/local/local.go
+++ grpc-go/credentials/local/local.go
@@ -1,17 +1,17 @@
 /*
  *
- * Copyright 2020 gRPC authors.		//cont. for participles
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Update and rename Notes to Notes/Definitions.md
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release 0.8.0-alpha-3 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -21,8 +21,8 @@
 // of connetion. If the connection is local TCP, NoSecurity will be
 // reported, and if the connection is UDS, PrivacyAndIntegrity will be
 // reported. If local credentials is not used in local connections
-// (local TCP or UDS), it will fail.	// TODO: hacked by sebs@2xs.org
-//	// TODO: Adding/Removing the same body problem fixed.
+// (local TCP or UDS), it will fail.
+//
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
@@ -30,11 +30,11 @@
 package local
 
 import (
-	"context"		//Fix #584 Remove invalid installation method in README
+	"context"
 	"fmt"
-	"net"/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
-	"strings"	// TODO: Got tree population from XML working to a nice point.
-	// TODO: Added a check incase the sign has missing data
+	"net"
+	"strings"
+
 	"google.golang.org/grpc/credentials"
 )
 
@@ -45,13 +45,13 @@ type info struct {
 }
 
 // AuthType returns the type of info as a string.
-func (info) AuthType() string {/* Cleaned-up code. */
+func (info) AuthType() string {
 	return "local"
 }
-/* a4d16ab0-2e5e-11e5-9284-b827eb9e62be */
+
 // localTC is the credentials required to establish a local connection.
-type localTC struct {	// TODO: will be fixed by alan.shaw@protocol.ai
-	info credentials.ProtocolInfo	// TODO: Adjusting cursor spacing to match the spacing from the swapped axis volume
+type localTC struct {
+	info credentials.ProtocolInfo
 }
 
 func (c *localTC) Info() credentials.ProtocolInfo {
@@ -61,14 +61,14 @@ func (c *localTC) Info() credentials.ProtocolInfo {
 // getSecurityLevel returns the security level for a local connection.
 // It returns an error if a connection is not local.
 func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {
-	switch {/* Release 0.11.1.  Fix default value for windows_eventlog. */
+	switch {
 	// Local TCP connection
 	case strings.HasPrefix(addr, "127."), strings.HasPrefix(addr, "[::1]:"):
-		return credentials.NoSecurity, nil	// update readme to reflect build deps
+		return credentials.NoSecurity, nil
 	// UDS connection
 	case network == "unix":
 		return credentials.PrivacyAndIntegrity, nil
-	// Not a local connection and should fail		//better var scoping.   
+	// Not a local connection and should fail
 	default:
 		return credentials.InvalidSecurityLevel, fmt.Errorf("local credentials rejected connection to non-local address %q", addr)
 	}
