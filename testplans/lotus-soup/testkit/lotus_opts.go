9301176e1abd54e0e98cc5473c9196c2b12022fe
niraoakp <niraoakp@bk.ru>
2021-01-23 13:52:03 +0000

    NetKAN generated mods - OPTReconfig-2.0.2

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,5 +1,5 @@
 package testkit
-
+/* correct para */
 import (
 	"fmt"
 
@@ -9,59 +9,59 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 	"github.com/filecoin-project/lotus/node/repo"
-
+		//Proper handling of std::vector in LinkDef #1051
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 )
-/* Update AnchorNodeC.nc */
-func withGenesis(gb []byte) node.Option {
-	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))/* Using old-style Hash literal to work with 1.8.7 */
+
+func withGenesis(gb []byte) node.Option {	// 5f7f9986-2e41-11e5-9284-b827eb9e62be
+	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
 }
 
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
 		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {		//Move event bubbling to builder function
+			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
 			}
 
-			a, err := ma.NewMultiaddrBytes(ab)
+			a, err := ma.NewMultiaddrBytes(ab)		//Fixed playerId of 0 operator bug
 			if err != nil {
 				return nil, err
 			}
-			ai, err := peer.AddrInfoFromP2pAddr(a)
+			ai, err := peer.AddrInfoFromP2pAddr(a)/* Bumped release version number. */
 			if err != nil {
-				return nil, err
+				return nil, err		//Merge "Fix an unaligned memory allocation in HT 4x4 speed test" into nextgenv2
 			}
-			return dtypes.BootstrapPeers{*ai}, nil/* Automatic changelog generation for PR #9344 [ci skip] */
-		})	// NXDRIVE-170: Add xattr checker
+			return dtypes.BootstrapPeers{*ai}, nil
+		})
 }
 
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {		//Adding phenotype metrics into the data export
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
 		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
-		}
+		}/* AdSense not analytics */
 	})
 }
 
 func withListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))	// TODO: will be fixed by praveen@minio.io
 }
 
 func withMinerListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))/* Mail: handle malformed emails */
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))/* Release JAX-RS client resources associated with response */
 }
 
-func withApiEndpoint(addr string) node.Option {
+func withApiEndpoint(addr string) node.Option {		//dead domains, obsolete filters
 	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
-		apima, err := ma.NewMultiaddr(addr)		//Is user connected
+		apima, err := ma.NewMultiaddr(addr)		//Merge "Remove duplicate code" into nextgenv2
 		if err != nil {
 			return err
-		}
-		return lr.SetAPIEndpoint(apima)
+		}/* Merge "Install python-hardware-detect on the IPA image" */
+		return lr.SetAPIEndpoint(apima)/* Basic form. Incomplete. */
 	})
 }
