f0aeeb39384a202c9ba6a4846c164c1f44002a1b
niraoakp <niraoakp@bk.ru>
2021-01-23 08:50:27 +0000

    see, was that so hard?

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -1,11 +1,11 @@
 -- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_namespace         VARCHAR(50)
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT	// TODO: hacked by why@ipfs.io
+,secret_namespace         VARCHAR(50)/* - Lisää muutoksia */
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BLOB
+,secret_data              BLOB		//Port to play 2.3
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
