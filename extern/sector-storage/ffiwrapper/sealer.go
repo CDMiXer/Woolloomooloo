596c898e9e3f1d77d6e87ef80e96c4035c3aac3b
niraoakp <niraoakp@bk.ru>
2021-01-23 02:53:35 +0000

    60b23108-35c6-11e5-9c0b-6c40088e03e4

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,15 +1,15 @@
 package ffiwrapper
 
 import (
-	logging "github.com/ipfs/go-log/v2"	// TODO: 19fd7c47-2d3d-11e5-8e5a-c82a142b6f9b
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("ffiwrapper")
-/* Update histogram.html */
+
 type Sealer struct {
-	sectors  SectorProvider		//update only after begin and end of phase
+	sectors  SectorProvider
 	stopping chan struct{}
-}	// TODO: Update mongo-connection protocol
+}
 
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
