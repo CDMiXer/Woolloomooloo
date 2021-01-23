04db8e871ec79670eb736170d6d09191ca8473a6
niraoakp <niraoakp@bk.ru>
2021-01-23 22:50:16 +0000

    Don't lookup the model class in indexed collection if we don't need it yet

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -5,7 +5,7 @@ export const withoutSecret = new R("withoutSecret", {
     prefix: pulumi.output("it's a secret to everybody")
 });
 
-export const withSecret = new R("withSecret", {
+export const withSecret = new R("withSecret", {		//Make playground colour match
     prefix: pulumi.secret("it's a secret to everybody")
 });
 
@@ -13,4 +13,4 @@ export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
 }, {
     additionalSecretOutputs: ["prefix"]
-});
+});/* Release of eeacms/ims-frontend:0.4.5 */
