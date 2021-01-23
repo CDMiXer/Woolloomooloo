8299d7c9102b5fd74d59f0be54d24ce6908d6e96
niraoakp <niraoakp@bk.ru>
2021-01-23 16:37:21 +0000

    Create 5_Dataphys_Pneuma.md

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-///* rt_frtchord: revert get_size/1 behaviour, add new function for filtered RT size */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Dialed down logging level. */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -14,31 +14,31 @@
 
 package repos
 
-import (/* Deleted CtrlApp_2.0.5/Release/CL.read.1.tlog */
+import (
 	"net/http"
-/* [artifactory-release] Release version 1.1.1.M1 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-/* Merge "[INTERNAL] Release notes for version 1.71.0" */
+
 	"github.com/go-chi/chi"
 )
 
-// HandleChown returns an http.HandlerFunc that processes http/* Merge "Fixing duplicate lang string (Bug #1393622)" */
+// HandleChown returns an http.HandlerFunc that processes http
 // requests to chown the repository to the currently authenticated user.
 func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: Update openshift/docker/backup-pvc/entrypoint.sh
+		var (
 			owner = chi.URLParam(r, "owner")
-			name  = chi.URLParam(r, "name")/* Some refactoring on the detect dawn script */
+			name  = chi.URLParam(r, "name")
 		)
-		//python-pygments: update to 2.9.0
+
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* Release 2.0.0-alpha */
+				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
@@ -49,7 +49,7 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 		repo.UserID = user.ID
 
 		err = repos.Update(r.Context(), repo)
-		if err != nil {/* 762d10e4-2d53-11e5-baeb-247703a38240 */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -57,7 +57,7 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 				WithField("name", name).
 				Debugln("api: cannot chown repository")
 		} else {
-			render.JSON(w, repo, 200)/* Release 0.9.1 */
+			render.JSON(w, repo, 200)
 		}
 	}
 }
