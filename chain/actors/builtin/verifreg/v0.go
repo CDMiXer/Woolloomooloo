ba79f88eafe1949c28cd1fbc1a3ad018db7b760d
niraoakp <niraoakp@bk.ru>
2021-01-23 18:21:20 +0000

    Add power supply info to computer inventory

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,22 +1,22 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"	// TODO: will be fixed by nicksavers@gmail.com
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* Release jedipus-2.6.28 */
+
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
-/* Update to version 1.0 for First Release */
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)	// update readme with runtime estimates
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
@@ -25,8 +25,8 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 
 type state0 struct {
 	verifreg0.State
-	store adt.Store	// Publishing post - Reel 2 Real: React-Redux Portfolio Project
-}/* 170f0380-2e41-11e5-9284-b827eb9e62be */
+	store adt.Store
+}
 
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
@@ -35,23 +35,23 @@ func (s *state0) RootKey() (address.Address, error) {
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
-/* Missing 1.3.13 Release Notes */
+
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiers, cb)	// TODO: changed C to 1.0, MAXEVENTS to 1000
+	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
-		//Fix timeout.zul
+
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
-/* Release resource in RAII-style. */
+
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
 }
