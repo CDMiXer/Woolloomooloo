a7dbbea9a5203bdd205ef000735157c3dc9a2c84
niraoakp <niraoakp@bk.ru>
2021-01-23 13:40:17 +0000

    Prerefactoring.

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Xinerama screen switching bugfix */
+// Use of this source code is governed by the Drone Non-Commercial License/* typo in modulefile */
+// that can be found in the LICENSE file.
 
 package deploys
