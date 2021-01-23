251e6f01547d95cfbe36cc07513b8938b669c4cf
niraoakp <niraoakp@bk.ru>
2021-01-23 09:02:18 +0000

    add support for emulator when using a datasource

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -1,65 +1,65 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Starting thw web block problem
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: hacked by cory@protocol.ai
 
 package secrets
 
-import (
+import (/* [1.1.15] Release */
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Merge "Release 3.0.0" into stable/havana */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-		//Update import common
+
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-	// TODO: get exec line from dekstop file
+
 func TestHandleAll(t *testing.T) {
-	controller := gomock.NewController(t)/* don't create exe files */
-	defer controller.Finish()		//Replacing ProgressUnitPlugin by ViewPlugin
-
-	secrets := mock.NewMockGlobalSecretStore(controller)		//Merge "[devstack] Remove all the dashboard config files"
-	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-
-	HandleAll(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {/* Delete qweigw4hw */
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	got, want := []*core.Secret{}, dummySecretListScrubbed
-	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* generic argument changes */
-		t.Errorf(diff)
-	}
-}
-/* Release 0.5 */
-func TestHandleAll_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)
+	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)		//HTML updates to layout (for now), including navigation bars
-
+	r := httptest.NewRequest("GET", "/", nil)
+/* Releases for everything! */
 	HandleAll(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {/* Merge "Release 3.2.3.343 Prima WLAN Driver" */
+	if got, want := w.Code, http.StatusOK; want != got {/* Create GameStateManager.java */
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: Not sure why it ever said state.actions, that wasn't the intention
-
-	got, want := new(errors.Error), errors.ErrNotFound	// TODO: update asker to 0.4.5
-	json.NewDecoder(w.Body).Decode(got)/* Release v0.4.1 */
+	}
+		//0e38224e-2e6b-11e5-9284-b827eb9e62be
+	got, want := []*core.Secret{}, dummySecretListScrubbed
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Merge "Release 3.2.3.349 Prima WLAN Driver" */
+		t.Errorf(diff)/* Create lint.css */
+	}
+}/* Fix [ 1790986 ] Bug while importing previous settings(Filezilla.xml) */
+
+func TestHandleAll_SecretListErr(t *testing.T) {
+	controller := gomock.NewController(t)/* Initial Release ( v-1.0 ) */
+	defer controller.Finish()/* Release notes and version bump 2.0 */
+
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)
+
+	w := httptest.NewRecorder()		//l7trmFMQcDGEO8EYkHEsMNPkKZe3VfLw
+	r := httptest.NewRequest("GET", "/", nil)
+
+	HandleAll(secrets).ServeHTTP(w, r)/* Release of eeacms/forests-frontend:1.5.9 */
+	if got, want := w.Code, http.StatusNotFound; want != got {/* Rename getTeam to getReleasegroup, use the same naming everywhere */
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
 	}
 }
