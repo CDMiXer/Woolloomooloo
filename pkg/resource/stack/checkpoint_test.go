28eb4d5be2354a713d630895fa7d9ebc29202300
niraoakp <niraoakp@bk.ru>
2021-01-23 12:18:00 +0000

    Make the run_start goal build the project first.

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -2,41 +2,41 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by lexy8russo@outlook.com
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Add utility method getParameter to SwaggerUtil
-// Unless required by applicable law or agreed to in writing, software/* cleanup & synchronization between all languages */
+///* Modified pom to allow snapshot UX releases via the Maven Release plugin */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Change format to set route from URL
 // limitations under the License.
 
 package stack
 
 import (
-	"io/ioutil"
-	"testing"
-
+	"io/ioutil"	// TODO: hacked by julia@jvns.ca
+	"testing"	// TODO: More NEAT separation
+	// dont over service name
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadV0Checkpoint(t *testing.T) {		//Updating build-info/dotnet/coreclr/master for preview-27109-03
+func TestLoadV0Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
 	assert.NoError(t, err)
-
+	// TODO: Add support for getting the name of STRING_CST
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
-	assert.NoError(t, err)/* Merge "Bug 1829943: Release submitted portfolios when deleting an institution" */
-	assert.NotNil(t, chk.Latest)
+	assert.NoError(t, err)
+	assert.NotNil(t, chk.Latest)/* Create Release Notes */
 	assert.Len(t, chk.Latest.Resources, 30)
-}
-	// TODO: Merge "Remove neutronclient mocks from network delete tests"
-func TestLoadV1Checkpoint(t *testing.T) {	// Merge "Add service of ns scale"
+}		//Delete DnDNext.json
+
+func TestLoadV1Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
 	assert.NoError(t, err)
 
-	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)/* Release 1.0.1 final */
+	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)		//3c21a214-2e40-11e5-9284-b827eb9e62be
+	assert.Len(t, chk.Latest.Resources, 30)
 }
