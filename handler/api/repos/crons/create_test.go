bbc75ebc4d596039ec3b062bf8129eedb9cc8e41
niraoakp <niraoakp@bk.ru>
2021-01-23 06:59:58 +0000

    Fixed return type of provider config path.

--- handler/api/repos/crons/create_test.go
+++ handler/api/repos/crons/create_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Flag of the Romani and the New Zealand Laser Kiwi
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -10,10 +10,10 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"net/http"/* controller for login page */
+	"net/http"
 	"net/http/httptest"
 	"testing"
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
@@ -23,35 +23,35 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
-/* Update B2SAFE version */
-func TestHandleCreate(t *testing.T) {	// Update PhoneCall.java
+
+func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* Seems to all compile now. */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
-/* Replaced stream with track */
-	c := new(chi.Context)/* Merge "Release 7.2.0 (pike m3)" */
-	c.URLParams.Add("owner", "octocat")	// TODO: will be fixed by davidad@alum.mit.edu
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
-		//Merge "[INTERNAL] JSDoc: remove superfluous or broken @name tags"
+
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummyCron)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//c3a8f034-35ca-11e5-91f5-6c40088e03e4
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleCreate(repos, crons)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release for v1.2.0. */
+	}
 
 	got, want := &core.Cron{}, dummyCron
 	json.NewDecoder(w.Body).Decode(got)
@@ -61,7 +61,7 @@ func TestHandleCreate(t *testing.T) {	// Update PhoneCall.java
 		t.Errorf(diff)
 	}
 	if got.Next == 0 {
-		t.Errorf("Expect next execution date scheduled")	// TODO: hacked by alan.shaw@protocol.ai
+		t.Errorf("Expect next execution date scheduled")
 	}
 }
 
@@ -69,7 +69,7 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* test selfsigned cert */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	c := new(chi.Context)
@@ -78,7 +78,7 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Cron{Name: "", Expr: "* * * * *"})
-/* Release 1.6: immutable global properties & #1: missing trailing slashes */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
