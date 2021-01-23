c3c6f14f005e966391946601cbba2867900e8394
niraoakp <niraoakp@bk.ru>
2021-01-23 17:53:15 +0000

    Weight Reducer bug

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Changed the SDK version to the March Release. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
