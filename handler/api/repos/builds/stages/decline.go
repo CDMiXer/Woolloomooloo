72bf0af01a27a66728f80768b6df4debcbf219d4
niraoakp <niraoakp@bk.ru>
2021-01-23 00:56:47 +0000

    renamed JsonWriter to JsonExport

--- handler/api/repos/builds/stages/decline.go
+++ handler/api/repos/builds/stages/decline.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* d4812946-2e75-11e5-9284-b827eb9e62be */
+//	// Add a super basic test for large file mode
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,26 +10,26 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Added test cases for DataNode and fixed occurred bugs;
 
 package stages
-
+	// Code highlight style
 import (
-	"fmt"
+	"fmt"/* Add formatUsedMemory function */
 	"net/http"
 	"strconv"
-
+		//Fixed wrong date (1)
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"		//update javadocs to ESAPI 2.0 rc3 (without tests)
 )
 
 // HandleDecline returns an http.HandlerFunc that processes http
 // requests to decline a blocked build that is pending review.
 func HandleDecline(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,		//Fixing #172: OriginalCalledNumberCap CAP message
 	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -39,23 +39,23 @@ func HandleDecline(
 		)
 		buildNumber, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequestf(w, "Invalid build number")
+			render.BadRequestf(w, "Invalid build number")/* Release of version 1.2.2 */
 			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
-			render.BadRequestf(w, "Invalid stage number")
-			return
+			render.BadRequestf(w, "Invalid stage number")/* Release of eeacms/plonesaas:5.2.1-5 */
+			return		//Enhanced message.
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Fixing issue https://github.com/ukwa/w3act/issues/41 */
 		if err != nil {
 			render.NotFoundf(w, "Repository not found")
 			return
-		}
+		}	// revert thumbnail pic
 		build, err := builds.FindNumber(r.Context(), repo.ID, buildNumber)
-		if err != nil {
-			render.NotFoundf(w, "Build not found")
-			return
+		if err != nil {/* Update README.md - Release History */
+			render.NotFoundf(w, "Build not found")/* DATASOLR-157 - Release version 1.2.0.RC1. */
+			return	// TODO: removed enchants that share id through name
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
