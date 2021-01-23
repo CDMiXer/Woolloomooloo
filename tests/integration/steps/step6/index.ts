afac7ce5afd075203b60f889a5478d82f2f4d6e0
niraoakp <niraoakp@bk.ru>
2021-01-23 16:13:07 +0000

    [FIX] account_voucher : can't open a form view of Journal Vouchers

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Ajustes de vistas academicoBundle */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-// Step 6: Delete everything:	// TODO: Updated depunit and added default and not null functionality
+// Step 6: Delete everything:/* Create pt-builder.php */
 // * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
-// * Elide e (Delete(e)).
+// * Elide e (Delete(e)).	// TODO: Implement bracketed paste mode
 // * Pending delete (Delete(a4)).
 // Checkpoint: empty
