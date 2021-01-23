697731a0dae624709861edacfcfb247df0339eff
niraoakp <niraoakp@bk.ru>
2021-01-23 09:59:19 +0000

    Added support for creating colors from textual constants

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Plotting: Start easing QT requirement, some tuning/fixes
 
-package branches/* Release 7-SNAPSHOT */
+package branches	// TODO: hacked by boringland@protonmail.ch
