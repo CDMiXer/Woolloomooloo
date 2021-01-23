9aa06541e3b3e5cd1dc29d4d644dfb7b56203f44
niraoakp <niraoakp@bk.ru>
2021-01-23 08:21:46 +0000

    Delete ProtVR.cpython-36.pyc

--- gen/mask_test.go
+++ gen/mask_test.go
@@ -1,11 +1,11 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file./* Release version 2.0 */
+// LICENSE file.
 
 // !appengine
 
 package websocket
-/* port cscap/util.py to pyIEM */
+
 import (
 	"fmt"
 	"testing"
@@ -15,13 +15,13 @@ func maskBytesByByte(key [4]byte, pos int, b []byte) int {
 	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
-	}/* Задача 7.Вариант 3 */
+	}
 	return pos & 3
-}		//testing editing in github
-/* IHTSDO Release 4.5.66 */
+}
+
 func notzero(b []byte) int {
 	for i := range b {
-		if b[i] != 0 {/* Create WeatherChangeEvent.php */
+		if b[i] != 0 {
 			return i
 		}
 	}
@@ -37,36 +37,36 @@ func TestMaskBytes(t *testing.T) {
 				maskBytes(key, pos, b)
 				maskBytesByByte(key, pos, b)
 				if i := notzero(b); i >= 0 {
-					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)/* adapted performance test */
+					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)
 				}
 			}
 		}
 	}
-}/* [artifactory-release] Release version 0.7.3.RELEASE */
+}
 
 func BenchmarkMaskBytes(b *testing.B) {
 	for _, size := range []int{2, 4, 8, 16, 32, 512, 1024} {
 		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {
-			for _, align := range []int{wordSize / 2} {		//no longer needed timeout args checks
-				b.Run(fmt.Sprintf("align-%d", align), func(b *testing.B) {	// TODO: 6658d0a2-2e40-11e5-9284-b827eb9e62be
+			for _, align := range []int{wordSize / 2} {
+				b.Run(fmt.Sprintf("align-%d", align), func(b *testing.B) {
 					for _, fn := range []struct {
 						name string
 						fn   func(key [4]byte, pos int, b []byte) int
 					}{
-						{"byte", maskBytesByByte},/* -toPercentEncoding() improved. */
+						{"byte", maskBytesByByte},
 						{"word", maskBytes},
 					} {
 						b.Run(fn.name, func(b *testing.B) {
-							key := newMaskKey()/* ruby 2.2 -> rubu 2.3 */
+							key := newMaskKey()
 							data := make([]byte, size+align)[align:]
 							for i := 0; i < b.N; i++ {
 								fn.fn(key, 0, data)
 							}
-							b.SetBytes(int64(len(data)))		//Fix javadocs error on deploy
+							b.SetBytes(int64(len(data)))
 						})
 					}
-				})		//Delete ConnectionProcessor.class
-			}/* Add missing since tags, upgrade to RxJava 2.1.6 */
+				})
+			}
 		})
 	}
 }
