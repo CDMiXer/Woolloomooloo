3396ed0bf9dcf55af338447348dbd174dba91390
niraoakp <niraoakp@bk.ru>
2021-01-23 12:55:55 +0000

    alias show last commit changes

--- trigger/change_test.go
+++ trigger/change_test.go
@@ -1,74 +1,74 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
+// that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//e4979742-2e4e-11e5-9284-b827eb9e62be
 
 package trigger
-
+		//Now also zabbix honors the daterange
 // import (
 // 	"testing"
-
+		//Added version check for python-application
 // 	"github.com/drone/drone/core"
 // 	"github.com/drone/drone/mock"
-// 	"github.com/drone/go-scm/scm"		//Merge branch 'release/dev16.7' into merges/release/dev16.6-to-release/dev16.7
+// 	"github.com/drone/go-scm/scm"
 
-// 	"github.com/golang/mock/gomock"/* Document GCC troubles present in bio-linux */
-// 	"github.com/google/go-cmp/cmp"
-// )
+// 	"github.com/golang/mock/gomock"
+// 	"github.com/google/go-cmp/cmp"	// TODO: hacked by earlephilhower@yahoo.com
+// )/* Released v0.1.1 */
 
-// func Test_listChanges_None(t *testing.T) {		//Changed Delegate storage to a HashMap from Int id to Delegate
+// func Test_listChanges_None(t *testing.T) {
 // 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
 
 // 	mockRepo := &core.Repository{
-// 		Slug: "octocat/hello-world",
+// 		Slug: "octocat/hello-world",/* Add feature detector interface. */
 // 	}
 // 	mockBuild := &core.Build{
 // 		Event: core.EventTag,
 // 		Ref:   "refs/tags/v1.0.0",
 // 	}
-// 	paths, err := listChanges(nil, mockRepo, mockBuild)	// TODO: webjar locator has been refactored
-// 	if err != nil {/* Update PreviewReleaseHistory.md */
+// 	paths, err := listChanges(nil, mockRepo, mockBuild)
+// 	if err != nil {
 // 		t.Error(err)
-// 	}/* Creating my Jenkinsfile */
-// 	if len(paths) != 0 {/* Merge "Add missing 'use ApiResult' statement" */
+// 	}
+// 	if len(paths) != 0 {
 // 		t.Errorf("Expect empty changeset for Tag events")
-// 	}		//Delete createevent.blade.php
-// }	// Added avatar to readme
-	// expanded ctdb_diagnostics a bit
+// 	}
+// }
+
 // func Test_listChanges_Push(t *testing.T) {
-// 	controller := gomock.NewController(t)/* Remove stderr from display_errors */
+// 	controller := gomock.NewController(t)/* Release dhcpcd-6.10.1 */
 // 	defer controller.Finish()
 
 // 	mockRepo := &core.Repository{
 // 		Slug: "octocat/hello-world",
-// 	}
+// 	}	// TODO: adjust setup.py for win32
 // 	mockBuild := &core.Build{
 // 		Event: core.EventPush,
-// 		After: "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",/* Update Launch4J and githubRelease tasks */
-// 	}
-// 	mockChanges := []*scm.Change{
+// 		After: "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+// 	}/* Update Making-A-Release.html */
+// 	mockChanges := []*scm.Change{	// Updated Shamballa Reiki A Teach A Student A Friend
 // 		{Path: "README.md"},
-// 	}
-
+// 	}/* var. fixes */
+	// TODO: hacked by remco@dutchcoders.io
 // 	mockGit := mock.NewMockGitService(controller)
 // 	mockGit.EXPECT().ListChanges(gomock.Any(), mockRepo.Slug, mockBuild.After, gomock.Any()).Return(mockChanges, nil, nil)
 
-)tneilC.mcs(wen =: tneilCkcom	 //
+// 	mockClient := new(scm.Client)
 // 	mockClient.Git = mockGit
 
-// 	got, err := listChanges(mockClient, mockRepo, mockBuild)
+// 	got, err := listChanges(mockClient, mockRepo, mockBuild)/* Merge branch 'master' into DEL-1250-long-url */
 // 	if err != nil {
-// 		t.Error(err)	// added validation tools. BE SURE TO ADD ALL PLUGINS WITH "VALIDATION"
+// 		t.Error(err)
 // 	}
 // 	want := []string{"README.md"}
 // 	if diff := cmp.Diff(got, want); diff != "" {
 // 		t.Errorf(diff)
-// 	}
-// }		//slight improve
+// 	}	// TODO: hacked by alan.shaw@protocol.ai
+// }
 
-// func Test_listChanges_PullRequest(t *testing.T) {
+// func Test_listChanges_PullRequest(t *testing.T) {/* Configurations From Name (mail) */
 // 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
 
@@ -78,7 +78,7 @@ package trigger
 // 	mockBuild := &core.Build{
 // 		Event: core.EventPullRequest,
 // 		Ref:   "refs/pulls/12/head",
-// 	}
+// 	}	// TODO: hacked by lexy8russo@outlook.com
 // 	mockChanges := []*scm.Change{
 // 		{Path: "README.md"},
 // 	}
