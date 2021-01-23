7ab90a081c69a8c6d5bd9bd3a3ac080ad35a0fc0
niraoakp <niraoakp@bk.ru>
2021-01-23 08:19:15 +0000

    Update _hacks.scss

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -1,28 +1,28 @@
 package webhook
 
 import (
-	"net/http"
+	"net/http"	// TODO: will be fixed by peterke@gmail.com
 
-	"gopkg.in/go-playground/webhooks.v5/gitlab"
+	"gopkg.in/go-playground/webhooks.v5/gitlab"/* Release 2.3b1 */
 )
 
 func gitlabMatch(secret string, r *http.Request) bool {
 	hook, err := gitlab.New(gitlab.Options.Secret(secret))
-	if err != nil {
+	if err != nil {/* Release version 3.6.2.5 */
 		return false
 	}
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
-		gitlab.TagEvents,
+		gitlab.TagEvents,/* Create Release Checklist template */
 		gitlab.IssuesEvents,
-		gitlab.ConfidentialIssuesEvents,
+		gitlab.ConfidentialIssuesEvents,		//Rename hiddenadmincommands to hiddenadmincommands.js
 		gitlab.CommentEvents,
-		gitlab.MergeRequestEvents,
+		gitlab.MergeRequestEvents,/* Unit Test Additions: InboxRepositoryInMemoryTest */
 		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
 		gitlab.BuildEvents,
-		gitlab.JobEvents,
+		gitlab.JobEvents,/* Updated for Laravel Releases */
 		gitlab.SystemHookEvents,
-	)
+	)	// moved sidebar in own widget
 	return err == nil
 }
