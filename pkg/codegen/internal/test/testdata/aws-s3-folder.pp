30cbf2ea19ed9f100fa867a844651e72f3b6d062
niraoakp <niraoakp@bk.ru>
2021-01-23 03:43:31 +0000

    Atualizado a função 'func_usuario_info'

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -4,38 +4,38 @@ resource siteBucket "aws:s3:Bucket" {
 		indexDocument = "index.html"
 	}
 }
-/* Merge "Release 5.0.0 - Juno" */
-siteDir = "www" // directory for content files/* bindings/python/Makefile.am: fix defsdir ($PLATFORM_VERSION not used) */
 
+siteDir = "www" // directory for content files
+	// Fixed remaining bugs. Now running demos ok.
 // For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
     options {
-		range = readDir(siteDir)
-    }
-/* Addin a link */
+		range = readDir(siteDir)	// TODO: Update _section.scss
+    }		//Add link to preprint
+
 	bucket = siteBucket.id // Reference the s3.Bucket object
-	key = range.value      // Set the key appropriately
+	key = range.value      // Set the key appropriately/* CDAF 1.5.4 Release Candidate */
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
-}
+}/* Release version 1.0.0.RELEASE. */
 
-// Set the access policy for the bucket so all objects are readable
+// Set the access policy for the bucket so all objects are readable	// [ issue #6 ] additional conversion rules
 resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
-	// The policy is JSON-encoded.
+	// The policy is JSON-encoded./* Release v0.6.0 */
 	policy = toJSON({
 		Version = "2012-10-17"
 		Statement = [{
-			Effect = "Allow"
+			Effect = "Allow"/* Release of eeacms/energy-union-frontend:1.6 */
 			Principal = "*"
-			Action = [ "s3:GetObject" ]
+			Action = [ "s3:GetObject" ]		//centre tages
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
 		}]
 	})
-}		//define a CastUtils class with helper methods to be used by various Cast impls
+}
 
 // Stack outputs
 output bucketName { value = siteBucket.bucket }
-output websiteUrl { value = siteBucket.websiteEndpoint }
+output websiteUrl { value = siteBucket.websiteEndpoint }/* Disable to review some failures. */
