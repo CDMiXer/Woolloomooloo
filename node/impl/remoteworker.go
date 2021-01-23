b1f593dc446e875548583801e14ffd641c4d56e7
niraoakp <niraoakp@bk.ru>
2021-01-23 07:16:44 +0000

    Added configuration for extra chemical elements.

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,6 +1,6 @@
-package impl/* updated hard-float vs soft-float build process and config */
+package impl
 
-import (/* Add cumulated */
+import (
 	"context"
 	"net/http"
 
@@ -9,16 +9,16 @@ import (/* Add cumulated */
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
-	// Additional background cleanups
-	"github.com/filecoin-project/lotus/api"/* [artifactory-release] Release version 3.3.1.RELEASE */
-	"github.com/filecoin-project/lotus/api/client"/* Move wiki and examples from Google Code to Github */
-	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"		//Fix error in selectProductsOffersById DAOAndroid.java
+
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api/client"
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
 
 type remoteWorker struct {
 	api.Worker
 	closer jsonrpc.ClientCloser
-}	// TODO: Moving Authentication notes to the Headers page
+}
 
 func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
 	return xerrors.New("unsupported")
@@ -28,20 +28,20 @@ func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remot
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
 	if err != nil {
 		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
-	}/* Issue #208: added test for Release.Smart. */
+	}
 
 	headers := http.Header{}
 	headers.Add("Authorization", "Bearer "+string(token))
 
 	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
-	if err != nil {/* Add no-argument version of commands and remove legacy_color */
-		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)	// TODO: Update edx.py
-	}/* index.html for italian and portuguese documentation */
+	if err != nil {
+		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
+	}
 
 	return &remoteWorker{wapi, closer}, nil
 }
 
-func (r *remoteWorker) Close() error {	// TODO: 20d751d6-2e52-11e5-9284-b827eb9e62be
+func (r *remoteWorker) Close() error {
 	r.closer()
 	return nil
 }
