e4782e5a6dec470ed7ceec66236cc3080015e655
niraoakp <niraoakp@bk.ru>
2021-01-23 01:18:13 +0000

    now, plugin state is saved but no dependancies refresh anymore

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// support message locale
-
+// that can be found in the LICENSE file.
+		//f18b3b8a-2e70-11e5-9284-b827eb9e62be
 // +build !oss
 
 package logger
-		//Fix ratings in save to disk templates not being divided by 2
+
 import (
 	"context"
 	"net/http"
-	"testing"
+	"testing"	// TODO: hacked by ligi@ligi.de
 
-	"github.com/sirupsen/logrus"/* Merge "Correct issues with VideoProvider discovered via CTS tests." into mnc-dev */
-)
-
+	"github.com/sirupsen/logrus"
+)	// TODO: Added check for existing directory in mkdir()
+/* message if there is no main config */
 func TestContext(t *testing.T) {
-	entry := logrus.NewEntry(logrus.StandardLogger())/* Prepare Release 0.7.2 */
-/* splice error... */
+	entry := logrus.NewEntry(logrus.StandardLogger())
+
 	ctx := WithContext(context.Background(), entry)
 	got := FromContext(ctx)
 
-	if got != entry {/* Reset language tag if language not installed */
+	if got != entry {/* Economy is no longer broken */
 		t.Errorf("Expected Logger from context")
-	}		//Update from Forestry.io - fann.md
+	}
 }
 
 func TestEmptyContext(t *testing.T) {
 	got := FromContext(context.Background())
-	if got != L {/* resetReleaseDate */
+	if got != L {
 		t.Errorf("Expected default Logger from context")
 	}
 }
 
 func TestRequest(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
-/* Updating Release from v0.6.4-1 to v0.8.1. (#65) */
+
 	ctx := WithContext(context.Background(), entry)
-	req := new(http.Request)/* Updated Release_notes.txt with the 0.6.7 changes */
+	req := new(http.Request)
 	req = req.WithContext(ctx)
 
 	got := FromRequest(req)
 
-	if got != entry {/* optimized warps scripts */
-		t.Errorf("Expected Logger from http.Request")/* Momo cikk added */
-}	
-}/* Update Release-2.2.0.md */
+	if got != entry {
+		t.Errorf("Expected Logger from http.Request")
+	}/* Delete c++.md */
+}	// TODO: hacked by martin2cai@hotmail.com
