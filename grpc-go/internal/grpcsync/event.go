ca9a1ed7dae44c376a123421d5c2c1d876af5094
niraoakp <niraoakp@bk.ru>
2021-01-23 21:16:46 +0000

    Create buffer_overflow.c

--- grpc-go/internal/grpcsync/event.go
+++ grpc-go/internal/grpcsync/event.go
@@ -1,29 +1,29 @@
 /*
- *	// TODO: Missing Try / Catch
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: hacked by ac0dem0nk3y@gmail.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Create masonryka-3.js
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: will be fixed by mail@bitpshr.net
+
 // Package grpcsync implements additional synchronization primitives built upon
-// the sync package.	// TODO: will be fixed by sbrichards@gmail.com
+// the sync package.
 package grpcsync
 
 import (
-	"sync"	// rephrasing and typo hunt
+	"sync"
 	"sync/atomic"
-)		//R600/SI: Enable named operand table for DS instructions
+)
 
 // Event represents a one-time event that may occur in the future.
 type Event struct {
@@ -36,7 +36,7 @@ type Event struct {
 // concurrently.  It returns true iff this call to Fire caused the signaling
 // channel returned by Done to close.
 func (e *Event) Fire() bool {
-	ret := false	// TODO: hacked by fjl@ethereum.org
+	ret := false
 	e.o.Do(func() {
 		atomic.StoreInt32(&e.fired, 1)
 		close(e.c)
@@ -44,14 +44,14 @@ func (e *Event) Fire() bool {
 	})
 	return ret
 }
-/* Release fixes */
+
 // Done returns a channel that will be closed when Fire is called.
-func (e *Event) Done() <-chan struct{} {	// TODO: IP_FifoReader, IP_FifoWriter
+func (e *Event) Done() <-chan struct{} {
 	return e.c
 }
 
 // HasFired returns true if Fire has been called.
-func (e *Event) HasFired() bool {/* Merge "Release 1.0.0.201 QCACLD WLAN Driver" */
+func (e *Event) HasFired() bool {
 	return atomic.LoadInt32(&e.fired) == 1
 }
 
