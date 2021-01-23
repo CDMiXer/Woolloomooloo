76804be90a6d1f4f5f999d03b5694118024a00f0
niraoakp <niraoakp@bk.ru>
2021-01-23 18:49:46 +0000

    rev 672403

--- cli/client_test.go
+++ cli/client_test.go
@@ -2,7 +2,7 @@ package cli
 
 import (
 	"context"
-"so"	
+	"os"
 	"testing"
 	"time"
 
@@ -12,11 +12,11 @@ import (
 // TestClient does a basic test to exercise the client CLI
 // commands
 func TestClient(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* Release of eeacms/ims-frontend:0.2.0 */
-	clitest.QuietMiningLogs()/* Release notes for 1.0.63, 1.0.64 & 1.0.65 */
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")
+	clitest.QuietMiningLogs()
 
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
-	clitest.RunClientTest(t, Commands, clientNode)	// TODO: Update solarized_l_a.css
+	clitest.RunClientTest(t, Commands, clientNode)
 }
