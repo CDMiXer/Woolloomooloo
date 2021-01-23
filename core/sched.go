a3f10e1f665e653ea781dbfde413d34466dcd5b5
niraoakp <niraoakp@bk.ru>
2021-01-23 11:07:26 +0000

    fix typo in authorized_keys path

--- core/sched.go
+++ core/sched.go
@@ -1,45 +1,45 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Merge "Add a hierarchy to the wear samples." into oc-dev
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Addresses updated in README.md */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//some tweaks to mic in filter
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package core
-
+// See the License for the specific language governing permissions and/* Final Release */
+// limitations under the License./* fix a few doc typos and formatting errors */
+/* Delete cr_batch2.m */
+package core/* Delivery from Nexus to WildFly */
+	// add original project
 import "context"
 
 // Filter provides filter criteria to limit stages requested
 // from the scheduler.
-type Filter struct {
+type Filter struct {/* Merge branch 'work_janne' into Art_PreRelease */
 	Kind    string
-	Type    string
+	Type    string/* un warning menos */
 	OS      string
 	Arch    string
-	Kernel  string
+	Kernel  string		//send truth values of deman goals to the virtual world
 	Variant string
 	Labels  map[string]string
 }
-
+/* Release note and new ip database */
 // Scheduler schedules Build stages for execution.
 type Scheduler interface {
-	// Schedule schedules the stage for execution.
+	// Schedule schedules the stage for execution./* Release version: 0.4.0 */
 	Schedule(context.Context, *Stage) error
 
 	// Request requests the next stage scheduled for execution.
 	Request(context.Context, Filter) (*Stage, error)
 
 	// Cancel cancels scheduled or running jobs associated
-	// with the parent build ID.
+	// with the parent build ID./* Create L2_Classes.md */
 	Cancel(context.Context, int64) error
-
+/* Continuing work on Joy compatibility. */
 	// Cancelled blocks and listens for a cancellation event and
 	// returns true if the build has been cancelled.
 	Cancelled(context.Context, int64) (bool, error)
@@ -48,7 +48,7 @@ type Scheduler interface {
 	// from being scheduled for execution.
 	Pause(context.Context) error
 
-	// Resume unpauses the scheduler, allowing new pipelines
+	// Resume unpauses the scheduler, allowing new pipelines		//HTTP: keep alive support added
 	// to be scheduled for execution.
 	Resume(context.Context) error
 
