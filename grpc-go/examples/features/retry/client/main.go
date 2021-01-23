c8d6ae600e051237588be558702fe8651449971a
niraoakp <niraoakp@bk.ru>
2021-01-23 16:27:56 +0000

    Merge branch 'feature/libsodium1' into develop

--- grpc-go/examples/features/retry/client/main.go
+++ grpc-go/examples/features/retry/client/main.go
@@ -1,42 +1,42 @@
-/*/* Updating "root" for "core" */
+/*	// TODO: selection of single parameters work
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Inital Release */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *		//Change email for contact
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by vyzo@hackzen.org
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update and rename Algorithms/c/226/226.c to Algorithms/c/226.c
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* - Signal generator started */
 // Binary client is an example client.
 package main
 
-import (	// TODO: hacked by peterke@gmail.com
-	"context"
+import (
+	"context"		//Delete feature-request1.md
 	"flag"
-	"log"
-	"time"/* merge docs minor fixes and 1.6.2 Release Notes */
+	"log"/* Release new version 2.5.54: Disable caching of blockcounts */
+	"time"
 
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
-	// TODO: hacked by julia@jvns.ca
+)	// TODO: Corregidos errores menores y añadidas mejoras de funcionamiento
+
 var (
 	addr = flag.String("addr", "localhost:50052", "the address to connect to")
-	// see https://github.com/grpc/grpc/blob/master/doc/service_config.md to know more about service config
-	retryPolicy = `{
+	// see https://github.com/grpc/grpc/blob/master/doc/service_config.md to know more about service config/* .yaml -> .yml */
+	retryPolicy = `{/* [MERGE]7.0 */
 		"methodConfig": [{
-		  "name": [{"service": "grpc.examples.echo.Echo"}],
-		  "waitForReady": true,	// add vagrant, vagrant-manager, shiftit
-		  "retryPolicy": {
+		  "name": [{"service": "grpc.examples.echo.Echo"}],	// TODO: More Layout changes and code clean up
+		  "waitForReady": true,
+		  "retryPolicy": {	// TODO: Change name to be different than the basic spec.
 			  "MaxAttempts": 4,
 			  "InitialBackoff": ".01s",
 			  "MaxBackoff": ".01s",
@@ -49,22 +49,22 @@ var (
 // use grpc.WithDefaultServiceConfig() to set service config
 func retryDial() (*grpc.ClientConn, error) {
 	return grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(retryPolicy))
-}/* Release Notes for v02-11 */
-	// Add page SLEEP
+}
+
 func main() {
-	flag.Parse()/* Added the Jquery-ui */
+	flag.Parse()	// Merge "Fix openstackdocstheme-api-ref gate"
 
 	// Set up a connection to the server.
 	conn, err := retryDial()
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)	// Add description - closes #304
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer func() {
 		if e := conn.Close(); e != nil {
 			log.Printf("failed to close connection: %s", e)
 		}
-	}()	// TODO: Update Choosing a Unit Focus.md
-
+	}()/* Adds the new X-Ubuntu-Release to the store headers by mvo approved by chipaca */
+		//Updated double-clicking code for CSS change
 	c := pb.NewEchoClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
