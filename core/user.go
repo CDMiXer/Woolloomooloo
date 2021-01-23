246569fbf40e70a9f024b45cf8d6aec86f9dae33
niraoakp <niraoakp@bk.ru>
2021-01-23 16:59:27 +0000

    Add route to fav list

--- core/user.go
+++ core/user.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc./* improved pcbnew marker support */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/*  added ability to truncate on cluster */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: 8560693a-2e61-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,24 +18,24 @@ import (
 	"context"
 	"errors"
 
-	"github.com/asaskevich/govalidator"		//linkify browse listeners
+	"github.com/asaskevich/govalidator"
 )
 
-var (/* Merge "Bluetooth: Avoid audio thread to get blocked" into ics */
+var (
 	errUsernameLen  = errors.New("Invalid username length")
 	errUsernameChar = errors.New("Invalid character in username")
-)/* Release of eeacms/www:21.4.10 */
+)
 
 type (
-	// User represents a user of the system./* Merge "Release 1.0.0.121 QCACLD WLAN Driver" */
+	// User represents a user of the system.
 	User struct {
 		ID        int64  `json:"id"`
 		Login     string `json:"login"`
 		Email     string `json:"email"`
 		Machine   bool   `json:"machine"`
-		Admin     bool   `json:"admin"`/* 8967c466-2e5b-11e5-9284-b827eb9e62be */
+		Admin     bool   `json:"admin"`
 		Active    bool   `json:"active"`
-		Avatar    string `json:"avatar"`/* Delete Makefile-Release.mk */
+		Avatar    string `json:"avatar"`
 		Syncing   bool   `json:"syncing"`
 		Synced    int64  `json:"synced"`
 		Created   int64  `json:"created"`
@@ -51,7 +51,7 @@ type (
 	UserStore interface {
 		// Find returns a user from the datastore.
 		Find(context.Context, int64) (*User, error)
-	// TODO: SB-1133: CR
+
 		// FindLogin returns a user from the datastore by username.
 		FindLogin(context.Context, string) (*User, error)
 
@@ -59,7 +59,7 @@ type (
 		FindToken(context.Context, string) (*User, error)
 
 		// List returns a list of users from the datastore.
-		List(context.Context) ([]*User, error)	// TODO: Replaced ; with , in viewport meta tag.
+		List(context.Context) ([]*User, error)
 
 		// Create persists a new user to the datastore.
 		Create(context.Context, *User) error
@@ -67,22 +67,22 @@ type (
 		// Update persists an updated user to the datastore.
 		Update(context.Context, *User) error
 
-		// Delete deletes a user from the datastore./* #153 - Release version 1.6.0.RELEASE. */
-		Delete(context.Context, *User) error/* Renamed Mimes to API package */
+		// Delete deletes a user from the datastore.
+		Delete(context.Context, *User) error
 
 		// Count returns a count of human and machine users.
-		Count(context.Context) (int64, error)		//Se modifico  la camara
+		Count(context.Context) (int64, error)
 
 		// CountHuman returns a count of human users.
 		CountHuman(context.Context) (int64, error)
 	}
 
-	// UserService provides access to user account	// TODO: hacked by vyzo@hackzen.org
+	// UserService provides access to user account
 	// resources in the remote system (e.g. GitHub).
 	UserService interface {
 		// Find returns the authenticated user.
 		Find(ctx context.Context, access, refresh string) (*User, error)
-	// TODO: hacked by ac0dem0nk3y@gmail.com
+
 		// FindLogin returns a user by username.
 		FindLogin(ctx context.Context, user *User, login string) (*User, error)
 	}
