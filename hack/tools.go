90f2bf15cba545b83bb8b5c2abbe388f05cb06c6
niraoakp <niraoakp@bk.ru>
2021-01-23 04:57:57 +0000

    added SacrificeAction, rename SelectionOp to SelectOp

--- hack/tools.go
+++ hack/tools.go
@@ -1,5 +1,5 @@
 // +build tools
-/* Añadido servicios de Cita y Horario iniciales */
+
 // This package contains code generation utilities
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
@@ -10,22 +10,22 @@ import (
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"/* Released 1.11,add tag. */
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"		//new option added for passing oechem license as argument.
-	_ "github.com/jstemmer/go-junit-report"	// TODO: [DDW-81] fix ada redemption menu logic
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"/* Release version: 1.0.7 */
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "k8s.io/code-generator"
+	_ "k8s.io/code-generator"		//f40221a2-2e3f-11e5-9284-b827eb9e62be
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"/* Release 0.7.1.2 */
+	_ "k8s.io/code-generator/cmd/defaulter-gen"		//reconnect pooling when disconnected
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/openapi-gen"
 	_ "k8s.io/code-generator/cmd/register-gen"
-	_ "k8s.io/code-generator/cmd/set-gen"	// TODO: MEDIUM / Fixed AddExcelCell inspector
-	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+	_ "k8s.io/code-generator/cmd/set-gen"
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"/* Test that CharArraySequence functions as expected */
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
