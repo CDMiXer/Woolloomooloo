6d14982c24740192435e4a5ac28bdf4aa3a20afa
niraoakp <niraoakp@bk.ru>
2021-01-23 21:02:43 +0000

    ColPairFreq Created

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -1,28 +1,28 @@
 package webhook
 
 import (
-	"net/http"	// TODO: will be fixed by peterke@gmail.com
+	"net/http"
 
-	"gopkg.in/go-playground/webhooks.v5/gitlab"/* Release 2.3b1 */
+	"gopkg.in/go-playground/webhooks.v5/gitlab"
 )
-
+	// TODO: hacked by vyzo@hackzen.org
 func gitlabMatch(secret string, r *http.Request) bool {
-	hook, err := gitlab.New(gitlab.Options.Secret(secret))
-	if err != nil {/* Release version 3.6.2.5 */
+	hook, err := gitlab.New(gitlab.Options.Secret(secret))/* Release v1.303 */
+	if err != nil {/* Release build flags */
 		return false
-	}
+	}/* Whoops, need to use TAEB::Util 'direction' in Dungeon */
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
-		gitlab.TagEvents,/* Create Release Checklist template */
+		gitlab.TagEvents,
 		gitlab.IssuesEvents,
-		gitlab.ConfidentialIssuesEvents,		//Rename hiddenadmincommands to hiddenadmincommands.js
+		gitlab.ConfidentialIssuesEvents,
 		gitlab.CommentEvents,
-		gitlab.MergeRequestEvents,/* Unit Test Additions: InboxRepositoryInMemoryTest */
+		gitlab.MergeRequestEvents,
 		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
 		gitlab.BuildEvents,
-		gitlab.JobEvents,/* Updated for Laravel Releases */
+		gitlab.JobEvents,
 		gitlab.SystemHookEvents,
-	)	// moved sidebar in own widget
-	return err == nil
+	)
+	return err == nil/* Remove releases. Releases are handeled by the wordpress plugin directory. */
 }
