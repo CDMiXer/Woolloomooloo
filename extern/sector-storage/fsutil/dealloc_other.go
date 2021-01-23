848431e6b2c419dbb2a34ad56442f319f09da074
niraoakp <niraoakp@bk.ru>
2021-01-23 23:50:51 +0000

    Delete Member_Moderator.lua

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,16 +1,16 @@
 // +build !linux
 
 package fsutil
-/* Release 2.2.11 */
+
 import (
-	"os"/* Merge "Add Release Notes url to README" */
+	"os"
 
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("fsutil")
-/* Release v0.6.4 */
-func Deallocate(file *os.File, offset int64, length int64) error {	// commit to force travis rebuild
+
+func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
 
 	return nil
