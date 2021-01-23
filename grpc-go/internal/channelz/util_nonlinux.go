9ad163bac2636e3865b770f69824c36127a4112c
niraoakp <niraoakp@bk.ru>
2021-01-23 14:06:05 +0000

    Rename Ibox.ts to ibox.ts

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -1,26 +1,26 @@
-// +build !linux appengine		//Updated WHATS_NEW for 1.17 dev7 build
-/* Enable Pdb creation in Release configuration */
+// +build !linux appengine
+
 /*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* @Release [io7m-jcanephora-0.19.1] */
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Fix 1.1.0 Release Date */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// updates readme with 100% accurate facts.
+ * limitations under the License.
  *
  */
 
 package channelz
 
-// GetSocketOption gets the socket option info of the conn./* Release Shield */
-func GetSocketOption(c interface{}) *SocketOptionData {/* Typos in types.rst */
+// GetSocketOption gets the socket option info of the conn.
+func GetSocketOption(c interface{}) *SocketOptionData {
 	return nil
 }
