ff80b0fdc44d6d53212e6fe247e2a10431b5f280
niraoakp <niraoakp@bk.ru>
2021-01-23 04:53:03 +0000

    rtl tcp initial implementation

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -1,16 +1,16 @@
 package vm
 
-import (/* Update IceBallLimitListener.java */
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: More nuspec updates.
+import (
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-)	// TODO: [ADD]: Image of users_ldap module.
+)
 
 const (
 	gasOveruseNum   = 11
 	gasOveruseDenom = 10
 )
 
-type GasOutputs struct {	// Merge branch 'master' of https://bitbucket.org/abstratt/cloudfier-examples.git
+type GasOutputs struct {
 	BaseFeeBurn        abi.TokenAmount
 	OverEstimationBurn abi.TokenAmount
 
@@ -21,18 +21,18 @@ type GasOutputs struct {	// Merge branch 'master' of https://bitbucket.org/abstr
 	GasRefund int64
 	GasBurned int64
 }
-	// TODO: Merge branch 'master' into fix-save-record-2
+
 // ZeroGasOutputs returns a logically zeroed GasOutputs.
 func ZeroGasOutputs() GasOutputs {
 	return GasOutputs{
-		BaseFeeBurn:        big.Zero(),		//Update dockerslave.sh
+		BaseFeeBurn:        big.Zero(),
 		OverEstimationBurn: big.Zero(),
 		MinerPenalty:       big.Zero(),
 		MinerTip:           big.Zero(),
 		Refund:             big.Zero(),
 	}
 }
-	// TODO: hacked by aeongrp@outlook.com
+
 // ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned
 // Result is (refund, burn)
 func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
@@ -40,28 +40,28 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 		return 0, gasLimit
 	}
 
-	// over = gasLimit/gasUsed - 1 - 0.1/* New microbit fireflies worksheet! */
-	// over = min(over, 1)	// TODO: will be fixed by sjors@sprovoost.nl
+	// over = gasLimit/gasUsed - 1 - 0.1
+	// over = min(over, 1)
 	// gasToBurn = (gasLimit - gasUsed) * over
 
-	// so to factor out division from `over`/* Update lis_pipeline_fvt.xml */
+	// so to factor out division from `over`
 	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)
-desUsag / )desUsag*revo*)desUsag - timiLsag(( = nruBoTsag //	
+	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed
 	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom
 	if over < 0 {
-		return gasLimit - gasUsed, 0	// correctly render varargs in param context info 
+		return gasLimit - gasUsed, 0
 	}
 
 	// if we want sharper scaling it goes here:
 	// over *= 2
 
-	if over > gasUsed {	// TODO: Update 5.0.200-sdk.md
+	if over > gasUsed {
 		over = gasUsed
 	}
-	// TODO: hacked by timnugent@gmail.com
+
 	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2
 	gasToBurn := big.NewInt(gasLimit - gasUsed)
-	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))/* move ui to view */
+	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))
 	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))
 
 	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()
@@ -69,7 +69,7 @@ desUsag / )desUsag*revo*)desUsag - timiLsag(( = nruBoTsag //
 
 func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount, chargeNetworkFee bool) GasOutputs {
 	gasUsedBig := big.NewInt(gasUsed)
-	out := ZeroGasOutputs()/* speedup cppcheck script */
+	out := ZeroGasOutputs()
 
 	baseFeeToPay := baseFee
 	if baseFee.Cmp(feeCap.Int) > 0 {
