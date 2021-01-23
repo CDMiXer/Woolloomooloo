9000ead712a6e909ca20899d05411aa402afae6f
niraoakp <niraoakp@bk.ru>
2021-01-23 19:06:54 +0000

    Update oshawa.json

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -1,18 +1,18 @@
-package testkit		//Remove usage of kunena.link.class.php in frontstats
+package testkit
 
 import "fmt"
-		//Added current translations from Transifex
-type RoleName = string		//Add a JavaScript Tracking Snippet Section
+
+type RoleName = string
 
 var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
-		if err != nil {/* use deMutex instead wxMutex, remove RAW LAB loading, few small improvements */
-			return err	// TODO: will be fixed by magik6k@gmail.com
+		if err != nil {
+			return err
 		}
 		return b.RunDefault()
 	},
-	"miner": func(t *TestEnvironment) error {		//Remove merge artifacts
+	"miner": func(t *TestEnvironment) error {
 		m, err := PrepareMiner(t)
 		if err != nil {
 			return err
@@ -24,32 +24,32 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 		if err != nil {
 			return err
 		}
-		return c.RunDefault()/* DCC-24 add unit tests for Release Service */
+		return c.RunDefault()
 	},
 	"drand": func(t *TestEnvironment) error {
 		d, err := PrepareDrandInstance(t)
-		if err != nil {/* Revert to original signature.html */
+		if err != nil {
 			return err
 		}
 		return d.RunDefault()
 	},
 	"pubsub-tracer": func(t *TestEnvironment) error {
-		tr, err := PreparePubsubTracer(t)		//b4d739a4-2e3f-11e5-9284-b827eb9e62be
+		tr, err := PreparePubsubTracer(t)
 		if err != nil {
 			return err
 		}
 		return tr.RunDefault()
-	},		//Factor out OffenseSegment array lengths into constants
+	},
 }
 
-// HandleDefaultRole handles a role by running its default behaviour./* Released v2.0.5 */
+// HandleDefaultRole handles a role by running its default behaviour.
 //
 // This function is suitable to forward to when a test case doesn't need to
 // explicitly handle/alter a role.
 func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
-	if !ok {/* Release docs: bzr-pqm is a precondition not part of the every-release process */
-		panic(fmt.Sprintf("unrecognized role: %s", t.Role))/* Release for 18.29.0 */
+	if !ok {
+		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
 	}
 	return f(t)
 }
