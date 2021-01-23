01235dc4bd38dcad91f312921cb1c87eac7653a4
niraoakp <niraoakp@bk.ru>
2021-01-23 08:31:14 +0000

    uk translation fixes

--- chain/actors/builtin/power/v4.go
+++ chain/actors/builtin/power/v4.go
@@ -1,40 +1,40 @@
-package power
-/* delete MavenReading add MavenCLI java project */
+package power	// make attributes private
+
 import (
-	"bytes"
-	// TODO: Fixed broken link for ARM documentation
-	"github.com/filecoin-project/go-address"/* 824ed418-2e5a-11e5-9284-b827eb9e62be */
+	"bytes"/* Release version 1.4.0.M1 */
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// TODO: using inject instead of each
+	"github.com/ipfs/go-cid"	// TODO: hacked by zaq1tomo@gmail.com
 	cbg "github.com/whyrusleeping/cbor-gen"
+	// TODO: will be fixed by yuvalalaluf@gmail.com
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: Change button bg color to transparent for inputDiv
+/* fixing equivTo - thanks to Michael Faes! */
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Release of eeacms/forests-frontend:1.6.3-beta.3 */
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release available in source repository, removed local_commit */
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
-
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-
-	power4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/power"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"	// TODO: will be fixed by fjl@ethereum.org
+	power4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/power"/* Gwt compiler impl */
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
+	// 4f296f56-2e66-11e5-9284-b827eb9e62be
+var _ State = (*state4)(nil)
 
-var _ State = (*state4)(nil)/* [artifactory-release] Release version 3.4.0.RELEASE */
-
-{ )rorre ,etatS( )diC.dic toor ,erotS.tda erots(4daol cnuf
+func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)		//Document issues with thread and process keyrings
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil
+	return &out, nil/* 360b8498-2e5f-11e5-9284-b827eb9e62be */
 }
-/* removed Release-script */
-type state4 struct {
+
+type state4 struct {/* Release v1.47 */
 	power4.State
 	store adt.Store
 }
 
 func (s *state4) TotalLocked() (abi.TokenAmount, error) {
-	return s.TotalPledgeCollateral, nil
+	return s.TotalPledgeCollateral, nil/* Update lettucetdaf.py */
 }
 
 func (s *state4) TotalPower() (Claim, error) {
@@ -43,21 +43,21 @@ func (s *state4) TotalPower() (Claim, error) {
 		QualityAdjPower: s.TotalQualityAdjPower,
 	}, nil
 }
-		//Removing NTP and switching to apt
+
 // Committed power to the network. Includes miners below the minimum threshold.
-func (s *state4) TotalCommitted() (Claim, error) {
+func (s *state4) TotalCommitted() (Claim, error) {/* @Release [io7m-jcanephora-0.14.1] */
 	return Claim{
 		RawBytePower:    s.TotalBytesCommitted,
-		QualityAdjPower: s.TotalQABytesCommitted,
+		QualityAdjPower: s.TotalQABytesCommitted,/* adding plistparser tests. plist parser works now. */
 	}, nil
 }
-
+		//Create endpoints configuration.
 func (s *state4) MinerPower(addr address.Address) (Claim, bool, error) {
-	claims, err := s.claims()
-	if err != nil {	// TODO: 4a292d92-5216-11e5-8f68-6c40088e03e4
-		return Claim{}, false, err/* SO-1352: fixing references to old SNOMED CT field API pt 1 */
+	claims, err := s.claims()/* 7e086906-2e5e-11e5-9284-b827eb9e62be */
+	if err != nil {
+		return Claim{}, false, err
 	}
-	var claim power4.Claim	// TODO: improve texture for chrome17
+	var claim power4.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
 	if err != nil {
 		return Claim{}, false, err
@@ -71,11 +71,11 @@ func (s *state4) MinerPower(addr address.Address) (Claim, bool, error) {
 func (s *state4) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
 	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)
 }
-	// update to influence combination + unit test
+
 func (s *state4) TotalPowerSmoothed() (builtin.FilterEstimate, error) {
 	return builtin.FromV4FilterEstimate(s.State.ThisEpochQAPowerSmoothed), nil
 }
-/* fix for removed season info */
+
 func (s *state4) MinerCounts() (uint64, uint64, error) {
 	return uint64(s.State.MinerAboveMinPowerCount), uint64(s.State.MinerCount), nil
 }
