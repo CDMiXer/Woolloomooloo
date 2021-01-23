a16cd196d17c0c5ad41c05de97c784a64d969190
niraoakp <niraoakp@bk.ru>
2021-01-23 22:47:53 +0000

    Improved resolution-reasons display

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -1,40 +1,40 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: 9dade436-2e6b-11e5-9284-b827eb9e62be
-//	// TODO: will be fixed by xiemengjun@gmail.com
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Make the server threaded */
-// You may obtain a copy of the License at/* Bug Fixes, Delete All Codes Confirmation - Version Release Candidate 0.6a */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Implementation of Country Office Request Service */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package deploy/*  - improving error message when using disconnected process instance */
+package deploy
 
-import (/* Add a unit label after cachesize field at RemoteSettingsDialog */
+import (
 	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"	// TODO: Add and include API keys settings (WIP).
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
 // changes during a refresh.
-/* don't shorten paths before sending them to preprocessors */
+
 func NewErrorSource(project tokens.PackageName) Source {
 	return &errorSource{project: project}
 }
-/* Release 0.1. */
-// A errorSource errors when iterated./* added timer for phases(2 min right now) */
+
+// A errorSource errors when iterated.
 type errorSource struct {
 	project tokens.PackageName
 }
-	// 9d276246-2e50-11e5-9284-b827eb9e62be
+
 func (src *errorSource) Close() error                { return nil }
-func (src *errorSource) Project() tokens.PackageName { return src.project }/* Release 0.7.3.1 with fix for svn 1.5. */
+func (src *errorSource) Project() tokens.PackageName { return src.project }
 func (src *errorSource) Info() interface{}           { return nil }
 
 func (src *errorSource) Iterate(
