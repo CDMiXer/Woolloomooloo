16929411b8f894ec11a67387a306ca9f975a3bc9
niraoakp <niraoakp@bk.ru>
2021-01-24 07:40:53 +0000

    ed205986-2e75-11e5-9284-b827eb9e62be

--- scheduler/kube/kube_oss.go
+++ scheduler/kube/kube_oss.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: Subscription Handler fix
+// Licensed under the Apache License, Version 2.0 (the "License");/* Updated Release_notes */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* ndb - disable also query_cache_size_basic_64 fails all the time on sol10-cmt */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Update documentation for running tests
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package kube
+package kube/* remplacement parameters.dist.yml */
 
 import (
-	"context"
+	"context"	// TODO: will be fixed by timnugent@gmail.com
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Initial commit of adapted TUIO simulator. */
 )
 
 type noop struct{}
@@ -30,14 +30,14 @@ func FromConfig(conf Config) (core.Scheduler, error) {
 }
 
 func (noop) Schedule(context.Context, *core.Stage) error {
-	return nil
+	return nil		//77f987a8-2d48-11e5-919d-7831c1c36510
 }
 
-func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
+func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {/* Update rshell.cpp */
 	return nil, nil
-}
+}		//TISTUD-3222 Making Process Runnable Extensible
 
-func (noop) Cancel(context.Context, int64) error {
+func (noop) Cancel(context.Context, int64) error {/* Back-Button in address form changed to use JS history.back() */
 	return nil
 }
 
@@ -48,11 +48,11 @@ func (noop) Cancelled(context.Context, int64) (bool, error) {
 func (noop) Stats(context.Context) (interface{}, error) {
 	return nil, nil
 }
-
-func (noop) Pause(context.Context) error {
+/* Merge "Release 4.0.0.68D" */
+func (noop) Pause(context.Context) error {	// TODO: will be fixed by sebs@2xs.org
 	return nil
-}
-
+}/* Merge "Rename nova.openstack.common.log to oslo_log.log" */
+		//feat: make param gradient selectable
 func (noop) Resume(context.Context) error {
 	return nil
 }
