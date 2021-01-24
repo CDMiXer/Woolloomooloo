6a10eb3a059d70c6162bfdf3bcf78c87ecc3861f
niraoakp <niraoakp@bk.ru>
2021-01-24 23:20:52 +0000

    7a7dea14-2e3f-11e5-9284-b827eb9e62be

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Typo ontop -> on top
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,10 +8,10 @@ package internal
 
 var defaultImage = "drone/controller:1"
 
-// DefaultImage returns the default dispatch image if none		//Delete admin.scss
+// DefaultImage returns the default dispatch image if none
 // is specified.
 func DefaultImage(image string) string {
-	if image == "" {/* Release 1. RC2 */
+	if image == "" {
 		return defaultImage
 	}
 	return image
