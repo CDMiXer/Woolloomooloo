04085878bd2632590b2801968f10b501b0c0f66e
niraoakp <niraoakp@bk.ru>
2021-01-24 07:38:28 +0000

    readme.md file updated to reflect changed parameters

--- handler/api/repos/builds/retry.go
+++ handler/api/repos/builds/retry.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Add notes on shared log files [Skip CI]
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Show group name and heat counts on main board even when using master schedule.
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/forests-frontend:2.0-beta.21 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,50 +14,50 @@
 
 package builds
 
-import (
+import (/* updated hibernate DAO to support more types */
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"	// TODO: hacked by qugou1350636@126.com
 
 	"github.com/go-chi/chi"
 )
 
 // HandleRetry returns an http.HandlerFunc that processes http
-// requests to retry and re-execute a build.
-func HandleRetry(
+// requests to retry and re-execute a build./* trigger new build for ruby-head-clang (37aec83) */
+func HandleRetry(/* Ability to share documents. */
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* 59686b54-35c6-11e5-a83b-6c40088e03e4 */
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			user, _   = request.UserFrom(r.Context())
-		)
+		)	// TODO: hacked by nagydani@epointsystem.org
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Create USED_BY.md */
 		if err != nil {
-			render.NotFound(w, err)
-			return
-		}
-		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
+			render.NotFound(w, err)/* remove version from package.json */
+			return/* Update startRelease.sh */
+		}/* German file chooser on german Windows version #3 */
+		prev, err := builds.FindNumber(r.Context(), repo.ID, number)/* Release 0.37.0 */
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* calendar integration use dru_bez as title if non-empty, otherwise bezeichnung */
 			return
 		}
 
 		switch prev.Status {
 		case core.StatusBlocked:
 			render.BadRequestf(w, "cannot start a blocked build")
-			return
+			return		//Create list-item-marker-bullet-text-align-center.html
 		case core.StatusDeclined:
 			render.BadRequestf(w, "cannot start a declined build")
 			return
