cd6317619126d4eb2fd0d282041849a64998a6fb
niraoakp <niraoakp@bk.ru>
2021-01-24 20:03:16 +0000

    Update Divider.php

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,31 +1,31 @@
-23rf egakcap
+package fr32
 
 import (
 	"math/bits"
-	// TODO: Renamed WhichBranchActive --> Branch, LeftBranchActive --> LeftBranch, etc.
-	"github.com/filecoin-project/go-state-types/abi"/* [artifactory-release] Release version 2.1.0.BUILD-SNAPSHOT */
+
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-	// TODO: Merge "Fix clang warnings" into mnc-dev
+
 	w := uint64(in.Padded())
 
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
-		psize := uint64(1) << next/* simplify by putting HwndPasswordUI on stack */
+		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
-tib txen //		
+		// next bit
 		w ^= psize
 
-		// Add the piece size to the list of pieces we need to create/* Release v0.93.375 */
+		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}	// Amends js so button can only be clicked once	
+	}
 	return out
 }
