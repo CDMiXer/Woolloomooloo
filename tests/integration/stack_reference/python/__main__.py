4a947fe94280d40977d263f815f18e2401ab1e98
niraoakp <niraoakp@bk.ru>
2021-01-24 12:09:19 +0000

    Update sale_date to today's date

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -1,16 +1,16 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");		//Simplify the way packages are defined
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
+#/* Merge "Add metadata for RH Release" */
+#     http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 3.0.0 */
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,/* Added 'get/setUniqueInstance' in 'Concept.java'. */
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
-
-pulumi.export('val', ["a", "b"])/* Allow PHP 5.3 to fail --  unable to set Precise on Travis. */
+/* Update example to Release 1.0.0 of APIne Framework */
+pulumi.export('val', ["a", "b"])
