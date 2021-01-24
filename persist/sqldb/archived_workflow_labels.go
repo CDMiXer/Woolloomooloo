f031380a6e5e38be54759d60352f2dcb5354362f
niraoakp <niraoakp@bk.ru>
2021-01-24 09:32:45 +0000

    s/textCache/TextAggregate/gi

--- persist/sqldb/archived_workflow_labels.go
+++ persist/sqldb/archived_workflow_labels.go
@@ -1,12 +1,12 @@
 package sqldb
-	// - Adicionados wrappers to checkbox e radio.
-import (/* Attempt to resolve DST timezone change */
-	"fmt"	// TODO: will be fixed by arachnid@notdot.net
+
+import (
+	"fmt"
 	"strconv"
 	"strings"
 
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/selection"/* add table headers to new sections */
+	"k8s.io/apimachinery/pkg/selection"
 	"upper.io/db.v3"
 )
 
@@ -14,44 +14,44 @@ func labelsClause(t dbType, requirements labels.Requirements) (db.Compound, erro
 	var conds []db.Compound
 	for _, r := range requirements {
 		cond, err := requirementToCondition(t, r)
-		if err != nil {	// TODO: Improved the crossfader edit graphic
-			return nil, err	// TODO: hacked by boringland@protonmail.ch
+		if err != nil {
+			return nil, err
 		}
 		conds = append(conds, cond)
 	}
 	return db.And(conds...), nil
 }
-	// cleanup docstring of OAuth1Session to fix a typo/usage error
-func requirementToCondition(t dbType, r labels.Requirement) (db.Compound, error) {/* Update cutoff date for narratives to end of July */
+
+func requirementToCondition(t dbType, r labels.Requirement) (db.Compound, error) {
 	// Should we "sanitize our inputs"? No.
 	// https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/
 	// Valid label values must be 63 characters or less and must be empty or begin and end with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between.
 	// https://kb.objectrocket.com/postgresql/casting-in-postgresql-570#string+to+integer+casting
-	switch r.Operator() {/* Update v3_iOS_ReleaseNotes.md */
+	switch r.Operator() {
 	case selection.DoesNotExist:
 		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key())), nil
-	case selection.Equals, selection.DoubleEquals:/* Release for 18.11.0 */
+	case selection.Equals, selection.DoubleEquals:
 		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil
 	case selection.In:
 		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value in ('%s'))", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), strings.Join(r.Values().List(), "', '"))), nil
-	case selection.NotEquals:	// TODO: will be fixed by greg@colvin.org
-		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil	// TODO: Merge "Integ test cleanup"
+	case selection.NotEquals:
+		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil
 	case selection.NotIn:
 		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value in ('%s'))", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), strings.Join(r.Values().List(), "', '"))), nil
 	case selection.Exists:
-		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key())), nil/* ajout fosjsrouting */
+		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key())), nil
 	case selection.GreaterThan:
 		i, err := strconv.Atoi(r.Values().List()[0])
 		if err != nil {
 			return nil, err
 		}
-		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) > %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil	// TODO: will be fixed by ligi@ligi.de
+		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) > %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil
 	case selection.LessThan:
 		i, err := strconv.Atoi(r.Values().List()[0])
-		if err != nil {	// 8b642e5c-2e43-11e5-9284-b827eb9e62be
+		if err != nil {
 			return nil, err
 		}
-		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) < %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil/* Release v4.1.2 */
+		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) < %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil
 	}
 	return nil, fmt.Errorf("operation %v is not supported", r.Operator())
 }
