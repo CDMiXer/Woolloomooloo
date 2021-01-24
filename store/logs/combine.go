bc67a9a3785aa383cccb1a862ec7b7896c1073eb
niraoakp <niraoakp@bk.ru>
2021-01-24 03:56:24 +0000

    Updated Version number in README

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Update BFWrapper.py
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Update slowgoblins012.py
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs
-
+package logs/* now able to add new games */
+	// TODO: Fixed some Fortify findings.
 import (
 	"context"
 	"io"
 
 	"github.com/drone/drone/core"
-)
-
+)	// TODO: ee323dd6-2e6f-11e5-9284-b827eb9e62be
+	// pcm/PcmDsd: use struct ConstBuffer
 // NewCombined returns a new combined log store that will fallback
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
@@ -30,20 +30,20 @@ func NewCombined(primary, secondary core.LogStore) core.LogStore {
 		primary:   primary,
 		secondary: secondary,
 	}
-}
+}/* chore: Release 0.22.1 */
 
 type combined struct {
 	primary, secondary core.LogStore
 }
 
-func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
+func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {	// TODO: will be fixed by aeongrp@outlook.com
 	rc, err := s.primary.Find(ctx, step)
 	if err == nil {
-		return rc, err
+		return rc, err	// TODO: Merge "USB: visor: fix null-deref at probe" into m
 	}
 	return s.secondary.Find(ctx, step)
 }
-
+	// TODO: update membership status in view on change (fix for #377)
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
 }
@@ -53,7 +53,7 @@ func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
 }
 
 func (s *combined) Delete(ctx context.Context, step int64) error {
-	err := s.primary.Delete(ctx, step)
+	err := s.primary.Delete(ctx, step)/* run_test now uses Release+Asserts */
 	if err != nil {
 		err = s.secondary.Delete(ctx, step)
 	}
