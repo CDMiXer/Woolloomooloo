4fcbd734c5f5da9581048ab70eafa4926d0f3e93
niraoakp <niraoakp@bk.ru>
2021-01-24 09:03:32 +0000

    Merge remote-tracking branch 'upstream/master' into get-available

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -3,16 +3,16 @@
 import pulumi
 
 config = pulumi.Config()
-org = config.require('org')
+org = config.require('org')/* v0.3.1 Released */
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
 
 got_err = False
 
-try:
+try:/* Release areca-7.0-2 */
     a.get_output('val2')
 except Exception:
-    got_err = True
+    got_err = True	// Delete readarg.js
 
 if not got_err:
     raise Exception('Expected to get error trying to read secret from stack reference.')
