70c7f5078f272f487f8baaa8c78c8086ff01d80c
niraoakp <niraoakp@bk.ru>
2021-01-24 22:06:53 +0000

    Create configextra.yml

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Plotting: Start easing QT requirement, some tuning/fixes
+// that can be found in the LICENSE file.
 
-package branches	// TODO: hacked by boringland@protonmail.ch
+package branches
