a4c25a552874b0350251c7de2d5ec71c4d71412c
niraoakp <niraoakp@bk.ru>
2021-01-24 08:49:04 +0000

    remove example.py, fix a few small errors

--- grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
+++ grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
@@ -3,14 +3,14 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* Add note about RawGit CDN. */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release 3.2.3.455 Prima WLAN Driver" */
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release: v4.6.0 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -22,7 +22,7 @@ package balancergroup
 
 import (
 	"testing"
-
+	// added impl+test for maxLogSize of the filesystem writers
 	"google.golang.org/grpc/internal/grpctest"
 )
 
@@ -32,4 +32,4 @@ type s struct {
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}
+}	// suggester based on keyword stats
