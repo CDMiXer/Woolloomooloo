772fabd56c98ac49fc0a621fcf4741b81f192e05
niraoakp <niraoakp@bk.ru>
2021-01-24 07:10:13 +0000

    Update Class4Lua.lua

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
-
+		//div, not canvas, what was I thinking...
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
