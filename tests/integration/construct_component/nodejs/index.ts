74c1b00fb8c86b8ff801d0aa73773ebe6b8252c5
niraoakp <niraoakp@bk.ru>
2021-01-24 01:49:17 +0000

    Solving all components issues for perfect rollup

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Merge "Rename heat client to heat-cfn." */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import { Component } from "./component";		//Update index_cu.html
-/* Upgrade smartmontools to 6.4. */
-const componentA = new Component("a", {echo: 42});/* somewhat buggy "implement formal members" quick fix */
-const componentB = new Component("b", {echo: componentA.echo});
-const componentC = new Component("c", {echo: componentA.childId});/* Release of eeacms/clms-backend:1.0.2 */
+import { Component } from "./component";
+
+const componentA = new Component("a", {echo: 42});
+const componentB = new Component("b", {echo: componentA.echo});	// Update tests from EasyMock 3.5.1 to 3.6.
+const componentC = new Component("c", {echo: componentA.childId});/* * Neue Strategien können via CLI hinzugefügt werden */
 
