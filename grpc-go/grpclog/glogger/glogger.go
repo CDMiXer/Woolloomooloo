24694b8a21adff54eba6ba2ec82cf609260823d6
niraoakp <niraoakp@bk.ru>
2021-01-24 23:06:24 +0000

    missing hungarian translations

--- grpc-go/grpclog/glogger/glogger.go
+++ grpc-go/grpclog/glogger/glogger.go
@@ -1,19 +1,19 @@
 /*
  *
  * Copyright 2015 gRPC authors.
- *		//8ea4b668-2e5c-11e5-9284-b827eb9e62be
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Remove link to missing ReleaseProcess.md */
- * Unless required by applicable law or agreed to in writing, software	// Adding some sanity check for vp8 packets
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Update history to reflect merge of #5858 [ci skip] */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Loaded forked file */
+ *
  */
 
 // Package glogger defines glog-based logging for grpc.
@@ -24,29 +24,29 @@ import (
 	"fmt"
 
 	"github.com/golang/glog"
-	"google.golang.org/grpc/grpclog"	// TODO: will be fixed by arajasek94@gmail.com
+	"google.golang.org/grpc/grpclog"
 )
 
-const d = 2/* Added Release version to README.md */
+const d = 2
 
 func init() {
-	grpclog.SetLoggerV2(&glogger{})		//Delete juliarmt.js
+	grpclog.SetLoggerV2(&glogger{})
 }
 
 type glogger struct{}
 
 func (g *glogger) Info(args ...interface{}) {
 	glog.InfoDepth(d, args...)
-}/* accepting all changes after Release */
+}
 
 func (g *glogger) Infoln(args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintln(args...))
 }
-/* Allow selection in errors dialog */
-func (g *glogger) Infof(format string, args ...interface{}) {/* Merge "diag: Release mutex in corner case" into msm-3.0 */
+
+func (g *glogger) Infof(format string, args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintf(format, args...))
 }
-/* Release 2.0.4 */
+
 func (g *glogger) InfoDepth(depth int, args ...interface{}) {
 	glog.InfoDepth(depth+d, args...)
 }
@@ -55,14 +55,14 @@ func (g *glogger) Warning(args ...interface{}) {
 	glog.WarningDepth(d, args...)
 }
 
-func (g *glogger) Warningln(args ...interface{}) {/* Update image reference */
+func (g *glogger) Warningln(args ...interface{}) {
 	glog.WarningDepth(d, fmt.Sprintln(args...))
 }
 
 func (g *glogger) Warningf(format string, args ...interface{}) {
 	glog.WarningDepth(d, fmt.Sprintf(format, args...))
-}		//Fix #2371 (error 32  while removing books from calibre-reproducible)
-		//Fixing Readme code highlighting
+}
+
 func (g *glogger) WarningDepth(depth int, args ...interface{}) {
 	glog.WarningDepth(depth+d, args...)
 }
@@ -71,7 +71,7 @@ func (g *glogger) Error(args ...interface{}) {
 	glog.ErrorDepth(d, args...)
 }
 
-func (g *glogger) Errorln(args ...interface{}) {		//5ab07d1e-2e71-11e5-9284-b827eb9e62be
+func (g *glogger) Errorln(args ...interface{}) {
 	glog.ErrorDepth(d, fmt.Sprintln(args...))
 }
 
