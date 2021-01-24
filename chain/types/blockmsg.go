43b6c23b59aecd44bf7fdbebcbdaebbb1d64b5d3
niraoakp <niraoakp@bk.ru>
2021-01-24 05:40:48 +0000

    atualização da Moneta1.0 para moneta.1.0.1-SNAPSHOT

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -2,23 +2,23 @@ package types
 
 import (
 	"bytes"
-/* SSL default cert fix from ayourtch */
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by martin2cai@hotmail.com
+	// TODO: remove URLDecoderEditor
+	"github.com/ipfs/go-cid"
 )
-
-type BlockMsg struct {/* Releases Webhook for Discord */
-	Header        *BlockHeader		//Merge branch 'master' into PTX-1680
-	BlsMessages   []cid.Cid	// improve parallel building
+		//can delete image file
+type BlockMsg struct {		//Rename VS-3D-data.pd to vs-3D-data.pd
+	Header        *BlockHeader
+	BlsMessages   []cid.Cid
 	SecpkMessages []cid.Cid
 }
 
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
 	var bm BlockMsg
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
-		return nil, err/* Release of eeacms/forests-frontend:2.0-beta.66 */
+		return nil, err
 	}
 
-	return &bm, nil
+	return &bm, nil/* Merge "msm_vidc: venc: Release encoder buffers" */
 }
 
 func (bm *BlockMsg) Cid() cid.Cid {
