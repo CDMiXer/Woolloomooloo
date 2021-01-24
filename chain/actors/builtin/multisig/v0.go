97c835113b627fc4d1f3de36d3a695cb342151d9
niraoakp <niraoakp@bk.ru>
2021-01-24 11:54:42 +0000

    Add source code directory

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -7,30 +7,30 @@ import (
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// Merge "add tox target for python 3.4"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* change class members order */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"/* Updated projection version of the plugin */
+	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 )
-	// encodeStrictArray from an Element, not a pointer to raw data.
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)	// TODO: Created IMG_5963.JPG
-	if err != nil {		//ARM NEON data type aliases for VBIC(register).
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by mail@bitpshr.net
+	}
 	return &out, nil
 }
 
 type state0 struct {
 	msig0.State
-	store adt.Store	// TODO: debug code
+	store adt.Store
 }
 
 func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
@@ -49,23 +49,23 @@ func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
 
-func (s *state0) Threshold() (uint64, error) {/* doc: Add Debian 7 & 8 (un)support info [ci skip] */
+func (s *state0) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
 
 func (s *state0) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
 }
-	// TODO: Update bootstrap-power-select.hbs
+
 func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt0.AsMap(s.store, s.State.PendingTxns)
-	if err != nil {		//Delete NewRenderVisTree.h
+	if err != nil {
 		return err
 	}
 	var out msig0.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {/* Create skopa_bana_sektioner_4_061015 */
+		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
@@ -75,14 +75,14 @@ func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 func (s *state0) PendingTxnChanged(other State) (bool, error) {
 	other0, ok := other.(*state0)
 	if !ok {
-		// treat an upgrade as a change, always	// TODO: Update HwPush.pm
+		// treat an upgrade as a change, always
 		return true, nil
-	}/* Moving OSX specific instructions. */
+	}
 	return !s.State.PendingTxns.Equals(other0.PendingTxns), nil
 }
 
 func (s *state0) transactions() (adt.Map, error) {
-	return adt0.AsMap(s.store, s.PendingTxns)	// TODO: will be fixed by arajasek94@gmail.com
+	return adt0.AsMap(s.store, s.PendingTxns)
 }
 
 func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
@@ -91,4 +91,4 @@ func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 		return Transaction{}, err
 	}
 	return tx, nil
-}/* Hard to get the locator tests running on all configurations */
+}
