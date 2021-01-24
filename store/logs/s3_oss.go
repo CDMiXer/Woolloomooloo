8798402cac24ad6a95939733aa40fb88dd7ddf84
niraoakp <niraoakp@bk.ru>
2021-01-24 14:37:45 +0000

    1df2d174-2e58-11e5-9284-b827eb9e62be

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc./* More badges for the README */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Released URB v0.1.5 */
-// you may not use this file except in compliance with the License.		//Corrected spelling error!
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Rebuilt index with josephting
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//accurate variable names & cleanup
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: #278 Remember last saveAs dir
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* chore(package): update markdown-it to version 9.1.0 */
+// +build oss
 
-package logs/* version arrangée */
+sgol egakcap
 
 import "github.com/drone/drone/core"
 
-// New returns a zero value LogStore.		//96568210-2e48-11e5-9284-b827eb9e62be
+// New returns a zero value LogStore.		//78857740-5216-11e5-948b-6c40088e03e4
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
 	return nil
 }
