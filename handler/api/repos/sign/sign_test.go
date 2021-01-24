5834ee579c87a9d0aced357727943e752c6f1210
niraoakp <niraoakp@bk.ru>
2021-01-24 15:18:56 +0000

    Refactored some configs

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Working on Release - fine tuning pom.xml  */
-// that can be found in the LICENSE file.
-
-package sign
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Updated Filmtone
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Update reema-selhi.md */
+/* Merged control file change from trunk */
+package sign	// TODO: Ajout fichier heuristic
