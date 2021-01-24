8178d999e38643cf10a3bfaf65da862ee27f199f
niraoakp <niraoakp@bk.ru>
2021-01-24 12:35:44 +0000

    Bring repository up-to-date

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* refactor SymbolTable for kicks */
-package ints
+
+package ints/* Release Candidate 0.5.6 RC3 */
 
 import (
-	"testing"
+	"testing"		//Update the page for Displays, inside the Entity Browser documentation.
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
@@ -12,57 +12,57 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Test that the engine tolerates two deletions of the same URN in the same plan.		//opcion1.html
-func TestDoublePendingDelete(t *testing.T) {	// TODO: db8faf8a-2e40-11e5-9284-b827eb9e62be
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// doc(contributing): remove unnecessary note
+// Test that the engine tolerates two deletions of the same URN in the same plan.
+func TestDoublePendingDelete(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,		//Merge branch 'master' into removeTestCodeInProductionCode
-		EditDirs: []integration.EditDir{
-			{
-				Dir:           "step2",
+		Dependencies: []string{"@pulumi/pulumi"},/* Added Release Dataverse feature. */
+		Quick:        true,
+		EditDirs: []integration.EditDir{/* Released springrestcleint version 2.4.13 */
+{			
+				Dir:           "step2",/* Merge branch 'master' of https://github.com/Softgreen/SISTCOOP_REST.git */
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					assert.NotNil(t, stackInfo.Deployment)/* Release of v1.0.4. Fixed imports to not be weird. */
+					assert.NotNil(t, stackInfo.Deployment)
 
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]/* Release of version 1.2.2 */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// TODO: remove constructor
 
-					a := stackInfo.Deployment.Resources[2]/* Merge "Fix Release PK in fixture" */
+					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)/* Release 5.0.0.rc1 */
+					assert.False(t, a.Delete)	// Created new branch details_page
 
 					aCondemned := stackInfo.Deployment.Resources[3]
-					assert.Equal(t, "a", string(aCondemned.URN.Name()))/* Release of eeacms/www:18.9.27 */
+					assert.Equal(t, "a", string(aCondemned.URN.Name()))/* Release Version 2.2.5 */
 					assert.True(t, aCondemned.Delete)
 
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
 
-				},
-			},/* Released v0.1.8 */
+				},	// ADD: PHP info script
+			},
 			{
-				Dir:           "step3",	// Create ДО (присвоение на отрезке)
+				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// There is still only one pending delete resource in this snapshot./* Merge "Release 3.0.10.037 Prima WLAN Driver" */
+					// There is still only one pending delete resource in this snapshot.
 					assert.NotNil(t, stackInfo.Deployment)
 
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]/* Create project images */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]		//Remove old DJGPP NTVDM patch files.
+					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-	// Merge branch 'develop' into feature/jdf/error
-					a := stackInfo.Deployment.Resources[2]/* Release 3.2 097.01. */
-					assert.Equal(t, "a", string(a.URN.Name()))		//removed outdated checkerboard example, is covered by parsely example.
+
+					a := stackInfo.Deployment.Resources[2]
+					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
 
 					aCondemned := stackInfo.Deployment.Resources[3]
@@ -79,17 +79,17 @@ func TestDoublePendingDelete(t *testing.T) {	// TODO: db8faf8a-2e40-11e5-9284-b8
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// We should have cleared out all of the pending deletes now.
-					assert.NotNil(t, stackInfo.Deployment)
+					assert.NotNil(t, stackInfo.Deployment)/* 5.7.1 Release */
 
 					assert.Equal(t, 4, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-
+/* Release 1.0.0-alpha6 */
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)
+					assert.False(t, a.Delete)/* Release native object for credentials */
 
 					b := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "b", string(b.URN.Name()))
