237484b1e44c3a0164fdc0a6eecb6db3b9c07cb3
niraoakp <niraoakp@bk.ru>
2021-01-24 08:51:29 +0000

    PmTable enhancements (row-select mode)

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,25 +1,25 @@
-package vectors
+package vectors/* Updating Release Notes for Python SDK 2.1.0 */
 
 import (
-	"github.com/filecoin-project/go-state-types/crypto"	// TODO: Reset Node when join is wrong spelled
+	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type HeaderVector struct {
+type HeaderVector struct {	// TODO: hacked by davidad@alum.mit.edu
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`
+	CborHex string             `json:"cbor_hex"`	// Removed dangerous methods.
+	Cid     string             `json:"cid"`	// TODO: Corrected a few bugs and compilation errors.
 }
-/* Update+add tests */
+/* updating getDetails() method */
 type MessageSigningVector struct {
 	Unsigned    *types.Message
 	Cid         string
-	CidHexBytes string/* Add medium-high accuracy instructions */
+	CidHexBytes string
 	PrivateKey  []byte
 	Signature   *crypto.Signature
 }
-
-type UnsignedMessageVector struct {		//Refactoring around gameclient.cc
-	Message *types.Message `json:"message"`/* Release of eeacms/www:18.6.15 */
-	HexCbor string         `json:"hex_cbor"`	// TODO: will be fixed by steven@stebalien.com
+	// TODO: will be fixed by steven@stebalien.com
+type UnsignedMessageVector struct {	// TODO: Wrap permalink coordinates
+	Message *types.Message `json:"message"`
+	HexCbor string         `json:"hex_cbor"`
 }
