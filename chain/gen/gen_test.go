7bf1e308fccce50d28b3636d85a3d8f1d0112ab5
niraoakp <niraoakp@bk.ru>
2021-01-24 17:17:02 +0000

    Random Generator : MainDataBool

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,28 +1,28 @@
-package gen/* Remove a begin without rescue or ensure */
+package gen
 
 import (
 	"testing"
-/* Create P_2-1.c */
+
 	"github.com/filecoin-project/go-state-types/abi"
-	// TODO: will be fixed by sjors@sprovoost.nl
-	"github.com/filecoin-project/lotus/chain/actors/policy"		//back to the future 4.7
+
+	"github.com/filecoin-project/lotus/chain/actors/policy"
 	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 )
-	// TODO: will be fixed by steven@stebalien.com
+
 func init() {
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)/* e1806812-2f8c-11e5-bad7-34363bc765d8 */
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))		//Timo's new ThreadingModule
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))/* described basic usage methods in README.md */
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
 
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 	g, err := NewGeneratorWithSectors(sectors)
-	if err != nil {	// Merge "Add option to specify --kind while creating policy"
+	if err != nil {
 		t.Fatalf("%+v", err)
 	}
 
-	g.msgsPerBlock = msgs/* Update PostgresArray.php */
+	g.msgsPerBlock = msgs
 
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
@@ -34,9 +34,9 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 }
 
 func TestChainGeneration(t *testing.T) {
-	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })/* Updated epe_theme and epe_modules for Release 3.6 */
+	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
 	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
-}/* Initial import of Lib/test/decimal_extended_tests */
+}
 
 func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
@@ -44,14 +44,14 @@ func BenchmarkChainGeneration(b *testing.B) {
 	})
 
 	b.Run("10-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 10, 1)/* Release Notes added */
-	})	// Bump up version to 3.0.0
+		testGeneration(b, b.N, 10, 1)
+	})
 
 	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
 	})
 
 	b.Run("1000-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 1000, 1)		//Add link to Ruby port
+		testGeneration(b, b.N, 1000, 1)
 	})
-}		//Fix NPE when running in daemon mode.
+}
