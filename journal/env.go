ae231d995fc06da24b06e684a94057dba50432d8
niraoakp <niraoakp@bk.ru>
2021-01-24 19:39:18 +0000

    "GPL is the devil" - glua chat

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
 
-import (/* Factorize optional homomorphism type. */
+import (/* Release of eeacms/bise-frontend:1.29.16 */
 	"os"
 )
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
-/* Release changes */
+
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
-		}		//NEW Introduce function dolGetFirstLineOfText
-	}
+		}
+	}	// TODO: 51a02c74-2e5d-11e5-9284-b827eb9e62be
 	// fallback if env variable is not set, or if it failed to parse.
 	return DefaultDisabledEvents
 }
