ef286b1dd5b66c980b1c592c53d38aa9529d5cf2
niraoakp <niraoakp@bk.ru>
2021-01-24 07:55:16 +0000

    New draw_text_intro.py

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,20 +1,20 @@
 package helpers
 
-import (
-	"context"/* Merge "Enhance error message if skip-validation is used with magic branch push" */
+import (	// TODO: hacked by peterke@gmail.com
+	"context"
 
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* Release notes for 1.0.74 */
 )
-
+	// TODO: hacked by fjl@ethereum.org
 // MetricsCtx is a context wrapper with metrics
-type MetricsCtx context.Context	// TODO: thin as production server
+type MetricsCtx context.Context
 
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
-func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
-	ctx, cancel := context.WithCancel(mctx)
+func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {		//Frases vacias efectos de los ataques
+	ctx, cancel := context.WithCancel(mctx)/* Return the task object. */
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
