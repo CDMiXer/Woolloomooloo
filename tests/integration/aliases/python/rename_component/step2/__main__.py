65ad8c6658c1d2ddd0ca455f66b8f9e1b56b6dc0
niraoakp <niraoakp@bk.ru>
2021-01-24 17:49:32 +0000

    added constructor and comments

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -1,22 +1,22 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Fix erreur en production */
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):/* [artifactory-release] Release version 3.3.7.RELEASE */
-    def __init__(self, name, opts=None):	// TODO: Log latest changes
+class Resource1(ComponentResource):
+    def __init__(self, name, opts=None):		//added dataset extractor
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #3 - rename a component (and all it's children)
 # No change to the component...
-class ComponentThree(ComponentResource):	// Changes in send_email method for report generation
-    def __init__(self, name, opts=None):
+class ComponentThree(ComponentResource):
+    def __init__(self, name, opts=None):	// Rights controller to angular
         super().__init__("my:module:ComponentThree", name, None, opts)
-        # Note that both un-prefixed and parent-name-prefixed child names are supported. For the/* Hook up warning for an incomplete scanlist in scanf format strings. */
-        # later, the implicit alias inherited from the parent alias will include replacing the name
+        # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
+        # later, the implicit alias inherited from the parent alias will include replacing the name/* short description in README */
         # prefix to match the parent alias name.
-        resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
+        resource1 = Resource1(name + "-child", ResourceOptions(parent=self))	// Adapted code to new page design
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
 
 # ...but applying an alias to the instance successfully renames both the component and the children.
-comp3 = ComponentThree("newcomp3", ResourceOptions(	// TODO: hacked by mowrain@yandex.com
+comp3 = ComponentThree("newcomp3", ResourceOptions(
     aliases=[Alias(name="comp3")]))
