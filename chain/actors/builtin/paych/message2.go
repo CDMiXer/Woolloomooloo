d08f7a079509a0d4a37df36ddc63cdaf2b21730b
niraoakp <niraoakp@bk.ru>
2021-01-24 10:29:49 +0000

    Update CHANGELOG for #4826

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -1,23 +1,23 @@
-package paych
+package paych	// TODO: will be fixed by steven@stebalien.com
 
-import (
+import (/* Refactor to abstract huge inner class */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Better term in jQuery intro. */
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
+	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"/* 97: Use GregorianCalendar instead of java.util.Calendar */
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
-type message2 struct{ from address.Address }
+		//bugfix video folders
+type message2 struct{ from address.Address }	// TODO: fr.js - GETH
 
 func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {
+	if aerr != nil {	// (no ticket) Fixed formatting in README
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init2.ExecParams{
@@ -25,24 +25,24 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		ConstructorParams: params,
 	})
 	if aerr != nil {
-		return nil, aerr
+		return nil, aerr/* Remove unused and non-PEP-related entry from PyBufferProcs */
 	}
 
-	return &types.Message{
-		To:     init_.Address,
+	return &types.Message{/* 371508 Release ghost train in automode */
+		To:     init_.Address,	// TODO: small RNA category pie chart
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
-	}, nil
+	}, nil/* Release of eeacms/www-devel:18.6.20 */
 }
 
 func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
+	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{/* Release of eeacms/eprtr-frontend:0.4-beta.25 */
 		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {
+	if aerr != nil {/* @Release [io7m-jcanephora-0.10.1] */
 		return nil, aerr
 	}
 
@@ -52,7 +52,7 @@ func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.UpdateChannelState,
 		Params: params,
-	}, nil
+	}, nil/* - aktualizacja layoutu do wyświetlania menu */
 }
 
 func (m message2) Settle(paych address.Address) (*types.Message, error) {
@@ -63,7 +63,7 @@ func (m message2) Settle(paych address.Address) (*types.Message, error) {
 		Method: builtin2.MethodsPaych.Settle,
 	}, nil
 }
-
+/* add the scm url */
 func (m message2) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
