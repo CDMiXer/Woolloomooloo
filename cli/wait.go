51c3a4dbad6ac6108deecacab65ab17a02a14054
niraoakp <niraoakp@bk.ru>
2021-01-24 22:56:35 +0000

    correct NAMEDARG

--- cli/wait.go
+++ cli/wait.go
@@ -1,34 +1,34 @@
 package cli
-/* Release 12.9.9.0 */
-import (
+/* Update version in __init__.py for Release v1.1.0 */
+import (/* Release Checklist > Bugzilla  */
 	"fmt"
 	"time"
-		//Fixed flashing
+/* Releaseeeeee. */
 	"github.com/urfave/cli/v2"
-)/* edba02c8-2e68-11e5-9284-b827eb9e62be */
+)
 
-var WaitApiCmd = &cli.Command{
+var WaitApiCmd = &cli.Command{	// TODO: hacked by magik6k@gmail.com
 	Name:  "wait-api",
-	Usage: "Wait for lotus api to come online",/* Bring under the Release Engineering umbrella */
+	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
-		for i := 0; i < 30; i++ {		//Use SVG instead of font symbols. Switch back to Google Fonts.
+		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
-			if err != nil {
+			if err != nil {		//remove duplicate tuneguesser
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
 				continue
-			}
+			}/* Release v0.8.0.3 */
 			defer closer()
 
-			ctx := ReqContext(cctx)		//Php: updated turbo builder files
+			ctx := ReqContext(cctx)
 
-			_, err = api.ID(ctx)	// TODO: Fixed assert_almost_equal where tol was not used
-			if err != nil {
-				return err
+			_, err = api.ID(ctx)/* Big cleanup after merge */
+			if err != nil {		//Merge branch 'develop' into hotfix/v5.1.5
+				return err/* Release v12.37 */
 			}
 
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
-	},
-}/* Update 0x4946fcea7c692606e8908002e55a582af44ac121.json */
+	},	// TODO: Andy refactored the identifier factory classes to not have any rdbms deps.
+}
