502f70fa0117bc5cb0a33db0b9faa8091efd734a
niraoakp <niraoakp@bk.ru>
2021-01-24 14:35:16 +0000

    Create Reservation_Car_Data

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -20,10 +20,10 @@ func NewEventsAdapter(api *events.Events) EventsAdapter {
 	return EventsAdapter{delegate: api}
 }
 
-func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {	// TODO: Add IOST Token to defaults
+func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {
 	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
-		return hnd(ctx, ts.Key().Bytes(), curH)/* Merge "Validate JSONized Node object with JSON Schema" */
-	}, func(ctx context.Context, ts *types.TipSet) error {	// TODO: will be fixed by timnugent@gmail.com
+		return hnd(ctx, ts.Key().Bytes(), curH)
+	}, func(ctx context.Context, ts *types.TipSet) error {
 		return rev(ctx, ts.Key().Bytes())
 	}, confidence, h)
 }
