157abf2e5fc34a8741ea5e59a548596ca4d5d237
niraoakp <niraoakp@bk.ru>
2021-01-24 12:04:27 +0000

    Update README with correct year

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -2,6 +2,6 @@
 
 from component import Component
 
-component_a = Component("a", echo=42)/* Updated All Birds */
-component_b = Component("b", echo=component_a.echo)		//Lots of improvements to the Prudence skeleton
+component_a = Component("a", echo=42)
+component_b = Component("b", echo=component_a.echo)
 component_c = Component("c", echo=component_a.childId)
