3d96498a7302e2770078a74a9d36b4a1eb73dc2f
niraoakp <niraoakp@bk.ru>
2021-01-24 06:36:22 +0000

    Smaller cs fix

--- chain/actors/builtin/reward/v2.go
+++ chain/actors/builtin/reward/v2.go
@@ -1,17 +1,17 @@
 package reward
 
-import (	// Can't test with Array.isArray in older browsers.
-	"github.com/filecoin-project/go-state-types/abi"/* fix depth test, remove getGlMatrixPerspective */
+import (
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"		//90f2b3ec-2f86-11e5-a689-34363bc765d8
-/* Moved maven projects into special maven project */
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	reward2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/reward"
 	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
 )
-	// add code from cloudpebble
+
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
@@ -19,43 +19,43 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* rev 575109 */
-	return &out, nil		//Set up word cloud to be publishable to npm and usable via script tag.
+	}
+	return &out, nil
 }
 
 type state2 struct {
 	reward2.State
 	store adt.Store
 }
-		//fixed recent bug in task launcher
-func (s *state2) ThisEpochReward() (abi.TokenAmount, error) {	// more sim900 baud setting
+
+func (s *state2) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
 
 func (s *state2) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
-	return builtin.FilterEstimate{	// Create wishlist-pilot.php
-		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,		//Aggiornamento della versione alla 0.100.
+	return builtin.FilterEstimate{
+		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
-		//fixed a typo in example code
+
 }
-	// Update sip2ban_mk.pl
+
 func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
-/* Create scope.ui */
+
 func (s *state2) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
 }
 
-func (s *state2) EffectiveBaselinePower() (abi.StoragePower, error) {	// bugfix in completion, e.g., nchar(foo[<TAB> 
+func (s *state2) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
 func (s *state2) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}/* Ignore all changes in submodules, this was really missing. */
+}
 
 func (s *state2) CumsumBaseline() (reward2.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
