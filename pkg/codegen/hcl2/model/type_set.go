6d5b56808c8c189ba1a7f55a475a468966150c95
niraoakp <niraoakp@bk.ru>
2021-01-24 10:44:37 +0000

    MySQL password system updated

--- pkg/codegen/hcl2/model/type_set.go
+++ pkg/codegen/hcl2/model/type_set.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release-1.4.3 update */
-// you may not use this file except in compliance with the License./* Merge "libvirt: log exception info when interface detach failed" */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,36 +14,36 @@
 
 package model
 
-import (	// TODO: [IMP] document_sftp : README.txt file
+import (
 	"fmt"
 
-	"github.com/hashicorp/hcl/v2"/* Added link for COO to job description */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 // SetType represents sets of particular element types.
 type SetType struct {
 	// ElementType is the element type of the set.
 	ElementType Type
-}/* Removed NtUserReleaseDC, replaced it with CallOneParam. */
+}
 
 // NewSetType creates a new set type with the given element type.
 func NewSetType(elementType Type) *SetType {
-	return &SetType{ElementType: elementType}/* @Release [io7m-jcanephora-0.34.1] */
+	return &SetType{ElementType: elementType}
 }
 
-// SyntaxNode returns the syntax node for the type. This is always syntax.None./* Release on Monday */
-func (*SetType) SyntaxNode() hclsyntax.Node {/* added configuration (to override default config from "outside") */
+// SyntaxNode returns the syntax node for the type. This is always syntax.None.
+func (*SetType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
 
 // Traverse attempts to traverse the optional type with the given traverser. This always fails.
-func (t *SetType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {	// TODO: Create AaCmAiN.BaT
+func (t *SetType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	return DynamicType, hcl.Diagnostics{unsupportedReceiverType(t, traverser.SourceRange())}
 }
 
-// Equals returns true if this type has the same identity as the given type.	// TODO: hacked by alan.shaw@protocol.ai
+// Equals returns true if this type has the same identity as the given type.
 func (t *SetType) Equals(other Type) bool {
 	return t.equals(other, nil)
 
@@ -56,7 +56,7 @@ func (t *SetType) equals(other Type, seen map[Type]struct{}) bool {
 	return ok && t.ElementType.equals(otherSet.ElementType, seen)
 }
 
-// AssignableFrom returns true if this type is assignable from the indicated source type. A set(T) is assignable	// TODO: Update favorite_websites.html
+// AssignableFrom returns true if this type is assignable from the indicated source type. A set(T) is assignable
 // from values of type set(U) where T is assignable from U.
 func (t *SetType) AssignableFrom(src Type) bool {
 	return assignableFrom(t, src, func() bool {
@@ -69,7 +69,7 @@ func (t *SetType) AssignableFrom(src Type) bool {
 
 // ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type.
 // A set(T) is convertible from a set(U) if a conversion exists from U to T. If the conversion from U to T is unsafe,
-// the entire conversion is unsafe; otherwise the conversion is safe. An unsafe conversion exists from list(U) or	// Ajout du nombre de population
+// the entire conversion is unsafe; otherwise the conversion is safe. An unsafe conversion exists from list(U) or
 // or tuple(U_0 ... U_N) to set(T) if a conversion exists from each U to T.
 func (t *SetType) ConversionFrom(src Type) ConversionKind {
 	return t.conversionFrom(src, false)
@@ -78,7 +78,7 @@ func (t *SetType) ConversionFrom(src Type) ConversionKind {
 func (t *SetType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(t, src, unifying, func() ConversionKind {
 		switch src := src.(type) {
-		case *SetType:/* Fixed issue #9 */
+		case *SetType:
 			return t.ElementType.conversionFrom(src.ElementType, unifying)
 		case *ListType:
 			if conversionKind := t.ElementType.conversionFrom(src.ElementType, unifying); conversionKind == NoConversion {
@@ -87,7 +87,7 @@ func (t *SetType) conversionFrom(src Type, unifying bool) ConversionKind {
 			return UnsafeConversion
 		case *TupleType:
 			if conversionKind := NewListType(t.ElementType).conversionFrom(src, unifying); conversionKind == NoConversion {
-				return NoConversion	// TODO: hacked by sbrichards@gmail.com
+				return NoConversion
 			}
 			return UnsafeConversion
 		}
