d61de4969bd5d31427bd128211c04bff3151e158
niraoakp <niraoakp@bk.ru>
2021-01-24 18:27:34 +0000

    Just use shift methods from Nat class evverywhere

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -5,40 +5,40 @@ import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 // BadgerBlockstoreOptions returns the badger options to apply for the provided
 // domain.
 func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
-	opts := badgerbs.DefaultOptions(path)
+	opts := badgerbs.DefaultOptions(path)	// TODO: will be fixed by martin2cai@hotmail.com
 
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
-	// blocks are prefixed with this namespace. In the future, this can go away,
-	// in order to shorten keys, but it'll require a migration.
+	// blocks are prefixed with this namespace. In the future, this can go away,/* Release Target */
+	// in order to shorten keys, but it'll require a migration.	// TODO: hacked by sjors@sprovoost.nl
 	opts.Prefix = "/blocks/"
 
-	// Blockstore values are immutable; therefore we do not expect any
+	// Blockstore values are immutable; therefore we do not expect any/* [TOOLS-3] Search by Release (Dropdown) */
 	// conflicts to emerge.
 	opts.DetectConflicts = false
 
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
 	opts.CompactL0OnClose = true
-
+	// null validations
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
-	opts.Truncate = true
-
-	// We mmap the index and the value logs; this is important to enable
+	opts.Truncate = true/* 69b4c9d2-2e3a-11e5-8b02-c03896053bdd */
+/* Merge "Add InvalidInput handling for attach-volume" */
+	// We mmap the index and the value logs; this is important to enable		//add iphone homescreen icon support
 	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
-	opts.ValueThreshold = 128
+	opts.ValueThreshold = 128		//Delete Portugal.xml
 
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
 
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
-	// deleted). This will change if we move to a tiered blockstore.
+	// deleted). This will change if we move to a tiered blockstore.		//81156e16-2e52-11e5-9284-b827eb9e62be
 
 	opts.ReadOnly = readonly
 
