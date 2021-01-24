f0d0f2a31c6ecd1536fb90bdc917a5617e5ed6ae
niraoakp <niraoakp@bk.ru>
2021-01-24 16:09:59 +0000

    Add le may may acute angles :^)

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release packaging wrt webpack */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//fixed bug: arithmetic negative was tranlsated as boolean negative.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Create CetakStruck.java
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Delete Release-Numbering.md */
+
 package repos
 
 import (
@@ -20,36 +20,36 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* [dist] Release v0.5.7 */
+	"github.com/drone/drone/logger"
 )
 
 // HandleAll returns an http.HandlerFunc that processes http
 // requests to list all repositories in the database.
-func HandleAll(repos core.RepositoryStore) http.HandlerFunc {		//Create kodi-checkinstall.txt
+func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			page    = r.FormValue("page")
 			perPage = r.FormValue("per_page")
 		)
-		offset, _ := strconv.Atoi(page)/* Created the tag for the 0.3.2 distribution. */
+		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 { // || limit > 100
 			limit = 25
 		}
-		switch offset {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		switch offset {
 		case 0, 1:
 			offset = 0
 		default:
 			offset = (offset - 1) * limit
-		}/* Release version 0.14.1. */
-		repo, err := repos.ListAll(r.Context(), limit, offset)		//[feenkcom/gtoolkit#1153] fix memory leak in GtWorldElement>>#showSpace:select:
+		}
+		repo, err := repos.ListAll(r.Context(), limit, offset)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				Debugln("api: cannot list repositories")
 		} else {
-			render.JSON(w, repo, 200)		//move files into place, adjust paths
+			render.JSON(w, repo, 200)
 		}
-	}/* [artifactory-release] Release version 3.1.0.RELEASE */
+	}
 }
