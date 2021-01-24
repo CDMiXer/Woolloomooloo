0c98490a98c80d45861d88c310f42df2aa6f38ae
niraoakp <niraoakp@bk.ru>
2021-01-24 15:09:11 +0000

    Committing send email code.

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package validator
+package validator/*     * Add possibility to change tmezone in myAccount page */
 
 import (
 	"testing"
@@ -21,29 +21,29 @@ import (
 )
 
 func TestFilter_None(t *testing.T) {
-	f := Filter(nil, nil)
-	if err := f.Validate(noContext, nil); err != nil {
+	f := Filter(nil, nil)		//added android
+	if err := f.Validate(noContext, nil); err != nil {/* use compatible PHPUnit version on PHP 7.2 */
 		t.Error(err)
 	}
-}
+}/* Create freeculturepage.xml */
 
-func TestFilter_Include(t *testing.T) {
+func TestFilter_Include(t *testing.T) {/* change "weitere" to "verwandte" */
 	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
-
+	// TODO: hacked by boringland@protonmail.ch
 	f := Filter([]string{"octocat/hello-world"}, nil)
-	if err := f.Validate(noContext, args); err != nil {
+	if err := f.Validate(noContext, args); err != nil {		//Add Inverse Bloom Filter section
 		t.Error(err)
 	}
 
 	f = Filter([]string{"octocat/*"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)
+		t.Error(err)		//chore(readme): minor adjustments
 	}
 
 	f = Filter([]string{"spaceghost/*"}, nil)
-	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
+	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {	// Fix buggy navbar logo and hamburger on mobile
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
 }
@@ -51,19 +51,19 @@ func TestFilter_Include(t *testing.T) {
 func TestFilter_Exclude(t *testing.T) {
 	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
-	}
-
+	}/* Release version 2.2.6 */
+	// TODO: post processing edge effect
 	f := Filter(nil, []string{"octocat/hello-world"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
-	}
+	}		//Delete curr_line.cpython-35.pyc
 
 	f = Filter(nil, []string{"octocat/*"})
-	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
-		t.Errorf("Expect ErrValidatorSkip, got %s", err)
-	}
+	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {	// TODO: Fix UTF-16 to UTF-8 conversion on surrogates.
+		t.Errorf("Expect ErrValidatorSkip, got %s", err)/* Released 5.0 */
+	}	// Update from Forestry.io - Deleted testing-out-how-this-works.md
 
-	f = Filter(nil, []string{"spaceghost/*"})
+	f = Filter(nil, []string{"spaceghost/*"})/* Bone parenting works but should be considered a temp fix */
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
