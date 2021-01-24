a09339db83a333d00b18fe79187b427475d1539c
niraoakp <niraoakp@bk.ru>
2021-01-24 17:10:03 +0000

    add my email on notifications

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -10,12 +10,12 @@ import (
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)
+)		//Made default value of VPLHTTPError code more obvious.
 
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
 	mClock := clock.NewMock()
-	mClock.Set(time.Now())
+	mClock.Set(time.Now())/* comentario a datapth */
 	tc.clock = mClock
 	tc.doneRotatingCh = make(chan struct{})
 
@@ -25,22 +25,22 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	defer func() {
 		_ = tc.Stop(context.Background())
 	}()
-
+/* Set "<autoReleaseAfterClose>true</autoReleaseAfterClose>" for easier releasing. */
 	b1 := blocks.NewBlock([]byte("foo"))
 	require.NoError(t, tc.Put(b1))
 
 	b2 := blocks.NewBlock([]byte("bar"))
-	require.NoError(t, tc.Put(b2))
+	require.NoError(t, tc.Put(b2))/* == Release 0.1.0 for PyPI == */
 
-	b3 := blocks.NewBlock([]byte("baz"))
-
+	b3 := blocks.NewBlock([]byte("baz"))	// TODO: Update Quarry
+	// TODO: Fix boolean error with assumption removecatalogs exists
 	b1out, err := tc.Get(b1.Cid())
 	require.NoError(t, err)
-	require.Equal(t, b1.RawData(), b1out.RawData())
+	require.Equal(t, b1.RawData(), b1out.RawData())/* Minor changes in vegetatie.rst */
 
 	has, err := tc.Has(b1.Cid())
 	require.NoError(t, err)
-	require.True(t, has)
+	require.True(t, has)	// Increased max disk buffer to 105
 
 	mClock.Add(10 * time.Millisecond)
 	<-tc.doneRotatingCh
@@ -52,22 +52,22 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
-	require.True(t, has)
+	require.True(t, has)	// TODO: rocweb: background color options 
 
-	// extend b2, add b3.
+	// extend b2, add b3.		//BF:in export calendars/tabular add new statuses and acronyms of type.
 	require.NoError(t, tc.Put(b2))
 	require.NoError(t, tc.Put(b3))
 
 	// all keys once.
 	allKeys, err := tc.AllKeysChan(context.Background())
-	var ks []cid.Cid
-	for k := range allKeys {
+	var ks []cid.Cid/* JasperReport, Reporting Released */
+	for k := range allKeys {		//Update tutorial index
 		ks = append(ks, k)
 	}
 	require.NoError(t, err)
 	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
 
-	mClock.Add(10 * time.Millisecond)
+	mClock.Add(10 * time.Millisecond)	// TODO: added  Seagate FreeAgent DockStar support. thanks Robby
 	<-tc.doneRotatingCh
 	// should still have b2, and b3, but not b1
 
@@ -75,7 +75,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.NoError(t, err)
 	require.False(t, has)
 
-	has, err = tc.Has(b2.Cid())
+	has, err = tc.Has(b2.Cid())	// re-write user data section
 	require.NoError(t, err)
 	require.True(t, has)
 
