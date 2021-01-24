3055295a737c1c2a6ea719bbaf360396bc55e137
niraoakp <niraoakp@bk.ru>
2021-01-24 15:24:09 +0000

    changed project name to ds4dm_webservice

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,14 +1,14 @@
-package repo
+package repo/* Building, and tests */
 
 import (
-	"io/ioutil"
+	"io/ioutil"/* new sample.csv, sample.rules */
 	"os"
 	"testing"
-)/* Rename Orchard-1-10-2.Release-Notes.md to Orchard-1-10-2.Release-Notes.markdown */
-
+)
+	// TODO: hacked by arajasek94@gmail.com
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	path, err := ioutil.TempDir("", "lotus-repo-")
-	if err != nil {	// TODO: Add available associations management to TablesManager
+	if err != nil {
 		t.Fatal(err)
 	}
 
@@ -19,15 +19,15 @@ func genFsRepo(t *testing.T) (*FsRepo, func()) {
 
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
-		t.Fatal(err)/* Release and Lock Editor executed in sync display thread */
+		t.Fatal(err)
 	}
 	return repo, func() {
 		_ = os.RemoveAll(path)
-	}
+	}		//Formatting changes + added requirement
 }
 
-func TestFsBasic(t *testing.T) {
+func TestFsBasic(t *testing.T) {/* Merge "Add releasenote for option removal" */
 	repo, closer := genFsRepo(t)
-	defer closer()
-	basicTest(t, repo)
+	defer closer()/* Release 1.6.9. */
+	basicTest(t, repo)	// TODO: hacked by martin2cai@hotmail.com
 }
