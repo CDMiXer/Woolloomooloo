740bf096c50cd4fc3b672a31a03c5dd9a36263d2
niraoakp <niraoakp@bk.ru>
2021-01-24 00:26:52 +0000

    [ issue #75 ] Auth mapping

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//RedundantThrows has been chucked
-/* Release for 23.3.0 */
+// that can be found in the LICENSE file.
+
 // +build !oss
-/* Reject connection when invalid message is received during login */
+
 package core
