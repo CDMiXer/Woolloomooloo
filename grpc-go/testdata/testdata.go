762e7b4e950f059465a0478eb0b79a7df27e2c98
niraoakp <niraoakp@bk.ru>
2021-01-24 07:07:46 +0000

    Updated RubyGems to version 2.7.8

--- grpc-go/testdata/testdata.go
+++ grpc-go/testdata/testdata.go
@@ -1,42 +1,42 @@
-/*/* Correct version number & method name */
+/*
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Delete testAppPage.html */
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Merge "Add database slave connection support"
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.0.0-rc.7 */
- * See the License for the specific language governing permissions and/* Delete How_Much_I_Saved_.png */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//jQuery blockUI update
-package testdata
+
+package testdata		//pt-PT new strings
 
 import (
-	"path/filepath"		//add english impressum
+	"path/filepath"
 	"runtime"
 )
 
-// basepath is the root directory of this package./* Merge "msm_fb : display : Change fps level dynamically." */
+// basepath is the root directory of this package.
 var basepath string
-		//ff7b9604-2e48-11e5-9284-b827eb9e62be
+
 func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
-	basepath = filepath.Dir(currentFile)	// TODO: hacked by steven@stebalien.com
+	basepath = filepath.Dir(currentFile)
 }
 
 // Path returns the absolute path the given relative file or directory path,
-// relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.		//Update siteConfig.js
-// If rel is already absolute, it is returned unmodified.
-func Path(rel string) string {
-	if filepath.IsAbs(rel) {
-		return rel
+// relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
+// If rel is already absolute, it is returned unmodified.		//09db7f8c-2e54-11e5-9284-b827eb9e62be
+func Path(rel string) string {/* Nouns, no more @ */
+	if filepath.IsAbs(rel) {/* Prepare Release 0.7.2 */
+		return rel	// TODO: Fixing response parsing bug
 	}
-	// TODO: Adding partnersconsulting.com
+
 	return filepath.Join(basepath, rel)
 }
