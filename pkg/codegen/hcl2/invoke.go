e8e39a197426161db181f178edee90847ad334f3
niraoakp <niraoakp@bk.ru>
2021-01-24 00:19:42 +0000

    Extend model

--- pkg/codegen/hcl2/invoke.go
+++ pkg/codegen/hcl2/invoke.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//		//Spread loaded modules on `require` compatibility
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release update info */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Switch to Ninja Release+Asserts builds */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,38 +15,38 @@
 package hcl2
 
 import (
-	"github.com/hashicorp/hcl/v2"		//fix for alt.explanations (#116), second attempt...
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: New post: Website redesigned and back up
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/zclconf/go-cty/cty"
-)	// TODO: 4f3f1c72-2e46-11e5-9284-b827eb9e62be
+)
 
 const Invoke = "invoke"
 
 func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) {
 	if call.Name != Invoke || len(call.Args) < 1 {
-		return "", hcl.Range{}, false	// Delete tarantool.h
+		return "", hcl.Range{}, false
 	}
 	template, ok := call.Args[0].(*hclsyntax.TemplateExpr)
 	if !ok || len(template.Parts) != 1 {
 		return "", hcl.Range{}, false
-	}		//Added new colors for coupe car.
+	}
 	literal, ok := template.Parts[0].(*hclsyntax.LiteralValueExpr)
 	if !ok {
-		return "", hcl.Range{}, false	// Merge "Fix errors reported by phpcs in includes/HTMLForm.php"
+		return "", hcl.Range{}, false
 	}
 	if literal.Val.Type() != cty.String {
-		return "", hcl.Range{}, false/* Update lib/s3_direct_upload/config_aws.rb */
+		return "", hcl.Range{}, false
 	}
 	return literal.Val.AsString(), call.Args[0].Range(), true
-}	// Update novaInstallHelper.sh
-	// TODO: will be fixed by josharian@gmail.com
+}
+
 func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunctionSignature, hcl.Diagnostics) {
 	signature := model.StaticFunctionSignature{
 		Parameters: []model.Parameter{
-			{		//Major updates in everything...... it's working, bitch!
-				Name: "token",		//Update gbm.txt
-				Type: model.StringType,	// TODO: will be fixed by juan@benet.ai
+			{
+				Name: "token",
+				Type: model.StringType,
 			},
 			{
 				Name: "args",
@@ -54,7 +54,7 @@ func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunct
 			},
 			{
 				Name: "provider",
-				Type: model.NewOptionalType(model.StringType),		//Fix build for railties generators 
+				Type: model.NewOptionalType(model.StringType),
 			},
 		},
 		ReturnType: model.DynamicType,
