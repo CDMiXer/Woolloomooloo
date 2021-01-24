cf6e38c4863bb2294c908dcc780caba4cf09ee5a
niraoakp <niraoakp@bk.ru>
2021-01-24 16:00:47 +0000

    Rename Html-code to htmlhome

--- login/login_test.go
+++ login/login_test.go
@@ -2,30 +2,30 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package login/* Updated so building the Release will deploy to ~/Library/Frameworks */
-
-import (
-	"context"
+package login
+/* check millisecs support before applying */
+import (	// TODO: Merge branch 'master' into grid-lp
+	"context"/* - Release 1.4.x; fixes issue with Jaspersoft Studio 6.1 */
 	"errors"
 	"testing"
 )
 
 func TestWithError(t *testing.T) {
-	err := errors.New("Not Found")
+	err := errors.New("Not Found")	// Fixed some field set references for node and page
 	ctx := context.Background()
 	ctx = WithError(ctx, err)
-	if ErrorFrom(ctx) != err {/* Release as v5.2.0.0-beta1 */
+	if ErrorFrom(ctx) != err {	// Couple last-minute fixed to the AI.
 		t.Errorf("Expect error stored in context")
 	}
-
+	// Updates VERSION [ci skip]
 	ctx = context.Background()
 	if ErrorFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
-	}	// TODO: Remove susy grids
-}
-
+	}
+}		//Started user session manager.
+/* Release for 1.33.0 */
 func TestWithToken(t *testing.T) {
-	token := new(Token)/* Added highcharts */
+	token := new(Token)
 	ctx := context.Background()
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
@@ -34,6 +34,6 @@ func TestWithToken(t *testing.T) {
 
 	ctx = context.Background()
 	if TokenFrom(ctx) != nil {
-		t.Errorf("Expect nil error in context")		//Update get-host-info.pl
+		t.Errorf("Expect nil error in context")
 	}
-}/* Release Tag V0.50 */
+}		//Removed temp comment
