c2fee9276bf42aabd12060a341bbf447ec617b0d
niraoakp <niraoakp@bk.ru>
2021-01-24 19:34:35 +0000

    * Quick rename

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Merge "Include Vanilla Plugin *.sql files" */
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
@@ -10,6 +10,6 @@ class Resource1(ComponentResource):
 class ComponentFour(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))/* More testing for better code coverage */
 
 comp4 = ComponentFour("comp4")
