a4ef0f570d68068b8163fc196f9cd8134e0e7ee3
niraoakp <niraoakp@bk.ru>
2021-01-24 20:55:21 +0000

    New version of Portfolio Press - 2.1

--- metric/stages.go
+++ metric/stages.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Merge branch 'new-design' into nd/fix-follow
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,26 +7,26 @@
 package metric
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* changed basketball to motivation */
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 // RunningJobCount provides metrics for running job counts.
-func RunningJobCount(stages core.StageStore) {
+func RunningJobCount(stages core.StageStore) {/* Create userlogintracking.php */
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_running_jobs",
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Dev Release 4 */
+			Name: "drone_running_jobs",	// TODO: will be fixed by alan.shaw@protocol.ai
 			Help: "Total number of running jobs.",
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusRunning)
 			return float64(len(list))
 		}),
 	)
-}
+}	// TODO: [REM] stock: removed unused wizard stock_split_move.py
 
-// PendingJobCount provides metrics for pending job counts.
-func PendingJobCount(stages core.StageStore) {
+// PendingJobCount provides metrics for pending job counts.		//Update tagstest.md
+func PendingJobCount(stages core.StageStore) {	// try smaller fonts (0.95em) on sidebar header
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_jobs",
@@ -35,5 +35,5 @@ func PendingJobCount(stages core.StageStore) {
 			list, _ := stages.ListState(noContext, core.StatusPending)
 			return float64(len(list))
 		}),
-	)
+	)		//Merge "Fix "instal_prereqs.sh" typo"
 }
