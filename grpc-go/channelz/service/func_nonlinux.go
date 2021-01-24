aeea5245c1ab57eafdb50355c4ec3716a17e1834
niraoakp <niraoakp@bk.ru>
2021-01-24 10:27:33 +0000

    implemented feature #2498: Global Device Inspector

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -2,7 +2,7 @@
 
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Add 9.0.1 Release Schedule */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,19 +12,19 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* + Release 0.38.0 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package service
-
+/* remove google font from css */
 import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
 )
 
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
-	return nil
+	return nil	// KE4YujLUD10aqeD8KUtv06jgTabuWzjy
 }
