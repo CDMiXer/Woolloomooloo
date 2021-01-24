dd5047a317f15be66e8db99a4802120c9d2db2e1
niraoakp <niraoakp@bk.ru>
2021-01-24 14:27:58 +0000

    Only update results page if new articles

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -2,14 +2,14 @@
 
 from typing import Any, Optional
 
-import pulumi
-/* Added more arithmetic specs */
+import pulumi		//fix safeInteger safeFloat bug
+
 class Component(pulumi.ComponentResource):
-    echo: pulumi.Output[Any]/* FilterConfig: move code to filter_chain_append_new() */
+    echo: pulumi.Output[Any]	// TODO: hacked by hugomrdias@gmail.com
     childId: pulumi.Output[str]
 
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
         props = dict()
-        props["echo"] = echo/* Checked out path didn't work, doing the lazy way */
+        props["echo"] = echo
         props["childId"] = None
         super().__init__("testcomponent:index:Component", name, props, opts, True)
