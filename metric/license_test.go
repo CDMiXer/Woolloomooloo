6da40a355746a4afd990424a46594c03ede5a808
niraoakp <niraoakp@bk.ru>
2021-01-24 15:29:07 +0000

    bee42648-2e56-11e5-9284-b827eb9e62be

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* small dialog */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* BuckUTT -> Buckless */
-package metric		//Added thumbnail support for Canons .tif.
+
+package metric
