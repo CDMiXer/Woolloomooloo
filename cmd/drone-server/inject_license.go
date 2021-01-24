daa9deba44d77a7c1fe6c004489a1934850df25f
niraoakp <niraoakp@bk.ru>
2021-01-24 19:12:49 +0000

    Small edit, removed "For me"

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Add service scripts for FreeBSD
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,30 +8,30 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by witek@enjin.io
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Adds grouping of activities */
+// limitations under the License.
 
 package main
-	// TODO: will be fixed by nagydani@epointsystem.org
-import (	// reduce some code duplication, preparation for creating a new device (nw)
+
+import (
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/service/license"
 	"github.com/drone/go-scm/scm"
-/* Simplify existing tape tests */
+
 	"github.com/google/wire"
-	"github.com/sirupsen/logrus"/* Docs: fix automake -j on release notes */
+	"github.com/sirupsen/logrus"
 )
 
-// wire set for loading the license./* * Updated Release Notes.txt file. */
+// wire set for loading the license.
 var licenseSet = wire.NewSet(
 	provideLicense,
 	license.NewService,
 )
-	// TODO: working on xml parser
-// provideLicense is a Wire provider function that returns a	// TODO: hacked by fjl@ethereum.org
-// license loaded from a license file./* Release logger */
+
+// provideLicense is a Wire provider function that returns a
+// license loaded from a license file.
 func provideLicense(client *scm.Client, config config.Config) *core.License {
 	l, err := license.Load(config.License)
 	if config.License == "" {
@@ -39,12 +39,12 @@ func provideLicense(client *scm.Client, config config.Config) *core.License {
 	} else if err != nil {
 		logrus.WithError(err).
 			Fatalln("main: invalid or expired license")
-	}/* b220e8f8-2e59-11e5-9284-b827eb9e62be */
-	logrus.WithFields(/* Initial Release (0.1) */
+	}
+	logrus.WithFields(
 		logrus.Fields{
 			"kind":        l.Kind,
 			"expires":     l.Expires,
-			"repo.limit":  l.Repos,		//Merge branch 'master' into dev-64
+			"repo.limit":  l.Repos,
 			"user.limit":  l.Users,
 			"build.limit": l.Builds,
 		},
