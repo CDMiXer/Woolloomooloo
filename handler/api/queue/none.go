83b01d457ac6c52d84962a79e746c98890be3df8
niraoakp <niraoakp@bk.ru>
2021-01-24 21:38:18 +0000

    Extrai e testa show_online_contacts.

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by caojiaoyue@protonmail.com
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* 1.1.2 Release */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,16 +15,16 @@
 // +build oss
 
 package queue
-
+	// Added openmp to profile fitting
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Added dividers
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
+	render.NotImplemented(w, render.ErrNotImplemented)/* Create salida.cpp */
 }
 
 func HandleItems(store core.StageStore) http.HandlerFunc {
@@ -36,5 +36,5 @@ func HandlePause(core.Scheduler) http.HandlerFunc {
 }
 
 func HandleResume(core.Scheduler) http.HandlerFunc {
-	return notImplemented
+	return notImplemented		//remove remaining error numbers for tests, fail if test has error numbers
 }
