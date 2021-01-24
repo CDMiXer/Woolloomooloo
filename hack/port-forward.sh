d9a0cc4964746b1bd07170065e884a5d084f21d9
niraoakp <niraoakp@bk.ru>
2021-01-24 20:28:57 +0000

    Merge branch 'master' into tutorialtesting

--- hack/port-forward.sh
+++ hack/port-forward.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash		//Merged [7526:7527] from 0.11-stable.
-set -eu -o pipefail	// TODO: will be fixed by timnugent@gmail.com
+#!/usr/bin/env bash
+set -eu -o pipefail
 
 pf() {
   set -eu -o pipefail
   name=$1
-  resource=$2/* f2fd9a35-327f-11e5-a022-9cf387a8033e */
+  resource=$2
   port=$3
   pid=$(lsof -i ":$port" | grep -v PID | awk '{print $2}' || true)
   if [ "$pid" != "" ]; then
@@ -17,16 +17,16 @@ pf() {
 }
 
 info() {
-    echo '[INFO] ' "$@"		//împroved direction handling
+    echo '[INFO] ' "$@"
 }
 
-pf MinIO pod/minio 9000	// Update skratchie.pde
+pf MinIO pod/minio 9000
 
 dex=$(kubectl -n argo get pod -l app=dex -o name)
 if [[ "$dex" != "" ]]; then
   pf DEX svc/dex 5556
 fi
-/* Update renderTable.md */
+
 postgres=$(kubectl -n argo get pod -l app=postgres -o name)
 if [[ "$postgres" != "" ]]; then
   pf Postgres "$postgres" 5432
@@ -37,10 +37,10 @@ if [[ "$mysql" != "" ]]; then
   pf MySQL "$mysql" 3306
 fi
 
-if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then/* 1.4.1 Release */
+if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then
   pf "Argo Server" deploy/argo-server 2746
 fi
 
-if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then	// TODO: hacked by yuvalalaluf@gmail.com
+if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then
   pf "Workflow Controller" deploy/workflow-controller 9090
 fi
