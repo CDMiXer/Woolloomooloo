c6e07a94cc5db8c67a60d801f10003029469bfaa
niraoakp <niraoakp@bk.ru>
2021-01-24 10:57:07 +0000

    Remove space from log lines (pre tags)

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* b85be873-327f-11e5-862b-9cf387a8033e */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Delete window.o
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'develop' into loglevel-extend */
 // that can be found in the LICENSE file.
-		//Fix ownCloud detector
-package manager
+
+package manager	// TODO: hacked by seth@sethvargo.com
