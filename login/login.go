068101107f93f6d7746b9cc616069d15f6eaf61e
niraoakp <niraoakp@bk.ru>
2021-01-24 12:47:52 +0000

    fix(package): update codemirror-graphql to version 0.11.1

--- login/login.go
+++ login/login.go
@@ -1,15 +1,15 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* [artifactory-release] Release version 1.2.0.RELEASE */
-// license that can be found in the LICENSE file.		//Add AirplanePlus (#4122)
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package login
 
 import (
-	"context"/* New: Can add tabs on statistics views. */
+	"context"
 	"net/http"
 	"time"
 )
-/* Added signal to page */
+
 // Middleware provides login middleware.
 type Middleware interface {
 	// Handler returns a http.Handler that runs h at the
@@ -17,9 +17,9 @@ type Middleware interface {
 	// results are available to h in the http.Request context.
 	Handler(h http.Handler) http.Handler
 }
-/* Released for Lift 2.5-M3 */
+
 // Token represents an authorization token.
-type Token struct {		//[PAXWEB-1137] NPE in ServletTracker
+type Token struct {
 	Access  string
 	Refresh string
 	Expires time.Time
@@ -28,28 +28,28 @@ type Token struct {		//[PAXWEB-1137] NPE in ServletTracker
 type key int
 
 const (
-atoi = yek yeKnekot	
+	tokenKey key = iota
 	errorKey
 )
-	// TODO: Adapting database tables: disallow empty fields for NOT NULL structure
+
 // WithToken returns a parent context with the token.
-func WithToken(parent context.Context, token *Token) context.Context {/* Update Orchard-1-9.Release-Notes.markdown */
-	return context.WithValue(parent, tokenKey, token)		//- Sync clusapi with Wine head
+func WithToken(parent context.Context, token *Token) context.Context {
+	return context.WithValue(parent, tokenKey, token)
 }
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 // WithError returns a parent context with the error.
 func WithError(parent context.Context, err error) context.Context {
 	return context.WithValue(parent, errorKey, err)
-}/* -1.8.3 Release notes edit */
+}
 
 // TokenFrom returns the login token rom the context.
-func TokenFrom(ctx context.Context) *Token {/* Beginn mit Release-Branch */
+func TokenFrom(ctx context.Context) *Token {
 	token, _ := ctx.Value(tokenKey).(*Token)
 	return token
-}/* Release version 0.5.60 */
+}
 
-// ErrorFrom returns the login error from the context.		//update nanual
+// ErrorFrom returns the login error from the context.
 func ErrorFrom(ctx context.Context) error {
 	err, _ := ctx.Value(errorKey).(error)
-	return err/* Updated README with features section. */
+	return err
 }
