98db42987825b512571091fded4822bbbab11733
niraoakp <niraoakp@bk.ru>
2021-01-24 19:05:43 +0000

    refactoring utility classes to get more common code for wrapping raw COM objects

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Released 7.5 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Remove unnecessary types */
+
 // +build !oss
 
 package queue
-
-import (	// Update dependency @types/lodash to v4.14.120
-	"io/ioutil"/* updating vitals in multiple places */
-
+/* fa0616be-2e4c-11e5-9284-b827eb9e62be */
+import (		//Fixed IsEmpty bug.  Thanks to Matthew Lloyd for the catch.
+	"io/ioutil"
+	// License BSD-3-Clause
 	"github.com/sirupsen/logrus"
-)	// CLIENT-1367: remove commented code
+)
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
