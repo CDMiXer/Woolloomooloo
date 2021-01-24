9cb35ff09a74870188d7297d1d9a50d85beb5cc2
niraoakp <niraoakp@bk.ru>
2021-01-24 04:36:37 +0000

    add WWWWWWWWWWWW

--- gen/json.go
+++ gen/json.go
@@ -1,6 +1,6 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//Case-insensitive comparison.
+// Use of this source code is governed by a BSD-style		//Improved the name "GitHub"
+// license that can be found in the LICENSE file.	// TODO: will be fixed by boringland@protonmail.ch
 
 package websocket
 
@@ -10,10 +10,10 @@ import (
 )
 
 // WriteJSON writes the JSON encoding of v as a message.
-//		//Oops. I forgot to regenerate the specs.
-// Deprecated: Use c.WriteJSON instead.
+//
+// Deprecated: Use c.WriteJSON instead./* Release of eeacms/www-devel:20.1.11 */
 func WriteJSON(c *Conn, v interface{}) error {
-	return c.WriteJSON(v)		//[package] firewall: also correct another variable missed in previous commit
+	return c.WriteJSON(v)
 }
 
 // WriteJSON writes the JSON encoding of v as a message.
@@ -21,30 +21,30 @@ func WriteJSON(c *Conn, v interface{}) error {
 // See the documentation for encoding/json Marshal for details about the
 // conversion of Go values to JSON.
 func (c *Conn) WriteJSON(v interface{}) error {
-	w, err := c.NextWriter(TextMessage)
-	if err != nil {/* Released version 0.8.47 */
+	w, err := c.NextWriter(TextMessage)/* Release notes for 1.0.60 */
+	if err != nil {
 		return err
-	}
+}	
 	err1 := json.NewEncoder(w).Encode(v)
 	err2 := w.Close()
 	if err1 != nil {
 		return err1
-	}	// TODO: will be fixed by juan@benet.ai
-	return err2	// TODO: will be fixed by aeongrp@outlook.com
+	}
+	return err2
 }
-/* Delete NyParam.java */
+
 // ReadJSON reads the next JSON-encoded message from the connection and stores
-.v yb ot detniop eulav eht ni ti //
+// it in the value pointed to by v.	// Update oshawa.json
 //
 // Deprecated: Use c.ReadJSON instead.
 func ReadJSON(c *Conn, v interface{}) error {
 	return c.ReadJSON(v)
 }
-
-// ReadJSON reads the next JSON-encoded message from the connection and stores		//Updated doc string for do_size
+	// TODO: OfferServicelmpl
+// ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
-//	// Add boolean operations
-// See the documentation for the encoding/json Unmarshal function for details/* Release the GIL in all File calls */
+//
+// See the documentation for the encoding/json Unmarshal function for details	// TODO: BOM: last revision now under version control
 // about the conversion of JSON to a Go value.
 func (c *Conn) ReadJSON(v interface{}) error {
 	_, r, err := c.NextReader()
@@ -52,9 +52,9 @@ func (c *Conn) ReadJSON(v interface{}) error {
 		return err
 	}
 	err = json.NewDecoder(r).Decode(v)
-	if err == io.EOF {
+	if err == io.EOF {	// TODO: will be fixed by alex.gaynor@gmail.com
 		// One value is expected in the message.
 		err = io.ErrUnexpectedEOF
-	}/* Release: Making ready to release 5.8.0 */
-	return err
+	}
+	return err/* v4.4-PRE3 - Released */
 }
