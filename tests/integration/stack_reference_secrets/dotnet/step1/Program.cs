508f31c2cd49f50b03a3dbc4780863e37bc955d8
niraoakp <niraoakp@bk.ru>
2021-01-24 22:47:25 +0000

    fix documentation info, it's english

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Collections.Generic;/* Added dwarf to the awesome list */
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)		//trying to import OLeditor from GitHub via svn:externals (1)
-    {
-        return Deployment.RunAsync(() =>
+    static Task<int> Main(string[] args)
+    {	// TODO: Merge lp:~matthias-troffaes/pybtex/webref
+        return Deployment.RunAsync(() =>/* Release 4.0.4 */
         {
-            return new Dictionary<string, object>
+            return new Dictionary<string, object>	// TODO: Update parser-helpers.c
             {
                 { "normal", Output.Create("normal") },
                 { "secret", Output.CreateSecret("secret") },
