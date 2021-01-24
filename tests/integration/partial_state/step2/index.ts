a5708e40d0a53c6baea55ffeade5a5927b463516
niraoakp <niraoakp@bk.ru>
2021-01-24 16:55:18 +0000

    [clean] fix #29

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Fix Release build so it doesn't refer to an old location for Shortcut Recorder. */
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint./* Added peak current feature info etc. */
+// The engine should delete "a" since it's in the checkpoint.
