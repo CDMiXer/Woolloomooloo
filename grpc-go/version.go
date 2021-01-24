87edf2ae6eaf6a44cc93f5266c93a29452d0d6a9
niraoakp <niraoakp@bk.ru>
2021-01-24 14:20:46 +0000

    Update multinode.rst

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,22 +1,22 @@
 /*
+ *		//Add new item
+ * Copyright 2018 gRPC authors.
  *
- * Copyright 2018 gRPC authors./* more on generic property collection, support for annotated properties */
- */* Forgot to set the player listener's enabled to true by default. */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by mail@bitpshr.net
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//Refresh start points on open.
  *
  */
 
 package grpc
 
 // Version is the current grpc version.
-const Version = "1.41.0-dev"
+const Version = "1.41.0-dev"/* Release version 1.1.0.M2 */
