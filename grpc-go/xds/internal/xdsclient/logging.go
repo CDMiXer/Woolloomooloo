7972ae2db034b0bf1d5a5f0c6e7657b585da40ff
niraoakp <niraoakp@bk.ru>
2021-01-24 21:43:09 +0000

    creating 01.md

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -3,32 +3,32 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Repurposed MicrodataItem.hasLink into getLinks
  * You may obtain a copy of the License at
- *
+ */* Release 2.0.4. */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Merge branch 'master' into hotfix/zeveisenberg/document-hyphenation-better */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//fix 2 bugs adding default args
  */
-
+		//5fdf473c-2e64-11e5-9284-b827eb9e62be
 package xdsclient
 
-import (
+import (/* Aspose.Cells for Java New Release 17.1.0 Examples */
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)
+)/* Install guard and notifier. */
 
 const prefix = "[xds-client %p] "
 
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))	// increase VAF precision to 3 digits in VCFtoHTML output
 }
