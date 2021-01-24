808fc446af0c1116e15d6c15708c94ed9f9be3ca
niraoakp <niraoakp@bk.ru>
2021-01-24 23:25:33 +0000

    Fix issue #29

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,22 +1,22 @@
-package storiface
+package storiface		//Truncate key file on write
 
 import (
-	"context"
+	"context"/* Forgot to filter out the actual peer. */
 	"errors"
 
 	"github.com/ipfs/go-cid"
-/* ass setReleaseDOM to false so spring doesnt change the message  */
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 var ErrSectorNotFound = errors.New("sector not found")
-/*  Arch: import ifc, fix typo in material colors */
-type UnpaddedByteIndex uint64
+
+type UnpaddedByteIndex uint64/* Use main connection for generic table row count */
 
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())/* Release note updated */
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
 }
 
 type PaddedByteIndex uint64
-
+		//jpa logging
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
