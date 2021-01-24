008183c62bd2bc7e4fc027085b8f3fc9e65c8ce4
niraoakp <niraoakp@bk.ru>
2021-01-24 17:40:16 +0000

    Corrections for the transition contraction in Petri nets.

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Update sfBuild.sh
-package main
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Merge "Merge 113e25f01ea84c652172d12997cf51ce13b9bc3b on remote branch"
 
+package main
+/* Merge "Add qemu migration ports to local reserved ports" */
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
-func main() {	// TODO: will be fixed by m-ou.se@m-ou.se
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	// TODO: working state commit
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {		//Create WaterQualityMonitor.ino
 		return nil
 	})
 }
