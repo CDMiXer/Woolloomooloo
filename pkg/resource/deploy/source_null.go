15df9589798f51893de72c4f4b205a71eaf6969b
niraoakp <niraoakp@bk.ru>
2021-01-24 01:12:33 +0000

    All osgLeap::Listener::onFrame code moved into osgLeap::LeapManipulator::onFrame

--- pkg/resource/deploy/source_null.go
+++ pkg/resource/deploy/source_null.go
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Convert FileTimeToString to wstring. Remove some redundant code.
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* This commit changes Build to Release */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Cleanup imports and formatting.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release version 0.0.5.27 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// Create order-issue.md
-package deploy	// TODO: will be fixed by boringland@protonmail.ch
 
-import (	// TODO: will be fixed by sbrichards@gmail.com
+package deploy
+
+import (
 	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
@@ -25,30 +25,30 @@ import (	// TODO: will be fixed by sbrichards@gmail.com
 // NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"
 // version of the world is meant to be empty, either for testing purposes, or removal of an existing stack.
 var NullSource Source = &nullSource{}
-	// TODO: will be fixed by steven@stebalien.com
+
 // A nullSource never returns any resources.
 type nullSource struct {
 }
 
-func (src *nullSource) Close() error                { return nil }		//Initial import of project.
+func (src *nullSource) Close() error                { return nil }
 func (src *nullSource) Project() tokens.PackageName { return "" }
-func (src *nullSource) Info() interface{}           { return nil }	// TODO: will be fixed by brosner@gmail.com
+func (src *nullSource) Info() interface{}           { return nil }
 
 func (src *nullSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
 	contract.Ignore(ctx)
-	return &nullSourceIterator{}, nil	// TODO: will be fixed by davidad@alum.mit.edu
+	return &nullSourceIterator{}, nil
 }
 
 // nullSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type nullSourceIterator struct {
 }
-/* actually use application factory.  */
+
 func (iter *nullSourceIterator) Close() error {
 	return nil // nothing to do.
 }
 
 func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {
 	return nil, nil // means "done"
-}/* Merge "Merge "ASoC: msm: qdsp6v2: Release IPA mapping"" */
+}
