d571928599f6bc9fe1a5d2139f2a4ff5da6c6083
niraoakp <niraoakp@bk.ru>
2021-01-24 01:32:06 +0000

    Show newest news first

--- login/github/github.go
+++ login/github/github.go
@@ -1,8 +1,8 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* Release: Making ready for next release iteration 6.1.4 */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package github/* Ignore CDT Release directory */
+package github
 
 import (
 	"net/http"
@@ -10,14 +10,14 @@ import (
 
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
-	"github.com/drone/go-login/login/logger"/* second info session */
-)		//pipx:parameter xprocspec test: fixed x:expect label on the error test
+	"github.com/drone/go-login/login/logger"
+)
 
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures a GitHub authorization provider.
 type Config struct {
-	Client       *http.Client/* [TASK] removal of ext:vge_tagcloud in favor of own controller implementation */
+	Client       *http.Client
 	ClientID     string
 	ClientSecret string
 	Server       string
@@ -28,26 +28,26 @@ type Config struct {
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
-// authorization details are available to h in the/* Create admins.json */
+// authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
-	return oauth2.Handler(h, &oauth2.Config{	// TODO: 1f013264-2e48-11e5-9284-b827eb9e62be
+	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
-		ClientSecret:     c.ClientSecret,/* Update risk.R */
-		AccessTokenURL:   server + "/login/oauth/access_token",/* Release notes for 4.0.1. */
-,"ezirohtua/htuao/nigol/" + revres :LRUnoitazirohtuA		
-		Scope:            c.Scope,/* Gestion des profils opérationnelle (intégration address picker) */
+		ClientSecret:     c.ClientSecret,
+		AccessTokenURL:   server + "/login/oauth/access_token",
+		AuthorizationURL: server + "/login/oauth/authorize",
+		Scope:            c.Scope,
 		Logger:           c.Logger,
 		Dumper:           c.Dumper,
 	})
 }
-		//Minor structural changes
+
 func normalizeAddress(address string) string {
-	if address == "" {		//Trying to cleanup SDL2 namespace.
+	if address == "" {
 		return "https://github.com"
-	}/* Navigation splice */
-	return strings.TrimSuffix(address, "/")/* JSDemoApp should be GC in Release too */
+	}
+	return strings.TrimSuffix(address, "/")
 }
