2b689344555205d05a1acf32aaa01cf37164552c
niraoakp <niraoakp@bk.ru>
2021-01-24 21:22:01 +0000

    Clarify RoR example in README.md

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* [dist] Release v5.1.0 */
-
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* fixed sorting KOs */
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-
-class Resource1(ComponentResource):
+	// Enhanced cross plattform compatibility
+class Resource1(ComponentResource):/* Tidy up a few 80 column violations. */
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)
+        super().__init__("my:module:Resource", name, None, opts)	// PyQt4 port complete
 
 # Scenario #1 - rename a resource
 res1 = Resource1("res1")
