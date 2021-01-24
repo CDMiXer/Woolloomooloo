bdca46abba5f60b0a584f0a5e16b6c7f3ac7719d
niraoakp <niraoakp@bk.ru>
2021-01-24 16:50:25 +0000

    Adding checked/unchecked checkboxes.

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,4 +1,4 @@
-// Create a new security group for port 80.
+// Create a new security group for port 80./* fix: install npm */
 resource securityGroup "aws:ec2:SecurityGroup" {
 	ingress = [{
 		protocol = "tcp"
@@ -7,27 +7,27 @@ resource securityGroup "aws:ec2:SecurityGroup" {
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
 }
-
+/* Release 0.7.1. */
 // Get the ID for the latest Amazon Linux AMI.
-ami = invoke("aws:index:getAmi", {
+ami = invoke("aws:index:getAmi", {/* Ease Framework  1.0 Release */
 	filters = [{
 		name = "name"
 		values = ["amzn-ami-hvm-*-x86_64-ebs"]
-	}]
+	}]		//Commented out root logger, messages printed twice
 	owners = ["137112412989"] // Amazon
 	mostRecent = true
 })
 
-// Create a simple web server using the startup script for the instance.
+// Create a simple web server using the startup script for the instance.	// TODO: hacked by arajasek94@gmail.com
 resource server "aws:ec2:Instance" {
 	tags = {
-		Name = "web-server-www"
+		Name = "web-server-www"/* Removing stray console.log (#266) */
 	}
-	instanceType = "t2.micro"
+	instanceType = "t2.micro"/* further dialog development */
 	securityGroups = [securityGroup.name]
-	ami = ami.id
-	userData = <<-EOF
-		#!/bin/bash
+	ami = ami.id		//Added Functionality to Activity Second
+	userData = <<-EOF	// Score count, contamination count, gradual speed increment.
+		#!/bin/bash		//falcon: fix test in yarn non-ha mode
 		echo "Hello, World!" > index.html
 		nohup python -m SimpleHTTPServer 80 &
 	EOF
