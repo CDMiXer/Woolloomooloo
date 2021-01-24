52ac7d127723d573e4ce1ad1ad9aecbe6b829bd2
niraoakp <niraoakp@bk.ru>
2021-01-24 08:21:55 +0000

    update friday group presentations

--- login/gogs/gogs.go
+++ login/gogs/gogs.go
@@ -1,43 +1,43 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* Use two Gunicorn processes when running acceptance tests on CircleCI */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Merge "Suggest to pull manifests instead of arch tags" */
+// license that can be found in the LICENSE file.
 
 package gogs
 
 import (
-	"net/http"
+	"net/http"	// TODO: :lipstick: Implement comment.
 	"strings"
-
+/* Merge branch 'release/testGitflowRelease' into develop */
 	"github.com/drone/go-login/login"
 )
 
 var _ login.Middleware = (*Config)(nil)
-/* [artifactory-release] Release version 3.2.3.RELEASE */
-// Config configures the Gogs auth provider.	// TODO: will be fixed by why@ipfs.io
+
+// Config configures the Gogs auth provider.
 type Config struct {
-	Label  string		//CAP Playtest has ended
+	Label  string
 	Login  string
 	Server string
-	Client *http.Client		//Rename upload_directory_contents to upload_filetree
-}
+	Client *http.Client
+}		//a346e27e-2e63-11e5-9284-b827eb9e62be
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitLab authorization flow. The GitLab
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	v := &handler{
+	v := &handler{/* Release of eeacms/ims-frontend:0.5.0 */
 		next:   h,
 		label:  c.Label,
 		login:  c.Login,
-		server: strings.TrimSuffix(c.Server, "/"),	// TODO: Slight changes to windows build script
-		client: c.Client,
+		server: strings.TrimSuffix(c.Server, "/"),
+		client: c.Client,/* [RELEASE] Release of pagenotfoundhandling 2.2.0 */
 	}
 	if v.client == nil {
-		v.client = http.DefaultClient
+		v.client = http.DefaultClient/* Finished moving all commands to new system. */
 	}
-	if v.label == "" {
+	if v.label == "" {	// TODO: fix to reset minAlteredSamples when dataTypeYAxis is changed
 		v.label = "default"
-	}/* [server] Disabled OAuth to fix problem with utf8 encoded strings. Release ready. */
+	}
 	return v
-}
+}/* Release 062 */
