d35d8ac5e12876cd329faa077ec7f0a1c572c801
niraoakp <niraoakp@bk.ru>
2021-01-24 20:17:34 +0000

    add unidoswiki on services

--- metric/users_test.go
+++ metric/users_test.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 2.1.3 - Calendar response content type */
-// that can be found in the LICENSE file./* Update between.md */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-// +build !oss/* Fix french translation, Release of STAVOR v1.0.0 in GooglePlay */
+// +build !oss
 
 package metric
 
 import (
 	"testing"
-	// TODO: Update icdar.py
+
 	"github.com/drone/drone/mock"
-		//And..... we are done. project finally compile
+
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
-)/* pulled the mobile nav bar out into it’s own partial */
+)
 
 func TestUserCount(t *testing.T) {
 	controller := gomock.NewController(t)
-/* Release 1.5 */
+
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot		//Update maven plugin and project configuration
-		controller.Finish()	// Add a project generator, closes #6
+		prometheus.DefaultRegisterer = snapshot
+		controller.Finish()
 	}()
 
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
-	// x2 repository count/* Added Vulkan API - Companion Guide */
+	// x2 repository count
 	count := int64(5)
 
-	store := mock.NewMockUserStore(controller)/* Tweak documentation again */
+	store := mock.NewMockUserStore(controller)
 	store.EXPECT().Count(gomock.Any()).Return(count, nil)
 	UserCount(store)
 
@@ -43,14 +43,14 @@ func TestUserCount(t *testing.T) {
 		return
 	}
 	if want, got := len(metrics), 1; want != got {
-		t.Errorf("Expect registered metric")		//Updated badges for coveralls and travis
-		return/* Merge "msm: rpc: Release spinlock irqsave before blocking operation" */
-	}	// TODO: will be fixed by alex.gaynor@gmail.com
+		t.Errorf("Expect registered metric")
+		return
+	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
-		t.Errorf("Expect metric value %f, got %f", want, got)		//Wine devel version 1.7.34
+		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
