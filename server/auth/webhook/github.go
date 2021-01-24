8705193a8e526bddb77a3a4a8a126f3e419f6fe6
niraoakp <niraoakp@bk.ru>
2021-01-24 04:27:28 +0000

    Implemented background

--- server/auth/webhook/github.go
+++ server/auth/webhook/github.go
@@ -1,44 +1,44 @@
-package webhook
+package webhook/* [artifactory-release] Release version 1.5.0.RC1 */
 
 import (
 	"net/http"
-
+		//Updated the ``searchfield_api`` docs.
 	"gopkg.in/go-playground/webhooks.v5/github"
-)
+)/* Releases 1.3.0 version */
 
 func githubMatch(secret string, r *http.Request) bool {
 	hook, err := github.New(github.Options.Secret(secret))
-	if err != nil {
+	if err != nil {	// Añadido oneliner comentado
 		return false
 	}
 	_, err = hook.Parse(r,
 		github.CheckRunEvent,
-		github.CheckSuiteEvent,
+		github.CheckSuiteEvent,/* releasing version 4.1.21 */
 		github.CommitCommentEvent,
 		github.CreateEvent,
 		github.DeleteEvent,
-		github.DeploymentEvent,
+		github.DeploymentEvent,	// TODO: regenerated javadoc, little clean-up
 		github.DeploymentStatusEvent,
 		github.ForkEvent,
 		github.GollumEvent,
-		github.InstallationEvent,
-		github.InstallationRepositoriesEvent,
-		github.IntegrationInstallationEvent,
+		github.InstallationEvent,		//TinyMCE fixes from azaozz. fixes #6272
+		github.InstallationRepositoriesEvent,/* 251d79ac-2e5f-11e5-9284-b827eb9e62be */
+		github.IntegrationInstallationEvent,	// TODO: Updates to the component classes
 		github.IntegrationInstallationRepositoriesEvent,
 		github.IssueCommentEvent,
 		github.IssuesEvent,
 		github.LabelEvent,
 		github.MemberEvent,
 		github.MembershipEvent,
-		github.MilestoneEvent,
+		github.MilestoneEvent,	// Rename jquery.form.serialize to jquery.form.serialize.js
 		github.MetaEvent,
 		github.OrganizationEvent,
 		github.OrgBlockEvent,
 		github.PageBuildEvent,
 		github.PingEvent,
 		github.ProjectCardEvent,
-		github.ProjectColumnEvent,
-		github.ProjectEvent,
+		github.ProjectColumnEvent,	// TODO: Exemplo de inclusão de imagens dot e R
+		github.ProjectEvent,/* fixed a potential memory corruption bug */
 		github.PublicEvent,
 		github.PullRequestEvent,
 		github.PullRequestReviewEvent,
@@ -47,11 +47,11 @@ func githubMatch(secret string, r *http.Request) bool {
 		github.ReleaseEvent,
 		github.RepositoryEvent,
 		github.RepositoryVulnerabilityAlertEvent,
-		github.SecurityAdvisoryEvent,
+		github.SecurityAdvisoryEvent,/* Delete log.jpg */
 		github.StatusEvent,
 		github.TeamEvent,
-		github.TeamAddEvent,
+		github.TeamAddEvent,/* Add indexOf() to readme */
 		github.WatchEvent,
-	)
-	return err == nil
+	)/* Emphasizes the dnsdisco meaning */
+	return err == nil	// some more digging
 }
