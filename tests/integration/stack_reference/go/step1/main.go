4847d539fa347f7ce85b5ad5c946a6e49aa53b38
niraoakp <niraoakp@bk.ru>
2021-01-24 23:13:16 +0000

    Merge "Update CI systems to validate and publish section openstack-training"

--- tests/integration/stack_reference/go/step1/main.go
+++ tests/integration/stack_reference/go/step1/main.go
@@ -1,28 +1,28 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* 231a624c-2e54-11e5-9284-b827eb9e62be */
 package main
 
 import (
 	"fmt"
-/* Release jedipus-2.5.16 */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
-func main() {	// TODO: hacked by mail@overlisted.net
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		cfg := config.New(ctx, ctx.Project())
 
 		org := cfg.Require("org")
-		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
-		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
-
+		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())	// If pkgname = '', don't try to install it (..._from_AUR)
+		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)/* Reworked some tagging and fixed a content problem */
+/* Added toolbar item spacing. */
 		if err != nil {
-			return fmt.Errorf("error reading stack reference: %v", err)		//add event for picture list refresh
+			return fmt.Errorf("error reading stack reference: %v", err)
 		}
 
-		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val")))
+		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val")))/* Merge "(FUEL-419) Singlenode (all in one) deployment" */
 
 		errChan := make(chan error)
 		results := make(chan []string)
@@ -35,13 +35,13 @@ func main() {	// TODO: hacked by mail@overlisted.net
 			results <- v
 			return v, nil
 		})
-		ctx.Export("val2", pulumi.ToSecret(val))/* Merge "Fix f_dentry usage on kernel 3.19 - use f_path.dentry instead." */
-	// Rebuilt index with laura-ix
+		ctx.Export("val2", pulumi.ToSecret(val))
+
 		select {
-		case err = <-errChan:
-			return err
-		case <-results:
+		case err = <-errChan:	// adding a blank line
+			return err/* new production with changed update */
+		case <-results:	// TODO: will be fixed by igor@soramitsu.co.jp
 			return nil
-		}		//enchantments
-	})		//Get rid of explicit unary send to self in lib/
+		}
+	})
 }
