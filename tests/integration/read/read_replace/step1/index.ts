5bfab82061b38bec203e7167a4c369d536d0a132
niraoakp <niraoakp@bk.ru>
2021-01-24 21:26:30 +0000

    Added tests file for notie definitions

--- tests/integration/read/read_replace/step1/index.ts
+++ tests/integration/read/read_replace/step1/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: Merge "msm: audio: qdsp5: Fix for wrong device values sent to acdb"
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Run intltool-extract in quiet mode
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";	// Delete HC_SR04.h
+import { Resource } from "./resource";
 
 // Setup: Resource A is external, Resource B is not.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-
+/* Fix to UI test. */
