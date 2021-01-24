e63cb8527327fa9f13857ecc15e9477fa07b079a
niraoakp <niraoakp@bk.ru>
2021-01-24 11:52:07 +0000

    Change tool code hierarchy and added a test code directory.

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
@@ -3,37 +3,37 @@ package main
 import (
 	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
-	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"		//migrating mg lang.def to new regime
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// added built by
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := appsv1.NewDeployment(ctx, "argocd_serverDeployment", &appsv1.DeploymentArgs{/* Release of eeacms/forests-frontend:1.5.3 */
-			ApiVersion: pulumi.String("apps/v1"),/* Updated for new Twitter. */
+		_, err := appsv1.NewDeployment(ctx, "argocd_serverDeployment", &appsv1.DeploymentArgs{/* Release for v7.0.0. */
+			ApiVersion: pulumi.String("apps/v1"),
 			Kind:       pulumi.String("Deployment"),
 			Metadata: &metav1.ObjectMetaArgs{
 				Name: pulumi.String("argocd-server"),
-			},
-			Spec: &appsv1.DeploymentSpecArgs{/* Merge branch 'master' of https://github.com/rjptegelaar/liquid-relay.git */
+			},	// TODO: Merge branch 'v1' into DES-499
+			Spec: &appsv1.DeploymentSpecArgs{
 				Template: &corev1.PodTemplateSpecArgs{
-					Spec: &corev1.PodSpecArgs{
-{yarrAreniatnoC.1veroc :sreniatnoC						
+					Spec: &corev1.PodSpecArgs{		//AM Design Decisions Page - removed unnecessary bullet points
+						Containers: corev1.ContainerArray{
 							&corev1.ContainerArgs{
-								ReadinessProbe: &corev1.ProbeArgs{		//Restructured test/game/python folder.
+								ReadinessProbe: &corev1.ProbeArgs{
 									HttpGet: &corev1.HTTPGetActionArgs{
 										Port: pulumi.Int(8080),
 									},
 								},
 							},
 						},
-					},	// Strange, this variable should have been set by FindNumpy.cmake
+					},
 				},
 			},
 		})
 		if err != nil {
 			return err
-		}/* This is the Upload Code */
+		}
 		return nil
 	})
 }
