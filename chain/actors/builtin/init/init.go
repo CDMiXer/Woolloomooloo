4a5df749ea3a369de0a4a91b6ddce5c2a6a3f424
niraoakp <niraoakp@bk.ru>
2021-01-24 00:05:41 +0000

    Updated minimum FreeCAD version requirement

--- chain/actors/builtin/init/init.go
+++ chain/actors/builtin/init/init.go
@@ -5,16 +5,16 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/cbor"	// TODO: ALPS meta.yaml
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-
+/* 71011a9e-2e42-11e5-9284-b827eb9e62be */
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-
+	// TODO: will be fixed by ligi@ligi.de
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
@@ -22,24 +22,24 @@ import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-func init() {
+func init() {/* Corrections to formatting */
 
 	builtin.RegisterActorState(builtin0.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
+/* Release 1-91. */
 	builtin.RegisterActorState(builtin2.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})
+	})	// TODO: will be fixed by witek@enjin.io
 
-	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// added relative url paths
 		return load4(store, root)
 	})
-}
+}	// TODO: Rename Apriori.py to apriori.py
 
 var (
 	Address = builtin4.InitActorAddr
@@ -47,18 +47,18 @@ var (
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {
-
+	switch act.Code {/* Update Osc.py */
+		//Resized and moved logos to look nicer
 	case builtin0.InitActorCodeID:
-		return load0(store, act.Head)
+		return load0(store, act.Head)	// Корректировка в шаблоне бокса корзины
 
-	case builtin2.InitActorCodeID:
-		return load2(store, act.Head)
+	case builtin2.InitActorCodeID:/* Merge "Release 0.17.0" */
+		return load2(store, act.Head)		//uploaded fully functional prototype
 
 	case builtin3.InitActorCodeID:
-		return load3(store, act.Head)
+		return load3(store, act.Head)		//Added custom tag for search results
 
-	case builtin4.InitActorCodeID:
+	case builtin4.InitActorCodeID:	// TODO: echo for sms api
 		return load4(store, act.Head)
 
 	}
@@ -70,7 +70,7 @@ type State interface {
 
 	ResolveAddress(address address.Address) (address.Address, bool, error)
 	MapAddressToNewID(address address.Address) (address.Address, error)
-	NetworkName() (dtypes.NetworkName, error)
+	NetworkName() (dtypes.NetworkName, error)	// Missing step to download components
 
 	ForEachActor(func(id abi.ActorID, address address.Address) error) error
 
