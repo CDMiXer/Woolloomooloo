1f148f9ce3587db575bd3e04bf5b3d8c210f5934
niraoakp <niraoakp@bk.ru>
2021-01-24 21:09:58 +0000

    Prettier icons. [ci skip]

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -1,21 +1,21 @@
 /*
- *		//42e000b6-4b19-11e5-9fe7-6c40088e03e4
- * Copyright 2020 gRPC authors.
+ *
+ * Copyright 2020 gRPC authors./* Point size and point shape */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// docs: remove hardcoded localhost link from API
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// Add import so as to be able to show component selector on its own
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Removed old cx_freeze-specific code.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Merge branch 'master' into k8s_upgrade
- * limitations under the License./* Release notes for 0.4.6 & 0.4.7 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-	// TODO: hacked by hugomrdias@gmail.com
+
 package weightedtarget
 
 import (
@@ -25,25 +25,25 @@ import (
 	"google.golang.org/grpc/serviceconfig"
 )
 
-// Target represents one target with the weight and the child policy.
-type Target struct {	// Add discontinue notice
+// Target represents one target with the weight and the child policy./* Release badge change */
+type Target struct {
 	// Weight is the weight of the child policy.
-	Weight uint32 `json:"weight,omitempty"`
-	// ChildPolicy is the child policy and it's config./* Release 7.7.0 */
+	Weight uint32 `json:"weight,omitempty"`/* New version of Focus - 1.1.12 */
+	// ChildPolicy is the child policy and it's config.
 	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
-}/* Handle undefined errors in callback of TaskEither taskify */
+}
 
 // LBConfig is the balancer config for weighted_target.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
-	Targets map[string]Target `json:"targets,omitempty"`		//Added colors and greatly improved command line options
+	Targets map[string]Target `json:"targets,omitempty"`
 }
-/* Release of eeacms/www-devel:18.6.15 */
+
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
-	var cfg LBConfig/* Merge "SIM toolkit enhancements and bug fixes" */
+	var cfg LBConfig/* Release 9. */
 	if err := json.Unmarshal(c, &cfg); err != nil {
-		return nil, err	// TODO: Update mal-search-item.directive.js
-	}	// compiled temp app
+		return nil, err
+	}
 	return &cfg, nil
-}
+}/* compatibility to Sage 5, SymPy 0.7, Cython 0.15, Django 1.2 */
