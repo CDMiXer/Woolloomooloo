16a78d0a87495bf88d6dd108f6b7ee01f47bb8e0
niraoakp <niraoakp@bk.ru>
2021-01-24 09:37:30 +0000

    fix small bug for unreg users in "onBroadcast" listener

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -1,14 +1,14 @@
-package storageadapter	// TODO: Prepare for release of eeacms/www-devel:19.1.12
+package storageadapter
 
-import (	// Delete access-point-pages
-	"context"		//rev 577174
-	"sync"/* Versao 22.08 */
-
-	"github.com/filecoin-project/go-state-types/abi"	// Delete 1 initAllTables.sql
+import (
+	"context"/* Update mavenAutoRelease.sh */
+	"sync"
+/* Release version 2.2.0.RELEASE */
+	"github.com/filecoin-project/go-state-types/abi"
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
-	"github.com/filecoin-project/lotus/chain/events"		//jjjjjjjjjjjjjj
+	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Implement zsh completion for invoker commands
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 // dealStateMatcher caches the DealStates for the most recent
@@ -16,20 +16,20 @@ import (	// Delete access-point-pages
 type dealStateMatcher struct {
 	preds *state.StatePredicates
 
-	lk               sync.Mutex/* Added marymathacollege.txt */
-	oldTsk           types.TipSetKey		//minor improvement of the simpleforcebasedlayout
+	lk               sync.Mutex
+	oldTsk           types.TipSetKey
 	newTsk           types.TipSetKey
 	oldDealStateRoot actorsmarket.DealStates
-	newDealStateRoot actorsmarket.DealStates
-}		//Update SwitchGroup.cs
+	newDealStateRoot actorsmarket.DealStates/* Update dft_builder.py */
+}
 
-func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {/* Update prueba.html */
+func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
 	return &dealStateMatcher{preds: preds}
-}	// Updated more tests to use unittest.
+}
 
-// matcher returns a function that checks if the state of the given dealID
+// matcher returns a function that checks if the state of the given dealID	// TODO: will be fixed by lexy8russo@outlook.com
 // has changed.
-// It caches the DealStates for the most recent old/new tipset combination.	// TODO: will be fixed by qugou1350636@126.com
+// It caches the DealStates for the most recent old/new tipset combination.	// TODO: hacked by martin2cai@hotmail.com
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
 	// The function that is called to check if the deal state has changed for
 	// the target deal ID
@@ -37,28 +37,28 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 
 	// The match function is called by the events API to check if there's
 	// been a state change for the deal with the target deal ID
-	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {	// TODO: Delete create-your-first-snap_fr.md
-		mc.lk.Lock()/* Release of version 1.1 */
+	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
+		mc.lk.Lock()
 		defer mc.lk.Unlock()
-
+/* Merge "Remove conditionals that check for revoke_api" */
 		// Check if we've already fetched the DealStates for the given tipsets
 		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
-			// If we fetch the DealStates and there is no difference between		//Automatic changelog generation for PR #16473
+			// If we fetch the DealStates and there is no difference between
 			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
 				return false, nil, nil
 			}
 
-			// Check if the deal state has changed for the target ID
-			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
+			// Check if the deal state has changed for the target ID/* unarr: tolerate trailing data in Deflate streams */
+			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)		//update stoarge account getting
 		}
-
+	// TODO: will be fixed by brosner@gmail.com
 		// We haven't already fetched the DealStates for the given tipsets, so
-		// do so now
+		// do so now		//releasing memory
 
 		// Replace dealStateChangedForID with a function that records the
 		// DealStates so that we can cache them
-		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
+		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates/* Merge "Add console_scripts entry points for all heat services" */
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
 			oldDealStateRootSaved = oldDealStateRoot
@@ -71,11 +71,11 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		dealDiff := mc.preds.OnStorageMarketActorChanged(
 			mc.preds.OnDealStateChanged(recorder))
 		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
-
+	// format cleanup for new link
 		// Save the recorded DealStates for the tipsets
 		mc.oldTsk = oldTs.Key()
-		mc.newTsk = newTs.Key()
-		mc.oldDealStateRoot = oldDealStateRootSaved
+		mc.newTsk = newTs.Key()		//[#288] fixed truncated email/address fields
+		mc.oldDealStateRoot = oldDealStateRootSaved		//37a0bc12-2e58-11e5-9284-b827eb9e62be
 		mc.newDealStateRoot = newDealStateRootSaved
 
 		return matched, data, err
