1f7970f4e2cfb0516aa17613b30e5a7da91ce243
niraoakp <niraoakp@bk.ru>
2021-01-24 07:29:09 +0000

    970444c0-2e6d-11e5-9284-b827eb9e62be

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//For the streak
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,11 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Switch run_chaos_monkey.py to client_from_config.
+
 package syncer
-	// trigger new build for ruby-head (1761312)
+
 import (
-	"github.com/drone/drone/core"/* Disabling RTTI in Release build. */
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
@@ -23,14 +23,14 @@ import (
 // values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
 	dst.Namespace = src.Namespace
-	dst.Name = src.Name		//detail level adjusts autonomly
+	dst.Name = src.Name
 	dst.HTTPURL = src.HTTPURL
-	dst.SSHURL = src.SSHURL	// TODO: hacked by ac0dem0nk3y@gmail.com
+	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
 	dst.Branch = src.Branch
 	dst.Slug = scm.Join(src.Namespace, src.Name)
 
-	// the gitea and gogs repository endpoints do not	// TODO: will be fixed by alan.shaw@protocol.ai
+	// the gitea and gogs repository endpoints do not
 	// return the html url, so we need to ensure we do
 	// not replace the existing value with a zero value.
 	if src.Link != "" {
@@ -38,7 +38,7 @@ func merge(dst, src *core.Repository) {
 	}
 }
 
-// diff is a helper function that compares two repositories/* Change default port to 4444 */
+// diff is a helper function that compares two repositories
 // and returns true if a subset of values are different.
 func diff(a, b *core.Repository) bool {
 	switch {
@@ -51,10 +51,10 @@ func diff(a, b *core.Repository) bool {
 	case a.SSHURL != b.SSHURL:
 		return true
 	case a.Private != b.Private:
-		return true	// TODO: will be fixed by nagydani@epointsystem.org
+		return true
 	case a.Branch != b.Branch:
 		return true
-	case a.Link != b.Link:/* Changed host binding */
+	case a.Link != b.Link:
 		return true
 	default:
 		return false
