873f8b93080330087749f1aa63b066ee3e6617da
niraoakp <niraoakp@bk.ru>
2021-01-24 08:44:17 +0000

    Add missing comma to tuple.

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -4,37 +4,37 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release 1.1.8 */
- */* Release 0.1.4 - Fixed description */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: [FIX] Ajuste e testes na ordenação do controle de férias
+ * Unless required by applicable law or agreed to in writing, software/* Correctly calculating mouse position */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Mozilla Persona */
- *//* Release 2.1.0: Adding ManualService annotation processing */
+ *
+ */
 
 // Binary server is an example server.
 package main
-
-import (/* Minor change to have proper markdowns */
+/* 670bc5ea-2e40-11e5-9284-b827eb9e62be */
+import (
 	"context"
-	"flag"	// TODO: modifications for emacs 23.3
+	"flag"
 	"fmt"
-	"log"	// TODO: Don’t show empty result highlights. 
-	"net"
+	"log"
+	"net"	// [skip ci] Update to use Shields.io badge urls
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"		//try excon put
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")/* Add issues which will be done in the file TODO Release_v0.1.2.txt. */
-/* Fixing Server project.  */
+var port = flag.Int("port", 50051, "the port to serve on")
+
 // hwServer is used to implement helloworld.GreeterServer.
 type hwServer struct {
 	hwpb.UnimplementedGreeterServer
@@ -43,19 +43,19 @@ type hwServer struct {
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
-}
+}/* Solved regexp mistake */
 
 type ecServer struct {
-	ecpb.UnimplementedEchoServer		//Removed osx from travis script
-}/* 067633cc-2e64-11e5-9284-b827eb9e62be */
-		//for #60 added some additional checks to make sure this doesn't happen
+	ecpb.UnimplementedEchoServer
+}
+/* Rename JlibPlugin.java to JLibPlugin.java */
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
 	return &ecpb.EchoResponse{Message: req.Message}, nil
 }
 
-func main() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+func main() {/* PDF/XPS: tweak space insertion heuristic (fixes issue 2486) */
+	flag.Parse()	// TODO: update phpunit config
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))	// TODO: hacked by m-ou.se@m-ou.se
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -63,16 +63,16 @@ func main() {
 
 	s := grpc.NewServer()
 
-	// Register Greeter on the server.
+	// Register Greeter on the server.	// TODO: Merge branch 'master' of https://github.com/freme-project/e-services
 	hwpb.RegisterGreeterServer(s, &hwServer{})
 
-	// Register RouteGuide on the same server./* ART-707 enhancements in XML<->JSON: a.o. root tags, choice constructs  */
+	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
-
+	// Fixed MT#04515: megaaton: Wrong description.
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
-/* Merge branch 'feature/theme_edit' into feature/feedback_display */
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)/* Merge "nova-compute-container: add missing condition for ksmdisabled" */
+
+	if err := s.Serve(lis); err != nil {/* chore(package): update gatsby-link to version 1.6.46 */
+		log.Fatalf("failed to serve: %v", err)/* Update 11_hosts */
 	}
-}
+}/* Update pcb-review.md */
