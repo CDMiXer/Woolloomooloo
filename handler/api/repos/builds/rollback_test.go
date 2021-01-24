aa2a9e09a5b3576c6b8dde4fbe6e051f9a66aaa1
niraoakp <niraoakp@bk.ru>
2021-01-24 08:14:30 +0000

    Removed coveralls badge.

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//bundle-size: 474c9f9cad9d29bf6edae2c16d63e30ed3d6e64c.json
+// +build !oss
 
 package builds
