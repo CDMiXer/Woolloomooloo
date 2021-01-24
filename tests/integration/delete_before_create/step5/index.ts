f46dbf6eacd86f2e2cbf534e5d28ad730c81a6da
niraoakp <niraoakp@bk.ru>
2021-01-24 11:01:43 +0000

    make round look ......... round

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -2,17 +2,17 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Release v0.1.0-beta.13 */
-// The DBR deletion of A triggers the deletion of C due to dependency.
+
+// The DBR deletion of A triggers the deletion of C due to dependency./* turn confgen text files into i18n_variables */
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent	// Possible issue fix up
+//   1. DeleteReplacement Dependent	// TODO: Fixed bug where data_dir was wrongly assigned
 //   2. DeleteReplacement Base
-//   3. Replace Base/* Version 2.0 Release Notes Updated */
-//   4. CreateReplacement Base	// Refactor by renaming variables (Refs #11199).
+//   3. Replace Base
+//   4. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
 
-//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)	// TODO: Statement compilation now uses shadowed infos
-//   5. DeleteReplacement Base-2
+//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
+//   5. DeleteReplacement Base-2	// TODO: 8a4f3e10-2e5e-11e5-9284-b827eb9e62be
 //   6. Replace Base-2
 //   7. CreateReplacement Base-2
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
