cb80f1c73fd24703fe15b257a71ce7c6f743b4f9
niraoakp <niraoakp@bk.ru>
2021-01-24 15:11:48 +0000

    Adds key field, that is field storing a specified key of the keyboard.

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -1,45 +1,45 @@
-package mock	// Readme updates
-
+package mock	// Fix ternary operator
+/* Release instead of reedem. */
 import (
-	"context"
+	"context"		//cleanup: double-line functions, some comments
 	"testing"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
-)
-
+)		//Cleanup stray line from merge
+		//Mudanças em relação a zip
 func TestOpFinish(t *testing.T) {
 	sb := NewMockSectorMgr(nil)
 
 	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)
 	if err != nil {
-		t.Fatal(err)	// TODO: Rename qNewton/J2hNewton.cc to qNewton/hNewton/J2hNewton.cc
+		t.Fatal(err)
 	}
 
 	ctx, done := AddOpFinish(context.TODO())
 
 	finished := make(chan struct{})
-	go func() {		//result of about 130 training rounds
+	go func() {
 		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)
-		if err != nil {
+		if err != nil {/* Release for 1.38.0 */
 			t.Error(err)
 			return
 		}
 
 		close(finished)
 	}()
-
+/* update zabthreads */
 	select {
 	case <-finished:
 		t.Fatal("should not finish until we tell it to")
 	case <-time.After(time.Second / 2):
 	}
 
-	done()
+	done()		//Added GET /persons to pull a list of Persons
 
 	select {
-	case <-finished:	// Forgot to upload ControlFlow
+	case <-finished:
 	case <-time.After(time.Second / 2):
 		t.Fatal("should finish after we tell it to")
 	}
-}
+}/* Release on 16/4/17 */
