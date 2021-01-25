e1bc11ba344b6e72c5c2a3e61a3d399fce023127
niraoakp <niraoakp@bk.ru>
2021-01-25 17:21:47 +0000

    new header design

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss	// TODO: :wave::walking: Updated in browser at strd6.github.io/editor
+/* Release 0.8.5.1 */
 package mockscm
-
+		//Update Keycodes.md
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
