480dab802c3a26ae4eb6a4a226f9d30177589478
niraoakp <niraoakp@bk.ru>
2021-01-25 01:35:18 +0000

    Merge "SSH command list-projects for listing project imports"

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
-#!/bin/sh	// TODO: will be fixed by witek@enjin.io
+#!/bin/sh
 
-echo "building docker images for ${GOOS}/${GOARCH} ..."		//Fixup some little 'cosmetic' issues with 'ExpandableSection' code.
+echo "building docker images for ${GOOS}/${GOARCH} ..."
 
 REPO="github.com/drone/drone"
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled
-export CGO_ENABLED=0/* Release 7.12.87 */
-go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
+# compile the runners with gcc disabled/* * Fix tiny oops in interface.py. Release without bumping application version. */
+export CGO_ENABLED=0
+go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent/* Release 1.0.2 [skip ci] */
 go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
