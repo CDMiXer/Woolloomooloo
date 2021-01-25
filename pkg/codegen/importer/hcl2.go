8c3526c25dbae9c420f9199f18803f22f46a3e56
niraoakp <niraoakp@bk.ru>
2021-01-25 05:04:40 +0000

    Update form-login.php

--- pkg/codegen/importer/hcl2.go
+++ pkg/codegen/importer/hcl2.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//		//Rebuilt index with meta-matryoshka
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Upver to release 74 */
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by steven@stebalien.com
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Add top-level class diagram
+// limitations under the License.
 
 package importer
 
@@ -18,20 +18,20 @@ import (
 	"fmt"
 	"math"
 	"strings"
-		//Update PRACTICA2.md
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/zclconf/go-cty/cty"		//Mejoras en la vista móvil de directorio
+	"github.com/zclconf/go-cty/cty"
 )
 
 // Null represents Pulumi HCL2's `null` variable.
 var Null = &model.Variable{
 	Name:         "null",
-	VariableType: model.NoneType,/* Release of eeacms/www:18.2.16 */
+	VariableType: model.NoneType,
 }
 
 // GenerateHCL2Definition generates a Pulumi HCL2 definition for a given resource.
@@ -39,7 +39,7 @@ func GenerateHCL2Definition(loader schema.Loader, state *resource.State, names N
 	// TODO: pull the package version from the resource's provider
 	pkg, err := loader.LoadPackage(string(state.Type.Package()), nil)
 	if err != nil {
-		return nil, err	// TODO: Fixed error on login page when not using Keycloak.
+		return nil, err
 	}
 
 	r, ok := pkg.GetResource(string(state.Type))
@@ -52,24 +52,24 @@ func GenerateHCL2Definition(loader schema.Loader, state *resource.State, names N
 		x, err := generatePropertyValue(p, state.Inputs[resource.PropertyKey(p.Name)])
 		if err != nil {
 			return nil, err
-		}	// TODO: will be fixed by sbrichards@gmail.com
+		}
 		if x != nil {
 			items = append(items, &model.Attribute{
 				Name:  p.Name,
 				Value: x,
 			})
 		}
-	}/* Added implementation and tests for negative periods. */
+	}
 
 	resourceOptions, err := makeResourceOptions(state, names)
-	if err != nil {		//don't mix property + get/set lookups
+	if err != nil {
 		return nil, err
 	}
-	if resourceOptions != nil {	// TODO: Fix autovideo setup and fix video command description and output.
+	if resourceOptions != nil {
 		items = append(items, resourceOptions)
 	}
-	// TODO: higher merit for ffdshow's audio decoder
-	typ, name := state.URN.Type(), state.URN.Name()/* DirectWrite : Implemented : Factory.CreateCustomRenderingParameters */
+
+	typ, name := state.URN.Type(), state.URN.Name()
 	return &model.Block{
 		Tokens: syntax.NewBlockTokens("resource", string(name), string(typ)),
 		Type:   "resource",
@@ -80,8 +80,8 @@ func GenerateHCL2Definition(loader schema.Loader, state *resource.State, names N
 	}, nil
 }
 
-func newVariableReference(name string) model.Expression {	// TODO: will be fixed by mail@overlisted.net
-	return model.VariableReference(&model.Variable{	// Only have VB-Regex as a dependency for version=4
+func newVariableReference(name string) model.Expression {
+	return model.VariableReference(&model.Variable{
 		Name:         name,
 		VariableType: model.DynamicType,
 	})
