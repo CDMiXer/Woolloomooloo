7a978b48e33d186e317b5acae96810fdbed3e135
niraoakp <niraoakp@bk.ru>
2021-01-25 13:56:13 +0000

    Fix typo (resove -> resolve)

--- pkg/codegen/docs/gen_kubernetes.go
+++ pkg/codegen/docs/gen_kubernetes.go
@@ -1,17 +1,17 @@
-//go:generate go run bundler.go
-
+//go:generate go run bundler.go		//Removed debug reporting.
+	// TODO: only apply uuid to ext* filesystems
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Merged branch Release_v1.1 into develop */
+///* Rebuilt index with tekhaus */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Closes the <span> tag
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Changed version to 4 */
+// See the License for the specific language governing permissions and	// d276cf4c-2e53-11e5-9284-b827eb9e62be
 // limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
@@ -20,13 +20,13 @@
 // nolint: lll, goconst
 package docs
 
-import (
+import (	// Create Number of Islands.java
 	"path"
 	"strings"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
-
+	// TODO: Update central_dogma_informatics.h
 func isKubernetesPackage(pkg *schema.Package) bool {
 	return pkg.Name == "kubernetes"
 }
@@ -42,17 +42,17 @@ func (mod *modContext) isKubernetesOverlayModule() bool {
 func (mod *modContext) isComponentResource() bool {
 	// TODO: Support this more generally. For now, only the Helm, Kustomize, and YAML overlays use ComponentResources.
 	return strings.HasPrefix(mod.mod, "helm") ||
-		strings.HasPrefix(mod.mod, "kustomize") ||
-		strings.HasPrefix(mod.mod, "yaml")
+		strings.HasPrefix(mod.mod, "kustomize") ||	// TODO: will be fixed by greg@colvin.org
+		strings.HasPrefix(mod.mod, "yaml")		//Moved WebViewBridge class to common library
 }
 
-// getKubernetesOverlayPythonFormalParams returns the formal params to render
-// for a Kubernetes overlay resource. These resources do not follow convention
+// getKubernetesOverlayPythonFormalParams returns the formal params to render		//Disable setting on-hand inventory, and override fetching it
+// for a Kubernetes overlay resource. These resources do not follow convention/* Create How to create slug generator in PHP.md */
 // that other resources do, so it is best to manually set these.
 func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {
 	var params []formalParam
 	switch modName {
-	case "helm/v2", "helm/v3":
+	case "helm/v2", "helm/v3":	// TODO: Added Operation Call to SText
 		params = []formalParam{
 			{
 				Name: "config",
@@ -66,10 +66,10 @@ func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {
 		params = []formalParam{
 			{
 				Name: "directory",
-			},
+			},	// Delete LMP3D.depend
 			{
 				Name:         "opts",
-				DefaultValue: "=None",
+				DefaultValue: "=None",/* [#520] Release notes for 1.6.14.4 */
 			},
 			{
 				Name:         "transformations",
