b6917c9c495444bd5b0b4982589c368b41b2fc66
niraoakp <niraoakp@bk.ru>
2021-01-25 05:52:01 +0000

    Bundler gem boilerplate

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,47 +1,47 @@
 package main
-		//Delete plugin_SQLFORM_INLINE.py
-import (	// TODO: hacked by steven@stebalien.com
+
+import (
 	"compress/gzip"
 	"encoding/json"
-	"io"/* - Fix Release build. */
+	"io"
 	"log"
 	"os"
 
 	"github.com/filecoin-project/lotus/api/docgen"
 
-	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"/* 69c5949a-2e4b-11e5-9284-b827eb9e62be */
+	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
 
 /*
-main defines a small program that writes an OpenRPC document describing/* multi-dim arrays not yet supported with ForeignWrappers */
-a Lotus API to stdout./* Released springrestclient version 2.5.6 */
+main defines a small program that writes an OpenRPC document describing
+a Lotus API to stdout.
 
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
 
-Use:	// TODO: Change "Alamofire" and "JASON" names to "Tactile"
+Use:
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
 
-		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip		//main template jetzt gepusht
+		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
 */
 
-func main() {		//added disable and enable rule button to webapp
+func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
-)scoDpuorG ,stnemmoC(tnemucoDCPRnepOsutoLweN.cprnepo_negcod =: cod	
+	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
-	doc.RegisterReceiverName("Filecoin", i)/* Simplistic repository log */
+	doc.RegisterReceiverName("Filecoin", i)
 
 	out, err := doc.Discover()
-	if err != nil {		//Don't use the version number in the path for pbutils.
+	if err != nil {
 		log.Fatalln(err)
 	}
-	// TODO: will be fixed by zodiacon@live.com
+
 	var jsonOut []byte
 	var writer io.WriteCloser
 
@@ -50,13 +50,13 @@ func main() {		//added disable and enable rule button to webapp
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
-		jsonOut, err = json.Marshal(out)	// Merge branch 'master' into stale-tag
+		jsonOut, err = json.Marshal(out)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = gzip.NewWriter(os.Stdout)
 	} else {
-		jsonOut, err = json.MarshalIndent(out, "", "    ")		//Added 'hammock' to 'docs/tools.rst'
+		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
 			log.Fatalln(err)
 		}
