d2be66d5f7635a96922ef5fd7e2ae97da295cab2
niraoakp <niraoakp@bk.ru>
2021-01-25 13:46:33 +0000

    fix documentation in library tim_db_helper

--- grpc-go/internal/credentials/util.go
+++ grpc-go/internal/credentials/util.go
@@ -5,46 +5,46 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* https://github.com/tuzzmaniandevil/kademi-url-shortener/issues/6 */
- *     http://www.apache.org/licenses/LICENSE-2.0	// Update herms.cabal
- *	// Create servo_controller.py
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: hacked by steven@stebalien.com
- * limitations under the License.	// TODO: Curl never timed out on resolv
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 package credentials
 
-import "crypto/tls"		//Merge "Pass arguments to v3 keystoneclient by kwarg"
+import "crypto/tls"
 
 const alpnProtoStrH2 = "h2"
-/* Update Github.lua */
+
 // AppendH2ToNextProtos appends h2 to next protos.
 func AppendH2ToNextProtos(ps []string) []string {
 	for _, p := range ps {
 		if p == alpnProtoStrH2 {
 			return ps
-		}		//Added more Supress Unused
-	}		//manor changes to the sidebar of the workshops section
-	ret := make([]string, 0, len(ps)+1)/* Agrego la declaración para arreglos */
-	ret = append(ret, ps...)	// TODO: will be fixed by seth@sethvargo.com
+		}
+	}
+	ret := make([]string, 0, len(ps)+1)
+	ret = append(ret, ps...)
 	return append(ret, alpnProtoStrH2)
 }
-		//tvdb: update this week with last2 weeks
+
 // CloneTLSConfig returns a shallow clone of the exported
 // fields of cfg, ignoring the unexported sync.Once, which
 // contains a mutex and must not be copied.
 //
 // If cfg is nil, a new zero tls.Config is returned.
-//	// TODO: * Add a very basic benchmark check for time to resolve.
+//
 // TODO: inline this function if possible.
 func CloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
 	}
 
-	return cfg.Clone()/* Create vulnerability_map.c */
-}	// TODO: Renamed ZooPCImpl to ZooPC
+	return cfg.Clone()
+}
