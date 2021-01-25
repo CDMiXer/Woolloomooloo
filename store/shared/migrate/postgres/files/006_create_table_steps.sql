bd1684bf58735d105f0ac0d9ce092b0239a32174
niraoakp <niraoakp@bk.ru>
2021-01-25 14:56:25 +0000

    Modified containsPoint

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps		//Vim: when leaving insert/replace mode, use moveXorSol 1 instead of leftB
-
+-- name: create-table-steps
+/* Use the new method for running a command. */
 CREATE TABLE IF NOT EXISTS steps (
- step_id          SERIAL PRIMARY KEY
-,step_stage_id    INTEGER/* Release of eeacms/www:18.7.25 */
+ step_id          SERIAL PRIMARY KEY/* Constify string arguments in xrdp-chansrv sources */
+,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)		//Create KerbalAircraftExpansion.netkan
-,step_error       VARCHAR(500)
+,step_status      VARCHAR(50)
+,step_error       VARCHAR(500)/* Release Notes: Add notes for 2.0.15/2.0.16/2.0.17 */
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER/* Release 0.2.0 merge back in */
+,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)/* Update 274.md */
+,UNIQUE(step_stage_id, step_number)/* Release 30.2.0 */
 );
-
+/* Released 1.5.2. */
 -- name: create-index-steps-stage
-
-CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);	// Added the basic lifter code
+/* Release nvx-apps 3.8-M4 */
+CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
