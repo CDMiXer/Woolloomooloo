86f7d88502c79bc56e608cd1d4687c414006483e
niraoakp <niraoakp@bk.ru>
2021-01-25 16:47:46 +0000

    Añadido esquema de la base de datos

--- core/license_test.go
+++ core/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* optimization lead to wrong result, trying to fix */
 // +build !oss
 
 package core
