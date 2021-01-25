7cee9f480ae2b7593fb47e3e87994f9b6296dc1c
niraoakp <niraoakp@bk.ru>
2021-01-25 02:53:56 +0000

    Included utf-8 specification

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
-import "github.com/ipfs/go-log/v2"
-
+import "github.com/ipfs/go-log/v2"		//Remove pdb statements
+/* Release 2.0 final. */
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")
-	_ = log.SetLogLevel("storageminer", "ERROR")
+	_ = log.SetLogLevel("sub", "ERROR")/* Release 1.0.39 */
+	_ = log.SetLogLevel("storageminer", "ERROR")	// Clean up profiles a bit.
 	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")/* Initial Commit and some RDD */
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")/* Projektbeschreibung verändert */
 }
