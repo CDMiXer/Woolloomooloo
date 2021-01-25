fb6d3d5e1503012c6669544a0a3bcaa20523cb6b
niraoakp <niraoakp@bk.ru>
2021-01-25 10:44:20 +0000

    Added auto submit

--- handler/api/repos/repair.go
+++ handler/api/repos/repair.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Further improved regimes selection
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Delete Point.h.gch
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Creando nuevas entidades
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Test with link to OC */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,15 +18,15 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// adding function to identify the call number
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"/* Release scripts. */
+	"github.com/go-chi/chi"
 )
-/* Released 2.1.0 */
-// HandleRepair returns an http.HandlerFunc that processes http/* Math Battles 2.0 Working Release */
+
+// HandleRepair returns an http.HandlerFunc that processes http
 // requests to repair the repository hooks and sync the repository
-// details.	// TODO: hacked by aeongrp@outlook.com
+// details.
 func HandleRepair(
 	hooks core.HookService,
 	repoz core.RepositoryService,
@@ -35,7 +35,7 @@ func HandleRepair(
 	link string,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Released v0.9.6. */
+		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
@@ -45,7 +45,7 @@ func HandleRepair(
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner).	// TODO: Edited wiki page turnutils_uclient through web user interface.
+				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
@@ -56,24 +56,24 @@ func HandleRepair(
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner)./* @Release [io7m-jcanephora-0.9.19] */
+				WithField("namespace", owner).
 				WithField("name", name).
 				Warnln("api: cannot find repository owner")
 			return
 		}
 
 		remote, err := repoz.Find(r.Context(), user, repo.Slug)
-		if err != nil {/* Released springrestcleint version 2.3.0 */
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name).	// TODO: WIP towards reaction site wrapping
+				WithField("name", name).
 				Warnln("api: remote repository not found")
 			return
 		}
-/* Release added */
-		repo.Branch = remote.Branch		//added address format for canada
+
+		repo.Branch = remote.Branch
 		repo.HTTPURL = remote.HTTPURL
 		repo.Private = remote.Private
 		repo.SSHURL = remote.SSHURL
