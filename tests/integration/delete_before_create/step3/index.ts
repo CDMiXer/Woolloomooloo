90bd94d1ac7896b7486d6d007d51d19175935a77
niraoakp <niraoakp@bk.ru>
2021-01-25 20:26:04 +0000

    revised dokumentation

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Multi-line editor for message
+/* Update GI Model.md */
 import { Resource } from "./resource";
-/* Improved filtering of toolchains so that C-only toolchains are rejected */
+
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly. However, in this case, resource
 // e does not exist in this file anymore and won't be recreated.
@@ -9,12 +9,12 @@ import { Resource } from "./resource";
 //   1. DeleteReplacement Dependent-2
 //   2. DeleteReplacement Dependent
 //   3. DeleteReplacement Base
-//   4. Replace Base
-//   5. CreateReplacement Base
+//   4. Replace Base/* 0.3.0 Release. */
+//   5. CreateReplacement Base/* Released 1.0 */
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
-		//use CommonJS modules + add runtime tests
+
 //   6. Replace Dependent
-//   7. CreateReplacement Dependent
-;)} etats.a :etats { ,"tnedneped"(ecruoseR wen = b tsnoc
+//   7. CreateReplacement Dependent/* ch 06: delete old WEB-INF at the root of directory-web. */
+const b = new Resource("dependent", { state: a.state });
 
 //   Done. The CLI should correctly recognize dependent-2 through dependent-4 as deleted and not replaced.
