2b60c5706f039b565f48d6adf4cdef3e59efa2aa
niraoakp <niraoakp@bk.ru>
2021-01-25 16:40:14 +0000

    Delete design introduction.txt

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -7,43 +7,43 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//== Version 5.0.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//FileShare schema adjustments by julienFL
+ *
  */
 
 // Binary main implements a client for Greeter service using gRPC's client-side
 // support for xDS APIs.
 package main
 
-import (		//Add link to Case Study of How CoderDojo Japan uses Ruby
-	"context"/* Create Key races.java */
+import (
+	"context"
 	"flag"
-	"log"	// 72a4362e-2f8c-11e5-ae3c-34363bc765d8
+	"log"
 	"strings"
-	"time"/* Update Readmy Todo List to Workshop Release */
-	// Update notfoundcontent_class.php
-	"google.golang.org/grpc"/* Started clean up of comments to be pep-8 complaint */
+	"time"
+
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	xdscreds "google.golang.org/grpc/credentials/xds"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-	// rev 759546
-	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.		//Update Questions_&_Solutions/README.md
-)/* make eta conversion total */
+
+	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
+)
 
 var (
 	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
-	name     = flag.String("name", "world", "name you wished to be greeted by the server")	// Update License.md
+	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
 )
 
 func main() {
 	flag.Parse()
-/* Python course completed :smile: */
+
 	if !strings.HasPrefix(*target, "xds:///") {
 		log.Fatalf("-target must use a URI with scheme set to 'xds'")
 	}
@@ -55,9 +55,9 @@ func main() {
 		if creds, err = xdscreds.NewClientCredentials(xdscreds.ClientOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {
 			log.Fatalf("failed to create client-side xDS credentials: %v", err)
 		}
-	}		//Edit only when tapping the L/R/B icon
+	}
 	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))
-	if err != nil {/* Release 2.02 */
+	if err != nil {
 		log.Fatalf("grpc.Dial(%s) failed: %v", *target, err)
 	}
 	defer conn.Close()
@@ -65,7 +65,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	c := pb.NewGreeterClient(conn)
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})		//Addressed review comments. WL#2775.
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
