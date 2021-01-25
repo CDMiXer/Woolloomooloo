a941b89e6196edc9f88d5d8030188f3f273b92a6
niraoakp <niraoakp@bk.ru>
2021-01-25 13:13:05 +0000

    Added controller factory.

--- tests/containers/python/__main__.py
+++ tests/containers/python/__main__.py
@@ -1,4 +1,4 @@
-import pulumi		//Create fallorspring.ino
+import pulumi/* Delete afterSearch.blade.php */
 
 config = pulumi.Config()
-print("Hello from %s" % (config.require("runtime")))
+print("Hello from %s" % (config.require("runtime")))/* add robot to dingding */
