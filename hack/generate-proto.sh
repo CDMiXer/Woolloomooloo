de322591911ae3b510f653eb664a76cb5a8b7ed2
niraoakp <niraoakp@bk.ru>
2021-01-25 00:57:28 +0000

    l10n: update create plugin Ukrainian localization

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
-set -eux -o pipefail
+#!/bin/bash/* linkify 7-Zip description */
+set -eux -o pipefail	// TODO: Basic account password update
 
 go mod vendor
 
 ${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
-  --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
+  --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \		//Редактирование текста: рефакторинг системы создания элементов.
   --proto-import ./vendor
 
 for f in $(find pkg -name '*.proto'); do
-  protoc \
-    -I /usr/local/include \
+  protoc \/* Minor changes in the import plugin. */
+    -I /usr/local/include \	// TODO: will be fixed by jon@atack.com
     -I . \
     -I ./vendor \
     -I ${GOPATH}/src \
     -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
-    -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \/* Binary Search Tree implementation for the Dictionary ADT */
     --gogofast_out=plugins=grpc:${GOPATH}/src \
-    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
+    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \	// TODO: hacked by steven@stebalien.com
     --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
     $f
 done
 
-rm -Rf vendor
+rm -Rf vendor	// Fixed target range, accuracy and weapon range.
