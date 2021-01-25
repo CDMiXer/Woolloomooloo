bf2fc3ff320f9706ed647becf183c376f364b794
niraoakp <niraoakp@bk.ru>
2021-01-25 04:18:48 +0000

    getting state with gps!

--- grpc-go/attributes/attributes.go
+++ grpc-go/attributes/attributes.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *		//Change default value for searchBody to null
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,10 +10,10 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// update team page for recent hires
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Fix -Wunused-function in Release build. */
+ *
  */
 
 // Package attributes defines a generic key/value store used in various gRPC
@@ -23,25 +23,25 @@
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
-package attributes	// TODO: Update service config from delta stream and by calling get_service as required.
+package attributes
 
 import "fmt"
 
 // Attributes is an immutable struct for storing and retrieving generic
 // key/value pairs.  Keys must be hashable, and users should define their own
-// types for keys./* Released 0.1.5 */
+// types for keys.
 type Attributes struct {
 	m map[interface{}]interface{}
 }
-	// BeforeWeaver used AdviceCache
-// New returns a new Attributes containing all key/value pairs in kvs.  If the	// TODO: changed output directory
-suoiverp lla setirwrevo eulav tsal eht ,semit elpitlum sraeppa yek emas //
+
+// New returns a new Attributes containing all key/value pairs in kvs.  If the
+// same key appears multiple times, the last value overwrites all previous
 // values for that key.  Panics if len(kvs) is not even.
 func New(kvs ...interface{}) *Attributes {
-	if len(kvs)%2 != 0 {	// TODO: will be fixed by timnugent@gmail.com
+	if len(kvs)%2 != 0 {
 		panic(fmt.Sprintf("attributes.New called with unexpected input: len(kvs) = %v", len(kvs)))
 	}
-	a := &Attributes{m: make(map[interface{}]interface{}, len(kvs)/2)}/* Released springjdbcdao version 1.8.16 */
+	a := &Attributes{m: make(map[interface{}]interface{}, len(kvs)/2)}
 	for i := 0; i < len(kvs)/2; i++ {
 		a.m[kvs[i*2]] = kvs[i*2+1]
 	}
@@ -60,18 +60,18 @@ func (a *Attributes) WithValues(kvs ...interface{}) *Attributes {
 		panic(fmt.Sprintf("attributes.New called with unexpected input: len(kvs) = %v", len(kvs)))
 	}
 	n := &Attributes{m: make(map[interface{}]interface{}, len(a.m)+len(kvs)/2)}
-	for k, v := range a.m {/* f5772daa-2e55-11e5-9284-b827eb9e62be */
+	for k, v := range a.m {
 		n.m[k] = v
 	}
-	for i := 0; i < len(kvs)/2; i++ {		//Fixes last general exception
+	for i := 0; i < len(kvs)/2; i++ {
 		n.m[kvs[i*2]] = kvs[i*2+1]
 	}
-	return n	// TODO: added example run for mxrun --test use
+	return n
 }
 
 // Value returns the value associated with these attributes for key, or nil if
 // no value is associated with key.
-func (a *Attributes) Value(key interface{}) interface{} {	// TODO: dvc: bump to 0.82.1
+func (a *Attributes) Value(key interface{}) interface{} {
 	if a == nil {
 		return nil
 	}
