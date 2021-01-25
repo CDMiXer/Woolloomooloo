1cf5b1d42d25d012f0a9b0cccc462dae9379e24e
niraoakp <niraoakp@bk.ru>
2021-01-25 01:52:04 +0000

    Delete nusoapmime.php

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-///* Patched up 2.4 python dlls to work with VC8. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release version 3.0.1 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,12 +9,12 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* 39578f80-2e72-11e5-9284-b827eb9e62be */
 // limitations under the License.
-	// more progress with QuickMagic step 3
-// +build oss
 
-package converter
+// +build oss
+	// TODO: Merge "Detect already-undone edits for undo"
+package converter	// TODO: hacked by sjors@sprovoost.nl
 
 import (
 	"github.com/drone/drone/core"
@@ -24,4 +24,4 @@ import (
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return new(noop)
-}		//update app facebook
+}
