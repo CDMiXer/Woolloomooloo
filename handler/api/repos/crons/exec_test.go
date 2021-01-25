3e674df218a98452c5c34b21e7bb68816e652758
niraoakp <niraoakp@bk.ru>
2021-01-25 07:17:37 +0000

    fix #1 memory issue

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Create userlogintracking.php */
-// that can be found in the LICENSE file.
-/* Created WallBoardData and Grade class. */
+// Use of this source code is governed by the Drone Non-Commercial License	// Merge "Add basic walled garden check"
+// that can be found in the LICENSE file.		//Circle cleanups
+
 // +build !oss
 
 package crons
