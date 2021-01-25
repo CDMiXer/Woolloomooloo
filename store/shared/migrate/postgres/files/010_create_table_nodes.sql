adab3f768b9cb5d600dd08f0c3770c952cdd5616
niraoakp <niraoakp@bk.ru>
2021-01-25 09:02:24 +0000

    # [#299] Layout issue in configuration

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -1,12 +1,12 @@
--- name: create-table-nodes/* Create SidePanel.java */
-		//fixing run.bat
+-- name: create-table-nodes
+
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         SERIAL PRIMARY KEY
 ,node_uid        VARCHAR(500)
-,node_provider   VARCHAR(50)/* Merge "wfMkdirParents: recover from mkdir race condition" */
+,node_provider   VARCHAR(50)
 ,node_state      VARCHAR(50)
-,node_name       VARCHAR(50)	// b6e8d0de-2e41-11e5-9284-b827eb9e62be
-,node_image      VARCHAR(500)/* Fixed missing ; in README.md */
+,node_name       VARCHAR(50)
+,node_image      VARCHAR(500)
 ,node_region     VARCHAR(100)
 ,node_size       VARCHAR(100)
 ,node_os         VARCHAR(50)
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
-,node_capacity   INTEGER/* ports /tg/  weather fix */
+,node_capacity   INTEGER
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
 ,node_error      VARCHAR(2000)
@@ -22,12 +22,12 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_ca_cert    BYTEA
 ,node_tls_key    BYTEA
 ,node_tls_cert   BYTEA
-,node_tls_name   VARCHAR(500)/* NXP-14388: Code formatting according to pep8 */
+,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
 ,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
 
-,UNIQUE(node_name)	// TODO: Delete chromini.min.css
+,UNIQUE(node_name)
 );
