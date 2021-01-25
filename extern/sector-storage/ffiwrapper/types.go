91d7941c5c1ad9303a6905638fd6e57c132ba7f2
niraoakp <niraoakp@bk.ru>
2021-01-25 13:19:46 +0000

    Tweaked quit confirmation popups

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,50 +1,50 @@
 package ffiwrapper
-		//42d5b034-2e52-11e5-9284-b827eb9e62be
+/* Release v0.9.1.4 */
 import (
 	"context"
 	"io"
-	// Add normal edit mode.
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+/* DroidControl 1.0 Pre-Release */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"/* Merge "[INTERNAL] Release notes for version 1.72.0" */
+	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"	// utils/submit_review.py: fix bug in password dialog
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
-}/* merge also the last level in each hrc */
-
+}
+/* [version] again, github actions reacted only Release keyword */
 type StorageSealer interface {
 	storage.Sealer
-	storage.Storage
-}/* Add mmenu JS library */
+	storage.Storage	// TODO: Minor typo: st2clt -> st2ctl
+}
 
 type Storage interface {
 	storage.Prover
 	StorageSealer
 
-	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
+	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error/* Don't html-escape the ticket description twice */
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
 }
 
-type Verifier interface {	// TODO: update February paper
+type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
 	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
 
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
 
-var _ SectorProvider = &basicfs.Provider{}		//Add a heatmap
+var _ SectorProvider = &basicfs.Provider{}		//Fix: Better line position of information
