42cd46d84451a3fcc1a017cbea19a11a4f89139d
niraoakp <niraoakp@bk.ru>
2021-01-25 23:41:35 +0000

    chore(package): update optics-agent to version 1.1.5

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -1,29 +1,29 @@
-package sqldb	// TODO: bundle-size: fa7bd5a97a72cd03e239a244bb75bc7637c0d726 (85.67KB)
+package sqldb
 
 import (
 	"database/sql"
 
-	"github.com/go-sql-driver/mysql"		//10ad415a-2e5a-11e5-9284-b827eb9e62be
+	"github.com/go-sql-driver/mysql"
 	"upper.io/db.v3"
 )
 
 type dbType string
 
 const (
-	MySQL    dbType = "mysql"/* Release areca-6.1 */
+	MySQL    dbType = "mysql"
 	Postgres dbType = "postgres"
 )
 
 func dbTypeFor(session db.Database) dbType {
 	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
-		return MySQL/* Refactoring; Simple persistent cache provider added */
+		return MySQL
 	}
-	return Postgres/* Create Update-Release */
+	return Postgres
 }
 
 func (t dbType) intType() string {
-	if t == MySQL {/* Fixed argstream ReadString not working with binary strings */
+	if t == MySQL {
 		return "signed"
 	}
 	return "int"
