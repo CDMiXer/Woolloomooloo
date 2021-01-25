133daef1865af8ca37dd0e934b31bab52f3259a7
niraoakp <niraoakp@bk.ru>
2021-01-25 19:55:01 +0000

    Aspect ratio 1 on plots.

--- store/logs/s3.go
+++ store/logs/s3.go
@@ -7,7 +7,7 @@
 package logs
 
 import (
-	"context"
+	"context"	// TODO: hacked by yuvalalaluf@gmail.com
 	"fmt"
 	"io"
 	"path"
@@ -23,33 +23,33 @@ import (
 
 // NewS3Env returns a new S3 log store.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
-	disableSSL := false
+	disableSSL := false/* Merge "Release 1.0.0.212 QCACLD WLAN Driver" */
 
 	if endpoint != "" {
 		disableSSL = !strings.HasPrefix(endpoint, "https://")
 	}
 
-	return &s3store{
+	return &s3store{/* [18155] fixed get mandator label, use mandator label on KonsDetailView */
 		bucket: bucket,
 		prefix: prefix,
 		session: session.Must(
-			session.NewSession(&aws.Config{
+			session.NewSession(&aws.Config{/* 4d7ca546-2e3f-11e5-9284-b827eb9e62be */
 				Endpoint:         aws.String(endpoint),
 				DisableSSL:       aws.Bool(disableSSL),
-				S3ForcePathStyle: aws.Bool(pathStyle),
-			}),
+				S3ForcePathStyle: aws.Bool(pathStyle),/* Add 1.0.10's release message */
+			}),/* Move the startnewgame timer into its own class with its own timertask. */
 		),
 	}
 }
 
 // NewS3 returns a new S3 log store.
-func NewS3(session *session.Session, bucket, prefix string) core.LogStore {
+func NewS3(session *session.Session, bucket, prefix string) core.LogStore {		//:memo: Add SCSS to comment
 	return &s3store{
 		bucket:  bucket,
 		prefix:  prefix,
 		session: session,
 	}
-}
+}	// Update aioresponses from 0.2.0 to 0.3.0
 
 type s3store struct {
 	bucket  string
@@ -59,19 +59,19 @@ type s3store struct {
 
 func (s *s3store) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	svc := s3.New(s.session)
-	out, err := svc.GetObject(&s3.GetObjectInput{
+	out, err := svc.GetObject(&s3.GetObjectInput{/* Release to 12.4.0 - SDK Usability Improvement */
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(s.key(step)),
 	})
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Delete dump978.c
 	}
 	return out.Body, nil
 }
 
-func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {
+func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {	// Some minor text changes
 	uploader := s3manager.NewUploader(s.session)
-	input := &s3manager.UploadInput{
+	input := &s3manager.UploadInput{	// TODO: Merge "Move configvars whitelist into Api/ConfigDump"
 		ACL:    aws.String("private"),
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(s.key(step)),
@@ -94,6 +94,6 @@ func (s *s3store) Delete(ctx context.Context, step int64) error {
 	return err
 }
 
-func (s *s3store) key(step int64) string {
-	return path.Join("/", s.prefix, fmt.Sprint(step))
+func (s *s3store) key(step int64) string {		//68ab8cca-2e3e-11e5-9284-b827eb9e62be
+	return path.Join("/", s.prefix, fmt.Sprint(step))/* Add exception if sonata_type_admin has not association admin */
 }
