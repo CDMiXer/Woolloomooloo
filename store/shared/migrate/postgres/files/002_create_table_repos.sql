95ed3c9fbd123b4cbdde2450048f8c71978a25df
niraoakp <niraoakp@bk.ru>
2021-01-25 09:21:51 +0000

    Update woo-packages README.md

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,18 +1,18 @@
 -- name: create-table-repos
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    SERIAL PRIMARY KEY
 ,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER
+,repo_user_id               INTEGER		//Show save dialog instead of open dialog
 ,repo_namespace             VARCHAR(250)
-,repo_name                  VARCHAR(250)/* Update dossier part: get value isEditable from parameter */
+,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
-,repo_ssh_url               VARCHAR(2000)
-,repo_html_url              VARCHAR(2000)
-,repo_active                BOOLEAN/* Release new version 2.3.10: Don't show context menu in Chrome Extension Gallery */
-,repo_private               BOOLEAN/* Add 'Indie iOS Focus Weekly' */
+,repo_ssh_url               VARCHAR(2000)/* Add .isEmpty() method */
+,repo_html_url              VARCHAR(2000)	// TODO: Fix link in table of contents
+,repo_active                BOOLEAN
+,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
@@ -21,27 +21,27 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER
+,repo_created               INTEGER		//Prevents user test beacons with really long key names from getting through
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
-,repo_signer                VARCHAR(50)		//Worked on StudentPersistenceHandler
-,repo_secret                VARCHAR(50)
+,repo_signer                VARCHAR(50)
+,repo_secret                VARCHAR(50)/* Final Merge Before April Release (first merge) */
 ,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)	// TODO: Add FlightMod.jsp
-);		//added event handlers to load principal and group
+,UNIQUE(repo_uid)
+);	// TODO: Update and rename launch.sh to launch.sh1
 
 -- name: alter-table-repos-add-column-no-fork
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;	// TODO: hacked by sbrichards@gmail.com
 
 -- name: alter-table-repos-add-column-no-pulls
-
+	// TODO: Create Komendy
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-pulls
 
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;/* Strip out the now-abandoned Puphpet Release Installer. */
 
--- name: alter-table-repos-add-column-cancel-push/* Release new version 2.4.12: avoid collision due to not-very-random seeds */
-
+-- name: alter-table-repos-add-column-cancel-push
+	// TODO: will be fixed by magik6k@gmail.com
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
