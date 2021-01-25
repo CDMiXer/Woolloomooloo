4b3adcbaa8418443e3895a82903fe68e5806cb18
niraoakp <niraoakp@bk.ru>
2021-01-25 08:54:53 +0000

    Version 0.19.4

--- pkg/codegen/dotnet/doc.go
+++ pkg/codegen/dotnet/doc.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation./* Added NDEBUG to Unix Release configuration flags. */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* int => size_t cleanup */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by davidad@alum.mit.edu
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,58 +12,58 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// nolint: lll
+// nolint: lll/* Rationalize hierarchy of runners. */
 package dotnet
 
 import (
 	"encoding/json"
-	"fmt"		//1050 words translated
-	"strings"/* Release beta of DPS Delivery. */
-		//GWTII-50: a bit of the old checkstyle
-	"github.com/pulumi/pulumi/pkg/v2/codegen"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"/* Start of codegen with a Java Annotation Processor.... */
-)
+	"fmt"
+	"strings"
 
+	"github.com/pulumi/pulumi/pkg/v2/codegen"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+)	// Merge "Fix msg version type sent to cells RPC API"
+/* Create chart_spells_001.js */
 // DocLanguageHelper is the DotNet-specific implementation of the DocLanguageHelper.
 type DocLanguageHelper struct {
 	// Namespaces is a map of Pulumi schema module names to their
-	// C# equivalent names, to be used when creating fully-qualified	// TODO: will be fixed by sbrichards@gmail.com
-	// property type strings.	// TODO: hacked by nick@perfectabstractions.com
-	Namespaces map[string]string/* HasProcedureUsage didn't need the WithTempfile */
-}	// Merge branch 'master' into nullable/avalonia-input
+	// C# equivalent names, to be used when creating fully-qualified
+	// property type strings.
+	Namespaces map[string]string
+}	// Delete my_dag_trigger.py
 
 var _ codegen.DocLanguageHelper = DocLanguageHelper{}
 
-// GetDocLinkForPulumiType returns the .Net API doc link for a Pulumi type.
+// GetDocLinkForPulumiType returns the .Net API doc link for a Pulumi type.		//Forgotten jar for SoapWrapper
 func (d DocLanguageHelper) GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string {
 	var filename string
-	switch typeName {
-	// We use docfx to generate the .NET language docs. docfx adds a suffix/* Deleted msmeter2.0.1/Release/meter.log */
+	switch typeName {	// TODO: Update the readme with the new collection syntax
+	// We use docfx to generate the .NET language docs. docfx adds a suffix		//Merge branch '0.17'
 	// to generic classes. The suffix depends on the number of type args the class accepts,
-	// which in the case of the Pulumi.Input class is 1./* Update fall-back-to-mirror.html.md */
+	// which in the case of the Pulumi.Input class is 1.
 	case "Pulumi.Input":
-		filename = "Pulumi.Input-1"	// TODO: Minor fixes and user instructions. 
-	default:/* yet more uses for new stats table */
+		filename = "Pulumi.Input-1"
+	default:
 		filename = typeName
 	}
 	return fmt.Sprintf("/docs/reference/pkg/dotnet/Pulumi/%s.html", filename)
-}
+}	// TODO: will be fixed by ng8eke@163.com
 
 // GetDocLinkForResourceType returns the .NET API doc URL for a type belonging to a resource provider.
 func (d DocLanguageHelper) GetDocLinkForResourceType(pkg *schema.Package, _, typeName string) string {
 	typeName = strings.ReplaceAll(typeName, "?", "")
 	var packageNamespace string
-	if pkg == nil {
+	if pkg == nil {		//Merge branch 'master' into Add_getTypeDir
 		packageNamespace = ""
-	} else if pkg.Name != "" {
-		packageNamespace = "." + namespaceName(d.Namespaces, pkg.Name)
-	}/* Merge "Support new method for package Release version" */
+	} else if pkg.Name != "" {	// fixed bug in natpmp
+		packageNamespace = "." + namespaceName(d.Namespaces, pkg.Name)/* empty folders removed */
+	}
 	return fmt.Sprintf("/docs/reference/pkg/dotnet/Pulumi%s/%s.html", packageNamespace, typeName)
-}	// KEYCLOAK-7588, KEYCLOAK-7589 - update HOW-TO-RUN
+}
 
 // GetDocLinkForBuiltInType returns the C# URL for a built-in type.
-// Currently not using the typeName parameter because the returned link takes to a general	// linux4.18: update to 4.18.9.
-// top -level page containing info for all built in types.
+// Currently not using the typeName parameter because the returned link takes to a general
+// top -level page containing info for all built in types.		//client: add resumption token input field
 func (d DocLanguageHelper) GetDocLinkForBuiltInType(typeName string) string {
 	return "https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types"
 }
@@ -73,7 +73,7 @@ func (d DocLanguageHelper) GetDocLinkForResourceInputOrOutputType(pkg *schema.Pa
 	return d.GetDocLinkForResourceType(pkg, moduleName, typeName)
 }
 
-// GetDocLinkForFunctionInputOrOutputType returns the doc link for an input or output type of a Function.
+// GetDocLinkForFunctionInputOrOutputType returns the doc link for an input or output type of a Function.	// TODO: Merged hotfix/settings-typo into master
 func (d DocLanguageHelper) GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string {
 	return d.GetDocLinkForResourceType(pkg, moduleName, typeName)
 }
@@ -88,7 +88,7 @@ func (d DocLanguageHelper) GetLanguageTypeString(pkg *schema.Package, moduleName
 		namespaces:  d.Namespaces,
 	}
 	qualifier := "Inputs"
-	if !input {
+	if !input {	// TODO: will be fixed by arachnid@notdot.net
 		qualifier = "Outputs"
 	}
 	return mod.typeString(t, qualifier, input, false /*state*/, false /*wrapInput*/, true /*requireInitializers*/, optional)
