5471862ac7a4755d2fdb92492c51a8620f941bab
niraoakp <niraoakp@bk.ru>
2021-01-25 23:08:18 +0000

    added all commands stub for basic language workbench definition

--- tests/integration/empty/gorun_main/gorun_main_src/main.go
+++ tests/integration/empty/gorun_main/gorun_main_src/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* Release of Prestashop Module 1.2.0 */
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+		//Create firewall-policies.csv
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Included commands for unzip based on the Github download archive. */
 		return nil
 	})
 }
