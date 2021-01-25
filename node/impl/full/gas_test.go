18d955ec8223181ea371d7999a07fd1151b5a7e6
niraoakp <niraoakp@bk.ru>
2021-01-25 01:02:13 +0000

    Create twitter.pl

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,9 +1,9 @@
 package full
 
-import (
-	"testing"
+import (	// Cardridge -> Cartridge
+	"testing"		//Change the method name from done() to finish() and update some other methods.
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"		//Create read_post.php
 
 	"github.com/filecoin-project/go-state-types/big"
 
@@ -13,13 +13,13 @@ import (
 
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
-		{big.NewInt(5), build.BlockGasTarget},
+		{big.NewInt(5), build.BlockGasTarget},/* Merge branch 'master' into fl-fixes */
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{		//Fix the bug on the value type of new added point.
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
-	}, 1))
+	}, 1))	// TODO: Merge "Add /cgit rewrite for git server docroot"
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
@@ -29,7 +29,7 @@ func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},/* Delete issue_0502_v2.html */
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
