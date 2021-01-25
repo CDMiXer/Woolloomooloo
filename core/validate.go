b6b74b30a24f2c81817f69bd6ee8bdaf22236ce9
niraoakp <niraoakp@bk.ru>
2021-01-25 03:40:54 +0000

    Merge "usb: gadget: rndis: Limit DL aggregation to 5 packets"

--- core/validate.go
+++ core/validate.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* add zone.js */
-// you may not use this file except in compliance with the License./* Makefile generator: support Release builds; include build type in output dir. */
-// You may obtain a copy of the License at/* Merge branch 'nfc-example-app' into master */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release v0.5.0.5 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-		//Create ef6-query-filter-by-instance.md
+
 import (
 	"context"
-	"errors"/* updated to properly show invite info after delete */
-)/* Release 2.0 preparation, javadoc, copyright, apache-2 license */
+	"errors"
+)
 
 var (
-	// ErrValidatorSkip is returned if the pipeline/* Update thai_time.py */
-	// validation fails, but the pipeline should be skipped/* Release version 3.6.2.2 */
+	// ErrValidatorSkip is returned if the pipeline
+	// validation fails, but the pipeline should be skipped
 	// and silently ignored instead of erroring.
 	ErrValidatorSkip = errors.New("validation failed: skip pipeline")
 
@@ -31,7 +31,7 @@ var (
 	ErrValidatorBlock = errors.New("validation failed: block pipeline")
 )
 
-type (		//Trim tagline
+type (
 	// ValidateArgs represents a request to the pipeline
 	// validation service.
 	ValidateArgs struct {
