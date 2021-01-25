31fc6ff748b1f11c20bae36fd08c1471dacbffd7
niraoakp <niraoakp@bk.ru>
2021-01-25 10:21:55 +0000

    moving parser factory to create caseinsensitive literals instead of literals

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// 810266ac-2e40-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Create 89-system.conf
-/* Merge "Fix quota usages update deleting same share from several API endpoints" */
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package collabs
-/* Release of eeacms/eprtr-frontend:0.4-beta.9 */
+
 import (
 	"context"
 	"encoding/json"
@@ -13,18 +13,18 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"		//Clarified licensing conditions.
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Release 1.9 Code Commit. */
+)
 
 func TestDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Błędy ortograficzne i brak znacznika zamykającego
+
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
@@ -32,19 +32,19 @@ func TestDelete(t *testing.T) {
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
 	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
-	// New post: The Rebirth of the Polymath
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// error handled (Fixes #1)
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
-	r = r.WithContext(	// TODO: Feature #903: add menu options
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(users, repos, members)(w, r)/* Cute Theme added */
+	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -63,13 +63,13 @@ func TestDelete_UserNotFound(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("member", "octocat")		//Add actual rule calculation.
-	// TODO: Add underline macro spec.
-	w := httptest.NewRecorder()/* Many improvements to the King Derby driver. */
+	c.URLParams.Add("member", "octocat")
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
-	r = r.WithContext(	// TODO: hacked by fjl@ethereum.org
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* [FIX] Patch from the debian repository */
+	)
 
 	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
