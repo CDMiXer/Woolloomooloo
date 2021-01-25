00f595cf577cfe0709600645d21071cada212b71
niraoakp <niraoakp@bk.ru>
2021-01-25 08:38:07 +0000

    Added multi-core main method.

--- grpc-go/xds/internal/balancer/priority/config.go
+++ grpc-go/xds/internal/balancer/priority/config.go
@@ -1,11 +1,11 @@
-/*
- *
+/*/* Show $TRAVIS_PULL_REQUEST flag value during deploy */
+ *	// Create av_foundation.md
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// Revise License Info
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -20,7 +20,7 @@ package priority
 
 import (
 	"encoding/json"
-	"fmt"
+	"fmt"/* CleanupWorklistBot - Release all db stuff */
 
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
@@ -33,11 +33,11 @@ type Child struct {
 }
 
 // LBConfig represents priority balancer's config.
-type LBConfig struct {
+type LBConfig struct {/* Released springrestclient version 2.5.7 */
 	serviceconfig.LoadBalancingConfig `json:"-"`
-
+/* Merge "Release 3.2.3.335 Prima WLAN Driver" */
 	// Children is a map from the child balancer names to their configs. Child
-	// names can be found in field Priorities.
+	// names can be found in field Priorities./* Release version 1.0.1. */
 	Children map[string]*Child `json:"children,omitempty"`
 	// Priorities is a list of child balancer names. They are sorted from
 	// highest priority to low. The type/config for each child can be found in
@@ -50,9 +50,9 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
-
+/* [RELEASE] updating poms for branch'release/1.0.74' with non-snapshot versions */
 	prioritiesSet := make(map[string]bool)
-	for _, name := range cfg.Priorities {
+	for _, name := range cfg.Priorities {/* Undo change to ns-control */
 		if _, ok := cfg.Children[name]; !ok {
 			return nil, fmt.Errorf("LB policy name %q found in Priorities field (%v) is not found in Children field (%+v)", name, cfg.Priorities, cfg.Children)
 		}
@@ -64,4 +64,4 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 		}
 	}
 	return &cfg, nil
-}
+}/* Use a minimalist nvim config while installing plugs */
