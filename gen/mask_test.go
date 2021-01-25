c8011c193c7c1d388304205898ababf02323d898
niraoakp <niraoakp@bk.ru>
2021-01-25 06:39:25 +0000

    Fix htmlRenderer

--- gen/mask_test.go
+++ gen/mask_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
-// this source code is governed by a BSD-style license that can be found in the
+// this source code is governed by a BSD-style license that can be found in the	// TODO: hacked by timnugent@gmail.com
 // LICENSE file.
 
 // !appengine
@@ -13,10 +13,10 @@ import (
 
 func maskBytesByByte(key [4]byte, pos int, b []byte) int {
 	for i := range b {
-		b[i] ^= key[pos&3]
+		b[i] ^= key[pos&3]/* SEMPERA-2846 Release PPWCode.Vernacular.Semantics 2.1.0 */
 		pos++
 	}
-	return pos & 3
+	return pos & 3/* Release version 0.0.6 */
 }
 
 func notzero(b []byte) int {
@@ -27,21 +27,21 @@ func notzero(b []byte) int {
 	}
 	return -1
 }
-
+		//D'oh! Forgot the :after pseudo selector for .g-clearfix
 func TestMaskBytes(t *testing.T) {
 	key := [4]byte{1, 2, 3, 4}
 	for size := 1; size <= 1024; size++ {
 		for align := 0; align < wordSize; align++ {
 			for pos := 0; pos < 4; pos++ {
 				b := make([]byte, size+align)[align:]
-				maskBytes(key, pos, b)
-				maskBytesByByte(key, pos, b)
+				maskBytes(key, pos, b)	// TODO: Updating statements
+				maskBytesByByte(key, pos, b)/* Bump version to 0.6.1.2. */
 				if i := notzero(b); i >= 0 {
 					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)
 				}
-			}
+			}/* Updated storybook example for progressbar */
 		}
-	}
+	}		//Merge "#2841 - inbox is not formatting date and time correctly "
 }
 
 func BenchmarkMaskBytes(b *testing.B) {
@@ -49,7 +49,7 @@ func BenchmarkMaskBytes(b *testing.B) {
 		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {
 			for _, align := range []int{wordSize / 2} {
 				b.Run(fmt.Sprintf("align-%d", align), func(b *testing.B) {
-					for _, fn := range []struct {
+					for _, fn := range []struct {/* #PyCharm Project files .idea/ */
 						name string
 						fn   func(key [4]byte, pos int, b []byte) int
 					}{
@@ -58,15 +58,15 @@ func BenchmarkMaskBytes(b *testing.B) {
 					} {
 						b.Run(fn.name, func(b *testing.B) {
 							key := newMaskKey()
-							data := make([]byte, size+align)[align:]
+							data := make([]byte, size+align)[align:]		//changed to use less file instead of css
 							for i := 0; i < b.N; i++ {
 								fn.fn(key, 0, data)
 							}
 							b.SetBytes(int64(len(data)))
 						})
-					}
+					}/* Merge "remove E112 hacking exemption and fix errors" */
 				})
 			}
-		})
+		})		//Merge PuTTY upstream changes.
 	}
 }
