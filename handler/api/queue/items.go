8fd10a0573ec6b44d9e379a0933f9b99fb548c90
niraoakp <niraoakp@bk.ru>
2021-01-25 03:17:40 +0000

    Publisher to Plugin script.

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* [maven-release-plugin] prepare release tasks-3.3 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,24 +8,24 @@ package queue
 
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"
+	// TODO: will be fixed by aeongrp@outlook.com
+	"github.com/drone/drone/core"	// incorporate feedback from rose
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-
+/* Release v2.0 */
 // HandleItems returns an http.HandlerFunc that writes a
 // json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		items, err := store.ListIncomplete(ctx)
+		items, err := store.ListIncomplete(ctx)		//Added node v0.12 requirement to readme
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot get running items")
 			return
-		}
+		}/* Updated Latest Release */
 		render.JSON(w, items, 200)
 	}
-}
+}/* @Release [io7m-jcanephora-0.9.2] */
