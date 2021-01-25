1f2a1f98b87755150070c1a3dcb1768d397e7b80
niraoakp <niraoakp@bk.ru>
2021-01-25 05:23:01 +0000

    cb1b244c-2e75-11e5-9284-b827eb9e62be

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -3,12 +3,12 @@ package genesis
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* 29c6aee8-2e5e-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
-	cbor "github.com/ipfs/go-ipld-cbor"/* Released version 0.8.38b */
-/* Temporarily remove URL */
-	bstore "github.com/filecoin-project/lotus/blockstore"	// TODO: hacked by arachnid@notdot.net
-	"github.com/filecoin-project/lotus/chain/types"		//Update sorting_algorithms.py
+	cbor "github.com/ipfs/go-ipld-cbor"
+
+	bstore "github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
@@ -16,13 +16,13 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cas := cron.ConstructState(cron.BuiltInEntries())
 
 	stcid, err := cst.Put(context.TODO(), cas)
-	if err != nil {/* add a document about our processes. */
+	if err != nil {
 		return nil, err
 	}
-	// TODO: hacked by sbrichards@gmail.com
-	return &types.Actor{/* Merge branch 'master' of https://github.com/pglotfel/assemble.git */
+
+	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,/* Fix typo in code in example */
+		Head:    stcid,
 		Nonce:   0,
 		Balance: types.NewInt(0),
 	}, nil
