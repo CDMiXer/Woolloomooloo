cab6c80d0afb6c90667292bf49e9a476e48b5c11
niraoakp <niraoakp@bk.ru>
2021-01-25 16:05:42 +0000

    allow manually sharing urls to subscribe activity

--- hack/tools.go
+++ hack/tools.go
@@ -1,31 +1,31 @@
 // +build tools
 
 // This package contains code generation utilities
-// This package imports things required by build scripts, to force `go mod` to see them as dependencies
-package tools
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies/* Release 0.66 */
+package tools	// que editor no pueda publicar y despublicar.
 
 import (
 	_ "bou.ke/staticfiles"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/gogo/protobuf/gogoproto"
-	_ "github.com/gogo/protobuf/protoc-gen-gogo"
+	_ "github.com/gogo/protobuf/protoc-gen-gogo"/* Release gdx-freetype for gwt :) */
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"/* Release version: 1.0.7 */
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"	// TODO: Update test dependencies
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "k8s.io/code-generator"		//f40221a2-2e3f-11e5-9284-b827eb9e62be
+	_ "golang.org/x/tools/cmd/goimports"/* 5c64610a-2e55-11e5-9284-b827eb9e62be */
+	_ "k8s.io/code-generator"/* Release version 2.3.2. */
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"		//reconnect pooling when disconnected
+	_ "k8s.io/code-generator/cmd/defaulter-gen"/* Release of eeacms/forests-frontend:1.8-beta.11 */
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
-	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "k8s.io/code-generator/cmd/lister-gen"/* Server-side bugfix on keystore load. */
 	_ "k8s.io/code-generator/cmd/openapi-gen"
 	_ "k8s.io/code-generator/cmd/register-gen"
 	_ "k8s.io/code-generator/cmd/set-gen"
-	_ "k8s.io/kube-openapi/cmd/openapi-gen"/* Test that CharArraySequence functions as expected */
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
