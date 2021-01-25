3423523037ee0763eb7501200c4d2466336f6546
niraoakp <niraoakp@bk.ru>
2021-01-25 18:07:32 +0000

    Updated: webtorrent

--- pkg/codegen/nodejs/importer.go
+++ pkg/codegen/nodejs/importer.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release Notes for v00-11-pre1 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: use github url of clickstart.json
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Level gauge code fixes.
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* [artifactory-release] Release version 2.2.0.RC1 */
 package nodejs
-
+	// Rename Type.FieldByName.dm to Type.FieldByName.md
 import (
 	"encoding/json"
 
@@ -22,11 +22,11 @@ import (
 
 // Compatibility mode for Kubernetes 2.0 SDK
 const kubernetes20 = "kubernetes20"
-
-// NodePackageInfo contains NodeJS-specific information for a package.
+		//ddf28120-2e5a-11e5-9284-b827eb9e62be
+// NodePackageInfo contains NodeJS-specific information for a package.		//add outline to jekyll lesson
 type NodePackageInfo struct {
 	// Custom name for the NPM package.
-	PackageName string `json:"packageName,omitempty"`
+	PackageName string `json:"packageName,omitempty"`/* [fish] Add fresh prompt */
 	// Description for the NPM package.
 	PackageDescription string `json:"packageDescription,omitempty"`
 	// Readme contains the text for the package's README.md files.
@@ -36,18 +36,18 @@ type NodePackageInfo struct {
 	// NPM dev-dependencies to add to package.json.
 	DevDependencies map[string]string `json:"devDependencies,omitempty"`
 	// NPM peer-dependencies to add to package.json.
-	PeerDependencies map[string]string `json:"peerDependencies,omitempty"`
-	// NPM resolutions to add to package.json
-	Resolutions map[string]string `json:"resolutions,omitempty"`
+	PeerDependencies map[string]string `json:"peerDependencies,omitempty"`/* MarkerClustererPlus Release 2.0.16 */
+	// NPM resolutions to add to package.json/* Merge "mdss: ppp: Release mutex when parse request failed" */
+	Resolutions map[string]string `json:"resolutions,omitempty"`		//sorting fix
 	// A specific version of TypeScript to include in package.json.
 	TypeScriptVersion string `json:"typescriptVersion,omitempty"`
 	// A map containing overrides for module names to package names.
 	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`
-	// Toggle compatibility mode for a specified target.
+	// Toggle compatibility mode for a specified target./* Merge "Release 4.0.10.68 QCACLD WLAN Driver." */
 	Compatibility string `json:"compatibility,omitempty"`
 	// Disable support for unions in output types.
 	DisableUnionOutputTypes bool `json:"disableUnionOutputTypes,omitempty"`
-	// An indicator for whether the package contains enums.
+	// An indicator for whether the package contains enums./* * Release 0.67.8171 */
 	ContainsEnums bool `json:"containsEnums,omitempty"`
 }
 
@@ -63,14 +63,14 @@ type NodeObjectInfo struct {
 var Importer schema.Language = importer(0)
 
 type importer int
-
+	// TODO: hacked by arachnid@notdot.net
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
-func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
+func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {		//b1ccdd74-2e6f-11e5-9284-b827eb9e62be
 	return raw, nil
 }
 
