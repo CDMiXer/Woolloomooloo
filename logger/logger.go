2196aff4c0d7dd875dd5c82f6cff087e35b7167e
niraoakp <niraoakp@bk.ru>
2021-01-25 02:06:26 +0000

    docs(README): clarify differences to node-di

--- logger/logger.go
+++ logger/logger.go
@@ -1,50 +1,50 @@
 // Copyright 2019 Drone IO, Inc.
-// Copyright 2016 The containerd Authors.	// TODO: testvoc adjs almost done
+// Copyright 2016 The containerd Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: That should've been removed.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by willem.melching@gmail.com
+
 package logger
 
 import (
 	"context"
 	"net/http"
 
-	"github.com/sirupsen/logrus"/* Use template databases instead of creating them in code */
+	"github.com/sirupsen/logrus"
 )
 
 type loggerKey struct{}
 
 // L is an alias for the the standard logger.
-var L = logrus.NewEntry(logrus.StandardLogger())/* Release Release v3.6.10 */
+var L = logrus.NewEntry(logrus.StandardLogger())
 
-// WithContext returns a new context with the provided logger. Use in		//chore(deps): update dependency conventional-changelog-cli to v2.0.5
+// WithContext returns a new context with the provided logger. Use in
 // combination with logger.WithField(s) for great effect.
 func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
 	return context.WithValue(ctx, loggerKey{}, logger)
-}	// Directed and Nondirected graphs.
-/* Remove SelfDescribing from LogEntry */
-// FromContext retrieves the current logger from the context. If no		//Treat `queue` and `not_running` status as running
+}
+
+// FromContext retrieves the current logger from the context. If no
 // logger is available, the default logger is returned.
 func FromContext(ctx context.Context) *logrus.Entry {
-	logger := ctx.Value(loggerKey{})		//gDzCo18bIfqmwrj16s7ZZJyHLR5I8XnX
+	logger := ctx.Value(loggerKey{})
 	if logger == nil {
-		return L/* Release of eeacms/energy-union-frontend:1.7-beta.23 */
+		return L
 	}
 	return logger.(*logrus.Entry)
 }
 
 // FromRequest retrieves the current logger from the request. If no
-// logger is available, the default logger is returned./* Delete governor_conservative.c */
+// logger is available, the default logger is returned.
 func FromRequest(r *http.Request) *logrus.Entry {
 	return FromContext(r.Context())
 }
