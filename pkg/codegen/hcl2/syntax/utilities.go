8293259dc988e9119f996822d90c8d705a9bf3fc
niraoakp <niraoakp@bk.ru>
2021-01-25 14:44:28 +0000

    Create SkypeStatus.php

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
-/* Release: Making ready for next release iteration 5.4.1 */
-import "github.com/hashicorp/hcl/v2/hclsyntax"/* Add contrib.storage.linux + tests. */
+		//e56dce8c-2e46-11e5-9284-b827eb9e62be
+import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.	// 4ee155e0-2e66-11e5-9284-b827eb9e62be
 var None hclsyntax.Node = &hclsyntax.Body{}
