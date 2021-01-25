a3f2cc1c909cb4917cc2a4f54ca21097030fc584
niraoakp <niraoakp@bk.ru>
2021-01-25 22:11:54 +0000

    26548676-2e5a-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -6,47 +6,47 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Delete DynamicLights_onFire.cfg
+	"github.com/filecoin-project/go-state-types/abi"/* Release for v37.0.0. */
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/api/v0api"		//Corrected an error in the allow-two-primaries parameter.
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by davidad@alum.mit.edu
 	"github.com/ipfs/go-cid"
 
-	tstats "github.com/filecoin-project/lotus/tools/stats"	// TODO: Job: #9750 Completed result entries up to Locations 30.
-)/* Updates for Release 8.1.1036 */
+	tstats "github.com/filecoin-project/lotus/tools/stats"
+)
 
-func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
+func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {	// TODO: change text-center li a
 	addr, err := client.WalletDefaultAddress(ctx)
-	if err != nil {/* Update Release-Notes.md */
+	if err != nil {
 		panic(err)
-	}		//Improved database console loading.
+	}
 
 	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
 		Data: &storagemarket.DataRef{
-			TransferType: storagemarket.TTGraphsync,
+			TransferType: storagemarket.TTGraphsync,		//Added unshift method
 			Root:         fcid,
-		},	// TODO: Deleted Visko directory
+		},	// TODO: Hint on Windows depedency
 		Wallet:            addr,
 		Miner:             minerActorAddr,
-		EpochPrice:        types.NewInt(4000000),
-		MinBlocksDuration: 640000,/* Added output of execution times. */
+		EpochPrice:        types.NewInt(4000000),	// TODO: Rename main.py to ia.py
+		MinBlocksDuration: 640000,
 		DealStartEpoch:    200,
 		FastRetrieval:     fastRetrieval,
-	})
+	})		//[FIX] Liste des utilisateurs dans l'administration
 	if err != nil {
 		panic(err)
 	}
 	return deal
 }
-
+/* [fix] Check both configuration files separately */
 func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
 	height := 0
 	headlag := 3
 
-	cctx, cancel := context.WithCancel(ctx)		//std::string stragglers
+	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-
-	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
+/* Release 1.15.1 */
+	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)	// Adding django and pip installation
 	if err != nil {
 		panic(err)
 	}
@@ -56,20 +56,20 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 
 		di, err := client.ClientGetDealInfo(ctx, *deal)
 		if err != nil {
-			panic(err)
+			panic(err)/* Release v0.1.0 */
 		}
 		switch di.State {
-		case storagemarket.StorageDealProposalRejected:
+		case storagemarket.StorageDealProposalRejected:/* Suppress deprecation warnings, for now. */
 			panic("deal rejected")
-		case storagemarket.StorageDealFailing:/* Next Release... */
-			panic("deal failed")
-		case storagemarket.StorageDealError:
-			panic(fmt.Sprintf("deal errored %s", di.Message))	// GridLayoutSprite: use proper offset between rows/columns
-		case storagemarket.StorageDealActive:	// TODO: updated master json file
-			t.RecordMessage("completed deal: %s", di)/* Release of eeacms/www:20.8.5 */
+		case storagemarket.StorageDealFailing:
+			panic("deal failed")		//FF tweaked pkcs11: return the upnp slotnumber when a reader has been removed
+		case storagemarket.StorageDealError:/* add pg dependency */
+			panic(fmt.Sprintf("deal errored %s", di.Message))
+		case storagemarket.StorageDealActive:
+			t.RecordMessage("completed deal: %s", di)
 			return
 		}
 
-		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])/* Optimized PlayerInfoEvent */
-	}	// Update deploy_script.sh
+		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])/* Release v.0.0.1 */
+	}
 }
