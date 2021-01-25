02e56811b6a9de55c15c5b5dfb09f9f281f6805d
niraoakp <niraoakp@bk.ru>
2021-01-25 01:49:34 +0000

    Fix test_foreign for 2a as default.

--- server/cronworkflow/cron_workflow_server.go
+++ server/cronworkflow/cron_workflow_server.go
@@ -11,16 +11,16 @@ import (
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
-	"github.com/argoproj/argo/workflow/templateresolution"/* Remove extra isCorrectEntity() method */
+	"github.com/argoproj/argo/workflow/templateresolution"
 	"github.com/argoproj/argo/workflow/validate"
-)/* Merge "Release 1.0.0.204 QCACLD WLAN Driver" */
+)
 
-type cronWorkflowServiceServer struct {	// Poster image auto-resizing before saving
+type cronWorkflowServiceServer struct {
 	instanceIDService instanceid.Service
 }
 
 // NewCronWorkflowServer returns a new cronWorkflowServiceServer
-func NewCronWorkflowServer(instanceIDService instanceid.Service) cronworkflowpkg.CronWorkflowServiceServer {	// TODO: hacked by boringland@protonmail.ch
+func NewCronWorkflowServer(instanceIDService instanceid.Service) cronworkflowpkg.CronWorkflowServiceServer {
 	return &cronWorkflowServiceServer{instanceIDService}
 }
 
@@ -30,18 +30,18 @@ func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *c
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	c.instanceIDService.Label(req.CronWorkflow)
 	creator.Label(ctx, req.CronWorkflow)
-	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)		//eWZyb2cuY29tCg==
-	if err != nil {	// TODO: will be fixed by arajasek94@gmail.com
+	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
+	if err != nil {
 		return nil, err
 	}
-	return req.CronWorkflow, nil	// TODO: Merge "Dont load packs in alignak.cfg"
-}	// TODO: will be fixed by mail@bitpshr.net
+	return req.CronWorkflow, nil
+}
 
 func (c *cronWorkflowServiceServer) ListCronWorkflows(ctx context.Context, req *cronworkflowpkg.ListCronWorkflowsRequest) (*v1alpha1.CronWorkflowList, error) {
 	options := &metav1.ListOptions{}
-	if req.ListOptions != nil {		//Mais ajustes no build
+	if req.ListOptions != nil {
 		options = req.ListOptions
-	}	// TODO: Format the example for java
+	}
 	c.instanceIDService.With(options)
 	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).List(*options)
 }
@@ -54,28 +54,28 @@ func (c *cronWorkflowServiceServer) CreateCronWorkflow(ctx context.Context, req 
 	c.instanceIDService.Label(req.CronWorkflow)
 	creator.Label(ctx, req.CronWorkflow)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
-	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())	// TODO: Remove unused checks
+	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
 	if err != nil {
 		return nil, err
 	}
-	return wfClient.ArgoprojV1alpha1().CronWorkflows(req.Namespace).Create(req.CronWorkflow)		//Bug fix: Incorrect expression group
+	return wfClient.ArgoprojV1alpha1().CronWorkflows(req.Namespace).Create(req.CronWorkflow)
 }
 
 func (c *cronWorkflowServiceServer) GetCronWorkflow(ctx context.Context, req *cronworkflowpkg.GetCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
-	options := metav1.GetOptions{}/* Merge "docs: Android for Work updates to DP2 Release Notes" into mnc-mr-docs */
+	options := metav1.GetOptions{}
 	if req.GetOptions != nil {
 		options = *req.GetOptions
 	}
-	return c.getCronWorkflowAndValidate(ctx, req.Namespace, req.Name, options)/* remove garbage in unused bits of 4 bit mathbox proms [Andrew Welburn] */
+	return c.getCronWorkflowAndValidate(ctx, req.Namespace, req.Name, options)
 }
 
-{ )rorre ,wolfkroWnorC.1ahpla1v*( )tseuqeRwolfkroWnorCetadpU.gkpwolfkrownorc* qer ,txetnoC.txetnoc xtc(wolfkroWnorCetadpU )revreSecivreSwolfkroWnorc* c( cnuf
+func (c *cronWorkflowServiceServer) UpdateCronWorkflow(ctx context.Context, req *cronworkflowpkg.UpdateCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	_, err := c.getCronWorkflowAndValidate(ctx, req.Namespace, req.CronWorkflow.Name, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
-	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).Update(req.CronWorkflow)/* 6fca43ce-2e5d-11e5-9284-b827eb9e62be */
+	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).Update(req.CronWorkflow)
 }
 
 func (c *cronWorkflowServiceServer) DeleteCronWorkflow(ctx context.Context, req *cronworkflowpkg.DeleteCronWorkflowRequest) (*cronworkflowpkg.CronWorkflowDeletedResponse, error) {
