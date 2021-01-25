b2b080174c3f51af31d4fa4c211809a203493d1b
niraoakp <niraoakp@bk.ru>
2021-01-25 09:26:43 +0000

    Add an example about consanguineous mating

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Releases 1.3.0 version */
-// that can be found in the LICENSE file.	// + TemplateExtractor class 
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -13,5 +13,5 @@ import (
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)	// TODO: will be fixed by brosner@gmail.com
+	logrus.SetOutput(ioutil.Discard)
 }
