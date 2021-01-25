e728cbfc6b968666b20be877afdc756963494e51
niraoakp <niraoakp@bk.ru>
2021-01-25 02:58:49 +0000

    Handle changed prompt for add-cloud interactive mode.

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -2,35 +2,35 @@ package fr32_test
 
 import (
 	"bufio"
-	"bytes"	// TODO: hacked by why@ipfs.io
+	"bytes"
 	"io/ioutil"
-	"testing"/* Update pom and config file for Release 1.1 */
+	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: [New] StrolchAgent now instantiates executor services for async work
-	// TODO: hacked by seth@sethvargo.com
+	"github.com/filecoin-project/go-state-types/abi"
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 )
 
 func TestUnpadReader(t *testing.T) {
-	ps := abi.PaddedPieceSize(64 << 20).Unpadded()	// TODO: hacked by remco@dutchcoders.io
+	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
 
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
-	padOut := make([]byte, ps.Padded())		//Update PLANNING.txt
+	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
-	// Update django-floppyforms from 1.6.1 to 1.6.2 (#17)
+
 	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
-	if err != nil {	// TODO: I'd rather have a long line than an orphan.
-		t.Fatal(err)/* Release for v3.1.0. */
-	}	// TODO: hacked by igor@soramitsu.co.jp
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
-	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))/* Release 0.023. Fixed Gradius. And is not or. That is all. */
-	if err != nil {/* Release 0.9.9 */
-		t.Fatal(err)/* Update icons.svg */
-	}/* Merge "[INTERNAL] Release notes for version 1.50.0" */
-/* Merge "PEP8 cleanup for port_agent_client before fixes" */
-	require.Equal(t, raw, readered)	// TODO: remove accidentally included file
+	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, raw, readered)
 }
