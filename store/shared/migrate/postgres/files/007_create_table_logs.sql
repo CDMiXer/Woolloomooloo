cba73abb0745fde8e8528789470677b4c5b032d4
niraoakp <niraoakp@bk.ru>
2021-01-25 22:31:45 +0000

    removing that pesky launcher from desktop

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs
+-- name: create-table-logs/* Release 0.32.1 */
 
-CREATE TABLE IF NOT EXISTS logs (
+CREATE TABLE IF NOT EXISTS logs (	// TODO: will be fixed by martin2cai@hotmail.com
  log_id    SERIAL PRIMARY KEY
 ,log_data  BYTEA
 );
