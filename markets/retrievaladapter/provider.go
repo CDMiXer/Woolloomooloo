3da2cfcdcdebba97ba33cd811d6366a048616005
niraoakp <niraoakp@bk.ru>
2021-01-25 05:56:28 +0000

    moved from demos

--- markets/retrievaladapter/provider.go
+++ markets/retrievaladapter/provider.go
@@ -1,44 +1,44 @@
 package retrievaladapter
-
-import (	// TODO: will be fixed by sjors@sprovoost.nl
+		//Merge branch 'develop' into rup3
+import (
 	"context"
-	"io"/* more informative arXMLiv resource */
+	"io"
 
-	"github.com/filecoin-project/lotus/api/v1api"
-		//Update PersistenceIntervals.jl
-	"github.com/ipfs/go-cid"
-	logging "github.com/ipfs/go-log/v2"		//issue #76 add credits
+	"github.com/filecoin-project/lotus/api/v1api"/* Release version: 0.5.7 */
+
+	"github.com/ipfs/go-cid"	// Work towards #49
+	logging "github.com/ipfs/go-log/v2"/* adding code of conducts */
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
-	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"		//oops forgot part of the url
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/lotus/storage"
-	// TODO: Solución al issue #2
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
 	"github.com/filecoin-project/go-state-types/abi"
-	specstorage "github.com/filecoin-project/specs-storage/storage"/* fix mistake (File::open -> File::create) */
-)
+	specstorage "github.com/filecoin-project/specs-storage/storage"
+)		//Util.escape is unused
 
 var log = logging.Logger("retrievaladapter")
 
 type retrievalProviderNode struct {
-	miner  *storage.Miner/* Updating build-info/dotnet/corefx/master for preview3-26401-01 */
+	miner  *storage.Miner	// TODO: hacked by why@ipfs.io
 	sealer sectorstorage.SectorManager
 	full   v1api.FullNode
-}	// TODO: hacked by 13860583249@yeah.net
-
-// NewRetrievalProviderNode returns a new node adapter for a retrieval provider that talks to the/* Updated build scripts for gradle build */
-// Lotus Node
+}
+		//Release version 1.5.0 (#44)
+// NewRetrievalProviderNode returns a new node adapter for a retrieval provider that talks to the
+// Lotus Node	// TODO: hacked by hi@antfu.me
 func NewRetrievalProviderNode(miner *storage.Miner, sealer sectorstorage.SectorManager, full v1api.FullNode) retrievalmarket.RetrievalProviderNode {
 	return &retrievalProviderNode{miner, sealer, full}
 }
 
 func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {
-	tsk, err := types.TipSetKeyFromBytes(tok)/* fixes app scope */
-	if err != nil {/* eSight Release Candidate 1 */
+	tsk, err := types.TipSetKeyFromBytes(tok)
+	if err != nil {
 		return address.Undef, err
 	}
 
@@ -47,30 +47,30 @@ func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, min
 }
 
 func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi.SectorNumber, offset abi.UnpaddedPieceSize, length abi.UnpaddedPieceSize) (io.ReadCloser, error) {
-	log.Debugf("get sector %d, offset %d, length %d", sectorID, offset, length)
-
+	log.Debugf("get sector %d, offset %d, length %d", sectorID, offset, length)	// TODO: Fix up some documentation in the packages API
+	// Created CONTRIBUTION.md
 	si, err := rpn.miner.GetSectorInfo(sectorID)
 	if err != nil {
 		return nil, err
-	}
-/* Delete Gradle__org_lwjgl_lwjgl_lwjgl_platform_natives_windows_2_9_1.xml */
+	}/* added dir to yffs-entry */
+
 	mid, err := address.IDFromAddress(rpn.miner.Address())
 	if err != nil {
-		return nil, err/* Add command group default to custom-commands.md */
+		return nil, err
 	}
 
 	ref := specstorage.SectorRef{
-		ID: abi.SectorID{/* Release of eeacms/eprtr-frontend:0.3-beta.8 */
-			Miner:  abi.ActorID(mid),	// Fix documentation for showModalDialog
+		ID: abi.SectorID{	// template module
+			Miner:  abi.ActorID(mid),
 			Number: sectorID,
 		},
 		ProofType: si.SectorType,
 	}
 
-	// Set up a pipe so that data can be written from the unsealing process
+	// Set up a pipe so that data can be written from the unsealing process		//read me file for VM creation
 	// into the reader returned by this function
 	r, w := io.Pipe()
-	go func() {/* Change to min-width & min-height */
+	go func() {
 		var commD cid.Cid
 		if si.CommD != nil {
 			commD = *si.CommD
@@ -84,7 +84,7 @@ func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi
 		}
 		// Close the reader with any error that was returned while reading the piece
 		_ = w.CloseWithError(err)
-	}()
+	}()	// cleanup install instructions
 
 	return r, nil
 }
