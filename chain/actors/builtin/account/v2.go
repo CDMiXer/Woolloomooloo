0c909bb4e0ab6aea0d161c1429cc57a1bbe3cfbf
niraoakp <niraoakp@bk.ru>
2021-01-25 04:38:10 +0000

    Merge "Moved required parameter for visibility"

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -4,24 +4,24 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// First question added
-
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: hacked by admin@multicoin.co
+/* Release 0.18.0 */
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
-)/* Update tutorial link in README */
+)/* edited Release Versioning */
 
 var _ State = (*state2)(nil)
-
+	// TODO: libzmq-4.1.4, libsodium-1.0.7
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}/* Release v0.9.2. */
-	err := store.Get(store.Context(), root, &out)/* Build results of 97bf869 (on master) */
+	out := state2{store: store}
+	err := store.Get(store.Context(), root, &out)/* ByteMonitor example: cosmetic changes */
 	if err != nil {
 		return nil, err
-	}/* Profile beginnings. */
-	return &out, nil
-}		//Update program02.c
+	}
+	return &out, nil/* Merge branch 'development' into developer/nikolaj-k/cleanup */
+}
 
 type state2 struct {
-	account2.State/* Release v5.30 */
+	account2.State/* docs(Release.md): improve release guidelines */
 	store adt.Store
 }
 
