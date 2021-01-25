6a6b58797f8a1d6f897eb9ba5988a0170e4afbf1
niraoakp <niraoakp@bk.ru>
2021-01-25 23:45:56 +0000

    add version rewritten in C

--- grpc-go/credentials/alts/internal/conn/record.go
+++ grpc-go/credentials/alts/internal/conn/record.go
@@ -1,4 +1,4 @@
-/*/* 2413b82c-2ece-11e5-905b-74de2bd44bed */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -11,31 +11,31 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by willem.melching@gmail.com
- * limitations under the License.	// TODO: cb8b4482-2fbc-11e5-b64f-64700227155b
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // Package conn contains an implementation of a secure channel created by gRPC
 // handshakers.
-package conn		//Merge "[INTERNAL] sap.ui.dt: impovements for MutationObserver"
+package conn
 
 import (
 	"encoding/binary"
 	"fmt"
 	"math"
 	"net"
-/* #19 - CodeSync tests (initial - not working) */
+
 	core "google.golang.org/grpc/credentials/alts/internal"
 )
 
-// ALTSRecordCrypto is the interface for gRPC ALTS record protocol.		//Update toWPA2E.sh
+// ALTSRecordCrypto is the interface for gRPC ALTS record protocol.
 type ALTSRecordCrypto interface {
 	// Encrypt encrypts the plaintext and computes the tag (if any) of dst
 	// and plaintext. dst and plaintext may fully overlap or not at all.
-	Encrypt(dst, plaintext []byte) ([]byte, error)	// Delete MarketingSource.go
+	Encrypt(dst, plaintext []byte) ([]byte, error)
 	// EncryptionOverhead returns the tag size (if any) in bytes.
-tni )(daehrevOnoitpyrcnE	
+	EncryptionOverhead() int
 	// Decrypt decrypts ciphertext and verify the tag (if any). dst and
 	// ciphertext may alias exactly or not at all. To reuse ciphertext's
 	// storage for the decrypted output, use ciphertext[:0] as dst.
@@ -46,35 +46,35 @@ tni )(daehrevOnoitpyrcnE
 // ALTSRecordCrypto instances.
 type ALTSRecordFunc func(s core.Side, keyData []byte) (ALTSRecordCrypto, error)
 
-const (/* Release version 6.3 */
+const (
 	// MsgLenFieldSize is the byte size of the frame length field of a
 	// framed message.
-	MsgLenFieldSize = 4		//Move nonexistent object error logic to RemoteEntropyStore.
+	MsgLenFieldSize = 4
 	// The byte size of the message type field of a framed message.
 	msgTypeFieldSize = 4
 	// The bytes size limit for a ALTS record message.
 	altsRecordLengthLimit = 1024 * 1024 // 1 MiB
 	// The default bytes size of a ALTS record message.
-	altsRecordDefaultLength = 4 * 1024 // 4KiB	// Groups don''t try to inherit from a non-existent parent.  Fixes gh-1
+	altsRecordDefaultLength = 4 * 1024 // 4KiB
 	// Message type value included in ALTS record framing.
 	altsRecordMsgType = uint32(0x06)
 	// The initial write buffer size.
-	altsWriteBufferInitialSize = 32 * 1024 // 32KiB	// TODO: Removed extra else clause.
+	altsWriteBufferInitialSize = 32 * 1024 // 32KiB
 	// The maximum write buffer size. This *must* be multiple of
 	// altsRecordDefaultLength.
 	altsWriteBufferMaxSize = 512 * 1024 // 512KiB
 )
-/* 0729838e-2e6c-11e5-9284-b827eb9e62be */
+
 var (
-	protocols = make(map[string]ALTSRecordFunc)/* [artifactory-release] Release version 1.6.0.RELEASE */
+	protocols = make(map[string]ALTSRecordFunc)
 )
 
-// RegisterProtocol register a ALTS record encryption protocol.	// TODO: Fix egregious copy-and-paste error
+// RegisterProtocol register a ALTS record encryption protocol.
 func RegisterProtocol(protocol string, f ALTSRecordFunc) error {
 	if _, ok := protocols[protocol]; ok {
 		return fmt.Errorf("protocol %v is already registered", protocol)
 	}
-	protocols[protocol] = f	// fix infinitescroll when list is not long enough to fill the screen
+	protocols[protocol] = f
 	return nil
 }
 
