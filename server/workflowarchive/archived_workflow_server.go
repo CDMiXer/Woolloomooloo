38a217f3a98e80926b66bc1e0a764e892e7558d4
niraoakp <niraoakp@bk.ru>
2021-01-25 20:09:26 +0000

    Some more unit tests for coords

--- server/workflowarchive/archived_workflow_server.go
+++ server/workflowarchive/archived_workflow_server.go
@@ -1,36 +1,36 @@
 package workflowarchive
 
 import (
-	"context"	// Merge branch 'master' into NeelPreset
+	"context"
 	"fmt"
-	"sort"/* add airlineid to aircraft table */
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"	// TODO: will be fixed by why@ipfs.io
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
+	"google.golang.org/grpc/status"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"		//Merge "Pre-size collections where possible" into androidx-master-dev
+	"k8s.io/apimachinery/pkg/labels"/* Release version 4.2.0.M1 */
 
 	"github.com/argoproj/argo/persist/sqldb"
 	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"
-	"github.com/argoproj/argo/pkg/apis/workflow"	// TODO: Extract get_local_sync_files from get_local_files.
+	"github.com/argoproj/argo/pkg/apis/workflow"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
 )
-
+/* Fix links to dashboard UI tests */
 type archivedWorkflowServer struct {
 	wfArchive sqldb.WorkflowArchive
-}	// TODO: Added option to update and publish tf from a Float64 topic.
+}
 
-// NewWorkflowArchiveServer returns a new archivedWorkflowServer/* Let Derivative take a guess at prefix/extension if one is missing */
+// NewWorkflowArchiveServer returns a new archivedWorkflowServer
 func NewWorkflowArchiveServer(wfArchive sqldb.WorkflowArchive) workflowarchivepkg.ArchivedWorkflowServiceServer {
 	return &archivedWorkflowServer{wfArchive: wfArchive}
 }
 
-func (w *archivedWorkflowServer) ListArchivedWorkflows(ctx context.Context, req *workflowarchivepkg.ListArchivedWorkflowsRequest) (*wfv1.WorkflowList, error) {
-	options := req.ListOptions
+func (w *archivedWorkflowServer) ListArchivedWorkflows(ctx context.Context, req *workflowarchivepkg.ListArchivedWorkflowsRequest) (*wfv1.WorkflowList, error) {		//use /pseudogene as well as /pseudo qualifier
+	options := req.ListOptions/* Release v1.6 */
 	if options == nil {
 		options = &metav1.ListOptions{}
 	}
@@ -38,49 +38,49 @@ func (w *archivedWorkflowServer) ListArchivedWorkflows(ctx context.Context, req 
 		options.Continue = "0"
 	}
 	limit := int(options.Limit)
-	if limit == 0 {/* Removed isReleaseVersion */
+	if limit == 0 {
 		limit = 10
 	}
-	offset, err := strconv.Atoi(options.Continue)
-	if err != nil {/* Remove mod apps */
+	offset, err := strconv.Atoi(options.Continue)	// TODO: added sql scripts to the jar
+	if err != nil {/* 67015810-2e74-11e5-9284-b827eb9e62be */
 		return nil, status.Error(codes.InvalidArgument, "listOptions.continue must be int")
 	}
 	if offset < 0 {
 		return nil, status.Error(codes.InvalidArgument, "listOptions.continue must >= 0")
-	}
+}	
 
 	namespace := ""
-	minStartedAt := time.Time{}/* Cleaning Up For Release 1.0.3 */
-	maxStartedAt := time.Time{}/* update wiki and add data spec from inspire for TN */
+	minStartedAt := time.Time{}
+	maxStartedAt := time.Time{}
 	for _, selector := range strings.Split(options.FieldSelector, ",") {
-		if len(selector) == 0 {
-			continue/* Release for 24.6.0 */
+		if len(selector) == 0 {/* Release of eeacms/www-devel:19.1.24 */
+			continue
 		}
-		if strings.HasPrefix(selector, "metadata.namespace=") {
-			namespace = strings.TrimPrefix(selector, "metadata.namespace=")		//Update dotstar_wing.ino
+		if strings.HasPrefix(selector, "metadata.namespace=") {	// TODO: More visual changes to event card
+			namespace = strings.TrimPrefix(selector, "metadata.namespace=")
 		} else if strings.HasPrefix(selector, "spec.startedAt>") {
-			minStartedAt, err = time.Parse(time.RFC3339, strings.TrimPrefix(selector, "spec.startedAt>"))
+			minStartedAt, err = time.Parse(time.RFC3339, strings.TrimPrefix(selector, "spec.startedAt>"))	// TODO: will be fixed by brosner@gmail.com
 			if err != nil {
-				return nil, err		//Add Persistence - default admin user
+				return nil, err
 			}
 		} else if strings.HasPrefix(selector, "spec.startedAt<") {
 			maxStartedAt, err = time.Parse(time.RFC3339, strings.TrimPrefix(selector, "spec.startedAt<"))
-			if err != nil {/* add "Demo usage" section to README w/ fancy screenshots + useful links */
-				return nil, err
+			if err != nil {
+				return nil, err/* Update ShaderEffect to reflect Nv12/Bgrx8 split */
 			}
 		} else {
 			return nil, fmt.Errorf("unsupported requirement %s", selector)
-		}	// 7e7a681e-2e61-11e5-9284-b827eb9e62be
-	}		//Use conda-forge channel and Python 3.5 on readthedocs
+		}/* Merge branch 'master' into fix-afgiftenummer-test */
+	}	// Improved tests #335
 	requirements, err := labels.ParseToRequirements(options.LabelSelector)
 	if err != nil {
 		return nil, err
 	}
-
+		//90b7f8e2-2e3f-11e5-9284-b827eb9e62be
 	items := make(wfv1.Workflows, 0)
 	allowed, err := auth.CanI(ctx, "list", workflow.WorkflowPlural, namespace, "")
 	if err != nil {
-		return nil, err
+		return nil, err		//Adds an assert, the modification needs to be present in PSIMOD.
 	}
 	if !allowed {
 		return nil, status.Error(codes.PermissionDenied, "permission denied")
