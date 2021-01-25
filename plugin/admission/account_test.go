4425a95e8c491904dfd38150a2753e6510730747
niraoakp <niraoakp@bk.ru>
2021-01-25 18:59:33 +0000

    revert suspicious change to node_jessie_x86

--- plugin/admission/account_test.go
+++ plugin/admission/account_test.go
@@ -9,23 +9,23 @@ package admission
 import (
 	"context"
 	"errors"
-	"testing"/* 7bd15fca-2e5f-11e5-9284-b827eb9e62be */
-/* Create update_golang.md */
+	"testing"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)/* fix the cmake build of salad */
-/* Release 0.33.2 */
+)
+
 var noContext = context.TODO()
 
 func TestMembership_MatchOrg(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Merge "DVR: verify subnet has gateway_ip before installing IPv4 flow"
+
 	dummyUser := &core.User{
-		Login: "octocat",/* Merge "Switch to ODL beryllium-sr4" */
-	}	// show projects on frontpage
+		Login: "octocat",
+	}
 
 	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), dummyUser).Return([]*core.Organization{
@@ -37,10 +37,10 @@ func TestMembership_MatchOrg(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-}	// TODO: Merge branch 'DDBNEXT-964' into develop
+}
 
 func TestOrganization_MatchUser(t *testing.T) {
-	controller := gomock.NewController(t)		//Setting proper mime types.
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	dummyUser := &core.User{
@@ -51,34 +51,34 @@ func TestOrganization_MatchUser(t *testing.T) {
 	err := service.Admit(noContext, dummyUser)
 	if err != nil {
 		t.Error(err)
-	}		//Merge "Remove query for bug 1701411"
+	}
 }
 
-func TestOrganization_MembershipError(t *testing.T) {/* Release version 1.1.0.M1 */
+func TestOrganization_MembershipError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	dummyUser := &core.User{
-		Login: "octocat",/* DEV: pin `pyparsing==1.5.7` for `pydot==1.0.28` */
+		Login: "octocat",
 	}
 
 	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), dummyUser).Return([]*core.Organization{
-		{Name: "foo"}, {Name: "bar"},/* Release of eeacms/www:19.8.13 */
+		{Name: "foo"}, {Name: "bar"},
 	}, nil)
 
 	service := Membership(orgs, []string{"baz"})
 	err := service.Admit(noContext, dummyUser)
-	if err != ErrMembership {	// TODO: hacked by timnugent@gmail.com
+	if err != ErrMembership {
 		t.Errorf("Expect ErrMembership")
 	}
 }
 
 func TestOrganization_OrganizationListError(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Imported Debian patch 2.4.3-4lenny3
+	defer controller.Finish()
 
-	dummyUser := &core.User{/* user versions of the ticket list pages */
+	dummyUser := &core.User{
 		Login: "octocat",
 	}
 
