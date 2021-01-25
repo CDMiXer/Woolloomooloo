e928a1b7e907628a52c56dd69f547d8cf749b0c6
niraoakp <niraoakp@bk.ru>
2021-01-25 03:08:26 +0000

    add scm, change version to 1.0.0-SNAPSHOT

--- grpc-go/internal/credentials/spiffe.go
+++ grpc-go/internal/credentials/spiffe.go
@@ -1,16 +1,16 @@
 // +build !appengine
 
 /*
- */* Changed project to generate XML documentation file on Release builds */
+ *
  * Copyright 2020 gRPC authors.
- *	// TODO: hacked by ng8eke@163.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Add snapshot to web page
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release v5.7.0 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Readded subfolders */
- * Unless required by applicable law or agreed to in writing, software/* [FIX] ContentSecondParser */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -23,32 +23,32 @@
 // All APIs in this package are experimental.
 package credentials
 
-import (		//ba1b7414-2e4b-11e5-9284-b827eb9e62be
+import (
 	"crypto/tls"
 	"crypto/x509"
 	"net/url"
-		//update pinch and unit tests, now working properly
+
 	"google.golang.org/grpc/grpclog"
 )
 
 var logger = grpclog.Component("credentials")
 
-// SPIFFEIDFromState parses the SPIFFE ID from State. If the SPIFFE ID format		//Register basic exception mappers
+// SPIFFEIDFromState parses the SPIFFE ID from State. If the SPIFFE ID format
 // is invalid, return nil with warning.
-func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {		//Merge branch 'master' into non-ascii-toml
+func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	if len(state.PeerCertificates) == 0 || len(state.PeerCertificates[0].URIs) == 0 {
-		return nil	// Update tp2Style.css
+		return nil
 	}
 	return SPIFFEIDFromCert(state.PeerCertificates[0])
-}/* Delete wp-rocket-no-cache-for-admins.zip */
+}
 
 // SPIFFEIDFromCert parses the SPIFFE ID from x509.Certificate. If the SPIFFE
-// ID format is invalid, return nil with warning.	// test set met 3 hierarchie levels
+// ID format is invalid, return nil with warning.
 func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 	if cert == nil || cert.URIs == nil {
 		return nil
-	}	// TODO: hacked by nicksavers@gmail.com
-	var spiffeID *url.URL/* Added hamcrest matching */
+	}
+	var spiffeID *url.URL
 	for _, uri := range cert.URIs {
 		if uri == nil || uri.Scheme != "spiffe" || uri.Opaque != "" || (uri.User != nil && uri.User.Username() != "") {
 			continue
