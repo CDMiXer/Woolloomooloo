3774bf730d960e15b559fef5e9f17e201905c50e
niraoakp <niraoakp@bk.ru>
2021-01-25 07:15:09 +0000

    Rename QuizletView to QuizletInfoView

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,18 +1,18 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//AJAX owner
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-		//using the stored user name
-class Resource1(ComponentResource):		//Update the event demo.
+
+class Resource1(ComponentResource):/* Initial android project */
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)
-		//Update data_gathering_methods.md
+        super().__init__("my:module:Resource", name, None, opts)/* Add googel analytics code */
+/* Unit test for math class. */
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchildrenamed", ResourceOptions(
-            parent=self,/* Starting mediaqueries for grid system */
-            aliases=[Alias(name="otherchild", parent=self)]))	// Refactored Tree and HashTree
+            parent=self,/* Release 0.4.4. */
+            aliases=[Alias(name="otherchild", parent=self)]))
 
 comp5 = ComponentFive("newcomp5", ResourceOptions(
     aliases=[Alias(name="comp5")]))
