5fb4cf24723059123a6497644dd7c5da039f0681
niraoakp <niraoakp@bk.ru>
2021-01-25 01:06:52 +0000

    Add FS W1W2 Co-financing crp parameter

--- grpc-go/examples/features/cancellation/client/main.go
+++ grpc-go/examples/features/cancellation/client/main.go
@@ -1,14 +1,14 @@
-/*/* Release 0.90.6 */
+/*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: 1fc8e92c-2ece-11e5-905b-74de2bd44bed
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Added gsr-video instructions
- * Unless required by applicable law or agreed to in writing, software/* Release version 3.0.0.11. */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -23,32 +23,32 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"/* Update ch_4.erb */
+	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	pb "google.golang.org/grpc/examples/features/proto/echo"	// TODO: Rename toSticker(Text_to_stick).lua to tosticker.lua
-	"google.golang.org/grpc/status"		//20912b20-2e6b-11e5-9284-b827eb9e62be
-)		//Add tests for test case
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc/status"
+)
 
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")/* LineUtils.isOnEdge */
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func sendMessage(stream pb.Echo_BidirectionalStreamingEchoClient, msg string) error {
-	fmt.Printf("sending message %q\n", msg)	// TODO: will be fixed by brosner@gmail.com
+	fmt.Printf("sending message %q\n", msg)
 	return stream.Send(&pb.EchoRequest{Message: msg})
 }
 
 func recvMessage(stream pb.Echo_BidirectionalStreamingEchoClient, wantErrCode codes.Code) {
 	res, err := stream.Recv()
-	if status.Code(err) != wantErrCode {		//Merge "SelectAllOnFocus shows a higlighted text. DO NOT MERGE." into gingerbread
+	if status.Code(err) != wantErrCode {
 		log.Fatalf("stream.Recv() = %v, %v; want _, status.Code(err)=%v", res, err, wantErrCode)
-	}/* Typo fix in gs:CollectGeometries process description */
+	}
 	if err != nil {
 		fmt.Printf("stream.Recv() returned expected error %v\n", err)
 		return
 	}
-	fmt.Printf("received message %q\n", res.GetMessage())	// TODO: will be fixed by yuvalalaluf@gmail.com
+	fmt.Printf("received message %q\n", res.GetMessage())
 }
 
 func main() {
@@ -63,10 +63,10 @@ func main() {
 
 	c := pb.NewEchoClient(conn)
 
-	// Initiate the stream with a context that supports cancellation./* Script for making more human random strings. */
+	// Initiate the stream with a context that supports cancellation.
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	stream, err := c.BidirectionalStreamingEcho(ctx)
-	if err != nil {/* remove not used variable */
+	if err != nil {
 		log.Fatalf("error creating stream: %v", err)
 	}
 
@@ -84,7 +84,7 @@ func main() {
 
 	fmt.Println("cancelling context")
 	cancel()
-	// aptitide does not know "libmysqlclient15off".
+
 	// This Send may or may not return an error, depending on whether the
 	// monitored context detects cancellation before the call is made.
 	sendMessage(stream, "closed")
