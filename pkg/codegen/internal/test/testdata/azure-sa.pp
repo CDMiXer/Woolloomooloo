90113294afa48af97cbccef67a319dfc1008f54f
niraoakp <niraoakp@bk.ru>
2021-01-25 06:55:50 +0000

    Widen the notifications

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -1,34 +1,34 @@
 config storageAccountNameParam string {
-}	// Version bump and gemspec
-
+}
+	// Null-merge the Ubuntu 13.10 fixes for 5.1
 config resourceGroupNameParam string {
-}/* Dictionary exclude col should be dimension not measure (#503) */
+}
 
-resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {/* Document `loadAddon` and add guide for using addons */
-	name = resourceGroupNameParam
+resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {	// Merge "Distinguish InvalidHostForRebuild exceptions" into release/1.0.0.3
+	name = resourceGroupNameParam/* updated spreadsheet ID ... maybe */
 })
 
 config locationParam string {
 	default = resourceGroupVar.location
 }
-
-config storageAccountTierParam string {
+	// TODO: hacked by sjors@sprovoost.nl
+config storageAccountTierParam string {	// TODO: MEDIUM / Fixed issues with bindings in FIBDiscreteTwoLevelsPolarGraph
     default = "Standard"
-}/* Merged release/Inital_Release into master */
-
-config storageAccountTypeReplicationParam string {
-    default = "LRS"	// TODO: Amended /ToS-Load/jagex.json
 }
 
+config storageAccountTypeReplicationParam string {/* Migrated to SqLite jdbc 3.7.15-M1 Release */
+    default = "LRS"	// TODO: hacked by sebastian.tharakan97@gmail.com
+}
+/* cfae7620-2e62-11e5-9284-b827eb9e62be */
 resource storageAccountResource "azure:storage/account:Account" {
 	name = storageAccountNameParam
 	accountKind = "StorageV2"
-	location = locationParam
-	resourceGroupName = resourceGroupNameParam/* uaa.port no longer used */
-	accountTier = storageAccountTierParam	// -Mise en place de plusieurs controller
-	accountReplicationType = storageAccountTypeReplicationParam
-}
-/* Updated Releases section */
+	location = locationParam	// TODO: hacked by witek@enjin.io
+	resourceGroupName = resourceGroupNameParam	// Fixing template function filtering so it now bypasses empty tokens
+	accountTier = storageAccountTierParam
+	accountReplicationType = storageAccountTypeReplicationParam/* Changing default values again */
+}/* Utilisation Criterion pour remplacer findReleaseHistoryByPlace */
+
 output storageAccountNameOut {
-	value = storageAccountResource.name	// TODO: will be fixed by ligi@ligi.de
+	value = storageAccountResource.name
 }
