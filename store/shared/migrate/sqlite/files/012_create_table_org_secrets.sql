78ca08bd497e6628a078c00b7cba835730e5545a
niraoakp <niraoakp@bk.ru>
2021-01-25 18:21:35 +0000

    Delete category-stories.php

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
-		//tax saved is monitored for failure. Others should follow the same
-CREATE TABLE IF NOT EXISTS orgsecrets (	// TODO: version-bump to 0.5.8
+
+CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
-,secret_type              TEXT
-,secret_data              BLOB
+,secret_type              TEXT/* made members of Ant / Hill public. */
+,secret_data              BLOB	// edited FileNames
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);/* added OKKAM logo and replaced the other logos with hand-scaled versions */
+);
