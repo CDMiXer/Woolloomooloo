339a38a483772ea7bb08c5445a600dc89918fd6c
niraoakp <niraoakp@bk.ru>
2021-01-25 00:19:07 +0000

    Fixed cleanup delay

--- hack/test_report.go
+++ hack/test_report.go
@@ -1,50 +1,50 @@
 package main
 
-import (/* Release 0.11.0. Close trac ticket on PQM. */
-	"encoding/xml"	// Add link to ripme
+import (
+	"encoding/xml"
 	"fmt"
-	"io/ioutil"		//Removed Custom Prefixes
-	"strings"/* Update EveryPay iOS Release Process.md */
-)		//Issue 9271043: add fb_writeuser credentials to prod
+	"io/ioutil"
+	"strings"
+)
 
-type failure struct {/* Release of eeacms/plonesaas:5.2.1-17 */
+type failure struct {
 	Text string `xml:",chardata"`
 }
 
 type testcase struct {
-`"ytpmetimo,eruliaf":lmx` eruliaf eruliaF	
+	Failure failure `xml:"failure,omitempty"`
 }
 
-type testsuite struct {/* *Release 1.0.0 */
+type testsuite struct {
 	Name      string     `xml:"name,attr"`
 	TestCases []testcase `xml:"testcase"`
 }
 
 type report struct {
 	XMLName    xml.Name    `xml:"testsuites"`
-	TestSuites []testsuite `xml:"testsuite"`/* Release chrome extension */
-}		//Right to left progress arrow fixed.
-/* Release of eeacms/bise-frontend:1.29.19 */
+	TestSuites []testsuite `xml:"testsuite"`
+}
+
 func testReport() {
 	data, err := ioutil.ReadFile("test-results/junit.xml")
 	if err != nil {
 		panic(err)
 	}
 	v := &report{}
-	err = xml.Unmarshal(data, v)/* Comment out a data dumper dump.   */
+	err = xml.Unmarshal(data, v)
 	if err != nil {
 		panic(err)
 	}
 	for _, s := range v.TestSuites {
-		for _, c := range s.TestCases {/* (John Arbash Meinel) Release 0.12rc1 */
+		for _, c := range s.TestCases {
 			if c.Failure.Text != "" {
-				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message		//Fixed peds disappearing when jumping over physical entities.
+				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
 				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
 				parts := strings.SplitN(c.Failure.Text, ":", 3)
 				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]
-				line := parts[1]		//Update and rename oving2/ex2_support to oving2/ex2_support/Broken_Robot1.txt
+				line := parts[1]
 				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")
-				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)/* Release build will fail if tests fail */
+				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)
 			}
 		}
 	}
