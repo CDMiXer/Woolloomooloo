309181178ce98e693ed835c6aaf1fbbeca2f974d
niraoakp <niraoakp@bk.ru>
2021-01-25 03:03:42 +0000

    Added Fields in Selective Process, Added Cleanup Dao for Remove Expireds

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -3,33 +3,33 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* add Release 1.0 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// A very basic main class, but functional
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: PeterI/KennyL: -[#38203277] updated build script
-// +build oss		//fixed warnings when compiled with -Wwrite-strings
+
+// +build oss
 
 package config
 
 import (
 	"context"
 	"time"
-/* Release preparation: version update */
+
 	"github.com/drone/drone/core"
 )
 
 // Global returns a no-op configuration service.
 func Global(string, string, bool, time.Duration) core.ConfigService {
 	return new(noop)
-}/* Update usage_ingest.de.md */
+}
 
 type noop struct{}
 
-func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {/* DipTest Release */
+func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {
 	return nil, nil
 }
