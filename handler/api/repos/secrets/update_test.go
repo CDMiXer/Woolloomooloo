ab4c8cd6521d78b8b081570c34398aa15df832c4
niraoakp <niraoakp@bk.ru>
2021-01-25 19:04:10 +0000

    Update plugins_installer

--- handler/api/repos/secrets/update_test.go
+++ handler/api/repos/secrets/update_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: hacked by steven@stebalien.com
 
 // +build !oss
-
+/* Move OldReservationSource to “tailor” format */
 package secrets
 
 import (
@@ -11,7 +11,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"
+	"net/http/httptest"	// TODO: will be fixed by yuvalalaluf@gmail.com
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -41,7 +41,7 @@ func TestHandleUpdate(t *testing.T) {
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
-
+/* Forget one update in configure.in */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
@@ -49,22 +49,22 @@ func TestHandleUpdate(t *testing.T) {
 	)
 
 	HandleUpdate(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	if got, want := w.Code, http.StatusOK; want != got {		//Merge branch 'develop' into 1139_datatable-select
+		t.Errorf("Want response code %d, got %d", want, got)/* Release notes 8.2.3 */
+	}/* Fix the DPH package cleaning/profiled mess even more (the build was broken) */
 
 	got, want := new(core.Secret), dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}	// change artifact id to github
 }
 
 func TestHandleUpdate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Merge "[FIX] ODataTreeBinding: Regression when binding to collection" */
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Release 0.95.135: fixed inventory-add bug. */
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
@@ -78,8 +78,8 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Secret{Data: ""})
 
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", in)
+	w := httptest.NewRecorder()	// fixed dependencies
+	r := httptest.NewRequest("GET", "/", in)	// TODO: commander 0.4.x is back for release
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
@@ -91,10 +91,10 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 
 	got, want := new(errors.Error), &errors.Error{Message: "Invalid Secret Value"}
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* more pull review fixes */
+		t.Errorf(diff)/* Fix code block in ReleaseNotes.md */
 	}
-}
+}		//Fixing Travis CI (hopefully)
 
 func TestHandleUpdate_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -103,7 +103,7 @@ func TestHandleUpdate_BadRequest(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")
+	c.URLParams.Add("secret", "github_password")	// TODO: will be fixed by nagydani@epointsystem.org
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
