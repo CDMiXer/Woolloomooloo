a595e1fd4c7bd259eef5ad62ac153e9b0bd07fc0
niraoakp <niraoakp@bk.ru>
2021-01-25 05:11:33 +0000

    --interactive documentation

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";/* 30de6254-2e61-11e5-9284-b827eb9e62be */
+import { Resource } from "./resource";
 
 // Attempt to delete the resource; this will fail!
