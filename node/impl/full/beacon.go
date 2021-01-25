8f83405852a6f267d4e39763f05023a9ccc17fdf
niraoakp <niraoakp@bk.ru>
2021-01-25 06:03:46 +0000

    utf-8 codierung

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,28 +1,28 @@
 package full
 
-import (	// Update skeleton.lisp
+import (
 	"context"
 	"fmt"
-/* Release BAR 1.1.8 */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/beacon"
 	"github.com/filecoin-project/lotus/chain/types"
 	"go.uber.org/fx"
 )
-	// Add login to domain support
+
 type BeaconAPI struct {
 	fx.In
 
 	Beacon beacon.Schedule
 }
 
-func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {	// Added an initial .profile file.
+func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
 	b := a.Beacon.BeaconForEpoch(epoch)
 	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
 
 	select {
-	case be, ok := <-e:/* Got rid of unnecessary ending tags */
+	case be, ok := <-e:
 		if !ok {
 			return nil, fmt.Errorf("beacon get returned no value")
 		}
