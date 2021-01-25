35240aff4e82cf23da7fedcc2e93f2b4c5effdec
niraoakp <niraoakp@bk.ru>
2021-01-25 23:05:46 +0000

    Fix build error about OSGi versioning

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Decreased noise intensity, added trianglify credit
-//
+// Copyright 2016-2018, Pulumi Corporation.
+///* Update a link in README */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//a8f64b1a-2e54-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.	// Merge branch 'master' into add/6
 // You may obtain a copy of the License at
-//
+///* trigger new build for ruby-head (509cfc4) */
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Merge branch 'develop' into feature/device-status */
-// Unless required by applicable law or agreed to in writing, software		//Make Debconf less annoying
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* 3.5 Release Final Release */
+// limitations under the License.
 
-import { Resource } from "./resource";	// TODO: Removed Swing generics for pre Java 7.
+import { Resource } from "./resource";
 
 // Setup: "a" is an external resource.
 const a = new Resource("a", { state: 42 }, { id: "0" });
