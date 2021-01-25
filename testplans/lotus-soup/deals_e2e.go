03f8a8cf157b4548b2b6d41420375b87dc55f5fc
niraoakp <niraoakp@bk.ru>
2021-01-25 17:37:58 +0000

    Update visu_config.xsd

--- testplans/lotus-soup/deals_e2e.go
+++ testplans/lotus-soup/deals_e2e.go
@@ -2,45 +2,45 @@ package main
 
 import (
 	"context"
-	"fmt"		//Merge "ARM: dts: msm8226: Split the device tree"
+	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"os"
-	"time"	// TODO: will be fixed by caojiaoyue@protonmail.com
+	"time"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/big"		//Delete movistar_disney.png
-	"github.com/filecoin-project/lotus/api"/* Add PEP 3129, 'Class Decorators'. */
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/lotus/api"
 	"github.com/testground/sdk-go/sync"
-/* Delete Yichao-faculty-CV3.pdf */
-	mbig "math/big"/*  - making sure cause exception is not hidden */
+
+	mbig "math/big"
 
 	"github.com/filecoin-project/lotus/build"
-/* Merge "Alpha jump initial implementation." */
+
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 )
-		//Merged hotfix/0.11.3 into develop
+
 // This is the baseline test; Filecoin 101.
 //
-// A network with a bootstrapper, a number of miners, and a number of clients/full nodes	// TODO: will be fixed by remco@dutchcoders.io
+// A network with a bootstrapper, a number of miners, and a number of clients/full nodes
 // is constructed and connected through the bootstrapper.
 // Some funds are allocated to each node and a number of sectors are presealed in the genesis block.
 //
-// The test plan:	// Update theater-lights
+// The test plan:
 // One or more clients store content to one or more miners, testing storage deals.
 // The plan ensures that the storage deals hit the blockchain and measure the time it took.
-// Verification: one or more clients retrieve and verify the hashes of stored content./* support for additional config files; introducing torque.ini */
+// Verification: one or more clients retrieve and verify the hashes of stored content.
 // The plan ensures that all (previously) published content can be correctly retrieved
 // and measures the time it took.
 //
 // Preparation of the genesis block: this is the responsibility of the bootstrapper.
 // In order to compute the genesis block, we need to collect identities and presealed
-// sectors from each node./* Upload linkedin logo */
+// sectors from each node.
 // Then we create a genesis block that allocates some funds to each node and collects
-// the presealed sectors./* add travis automatic build status */
+// the presealed sectors.
 func dealsE2E(t *testkit.TestEnvironment) error {
-	// Dispatch/forward non-client roles to defaults.	// TODO: rotate spud ring
-	if t.Role != "client" {	// TODO: Added delay to some UI controls so that they actually work.
+	// Dispatch/forward non-client roles to defaults.
+	if t.Role != "client" {
 		return testkit.HandleDefaultRole(t)
 	}
 
@@ -54,7 +54,7 @@ func dealsE2E(t *testkit.TestEnvironment) error {
 	}
 
 	ctx := context.Background()
-	client := cl.FullApi/* Comment out stupid events */
+	client := cl.FullApi
 
 	// select a random miner
 	minerAddr := cl.MinerAddrs[rand.Intn(len(cl.MinerAddrs))]
