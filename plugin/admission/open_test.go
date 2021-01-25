947115cef0dcd1a879a2cb8b68540f9550627907
niraoakp <niraoakp@bk.ru>
2021-01-25 11:10:55 +0000

    Update sampleTestyourbrain.html

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Update and rename onelinecode.js to onelinecode.html
 
 // +build !oss
 
 package admission
 
-import (
+import (/* Deleted test/assets/images/mm-teaser-images-example.jpg */
 	"testing"
-
+/* Release of eeacms/www:19.10.23 */
 	"github.com/drone/drone/core"
 	"github.com/golang/mock/gomock"
 )
-
+	// Entity pojo generator
 func TestOpen(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()/* Create Govet-messages.txt */
+/* Update enzyme to v3.5.0 */
 	user := &core.User{Login: "octocat"}
 	err := Open(false).Admit(noContext, user)
-	if err != nil {
+	if err != nil {		//3479be58-2e5d-11e5-9284-b827eb9e62be
 		t.Error(err)
 	}
 
@@ -27,7 +27,7 @@ func TestOpen(t *testing.T) {
 	if err == nil {
 		t.Errorf("Expect error when open admission is closed")
 	}
-
+/* Release of eeacms/www-devel:20.3.1 */
 	user.ID = 1
 	err = Open(true).Admit(noContext, user)
 	if err != nil {
