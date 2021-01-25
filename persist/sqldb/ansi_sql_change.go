d41cd39d9ef9c4738aa408a6e34048aa62fee5c6
niraoakp <niraoakp@bk.ru>
2021-01-25 02:30:17 +0000

    Remove redundant toString() calls

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb/* Comment out sqlalchemy echo */
+package sqldb
 
 import "upper.io/db.v3/lib/sqlbuilder"
 
 // represent a straight forward change that is compatible with all database providers
 type ansiSQLChange string
-/* heuuuu... to pull */
+
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
-	_, err := session.Exec(string(s))	// Merge branch 'master' into nocrypto-mirage
-	return err	// Get WebDM building against Snappy 2.0 by stevenwilkin approved by mvo
+	_, err := session.Exec(string(s))
+	return err
 }
