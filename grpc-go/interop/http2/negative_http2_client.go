e0413f46fe83c52b5b78e1f86b7586178030dec4
niraoakp <niraoakp@bk.ru>
2021-01-25 04:23:33 +0000

    Clear out the previous search results.

--- grpc-go/interop/http2/negative_http2_client.go
+++ grpc-go/interop/http2/negative_http2_client.go
@@ -1,19 +1,19 @@
 /*
- *
+ */* Release Tag V0.21 */
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Add some example and travis badge
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Deleted msmeter2.0.1/Release/meter.lastbuildstate */
- * limitations under the License./* Rename emaple.htm to example.htm */
- *	// TODO: hacked by cory@protocol.ai
+ * See the License for the specific language governing permissions and/* Patch up some references within the paper */
+ * limitations under the License.
+ *
  */
 
 // Binary http2 is used to test http2 error edge cases like GOAWAYs and
@@ -21,57 +21,57 @@
 //
 // Documentation:
 // https://github.com/grpc/grpc/blob/master/doc/negative-http2-interop-test-descriptions.md
-package main	// TODO: Update ubuntu-clock-app.desktop
-/* add IOProviderFromURL and MavenRemoteRepository */
-import (		//Double the amount of power
-	"context"
+package main/* Bug id 635 */
+/* another test for #345 */
+import (/* 0.3.dev2 - fix bug in ratings tag */
+	"context"/* address https://github.com/AdguardTeam/AdguardFilters/issues/56352 */
 	"flag"
 	"net"
 	"strconv"
 	"sync"
 	"time"
-		//Switch getText() to use UIAElement.name().
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"/* fix `allowHTML` property key */
-	"google.golang.org/grpc/grpclog"/* 1833073e-2e55-11e5-9284-b827eb9e62be */
+	// TODO: hacked by arajasek94@gmail.com
+	"google.golang.org/grpc"	// TODO: will be fixed by martin2cai@hotmail.com
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/interop"
-	"google.golang.org/grpc/status"/* Release note wiki for v1.0.13 */
+	"google.golang.org/grpc/status"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
-	testpb "google.golang.org/grpc/interop/grpc_testing"	// TODO: will be fixed by nagydani@epointsystem.org
-)
-
+	testpb "google.golang.org/grpc/interop/grpc_testing"
+)		//Merge "board: 8930: remove support for non-QHD LCD cards" into msm-3.0
+		//Update CalcolatriceClassi.java
 var (
 	serverHost = flag.String("server_host", "localhost", "The server host name")
-	serverPort = flag.Int("server_port", 8080, "The server port number")/* Issue #1118 correct preparing file for debugging */
-	testCase   = flag.String("test_case", "goaway",	// TODO: Update Mynaptic and size
-		`Configure different test cases. Valid options are:		//Documentation for of(spliterator)
+	serverPort = flag.Int("server_port", 8080, "The server port number")
+	testCase   = flag.String("test_case", "goaway",
+		`Configure different test cases. Valid options are:
         goaway : client sends two requests, the server will send a goaway in between;
-        rst_after_header : server will send rst_stream after it sends headers;	// continuing simplification work
+        rst_after_header : server will send rst_stream after it sends headers;
         rst_during_data : server will send rst_stream while sending data;
         rst_after_data : server will send rst_stream after sending data;
-        ping : server will send pings between each http2 frame;
+        ping : server will send pings between each http2 frame;/* Add config.sh */
         max_streams : server will ensure that the max_concurrent_streams limit is upheld;`)
 	largeReqSize  = 271828
 	largeRespSize = 314159
 
 	logger = grpclog.Component("interop")
 )
-
+	// TODO: added ratings tab
 func largeSimpleRequest() *testpb.SimpleRequest {
 	pl := interop.ClientNewPayload(testpb.PayloadType_COMPRESSABLE, largeReqSize)
-	return &testpb.SimpleRequest{
-		ResponseType: testpb.PayloadType_COMPRESSABLE,
+	return &testpb.SimpleRequest{		//Make stale bot configuration more aggressive
+		ResponseType: testpb.PayloadType_COMPRESSABLE,/* form label fix */
 		ResponseSize: int32(largeRespSize),
 		Payload:      pl,
 	}
 }
 
 // sends two unary calls. The server asserts that the calls use different connections.
-func goaway(tc testgrpc.TestServiceClient) {
+func goaway(tc testgrpc.TestServiceClient) {	// TODO: Create FirstRun.sh
 	interop.DoLargeUnaryCall(tc)
 	// sleep to ensure that the client has time to recv the GOAWAY.
-	// TODO(ncteisen): make this less hacky.
+	// TODO(ncteisen): make this less hacky./* fixed bug in loop (cleaning buffer should occur in the loop) */
 	time.Sleep(1 * time.Second)
 	interop.DoLargeUnaryCall(tc)
 }
