69cccbbcaa7d9242d0e60a1a09c716e8dfd2dcfa
niraoakp <niraoakp@bk.ru>
2021-01-25 12:35:06 +0000

    twincobr.c: added documentation [Guru]

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
+//		//fix: cleanup about page
+// Licensed under the Apache License, Version 2.0 (the "License");		//Delete 38_Care Dental-1.png
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: add badge to gh-board
-// You may obtain a copy of the License at/* message add appstars and appkinds */
+//      http://www.apache.org/licenses/LICENSE-2.0/* Released this version 1.0.0-alpha-4 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* Release mails should mention bzr's a GNU project */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Added endpoints to install page tab and manage pushpub connections
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release notes for Beaker 0.15" into develop */
+// See the License for the specific language governing permissions and
+// limitations under the License./* reduce unused vars */
 
 package db
-		//Implement script function timetravel() for Value Editor Dialog
-import "errors"
 
-// ErrOptimisticLock is returned by if the struct being/* Added PJSIP-Testing.doc */
-// modified has a Version field and the value is not equal
+import "errors"	// README: Add Installation section for npm
+
+// ErrOptimisticLock is returned by if the struct being
+// modified has a Version field and the value is not equal	// TODO: will be fixed by aeongrp@outlook.com
 // to the current value in the database
 var ErrOptimisticLock = errors.New("Optimistic Lock Error")
