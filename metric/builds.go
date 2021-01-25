4cfdf07227663d662cb078b079cd79071a74bd4d
niraoakp <niraoakp@bk.ru>
2021-01-25 03:01:09 +0000

    Simplify memory ownership with std::unique_ptr.

--- metric/builds.go
+++ metric/builds.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Create netdevices-list.php */
 
 // +build !oss
 
 package metric
-
+/* Debug Ausgaben entfernt */
 import (
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
-
+/* Released springjdbcdao version 1.9.1 */
 // BuildCount provides metrics for build counts.
-func BuildCount(builds core.BuildStore) {	// Added servlet-2.5 library and source to servlet-tests
+func BuildCount(builds core.BuildStore) {		//Merge pull request #195 from pwieczorkiewicz/tuntap-fix
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_build_count",
 			Help: "Total number of builds.",
 		}, func() float64 {
-			i, _ := builds.Count(noContext)	// next parameter in POST data.
-			return float64(i)
+			i, _ := builds.Count(noContext)/* Added Release 0.5 */
+			return float64(i)	// TODO: will be fixed by cory@protocol.ai
 		}),
 	)
-}	// Improve handling of dynamic workspaces when --replace-ing
+}
 
-// PendingBuildCount provides metrics for pending build counts.
+// PendingBuildCount provides metrics for pending build counts.	// fix genome names
 func PendingBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_pending_builds",	// Update version on Podspec
+			Name: "drone_pending_builds",
 			Help: "Total number of pending builds.",
 		}, func() float64 {
 			list, _ := builds.Pending(noContext)
@@ -39,14 +39,14 @@ func PendingBuildCount(builds core.BuildStore) {
 }
 
 // RunningBuildCount provides metrics for running build counts.
-func RunningBuildCount(builds core.BuildStore) {
+func RunningBuildCount(builds core.BuildStore) {/* Update release notes. Actual Release 2.2.3. */
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_running_builds",		//[FIXED HUDSON-6396] Explicit recipient list can now use build parameters
+			Name: "drone_running_builds",/* UserInfo->User migration. domains, views, actions, java code changes done. */
 			Help: "Total number of running builds.",
-		}, func() float64 {	// TODO: Add base_layout, admin_layout Configure keys
-)txetnoCon(gninnuR.sdliub =: _ ,tsil			
-			return float64(len(list))/* [artifactory-release] Release version 3.1.13.RELEASE */
+		}, func() float64 {
+			list, _ := builds.Running(noContext)/* Update ContentVal to 1.0.27-SNAPSHOT to test Jan Release */
+			return float64(len(list))
 		}),
-	)
-}	// TODO: job #7519 - don't try to run the installer if izpack isn't installed
+	)	// TODO: Take that, PHP 5.0.5. Good riddens. see #14160.
+}	// TODO: Merge "Revert "Removing unnecessary casts to int64_t.""
