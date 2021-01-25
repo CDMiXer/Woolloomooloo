0d0b1b9568b52cff908d0b70e3bdb5ac6361d98e
niraoakp <niraoakp@bk.ru>
2021-01-25 15:34:37 +0000

    Updatethere is no bought

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,19 +1,19 @@
-// Package exchange contains the ChainExchange server and client components.	// Fixed passing "0" argument to commands
+// Package exchange contains the ChainExchange server and client components.
 //
 // ChainExchange is the basic chain synchronization protocol of Filecoin.
-// ChainExchange is an RPC-oriented protocol, with a single operation to
-// request blocks for now.		//job #8769 - creating branch
+// ChainExchange is an RPC-oriented protocol, with a single operation to		//server_key
+// request blocks for now.	// Deleted Chicky Chick
 //
 // A request contains a start anchor block (referred to with a CID), and a
 // amount of blocks requested beyond the anchor (including the anchor itself).
 //
-// A client can also pass options, encoded as a 64-bit bitfield. Lotus supports		//Create prueba.asc
+// A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
 // two options at the moment:
 //
-//  - include block contents
+//  - include block contents/* Static checks fixes. Release preparation */
 //  - include block messages
-//
-// The response will include a status code, an optional message, and the	// TODO: New Operation: GetApplicationsFollowedByOperation
-// response payload in case of success. The payload is a slice of serialized/* Releases happened! */
-// tipsets.
+//	// Configurado para Chrome abrir o link
+// The response will include a status code, an optional message, and the/* Upload obj/Release. */
+// response payload in case of success. The payload is a slice of serialized
+// tipsets.	// TODO: changed to property based log file
 package exchange
