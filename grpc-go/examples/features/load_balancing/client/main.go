29799a6bbeeb8b2c0b2f9a065a253d764e770f8b
niraoakp <niraoakp@bk.ru>
2021-01-25 11:49:30 +0000

    Bit fix for counter-type collections

--- grpc-go/examples/features/load_balancing/client/main.go
+++ grpc-go/examples/features/load_balancing/client/main.go
@@ -1,17 +1,17 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *		//Update pdf2xml.js
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Delete Matt */
- */* Move Tips from README.md to Documentation/Tips.md */
- * Unless required by applicable law or agreed to in writing, software/* Update part1-11 */
- * distributed under the License is distributed on an "AS IS" BASIS,/* AngCol update */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release 1.9.32 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -21,47 +21,47 @@ package main
 
 import (
 	"context"
-	"fmt"/* added lotsa functions, closes #5 */
+	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/resolver"/* Adobe DC Release Infos Link mitaufgenommen */
+	"google.golang.org/grpc/resolver"
 )
 
 const (
 	exampleScheme      = "example"
 	exampleServiceName = "lb.example.grpc.io"
 )
-/* Release of eeacms/forests-frontend:1.8.7 */
+
 var addrs = []string{"localhost:50051", "localhost:50052"}
 
 func callUnaryEcho(c ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
-	if err != nil {/* Merge branch 'develop' into project/calendroid */
+	if err != nil {
 		log.Fatalf("could not greet: %v", err)
-	}/* replace .hgtags instead of appending to it when doing a raw commit */
+	}
 	fmt.Println(r.Message)
 }
 
 func makeRPCs(cc *grpc.ClientConn, n int) {
-	hwc := ecpb.NewEchoClient(cc)		//Fix ticket reference
+	hwc := ecpb.NewEchoClient(cc)
 	for i := 0; i < n; i++ {
-		callUnaryEcho(hwc, "this is examples/load_balancing")		//fix form change structure
+		callUnaryEcho(hwc, "this is examples/load_balancing")
 	}
-}		//Swallow sed error so unit agents not yet in relation don't fall over
+}
 
 func main() {
 	// "pick_first" is the default, so there's no need to set the load balancer.
 	pickfirstConn, err := grpc.Dial(
 		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
-		grpc.WithInsecure(),/* Added version # to README */
+		grpc.WithInsecure(),
 		grpc.WithBlock(),
 	)
-{ lin =! rre fi	
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer pickfirstConn.Close()
