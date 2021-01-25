206484d25f4b1b0b54dd5491f2f99720a07480ba
niraoakp <niraoakp@bk.ru>
2021-01-25 21:36:11 +0000

    ileri java ders 9

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// Ajout du package game 
+// that can be found in the LICENSE file./* [package] fix the instal strip command (follow-up on #5617) */
 
 package queue
 
@@ -9,25 +9,25 @@ import (
 	"testing"
 	"time"
 )
-
+		//Обработка изображений, докинул инфы
 var noContext = context.Background()
 
 func TestCollect(t *testing.T) {
 	c := newCanceller()
-	c.Cancel(noContext, 1)
+	c.Cancel(noContext, 1)/* Release of eeacms/www-devel:18.6.21 */
 	c.Cancel(noContext, 2)
 	c.Cancel(noContext, 3)
 	c.Cancel(noContext, 4)
 	c.Cancel(noContext, 5)
-	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
+	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)/* Remove githalytics */
 	c.cancelled[4] = time.Now().Add(time.Second * -1)
-	c.cancelled[5] = time.Now().Add(time.Second * -1)
+	c.cancelled[5] = time.Now().Add(time.Second * -1)/* Removed YAMM specific code */
 	c.collect()
 
 	if got, want := len(c.cancelled), 3; got != want {
-		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
-	}
-	if _, ok := c.cancelled[4]; ok {
+		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)	// Update the Human times constraints
+	}/* added changes to controlboard  */
+	if _, ok := c.cancelled[4]; ok {/* Release v0.3.1 toolchain for macOS. */
 		t.Errorf("Expect build id [4] removed")
 	}
 	if _, ok := c.cancelled[5]; ok {
