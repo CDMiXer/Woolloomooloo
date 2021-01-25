226582e308362be4bcf142ba2959dc72480e542c
niraoakp <niraoakp@bk.ru>
2021-01-25 22:27:01 +0000

    debug statement in motion active handler

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Create flu_style.css
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: BUG: string prefix for raw binary is br, not rb
+	// TODO: will be fixed by ligi@ligi.de
+package queue/* Updating build-info/dotnet/corefx/master for preview6.19257.5 */
 
-package queue
-
-import (
+import (		//comment sensmail for missingpages, fix names too
 	"net/http"
-
+		//No fullscreen (automatic) and move leds-on-screen to the general options
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/handler/api/render"	// TODO: Fix deprecated error
+	"github.com/drone/drone/logger"		//Warcs ready to go to production
 )
 
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		ctx := r.Context()	// Plugin Update to 1.0.2 - Encoding fixes
 		err := scheduler.Resume(ctx)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)	// TODO: hacked by qugou1350636@126.com
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: cannot resume scheduler")
+				Errorln("api: cannot resume scheduler")		//Updated committees.
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
