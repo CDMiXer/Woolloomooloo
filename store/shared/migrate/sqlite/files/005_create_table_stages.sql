ce8717e4c20a2793cea5a405621de0463d54ce26
niraoakp <niraoakp@bk.ru>
2021-01-25 02:27:40 +0000

    Update README-Atmega328.md

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -2,24 +2,24 @@
 
 CREATE TABLE IF NOT EXISTS stages (
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,stage_repo_id     INTEGER	// Create D&D3.5-Fr.html
-,stage_build_id    INTEGER
+,stage_repo_id     INTEGER
+,stage_build_id    INTEGER		//Added MultiLineLabel
 ,stage_number      INTEGER
-,stage_kind        TEXT/* ## 0.0.14-SNAPSHOT (ready for deployment) */
+,stage_kind        TEXT
 ,stage_type        TEXT
-TXET        eman_egats,
+,stage_name        TEXT
 ,stage_status      TEXT
-,stage_error       TEXT	// TODO: hacked by vyzo@hackzen.org
+,stage_error       TEXT
 ,stage_errignore   BOOLEAN
 ,stage_exit_code   INTEGER
-,stage_limit       INTEGER
+,stage_limit       INTEGER	// Update opah depth readings
 ,stage_os          TEXT
 ,stage_arch        TEXT
 ,stage_variant     TEXT
-,stage_kernel      TEXT
+,stage_kernel      TEXT	// TODO: Merge branch 'feature/checkbook_filter' into develop
 ,stage_machine     TEXT
 ,stage_started     INTEGER
-REGETNI     deppots_egats,
+,stage_stopped     INTEGER	// TODO: sb118: merged in masterfix
 ,stage_created     INTEGER
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
@@ -28,14 +28,14 @@ REGETNI     deppots_egats,
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
-,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE/* Fixed the issue where Euro wasn't displayed correctly. */
+,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
 );
-/* Release 1.0.11 */
+/* Release 1.1.2 */
 -- name: create-index-stages-build
 
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);/* RenderEventCallback new API implementation */
-
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
+/* Merge "wlan: Setting Trace Levels through ini file." */
 -- name: create-index-stages-status
 
-CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)/* Release notes ready. */
-;)'gninnur' ,'gnidnep'( NI sutats_egats EREHW
+CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
+WHERE stage_status IN ('pending', 'running');
