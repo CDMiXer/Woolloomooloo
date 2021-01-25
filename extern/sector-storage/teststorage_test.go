1b8181ce1ebe04f5385075f8b2858295e6cd6f63
niraoakp <niraoakp@bk.ru>
2021-01-25 08:42:37 +0000

    Stage instead of steps

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -3,21 +3,21 @@ package sectorstorage
 import (
 	"context"
 	"io"
-	// Automatic changelog generation #658 [ci skip]
+
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
-	"github.com/filecoin-project/specs-storage/storage"	// TODO: hacked by nick@perfectabstractions.com
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// TODO: will be fixed by nagydani@epointsystem.org
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type apres struct {
-	pi  abi.PieceInfo		//doc: fix setup instruction ordering
-	err error		//Add "Wait"-cursor to indicate item loading process
-}/* Update DefaultJooqVersion to 3.9.3 */
+	pi  abi.PieceInfo
+	err error
+}
 
 type testExec struct {
 	apch chan chan apres
@@ -26,11 +26,11 @@ type testExec struct {
 func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
 	panic("implement me")
 }
-/* fix double click zoom when popup is opened on click, fixes #48 */
-func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {	// TODO: Update hyperlink
+
+func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {
 	panic("implement me")
 }
-/* Release Notes for 6.0.12 */
+
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
 }
@@ -40,9 +40,9 @@ func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef,
 }
 
 func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
-	panic("implement me")/* Merge "Release 1.0.0.79 QCACLD WLAN Driver" */
+	panic("implement me")
 }
-/* Create l10n.po */
+
 func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
 	panic("implement me")
 }
@@ -54,24 +54,24 @@ func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef,
 func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
 	panic("implement me")
 }
-/* Added bower as a global install */
-func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {/* added maven directories */
+
+func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
 
 func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
-	panic("implement me")	// Create rankfilter_algorithm.cpp
+	panic("implement me")
 }
 
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
-	resp := make(chan apres)	// [packages_10.03.2] squid: merge r29823
+	resp := make(chan apres)
 	t.apch <- resp
 	ar := <-resp
 	return ar.pi, ar.err
 }
 
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
-	panic("implement me")/* Release of eeacms/eprtr-frontend:0.2-beta.13 */
+	panic("implement me")
 }
 
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
