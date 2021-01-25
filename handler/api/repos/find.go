7b69284cdd88dede3b1785ccadff21ca09dd82f9
niraoakp <niraoakp@bk.ru>
2021-01-25 09:46:07 +0000

    a try to center things with css

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
-//	// made the handshake timeout configurable and defaults to 10 seconds
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* mention boolean fix by @igagnidz */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: added EVAL built-in operation for dynamic expression evaluation
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos/* Release the GIL in all Request methods */
-	// Changed Spin Code to WHATSNEW
-import (
+package repos		//buildpack -> buildpacks
+/* Merge branch 'master' into less-warnings */
+import (/* Solo Me Falta Un Ejercicio de los Optativos */
 	"net/http"
-
-	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by boringland@protonmail.ch
+	// TODO: Improve DateTimeParamConverter documentation
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
 
 // HandleFind returns an http.HandlerFunc that writes the
-// json-encoded repository details to the response body./* reset to Release build type */
+// json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
