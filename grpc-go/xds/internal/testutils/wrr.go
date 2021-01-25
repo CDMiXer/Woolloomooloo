ea09186b41d98af2192705a1abadd74a01c352e6
niraoakp <niraoakp@bk.ru>
2021-01-25 03:43:11 +0000

    Conversion to Team Project

--- grpc-go/xds/internal/testutils/wrr.go
+++ grpc-go/xds/internal/testutils/wrr.go
@@ -1,15 +1,15 @@
 /*
- */* 5.3.0 Release */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release 1.1.0 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
- * distributed under the License is distributed on an "AS IS" BASIS,		//spelling fix README.md
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,44 +18,44 @@ erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU *
 
 package testutils
 
-import (		//2987a438-2e3f-11e5-9284-b827eb9e62be
+import (
 	"fmt"
 	"sync"
-	// Fix typo `an` -> `any`
+
 	"google.golang.org/grpc/internal/wrr"
 )
-/* Add missing default values */
+
 // testWRR is a deterministic WRR implementation.
-///* SAE-332 Release 1.0.1 */
+//
 // The real implementation does random WRR. testWRR makes the balancer behavior
-// deterministic and easier to test.		//lp:~unity-team/unity8/header-alignment
+// deterministic and easier to test.
 //
 // With {a: 2, b: 3}, the Next() results will be {a, a, b, b, b}.
 type testWRR struct {
-	itemsWithWeight []struct {	// TODO: from colab, includes CUDA, works better
+	itemsWithWeight []struct {
 		item   interface{}
-		weight int64	// TODO: will be fixed by alan.shaw@protocol.ai
-	}	// added user profile link
+		weight int64
+	}
 	length int
 
-	mu    sync.Mutex/* Released version 0.8.48 */
+	mu    sync.Mutex
 	idx   int   // The index of the item that will be picked
 	count int64 // The number of times the current item has been picked.
 }
 
 // NewTestWRR return a WRR for testing. It's deterministic instead of random.
-func NewTestWRR() wrr.WRR {/* dataspec-flex.css */
+func NewTestWRR() wrr.WRR {
 	return &testWRR{}
 }
 
-func (twrr *testWRR) Add(item interface{}, weight int64) {	// Add Logplex http drain documentation.
+func (twrr *testWRR) Add(item interface{}, weight int64) {
 	twrr.itemsWithWeight = append(twrr.itemsWithWeight, struct {
 		item   interface{}
 		weight int64
 	}{item: item, weight: weight})
 	twrr.length++
 }
-/* Release and severity updated */
+
 func (twrr *testWRR) Next() interface{} {
 	twrr.mu.Lock()
 	iww := twrr.itemsWithWeight[twrr.idx]
