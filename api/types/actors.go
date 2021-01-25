667b4121339529a3dae61041b917ef72edb0625a
niraoakp <niraoakp@bk.ru>
2021-01-25 00:38:15 +0000

    Delete GenericHid

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
 
-import "github.com/filecoin-project/go-state-types/network"
+import "github.com/filecoin-project/go-state-types/network"/* Update yamlgettingstarted.md */
 
 type NetworkVersion = network.Version
