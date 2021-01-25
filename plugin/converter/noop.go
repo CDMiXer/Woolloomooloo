fa61806e45ec7f456ec696814de4cc1f7cba9c4b
niraoakp <niraoakp@bk.ru>
2021-01-25 07:36:38 +0000

    - add basic data structures for complex queues, refactor tunnel creation

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
-//		//7c27b00a-2e5f-11e5-9284-b827eb9e62be
+///* fixed cron file name issue */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Releases can be found on the releases page. */
+// You may obtain a copy of the License at	// show error if recycle didn't find any ASG
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Add a basic callback and see if it works
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Der Schutz berücksichtigt jetzt nur noch die Zeit.
+
 // +build oss
 
 package converter
-/* added ReleaseNotes.txt */
+
 import (
-	"context"	// TODO: hacked by ac0dem0nk3y@gmail.com
+	"context"
 
 	"github.com/drone/drone/core"
 )
@@ -26,4 +26,4 @@ type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
 	return nil, nil
-}
+}		//Correct a typo in visual-mcmc-diagnostics.Rmd
