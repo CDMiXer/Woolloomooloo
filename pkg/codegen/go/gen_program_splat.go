278047e7526fa5d8fb1742ec864e3fc18c0ff179
niraoakp <niraoakp@bk.ru>
2021-01-25 04:35:43 +0000

    This was plain wrong - works now!

--- pkg/codegen/go/gen_program_splat.go
+++ pkg/codegen/go/gen_program_splat.go
@@ -1,35 +1,35 @@
-package gen
+package gen/* deps: update bson@0.5.7 */
 
 import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclsyntax"/* Removed tests for 0.8 */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
-
+/* Merge "Remove skips in test server rescue" */
 type splatTemp struct {
 	Name  string
 	Value *model.SplatExpression
 }
 
 func (st *splatTemp) Type() model.Type {
-	return st.Value.Type()	// fixed resource installation/finding under linux
+	return st.Value.Type()
 }
-
+		//Add Vadim's email to `package.json`
 func (st *splatTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return st.Type().Traverse(traverser)
 }
 
 func (st *splatTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
-}
+}		//docs: update node.js version in local development
 
 type splatSpiller struct {
-	temps []*splatTemp
+	temps []*splatTemp		//Delete konzepteschulspezifisch
 	count int
-}		//refactored leave request views (prepairing for issue #203) 
+}
 
 func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *splatTemp
@@ -37,27 +37,27 @@ func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, h
 	case *model.SplatExpression:
 		temp = &splatTemp{
 			Name:  fmt.Sprintf("splat%d", ss.count),
-			Value: x,
-		}		//bodyHandlers
+			Value: x,/* Merge "Migrate cloud image URL/Release options to DIB_." */
+		}
 		ss.temps = append(ss.temps, temp)
 		ss.count++
 	default:
 		return x, nil
-	}
+	}/* [artifactory-release] Release version 0.7.12.RELEASE */
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
-		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
+		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},		//Jörg Dietrich: add mta
 		Parts:     []model.Traversable{temp},
-lin ,}	
-}
+	}, nil/* Ch09: Removed disable speculative execution. */
+}	// TODO: save/load implemented, testing
 
-func (g *generator) rewriteSplat(	// TODO: hacked by greg@colvin.org
-	x model.Expression,/* improved impl of refactoring participants */
+func (g *generator) rewriteSplat(
+	x model.Expression,
 	spiller *splatSpiller,
 ) (model.Expression, []*splatTemp, hcl.Diagnostics) {
-	spiller.temps = nil	// TODO: fix #3903 by producing a nicer error message
-	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
+	spiller.temps = nil
+	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)		//Removed deprecated getKeySet method.
 
-	return x, spiller.temps, diags/* Restore azure and aws testing. */
+	return x, spiller.temps, diags	// d77e64c0-2e6f-11e5-9284-b827eb9e62be
 
-}
+}/* 4.4.0 Release */
