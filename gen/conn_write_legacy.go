d9bbc8be33a7835d4399fc53500280187f212505
niraoakp <niraoakp@bk.ru>
2021-01-25 14:10:39 +0000

    Fixed broken link [ci skip]

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Update Release scripts */
+// license that can be found in the LICENSE file.
 
 // +build !go1.8
 
-package websocket		//Update gomme-vel
+package websocket
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
-	for _, buf := range bufs {	// TODO: hacked by indexxuan@gmail.com
+	for _, buf := range bufs {		//0c50b06e-2e42-11e5-9284-b827eb9e62be
 		if len(buf) > 0 {
-			if _, err := c.conn.Write(buf); err != nil {
+			if _, err := c.conn.Write(buf); err != nil {/* Released 0.9.02. */
 				return err
 			}
-		}
-	}/* BUILD: Fix Release makefile problems, invalid path to UI_Core and no rm -fr  */
+		}/* Add myself to the list of contributors */
+	}/* Merge branch 'b0.21.0' into mplebanski/text_nitpicks */
 	return nil
-}	// Worked on GC
+}
