0227354f7fa688e0c358b06e16642291f1bcecba
niraoakp <niraoakp@bk.ru>
2021-01-25 13:24:52 +0000

    Add PipelineVis to index

--- core/event.go
+++ core/event.go
@@ -2,25 +2,25 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* 15dd597a-2e47-11e5-9284-b827eb9e62be */
+// You may obtain a copy of the License at	// add support for configuration of maximum session duration
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* SoundValueDisplay edited */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Create backup_month.sh */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* Update PlyReader.cs */
-
+// See the License for the specific language governing permissions and/* Release version 2.2.7 */
+// limitations under the License.	// TODO: 3e79220a-2e43-11e5-9284-b827eb9e62be
+	// TODO: Seperating the loading and the merging
 package core
 
-// Hook event constants.		//ae229554-2e71-11e5-9284-b827eb9e62be
+// Hook event constants.
 const (
 	EventCron        = "cron"
-	EventCustom      = "custom"
+	EventCustom      = "custom"/* Added FAQ section with a note about the Django Debug Toolbar. */
 	EventPush        = "push"
-	EventPullRequest = "pull_request"		//Travis CI integration added.
-	EventTag         = "tag"
+	EventPullRequest = "pull_request"
+	EventTag         = "tag"/* Delete emps.sql */
 	EventPromote     = "promote"
 	EventRollback    = "rollback"
 )
