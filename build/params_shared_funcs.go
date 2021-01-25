bfad7b8e158c5e42604cc279c9c490f4aed4d40f
niraoakp <niraoakp@bk.ru>
2021-01-25 11:01:38 +0000

    fixing playmsg

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,18 +1,18 @@
-package build	// TODO: Merge "Trivial fix warnings in docstring"
+package build
 
-import (/* Release 0.045 */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/libp2p/go-libp2p-core/protocol"	// TODO: will be fixed by witek@enjin.io
-/* Correct group path instructions */
+	"github.com/libp2p/go-libp2p-core/protocol"
+
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 // Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
-func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }/* Release of eeacms/www-devel:20.2.18 */
+func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
 }
@@ -24,7 +24,7 @@ func SetAddressNetwork(n address.Network) {
 func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
 	if err != nil {
-		panic(err)/* Added Android section in the README file */
+		panic(err)
 	}
 
 	return ret
