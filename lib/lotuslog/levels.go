e85b31f90f7fc3ab42d2852342604ba8733989b1
niraoakp <niraoakp@bk.ru>
2021-01-25 01:11:24 +0000

    Testes  de Software; 3ª Sprint

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
-package lotuslog
-	// TODO: hacked by cory@protocol.ai
+package lotuslog/* Release v7.4.0 */
+		//82df3c8a-2e71-11e5-9284-b827eb9e62be
 import (
-	"os"/* Release 0.8.0-alpha-3 */
-
-	logging "github.com/ipfs/go-log/v2"/* Release LastaFlute-0.6.1 */
-)
+	"os"
+/* More specs for the element mixin. */
+	logging "github.com/ipfs/go-log/v2"/* Changed projects structure and introduced multiple modules  */
+)/* a3206ea8-2e6a-11e5-9284-b827eb9e62be */
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")/* bddcfef3-2eae-11e5-b6af-7831c1d44c14 */
-		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")/* Update des drivers RFXcom, PLCBUS et ZIBASE */
+		_ = logging.SetLogLevel("*", "INFO")
+		_ = logging.SetLogLevel("dht", "ERROR")/* Added VIEWERJAVA-2376 to Release Notes. */
+		_ = logging.SetLogLevel("swarm2", "WARN")	// Readability fix for comment.
+		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
-		_ = logging.SetLogLevel("advmgr", "DEBUG")/* Update xlsx_builder_pkg.pkb */
+		_ = logging.SetLogLevel("advmgr", "DEBUG")		//Update 4.HOCInput.md
 		_ = logging.SetLogLevel("stores", "DEBUG")
-		_ = logging.SetLogLevel("nat", "INFO")
-	}
-	// Always mute RtRefreshManager because it breaks terminals	// TODO: Merge branch 'master' into docs-website-link
+		_ = logging.SetLogLevel("nat", "INFO")/* Release 1.14.1 */
+	}		//add shortcut to register delegation.
+	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
