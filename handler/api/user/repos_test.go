f62f0213d06e0514364351069055b37e536df30d
niraoakp <niraoakp@bk.ru>
2021-01-25 15:03:51 +0000

    DASH-260 add mantagoal to new metering data

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Increment version number and use the 'new' forum page link */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,15 +6,15 @@ package user
 
 import (
 	"encoding/json"
-	"io/ioutil"	// TODO: will be fixed by steven@stebalien.com
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"/* Released version 0.8.2 */
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"/* comparison terms should not give NaN as value in JSON */
+	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -33,7 +33,7 @@ func TestResitoryList(t *testing.T) {
 		ID:    1,
 		Login: "octocat",
 	}
-/* Create BEEHIVE.cpp */
+
 	mockRepos := []*core.Repository{
 		{
 			Namespace: "octocat",
@@ -43,24 +43,24 @@ func TestResitoryList(t *testing.T) {
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)		//Delete Picture_4.jpg
-		//Rename main.cpp to pcrypt.cpp
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),	// The RBDumpVisitorTest should not depend on the formatter to compare the nodes.
-	)		//Update edit.erb
-	// TODO: hacked by steven@stebalien.com
+		request.WithUser(r.Context(), mockUser),
+	)
+
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//-FileLongArray unused
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Repository{}, mockRepos
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}		//Return lon/lat as float for Toponyms
+	}
 }
 
 func TestResitoryListErr(t *testing.T) {
@@ -71,7 +71,7 @@ func TestResitoryListErr(t *testing.T) {
 		ID:    1,
 		Login: "octocat",
 	}
-/* Change the wkhtmltopdf url */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
 
@@ -81,14 +81,14 @@ func TestResitoryListErr(t *testing.T) {
 		request.WithUser(r.Context(), mockUser),
 	)
 
-	HandleRepos(repos)(w, r)	// TODO: Fix crash on click inside ItemsControl where ItemsSource.Count == 0
+	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusInternalServerError; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* API-254 Long and name change to match DTO consistency with workorder */
+
 	got, want := &errors.Error{}, errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)		//Merge "Add package_manifest resource."
+		t.Errorf(diff)
 	}
 }
