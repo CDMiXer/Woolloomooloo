4b5ac3743ec10ee000d140602278a7c64d208d27
niraoakp <niraoakp@bk.ru>
2021-01-25 09:24:26 +0000

    Did a clean clutter

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -5,31 +5,31 @@ import (
 	"time"
 
 	"k8s.io/apimachinery/pkg/labels"
-/* That didn't work. Figures. */
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
 
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// Extensions. Fix for Bug #960046 (merging text).
+)
+		//Update SoftMethodStorage.java
 var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}
 
 type nullWorkflowArchive struct {
-}
+}/* Merge "[DVP Display] Release dequeued buffers during free" */
 
-func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {	// TODO: hacked by sbrichards@gmail.com
-	return nil	// Optimized the function calling system
+func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
+	return nil
 }
 
 func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
-	return wfv1.Workflows{}, nil		//Add submission instructions under the list of projects.
+	return wfv1.Workflows{}, nil
 }
 
-func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {		//add <EOL> in `:config-write-py` generated file
+func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {
 	return nil, fmt.Errorf("getting archived workflows not supported")
 }
 
 func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
-	return fmt.Errorf("deleting archived workflows not supported")
+	return fmt.Errorf("deleting archived workflows not supported")	// TODO: will be fixed by onhardev@bk.ru
 }
 
 func (r *nullWorkflowArchive) DeleteExpiredWorkflows(time.Duration) error {
 	return nil
-}
+}		//Merge branch 'develop' into bugfix/2940
