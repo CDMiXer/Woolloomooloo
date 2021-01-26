a3c32265f156870e9b4dffa1980183f9673a7e2e
niraoakp <niraoakp@bk.ru>
2021-01-26 05:35:47 +0000

    o Changed indentation to 2 spaces per level to match usual POM formatting

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
-
+	// TODO: hacked by xiemengjun@gmail.com
 type FsStat struct {
-	Capacity    int64
+	Capacity    int64		//Added new_pod_repository for PodToBUILD
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem
+	FSAvailable int64 // Available in the filesystem	// Add configuration for Clock. "java" cron does not work for now
 	Reserved    int64
-
-	// non-zero when storage has configured MaxStorage
+	// TODO: 0.4 is out.
+	// non-zero when storage has configured MaxStorage	// shm/shm: add method CalcMemoryMapSize()
 	Max  int64
 	Used int64
 }
