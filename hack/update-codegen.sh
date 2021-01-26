bb031e814786bd71ea105580d7130ec55f5984cc
niraoakp <niraoakp@bk.ru>
2021-01-26 11:37:04 +0000

    Delete bridesmaid3.jpg

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
-/* make file working */
+
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
-  workflow:v1alpha1 \	// TODO: Chat system added
+  workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
