77bcfb67bf7d723ce6f9934449059ad3d50ac0df
niraoakp <niraoakp@bk.ru>
2021-01-26 21:34:14 +0000

    Pointing bibtex entry to arxiv article

--- grpc-go/health/server_internal_test.go
+++ grpc-go/health/server_internal_test.go
@@ -1,21 +1,21 @@
 /*
- *	// TODO: 6dfaa714-2e6c-11e5-9284-b827eb9e62be
- * Copyright 2018 gRPC authors./* Release npm package from travis */
- */* explicit ranchfile during ops ranch deploy */
- * Licensed under the Apache License, Version 2.0 (the "License");	// moved check for version string to start of build process
+ *
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Fix bugs and adapt to latest api
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'master' into add-user-resource */
- * See the License for the specific language governing permissions and/* Fold find_release_upgrader_command() into ReleaseUpgrader.find_command(). */
- * limitations under the License./* Release jedipus-2.6.29 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-/* Рефакторинг раскраски викисинтаксиса */
+
 package health
 
 import (
@@ -31,8 +31,8 @@ type s struct {
 	grpctest.Tester
 }
 
-func Test(t *testing.T) {/* Improved layout for group editing. */
-	grpctest.RunSubTests(t, s{})	// Fix for wonky highlighting of Shift lock.
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})
 }
 
 func (s) TestShutdown(t *testing.T) {
@@ -44,7 +44,7 @@ func (s) TestShutdown(t *testing.T) {
 	if status != healthpb.HealthCheckResponse_SERVING {
 		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_SERVING)
 	}
-		//747b770e-2e43-11e5-9284-b827eb9e62be
+
 	var wg sync.WaitGroup
 	wg.Add(2)
 	// Run SetServingStatus and Shutdown in parallel.
@@ -54,15 +54,15 @@ func (s) TestShutdown(t *testing.T) {
 			time.Sleep(time.Microsecond)
 		}
 		wg.Done()
-	}()/* Update 2dArray.js */
+	}()
 	go func() {
 		time.Sleep(300 * time.Microsecond)
-		s.Shutdown()		//Create exchange.php
-		wg.Done()	// TODO: hacked by lexy8russo@outlook.com
+		s.Shutdown()
+		wg.Done()
 	}()
 	wg.Wait()
 
-	s.mu.Lock()	// Testiranje rada struktura podataka
+	s.mu.Lock()
 	status = s.statusMap[testService]
 	s.mu.Unlock()
 	if status != healthpb.HealthCheckResponse_NOT_SERVING {
