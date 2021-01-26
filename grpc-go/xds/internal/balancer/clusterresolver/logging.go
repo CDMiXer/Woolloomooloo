da1f06146dfa127c89033b35e4066b6a0e0641ca
niraoakp <niraoakp@bk.ru>
2021-01-26 02:14:54 +0000

    eclipse pydev project files

--- grpc-go/xds/internal/balancer/clusterresolver/logging.go
+++ grpc-go/xds/internal/balancer/clusterresolver/logging.go
@@ -1,32 +1,32 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Add TreatmentCategorization to Category (story #537) */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Removes unused command. */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Updated README.md so it is converted correctly */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by seth@sethvargo.com
  * limitations under the License.
- *
+ */* implement Disposable HQ */
  */
 
 package clusterresolver
 
 import (
 	"fmt"
-
-	"google.golang.org/grpc/grpclog"
+	// TODO: hacked by davidad@alum.mit.edu
+	"google.golang.org/grpc/grpclog"		//stabilizing a few randomized tests some more
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[xds-cluster-resolver-lb %p] "
-
+	// TODO: Added grunt file for deployment
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *clusterResolverBalancer) *internalgrpclog.PrefixLogger {
