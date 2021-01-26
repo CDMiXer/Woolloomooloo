e9147b6cf67e9bad2d9078b39dc9f69894aa8f5f
niraoakp <niraoakp@bk.ru>
2021-01-26 16:10:57 +0000

    locale fixes for OpenKODE

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,8 +1,8 @@
 package build
-
+	// TODO: add italian index
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)/* Change es6 shorthand notation to es5 notation */
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
