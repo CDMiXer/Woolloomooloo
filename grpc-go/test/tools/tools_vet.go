8a7e4da00a557506d95ee12e81a59f31abb4b7d5
niraoakp <niraoakp@bk.ru>
2021-01-26 08:48:48 +0000

    Added possibility to instantiate ImdbInfo with imdbSite

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,7 +1,7 @@
 /*
- *
+ *		//added player 2 - gerhard
  * Copyright 2021 gRPC authors.
- *
+* 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,12 +10,12 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update ok.svg
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: d42b7cae-327f-11e5-bde0-9cf387a8033e
  *
  */
 
 // Package tools is used to pin specific versions of external tools in this
 // module's go.mod that gRPC uses for internal testing.
-package tools
+package tools/* Release: 6.0.2 changelog */
