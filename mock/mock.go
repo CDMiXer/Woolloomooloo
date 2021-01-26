dfb13b3e9f1cb47949c50b2b18e859b07f1783f5
niraoakp <niraoakp@bk.ru>
2021-01-26 22:41:46 +0000

    New translations media-gallery.json (Spanish)

--- mock/mock.go
+++ mock/mock.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Added host name to exceptions log message (BILLRUN-470)
 // that can be found in the LICENSE file.
 
 // +build !oss
