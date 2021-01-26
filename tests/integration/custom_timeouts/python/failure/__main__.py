cbc066a85c83004fc27fb3d9d351f209ab186038
niraoakp <niraoakp@bk.ru>
2021-01-26 06:26:47 +0000

    Merge "[INTERNAL][TEST] sap.ui.integration: Add QUnit tests for Card Editor"

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,12 +1,12 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 4.0.27-dev Release */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
-class Resource1(ComponentResource):/* [artifactory-release] Release version 2.2.0.M2 */
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Attempt to create a resource with a CustomTimeout that should fail
+# Attempt to create a resource with a CustomTimeout that should fail	// fix issue 269
 res5 = Resource1("res5",
     opts=ResourceOptions(custom_timeouts='asdf')
 )
