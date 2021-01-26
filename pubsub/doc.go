07c42a1e6690081bfd4a7417bd2614e32e5ff48e
niraoakp <niraoakp@bk.ru>
2021-01-26 14:08:28 +0000

    Create Problem 8 - Largest product in a series.py

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 3.2.3.331 Prima WLAN Driver" */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release version: 1.2.2 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// Create 01c-french.md
 // Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
 // multiple subscribers.
-package pubsub
+package pubsub/* Merge remote-tracking branch 'origin/development' into feature/INFUND-2604 */
