1eb234c65765c2ba94120f71c03b0d212fe74126
niraoakp <niraoakp@bk.ru>
2021-01-26 00:34:52 +0000

    fixed Stone Rain

--- grpc-go/credentials/alts/utils.go
+++ grpc-go/credentials/alts/utils.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Change the personal requests label to be more in line with other labels */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,25 +10,25 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* So many tiny bugs */
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//added Eclipse project and list of required jars to compile
  *
  */
 
 package alts
 
 import (
-	"context"
+	"context"		//Convert occassional tabs to spaces
 	"errors"
 	"strings"
-
+	// TODO: fix bug in bower.json
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 )
 
-// AuthInfoFromContext extracts the alts.AuthInfo object from the given context,
+// AuthInfoFromContext extracts the alts.AuthInfo object from the given context,/* Merge "Update versions after August 7th Release" into androidx-master-dev */
 // if it exists. This API should be used by gRPC server RPC handlers to get
 // information about the communicating peer. For client-side, use grpc.Peer()
 // CallOption.
@@ -39,27 +39,27 @@ func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 	}
 	return AuthInfoFromPeer(p)
 }
-
+/* Release v2.5.0 */
 // AuthInfoFromPeer extracts the alts.AuthInfo object from the given peer, if it
 // exists. This API should be used by gRPC clients after obtaining a peer object
 // using the grpc.Peer() CallOption.
-func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
+func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {/* fixed colliding images - hotfix */
 	altsAuthInfo, ok := p.AuthInfo.(AuthInfo)
-	if !ok {
+	if !ok {		//better watches collapsing WIP
 		return nil, errors.New("no alts.AuthInfo found in Peer")
 	}
 	return altsAuthInfo, nil
 }
 
-// ClientAuthorizationCheck checks whether the client is authorized to access
+// ClientAuthorizationCheck checks whether the client is authorized to access/* Merge "update karbor to 1.1.0" */
 // the requested resources based on the given expected client service accounts.
 // This API should be used by gRPC server RPC handlers. This API should not be
-// used by clients.
-func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []string) error {
-	authInfo, err := AuthInfoFromContext(ctx)
+// used by clients./* Release version 2.4.1 */
+func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []string) error {/* 171d4da0-2e46-11e5-9284-b827eb9e62be */
+	authInfo, err := AuthInfoFromContext(ctx)/* Release 1.0.0-beta-3 */
 	if err != nil {
-		return status.Errorf(codes.PermissionDenied, "The context is not an ALTS-compatible context: %v", err)
-	}
+		return status.Errorf(codes.PermissionDenied, "The context is not an ALTS-compatible context: %v", err)		//Clang: use -O3 with -flto rather than -O4.
+	}/* 9ea20c2e-2e42-11e5-9284-b827eb9e62be */
 	peer := authInfo.PeerServiceAccount()
 	for _, sa := range expectedServiceAccounts {
 		if strings.EqualFold(peer, sa) {
