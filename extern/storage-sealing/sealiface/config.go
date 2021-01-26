b9866f46cc1d2c4ba8b75c6e94d33b6bfcb23dd4
niraoakp <niraoakp@bk.ru>
2021-01-26 13:22:41 +0000

    add gitter webhooks

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -5,15 +5,15 @@ import "time"
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
-	// 0 = no limit	// TODO: Improved: Scanning the dial heap's cost table is faster now
-	MaxWaitDealsSectors uint64		//Making some small grammatical changes.
-/* Updating CassandraApplicationRepository to delegate Row Mapping */
+	// 0 = no limit
+	MaxWaitDealsSectors uint64
+
 	// includes failed, 0 = no limit
-	MaxSealingSectors uint64/* Release of eeacms/www:18.12.5 */
-/* Merge "Release notes for implied roles" */
-	// includes failed, 0 = no limit	// TODO: will be fixed by steven@stebalien.com
+	MaxSealingSectors uint64
+
+	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
-	// Added GPL Header
+
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
