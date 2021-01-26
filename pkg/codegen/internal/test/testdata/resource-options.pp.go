19dfca2e145fc6f3f420f15d2d634d719fcca18e
niraoakp <niraoakp@bk.ru>
2021-01-26 19:45:19 +0000

    gitignore dosyası eklendi

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,28 +1,28 @@
 package main
-/* rev 833987 */
+
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)/* Release version 1.0.0.M3 */
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
-			Region: pulumi.String("us-west-2"),
+		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{	// TODO: - Made chat bigger by default
+			Region: pulumi.String("us-west-2"),/* tweaked slovak TTS voice commands */
 		})
 		if err != nil {
 			return err
 		}
-		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{/* # deleted quickstart from site docs */
-			provider,	// TODO: faq: fix typo
+		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
+			provider,
 		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
 			"bucket",
 			"lifecycleRules[0]",
 		}))
-		if err != nil {
+		if err != nil {		//Minor translation edit (fr)
 			return err
-		}
+		}	// TODO: will be fixed by nagydani@epointsystem.org
 		return nil
 	})
 }
