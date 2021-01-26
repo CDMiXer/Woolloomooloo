3a495cf561ff75da85080f79c0a1df0d13dce54f
niraoakp <niraoakp@bk.ru>
2021-01-26 06:39:28 +0000

    Clean up to get rid of PEP8 complains

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -5,17 +5,17 @@ const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
-    name: resourceGroupNameParam,/* Update mavenAutoRelease.sh */
-});		//Updated badge link URLs
+    name: resourceGroupNameParam,
+});
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
-    name: storageAccountNameParam,	// TODO: Forgot to enable lzma compression again.
+    name: storageAccountNameParam,
     accountKind: "StorageV2",
     location: locationParam,
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
-    accountReplicationType: storageAccountTypeReplicationParam,		//Added parser support for IAR and PcLint warnings.
+    accountReplicationType: storageAccountTypeReplicationParam,
 });
 export const storageAccountNameOut = storageAccountResource.name;
