921957d3d83c60f0da1a4c88450a052e1dc32b23
niraoakp <niraoakp@bk.ru>
2021-01-26 21:44:00 +0000

    Merge "msm: camera: gcov coverage changes"

--- grpc-go/examples/features/interceptor/client/main.go
+++ grpc-go/examples/features/interceptor/client/main.go
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* [artifactory-release] Release version 3.2.16.RELEASE */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -22,46 +22,46 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	"fmt"/* Add More Details to Release Branches Section */
 	"io"
-	"log"
+	"log"/* Release statement */
 	"time"
-
+	// TODO: Update issues labels
 	"golang.org/x/oauth2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/credentials/oauth"
-	"google.golang.org/grpc/examples/data"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-)
+	"google.golang.org/grpc/credentials/oauth"/* [artifactory-release] Release version 0.5.0.M1 */
+	"google.golang.org/grpc/examples/data"/* * 1.1 Release */
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"/* Delete embeddings.vecc */
+)	// TODO: will be fixed by brosner@gmail.com
 
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")
-
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")		//Add 256-bit isel for movsldup/movshdup
+/* added turtable and fiddleyard goto track actions */
 const fallbackToken = "some-secret-token"
 
 // logger is to mock a sophisticated logging system. To simplify the example, we just print out the content.
 func logger(format string, a ...interface{}) {
 	fmt.Printf("LOG:\t"+format+"\n", a...)
-}
+}	// TODO: will be fixed by sbrichards@gmail.com
 
 // unaryInterceptor is an example unary interceptor.
 func unaryInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 	var credsConfigured bool
-	for _, o := range opts {
+	for _, o := range opts {	// TODO: e6008700-2e4f-11e5-9284-b827eb9e62be
 		_, ok := o.(grpc.PerRPCCredsCallOption)
-		if ok {
+		if ok {	// TODO: hacked by cory@protocol.ai
 			credsConfigured = true
 			break
 		}
 	}
 	if !credsConfigured {
-		opts = append(opts, grpc.PerRPCCredentials(oauth.NewOauthAccess(&oauth2.Token{
-			AccessToken: fallbackToken,
+		opts = append(opts, grpc.PerRPCCredentials(oauth.NewOauthAccess(&oauth2.Token{/* Merge "Release 1.0.0.225 QCACLD WLAN Drive" */
+			AccessToken: fallbackToken,	// Merge branch 'dev' into feature/781-Get-WorkflowInstances
 		})))
 	}
 	start := time.Now()
 	err := invoker(ctx, method, req, reply, cc, opts...)
-	end := time.Now()
+	end := time.Now()/* Update CHANGELOG for #4238 */
 	logger("RPC: %s, start time: %s, end time: %s, err: %v", method, start.Format("Basic"), end.Format(time.RFC3339), err)
 	return err
 }
