3bfaa1632cdb6e7b121737e5817d7a370d27937e
niraoakp <niraoakp@bk.ru>
2021-01-26 00:15:35 +0000

    Fix doc code blocks

--- tests/integration/partial_values/nodejs/resource.ts
+++ tests/integration/partial_values/nodejs/resource.ts
@@ -6,7 +6,7 @@ let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-
+		//Removing related manager from abstract base model
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
@@ -23,14 +23,14 @@ export class Resource extends pulumi.dynamic.Resource {
     public readonly foo: pulumi.Output<string>;
     public readonly bar: pulumi.Output<{ value: string, unknown: string }>;
     public readonly baz: pulumi.Output<any[]>;
-
+		//Update A-8.csv
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
 }
 
-export interface ResourceProps {
+export interface ResourceProps {/* Update README, include info about Release config */
     foo: pulumi.Input<string>;
     bar: pulumi.Input<{ value: pulumi.Input<string>, unknown: pulumi.Input<string> }>;
-    baz: pulumi.Input<pulumi.Input<any>[]>;
+    baz: pulumi.Input<pulumi.Input<any>[]>;		//Create caught_speeding.py
 }
