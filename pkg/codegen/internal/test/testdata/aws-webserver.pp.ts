58579cb6984a1562b81925a9697c10af5e2a7816
niraoakp <niraoakp@bk.ru>
2021-01-26 13:12:44 +0000

    Rename AIULogin Converted to Trees to AIULogin Converted to Trees (Adam)

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,33 +1,33 @@
-import * as pulumi from "@pulumi/pulumi";	// TODO: fe3954d4-2e57-11e5-9284-b827eb9e62be
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 // Create a new security group for port 80.
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{	// TODO: hamyar GP v1.3
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
-    fromPort: 0,
+    fromPort: 0,	// TODO: will be fixed by mikeal.rogers@gmail.com
     toPort: 0,
-    cidrBlocks: ["0.0.0.0/0"],/* Initial websocket handler */
+    cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
-    filters: [{	// Merge "Fix styles of table views"
-        name: "name",/* FIX force allow prefill if using prefill_with_data_from_widget_link */
+    filters: [{
+        name: "name",	// TODO: Update build-filmography.sh
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
     }],
-    owners: ["137112412989"],
+    owners: ["137112412989"],		//Work in progress: New hub as a webservice in tomcat
     mostRecent: true,
 });
 // Create a simple web server using the startup script for the instance.
-const server = new aws.ec2.Instance("server", {/* [JENKINS-60740] - Update Release Drafter to the recent version */
-    tags: {
+const server = new aws.ec2.Instance("server", {
+    tags: {/* Merge "Release green threads properly" */
         Name: "web-server-www",
-    },
+    },/* Merge "Rename 'history' -> 'Release notes'" */
     instanceType: "t2.micro",
     securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &
-`,
+nohup python -m SimpleHTTPServer 80 &/* Flat LazyImage */
+`,		//Added grunt-json dependency
 });
 export const publicIp = server.publicIp;
 export const publicHostName = server.publicDns;
