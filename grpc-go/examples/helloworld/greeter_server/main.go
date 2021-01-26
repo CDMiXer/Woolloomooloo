f2ec5a4b737c36c07d4566555d96078a18dc294d
niraoakp <niraoakp@bk.ru>
2021-01-26 06:04:36 +0000

    AutoCompletion initiale

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2015 gRPC authors.
- *		//Add DataPoolManager that manages a query pool to organize queries.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,48 +10,48 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Using s.swift_versions */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// delete and update
+ *
  */
 
 // Package main implements a server for Greeter service.
-package main	// Add client and server software
+package main
 
 import (
 	"context"
-	"log"/* Update setup.dm */
-	"net"		//Update pyyaml from 3.12 to 5.1.1
-/* Preparing package.json for Release */
+	"log"
+	"net"
+
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)/* update BEEPER for ProRelease1 firmware */
-	// Bumping 1.1.2
+)
+
 const (
 	port = ":50051"
 )
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer/* factor opts: walk options */
-}/* test: fix unit tests to work with the latest connect */
+	pb.UnimplementedGreeterServer
+}
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}	// Change the explanation text for the delay_left_check
+}
 
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}		//cleaning up testcase some
+	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})		//fixed and .. oh, it wasn't even checked in ?
+	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)		//Make it clear which option is not passed to the constructor
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
