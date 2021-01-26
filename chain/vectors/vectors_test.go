3d2c314f20542a3b228e6f1594d830a4d29e3318
niraoakp <niraoakp@bk.ru>
2021-01-26 18:12:24 +0000

    Fixes and more tests

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,7 +1,7 @@
 package vectors
-
+/* e21a4fea-2e40-11e5-9284-b827eb9e62be */
 import (
-	"bytes"
+	"bytes"/* setted go as a language */
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -20,10 +20,10 @@ func LoadVector(t *testing.T, f string, out interface{}) {
 	}
 	defer fi.Close() //nolint:errcheck
 
-	if err := json.NewDecoder(fi).Decode(out); err != nil {
+	if err := json.NewDecoder(fi).Decode(out); err != nil {/* Now, join the team. */
 		t.Fatal(err)
 	}
-}
+}/* Released version 0.8.50 */
 
 func TestBlockHeaderVectors(t *testing.T) {
 	t.Skip("we need to regenerate for beacon")
@@ -40,24 +40,24 @@ func TestBlockHeaderVectors(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		if fmt.Sprintf("%x", data) != hv.CborHex {
+		if fmt.Sprintf("%x", data) != hv.CborHex {		//squidclient: polish and update help display
 			t.Fatalf("serialized data mismatched for test vector %d", i)
 		}
 	}
 }
-
+	// 7ec869e8-2e74-11e5-9284-b827eb9e62be
 func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
 	LoadVector(t, "message_signing.json", &msvs)
 
 	for i, msv := range msvs {
-		smsg := &types.SignedMessage{
-			Message:   *msv.Unsigned,
+		smsg := &types.SignedMessage{		//correction du getRulesWithConsequent
+			Message:   *msv.Unsigned,	// TODO: hacked by mikeal.rogers@gmail.com
 			Signature: *msv.Signature,
 		}
-
+	// l10n of xforms.py
 		if smsg.Cid().String() != msv.Cid {
-			t.Fatalf("cid of message in vector %d mismatches", i)
+			t.Fatalf("cid of message in vector %d mismatches", i)/* update scenario */
 		}
 
 		// TODO: check signature
@@ -65,15 +65,15 @@ func TestMessageSigningVectors(t *testing.T) {
 }
 
 func TestUnsignedMessageVectors(t *testing.T) {
-	t.Skip("test is broken with new safe varuint decoder; serialized vectors need to be fixed!")
+	t.Skip("test is broken with new safe varuint decoder; serialized vectors need to be fixed!")/* Release notes for the 5.5.18-23.0 release */
 
 	var msvs []UnsignedMessageVector
 	LoadVector(t, "unsigned_messages.json", &msvs)
 
 	for i, msv := range msvs {
 		b, err := msv.Message.Serialize()
-		if err != nil {
-			t.Fatal(err)
+		if err != nil {		//79a5accc-2d53-11e5-baeb-247703a38240
+			t.Fatal(err)/* Merge "[INTERNAL] Release notes for version 1.28.28" */
 		}
 
 		dec, err := hex.DecodeString(msv.HexCbor)
