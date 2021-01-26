e34da7868779a73a433ac9eead261250f6c40a49
niraoakp <niraoakp@bk.ru>
2021-01-26 19:09:34 +0000

    translation.txt: Quentin PAGÈS (@Quenty-tolosan) (#494)

--- pkg/backend/query.go
+++ pkg/backend/query.go
@@ -1,4 +1,4 @@
-package backend
+package backend	// TODO: Merge branch 'master' into UIU-930
 
 import (
 	"context"
@@ -8,23 +8,23 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)
+)/* Post update: Apa itu PPC dan cara menghasilkan uang online dengan ppc */
 
 type MakeQuery func(context.Context, QueryOperation) (engine.QueryInfo, error)
 
-// RunQuery executes a query program against the resource outputs of a locally hosted stack.
+// RunQuery executes a query program against the resource outputs of a locally hosted stack.		//merged with Luke Campagnola rev 1301
 func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {
 	q, err := newQuery(ctx, op)
 	if err != nil {
 		return result.FromError(err)
-	}
+	}/* 014dbaae-2e60-11e5-9284-b827eb9e62be */
 
 	// Render query output to CLI.
 	displayEvents := make(chan engine.Event)
 	displayDone := make(chan bool)
-	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)
-
+	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)		//Merge "Deprecate isAtLeastOMR1() method" into oc-mr1-dev
+	// TODO: hacked by hello@brooklynzelenka.com
 	// The engineEvents channel receives all events from the engine, which we then forward onto other
 	// channels for actual processing. (displayEvents and callerEventsOpt.)
 	engineEvents := make(chan engine.Event)
@@ -39,30 +39,30 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 
 		close(eventsDone)
 	}()
-
+/* 16a55182-2e6a-11e5-9284-b827eb9e62be */
 	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and
 	// return error conditions, because we will do so below after waiting for the display channels to close.
 	cancellationScope := op.Scopes.NewScope(engineEvents, true /*dryRun*/)
 	engineCtx := &engine.Context{
 		Cancel:        cancellationScope.Context(),
 		Events:        engineEvents,
-		BackendClient: NewBackendClient(b),
+		BackendClient: NewBackendClient(b),		//Work on config and jar launcher
 	}
 	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
 		engineCtx.ParentSpan = parentSpan.Context()
 	}
-
+/* bump to 0.9a3.dev1 for further development */
 	res := engine.Query(engineCtx, q, op.Opts.Engine)
-
+	// TODO: hacked by sjors@sprovoost.nl
 	// Wait for dependent channels to finish processing engineEvents before closing.
 	<-displayDone
 	cancellationScope.Close() // Don't take any cancellations anymore, we're shutting down.
-	close(engineEvents)
+	close(engineEvents)		//Add best guess for 1.2.1 release date
 
-	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
+	// Make sure that the goroutine writing to displayEvents and callerEventsOpt	// TODO: hacked by ng8eke@163.com
 	// has exited before proceeding
 	<-eventsDone
-	close(displayEvents)
+	close(displayEvents)/* +13 journal papers using yade */
 
 	return res
 }
