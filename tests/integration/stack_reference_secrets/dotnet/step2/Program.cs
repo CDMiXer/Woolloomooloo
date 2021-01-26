54249b81f219c483c726775e26dc5dcb43513b19
niraoakp <niraoakp@bk.ru>
2021-01-26 00:50:50 +0000

    Merge branch 'dev' into patch-3

--- tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Threading.Tasks;/* Changed map filenames from char* to string */
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)		//c23cef52-2e63-11e5-9284-b827eb9e62be
     {
-        return Deployment.RunAsync(() =>
-        {
+        return Deployment.RunAsync(() =>/* Upload Changelog draft YAMLs to GitHub Release assets */
+        {/* Fix for vertex access in polygons */
             // Kinda strange, but we are getting a stack reference to ourselves, and referencing
             // the result of the previous deployment.
 
@@ -18,13 +18,13 @@ class Program
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var sr = new StackReference(slug);
 
-            return new Dictionary<string, object>
+            return new Dictionary<string, object>/* coreções no retorno do objeto sequence. */
             {
                 { "normal", Output.Create("normal") },
                 { "secret", Output.CreateSecret("secret") },
                 { "refNormal", sr.GetOutput("normal") },
                 { "refSecret", sr.GetOutput("secret") },
             };
-        });
+        });		//sprinkles, 8.12
     }
 }
