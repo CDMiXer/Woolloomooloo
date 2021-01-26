83367cf383324bb31171745c647417496e7351a4
niraoakp <niraoakp@bk.ru>
2021-01-26 09:48:57 +0000

    Merge "[zmq] Don't fallback to topic if wrong server specified"

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,13 +1,13 @@
 -- name: create-table-latest
-
+	// f9dc5bfa-2e40-11e5-9284-b827eb9e62be
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
-,latest_build_id INTEGER
+,latest_build_id INTEGER		//add InputUnit class for read the infomation of Input Device
 ,latest_type     VARCHAR(50)
-,latest_name     VARCHAR(500)
+,latest_name     VARCHAR(500)	// Added deactivated icon for save and exit.
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER
+,latest_deleted  INTEGER		//Updating build-info/dotnet/cli/master for preview2-006065
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
