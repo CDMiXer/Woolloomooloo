92f6a200055ecddbee0416cf7223ac09bff705ce
niraoakp <niraoakp@bk.ru>
2021-01-26 19:05:01 +0000

    2b0a0b32-2e73-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,9 +1,9 @@
--- name: create-table-secrets/* Release the transform to prevent a leak. */
+-- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_repo_id           INTEGER	// Adding mirroring support for PCB backside text
-,secret_name              TEXT
+,secret_repo_id           INTEGER
+,secret_name              TEXT		//7cd9eea6-2d5f-11e5-94b6-b88d120fff5e
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS secrets (
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-secrets-repo
+-- name: create-index-secrets-repo		//switch a couple graphs around
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-
+	// TODO: will be fixed by fjl@ethereum.org
 -- name: create-index-secrets-repo-name
-
+/* Merge "Mark additional tasks info column as deferred" */
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
