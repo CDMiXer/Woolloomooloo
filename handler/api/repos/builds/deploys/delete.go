c1eef5d6f07217b4f66d76cfa030a93cb5338719
niraoakp <niraoakp@bk.ru>
2021-01-26 10:34:51 +0000

    44a5cd3c-2e4c-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,62 +1,62 @@
 // Copyright 2019 Drone IO, Inc.
-///* try sending form */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* added Host and SetHost methods */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* release 0.9K */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// looks like I broke the gray bg styling when I abbreviated the UA names. fix it.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release to update README on npm */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by nicksavers@gmail.com
-package deploys
 
+package deploys	// Update and rename Owner_phoneModel.py to Owner_phone.py
+/* Merge "Fix obsolete advice in RelativeLayout's documentation." */
 import (
-	"net/http"		//Schema do SQL do banco de dados newsicop limpo, sem registros.
-
+	"net/http"
+	// TODO: added shields.io buttons to README.md
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Create Terceiro Passo.html */
-
+	"github.com/drone/drone/handler/api/render"/* Release 0.13.0 - closes #3 closes #5 */
+	"github.com/drone/drone/logger"/* removing alpha versions from dependencies */
+/* Merge "diag: Release wakeup sources correctly" */
 	"github.com/go-chi/chi"
-)/* Release version 0.0.1 */
-/* Rename xxx_grundtal.txt to 001_grundtal.txt */
+)/* Modified some build settings to make Release configuration actually work. */
+
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {/* The variable cookieBarHide should be global. */
+) http.HandlerFunc {	// TODO: Readme: Simplify introduction
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
-			target    = chi.URLParam(r, "*")
-		)	// TODO: that should work
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: will be fixed by ligi@ligi.de
+			name      = chi.URLParam(r, "name")	// TODO: no params is nil
+			target    = chi.URLParam(r, "*")		//b30c82f0-2e4e-11e5-9284-b827eb9e62be
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err).
+			logger.FromRequest(r)./* Release v2.5 */
+				WithError(err)./* Create JetPHPAPI.php */
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")
+				Debugln("api: cannot find repository")/* Remove autotest helper gems (for now) */
 			return
-		}
+		}/* metis-grabber (wip) */
 
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
-		if err != nil {
+		if err != nil {	// TODO: New correlation feature, changes in overlap and adapted mother GUI
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).	// TODO: hacked by fjl@ethereum.org
-				Debugln("api: cannot delete deployment")
+				WithField("name", name).
+				Debugln("api: cannot delete deployment")	// TODO: will be fixed by nick@perfectabstractions.com
 		} else {
-			w.WriteHeader(http.StatusNoContent)/* Release for 18.31.0 */
+			w.WriteHeader(http.StatusNoContent)
 		}
 	}
 }
