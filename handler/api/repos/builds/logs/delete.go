657595b24e05eb6f069e88d3998c8c78d510e7fd
niraoakp <niraoakp@bk.ru>
2021-01-26 19:24:03 +0000

    3cce5ecc-35c6-11e5-99da-6c40088e03e4

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,64 +1,64 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by alex.gaynor@gmail.com
+//	// Merge branch 'release/3.3' into prop-table-detailed
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Imported Pax Web and its dependencies */
 // You may obtain a copy of the License at
-//	// TODO: Merge "Add tool for reporting Bandit OpenStack coverage"
+//		//1bc98c82-2d5c-11e5-ba3e-b88d120fff5e
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Use double-buffering on Linux */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added new blockstates. #Release */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,		//Add a missing comma in annotations-reference.rst
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package logs
 
-import (	// Added WL_RELEASE file for build 17
-	"net/http"
-	"strconv"
-
+import (
+	"net/http"	// Offer controller
+	"strconv"		//Merge "msm: ipa: add empty implementation for iommu functions"
+		//remove deprecated soyc parameter
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Update digest.jade
-
+	"github.com/drone/drone/handler/api/render"
+/* Subsection Manager 1.0.1 (Bugfix Release) */
 	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the logs.
-func HandleDelete(
+// requests to delete the logs.	// Makefile: use Android NDK r10
+func HandleDelete(/* added GetReleaseInfo, GetReleaseTaskList actions. */
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,	// Merge "Set AuthPlugin in __init__()"
+	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
-) http.HandlerFunc {/* Release 2.0.0: Upgrade to ECM 3 */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Merge "Removing PARAMS macro for consistency" */
+		)	// TODO: hacked by vyzo@hackzen.org
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {
-			render.BadRequest(w, err)/* Add NtGdiTransformPoints stub. */
+		if err != nil {/* Implemented LGPL license */
+			render.BadRequest(w, err)
 			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))/* Release: Making ready for next release cycle 3.1.4 */
-		if err != nil {
-			render.BadRequest(w, err)		//949c3932-2e3f-11e5-9284-b827eb9e62be
-			return
-		}/* Release 1.0.64 */
-		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: by joachim: Fixed api.php docs.
+		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
+		if err != nil {
+			render.BadRequest(w, err)/* Release into the public domain */
+			return/* OpenBSD fixes. */
+		}
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//f51b9328-2e69-11e5-9284-b827eb9e62be
+			return
 		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)		//Handle database exception
 		if err != nil {
 			render.NotFound(w, err)
 			return
@@ -72,8 +72,8 @@ func HandleDelete(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
-		err = logs.Delete(r.Context(), step.ID)/* Merge "Release 4.0.10.004  QCACLD WLAN Driver" */
+		}/* Release of eeacms/forests-frontend:1.7-beta.4 */
+		err = logs.Delete(r.Context(), step.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			return
