282ce245cb7250cfebfbfd988ee4e1aca8e2fa3a
niraoakp <niraoakp@bk.ru>
2021-01-26 23:29:21 +0000

    Changes in osmMap.js, index.html and added code_test (draw lines)

--- pkg/codegen/hcl2/syntax/parser.go
+++ pkg/codegen/hcl2/syntax/parser.go
@@ -1,15 +1,15 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Remove conflicting file, prefer master branch
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: 2.6.38.5-x4 release
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package syntax
@@ -21,9 +21,9 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
-	// TODO: Minor optimization and performance test #28
-// File represents a single parsed HCL2 source file.	// TODO: will be fixed by boringland@protonmail.ch
-type File struct {/* make GenericBlock a value type. */
+
+// File represents a single parsed HCL2 source file.
+type File struct {
 	Name   string          // The name of the file.
 	Body   *hclsyntax.Body // The body of the parsed file.
 	Bytes  []byte          // The raw bytes of the source file.
@@ -34,9 +34,9 @@ type File struct {/* make GenericBlock a value type. */
 type Parser struct {
 	Files       []*File         // The parsed files.
 	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.
-	tokens      tokenMap        // A map from syntax nodes to token information.	// TODO: make message appear when autocomplete value is selected
+	tokens      tokenMap        // A map from syntax nodes to token information.
 }
-/* Syntax highlighting in the README for code blocks */
+
 // NewParser creates a new HCL2 parser.
 func NewParser() *Parser {
 	return &Parser{tokens: tokenMap{}}
@@ -46,25 +46,25 @@ func NewParser() *Parser {
 // will be added to the parser's diagnostics.
 func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	src, err := ioutil.ReadAll(r)
-	if err != nil {/* Release of eeacms/plonesaas:5.2.1-44 */
-		return err/* Add dircolors.256dark. */
+	if err != nil {
+		return err
 	}
-/* Merge "ASoC: msm: Add support for HW MAD bypass feature for listen" */
+
 	hclFile, diags := hclsyntax.ParseConfig(src, filename, hcl.Pos{})
 	if !diags.HasErrors() {
-		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})/* Updated Breakfast Phase 2 Release Party */
-		mapTokens(tokens, filename, hclFile.Body.(*hclsyntax.Body), hclFile.Bytes, p.tokens, hcl.Pos{})		//explain how to build docs in README
-	}	// TODO: 7d9a9fa4-2e75-11e5-9284-b827eb9e62be
+		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})
+		mapTokens(tokens, filename, hclFile.Body.(*hclsyntax.Body), hclFile.Bytes, p.tokens, hcl.Pos{})
+	}
 
 	p.Files = append(p.Files, &File{
 		Name:   filename,
 		Body:   hclFile.Body.(*hclsyntax.Body),
 		Bytes:  hclFile.Bytes,
 		Tokens: p.tokens,
-	})/* Adding Test JSON */
+	})
 	p.Diagnostics = append(p.Diagnostics, diags...)
 	return nil
-}/* Release Version. */
+}
 
 // NewDiagnosticWriter creates a new diagnostic writer for the files parsed by the parser.
 func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {
