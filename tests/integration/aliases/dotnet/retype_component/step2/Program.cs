dfcb5d1d1bb2a4ad4a5d6b0b946bbb439c2caee1
niraoakp <niraoakp@bk.ru>
2021-01-26 11:41:57 +0000

    oops, "mute" bit should not have been set

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -12,17 +12,17 @@ class Resource : ComponentResource
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour : ComponentResource/* gnunet-service-dns uses the new mesh */
+class ComponentFour : ComponentResource
 {
     private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
         {
-            // Add an alias that references the old type of this resource		//CODENVY-27; adopt token service to workspace sharing (#245)
+            // Add an alias that references the old type of this resource
             // and then make the base() call with the new type of this resource and the added alias.
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
-        }))		//added functionality to manually load patient from csv file
+        }))
     {
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
@@ -30,12 +30,12 @@ class ComponentFour : ComponentResource/* gnunet-service-dns uses the new mesh *
 }
 
 class Program
-{	// TODO: will be fixed by vyzo@hackzen.org
-    static Task<int> Main(string[] args)/* Release of eeacms/eprtr-frontend:0.4-beta.23 */
+{
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
-            var comp4 = new ComponentFour("comp4");	// TODO: hacked by mail@bitpshr.net
+            var comp4 = new ComponentFour("comp4");
         });
     }
 }
