efd6441a7d33dfc9ddf86d39bb2da0a0db7193e2
niraoakp <niraoakp@bk.ru>
2021-01-26 08:20:10 +0000

    Cleanup. Strip off CLIENT/env manipulation

--- handler/api/secrets/list.go
+++ handler/api/secrets/list.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release socket in KVM driver on destroy */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: hacked by zaq1tomo@gmail.com
 
 package secrets
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-
+	"github.com/drone/drone/core"/* Release 0.4 */
+	"github.com/drone/drone/handler/api/render"/* Fix bug returning string default value */
+/* Add a new repositoy method _generate_text_key_index for use by reconcile/check. */
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
 func HandleList(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		namespace := chi.URLParam(r, "namespace")
+	return func(w http.ResponseWriter, r *http.Request) {/* Change view of cmd instaling system packages */
+		namespace := chi.URLParam(r, "namespace")	// TODO: Added Steve Schultz
 		list, err := secrets.List(r.Context(), namespace)
 		if err != nil {
 			render.NotFound(w, err)
