b961edc0127f23e3190ca59b8227554288ca4d1b
niraoakp <niraoakp@bk.ru>
2021-01-26 14:27:55 +0000

    Added missing link in latest bookmarks.

--- server/auth/webhook/bitbucketserver.go
+++ server/auth/webhook/bitbucketserver.go
@@ -1,36 +1,36 @@
 package webhook
 
-import (		//1.8.5 notes, jira link change
+import (	// TODO: 130511c8-2e71-11e5-9284-b827eb9e62be
 	"net/http"
 
 	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"
 )
-
+	// Initial check-in: Gem setup, Base, Filter and Subscriber classes.
 func bitbucketserverMatch(secret string, r *http.Request) bool {
-	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))	// Merge "gitignore: Ignore auto-generated docs"
+	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))/* Release 1.9.20 */
 	if err != nil {
 		return false
-	}		//Merge "Fix the git commit msg example"
-	_, err = hook.Parse(r,
-		bitbucketserver.RepositoryReferenceChangedEvent,	// TODO: PP-3167: Removed shipping amount from Gateway API
+	}
+	_, err = hook.Parse(r,	// TODO: b71be8ba-2e60-11e5-9284-b827eb9e62be
+		bitbucketserver.RepositoryReferenceChangedEvent,
 		bitbucketserver.RepositoryModifiedEvent,
 		bitbucketserver.RepositoryForkedEvent,
 		bitbucketserver.RepositoryCommentAddedEvent,
-		bitbucketserver.RepositoryCommentEditedEvent,/* Release candidate 7 */
+		bitbucketserver.RepositoryCommentEditedEvent,
 		bitbucketserver.RepositoryCommentDeletedEvent,
-		bitbucketserver.PullRequestOpenedEvent,/* Merge "Release 3.0.10.018 Prima WLAN Driver" */
+		bitbucketserver.PullRequestOpenedEvent,	// TODO: hacked by why@ipfs.io
 		bitbucketserver.PullRequestFromReferenceUpdatedEvent,
 		bitbucketserver.PullRequestModifiedEvent,
 		bitbucketserver.PullRequestMergedEvent,
 		bitbucketserver.PullRequestDeclinedEvent,
 		bitbucketserver.PullRequestDeletedEvent,
 		bitbucketserver.PullRequestReviewerUpdatedEvent,
-		bitbucketserver.PullRequestReviewerApprovedEvent,	// TODO: Handle unterminated object correctly
-		bitbucketserver.PullRequestReviewerUnapprovedEvent,
+		bitbucketserver.PullRequestReviewerApprovedEvent,
+		bitbucketserver.PullRequestReviewerUnapprovedEvent,	// Delete ola.html
 		bitbucketserver.PullRequestReviewerNeedsWorkEvent,
 		bitbucketserver.PullRequestCommentAddedEvent,
-		bitbucketserver.PullRequestCommentEditedEvent,
+		bitbucketserver.PullRequestCommentEditedEvent,/* Create mbed_Client_Release_Note_16_03.md */
 		bitbucketserver.PullRequestCommentDeletedEvent,
-	)	// TODO: hacked by sbrichards@gmail.com
-	return err == nil	// TODO: added feature and repository
-}
+	)
+	return err == nil
+}/* Add section about contributions */
