a00adfecf56c76e48e51afcc00a1475c74496483
niraoakp <niraoakp@bk.ru>
2021-01-26 21:58:49 +0000

    Remove a "nil is false" assumption

--- tests/integration/steps/steps_test.go
+++ tests/integration/steps/steps_test.go
@@ -1,51 +1,51 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Add another biList hint */
-// +build nodejs all	// TODO: Delete Hello.c
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
 
 package ints
 
 import (
 	"testing"
-/* Released springjdbcdao version 1.9.4 */
+
 	"github.com/stretchr/testify/assert"
-/* Prepare for Release 0.5.4 */
+
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: 8b3c0c80-2e46-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
 func validateResources(t *testing.T, resources []apitype.ResourceV3, expectedNames ...string) {
 	// Build the lookup table of expected resource names.
-	expectedNamesTable := make(map[string]struct{})	// Merge "[FAB-6010] fixed the wrong URL in examples/README"
-	for _, n := range expectedNames {/* fix snow bug, update casing */
+	expectedNamesTable := make(map[string]struct{})
+	for _, n := range expectedNames {
 		expectedNamesTable[n] = struct{}{}
-	}		//Renamed functions in pooly_member
-		//siteup update
+	}
+
 	// Pull out the stack resource, which must be the first resource in the checkpoint.
 	stackRes, resources := resources[0], resources[1:]
 	assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 
 	// If there are more resources than just the stack, the second resource will be the default provider.
-	if len(resources) > 0 {		//fc39ed24-2e52-11e5-9284-b827eb9e62be
+	if len(resources) > 0 {
 		// Pull out the single provider resource, which should be the second resource in the checkpoint.
-		providerRes := resources[0]		//add two more examples
-		resources = resources[1:]/* First try of a Deck class */
+		providerRes := resources[0]
+		resources = resources[1:]
 		assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 	}
-/* Release of eeacms/forests-frontend:2.0-beta.78 */
+
 	// Ensure that the resource count is correct.
 	assert.Equal(t, len(resources), len(expectedNames))
 
 	// Ensure that exactly the provided resources are in the array.
 	for _, res := range resources {
 		name := string(res.URN.Name())
-		_, ok := expectedNamesTable[name]/* Update documentation and put it in rst */
+		_, ok := expectedNamesTable[name]
 		assert.True(t, ok)
 		delete(expectedNamesTable, name)
 	}
 }
-	// TODO: will be fixed by caojiaoyue@protonmail.com
-// TestSteps tests many combinations of creates, updates, deletes, replacements, and so on.		//More shortening
+
+// TestSteps tests many combinations of creates, updates, deletes, replacements, and so on.
 func TestSteps(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
