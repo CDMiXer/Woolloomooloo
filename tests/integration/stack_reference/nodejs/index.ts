8d384368fe5f9fb1b2e56d431408ebe40aa41c5c
niraoakp <niraoakp@bk.ru>
2021-01-26 17:36:18 +0000

    no need for so much search logging now

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Full array copy implementation */
+import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
-export const val = ["a", "b"];/* PyWebKitGtk 1.1 Release */
+export const val = ["a", "b"];
