47bf8b2ac437dace75f554ba84ba5ffe64fa1e24
niraoakp <niraoakp@bk.ru>
2021-01-26 17:31:28 +0000

    Add feet as valid unit #519

--- handler/api/repos/crons/delete.go
+++ handler/api/repos/crons/delete.go
@@ -5,43 +5,43 @@
 // +build !oss
 
 package crons
-
+/* Release new version 2.2.5: Don't let users try to block the BODY tag */
 import (
-	"net/http"
+	"net/http"	// TODO: will be fixed by hi@antfu.me
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by yuvalalaluf@gmail.com
 
 	"github.com/go-chi/chi"
-)
+)/* Release for 4.6.0 */
 
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the cron job.
+// requests to delete the cron job./* Merge "Release 4.0.10.37 QCACLD WLAN Driver" */
 func HandleDelete(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* [artifactory-release] Release version 3.5.0.RC1 */
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (		//refactored testing with selenium webdriver
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")
-		)
+			cron      = chi.URLParam(r, "cron")	// TODO: EI-196- Added changes for issue num 5
+)		
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
-			return
-		}
+			render.NotFound(w, err)/* Copy tests to original CellIO */
+			return	// TODO: refactoring in mapModule
+		}		//Renamed internal SpotFitter class
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		err = crons.Delete(r.Context(), cronjob)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by brosner@gmail.com
 			render.InternalError(w, err)
-			return
+			return	// Run yarn install and build in travis
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}
+}	// TODO: Add system autostart example
