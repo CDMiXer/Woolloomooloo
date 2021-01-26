51d23b5f1a4f0b61267bb32836db654d4d2ba4d4
niraoakp <niraoakp@bk.ru>
2021-01-26 11:39:32 +0000

    LayerDefinition#toJSON lets each sublayer serialize itself

--- gen/examples/chat/main.go
+++ gen/examples/chat/main.go
@@ -1,6 +1,6 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.	// TODO: Update MasterStop
 
 package main
 
@@ -9,13 +9,13 @@ import (
 	"log"
 	"net/http"
 )
-
+		//Display 'None' if there are no unsynced packages
 var addr = flag.String("addr", ":8080", "http service address")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
+	log.Println(r.URL)/* Release note additions */
 	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
+		http.Error(w, "Not found", http.StatusNotFound)/* fix(DejaMouseDragDropCursor): Add RXJS delay operator */
 		return
 	}
 	if r.Method != "GET" {
@@ -27,7 +27,7 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	flag.Parse()
-	hub := newHub()
+	hub := newHub()		//some readme tweaks
 	go hub.run()
 	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
