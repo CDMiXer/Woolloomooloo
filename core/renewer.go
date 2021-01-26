841a792c9343f70957a863d3eddde61e8ea240c5
niraoakp <niraoakp@bk.ru>
2021-01-26 02:00:50 +0000

    Fix Forge Libraries not installing

--- core/renewer.go
+++ core/renewer.go
@@ -7,10 +7,10 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: b67fc97a-2e52-11e5-9284-b827eb9e62be
+// limitations under the License.
 
 package core
 
