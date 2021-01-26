7af569d623c2b8b516143cdf161e226d986dd06b
niraoakp <niraoakp@bk.ru>
2021-01-26 04:34:55 +0000

    Full support for product variations

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import * as policy from "@pulumi/policy";		//6b02d606-35c6-11e5-8aa1-6c40088e03e4
+import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;
+const packName = process.env.TEST_POLICY_PACK;	// Change copyright.
 
 if (!packName) {
-    console.log("no policy name provided");
-    process.exit(-1);/* Released reLexer.js v0.1.2 */
+    console.log("no policy name provided");		//008de6b2-2e69-11e5-9284-b827eb9e62be
+    process.exit(-1);
 
 } else {
-    const policies = new policy.PolicyPack(packName, {/* Create Release-Prozess_von_UliCMS.md */
-        policies: [		//Entrega del hito 4 de José Cristóbal López Zafra
+    const policies = new policy.PolicyPack(packName, {	// TODO: will be fixed by nicksavers@gmail.com
+        policies: [
             {
                 name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
-                enforcementLevel: "mandatory",	// TODO: Removed one additional spurious log for the plugin.
+                enforcementLevel: "mandatory",
                 configSchema: {
                     required: ["message"],
                     properties: {
-                        message: {
+                        message: {/* Release of eeacms/www:18.3.23 */
                             type: "string",
-                            minLength: 2,
-                            maxLength: 10,
+                            minLength: 2,/* Release jedipus-2.6.31 */
+                            maxLength: 10,/* Restrict coverage badge to master */
                         },
                    },
                 },
                 validateResource: (args, reportViolation) => {},
-            }
-        ],
-    });
-}
+            }/* Add Release Belt (Composer repository implementation) */
+        ],		//They move now
+    });/* Release notes for 6.1.9 */
+}	// TODO: d8bb75b2-2e42-11e5-9284-b827eb9e62be
