904fe072345b915e1482ba1d95011700e5c5e233
niraoakp <niraoakp@bk.ru>
2021-01-26 23:56:40 +0000

    [-change] perl modules don't need shellbang

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Use Rails 3's prefered interface while maintaining backwards compatibility. 
 // that can be found in the LICENSE file.
-
+/* Merge "[Release] Webkit2-efl-123997_0.11.75" into tizen_2.2 */
 package runner
