76a9f909e5015bf639f641b52014564811f77d4d
niraoakp <niraoakp@bk.ru>
2021-01-26 09:07:26 +0000

    Changed package names to "com"

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,6 +1,6 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file./* Release Django Evolution 0.6.1. */
+// LICENSE file.
 
 // +build appengine
 
