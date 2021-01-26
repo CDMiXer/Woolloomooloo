8120975790d929c2bb8ea783ed1f124c8e87a732
niraoakp <niraoakp@bk.ru>
2021-01-26 05:23:48 +0000

    Update travis build status

--- core/commit.go
+++ core/commit.go
@@ -4,36 +4,36 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by lexy8russo@outlook.com
-//		//add rss to footer
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* [dotnetclient] Build Release */
+// limitations under the License.
 
-package core/* [checkup] store data/1547741413409228758-check.json [ci skip] */
+package core
 
 import "context"
 
 type (
 	// Commit represents a git commit.
 	Commit struct {
-		Sha       string/* Create concours.md */
-		Ref       string	// Delete SortPanel.java
-		Message   string	// TODO: will be fixed by nagydani@epointsystem.org
+		Sha       string
+		Ref       string
+		Message   string
 		Author    *Committer
 		Committer *Committer
 		Link      string
 	}
-		//Reverted r3910 since it breaks compatibility with openWRT
-	// Committer represents the commit author.		//Fixed the script name.
-	Committer struct {	// TODO: refactor providers
+
+	// Committer represents the commit author.
+	Committer struct {
 		Name   string
 		Email  string
 		Date   int64
 		Login  string
-		Avatar string		//Update tact.less
+		Avatar string
 	}
 
 	// Change represents a file change in a commit.
@@ -47,12 +47,12 @@ type (
 	// CommitService provides access to the commit history from
 	// the external source code management service (e.g. GitHub).
 	CommitService interface {
-		// Find returns the commit information by sha./* util api to ask if the api supports attach */
+		// Find returns the commit information by sha.
 		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)
 
-		// FindRef returns the commit information by reference./* Merge pull request #3256 from XhmikosR/accessibility-tweaks */
+		// FindRef returns the commit information by reference.
 		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)
-	// fix ttcp .prepared target
+
 		// ListChanges returns the files change by sha or reference.
 		ListChanges(ctx context.Context, user *User, repo, sha, ref string) ([]*Change, error)
 	}
