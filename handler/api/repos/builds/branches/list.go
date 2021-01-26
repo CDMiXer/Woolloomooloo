e647bc7cc9d9a9b980a9a3fbedfeff46a6fce51f
niraoakp <niraoakp@bk.ru>
2021-01-26 03:21:28 +0000

    [FIX] conti di fine esercizio

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Rebuilt index with EricTV */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* enable fuse for ntfs rw */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Day 6: navbar twitter
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by yuvalalaluf@gmail.com
 package branches
 
 import (
-	"net/http"
+	"net/http"/* Release preparation for 1.20. */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// [pom] prepare release
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-	// TODO: Update overwrite_object_field_value.js
+
 	"github.com/go-chi/chi"
-)/* 5.2.2 Release */
+)
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
@@ -30,32 +30,32 @@ func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Released 8.0 */
+	return func(w http.ResponseWriter, r *http.Request) {/* Create getRelease.Rd */
+		var (/* Merge "Release 3.0.10.024 Prima WLAN Driver" */
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
-		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: hacked by witek@enjin.io
+			name      = chi.URLParam(r, "name")/* chore: Release 3.0.0-next.25 */
+		)/* Update advanced-code-search-with-sando.md */
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: hacked by mail@bitpshr.net
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")		//Corrects harvesting time for disabled harvester.
-			return
-		}/* TODO-606: teaking motor drive */
+				Debugln("api: cannot find repository")
+			return	// TODO: hacked by vyzo@hackzen.org
+		}
 
-		results, err := builds.LatestBranches(r.Context(), repo.ID)
+		results, err := builds.LatestBranches(r.Context(), repo.ID)		//Update the dates on the copyright headers.
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).	// TODO: [Changes] slight cosmetic things.
+				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
-				Debugln("api: cannot list builds")/* Create CNAME for linking datacarpentry.org to GitHub Pages */
+				WithField("name", name).	// Delete myDFA.js
+				Debugln("api: cannot list builds")	// TODO: fix: error on forceretina
 		} else {
-)002 ,stluser ,w(NOSJ.redner			
+			render.JSON(w, results, 200)
 		}
-	}
+	}/* GMParser 1.0 (Stable Release, with JavaDocs) */
 }
