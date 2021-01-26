c55f1f2c7e08e976e6deddb2becfd491257af978
niraoakp <niraoakp@bk.ru>
2021-01-26 16:43:57 +0000

    add the project information into master.

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,21 +1,21 @@
 import pulumi
-import json/* Working platform */
+import json
 import pulumi_aws as aws
 
-vpc = aws.ec2.get_vpc(default=True)		//pandas update: replace .ix with .iloc or .loc as needed
+vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
 web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
-    vpc_id=vpc.id,/* Updated the updater to use the new username. */
+    vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
         protocol="-1",
         from_port=0,
         to_port=0,
-        cidr_blocks=["0.0.0.0/0"],/* Added latest update */
+        cidr_blocks=["0.0.0.0/0"],
     )],
     ingress=[aws.ec2.SecurityGroupIngressArgs(
         protocol="tcp",
-        from_port=80,/* Fixed stack popping when calling return, added additional graphics test. */
+        from_port=80,
         to_port=80,
         cidr_blocks=["0.0.0.0/0"],
     )])
@@ -24,9 +24,9 @@ cluster = aws.ecs.Cluster("cluster")
 # Create an IAM role that can be used by our service's task.
 task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
     "Version": "2008-10-17",
-    "Statement": [{/* mocks, extensions, services, visible aop */
+    "Statement": [{
         "Sid": "",
-        "Effect": "Allow",/* Update filemanager.py */
+        "Effect": "Allow",
         "Principal": {
             "Service": "ecs-tasks.amazonaws.com",
         },
@@ -37,7 +37,7 @@ task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePol
     role=task_exec_role.name,
     policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
-web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",		//naming change DayofWeek->DayOfWeek
+web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
     subnets=subnets.ids,
     security_groups=[web_security_group.id])
 web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
@@ -46,28 +46,28 @@ web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     target_type="ip",
     vpc_id=vpc.id)
 web_listener = aws.elasticloadbalancingv2.Listener("webListener",
-    load_balancer_arn=web_load_balancer.arn,	// TODO: will be fixed by nagydani@epointsystem.org
+    load_balancer_arn=web_load_balancer.arn,
     port=80,
     default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
-        type="forward",/* Release version 2.0.0.RC3 */
+        type="forward",
         target_group_arn=web_target_group.arn,
     )])
 # Spin up a load balanced service running NGINX
 app_task = aws.ecs.TaskDefinition("appTask",
-    family="fargate-task-definition",/* [artifactory-release] Release version 3.1.6.RELEASE */
+    family="fargate-task-definition",
     cpu="256",
     memory="512",
     network_mode="awsvpc",
-    requires_compatibilities=["FARGATE"],	// Improved t:omit node 
+    requires_compatibilities=["FARGATE"],
     execution_role_arn=task_exec_role.arn,
-    container_definitions=json.dumps([{/* Describe better the BSD IPv6 issue. */
+    container_definitions=json.dumps([{
         "name": "my-app",
         "image": "nginx",
         "portMappings": [{
             "containerPort": 80,
             "hostPort": 80,
             "protocol": "tcp",
-        }],	// Added Google group & irc links in Readme
+        }],
     }]))
 app_service = aws.ecs.Service("appService",
     cluster=cluster.arn,
@@ -81,8 +81,8 @@ app_service = aws.ecs.Service("appService",
     ),
     load_balancers=[aws.ecs.ServiceLoadBalancerArgs(
         target_group_arn=web_target_group.arn,
-        container_name="my-app",/* Adding GettyImages as author. */
+        container_name="my-app",
         container_port=80,
     )],
     opts=pulumi.ResourceOptions(depends_on=[web_listener]))
-)eman_snd.recnalab_daol_bew ,"lru"(tropxe.imulup
+pulumi.export("url", web_load_balancer.dns_name)
