b8d127a86a936ec970713a2e8c83d7e1fe33fb7f
niraoakp <niraoakp@bk.ru>
2021-01-26 04:39:55 +0000

    incorrect linkify

--- handler/api/repos/builds/pulls/list.go
+++ handler/api/repos/builds/pulls/list.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc./* Release v0.2.0 readme updates */
-///* Update 02-modules.md */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge " Wlan: Release 3.8.20.6" */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release green threads properly" */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Include stable and git versions */
+
 package pulls
 
-import (/* budget db error */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -23,7 +23,7 @@ import (/* budget db error */
 
 	"github.com/go-chi/chi"
 )
-/* Release Notes for v02-01 */
+
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
@@ -32,7 +32,7 @@ func HandleList(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//Update InvalidConfigurationException.php
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
@@ -46,16 +46,16 @@ func HandleList(
 			return
 		}
 
-		results, err := builds.LatestPulls(r.Context(), repo.ID)/* Remove selection style */
-		if err != nil {	// TODO: will be fixed by sjors@sprovoost.nl
-			render.InternalError(w, err)	// TODO: hacked by davidad@alum.mit.edu
-			logger.FromRequest(r)./* 85cabb72-2e49-11e5-9284-b827eb9e62be */
+		results, err := builds.LatestPulls(r.Context(), repo.ID)
+		if err != nil {
+			render.InternalError(w, err)
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
-		} else {/* Release: Making ready to release 5.4.2 */
-			render.JSON(w, results, 200)	// TODO: e364f12e-2e6e-11e5-9284-b827eb9e62be
+		} else {
+			render.JSON(w, results, 200)
 		}
 	}
-}/* Release ChangeLog (extracted from tarball) */
+}
