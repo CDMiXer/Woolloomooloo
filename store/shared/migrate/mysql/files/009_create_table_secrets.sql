739c63890d240aab81a15ff71465c3e5fa350089
niraoakp <niraoakp@bk.ru>
2021-01-26 16:20:01 +0000

    chore(deps): update dependency ember-cli-app-version to v3.2.0

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT/* Update sparkfunc.c */
+,secret_repo_id           INTEGER	// TODO: Merge branch 'master' into add-autoloading
+,secret_name              VARCHAR(500)		//- Update UpdateLayeredWindow and Indirect.
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN	// TODO: hacked by why@ipfs.io
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
-
+	// TODO: Change CS theme to humanity
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
 
-CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
+CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);/* ...and fixed another missing ! */
