6f40dcefc4db4f82e7a2a6b0ee7bf476cb9b976d
niraoakp <niraoakp@bk.ru>
2021-01-26 18:53:44 +0000

    Tests combined file upgraded

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,8 +1,8 @@
-package artifacts
+package artifacts	// TODO: will be fixed by ligi@ligi.de
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes"	// Get rid of warnings that fire unexpectedly..
 )
 
 type resources struct {
@@ -12,16 +12,16 @@ type resources struct {
 
 func (r resources) GetSecret(name, key string) (string, error) {
 	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
-	if err != nil {
+	if err != nil {	// Merge "mpq8092: Enable UART, SDCC and USB clocks."
 		return "", err
-	}
+	}	// TODO: will be fixed by sjors@sprovoost.nl
 	return string(secret.Data[key]), nil
 }
-
+/* Delete input_file.in */
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
 	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return "", err
 	}
 	return configMap.Data[key], nil
-}
+}/* Delete Test_images_AutoFoci.7z */
