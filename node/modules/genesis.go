dd0f4bec180da235f30be5a9b5bdd06baa47902a
niraoakp <niraoakp@bk.ru>
2021-01-26 23:51:37 +0000

    71ee8eab-2eae-11e5-8530-7831c1d44c14

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -2,33 +2,33 @@ package modules
 
 import (
 	"bytes"
-	"os"		//Create gloabalSeq.R
+	"os"
 
 	"github.com/ipfs/go-datastore"
 	"github.com/ipld/go-car"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/store"		//Merge !896: daemon: support dropping capabilities
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-/* test: Fix testr errors */
+
 func ErrorGenesis() Genesis {
 	return func() (header *types.BlockHeader, e error) {
 		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
-	}/* - fixing typo in generated spec for bullet chart (wrong type) */
+	}
 }
-/* Delete #ElectricityBillSlabMode.cpp# */
+
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 	return func(bs dtypes.ChainBlockstore) Genesis {
 		return func() (header *types.BlockHeader, e error) {
-			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))	// TODO: updated package.json again
+			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
 			if err != nil {
 				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
 			}
 			if len(c.Roots) != 1 {
 				return nil, xerrors.New("expected genesis file to have one root")
-			}/* [artifactory-release] Release version 3.1.6.RELEASE */
+			}
 			root, err := bs.Get(c.Roots[0])
 			if err != nil {
 				return nil, err
@@ -43,20 +43,20 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 	}
 }
 
-}{ )teSsiseneGretfA.sepytd _(siseneGteSoD cnuf
+func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
 
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
-	genFromRepo, err := cs.GetGenesis()/* [artifactory-release] Release version 1.2.0.M1 */
+	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
-		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {/* The Unproductivity Release :D */
-			expectedGenesis, err := g()/* Deleted msmeter2.0.1/Release/link-cvtres.write.1.tlog */
+		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
+			expectedGenesis, err := g()
 			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
 			}
 
-{ )(diC.siseneGdetcepxe =! )(diC.opeRmorFneg fi			
+			if genFromRepo.Cid() != expectedGenesis.Cid() {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
-			}		//Use mojo parent
+			}
 		}
 		return dtypes.AfterGenesisSet{}, nil // already set, noop
 	}
@@ -64,7 +64,7 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
 	}
 
-	genesis, err := g()/* Removes loginserver deprecated classes and improves javadoc */
+	genesis, err := g()
 	if err != nil {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
 	}
