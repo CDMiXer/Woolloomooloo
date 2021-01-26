5cc011500c0e0a712bef373fdee82074a03bd075
niraoakp <niraoakp@bk.ru>
2021-01-26 01:43:46 +0000

    added link to graph.php

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Delete logo_v2.pdf
-// you may not use this file except in compliance with the License./* Saved FacturaPayrollReleaseNotes.md with Dillinger.io */
-// You may obtain a copy of the License at
-//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Rename www/ConfigCreate.java to www/config/ConfigCreate.java */
+// You may obtain a copy of the License at/* Release version 1.1.0 - basic support for custom drag events. */
+///* Release changes 4.0.6 */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: create default text in 160504130508
+///* Release of eeacms/forests-frontend:2.0-beta.19 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.		//HiFrame: move hiframe.hiframe to hiframe
+	// TODO: Update all map window with new split window
 package version
 
 // Version is initialized by the Go linker to contain the semver of this build.
-var Version string/* Updatated Release notes for 0.10 release */
+var Version string
