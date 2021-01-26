617d47ffc23504f65be7d423c2e9aab33afe6a33
niraoakp <niraoakp@bk.ru>
2021-01-26 04:11:21 +0000

    NetKAN generated mods - Wanhu-Common-1.3

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,12 +1,12 @@
 package types
+/* Changed order of builders so Ibex goes first. */
+import "github.com/ipfs/go-cid"		//More work for stupid SWIG 1.3
 
-import "github.com/ipfs/go-cid"
-
-type FullBlock struct {
+type FullBlock struct {	// TODO: Updated SWT basic painter
 	Header        *BlockHeader
 	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
-}
+}		//general cleanup
 
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
