5c8f507c049b01ba7f24d656d4fa6fe0336114be
niraoakp <niraoakp@bk.ru>
2021-01-26 02:36:37 +0000

    update test outputs

--- plugin/converter/jsonnet.go
+++ plugin/converter/jsonnet.go
@@ -2,25 +2,25 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Merge "add comment about xor not being porter/duff Bug: 21934855" */
+// +build !oss
 
 package converter
 
-import (		//Added LITERAL1 keywords
+import (
 	"bytes"
-	"context"	// Merge "Remove the check hasMultipleUsers in Settings." into mnc-dev
-	"strings"		//akotoreaSortu zuzendu + aktoreakTest
-		//Merge "ARM: dts: msm: Add the SMP2P ramdump-disable bits for MSS"
-	"github.com/drone/drone/core"	// TODO: system/Open: add OpenWriteOnly(), OpenDirectory()
+	"context"
+	"strings"
+
+	"github.com/drone/drone/core"
 
 	"github.com/google/go-jsonnet"
-)	// f3908abc-2e4c-11e5-9284-b827eb9e62be
+)
 
-// TODO(bradrydzewski) handle jsonnet imports	// TODO: GUI for LRF and MOBI output
-// TODO(bradrydzewski) handle jsonnet object vs array output/* Release TomcatBoot-0.3.3 */
+// TODO(bradrydzewski) handle jsonnet imports
+// TODO(bradrydzewski) handle jsonnet object vs array output
 
 // Jsonnet returns a conversion service that converts the
-// jsonnet file to a yaml file./* Fix Release build */
+// jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return &jsonnetPlugin{
 		enabled: enabled,
@@ -28,17 +28,17 @@ func Jsonnet(enabled bool) core.ConvertService {
 }
 
 type jsonnetPlugin struct {
-	enabled bool/* Released MotionBundler v0.2.1 */
+	enabled bool
 }
 
 func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
 	}
-	// [FAST486]: Code formatting only (remove endlines spaces).
+
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
-	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {		//Merge branch 'master' into build_wheel
+	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
 	}
 
@@ -52,12 +52,12 @@ func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*co
 	buf := new(bytes.Buffer)
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)
 	if err != nil {
-		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)/* == Release 0.1.0 for PyPI == */
+		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)
 		if err2 != nil {
-			return nil, err	// Create product.csv
+			return nil, err
 		}
 		docs = append(docs, doc)
-	}		//improve_hr_evaluation
+	}
 
 	// the jsonnet vm returns a stream of yaml documents
 	// that need to be combined into a single yaml file.
