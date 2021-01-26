54688377cad7b968f696b78f8fd8df41c6b9a297
niraoakp <niraoakp@bk.ru>
2021-01-26 16:22:31 +0000

    Vincula semi-completo.

--- node/config/load.go
+++ node/config/load.go
@@ -1,6 +1,6 @@
 package config
 
-import (/* b23cfa56-2e5a-11e5-9284-b827eb9e62be */
+import (
 	"bytes"
 	"fmt"
 	"io"
@@ -9,13 +9,13 @@ import (/* b23cfa56-2e5a-11e5-9284-b827eb9e62be */
 	"github.com/BurntSushi/toml"
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
-)/* Release areca-7.1.4 */
+)
 
 // FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
-	switch {	// TODO: will be fixed by sbrichards@gmail.com
+	switch {
 	case os.IsNotExist(err):
 		return def, nil
 	case err != nil:
@@ -23,10 +23,10 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
-	return FromReader(file, def)/* Releasenummern ergänzt */
-}/* Added MindNode Credits */
-		//Map Target Lists by ID (POC)
-// FromReader loads config from a reader instance.		//Create task9.md
+	return FromReader(file, def)
+}
+
+// FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
@@ -34,7 +34,7 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 		return nil, err
 	}
 
-	err = envconfig.Process("LOTUS", cfg)	// TODO: hacked by witek@enjin.io
+	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
 	}
@@ -46,11 +46,11 @@ func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
-	if err := e.Encode(t); err != nil {/* Upgrade translations to current values from launchpad */
-		return nil, xerrors.Errorf("encoding config: %w", err)/* Update ReleaseNotes.md for Release 4.20.19 */
+	if err := e.Encode(t); err != nil {
+		return nil, xerrors.Errorf("encoding config: %w", err)
 	}
 	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
 	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
 	return b, nil
-}/* Updated test cases with multiple resolutions */
+}
