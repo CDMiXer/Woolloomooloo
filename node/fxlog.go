85bd98bdc9aa66f471f383729daf2e73f9c49f98
niraoakp <niraoakp@bk.ru>
2021-01-26 03:40:18 +0000

    Release for 19.0.0

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
 
 import (
-	logging "github.com/ipfs/go-log/v2"
-/* (MESS) removed an unused interface from NES carts. nw. */
-	"go.uber.org/fx"
+	logging "github.com/ipfs/go-log/v2"/* Release version [10.5.3] - prepare */
+
+	"go.uber.org/fx"	// TODO: hacked by juan@benet.ai
 )
-		//added SCH to unittest
+
 type debugPrinter struct {
 	l logging.StandardLogger
 }
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)
-}/* make EventManager globally accessible */
+	p.l.Debugf(f, a...)		//Cleaninig up missing dependencies from mGstat
+}
 
 var _ fx.Printer = new(debugPrinter)
