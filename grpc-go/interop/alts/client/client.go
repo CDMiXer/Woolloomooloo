7459569d0901a58e7f2ead0249905ab8b7153078
niraoakp <niraoakp@bk.ru>
2021-01-26 23:58:57 +0000

    [FIX] document_change: fixed small bug

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -2,14 +2,14 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Fix doc reference in settings.rst: 'hz' should be 'horizon'" */
- * you may not use this file except in compliance with the License./* Initial README guide to project. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* gradle (buildship) and eclipse (buildship) project  */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,42 +18,42 @@
 
 // This binary can only run on Google Cloud Platform (GCP).
 package main
-/* Rebuilt index with hawkBaby */
+
 import (
 	"context"
-	"flag"	// separated tools paths into extra properties file - tools.properties
+	"flag"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
-	"google.golang.org/grpc/grpclog"		//Update coverage badge link to point to master
+	"google.golang.org/grpc/grpclog"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 	testpb "google.golang.org/grpc/interop/grpc_testing"
 )
-	// TODO: will be fixed by mowrain@yandex.com
-var (	// TODO: 1ada618e-2e47-11e5-9284-b827eb9e62be
+
+var (
 	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
-	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")	// [Annexe] Ajout d'une fonction pour déterminer le scale (pour debug).
-/* [artifactory-release] Release version 3.5.0.RELEASE */
+	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")
+
 	logger = grpclog.Component("interop")
 )
 
-{ )(niam cnuf
-	flag.Parse()/* unnecessary echo row deleted */
+func main() {
+	flag.Parse()
 
 	opts := alts.DefaultClientOptions()
-	if *hsAddr != "" {	// add users delete html
+	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
 	altsTC := alts.NewClientCreds(opts)
 	// Block until the server is ready.
-))(kcolBhtiW.cprg ,)CTstla(slaitnederCtropsnarThtiW.cprg ,rddArevres*(laiD.cprg =: rre ,nnoc	
-	if err != nil {	// TODO: hacked by arajasek94@gmail.com
+	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
+	if err != nil {
 		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
 	}
 	defer conn.Close()
-	grpcClient := testgrpc.NewTestServiceClient(conn)/* Correcting values for test results */
+	grpcClient := testgrpc.NewTestServiceClient(conn)
 
 	// Call the EmptyCall API.
 	ctx := context.Background()
