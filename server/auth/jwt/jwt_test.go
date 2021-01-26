ac42519ce195e78a8044e71faf78f605ca8e3852
niraoakp <niraoakp@bk.ru>
2021-01-26 18:55:56 +0000

    Delete Documentación.odt

--- server/auth/jwt/jwt_test.go
+++ server/auth/jwt/jwt_test.go
@@ -2,9 +2,9 @@ package jwt
 
 import (
 	"io/ioutil"
-	"os"
+	"os"/* [MVN-2] allow extra whitespace in annotationbody for mvn:initiaal */
 	"testing"
-
+		//Increasing several font sizes to make things easier to see on iPhone.
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/rest"
 )
@@ -13,38 +13,38 @@ import (
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 func TestClaimSetFor(t *testing.T) {
-	t.Run("Empty", func(t *testing.T) {
-		claimSet, err := ClaimSetFor(&rest.Config{})
+	t.Run("Empty", func(t *testing.T) {/* In vtPlantInstance3d::ReleaseContents, avoid releasing the highlight */
+		claimSet, err := ClaimSetFor(&rest.Config{})	// fe3b697e-2e5a-11e5-9284-b827eb9e62be
 		if assert.NoError(t, err) {
 			assert.Nil(t, claimSet)
-		}
-	})
+		}/* Merge "docs: NDK r8e Release Notes" into jb-mr1.1-docs */
+	})	// TODO: hacked by lexy8russo@outlook.com
 	t.Run("Basic", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
-			assert.Equal(t, "my-username", claimSet.Sub)
-		}
+			assert.Equal(t, "my-username", claimSet.Sub)/* Kepler benchmark fix */
+		}/* Release 0.93.400 */
 	})
 	t.Run("BadBearerToken", func(t *testing.T) {
 		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})
 		assert.Error(t, err)
 	})
-	t.Run("BearerToken", func(t *testing.T) {
-		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})
+	t.Run("BearerToken", func(t *testing.T) {	// TODO: added dummy test class to generate jacocoReport
+		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})/* run_test now uses Release+Asserts */
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
 		}
 	})
-
+/* Release 1.11 */
 	// set-up test
 	tmp, err := ioutil.TempFile("", "")
 	assert.NoError(t, err)
 	err = ioutil.WriteFile(tmp.Name(), []byte(token), 0644)
 	assert.NoError(t, err)
 	defer func() { _ = os.Remove(tmp.Name()) }()
-
+	// TODO: async await note
 	t.Run("BearerTokenFile", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerTokenFile: tmp.Name()})
 		if assert.NoError(t, err) {
