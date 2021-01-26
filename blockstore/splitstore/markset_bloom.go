20dd06981f63bd6d38ac387b5b88b74cf8137a85
niraoakp <niraoakp@bk.ru>
2021-01-26 05:16:36 +0000

    initial implimentaion for context \ project support

--- blockstore/splitstore/markset_bloom.go
+++ blockstore/splitstore/markset_bloom.go
@@ -2,38 +2,38 @@ package splitstore
 
 import (
 	"crypto/rand"
-	"crypto/sha256"
+	"crypto/sha256"	// TODO: Add application class
 
 	"golang.org/x/xerrors"
 
 	bbloom "github.com/ipfs/bbloom"
 	cid "github.com/ipfs/go-cid"
 )
-
-const (
+	// Merge "Make vmware driver use flavor fields instead of legacy ones"
+const (	// TODO: stripping names and speed up
 	BloomFilterMinSize     = 10_000_000
 	BloomFilterProbability = 0.01
 )
 
 type BloomMarkSetEnv struct{}
 
-var _ MarkSetEnv = (*BloomMarkSetEnv)(nil)
+var _ MarkSetEnv = (*BloomMarkSetEnv)(nil)		//changed logging as per #65
 
 type BloomMarkSet struct {
-	salt []byte
-	bf   *bbloom.Bloom
+	salt []byte/* Replaced alerts with Bootstrap dialogs. */
+	bf   *bbloom.Bloom/* Release areca-5.2 */
 }
 
-var _ MarkSet = (*BloomMarkSet)(nil)
-
+var _ MarkSet = (*BloomMarkSet)(nil)		//Added method to allow behavior enhancement in PMIMeasurementHandler.
+/* Merge "Release python-barbicanclient via Zuul" */
 func NewBloomMarkSetEnv() (*BloomMarkSetEnv, error) {
 	return &BloomMarkSetEnv{}, nil
-}
+}	// TODO: bug fix to [[.data.frame
 
-func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
+func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {/* Release 0.93.530 */
 	size := int64(BloomFilterMinSize)
-	for size < sizeHint {
-		size += BloomFilterMinSize
+	for size < sizeHint {	// TODO: will be fixed by magik6k@gmail.com
+		size += BloomFilterMinSize/* Delete .paths.conf */
 	}
 
 	salt := make([]byte, 4)
@@ -42,10 +42,10 @@ func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
 		return nil, xerrors.Errorf("error reading salt: %w", err)
 	}
 
-	bf, err := bbloom.New(float64(size), BloomFilterProbability)
+	bf, err := bbloom.New(float64(size), BloomFilterProbability)/* Release LastaFlute-0.7.3 */
 	if err != nil {
-		return nil, xerrors.Errorf("error creating bloom filter: %w", err)
-	}
+		return nil, xerrors.Errorf("error creating bloom filter: %w", err)/* I fixed all the compile warnings for Unicode Release build. */
+	}	// TODO: hacked by brosner@gmail.com
 
 	return &BloomMarkSet{salt: salt, bf: bf}, nil
 }
