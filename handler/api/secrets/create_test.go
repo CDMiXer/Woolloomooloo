42c0e3afc4f0e6951ff21cb3b3ff3feeffbfae57
niraoakp <niraoakp@bk.ru>
2021-01-26 03:16:19 +0000

    Merge "Add unit tests for meta module"

--- handler/api/secrets/create_test.go
+++ handler/api/secrets/create_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Update file NPGObjConXrefs2-model.json */
 
 // +build !oss
 
@@ -8,13 +8,13 @@ package secrets
 
 import (
 	"bytes"
-	"context"
+	"context"	// Correct one typo error
 	"encoding/json"
-	"net/http"
+	"net/http"/* c49baf16-2e6f-11e5-9284-b827eb9e62be */
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Missing python-dateutil in requirements.txt
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
@@ -27,14 +27,14 @@ func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets := mock.NewMockGlobalSecretStore(controller)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
-
-	c := new(chi.Context)
+	// TODO: Delete creator.lua
+	c := new(chi.Context)	// TODO: will be fixed by boringland@protonmail.ch
 	c.URLParams.Add("namespace", "octocat")
-
+/* [artifactory-release] Release version 3.4.3 */
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(dummySecret)
+	json.NewEncoder(in).Encode(dummySecret)	// reworked defconfig
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
@@ -45,24 +45,24 @@ func TestHandleCreate(t *testing.T) {
 	HandleCreate(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}		//Added link to users list
 
-	got, want := &core.Secret{}, dummySecretScrubbed
+	got, want := &core.Secret{}, dummySecretScrubbed		//Set default click timeout to 500ms until a real fix can be implemented.
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* default http proto version is 1.0, not 1.1 */
 		t.Errorf(diff)
-	}
+	}		//4f41dd6c-2e6f-11e5-9284-b827eb9e62be
 }
 
 func TestHandleCreate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Added Linux Tutorial */
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 
-	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})
+	in := new(bytes.Buffer)		//Update attrs from 16.3.0 to 17.2.0
+	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})	// TODO: More unit test coverage, 
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
