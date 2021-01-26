65c05ed0456973ea0486ea32214dfd840ecd803c
niraoakp <niraoakp@bk.ru>
2021-01-26 21:49:03 +0000

    Rebase with master

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
-
+import * as pulumi from "@pulumi/pulumi";	// [ task #748 ] Add a link "Dolibarr" into left menu
+	// [IMP]Technical Database Structure Models :Form View Improved
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
@@ -11,62 +11,62 @@ class Resource extends pulumi.ComponentResource {
 // Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
 // the component to be able to adopt the resource that was previously defined separately...
 class Component extends pulumi.ComponentResource {
-    resource: Resource;	// TODO: hacked by lexy8russo@outlook.com
+    resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component", name, {}, opts);/* $logroot should default to central setting */
+        super("my:module:Component", name, {}, opts);
         // The resource creation was moved from top level to inside the component.
         this.resource = new Resource(`${name}-child`, {
             // With a new parent
-            parent: this,		//0d1314ca-2e64-11e5-9284-b827eb9e62be
+            parent: this,
             // But with an alias provided based on knowing where the resource existing before - in this case at top
             // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
-.tnenopmoc siht otni detpoda gnieb ot roirp yhcrareih eht ni noitacol rehto yrartibra emos ni saw taht //            
-            aliases: [pulumi.createUrn("res2", "my:module:Resource")],	// Add purchaseHelper disconnect call onDestroy
+            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
+            aliases: [pulumi.createUrn("res2", "my:module:Resource")],	// TODO: Added 'get/setUniqueInstance' in 'Concept.java'.
         });
     }
-}/* Added design doc for testing on native runtime */
+}/* changes of game post */
 // The creation of the component is unchanged.
 const comp2 = new Component("comp2");
 
 // Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component2", name, {}, opts);/* fix direction and power adjustments on turn */
+class Component2 extends pulumi.ComponentResource {/* Updated scaladoc of Query.scala */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Update SparkR_IDE_Setup.sh
+        super("my:module:Component2", name, {}, opts);/* Refactor batch stuff */
     }
-}/* Initial Git Release. */
-	// TODO: hacked by witek@enjin.io
+}
+	// fix unexpected NULL pointer dereferences and heap corruption
 // validate that "parent: undefined" means "i didn't have a parent previously"
 new Component2("unparented", {
     aliases: [{ parent: pulumi.rootStackResource }],
     parent: comp2,
 });
+	// TODO: hacked by mail@overlisted.net
 
-
-// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-// in the next step to be parented by this.  Make sure that works with an opts with no parent
+// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix	// TODO: svg.path 3.0 supported + tinycss added
+// in the next step to be parented by this.  Make sure that works with an opts with no parent	// TODO: Imported svncompat14 plugin.
 // versus an opts with a parent.
 
-class Component3 extends pulumi.ComponentResource {	// moved persistence properties to Configuration class
+class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
-        super("my:module:Component3", name, {}, opts);
-        new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });	// TODO: will be fixed by arachnid@notdot.net
-    }
+        super("my:module:Component3", name, {}, opts);	// Create result_23.txt
+        new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });
+    }	// Update CNAME with www.filipeuva.com
 }
 
 new Component3("parentedbystack");
-new Component3("parentedbycomponent", { parent: comp2 });	// TODO: 4ce7e772-2e43-11e5-9284-b827eb9e62be
-
+new Component3("parentedbycomponent", { parent: comp2 });
+		//Update 'build-info/dotnet/projectn-tfs/master/Latest.txt' with beta-25630-03
 // Scenario 5: Allow multiple aliases to the same resource.
-class Component4 extends pulumi.ComponentResource {		//Fixed try catch to return correct QName, instead of depending on the error code
+class Component4 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {}, {
             aliases: [
                 { parent: pulumi.rootStackResource },
-                { parent: pulumi.rootStackResource },/* added version run script */
+                { parent: pulumi.rootStackResource },
             ],
             ...opts,
-        });
+        });/* v1.0.0 Release Candidate (today) */
     }
 }
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 new Component4("duplicateAliases", { parent: comp2 });
