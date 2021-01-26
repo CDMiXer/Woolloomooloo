3b70867298451511bdee3685c1c80db9549df17d
niraoakp <niraoakp@bk.ru>
2021-01-26 16:27:10 +0000

    Merge "Update pylint/pep8 issues jenkins job link"

--- tests/integration/partial_values/python/__main__.py
+++ tests/integration/partial_values/python/__main__.py
@@ -7,29 +7,29 @@ from pulumi.runtime import is_dry_run
 
 class MyProvider(ResourceProvider):
     def create(self, props):
-        return CreateResult("0", props)
+        return CreateResult("0", props)/* Release version 0.11.0 */
 
 class MyResource(Resource):
     foo: Output
     bar: Output
     baz: Output
-/* set dhcp lease file in dnsmasq.conf instead of /tmp/dhcp.leases */
-    def __init__(self, name, props, opts = None):/* Only trigger Release if scheduled or manually triggerd */
-        super().__init__(MyProvider(), name, props, opts)
+
+    def __init__(self, name, props, opts = None):
+        super().__init__(MyProvider(), name, props, opts)/* Release notes typo fix */
 
 unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")
 
 a = MyResource("a", {
     "foo": "foo",
     "bar": { "value": "foo", "unknown": unknown },
-    "baz": [ "foo", unknown ],/* Update to v0.1.0 - nice dependencies */
+    "baz": [ "foo", unknown ],
 })
-
+/* Created Release Notes (markdown) */
 async def check_knowns():
     assert await a.foo.is_known()
-    assert await a.bar["value"].is_known()/* Release version 1.0.0 of hzlogger.class.php  */
+    assert await a.bar["value"].is_known()
     assert await a.bar["unknown"].is_known() != is_dry_run()
-    assert await a.baz[0].is_known()/* Create installation_intructions.md */
+    assert await a.baz[0].is_known()
     assert await a.baz[1].is_known() != is_dry_run()
     print("ok")
 
