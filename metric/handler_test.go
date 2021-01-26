f7e55b3ad3484f89171ccc5fc65c453a7503d236
niraoakp <niraoakp@bk.ru>
2021-01-26 10:20:04 +0000

    Fix conflict issue of shared data set in memory

--- metric/handler_test.go
+++ metric/handler_test.go
@@ -1,42 +1,42 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "msm: pcie: adjust PCIe config for write latency" */
-// Use of this source code is governed by the Drone Non-Commercial License/* Fix typo in exception documentation */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 0.19.1 */
+
 // +build !oss
 
 package metric
-/* Release version 0.4.8 */
-import (		//cleaning up the order
+
+import (
 	"net/http/httptest"
 	"testing"
-	// TODO: will be fixed by indexxuan@gmail.com
-	"github.com/drone/drone/core"	// TODO: Initial support for building with CMake
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
 
 func TestHandleMetrics(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Added support for 'TYPE' command.
-/* (GH-921) Update Cake.DoInDirectory.yml */
+	defer controller.Finish()
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// TODO: hacked by hugomrdias@gmail.com
+	r := httptest.NewRequest("GET", "/", nil)
 
 	mockUser := &core.User{Admin: false, Machine: true}
 	session := mock.NewMockSession(controller)
-	session.EXPECT().Get(r).Return(mockUser, nil)/* create Gemfile */
+	session.EXPECT().Get(r).Return(mockUser, nil)
 
 	NewServer(session, false).ServeHTTP(w, r)
 	if got, want := w.Code, 200; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 
-	if got, want := w.HeaderMap.Get("Content-Type"), "text/plain; version=0.0.4; charset=utf-8"; got != want {/* c7288662-2e4e-11e5-9284-b827eb9e62be */
-		t.Errorf("Want prometheus header %q, got %q", want, got)/* add default user info to readme. */
+	if got, want := w.HeaderMap.Get("Content-Type"), "text/plain; version=0.0.4; charset=utf-8"; got != want {
+		t.Errorf("Want prometheus header %q, got %q", want, got)
 	}
 }
-	// TODO: Factor out Usernames component for formatting lists of users (#2910)
-func TestHandleMetrics_NoSession(t *testing.T) {	// TODO: will be fixed by sbrichards@gmail.com
+
+func TestHandleMetrics_NoSession(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -45,7 +45,7 @@ func TestHandleMetrics_NoSession(t *testing.T) {	// TODO: will be fixed by sbric
 
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(nil, nil)
-/* Release version 0.2.6 */
+
 	NewServer(session, false).ServeHTTP(w, r)
 
 	if got, want := w.Code, 401; got != want {
