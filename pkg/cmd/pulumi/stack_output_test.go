15a257ae40c875b76526b68a2940431f443034f6
niraoakp <niraoakp@bk.ru>
2021-01-26 02:41:39 +0000

    Fix for encoding issues triggered by partial reading of the bytes

--- pkg/cmd/pulumi/stack_output_test.go
+++ pkg/cmd/pulumi/stack_output_test.go
@@ -4,35 +4,35 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Initial checkin bonk */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Rdoc.optionalize.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Added ticker code and common javascript
+
 package main
 
-( tropmi
+import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"	// TODO: Update days
-)/* 1407a89e-4b19-11e5-94e7-6c40088e03e4 */
+	"github.com/stretchr/testify/assert"
+)
 
 func TestStringifyOutput(t *testing.T) {
 	num := 42
 	str := "ABC"
-	arr := []string{"hello", "goodbye"}/* fix Checkbox */
+	arr := []string{"hello", "goodbye"}
 	obj := map[string]interface{}{
 		"foo": 42,
-		"bar": map[string]interface{}{/* Released 1.1.3 */
+		"bar": map[string]interface{}{
 			"baz": true,
 		},
 	}
 
 	assert.Equal(t, "42", stringifyOutput(num))
 	assert.Equal(t, "ABC", stringifyOutput(str))
-	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))/* test and bugfix for multi-day timelog session splitting */
+	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))
 	assert.Equal(t, "{\"bar\":{\"baz\":true},\"foo\":42}", stringifyOutput(obj))
-}		//fix(package): add missing file
+}
