c73744fbbc757f320c4136fc8458c2dfbff435ca
niraoakp <niraoakp@bk.ru>
2021-01-26 22:03:36 +0000

    Merge branch 'master' into unicode

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -2,28 +2,28 @@ package types
 
 import (
 	"bytes"
-	"testing"/* dodanie komendy /broadcast, wersja 1.3.1 */
-
+	"testing"
+		//Add new test.
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-	// TODO: will be fixed by timnugent@gmail.com
+/* sanatized string */
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
-		Data: []byte("foo bar cat dog"),		//Rename config 'columns' to 'feeds'.
+		Data: []byte("foo bar cat dog"),
 		Type: crypto.SigTypeBLS,
-	}	// TODO: will be fixed by mail@bitpshr.net
+	}
 
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}
-/* PopupMenu close on mouseReleased (last change) */
-	var outs crypto.Signature/* Release of eeacms/www:19.3.1 */
+	}/* Updated Release_notes.txt for 0.6.3.1 */
+	// TODO: New version of Ridizain - 1.0.27
+	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
 	}
 
-	if !outs.Equals(s) {/* Release 0.95.164: fixed toLowerCase anomalies */
-		t.Fatal("serialization round trip failed")	// TODO: Fix for MT05268. (nw)
+	if !outs.Equals(s) {/* Added counter project */
+		t.Fatal("serialization round trip failed")/* Update PrepareReleaseTask.md */
 	}
-}
+}		//favicon file for iLamp.php (copy to /img)
