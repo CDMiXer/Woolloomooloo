e0acfad4712c72f007329e37fd31266f0f333ce2
niraoakp <niraoakp@bk.ru>
2021-01-26 14:51:49 +0000

    Lookup PubChem names/refs for added compounds and metabolites (#227)

--- gen/trace.go
+++ gen/trace.go
@@ -1,16 +1,16 @@
-// +build go1.8/* Merge "[IMPROV] Don't use carriage returns" */
+// +build go1.8
 
-package websocket/* forgot to mention programming language */
-/* Release Version 1.3 */
-import (/* Updated the r-shinyjqui feedstock. */
-	"crypto/tls"/* Merge "Updated README.md to be more accurate" */
+package websocket
+
+import (
+	"crypto/tls"
 	"net/http/httptrace"
 )
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
-	}		//updating poms for branch'release/1.0' with non-snapshot versions
+	}
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
