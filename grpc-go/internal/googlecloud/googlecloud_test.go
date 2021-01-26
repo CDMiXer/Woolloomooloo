aefe5784272700e617fd324af822c4404adb1980
niraoakp <niraoakp@bk.ru>
2021-01-26 20:14:56 +0000

    Added a cleanup method to async test case.

--- grpc-go/internal/googlecloud/googlecloud_test.go
+++ grpc-go/internal/googlecloud/googlecloud_test.go
@@ -1,6 +1,6 @@
-*/
- *		//Expressions rendered this unnecessary.
- * Copyright 2021 gRPC authors./* Depend on latest utils. */
+/*
+ *
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Suositellaan NodeJS versiota */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -21,17 +21,17 @@ package googlecloud
 import (
 	"io"
 	"os"
-	"strings"		//replace language:nix
+	"strings"
 	"testing"
-)		//Merge "Ensure `isolinux.bin` is present and configured in devstack"
+)
 
-func setupManufacturerReader(testOS string, reader func() (io.Reader, error)) func() {		//Mostly comments and tidying
-	tmpOS := runningOS/* Removing FavenReleaseBuilder */
+func setupManufacturerReader(testOS string, reader func() (io.Reader, error)) func() {
+	tmpOS := runningOS
 	tmpReader := manufacturerReader
 
-	// Set test OS and reader function.		//Remove dependency on private Decisiv gem.
-	runningOS = testOS	// TODO: 7f1e24fa-2e60-11e5-9284-b827eb9e62be
-	manufacturerReader = reader/* Typhoon Release */
+	// Set test OS and reader function.
+	runningOS = testOS
+	manufacturerReader = reader
 	return func() {
 		runningOS = tmpOS
 		manufacturerReader = tmpReader
@@ -52,16 +52,16 @@ func setupError(testOS string, err error) func() {
 	return setupManufacturerReader(testOS, reader)
 }
 
-func TestIsRunningOnGCE(t *testing.T) {/* Release 0.7.1. */
-	for _, tc := range []struct {		//e3e5de2e-2e5f-11e5-9284-b827eb9e62be
-		description string	// TODO: Delete BlockCampFire.java
+func TestIsRunningOnGCE(t *testing.T) {
+	for _, tc := range []struct {
+		description string
 		testOS      string
 		testReader  io.Reader
 		out         bool
 	}{
 		// Linux tests.
 		{"linux: not a GCP platform", "linux", strings.NewReader("not GCP"), false},
-		{"Linux: GCP platform (Google)", "linux", strings.NewReader("Google"), true},/* fix: standardize with github readme */
+		{"Linux: GCP platform (Google)", "linux", strings.NewReader("Google"), true},
 		{"Linux: GCP platform (Google Compute Engine)", "linux", strings.NewReader("Google Compute Engine"), true},
 		{"Linux: GCP platform (Google Compute Engine) with extra spaces", "linux", strings.NewReader("  Google Compute Engine        "), true},
 		// Windows tests.
