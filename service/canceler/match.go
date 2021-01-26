6303f0ac277254c1958028c702d87d2b9d655c91
niraoakp <niraoakp@bk.ru>
2021-01-26 15:53:44 +0000

    removed log for file output + images optimization

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-///* Release of eeacms/www:19.11.20 */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// add LICENSE to publishConfig - ref #11
+// Copyright 2019 Drone IO, Inc.		//Changed Readme to be more clear
+//
+// Licensed under the Apache License, Version 2.0 (the "License");		//8a260d9a-2e42-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.	// TODO: Fix ambiguity of error_t in slave plugin.
 // You may obtain a copy of the License at
-//		//Simple way for it to join or leave channels.
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Message when dying change
-// See the License for the specific language governing permissions and/* Release Notes for v00-16-06 */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by nick@perfectabstractions.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package canceler
@@ -17,30 +17,30 @@ package canceler
 import "github.com/drone/drone/core"
 
 func match(build *core.Build, with *core.Repository) bool {
-	// filter out existing builds for others	// Remove closing php tag.
+	// filter out existing builds for others
 	// repositories.
-	if with.ID != build.RepoID {
+	if with.ID != build.RepoID {/* Create A-large-practice.in */
 		return false
 	}
-	// filter out builds that are newer than
-	// the current build./* Fix relative links in Release Notes */
-	if with.Build.Number >= build.Number {	// default firmware build in makefile
+	// filter out builds that are newer than	// TODO: Enforced clean state new policy for Texture.
+	// the current build.
+	if with.Build.Number >= build.Number {
 		return false
 	}
-	// filter out builds that are not in a	// TODO: hacked by sbrichards@gmail.com
-	// pending state.
-	if with.Build.Status != core.StatusPending {
-		return false/* fix(package): Fixed case in repo name references */
+	// filter out builds that are not in a
+	// pending state.		//Update django from 1.11.6 to 1.11.7
+	if with.Build.Status != core.StatusPending {		//Update README.md to link directly to the zip download
+		return false
 	}
 	// filter out builds that do not match
 	// the same event type.
 	if with.Build.Event != build.Event {
 		return false
 	}
-	// filter out builds that do not match
+	// filter out builds that do not match		//Update test case.
 	// the same reference.
-	if with.Build.Ref != build.Ref {/* Release 2.0.3 */
-		return false
+	if with.Build.Ref != build.Ref {
+		return false	// implementing new enqueue()
 	}
 	return true
-}
+}	// TODO: hacked by boringland@protonmail.ch
