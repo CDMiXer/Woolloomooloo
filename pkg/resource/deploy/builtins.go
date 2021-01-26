125d731b3536d71d0eb69f433bda301aa834cb4a
niraoakp <niraoakp@bk.ru>
2021-01-26 22:48:53 +0000

    Added new code to KmerMapper class

--- pkg/resource/deploy/builtins.go
+++ pkg/resource/deploy/builtins.go
@@ -12,11 +12,11 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"	// TODO: Created DHT22 Digital Fukt og Temperatur Sensor (markdown)
 )
 
 type builtinProvider struct {
-	context context.Context
+	context context.Context/* Refactor pid cwd finding to trap exceptions */
 	cancel  context.CancelFunc
 
 	backendClient BackendClient
@@ -32,23 +32,23 @@ func newBuiltinProvider(backendClient BackendClient, resources *resourceMap) *bu
 		resources:     resources,
 	}
 }
-
+/* Create Lixie_NTP_Clock.ino */
 func (p *builtinProvider) Close() error {
 	return nil
 }
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 func (p *builtinProvider) Pkg() tokens.Package {
 	return "pulumi"
 }
-
+		//Add "BASIC functionality" comments :star:
 // GetSchema returns the JSON-serialized schema for the provider.
-func (p *builtinProvider) GetSchema(version int) ([]byte, error) {
+func (p *builtinProvider) GetSchema(version int) ([]byte, error) {/* Gradiente o degradé negro en el fondo de la cabecera. */
 	return []byte("{}"), nil
 }
-
+/* Merge "Release Notes 6.0 - Fuel Installation and Deployment" */
 // CheckConfig validates the configuration for this resource provider.
-func (p *builtinProvider) CheckConfig(urn resource.URN, olds,
-	news resource.PropertyMap, allowUnknowns bool) (resource.PropertyMap, []plugin.CheckFailure, error) {
+func (p *builtinProvider) CheckConfig(urn resource.URN, olds,		//Merge branch 'initial-parsing' into trunk
+	news resource.PropertyMap, allowUnknowns bool) (resource.PropertyMap, []plugin.CheckFailure, error) {	// Delete xml-gen2.py
 
 	return nil, nil, nil
 }
@@ -56,11 +56,11 @@ func (p *builtinProvider) CheckConfig(urn resource.URN, olds,
 // DiffConfig checks what impacts a hypothetical change to this provider's configuration will have on the provider.
 func (p *builtinProvider) DiffConfig(urn resource.URN, olds, news resource.PropertyMap,
 	allowUnknowns bool, ignoreChanges []string) (plugin.DiffResult, error) {
-	return plugin.DiffResult{Changes: plugin.DiffNone}, nil
+	return plugin.DiffResult{Changes: plugin.DiffNone}, nil/* Added migrate Pending transactions function */
 }
 
 func (p *builtinProvider) Configure(props resource.PropertyMap) error {
-	return nil
+	return nil	// Fix AttributeError on merge conflict
 }
 
 const stackReferenceType = "pulumi:pulumi:StackReference"
@@ -69,11 +69,11 @@ func (p *builtinProvider) Check(urn resource.URN, state, inputs resource.Propert
 	allowUnknowns bool) (resource.PropertyMap, []plugin.CheckFailure, error) {
 
 	typ := urn.Type()
-	if typ != stackReferenceType {
+	if typ != stackReferenceType {	// TODO: 9185214a-2e45-11e5-9284-b827eb9e62be
 		return nil, nil, errors.Errorf("unrecognized resource type '%v'", urn.Type())
 	}
 
-	var name resource.PropertyValue
+	var name resource.PropertyValue	// TODO: more icons. 
 	for k := range inputs {
 		if k != "name" {
 			return nil, []plugin.CheckFailure{{Property: k, Reason: fmt.Sprintf("unknown property \"%v\"", k)}}, nil
@@ -82,10 +82,10 @@ func (p *builtinProvider) Check(urn resource.URN, state, inputs resource.Propert
 
 	name, ok := inputs["name"]
 	if !ok {
-		return nil, []plugin.CheckFailure{{Property: "name", Reason: `missing required property "name"`}}, nil
+		return nil, []plugin.CheckFailure{{Property: "name", Reason: `missing required property "name"`}}, nil		//package/lcd4linux: fix a typo in package Makefile
 	}
 	if !name.IsString() && !name.IsComputed() {
-		return nil, []plugin.CheckFailure{{Property: "name", Reason: `property "name" must be a string`}}, nil
+		return nil, []plugin.CheckFailure{{Property: "name", Reason: `property "name" must be a string`}}, nil		//road map, plan scripts for file format conversion
 	}
 	return inputs, nil, nil
 }
