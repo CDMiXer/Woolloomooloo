965bbc90f406d07d3e51ab838a7659eaa9e773a8
niraoakp <niraoakp@bk.ru>
2021-01-26 04:37:33 +0000

    Merge pull request #33 from ferhung/master

--- core/step_test.go
+++ core/step_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Replace calls to `renderLines` w/ `resetDisplay` in `Editor`
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package core/* [snomed] Move SnomedReleases helper class to snomed.core.domain package */
-/* Release 0.4.1 */
-import "testing"	// Make some teld methods private.
+package core
 
-func TestStepIsDone(t *testing.T) {/* Release for 22.3.1 */
-	for _, status := range statusDone {
-		v := Step{Status: status}		//made workshop page
-		if v.IsDone() == false {
+import "testing"		//Delete Produtos-HistoricoDeVendas01.png
+
+func TestStepIsDone(t *testing.T) {		//Delete furnace_front_active_mt.png
+	for _, status := range statusDone {/* Merge branch 'dev' into fix/replication */
+		v := Step{Status: status}/* Clarify AUTHORS */
+		if v.IsDone() == false {/* Added ReleaseNotes to release-0.6 */
 			t.Errorf("Expect status %s is done", status)
-		}	// TODO: ADD: the user can add test cases, test suites or test sets to a test run
+		}		//Udpate copyright date
 	}
-	// TODO: hacked by jon@atack.com
-	for _, status := range statusNotDone {/* Remove duplicates parameters */
+
+	for _, status := range statusNotDone {
 		v := Step{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
