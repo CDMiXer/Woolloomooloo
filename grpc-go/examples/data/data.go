28e629da30bafe3f9a7bb9fd9cad04277bb8ac97
niraoakp <niraoakp@bk.ru>
2021-01-26 03:49:52 +0000

    Delete ZXCT1009F.lib

--- grpc-go/examples/data/data.go
+++ grpc-go/examples/data/data.go
@@ -1,16 +1,16 @@
 /*
  * Copyright 2020 gRPC authors.
- *
+ *		//Add GitPitch badge
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: hacked by fkautz@pseudocode.cc
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by m-ou.se@m-ou.se
  * limitations under the License.
  *
  */
@@ -19,13 +19,13 @@
 // directory.
 package data
 
-import (
-	"path/filepath"
+import (/* Ant files for ReleaseManager added. */
+	"path/filepath"/* Fixing Riak stats (again) */
 	"runtime"
 )
 
 // basepath is the root directory of this package.
-var basepath string
+var basepath string		//Added loader and pack classes and related libs
 
 func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
@@ -38,7 +38,7 @@ func init() {
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
 		return rel
-	}
+	}	// Fix "Sails.js in Action" link and add estimated publication date
 
 	return filepath.Join(basepath, rel)
 }
