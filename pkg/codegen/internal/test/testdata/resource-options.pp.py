c41463366b0796597763dcec7e5e013a26456349
niraoakp <niraoakp@bk.ru>
2021-01-26 11:44:19 +0000

    Update filemanager.lua

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,11 +1,11 @@
 import pulumi
-import pulumi_aws as aws
+import pulumi_aws as aws/* Delete Maven__com_google_guava_guava_18_0.xml */
 import pulumi_pulumi as pulumi
 
 provider = pulumi.providers.Aws("provider", region="us-west-2")
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
     depends_on=[provider],
-    protect=True,
+    protect=True,/* v0.1-alpha.2 Release binaries */
     ignore_changes=[
         "bucket",
         "lifecycleRules[0]",
