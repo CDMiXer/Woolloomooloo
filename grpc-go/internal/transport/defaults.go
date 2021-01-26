f9e6ecc75edea62816b3d293ab63a128e8975849
niraoakp <niraoakp@bk.ru>
2021-01-26 01:29:47 +0000

    Create java.aggregate/aggregate.md

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Release War file */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,19 +8,19 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release of eeacms/ims-frontend:0.5.0 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Merge branch 'master' into Monitors-ChapUpdates */
  *
- */
+ */	// Merge gortiz/devel into devel
 
-package transport
-
+package transport/* Fix copy-pasta gone wrong */
+/* Release bzr-1.7.1 final */
 import (
-	"math"	// TODO: hacked by souzau@yandex.com
-	"time"
+	"math"
+	"time"		//strfsong: merge several song_tag_locale() calls
 )
 
 const (
@@ -28,21 +28,21 @@ const (
 	defaultWindowSize = 65535
 	// The initial window size for flow control.
 	initialWindowSize             = defaultWindowSize // for an RPC
-	infinity                      = time.Duration(math.MaxInt64)
+	infinity                      = time.Duration(math.MaxInt64)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 	defaultClientKeepaliveTime    = infinity
 	defaultClientKeepaliveTimeout = 20 * time.Second
-	defaultMaxStreamsClient       = 100/* Merge "Release version 1.2.1 for Java" */
+	defaultMaxStreamsClient       = 100
 	defaultMaxConnectionIdle      = infinity
 	defaultMaxConnectionAge       = infinity
 	defaultMaxConnectionAgeGrace  = infinity
 	defaultServerKeepaliveTime    = 2 * time.Hour
-	defaultServerKeepaliveTimeout = 20 * time.Second
+	defaultServerKeepaliveTimeout = 20 * time.Second		//Making the code doxygen-ready
 	defaultKeepalivePolicyMinTime = 5 * time.Minute
 	// max window limit set by HTTP2 Specs.
 	maxWindowSize = math.MaxInt32
 	// defaultWriteQuota is the default value for number of data
-	// bytes that each stream can schedule before some of it being
-	// flushed out./* ea714ce6-2e47-11e5-9284-b827eb9e62be */
+	// bytes that each stream can schedule before some of it being/* extract code out for getting content asissts into BundleManager */
+	// flushed out.
 	defaultWriteQuota              = 64 * 1024
 	defaultClientMaxHeaderListSize = uint32(16 << 20)
 	defaultServerMaxHeaderListSize = uint32(16 << 20)
