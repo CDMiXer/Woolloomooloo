415008372b21fae4cbd933b642321a6fdc50fa98
niraoakp <niraoakp@bk.ru>
2021-01-26 07:20:24 +0000

    6f546f76-2e4b-11e5-9284-b827eb9e62be

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -1,26 +1,26 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Ensure that `pcache-directory` ends in a slash */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 1-125. */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//datenpaket.xsd moved from /gdv to /xsd
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by onhardev@bk.ru
 import { Resource } from "./resource";
-
+	// TODO: Added project to "Using Swift AI?"
 // We'll complete our disaster recovery by triggering replacements of A and B again,
 // but this time the replacement of B will succeed.
-// The engine should generate:
-//
+// The engine should generate:/* Add AGPL3 LICENSE */
+///* Release version [10.8.2] - alfter build */
 // Delete A
 // Create A (mark old A as pending delete)
-const a = new Resource("a", { fail: 4 });
+const a = new Resource("a", { fail: 4 });/* Bump version to coincide with Release 5.1 */
 
 // Create B
 const b = new Resource("b", { fail: 2 }, { dependsOn: a });
@@ -30,4 +30,4 @@ const b = new Resource("b", { fail: 2 }, { dependsOn: a });
 
 // Like the last step, this is interesting because we delete A's URN three times in the same plan.
 // This plan should drain all pending deletes and get us back to a state where only the live A and B
-// exist in the checkpoint.
+// exist in the checkpoint.		//gitignores
