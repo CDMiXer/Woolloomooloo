bbc0be46488be433c0122a48c1d4409a1da90c3c
niraoakp <niraoakp@bk.ru>
2021-01-26 09:00:27 +0000

    Merge "[FIX] jQuery: Make parseHTML work with domain relaxation"

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Reintroduces the preference on Quadtree optimization
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//release 0.5.6
-/* Get rid of command line parser */
+import * as pulumi from "@pulumi/pulumi";
+
 // Step 3: Run a query during `pulumi query`.
 pulumi.runtime
-    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
-    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
+    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")/* Apply maven formatting style */
+    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)	// Fixed dependencies to properly compile
     .all(async function(group) {
-        const count = await group.count();/* added new packages to jdk feature. */
+        const count = await group.count();
         if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
-            throw Error(`Expected 2 registered resources, got ${count}`);
+            throw Error(`Expected 2 registered resources, got ${count}`);/* Update celem.c */
         }
         console.log(group.key);
-        return (	// printInfo in the end
+        return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
             group.key === "pulumi:providers:pulumi-nodejs" ||
-            group.key === "pulumi:pulumi:Stack"
+            group.key === "pulumi:pulumi:Stack"/* Add content to the new file HowToRelease.md. */
         );
     })
     .then(res => {
-        if (res !== true) {/* Fix typo in PointerReleasedEventMessage */
+        if (res !== true) {
             throw Error("Expected query to return dynamic resource, provider, and stack resource");
-        }/* New Released. */
-    });
+        }
+    });/* junit test for loan charge create */
