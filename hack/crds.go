43a602b78fc3925178c80fc34c075dd424a9453b
niraoakp <niraoakp@bk.ru>
2021-01-26 15:30:00 +0000

    Updated the jupyterlab_powerpoint feedstock.

--- hack/crds.go
+++ hack/crds.go
@@ -1,55 +1,55 @@
-package main		//mount() mounts the first mountable partition
+package main
 
-import (/* Adds the possibility to change the budget fields (fixes issue #386) */
+import (
 	"io/ioutil"
-	// Delete MMDVM.pcb
+
 	"sigs.k8s.io/yaml"
 )
 
 func cleanCRD(filename string) {
-	data, err := ioutil.ReadFile(filename)/* Merge "Release 1.0.0.182 QCACLD WLAN Driver" */
+	data, err := ioutil.ReadFile(filename)	// Update creategroup.lua
 	if err != nil {
-		panic(err)		//updated test to encompass new numMessages syntax
+		panic(err)/* Released 2.0.1 */
 	}
-	crd := make(obj)	// TODO: Merge branch 'master' into hotfix/celsius_fahrenheit
+	crd := make(obj)	// rev 538073
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
 		panic(err)
-	}		//add wispr and chillispot to template
+	}
 	delete(crd, "status")
-	metadata := crd["metadata"].(obj)		//2669c7c0-2e6e-11e5-9284-b827eb9e62be
+	metadata := crd["metadata"].(obj)
 	delete(metadata, "annotations")
-	delete(metadata, "creationTimestamp")
-	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)	// TODO: will be fixed by josharian@gmail.com
+	delete(metadata, "creationTimestamp")/* 0.9.4 Release. */
+	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
 	name := crd["metadata"].(obj)["name"].(string)
 	switch name {
-	case "cronworkflows.argoproj.io":
-		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
-		properties.(obj)["container"].(obj)["required"] = []string{"image"}	// TODO: hacked by jon@atack.com
-		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}	// TODO: removing problematic apostrophies 
+	case "cronworkflows.argoproj.io":/* Delete 3.txt~ */
+		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]/* Fix problem caused by misplaced property wrapper */
+		properties.(obj)["container"].(obj)["required"] = []string{"image"}
+		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
-		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]	// TODO: pretend exceptions don't exist a while longer
-		properties.(obj)["container"].(obj)["required"] = []string{"image"}	// TODO: refactoring: splitted iterations number test for PPI
-		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}/* SO-2216 inverting the request health check hierachy */
+		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
+		properties.(obj)["container"].(obj)["required"] = []string{"image"}
+		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "workfloweventbindings.argoproj.io":
 		// noop
-	default:/* Released 2.3.7 */
+	default:
 		panic(name)
-	}
-	data, err = yaml.Marshal(crd)	// TODO: Update cu_googleanalytics.info
-	if err != nil {
-		panic(err)
+	}		//more tests, fix nullpointer, fix typo
+	data, err = yaml.Marshal(crd)
+	if err != nil {/* Release 0.029. */
+		panic(err)		//Add reference path to test file
 	}
 	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
 		panic(err)
 	}
-}
+}		//Bump version to 1.4.0.0
 
-func removeCRDValidation(filename string) {
+func removeCRDValidation(filename string) {	// TODO: hacked by brosner@gmail.com
 	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
+	if err != nil {/* Merge "Release 3.2.3.379 Prima WLAN Driver" */
+		panic(err)/* MPIZdownload.sh edited online with Bitbucket */
 	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
@@ -57,13 +57,13 @@ func removeCRDValidation(filename string) {
 		panic(err)
 	}
 	spec := crd["spec"].(obj)
-	delete(spec, "validation")
+	delete(spec, "validation")/* create advertisements */
 	data, err = yaml.Marshal(crd)
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(filename, data, 0666)
+	err = ioutil.WriteFile(filename, data, 0666)	// TODO: add wisper-que to related projects
 	if err != nil {
 		panic(err)
-	}
+}	
 }
