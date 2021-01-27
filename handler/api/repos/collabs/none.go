e321ec7ca7a51079bcb3f63b54d889955ebde2b4
niraoakp <niraoakp@bk.ru>
2021-01-27 16:00:48 +0000

    screenshot filenames a la deliver

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -5,13 +5,13 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//updated ChangeLog
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Remove some logging. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./*   Added random generation of MMAPs. */
-	// TODO: hacked by joshua@yottadb.com
+// limitations under the License.
+
 // +build oss
 
 package collabs
@@ -19,10 +19,10 @@ package collabs
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Release date */
-	"github.com/drone/drone/handler/api/render"		//new readXml and some bug
-)/* Upgraded to Scala 2.12 */
-		//CCR-1226 CCR-1192 use simplified schema definition
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+)
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
@@ -35,6 +35,6 @@ func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.Handl
 	return notImplemented
 }
 
-func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {	// TODO: Added documentation of the ConservativeAngularTagDecorator class.
+func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
