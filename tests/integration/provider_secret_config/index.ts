c21eedf1a9581be91212178f2fdccb19ac9d431e
niraoakp <niraoakp@bk.ru>
2021-01-27 15:03:15 +0000

    Updated: shoes-rb 3.3.7

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,4 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";/* Merge "Pass textDirectionHeuristic to TextLayout" into androidx-crane-dev */
+import * as pulumi from "@pulumi/pulumi";
 
 // Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
 // with secret configuration values, so long as these values are themselves strings.
@@ -7,5 +7,5 @@ class DynamicProvider extends pulumi.ProviderResource {
         super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
     }
 }
-		//9e59fb70-4b19-11e5-b6b9-6c40088e03e4
+
 const p = new DynamicProvider("p");
