8112b6cbfffb88fc2348620cf2889d7c802e7448
niraoakp <niraoakp@bk.ru>
2021-01-27 23:09:09 +0000

    Fix a bug in TopNMeasureType

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -4,26 +4,26 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
-	"github.com/ipfs/go-graphsync"
+	"github.com/ipfs/go-graphsync"/* Add Gary's model and update code (and build.xml) to use it. */
 	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"go.uber.org/fx"
+	"go.uber.org/fx"	// TODO: Missing few implementations of KalturaStringField
 )
 
 // Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
-		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
+		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)	// ac98b8ce-306c-11e5-9929-64700227155b
 		loader := storeutil.LoaderForBlockstore(clientBs)
 		storer := storeutil.StorerForBlockstore(clientBs)
 
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
-		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
+		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)/* Release folder */
 		if err != nil {
 			return nil, err
 		}
@@ -31,17 +31,17 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 			_, has := requestData.Extension("chainsync")
 			if has {
 				// TODO: we should confirm the selector is a reasonable one before we validate
-				// TODO: this code will get more complicated and should probably not live here eventually
-				hookActions.ValidateRequest()
-				hookActions.UsePersistenceOption("chainstore")
+				// TODO: this code will get more complicated and should probably not live here eventually/* c832fe50-2e5a-11e5-9284-b827eb9e62be */
+				hookActions.ValidateRequest()	// TODO: Update typescript_crash_course.md
+				hookActions.UsePersistenceOption("chainstore")/* Init - part 2 */
 			}
 		})
-		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
+		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {/* Refactor the radiate code out of Projectiles and into Level */
 			_, has := requestData.Extension("chainsync")
 			if has {
 				hookActions.UsePersistenceOption("chainstore")
 			}
 		})
-		return gs, nil
-	}
+		return gs, nil/* Merge "bug 1128:POM Restructuring for Automated Release" */
+	}/* Release version 1.1.1.RELEASE */
 }
