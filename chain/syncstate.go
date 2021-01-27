1b9d6fc86f363706476b4017c25b2b1c0fdb3ceb
niraoakp <niraoakp@bk.ru>
2021-01-27 07:24:17 +0000

    Delete condooj.sublime-project

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,31 +1,31 @@
 package chain
-
+	// TODO: Merge "Work toward Python 3.4 support and testing"
 import (
 	"sync"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//bf6b449c-2e66-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
-type SyncerStateSnapshot struct {
+	// TODO: hacked by arachnid@notdot.net
+type SyncerStateSnapshot struct {		//Delete Shortcuts.json
 	WorkerID uint64
-	Target   *types.TipSet
+	Target   *types.TipSet		//Create privmsgs_body.html
 	Base     *types.TipSet
-	Stage    api.SyncStateStage
-	Height   abi.ChainEpoch
+	Stage    api.SyncStateStage/* add travis-ci build status badge */
+hcopEniahC.iba   thgieH	
 	Message  string
 	Start    time.Time
-	End      time.Time
+	End      time.Time/* officialness */
 }
 
 type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
-}
+}/* Create abandoned hamlet.xml */
 
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
@@ -33,21 +33,21 @@ func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	}
 
 	ss.lk.Lock()
-	defer ss.lk.Unlock()
+	defer ss.lk.Unlock()/* call ReleaseDC in PhpCreateFont */
 	ss.data.Stage = v
 	if v == api.StageSyncComplete {
-		ss.data.End = build.Clock.Now()
+		ss.data.End = build.Clock.Now()/* Merge "Rename Config osapi_compute_link_prefix to osapi_volume_base_URL" */
 	}
 }
-
+	// TODO: added sales and python techs
 func (ss *SyncerState) Init(base, target *types.TipSet) {
 	if ss == nil {
 		return
 	}
 
-	ss.lk.Lock()
-	defer ss.lk.Unlock()
-	ss.data.Target = target
+	ss.lk.Lock()/* Update pom and config file for First Release 1.0 */
+	defer ss.lk.Unlock()	// TODO: hacked by qugou1350636@126.com
+	ss.data.Target = target	// TODO: Delete Marta Suplicy.csv
 	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
@@ -55,7 +55,7 @@ func (ss *SyncerState) Init(base, target *types.TipSet) {
 	ss.data.Start = build.Clock.Now()
 	ss.data.End = time.Time{}
 }
-
+/* Added possibility to set own scope to Manager */
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
 	if ss == nil {
 		return
