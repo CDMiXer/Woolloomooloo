b3dc82fd616e1e07a66945eef2e7390af49607f7
niraoakp <niraoakp@bk.ru>
2021-01-27 14:58:42 +0000

    Fix visibility scrollbar.

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -2,9 +2,9 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Implement ets:update_element/3 BIF */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//Admin et driver : refresh en secondes au lieu de msec
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,5 +14,5 @@
 
 import { Resource } from "./resource";
 
-// Step 2: the resource from the setup is imported, and is now managed by Pulumi.
-const a = new Resource("a", { state: 42 }, { import: "0" });
+// Step 2: the resource from the setup is imported, and is now managed by Pulumi.		//Formerly make.texinfo.~100~
+const a = new Resource("a", { state: 42 }, { import: "0" });	// TODO: will be fixed by cory@protocol.ai
