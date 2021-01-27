567baa8f4532d427812a7a5b3aac5320ca669737
niraoakp <niraoakp@bk.ru>
2021-01-27 13:10:17 +0000

    Update intra-lib.md

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// logger package doc minor changes
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: Corregir enlace a meetups
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Gerrit 2.4 ReleaseNotes" into stable-2.4 */
-//
+//     http://www.apache.org/licenses/LICENSE-2.0
+///* Merge branch 'master' into add-alt-text-to-delete-button */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//connexion -> connection
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning.
+// goconst linter's warning.	// TODO: support for Ctrl+C
 //
 // nolint: lll, goconst
-package nodejs/* fix beeper function of ProRelease3 */
+package nodejs
 
 import (
 	"testing"
-
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+/* Branches gemerged. #1 */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"/* changed file extension */
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,29 +30,29 @@ var testPackageSpec = schema.PackageSpec{
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
-,}	
-	Types: map[string]schema.ComplexTypeSpec{	// TODO: Minor typographic and punctuation changes.
+	},
+	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
-			ObjectTypeSpec: schema.ObjectTypeSpec{
+			ObjectTypeSpec: schema.ObjectTypeSpec{/* [IOWorker] Forward declaration of a few class */
 				Description: "The resource options object.",
-				Type:        "object",
+				Type:        "object",	// TODO: [Automated] [zoren] New translations
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
 						Description: "A string prop.",
 						TypeSpec: schema.TypeSpec{
-							Type: "string",	// TODO: will be fixed by remco@dutchcoders.io
-						},
-					},		//f63438f2-2e58-11e5-9284-b827eb9e62be
+							Type: "string",
+						},/* suggest changes to thor travis:vms:update */
+					},
 				},
 			},
 		},
-	},
+	},/* TIBCO Release 2002Q300 */
 	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
-			InputProperties: map[string]schema.PropertySpec{	// Update NFCDetectPresenter.java
-				"corsRules": {	// TODO: will be fixed by ng8eke@163.com
+			InputProperties: map[string]schema.PropertySpec{
+				"corsRules": {
 					TypeSpec: schema.TypeSpec{
-						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
+						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",		//JsonBucketHolder to BucketHolder.
 					},
 				},
 			},
@@ -63,30 +63,30 @@ var testPackageSpec = schema.PackageSpec{
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
 
-	pkg, err := schema.ImportSpec(testPackageSpec, nil)	// refactor plus working new command
+	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
-}/* Released version 0.8.1 */
+}
 
 func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
-	pkg := getTestPackage(t)
-	d := DocLanguageHelper{}
-	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {/* Add viewBox to logo north image */
+	pkg := getTestPackage(t)	// Adding Reactions.thrift with Matcher definitions
+	d := DocLanguageHelper{}	// TODO: hacked by fkautz@pseudocode.cc
+	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"
-		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")
-		assert.Equal(t, expected, link)
+		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")	// remove extra slots
+		assert.Equal(t, expected, link)	// TODO: Added implementation of FundingCapacity (see "Discounting Damage").
 	})
 	t.Run("GenerateInvokeOptionsLink", func(t *testing.T) {
-		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"
+		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"/* Released springjdbcdao version 1.8.3 */
 		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")
-		assert.Equal(t, expected, link)
+		assert.Equal(t, expected, link)/* Let's try this again. Testing Jenkins */
 	})
 }
 
 func TestGetDocLinkForResourceType(t *testing.T) {
-	pkg := getTestPackage(t)	// TODO: will be fixed by ng8eke@163.com
+	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
-	expected := "/docs/reference/pkg/nodejs/pulumi/aws/s3/#Bucket"/* Release v6.6 */
+	expected := "/docs/reference/pkg/nodejs/pulumi/aws/s3/#Bucket"
 	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")
 	assert.Equal(t, expected, link)
 }
