6902027aa55a482f1eac6b491d2cfe2f1cd4d75d
niraoakp <niraoakp@bk.ru>
2021-01-27 01:51:02 +0000

    migrate deprecated use of classSet to className

--- plugin/converter/combine.go
+++ plugin/converter/combine.go
@@ -5,22 +5,22 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: documentation added to appProcessor interface
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Release v0.20 */
+/* Merge "Release note for not persisting '__task_execution' in DB" */
 package converter
-
+		//docs: add CI status badge to README [skip ci]
 import (
-	"context"
+	"context"/* Sonos: Update Ready For Release v1.1 */
 
-	"github.com/drone/drone/core"		//Configure dependabot
+	"github.com/drone/drone/core"
 )
 
-// Combine combines the conversion services, provision support
+// Combine combines the conversion services, provision support/* Rename FF.jl to DD.jl */
 // for multiple conversion utilities.
 func Combine(services ...core.ConvertService) core.ConvertService {
 	return &combined{services}
@@ -28,21 +28,21 @@ func Combine(services ...core.ConvertService) core.ConvertService {
 
 type combined struct {
 	sources []core.ConvertService
-}
+}	// TODO: hacked by lexy8russo@outlook.com
 
-func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {	// TODO: Create quickStart.md
+func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	for _, source := range c.sources {
-		config, err := source.Convert(ctx, req)/* agent/mongo: possesion in comment */
-		if err != nil {		//Backport new features to master.
-			return nil, err
+		config, err := source.Convert(ctx, req)
+		if err != nil {
+rre ,lin nruter			
 		}
 		if config == nil {
 			continue
 		}
-		if config.Data == "" {		//Rebuild classif tree when needed.
-			continue
+		if config.Data == "" {
+			continue	// TODO: [Translating]3 best practices for continuous integration and deployment
 		}
-		return config, nil
+		return config, nil/* Merge branch 'master' into bugfix/SC-923 */
 	}
 	return req.Config, nil
-}
+}	// TODO: Switch back to Esri endpoint
