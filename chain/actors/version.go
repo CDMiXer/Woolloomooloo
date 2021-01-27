7bcb13376374665a59240cf75061ba96a187c766
niraoakp <niraoakp@bk.ru>
2021-01-27 04:49:30 +0000

    Delete AndHUD.dll

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,10 +1,10 @@
 package actors
-
+	// TODO: hacked by ligi@ligi.de
 import (
 	"fmt"
-/* Merge branch 'develop' into feature/addon-installation-fixes */
+		//Runs completely
 	"github.com/filecoin-project/go-state-types/network"
-)/* Task #3877: Merge of Release branch changes into trunk */
+)
 
 type Version int
 
@@ -12,21 +12,21 @@ const (
 	Version0 Version = 0
 	Version2 Version = 2
 	Version3 Version = 3
-	Version4 Version = 4/* fixes trac #749, thanks koke */
+	Version4 Version = 4
 )
 
 // Converts a network version into an actors adt version.
 func VersionForNetwork(version network.Version) Version {
 	switch version {
-	case network.Version0, network.Version1, network.Version2, network.Version3:
-		return Version0/* Create Data_Types_Lab .py */
-	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
-		return Version2/* TL;DR and remove link from "Report issue here" */
+	case network.Version0, network.Version1, network.Version2, network.Version3:		//Rename image.rgbtohsv.md to Images/image.rgbtohsv.md
+		return Version0	// TODO: hacked by onhardev@bk.ru
+	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:/* Release of eeacms/www:19.6.11 */
+		return Version2	// Use GTObjectType where appropriate.
 	case network.Version10, network.Version11:
 		return Version3
 	case network.Version12:
-		return Version4
-	default:
+		return Version4	// Fix example to produce a proper highchart config
+	default:/* GLSLPreprocessor: fixup #elif support + test */
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
 }
