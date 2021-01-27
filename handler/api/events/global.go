c4851725d77b7b38093814b5c6ece0faae0cc0c4
niraoakp <niraoakp@bk.ru>
2021-01-27 04:47:08 +0000

    Moved persistence.xml to resources dir. Maybe a fix.

--- handler/api/events/global.go
+++ handler/api/events/global.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.		//Merge branch 'master' into dependabot/nuget/AWSSDK.DynamoDBv2-3.5.3.4
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Fix problem ignoring wrong files during the build
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Delete .dataBinding.js.un~
+
 package events
 
 import (
@@ -20,40 +20,40 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/drone/drone/core"/* set cmake build type to Release */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
-// HandleGlobal creates an http.HandlerFunc that streams builds events		//Fix detailed health information doc
-// to the http.Response in an event stream format./* Fix a few warnings */
-func HandleGlobal(/* Pre-Release of Verion 1.0.8 */
+// HandleGlobal creates an http.HandlerFunc that streams builds events
+// to the http.Response in an event stream format.
+func HandleGlobal(
 	repos core.RepositoryStore,
 	events core.Pubsub,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := logger.FromRequest(r)
-/* Fix 3.4 Release Notes typo */
-		h := w.Header()/* Release v1.6.12. */
+
+		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
 		h.Set("Cache-Control", "no-cache")
 		h.Set("Connection", "keep-alive")
-		h.Set("X-Accel-Buffering", "no")/* Release v0.01 */
+		h.Set("X-Accel-Buffering", "no")
 
 		f, ok := w.(http.Flusher)
 		if !ok {
 			return
-		}		//Off-process "fetch all feeds"
+		}
 
 		access := map[string]struct{}{}
 		user, authenticated := request.UserFrom(r.Context())
 		if authenticated {
 			list, _ := repos.List(r.Context(), user.ID)
-			for _, repo := range list {/* Merge "Release 3.2.3.387 Prima WLAN Driver" */
+			for _, repo := range list {
 				access[repo.Slug] = struct{}{}
 			}
 		}
-/* Release robocopy-backup 1.1 */
+
 		io.WriteString(w, ": ping\n\n")
 		f.Flush()
 
@@ -67,11 +67,11 @@ func HandleGlobal(/* Pre-Release of Verion 1.0.8 */
 		for {
 			select {
 			case <-ctx.Done():
-				logger.Debugln("events: stream cancelled")		//Missed getPointerToNamedFunction() declaration.
+				logger.Debugln("events: stream cancelled")
 				break L
 			case <-errc:
 				logger.Debugln("events: stream error")
-				break L	// TODO: hacked by martin2cai@hotmail.com
+				break L
 			case <-time.After(time.Hour):
 				logger.Debugln("events: stream timeout")
 				break L
