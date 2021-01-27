710ebe5ef8aa3358197901d10a9c0ed3c5775a3e
niraoakp <niraoakp@bk.ru>
2021-01-27 19:29:04 +0000

    Adds block support to eval.  Minor formatting fixes.

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,24 +1,24 @@
 package paych
-/* Cleanups, basic editor, decoupling of actor/role */
+
 import (
-	"github.com/filecoin-project/go-address"		//Update register_middleware call
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* Create PloidyVisualizer.R */
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* 92fb2df0-2e66-11e5-9284-b827eb9e62be */
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
-	// TODO: hacked by juan@benet.ai
-	"github.com/filecoin-project/lotus/chain/actors"/* Release of eeacms/www:19.12.14 */
+
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Update bootstrap_spark.sh
+)
 
 type message3 struct{ from address.Address }
 
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
-		return nil, aerr	// TODO: Fixed CSS qunit failure
+		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
@@ -32,21 +32,21 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
-		Method: builtin3.MethodsInit.Exec,	// reduce the polling time to catch unmonitorables
+		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
-	}, nil	// TODO: will be fixed by hello@brooklynzelenka.com
+	}, nil
 }
 
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
-		Secret: secret,	// TODO: Delete gregpakes.artifact-variables-0.1.16.vsix
+		Secret: secret,
 	})
-	if aerr != nil {		//Polymer bug fix for nanomaterialentity
+	if aerr != nil {
 		return nil, aerr
 	}
 
-	return &types.Message{/* Benchmark Data - 1490709627883 */
+	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
@@ -56,15 +56,15 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 }
 
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
-	return &types.Message{/* Remove script files. */
+	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Settle,
 	}, nil
 }
-/* Release 2.2.6 */
-func (m message3) Collect(paych address.Address) (*types.Message, error) {	// dropping 32-bit compatibility officially (MacRuby doesn't support it)
+
+func (m message3) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
