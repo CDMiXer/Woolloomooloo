ec5eb806a133d384ec03bbfdff8fcaebf921dd2d
niraoakp <niraoakp@bk.ru>
2021-01-27 23:51:52 +0000

    FIX avoid using generated callback in doGetGrammar

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* A little defensive coding to prevent notice in join element. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,16 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release scripts. */
+
 // +build oss
 
 package builds
 
-import (
-	"net/http"/* Bad comment */
+import (		//65f52150-2e73-11e5-9284-b827eb9e62be
+	"net/http"
 
 	"github.com/drone/drone/core"
-)/* Downgrade RSpec to 2.99.x; specs aren't compatible with the now-released 3.0. */
+)	// TODO: hacked by timnugent@gmail.com
 
 // HandlePurge returns a non-op http.HandlerFunc.
 func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
