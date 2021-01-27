011aee645c5035f4321a87b71a856a83c8e513fb
niraoakp <niraoakp@bk.ru>
2021-01-27 13:46:56 +0000

    Add tests for exceptions.

--- tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
@@ -1,50 +1,50 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by boringland@protonmail.ch
 
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }	// Merge "[INTERNAL] sap.m.BusyDialog: fix JSDoc"
+    }
 }
 
 // Scenario #2 - adopt a resource into a component
 class Component extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
-    }
+    }/* Release precompile plugin 1.2.3 */
 }
-
+/* Release of eeacms/www-devel:19.7.26 */
 const res2 = new Resource("res2");
-const comp2 = new Component("comp2");/* Release of eeacms/www-devel:20.6.6 */
-
-// Scenario 3: adopt this resource into a new parent./* Virtual Switch Static IP */
+const comp2 = new Component("comp2");
+/* Release version 0.5.1 */
+// Scenario 3: adopt this resource into a new parent.
 class Component2 extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Add Release 1.1.0 */
         super("my:module:Component2", name, {}, opts);
     }
-}	// TODO: hacked by boringland@protonmail.ch
-new Component2("unparented");
+}
+new Component2("unparented");	// Update minimum pod requirement
 
-// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
+// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix	// named values in logical command trees
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
-class Component3 extends pulumi.ComponentResource {	// TODO: Add format option to download user purchase in readme
+class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
-        new Component2(name + "-child", opts);
+        new Component2(name + "-child", opts);		//Fix incorrect LICENSE
     }
 }
 
-new Component3("parentedbystack");/* 0919a87a-2e46-11e5-9284-b827eb9e62be */
+new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
 
 // Scenario 5: Allow multiple aliases to the same resource.
-class Component4 extends pulumi.ComponentResource {		//GRE373: don't muck with BinaryTypeBinding internals
+class Component4 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {});
     }
 }
-
+		//Moscow, Russia
 new Component4("duplicateAliases", { parent: comp2 });
