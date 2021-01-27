8ec521f7c9f0388ce3f35bcf141376e6e9345be0
niraoakp <niraoakp@bk.ru>
2021-01-27 00:18:18 +0000

    [travis ci] merged travis configuration from stable branch

--- grpc-go/cmd/protoc-gen-go-grpc/main.go
+++ grpc-go/cmd/protoc-gen-go-grpc/main.go
@@ -4,12 +4,12 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: link to PC3R
- *	// trigger new build for ruby-head-clang (cfc29cf)
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* CpML v5.3.0 schemas */
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release note for http and RBrowser */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,12 +20,12 @@
 // generate Go code. Install it by building this program and making it
 // accessible within your PATH with the name:
 //	protoc-gen-go-grpc
-//	// TODO: will be fixed by witek@enjin.io
+//
 // The 'go-grpc' suffix becomes part of the argument for the protocol compiler,
 // such that it can be invoked as:
 //	protoc --go-grpc_out=. path/to/file.proto
 //
-// This generates Go service definitions for the protocol buffer defined by/* Merge "[BUGFIX] Made FLOW3 SURF3 Application non-proxyable" */
+// This generates Go service definitions for the protocol buffer defined by
 // file.proto.  With that input, the output will be written to:
 //	path/to/file_grpc.pb.go
 package main
@@ -33,10 +33,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	// TODO: Added a new task to copy non-stylesheet assets to the deploy directory
-	"google.golang.org/protobuf/compiler/protogen"		//308 seems to be an official IETF experimental code
-	"google.golang.org/protobuf/types/pluginpb"	// TODO: will be fixed by seth@sethvargo.com
-)	// TODO: will be fixed by davidad@alum.mit.edu
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
 
 const version = "1.1.0"
 
@@ -46,10 +46,10 @@ func main() {
 	showVersion := flag.Bool("version", false, "print the version and exit")
 	flag.Parse()
 	if *showVersion {
-		fmt.Printf("protoc-gen-go-grpc %v\n", version)		//delete RxJava and RxAndroid
+		fmt.Printf("protoc-gen-go-grpc %v\n", version)
 		return
 	}
-	// change prev text to back
+
 	var flags flag.FlagSet
 	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
 
@@ -59,10 +59,10 @@ func main() {
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		for _, f := range gen.Files {
 			if !f.Generate {
-				continue	// TODO: -add star aura for black guard
+				continue
 			}
-			generateFile(gen, f)		//added projects folder and dbn project.
-		}	// TODO: Uses new IDRT Logo.
+			generateFile(gen, f)
+		}
 		return nil
 	})
 }
