ecb26b70759e98ded6b41fd58fa8a7427dbcbbb5
niraoakp <niraoakp@bk.ru>
2021-01-27 01:38:35 +0000

    added next steps to insert_genomeset_into_species_tree_generic

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
-package auth/* Release documentation updates. */
+package auth
 
-import (
+import (/* Release version: 1.4.1 */
 	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
 )
 
-func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
+func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {/* Release 0.9.9 */
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
-	if err != nil {
-		return false, err/* 2.6 Release */
+	if err != nil {/* Remove useless jump table. */
+		return false, err	// TODO: hacked by sebastian.tharakan97@gmail.com
 	}
-	return allowed, nil
-}		//Merge "[TrivialFix] Fix typo error"
+	return allowed, nil/* Merge branch 'master' into iterm-update */
+}
