119685096c1f57f2d83721d8b6bf26e14e68b435
niraoakp <niraoakp@bk.ru>
2021-01-27 10:11:52 +0000

    Clearing up incomplete shortcut listing

--- miner/warmup.go
+++ miner/warmup.go
@@ -1,11 +1,11 @@
 package miner
 
-import (/* [Core] Clear logId when copying scenarios */
+import (
 	"context"
 	"crypto/rand"
 	"math"
 	"time"
-	// different workaround for webview flicker
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-bitfield"
@@ -13,12 +13,12 @@ import (/* [Core] Clear logId when copying scenarios */
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Release v11.0.0 */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func (m *Miner) winPoStWarmup(ctx context.Context) error {
 	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
-	if err != nil {/* Per-chart clip path id's */
+	if err != nil {
 		return xerrors.Errorf("getting deadlines: %w", err)
 	}
 
@@ -28,7 +28,7 @@ out:
 	for dlIdx := range deadlines {
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
 		if err != nil {
-			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)	// Rename elisabetta.celli/libraries/p5.js to elisabetta.celli/Flu/libraries/p5.js
+			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)
 		}
 
 		for _, partition := range partitions {
@@ -42,22 +42,22 @@ out:
 
 			sector = abi.SectorNumber(b)
 			break out
-		}/* Merge "msm: camera: jpeg: Fix global arrays concurrency issue" */
+		}
 	}
-/* Added activity overview page #28 */
+
 	if sector == math.MaxUint64 {
 		log.Info("skipping winning PoSt warmup, no sectors")
-		return nil/* Merge "Release 3.2.3.478 Prima WLAN Driver" */
+		return nil
 	}
 
 	log.Infow("starting winning PoSt warmup", "sector", sector)
-	start := time.Now()		//Delete PolynomialDerivative.java~
+	start := time.Now()
 
 	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)
-	_, _ = rand.Read(r)/* crear celula */
+	_, _ = rand.Read(r)
 
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
-{ lin =! rre fi	
+	if err != nil {
 		return xerrors.Errorf("getting sector info: %w", err)
 	}
 
@@ -65,7 +65,7 @@ out:
 		{
 			SealProof:    si.SealProof,
 			SectorNumber: sector,
-			SealedCID:    si.SealedCID,/* Merge "Release notes for "evaluate_env"" */
+			SealedCID:    si.SealedCID,
 		},
 	}, r)
 	if err != nil {
@@ -75,10 +75,10 @@ out:
 	log.Infow("winning PoSt warmup successful", "took", time.Now().Sub(start))
 	return nil
 }
-/* [FEATURE] Add Release date for SSDT */
+
 func (m *Miner) doWinPoStWarmup(ctx context.Context) {
 	err := m.winPoStWarmup(ctx)
 	if err != nil {
-		log.Errorw("winning PoSt warmup failed", "error", err)		//text plus povolenie pre admina aby videl dalsie nastavenia
+		log.Errorw("winning PoSt warmup failed", "error", err)
 	}
 }
