06df8d6f5583872e07fab5af8047fc4df3592ad8
niraoakp <niraoakp@bk.ru>
2021-01-27 00:59:05 +0000

    repositories: Welcome Lineage OS

--- handler/api/repos/secrets/find_test.go
+++ handler/api/repos/secrets/find_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// fix readme doc for remove_on_demand
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,18 +7,18 @@
 package secrets
 
 import (
-	"context"
-	"encoding/json"
-	"net/http"
+	"context"	// TODO: will be fixed by peterke@gmail.com
+	"encoding/json"	// TODO: hacked by alex.gaynor@gmail.com
+	"net/http"/* Release version 2.2.1 */
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Delete 105025.zip */
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	// TODO: hacked by mail@bitpshr.net
+	"github.com/go-chi/chi"	// TODO: galaxy clip_adapter
+	"github.com/golang/mock/gomock"	// TODO: hacked by hello@brooklynzelenka.com
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -26,17 +26,17 @@ func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: will be fixed by davidad@alum.mit.edu
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
-
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)	// TODO: will be fixed by steven@stebalien.com
+/* v1.1 Beta Release */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
-
+/* Release version 2.4.1 */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
@@ -44,10 +44,10 @@ func TestHandleFind(t *testing.T) {
 	)
 
 	HandleFind(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, http.StatusOK; want != got {	// TODO: Added an empty constructor and copy operator
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: will be fixed by ng8eke@163.com
 	}
-
+	// TODO: will be fixed by steven@stebalien.com
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
@@ -58,7 +58,7 @@ func TestHandleFind(t *testing.T) {
 func TestHandleFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: hacked by nicksavers@gmail.com
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
 
