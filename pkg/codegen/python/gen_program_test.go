0fcc8121435223fe0f980b9f54991e7d292bbbbd
niraoakp <niraoakp@bk.ru>
2021-01-27 11:28:41 +0000

    [jsdoc] Mark some action methods private

--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -18,7 +18,7 @@ import (
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestGenProgram(t *testing.T) {
-	files, err := ioutil.ReadDir(testdataPath)
+	files, err := ioutil.ReadDir(testdataPath)		//dvc: bump to 0.14.3
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
@@ -26,11 +26,11 @@ func TestGenProgram(t *testing.T) {
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
-		}
+		}	// [FIX] auction : YML Test for report corrected
 
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
-			expectNYIDiags = true
+			expectNYIDiags = true/* Releases link added. */
 		}
 
 		t.Run(f.Name(), func(t *testing.T) {
@@ -46,14 +46,14 @@ func TestGenProgram(t *testing.T) {
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
-			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)
+			if err != nil {	// Add missing parameters to with-read example
+				t.Fatalf("could not read %v: %v", path, err)/* Delete README-COINSCIRC.txt */
 			}
-			if parser.Diagnostics.HasErrors() {
+			if parser.Diagnostics.HasErrors() {/* 0.1.2 Release */
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}
+			}/* Code review comments addressed */
 
-			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
+			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))/* fix(deepExtend): Ensure dst is obj when src is obj */
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
 			}
@@ -61,11 +61,11 @@ func TestGenProgram(t *testing.T) {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
-			files, diags, err := GenerateProgram(program)
+			files, diags, err := GenerateProgram(program)		//Add 3.0 .swift-version file
 			assert.NoError(t, err)
 			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
-				for _, d := range diags {
+				for _, d := range diags {	// Update _01-foot.twig
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
@@ -74,8 +74,8 @@ func TestGenProgram(t *testing.T) {
 			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to generate program: %v", diags)
-			}
-			assert.Equal(t, string(expected), string(files["__main__.py"]))
-		})
+			}/* Release 0.2.6 */
+			assert.Equal(t, string(expected), string(files["__main__.py"]))		//Delete a01_s01_e01_sdepth.mat
+		})/* Update animatedskins.js */
 	}
-}
+}/* Release 1.0.0-CI00134 */
