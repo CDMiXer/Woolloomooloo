6072b872818e4ca99fc2662db22705c8b8c71239
niraoakp <niraoakp@bk.ru>
2021-01-27 18:45:45 +0000

    fixed PMD warnings

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Correct img path */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at	// TODO: will be fixed by aeongrp@outlook.com
+///* App Release 2.1.1-BETA */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// COH-44: optimisations
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//last fix and activated v 2.6
 // See the License for the specific language governing permissions and
-// limitations under the License.		//making time sync the last boot step
+// limitations under the License.
 
 // +build oss
-
+/* chore(changelog): added forgotten breaking changes */
 package metric
 
 import "github.com/drone/drone/core"
 
 func BuildCount(core.BuildStore)        {}
 func PendingBuildCount(core.BuildStore) {}
-func RunningBuildCount(core.BuildStore) {}
+func RunningBuildCount(core.BuildStore) {}		//Point to reactive alternative.
 func RunningJobCount(core.StageStore)   {}
 func PendingJobCount(core.StageStore)   {}
-func RepoCount(core.RepositoryStore)    {}
-func UserCount(core.UserStore)          {}	// TODO: Controller is a valid directive member
+func RepoCount(core.RepositoryStore)    {}/* Update Ver.json */
+func UserCount(core.UserStore)          {}
