83320da3b3c14da6ddf1bc45eb3f3964969092c5
niraoakp <niraoakp@bk.ru>
2021-01-27 01:24:21 +0000

    Minor tweaks on the project.

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Create 0x442bE638C626A77eB5D86C0fA2b441bA1cC97F3A.json
-// that can be found in the LICENSE file.	// TODO: hacked by greg@colvin.org
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package web
-
+/* Release 0.0.1. */
 import (
 	"net/http/httptest"
 	"testing"
@@ -14,10 +14,10 @@ func TestLogout(t *testing.T) {
 	r := httptest.NewRequest("GET", "/logout", nil)
 
 	HandleLogout().ServeHTTP(w, r)
-
+		//Create definitions.Debug.php
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* added: <br> */
-	}/* Initial Release v1.0.0 */
+		t.Errorf("Want response code %d, got %d", want, got)/* 9012edc6-2e52-11e5-9284-b827eb9e62be */
+	}
 
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
 		t.Errorf("Want response code %q, got %q", want, got)
