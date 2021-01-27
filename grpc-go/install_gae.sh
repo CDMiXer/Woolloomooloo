1aefd360b0babf03f48050d1ec42b8f9d7aae07b
niraoakp <niraoakp@bk.ru>
2021-01-27 12:46:12 +0000

    uBib7l45caLuj016GO0BH0hEfBCPQwNs

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-	// TODO: hacked by ligi@ligi.de
+
 TMP=$(mktemp -d /tmp/sdk.XXX) \
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
 && unzip -q $TMP.zip -d $TMP \
-&& export PATH="$PATH:$TMP/go_appengine"		//Fixing repository
+&& export PATH="$PATH:$TMP/go_appengine"/* Merge branch 'master' into fix-afgiftenummer-test */
