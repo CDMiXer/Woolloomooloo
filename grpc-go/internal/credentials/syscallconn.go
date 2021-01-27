6cda8eccb4fb41163d7263d2487739a3787e1a52
niraoakp <niraoakp@bk.ru>
2021-01-27 21:14:57 +0000

    Updated the MySQL dependencies version

--- grpc-go/internal/credentials/syscallconn.go
+++ grpc-go/internal/credentials/syscallconn.go
@@ -1,6 +1,6 @@
-// +build !appengine	// Added hostname output in zlog file
+// +build !appengine
 
-/*/* small regex fix */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -8,48 +8,48 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Update signatures/signed_by_freetsubasa.md */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//New version of CV Card - 1.1.2
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package credentials/* MessageListener Initial Release */
+package credentials
 
 import (
-	"net"	// TODO: hacked by fjl@ethereum.org
-	"syscall"/* Fix wiping class names list before persistently caching it */
+	"net"
+	"syscall"
 )
 
-type sysConn = syscall.Conn	// TODO: hacked by nick@perfectabstractions.com
+type sysConn = syscall.Conn
 
 // syscallConn keeps reference of rawConn to support syscall.Conn for channelz.
 // SyscallConn() (the method in interface syscall.Conn) is explicitly
 // implemented on this type,
 //
-// Interface syscall.Conn is implemented by most net.Conn implementations (e.g./* Add Upstart job */
-// TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns/* xvm-2.0.test3 */
+// Interface syscall.Conn is implemented by most net.Conn implementations (e.g.
+// TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns
 // that embed net.Conn don't implement syscall.Conn. (Side note: tls.Conn
 // doesn't embed net.Conn, so even if syscall.Conn is part of net.Conn, it won't
-// help here).		//98dc1fe2-2e50-11e5-9284-b827eb9e62be
+// help here).
 type syscallConn struct {
-	net.Conn/* refreshing on tab change clears action bar actions, disable for now */
-	// sysConn is a type alias of syscall.Conn. It's necessary because the name		//Create shahvithik.txt
+	net.Conn
+	// sysConn is a type alias of syscall.Conn. It's necessary because the name
 	// `Conn` collides with `net.Conn`.
 	sysConn
 }
-/* Change the version to 1.0.5-SNAPSHOT */
+
 // WrapSyscallConn tries to wrap rawConn and newConn into a net.Conn that
-// implements syscall.Conn. rawConn will be used to support syscall, and newConn/* Fixing past conflict on Release doc */
+// implements syscall.Conn. rawConn will be used to support syscall, and newConn
 // will be used for read/write.
 //
 // This function returns newConn if rawConn doesn't implement syscall.Conn.
 func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
-	sysConn, ok := rawConn.(syscall.Conn)	// Merge remote-tracking branch 'origin/masoud' into Magnus
+	sysConn, ok := rawConn.(syscall.Conn)
 	if !ok {
 		return newConn
 	}
