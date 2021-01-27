6920e433959f9b21788dc1e7ccd1fe9a86a420d6
niraoakp <niraoakp@bk.ru>
2021-01-27 22:47:55 +0000

    Include start CTR when comparing 2 data parent nodes

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,14 +1,14 @@
-using Pulumi;	// TODO: Time to try multiple JDK support for JRuby
+using Pulumi;
 using Random = Pulumi.Random;
 
 class MyStack : Stack
-{/* Short default name for State */
+{
     public MyStack()
-    {	// TODO: will be fixed by cory@protocol.ai
-        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs		//Merge "better beta support for nokia E75"
-        {	// TODO: 649dd108-2e5f-11e5-9284-b827eb9e62be
+    {
+        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
+        {
             Prefix = "doggo",
         });
-    }
+    }	// TODO: Handle the inclussive request
 
 }
