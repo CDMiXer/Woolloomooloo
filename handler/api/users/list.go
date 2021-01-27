a56d5dfa41810413dbd4f0664e071bd17eced7af
niraoakp <niraoakp@bk.ru>
2021-01-27 23:33:14 +0000

    Gitignore for unity

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -5,28 +5,28 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* minor form change */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Target 1.5 for backward compatibility */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package users
-		//added callback for devise mailer
+
 import (
 	"net/http"
-		//Replace appveyor's badge
-	"github.com/drone/drone/core"/* Fix theme install location */
+/* Cria 'obter-autorizacao-para-o-manejo-de-fauna-exotica-invasora' */
+	"github.com/drone/drone/core"/* Deleted msmeter2.0.1/Release/StdAfx.obj */
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"	// TODO: will be fixed by yuvalalaluf@gmail.com
 )
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded		//Add node directive at top of script.
-// list of all registered system users to the response body./* Merge branch 'develop' into greenkeeper/eslint-4.13.1 */
+// HandleList returns an http.HandlerFunc that writes a json-encoded
+// list of all registered system users to the response body.
 func HandleList(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		users, err := users.List(r.Context())	// Changing browserstack-runner to be the ashward repo (with ie6 fix)
+		users, err := users.List(r.Context())
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
@@ -34,5 +34,5 @@ func HandleList(users core.UserStore) http.HandlerFunc {
 		} else {
 			render.JSON(w, users, 200)
 		}
-	}	// TODO: c48db6f6-2e68-11e5-9284-b827eb9e62be
+	}
 }
