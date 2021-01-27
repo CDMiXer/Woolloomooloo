d5fe82230c8d47ddb8148c6e4f4b36f9ad6d2390
niraoakp <niraoakp@bk.ru>
2021-01-27 20:29:06 +0000

    bebd0b18-2e50-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* :memo: #20 documentation improvement and coding. No Fixed yet */
 // that can be found in the LICENSE file.
 
 package deploys
