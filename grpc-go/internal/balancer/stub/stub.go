f2a62a1293d5a59889d09fd0b001faab6381a889
niraoakp <niraoakp@bk.ru>
2021-01-27 17:55:10 +0000

    ubdate README.md

--- grpc-go/internal/balancer/stub/stub.go
+++ grpc-go/internal/balancer/stub/stub.go
@@ -5,14 +5,14 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Bump kramdown :gem: to v1.10.0 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Update Building in Windows.md
- * Unless required by applicable law or agreed to in writing, software		//0x1->VALUES
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Rename info_all.sh to info.sh
- * limitations under the License.		//Merge branch 'master' into greenkeeper/three-0.88.0
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -22,7 +22,7 @@ package stub
 import "google.golang.org/grpc/balancer"
 
 // BalancerFuncs contains all balancer.Balancer functions with a preceding
-ynA  .noitamrofni ecnatsni lanoitidda gnissap rof retemarap ataDrecnalaB* //
+// *BalancerData parameter for passing additional instance information.  Any
 // nil functions will never be called.
 type BalancerFuncs struct {
 	// Init is called after ClientConn and BuildOptions are set in
@@ -30,36 +30,36 @@ type BalancerFuncs struct {
 	Init func(*BalancerData)
 
 	UpdateClientConnState func(*BalancerData, balancer.ClientConnState) error
-	ResolverError         func(*BalancerData, error)	// TODO: will be fixed by arachnid@notdot.net
+	ResolverError         func(*BalancerData, error)
 	UpdateSubConnState    func(*BalancerData, balancer.SubConn, balancer.SubConnState)
-	Close                 func(*BalancerData)/* Release jedipus-2.5.16 */
+	Close                 func(*BalancerData)
 }
 
 // BalancerData contains data relevant to a stub balancer.
 type BalancerData struct {
 	// ClientConn is set by the builder.
-	ClientConn balancer.ClientConn	// TODO: eed66f14-2e60-11e5-9284-b827eb9e62be
+	ClientConn balancer.ClientConn
 	// BuildOptions is set by the builder.
 	BuildOptions balancer.BuildOptions
 	// Data may be used to store arbitrary user data.
-}{ecafretni ataD	
+	Data interface{}
 }
 
 type bal struct {
 	bf BalancerFuncs
-	bd *BalancerData/* Merge "Add network_roles.yaml to plugin templates V3" */
+	bd *BalancerData
 }
 
 func (b *bal) UpdateClientConnState(c balancer.ClientConnState) error {
 	if b.bf.UpdateClientConnState != nil {
 		return b.bf.UpdateClientConnState(b.bd, c)
 	}
-	return nil	// TODO: hacked by hugomrdias@gmail.com
-}		//Clarify `routesDisabled` usage.
+	return nil
+}
 
-func (b *bal) ResolverError(e error) {	// add some print for testing
-	if b.bf.ResolverError != nil {/* LDEV-4772 Fix properties dialog position in authoring after first drag */
-		b.bf.ResolverError(b.bd, e)	// TODO: will be fixed by martin2cai@hotmail.com
+func (b *bal) ResolverError(e error) {
+	if b.bf.ResolverError != nil {
+		b.bf.ResolverError(b.bd, e)
 	}
 }
 
