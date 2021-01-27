8618db93c1f20ba41afcfa334445909800954abb
niraoakp <niraoakp@bk.ru>
2021-01-27 01:40:59 +0000

    Filtragem pela jComboBox Categorias - closes #3

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,43 +1,43 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: 1ed56014-2e50-11e5-9284-b827eb9e62be
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software		//Disabling Railcraft's ore mines and other ore gen
+// distributed under the License is distributed on an "AS IS" BASIS,/* generic argument rather than specific */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: october 22 
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package livelog
-
-import (
+package livelog/* Release 2.64 */
+/* e6601b62-2e6c-11e5-9284-b827eb9e62be */
+import (		//added virtualhost templating to apache_sites
 	"sync"
 
 	"github.com/drone/drone/core"
 )
 
 type subscriber struct {
-	sync.Mutex
+	sync.Mutex/* 39db1b98-4b19-11e5-9adf-6c40088e03e4 */
 
-	handler chan *core.Line
+	handler chan *core.Line/* Merge "Add support for initial state to the register_or_update workflow" */
 	closec  chan struct{}
 	closed  bool
 }
 
 func (s *subscriber) publish(line *core.Line) {
 	select {
-	case <-s.closec:
+	case <-s.closec:/* Add MiniRelease1 schematics */
 	case s.handler <- line:
-	default:
+	default:		//1c74dcbc-2e43-11e5-9284-b827eb9e62be
 		// lines are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
 		// are ignored.
-	}
+	}/* Rename 32_yinzcam.md to 35_yinzcam.md */
 }
 
 func (s *subscriber) close() {
@@ -46,5 +46,5 @@ func (s *subscriber) close() {
 		close(s.closec)
 		s.closed = true
 	}
-	s.Unlock()
+	s.Unlock()	// 83ebe6a4-4b19-11e5-b4f3-6c40088e03e4
 }
