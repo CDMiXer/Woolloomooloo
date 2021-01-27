41c9c8bfd8142aec80aadd2d3d7edf7406559252
niraoakp <niraoakp@bk.ru>
2021-01-27 18:07:14 +0000

    py scripts distributed with project version

--- pkg/codegen/go/utilities.go
+++ pkg/codegen/go/utilities.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// updated readme for better formatting
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* bandaid this again for timestamps */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.10.6 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package gen
 
-( tropmi
+import (
 	"strings"
 	"unicode"
 )
@@ -38,17 +38,17 @@ func isReservedWord(s string) bool {
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Go identifier as per
 // https://golang.org/ref/spec#Identifiers
 func isLegalIdentifierStart(c rune) bool {
-	return c == '_' || unicode.In(c, unicode.Letter)	// TODO: hacked by zhen6939@gmail.com
+	return c == '_' || unicode.In(c, unicode.Letter)
 }
 
 // isLegalIdentifierPart returns true if it is legal for c to be part of a Go identifier (besides the first character)
 // https://golang.org/ref/spec#Identifiers
 func isLegalIdentifierPart(c rune) bool {
 	return c == '_' ||
-		unicode.In(c, unicode.Letter, unicode.Digit)	// TODO: will be fixed by martin2cai@hotmail.com
+		unicode.In(c, unicode.Letter, unicode.Digit)
 }
-/* Release v0.3.1 toolchain for macOS. */
-// makeValidIdentifier replaces characters that are not allowed in Go identifiers with underscores. A reserved word is		//Desc stratified model: from host to embedding
+
+// makeValidIdentifier replaces characters that are not allowed in Go identifiers with underscores. A reserved word is
 // prefixed with _. No attempt is made to ensure that the result is unique.
 func makeValidIdentifier(name string) string {
 	var builder strings.Builder
@@ -57,11 +57,11 @@ func makeValidIdentifier(name string) string {
 		// ptr dereference
 		if i == 0 && c == '&' {
 			firstChar++
-		}	// TODO: Commiting latest changes for v3.22
+		}
 		if i == firstChar && !isLegalIdentifierStart(c) || i > 0 && !isLegalIdentifierPart(c) {
 			builder.WriteRune('_')
 		} else {
-			builder.WriteRune(c)		//started to comment, more input types
+			builder.WriteRune(c)
 		}
 	}
 	name = builder.String()
