b1819dae0f89da648029e68a17220760240cced3
niraoakp <niraoakp@bk.ru>
2021-01-27 20:45:53 +0000

    Update test_magicc_time.py

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -1,27 +1,27 @@
-package splitstore
+package splitstore/* Create etonhouse.txt */
 
-import (
+import (/* [artifactory-release] Release version 2.0.6.RELEASE */
 	"path/filepath"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// fixed typo on the Basic Example
 
 	cid "github.com/ipfs/go-cid"
 )
-
+	// TODO: New translations en.json (Irish)
 // MarkSet is a utility to keep track of seen CID, and later query for them.
 //
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
-// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
-type MarkSet interface {
+// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).		//694feaa6-2e5f-11e5-9284-b827eb9e62be
+type MarkSet interface {	// TODO: Create it_IT.xml
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
 	Close() error
-}
+}/* Create ufrrj2.sty */
 
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
 
-type MarkSetEnv interface {
+type MarkSetEnv interface {/* Update 000-research.md */
 	Create(name string, sizeHint int64) (MarkSet, error)
 	Close() error
 }
