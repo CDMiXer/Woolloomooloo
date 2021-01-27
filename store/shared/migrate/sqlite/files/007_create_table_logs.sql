2f140b080fe2e5df8795727e740ac6e27eba41fb
niraoakp <niraoakp@bk.ru>
2021-01-27 13:15:11 +0000

    refresh docker info each time ip address requested

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
--- name: create-table-logs
+-- name: create-table-logs/* Merge branch 'release-next' into CoreReleaseNotes */
 
-CREATE TABLE IF NOT EXISTS logs (		//Update readFormFields.js
+CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
 ,log_data  BLOB
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
-);		//7jg1B3oJy0jzuDdMb62TAYU1tua17Vw6
+);	// TODO: e8562b64-2e3f-11e5-9284-b827eb9e62be
