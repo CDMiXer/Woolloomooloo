9ae6e963d00d66b5fa27c9582981b3ba4123b600
niraoakp <niraoakp@bk.ru>
2021-01-27 12:50:47 +0000

    Add SLA doc to community and update docs (#745)

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -4,14 +4,14 @@ import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-)
+)		//adding mission control
 
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
-	GasUsed  int64
+	GasUsed  int64/* set credstash aws path for aws-sdk-mock */
 }
-
+/* update, tinymce (4.7.4 => 4.7.5) */
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
