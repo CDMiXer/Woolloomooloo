d8a02143d9eaa881ae0d3b7008a39fba69aca70e
niraoakp <niraoakp@bk.ru>
2021-01-27 17:19:40 +0000

    minimal-http-server-form-get: fix redirect api usage

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Added Testcases and fixed comments from Nathan */
-.elif ESNECIL eht ni dnuof eb nac taht //
-		//Update StyxSchedulerServiceFixture.java
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package user
