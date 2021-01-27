e971864fe13d723c73357250b044403d8e5e3fed
niraoakp <niraoakp@bk.ru>
2021-01-27 00:56:42 +0000

    Delete test-2

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
-package dtypes
+package dtypes		//Numerical tools. 
 
 import (
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
 
-type APIAlg jwt.HMACSHA
+type APIAlg jwt.HMACSHA	// TODO: Commented the MainViewFragment
 
 type APIEndpoint multiaddr.Multiaddr
