b12e2bc041e7ab75de14b3116314fcd06ee1ac42
niraoakp <niraoakp@bk.ru>
2021-01-27 02:55:02 +0000

    Updating build-info/dotnet/core-setup/master for preview5-27619-04

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
 package nullreader
 
-type Reader struct{}/* Release of eeacms/forests-frontend:2.0-beta.43 */
+type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {
+func (Reader) Read(out []byte) (int, error) {/* Create vmlist.csv */
 	for i := range out {
 		out[i] = 0
 	}
 	return len(out), nil
-}/* use go 1.14.4 when building logjam container */
+}	// [Spider]ForkJoinPool Supported
