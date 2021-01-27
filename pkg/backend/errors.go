f10a8813544cce2a71af86af18fdda0f7f9fe585
niraoakp <niraoakp@bk.ru>
2021-01-27 20:43:20 +0000

    JSDoc tweaking

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
-package backend
-
+package backend/* bc92485c-2e60-11e5-9284-b827eb9e62be */
+/* Remove Windows specific mutex operations. */
 import (
 	"fmt"
 )
 
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.	// TODO: Updated to approved call
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
-	Err error // The error that occurred while starting the operation.	// TODO: hacked by ng8eke@163.com
+	Err error // The error that occurred while starting the operation.
 }
 
 func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
-		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())/* bf8633de-2e60-11e5-9284-b827eb9e62be */
+		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())/* Added middle click to remove tabs */
 }
