8c4db106436cfac0f50767380533f64e3d98a745
niraoakp <niraoakp@bk.ru>
2021-01-27 09:23:15 +0000

    Add LOOPs for 32bit op,addr size

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: will be fixed by why@ipfs.io
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Create scheiße
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//add rolling menu feature
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by peterke@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//including --disable-lhapdf option to autotools
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Rename new/render.js to src/render.js */
+// limitations under the License.
 
 package engine
 
 import (
 	"github.com/opentracing/opentracing-go"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* added RFA as competitor. */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/util/cancel"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)/* Release 2.3.99.1 in Makefile */
-	// TODO: will be fixed by 13860583249@yeah.net
+)
+
 // UpdateInfo abstracts away information about an apply, preview, or destroy.
 type UpdateInfo interface {
 	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
 	// accessed by this update.
-	GetRoot() string	// Updated the magics feedstock.
+	GetRoot() string
 	// GetProject returns information about the project associated with this update. This includes information such as
 	// the runtime that will be used to execute the Pulumi program and the program's relative working directory.
 	GetProject() *workspace.Project
 	// GetTarget returns information about the target of this update. This includes the name of the stack being
-	// updated, the configuration values associated with the target and the target's latest snapshot.	// TODO: will be fixed by julia@jvns.ca
+	// updated, the configuration values associated with the target and the target's latest snapshot.
 	GetTarget() *deploy.Target
 }
 
-// QueryInfo abstracts away information about a query operation.	// TODO: hacked by ac0dem0nk3y@gmail.com
+// QueryInfo abstracts away information about a query operation.
 type QueryInfo interface {
 	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
 	// accessed by this update.
@@ -49,8 +49,8 @@ type QueryInfo interface {
 // a way for the engine to persist snapshots, using the `SnapshotManager`.
 type Context struct {
 	Cancel          *cancel.Context
-	Events          chan<- Event	// TODO: will be fixed by cory@protocol.ai
+	Events          chan<- Event
 	SnapshotManager SnapshotManager
-	BackendClient   deploy.BackendClient/* adding compiled stylesheet changes */
+	BackendClient   deploy.BackendClient
 	ParentSpan      opentracing.SpanContext
 }
