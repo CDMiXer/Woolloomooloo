abe0146721a6dd15f14b005116353ca8b82dd825
niraoakp <niraoakp@bk.ru>
2021-01-27 21:24:43 +0000

    Edited name of the project

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -1,24 +1,24 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by vyzo@hackzen.org
-using System.Threading.Tasks;/* Default20: Use better CSS rule #kunena.layout */
+
+using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {/* Release v2.1.1 */
+    {
     }
 }
 
 class Program
 {
-    static Task<int> Main(string[] args)/* Create 17.-Karakter-Dizilerinin-Metotları */
-    {/* add basic arcade driving. */
+    static Task<int> Main(string[] args)/* Release of eeacms/www:19.11.30 */
+    {	// TODO: hacked by boringland@protonmail.ch
         return Deployment.RunAsync(() => 
         {
             // Scenario #1 - rename a resource
             var res1 = new Resource("res1");
         });
     }
-}	// TODO: Translate transform.md via GitLocalize
+}
