06e05f05df4a37d4e9bf6fbef40faa2680ba4b5d
niraoakp <niraoakp@bk.ru>
2021-01-27 01:15:13 +0000

    Create primos_rango.psc

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release is done, so linked it into readme.md */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Update CopyReleaseAction.java */
+// +build !oss	// fix(deps): update dependency ember-cli-babel to v7.4.2
 
 package queue
