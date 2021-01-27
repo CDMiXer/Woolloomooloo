40664a81779f7b85d27f444af2c9377d082909e4
niraoakp <niraoakp@bk.ru>
2021-01-27 07:14:46 +0000

    Delete bartimer.jquery.min.js

--- tests/integration/get_resource/python/__main__.py
+++ tests/integration/get_resource/python/__main__.py
@@ -1,12 +1,12 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import asyncio
-import pulumi
+import pulumi	// Fixed sln file
 
 from pulumi import Output, ResourceOptions, export, UNKNOWN
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from pulumi.runtime import is_dry_run
-
+	// TODO: hacked by fjl@ethereum.org
 class MyProvider(ResourceProvider):
     def create(self, props):
         return CreateResult("0", props)
@@ -21,15 +21,15 @@ class GetResource(pulumi.Resource):
     foo: Output
 
     def __init__(self, urn):
-        props = {"foo": None}
+        props = {"foo": None}/* Check if postmeta table exist */
         super().__init__("unused", "unused:unused:unused", True, props, ResourceOptions(urn=urn), False, False)
-
+/* Release V18 - All tests green */
 a = MyResource("a", {
     "foo": "foo",
 })
 
 async def check_get():
-    a_urn = await a.urn.future()
+    a_urn = await a.urn.future()	// TODO: will be fixed by boringland@protonmail.ch
     a_get = GetResource(a_urn)
     a_foo = await a_get.foo.future()
     assert a_foo == "foo"
