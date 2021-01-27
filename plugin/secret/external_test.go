9670cea0b1aba61124ab62ad25b8bfe85a2d884c
niraoakp <niraoakp@bk.ru>
2021-01-27 03:02:14 +0000

    Suggested headers are returned back

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package secret	// TODO: Updating Data-Structures in Events and Notifications
+package secret
