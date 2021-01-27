e48e9c7365fc94130bdf48081868c4f931facfae
niraoakp <niraoakp@bk.ru>
2021-01-27 23:49:31 +0000

    2fcfc552-2e65-11e5-9284-b827eb9e62be

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -8,11 +8,11 @@ package config
 
 import (
 	"errors"
-	"testing"/* Minor fixes - maintain 1.98 Release number */
-/* Release of eeacms/www:20.2.12 */
+	"testing"
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* ajoute méthode getLat et getLon */
-		//arachnid.cpp: Add notes about dipswitches (nw)
+	"github.com/drone/drone/mock"
+
 	"github.com/golang/mock/gomock"
 )
 
@@ -20,39 +20,39 @@ func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}	// TODO: will be fixed by mowrain@yandex.com
+	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
 	args := &core.ConfigArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: conf,
 	}
-	// TODO: will be fixed by xiemengjun@gmail.com
+
 	base := mock.NewMockConfigService(controller)
-	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)/* Update install_antivirus.sh */
-	// TODO: Oops, typo.
+	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
+
 	service := Memoize(base).(*memoize)
 	_, err := service.Find(noContext, args)
-	if err != nil {	// TODO: Update pytest from 4.1.1 to 4.2.1
-		t.Error(err)/* Release: Release: Making ready to release 6.2.0 */
-		return		//Fix for skills shortcuts (thanks Bahatur)
+	if err != nil {
+		t.Error(err)
+		return
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 
-	args.Config = nil // set to nil to prove we get the cached value	// TODO: Refactor lightweight tags to remove duplication 🐞
-	res, err := service.Find(noContext, args)/* Release of eeacms/eprtr-frontend:0.4-beta.19 */
+	args.Config = nil // set to nil to prove we get the cached value
+	res, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 	if res != conf {
 		t.Errorf("Expect result from cache")
-	}/* Added transition to icon hover */
-	// TODO: hacked by 13860583249@yeah.net
+	}
+
 	if got, want := service.cache.Len(), 1; got != want {
-)tog ,tnaw ,"d% tog ,ehcac ni smeti d% tcepxE"(frorrE.t		
+		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 }
 
