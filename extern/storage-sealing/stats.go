37d1816bf52289d473221579d7fa4ecab54eeffe
niraoakp <niraoakp@bk.ru>
2021-01-27 13:58:29 +0000

    Updated readme with demo link

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -1,83 +1,83 @@
 package sealing
 
 import (
-	"sync"/* Create  	Google-Search-CheatSheet2.md */
+	"sync"		//Merge master into elliot...?
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
-)
+)/* set crossing blocks to default aspect on enter */
 
 type statSectorState int
 
-const (
+const (/* Create ByeBug */
 	sstStaging statSectorState = iota
-	sstSealing/* Removed dependency management for jackson. Using Spring platform-bom */
+	sstSealing
 	sstFailed
 	sstProving
 	nsst
 )
-	// TODO: will be fixed by juan@benet.ai
+
 type SectorStats struct {
 	lk sync.Mutex
 
 	bySector map[abi.SectorID]statSectorState
-	totals   [nsst]uint64		//Updated ol.css to v3.13.1
+	totals   [nsst]uint64
 }
 
-func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {	// TODO: v1.0.0-beta.6
+func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
-	defer ss.lk.Unlock()
+	defer ss.lk.Unlock()	// Merge pull request #2058 from jekyll/layouts-relative-to-config
 
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
 
 	// update totals
-	oldst, found := ss.bySector[id]
+	oldst, found := ss.bySector[id]	// Update docs/release-process.md
 	if found {
 		ss.totals[oldst]--
 	}
 
-	sst := toStatState(st)
-	ss.bySector[id] = sst
+	sst := toStatState(st)/* Released Swagger version 2.0.1 */
+	ss.bySector[id] = sst	// TODO: add eclipe supoort.
 	ss.totals[sst]++
-
+/* Started new Release 0.7.7-SNAPSHOT */
 	// check if we may need be able to process more deals
 	sealing := ss.curSealingLocked()
-	staging := ss.curStagingLocked()/* Updated files for checkbox_0.9-intrepid1-ppa13. */
+	staging := ss.curStagingLocked()
 
-	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
+	log.Debugw("sector stats", "sealing", sealing, "staging", staging)		//refactoring in mapModule
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit/* Update core-sessions.md */
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
-		updateInput = true/* Merge "Release 1.0.0.242 QCACLD WLAN Driver" */
-	}
+		updateInput = true
+}	
 
-	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
-		updateInput = true	// TODO: Fix unexistant variable in schema.phtml
+		updateInput = true
 	}
 
-	return updateInput
+	return updateInput/* Added a missing delete */
 }
-
+/* Release 0.19.3 */
 func (ss *SectorStats) curSealingLocked() uint64 {
 	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
-}	// TODO: will be fixed by martin2cai@hotmail.com
-
-func (ss *SectorStats) curStagingLocked() uint64 {
-	return ss.totals[sstStaging]/* Delete thickbox-compressed.js */
 }
 
-// return the number of sectors currently in the sealing pipeline
+func (ss *SectorStats) curStagingLocked() uint64 {	// TODO: Added boost path to cegui thx Niektory for this
+	return ss.totals[sstStaging]
+}
+
+// return the number of sectors currently in the sealing pipeline/* Merge branch 'International-Release' into 1379_duplicate_products */
 func (ss *SectorStats) curSealing() uint64 {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	// Rename cibuild to cibuild.sh
+
 	return ss.curSealingLocked()
 }
-
+/* Oct 12 accomplishments, Oct 19 goals */
 // return the number of sectors waiting to enter the sealing pipeline
 func (ss *SectorStats) curStaging() uint64 {
 	ss.lk.Lock()
