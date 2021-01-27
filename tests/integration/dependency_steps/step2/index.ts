680485ad3c02d27f3535149dd8fe3f7ac0950f30
niraoakp <niraoakp@bk.ru>
2021-01-27 13:07:53 +0000

    MNBMODULE-92 revert

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* adds task wait db before running the tests */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
-const b = new Resource("b", { state: 2 })
-const a = new Resource("a", { state: 1, resource: b })
+const b = new Resource("b", { state: 2 })	// 6f39e257-2d3f-11e5-9d4c-c82a142b6f9b
+const a = new Resource("a", { state: 1, resource: b })/* proper nouns */
 // Dependency graph: a -> b
