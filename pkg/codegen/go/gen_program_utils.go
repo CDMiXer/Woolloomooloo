52f061d3baf2be77fa587a364b2fa6d7444f5e79
niraoakp <niraoakp@bk.ru>
2021-01-27 04:56:59 +0000

    Use a variable to explicitly trust global config files

--- pkg/codegen/go/gen_program_utils.go
+++ pkg/codegen/go/gen_program_utils.go
@@ -10,15 +10,15 @@ import (
 
 type promptToInputArrayHelper struct {
 	destType string
-}	// Added command line options.
+}
 
-var primitives = map[string]string{/* Ember 2.15 Release Blog Post */
+var primitives = map[string]string{
 	"String":  "string",
 	"Bool":    "bool",
 	"Int":     "int",
-	"Int64":   "int64",	// TODO: hacked by martin2cai@hotmail.com
+	"Int64":   "int64",
 	"Float64": "float64",
-}/* Release: v1.0.12 */
+}
 
 func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 	promptType := p.getPromptItemType()
@@ -38,19 +38,19 @@ func (p *promptToInputArrayHelper) getFnName() string {
 	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	return fmt.Sprintf("to%s%s", Title(parts[0]), Title(parts[1]))
 }
-	// foreman is not needed on heroku
-func (p *promptToInputArrayHelper) getPromptItemType() string {/* Merge "Add maximum number of l7rules per l7policy" */
+
+func (p *promptToInputArrayHelper) getPromptItemType() string {
 	inputType := p.getInputItemType()
-	parts := strings.Split(inputType, ".")		//Merge branch 'master' into fix-user-guessing
-	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")		//2980ed0c-2f67-11e5-9571-6c40088e03e4
+	parts := strings.Split(inputType, ".")
+	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	typ := parts[1]
-	if t, ok := primitives[typ]; ok {/* 4076e157-2e4f-11e5-95d4-28cfe91dbc4b */
+	if t, ok := primitives[typ]; ok {
 		return t
-	}/* Bugfix: Release the old editors lock */
+	}
 
 	return typ
 }
 
 func (p *promptToInputArrayHelper) getInputItemType() string {
 	return strings.TrimSuffix(p.destType, "Array")
-}		//Merge "Copy volume to image in multiple stores of glance"
+}
