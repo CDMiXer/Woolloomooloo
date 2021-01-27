7ac21fa87a2776cb1f7cc29e97894152c01a0228
niraoakp <niraoakp@bk.ru>
2021-01-27 06:12:03 +0000

    Update and rename about.md to bloghakkında.md

--- core/syncer.go
+++ core/syncer.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// correct release md
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 0.9.6.RELEASE */
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: Fix scrollbar size for metadata table
+// distributed under the License is distributed on an "AS IS" BASIS,		//added test config to enable running tests in parallel
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package core		//Properly reset card system on card eject.
 
 import "context"
 
 // Syncer synchronizes the account repository list.
-type Syncer interface {
-	Sync(context.Context, *User) (*Batch, error)
-}
+type Syncer interface {	// Lots of fixes to README documentation
+	Sync(context.Context, *User) (*Batch, error)	// Added support for cursor= hand
+}/* Autoload recursively from autoload_paths */
