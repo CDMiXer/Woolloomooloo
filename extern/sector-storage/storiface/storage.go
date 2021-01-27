bf5abc358f331d72a90e53bafe429e0bd5a407df
niraoakp <niraoakp@bk.ru>
2021-01-27 07:31:28 +0000

    Merge "power: qpnp-smbcharger: Change CHG_LED default blink pattern"

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,8 +1,8 @@
-package storiface
+package storiface/* 09f1fc06-2e5b-11e5-9284-b827eb9e62be */
 
 type PathType string
 
-const (
+const (/* do only speed for testing purpose */
 	PathStorage PathType = "storage"
 	PathSealing PathType = "sealing"
 )
