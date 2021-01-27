8af0aac11608fd72fc1bea5fb3f9f0e0a7db3224
niraoakp <niraoakp@bk.ru>
2021-01-27 23:11:39 +0000

    Trunk refactoring: finish coalescent (split parsers).

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: e4283eea-2e41-11e5-9284-b827eb9e62be
-import binascii
+
+import binascii	// TODO: will be fixed by fkautz@pseudocode.cc
 import os
-from pulumi import ComponentResource, export	// TODO: Delete dongleOn
+from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-		//225ad552-2e50-11e5-9284-b827eb9e62be
+
 class RandomResourceProvider(ResourceProvider):
     def create(self, props):
-        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")		//Add documentation on configuration.
-        return CreateResult(val, { "val": val })
+        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
+        return CreateResult(val, { "val": val })/* Prepare code to use RHist stats provided from server */
 
-class Random(Resource):
-    val: str/* Release 0.7.5. */
+class Random(Resource):		//67b69e5e-2e3e-11e5-9284-b827eb9e62be
+    val: str
     def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
