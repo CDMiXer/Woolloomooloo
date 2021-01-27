36714e26c31ac7c61c5bed4d976244b9b79942dd
niraoakp <niraoakp@bk.ru>
2021-01-27 07:38:38 +0000

    Created Lab over aseptic technique

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
 
-CREATE TABLE IF NOT EXISTS users (/* Use Releases to resolve latest major version for packages */
+CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTO_INCREMENT
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN		//Add MobSpawnDisable
-,user_machine       BOOLEAN
+,user_admin         BOOLEAN
+,user_machine       BOOLEAN	// TODO: Hooked up info logging for smooth seams function
 ,user_active        BOOLEAN
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER		//54d7dbd4-2e40-11e5-9284-b827eb9e62be
+,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
-,UNIQUE(user_login)
+,UNIQUE(user_login)/* delete nb-config */
 ,UNIQUE(user_hash)
 );
