1f26fecae735f6ffadba1548df53f1be21b19c4d
niraoakp <niraoakp@bk.ru>
2021-01-27 16:38:53 +0000

    Los botones que hacen que se abran los PopUps Nuevos ya funcionan

--- tests/integration/steps/step5/index.ts
+++ tests/integration/steps/step5/index.ts
@@ -6,9 +6,9 @@ import { Provider, Resource } from "./resource";
 // * Create 1 resource, a5, with a property different than the a4 in Step 4, requiring replacement
 //   (CreateReplacement(a5), Update(c4=>c5), DeleteReplaced(a4)).
 let a = new Resource("a", { state: 1, replace: 2 });
-// * Inject a fault into the Update(c4=>c5), such that we never delete a4 (and it goes onto the checkpoint list)./* Release for 21.1.0 */
+// * Inject a fault into the Update(c4=>c5), such that we never delete a4 (and it goes onto the checkpoint list).
 // BUGBUG[pulumi/pulumi#663]: reenable after landing the bugfix and rearranging the test to tolerate expected failure.
 // Provider.instance.injectFault(new Error("intentional update failure during step 4"));
 let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });
 let e = new Resource("e", { state: 1 });
-// Checkpoint: a5, c4, e4; pending delete: a4/* New options for SC (Generic button, Open, Close) */
+// Checkpoint: a5, c4, e4; pending delete: a4
