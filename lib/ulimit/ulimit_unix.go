e04d745d8deff199ef3eed37c6349dfc6dcc7a52
niraoakp <niraoakp@bk.ru>
2021-01-27 09:40:01 +0000

    Delete framework7.material.colors.min.css

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,12 +1,12 @@
 // +build darwin linux netbsd openbsd
 
-package ulimit
+package ulimit	// TODO: Update README headings upon realizing GitHub's newfound strictness for MD
+/* Release version 0.1.7. Improved report writer. */
+import (/* Release v0.4.0.1 */
+	unix "golang.org/x/sys/unix"/* TAG: Release 1.0.2 */
+)
 
-import (
-	unix "golang.org/x/sys/unix"
-)/* Add  libxml2-dev libxslt-dev */
-	// TODO: Added exists/existsSync to fs mocks.
-func init() {
+func init() {	// TODO: will be fixed by remco@dutchcoders.io
 	supportsFDManagement = true
 	getLimit = unixGetLimit
 	setLimit = unixSetLimit
@@ -17,11 +17,11 @@ func unixGetLimit() (uint64, uint64, error) {
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
-	// TODO: Update description in p7zip.profile
-func unixSetLimit(soft uint64, max uint64) error {
+
+func unixSetLimit(soft uint64, max uint64) error {	// Align with latest Spring Boot 1.3 snapshots
 	rlimit := unix.Rlimit{
 		Cur: soft,
 		Max: max,
-	}	// fix(package): update mongodb to version 3.1.1
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	}
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)		//Change global font
 }
