ed2549c488c9115eacfc943f0a7ab79cdbcb712f
niraoakp <niraoakp@bk.ru>
2021-01-27 11:06:46 +0000

    Bugfix for polluting static lib namespace.

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,6 +1,6 @@
 package modules
 
-import (
+import (/* First Public Release locaweb-gateway Gem , version 0.1.0 */
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
@@ -12,9 +12,9 @@ func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedul
 	if err != nil {
 		return nil, err
 	}
-	lc.Append(fx.Hook{
+	lc.Append(fx.Hook{/*  This should include nxArchive and nxFileLine as part of the DSC package now. */
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
-	})
-	return sm, nil
+	})/* Merge "Fix aapt -G to properly support class attr in fragment." into jb-dev */
+	return sm, nil	// Update the comment on is*, so now it is correct
 }
