3f0cff32805e2c4621786f390d2187f418c35801
niraoakp <niraoakp@bk.ru>
2021-01-27 08:25:06 +0000

    Fix for concurrent modification issue.

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Updated field names. Added new script.  */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/jenkins-slave-dind:17.12-3.18.1 */
 
-package ints		//Remoção de tela antiga de cadastro de projetos
-
-import (		//fix NPE in SecurityFilter
+package ints
+	// TODO: hacked by jon@atack.com
+import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Simplified specs by version table */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Delete Produtos-HistoricoDeVendas01.png */
 	"github.com/stretchr/testify/assert"
 )
 
 var Dirs = []string{
-	"simple",/* Update README.md to add NiftyNet paper */
+	"simple",
 }
 
 func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
@@ -25,60 +25,60 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
-			if res.URN.Name() == "res1" {	// TODO: add link to show payment summary
+			if res.URN.Name() == "res1" {
 				foundRes1 = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))/* Merge "Release MediaPlayer if suspend() returns false." */
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child"/* Release 2.0.0: Upgrade to ECM 3.0 */
+			// "child"
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))	// disable sourcemaps in production
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))	// Unset wmgUseCirrusSearch and wmgSearchType for all wikis
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
 			// "res3" is impacted by a global stack transformation which sets
-			// optionalDefault to "stackDefault"/* Reflect actual code contributors */
+			// optionalDefault to "stackDefault"
 			if res.URN.Name() == "res3" {
-				foundRes3 = true
+				foundRes3 = true	// TODO: Some typo and grammar fixes
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)
+				assert.NotNil(t, optionalInput)		//more stuff and rules
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res4" is impacted by two component parent transformations which set
-			// optionalDefault to "default1" and then "default2" and also a global stack
-			// transformation which sets optionalDefault to "stackDefault".  The end
-			// result should be "stackDefault".
-			if res.URN.Name() == "res4-child" {		//Create RNASeq_lab_II-DESeq_analysis.md
-				foundRes4Child = true		//Delete Match.js
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))	// TODO: 19577b62-2e5b-11e5-9284-b827eb9e62be
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))/* Release version: 0.6.1 */
+			// optionalDefault to "default1" and then "default2" and also a global stack	// TODO: will be fixed by cory@protocol.ai
+			// transformation which sets optionalDefault to "stackDefault".  The end		//Merge "Fix aggregate_hosts.host migration for sqlite" into milestone-proposed
+			// result should be "stackDefault".	// TODO: Merge "Allow setting a tenant router's external IP"
+			if res.URN.Name() == "res4-child" {	// TODO: hacked by boringland@protonmail.ch
+				foundRes4Child = true
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)		//vcf format
-				assert.Equal(t, "stackDefault", optionalInput.(string))
+				assert.NotNil(t, optionalInput)/* Released springjdbcdao version 1.9.4 */
+				assert.Equal(t, "stackDefault", optionalInput.(string))/* Just send an album. */
 			}
-			// "res5" modifies one of its children to depend on another of its children.
+			// "res5" modifies one of its children to depend on another of its children./* Removed dead branch */
 			if res.URN.Name() == "res5-child1" {
-				foundRes5Child = true
+				foundRes5Child = true/* Release 0.10 */
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				// TODO[pulumi/pulumi#3282] Due to this bug, the dependency information
 				// will not be correctly recorded in the state file, and so cannot be
 				// verified here.
 				//
-				// assert.Len(t, res.PropertyDependencies, 1)/* Release maintenance v1.1.4 */
+				// assert.Len(t, res.PropertyDependencies, 1)
 				input := res.Inputs["input"]
-				assert.NotNil(t, input)
+				assert.NotNil(t, input)/* more use of 1L etc */
 				assert.Equal(t, "b", input.(string))
 			}
 		}
 		assert.True(t, foundRes1)
 		assert.True(t, foundRes2Child)
 		assert.True(t, foundRes3)
-		assert.True(t, foundRes4Child)	// Automatic changelog generation for PR #12812 [ci skip]
+		assert.True(t, foundRes4Child)
 		assert.True(t, foundRes5Child)
 	}
 }
