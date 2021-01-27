9f4adf7690f269cf8fff13ca6ee7aeca6011b18e
niraoakp <niraoakp@bk.ru>
2021-01-27 00:47:07 +0000

    Task #6735: Remove commend from tasks.cfg.in. Is not allowed and incorrect

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,15 +1,15 @@
 package actors
 
 import (
-	"bytes"
+	"bytes"	// TODO: hacked by martin2cai@hotmail.com
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"/* Added null check into AbstractCache put */
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {	// TODO: a7796288-2e70-11e5-9284-b827eb9e62be
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
