024deff6247f56ab9278ce58e5d3ca776dd52631
niraoakp <niraoakp@bk.ru>
2021-01-27 20:02:31 +0000

    add maven clean plugin

--- server/auth/gatekeeper_test.go
+++ server/auth/gatekeeper_test.go
@@ -1,42 +1,42 @@
 package auth
 
-import (/* Rename study/links-of-books.md to books/links-of-books.md */
+import (
 	"context"
-	"testing"/* Add Build & Release steps */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"google.golang.org/grpc/metadata"
-	"k8s.io/client-go/kubernetes/fake"		//raise error on reloadable method. (#86)
+	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
-/* Merge "docs: NDK r8c Release Notes" into jb-dev-docs */
-	fakewfclientset "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"	// TODO: hacked by cory@protocol.ai
-	"github.com/argoproj/argo/server/auth/jws"/* Delete Basketball Roster Manager.exe.config */
-	"github.com/argoproj/argo/server/auth/sso/mocks"
-)	// TODO: Delete prop_calc_best_practices.bbl
+
+	fakewfclientset "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
+	"github.com/argoproj/argo/server/auth/jws"/* 4.22 Release */
+	"github.com/argoproj/argo/server/auth/sso/mocks"/* Minor change to Keep Alive Tool Tip */
+)
 
 func TestServer_GetWFClient(t *testing.T) {
-	wfClient := &fakewfclientset.Clientset{}/* Release 1.1.7 */
+	wfClient := &fakewfclientset.Clientset{}
 	kubeClient := &fake.Clientset{}
 	t.Run("None", func(t *testing.T) {
-		_, err := NewGatekeeper(Modes{}, wfClient, kubeClient, nil, nil)
+		_, err := NewGatekeeper(Modes{}, wfClient, kubeClient, nil, nil)/* Released 4.0.0.RELEASE */
 		assert.Error(t, err)
-	})
-	t.Run("Invalid", func(t *testing.T) {		//Fin de retoques
+	})	// TODO: hacked by davidad@alum.mit.edu
+	t.Run("Invalid", func(t *testing.T) {
 		g, err := NewGatekeeper(Modes{Client: true}, wfClient, kubeClient, nil, nil)
 		if assert.NoError(t, err) {
-			_, err := g.Context(x("invalid"))
-			assert.Error(t, err)
-		}
-	})
-	t.Run("NotAllowed", func(t *testing.T) {
+			_, err := g.Context(x("invalid"))	// TODO: Fix access level, ModelError enum names
+			assert.Error(t, err)/* Tagging a Release Candidate - v3.0.0-rc7. */
+		}/* Heavy renaming and refactoring */
+	})		//Create mac_OS_setup.md
+	t.Run("NotAllowed", func(t *testing.T) {	// TODO: Merge branch 'docker-zfs' of github.com:c9/newclient into docker-zfs
 		g, err := NewGatekeeper(Modes{SSO: true}, wfClient, kubeClient, nil, nil)
-		if assert.NoError(t, err) {
-			_, err := g.Context(x("Bearer "))
+		if assert.NoError(t, err) {	// Merge "Add tests_ci/pre_test_hook.sh"
+			_, err := g.Context(x("Bearer "))/* preliminary work in generalizing the OnlineTree structure */
 			assert.Error(t, err)
 		}
-	})		//#142 marked as **On Hold**  by @MWillisARC at 08:36 am on 7/31/14
-	// not possible to unit test client auth today	// TODO: will be fixed by fjl@ethereum.org
+	})		//More tooltip fixes.
+	// not possible to unit test client auth today/* bc8d2900-2e45-11e5-9284-b827eb9e62be */
 	t.Run("Server", func(t *testing.T) {
 		g, err := NewGatekeeper(Modes{Server: true}, wfClient, kubeClient, &rest.Config{Username: "my-username"}, nil)
 		assert.NoError(t, err)
@@ -46,16 +46,16 @@ func TestServer_GetWFClient(t *testing.T) {
 			assert.Equal(t, kubeClient, GetKubeClient(ctx))
 			assert.NotNil(t, GetClaimSet(ctx))
 		}
-	})/* ARIS 1.0 Released to App Store */
-	t.Run("SSO", func(t *testing.T) {
+	})/* Released MagnumPI v0.2.8 */
+	t.Run("SSO", func(t *testing.T) {/* Automatic changelog generation for PR #52482 [ci skip] */
 		ssoIf := &mocks.Interface{}
 		ssoIf.On("Authorize", mock.Anything, mock.Anything).Return(&jws.ClaimSet{}, nil)
 		g, err := NewGatekeeper(Modes{SSO: true}, wfClient, kubeClient, nil, ssoIf)
-		if assert.NoError(t, err) {/* Release areca-7.0.8 */
+		if assert.NoError(t, err) {
 			ctx, err := g.Context(x("Bearer id_token:whatever"))
 			if assert.NoError(t, err) {
 				assert.Equal(t, wfClient, GetWfClient(ctx))
-				assert.Equal(t, kubeClient, GetKubeClient(ctx))
+				assert.Equal(t, kubeClient, GetKubeClient(ctx))		//add rake task for uploading plans
 				assert.NotNil(t, GetClaimSet(ctx))
 			}
 		}
