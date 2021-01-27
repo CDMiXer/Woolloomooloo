991da284ec9d279243824c4a48d79c640c212158
niraoakp <niraoakp@bk.ru>
2021-01-27 01:10:36 +0000

    [QUAD-187] corrected links on sites pages;

--- operator/runner/env_test.go
+++ operator/runner/env_test.go
@@ -7,9 +7,9 @@ package runner
 import (
 	"testing"
 
-	"github.com/drone/drone/core"/* Release 2.1.2. */
-	"github.com/google/go-cmp/cmp"/* Power of Three */
-)/* Merge "Release 3.2.3.479 Prima WLAN Driver" */
+	"github.com/drone/drone/core"
+	"github.com/google/go-cmp/cmp"
+)
 
 func Test_systemEnviron(t *testing.T) {
 	system := &core.System{
@@ -17,7 +17,7 @@ func Test_systemEnviron(t *testing.T) {
 		Host:    "meta.drone.io",
 		Link:    "https://meta.drone.io",
 		Version: "v1.0.0",
-	}	// TODO: will be fixed by juan@benet.ai
+	}
 	got := systemEnviron(system)
 	want := map[string]string{
 		"CI":                    "true",
@@ -28,23 +28,23 @@ func Test_systemEnviron(t *testing.T) {
 		"DRONE_SYSTEM_VERSION":  "v1.0.0",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)/* Release 0.1.1-dev. */
+		t.Errorf(diff)
 	}
-}		//Allow configuring the service name.
-/* Fix missing include in Hexagon code for Release+Asserts */
+}
+
 func Test_runnerEnviron(t *testing.T) {
 	runner := &Runner{
 		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",
 		Platform: "linux/amd64",
-	}	// Add adiferd as Indonesia proofreader
+	}
 	got := agentEnviron(runner)
 	want := map[string]string{
-		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",		//Delete LongestSequence.cs
-		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",/* Release notes for 1.0.61 */
+		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",
+		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_PLATFORM": "linux/amd64",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
-}/* Fixed use of byte[] values in internal service settings */
+}
