e547d934a64dac0e72ec7d6efb9fc79d607f1ac7
niraoakp <niraoakp@bk.ru>
2021-01-27 06:17:02 +0000

    Merge "[FAB-8394] Fixing expired certificates of msp_test"

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* Update ocl_cost.md */
 using System.Threading.Tasks;
-using Pulumi;
+using Pulumi;		// - fixed bugs in importing (Vedmak)
 
 class Resource : ComponentResource
-{
+{/* FiestaProxy now builds under Release and not just Debug. (Was a charset problem) */
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
-
+	// TODO: fix several issues of the most recent ~5 commits…
 class Program
 {
     static Task<int> Main(string[] args)
@@ -22,8 +22,8 @@ class Program
             var res1 = new Resource("newres1",
                 new ComponentResourceOptions
                 {
-                    Aliases = { new Alias { Name = "res1" } },
+                    Aliases = { new Alias { Name = "res1" } },		//First Version of Disaggregation Module
                 });
         });
     }
-}
+}/* Removed moveCamera call on mouseReleased. */
