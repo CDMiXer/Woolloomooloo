85d8582e16b77c92483211b85e57baa6fd8d5b46
niraoakp <niraoakp@bk.ru>
2021-01-27 06:14:24 +0000

    Add extended midi control support to configure.

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
-package cron/* Create build path for coveralls */
-/* Removed lancet as a recommended dependency */
-import (/* Release 1.08 all views are resized */
+package cron
+
+import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (		//Merge branch 'master' into scottmo/fixProcfile
+var (
 	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
 )
