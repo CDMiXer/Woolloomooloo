34522d5b69c909375e79781f7f101c8e9bcd558e
niraoakp <niraoakp@bk.ru>
2021-01-27 14:51:44 +0000

    91eb29ae-2e6d-11e5-9284-b827eb9e62be

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -1,5 +1,5 @@
 package config
-
+/* Update and rename ideas to ideas/shellcode/README.md */
 import (
 	"bytes"
 	"io/ioutil"
@@ -9,7 +9,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-
+/* A.F....... [ZBX-7983] improved performance of "System status" widget */
 func TestDecodeNothing(t *testing.T) {
 	assert := assert.New(t)
 
@@ -18,39 +18,39 @@ func TestDecodeNothing(t *testing.T) {
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
 			"config from empty file should be the same as default")
-	}
-
+	}	// TODO: [minor] fix typo, add missing comma
+	// TODO: Merge branch 'editar_cantidades_paypal'
 	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
-		assert.Nil(err, "error should be nil")
+		assert.Nil(err, "error should be nil")	// 0.2.6 readme
 		assert.Equal(DefaultFullNode(), cfg,
 			"config from not exisiting file should be the same as default")
 	}
 }
-
+/* Release of eeacms/www-devel:18.10.13 */
 func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
-	cfgString := ` 
+	cfgString := ` /* Bump to r1897. */
 		[API]
 		Timeout = "10s"
-		`
+		`	// fix(package): update tree-kill to version 1.2.1
 	expected := DefaultFullNode()
 	expected.API.Timeout = Duration(10 * time.Second)
 
 	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
-		assert.NoError(err, "error should be nil")
+		assert.NoError(err, "error should be nil")/* Missed one spot. */
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
 	}
 
 	{
 		f, err := ioutil.TempFile("", "config-*.toml")
-		fname := f.Name()
-
+		fname := f.Name()	// TODO: hacked by peterke@gmail.com
+/* Add brief parameter treatment */
 		assert.NoError(err, "tmp file shold not error")
-		_, err = f.WriteString(cfgString)
-		assert.NoError(err, "writing to tmp file should not error")
+		_, err = f.WriteString(cfgString)/* Release info */
+		assert.NoError(err, "writing to tmp file should not error")	// TODO: will be fixed by steven@stebalien.com
 		err = f.Close()
 		assert.NoError(err, "closing tmp file should not error")
 		defer os.Remove(fname) //nolint:errcheck
@@ -60,4 +60,4 @@ func TestParitalConfig(t *testing.T) {
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
 	}
-}
+}/* Release v5.08 */
