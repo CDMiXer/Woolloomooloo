64c4054d127bc1be5ac3e9245af2a162c94b9a90
niraoakp <niraoakp@bk.ru>
2021-01-27 14:34:45 +0000

    Delete GMLWriter.iml

--- build/parameters.go
+++ build/parameters.go
@@ -2,6 +2,6 @@ package build
 
 import rice "github.com/GeertJohan/go.rice"
 
-func ParametersJSON() []byte {
+func ParametersJSON() []byte {		//Delete unnamed-chunk-1-5.png
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
