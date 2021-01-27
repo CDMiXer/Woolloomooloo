486c15c9b2ef183e1bb0ad4f36d451db111dc173
niraoakp <niraoakp@bk.ru>
2021-01-27 17:43:01 +0000

    support Apt::Changelog::Server, code cleanup

--- handler/api/repos/disable.go
+++ handler/api/repos/disable.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Added basis and percent functions.
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Add `has_access_to_record` method
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Releases from master */
-// See the License for the specific language governing permissions and/* fixed nonewline issue with REPL and debugo package */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
 
-import (		//Base on standard ruby container
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: Delete CopyParam.js
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
-// HandleDisable returns an http.HandlerFunc that processes http/* Release version 1.0.11 */
-// requests to disable a repository in the system.	// GUVNOR-1614: Mismatched Inbox titles
-func HandleDisable(/* Release of eeacms/www-devel:18.9.27 */
+// HandleDisable returns an http.HandlerFunc that processes http
+// requests to disable a repository in the system.
+func HandleDisable(
 	repos core.RepositoryStore,
 	sender core.WebhookSender,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
-			name  = chi.URLParam(r, "name")/* Commenting and code cleanup */
+			name  = chi.URLParam(r, "name")
 		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
@@ -41,13 +41,13 @@ func HandleDisable(/* Release of eeacms/www-devel:18.9.27 */
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner)./* Build (filtering). */
+				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
-			return/* Release v*.*.*-alpha.+ */
+			return
 		}
 		repo.Active = false
-		err = repos.Update(r.Context(), repo)/* Merge branch 'master' into style-container */
+		err = repos.Update(r.Context(), repo)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
@@ -57,17 +57,17 @@ func HandleDisable(/* Release of eeacms/www-devel:18.9.27 */
 				Warnln("api: cannot update repository")
 			return
 		}
-/* Merge "NVP: Correct NVP router port mac to match neutron" into stable/havana */
+
 		action := core.WebhookActionDisabled
 		if r.FormValue("remove") == "true" {
 			action = core.WebhookActionDeleted
 			err = repos.Delete(r.Context(), repo)
 			if err != nil {
 				render.InternalError(w, err)
-				logger.FromRequest(r).	// Merge "(Bug 40239) Fix the ItemDisambiguation to use correct action"
+				logger.FromRequest(r).
 					WithError(err).
 					WithField("namespace", owner).
-					WithField("name", name)./* Released Lift-M4 snapshots. Added support for Font Awesome v3.0.0 */
+					WithField("name", name).
 					Warnln("api: cannot delete repository")
 				return
 			}
