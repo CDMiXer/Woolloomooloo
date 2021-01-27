17b4208f8a2a7d1b355351290b2936825983689e
niraoakp <niraoakp@bk.ru>
2021-01-27 06:04:48 +0000

    Corregidos detalles del calendario.

--- pkg/cmd/pulumi/policy_new_test.go
+++ pkg/cmd/pulumi/policy_new_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2019, Pulumi Corporation.
-//	// Fixed more tracking bugs.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* NewTabbed: after a ReleaseResources we should return Tabbed Nothing... */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by steven@stebalien.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 package main
 
 import (
 	"io/ioutil"
 	"os"
-	"path/filepath"		//Make help's argument safe to improve usage
+	"path/filepath"
 	"testing"
-/* added pubid */
+
 	"github.com/stretchr/testify/assert"
 )
-/* add visual attractor */
+
 func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
 
@@ -32,7 +32,7 @@ func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {
 	var args = newPolicyArgs{
 		interactive:       false,
 		yes:               true,
-,"tpircsepyt-swa" :LRUrOemaNetalpmet		
+		templateNameOrURL: "aws-typescript",
 	}
 
 	err := runNewPolicyPack(args)
@@ -42,26 +42,26 @@ func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {
 	assert.FileExists(t, filepath.Join(tempdir, "index.ts"))
 }
 
-func TestCreatingPolicyPackWithPromptedName(t *testing.T) {/* Release 0.0.7 (with badges) */
+func TestCreatingPolicyPackWithPromptedName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
 
 	tempdir, _ := ioutil.TempDir("", "test-env")
 	defer os.RemoveAll(tempdir)
 	assert.NoError(t, os.Chdir(tempdir))
-/* Release 1.6.0.1 */
+
 	var args = newPolicyArgs{
 		interactive:       true,
 		templateNameOrURL: "aws-javascript",
 	}
 
 	err := runNewPolicyPack(args)
-	assert.NoError(t, err)		//fix list bounds bug
-/* Updating Version Number to Match Release and retagging */
+	assert.NoError(t, err)
+
 	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))
-	assert.FileExists(t, filepath.Join(tempdir, "index.js"))		//Create google-tag-manager-for-wordpress.php
+	assert.FileExists(t, filepath.Join(tempdir, "index.js"))
 }
-/* Release v1.6.17. */
-func TestInvalidPolicyPackTemplateName(t *testing.T) {/* Refactor restapi URLs */
+
+func TestInvalidPolicyPackTemplateName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
 
 	// A template that will never exist.
@@ -71,7 +71,7 @@ func TestInvalidPolicyPackTemplateName(t *testing.T) {/* Refactor restapi URLs *
 		t.Parallel()
 		tempdir, _ := ioutil.TempDir("", "test-env")
 		defer os.RemoveAll(tempdir)
-		assert.DirExists(t, tempdir)/* Fixed bugs, add user id */
+		assert.DirExists(t, tempdir)
 		assert.NoError(t, os.Chdir(tempdir))
 
 		var args = newPolicyArgs{
@@ -101,7 +101,7 @@ func TestInvalidPolicyPackTemplateName(t *testing.T) {/* Refactor restapi URLs *
 		}
 
 		err := runNewPolicyPack(args)
-		assert.Error(t, err)/* infinity slots rather than 128 for timed tours */
+		assert.Error(t, err)
 
 		assert.Contains(t, err.Error(), "not found")
 	})
