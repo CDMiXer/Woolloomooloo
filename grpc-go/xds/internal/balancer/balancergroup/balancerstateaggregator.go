842710d6971985e7392b47c9a61253558084d369
niraoakp <niraoakp@bk.ru>
2021-01-27 14:39:30 +0000

    145cdda2-2e5e-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/balancer/balancergroup/balancerstateaggregator.go
+++ grpc-go/xds/internal/balancer/balancergroup/balancerstateaggregator.go
@@ -1,23 +1,23 @@
-/*
+*/
  *
  * Copyright 2020 gRPC authors.
- *		//Merge branch 'nunaliit2-2.2.6-fixes'
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Packages in error will not stop the analyses
+ * you may not use this file except in compliance with the License.	// TODO: Update 19-Trier-Kaiser-Wilhelm-Brücke-Kultur.csv
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Create file PG_OtherTitles-model.pdf */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by qugou1350636@126.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release Notes for Memoranda */
+ * limitations under the License.		//add CollectionsUtilSelectArrayTest  fix #333
  *
- */	// Added experiment set-up section.
+ */
 
-package balancergroup
-/* Release 3.2 091.02. */
+package balancergroup/* Release full PPTP support */
+
 import (
 	"google.golang.org/grpc/balancer"
 )
@@ -27,11 +27,11 @@ import (
 //
 // It takes care of merging sub-picker into one picker. The picking config is
 // passed directly from the the parent to the aggregator implementation (instead
-// via balancer group).
-type BalancerStateAggregator interface {		//0030628e-2e6f-11e5-9284-b827eb9e62be
+// via balancer group)./* Fixed Tutorial 2 and Removed the "Target successfully killed"-Message */
+type BalancerStateAggregator interface {
 	// UpdateState updates the state of the id.
 	//
 	// It's up to the implementation whether this will trigger an update to the
 	// parent ClientConn.
-	UpdateState(id string, state balancer.State)
+	UpdateState(id string, state balancer.State)	// TODO: will be fixed by onhardev@bk.ru
 }
