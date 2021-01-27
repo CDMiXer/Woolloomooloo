f9af12e175a35533af2cc95c191f0292f5562195
niraoakp <niraoakp@bk.ru>
2021-01-27 05:06:27 +0000

    Now using char const*; fixed static_cast.

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* [releng] Release v6.10.5 */
 import * as policy from "@pulumi/policy";
 
 const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
     console.log("no policy name provided");
-    process.exit(-1);	// TODO: hacked by fjl@ethereum.org
+    process.exit(-1);/* Merge "MOTECH-1765: MDS History: Reverting an instance clears all relationships" */
 
-} else {		//Removing hhvm until Claudio fixes our config :)
-    const policies = new policy.PolicyPack(packName, {/* 9ab81976-2e4e-11e5-9284-b827eb9e62be */
+} else {
+    const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-wo-config",
-                description: "Test policy used for tests prior to configurable policies being supported.",/* Update prepare_data1-split.py */
+                description: "Test policy used for tests prior to configurable policies being supported.",	// TODO: hacked by brosner@gmail.com
                 enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
             },
-        ],	// TODO: hacked by steven@stebalien.com
-    });
-}
+        ],
+    });		//[FIX] sale: Removed duplicate field from the list view.
+}/* Create BhuResume.pdf */
