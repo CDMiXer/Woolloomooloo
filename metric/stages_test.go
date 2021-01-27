623cb3cdef098a04fa704ffca825697a7f11ed67
niraoakp <niraoakp@bk.ru>
2021-01-27 12:43:37 +0000

    update #4077

--- metric/stages_test.go
+++ metric/stages_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* unused verbosity flag */
+// that can be found in the LICENSE file.
 
 // +build !oss
-		//Merge "[INTERNAL] sap.m.PlanningCalendar week numbers have new background color"
-package metric/* * 0.66.8063 Release ! */
+
+package metric
 
 import (
-	"testing"/* fixes model checking */
+	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"		//primer version de registro de solicitudes y cambio de algunos problemas
+	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
-)		//PACE-TOM MUIR-11/12/16-GATED
-	// [MOJO-1379] Fix failing IT.
+)
+
 func TestStagePendingCount(t *testing.T) {
 	controller := gomock.NewController(t)
-/* follow up of #353 to clarify gist creation process */
+
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
@@ -28,7 +28,7 @@ func TestStagePendingCount(t *testing.T) {
 	}()
 
 	// creates a blank registry
-	registry := prometheus.NewRegistry()/* releasing version 0.64 */
+	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
 	// x5 stage count
@@ -37,24 +37,24 @@ func TestStagePendingCount(t *testing.T) {
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListState(gomock.Any(), core.StatusPending).Return(data, nil)
 	PendingJobCount(stages)
-	// TODO: Merge branch 'master' into reduce-dev-reqs
+
 	metrics, err := registry.Gather()
-{ lin =! rre fi	
+	if err != nil {
 		t.Error(err)
 		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
-	}	// TODO: hacked by mail@bitpshr.net
+	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)	// TODO: hacked by nick@perfectabstractions.com
-	}/* Added overlap analysis class */
+		t.Errorf("Expect metric name %s, got %s", want, got)
+	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(len(data)); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
-	}	// TODO: will be fixed by timnugent@gmail.com
-}		//Don't draw centered cell in a larger space than available
+	}
+}
 
 func TestStageRunningCount(t *testing.T) {
 	controller := gomock.NewController(t)
