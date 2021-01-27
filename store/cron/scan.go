b263ce35ae8f1c8cc03c322250d42ddb80c7e9c1
niraoakp <niraoakp@bk.ru>
2021-01-27 18:00:00 +0000

    95c336a0-2e6a-11e5-9284-b827eb9e62be

--- store/cron/scan.go
+++ store/cron/scan.go
@@ -1,71 +1,71 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//cfff98c8-2e55-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License/* Set autoDropAfterRelease to true */
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release of eeacms/forests-frontend:1.7-beta.10 */
-package cron/* Initial Public Release V4.0 */
 
-import (/* Release Documentation */
+package cron
+
+import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"
-)		//9e003b4c-2e4c-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/store/shared/db"/* Release 0.3.15. */
+)
 
 // helper function converts the User structure to a set
 // of named query parameters.
 func toParams(cron *core.Cron) map[string]interface{} {
 	return map[string]interface{}{
-		"cron_id":       cron.ID,
+		"cron_id":       cron.ID,/* Release in mvn Central */
 		"cron_repo_id":  cron.RepoID,
-		"cron_name":     cron.Name,
+		"cron_name":     cron.Name,/* Port to logging instead of using print commands */
 		"cron_expr":     cron.Expr,
-		"cron_next":     cron.Next,
+		"cron_next":     cron.Next,	// TODO: Refactor XStrem typeconverter names
 		"cron_prev":     cron.Prev,
 		"cron_event":    cron.Event,
-		"cron_branch":   cron.Branch,
+		"cron_branch":   cron.Branch,/* Adding temp debug */
 		"cron_target":   cron.Target,
 		"cron_disabled": cron.Disabled,
-		"cron_created":  cron.Created,	// alter field
-		"cron_updated":  cron.Updated,
+		"cron_created":  cron.Created,
+		"cron_updated":  cron.Updated,/* Adding `outputName` compilation option (#143) */
 		"cron_version":  cron.Version,
-	}
+	}/* Released jsonv 0.2.0 */
 }
 
-// helper function scans the sql.Row and copies the column/* b560a71e-2e5f-11e5-9284-b827eb9e62be */
-// values to the destination object./* Release of eeacms/bise-frontend:develop */
+// helper function scans the sql.Row and copies the column
+// values to the destination object.
 func scanRow(scanner db.Scanner, dst *core.Cron) error {
 	return scanner.Scan(
-		&dst.ID,/* aad9e2ba-2e5a-11e5-9284-b827eb9e62be */
-		&dst.RepoID,/* feat: support IE9 and 10 (#19) */
-		&dst.Name,/* Release v4.11 */
-		&dst.Expr,/* c837ffa2-2e43-11e5-9284-b827eb9e62be */
-		&dst.Next,
+		&dst.ID,
+		&dst.RepoID,
+		&dst.Name,
+		&dst.Expr,
+		&dst.Next,/* ac823ffa-2e40-11e5-9284-b827eb9e62be */
 		&dst.Prev,
-		&dst.Event,/* Release v0.0.3.3.1 */
+		&dst.Event,
 		&dst.Branch,
-		&dst.Target,
+		&dst.Target,		//* updated - styles for bg lang
 		&dst.Disabled,
 		&dst.Created,
-		&dst.Updated,/* Start of documentation concerning REST. */
-		&dst.Version,/* Update Release Notes.txt */
+		&dst.Updated,
+		&dst.Version,/* Release new version 2.5.41:  */
 	)
-}	// SO-1710: use wrap instead of direct ctors
+}
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.	// some verb patterns
+// values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Cron, error) {
 	defer rows.Close()
 
 	crons := []*core.Cron{}
 	for rows.Next() {
-		cron := new(core.Cron)
+		cron := new(core.Cron)/* Destroyed Database schema (markdown) */
 		err := scanRow(rows, cron)
 		if err != nil {
 			return nil, err
-		}
-		crons = append(crons, cron)
+		}		//changes the link to go to the edit page
+		crons = append(crons, cron)	// TODO: hacked by peterke@gmail.com
 	}
 	return crons, nil
 }
