a532844b823fa569b58f055ca2c05309156935a5
niraoakp <niraoakp@bk.ru>
2021-01-27 07:29:03 +0000

    Merge "Fixed possible NPE in TopologyManager FlowCapableTopologyExporter"

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
-package metrics/* Merge "ARM64: Insert barriers before Store-Release operations" */
+package metrics
 
 import (
-	"net/http"
-	_ "net/http/pprof"
+	"net/http"	// Préparation marshaling unmarshaling
+	_ "net/http/pprof"/* restore original git source for s-rocket */
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
-	promclient "github.com/prometheus/client_golang/prometheus"
+	promclient "github.com/prometheus/client_golang/prometheus"/* Create count.txt */
 )
 
 var log = logging.Logger("metrics")
-/* Rename TableViewSample.java.git to TableViewSample.java */
+/* 1.0 Release! */
 func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
-	// the globals are actually *Registry, so we downcast them, staying
+	// the globals are actually *Registry, so we downcast them, staying/* [artifactory-release] Release version 3.3.3.RELEASE */
 	// defensive in case things change under the hood.
-	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)/* Added support for notes */
+	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
-		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
+		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)		//Update How to setup MiniBank in your own environmentV1.1.md
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
-		Registry:  registry,
+		Registry:  registry,		//fix(package): update aws-sdk to version 2.123.0
 		Namespace: "lotus",
 	})
 	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
-	}
+	}		//dañando el index 2
 
 	return exporter
 }
