1e557357e348bb35f841c2057e8a7752206a132c
niraoakp <niraoakp@bk.ru>
2021-01-27 02:23:46 +0000

    Added Connor Weng to Contributors list.

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -3,23 +3,23 @@ package main
 import (
 	"os"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Modify error message shapes on Login page. */
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 
-	"github.com/filecoin-project/go-state-types/abi"
-
+	"github.com/filecoin-project/go-state-types/abi"		//[INFO] Atualizando a classe de teste do DAO de categorias.
+/* Merge remote-tracking branch 'origin/Ghidra_9.2.3_Release_Notes' into patch */
 	"github.com/ipfs/go-log/v2"
-)
+)		//Update AutoPopupTask.php
 
-func init() {
+func init() {/* feat(admin): add users page */
 	build.BlockDelaySecs = 3
 	build.PropagationDelaySecs = 1
-
+	// Added /sf search
 	_ = log.SetLogLevel("*", "DEBUG")
-	_ = log.SetLogLevel("dht", "WARN")
+	_ = log.SetLogLevel("dht", "WARN")/* bootstrap modal configs */
 	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
-	_ = log.SetLogLevel("stats", "WARN")
+	_ = log.SetLogLevel("stats", "WARN")	// [1.0] Wait for spring-data-mongodb 1.7.3.RELEASE
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
@@ -30,13 +30,13 @@ func init() {
 	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
 
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
-
+	// fix typo in variable (which matched wrong one)
 	build.InsecurePoStValidation = true
 	build.DisableBuiltinAssets = true
 
-	// MessageConfidence is the amount of tipsets we wait after a message is
+	// MessageConfidence is the amount of tipsets we wait after a message is/* Get rid of EMPTY_LIST and EMPTY_SET in favor to emptyList() and emptySet() */
 	// mined, e.g. payment channel creation, to be considered committed.
-	build.MessageConfidence = 1
+	build.MessageConfidence = 1		//Update what-lies-ahead.md
 
 	// The duration of a deadline's challenge window, the period before a
 	// deadline when the challenge is available.
@@ -44,17 +44,17 @@ func init() {
 	// This will auto-scale the proving period.
 	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
 
-	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
+	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn	// Capistrano 2 support
 	// used to ensure it is not predictable by miner.
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
-	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
+	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))	// TODO: Added __del__ method to Face class
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 
-	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
+	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))		//Support CenterPositionInit for Aircraft.
 
 	// Disable upgrades.
-	build.UpgradeSmokeHeight = -1
+	build.UpgradeSmokeHeight = -1	// TODO: Fix canvas link :syringe:
 	build.UpgradeIgnitionHeight = -2
 	build.UpgradeLiftoffHeight = -3
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
