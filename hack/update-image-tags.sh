0d8e1ce4fde8cecb1798f0da073d6fe1447309ac
niraoakp <niraoakp@bk.ru>
2021-01-27 00:34:46 +0000

    Rename app-options

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
-
+	// TODO: hacked by nick@perfectabstractions.com
 dir=$1
 image_tag=$2
-
+/* Add common workflows topic */
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
