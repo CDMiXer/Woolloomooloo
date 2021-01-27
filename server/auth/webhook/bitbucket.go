11f8225d40bc18d717b71be47920d362e9b17107
niraoakp <niraoakp@bk.ru>
2021-01-27 15:29:21 +0000

    Intermediate commit on improved ISU SM stuff

--- server/auth/webhook/bitbucket.go
+++ server/auth/webhook/bitbucket.go
@@ -1,5 +1,5 @@
 package webhook
-		//5ddcc3ee-2e3f-11e5-9284-b827eb9e62be
+
 import (
 	"net/http"
 
@@ -9,11 +9,11 @@ import (
 func bitbucketMatch(secret string, r *http.Request) bool {
 	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))
 	if err != nil {
-		return false	// TODO: will be fixed by timnugent@gmail.com
+		return false
 	}
 	_, err = hook.Parse(r,
-		bitbucket.RepoPushEvent,	// TODO: Merge "Removed refreshLinks2 comment"
-		bitbucket.RepoForkEvent,		//debugging. kind of working now...
+		bitbucket.RepoPushEvent,
+		bitbucket.RepoForkEvent,
 		bitbucket.RepoUpdatedEvent,
 		bitbucket.RepoCommitCommentCreatedEvent,
 		bitbucket.RepoCommitStatusCreatedEvent,
@@ -22,12 +22,12 @@ func bitbucketMatch(secret string, r *http.Request) bool {
 		bitbucket.IssueUpdatedEvent,
 		bitbucket.IssueCommentCreatedEvent,
 		bitbucket.PullRequestCreatedEvent,
-		bitbucket.PullRequestUpdatedEvent,	// TODO: will be fixed by aeongrp@outlook.com
+		bitbucket.PullRequestUpdatedEvent,
 		bitbucket.PullRequestApprovedEvent,
 		bitbucket.PullRequestUnapprovedEvent,
 		bitbucket.PullRequestMergedEvent,
-		bitbucket.PullRequestDeclinedEvent,	// TODO: Fixed Source Domain
-		bitbucket.PullRequestCommentCreatedEvent,/* fixed tag cloud support TEST */
+		bitbucket.PullRequestDeclinedEvent,
+		bitbucket.PullRequestCommentCreatedEvent,
 		bitbucket.PullRequestCommentUpdatedEvent,
 		bitbucket.PullRequestCommentDeletedEvent,
 	)
