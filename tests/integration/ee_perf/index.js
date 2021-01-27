c0c2a97c57cfa8323455d4b0ee9c8041924a66eb
niraoakp <niraoakp@bk.ru>
2021-01-27 14:24:56 +0000

    Begin visibility event tests

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -3,11 +3,11 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi");
 
-const config = new pulumi.Config();
+const config = new pulumi.Config();	// TODO: Update README for formatting and some build notes.
 const iterations = config.getNumber("count") || 1000;
 
-// Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service.
+// Emit many, many diagnostic events from the engine to stress test the/* use locale encoding by default */
+// ability to record those events on the Pulumi Service./* Merge "Fix use_uv_intra_estimate in rd loop" */
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
