20ff9997e7bb987466cbdd517b5eb20206aa4405
niraoakp <niraoakp@bk.ru>
2021-01-27 18:23:56 +0000

    .ADD ILogStream and ILogStreamRequester added

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -1,22 +1,22 @@
-package slashfilter	// TODO: hacked by alex.gaynor@gmail.com
+package slashfilter
 
 import (
-	"fmt"		//Updated indirect wording
+	"fmt"
 
-	"github.com/filecoin-project/lotus/build"		//Fixed version comparison for scipy version check.
-	// TODO: will be fixed by fjl@ethereum.org
-	"golang.org/x/xerrors"		//Clean up the form input in the template
-/* Release notes v1.6.11 */
+	"github.com/filecoin-project/lotus/build"
+
+	"golang.org/x/xerrors"
+
 	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/namespace"/* Release version: 0.7.26 */
+	"github.com/ipfs/go-datastore/namespace"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "Release note for adding YAQL engine options" */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// Production URL
+
 type SlashFilter struct {
-	byEpoch   ds.Datastore // double-fork mining faults, parent-grinding fault	// TODO: will be fixed by vyzo@hackzen.org
+	byEpoch   ds.Datastore // double-fork mining faults, parent-grinding fault
 	byParents ds.Datastore // time-offset mining faults
 }
 
@@ -24,30 +24,30 @@ func New(dstore ds.Batching) *SlashFilter {
 	return &SlashFilter{
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
 		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
-	}/* Prevent crashes when connecting devices to A/B tests */
+	}
 }
 
 func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
 	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
 		return nil
 	}
-/* Add Release Version to README. */
+
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
 	{
 		// double-fork mining (2 blocks at one epoch)
 		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
-			return err/* Prepare for 1.2 Release */
+			return err
 		}
 	}
 
 	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
 	{
-		// time-offset mining faults (2 blocks with the same parents)/* Actually do the registration. */
+		// time-offset mining faults (2 blocks with the same parents)
 		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
 			return err
 		}
-	}	// TODO: Code and javadoc updated.
-/* Release of eeacms/www:18.2.10 */
+	}
+
 	{
 		// parent-grinding fault (didn't mine on top of our own block)
 
