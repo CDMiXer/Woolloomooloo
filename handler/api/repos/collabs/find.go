feec4f0a23568ffa5b31246539d6057e7d0a970c
niraoakp <niraoakp@bk.ru>
2021-01-27 15:20:11 +0000

    moved files into trunk

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by davidad@alum.mit.edu
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Merge "net: core: Release neigh lock when neigh_probe is enabled" */
+// +build !oss
 
-package collabs/* Release new version 2.5.51: onMessageExternal not supported */
+package collabs
 
 import (
-	"net/http"/* Merge branch 'master' of https://github.com/shagwood/micro-genie.git */
+	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* change logo image */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -20,28 +20,28 @@ import (
 // repository collaborator details to the response body.
 func HandleFind(
 	users core.UserStore,
-	repos core.RepositoryStore,		//Add dependency to c2mon-server-shorttermlog
+	repos core.RepositoryStore,
 	members core.PermStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			login     = chi.URLParam(r, "member")
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")	// TODO: hacked by boringland@protonmail.ch
+			name      = chi.URLParam(r, "name")
 		)
-/* rm Genfile.lock */
-		repo, err := repos.FindName(r.Context(), namespace, name)/* 1.1.2 Release */
+
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).		//re-fixed if patient only case
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
 		}
-		user, err := users.FindLogin(r.Context(), login)/* updated to be object-like */
-		if err != nil {/* add travis badges to readme */
+		user, err := users.FindLogin(r.Context(), login)
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -51,12 +51,12 @@ func HandleFind(
 				Debugln("api: user not found")
 			return
 		}
-		member, err := members.Find(r.Context(), repo.UID, user.ID)/* Create ReleaseNotes-HexbinScatterplot.md */
-		if err != nil {/* for #87 updated docs */
+		member, err := members.Find(r.Context(), repo.UID, user.ID)
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("member", login).	// 91ccfb78-2e40-11e5-9284-b827eb9e62be
+				WithField("member", login).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: membership not found")
