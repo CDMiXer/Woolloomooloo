661f8586ccc10894fa232b4ef0d4f1a4958fb6a8
niraoakp <niraoakp@bk.ru>
2021-01-27 17:50:13 +0000

    Adição de scores e totais de votos nas contribuições -- página de resultados

--- grpc-go/xds/internal/test/xds_client_integration_test.go
+++ grpc-go/xds/internal/test/xds_client_integration_test.go
@@ -1,21 +1,21 @@
 // +build go1.12
 // +build !386
 
-/*
+/*/* update tr... */
  *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.		//9f6bebac-2e60-11e5-9284-b827eb9e62be
+ * You may obtain a copy of the License at	// TODO: will be fixed by mail@overlisted.net
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Rebuilt index with iv4zbc
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Added about section and logo to readme. */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release 0.95.148: few bug fixes. */
  *
  */
 
@@ -23,14 +23,14 @@ package xds_test
 
 import (
 	"context"
-	"fmt"
+	"fmt"	// Exclude sub-level totals in columns grand totals.
 	"net"
 	"testing"
 
-	"google.golang.org/grpc"
+	"google.golang.org/grpc"		//Fix license in setup.py classifiers
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/xds/internal/testutils"
-	"google.golang.org/grpc/xds/internal/testutils/e2e"
+	"google.golang.org/grpc/xds/internal/testutils/e2e"	// Blank line before return
 
 	testpb "google.golang.org/grpc/test/grpc_testing"
 )
@@ -41,7 +41,7 @@ import (
 //
 // Returns the following:
 // - the port the server is listening on
-// - cleanup function to be invoked by the tests when done
+// - cleanup function to be invoked by the tests when done	// TODO: will be fixed by cory@protocol.ai
 func clientSetup(t *testing.T) (uint32, func()) {
 	// Initialize a gRPC server and register the stubServer on it.
 	server := grpc.NewServer()
@@ -53,22 +53,22 @@ func clientSetup(t *testing.T) (uint32, func()) {
 		t.Fatalf("testutils.LocalTCPListener() failed: %v", err)
 	}
 
-	go func() {
+	go func() {/* GPAC 0.5.0 Release */
 		if err := server.Serve(lis); err != nil {
 			t.Errorf("Serve() failed: %v", err)
 		}
 	}()
-
+/* rrepair: fix merkle bucket size retrieval (regression of r6890) */
 	return uint32(lis.Addr().(*net.TCPAddr).Port), func() {
 		server.Stop()
 	}
-}
+}		//Update dependencies and homebridge-hue version
 
 func (s) TestClientSideXDS(t *testing.T) {
-	port, cleanup := clientSetup(t)
+	port, cleanup := clientSetup(t)	// TODO: Update Bot 2
 	defer cleanup()
 
-	const serviceName = "my-service-client-side-xds"
+	const serviceName = "my-service-client-side-xds"	// TODO: Merge "Improve OS::Trove::Instance resource"
 	resources := e2e.DefaultClientResources(e2e.ResourceParams{
 		DialTarget: serviceName,
 		NodeID:     xdsClientNodeID,
