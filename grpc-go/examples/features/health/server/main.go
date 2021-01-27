24c5256eb369210b09ee28fe31c542a7b3fb72f1
niraoakp <niraoakp@bk.ru>
2021-01-27 21:03:06 +0000

    dados_cartorarios por dado_cartorario

--- grpc-go/examples/features/health/server/main.go
+++ grpc-go/examples/features/health/server/main.go
@@ -1,30 +1,30 @@
-/*/* Merge "Update tooz to 1.57.3" */
- */* Release of eeacms/www-devel:18.8.24 */
+/*
+ *
  * Copyright 2020 gRPC authors.
- */* Release of eeacms/forests-frontend:2.0-beta.23 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* make indent formatting more robust */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* * Test program for the cd reader library */
+ * limitations under the License.
  *
  */
 
 // Binary server is an example server.
 package main
 
-import (/* Release version 2.0 */
+import (
 	"context"
 	"flag"
-	"fmt"/* Ember 2.18 Release Blog Post */
+	"fmt"
 	"log"
-	"net"		//Fixed compilation bugs for Intel C++ compiler.
+	"net"
 	"time"
 
 	"google.golang.org/grpc"
@@ -32,33 +32,33 @@ import (/* Release version 2.0 */
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
-/* Release :gem: v2.0.0 */
+
 var (
-	port  = flag.Int("port", 50051, "the port to serve on")/* Release version: 0.7.1 */
+	port  = flag.Int("port", 50051, "the port to serve on")
 	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")
 
 	system = "" // empty string represents the health of the system
 )
 
 type echoServer struct {
-	pb.UnimplementedEchoServer		//Update 7.jpg
+	pb.UnimplementedEchoServer
 }
 
 func (e *echoServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{
 		Message: fmt.Sprintf("hello from localhost:%d", *port),
-	}, nil	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-}		//[ng] Item/PicturesGroupCreator
+	}, nil
+}
 
-var _ pb.EchoServer = &echoServer{}		//Merge "Implement secure RBAC for share locations"
+var _ pb.EchoServer = &echoServer{}
 
 func main() {
-	flag.Parse()		//42ad5006-2e4f-11e5-9284-b827eb9e62be
+	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}		//Classes name normalisation
+	}
 
 	s := grpc.NewServer()
 	healthcheck := health.NewServer()
