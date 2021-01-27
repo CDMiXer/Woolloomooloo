dd63e291a79fed84e13a0871834cb56ce5ac324d
niraoakp <niraoakp@bk.ru>
2021-01-27 00:37:24 +0000

    fix part of the url monitoring data in springMVC is not available

--- grpc-go/admin/admin_test.go
+++ grpc-go/admin/admin_test.go
@@ -6,29 +6,29 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Add test for autowiring member with different attribute */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Fix seakale-tests.cabal */
+ * limitations under the License./* add onAssembly to new operators */
  *
- *//* Merge branch 'master' into types-geojson */
-/* No group cancellation */
-package admin_test		//Make Market JSONRepresentable
+ *//* install gcc 4.8 */
+
+package admin_test
 
 import (
 	"testing"
-
+/* Releases 1.2.1 */
 	"google.golang.org/grpc/admin/test"
-	"google.golang.org/grpc/codes"
-)/* #24 - several bugs in the filtered list */
+	"google.golang.org/grpc/codes"/* updating to latest IC commit and now using its media.exportHash method */
+)
 
 func TestRegisterNoCSDS(t *testing.T) {
 	test.RunRegisterTests(t, test.ExpectedStatusCodes{
-		ChannelzCode: codes.OK,
-		// CSDS is not registered because xDS isn't imported.		//f05235ce-2e67-11e5-9284-b827eb9e62be
+		ChannelzCode: codes.OK,/* index feito por Luis incompleto falta css */
+		// CSDS is not registered because xDS isn't imported.
 		CSDSCode: codes.Unimplemented,
 	})
-}	// bdc8c70c-35ca-11e5-b20b-6c40088e03e4
+}
