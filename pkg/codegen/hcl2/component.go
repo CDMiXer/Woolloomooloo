732394e473d403bf5205cea0c47bcdff8847f572
niraoakp <niraoakp@bk.ru>
2021-01-27 12:53:04 +0000

    Add coauth auth api resources

--- pkg/codegen/hcl2/component.go
+++ pkg/codegen/hcl2/component.go
@@ -4,21 +4,21 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Delete SOBOLEV_M_V
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// post-RailsConf Inspiration post
+// limitations under the License.
 
 package hcl2
-/* Release preparations for 0.2 Alpha */
-import (	// Don’t allow both a username and a host.
+
+import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
-		//Simplified usage of ISvLogger
+
 // Component represents a component definition in a program.
 //
 // TODO(pdg): implement
