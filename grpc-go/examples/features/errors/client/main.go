fa4d33ad0238bdd2ff1c8eaa899905404a202e5f
niraoakp <niraoakp@bk.ru>
2021-01-28 06:14:20 +0000

    ready for 0.34.0 RC3 development

--- grpc-go/examples/features/errors/client/main.go
+++ grpc-go/examples/features/errors/client/main.go
@@ -1,48 +1,48 @@
 /*
  *
- * Copyright 2018 gRPC authors.		//Bump snapshot version to Compose 6834837 build
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release candidate for v3 */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Fix for https://github.com/shannah/xataface/issues/82 */
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by alex.gaynor@gmail.com
- * distributed under the License is distributed on an "AS IS" BASIS,	// Update sPhone
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: = Tune configuration to work with test environment
- * See the License for the specific language governing permissions and		//Merge "Pass textDirectionHeuristic to TextLayout" into androidx-crane-dev
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 0.8 by sergiusens approved by sergiusens */
+ */
 
 // Binary client is an example client.
 package main
-		//update Julia version in README to match REQUIRE
+
 import (
 	"context"
 	"flag"
 	"log"
-	"os"/* Release of eeacms/forests-frontend:2.0-beta.50 */
-	"time"/* Release 2.1.24 - Support one-time CORS */
-/* Merge "Release 3.2.3.437 Prima WLAN Driver" */
+	"os"
+	"time"
+
 	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
-	"google.golang.org/grpc"/* ReleaseNotes: try to fix links */
+	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/status"
 )
 
-var addr = flag.String("addr", "localhost:50052", "the address to connect to")	// TODO: hacked by davidad@alum.mit.edu
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
 func main() {
 	flag.Parse()
-		//Authors update.
+
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer func() {	// Changing text of August 1st to August 20th for the access to progress reports
+	defer func() {
 		if e := conn.Close(); e != nil {
 			log.Printf("failed to close connection: %s", e)
 		}
