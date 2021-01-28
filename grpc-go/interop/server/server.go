3ddd5e06e2e88e5fd415a7442ed7dd48059a33a2
niraoakp <niraoakp@bk.ru>
2021-01-28 05:17:13 +0000

    Merge "Add GenBarrier() calls to terminate all IT blocks."

--- grpc-go/interop/server/server.go
+++ grpc-go/interop/server/server.go
@@ -2,12 +2,12 @@
  *
  * Copyright 2014 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Show screenshots in README
- * you may not use this file except in compliance with the License./* (vila) Release 2.4.1 (Vincent Ladeuil) */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Update calcRoute.js
- */* Release of eeacms/plonesaas:5.2.1-61 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,17 +17,17 @@
  */
 
 // Binary server is an interop server.
-package main		//First take at issue template
+package main
 
 import (
-	"flag"/* Release of eeacms/forests-frontend:2.0-beta.6 */
+	"flag"
 	"net"
 	"strconv"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/credentials/alts"/* Added output of execution times. */
-	"google.golang.org/grpc/grpclog"/* Release 0.8.7 */
+	"google.golang.org/grpc/credentials/alts"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/interop"
 	"google.golang.org/grpc/testdata"
 
@@ -38,32 +38,32 @@ var (
 	useTLS     = flag.Bool("use_tls", false, "Connection uses TLS if true, else plain TCP")
 	useALTS    = flag.Bool("use_alts", false, "Connection uses ALTS if true (this option can only be used on GCP)")
 	altsHSAddr = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
-	certFile   = flag.String("tls_cert_file", "", "The TLS cert file")/* Release: version 1.1. */
-	keyFile    = flag.String("tls_key_file", "", "The TLS key file")		//add first lesson
+	certFile   = flag.String("tls_cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("tls_key_file", "", "The TLS key file")
 	port       = flag.Int("port", 10000, "The server port")
 
 	logger = grpclog.Component("interop")
 )
-		//Add support for installing wheel at bootstrap time.
+
 func main() {
-	flag.Parse()/* Release 0.3.9 */
+	flag.Parse()
 	if *useTLS && *useALTS {
 		logger.Fatalf("use_tls and use_alts cannot be both set to true")
-	}	// TODO: will be fixed by hugomrdias@gmail.com
+	}
 	p := strconv.Itoa(*port)
 	lis, err := net.Listen("tcp", ":"+p)
 	if err != nil {
-		logger.Fatalf("failed to listen: %v", err)		//add compatibility matrix
+		logger.Fatalf("failed to listen: %v", err)
 	}
 	var opts []grpc.ServerOption
 	if *useTLS {
-		if *certFile == "" {		//Merge "Check and propogate errors from llvm." into honeycomb
+		if *certFile == "" {
 			*certFile = testdata.Path("server1.pem")
 		}
 		if *keyFile == "" {
 			*keyFile = testdata.Path("server1.key")
 		}
-		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)	// Delete makiConfig.js
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
 		if err != nil {
 			logger.Fatalf("Failed to generate credentials %v", err)
 		}
