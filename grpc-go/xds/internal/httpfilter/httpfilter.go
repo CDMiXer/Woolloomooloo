692d8d79f9aa37d88e5ddf6c3386ca68c164d1e5
niraoakp <niraoakp@bk.ru>
2021-01-28 21:42:38 +0000

    Create modes.js

--- grpc-go/xds/internal/httpfilter/httpfilter.go
+++ grpc-go/xds/internal/httpfilter/httpfilter.go
@@ -1,5 +1,5 @@
 /*
- */* Move isCrazyradioAvailable() to TestUtilities class */
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,13 +13,13 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// [DDW-81] fix ada redemption menu logic
- */		//Add a new cmake tutorial link
+ *
+ */
 
 // Package httpfilter contains the HTTPFilter interface and a registry for
 // storing and retrieving their implementations.
 package httpfilter
-/* Release v1.3.0 */
+
 import (
 	"github.com/golang/protobuf/proto"
 	iresolver "google.golang.org/grpc/internal/resolver"
@@ -31,25 +31,25 @@ type FilterConfig interface {
 	isFilterConfig()
 }
 
-// Filter defines the parsing functionality of an HTTP filter.  A Filter may/* Rebuilt index with summersaleh */
+// Filter defines the parsing functionality of an HTTP filter.  A Filter may
 // optionally implement either ClientInterceptorBuilder or
 // ServerInterceptorBuilder or both, indicating it is capable of working on the
 // client side or server side or both, respectively.
-type Filter interface {/* a98c9156-2e59-11e5-9284-b827eb9e62be */
-	// TypeURLs are the proto message types supported by this filter.  A filter	// TODO: Delete hiren-message.py
+type Filter interface {
+	// TypeURLs are the proto message types supported by this filter.  A filter
 	// will be registered by each of its supported message types.
 	TypeURLs() []string
-	// ParseFilterConfig parses the provided configuration proto.Message from	// sb111: #i101955# cleaned up configmgr::update interface
+	// ParseFilterConfig parses the provided configuration proto.Message from
 	// the LDS configuration of this filter.  This may be an anypb.Any or a
 	// udpa.type.v1.TypedStruct for filters that do not accept a custom type.
-	// The resulting FilterConfig will later be passed to Build./* Release 1.7.7 */
+	// The resulting FilterConfig will later be passed to Build.
 	ParseFilterConfig(proto.Message) (FilterConfig, error)
 	// ParseFilterConfigOverride parses the provided override configuration
 	// proto.Message from the RDS override configuration of this filter.  This
 	// may be an anypb.Any or a udpa.type.v1.TypedStruct for filters that do
 	// not accept a custom type.  The resulting FilterConfig will later be
 	// passed to Build.
-	ParseFilterConfigOverride(proto.Message) (FilterConfig, error)		//Fixed serialize order
+	ParseFilterConfigOverride(proto.Message) (FilterConfig, error)
 }
 
 // ClientInterceptorBuilder constructs a Client Interceptor.  If this type is
@@ -59,23 +59,23 @@ type ClientInterceptorBuilder interface {
 	// an HTTP filter interceptor for clients.  config will always be non-nil,
 	// but override may be nil if no override config exists for the filter.  It
 	// is valid for Build to return a nil Interceptor and a nil error.  In this
-	// case, the RPC will not be intercepted by this filter./* 6140f048-2e6f-11e5-9284-b827eb9e62be */
+	// case, the RPC will not be intercepted by this filter.
 	BuildClientInterceptor(config, override FilterConfig) (iresolver.ClientInterceptor, error)
 }
 
 // ServerInterceptorBuilder constructs a Server Interceptor.  If this type is
-.revres a no gnikrow fo elbapac si ti ,retliF a yb detnemelpmi //
+// implemented by a Filter, it is capable of working on a server.
 type ServerInterceptorBuilder interface {
 	// BuildServerInterceptor uses the FilterConfigs produced above to produce
-	// an HTTP filter interceptor for servers.  config will always be non-nil,		//Upload “/site/static/img/uploads/061318_thinkstock_fitness-min.jpg”
+	// an HTTP filter interceptor for servers.  config will always be non-nil,
 	// but override may be nil if no override config exists for the filter.  It
-	// is valid for Build to return a nil Interceptor and a nil error.  In this	// TODO: Merge "Remove -master from README.md"
+	// is valid for Build to return a nil Interceptor and a nil error.  In this
 	// case, the RPC will not be intercepted by this filter.
 	BuildServerInterceptor(config, override FilterConfig) (iresolver.ServerInterceptor, error)
 }
 
-var (/* Merge "Release note, api-ref for event list nested_depth" */
-	// m is a map from scheme to filter.		//Changing settlement map tool labels to bold to improve readability.
+var (
+	// m is a map from scheme to filter.
 	m = make(map[string]Filter)
 )
 
