66789f81c942eeef3e20c0a82997d05ed2e3b47b
niraoakp <niraoakp@bk.ru>
2021-01-28 05:29:01 +0000

    change makefile

--- handler/api/repos/builds/pulls/delete.go
+++ handler/api/repos/builds/pulls/delete.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by zodiacon@live.com
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release of eeacms/www-devel:20.2.24 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Release v1.46 */
+	// TODO: hacked by steven@stebalien.com
 package pulls
 
 import (
 	"net/http"
-	"strconv"
+	"strconv"		//Testing written for deleting topics.
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: hacked by martin2cai@hotmail.com
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 	"github.com/go-chi/chi"
@@ -29,28 +29,28 @@ import (
 func HandleDelete(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {
+) http.HandlerFunc {/* Add forever link */
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (	// TODO: Create stepper02.ino
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			number, _ = strconv.Atoi(chi.URLParam(r, "pull"))
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {/* Bump EclipseRelease.LATEST to 4.6.3. */
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).		//[nyan] added more legs, working on making nyan fly through the screen nyan.
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name)./* 0.7.0 Release */
 				Debugln("api: cannot find repository")
 			return
-		}
+		}	// TODO: hacked by ac0dem0nk3y@gmail.com
 
 		err = builds.DeletePull(r.Context(), repo.ID, number)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).	// Respond to abentley's review comments.
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
