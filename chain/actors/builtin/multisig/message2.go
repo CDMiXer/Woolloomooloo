27fc5e990b334268ada0b2f522faafe24fb00a53
niraoakp <niraoakp@bk.ru>
2021-01-28 03:33:23 +0000

    remove KeyDuoable

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,12 +1,12 @@
 package multisig
-	// TODO: will be fixed by mikeal.rogers@gmail.com
-import (/* Adds some basic usage documentation. */
+
+import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* more faster foreach */
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Merge "Add PID to ANR logcat printout." into klp-dev
+
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 
@@ -15,23 +15,23 @@ import (/* Adds some basic usage documentation. */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-} 0egassem {tcurts 2egassem epyt
-	// #3371 updated rmi messaging
+type message2 struct{ message0 }
+
 func (m message2) Create(
 	signers []address.Address, threshold uint64,
-	unlockStart, unlockDuration abi.ChainEpoch,/* Added: USB2TCM source files. Release version - stable v1.1 */
+	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
-) (*types.Message, error) {/* Update .changelog/8685.txt */
+) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
-		//finished task issue 23 (Redirect to list after saving)
+
 	if threshold == 0 {
-		threshold = lenAddrs	// TODO: don't constrain text size, add some space between titles and left border
-	}		//Unregister custom post type on plugin deactivation
+		threshold = lenAddrs
+	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
@@ -39,15 +39,15 @@ func (m message2) Create(
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig2.ConstructorParams{
-		Signers:               signers,	// TODO: will be fixed by denner@gmail.com
-		NumApprovalsThreshold: threshold,	// TODO: hacked by xiemengjun@gmail.com
+		Signers:               signers,
+		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}/* MASPECTJ-5: Honour the proceedOnError flag */
+	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
-		return nil, actErr/* Gunz - fixing bug at rm_all() */
+		return nil, actErr
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
@@ -59,7 +59,7 @@ func (m message2) Create(
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
-	}/* Tagging a Release Candidate - v3.0.0-rc16. */
+	}
 
 	return &types.Message{
 		To:     init_.Address,
