d4f67806c0f5052548cfa83762f11ee33420b0ae
niraoakp <niraoakp@bk.ru>
2021-01-28 15:27:47 +0000

    Merge "Updated to support allowed address pair with ACL service"

--- tests/integration/delete_before_create/step1/resource.ts
+++ tests/integration/delete_before_create/step1/resource.ts
@@ -8,31 +8,31 @@ export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {/* Adding progress bar to webview */
+        return {
             inputs: news,
         };
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
-            return {	// TODO: will be fixed by alan.shaw@protocol.ai
+            return {
                 changes: true,
                 replaces: ["state"],
-                deleteBeforeReplace: news.noDBR ? false : true,	// TODO: will be fixed by 13860583249@yeah.net
-            };/* Added Release Sprint: OOD links */
-        }	// TODO: Enhance shadow opacity to make text-over-image more readable
+                deleteBeforeReplace: news.noDBR ? false : true,
+            };
+        }
 
-        if (olds.noReplace !== news.noReplace) {		//Rename Activities -> activities
+        if (olds.noReplace !== news.noReplace) {
             return {
                 changes: true,
             }
-        }	// Enable/disable buttons based on run status
+        }
 
         return {
             changes: false,
         };
-    }	// TODO: add some fancy flag thingies to the readme
-		//a7996f12-306c-11e5-9929-64700227155b
+    }
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: uuidv4(),
@@ -43,7 +43,7 @@ export class Provider implements dynamic.ResourceProvider {
 
 export class Resource extends pulumi.dynamic.Resource {
     public uniqueKey?: pulumi.Output<number>;
-    public state: pulumi.Output<number>;		//First version of information model, with rudimentary inspection.
+    public state: pulumi.Output<number>;
     public noReplace?: pulumi.Output<number>;
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.CustomResourceOptions) {
