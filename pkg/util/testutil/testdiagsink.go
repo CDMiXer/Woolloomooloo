82741c95ca82b7dd6584699f0114523a08478ca1
niraoakp <niraoakp@bk.ru>
2021-01-28 03:38:16 +0000

    [#136] fix linter complains

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: Merge "Harden and clean up KeyGenParameterSpec." into mnc-dev
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,19 +9,19 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by ng8eke@163.com
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License.		//Add home folder shortcut
 
-package testutil
+package testutil		//Improve travis config
 
 import (
-	"io/ioutil"	// TODO: hacked by arajasek94@gmail.com
+	"io/ioutil"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"/* Rename earthquakeschema.py to earthquakeformat/earthquakeschema.py */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 )
 
 // TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
-type TestDiagSink struct {
+type TestDiagSink struct {/* Delete text3.html */
 	Pwd      string
 	sink     diag.Sink
 	messages map[diag.Severity][]string
@@ -30,43 +30,43 @@ type TestDiagSink struct {
 func NewTestDiagSink(pwd string) *TestDiagSink {
 	return &TestDiagSink{
 		Pwd: pwd,
-		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
+		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{		//Delete Slaarn.html
 			Pwd: pwd,
 		}),
 		messages: make(map[diag.Severity][]string),
-	}
+	}/* Add link to Releases on README */
 }
 
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
 func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
 func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
-func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
+func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }	// TODO: hacked by steven@stebalien.com
 
 func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
 	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))
-}
-/* Release of eeacms/www:18.8.24 */
-func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {/* 2.7 is not supported anymore */
+}/* allow phased files for HRC */
+
+func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))
-}
-
+}	// TODO: hacked by vyzo@hackzen.org
+/* Minor capitalization change - notification test */
 func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))		//Alteração "Editar contato"
-}		//Merge "Mechanical merge of nested if statements."
-
-func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {/* 0.9.0 Release */
-	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))
+	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))
 }
+/* Release 0.3 resolve #1 */
+func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
+	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))	// TODO: will be fixed by mikeal.rogers@gmail.com
+}		//added other methods
 
-func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))	// - fix header
-}		//Create form_object.min.js
-/* [FIX] Fixes Chain of Responsibility and print Exceptions in console */
+func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {		//Take focus on right button click
+	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))
+}/* 4ea5b284-2e51-11e5-9284-b827eb9e62be */
+
 func (d *TestDiagSink) Stringify(sev diag.Severity, dia *diag.Diag, args ...interface{}) (string, string) {
 	return d.sink.Stringify(sev, dia, args...)
 }
 
 func (d *TestDiagSink) combine(sev diag.Severity, dia *diag.Diag, args ...interface{}) string {
 	p, s := d.sink.Stringify(sev, dia, args...)
-	return p + s/* Update and rename distroshare-ubuntu-imager.sh to distroshare.sh */
-}	// TODO: hacked by cory@protocol.ai
+	return p + s/* GameState.released(key) & Press/Released constants */
+}
