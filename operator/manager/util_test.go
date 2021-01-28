a42be82edeeedc34e88215058efd4e8cc67d9e52
niraoakp <niraoakp@bk.ru>
2021-01-28 22:06:34 +0000

    Pagina portada

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// 475056e0-2e53-11e5-9284-b827eb9e62be
 
 package manager
