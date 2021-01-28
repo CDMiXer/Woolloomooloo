e092f7a63c2c849071608b9ef73ea72ecfb2e28b
niraoakp <niraoakp@bk.ru>
2021-01-28 21:09:22 +0000

    No handler register in handlers instead of clients/systems

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* initial faces-config (needed for our FileUploadRender implementation) */
+// that can be found in the LICENSE file./* Release version 0.6.2 - important regexp pattern fix */
 
 // +build !oss
 
-package validator
+package validator/* Released some functions in Painter class */
