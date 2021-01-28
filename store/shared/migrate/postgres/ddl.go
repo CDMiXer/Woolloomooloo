3310d5c5467b27800a4ced8ac83804e897e9f63b
niraoakp <niraoakp@bk.ru>
2021-01-28 22:30:32 +0000

    Front-page typo fix :)

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "Write eventlog entries for notification visibility"
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package postgres
+package postgres	// TODO: rev 718456
 
 //go:generate togo ddl -package postgres -dialect postgres
