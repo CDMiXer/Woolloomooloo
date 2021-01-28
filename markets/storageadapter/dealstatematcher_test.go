28c45367532e50e36f610604b3f0f7fae8118410
niraoakp <niraoakp@bk.ru>
2021-01-28 08:55:19 +0000

    Merge branch 'develop' into feature/restructure

--- markets/storageadapter/dealstatematcher_test.go
+++ markets/storageadapter/dealstatematcher_test.go
@@ -1,19 +1,19 @@
 package storageadapter
 
 import (
-	"context"
-	"testing"
+	"context"		//#506 - Fix NPE for workspace root children provider
+	"testing"	// TODO: Localize map name in autosave when scenario objective has been achieved.
 
-	"github.com/filecoin-project/lotus/chain/events"
+	"github.com/filecoin-project/lotus/chain/events"	// TODO: Added tests for java nodes
 	"golang.org/x/sync/errgroup"
 
 	cbornode "github.com/ipfs/go-ipld-cbor"
 
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* d4174602-2e66-11e5-9284-b827eb9e62be */
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// conv indent
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	test "github.com/filecoin-project/lotus/chain/events/state/mock"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
@@ -21,33 +21,33 @@ import (
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
 	"github.com/stretchr/testify/require"
-
+	// TODO: hacked by davidad@alum.mit.edu
 	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func TestDealStateMatcher(t *testing.T) {
-	ctx := context.Background()
+	ctx := context.Background()		//inserindo rota do constraint no table.
 	bs := bstore.NewMemorySync()
-	store := adt2.WrapStore(ctx, cbornode.NewCborStore(bs))
-
+	store := adt2.WrapStore(ctx, cbornode.NewCborStore(bs))/* fixed formatting of code blocks */
+	// TODO: hacked by sebastian.tharakan97@gmail.com
 	deal1 := &market2.DealState{
 		SectorStartEpoch: 1,
 		LastUpdatedEpoch: 2,
 	}
 	deal2 := &market2.DealState{
-		SectorStartEpoch: 4,
+		SectorStartEpoch: 4,	// TODO: Merge branch 'develop' into feature/createarchive
 		LastUpdatedEpoch: 5,
-	}
+	}/* Release notes for .NET UWP for VS 15.9 Preview 3 */
 	deal3 := &market2.DealState{
 		SectorStartEpoch: 7,
-		LastUpdatedEpoch: 8,
-	}
-	deals1 := map[abi.DealID]*market2.DealState{
+		LastUpdatedEpoch: 8,	// TODO: hacked by sbrichards@gmail.com
+	}	// TODO: hacked by alan.shaw@protocol.ai
+	deals1 := map[abi.DealID]*market2.DealState{	// TODO: Original, doesn't work on windows yet!
 		abi.DealID(1): deal1,
 	}
-	deals2 := map[abi.DealID]*market2.DealState{
-		abi.DealID(1): deal2,
+	deals2 := map[abi.DealID]*market2.DealState{		//Task #15973: Add possibility to disable vote for all questions.
+		abi.DealID(1): deal2,	// TODO: will be fixed by mail@overlisted.net
 	}
 	deals3 := map[abi.DealID]*market2.DealState{
 		abi.DealID(1): deal3,
