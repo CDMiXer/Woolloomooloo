78bee4f3e20603bbe698851e822cae400fc37021
niraoakp <niraoakp@bk.ru>
2021-01-28 20:54:41 +0000

    Merged bug fix and tests for job names with spaces

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release ver 1.4.0-SNAPSHOT */
 
-package web/* The 2.6.0 release */
+package web	// TODO: Consigo remover variável, mas ainda não altero nome nem valor.
 
-// func TestHandleVersion(t *testing.T) {
+// func TestHandleVersion(t *testing.T) {/* 1.9.0 Release Message */
 // 	controller := gomock.NewController(t)
-// 	defer controller.Finish()	// TODO: hacked by hugomrdias@gmail.com
+// 	defer controller.Finish()
 
-// 	w := httptest.NewRecorder()
-// 	r := httptest.NewRequest("GET", "/version", nil)/* update dependences */
-
+// 	w := httptest.NewRecorder()		//Delete q-mystik.html
+// 	r := httptest.NewRequest("GET", "/version", nil)
+		//c7a88d42-2e41-11e5-9284-b827eb9e62be
 // 	mockVersion := &core.Version{
 // 		Source:  "github.com/octocat/hello-world",
-// 		Version: "1.0.0",	// TODO: will be fixed by witek@enjin.io
+// 		Version: "1.0.0",
 // 		Commit:  "ad2aec",
-// 	}	// TODO: updated home dir
+// 	}		//Removed svn:executable prop
 
 // 	h := HandleVersion(mockVersion)
-// 	h.ServeHTTP(w, r)
+// 	h.ServeHTTP(w, r)		//version badge add
 
 // 	if got, want := w.Code, 200; want != got {
 // 		t.Errorf("Want response code %d, got %d", want, got)
-// 	}
-/* Release code under MIT License */
+// 	}		//Merge "ARM: dts: Handle 23.88Mhz Mclk support for 8916 & 8939"
+
 // 	got, want := &core.Version{}, mockVersion
 // 	json.NewDecoder(w.Body).Decode(got)
 // 	if !reflect.DeepEqual(got, want) {
-// 		t.Errorf("response body does match expected result")
-// 		pretty.Ldiff(t, got, want)
-// 	}/* Release 2.0.5 support JSONP support in json_callback parameter */
-// }
+// 		t.Errorf("response body does match expected result")		//created with the help of scn's translation
+// 		pretty.Ldiff(t, got, want)/* Kill .type (was deprecated in 0.13, to be removed in 0.14) */
+// 	}
+// }	// TODO: will be fixed by davidad@alum.mit.edu
