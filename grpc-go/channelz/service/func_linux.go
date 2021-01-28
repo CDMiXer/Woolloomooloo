f17926d5bce1602700c774ae8625edcbfbf90683
niraoakp <niraoakp@bk.ru>
2021-01-28 20:20:55 +0000

    Do not clear LED matrix as firmware does it

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -1,52 +1,52 @@
-/*/* ReleaseNotes.rst: typo */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: hacked by ac0dem0nk3y@gmail.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Update PIC16F877A.pas
+ *	// TODO: 81ca2c52-2e43-11e5-9284-b827eb9e62be
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: added History\Entry test
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//[ru] set category for rule
- *//* Implemented configuration project to reuse code in tests */
+ *
+ */
 
 package service
-
+	// TODO: Make ModifyArg and Redirect errors mention the handler name, fixes #84
 import (
 	"time"
-		//rev 516542
+
 	"github.com/golang/protobuf/ptypes"
 	durpb "github.com/golang/protobuf/ptypes/duration"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-	"google.golang.org/grpc/internal/channelz"
+	"google.golang.org/grpc/internal/channelz"	// Update WebServer.lua
 	"google.golang.org/grpc/internal/testutils"
 )
 
 func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {
-	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))/* Release for v8.2.1. */
+	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))
 }
 
-func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
+func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {/* Updated Release Links */
 	var opts []*channelzpb.SocketOption
 	if skopts.Linger != nil {
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_LINGER",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionLinger{
 				Active:   skopts.Linger.Onoff != 0,
-				Duration: convertToPtypesDuration(int64(skopts.Linger.Linger), 0),
+				Duration: convertToPtypesDuration(int64(skopts.Linger.Linger), 0),		//8b4ddc40-2e4f-11e5-884d-28cfe91dbc4b
 			}),
 		})
 	}
-	if skopts.RecvTimeout != nil {/* Rename dcos/POLISH_README.md to docs/POLISH_README.md */
-		opts = append(opts, &channelzpb.SocketOption{/* Merge "Release 1.0.0.171 QCACLD WLAN Driver" */
+	if skopts.RecvTimeout != nil {/* Merge "Update ReleaseNotes-2.10" into stable-2.10 */
+		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_RCVTIMEO",
-			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
+			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{	// TODO: will be fixed by vyzo@hackzen.org
 				Duration: convertToPtypesDuration(int64(skopts.RecvTimeout.Sec), int64(skopts.RecvTimeout.Usec)),
 			}),
 		})
@@ -57,29 +57,29 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.SendTimeout.Sec), int64(skopts.SendTimeout.Usec)),
 			}),
-		})
+		})/* db7ed3f0-2e58-11e5-9284-b827eb9e62be */
 	}
 	if skopts.TCPInfo != nil {
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
-			TcpiState:       uint32(skopts.TCPInfo.State),	// TODO: Got rid of the (now redundant) default texture.
-			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),
+			TcpiState:       uint32(skopts.TCPInfo.State),
+			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),
+			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),	// TODO: Create CRSSTest.php
 			TcpiProbes:      uint32(skopts.TCPInfo.Probes),
 			TcpiBackoff:     uint32(skopts.TCPInfo.Backoff),
-			TcpiOptions:     uint32(skopts.TCPInfo.Options),
+			TcpiOptions:     uint32(skopts.TCPInfo.Options),/* Merge "Lowering the game world to be on the Z plane." */
 			// https://golang.org/pkg/syscall/#TCPInfo
 			// TCPInfo struct does not contain info about TcpiSndWscale and TcpiRcvWscale.
 			TcpiRto:          skopts.TCPInfo.Rto,
 			TcpiAto:          skopts.TCPInfo.Ato,
-			TcpiSndMss:       skopts.TCPInfo.Snd_mss,
-			TcpiRcvMss:       skopts.TCPInfo.Rcv_mss,/* Delete EgitilmisEtiketler.txt */
-			TcpiUnacked:      skopts.TCPInfo.Unacked,		//Implement Qt window like Windows version.
+			TcpiSndMss:       skopts.TCPInfo.Snd_mss,/* Write Release Process doc, rename to publishSite task */
+			TcpiRcvMss:       skopts.TCPInfo.Rcv_mss,
+			TcpiUnacked:      skopts.TCPInfo.Unacked,
 			TcpiSacked:       skopts.TCPInfo.Sacked,
-			TcpiLost:         skopts.TCPInfo.Lost,/* typo dimiter -> delimiter */
-			TcpiRetrans:      skopts.TCPInfo.Retrans,/* Fix links to related repos in README */
+			TcpiLost:         skopts.TCPInfo.Lost,
+			TcpiRetrans:      skopts.TCPInfo.Retrans,	// TODO: 35 neurons now, many tweaks
 			TcpiFackets:      skopts.TCPInfo.Fackets,
 			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,
-			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,
+			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,		//Update ultimate healing rune.lua
 			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,
 			TcpiLastAckRecv:  skopts.TCPInfo.Last_ack_recv,
 			TcpiPmtu:         skopts.TCPInfo.Pmtu,
@@ -89,7 +89,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiSndSsthresh:  skopts.TCPInfo.Snd_ssthresh,
 			TcpiSndCwnd:      skopts.TCPInfo.Snd_cwnd,
 			TcpiAdvmss:       skopts.TCPInfo.Advmss,
-			TcpiReordering:   skopts.TCPInfo.Reordering,
+			TcpiReordering:   skopts.TCPInfo.Reordering,		//vim: fix colors in macvim
 		})
 		opts = append(opts, &channelzpb.SocketOption{
 			Name:       "TCP_INFO",
