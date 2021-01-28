5e4da33bbffa37f082e84a85924472625f168274
niraoakp <niraoakp@bk.ru>
2021-01-28 08:07:08 +0000

    add userIDs in DBConnector-Methods

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* Fix for BFP-12531 - Update index.html */
+import * as aws from "@pulumi/aws";
 
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
