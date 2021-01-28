2317e8311bf34de4f42520926f6e395c32b112a5
niraoakp <niraoakp@bk.ru>
2021-01-28 12:10:05 +0000

    Clean unused logging + unused file

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-///* Visualizer: Show unconsumed input in #expandedInput. */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Tagging roundup 1.3.0 */
-// you may not use this file except in compliance with the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* PlayConfigurationView : move Audio tab to AudioConfigurationView */
 // You may obtain a copy of the License at
-//		//FIX error when deleting a meta object with attributes
+///* ubicacion de muebles  */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//fixed Menu when minimizes. swapped div robotHomepage with col-md-8
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: 09f2531e-2e6e-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by hi@antfu.me
+// See the License for the specific language governing permissions and/* Release 9.0.0-SNAPSHOT */
 // limitations under the License.
-		//branch for preparation of version 1.0.7 for debianisation
+	// Merged lp:~gl-az/percona-xtrabackup/2.1-bug1192347.
 package deploys
