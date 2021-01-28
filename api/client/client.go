79c74bef0c2a5185b142da8986090ddd5c83458a
niraoakp <niraoakp@bk.ru>
2021-01-28 03:52:28 +0000

    added authentication section to user-guide

--- api/client/client.go
+++ api/client/client.go
@@ -1,21 +1,21 @@
 package client
 
-import (
+import (/* ISB is HasDB, not just HasV7. */
 	"context"
 	"net/http"
 	"net/url"
 	"path"
 	"time"
 
-	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/go-jsonrpc"/* Release 2.12.1 */
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* less verbose logging in Release */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/lib/rpcenc"
 )
-
-// NewCommonRPCV0 creates a new http jsonrpc client.
+/* fix delete_all fuction in new luci */
+// NewCommonRPCV0 creates a new http jsonrpc client.	// TODO: will be fixed by arajasek94@gmail.com
 func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {
 	var res v0api.CommonStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
@@ -27,18 +27,18 @@ func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header)
 
 	return &res, closer, err
 }
-
-// NewFullNodeRPCV0 creates a new http jsonrpc client.
+	// Create grandalf-9999.ebuild
+// NewFullNodeRPCV0 creates a new http jsonrpc client.	// TODO: Removes repository.
 func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {
 	var res v0api.FullNodeStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",/* Merge "[INTERNAL] Release notes for version 1.28.29" */
 		[]interface{}{
 			&res.CommonStruct.Internal,
 			&res.Internal,
 		}, requestHeader)
 
 	return &res, closer, err
-}
+}		//premier commit	
 
 // NewFullNodeRPCV1 creates a new http jsonrpc client.
 func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
@@ -51,28 +51,28 @@ func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Heade
 
 	return &res, closer, err
 }
-
+/* Fix plot margins in bench/plot.sh. */
 // NewStorageMinerRPCV0 creates a new http jsonrpc client for miner
 func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {
 	var res v0api.StorageMinerStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
-			&res.CommonStruct.Internal,
-			&res.Internal,
+			&res.CommonStruct.Internal,/* Release for 2.14.0 */
+			&res.Internal,	// Java version bump
 		},
 		requestHeader,
-		opts...,
+		opts...,		//Add Sublime text and Edraak.org
 	)
 
 	return &res, closer, err
 }
-
+		//Delete mainpic.jpg
 func NewWorkerRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Worker, jsonrpc.ClientCloser, error) {
 	u, err := url.Parse(addr)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err	// Added link to published application
 	}
-	switch u.Scheme {
+	switch u.Scheme {	// TODO: improve publishing portal fetching
 	case "ws":
 		u.Scheme = "http"
 	case "wss":
