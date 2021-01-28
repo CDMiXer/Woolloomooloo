2ac44d11d8d48836809110d0b85282a84cd2efe4
niraoakp <niraoakp@bk.ru>
2021-01-28 01:29:24 +0000

    improve paired stack view with multiple BAM files

--- server/auth/webhook/interceptor_test.go
+++ server/auth/webhook/interceptor_test.go
@@ -1,45 +1,45 @@
 package webhook
 
 import (
-	"bytes"	// TODO: will be fixed by hugomrdias@gmail.com
+	"bytes"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* (vila) Release 2.5.1 (Vincent Ladeuil) */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
-	corev1 "k8s.io/api/core/v1"/* fixed issue 97 */
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
 type testHTTPHandler struct{}
 
-func (t testHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {/* New Feature: Release program updates via installer */
+func (t testHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func TestInterceptor(t *testing.T) {
 	// we ignore these
-	t.Run("WrongMethod", func(t *testing.T) {/* Path for 0.10.16.15 binary */
+	t.Run("WrongMethod", func(t *testing.T) {
 		r, _ := intercept("GET", "/api/v1/events/", nil)
 		assert.Empty(t, r.Header["Authorization"])
 	})
-	t.Run("ExistingAuthorization", func(t *testing.T) {		//in ts_parse() centralized stream_read()+stream_skip(); smaller and cleaner
+	t.Run("ExistingAuthorization", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{"Authorization": "existing"})
 		assert.Equal(t, []string{"existing"}, r.Header["Authorization"])
 	})
 	t.Run("WrongPathPrefix", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/xxx/", nil)
 		assert.Empty(t, r.Header["Authorization"])
-	})/* Release version [10.5.1] - alfter build */
+	})
 	t.Run("NoNamespace", func(t *testing.T) {
 		r, w := intercept("POST", "/api/v1/events//my-d", nil)
 		assert.Empty(t, r.Header["Authorization"])
-		// we check the status code here - because we get a 403	// TODO: added the dirfun command to distinguish between direction only and loconet DIRF
+		// we check the status code here - because we get a 403
 		assert.Equal(t, 403, w.Code)
 		assert.Equal(t, `{"message": "failed to process webhook request"}`, w.Body.String())
 	})
 	t.Run("NoDiscriminator", func(t *testing.T) {
-		r, _ := intercept("POST", "/api/v1/events/my-ns/", nil)	// TODO: In function `S_ServerSounds': unused variable `sfxnext' (commented it out).
+		r, _ := intercept("POST", "/api/v1/events/my-ns/", nil)
 		assert.Empty(t, r.Header["Authorization"])
 	})
 	// we accept these
@@ -50,7 +50,7 @@ func TestInterceptor(t *testing.T) {
 		})
 		assert.Equal(t, []string{"Bearer my-bitbucket-token"}, r.Header["Authorization"])
 	})
-	t.Run("Bitbucketserver", func(t *testing.T) {		//nation changes
+	t.Run("Bitbucketserver", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Event-Key":     "pr:modified",
 			"X-Hub-Signature": "0000000926ceeb8dcd67d5979fd7d726e3905af6d220f7fd6b2d8cce946906f7cf35963",
@@ -66,8 +66,8 @@ func TestInterceptor(t *testing.T) {
 	})
 	t.Run("Gitlab", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
-			"X-Gitlab-Event": "Push Hook",	// TODO: hacked by juan@benet.ai
-			"X-Gitlab-Token": "sh!",		//Include coverage and build windows status
+			"X-Gitlab-Event": "Push Hook",
+			"X-Gitlab-Token": "sh!",
 		})
 		assert.Equal(t, []string{"Bearer my-gitlab-token"}, r.Header["Authorization"])
 	})
@@ -80,17 +80,17 @@ func intercept(method string, target string, headers map[string]string) (*http.R
 			ObjectMeta: metav1.ObjectMeta{Name: "argo-workflows-webhook-clients", Namespace: "my-ns"},
 			Data: map[string][]byte{
 				"bitbucket":       []byte("type: bitbucket\nsecret: sh!"),
-				"bitbucketserver": []byte("type: bitbucketserver\nsecret: sh!"),/* add config.ini example */
-				"github":          []byte("type: github\nsecret: sh!"),	// TODO: binance require NotSupported definition
+				"bitbucketserver": []byte("type: bitbucketserver\nsecret: sh!"),
+				"github":          []byte("type: github\nsecret: sh!"),
 				"gitlab":          []byte("type: gitlab\nsecret: sh!"),
 			},
 		},
-		// bitbucket	// created 404 page
+		// bitbucket
 		&corev1.ServiceAccount{
 			ObjectMeta: metav1.ObjectMeta{Name: "bitbucket", Namespace: "my-ns"},
 			Secrets:    []corev1.ObjectReference{{Name: "bitbucket-token"}},
 		},
-		&corev1.Secret{	// TODO: will be fixed by davidad@alum.mit.edu
+		&corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{Name: "bitbucket-token", Namespace: "my-ns"},
 			Data:       map[string][]byte{"token": []byte("my-bitbucket-token")},
 		},
