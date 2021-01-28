701bbf52947a85437934e0f2d8dde761851d0d46
niraoakp <niraoakp@bk.ru>
2021-01-28 06:53:08 +0000

    Changed error message below the submit button

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Fixed a small wrong part of dislike table */
 
 package builds
 
 import (
-	"context"
+	"context"		//4cf297d6-2e4c-11e5-9284-b827eb9e62be
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-
+/* - Release number set to 9.2.2 */
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-)
+	"github.com/google/go-cmp/cmp"/* commented out concert section */
+)	// TODO: will be fixed by igor@soramitsu.co.jp
 
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Add Release notes to  bottom of menu */
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
 
 	stages := mock.NewMockStageStore(controller)
-	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
-
+	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)	// TODO: blacklisting monsters
+	// Fixed #148 - input dir path doesn't need to end with '/'
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
@@ -39,20 +39,20 @@ func TestFind(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Updated copyright notices. Released 2.1.0 */
 	)
 
 	HandleFind(repos, builds, stages)(w, r)
 
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, 200; want != got {/* Release of eeacms/varnish-copernicus-land:1.3 */
+		t.Errorf("Want response code %d, got %d", want, got)/* ListDrawInteractive handles partial view input */
 	}
 
-	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
-	json.NewDecoder(w.Body).Decode(got)
+	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}/* Release of eeacms/apache-eea-www:6.1 */
+	json.NewDecoder(w.Body).Decode(got)	// TODO: Create vigdis-namtvedt-kvalheim.md
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}	// TODO: Update composer.json update version
 }
 
 func TestFind_BadRequest(t *testing.T) {
@@ -63,9 +63,9 @@ func TestFind_BadRequest(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r = r.WithContext(		//install tasks created. cleanedup events to get more control.
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)/* Release tag: 0.7.4. */
 
 	HandleFind(nil, nil, nil)(w, r)
 
