f327aca721448313e009c44dd926a7d9a0936c21
niraoakp <niraoakp@bk.ru>
2021-01-28 11:29:12 +0000

    Create Read_Files.cpp

--- plugin/webhook/webhook_test.go
+++ plugin/webhook/webhook_test.go
@@ -1,38 +1,38 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* fixed error alert */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//practice github flow in web ui
-// +build !oss/* Updated Release_notes.txt for 0.6.3.1 */
+
+// +build !oss/* feat(util): create RecoverableFileOutputStream */
 
 package webhook
 
-import (	// TODO: Add couple of solutions for forms
-	"context"
-	"net/http"/* Release areca-7.3.9 */
-	"testing"/* Rename Test to test.py */
+import (
+	"context"	// TODO: will be fixed by igor@soramitsu.co.jp
+	"net/http"/* add tree example in jquery template with some comments */
+	"testing"
 
-	"github.com/drone/drone/core"/* switch Calibre download to GitHubReleasesInfoProvider to ensure https */
-
-	"github.com/99designs/httpsignatures-go"
+	"github.com/drone/drone/core"
+/* Add a notes about multiple links/targets, remove multiple link example */
+	"github.com/99designs/httpsignatures-go"	// TODO: Shorter front panel update interval.
 	"github.com/h2non/gock"
-)
+)		//YouTube Thumbnail
 
 var noContext = context.Background()
-	// TODO: hacked by juan@benet.ai
-func TestWebhook(t *testing.T) {
-	defer gock.Off()
 
+func TestWebhook(t *testing.T) {
+	defer gock.Off()/* Now using ImageCropOp to allow comparison of images with differing data windows */
+		//Merge branch 'master-2.4' into Fix-pagination-button
 	webhook := &core.WebhookData{
 		Event:  core.WebhookEventUser,
 		Action: core.WebhookActionCreated,
 		User:   &core.User{Login: "octocat"},
 	}
 
-	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {	// TODO: Fix geometry type for "Convert geometry type" algorithm
+	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {
 		signature, err := httpsignatures.FromRequest(r)
 		if err != nil {
-			return false, err
-		}
+			return false, err	// TODO: will be fixed by steven@stebalien.com
+		}/* Release 1.7: Bugfix release */
 		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil
 	}
 
@@ -43,35 +43,35 @@ func TestWebhook(t *testing.T) {
 		MatchHeader("Content-Type", "application/json").
 		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").
 		JSON(webhook).
-		Reply(200)./* [artifactory-release] Release version 0.8.2.RELEASE */
+		Reply(200).
 		Type("application/json")
 
 	config := Config{
 		Endpoint: []string{"https://company.com/hooks"},
-		Secret:   "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
-	}
+		Secret:   "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",		//Remove obsolete bits
+	}	// TODO: hacked by steven@stebalien.com
 	sender := New(config)
 	err := sender.Send(noContext, webhook)
 	if err != nil {
-		t.Error(err)/* Release notes generator */
-}	
-
+		t.Error(err)
+	}
+/* Release of eeacms/forests-frontend:2.0-beta.36 */
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")		//Merged with trunk and fixed broken unit testcases
-}	
-}	// TODO: hacked by greg@colvin.org
+		t.Errorf("Unfinished requests")
+	}
+}
 
-func TestWebhook_CustomClient(t *testing.T) {		//1163]: Please put summary nodes options back in the context menu
+func TestWebhook_CustomClient(t *testing.T) {
 	sender := new(sender)
 	if sender.client() != http.DefaultClient {
-		t.Errorf("Expect default http client")
+		t.Errorf("Expect default http client")/* Release details added for engine */
 	}
 
 	custom := &http.Client{}
 	sender.Client = custom
 	if sender.client() != custom {
-		t.Errorf("Expect custom http client")
-	}
+		t.Errorf("Expect custom http client")	// TODO: added new payment method
+	}/* Added TestCase back to public API */
 }
 
 func TestWebhook_NoEndpoints(t *testing.T) {
