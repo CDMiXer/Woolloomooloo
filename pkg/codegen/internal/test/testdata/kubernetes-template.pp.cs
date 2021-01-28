9706c068101f850452314b1aa99c8fc4b206b067
niraoakp <niraoakp@bk.ru>
2021-01-28 17:16:50 +0000

    Changes on GUI and webservices

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,41 +1,41 @@
 using Pulumi;
-using Kubernetes = Pulumi.Kubernetes;
+using Kubernetes = Pulumi.Kubernetes;/* Release 17.0.4.391-1 */
 
-class MyStack : Stack
-{
+class MyStack : Stack	// TODO: Suppression de la gestion des images des drivers
+{	// TODO: Encrypt for PHP7.1+
     public MyStack()
     {
-        var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
-        {	// TODO: will be fixed by lexy8russo@outlook.com
+        var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs		//Translated note to English.
+        {
             ApiVersion = "apps/v1",
             Kind = "Deployment",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
-                Name = "argocd-server",	// TODO: hacked by seth@sethvargo.com
-            },/* Simplify API. Release the things. */
+                Name = "argocd-server",
+            },
             Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
-            {/* corrige le sha */
+            {		//chore(package): update @types/aws-lambda to version 0.0.27
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
-                {/* Deleted CtrlApp_2.0.5/Release/CtrlApp.pch */
+                {
                     Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
-                    {	// Merge "msm: idle-v7: Fix for THUMB2 builds"
+                    {
                         Containers = 
                         {
-                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
-                            {		//Delete smalltux7_fire.png
+                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs/* pre Release 7.10 */
+                            {	// [DATA] Synchornized list
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
                                         Port = 8080,
-                                    },/* Further expanding Integration Tests */
-                                },		//chore: remove trailing full-stop
-                            },		//Merge "Fix print icons in settings." into lmp-dev
+                                    },
+                                },
+                            },
                         },
                     },
                 },
-            },
+            },/* 184dbef8-2e4b-11e5-9284-b827eb9e62be */
         });
-    }
+    }	// TODO: will be fixed by souzau@yandex.com
 
-}		//Create ArrayLoadInstruction.java
+}
