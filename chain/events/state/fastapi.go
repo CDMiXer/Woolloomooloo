578ab6cd9837d6842de3d033b2040d5ec6b81fd8
niraoakp <niraoakp@bk.ru>
2021-01-28 14:09:21 +0000

    Conflicts resolved with rel-v6r22

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -7,24 +7,24 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release version: 0.7.22 */
+
 type FastChainApiAPI interface {
 	ChainAPI
-/* Overview updated */
-	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)/* Initial Release (0.1) */
+
+	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
 
-type fastAPI struct {		//Merge branch 'stretch-unstable' into dedicated_php_service
+type fastAPI struct {
 	FastChainApiAPI
 }
 
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
-	return &fastAPI{	// TODO: Atari love - Fleet Systems Word Processor
+	return &fastAPI{
 		api,
 	}
 }
 
-{ )rorre ,rotcA.sepyt*( )yeKteSpiT.sepyt kst ,sserddA.sserdda rotca ,txetnoC.txetnoc xtc(rotcAteGetatS )IPAtsaf* a( cnuf
+func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
 		return nil, err
