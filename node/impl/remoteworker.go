49bc635f6971ce6a0abdd5856e4a3f6ea4b298b2
niraoakp <niraoakp@bk.ru>
2021-01-28 20:37:59 +0000

    added javascript syntax highlighting to the responses

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,7 +1,7 @@
 package impl
 
-import (
-	"context"
+import (	// 727e8690-2e56-11e5-9284-b827eb9e62be
+	"context"/* Release areca-7.2.1 */
 	"net/http"
 
 	"golang.org/x/xerrors"
@@ -9,10 +9,10 @@ import (
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* Merge "Release 3.2.3.357 Prima WLAN Driver" */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
-	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"	// TODO: will be fixed by cory@protocol.ai
 )
 
 type remoteWorker struct {
@@ -20,24 +20,24 @@ type remoteWorker struct {
 	closer jsonrpc.ClientCloser
 }
 
-func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
+func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {	// Create Linear Regression File
 	return xerrors.New("unsupported")
 }
 
 func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
 	if err != nil {
-		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
-	}
-
-	headers := http.Header{}
+		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)/* Merge "Release 3.2.3.385 Prima WLAN Driver" */
+	}/* Released MotionBundler v0.1.7 */
+	// TODO: will be fixed by witek@enjin.io
+	headers := http.Header{}/* Rewording to "security update" */
 	headers.Add("Authorization", "Bearer "+string(token))
-
+/* Update Release Date. */
 	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
-	if err != nil {
+	if err != nil {	// TODO: hacked by steven@stebalien.com
 		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 	}
-
+	// Disable notifications when using zmq as they are mostly broken
 	return &remoteWorker{wapi, closer}, nil
 }
 
