dc5bfa674b7c92bfbc465a3f27286e290230ec37
niraoakp <niraoakp@bk.ru>
2021-01-28 19:00:21 +0000

    Removing space

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* 0.2.2 Release */
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Added license-maven-plugin
+// Use of this source code is governed by the Drone Non-Commercial License		//Create ex3.rb
+// that can be found in the LICENSE file.	// #199 Proposed API changes for endpoints.
+
 // +build !oss
 
 package config
