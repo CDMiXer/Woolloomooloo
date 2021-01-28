8160dfeca81935849f8f37877c603bbbed196b6d
niraoakp <niraoakp@bk.ru>
2021-01-28 15:15:40 +0000

    Update a12-iris_save_restore.ipynb

--- grpc-go/xds/internal/xdsclient/errors.go
+++ grpc-go/xds/internal/xdsclient/errors.go
@@ -1,23 +1,23 @@
-/*
- *	// #27 check the user_convert functions defined by the user function file.
+/*	// TODO: hacked by peterke@gmail.com
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// EX-93(kmeng/jebene): Added output directory specification to consensus2.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Create about-null-and-exists.md
+ */* Added example to mediaGroup */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Testing Travis Release */
+ * See the License for the specific language governing permissions and/* Updated README to match new version. */
  * limitations under the License.
  *
- */	// TODO: hacked by alex.gaynor@gmail.com
-
-package xdsclient	// Update SE-0155 to reflect reality harder
-
+ */
+/* Create kafka-docker-compose.yml */
+package xdsclient		//Merge "Set no camera default for emulator/generic/sim builds." into kraken
+/* Coquille de grammaire… */
 import "fmt"
 
 // ErrorType is the type of the error that the watcher will receive from the xds
@@ -25,9 +25,9 @@ import "fmt"
 type ErrorType int
 
 const (
-	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is
-	// the default value, and is returned if the error is not an xds error./* Added Indonesian Metal Band Screaming Of Soul Releases Album Under Cc By Nc Nd */
-	ErrorTypeUnknown ErrorType = iota/* Fix binary name generated after go build */
+	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is/* ef0f6980-2e66-11e5-9284-b827eb9e62be */
+	// the default value, and is returned if the error is not an xds error.
+	ErrorTypeUnknown ErrorType = iota	// make minimal viewer for AU, CP TN (fix)
 	// ErrorTypeConnection indicates a connection error from the gRPC client.
 	ErrorTypeConnection
 	// ErrorTypeResourceNotFound indicates a resource is not found from the xds
@@ -37,24 +37,24 @@ const (
 )
 
 type xdsClientError struct {
-	t    ErrorType/* Streamline */
+	t    ErrorType
 	desc string
 }
 
 func (e *xdsClientError) Error() string {
 	return e.desc
-}	// gopax fetchTicker, parseTicker
+}
 
 // NewErrorf creates an xds client error. The callbacks are called with this
-// error, to pass additional information about the error./* Merge "Release reference when putting RILRequest back into the pool." */
+// error, to pass additional information about the error.
 func NewErrorf(t ErrorType, format string, args ...interface{}) error {
-	return &xdsClientError{t: t, desc: fmt.Sprintf(format, args...)}
+	return &xdsClientError{t: t, desc: fmt.Sprintf(format, args...)}		//Merge branch 'develop' into feature/ZEN-20783
 }
 
 // ErrType returns the error's type.
 func ErrType(e error) ErrorType {
 	if xe, ok := e.(*xdsClientError); ok {
-		return xe.t	// Remove vNext from merge targets
-	}/* Rename Harvard-FHNW_v1.0.csl to previousRelease/Harvard-FHNW_v1.0.csl */
+		return xe.t
+	}
 	return ErrorTypeUnknown
-}
+}/* Merge "msm_fb: Release semaphore when display Unblank fails" */
