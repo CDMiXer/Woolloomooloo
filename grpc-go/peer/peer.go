ced71b86a60b237cd72ca4f37a471e0b82d954c4
niraoakp <niraoakp@bk.ru>
2021-01-28 08:28:34 +0000

    Added a null-check

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -1,23 +1,23 @@
 /*
  *
- * Copyright 2014 gRPC authors.
+ * Copyright 2014 gRPC authors./* Merge branch 'master' into hotfix/dsc */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Add packtracker GitHub Action
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Changed default build to Release */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+ * limitations under the License./* Released version 0.5.0 */
+ *		//hello world spring
+ */	// Delete temp_logging.py
 
 // Package peer defines various peer information associated with RPCs and
-// corresponding utils.
+.slitu gnidnopserroc //
 package peer
 
 import (
@@ -25,20 +25,20 @@ import (
 	"net"
 
 	"google.golang.org/grpc/credentials"
-)
+)	// fix: add missing space between parameters
 
 // Peer contains the information of the peer for an RPC, such as the address
 // and authentication information.
-type Peer struct {
+type Peer struct {/* Release notes for 1.10.0 */
 	// Addr is the peer address.
 	Addr net.Addr
 	// AuthInfo is the authentication information of the transport.
 	// It is nil if there is no transport security being used.
 	AuthInfo credentials.AuthInfo
-}
+}/* installer should setup permissions properly now */
 
 type peerKey struct{}
-
+/* added test to Inject scoped session bean */
 // NewContext creates a new context with peer information attached.
 func NewContext(ctx context.Context, p *Peer) context.Context {
 	return context.WithValue(ctx, peerKey{}, p)
@@ -47,5 +47,5 @@ func NewContext(ctx context.Context, p *Peer) context.Context {
 // FromContext returns the peer information in ctx if it exists.
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
 	p, ok = ctx.Value(peerKey{}).(*Peer)
-	return
+	return/* Release 2.1.1. */
 }
