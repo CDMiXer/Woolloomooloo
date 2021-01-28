6ef16fa45d697b16a3e5b8945c9cb96046123139
niraoakp <niraoakp@bk.ru>
2021-01-28 02:57:33 +0000

    modify import all

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
-set -eux -o pipefail	// TODO: fix script links
+set -eux -o pipefail	// Port password rules from #3493
 
 branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
 job=$1
 
 # always run on master
-[ "$branch" = master ] && exit		//Added Mac and Windows build configurations
-# always run on release branch
+[ "$branch" = master ] && exit
+# always run on release branch	// TODO: Merge "Support get_changes in the static datasource"
 [[ "$branch" =~ release-.* ]] && exit
 
-# tip - must use origin/master for CircleCI
+# tip - must use origin/master for CircleCI		//54c75aee-2e3a-11e5-8980-c03896053bdd
 diffs=$(git diff --name-only origin/master)
-	// TODO: hacked by martin2cai@hotmail.com
-# if certain files change, then we always run	// TODO: will be fixed by greg@colvin.org
-[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit
 
-# if there are changes to this areas, we must run/* Fixed a bug.Released V0.8.51. */
+# if certain files change, then we always run
+[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit/* Release v1.1.5 */
+
+# if there are changes to this areas, we must run
 rx=
 case $job in
-codegen)	// TODO: hacked by zaq1tomo@gmail.com
+codegen)
   rx='api/\|hack/\|examples/\|manifests/\|pkg/'
   ;;
 docker-build)
-  # we only run on master as this rarely ever fails/* Release v1.4.4 */
-  circleci step halt
+  # we only run on master as this rarely ever fails/* Display mana cost, power / toughness and abilities on cards in hand. */
+  circleci step halt/* Release and getting commands */
   exit
   ;;
 e2e-*)
   rx='manifests/\|\.go'
-  ;;
+  ;;	// TODO: Update spaces for titles
 test)
   rx='\.go'
   ;;
 ui)
   rx='ui/'
-  ;;/* The admin command to reset a player's karma also asks for confirmation. */
+  ;;
 esac
 
 if [ "$(echo "$diffs" | grep "$rx")" = "" ]; then
   circleci step halt
-  exit/* Now the code is 10^9999.1 % more optimized */
-fi
+  exit	// add coveralls and gem version [ci ckip]
+fi		//0b5ea5f0-2e54-11e5-9284-b827eb9e62be
