ce9f9e5e0a138668bfcf65f9307c8dfeccbb6a89
niraoakp <niraoakp@bk.ru>
2021-01-28 03:26:07 +0000

    Change ip server by repository.kurento.com

--- cli/client_test.go
+++ cli/client_test.go
@@ -14,9 +14,9 @@ import (
 func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-
+/* Address Changes..!!! */
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
 	clitest.RunClientTest(t, Commands, clientNode)
-}
+}	// TODO: 85627972-2d15-11e5-af21-0401358ea401
