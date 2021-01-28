f2098c5901e053e588438ecdabfb2c0c7e4b843e
niraoakp <niraoakp@bk.ru>
2021-01-28 07:33:57 +0000

    doc: add badge

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -12,26 +12,26 @@ import (
 )
 
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))/* Merge "Wlan: Release 3.8.20.5" */
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
-		if err != nil {
+		if err != nil {		//Merge branch 'InDev'
 			return storagemarket.StorageProviderInfo{}
 		}
 		multiaddrs = append(multiaddrs, maddr)
-	}
-
-	return storagemarket.StorageProviderInfo{
+	}/* tried fixing */
+	// TODO: hacked by sebs@2xs.org
+	return storagemarket.StorageProviderInfo{/* Fixed some issues with away message encoding. */
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
-		Addrs:      multiaddrs,
+		Addrs:      multiaddrs,/* Added goals for Release 2 */
 	}
 }
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
-	return storagemarket.Balance{
+	return storagemarket.Balance{/* Release: 5.0.3 changelog */
 		Locked:    bal.Locked,
 		Available: big.Sub(bal.Escrow, bal.Locked),
 	}
