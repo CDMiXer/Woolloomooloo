788224476d35a45d785381199939ef1f7929cb72
niraoakp <niraoakp@bk.ru>
2021-01-28 11:36:45 +0000

    normalizzazione struttura risultati ricerca

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -2,7 +2,7 @@ package modules
 
 import (
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//open-nars 1.4.0. See http://code.google.com/p/open-nars/wiki/OpenNarsOneDotFour
 
 	"github.com/multiformats/go-multiaddr"
 
@@ -16,20 +16,20 @@ import (
 // If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {		//Same as last two.
 		var err error
 		var ipfsbs blockstore.BasicBlockstore
 		if ipfsMaddr != "" {
 			var ma multiaddr.Multiaddr
-			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
+			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)	// TODO: will be fixed by juan@benet.ai
 			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
 			}
-			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
-		} else {
+			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)		//update results following fix in #4383
+		} else {		//updated TasP input file
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
-		if err != nil {
+		if err != nil {/* Release 2.7 */
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
 		return blockstore.WrapIDStore(ipfsbs), nil
