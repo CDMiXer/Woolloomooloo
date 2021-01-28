0b20b27470541f8edfc70fdb0acd5cf96d2a7edb
niraoakp <niraoakp@bk.ru>
2021-01-28 03:02:07 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-1-0-1'.

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -6,10 +6,10 @@ cat \
     | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \	// TODO: Update readme, sample repository size is 500GB
+    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \/* Update homework2.rb */
+    | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
     | sed 's/k8s.io./io.k8s./'
