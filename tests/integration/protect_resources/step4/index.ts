c06b091975921d44c5b8e617f3d7d2607ec28fa1
niraoakp <niraoakp@bk.ru>
2021-01-28 01:31:48 +0000

    a9126184-2e3f-11e5-9284-b827eb9e62be

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Rename Addplugins to Addplugins.lua
 import { Resource } from "./resource";
 
 // Next, just unprotect the resource:
