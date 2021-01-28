4a84cc68379f1c088a0678838b8f8bad2b399066
niraoakp <niraoakp@bk.ru>
2021-01-28 20:57:19 +0000

    Update iq_abs.lua

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release scripts. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -15,39 +15,39 @@ import (
 
 	"github.com/go-chi/chi"
 )
-
+/* Added Release Notes link to README.md */
 // HandleList returns an http.HandlerFunc that write a json-encoded
 // list of repository collaborators to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {
+) http.HandlerFunc {/* Awn-Terminal: Remove the Awn prefix from the desktop file */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// Create startClusterInstance.groovy
 		)
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)
+		if err != nil {		//add contact section to read me 
+			render.NotFound(w, err)	// TODO: hacked by igor@soramitsu.co.jp
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* Add .localdomain hostname */
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
-			return
+			return	// Delete he5.lua
 		}
 		members, err := members.List(r.Context(), repo.UID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).		//Thermostat stuff now working. Needs tidying.
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Warnln("api: cannot get member list")
 		} else {
-			render.JSON(w, members, 200)/* Added examples for 'region' and 'regionPrios' */
+			render.JSON(w, members, 200)
 		}
 	}
 }
