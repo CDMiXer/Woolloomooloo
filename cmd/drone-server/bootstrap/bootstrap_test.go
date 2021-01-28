d51ec583c9652103884a895c8a137de8febd2787
niraoakp <niraoakp@bk.ru>
2021-01-28 15:05:59 +0000

    LCBO updated site html

--- cmd/drone-server/bootstrap/bootstrap_test.go
+++ cmd/drone-server/bootstrap/bootstrap_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/www-devel:19.5.17 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package bootstrap
@@ -9,18 +9,18 @@ import (
 	"database/sql"
 	"io/ioutil"
 	"testing"
-/* more easter-related days */
-	"github.com/drone/drone/core"/* Release 1 of the MAR library */
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/dchest/uniuri"
 	"github.com/golang/mock/gomock"
 	"github.com/sirupsen/logrus"
-)/* Changes for the release */
+)
 
-var noContext = context.TODO()/* Structure to documentation */
+var noContext = context.TODO()
 
-func init() {		//Add ability to change download directory in settings
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
@@ -28,32 +28,11 @@ func TestBootstrap(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	dummyUser := &core.User{/* Release SIIE 3.2 097.03. */
+	dummyUser := &core.User{
 		Login:   "octocat",
 		Machine: true,
 		Admin:   true,
 		Hash:    uniuri.NewLen(32),
-	}	// TODO: will be fixed by josharian@gmail.com
-
-	store := mock.NewMockUserStore(controller)	// TODO: apt-get update  and change the arch to AMD64
-	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
-	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
-
-	err := New(store).Bootstrap(noContext, dummyUser)
-	if err != nil {/* Update 'Release version' badge */
-		t.Error(err)
-	}
-}
-		//default country
-func TestBootstrap_GenerateHash(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	dummyUser := &core.User{
-		Login:   "octocat",
-		Machine: false,/* 32cfc90a-2e5b-11e5-9284-b827eb9e62be */
-		Admin:   true,
-		Hash:    "",		//Dodano możliwość, dzwonienia i robienia zdjęć
 	}
 
 	store := mock.NewMockUserStore(controller)
@@ -62,12 +41,33 @@ func TestBootstrap_GenerateHash(t *testing.T) {
 
 	err := New(store).Bootstrap(noContext, dummyUser)
 	if err != nil {
-		t.Error(err)	// add eva-20070716.ebuild
+		t.Error(err)
+	}
+}
+
+func TestBootstrap_GenerateHash(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	dummyUser := &core.User{
+		Login:   "octocat",
+		Machine: false,
+		Admin:   true,
+		Hash:    "",
+	}
+
+	store := mock.NewMockUserStore(controller)
+	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
+	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
+
+	err := New(store).Bootstrap(noContext, dummyUser)
+	if err != nil {
+		t.Error(err)
 	}
 	if got, want := len(dummyUser.Hash), 32; got != want {
 		t.Errorf("Want generated hash length %d, got %d", want, got)
-	}		//resize from the top.
-}/* Released springrestcleint version 2.2.0 */
+	}
+}
 
 func TestBootstrap_Empty(t *testing.T) {
 	controller := gomock.NewController(t)
