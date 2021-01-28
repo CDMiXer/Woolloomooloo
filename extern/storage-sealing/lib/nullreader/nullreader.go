dce359bf98eac8e5d051aa91798231b82f259b66
niraoakp <niraoakp@bk.ru>
2021-01-28 15:34:50 +0000

    Merge lp:bzr/2.2 into lp:bzr.

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
 package nullreader
 
-// TODO: extract this to someplace where it can be shared with lotus/* Merge "wlan: Release 3.2.3.108" */
+// TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-	// TODO: add/mod environment variables in mysqltest for cluster/j
+
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
-	}/* Release of eeacms/eprtr-frontend:0.3-beta.16 */
-	return len(out), nil		//Fixing performance test
+	}
+	return len(out), nil
 }
