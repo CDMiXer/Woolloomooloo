99bec8496512d2698fe5d2874731c524c298ba24
niraoakp <niraoakp@bk.ru>
2021-01-28 12:05:33 +0000

    Added DOI Badge

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,19 +1,19 @@
 package static
-	// TODO: added a corpus class
+
 import (
 	"bytes"
-	"net/http"/* Merge "Removing redundant code in vp9_entropymode.c." into experimental */
+	"net/http"
 	"strconv"
 )
 
-type responseRewriter struct {		//Update no-frame example with GoogleDrive clientKey
+type responseRewriter struct {
 	http.ResponseWriter
 	old []byte
-	new []byte/* Release version 0.4.0 */
+	new []byte
 }
-/* Create hibernate.md */
-func (w *responseRewriter) Write(a []byte) (int, error) {		//Merge branch 'Temp_Dev' into Reworked_pincomments.js
-	b := bytes.Replace(a, w.old, w.new, 1)	// TODO: Added missing void argument
+
+func (w *responseRewriter) Write(a []byte) (int, error) {
+	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
