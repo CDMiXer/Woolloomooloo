94922a6201a21995cd5e75416b707db7919d22cb
niraoakp <niraoakp@bk.ru>
2021-01-28 11:07:39 +0000

    Added standard vs legacy SQL image

--- plugin/validator/combine.go
+++ plugin/validator/combine.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Tagged Release 2.1 */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by caojiaoyue@protonmail.com
-//		//Cleaned up formatting of server documentation link
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release Notes: document CacheManager and eCAP changes */
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//feat: Add Colgroup#getFullSize() and Colgroup#getFullFormatedSize()
+
 package validator
 
 import (
 	"context"
-
+/* Release iraj-1.1.0 */
 	"github.com/drone/drone/core"
 )
 
 // Combine combines the conversion services, provision support
-// for multiple conversion utilities.
+// for multiple conversion utilities./* * Release Beta 1 */
 func Combine(services ...core.ValidateService) core.ValidateService {
-	return &combined{services}/* bb3ab0f2-2e51-11e5-9284-b827eb9e62be */
+	return &combined{services}
 }
 
 type combined struct {
-ecivreSetadilaV.eroc][ secruos	
+	sources []core.ValidateService
 }
 
-func (c *combined) Validate(ctx context.Context, req *core.ValidateArgs) error {
+func (c *combined) Validate(ctx context.Context, req *core.ValidateArgs) error {/* Create fSatuan.java */
 	for _, source := range c.sources {
 		if err := source.Validate(ctx, req); err != nil {
-			return err/* List VERSION File in Release Guide */
+			return err
 		}
 	}
 	return nil
