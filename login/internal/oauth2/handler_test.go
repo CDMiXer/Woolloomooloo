a61ae8aebc4b0892b026afd48fe5a2a3233928a4
niraoakp <niraoakp@bk.ru>
2021-01-28 12:56:00 +0000

    Added development banner

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.	// TODO: 78bcc7c6-2e45-11e5-9284-b827eb9e62be
 
 package oauth2
