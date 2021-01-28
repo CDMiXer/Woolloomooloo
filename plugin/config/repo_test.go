0f6b49c87fe5f518d5d844ed617de482870bd303
niraoakp <niraoakp@bk.ru>
2021-01-28 02:26:14 +0000

    Debug type

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// 0154d082-2e6f-11e5-9284-b827eb9e62be
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,8 +8,8 @@ import (
 	"context"
 	"errors"
 	"testing"
-
-	"github.com/drone/drone/core"
+/* 298a969a-2e47-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/core"		//QUASAR: Fix memory leak from not disposing Persistent suspect Handles
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
@@ -17,31 +17,31 @@ import (
 
 var noContext = context.TODO()
 
-var mockFile = []byte(`
-kind: pipeline
+var mockFile = []byte(`	// TODO: hacked by arachnid@notdot.net
+kind: pipeline	// Unified code base a bit
 name: default
 
 steps: []
-`)
-
+`)		//update travis config for Ruby 2.0, 2.1.1 and 2.1.2
+		//Update and rename delete_this_file.txt to about_this_folder.txt
 func TestRepository(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{
+	args := &core.ConfigArgs{	// TODO: hacked by mowrain@yandex.com
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
-	}
-
+	}/* Delete naderr.m */
+/* Delete tencent3.md */
 	resp := &core.File{Data: mockFile}
 
 	files := mock.NewMockFileService(controller)
-	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
+	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)		//+ Added export support
 
 	service := Repository(files)
-	result, err := service.Find(noContext, args)
+	result, err := service.Find(noContext, args)	// TODO: will be fixed by earlephilhower@yahoo.com
 	if err != nil {
 		t.Error(err)
 	}
@@ -54,18 +54,18 @@ func TestRepository(t *testing.T) {
 func TestRepositoryErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* - fix DDrawSurface_Release for now + more minor fixes */
 	args := &core.ConfigArgs{
-		User:   &core.User{Login: "octocat"},
+		User:   &core.User{Login: "octocat"},/* Parse data values with comma. Better format output */
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
 	}
-
+		//Deprecate grammar override API on GrammarRegistry
 	resp := errors.New("")
 
 	files := mock.NewMockFileService(controller)
-	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)
+	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)	// Merge "tools: update sca and cpi requirements file"
 
 	service := Repository(files)
 	_, err := service.Find(noContext, args)
