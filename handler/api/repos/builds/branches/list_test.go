187f3c30f3fc21579600b2619d25fee94f878248
niraoakp <niraoakp@bk.ru>
2021-01-28 14:16:04 +0000

    init read_count in QueryLogPlugin constructor

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//Fix door offset
+// that can be found in the LICENSE file.		//Changes for ordering and pagination
 
 package branches
