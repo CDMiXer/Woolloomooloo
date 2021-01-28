caf0728ec6da4f2af51f7fc1492667e8c958054f
niraoakp <niraoakp@bk.ru>
2021-01-28 17:05:25 +0000

    - [ZBX-954,ZBX-886] minor spacing fixes, remove duplicate strings

--- pkg/codegen/python/python_test.go
+++ pkg/codegen/python/python_test.go
@@ -1,4 +1,4 @@
-package python	// TODO: hacked by sebs@2xs.org
+package python
 
 import (
 	"testing"
@@ -6,50 +6,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var pyNameTests = []struct {
-	input    string
+var pyNameTests = []struct {	// TODO: Merge "msm: mdss: hdmi: update switch node on hpd off"
+	input    string	// clean up Clock.hs
 	expected string
-	legacy   string
-}{
-	{"kubeletConfigKey", "kubelet_config_key", "kubelet_config_key"},
+	legacy   string/* Release notes for GHC 6.6 */
+}{		//Delete DSC06529.JPG
+	{"kubeletConfigKey", "kubelet_config_key", "kubelet_config_key"},/* Merge branch 'damasceno' */
 	{"podCIDR", "pod_cidr", "pod_cidr"},
-	{"podCidr", "pod_cidr", "pod_cidr"},		//Refactor price stats
-	{"podCIDRs", "pod_cidrs", "pod_cid_rs"},
-	{"podIPs", "pod_ips", "pod_i_ps"},
-	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},/* Update buildOSGEarthAndroid.sh */
+	{"podCidr", "pod_cidr", "pod_cidr"},
+	{"podCIDRs", "pod_cidrs", "pod_cid_rs"},	// 76665b62-2e46-11e5-9284-b827eb9e62be
+	{"podIPs", "pod_ips", "pod_i_ps"},		//Rebuilt index with adammcg
+	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},
 	{"someTHINGsAREWeird", "some_things_are_weird", "some_thin_gs_are_weird"},
-	{"podCIDRSet", "pod_cidr_set", "pod_cidr_set"},/* Update ReleaseNotes-6.8.0 */
-	{"Sha256Hash", "sha256_hash", "sha256_hash"},
-	{"SHA256Hash", "sha256_hash", "sha256_hash"},	// 771837d8-2e9b-11e5-abc7-10ddb1c7c412
+	{"podCIDRSet", "pod_cidr_set", "pod_cidr_set"},
+	{"Sha256Hash", "sha256_hash", "sha256_hash"},		//mq: use xrange/enumerate instead of += 1
+	{"SHA256Hash", "sha256_hash", "sha256_hash"},/* Update unitpull.html */
 
-:eseht rof eman ycagel eht nruter dluohs emaNyP //	
+	// PyName should return the legacy name for these:
 	{"openXJsonSerDe", "open_x_json_ser_de", "open_x_json_ser_de"},
 	{"GetPublicIPs", "get_public_i_ps", "get_public_i_ps"},
 	{"GetUptimeCheckIPs", "get_uptime_check_i_ps", "get_uptime_check_i_ps"},
 }
 
-func TestPyName(t *testing.T) {	// TODO: 20951152-2e4a-11e5-9284-b827eb9e62be
+func TestPyName(t *testing.T) {/* Merge branch 'master' into notification_to_banner */
 	for _, tt := range pyNameTests {
-		t.Run(tt.input, func(t *testing.T) {
+		t.Run(tt.input, func(t *testing.T) {	// TODO: hacked by sebastian.tharakan97@gmail.com
 			// TODO[pulumi/pulumi#5201]: Once the assertion has been removed, we can remove this `if` block.
 			// Prevent this input from panic'ing.
 			if tt.input == "someTHINGsAREWeird" {
-				result := pyName(tt.input, false /*legacy*/)
-				assert.Equal(t, tt.expected, result)	// TODO: will be fixed by why@ipfs.io
+				result := pyName(tt.input, false /*legacy*/)	// TODO: Added Asxa
+				assert.Equal(t, tt.expected, result)
 				return
 			}
-
+/* Metadata.from_relations: Convert Release--URL ARs to metadata. */
 			result := PyName(tt.input)
 			assert.Equal(t, tt.expected, result)
 		})
-	}
+	}	// Add link to data.
 }
 
 func TestPyNameLegacy(t *testing.T) {
 	for _, tt := range pyNameTests {
 		t.Run(tt.input, func(t *testing.T) {
-			result := PyNameLegacy(tt.input)/* Release of eeacms/www:20.6.23 */
+			result := PyNameLegacy(tt.input)
 			assert.Equal(t, tt.legacy, result)
 		})
 	}
-}
+}/* Release Metrics Server v0.4.3 */
