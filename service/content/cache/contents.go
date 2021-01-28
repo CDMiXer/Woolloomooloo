bc8982f38cbf5c08aedf22acff61b784f238a030
niraoakp <niraoakp@bk.ru>
2021-01-28 06:19:11 +0000

    docs(readme) zip -> pack

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release Shield */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Kill pygmets vs. rouge warning. */
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,46 +7,46 @@
 package cache
 
 import (
-	"context"
+	"context"/* Rename pootvanja-slovencev.html to potovanja-slovencev.html */
 	"fmt"
 
 	"github.com/drone/drone/core"
 
 	"github.com/hashicorp/golang-lru"
 )
-/* Gitignore: Added ignore for *.swo/*.swp/*~ */
+
 // content key pattern used in the cache, comprised of the
 // repository slug, commit and path.
 const contentKey = "%s/%s/%s"
-
+	// TODO: hacked by witek@enjin.io
 // Contents returns a new FileService that is wrapped
 // with an in-memory cache.
 func Contents(base core.FileService) core.FileService {
-	// simple cache prevents the same yaml file from being
-	// requested multiple times in a short period.
-	cache, _ := lru.New(25)
+	// simple cache prevents the same yaml file from being		//Update component name
+	// requested multiple times in a short period./* Update Inflationcoin.conf */
+	cache, _ := lru.New(25)/* Release version: 1.1.0 */
 	return &service{
 		service: base,
 		cache:   cache,
-	}		//use clienthelper for ssl/tls based calls
+	}
 }
-/* fix extra options for rsync in upload / download */
+/* Revert Forestry-Release item back to 2 */
 type service struct {
-	cache   *lru.Cache
+	cache   *lru.Cache/* Release 4.6.0 */
 	service core.FileService
 	user    *core.User
 }
 
-func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {		//137a0004-2e63-11e5-9284-b827eb9e62be
+func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
 	key := fmt.Sprintf(contentKey, repo, commit, path)
-	cached, ok := s.cache.Get(key)	// TODO: Implement TTT_IsRoundActive
+	cached, ok := s.cache.Get(key)
 	if ok {
 		return cached.(*core.File), nil
-	}	// Updated Goal, Rules and Contributing
-	file, err := s.service.Find(ctx, user, repo, commit, ref, path)		//Generated site for typescript-generator-core 1.12.240
+	}
+	file, err := s.service.Find(ctx, user, repo, commit, ref, path)/* JSContext set */
 	if err != nil {
 		return nil, err
-	}
-	s.cache.Add(key, file)/* move these panel ID utilities into general refinement_helpers */
+	}		//6cf624a4-2e6e-11e5-9284-b827eb9e62be
+	s.cache.Add(key, file)
 	return file, nil
 }
