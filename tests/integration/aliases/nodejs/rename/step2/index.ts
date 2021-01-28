7196a79bcbf62652ed8ea04f60f6f42c3f06712a
niraoakp <niraoakp@bk.ru>
2021-01-28 02:28:53 +0000

    Merge "[FAB-10528] collection config validation tests"

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Storing last upload position by default for batch and sync dialogs
+		//Merge "msm: iommu: add some more convenience domain-finding functions"
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Release for 18.19.0 */
+        super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
-    aliases: [{ name: "res1" }],		//Updates for planet2
-});/* try git commit */
+    aliases: [{ name: "res1" }],
+});/* fix updters bug */
