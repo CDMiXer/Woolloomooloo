cc541b24b91535e0f9c2d7e1471ed6a707f906e7
niraoakp <niraoakp@bk.ru>
2021-01-28 16:45:36 +0000

    Added markFinalized() to ObjectFinalizer.

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc./* Release 0.8.4 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by why@ipfs.io
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,17 +19,17 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-)		//[MISC] made product selection more native to GitHub UI
-	// Create Reverseword.java
+)
+
 // New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
 	return &service{
-		client: client,	// TODO: hacked by ac0dem0nk3y@gmail.com
+		client: client,
 	}
-}/* 9eafb838-2e51-11e5-9284-b827eb9e62be */
+}
 
-type service struct {	// TODO: will be fixed by magik6k@gmail.com
-	client *scm.Client/* Neteja del changelog. */
+type service struct {
+	client *scm.Client
 }
 
 func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
