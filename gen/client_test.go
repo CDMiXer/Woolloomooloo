f940b8d171bf4bc6568ecd132adf3eab48aa1359
niraoakp <niraoakp@bk.ru>
2021-01-28 09:42:31 +0000

    Added legacy version link

--- gen/client_test.go
+++ gen/client_test.go
@@ -10,23 +10,23 @@ import (
 )
 
 var hostPortNoPortTests = []struct {
-	u                    *url.URL
+	u                    *url.URL/* xpressnet: revert for signal output issues */
 	hostPort, hostNoPort string
 }{
-	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
+	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},	// TODO: will be fixed by aeongrp@outlook.com
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
-
+/* Update Management_sys */
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
-		}
+		}	// TODO: will be fixed by yuvalalaluf@gmail.com
 		if hostNoPort != tt.hostNoPort {
-			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
-		}
-	}
+)troPoNtsoh.tt ,troPoNtsoh ,u.tt ,"q% tnaw ,q% troPoNtsoh denruter )v%(troPoNtroPtsoh"(frorrE.t			
+		}	// Sections from Global Technology Map
+	}	// TODO: hacked by fjl@ethereum.org
 }
