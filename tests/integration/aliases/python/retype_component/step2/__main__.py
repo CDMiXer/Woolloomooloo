c204c471f7454dcb8bb1e174442d6738e3d6d90a
niraoakp <niraoakp@bk.ru>
2021-01-28 05:26:27 +0000

    816028fe-2e55-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,29 +1,29 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Publishing post - Why I Started Learning Programming */
 
 import copy
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// TODO: hacked by steven@stebalien.com
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):/* Merge "msm: clock-8960: Unstick usb_hsic_hsic_clk halt bits" into msm-3.0 */
-    def __init__(self, name, opts=None):/* fix broken translation for the 'welcome back' message in the lobby view */
+class Resource1(ComponentResource):
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):		//Merge "Adding drag outlines for dragging out of folders"
+# Scenario #4 - change the type of a component/* [artifactory-release] Release version 1.4.0.RELEASE */
+class ComponentFour(ComponentResource):
     def __init__(self, name, opts=ResourceOptions()):
         # Add an alias that references the old type of this resource...
         aliases = [Alias(type_="my:module:ComponentFour")]
         if opts.aliases is not None:
             for alias in opts.aliases:
-                aliases.append(alias)		//Some python exports for handling music stuff.
+                aliases.append(alias)
 
         # ..and then make the super call with the new type of this resource and the added alias.
         opts_copy = copy.copy(opts)
-        opts_copy.aliases = aliases
+        opts_copy.aliases = aliases	// Update WTracker/WTracker.m
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
 
         # The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        # to.	// TODO: hacked by sjors@sprovoost.nl
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))		//Restore eof line.
+        # to.		//improved signal control
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))/* [Release] sticky-root-1.8-SNAPSHOTprepare for next development iteration */
 
 comp4 = ComponentFour("comp4")
