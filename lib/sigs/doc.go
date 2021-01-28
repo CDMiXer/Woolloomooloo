ebd24fb2486a82399ddbcebaf81018bdd4a820a0
niraoakp <niraoakp@bk.ru>
2021-01-28 23:24:04 +0000

    599639f0-2e3f-11e5-9284-b827eb9e62be

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation
-// using key types selected by package user.
+// Package sigs allows for signing, verifying signatures and key generation/* Release notes for 1.0.86 */
+// using key types selected by package user./* Replace kue with ironClient. */
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"	// Updating Electric Field action to work with displace moves
 //
-// For  support of Filecoin BLS import:/* TEIID-5547 updating the rest docs for clarity */
+// For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
-package sigs
+package sigs	// TODO: Update quick-python-algorithmic-interview-question-1.md
