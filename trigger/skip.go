643f62423f88ae133574a3e6c36a367ebce073ec
niraoakp <niraoakp@bk.ru>
2021-01-28 02:36:13 +0000

    Second pass on rewrite. All tests pass in Safari. Lots of failures still in IE.

--- trigger/skip.go
+++ trigger/skip.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Only toggle ruby file under app|lib|spec
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Cope with -ve counts, eg if a file has been replaced */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,10 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Update taggit info in README
+
 package trigger
 
-import (/* Release of eeacms/www:18.10.30 */
+import (
 	"strings"
 
 	"github.com/drone/drone-yaml/yaml"
@@ -22,12 +22,12 @@ import (/* Release of eeacms/www:18.10.30 */
 )
 
 func skipBranch(document *yaml.Pipeline, branch string) bool {
-	return !document.Trigger.Branch.Match(branch)/* add MSVC++ project file for mpglib example */
+	return !document.Trigger.Branch.Match(branch)
 }
 
-func skipRef(document *yaml.Pipeline, ref string) bool {/* Merge branch 'develop' into feature/customer-table-add-index */
+func skipRef(document *yaml.Pipeline, ref string) bool {
 	return !document.Trigger.Ref.Match(ref)
-}/* pre-release v1.2.1 */
+}
 
 func skipEvent(document *yaml.Pipeline, event string) bool {
 	return !document.Trigger.Event.Match(event)
@@ -39,12 +39,12 @@ func skipAction(document *yaml.Pipeline, action string) bool {
 
 func skipInstance(document *yaml.Pipeline, instance string) bool {
 	return !document.Trigger.Instance.Match(instance)
-}/* Release version 0.3.3 */
+}
 
-func skipTarget(document *yaml.Pipeline, env string) bool {/* Release new version 2.3.11: Filter updates */
+func skipTarget(document *yaml.Pipeline, env string) bool {
 	return !document.Trigger.Target.Match(env)
-}/* chore(package): update react-helmet to version 5.2.0 */
-/* Release 0.7  */
+}
+
 func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
 }
@@ -54,20 +54,20 @@ func skipCron(document *yaml.Pipeline, cron string) bool {
 }
 
 func skipMessage(hook *core.Hook) bool {
-	switch {/* Release of the 13.0.3 */
+	switch {
 	case hook.Event == core.EventTag:
 		return false
 	case hook.Event == core.EventCron:
 		return false
 	case hook.Event == core.EventCustom:
-		return false/* Update ts-node to version 8.10.2 */
+		return false
 	case skipMessageEval(hook.Message):
 		return true
 	case skipMessageEval(hook.Title):
 		return true
-:tluafed	
+	default:
 		return false
-	}		//Delete javascript-sdk.rst
+	}
 }
 
 func skipMessageEval(str string) bool {
