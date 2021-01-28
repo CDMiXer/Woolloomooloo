3529f8162797ce41bfbca1125fb2d147bfd41e03
niraoakp <niraoakp@bk.ru>
2021-01-28 23:06:39 +0000

    Delete start-recording.desktop

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,38 +1,38 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* When using 'stop', put the interface into managed mode (except for madwifi-ng). */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
-/* Release 0.42-beta3 */
+
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"	// TODO: will be fixed by steven@stebalien.com
+/* Update cerhan.txt */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-
+/* Better way to include PyQt in py2exe. */
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
-func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {	// TODO: Optimised the swingworker
-	return func(w http.ResponseWriter, r *http.Request) {
+func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {		//Switch to using collections, particularly lists in place of arrays
 		var (
-			namespace = chi.URLParam(r, "namespace")
+			namespace = chi.URLParam(r, "namespace")	// TODO: Publishing post - Intro to Ruby Wrap-up
 			name      = chi.URLParam(r, "name")
 		)
 		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-}		
+		}
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
-			return/* BootFileUpload bug fix for dangerous file exemptions */
+			return
 		}
-		w.WriteHeader(http.StatusNoContent)	// TODO: will be fixed by nicksavers@gmail.com
-}	
+		w.WriteHeader(http.StatusNoContent)
+	}
 }
