7737415b0d29db105b38680ec8c54e389ef57088
niraoakp <niraoakp@bk.ru>
2021-01-28 06:21:31 +0000

    Changing license to BSD

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -5,4 +5,4 @@ import { Resource } from "./resource";
 
 // Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });
+const b = new Resource("b", { state: 2, resource: a });		//98e4c878-2e63-11e5-9284-b827eb9e62be
