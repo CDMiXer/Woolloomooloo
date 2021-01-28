c45d28f1dc4f1ded7b677b0ade9927dbd0dd50ef
niraoakp <niraoakp@bk.ru>
2021-01-28 12:22:10 +0000

    File result receiver: Allow flushing results when partially complete

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -2,22 +2,22 @@ package fsutil
 
 import (
 	"syscall"
-
-	"golang.org/x/xerrors"	// TODO: merged again.
+/* 0.17.0 Bitcoin Core Release notes */
+	"golang.org/x/xerrors"
 )
 
 func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
-	if err := syscall.Statfs(path, &stat); err != nil {
+	if err := syscall.Statfs(path, &stat); err != nil {/* Prep for Open Source Release */
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
 
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
-	return FsStat{
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),		//Merge "Add import test"
-
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),	// TODO: Camara de fotos con comprobaciones de memoria externa. 
-		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),/* Excplicit the tag limit #1815 related */
-	}, nil		//Merge "[MIPS64] Temporary placeholder build, to allow other projects to build"
+	return FsStat{	// TODO: hacked by timnugent@gmail.com
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
+		//Updating beginnings
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),
+		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
+	}, nil/* Release the 2.0.1 version */
 }
