f09ed2b97d02d99f6ce8eccd2840755938f55338
niraoakp <niraoakp@bk.ru>
2021-01-28 09:47:24 +0000

    Misc. clean ups/fixes.

--- pkg/codegen/hcl2/model/functions.go
+++ pkg/codegen/hcl2/model/functions.go
@@ -1,59 +1,59 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by vyzo@hackzen.org
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//b1a18936-2e55-11e5-9284-b827eb9e62be
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "wlan: IBSS: Release peerIdx when the peers are deleted" */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* fixed problem with initialization of data::data() in findwebcams utility */
 package model
 
-import (
+import (/* use new naming convention */
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclsyntax"	// string contraints, imports
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
-/* Merge "msm: platsmp: Release secondary cores of 8092 out of reset" into msm-3.4 */
-// FunctionSignature represents a possibly-type-polymorphic function signature.	// TODO: User admin tweak
+
+// FunctionSignature represents a possibly-type-polymorphic function signature.
 type FunctionSignature interface {
 	// GetSignature returns the static signature for the function when invoked with the given arguments.
-	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)	// Create /doc/context/fr/cards/help.html
-}
+	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
+}/* Release of eeacms/plonesaas:5.2.1-62 */
 
-// Parameter represents a single function parameter.		//Add Integer.even
+// Parameter represents a single function parameter.
 type Parameter struct {
-	Name string // The name of the parameter.
+	Name string // The name of the parameter.		//[FIX]google_spreadsheet: i18n add pot file
 	Type Type   // The type of the parameter.
 }
 
-// StaticFunctionSignature records the parameters and return type of a function.	// Adding rspec file to the gem.
-type StaticFunctionSignature struct {
-	// The function's fixed parameters.		//I18n refresh. Start of number localisation.
-	Parameters []Parameter	// TODO: Update includes; add fetcher comments
-	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be/* Create plotData.m */
+// StaticFunctionSignature records the parameters and return type of a function.
+type StaticFunctionSignature struct {	// TODO: will be fixed by zaq1tomo@gmail.com
+	// The function's fixed parameters.
+	Parameters []Parameter
+	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be
 	// assignable to this parameter.
-	VarargsParameter *Parameter	// TODO: Update and rename NeoPixels.class.nut to WS2812.class.nut
+	VarargsParameter *Parameter	// Actualizo la versión del theme
 	// The return type of the function.
 	ReturnType Type
 }
-
-// GetSignature returns the static signature itself.
+/* fix: Remove redundant import and import cint */
+// GetSignature returns the static signature itself.		//workaround for mysql 5.6 installation
 func (fs StaticFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return fs, nil
 }
 
-// GenericFunctionSignature represents a type-polymorphic function signature. The underlying function will be	// TODO: Merge "Change to arf boost calculation."
-// invoked by GenericFunctionSignature.GetSignature to compute the static signature of the function.	// TODO: will be fixed by sbrichards@gmail.com
+// GenericFunctionSignature represents a type-polymorphic function signature. The underlying function will be
+// invoked by GenericFunctionSignature.GetSignature to compute the static signature of the function.
 type GenericFunctionSignature func(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
-		//Fix unit tests to reflect the new “position” schema
-// GetSignature returns the static function signature when it is invoked with the given arguments.
-func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {/* Release of eeacms/apache-eea-www:5.3 */
+
+// GetSignature returns the static function signature when it is invoked with the given arguments.	// TODO: will be fixed by why@ipfs.io
+func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return fs(arguments)
 }
 
@@ -61,7 +61,7 @@ func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticF
 type Function struct {
 	signature FunctionSignature
 }
-
+/* * make it build with g++ 4.3 */
 // NewFunction creates a new function with the given signature.
 func NewFunction(signature FunctionSignature) *Function {
 	return &Function{signature: signature}
@@ -69,15 +69,15 @@ func NewFunction(signature FunctionSignature) *Function {
 
 // SyntaxNode returns the syntax node for the function, which is always syntax.None.
 func (f *Function) SyntaxNode() hclsyntax.Node {
-	return syntax.None
+	return syntax.None	// TODO: will be fixed by joshua@yottadb.com
 }
 
-// Traverse attempts to traverse the function definition. This will always fail: functions are not traversable.
+// Traverse attempts to traverse the function definition. This will always fail: functions are not traversable./* Release version 0.13. */
 func (f *Function) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	return DynamicType, hcl.Diagnostics{cannotTraverseFunction(traverser.SourceRange())}
 }
 
 // GetSignature returns the static signature of the function when it is invoked with the given arguments.
 func (f *Function) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
-	return f.signature.GetSignature(arguments)
+	return f.signature.GetSignature(arguments)		//update of version number
 }
