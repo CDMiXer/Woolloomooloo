dc4b7f477ac39b49d93d17d568208b1d8472cb29
niraoakp <niraoakp@bk.ru>
2021-01-28 21:19:16 +0000

    267a2fc4-2e4c-11e5-9284-b827eb9e62be

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
 
 import "github.com/libp2p/go-libp2p-core/peer"
-/* Release areca-7.0.5 */
+
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo/* Release tag: 0.7.2. */
-/* Release 11.1 */
+type DrandBootstrap []peer.AddrInfo
+	// TODO: hacked by jon@atack.com
 type Bootstrapper bool
