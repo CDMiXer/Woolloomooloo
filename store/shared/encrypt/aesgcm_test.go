640f6fbf94b4631b73b2315ca37016eb60afe4ab
niraoakp <niraoakp@bk.ru>
2021-01-28 07:55:32 +0000

    Multiple output handling, returning value proxy generation

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update AccountSwitch.java */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package encrypt	// TODO: Adding httpd_port and jsonapi example to hiera
+package encrypt
 
 import "testing"
 
-func TestAesgcm(t *testing.T) {	// TODO: will be fixed by ng8eke@163.com
+func TestAesgcm(t *testing.T) {
 	s := "correct-horse-batter-staple"
 	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
 	ciphertext, err := n.Encrypt(s)
 	if err != nil {
 		t.Error(err)
 	}
-	plaintext, err := n.Decrypt(ciphertext)/* Added more URLs */
+	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
 		t.Error(err)
 	}
