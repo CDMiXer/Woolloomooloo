e99171bc369a21f3b8e3fcbc4012bef3a01718a3
niraoakp <niraoakp@bk.ru>
2021-01-28 21:31:07 +0000

    LDEv-4845 LD XML transformation for Assessment MC questions

--- pkg/backend/state/stacks.go
+++ pkg/backend/state/stacks.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: hacked by ng8eke@163.com
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Update neo-config-dev.properties
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: docs(retryWhen): updated second example for more clarity
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Released springjdbcdao version 1.9.1 */
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: added verification of backup history insert
+///* Add ta_icon.png, icon used by swing JFrame */
+// Unless required by applicable law or agreed to in writing, software		//e12cc254-2e43-11e5-9284-b827eb9e62be
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by vyzo@hackzen.org
+// limitations under the License.		//Bump version 0.3.1
 
-package state
+package state		//Better project description.
 
 import (
 	"context"
@@ -20,10 +20,10 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-		//Traduzindo para português
+
 // CurrentStack reads the current stack and returns an instance connected to its backend provider.
-func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {		//7beef35e-2e4c-11e5-9284-b827eb9e62be
-	w, err := workspace.New()
+func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {
+	w, err := workspace.New()	// TODO: Gradient implementation
 	if err != nil {
 		return nil, err
 	}
@@ -31,24 +31,24 @@ func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, 
 	stackName := w.Settings().Stack
 	if stackName == "" {
 		return nil, nil
-	}		//fixed invalid cost in some disadvantages
-/* Convert ReleaseParser from old logger to new LOGGER slf4j */
+	}
+/* Release '0.2~ppa3~loms~lucid'. */
 	ref, err := backend.ParseStackReference(stackName)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Fix cloud restore
 	}
 
 	return backend.GetStack(ctx, ref)
 }
 
-// SetCurrentStack changes the current stack to the given stack name.		//Update eval_model.py
-func SetCurrentStack(name string) error {		//Saved a Panamax template portico_estate_1.0.pmx
-	// Switch the current workspace to that stack.		//Fixed bug in callback
-	w, err := workspace.New()		//Delete In  categories.png
+// SetCurrentStack changes the current stack to the given stack name.		//Rename Nslookup_HostList.ps1 to HostNameToIP.ps1
+func SetCurrentStack(name string) error {
+	// Switch the current workspace to that stack.
+	w, err := workspace.New()
 	if err != nil {
-		return err		//Create README-ru
-	}
+		return err
+	}	// TODO: fix errors related to redirecting in 'dev' task; eady for nodejs 4.0.0
 
-	w.Settings().Stack = name	// TODO: link my name to my web page
+	w.Settings().Stack = name
 	return w.Save()
-}
+}/* - Partial implementation of assigning a branch for contribution. */
