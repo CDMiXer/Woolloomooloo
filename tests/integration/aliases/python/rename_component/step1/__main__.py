c89522870a0aa158dd0c7f6ef654d19d735f1896
niraoakp <niraoakp@bk.ru>
2021-01-28 08:33:29 +0000

    rolling back to more stable approach

--- tests/integration/aliases/python/rename_component/step1/__main__.py
+++ tests/integration/aliases/python/rename_component/step1/__main__.py
@@ -1,19 +1,19 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* [artifactory-release] Release version 3.2.17.RELEASE */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* Release 2.2.10 */
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)
+        super().__init__("my:module:Resource", name, None, opts)	// TODO: will be fixed by davidad@alum.mit.edu
 
 # Scenario #3 - rename a component (and all it's children)
-class ComponentThree(ComponentResource):/* changed 0 to 1 :P */
+class ComponentThree(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentThree", name, None, opts)
         # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
         # later, the implicit alias inherited from the parent alias will include replacing the name
-        # prefix to match the parent alias name./* Update readme to Adldap2 v7 */
+        # prefix to match the parent alias name.		//add several class method's descriptions
         resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
-		//New version of Xodogo - 1.1.1
-comp3 = ComponentThree("comp3")
+
+comp3 = ComponentThree("comp3")/* Added Mockito.any() with sugar */
