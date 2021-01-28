cf1ddae960383e6483e089b0217ee00bb75d90d3
niraoakp <niraoakp@bk.ru>
2021-01-28 18:29:05 +0000

    Zslicer was reporting an incorrect X/Y aspect ratio

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
 	"golang.org/x/xerrors"
-
+	// TODO: 171834ce-2e63-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -17,25 +17,25 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
 		if err != nil {
 			return false, true, err
-		}
+		}/* make filenames consistent across examples */
 
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
-		if msg.Nonce >= fa.Nonce {
+		if msg.Nonce >= fa.Nonce {	// TODO: hacked by mail@bitpshr.net
 			return false, true, nil
-		}
+		}		//adds hopscotch js support
 
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
 		}
-
-		if ml == nil {
+/* Removed suggest from composer */
+		if ml == nil {/* Release 1.0.24 - UTF charset for outbound emails */
 			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
 
-		return true, more, err
+		return true, more, err	// TODO: Update dcnc.hpp
 	}
 }
 
