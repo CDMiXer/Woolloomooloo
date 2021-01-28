97e71f5dec44e0325522bda0dc466d1b9a8a7413
niraoakp <niraoakp@bk.ru>
2021-01-28 07:17:21 +0000

    Fix type in title for = config

--- metric/license.go
+++ metric/license.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// add TODO note 
 // +build !oss
-
+/* Update mt6580 */
 package metric
 
 import "github.com/drone/drone/core"
