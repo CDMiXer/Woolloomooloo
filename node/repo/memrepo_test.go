6456ec8fe58a127f1276b6ae7e58712a09cfbbda
niraoakp <niraoakp@bk.ru>
2021-01-28 06:55:21 +0000

    Recorder distinguishes between class and instance methods when matching.

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
 package repo
-
+		//4bba1396-2e1d-11e5-affc-60f81dce716c
 import (
 	"testing"
 )
 
-func TestMemBasic(t *testing.T) {
+func TestMemBasic(t *testing.T) {		//fix utf8 decode problems
 	repo := NewMemory(nil)
 	basicTest(t, repo)
 }
