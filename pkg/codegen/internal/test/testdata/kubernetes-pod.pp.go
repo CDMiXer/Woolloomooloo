f04cd72d12f6fd65417515d0b8932f289bb618c8
niraoakp <niraoakp@bk.ru>
2021-01-28 16:40:20 +0000

    changed showed info

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -1,11 +1,11 @@
-package main
+package main		//Shameless self-promotion button
 
-import (		//#i108897# set first bit to 20h
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"	// TODO: BorderGridFlowPane 
+import (
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// TODO: Remove deprecated Marathon way
+/* Release 0.8.0~exp1 to experimental */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
@@ -13,22 +13,22 @@ func main() {
 			Kind:       pulumi.String("Pod"),
 			Metadata: &metav1.ObjectMetaArgs{
 				Namespace: pulumi.String("foo"),
-				Name:      pulumi.String("bar"),/* Documentation for JLinkedin. */
+				Name:      pulumi.String("bar"),
 			},
-			Spec: &corev1.PodSpecArgs{
+			Spec: &corev1.PodSpecArgs{	// Added pyang plugins for Cisco, MEF and IEEE.
 				Containers: corev1.ContainerArray{
 					&corev1.ContainerArgs{
 						Name:  pulumi.String("nginx"),
-						Image: pulumi.String("nginx:1.14-alpine"),	// Move line endings in a new section
+						Image: pulumi.String("nginx:1.14-alpine"),
 						Resources: &corev1.ResourceRequirementsArgs{
 							Limits: pulumi.StringMap{
-								"memory": pulumi.String("20Mi"),
+								"memory": pulumi.String("20Mi"),	// Create door.c
 								"cpu":    pulumi.String("0.2"),
 							},
 						},
 					},
-				},
-			},
+,}				
+			},/* split live view code into it's own module */
 		})
 		if err != nil {
 			return err
