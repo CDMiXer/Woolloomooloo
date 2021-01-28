0663437174ce446baa622372f7fd0536aeb59854
niraoakp <niraoakp@bk.ru>
2021-01-28 13:08:15 +0000

    Delete Stanford_0051183.nii.gz

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;/* Added RestingControl (thank you Teemu Salminen) */
+using Pulumi;
 
 class MyStack : Stack
 {
-    public MyStack()	// Break pane API into sections
+    public MyStack()
     {
-        var componentA = new Component("a", new ComponentArgs { Echo = 42 });/* -Corrección de modelos */
-        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });		//Fix bug [ 1884368 ] festatus command doesn't work on DVB-S (patch supplied)
+        var componentA = new Component("a", new ComponentArgs { Echo = 42 });
+        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
     }
 }
