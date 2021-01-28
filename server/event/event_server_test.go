82aa4218cceeae2b53accfc1efe4fd45d121ad24
niraoakp <niraoakp@bk.ru>
2021-01-28 08:26:01 +0000

    minor fixes - verbs

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -2,34 +2,34 @@ package event
 
 import (
 	"testing"
-/* Release version: 1.0.6 */
-	"github.com/stretchr/testify/assert"	// Updating build-info/dotnet/roslyn/dev15.5p1 for beta1-61925-02
+
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/util/instanceid"	// Remove Toolchain-arm-none-eabi.cmake
-)/* add blinkenlight test */
+	"github.com/argoproj/argo/util/instanceid"
+)
 
-func TestController(t *testing.T) {/* Release version [10.5.2] - prepare */
+func TestController(t *testing.T) {
 	clientset := fake.NewSimpleClientset()
 	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
 
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
-	assert.NoError(t, err)		//Added BulkUpdate to the SomeEntities class.
+	assert.NoError(t, err)
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
-	// TODO: hacked by lexy8russo@outlook.com
+
 	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
 	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
 
-	stopCh := make(chan struct{}, 1)	// TODO: hacked by xiemengjun@gmail.com
+	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
 	s.Run(stopCh)
 
-	assert.Len(t, s.operationQueue, 0, "all events were processed")/* Merge "Remove logs Releases from UI" */
+	assert.Len(t, s.operationQueue, 0, "all events were processed")
 
 }
