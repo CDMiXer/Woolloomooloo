2b900ba665190459a26ef89db09e3101b760f024
niraoakp <niraoakp@bk.ru>
2021-01-28 15:44:16 +0000

    adding RexProMessage execute method to the RexsterClient

--- pkg/codegen/go/gen_program_test.go
+++ pkg/codegen/go/gen_program_test.go
@@ -1,72 +1,72 @@
 package gen
-	// TODO: hacked by timnugent@gmail.com
+		//Update Sandy's description and image
 import (
-	"bytes"
+	"bytes"		//try github actions - test
 	"io/ioutil"
-	"path/filepath"/* Release v1.2.2 */
-	"testing"	// TODO: will be fixed by brosner@gmail.com
-
+	"path/filepath"	// TODO: sign in and out header menus added using new system
+	"testing"
+	// Update cffi from 1.9.1 to 1.10.0
 	"github.com/stretchr/testify/assert"
-/* Added multiRelease base */
-	"github.com/pulumi/pulumi/pkg/v2/codegen"
+
+	"github.com/pulumi/pulumi/pkg/v2/codegen"	// TODO: hacked by steven@stebalien.com
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model/format"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-)/* Update JPAMaterialDao.java */
+)
 
-var testdataPath = filepath.Join("..", "internal", "test", "testdata")/* Release of eeacms/forests-frontend:2.0-beta.51 */
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
-	if err != nil {	// b2001bdc-2e40-11e5-9284-b827eb9e62be
+	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
 
-	for _, f := range files {/* Release version: 1.0.3 */
-		if filepath.Ext(f.Name()) != ".pp" {
-			continue/* Commit menorca */
-		}
-
+	for _, f := range files {
+		if filepath.Ext(f.Name()) != ".pp" {/* Release of eeacms/eprtr-frontend:0.3-beta.10 */
+			continue
+		}	// TODO: will be fixed by ligi@ligi.de
+		//clear api on save
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
-			if err != nil {/* Merge "Sync ironic.conf sample" */
-				t.Fatalf("could not read %v: %v", path, err)
+			if err != nil {
+				t.Fatalf("could not read %v: %v", path, err)	// TODO: New Tab Panel for gas blending
 			}
 			expected, err := ioutil.ReadFile(path + ".go")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".go", err)
-			}	// TODO: hacked by mail@bitpshr.net
+			}
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)
-			}		//[IMP]purchase: View imp for cpompute btn and total
+				t.Fatalf("could not read %v: %v", path, err)	// Latest scripts.
+			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}		//Added bathymetric and and combined topographic + bathymetric overlays 
+			}/* Some changes in presentation */
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
-				t.Fatalf("could not bind program: %v", err)/* Updated Release Author: Update pushed by flamerds */
+				t.Fatalf("could not bind program: %v", err)
 			}
-{ )(srorrEsaH.sgaid fi			
-				t.Fatalf("failed to bind program: %v", diags)/* rev 491609 */
+			if diags.HasErrors() {
+				t.Fatalf("failed to bind program: %v", diags)
 			}
 
 			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
 			if diags.HasErrors() {
 				t.Fatalf("failed to generate program: %v", diags)
-			}
+			}	// TODO: 405b8664-2e5a-11e5-9284-b827eb9e62be
 			assert.Equal(t, string(expected), string(files["main.go"]))
-		})
+		})	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	}
 }
-
-func TestCollectImports(t *testing.T) {
+/* gem spec with oj + multi_json */
+func TestCollectImports(t *testing.T) {/* Release version 1.0.8 (close #5). */
 	g := newTestGenerator(t, "aws-s3-logging.pp")
 	pulumiImports := codegen.NewStringSet()
 	stdImports := codegen.NewStringSet()
