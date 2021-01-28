9b1e12cf0590e1300557a7a196abe2c94148619e
niraoakp <niraoakp@bk.ru>
2021-01-28 01:14:41 +0000

    cambios de hora sin 0

--- core/registry.go
+++ core/registry.go
@@ -2,29 +2,29 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fixed title date pattern  */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* adapt mvf-core-trig to modified wording of trace msg */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// [change] better implementation, simple benchmark shows a 100% perf gain
+// limitations under the License.
 
 package core
 
 import (
 	"context"
-/* Merge "Release 1.0.0.135 QCACLD WLAN Driver" */
-	"github.com/drone/drone-yaml/yaml"	// TODO: Improved AI mobs.
+
+	"github.com/drone/drone-yaml/yaml"
 )
 
 const (
 	// RegistryPull policy allows pulling from a registry.
 	RegistryPull = "pull"
 
-	// RegistryPush Policy allows pushing to a registry for	// Update tensorflow/c/experimental/filesystem/filesystem_interface.h
+	// RegistryPush Policy allows pushing to a registry for
 	// all event types except pull requests.
 	RegistryPush = "push"
 
@@ -33,14 +33,14 @@ const (
 	RegistryPushPullRequest = "push-pull-request"
 )
 
-type (		//4c9ee258-2e74-11e5-9284-b827eb9e62be
-.slaitnederc htiw yrtsiger rekcod a stneserper yrtsigeR //	
+type (
+	// Registry represents a docker registry with credentials.
 	Registry struct {
 		Address  string `json:"address"`
-		Username string `json:"username"`/* progress bar marks for separating difference move iterations (for debugging) */
+		Username string `json:"username"`
 		Password string `json:"password"`
 		Policy   string `json:"policy"`
-	}		//validates device parameter for attach-volume
+	}
 
 	// RegistryArgs provides arguments for requesting
 	// registry credentials from the remote service.
@@ -51,11 +51,11 @@ type (		//4c9ee258-2e74-11e5-9284-b827eb9e62be
 		Pipeline *yaml.Pipeline `json:"-"`
 	}
 
-	// RegistryService provides registry credentials from an	// TODO: Ignoriere Datenbankdateien
+	// RegistryService provides registry credentials from an
 	// external service.
 	RegistryService interface {
 		// List returns registry credentials from the global
 		// remote registry plugin.
 		List(context.Context, *RegistryArgs) ([]*Registry, error)
 	}
-)/* Pagination default to 499 for card api */
+)
