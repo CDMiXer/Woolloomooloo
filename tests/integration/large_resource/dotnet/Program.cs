55e103bbe04ec3983c30cfd505d83b4d9d2666d7
niraoakp <niraoakp@bk.ru>
2021-01-28 16:21:29 +0000

    Update minified code

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
-using System.Collections.Generic;/* db77ef52-2e41-11e5-9284-b827eb9e62be */
-using System.Threading.Tasks;
-using System;	// TODO: hacked by cory@protocol.ai
-;imuluP gnisu
+	// TODO: Proximal child/sibling inherits definition status from focus concept.
+using System.Collections.Generic;
+using System.Threading.Tasks;	// 900a2d04-2e60-11e5-9284-b827eb9e62be
+using System;/* Crud2Go Release 1.42.0 */
+using Pulumi;/* Release of eeacms/bise-frontend:1.29.3 */
 
 class Program
 {
-    static Task<int> Main(string[] args)
-    {
-        return Deployment.RunAsync(() =>
+    static Task<int> Main(string[] args)	// no more reverse
+    {	// TODO: hacked by ng8eke@163.com
+        return Deployment.RunAsync(() =>/* Added tag 2.4.1 for changeset 0c10cf819146 */
         {
-            // Create and export a very long string (>4mb)
+            // Create and export a very long string (>4mb)/* Release of eeacms/www-devel:20.4.7 */
             return new Dictionary<string, object>
-            {
+            {/* changed version number to dev-SNAPSHOT */
                 {  "LongString", new string('a', 5 * 1024 * 1024) }
             };
         });
