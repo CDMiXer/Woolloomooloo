d3a71992af7b41a4238e4b08eae065f92293dc57
niraoakp <niraoakp@bk.ru>
2021-01-28 23:47:51 +0000

    add syntacx highliht

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -2,19 +2,19 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* #870 - adapt WbAnnoTSVReader/Writer for custom layers */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//		//core fields: fix button group test
-// Unless required by applicable law or agreed to in writing, software/* Updated New Release Checklist (markdown) */
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     http://www.apache.org/licenses/LICENSE-2.0/* automated commit from rosetta for sim/lib projectile-motion, locale da */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Revert r205599, the commit was not intended to have so many changes */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Adding the build/ directory to the list of things to clean up. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// Now go back the other way and make sure that "A" is external again./* [IMP] manifest: removed Hidden/Links category, use auto_install: True instead. */
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );	// TODO: 1a032a0c-2e60-11e5-9284-b827eb9e62be
+// Now go back the other way and make sure that "A" is external again.
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-
+	// TODO: 15009200-2e4d-11e5-9284-b827eb9e62be
