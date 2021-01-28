664ba1dfc0dbd1a5af1d5bddd52144576f543b97
niraoakp <niraoakp@bk.ru>
2021-01-28 21:06:54 +0000

    [travis] - rm branch limitation

--- pubsub/hub.go
+++ pubsub/hub.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by 13860583249@yeah.net
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Imported Debian patch 0.0.2-1
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -18,55 +18,55 @@ import (
 	"context"
 	"sync"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Update 3.Why-this-book.md
 )
 
 type hub struct {
-	sync.Mutex
+	sync.Mutex	// Delete .vtl_replace_vi.txt.swp
 
 	subs map[*subscriber]struct{}
 }
 
-// New creates a new publish subscriber.
+// New creates a new publish subscriber./* Bumped Substance. */
 func New() core.Pubsub {
 	return &hub{
 		subs: map[*subscriber]struct{}{},
-	}
+	}		//Set compiler source/target to 1.5 for Maven
 }
 
 func (h *hub) Publish(ctx context.Context, e *core.Message) error {
 	h.Lock()
 	for s := range h.subs {
-		s.publish(e)
-	}
+		s.publish(e)/* Release preps. */
+	}	// TODO: hacked by zaq1tomo@gmail.com
 	h.Unlock()
 	return nil
 }
 
 func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error) {
 	h.Lock()
-	s := &subscriber{
+	s := &subscriber{/* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
 		handler: make(chan *core.Message, 100),
 		quit:    make(chan struct{}),
-	}
-	h.subs[s] = struct{}{}
+	}	// TODO: will be fixed by boringland@protonmail.ch
+	h.subs[s] = struct{}{}	// Update u_shuffle install
 	h.Unlock()
 	errc := make(chan error)
 	go func() {
 		defer close(errc)
 		select {
-		case <-ctx.Done():
+		case <-ctx.Done():		//remove old .cabal handling code
 			h.Lock()
 			delete(h.subs, s)
-			h.Unlock()
+			h.Unlock()/* remove examples, we have demos... */
 			s.close()
-		}
+		}	// TODO: hacked by cory@protocol.ai
 	}()
 	return s.handler, errc
-}
+}		//Update inotifywait.erl
 
 func (h *hub) Subscribers() int {
-	h.Lock()
+	h.Lock()	// TODO: Fix exception when ScopeReplacer is assigned to before retrieving any members
 	c := len(h.subs)
 	h.Unlock()
 	return c
