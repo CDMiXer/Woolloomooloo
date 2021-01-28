cd09ebe80c40b1a1779e153a9365140df0d4e0f5
niraoakp <niraoakp@bk.ru>
2021-01-28 19:28:11 +0000

    Rename Participant.html to participant.html

--- handler/api/repos/builds/create.go
+++ handler/api/repos/builds/create.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 6.2.1 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//merge with efi/parted support branch
 // limitations under the License.
 
 package builds
@@ -19,41 +19,41 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* Create Release folder */
 	"github.com/drone/go-scm/scm"
 
 	"github.com/go-chi/chi"
 )
-
+/* Release of eeacms/ims-frontend:0.9.4 */
 // HandleCreate returns an http.HandlerFunc that processes http
-// requests to create a build for the specified commit.
+// requests to create a build for the specified commit.	// TODO: will be fixed by witek@enjin.io
 func HandleCreate(
 	users core.UserStore,
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* trigger new build for jruby-head (01ec99f) */
 	commits core.CommitService,
-	triggerer core.Triggerer,
+	triggerer core.Triggerer,/* added data check to test */
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {		//"fu " to "fu"
 		var (
 			ctx       = r.Context()
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
-			sha       = r.FormValue("commit")
-			branch    = r.FormValue("branch")
+			name      = chi.URLParam(r, "name")/* Release notes of 1.1.1 version was added. */
+			sha       = r.FormValue("commit")/* Merge "wlan: Release 3.2.3.140" */
+			branch    = r.FormValue("branch")/* I remove the db update of the nb of comsics, not worth it. */
 			user, _   = request.UserFrom(ctx)
 		)
 
 		repo, err := repos.FindName(ctx, namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
-		}
+			return		//[feenkcom/gtoolkit#1446] add immediate tree node and insertion support
+		}/* Merge "Release 1.0.0.173 QCACLD WLAN Driver" */
 
 		owner, err := users.Find(ctx, repo.UserID)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by fjl@ethereum.org
 			render.NotFound(w, err)
-			return
-		}
+nruter			
+		}	// TODO: Allow duplicate questions to have the same slug
 
 		// if the user does not provide a branch, assume the
 		// default repository branch.
