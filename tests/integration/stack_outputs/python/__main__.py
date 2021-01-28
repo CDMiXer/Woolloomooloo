596dd7434f2439a9f5280ff04c412ed160f5180c
niraoakp <niraoakp@bk.ru>
2021-01-28 09:40:17 +0000

    Delete data.scss

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.
-#/* Release version [10.5.1] - alfter build */
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License./* Release candidate 1. */
-# You may obtain a copy of the License at/* 38e58f86-2e61-11e5-9284-b827eb9e62be */
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,/* replaced diagram */
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Linked list */
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pulumi/* Update sqlalchemy from 1.2.10 to 1.2.12 */
+import pulumi
 
 pulumi.export("xyz", "ABC")
 pulumi.export("foo", 42)
