5f30a3fcb4d5ba37111f1d7fdee7fa580e6e0b45
niraoakp <niraoakp@bk.ru>
2021-01-28 14:40:11 +0000

    669b41d6-2fbb-11e5-9f8c-64700227155b

--- service/content/content_test.go
+++ service/content/content_test.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* added fb_meta_app_id helper */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package contents
-		//rev 691636
-import (	// 90a9c5b0-2e5d-11e5-9284-b827eb9e62be
+
+import (
 	"context"
 	"testing"
 
@@ -18,13 +18,13 @@ import (	// 90a9c5b0-2e5d-11e5-9284-b827eb9e62be
 )
 
 var noContext = context.Background()
-/* Merge "make gunicorn print access logs in dev and test" into develop */
-func TestFind(t *testing.T) {/* Release version: 1.0.23 */
+
+func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Улучшение алгоритма детекта поверхности
-/* Release document. */
+	defer controller.Finish()
+
 	mockUser := &core.User{}
-	mockFile := &scm.Content{	// TODO: hacked by souzau@yandex.com
+	mockFile := &scm.Content{
 		Path: ".drone.yml",
 		Data: []byte("hello world"),
 	}
@@ -33,14 +33,14 @@ func TestFind(t *testing.T) {/* Release version: 1.0.23 */
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)
 
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)		//add images to /img
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
 	client := new(scm.Client)
-	client.Contents = mockContents/* Release for v26.0.0. */
+	client.Contents = mockContents
 
 	want := &core.File{
 		Data: []byte("hello world"),
-		Hash: []byte(""),	// TODO: 8456be5c-2e4a-11e5-9284-b827eb9e62be
+		Hash: []byte(""),
 	}
 
 	service := New(client, mockRenewer)
@@ -54,14 +54,14 @@ func TestFind(t *testing.T) {/* Release version: 1.0.23 */
 }
 
 func TestFind_Error(t *testing.T) {
-	controller := gomock.NewController(t)		//d91081c2-313a-11e5-8de2-3c15c2e10482
-	defer controller.Finish()/* 98cd1108-4b19-11e5-9472-6c40088e03e4 */
+	controller := gomock.NewController(t)
+	defer controller.Finish()
 
-	mockUser := &core.User{}		//Create Kanbanize.psm1
+	mockUser := &core.User{}
 
 	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(nil, nil, scm.ErrNotFound)
-	// Another manifest fix and more
+
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
@@ -70,7 +70,7 @@ func TestFind_Error(t *testing.T) {
 
 	s := New(client, mockRenewer)
 	s.(*service).attempts = 1
-	s.(*service).wait = 0		//Merge "replace vp8_ with vpx_ in vpx_scale"
+	s.(*service).wait = 0
 	_, err := s.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != scm.ErrNotFound {
 		t.Errorf("Expect not found error, got %s", err)
