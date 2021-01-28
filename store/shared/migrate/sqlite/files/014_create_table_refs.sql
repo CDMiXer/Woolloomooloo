16c9b90ec75378496a2c980eeb77d598779e702e
niraoakp <niraoakp@bk.ru>
2021-01-28 23:26:17 +0000

    Updated the Contributors list

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest		//added assembly plugin..
+-- name: create-table-latest	// more verbose exception output
 
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER/* Release v0.2 toolchain for macOS. */
-,latest_build_id INTEGER/* Released 3.6.0 */
+ latest_repo_id  INTEGER
+,latest_build_id INTEGER
 ,latest_type     TEXT -- branch | tag     | pull_request | promote
-,latest_name     TEXT -- master | v1.0.0, | 42           | production
+,latest_name     TEXT -- master | v1.0.0, | 42           | production/* Release of 2.4.0 */
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER
+,latest_deleted  INTEGER	// Merge branch 'master' into measurement-blackboard
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);	// TODO: 4ed32254-2e43-11e5-9284-b827eb9e62be
+);
 
--- name: create-index-latest-repo		//5d29c934-2e66-11e5-9284-b827eb9e62be
-
+-- name: create-index-latest-repo
+	// updated gem dependencies and removed unnesseccary ones
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
