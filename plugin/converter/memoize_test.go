2668043b6032390c18bb2c4abd48ba0f44004fdd
niraoakp <niraoakp@bk.ru>
2021-01-28 12:51:12 +0000

    Update uprofileadd.php

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* updated URL in comment example */
+
 // +build !oss
 
 package converter
 
 import (
-	"errors"/* Renamed report api call parameters to clarify ELFIN CLASSE used. */
-	"testing"	// Commit TODO prior to pull
+	"errors"
+	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* [NGRINDER-287]3.0 Release: Table titles are overlapped on running page. */
+	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
-/* Bux fix spellchecker */
+
 func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -24,10 +24,10 @@ func TestMemoize(t *testing.T) {
 	args := &core.ConvertArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
-		Config: conf,	// TODO: hacked by witek@enjin.io
+		Config: conf,
 	}
 
-	base := mock.NewMockConvertService(controller)	// report is working for driver.asl agent and cooperative-driver.asl.
+	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
@@ -37,17 +37,17 @@ func TestMemoize(t *testing.T) {
 		return
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {	// TODO: will be fixed by vyzo@hackzen.org
-		t.Errorf("Expect %d items in cache, got %d", want, got)/* Clarifying intended use of the secondary menu (fixes #875) */
+	if got, want := service.cache.Len(), 1; got != want {
+		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
-/* More apport notes */
+
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Convert(noContext, args)
-	if err != nil {	// TODO: don't disable inet
+	if err != nil {
 		t.Error(err)
 		return
 	}
-	if res != conf {/* job #10529 - Release notes and Whats New for 6.16 */
+	if res != conf {
 		t.Errorf("Expect result from cache")
 	}
 
@@ -56,7 +56,7 @@ func TestMemoize(t *testing.T) {
 	}
 }
 
-func TestMemoize_Tag(t *testing.T) {/* Release 3.0.0 doc */
+func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -69,7 +69,7 @@ func TestMemoize_Tag(t *testing.T) {/* Release 3.0.0 doc */
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
-	service := Memoize(base).(*memoize)		//Added the author value.
+	service := Memoize(base).(*memoize)
 	res, err := service.Convert(noContext, args)
 	if err != nil {
 		t.Error(err)
@@ -77,7 +77,7 @@ func TestMemoize_Tag(t *testing.T) {/* Release 3.0.0 doc */
 	}
 	if res != args.Config {
 		t.Errorf("Expect result from cache")
-	}	// TODO: src/paf.c : Replace ppaf24->samplesperblock with a compile time constant.
+	}
 }
 
 func TestMemoize_Empty(t *testing.T) {
@@ -88,7 +88,7 @@ func TestMemoize_Empty(t *testing.T) {
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: ""}, // empty
-	}/* Updtate Release Notes URL */
+	}
 
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
