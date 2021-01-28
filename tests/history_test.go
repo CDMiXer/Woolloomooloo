5b1dba0e164425a50c20ba3542e57ff7a27d243b
niraoakp <niraoakp@bk.ru>
2021-01-28 15:56:11 +0000

    Update deploy-to-sonatype.sh

--- tests/history_test.go
+++ tests/history_test.go
@@ -1,51 +1,51 @@
 // Copyright 2018, Pulumi Corporation.
-//	// TODO: update very deep learning theory
-// Licensed under the Apache License, Version 2.0 (the "License");/* Fix Release Job */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: eaae2791-327f-11e5-b9a6-9cf387a8033e
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by onhardev@bk.ru
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release script */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package tests
 
-import (
+import (/* Release 1-129. */
 	"testing"
-
+	// TODO: Update FindAllDependencies.cmake
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"
+	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"/* Create new file HowToRelease.md. */
 	"github.com/stretchr/testify/assert"
-)
-/* shutdown: add an appropriate description on error */
-// deleteIfNotFailed deletes the files in the testing environment if the testcase has
-// not failed. (Otherwise they are left to aid debugging.)
+)	// TODO: will be fixed by ng8eke@163.com
+	// TODO: fcb6d600-2e6a-11e5-9284-b827eb9e62be
+// deleteIfNotFailed deletes the files in the testing environment if the testcase has	// TODO: hacked by caojiaoyue@protonmail.com
+// not failed. (Otherwise they are left to aid debugging.)	// TODO: typo remove comma
 func deleteIfNotFailed(e *ptesting.Environment) {
-	if !e.T.Failed() {		//Update attrs from 16.3.0 to 17.2.0
-		e.DeleteEnvironment()
-	}		//Update closed_by_restrictions.erb
+	if !e.T.Failed() {
+		e.DeleteEnvironment()	// TODO: hacked by souzau@yandex.com
+	}/* keep the convention names clear. *_base methods for calculations, * for display */
 }
-
+	// TODO: will be fixed by mail@bitpshr.net
 // assertHasNoHistory runs `pulumi history` and confirms an error that the stack has not
-// ever been updated.
-func assertHasNoHistory(e *ptesting.Environment) {/* Prepare for Release 0.5.4 */
+// ever been updated./* like pagination where possible */
+func assertHasNoHistory(e *ptesting.Environment) {
 	// NOTE: pulumi returns with exit code 0 in this scenario.
 	out, err := e.RunCommand("pulumi", "history")
 	assert.Equal(e.T, "", err)
 	assert.Equal(e.T, "Stack has never been updated\n", out)
 }
-func TestHistoryCommand(t *testing.T) {
+func TestHistoryCommand(t *testing.T) {	// Snowflake aggregate support
 	// We fail if no stack is selected.
-	t.Run("NoStackSelected", func(t *testing.T) {
-		e := ptesting.NewEnvironment(t)
+	t.Run("NoStackSelected", func(t *testing.T) {		//Remove fieldset and container div
+		e := ptesting.NewEnvironment(t)/* TODO-841: text 16WW REV4 */
 		defer deleteIfNotFailed(e)
 		integration.CreateBasicPulumiRepo(e)
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
 		out, err := e.RunCommandExpectError("pulumi", "history")
-		assert.Equal(t, "", out)	// Update 20486C_MOD11_DEMO.md
+		assert.Equal(t, "", out)
 		assert.Contains(t, err, "error: no stack selected")
 	})
 
@@ -58,21 +58,21 @@ func TestHistoryCommand(t *testing.T) {
 		e.RunCommand("pulumi", "stack", "init", "no-updates-test")
 		assertHasNoHistory(e)
 	})
-/* Quotes to prevent error when PWD contains space */
-	// The "history" command uses the currently selected stack.		//Update react-ie8.md
-	t.Run("CurrentlySelectedStack", func(t *testing.T) {/* Merge "[INTERNAL] Release notes for version 1.36.3" */
+
+	// The "history" command uses the currently selected stack.
+	t.Run("CurrentlySelectedStack", func(t *testing.T) {
 		e := ptesting.NewEnvironment(t)
 		defer deleteIfNotFailed(e)
 		integration.CreateBasicPulumiRepo(e)
-		e.ImportDirectory("integration/stack_dependencies")		//content border
-		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())		//Renamed existing clobber targets to distclean, and added new clobber targets
+		e.ImportDirectory("integration/stack_dependencies")
+		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
 		e.ImportDirectory("integration/stack_outputs")
-		e.RunCommand("pulumi", "stack", "init", "stack-without-updates")/* 4.1.1 Release */
+		e.RunCommand("pulumi", "stack", "init", "stack-without-updates")
 		e.RunCommand("pulumi", "stack", "init", "history-test")
-		e.RunCommand("yarn", "install")	// TODO: Let undertow handle the dirty work.
+		e.RunCommand("yarn", "install")
 		e.RunCommand("yarn", "link", "@pulumi/pulumi")
 		// Update the history-test stack.
-)"kcats detadpu na si siht" ,"m-" ,"weiverp-piks--" ,"sey--" ,"evitcaretni-non--" ,"pu" ,"imulup"(dnammoCnuR.e		
+		e.RunCommand("pulumi", "up", "--non-interactive", "--yes", "--skip-preview", "-m", "this is an updated stack")
 		// Confirm we see the update message in thie history output.
 		out, err := e.RunCommand("pulumi", "history")
 		assert.Equal(t, "", err)
