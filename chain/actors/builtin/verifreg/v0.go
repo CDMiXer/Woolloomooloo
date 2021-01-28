d82580be221edfc18c9a89819f35d2aa2792ee6f
niraoakp <niraoakp@bk.ru>
2021-01-28 01:21:56 +0000

    Improved the SED1330 interface. (no whatsnew)

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,15 +1,15 @@
 package verifreg
-
+/* Create announcer.py */
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Input and output format added
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* [artifactory-release] Release version 0.9.7.RELEASE */
+	// TODO: f07a3734-2e6e-11e5-9284-b827eb9e62be
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// test order reminder with open orders
 )
 
 var _ State = (*state0)(nil)
@@ -24,7 +24,7 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 }
 
 type state0 struct {
-	verifreg0.State
+	verifreg0.State/* Actualización ppal */
 	store adt.Store
 }
 
@@ -34,24 +34,24 @@ func (s *state0) RootKey() (address.Address, error) {
 
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
-}
-
+}/* (jam) Release 2.1.0b4 */
+/* Release 0.2.1rc1 */
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
-}
+}		//Merge "[Path 7] SILKROAD-1771 - Limit the resize request in the rem-image api"
 
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// Delete names from global scope
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
-}
-
-func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+}		//Delete jRoll.js
+	// Added DBSchema
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// TODO: will be fixed by xiemengjun@gmail.com
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
-
+	// TODO: hacked by 13860583249@yeah.net
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
-}
+}		//74d5f6d4-2e52-11e5-9284-b827eb9e62be
 
-func (s *state0) verifiers() (adt.Map, error) {
+func (s *state0) verifiers() (adt.Map, error) {/* Create EntityConfigurationDemo.cs */
 	return adt0.AsMap(s.store, s.Verifiers)
 }
