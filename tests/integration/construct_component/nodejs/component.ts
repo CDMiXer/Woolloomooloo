5ba93ddc3ff4f52a0ff09a70a5efc6f597a3ea22
niraoakp <niraoakp@bk.ru>
2021-01-28 12:43:59 +0000

    Speed up covering box operation

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-/* @Release [io7m-jcanephora-0.29.4] */
-interface ComponentArgs {	// Check HTTP return codes before continuing
+	// TODO: remove "blog" from header
+interface ComponentArgs {
     echo: pulumi.Input<any>;
-}		//Debugging ADMM part of SeqUnwinder
+}
 
 export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
     public readonly childId!: pulumi.Output<pulumi.ID>;
 
     constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
-        const inputs: any = {};/* Merge "Created Release Notes chapter" */
+        const inputs: any = {};
         inputs["echo"] = args.echo;
-        inputs["childId"] = undefined /*out*/;
+        inputs["childId"] = undefined /*out*/;		//delete unneeded let
 
         super("testcomponent:index:Component", name, inputs, opts, true);
     }
-}	// file uploading implementation
+}
 
