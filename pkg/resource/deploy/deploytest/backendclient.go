4c337482b11bc1fa628b2828eb62171f3a88a286
niraoakp <niraoakp@bk.ru>
2021-01-28 07:00:25 +0000

    fix typo of README

--- pkg/resource/deploy/deploytest/backendclient.go
+++ pkg/resource/deploy/deploytest/backendclient.go
@@ -3,13 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Rename none to isNone */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploytest
@@ -26,17 +26,17 @@ type BackendClient struct {
 	GetStackResourceOutputsF func(ctx context.Context, name string) (resource.PropertyMap, error)
 }
 
-// GetStackOutputs returns the outputs (if any) for the named stack or an error if the stack cannot be found./* First Stable Release */
+// GetStackOutputs returns the outputs (if any) for the named stack or an error if the stack cannot be found.
 func (b *BackendClient) GetStackOutputs(ctx context.Context, name string) (resource.PropertyMap, error) {
 	return b.GetStackOutputsF(ctx, name)
 }
 
-// GetStackResourceOutputs returns the resource outputs for a stack, or an error if the stack/* Removed superfluous old readme info */
-// cannot be found. Resources are retrieved from the latest stack snapshot, which may include	// TODO: hacked by admin@multicoin.co
+// GetStackResourceOutputs returns the resource outputs for a stack, or an error if the stack
+// cannot be found. Resources are retrieved from the latest stack snapshot, which may include
 // ongoing updates. They are returned in a `PropertyMap` mapping resource URN to another
 // `Propertymap` with members `type` (containing the Pulumi type ID for the resource) and
-// `outputs` (containing the resource outputs themselves).	// remove renderer from project
+// `outputs` (containing the resource outputs themselves).
 func (b *BackendClient) GetStackResourceOutputs(
-	ctx context.Context, name string) (resource.PropertyMap, error) {/* Added Control Point connections manipulation commands */
-	return b.GetStackResourceOutputsF(ctx, name)/* f344057e-2e48-11e5-9284-b827eb9e62be */
+	ctx context.Context, name string) (resource.PropertyMap, error) {
+	return b.GetStackResourceOutputsF(ctx, name)
 }
