ebbf5e13b5232843e17944e14cb8275689f744f1
niraoakp <niraoakp@bk.ru>
2021-01-28 10:36:58 +0000

    Merge "Split out Neutron metering labels client"

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,32 +1,32 @@
 package genesis
 
 import (
-	"context"/* 7a5f3ce0-2e62-11e5-9284-b827eb9e62be */
-
+	"context"
+	// Update RegEx.txt
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"	// - update peer counter
 	cbor "github.com/ipfs/go-ipld-cbor"
-	// 4edcd447-2e9d-11e5-b408-a45e60cdfd11
+/* Release of eeacms/www:18.2.27 */
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Release version 1.1.2.RELEASE */
+)
 
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
-		//Have do_grep() and do_gsub() use bytes if needed.
+
 	st := reward0.ConstructState(qaPower)
 
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
-	}/* Merge "docs: Android 4.0.2 (SDK Tools r16) Release Notes - RC6" into ics-mr0 */
+	}
 
-	return &types.Actor{/* 22133f20-2e65-11e5-9284-b827eb9e62be */
+	return &types.Actor{
 		Code:    builtin.RewardActorCodeID,
-		Balance: types.BigInt{Int: build.InitialRewardBalance},	// TODO: hacked by zaq1tomo@gmail.com
+		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
 	}, nil
 }
