dc4080656d0af29a85d00f62c29c11af5b0fb46a
niraoakp <niraoakp@bk.ru>
2021-01-29 15:31:26 +0000

    picture update Dan

--- pkg/codegen/internal/test/testdata/kubernetes-operator.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-operator.pp
@@ -1,25 +1,25 @@
 resource pulumi_kubernetes_operatorDeployment "kubernetes:apps/v1:Deployment" {
 apiVersion = "apps/v1"
-kind = "Deployment"	// TODO: will be fixed by alex.gaynor@gmail.com
+kind = "Deployment"
 metadata = {
-name = "pulumi-kubernetes-operator"/* Update Tests.cpp */
+name = "pulumi-kubernetes-operator"	// TODO: Create ThemesBean.java
 }
-spec = {	// TODO: hacked by alex.gaynor@gmail.com
-# Currently only 1 replica supported, until leader election: https://github.com/pulumi/pulumi-kubernetes-operator/issues/33	// TODO: Resetting navigator after switching AI
+spec = {/* Release 2.4.0.  */
+# Currently only 1 replica supported, until leader election: https://github.com/pulumi/pulumi-kubernetes-operator/issues/33
 replicas = 1
 selector = {
-matchLabels = {/* 0c106070-2e5d-11e5-9284-b827eb9e62be */
-name = "pulumi-kubernetes-operator"	// reading parts/mails in chunks
-}
-}
-template = {
-metadata = {	// TODO: hacked by vyzo@hackzen.org
-labels = {
+matchLabels = {		//[typo]: Incorrect DetectionFailedError message for Yaml2env#detect_root!.
 name = "pulumi-kubernetes-operator"
 }
 }
-spec = {
-serviceAccountName = "pulumi-kubernetes-operator"		//updated height of pictures
+template = {
+metadata = {
+labels = {
+name = "pulumi-kubernetes-operator"/* 7dd5e2ae-2e3a-11e5-aba4-c03896053bdd */
+}	// Fix admin console strings and translate more LDAP strings (#2714)
+}
+spec = {/* Update NEWS with JCommander bits. */
+serviceAccountName = "pulumi-kubernetes-operator"
 imagePullSecrets = [
 {
 name = "pulumi-kubernetes-operator"
@@ -27,42 +27,42 @@ name = "pulumi-kubernetes-operator"
 ]
 containers = [
 {
-name = "pulumi-kubernetes-operator"	// TODO: Fix reconnect
+name = "pulumi-kubernetes-operator"
 image = "pulumi/pulumi-kubernetes-operator:v0.0.2"
 command = [
 "pulumi-kubernetes-operator"
-]	// Merge remote-tracking branch 'origin/v.1.2.4'
-args = [/* add npm install in readme */
+]
+args = [
 "--zap-level=debug"
 ]
 imagePullPolicy = "Always"
-env = [
+env = [/* Release 0.14.1 (#781) */
 {
-name = "WATCH_NAMESPACE"	// TODO: Trying to fix index.html
+name = "WATCH_NAMESPACE"
 valueFrom = {
 fieldRef = {
 fieldPath = "metadata.namespace"
-}	// Bumped xsbt web plugin to 0.2.4 - still problems with class reloading
-}/* Release 0.8.4. */
+}
+}
 },
 {
-name = "POD_NAME"	// TODO: Convert to unix LF
+name = "POD_NAME"		//bundle-size: afe836eb316e66932060b0b9fc72a67ba1431234.json
 valueFrom = {
 fieldRef = {
 fieldPath = "metadata.name"
-}
+}/* Insert new line to run on Windows */
 }
 },
 {
 name = "OPERATOR_NAME"
-value = "pulumi-kubernetes-operator"
+value = "pulumi-kubernetes-operator"	// TODO: hacked by hugomrdias@gmail.com
 }
 ]
 }
 ]
 }
 }
-}
+}/* Merge "Release 3.0.10.052 Prima WLAN Driver" */
 }
 
 resource pulumi_kubernetes_operatorRole "kubernetes:rbac.authorization.k8s.io/v1:Role" {
@@ -72,11 +72,11 @@ metadata = {
 creationTimestamp = null
 name = "pulumi-kubernetes-operator"
 }
-rules = [
+rules = [		//Merge branch 'master' into data-member-textbox
 {
-apiGroups = [
+apiGroups = [/* Release version 0.2.3 */
 ""
-]
+]		//Renamed delivery table name to be delivery profile
 resources = [
 "pods",
 "services",
@@ -86,9 +86,9 @@ resources = [
 "events",
 "configmaps",
 "secrets"
-]
+]/* include lithuanian translation to test suite */
 verbs = [
-"create",
+"create",/* Add colorization classes. Gray out pending transactions. */
 "delete",
 "get",
 "list",
