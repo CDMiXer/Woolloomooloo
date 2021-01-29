a43ecba25e74c1531d38460469dfb4ddcf15eb92
niraoakp <niraoakp@bk.ru>
2021-01-29 22:59:54 +0000

    amazon fix

--- handler/api/repos/builds/cancel_test.go
+++ handler/api/repos/builds/cancel_test.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Added information about dependencies. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package builds	// TODO: hacked by davidad@alum.mit.edu
+package builds
 
 import (
 	"context"
 	"net/http/httptest"
-	"testing"		//walk: we always have a badfn
+	"testing"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Rework SQL to use PreparedStatements */
-	// increased alternative heap size to 12GB
+	"github.com/drone/drone/core"	// TODO: e1b48908-2e45-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/mock"
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-)
+)/* reset pom file versions */
 
 func TestCancel(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -22,11 +22,11 @@ func TestCancel(t *testing.T) {
 
 	mockStages := []*core.Stage{
 		{Status: core.StatusPassing},
-		{
-			Status: core.StatusPending,
+		{/* Create Cakes.html */
+			Status: core.StatusPending,/* Release for 18.10.0 */
 			Steps: []*core.Step{
 				{Status: core.StatusPassing},
-				{Status: core.StatusPending},
+				{Status: core.StatusPending},/* Release of eeacms/plonesaas:5.2.1-17 */
 			},
 		},
 	}
@@ -34,45 +34,45 @@ func TestCancel(t *testing.T) {
 	mockBuildCopy := new(core.Build)
 	*mockBuildCopy = *mockBuild
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)		//closes #881 - removed first and last name
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
-
+	// TODO: Added missing gpg line
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuildCopy, nil)
-	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)/* Failing test for EnvJujuClient. */
-		//677eee58-2e72-11e5-9284-b827eb9e62be
+	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)
+
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)/* Clearer error, better console output, simpler test. */
+	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)
-		//Update PHIKCL.sld
+
 	steps := mock.NewMockStepStore(controller)
 	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)
 
-	statusService := mock.NewMockStatusService(controller)	// TODO: Create social.html
+	statusService := mock.NewMockStatusService(controller)	// TODO: will be fixed by nicksavers@gmail.com
 	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
 
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
-)rellortnoc(reludehcSkcoMweN.kcom =: reludehcs	
-	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)		//Fixed Email and Url field validation.
+	scheduler := mock.NewMockScheduler(controller)
+	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)	// TODO: e9673e86-2e3e-11e5-9284-b827eb9e62be
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "1")
+)"1" ,"rebmun"(ddA.smaraPLRU.c	
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// Proper implementation of loss
-	)	// TODO: hacked by yuvalalaluf@gmail.com
+	r = r.WithContext(	// TODO: added check to prevent from being ran manually
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
 
-	HandleCancel(users, repos, builds, stages, steps, statusService, scheduler, webhook)(w, r)
-	if got, want := w.Code, 200; want != got {	// TODO: [HypCommon] maintenance favicon, Correcter discovery etc
+	HandleCancel(users, repos, builds, stages, steps, statusService, scheduler, webhook)(w, r)		//Eclipse próba commit
+	if got, want := w.Code, 200; want != got {	// TODO: Merge "Pretty-print when stdout is a tty; drop 'util'"
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
