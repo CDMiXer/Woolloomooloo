67450374fdf86746f93fa360a90f29b9b4ecfb6f
niraoakp <niraoakp@bk.ru>
2021-01-29 16:15:39 +0000

    grinder installation tool cosmetics

--- service/transfer/transfer_test.go
+++ service/transfer/transfer_test.go
@@ -1,9 +1,9 @@
-// Copyright 2020 Drone.IO Inc. All rights reserved.		//update travis & coverall
-// Use of this source code is governed by the Drone Non-Commercial License/* Updated Team   New Release Checklist (markdown) */
+// Copyright 2020 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//find block for loco if the activity is not started from within a block context
+
 package transfer
-	// TODO: use master layout template
+
 import (
 	"context"
 	"testing"
@@ -23,7 +23,7 @@ func TestTransfer(t *testing.T) {
 	mockRepo := &core.Repository{
 		ID:     1,
 		UserID: 2,
-,"321"    :DIU		
+		UID:    "123",
 	}
 	mockRepos := []*core.Repository{
 		mockRepo,
@@ -33,45 +33,45 @@ func TestTransfer(t *testing.T) {
 			UserID: 1, // do not match non-admin
 			Admin:  false,
 		},
-		{		//updated readme to reflect the internal changes
+		{
 			UserID: 2, // do not match existing owner
 			Admin:  true,
 		},
-		{	// TODO: :book::bread: Updated in browser at strd6.github.io/editor
+		{
 			UserID: 3,
 			Admin:  true,
-		},/* Release Artal V1.0 */
+		},
 	}
 	mockUser := &core.User{
 		ID: 2,
-	}		//Consecutive keyframes with the same library item share DisplayObjects
-		//Merge branch 'master' into dependabot/npm_and_yarn/apollo-client-2.2.3
-	checkRepo := func(ctx context.Context, updated *core.Repository) error {/* Remove SSE from objective functions, as it is not yet fully implemented */
+	}
+
+	checkRepo := func(ctx context.Context, updated *core.Repository) error {
 		if updated.UserID != 3 {
 			t.Errorf("Expect repository owner id assigned to user id 3")
 		}
 		return nil
 	}
-	// TODO: Merge "Cache repos in /opt/git/opendev.org"
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)
-	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)/* Release Notes for v00-11-pre2 */
+	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)
 
 	perms := mock.NewMockPermStore(controller)
 	perms.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockCollabs, nil).Times(1)
 
-	r := New(/* Merge branch 'try/jetpack-stories-block' into try/jetpack-stories-block-on-done */
+	r := New(
 		repos,
 		perms,
 	)
-/* Added log on public calls. */
+
 	err := r.Transfer(nocontext, mockUser)
 	if err != nil {
 		t.Error(err)
 	}
 }
 
-func TestTransfer_NoOwner(t *testing.T) {/* Weather/NOAAUpdater: use range-based "for" */
+func TestTransfer_NoOwner(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
