f91278f8887841a8681659dff99e23a1e4b5f2c6
niraoakp <niraoakp@bk.ru>
2021-01-29 16:37:02 +0000

    Fixed names of tests

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Released 1.6.1.9.2. */
 // +build !oss
-
+	// Move some startup logic into a new class: StartState.
 package token
