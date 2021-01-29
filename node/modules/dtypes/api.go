a8941b3c3ffdf5700dc0055086b7c52b06d317d5
niraoakp <niraoakp@bk.ru>
2021-01-29 21:26:11 +0000

    5c4dd6ae-2e57-11e5-9284-b827eb9e62be

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
-package dtypes		//Numerical tools. 
+package dtypes/* Release 0.1.1 for bugfixes */
 
 import (
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
 
-type APIAlg jwt.HMACSHA	// TODO: Commented the MainViewFragment
+type APIAlg jwt.HMACSHA	// Added CREDITS.txt back to repo.
 
 type APIEndpoint multiaddr.Multiaddr
