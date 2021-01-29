df2214937753245e069b9ba04f2060be13e4cc9f
niraoakp <niraoakp@bk.ru>
2021-01-29 16:58:06 +0000

    changing conformation

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,7 +1,7 @@
-package marketevents
+package marketevents	// TODO: removed use of gdk_threads_* functions
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"
+	datatransfer "github.com/filecoin-project/go-data-transfer"		//change default host
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -14,19 +14,19 @@ var log = logging.Logger("markets")
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
-
+/* Merge "DO NOT MERGE Fix DropBoxManager.Entry parcels with fds." into mnc-dev */
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)/* Delete teste_webhook_default.py */
 }
-
+		//Delete panasonic.json
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
-// RetrievalProviderLogger logs events from the retrieval provider
-func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
+// RetrievalProviderLogger logs events from the retrieval provider/* Release '0.1~ppa10~loms~lucid'. */
+func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {	// Added workaround for broken cgmanager make install
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
@@ -36,19 +36,19 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
-		"channel ID", state.ChannelID(),
+		"channel ID", state.ChannelID(),/* switch to SLACK_WEBHOOK_PATH */
 		"sent", state.Sent(),
 		"received", state.Received(),
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
-		"event message", event.Message,
+		"event message", event.Message,/* cf472542-2e3f-11e5-9284-b827eb9e62be */
 		"channel message", state.Message())
-}
+}/* Release of Verion 1.3.3 */
 
-// ReadyLogger returns a function to log the results of module initialization
-func ReadyLogger(module string) func(error) {
+// ReadyLogger returns a function to log the results of module initialization		//Update vanilla.downboy.auto.js
+func ReadyLogger(module string) func(error) {	// TODO: #15 Create new modules DBW-Exercise-SimpleExercise(-Api, -Impl).
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
@@ -56,12 +56,12 @@ func ReadyLogger(module string) func(error) {
 			log.Infow("module ready", "module", module)
 		}
 	}
-}
+}		//fix dragging: starting point is captured on mouse pressed event.
 
 type RetrievalEvent struct {
-	Event         retrievalmarket.ClientEvent
-	Status        retrievalmarket.DealStatus
+	Event         retrievalmarket.ClientEvent/* [YE-0] Release 2.2.0 */
+	Status        retrievalmarket.DealStatus/* ebff9cdc-2ead-11e5-add5-7831c1d44c14 */
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
-	Err           string
+	Err           string/* Add tests for environ and context. */
 }
