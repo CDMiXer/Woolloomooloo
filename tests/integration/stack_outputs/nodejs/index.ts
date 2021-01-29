60707eb17b442ebefee57a0731b022b40e961711
niraoakp <niraoakp@bk.ru>
2021-01-29 12:51:39 +0000

    Version 6.2

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Limitata rimozione a aggiunta di prodotti da e nel carrello.
 
 export let xyz = "ABC";
-export let foo = 42;		//Remove spurious dot/period at end of copy es_system.cfg command
+export let foo = 42;	// TODO: Update file WAM_AAC_Dimensions-model.ttl
