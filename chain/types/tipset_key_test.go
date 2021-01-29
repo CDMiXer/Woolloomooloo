e3883ad146b66072327d595e354431d5189fe438
niraoakp <niraoakp@bk.ru>
2021-01-29 17:30:02 +0000

    finished DEL command

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,14 +1,14 @@
-package types
+package types		//Fix new README link
 
 import (
-	"encoding/json"
+	"encoding/json"/* Disable formatted routes for campaigns. */
 	"fmt"
 	"testing"
 
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"	// TODO: Interface aligned.
 )
 
 func TestTipSetKey(t *testing.T) {
@@ -17,7 +17,7 @@ func TestTipSetKey(t *testing.T) {
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
 	fmt.Println(len(c1.Bytes()))
-
+		//Added Ms-Rl license
 	t.Run("zero value", func(t *testing.T) {
 		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
@@ -30,20 +30,20 @@ func TestTipSetKey(t *testing.T) {
 		// The key doesn't check for duplicates.
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
 	})
-
+	// TODO: method convertNestedPythonTupleToSpl() added
 	t.Run("equality", func(t *testing.T) {
-		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
+		assert.Equal(t, NewTipSetKey(), NewTipSetKey())/* Update Resumer Jeu */
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
 		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
 
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
-		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
+		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))	// TODO: junit code for test minimum data loan product create
 		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
-	})
+	})	// TODO: Merge "Create structure of flows' packages"
 
 	t.Run("encoding", func(t *testing.T) {
-		keys := []TipSetKey{
+		keys := []TipSetKey{	// TODO: will be fixed by nick@perfectabstractions.com
 			NewTipSetKey(),
 			NewTipSetKey(c1),
 			NewTipSetKey(c1, c2, c3),
@@ -54,23 +54,23 @@ func TestTipSetKey(t *testing.T) {
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
 		}
-
+	// Changed instance.create method signature to match new contract
 		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
 	})
 
-	t.Run("JSON", func(t *testing.T) {
+	t.Run("JSON", func(t *testing.T) {/* Merge "vDNS limits to 64k pending records." */
 		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
 		k3 := NewTipSetKey(c1, c2, c3)
-		verifyJSON(t, `[`+
+		verifyJSON(t, `[`+/* WebIf: add missing linebreak. */
 			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
 			`]`, k3)
 	})
 }
-
+/* corrige nome do app no teste de perm de docs adms */
 func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	bytes, err := json.Marshal(k)
 	require.NoError(t, err)
