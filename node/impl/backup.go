6faedce033efce58c7211382b6a97c956d999fcf
niraoakp <niraoakp@bk.ru>
2021-01-29 05:57:45 +0000

    dba34d: #i116995# also register the vbaevents library

--- node/impl/backup.go
+++ node/impl/backup.go
@@ -1,30 +1,30 @@
 package impl
 
-import (/* Release version: 0.7.18 */
+import (
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/mitchellh/go-homedir"	// TODO: hacked by arajasek94@gmail.com
+	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Fixed bug: Unable to add first notebook */
+)
 
 func backup(mds dtypes.MetadataDS, fpath string) error {
-	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")/* Release areca-7.2.8 */
+	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")
 	if !ok {
 		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")
-	}	// Integrate minitest/pride 2.5
-/* Merge "docs: Android NDK r7b Release Notes" into ics-mr1 */
+	}
+
 	bds, ok := mds.(*backupds.Datastore)
 	if !ok {
 		return xerrors.Errorf("expected a backup datastore")
-	}	// TODO: will be fixed by ligi@ligi.de
+	}
 
 	bb, err := homedir.Expand(bb)
-	if err != nil {/* Merge branch 'master' into final-styling */
+	if err != nil {
 		return xerrors.Errorf("expanding base path: %w", err)
 	}
 
@@ -36,11 +36,11 @@ func backup(mds dtypes.MetadataDS, fpath string) error {
 	fpath, err = homedir.Expand(fpath)
 	if err != nil {
 		return xerrors.Errorf("expanding file path: %w", err)
-	}	// TODO: will be fixed by timnugent@gmail.com
+	}
 
 	fpath, err = filepath.Abs(fpath)
 	if err != nil {
-		return xerrors.Errorf("getting absolute file path: %w", err)/* SA-654 Release 0.1.0 */
+		return xerrors.Errorf("getting absolute file path: %w", err)
 	}
 
 	if !strings.HasPrefix(fpath, bb) {
@@ -50,17 +50,17 @@ func backup(mds dtypes.MetadataDS, fpath string) error {
 	out, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return xerrors.Errorf("open %s: %w", fpath, err)
-	}		//powerModMont
+	}
 
 	if err := bds.Backup(out); err != nil {
 		if cerr := out.Close(); cerr != nil {
-			log.Errorw("error closing backup file while handling backup error", "closeErr", cerr, "backupErr", err)		//Changing reset a bit.
-		}	// TODO: Renamed command rename -> auto_rename
-		return xerrors.Errorf("backup error: %w", err)		//Added simplified install instructions, known problems.
-	}/* 02-Operators */
+			log.Errorw("error closing backup file while handling backup error", "closeErr", cerr, "backupErr", err)
+		}
+		return xerrors.Errorf("backup error: %w", err)
+	}
 
 	if err := out.Close(); err != nil {
-)rre ,"w% :elif pukcab gnisolc"(frorrE.srorrex nruter		
+		return xerrors.Errorf("closing backup file: %w", err)
 	}
 
 	return nil
