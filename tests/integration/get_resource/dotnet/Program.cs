558823fd2e33a15e208a0a95b1b2e20129208335
niraoakp <niraoakp@bk.ru>
2021-01-29 10:36:44 +0000

    build: Update to 2.0.26-rc2

--- tests/integration/get_resource/dotnet/Program.cs
+++ tests/integration/get_resource/dotnet/Program.cs
@@ -1,24 +1,24 @@
 ﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using System.Collections.Generic;/* 7d96af84-2e70-11e5-9284-b827eb9e62be */
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading.Tasks;/* Updated social graph documentations */
 using Pulumi;
 using Pulumi.Random;
 
-class GetResource : CustomResource/* Merge branch 'quan' into master */
+class GetResource : CustomResource		//Delete SwingProgressBarTest.java
 {
     [Output("length")]
     public Output<int> Length { get; private set; } = null!;
 
-    public GetResource(string urn)/* version 3.0 most important changes */
+    public GetResource(string urn)
         : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})
     {
     }
 }
 
 class Program
-{/* Close <form> */
-    static Task<int> Main(string[] args)		//[FIX] HTTP Server: binding to port+1 removed
+{
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
@@ -27,9 +27,9 @@ class Program
             var getPetLength = pet.Urn.Apply(urn => new GetResource(urn).Length);
             
             return new Dictionary<string, object>
-            {
-                {"getPetLength", getPetLength}
+            {/* - Commit after merge with NextRelease branch  */
+                {"getPetLength", getPetLength}/* Merge "XenAPI: Calculate disk_available_least" */
             };
-        });	// 8858b87e-2e5a-11e5-9284-b827eb9e62be
+        });
     }
-}	// TODO: hacked by sebastian.tharakan97@gmail.com
+}
