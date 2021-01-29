5adcb61da798b48f94168b177a481afd6dc9ce16
niraoakp <niraoakp@bk.ru>
2021-01-29 17:44:34 +0000

    Add footer template to homepage

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,32 +1,32 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Following to commit 1e0f71b1b - Fix typo in `randombox` auto-completion
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Merge "Release 1.0.0.243 QCACLD WLAN Driver" */
+import * as dynamic from "@pulumi/pulumi/dynamic";		//cucko hash table working delete and reinserting
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//Software Engineer (XCode)
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    // Ensure that the arrow in the following comment does not throw		//Update Engine_BuiltInShaders.cpp
-    //  off how Pulumi serializes classes/functions./* tests so far. */
-    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-		//f6b4c77a-2e61-11e5-9284-b827eb9e62be
+    // Ensure that the arrow in the following comment does not throw
+    //  off how Pulumi serializes classes/functions.
+    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// TODO: Update GhostHunt.py
+
     constructor() {
         this.create = async (inputs: any) => {
-            return {/* Merge "Correct display of warnings in Deploy Changes dialog" */
+            return {
                 id: "0",
-                outs: undefined,	// TODO: Add error_log
+                outs: undefined,
             };
         };
-    }	// TODO: jekyll-logo
-}
-
-class SimpleResource extends dynamic.Resource {/* Refactored JMudObjectUtils.adopt() to .changeParent() */
-    public value = 4;/* [#4590] Allow multiple connections to be defined for master/slave configurations */
-
-    constructor(name: string) {		//minor heading tweak
-        super(new SimpleProvider(), name, {}, undefined);	// RE #26572 Save button disabled after algorithm is launched
     }
 }
-/* Released 1.1.13 */
-let r = new SimpleResource("foo");	// TODO: will be fixed by boringland@protonmail.ch
+
+class SimpleResource extends dynamic.Resource {
+    public value = 4;
+
+    constructor(name: string) {
+        super(new SimpleProvider(), name, {}, undefined);/* Created Release version */
+    }
+}
+
+let r = new SimpleResource("foo");
 export const val = r.value;
