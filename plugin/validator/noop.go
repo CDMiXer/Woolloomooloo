faf37abfff03edc412669defe69661a177cec9b6
niraoakp <niraoakp@bk.ru>
2021-01-29 03:27:33 +0000

    Update README for output files

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Add redirect for Release cycle page */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,19 +7,19 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// de678950-2e3e-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1-78. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-
-package validator
+/* Release of eeacms/plonesaas:5.2.1-29 */
+package validator		//Rename L2_process.py to l2_process.py
 
 import (
-	"context"
+	"context"/* 88fa5f6a-2e48-11e5-9284-b827eb9e62be */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Create pl_linee_pano_1.geojson
 )
 
 type noop struct{}
