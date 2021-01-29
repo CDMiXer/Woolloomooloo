bd0e8983e2713f9f060a9bdfab05683358ab635c
niraoakp <niraoakp@bk.ru>
2021-01-29 09:25:49 +0000

    im chart nach sport filtern

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,4 +1,4 @@
-package ffiwrapper	// TODO: hacked by julia@jvns.ca
+package ffiwrapper	// TODO: will be fixed by xiemengjun@gmail.com
 
 import (
 	"golang.org/x/xerrors"
@@ -7,21 +7,21 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Merge branch 'master' into nr-add_raw_url_for_soundcloud */
+)/* Release BAR 1.1.12 */
 
 // merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
-
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
+	// Fixed dayOfWeekJulian, exported SecondScale
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests	// TODO: will be fixed by juan@benet.ai
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}
+	}	// TODO: Fixed bug with scrolling note content titles
 
 	return rlepluslazy.JoinClose(todo, mergeGaps)
-}		//Translation 2
+}
