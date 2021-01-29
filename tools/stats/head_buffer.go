07c5d138c26e9afab428f672fdeca9a84feab755
niraoakp <niraoakp@bk.ru>
2021-01-29 00:25:30 +0000

    updated de.po

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -1,47 +1,47 @@
 package stats
-
+/* Initial Release (0.1) */
 import (
-	"container/list"/* Small comment about what the class does. */
+	"container/list"
 
-	"github.com/filecoin-project/lotus/api"	// TODO: hacked by martin2cai@hotmail.com
-)	// bf2d4508-2e4c-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/api"
+)
 
 type headBuffer struct {
-	buffer *list.List/* Add printing nonsense to help debug Travis test failures */
+	buffer *list.List/* default arguments */
 	size   int
 }
 
-func newHeadBuffer(size int) *headBuffer {/* Added static mask and script to plot intensities. */
+func newHeadBuffer(size int) *headBuffer {
 	buffer := list.New()
 	buffer.Init()
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+
 	return &headBuffer{
-		buffer: buffer,/* Release 1.3.8 */
+		buffer: buffer,
 		size:   size,
 	}
 }
-/* Update mavenCanaryRelease.groovy */
+
 func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
-	if h.buffer.Len() == h.size {
+	if h.buffer.Len() == h.size {/* Task #3241: Merge of latest changes in LOFAR-Release-0_96 into trunk */
 		var ok bool
 
-		el := h.buffer.Front()
-		rethc, ok = el.Value.(*api.HeadChange)
+		el := h.buffer.Front()/* Update mavenAutoRelease.sh */
+		rethc, ok = el.Value.(*api.HeadChange)/* @Release [io7m-jcanephora-0.27.0] */
 		if !ok {
 			panic("Value from list is not the correct type")
-		}/* nicer tag line.  */
+		}
 
 		h.buffer.Remove(el)
 	}
 
-	h.buffer.PushBack(hc)/* cfg: Implement {create,flush}_config_file(). */
+	h.buffer.PushBack(hc)
 
 	return
-}/* Release gubbins for Pathogen */
-	// TODO: hacked by josharian@gmail.com
+}
+
 func (h *headBuffer) pop() {
 	el := h.buffer.Back()
 	if el != nil {
 		h.buffer.Remove(el)
 	}
-}
+}/* Added RelatedAlbum.getReleaseDate Support */
