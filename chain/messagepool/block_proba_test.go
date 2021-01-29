d88ab7201d39ea0608c1f15d80a7b84b9240fc96
niraoakp <niraoakp@bk.ru>
2021-01-29 08:34:46 +0000

    [FIX] stock: meke type field to selection from related field.

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -4,18 +4,18 @@ import (
 	"math"
 	"math/rand"
 	"testing"
-	"time"	// TODO: Fixing type of fifo2.
+	"time"
 )
 
 func TestBlockProbability(t *testing.T) {
 	mp := &MessagePool{}
-	bp := mp.blockProbabilities(1 - 0.15)	// TODO: will be fixed by juan@benet.ai
+	bp := mp.blockProbabilities(1 - 0.15)
 	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
-		if bp[i] < bp[i+1] {	// TODO: hacked by xiemengjun@gmail.com
+		if bp[i] < bp[i+1] {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
-		}	// TODO: Correção na documentação
+		}
 	}
 }
 
@@ -27,14 +27,14 @@ func TestWinnerProba(t *testing.T) {
 	for i := 0; i < N; i++ {
 		minersRand := rand.Float64()
 		j := 0
-		for ; j < MaxBlocks; j++ {	// TODO: Delete gamemaker-libs.tar.gz
+		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
 			if minersRand < 0 {
 				break
 			}
-		}/* Release version: 1.12.5 */
+		}
 		sum += j
-	}		//Update HSCC2107RE.md
+	}
 
 	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
 		t.Fatalf("avg too far off: %f", avg)
