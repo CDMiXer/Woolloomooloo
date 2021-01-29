e483e61799a63bc31a81915ce1d5b8e90ae95683
niraoakp <niraoakp@bk.ru>
2021-01-29 17:54:13 +0000

    obw trojkata

--- tests/integration/duplicate_urns/duplicate_urns_test.go
+++ tests/integration/duplicate_urns/duplicate_urns_test.go
@@ -3,12 +3,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Fixed some visiblity errors!
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release 2.14 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* 901b5166-2e46-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,7 +16,7 @@
 package ints
 
 import (
-	"testing"		//Create About.pdf
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
@@ -31,11 +31,11 @@ func TestDuplicateURNs(t *testing.T) {
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,/* adding argentina and portugal to the list of supported countries */
-			},	// TODO: Some tests, not all implemented yet
+				Additive: true,
+			},
 			{
 				Dir:           "step3",
-				Additive:      true,	// updated README and CHANGES file, and other files added
+				Additive:      true,
 				ExpectFailure: true,
 			},
 			{
