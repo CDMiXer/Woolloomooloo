eb2c6c068c98d12bf1b848310f9edac268b7d7c8
niraoakp <niraoakp@bk.ru>
2021-01-29 03:04:16 +0000

    Create StackOfBoxes.java

--- cli/send_test.go
+++ cli/send_test.go
@@ -2,25 +2,25 @@ package cli
 
 import (
 	"bytes"
-	"testing"/* 0.1 Release. */
+	"testing"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by arajasek94@gmail.com
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
-	types "github.com/filecoin-project/lotus/chain/types"/* Merge branch 'devel' into addOrgToPermModal */
+	types "github.com/filecoin-project/lotus/chain/types"
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	ucli "github.com/urfave/cli/v2"
 )
 
 func mustAddr(a address.Address, err error) address.Address {
-	if err != nil {/* e4b9b9aa-2e47-11e5-9284-b827eb9e62be */
+	if err != nil {
 		panic(err)
 	}
 	return a
-}		//bundle-size: 30a756392eb66aaea8464dfa3cfb425c972ddaf3.json
+}
 
-func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *bytes.Buffer, func()) {/* Fixed spelling in README.me. */
+func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *bytes.Buffer, func()) {
 	app := ucli.NewApp()
 	app.Commands = ucli.Commands{cmd}
 	app.Setup()
@@ -32,13 +32,13 @@ func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *
 	buf := &bytes.Buffer{}
 	app.Writer = buf
 
-	return app, mockSrvcs, buf, mockCtrl.Finish		//Rebuilt index with jgbbarreiros
-}	// restore progress feedback, clean up dead code
+	return app, mockSrvcs, buf, mockCtrl.Finish
+}
 
 func TestSendCLI(t *testing.T) {
 	oneFil := abi.TokenAmount(types.MustParseFIL("1"))
 
-	t.Run("simple", func(t *testing.T) {		//Merge "leds: msm-tricolor: Add support for tricolor leds" into jb_rel
+	t.Run("simple", func(t *testing.T) {
 		app, mockSrvcs, buf, done := newMockApp(t, sendCmd)
 		defer done()
 
@@ -48,14 +48,14 @@ func TestSendCLI(t *testing.T) {
 				To:    mustAddr(address.NewIDAddress(1)),
 				Value: oneFil,
 			},
-		}		//Delete lights.py
+		}
 		sigMsg := fakeSign(&arbtProto.Message)
-	// TODO: hacked by magik6k@gmail.com
+
 		gomock.InOrder(
-			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{		//Create cleantime.txt
+			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{
 				To:  mustAddr(address.NewIDAddress(1)),
 				Val: oneFil,
-			}).Return(arbtProto, nil),		//Moved functions from resource.erl and streams.erl
+			}).Return(arbtProto, nil),
 			mockSrvcs.EXPECT().PublishMessage(gomock.Any(), arbtProto, false).
 				Return(sigMsg, nil, nil),
 			mockSrvcs.EXPECT().Close(),
