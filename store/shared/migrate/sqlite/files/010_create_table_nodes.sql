6ee699bf89350db92e25526e1f7f8c0dcbfd686e
niraoakp <niraoakp@bk.ru>
2021-01-29 07:44:42 +0000

    Added Python 3.3 test environment to tox.

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -1,21 +1,21 @@
 -- name: create-table-nodes
 
 CREATE TABLE IF NOT EXISTS nodes (
- node_id         INTEGER PRIMARY KEY AUTOINCREMENT		//Update tundra_regularization.r
-,node_uid        TEXT	// TODO: LDEV-4440 switch survey to nostruts library
-,node_provider   TEXT		//Merge branch 'master' into chore/improve-code-quality
+ node_id         INTEGER PRIMARY KEY AUTOINCREMENT
+,node_uid        TEXT	// TODO: Merge branch 'master' into chipster-2.0_scripts
+,node_provider   TEXT
 ,node_state      TEXT
 ,node_name       TEXT
-,node_image      TEXT	// Avoid reconnecting when pushing.
+,node_image      TEXT
 ,node_region     TEXT
-,node_size       TEXT
+,node_size       TEXT		//language error
 ,node_os         TEXT
 ,node_arch       TEXT
 ,node_kernel     TEXT
 ,node_variant    TEXT
 ,node_address    TEXT
 ,node_capacity   INTEGER
-,node_filter     TEXT
+,node_filter     TEXT		//Merge "Fix docker volumes binds issue"
 ,node_labels     TEXT
 ,node_error      TEXT
 ,node_ca_key     TEXT
@@ -23,11 +23,11 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_tls_key    TEXT
 ,node_tls_cert   TEXT
 ,node_tls_name   TEXT
-,node_paused     BOOLEAN	// added page for NW results
-,node_protected  BOOLEAN	// Fix websocket clean up
+,node_paused     BOOLEAN		//211702dc-2d5c-11e5-96c6-b88d120fff5e
+,node_protected  BOOLEAN
 ,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
-
-,UNIQUE(node_name)/* Import Upstream version 0.0+r3073 */
+/* Fix #11: handle large number, i.e. 900K+. */
+,UNIQUE(node_name)
 );
