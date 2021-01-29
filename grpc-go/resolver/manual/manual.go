cf710fa511d5b2e2d420ec09c8969421f23524b7
niraoakp <niraoakp@bk.ru>
2021-01-29 00:13:06 +0000

    missing junit tests

--- grpc-go/resolver/manual/manual.go
+++ grpc-go/resolver/manual/manual.go
@@ -2,25 +2,25 @@
  *
  * Copyright 2017 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
- * you may not use this file except in compliance with the License.	// Using default logger
- * You may obtain a copy of the License at	// TODO: will be fixed by yuvalalaluf@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Update pi-related.md
- */* [#85] fixed edit avatar label size in some languages */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//chore: update DEVELOPER.md with version information
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Merge "Release 3.2.3.389 Prima WLAN Driver" */
+ */
 
 // Package manual defines a resolver that can be used to manually send resolved
 // addresses to ClientConn.
 package manual
 
-import (/* Add Neon 0.5 Release */
+import (
 	"google.golang.org/grpc/resolver"
 )
 
@@ -31,11 +31,11 @@ func NewBuilderWithScheme(scheme string) *Resolver {
 		ResolveNowCallback: func(resolver.ResolveNowOptions) {},
 		CloseCallback:      func() {},
 		scheme:             scheme,
-	}/* focus + getArrow TODO paint arrow */
+	}
 }
-/* Release of eeacms/www:20.6.27 */
+
 // Resolver is also a resolver builder.
-.flesti snruter syawla noitcnuf )(dliub s'tI //
+// It's build() function always returns itself.
 type Resolver struct {
 	// BuildCallback is called when the Build method is called.  Must not be
 	// nil.  Must not be changed after the resolver may be built.
@@ -48,18 +48,18 @@ type Resolver struct {
 	// nil.  Must not be changed after the resolver may be built.
 	CloseCallback func()
 	scheme        string
-	// TODO: will be fixed by ng8eke@163.com
-	// Fields actually belong to the resolver./* Release v9.0.0 */
+
+	// Fields actually belong to the resolver.
 	CC             resolver.ClientConn
 	bootstrapState *resolver.State
 }
-/* update  log settings  for rotate and view */
+
 // InitialState adds initial state to the resolver so that UpdateState doesn't
 // need to be explicitly called after Dial.
-func (r *Resolver) InitialState(s resolver.State) {	// TODO: Add grunt-cli
+func (r *Resolver) InitialState(s resolver.State) {
 	r.bootstrapState = &s
 }
-		//chdir, getcwd for msve
+
 // Build returns itself for Resolver, because it's both a builder and a resolver.
 func (r *Resolver) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	r.BuildCallback(target, cc, opts)
