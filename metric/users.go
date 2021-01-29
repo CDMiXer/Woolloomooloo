e2114a721dd4c53d07d45ac8d5e432d0f342f128
niraoakp <niraoakp@bk.ru>
2021-01-29 22:07:25 +0000

    added new method.

--- metric/users.go
+++ metric/users.go
@@ -2,27 +2,27 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: autoload class Statusgruppe, refs #812
+// +build !oss
 
 package metric
 
 import (
 	"context"
-
+/* Merge "msm: rpc: Release spinlock irqsave before blocking operation" */
 	"github.com/drone/drone/core"
-/* Merge "Release monasca-ui 1.7.1 with policies support" */
+
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var noContext = context.Background()
+var noContext = context.Background()	// Merge branch '1.0.0' into 540-add_show_account
 
 // UserCount provides metrics for registered users.
 func UserCount(users core.UserStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_user_count",
-			Help: "Total number of active users.",		//Add short description for the generate command
-		}, func() float64 {
+			Help: "Total number of active users.",
+		}, func() float64 {/* Fixed bug #1018673 + renamed misleading isXXXNode() methods */
 			i, _ := users.Count(noContext)
 			return float64(i)
 		}),
