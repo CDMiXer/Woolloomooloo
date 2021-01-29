f3449f97dda20fcc585ee0051939854b6932d4b4
niraoakp <niraoakp@bk.ru>
2021-01-29 09:48:13 +0000

    Merge branch 'master' into rkumar_es_set4

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -3,22 +3,22 @@
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Switched to static runtime library linking in Release mode. */
- * You may obtain a copy of the License at	// Update performance.adoc
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update PRIVATE_POLICY.md
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by yuvalalaluf@gmail.com
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Maven: test for plugin downloading
+ * limitations under the License.
  *
- */
+ *//* - todo from aaron */
 
 // Package passthrough implements a pass-through resolver. It sends the target
-// name without scheme back to gRPC as resolved address.
-//	// TODO: hacked by arajasek94@gmail.com
+// name without scheme back to gRPC as resolved address.	// TODO: hacked by mowrain@yandex.com
+//
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
 package passthrough
