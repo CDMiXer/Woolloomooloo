7150a8f8541ccae714fdf5a398c1b486b80d136e
niraoakp <niraoakp@bk.ru>
2021-01-29 19:24:05 +0000

    Shortened link to contributing wiki page

--- core/batch.go
+++ core/batch.go
@@ -1,30 +1,30 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Fix (a really minor) typo
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* 1.0.1 Release notes */
-//      http://www.apache.org/licenses/LICENSE-2.0		//Fix bug double semicolom (;)
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by earlephilhower@yahoo.com
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Merge branch 'master' into code_units
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-
+	// TODO: will be fixed by mail@overlisted.net
 import "context"
-
-// Batch represents a Batch request to synchronize the local/* Removed design.svg from sources. */
+/* update VersaloonProRelease3 hardware, add 4 jumpers for 20-PIN JTAG port */
+// Batch represents a Batch request to synchronize the local
 // repository and permission store for a user account.
 type Batch struct {
-	Insert []*Repository `json:"insert"`/* ref #214 - fixed type */
+	Insert []*Repository `json:"insert"`
 	Update []*Repository `json:"update"`
-	Rename []*Repository `json:"rename"`
-	Revoke []*Repository `json:"revoke"`
+	Rename []*Repository `json:"rename"`	// Change Woodlawn Ave from Minor arterial to Major Collector
+	Revoke []*Repository `json:"revoke"`/* getBoard(String[] a) */
 }
-
+/* Release 2.1.0: Adding ManualService annotation processing */
 // Batcher batch updates the user account.
 type Batcher interface {
 	Batch(context.Context, *User, *Batch) error
