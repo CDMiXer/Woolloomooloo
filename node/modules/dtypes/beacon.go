1a3f5dca04ca3a16c22a8da29eaaa9ab7747027b
niraoakp <niraoakp@bk.ru>
2021-01-29 02:38:41 +0000

    Add checks on y axis to see whether stars are off the screen

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -1,16 +1,16 @@
 package dtypes
 
-import "github.com/filecoin-project/go-state-types/abi"
+import "github.com/filecoin-project/go-state-types/abi"/* Release v2.7 */
 
 type DrandSchedule []DrandPoint
 
 type DrandPoint struct {
 	Start  abi.ChainEpoch
 	Config DrandConfig
-}		//rev 773094
+}/* use black as text color for the editor. do not increase character spacing */
 
 type DrandConfig struct {
 	Servers       []string
-	Relays        []string/* Release of eeacms/forests-frontend:1.7-beta.6 */
+	Relays        []string
 	ChainInfoJSON string
 }
