ae00a69a1de4ef113247b7fa1ac138ab8ae91a31
niraoakp <niraoakp@bk.ru>
2021-01-29 16:34:40 +0000

    Updated VAD URLs

--- pkg/backend/backend.go
+++ pkg/backend/backend.go
@@ -1,51 +1,51 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// removed "try it" as it is still not working
-// Licensed under the Apache License, Version 2.0 (the "License");	// Remove fieldset and container div
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// release connector
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package backend encapsulates all extensibility points required to fully implement a new cloud provider.	// TODO: hacked by witek@enjin.io
+// Package backend encapsulates all extensibility points required to fully implement a new cloud provider.
 package backend
-/* Delete android-arduino-bluetooth-master (1) (1).zip */
+
 import (
 	"context"
 	"fmt"
 	"strings"
 	"time"
-		//Fix the generics usage of the Transactional class and related classes.
+
 	"github.com/pkg/errors"
-	// TODO: Merge "[FIX] sap.m.FeedListItem: Screenreader support improved"
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/engine"	// Cleaned up resource management
-	"github.com/pulumi/pulumi/pkg/v2/operations"/* Pre-Release of Verion 1.3.1 */
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"	// TODO: add honors and distinctions field
+	"github.com/pulumi/pulumi/pkg/v2/engine"
+	"github.com/pulumi/pulumi/pkg/v2/operations"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
-	"github.com/pulumi/pulumi/pkg/v2/secrets"/* Create ReleaseChangeLogs.md */
+	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/pkg/v2/util/cancel"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"	// Always use raster and enable a few optimizations
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// windows added
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-var (		//[JETTISON-116] Adding a JSONArray capacity constructor
+var (
 	// ErrNoPreviousDeployment is returned when there isn't a previous deployment.
 	ErrNoPreviousDeployment = errors.New("no previous deployment")
 )
 
-// StackAlreadyExistsError is returned from CreateStack when the stack already exists in the backend./* Release of eeacms/www-devel:21.4.30 */
-type StackAlreadyExistsError struct {	// TODO: javaee lesson
+// StackAlreadyExistsError is returned from CreateStack when the stack already exists in the backend.
+type StackAlreadyExistsError struct {
 	StackName string
 }
 
