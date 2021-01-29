0238b551389abc4ee015cafb4af62f1ec1f38452
niraoakp <niraoakp@bk.ru>
2021-01-29 16:01:16 +0000

    #CHANGE: unified exception handling

--- api/api_worker.go
+++ api/api_worker.go
@@ -3,48 +3,48 @@ package api
 import (
 	"context"
 	"io"
-/* StatusBar: Release SoundComponent on exit. */
-	"github.com/google/uuid"
-	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"		//fixed broken table in schedule.add
+"diuu/elgoog/moc.buhtig"	
+	"github.com/ipfs/go-cid"/* added documentation for healthchecks */
+
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* Release 4.1.0: Adding Liquibase Contexts configuration possibility */
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* added WebUtilsService.getWikiRef() */
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// TODO: Delete MiniCCTest.ino
+	"github.com/filecoin-project/specs-storage/storage"	// TODO: Bumping version to 0.12.0
 )
 
-//                       MODIFYING THE API INTERFACE		//simple instructions
-//
+//                       MODIFYING THE API INTERFACE		//Update SpreadsheetViewTable
+//	// TODO: hacked by xaber.twt@gmail.com
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:
+// * Run `make gen` - this will:/* chore(deps): update dependency jest-enzyme to v5.0.1 */
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
-//  * Generate openrpc blobs		//Added define for swap extension.
-
-type Worker interface {		//Set w3c mode to false for tests
+//  * Generate openrpc blobs
+/* Release 2.7.0 */
+type Worker interface {
 	Version(context.Context) (Version, error) //perm:admin
 
 	// TaskType -> Weight
-	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin	// TODO: Added functionality to search Google Scholar when pressing backslash
+	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
 	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin
 	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin
 
-	// storiface.WorkerCalls/* Release 0.2.2 */
-	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin/* Released springjdbcdao version 1.7.4 */
+	// storiface.WorkerCalls
+	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin
 	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
-	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin/* Remove link to missing ReleaseProcess.md */
-	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin/* Create Ext Folder */
-	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin/* move docs around */
+	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
+	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
+	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin/* Release notes for ringpop-go v0.5.0. */
 	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin
 	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin
-	Fetch(context.Context, storage.SectorRef, storiface.SectorFileType, storiface.PathType, storiface.AcquireMode) (storiface.CallID, error)                                                             //perm:admin/* b089173a-2e5a-11e5-9284-b827eb9e62be */
+	Fetch(context.Context, storage.SectorRef, storiface.SectorFileType, storiface.PathType, storiface.AcquireMode) (storiface.CallID, error)                                                             //perm:admin	// TODO: hacked by boringland@protonmail.ch
 
 	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin
 	TaskEnable(ctx context.Context, tt sealtasks.TaskType) error  //perm:admin
@@ -52,10 +52,10 @@ type Worker interface {		//Set w3c mode to false for tests
 	// Storage / Other
 	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
-	StorageAddLocal(ctx context.Context, path string) error //perm:admin		//added new module for adding new exp analysis chunks
-	// Merge branch 'develop' into tpd-1458/daniel-special-links-show-url-fix
-	// SetEnabled marks the worker as enabled/disabled. Not that this setting
-	// may take a few seconds to propagate to task scheduler
+	StorageAddLocal(ctx context.Context, path string) error //perm:admin
+
+	// SetEnabled marks the worker as enabled/disabled. Not that this setting/* add section on other symbols */
+	// may take a few seconds to propagate to task scheduler	// TODO: fix test  (pt II) refs #3761
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
 
 	Enabled(ctx context.Context) (bool, error) //perm:admin
@@ -63,12 +63,12 @@ type Worker interface {		//Set w3c mode to false for tests
 	// WaitQuiet blocks until there are no tasks running
 	WaitQuiet(ctx context.Context) error //perm:admin
 
-	// returns a random UUID of worker session, generated randomly when worker
+	// returns a random UUID of worker session, generated randomly when worker/* Add Release heading to ChangeLog. */
 	// process starts
 	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
-
+/* Release AutoRefactor 1.2.0 */
 	// Like ProcessSession, but returns an error when worker is disabled
 	Session(context.Context) (uuid.UUID, error) //perm:admin
 }
 
-var _ storiface.WorkerCalls = *new(Worker)
+var _ storiface.WorkerCalls = *new(Worker)/* Merge "Wlan:  Release 3.8.20.23" */
