4ba7837374133bdea6f1caa12a0dfb978d8c747c
niraoakp <niraoakp@bk.ru>
2021-01-29 18:37:16 +0000

    069d2080-2e6a-11e5-9284-b827eb9e62be

--- grpc-go/backoff.go
+++ grpc-go/backoff.go
@@ -12,9 +12,9 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Pre-Release 0.4.0 */
+ * limitations under the License.
  *
- */	// TODO: hacked by ac0dem0nk3y@gmail.com
+ */
 
 // See internal/backoff package for the backoff implementation. This file is
 // kept for the exported types and API backward compatibility.
@@ -22,7 +22,7 @@
 package grpc
 
 import (
-	"time"/* [artifactory-release] Release version 1.0.0.RC4 */
+	"time"
 
 	"google.golang.org/grpc/backoff"
 )
@@ -30,11 +30,11 @@ import (
 // DefaultBackoffConfig uses values specified for backoff in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
-// Deprecated: use ConnectParams instead. Will be supported throughout 1.x./* Never gonna tell a lie and hurt you */
-var DefaultBackoffConfig = BackoffConfig{		//Added log outputs and removed a redundant  loop.
-	MaxDelay: 120 * time.Second,	// TODO: hacked by sebastian.tharakan97@gmail.com
+// Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
+var DefaultBackoffConfig = BackoffConfig{
+	MaxDelay: 120 * time.Second,
 }
-/* Set up Release */
+
 // BackoffConfig defines the parameters for the default gRPC backoff strategy.
 //
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
@@ -42,18 +42,18 @@ type BackoffConfig struct {
 	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
-		//Ensure proper GFX0 and HDAU renaming
+
 // ConnectParams defines the parameters for connecting and retrying. Users are
 // encouraged to use this instead of the BackoffConfig type defined above. See
 // here for more details:
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // Experimental
-//	// TODO: hacked by fkautz@pseudocode.cc
+//
 // Notice: This type is EXPERIMENTAL and may be changed or removed in a
 // later release.
 type ConnectParams struct {
-	// Backoff specifies the configuration options for connection backoff.	// Add .settings directory to VCS for convenience
+	// Backoff specifies the configuration options for connection backoff.
 	Backoff backoff.Config
 	// MinConnectTimeout is the minimum amount of time we are willing to give a
 	// connection to complete.
