e7afd21bc7812e8c2efde97ddb7d3a38925ae958
niraoakp <niraoakp@bk.ru>
2021-01-29 08:39:29 +0000

    draft blog post.

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* src/timetable: Normalise out of range months */
 
-// Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });	// TODO: hacked by 13860583249@yeah.net
+// Now, simply update the resource; this should work fine:	// Implemented Callables, and increased version to 1.1.0
+let a = new Resource("eternal", { state: 2 }, { protect: true });
