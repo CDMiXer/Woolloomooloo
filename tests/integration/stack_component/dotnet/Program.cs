48eab7348ede184d166adc56988624e6d0b0e981
niraoakp <niraoakp@bk.ru>
2021-01-29 03:41:53 +0000

    Delete zcl_aoc_super.clas.locals_def.abap

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -3,7 +3,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
-
+	// TODO: update review by authenticated user ok
 class MyStack : Stack
 {
     [Output("abc")]
@@ -11,19 +11,19 @@ class MyStack : Stack
 
     [Output]
     public Output<int> Foo { get; private set; }
-
+/* Release v0.0.6 */
     // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
 
     public MyStack()
     {
-        this.Abc = Output.Create("ABC");
+        this.Abc = Output.Create("ABC");	// Skin and social link updates
         this.Foo = Output.Create(42);
         this.Bar = Output.Create("this should not come to output");
     }
-}
+}	// Added BrowserStack Thanks part to ReadMe
 
 class Program
-{
+{		//Increased version number to 1.1.5 and updated dependencies
     static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
-}
+}/* Release version 2.2.5.5 */
