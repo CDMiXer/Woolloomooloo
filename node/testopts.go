11ae02b4c9242fa95080c820953d2aebb458fc46
niraoakp <niraoakp@bk.ru>
2021-01-29 03:08:58 +0000

    Delete ToyDiff_fixed.xlsx

--- node/testopts.go
+++ node/testopts.go
@@ -6,11 +6,11 @@ import (
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)	// TODO: Delete design introduction.txt
+)
 
 func MockHost(mn mocknet.Mocknet) Option {
-	return Options(/* Merge "Release 3.0.10.027 Prima WLAN Driver" */
-		ApplyIf(func(s *Settings) bool { return !s.Online },		//Merge branch 'master' into asonix/reorganize-types
+	return Options(
+		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
 		),
 
