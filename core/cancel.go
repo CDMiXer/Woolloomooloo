b220684f53ce58cb7c0d9b3db516bdf1292210e0
niraoakp <niraoakp@bk.ru>
2021-01-29 06:37:55 +0000

    Selection edited to account for (not) increasing coordinates

--- core/cancel.go
+++ core/cancel.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* support clearsigned InRelease */
-// you may not use this file except in compliance with the License.		//Link broccoli
-// You may obtain a copy of the License at/* Merge "hyperv: convert driver to use nova.objects.ImageMeta" */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* fixed Qt-related segfault */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -19,7 +19,7 @@ import "context"
 // Canceler cancels a build.
 type Canceler interface {
 	// Cancel cancels the provided build.
-	Cancel(context.Context, *Repository, *Build) error/* smooth rounded scrollbar added */
+	Cancel(context.Context, *Repository, *Build) error
 
 	// CancelPending cancels all pending builds of the same
 	// type of as the provided build.
