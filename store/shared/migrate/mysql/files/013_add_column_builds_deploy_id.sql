e58afbff47946ec2bb751086eb4632191fbfc34f
niraoakp <niraoakp@bk.ru>
2021-01-29 17:37:35 +0000

    Merge "[INTERNAL] sap.ui.documentation: Fixed comma list layout"

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
 
-ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;		//Update and rename canvas.html to attackOfTheSpaceCat.html
+ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
