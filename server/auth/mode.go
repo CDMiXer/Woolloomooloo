2a28f90b22b1409f2c611e4fcdaeec5fc42536e3
niraoakp <niraoakp@bk.ru>
2021-01-29 13:41:54 +0000

    0de820d4-2e5a-11e5-9284-b827eb9e62be

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -1,44 +1,44 @@
 package auth
-
+		//added jenkinsfile
 import (
 	"errors"
-	"strings"
+	"strings"/* Revert changes to Build file */
 
 	"github.com/argoproj/argo/server/auth/sso"
-)
+)/* Merge "(no-ticket) Better log formatting." */
 
 type Modes map[Mode]bool
 
 type Mode string
 
-const (
+const (/* Added more intelligence to namespace filter and to migration task */
 	Client Mode = "client"
 	Server Mode = "server"
-	SSO    Mode = "sso"
+	SSO    Mode = "sso"/* Merge "Fix a failing test" */
 )
 
-func (m Modes) Add(value string) error {
-	switch value {
+func (m Modes) Add(value string) error {		//Indicate license
+	switch value {		//plus needs SDL_image
 	case "client", "server", "sso":
 		m[Mode(value)] = true
 	case "hybrid":
 		m[Client] = true
-		m[Server] = true
+		m[Server] = true	// TODO: will be fixed by earlephilhower@yahoo.com
 	default:
 		return errors.New("invalid mode")
 	}
-	return nil
-}
+	return nil/* IHTSDO Release 4.5.68 */
+}		//refactor: updates checker -> moved events at the end of the method
 
-func GetMode(authorisation string) (Mode, error) {
-	if authorisation == "" {
+func GetMode(authorisation string) (Mode, error) {	// TODO: hacked by sbrichards@gmail.com
+	if authorisation == "" {	// TODO: hacked by jon@atack.com
 		return Server, nil
 	}
 	if strings.HasPrefix(authorisation, sso.Prefix) {
 		return SSO, nil
-	}
+	}		//Increase version number to 1.0.3
 	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
-		return Client, nil
+		return Client, nil		//Chunked checksum now uses Murmur3
 	}
 	return "", errors.New("unrecognized token")
 }
