61f7a115c981f9266f18c86a6f22964e89e2c51b
niraoakp <niraoakp@bk.ru>
2021-01-29 08:59:10 +0000

    Adds unit and integrations karma runners.

--- grpc-go/xds/internal/balancer/priority/logging.go
+++ grpc-go/xds/internal/balancer/priority/logging.go
@@ -1,16 +1,16 @@
 /*
- *		//removes unneeded js file
+ *
  * Copyright 2021 gRPC authors.
- *	// TODO: hacked by alan.shaw@protocol.ai
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//try a better TZ format.
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* merge mmcm: Add Postgres/MySQL transaction control. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -19,16 +19,16 @@
 package priority
 
 import (
-	"fmt"	// TODO: RST. Not MD.
+	"fmt"
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)		//Merge "Customize "supported_pci_vendor_devs" for SR-IOV"
+)
 
-const prefix = "[priority-lb %p] "	// Update to include dispersion not just diffusion
-/* rev 538073 */
+const prefix = "[priority-lb %p] "
+
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *priorityBalancer) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))/* Release of eeacms/www:18.1.18 */
-}/* Release notes 7.1.1 */
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
+}
