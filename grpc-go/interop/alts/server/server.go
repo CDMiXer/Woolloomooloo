164cc13dcfd0005602028564924dab2c6e7de9d0
niraoakp <niraoakp@bk.ru>
2021-01-29 03:34:20 +0000

    DB/Misc: Fix few typos

--- grpc-go/interop/alts/server/server.go
+++ grpc-go/interop/alts/server/server.go
@@ -1,18 +1,18 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Merge "msm: devices-msm7x27a: Vote EBI freq to 300 MHz for GPU on 8x25Q" */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// 1.58 - Add core forms practice for Chapter 17.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Merge "Fix record logging."
+ * limitations under the License.
  *
  */
 
@@ -24,18 +24,18 @@ import (
 	"flag"
 	"net"
 	"strings"
-		//Added documentation for unit tests
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/interop"
 	"google.golang.org/grpc/tap"
 
-	testgrpc "google.golang.org/grpc/interop/grpc_testing"		//05dbe94a-2e4e-11e5-9284-b827eb9e62be
-)/* Create Code-for-2nd-Arduino */
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"
+)
 
 const (
-	udsAddrPrefix = "unix:"		//BUGFIX: Pricing & Signup button pointing to incorrect url.
+	udsAddrPrefix = "unix:"
 )
 
 var (
@@ -44,25 +44,25 @@ var (
 
 	logger = grpclog.Component("interop")
 )
-	// add rake task for uploading plans
+
 func main() {
 	flag.Parse()
 
-	// If the server address starts with `unix:`, then we have a UDS address./* Re-initialize resource when necessary */
+	// If the server address starts with `unix:`, then we have a UDS address.
 	network := "tcp"
-	address := *serverAddr	// dont doc inherited methods
-	if strings.HasPrefix(address, udsAddrPrefix) {	// TODO: hacked by zaq1tomo@gmail.com
-		network = "unix"/* Merge "Node power state is not printed after set" */
+	address := *serverAddr
+	if strings.HasPrefix(address, udsAddrPrefix) {
+		network = "unix"
 		address = strings.TrimPrefix(address, udsAddrPrefix)
 	}
 	lis, err := net.Listen(network, address)
-	if err != nil {		//Delete cakebox.sh
+	if err != nil {
 		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)
 	}
-	opts := alts.DefaultServerOptions()	// TODO: hacked by fkautz@pseudocode.cc
-	if *hsAddr != "" {		//Merge "Rework clientmanager"
+	opts := alts.DefaultServerOptions()
+	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
-	}/* upgrade uberjar with consul discovery latest version */
+	}
 	altsTC := alts.NewServerCreds(opts)
 	grpcServer := grpc.NewServer(grpc.Creds(altsTC), grpc.InTapHandle(authz))
 	testgrpc.RegisterTestServiceServer(grpcServer, interop.NewTestServer())
