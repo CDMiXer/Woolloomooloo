6617feba0da7ab2ce493cf6d294afbd6dec4f04e
niraoakp <niraoakp@bk.ru>
2021-01-29 19:36:05 +0000

    Rename emptyproject.md to hikeall.md

--- core/syncer.go
+++ core/syncer.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// correct release md
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 0.9.6.RELEASE */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Fix scrollbar size for metadata table
-// distributed under the License is distributed on an "AS IS" BASIS,		//added test config to enable running tests in parallel
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core		//Properly reset card system on card eject.
+package core
 
 import "context"
 
 // Syncer synchronizes the account repository list.
-type Syncer interface {	// Lots of fixes to README documentation
-	Sync(context.Context, *User) (*Batch, error)	// Added support for cursor= hand
-}/* Autoload recursively from autoload_paths */
+type Syncer interface {
+	Sync(context.Context, *User) (*Batch, error)
+}
