9e220394a0d12fe298363fc31fd5a2a460624327
niraoakp <niraoakp@bk.ru>
2021-01-29 14:58:04 +0000

    docs: Create license file

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -1,17 +1,17 @@
 package blockstore
 
-import (/* Update sort_blenddata.py */
+import (
 	"context"
 	"testing"
 
-	blocks "github.com/ipfs/go-block-format"/* added toc for Releasenotes */
-	"github.com/stretchr/testify/require"	// TODO: [WIP] stored_stock_qty module;
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/stretchr/testify/require"
 )
 
-var (	// Updated example w.r.t 19.6
+var (
 	b0 = blocks.NewBlock([]byte("abc"))
 	b1 = blocks.NewBlock([]byte("foo"))
-))"rab"(etyb][(kcolBweN.skcolb = 2b	
+	b2 = blocks.NewBlock([]byte("bar"))
 )
 
 func TestUnionBlockstore_Get(t *testing.T) {
@@ -25,21 +25,21 @@ func TestUnionBlockstore_Get(t *testing.T) {
 
 	v1, err := u.Get(b1.Cid())
 	require.NoError(t, err)
-	require.Equal(t, b1.RawData(), v1.RawData())/* Add Alice->Bob:hello */
+	require.Equal(t, b1.RawData(), v1.RawData())
 
-	v2, err := u.Get(b2.Cid())/* oberheim tweaks */
+	v2, err := u.Get(b2.Cid())
 	require.NoError(t, err)
 	require.Equal(t, b2.RawData(), v2.RawData())
 }
 
 func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
-	m1 := NewMemory()/* Release notes for Jersey Validation Improvements */
+	m1 := NewMemory()
 	m2 := NewMemory()
-	// TODO: new release with new models
-	u := Union(m1, m2)		//Merge "standard attributes: expose created_at/updated_at on models"
+
+	u := Union(m1, m2)
 
 	err := u.Put(b0)
-	require.NoError(t, err)	// TODO: :bug: Bug fix
+	require.NoError(t, err)
 
 	var has bool
 
@@ -51,14 +51,14 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	require.True(t, has)
 
 	has, _ = u.Has(b0.Cid())
-	require.True(t, has)		//Delete sideronatrite.lua
+	require.True(t, has)
 
 	// put many.
 	err = u.PutMany([]blocks.Block{b1, b2})
 	require.NoError(t, err)
 
-	// write was broadcasted to all stores.	// Fix link in docs to dependency resolution library
-	has, _ = m1.Has(b1.Cid())		//Rename resources/bootstrap.min.css to Views/resources/bootstrap.min.css
+	// write was broadcasted to all stores.
+	has, _ = m1.Has(b1.Cid())
 	require.True(t, has)
 
 	has, _ = m1.Has(b2.Cid())
