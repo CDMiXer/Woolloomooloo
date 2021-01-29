c49b820db09292733adb6889c99ca2907207d2a7
niraoakp <niraoakp@bk.ru>
2021-01-29 17:07:35 +0000

    Removed Theme

--- pkg/codegen/dotnet/importer.go
+++ pkg/codegen/dotnet/importer.go
@@ -1,32 +1,32 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Updated project name.
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by steven@stebalien.com
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Merge "Fixing neutron-mlnx-agent service crash bug"
-/* Se revierten cambios no autorizados */
+// limitations under the License.
+
 package dotnet
 
 import (
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* Release: Making ready for next release iteration 6.6.2 */
+)
 
 // CSharpPropertyInfo represents the C# language-specific info for a property.
 type CSharpPropertyInfo struct {
-	Name string `json:"name,omitempty"`/* fe149052-2e4f-11e5-9284-b827eb9e62be */
+	Name string `json:"name,omitempty"`
 }
 
 // CSharpPackageInfo represents the C# language-specific info for a package.
-type CSharpPackageInfo struct {	// TODO: Merge "[FAB-6010] fixed the wrong URL in examples/README"
+type CSharpPackageInfo struct {
 	PackageReferences      map[string]string `json:"packageReferences,omitempty"`
 	Namespaces             map[string]string `json:"namespaces,omitempty"`
 	Compatibility          string            `json:"compatibility,omitempty"`
@@ -39,25 +39,25 @@ var Importer schema.Language = importer(0)
 type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
-func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {		//Merge "Skin::getCommonStylePath() was removed"
+func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
-	var info CSharpPropertyInfo		//Update README for output files
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {		//Avoid reporting the same missing dependecy twice.
-		return nil, err/* Make the main frame as small (and hopefully unobtrusive) as possible. */
-	}/* Release jedipus-2.6.43 */
-	return info, nil	// Redirect to bookmark after login
+	var info CSharpPropertyInfo
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {
+		return nil, err
+	}
+	return info, nil
 }
 
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
-	return raw, nil		//Automatic changelog generation for PR #30709 [ci skip]
-}/* Registered Mail and AttachableMail in ConfigurationSerilization. */
+	return raw, nil
+}
 
-// ImportResourceSpec decodes language-specific metadata associated with a Resource.		//1. Updating plugin to use jQuery.
+// ImportResourceSpec decodes language-specific metadata associated with a Resource.
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
