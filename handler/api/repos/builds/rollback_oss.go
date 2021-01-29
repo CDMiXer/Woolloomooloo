04c8ec96c0234a1e35302682eb8fedb3f3ebf013
niraoakp <niraoakp@bk.ru>
2021-01-29 12:56:39 +0000

    intercepts overflow guard in crosshair code

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-///* Fix to pass buffer size. */
-// Licensed under the Apache License, Version 2.0 (the "License");		//Update treasure_spec.rb
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by timnugent@gmail.com
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Remove flattening of source files. */
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by yuvalalaluf@gmail.com
-// distributed under the License is distributed on an "AS IS" BASIS,	// Increase Library dev version
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fixes for lib_scope
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,21 +17,21 @@
 package builds
 
 import (
-	"net/http"	// TODO: fix namespace resolution on adLDAP
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-	// TODO: single series to bitmap: check!
+
 var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-/* FLUX added implementation for interface method, status. */
+
 // HandleRollback returns a non-op http.HandlerFunc.
 func HandleRollback(
 	core.RepositoryStore,
 	core.BuildStore,
-	core.Triggerer,/* Release jedipus-2.5.16 */
-) http.HandlerFunc {/* Release notes: fix wrong link to Translations */
-	return rollbackNotImplemented	// renamed all views
+	core.Triggerer,
+) http.HandlerFunc {
+	return rollbackNotImplemented
 }
