5a723618eecdd699adb0c344d54ea6a05fea4d02
niraoakp <niraoakp@bk.ru>
2021-01-29 12:13:12 +0000

    Limitando resultados, aplicando ordem.

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* create content for README.md */
-	// TODO: [symfony4] update exception types
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // Base should not be delete-before-replaced, but should still be replaced.
-const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
+const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });/* Merge branch 'feature/68469' into develop */
 
-// Base-2 should not be delete-before-replaced, but should still be replaced./* PreRelease metadata cleanup. */
-const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });		//NetKAN added mod - DSEV-PlayMode-ClassicStock-v3.7.0
+// Base-2 should not be delete-before-replaced, but should still be replaced.
+const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
 
-// Dependent should be delete-before-replaced.	// TODO: will be fixed by lexy8russo@outlook.com
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
+// Dependent should be delete-before-replaced.	// TODO: will be fixed by sbrichards@gmail.com
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: hacked by vyzo@hackzen.org
