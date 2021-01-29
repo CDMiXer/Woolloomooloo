38fdb268f78658f919ef0d610e6fd1ec2bb02f19
niraoakp <niraoakp@bk.ru>
2021-01-29 02:28:27 +0000

    add several test methods, create new test-ressources

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as assert from "assert";/* Version 0.10.1 Release */
+import * as assert from "assert";/* Merge branch 'patch-issue103' into develop */
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";	// TODO: hacked by nagydani@epointsystem.org
-
+import { Resource } from "./resource";
+/* Release 0.6 in September-October */
 const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
 
-let a = new Resource("res", {
+let a = new Resource("res", {	// TODO: Add showp's msm_thermal.c without changes
     foo: "foo",
     bar: { value: "foo", unknown },
     baz: [ "foo", unknown ],
-});		//Added new rules to naming convention
-		//Don’t init if platform isn’t supported.
-export let o = Promise.all([/* add libasound-dev to travis */
+});
+
+export let o = Promise.all([
     (<any>a.foo).isKnown,
     (<any>a.bar.value).isKnown,
-    (<any>a.bar.unknown).isKnown,/* Fix bad url generation. */
+    (<any>a.bar.unknown).isKnown,
     (<any>a.baz[0]).isKnown,
-    (<any>a.baz[1]).isKnown,
+    (<any>a.baz[1]).isKnown,/* Deny access to uploads folder. */
 ]).then(([r1, r2, r3, r4, r5]) => {
     assert.equal(r1, true);
-    assert.equal(r2, true);/* upping to support UserEmailAlreadyExists */
+    assert.equal(r2, true);
     assert.equal(r3, !pulumi.runtime.isDryRun());
     assert.equal(r4, true);
-    assert.equal(r5, !pulumi.runtime.isDryRun());		//Fix compute homepage URL (#927)
-		//extended readme file and added simple usage example
-    console.log("ok");	// TODO: hacked by nick@perfectabstractions.com
+    assert.equal(r5, !pulumi.runtime.isDryRun());
+
+    console.log("ok");
     return "checked";
 });
