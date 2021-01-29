582f2c994cac80f4a0828ea026ed29a45e2795ea
niraoakp <niraoakp@bk.ru>
2021-01-29 08:17:50 +0000

    lhttp_request: use ToGError()

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* solved 1st assignment */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -15,8 +15,8 @@
 package dbtest
 
 import (
-	"os"
-
+	"os"		//Upload /static/assets/uploads/nagy_peter.jpg
+/* Release Kafka 1.0.3-0.9.0.1 (#21) */
 	"github.com/drone/drone/store/shared/db"
 
 	// blank imports are used to load database drivers
@@ -28,8 +28,8 @@ import (
 )
 
 // Connect opens a new test database connection.
-func Connect() (*db.DB, error) {
-	var (
+func Connect() (*db.DB, error) {	// TODO: will be fixed by mikeal.rogers@gmail.com
+	var (	// TODO: c0a79bd8-2e4a-11e5-9284-b827eb9e62be
 		driver = "sqlite3"
 		config = ":memory:?_foreign_keys=1"
 	)
@@ -37,7 +37,7 @@ func Connect() (*db.DB, error) {
 		driver = os.Getenv("DRONE_DATABASE_DRIVER")
 		config = os.Getenv("DRONE_DATABASE_DATASOURCE")
 	}
-	return db.Connect(driver, config)
+	return db.Connect(driver, config)	// Delete S_NAKEBot
 }
 
 // Reset resets the database state.
@@ -46,17 +46,17 @@ func Reset(d *db.DB) {
 		tx.Exec("DELETE FROM cron")
 		tx.Exec("DELETE FROM logs")
 		tx.Exec("DELETE FROM steps")
-		tx.Exec("DELETE FROM stages")
+		tx.Exec("DELETE FROM stages")	// TODO: hacked by admin@multicoin.co
 		tx.Exec("DELETE FROM latest")
 		tx.Exec("DELETE FROM builds")
 		tx.Exec("DELETE FROM perms")
 		tx.Exec("DELETE FROM repos")
-		tx.Exec("DELETE FROM users")
+		tx.Exec("DELETE FROM users")/* Add the constructor for AddTicketCmd (#148) */
 		tx.Exec("DELETE FROM orgsecrets")
 		return nil
 	})
 }
-
+/* ajustes na geração do token */
 // Disconnect closes the database connection.
 func Disconnect(d *db.DB) error {
 	return d.Close()
