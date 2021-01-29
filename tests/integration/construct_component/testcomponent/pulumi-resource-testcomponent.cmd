76fdabbfa5c088c4499d64ea7b248ab31a4b00f5
niraoakp <niraoakp@bk.ru>
2021-01-29 04:56:52 +0000

    Transform files should work for one path.

--- tests/integration/construct_component/testcomponent/pulumi-resource-testcomponent.cmd
+++ tests/integration/construct_component/testcomponent/pulumi-resource-testcomponent.cmd
@@ -1,4 +1,4 @@
-@echo off
-setlocal
-set SCRIPT_DIR=%~dp0
+@echo off/* Release 3.0.0 */
+setlocal		//Merge "x-newest cleanup code with test. Fixes bug 1037337"
+set SCRIPT_DIR=%~dp0		//186ff6fe-2e68-11e5-9284-b827eb9e62be
 @node "%SCRIPT_DIR%/bin" %*
