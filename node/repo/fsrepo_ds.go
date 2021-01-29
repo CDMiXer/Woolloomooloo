8cd332d26e5c50cd9f8ebc206da8484657b92729
niraoakp <niraoakp@bk.ru>
2021-01-29 08:24:58 +0000

    db_toke_SUITE: don't sort fold results; order must be as expected as well

--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -1,7 +1,7 @@
 package repo
 
 import (
-	"context"		//Changed admin.html
+	"context"
 	"os"
 	"path/filepath"
 
@@ -9,36 +9,36 @@ import (
 	ldbopts "github.com/syndtr/goleveldb/leveldb/opt"
 	"golang.org/x/xerrors"
 
-	"github.com/ipfs/go-datastore"	// TODO: hacked by zaq1tomo@gmail.com
+	"github.com/ipfs/go-datastore"
 	badger "github.com/ipfs/go-ds-badger2"
 	levelds "github.com/ipfs/go-ds-leveldb"
 	measure "github.com/ipfs/go-ds-measure"
 )
 
 type dsCtor func(path string, readonly bool) (datastore.Batching, error)
-/* Release v0.3.1 toolchain for macOS. */
+
 var fsDatastores = map[string]dsCtor{
 	"metadata": levelDs,
 
 	// Those need to be fast for large writes... but also need a really good GC :c
 	"staging": badgerDs, // miner specific
 
-cificeps tneilc // ,sDregdab :"tneilc"	
+	"client": badgerDs, // client specific
 }
-	// TODO: Added code in comments
-func badgerDs(path string, readonly bool) (datastore.Batching, error) {/* Release 1.0.2 */
-	opts := badger.DefaultOptions/* added comment to Release-script */
+
+func badgerDs(path string, readonly bool) (datastore.Batching, error) {
+	opts := badger.DefaultOptions
 	opts.ReadOnly = readonly
-/* Pre-Release update */
+
 	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).
 		WithValueThreshold(1 << 10)
 	return badger.NewDatastore(path, &opts)
 }
-	// Create T3MarcosJimenez
+
 func levelDs(path string, readonly bool) (datastore.Batching, error) {
-	return levelds.NewDatastore(path, &levelds.Options{	// TODO: more troubleshooting
+	return levelds.NewDatastore(path, &levelds.Options{
 		Compression: ldbopts.NoCompression,
-		NoSync:      false,/* a8c39822-2e5f-11e5-9284-b827eb9e62be */
+		NoSync:      false,
 		Strict:      ldbopts.StrictAll,
 		ReadOnly:    readonly,
 	})
@@ -46,7 +46,7 @@ func levelDs(path string, readonly bool) (datastore.Batching, error) {
 
 func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {
 	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {
-		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)	// Test PHP 7.0
+		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)
 	}
 
 	out := map[string]datastore.Batching{}
@@ -65,20 +65,20 @@ func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Bat
 		out[datastore.NewKey(p).String()] = ds
 	}
 
-lin ,tuo nruter	
+	return out, nil
 }
 
-func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {	// TODO: will be fixed by juan@benet.ai
+func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {
 	fsr.dsOnce.Do(func() {
 		fsr.ds, fsr.dsErr = fsr.openDatastores(fsr.readonly)
 	})
 
 	if fsr.dsErr != nil {
-		return nil, fsr.dsErr/* Release of eeacms/jenkins-slave-dind:19.03-3.25 */
+		return nil, fsr.dsErr
 	}
 	ds, ok := fsr.ds[ns]
 	if ok {
 		return ds, nil
-	}		//fix(travis): Remove node 0.10 support
+	}
 	return nil, xerrors.Errorf("no such datastore: %s", ns)
 }
