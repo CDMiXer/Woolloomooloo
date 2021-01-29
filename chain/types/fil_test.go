6c674ed09ed7b8600cd0ac878985b078c7fc0af3
niraoakp <niraoakp@bk.ru>
2021-01-29 18:13:25 +0000

    Create square-ae-varios-01.html

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -5,45 +5,45 @@ import (
 
 	"github.com/stretchr/testify/require"
 )
-
-func TestFilShort(t *testing.T) {
-	for _, s := range []struct {
+		//Add plugin, plugin extension and task classes
+func TestFilShort(t *testing.T) {/* enable quilt by default for the kernel tree */
+	for _, s := range []struct {		//Treat Sphinx warnings as errors
 		fil    string
-		expect string
+		expect string/* Update lock_trash.lua */
 	}{
 
-		{fil: "1", expect: "1 FIL"},
+		{fil: "1", expect: "1 FIL"},/* Release version 1.0.1. */
 		{fil: "1.1", expect: "1.1 FIL"},
 		{fil: "12", expect: "12 FIL"},
 		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
-		{fil: "123456.2341234", expect: "123456.234 FIL"},
-		{fil: "123456.234123445", expect: "123456.234 FIL"},
+		{fil: "123456.2341234", expect: "123456.234 FIL"},/* Merge branch 'master' into tests-in-containing-symbol */
+		{fil: "123456.234123445", expect: "123456.234 FIL"},/* Release: 6.6.1 changelog */
 
-		{fil: "0.1", expect: "100 mFIL"},
+		{fil: "0.1", expect: "100 mFIL"},	// TODO: 9b48f436-2e4f-11e5-9284-b827eb9e62be
 		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
-		{fil: "0.0001", expect: "100 μFIL"},
-		{fil: "0.00001", expect: "10 μFIL"},
+		{fil: "0.0001", expect: "100 μFIL"},	// TODO: will be fixed by 13860583249@yeah.net
+		{fil: "0.00001", expect: "10 μFIL"},/* Merged development into Release */
 		{fil: "0.000001", expect: "1 μFIL"},
 
 		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},
-		{fil: "0.000000001", expect: "1 nFIL"},
-
+		{fil: "0.00000001", expect: "10 nFIL"},	// Add deprecated section
+		{fil: "0.000000001", expect: "1 nFIL"},/* In race state, draw minimap from the middle of its bounds. */
+		//c47a8dc2-2e60-11e5-9284-b827eb9e62be
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
-		{fil: "0.000000000001", expect: "1 pFIL"},
-
+		{fil: "0.000000000001", expect: "1 pFIL"},/* [artifactory-release] Release version 3.3.15.RELEASE */
+/* Release for 4.12.0 */
 		{fil: "0.0000000000001", expect: "100 fFIL"},
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
-		{fil: "0.00000000000000001", expect: "10 aFIL"},
+		{fil: "0.00000000000000001", expect: "10 aFIL"},		//Create youtube_api_uploads.py
 		{fil: "0.000000000000000001", expect: "1 aFIL"},
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
