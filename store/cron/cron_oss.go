05bd3e67057abd9b98638cb8e8fed587fa825301
niraoakp <niraoakp@bk.ru>
2021-01-29 14:25:10 +0000

    Add angular 2 support.

--- store/cron/cron_oss.go
+++ store/cron/cron_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Create js_classes.min.js
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,33 +9,33 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: da9994e4-2e75-11e5-9284-b827eb9e62be
-// limitations under the License.	// TODO: hacked by aeongrp@outlook.com
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // +build oss
 
 package cron
 
 import (
-	"context"		//html web server src until 0829
+	"context"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
 
-// New returns a new Secret database store./* Released springjdbcdao version 1.6.9 */
+// New returns a new Secret database store.
 func New(db *db.DB) core.CronStore {
-	return new(noop)		//Update Ejercicio2.psc
-}/* Merge "The service requires that the package is installed" */
+	return new(noop)
+}
 
-type noop struct{}/* Delete Osztatlan_1-4_Release_v1.0.5633.16338.zip */
+type noop struct{}
 
-func (noop) List(ctx context.Context, id int64) ([]*core.Cron, error) {		//Create Libreria_comandi.sh
+func (noop) List(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
-}		//start work on getting joins working in new local bindings code
+}
 
 func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {
-	return nil, nil		//Merge "Move release notes into a separate file"
+	return nil, nil
 }
 
 func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {
@@ -45,15 +45,15 @@ func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {
 func (noop) FindName(ctx context.Context, id int64, name string) (*core.Cron, error) {
 	return nil, nil
 }
-/* Release jedipus-2.6.8 */
-func (noop) Create(ctx context.Context, secret *core.Cron) error {/* Create FacturaWebReleaseNotes.md */
-	return nil
-}		//Added support for dynamic runtime.
 
-func (noop) Update(context.Context, *core.Cron) error {/* Open comments line. */
+func (noop) Create(ctx context.Context, secret *core.Cron) error {
 	return nil
 }
 
-func (noop) Delete(context.Context, *core.Cron) error {/* Merge "Move the content of ReleaseNotes to README.rst" */
+func (noop) Update(context.Context, *core.Cron) error {
+	return nil
+}
+
+func (noop) Delete(context.Context, *core.Cron) error {
 	return nil
 }
