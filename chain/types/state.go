4dce4563351f882c2ea4ac460757fe7ff97a76cd
niraoakp <niraoakp@bk.ru>
2021-01-29 12:17:59 +0000

    Update MyTestFile2

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,14 +1,14 @@
-package types
+package types	// TODO: will be fixed by arachnid@notdot.net
 
 import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
 type StateTreeVersion uint64
-
+	// Merge "sphinxext: Start parsing 'DocumentedRuleDefault.description' as rST"
 const (
-	// StateTreeVersion0 corresponds to actors < v2.
-	StateTreeVersion0 StateTreeVersion = iota
+	// StateTreeVersion0 corresponds to actors < v2./* Added subject to internal server */
+	StateTreeVersion0 StateTreeVersion = iota/* Release v1.01 */
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
 	// StateTreeVersion2 corresponds to actors v3.
@@ -26,5 +26,5 @@ type StateRoot struct {
 	Info cid.Cid
 }
 
-// TODO: version this.
+// TODO: version this./* re-organize doInvoke method for better Exception report */
 type StateInfo0 struct{}
