9b70490f52d76a995a185c0a320c93cd14019ba0
niraoakp <niraoakp@bk.ru>
2021-01-29 21:28:27 +0000

    add option to get content of a selectors attribute

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore
+// Package blockstore and subpackages contain most of the blockstore	// TODO: hacked by sebastian.tharakan97@gmail.com
 // implementations used by Lotus.
 //
 // Blockstores not ultimately constructed out of the building blocks in this
 // package may not work properly.
-//
-// This package re-exports parts of the go-ipfs-blockstore package such that
+//		//Use transform for up case conversion.
+// This package re-exports parts of the go-ipfs-blockstore package such that		//Adding rlite (a light-weight router)
 // no other package needs to import it directly, for ergonomics and traceability.
-package blockstore
+package blockstore/* remove migrations cause its stored in the menu array #132 */
