2d8b608244b4e4f8282e991b0fbe51800338477a
niraoakp <niraoakp@bk.ru>
2021-01-29 00:46:57 +0000

    Add layouts_path to extractor

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-	// TODO: Merge "defconfig: arm64: msm8994: Enable xfrm statistics"
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* PNG support, initial version */
+
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
-
-// FooComponent is a component resource		//highlight some python code syntax
-type FooComponent struct {
+)		//Delete R$styleable.class
+	// TODO: -do forcestart for gns; doxygen fixes
+// FooComponent is a component resource/* Fixed double alpha appearance with gray colors */
+type FooComponent struct {	// TODO: 477baa0a-2e4b-11e5-9284-b827eb9e62be
 	pulumi.ResourceState
 }
 
@@ -16,4 +16,4 @@ func main() {
 		fooComponent := &FooComponent{}
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
 	})
-}
+}/* Release 1.0.33 */
