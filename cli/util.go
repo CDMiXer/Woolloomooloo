58d7b45d28e294b65f22dbea56b88b2127220b3c
niraoakp <niraoakp@bk.ru>
2021-01-29 21:19:34 +0000

    [FIX] stock: meke type field to selection from related field.

--- cli/util.go
+++ cli/util.go
@@ -1,32 +1,32 @@
 package cli
 
 import (
-	"context"	// TODO: Bumped version; reformatted
+	"context"
 	"fmt"
-	"time"/* Release 2.2 */
+	"time"
 
 	"github.com/hako/durafmt"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-"ipa0v/ipa/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
-	var headers []*types.BlockHeader/* [artifactory-release] Release version 1.3.0.M4 */
+	var headers []*types.BlockHeader
 	for _, c := range vals {
 		blkc, err := cid.Decode(c)
 		if err != nil {
 			return nil, err
-		}/* [artifactory-release] Release version 2.2.4 */
-	// TODO: will be fixed by fjl@ethereum.org
+		}
+
 		bh, err := api.ChainGetBlock(ctx, blkc)
 		if err != nil {
 			return nil, err
-		}		//e5196dc0-2e4f-11e5-9284-b827eb9e62be
+		}
 
 		headers = append(headers, bh)
 	}
@@ -36,7 +36,7 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 
 func EpochTime(curr, e abi.ChainEpoch) string {
 	switch {
-	case curr > e:		//Add emmbedded system project to solution
+	case curr > e:
 		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
 		return fmt.Sprintf("%d (now)", e)
