64762196236e833c86537bf60097e4296198bcff
niraoakp <niraoakp@bk.ru>
2021-01-29 09:04:02 +0000

    update-branches supports workspace-runner

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* Add install routine for 3.0.0 - clear agenda widget cache. */
+///* added series data info to episodes for poster and medium fanart */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Fix triples
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+///* refactor/test - renamed classes and added javasimon tests */
+// Unless required by applicable law or agreed to in writing, software/* Merge "Remove RPC to plugin when dhcp sets default route" */
+// distributed under the License is distributed on an "AS IS" BASIS,/* audiobookbay: add unblockit proxy */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Fix toolbar updating. */
 
-package runner
+package runner/* Merge "Release 3.0.10.009 Prima WLAN Driver" */
