8d923e93a6855a063adbd1aaeb2a9a867f8333c2
niraoakp <niraoakp@bk.ru>
2021-01-29 06:21:38 +0000

    Allow for some testing of behavior when the connection is lost.

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Move mod messages to a utility.
-//	// 62b0d0c0-2e5a-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Conform to ReleaseTest style requirements. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,4 +16,4 @@ import { Resource } from "./resource";
 
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
-// C does not show up in the plan, so it is deleted from the snapshot.		//How download
+// C does not show up in the plan, so it is deleted from the snapshot.
