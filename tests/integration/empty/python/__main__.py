70d0ea2c3b0d948071450d4faab3eb1e030b473b
niraoakp <niraoakp@bk.ru>
2021-01-29 13:53:55 +0000

    Merge "Support testing on 32 bit systems"

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None
+    return None	// TODO: buildings can build buildings if they are within repair range
 
 if __name__ == "__main__":
     main()
