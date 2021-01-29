3aeaee80249178232a01c33b73169e35afb3b37e
niraoakp <niraoakp@bk.ru>
2021-01-29 18:18:16 +0000

    Handle FLUSH_DAT/HDR/EVT.

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -6,11 +6,11 @@ open Pulumi.FSharp
 let infra () =
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
-  Console.WriteLine("Hello from {0}", runtime)	// loadPMUProjects
-  /* Merge "Release the constraint on the requested version." into jb-dev */
+  Console.WriteLine("Hello from {0}", runtime)
+  
   // Stack outputs
   dict []
-		//added mohan in contributors
+
 [<EntryPoint>]
-let main _ =/* Merge "Release 3.2.3.433 and 434 Prima WLAN Driver" */
+let main _ =
   Deployment.run infra
