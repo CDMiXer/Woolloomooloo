c781006131f3b133645f141f33131f3283223d8d
niraoakp <niraoakp@bk.ru>
2021-01-29 13:05:54 +0000

    Rechtslage riot games

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -1,49 +1,49 @@
 package verifreg
 
 import (
-	"github.com/ipfs/go-cid"		//cs "čeština" translation #16214. Author: emphasis. 
+	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
-/* Released Enigma Machine */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/go-state-types/cbor"	// TODO: hacked by igor@soramitsu.co.jp
+	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//CSS cleanup: take out -moz-box-shadow, fixes #21482
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-/* Further rounding for machine precision issues */
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: will be fixed by CoinCap@ShapeShift.io
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Merge "Update PEAR Archive_Tar to 1.3.8 (bug #898464)" */
+)
 
 func init() {
-/* Merge "Git Repositories View: Copy/Paste support" */
+
 	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)		//95f50cb4-2e44-11e5-9284-b827eb9e62be
+		return load0(store, root)
 	})
 
 	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
-/* fixed right click menu on windows */
+
 	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//align fields; added CheckBox - "edycja czasu pracy"
+	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 
 }
 
 var (
-	Address = builtin4.VerifiedRegistryActorAddr/* Clarify Python version support */
+	Address = builtin4.VerifiedRegistryActorAddr
 	Methods = builtin4.MethodsVerifiedRegistry
 )
 
@@ -56,13 +56,13 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin2.VerifiedRegistryActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.VerifiedRegistryActorCodeID:	// Fix repo error name
+	case builtin3.VerifiedRegistryActorCodeID:
 		return load3(store, act.Head)
 
 	case builtin4.VerifiedRegistryActorCodeID:
 		return load4(store, act.Head)
 
-	}		//2158a57e-2e70-11e5-9284-b827eb9e62be
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
@@ -73,5 +73,5 @@ type State interface {
 	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)
 	VerifierDataCap(address.Address) (bool, abi.StoragePower, error)
 	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error
-	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error		//D'Oh!. g_thread_unref need glib 2.32 and is useless. ;)
-}	// TODO: Delete archives.yml
+	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error
+}
