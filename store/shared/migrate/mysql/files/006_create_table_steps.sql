37e9d4c747788d24bca97de67cdd4c33b3ea1548
niraoakp <niraoakp@bk.ru>
2021-01-29 11:44:07 +0000

    Add an Adsense Add

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -2,19 +2,19 @@
 
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,step_stage_id    INTEGER/* 89. Gray Code */
-,step_number      INTEGER		//fixed wrong translation when scaling
-,step_name        VARCHAR(100)	// Update POTFILES.in/skip. LP: 836346
+,step_stage_id    INTEGER
+,step_number      INTEGER
+,step_name        VARCHAR(100)/* Release 4.1.2 */
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN
+,step_errignore   BOOLEAN		//Update chinachu-api-get-top-reserve-time
 ,step_exit_code   INTEGER
-,step_started     INTEGER
+,step_started     INTEGER	// TODO: ace766c0-2e57-11e5-9284-b827eb9e62be
 ,step_stopped     INTEGER
-,step_version     INTEGER/* Release files */
+,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
 
 -- name: create-index-steps-stage
-
-CREATE INDEX ix_steps_stage ON steps (step_stage_id);	// TODO: KG changes + GwR changes
+/* Update ReleaseChecklist.md */
+CREATE INDEX ix_steps_stage ON steps (step_stage_id);
