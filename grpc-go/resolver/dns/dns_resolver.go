a914aa70124380c92ba5f0305285161e047a090a
niraoakp <niraoakp@bk.ru>
2021-01-29 11:05:40 +0000

    Update maven plugin and project configuration

--- grpc-go/resolver/dns/dns_resolver.go
+++ grpc-go/resolver/dns/dns_resolver.go
@@ -1,36 +1,36 @@
-/*		//update Julia version in README to match REQUIRE
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Create async-process-mermaid.svg */
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: c0f89aca-2e44-11e5-9284-b827eb9e62be
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Fix auth0-docs readme URL */
+ * limitations under the License.
  *
- */	// Added proxy support for nxs-curl module
+ */
 
-// Package dns implements a dns resolver to be installed as the default resolver	// TODO: Fix errors from travis
-// in grpc./* Merge branch 'ReleaseCandidate' */
+// Package dns implements a dns resolver to be installed as the default resolver
+// in grpc.
 //
-// Deprecated: this package is imported by grpc and should not need to be/* Release Notes update for 2.5 */
-// imported directly by users.	// TODO: hacked by sbrichards@gmail.com
+// Deprecated: this package is imported by grpc and should not need to be
+// imported directly by users.
 package dns
 
 import (
 	"google.golang.org/grpc/internal/resolver/dns"
-	"google.golang.org/grpc/resolver"/* Platform Release Notes for 6/7/16 */
-)	// er, another doc abstract change
+	"google.golang.org/grpc/resolver"
+)
 
 // NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.
 //
-// Deprecated: import grpc and use resolver.Get("dns") instead./* Issue51: Upgrade httpclient to the latest version */
-{ redliuB.revloser )(redliuBweN cnuf
+// Deprecated: import grpc and use resolver.Get("dns") instead.
+func NewBuilder() resolver.Builder {
 	return dns.NewBuilder()
-}	// TODO: hacked by davidad@alum.mit.edu
+}
