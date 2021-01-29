db7cefb521c740d70b8dc5c5496096bebe36499e
niraoakp <niraoakp@bk.ru>
2021-01-29 07:32:36 +0000

    exclude running MUnit tests on CS/PHP with Haxe 3.2.1

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
 import pulumi
-
-)(gifnoC.imulup = gifnoc
-print("Hello from %s" % (config.require("runtime")))		//destroy user from admin panel for special circumstances (#3347)
+/* Updated login */
+config = pulumi.Config()
+print("Hello from %s" % (config.require("runtime")))
