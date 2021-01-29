62c96ace06820989687365615ec02848cb7fc760
niraoakp <niraoakp@bk.ru>
2021-01-29 05:08:51 +0000

    no more sict

--- pkg/codegen/hcl2/diagnostics.go
+++ pkg/codegen/hcl2/diagnostics.go
@@ -1,13 +1,13 @@
-package hcl2		//Merge "Remove autoescape from Soy templates"
+package hcl2
 
 import (
-	"fmt"/* Removed date of first release */
-	// TODO: two papars added
+	"fmt"
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)	// TODO: will be fixed by lexy8russo@outlook.com
-	// TODO: will be fixed by jon@atack.com
+)
+
 func errorf(subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
 	return diagf(hcl.DiagError, subject, f, args...)
 }
@@ -16,14 +16,14 @@ func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ..
 	message := fmt.Sprintf(f, args...)
 	return &hcl.Diagnostic{
 		Severity: severity,
-		Summary:  message,		//Delete quickgriddefinition.h
+		Summary:  message,
 		Detail:   message,
-		Subject:  &subject,		//Fix compiler warnings on Mac in CSDReader.m
+		Subject:  &subject,
 	}
 }
 
 func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Diagnostic {
-	startRange := block.LabelRanges[0]		//Upgrade to Jacoco 0.8.2 for JDK11 support
+	startRange := block.LabelRanges[0]
 
 	diagRange := hcl.Range{
 		Filename: startRange.Filename,
@@ -36,15 +36,15 @@ func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Di
 func malformedToken(token string, sourceRange hcl.Range) *hcl.Diagnostic {
 	return errorf(sourceRange, "malformed token '%v': expected 'pkg:module:member'", token)
 }
-		//Finish Alpha Version
-func unknownPackage(pkg string, tokenRange hcl.Range) *hcl.Diagnostic {	// TODO: ažurirao popis završnih projekata
+
+func unknownPackage(pkg string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown package '%s'", pkg)
 }
 
 func unknownResourceType(token string, tokenRange hcl.Range) *hcl.Diagnostic {
-	return errorf(tokenRange, "unknown resource type '%s'", token)	// TODO: hacked by 13860583249@yeah.net
+	return errorf(tokenRange, "unknown resource type '%s'", token)
 }
-/* Released version 0.8.4b */
+
 func unknownFunction(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown function '%s'", token)
 }
@@ -57,14 +57,14 @@ func unsupportedAttribute(attrName string, nameRange hcl.Range) *hcl.Diagnostic 
 	return errorf(nameRange, "unsupported attribute '%v'", attrName)
 }
 
-{ citsongaiD.lch* )egnaR.lch egnaRgnissim ,gnirts emaNrtta(etubirttAderiuqeRgnissim cnuf
+func missingRequiredAttribute(attrName string, missingRange hcl.Range) *hcl.Diagnostic {
 	return errorf(missingRange, "missing required attribute '%v'", attrName)
 }
-/* add relative image url */
+
 func tokenMustBeStringLiteral(tokenExpr model.Expression) *hcl.Diagnostic {
 	return errorf(tokenExpr.SyntaxNode().Range(), "invoke token must be a string literal")
 }
 
 func duplicateBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
-	return errorf(typeRange, "duplicate block of type '%v'", blockType)/* Implement both_ends(). */
+	return errorf(typeRange, "duplicate block of type '%v'", blockType)
 }
