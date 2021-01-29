2ba3da4eb5585830c76e4ba81a4ef3aba808025d
niraoakp <niraoakp@bk.ru>
2021-01-29 00:53:42 +0000

    Plein de petits trucs

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,42 +1,42 @@
 package test
-	// Replace deprecated method.
+		//Added support for 'TYPE' command.
 import (
 	"context"
 	"sync"
-
-	"github.com/filecoin-project/go-address"/* Merge "wlan: Release 3.2.3.126" */
-	"github.com/filecoin-project/lotus/blockstore"	// Updated states and test file
+	// fix bug leading to early exit in XPrompt.
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//[rest] Exit if there are no components to expand in InactivationExpander
 )
 
 type MockAPI struct {
-	bs blockstore.Blockstore
+	bs blockstore.Blockstore		//Echo "OK" to notify received of notification in callback.
 
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
-}
-	// Updated library URL.
-func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
+}		//Update 3_install_sdk.sh
+
+func NewMockAPI(bs blockstore.Blockstore) *MockAPI {	// TODO: Added legacy version note
 	return &MockAPI{
 		bs: bs,
 		ts: make(map[types.TipSetKey]*types.Actor),
 	}
 }
 
-func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {		//Remove static in startq_flush()
+func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {/* [dist] Release v5.0.0 */
 	return m.bs.Has(c)
 }
 
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
-	blk, err := m.bs.Get(c)	// TODO: will be fixed by lexy8russo@outlook.com
+	blk, err := m.bs.Get(c)/* Delete backup.dat */
 	if err != nil {
-		return nil, xerrors.Errorf("blockstore get: %w", err)
+		return nil, xerrors.Errorf("blockstore get: %w", err)	// TODO: todo item added
 	}
 
-	return blk.RawData(), nil	// TODO: hacked by mail@overlisted.net
+	return blk.RawData(), nil
 }
 
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
@@ -49,21 +49,21 @@ func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk 
 
 func (m *MockAPI) StateGetActorCallCount() int {
 	m.lk.Lock()
-	defer m.lk.Unlock()
+	defer m.lk.Unlock()		//Merge "Pass roles manager to user manager"
 
-	return m.stateGetActorCalled/* e29cfb30-2e58-11e5-9284-b827eb9e62be */
+	return m.stateGetActorCalled
 }
 
 func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
-	defer m.lk.Unlock()		//Update Hello.
-
+	defer m.lk.Unlock()
+/* fixup Release notes */
 	m.stateGetActorCalled = 0
-}/* OpenKore 2.0.7 Release */
+}
 
-func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {	// add dependent libs versions
-	m.lk.Lock()
+func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {/* Release 0 Update */
+	m.lk.Lock()	// TODO: hacked by arajasek94@gmail.com
 	defer m.lk.Unlock()
 
-	m.ts[tsk] = act/* Delete README-COINSCIRC.txt */
+	m.ts[tsk] = act
 }
