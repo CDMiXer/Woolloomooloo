8f45c80f546b857712cb81b5a1b6744caaa2e2b7
niraoakp <niraoakp@bk.ru>
2021-01-29 17:49:11 +0000

    added new task that is required before being able to set the domain root

--- grpc-go/channelz/service/util_sktopt_386_test.go
+++ grpc-go/channelz/service/util_sktopt_386_test.go
@@ -1,5 +1,5 @@
 // +build 386,linux
-/* Update title visuals similar to note graph branch */
+
 /*
  *
  * Copyright 2018 gRPC authors.
@@ -7,12 +7,12 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Search across data */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Added MoreLikeThis search to the Solr connector */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Start SimpleCov
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -23,8 +23,8 @@ package service
 import (
 	"golang.org/x/sys/unix"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-)/* fix typo - floopy => floppy */
-		//Updated build file to new names. 
+)
+
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
 	sec, usec := convertToDuration(protoTime.GetDuration())
