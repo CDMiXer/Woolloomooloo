82831fbff2e3778a3687db95e013ab4fa887b5c1
niraoakp <niraoakp@bk.ru>
2021-01-29 19:07:44 +0000

    Post, retrieve. not working condition

--- core/perm.go
+++ core/perm.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc./* Release specifics */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* [artifactory-release] Release version 1.3.2.RELEASE */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// boostrap: better workaround.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Restructure result to OOP based and Visualize */
-// limitations under the License.		//Fix IsValidForPrerenderPage cannot return true if WhiteListPattern is null
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package core
 
-import "context"	// Fix devices last status restore.
+import "context"
 
 type (
 	// Perm represents an individuals repository
@@ -22,13 +22,13 @@ type (
 	Perm struct {
 		UserID  int64  `db:"perm_user_id"  json:"-"`
 		RepoUID string `db:"perm_repo_uid" json:"-"`
-		Read    bool   `db:"perm_read"     json:"read"`	// Copy userBio140 to correct controller
+		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"-"`
 		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
-	}	// Using new Build Image in Wercker
+	}
 
 	// Collaborator represents a project collaborator,
 	// and provides the account and repository permissions
@@ -41,23 +41,23 @@ type (
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
-		Synced  int64  `db:"perm_synced"   json:"synced"`/* Release for v38.0.0. */
+		Synced  int64  `db:"perm_synced"   json:"synced"`
 		Created int64  `db:"perm_created"  json:"created"`
 		Updated int64  `db:"perm_updated"  json:"updated"`
 	}
 
 	// PermStore defines operations for working with
 	// repository permissions.
-	PermStore interface {/* Fixed the crash problem in findsubtitles in a better way */
+	PermStore interface {
 		// Find returns a project member from the
-		// datastore./* we don't support kernels < 3.3 */
+		// datastore.
 		Find(ctx context.Context, repoUID string, userID int64) (*Perm, error)
 
 		// List returns a list of project members from the
-		// datastore./* Merge branch 'upgrade--polymer3' into prepare-for-deploy */
+		// datastore.
 		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
 
-		// Update persists an updated project member	// TODO: Delete unnecessary semicolons
+		// Update persists an updated project member
 		// to the datastore.
 		Update(context.Context, *Perm) error
 
