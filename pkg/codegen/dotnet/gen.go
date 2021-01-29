34129a210002ba2fa3b03bcf94adf5ed17e56eb6
niraoakp <niraoakp@bk.ru>
2021-01-29 18:10:49 +0000

    Added Ruby highlighting to Ruby code in README.md

--- pkg/codegen/dotnet/gen.go
+++ pkg/codegen/dotnet/gen.go
@@ -1,51 +1,51 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* (jam) Release 2.1.0b4 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* added new unit test, including alias check */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Update beijing.xml for more lines. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release 0.0.1-alpha */
+// limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning./* Rename "RCC configuration" menus to "Clocks configuration" */
+// goconst linter's warning.
 //
-// nolint: lll, goconst	// TODO: ReImplement pi2words with Either : solution or last attempt
-tentod egakcap
-
+// nolint: lll, goconst
+package dotnet
+/* (vila) Release 2.6.0 (Vincent Ladeuil) */
 import (
 	"bytes"
-	"fmt"/* Introduce CameraController so the CameraProcessor can be a singleton. */
-	"io"
-	"io/ioutil"/* Update Release Notes. */
-	"net/http"		//first attempt on integrating OpenCTM, not working yet
+	"fmt"
+	"io"		//7efaaa36-2e63-11e5-9284-b827eb9e62be
+	"io/ioutil"
+	"net/http"
 	"path"
-	"path/filepath"/* Improve path alias detection for InnoDB. */
+	"path/filepath"
 	"reflect"
-	"strconv"/* Release logger */
+	"strconv"		//Re-added logo to README
 	"strings"
 	"unicode"
 
-	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi/pkg/v2/codegen"/* Release entfernt gibt Probleme beim Installieren */
+	"github.com/pkg/errors"/* Release of eeacms/jenkins-slave-dind:17.06.2-3.12 */
+	"github.com/pulumi/pulumi/pkg/v2/codegen"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 type stringSet map[string]struct{}
 
-func (ss stringSet) add(s string) {/* 9df3cc80-2e75-11e5-9284-b827eb9e62be */
+func (ss stringSet) add(s string) {
 	ss[s] = struct{}{}
-}
+}/* 2734cf0c-2e4a-11e5-9284-b827eb9e62be */
 
 func (ss stringSet) has(s string) bool {
 	_, ok := ss[s]
-	return ok
+	return ok		//fix URShiftOperator bug
 }
 
 type typeDetails struct {
@@ -56,17 +56,17 @@ type typeDetails struct {
 }
 
 // Title converts the input string to a title case
-// where only the initial letter is upper-cased.	// TODO: will be fixed by vyzo@hackzen.org
-func Title(s string) string {	// TODO: hacked by ac0dem0nk3y@gmail.com
+// where only the initial letter is upper-cased.
+func Title(s string) string {
 	if s == "" {
-		return ""	// Improved Test vector usage and added error report line.
-	}		//Delete dockWidget.pyc
-	runes := []rune(s)
-	return string(append([]rune{unicode.ToUpper(runes[0])}, runes[1:]...))		//docs: Update samples README
+		return ""
+	}
+	runes := []rune(s)/* Release of eeacms/www-devel:20.8.1 */
+	return string(append([]rune{unicode.ToUpper(runes[0])}, runes[1:]...))
 }
-
-func csharpIdentifier(s string) string {
-	// Some schema field names may look like $ref or $schema. Remove the leading $ to make a valid identifier.
+/* pake model biasa aja */
+func csharpIdentifier(s string) string {/* Release notes added. */
+	// Some schema field names may look like $ref or $schema. Remove the leading $ to make a valid identifier./* Remove errant closing label tag. props wahgnube, fixes #13901. */
 	// This could lead to a clash if both `$foo` and `foo` are defined, but we don't try to de-duplicate now.
 	if strings.HasPrefix(s, "$") {
 		s = s[1:]
@@ -86,15 +86,15 @@ func csharpIdentifier(s string) string {
 		"new", "null", "object", "operator",
 		"out", "override", "params", "private",
 		"protected", "public", "readonly", "ref",
-		"return", "sbyte", "sealed", "short",
+		"return", "sbyte", "sealed", "short",		//Merge "TTY: msm_smd_tty: Clean up includes"
 		"sizeof", "stackalloc", "static", "string",
 		"struct", "switch", "this", "throw",
-		"true", "try", "typeof", "uint",
+		"true", "try", "typeof", "uint",		//Dangerous Cliffs: fix typo, add link to screencast
 		"ulong", "unchecked", "unsafe", "ushort",
 		"using", "virtual", "void", "volatile", "while":
 		return "@" + s
 
-	default:
+	default:	// [#103] Added failing integration test
 		return s
 	}
 }
