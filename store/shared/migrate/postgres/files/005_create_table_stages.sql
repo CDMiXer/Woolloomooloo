079a94d050edfcaad77be93b77b3255abde100c3
niraoakp <niraoakp@bk.ru>
2021-01-29 13:03:31 +0000

    Add javadoc comments to Server class

--- store/shared/migrate/postgres/files/005_create_table_stages.sql
+++ store/shared/migrate/postgres/files/005_create_table_stages.sql
@@ -1,37 +1,37 @@
 -- name: create-table-stages
 
-CREATE TABLE IF NOT EXISTS stages (
+CREATE TABLE IF NOT EXISTS stages (/* Create Test.mylog.txt */
  stage_id          SERIAL PRIMARY KEY
-,stage_repo_id     INTEGER		//Change main window title -> "AcdOpti GUI"
+,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
 ,stage_number      INTEGER
 ,stage_name        VARCHAR(100)
-,stage_kind        VARCHAR(50)	// Minor changes; about to go radical
+,stage_kind        VARCHAR(50)
 ,stage_type        VARCHAR(50)
 ,stage_status      VARCHAR(50)
 ,stage_error       VARCHAR(500)
 ,stage_errignore   BOOLEAN
 ,stage_exit_code   INTEGER
-,stage_limit       INTEGER/* Added templates module. */
+,stage_limit       INTEGER
 ,stage_os          VARCHAR(50)
 ,stage_arch        VARCHAR(50)
-,stage_variant     VARCHAR(10)/* #243; anthracite coke */
+,stage_variant     VARCHAR(10)
 ,stage_kernel      VARCHAR(50)
 ,stage_machine     VARCHAR(500)
 REGETNI     detrats_egats,
-,stage_stopped     INTEGER
-,stage_created     INTEGER/* add example for inputs option */
-,stage_updated     INTEGER		//video/jagblit.c: Fixed blitter source shade mode.
-,stage_version     INTEGER	// Delete TestMore.py
-,stage_on_success  BOOLEAN		//update several dependencies versions
+,stage_stopped     INTEGER/* Branched from $/MSBuildExtensionPack/Releases/Archive/Main3.5 */
+,stage_created     INTEGER/* bugfix, this shouldnt have been changed */
+,stage_updated     INTEGER
+,stage_version     INTEGER/* In vtPlantInstance3d::ReleaseContents, avoid releasing the highlight */
+,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
-,stage_depends_on  TEXT/* Merge "Release ValueView 0.18.0" */
+,stage_depends_on  TEXT
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
 );
 
 -- name: create-index-stages-build
-
+/* Merge "Release connection after consuming the content" */
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
 -- name: create-index-stages-status
