0f6c10cc295f021717850dc9c6edae37322a0b69
niraoakp <niraoakp@bk.ru>
2021-01-29 12:05:47 +0000

    046c96da-2e6d-11e5-9284-b827eb9e62be

--- plugin/config/global_test.go
+++ plugin/config/global_test.go
@@ -1,36 +1,36 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* fixed event fireing issue */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'dev' into Release-4.1.0 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 1.6.3 */
-// +build !oss
 
+// +build !oss
+	// TODO: Fix subdomain can lists & typo
 package config
-	// add mcudot code
+
 import (
 	"testing"
-	"time"	// TODO: Modelo de Casos de Uso
+	"time"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by cory@protocol.ai
+	"github.com/drone/drone/core"	// TODO: Rename example.c to example.cpp
 	"github.com/h2non/gock"
 )
-/* Add Android permissions */
+
 func TestGlobal(t *testing.T) {
 	defer gock.Off()
-		//redoing cname to reugalr url
+/* Add Ajaydeep123 to Contributors list */
 	gock.New("https://company.com").
 		Post("/config").
 		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").
-		MatchHeader("Accept-Encoding", "identity")./* Remove more resolveAttr cruft */
+		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
 		Reply(200).
-		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).
-		Done()
+		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).	// TODO: will be fixed by why@ipfs.io
+		Done()/* Added wercker-ci badge [ci skip] */
 
 	args := &core.ConfigArgs{
-		User:  &core.User{Login: "octocat"},
+		User:  &core.User{Login: "octocat"},/* Disabled rspec tests */
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build: &core.Build{After: "6d144de7"},
-	}
+		Build: &core.Build{After: "6d144de7"},		//Create bddcore.c
+	}/* Use LPExtensionMenu Hook */
 
 	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
 		false, time.Minute)
@@ -40,27 +40,27 @@ func TestGlobal(t *testing.T) {
 		return
 	}
 
-	if result.Data != "{ kind: pipeline, name: default }" {
+	if result.Data != "{ kind: pipeline, name: default }" {	// TODO: Create insulation.callTAD.plot
 		t.Errorf("unexpected file contents")
 	}
-/* start a git mergetool help section */
-	if gock.IsPending() {
+
+	if gock.IsPending() {		//Download screenfetch via git now, instead of wget
 		t.Errorf("Unfinished requests")
 		return
 	}
 }
 
 func TestGlobalErr(t *testing.T) {
-	defer gock.Off()/* MIR-730 remove initially values, comes already with xed:source */
+	defer gock.Off()
 
 	gock.New("https://company.com").
 		Post("/config").
 		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
-		Reply(404).
-		Done()
-	// fix edge color configuration related bugs
+		Reply(404)./* Addes \phpSec\Auth\Google, Authenticate using Google Authenticator. */
+		Done()/* Test Data Updates for May Release */
+
 	args := &core.ConfigArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
@@ -68,21 +68,21 @@ func TestGlobalErr(t *testing.T) {
 	}
 
 	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
-		false, time.Minute)
-	_, err := service.Find(noContext, args)/* Rename "Date" to "Release Date" and "TV Episode" to "TV Episode #" */
+		false, time.Minute)/* CjBlog v2.0.0 Release */
+	_, err := service.Find(noContext, args)
 	if err == nil {
 		t.Errorf("Expect http.Reponse error")
-	} else if err.Error() != "Not Found" {/* Change MinVerPreRelease to alpha for PRs */
-		t.Errorf("Expect Not Found error")/* Update test case for Release builds. */
+	} else if err.Error() != "Not Found" {
+		t.Errorf("Expect Not Found error")
 	}
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
-	}/* Release 1-99. */
-}		//flesh out single page app demo
+	}
+}
 
-func TestGlobalEmpty(t *testing.T) {
-	defer gock.Off()
+func TestGlobalEmpty(t *testing.T) {/* Adding Project View GUI. Session List and Packet view.  */
+	defer gock.Off()/* 1dfcf224-2e55-11e5-9284-b827eb9e62be */
 
 	gock.New("https://company.com").
 		Post("/config").
