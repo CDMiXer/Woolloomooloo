ad4ed144464491e9f0828299de50eb3ebde92186
niraoakp <niraoakp@bk.ru>
2021-01-29 10:12:07 +0000

    [MOD] hr_expense : small change

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,30 +1,30 @@
-package account/* Back Button Released (Bug) */
+package account
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"	// TODO: hacked by souzau@yandex.com
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* Merge "Promote ara logging and site_logs secret to base job" */
+
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
 )
-/* Remove goog.math.modulo and goog.math.lerp */
+
 var _ State = (*state3)(nil)
-	// TODO: external loans
+
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* Make content to archive available at runtime */
+	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}		//New translations firefly.php (Indonesian)
+	}
 	return &out, nil
 }
 
 type state3 struct {
 	account3.State
 	store adt.Store
-}/* uploading first part */
+}
 
-func (s *state3) PubkeyAddress() (address.Address, error) {		//js minified, start on doom ready
+func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
