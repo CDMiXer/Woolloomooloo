a791ba87be2a8549222c502127d2c37ecd6aa976
niraoakp <niraoakp@bk.ru>
2021-01-29 11:56:08 +0000

    Make sure the correct type of Config class is instanciated

--- handler/api/repos/crons/delete_test.go
+++ handler/api/repos/crons/delete_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//some skill check condition
-// +build !oss
-		//Update ToastyUtils.java
+
+// +build !oss/* Updated Solution Files for Release 3.4.0 */
+
 package crons
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"/* Release v6.4.1 */
 	"net/http"
-	"net/http/httptest"
+	"net/http/httptest"	// TODO: will be fixed by joshua@yottadb.com
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"	// DCAT temporal generated only if both start and end dates are valid.
 	"github.com/drone/drone/mock"
-/* Release for 2.7.0 */
+/* New IAU names on Mimas and Tethys */
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -23,16 +23,16 @@ import (
 
 func TestHandleDelete(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: Add innodb_check_free for alerting if freespace < threshold
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)/* V0.1 Release */
-
-	crons := mock.NewMockCronStore(controller)/* [travis] Add PPA with a newer version of gstreamer */
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)	// TODO: hacked by cory@protocol.ai
+	// TODO: hacked by cory@protocol.ai
+	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 	crons.EXPECT().Delete(gomock.Any(), dummyCron).Return(nil)
-	// TODO: will be fixed by xaber.twt@gmail.com
-	c := new(chi.Context)
+/* Release 1.9.31 */
+	c := new(chi.Context)/* Release notes for v1.5 */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
@@ -41,33 +41,33 @@ func TestHandleDelete(t *testing.T) {
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
+	)/* lastest vimperator config file */
+	// TODO: will be fixed by mowrain@yandex.com
 	HandleDelete(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
-	// TODO: will be fixed by ligi@ligi.de
-func TestHandleDelete_RepoNotFound(t *testing.T) {	// TODO: e7361036-2e6d-11e5-9284-b827eb9e62be
+
+func TestHandleDelete_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* #31 - Release version 1.3.0.RELEASE. */
+	defer controller.Finish()	// Linux - shuf actually has its own -n flag
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)/* Release already read bytes from delivery when sender aborts. */
-		//apertium-tinylex as related software
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")	// Merge "Return no active network if the agent has not been learnt yet"
-	c.URLParams.Add("name", "hello-world")
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)/* Small update to Release notes: uname -a. */
+	c.URLParams.Add("owner", "octocat")/* Merge "sensors: add loop back support for compass" */
+	c.URLParams.Add("name", "hello-world")	// TODO: will be fixed by nicksavers@gmail.com
 	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// TODO: hacked by steven@stebalien.com
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-)	
-/* Release candidate 0.7.3 */
-	HandleDelete(repos, nil).ServeHTTP(w, r)/* Release 0.9.15 */
+	)
+
+	HandleDelete(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
