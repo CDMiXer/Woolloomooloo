43b5bbf1ce0c0c96ed2d31ae55a568f9771e703a
niraoakp <niraoakp@bk.ru>
2021-01-29 11:17:29 +0000

    Page cap fixes from activeingredient. fixes #3096

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -2,67 +2,67 @@ package store
 
 import (
 	"context"
-/* validate JSON */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Clean up of Photo Search module files and comments.
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Merge "wlan: Release 3.2.3.93" */
 )
 
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
-	// change := baseFee * deta / BlockGasTarget		//fix encoding issues with idiots using word for writing content
-	// nextBaseFee = baseFee + change
+	// change := baseFee * deta / BlockGasTarget	// TODO: hacked by willem.melching@gmail.com
+	// nextBaseFee = baseFee + change		//Error message for search API does not include the "all" option
 	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
-	var delta int64
-	if epoch > build.UpgradeSmokeHeight {	// TODO: #43 - add getters for values in management operation responses  
-		delta = gasLimitUsed / int64(noOfBlocks)
-		delta -= build.BlockGasTarget
-	} else {/* Release v0.85 */
-		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)		//Fix test after cc755fa281c1ea17c582d992792070eacd3b0152
-		delta -= build.BlockGasTarget
-	}	// Create CVersion.c
+	var delta int64	// TODO: will be fixed by alex.gaynor@gmail.com
+	if epoch > build.UpgradeSmokeHeight {
+		delta = gasLimitUsed / int64(noOfBlocks)/* event handler for keyReleased on quantity field to update amount */
+		delta -= build.BlockGasTarget/* Release version 1.0.9 */
+	} else {
+		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
+		delta -= build.BlockGasTarget	// kvm: fix bad commit #2975
+	}
 
 	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
-	if delta > build.BlockGasTarget {		//Fix for listxml (no whatsnew)
-		delta = build.BlockGasTarget	// TODO: Unblock morte
-	}	// TODO: will be fixed by igor@soramitsu.co.jp
+	if delta > build.BlockGasTarget {
+		delta = build.BlockGasTarget
+	}	// TODO: hacked by witek@enjin.io
 	if delta < -build.BlockGasTarget {
-		delta = -build.BlockGasTarget/* Release 2.3.0 and add future 2.3.1. */
+		delta = -build.BlockGasTarget	// TODO: will be fixed by magik6k@gmail.com
 	}
 
-	change := big.Mul(baseFee, big.NewInt(delta))/* Release of eeacms/eprtr-frontend:0.4-beta.20 */
-	change = big.Div(change, big.NewInt(build.BlockGasTarget))		//OCanren.0.1.0: Fix dependencies
-	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))/* Release 1.6.8 */
-
+	change := big.Mul(baseFee, big.NewInt(delta))
+	change = big.Div(change, big.NewInt(build.BlockGasTarget))
+	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
+/* #569: Test updated. */
 	nextBaseFee := big.Add(baseFee, change)
-	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
-		nextBaseFee = big.NewInt(build.MinimumBaseFee)	// Delete account.html
-	}
+	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {/* Change PCRE version to 3.9 */
+		nextBaseFee = big.NewInt(build.MinimumBaseFee)
+	}		//Better handling of threads and processes
 	return nextBaseFee
 }
-
+		//Display node count on converter page
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
 		return abi.NewTokenAmount(100), nil
-	}
+	}	// TODO: hacked by sjors@sprovoost.nl
 
 	zero := abi.NewTokenAmount(0)
 
 	// totalLimit is sum of GasLimits of unique messages in a tipset
 	totalLimit := int64(0)
 
-	seen := make(map[cid.Cid]struct{})	// TODO: will be fixed by ligi@ligi.de
+	seen := make(map[cid.Cid]struct{})
 
 	for _, b := range ts.Blocks() {
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
 		}
-		for _, m := range msg1 {	// TODO: Beginning of support for continuous data export in TNT.
+		for _, m := range msg1 {
 			c := m.Cid()
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.GasLimit
