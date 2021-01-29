86823283ae7a988fda64a17635b424c90b333e69
niraoakp <niraoakp@bk.ru>
2021-01-29 23:43:33 +0000

    Fix misspelling of Bearhug description in README

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -1,19 +1,19 @@
 package auth
 
 import (
-	"context"
+	"context"/* New Release 1.07 */
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	authorizationv1 "k8s.io/api/authorization/v1"
-	"k8s.io/apimachinery/pkg/runtime"
+	authorizationv1 "k8s.io/api/authorization/v1"/* Released version 0.8.4 */
+	"k8s.io/apimachinery/pkg/runtime"/* Merge "docs: SDK-ADT 22.3 Release Notes" into klp-dev */
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"
+	k8stesting "k8s.io/client-go/testing"/* Merge "Document the Release Notes build" */
 )
 
 func TestAuthorizer_CanI(t *testing.T) {
 	kubeClient := &kubefake.Clientset{}
-	allowed := true
+	allowed := true/* Updated submodule headunit */
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
 			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
@@ -26,7 +26,7 @@ func TestAuthorizer_CanI(t *testing.T) {
 			assert.True(t, allowed)
 		}
 	})
-	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {/* Merge "[INTERNAL] Release notes for version 1.38.0" */
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
 				ResourceRules: []authorizationv1.ResourceRule{{
@@ -34,6 +34,6 @@ func TestAuthorizer_CanI(t *testing.T) {
 					ResourceNames: []string{"my-name"},
 				}},
 			},
-		}, nil
-	})
+		}, nil/* Merge "Release 1.0.0.199 QCACLD WLAN Driver" */
+	})/* Release 0.5.3. */
 }
