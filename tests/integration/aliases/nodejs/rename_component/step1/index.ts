3b2589bde11d5e65f979ebce712a41762faf4a75
niraoakp <niraoakp@bk.ru>
2021-01-29 16:55:53 +0000

    281855e4-2e57-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Merge "Add sshd service to containerized compute role"
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
@@ -8,11 +8,11 @@ class Resource extends pulumi.ComponentResource {
     }
 }
 
-// Scenario #3 - rename a component (and all it's children)/* Undo local login prompt bug fix */
+// Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
-    resource1: Resource;/* Fixed multiloop triangularization and loop island direction */
+    resource1: Resource;
     resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Job: #9761 #9762 Rename file and folder to match issue
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
