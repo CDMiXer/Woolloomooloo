08be85569ee1236e6e802eaee934fd893fe44015
niraoakp <niraoakp@bk.ru>
2021-01-29 20:09:48 +0000

    Merge branch 'master' into sdr-proxy

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* - Add: git ignore file */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: Update EvolveHelper.js
+import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// resource "not-doomed" is created successfully.
-const a = new Resource("not-doomed", 5);
-/* Added defaults definition and expanded to include EEPROM reading */
+// resource "not-doomed" is created successfully.		//Cleanup on extension field code.
+const a = new Resource("not-doomed", 5);/* Merge branch 'master' into greenkeeper/cssnano-4.0.5 */
+
 // "a" should be in the checkpoint.
