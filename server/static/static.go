82321e9a2e06b780f99da81925393edab1afecd3
niraoakp <niraoakp@bk.ru>
2021-01-29 01:03:28 +0000

    Update AirBox-SiteName-Penghu.txt

--- server/static/static.go
+++ server/static/static.go
@@ -2,37 +2,37 @@ package static
 
 import (
 	"fmt"
-	"net/http"
+	"net/http"	// TODO: will be fixed by why@ipfs.io
 )
 
 type FilesServer struct {
 	baseHRef string
 	hsts     bool
-}	// TODO: Update SEN.h
+}
 
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 	return &FilesServer{baseHRef, hsts}
-}
+}	// TODO: will be fixed by mikeal.rogers@gmail.com
 
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	//// If there is no stored static file, we'll redirect to the js app
-	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
+	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {/* Update Rules and tests for CEG-generation */
 	//	r.URL.Path = "index.html"
 	//}
 
 	if r.URL.Path == "index.html" {
-		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on/* Release changes 5.0.1 */
+		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
-		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
+		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}/* Release Scelight 6.4.3 */
 	}
 
 	w.Header().Set("X-Frame-Options", "DENY")
-	// `data:` is need for Monaco editors wiggly red lines		//rev 489293
+	// `data:` is need for Monaco editors wiggly red lines
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000")	// TODO:  - [DEV-137] add an example frontend configuration file
-	}
-	///* 171c9ed0-2e70-11e5-9284-b827eb9e62be */
-	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
-	//ServeHTTP(w, r)/* add "contributing" */
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
+	}	// TODO: answer lvm stripe task
+	//	// TODO: hacked by cory@protocol.ai
+	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine/* Bump version to 3.0.6 */
+	//ServeHTTP(w, r)
 }
