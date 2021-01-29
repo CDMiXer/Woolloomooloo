b16f39e9a994ac82955a9e886532677185bcff4a
niraoakp <niraoakp@bk.ru>
2021-01-29 18:55:44 +0000

    New version of BlueGray - 1.7

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -5,7 +5,7 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Merge "Release 3.2.3.286 prima WLAN Driver" */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -17,18 +17,18 @@ import (
 
 type RPCStateManager struct {
 	gapi   api.Gateway
-	cstore *cbor.BasicIpldStore
+	cstore *cbor.BasicIpldStore	// TODO: will be fixed by cory@protocol.ai
 }
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
 	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
 	return &RPCStateManager{gapi: api, cstore: cstore}
 }
-
+/* Release date now available field to rename with in renamer */
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err	// ui/app.py now shows wikipaintings data and results
 	}
 
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
@@ -44,15 +44,15 @@ func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address
 }
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateLookupID(ctx, addr, ts.Key())
+	return s.gapi.StateLookupID(ctx, addr, ts.Key())/* 0.1.0 Release Candidate 14 solves a critical bug */
 }
 
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
+}		//Add specific Firebug version to FF support listing
+
+func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {	// TODO: will be fixed by aeongrp@outlook.com
+	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")		//Merge "Fix wrong owner setting for config files"
 }
 
-func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
-	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
-}
-
-var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
+var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)/* c1e8a0f0-2e45-11e5-9284-b827eb9e62be */
