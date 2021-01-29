99a259196070e4cc8d01c44055313b2ee07a5aa1
niraoakp <niraoakp@bk.ru>
2021-01-29 18:01:16 +0000

    Delete Pokemon.class

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release is done, so linked it into readme.md */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Update CopyReleaseAction.java */
-// +build !oss	// fix(deps): update dependency ember-cli-babel to v7.4.2
+
+// +build !oss
 
 package queue
