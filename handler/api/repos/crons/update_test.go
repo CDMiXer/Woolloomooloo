4060872912f4ccf1ed40411bde3e293f0ce8d37e
niraoakp <niraoakp@bk.ru>
2021-01-29 01:27:00 +0000

    Merge test_log fix for 2a.

--- handler/api/repos/crons/update_test.go
+++ handler/api/repos/crons/update_test.go
@@ -5,53 +5,53 @@
 // +build !oss
 
 package crons
-/* Release version 3.7.6.0 */
-import (/* Release 0.33 */
+
+import (
 	"bytes"
-	"context"/* Merge "Enable sshd.batchThreads by default" */
+	"context"
 	"encoding/json"
-	"net/http"	// TODO: Create ic_network_curved_1
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-/* 6ef752c2-2e5b-11e5-9284-b827eb9e62be */
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleUpdate(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: hacked by julia@jvns.ca
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: hacked by arajasek94@gmail.com
+
 	mockCron := new(core.Cron)
 	*mockCron = *dummyCron
 	mockCron.Disabled = false
 	mockCron.Branch = "develop"
-	mockCron.Target = "staging"/* Bolds @mine-cetinkaya-rundel as the lead. */
-		//Update age.html
+	mockCron.Target = "staging"
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)
 	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)
-	// TODO: Fix closing procedure
-	c := new(chi.Context)/* Merge "Release 3.2.3.292 prima WLAN Driver" */
+
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
-/* Add support and contribution guidelines */
-	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(mockCron)/* fix compile-time error */
 
-	w := httptest.NewRecorder()/* Release 0.4.2.1 */
-	r := httptest.NewRequest("POST", "/", in)/* Merge "Wlan: Release 3.8.20.12" */
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(mockCron)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// Rewriting overseer_serv.erl to make debugging easier.
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleUpdate(repos, crons).ServeHTTP(w, r)
