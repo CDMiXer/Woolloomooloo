e7787ea811393859dd158942d7a4519c87e660f1
niraoakp <niraoakp@bk.ru>
2021-01-29 21:02:48 +0000

    [FIX] Use the context to use the language from the user

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release Scelight 6.3.0 */
-// You may obtain a copy of the License at/* Release v0.6.0.2 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,20 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
-	// TODO: will be fixed by steven@stebalien.com
-package ccmenu
+// +build oss/* Agregado ResultadoEliminarReserva, falta especificar errores */
+
+package ccmenu	// TODO: hacked by davidad@alum.mit.edu
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"		//THOROUGH README CHANGES.
-	"github.com/drone/drone/handler/api/render"/* Release version 2.6.0. */
-)
+	"github.com/drone/drone/core"	// TODO: dropdown implmentation 
+	"github.com/drone/drone/handler/api/render"
+)/* Delete scss.vim */
 
-// Handler returns a no-op http.HandlerFunc.	// TODO: hacked by cory@protocol.ai
+// Handler returns a no-op http.HandlerFunc.	// TODO: hacked by aeongrp@outlook.com
 func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by alex.gaynor@gmail.com
+	return func(w http.ResponseWriter, r *http.Request) {/* Merge "Reuse identical API v2 code for v1" */
 		render.NotImplemented(w, render.ErrNotImplemented)
-	}/* Release 0.1.11 */
+	}
 }
