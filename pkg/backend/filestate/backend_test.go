4c56e779332ea706ecd612e1174781631a5771f3
niraoakp <niraoakp@bk.ru>
2021-01-29 05:01:38 +0000

    Automatic changelog generation for PR #36015 [ci skip]

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -1,56 +1,56 @@
 package filestate
 
-import (/* Merge "ASoc: msm: Add support for multiple inputs to kcontrol" into msm-3.0 */
+import (
 	"path/filepath"
 	"runtime"
 	"testing"
-		//fixed ident
+
 	"github.com/stretchr/testify/assert"
 	user "github.com/tweekmonster/luser"
 
 	"github.com/pulumi/pulumi/pkg/v2/operations"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* Release 7.12.37 */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
 func TestMassageBlobPath(t *testing.T) {
 	testMassagePath := func(t *testing.T, s string, want string) {
 		massaged, err := massageBlobPath(s)
-		assert.NoError(t, err)	// TODO: Move to "frameworks" in yaml. by chipaca approved by mvo
+		assert.NoError(t, err)
 		assert.Equal(t, want, massaged,
 			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)
 	}
 
 	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
-	t.Run("NonFilePrefixed", func(t *testing.T) {/* Making the category code more concise */
+	t.Run("NonFilePrefixed", func(t *testing.T) {
 		testMassagePath(t, "asdf-123", "asdf-123")
 	})
 
 	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
 	t.Run("PrefixedWithTilde", func(t *testing.T) {
-		usr, err := user.Current()		//Implemented and tested reverseSorted
+		usr, err := user.Current()
 		if err != nil {
-			t.Fatalf("Unable to get current user: %v", err)	// TODO: will be fixed by davidad@alum.mit.edu
-}		
+			t.Fatalf("Unable to get current user: %v", err)
+		}
 
 		homeDir := usr.HomeDir
 
-		// When running on Windows, the "home directory" takes on a different meaning./* (vila) Release 2.4b5 (Vincent Ladeuil) */
+		// When running on Windows, the "home directory" takes on a different meaning.
 		if runtime.GOOS == "windows" {
 			t.Logf("Running on %v", runtime.GOOS)
 
-			t.Run("NormalizeDirSeparator", func(t *testing.T) {/* SSH tunnel fix */
+			t.Run("NormalizeDirSeparator", func(t *testing.T) {
 				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")
 			})
 
-			newHomeDir := "/" + filepath.ToSlash(homeDir)		//Some bug fixes.  
+			newHomeDir := "/" + filepath.ToSlash(homeDir)
 			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
 			homeDir = newHomeDir
 		}
-		//Removing 0.4 build since it is unsupported
+
 		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)
-		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")/* Release notes updated with fix issue #2329 */
+		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")
 	})
 
 	t.Run("MakeAbsolute", func(t *testing.T) {
@@ -74,9 +74,9 @@ func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {
 		Config:    config.Map{},
 		Decrypter: config.NopDecrypter,
 		Snapshot:  nil,
-	}	// TODO: Delete ElevensV9
+	}
 	query := operations.LogQuery{}
 	res, err := GetLogsForTarget(target, query)
 	assert.NoError(t, err)
 	assert.Nil(t, res)
-}	// TODO: Update the link to samples
+}
