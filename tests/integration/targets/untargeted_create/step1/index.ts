1ca50c56021a19d4f4b22fe1bc5db6428e161a36
niraoakp <niraoakp@bk.ru>
2021-01-29 08:10:34 +0000

    Added most of the text for the Readme file

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -1,32 +1,32 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//branch for 2.1
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// adjusted help
+/* Update mcs/class/System/System.Net/WebRequest.cs */
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
-    public static instance = new Provider();	// TODO: Remove duplicate wire/dom
+    public static instance = new Provider();/* update basic http auth credentials */
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-		//Improve mapping of custom column type.
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Comparison of tables during altering fixed. (BUG#39399) */
+
     constructor() {
-        this.create = async (inputs: any) => {	// TODO: will be fixed by zaq1tomo@gmail.com
+        this.create = async (inputs: any) => {
             return {
-                id: (currentID++) + "",
+                id: (currentID++) + "",	// TODO: hacked by brosner@gmail.com
                 outs: undefined,
             };
         };
     }
-}/* Updating xlslib. */
-/* Update svg importer for issue #81 */
+}	// Merge branch 'master' into fix/#679
+
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, {}, opts);
-    }
+    }		//fix bug #592436
 }
 
-// Create a resource using the default dynamic provider instance./* Release: 1.4.2. */
-let a = new Resource("a");/* prepare for stable */
-let b = new Resource("b");
+// Create a resource using the default dynamic provider instance.
+let a = new Resource("a");
+let b = new Resource("b");/* [hands free] */
 
 export const urn = a.urn;
