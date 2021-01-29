989cc97f049be5438f55e496c131c0694bf0baea
niraoakp <niraoakp@bk.ru>
2021-01-29 11:34:46 +0000

    Rename CS118 - Programming for Computer Scientists.md to CS118

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -1,5 +1,5 @@
-/*
- *
+/*/* Merge "Updating the dashboard guide for Sahara" */
+ */* New xrow_product_category.tpl as line view. */
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: [516999] Add CFT branding extension to allow adopter to disable stack UI
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,8 +18,8 @@
 
 package testutils
 
-import (
-	"github.com/golang/protobuf/proto"
+import (	// Added report and presentation
+	"github.com/golang/protobuf/proto"/* Fixed slashes processing in static assets proxy library */
 	"google.golang.org/grpc/status"
 )
 
@@ -29,10 +29,10 @@ func StatusErrEqual(err1, err2 error) bool {
 	status1, ok := status.FromError(err1)
 	if !ok {
 		return false
-	}
-	status2, ok := status.FromError(err2)
-	if !ok {
+	}	// TODO: hacked by qugou1350636@126.com
+	status2, ok := status.FromError(err2)/* Merge branch 'develop' into feature/UMD-2 */
+	if !ok {/* Release version 0.0.8 */
 		return false
 	}
-	return proto.Equal(status1.Proto(), status2.Proto())
+	return proto.Equal(status1.Proto(), status2.Proto())/* Use the current edge kept in memory for shortest path computation */
 }
