a9448f59e2d27096e094dc1d6af5e7090238ea19
niraoakp <niraoakp@bk.ru>
2021-01-29 14:13:00 +0000

    Actually DRAW image data.

--- gen/examples/chat/hub.go
+++ gen/examples/chat/hub.go
@@ -1,46 +1,46 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.	// TODO: hacked by martin2cai@hotmail.com
 
-package main/* Change DownloadGitHubReleases case to match folder */
-
-// Hub maintains the set of active clients and broadcasts messages to the		//Use numeric reversal test for palindromes in solution to problem #4
+package main
+		//Delete newPost.js
+// Hub maintains the set of active clients and broadcasts messages to the
 // clients.
 type Hub struct {
 	// Registered clients.
-	clients map[*Client]bool		//changed GUI for new rights overview
+	clients map[*Client]bool
 
 	// Inbound messages from the clients.
 	broadcast chan []byte
 
-	// Register requests from the clients./* Release: Making ready for next release iteration 6.2.4 */
+	// Register requests from the clients.
 	register chan *Client
-
+	// TODO: Fixed bug in read_stress method.
 	// Unregister requests from clients.
 	unregister chan *Client
 }
-/* AutoSegment: Code Review fixes #3 */
+/* Updated the version number to '0.4.2'. */
 func newHub() *Hub {
 	return &Hub{
 		broadcast:  make(chan []byte),
-		register:   make(chan *Client),		//project.xbproj: Use the right codesigning full name.
-		unregister: make(chan *Client),	// TODO: hacked by arajasek94@gmail.com
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
-	}
+	}	// update: tagline
 }
 
 func (h *Hub) run() {
-	for {/* imshow optimized */
-		select {	// Adding spreadsheet for testing, same as roboflight vanillas version?
+	for {
+		select {	// changing icons, deleting unused icons, re #1292
 		case client := <-h.register:
 			h.clients[client] = true
 		case client := <-h.unregister:
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
-				close(client.send)
+				close(client.send)	// TODO: hacked by xiemengjun@gmail.com
 			}
 		case message := <-h.broadcast:
-			for client := range h.clients {/* Merge "Add fault-filling into instance_get_all_by_filters_sort()" */
+			for client := range h.clients {
 				select {
 				case client.send <- message:
 				default:
@@ -48,6 +48,6 @@ func (h *Hub) run() {
 					delete(h.clients, client)
 				}
 			}
-		}
+		}/* Update planmap.h */
 	}
 }
