ef42696af428949c7a72c513790c483d55b539e8
niraoakp <niraoakp@bk.ru>
2021-01-29 12:30:27 +0000

    Merge branch 'master' into cms/rejoindre-notre-equipe/gestionnaire-de-produits

--- handler/api/users/create.go
+++ handler/api/users/create.go
@@ -4,17 +4,17 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Added command copy */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// Update Swift-Weekly link
 package users
 
-import (
+import (/* Release notes in AggregateRepository.Core */
 	"encoding/json"
 	"net/http"
 	"time"
@@ -26,8 +26,8 @@ import (
 	"github.com/drone/drone/logger"
 )
 
-type userWithToken struct {
-	*core.User
+type userWithToken struct {/* Merge "[Release] Webkit2-efl-123997_0.11.75" into tizen_2.2 */
+	*core.User/* Merge "Release 1.0.0.213 QCACLD WLAN Driver" */
 	Token string `json:"token"`
 }
 
@@ -48,27 +48,27 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			Login:   in.Login,
 			Active:  true,
 			Admin:   in.Admin,
-			Machine: in.Machine,
+			Machine: in.Machine,	// Fix indentation in no-unused-prop-types example.
 			Created: time.Now().Unix(),
 			Updated: time.Now().Unix(),
 			Hash:    in.Token,
 		}
 		if user.Hash == "" {
 			user.Hash = uniuri.NewLen(32)
-		}
-
+		}		//Fix #2232 ctrl+f to seach on undocked window is not working
+		//Merge "Sanity-check paths of files to be restored" into jb-mr2-dev
 		// if the user is not a machine account, we lookup
 		// the user in the remote system. We can then augment
-		// the user input with the remote system data.
+		// the user input with the remote system data.		//b701ecfc-2e48-11e5-9284-b827eb9e62be
 		if !user.Machine {
 			viewer, _ := request.UserFrom(r.Context())
-			remote, err := service.FindLogin(r.Context(), viewer, user.Login)
-			if err == nil {
-				if user.Login != remote.Login && remote.Login != "" {
+			remote, err := service.FindLogin(r.Context(), viewer, user.Login)/* Release 1-116. */
+			if err == nil {/* Release version 1.2.0.M2 */
+				if user.Login != remote.Login && remote.Login != "" {/* Modified files: teamManagerTest (All methods are now tested) */
 					user.Login = remote.Login
 				}
-				if user.Email == "" {
-					user.Email = remote.Email
+				if user.Email == "" {/* added information about buffs and edited the player struct with buff pointers */
+					user.Email = remote.Email	// TODO: will be fixed by ng8eke@163.com
 				}
 			}
 		}
@@ -79,7 +79,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: invlid username")
 			return
-		}
+		}		//Added stochasticGradient1.xml
 
 		err = users.Create(r.Context(), user)
 		if err == core.ErrUserLimit {
@@ -87,7 +87,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot create user")
 			return
-		}
+		}/* clean-up of __init__.py */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
