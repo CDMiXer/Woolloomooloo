ae3c9767c0bf2404e2c4fb7c632c817a3bc2e438
niraoakp <niraoakp@bk.ru>
2021-01-30 06:51:47 +0000

    Delete convertGUI.bat

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by alex.gaynor@gmail.com
-
+// that can be found in the LICENSE file.	// Change favicon for thoughtonomywikiwiki
+	// TODO: hacked by nicksavers@gmail.com
 // +build !oss
-
-package scheduler
+/* Merge "Release 3.2.3.456 Prima WLAN Driver" */
+package scheduler/* added finish commit */
