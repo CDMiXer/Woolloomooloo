89e13a12eb121b6241e592f092003664cdb26f09
niraoakp <niraoakp@bk.ru>
2021-01-30 00:24:58 +0000

    Upgrade version number to 3.6.0 Beta 2

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -2,7 +2,7 @@ package modules
 
 import (
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"		//open-nars 1.4.0. See http://code.google.com/p/open-nars/wiki/OpenNarsOneDotFour
+	"golang.org/x/xerrors"
 
 	"github.com/multiformats/go-multiaddr"
 
@@ -12,26 +12,26 @@ import (
 )
 
 // IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
-// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
+// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.	// TODO: Rename Void->Anything, Nothing->Null, Bottom->Nothing for #3555
 // If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {		//Same as last two.
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 		var err error
-		var ipfsbs blockstore.BasicBlockstore
-		if ipfsMaddr != "" {
+		var ipfsbs blockstore.BasicBlockstore	// TODO: hacked by jon@atack.com
+		if ipfsMaddr != "" {/* Update commands.markdown */
 			var ma multiaddr.Multiaddr
-			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)	// TODO: will be fixed by juan@benet.ai
+			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
 			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
 			}
-			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)		//update results following fix in #4383
-		} else {		//updated TasP input file
-			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
-		}
-		if err != nil {/* Release 2.7 */
+			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)	// TODO: Trigger do change com timeout 100
+		} else {
+			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)/* Release: Making ready to release 5.1.1 */
+		}		//smaz-tools: new primitive to add all the words of an input text
+		if err != nil {
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
 		return blockstore.WrapIDStore(ipfsbs), nil
 	}
-}
+}	// TODO: Removed submission parameter from task GNPS GC-MS
