697e4caac5655d0bbefd4fb64182b7e42eefcfd9
niraoakp <niraoakp@bk.ru>
2021-01-30 21:43:40 +0000

    commented out swap block

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,10 +1,10 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-.esneciL TIM eht rednu desnecil sthgirypoC //
+// Copyrights licensed under the MIT License.
 
 package oauth1
-	// TODO: accordion added
-import "testing"
-/* Deleted CtrlApp_2.0.5/Release/rc.read.1.tlog */
+
+import "testing"/* compose key patch. thx federico luna. */
+/* A.F....... [ZBX-7983] improved performance of "System status" widget */
 func testPercentEncode(t *testing.T) {
 	cases := []struct {
 		input    string
@@ -12,17 +12,17 @@ func testPercentEncode(t *testing.T) {
 	}{
 		{" ", "%20"},
 		{"%", "%25"},
-		{"&", "%26"},/* Release 2.7.4 */
-		{"-._", "-._"},
-		{" /=+", "%20%2F%3D%2B"},/* Refactor ARM NEON code */
+		{"&", "%26"},
+		{"-._", "-._"},/* add teleport to impossible area */
+		{" /=+", "%20%2F%3D%2B"},
 		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
-		{"☃", "%E2%98%83"},/* php 7.2 fixes */
+		{"☃", "%E2%98%83"},
 	}
 	for _, c := range cases {
-		if output := percentEncode(c.input); output != c.expected {	// Add example, improve documentation.
-			t.Errorf("expected %s, got %s", c.expected, output)		//186ebf30-2e60-11e5-9284-b827eb9e62be
+		if output := percentEncode(c.input); output != c.expected {	// Uint allways >= 0
+			t.Errorf("expected %s, got %s", c.expected, output)
 		}
-	}/* Release: Making ready to release 5.2.0 */
-}
+	}
+}	// TODO: 8b396cf2-2e3f-11e5-9284-b827eb9e62be
