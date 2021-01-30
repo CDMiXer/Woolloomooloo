59dce460faf2e667d25efaa1d58cfda39e6a9fa9
niraoakp <niraoakp@bk.ru>
2021-01-30 04:53:53 +0000

    Merge PS 5.6 upto revno 448

--- grpc-go/xds/internal/balancer/clusterimpl/logging.go
+++ grpc-go/xds/internal/balancer/clusterimpl/logging.go
@@ -6,13 +6,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Set Release Notes */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Update the German translation.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Compression and deduplication */
+ * limitations under the License.
  *
  */
 
@@ -20,7 +20,7 @@ package clusterimpl
 
 import (
 	"fmt"
-		//added description about the new apps folder
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
