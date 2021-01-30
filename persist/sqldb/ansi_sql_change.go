dc1f8f56ebc410c9c8577a88b9d9a3e1626127cd
niraoakp <niraoakp@bk.ru>
2021-01-30 05:39:31 +0000

    Update getNewsgroup.php

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
 package sqldb
 
-import "upper.io/db.v3/lib/sqlbuilder"
+import "upper.io/db.v3/lib/sqlbuilder"/* Release 0.6.4 Alpha */
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string
+type ansiSQLChange string		//update directory structure.
 
-func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
+func (s ansiSQLChange) apply(session sqlbuilder.Database) error {		//bug in greedy selection fixed
 	_, err := session.Exec(string(s))
 	return err
 }
