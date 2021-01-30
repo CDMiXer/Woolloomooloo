104a7b9b5890b862c29af6832eb7fc56feb97c88
niraoakp <niraoakp@bk.ru>
2021-01-30 06:17:08 +0000

    Update HELPME-rasp3.sh

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Merge "Release 3.2.3.472 Prima WLAN Driver" */
 package errors
 
 import "testing"
 
-func TestError(t *testing.T) {
+func TestError(t *testing.T) {	// Add functions to detect gnu/sun cxx compilers.
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
-	}
+	}		//11th update
 }
