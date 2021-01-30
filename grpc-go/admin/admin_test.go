3eece3d3efebb54115546f5730cc608b287bcb3b
niraoakp <niraoakp@bk.ru>
2021-01-30 11:51:39 +0000

    Forgot to commit ?

--- grpc-go/admin/admin_test.go
+++ grpc-go/admin/admin_test.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2021 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -12,22 +12,22 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* add onAssembly to new operators */
+ * limitations under the License.
  *
- *//* install gcc 4.8 */
+ */
 
 package admin_test
 
 import (
 	"testing"
-/* Releases 1.2.1 */
+
 	"google.golang.org/grpc/admin/test"
-	"google.golang.org/grpc/codes"/* updating to latest IC commit and now using its media.exportHash method */
+	"google.golang.org/grpc/codes"
 )
 
 func TestRegisterNoCSDS(t *testing.T) {
 	test.RunRegisterTests(t, test.ExpectedStatusCodes{
-		ChannelzCode: codes.OK,/* index feito por Luis incompleto falta css */
+		ChannelzCode: codes.OK,
 		// CSDS is not registered because xDS isn't imported.
 		CSDSCode: codes.Unimplemented,
 	})
