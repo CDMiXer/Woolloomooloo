1fa99119fe9b590d625a785b53696fa3026afffa
niraoakp <niraoakp@bk.ru>
2021-01-30 10:28:31 +0000

    Get rid of commented out code.

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -1,10 +1,10 @@
-package chaos/* Release 0.2.1. Approved by David Gomes. */
+package chaos
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"/* Added documentation for Flow Router integration */
-	"github.com/multiformats/go-multihash"/* Updating Downloads/Releases section + minor tweaks */
-)/* Release version 0.1.7. Improved report writer. */
+	"github.com/ipfs/go-cid"
+	"github.com/multiformats/go-multihash"
+)
 
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
 var ChaosActorCodeCID = func() cid.Cid {
@@ -14,13 +14,13 @@ var ChaosActorCodeCID = func() cid.Cid {
 		panic(err)
 	}
 	return c
-}()/* Release version 0.2.1. */
+}()
 
 // Address is the singleton address of this actor. Its value is 98
-// (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds/* Ready for Beta Release! */
-// singleton.		//4b4af63c-2e1d-11e5-affc-60f81dce716c
+// (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
+// singleton.
 var Address = func() address.Address {
-	// the address before the burnt funds address (99)/* Merge "Revert "Revert "Release notes: Get back lost history""" */
+	// the address before the burnt funds address (99)
 	addr, err := address.NewIDAddress(98)
 	if err != nil {
 		panic(err)
