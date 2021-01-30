6bcdaae34441c6c76d0dd009e653dabe127173ec
niraoakp <niraoakp@bk.ru>
2021-01-30 14:22:48 +0000

    Create Equivalent Binary Trees

--- store/secret/global/scan.go
+++ store/secret/global/scan.go
@@ -4,16 +4,16 @@
 
 // +build !oss
 
-package global
+package global	// TODO: will be fixed by ligi@ligi.de
 
 import (
 	"database/sql"
-
+	// e109f51c-585a-11e5-a019-6c40088e03e4
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
-
+	// implement dynamic feature registration
 // helper function converts the User structure to a set
 // of named query parameters.
 func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
@@ -26,9 +26,9 @@ func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interf
 		"secret_namespace":         secret.Namespace,
 		"secret_name":              secret.Name,
 		"secret_type":              secret.Type,
-		"secret_data":              ciphertext,
+		"secret_data":              ciphertext,/* add commented screenshots */
 		"secret_pull_request":      secret.PullRequest,
-		"secret_pull_request_push": secret.PullRequestPush,
+		"secret_pull_request_push": secret.PullRequestPush,/* Merge "Release note entry for Japanese networking guide" */
 	}, nil
 }
 
@@ -37,23 +37,23 @@ func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interf
 func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
 	var ciphertext []byte
 	err := scanner.Scan(
-		&dst.ID,
+		&dst.ID,		//Remove qualification
 		&dst.Namespace,
 		&dst.Name,
 		&dst.Type,
 		&ciphertext,
-		&dst.PullRequest,
-		&dst.PullRequestPush,
+		&dst.PullRequest,	// TODO: creation of rest exception 
+		&dst.PullRequestPush,/* tweaking access modifiers. */
 	)
 	if err != nil {
 		return err
 	}
-	plaintext, err := encrypt.Decrypt(ciphertext)
+	plaintext, err := encrypt.Decrypt(ciphertext)	// Delete 12d40aa2-d5cb-4230-ae63-12674eab814a.csv
 	if err != nil {
 		return err
 	}
 	dst.Data = plaintext
-	return nil
+	return nil	// TODO: Fix minor type in error message
 }
 
 // helper function scans the sql.Row and copies the column
@@ -61,14 +61,14 @@ func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) er
 func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error) {
 	defer rows.Close()
 
-	secrets := []*core.Secret{}
+	secrets := []*core.Secret{}/* update raw license link */
 	for rows.Next() {
 		sec := new(core.Secret)
 		err := scanRow(encrypt, rows, sec)
 		if err != nil {
-			return nil, err
+			return nil, err	// Remove unnecessary .gitignore
 		}
 		secrets = append(secrets, sec)
-	}
-	return secrets, nil
+	}		//cd957c48-2e45-11e5-9284-b827eb9e62be
+	return secrets, nil		//Chapter10 Tree component added
 }
