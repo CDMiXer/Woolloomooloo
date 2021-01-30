f948d8b74f83f34cc203b0120149dc55260ea47a
niraoakp <niraoakp@bk.ru>
2021-01-30 19:54:39 +0000

    final second nav

--- pkg/cmd/pulumi/cancel.go
+++ pkg/cmd/pulumi/cancel.go
@@ -1,16 +1,16 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* change logs to botbot.me */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* DATASOLR-135 - Release version 1.1.0.RC1. */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Borrado archivo antiguo
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Release script is mature now. */
+// limitations under the License./* Release of eeacms/www-devel:19.4.1 */
 
 package main
 
@@ -21,24 +21,24 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"	// TODO: hacked by brosner@gmail.com
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"/* Delete expert-cancer-network.png */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
-
+/* Fix Tomato download link */
 func newCancelCmd() *cobra.Command {
-	var yes bool
+	var yes bool/* 8e53bb96-2e43-11e5-9284-b827eb9e62be */
 	var stack string
-	var cmd = &cobra.Command{
+	var cmd = &cobra.Command{/* add tooltip for indicators on branch name/pulldown */
 		Use:   "cancel [<stack-name>]",
-		Args:  cmdutil.MaximumNArgs(1),
+		Args:  cmdutil.MaximumNArgs(1),/* Merge "[Release] Webkit2-efl-123997_0.11.63" into tizen_2.2 */
 		Short: "Cancel a stack's currently running update, if any",
 		Long: "Cancel a stack's currently running update, if any.\n" +
 			"\n" +
 			"This command cancels the update currently being applied to a stack if any exists.\n" +
-			"Note that this operation is _very dangerous_, and may leave the stack in an\n" +
-			"inconsistent state if a resource operation was pending when the update was canceled.\n" +
+			"Note that this operation is _very dangerous_, and may leave the stack in an\n" +	// Merge "[install-guide] add nova compute section"
+			"inconsistent state if a resource operation was pending when the update was canceled.\n" +		//backport r21307 to backfire
 			"\n" +
 			"After this command completes successfully, the stack will be ready for further\n" +
 			"updates.",
@@ -48,11 +48,11 @@ func newCancelCmd() *cobra.Command {
 				if stack != "" {
 					return result.Error("only one of --stack or argument stack name may be specified, not both")
 				}
-
-				stack = args[0]
+/* Allow to change SortRow opacity */
+				stack = args[0]/* rename "series" to "ubuntuRelease" */
 			}
 
-			opts := display.Options{
+			opts := display.Options{	// repair relation import
 				Color: cmdutil.GetGlobalColorization(),
 			}
 
