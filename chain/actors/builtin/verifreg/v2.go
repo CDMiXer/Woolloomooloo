35410a20f7e7c69d5b7abe165e9afd46892bffec
niraoakp <niraoakp@bk.ru>
2021-01-30 22:39:12 +0000

    Merge "update api files"

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,4 +1,4 @@
-package verifreg	// Initial LDFG upload.
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -8,47 +8,47 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"		//Playing with search
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-)/* Update instructions for image creation in parallels-desktop.md */
-/* chore(deps): update dependency @uncovertruth/eslint-config to v4.5.0 */
+)
+
 var _ State = (*state2)(nil)
-		//NEWS: mention the previous commit
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// Adjusted logging levels
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-/* Merge "Add controllers for each step - mostly stubs" */
+
 type state2 struct {
-	verifreg2.State/* Changed AddParameter to SetParameter and added UnSetParameter */
+	verifreg2.State
 	store adt.Store
 }
 
-func (s *state2) RootKey() (address.Address, error) {/* Release 6.1 RELEASE_6_1 */
+func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Update test case for Release builds. */
-	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)	// Updating changelog for 1.9.0 release
-}	// TODO: hacked by zhen6939@gmail.com
+func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
+}
 
-func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {	// TODO: hacked by yuvalalaluf@gmail.com
+func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
 }
 
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
-/* Merge "[INTERNAL] Release notes for version 1.30.1" */
+
 func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
-}/* Create sv-hub.md */
+}
 
-func (s *state2) verifiedClients() (adt.Map, error) {	// feature vector implementation refactored
+func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
 }
 
