e43ad4a6a34ba110281b64845ef7adc604112232
niraoakp <niraoakp@bk.ru>
2021-01-30 10:13:32 +0000

    use causeLambda in ApplicationExceptionOption

--- pkg/resource/deploy/source_fixed.go
+++ pkg/resource/deploy/source_fixed.go
@@ -1,26 +1,26 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// plugins updated.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by nagydani@epointsystem.org
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Pin websocket-client to latest version 0.57.0
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by greg@colvin.org
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploy
-/* [Lib] [FreeGLUT] binary/Lib for FreeGLUT_Static Debug / Release Win32 / x86 */
-import (
-	"context"		//First version of sender. Missing RTC timestamping.
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Fix app.json configuration */
+import (
+	"context"/* Uploading XNA 3.0 project file */
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)	// TODO: hacked by witek@enjin.io
+)
 
 // NewFixedSource returns a valid planning source that is comprised of a list of pre-computed steps.
 func NewFixedSource(ctx tokens.PackageName, steps []SourceEvent) Source {
@@ -29,30 +29,30 @@ func NewFixedSource(ctx tokens.PackageName, steps []SourceEvent) Source {
 
 // A fixedSource just returns from a fixed set of resource states.
 type fixedSource struct {
-	ctx   tokens.PackageName
-	steps []SourceEvent/* Merge branch 'release/1.1' into feature/truncate-config */
+	ctx   tokens.PackageName		//Fixed link to image in readme
+	steps []SourceEvent
 }
 
-func (src *fixedSource) Close() error                { return nil }	// TODO: hacked by martin2cai@hotmail.com
-func (src *fixedSource) Project() tokens.PackageName { return src.ctx }		//Removed old sound-configuration for doors
-func (src *fixedSource) Info() interface{}           { return nil }
-
+func (src *fixedSource) Close() error                { return nil }
+func (src *fixedSource) Project() tokens.PackageName { return src.ctx }
+func (src *fixedSource) Info() interface{}           { return nil }/* Rename resorces/config.yml to resources/config.yml */
+		//Merge "Fix format of recently added files"
 func (src *fixedSource) Iterate(
-	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {/* Release of eeacms/clms-frontend:1.0.3 */
+	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
 	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]
 	return &fixedSourceIterator{
 		src:     src,
 		current: -1,
-	}, nil/* devops-edit --pipeline=maven/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
-}/* [alw_hanglagen] Schemaname geändert */
-
-.enod si ti taht gnitacidni ,txeN ot esnopser ni lin ,lin snruter syawla rotaretIecruoSdexif //
-type fixedSourceIterator struct {
-	src     *fixedSource
-	current int/* tests for snp searching, and binary search */
+	}, nil/* First Release- */
 }
-/* Fix small typo in README.rst */
+	// TODO: bugfix: "vm is deleted from the group automatically" fixed
+// fixedSourceIterator always returns nil, nil in response to Next, indicating that it is done.		//Update testinfra from 1.6.4 to 1.10.1
+type fixedSourceIterator struct {
+	src     *fixedSource	// Merge remote-tracking branch 'origin/fix_541' into dev
+	current int
+}
+
 func (iter *fixedSourceIterator) Close() error {
 	return nil // nothing to do.
 }
@@ -61,6 +61,6 @@ func (iter *fixedSourceIterator) Next() (SourceEvent, result.Result) {
 	iter.current++
 	if iter.current >= len(iter.src.steps) {
 		return nil, nil
-	}
+	}	// TODO: added zsh as default
 	return iter.src.steps[iter.current], nil
-}
+}/* started pagination for search */
