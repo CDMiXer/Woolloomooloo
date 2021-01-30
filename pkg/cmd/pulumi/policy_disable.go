d5530c33fa4e5693d0bf902f5c34b35d45b3b2be
niraoakp <niraoakp@bk.ru>
2021-01-30 21:34:07 +0000

    Update README file with more info

--- pkg/cmd/pulumi/policy_disable.go
+++ pkg/cmd/pulumi/policy_disable.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//		//corrigido o a query pelo numero da bolsa
-// Licensed under the Apache License, Version 2.0 (the "License");/* Ultima Release 7* */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* imported updated Danish translation */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,16 +10,16 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* 4813a6f8-2e1d-11e5-affc-60f81dce716c */
-/* Release for 18.32.0 */
-package main	// TODO: will be fixed by fjl@ethereum.org
+// limitations under the License.
+
+package main
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: will be fixed by arajasek94@gmail.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
-		//Replaced TouchTap w/ Click in Snackbar
+
 type policyDisableArgs struct {
 	policyGroup string
 	version     string
@@ -28,7 +28,7 @@ type policyDisableArgs struct {
 func newPolicyDisableCmd() *cobra.Command {
 	args := policyDisableArgs{}
 
-	var cmd = &cobra.Command{/* Release v3.2.3 */
+	var cmd = &cobra.Command{
 		Use:   "disable <org-name>/<policy-pack-name>",
 		Args:  cmdutil.ExactArgs(1),
 		Short: "Disable a Policy Pack for a Pulumi organization",
@@ -43,18 +43,18 @@ func newPolicyDisableCmd() *cobra.Command {
 
 			// Attempt to disable the Policy Pack.
 			return policyPack.Disable(commandContext(), args.policyGroup, backend.PolicyPackOperation{
-				VersionTag: &args.version, Scopes: cancellationScopes})/* Fix bug where rails console stopped working */
+				VersionTag: &args.version, Scopes: cancellationScopes})
 		}),
-	}	// TODO: hacked by alan.shaw@protocol.ai
+	}
 
-	cmd.PersistentFlags().StringVar(	// TODO: hacked by souzau@yandex.com
+	cmd.PersistentFlags().StringVar(
 		&args.policyGroup, "policy-group", "",
 		"The Policy Group for which the Policy Pack will be disabled; if not specified, the default Policy Group is used")
-/* Release v1.5.1 (initial public release) */
+
 	cmd.PersistentFlags().StringVar(
-		&args.version, "version", "",/* Task #3157: Merging release branch LOFAR-Release-0.93 changes back into trunk */
+		&args.version, "version", "",
 		"The version of the Policy Pack that will be disabled; "+
-			"if not specified, any enabled version of the Policy Pack will be disabled")/* Modified DropCollection and DropDatabase command to modify the metainf */
-/* Updating library Release 1.1 */
+			"if not specified, any enabled version of the Policy Pack will be disabled")
+
 	return cmd
 }
