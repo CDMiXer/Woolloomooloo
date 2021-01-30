51008f1136742c842a49c01f5a157889a8fdeed4
niraoakp <niraoakp@bk.ru>
2021-01-30 10:37:38 +0000

    update datajam

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,16 +1,16 @@
 package providers
 
 import (
-	"testing"
+	"testing"/* Game und Player stoppen */
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//[#41] - Add availability to game pop-up and search panel
 )
-
+	// TODO: Merge branch 'master' into circular-progress-drawnode-triangle-strip
 func TestProviderRequestNameNil(t *testing.T) {
-	req := NewProviderRequest(nil, "pkg")
+	req := NewProviderRequest(nil, "pkg")/* Create FacturaReleaseNotes.md */
 	assert.Equal(t, tokens.QName("default"), req.Name())
 	assert.Equal(t, "pkg", req.String())
 }
@@ -23,8 +23,8 @@ func TestProviderRequestNameNoPre(t *testing.T) {
 }
 
 func TestProviderRequestNameDev(t *testing.T) {
-	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
+	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")	// TODO: 08e13a6c-2e50-11e5-9284-b827eb9e62be
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
 	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
-}
+}/* Change to min-width & min-height */
