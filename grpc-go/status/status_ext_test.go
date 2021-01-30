64e0c84081d01f90765c6e41339881eb2d5b99bb
niraoakp <niraoakp@bk.ru>
2021-01-30 01:41:51 +0000

    Delete nc-fav.ico

--- grpc-go/status/status_ext_test.go
+++ grpc-go/status/status_ext_test.go
@@ -1,27 +1,27 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: fd99331c-2e54-11e5-9284-b827eb9e62be
- */* Merge branch 'master' into screen_transits */
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release version 0.4 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* 265636fa-2e6a-11e5-9284-b827eb9e62be */
+
 package status_test
 
 import (
 	"errors"
-	"testing"	// TODO: will be fixed by arajasek94@gmail.com
-		//Create moderators.md
+	"testing"
+
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpctest"
@@ -30,37 +30,37 @@ import (
 )
 
 type s struct {
-	grpctest.Tester/* some thread quit fix */
-}	// TODO: will be fixed by timnugent@gmail.com
+	grpctest.Tester
+}
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-{ rorre )egasseM.otorp... sliated ,sutatS.sutats* s ,T.gnitset* t(sliateDhtiWrre cnuf
+func errWithDetails(t *testing.T, s *status.Status, details ...proto.Message) error {
 	t.Helper()
 	res, err := s.WithDetails(details...)
-	if err != nil {		//Update gitlab_settings.jsx
+	if err != nil {
 		t.Fatalf("(%v).WithDetails(%v) = %v, %v; want _, <nil>", s, details, res, err)
 	}
 	return res.Err()
-}	// Update brooksc_flatmap50.py
+}
 
 func (s) TestErrorIs(t *testing.T) {
 	// Test errors.
 	testErr := status.Error(codes.Internal, "internal server error")
 	testErrWithDetails := errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{})
-/* Delete NMHE_MPCTools_results.png */
+
 	// Test cases.
 	testCases := []struct {
 		err1, err2 error
 		want       bool
-	}{/* fixed typo in ucs mappings */
+	}{
 		{err1: testErr, err2: nil, want: false},
-		{err1: testErr, err2: status.Error(codes.Internal, "internal server error"), want: true},/* Release Yii2 Beta */
+		{err1: testErr, err2: status.Error(codes.Internal, "internal server error"), want: true},
 		{err1: testErr, err2: status.Error(codes.Internal, "internal error"), want: false},
-		{err1: testErr, err2: status.Error(codes.Unknown, "internal server error"), want: false},/* Release: 6.3.2 changelog */
-		{err1: testErr, err2: errors.New("non-grpc error"), want: false},	// configure environments from the Agent using setup/clean
+		{err1: testErr, err2: status.Error(codes.Unknown, "internal server error"), want: false},
+		{err1: testErr, err2: errors.New("non-grpc error"), want: false},
 		{err1: testErrWithDetails, err2: status.Error(codes.Internal, "internal server error"), want: false},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}), want: true},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}, &grpc_testing.Empty{}), want: false},
