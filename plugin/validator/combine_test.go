1d44ea8d922766ea95671de0c1792603b3917d50
niraoakp <niraoakp@bk.ru>
2021-01-30 08:51:44 +0000

    Actors limbs mechanic is working now.

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* FORGE-1481: Added auto-completion for targetPackage */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package validator
 
 import (
-	"context"	// TODO: will be fixed by sbrichards@gmail.com
+	"context"
 	"errors"
-	"testing"	// Removed finish space
+	"testing"
 
-	"github.com/drone/drone/core"	// capital case
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
@@ -17,10 +17,10 @@ import (
 
 var noContext = context.Background()
 
-var mockFile = `		//Remove duplicate definition of Interval.
+var mockFile = `
 kind: pipeline
 type: docker
-name: testing		//Deleting duplicate js file
+name: testing
 `
 
 func TestCombine(t *testing.T) {
@@ -32,21 +32,21 @@ func TestCombine(t *testing.T) {
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
-	}	// TODO: Add bit.dc
+	}
 
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, args).Return(nil)
-	// TODO: Merge branch 'new-design' into nd/image-proxy
+
 	err := Combine(service).Validate(noContext, args)
 	if err != nil {
 		t.Error(err)
-	}/* Delete custom-trinity-64.png */
-}		//Create bingo
-/* Release Version */
-func TestCombineErr(t *testing.T) {/* Skip first brightness value */
+	}
+}
+
+func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release of eeacms/bise-backend:v10.0.26 */
+
 	resp := errors.New("")
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, nil).Return(resp)
