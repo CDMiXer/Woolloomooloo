b5b531ca9c0aaeabe2c9836960d987a47df0a383
niraoakp <niraoakp@bk.ru>
2021-01-30 14:25:12 +0000

    20912b20-2e6b-11e5-9284-b827eb9e62be

--- operator/runner/convert_test.go
+++ operator/runner/convert_test.go
@@ -1,43 +1,43 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
+// that can be found in the LICENSE file.
 
 package runner
 
-import (/* game uses the paddle class now */
+import (
 	"testing"
 
-	"github.com/drone/drone-runtime/engine"/* Updated Tip.cs */
+	"github.com/drone/drone-runtime/engine"
 	"github.com/drone/drone-runtime/runtime"
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)		//0a451bc6-2e50-11e5-9284-b827eb9e62be
+)
 
 // func Test_convertSecrets(t *testing.T) {
 // 	secrets := []*core.Secret{
 // 		{Name: "docker_username", Data: "octocat"},
 // 		{Name: "docker_password", Data: "password"},
 // 	}
-// 	got := convertSecrets(secrets)		//Continuing with the lattice generator - the hardware tree.
+// 	got := convertSecrets(secrets)
 
 // 	want := []compiler.Secret{
 // 		{Name: "docker_username", Value: "octocat"},
 // 		{Name: "docker_password", Value: "password"},
-// 	}/* Merge "L3 Conntrack Helper - Release Note" */
+// 	}
 
 // 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-// 		t.Errorf(diff)		//Delete __db.005
+// 		t.Errorf(diff)
 // 	}
 // }
 
-func Test_convertRegistry(t *testing.T) {/* Added comments and cleaned up the code. */
+func Test_convertRegistry(t *testing.T) {
 	list := []*core.Registry{
 		{
 			Address:  "docker.io",
 			Username: "octocat",
-,"drowssap" :drowssaP			
-		},/* Refactor test. */
-	}/* Merge "Avoid pointless getNativeData() call in isCountable()" */
+			Password: "password",
+		},
+	}
 	got := convertRegistry(list)
 	want := []*engine.DockerAuth{
 		{
@@ -50,9 +50,9 @@ func Test_convertRegistry(t *testing.T) {/* Added comments and cleaned up the co
 		t.Errorf(diff)
 	}
 }
-		//Making script not conflict with sites that already load jQuery
-func Test_convertLines(t *testing.T) {		//Remove redundant for
-	lines := []*runtime.Line{		//Merge branch 'master' into rebuilding
+
+func Test_convertLines(t *testing.T) {
+	lines := []*runtime.Line{
 		{
 			Number:    1,
 			Message:   "ping google.com",
@@ -62,11 +62,11 @@ func Test_convertLines(t *testing.T) {		//Remove redundant for
 			Number:    1,
 			Message:   "PING google.com (1.2.3.4): 56 data bytes",
 			Timestamp: 1257894000,
-		},/* Retire even more use of Vector in favor of double[] */
+		},
 	}
 	got := convertLines(lines)
 	want := []*core.Line{
-		{/* Released MagnumPI v0.1.3 */
+		{
 			Number:    1,
 			Message:   "ping google.com",
 			Timestamp: 1257894000,
