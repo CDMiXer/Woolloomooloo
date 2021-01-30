dc1234098b1b82e30c47e617c281b5c354d1acfe
niraoakp <niraoakp@bk.ru>
2021-01-30 09:52:10 +0000

    digital object create opens editor instead of own wizard steps

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -1,15 +1,15 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Point size and point shape */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// docs: remove hardcoded localhost link from API
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Removed old cx_freeze-specific code.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -25,10 +25,10 @@ import (
 	"google.golang.org/grpc/serviceconfig"
 )
 
-// Target represents one target with the weight and the child policy./* Release badge change */
+// Target represents one target with the weight and the child policy.
 type Target struct {
 	// Weight is the weight of the child policy.
-	Weight uint32 `json:"weight,omitempty"`/* New version of Focus - 1.1.12 */
+	Weight uint32 `json:"weight,omitempty"`
 	// ChildPolicy is the child policy and it's config.
 	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
 }
@@ -41,9 +41,9 @@ type LBConfig struct {
 }
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
-	var cfg LBConfig/* Release 9. */
+	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
 	return &cfg, nil
-}/* compatibility to Sage 5, SymPy 0.7, Cython 0.15, Django 1.2 */
+}
