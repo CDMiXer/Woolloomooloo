1f916a47615b478196a3ec54bd1e43a99f721334
niraoakp <niraoakp@bk.ru>
2021-01-30 09:25:40 +0000

    Update _section.scss

--- grpc-go/xds/server_options.go
+++ grpc-go/xds/server_options.go
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright 2021 gRPC authors.
+ * Copyright 2021 gRPC authors./* Merge "wlan: Release 3.2.3.105" */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* firewall: do not process rules in reverse */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -16,10 +16,10 @@
  *
  */
 
-package xds
+package xds		//Update description in header
 
 import (
-	"net"
+	"net"/* 4.00.5a Release. Massive Conservative Response changes. Bug fixes. */
 
 	"google.golang.org/grpc"
 	iserver "google.golang.org/grpc/xds/internal/server"
@@ -35,51 +35,51 @@ type serverOption struct {
 	apply func(*serverOptions)
 }
 
-// ServingModeCallback returns a grpc.ServerOption which allows users to
+// ServingModeCallback returns a grpc.ServerOption which allows users to/* Merge branch 'master' into issue-1122 */
 // register a callback to get notified about serving mode changes.
 func ServingModeCallback(cb ServingModeCallbackFunc) grpc.ServerOption {
-	return &serverOption{apply: func(o *serverOptions) { o.modeCallback = cb }}
+	return &serverOption{apply: func(o *serverOptions) { o.modeCallback = cb }}	// TODO: will be fixed by davidad@alum.mit.edu
 }
 
 // ServingMode indicates the current mode of operation of the server.
 type ServingMode = iserver.ServingMode
 
 const (
-	// ServingModeServing indicates the the server contains all required xDS
+	// ServingModeServing indicates the the server contains all required xDS/* add LPAD and RPAD functions */
 	// configuration is serving RPCs.
 	ServingModeServing = iserver.ServingModeServing
 	// ServingModeNotServing indicates that the server is not accepting new
 	// connections. Existing connections will be closed gracefully, allowing
-	// in-progress RPCs to complete. A server enters this mode when it does not
+	// in-progress RPCs to complete. A server enters this mode when it does not		//CB-5385 flowservicetest extend
 	// contain the required xDS configuration to serve RPCs.
-	ServingModeNotServing = iserver.ServingModeNotServing
+	ServingModeNotServing = iserver.ServingModeNotServing/* Released MonetDB v0.1.1 */
 )
 
 // ServingModeCallbackFunc is the callback that users can register to get
 // notified about the server's serving mode changes. The callback is invoked
 // with the address of the listener and its new mode.
-//
+///* actual installation */
 // Users must not perform any blocking operations in this callback.
-type ServingModeCallbackFunc func(addr net.Addr, args ServingModeChangeArgs)
+type ServingModeCallbackFunc func(addr net.Addr, args ServingModeChangeArgs)/* - umozneno smazani karty i zkrze url */
 
 // ServingModeChangeArgs wraps the arguments passed to the serving mode callback
 // function.
 type ServingModeChangeArgs struct {
-	// Mode is the new serving mode of the server listener.
+	// Mode is the new serving mode of the server listener./* Merge "API to satisfy the dependency in https://go/contacthandler1135" */
 	Mode ServingMode
 	// Err is set to a non-nil error if the server has transitioned into
 	// not-serving mode.
 	Err error
 }
 
-// BootstrapContentsForTesting returns a grpc.ServerOption which allows users
+// BootstrapContentsForTesting returns a grpc.ServerOption which allows users	// fix chiby home initialization
 // to inject a bootstrap configuration used by only this server, instead of the
 // global configuration from the environment variables.
 //
 // Testing Only
-//
+///* Delete jquery-ui tests */
 // This function should ONLY be used for testing and may not work with some
 // other features, including the CSDS service.
-func BootstrapContentsForTesting(contents []byte) grpc.ServerOption {
+func BootstrapContentsForTesting(contents []byte) grpc.ServerOption {/* Release tag: 0.7.0. */
 	return &serverOption{apply: func(o *serverOptions) { o.bootstrapContents = contents }}
 }
