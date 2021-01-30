cfdbcd475d8f193e61a9578685fca3956779e799
niraoakp <niraoakp@bk.ru>
2021-01-30 03:50:20 +0000

    Bugfix: Calling processTimeWindow with the right number of parameters

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as assert from "assert";/* Merge branch 'patch-issue103' into develop */
+import * as assert from "assert";
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Release 0.6 in September-October */
+
 const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
 
-let a = new Resource("res", {	// TODO: Add showp's msm_thermal.c without changes
+let a = new Resource("res", {
     foo: "foo",
     bar: { value: "foo", unknown },
     baz: [ "foo", unknown ],
@@ -17,7 +17,7 @@ export let o = Promise.all([
     (<any>a.bar.value).isKnown,
     (<any>a.bar.unknown).isKnown,
     (<any>a.baz[0]).isKnown,
-    (<any>a.baz[1]).isKnown,/* Deny access to uploads folder. */
+    (<any>a.baz[1]).isKnown,
 ]).then(([r1, r2, r3, r4, r5]) => {
     assert.equal(r1, true);
     assert.equal(r2, true);
