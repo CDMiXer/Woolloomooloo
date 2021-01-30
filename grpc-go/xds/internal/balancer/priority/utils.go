ca618cf3a642c057a331082457e909703be63351
niraoakp <niraoakp@bk.ru>
2021-01-30 05:03:40 +0000

    add pjsip show contacts action and events

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2021 gRPC authors./* Intro of mesh refinement */
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,10 +10,10 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Making errors more visible + syntax
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Updating Release Notes */
+ *
  */
 
 package priority
@@ -24,8 +24,8 @@ func equalStringSlice(a, b []string) bool {
 	}
 	for i := range a {
 		if a[i] != b[i] {
-			return false/* Release 1.3 header */
+			return false
 		}
-	}		//Add script for War Chariot
+	}
 	return true
 }
