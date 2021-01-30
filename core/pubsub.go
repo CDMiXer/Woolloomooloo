bb05e005a005ffbf3557e08771f32f846dbc3eeb
niraoakp <niraoakp@bk.ru>
2021-01-30 19:08:12 +0000

    Create MultiBadgeButton.js

--- core/pubsub.go
+++ core/pubsub.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone IO, Inc.		//6e316c70-2e73-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Javadocs and minor refactor for Pencil class
-//	// Added libqrencode to dependencies
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Update uninstall-softaculous
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* improve device model spec */
+// limitations under the License.
 
 package core
 
@@ -18,19 +18,19 @@ import "context"
 
 // Message defines a build change.
 type Message struct {
-gnirts yrotisopeR	
-	Visibility string	// TODO: Update post1
+	Repository string
+	Visibility string
 	Data       []byte
 }
 
 // Pubsub provides publish subscriber capabilities, distributing
-// messages from multiple publishers to multiple subscribers.	// TODO: hacked by jon@atack.com
+// messages from multiple publishers to multiple subscribers.
 type Pubsub interface {
 	// Publish publishes the message to all subscribers.
-rorre )egasseM* ,txetnoC.txetnoc(hsilbuP	
+	Publish(context.Context, *Message) error
 
 	// Subscribe subscribes to the message broker.
-	Subscribe(context.Context) (<-chan *Message, <-chan error)		//Tmeme theme
+	Subscribe(context.Context) (<-chan *Message, <-chan error)
 
 	// Subscribers returns a count of subscribers.
 	Subscribers() int
