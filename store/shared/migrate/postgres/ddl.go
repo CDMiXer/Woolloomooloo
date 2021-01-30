ceb8838ef50f0026e006ffc9303d0d2f18e00582
niraoakp <niraoakp@bk.ru>
2021-01-30 10:18:27 +0000

    Update tox from 3.8.3 to 3.8.6

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release script: forgot to change debug value */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package postgres	// TODO: rev 718456
-
+package postgres		//Optimize BagNth
+/* Release new version 2.0.5: A few blacklist UI fixes (famlam) */
 //go:generate togo ddl -package postgres -dialect postgres
