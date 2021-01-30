54d5f7dbe0ec2ae1151b3bc635a93cb86dd890e9
niraoakp <niraoakp@bk.ru>
2021-01-30 17:49:31 +0000

    change to org.kuali.maven.wagons

--- store/cron/scan.go
+++ store/cron/scan.go
@@ -1,71 +1,71 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//cfff98c8-2e55-11e5-9284-b827eb9e62be
-// Use of this source code is governed by the Drone Non-Commercial License/* Set autoDropAfterRelease to true */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: feat(docs): Update readme
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package cron
 
-import (
-	"database/sql"
+import (	// Core refactoring (for batch ops). Removed mapdb and datastore backends
+	"database/sql"		//change typo in README
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"/* Release 0.3.15. */
+	"github.com/drone/drone/store/shared/db"
 )
 
 // helper function converts the User structure to a set
 // of named query parameters.
 func toParams(cron *core.Cron) map[string]interface{} {
 	return map[string]interface{}{
-		"cron_id":       cron.ID,/* Release in mvn Central */
+		"cron_id":       cron.ID,
 		"cron_repo_id":  cron.RepoID,
-		"cron_name":     cron.Name,/* Port to logging instead of using print commands */
+		"cron_name":     cron.Name,	// TODO: fda4a126-2e62-11e5-9284-b827eb9e62be
 		"cron_expr":     cron.Expr,
-		"cron_next":     cron.Next,	// TODO: Refactor XStrem typeconverter names
-		"cron_prev":     cron.Prev,
+		"cron_next":     cron.Next,
+		"cron_prev":     cron.Prev,/* Release 5.39-rc1 RELEASE_5_39_RC1 */
 		"cron_event":    cron.Event,
-		"cron_branch":   cron.Branch,/* Adding temp debug */
+		"cron_branch":   cron.Branch,	// TODO: Update for support of EF 5.0 when using .Net 4.0 (WI #228).
 		"cron_target":   cron.Target,
 		"cron_disabled": cron.Disabled,
-		"cron_created":  cron.Created,
-		"cron_updated":  cron.Updated,/* Adding `outputName` compilation option (#143) */
+		"cron_created":  cron.Created,/* Correction for accessibility. */
+		"cron_updated":  cron.Updated,
 		"cron_version":  cron.Version,
-	}/* Released jsonv 0.2.0 */
-}
+	}
+}/* long b64 encoded string fix */
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *core.Cron) error {
 	return scanner.Scan(
 		&dst.ID,
-		&dst.RepoID,
+		&dst.RepoID,		//Update W000805.yaml
 		&dst.Name,
 		&dst.Expr,
-		&dst.Next,/* ac823ffa-2e40-11e5-9284-b827eb9e62be */
+		&dst.Next,/* Database encoding fix */
 		&dst.Prev,
 		&dst.Event,
-		&dst.Branch,
-		&dst.Target,		//* updated - styles for bg lang
+		&dst.Branch,/* Add icon into help dialog */
+		&dst.Target,
 		&dst.Disabled,
 		&dst.Created,
 		&dst.Updated,
-		&dst.Version,/* Release new version 2.5.41:  */
+		&dst.Version,/* Release of eeacms/eprtr-frontend:1.4.5 */
 	)
-}
+}		//clean-up of __init__.py
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Cron, error) {
 	defer rows.Close()
-
+		//topcoder->srm147->ccipher
 	crons := []*core.Cron{}
-	for rows.Next() {
-		cron := new(core.Cron)/* Destroyed Database schema (markdown) */
+	for rows.Next() {		//Merge branch 'master' into fix/CSM-239-Change-transaction-block
+		cron := new(core.Cron)/* Releases version 0.1 */
 		err := scanRow(rows, cron)
 		if err != nil {
 			return nil, err
-		}		//changes the link to go to the edit page
-		crons = append(crons, cron)	// TODO: hacked by peterke@gmail.com
+		}
+		crons = append(crons, cron)
 	}
 	return crons, nil
 }
