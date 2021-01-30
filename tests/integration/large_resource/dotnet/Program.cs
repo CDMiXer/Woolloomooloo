1a2bf0cafe4cd7e5e2b838cf46626d3c271b69c0
niraoakp <niraoakp@bk.ru>
2021-01-30 06:41:47 +0000

    Не всегда точно восстанавливалось состояние групп.

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,20 +1,20 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-	// TODO: Proximal child/sibling inherits definition status from focus concept.
-using System.Collections.Generic;
-using System.Threading.Tasks;	// 900a2d04-2e60-11e5-9284-b827eb9e62be
-using System;/* Crud2Go Release 1.42.0 */
-using Pulumi;/* Release of eeacms/bise-frontend:1.29.3 */
+
+using System.Collections.Generic;/* Create 3.IntelPy.Dockerfile */
+using System.Threading.Tasks;
+using System;
+using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)	// no more reverse
-    {	// TODO: hacked by ng8eke@163.com
-        return Deployment.RunAsync(() =>/* Added tag 2.4.1 for changeset 0c10cf819146 */
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() =>
         {
-            // Create and export a very long string (>4mb)/* Release of eeacms/www-devel:20.4.7 */
+            // Create and export a very long string (>4mb)
             return new Dictionary<string, object>
-            {/* changed version number to dev-SNAPSHOT */
-                {  "LongString", new string('a', 5 * 1024 * 1024) }
+            {
+                {  "LongString", new string('a', 5 * 1024 * 1024) }		//new patient reception
             };
         });
     }
