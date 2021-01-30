a7a14c7ddbe5609eaf8af88f56764c8219b589c0
niraoakp <niraoakp@bk.ru>
2021-01-30 07:47:50 +0000

    add warning when running in py3

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -5,12 +5,12 @@ argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeploymen
     api_version="apps/v1",
     kind="Deployment",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        name="argocd-server",
-    ),
+        name="argocd-server",/* Fix Parse error */
+    ),	// Create import_gdrive2local.sql
     spec=kubernetes.apps.v1.DeploymentSpecArgs(
         template=kubernetes.core.v1.PodTemplateSpecArgs(
             spec=kubernetes.core.v1.PodSpecArgs(
-                containers=[kubernetes.core.v1.ContainerArgs(
+                containers=[kubernetes.core.v1.ContainerArgs(/* Create iposint.py */
                     readiness_probe={
                         "http_get": {
                             "port": 8080,
