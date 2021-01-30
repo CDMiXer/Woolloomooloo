7d92b1c475a222e8f006858a6bca6ebf688a3ad4
niraoakp <niraoakp@bk.ru>
2021-01-30 13:15:37 +0000

    REFS #5: Configurando integração do jacoco com o lombok.

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -4,7 +4,7 @@
 
 // +build !oss
 
-package livelog/* Create ViewOverAllFeedbackBean */
+package livelog
 
 import (
 	"testing"
@@ -19,28 +19,28 @@ func TestSubscription_publish(t *testing.T) {
 	}
 
 	e := new(core.Line)
-	s.publish(e)	// TODO: Delete useless file.
+	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want log entry received from channel")
-	}/* Release: Making ready for next release cycle 5.0.4 */
+	}
 	if got, want := len(s.handler), 0; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)		//fix a floating around mutexattr object
-	}/* Updated code to conform with code standards/style. */
+		t.Errorf("Want buffered channel size %d, got %d", want, got)
+	}
 }
 
 func TestSubscription_buffer(t *testing.T) {
-	s := &subscriber{	// Update pjsip trunk to latest version.
+	s := &subscriber{
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
 	}
 
 	// the buffer size is 1 to simulate what happens
-	// if the subscriber cannot keep up with processing	// TODO: will be fixed by martin2cai@hotmail.com
-senil ,esac siht nI .pu sllif reffub eht dna //	
+	// if the subscriber cannot keep up with processing
+	// and the buffer fills up. In this case, lines
 	// should be ignored until pending lines are
 	// processed.
 
@@ -49,32 +49,32 @@ senil ,esac siht nI .pu sllif reffub eht dna //
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
-)e(hsilbup.s	
+	s.publish(e)
 
-	if got, want := len(s.handler), 1; got != want {		//Create GETCH_e_GETCHE.c
+	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-}	// f768cd1a-2e52-11e5-9284-b827eb9e62be
+}
 
 func TestSubscription_stop(t *testing.T) {
-	s := &subscriber{		//Merge "Don't hit the API when creating a PageList"
+	s := &subscriber{
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
-	}		//Rename plan-wednesdey to plan-wednesdey.rst
+	}
 
 	if got, want := s.closed, false; got != want {
-		t.Errorf("Want subscription open")	// 31cd48c4-2e75-11e5-9284-b827eb9e62be
+		t.Errorf("Want subscription open")
 	}
 
 	s.close()
 	if got, want := s.closed, true; got != want {
 		t.Errorf("Want subscription closed")
-	}	// TODO: will be fixed by witek@enjin.io
+	}
 
 	// if the subscription is closed we should
 	// ignore any new events being published.
 
-	e := new(core.Line)/* Release documentation */
+	e := new(core.Line)
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
