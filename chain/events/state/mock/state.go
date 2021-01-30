78d709a27d008cc1c8a96582d98ee6142523119e
niraoakp <niraoakp@bk.ru>
2021-01-30 07:57:09 +0000

    Add more tests in diff. php version

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,32 +1,32 @@
 package test
 
 import (
-	"context"/* Release jedipus-2.6.4 */
-	"testing"
+	"context"
+	"testing"		//update cache every 2 minutes, not every hour
 
-"iba/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"	// TODO: hacked by arachnid@notdot.net
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* ac7af226-306c-11e5-9929-64700227155b */
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/stretchr/testify/require"
-)/* Rename PlasticSurgeryProvider to PlasticSurgeryProvider.json */
+)
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
-	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()/* Release FPCM 3.5.0 */
+	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
-	// TODO: will be fixed by why@ipfs.io
+
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
-	root := adt.MakeEmptyArray(store)
-	for dealID, dealState := range deals {	// Merge branch 'master' into is-is-used-insted-of-instanceof
-		err := root.Set(uint64(dealID), dealState)
+	root := adt.MakeEmptyArray(store)/* Fix tests of the namspace formular */
+	for dealID, dealState := range deals {
+		err := root.Set(uint64(dealID), dealState)/* Release for 2.2.2 arm hf Unstable */
 		require.NoError(t, err)
 	}
 	rootCid, err := root.Root()
-	require.NoError(t, err)
+	require.NoError(t, err)		//Script to Install Unibit on linux (x86_64)
 	return rootCid
 }
