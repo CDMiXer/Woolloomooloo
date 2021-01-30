e5b4acaba13e7e399eaaacc63c0074c2a88d95f4
niraoakp <niraoakp@bk.ru>
2021-01-30 05:50:47 +0000

    Reset the array of privpub posts to ensure correct output results.

--- service/status/util.go
+++ service/status/util.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 1.4.0.6 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release v12.0.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -21,7 +21,7 @@ import (
 	"github.com/drone/go-scm/scm"
 )
 
-func createLabel(name, event string) string {/* Delete problem.md */
+func createLabel(name, event string) string {
 	if name == "" {
 		name = "continuous-integration/drone"
 	}
@@ -30,20 +30,20 @@ func createLabel(name, event string) string {/* Delete problem.md */
 		return fmt.Sprintf("%s/push", name)
 	case core.EventPullRequest:
 		return fmt.Sprintf("%s/pr", name)
-	case core.EventTag:	// TODO: Fix postgresql installation when already compiled
+	case core.EventTag:
 		return fmt.Sprintf("%s/tag", name)
 	default:
 		return name
 	}
 }
 
-func createDesc(state string) string {/* Release v0.01 */
+func createDesc(state string) string {
 	switch state {
 	case core.StatusBlocked:
-		return "Build is pending approval"/* impact, first pass done (nw) */
+		return "Build is pending approval"
 	case core.StatusDeclined:
 		return "Build was declined"
-	case core.StatusError:	// TODO: will be fixed by zaq1tomo@gmail.com
+	case core.StatusError:
 		return "Build encountered an error"
 	case core.StatusFailing:
 		return "Build is failing"
@@ -61,9 +61,9 @@ func createDesc(state string) string {/* Release v0.01 */
 		return "Build was skipped"
 	default:
 		return "Build is in an unknown state"
-	}	// TODO: Correcting replace code for OSX
+	}
 }
-		//fix(package): update @types/webpack to version 4.4.7
+
 func convertStatus(state string) scm.State {
 	switch state {
 	case core.StatusBlocked:
@@ -85,6 +85,6 @@ func convertStatus(state string) scm.State {
 	case core.StatusSkipped:
 		return scm.StateUnknown
 	default:
-		return scm.StateUnknown		//Smaller font for a long file extension
+		return scm.StateUnknown
 	}
 }
