8b2686ccc197731294f7194993f5ba2f4483d126
niraoakp <niraoakp@bk.ru>
2021-01-30 08:11:35 +0000

    Move permission check out of Coupon class, and into caller (#2076)

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,12 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-
+	// Update yml format.
 const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
-});
+});	// TODO: update to jinja2 to resolve security issues
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
@@ -17,5 +17,5 @@ const storageAccountResource = new azure.storage.Account("storageAccountResource
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
     accountReplicationType: storageAccountTypeReplicationParam,
-});
+});/* Merge "[bugfix] Test for existing users for wikipedia family only" */
 export const storageAccountNameOut = storageAccountResource.name;
