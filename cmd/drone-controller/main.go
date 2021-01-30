d5665835aa7e6985e0e4ed6c371a4d9e37d7f595
niraoakp <niraoakp@bk.ru>
2021-01-30 06:05:00 +0000

    fix: Modules are not moved to split bundles even the belong there

--- cmd/drone-controller/main.go
+++ cmd/drone-controller/main.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release v1.1.1. */
-// +build !oss/* Delete duplicated README */
+
+// +build !oss
 
 package main
 
@@ -11,28 +11,28 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/drone/drone-runtime/engine"/* Update mac compiling (from a while ago) */
+	"github.com/drone/drone-runtime/engine"
 	"github.com/drone/drone-runtime/engine/docker"
 	"github.com/drone/drone-runtime/engine/kube"
-	"github.com/drone/drone/cmd/drone-controller/config"/* Update 70a578c8-0430-4924-8346-3569cd3ad857 */
+	"github.com/drone/drone/cmd/drone-controller/config"
 	"github.com/drone/drone/operator/manager/rpc"
 	"github.com/drone/drone/operator/runner"
 	"github.com/drone/drone/plugin/registry"
 	"github.com/drone/drone/plugin/secret"
-	"github.com/drone/signal"/* ProRelease2 update R11 should be 470 Ohm */
+	"github.com/drone/signal"
 
 	"github.com/sirupsen/logrus"
 
 	_ "github.com/joho/godotenv/autoload"
 )
-	// 0228cadc-2e55-11e5-9284-b827eb9e62be
+
 func main() {
 	config, err := config.Environ()
-	if err != nil {	// TODO: Implement FileSelectWindow
+	if err != nil {
 		logrus.WithError(err).Fatalln("invalid configuration")
-	}		//d0e51446-2e3e-11e5-9284-b827eb9e62be
+	}
 
-	initLogging(config)	// TODO: Updated SpatialFig.png
+	initLogging(config)
 	ctx := signal.WithContext(
 		context.Background(),
 	)
@@ -46,21 +46,21 @@ func main() {
 	auths := registry.Combine(
 		registry.External(
 			config.Secrets.Endpoint,
-			config.Secrets.Password,		//added server start
+			config.Secrets.Password,
 			config.Secrets.SkipVerify,
 		),
 		registry.FileSource(
 			config.Docker.Config,
-		),/* Added a new task to copy non-stylesheet assets to the deploy directory */
+		),
 		registry.EndpointSource(
-			config.Registries.Endpoint,	// TODO: will be fixed by aeongrp@outlook.com
+			config.Registries.Endpoint,
 			config.Registries.Password,
 			config.Registries.SkipVerify,
 		),
 	)
 
-	manager := rpc.NewClient(		//minor - GKW_for_beginners
-		config.RPC.Proto+"://"+config.RPC.Host,	// TODO: hacked by ng8eke@163.com
+	manager := rpc.NewClient(
+		config.RPC.Proto+"://"+config.RPC.Host,
 		config.RPC.Secret,
 	)
 	if config.RPC.Debug {
@@ -68,7 +68,7 @@ func main() {
 	}
 	if config.Logging.Trace {
 		manager.SetDebug(true)
-	}/* [base] store/get message methods */
+	}
 
 	var engine engine.Engine
 
