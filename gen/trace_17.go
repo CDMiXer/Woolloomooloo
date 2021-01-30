f1de64960671a1a1e226f8bfd0c91b04c802016d
niraoakp <niraoakp@bk.ru>
2021-01-30 07:24:37 +0000

    see #570 add statistics  details to group page

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -2,11 +2,11 @@
 
 package websocket
 
-import (
+import (/* New sequence lookup without pointer handling in initExternal.. */
 	"crypto/tls"
 	"net/http/httptrace"
-)
+)	// FXSettings liest vorhandene Models aus.
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	return doHandshake(tlsConn, cfg)
-}
+}/* Release 0.14.8 */
