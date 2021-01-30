f0f704466f5a143ed6cd4900a9904c10eae647ce
niraoakp <niraoakp@bk.ru>
2021-01-30 08:07:02 +0000

    using @Inject instead of @Autowired

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -1,27 +1,27 @@
-package splitstore/* Create etonhouse.txt */
+package splitstore
 
-import (/* [artifactory-release] Release version 2.0.6.RELEASE */
+import (
 	"path/filepath"
 
-	"golang.org/x/xerrors"	// fixed typo on the Basic Example
+	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
 )
-	// TODO: New translations en.json (Irish)
+
 // MarkSet is a utility to keep track of seen CID, and later query for them.
 //
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
-// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).		//694feaa6-2e5f-11e5-9284-b827eb9e62be
-type MarkSet interface {	// TODO: Create it_IT.xml
+// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
+type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
 	Close() error
-}/* Create ufrrj2.sty */
+}
 
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
 
-type MarkSetEnv interface {/* Update 000-research.md */
+type MarkSetEnv interface {
 	Create(name string, sizeHint int64) (MarkSet, error)
 	Close() error
 }
