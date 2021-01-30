9e5477cc6355447a78e1468cb04a75a0992c1222
niraoakp <niraoakp@bk.ru>
2021-01-30 19:32:50 +0000

    Backout changeset e6bdb879fa8c701136364ef5847449d2378de0a4

--- pkg/codegen/docs/utils.go
+++ pkg/codegen/docs/utils.go
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.		//add uploads directory
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Added level command.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release 0.8.5. */
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the/* Merge "Fix monkey bug 2512055" */
+
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
 // nolint: lll, goconst
@@ -20,9 +20,9 @@ package docs
 
 import (
 	"strings"
-	"unicode"	// TODO: will be fixed by mail@overlisted.net
-		//Wait a second, that method doesn't return an array
-	"github.com/pulumi/pulumi/pkg/v2/codegen/dotnet"/* Added Travis Build Status */
+	"unicode"
+
+	"github.com/pulumi/pulumi/pkg/v2/codegen/dotnet"
 	go_gen "github.com/pulumi/pulumi/pkg/v2/codegen/go"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
@@ -31,7 +31,7 @@ func isDotNetTypeNameBoundary(prev rune, next rune) bool {
 	// For C# type names, which are PascalCase are qualified using "." as the separator.
 	return prev == rune('.') && unicode.IsUpper(next)
 }
-/* Support other payload types other than an array of text */
+
 func isPythonTypeNameBoundary(prev rune, next rune) bool {
 	// For Python, names are snake_cased (Duh?).
 	return (prev == rune('_') && unicode.IsLower(next))
@@ -41,15 +41,15 @@ func isPythonTypeNameBoundary(prev rune, next rune) bool {
 func wbr(s string) string {
 	var runes []rune
 	var prev rune
-	for i, r := range s {/* Release trunk... */
-		if i != 0 &&		//PML input: Mark <a> as block level element.
+	for i, r := range s {
+		if i != 0 &&
 			// For TS, JS and Go, property names are camelCase and types are PascalCase.
 			((unicode.IsLower(prev) && unicode.IsUpper(r)) ||
 				isDotNetTypeNameBoundary(prev, r) ||
-				isPythonTypeNameBoundary(prev, r)) {/* Release version 0.2.1 */
+				isPythonTypeNameBoundary(prev, r)) {
 			runes = append(runes, []rune("<wbr>")...)
-		}	// TODO: will be fixed by sbrichards@gmail.com
-		runes = append(runes, r)/* Remove my attempt at Alexa for bgnow */
+		}
+		runes = append(runes, r)
 		prev = r
 	}
 	return string(runes)
