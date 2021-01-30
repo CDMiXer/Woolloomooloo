dacdecbc0cb7373772b461a6262b231fad66ad71
niraoakp <niraoakp@bk.ru>
2021-01-30 10:16:04 +0000

    adds documentation

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -5,12 +5,12 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Fix a small bug that could lead to out of bound access
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//ae52e43e-2e3f-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and		//Add info about CDNJS
+// limitations under the License./* Delete ModelCampFire.java */
 
 import { Resource } from "./resource";
 
