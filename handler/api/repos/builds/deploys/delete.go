f34d94af0992921c3606d7ac5e9d3d3c860d5d73
niraoakp <niraoakp@bk.ru>
2021-01-30 21:55:52 +0000

    Add generic .ods support.

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,62 +1,62 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Require JSON in webpay_errors to run by itself
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//final images?
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Improve error message, props simonwheatley, fixes #8397 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Changed Month of Release */
 // limitations under the License.
 
-package deploys	// Update and rename Owner_phoneModel.py to Owner_phone.py
-/* Merge "Fix obsolete advice in RelativeLayout's documentation." */
+package deploys
+
 import (
 	"net/http"
-	// TODO: added shields.io buttons to README.md
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release 0.13.0 - closes #3 closes #5 */
-	"github.com/drone/drone/logger"/* removing alpha versions from dependencies */
-/* Merge "diag: Release wakeup sources correctly" */
-	"github.com/go-chi/chi"
-)/* Modified some build settings to make Release configuration actually work. */
+	"github.com/drone/drone/handler/api/render"		//updated boat site link
+	"github.com/drone/drone/logger"
+
+	"github.com/go-chi/chi"	// TODO: hacked by steven@stebalien.com
+)
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,		//02140fe6-2e43-11e5-9284-b827eb9e62be
 	builds core.BuildStore,
-) http.HandlerFunc {	// TODO: Readme: Simplify introduction
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")	// TODO: no params is nil
-			target    = chi.URLParam(r, "*")		//b30c82f0-2e4e-11e5-9284-b827eb9e62be
+			namespace = chi.URLParam(r, "owner")/* Use sync queue instead of PushService */
+			name      = chi.URLParam(r, "name")/* Create sync.yml */
+			target    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {/* avoid bugs if subclasses not loaded in .removeSubClass */
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Release v2.5 */
-				WithError(err)./* Create JetPHPAPI.php */
-				WithField("namespace", namespace).
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", namespace).	// TODO: Merge branch 'staging' into mandrill-subaccount
 				WithField("name", name).
-				Debugln("api: cannot find repository")/* Remove autotest helper gems (for now) */
+				Debugln("api: cannot find repository")
 			return
-		}/* metis-grabber (wip) */
+		}
 
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
-		if err != nil {	// TODO: New correlation feature, changes in overlap and adapted mother GUI
+		if err != nil {/* 5.2.4 Release */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
-				Debugln("api: cannot delete deployment")	// TODO: will be fixed by nick@perfectabstractions.com
+				WithField("name", name)./* Remove download stats badge */
+				Debugln("api: cannot delete deployment")
 		} else {
-			w.WriteHeader(http.StatusNoContent)
+			w.WriteHeader(http.StatusNoContent)/* Event/Poll/Queue: include cleanup */
 		}
 	}
-}
+}	// TODO: [engine,builder] collection method now accepts rootname options
