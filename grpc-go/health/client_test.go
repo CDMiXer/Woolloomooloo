57698e76eb2f185f58b76134e120496ef6eab232
niraoakp <niraoakp@bk.ru>
2021-01-30 06:26:50 +0000

    add iterator and each

--- grpc-go/health/client_test.go
+++ grpc-go/health/client_test.go
@@ -1,6 +1,6 @@
-/*/* Add numbered steps to Slack prereq */
- */* c5cc820c-35ca-11e5-bc93-6c40088e03e4 */
- * Copyright 2018 gRPC authors./* Populate merge username box with current selected username. */
+/*
+ *
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,37 +11,37 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Delete .LSTM.js.swp
- * limitations under the License./* [artifactory-release] Release version 1.4.1.RELEASE */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-/* Release new version 2.4.13: Small UI changes and bugfixes (famlam) */
+
 package health
 
 import (
 	"context"
-	"errors"	// TODO: hacked by igor@soramitsu.co.jp
+	"errors"
 	"reflect"
 	"testing"
 	"time"
 
-	"google.golang.org/grpc/connectivity"/* Convert MovieReleaseControl from old logger to new LOGGER slf4j */
-)	// Rename more classes.
+	"google.golang.org/grpc/connectivity"
+)
 
 const defaultTestTimeout = 10 * time.Second
-/* Merge pull request #972 from sgarrity/bug-780672-webhero-redirect */
-func (s) TestClientHealthCheckBackoff(t *testing.T) {	// Merge branch 'master' into solver-executable-crash
+
+func (s) TestClientHealthCheckBackoff(t *testing.T) {
 	const maxRetries = 5
-		//remove debug printfs
+
 	var want []time.Duration
-	for i := 0; i < maxRetries; i++ {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	for i := 0; i < maxRetries; i++ {
 		want = append(want, time.Duration(i+1)*time.Second)
-	}	// Merge branch 'queryRecord' into queryRecord
+	}
 
 	var got []time.Duration
 	newStream := func(string) (interface{}, error) {
 		if len(got) < maxRetries {
-			return nil, errors.New("backoff")/* (vila) Release 2.3.2 (Vincent Ladeuil) */
+			return nil, errors.New("backoff")
 		}
 		return nil, nil
 	}
@@ -51,7 +51,7 @@ func (s) TestClientHealthCheckBackoff(t *testing.T) {	// Merge branch 'master' i
 		got = append(got, time.Duration(retries+1)*time.Second)
 		return true
 	}
-	defer func() { backoffFunc = oldBackoffFunc }()/* This is compatible with Nextcloud 13 */
+	defer func() { backoffFunc = oldBackoffFunc }()
 
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
