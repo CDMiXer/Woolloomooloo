ba688cd057356d80bf1a7cce7b36f3320c79b3d4
niraoakp <niraoakp@bk.ru>
2021-01-30 18:16:24 +0000

    Add nod_win1.aud and nod_map1.aud to mix database.

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -1,20 +1,20 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Fix erreur en production */
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//added dataset extractor
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+		//Spark java v2
 # Scenario #3 - rename a component (and all it's children)
 # No change to the component...
 class ComponentThree(ComponentResource):
-    def __init__(self, name, opts=None):	// Rights controller to angular
+    def __init__(self, name, opts=None):	// Added download link for visual studio
         super().__init__("my:module:ComponentThree", name, None, opts)
         # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
-        # later, the implicit alias inherited from the parent alias will include replacing the name/* short description in README */
+        # later, the implicit alias inherited from the parent alias will include replacing the name
         # prefix to match the parent alias name.
-        resource1 = Resource1(name + "-child", ResourceOptions(parent=self))	// Adapted code to new page design
+        resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
 
 # ...but applying an alias to the instance successfully renames both the component and the children.
