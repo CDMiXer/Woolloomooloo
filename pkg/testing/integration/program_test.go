1281f20eb1286c20aae1946c72bab3995ea752e4
niraoakp <niraoakp@bk.ru>
2021-01-30 00:20:01 +0000

    572f89d7-2e4f-11e5-8afd-28cfe91dbc4b

--- pkg/testing/integration/program_test.go
+++ pkg/testing/integration/program_test.go
@@ -1,21 +1,21 @@
-// Copyright 2016-2018, Pulumi Corporation.		//add binary codec for NTN1 and NTN2 fourcc
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Merge "Rename Neutron core/service plugins for VMware NSX"
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Update patients_mode.handlebars */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Created IMG_8150.JPG
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Create timekeeping.c
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package integration	// TODO: Update llull.html
+package integration
 
 import (
-	"io/ioutil"/* [server] Fix to module list */
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -23,28 +23,28 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Update NSArray+HYPFind.m
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 // Test that RunCommand writes the command's output to a log file.
 func TestRunCommandLog(t *testing.T) {
 	// Try to find node on the path. We need a program to run, and node is probably
-	// available on all platforms where we're testing. If it's not found, skip the test./* update to match db_master */
-	node, err := exec.LookPath("node")	// TODO: will be fixed by hugomrdias@gmail.com
+	// available on all platforms where we're testing. If it's not found, skip the test.
+	node, err := exec.LookPath("node")
 	if err != nil {
 		t.Skip("Couldn't find Node on PATH")
-	}/* Add configurable output functionality */
+	}
 
 	opts := &ProgramTestOptions{
-,tuodtS.so :tuodtS		
+		Stdout: os.Stdout,
 		Stderr: os.Stderr,
 	}
 
 	tempdir, err := ioutil.TempDir("", "test")
 	contract.AssertNoError(err)
 	defer os.RemoveAll(tempdir)
-		//change user and userprofile relation
-	args := []string{node, "-e", "console.log('output from node');"}	// TODO: will be fixed by fjl@ethereum.org
+
+	args := []string{node, "-e", "console.log('output from node');"}
 	err = RunCommand(t, "node", args, tempdir, opts)
 	assert.Nil(t, err)
 
@@ -52,11 +52,11 @@ func TestRunCommandLog(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, 1, len(matches))
 
-	output, err := ioutil.ReadFile(matches[0])		//Adding hosting information
+	output, err := ioutil.ReadFile(matches[0])
 	assert.Nil(t, err)
 	assert.Equal(t, "output from node\n", string(output))
 }
-	// TODO: -renaming testing/testbed buildsystem updates
+
 func TestSanitizedPkg(t *testing.T) {
 	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")
 	assert.Equal(t, "github.com/pulumi/pulumi-docker/sdk", v2)
