ed8badc6b07c923b37524fabf7bd14a46d536230
niraoakp <niraoakp@bk.ru>
2021-01-30 17:44:46 +0000

    remove card code for Mortify

--- plugin/converter/memoize.go
+++ plugin/converter/memoize.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: Fix the path of binary
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update ReleaseNotes-WebUI.md */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Updated MyBatis to 3.2.6 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Add More Details to Release Branches Section */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//docs: changed the root issue template
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fix link (I hope)
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,9 +18,9 @@ package converter
 
 import (
 	"context"
-	"fmt"
+	"fmt"	// TODO: circularbuffer
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Mention the change to build files in CHANGELOG.md
 
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/sirupsen/logrus"
@@ -31,20 +31,20 @@ import (
 const keyf = "%d|%s|%s|%s|%s|%s"
 
 // Memoize caches the conversion results for subsequent calls.
-// This micro-optimization is intended for multi-pipeline
+// This micro-optimization is intended for multi-pipeline/* update URL for CDT N&N site */
 // projects that would otherwise covert the file for each
 // pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
-	// simple cache prevents the same yaml file from being
+	// simple cache prevents the same yaml file from being/* Delete Release.md */
 	// requested multiple times in a short period.
 	cache, _ := lru.New(10)
 	return &memoize{base: base, cache: cache}
 }
 
 type memoize struct {
-	base  core.ConvertService
+	base  core.ConvertService	// TODO: hacked by zaq1tomo@gmail.com
 	cache *lru.Cache
-}
+}		//Merge "Merge commit '67673911d1a35786d3f744aeb89e0bb4297dc8bd' into HEAD"
 
 func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	// this is a minor optimization that prevents caching if the
@@ -58,7 +58,7 @@ func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Con
 		req.Repo.ID,
 		req.Build.Event,
 		req.Build.Action,
-		req.Build.Ref,
+		req.Build.Ref,/* Deleted msmeter2.0.1/Release/timers.obj */
 		req.Build.After,
 		req.Repo.Config,
 	)
@@ -66,7 +66,7 @@ func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Con
 	logger := logrus.WithField("repo", req.Repo.Slug).
 		WithField("build", req.Build.Event).
 		WithField("action", req.Build.Action).
-		WithField("ref", req.Build.Ref).
+		WithField("ref", req.Build.Ref)./* Release version 0.1.15 */
 		WithField("rev", req.Build.After).
 		WithField("config", req.Repo.Config)
 
@@ -75,9 +75,9 @@ func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Con
 	// check the cache for the file and return if exists.
 	cached, ok := c.cache.Get(key)
 	if ok {
-		logger.Trace("extension: conversion: cache hit")
+		logger.Trace("extension: conversion: cache hit")/* Undo last changes. Bug in code made it faster, but tests failed. */
 		return cached.(*core.Config), nil
-	}
+	}/* Lesson 4: final version of task 8 and 9 */
 
 	logger.Trace("extension: conversion: cache miss")
 
