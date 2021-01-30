cd0cd626f29986e2300264b2a72298c6cdc60f07
niraoakp <niraoakp@bk.ru>
2021-01-30 04:04:15 +0000

    Create stacked.scss

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -2,7 +2,7 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-
+	// TODO: will be fixed by greg@colvin.org
 const sleep = require("sleep-promise");
 const assert = require("assert");
 
@@ -11,15 +11,15 @@ class NullProvider implements dynamic.ResourceProvider {
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();/* proposeKeywords method added */
 }
 
-class NullResource extends dynamic.Resource {
+class NullResource extends dynamic.Resource {	// TODO: will be fixed by davidad@alum.mit.edu
     constructor(name: string) {
         super(new NullProvider(), name, {}, undefined);
     }
 }
-
+	// TODO: Update CANBusBBB.md
 (async () => {
     try {
         const a = new NullResource("a");
@@ -29,7 +29,7 @@ class NullResource extends dynamic.Resource {
         const c = new NullResource("c");
         const urn = await b.urn;
         assert.notStrictEqual(urn, undefined, "expected a defined urn");
-        assert.notStrictEqual(urn, "", "expected a valid urn");
+        assert.notStrictEqual(urn, "", "expected a valid urn");	// TODO: will be fixed by onhardev@bk.ru
     } catch (err) {
         console.error(err);
         process.exit(-1);
