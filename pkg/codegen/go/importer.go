7e225d14437a8eee864632ca6591c3abf56b9baa
niraoakp <niraoakp@bk.ru>
2021-01-30 11:25:04 +0000

    update Brazillian translation (Francisco Fuchs)

--- pkg/codegen/go/importer.go
+++ pkg/codegen/go/importer.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* october 22  */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by magik6k@gmail.com
-///* log pull request data to tests */
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// fixed positions for plain wires
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* - Ported some ILOps */
-// limitations under the License.	// fix forum answer and remove admin
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package gen
-/* ThumbnailWriter are instantiated by reflection and specified in web.xml */
+
 import (
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
-// GoPackageInfo holds information required to generate the Go SDK from a schema.		//Update GetUninstallString.ps1
+// GoPackageInfo holds information required to generate the Go SDK from a schema.
 type GoPackageInfo struct {
 	// Base path for package imports
 	//
@@ -32,26 +32,26 @@ type GoPackageInfo struct {
 	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
 	//
 	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`
-		//Create a content categories enum
-	// Map from package name -> package alias/* automated commit from rosetta for sim/lib shred, locale eu */
+
+	// Map from package name -> package alias
 	//
 	//    { "github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes/flowcontrol/v1alpha1": "flowcontrolv1alpha1" }
-	//	// TODO: hacked by sjors@sprovoost.nl
+	//
 	PackageImportAliases map[string]string `json:"packageImportAliases,omitempty"`
 }
-	// TODO: Update JavaScriptLab.js
+
 // Importer implements schema.Language for Go.
 var Importer schema.Language = importer(0)
 
-type importer int	// TODO: will be fixed by hello@brooklynzelenka.com
+type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
-	return raw, nil/* non-US multi-sig in Release.gpg and 2.2r5 */
-}	// TODO: Create Kitchen RGB device type
+	return raw, nil
+}
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
-func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {		//Implementação de Delete em Consulta_has_ProcedimentoDAO
+func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
