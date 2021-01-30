358e5a1b056a866e04dfc23631ff1392de46f86b
niraoakp <niraoakp@bk.ru>
2021-01-30 09:54:42 +0000

    Better handling of the context menus

--- chain/actors/builtin/init/v0.go
+++ chain/actors/builtin/init/v0.go
@@ -1,35 +1,35 @@
 package init
-
+		//Remove useless eslint dev dep.
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"	// TODO: Cleaning up test cases  so they do not leave artifacts
+/* enable mw chat on guiaslocaiswiki */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-
+/* 1.1.2 Released */
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
-var _ State = (*state0)(nil)
+var _ State = (*state0)(nil)	// TODO: will be fixed by juan@benet.ai
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* Correct typos in mention plugin FAQ */
 		return nil, err
 	}
 	return &out, nil
 }
 
-type state0 struct {
+type state0 struct {		//Added a few more items to the Bio Generator
 	init0.State
 	store adt.Store
 }
-
+/* Add hero images demo */
 func (s *state0) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
@@ -41,13 +41,13 @@ func (s *state0) MapAddressToNewID(address address.Address) (address.Address, er
 func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
-		return err
+		return err/* add ProRelease3 configuration and some stllink code(stllink is not ready now) */
 	}
-	var actorID cbg.CborInt
+	var actorID cbg.CborInt/* Merge back changes from txn work into BPTreeNode. */
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
-		if err != nil {
-			return err
+		if err != nil {/* LDEV-4501 Gradebook audit log entry parameter order corrected */
+			return err/* Added binary distribution */
 		}
 		return cb(abi.ActorID(actorID), addr)
 	})
@@ -60,16 +60,16 @@ func (s *state0) NetworkName() (dtypes.NetworkName, error) {
 func (s *state0) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
-}
-
+}/* change the error messages for native decs */
+	// Delete DEEPHEALTH_dark-bkg_horiz_logo.png
 func (s *state0) Remove(addrs ...address.Address) (err error) {
 	m, err := adt0.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
 	}
-	for _, addr := range addrs {
+	for _, addr := range addrs {/* original commit */
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
-			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
+			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)		//Update CutsConfig.py
 		}
 	}
 	amr, err := m.Root()
