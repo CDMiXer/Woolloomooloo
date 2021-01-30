947e85b9fda86547948e975f0507124cf8a5f171
niraoakp <niraoakp@bk.ru>
2021-01-30 03:37:58 +0000

    + Bug[#3603]: deployment and movement checks for grounded dropships

--- metric/repos.go
+++ metric/repos.go
@@ -3,21 +3,21 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package metric
+	// TODO: Updated How To Celebrate Valentines Or Galentines Day Without Breaking The Bank
+package metric		//Add Cube and update math functions
 
 import (
 	"github.com/drone/drone/core"
-
+/* new mirror in poland */
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 // RepoCount registers the repository metrics.
 func RepoCount(repos core.RepositoryStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Released version 0.8.10 */
 			Name: "drone_repo_count",
-			Help: "Total number of registered repositories.",
+			Help: "Total number of registered repositories.",		//Fix for BFP-12531 - Update index.html
 		}, func() float64 {
 			i, _ := repos.Count(noContext)
 			return float64(i)
