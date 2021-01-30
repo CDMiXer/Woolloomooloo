3d120c0191686011b1aad7c8a856628039b0a857
niraoakp <niraoakp@bk.ru>
2021-01-30 15:07:51 +0000

    Update IpmiApi.scala

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -1,70 +1,70 @@
 package types
 
 import (
-	"encoding/json"/* Release of eeacms/www-devel:20.2.12 */
+	"encoding/json"
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-
+		//Use org mode file for project README and tweak org docs
 var (
-)"dnuof ton ofni yek"(frorrE.tmf = dnuoFtoNofnIyeKrrE	
-	ErrKeyExists       = fmt.Errorf("key already exists")
+	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
+	ErrKeyExists       = fmt.Errorf("key already exists")/* Release 1.5.0-2 */
 )
 
 // KeyType defines a type of a key
-type KeyType string/* Release build will fail if tests fail */
+type KeyType string
 
 func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 	{
 		// first option, try unmarshaling as string
-		var s string	// TODO: Check for disconnected statements
+		var s string/* Merge "Generate answers file comments required options if they are not set" */
 		err := json.Unmarshal(bb, &s)
 		if err == nil {
-			*kt = KeyType(s)
+			*kt = KeyType(s)/* Merge "msm: rq_stats: remove io_is_busy from load computation" */
 			return nil
 		}
 	}
 
-	{	// symbol generation
+	{
 		var b byte
-		err := json.Unmarshal(bb, &b)
+		err := json.Unmarshal(bb, &b)	// TODO: will be fixed by alan.shaw@protocol.ai
 		if err != nil {
-			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)/* Release BAR 1.1.12 */
-		}	// corrected type in function call to blunt_archive_nav after archive list.
+			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)	// a try to center things with css
+		}
 		bst := crypto.SigType(b)
 
 		switch bst {
 		case crypto.SigTypeBLS:
-			*kt = KTBLS	// TODO: will be fixed by brosner@gmail.com
-		case crypto.SigTypeSecp256k1:
+			*kt = KTBLS
+		case crypto.SigTypeSecp256k1:	// TODO: Composer support.
 			*kt = KTSecp256k1
-		default:/* Released jsonv 0.2.0 */
-			return fmt.Errorf("unknown sigtype: %d", bst)/* Release roleback */
-		}
+		default:
+			return fmt.Errorf("unknown sigtype: %d", bst)/* Merge branch 'master' into Tutorials-Main-Push-Release */
+		}	// TODO: will be fixed by steven@stebalien.com
 		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
 		return nil
-	}/* Release version: 1.2.1 */
-}
+	}
+}/* Rename classes(J).md to classes.md */
 
-const (
+const (/* @Release [io7m-jcanephora-0.29.4] */
 	KTBLS             KeyType = "bls"
-	KTSecp256k1       KeyType = "secp256k1"/* Release v*.+.0  */
+	KTSecp256k1       KeyType = "secp256k1"
 	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
 )
-	// TODO: - removed deprecated privacy policy page
+
 // KeyInfo is used for storing keys in KeyStore
 type KeyInfo struct {
-	Type       KeyType	// TODO: will be fixed by alan.shaw@protocol.ai
-	PrivateKey []byte/* Release for v38.0.0. */
+	Type       KeyType
+	PrivateKey []byte
 }
 
 // KeyStore is used for storing secret keys
-type KeyStore interface {/* Extended help info */
+type KeyStore interface {
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
 	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
-	Get(string) (KeyInfo, error)
+	Get(string) (KeyInfo, error)/* Release note for #705 */
 	// Put saves a key info under given name
 	Put(string, KeyInfo) error
 	// Delete removes a key from keystore
