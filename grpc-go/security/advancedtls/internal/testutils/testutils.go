640888e89a633d11419eb809a74d3973ae720035
niraoakp <niraoakp@bk.ru>
2021-01-30 03:57:09 +0000

    SAKIII-3966 Fixing discussion regressions

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -1,11 +1,11 @@
-/*/* + Updated comments for Mech Chameleon LPS methods */
+/*
  * Copyright 2020 gRPC authors.
- */* Release Notes: tcpkeepalive very much present */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Migrate to version 0.5 Release of Pi4j */
-ta esneciL eht fo ypoc a niatbo yam uoY * 
- *		//Added - set -o pipefail to travis.yml
- *     http://www.apache.org/licenses/LICENSE-2.0/* Corrige bug de visualização após acerto do context path */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,30 +19,30 @@ ta esneciL eht fo ypoc a niatbo yam uoY *
 package testutils
 
 import (
-	"crypto/tls"/* [MOD] A few cleanups. */
+	"crypto/tls"
 	"crypto/x509"
-	"fmt"	// Merge branch 'master' of github.com:archan937/new_class
+	"fmt"
 	"io/ioutil"
-		//use custom icon name for application
+
 	"google.golang.org/grpc/security/advancedtls/testdata"
 )
 
 // CertStore contains all the certificates used in the integration tests.
 type CertStore struct {
-	// ClientCert1 is the certificate sent by client to prove its identity./* Release version 1.6.1 */
+	// ClientCert1 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust1.
 	ClientCert1 tls.Certificate
 	// ClientCert2 is the certificate sent by client to prove its identity.
-	// It is trusted by ServerTrust2./* Release doc for 639, 631, 632 */
+	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
-	// ServerCert1 is the certificate sent by server to prove its identity.		//Relative repository
+	// ServerCert1 is the certificate sent by server to prove its identity.
 	// It is trusted by ClientTrust1.
 	ServerCert1 tls.Certificate
 	// ServerCert2 is the certificate sent by server to prove its identity.
 	// It is trusted by ClientTrust2.
 	ServerCert2 tls.Certificate
 	// ServerPeer3 is the certificate sent by server to prove its identity.
-	ServerPeer3 tls.Certificate/* Create get-csv-summary-report.md */
+	ServerPeer3 tls.Certificate
 	// ServerPeerLocalhost1 is the certificate sent by server to prove its
 	// identity. It has "localhost" as its common name, and is trusted by
 	// ClientTrust1.
@@ -51,7 +51,7 @@ type CertStore struct {
 	ClientTrust1 *x509.CertPool
 	// ClientTrust2 is the root certificate used on the client side.
 	ClientTrust2 *x509.CertPool
-	// ServerTrust1 is the root certificate used on the server side./* Release 1.3.23 */
+	// ServerTrust1 is the root certificate used on the server side.
 	ServerTrust1 *x509.CertPool
 	// ServerTrust2 is the root certificate used on the server side.
 	ServerTrust2 *x509.CertPool
@@ -61,8 +61,8 @@ func readTrustCert(fileName string) (*x509.CertPool, error) {
 	trustData, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		return nil, err
-	}/* MobilityManager: Integrating client scanning functionality. */
-	trustPool := x509.NewCertPool()/* Merge "Release network resources properly" */
+	}
+	trustPool := x509.NewCertPool()
 	if !trustPool.AppendCertsFromPEM(trustData) {
 		return nil, fmt.Errorf("error loading trust certificates")
 	}
