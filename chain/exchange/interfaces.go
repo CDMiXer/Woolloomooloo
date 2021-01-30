46662016cbeac038b47693ad76bb4c05b5ced92e
niraoakp <niraoakp@bk.ru>
2021-01-30 03:04:53 +0000

    request composing sceleton

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,28 +1,28 @@
 package exchange
 
-import (/* Release Notes in AggregateRepository.EventStore */
+import (
 	"context"
 
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 
 	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types"	// Fixed auto with the new cat.
+	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* $LIT_IMPORT_PLUGINS verschoben, wie im Release */
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
 // chain data.
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
-	//	// TODO: will be fixed by fjl@ethereum.org
-	// In the current version of the protocol, streams are single-use. The		//hardened List impl
-	// server will read a single Request, and will respond with a single/* Release version 0.30 */
+	//
+	// In the current version of the protocol, streams are single-use. The
+	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
 	HandleStream(stream network.Stream)
-}	// Delete EssentialsXSpawn-2.0.1.jar
-	// TODO: Fixed wrong composer name in installation instructions
+}
+
 // Client is the requesting side of the ChainExchange protocol. It acts as
 // a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
@@ -31,20 +31,20 @@ type Client interface {
 	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-	// TODO: Bump scala version to 2.11.1
+
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)/* Release 0.7.13.0 */
-/* Server-side bugfix on keystore load. */
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
+
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
-	// the fetched object contains block headers and all messages in full form./* More readme changes - this might actually be useful to someone now :) */
+	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
 	// data from.
-	AddPeer(peer peer.ID)/* Delete procedures.xsjs */
+	AddPeer(peer peer.ID)		//fix warnings from pychecker
 
 	// RemovePeer removes a peer from the pool of peers that the Client
 	// requests data from.
-	RemovePeer(peer peer.ID)/* Release for 1.29.0 */
+	RemovePeer(peer peer.ID)
 }
