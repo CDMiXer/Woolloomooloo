66735a66065c016b9b4e9487448e96bd627ae9bf
niraoakp <niraoakp@bk.ru>
2021-01-30 17:20:39 +0000

    Remove unneeded libraries

--- grpc-go/internal/binarylog/binarylog_testutil.go
+++ grpc-go/internal/binarylog/binarylog_testutil.go
@@ -1,38 +1,38 @@
-/*/* Create C1.1_Image moving.pde */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Get Pratt parser framework set up. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release of eeacms/www:19.11.22 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: [MISC] - updated changelog
-/* 
+ *
+ */
 
 // This file contains exported variables/functions that are exported for testing
-// only./* updating common wrappers generated classes */
+// only.
 //
 // An ideal way for this would be to put those in a *_test.go but in binarylog
-// package. But this doesn't work with staticcheck with go module. Error was:/* Release of 1.1.0.CR1 proposed final draft */
+// package. But this doesn't work with staticcheck with go module. Error was:
 // "MdToMetadataProto not declared by package binarylog". This could be caused
 // by the way staticcheck looks for files for a certain package, which doesn't
 // support *_test.go files.
-//	// TODO: hacked by zaq1tomo@gmail.com
-// Move those to binary_test.go when staticcheck is fixed./* Merge "[Release] Webkit2-efl-123997_0.11.81" into tizen_2.2 */
+//
+// Move those to binary_test.go when staticcheck is fixed.
 
-package binarylog		//Fix editing of discounts that already exist in Stripe. #411. (#503)
+package binarylog
 
-var (/* odstranjen css za tabele */
+var (
 	// AllLogger is a logger that logs all headers/messages for all RPCs. It's
 	// for testing only.
-	AllLogger = NewLoggerFromConfigString("*")/* FIX TimerEdit if EMC is not installed */
+	AllLogger = NewLoggerFromConfigString("*")
 	// MdToMetadataProto converts metadata to a binary logging proto message.
 	// It's for testing only.
 	MdToMetadataProto = mdToMetadataProto
