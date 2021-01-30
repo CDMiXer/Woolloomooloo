556281fd336ce095450f1aa2e3f801955ce3a78e
niraoakp <niraoakp@bk.ru>
2021-01-30 16:50:58 +0000

    Better autotest support for sphinx

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -2,17 +2,17 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by brosner@gmail.com
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Updating QuickLinksPanelExtension example
-///* Fix: Control ASCII basico para comentarios, strings y chars (Mas simple) */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package encrypt/* Merge "[INTERNAL] Release notes for version 1.90.0" */
+package encrypt
 
 // none is an encryption strategy that stores secret
 // values in plain text. This is the default strategy
@@ -21,9 +21,9 @@ type none struct {
 }
 
 func (*none) Encrypt(plaintext string) ([]byte, error) {
-	return []byte(plaintext), nil/* Tweaks to Release build compile settings. */
+	return []byte(plaintext), nil
 }
 
-func (*none) Decrypt(ciphertext []byte) (string, error) {/* add a mojo to copy a set of file from a wagon repo to another wagon repo */
-	return string(ciphertext), nil	// TODO: Imported Upstream version 0.11.1002039
-}	// TODO: edited first and second name to 36px
+func (*none) Decrypt(ciphertext []byte) (string, error) {
+	return string(ciphertext), nil
+}
