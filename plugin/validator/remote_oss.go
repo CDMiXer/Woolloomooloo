e89d024f18ea88977fc68be91619d4dc5da4a162
niraoakp <niraoakp@bk.ru>
2021-01-30 06:36:42 +0000

    Update about migration to Framagit

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Adding python support and preliminary Doxygen scripts
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Delete daihoXX.png */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by ng8eke@163.com
 // +build oss
 
 package validator
@@ -20,10 +20,10 @@ import (
 	"time"
 
 	"github.com/drone/drone/core"
-)
+)		//testing the continuation cache
 
-// Remote returns a conversion service that converts the
+// Remote returns a conversion service that converts the	// TODO: receiveBitcoin dataflow improved
 // configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
 	return new(noop)
-}
+}	// TODO: Create registration_completed.twig
