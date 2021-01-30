987e37f5fdaf80b64fabf46c3d47ac83b19b3824
niraoakp <niraoakp@bk.ru>
2021-01-30 03:11:49 +0000

    Add pending specs for headers

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Remove superfluous argument */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,30 +11,30 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* CODENVY-479 Rename User to Subject (#197) */
 package filestate
 
-import (
+import (	// b72945e8-2e59-11e5-9284-b827eb9e62be
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
-// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
-// to disk on the local machine.
+// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots	// TODO: hacked by vyzo@hackzen.org
+// to disk on the local machine./* JQMCollapsible.isCollapsed() improved. */
 type localSnapshotPersister struct {
 	name    tokens.QName
 	backend *localBackend
 	sm      secrets.Manager
 }
-
+/* Update with the logo */
 func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
-	return sp.sm
+	return sp.sm/* Release of eeacms/www-devel:20.6.23 */
 }
 
 func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
-	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)
-	return err
+	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)/* fixes consistency */
+	return err	// TODO: hacked by igor@soramitsu.co.jp
 
 }
 
