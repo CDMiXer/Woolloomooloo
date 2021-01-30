9244eaf926e0e21fe9fadb84f31ccedae6cca37e
niraoakp <niraoakp@bk.ru>
2021-01-30 08:54:02 +0000

    Merge "Split metadata copying from mw.UploadWizardDetails"

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Create flu_style.css
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//added {{ site.baseurl }} to permalink
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: BUG: string prefix for raw binary is br, not rb
-	// TODO: will be fixed by ligi@ligi.de
-package queue/* Updating build-info/dotnet/corefx/master for preview6.19257.5 */
+// +build !oss
 
-import (		//comment sensmail for missingpages, fix names too
+package queue
+
+import (
 	"net/http"
-		//No fullscreen (automatic) and move leds-on-screen to the general options
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: Fix deprecated error
-	"github.com/drone/drone/logger"		//Warcs ready to go to production
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/logger"
 )
 
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()	// Plugin Update to 1.0.2 - Encoding fixes
+	return func(w http.ResponseWriter, r *http.Request) {		//added logging around sharding counter memcache hit/miss
+		ctx := r.Context()
 		err := scheduler.Resume(ctx)
 		if err != nil {
-			render.InternalError(w, err)	// TODO: hacked by qugou1350636@126.com
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: cannot resume scheduler")		//Updated committees.
+				Errorln("api: cannot resume scheduler")/* Create fullAutoRelease.sh */
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
