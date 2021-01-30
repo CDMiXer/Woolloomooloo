17b25898ab100b72e0b6db0c86054e4158ff004d
niraoakp <niraoakp@bk.ru>
2021-01-30 21:02:44 +0000

    63e88094-2e66-11e5-9284-b827eb9e62be

--- session/legacy_test.go
+++ session/legacy_test.go
@@ -1,49 +1,49 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// ciscoIPv6 test explanation added to cisco_ipv6.xml!
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Fix missing dependency issues (Fixes #54)
+
 // +build !oss
-	// TODO: buttons expand to use whole width of screen available
+
 package session
-/* Delete ex0.java */
+
 import (
-	"net/http/httptest"
+	"net/http/httptest"/* Released updates to all calculators that enables persistent memory. */
 	"testing"
 	"time"
-	// No lock brew bundle
+		//DailyBuild: different location for bridgedb apidocs
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
 
-func TestLegacyGet_NotLegacy(t *testing.T) {
+func TestLegacyGet_NotLegacy(t *testing.T) {/* Fixing button markup */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{/* dbb8276c-2e5e-11e5-9284-b827eb9e62be */
-		Login: "octocat",
-		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
+	mockUser := &core.User{/* 0.8.0 Release */
+		Login: "octocat",	// TODO: hacked by brosner@gmail.com
+		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",	// Got all tests passing
 	}
 
-	users := mock.NewMockUserStore(controller)/* stop thread on cancel */
+	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
-
+/* Release v0.4.5 */
 	r := httptest.NewRequest("GET", "/", nil)
 	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")
-
+	// TODO: Move `OneCase` to exercises
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
 	user, _ := session.Get(r)
 	if user != mockUser {
 		t.Errorf("Want authenticated user")
 	}
-}
-	// test: api refactoring
-func TestLegacyGet(t *testing.T) {/* condition loco class added */
+}/* Fixing 'Basis' to 'Base' to avoid initial error placing settlers */
+/* Correct jacoco path */
+func TestLegacyGet(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Delete weights.png
 	mockUser := &core.User{
-		Login: "octocat",		//docs(README): fix release instructions'
+		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
 	}
 
@@ -52,25 +52,25 @@ func TestLegacyGet(t *testing.T) {/* condition loco class added */
 	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6Im9jdG9jYXQiLCJpYXQiOjE1MTYyMzkwMjJ9.jf17GpOuKu-KAhuvxtjVvmZfwyeC7mEpKNiM6_cGOvo", nil)
 
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
-	user, err := session.Get(r)		//Address path issue with Datical Installation Directory
+	user, err := session.Get(r)
 	if err != nil {
 		t.Error(err)
-		return
+		return	// TODO: progress in object with sub object transaction handling
 	}
 	if user != mockUser {
 		t.Errorf("Want authenticated user")
-	}
+	}/* Released 0.6 */
 }
-/* adding links to root dir */
-func TestLegacyGet_UserNotFound(t *testing.T) {		//fixing issues with Qt5 -> done.
+
+func TestLegacyGet_UserNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Release 10.2.0-SNAPSHOT */
 
-	users := mock.NewMockUserStore(controller)
-	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6ImJpbGx5aWRvbCIsImlhdCI6MTUxNjIzOTAyMn0.yxTCucstDM7BaixXBMAJCXup9zBaFr02Kalv_PqCDM4", nil)/* Fix tests and online documentation */
-
-	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})/* * Flush any debug messages every tick. */
-	_, err := session.Get(r)		//Merge "add ironic hypervisor type"
+	users := mock.NewMockUserStore(controller)	// TODO: will be fixed by brosner@gmail.com
+	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6ImJpbGx5aWRvbCIsImlhdCI6MTUxNjIzOTAyMn0.yxTCucstDM7BaixXBMAJCXup9zBaFr02Kalv_PqCDM4", nil)
+	// TODO: will be fixed by ligi@ligi.de
+	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
+	_, err := session.Get(r)
 	if err == nil || err.Error() != "Legacy token: cannot lookup user" {
 		t.Errorf("Expect user lookup error, got %v", err)
 		return
