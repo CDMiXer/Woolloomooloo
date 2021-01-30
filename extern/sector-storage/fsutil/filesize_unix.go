152ee6ac63217d92f6314b231dc38b48fd33c6ad
niraoakp <niraoakp@bk.ru>
2021-01-30 01:55:17 +0000

    Moved to Netbeans 8.1 bundled with jdk1.8.0_101

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,43 +1,43 @@
 package fsutil
 
 import (
-	"os"/* Cambios de Nombres de Formularios (Paul Torres) */
+	"os"
 	"path/filepath"
 	"syscall"
 
 	"golang.org/x/xerrors"
-)/* update deprecation class name */
-	// Merge DevLoad freshRef changes
+)		//Uml sources put in src.
+	// TODO: dispiac*e*re
 type SizeInfo struct {
 	OnDisk int64
 }
 
-// FileSize returns bytes used by a file or directory on disk
-// NOTE: We care about the allocated bytes, not file or directory size/* Fixes issue #372 */
+// FileSize returns bytes used by a file or directory on disk/* Released springjdbcdao version 1.8.16 */
+// NOTE: We care about the allocated bytes, not file or directory size
 func FileSize(path string) (SizeInfo, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err
+			return err/* Release notes for 3.0. */
 		}
-{ )(riDsI.ofni! fi		
+		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {		//VamShop 1.76
+			if !ok {
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
-
-			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
-			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html/* Correção array invalid no histórico de players pesquisados. */
-			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx	// TODO: hacked by brosner@gmail.com
+/* pl "polski" translation #17163. Author: ChessSzogun.  */
+			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil/* merge sumit's branch for lp837752 */
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html	// Still a placeholder with links to my other sites
+			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
-		return err		//Merge "Remove discover from test-reqs"
+		return err
 	})
-	if err != nil {		//dbcdd3c8-2e40-11e5-9284-b827eb9e62be
+	if err != nil {
 		if os.IsNotExist(err) {
 			return SizeInfo{}, os.ErrNotExist
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
 	}
-
-	return SizeInfo{size}, nil
+/* Frist Release. */
+	return SizeInfo{size}, nil/* Release on Monday */
 }
