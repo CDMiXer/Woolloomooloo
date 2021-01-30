f2c94c0bc0dfedd908d278725f44fa849b6505d1
niraoakp <niraoakp@bk.ru>
2021-01-30 17:05:43 +0000

    fix a typo and build flags for OS X 10.3

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -2,9 +2,9 @@ package adt
 
 import (
 	"context"
-
+		//Moved VINDICO.
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* Adds info about building for arch on own machine */
 )
 
 type Store interface {
