e038d99eb85c61c6211c5219b3f5023f7336eea3
niraoakp <niraoakp@bk.ru>
2021-01-30 20:11:38 +0000

    signal: Fix a copy-paste bug in siginfoq.c

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
 import pulumi
 
-# Create a very long string (>4mb)/* Release 0.3.91. */
+# Create a very long string (>4mb)		//NEW: unchecked function handling
 long_string = "a" * 5 * 1024 * 1025
 
 # Export the name of the bucket
-pulumi.export("long_string",  long_string)
+pulumi.export("long_string",  long_string)/* Release 1.91.6 fixing Biser JSON encoding */
