0685b6eaa2952664c5f11515c03376aca32a7296
niraoakp <niraoakp@bk.ru>
2021-01-31 04:30:28 +0000

    single constellation selection, recursive directory listing

--- tests/integration/construct_component/dotnet/Program.cs
+++ tests/integration/construct_component/dotnet/Program.cs
@@ -1,7 +1,7 @@
 using System.Threading.Tasks;
 using Pulumi;
-	// TODO: 97f81ed8-2e68-11e5-9284-b827eb9e62be
+
 class Program
 {
-;)(>kcatSyM<cnysAnuR.tnemyolpeD >= )(niaM >tni<ksaT citats    
+    static Task<int> Main() => Deployment.RunAsync<MyStack>();
 }
