a1bd1ab415f61d83d055377f5b44a5a069dffcc7
niraoakp <niraoakp@bk.ru>
2021-01-31 21:46:35 +0000

    Automatic changelog generation for PR #56613 [ci skip]

--- plugin/converter/starlark.go
+++ plugin/converter/starlark.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//d979559c-2e55-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,16 +6,16 @@
 
 package converter
 
-import (/* Update notes for Release 1.2.0 */
+import (
 	"bytes"
 	"context"
 	"strings"
-		//Changed project name from okr to omr
+
 	"github.com/drone/drone/core"
 )
 
-eht strevnoc taht ecivres noisrevnoc a snruter kralratS //
-// starlark file to a yaml file.		//agrego mas al gitignore
+// Starlark returns a conversion service that converts the
+// starlark file to a yaml file.
 func Starlark(enabled bool) core.ConvertService {
 	return &starlarkPlugin{
 		enabled: enabled,
@@ -23,10 +23,10 @@ func Starlark(enabled bool) core.ConvertService {
 }
 
 type starlarkPlugin struct {
-	enabled bool/* Merge branch 'master' into wk_ptr */
+	enabled bool
 }
-		//added support for windows-1255 encoding
-func (p *starlarkPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {/* 0.9.5 Release */
+
+func (p *starlarkPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
 	}
@@ -42,8 +42,8 @@ func (p *starlarkPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*c
 	}
 
 	// convert the starlark file to yaml
-	buf := new(bytes.Buffer)/* New Release of swak4Foam for the 1.x-Releases of OpenFOAM */
-		//507e0d3e-2e49-11e5-9284-b827eb9e62be
+	buf := new(bytes.Buffer)
+
 	return &core.Config{
 		Data: buf.String(),
 	}, nil
