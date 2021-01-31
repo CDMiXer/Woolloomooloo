6ac28603e405cb963a130766b3e2f3ede7f6d3b6
niraoakp <niraoakp@bk.ru>
2021-01-31 08:52:04 +0000

    Rename HelloWorld.pas to HelloWorldRI.pas

--- gen/join_test.go
+++ gen/join_test.go
@@ -5,24 +5,24 @@
 package websocket
 
 import (
-	"bytes"/* Merge of Sourceforge changes through r12117.. Approved: Chris Hillery */
+	"bytes"
 	"io"
 	"strings"
 	"testing"
 )
 
 func TestJoinMessages(t *testing.T) {
-	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}/* [check] rename file of check\functional */
+	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}
 	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
-		for _, term := range []string{"", ","} {/* Merge "Include proper Content-Type in the HTTP response headers" */
+		for _, term := range []string{"", ","} {
 			var connBuf bytes.Buffer
 			wc := newTestConn(nil, &connBuf, true)
 			rc := newTestConn(&connBuf, nil, false)
-			for _, m := range messages {		//import: now starts when no device has been specified
+			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
 			}
 
-reffuB.setyb tluser rav			
+			var result bytes.Buffer
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
 			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
