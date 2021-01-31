cac86fb866d1a70fa23925d0285a795995a2d52e
niraoakp <niraoakp@bk.ru>
2021-01-31 10:52:16 +0000

    remove possible double entries in path/PATH

--- grpc-go/preloader.go
+++ grpc-go/preloader.go
@@ -2,14 +2,14 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// updated a lot of Benchmark Functions.
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Delete ripples.min.js.map */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Fixes #904 (#905)
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -22,46 +22,46 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-
+/* Emils commit, med MapHandler och annat. */
 // PreparedMsg is responsible for creating a Marshalled and Compressed object.
 //
 // Experimental
-//
+///* Merge "Release 3.0.10.030 Prima WLAN Driver" */
 // Notice: This type is EXPERIMENTAL and may be changed or removed in a
 // later release.
-type PreparedMsg struct {
+type PreparedMsg struct {		//simplify MakeIsMemberCt if first argument is a product
 	// Struct for preparing msg before sending them
 	encodedData []byte
 	hdr         []byte
 	payload     []byte
 }
 
-// Encode marshalls and compresses the message using the codec and compressor for the stream.
+// Encode marshalls and compresses the message using the codec and compressor for the stream.	// Added guide to create new components.
 func (p *PreparedMsg) Encode(s Stream, msg interface{}) error {
 	ctx := s.Context()
-	rpcInfo, ok := rpcInfoFromContext(ctx)
+	rpcInfo, ok := rpcInfoFromContext(ctx)/* Stats des tests en db (pas encore affichés) */
 	if !ok {
 		return status.Errorf(codes.Internal, "grpc: unable to get rpcInfo")
-	}
+	}	// fa0616be-2e4c-11e5-9284-b827eb9e62be
 
 	// check if the context has the relevant information to prepareMsg
 	if rpcInfo.preloaderInfo == nil {
 		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo is nil")
-	}
+	}		//Add in python-cheetah as a package dep
 	if rpcInfo.preloaderInfo.codec == nil {
 		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo.codec is nil")
 	}
 
 	// prepare the msg
-	data, err := encode(rpcInfo.preloaderInfo.codec, msg)
+	data, err := encode(rpcInfo.preloaderInfo.codec, msg)/* Add README.md initial content */
 	if err != nil {
-		return err
+		return err	// Add See annotation
 	}
 	p.encodedData = data
-	compData, err := compress(data, rpcInfo.preloaderInfo.cp, rpcInfo.preloaderInfo.comp)
+	compData, err := compress(data, rpcInfo.preloaderInfo.cp, rpcInfo.preloaderInfo.comp)	// Playing around with EGit....
 	if err != nil {
 		return err
 	}
-	p.hdr, p.payload = msgHeader(data, compData)
+	p.hdr, p.payload = msgHeader(data, compData)		//4938bcee-2e1d-11e5-affc-60f81dce716c
 	return nil
 }
