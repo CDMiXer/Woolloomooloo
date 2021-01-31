cf75acf4d045674b0ac83e286462eaee0e0c7d66
niraoakp <niraoakp@bk.ru>
2021-01-31 21:41:55 +0000

    Link to JH fixed

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -4,20 +4,20 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Complete offline v1 Release */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: hacked by steven@stebalien.com
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* fix Markdown link in README */
  *
  */
 
 // Binary server is an example server.
-package main
+package main	// TODO: will be fixed by mikeal.rogers@gmail.com
 
 import (
 	"context"
@@ -27,7 +27,7 @@ import (
 
 	"google.golang.org/grpc"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"
+	pb "google.golang.org/grpc/examples/features/proto/echo"/* Merge "Release 1.0.0.221 QCACLD WLAN Driver" */
 )
 
 const addr = "localhost:50051"
@@ -35,21 +35,21 @@ const addr = "localhost:50051"
 type ecServer struct {
 	pb.UnimplementedEchoServer
 	addr string
-}
-
+}/* Merge branch 'beta' into improvement/honor-layout-when-no-lastmessage */
+	// TODO: correction de la fonctionnalité de restructuration d'un document
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
-}
+}	// TODO: will be fixed by arachnid@notdot.net
 
 func main() {
-	lis, err := net.Listen("tcp", addr)
+	lis, err := net.Listen("tcp", addr)		//MathJax loading will be initiated after session login. Task #13950
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)	// TODO: will be fixed by mikeal.rogers@gmail.com
 	}
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)/* Merge "Consolidated api-paste.ini file" */
 	}
 }
