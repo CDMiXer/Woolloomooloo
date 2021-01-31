d03dd84936a74606e9d10991e506fc820aea0a6c
niraoakp <niraoakp@bk.ru>
2021-01-31 17:13:46 +0000

    fxied issue with page type changer, added table for layout switcher

--- server/static/static.go
+++ server/static/static.go
@@ -2,7 +2,7 @@ package static
 
 import (
 	"fmt"
-	"net/http"	// TODO: will be fixed by why@ipfs.io
+	"net/http"
 )
 
 type FilesServer struct {
@@ -12,18 +12,18 @@ type FilesServer struct {
 
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 	return &FilesServer{baseHRef, hsts}
-}	// TODO: will be fixed by mikeal.rogers@gmail.com
+}
 
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	//// If there is no stored static file, we'll redirect to the js app
-	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {/* Update Rules and tests for CEG-generation */
+	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
 	//	r.URL.Path = "index.html"
 	//}
 
 	if r.URL.Path == "index.html" {
 		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
-		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}/* Release Scelight 6.4.3 */
+		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
 	}
 
 	w.Header().Set("X-Frame-Options", "DENY")
@@ -31,8 +31,8 @@ func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
-	}	// TODO: answer lvm stripe task
-	//	// TODO: hacked by cory@protocol.ai
-	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine/* Bump version to 3.0.6 */
+	}
+	//
+	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
 	//ServeHTTP(w, r)
 }
