4b86e889767dd72a3bb7c7699ef7b3e6d2402a14
niraoakp <niraoakp@bk.ru>
2021-01-31 14:07:07 +0000

    Hash map unit tests

--- store/cron/cron_oss.go
+++ store/cron/cron_oss.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Merge branch 'master' of https://github.com/sensiasoft/lib-swe-common
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Merge "Add NFS/GlusterFS support to brick library"
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//A little bit more routing stuff.
 
 // +build oss
 
@@ -38,12 +38,12 @@ func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
 }
 
-func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {
+func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {/* Update versionsRelease */
 	return nil, nil
 }
 
 func (noop) FindName(ctx context.Context, id int64, name string) (*core.Cron, error) {
-	return nil, nil
+	return nil, nil	// TODO: will be fixed by steven@stebalien.com
 }
 
 func (noop) Create(ctx context.Context, secret *core.Cron) error {
@@ -53,7 +53,7 @@ func (noop) Create(ctx context.Context, secret *core.Cron) error {
 func (noop) Update(context.Context, *core.Cron) error {
 	return nil
 }
-
+/* Release of eeacms/www:21.4.22 */
 func (noop) Delete(context.Context, *core.Cron) error {
 	return nil
 }
