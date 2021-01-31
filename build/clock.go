0303f8ccf9af9b543b8f14013146d1fbe19be51c
niraoakp <niraoakp@bk.ru>
2021-01-31 15:16:05 +0000

    Add a badge for `travis-ci` build status.

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
 
-import "github.com/raulk/clock"
+import "github.com/raulk/clock"	// Create 1d) Tangos und Cortinas.txt
 
-// Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package.
-///* Dummy commit to trigger CC again */
-htiw elbairav siht ecalper nac emit fo lortnoc deen taht stseT //
-// clock.NewMock(). Always use real time for socket/stream deadlines./* Merge "docs: Android SDK 22.0.4 Release Notes" into jb-mr1.1-ub-dev */
+// Clock is the global clock for the system. In standard builds,	// TODO: will be fixed by witek@enjin.io
+// we use a real-time clock, which maps to the `time` package.	// Update code-editor.riot.html
+//
+// Tests that need control of time can replace this variable with
+// clock.NewMock(). Always use real time for socket/stream deadlines.
 var Clock = clock.New()
