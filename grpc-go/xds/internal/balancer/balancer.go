67cc2c73a7691ffcac74fde36062821787a108d5
niraoakp <niraoakp@bk.ru>
2021-01-31 17:35:47 +0000

    Eliminated need for browser.js forever :)

--- grpc-go/xds/internal/balancer/balancer.go
+++ grpc-go/xds/internal/balancer/balancer.go
@@ -12,18 +12,18 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: @Release [io7m-jcanephora-0.34.3]
+ * limitations under the License.
  *
  */
 
 // Package balancer installs all the xds balancers.
 package balancer
-/* Release 0.14 */
+
 import (
 	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"     // Register the CDS balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clusterimpl"     // Register the xds_cluster_impl balancer
-	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer/* added support for wma and m4a */
-	_ "google.golang.org/grpc/xds/internal/balancer/clusterresolver" // Register the xds_cluster_resolver balancer/* minor error check. */
+	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer
+	_ "google.golang.org/grpc/xds/internal/balancer/clusterresolver" // Register the xds_cluster_resolver balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/priority"        // Register the priority balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/weightedtarget"  // Register the weighted_target balancer
 )
