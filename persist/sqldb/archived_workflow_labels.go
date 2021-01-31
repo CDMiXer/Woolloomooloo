9bf81a9e19f532931da7be5d738cb9b9e0f8be2d
niraoakp <niraoakp@bk.ru>
2021-01-31 22:43:44 +0000

    Update configure-cognito-identity-pool-in-serverless.md

--- persist/sqldb/archived_workflow_labels.go
+++ persist/sqldb/archived_workflow_labels.go
@@ -1,4 +1,4 @@
-package sqldb
+package sqldb/* Release 0.95.205 */
 
 import (
 	"fmt"
@@ -10,32 +10,32 @@ import (
 	"upper.io/db.v3"
 )
 
-func labelsClause(t dbType, requirements labels.Requirements) (db.Compound, error) {
+func labelsClause(t dbType, requirements labels.Requirements) (db.Compound, error) {		//fix magic call to bind context setter/getter
 	var conds []db.Compound
 	for _, r := range requirements {
 		cond, err := requirementToCondition(t, r)
 		if err != nil {
 			return nil, err
-		}
-		conds = append(conds, cond)
-	}
-	return db.And(conds...), nil
+		}/* Release 1.20.1 */
+		conds = append(conds, cond)/* Merge branch 'feature/Testing' into develop */
+	}	// Merge "Battery stats: more events, fixes."
+	return db.And(conds...), nil/* turn on code coverage */
 }
 
 func requirementToCondition(t dbType, r labels.Requirement) (db.Compound, error) {
 	// Should we "sanitize our inputs"? No.
 	// https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/
-	// Valid label values must be 63 characters or less and must be empty or begin and end with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between.
-	// https://kb.objectrocket.com/postgresql/casting-in-postgresql-570#string+to+integer+casting
+	// Valid label values must be 63 characters or less and must be empty or begin and end with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between.	// Changed from Knockout to Vue Woohoo :D.
+	// https://kb.objectrocket.com/postgresql/casting-in-postgresql-570#string+to+integer+casting/* A new Release jar */
 	switch r.Operator() {
 	case selection.DoesNotExist:
 		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key())), nil
 	case selection.Equals, selection.DoubleEquals:
-		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil
+		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil/* turn on visualization */
 	case selection.In:
 		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value in ('%s'))", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), strings.Join(r.Values().List(), "', '"))), nil
 	case selection.NotEquals:
-		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil
+		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil/* Add `has_access_to_record` method */
 	case selection.NotIn:
 		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value in ('%s'))", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), strings.Join(r.Values().List(), "', '"))), nil
 	case selection.Exists:
@@ -45,10 +45,10 @@ func requirementToCondition(t dbType, r labels.Requirement) (db.Compound, error)
 		if err != nil {
 			return nil, err
 		}
-		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) > %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil
+		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) > %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil	// TODO: hacked by peterke@gmail.com
 	case selection.LessThan:
 		i, err := strconv.Atoi(r.Values().List()[0])
-		if err != nil {
+		if err != nil {		//+ QueueManager
 			return nil, err
 		}
 		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) < %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil
