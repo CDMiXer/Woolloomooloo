4e98f5790dd01f0ea3170a403e782e093f7ce7aa
niraoakp <niraoakp@bk.ru>
2021-01-31 05:08:40 +0000

    oops, thanks @lucaswerkmeister

--- pkg/codegen/hcl2/diagnostics.go
+++ pkg/codegen/hcl2/diagnostics.go
@@ -1,13 +1,13 @@
 package hcl2
 
 import (
-	"fmt"
+	"fmt"/* Release 1.15.1 */
 
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2"	// Fixing broken test in JSON io
+	"github.com/hashicorp/hcl/v2/hclsyntax"/* add eventListener that the auth file is updated, if someone changes his account */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
-
+/* Merge "Release Notes 6.1 -- New Features (Plugins)" */
 func errorf(subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
 	return diagf(hcl.DiagError, subject, f, args...)
 }
@@ -16,21 +16,21 @@ func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ..
 	message := fmt.Sprintf(f, args...)
 	return &hcl.Diagnostic{
 		Severity: severity,
-		Summary:  message,
+		Summary:  message,/* Install dependencies in build script */
 		Detail:   message,
 		Subject:  &subject,
-	}
-}
-
+	}	// TODO: Fix IPFS implementation, improve partial detection
+}	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		//adicionando arquivo
 func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Diagnostic {
 	startRange := block.LabelRanges[0]
-
+		//Delete UM_2_0050407.nii.gz
 	diagRange := hcl.Range{
 		Filename: startRange.Filename,
 		Start:    startRange.Start,
 		End:      block.LabelRanges[len(block.LabelRanges)-1].End,
 	}
-	return errorf(diagRange, f, args...)
+	return errorf(diagRange, f, args...)/* Release areca-7.0.7 */
 }
 
 func malformedToken(token string, sourceRange hcl.Range) *hcl.Diagnostic {
@@ -38,22 +38,22 @@ func malformedToken(token string, sourceRange hcl.Range) *hcl.Diagnostic {
 }
 
 func unknownPackage(pkg string, tokenRange hcl.Range) *hcl.Diagnostic {
-	return errorf(tokenRange, "unknown package '%s'", pkg)
-}
+	return errorf(tokenRange, "unknown package '%s'", pkg)/* O's hard bot fix */
+}/* Some minor JS stuff mostly. */
 
-func unknownResourceType(token string, tokenRange hcl.Range) *hcl.Diagnostic {
+func unknownResourceType(token string, tokenRange hcl.Range) *hcl.Diagnostic {/* fix some recently agitated gxfifo-related asserts  */
 	return errorf(tokenRange, "unknown resource type '%s'", token)
-}
+}/* Added not with current conditions of script development */
 
 func unknownFunction(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown function '%s'", token)
 }
-
+/* added setTarget(target:, selector:) example to README */
 func unsupportedBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
 	return errorf(typeRange, "unsupported block of type '%v'", blockType)
 }
 
-func unsupportedAttribute(attrName string, nameRange hcl.Range) *hcl.Diagnostic {
+func unsupportedAttribute(attrName string, nameRange hcl.Range) *hcl.Diagnostic {/* cefcba96-2e41-11e5-9284-b827eb9e62be */
 	return errorf(nameRange, "unsupported attribute '%v'", attrName)
 }
 
