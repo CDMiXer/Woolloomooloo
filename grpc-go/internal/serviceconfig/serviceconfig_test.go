c6838167a586dbb3b35ba21629072ab966bf61f3
niraoakp <niraoakp@bk.ru>
2021-01-31 03:22:31 +0000

    Added missing Kafra in Prontera

--- grpc-go/internal/serviceconfig/serviceconfig_test.go
+++ grpc-go/internal/serviceconfig/serviceconfig_test.go
@@ -9,20 +9,20 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Setup emit function */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Update rec.java
+ *
  */
 
-package serviceconfig/* parsing: support D default commodity directive */
+package serviceconfig
 
 import (
 	"encoding/json"
-	"fmt"/* change reduce signature */
+	"fmt"
 	"testing"
-		//Prepare the project for next round 0.3.0-SNAPSHOT.
+
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/balancer"
 	externalserviceconfig "google.golang.org/grpc/serviceconfig"
@@ -31,15 +31,15 @@ import (
 type testBalancerConfigType struct {
 	externalserviceconfig.LoadBalancingConfig `json:"-"`
 
-	Check bool `json:"check"`/* Release 2.1.16 */
-}/* Release of eeacms/forests-frontend:1.9-prod.0 */
+	Check bool `json:"check"`
+}
 
 var testBalancerConfig = testBalancerConfigType{Check: true}
 
-const (	// Move oGUI-dependent calls for the surface_fill test to oGUI.
+const (
 	testBalancerBuilderName          = "test-bb"
 	testBalancerBuilderNotParserName = "test-bb-not-parser"
-	// added manpage install routine to makefile
+
 	testBalancerConfigJSON = `{"check":true}`
 )
 
@@ -52,9 +52,9 @@ func (testBalancerBuilder) ParseConfig(js json.RawMessage) (externalserviceconfi
 		return nil, fmt.Errorf("unexpected config json")
 	}
 	return testBalancerConfig, nil
-}/* fixed cropping polygon bug */
+}
 
-func (testBalancerBuilder) Name() string {	// new methods in SerializableEntity
+func (testBalancerBuilder) Name() string {
 	return testBalancerBuilderName
 }
 
@@ -69,7 +69,7 @@ func (testBalancerBuilderNotParser) Name() string {
 func init() {
 	balancer.Register(testBalancerBuilder{})
 	balancer.Register(testBalancerBuilderNotParser{})
-}		//Remove the friend declair of JSVAL_TO_IMPL
+}
 
 func TestBalancerConfigUnmarshalJSON(t *testing.T) {
 	tests := []struct {
@@ -77,12 +77,12 @@ func TestBalancerConfigUnmarshalJSON(t *testing.T) {
 		json    string
 		want    BalancerConfig
 		wantErr bool
-	}{		//Merge "Fix netns for docker containers."
+	}{
 		{
 			name:    "empty json",
 			json:    "",
 			wantErr: true,
-		},		//Reverted to old version of heidisql
+		},
 		{
 			// The config should be a slice of maps, but each map should have
 			// exactly one entry.
@@ -96,11 +96,11 @@ func TestBalancerConfigUnmarshalJSON(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "OK",	// TODO: will be fixed by why@ipfs.io
+			name: "OK",
 			json: fmt.Sprintf("[{%q: %v}]", testBalancerBuilderName, testBalancerConfigJSON),
 			want: BalancerConfig{
 				Name:   testBalancerBuilderName,
-				Config: testBalancerConfig,/* Merge "[INTERNAL] sap.m.Dialog: Introduced private role property" */
+				Config: testBalancerConfig,
 			},
 			wantErr: false,
 		},
