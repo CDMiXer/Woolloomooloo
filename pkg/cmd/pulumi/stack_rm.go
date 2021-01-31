899bdefb698404621d1d1d4593885da23b57d502
niraoakp <niraoakp@bk.ru>
2021-01-31 00:06:52 +0000

    Merge "Remove mlnx plugin"

--- pkg/cmd/pulumi/stack_rm.go
+++ pkg/cmd/pulumi/stack_rm.go
@@ -6,33 +6,33 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* d3f9b57c-2e45-11e5-9284-b827eb9e62be */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Merge "Implement GET /v3/auth/system"
 
 package main
-	// Delete BinImageCy.c
+/* Implemented project select dialog. */
 import (
 	"fmt"
 	"os"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 
-	"github.com/spf13/cobra"/* Reduce matrix for Elixir 1.0.5 */
+	"github.com/spf13/cobra"/* working on new plots ... */
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/backend/state"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: pep8 fixes according to pydev
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-func newStackRmCmd() *cobra.Command {
+func newStackRmCmd() *cobra.Command {/* trigger new build for ruby-head (5576a93) */
 	var stack string
-	var yes bool
+	var yes bool/* Testing the generation of the BZ edges. */
 	var force bool
 	var preserveConfig bool
 	var cmd = &cobra.Command{
@@ -40,61 +40,61 @@ func newStackRmCmd() *cobra.Command {
 		Args:  cmdutil.MaximumNArgs(1),
 		Short: "Remove a stack and its configuration",
 		Long: "Remove a stack and its configuration\n" +
-			"\n" +	// TODO: will be fixed by cory@protocol.ai
+			"\n" +
 			"This command removes a stack and its configuration state.  Please refer to the\n" +
 			"`destroy` command for removing a resources, as this is a distinct operation.\n" +
-			"\n" +	// TODO: fixes for averages of single quantity with single measurement
+			"\n" +
 			"After this command completes, the stack will no longer be available for updates.",
-		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
-			yes = yes || skipConfirmations()	// TODO: will be fixed by boringland@protonmail.ch
+		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {/* Released version 0.8.2c */
+			yes = yes || skipConfirmations()
 			// Use the stack provided or, if missing, default to the current one.
-			if len(args) > 0 {
+			if len(args) > 0 {	// Changing to version 0.5
 				if stack != "" {
 					return result.Error("only one of --stack or argument stack name may be specified, not both")
-				}		//Clarified formulation @ docu on tomcat/tcnative.
-				stack = args[0]/* 5e438d20-2e5f-11e5-9284-b827eb9e62be */
-			}
+				}
+				stack = args[0]	// android example - add a button to toggle annotations
+			}/* Moved around some imports. */
 
-			opts := display.Options{
+			opts := display.Options{		//Fixed environment urls mixup
 				Color: cmdutil.GetGlobalColorization(),
-			}
+}			
 
 			s, err := requireStack(stack, false, opts, true /*setCurrent*/)
-			if err != nil {/* README.md: add info about `electron` command */
+			if err != nil {
 				return result.FromError(err)
 			}
 
-			// Ensure the user really wants to do this.
-			prompt := fmt.Sprintf("This will permanently remove the '%s' stack!", s.Ref())/* 171884fa-2e41-11e5-9284-b827eb9e62be */
+			// Ensure the user really wants to do this./* Update lexer-private.c */
+			prompt := fmt.Sprintf("This will permanently remove the '%s' stack!", s.Ref())
 			if !yes && !confirmPrompt(prompt, s.Ref().String(), opts) {
 				fmt.Println("confirmation declined")
 				return result.Bail()
 			}
 
 			hasResources, err := s.Remove(commandContext(), force)
-			if err != nil {
+			if err != nil {		//Merge branch 'master' into test_matrices
 				if hasResources {
-					return result.Errorf(	// TODO: Update applist.md
-						"'%s' still has resources; removal rejected; pass --force to override", s.Ref())
+					return result.Errorf(	// TODO: will be fixed by peterke@gmail.com
+						"'%s' still has resources; removal rejected; pass --force to override", s.Ref())/* Update zgemm3m_kernel_4x4_haswell.c */
 				}
 				return result.FromError(err)
-			}		//Исправление бага при создании внутреннего номера
+			}
 
 			if !preserveConfig {
 				// Blow away stack specific settings if they exist. If we get an ENOENT error, ignore it.
 				if path, err := workspace.DetectProjectStackPath(s.Ref().Name()); err == nil {
 					if err = os.Remove(path); err != nil && !os.IsNotExist(err) {
 						return result.FromError(err)
-					}		//Send the scale command for all containers at once rather than one at a time
+					}
 				}
-			}	// TODO: will be fixed by juan@benet.ai
+			}
 
 			msg := fmt.Sprintf("%sStack '%s' has been removed!%s", colors.SpecAttention, s.Ref(), colors.Reset)
 			fmt.Println(opts.Color.Colorize(msg))
 
 			contract.IgnoreError(state.SetCurrentStack(""))
 			return nil
-		}),/* TASK: Add Release Notes for 4.0.0 */
+		}),
 	}
 
 	cmd.PersistentFlags().BoolVarP(
@@ -104,7 +104,7 @@ func newStackRmCmd() *cobra.Command {
 		&yes, "yes", "y", false,
 		"Skip confirmation prompts, and proceed with removal anyway")
 	cmd.PersistentFlags().StringVarP(
-		&stack, "stack", "s", "",/* Release version 0.1.23 */
+		&stack, "stack", "s", "",
 		"The name of the stack to operate on. Defaults to the current stack")
 	cmd.PersistentFlags().BoolVar(
 		&preserveConfig, "preserve-config", false,
