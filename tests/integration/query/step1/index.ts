f579f0ecee2922e0424aa28c28f5111876d93506
niraoakp <niraoakp@bk.ru>
2021-01-31 23:58:07 +0000

    no need to cast to (char*)

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -3,6 +3,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Step 1: Create a simple resource graph.
+// Step 1: Create a simple resource graph.		//error handling for secrets.inc
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });		//98e4c878-2e63-11e5-9284-b827eb9e62be
+const b = new Resource("b", { state: 2, resource: a });
