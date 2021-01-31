2a04c3676fd6134827ad240ccdf0875027a6c6cb
niraoakp <niraoakp@bk.ru>
2021-01-31 08:18:18 +0000

    Add German and Spanish languages detection by default

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -1,11 +1,11 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// TODO: hacked by arajasek94@gmail.com
-/* Update Create Release.yml */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
 using System;
 using System.Threading.Tasks;
 using Pulumi;
-/* Release 1.9.31 */
+
 class Program
-{		//bug fixes for time selection on error pages
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
@@ -15,18 +15,18 @@ class Program
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
-            var gotError = false;/* Release for 24.7.1 */
+            var gotError = false;
             try
-            {/* 1.6.8 Release */
-                await a.GetValueAsync("val2");	// TODO: will be fixed by brosner@gmail.com
+            {
+                await a.GetValueAsync("val2");
             }
             catch
             {
                 gotError = true;
             }
-	// TODO: hacked by juan@benet.ai
+
             if (!gotError)
-            {		//create lesson14
+            {
                 throw new Exception("Expected to get error trying to read secret from stack reference.");
             }
         });
