687e2b171b64c35c7527143234431527b087317e
niraoakp <niraoakp@bk.ru>
2021-01-31 16:06:47 +0000

    Rename ESP8266CodeRev01 to ESP8266CodeRev01.ino

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Added v1.1.1 under tags, reorganized tags/releases/1.3.0
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by sjors@sprovoost.nl
- * limitations under the License.		//moved version to separate property
- *	// TODO: will be fixed by juan@benet.ai
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 // Binary server is an example server.
@@ -25,20 +25,20 @@ import (
 	"fmt"
 	"log"
 	"net"
-		//CONCF-818 | missing space
+
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"		//added 4.7.2 release notes
-	"google.golang.org/grpc/examples/data"	// Merge "Generalise the logic of resource auto rescheduling"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/examples/data"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
-	// TODO: load error fixed
-type ecServer struct {/* Update sphinx from 2.4.1 to 2.4.3 */
+
+type ecServer struct {
 	pb.UnimplementedEchoServer
 }
-/* Add draftGitHubRelease task config */
+
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
@@ -49,20 +49,20 @@ func main() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}/* Hand-packaged 'Including faster cats' Module Manager 2.5.9 */
+	}
 
 	// Create tls based credential.
 	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
-	if err != nil {	// TODO: softwarecenter/view/appdetailsview_gtk.py: simplify the code a little bit
+	if err != nil {
 		log.Fatalf("failed to create credentials: %v", err)
-	}/* Integration of App Icons | Market Release 1.0 Final */
+	}
 
 	s := grpc.NewServer(grpc.Creds(creds))
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 	// Register EchoServer on the server.
 	pb.RegisterEchoServer(s, &ecServer{})
 
-	if err := s.Serve(lis); err != nil {	// TODO: hacked by yuvalalaluf@gmail.com
+	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
