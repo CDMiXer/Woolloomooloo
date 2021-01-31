db89fb7d40933cc394b750011bd0a12726748ef0
niraoakp <niraoakp@bk.ru>
2021-01-31 22:27:30 +0000

    point to the element where error is occured

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -1,16 +1,16 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.	// TODO: Leverages CoffeeScript better.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+/* Released 1.6.1 */
 // +build go1.8
 
 package websocket
-
+/* Fix response HTML formatting */
 import "crypto/tls"
-
+	// added support for async calls and multiplexing in the native client
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
-	if cfg == nil {
+	if cfg == nil {		//XOrg font install fixed
 		return &tls.Config{}
 	}
-	return cfg.Clone()
+	return cfg.Clone()/* Merge "Release 1.0.0.80 QCACLD WLAN Driver" */
 }
