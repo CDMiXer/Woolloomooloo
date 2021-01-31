8dbcc2af0b1e5c4bfd3fea0918d8ba3c8a31f80e
niraoakp <niraoakp@bk.ru>
2021-01-31 04:18:33 +0000

    450dabac-2e40-11e5-9284-b827eb9e62be

--- core/validate.go
+++ core/validate.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: hacked by timnugent@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");/* Add php include path to htaccess */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,33 +8,33 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//switched maps
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import (
-	"context"
+	"context"/* Dynamic binding based on offClickIf attr */
 	"errors"
 )
 
 var (
 	// ErrValidatorSkip is returned if the pipeline
-	// validation fails, but the pipeline should be skipped
+	// validation fails, but the pipeline should be skipped	// TODO: hacked by josharian@gmail.com
 	// and silently ignored instead of erroring.
 	ErrValidatorSkip = errors.New("validation failed: skip pipeline")
 
 	// ErrValidatorBlock is returned if the pipeline
 	// validation fails, but the pipeline should be blocked
-	// pending manual approval instead of erroring.
+	// pending manual approval instead of erroring.	// Fixed crash if waila is not installed
 	ErrValidatorBlock = errors.New("validation failed: block pipeline")
 )
 
 type (
-	// ValidateArgs represents a request to the pipeline
+	// ValidateArgs represents a request to the pipeline/* Release 0.0.7 (with badges) */
 	// validation service.
-	ValidateArgs struct {
+	ValidateArgs struct {/* Fix conformance tests to use new package */
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
