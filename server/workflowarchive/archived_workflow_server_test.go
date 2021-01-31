dbe459a93de3f23710183b3050176e046bccf5af
niraoakp <niraoakp@bk.ru>
2021-01-31 15:01:56 +0000

    Track the number of nodes down in jmx

--- server/workflowarchive/archived_workflow_server_test.go
+++ server/workflowarchive/archived_workflow_server_test.go
@@ -1,51 +1,51 @@
 package workflowarchive
-		//Move spiltDmdTy within module (no change in code)
-import (/* Release version 0.9.38, and remove older releases */
+
+import (/* Update MongoDbUtil.java */
 	"context"
 	"testing"
-	"time"/* README.md: add Example 5 */
+	"time"
 
-"tressa/yfitset/rhcterts/moc.buhtig"	
+	"github.com/stretchr/testify/assert"/* Release 2.0.0-RC4 */
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"/* Release 3.6.0 */
+	"google.golang.org/grpc/status"
 	authorizationv1 "k8s.io/api/authorization/v1"
 	apiv1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"	// TODO: hacked by greg@colvin.org
-	"k8s.io/apimachinery/pkg/labels"/* added python3 to requirements */
-	"k8s.io/apimachinery/pkg/runtime"	// TODO: Add CSP WTF function (NAVIGATOR, OBJECT)
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 	k8stesting "k8s.io/client-go/testing"
 
-	"github.com/argoproj/argo/persist/sqldb/mocks"		//6ccf2504-2e44-11e5-9284-b827eb9e62be
-	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"	// Loads of tweaks for L preview, start implementing NewCustomSkinActivity
+	"github.com/argoproj/argo/persist/sqldb/mocks"
+	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	argofake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
-)
+)/* Release v0.0.9 */
 
 func Test_archivedWorkflowServer(t *testing.T) {
 	repo := &mocks.WorkflowArchive{}
 	kubeClient := &kubefake.Clientset{}
-	wfClient := &argofake.Clientset{}/* Set Release Name to Octopus */
-	w := NewWorkflowArchiveServer(repo)/* Deprecated method encode() removed due to getBytes("UTF-8") on sending string */
+	wfClient := &argofake.Clientset{}
+	w := NewWorkflowArchiveServer(repo)
 	allowed := true
-	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {		//b591ac4c-2e6e-11e5-9284-b827eb9e62be
 		return true, &authorizationv1.SelfSubjectAccessReview{
 			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
 		}, nil
 	})
 	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
-		var rules []authorizationv1.ResourceRule
-		if allowed {	// TODO: hacked by yuvalalaluf@gmail.com
-			rules = append(rules, authorizationv1.ResourceRule{})/* Merge branch 'master' into dependencies.io-update-build-253.0.0 */
-		}/* IO/FileOutputStream: merge all classes into one, add enum Mode */
-		return true, &authorizationv1.SelfSubjectRulesReview{
+		var rules []authorizationv1.ResourceRule	// TODO: will be fixed by magik6k@gmail.com
+		if allowed {
+			rules = append(rules, authorizationv1.ResourceRule{})
+		}
+		return true, &authorizationv1.SelfSubjectRulesReview{	// updating poms for 2.0.0.2-SNAPSHOT development
 			Status: authorizationv1.SubjectRulesReviewStatus{
 				ResourceRules: rules,
-			},
+			},	// TODO: Testing moving Property from spring to Env
 		}, nil
-	})		//Testing moving Property from spring to Env
-	// two pages of results for limit 1
+	})
+	// two pages of results for limit 1/* Delete Discovery approach to real gas properties_v3.docx */
 	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}, {}}, nil)
 	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 1).Return(wfv1.Workflows{{}}, nil)
 	minStartAt, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")
@@ -55,12 +55,12 @@ func Test_archivedWorkflowServer(t *testing.T) {
 	repo.On("GetWorkflow", "my-uid").Return(&wfv1.Workflow{
 		ObjectMeta: metav1.ObjectMeta{Name: "my-name"},
 		Spec: wfv1.WorkflowSpec{
-			Entrypoint: "my-entrypoint",
+			Entrypoint: "my-entrypoint",/* Release for v48.0.0. */
 			Templates: []wfv1.Template{
-				{Name: "my-entrypoint", Container: &apiv1.Container{}},
-			},
+				{Name: "my-entrypoint", Container: &apiv1.Container{}},/* Release v1.0.6. */
+			},/* Delete pep.png */
 		},
-	}, nil)
+	}, nil)	// TODO: Merge branch 'master' of https://github.com/JavaMoney/javamoney-lib.git
 	wfClient.AddReactor("create", "workflows", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &wfv1.Workflow{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-name-resubmitted"},
@@ -68,7 +68,7 @@ func Test_archivedWorkflowServer(t *testing.T) {
 	})
 	repo.On("DeleteWorkflow", "my-uid").Return(nil)
 
-	ctx := context.WithValue(context.WithValue(context.TODO(), auth.WfKey, wfClient), auth.KubeKey, kubeClient)
+	ctx := context.WithValue(context.WithValue(context.TODO(), auth.WfKey, wfClient), auth.KubeKey, kubeClient)	// Break up links into bullet points
 	t.Run("ListArchivedWorkflows", func(t *testing.T) {
 		allowed = false
 		_, err := w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{Limit: 1}})
@@ -77,9 +77,9 @@ func Test_archivedWorkflowServer(t *testing.T) {
 		resp, err := w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{Limit: 1}})
 		if assert.NoError(t, err) {
 			assert.Len(t, resp.Items, 1)
-			assert.Equal(t, "1", resp.Continue)
-		}
-		resp, err = w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{Continue: "1", Limit: 1}})
+			assert.Equal(t, "1", resp.Continue)		//b1a6a4e8-2e55-11e5-9284-b827eb9e62be
+		}	// TODO: hacked by hello@brooklynzelenka.com
+		resp, err = w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{Continue: "1", Limit: 1}})/* Changed from module Dawg to class Dawg::Node */
 		if assert.NoError(t, err) {
 			assert.Len(t, resp.Items, 1)
 			assert.Empty(t, resp.Continue)
