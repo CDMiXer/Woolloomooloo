bed1c863ed77db1c1473189b296f2ac876440037
niraoakp <niraoakp@bk.ru>
2021-01-31 12:34:49 +0000

    sort datastore before flushing on disk

--- hack/main.go
+++ hack/main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"os"
-)
+)		//Fixing spelling mistake in method name.
 
 func main() {
 	switch os.Args[1] {
-	case "cleancrd":
+	case "cleancrd":	// TODO: Merge "libvirt: add ability to add file and block based filesystem"
 		cleanCRD(os.Args[2])
 	case "removecrdvalidation":
-		removeCRDValidation(os.Args[2])
+		removeCRDValidation(os.Args[2])	// ffmpeg-mt branch: merge from trunk up to rev 2521
 	case "docgen":
 		generateDocs()
-	case "kubeifyswagger":
+	case "kubeifyswagger":/* flags: Include flags in Debug and Release */
 		kubeifySwagger(os.Args[2], os.Args[3])
-	case "secondaryswaggergen":
+	case "secondaryswaggergen":		//Renaming of all editor related projects
 		secondarySwaggerGen()
 	case "parseexamples":
-		parseExamples()
+		parseExamples()/* Delete SuperGroup.lua */
 	case "test-report":
 		testReport()
 	default:
