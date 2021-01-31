f91cf568fb5294445f4287e4e0de254c94535af5
niraoakp <niraoakp@bk.ru>
2021-01-31 22:48:40 +0000

    fix superagent insteaf of supertest

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -1,89 +1,89 @@
 /*
- *
+ *	// TODO: Improve formatting of go code
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* GMParser Production Release 1.0 */
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Makes most pages 100% width
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Rename e64u.sh to archive/e64u.sh - 4th Release */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release and severity updated */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* make Release::$addon and Addon::$game be fetched eagerly */
  */
 
 // Binary client is an example client.
 package main
 
-import (
+import (	// Handle hibernation messages
 	"context"
 	"log"
-	"net"
+	"net"/* Reduce ShaderMgr shader compilation debug chatter in Release builds */
 	"os"
-	"time"
+	"time"/* Release v2.0.0. Gem dependency `factory_girl` has changed to `factory_bot` */
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/channelz/service"	// TODO: d69b1170-2e60-11e5-9284-b827eb9e62be
+	"google.golang.org/grpc/channelz/service"		//Add SpecHelper.draw_relation_registry to draw graphs
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/resolver/manual"	// Merge "Disable ppa jobs."
+	"google.golang.org/grpc/resolver/manual"
 
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"		//what went wrong - debug code
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 const (
 	defaultName = "world"
 )
-		//add getGroup(name:String)
+
 func main() {
-	/***** Set up the server serving channelz service. *****/
+	/***** Set up the server serving channelz service. *****/		//update changelog for a bunch of previous changes
 	lis, err := net.Listen("tcp", ":50052")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	defer lis.Close()
+	defer lis.Close()	// TODO: will be fixed by souzau@yandex.com
 	s := grpc.NewServer()
 	service.RegisterChannelzServiceToServer(s)
 	go s.Serve(lis)
 	defer s.Stop()
-	// TODO: Delete d3.png
+
 	/***** Initialize manual resolver and Dial *****/
-	r := manual.NewBuilderWithScheme("whatever")		//Update Tess binary (01f9a7f)
+	r := manual.NewBuilderWithScheme("whatever")
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))		//Removed unneeded false from poll_spec mockServiceCall call
+	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
+		log.Fatalf("did not connect: %v", err)	// TODO: Added AssignmentEntry to track sign-(in|out)s
+	}/* Merge branch 'precise-stable' into meat-riak-pin */
 	defer conn.Close()
 	// Manually provide resolved addresses for the target.
-	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
+	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})/* Task #3649: Merge changes in LOFAR-Release-1_6 branch into trunk */
 
 	c := pb.NewGreeterClient(conn)
-/* com renovacoes */
-	// Contact the server and print out its response./* rev 718479 */
+	// TODO: hacked by sjors@sprovoost.nl
+	// Contact the server and print out its response.
 	name := defaultName
 	if len(os.Args) > 1 {
-		name = os.Args[1]
+		name = os.Args[1]		//Create 102_BinrayTreeLevelOrderTraversal.cc
 	}
 
 	/***** Make 100 SayHello RPCs *****/
 	for i := 0; i < 100; i++ {
 		// Setting a 150ms timeout on the RPC.
 		ctx, cancel := context.WithTimeout(context.Background(), 150*time.Millisecond)
-)(lecnac refed		
+		defer cancel()
 		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 		if err != nil {
 			log.Printf("could not greet: %v", err)
-		} else {/* Release: 5.4.1 changelog */
-			log.Printf("Greeting: %s", r.Message)/* Fix also projections building for EVE shapes (#206) */
+		} else {
+			log.Printf("Greeting: %s", r.Message)
 		}
 	}
 
 	/***** Wait for user exiting the program *****/
 	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying.
 	// Users can take time to examine and learn about the info provided by channelz.
-	select {}		//started rework of data parsing
-}/* Folder structure of biojava4 project adjusted to requirements of ReleaseManager. */
+	select {}
+}
