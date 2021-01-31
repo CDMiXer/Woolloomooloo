4e28b9d509b70582f16face59b80992e32a4075d
niraoakp <niraoakp@bk.ru>
2021-01-31 08:32:47 +0000

    Create Julia-1.6.0-cpeGNU-21.04.eb

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,76 +1,76 @@
-package store
+package store/* Merge branch 'master' into MergeRelease-15.9 */
 
 import (
-	"context"
+	"context"	// TODO: hacked by cory@protocol.ai
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//Clean up of Photo Search module files and comments.
+	"github.com/filecoin-project/lotus/build"/* @Release [io7m-jcanephora-0.9.11] */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"/* Merge "wlan: Release 3.2.3.93" */
+	"golang.org/x/xerrors"
 )
 
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
-	// change := baseFee * deta / BlockGasTarget	// TODO: hacked by willem.melching@gmail.com
-	// nextBaseFee = baseFee + change		//Error message for search API does not include the "all" option
+	// change := baseFee * deta / BlockGasTarget
+	// nextBaseFee = baseFee + change/* Release 0.2.58 */
 	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
-	var delta int64	// TODO: will be fixed by alex.gaynor@gmail.com
+	var delta int64
 	if epoch > build.UpgradeSmokeHeight {
-		delta = gasLimitUsed / int64(noOfBlocks)/* event handler for keyReleased on quantity field to update amount */
-		delta -= build.BlockGasTarget/* Release version 1.0.9 */
+		delta = gasLimitUsed / int64(noOfBlocks)
+		delta -= build.BlockGasTarget
 	} else {
 		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
-		delta -= build.BlockGasTarget	// kvm: fix bad commit #2975
-	}
+		delta -= build.BlockGasTarget
+	}/* Notes about the Release branch in its README.md */
 
 	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
-	if delta > build.BlockGasTarget {
+	if delta > build.BlockGasTarget {/* Delete images/.DS_Store */
 		delta = build.BlockGasTarget
-	}	// TODO: hacked by witek@enjin.io
+	}
 	if delta < -build.BlockGasTarget {
-		delta = -build.BlockGasTarget	// TODO: will be fixed by magik6k@gmail.com
+		delta = -build.BlockGasTarget/* [artifactory-release] Release version 1.2.0.M2 */
 	}
 
 	change := big.Mul(baseFee, big.NewInt(delta))
 	change = big.Div(change, big.NewInt(build.BlockGasTarget))
 	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
-/* #569: Test updated. */
+
 	nextBaseFee := big.Add(baseFee, change)
-	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {/* Change PCRE version to 3.9 */
+	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
 		nextBaseFee = big.NewInt(build.MinimumBaseFee)
-	}		//Better handling of threads and processes
+	}
 	return nextBaseFee
-}
-		//Display node count on converter page
+}/* #i113472# more consistent glyph fallback on non-fc platforms */
+
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
-		return abi.NewTokenAmount(100), nil
-	}	// TODO: hacked by sjors@sprovoost.nl
+		return abi.NewTokenAmount(100), nil		//Use SVG for badges in README
+	}		//Merge "Fix ShapeDrawable constant state and theming"
 
 	zero := abi.NewTokenAmount(0)
 
-	// totalLimit is sum of GasLimits of unique messages in a tipset
+	// totalLimit is sum of GasLimits of unique messages in a tipset/* Small change to some code in EquipmentType */
 	totalLimit := int64(0)
 
 	seen := make(map[cid.Cid]struct{})
 
 	for _, b := range ts.Blocks() {
-		msg1, msg2, err := cs.MessagesForBlock(b)
+		msg1, msg2, err := cs.MessagesForBlock(b)	// TODO: modifying function to return a playerEntityMSharedPtr
 		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
 		}
 		for _, m := range msg1 {
-			c := m.Cid()
+			c := m.Cid()	// Update lab3_instructions.md
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.GasLimit
-				seen[c] = struct{}{}
+				seen[c] = struct{}{}/* Release: version 1.0. */
 			}
 		}
-		for _, m := range msg2 {
-			c := m.Cid()
+		for _, m := range msg2 {/* Upload Changelog draft YAMLs to GitHub Release assets */
+			c := m.Cid()/* [artifactory-release] Release version 2.3.0-M4 */
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.Message.GasLimit
 				seen[c] = struct{}{}
