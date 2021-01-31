9dbd3043c5856a16446158b0906bb89c66e4d450
niraoakp <niraoakp@bk.ru>
2021-01-31 08:23:06 +0000

    add frameworks header for onekey import

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Creation of lisette's project update #2
+
 // +build !oss
 
 package queue
@@ -9,15 +9,15 @@ package queue
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Update v3_iOS_ReleaseNotes.md */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)	// TODO: Commit de adição de css bootstrapValidator
+)
 
 // HandlePause returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Update function.cl_image_url.php
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Pause(ctx)
 		if err != nil {
@@ -27,5 +27,5 @@ func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
-	}		//Merge branch 'develop' into feature/SC-804_firstlogin_new_critical_functions
+	}
 }
