7a485cc7bf2fe88bf322652e5b3f38f00ab91599
niraoakp <niraoakp@bk.ru>
2021-01-31 14:30:56 +0000

    Moved classes

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,30 +1,30 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"	// 508316dc-2e5b-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
+	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"/* 5.0.0 Release Update */
 )
 
 var _ State = (*state0)(nil)
-
+	// TODO: hacked by ligi@ligi.de
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)	// TODO: to generate only finished games
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}/* v3.1 Release */
-
-type state0 struct {/* Tolerate lost workers during generated state count calculation */
-	account0.State
-	store adt.Store	// fix #3907 by capturing the current restriction in the spec
 }
-	// rev 848315
-func (s *state0) PubkeyAddress() (address.Address, error) {	// Merge "Setup libvirt/kvm permissions on openSUSE"
+
+type state0 struct {/* Release of eeacms/www:19.12.5 */
+	account0.State
+	store adt.Store
+}
+/* Avoid introducing ./ in paths unnecessarily */
+func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
