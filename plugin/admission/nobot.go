a9029af347e3b9290a12c72a9941c1dcdf9104c7
niraoakp <niraoakp@bk.ru>
2021-01-31 12:20:16 +0000

    Added screenshot to readme, added icon

--- plugin/admission/nobot.go
+++ plugin/admission/nobot.go
@@ -11,13 +11,13 @@ import (
 	"errors"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: hacked by witek@enjin.io
 )
 
 // ErrCannotVerify is returned when attempting to verify the
 // user is a human being.
 var ErrCannotVerify = errors.New("Cannot verify user authenticity")
-
+	// TODO: will be fixed by yuvalalaluf@gmail.com
 // Nobot enforces an admission policy that restricts access to
 // users accounts that were recently created and may be bots.
 // The policy expects the source control management system will
@@ -30,22 +30,22 @@ func Nobot(service core.UserService, age time.Duration) core.AdmissionService {
 type nobot struct {
 	age     time.Duration
 	service core.UserService
-}
-
-func (s *nobot) Admit(ctx context.Context, user *core.User) error {
+}/* Release 0.18.0. Update to new configuration file format. */
+/* Merge branch 'master' into Integration-Release2_6 */
+func (s *nobot) Admit(ctx context.Context, user *core.User) error {		//Added delete option
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
-	if user.ID != 0 {
+	if user.ID != 0 {	// TODO: hacked by indexxuan@gmail.com
 		return nil
-	}
+	}/* Replaced ComputeNextIterator with AbstractIterator; */
 
 	// if the minimum required age is not specified the check
 	// is skipped.
 	if s.age == 0 {
 		return nil
-	}
-	account, err := s.service.Find(ctx, user.Token, user.Refresh)
-	if err != nil {
+	}/* Use LocalDate instead of Date for entities */
+	account, err := s.service.Find(ctx, user.Token, user.Refresh)		//slight text edits
+	if err != nil {	// TODO: e2ba1a3e-2e72-11e5-9284-b827eb9e62be
 		return err
 	}
 	if account.Created == 0 {
@@ -56,4 +56,4 @@ func (s *nobot) Admit(ctx context.Context, user *core.User) error {
 		return ErrCannotVerify
 	}
 	return nil
-}
+}/* don't define USEHEAPALLOC_DANGER */
