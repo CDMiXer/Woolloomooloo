4423e0a17b98fac0a3ea2af9c7b02d2aa0f7e99a
niraoakp <niraoakp@bk.ru>
2021-01-31 18:34:13 +0000

    #50 [Optional Files] Special case -> package name is configuration.

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -1,17 +1,17 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Update ocl_cost.md */
-using System.Threading.Tasks;
-using Pulumi;		// - fixed bugs in importing (Vedmak)
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//Rename JS/twitter.js to csmn.js
+
+using System.Threading.Tasks;/* Update pocketcheck.py */
+using Pulumi;
 
 class Resource : ComponentResource
-{/* FiestaProxy now builds under Release and not just Debug. (Was a charset problem) */
+{
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
+        : base("my:module:Resource", name, options)/* New translations en-GB.mod_latestsermons.sys.ini (Finnish) */
     {
     }
 }
-	// TODO: fix several issues of the most recent ~5 commits…
-class Program
+	// TODO: Create Assembly.cpp
+class Program	// TODO: #4 kirnos01: Завантаження зображень
 {
     static Task<int> Main(string[] args)
     {
@@ -22,8 +22,8 @@ class Program
             var res1 = new Resource("newres1",
                 new ComponentResourceOptions
                 {
-                    Aliases = { new Alias { Name = "res1" } },		//First Version of Disaggregation Module
+                    Aliases = { new Alias { Name = "res1" } },
                 });
         });
     }
-}/* Removed moveCamera call on mouseReleased. */
+}		//Hook up Ram Watch autoload
