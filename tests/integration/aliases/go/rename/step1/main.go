163b9906812c6dea1401530e578342133c30d948
niraoakp <niraoakp@bk.ru>
2021-01-31 16:52:41 +0000

    Updated for license compliance.

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,19 +1,19 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* PNG support, initial version */
-
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Merge "Add keystone::bootstrap hiera data"
+/* Adjust for hotmap */
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)		//Delete R$styleable.class
-	// TODO: -do forcestart for gns; doxygen fixes
-// FooComponent is a component resource/* Fixed double alpha appearance with gray colors */
-type FooComponent struct {	// TODO: 477baa0a-2e4b-11e5-9284-b827eb9e62be
+)
+
+// FooComponent is a component resource
+type FooComponent struct {
 	pulumi.ResourceState
 }
-
+		//afd4b076-2e44-11e5-9284-b827eb9e62be
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
 	})
-}/* Release 1.0.33 */
+}
