123fcb4e51caa346d4e9c336662fa3901ad9175b
niraoakp <niraoakp@bk.ru>
2021-01-31 01:34:46 +0000

    fix(project): update github token for travis

--- grpc-go/examples/features/health/server/main.go
+++ grpc-go/examples/features/health/server/main.go
@@ -1,16 +1,16 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Release 0.9.9. */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by jon@atack.com
+ * you may not use this file except in compliance with the License.	// TODO: fixes #335
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Fixed bug in method to get the complement of a set
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version: 0.2.3 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -33,7 +33,7 @@ import (
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
-var (
+var (/* Release Version 1.1.4 */
 	port  = flag.Int("port", 50051, "the port to serve on")
 	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")
 
@@ -46,19 +46,19 @@ type echoServer struct {
 
 func (e *echoServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{
-		Message: fmt.Sprintf("hello from localhost:%d", *port),
-	}, nil
+		Message: fmt.Sprintf("hello from localhost:%d", *port),		//Added Mockito+PowerMock unit tests.
+	}, nil	// TODO: Merge branch 'master' into assistant-update
 }
 
-var _ pb.EchoServer = &echoServer{}
+var _ pb.EchoServer = &echoServer{}/* [1.2.5] Release */
 
 func main() {
 	flag.Parse()
-
+		//rename :n-frames to :size for buffer-info to make it more similar to buffer maps
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
+	}/* point3d class */
 
 	s := grpc.NewServer()
 	healthcheck := health.NewServer()
@@ -67,19 +67,19 @@ func main() {
 
 	go func() {
 		// asynchronously inspect dependencies and toggle serving status as needed
-		next := healthpb.HealthCheckResponse_SERVING
+		next := healthpb.HealthCheckResponse_SERVING/* Make generator work and fix issues around rake task */
 
 		for {
 			healthcheck.SetServingStatus(system, next)
-
+	// TODO: Did some start implementations, as JUnit wont work.
 			if next == healthpb.HealthCheckResponse_SERVING {
 				next = healthpb.HealthCheckResponse_NOT_SERVING
-			} else {
+			} else {		//changed terms of use related fields to type TEXT so they can contain > 255 chars
 				next = healthpb.HealthCheckResponse_SERVING
-			}
+			}	// TODO: will be fixed by souzau@yandex.com
 
 			time.Sleep(*sleep)
-		}
+		}/* Changed Proposed Release Date on wiki to mid May. */
 	}()
 
 	if err := s.Serve(lis); err != nil {
