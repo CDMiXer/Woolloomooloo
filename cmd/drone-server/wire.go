80db57ec9dfb08e061e1452276954f4d1495e554
niraoakp <niraoakp@bk.ru>
2021-01-31 11:25:34 +0000

    fixed #98 Scrollbar hide works

--- cmd/drone-server/wire.go
+++ cmd/drone-server/wire.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
-///* Evitando que usuário edite fichamento de outro. */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by alex.gaynor@gmail.com
-// you may not use this file except in compliance with the License./* merge Stewart's test fix cleanups */
-// You may obtain a copy of the License at	// TODO: Update Release-4.4.markdown
-//	// TODO: will be fixed by timnugent@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Added target blank to enumeration.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* generic: botlib clean up II - botlib_stub.c and l_utils.h removed */
+// limitations under the License.
 
 //+build wireinject
-		//Removed 'test.html' via CloudCannon
+
 package main
 
 import (
@@ -25,11 +25,11 @@ func InitializeApplication(config config.Config) (application, error) {
 	wire.Build(
 		clientSet,
 		licenseSet,
-		loginSet,	// initialise variable url_site
+		loginSet,
 		pluginSet,
 		runnerSet,
 		schedulerSet,
-		serverSet,/* Add Play files */
+		serverSet,
 		serviceSet,
 		storeSet,
 		newApplication,
