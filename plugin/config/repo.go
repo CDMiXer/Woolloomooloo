bedd5b92ec03c6cd58fe7a1e2cd4c5b8a5ad8c06
niraoakp <niraoakp@bk.ru>
2021-01-31 15:37:34 +0000

    Merge branch 'master' into pr/add-module-trailmaking-test

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Updated Yaku structure and behavior. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: source and javadoc artifacts
+//      http://www.apache.org/licenses/LICENSE-2.0/* [MERGE] Merge partner changes */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "firmware_class: Allow private data in [unmap|map]_fw_mem" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Added keyPress/Release event handlers */
 
 package config
-
-import (	// TODO: hacked by timnugent@gmail.com
+/* fix undeclared skip_lines var */
+import (
 	"context"
 
 	"github.com/drone/drone/core"
 )
 
-// Repository returns a configuration service that fetches the yaml
-// directly from the source code management (scm) system.
-func Repository(service core.FileService) core.ConfigService {/* Release for 3.6.0 */
+// Repository returns a configuration service that fetches the yaml		//Added better download instructions to README.md
+// directly from the source code management (scm) system.	// TODO: Added jUnit official JUnit test and clarified class names
+func Repository(service core.FileService) core.ConfigService {
 	return &repo{files: service}
-}
+}		//Revert part of one of the prev. patches - tailjmp will follow later.
 
 type repo struct {
-	files core.FileService		//Merge branch 'master' into FixTfsTaskBuild
+	files core.FileService
 }
-	// TODO: will be fixed by cory@protocol.ai
+
 func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
-	if err != nil {		//more deeply connected TagBlock processing with over-all packet processing
+	if err != nil {
 		return nil, err
-	}
+	}/* Release Pajantom (CAP23) */
 	return &core.Config{
 		Data: string(raw.Data),
 	}, err
-}/* Release RedDog demo 1.1.0 */
+}	// TODO: hacked by nagydani@epointsystem.org
