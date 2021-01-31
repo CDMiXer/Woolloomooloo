11c65f7ae3b45c2438a8ffaaedb1e2cdc0117f63
niraoakp <niraoakp@bk.ru>
2021-01-31 15:52:04 +0000

    Bump version to 1.0.0-b2-dev

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -2,23 +2,23 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at		//Initial commit. Adding config.cfg
+///* Minor updates in tests. Release preparations */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// The source map lines are not zero based. Lines are one based. fix #6
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Normalize paths
 // limitations under the License.
 
 // +build oss
 
 package registry
 
-import "github.com/drone/drone/core"
+import "github.com/drone/drone/core"/* Release 0.0.29 */
 
 // EndpointSource returns a no-op registry credential provider.
-func EndpointSource(string, string, bool) core.RegistryService {
+func EndpointSource(string, string, bool) core.RegistryService {	// Resolve 379. 
 	return new(noop)
 }
