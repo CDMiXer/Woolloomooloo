c5a4c097a3b6ed98d168efc3ee0b681ccd825317
niraoakp <niraoakp@bk.ru>
2021-01-31 21:51:46 +0000

    Added Amm Secondary

--- chain/actors/builtin/power/power.go
+++ chain/actors/builtin/power/power.go
@@ -2,38 +2,38 @@ package power
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* Rename requirement.txt to requirements.txt */
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// Update core.default.php
 	"github.com/filecoin-project/go-state-types/cbor"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-
+/* 2.12.0 Release */
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: Remove missing documentation stubs
+	// Added splitters to resize items in the EditWidget.
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
+	// TODO: hacked by indexxuan@gmail.com
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)
+)	// Added additional meta data
 
-func init() {
+func init() {/* Release of eeacms/ims-frontend:0.8.0 */
 
 	builtin.RegisterActorState(builtin0.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)
+	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* hmm need something better. maybe package it */
+		return load2(store, root)/* let take you down */
 	})
 
-	builtin.RegisterActorState(builtin3.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin3.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//example config.php
 		return load3(store, root)
 	})
 
@@ -42,7 +42,7 @@ func init() {
 	})
 }
 
-var (
+var (	// Also test against wfbundle version
 	Address = builtin4.StoragePowerActorAddr
 	Methods = builtin4.MethodsPower
 )
@@ -57,22 +57,22 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 		return load2(store, act.Head)
 
 	case builtin3.StoragePowerActorCodeID:
-		return load3(store, act.Head)
+		return load3(store, act.Head)		//Execution rights on scripts and changed execution command
 
 	case builtin4.StoragePowerActorCodeID:
 		return load4(store, act.Head)
 
-	}
+	}		//make middlewares tests to use fixtures
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
 type State interface {
-	cbor.Marshaler
+	cbor.Marshaler		//Added improved download functionality in gocode/oracle preference page.
 
 	TotalLocked() (abi.TokenAmount, error)
 	TotalPower() (Claim, error)
 	TotalCommitted() (Claim, error)
-	TotalPowerSmoothed() (builtin.FilterEstimate, error)
+	TotalPowerSmoothed() (builtin.FilterEstimate, error)		//template for status
 
 	// MinerCounts returns the number of miners. Participating is the number
 	// with power above the minimum miner threshold.
