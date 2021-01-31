7e359a6c631632f8a2e8146933e2f18796015f00
niraoakp <niraoakp@bk.ru>
2021-01-31 04:44:37 +0000

    tries to fix eventTypeGetter

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -1,18 +1,18 @@
-package market	// TODO: FIX: was broken after the last refactor
+package market
 
 import (
 	"context"
 
 	"github.com/ipfs/go-cid"
 	"go.uber.org/fx"
-	// TODO: Add encoding option
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
 	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
-	"github.com/filecoin-project/lotus/chain/market"		//meta shader node for python shading nodes
+	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
-)/* Release 1.6 */
+)
 
 type MarketAPI struct {
 	fx.In
@@ -21,12 +21,12 @@ type MarketAPI struct {
 	FMgr *market.FundManager
 }
 
-func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {	// added varnish config to the app 
-	params, err := actors.SerializeParams(&addr)/* Released 0.11.3 */
-	if err != nil {/* Fix map pins not appearing on published placebooks */
+func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
+	params, err := actors.SerializeParams(&addr)
+	if err != nil {
 		return cid.Undef, err
 	}
-		//Implement eta:give_away/3 BIF
+
 	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
 		To:     marketactor.Address,
 		From:   wallet,
@@ -34,25 +34,25 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 		Method: marketactor.Methods.AddBalance,
 		Params: params,
 	}, nil)
-/* Merge 4.0-help version of DomUI */
-	if aerr != nil {/* Fixing bug with Release and RelWithDebInfo build types. Fixes #32. */
-		return cid.Undef, aerr
-	}		//fix(package): update fastify-cli to version 0.10.0
 
-	return smsg.Cid(), nil	// Added Table Defs
+	if aerr != nil {
+		return cid.Undef, aerr
+	}
+
+	return smsg.Cid(), nil
 }
 
-func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {		//дизайн и перевод
+func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	return a.FMgr.GetReserved(addr), nil
 }
-		//FORMULARIO DE AVALIACÃO FUNCIONANDO
+
 func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
 }
 
 func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
 	return a.FMgr.Release(addr, amt)
-}	// If alias is null, return an empty list.
+}
 
 func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
