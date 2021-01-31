d259e19c0065d7bcfd44f6f9307d59f3285c0a48
niraoakp <niraoakp@bk.ru>
2021-01-31 13:19:13 +0000

    Deleted include markdown-panel-12.md

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -4,30 +4,30 @@ import * as aws from "@pulumi/aws";
 // Create a new security group for port 80.
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
-    fromPort: 0,	// TODO: will be fixed by mikeal.rogers@gmail.com
+    fromPort: 0,/* [FIX] mail: get set (python) of partners for email_from */
     toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
     filters: [{
-        name: "name",	// TODO: Update build-filmography.sh
+        name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
     }],
-    owners: ["137112412989"],		//Work in progress: New hub as a webservice in tomcat
+    owners: ["137112412989"],/* Rename EDaeShee2Ah.shtest to test */
     mostRecent: true,
-});
+});		//Sending email to parent when choice is accepted by handler from the waiting list
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
-    tags: {/* Merge "Release green threads properly" */
+    tags: {
         Name: "web-server-www",
-    },/* Merge "Rename 'history' -> 'Release notes'" */
+    },
     instanceType: "t2.micro",
     securityGroups: [securityGroup.name],
-    ami: ami.then(ami => ami.id),
+    ami: ami.then(ami => ami.id),	// TODO: seeems work
     userData: `#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &/* Flat LazyImage */
-`,		//Added grunt-json dependency
-});
+nohup python -m SimpleHTTPServer 80 &/* Новый дизайн бокса контент */
+`,/* Merge "If an exposed method returns nothing, reply with an HTTP 204." */
+});	// TODO: hacked by arachnid@notdot.net
 export const publicIp = server.publicIp;
 export const publicHostName = server.publicDns;
