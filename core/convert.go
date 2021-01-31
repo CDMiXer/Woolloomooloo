f0e2deb716eecd1eb8becd967127f46455ae564b
niraoakp <niraoakp@bk.ru>
2021-01-31 14:52:40 +0000

    additional features added to executable axldiff

--- core/convert.go
+++ core/convert.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
-///* [dist] Release v0.5.7 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release version 4.0 */
-///* Bugfix: Copied code incorrectly.. */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Increased Coverage
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Fixed image formatting on newsletter page
+// limitations under the License.
 
 package core
 
-import "context"	// Rewrite kazoo mention
+import "context"
 
-type (/* Orc Mystic skill Fear affects Players (tnx slyce) */
-	// ConvertArgs represents a request to the pipeline/* [PAXEXAM-511] New module regression-karaf */
-	// conversion service./* Fixed errors in FR translations */
+type (
+	// ConvertArgs represents a request to the pipeline
+	// conversion service.
 	ConvertArgs struct {
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
-	}	// TODO: hacked by mail@bitpshr.net
+	}
 
 	// ConvertService converts non-native pipeline
 	// configuration formats to native configuration
 	// formats (e.g. jsonnet to yaml).
 	ConvertService interface {
-		Convert(context.Context, *ConvertArgs) (*Config, error)		//Added test for static initializer and final class removal
+		Convert(context.Context, *ConvertArgs) (*Config, error)
 	}
 )
