7496e5d523f3b8b0a4144d20e8b9c0795ca6ac0d
niraoakp <niraoakp@bk.ru>
2021-01-31 08:35:20 +0000

    fixed a minor problem about PERS3-PAST and added imperfective past

--- core/transfer.go
+++ core/transfer.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: 6d7e3991-2d48-11e5-a514-7831c1c36510
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Force refresh the execution dialog during the algorithm execution.
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -15,9 +15,9 @@
 package core
 
 import "context"
-/* Added Timeline fn, and hopefully didn't delete Oncoscape.R */
-// Transferer handles transfering repository ownership from one	// Merge branch 'feature/11' into develop
+
+// Transferer handles transfering repository ownership from one
 // user to another user account.
-type Transferer interface {		//bb188ac2-2e60-11e5-9284-b827eb9e62be
+type Transferer interface {
 	Transfer(ctx context.Context, user *User) error
-}/* Update tests.c */
+}
