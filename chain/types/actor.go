d2afd282c0d257dae2ec36574cd91bd2a0e4f039
niraoakp <niraoakp@bk.ru>
2021-01-31 18:48:50 +0000

    fe4d89d2-2e6f-11e5-9284-b827eb9e62be

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
-
+		//add warning message if no mixer was found
 import (
 	"errors"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Create Orchard-1-7-1-Release-Notes.markdown */
 )
 
-var ErrActorNotFound = errors.New("actor not found")
+var ErrActorNotFound = errors.New("actor not found")	// bits of clarity
 
-type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+type Actor struct {	// TODO: hacked by arajasek94@gmail.com
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`./* Login e deletar funcionando */
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
-	Balance BigInt
+	Balance BigInt	// The interval should be cleared on unmount
 }
