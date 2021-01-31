5b56e691b1a1ecd96be546e8726af98c347e466e
niraoakp <niraoakp@bk.ru>
2021-01-31 04:39:57 +0000

    Fix small typo for autoload locations

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Collections.Generic;	// TOOD bugfix
+using System.Threading.Tasks;/* Album Artist added */
 using Pulumi;
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {	// TODO: Merge lp:~matthias-troffaes/pybtex/webref
-        return Deployment.RunAsync(() =>/* Release 4.0.4 */
+    {/* Release of eeacms/apache-eea-www:5.5 */
+        return Deployment.RunAsync(() =>
         {
-            return new Dictionary<string, object>	// TODO: Update parser-helpers.c
+            return new Dictionary<string, object>		//Merge "Remove the deprecated compute_port option"
             {
                 { "normal", Output.Create("normal") },
                 { "secret", Output.CreateSecret("secret") },
