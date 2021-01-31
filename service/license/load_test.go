78fb63b1fe65571a238c8c8ca27a7324dd870445
niraoakp <niraoakp@bk.ru>
2021-01-31 21:05:58 +0000

    Updated toolbox setting function

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// you may not use this file except in compliance with the License.		//Merge "Hwui: Remove unused variables"
+// You may obtain a copy of the License at/* Generating test coverage report */
+///* Rename notebooks/notebook01.md to notebooks.md */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Use core 2.7.2 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
