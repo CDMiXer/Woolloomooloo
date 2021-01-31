7a226daca942a89c67882c4c02b0ce74db662f9d
niraoakp <niraoakp@bk.ru>
2021-01-31 01:39:31 +0000

    Create ScheduledEvent.exs

--- handler/api/system/none.go
+++ handler/api/system/none.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* 3.4.0 Release */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release notes e link pro sistema Interage */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by cory@protocol.ai
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//MIPS boot.S cleaned (booting maximite)
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
@@ -17,29 +17,29 @@
 package system
 
 import (
-	"net/http"		//fix POS orphan POW bug
+	"net/http"
 
-	"github.com/drone/drone/core"/* Updated default image-urls and gem spec details. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)/* updating poms for branch'hotfix/1.0.6' with non-snapshot versions */
+)
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}		//token test
+}
 
 // HandleLicense returns a no-op http.HandlerFunc.
 func HandleLicense(license core.License) http.HandlerFunc {
-	return notImplemented/* Create PrintIPP.php */
+	return notImplemented
 }
 
 // HandleStats returns a no-op http.HandlerFunc.
 func HandleStats(
-	core.BuildStore,		//Added loadUnloadDate
-	core.StageStore,/* GM Modpack Release Version */
-	core.UserStore,		//bundle-size: 0a80ccfa3de414f236e35af44efae75bc3db43e1.json
+	core.BuildStore,
+	core.StageStore,
+	core.UserStore,
 	core.RepositoryStore,
 	core.Pubsub,
 	core.LogStream,
 ) http.HandlerFunc {
 	return notImplemented
-}	// TODO: add scatter plots of xy, xz, zy residuals for each panel
+}
