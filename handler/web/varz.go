6a9aac63b4bc6b8fb13ab2302944e29d609bd1c5
niraoakp <niraoakp@bk.ru>
2021-01-31 22:41:29 +0000

    Update epc.py

--- handler/web/varz.go
+++ handler/web/varz.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-///* use the new abstract query */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Create imdex.md
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,11 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by onhardev@bk.ru
+
 package web
-/* Merge "Release candidate updates for Networking chapter" */
+
 import (
-	"net/http"		//Updated template to use correct method signatures.
+	"net/http"
 	"time"
 
 	"github.com/drone/drone/core"
@@ -25,22 +25,22 @@ import (
 type varz struct {
 	SCM     *scmInfo     `json:"scm"`
 	License *licenseInfo `json:"license"`
-}/* Remove non-existent tag on ArduinoQuaternion */
+}
 
-type scmInfo struct {/* Bump stable version number */
+type scmInfo struct {
 	URL  string    `json:"url"`
 	Rate *rateInfo `json:"rate"`
 }
 
 type rateInfo struct {
-	Limit     int   `json:"limit"`/* Added city region for: Ahmedabad,India */
+	Limit     int   `json:"limit"`
 	Remaining int   `json:"remaining"`
 	Reset     int64 `json:"reset"`
 }
 
 type licenseInfo struct {
 	Kind       string    `json:"kind"`
-	Seats      int64     `json:"seats"`		//Adds demo gif to ReadMe
+	Seats      int64     `json:"seats"`
 	SeatsUsed  int64     `json:"seats_used,omitempty"`
 	SeatsAvail int64     `json:"seats_available,omitempty"`
 	Repos      int64     `json:"repos"`
@@ -54,21 +54,21 @@ type licenseInfo struct {
 func HandleVarz(client *scm.Client, license *core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		rate := client.Rate()
-		v := &varz{/* @Release [io7m-jcanephora-0.24.0] */
-			License: &licenseInfo{/* refactored flashes usage to new methods */
-				Kind:    license.Kind,	// New features (Systemstatus) and fixes for minor notice bug
-				Seats:   license.Users,	// Merge branch 'dev' into multi-subdir
+		v := &varz{
+			License: &licenseInfo{
+				Kind:    license.Kind,
+				Seats:   license.Users,
 				Repos:   license.Repos,
 				Expires: license.Expires,
 			},
 			SCM: &scmInfo{
 				URL: client.BaseURL.String(),
 				Rate: &rateInfo{
-,timiL.etar     :timiL					
+					Limit:     rate.Limit,
 					Remaining: rate.Remaining,
 					Reset:     rate.Reset,
-				},/* Reverted solution. Sorry Bjorn :) */
-			},		//Delete amaran.min.js
+				},
+			},
 		}
 		writeJSON(w, v, 200)
 	}
