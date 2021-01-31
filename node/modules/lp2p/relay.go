20484815f5c5b6943a5645659d513e6f539fb646
niraoakp <niraoakp@bk.ru>
2021-01-31 22:36:46 +0000

    Add log levels everywhere

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -10,19 +10,19 @@ import (
 )
 
 func NoRelay() func() (opts Libp2pOpts, err error) {
-	return func() (opts Libp2pOpts, err error) {	// TODO: hacked by mail@bitpshr.net
-		// always disabled, it's an eclipse attack vector	// TODO: will be fixed by joshua@yottadb.com
-		opts.Opts = append(opts.Opts, libp2p.DisableRelay())	// TODO: hacked by lexy8russo@outlook.com
+	return func() (opts Libp2pOpts, err error) {
+		// always disabled, it's an eclipse attack vector
+		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
-	}	// restore travis command for behat tests, line 104
+	}
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")		//Reformulando tratamento de erros
+		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
-/* Updated File Parsing and IO. */
+
 	return discovery.NewRoutingDiscovery(crouter), nil
-}	// TODO: start aggregation operations
+}
