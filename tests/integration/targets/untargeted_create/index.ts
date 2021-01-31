aa02c02d7fb09e08dd80cccef9437ee180475438
niraoakp <niraoakp@bk.ru>
2021-01-31 22:17:41 +0000

    pyo +  Ejemplo de síntesis en Raspberry Pi

--- tests/integration/targets/untargeted_create/index.ts
+++ tests/integration/targets/untargeted_create/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Updating SSL support and adding documented commands. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* Release 0.13.1 (#703) */
+import * as pulumi from "@pulumi/pulumi";/* Release v0.4.1 */
 
-import * as pulumi from "@pulumi/pulumi";/* bf64da24-2e68-11e5-9284-b827eb9e62be */
-	// TODO: Extra printout
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
@@ -10,22 +10,22 @@ class Provider implements pulumi.dynamic.ResourceProvider {
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {/* Changed Error handling code in the RTSS's sub-render states to inform on errors */
-            return {		//PI-38 Minor formatting
-                id: (currentID++) + "",		//Update howto use this library
-                outs: undefined,
+        this.create = async (inputs: any) => {
+            return {
+                id: (currentID++) + "",
+                outs: undefined,	// Merge "Run the v3 only job in neutron"
             };
-        };/* Optimization and error handling. */
-    }/* Update Recent and Upcoming Releases */
+        };
+    }/* Preparing WIP-Release v0.1.39.1-alpha */
 }
-/* 6f8fc912-2e65-11e5-9284-b827eb9e62be */
+
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);	// TODO: a43cb0c8-2e72-11e5-9284-b827eb9e62be
+        super(Provider.instance, name, {}, opts);
     }
 }
 
 // Create a resource using the default dynamic provider instance.
-let a = new Resource("a");		//Merge branch 'develop' of https://github.com/peuter/CometVisu.git into develop
+let a = new Resource("a");
 
-export const urn = a.urn;/* Create simple-credo.gabc */
+export const urn = a.urn;
