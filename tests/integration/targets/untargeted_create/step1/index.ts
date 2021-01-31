20c54d8552c7f56d0122b4833bf34693cb823a97
niraoakp <niraoakp@bk.ru>
2021-01-31 10:37:36 +0000

    automated commit from rosetta for sim/lib graphing-quadratics, locale pt_BR

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-/* Update mcs/class/System/System.Net/WebRequest.cs */
+
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
-    public static instance = new Provider();/* update basic http auth credentials */
+    public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Comparison of tables during altering fixed. (BUG#39399) */
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
             return {
-                id: (currentID++) + "",	// TODO: hacked by brosner@gmail.com
+                id: (currentID++) + "",
                 outs: undefined,
             };
         };
     }
-}	// Merge branch 'master' into fix/#679
+}
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, {}, opts);
-    }		//fix bug #592436
+    }
 }
 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
-let b = new Resource("b");/* [hands free] */
+let b = new Resource("b");
 
 export const urn = a.urn;
