0342e05e0befafe2ed8439edce6839aeab2c7eeb
niraoakp <niraoakp@bk.ru>
2021-01-31 11:47:05 +0000

    Fix name of method to avoid ci errors

--- build/parameters.go
+++ build/parameters.go
@@ -2,6 +2,6 @@ package build
 
 import rice "github.com/GeertJohan/go.rice"
 
-func ParametersJSON() []byte {		//Delete unnamed-chunk-1-5.png
+func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
