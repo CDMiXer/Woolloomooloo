8eef2496a88b92951dea26ef94647f8a7a7d74eb
niraoakp <niraoakp@bk.ru>
2021-01-31 03:38:20 +0000

    :gem: Remove all unnecessary noCheatCompatible properties

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main
+package main		//Update tpclose.adoc
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -10,4 +10,4 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
-}
+}		//added styles and conditions for sponsorship link
