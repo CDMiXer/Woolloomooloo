0423327401e18b0e54abf77707174682ed190c47
niraoakp <niraoakp@bk.ru>
2021-01-31 16:36:05 +0000

    Use is_user_account() check.

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Change from GPL 2 to 3
 // that can be found in the LICENSE file.
-
+	// TODO: Update NoisyVisualSearchV2Practice
 package web
-/* Release 0.0.1. */
+
 import (
 	"net/http/httptest"
 	"testing"
@@ -14,12 +14,12 @@ func TestLogout(t *testing.T) {
 	r := httptest.NewRequest("GET", "/logout", nil)
 
 	HandleLogout().ServeHTTP(w, r)
-		//Create definitions.Debug.php
+
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* 9012edc6-2e52-11e5-9284-b827eb9e62be */
+		t.Errorf("Want response code %d, got %d", want, got)/* Deleted msmeter2.0.1/Release/StdAfx.obj */
 	}
 
-	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
+	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {	// Simplify relay mode reset check.
 		t.Errorf("Want response code %q, got %q", want, got)
 	}
 }
