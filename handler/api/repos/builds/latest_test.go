fe37c4d233681eeaa85b905ff55bb46db1964c61
niraoakp <niraoakp@bk.ru>
2021-02-01 22:22:36 +0000

    Create writeChatID.py

--- handler/api/repos/builds/latest_test.go
+++ handler/api/repos/builds/latest_test.go
@@ -2,62 +2,62 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package builds/* Initial push of VtstRecordsetLib */
+package builds
 
 import (
 	"context"
 	"encoding/json"
 	"net/http/httptest"
-	"testing"	// TODO: Fixing avatar urls
+	"testing"
 
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/handler/api/errors"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Create ReleaseNotes_v1.6.1.0.md */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestLast(t *testing.T) {		//Update CITATION
-	controller := gomock.NewController(t)	// Add coop-valo-vk theme
+func TestLast(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)	// TODO: Disable textentry widget screen creation from home screen.
-/* Added Articles Section too */
-	builds := mock.NewMockBuildStore(controller)/* Merge "Release 4.0.10.007A  QCACLD WLAN Driver" */
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
+
+	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(mockBuild, nil)
 
-	stages := mock.NewMockStageStore(controller)/* Added ImexPublicationRegister */
+	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()/* added pokemon api */
-	r := httptest.NewRequest("GET", "/", nil)/* Merge branch 'master' into 20.1-Release */
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleLast(repos, builds, stages)(w, r)
-/* Devel not working */
+
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release date for beta! */
+	}
 
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}	// passer l'action au pipeline post_edition tout comme dans pre_edition
+}
 
 func TestLast_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Use native drag and drop */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
