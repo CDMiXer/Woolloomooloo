f1680fb1acb91d3efe45418a1c6be7074d2b9475
niraoakp <niraoakp@bk.ru>
2021-02-01 14:09:33 +0000

    fixed client bug in use of orphan method

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-		//trailify gruppenzuordnung, fixes #3142
-class Provider implements pulumi.dynamic.ResourceProvider {/* Some neo4j bug improvement */
-    public static instance = new Provider();
-	// TODO: Merge "[INTERNAL] Form: forward editable state to FormElement"
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {
+class Provider implements pulumi.dynamic.ResourceProvider {
+    public static instance = new Provider();
+/* Merge "[Release] Webkit2-efl-123997_0.11.11" into tizen_2.1 */
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Merge "Return missing authtoken options" */
+/* Merge "msm: 8660: audio: Add headset speaker stereo device." into msm-2.6.35 */
+    constructor() {/* add fake mouseReleaseEvent in contextMenuEvent (#285) */
         this.create = async (inputs: any) => {
-            return {/* Changelog for #5409, #5404 & #5412 + Release date */
+            return {
                 id: "0",
                 outs: undefined,
-            };
+            };/* 112f7c35-2e9d-11e5-b54a-a45e60cdfd11 */
         };
     }
 }
-/* Merge branch 'master' into globematerials */
-class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);
-    }
-}/* Detect props on <FormControl> `accepter` component */
 
-// Create a resource using the default dynamic provider instance./* Release date added, version incremented. */
+class Resource extends pulumi.dynamic.Resource {/* Release for 2.9.0 */
+    constructor(name: string, opts?: pulumi.ResourceOptions) {/* Added changes from Release 25.1 to Changelog.txt. */
+        super(Provider.instance, name, {}, opts);
+    }		//Delete README.cron
+}
+
+// Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
-		//Initial import of Seda 2 feature project (RCP).
+
 // Attempt to read the created resource.
 let b = new Resource("b", { id: a.id });
