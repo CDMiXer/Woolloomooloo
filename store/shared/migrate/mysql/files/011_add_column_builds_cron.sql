ef3970b9c8ab6a56fd8b4d1116676c996e819102
niraoakp <niraoakp@bk.ru>
2021-02-01 08:25:59 +0000

    Scénario save improvement

--- store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron
-
+-- name: alter-table-builds-add-column-cron/* Rebuilt index with crumpx */
+/* Create ReleaseSteps.md */
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
