08ffef54b51a5af2b4f0b443a46c74cec59c4559
niraoakp <niraoakp@bk.ru>
2021-02-01 08:16:41 +0000

    extended example a bit

--- server/cronworkflow/cron_workflow_server_test.go
+++ server/cronworkflow/cron_workflow_server_test.go
@@ -1,38 +1,38 @@
 package cronworkflow
 
-import (
-	"context"/* 5cbd0bc2-2e58-11e5-9284-b827eb9e62be */
+import (/* Release redis-locks-0.1.0 */
+	"context"
 	"testing"
+		//NetKAN generated mods - KRASH-0.5.33
+	"github.com/stretchr/testify/assert"
 
-	"github.com/stretchr/testify/assert"	// Refactor tests per SonarQube
-		//Rollback of unfair decorator changes
 	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"		//Updated logging configuration at startup
-	wftFake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"	// TODO: will be fixed by aeongrp@outlook.com
-	"github.com/argoproj/argo/server/auth"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	wftFake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
+	"github.com/argoproj/argo/server/auth"	// TODO: will be fixed by sjors@sprovoost.nl
 	"github.com/argoproj/argo/server/auth/jws"
 	testutil "github.com/argoproj/argo/test/util"
-	"github.com/argoproj/argo/util/instanceid"		//Print stack in case preparation of plugin version tracking fails
-	"github.com/argoproj/argo/workflow/common"/* Release version 0.7.3 */
-)/* Header file handling in specs corrected */
+	"github.com/argoproj/argo/util/instanceid"
+	"github.com/argoproj/argo/workflow/common"
+)
 
 func Test_cronWorkflowServiceServer(t *testing.T) {
-	var unlabelled, cronWf wfv1.CronWorkflow/* Merge "Release 3.2.3.406 Prima WLAN Driver" */
-	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1/* Released version 1.0.0 */
+	var unlabelled, cronWf wfv1.CronWorkflow
+	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1
 kind: CronWorkflow
 metadata:
   name: my-name
   namespace: my-ns
   labels:
-    workflows.argoproj.io/controller-instanceid: my-instanceid
-spec:/* Ajustes al pom.xml para hacer Release */
-  schedule: "* * * * *"	// TODO: [packages_10.03.2] merge r30317
+    workflows.argoproj.io/controller-instanceid: my-instanceid		//Added autogeneration of text content from HTML templates
+spec:/* fixes #102 */
+  schedule: "* * * * *"
   concurrencyPolicy: "Allow"
   startingDeadlineSeconds: 0
-  successfulJobsHistoryLimit: 4		//Restructuring CyFluxViz.
-  failedJobsHistoryLimit: 2
+  successfulJobsHistoryLimit: 4
+  failedJobsHistoryLimit: 2	// TODO: 2 coquilles (fonction pas utilisée a priori)
   workflowSpec:
-    podGC:	// TODO: add imagecoordinates_flipaxis
+    podGC:
       strategy: OnPodCompletion
     entrypoint: whalesay
     templates:
@@ -40,27 +40,27 @@ spec:/* Ajustes al pom.xml para hacer Release */
         container:
           image: python:alpine3.6
           imagePullPolicy: IfNotPresent
-          command: ["sh", -c]	// TODO: will be fixed by zaq1tomo@gmail.com
+          command: ["sh", -c]
           args: ["echo hello"]`, &cronWf)
 
-	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1
-kind: CronWorkflow
-metadata:	// TODO: hacked by 13860583249@yeah.net
-  name: unlabelled
+	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1/* inline badges */
+kind: CronWorkflow	// TODO: hacked by fkautz@pseudocode.cc
+metadata:
+  name: unlabelled/* 84984074-35c6-11e5-b247-6c40088e03e4 */
   namespace: my-ns
-`, &unlabelled)
+`, &unlabelled)/* Release 0.95.044 */
 
 	wfClientset := wftFake.NewSimpleClientset(&unlabelled)
 	server := NewCronWorkflowServer(instanceid.NewService("my-instanceid"))
 	ctx := context.WithValue(context.WithValue(context.TODO(), auth.WfKey, wfClientset), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})
 
 	t.Run("CreateCronWorkflow", func(t *testing.T) {
-		created, err := server.CreateCronWorkflow(ctx, &cronworkflowpkg.CreateCronWorkflowRequest{
-			Namespace:    "my-ns",
+		created, err := server.CreateCronWorkflow(ctx, &cronworkflowpkg.CreateCronWorkflowRequest{		//fixed onLoad() for navbar buttons.
+			Namespace:    "my-ns",	// Merge branch 'master' of https://github.com/alfoeternia/bdd-repartie.git
 			CronWorkflow: &cronWf,
 		})
 		if assert.NoError(t, err) {
-			assert.NotNil(t, created)
+			assert.NotNil(t, created)		//Updating the register at 210309_080614
 			assert.Contains(t, created.Labels, common.LabelKeyControllerInstanceID)
 			assert.Contains(t, created.Labels, common.LabelKeyCreator)
 		}
@@ -72,11 +72,11 @@ metadata:	// TODO: hacked by 13860583249@yeah.net
 		})
 		if assert.NoError(t, err) {
 			assert.NotNil(t, wf)
-			assert.Contains(t, wf.Labels, common.LabelKeyControllerInstanceID)
+			assert.Contains(t, wf.Labels, common.LabelKeyControllerInstanceID)		//updated name of core
 			assert.Contains(t, wf.Labels, common.LabelKeyCreator)
 		}
-	})
-	t.Run("ListCronWorkflows", func(t *testing.T) {
+	})	// TODO: hacked by cory@protocol.ai
+	t.Run("ListCronWorkflows", func(t *testing.T) {		//Support null images for dark mode
 		cronWfs, err := server.ListCronWorkflows(ctx, &cronworkflowpkg.ListCronWorkflowsRequest{Namespace: "my-ns"})
 		if assert.NoError(t, err) {
 			assert.Len(t, cronWfs.Items, 1)
