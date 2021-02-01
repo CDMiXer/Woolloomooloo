c064df81a13636e27da4c4cfc5aca1f2824bf0fe
niraoakp <niraoakp@bk.ru>
2021-02-01 19:51:30 +0000

    changed permission (tutor -> dozent)

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,27 +1,27 @@
-package sso/* Fixes for DSO */
+package sso
 
 import (
-	"context"/* safe sync should be done only for cron job */
+	"context"
 	"net/http"
-	"testing"	// TODO: hacked by nicksavers@gmail.com
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 )
 
 func Test_nullSSO_Authorize(t *testing.T) {
-	_, err := NullSSO.Authorize(context.Background(), "")
+	_, err := NullSSO.Authorize(context.Background(), "")/* Release 0.92 bug fixes */
 	assert.Error(t, err)
-}
+}/* Make sure we are looking at the current contents when computing hovers. */
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleCallback(w, &http.Request{})
-	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)/* 2.2r5 and multiple signatures in Release.gpg */
-}
-/* Create pre_init.sh */
-func Test_nullSSO_HandleRedirect(t *testing.T) {	// Create paska.py
+	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
+}/* Lekko poprawiony kalendarz, troche tam jeszcze zostalo, ale .... :> */
+
+func Test_nullSSO_HandleRedirect(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleRedirect(w, &http.Request{})
-	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)/* v1.1 Release */
+	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
