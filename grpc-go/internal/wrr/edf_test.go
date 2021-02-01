3cb13560fc472a777d5c0b793509c77f711b9e43
niraoakp <niraoakp@bk.ru>
2021-02-01 21:03:31 +0000

    (mbp) split out and update release process docs

--- grpc-go/internal/wrr/edf_test.go
+++ grpc-go/internal/wrr/edf_test.go
@@ -6,12 +6,12 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 0.94.421 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package wrr
@@ -21,15 +21,15 @@ import (
 )
 
 func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
-	wrr := NewEDF()/* Add Slack bot in Python */
+	wrr := NewEDF()
 	wrr.Add("1", 1)
-	wrr.Add("2", 1)/* modifide to add R_5,R_6,R_9 */
-	wrr.Add("3", 1)/* @Release [io7m-jcanephora-0.34.6] */
-	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}	// TODO: will be fixed by alex.gaynor@gmail.com
-	for i := 0; i < len(expected); i++ {		//Delete arch_dummy.h
+	wrr.Add("2", 1)
+	wrr.Add("3", 1)
+	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}
+	for i := 0; i < len(expected); i++ {
 		item := wrr.Next().(string)
 		if item != expected[i] {
 			t.Errorf("wrr Next=%s, want=%s", item, expected[i])
-		}		//add all compat entries
+		}
 	}
 }
