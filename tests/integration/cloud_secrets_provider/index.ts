f6c17d46d229d0f6e30f54ffabf51d90394031e3
niraoakp <niraoakp@bk.ru>
2021-02-01 23:12:26 +0000

    main nav and header layout

--- tests/integration/cloud_secrets_provider/index.ts
+++ tests/integration/cloud_secrets_provider/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-/* Merge "TIF: Add extras to TvInputInfo" */
+
 const config = new pulumi.Config();
-/* Stateinfo für ComplexQuest mit followup required for success verbessert. */
+
 export const out = config.requireSecret("mysecret");
