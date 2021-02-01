464ddd22b1262b508fff4a4b944b9b2e2222764e
niraoakp <niraoakp@bk.ru>
2021-02-01 17:56:35 +0000

    Version changed from 1.0-SNAPSHOT to 1.0.

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,26 +1,26 @@
 package stores
-	// TODO: will be fixed by sbrichards@gmail.com
-import (
-	"context"
 
+import (		//use miniwindows in some plugins
+	"context"
+		//added an experimental implementation of a wireframe model.
 	"github.com/filecoin-project/go-state-types/abi"
-		//Update SpeedTestV130.js
-	"github.com/filecoin-project/specs-storage/storage"	// TODO: will be fixed by hi@antfu.me
+
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type Store interface {
-	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)/* Create Fix.txt */
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
-/* Release of eeacms/forests-frontend:2.0-beta.54 */
+
 	// like remove, but doesn't remove the primary sector copy, nor the last
 	// non-primary copy if there no primary copies
-	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error	// TODO: Create CocktailSort.java
+	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
 	// move sectors into storage
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
 
-	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)/* MarkerClusterer Release 1.0.2 */
+	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
 }
