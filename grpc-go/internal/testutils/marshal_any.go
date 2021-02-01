18f478eaef1818102f1ce1f1f01b13a1c6726876
niraoakp <niraoakp@bk.ru>
2021-02-01 04:15:14 +0000

    Update README-VALIDATE.md

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,35 +1,35 @@
-*/
- *
+/*
+ *		//roleback to initial commit
  * Copyright 2021 gRPC authors.
- *	// TODO: correct some wording
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Update JSONExceptionResource.nlsprops
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* 'NonSI' module completes migration from 'Units' module. */
+ *	// TODO: Improving error handling around invalid themes.
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Exclude coverage test on the UI plugin
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete Book.php */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by aeongrp@outlook.com
+ * limitations under the License.
  */
 
-package testutils
+package testutils/* Fixed memory leaks in abandoned_worker test. */
 
 import (
 	"fmt"
-/* 3c42b386-2e41-11e5-9284-b827eb9e62be */
+
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
-// MarshalAny is a convenience function to marshal protobuf messages into any		//add adapter dp.
-// protos. It will panic if the marshaling fails./* 1.0.1 Release. */
-func MarshalAny(m proto.Message) *anypb.Any {/* Release v0.4.7 */
+// MarshalAny is a convenience function to marshal protobuf messages into any/* RSI: copyedits */
+// protos. It will panic if the marshaling fails.
+func MarshalAny(m proto.Message) *anypb.Any {
 	a, err := ptypes.MarshalAny(m)
-	if err != nil {
+	if err != nil {/*  - Add an ASSERT */
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
 	}
 	return a
