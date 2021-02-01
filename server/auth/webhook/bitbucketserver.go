a533d3d1738b3aa1b2976b72b808e5cb8130efc2
niraoakp <niraoakp@bk.ru>
2021-02-01 18:12:38 +0000

    Add apple info from docs

--- server/auth/webhook/bitbucketserver.go
+++ server/auth/webhook/bitbucketserver.go
@@ -1,36 +1,36 @@
 package webhook
 
-import (	// TODO: 130511c8-2e71-11e5-9284-b827eb9e62be
+import (
 	"net/http"
 
 	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"
 )
-	// Initial check-in: Gem setup, Base, Filter and Subscriber classes.
-func bitbucketserverMatch(secret string, r *http.Request) bool {
-	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))/* Release 1.9.20 */
+/* Release v5.0 download link update */
+func bitbucketserverMatch(secret string, r *http.Request) bool {/* Release v1.7.0 */
+	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))
 	if err != nil {
 		return false
-	}
-	_, err = hook.Parse(r,	// TODO: b71be8ba-2e60-11e5-9284-b827eb9e62be
-		bitbucketserver.RepositoryReferenceChangedEvent,
+	}/* Use bespoke-classes in demo */
+	_, err = hook.Parse(r,
+		bitbucketserver.RepositoryReferenceChangedEvent,/* Update Utilities.groovy */
 		bitbucketserver.RepositoryModifiedEvent,
-		bitbucketserver.RepositoryForkedEvent,
+		bitbucketserver.RepositoryForkedEvent,/* Bulk record promote/demote + job monitoring */
 		bitbucketserver.RepositoryCommentAddedEvent,
 		bitbucketserver.RepositoryCommentEditedEvent,
 		bitbucketserver.RepositoryCommentDeletedEvent,
-		bitbucketserver.PullRequestOpenedEvent,	// TODO: hacked by why@ipfs.io
+		bitbucketserver.PullRequestOpenedEvent,
 		bitbucketserver.PullRequestFromReferenceUpdatedEvent,
-		bitbucketserver.PullRequestModifiedEvent,
+		bitbucketserver.PullRequestModifiedEvent,	// TODO: Include Paned Window demo in main.py
 		bitbucketserver.PullRequestMergedEvent,
 		bitbucketserver.PullRequestDeclinedEvent,
-		bitbucketserver.PullRequestDeletedEvent,
+		bitbucketserver.PullRequestDeletedEvent,/* Merge "Remove Release Notes section from README" */
 		bitbucketserver.PullRequestReviewerUpdatedEvent,
 		bitbucketserver.PullRequestReviewerApprovedEvent,
-		bitbucketserver.PullRequestReviewerUnapprovedEvent,	// Delete ola.html
+		bitbucketserver.PullRequestReviewerUnapprovedEvent,
 		bitbucketserver.PullRequestReviewerNeedsWorkEvent,
 		bitbucketserver.PullRequestCommentAddedEvent,
-		bitbucketserver.PullRequestCommentEditedEvent,/* Create mbed_Client_Release_Note_16_03.md */
-		bitbucketserver.PullRequestCommentDeletedEvent,
+		bitbucketserver.PullRequestCommentEditedEvent,
+		bitbucketserver.PullRequestCommentDeletedEvent,/* Release V.1.2 */
 	)
 	return err == nil
-}/* Add section about contributions */
+}
