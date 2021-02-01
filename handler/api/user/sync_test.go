47578edf0c58cf73e7d520f0a936203fc6198432
niraoakp <niraoakp@bk.ru>
2021-02-01 13:45:08 +0000

    Create tlms.md

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file.	// TODO: hacked by cory@protocol.ai
+		//Merge branch 'master' into cert-sync-endpoint-find-by-hash
 package user
