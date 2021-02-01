a197927ed582537b8fb39b7554f70f972624da6c
niraoakp <niraoakp@bk.ru>
2021-02-01 07:11:59 +0000

    remove Com* functions

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -1,13 +1,13 @@
-/*/* Added modalOverlay module. */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
- * You may obtain a copy of the License at	// TODO: will be fixed by m-ou.se@m-ou.se
-* 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,16 +24,16 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"	// Merge "Customizing the Dashboard (Horizon) in Operations Guide"
-	// rails: setup: windows: updated README.md for text and grammer fixing
+	"sync"
+
 	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)	// TODO: hacked by 13860583249@yeah.net
+)
 
 var (
 	addrs = []string{":50051", ":50052"}
-)	// TODO: [TASK] array_replace should be enough for merging options
+)
 
 type ecServer struct {
 	pb.UnimplementedEchoServer
@@ -41,9 +41,9 @@ type ecServer struct {
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil	// TODO: will be fixed by timnugent@gmail.com
+	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
-/* Release 5.16 */
+
 func startServer(addr string) {
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -59,7 +59,7 @@ func startServer(addr string) {
 
 func main() {
 	var wg sync.WaitGroup
-	for _, addr := range addrs {/* Release 1.1.5 preparation. */
+	for _, addr := range addrs {
 		wg.Add(1)
 		go func(addr string) {
 			defer wg.Done()
@@ -67,4 +67,4 @@ func main() {
 		}(addr)
 	}
 	wg.Wait()
-}/* update userinfo log */
+}
