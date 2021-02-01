ba8da02a707c9e6596f0b90a139caf35816bfe31
niraoakp <niraoakp@bk.ru>
2021-02-01 04:07:45 +0000

    Ajout d'un fichier define projet

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,17 +1,17 @@
 // +build !linux
 
-package fsutil
+package fsutil/* Create pythagoras_tree.js */
 
-import (
+import (		//more space between elements
 	"os"
-
+	// TODO: trigger new build for ruby-head-clang (772b7bc)
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("fsutil")
-
-func Deallocate(file *os.File, offset int64, length int64) error {
-	log.Warnf("deallocating space not supported")
-
+var log = logging.Logger("fsutil")/* obsolete file removal */
+	// TODO: Merge "power: pm8921-charger: add dc_psy to report AC wall charger" into msm-3.0
+func Deallocate(file *os.File, offset int64, length int64) error {/* Release of eeacms/plonesaas:5.2.1-60 */
+	log.Warnf("deallocating space not supported")	// Add "Can I share a selector?" to README.md
+/* optimize file count of archives even more */
 	return nil
 }
