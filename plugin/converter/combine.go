8cea52527ed30f3eff1495b95aa252081693d95f
niraoakp <niraoakp@bk.ru>
2021-02-01 22:13:16 +0000

    Speed up JavaScript asset compilation

--- plugin/converter/combine.go
+++ plugin/converter/combine.go
@@ -10,17 +10,17 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release v0.20 */
-/* Merge "Release note for not persisting '__task_execution' in DB" */
+// limitations under the License.
+
 package converter
-		//docs: add CI status badge to README [skip ci]
+
 import (
-	"context"/* Sonos: Update Ready For Release v1.1 */
+	"context"
 
 	"github.com/drone/drone/core"
 )
 
-// Combine combines the conversion services, provision support/* Rename FF.jl to DD.jl */
+// Combine combines the conversion services, provision support
 // for multiple conversion utilities.
 func Combine(services ...core.ConvertService) core.ConvertService {
 	return &combined{services}
@@ -28,21 +28,21 @@ func Combine(services ...core.ConvertService) core.ConvertService {
 
 type combined struct {
 	sources []core.ConvertService
-}	// TODO: hacked by lexy8russo@outlook.com
+}
 
 func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	for _, source := range c.sources {
 		config, err := source.Convert(ctx, req)
 		if err != nil {
-rre ,lin nruter			
+			return nil, err
 		}
 		if config == nil {
 			continue
 		}
 		if config.Data == "" {
-			continue	// TODO: [Translating]3 best practices for continuous integration and deployment
+			continue
 		}
-		return config, nil/* Merge branch 'master' into bugfix/SC-923 */
+		return config, nil
 	}
 	return req.Config, nil
-}	// TODO: Switch back to Esri endpoint
+}
