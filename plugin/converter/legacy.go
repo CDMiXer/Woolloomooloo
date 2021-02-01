46bbeea7e8856f88c5e64a95130ec1137d9f0186
niraoakp <niraoakp@bk.ru>
2021-02-01 01:12:38 +0000

    202676aa-2ece-11e5-905b-74de2bd44bed

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//bugfix td value without defined field
+// that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* internal: fix compiler warning during Release builds. */
 
-package converter/* Add TimeToLiveSet */
-
+package converter
+	// TODO: Edit headings
 import (
 	"context"
-
+/* Test session */
 	"github.com/drone/drone/core"
-)/* Release Notes for v02-00-00 */
-
+)
+		//correct shell
 // Legacy returns a conversion service that converts a
-// legacy 0.8 yaml file to a yaml file.
+// legacy 0.8 yaml file to a yaml file./* ruby 1.9 hash syntax to appease rubocop */
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
 		enabled: enabled,
 	}
 }
 
-type legacyPlugin struct {	// TODO: DB Schema file to make life easier!
-	enabled bool
+type legacyPlugin struct {
+	enabled bool	// TODO: will be fixed by onhardev@bk.ru
 }
-
+/* Working on menu buttons */
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
 	}
-	return &core.Config{
-		Data: req.Config.Data,
-	}, nil
+	return &core.Config{/* Mixin 0.4.1 Release */
+		Data: req.Config.Data,/* trim white space from optimal answer */
+	}, nil	// TODO: QVM compiler improvements
 }
