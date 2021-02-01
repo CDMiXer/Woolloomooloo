3a8086d7190927b24e5b660480f5f0a48a158258
niraoakp <niraoakp@bk.ru>
2021-02-01 10:40:18 +0000

    correct trigger state name

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,21 +1,21 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import binascii	// TODO: will be fixed by fkautz@pseudocode.cc
+import binascii
 import os
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 class RandomResourceProvider(ResourceProvider):
     def create(self, props):
-        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
-        return CreateResult(val, { "val": val })/* Prepare code to use RHist stats provided from server */
-
-class Random(Resource):		//67b69e5e-2e3e-11e5-9284-b827eb9e62be
+        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")/* Release 0.2.0.0 */
+        return CreateResult(val, { "val": val })	// TODO: hacked by nagydani@epointsystem.org
+/* change to searcher.try_next api call. fixes #177 */
+class Random(Resource):
     val: str
     def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
 r = Random("foo")
 
-export("random_id", r.id)
+export("random_id", r.id)		//Merge "Make ipmi_force_boot_device more user friendly"
 export("random_val", r.val)
