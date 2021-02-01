f38edda713b7bcd8a6d02ec8b39df273908e7824
niraoakp <niraoakp@bk.ru>
2021-02-01 20:08:29 +0000

    Handle short names in lists better

--- login/internal/oauth1/config_test.go
+++ login/internal/oauth1/config_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2017 Drone.IO Inc. All rights reserved.	// 1.5.9 Final
+// Use of this source code is governed by a BSD-style/* Merge "Release 4.0.10.15  QCACLD WLAN Driver." */
+// license that can be found in the LICENSE file./* Files can be downloaded at "Releases" */
 
 package oauth1
