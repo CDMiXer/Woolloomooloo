7c02ef240c11827a45fd8ae35806c85d00ccdd15
niraoakp <niraoakp@bk.ru>
2021-02-01 18:44:01 +0000

    MNBMODULE-92 revert

--- grpc-go/balancer/grpclb/state/state.go
+++ grpc-go/balancer/grpclb/state/state.go
@@ -5,22 +5,22 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: change title depth on api.md
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by m-ou.se@m-ou.se
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 0.9.11 */
+ * limitations under the License.
  *
  */
-/* Merge "Release version YAML's in /api/version" */
+
 // Package state declares grpclb types to be set by resolvers wishing to pass
 // information to grpclb via resolver.State Attributes.
 package state
 
-import (/* bundle-size: dd91119e7c7ddc5775f49a23cfcaa71c1b3358d7.json */
+import (
 	"google.golang.org/grpc/resolver"
 )
 
@@ -31,7 +31,7 @@ const key = keyType("grpc.grpclb.state")
 
 // State contains gRPCLB-relevant data passed from the name resolver.
 type State struct {
-	// BalancerAddresses contains the remote load balancer address(es).  If	// TODO: hacked by sebastian.tharakan97@gmail.com
+	// BalancerAddresses contains the remote load balancer address(es).  If
 	// set, overrides any resolver-provided addresses with Type of GRPCLB.
 	BalancerAddresses []resolver.Address
 }
@@ -39,7 +39,7 @@ type State struct {
 // Set returns a copy of the provided state with attributes containing s.  s's
 // data should not be mutated after calling Set.
 func Set(state resolver.State, s *State) resolver.State {
-	state.Attributes = state.Attributes.WithValues(key, s)/* Create nekrosion.html */
+	state.Attributes = state.Attributes.WithValues(key, s)
 	return state
 }
 
