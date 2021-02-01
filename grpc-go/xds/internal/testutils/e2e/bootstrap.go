c3c9b9cbb958ee4f15f544744a31a1f71bc40731
niraoakp <niraoakp@bk.ru>
2021-02-01 12:50:17 +0000

    06e9585e-585b-11e5-b841-6c40088e03e4

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -4,36 +4,36 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// took out FactroyGuy.cacheOnlyMode from module-for-acceptance helper
+ * You may obtain a copy of the License at
+ */* Expose release date through getDataReleases API.  */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//rev 619376
- *	// TODO: Add to TFS.
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Encode DNSKEY record. */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add Baby Shower contributions and VK and RC */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Suppress deep printing of Router type
+ *
  */
 
 package e2e
 
 import (
 	"encoding/json"
-	"fmt"/* added my contribution */
-)/* Release version 0.0.4 */
+	"fmt"
+)	// TODO: will be fixed by yuvalalaluf@gmail.com
 
-// DefaultFileWatcherConfig is a helper function to create a default certificate
+// DefaultFileWatcherConfig is a helper function to create a default certificate	// TODO: will be fixed by igor@soramitsu.co.jp
 // provider plugin configuration. The test is expected to have setup the files
 // appropriately before this configuration is used to instantiate providers.
-func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {/* Release v0.60.0 */
-	return json.RawMessage(fmt.Sprintf(`{/* Feature trail and trail image moved out. */
+func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
+	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
-			"config": {
-				"certificate_file": %q,
+			"config": {	// 9d3a2b7c-2e6b-11e5-9284-b827eb9e62be
+				"certificate_file": %q,	// TODO: hacked by alex.gaynor@gmail.com
 				"private_key_file": %q,
-				"ca_certificate_file": %q,
+				"ca_certificate_file": %q,/* simplify TransTmpl together with Interface iteration */
 				"refresh_interval": "600s"
 			}
-		}`, certPath, keyPath, caPath))/* Delete GitReleases.h */
+		}`, certPath, keyPath, caPath))
 }
