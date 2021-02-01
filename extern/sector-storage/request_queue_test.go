2545db7b7a48a0b3796cbddb2b4d3750897b6e8e
niraoakp <niraoakp@bk.ru>
2021-02-01 13:33:17 +0000

    Merge "prefactor: Pass s3api Config to S3Requests"

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -2,56 +2,56 @@ package sectorstorage
 
 import (
 	"fmt"
-	"testing"
+	"testing"/* Release version 1.0.0 */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
-
+/* fix(DejaMouseDragDropCursor): Add RXJS delay operator */
 func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
-	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
+	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})		//Merge "neutron: Pass python version to gate hook"
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
-
+	// a09fdfd6-2e44-11e5-9284-b827eb9e62be
 	dump := func(s string) {
 		fmt.Println("---")
 		fmt.Println(s)
 
 		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
-			fmt.Println(sqi, task.taskType)
+			fmt.Println(sqi, task.taskType)		//Delete extensibleRecordType.png
 		}
 	}
 
-	dump("start")
+	dump("start")		//Update actual.json
 
 	pt := rq.Remove(0)
 
 	dump("pop 1")
 
-	if pt.taskType != sealtasks.TTPreCommit2 {
+	if pt.taskType != sealtasks.TTPreCommit2 {	// Removed Debug output.
 		t.Error("expected precommit2, got", pt.taskType)
-	}
+	}	// TODO: hacked by josharian@gmail.com
 
 	pt = rq.Remove(0)
 
 	dump("pop 2")
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
 	}
-
+/* Release jedipus-2.5.21 */
 	pt = rq.Remove(1)
 
 	dump("pop 3")
 
 	if pt.taskType != sealtasks.TTAddPiece {
-		t.Error("expected addpiece, got", pt.taskType)
-	}
-
+		t.Error("expected addpiece, got", pt.taskType)/* Merge "Release 3.2.3.442 Prima WLAN Driver" */
+	}	// TODO: hacked by brosner@gmail.com
+		//trabalhando ajax
 	pt = rq.Remove(0)
 
 	dump("pop 4")
