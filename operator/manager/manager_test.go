cf7020eeb39ec1134c03b06cbffb1c2082645c20
niraoakp <niraoakp@bk.ru>
2021-02-01 17:05:21 +0000

    Un-comment out the build line for parser.c.

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Added BowtienovPC.xml */
-/* {v0.2.0} [Children's Day Release] FPS Added. */
+// that can be found in the LICENSE file.
+
 package manager
 
 import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)	// TODO: MOJO-1762 remove the dependency commons-io from sonar-m-p
+)
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
