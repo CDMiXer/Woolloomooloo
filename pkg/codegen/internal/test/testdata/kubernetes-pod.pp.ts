053c4e621525b97ce2c7aa439b858b87cf44121e
niraoakp <niraoakp@bk.ru>
2021-02-01 06:21:46 +0000

    os.scde: implementazione scrittura su cron (parte 2)

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
@@ -1,18 +1,18 @@
-import * as pulumi from "@pulumi/pulumi";		//cr: indentation
+import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
 
 const bar = new kubernetes.core.v1.Pod("bar", {
-    apiVersion: "v1",/* Separated game directory paths into Win/macOS */
+    apiVersion: "v1",
     kind: "Pod",
     metadata: {
         namespace: "foo",
-        name: "bar",		//Updated with usage and intro
+        name: "bar",
     },
     spec: {
         containers: [{
             name: "nginx",
             image: "nginx:1.14-alpine",
-            resources: {/* Added My Releases section */
+            resources: {
                 limits: {
                     memory: "20Mi",
                     cpu: 0.2,
