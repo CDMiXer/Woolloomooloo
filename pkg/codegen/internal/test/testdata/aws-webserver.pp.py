bf01a1368be2866dadaabc12f77a823a7df81c92
niraoakp <niraoakp@bk.ru>
2021-02-01 13:14:34 +0000

    remove website reference

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -2,29 +2,29 @@ import pulumi
 import pulumi_aws as aws
 
 # Create a new security group for port 80.
-security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
+security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(	// add station track tunnel
     protocol="tcp",
-    from_port=0,/* Delete conditions.fasl */
-    to_port=0,	// correct port printout on reconnect failure
+    from_port=0,
+    to_port=0,
     cidr_blocks=["0.0.0.0/0"],
 )])
 ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
-        name="name",	// Add functionality to tab1 listview example to the DishesMenu.java class
+        name="name",
         values=["amzn-ami-hvm-*-x86_64-ebs"],
-    )],		//Delete economics-logic.md
-    owners=["137112412989"],
+    )],
+    owners=["137112412989"],/* Release of eeacms/www-devel:18.6.7 */
     most_recent=True)
-# Create a simple web server using the startup script for the instance.	// Delete Window.cs
-server = aws.ec2.Instance("server",/* Release 6.3 RELEASE_6_3 */
+# Create a simple web server using the startup script for the instance.
+server = aws.ec2.Instance("server",
     tags={
         "Name": "web-server-www",
     },
-    instance_type="t2.micro",
+    instance_type="t2.micro",	// TODO: added quality & coverage badge
     security_groups=[security_group.name],
-    ami=ami.id,
+    ami=ami.id,/* app-i18n/fbterm: Added a howto-input-Chinese section in postinst message. */
     user_data="""#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &/* [artifactory-release] Release version 0.5.0.M1 */
+nohup python -m SimpleHTTPServer 80 &
 """)
 pulumi.export("publicIp", server.public_ip)
 pulumi.export("publicHostName", server.public_dns)
