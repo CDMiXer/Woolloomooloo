9cb36b6f6c65b513c2b1e86f4da1fda90f88919b
niraoakp <niraoakp@bk.ru>
2021-02-01 08:09:33 +0000

    Merge "ARM: dts: msm: Fix M4M frequency list for MSM8996V3"

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,11 +1,11 @@
--- name: create-table-org-secrets
+-- name: create-table-org-secrets/* Preparing WIP-Release v0.1.26-alpha-build-00 */
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
-,secret_namespace         VARCHAR(50)
+,secret_namespace         VARCHAR(50)	// open libssh2_trace feature. clean code with pretty code style.
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BYTEA
+,secret_data              BYTEA/* Merge "Revert "Release notes: Get back lost history"" */
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
