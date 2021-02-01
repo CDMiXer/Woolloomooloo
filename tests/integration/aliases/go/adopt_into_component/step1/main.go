7c924640f6ee7feea6f8f4f2bd025a7e56970e7d
niraoakp <niraoakp@bk.ru>
2021-02-01 22:27:28 +0000

    update readme info on variables use

--- tests/integration/aliases/go/adopt_into_component/step1/main.go
+++ tests/integration/aliases/go/adopt_into_component/step1/main.go
@@ -8,18 +8,18 @@ import (
 
 // FooComponent is a component resource
 type FooResource struct {
-	pulumi.ResourceState
+	pulumi.ResourceState	// Delete gushi.jpg
 }
-
+/* Release 1.119 */
 type FooComponent struct {
 	pulumi.ResourceState
-}
+}	// TODO: c863ee96-2e66-11e5-9284-b827eb9e62be
 
-type FooComponent2 struct {
+type FooComponent2 struct {	// TODO: hacked by alex.gaynor@gmail.com
 	pulumi.ResourceState
 }
 
-type FooComponent3 struct {
+type FooComponent3 struct {		//803a774e-2e56-11e5-9284-b827eb9e62be
 	pulumi.ResourceState
 }
 
@@ -31,20 +31,20 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err
+		return nil, err/* Release 0.14.2 (#793) */
 	}
 	return fooRes, nil
-}
+}/* Release of eeacms/www:21.4.18 */
 
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
+	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)	// TODO: Specify algorithm for encoding and decoding
 	if err != nil {
 		return nil, err
 	}
-	return fooComp, nil
+	return fooComp, nil	// TODO: [src/set_ld.c] Added a note about the test for infinity.
 }
-
+	// mikroserisy
 func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent2, error) {
 	fooComp := &FooComponent2{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)
@@ -53,15 +53,15 @@ func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceO
 	}
 	return fooComp, nil
 }
-
+		//Workflow: allow saving and restoring with an empty queue
 func NewFooComponent3(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent3, error) {
 	fooComp := &FooComponent3{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opts...)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: hacked by ng8eke@163.com
 	}
-	_, err = NewFooComponent2(ctx, name+"-child", opts...)
-	if err != nil {
+	_, err = NewFooComponent2(ctx, name+"-child", opts...)/* interrupted in the middle of doing things, NFC whats broken/not */
+	if err != nil {	// TODO: TEIID-2982 expanding the docs around the model visiblity override
 		return nil, err
 	}
 	return fooComp, nil
