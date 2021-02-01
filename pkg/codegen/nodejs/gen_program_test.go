3eae073792e9921b58e01338023ff1319de4be7d
niraoakp <niraoakp@bk.ru>
2021-02-01 23:29:25 +0000

    Numbered specs in sprintf failed if the number ended in zero. (PR#14975)

--- pkg/codegen/nodejs/gen_program_test.go
+++ pkg/codegen/nodejs/gen_program_test.go
@@ -1,7 +1,7 @@
 package nodejs
-/* Release v0.8.0 */
+
 import (
-	"bytes"		//added reactive mongo
+	"bytes"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
@@ -12,7 +12,7 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* SE: update skins */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
@@ -22,16 +22,16 @@ func TestGenProgram(t *testing.T) {
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
-		//move layouts and partials into src
+
 	for _, f := range files {
-		if filepath.Ext(f.Name()) != ".pp" {	// TODO: [FIX] remove a line let the line invoiceable;
+		if filepath.Ext(f.Name()) != ".pp" {
 			continue
-		}/* Released V0.8.60. */
+		}
 
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
-		}	// TODO: hacked by peterke@gmail.com
+		}
 
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
@@ -39,14 +39,14 @@ func TestGenProgram(t *testing.T) {
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			expected, err := ioutil.ReadFile(path + ".ts")	// TODO: Automatic changelog generation #2141 [ci skip]
+			expected, err := ioutil.ReadFile(path + ".ts")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".ts", err)
 			}
 
 			parser := syntax.NewParser()
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())/* Released version 0.0.2 */
-			if err != nil {/* new robot sfx  */
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
+			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
@@ -56,23 +56,23 @@ func TestGenProgram(t *testing.T) {
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
-			}		//Created new page_url tag.
+			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
-			files, diags, err := GenerateProgram(program)	// Delete diagrama.png
+			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
-			if expectNYIDiags {/* Create 677-Map-Sum-Pairs.py */
+			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
 				for _, d := range diags {
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
-					}/* was/client: use ReleaseControl() in ResponseEof() */
+					}
 				}
 				diags = tmpDiags
 			}
-			if diags.HasErrors() {	// closes #412
+			if diags.HasErrors() {
 				t.Fatalf("failed to generate program: %v", diags)
 			}
 			assert.Equal(t, string(expected), string(files["index.ts"]))
