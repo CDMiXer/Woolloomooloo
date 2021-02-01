30c82434b00b4b527bf1aa5414f9e368d1ee2822
niraoakp <niraoakp@bk.ru>
2021-02-01 12:38:11 +0000

    Attempt to please the Wiki gods

--- core/hook_test.go
+++ core/hook_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Fix "Bobbing for Apples" achievement test */
-/* Add Release files. */
-// +build !oss	// TODO: hacked by why@ipfs.io
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Delete _smartme.apk
+// Use of this source code is governed by the Drone Non-Commercial License		//acciones semanticas para variables y punteros y conjutos mojamutos
+// that can be found in the LICENSE file.
+
+// +build !oss/* Tagging a Release Candidate - v4.0.0-rc17. */
 
 package core
