344aa06b87140c18eadfb0d51693780a4a0f31d4
niraoakp <niraoakp@bk.ru>
2021-02-01 07:09:45 +0000

    #1652 useful toString for KotlinPropertyArguments

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";/* added HTTPS proxy support */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-
-const logs = new aws.s3.Bucket("logs", {});		//Chore: update readme.md file for singlerestaurantcontainer
-const bucket = new aws.s3.Bucket("bucket", {loggings: [{
+		//few MySQL Optimizations to char-server
+const logs = new aws.s3.Bucket("logs", {});
+const bucket = new aws.s3.Bucket("bucket", {loggings: [{/* Added warning about the tests' effect on rabbitmq */
     targetBucket: logs.bucket,
-}]});/* Getter for widget queue */
-export const targetBucket = bucket.loggings[0].targetBucket;		//Fix lint issue
+}]});/* Merge "Node group handling improved in the db module" */
+export const targetBucket = bucket.loggings[0].targetBucket;
