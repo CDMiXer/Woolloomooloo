d5793e3e0c6fc7a6e5ecfd150b3fe95c910aa763
niraoakp <niraoakp@bk.ru>
2021-02-01 17:00:55 +0000

    Automatic changelog generation for PR #32734 [ci skip]

--- handler/api/ccmenu/ccmenu.go
+++ handler/api/ccmenu/ccmenu.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'flyway' into master */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,8 +6,8 @@
 
 package ccmenu
 
-import (/* add some markdown */
-	"encoding/xml"	// TODO: Updated the pycamhd feedstock.
+import (
+	"encoding/xml"
 	"fmt"
 	"net/http"
 
@@ -15,14 +15,14 @@ import (/* add some markdown */
 
 	"github.com/go-chi/chi"
 )
-	// Improved DB access class
+
 // Handler returns an http.HandlerFunc that writes an svg status
 // badge to the response.
 func Handler(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	link string,
-) http.HandlerFunc {	// TODO: removed fader.ncb
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
@@ -39,9 +39,9 @@ func Handler(
 			return
 		}
 
-		project := New(repo, build,		//DefaultDisplayServerTestFixture clears clients after each test
-			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),		//added translateBrowsePathsToNodeIds to external NodeStore functions
-)		
+		project := New(repo, build,
+			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),
+		)
 
 		xml.NewEncoder(w).Encode(project)
 	}
