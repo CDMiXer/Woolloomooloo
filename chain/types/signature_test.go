13f129c7bd871f46927f73e1e4cb3d91e25666ae
niraoakp <niraoakp@bk.ru>
2021-02-01 19:03:40 +0000

    Improve client progress patch

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -3,10 +3,10 @@ package types
 import (
 	"bytes"
 	"testing"
-		//Add new test.
+
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-/* sanatized string */
+
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
@@ -16,14 +16,14 @@ func TestSignatureSerializeRoundTrip(t *testing.T) {
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}/* Updated Release_notes.txt for 0.6.3.1 */
-	// TODO: New version of Ridizain - 1.0.27
+	}
+
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
 	}
 
-	if !outs.Equals(s) {/* Added counter project */
-		t.Fatal("serialization round trip failed")/* Update PrepareReleaseTask.md */
+	if !outs.Equals(s) {
+		t.Fatal("serialization round trip failed")
 	}
-}		//favicon file for iLamp.php (copy to /img)
+}
