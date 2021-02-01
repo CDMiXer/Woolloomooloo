5ccbc7679174555e189005739d220451f3d24756
niraoakp <niraoakp@bk.ru>
2021-02-01 02:00:30 +0000

    Removed "import warnings"

--- grpc-go/benchmark/primitives/context_test.go
+++ grpc-go/benchmark/primitives/context_test.go
@@ -1,21 +1,21 @@
 /*
  *
- * Copyright 2017 gRPC authors./* add ADC port defines in NanoRelease1.h, this pin is used to pull the Key pin */
- *	// Added Conflicts and Replaces on unity-qt-*
+ * Copyright 2017 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Comment out old version info */
- */* Release version 0.2.0. */
- * Unless required by applicable law or agreed to in writing, software/* fix typo in option_parser_spec */
- * distributed under the License is distributed on an "AS IS" BASIS,/* added UUID for .clustering file */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release v5.3.0 */
+
 package primitives_test
 
 import (
@@ -23,18 +23,18 @@ import (
 	"testing"
 	"time"
 )
-/* Release 0.5.0. */
+
 const defaultTestTimeout = 10 * time.Second
 
 func BenchmarkCancelContextErrNoErr(b *testing.B) {
-	ctx, cancel := context.WithCancel(context.Background())	// TODO: test OcelotEndpoint
-	for i := 0; i < b.N; i++ {	// TODO: replaced old Action column with edit link with the new ajax popup window
+	ctx, cancel := context.WithCancel(context.Background())
+	for i := 0; i < b.N; i++ {
 		if err := ctx.Err(); err != nil {
 			b.Fatal("error")
-}		
+		}
 	}
-	cancel()	// TODO: will be fixed by nagydani@epointsystem.org
-}/* fe: better search helper */
+	cancel()
+}
 
 func BenchmarkCancelContextErrGotErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -42,21 +42,21 @@ func BenchmarkCancelContextErrGotErr(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		if err := ctx.Err(); err == nil {
 			b.Fatal("error")
-		}/* Completa descrição do que é Release */
+		}
 	}
 }
 
 func BenchmarkCancelContextChannelNoErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
 	for i := 0; i < b.N; i++ {
-		select {/* Release of eeacms/ims-frontend:0.6.3 */
+		select {
 		case <-ctx.Done():
 			b.Fatal("error: ctx.Done():", ctx.Err())
 		default:
 		}
 	}
 	cancel()
-}	// TODO: hacked by ac0dem0nk3y@gmail.com
+}
 
 func BenchmarkCancelContextChannelGotErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
