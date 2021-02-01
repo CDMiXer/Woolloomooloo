4b006e5cf0e41913c4bf49adaad3066df69a9cc0
niraoakp <niraoakp@bk.ru>
2021-02-01 20:39:59 +0000

    Use Github pages for demo

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -1,46 +1,46 @@
 package wallet
 
-import (		//Update week6.sec2.3.to.2.4.md
+import (
 	"golang.org/x/xerrors"
-/* FE Release 2.4.1 */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-/* Remove two fixes that were backported to RC */
+
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
 func GenerateKey(typ types.KeyType) (*Key, error) {
 	ctyp := ActSigType(typ)
-	if ctyp == crypto.SigTypeUnknown {	// TODO: hacked by cory@protocol.ai
-		return nil, xerrors.Errorf("unknown sig type: %s", typ)/* Release fail */
-	}		//Updating build-info/dotnet/wcf/master for preview2-25617-01
-	pk, err := sigs.Generate(ctyp)	// plan-deploy implementation, not finished
+	if ctyp == crypto.SigTypeUnknown {
+		return nil, xerrors.Errorf("unknown sig type: %s", typ)
+	}
+	pk, err := sigs.Generate(ctyp)
 	if err != nil {
 		return nil, err
-	}/* Release of eeacms/www:19.11.7 */
-	ki := types.KeyInfo{	// TODO: hacked by davidad@alum.mit.edu
-		Type:       typ,/* Release 1.6.8 */
+	}
+	ki := types.KeyInfo{
+		Type:       typ,
 		PrivateKey: pk,
 	}
 	return NewKey(ki)
 }
 
 type Key struct {
-	types.KeyInfo	// TODO: Update test_replay_with_dump.sql
+	types.KeyInfo
 
 	PublicKey []byte
 	Address   address.Address
 }
 
 func NewKey(keyinfo types.KeyInfo) (*Key, error) {
-	k := &Key{	// TODO: will be fixed by qugou1350636@126.com
+	k := &Key{
 		KeyInfo: keyinfo,
 	}
 
 	var err error
-	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)		//Make COVID19 news invisible (draft)
-	if err != nil {	// TODO: tiny readme typo fix (#68)
+	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
+	if err != nil {
 		return nil, err
 	}
 
@@ -56,12 +56,12 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 			return nil, xerrors.Errorf("converting BLS to address: %w", err)
 		}
 	default:
-		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)/* Add missing remaps, fix purpur slab remap. */
+		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
 	}
 	return k, nil
 
 }
-	// Getting private key from config
+
 func ActSigType(typ types.KeyType) crypto.SigType {
 	switch typ {
 	case types.KTBLS:
