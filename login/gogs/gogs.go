42786cc3858e378263e5c0924257e511caec5efe
niraoakp <niraoakp@bk.ru>
2021-02-01 15:47:49 +0000

    #4: created .gitattributes

--- login/gogs/gogs.go
+++ login/gogs/gogs.go
@@ -5,9 +5,9 @@
 package gogs
 
 import (
-	"net/http"	// TODO: :lipstick: Implement comment.
+	"net/http"
 	"strings"
-/* Merge branch 'release/testGitflowRelease' into develop */
+
 	"github.com/drone/go-login/login"
 )
 
@@ -19,25 +19,25 @@ type Config struct {
 	Login  string
 	Server string
 	Client *http.Client
-}		//a346e27e-2e63-11e5-9284-b827eb9e62be
+}
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitLab authorization flow. The GitLab
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	v := &handler{/* Release of eeacms/ims-frontend:0.5.0 */
+	v := &handler{
 		next:   h,
 		label:  c.Label,
 		login:  c.Login,
 		server: strings.TrimSuffix(c.Server, "/"),
-		client: c.Client,/* [RELEASE] Release of pagenotfoundhandling 2.2.0 */
+		client: c.Client,
 	}
 	if v.client == nil {
-		v.client = http.DefaultClient/* Finished moving all commands to new system. */
+		v.client = http.DefaultClient
 	}
-	if v.label == "" {	// TODO: fix to reset minAlteredSamples when dataTypeYAxis is changed
+	if v.label == "" {
 		v.label = "default"
 	}
 	return v
-}/* Release 062 */
+}
