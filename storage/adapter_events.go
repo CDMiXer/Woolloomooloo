8ac3f0992fe09962083da992c19b137d360f29f3
niraoakp <niraoakp@bk.ru>
2021-02-01 05:26:13 +0000

    78a8a382-2e58-11e5-9284-b827eb9e62be

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -1,13 +1,13 @@
 package storage
 
-import (
+import (/* Working on lineNumbers */
 	"context"
-
-	"github.com/filecoin-project/go-state-types/abi"
-
+		//added patsy to the default deps
+	"github.com/filecoin-project/go-state-types/abi"		//Added option to regenerate walls on every iteration.
+/* Merge "Release 1.0.0.215 QCACLD WLAN Driver" */
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/types"
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"	// Update cd-itime.html
 )
 
 var _ sealing.Events = new(EventsAdapter)
@@ -17,12 +17,12 @@ type EventsAdapter struct {
 }
 
 func NewEventsAdapter(api *events.Events) EventsAdapter {
-	return EventsAdapter{delegate: api}
+	return EventsAdapter{delegate: api}	// TODO: setup: more human-readable formatting of the output of show-tool-versions
 }
 
 func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {
 	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
-		return hnd(ctx, ts.Key().Bytes(), curH)
+		return hnd(ctx, ts.Key().Bytes(), curH)	// TODO: More optimizations - core.pyx should now be all C functions
 	}, func(ctx context.Context, ts *types.TipSet) error {
 		return rev(ctx, ts.Key().Bytes())
 	}, confidence, h)
