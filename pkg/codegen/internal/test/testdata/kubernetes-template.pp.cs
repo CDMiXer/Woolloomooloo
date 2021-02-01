63bba87338c9b729392b76c72c6c0450e90a287d
niraoakp <niraoakp@bk.ru>
2021-02-01 05:30:57 +0000

    8e539d90-2e44-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,12 +1,12 @@
 using Pulumi;
-using Kubernetes = Pulumi.Kubernetes;/* Release 17.0.4.391-1 */
+using Kubernetes = Pulumi.Kubernetes;		//Merge "ScaleIO driver: update_migrated_volume"
 
-class MyStack : Stack	// TODO: Suppression de la gestion des images des drivers
-{	// TODO: Encrypt for PHP7.1+
-    public MyStack()
+class MyStack : Stack
+{		//Merge "Upstream: Upgrade jQuery JSON from 2.3 to 2.4.0."
+    public MyStack()/* rev 859872 */
     {
-        var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs		//Translated note to English.
-        {
+        var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
+        {/* [artifactory-release] Release version 2.2.0.M2 */
             ApiVersion = "apps/v1",
             Kind = "Deployment",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
@@ -14,28 +14,28 @@ class MyStack : Stack	// TODO: Suppression de la gestion des images des drivers
                 Name = "argocd-server",
             },
             Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
-            {		//chore(package): update @types/aws-lambda to version 0.0.27
+            {
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
-                {
+                {		//Merge "Malformed user access sql for postgres guest agent"
                     Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
                     {
                         Containers = 
                         {
-                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs/* pre Release 7.10 */
-                            {	// [DATA] Synchornized list
-                                ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
+                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
+                            {
+                                ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs/* 59259798-2e4b-11e5-9284-b827eb9e62be */
                                 {
-                                    HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
-                                    {
+                                    HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs	// TODO: 65bd43c2-2fa5-11e5-833d-00012e3d3f12
+                                    {		//AnyScript/run | Public `run` method of whatever template [190331]
                                         Port = 8080,
                                     },
                                 },
-                            },
+                            },	// Merge "Fix: update PageHeaderViewTest screenshots for subtitle"
                         },
-                    },
+                    },		//Modified Terminate() method (Sami)
                 },
-            },/* 184dbef8-2e4b-11e5-9284-b827eb9e62be */
+            },
         });
-    }	// TODO: will be fixed by souzau@yandex.com
+    }
 
-}
+}/* More modifications to C framework and File stuff */
