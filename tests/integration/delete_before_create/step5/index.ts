b5fbb159665bbb7aab614bedf2ab749e5dcfef0d
niraoakp <niraoakp@bk.ru>
2021-02-01 21:58:59 +0000

    Add optional peer dependency on yeoman-environment

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -3,16 +3,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// The DBR deletion of A triggers the deletion of C due to dependency./* turn confgen text files into i18n_variables */
+// The DBR deletion of A triggers the deletion of C due to dependency.
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent	// TODO: Fixed bug where data_dir was wrongly assigned
+//   1. DeleteReplacement Dependent
 //   2. DeleteReplacement Base
 //   3. Replace Base
 //   4. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
 
 //   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
-//   5. DeleteReplacement Base-2	// TODO: 8a4f3e10-2e5e-11e5-9284-b827eb9e62be
+//   5. DeleteReplacement Base-2
 //   6. Replace Base-2
 //   7. CreateReplacement Base-2
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
