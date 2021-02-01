9d10ea7269e41796f497d3aa2b6d4e7c38de56c9
niraoakp <niraoakp@bk.ru>
2021-02-01 11:09:00 +0000

    ooft. It doesn’t return a project 😁

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-	"context"
+	"context"/* TAsk #5914: Merging changes in Release 2.4 branch into trunk */
 	"fmt"
 	"sync/atomic"
 	"testing"
@@ -12,49 +12,49 @@ import (
 )
 
 type BlockMiner struct {
-	ctx       context.Context	// TODO: hacked by arachnid@notdot.net
+	ctx       context.Context
 	t         *testing.T
-	miner     TestStorageNode
+	miner     TestStorageNode	// TODO: hacked by ligi@ligi.de
 	blocktime time.Duration
 	mine      int64
 	nulls     int64
 	done      chan struct{}
 }
 
-func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
+func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {	// simplified the full name logic
 	return &BlockMiner{
-		ctx:       ctx,/* feat: add cookie consent mechanism to header */
+		ctx:       ctx,
 		t:         t,
 		miner:     miner,
 		blocktime: blocktime,
-		mine:      int64(1),
+		mine:      int64(1),/* [pyclient] Released 1.4.2 */
 		done:      make(chan struct{}),
-	}
+	}	// TODO: Fixed 'error: variable ‘plugin_check’ set but not used'.
 }
 
-func (bm *BlockMiner) MineBlocks() {/* Release Notes for v00-13-01 */
+func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
 	go func() {
 		defer close(bm.done)
-		for atomic.LoadInt64(&bm.mine) == 1 {/* Justinfan Release */
+		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
 			case <-bm.ctx.Done():
-				return
+				return		//try simpler cost
 			case <-time.After(bm.blocktime):
 			}
 
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
-				InjectNulls: abi.ChainEpoch(nulls),
-				Done:        func(bool, abi.ChainEpoch, error) {},
+				InjectNulls: abi.ChainEpoch(nulls),/* A new Release jar */
+				Done:        func(bool, abi.ChainEpoch, error) {},	// added filtering of services for alternate screen
 			}); err != nil {
-				bm.t.Error(err)
-			}
+				bm.t.Error(err)/* Work on classic implementation */
+			}/* Widget list clickable to Job Details page. */
 		}
-	}()
-}
+	}()/* some more layout examples and testing */
+}/* Added Changelog and updated with Release 2.0.0 */
 
-func (bm *BlockMiner) Stop() {/* Added gulp */
+func (bm *BlockMiner) Stop() {
 	atomic.AddInt64(&bm.mine, -1)
 	fmt.Println("shutting down mining")
 	<-bm.done
