3eec42e4a56dc18a76c95e561821c03571485f63
niraoakp <niraoakp@bk.ru>
2021-02-01 03:31:50 +0000

    3a100b02-2e66-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -1,39 +1,39 @@
 /*
- *		//Update flask-cors from 2.1.2 to 3.0.4
- * Copyright 2018 gRPC authors.		//Delete photo5.PNG
+ *
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "Release 1.0.0.152 QCACLD WLAN Driver" */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create count-the-repetitions.cpp */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Update and rename Finalproject.md to final-project.md
+ * limitations under the License.
  *
  */
 
 // Binary server is an example server.
-package main	// Create 104.c
-/* Stopped automatic Releases Saturdays until release. Going to reacvtivate later. */
+package main
+
 import (
 	"context"
 	"flag"
 	"fmt"
 	"log"
-	"net"/* Change suggestion wording */
-/* Changed default parameters for Karpov's algorithm. */
+	"net"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")/* calctabcolor: added <limits> header. */
-	// Add new citations
+var port = flag.Int("port", 50051, "the port to serve on")
+
 type ecServer struct {
 	pb.UnimplementedEchoServer
 }
@@ -41,17 +41,17 @@ type ecServer struct {
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
-/* 5.5.1 Release */
-func main() {/* Add support for create download pages. Release 0.2.0. */
+
+func main() {
 	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {/* Merge "Release 1.0.0.178 QCACLD WLAN Driver." */
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	// Create alts based credential.
 	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
-/* Load choosen Auv from wizard. */
+
 	s := grpc.NewServer(grpc.Creds(altsTC))
 
 	// Register EchoServer on the server.
