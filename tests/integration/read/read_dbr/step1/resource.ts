71784763eb9559664504e798eb4902630a468002
niraoakp <niraoakp@bk.ru>
2021-02-01 00:39:05 +0000

    item type enumerator includes NOTE

--- tests/integration/read/read_dbr/step1/resource.ts
+++ tests/integration/read/read_dbr/step1/resource.ts
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by greg@colvin.org
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,20 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as pulumi from "@pulumi/pulumi";		//Fixed ListField in uniforms-semantic.
-import * as dynamic from "@pulumi/pulumi/dynamic";	// TODO: will be fixed by hugomrdias@gmail.com
-	// add readme for introduction
+import * as pulumi from "@pulumi/pulumi";
+import * as dynamic from "@pulumi/pulumi/dynamic";
+
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();	// BcnDU3DOTJ3bwuYSWCyEcHpYwAb2DxnG
+    public static readonly instance = new Provider();
 
     private id: number = 0;
 
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {		//outside padding fix
-        return {	// horizontal divider
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
+        return {
             inputs: news,
         }
     }
-/* Extend installation options to partially cover #4 */
+
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
@@ -38,25 +38,25 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             changes: false,
         }
-}    
+    }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {/* Release version [10.7.1] - prepare */
-            id: (this.id++).toString(),/* Released springjdbcdao version 1.8.9 */
+        return {
+            id: (this.id++).toString(),
             outs: inputs,
-        }/* Delete rd.svg */
+        }
     }
-	// Revert enabling benchmark
+
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
-    }/* Create setup_servers.md */
+    }
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
-            id: id,/* Remove unused WorksheetRESTView and WorksheetsRESTView.add_worksheet. */
+            id: id,
             props: props,
         }
-    }		//Create inalco.txt
+    }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
