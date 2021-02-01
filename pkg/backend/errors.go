85499b504ea40342fe5ae5f63cbc82637f7a34bb
niraoakp <niraoakp@bk.ru>
2021-02-01 14:36:20 +0000

    Create MDFConversion.cpp

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
-package backend/* bc92485c-2e60-11e5-9284-b827eb9e62be */
-/* Remove Windows specific mutex operations. */
+package backend
+
 import (
 	"fmt"
 )
 
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.	// TODO: Create MainGUI.java
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
-}
+}/* Release JettyBoot-0.3.4 */
 
 func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
-		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())/* Added middle click to remove tabs */
-}
+		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
+}/* Release 1.1.0-CI00240 */
