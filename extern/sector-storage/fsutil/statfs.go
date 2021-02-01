07721f98dc3e403fe1565ef4da84f19094897324
niraoakp <niraoakp@bk.ru>
2021-02-01 02:09:54 +0000

    update generator instructions

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
-	// TODO: hacked by xiemengjun@gmail.com
+
 type FsStat struct {
-	Capacity    int64		//Added new_pod_repository for PodToBUILD
+	Capacity    int64
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem	// Add configuration for Clock. "java" cron does not work for now
+	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-	// TODO: 0.4 is out.
-	// non-zero when storage has configured MaxStorage	// shm/shm: add method CalcMemoryMapSize()
+
+	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
 }
