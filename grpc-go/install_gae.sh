ef1c7e418bd3f60ceffcc09746ebd7234487be02
niraoakp <niraoakp@bk.ru>
2021-02-01 00:19:47 +0000

    add aticle apache mysql nginx set up in OS X 10.10

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash	// TODO: more principles
 
 TMP=$(mktemp -d /tmp/sdk.XXX) \
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
 && unzip -q $TMP.zip -d $TMP \
-&& export PATH="$PATH:$TMP/go_appengine"/* Merge branch 'master' into fix-afgiftenummer-test */
+&& export PATH="$PATH:$TMP/go_appengine"
