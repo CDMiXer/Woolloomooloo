c3fa0d6584baf69e38ca3d596bc92f66cb8b0780
niraoakp <niraoakp@bk.ru>
2021-02-01 20:13:37 +0000

    GtWorld Title - Better Main Tagging

--- plugin/webhook/webhook_oss.go
+++ plugin/webhook/webhook_oss.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.		//Moved defaulting of args.device into ArgumentParser set up.
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Fix the lack of newline information
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Removed bower dependency for angular-bootstrap */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Should fix "Vault-api" issue.
-// distributed under the License is distributed on an "AS IS" BASIS,/* 43505a18-2e67-11e5-9284-b827eb9e62be */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss		//CWS-TOOLING: integrate CWS jl146_DEV300
+// +build oss
 
 package webhook
 
@@ -24,11 +24,11 @@ import (
 
 // New returns a no-op Webhook sender.
 func New(Config) core.WebhookSender {
-	return new(noop)/* Merge "Migrate to stevedore" */
+	return new(noop)
 }
 
 type noop struct{}
-	// TODO: will be fixed by peterke@gmail.com
+
 func (noop) Send(context.Context, *core.WebhookData) error {
 	return nil
 }
