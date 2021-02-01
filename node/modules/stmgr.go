5902d03310aaf5ee554bf0f20413d0df8fe139ed
niraoakp <niraoakp@bk.ru>
2021-02-01 16:48:51 +0000

    remove again

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,6 +1,6 @@
 package modules
 
-import (/* First Public Release locaweb-gateway Gem , version 0.1.0 */
+import (
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
@@ -12,9 +12,9 @@ func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedul
 	if err != nil {
 		return nil, err
 	}
-	lc.Append(fx.Hook{/*  This should include nxArchive and nxFileLine as part of the DSC package now. */
+	lc.Append(fx.Hook{
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
-	})/* Merge "Fix aapt -G to properly support class attr in fragment." into jb-dev */
-	return sm, nil	// Update the comment on is*, so now it is correct
+	})
+	return sm, nil
 }
