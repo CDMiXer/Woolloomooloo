98bd92a1b43d2743510afd7003a40024f6523aff
niraoakp <niraoakp@bk.ru>
2021-02-01 02:50:34 +0000

    926a51da-2e67-11e5-9284-b827eb9e62be

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Add install routine for 3.0.0 - clear agenda widget cache. */
-///* added series data info to episodes for poster and medium fanart */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Fix triples
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* refactor/test - renamed classes and added javasimon tests */
-// Unless required by applicable law or agreed to in writing, software/* Merge "Remove RPC to plugin when dhcp sets default route" */
-// distributed under the License is distributed on an "AS IS" BASIS,/* audiobookbay: add unblockit proxy */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Fix toolbar updating. */
+// limitations under the License.
 
-package runner/* Merge "Release 3.0.10.009 Prima WLAN Driver" */
+package runner
