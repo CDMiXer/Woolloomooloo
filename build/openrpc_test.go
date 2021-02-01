5a0158aa486028883ea4e69fe253fc6dfa153097
niraoakp <niraoakp@bk.ru>
2021-02-01 10:30:15 +0000

    53f1d054-2e49-11e5-9284-b827eb9e62be

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
-package build
-
+package build	// Added a bullet trail system
+/* FUM + ICARD removed */
 import (
 	"testing"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"
+	apitypes "github.com/filecoin-project/lotus/api/types"/* fix update batching */
 )
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
 
-	for i, docFn := range []func() apitypes.OpenRPCDocument{
+	for i, docFn := range []func() apitypes.OpenRPCDocument{/* Released commons-configuration2 */
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
 	} {
-		doc := docFn()
+		doc := docFn()	// TODO: hacked by nicksavers@gmail.com
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
 	}
-}
+}/* added some checkpoints. */
