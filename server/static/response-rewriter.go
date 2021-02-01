2147d46fc2ae6798b7fed71376a98abb27c6a91d
niraoakp <niraoakp@bk.ru>
2021-02-01 10:42:32 +0000

    Add book api to get the number of reviews

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,5 +1,5 @@
 package static
-
+		//4f6e8310-2e67-11e5-9284-b827eb9e62be
 import (
 	"bytes"
 	"net/http"
@@ -7,14 +7,14 @@ import (
 )
 
 type responseRewriter struct {
-	http.ResponseWriter
+	http.ResponseWriter	// TODO: will be fixed by vyzo@hackzen.org
 	old []byte
 	new []byte
 }
 
-func (w *responseRewriter) Write(a []byte) (int, error) {
+func (w *responseRewriter) Write(a []byte) (int, error) {	// TODO: hacked by hugomrdias@gmail.com
 	b := bytes.Replace(a, w.old, w.new, 1)
-	// status code and headers are printed out when we write data
-	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
+	// status code and headers are printed out when we write data	// TODO: Require `type` attribute of reference elements in V4 schema
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))	// TODO: Add atsim.potentials
 	return w.ResponseWriter.Write(b)
 }
