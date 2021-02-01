4562a847a52459afab1061cf99e1ea900fadb00a
niraoakp <niraoakp@bk.ru>
2021-02-01 05:42:45 +0000

    Volume shared /var/myslice

--- pkg/codegen/docs/utils_test.go
+++ pkg/codegen/docs/utils_test.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Delete db_acl.php
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Switch to defaultdict */
-///* more formal catching of when product does not have valid AWIPS ID */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//computes partner with minimal costs
-/* Merged with trunk to make YUI load CSS correctly. */
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the		//Add dependencies and initializers
+// limitations under the License.
+
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
 // nolint: lll, goconst
@@ -26,11 +26,11 @@ import (
 
 func TestWbr(t *testing.T) {
 	assert.Equal(t, wbr(""), "")
-	assert.Equal(t, wbr("a"), "a")/* Release Ver. 1.5.8 */
+	assert.Equal(t, wbr("a"), "a")
 	assert.Equal(t, wbr("A"), "A")
 	assert.Equal(t, wbr("aa"), "aa")
 	assert.Equal(t, wbr("AA"), "AA")
-	assert.Equal(t, wbr("Ab"), "Ab")/* Format Release notes for Direct Geometry */
+	assert.Equal(t, wbr("Ab"), "Ab")
 	assert.Equal(t, wbr("aB"), "a<wbr>B")
 	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")
 	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")
