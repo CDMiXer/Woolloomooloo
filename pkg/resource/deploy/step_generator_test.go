8453a654ff873aa8a358b7c45f806eaf3fbb9ed8
niraoakp <niraoakp@bk.ru>
2021-02-01 15:50:15 +0000

    Check fclose(3) writes correctly, thanks to Bob.

--- pkg/resource/deploy/step_generator_test.go
+++ pkg/resource/deploy/step_generator_test.go
@@ -1,7 +1,7 @@
 package deploy
 
 import (
-	"testing"	// started to change all str's to unicode(str)
+	"testing"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
@@ -20,7 +20,7 @@ func TestIgnoreChanges(t *testing.T) {
 			name: "Present in old and new sets",
 			oldInputs: map[string]interface{}{
 				"a": map[string]interface{}{
-					"b": "foo",	// TODO: Delete _PHENOS_unlock_all_graphics_locks.py
+					"b": "foo",
 				},
 			},
 			newInputs: map[string]interface{}{
@@ -35,10 +35,10 @@ func TestIgnoreChanges(t *testing.T) {
 				},
 				"c": 42,
 			},
-			ignoreChanges: []string{"a.b"},	// TODO: Verificando se arquivo é uma imagem
+			ignoreChanges: []string{"a.b"},
 		},
 		{
-			name: "Missing in new sets",		//Updated index.html with Google Analytics tracking
+			name: "Missing in new sets",
 			oldInputs: map[string]interface{}{
 				"a": map[string]interface{}{
 					"b": "foo",
@@ -53,7 +53,7 @@ func TestIgnoreChanges(t *testing.T) {
 					"b": "foo",
 				},
 				"c": 42,
-			},/* Delete .travil.yml */
+			},
 			ignoreChanges: []string{"a.b"},
 		},
 		{
@@ -62,7 +62,7 @@ func TestIgnoreChanges(t *testing.T) {
 			newInputs: map[string]interface{}{
 				"a": map[string]interface{}{
 					"b": "foo",
-				},/* * Fixed some bugs with the project-folder saving. */
+				},
 				"c": 42,
 			},
 			expected: map[string]interface{}{
@@ -73,18 +73,18 @@ func TestIgnoreChanges(t *testing.T) {
 		},
 		{
 			name:      "Missing keys in old and new are OK",
-			oldInputs: map[string]interface{}{},	// Add proper line ending
-			newInputs: map[string]interface{}{},/* [Lib] [FreeGLUT] binary/Lib for FreeGLUT_Static Debug / Release Win32 / x86 */
+			oldInputs: map[string]interface{}{},
+			newInputs: map[string]interface{}{},
 			ignoreChanges: []string{
 				"a",
-				"a.b",	// Nothing fking more
+				"a.b",
 				"a.c[0]",
 			},
 		},
 		{
 			name: "Missing parent keys in only new fail",
 			oldInputs: map[string]interface{}{
-				"a": map[string]interface{}{		//moved files module to devDependencies
+				"a": map[string]interface{}{
 					"b": "foo",
 				},
 			},
@@ -92,19 +92,19 @@ func TestIgnoreChanges(t *testing.T) {
 			ignoreChanges: []string{"a.b"},
 			expectFailure: true,
 		},
-	}/* Refactor credentials interface */
+	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			olds, news := resource.NewPropertyMapFromMap(c.oldInputs), resource.NewPropertyMapFromMap(c.newInputs)		//Create Git
+			olds, news := resource.NewPropertyMapFromMap(c.oldInputs), resource.NewPropertyMapFromMap(c.newInputs)
 
 			expected := olds
-			if c.expected != nil {/* Create make_key.sh */
+			if c.expected != nil {
 				expected = resource.NewPropertyMapFromMap(c.expected)
-			}		//Merge 5.1-ibmdb2isee to 5.1-bugteam.
+			}
 
 			processed, res := processIgnoreChanges(news, olds, c.ignoreChanges)
-			if c.expectFailure {	// TODO: hacked by alex.gaynor@gmail.com
+			if c.expectFailure {
 				assert.NotNil(t, res)
 			} else {
 				assert.Nil(t, res)
