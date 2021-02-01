6c7f3a8914cfc18405871615a3546b10ecb9b3fa
niraoakp <niraoakp@bk.ru>
2021-02-01 21:41:58 +0000

    add new chapter

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* @Release [io7m-jcanephora-0.29.0] */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
