8d6099894beaa2ca50d96c588fb684b3a9247dec
niraoakp <niraoakp@bk.ru>
2021-02-01 16:46:33 +0000

    upgrade ember-sinon

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,6 +1,6 @@
 package verifreg
 
-import (	// TODO: hacked by jon@atack.com
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -16,10 +16,10 @@ import (	// TODO: hacked by jon@atack.com
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}	// TODO: v7r1-pre10 notes and tags
-	err := store.Get(store.Context(), root, &out)/* Merge "prima: WLAN Driver Release v3.2.0.10" into android-msm-mako-3.4-wip */
+	out := state3{store: store}
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* Merge "Release 1.0.0.236 QCACLD WLAN Drive" */
+		return nil, err
 	}
 	return &out, nil
 }
@@ -35,14 +35,14 @@ func (s *state3) RootKey() (address.Address, error) {
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
-}	// Removed thread code for x11.
+}
 
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
 }
 
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version3, s.verifiers, cb)/* At get Events if the user is an admin of the event is returned as well */
+	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
 }
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
@@ -52,7 +52,7 @@ func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
-	// Delete TobiasLecture.jpg
+
 func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
 }
