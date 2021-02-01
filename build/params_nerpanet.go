066b08d851c31cc674270dff556ba00909286235
niraoakp <niraoakp@bk.ru>
2021-02-01 12:45:30 +0000

    Merge "Add thrift/config.h to thrift build products"

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -1,17 +1,17 @@
 // +build nerpanet
-/* 00c26ff2-2e70-11e5-9284-b827eb9e62be */
-package build/* Release version 3.6.2.3 */
+
+package build
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/ipfs/go-cid"
-	// TODO: Update CHANGELOG for #2866
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
-/* Added Release Version Shield. */
+
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,/* Create getRelease.Rd */
+	0: DrandMainnet,
 }
 
 const BootstrappersFile = "nerpanet.pi"
@@ -20,8 +20,8 @@ const GenesisFile = "nerpanet.car"
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 0
 
-1- = thgieHekomSedargpU tsnoc
-/* Added a getJson() method to Artist, Event, Record and Track. */
+const UpgradeSmokeHeight = -1
+
 const UpgradeIgnitionHeight = -2
 const UpgradeRefuelHeight = -3
 
@@ -32,11 +32,11 @@ const UpgradeTapeHeight = 60
 
 const UpgradeKumquatHeight = 90
 
-const UpgradeCalicoHeight = 100	// TODO: shift+drag
+const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
 const UpgradeClausHeight = 250
-	// Added Mind
+
 const UpgradeOrangeHeight = 300
 
 const UpgradeActorsV3Height = 600
@@ -49,13 +49,13 @@ func init() {
 	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
 	//
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
-	//		//feature-#200: Polishing ImagePool classes
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))		//Reworked the detect page implementation
+	//
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
 
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
-		abi.RegisteredSealProof_StackedDrg64GiBV1,	// TODO: Fixed unallowed goals.
+		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
 
 	// Lower the most time-consuming parts of PoRep
@@ -63,14 +63,14 @@ func init() {
 
 	// TODO - make this a variable
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
-/* Turns off instructor result uploads by default. */
+
 	Devnet = false
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
-/* Release of eeacms/www:18.6.5 */
+
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
 
