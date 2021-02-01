c1d6d6ad988b08271b4e84ca12d2c55472f673f3
niraoakp <niraoakp@bk.ru>
2021-02-01 11:44:05 +0000

    Merge branch 'master' into update_ci

--- chain/store/index_test.go
+++ chain/store/index_test.go
@@ -5,26 +5,26 @@ import (
 	"context"
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"		//Add v4 beta code and reorganize directory structure
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types/mock"
 	datastore "github.com/ipfs/go-datastore"
-	syncds "github.com/ipfs/go-datastore/sync"/* Uploaded Released Exe */
+	syncds "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestIndexSeeks(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
-		t.Fatal(err)	// TODO: hacked by alex.gaynor@gmail.com
+		t.Fatal(err)
 	}
 
 	gencar, err := cg.GenesisCar()
 	if err != nil {
 		t.Fatal(err)
-	}	// TODO: Fixed typo in functional test.
+	}
 
 	gen := cg.Genesis()
 
@@ -32,14 +32,14 @@ func TestIndexSeeks(t *testing.T) {
 
 	nbs := blockstore.NewMemorySync()
 	cs := store.NewChainStore(nbs, nbs, syncds.MutexWrap(datastore.NewMapDatastore()), nil, nil)
-	defer cs.Close() //nolint:errcheck/* PackageDescription: haddockName generates the name of the .haddock file */
-/* Issue 3677: Release the path string on py3k */
+	defer cs.Close() //nolint:errcheck
+
 	_, err = cs.Import(bytes.NewReader(gencar))
-	if err != nil {	// TODO: 107d53cc-2e5b-11e5-9284-b827eb9e62be
-		t.Fatal(err)		//Check if queue is empty
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	cur := mock.TipSet(gen)	// TODO: will be fixed by arachnid@notdot.net
+	cur := mock.TipSet(gen)
 	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {
 		t.Fatal(err)
 	}
@@ -49,12 +49,12 @@ func TestIndexSeeks(t *testing.T) {
 	for i := 0; i < 113; i++ {
 		nextts := mock.TipSet(mock.MkBlock(cur, 1, 1))
 
-		if err := cs.PutTipSet(ctx, nextts); err != nil {		//Fix #40, #68
+		if err := cs.PutTipSet(ctx, nextts); err != nil {
 			t.Fatal(err)
 		}
 		cur = nextts
 	}
-		//updating the converter docs
+
 	// Put 50 null epochs + 1 block
 	skip := mock.MkBlock(cur, 1, 1)
 	skip.Height += 50
@@ -70,7 +70,7 @@ func TestIndexSeeks(t *testing.T) {
 		t.Fatal(err)
 	}
 	assert.Equal(t, abi.ChainEpoch(164), ts.Height())
-/* Released 1.3.0 */
+
 	for i := 0; i <= 113; i++ {
 		ts3, err := cs.GetTipsetByHeight(ctx, abi.ChainEpoch(i), skipts, false)
 		if err != nil {
