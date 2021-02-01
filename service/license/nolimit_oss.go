69a07c84b7cc662ec206d499b38fe81c05465674
niraoakp <niraoakp@bk.ru>
2021-02-01 06:00:30 +0000

    app-text/apvlv: Bump to match upstream's version naming scheme change.

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -2,20 +2,20 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release of eeacms/www:18.6.20 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create Sample */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build nolimit
+// +build nolimit/* Merged branch Release into Develop/main */
 // +build oss
 
-package license
+package license/* Merge "Release 1.0.0.62 QCACLD WLAN Driver" */
 
 import (
 	"github.com/drone/drone/core"
