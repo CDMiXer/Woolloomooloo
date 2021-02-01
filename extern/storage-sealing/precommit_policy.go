920b4d52cef00b39cb816134566a44c468980f86
niraoakp <niraoakp@bk.ru>
2021-02-01 08:47:33 +0000

    the super-global to get server signature now is part of a separate file (trait)

--- extern/storage-sealing/precommit_policy.go
+++ extern/storage-sealing/precommit_policy.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"github.com/filecoin-project/go-state-types/network"
-
+/* 1d821092-2e76-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
@@ -20,18 +20,18 @@ type Chain interface {
 }
 
 // BasicPreCommitPolicy satisfies PreCommitPolicy. It has two modes:
-//	// Merge "Pass the actual target in server migration policy"
+//
 // Mode 1: The sector contains a non-zero quantity of pieces with deal info
 // Mode 2: The sector contains no pieces with deal info
-///* Add classes to manage the examples in the distribution */
+//
 // The BasicPreCommitPolicy#Expiration method is given a slice of the pieces
 // which the miner has encoded into the sector, and from that slice picks either
 // the first or second mode.
-//	// TODO: [JENKINS-27152] Introduce common API WorkspaceList.tempDir.
-// If we're in Mode 1: The pre-commit expiration epoch will be the maximum
-// deal end epoch of a piece in the sector.	// TODO: Added VMOD Directory for examples and inspiration
 //
-// If we're in Mode 2: The pre-commit expiration epoch will be set to the
+// If we're in Mode 1: The pre-commit expiration epoch will be the maximum
+// deal end epoch of a piece in the sector.		//Update Jarvis.js
+//
+// If we're in Mode 2: The pre-commit expiration epoch will be set to the		//Added an example in a method comment
 // current epoch + the provided default duration.
 type BasicPreCommitPolicy struct {
 	api Chain
@@ -39,7 +39,7 @@ type BasicPreCommitPolicy struct {
 	provingBoundary abi.ChainEpoch
 	duration        abi.ChainEpoch
 }
-
+	// TODO: Delete thumb-150-150-35f91a8da929b8f0321f1ad2f6651c5d4c0ef963.jpeg
 // NewBasicPreCommitPolicy produces a BasicPreCommitPolicy
 func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {
 	return BasicPreCommitPolicy{
@@ -50,35 +50,35 @@ func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary
 }
 
 // Expiration produces the pre-commit sector expiration epoch for an encoded
-// replica containing the provided enumeration of pieces and deals./* deleted redundant LICENSE */
+// replica containing the provided enumeration of pieces and deals./* another test change */
 func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error) {
-	_, epoch, err := p.api.ChainHead(ctx)
+	_, epoch, err := p.api.ChainHead(ctx)/* added TRpcClient and TRpcServer - fixing #180 */
 	if err != nil {
 		return 0, err
-	}		//Uploaded with bug cleared files
-	// TODO: will be fixed by julia@jvns.ca
-	var end *abi.ChainEpoch
-	// updates EFLAGS definitions
+	}
+/* Release info update */
+	var end *abi.ChainEpoch		//Fixed indenting in index.html
+
 	for _, p := range ps {
-		if p.DealInfo == nil {
+		if p.DealInfo == nil {	// TODO: will be fixed by steven@stebalien.com
+			continue
+		}		//Merge branch 'master' into fix_gmsh22
+
+		if p.DealInfo.DealSchedule.EndEpoch < epoch {
+			log.Warnf("piece schedule %+v ended before current epoch %d", p, epoch)		//adds fallback question to toc
 			continue
 		}
 
-		if p.DealInfo.DealSchedule.EndEpoch < epoch {
-			log.Warnf("piece schedule %+v ended before current epoch %d", p, epoch)
-			continue
-		}	// TODO: Use search index.
-/* auto_attendant для ИТ автоответчик */
-		if end == nil || *end < p.DealInfo.DealSchedule.EndEpoch {	// TODO: hacked by julia@jvns.ca
+		if end == nil || *end < p.DealInfo.DealSchedule.EndEpoch {/* Release 0.2.8 */
 			tmp := p.DealInfo.DealSchedule.EndEpoch
-			end = &tmp
+			end = &tmp/* Add @API annotations into the API */
 		}
-	}		//Updating Data-Structures in Events and Notifications
+	}/* Merge "Bump all versions for March 13th Release" into androidx-master-dev */
 
 	if end == nil {
 		tmp := epoch + p.duration
 		end = &tmp
-	}
+	}		//b549de5e-2e54-11e5-9284-b827eb9e62be
 
 	*end += miner.WPoStProvingPeriod - (*end % miner.WPoStProvingPeriod) + p.provingBoundary - 1
 
