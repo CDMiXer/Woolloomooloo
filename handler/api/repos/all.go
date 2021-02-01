1528b9d38374e7c0d457e0cb2b112ff68099e699
niraoakp <niraoakp@bk.ru>
2021-02-01 00:26:38 +0000

    Bugfix: when creating async response fails show a message box with the error.

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -1,53 +1,53 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Delete globals.h */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* A few bug fixes. Release 0.93.491 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release 0.9.13 */
 // limitations under the License.
 
-package repos
+package repos/* Release version 3.0.0.M4 */
 
 import (
 	"net/http"
-	"strconv"
+	"strconv"		//Created "IParser" interface to simplify chaining code.
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge "Release 1.0.0.253 QCACLD WLAN Driver" */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-
+	// Empty portrayals
 // HandleAll returns an http.HandlerFunc that processes http
 // requests to list all repositories in the database.
 func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+	return func(w http.ResponseWriter, r *http.Request) {/* Updated the metadata. */
+		var (		//rev 680309
 			page    = r.FormValue("page")
 			perPage = r.FormValue("per_page")
 		)
 		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 { // || limit > 100
-			limit = 25
+			limit = 25/* Merge "Fix mesos monitor for handling multiple masters" */
 		}
 		switch offset {
 		case 0, 1:
 			offset = 0
 		default:
 			offset = (offset - 1) * limit
-		}
-		repo, err := repos.ListAll(r.Context(), limit, offset)
-		if err != nil {
+		}/* Merge "Release 1.0.0.95 QCACLD WLAN Driver" */
+		repo, err := repos.ListAll(r.Context(), limit, offset)	// Delete leave.php
+		if err != nil {/* [:wrench:] Clean up ignores */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				Debugln("api: cannot list repositories")
+				Debugln("api: cannot list repositories")		//5b07bbb4-2e72-11e5-9284-b827eb9e62be
 		} else {
 			render.JSON(w, repo, 200)
 		}
