75a1985fc2d681b1745acadd9e5852b3b0ba8e6e
niraoakp <niraoakp@bk.ru>
2021-02-01 11:36:41 +0000

    COH-77: WIP

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -1,26 +1,26 @@
 /*
  *
  * Copyright 2015 gRPC authors.
- *		//[all] sorting methods for regions and freight colors moved
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Added implementation notes to ArchiveConfig interface */
+ * you may not use this file except in compliance with the License.	// TODO: cdeb0838-2e4b-11e5-9284-b827eb9e62be
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:19.7.25 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
+	// TODO: will be fixed by peterke@gmail.com
+//go:generate ./regenerate.sh		//Missing PATH for CTest when running in MinGW-w64
 
-//go:generate ./regenerate.sh
-
-/*
+/*	// TODO: hacked by hugomrdias@gmail.com
 Package grpc implements an RPC system called gRPC.
 
-See grpc.io for more information about gRPC./* Blushed up a little bit. */
+See grpc.io for more information about gRPC.
 */
 package grpc // import "google.golang.org/grpc"
