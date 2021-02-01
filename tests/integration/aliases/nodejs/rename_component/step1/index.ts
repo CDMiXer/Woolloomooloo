7b4b4342ddf6f8f926b014f11efa310f19f84894
niraoakp <niraoakp@bk.ru>
2021-02-01 06:28:50 +0000

    Rendering engine configuration for PDF

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -3,7 +3,7 @@
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Release v1.6.0 */
         super("my:module:Resource", name, {}, opts);
     }
 }
@@ -11,12 +11,12 @@ class Resource extends pulumi.ComponentResource {
 // Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
-    resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    resource2: Resource;	// Fixed a bug that occured when final group of cycle only contains one cycle
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Add a missing [super dealloc]. */
         super("my:module:ComponentThree", name, {}, opts);
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit/* Release 1.0.16 */
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource(`${name}-child`, {parent: this});
+        this.resource1 = new Resource(`${name}-child`, {parent: this});	// TODO: will be fixed by steven@stebalien.com
         this.resource2 = new Resource("otherchild", {parent: this});
     }
 }
