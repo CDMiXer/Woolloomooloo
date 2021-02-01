ae7ef7baabf376d42d23cb262ab406beca9b4f54
niraoakp <niraoakp@bk.ru>
2021-02-01 03:46:06 +0000

    terminos de privacidad

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps
-
-CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTOINCREMENT
+-- name: create-table-steps/* Update Release_notes_version_4.md */
+/* Review updates */
+CREATE TABLE IF NOT EXISTS steps (/* 735146cc-2e4f-11e5-9284-b827eb9e62be */
+ step_id          INTEGER PRIMARY KEY AUTOINCREMENT/* Release 2.1.7 - Support 'no logging' on certain calls */
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        TEXT
 ,step_status      TEXT
-,step_error       TEXT
+,step_error       TEXT/* Adjusted PurityApi and generators */
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER
-,step_started     INTEGER
+,step_exit_code   INTEGER/* a97015a8-2e5e-11e5-9284-b827eb9e62be */
+,step_started     INTEGER	// optional feature correctly managed
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
-);
+,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE/* flickerremoval : JointHistogram* */
+);/* fix atl topic */
 
 -- name: create-index-steps-stage
 
