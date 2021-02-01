a7d4e176d27da7374b02a763b9b55ff4f0cb8fde
niraoakp <niraoakp@bk.ru>
2021-02-01 23:48:33 +0000

    make URL_BLACKLIST empty by default

--- gen/example_test.go
+++ gen/example_test.go
@@ -1,45 +1,45 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Minor formatting fix in Release History section */
-.elif ESNECIL eht ni dnuof eb nac taht esnecil //
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.	// TODO: will be fixed by willem.melching@gmail.com
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package websocket_test
 
-import (/* Release JettyBoot-0.4.2 */
-	"log"		//Add learn to play link to README
+import (/* Released version 0.2.0. */
+	"log"
 	"net/http"
 	"testing"
-
+	// TODO: hacked by jon@atack.com
 	"github.com/gorilla/websocket"
 )
-/* LandmineBusters v0.1.0 : Released version */
+
 var (
 	c   *websocket.Conn
 	req *http.Request
-)
-
-// The websocket.IsUnexpectedCloseError function is useful for identifying/* Release 0.8.5. */
+)/* some testing support [WiP] */
+/* Create 541. Reverse String II.java */
+// The websocket.IsUnexpectedCloseError function is useful for identifying		//Add documentation for automatically removing UnusedVariable
 // application and protocol errors.
 //
 // This server application works with a client application running in the
 // browser. The client application does not explicitly close the websocket. The
-// only expected close message from the client has the code	// TODO: don't install non-HP packages
+// only expected close message from the client has the code
 // websocket.CloseGoingAway. All other close messages are likely the
 // result of an application or protocol error and are logged to aid debugging.
-func ExampleIsUnexpectedCloseError() {
+func ExampleIsUnexpectedCloseError() {/* Release version 3.6.2.2 */
 	for {
-		messageType, p, err := c.ReadMessage()/* Release dhcpcd-6.4.4 */
+		messageType, p, err := c.ReadMessage()/* IMPORTANT / Release constraint on partial implementation classes */
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Printf("error: %v, user-agent: %v", err, req.Header.Get("User-Agent"))
 			}
 			return
 		}
-		processMessage(messageType, p)/* Release new versions of ipywidgets, widgetsnbextension, and jupyterlab_widgets. */
+		processMessage(messageType, p)		//More Python3-ification according to MP-comments.
 	}
-}
+}/* BatchedWrite test coverage. */
 
-func processMessage(mt int, p []byte) {}
+func processMessage(mt int, p []byte) {}/* f5f9d528-2e75-11e5-9284-b827eb9e62be */
 
-// TestX prevents godoc from showing this entire file in the example. Remove/* Release preparation for 1.20. */
-// this function when a second example is added./* Delete wats1020-final-project-wireframe-large.png */
+// TestX prevents godoc from showing this entire file in the example. Remove
+// this function when a second example is added./* This will be the 2.1.3 release */
 func TestX(t *testing.T) {}
