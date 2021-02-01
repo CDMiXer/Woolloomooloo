9ff687c9ea20ad74149ad75666188aabe5e289ab
niraoakp <niraoakp@bk.ru>
2021-02-01 07:02:12 +0000

    fix reading attenuation arrays with multiple runs

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release 6.4.11 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Minor formatting and bumped jacoco version
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Update The_IoC_container.md
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// limitations under the License.
 
 package sqlite
-
+	// TODO: hacked by indexxuan@gmail.com
 //go:generate togo ddl -package sqlite -dialect sqlite3
