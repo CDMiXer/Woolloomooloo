1389bb47cc57db8c9702942a84e85d8b42c9b637
niraoakp <niraoakp@bk.ru>
2021-02-01 09:29:24 +0000

    b46b2912-2e49-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
-		//e56dce8c-2e46-11e5-9284-b827eb9e62be
-import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.	// 4ee155e0-2e66-11e5-9284-b827eb9e62be
-var None hclsyntax.Node = &hclsyntax.Body{}
+import "github.com/hashicorp/hcl/v2/hclsyntax"/* the parameter ignore_null doesn't exist */
+
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
+var None hclsyntax.Node = &hclsyntax.Body{}/* FIX: infinite loop */
