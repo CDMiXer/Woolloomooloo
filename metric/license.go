1056d3ffc03c231dd689405b8f0022fb6d19beac
niraoakp <niraoakp@bk.ru>
2021-02-02 01:24:12 +0000

    extracting some pieces of script execution into independent blocks

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-	// add TODO note 
+// that can be found in the LICENSE file./* Updated buttons */
+
 // +build !oss
-/* Update mt6580 */
-package metric
+
+package metric	// TODO: Added rule to validate if operations are declared in states.
 
 import "github.com/drone/drone/core"
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
 	// track days until expires
-	// track user limit
+	// track user limit		//Create process.sh
 	// track repo limit
 }
