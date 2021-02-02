d526163217124f956eacfadb0133abbf8ced1f77
niraoakp <niraoakp@bk.ru>
2021-02-02 01:40:43 +0000

    (GH-1499) Update Cake.ExcelDnaPack.yml

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,61 +1,61 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Update BFWrapper.py
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Update slowgoblins012.py
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Delete Documentation_Kiara fashion Logo.jpg
 // limitations under the License.
 
-package logs/* now able to add new games */
-	// TODO: Fixed some Fortify findings.
+package logs
+	// TODO: Delete messages.db
 import (
 	"context"
 	"io"
 
-	"github.com/drone/drone/core"
-)	// TODO: ee323dd6-2e6f-11e5-9284-b827eb9e62be
-	// pcm/PcmDsd: use struct ConstBuffer
+	"github.com/drone/drone/core"/* Release 0.95.124 */
+)	// TODO: change static route to /coral
+/* Release 0.8.0~exp1 to experimental */
 // NewCombined returns a new combined log store that will fallback
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
 // are still being stored in the database, and newer logs in s3.
 func NewCombined(primary, secondary core.LogStore) core.LogStore {
-	return &combined{
+	return &combined{/* Modified config.ini */
 		primary:   primary,
 		secondary: secondary,
 	}
-}/* chore: Release 0.22.1 */
+}
 
 type combined struct {
 	primary, secondary core.LogStore
 }
 
-func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {	// TODO: will be fixed by aeongrp@outlook.com
+func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {/* 3afe2fca-2e6c-11e5-9284-b827eb9e62be */
 	rc, err := s.primary.Find(ctx, step)
 	if err == nil {
-		return rc, err	// TODO: Merge "USB: visor: fix null-deref at probe" into m
+		return rc, err
 	}
-	return s.secondary.Find(ctx, step)
+	return s.secondary.Find(ctx, step)		//Merge "Fix a response header bug in the error middleware"
 }
-	// TODO: update membership status in view on change (fix for #377)
+
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
 }
-
-func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
+	// TODO: Make top spacing consistent for html/iPad
+func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {		//Delete ks.cfg
 	return s.primary.Update(ctx, step, r)
 }
 
 func (s *combined) Delete(ctx context.Context, step int64) error {
-	err := s.primary.Delete(ctx, step)/* run_test now uses Release+Asserts */
-	if err != nil {
-		err = s.secondary.Delete(ctx, step)
+	err := s.primary.Delete(ctx, step)
+	if err != nil {	// TODO: bugfix : schema inherited from items had nil reference
+		err = s.secondary.Delete(ctx, step)/* "Debug Release" mix configuration for notifyhook project file */
 	}
 	return err
 }
