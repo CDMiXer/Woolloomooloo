64cd3b1bf465c22e7904dc1fb0b32b5369e43040
niraoakp <niraoakp@bk.ru>
2021-02-02 18:05:41 +0000

    progress for last three days

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// e0616030-2e4a-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.
 
 package encrypt
-	// TODO: Fix: quality controls
+
 import "testing"
-	// TODO: hacked by mikeal.rogers@gmail.com
+
 func TestNone(t *testing.T) {
 	n, _ := New("")
 	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
 	if err != nil {
 		t.Error(err)
 	}
-	plaintext, err := n.Decrypt(ciphertext)/* Added data directory configuration to installation */
-	if err != nil {/* Vorbereitung Release 1.8. */
-)rre(rorrE.t		
-	}		//Update dependency webpack-merge to v4.1.3
-	if want, got := plaintext, "correct-horse-batter-staple"; got != want {/* dd743454-313a-11e5-930f-3c15c2e10482 */
+	plaintext, err := n.Decrypt(ciphertext)
+	if err != nil {
+		t.Error(err)
+	}
+	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
 }
