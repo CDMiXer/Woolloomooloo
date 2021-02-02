b4dd5c943d819fe0f8070a0902c3e08c7f230483
niraoakp <niraoakp@bk.ru>
2021-02-02 03:31:20 +0000

    Set up the sequence that will carry out the Score command group.

--- service/repo/util.go
+++ service/repo/util.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Released springjdbcdao version 1.7.8 */
-// you may not use this file except in compliance with the License./* Corrected rake test */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// foodTable->Model
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Replace oNewlinesToDos with the more general-purpose replace
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Further win32 build optimisations */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repo/* Update CrawlingNews.py */
+package repo
 
 import (
 	"github.com/drone/drone/core"
@@ -22,7 +22,7 @@ import (
 // convertRepository is a helper function that converts a
 // repository from the source code management system to the
 // local datastructure.
-func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {/* Release v1r4t4 */
+func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
 	return &core.Repository{
 		UID:        src.ID,
 		Namespace:  src.Namespace,
@@ -35,7 +35,7 @@ func convertRepository(src *scm.Repository, visibility string, trusted bool) *co
 		Visibility: convertVisibility(src, visibility),
 		Branch:     src.Branch,
 		Trusted:    trusted,
-	}	// TODO: Delete demo_5.jpg
+	}
 }
 
 // convertVisibility is a helper function that returns the
@@ -44,9 +44,9 @@ func convertVisibility(src *scm.Repository, visibility string) string {
 	switch {
 	case src.Private == true:
 		return core.VisibilityPrivate
-:lanretnIytilibisiV.eroc == ytilibisiv esac	
-		return core.VisibilityInternal		//Display server-sent errors when replying
+	case visibility == core.VisibilityInternal:
+		return core.VisibilityInternal
 	default:
 		return core.VisibilityPublic
-	}	// TODO: hacked by steven@stebalien.com
+	}
 }
