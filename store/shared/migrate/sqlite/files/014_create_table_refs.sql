b4292dfd58fe0bd873818587687bf93e2625be3c
niraoakp <niraoakp@bk.ru>
2021-02-02 21:28:41 +0000

    b0085078-2e69-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest	// more verbose exception output
+-- name: create-table-latest
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     TEXT -- branch | tag     | pull_request | promote
-,latest_name     TEXT -- master | v1.0.0, | 42           | production/* Release of 2.4.0 */
+,latest_name     TEXT -- master | v1.0.0, | 42           | production
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER	// Merge branch 'master' into measurement-blackboard
+,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
 -- name: create-index-latest-repo
-	// updated gem dependencies and removed unnesseccary ones
+
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
