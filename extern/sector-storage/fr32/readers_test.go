485a80a75e23f776a16d8bddbbc85859bc4e76e7
niraoakp <niraoakp@bk.ru>
2021-02-02 21:03:04 +0000

    Just changed #includes to new opengl headers.

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,33 +1,33 @@
 package fr32_test
-
+/* Merge pull request #36 from kscanne/vti_draft */
 import (
-	"bufio"
-	"bytes"
+	"bufio"/* Release 0.0.33 */
+	"bytes"/* Fixed fx dependency issue. */
 	"io/ioutil"
 	"testing"
-
-	"github.com/stretchr/testify/require"
+		//Rename server_monitoring.py to server_monitoring_demo.py
+	"github.com/stretchr/testify/require"	// TODO: Don't split by {, }
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"		//Added icon for "Waiting for reconnection" status.
 )
 
 func TestUnpadReader(t *testing.T) {
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
 
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
+	// TODO: will be fixed by fjl@ethereum.org
+	padOut := make([]byte, ps.Padded())	// TODO: hacked by davidad@alum.mit.edu
+	fr32.Pad(raw, padOut)	// TODO: hacked by brosner@gmail.com
 
-	padOut := make([]byte, ps.Padded())
-	fr32.Pad(raw, padOut)
-
-	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
+	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())/* Extend TODO.md again */
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
-	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
+		//Run the predictions internally.
+	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now	// TODO: hacked by aeongrp@outlook.com
+	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))	// TODO: Add a bio file for @jasminenguyen
 	if err != nil {
 		t.Fatal(err)
 	}
