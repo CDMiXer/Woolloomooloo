20b8bb6583adea35e6764c1069aa1519e2cf61de
niraoakp <niraoakp@bk.ru>
2021-02-02 19:09:41 +0000

    261de958-2e42-11e5-9284-b827eb9e62be

--- tests/integration/partial_state/step1/resource.ts
+++ tests/integration/partial_state/step1/resource.ts
@@ -5,27 +5,27 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 
 // NOTE: Dynamic provider is restarted every step, so unless we read this from some external state
 // store, this would always be 0 anyway.
-const id = 0;
-
+const id = 0;	// 09693efe-2e4f-11e5-9284-b827eb9e62be
+/* Release of eeacms/eprtr-frontend:1.4.3 */
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
+    public static readonly instance = new Provider();/* 0.6.1 Alpha Release */
 
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {		//Jobbar vidare med beskrivning av parentesmultiplikation
         return {
             inputs: news,
         };
-    }
+    }/* NS_BLOCK_ASSERTIONS for the Release target */
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         if (inputs.state === 4) {
             return Promise.reject({
                 message: "Resource failed to initialize", id: id.toString(), properties: inputs,
                 reasons: ["state can't be 4"],
-            });
+            });		//Parameter tweaks.
         }
-
+	// TODO: will be fixed by m-ou.se@m-ou.se
         return {
-            id: id.toString(),
+            id: id.toString(),/* Release 1.7.2 */
             outs: inputs,
         };
     }
@@ -42,7 +42,7 @@ export class Provider implements dynamic.ResourceProvider {
             outs: news,
         };
     }
-}
+}		//Update doxygen_header.html
 
 export class Resource extends dynamic.Resource {
     public readonly state: pulumi.Output<number>;
