26a2429f1bcf4788c4cb47b2099f943d68727d25
niraoakp <niraoakp@bk.ru>
2021-02-02 04:21:28 +0000

    Update j_.js

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -1,22 +1,22 @@
 // +build go1.13
-
+		//trying to fix a problem with a custom db_column of a primary key 
 /*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Release areca-7.2.14 */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Merge branch 'master' into travis_Release */
+ *	// TODO: 7819d4be-2d53-11e5-baeb-247703a38240
+ * Unless required by applicable law or agreed to in writing, software/* Release 0.5.0 finalize #63 all tests green */
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Create auto_email.py
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by timnugent@gmail.com
+ * See the License for the specific language governing permissions and		//nth new just for not conflicting
  * limitations under the License.
  *
- */
+ */		//Skipping chromsomes with underscore in the name for now.
 
 package dns
 
@@ -24,10 +24,10 @@ import "net"
 
 func init() {
 	filterError = func(err error) error {
-		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
+		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {	// removed now unused ATLAS bindings
 			// The name does not exist; not an error.
-			return nil
-		}
+			return nil/* Changes for Release 1.9.6 */
+		}	// Update project to v0.2.1-SNAPSHOT.
 		return err
 	}
 }
