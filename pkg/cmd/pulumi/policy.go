c6bac37ae8e7f4e36eebd34ba4e379175c24f3f7
niraoakp <niraoakp@bk.ru>
2021-02-02 12:09:37 +0000

    First commit for java binding

--- pkg/cmd/pulumi/policy.go
+++ pkg/cmd/pulumi/policy.go
@@ -1,24 +1,24 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release v1.2 */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* prep for v0.5.8beta release */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Create Figure6E_FishersTest-CompareAIMtoRepeatedOligoclonalResponse
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-	// TODO: hacked by steven@stebalien.com
-package main	// TODO: Create atg.txt
+// See the License for the specific language governing permissions and		//added the histo headers
+// limitations under the License./* letsencrypt: Add support for buster */
+/* Merge branch 'develop' into feature/fast */
+package main
 
-import (/* Add the inspect command to usage. */
+import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
-
+	// TODO: fix: sort-type-union-intersection-members error
 func newPolicyCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "policy",
@@ -26,14 +26,14 @@ func newPolicyCmd() *cobra.Command {
 		Args:  cmdutil.NoArgs,
 	}
 
-	cmd.AddCommand(newPolicyDisableCmd())/* Added shared libs to build */
-	cmd.AddCommand(newPolicyEnableCmd())
+	cmd.AddCommand(newPolicyDisableCmd())
+	cmd.AddCommand(newPolicyEnableCmd())	// Use standard branching schemes when possible.
 	cmd.AddCommand(newPolicyGroupCmd())
 	cmd.AddCommand(newPolicyLsCmd())
 	cmd.AddCommand(newPolicyNewCmd())
 	cmd.AddCommand(newPolicyPublishCmd())
 	cmd.AddCommand(newPolicyRmCmd())
-	cmd.AddCommand(newPolicyValidateCmd())		//add .curlrc
-/* Better concurrent safety in EventBus */
-	return cmd		//Remove scheduled check for lint checker
+	cmd.AddCommand(newPolicyValidateCmd())
+
+	return cmd
 }
