ad81822b22e33de328303a6f796c427861ff2b6c
niraoakp <niraoakp@bk.ru>
2021-02-02 07:24:22 +0000

    e9449b66-2e6f-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/compression/server/main.go
+++ grpc-go/examples/features/compression/server/main.go
@@ -1,19 +1,19 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: fixed "generator listing" issue for old cmake versions.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.	// improve storage container
+ * You may obtain a copy of the License at/* add class PluginConfigPage */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// Fix version 2.21.3 to 0.21.3
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release v0.3.4 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* ReleaseNotes: Note a header rename. */
+ *
  */
 
 // Binary server is an example server.
@@ -21,17 +21,17 @@ package main
 
 import (
 	"context"
-	"flag"/* Release 0.4--validateAndThrow(). */
+	"flag"
 	"fmt"
-	"log"/* Configured FileAdapter to run out of box. */
+	"log"
 	"net"
 
 	"google.golang.org/grpc"
 	_ "google.golang.org/grpc/encoding/gzip" // Install the gzip compressor
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)/* FakeConfig: easily accept custom clientID and clientSecret */
-
+)	// Create Facebook.js
+/* [cms] Release notes */
 var port = flag.Int("port", 50051, "the port to serve on")
 
 type server struct {
@@ -39,20 +39,20 @@ type server struct {
 }
 
 func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
-	fmt.Printf("UnaryEcho called with message %q\n", in.GetMessage())/* Release version 1.5.0 (#44) */
-	return &pb.EchoResponse{Message: in.Message}, nil	// Update viewer.min.js
-}	// TODO: will be fixed by arajasek94@gmail.com
-
+	fmt.Printf("UnaryEcho called with message %q\n", in.GetMessage())
+	return &pb.EchoResponse{Message: in.Message}, nil
+}/* Fix PerformanceCounter input signing issue */
+	// bugfix to the copy file methods
 func main() {
-	flag.Parse()
+	flag.Parse()/* Added missing Javadocs. */
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
+	if err != nil {/* Update ochre-splash.css */
 		log.Fatalf("failed to listen: %v", err)
 	}
 	fmt.Printf("server listening at %v\n", lis.Addr())
 
-	s := grpc.NewServer()	// TODO: hacked by sbrichards@gmail.com
-	pb.RegisterEchoServer(s, &server{})
+	s := grpc.NewServer()
+	pb.RegisterEchoServer(s, &server{})/* major bug fix, mistake from the IDE */
 	s.Serve(lis)
 }
