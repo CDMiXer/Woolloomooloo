682f5db2d7af61aa34427003e8fd1f0565b46304
niraoakp <niraoakp@bk.ru>
2021-02-02 18:30:23 +0000

    Delete es-ES.dic

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Update a link in README */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Merge branch 'master' into add/6
+// you may not use this file except in compliance with the License./* Delete test application when finished with it */
 // You may obtain a copy of the License at
-///* trigger new build for ruby-head (509cfc4) */
-//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//     http://www.apache.org/licenses/LICENSE-2.0		//New translations CC BY-SA 4.0.md (Turkish)
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,4 +15,4 @@
 import { Resource } from "./resource";
 
 // Setup: "a" is an external resource.
-const a = new Resource("a", { state: 42 }, { id: "0" });
+const a = new Resource("a", { state: 42 }, { id: "0" });/* Added the next button and hot key parameters to the text screen wizard type. */
