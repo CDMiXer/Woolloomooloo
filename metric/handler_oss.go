aacfc3cd90377d29ab188d87c61901458c5dabc6
niraoakp <niraoakp@bk.ru>
2021-02-02 22:29:49 +0000

    Movido mayo, añadido junio.

--- metric/handler_oss.go
+++ metric/handler_oss.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added correct instructions to run the setup script */
+// you may not use this file except in compliance with the License./* Delete db_dump.sql */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// removed unnecessary links
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Implemented CloseableZooKeeper.getData
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,9 +19,9 @@ package metric
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-)
-
+	"github.com/drone/drone/core"	// TODO: hacked by ng8eke@163.com
+)	// TODO: hacked by yuvalalaluf@gmail.com
+/* Release of eeacms/forests-frontend:2.0-beta.84 */
 // Server is a no-op http Metrics server.
 type Server struct {
 }
@@ -29,7 +29,7 @@ type Server struct {
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
 	return new(Server)
-}
-
+}/* Merge "Log warning when animator detects NaN value" into jb-mr2-dev */
+/* Release of eeacms/www-devel:20.8.7 */
 // ServeHTTP is a no-op http handler.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
