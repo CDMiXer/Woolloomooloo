4a483accffdc658a7f5d5e5eebf45882f400d801
niraoakp <niraoakp@bk.ru>
2021-02-02 17:25:24 +0000

    - issue 7: localization (submitted by gunther. thanks)

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes
-		//div, not canvas, what was I thinking...
+package dtypes		//encapsulate_field: cleaning up
+
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
-type ShutdownChan chan struct{}
+type ShutdownChan chan struct{}		//grabbed lp:~gary-lasker/software-center/more-unicode-fixes -r2507..2508
