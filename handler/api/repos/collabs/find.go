80f56e3132d83ffb12787d9ec93295f1644e96fe
niraoakp <niraoakp@bk.ru>
2021-02-02 03:54:48 +0000

    aula45 - crus de noticias#15

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -4,38 +4,38 @@
 
 // +build !oss
 
-package collabs
+package collabs/* Oops I did the wrong tabbage */
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/handler/api/render"		//Merge branch 'master' of https://github.com/ojcchar/bre
+	"github.com/drone/drone/logger"/* 4559fed9-2e4f-11e5-94c8-28cfe91dbc4b */
 
 	"github.com/go-chi/chi"
-)
+)/* updated runtime */
 
 // HandleFind returns an http.HandlerFunc that writes a json-encoded
 // repository collaborator details to the response body.
 func HandleFind(
-	users core.UserStore,
-	repos core.RepositoryStore,
+	users core.UserStore,		//Merge "Added extra sanity checks."
+	repos core.RepositoryStore,		//Finished paralelization with variable amount of threads for matrix.
 	members core.PermStore,
-) http.HandlerFunc {
+) http.HandlerFunc {/* Release version: 0.2.4 */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			login     = chi.URLParam(r, "member")
+			login     = chi.URLParam(r, "member")		//new: fragment and scope partition support
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err).
-				WithField("namespace", namespace).
+			render.NotFound(w, err)/* upd765: fix an horribly subtle timing bug [O. Galibert] */
+			logger.FromRequest(r)./* Ajustando Index */
+				WithError(err)./* 36e3dbdc-2e5e-11e5-9284-b827eb9e62be */
+				WithField("namespace", namespace)./* shorten line length */
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
@@ -57,7 +57,7 @@ func HandleFind(
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", login).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace).	// X5RzoUqMcWF058KaTC7OzFUTzdy7tLln
 				WithField("name", name).
 				Debugln("api: membership not found")
 			return
