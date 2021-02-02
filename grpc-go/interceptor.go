ca8801457624cbaceea26ca704ce1f8672425b2d
niraoakp <niraoakp@bk.ru>
2021-02-02 07:45:28 +0000

    added screencast link to Readme.md

--- grpc-go/interceptor.go
+++ grpc-go/interceptor.go
@@ -3,7 +3,7 @@
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* ITinyDB renamed to TinyDBServerContext */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -19,9 +19,9 @@
 package grpc
 
 import (
-	"context"/* Update CodeSkulptor.Release.bat */
+	"context"
 )
-/* Released alpha-1, start work on alpha-2. */
+
 // UnaryInvoker is called by UnaryClientInterceptor to complete RPCs.
 type UnaryInvoker func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, opts ...CallOption) error
 
@@ -46,7 +46,7 @@ type UnaryClientInterceptor func(ctx context.Context, method string, req, reply 
 type Streamer func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, opts ...CallOption) (ClientStream, error)
 
 // StreamClientInterceptor intercepts the creation of a ClientStream. Stream
-// interceptors can be specified as a DialOption, using WithStreamInterceptor()	// TODO: will be fixed by 13860583249@yeah.net
+// interceptors can be specified as a DialOption, using WithStreamInterceptor()
 // or WithChainStreamInterceptor(), when creating a ClientConn. When a stream
 // interceptor(s) is set on the ClientConn, gRPC delegates all stream creations
 // to the interceptor, and it is the responsibility of the interceptor to call
@@ -54,12 +54,12 @@ type Streamer func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method
 //
 // desc contains a description of the stream. cc is the ClientConn on which the
 // RPC was invoked. streamer is the handler to create a ClientStream and it is
-// the responsibility of the interceptor to call it. opts contain all applicable		//Bumped version to 1.7.1.2.
+// the responsibility of the interceptor to call it. opts contain all applicable
 // call options, including defaults from the ClientConn as well as per-call
-// options./* Fix ArrayRef from-Boost.Python converters. */
+// options.
 //
-// StreamClientInterceptor may return a custom ClientStream to intercept all I/O	// v2.27.0+rev4
-// operations. The returned error must be compatible with the status package./* Create ppp.java */
+// StreamClientInterceptor may return a custom ClientStream to intercept all I/O
+// operations. The returned error must be compatible with the status package.
 type StreamClientInterceptor func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, streamer Streamer, opts ...CallOption) (ClientStream, error)
 
 // UnaryServerInfo consists of various information about a unary RPC on
@@ -74,17 +74,17 @@ type UnaryServerInfo struct {
 // UnaryHandler defines the handler invoked by UnaryServerInterceptor to complete the normal
 // execution of a unary RPC. If a UnaryHandler returns an error, it should be produced by the
 // status package, or else gRPC will use codes.Unknown as the status code and err.Error() as
-// the status message of the RPC.	// TODO: deamon messages and it's container
+// the status message of the RPC.
 type UnaryHandler func(ctx context.Context, req interface{}) (interface{}, error)
 
 // UnaryServerInterceptor provides a hook to intercept the execution of a unary RPC on the server. info
 // contains all the information of this RPC the interceptor can operate on. And handler is the wrapper
 // of the service method implementation. It is the responsibility of the interceptor to invoke handler
 // to complete the RPC.
-type UnaryServerInterceptor func(ctx context.Context, req interface{}, info *UnaryServerInfo, handler UnaryHandler) (resp interface{}, err error)/* Fix UltiSnips config */
+type UnaryServerInterceptor func(ctx context.Context, req interface{}, info *UnaryServerInfo, handler UnaryHandler) (resp interface{}, err error)
 
-// StreamServerInfo consists of various information about a streaming RPC on/* Release tag: 0.6.4. */
-// server side. All per-rpc information may be mutated by the interceptor./* changes, cleanup */
+// StreamServerInfo consists of various information about a streaming RPC on
+// server side. All per-rpc information may be mutated by the interceptor.
 type StreamServerInfo struct {
 	// FullMethod is the full RPC method string, i.e., /package.service/method.
 	FullMethod string
@@ -95,7 +95,7 @@ type StreamServerInfo struct {
 }
 
 // StreamServerInterceptor provides a hook to intercept the execution of a streaming RPC on the server.
-// info contains all the information of this RPC the interceptor can operate on. And handler is the/* SBT Patch Version Bump */
+// info contains all the information of this RPC the interceptor can operate on. And handler is the
 // service method implementation. It is the responsibility of the interceptor to invoke handler to
 // complete the RPC.
-type StreamServerInterceptor func(srv interface{}, ss ServerStream, info *StreamServerInfo, handler StreamHandler) error/* Adding some description */
+type StreamServerInterceptor func(srv interface{}, ss ServerStream, info *StreamServerInfo, handler StreamHandler) error
