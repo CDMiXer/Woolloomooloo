df6b23960759b6f570a172347dc2410baa16344a
niraoakp <niraoakp@bk.ru>
2021-02-02 13:57:10 +0000

    Add Grunt badge.

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Updated text files to match LRE-1.2.1
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package events	// TODO: will be fixed by arajasek94@gmail.com
+/* LGPLv3 => LGPLv3 + ASLv2 */
+package events	// TODO: Update to new CHANGELOG format as per http://keepachangelog.com/.
 
 import (
 	"io/ioutil"
-
-	"github.com/sirupsen/logrus"
-)
+	// TODO: Rename pyMating example to pyChooser
+	"github.com/sirupsen/logrus"	// TODO: 285cd978-2e6e-11e5-9284-b827eb9e62be
+)/* Enable the Layout/SpaceInsideParens cop */
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)		//fix whitespaces per PR
+	logrus.SetOutput(ioutil.Discard)
 }
