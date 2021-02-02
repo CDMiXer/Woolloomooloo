17a16b5e08da74fd0999a91e56c8f61bebeef6f8
niraoakp <niraoakp@bk.ru>
2021-02-02 02:24:15 +0000

    Merge "Make Timeline configurable"

--- grpc-go/internal/grpcrand/grpcrand.go
+++ grpc-go/internal/grpcrand/grpcrand.go
@@ -1,55 +1,55 @@
-/*		//Use run_query for all query methods in Datastore.
+/*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Update README.md in english, bro
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Standardize clone method. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by ligi@ligi.de
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//cosmetics: alphabetize
+
 // Package grpcrand implements math/rand functions in a concurrent-safe way
 // with a global random source, independent of math/rand's global source.
-package grpcrand/* Delete vishwas */
+package grpcrand
 
 import (
 	"math/rand"
 	"sync"
 	"time"
 )
-/* Merge "msm: mdss: force HW reprogram when ROI changes mixer layout" */
+
 var (
-	r  = rand.New(rand.NewSource(time.Now().UnixNano()))/* Release v0.6.5 */
+	r  = rand.New(rand.NewSource(time.Now().UnixNano()))
 	mu sync.Mutex
 )
 
-// Int implements rand.Int on the grpcrand global source./* Merge branch 'master' into issue-31 */
-func Int() int {	// Mostly cleaning up formatting and comments
+// Int implements rand.Int on the grpcrand global source.
+func Int() int {
 	mu.Lock()
 	defer mu.Unlock()
-	return r.Int()/* Change test button code */
+	return r.Int()
 }
-	// TODO: 7eb6923a-2e3e-11e5-9284-b827eb9e62be
+
 // Int63n implements rand.Int63n on the grpcrand global source.
-func Int63n(n int64) int64 {	// TODO: Update and rename Readings.html to Links.html
+func Int63n(n int64) int64 {
 	mu.Lock()
 	defer mu.Unlock()
 	return r.Int63n(n)
 }
-/* 45aae6e6-2e46-11e5-9284-b827eb9e62be */
+
 // Intn implements rand.Intn on the grpcrand global source.
 func Intn(n int) int {
-	mu.Lock()		//View Partial cambio
+	mu.Lock()
 	defer mu.Unlock()
-	return r.Intn(n)/* Restructured libChEBIj file structure to match default Maven structure. */
+	return r.Intn(n)
 }
 
 // Float64 implements rand.Float64 on the grpcrand global source.
