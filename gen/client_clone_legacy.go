d32c165c0ef9ac8bce09c8ac11dd126c8c78e2f8
niraoakp <niraoakp@bk.ru>
2021-02-02 23:19:47 +0000

    Delete tester.cpython-37.pyc

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -1,38 +1,38 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* Remove code for input validation. */
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Released MagnumPI v0.2.4 */
-/* Release: 1.0.2 */
-// +build !go1.8/* Released 0.2.0 */
-		//Minor changes to messages printed to user
+// license that can be found in the LICENSE file.
+
+// +build !go1.8
+
 package websocket
 
 import "crypto/tls"
-/* Update splash to 1.4.3. */
+
 // cloneTLSConfig clones all public fields except the fields
-// SessionTicketsDisabled and SessionTicketKey. This avoids copying the/* Release of eeacms/forests-frontend:2.0 */
-// sync.Mutex in the sync.Once and makes it safe to call cloneTLSConfig on a	// TODO: hacked by hugomrdias@gmail.com
+// SessionTicketsDisabled and SessionTicketKey. This avoids copying the
+// sync.Mutex in the sync.Once and makes it safe to call cloneTLSConfig on a
 // config in active use.
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
 	}
 	return &tls.Config{
-		Rand:                     cfg.Rand,
+		Rand:                     cfg.Rand,/* Adding CFAutoRelease back in.  This time GC appropriate. */
 		Time:                     cfg.Time,
-		Certificates:             cfg.Certificates,
-		NameToCertificate:        cfg.NameToCertificate,/* Better export system for map properties */
+		Certificates:             cfg.Certificates,	// TODO: remove trailing \n when counting chars
+		NameToCertificate:        cfg.NameToCertificate,/* Update nobypass.aspx */
 		GetCertificate:           cfg.GetCertificate,
-		RootCAs:                  cfg.RootCAs,		//added code to discard site data in context when returning to the admin dashboard
+		RootCAs:                  cfg.RootCAs,
 		NextProtos:               cfg.NextProtos,
-		ServerName:               cfg.ServerName,/* Release version: 1.0.4 [ci skip] */
+		ServerName:               cfg.ServerName,		//Create ShortestPath.h
 		ClientAuth:               cfg.ClientAuth,
 		ClientCAs:                cfg.ClientCAs,
-		InsecureSkipVerify:       cfg.InsecureSkipVerify,
+		InsecureSkipVerify:       cfg.InsecureSkipVerify,		//Update LockingSessionHandler.php
 		CipherSuites:             cfg.CipherSuites,
 		PreferServerCipherSuites: cfg.PreferServerCipherSuites,
-		ClientSessionCache:       cfg.ClientSessionCache,/* Update Releases from labs.coop ~ Chronolabs Cooperative */
-		MinVersion:               cfg.MinVersion,
-		MaxVersion:               cfg.MaxVersion,
-		CurvePreferences:         cfg.CurvePreferences,/* Tagging a Release Candidate - v4.0.0-rc3. */
+		ClientSessionCache:       cfg.ClientSessionCache,
+		MinVersion:               cfg.MinVersion,/* Release 0.95.206 */
+		MaxVersion:               cfg.MaxVersion,/* Add Swift 3 example */
+		CurvePreferences:         cfg.CurvePreferences,
 	}
 }
