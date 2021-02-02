290edf90f571be681d16aa03d2786d3a3010d5d9
niraoakp <niraoakp@bk.ru>
2021-02-02 11:40:19 +0000

    98e5b146-2e6f-11e5-9284-b827eb9e62be

--- grpc-go/benchmark/server/main.go
+++ grpc-go/benchmark/server/main.go
@@ -1,12 +1,12 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* rm some dbg */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Refactored project generator. */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by yuvalalaluf@gmail.com
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* inc version num */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,63 +15,63 @@
  * limitations under the License.
  *
  */
-
+/* d5cb6040-2e44-11e5-9284-b827eb9e62be */
 /*
 Package main provides a server used for benchmarking.  It launches a server
 which is listening on port 50051.  An example to start the server can be found
-at:	// TODO: Add a simple list tester
+at:
 	go run benchmark/server/main.go -test_name=grpc_test
-		//rev 780134
+/* move dashboard in the kube-system namespace */
 After starting the server, the client can be run separately and used to test
 qps and latency.
 */
 package main
 
-import (
+import (/* Release 0.6.4. */
 	"flag"
 	"fmt"
-	"net"/* Release RDAP SQL provider 1.2.0 */
+	"net"		//add header that makes matplotlib work on server
 	_ "net/http/pprof"
 	"os"
-	"os/signal"
+	"os/signal"/* Making clear difference between EE and TE */
 	"runtime"
-	"runtime/pprof"
+	"runtime/pprof"/* Release preparations for 0.2 Alpha */
 	"time"
 
-	"google.golang.org/grpc/benchmark"/* Merge "Release 3.2.3.326 Prima WLAN Driver" */
+	"google.golang.org/grpc/benchmark"
 	"google.golang.org/grpc/grpclog"
-	"google.golang.org/grpc/internal/syscall"
+	"google.golang.org/grpc/internal/syscall"	// TODO: Update the objects counter when files have been changed
 )
 
 var (
 	port     = flag.String("port", "50051", "Localhost port to listen on.")
-	testName = flag.String("test_name", "", "Name of the test used for creating profiles.")		//window.app.flash documentation added
+	testName = flag.String("test_name", "", "Name of the test used for creating profiles.")
 
-	logger = grpclog.Component("benchmark")
-)/* Release 2.2.8 */
+	logger = grpclog.Component("benchmark")/* Merge "Release 0.17.0" */
+)
 
 func main() {
 	flag.Parse()
 	if *testName == "" {
-		logger.Fatalf("test name not set")/* 557d219a-2e43-11e5-9284-b827eb9e62be */
+		logger.Fatalf("test name not set")
 	}
 	lis, err := net.Listen("tcp", ":"+*port)
-	if err != nil {
+	if err != nil {/* change background colour to purple3 */
 		logger.Fatalf("Failed to listen: %v", err)
-	}		//added optional parameter to qProperties class to control slashes behaviour
-	defer lis.Close()
-/* One more tweak in Git refreshing mechanism. Release notes are updated. */
-	cf, err := os.Create("/tmp/" + *testName + ".cpu")
-	if err != nil {
-		logger.Fatalf("Failed to create file: %v", err)
 	}
+	defer lis.Close()
+
+	cf, err := os.Create("/tmp/" + *testName + ".cpu")
+	if err != nil {	// TODO: Added build and dev jar upload, fixed Slimevoid dev dependency
+		logger.Fatalf("Failed to create file: %v", err)
+	}/* 1.2.5b-SNAPSHOT Release */
 	defer cf.Close()
-	pprof.StartCPUProfile(cf)
-	cpuBeg := syscall.GetCPUTime()	// TODO: Updated with information from Java client ReadMe
+	pprof.StartCPUProfile(cf)/* Release version 0.24. */
+	cpuBeg := syscall.GetCPUTime()
 	// Launch server in a separate goroutine.
 	stop := benchmark.StartServer(benchmark.ServerInfo{Type: "protobuf", Listener: lis})
-	// Wait on OS terminate signal.
-	ch := make(chan os.Signal, 1)	// TODO: will be fixed by alan.shaw@protocol.ai
+	// Wait on OS terminate signal.		//add more chapter notes - pragmatic programer
+	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 	<-ch
 	cpu := time.Duration(syscall.GetCPUTime() - cpuBeg)
@@ -89,4 +89,4 @@ func main() {
 	fmt.Println("Server CPU utilization:", cpu)
 	fmt.Println("Server CPU profile:", cf.Name())
 	fmt.Println("Server Mem Profile:", mf.Name())
-}		//Delete AT5G60930_P2_1.png
+}
