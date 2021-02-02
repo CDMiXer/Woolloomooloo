8dea2140d8fd5e9d642c43a7a864ef059cae5f0d
niraoakp <niraoakp@bk.ru>
2021-02-02 15:42:46 +0000

    Ajout de math.ceil pour arrondi du diffFromToday

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* [artifactory-release] Release version 1.4.0.M2 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Bump SO version to 5 for last Log4jUdpAppender change. */
 package branches
