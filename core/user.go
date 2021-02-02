fd1952c60727ebe2f26d926576fea0045de41717
niraoakp <niraoakp@bk.ru>
2021-02-02 11:55:04 +0000

    Create appLayout.js

--- core/user.go
+++ core/user.go
@@ -7,38 +7,38 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* minor operations changes */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Changed workspace padding for small layout. */
+// limitations under the License.
 
 package core
 
-import (/* Update rectmode.js */
-	"context"/* Update ip2email.sh */
+import (
+	"context"
 	"errors"
 
 	"github.com/asaskevich/govalidator"
 )
-	// TODO: hacked by witek@enjin.io
+
 var (
-	errUsernameLen  = errors.New("Invalid username length")/* added ReleaseHandler */
+	errUsernameLen  = errors.New("Invalid username length")
 	errUsernameChar = errors.New("Invalid character in username")
-)	// Added a note about C++ (frolvlad/alpine-gxx) image
-/* v0.0.4 Release */
+)
+
 type (
 	// User represents a user of the system.
-	User struct {/* OpenNARS-1.6.3 Release Commit (Curiosity Parameter Adjustment) */
+	User struct {
 		ID        int64  `json:"id"`
-		Login     string `json:"login"`	// Offer Finshir
+		Login     string `json:"login"`
 		Email     string `json:"email"`
 		Machine   bool   `json:"machine"`
 		Admin     bool   `json:"admin"`
 		Active    bool   `json:"active"`
 		Avatar    string `json:"avatar"`
-		Syncing   bool   `json:"syncing"`/* Updated with compiled plugin */
+		Syncing   bool   `json:"syncing"`
 		Synced    int64  `json:"synced"`
-		Created   int64  `json:"created"`/* Delete MitoCirco5_Annotation.sh */
+		Created   int64  `json:"created"`
 		Updated   int64  `json:"updated"`
 		LastLogin int64  `json:"last_login"`
 		Token     string `json:"-"`
@@ -55,15 +55,15 @@ type (
 		// FindLogin returns a user from the datastore by username.
 		FindLogin(context.Context, string) (*User, error)
 
-		// FindToken returns a user from the datastore by token./* Release 1.2.2. */
+		// FindToken returns a user from the datastore by token.
 		FindToken(context.Context, string) (*User, error)
-/* Release '0.1~ppa17~loms~lucid'. */
-		// List returns a list of users from the datastore./* Merge branch 'DDB-NEXT-Multipolygone_byId' into develop */
+
+		// List returns a list of users from the datastore.
 		List(context.Context) ([]*User, error)
 
 		// Create persists a new user to the datastore.
 		Create(context.Context, *User) error
-	// TODO: a218b9d6-2e5e-11e5-9284-b827eb9e62be
+
 		// Update persists an updated user to the datastore.
 		Update(context.Context, *User) error
 
