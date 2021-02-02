f21270cdf44cdc928d5cfd5ba566d092aed777fa
niraoakp <niraoakp@bk.ru>
2021-02-02 22:46:47 +0000

    Fixed SMTPServer to SMTP

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* Release of eeacms/jenkins-slave-eea:3.18 */
+/* Create newsandupdate.css */
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+/* Release 1.6.1 */
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive(ComponentResource):		//sorts tidying and correct chipmunk positioning
+class ComponentFive(ComponentResource):	// 830d25d2-2e53-11e5-9284-b827eb9e62be
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFive", name, None, opts)
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))	// TODO: hacked by martin2cai@hotmail.com
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp5 = ComponentFive("comp5")
