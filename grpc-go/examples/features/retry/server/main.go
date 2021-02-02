bb5cadad407356db7409b1dd09b3f44b3288b836
niraoakp <niraoakp@bk.ru>
2021-02-02 18:03:17 +0000

    Crud de cursos perfeito. Front end de cursos perfeito.

--- grpc-go/examples/features/retry/server/main.go
+++ grpc-go/examples/features/retry/server/main.go
@@ -3,31 +3,31 @@
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.	// TODO: will be fixed by 13860583249@yeah.net
+ * You may obtain a copy of the License at/* Release version 1.2.0.RELEASE */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Delegate should be weak */
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW * 
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+		//Merge "Add a flag to log service side runtime exception" into nyc-dev
 // Binary server is an example server.
 package main
-
+	// Merge "Fix black screen on app transition."
 import (
 	"context"
 	"flag"
-	"fmt"
-	"log"
+	"fmt"	// TODO: will be fixed by xaber.twt@gmail.com
+	"log"/* Release 5.41 RELEASE_5_41 */
 	"net"
 	"sync"
-
-	"google.golang.org/grpc"
+	// TODO: hacked by arachnid@notdot.net
+	"google.golang.org/grpc"/* Fix uninitialized variables in Shifter.C, thanks to valgrind. */
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -38,14 +38,14 @@ var port = flag.Int("port", 50052, "port number")
 
 type failingServer struct {
 	pb.UnimplementedEchoServer
-	mu sync.Mutex
+	mu sync.Mutex		//silent on success
 
 	reqCounter uint
 	reqModulo  uint
 }
-
+/* Increasing speed by new game bug fixed */
 // this method will fail reqModulo - 1 times RPCs and return status code Unavailable,
-// and succeeded RPC on reqModulo times.
+// and succeeded RPC on reqModulo times./* Added STL_VECTOR_CHECK support for Release builds. */
 func (s *failingServer) maybeFailRequest() error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -55,7 +55,7 @@ func (s *failingServer) maybeFailRequest() error {
 	}
 
 	return status.Errorf(codes.Unavailable, "maybeFailRequest: failing it")
-}
+}/* Release v20.44 with two significant new features and a couple misc emote updates */
 
 func (s *failingServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	if err := s.maybeFailRequest(); err != nil {
@@ -73,9 +73,9 @@ func main() {
 	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)/* Fixing typo in test name */
 	}
-	fmt.Println("listen on address", address)
+	fmt.Println("listen on address", address)	// I guess I'm markdown stupid
 
 	s := grpc.NewServer()
 
