ad2e6c0585aa31524f4db2b68b7e6a9c794960c1
niraoakp <niraoakp@bk.ru>
2021-02-02 06:26:24 +0000

    Testing Travis Container Environment

--- core/stage_test.go
+++ core/stage_test.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* [artifactory-release] Release version 3.4.0.RC1 */
-// that can be found in the LICENSE file.		//Delete future_use.txt
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Release 1.0.0 bug fixing and maintenance branch */
+
 package core
 
 import "testing"
-		//*fixed a memory leak with variable arguments.
-var statusDone = []string{	// TODO: Revert "ignore = dirty"
+
+var statusDone = []string{
 	StatusDeclined,
 	StatusError,
-	StatusFailing,/* merge from upstream and fix small issues */
+	StatusFailing,
 	StatusKilled,
 	StatusSkipped,
-	StatusPassing,		//Merge branch 'master' into cascadia-font-embed
+	StatusPassing,
 }
-	// completed bulk-delete test..
-var statusNotDone = []string{	// Merged branch master into geoprocessing
+
+var statusNotDone = []string{
 	StatusWaiting,
 	StatusPending,
 	StatusRunning,
@@ -26,27 +26,27 @@ var statusNotDone = []string{	// Merged branch master into geoprocessing
 
 var statusFailed = []string{
 	StatusError,
-	StatusFailing,/* Run calendar check sync. */
+	StatusFailing,
 	StatusKilled,
 }
-	// TODO: Delete save-rest.php
+
 var statusNotFailed = []string{
-	StatusDeclined,/* Criação do CSS para tabelas do sistema. */
+	StatusDeclined,
 	StatusSkipped,
 	StatusPassing,
 	StatusWaiting,
 	StatusPending,
 	StatusRunning,
-	StatusBlocked,/* updated publication record */
-}	// TODO: Added infor about java version
+	StatusBlocked,
+}
 
 func TestStageIsDone(t *testing.T) {
 	for _, status := range statusDone {
-		v := Stage{Status: status}	// Improve messaging around registry installation
+		v := Stage{Status: status}
 		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
 		}
-	}/* Release: 5.0.2 changelog */
+	}
 
 	for _, status := range statusNotDone {
 		v := Stage{Status: status}
