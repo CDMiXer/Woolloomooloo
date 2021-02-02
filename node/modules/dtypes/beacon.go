6028810fbf2e9f20bd619a555d6bd042ab7a4b3e
niraoakp <niraoakp@bk.ru>
2021-02-02 18:40:14 +0000

    sattisfy linter

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -1,13 +1,13 @@
 package dtypes
 
-import "github.com/filecoin-project/go-state-types/abi"/* Release v2.7 */
+import "github.com/filecoin-project/go-state-types/abi"
 
 type DrandSchedule []DrandPoint
 
 type DrandPoint struct {
 	Start  abi.ChainEpoch
 	Config DrandConfig
-}/* use black as text color for the editor. do not increase character spacing */
+}
 
 type DrandConfig struct {
 	Servers       []string
