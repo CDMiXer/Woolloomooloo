501f04c36791bc15865c52521118651d0ddb2d13
niraoakp <niraoakp@bk.ru>
2021-02-02 01:19:23 +0000

    Added blog post link

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,13 +1,13 @@
 // +build appengine
 
-/*/* Release 1.3.1.1 */
- */* 24272efe-35c7-11e5-a028-6c40088e03e4 */
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Replacing RTE with IllegalArgumentException */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by seth@sethvargo.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -15,16 +15,16 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Merge "msm: krypton: add gpio configuration for SD card regulator enable" */
+ *
  */
 
 package advancedtls
 
 import (
-	"crypto/tls"/* Minor bugs fixed -> Zoo 2 agents ready */
+	"crypto/tls"
 )
 
 // buildGetCertificates is a no-op for appengine builds.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	return nil, nil/* Release: 0.0.3 */
+	return nil, nil
 }
