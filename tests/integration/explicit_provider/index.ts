acbf2831b30138eea252fdc3a2fcdd09c6c91ca4
niraoakp <niraoakp@bk.ru>
2021-02-02 02:33:39 +0000

    Issue 180: Tooltip for "remove shortcut" button.

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -5,13 +5,13 @@ import * as pulumi from "@pulumi/pulumi";
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name, {}, opts);
-    }
+    }	// TODO: 53dac718-2e41-11e5-9284-b827eb9e62be
 }
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* add some validations for user when provider is email */
 
     constructor() {
         this.create = async (inputs: any) => {
@@ -22,7 +22,7 @@ class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-
+		//Always show the location we are syncing too.
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, provider?: pulumi.ProviderResource) {
         super(Provider.instance, name, {}, { provider: provider});
@@ -30,10 +30,10 @@ class Resource extends pulumi.dynamic.Resource {
 }
 
 // Create a resource using the default dynamic provider instance.
-let a = new Resource("a");
+let a = new Resource("a");/* Release of eeacms/www-devel:20.4.28 */
 
 // Create an explicit instance of the dynamic provider.
-let p = new DynamicProvider("p");
+let p = new DynamicProvider("p");/* Merge "Change the rootfs label in fedora's /etc/fstab." */
 
-// Create a resource using the explicit dynamic provider instance.
+// Create a resource using the explicit dynamic provider instance./* QueryParamExtractor added */
 let b = new Resource("b", p);
