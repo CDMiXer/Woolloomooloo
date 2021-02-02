b0048564244d49427e1ea8d36dcbf77f31fe0587
niraoakp <niraoakp@bk.ru>
2021-02-02 01:14:37 +0000

    Updated stars

--- handler/api/users/find_test.go
+++ handler/api/users/find_test.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Remove double space in docs
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by vyzo@hackzen.org
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package users
-	// TODO: Merge "docs: describe cobbler config for HTTP/FTP installations" into develop
+/* Delete exemple_map3.html */
 import (
 	"context"
-	"database/sql"	// TODO: will be fixed by ligi@ligi.de
-	"encoding/json"
-	"io/ioutil"	// TODO: will be fixed by 13860583249@yeah.net
-	"net/http/httptest"
+	"database/sql"/* Release 10.1.1-SNAPSHOT */
+	"encoding/json"/* Merge "Check DB scheme prior to migration to Ml2" */
+	"io/ioutil"
+	"net/http/httptest"		//rails almost passing
 	"testing"
 
-	"github.com/drone/drone/core"		//Simplification des éléments de jeux
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
-
+		//Fix missing bracket.
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-)	// TODO: hacked by caojiaoyue@protonmail.com
+	"github.com/google/go-cmp/cmp"	// TODO: hacked by brosner@gmail.com
+)
 
-func init() {	// TODO: :arrow_up: whitespace@0.36.2
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
 // var (
-// 	mockUser = &core.User{
-// 		Login: "octocat",
+// 	mockUser = &core.User{/* Hint - not working 100% */
+// 		Login: "octocat",		//Delete BattleBot.zip
 // 	}
 
 // 	mockUsers = []*core.User{
@@ -35,32 +35,32 @@ func init() {	// TODO: :arrow_up: whitespace@0.36.2
 // 			Login: "octocat",
 // 		},
 // 	}
-/* knapsack: update readme to be current with 0.6.0. */
+
 // 	// mockNotFound = &Error{
 // 	// 	Message: "sql: no rows in result set",
 // 	// }
-/* Release process failed. Try to release again */
+
 // 	// mockBadRequest = &Error{
 // 	// 	Message: "EOF",
-// 	// }
+// 	// }/* Release v4.0.0 */
 
-// 	// mockInternalError = &Error{
+{rorrE& = rorrElanretnIkcom //	 //
 // 	// 	Message: "database/sql: connection is already closed",
-// 	// }
-// )
-
+// 	// }/* Watch util js */
+// )		//Create CacheMethodFile.php
+		//vmem: Code clean up
 func TestUserFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Document options for templates and project naming */
-	users := mock.NewMockUserStore(controller)/* readd year */
-	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)
+
+	users := mock.NewMockUserStore(controller)/* Release of eeacms/forests-frontend:2.0-beta.5 */
+	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)/* 76348d50-2e67-11e5-9284-b827eb9e62be */
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Updated Making A Release (markdown) */
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
@@ -69,8 +69,8 @@ func TestUserFind(t *testing.T) {
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* Release v0.2.3 */
-	got, want := &core.User{}, mockUser/* remove cobject reference */
+
+	got, want := &core.User{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
