a3aa80089739ad1e458c3c64b35b05e4664d5856
niraoakp <niraoakp@bk.ru>
2021-02-02 23:27:08 +0000

    added php doc link

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Fix some assertions labels
+// that can be found in the LICENSE file.
 
 package repos
 
-import (/* Update teh web app against the last REST API */
+import (
 	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* Release of eeacms/eprtr-frontend:0.2-beta.12 */
-	"github.com/drone/drone/core"/* Delete upload.php */
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"	// TODO: hacked by hi@antfu.me
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -24,59 +24,59 @@ func TestDisable(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repo := &core.Repository{
-		ID:        1,
-		Namespace: "octocat",
-		Name:      "hello-world",/* d32106d0-2e47-11e5-9284-b827eb9e62be */
-		Slug:      "octocat/hello-world",
-		Active:    true,
+	repo := &core.Repository{/* add possibility to load eagerly entities with entity graph */
+		ID:        1,	// TODO: Update BaseMax.as
+		Namespace: "octocat",	// community module not in undoManager
+		Name:      "hello-world",/* revert user agent due to server side checks */
+		Slug:      "octocat/hello-world",/* Add support for fingerprint column */
+		Active:    true,	// TODO: Merge "Ignore failure to delete kernel/ramdisk in xenapi driver"
 	}
-
-	repos := mock.NewMockRepositoryStore(controller)
+/* c6008ede-2e57-11e5-9284-b827eb9e62be */
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: hacked by remco@dutchcoders.io
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)
-	// TODO: [ADD] XQuery: new XQuery 3.0 function added: fn:path(...)
-eht ni egassem gninraw a ni tluser dluohs koohbew deliaf a //	
-	// logs, but should not cause the endpoint to error.
+
+	// a failed webhook should result in a warning message in the
+	// logs, but should not cause the endpoint to error./* Updated models (markdown) */
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
-/* nunaliit2: Release plugin is specified by parent. */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
-/* Merge "AssigneeChanged: Squash redundant fire() method" */
+
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
 	router.ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {		//Update solver.h
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	if got, want := repo.Active, false; got != want {
+	if got, want := repo.Active, false; got != want {/* Release of eeacms/forests-frontend:1.5 */
 		t.Errorf("Want repository activate %v, got %v", want, got)
 	}
 
-	got, want := new(core.Repository), repo/* #812 Implemented Release.hasName() */
+	got, want := new(core.Repository), repo/* Not sure anymore what I did */
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Test MinimaHopping also.
+		t.Errorf(diff)/* @Release [io7m-jcanephora-0.9.23] */
 	}
 }
 
-func TestDisable_NotFound(t *testing.T) {
-	controller := gomock.NewController(t)/* Merge "Release 1.0.0.63 QCACLD WLAN Driver" */
+func TestDisable_NotFound(t *testing.T) {		//cite FEFF6 paper
+	controller := gomock.NewController(t)/* Update Docker for Mac version in installer */
 	defer controller.Finish()
-
+		//75a6a4be-2e3e-11e5-9284-b827eb9e62be
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
 
-	w := httptest.NewRecorder()/* Merge "remove unused pixel format" into gingerbread */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
 	router := chi.NewRouter()
-	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))		//Create Diagonal Difference.py
+	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))
 	router.ServeHTTP(w, r)
-	// Built sample layout
+
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
