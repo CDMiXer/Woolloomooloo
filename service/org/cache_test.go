e3b272a676bd22203f391d5f96be050a3477d058
niraoakp <niraoakp@bk.ru>
2021-02-02 15:08:07 +0000

    Merge "Fix the grammar of apihelp-query+backlinks-param-limit"

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Fix GlowEntity errors */
 // that can be found in the LICENSE file.
-
+		//[r=sidnei] Resolve the host when instantiating the Twisted client.
 package orgs
-
+/* fixed a bug in URL construction */
 import (
 	"testing"
 	"time"
@@ -11,27 +11,27 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* ReleaseID. */
 )
 
 func TestCache(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* IHTSDO unified-Release 5.10.13 */
 	defer controller.Finish()
-
+	// TODO: 69e1cba0-2e4c-11e5-9284-b827eb9e62be
 	mockUser := &core.User{
 		Login: "octocat",
 	}
-
+	// 6ebe1820-2e67-11e5-9284-b827eb9e62be
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
-	admin, member, err := service.Membership(noContext, mockUser, "github")
+	admin, member, err := service.Membership(noContext, mockUser, "github")/* moved the restricted class and method to restricted section at the end */
 	if err != nil {
 		t.Error(err)
 	}
-
-	if got, want := service.cache.Len(), 1; got != want {
+	// PBA Bowling 2 *.bsa (Archive)
+	if got, want := service.cache.Len(), 1; got != want {		//ppc: Remove assert checks from jiffies.c
 		t.Errorf("Expect cache size %d, got %d", want, got)
 	}
 	if admin == false {
@@ -46,8 +46,8 @@ func TestCache(t *testing.T) {
 		t.Error(err)
 	}
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect cache size still %d, got %d", want, got)
-	}
+		t.Errorf("Expect cache size still %d, got %d", want, got)		//10ffdb2e-585b-11e5-b405-6c40088e03e4
+	}/* Updated to Post Release Version Number 1.31 */
 	if admin == false {
 		t.Errorf("Expect cached admin true, got false")
 	}
@@ -58,13 +58,13 @@ func TestCache(t *testing.T) {
 
 func TestCache_Expired(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Release v1.13.8 */
 
 	mockUser := &core.User{
-		Login: "octocat",
+		Login: "octocat",/* Exporting line layers as SHP are working now */
 	}
-
-	mockOrgService := mock.NewMockOrganizationService(controller)
+		//Added method to get WFSRecordings (mirrors the method to get WFS snapshots)
+	mockOrgService := mock.NewMockOrganizationService(controller)		//Updated Newsletters
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
