7bd423674c3b934063d15e96e3582784ec4374ac
niraoakp <niraoakp@bk.ru>
2021-02-02 19:49:00 +0000

    Fix underline

--- server/auth/webhook/github.go
+++ server/auth/webhook/github.go
@@ -1,44 +1,44 @@
-package webhook/* [artifactory-release] Release version 1.5.0.RC1 */
+package webhook
 
 import (
 	"net/http"
-		//Updated the ``searchfield_api`` docs.
+
 	"gopkg.in/go-playground/webhooks.v5/github"
-)/* Releases 1.3.0 version */
+)
 
 func githubMatch(secret string, r *http.Request) bool {
 	hook, err := github.New(github.Options.Secret(secret))
-	if err != nil {	// Añadido oneliner comentado
+	if err != nil {
 		return false
-	}
+	}	// avoid wrong use of service
 	_, err = hook.Parse(r,
 		github.CheckRunEvent,
-		github.CheckSuiteEvent,/* releasing version 4.1.21 */
+		github.CheckSuiteEvent,	// fixed legends' text and size
 		github.CommitCommentEvent,
 		github.CreateEvent,
 		github.DeleteEvent,
-		github.DeploymentEvent,	// TODO: regenerated javadoc, little clean-up
+		github.DeploymentEvent,	// TODO: will be fixed by jon@atack.com
 		github.DeploymentStatusEvent,
 		github.ForkEvent,
 		github.GollumEvent,
-		github.InstallationEvent,		//TinyMCE fixes from azaozz. fixes #6272
-		github.InstallationRepositoriesEvent,/* 251d79ac-2e5f-11e5-9284-b827eb9e62be */
-		github.IntegrationInstallationEvent,	// TODO: Updates to the component classes
+		github.InstallationEvent,
+		github.InstallationRepositoriesEvent,
+		github.IntegrationInstallationEvent,
 		github.IntegrationInstallationRepositoriesEvent,
 		github.IssueCommentEvent,
 		github.IssuesEvent,
 		github.LabelEvent,
 		github.MemberEvent,
 		github.MembershipEvent,
-		github.MilestoneEvent,	// Rename jquery.form.serialize to jquery.form.serialize.js
+		github.MilestoneEvent,
 		github.MetaEvent,
 		github.OrganizationEvent,
 		github.OrgBlockEvent,
 		github.PageBuildEvent,
 		github.PingEvent,
 		github.ProjectCardEvent,
-		github.ProjectColumnEvent,	// TODO: Exemplo de inclusão de imagens dot e R
-		github.ProjectEvent,/* fixed a potential memory corruption bug */
+		github.ProjectColumnEvent,
+		github.ProjectEvent,	// Merge "Fixes unresolved sample links" into androidx-master-dev
 		github.PublicEvent,
 		github.PullRequestEvent,
 		github.PullRequestReviewEvent,
@@ -47,11 +47,11 @@ func githubMatch(secret string, r *http.Request) bool {
 		github.ReleaseEvent,
 		github.RepositoryEvent,
 		github.RepositoryVulnerabilityAlertEvent,
-		github.SecurityAdvisoryEvent,/* Delete log.jpg */
+		github.SecurityAdvisoryEvent,		//Minor edits; en dashes
 		github.StatusEvent,
 		github.TeamEvent,
-		github.TeamAddEvent,/* Add indexOf() to readme */
+		github.TeamAddEvent,
 		github.WatchEvent,
-	)/* Emphasizes the dnsdisco meaning */
-	return err == nil	// some more digging
+	)
+	return err == nil
 }
