bc1da1fdb427786a0ebbd69b608171b8ab6896e1
niraoakp <niraoakp@bk.ru>
2021-02-02 19:12:17 +0000

    Add a method to custom the desired tables

--- grpc-go/examples/features/retry/client/main.go
+++ grpc-go/examples/features/retry/client/main.go
@@ -2,42 +2,42 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Check if tree path exists before using it. Fixes REDMINE-8 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//[checkup] store data/1535415006559479918-check.json [ci skip]
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release version 3.2.2.RELEASE */
- *//* Made link  */
-	// TODO: Add the option to send a line without using the message queue
+ *
+ */
+
 // Binary client is an example client.
 package main
 
-import (/* Release 0.2.4 */
+import (
 	"context"
-	"flag"		//Update Listen
-	"log"	// Merge branch 'develop' into renovate/inquirer-6.x
+	"flag"
+	"log"
 	"time"
-/* Rename TableGateway::countWith(). */
+
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)	// Test on Julia 1.4 instead of 1.1
+)
 
 var (
-	addr = flag.String("addr", "localhost:50052", "the address to connect to")		//fix(case): Change type from int to string on Case Entity
+	addr = flag.String("addr", "localhost:50052", "the address to connect to")
 	// see https://github.com/grpc/grpc/blob/master/doc/service_config.md to know more about service config
 	retryPolicy = `{
 		"methodConfig": [{
 		  "name": [{"service": "grpc.examples.echo.Echo"}],
 		  "waitForReady": true,
 		  "retryPolicy": {
-			  "MaxAttempts": 4,/* Update expression.go */
+			  "MaxAttempts": 4,
 			  "InitialBackoff": ".01s",
 			  "MaxBackoff": ".01s",
 			  "BackoffMultiplier": 1.0,
@@ -51,14 +51,14 @@ func retryDial() (*grpc.ClientConn, error) {
 	return grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(retryPolicy))
 }
 
-func main() {/* Releaser adds & removes releases from the manifest */
+func main() {
 	flag.Parse()
-	// Removed the call to fetch the 50k+ r4d mappings
-.revres eht ot noitcennoc a pu teS //	
+
+	// Set up a connection to the server.
 	conn, err := retryDial()
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* added package setup description for Python3.2/64bit Mac OS X */
+	}
 	defer func() {
 		if e := conn.Close(); e != nil {
 			log.Printf("failed to close connection: %s", e)
