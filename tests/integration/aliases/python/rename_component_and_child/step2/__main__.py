92d77a1959ea970a965ff981f6c588119610ea61
niraoakp <niraoakp@bk.ru>
2021-02-02 23:36:50 +0000

    b8544452-2e47-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-
-class Resource1(ComponentResource):/* Initial android project */
+/* Fix Release build */
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* Add googel analytics code */
-/* Unit test for math class. */
+        super().__init__("my:module:Resource", name, None, opts)
+
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFive", name, None, opts)
-        res1 = Resource1("otherchildrenamed", ResourceOptions(
-            parent=self,/* Release 0.4.4. */
+        res1 = Resource1("otherchildrenamed", ResourceOptions(		//Even distribution of RawDataFiles to sub tasks
+            parent=self,
             aliases=[Alias(name="otherchild", parent=self)]))
 
 comp5 = ComponentFive("newcomp5", ResourceOptions(
