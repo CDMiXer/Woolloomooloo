21db9dbebc55b74057104bfee024abcecae5e7dd
niraoakp <niraoakp@bk.ru>
2021-02-02 09:07:53 +0000

    Rewrite full javascript state update code to use the builder

--- store/shared/migrate/sqlite/files/004_create_table_builds.sql
+++ store/shared/migrate/sqlite/files/004_create_table_builds.sql
@@ -1,49 +1,49 @@
--- name: create-table-builds/* update reference mobil */
-
+-- name: create-table-builds	// TODO: will be fixed by alan.shaw@protocol.ai
+		//Create test5.Rmd
 CREATE TABLE IF NOT EXISTS builds (
- build_id            INTEGER PRIMARY KEY AUTOINCREMENT/* [IMP] account: Improved general and partner ledger reports for currency */
+ build_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,build_repo_id       INTEGER
-,build_trigger       TEXT
+,build_trigger       TEXT	// TODO: hacked by witek@enjin.io
 ,build_number        INTEGER
-,build_parent        INTEGER
+,build_parent        INTEGER		//template importation synchronized
 ,build_status        TEXT
-,build_error         TEXT/* Release notes for 1.0.1 */
+,build_error         TEXT
 ,build_event         TEXT
 ,build_action        TEXT
-,build_link          TEXT/* Função nova: zzfilme - Pesquisa informações sobre filmes. */
+,build_link          TEXT
 ,build_timestamp     INTEGER
 ,build_title         TEXT
-,build_message       TEXT
+,build_message       TEXT		//Added package I needed to install necessary gems
 ,build_before        TEXT
 ,build_after         TEXT
 ,build_ref           TEXT
 ,build_source_repo   TEXT
 ,build_source        TEXT
 ,build_target        TEXT
-,build_author        TEXT/* Release Version 12 */
+,build_author        TEXT/* Remove regression detection as it's now part of peril */
 ,build_author_name   TEXT
-,build_author_email  TEXT	// TODO: Jon Adopted! 💗
-,build_author_avatar TEXT/* Merge "Improve safeGetLag() return docs" */
-,build_sender        TEXT
+,build_author_email  TEXT
+,build_author_avatar TEXT
+,build_sender        TEXT		//Disabling cookie setting.
 ,build_deploy        TEXT
 ,build_params        TEXT
 ,build_started       INTEGER
 ,build_finished      INTEGER
 ,build_created       INTEGER
-,build_updated       INTEGER	// TODO: Add STM32F7-chipVectors.cpp
+,build_updated       INTEGER
 ,build_version       INTEGER
-,UNIQUE(build_repo_id, build_number)/* Release 1.9.31 */
+,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-builds-repo/* Added Release notes to docs */
+-- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-
+/* 2.0 Release preperations */
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
-	// TODO: hacked by indexxuan@gmail.com
+
 -- name: create-index-builds-sender
 
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
@@ -52,7 +52,7 @@ CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
 CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
 
--- name: create-index-build-incomplete
+-- name: create-index-build-incomplete/* Merge "diag: Release wake source in case for write failure" */
 
 CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
