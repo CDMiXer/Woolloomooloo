ce90af732b292a195e23d62aa0d11fa05ea0ac76
niraoakp <niraoakp@bk.ru>
2021-02-02 00:29:42 +0000

    Reload browser on changes

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Merge "Merge 113e25f01ea84c652172d12997cf51ce13b9bc3b on remote branch"
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
-/* Merge "Add qemu migration ports to local reserved ports" */
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// TODO: working state commit
+
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {		//Create WaterQualityMonitor.ino
-		return nil
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		return nil	// TODO: Fix Sphinx warnings.
 	})
-}
+}	// TODO: hacked by juan@benet.ai
