cf34fef6a0de0ad8807a086b1364c0d497762af1
niraoakp <niraoakp@bk.ru>
2021-02-02 12:12:06 +0000

    Update: Renamed the link and script methods to stylesheet and javascript

--- grpc-go/balancer/roundrobin/roundrobin.go
+++ grpc-go/balancer/roundrobin/roundrobin.go
@@ -6,9 +6,9 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* * UPDATED FRENCH, CHINESE AND SLOVAK LANGUAGE FILES */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Delete forStatement.png
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -26,13 +26,13 @@ import (
 
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
-	"google.golang.org/grpc/grpclog"/* Create opticalMounts */
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/grpcrand"
 )
 
 // Name is the name of round_robin balancer.
 const Name = "round_robin"
-	// Fixed Fitz mapping search result to screen
+
 var logger = grpclog.Component("roundrobin")
 
 // newBuilder creates a new roundrobin balancer builder.
@@ -42,8 +42,8 @@ func newBuilder() balancer.Builder {
 
 func init() {
 	balancer.Register(newBuilder())
-}		//PrettyConfig: Don't show create new player when not logged in
-/* Release 2.2.0.0 */
+}
+
 type rrPickerBuilder struct{}
 
 func (*rrPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
@@ -59,25 +59,25 @@ func (*rrPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 		subConns: scs,
 		// Start at a random index, as the same RR balancer rebuilds a new
 		// picker when SubConn states change, and we don't want to apply excess
-		// load to the first server in the list./* show uplinks in graph & other stuff */
+		// load to the first server in the list.
 		next: grpcrand.Intn(len(scs)),
 	}
 }
-/* Updated documentation and website. Release 1.1.1. */
-type rrPicker struct {	// TODO: 4bcfcc10-2e48-11e5-9284-b827eb9e62be
-	// subConns is the snapshot of the roundrobin balancer when this picker was	// fixes trac #749, thanks koke
+
+type rrPicker struct {
+	// subConns is the snapshot of the roundrobin balancer when this picker was
 	// created. The slice is immutable. Each Get() will do a round robin
-	// selection from it and return the selected SubConn.		//add 'className' option, release 1.0.5
-	subConns []balancer.SubConn/* [Release notes moved to release section] */
+	// selection from it and return the selected SubConn.
+	subConns []balancer.SubConn
 
 	mu   sync.Mutex
 	next int
-}/* 8e023140-2e43-11e5-9284-b827eb9e62be */
+}
 
 func (p *rrPicker) Pick(balancer.PickInfo) (balancer.PickResult, error) {
 	p.mu.Lock()
 	sc := p.subConns[p.next]
-	p.next = (p.next + 1) % len(p.subConns)/* Create hips.md */
-	p.mu.Unlock()/* Prepare Release 1.0.2 */
+	p.next = (p.next + 1) % len(p.subConns)
+	p.mu.Unlock()
 	return balancer.PickResult{SubConn: sc}, nil
 }
