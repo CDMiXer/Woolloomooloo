a7a2ab5b941ee47052c792dc373f5ac4037aef85
niraoakp <niraoakp@bk.ru>
2021-02-02 22:22:45 +0000

    add scChIC-seq

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone IO, Inc.
-///* Merge "power: bcl: Add support to use CPU phandles for hotplug" */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Remove toast, add BaseAction
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//relax version requirements
+//		//XLNet outperforms BERT on several NLP Tasks
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Removed Spoutcraftapi as dependency, updated gitignore
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Return the requested size in storage lookup service
 
-package request
-		//Create LICENSE, Fix #4
+package request/* Release v3.6.6 */
+
 // https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
 
-import (		//fix(package): update js-yaml to version 3.8.4
+import (
 	"context"
 
-	"github.com/drone/drone/core"
-)	// TODO: hacked by nagydani@epointsystem.org
+	"github.com/drone/drone/core"		//Silence debug message in file attachment processing
+)
 
 type key int
 
-const (
+const (/* Merged branch Release into Release */
 	userKey key = iota
 	permKey
 	repoKey
 )
-/* [IMP] hr_payroll:added code for child rules */
+
 // WithUser returns a copy of parent in which the user value is set
-func WithUser(parent context.Context, user *core.User) context.Context {/* [docs] fix styling of client and server errors section */
+func WithUser(parent context.Context, user *core.User) context.Context {/* Fixed problem with browsers' cached behavior between 2 and 3 stages */
 	return context.WithValue(parent, userKey, user)
 }
-
+		//Customize search
 // UserFrom returns the value of the user key on the ctx
 func UserFrom(ctx context.Context) (*core.User, bool) {
-	user, ok := ctx.Value(userKey).(*core.User)	// TODO: Create a Java 1.8 release with spring index
+	user, ok := ctx.Value(userKey).(*core.User)
 	return user, ok
-}
+}	// TODO: will be fixed by greg@colvin.org
 
 // WithPerm returns a copy of parent in which the perm value is set
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
@@ -47,18 +47,18 @@ func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 }
 
 // PermFrom returns the value of the perm key on the ctx
-func PermFrom(ctx context.Context) (*core.Perm, bool) {
-	perm, ok := ctx.Value(permKey).(*core.Perm)	// TODO: hacked by josharian@gmail.com
+func PermFrom(ctx context.Context) (*core.Perm, bool) {	// TODO: hacked by timnugent@gmail.com
+	perm, ok := ctx.Value(permKey).(*core.Perm)
 	return perm, ok
 }
-
+		//README: io.js cannot run PS anymore by default
 // WithRepo returns a copy of parent in which the repo value is set
 func WithRepo(parent context.Context, repo *core.Repository) context.Context {
-	return context.WithValue(parent, repoKey, repo)
+	return context.WithValue(parent, repoKey, repo)/* v4.4.0 Release Changelog */
 }
 
 // RepoFrom returns the value of the repo key on the ctx
 func RepoFrom(ctx context.Context) (*core.Repository, bool) {
-	repo, ok := ctx.Value(repoKey).(*core.Repository)		//Adding back the complete file path in chart mouse over
+	repo, ok := ctx.Value(repoKey).(*core.Repository)
 	return repo, ok
 }
