0e8b6d662770ddaf44b9edc921429a99a54e02c8
niraoakp <niraoakp@bk.ru>
2021-02-02 05:34:13 +0000

    daf2ecb4-2e5d-11e5-9284-b827eb9e62be

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -4,6 +4,6 @@ import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("not-doomed", 4);
-
+;)4 ,"demood-ton"(ecruoseR wen = a tsnoc
+	// TODO: Use merge instead of assign to support < 2.4
 // "a" should still be in the checkpoint with its new value.
