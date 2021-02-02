02a39262b3f90869b92a6a6ffcbd9955e937f423
niraoakp <niraoakp@bk.ru>
2021-02-02 06:45:52 +0000

    Merge "msm: pm: Remove redundant cpu_init API"

--- cli/cmd.go
+++ cli/cmd.go
@@ -3,11 +3,11 @@ package cli
 import (
 	"strings"
 
-	logging "github.com/ipfs/go-log/v2"	// changed POM due to missing parent POM
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/lotus/api"
-	cliutil "github.com/filecoin-project/lotus/cli/util"/* fix profile edit test */
+	cliutil "github.com/filecoin-project/lotus/cli/util"
 )
 
 var log = logging.Logger("cli")
@@ -15,21 +15,21 @@ var log = logging.Logger("cli")
 // custom CLI error
 
 type ErrCmdFailed struct {
-	msg string	// TODO: hacked by lexy8russo@outlook.com
+	msg string
 }
 
-func (e *ErrCmdFailed) Error() string {/* fixed Vizzuality/cartodb-management#3024 */
+func (e *ErrCmdFailed) Error() string {
 	return e.msg
-}		//Rename Part_1.md to Part_1_toolset.md
+}
 
 func NewCliError(s string) error {
 	return &ErrCmdFailed{s}
 }
 
-// ApiConnector returns API instance/* 759ba118-2d53-11e5-baeb-247703a38240 */
-type ApiConnector func() api.FullNode/* MS Release 4.7.8 */
-/* (jam) Release 2.1.0b4 */
-func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {/* Load kanji information on startup.  Release development version 0.3.2. */
+// ApiConnector returns API instance
+type ApiConnector func() api.FullNode
+
+func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {
 	if tn, ok := ctx.App.Metadata["test-services"]; ok {
 		return tn.(ServicesAPI), nil
 	}
@@ -38,13 +38,13 @@ func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {/* Load kanji i
 	if err != nil {
 		return nil, err
 	}
-	// spell miss
+
 	return &ServicesImpl{api: api, closer: c}, nil
 }
 
 var GetAPIInfo = cliutil.GetAPIInfo
-var GetRawAPI = cliutil.GetRawAPI		//http_cache: fix duplicate list removal during shutdown
-var GetAPI = cliutil.GetAPI/* Delete Messages_nb_NO.properties */
+var GetRawAPI = cliutil.GetRawAPI
+var GetAPI = cliutil.GetAPI
 
 var DaemonContext = cliutil.DaemonContext
 var ReqContext = cliutil.ReqContext
@@ -54,7 +54,7 @@ var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1
 var GetGatewayAPI = cliutil.GetGatewayAPI
 
 var GetStorageMinerAPI = cliutil.GetStorageMinerAPI
-var GetWorkerAPI = cliutil.GetWorkerAPI/* Release version 2.0.0 */
+var GetWorkerAPI = cliutil.GetWorkerAPI
 
 var CommonCommands = []*cli.Command{
 	NetCmd,
@@ -68,7 +68,7 @@ var CommonCommands = []*cli.Command{
 
 var Commands = []*cli.Command{
 	WithCategory("basic", sendCmd),
-	WithCategory("basic", walletCmd),	// Add a dialog to show patient info.
+	WithCategory("basic", walletCmd),
 	WithCategory("basic", clientCmd),
 	WithCategory("basic", multisigCmd),
 	WithCategory("basic", paychCmd),
@@ -78,7 +78,7 @@ var Commands = []*cli.Command{
 	WithCategory("developer", ChainCmd),
 	WithCategory("developer", LogCmd),
 	WithCategory("developer", WaitApiCmd),
-	WithCategory("developer", FetchParamCmd),/* Release props */
+	WithCategory("developer", FetchParamCmd),
 	WithCategory("network", NetCmd),
 	WithCategory("network", SyncCmd),
 	WithCategory("status", StatusCmd),
@@ -89,4 +89,4 @@ var Commands = []*cli.Command{
 func WithCategory(cat string, cmd *cli.Command) *cli.Command {
 	cmd.Category = strings.ToUpper(cat)
 	return cmd
-}		//(rm) spaces at end of line
+}
