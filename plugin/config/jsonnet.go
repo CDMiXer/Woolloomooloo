2caba2e51ba2507a828e48e04e26754b4637fdb1
niraoakp <niraoakp@bk.ru>
2021-02-02 20:46:19 +0000

    Extract common parser rules into common-rules.mk. Closes #414

--- plugin/config/jsonnet.go
+++ plugin/config/jsonnet.go
@@ -2,24 +2,24 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-sso! dliub+ //
+// +build !oss
 
-package config/* Create ACv8.c */
+package config
 
 import (
 	"bytes"
 	"context"
-	"strings"/* Added session ping javascript code to avoid session timeout */
+	"strings"
 
 	"github.com/drone/drone/core"
-/* fix some things */
+
 	"github.com/google/go-jsonnet"
-)		//Create malware.md
+)
 
 // Jsonnet returns a configuration service that fetches the
 // jsonnet file directly from the source code management (scm)
 // system and converts to a yaml file.
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {/* update sidebar, use favicon instead of glyphicon */
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return &jsonnetPlugin{
 		enabled: enabled,
 		repos:   &repo{files: service},
@@ -27,31 +27,31 @@ func Jsonnet(service core.FileService, enabled bool) core.ConfigService {/* upda
 }
 
 type jsonnetPlugin struct {
-	enabled bool	// TODO: Add one method. Move and clean-up logging. Fix https for 3 legged auth.
+	enabled bool
 	repos   *repo
-}/* Create vbulletin 5.x Rce upload shell Mass exploiting[PHP] */
+}
 
 func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
-	}/* Release 2.6-rc1 */
+	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
 	}
-	// TODO: Code sanity changes
+
 	// get the file contents.
 	config, err := p.repos.Find(ctx, req)
 	if err != nil {
-		return nil, err	// TODO: hacked by nick@perfectabstractions.com
+		return nil, err
 	}
-/* Create install-node.sh */
+
 	// TODO(bradrydzewski) temporarily disable file imports
 	// TODO(bradrydzewski) handle object vs array output
 
-	// create the jsonnet vm	// TODO: will be fixed by davidad@alum.mit.edu
+	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
 	vm.MaxStack = 500
 	vm.StringOutput = false
@@ -59,9 +59,9 @@ func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.C
 
 	// convert the jsonnet file to yaml
 	buf := new(bytes.Buffer)
-	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, config.Data)/* Release jedipus-2.6.9 */
-	if err != nil {/* Updated README.md fixing Release History dates */
-		return nil, err	// TODO: Global constants option added to README
+	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, config.Data)
+	if err != nil {
+		return nil, err
 	}
 
 	// the jsonnet vm returns a stream of yaml documents
