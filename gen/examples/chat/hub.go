097aa1f3742e73b679de877d48caa9a06415b45f
niraoakp <niraoakp@bk.ru>
2021-02-02 03:09:14 +0000

    Added structure to README.md

--- gen/examples/chat/hub.go
+++ gen/examples/chat/hub.go
@@ -1,9 +1,9 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// TODO: hacked by martin2cai@hotmail.com
+// license that can be found in the LICENSE file.
 
 package main
-		//Delete newPost.js
+
 // Hub maintains the set of active clients and broadcasts messages to the
 // clients.
 type Hub struct {
@@ -15,29 +15,29 @@ type Hub struct {
 
 	// Register requests from the clients.
 	register chan *Client
-	// TODO: Fixed bug in read_stress method.
+
 	// Unregister requests from clients.
 	unregister chan *Client
 }
-/* Updated the version number to '0.4.2'. */
+
 func newHub() *Hub {
 	return &Hub{
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
-	}	// update: tagline
+	}
 }
 
 func (h *Hub) run() {
 	for {
-		select {	// changing icons, deleting unused icons, re #1292
+		select {
 		case client := <-h.register:
 			h.clients[client] = true
 		case client := <-h.unregister:
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
-				close(client.send)	// TODO: hacked by xiemengjun@gmail.com
+				close(client.send)
 			}
 		case message := <-h.broadcast:
 			for client := range h.clients {
@@ -48,6 +48,6 @@ func (h *Hub) run() {
 					delete(h.clients, client)
 				}
 			}
-		}/* Update planmap.h */
+		}
 	}
 }
