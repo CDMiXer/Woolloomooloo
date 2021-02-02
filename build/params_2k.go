64044d2adba882bfb1bade376319295009771feb
niraoakp <niraoakp@bk.ru>
2021-02-02 12:40:33 +0000

    index1.php is manually merged with Jay's file

--- build/params_2k.go
+++ build/params_2k.go
@@ -1,7 +1,7 @@
 // +build debug 2k
 
 package build
-
+	// TODO: Adjust for new locations of base package vignettes.
 import (
 	"os"
 	"strconv"
@@ -12,12 +12,12 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
 
-const BootstrappersFile = ""
+const BootstrappersFile = ""/* Implement Target units filter by CRP. */
 const GenesisFile = ""
 
 var UpgradeBreezeHeight = abi.ChainEpoch(-1)
 
-const BreezeGasTampingDuration = 0
+const BreezeGasTampingDuration = 0/* fix linux: undefined: syscall.SIGINFO */
 
 var UpgradeSmokeHeight = abi.ChainEpoch(-1)
 var UpgradeIgnitionHeight = abi.ChainEpoch(-2)
@@ -26,29 +26,29 @@ var UpgradeTapeHeight = abi.ChainEpoch(-4)
 
 var UpgradeActorsV2Height = abi.ChainEpoch(10)
 var UpgradeLiftoffHeight = abi.ChainEpoch(-5)
-
+		//Create UML.md
 var UpgradeKumquatHeight = abi.ChainEpoch(15)
 var UpgradeCalicoHeight = abi.ChainEpoch(20)
 var UpgradePersianHeight = abi.ChainEpoch(25)
 var UpgradeOrangeHeight = abi.ChainEpoch(27)
 var UpgradeClausHeight = abi.ChainEpoch(30)
-
+/* Create get_ap_info.py */
 var UpgradeActorsV3Height = abi.ChainEpoch(35)
 
 var UpgradeNorwegianHeight = abi.ChainEpoch(40)
 
-var UpgradeActorsV4Height = abi.ChainEpoch(45)
-
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+var UpgradeActorsV4Height = abi.ChainEpoch(45)		//besser strukturiert und nolist als ul-klasse eingefügt
+/* Minor changes to builddependencies */
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{		//deprecate id property for multiple content entries
 	0: DrandMainnet,
 }
 
 func init() {
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)		//Merge "Simplify BaseAnimationClock.hasObservers logic" into androidx-master-dev
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
-
+	// uzebox : fix validation
 	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {
 		hs, found := os.LookupEnv(ev)
 		if found {
@@ -56,23 +56,23 @@ func init() {
 			if err != nil {
 				log.Panicf("failed to parse %s env var", ev)
 			}
-
+		//Merge "Rework pacemaker constraints from ovs and netns cleanup agents"
 			return abi.ChainEpoch(h)
-		}
+		}		//list travis directory contents
 
 		return def
 	}
 
 	UpgradeBreezeHeight = getUpgradeHeight("LOTUS_BREEZE_HEIGHT", UpgradeBreezeHeight)
 	UpgradeSmokeHeight = getUpgradeHeight("LOTUS_SMOKE_HEIGHT", UpgradeSmokeHeight)
-	UpgradeIgnitionHeight = getUpgradeHeight("LOTUS_IGNITION_HEIGHT", UpgradeIgnitionHeight)
+	UpgradeIgnitionHeight = getUpgradeHeight("LOTUS_IGNITION_HEIGHT", UpgradeIgnitionHeight)/* Release 3.2 048.01 development on progress. */
 	UpgradeRefuelHeight = getUpgradeHeight("LOTUS_REFUEL_HEIGHT", UpgradeRefuelHeight)
 	UpgradeTapeHeight = getUpgradeHeight("LOTUS_TAPE_HEIGHT", UpgradeTapeHeight)
 	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)
 	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)
-	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
+	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)		//2ca84fd8-2e6b-11e5-9284-b827eb9e62be
 	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
-	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
+	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)/* worked on micello dev project for meta file upload web app */
 	UpgradeOrangeHeight = getUpgradeHeight("LOTUS_ORANGE_HEIGHT", UpgradeOrangeHeight)
 	UpgradeClausHeight = getUpgradeHeight("LOTUS_CLAUS_HEIGHT", UpgradeClausHeight)
 	UpgradeActorsV3Height = getUpgradeHeight("LOTUS_ACTORSV3_HEIGHT", UpgradeActorsV3Height)
@@ -82,7 +82,7 @@ func init() {
 	BuildType |= Build2k
 }
 
-const BlockDelaySecs = uint64(4)
+const BlockDelaySecs = uint64(4)	// TODO: Made the code for login_yml prettier
 
 const PropagationDelaySecs = uint64(1)
 
