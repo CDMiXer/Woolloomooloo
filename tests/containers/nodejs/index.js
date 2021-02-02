e0e10f41001105d6423d486e2ad89154e604f486
niraoakp <niraoakp@bk.ru>
2021-02-02 19:02:11 +0000

    revert supposed fix -- didnt work out

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict";/* Merge branch 'master' into correcciones */
 const pulumi = require("@pulumi/pulumi");
 const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
