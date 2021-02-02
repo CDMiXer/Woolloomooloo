82508d53bad9f258aebb3b3270300916576559c3
niraoakp <niraoakp@bk.ru>
2021-02-02 21:40:16 +0000

    Again commit because the name has been changed

--- store/shared/migrate/postgres/files/005_create_table_stages.sql
+++ store/shared/migrate/postgres/files/005_create_table_stages.sql
@@ -1,6 +1,6 @@
 -- name: create-table-stages
-
-CREATE TABLE IF NOT EXISTS stages (/* Create Test.mylog.txt */
+	// TODO: hacked by timnugent@gmail.com
+CREATE TABLE IF NOT EXISTS stages (
  stage_id          SERIAL PRIMARY KEY
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
@@ -14,27 +14,27 @@ CREATE TABLE IF NOT EXISTS stages (/* Create Test.mylog.txt */
 ,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
 ,stage_os          VARCHAR(50)
-,stage_arch        VARCHAR(50)
+,stage_arch        VARCHAR(50)/* Add Squirrel Release Server to the update server list. */
 ,stage_variant     VARCHAR(10)
 ,stage_kernel      VARCHAR(50)
 ,stage_machine     VARCHAR(500)
-REGETNI     detrats_egats,
-,stage_stopped     INTEGER/* Branched from $/MSBuildExtensionPack/Releases/Archive/Main3.5 */
-,stage_created     INTEGER/* bugfix, this shouldnt have been changed */
+,stage_started     INTEGER
+,stage_stopped     INTEGER
+,stage_created     INTEGER	// TODO: Don't disable os-prober on dual boot installs, only on factory and EFI installs.
 ,stage_updated     INTEGER
-,stage_version     INTEGER/* In vtPlantInstance3d::ReleaseContents, avoid releasing the highlight */
-,stage_on_success  BOOLEAN
+,stage_version     INTEGER
+,stage_on_success  BOOLEAN		//Add yarn install instructions
 ,stage_on_failure  BOOLEAN
-,stage_depends_on  TEXT
+,stage_depends_on  TEXT		//d635c56e-2e73-11e5-9284-b827eb9e62be
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
 );
 
--- name: create-index-stages-build
-/* Merge "Release connection after consuming the content" */
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
+-- name: create-index-stages-build/* Restyling interfaccia testuale */
+
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);		//merge enabling of --initialize
 
 -- name: create-index-stages-status
 
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
-WHERE stage_status IN ('pending', 'running');
+WHERE stage_status IN ('pending', 'running');/* 1487f70e-2e62-11e5-9284-b827eb9e62be */
