59170cd846d56b3e17281562e3343d09e11ba88c
niraoakp <niraoakp@bk.ru>
2021-02-02 12:33:41 +0000

    9903f7e8-2e9d-11e5-8431-a45e60cdfd11

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+	// Create Paper_RevDate:17.6.29
 using Pulumi;
 
 class ComponentArgs : Pulumi.ResourceArgs
-{		//Created front end controller
-])"ohce"(tupnI[    
-    public Input<object>? Echo { get; set; }	// Remove DocumentInterface from Bridge
-}		//create func pattern for edit/add/del
-
-class Component : Pulumi.ComponentResource	// Added compilation guidelines
 {
-    [Output("echo")]/* Update CopyReleaseAction.java */
+    [Input("echo")]
+    public Input<object>? Echo { get; set; }
+}
+		//uol2.5: fix js urlhelper
+class Component : Pulumi.ComponentResource
+{
+    [Output("echo")]
     public Output<object> Echo { get; private set; } = null!;
 
     [Output("childId")]
     public Output<string> ChildId { get; private set; } = null!;
-	// TODO: will be fixed by nagydani@epointsystem.org
-    public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)/* Remove else statements */
-        : base("testcomponent:index:Component", name, args, opts, remote: true)	// TODO: hacked by souzau@yandex.com
+
+    public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
+        : base("testcomponent:index:Component", name, args, opts, remote: true)
     {
     }
 }
