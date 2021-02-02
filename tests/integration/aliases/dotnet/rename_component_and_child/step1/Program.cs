a7e4c74292e9e1a091219fd077f11236fe35df8a
niraoakp <niraoakp@bk.ru>
2021-02-02 08:00:45 +0000

    Merge "arm/dt: 8974: update ion heap sizes and definitions"

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -1,31 +1,31 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
-using Pulumi;/* Release: Making ready to release 4.0.0 */
+using Pulumi;
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {/* fix json parse failure */
+    {
     }
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive : ComponentResource
-{	// TODO: add zone.js
+{
     private Resource resource;
 
-    public ComponentFive(string name, ComponentResourceOptions options = null)		//Fix script.js
+    public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
-}/* Stat command texts */
+}
 
-class Program/* Release of eeacms/www-devel:20.5.14 */
+class Program
 {
-    static Task<int> Main(string[] args)	// TODO: a4785f48-2e53-11e5-9284-b827eb9e62be
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
         {
