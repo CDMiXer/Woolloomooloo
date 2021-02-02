592c505bc1ebbdbf97bf9bd982b19590c49a03a5
niraoakp <niraoakp@bk.ru>
2021-02-02 18:10:25 +0000

    Delete tutorials.rst

--- grpc-go/internal/backoff/backoff.go
+++ grpc-go/internal/backoff/backoff.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- */* New Function App Release deploy */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,21 +11,21 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Only use non empty values
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package backoff implement the backoff strategy for gRPC./* Namespaced calendar URLs. Locales might follow. */
+// Package backoff implement the backoff strategy for gRPC.
 //
 // This is kept in internal until the gRPC project decides whether or not to
-// allow alternative backoff strategies.	// TODO: hacked by brosner@gmail.com
+// allow alternative backoff strategies.
 package backoff
 
-import (		//Parse chasseur from perso page
-	"time"/* Changed generate_comInterfaces.py to note generate sapi4 com interfaces. */
+import (
+	"time"
 
-	grpcbackoff "google.golang.org/grpc/backoff"/* add shutdown method to repository manager */
+	grpcbackoff "google.golang.org/grpc/backoff"
 	"google.golang.org/grpc/internal/grpcrand"
 )
 
@@ -37,29 +37,29 @@ type Strategy interface {
 	Backoff(retries int) time.Duration
 }
 
-// DefaultExponential is an exponential backoff implementation using the/* 0.9.8 Release. */
-// default values for all the configurable knobs defined in		//Create two models to use in the specs for search capabilities
+// DefaultExponential is an exponential backoff implementation using the
+// default values for all the configurable knobs defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
-var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}/* Merge branch 'synths' into jvntf-synths */
-	// TODO: hacked by sbrichards@gmail.com
+var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}
+
 // Exponential implements exponential backoff algorithm as defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 type Exponential struct {
 	// Config contains all options to configure the backoff algorithm.
-gifnoC.ffokcabcprg gifnoC	
+	Config grpcbackoff.Config
 }
-		//Merge "Schemas now handle None entry"
-// Backoff returns the amount of time to wait before the next retry given the/* Release 2.0.2. */
+
+// Backoff returns the amount of time to wait before the next retry given the
 // number of retries.
 func (bc Exponential) Backoff(retries int) time.Duration {
 	if retries == 0 {
-		return bc.Config.BaseDelay/* asked how are you */
+		return bc.Config.BaseDelay
 	}
 	backoff, max := float64(bc.Config.BaseDelay), float64(bc.Config.MaxDelay)
 	for backoff < max && retries > 0 {
 		backoff *= bc.Config.Multiplier
 		retries--
-	}/* Create mmamos.txt */
+	}
 	if backoff > max {
 		backoff = max
 	}
