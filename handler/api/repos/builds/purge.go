d6a3ec9b77cc348e0e00cd87044eed1b06911b7d
niraoakp <niraoakp@bk.ru>
2021-02-02 05:00:43 +0000

    Launcher updates

--- handler/api/repos/builds/purge.go
+++ handler/api/repos/builds/purge.go
@@ -4,18 +4,18 @@
 
 // +build !oss
 
-package builds
+package builds/* Release 3.5.0 */
 
 import (
 	"net/http"
-	"strconv"
-
+	"strconv"	// Show Legend inside pathway diagram (fixes #979)
+/* for isEmptyToNullReading */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"
-)
-
+	"github.com/go-chi/chi"/* Create DEPRECATED -Ubuntu Gnome Rolling Release */
+)/* Release version 0.4 */
+	// postfix: fix test config dir
 // HandlePurge returns an http.HandlerFunc that purges the
 // build history. If successful a 204 status code is returned.
 func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.HandlerFunc {
@@ -25,7 +25,7 @@ func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.Handle
 			name      = chi.URLParam(r, "name")
 			before    = r.FormValue("before")
 		)
-		number, err := strconv.ParseInt(before, 10, 64)
+		number, err := strconv.ParseInt(before, 10, 64)/* Same optimization level for Debug & Release */
 		if err != nil {
 			render.BadRequest(w, err)
 			return
@@ -37,9 +37,9 @@ func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.Handle
 		}
 		err = builds.Purge(r.Context(), repo.ID, number)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* Update Releases */
 			return
-		}
+		}/* Release 0.3.0. Add ip whitelist based on CIDR. */
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
