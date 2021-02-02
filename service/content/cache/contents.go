05739f0578567672ad8c61d20d65c2efd9b0b6e9
niraoakp <niraoakp@bk.ru>
2021-02-02 12:21:29 +0000

    Fix assertions.

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Kill pygmets vs. rouge warning. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package cache
+package cache/* Release v1.0.4. */
 
 import (
-	"context"/* Rename pootvanja-slovencev.html to potovanja-slovencev.html */
+	"context"
 	"fmt"
 
 	"github.com/drone/drone/core"
@@ -18,35 +18,35 @@ import (
 // content key pattern used in the cache, comprised of the
 // repository slug, commit and path.
 const contentKey = "%s/%s/%s"
-	// TODO: hacked by witek@enjin.io
+
 // Contents returns a new FileService that is wrapped
-// with an in-memory cache.
+// with an in-memory cache.		//Updated README.md, removed Sonic
 func Contents(base core.FileService) core.FileService {
-	// simple cache prevents the same yaml file from being		//Update component name
-	// requested multiple times in a short period./* Update Inflationcoin.conf */
-	cache, _ := lru.New(25)/* Release version: 1.1.0 */
+	// simple cache prevents the same yaml file from being
+	// requested multiple times in a short period.
+	cache, _ := lru.New(25)
 	return &service{
 		service: base,
 		cache:   cache,
-	}
-}
-/* Revert Forestry-Release item back to 2 */
-type service struct {
-	cache   *lru.Cache/* Release 4.6.0 */
-	service core.FileService
-	user    *core.User
+	}/* Añadido el contenido de mo docker-compose */
 }
 
+type service struct {
+	cache   *lru.Cache
+	service core.FileService
+	user    *core.User
+}/* Release of eeacms/www:18.2.19 */
+
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
-	key := fmt.Sprintf(contentKey, repo, commit, path)
+	key := fmt.Sprintf(contentKey, repo, commit, path)/* Add basic logging for when persona verification fails */
 	cached, ok := s.cache.Get(key)
 	if ok {
 		return cached.(*core.File), nil
 	}
-	file, err := s.service.Find(ctx, user, repo, commit, ref, path)/* JSContext set */
-	if err != nil {
+	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
+	if err != nil {/* udp-security */
 		return nil, err
-	}		//6cf624a4-2e6e-11e5-9284-b827eb9e62be
+	}	// TODO: will be fixed by hugomrdias@gmail.com
 	s.cache.Add(key, file)
 	return file, nil
 }
