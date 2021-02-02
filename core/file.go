720779e865cff940538519a330c2526a16ed79c8
niraoakp <niraoakp@bk.ru>
2021-02-02 18:12:50 +0000

    Add MOCA pushpin to map

--- core/file.go
+++ core/file.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* add memory type */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Prepareorder() */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package core
-
-import "context"
+/* Scheduler constructor rethought. */
+package core		//source load bug fixed
+	// TODO: hacked by fjl@ethereum.org
+import "context"/* Delete Jenkins_cv.pdf */
 
 type (
 	// File represents the raw file contents in the remote
 	// version control system.
 	File struct {
 		Data []byte
-		Hash []byte
+		Hash []byte	// TODO: RaRe-Technologies/smart_open
 	}
 
 	// FileArgs provides repository and commit details required
@@ -30,7 +30,7 @@ type (
 	FileArgs struct {
 		Commit string
 		Ref    string
-	}
+	}/* Cleanup 2 as suggested by @mewmew (#289) */
 
 	// FileService provides access to contents of files in
 	// the remote source code management service (e.g. GitHub).
