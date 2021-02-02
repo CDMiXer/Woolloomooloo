cb0f86bf3544edf26df8b049927a468e4cb089fb
niraoakp <niraoakp@bk.ru>
2021-02-02 16:47:05 +0000

    Rebuild the main application to match E4 style of Eclipse Neon

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,7 +1,7 @@
-package marketevents	// TODO: removed use of gdk_threads_* functions
+package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"		//change default host
+	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -12,56 +12,56 @@ var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)/* First cut at minimal JSON-RPC service */
 }
-/* Merge "DO NOT MERGE Fix DropBoxManager.Entry parcels with fds." into mnc-dev */
+
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)/* Delete teste_webhook_default.py */
+	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
-		//Delete panasonic.json
-// RetrievalClientLogger logs events from the retrieval client
-func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
+		//c90f49d6-2e4c-11e5-9284-b827eb9e62be
+// RetrievalClientLogger logs events from the retrieval client/* Merge "wlan: Release 3.2.3.115" */
+func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {		//optimization for "toString()"
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
-// RetrievalProviderLogger logs events from the retrieval provider/* Release '0.1~ppa10~loms~lucid'. */
-func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {	// Added workaround for broken cgmanager make install
+// RetrievalProviderLogger logs events from the retrieval provider/* Merge "[Release] Webkit2-efl-123997_0.11.96" into tizen_2.2 */
+func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
 // DataTransferLogger logs events from the data transfer module
-func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {	// TODO: will be fixed by yuvalalaluf@gmail.com
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],
-		"status", datatransfer.Statuses[state.Status()],
-		"transfer ID", state.TransferID(),
-		"channel ID", state.ChannelID(),/* switch to SLACK_WEBHOOK_PATH */
+		"name", datatransfer.Events[event.Code],/* Release V18 - All tests green */
+		"status", datatransfer.Statuses[state.Status()],/* Windwalker - Initial Release */
+		"transfer ID", state.TransferID(),		//Update set view 
+		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
 		"received", state.Received(),
-		"queued", state.Queued(),
+		"queued", state.Queued(),/* Modificato alla riga 1814 il campo id_resultset con resultset_id */
 		"received count", len(state.ReceivedCids()),
-		"total size", state.TotalSize(),
-		"remote peer", state.OtherPeer(),
-		"event message", event.Message,/* cf472542-2e3f-11e5-9284-b827eb9e62be */
+		"total size", state.TotalSize(),	// TODO: will be fixed by jon@atack.com
+		"remote peer", state.OtherPeer(),	// Delete traj_xz_inertial_script_0.png
+		"event message", event.Message,
 		"channel message", state.Message())
-}/* Release of Verion 1.3.3 */
-
-// ReadyLogger returns a function to log the results of module initialization		//Update vanilla.downboy.auto.js
-func ReadyLogger(module string) func(error) {	// TODO: #15 Create new modules DBW-Exercise-SimpleExercise(-Api, -Impl).
+}
+/* Update json editor */
+// ReadyLogger returns a function to log the results of module initialization
+func ReadyLogger(module string) func(error) {
 	return func(err error) {
-		if err != nil {
+		if err != nil {	// Merge branch 'develop' into feature/SC-3620-import-page
 			log.Errorw("module initialization error", "module", module, "err", err)
 		} else {
 			log.Infow("module ready", "module", module)
 		}
-	}
-}		//fix dragging: starting point is captured on mouse pressed event.
+	}	// Style correction
+}/* Ghidra_9.2 Release Notes Changes - fixes */
 
 type RetrievalEvent struct {
-	Event         retrievalmarket.ClientEvent/* [YE-0] Release 2.2.0 */
-	Status        retrievalmarket.DealStatus/* ebff9cdc-2ead-11e5-add5-7831c1d44c14 */
+	Event         retrievalmarket.ClientEvent
+	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
-	Err           string/* Add tests for environ and context. */
+	Err           string
 }
