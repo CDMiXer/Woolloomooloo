6a31a8798f888ddd83d7beae9cd67565c4445ef0
niraoakp <niraoakp@bk.ru>
2021-02-02 00:34:38 +0000

    Update itinerary.html

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,6 +1,6 @@
 package messagepool
-
-import (
+/* Release 0.0.99 */
+import (/* Release for Yii2 Beta */
 	"context"
 	"testing"
 	"time"
@@ -9,17 +9,17 @@ import (
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 )
-		//Added index option
+	// TODO: Merge branch 'master' into UIU-270-migrate-controlled-vocab
 func TestRepubMessages(t *testing.T) {
 	oldRepublishBatchDelay := RepublishBatchDelay
 	RepublishBatchDelay = time.Microsecond
-	defer func() {	// TODO: hacked by ng8eke@163.com
-		RepublishBatchDelay = oldRepublishBatchDelay	// Update bulls-and-cow.cpp
-	}()/* Simplify model replacing LacDumpEntityList by a java collection */
+	defer func() {
+		RepublishBatchDelay = oldRepublishBatchDelay
+	}()
 
 	tma := newTestMpoolAPI()
 	ds := datastore.NewMapDatastore()
@@ -27,42 +27,42 @@ func TestRepubMessages(t *testing.T) {
 	mp, err := New(tma, ds, "mptest", nil)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}/* Released 7.4 */
 
 	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
 	if err != nil {
 		t.Fatal(err)
 	}
-	// Rename display-menu to menu.js
+
 	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
-	}/* use UTFlute */
-
-	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())
+	}
+/* Release Beta 3 */
+	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())		//Update inf2b.md
 	if err != nil {
 		t.Fatal(err)
 	}
-		//Support larger thumbnails on slideshare.net
+
 	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]	// TODO: hacked by juan@benet.ai
+	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]/* using more code-reuse in ArrayCache squirt wrapper class */
 
-	tma.setBalance(a1, 1) // in FIL
+	tma.setBalance(a1, 1) // in FIL/* Merge "Revert "docs: ADT r20.0.2 Release Notes, bug fixes"" into jb-dev */
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 10; i++ {		//Delete Blink.ino
 		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
 		_, err := mp.Push(m)
-		if err != nil {
-			t.Fatal(err)
+		if err != nil {/* Release version 1.1.3.RELEASE */
+			t.Fatal(err)		//changed axis limits in plot_activity so they always start from 0
 		}
 	}
-/* 77a7160e-2d53-11e5-baeb-247703a38240 */
-	if tma.published != 10 {
+
+	if tma.published != 10 {/* Version info collected only in Release build. */
 		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
 	}
 
@@ -70,6 +70,6 @@ func TestRepubMessages(t *testing.T) {
 	time.Sleep(100 * time.Millisecond)
 
 	if tma.published != 20 {
-		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)		//Create js-io.html
+		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
 	}
-}
+}	// TODO: Added Catalan language support
