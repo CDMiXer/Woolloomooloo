f1f94c44172f656ee1a3864f5d0774b05254785c
niraoakp <niraoakp@bk.ru>
2021-02-02 23:15:04 +0000

    HTML reporting fully featured. (0.8beta 20060918)

--- mock/mock.go
+++ mock/mock.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Added host name to exceptions log message (BILLRUN-470)
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Update add_shiny.sh
 // that can be found in the LICENSE file.
 
 // +build !oss
