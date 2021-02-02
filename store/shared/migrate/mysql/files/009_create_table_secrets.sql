ce7e44f99b833214c4f208ee46a3b4eb86e55660
niraoakp <niraoakp@bk.ru>
2021-02-02 14:56:25 +0000

    Remove spurious use of sudo.

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
--- name: create-table-secrets
+-- name: create-table-secrets	// TODO: Update and rename query.php.md to readme.md
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT/* Update sparkfunc.c */
-,secret_repo_id           INTEGER	// TODO: Merge branch 'master' into add-autoloading
-,secret_name              VARCHAR(500)		//- Update UpdateLayeredWindow and Indirect.
-,secret_data              BLOB
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
+,secret_repo_id           INTEGER
+,secret_name              VARCHAR(500)
+,secret_data              BLOB/* commit hotnews */
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN	// TODO: hacked by why@ipfs.io
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
-	// TODO: Change CS theme to humanity
-CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name
+CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);		//Merge "reorder tasks"
+/* Release of eeacms/eprtr-frontend:0.2-beta.41 */
+-- name: create-index-secrets-repo-name/* Release of the 13.0.3 */
 
-CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);/* ...and fixed another missing ! */
+CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
