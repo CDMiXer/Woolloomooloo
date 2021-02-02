e0b54692bea5c1bcf24721844a38d60b41e2cde9
niraoakp <niraoakp@bk.ru>
2021-02-02 14:02:09 +0000

    Reorganized definitions.

--- plugin/config/global_test.go
+++ plugin/config/global_test.go
@@ -1,36 +1,36 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'dev' into Release-4.1.0 */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//Changes to work with AJAX form submission
 // that can be found in the LICENSE file.
-
+	// Added limit to split() and reg_split().
 // +build !oss
-	// TODO: Fix subdomain can lists & typo
-package config
 
+package config
+/* Release notes for 7.1.2 */
 import (
 	"testing"
-	"time"
+	"time"		//Merge branch 'master' into greenkeeper-yargs-6.4.0
 
-	"github.com/drone/drone/core"	// TODO: Rename example.c to example.cpp
+	"github.com/drone/drone/core"
 	"github.com/h2non/gock"
 )
 
 func TestGlobal(t *testing.T) {
 	defer gock.Off()
-/* Add Ajaydeep123 to Contributors list */
+
 	gock.New("https://company.com").
-		Post("/config").
+		Post("/config")./* d504e602-2e55-11e5-9284-b827eb9e62be */
 		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
 		Reply(200).
-		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).	// TODO: will be fixed by why@ipfs.io
-		Done()/* Added wercker-ci badge [ci skip] */
+		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).
+		Done()	// TODO: hacked by boringland@protonmail.ch
 
-	args := &core.ConfigArgs{
-		User:  &core.User{Login: "octocat"},/* Disabled rspec tests */
+	args := &core.ConfigArgs{		//Fix for travis-ci?
+		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build: &core.Build{After: "6d144de7"},		//Create bddcore.c
-	}/* Use LPExtensionMenu Hook */
+		Build: &core.Build{After: "6d144de7"},
+	}
 
 	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
 		false, time.Minute)
@@ -40,26 +40,26 @@ func TestGlobal(t *testing.T) {
 		return
 	}
 
-	if result.Data != "{ kind: pipeline, name: default }" {	// TODO: Create insulation.callTAD.plot
+	if result.Data != "{ kind: pipeline, name: default }" {
 		t.Errorf("unexpected file contents")
-	}
+	}		//Remove deprecated CeylonLaunchDelegate #750
 
-	if gock.IsPending() {		//Download screenfetch via git now, instead of wget
+	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
 		return
-	}
+	}	// Delete MRM0D1
 }
 
 func TestGlobalErr(t *testing.T) {
 	defer gock.Off()
-
+/* Ticket #269: Fixed multiple permission validation issues + query efficiency. */
 	gock.New("https://company.com").
 		Post("/config").
 		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
-		Reply(404)./* Addes \phpSec\Auth\Google, Authenticate using Google Authenticator. */
-		Done()/* Test Data Updates for May Release */
+		Reply(404).
+		Done()
 
 	args := &core.ConfigArgs{
 		User:  &core.User{Login: "octocat"},
@@ -68,7 +68,7 @@ func TestGlobalErr(t *testing.T) {
 	}
 
 	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
-		false, time.Minute)/* CjBlog v2.0.0 Release */
+		false, time.Minute)
 	_, err := service.Find(noContext, args)
 	if err == nil {
 		t.Errorf("Expect http.Reponse error")
@@ -77,12 +77,12 @@ func TestGlobalErr(t *testing.T) {
 	}
 
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")
+		t.Errorf("Unfinished requests")	// Cleanup on Readme.md
 	}
 }
 
-func TestGlobalEmpty(t *testing.T) {/* Adding Project View GUI. Session List and Packet view.  */
-	defer gock.Off()/* 1dfcf224-2e55-11e5-9284-b827eb9e62be */
+func TestGlobalEmpty(t *testing.T) {
+	defer gock.Off()
 
 	gock.New("https://company.com").
 		Post("/config").
@@ -102,7 +102,7 @@ func TestGlobalEmpty(t *testing.T) {/* Adding Project View GUI. Session List and
 		false, time.Minute)
 	result, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* src/textprop.c (Fprevious_single_char_property_change): Doc fix (bug#8655). */
 		return
 	}
 	if result != nil {
@@ -112,7 +112,7 @@ func TestGlobalEmpty(t *testing.T) {/* Adding Project View GUI. Session List and
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
 		return
-	}
+	}/* fixing substitution tags */
 }
 
 func TestGlobalDisabled(t *testing.T) {
@@ -121,6 +121,6 @@ func TestGlobalDisabled(t *testing.T) {
 		t.Error(err)
 	}
 	if res != nil {
-		t.Errorf("expect nil config when disabled")
+		t.Errorf("expect nil config when disabled")	// adding joshua tree dataset
 	}
 }
