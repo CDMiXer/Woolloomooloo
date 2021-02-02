c5223732c0cf9722404ccac53179adcbf5fec8cb
niraoakp <niraoakp@bk.ru>
2021-02-02 06:19:35 +0000

    updated start product

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,13 +1,13 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of		//found and fixed another average position calculation ...
 // this source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
-
+/* 11b4af70-2e5a-11e5-9284-b827eb9e62be */
 // +build appengine
 
-package websocket
+package websocket/* update Dapper and url */
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
-	for i := range b {
+	for i := range b {/* Create sql/sqlite-05.png */
 		b[i] ^= key[pos&3]
 		pos++
 	}
