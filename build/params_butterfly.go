d1fc00bbad8b8d9bfad143aec0533bd6ef130e90
niraoakp <niraoakp@bk.ru>
2021-02-02 02:36:06 +0000

    Merge branch 'dev' into bluetooth

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,57 +1,57 @@
-// +build butterflynet/* Patch Release Panel; */
-
-package build/* Release of eeacms/www-devel:21.5.7 */
+// +build butterflynet
+		//[maven-release-plugin]  copy for tag techytax-parent-2.3.0
+package build
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// Hopefully (almost) done BFS
-"dic-og/sfpi/moc.buhtig"	
-)/* Unit text: Simplified restart-from-0-problem */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: ugly but working hacks to annotate output svg with <g> wrappers
+	"github.com/ipfs/go-cid"
+)
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,/* Release notes and NEWS for 1.9.1. refs #1776 */
+	0: DrandMainnet,
 }
-
+/* changed Rubi output in Symbol.java */
 const BootstrappersFile = "butterflynet.pi"
-const GenesisFile = "butterflynet.car"	// TODO: will be fixed by magik6k@gmail.com
-	// [core] [refactor] [minor] Cleaned up code for resource.use
+const GenesisFile = "butterflynet.car"
+
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-const UpgradeSmokeHeight = -2
-const UpgradeIgnitionHeight = -3
-const UpgradeRefuelHeight = -4/* Release of eeacms/jenkins-master:2.222.4 */
-/* Fixed size of the comment view in the file properties */
-var UpgradeActorsV2Height = abi.ChainEpoch(30)/* fix quan-lycanbo.jsp */
-		//Update contributing section with a more detailed explanation.
-const UpgradeTapeHeight = 60
+const UpgradeSmokeHeight = -2	// TODO: Delete Combo2.php
+const UpgradeIgnitionHeight = -3		//a9c7eaa8-35ca-11e5-b7d7-6c40088e03e4
+const UpgradeRefuelHeight = -4
+
+var UpgradeActorsV2Height = abi.ChainEpoch(30)	// TODO: will be fixed by davidad@alum.mit.edu
+
+const UpgradeTapeHeight = 60	// actividades proyectos salud
 const UpgradeLiftoffHeight = -5
-const UpgradeKumquatHeight = 90	// Extends and improves main page
+const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
-const UpgradeClausHeight = 180
+const UpgradeClausHeight = 180/* Update VerifySvnFolderReleaseAction.java */
 const UpgradeOrangeHeight = 210
-const UpgradeActorsV3Height = 240	// TODO: Delete header_home_longtext2.html
+const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
-const UpgradeActorsV4Height = 8922
+const UpgradeActorsV4Height = 8922	// TODO: will be fixed by brosner@gmail.com
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
-	policy.SetSupportedProofTypes(
-		abi.RegisteredSealProof_StackedDrg512MiBV1,	// TODO: Remove build from git and update release documents
-	)
+	policy.SetSupportedProofTypes(/* updated comparion of table components */
+		abi.RegisteredSealProof_StackedDrg512MiBV1,/* Merge "sound: add Headphone:X postprocessing config for 8996" */
+	)	// TODO: will be fixed by why@ipfs.io
 
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true
+	Devnet = true	// hwhoops! Quick fix.
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-
-const PropagationDelaySecs = uint64(6)/* Macro configuration */
+	// TODO: Update i18next to version 19.4.3
+const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 2
+const BootstrapPeerThreshold = 2/* Release 0.7.100.3 */
 
 var WhitelistedBlock = cid.Undef
