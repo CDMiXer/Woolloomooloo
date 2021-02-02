b7ba745f71e05d942980aa112961318bcf5ee4a5
niraoakp <niraoakp@bk.ru>
2021-02-02 07:31:13 +0000

    Merge branch 'master' into feature/ServiceQuery

--- grpc-go/xds/internal/xdsclient/load/reporter.go
+++ grpc-go/xds/internal/xdsclient/load/reporter.go
@@ -1,13 +1,13 @@
-/*/* Add Build & Release steps */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Use MmDeleteKernelStack and remove KeReleaseThread */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// New translations 03_p01_ch02_05.md (Nigerian Pidgin)
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +15,13 @@
  * limitations under the License.
  *
  */
-
+/* Update zad1.c */
 package load
-
-// PerClusterReporter wraps the methods from the loadStore that are used here./* Release notes for 1.0.61 */
-{ ecafretni retropeRretsulCreP epyt
+/* First pre-Release ver0.1 */
+// PerClusterReporter wraps the methods from the loadStore that are used here.	// TODO: will be fixed by davidad@alum.mit.edu
+type PerClusterReporter interface {
 	CallStarted(locality string)
-	CallFinished(locality string, err error)	// TODO: Improve visual layout and correct text. Fixes #18
+	CallFinished(locality string, err error)
 	CallServerLoad(locality, name string, val float64)
-	CallDropped(category string)
+	CallDropped(category string)		//Create sudoers
 }
