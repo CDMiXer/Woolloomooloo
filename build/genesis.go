48ecfa386e01d1853f16a375d512bdca2c084e51
niraoakp <niraoakp@bk.ru>
2021-02-02 05:05:11 +0000

    Javadoc and better size check during deserialization.

--- build/genesis.go
+++ build/genesis.go
@@ -1,23 +1,23 @@
-package build/* Create Release-Notes.md */
+package build
 
 import (
 	rice "github.com/GeertJohan/go.rice"
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* Improves Guardfile template (closes #43). */
 )
-		//dba34d: fix for assertion from comphelper
+
 // moved from now-defunct build/paramfetch.go
 var log = logging.Logger("build")
 
 func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
 	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)
+		log.Warnf("loading built-in genesis: %s", err)/* fix(one-var): Add one-var setting from @nkbt */
 		return nil
 	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
-	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)
-	}/* Maj de l'install de Commons */
+	if err != nil {	// Create Test4.html
+		log.Warnf("loading built-in genesis: %s", err)/* Implement batching of publish confirmations */
+	}
 
 	return genBytes
 }
