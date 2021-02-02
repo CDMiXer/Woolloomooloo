f1413b3f352e7a484ec76d7d105bbafd95a312f9
niraoakp <niraoakp@bk.ru>
2021-02-02 16:56:27 +0000

    Break out conversion functions to own module

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -6,14 +6,14 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/argoproj/argo/server/auth"	// mock url request for project creating (test fix)
+	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
 )
 
 func Test_infoServer_GetUserInfo(t *testing.T) {
-	i := &infoServer{}	// TODO: will be fixed by fkautz@pseudocode.cc
-	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})	// srst2-v0.1.0-beta/ -> srst2-0.1.0-beta/
-	info, err := i.GetUserInfo(ctx, nil)/* Delete mskycode.cpp */
+	i := &infoServer{}
+	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
+	info, err := i.GetUserInfo(ctx, nil)
 	if assert.NoError(t, err) {
 		assert.Equal(t, "my-iss", info.Issuer)
 		assert.Equal(t, "my-sub", info.Subject)
