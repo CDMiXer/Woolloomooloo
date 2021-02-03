8ec2b2926bcbfe5c6925249d9931d3bf6f025fca
niraoakp <niraoakp@bk.ru>
2021-02-03 07:41:39 +0000

    script: strip cr chars

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,9 +1,9 @@
 -- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
- build_id            SERIAL PRIMARY KEY
+ build_id            SERIAL PRIMARY KEY/* Release1.3.8 */
 ,build_repo_id       INTEGER
-,build_config_id     INTEGER	// TODO: will be fixed by ligi@ligi.de
+,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
 ,build_parent        INTEGER
@@ -13,46 +13,46 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_action        VARCHAR(50)
 ,build_link          VARCHAR(2000)
 ,build_timestamp     INTEGER
-,build_title         VARCHAR(2000)		//jenkins job checker, configuration
-,build_message       VARCHAR(2000)
+,build_title         VARCHAR(2000)
+,build_message       VARCHAR(2000)/* add link to repo with publication data */
 ,build_before        VARCHAR(50)
-,build_after         VARCHAR(50)
+,build_after         VARCHAR(50)/* Release 0.44 */
 ,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
 ,build_source        VARCHAR(500)
 ,build_target        VARCHAR(500)
-,build_author        VARCHAR(500)
-,build_author_name   VARCHAR(500)/* Merge "Fix message key "sudo-error-sudo-ip"" */
+,build_author        VARCHAR(500)/* chore(package): update @types/execa to version 0.8.0 */
+,build_author_name   VARCHAR(500)/* Creando la clase Buffer que almacena los Items antes de guardarlos */
 ,build_author_email  VARCHAR(500)
 ,build_author_avatar VARCHAR(2000)
 ,build_sender        VARCHAR(500)
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(4000)
-,build_started       INTEGER
+,build_started       INTEGER/* f4739078-2e5b-11e5-9284-b827eb9e62be */
 ,build_finished      INTEGER
-,build_created       INTEGER/* Merge "msm_vidc: venc: Release encoder buffers" */
+,build_created       INTEGER
 ,build_updated       INTEGER
 ,build_version       INTEGER
-,UNIQUE(build_repo_id, build_number)		//-updated documentation
+,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-	// TODO: Add default figure config static methods.
+
 -- name: create-index-builds-incomplete
 
-CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)/* added interpreter shabang to Release-script */
-WHERE build_status IN ('pending', 'running');/* Fix some build&update changelog */
-/* Release script: small optimimisations */
+CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
+WHERE build_status IN ('pending', 'running');
+		//trigger new build for ruby-head-clang (cd2eb1f)
 -- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-
+/* Release v5.18 */
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
-
+/* Merge branch 'master' into jpaImpl */
 -- name: create-index-builds-sender
 
-CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);		//Internal Selector functionality deprecated and removed
+CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
 
