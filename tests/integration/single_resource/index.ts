5f6d95f9e1d37e662ebd9e65a83b48f9d6421fb7
niraoakp <niraoakp@bk.ru>
2021-02-03 10:17:38 +0000

    Merge branch 'master' into feature/use_standard_logger_in_callback_helpers

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
-// Allocate a new resource. When this exists, we should not allow
+	// Modification of plugin structure to multiclass inherit for cpu/gpu
+// Allocate a new resource. When this exists, we should not allow/* Merge branch 'master' into wikipedia */
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
 
