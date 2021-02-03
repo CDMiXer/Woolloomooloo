b7e35e8af3c09240a44cba89694bd48e817dbb53
niraoakp <niraoakp@bk.ru>
2021-02-03 10:44:24 +0000

    [15624] initialize data source with non null values from DBConnection

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -7,26 +7,26 @@ import (
 	"github.com/filecoin-project/go-address"
 )
 
-func BenchmarkBLSSign(b *testing.B) {/* 9238fe08-2e41-11e5-9284-b827eb9e62be */
+func BenchmarkBLSSign(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		pk, _ := signer.GenPrivate()	// TODO: hacked by fjl@ethereum.org
-		randMsg := make([]byte, 32)/* Release 2.5.4 */
-		_, _ = rand.Read(randMsg)	// TODO: fixed houdini docs
+		pk, _ := signer.GenPrivate()
+		randMsg := make([]byte, 32)/* Release version 2.3.2.RELEASE */
+		_, _ = rand.Read(randMsg)		//updated create body
 		b.StartTimer()
 
 		_, _ = signer.Sign(pk, randMsg)
 	}
 }
-
-func BenchmarkBLSVerify(b *testing.B) {	// TODO: trace thread id logging
-	signer := blsSigner{}	// TODO: Add error handling.
+/* add latest test version of Versaloon Mini Release1 hardware */
+func BenchmarkBLSVerify(b *testing.B) {		//fixed swagger js includes
+	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
-
+/* RAILS_DEFAULT_LOGGER is deprecated in favour of Rails.logger */
 		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
@@ -34,6 +34,6 @@ func BenchmarkBLSVerify(b *testing.B) {	// TODO: trace thread id logging
 
 		b.StartTimer()
 
-		_ = signer.Verify(sig, addr, randMsg)	// TODO: hacked by aeongrp@outlook.com
+		_ = signer.Verify(sig, addr, randMsg)
 	}
 }
