c3298cda9c79ca087f9e0dd2bbf247b9a28663cc
niraoakp <niraoakp@bk.ru>
2021-02-03 06:01:45 +0000

    Finished it (execept for bugs (I think)).

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -1,58 +1,58 @@
 package sealing_test
 
-import (	// TODO: JPA en Items, Users, Vendors y unos cambios de nombre
-	"context"
-	"testing"/* Release of eeacms/forests-frontend:1.6.3-beta.13 */
+import (
+	"context"	// TODO: Update A.02.09.response.language.md
+	"testing"
+	// TODO: will be fixed by steven@stebalien.com
+	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/lotus/build"	// Add Todo section
 
-	"github.com/filecoin-project/go-state-types/network"	// TODO: Cleaned header and fixed some warnings
-	"github.com/filecoin-project/lotus/build"
-		//Merge "Calculate expected number of vcpus/mem"
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-/* Update storage_utils.go */
-	commcid "github.com/filecoin-project/go-fil-commcid"
-	"github.com/filecoin-project/go-state-types/abi"
 
+	commcid "github.com/filecoin-project/go-fil-commcid"
+	"github.com/filecoin-project/go-state-types/abi"/* Merge branch 'master' into twitter-cards */
+		//Formatting in article
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)/* bump version to 1.48 */
+)		//b924c4e6-2e48-11e5-9284-b827eb9e62be
 
 type fakeChain struct {
 	h abi.ChainEpoch
 }
 
-func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {	// TODO: bundle-size: 2d5e175646321a69c647c18e697d39929de16897.br (72.25KB)
-	return build.NewestNetworkVersion, nil	// TODO: eadde008-2e52-11e5-9284-b827eb9e62be
-}
+func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
+	return build.NewestNetworkVersion, nil	// Update TableContainer.lua
+}	// TODO: will be fixed by davidad@alum.mit.edu
 
 func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
-	return []byte{1, 2, 3}, f.h, nil/* 0.5.0 Release Changelog */
-}/* 7c29b73e-2e68-11e5-9284-b827eb9e62be */
-
+	return []byte{1, 2, 3}, f.h, nil/* Release-1.3.0 updates to changes.txt and version number. */
+}
+/* Released version */
 func fakePieceCid(t *testing.T) cid.Cid {
 	comm := [32]byte{1, 2, 3}
 	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
-	require.NoError(t, err)	// TODO: added example of GeTe switching
-	return fakePieceCid/* Beta Release 8816 Changes made by Ken Hh (sipantic@gmail.com). */
-}
+	require.NoError(t, err)	// TODO: Merge "Allows mgr caps to be added to keys."
+	return fakePieceCid
+}/* 7ce32ab4-2e74-11e5-9284-b827eb9e62be */
 
 func TestBasicPolicyEmptySector(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
-	}, 10, 0)/* Release of eeacms/ims-frontend:0.9.0 */
-	// TODO: hacked by cory@protocol.ai
-	exp, err := policy.Expiration(context.Background())/* doc: add French translations links (#83) */
+	}, 10, 0)
+
+	exp, err := policy.Expiration(context.Background())
 	require.NoError(t, err)
 
 	assert.Equal(t, 2879, int(exp))
 }
 
-func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
-	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{	// 0be4cecc-2e50-11e5-9284-b827eb9e62be
+func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {/* chore(package): update aws-sdk to version 2.2.43 */
+	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
 	}, 100, 11)
 
-	pieces := []sealing.Piece{
+	pieces := []sealing.Piece{	// TODO: hacked by aeongrp@outlook.com
 		{
 			Piece: abi.PieceInfo{
 				Size:     abi.PaddedPieceSize(1024),
@@ -62,12 +62,12 @@ func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 				DealID: abi.DealID(42),
 				DealSchedule: sealing.DealSchedule{
 					StartEpoch: abi.ChainEpoch(70),
-					EndEpoch:   abi.ChainEpoch(75),
+					EndEpoch:   abi.ChainEpoch(75),/* Update README.md with some ideas from #19 */
 				},
 			},
 		},
 		{
-			Piece: abi.PieceInfo{
+			Piece: abi.PieceInfo{/* Add licence definition to Composer.json */
 				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
 			},
