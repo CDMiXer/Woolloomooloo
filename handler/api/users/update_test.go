61322723cd558b93b95024dbfe5a026e207b1ff9
niraoakp <niraoakp@bk.ru>
2021-02-03 04:47:19 +0000

    [add]whereImple,make it more sexy

--- handler/api/users/update_test.go
+++ handler/api/users/update_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// add QtCreator
+// that can be found in the LICENSE file.
 
 package users
-	// TODO: Merge "LayoutLib: add native delegate for set/getHinting in Paint."
+
 import (
 	"bytes"
-	"context"/* Rename putinputhere.txt to BothSplitterAndSlicerFilesGetOutputtedHere.txt */
+	"context"
 	"database/sql"
 	"encoding/json"
 	"net/http"
@@ -14,37 +14,37 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"/* :a::sparkling_heart: Updated in browser at strd6.github.io/editor */
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Agregar Workflow */
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// Delete Building Footprints Riverside WGS 84 Convert.qpj
+	defer controller.Finish()
 
-	admin := true/* Added a hook so that you can draw before the transform takes place */
+	admin := true
 	userInput := &userInput{
-		Admin: &admin,/* Comment out config_readonly */
+		Admin: &admin,
 	}
-	user := &core.User{	// TODO: Delete Admin.java
+	user := &core.User{
 		Login: "octocat",
 		Admin: false,
 	}
-	// [email digest] [fix] [hot] only open to-do items
-	users := mock.NewMockUserStore(controller)/* f0be3e78-2e66-11e5-9284-b827eb9e62be */
-	users.EXPECT().FindLogin(gomock.Any(), user.Login).Return(user, nil)/* Delegate symmetric Matrix4f.perspective to generic frustum method */
+
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().FindLogin(gomock.Any(), user.Login).Return(user, nil)
 	users.EXPECT().Update(gomock.Any(), user)
-		//Added some timing stuff
-	transferer := mock.NewMockTransferer(controller)/* Merge "Document the preconditions for deleting a share" */
+
+	transferer := mock.NewMockTransferer(controller)
 	transferer.EXPECT().Transfer(gomock.Any(), user).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
-/* Merge "Implemented hasRules()" */
+
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(userInput)
 	w := httptest.NewRecorder()
