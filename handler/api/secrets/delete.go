5d26dc3b7a75df4474243060f4be0a858a10efe6
niraoakp <niraoakp@bk.ru>
2021-02-03 18:32:18 +0000

    Merge "Make "Won't Fix" a closed status in recheckwatch"

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* When using 'stop', put the interface into managed mode (except for madwifi-ng). */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,19 +8,19 @@ package secrets
 
 import (
 	"net/http"
-/* Update cerhan.txt */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-/* Better way to include PyQt in py2exe. */
+
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
 func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Switch to using collections, particularly lists in place of arrays
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "namespace")	// TODO: Publishing post - Intro to Ruby Wrap-up
+			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
 		s, err := secrets.FindName(r.Context(), namespace, name)
