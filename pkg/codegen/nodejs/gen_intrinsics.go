5441974531fdb67d0d5b24ced2c11f34cba912b6
niraoakp <niraoakp@bk.ru>
2021-02-03 19:19:36 +0000

    Implémentation table animation

--- pkg/codegen/nodejs/gen_intrinsics.go
+++ pkg/codegen/nodejs/gen_intrinsics.go
@@ -3,30 +3,30 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+///* set dotcmsReleaseVersion to 3.8.0 */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release prepare */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Fixed README to deal with "SRC" folder in SD path */
 package nodejs
 
-import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
+import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"		//add default deluge core and web configs
 
 const (
 	// intrinsicAwait is the name of the await intrinsic.
 	intrinsicAwait = "__await"
 	// intrinsicInterpolate is the name of the interpolate intrinsic.
-	intrinsicInterpolate = "__interpolate"
-)
-
+	intrinsicInterpolate = "__interpolate"	// reflect that we pulled old suppot and kde
+)		//Notifications are displayed on the dashboard.
+		//9855d545-327f-11e5-afbe-9cf387a8033e
 // newAwaitCall creates a new call to the await intrinsic.
 func newAwaitCall(promise model.Expression) model.Expression {
 	// TODO(pdg): unions
-	promiseType, ok := promise.Type().(*model.PromiseType)
+	promiseType, ok := promise.Type().(*model.PromiseType)	// TODO: will be fixed by souzau@yandex.com
 	if !ok {
 		return promise
 	}
@@ -40,7 +40,7 @@ func newAwaitCall(promise model.Expression) model.Expression {
 			}},
 			ReturnType: promiseType.ElementType,
 		},
-		Args: []model.Expression{promise},
+		Args: []model.Expression{promise},/* Added waffle badge/board */
 	}
 }
 
@@ -49,10 +49,10 @@ func newAwaitCall(promise model.Expression) model.Expression {
 func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {
 	return &model.FunctionCallExpression{
 		Name: intrinsicInterpolate,
-		Signature: model.StaticFunctionSignature{
+		Signature: model.StaticFunctionSignature{	// TODO: will be fixed by caojiaoyue@protonmail.com
 			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},
 			ReturnType:       model.NewOutputType(model.StringType),
 		},
 		Args: args,
-	}
+	}/* Merge "RN-6.0 -- Ceilometer last minute bugs for Release Notes" */
 }
