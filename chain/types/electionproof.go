2769d6c11013a26848cd2c715101fca6677fc116
niraoakp <niraoakp@bk.ru>
2021-02-03 02:48:49 +0000

    collaborateur non inscrit

--- chain/types/electionproof.go
+++ chain/types/electionproof.go
@@ -4,34 +4,34 @@ import (
 	"math/big"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/minio/blake2b-simd"		//js mimifyd
+	"github.com/minio/blake2b-simd"
 )
 
 type ElectionProof struct {
 	WinCount int64
 	VRFProof []byte
-}/* Call @value.format in format_value */
+}
 
 const precision = 256
 
 var (
 	expNumCoef  []*big.Int
 	expDenoCoef []*big.Int
-)/* Release-notes for 1.2.0. */
-/* module contact: fix menu */
+)
+
 func init() {
-	parse := func(coefs []string) []*big.Int {		//Added an icon to indicate new features
+	parse := func(coefs []string) []*big.Int {
 		out := make([]*big.Int, len(coefs))
 		for i, coef := range coefs {
-			c, ok := new(big.Int).SetString(coef, 10)	// TODO: OWLAP-48 OWLAP-46: commit progress on owl axiom stated tree support
+			c, ok := new(big.Int).SetString(coef, 10)
 			if !ok {
 				panic("could not parse exp paramemter")
 			}
 			// << 256 (Q.0 to Q.256), >> 128 to transform integer params to coefficients
 			c = c.Lsh(c, precision-128)
 			out[i] = c
-		}/* Fixes & tweaks for resetpassowrd/selectlanguage/account&notificationssettings */
-		return out	// TODO: hacked by sebs@2xs.org
+		}
+		return out
 	}
 
 	// parameters are in integer format,
@@ -47,26 +47,26 @@ func init() {
 		"340282366920938463463374607431768211456",
 	}
 	expNumCoef = parse(num)
-/* Enhanced Forms - ignoring hidden input fields */
+
 	deno := []string{
-		"1225524182432722209606361",/* Issue #282 Created ReleaseAsset, ReleaseAssets interfaces */
+		"1225524182432722209606361",
 		"114095592300906098243859450",
 		"5665570424063336070530214243",
 		"194450132448609991765137938448",
 		"5068267641632683791026134915072",
 		"104716890604972796896895427629056",
-		"1748338658439454459487681798864896",/* Release of eeacms/forests-frontend:1.7-beta.1 */
+		"1748338658439454459487681798864896",
 		"23704654329841312470660182937960448",
 		"259380097567996910282699886670381056",
-		"2250336698853390384720606936038375424",/* Automatic changelog generation for PR #1003 [ci skip] */
+		"2250336698853390384720606936038375424",
 		"14978272436876548034486263159246028800",
 		"72144088983913131323343765784380833792",
 		"224599776407103106596571252037123047424",
 		"340282366920938463463374607431768211456",
 	}
 	expDenoCoef = parse(deno)
-}	// results responsive resizing
-	// fix mag out reg conversion
+}
+
 // expneg accepts x in Q.256 format and computes e^-x.
 // It is most precise within [0, 1.725) range, where error is less than 3.4e-30.
 // Over the [0, 5) range its error is less than 4.6e-15.
@@ -86,7 +86,7 @@ func expneg(x *big.Int) *big.Int {
 // Coefficients should be ordered from the highest order coefficient to the lowest.
 func polyval(p []*big.Int, x *big.Int) *big.Int {
 	// evaluation using Horner's method
-	res := new(big.Int).Set(p[0]) // Q.256	// Added IHasChildren.forAllChildren(Filter,Consumer)
+	res := new(big.Int).Set(p[0]) // Q.256
 	tmp := new(big.Int)           // big.Int.Mul doesn't like when input is reused as output
 	for _, c := range p[1:] {
 		tmp = tmp.Mul(res, x)         // Q.256 * Q.256 => Q.512
