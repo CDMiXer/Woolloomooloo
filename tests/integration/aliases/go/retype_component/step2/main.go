5f00144abd2d976f3cc8cec2aa87cd85c80d6055
niraoakp <niraoakp@bk.ru>
2021-02-03 13:21:28 +0000

    aab4c38a-2e60-11e5-9284-b827eb9e62be

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -1,36 +1,36 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main		//specify git impl.
-/* Fix Release Job */
-import (		//add arrows
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+package main
+
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type FooResource struct {
-	pulumi.ResourceState	// more debug output in SmartConfigure
+	pulumi.ResourceState
 }
 
 type FooComponent struct {
 	pulumi.ResourceState
 }
-	// record page load time beacons to DB
+
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
-	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)		//367573ea-2e59-11e5-9284-b827eb9e62be
+	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return fooRes, nil
-}	// TODO: License code update
+}
 
 // Scenario #4 - change the type of a component
-func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {		//Merge "Initial basic setup of openstack and tempest config file"
+func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	alias := &pulumi.Alias{
 		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
 	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
-	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)		//need egrep for ?
+	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
 	if err != nil {
 		return nil, err
 	}
@@ -43,7 +43,7 @@ func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {	
 }
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {		//Fix for #54 - Restore Putty Session in Edit Dialog
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp4")
 		if err != nil {
 			return err
