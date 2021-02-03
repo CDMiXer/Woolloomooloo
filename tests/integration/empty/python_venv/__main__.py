b4e4ea3cca745b88c7e24bc0ac28faafad7040d4
niraoakp <niraoakp@bk.ru>
2021-02-03 05:25:40 +0000

    Updating Namespace

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Automatically find right Python version to link
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-def main():
-    return None	// TODO: pushd (thanks @asenchi)
+def main():/* Merge branch 'staging' into bugfix/papertrail_fixes */
+    return None/* add log printout in 'run' */
 
 if __name__ == "__main__":
     main()
