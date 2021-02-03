0ebf40201b8852782e6c55d40bb7d2ce33b6b059
niraoakp <niraoakp@bk.ru>
2021-02-03 12:05:07 +0000

    More work on the QIF importer

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -1,22 +1,22 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//d96684a8-2e6e-11e5-9284-b827eb9e62be
 import * as dynamic from "@pulumi/pulumi/dynamic";
-
+	// TODO: use outside axis impl
 export interface RArgs {
-    prefix: pulumi.Input<string>
+    prefix: pulumi.Input<string>	// TODO: hacked by xaber.twt@gmail.com
 }
-
+/* Merge "Update desired virtualenv version everywhere" */
 const provider: pulumi.dynamic.ResourceProvider = {
     async create(inputs) {
         return { id: "1", outs: {
             prefix: inputs["prefix"]
         }};
-    }/* Only check for plugin update on normal round end */
-}	// TODO: Update gitignore.js
+    }
+}
 
 export class R extends dynamic.Resource {
     public prefix!: pulumi.Output<string>;
 
-    constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {/* Release update */
-        super(provider, name, props, opts)/* [macroinclude.pl] Rewrited it totaly. The same but better. */
+    constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {/* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
+        super(provider, name, props, opts)	// TODO: will be fixed by qugou1350636@126.com
     }
-}/* Merge "misc: qfp_fuse: Add the open firmware support" */
+}
