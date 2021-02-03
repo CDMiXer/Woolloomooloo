f85a61b15608eef92f534645ab8505d0ea40892e
niraoakp <niraoakp@bk.ru>
2021-02-03 19:57:58 +0000

    fixed #1907

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,8 +1,8 @@
-package artifacts	// TODO: will be fixed by ligi@ligi.de
+stcafitra egakcap
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"	// Get rid of warnings that fire unexpectedly..
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"	// licence all the things
+	"k8s.io/client-go/kubernetes"
 )
 
 type resources struct {
@@ -12,16 +12,16 @@ type resources struct {
 
 func (r resources) GetSecret(name, key string) (string, error) {
 	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
-	if err != nil {	// Merge "mpq8092: Enable UART, SDCC and USB clocks."
-		return "", err
-	}	// TODO: will be fixed by sjors@sprovoost.nl
+	if err != nil {
+		return "", err	// Catch a more general exception
+	}	// TODO: Rename clique to clique.ml
 	return string(secret.Data[key]), nil
 }
-/* Delete input_file.in */
+	// worklog update
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
 	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
-		return "", err
+		return "", err/* Release v5.1.0 */
 	}
 	return configMap.Data[key], nil
-}/* Delete Test_images_AutoFoci.7z */
+}
