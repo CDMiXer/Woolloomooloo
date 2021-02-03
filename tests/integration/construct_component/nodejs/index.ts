d7f42493f1572527b079426a9e7372ee2da99e0e
niraoakp <niraoakp@bk.ru>
2021-02-03 15:40:35 +0000

    Add date expression

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -3,6 +3,6 @@
 import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
-const componentB = new Component("b", {echo: componentA.echo});	// Update tests from EasyMock 3.5.1 to 3.6.
-const componentC = new Component("c", {echo: componentA.childId});/* * Neue Strategien können via CLI hinzugefügt werden */
+const componentB = new Component("b", {echo: componentA.echo});
+const componentC = new Component("c", {echo: componentA.childId});
 
