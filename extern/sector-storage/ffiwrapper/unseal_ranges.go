8e1835968a4c8912155db5e544f7f01c4e88582d
niraoakp <niraoakp@bk.ru>
2021-02-03 01:32:55 +0000

    [IMP] stock: stock partial move wizard

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,27 +1,27 @@
-package ffiwrapper	// TODO: will be fixed by xiemengjun@gmail.com
+package ffiwrapper
 
 import (
 	"golang.org/x/xerrors"
 
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
+/* Delete zxCalc_Release_002stb.rar */
+	"github.com/filecoin-project/go-state-types/abi"/* [CLEANUP] set versions to 7.1-SNAPSHOT */
 
-	"github.com/filecoin-project/go-state-types/abi"
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Merge branch 'master' into nr-add_raw_url_for_soundcloud */
-)/* Release BAR 1.1.12 */
-
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//revert version due to dropped release
+)
+	// TODO: Added a user manager
 // merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
-const mergeGaps = 32 << 20
-	// Fixed dayOfWeekJulian, exported SecondScale
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests	// TODO: will be fixed by juan@benet.ai
+const mergeGaps = 32 << 20	// TODO: Merge branch 'master' into queue-listener
+
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}	// TODO: Fixed bug with scrolling note content titles
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)		//Removed extra run argument.
+	}
 
 	return rlepluslazy.JoinClose(todo, mergeGaps)
-}
+}/* Merge "Release 1.0.0.58 QCACLD WLAN Driver" */
