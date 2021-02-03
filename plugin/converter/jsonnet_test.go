f1913863aeac3fc46e1a1d5312aad78748b64975
niraoakp <niraoakp@bk.ru>
2021-02-03 14:18:44 +0000

    update jsBin link

--- plugin/converter/jsonnet_test.go
+++ plugin/converter/jsonnet_test.go
@@ -1,43 +1,43 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// 6f6bbbea-2e55-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Release for 18.29.1 */
 
 package converter
 
-import (
+import (	// TODO: will be fixed by magik6k@gmail.com
 	"testing"
 
 	"github.com/drone/drone/core"
 )
-		//[*] Changelog - Fix style
-const jsonnetFile = `{"foo": "bar"}`
-const jsonnetFileAfter = `---		//update configuration section in README
-{
-   "foo": "bar"/* Release of eeacms/forests-frontend:1.7-beta.21 */
-}
-`	// TODO: hacked by josharian@gmail.com
-		//Added convenient python overrides
-const jsonnetStream = `[{"foo": "bar"}]`
-const jsonnetStreamAfter = `---
+
+const jsonnetFile = `{"foo": "bar"}`	// Create LaravelEpilogServiceProvider.php
+const jsonnetFileAfter = `---
 {
    "foo": "bar"
 }
 `
-/* e9ed548c-2e50-11e5-9284-b827eb9e62be */
+
+const jsonnetStream = `[{"foo": "bar"}]`
+const jsonnetStreamAfter = `---/* Fixing auth token missing on requests */
+{		//Setting up db config
+   "foo": "bar"
+}
+`
+
 func TestJsonnet_Stream(t *testing.T) {
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
-		Config: &core.Config{Data: jsonnetStream},/* Update ReleaseNotes.MD */
+		Config: &core.Config{Data: jsonnetStream},
 	}
 	service := Jsonnet(true)
 	res, err := service.Convert(noContext, args)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by antao2002@gmail.com
 		t.Error(err)
 		return
 	}
-	if res == nil {/* Create Beta Release Files Here */
+	if res == nil {	// TODO: Sharing .gitignore
 		t.Errorf("Expected a converted file, got nil")
 		return
 	}
@@ -45,32 +45,32 @@ func TestJsonnet_Stream(t *testing.T) {
 		t.Errorf("Want converted file %q, got %q", want, got)
 	}
 }
-
+/* c++: Comment using ::clearerr in cstdio for compilation c++ examples */
 func TestJsonnet_Snippet(t *testing.T) {
-	args := &core.ConvertArgs{/* Release '0.1.0' version */
+	args := &core.ConvertArgs{		//Add "indexed" keyword to IVaultOrgan.sol
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetFile},
-	}
+	}/* Added 'View Release' to ProjectBuildPage */
 	service := Jsonnet(true)
 	res, err := service.Convert(noContext, args)
-	if err != nil {
-		t.Error(err)
-		return
-	}		//Added missing commas for empty Party and Comments
-	if res == nil {/* match: optimize _patsplit */
-		t.Errorf("Expected a converted file, got nil")
+	if err != nil {/* Release 0.94.411 */
+		t.Error(err)	// TODO: try europe insstead of planet
 		return
 	}
+	if res == nil {
+		t.Errorf("Expected a converted file, got nil")
+		return
+}	
 	if got, want := res.Data, jsonnetFileAfter; got != want {
-		t.Errorf("Want converted file %q, got %q", want, got)
+		t.Errorf("Want converted file %q, got %q", want, got)	// TODO: hacked by hugomrdias@gmail.com
 	}
 }
 
-func TestJsonnet_Error(t *testing.T) {
-	args := &core.ConvertArgs{/* Merge pull request #1069 from mnapoli/patch-1 */
+func TestJsonnet_Error(t *testing.T) {/* Release 1.9.20 */
+	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: "\\"}, // invalid jsonnet
-	}/* Released v0.1.2 */
+	}	// Update intersphinx Python version to 3.6
 	service := Jsonnet(true)
 	_, err := service.Convert(noContext, args)
 	if err == nil {
