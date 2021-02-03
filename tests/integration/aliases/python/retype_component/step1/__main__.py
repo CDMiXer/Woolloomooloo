03f3352a66488689e1d40be40f1cc4a6d4094c76
niraoakp <niraoakp@bk.ru>
2021-02-03 12:00:14 +0000

    if size is know, use it

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Merge "Include Vanilla Plugin *.sql files" */
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+	// TODO: Merge devYears into development
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// Create asteroid.py
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):
+class ComponentFour(ComponentResource):/* Merge "Release Notes 6.0 -- Other issues" */
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))/* More testing for better code coverage */
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp4 = ComponentFour("comp4")
