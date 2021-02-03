6a8016817c594cb438bce0d929859292b01de5f2
niraoakp <niraoakp@bk.ru>
2021-02-03 09:53:31 +0000

    Update subject

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -4,11 +4,11 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by qugou1350636@126.com
-//		//fix(ediscovery): fix jsdoc format
-// Unless required by applicable law or agreed to in writing, software/* Increment version number after release */
-// distributed under the License is distributed on an "AS IS" BASIS,		//Delete ng-bootstrap.module.ts
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Tests are running again */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,11 +17,11 @@ import { Resource } from "./resource";
 // Resource A was read in the previous plan, but it's now created.
 const a = new Resource("a", { state: 42 });
 
-// B must be replaced.		//Merge "Set tuned profile for compute roles"
+// B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-/* Update to Market Version 1.1.5 | Preparing Sphero Release */
-// The engine generates:/* Released springrestclient version 1.9.12 */
+
+// The engine generates:
 // A: CreateReplacement
-// B: CreateReplacement/* [Translating] Guake 0.7.0 Released – A Drop-Down Terminal for Gnome Desktops */
+// B: CreateReplacement
 // B: DeleteReplacement
 // A: DeleteReplacement
