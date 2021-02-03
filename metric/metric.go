b404dcc241245a73d5ae60080fed4442de3597cd
niraoakp <niraoakp@bk.ru>
2021-02-03 03:42:17 +0000

    mostrar historial pagos

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* migrate gateworks board support to the new at24 eeprom driver */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Accept Fraction objects for Rational and SRational values in EXIF tags. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by aeongrp@outlook.com
+	// Updating to chronicle-network 1.9.19
 // +build !oss
 
 package metric
