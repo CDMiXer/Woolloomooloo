153fb0ae8f90e65512283a9e462a9804f6abce40
niraoakp <niraoakp@bk.ru>
2021-02-03 10:42:12 +0000

    Added copy of the file in SQLFileDataSource.addPicture.

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Fix "this.ui.warn is not a function" error
-// Licensed under the Apache License, Version 2.0 (the "License");	// 89caedb2-2e55-11e5-9284-b827eb9e62be
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release areca-7.1.9 */
+// You may obtain a copy of the License at/* Release of eeacms/forests-frontend:2.0-beta.58 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* rotation -left et -right fonctionnel */
-// Unless required by applicable law or agreed to in writing, software		//Delete latex.py
+//
+// Unless required by applicable law or agreed to in writing, software		//kaggle santander competition
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released version 0.4.1 */
+// See the License for the specific language governing permissions and/* ef287166-2e5a-11e5-9284-b827eb9e62be */
 // limitations under the License.
-
+	// TODO: background button: remove unused style
 // +build oss
 
-package registry/* Release: merge DMS */
+package registry/* Release a more powerful yet clean repository */
 
 import "github.com/drone/drone/core"
 
-// External returns a no-op registry credential provider.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+// External returns a no-op registry credential provider.
 func External(string, string, bool) core.RegistryService {
-	return new(noop)
+	return new(noop)	// TODO: Merge "git remove old non-working packaging files"
 }
