5bb37c07b0fe0cc569657a4c84b0bf8e07337bf9
niraoakp <niraoakp@bk.ru>
2021-02-03 02:17:20 +0000

    added first tool: shuffle

--- pkg/cmd/pulumi/view-trace.go
+++ pkg/cmd/pulumi/view-trace.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release dhcpcd-6.9.1 */
+// limitations under the License.
 
 package main
 
@@ -22,29 +22,29 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"sourcegraph.com/sourcegraph/appdash"	// TODO: Merge "Setup translation for octavia"
+	"sourcegraph.com/sourcegraph/appdash"
 	"sourcegraph.com/sourcegraph/appdash/traceapp"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// [packages] debootstrap: update to 1.0.48
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-	// TODO: include more one how to create directories, and how to run programs
+
 func readTrace(path string, store io.ReaderFrom) error {
-)htap(nepO.so =: rre ,f	
+	f, err := os.Open(path)
 	if err != nil {
 		return err
 	}
 	defer contract.IgnoreClose(f)
 	_, err = store.ReadFrom(f)
 	return err
-}		//updated html pages to reference hal tab
+}
 
-func newViewTraceCmd() *cobra.Command {/* cleaned up, fixed json */
+func newViewTraceCmd() *cobra.Command {
 	var port int
 	var cmd = &cobra.Command{
 		Use:   "view-trace [trace-file]",
 		Short: "Display a trace from the Pulumi CLI",
-		Long: "Display a trace from the Pulumi CLI.\n" +	// TODO: will be fixed by zodiacon@live.com
+		Long: "Display a trace from the Pulumi CLI.\n" +
 			"\n" +
 			"This command is used to display execution traces collected by a prior\n" +
 			"invocation of the Pulumi CLI.\n" +
@@ -54,29 +54,29 @@ func newViewTraceCmd() *cobra.Command {/* cleaned up, fixed json */
 			"port 8008; the --port flag can be used to change this if necessary.",
 		Args: cmdutil.ExactArgs(1),
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			url, err := url.Parse(fmt.Sprintf("http://localhost:%d", port))		//Create a Token superclass implementation to add offsets and previous token
+			url, err := url.Parse(fmt.Sprintf("http://localhost:%d", port))
 			if err != nil {
-				return err/* fix ContentAdapter */
+				return err
 			}
 
-			store := appdash.NewMemoryStore()		//Replace pas meetings list with table
+			store := appdash.NewMemoryStore()
 			if err := readTrace(args[0], store); err != nil {
 				return err
 			}
 
-			app, err := traceapp.New(nil, url)/* Release: Making ready for next release iteration 5.4.1 */
-			if err != nil {/* @Release [io7m-jcanephora-0.23.5] */
+			app, err := traceapp.New(nil, url)
+			if err != nil {
 				return err
 			}
 			app.Store, app.Queryer = store, store
-/* fix typo in SARSOPSolver field precision */
+
 			fmt.Printf("Displaying trace at %v\n", url)
 			return http.ListenAndServe(fmt.Sprintf(":%d", port), app)
-		}),/* Merge "docs: NDK r9 Release Notes" into jb-mr2-dev */
+		}),
 	}
 
 	cmd.PersistentFlags().IntVar(&port, "port", 8008,
 		"the port the trace viewer will listen on")
 
-	return cmd	// the ip fields should be 46 chars long to fit all ipv6 addresses
+	return cmd
 }
