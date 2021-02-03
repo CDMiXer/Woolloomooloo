7541fc53d6945d4e60d240734a7e3646ea80b28c
niraoakp <niraoakp@bk.ru>
2021-02-03 06:30:15 +0000

    create property directly in model

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -5,14 +5,14 @@
 // +build !oss
 
 package internal
-
+	// TODO: fixed issue where lat long was not going to server
 var defaultImage = "drone/controller:1"
-
+/* Release notes for Trimble.SQLite package */
 // DefaultImage returns the default dispatch image if none
 // is specified.
 func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
-	}
+	}	// TODO: will be fixed by mikeal.rogers@gmail.com
 	return image
-}
+}	// TODO: Python path changed
