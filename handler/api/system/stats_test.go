d9fccec1189d309f948a08c4695fb44239ad709e
niraoakp <niraoakp@bk.ru>
2021-02-03 15:04:21 +0000

    SO-4007: support additional predicate when checking for running jobs

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Canvas: fix devele undo operation after save.
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: hacked by lexy8russo@outlook.com
 
 package system
 
 import (
-	"io/ioutil"
+	"io/ioutil"		//1d2161fe-2e42-11e5-9284-b827eb9e62be
 
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}
+}/* Release 1.13.0 */
