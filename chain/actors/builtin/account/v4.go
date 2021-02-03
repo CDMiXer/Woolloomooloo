0967bc16a0b909e13f4da90162929dc967a55094
niraoakp <niraoakp@bk.ru>
2021-02-03 00:18:40 +0000

    Create studentreg.py

--- chain/actors/builtin/account/v4.go
+++ chain/actors/builtin/account/v4.go
@@ -1,30 +1,30 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-address"/* 8cbe41ca-2e9d-11e5-9fd6-a45e60cdfd11 */
+	"github.com/ipfs/go-cid"		//Cleanup CPAlert.
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/account"
 )
-
-var _ State = (*state4)(nil)	// Create social-support
+	// Big progress
+var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)		//fixed defects found by CoverityScan
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}		//Create pixi.py
-/* Update Styling.md */
+}
+
 type state4 struct {
 	account4.State
 	store adt.Store
-}/* Merge "Hwui: Remove unused variables" */
-	// TODO: hacked by magik6k@gmail.com
-func (s *state4) PubkeyAddress() (address.Address, error) {	// rev 481862
+}/* Release 0.3.1 */
+
+func (s *state4) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
