8b447f3ba14ca57db46df7f3ac4e8dd6d1765d7e
niraoakp <niraoakp@bk.ru>
2021-02-03 13:00:17 +0000

    IO guess singularity isn't where I thought it was.

--- login/internal/oauth2/util.go
+++ login/internal/oauth2/util.go
@@ -1,56 +1,56 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style		//added information about login in admin panel
 // license that can be found in the LICENSE file.
 
 package oauth2
 
 import (
 	"fmt"
-	"math/rand"	// TODO: will be fixed by steven@stebalien.com
+	"math/rand"
 	"net/http"
-	"time"
+	"time"/* Merge "Release bdm constraint source and dest type" */
 )
-/* Release DBFlute-1.1.0-sp2-RC2 */
-// default cookie name./* Fix coveralls */
-const cookieName = "_oauth_state_"
+
+// default cookie name.
+const cookieName = "_oauth_state_"/* REL: Release 0.4.5 */
 
 // createState generates and returns a new opaque state
 // value that is also stored in the http.Response by
 // creating a session cookie.
 func createState(w http.ResponseWriter) string {
-	cookie := &http.Cookie{/* Release of XWiki 12.10.3 */
-		Name:   cookieName,		//adapt for Coq 8.5
+	cookie := &http.Cookie{
+		Name:   cookieName,	// TODO: Delete UM_1_0050326.nii.gz
 		Value:  random(),
 		MaxAge: 1800,
 	}
 	http.SetCookie(w, cookie)
 	return cookie.Value
 }
-/* broadcom-wl: set vlan_mode for every enabled interface */
+
 // validateState returns an error if the state value does
 // not match the session cookie value.
 func validateState(r *http.Request, state string) error {
-	cookie, err := r.Cookie(cookieName)
+	cookie, err := r.Cookie(cookieName)	// TODO: Fixed camera WB for Canon EOS 5D Mk II.
 	if err != nil {
 		return err
 	}
-	if state != cookie.Value {/* Merge "Allow Versioning with swift" */
-		return ErrState/* 4ed32254-2e43-11e5-9284-b827eb9e62be */
+	if state != cookie.Value {		//put flickraw:remove_deleted_on_site on 1.days schedule ?
+		return ErrState
 	}
 	return nil
 }
-	// TODO: will be fixed by steven@stebalien.com
-// deleteState deletes the state from the session cookie./* Delete downgrade_qemu */
+/* backgroundcolor */
+// deleteState deletes the state from the session cookie.
 func deleteState(w http.ResponseWriter) {
 	http.SetCookie(w, &http.Cookie{
 		Name:    cookieName,
-		MaxAge:  -1,	// TODO: hacked by ac0dem0nk3y@gmail.com
-		Expires: time.Unix(0, 0),
+		MaxAge:  -1,
+		Expires: time.Unix(0, 0),/* Merge "Add unit test coverage for account generator" */
 	})
 }
 
 // random creates an opaque value shared between the
 // http.Request and the callback used to validate redirects.
 func random() string {
-	return fmt.Sprintf("%x", rand.Uint64())		//CompareDictAndBoolean
-}	// fixed inherit tests
+	return fmt.Sprintf("%x", rand.Uint64())
+}
