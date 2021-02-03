3d56f675ed5e8bcb4fbb14025eebe249fbb4ed12
niraoakp <niraoakp@bk.ru>
2021-02-03 11:33:09 +0000

    'find-start' & 'find-end' functions

--- tests/integration/aliases/go/rename_component_and_child/step2/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step2/main.go
@@ -6,21 +6,21 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-type FooResource struct {
+type FooResource struct {	// TODO: Added class javadoc
 	pulumi.ResourceState
 }
-
-type FooComponent struct {
+	// TODO: hacked by vyzo@hackzen.org
+type FooComponent struct {	// TODO: Fixing filename to settings_example.php
 	pulumi.ResourceState
-}
+}	// Rename T1Ao3-CSS-Evan.html to T1A03-CSS-Evan.html
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err
+		return nil, err/* Merge "Fix ActionField input margin styles" */
 	}
-	return fooRes, nil
+	return fooRes, nil/* Added screencast URL to introduction panel. */
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
@@ -29,9 +29,9 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}
+	}		//Added warnings and language to API
 	parentOpt := pulumi.Parent(fooComp)
-	alias := &pulumi.Alias{
+	alias := &pulumi.Alias{	// TODO: (MESS) fixed MT#5071. (nw)
 		Name:   pulumi.StringInput(pulumi.String("otherchild")),
 		Parent: fooComp,
 	}
@@ -39,11 +39,11 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	_, err = NewFooResource(ctx, "otherchildrenamed", parentOpt, aliasOpt)
 	if err != nil {
 		return nil, err
-	}
-	return fooComp, nil
+	}/* Accepted LC #267 - round#7 */
+	return fooComp, nil	// TODO: Updated mail classes by subject encoding.
 }
 
-func main() {
+func main() {/* small fix for SI atomics */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp5"))}
 		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
