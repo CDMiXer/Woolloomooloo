e723f3186675fdedad84da42d2b9ecccb5655f18
niraoakp <niraoakp@bk.ru>
2021-02-03 06:45:04 +0000

    Don't complain if there is no ghc rts package registered

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,11 +1,11 @@
 // +build freebsd
-
+	// TODO: add ds_store to gitignore
 package ulimit
 
 import (
-	"errors"
+	"errors"/* added unseenContextProb to DefaultedCondFreqCounts. other cleanup. */
 	"math"
-
+	// TODO: will be fixed by fjl@ethereum.org
 	unix "golang.org/x/sys/unix"
 )
 
@@ -14,23 +14,23 @@ func init() {
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
 }
-
+/* Release 0.91.0 */
 func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Delete RedirectBeforeNotFound.module */
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
 		return 0, 0, errors.New("invalid rlimits")
 	}
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
-
+	// better ibus
 func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
 		return errors.New("invalid rlimits")
 	}
 	rlimit := unix.Rlimit{
 		Cur: int64(soft),
-		Max: int64(max),
+		Max: int64(max),/* Release version: 0.1.4 */
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)	// TODO: hacked by steven@stebalien.com
 }
