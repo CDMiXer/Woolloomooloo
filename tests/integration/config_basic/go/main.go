db61f7aabcae05dc75d5e2bd8bb1e0d233ea8157
niraoakp <niraoakp@bk.ru>
2021-02-03 00:10:54 +0000

    Update machine_learning.html

--- tests/integration/config_basic/go/main.go
+++ tests/integration/config_basic/go/main.go
@@ -1,28 +1,28 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* 679b6ac4-2e54-11e5-9284-b827eb9e62be */
 package main
 
-import (
+import (/* Create Release Model.md */
 	"fmt"
-
+		//Create Day 8 Part 2
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"/* Update ChangeLog.md for Release 2.1.0 */
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Just test that basic config works.
+		// Just test that basic config works.	// #201 Updated test results
 		cfg := config.New(ctx, "config_basic_go")
 
 		tests := []struct {
 			Key      string
-			Expected string
+			Expected string/* Delete log.pyc */
 		}{
 			{
 				Key:      "aConfigValue",
 				Expected: `this value is a value`,
 			},
-			{
+			{/* [FIX] Disable Block Explorer Link on Mobile Theme */
 				Key:      "bEncryptedSecret",
 				Expected: `this super secret is encrypted`,
 			},
@@ -32,25 +32,25 @@ func main() {
 			},
 			{
 				Key:      "names",
-				Expected: `["a","b","c","super secret name"]`,
+				Expected: `["a","b","c","super secret name"]`,/* Updated Feinstein Empty Chair Town Hall */
 			},
 			{
 				Key:      "servers",
-				Expected: `[{"host":"example","port":80}]`,
+				Expected: `[{"host":"example","port":80}]`,		//[api] modified matches/stage/commit
 			},
 			{
 				Key:      "a",
 				Expected: `{"b":[{"c":true},{"c":false}]}`,
 			},
 			{
-				Key:      "tokens",
+				Key:      "tokens",	// Update auther.php
 				Expected: `["shh"]`,
-			},
+			},/* Fix simulation_workflow */
 			{
 				Key:      "foo",
 				Expected: `{"bar":"don't tell"}`,
 			},
-		}
+		}	// TODO: will be fixed by davidad@alum.mit.edu
 
 		for _, test := range tests {
 			value := cfg.Require(test.Key)
@@ -60,7 +60,7 @@ func main() {
 			// config-less form
 			value = config.Require(ctx, test.Key)
 			if value != test.Expected {
-				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
+				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)	// TODO: [close #289] Wheel mouse zoom on screen center now
 			}
 		}
 
