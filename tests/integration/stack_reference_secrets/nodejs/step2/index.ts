50672dce1d3fda78927ee80dd8c4d8f36a0c18e8
niraoakp <niraoakp@bk.ru>
2021-02-03 17:56:35 +0000

    Fix 2 leaks. (unlikely to occur though)

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");
+export const secret = pulumi.secret("secret");	// Graphical additions and adjustments
 
-// Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
+// Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous//* Changed the script file */
 // deployment.
-const org = new pulumi.Config().require("org");
+const org = new pulumi.Config().require("org");	// use a dialog to add users to an address-book re #4220
 const project = pulumi.getProject();
 const stack = pulumi.getStack();
-const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
+const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);/* [FIX] website editor snippet: double insert */
 
-export const refNormal = sr.getOutput("normal");
+export const refNormal = sr.getOutput("normal");		//Merge "Bump domino dependency to 1.0.11."
 export const refSecret = sr.getOutput("secret");
