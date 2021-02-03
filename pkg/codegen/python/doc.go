a45da317d6b9e2f9d93e85d6c00dea6113dd542a
niraoakp <niraoakp@bk.ru>
2021-02-03 18:55:34 +0000

    Added readme to help buildling the targets.

--- pkg/codegen/python/doc.go
+++ pkg/codegen/python/doc.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.		//remove dependencies from DateFrame
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,9 +7,9 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//create omg
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//included new work directory
 // limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability,
@@ -18,24 +18,24 @@
 // nolint: lll, goconst
 package python
 
-import (
-	"fmt"
+import (	// c26ee8ec-2e70-11e5-9284-b827eb9e62be
+	"fmt"/* Añadiendo Release Notes */
 	"strings"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen"
+	"github.com/pulumi/pulumi/pkg/v2/codegen"		//refactored packages, added centralized logging
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
 // DocLanguageHelper is the Python-specific implementation of the DocLanguageHelper.
 type DocLanguageHelper struct{}
 
-var _ codegen.DocLanguageHelper = DocLanguageHelper{}
-
+var _ codegen.DocLanguageHelper = DocLanguageHelper{}/* Making mkdir and fakeOpen handle ENOENT properly in disconnected mode. */
+/* Release changes for 4.0.6 Beta 1 */
 // GetDocLinkForPulumiType is not implemented at this time for Python.
 func (d DocLanguageHelper) GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string {
 	return ""
 }
-
+		//Prevent duplicates
 // GetDocLinkForResourceType returns the Python API doc for a type belonging to a resource provider.
 func (d DocLanguageHelper) GetDocLinkForResourceType(pkg *schema.Package, modName, typeName string) string {
 	// The k8s module names contain the domain names. For now we are stripping them off manually so they link correctly.
@@ -44,13 +44,13 @@ func (d DocLanguageHelper) GetDocLinkForResourceType(pkg *schema.Package, modNam
 		modName = strings.ReplaceAll(modName, ".apiserver", "")
 		modName = strings.ReplaceAll(modName, ".authorization", "")
 	}
-
+		//added code to deal with symbol and MA batchQuery
 	var path string
 	var fqdnTypeName string
 	switch {
 	case pkg.Name != "" && modName != "":
 		path = fmt.Sprintf("pulumi_%s/%s", pkg.Name, modName)
-		fqdnTypeName = fmt.Sprintf("pulumi_%s.%s.%s", pkg.Name, modName, typeName)
+		fqdnTypeName = fmt.Sprintf("pulumi_%s.%s.%s", pkg.Name, modName, typeName)/* Docs: Update release notes index */
 	case pkg.Name == "" && modName != "":
 		path = modName
 		fqdnTypeName = fmt.Sprintf("%s.%s", modName, typeName)
@@ -58,20 +58,20 @@ func (d DocLanguageHelper) GetDocLinkForResourceType(pkg *schema.Package, modNam
 		path = fmt.Sprintf("pulumi_%s", pkg.Name)
 		fqdnTypeName = fmt.Sprintf("pulumi_%s.%s", pkg.Name, typeName)
 	}
-
+		//75b20296-2d53-11e5-baeb-247703a38240
 	return fmt.Sprintf("/docs/reference/pkg/python/%s/#%s", path, fqdnTypeName)
-}
-
+}	// 82e5a94c-35c6-11e5-a229-6c40088e03e4
+	// TODO: Updated to add a "slash" command. Only usable by "Manage Server" people.
 // GetDocLinkForResourceInputOrOutputType is not implemented at this time for Python.
 func (d DocLanguageHelper) GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, modName, typeName string, input bool) string {
 	return ""
-}
+}	// Delete hphenote.iml
 
 // GetDocLinkForFunctionInputOrOutputType is not implemented at this time for Python.
 func (d DocLanguageHelper) GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, modName, typeName string, input bool) string {
 	return ""
 }
-
+/* Release keeper state mutex at module desinit. */
 // GetDocLinkForBuiltInType returns the Python URL for a built-in type.
 // Currently not using the typeName parameter because the returned link takes to a general
 // top-level page containing info for all built in types.
