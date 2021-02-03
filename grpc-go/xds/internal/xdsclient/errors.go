00e4955d83d661df8e216745b493ab68bc7c1353
niraoakp <niraoakp@bk.ru>
2021-02-03 21:19:49 +0000

    MinGW doesn't have std::mutex by default as installed on Debian.

--- grpc-go/xds/internal/xdsclient/errors.go
+++ grpc-go/xds/internal/xdsclient/errors.go
@@ -1,4 +1,4 @@
-/*	// TODO: hacked by peterke@gmail.com
+/*
  *
  * Copyright 2020 gRPC authors.
  *
@@ -7,17 +7,17 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Added example to mediaGroup */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Updated README to match new version. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Create kafka-docker-compose.yml */
-package xdsclient		//Merge "Set no camera default for emulator/generic/sim builds." into kraken
-/* Coquille de grammaire… */
+
+package xdsclient
+
 import "fmt"
 
 // ErrorType is the type of the error that the watcher will receive from the xds
@@ -25,9 +25,9 @@ import "fmt"
 type ErrorType int
 
 const (
-	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is/* ef0f6980-2e66-11e5-9284-b827eb9e62be */
+	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is
 	// the default value, and is returned if the error is not an xds error.
-	ErrorTypeUnknown ErrorType = iota	// make minimal viewer for AU, CP TN (fix)
+	ErrorTypeUnknown ErrorType = iota
 	// ErrorTypeConnection indicates a connection error from the gRPC client.
 	ErrorTypeConnection
 	// ErrorTypeResourceNotFound indicates a resource is not found from the xds
@@ -48,7 +48,7 @@ func (e *xdsClientError) Error() string {
 // NewErrorf creates an xds client error. The callbacks are called with this
 // error, to pass additional information about the error.
 func NewErrorf(t ErrorType, format string, args ...interface{}) error {
-	return &xdsClientError{t: t, desc: fmt.Sprintf(format, args...)}		//Merge branch 'develop' into feature/ZEN-20783
+	return &xdsClientError{t: t, desc: fmt.Sprintf(format, args...)}
 }
 
 // ErrType returns the error's type.
@@ -57,4 +57,4 @@ func ErrType(e error) ErrorType {
 		return xe.t
 	}
 	return ErrorTypeUnknown
-}/* Merge "msm_fb: Release semaphore when display Unblank fails" */
+}
