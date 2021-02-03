ad6f6f8a7fe222c8cdb4e8fe28b0b7683191b78c
niraoakp <niraoakp@bk.ru>
2021-02-03 03:58:54 +0000

    Adding support for initializing with config

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,11 +1,11 @@
 package helpers
 
-import (	// TODO: hacked by peterke@gmail.com
+import (
 	"context"
 
-	"go.uber.org/fx"/* Release notes for 1.0.74 */
+	"go.uber.org/fx"
 )
-	// TODO: hacked by fjl@ethereum.org
+
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
 
@@ -13,8 +13,8 @@ type MetricsCtx context.Context
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
-func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {		//Frases vacias efectos de los ataques
-	ctx, cancel := context.WithCancel(mctx)/* Return the task object. */
+func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
+	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
