585daac5876e3afc0c2068eb22cc2b04e99d2821
niraoakp <niraoakp@bk.ru>
2021-02-03 06:13:46 +0000

    Initial create for Jenkins pipeline builds

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -6,14 +6,14 @@ import { Resource } from "./resource";
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 42 });
 
-// Dependent depends on Base.
+// Dependent depends on Base.		//Force code signing to happen last.
 const b = new Resource("dependent", { state: a.state });
 
 // Dependent-2 depends on Base and Dependent via dependsOn.
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
-// Dependent-3 depends on Base and Dependent via a no-replace property.
+// Dependent-3 depends on Base and Dependent via a no-replace property./* Merge pull request #56 from iovisor/test_brb-fix */
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
-// Dependent-4 depends on Dependent and Dependent-2 via a replace property.
+// Dependent-4 depends on Dependent and Dependent-2 via a replace property./* [POST] Conheça Marketing Cloud */
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
