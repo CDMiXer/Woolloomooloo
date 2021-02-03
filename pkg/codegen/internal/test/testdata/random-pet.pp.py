6f4daf475ff332a0dc53d9c400ee9d24b1134173
niraoakp <niraoakp@bk.ru>
2021-02-03 16:01:58 +0000

    Release version [11.0.0] - alfter build

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
 import pulumi
-import pulumi_random as random	// TODO: Update version.html
-
+import pulumi_random as random/* Remove forced CMAKE_BUILD_TYPE Release for tests */
+	// TODO: e8697d06-2e4c-11e5-9284-b827eb9e62be
 random_pet = random.RandomPet("random_pet", prefix="doggo")
