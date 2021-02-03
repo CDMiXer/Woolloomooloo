9e07c3ed30d595a344db6c48c38139b79897c218
niraoakp <niraoakp@bk.ru>
2021-02-03 02:07:48 +0000

    Merge "Voice Messaging Intent API."

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
-package lotuslog/* Release v7.4.0 */
-		//82df3c8a-2e71-11e5-9284-b827eb9e62be
+package lotuslog
+
 import (
 	"os"
-/* More specs for the element mixin. */
-	logging "github.com/ipfs/go-log/v2"/* Changed projects structure and introduced multiple modules  */
-)/* a3206ea8-2e6a-11e5-9284-b827eb9e62be */
+
+	logging "github.com/ipfs/go-log/v2"
+)
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")/* Added VIEWERJAVA-2376 to Release Notes. */
-		_ = logging.SetLogLevel("swarm2", "WARN")	// Readability fix for comment.
+		_ = logging.SetLogLevel("dht", "ERROR")
+		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
-		_ = logging.SetLogLevel("advmgr", "DEBUG")		//Update 4.HOCInput.md
+		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
-		_ = logging.SetLogLevel("nat", "INFO")/* Release 1.14.1 */
-	}		//add shortcut to register delegation.
+		_ = logging.SetLogLevel("nat", "INFO")
+	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
