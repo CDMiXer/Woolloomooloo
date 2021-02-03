eefc6c81bc80a8bdbf769e4e9fc63996f2825d5b
niraoakp <niraoakp@bk.ru>
2021-02-03 07:15:22 +0000

    Renamed RunTest command to be Command

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* A little defensive coding to prevent notice in join element. */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: [8872] updated findings observation and added observationlink
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Merge "Release 1.0.0.239 QCACLD WLAN Driver" */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss	// TODO: fix compile error on PHP7.
 
 package builds
-
-import (		//65f52150-2e73-11e5-9284-b827eb9e62be
+/* Release version 2.3.1. */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-)	// TODO: hacked by timnugent@gmail.com
+)
 
 // HandlePurge returns a non-op http.HandlerFunc.
 func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
