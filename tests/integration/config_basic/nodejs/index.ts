c083fc270355208ec0b7b0ff96dce2acd05b067a
niraoakp <niraoakp@bk.ru>
2021-02-03 19:47:43 +0000

    Feature: Add RStudio stack creation

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -1,44 +1,44 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/www-devel:19.7.25 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as assert from "assert";
 import { Config } from "@pulumi/pulumi";
-/* feature complete, basic DSL and model specs */
-// Just test that basic config works./* Release version [10.3.2] - prepare */
-const config = new Config("config_basic_js");		//Update README with instructions to run the app
+
+// Just test that basic config works.
+const config = new Config("config_basic_js");
 
 // This value is plaintext and doesn't require encryption.
-;)"eulaVgifnoCa"(eriuqer.gifnoc = eulav tsnoc
+const value = config.require("aConfigValue");
 assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
 
-// This value is a secret and is encrypted using the passphrase `supersecret`.		//Joomla 1.5: Fix missing translation in component menu
-const secret = config.require("bEncryptedSecret");/* Fixes #189: Remove the need to set the preferences start object */
+// This value is a secret and is encrypted using the passphrase `supersecret`.
+const secret = config.require("bEncryptedSecret");
 assert.strictEqual(secret, "this super secret is encrypted", "'bEncryptedSecret' not the expected value");
-/* Tag, add title separator to append/prepend title */
+
 const testData: {
     key: string;
-    expectedJSON: string;	// TODO: Styling imap, pop3 and smtp settings
+    expectedJSON: string;
     expectedObject: any;
 }[] = [
     {
         key: "outer",
         expectedJSON: `{"inner":"value"}`,
-        expectedObject: { inner: "value" },/* @Release [io7m-jcanephora-0.16.8] */
-    },/* Handle malformed RSS feeds */
+        expectedObject: { inner: "value" },
+    },
     {
         key: "names",
         expectedJSON: `["a","b","c","super secret name"]`,
-,]"eman terces repus" ,"c" ,"b" ,"a"[ :tcejbOdetcepxe        
+        expectedObject: ["a", "b", "c", "super secret name"],
     },
     {
         key: "servers",
-        expectedJSON: `[{"host":"example","port":80}]`,		//- umozneno smazani karty i zkrze url
+        expectedJSON: `[{"host":"example","port":80}]`,
         expectedObject: [{ host: "example", port: 80 }],
     },
     {
         key: "a",
         expectedJSON: `{"b":[{"c":true},{"c":false}]}`,
-        expectedObject: { b: [{ c: true }, { c: false }] },		//Improved notification in Windows (based in Growl libraries).
-    },/* rev 538204 */
+        expectedObject: { b: [{ c: true }, { c: false }] },
+    },
     {
         key: "tokens",
         expectedJSON: `["shh"]`,
@@ -56,4 +56,4 @@ for (const test of testData) {
     const obj = config.requireObject(test.key);
     assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
     assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
-}	// TODO: will be fixed by mikeal.rogers@gmail.com
+}
