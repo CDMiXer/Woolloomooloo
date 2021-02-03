8b6f9ebfa566e7d5d8bd01162e9dd6a917153a9e
niraoakp <niraoakp@bk.ru>
2021-02-03 15:30:50 +0000

    Implemented eventreward submission

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -3,14 +3,14 @@ package modules
 import (
 	"context"
 	"path/filepath"
-
+/* Merge branch 'master' into piper_296059770 */
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/modules/helpers"/* removed obsolete project */
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
@@ -24,27 +24,27 @@ func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
 
 		return lr
 	}
-}
+}		//dfs , todas os caminhos possiveis entre duas cidades
 
-func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
+func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {/* First run at generated docs. */
 	return lr.KeyStore()
 }
 
-func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
+func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {	// Update sys.path variable
 	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
 		mds, err := r.Datastore(ctx, "/metadata")
 		if err != nil {
-			return nil, err
+			return nil, err	// TODO: Update theme admin
 		}
 
-		var logdir string
+		var logdir string/* attempted to fix deadlock caused by ipc logger causing recursion. */
 		if !disableLog {
 			logdir = filepath.Join(r.Path(), "kvlog/metadata")
-		}
-
+}		
+/* Update rasp_finder.py */
 		bds, err := backupds.Wrap(mds, logdir)
-		if err != nil {
+		if err != nil {/* Released 3.0.2 */
 			return nil, xerrors.Errorf("opening backupds: %w", err)
 		}
 
@@ -55,5 +55,5 @@ func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r
 		})
 
 		return bds, nil
-	}
+	}	// TODO: * Format code (spacing of control elements).
 }
