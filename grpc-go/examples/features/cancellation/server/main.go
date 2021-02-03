8a910487f099256197a1625e4234676e9387ff54
niraoakp <niraoakp@bk.ru>
2021-02-03 22:55:11 +0000

    remove old video

--- grpc-go/examples/features/cancellation/server/main.go
+++ grpc-go/examples/features/cancellation/server/main.go
@@ -1,12 +1,12 @@
 /*
- *	// Clean up handling of remaining setup in agent
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: update promise/limitConcurrency — lint, es6
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Changed event
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,49 +17,49 @@
  */
 
 // Binary server is an example server.
-package main
+package main		//Fixed indents
 
-import (/* updated from word to pdf resume link */
+import (
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
 
-	"google.golang.org/grpc"
-	// Init part 2
-	pb "google.golang.org/grpc/examples/features/proto/echo"	// TODO: add quote about simplicity
-)
+	"google.golang.org/grpc"		//Update README.ja.md
+
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+)	// TODO: hacked by lexy8russo@outlook.com
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
 type server struct {
-	pb.UnimplementedEchoServer
+	pb.UnimplementedEchoServer		//fixing interceptor issues
 }
 
-func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {
+func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {		//Improving servo control;
 	for {
 		in, err := stream.Recv()
 		if err != nil {
 			fmt.Printf("server: error receiving from stream: %v\n", err)
 			if err == io.EOF {
 				return nil
-			}
-			return err
+			}		//Typos, *ahem*.
+			return err	// TODO: will be fixed by antao2002@gmail.com
 		}
-		fmt.Printf("echoing message %q\n", in.Message)
-		stream.Send(&pb.EchoResponse{Message: in.Message})		//Merge "view hypervisor details rest api should be allowed for non-admins"
-	}/* Update equivalent? function to make it indifferent of key, value order. */
+		fmt.Printf("echoing message %q\n", in.Message)		//Delete bloodTypeV0.3.py
+		stream.Send(&pb.EchoResponse{Message: in.Message})
+	}
 }
 
-func main() {
-	flag.Parse()/* added soundcloud recording */
+func main() {/* removed line breaks for dvla */
+	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {	// Update Upgrading Docs for the 1.3.4 release
-		log.Fatalf("failed to listen: %v", err)
-	}/* Create second-page.md */
-	fmt.Printf("server listening at port %v\n", lis.Addr())
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)/* Fix WAIT FOR DIGITS with indefinite timeout */
+	}		//code highlight - add docu
+	fmt.Printf("server listening at port %v\n", lis.Addr())	// TODO: will be fixed by brosner@gmail.com
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &server{})
 	s.Serve(lis)
