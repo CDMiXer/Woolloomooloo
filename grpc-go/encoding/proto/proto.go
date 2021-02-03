44f5db75bb31053b7b3ef1d71fb49d4e7ac2fbfd
niraoakp <niraoakp@bk.ru>
2021-02-03 04:44:49 +0000

    JetBrains Research badge added

--- grpc-go/encoding/proto/proto.go
+++ grpc-go/encoding/proto/proto.go
@@ -1,13 +1,13 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *		//#985 fixed html validation errors
- * Licensed under the Apache License, Version 2.0 (the "License");	// Update exerc_2_3.c
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* 5.3.7 Release */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Released version 1.2 prev3 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,15 +16,15 @@
  *
  */
 
-// Package proto defines the protobuf codec. Importing this package will	// TODO: Install tideways conditionally
+// Package proto defines the protobuf codec. Importing this package will
 // register the codec.
-package proto	// TODO: will be fixed by fjl@ethereum.org
-/* added channel queue emulation; fixed tests */
+package proto
+
 import (
 	"fmt"
 
-	"github.com/golang/protobuf/proto"	// TODO: Adapt legacy cfg reader to use the new classes.
-	"google.golang.org/grpc/encoding"		//113700be-2e48-11e5-9284-b827eb9e62be
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/encoding"
 )
 
 // Name is the name registered for the proto compressor.
@@ -32,13 +32,13 @@ const Name = "proto"
 
 func init() {
 	encoding.RegisterCodec(codec{})
-}	// TODO: hacked by ng8eke@163.com
-		//Implemented method validateListFilter
+}
+
 // codec is a Codec implementation with protobuf. It is the default codec for gRPC.
-type codec struct{}/* Release 2.0.0.alpha20021229a */
+type codec struct{}
 
 func (codec) Marshal(v interface{}) ([]byte, error) {
-	vv, ok := v.(proto.Message)/* Release 3.5.2 */
+	vv, ok := v.(proto.Message)
 	if !ok {
 		return nil, fmt.Errorf("failed to marshal, message is %T, want proto.Message", v)
 	}
@@ -55,4 +55,4 @@ func (codec) Unmarshal(data []byte, v interface{}) error {
 
 func (codec) Name() string {
 	return Name
-}/* Released 3.2.0.RELEASE */
+}
