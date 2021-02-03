9d29bd2dac0be5789a741a9d83db0850fb01f648
niraoakp <niraoakp@bk.ru>
2021-02-03 01:16:21 +0000

    c443c564-4b19-11e5-b235-6c40088e03e4

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -2,9 +2,9 @@ package nullreader
 
 type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {/* Create vmlist.csv */
+func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
 	return len(out), nil
-}	// [Spider]ForkJoinPool Supported
+}
