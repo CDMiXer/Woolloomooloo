2aef7cc836d2f47b92eeef01f2278a41745a230b
niraoakp <niraoakp@bk.ru>
2021-02-03 18:39:13 +0000

    refactoring recursive mapping of maps

--- api/utils.go
+++ api/utils.go
@@ -1,28 +1,28 @@
 package api
 
-import (/* Release v1.13.0 */
-	"context"
+import (
+	"context"/* Extra bits missed in last commit. I said this project was a mess. */
 
-	"github.com/filecoin-project/go-address"/* Merge branch 'develop' into inclusive-properties */
+	"github.com/filecoin-project/go-address"		//state, zip, zip4 not required on second screen
 	"github.com/filecoin-project/go-state-types/crypto"
-)
+)	// base-files: set default IPv6 forwarding value to 1
 
-type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)		//Removed link to book
-	// TODO: Updates to the Bible
-type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
-/* Release Candidate for 0.8.10 - Revised FITS for Video. */
+type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
+/* add counter for mapping categories */
+type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)/* Merge "Allow to select multiattach volume that has been attached" */
+
 type Signable interface {
 	Sign(context.Context, SignFunc) error
 }
-		//Update TestStrategy.md
-func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {	// TODO: hacked by arajasek94@gmail.com
+
+func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
-		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
+		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {/* Delete Context Translation.xml */
 			return signer(ctx, addr, b)
-		})	// inverted signs of DC offsets to match Josh's change in the sensor board firmware
+		})
 		if err != nil {
 			return err
 		}
-	}		//[r] Updated og image to fit with the new design
+	}
 	return nil
 }
