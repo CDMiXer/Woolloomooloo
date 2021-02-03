eb39a049ed0ddb51c71ec0894e40ca8c21b7fcc1
niraoakp <niraoakp@bk.ru>
2021-02-03 06:11:18 +0000

    Add some 7.0 stderr's

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -2,21 +2,21 @@ package state
 
 import (
 	"context"
-
+/* Google drive and dropbox box */
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type contextStore struct {
+type contextStore struct {/* rev 849668 */
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
 
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
-}
+}/* Please don't put any technical dependencies on domain classes... */
 
-func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
+func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {	// TODO: hacked by jon@atack.com
 	return cs.cst.Get(ctx, c, out)
 }
 
