3f359d4ad2e4b6e146b4296c6a0e7f1d04fc5a0b
niraoakp <niraoakp@bk.ru>
2021-02-03 17:54:13 +0000

    Removed obsolete code that previously was for testing purposes

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,20 +1,20 @@
 // +build !appengine,!go1.14
-
-/*
+/* ARIS 1.0 Released to App Store */
+/*/* Released 4.3.0 */
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: 97c5873e-2e68-11e5-9284-b827eb9e62be
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Update openpyxl from 2.4.10 to 2.5.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* New excesises */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: hacked by cory@protocol.ai
- * limitations under the License./* Start Release 1.102.5-SNAPSHOT */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release notes for 0.7.5 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -23,20 +23,20 @@ package advancedtls
 import (
 	"crypto/tls"
 	"fmt"
-)/* Release Notes for v00-03 */
-
+)
+/* Release under GPL */
 // buildGetCertificates returns the first cert contained in ServerOptions for
 // non-appengine builds before version 1.4.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
-	}/* Release version 1.2.6 */
+	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
-	if err != nil {	// TODO: Remove old ‘AuxArray’ from schema
+	if err != nil {
 		return nil, err
-	}		//Center images on item show page
-	if len(certificates) == 0 {		//Commit: "Integrated Paypal recurring payments"
-		return nil, fmt.Errorf("no certificates configured")	// Fix two issues found by Merlin, thanks.
+	}
+	if len(certificates) == 0 {
+		return nil, fmt.Errorf("no certificates configured")
 	}
 	return certificates[0], nil
 }
