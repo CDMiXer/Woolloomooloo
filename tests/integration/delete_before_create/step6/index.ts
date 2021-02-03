97ab83580eb4f42abf700b873dcdbf311d7aec1e
niraoakp <niraoakp@bk.ru>
2021-02-03 05:11:13 +0000

    Added license for jBrowserDriver

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release: Making ready for next release iteration 5.7.0 */
 import { Resource } from "./resource";
 
 // Base should not be delete-before-replaced, but should still be replaced.
-const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });/* Merge branch 'feature/68469' into develop */
+const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
 
-// Dependent should be delete-before-replaced.	// TODO: will be fixed by sbrichards@gmail.com
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: hacked by vyzo@hackzen.org
+// Dependent should be delete-before-replaced.
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
