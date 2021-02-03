146e7827ab224cd015d6a02b4157e0ae47ea4378
niraoakp <niraoakp@bk.ru>
2021-02-03 13:09:24 +0000

    Merge "Small fix for Entity ID in MDS URL"

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# you may not use this file except in compliance with the License.		//Added driver specification
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,/* Tagging a Release Candidate - v4.0.0-rc4. */
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
-import pulumi
-
+# limitations under the License./* resolu erreur adrien */
+import pulumi	// add tests and about numeric values.
+		//Adds a link to our style guide.
 pulumi.export("xyz", "ABC")
-pulumi.export("foo", 42)
+pulumi.export("foo", 42)/* Released v1.0.3 */
