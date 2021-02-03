3f281eea93ce1b9d0c7905d12e4c7eeb8e9ff33a
niraoakp <niraoakp@bk.ru>
2021-02-03 21:10:41 +0000

    Update A.02.IR04.extended.capabilities.node.md

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
 package auth
 
-import (/* Release version: 1.4.1 */
+import (
 	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
 )
 
-func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {/* Release 0.9.9 */
-	kubeClientset := GetKubeClient(ctx)
+func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
+	kubeClientset := GetKubeClient(ctx)	// TODO: hacked by vyzo@hackzen.org
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
-	if err != nil {/* Remove useless jump table. */
-		return false, err	// TODO: hacked by sebastian.tharakan97@gmail.com
+	if err != nil {
+		return false, err
 	}
-	return allowed, nil/* Merge branch 'master' into iterm-update */
+	return allowed, nil
 }
