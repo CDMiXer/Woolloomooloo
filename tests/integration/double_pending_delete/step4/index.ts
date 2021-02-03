c76699bb90fc3cfcd724cfe3d20765c5da3ac294
niraoakp <niraoakp@bk.ru>
2021-02-03 12:41:10 +0000

    Rename nida.js to nida.sql

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -1,26 +1,26 @@
-// Copyright 2016-2018, Pulumi Corporation./* Ensure that `pcache-directory` ends in a slash */
-//
+// Copyright 2016-2018, Pulumi Corporation.
+///* Delete CS-ASP-026-Solution-master.zip */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 1-125. */
+// you may not use this file except in compliance with the License.	// Update DBInventoryDAO.java
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//datenpaket.xsd moved from /gdv to /xsd
+// Unless required by applicable law or agreed to in writing, software/* 2.0 Release preperations */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by onhardev@bk.ru
+	// Fix assignment error.
 import { Resource } from "./resource";
-	// TODO: Added project to "Using Swift AI?"
+/* Release w/ React 15 */
 // We'll complete our disaster recovery by triggering replacements of A and B again,
-// but this time the replacement of B will succeed.
-// The engine should generate:/* Add AGPL3 LICENSE */
-///* Release version [10.8.2] - alfter build */
+// but this time the replacement of B will succeed.	// 179912b2-2e6b-11e5-9284-b827eb9e62be
+// The engine should generate:
+//	// Update daq-gitlab-ci.yml
 // Delete A
 // Create A (mark old A as pending delete)
-const a = new Resource("a", { fail: 4 });/* Bump version to coincide with Release 5.1 */
+const a = new Resource("a", { fail: 4 });
 
 // Create B
 const b = new Resource("b", { fail: 2 }, { dependsOn: a });
@@ -30,4 +30,4 @@ const b = new Resource("b", { fail: 2 }, { dependsOn: a });
 
 // Like the last step, this is interesting because we delete A's URN three times in the same plan.
 // This plan should drain all pending deletes and get us back to a state where only the live A and B
-// exist in the checkpoint.		//gitignores
+// exist in the checkpoint.
