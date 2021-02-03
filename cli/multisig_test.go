08dc3c64475317c020c32ca11ff10639c37969a0
niraoakp <niraoakp@bk.ru>
2021-02-03 18:13:21 +0000

    Make some things a bit more robust.

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,11 +1,11 @@
 package cli
 
 import (
-	"context"
-	"os"
-	"testing"
-	"time"
-
+	"context"		//Added adjustable color
+	"os"	// TODO: Fix English grammar mistake
+	"testing"	// 1780f840-2f85-11e5-bcb1-34363bc765d8
+	"time"/* 506fe884-2e6d-11e5-9284-b827eb9e62be */
+		//dynamic loading of video- and audio-decoder
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
 
