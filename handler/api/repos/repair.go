1be0666f02da1c65d10f24220ef5057afc04f79e
niraoakp <niraoakp@bk.ru>
2021-02-03 13:57:29 +0000

    Merge "Skin: Remove long-deprecated aliases for Linker methods"

--- handler/api/repos/repair.go
+++ handler/api/repos/repair.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.		//Merge "Merge "wlan: extra channel 144 support, host only""
+///* upgraded server (pinging clients), fixed msg */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by why@ipfs.io
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Fixed JavaScript editor save/close etc */
 // limitations under the License.
 
 package repos
@@ -20,7 +20,7 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-
+	// TODO: hacked by ac0dem0nk3y@gmail.com
 	"github.com/go-chi/chi"
 )
 
@@ -28,22 +28,22 @@ import (
 // requests to repair the repository hooks and sync the repository
 // details.
 func HandleRepair(
-	hooks core.HookService,
+	hooks core.HookService,/* Release 0.3.3 (#46) */
 	repoz core.RepositoryService,
 	repos core.RepositoryStore,
-	users core.UserStore,
+	users core.UserStore,	// TODO: add official design documentation
 	link string,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			owner = chi.URLParam(r, "owner")
+			owner = chi.URLParam(r, "owner")	// TODO: Merge "Update VP8DX_BOOL_DECODER_FILL to better detect EOS"
 			name  = chi.URLParam(r, "name")
-		)
+		)	// TODO: will be fixed by why@ipfs.io
 
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).	// Updates to argparse PEP based on python-dev feedback.
 				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
@@ -61,14 +61,14 @@ func HandleRepair(
 				Warnln("api: cannot find repository owner")
 			return
 		}
-
-		remote, err := repoz.Find(r.Context(), user, repo.Slug)
-		if err != nil {
-			render.NotFound(w, err)
+/* Merged text-editor into develop */
+		remote, err := repoz.Find(r.Context(), user, repo.Slug)		//a2cc16fa-2e51-11e5-9284-b827eb9e62be
+		if err != nil {	// TODO: will be fixed by steven@stebalien.com
+			render.NotFound(w, err)/* Added support for multiple series in chart */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name).
+				WithField("name", name)./* dcf019ce-2e76-11e5-9284-b827eb9e62be */
 				Warnln("api: remote repository not found")
 			return
 		}
@@ -77,7 +77,7 @@ func HandleRepair(
 		repo.HTTPURL = remote.HTTPURL
 		repo.Private = remote.Private
 		repo.SSHURL = remote.SSHURL
-
+	// Load older posts link is fixed.
 		// the gitea and gogs repository endpoints do not
 		// return the http url, so we need to ensure we do
 		// not replace the existing value with a zero value.
