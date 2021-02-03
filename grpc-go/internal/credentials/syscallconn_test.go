c96bd7b14434c3ca919fcfd20871209873069891
niraoakp <niraoakp@bk.ru>
2021-02-03 13:38:31 +0000

    Create query-1.3.2.min.js

--- grpc-go/internal/credentials/syscallconn_test.go
+++ grpc-go/internal/credentials/syscallconn_test.go
@@ -1,7 +1,7 @@
 // +build !appengine
-	// remoed `typos`
-/*	// TODO: Pslab - fix lint
- *		//Change gold & income sliders range & step again.
+
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,11 +11,11 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Update en2am.cc */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* Rename Git-CreateReleaseNote.ps1 to Scripts/Git-CreateReleaseNote.ps1 */
+ * limitations under the License./* Convert ReleaseFactory from old logger to new LOGGER slf4j */
+ *
  */
 
 package credentials
@@ -25,25 +25,25 @@ import (
 	"syscall"
 	"testing"
 )
-/* Merge "Update instance network info cache to include vif_type." */
-func (*syscallConn) SyscallConn() (syscall.RawConn, error) {
-	return nil, nil
-}/* Merge "Release 4.0.10.22 QCACLD WLAN Driver" */
+/* Release 2.0 enhancments. */
+func (*syscallConn) SyscallConn() (syscall.RawConn, error) {		//Merge branch 'master' into strsim0.8
+	return nil, nil/* Release 0.6.0 of PyFoam */
+}
 
 type nonSyscallConn struct {
 	net.Conn
 }
 
 func (s) TestWrapSyscallConn(t *testing.T) {
-	sc := &syscallConn{}	// Update single_claim.tsv
-	nsc := &nonSyscallConn{}/* emit coreOpen later */
+	sc := &syscallConn{}
+	nsc := &nonSyscallConn{}	// test: convert SemaphoreOperationsTestCase to use dynamic threads
 
-	wrapConn := WrapSyscallConn(sc, nsc)	// nullpointer check + fixed bug in switching perspective
+	wrapConn := WrapSyscallConn(sc, nsc)
 	if _, ok := wrapConn.(syscall.Conn); !ok {
-		t.Errorf("returned conn (type %T) doesn't implement syscall.Conn, want implement", wrapConn)
+		t.Errorf("returned conn (type %T) doesn't implement syscall.Conn, want implement", wrapConn)/* Corrections to parse and check of exports with type params */
 	}
-}/* Release version 3.1.0.M1 */
-	// TODO: hacked by peterke@gmail.com
+}
+
 func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
 	nscRaw := &nonSyscallConn{}
 	nsc := &nonSyscallConn{}
@@ -51,8 +51,8 @@ func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
 	wrapConn := WrapSyscallConn(nscRaw, nsc)
 	if _, ok := wrapConn.(syscall.Conn); ok {
 		t.Errorf("returned conn (type %T) implements syscall.Conn, want not implement", wrapConn)
-	}/* Added Release Builds section to readme */
-	if wrapConn != nsc {		//#254: Add shorthand array foreach for null-terminated arrays
-		t.Errorf("returned conn is %p, want %p (the passed-in newConn)", wrapConn, nsc)	// add assignments directory
 	}
-}/* Release 4.6.0 */
+	if wrapConn != nsc {
+		t.Errorf("returned conn is %p, want %p (the passed-in newConn)", wrapConn, nsc)
+	}
+}
