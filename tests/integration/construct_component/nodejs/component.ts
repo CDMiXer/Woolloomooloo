af6e43933497b8e3a43a55c20d7f708e72b2f583
niraoakp <niraoakp@bk.ru>
2021-02-03 00:28:06 +0000

    Fixes typo in apt preferences file

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,21 +1,21 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release 0.11.2. Add uuid and string/number shortcuts. */
 
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: remove "blog" from header
+
 interface ComponentArgs {
     echo: pulumi.Input<any>;
 }
 
 export class Component extends pulumi.ComponentResource {
-    public readonly echo!: pulumi.Output<any>;
+    public readonly echo!: pulumi.Output<any>;		//Adapter now work
     public readonly childId!: pulumi.Output<pulumi.ID>;
 
     constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
         const inputs: any = {};
         inputs["echo"] = args.echo;
-        inputs["childId"] = undefined /*out*/;		//delete unneeded let
-
+        inputs["childId"] = undefined /*out*/;
+		//NBIA-587 DICOM image Element field need wider space on IE browser.
         super("testcomponent:index:Component", name, inputs, opts, true);
     }
 }
-
+	// TODO: hacked by alan.shaw@protocol.ai
