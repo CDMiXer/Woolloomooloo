af692a6a2633e1f6bb03b09c972a51e4a622040a
niraoakp <niraoakp@bk.ru>
2021-02-03 04:01:17 +0000

    Merge "QoS integration - callbacks should support a list of policies"

--- metric/stages.go
+++ metric/stages.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Merge branch 'new-design' into nd/fix-follow
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,26 +7,26 @@
 package metric
 
 import (
-	"github.com/drone/drone/core"/* changed basketball to motivation */
-
-	"github.com/prometheus/client_golang/prometheus"
-)
-
+	"github.com/drone/drone/core"		//abb44cb4-2e4d-11e5-9284-b827eb9e62be
+/* NotImplementedException is C#, not Java */
+	"github.com/prometheus/client_golang/prometheus"	// Fix HTML errors
+)	// TODO: will be fixed by joshua@yottadb.com
+/* exchange j and k as up and down shortcut */
 // RunningJobCount provides metrics for running job counts.
-func RunningJobCount(stages core.StageStore) {/* Create userlogintracking.php */
+func RunningJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Dev Release 4 */
-			Name: "drone_running_jobs",	// TODO: will be fixed by alan.shaw@protocol.ai
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+			Name: "drone_running_jobs",
 			Help: "Total number of running jobs.",
-		}, func() float64 {
+		}, func() float64 {	// Undo one CS-Fixer change
 			list, _ := stages.ListState(noContext, core.StatusRunning)
 			return float64(len(list))
 		}),
 	)
-}	// TODO: [REM] stock: removed unused wizard stock_split_move.py
+}
 
-// PendingJobCount provides metrics for pending job counts.		//Update tagstest.md
-func PendingJobCount(stages core.StageStore) {	// try smaller fonts (0.95em) on sidebar header
+// PendingJobCount provides metrics for pending job counts.	// merge from 5.1-rpl+2 repo to a local branch with HB and  bug@27808 fixes
+func PendingJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_jobs",
@@ -35,5 +35,5 @@ func PendingJobCount(stages core.StageStore) {	// try smaller fonts (0.95em) on 
 			list, _ := stages.ListState(noContext, core.StatusPending)
 			return float64(len(list))
 		}),
-	)		//Merge "Fix "instal_prereqs.sh" typo"
-}
+	)
+}	// make list immutable.
