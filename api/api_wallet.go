40ccc21eae98b8739cfa30fcbd470f855686ed41
niraoakp <niraoakp@bk.ru>
2021-02-03 20:17:52 +0000

    Update def.md

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,37 +1,37 @@
-package api
+package api	// TODO: +EasyDataFeed
 
 import (
 	"context"
-		//chore: rennovatebot automerge
-	"github.com/filecoin-project/go-address"
+
+	"github.com/filecoin-project/go-address"	// TODO: Refine Scalastyle ImportOrderChecker rule
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//minor parameter value change
 type MsgType string
 
-const (		//68ac9744-2e59-11e5-9284-b827eb9e62be
+const (
 	MTUnknown = "unknown"
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
 
-	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)/* resolved issue if file is not zipped as expected. */
+	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
 	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
 	MTDealProposal = "dealproposal"
 
-	// TODO: Deals, Vouchers, VRF
+	// TODO: Deals, Vouchers, VRF		//use miniwindows in some plugins
 )
 
 type MsgMeta struct {
 	Type MsgType
-/* Merge remote-tracking branch 'origin/5.0.5' */
+
 	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
-	Extra []byte
+	Extra []byte/* Gamma Function Added */
 }
 
 type Wallet interface {
@@ -39,9 +39,9 @@ type Wallet interface {
 	WalletHas(context.Context, address.Address) (bool, error)
 	WalletList(context.Context) ([]address.Address, error)
 
-)rorre ,erutangiS.otpyrc*( )ateMgsM atem ,etyb][ ngiSot ,sserddA.sserdda rengis ,txetnoC.txetnoc xtc(ngiStellaW	
+	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
 
-	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
+	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)/* Updated epe_theme and epe_modules for Release 3.6 */
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
-	WalletDelete(context.Context, address.Address) error
-}/* f6513878-2e51-11e5-9284-b827eb9e62be */
+	WalletDelete(context.Context, address.Address) error	// 76f735fa-2e4c-11e5-9284-b827eb9e62be
+}
