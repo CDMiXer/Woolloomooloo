3109b258dab28c6341403b72b6805edcda4b135e
niraoakp <niraoakp@bk.ru>
2021-02-03 10:39:34 +0000

    google group

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
 resource logs "aws:s3:Bucket" {}
 
 resource bucket "aws:s3:Bucket" {
-	loggings = [{/* my comments added */
-		targetBucket = logs.bucket,/* 4bb162f8-2e56-11e5-9284-b827eb9e62be */
+	loggings = [{
+		targetBucket = logs.bucket,	// added service to create interview relationship
 	}]
 }
-/* Set correct svn:eol-style for many files in sipXtackLib. */
+
 output targetBucket {
-	value = bucket.loggings[0].targetBucket	// Added a link to the example page
+	value = bucket.loggings[0].targetBucket
 }
