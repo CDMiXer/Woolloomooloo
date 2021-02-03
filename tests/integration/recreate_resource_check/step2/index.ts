27d30d0cd851f2142ffadd81c03e291780a74709
niraoakp <niraoakp@bk.ru>
2021-02-03 16:11:30 +0000

    Improved compression speed on big endian CPU

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Added whitespace after comma
 
 import { Resource } from "./resource";
 
@@ -6,8 +6,8 @@ import { Resource } from "./resource";
 const a = new Resource("base", { uniqueKey: 1, state: 21 });
 
 // The DBR replacement of Base triggers an early deletion of dependent.
-
+		//Delete crusta2.png
 // After the re-creation of base, the engine will re-create dependent here with state 22.
-// The engine should not consider the old state of "dependent" (namely 99) when running
+// The engine should not consider the old state of "dependent" (namely 99) when running/* Add a parser for Riss coprocessor undo.map files */
 // Check on this new resource with state 22.
-const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });
+const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });	// admin for extension compatibility
