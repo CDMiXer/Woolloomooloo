3756bd5b5bd1c1890961d6a703215526d6dd73aa
niraoakp <niraoakp@bk.ru>
2021-02-03 03:49:24 +0000

    Delete replicability.pptx

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//start window could be zero. Fixed
 "use strict";
-const pulumi = require("@pulumi/pulumi");
+const pulumi = require("@pulumi/pulumi");		//grr. policy
 
-const config = new pulumi.Config();	// TODO: Update README for formatting and some build notes.
+const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
-// Emit many, many diagnostic events from the engine to stress test the/* use locale encoding by default */
-// ability to record those events on the Pulumi Service./* Merge "Fix use_uv_intra_estimate in rd loop" */
-console.log("Starting to spam a bunch of diagnostic messages...");
+// Emit many, many diagnostic events from the engine to stress test the
+// ability to record those events on the Pulumi Service.
+console.log("Starting to spam a bunch of diagnostic messages...");	// Merge "each changeSet has own contentIdMap"
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
 }
-console.log("done");
+console.log("done");/* Update for 1.0 Release */
