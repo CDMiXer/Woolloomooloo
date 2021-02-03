10295a1ff6067f0a9ac05c5b7a11d053817dc376
niraoakp <niraoakp@bk.ru>
2021-02-03 16:08:54 +0000

    lowered the running time by minimizing database trips

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc./* Added option cutinId for separate execution. */
-//
+// Copyright 2019 Drone IO, Inc.	// Adding description to Google work; updating paper citation.
+//	// Update gmodserver version id
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,8 +8,8 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: Making ready for next release cycle 4.5.1 */
-// See the License for the specific language governing permissions and	// TODO: will be fixed by alex.gaynor@gmail.com
-// limitations under the License.	// TODO: Changed quote position.
-
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/* change "History" => "Release Notes" */
 package branches
