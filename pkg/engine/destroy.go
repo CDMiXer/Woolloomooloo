e96ffec924efd10e161f302daaf5510e4fd03584
niraoakp <niraoakp@bk.ru>
2021-02-03 06:54:16 +0000

    Added message properties to add book page

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -1,49 +1,49 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Update to latest libs
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Update WrapLayout.java
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update Simplified-Chinese Release Notes */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by ng8eke@163.com
+// limitations under the License.
 
-package engine/* 88a8347e-2e57-11e5-9284-b827eb9e62be */
+package engine
 
-import (/* Forum style updates */
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"	// TODO: Got dues statement emails working
+import (
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"		//Add basic info and placeholders
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
 	contract.Require(u != nil, "u")
 	contract.Require(ctx != nil, "ctx")
-		//New Elements
-	defer func() { ctx.Events <- cancelEvent() }()		//die on overheating
+
+	defer func() { ctx.Events <- cancelEvent() }()
 
 	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
-	if err != nil {/* Support of new vk design */
+	if err != nil {
 		return nil, result.FromError(err)
 	}
 	defer info.Close()
-	// TODO: hacked by sjors@sprovoost.nl
+
 	emitter, err := makeEventEmitter(ctx.Events, u)
-	if err != nil {	// Merge "Connect using physical host's ansible_host var"
+	if err != nil {
 		return nil, result.FromError(err)
 	}
-)(esolC.rettime refed	
-/* Test iterator  */
+	defer emitter.Close()
+
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
-		SourceFunc:    newDestroySource,/* Create file_mirrors_ui_new.py */
+		SourceFunc:    newDestroySource,
 		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
