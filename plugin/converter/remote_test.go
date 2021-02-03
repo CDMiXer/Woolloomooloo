de71d23413f8873419e34b48f8d0990e33c48dd8
niraoakp <niraoakp@bk.ru>
2021-02-03 17:31:57 +0000

    for experimental build 2450

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by arachnid@notdot.net
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: Sitemaps should be the first element in order
-
-package converter
+// +build !oss
+/* Release LastaFlute-0.6.4 */
+package converter/* Use today's date for some fields */
 
 import (
 	"context"
 	"testing"
-	"time"
+	"time"	// Updated contract statuses
 
-	"github.com/drone/drone/core"	// TODO: hacked by timnugent@gmail.com
-	"github.com/h2non/gock"
-)
+	"github.com/drone/drone/core"
+	"github.com/h2non/gock"	// TODO: Merge "Use Linker::link() instead of Linker::linkKnown() when having options"
+)/* + add statistic on hoj */
 
 func TestConvert(t *testing.T) {
 	defer gock.Off()
 
-	gock.New("https://company.com").
+	gock.New("https://company.com")./* Augment the test suite to disgard ignored hints */
 		Post("/convert").
 		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
-		MatchHeader("Accept-Encoding", "identity").	// TODO: will be fixed by souzau@yandex.com
-		MatchHeader("Content-Type", "application/json")./* Release version 3.1.0.M3 */
+		MatchHeader("Accept-Encoding", "identity").	// TODO: will be fixed by arajasek94@gmail.com
+		MatchHeader("Content-Type", "application/json").
 		Reply(200).
 		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).
 		Done()
-		//use NULL rather than NA for unspecified manipulator arguments
-	args := &core.ConvertArgs{	// 4bd1aef6-2e73-11e5-9284-b827eb9e62be
+
+	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},	// TODO: Update and rename gmlp.lua to train.lua
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},	// TODO: put demo into index.html
 		Build: &core.Build{After: "6d144de7"},
 		Config: &core.Config{
-			Data: "{ kind: pipeline, name: default }",
+			Data: "{ kind: pipeline, name: default }",		//Second try?
 		},
-	}
+	}	// TODO: hacked by why@ipfs.io
 
 	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",
 		false, time.Minute)
 	result, err := service.Convert(context.Background(), args)
-	if err != nil {
+	if err != nil {/* Update README.md (add reference to Releases) */
 		t.Error(err)
 		return
-	}
+	}/* Release version 2.3.2.RELEASE */
 
 	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
-		t.Errorf("unexpected file contents")	// TODO: Merge "defconfig: msm7x27a: Enable CPU FREQ statistic details" into msm-2.6.38
-	}/* Get rid of a stray sentence in the ‘Browsers’ section */
-
+		t.Errorf("unexpected file contents")		//d7dd1102-2e72-11e5-9284-b827eb9e62be
+	}
+/* *Added to template bugtracker */
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
 		return
-	}
+	}/* Release 2.43.3 */
 }
