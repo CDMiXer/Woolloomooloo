85367554c15a0b396bb67189f880cf2ba5848672
niraoakp <niraoakp@bk.ru>
2021-02-03 00:21:00 +0000

    Delete drawing_tool2

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release ver 1.4.0-SNAPSHOT */
+// that can be found in the LICENSE file.
 
-package web	// TODO: Consigo remover variável, mas ainda não altero nome nem valor.
+package web
 
-// func TestHandleVersion(t *testing.T) {/* 1.9.0 Release Message */
+// func TestHandleVersion(t *testing.T) {
 // 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
 
-// 	w := httptest.NewRecorder()		//Delete q-mystik.html
+// 	w := httptest.NewRecorder()
 // 	r := httptest.NewRequest("GET", "/version", nil)
-		//c7a88d42-2e41-11e5-9284-b827eb9e62be
+
 // 	mockVersion := &core.Version{
 // 		Source:  "github.com/octocat/hello-world",
 // 		Version: "1.0.0",
 // 		Commit:  "ad2aec",
-// 	}		//Removed svn:executable prop
+// 	}
 
 // 	h := HandleVersion(mockVersion)
-// 	h.ServeHTTP(w, r)		//version badge add
+// 	h.ServeHTTP(w, r)
 
 // 	if got, want := w.Code, 200; want != got {
 // 		t.Errorf("Want response code %d, got %d", want, got)
-// 	}		//Merge "ARM: dts: Handle 23.88Mhz Mclk support for 8916 & 8939"
+// 	}
 
 // 	got, want := &core.Version{}, mockVersion
 // 	json.NewDecoder(w.Body).Decode(got)
 // 	if !reflect.DeepEqual(got, want) {
-// 		t.Errorf("response body does match expected result")		//created with the help of scn's translation
-// 		pretty.Ldiff(t, got, want)/* Kill .type (was deprecated in 0.13, to be removed in 0.14) */
+// 		t.Errorf("response body does match expected result")
+// 		pretty.Ldiff(t, got, want)
 // 	}
-// }	// TODO: will be fixed by davidad@alum.mit.edu
+// }
