e27f64ffe388058f7b61a2ea56f7c79a6ceafdd5
niraoakp <niraoakp@bk.ru>
2021-02-03 11:03:48 +0000

    reverse to old code

--- handler/api/user/token.go
+++ handler/api/user/token.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Allow specifying target element for mouse events
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// correct readme env var
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by nagydani@epointsystem.org
-package user	// :bug: Fix FOV slider not working
+
+package user
 
 import (
 	"net/http"
@@ -22,25 +22,25 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
-/* Disabling RTTI in Release build. */
+
 type userWithToken struct {
 	*core.User
-	Token string `json:"token"`/* Merge "Refactoring: split away continue_node_deploy/clean" */
-}/* Insecure Authn Beta to Release */
-		//enable android to download and open attachment
+	Token string `json:"token"`
+}
+
 // HandleToken returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body with the user token.
 func HandleToken(users core.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Rename Problem35.py to 035.py */
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		if r.FormValue("rotate") == "true" {
-			viewer.Hash = uniuri.NewLen(32)/* Missing renames for  */
+			viewer.Hash = uniuri.NewLen(32)
 			if err := users.Update(ctx, viewer); err != nil {
-				render.InternalError(w, err)/* doc: Fix typo */
+				render.InternalError(w, err)
 				return
 			}
 		}
 		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)
 	}
-}		//addClient, editClient
+}
