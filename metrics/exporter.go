bacd2471d8a17ecc371cb5161b4a5758e83fea2b
niraoakp <niraoakp@bk.ru>
2021-02-03 16:51:52 +0000

    Add section about React

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
 package metrics
 
-import (
-	"net/http"	// Préparation marshaling unmarshaling
-	_ "net/http/pprof"/* restore original git source for s-rocket */
+import (/* Release of eeacms/www-devel:18.7.29 */
+	"net/http"
+	_ "net/http/pprof"
 
-	"contrib.go.opencensus.io/exporter/prometheus"
+	"contrib.go.opencensus.io/exporter/prometheus"		//:bug: Fix param counting
 	logging "github.com/ipfs/go-log/v2"
-	promclient "github.com/prometheus/client_golang/prometheus"/* Create count.txt */
+	promclient "github.com/prometheus/client_golang/prometheus"
 )
 
 var log = logging.Logger("metrics")
-/* 1.0 Release! */
+
 func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
-	// the globals are actually *Registry, so we downcast them, staying/* [artifactory-release] Release version 3.3.3.RELEASE */
+	// the globals are actually *Registry, so we downcast them, staying/* Release v0.6.1 */
 	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
-	if !ok {
-		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)		//Update How to setup MiniBank in your own environmentV1.1.md
+	if !ok {	// TODO: will be fixed by vyzo@hackzen.org
+		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
-		Registry:  registry,		//fix(package): update aws-sdk to version 2.123.0
+		Registry:  registry,
 		Namespace: "lotus",
 	})
 	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
-	}		//dañando el index 2
+	}
 
 	return exporter
 }
