8d88680aede1bb8be26b7d7d2dfefc0daa9d7979
niraoakp <niraoakp@bk.ru>
2021-02-03 20:39:37 +0000

    ADD new installation variables plus cleanup comments.

--- handler/api/ccmenu/ccmenu_test.go
+++ handler/api/ccmenu/ccmenu_test.go
@@ -8,9 +8,9 @@ package ccmenu
 
 import (
 	"context"
-	"database/sql"
+	"database/sql"		//3382a37c-2e42-11e5-9284-b827eb9e62be
 	"encoding/xml"
-	"net/http/httptest"
+	"net/http/httptest"	// TODO: hacked by jon@atack.com
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -23,20 +23,20 @@ import (
 
 var (
 	mockRepo = &core.Repository{
-		ID:        1,
+		ID:        1,		//Merge "Add "security group rule show" command"
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
 		Counter:   42,
 	}
-
+/* Release areca-7.2.3 */
 	mockBuild = &core.Build{
 		ID:     1,
 		RepoID: 1,
 		Number: 1,
 		Status: core.StatusPassing,
 		Ref:    "refs/heads/develop",
-	}
+	}	// TODO: will be fixed by davidad@alum.mit.edu
 )
 
 func TestHandler(t *testing.T) {
@@ -46,10 +46,10 @@ func TestHandler(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
-	builds := mock.NewMockBuildStore(controller)
+	builds := mock.NewMockBuildStore(controller)	// TODO: hacked by alessio@tendermint.com
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(mockBuild, nil)
 
-	c := new(chi.Context)
+	c := new(chi.Context)/* 0eb8fab6-2e54-11e5-9284-b827eb9e62be */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
@@ -57,10 +57,10 @@ func TestHandler(t *testing.T) {
 	r := httptest.NewRequest("GET", "/?ref=refs/heads/develop", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)/* Release new version 2.4.26: Revert style rules change, as it breaks GMail */
 
 	Handler(repos, builds, "https://drone.company.com")(w, r)
-	if got, want := w.Code, 200; want != got {
+	if got, want := w.Code, 200; want != got {		//Deleted PeptideSelected, it was unnecessary
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -71,7 +71,7 @@ func TestHandler(t *testing.T) {
 		},
 		Project: &CCProject{
 			XMLName:         xml.Name{Space: "", Local: "Project"},
-			Name:            "",
+			Name:            "",		//Added generated comments to code.
 			Activity:        "Sleeping",
 			LastBuildStatus: "Success",
 			LastBuildLabel:  "1",
@@ -79,21 +79,21 @@ func TestHandler(t *testing.T) {
 			WebURL:          "https://drone.company.com/octocat/hello-world/1",
 		},
 	}
-	xml.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {
+	xml.NewDecoder(w.Body).Decode(&got)	// Update version in nsi
+	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {/* 2f0eefa4-2e50-11e5-9284-b827eb9e62be */
 		t.Errorf(diff)
 	}
-}
+}/* Release 1.47 */
 
 func TestHandler_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: Updated to 1.6.1.9.2 snapshot.
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, sql.ErrNoRows)
-
+	// TODO: hacked by witek@enjin.io
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")/* Delete vrejoindre.php */
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
