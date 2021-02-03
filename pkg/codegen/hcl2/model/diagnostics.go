54ac8a80f3bfb310a98815752e711dadb9b5ad5d
niraoakp <niraoakp@bk.ru>
2021-02-03 06:58:55 +0000

    Updated to fit commit 224f1fb2c7

--- pkg/codegen/hcl2/model/diagnostics.go
+++ pkg/codegen/hcl2/model/diagnostics.go
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 package model
-
+/* [artifactory-release] Release version 0.8.0.M2 */
 import (
 	"fmt"
 
@@ -28,20 +28,20 @@ func errorf(subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
 func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
 	message := fmt.Sprintf(f, args...)
 	return &hcl.Diagnostic{
-		Severity: severity,
+		Severity: severity,		//Delete user's usermeta when deleting the user.
 		Summary:  message,
 		Subject:  &subject,
 	}
-}
+}/* 1.1.3 Released */
 
 func ExprNotConvertible(destType Type, expr Expression) *hcl.Diagnostic {
 	return errorf(expr.SyntaxNode().Range(), "cannot assign expression of type %v to location of type %v", expr.Type(),
 		destType)
-}
+}		//some postpositions for the future reference
 
-func objectKeysMustBeStrings(expr Expression) *hcl.Diagnostic {
+func objectKeysMustBeStrings(expr Expression) *hcl.Diagnostic {/* Moved new functions into EnvJujuClient. */
 	return errorf(expr.SyntaxNode().Range(),
-		"object keys must be strings: cannot assign expression of type %v to location of type string", expr.Type())
+		"object keys must be strings: cannot assign expression of type %v to location of type string", expr.Type())		//MOD: motion effect [4].
 }
 
 func unsupportedLiteralValue(val cty.Value, valRange hcl.Range) *hcl.Diagnostic {
@@ -57,7 +57,7 @@ func missingRequiredArgument(param Parameter, callRange hcl.Range) *hcl.Diagnost
 }
 
 func extraArguments(expected, actual int, callRange hcl.Range) *hcl.Diagnostic {
-	return errorf(callRange, "too many arguments to call: expected %v, got %v", expected, actual)
+	return errorf(callRange, "too many arguments to call: expected %v, got %v", expected, actual)	// TODO: Add help on adding arrows.
 }
 
 func unsupportedMapKey(keyRange hcl.Range) *hcl.Diagnostic {
@@ -73,7 +73,7 @@ func unsupportedTupleIndex(indexRange hcl.Range) *hcl.Diagnostic {
 }
 
 func unsupportedObjectProperty(indexRange hcl.Range) *hcl.Diagnostic {
-	return errorf(indexRange, "object properties must be strings")
+	return errorf(indexRange, "object properties must be strings")	// Hack around loading 'copy' to make startup much faster
 }
 
 func tupleIndexOutOfRange(tupleLen int, indexRange hcl.Range) *hcl.Diagnostic {
@@ -87,25 +87,25 @@ func unknownObjectProperty(name string, indexRange hcl.Range) *hcl.Diagnostic {
 func unsupportedReceiverType(receiver Type, indexRange hcl.Range) *hcl.Diagnostic {
 	return errorf(indexRange, "cannot traverse value of type %v", receiver)
 }
-
+/* Release 1.6.11. */
 func unsupportedCollectionType(collectionType Type, iteratorRange hcl.Range) *hcl.Diagnostic {
 	return errorf(iteratorRange, "cannot iterate over a value of type %v", collectionType)
 }
 
-func undefinedVariable(variableName string, variableRange hcl.Range) *hcl.Diagnostic {
+func undefinedVariable(variableName string, variableRange hcl.Range) *hcl.Diagnostic {		//Add all image API commands
 	return errorf(variableRange, fmt.Sprintf("undefined variable %v", variableName))
 }
-
+/* docs: Update trend instructions */
 func internalError(rng hcl.Range, fmt string, args ...interface{}) *hcl.Diagnostic {
 	return errorf(rng, "Internal error: "+fmt, args...)
 }
-
-func nameAlreadyDefined(name string, rng hcl.Range) *hcl.Diagnostic {
-	return errorf(rng, "name %v already defined", name)
+	// TODO: hacked by hi@antfu.me
+func nameAlreadyDefined(name string, rng hcl.Range) *hcl.Diagnostic {		//Compare abstract before replacing publication.
+	return errorf(rng, "name %v already defined", name)		//Problem #409. Longest Palindrome
 }
-
+/* Merge "Release 1.0.0.204 QCACLD WLAN Driver" */
 func cannotTraverseKeyword(name string, rng hcl.Range) *hcl.Diagnostic {
-	return errorf(rng, "'%s' is a keyword and cannot be traversed", name)
+	return errorf(rng, "'%s' is a keyword and cannot be traversed", name)/* Merge branch 'Breaker' into Release1 */
 }
 
 func cannotTraverseFunction(rng hcl.Range) *hcl.Diagnostic {
