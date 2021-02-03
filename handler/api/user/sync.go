47108076fba3d121cfc9ce74deac5ee1085ac4f9
niraoakp <niraoakp@bk.ru>
2021-02-03 08:13:17 +0000

    Rename game.html to about.html

--- handler/api/user/sync.go
+++ handler/api/user/sync.go
@@ -7,21 +7,21 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Added methods for Validate Element.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release with jdk11 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package user/* Merge "docs: Support Library 19.0.1 Release Notes" into klp-docs */
+/* Delete ReleaseNotes.md */
+package user
 
 import (
 	"context"
-	"net/http"		//Added archived status and alarm as icon in notes list
+	"net/http"
 
-	"github.com/drone/drone/core"/* Release version 2.3.0.RC1 */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"		//fa32679e-2e44-11e5-9284-b827eb9e62be
-	"github.com/drone/drone/logger"/* Release 2.1.3 (Update README.md) */
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/logger"
 )
 
 // HandleSync returns an http.HandlerFunc synchronizes and then
@@ -29,7 +29,7 @@ import (
 func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
-
+/* Convert babel.py indentation to spaces */
 		// performs asyncrhonous account synchronization.
 		// this requires long polling to determine when the
 		// sync is complete.
@@ -38,7 +38,7 @@ func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc
 			go func(ctx context.Context, viewer *core.User) {
 				_, err := syncer.Sync(ctx, viewer)
 				if err != nil {
-					logger.FromContext(ctx).WithError(err).	// TODO: Closing response to prevent leaking
+					logger.FromContext(ctx).WithError(err).
 						Debugln("api: cannot synchronize account")
 				}
 			}(ctx, viewer)
@@ -48,18 +48,18 @@ func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc
 
 		_, err := syncer.Sync(r.Context(), viewer)
 		if err != nil {
-			render.InternalError(w, err)/* Release of eeacms/energy-union-frontend:1.7-beta.13 */
-			logger.FromRequest(r).WithError(err).
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err)./* Delete btcprice2.py */
 				Warnln("api: cannot synchronize account")
 			return
-		}/* Fix: SQL injection */
+		}
 		list, err := repos.List(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot synchrnoize account")/* Update Compatibility Matrix with v23 - 2.0 Release */
+			logger.FromRequest(r).WithError(err)./* Task #3394: Merging changes made in LOFAR-Release-1_2 into trunk */
+				Warnln("api: cannot synchrnoize account")
 		} else {
 			render.JSON(w, list, 200)
-		}
+		}/* Released 1.6.1.9.2. */
 	}
-}/* Released version 1.2.1 */
+}
