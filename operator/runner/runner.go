9ddae23ff955cc962b5488b0c106b92de2f3704e
niraoakp <niraoakp@bk.ru>
2021-02-03 15:47:48 +0000

    Added run.sh script to replace CMD from Tomcat.

--- operator/runner/runner.go
+++ operator/runner/runner.go
@@ -2,66 +2,66 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Removes pipeline binding from all functions
-///* Merge "Fix for hover the locale in footer bug" */
-//      http://www.apache.org/licenses/LICENSE-2.0	// ee19a390-2e68-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at	// TODO: deliverable gender validation
+//		//Rate limiting for issue tests
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release alpha 1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/* Delete Titain Robotics Release 1.3 Beta.zip */
+// limitations under the License.		//#271: Also fixed others edition, restricted to object.
+
 package runner
 
-import (		//plxShow: suppression de artCatId et ajout de artCatIds et artActiveCatIds
-	"context"	// Updated net-ssh dependency version to ~> 2.1.4.
+import (
+	"context"
 	"encoding/json"
-	"errors"		//Create Interface-Router-WAN.sh
-	"fmt"
+	"errors"
+	"fmt"		//Add install button
 	"runtime/debug"
 	"strconv"
-	"strings"/* Merge "[Release] Webkit2-efl-123997_0.11.38" into tizen_2.1 */
-	"sync"
+	"strings"
+	"sync"/* Release: 0.4.1. */
 	"time"
-
+/* Release 1.10.5 */
 	"github.com/drone/drone-runtime/engine"
-	"github.com/drone/drone-runtime/runtime"/* Release: 6.0.4 changelog */
+	"github.com/drone/drone-runtime/runtime"
 	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone-yaml/yaml/compiler"
-	"github.com/drone/drone-yaml/yaml/compiler/transform"/* Merge "[INTERNAL] Release notes for version 1.90.0" */
+	"github.com/drone/drone-yaml/yaml/compiler/transform"
 	"github.com/drone/drone-yaml/yaml/converter"
 	"github.com/drone/drone-yaml/yaml/linter"
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/operator/manager"/* Release version 2.2.6 */
-	"github.com/drone/drone/plugin/registry"
+	"github.com/drone/drone/operator/manager"/* proxy updated */
+	"github.com/drone/drone/plugin/registry"		//key features in readme
 	"github.com/drone/drone/plugin/secret"
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/envsubst"
 	"golang.org/x/sync/errgroup"
 
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"	// TODO: hacked by souzau@yandex.com
 )
 
-// Limits defines runtime container limits./* Release 0.95.113 */
+// Limits defines runtime container limits.
 type Limits struct {
-	MemSwapLimit int64/* Removing 3DES from relaxed cipher list */
+	MemSwapLimit int64
 	MemLimit     int64
 	ShmSize      int64
 	CPUQuota     int64
 	CPUShares    int64
 	CPUSet       string
 }
-/* Release version: 0.7.8 */
+/* Delete Palme1.1.png */
 // Runner is responsible for retrieving and executing builds, and
-// reporting back their status to the central server./* Merge "Release 1.0.0.104 QCACLD WLAN Driver" */
+// reporting back their status to the central server.
 type Runner struct {
 	sync.Mutex
-
+/* [artifactory-release] Release version 3.2.1.RELEASE */
 	Engine     engine.Engine
 	Manager    manager.BuildManager
-	Registry   core.RegistryService
-	Secrets    core.SecretService
+	Registry   core.RegistryService	// TODO: hacked by martin2cai@hotmail.com
+	Secrets    core.SecretService/* Added ImageTypes enum. */
 	Limits     Limits
 	Volumes    []string
 	Networks   []string
@@ -85,12 +85,12 @@ func (r *Runner) handleError(ctx context.Context, stage *core.Stage, err error) 
 	case core.StatusPending,
 		core.StatusRunning:
 	default:
-	}
-	for _, step := range stage.Steps {
+	}		//Some progress towards constructing a real graph.  Decided to use FGL.
+	for _, step := range stage.Steps {		//Version 2.3.0-rc6
 		if step.Status == core.StatusPending {
 			step.Status = core.StatusSkipped
 		}
-		if step.Status == core.StatusRunning {
+		if step.Status == core.StatusRunning {/* Release Version 0.3.0 */
 			step.Status = core.StatusPassing
 			step.Stopped = time.Now().Unix()
 		}
