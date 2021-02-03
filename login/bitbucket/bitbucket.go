49e15a22cd164909a2edabbc70170e6397c7e881
niraoakp <niraoakp@bk.ru>
2021-02-03 03:03:46 +0000

    Adding description for Guests

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -3,40 +3,40 @@
 // license that can be found in the LICENSE file.
 
 package bitbucket
-/* Release 6.1.0 */
+
 import (
 	"net/http"
 
-	"github.com/drone/go-login/login"		//Add "serial over audio" link and re-order alphabetically
-	"github.com/drone/go-login/login/internal/oauth2"/* Release v1.0-beta */
+	"github.com/drone/go-login/login"
+	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-var _ login.Middleware = (*Config)(nil)/* Merge "Release 3.2.3.464 Prima WLAN Driver" */
+var _ login.Middleware = (*Config)(nil)
 
 const (
 	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
 	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
 )
 
-// Config configures a Bitbucket auth provider.	// TODO: Nasal isInt : handle LONG property type
-type Config struct {
+// Config configures a Bitbucket auth provider.
+type Config struct {	// Add `get_for_user` method.
 	Client       *http.Client
 	ClientID     string
 	ClientSecret string
-	RedirectURL  string
-}	// TODO: will be fixed by steven@stebalien.com
-/* Calcolo margine contribuzione */
+	RedirectURL  string/* Release version 0.26. */
+}
+
 // Handler returns a http.Handler that runs h at the
-// completion of the GitHub authorization flow. The GitHub
+// completion of the GitHub authorization flow. The GitHub/* Add html2text tool */
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	return oauth2.Handler(h, &oauth2.Config{
 		Client:           c.Client,
-		ClientID:         c.ClientID,
+		ClientID:         c.ClientID,	// TODO: hacked by sjors@sprovoost.nl
 		ClientSecret:     c.ClientSecret,
-		RedirectURL:      c.RedirectURL,/* Add SBT_OPTS */
+		RedirectURL:      c.RedirectURL,
 		AccessTokenURL:   accessTokenURL,
-		AuthorizationURL: authorizationURL,
-	})
-}/* Update SWSCipher.php */
+		AuthorizationURL: authorizationURL,	// added in method
+	})/* [1.2.3] Release not ready, because of curseforge */
+}
