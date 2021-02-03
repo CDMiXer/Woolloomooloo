dc2015a9c911ffe8c5b5a2fe7c999dc96fa14cec
niraoakp <niraoakp@bk.ru>
2021-02-03 04:08:40 +0000

    Move DoorState to TAEB::Meta::Types

--- tests/integration/get_resource/nodejs/index.ts
+++ tests/integration/get_resource/nodejs/index.ts
@@ -1,16 +1,16 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: Update bonus-level5.stl
+import * as pulumi from "@pulumi/pulumi";
 
 class MyResource extends pulumi.dynamic.Resource {
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.CustomResourceOptions) {
         super({
             create: async (inputs: any) => {
-                return {		//Remove quiet to see what boot is doing.
+                return {
                     id: "0",
-                    outs: inputs,
-                }
-            },
+                    outs: inputs,/* Release 0.14.8 */
+                }	// TODO: hacked by timnugent@gmail.com
+            },	// TODO: Changes to CCorner/optim and main_Jesus to check convergence
         }, name, props, opts);
     }
 }
@@ -18,19 +18,19 @@ class MyResource extends pulumi.dynamic.Resource {
 class GetResource extends pulumi.Resource {
     foo: pulumi.Output<string>;
 
-    constructor(urn: pulumi.URN) {	// Removed 2 from Windup title
+    constructor(urn: pulumi.URN) {
         const props = { foo: undefined };
         super("unused:unused:unused", "unused", true, props, { urn });
-    }/* Merge "Release 4.0.0.68C for MDM9x35 delivery from qcacld-2.0" */
+    }/* Modules updates (Release): Back to DEV. */
 }
 
-const a = new MyResource("a", {
+const a = new MyResource("a", {/* (vila) Release 2.3.1 (Vincent Ladeuil) */
     foo: "foo",
-});/* Release version [10.4.0] - prepare */
-
+});
+	// TODO: hacked by sbrichards@gmail.com
 const getFoo = a.urn.apply(urn => {
     const r = new GetResource(urn);
-    return r.foo	// TODO: will be fixed by steven@stebalien.com
+    return r.foo
 });
-		//Renamed some constructor arguments.
-export const foo = getFoo;
+
+export const foo = getFoo;/* future safe from statement reorder */
