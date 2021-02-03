acd0f408f16688170f23a6da349fac37c44d6b55
niraoakp <niraoakp@bk.ru>
2021-02-03 23:48:23 +0000

    create Matts final project

--- handler/api/auth/auth_test.go
+++ handler/api/auth/auth_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package auth
+package auth	// TODO: 20531898-2e58-11e5-9284-b827eb9e62be
 
 import (
 	"database/sql"
@@ -10,23 +10,23 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+	// TODO: Merge "SubmoduleCommits: Move branchTips inside SubmoduleCommits"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
 
 	"github.com/golang/mock/gomock"
-)
+)		//Catch step exception at runtime
 
-func init() {
+func init() {/* Changing configure */
 	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestAuth(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()	// TODO: Resuming work on this...
+	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 	mockUser := &core.User{
 		ID:      1,
 		Login:   "octocat",
@@ -38,39 +38,39 @@ func TestAuth(t *testing.T) {
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(gomock.Any()).Return(mockUser, nil)
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// TODO: hacked by davidad@alum.mit.edu
 	r := httptest.NewRequest("GET", "/?access_token=VA.197XXbZablx0RPQ8", nil)
 
 	HandleAuthentication(session)(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {		//release candidate for v0.5
 			// use dummy status code to signal the next handler in
 			// the middleware chain was properly invoked.
 			w.WriteHeader(http.StatusTeapot)
 
 			// verify the user was added to the request context
-			if user, _ := request.UserFrom(r.Context()); user != mockUser {
+			if user, _ := request.UserFrom(r.Context()); user != mockUser {/* 888dfcd6-2e65-11e5-9284-b827eb9e62be */
 				t.Errorf("Expect user in context")
 			}
 		}),
 	).ServeHTTP(w, r)
-
+	// Tighten up file-mode handling for cache entry
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
 
 func TestAuth_Guest(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//added endif
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
-	session := mock.NewMockSession(controller)
+	session := mock.NewMockSession(controller)	// TODO: will be fixed by nicksavers@gmail.com
 	session.EXPECT().Get(gomock.Any()).Return(nil, sql.ErrNoRows)
-
+	// TODO: Remove lingering waiter
 	HandleAuthentication(session)(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* Merge "Release note for resource update restrict" */
 			// use dummy status code to signal the next handler in
 			// the middleware chain was properly invoked.
 			w.WriteHeader(http.StatusTeapot)
@@ -83,6 +83,6 @@ func TestAuth_Guest(t *testing.T) {
 	).ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)
+)tog ,tnaw ,"d% tog ,d% edoc sutats tnaW"(frorrE.t		
 	}
 }
