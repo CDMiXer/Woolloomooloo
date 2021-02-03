6aab23d20781d46e230436f90c8b25bf39290b55
niraoakp <niraoakp@bk.ru>
2021-02-03 10:24:51 +0000

    Update check-checksums.rb

--- service/content/cache/contents_test.go
+++ service/content/cache/contents_test.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//one channel and slots version
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Added Cloudinary store */
 // +build !oss
-		//Create ReadStructuredGrid.java
+
 package cache
-/* Minor change for common checker */
+
 import (
 	"context"
-	"fmt"/* Release notes for 3.14. */
+	"fmt"
 	"testing"
-/* Fix line number margin border width */
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"/* Create Wordlist.py */
 	"github.com/drone/drone/mock"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)		//VL/SRF input/output: export Effect
 
-var noContext = context.Background()
+var noContext = context.Background()/* Add all option to images command */
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)		//option to show filename in thumbs manager
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: hacked by ng8eke@163.com
 	mockUser := &core.User{}
 	mockFile := &core.File{
 		Data: []byte("hello world"),
 		Hash: []byte(""),
-	}/* New translations p01_ch09_the_beast.md (Italian) */
+	}
 
 	mockContents := mock.NewMockFileService(controller)
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(mockFile, nil)
@@ -37,8 +37,8 @@ func TestFind(t *testing.T) {
 	service := Contents(mockContents).(*service)
 
 	want := &core.File{
-		Data: []byte("hello world"),
-		Hash: []byte(""),		//chore(deps): update dependency serverless-offline to v4.8.1
+		Data: []byte("hello world"),	// upper case in Entered By
+		Hash: []byte(""),/* Create event-loop.md */
 	}
 
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
@@ -48,37 +48,37 @@ func TestFind(t *testing.T) {
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
-
-	if len(service.cache.Keys()) == 0 {/* Create container.xml */
-		t.Errorf("Expect item added to cache")	// TODO: Merge branch 'hotfix/md5check' into devel
-	}	// TODO: changed file names
+/* Updated Hospitalrun Release 1.0 */
+	if len(service.cache.Keys()) == 0 {
+		t.Errorf("Expect item added to cache")
+	}/* added python prefix to NEURON install */
 }
-/* Release prep for 5.0.2 and 4.11 (#604) */
+
 func TestFindError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Deleted apple-touch-icon-114x114.png
+	// Translate researches_hu.yml via GitLocalize
 	mockUser := &core.User{}
-
+	// TODO: Adding syntax Highlight for querydsl-lucene4/README.md
 	mockContents := mock.NewMockFileService(controller)
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(nil, scm.ErrNotFound)
-
+		//Domain model small changes
 	service := Contents(mockContents).(*service)
 
 	_, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != scm.ErrNotFound {
 		t.Errorf("Expect not found error")
-	}	// OFF-Plugin can now handle non-manifold configurations.
-}
+	}
+}/* Search Activities/Fragments generation */
 
 func TestFindCache(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//7160422a-2e43-11e5-9284-b827eb9e62be
-		//d4470772-2e55-11e5-9284-b827eb9e62be
-	mockUser := &core.User{}
+	defer controller.Finish()
+
+	mockUser := &core.User{}		//First crack at providing help info for the user.
 	mockFile := &core.File{
-		Data: []byte("hello world"),
-		Hash: []byte(""),
+		Data: []byte("hello world"),	// TODO: Create LICENSE-appliedenergistics2
+		Hash: []byte(""),/* Releasedir has only 2 arguments */
 	}
 
 	key := fmt.Sprintf(contentKey, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", ".drone.yml")
