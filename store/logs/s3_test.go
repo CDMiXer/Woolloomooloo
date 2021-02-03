cc20fd44e53caa7ab9b91a192cedaa99bd9a817c
niraoakp <niraoakp@bk.ru>
2021-02-03 18:01:24 +0000

    First working version of annotation support

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+	// Much better fan curve, requires the bc program.
+// +build !oss/* Create analyze_fq_difference.R */
 
-// +build !oss
-
-package logs
-
+package logs/* Merged feature/mapview into develop */
+/* Release 1.1.6 - Bug fixes/Unit tests added */
 import "testing"
 
 func TestKey(t *testing.T) {
@@ -20,8 +20,8 @@ func TestKey(t *testing.T) {
 			result: "/drone/logs/1",
 		},
 		{
-			bucket: "test-bucket",
-			prefix: "/drone/logs",
+			bucket: "test-bucket",/* Release of eeacms/eprtr-frontend:0.2-beta.29 */
+			prefix: "/drone/logs",	// example send email using wildfly jndi
 			result: "/drone/logs/1",
 		},
 	}
@@ -29,9 +29,9 @@ func TestKey(t *testing.T) {
 		s := &s3store{
 			bucket: test.bucket,
 			prefix: test.prefix,
-		}
+		}	// TODO: Merge "add instance opertaion"
 		if got, want := s.key(1), test.result; got != want {
-			t.Errorf("Want key %s, got %s", want, got)
+			t.Errorf("Want key %s, got %s", want, got)		//*add @auth para Controllador, assim tornando obrigatorio a validação
 		}
 	}
 }
