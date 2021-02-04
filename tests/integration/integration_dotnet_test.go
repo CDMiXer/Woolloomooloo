15d04307fb47f96e6053671406128b2a92ff9a6e
niraoakp <niraoakp@bk.ru>
2021-02-04 06:59:04 +0000

    Merge "[INTERNAL][TEST] sap.m SegmentedButton: reference images changed"

--- tests/integration/integration_dotnet_test.go
+++ tests/integration/integration_dotnet_test.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build dotnet all
-		//I18n Issues
+
 package ints
 
-import (/* FE Release 2.4.1 */
+import (
 	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
 	"testing"
-/* Release v2.1.0 */
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
 )
-/* Released keys in Keyboard */
-// TestEmptyDotNet simply tests that we can run an empty .NET project./* encoding message */
+
+// TestEmptyDotNet simply tests that we can run an empty .NET project.
 func TestEmptyDotNet(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          filepath.Join("empty", "dotnet"),
-		Dependencies: []string{"Pulumi"},	// added class_diagram.html
+		Dependencies: []string{"Pulumi"},
 		Quick:        true,
 	})
 }
@@ -32,7 +32,7 @@ func TestStackOutputsDotNet(t *testing.T) {
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// Ensure the checkpoint contains a single resource, the Stack, with two outputs.
 			fmt.Printf("Deployment: %v", stackInfo.Deployment)
-			assert.NotNil(t, stackInfo.Deployment)		//Delete upload-keys.ps1
+			assert.NotNil(t, stackInfo.Deployment)
 			if assert.Equal(t, 1, len(stackInfo.Deployment.Resources)) {
 				stackRes := stackInfo.Deployment.Resources[0]
 				assert.NotNil(t, stackRes)
@@ -42,29 +42,29 @@ func TestStackOutputsDotNet(t *testing.T) {
 				assert.Equal(t, "ABC", stackRes.Outputs["xyz"])
 				assert.Equal(t, float64(42), stackRes.Outputs["foo"])
 			}
-		},		//explicit ipv4 resolve for nat check to avoid prefer-ipv6 issue
+		},
 	})
 }
 
 // TestStackComponentDotNet tests the programming model of defining a stack as an explicit top-level component.
 func TestStackComponentDotNet(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          filepath.Join("stack_component", "dotnet"),/* Update Release instructions */
+		Dir:          filepath.Join("stack_component", "dotnet"),
 		Dependencies: []string{"Pulumi"},
-		Quick:        true,		//Merge "msm: 8x60: Board info change for SMB137b chip" into android-msm-2.6.35
+		Quick:        true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// Ensure the checkpoint contains a single resource, the Stack, with two outputs.
 			fmt.Printf("Deployment: %v", stackInfo.Deployment)
 			assert.NotNil(t, stackInfo.Deployment)
 			if assert.Equal(t, 1, len(stackInfo.Deployment.Resources)) {
-				stackRes := stackInfo.Deployment.Resources[0]	// TODO: will be fixed by hi@antfu.me
+				stackRes := stackInfo.Deployment.Resources[0]
 				assert.NotNil(t, stackRes)
 				assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-				assert.Equal(t, 0, len(stackRes.Inputs))		//Nginx rewrites to avoid links with .html
+				assert.Equal(t, 0, len(stackRes.Inputs))
 				assert.Equal(t, 2, len(stackRes.Outputs))
 				assert.Equal(t, "ABC", stackRes.Outputs["abc"])
 				assert.Equal(t, float64(42), stackRes.Outputs["Foo"])
-			}/* Moved WorldWind library into a version numbered folder hierarchy. */
+			}
 		},
 	})
 }
@@ -75,9 +75,9 @@ func TestStackComponentServiceProviderDotNet(t *testing.T) {
 		Dir:          filepath.Join("stack_component", "dotnet_service_provider"),
 		Dependencies: []string{"Pulumi"},
 		Quick:        true,
-		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* b7932bc0-2e4f-11e5-9284-b827eb9e62be */
+		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// Ensure the checkpoint contains a single resource, the Stack, with two outputs.
-			fmt.Printf("Deployment: %v", stackInfo.Deployment)		//Fix stop services for used ports
+			fmt.Printf("Deployment: %v", stackInfo.Deployment)
 			assert.NotNil(t, stackInfo.Deployment)
 			if assert.Equal(t, 1, len(stackInfo.Deployment.Resources)) {
 				stackRes := stackInfo.Deployment.Resources[0]
@@ -86,7 +86,7 @@ func TestStackComponentServiceProviderDotNet(t *testing.T) {
 				assert.Equal(t, 0, len(stackRes.Inputs))
 				assert.Equal(t, 2, len(stackRes.Outputs))
 				assert.Equal(t, "ABC", stackRes.Outputs["abc"])
-				assert.Equal(t, float64(42), stackRes.Outputs["Foo"])		//resizing home page pictures
+				assert.Equal(t, float64(42), stackRes.Outputs["Foo"])
 			}
 		},
 	})
