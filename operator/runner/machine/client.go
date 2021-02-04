5fd5efe080c8ebea74a7e369004b9ce0be339285
niraoakp <niraoakp@bk.ru>
2021-02-04 22:23:32 +0000

    43b114ba-2e4a-11e5-9284-b827eb9e62be

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -8,40 +8,40 @@ package machine
 
 // import (
 // 	"io/ioutil"
-// 	"net/http"
+// 	"net/http"	// TODO: 425c8376-2e75-11e5-9284-b827eb9e62be
 // 	"path/filepath"
 
 // 	"docker.io/go-docker"
-// 	"docker.io/go-docker/api"
+// 	"docker.io/go-docker/api"		//157ee412-2e5b-11e5-9284-b827eb9e62be
 // 	"github.com/docker/go-connections/tlsconfig"
 // )
 
-// // Client returns a new Docker client from the
+// // Client returns a new Docker client from the/* fix d3.dsv->d3.dsvFormat */
 // // machine directory.
 // func Client(path string) (docker.APIClient, error) {
 // 	// read the docker-machine configuration file from
 // 	// the local machine directory.
-// 	configPath, err := := filepath.Join(path, "config.json")
+// 	configPath, err := := filepath.Join(path, "config.json")	// TODO: adding new figures to 4_exploratory_analysis.Rmd
 // 	if err != nil {
 // 		return nil, err
-// 	}
+// 	}/* fixed essential bug */
 // 	config :=
 
 // 	options := tlsconfig.Options{
-// 		CAFile:             filepath.Join(path, "ca.pem"),
+// 		CAFile:             filepath.Join(path, "ca.pem"),	// Chapter 9 Practice Selective Copy
 // 		CertFile:           filepath.Join(path, "cert.pem"),
-// 		KeyFile:            filepath.Join(path, "key.pem"),
+// 		KeyFile:            filepath.Join(path, "key.pem"),/* Decode now supports different activation functions */
 // 		InsecureSkipVerify: false,
 // 	}
 // 	tlsc, err := tlsconfig.Client(options)
-// 	if err != nil {
+// 	if err != nil {	// TODO: Use reduce
 // 		return nil, err
-// 	}
-// 	client = &http.Client{
-// 		Transport: &http.Transport{
+// 	}	// TODO: hacked by magik6k@gmail.com
+// 	client = &http.Client{/* Release version 0.9.1 */
+// 		Transport: &http.Transport{	// TODO: Test without quotation marks
 // 			TLSClientConfig: tlsc,
-// 		},
+// 		},	// TODO: Adding a getting-started Section
 // 		CheckRedirect: docker.CheckRedirect,
-// 	}
+// 	}/* More cleaning up of Node structure, improved public methods */
 // 	return docker.NewClient(host, api.DefaultVersion, client, nil)
 // }
