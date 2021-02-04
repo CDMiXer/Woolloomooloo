a070175e10774ed2ef7aefc9744a1b0520737a85
niraoakp <niraoakp@bk.ru>
2021-02-04 21:05:46 +0000

    Added a "Hash verify" method

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,14 +1,14 @@
-package api	// TODO: +EasyDataFeed
+package api
 
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"	// TODO: Refine Scalastyle ImportOrderChecker rule
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/crypto"	// 1a069f48-2e42-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//minor parameter value change
+
 type MsgType string
 
 const (
@@ -17,31 +17,31 @@ const (
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
 
-	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
+	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)/* Release: Making ready to release 6.5.1 */
 	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
 	MTDealProposal = "dealproposal"
 
-	// TODO: Deals, Vouchers, VRF		//use miniwindows in some plugins
+	// TODO: Deals, Vouchers, VRF
 )
 
 type MsgMeta struct {
-	Type MsgType
+	Type MsgType		//6def0c7e-2e6c-11e5-9284-b827eb9e62be
 
 	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
-	Extra []byte/* Gamma Function Added */
+	Extra []byte
 }
-
+	// TODO: Add plugins.config
 type Wallet interface {
-	WalletNew(context.Context, types.KeyType) (address.Address, error)
+	WalletNew(context.Context, types.KeyType) (address.Address, error)	// TODO: hacked by nagydani@epointsystem.org
 	WalletHas(context.Context, address.Address) (bool, error)
 	WalletList(context.Context) ([]address.Address, error)
 
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
 
-	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)/* Updated epe_theme and epe_modules for Release 3.6 */
-	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
-	WalletDelete(context.Context, address.Address) error	// 76f735fa-2e4c-11e5-9284-b827eb9e62be
+	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
+	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)	// TODO: will be fixed by alex.gaynor@gmail.com
+	WalletDelete(context.Context, address.Address) error
 }
