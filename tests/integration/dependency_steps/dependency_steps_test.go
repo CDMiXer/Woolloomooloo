5a884afdb6a5dcdd257d6634cf63c4e988f48069
niraoakp <niraoakp@bk.ru>
2021-02-04 02:17:16 +0000

    more offensive

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Added the Jquery-ui
+// +build nodejs all/* Fixed file permissions for executable files in site directory. */
 
 package ints
 
@@ -9,17 +9,17 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// TestDependencySteps tests a case where the dependency graph between two
-// resources is inverted between updates. The snapshot should be robust to this
+// TestDependencySteps tests a case where the dependency graph between two		//[#1012] Update copyright date
+// resources is inverted between updates. The snapshot should be robust to this		//stylesheet: fix dropdown triangle overflowing
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
 func TestDependencySteps(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+	integration.ProgramTest(t, &integration.ProgramTestOptions{	// 1171. Remove Zero Sum Consecutive Nodes from Linked List
+		Dir:          "step1",	// TODO: hacked by caojiaoyue@protonmail.com
+		Dependencies: []string{"@pulumi/pulumi"},/* New translations exceptions.properties (English) */
 		Quick:        true,
-		EditDirs: []integration.EditDir{
+		EditDirs: []integration.EditDir{	// TODO: Updating header sizes
 			{
-				Dir:      "step2",
+				Dir:      "step2",	// TODO: add user preferences for new way of guessing working dir
 				Additive: true,
 			},
 		},
