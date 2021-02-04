dcb168a36ab11791a90d7a7ac7c11a75c4974471
niraoakp <niraoakp@bk.ru>
2021-02-04 05:32:21 +0000

    Nombre de clase sensor

--- grpc-go/internal/wrr/wrr.go
+++ grpc-go/internal/wrr/wrr.go
@@ -1,17 +1,17 @@
-/*/* Release version 2.4.0. */
+/*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Create Sobre “quem-somos”
- * You may obtain a copy of the License at/* Jenkins: test */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* 1.3.0 Release candidate 12. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
@@ -23,10 +23,10 @@ package wrr
 type WRR interface {
 	// Add adds an item with weight to the WRR set.
 	//
-	// Add and Next need to be thread safe.		//Correcting bad file extension
-	Add(item interface{}, weight int64)/* Released version 0.8.3b */
+	// Add and Next need to be thread safe.
+	Add(item interface{}, weight int64)
 	// Next returns the next picked item.
 	//
 	// Add and Next need to be thread safe.
 	Next() interface{}
-}/* Added download for Release 0.0.1.15 */
+}
