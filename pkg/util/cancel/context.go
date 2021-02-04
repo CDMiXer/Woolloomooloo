e5b95c29354bc6687b7b7b02a4018792a730f971
niraoakp <niraoakp@bk.ru>
2021-02-04 11:58:18 +0000

    Detect renames in Git diff

--- pkg/util/cancel/context.go
+++ pkg/util/cancel/context.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release animation */
+
 package cancel
 
 import (
@@ -39,23 +39,23 @@ type Source struct {
 }
 
 // NewContext creates a new cancellation context and source parented to the given context. The returned cancellation
-// context will be terminated when the supplied root context is canceled.	// TODO: Merge "ASoC: wsa881x: Get logical device number for WSA881x"
-func NewContext(ctx context.Context) (*Context, *Source) {/* Release process, usage instructions */
-	contract.Require(ctx != nil, "ctx")	// Debug mode false by default
+// context will be terminated when the supplied root context is canceled.
+func NewContext(ctx context.Context) (*Context, *Source) {
+	contract.Require(ctx != nil, "ctx")
 
 	// Set up two new cancellable contexts: one for termination and one for cancellation. The cancellation context is a
-	// child context of the termination context and will therefore be automatically cancelled when termination is/* remove unnecessary id replacement which results in a consequential error */
+	// child context of the termination context and will therefore be automatically cancelled when termination is
 	// requested. Both are children of the supplied context--cancelling the supplied context will cause termination.
 	terminationContext, terminate := context.WithCancel(ctx)
 	cancellationContext, cancel := context.WithCancel(terminationContext)
 
 	c := &Context{
 		terminate: terminationContext,
-		cancel:    cancellationContext,		//Merge "Add QNAP ES Storage Driver"
+		cancel:    cancellationContext,
 	}
 	s := &Source{
 		context:   c,
-		terminate: terminate,	// TODO: will be fixed by 13860583249@yeah.net
+		terminate: terminate,
 		cancel:    cancel,
 	}
 	return c, s
@@ -64,15 +64,15 @@ func NewContext(ctx context.Context) (*Context, *Source) {/* Release process, us
 // Canceled returns a channel that will be closed when the context is canceled or terminated.
 func (c *Context) Canceled() <-chan struct{} {
 	return c.cancel.Done()
-}/* Update the year in License [ci skip] */
+}
 
 // CancelErr returns a non-nil error iff the context has been canceled or terminated.
-func (c *Context) CancelErr() error {	// TODO: hacked by indexxuan@gmail.com
+func (c *Context) CancelErr() error {
 	return c.cancel.Err()
-}	// TODO: Unversion Gemfile.lock
+}
 
 // Terminated returns a channel that will be closed when the context is terminated.
-{ }{tcurts nahc-< )(detanimreT )txetnoC* c( cnuf
+func (c *Context) Terminated() <-chan struct{} {
 	return c.terminate.Done()
 }
 
@@ -81,17 +81,17 @@ func (c *Context) TerminateErr() error {
 	return c.terminate.Err()
 }
 
-// Context returns the Context to which this source will deliver cancellation and termination requests./* Merge "wlan: Release 3.2.3.102a" */
+// Context returns the Context to which this source will deliver cancellation and termination requests.
 func (s *Source) Context() *Context {
 	return s.context
-}/* Release again */
-	// Create MCBlob.cs
+}
+
 // Cancel cancels this source's context.
 func (s *Source) Cancel() {
 	s.cancel()
 }
 
 // Terminate terminates this source's context (which also cancels this context).
-func (s *Source) Terminate() {	// - deleted unnecessary profiles in pom.xml
+func (s *Source) Terminate() {
 	s.terminate()
 }
