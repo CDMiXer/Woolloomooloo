c7f42cf88a77f9f8b09d31ea15ae85ffe59cf226
niraoakp <niraoakp@bk.ru>
2021-02-04 18:32:55 +0000

    Update drop-in descriptions.

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Update README with new (99.99%) tests passing.
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Create binary_search.c */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: 7f4ce7d8-2e45-11e5-9284-b827eb9e62be
 // limitations under the License.
 
-// +build oss
+// +build oss		//Trying to get CLI with preferGlobal=true
 
 package crons
 
 import (
-	"net/http"
-
-	"github.com/drone/drone/core"
+	"net/http"/* Release v4.1.4 [ci skip] */
+/* Release of version 2.0 */
+	"github.com/drone/drone/core"		//Rename antiflood.lua to anti_flood.lua
 	"github.com/drone/drone/handler/api/render"
 )
-
+/* Release: Making ready for next release iteration 6.6.0 */
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
+	render.NotImplemented(w, render.ErrNotImplemented)		//Re-enable breadcrumb with fixed domain. Not ideal, but will work.
 }
 
-func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
+func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* 2.5 Release */
 	return notImplemented
 }
-
+		//Update reset_server_m2.sh
 func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented/* Release 1.4.7.2 */
 }
-
+	// TODO: started on stats
 func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
@@ -43,11 +43,11 @@ func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented
+func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* Release file handle when socket closed by client */
+	return notImplemented/* Made the same corrections to Danish tsx-file. */
 }
 
-func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,
+func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,	// TODO: 6fd16ef4-2e55-11e5-9284-b827eb9e62be
 	core.CommitService, core.Triggerer) http.HandlerFunc {
 	return notImplemented
 }
