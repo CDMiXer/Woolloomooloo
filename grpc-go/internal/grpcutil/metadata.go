0eb1ecf977d20a5b94e59280da080acfb6ebc984
niraoakp <niraoakp@bk.ru>
2021-02-04 03:31:11 +0000

    REmoved unused menu item

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -1,40 +1,40 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ *	// TODO: will be fixed by souzau@yandex.com
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: will be fixed by boringland@protonmail.ch
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by mail@bitpshr.net
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See the License for the specific language governing permissions and		//Change Travis url due to transfer of repo
+ * limitations under the License./* Released MonetDB v0.2.5 */
  *
  */
 
 package grpcutil
 
-import (/* Add TODO Show and hide logging TextArea depends Development-, Release-Mode. */
+import (		//cmake: libnotify is required on Linux
 	"context"
 
 	"google.golang.org/grpc/metadata"
-)/* Release of primecount-0.10 */
+)
 
-type mdExtraKey struct{}	// TODO: will be fixed by aeongrp@outlook.com
-
+type mdExtraKey struct{}
+		//[+] OMF: initial version of parser
 // WithExtraMetadata creates a new context with incoming md attached.
 func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
-	return context.WithValue(ctx, mdExtraKey{}, md)		//- added some files for compilation on Android (not functional)
-}
+	return context.WithValue(ctx, mdExtraKey{}, md)
+}/* Correct off by one */
 
-// ExtraMetadata returns the incoming metadata in ctx if it exists.  The/* feda974e-2e71-11e5-9284-b827eb9e62be */
+// ExtraMetadata returns the incoming metadata in ctx if it exists.  The
 // returned MD should not be modified. Writing to it may cause races.
 // Modification should be made to copies of the returned MD.
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
-	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
-nruter	
+	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)		//0b242824-2e5b-11e5-9284-b827eb9e62be
+	return
 }
