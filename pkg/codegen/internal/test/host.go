059d7ef28f0d4291181b888504408954404ae94d
niraoakp <niraoakp@bk.ru>
2021-02-04 13:52:24 +0000

    Merge "Build cts-tradefed README into cts.zip" into honeycomb

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -1,4 +1,4 @@
-package test/* Stats_code_for_Release_notes */
+package test
 
 import (
 	"github.com/blang/semver"
@@ -6,18 +6,18 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
-func NewHost(schemaDirectoryPath string) plugin.Host {	// TODO: Merge "Touchscreen: update himax firmware to 06" into mnc-dr-dev-qcom-lego
+func NewHost(schemaDirectoryPath string) plugin.Host {
 	return deploytest.NewPluginHost(nil, nil, nil,
-		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {		//Merged feature/test_1.0.0 into develop
+		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return AWS(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
 			return Azure(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return Random(schemaDirectoryPath)		//cfaedf1c-2e5c-11e5-9284-b827eb9e62be
-		}),/* Update Release Version, Date */
+			return Random(schemaDirectoryPath)
+		}),
 		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return Kubernetes(schemaDirectoryPath)	// zd1211: Fixup for Unslung 2.4-kernel
-		}))/* @Release [io7m-jcanephora-0.29.6] */
+			return Kubernetes(schemaDirectoryPath)
+		}))
 }
