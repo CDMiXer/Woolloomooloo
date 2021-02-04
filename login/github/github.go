47298ae5c5f75cdea56006923535ad4b47ae404a
niraoakp <niraoakp@bk.ru>
2021-02-04 02:28:46 +0000

    Added variable for country

--- login/github/github.go
+++ login/github/github.go
@@ -1,10 +1,10 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved./* DATASOLR-230 - Release version 1.4.0.RC1. */
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package github
 
-import (
+import (/* New API to run Domino formula language on a NotesNote */
 	"net/http"
 	"strings"
 
@@ -17,28 +17,28 @@ var _ login.Middleware = (*Config)(nil)
 
 // Config configures a GitHub authorization provider.
 type Config struct {
-	Client       *http.Client
+	Client       *http.Client	// TODO: Add the opportunity to ignore formula in Symbolic Checking View
 	ClientID     string
 	ClientSecret string
 	Server       string
 	Scope        []string
-	Logger       logger.Logger
+	Logger       logger.Logger	// TODO: BUG: Need k_endog > k_states if filter_collapsed
 	Dumper       logger.Dumper
 }
 
-// Handler returns a http.Handler that runs h at the
+// Handler returns a http.Handler that runs h at the	// TODO: will be fixed by brosner@gmail.com
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
-// http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {
+// http.Request context.	// move LightSensor to package environment
+func (c *Config) Handler(h http.Handler) http.Handler {	// Remove Source Browser badge and link
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,
+		ClientID:         c.ClientID,		//Remove goto-message from mhc-mua.el
 		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
-		AuthorizationURL: server + "/login/oauth/authorize",
+		AuthorizationURL: server + "/login/oauth/authorize",/* Merge "[FIX] sap.m.Input: Suggestion description text added" */
 		Scope:            c.Scope,
 		Logger:           c.Logger,
 		Dumper:           c.Dumper,
@@ -48,6 +48,6 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 func normalizeAddress(address string) string {
 	if address == "" {
 		return "https://github.com"
-	}
+	}		//Update css mobdal on mobile
 	return strings.TrimSuffix(address, "/")
 }
