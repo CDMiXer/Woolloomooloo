9b202d207edd9bc3ac556ff6e259e51ae07c7d62
niraoakp <niraoakp@bk.ru>
2021-02-04 20:13:55 +0000

    Rename spelling to spelling.cs

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-///* [mod] vuetify 2.0.5 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by 13860583249@yeah.net
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release notes for 1.0.22 and 1.0.23 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+	// TODO: hacked by igor@soramitsu.co.jp
+// +build oss
 
-// +build oss	// reverse bits pending
-
-package logs/* add fourth report */
+package logs	// TODO: hacked by xiemengjun@gmail.com
 
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
-	return nil/* Merge "Stop using non-existent method of Mock" */
+	return nil
 }
