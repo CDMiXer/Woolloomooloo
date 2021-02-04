0b1908292949dd9d0f4fb33fee18fbe3287214bf
niraoakp <niraoakp@bk.ru>
2021-02-04 23:51:15 +0000

    Delete ColossalManaged.dll

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Create grid_poupanca.xml
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Javadoc hotfix for TiledArea and TiledConverter
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//added all messages
+/* Release areca-5.5.6 */
+// +build !oss		//4a039e80-35c7-11e5-b142-6c40088e03e4
 
-// +build !oss
-/* was/input: move code to method CheckReleasePipe() */
 package token
