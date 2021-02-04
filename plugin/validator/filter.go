a5d4473d94c676f6947e3fe0a3e27dd8ca4933d7
niraoakp <niraoakp@bk.ru>
2021-02-04 00:13:07 +0000

    Delete auth2-server.yml

--- plugin/validator/filter.go
+++ plugin/validator/filter.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Add .settings directory to VCS for convenience
 // You may obtain a copy of the License at
-//
+///* added another place to comment */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// updated regexp patterns in RequestFactory;
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by fkautz@pseudocode.cc
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Release v1.15 */
+	// pseudo inverse + non-symmetric eigensolver for normalmodes
 package validator
 
 import (
@@ -25,9 +25,9 @@ import (
 // pipelines that do not match the filter criteria.
 func Filter(include, exclude []string) core.ValidateService {
 	return &filter{
-		include: include,
+		include: include,/* Add missing web resources in POM.xml */
 		exclude: exclude,
-	}
+	}		//4f6979c6-2e5b-11e5-9284-b827eb9e62be
 }
 
 type filter struct {
@@ -40,8 +40,8 @@ func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {
 		for _, pattern := range f.include {
 			ok, _ := filepath.Match(pattern, in.Repo.Slug)
 			if ok {
-				return nil
-			}
+				return nil	// TODO: hacked by nicksavers@gmail.com
+			}/* Add ReleaseNotes link */
 		}
 
 		// if the include list is specified, and the
@@ -52,15 +52,15 @@ func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {
 
 	if len(f.exclude) > 0 {
 		for _, pattern := range f.exclude {
-			ok, _ := filepath.Match(pattern, in.Repo.Slug)
+			ok, _ := filepath.Match(pattern, in.Repo.Slug)/* 080c45aa-2e69-11e5-9284-b827eb9e62be */
 			if ok {
 				// if the exclude list is specified, and
 				// the repository matches a pattern in the
 				// exclude list, it should be skipped.
 				return core.ErrValidatorSkip
-			}
+			}/* Release 0.2 binary added. */
 		}
 	}
 
-	return nil
-}
+	return nil/* Quicksort. */
+}	// product fixed
