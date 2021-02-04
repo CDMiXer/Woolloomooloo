92585902cd6693f170e4e405c21bafbf75be7fcf
niraoakp <niraoakp@bk.ru>
2021-02-04 09:15:26 +0000

    6adff920-2e4f-11e5-9284-b827eb9e62be

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -9,22 +9,22 @@ package builds
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Update formatting in Readme.md */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
 // HandleIncomplete returns an http.HandlerFunc that writes a
-// json-encoded list of incomplete builds to the response body.
+// json-encoded list of incomplete builds to the response body./* Merge "Parse out '@' in volume['host'] to do discovery" */
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		list, err := repos.ListIncomplete(r.Context())
+		list, err := repos.ListIncomplete(r.Context())	// TODO: hacked by arajasek94@gmail.com
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list incomplete builds")
-		} else {
-			render.JSON(w, list, 200)
+		} else {/* Release Candidate. */
+			render.JSON(w, list, 200)/* Release changes 4.1.2 */
 		}
 	}
 }
