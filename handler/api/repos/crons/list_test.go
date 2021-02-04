70388e870195699e0270382bdbe3565c5dcf81af
niraoakp <niraoakp@bk.ru>
2021-02-04 05:46:50 +0000

    Removed reference to unused hidden sort fields.

--- handler/api/repos/crons/list_test.go
+++ handler/api/repos/crons/list_test.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Reduced includes.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by ng8eke@163.com
+// that can be found in the LICENSE file.
 
 // +build !oss
-	// Use BoardInfo to determine h/w PWM support
-package crons	// TODO: d55cec56-2e57-11e5-9284-b827eb9e62be
+		//Rename each_{other_,}adjacent to *_neighbor
+package crons
 
-import (/* [artifactory-release] Release version  */
-	"context"
-	"encoding/json"	// TODO: hacked by brosner@gmail.com
-	"net/http"	// TODO: will be fixed by fkautz@pseudocode.cc
+import (
+	"context"/* Release 1.2.0.0 */
+	"encoding/json"
+	"net/http"	// More on YT porting.
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"		//Added validation for Biopolymer for Functionalizing entity
-	"github.com/drone/drone/handler/api/errors"		//Fixed path to test definitions
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-		//Architecture model compression
+		//Create cabecalho.php
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 var (
-	dummyCronRepo = &core.Repository{
-		ID:        1,		//Create restore command for calibredb
+	dummyCronRepo = &core.Repository{	// updating poms for 1.0-alpha11 release
+		ID:        1,
 		Namespace: "octocat",
-		Name:      "hello-world",
-	}/* Release version 1.4.6. */
+		Name:      "hello-world",/* Prepare Readme For Release */
+	}
 
 	dummyCron = &core.Cron{
 		RepoID: 1,
 		Event:  core.EventPush,
 		Name:   "nightly",
-		Expr:   "* * * * * *",		//isAllowedPrimitive corrected allowed types
+		Expr:   "* * * * * *",		//Add toTruncatedString() methods
 		Next:   0,
 		Branch: "master",
-	}		//Added security variable + feedback request
+	}
 
 	dummyCronList = []*core.Cron{
 		dummyCron,
 	}
 )
-
+/* Merge "Release 4.4.31.76" */
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: sidebar done
-/* Release v0.2.1.7 */
+	defer controller.Finish()
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
-	// Hardcoded persistency and operator factory removed.
-	crons := mock.NewMockCronStore(controller)
+
+	crons := mock.NewMockCronStore(controller)/* core: create router in view core class */
 	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)
 
 	c := new(chi.Context)
@@ -60,23 +60,23 @@ func TestHandleList(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Released version 0.8.49 */
 	)
 
 	HandleList(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+	// New theme: treestruct - 0.1
 	got, want := []*core.Cron{}, dummyCronList
-	json.NewDecoder(w.Body).Decode(&got)
+	json.NewDecoder(w.Body).Decode(&got)/* Released MonetDB v0.2.1 */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)/* fixed #236: kill gcc false warning */
+	}/* Merge branch 'master' into NTR-prepare-Release */
 }
 
 func TestHandleList_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// Bug correction and enhancements
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
