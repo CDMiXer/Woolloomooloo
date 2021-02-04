9bfc69bc5133fbf3ef83c96bf1354a9065072c7f
niraoakp <niraoakp@bk.ru>
2021-02-04 12:17:52 +0000

    Update and rename study/html5.html to tutorials/html/html5.html

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package tests		//Added metal block and slab models
-
-import (/* First Release of the Plugin on the Update Site. */
-	"fmt"	// TODO: Path fixes and removed php 5.4 from travis
-	"os"
+package tests
+/* Improved indexed metadata */
+import (
+	"fmt"/* Fixing security issue with request module del */
+	"os"/* Ignore files generated with the execution of the Maven Release plugin */
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"/* json: remove not used workaround for json parser with gcc 4.8.x */
+	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 )
-
-func TestMain(m *testing.M) {/* Starting conversion for high level api based on pos ids per field */
+/* Release Candidate 0.5.6 RC4 */
+func TestMain(m *testing.M) {
 	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
-	// backups of test stacks.
-	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
-		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)/* Create install_database_objects.ps1 */
-		os.Exit(1)/* SIG-Release leads updated */
+	// backups of test stacks./* Releases and maven details */
+	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {		//address requested changes
+		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
+		os.Exit(1)
 	}
-
+		//Better failure handling while putting
 	code := m.Run()
 	os.Exit(code)
 }
