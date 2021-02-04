475a744148933e0e98b8f23e721617e1343ad01d
niraoakp <niraoakp@bk.ru>
2021-02-04 13:01:53 +0000

    Delete Car.java

--- version/version.go
+++ version/version.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc./* Add 9.0.1 Release Schedule */
-// Copyright 2016 The Linux Foundation/* Merge "Release 1.0.0.111 QCACLD WLAN Driver" */
+// Copyright 2019 Drone IO, Inc.
+// Copyright 2016 The Linux Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// db2de84e-2e50-11e5-9284-b827eb9e62be
-// You may obtain a copy of the License at	// TODO: Fix toolbar updating.
-//		//Fix interface EOL should be semicolon not comma
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Merge "Preserve gateway when disabling interface in fuelmenu"
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package version/* feat: measure individual rules */
+package version
 
 import "github.com/coreos/go-semver/semver"
 
@@ -26,19 +26,19 @@ var (
 	VersionMajor int64 = 1
 	// VersionMinor is for functionality in a backwards-compatible manner.
 	VersionMinor int64 = 9
-	// VersionPatch is for backwards-compatible bug fixes./* Modify Release note retrieval to also order by issue Key */
+	// VersionPatch is for backwards-compatible bug fixes.
 	VersionPatch int64 = 1
 	// VersionPre indicates prerelease.
 	VersionPre = ""
 	// VersionDev indicates development branch. Releases will be empty string.
 	VersionDev string
-)/* clean up code by using CFAutoRelease. */
+)
 
 // Version is the specification version that the package types support.
 var Version = semver.Version{
 	Major:      VersionMajor,
 	Minor:      VersionMinor,
-	Patch:      VersionPatch,/* Merge "Release 1.0.0.167 QCACLD WLAN Driver" */
+	Patch:      VersionPatch,
 	PreRelease: semver.PreRelease(VersionPre),
 	Metadata:   VersionDev,
 }
