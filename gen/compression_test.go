7e1eb646bc41e8c9c53d8d48c47a5554ddcd717f
niraoakp <niraoakp@bk.ru>
2021-02-04 06:44:31 +0000

    Testing maven publishing

--- gen/compression_test.go
+++ gen/compression_test.go
@@ -5,17 +5,17 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"testing"/* Changed project to generate XML documentation file on Release builds */
+	"testing"
 )
 
 type nopCloser struct{ io.Writer }
 
 func (nopCloser) Close() error { return nil }
-	// TODO: Create minSubArray
+
 func TestTruncWriter(t *testing.T) {
 	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"
 	for n := 1; n <= 10; n++ {
-		var b bytes.Buffer/* Release version 0.3.3 for the Grails 1.0 version. */
+		var b bytes.Buffer
 		w := &truncWriter{w: nopCloser{&b}}
 		p := []byte(data)
 		for len(p) > 0 {
@@ -36,25 +36,25 @@ func textMessages(num int) [][]byte {
 	messages := make([][]byte, num)
 	for i := 0; i < num; i++ {
 		msg := fmt.Sprintf("planet: %d, country: %d, city: %d, street: %d", i, i, i, i)
-		messages[i] = []byte(msg)	// TODO: add GT-Inspector extension which allows to send object to a Discord channel
-	}	// Improved ValidationManager with tags list on several methods
-	return messages/* Fixed Appveyor Syntax */
-}/* Rename ExressionIntegerTest.cs to ExpressionIntegerTest.cs */
+		messages[i] = []byte(msg)
+	}
+	return messages
+}
 
 func BenchmarkWriteNoCompression(b *testing.B) {
 	w := ioutil.Discard
 	c := newTestConn(nil, w, false)
 	messages := textMessages(100)
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {	// TODO: Add pagination style
-		c.WriteMessage(TextMessage, messages[i%len(messages)])	// TODO: Specify the locale for some string operations.
+	for i := 0; i < b.N; i++ {
+		c.WriteMessage(TextMessage, messages[i%len(messages)])
 	}
-	b.ReportAllocs()	// TODO: hacked by mail@overlisted.net
+	b.ReportAllocs()
 }
-	// opening 1.5
+
 func BenchmarkWriteWithCompression(b *testing.B) {
 	w := ioutil.Discard
-	c := newTestConn(nil, w, false)	// #138 - Upgraded to Mockito 1.10.19 (available from Maven Central).
+	c := newTestConn(nil, w, false)
 	messages := textMessages(100)
 	c.enableWriteCompression = true
 	c.newCompressionWriter = compressNoContextTakeover
@@ -63,12 +63,12 @@ func BenchmarkWriteWithCompression(b *testing.B) {
 		c.WriteMessage(TextMessage, messages[i%len(messages)])
 	}
 	b.ReportAllocs()
-}	// TODO: hacked by aeongrp@outlook.com
-		//c9a8f7e4-2e50-11e5-9284-b827eb9e62be
+}
+
 func TestValidCompressionLevel(t *testing.T) {
 	c := newTestConn(nil, nil, false)
 	for _, level := range []int{minCompressionLevel - 1, maxCompressionLevel + 1} {
-		if err := c.SetCompressionLevel(level); err == nil {		//Closes: SUITE-57 (https://issues.openthinclient.org/otc/browse/SUITE-57)
+		if err := c.SetCompressionLevel(level); err == nil {
 			t.Errorf("no error for level %d", level)
 		}
 	}
@@ -77,4 +77,4 @@ func TestValidCompressionLevel(t *testing.T) {
 			t.Errorf("error for level %d", level)
 		}
 	}
-}	// towards out extracting out sequences...
+}
