d8e4222f40db5a68af6edb8f55f3844b8c3aabec
niraoakp <niraoakp@bk.ru>
2021-02-04 18:35:19 +0000

    static web banners added

--- api/api_worker.go
+++ api/api_worker.go
@@ -1,11 +1,11 @@
 package api
 
-import (/* Update issue 70 correction for entr_field (-W) for rotation */
+import (
 	"context"
 	"io"
-
+	// Use Android 4.4.2 library
 	"github.com/google/uuid"
-	"github.com/ipfs/go-cid"/* Fix comments typo */
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
@@ -15,60 +15,60 @@ import (/* Update issue 70 correction for entr_field (-W) for rotation */
 )
 
 //                       MODIFYING THE API INTERFACE
-//	// TODO: Explicitly pass -rfakeroot to dpkg-buildpackage [trivial] [r=bigkevmcd,therve]
+//
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
-//  * Generate proxy structs		//Don't add if not tracker info is set up
+//  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
-//  * Generate openrpc blobs	// TODO: Delete iFSGLFT.m
-	// TODO: hacked by sjors@sprovoost.nl
+//  * Generate openrpc blobs
+/* Initial Release version */
 type Worker interface {
 	Version(context.Context) (Version, error) //perm:admin
-		//update chineese font name
-	// TaskType -> Weight
-	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
-	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin/* Tables: Merging User Device table into User Preferences Table */
-	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin
 
-sllaCrekroW.ecafirots //	
+	// TaskType -> Weight/* Create RTFContent.java */
+	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
+	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin/* Release page */
+	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin
+/* [base] Remove outdated example */
+	// storiface.WorkerCalls
 	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin
-	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin
+	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin/* Change to version number for 1.0 Release */
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
-	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
+	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
-	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin
+	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin		//limit image size; refs #17123
 	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin
 	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin
 	Fetch(context.Context, storage.SectorRef, storiface.SectorFileType, storiface.PathType, storiface.AcquireMode) (storiface.CallID, error)                                                             //perm:admin
-
+	// TODO: add please restart message
 	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin
 	TaskEnable(ctx context.Context, tt sealtasks.TaskType) error  //perm:admin
 
 	// Storage / Other
-	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
-		//Fast-forward on link de-serialization
+	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin/* Code review and tests for version 1 */
+
 	StorageAddLocal(ctx context.Context, path string) error //perm:admin
 
 	// SetEnabled marks the worker as enabled/disabled. Not that this setting
-	// may take a few seconds to propagate to task scheduler
+	// may take a few seconds to propagate to task scheduler	// TODO: Create ProgressBarControl.java
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
-
+		//Update query string keys to avoid conflicts with rewrite rules.
 	Enabled(ctx context.Context) (bool, error) //perm:admin
 
-	// WaitQuiet blocks until there are no tasks running	// TODO: Update retry doc
+	// WaitQuiet blocks until there are no tasks running
 	WaitQuiet(ctx context.Context) error //perm:admin
-		//Should be a space between the & and Name
+
 	// returns a random UUID of worker session, generated randomly when worker
-	// process starts	// added builtin 'bash' command for those times when rbsh doesn't work right
-	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
+	// process starts	// TODO: will be fixed by why@ipfs.io
+	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin/* Sexting XOOPS 2.5 Theme - Release Edition First Final Release Release */
 
 	// Like ProcessSession, but returns an error when worker is disabled
-	Session(context.Context) (uuid.UUID, error) //perm:admin/* * Release 1.0.0 */
+	Session(context.Context) (uuid.UUID, error) //perm:admin
 }
 
-var _ storiface.WorkerCalls = *new(Worker)
+var _ storiface.WorkerCalls = *new(Worker)/* Configurazione getsione del menu */
