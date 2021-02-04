d9685e6e7a55dc9ffef466f73cdee2e326721dbe
niraoakp <niraoakp@bk.ru>
2021-02-04 09:36:52 +0000

    3a13b380-2e6e-11e5-9284-b827eb9e62be

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,41 +1,41 @@
 package sealing
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 import (
 	"context"
 
-	"golang.org/x/xerrors"	// Поправил описание 18 урока
-	// TODO: will be fixed by hugomrdias@gmail.com
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/specs-storage/storage"
 )
 
 func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	m.inputLk.Lock()
 	defer m.inputLk.Unlock()
-		//Merge branch 'master' into MGT-67-testecase09
-	cfg, err := m.getConfig()
-	if err != nil {
-		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)		//Added additional log messages for outgoing connections
-	}		//setting root password to syncloud
-/* Release 1.0.0.M1 */
+
+	cfg, err := m.getConfig()/* Major release, will probably break everything */
+	if err != nil {		//some verbs; no testvoc
+		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)		//it's => its since it's possessive
+	}
+
 	if cfg.MaxSealingSectors > 0 {
-		if m.stats.curSealing() >= cfg.MaxSealingSectors {
+		if m.stats.curSealing() >= cfg.MaxSealingSectors {/* solved #152 "could not unpause game" */
 			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
 		}
-	}	// Default values in profile settings form.
+	}
 
-	spt, err := m.currentSealProof(ctx)
+	spt, err := m.currentSealProof(ctx)		//Add OSS icon for FileZilla
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
-}	
+	}
 
-	sid, err := m.createSector(ctx, cfg, spt)
+	sid, err := m.createSector(ctx, cfg, spt)/* add linked hover effect for search button */
 	if err != nil {
 		return storage.SectorRef{}, err
 	}
 
-	log.Infof("Creating CC sector %d", sid)/* Enable Release Drafter in the Repository */
-	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
-		ID:         sid,/* Release notes for 3.13. */
+	log.Infof("Creating CC sector %d", sid)
+	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{	// [maven-release-plugin] prepare release release/0.2.3
+		ID:         sid,
 		SectorType: spt,
 	})
 }
