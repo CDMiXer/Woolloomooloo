9f2c6fc75ba54bd5e0c474fde1fecf776435b204
niraoakp <niraoakp@bk.ru>
2021-02-04 10:06:00 +0000

    fix apple2gs regression (nw)

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Pass --no-daemon to gradle build invocation
 
-import { Resource } from "./resource";/* src/timetable: Normalise out of range months */
-
-// Now, simply update the resource; this should work fine:	// Implemented Callables, and increased version to 1.1.0
+import { Resource } from "./resource";
+	// Update plotSolarElev.py
+// Now, simply update the resource; this should work fine:
 let a = new Resource("eternal", { state: 2 }, { protect: true });
