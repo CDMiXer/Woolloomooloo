702d89a1cc86c91725dca76561b68618000839a6
niraoakp <niraoakp@bk.ru>
2021-02-04 06:16:00 +0000

    Cleared converted.txt and Parsed_CSV directories. Will add to gitignore.

--- tests/integration/transformations/transformations_py_test.go
+++ tests/integration/transformations/transformations_py_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
-
+		//Added part of the reports for IpGTT, BMD
 package ints
 
 import (
 	"path/filepath"
-	"testing"
+	"testing"		//d7470194-2e54-11e5-9284-b827eb9e62be
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-func TestPythonTransformations(t *testing.T) {
+func TestPythonTransformations(t *testing.T) {		//fix slide style
 	for _, dir := range Dirs {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: d,
-				Dependencies: []string{
+				Dependencies: []string{	// TODO: Reporting methods that save the population to a plain-text file.
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick:                  true,
-				ExtraRuntimeValidation: Validator("python"),
+				ExtraRuntimeValidation: Validator("python"),/* Merge "ARM: dts: msm: Update Qos and ds settings for 8976" */
 			})
-		})
+		})/* Catch error on shutdown */
 	}
 }
