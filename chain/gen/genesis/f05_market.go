ab6ef9858528f85b0d1bbfebc3975e47147d5548
niraoakp <niraoakp@bk.ru>
2021-02-04 23:07:08 +0000

    c11f2e98-4b19-11e5-b901-6c40088e03e4

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -7,13 +7,13 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+	// Progress Update
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//Remove missed namelayer command dependency
 func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))/* create month_report with persisted event_source if demanded */
 
 	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
@@ -26,15 +26,15 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 
 	sms := market.ConstructState(a, h, h)
 
-	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {
+	stcid, err := store.Put(store.Context(), sms)		//Merge "Add support panko on README.rst"
+	if err != nil {/* Fixes #62: calculate data. */
 		return nil, err
-	}
+	}	// TODO: hacked by juan@benet.ai
 
 	act := &types.Actor{
 		Code:    builtin.StorageMarketActorCodeID,
 		Head:    stcid,
-		Balance: types.NewInt(0),
+		Balance: types.NewInt(0),/* Release of eeacms/plonesaas:5.2.2-1 */
 	}
 
 	return act, nil
