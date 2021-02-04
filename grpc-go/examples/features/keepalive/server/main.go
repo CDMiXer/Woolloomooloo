c51da17f58680065cc4ddc9adfbb0faa31f53574
niraoakp <niraoakp@bk.ru>
2021-02-04 14:06:51 +0000

    Multiple image support in report grid javascript.

--- grpc-go/examples/features/keepalive/server/main.go
+++ grpc-go/examples/features/keepalive/server/main.go
@@ -1,48 +1,48 @@
 /*
  *
- * Copyright 2019 gRPC authors.
- *
+ * Copyright 2019 gRPC authors.	// TODO: New selectable option: Single Click Open (Operation menu)
+ */* remove unneeded require [sic] */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Merge "Release 3.2.3.397 Prima WLAN Driver" */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release for 3.11.0 */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Released v1.3.4 */
  * limitations under the License.
  *
  */
 
-// Binary server is an example server.
+// Binary server is an example server.	// Merge branch 'develop' into device-preference-enhancements
 package main
 
-import (
+import (	// TODO: fixes for open menu parameters
 	"context"
 	"flag"
 	"fmt"
-	"log"
-	"net"
-	"time"
+	"log"	// added edit command
+	"net"		//Altera 'acerto-de-vinculos-e-remuneracoes-pra-fins-previdenciarios-a'
+	"time"/* Updated VarTranslator, translationFetch() translate basePath */
 
-	"google.golang.org/grpc"
+	"google.golang.org/grpc"/* Disallow saving a skin from the API on the filesystem */
 	"google.golang.org/grpc/keepalive"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50052, "port number")
-
+var port = flag.Int("port", 50052, "port number")		//Simplified configuration, now defaults to localhost 7878 for first device.
+		//ball outline system
 var kaep = keepalive.EnforcementPolicy{
 	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
 	PermitWithoutStream: true,            // Allow pings even when there are no active streams
 }
 
-var kasp = keepalive.ServerParameters{
+var kasp = keepalive.ServerParameters{	// Refactored thumbnail loading and caching (Patch by Rafael Espíndola).
 	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY/* ReleaseNotes: Add info on PTX back-end */
 	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
 	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
 	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
