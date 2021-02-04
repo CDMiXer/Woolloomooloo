db434e7b81fc42ef2fd81242585b65d5451a83b5
niraoakp <niraoakp@bk.ru>
2021-02-04 13:37:58 +0000

    Updated setup instructions for cuke-rails 0.2

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release fix */
-// that can be found in the LICENSE file./* Merge "SpecialMobileCite is a redirect page" */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package user
 
-import (	// TODO: hacked by vyzo@hackzen.org
-	"encoding/json"/* SlidePane fix and Release 0.7 */
+import (
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
-		//adding myself
+
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"/* [bugfix] Serious error in looping */
-	"github.com/drone/drone/mock"		//Delete 178643.user.js
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Small CS fix [2]
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestToken(t *testing.T) {
-	controller := gomock.NewController(t)/* don't include uiConf in the mwEmbedLoader for test file */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{	// TODO: will be fixed by davidad@alum.mit.edu
+	mockUser := &core.User{
 		ID:    1,
-		Login: "octocat",	// TODO: 979c63ce-2e6b-11e5-9284-b827eb9e62be
+		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
-	}		//Bumped required version of cherrypy
+	}
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", nil)	// TODO: will be fixed by yuvalalaluf@gmail.com
+	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
@@ -43,12 +43,12 @@ func TestToken(t *testing.T) {
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 
-	if got, want := got.Token, want.Hash; got != want {/* Release 20040116a. */
-		t.Errorf("Expect user secret returned")	// TODO: Added links to other contributers
+	if got, want := got.Token, want.Hash; got != want {
+		t.Errorf("Expect user secret returned")
 	}
 }
 
-// the purpose of this unit test is to verify that the token/* Delete MagicSpace.csproj.FileListAbsolute.txt */
+// the purpose of this unit test is to verify that the token
 // is refreshed if the user ?refresh=true query parameter is
 // included in the http request.
 func TestTokenRotate(t *testing.T) {
