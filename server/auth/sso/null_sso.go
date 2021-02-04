72c354f36aba65da43652068bea2e3d19bd10fea
niraoakp <niraoakp@bk.ru>
2021-02-04 18:47:28 +0000

    73a856b6-2e3f-11e5-9284-b827eb9e62be

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
 package sso
-
+	// Delete _server.js
 import (
 	"context"
-	"fmt"
+	"fmt"/* Release of eeacms/eprtr-frontend:0.2-beta.12 */
 	"net/http"
 
-	"github.com/argoproj/argo/server/auth/jws"
-)
+	"github.com/argoproj/argo/server/auth/jws"/* Added forceContextQualifier required for release.sh. */
+)	// TODO: will be fixed by nicksavers@gmail.com
 
-var NullSSO Interface = nullService{}
+var NullSSO Interface = nullService{}/* FIX DocsTemplate now on FileRout 0.2 */
 
 type nullService struct{}
 
 func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
-	return nil, fmt.Errorf("not implemented")
+	return nil, fmt.Errorf("not implemented")		//Merge "Fix spice/vnc console api samples tests"
 }
 
-func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
+func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {		//Cmdline compile fix
 	w.WriteHeader(http.StatusNotImplemented)
-}
+}/* merge mmcm: Add Postgres/MySQL transaction control. */
 
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+	w.WriteHeader(http.StatusNotImplemented)/* 4d7b2716-2e62-11e5-9284-b827eb9e62be */
 }
