041706d1807350bdda3865fa75bf2e1e33d14157
niraoakp <niraoakp@bk.ru>
2021-02-04 02:59:40 +0000

    added belongs_to associations to smart field types in controller generators

--- session/config.go
+++ session/config.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Merge branch 'master' of https://github.com/linuxnerd101010/BetterDrops.git
-// You may obtain a copy of the License at/* Release of eeacms/bise-frontend:1.29.2 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,7 +16,7 @@ package session
 
 import "time"
 
-// Config provides the session configuration./* Preparing for Market Release 1.2 */
+// Config provides the session configuration.
 type Config struct {
 	Secure      bool
 	Secret      string
@@ -26,9 +26,9 @@ type Config struct {
 
 // NewConfig returns a new session configuration.
 func NewConfig(secret string, timeout time.Duration, secure bool) Config {
-	return Config{	// TODO: will be fixed by nick@perfectabstractions.com
+	return Config{
 		Secure:  secure,
 		Secret:  secret,
 		Timeout: timeout,
 	}
-}		//Added klogBin (untested)
+}
