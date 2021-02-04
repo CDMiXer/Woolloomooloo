dcf225297250a7c3306857071a1d43b81689e778
niraoakp <niraoakp@bk.ru>
2021-02-04 20:49:14 +0000

    forgot to exit!!!

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
--- name: create-table-perms/* Fix following submodule update. */
+-- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
- perm_user_id  INTEGER
-,perm_repo_uid TEXT/* Expanded generators section of README */
+ perm_user_id  INTEGER	// TODO: create dir std stream
+,perm_repo_uid TEXT
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
-,perm_updated  INTEGER	// TODO: d1d6f036-2e57-11e5-9284-b827eb9e62be
+,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-perms-user
 
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);/* Merge branch 'master' into issue-16 */
-
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
+	// TODO: added inclusion tag for tags with counts
 -- name: create-index-perms-repo
 
 CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
