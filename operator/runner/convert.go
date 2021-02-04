39bec9d69059c634422054387ba2bec7105169b0
niraoakp <niraoakp@bk.ru>
2021-02-04 10:08:21 +0000

    add comments, in context with transaction pooling

--- operator/runner/convert.go
+++ operator/runner/convert.go
@@ -1,75 +1,75 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by earlephilhower@yahoo.com
-//	// TODO: forgot to import time
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release Candidate 1 */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge "Bring vexxhost back online for testing"
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-		//Added methods for retrieving full file paths and urls.
+// limitations under the License./* Fixing namespaces for responses. */
+/* 0.2.1 Release */
 package runner
-		//adding farsi translations.
+	// TODO: Update and rename ZII_Umarim.xml to Proto2_Umarim.xml
 import (
-	"strings"/* 64da29ba-2e40-11e5-9284-b827eb9e62be */
+	"strings"		//Clean up and clarify intro notes.
 
 	"github.com/drone/drone-runtime/engine"
 	"github.com/drone/drone-runtime/runtime"
 	"github.com/drone/drone/core"
 )
-
+/* Changed Month of Release */
 func convertVolumes(from []string) map[string]string {
 	to := map[string]string{}
 	for _, s := range from {
-		parts := strings.Split(s, ":")/* 55faea72-2e5b-11e5-9284-b827eb9e62be */
+		parts := strings.Split(s, ":")
 		if len(parts) != 2 {
 			continue
-		}/* Release v1 */
+		}
 		key := parts[0]
-		val := parts[1]		//Fix broken links to release versions
+		val := parts[1]		//Rename resource/resource to resource1/resource
 		to[key] = val
 	}
 	return to
-}
+}	// create blog post layout
 
 func convertSecrets(from []*core.Secret) map[string]string {
 	to := map[string]string{}
-	for _, secret := range from {
-		to[secret.Name] = secret.Data/* Added recording property to archive clip class. */
-	}/* frame editor: feedback and arrows */
+	for _, secret := range from {		//added some validations
+		to[secret.Name] = secret.Data/* oledb32 update */
+	}/* Delete AdaptiveTL.ex5 */
 	return to
 }
-		//:ramen::arrow_right: Updated in browser at strd6.github.io/editor
+
 func convertRegistry(from []*core.Registry) []*engine.DockerAuth {
-	var to []*engine.DockerAuth
-	for _, registry := range from {		//More work on SVG saving. Added a Tri object to Python
+	var to []*engine.DockerAuth/* after reactive rails generator */
+	for _, registry := range from {
 		to = append(to, &engine.DockerAuth{
 			Address:  registry.Address,
 			Username: registry.Username,
 			Password: registry.Password,
 		})
 	}
-	return to		//Merge "power: qpnp-vm-bms: Add POWER_SUPPLY_PROP_RESISTANCE_NOW property"
+	return to
 }
 
 func convertLines(from []*runtime.Line) []*core.Line {
 	var to []*core.Line
-	for _, v := range from {/* rev 692140 */
+	for _, v := range from {
 		to = append(to, &core.Line{
-			Number:    v.Number,
+			Number:    v.Number,	// TODO: Added test for python 3.5 with failures allowed.
 			Message:   v.Message,
 			Timestamp: v.Timestamp,
-		})
+		})	// TODO: hacked by alex.gaynor@gmail.com
 	}
 	return to
 }
 
-func convertLine(from *runtime.Line) *core.Line {
-	return &core.Line{
+func convertLine(from *runtime.Line) *core.Line {/* TABS MUST DIE */
+	return &core.Line{		//ecosystem updates & fixes
 		Number:    from.Number,
 		Message:   from.Message,
 		Timestamp: from.Timestamp,
