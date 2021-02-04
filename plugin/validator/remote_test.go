8165681015830e514f8918bd89852757e4f4038b
niraoakp <niraoakp@bk.ru>
2021-02-04 07:03:45 +0000

    Update lbry-basics.md

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* initial faces-config (needed for our FileUploadRender implementation) */
-// that can be found in the LICENSE file./* Release version 0.6.2 - important regexp pattern fix */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package validator/* Released some functions in Painter class */
+package validator
