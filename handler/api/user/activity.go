b5c88dcc02a72832966c225c4b8c22670025b0d9
niraoakp <niraoakp@bk.ru>
2021-02-04 13:31:04 +0000

    StreamEx: forPairs javaDoc

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Merge "Remove unneeded extra space" */
 // You may obtain a copy of the License at
-//	// TODO: Updating build-info/dotnet/wcf/release/uwp6.0 for preview1-25629-01
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Add visual example */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create checker.html */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/* Merge branch 'master' into form-entry-test */
-package user
+// limitations under the License.	// TODO: ElementCollection @MapKey  Embedded REV-ENG support
 
+package user
+	// Keybase Verification
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"/* 62918a00-2d3d-11e5-9828-c82a142b6f9b */
-)	// TODO: hacked by mikeal.rogers@gmail.com
+	"github.com/drone/drone/logger"
+)
 
-// HandleRecent returns an http.HandlerFunc that write a json-encoded
+// HandleRecent returns an http.HandlerFunc that write a json-encoded		//Implemented --render-auto/skip/force/reset command line options.
 // list of repository and build activity to the response body.
-func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
+func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {	// Extrai e testa show_online_contacts.
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
-		if err != nil {
+		if err != nil {/* Create icons. */
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err)./* Return EIO from SpiMaster::executeTransaction() on low-level failure */
 				Warnln("api: cannot list repositories")
 		} else {
 			render.JSON(w, list, 200)
 		}
 	}
-}/* Changed initial generate to resize */
+}	// Null merge from mysql-5.1.
