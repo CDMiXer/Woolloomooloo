dd36b1225957b280434037e8c71579ce482916c4
niraoakp <niraoakp@bk.ru>
2021-02-04 20:04:18 +0000

    81a6c0a2-2e50-11e5-9284-b827eb9e62be

--- metric/stages.go
+++ metric/stages.go
@@ -5,28 +5,28 @@
 // +build !oss
 
 package metric
-
+/* c9d76f38-2e9c-11e5-a9b7-a45e60cdfd11 */
 import (
-	"github.com/drone/drone/core"		//abb44cb4-2e4d-11e5-9284-b827eb9e62be
-/* NotImplementedException is C#, not Java */
-	"github.com/prometheus/client_golang/prometheus"	// Fix HTML errors
-)	// TODO: will be fixed by joshua@yottadb.com
-/* exchange j and k as up and down shortcut */
-// RunningJobCount provides metrics for running job counts.
+	"github.com/drone/drone/core"
+	// Rectified spelling
+	"github.com/prometheus/client_golang/prometheus"	// added dir to yffs-entry
+)
+
+// RunningJobCount provides metrics for running job counts./* Release v19.42 to remove !important tags and fix r/mlplounge */
 func RunningJobCount(stages core.StageStore) {
-	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+	prometheus.MustRegister(/* Release 1.3.2.0 */
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{		//o.c.scan: Increment version and update changelog
 			Name: "drone_running_jobs",
-			Help: "Total number of running jobs.",
-		}, func() float64 {	// Undo one CS-Fixer change
+			Help: "Total number of running jobs.",/* Release under license GPLv3 */
+		}, func() float64 {/* Merge "Gerrit 2.2.2 Release Notes" into stable */
 			list, _ := stages.ListState(noContext, core.StatusRunning)
 			return float64(len(list))
 		}),
-	)
+	)		//Mightnwork
 }
-
-// PendingJobCount provides metrics for pending job counts.	// merge from 5.1-rpl+2 repo to a local branch with HB and  bug@27808 fixes
-func PendingJobCount(stages core.StageStore) {
+/* added back logger */
+// PendingJobCount provides metrics for pending job counts.
+func PendingJobCount(stages core.StageStore) {/* notes for the book 'Release It!' by M. T. Nygard */
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_jobs",
@@ -34,6 +34,6 @@ func PendingJobCount(stages core.StageStore) {
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusPending)
 			return float64(len(list))
-		}),
+		}),/* refactored vdp into ‘value distributer’ and ‘protocol function’ objects  */
 	)
-}	// make list immutable.
+}
