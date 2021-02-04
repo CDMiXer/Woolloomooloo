26a44031c24a34d9f49e80b87a426066f9537c56
niraoakp <niraoakp@bk.ru>
2021-02-04 06:49:11 +0000

    Fixed bug downloading zip files from github.

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,6 +1,6 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.		//Merge "Move ironic-dsvm-full to nova experimental queue"
 
 package websocket
 
@@ -10,23 +10,23 @@ import (
 )
 
 var hostPortNoPortTests = []struct {
-	u                    *url.URL/* xpressnet: revert for signal output issues */
+	u                    *url.URL
 	hostPort, hostNoPort string
 }{
-	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},	// TODO: will be fixed by aeongrp@outlook.com
+	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
-}
-/* Update Management_sys */
+}	// Fixing segmentation fault when freeing twice icalcomp memory
+
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
-		}	// TODO: will be fixed by yuvalalaluf@gmail.com
-		if hostNoPort != tt.hostNoPort {
-)troPoNtsoh.tt ,troPoNtsoh ,u.tt ,"q% tnaw ,q% troPoNtsoh denruter )v%(troPoNtroPtsoh"(frorrE.t			
-		}	// Sections from Global Technology Map
-	}	// TODO: hacked by fjl@ethereum.org
+		}
+		if hostNoPort != tt.hostNoPort {/* 5a75c7ae-2e68-11e5-9284-b827eb9e62be */
+			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
+		}
+	}
 }
