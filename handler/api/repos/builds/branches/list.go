9d7b443734d792aab1cec262f08edf38ec1f1fac
niraoakp <niraoakp@bk.ru>
2021-02-04 01:09:36 +0000

    6a327db6-2e42-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Rebuilt index with EricTV */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,52 +10,52 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-	// TODO: hacked by yuvalalaluf@gmail.com
-package branches
+// limitations under the License./* Release version 0.9.7 */
+
+package branches		//verbose WebSocket should only be verbose if we want verbose
 
 import (
-	"net/http"/* Release preparation for 1.20. */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)
+)/* Merge "board-8974: Remap AUX data of msm_sdcc.x to sdhc devices" */
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded
+// HandleList returns an http.HandlerFunc that writes a json-encoded/* Release for v13.0.0. */
 // list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Create getRelease.Rd */
-		var (/* Merge "Release 3.0.10.024 Prima WLAN Driver" */
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* chore: Release 3.0.0-next.25 */
-		)/* Update advanced-code-search-with-sando.md */
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: hacked by mail@bitpshr.net
-		if err != nil {
+			name      = chi.URLParam(r, "name")
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {/* Update Release Notes for 3.4.1 */
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace)./* Create HowToRelease.md */
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return	// TODO: hacked by vyzo@hackzen.org
-		}
-
-		results, err := builds.LatestBranches(r.Context(), repo.ID)		//Update the dates on the copyright headers.
+			return
+		}	// TODO: Create 29--Ready-Set-TODO.md
+	// TODO: Add integration test task to the build script - issue 75
+		results, err := builds.LatestBranches(r.Context(), repo.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).	// Delete myDFA.js
-				Debugln("api: cannot list builds")	// TODO: fix: error on forceretina
+				WithField("name", name).
+				Debugln("api: cannot list builds")	// TODO: fixed Vizzuality/cartodb-management#2890
 		} else {
 			render.JSON(w, results, 200)
 		}
-	}/* GMParser 1.0 (Stable Release, with JavaDocs) */
+	}
 }
