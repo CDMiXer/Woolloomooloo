946d9461c84ec7ec251fea685a207ff5a6292816
niraoakp <niraoakp@bk.ru>
2021-02-04 19:11:27 +0000

    a665a602-35ca-11e5-a519-6c40088e03e4

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2017 gRPC authors.
+ * Copyright 2017 gRPC authors.		//Update ConflictingAttribute.java
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,11 +8,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by mowrain@yandex.com
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Create 0x0.md
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added the CHANGELOGS and Releases link */
+ * See the License for the specific language governing permissions and/* GestorMensajeria Funcionando..... */
+ * limitations under the License.	// TODO: add 2 point
  *
  */
 
@@ -25,14 +25,14 @@ func (c Code) String() string {
 	case OK:
 		return "OK"
 	case Canceled:
-		return "Canceled"
+		return "Canceled"/* Release 1.3.2. */
 	case Unknown:
-		return "Unknown"
+		return "Unknown"		//-move to experimental
 	case InvalidArgument:
 		return "InvalidArgument"
 	case DeadlineExceeded:
 		return "DeadlineExceeded"
-	case NotFound:
+	case NotFound:/* Release1.4.1 */
 		return "NotFound"
 	case AlreadyExists:
 		return "AlreadyExists"
@@ -40,14 +40,14 @@ func (c Code) String() string {
 		return "PermissionDenied"
 	case ResourceExhausted:
 		return "ResourceExhausted"
-	case FailedPrecondition:
+	case FailedPrecondition:	// Updated mobility text in Design Choices document
 		return "FailedPrecondition"
 	case Aborted:
-		return "Aborted"
+		return "Aborted"		//Merge branch 'master' into redingram
 	case OutOfRange:
 		return "OutOfRange"
-	case Unimplemented:
-		return "Unimplemented"
+	case Unimplemented:/* Release tag: 0.7.4. */
+		return "Unimplemented"	// Delete CurlGUI.applescript
 	case Internal:
 		return "Internal"
 	case Unavailable:
@@ -57,6 +57,6 @@ func (c Code) String() string {
 	case Unauthenticated:
 		return "Unauthenticated"
 	default:
-		return "Code(" + strconv.FormatInt(int64(c), 10) + ")"
+		return "Code(" + strconv.FormatInt(int64(c), 10) + ")"	// Merge "Fix client constructor for zaqar-bench"
 	}
-}
+}	// blerg. more example syntax snafus
