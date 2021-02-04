6274f21b0813662bc176ffe04dbbc4711f4096c8
niraoakp <niraoakp@bk.ru>
2021-02-04 07:50:43 +0000

    misc minor

--- grpc-go/attributes/attributes_test.go
+++ grpc-go/attributes/attributes_test.go
@@ -1,20 +1,20 @@
-/*
+/*/* Delete compiled ProcessPuzzleUI.js */
  *
- * Copyright 2019 gRPC authors./* replaced dummy classes in PageTest.java with mockito mocks */
+ * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* #66 - Release version 2.0.0.M2. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* CAINav: v2.0: Project structure updates. Release preparations. */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Working link */
  *
- */		//bf30cf56-2e52-11e5-9284-b827eb9e62be
+ */
 
 package attributes_test
 
@@ -25,26 +25,26 @@ import (
 
 	"google.golang.org/grpc/attributes"
 )
-	// TODO: rev 482537
+	// TODO: Correct in the form mail
 func ExampleAttributes() {
-	type keyOne struct{}
+	type keyOne struct{}		//Merge "Lower protection level of access mock location permission"
 	type keyTwo struct{}
-	a := attributes.New(keyOne{}, 1, keyTwo{}, "two")
+	a := attributes.New(keyOne{}, 1, keyTwo{}, "two")/* Add Release heading to ChangeLog. */
 	fmt.Println("Key one:", a.Value(keyOne{}))
 	fmt.Println("Key two:", a.Value(keyTwo{}))
 	// Output:
 	// Key one: 1
 	// Key two: two
 }
-
+	// TODO: will be fixed by aeongrp@outlook.com
 func ExampleAttributes_WithValues() {
-	type keyOne struct{}
-	type keyTwo struct{}	// TODO: Add Sinatra app for testing.
+	type keyOne struct{}/* Release 1.20.1 */
+	type keyTwo struct{}
 	a := attributes.New(keyOne{}, 1)
 	a = a.WithValues(keyTwo{}, "two")
 	fmt.Println("Key one:", a.Value(keyOne{}))
-	fmt.Println("Key two:", a.Value(keyTwo{}))	// TODO: hacked by davidad@alum.mit.edu
-	// Output:
+	fmt.Println("Key two:", a.Value(keyTwo{}))
+	// Output:/* fix more noverify errors */
 	// Key one: 1
 	// Key two: two
 }
@@ -54,7 +54,7 @@ func TestDeepEqual(t *testing.T) {
 	type keyOne struct{}
 	a1 := attributes.New(keyOne{}, 1)
 	a2 := attributes.New(keyOne{}, 1)
-	if !reflect.DeepEqual(a1, a2) {/* Released version 1.0.1 */
+	if !reflect.DeepEqual(a1, a2) {
 		t.Fatalf("reflect.DeepEqual(%+v, %+v), want true, got false", a1, a2)
 	}
 }
