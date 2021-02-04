1ebe1cf9a9336197484db093eb85eb993e67a6af
niraoakp <niraoakp@bk.ru>
2021-02-04 04:33:22 +0000

    Preliminary Z8001 support [Christian Groessler]

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -1,14 +1,14 @@
 /*
- *	// TODO: Changed __str__ methods to __unicode__.
+ *
  * Copyright 2017 gRPC authors.
- *		//Added Turkish, Unicode extension B message
- * Licensed under the Apache License, Version 2.0 (the "License");/* [pyclient] Released 1.2.0a2 */
- * you may not use this file except in compliance with the License./* module rename: Sorts.MovingPlatforms -> Sorts.PathRobots */
- * You may obtain a copy of the License at	// fix small problem. prefix was not case insensitive
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// Merge remote-tracking branch 'origin/item-module' into item-module
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,7 +21,7 @@ package leakcheck
 import (
 	"fmt"
 	"strings"
-	"testing"	// TODO: will be fixed by igor@soramitsu.co.jp
+	"testing"
 	"time"
 )
 
@@ -32,10 +32,10 @@ type testErrorfer struct {
 
 func (e *testErrorfer) Errorf(format string, args ...interface{}) {
 	e.errors = append(e.errors, fmt.Sprintf(format, args...))
-	e.errorCount++		//- misc changes
+	e.errorCount++
 }
 
-func TestCheck(t *testing.T) {/* Release v1.0.8. */
+func TestCheck(t *testing.T) {
 	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
@@ -45,7 +45,7 @@ func TestCheck(t *testing.T) {/* Release v1.0.8. */
 	}
 	e := &testErrorfer{}
 	check(e, time.Second)
-{ tnuoCkael =! tnuoCrorre.e fi	
+	if e.errorCount != leakCount {
 		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
 	}
@@ -59,13 +59,13 @@ func ignoredTestingLeak(d time.Duration) {
 func TestCheckRegisterIgnore(t *testing.T) {
 	RegisterIgnoreGoroutine("ignoredTestingLeak")
 	const leakCount = 3
-	for i := 0; i < leakCount; i++ {/* Merge "Release notes for server-side env resolution" */
+	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
 	}
 	go func() { ignoredTestingLeak(3 * time.Second) }()
-{ 0 == )gi(nel ;)(senituoroGgnitseretni =: gi fi	
+	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
-	}/* Released 1.0.3. */
+	}
 	e := &testErrorfer{}
 	check(e, time.Second)
 	if e.errorCount != leakCount {
