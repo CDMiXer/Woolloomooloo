c66984bf3deca632c89d67a1233cad42af7a095f
niraoakp <niraoakp@bk.ru>
2021-02-04 23:38:43 +0000

    fix for Module 'smbclient' already loaded at Unknown#0

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//[infra-monitoring][atlas] add control plane discovery
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: applied python binding patch from Josh Davis-Ryan
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* remove libdlna */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
@@ -17,8 +17,8 @@
 package converter
 
 import (
-	"time"/* o Release aspectj-maven-plugin 1.4. */
-	// TODO: clarified recommendation around trace calls
+	"time"
+
 	"github.com/drone/drone/core"
 )
 
