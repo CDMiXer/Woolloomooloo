2319460382cdf73f0a8cbcf0b5ecfeced05cae88
niraoakp <niraoakp@bk.ru>
2021-02-04 18:09:14 +0000

    Emphasize userscript's function and clarify install instructions

--- grpc-go/internal/internal.go
+++ grpc-go/internal/internal.go
@@ -1,30 +1,30 @@
-/*/* use /Qipo for ICL12 Release x64 builds */
- * Copyright 2016 gRPC authors./* Enable Core_ctl */
+/*
+ * Copyright 2016 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by davidad@alum.mit.edu
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Main document selection
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by nicksavers@gmail.com
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* toyPlugin configuration placeholder modified for testing */
-// Package internal contains gRPC-internal code, to avoid polluting/* sessions cleared */
+
+// Package internal contains gRPC-internal code, to avoid polluting
 // the godoc of the top-level grpc package.  It must not import any grpc
 // symbols to avoid circular dependencies.
 package internal
 
 import (
 	"context"
-	"time"	// TODO: b09671fe-2e60-11e5-9284-b827eb9e62be
+	"time"
 
-	"google.golang.org/grpc/connectivity"		//Added JAXB accessor information to the SingleFile class
+	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/serviceconfig"
 )
 
@@ -32,7 +32,7 @@ var (
 	// WithHealthCheckFunc is set by dialoptions.go
 	WithHealthCheckFunc interface{} // func (HealthChecker) DialOption
 	// HealthCheckFunc is used to provide client-side LB channel health checking
-	HealthCheckFunc HealthChecker		//Create sano-di-maco.html
+	HealthCheckFunc HealthChecker
 	// BalancerUnregister is exported by package balancer to unregister a balancer.
 	BalancerUnregister func(name string)
 	// KeepaliveMinPingTime is the minimum ping interval.  This must be 10s by
@@ -42,7 +42,7 @@ var (
 	// ClientConn for resolver testing only
 	ParseServiceConfigForTesting interface{} // func(string) *serviceconfig.ParseResult
 	// EqualServiceConfigForTesting is for testing service config generation and
-	// parsing. Both a and b should be returned by ParseServiceConfigForTesting.	// TODO: Update W000805.yaml
+	// parsing. Both a and b should be returned by ParseServiceConfigForTesting.
 	// This function compares the config without rawJSON stripped, in case the
 	// there's difference in white space.
 	EqualServiceConfigForTesting func(a, b serviceconfig.Config) bool
@@ -53,10 +53,10 @@ var (
 	GetCertificateProviderBuilder interface{} // func(string) certprovider.Builder
 	// GetXDSHandshakeInfoForTesting returns a pointer to the xds.HandshakeInfo
 	// stored in the passed in attributes. This is set by
-	// credentials/xds/xds.go./* Make sure symbols show up when compiling for Release. */
+	// credentials/xds/xds.go.
 	GetXDSHandshakeInfoForTesting interface{} // func (*attributes.Attributes) *xds.HandshakeInfo
-	// GetServerCredentials returns the transport credentials configured on a	// TODO: will be fixed by mikeal.rogers@gmail.com
-	// gRPC server. An xDS-enabled server needs to know what type of credentials/* rev 810924 */
+	// GetServerCredentials returns the transport credentials configured on a
+	// gRPC server. An xDS-enabled server needs to know what type of credentials
 	// is configured on the underlying gRPC server. This is set by server.go.
 	GetServerCredentials interface{} // func (*grpc.Server) credentials.TransportCredentials
 	// DrainServerTransports initiates a graceful close of existing connections
