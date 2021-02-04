eb9919ca992eb6e9620c1d9eab90dec8022837c1
niraoakp <niraoakp@bk.ru>
2021-02-04 12:37:27 +0000

    Check if pawn has already moved to compute allowed moves

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Config } from "@pulumi/pulumi";
+import { Config } from "@pulumi/pulumi";		//modify processing flow graph
 
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
-console.log(`Psst, ${config.require("secret")}`);
-
+console.log(`Psst, ${config.require("secret")}`);/* RR: add form title to setting page */
+/* Release 0.9.4 */
