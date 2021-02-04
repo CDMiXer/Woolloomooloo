c88089d600a8e01ac23e37a22b5c18f8819a36a8
niraoakp <niraoakp@bk.ru>
2021-02-04 02:07:27 +0000

    Update Nework.cpp

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -9,14 +9,14 @@ type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct
+type StorageMinerStruct = api.StorageMinerStruct	// TODO: will be fixed by aeongrp@outlook.com
 
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
-
+	// 6ead1856-2e60-11e5-9284-b827eb9e62be
 type Wallet = api.Wallet
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {		//Update code, to new payment methods and add redirect version API.
 	return api.PermissionedStorMinerAPI(a)
 }
 
