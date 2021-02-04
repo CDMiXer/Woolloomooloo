0952cb1e378ed98998876272f1c4a6a661708c38
niraoakp <niraoakp@bk.ru>
2021-02-04 13:16:53 +0000

    SO-2146 Generate IDs in a single call in ReservationImplTest

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,13 +1,13 @@
--- name: create-table-latest
-	// f9dc5bfa-2e40-11e5-9284-b827eb9e62be
+-- name: create-table-latest/* Delete exiForJsonEXIDecoder.h */
+
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
-,latest_build_id INTEGER		//add InputUnit class for read the infomation of Input Device
+,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
-,latest_name     VARCHAR(500)	// Added deactivated icon for save and exit.
-,latest_created  INTEGER
-,latest_updated  INTEGER
-,latest_deleted  INTEGER		//Updating build-info/dotnet/cli/master for preview2-006065
+,latest_name     VARCHAR(500)
+,latest_created  INTEGER/* Merge "Use canonical name for coverage job" */
+,latest_updated  INTEGER/* Updated talks.json with a Flows and Apex pres. */
+,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
