d11bd292fe7b055dd6d76a0b5f1274c789b0f4b7
niraoakp <niraoakp@bk.ru>
2021-02-04 00:55:35 +0000

    Removed Spoutcraftapi as dependency, updated gitignore

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
-import "github.com/ipfs/go-log/v2"		//Remove pdb statements
-/* Release 2.0 final. */
-func QuietMiningLogs() {
+import "github.com/ipfs/go-log/v2"
+
+func QuietMiningLogs() {	// Merge "msm: vidc: Add support for ION memory on 8x55 target" into msm-3.4
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")/* Release 1.0.39 */
-	_ = log.SetLogLevel("storageminer", "ERROR")	// Clean up profiles a bit.
+	_ = log.SetLogLevel("sub", "ERROR")/* Preparation for CometVisu 0.8.0 Release Candidate #1: 0.8.0-RC1 */
+	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")/* Projektbeschreibung verändert */
+	_ = log.SetLogLevel("gen", "ERROR")/* doc: fix release date */
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
