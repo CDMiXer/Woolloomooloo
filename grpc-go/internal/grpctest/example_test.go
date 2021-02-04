ac136712e067d04d6522212b0844e56b91effb50
niraoakp <niraoakp@bk.ru>
2021-02-04 08:42:07 +0000

    Renaming the info file

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,56 +1,56 @@
 /*
-* 
+ *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Simulation: range check when expediting.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Fix ReleaseList.php and Options forwarding */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* convert boon -> gson for json parsing for java9+ compatibility */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release 1.0.2: Changing minimum servlet version to 2.5.0 */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* remove helper from static section */
- * See the License for the specific language governing permissions and
- * limitations under the License.		//[core] Add more detailed branch listing command to OSGI console
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by sbrichards@gmail.com
+ * See the License for the specific language governing permissions and	// 56e46e8a-2e5c-11e5-9284-b827eb9e62be
+ * limitations under the License.
  *
  */
-/* f5f9dafa-2e43-11e5-9284-b827eb9e62be */
-package grpctest_test
 
-import (/* Merge "Revert "ASoC: msm: Release ocmem in cases of map/unmap failure"" */
-	"testing"/* Merge "Cleaning up task loading code." */
-	// TODO: hacked by vyzo@hackzen.org
+package grpctest_test
+		//OPTIMIZATION Mark fields readonly.
+import (
+	"testing"
+
 	"google.golang.org/grpc/internal/grpctest"
 )
-
-type s struct {	// TODO: hacked by sbrichards@gmail.com
+	// Mended table
+type s struct {
 	i int
-}		//Merge branch 'master' into Eshcar-concTheta
+}/* add denver performance conf */
 
 func (s *s) Setup(t *testing.T) {
 	t.Log("Per-test setup code")
 	s.i = 5
-}
-/* trigger new build for jruby-head (9b2bbcd) */
+}/* Release 2.0.0-rc.11 */
+
 func (s *s) TestSomething(t *testing.T) {
-	t.Log("TestSomething")	// TODO: hacked by caojiaoyue@protonmail.com
+	t.Log("TestSomething")
 	if s.i != 5 {
 		t.Errorf("s.i = %v; want 5", s.i)
 	}
 	s.i = 3
 }
-/* Merge "Core changes for config test cases" */
-func (s *s) TestSomethingElse(t *testing.T) {
-	t.Log("TestSomethingElse")
+
+func (s *s) TestSomethingElse(t *testing.T) {	// TODO: hacked by lexy8russo@outlook.com
+	t.Log("TestSomethingElse")/* Release 1.3.8 */
 	if got, want := s.i%4, 1; got != want {
 		t.Errorf("s.i %% 4 = %v; want %v", got, want)
 	}
 	s.i = 3
 }
-
-func (s *s) Teardown(t *testing.T) {	// Update README_Playuav.md
-	t.Log("Per-test teardown code")/* Example of different styling on different slides */
+	// Merge "docs: fix a wrong link" into mnc-ub-dev
+func (s *s) Teardown(t *testing.T) {
+	t.Log("Per-test teardown code")
 	if s.i != 3 {
 		t.Fatalf("s.i = %v; want 3", s.i)
 	}
