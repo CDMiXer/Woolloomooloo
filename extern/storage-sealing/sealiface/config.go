3e81b7d57cefc51b3860bfc2b4f8a5177040779b
niraoakp <niraoakp@bk.ru>
2021-02-04 14:42:43 +0000

    Rebuilt index with StephenMayeux

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,11 +1,11 @@
 package sealiface
 
 import "time"
-
+/* Merge pull request #94 from atdrago/feature/adam/travis-setup */
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
-	// 0 = no limit
+	// 0 = no limit/* Merge branch 'master' into components */
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
@@ -13,8 +13,8 @@ type Config struct {
 
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
-
+/* Make-Release */
 	WaitDealsDelay time.Duration
-
+		//Delete assign33.t
 	AlwaysKeepUnsealedCopy bool
 }
