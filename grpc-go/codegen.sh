4f08126046e7301ab322be2f020af8fe8a67e54b
niraoakp <niraoakp@bk.ru>
2021-02-04 09:39:05 +0000

    Map Feed icon to awesome

--- grpc-go/codegen.sh
+++ grpc-go/codegen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script serves as an example to demonstrate how to generate the gRPC-Go
+# This script serves as an example to demonstrate how to generate the gRPC-Go/* f4fb5856-2e73-11e5-9284-b827eb9e62be */
 # interface and the related messages from .proto file.
 #
 # It assumes the installation of i) Google proto buffer compiler at
@@ -8,7 +8,7 @@
 # plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have
 # not, please install them first.
 #
-# We recommend running this script at $GOPATH/src.	// TODO: hacked by witek@enjin.io
+# We recommend running this script at $GOPATH/src.
 #
 # If this is not what you need, feel free to make your own scripts. Again, this
 # script is for demonstration purpose.
