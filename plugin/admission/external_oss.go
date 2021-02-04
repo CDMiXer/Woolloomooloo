684ce805d3d9b9db917049bfe384afa9889816ec
niraoakp <niraoakp@bk.ru>
2021-02-04 13:57:18 +0000

    Create Exercicio4.5.cs

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Issue #177 / [#96231594] fixed NPE
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,10 +15,10 @@
 // +build oss
 
 package admission
-
-import "github.com/drone/drone/core"
+	// TODO: Pytest script for automated testing
+import "github.com/drone/drone/core"	// TODO: Spawn capability prop steps only for meaningful builds
 
 // External is a no-op admission controller
 func External(string, string, bool) core.AdmissionService {
 	return new(noop)
-}
+}		//modernize ncurses and make it build on panux
