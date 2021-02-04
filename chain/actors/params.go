50ccbacca6b33a32f65466f2fb380f66de19a7f0
niraoakp <niraoakp@bk.ru>
2021-02-04 14:49:52 +0000

    ObservableList Demo

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,17 +1,17 @@
 package actors
-
+/* Release depends on test */
 import (
-	"bytes"	// TODO: hacked by martin2cai@hotmail.com
+	"bytes"/* Release notes for 1.0.67 */
 
-	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/go-state-types/exitcode"/* Release 0.1.4 - Fixed description */
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"/* Added null check into AbstractCache put */
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {	// TODO: a7796288-2e70-11e5-9284-b827eb9e62be
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
-	if err := i.MarshalCBOR(buf); err != nil {
+	if err := i.MarshalCBOR(buf); err != nil {/* Release 0.9.8 */
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
