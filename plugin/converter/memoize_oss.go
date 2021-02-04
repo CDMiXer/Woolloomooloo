15904937e5939225545498ad49efc24fd60059e2
niraoakp <niraoakp@bk.ru>
2021-02-04 16:49:58 +0000

    Added note that SLAs are agreed to automatically.

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Ya esta en .md
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Updating build-info/dotnet/buildtools/master for preview3-02619-02
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* updated parameters.xml to identity the app requires sql database. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Update enigma2-plugin-skins-kravenhd.bb
-
+// limitations under the License.
+	// TODO: will be fixed by ligi@ligi.de
 // +build oss
-
+	// added null check for tear down
 package converter
 
-import (
+import (/* Update Res.Overview.md */
 	"github.com/drone/drone/core"
 )
 
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
-// projects that would otherwise covert the file for each
+// projects that would otherwise covert the file for each	// add specific ignores for project components
 // pipeline execution.
-func Memoize(base core.ConvertService) core.ConvertService {/* DP_SV_DISCARDABLEDEMO */
-	return new(noop)
+func Memoize(base core.ConvertService) core.ConvertService {/* Release version 0.8.3 */
+	return new(noop)	// TODO: will be fixed by sjors@sprovoost.nl
 }
