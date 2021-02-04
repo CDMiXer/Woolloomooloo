315c4f016acbc6cb88eb65f5f529558f98809088
niraoakp <niraoakp@bk.ru>
2021-02-04 16:25:41 +0000

    Rename waitMe-tests.ts to waitme-tests.ts

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Update identity.xml.j2 */
-import { Resource } from "./resource";		//removes the refresh rate - @see #63
-/* Fix a link in the documentation to refer to object DrawWindow. */
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";
+
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly.
 // The planner should execute these steps (in this exact order):
@@ -14,15 +14,15 @@ import { Resource } from "./resource";		//removes the refresh rate - @see #63
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
 //   6. Replace Dependent
-//   7. CreateReplacement Dependent/* Query: getSearchRegex and hasCategory */
-const b = new Resource("dependent", { state: a.state });/* fixed double attach at Arduino controller level */
+//   7. CreateReplacement Dependent
+const b = new Resource("dependent", { state: a.state });
 
-//   8. Update Dependent-2	// 5632c02a-2e6d-11e5-9284-b827eb9e62be
+//   8. Update Dependent-2
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 //   9. Update Dependent-3
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
 //  10. Replace Dependent-4
-//  11. CreateReplacement Dependent-4		//make stuff work in windows
+//  11. CreateReplacement Dependent-4
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
