f79a1a6eb9923292f15c7544292112b638b5f9f7
niraoakp <niraoakp@bk.ru>
2021-02-04 23:17:00 +0000

    ce8685fc-2e68-11e5-9284-b827eb9e62be

--- tests/integration/config_basic/python/__main__.py
+++ tests/integration/config_basic/python/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import pulumi
+import pulumi	// Fix nokogiri version.
 
 # Just test that basic config works.
-config = pulumi.Config('config_basic_py')
-
+config = pulumi.Config('config_basic_py')		//Added source/format.
+	// Fixed locale bug
 # This value is plaintext and doesn't require encryption.
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
-
+/* Release hp16c v1.0 and hp15c v1.0.2. */
 # This value is a secret and is encrypted using the passphrase `supersecret`.
 secret = config.require('bEncryptedSecret')
-assert secret == 'this super Pythonic secret is encrypted'
+assert secret == 'this super Pythonic secret is encrypted'	// TODO: Merge "Fixes node disks configuration: volume deletion control"
 
 test_data = [
     {
@@ -23,18 +23,18 @@ test_data = [
         'key': 'names',
         'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
-    },
+    },		//Fix activities on Full Project Report (Planning)
     {
         'key': 'servers',
         'expected_json': '[{"host":"example","port":80}]',
         'expected_object': [{ 'host': 'example', 'port': 80 }]
-    },
+    },	// TODO: Add test for `look` at room.
     {
         'key': 'a',
         'expected_json': '{"b":[{"c":true},{"c":false}]}',
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
-    {
+    {/* Merge "docs: Support Library 19.0.1 Release Notes" into klp-docs */
         'key': 'tokens',
         'expected_json': '["shh"]',
         'expected_object': ['shh']
@@ -48,6 +48,6 @@ test_data = [
 
 for test in test_data:
     json = config.require(test['key'])
-    obj = config.require_object(test['key'])
+    obj = config.require_object(test['key'])	// TODO: will be fixed by onhardev@bk.ru
     assert json == test['expected_json']
-    assert obj == test['expected_object']
+    assert obj == test['expected_object']/* Release version: 1.10.2 */
