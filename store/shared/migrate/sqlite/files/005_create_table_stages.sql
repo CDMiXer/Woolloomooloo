288396f2840bc0b107d85c640b137368f4228643
niraoakp <niraoakp@bk.ru>
2021-02-04 10:20:44 +0000

    Renaming transformers

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS stages (
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,stage_repo_id     INTEGER
-,stage_build_id    INTEGER		//Added MultiLineLabel
+,stage_build_id    INTEGER
 ,stage_number      INTEGER
 ,stage_kind        TEXT
 ,stage_type        TEXT
@@ -12,14 +12,14 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_error       TEXT
 ,stage_errignore   BOOLEAN
 ,stage_exit_code   INTEGER
-,stage_limit       INTEGER	// Update opah depth readings
+,stage_limit       INTEGER
 ,stage_os          TEXT
 ,stage_arch        TEXT
 ,stage_variant     TEXT
-,stage_kernel      TEXT	// TODO: Merge branch 'feature/checkbook_filter' into develop
+,stage_kernel      TEXT
 ,stage_machine     TEXT
 ,stage_started     INTEGER
-,stage_stopped     INTEGER	// TODO: sb118: merged in masterfix
+,stage_stopped     INTEGER
 ,stage_created     INTEGER
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
@@ -30,11 +30,11 @@ CREATE TABLE IF NOT EXISTS stages (
 ,UNIQUE(stage_build_id, stage_number)
 ,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
 );
-/* Release 1.1.2 */
+
 -- name: create-index-stages-build
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
-/* Merge "wlan: Setting Trace Levels through ini file." */
+
 -- name: create-index-stages-status
 
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
