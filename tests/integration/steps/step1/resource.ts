3c0bc41c660c1266941a683622d6d81f18b15d47
niraoakp <niraoakp@bk.ru>
2021-02-04 13:24:15 +0000

    Require DataElementBehaviour

--- tests/integration/steps/step1/resource.ts
+++ tests/integration/steps/step1/resource.ts
@@ -1,37 +1,37 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release jedipus-2.6.8 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-/* Debugger: hot swapping with preserved debugger state! */
-let currentID = 0;		//no ajax timeout when query is undefined
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
+let currentID = 0;
+
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private inject: Error | undefined;
-/* 9faf0640-35c6-11e5-a993-6c40088e03e4 */
+
     constructor() {
     }
-/* adds mervin review model plugin */
+
     public async diff(id: pulumi.ID, olds: any, news: any) {
-        let replaces: string[] = [];		//Create words_random
+        let replaces: string[] = [];
         let deleteBeforeReplace: boolean = false;
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
         }
         if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
-            replaces.push("replaceDBR");		//Remove bad composer command
+            replaces.push("replaceDBR");
             deleteBeforeReplace = true;
-}        
+        }
         return {
             replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
-        };/* Release `0.5.4-beta` */
+        };
     }
-/* Put the function type declarations at the right place. Patch by Brezenbak. */
+
     public async create(inputs: any) {
         if (this.inject) {
             throw this.inject;
-        }	// TODO: Fix Contributing header level
+        }
         return {
             id: (currentID++).toString(),
             outs: undefined,
@@ -61,12 +61,12 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
 export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }		//Add whitespace for flake8
+    }
 }
 
-export interface ResourceProps {		//chore(deps): update codecov orb to v1.0.4
+export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
-    replace?: any; // arbitrary state bag that requires replacement when updating.	// Wine-20041201 vendor drop
+    replace?: any; // arbitrary state bag that requires replacement when updating.
     replaceDBR?: any; // arbitrary state bag that requires replacement (with delete-before-replace=true).
     resource?: pulumi.Resource; // to force a dependency on a resource.
 }
