601da781cf2cd722e7bcca8b262cc3ef8535807f
niraoakp <niraoakp@bk.ru>
2021-02-04 10:30:09 +0000

    don't need datetime in autohandler

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,24 +1,24 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
-
+// +build nodejs all/* Release 1.6.12 */
+/* update assertion_order.go according review notes */
 package ints
-
+	// TODO: hacked by onhardev@bk.ru
 import (
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Release version 0.2.2 */
-	// TODO: will be fixed by timnugent@gmail.com
+)
+	// TODO: will be fixed by zaq1tomo@gmail.com
 func TestNodejsTransformations(t *testing.T) {
-	for _, dir := range Dirs {
-		d := filepath.Join("nodejs", dir)
+	for _, dir := range Dirs {	// TODO: hacked by steven@stebalien.com
+		d := filepath.Join("nodejs", dir)	// Edit relations documents
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-,d                    :riD				
-,}"imulup/imulup@"{gnirts][           :seicnednepeD				
-				Quick:                  true,/* Merge branch 'develop' into bsp-launch-jar */
-				ExtraRuntimeValidation: Validator("nodejs"),
+				Dir:                    d,
+				Dependencies:           []string{"@pulumi/pulumi"},
+				Quick:                  true,
+				ExtraRuntimeValidation: Validator("nodejs"),/* Update Documentation/Orchard-1-4-Release-Notes.markdown */
 			})
 		})
 	}
