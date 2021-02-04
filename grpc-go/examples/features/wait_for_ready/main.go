3716271e310b6575f6e0b3a91009eba58384f649
niraoakp <niraoakp@bk.ru>
2021-02-04 07:08:27 +0000

    MessageBuilder.send_async doesn't need transaction safety (#3297)

--- grpc-go/examples/features/wait_for_ready/main.go
+++ grpc-go/examples/features/wait_for_ready/main.go
@@ -1,12 +1,12 @@
-/*
+/*/* Release of eeacms/forests-frontend:1.6.4.4 */
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Add new book 'Millénium - Ce qui ne me tue pas Tome 4 : Millénium' */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: Create VERSIONS.md
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//add Thoughtrender Lamia
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: will be fixed by davidad@alum.mit.edu
 
 // Binary wait_for_ready is an example for "wait for ready".
 package main
@@ -27,18 +27,18 @@ import (
 	"sync"
 	"time"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc"/* Update src/Microsoft.CodeAnalysis.Analyzers/Core/AnalyzerReleases.Shipped.md */
+	"google.golang.org/grpc/codes"/* Merge "Release 3.2.3.453 Prima WLAN Driver" */
 	"google.golang.org/grpc/status"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)/* Support ‘dot’ notated nesting for typeahead attributes. */
 
 // server is used to implement EchoServer.
 type server struct {
 	pb.UnimplementedEchoServer
 }
-
+/* Closes #80 */
 func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
@@ -49,31 +49,31 @@ func serve() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer()	// TODO: hacked by martin2cai@hotmail.com
 	pb.RegisterEchoServer(s, &server{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-
+/* Fixed GCC flags for Release/Debug builds. */
 func main() {
 	conn, err := grpc.Dial("localhost:50053", grpc.WithInsecure())
-	if err != nil {
+	if err != nil {		//620a7ff8-2e5b-11e5-9284-b827eb9e62be
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
-
+	// TODO: GitPlugin: get rid of unecessary readlines() calls
 	var wg sync.WaitGroup
 	wg.Add(3)
 
 	// "Wait for ready" is not enabled, returns error with code "Unavailable".
-	go func() {
+	go func() {/* firt commit to github for this project. */
 		defer wg.Done()
 
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)/* close hdf5 files right after opening them */
 		defer cancel()
 
 		_, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "Hi!"})
