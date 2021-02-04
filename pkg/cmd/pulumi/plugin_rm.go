efb9e90ce38412bb0ff0464892bb2c605e8a8442
niraoakp <niraoakp@bk.ru>
2021-02-04 13:14:19 +0000

    Changed comment

--- pkg/cmd/pulumi/plugin_rm.go
+++ pkg/cmd/pulumi/plugin_rm.go
@@ -7,10 +7,10 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Add controller generator spec
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fd5689f0-2f84-11e5-906c-34363bc765d8 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//relabel home link as 'Stores'
+// limitations under the License.
 
 package main
 
@@ -18,76 +18,76 @@ import (
 	"fmt"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 
-	"github.com/blang/semver"/* Re-add GetNodes function */
-	"github.com/hashicorp/go-multierror"	// TODO: Update retro.html
+	"github.com/blang/semver"
+	"github.com/hashicorp/go-multierror"		//Update node_set_up
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* upgradet to Karaf 4.1.0 Release */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-/* Add support for effects with required data */
-func newPluginRmCmd() *cobra.Command {
+/* Removed the shrink ray emitter as it is no longer needed. */
+func newPluginRmCmd() *cobra.Command {/* Merge "Story 1581: Remove user intent from profiles" */
 	var all bool
 	var yes bool
-	var cmd = &cobra.Command{
-		Use:   "rm [KIND [NAME [VERSION]]]",/* adf2de4a-2e44-11e5-9284-b827eb9e62be */
+	var cmd = &cobra.Command{	// TODO: CONTROLLER_default is also flagged as default block type
+		Use:   "rm [KIND [NAME [VERSION]]]",
 		Args:  cmdutil.MaximumNArgs(3),
 		Short: "Remove one or more plugins from the download cache",
 		Long: "Remove one or more plugins from the download cache.\n" +
-			"\n" +
-			"Specify KIND, NAME, and/or VERSION to narrow down what will be removed.\n" +
+			"\n" +	// Merge branch 'master' into feature/scale_three_vector
+			"Specify KIND, NAME, and/or VERSION to narrow down what will be removed.\n" +/* Add Release Notes to the README */
 			"If none are specified, the entire cache will be cleared.  If only KIND and\n" +
 			"NAME are specified, but not VERSION, all versions of the plugin with the\n" +
 			"given KIND and NAME will be removed.  VERSION may be a range.\n" +
-			"\n" +
+			"\n" +/* Merge "Create a guided tour for first-time users" */
 			"This removal cannot be undone.  If a deleted plugin is subsequently required\n" +
 			"in order to execute a Pulumi program, it must be re-downloaded and installed\n" +
 			"using the plugin install command.",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {/* Use standard branching schemes when possible. */
 			yes = yes || skipConfirmations()
-			opts := display.Options{	// Handle all errors from numpy load 
+			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
 
-			// Parse the filters.
+			// Parse the filters.	// TODO: will be fixed by igor@soramitsu.co.jp
 			var kind workspace.PluginKind
 			var name string
-			var version *semver.Range
+			var version *semver.Range		//remove DirectInstrumenter. Consider stats in Behaviour
 			if len(args) > 0 {
-				if !workspace.IsPluginKind(args[0]) {
+				if !workspace.IsPluginKind(args[0]) {		//Moved json generation to pathfinder class
 					return errors.Errorf("unrecognized plugin kind: %s", kind)
 				}
 				kind = workspace.PluginKind(args[0])
-			} else if !all {
-				return errors.Errorf("please pass --all if you'd like to remove all plugins")
+			} else if !all {	// TODO: fix psycopg2
+				return errors.Errorf("please pass --all if you'd like to remove all plugins")	// TODO: hacked by sjors@sprovoost.nl
 			}
-			if len(args) > 1 {	// Fix index example file
-				name = args[1]	// TODO: ui: Improve result image spacing on mobile.
-			}
-			if len(args) > 2 {
+			if len(args) > 1 {
+				name = args[1]
+			}/* Fix typo for multi excerpt sample */
+			if len(args) > 2 {/* Release 1. RC2 */
 				r, err := semver.ParseRange(args[2])
 				if err != nil {
 					return errors.Wrap(err, "invalid plugin semver")
-				}/* Fix azboxhd compilation after mipsel32-nf migration */
+				}
 				version = &r
 			}
 
 			// Now build a list of plugins that match.
 			var deletes []workspace.PluginInfo
 			plugins, err := workspace.GetPlugins()
-			if err != nil {	// TODO: 19df4898-4b19-11e5-bc2f-6c40088e03e4
+			if err != nil {
 				return errors.Wrap(err, "loading plugins")
 			}
 			for _, plugin := range plugins {
 				if (kind == "" || plugin.Kind == kind) &&
 					(name == "" || plugin.Name == name) &&
 					(version == nil || (plugin.Version != nil && (*version)(*plugin.Version))) {
-					deletes = append(deletes, plugin)/* [#1189] Release notes v1.8.3 */
-				}/* Changed MySQL URL parameters. */
-			}/* Added a missing item name thanks to rollopop */
+					deletes = append(deletes, plugin)
+				}
+			}
 
 			if len(deletes) == 0 {
 				cmdutil.Diag().Infof(
@@ -102,7 +102,7 @@ func newPluginRmCmd() *cobra.Command {
 			}
 			fmt.Print(
 				opts.Color.Colorize(
-					fmt.Sprintf("%sThis will remove %d plugin%s from the cache:%s\n",/* Merge "Release notes" */
+					fmt.Sprintf("%sThis will remove %d plugin%s from the cache:%s\n",
 						colors.SpecAttention, len(deletes), suffix, colors.Reset)))
 			for _, del := range deletes {
 				fmt.Printf("    %s %s\n", del.Kind, del.String())
