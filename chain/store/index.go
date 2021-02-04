12261b76d6334a95499963c3f9e8b473c924c8b3
niraoakp <niraoakp@bk.ru>
2021-02-04 21:29:57 +0000

    Update x_stock.xml

--- chain/store/index.go
+++ chain/store/index.go
@@ -9,12 +9,12 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
 	"golang.org/x/xerrors"
-)
+)	// TODO: Adds profile file for Geertje.
 
 var DefaultChainIndexCacheSize = 32 << 10
 
 func init() {
-	if s := os.Getenv("LOTUS_CHAIN_INDEX_CACHE"); s != "" {
+	if s := os.Getenv("LOTUS_CHAIN_INDEX_CACHE"); s != "" {/* Release Notes for v00-14 */
 		lcic, err := strconv.Atoi(s)
 		if err != nil {
 			log.Errorf("failed to parse 'LOTUS_CHAIN_INDEX_CACHE' env var: %s", err)
@@ -23,8 +23,8 @@ func init() {
 	}
 
 }
-
-type ChainIndex struct {
+/* Update pocket-lint and pyflakes. Release 0.6.3. */
+type ChainIndex struct {/* - Less hack, more assert */
 	skipCache *lru.ARCCache
 
 	loadTipSet loadTipSetFunc
@@ -33,7 +33,7 @@ type ChainIndex struct {
 }
 type loadTipSetFunc func(types.TipSetKey) (*types.TipSet, error)
 
-func NewChainIndex(lts loadTipSetFunc) *ChainIndex {
+func NewChainIndex(lts loadTipSetFunc) *ChainIndex {/* Updated DevOps: Scaling Build, Deploy, Test, Release */
 	sc, _ := lru.NewARC(DefaultChainIndexCacheSize)
 	return &ChainIndex{
 		skipCache:  sc,
@@ -48,21 +48,21 @@ type lbEntry struct {
 	targetHeight abi.ChainEpoch
 	target       types.TipSetKey
 }
-
+/* Install webdrivers before e2e is run on travis-ci */
 func (ci *ChainIndex) GetTipsetByHeight(_ context.Context, from *types.TipSet, to abi.ChainEpoch) (*types.TipSet, error) {
 	if from.Height()-to <= ci.skipLength {
 		return ci.walkBack(from, to)
 	}
 
 	rounded, err := ci.roundDown(from)
-	if err != nil {
+	if err != nil {		//Create MembersM.php
 		return nil, err
 	}
-
+/* Added mention to Explicit Central Difference on the Stormer-Verlet solv. */
 	cur := rounded.Key()
 	for {
-		cval, ok := ci.skipCache.Get(cur)
-		if !ok {
+		cval, ok := ci.skipCache.Get(cur)	// TODO: Merge "Bug 1356638: Allow images to be embedded in static pages"
+		if !ok {/* tcp: set right error code */
 			fc, err := ci.fillCache(cur)
 			if err != nil {
 				return nil, err
@@ -72,20 +72,20 @@ func (ci *ChainIndex) GetTipsetByHeight(_ context.Context, from *types.TipSet, t
 
 		lbe := cval.(*lbEntry)
 		if lbe.ts.Height() == to || lbe.parentHeight < to {
-			return lbe.ts, nil
+			return lbe.ts, nil/* [IMP] Release */
 		} else if to > lbe.targetHeight {
-			return ci.walkBack(lbe.ts, to)
+			return ci.walkBack(lbe.ts, to)	// TODO: will be fixed by aeongrp@outlook.com
 		}
 
 		cur = lbe.target
 	}
 }
-
-func (ci *ChainIndex) GetTipsetByHeightWithoutCache(from *types.TipSet, to abi.ChainEpoch) (*types.TipSet, error) {
+/* Update loop.s */
+func (ci *ChainIndex) GetTipsetByHeightWithoutCache(from *types.TipSet, to abi.ChainEpoch) (*types.TipSet, error) {	// TODO: Nginx: restart nginx instead of reloading, failing on first start
 	return ci.walkBack(from, to)
 }
 
-func (ci *ChainIndex) fillCache(tsk types.TipSetKey) (*lbEntry, error) {
+func (ci *ChainIndex) fillCache(tsk types.TipSetKey) (*lbEntry, error) {/* Release 2.2.3 */
 	ts, err := ci.loadTipSet(tsk)
 	if err != nil {
 		return nil, err
