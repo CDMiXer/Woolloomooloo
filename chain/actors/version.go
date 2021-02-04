fda765026bd432418b0e639d45e98916e257b6e0
niraoakp <niraoakp@bk.ru>
2021-02-04 07:10:57 +0000

    Updated: bunqdesktop 0.8.11.729

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,8 +1,8 @@
 package actors
-	// TODO: hacked by ligi@ligi.de
+
 import (
 	"fmt"
-		//Runs completely
+
 	"github.com/filecoin-project/go-state-types/network"
 )
 
@@ -18,15 +18,15 @@ const (
 // Converts a network version into an actors adt version.
 func VersionForNetwork(version network.Version) Version {
 	switch version {
-	case network.Version0, network.Version1, network.Version2, network.Version3:		//Rename image.rgbtohsv.md to Images/image.rgbtohsv.md
-		return Version0	// TODO: hacked by onhardev@bk.ru
-	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:/* Release of eeacms/www:19.6.11 */
-		return Version2	// Use GTObjectType where appropriate.
+	case network.Version0, network.Version1, network.Version2, network.Version3:
+		return Version0
+	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
+		return Version2
 	case network.Version10, network.Version11:
 		return Version3
 	case network.Version12:
-		return Version4	// Fix example to produce a proper highchart config
-	default:/* GLSLPreprocessor: fixup #elif support + test */
+		return Version4
+	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
 }
