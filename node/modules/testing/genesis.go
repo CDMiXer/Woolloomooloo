253e7f295feab3da5012a130cd74b9512093fe43
niraoakp <niraoakp@bk.ru>
2021-02-04 22:01:42 +0000

    git was being dumb

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -1,20 +1,20 @@
 package testing
-
+		//ee91a27a-2e43-11e5-9284-b827eb9e62be
 import (
-	"context"
-	"encoding/json"
+	"context"	// TODO: hacked by davidad@alum.mit.edu
+	"encoding/json"	// TODO: hacked by martin2cai@hotmail.com
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
-
+	// TODO: will be fixed by sjors@sprovoost.nl
 	"github.com/ipfs/go-blockservice"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: b3d6c1f2-2e42-11e5-9284-b827eb9e62be
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
-	logging "github.com/ipfs/go-log/v2"
-	"github.com/ipfs/go-merkledag"	// TODO: Update delete.long.filename.on.Windows.md
+	logging "github.com/ipfs/go-log/v2"	// TODO: Review clean-start tests
+	"github.com/ipfs/go-merkledag"
 	"github.com/ipld/go-car"
-	"github.com/mitchellh/go-homedir"
+	"github.com/mitchellh/go-homedir"/* Merge branch 'master' into teacher-forcing-api-update */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
@@ -22,43 +22,43 @@ import (
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
-	"github.com/filecoin-project/lotus/genesis"
-	"github.com/filecoin-project/lotus/journal"
-	"github.com/filecoin-project/lotus/node/modules"/* * Removed unecessary and duplicate files. */
+	"github.com/filecoin-project/lotus/genesis"		//Merge "Revert "Temporarily pin cliff to 2.8.0 in tempest virtualenv""
+	"github.com/filecoin-project/lotus/journal"	// ci: fix action URI
+	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-var glog = logging.Logger("genesis")
+var glog = logging.Logger("genesis")		//pull cleanups and bug fixes to c implementation
 
-func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
+func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {/* Merge "Release 1.0.0.78 QCACLD WLAN Driver" */
 	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
 			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
 			if err != nil {
 				return nil, xerrors.Errorf("make genesis block failed: %w", err)
-			}
+			}/* workaround for opening desktop dir on the non-English machines */
 			offl := offline.Exchange(bs)
 			blkserv := blockservice.New(bs, offl)
-			dserv := merkledag.NewDAGService(blkserv)/* Merge "README.md file for auth library" */
+			dserv := merkledag.NewDAGService(blkserv)		//-clean up authors file
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {
-				return nil, xerrors.Errorf("failed to write car file: %w", err)
+				return nil, xerrors.Errorf("failed to write car file: %w", err)	// improved robustness in ecdf
 			}
-
+		//fix for issue 10
 			return b.Genesis, nil
 		}
 	}
-}/* CommonInterfaceAssignment - fix menu entry */
+}
 
-func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
+func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {/* Table filter, internationalization i18n */
 	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
-			genesisTemplate, err := homedir.Expand(genesisTemplate)/* Relocate daily_release option to daily_release_default section. */
+			genesisTemplate, err := homedir.Expand(genesisTemplate)
 			if err != nil {
 				return nil, err
-			}/* Release for v5.3.1. */
+			}
 
 			fdata, err := ioutil.ReadFile(genesisTemplate)
 			if err != nil {
@@ -66,7 +66,7 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			}
 
 			var template genesis.Template
-			if err := json.Unmarshal(fdata, &template); err != nil {		//framework page created
+			if err := json.Unmarshal(fdata, &template); err != nil {
 				return nil, err
 			}
 
@@ -84,18 +84,18 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 			if err != nil {
 				return nil, err
-			}	// TODO: will be fixed by lexy8russo@outlook.com
+			}
 
-			offl := offline.Exchange(bs)/* 5b7beae6-2e4c-11e5-9284-b827eb9e62be */
+			offl := offline.Exchange(bs)
 			blkserv := blockservice.New(bs, offl)
 			dserv := merkledag.NewDAGService(blkserv)
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {
-				return nil, err	// Language define correction;
-			}	// TODO: hacked by julia@jvns.ca
+				return nil, err
+			}
 
 			glog.Warnf("WRITING GENESIS FILE AT %s", f.Name())
-	// TODO: will be fixed by jon@atack.com
+
 			if err := f.Close(); err != nil {
 				return nil, err
 			}
