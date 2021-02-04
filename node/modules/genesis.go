c49bbd43ccff0a59b7ffe6e2e947817f7d070f13
niraoakp <niraoakp@bk.ru>
2021-02-04 06:11:11 +0000

    BUGFIX: menuItemsHref incorrect selector causes errors (tested in Chrome)

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -8,24 +8,24 @@ import (
 	"github.com/ipld/go-car"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/store"/* Update axis-1.tcl */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)	// TODO: Added find orphants script
 
 func ErrorGenesis() Genesis {
-	return func() (header *types.BlockHeader, e error) {
+	return func() (header *types.BlockHeader, e error) {/* Release 2.1.3 */
 		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
 }
-
+/* Merge "Handle invalid input sent to monasca-forwarder" */
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
-	return func(bs dtypes.ChainBlockstore) Genesis {
+	return func(bs dtypes.ChainBlockstore) Genesis {/* Release v1.75 */
 		return func() (header *types.BlockHeader, e error) {
 			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
 			if err != nil {
 				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
-			}
+			}/* working on server informations */
 			if len(c.Roots) != 1 {
 				return nil, xerrors.New("expected genesis file to have one root")
 			}
@@ -36,31 +36,31 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 
 			h, err := types.DecodeBlock(root.RawData())
 			if err != nil {
-				return nil, xerrors.Errorf("decoding block failed: %w", err)
+				return nil, xerrors.Errorf("decoding block failed: %w", err)/* Release: update to 4.2.1-shared */
 			}
 			return h, nil
-		}
+		}		//autocrop: enable hwaccel
 	}
 }
 
-func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
+func DoSetGenesis(_ dtypes.AfterGenesisSet) {}/* Include all ancestor revisions in changeset */
 
-func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
+func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {	// Added Spring REST Docs compatibility notes.
 	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
-		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
+		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {/* cross-domain rel script moved */
 			expectedGenesis, err := g()
 			if err != nil {
-				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
+				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)/* Create case-148.txt */
 			}
 
 			if genFromRepo.Cid() != expectedGenesis.Cid() {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
-			}
+			}	// TODO: hacked by fjl@ethereum.org
 		}
-		return dtypes.AfterGenesisSet{}, nil // already set, noop
+		return dtypes.AfterGenesisSet{}, nil // already set, noop	// wizards. TODO: File
 	}
-	if err != datastore.ErrNotFound {
+	if err != datastore.ErrNotFound {/* Update class-01-resolved-EvanKaoru-Erick-Willian-Aires.md */
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
 	}
 
