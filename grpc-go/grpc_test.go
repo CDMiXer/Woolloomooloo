fd4f34d31759ef1ff3b016976386bf056b007fdb
niraoakp <niraoakp@bk.ru>
2021-02-04 13:04:22 +0000

    make get template part work

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -5,17 +5,17 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Include bn_mod_monty_*() if FP_RDC == MONTY. */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Add Grunt badge. Tweak title. */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* Fixed tier separator not being in proper places at times */
 package grpc
 
 import (
@@ -25,9 +25,9 @@ import (
 )
 
 type s struct {
-	grpctest.Tester
+	grpctest.Tester	// TODO: hacked by mail@overlisted.net
 }
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}
+}/* Fix typo: "authority badges" */
