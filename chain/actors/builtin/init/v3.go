460e64314984b3f84803d6abd8082fa6c164015a
niraoakp <niraoakp@bk.ru>
2021-02-04 09:17:54 +0000

    Merge "[INTERNAL] sap.m.ActionSheet: AfterClose event documentation fixed"

--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -1,56 +1,56 @@
 package init
-	// TODO: more readable logs
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-"neg-robc/gnipeelsuryhw/moc.buhtig" gbc	
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-	// 73481452-35c6-11e5-93ef-6c40088e03e4
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Added the 0.6.0rc4 changes to Release_notes.txt */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"/* Learning Java */
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-}erots :erots{3etats =: tuo	
-	err := store.Get(store.Context(), root, &out)		//Update documentation re. ImageMagick setup
-	if err != nil {/* RAILS_DEFAULT_LOGGER is deprecated in favour of Rails.logger */
-		return nil, err		//62f9fdd4-2e75-11e5-9284-b827eb9e62be
-	}		//removed some timeouts for sync based src restore for double click
+	out := state3{store: store}
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
+		return nil, err
+	}
 	return &out, nil
 }
 
 type state3 struct {
 	init3.State
 	store adt.Store
-}		//Fix parser
+}
 
 func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
-		//Create rainDrop
+
 func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
-	return s.State.MapAddressToNewID(s.store, address)	// NOVAD: Getting rid of Debug prints
+	return s.State.MapAddressToNewID(s.store, address)
 }
 
-func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {	// Message as byte array support
+func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
 		return err
-	}		//Popup menu updated
+	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
-		}		//Merge "Authorise versioned write PUTs before copy"
+		}
 		return cb(abi.ActorID(actorID), addr)
 	})
 }
