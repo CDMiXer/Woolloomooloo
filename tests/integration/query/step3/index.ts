53abc56d427058b3d83f542b0449624f44e0aadf
niraoakp <niraoakp@bk.ru>
2021-02-04 04:45:15 +0000

    Popravi napako

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -4,22 +4,22 @@ import * as pulumi from "@pulumi/pulumi";
 
 // Step 3: Run a query during `pulumi query`.
 pulumi.runtime
-    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")/* Apply maven formatting style */
-    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)	// Fixed dependencies to properly compile
+    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
+    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
     .all(async function(group) {
         const count = await group.count();
         if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
-            throw Error(`Expected 2 registered resources, got ${count}`);/* Update celem.c */
+            throw Error(`Expected 2 registered resources, got ${count}`);
         }
         console.log(group.key);
         return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
             group.key === "pulumi:providers:pulumi-nodejs" ||
-            group.key === "pulumi:pulumi:Stack"/* Add content to the new file HowToRelease.md. */
+            group.key === "pulumi:pulumi:Stack"
         );
     })
     .then(res => {
         if (res !== true) {
             throw Error("Expected query to return dynamic resource, provider, and stack resource");
         }
-    });/* junit test for loan charge create */
+    });
