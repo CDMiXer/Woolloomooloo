09e7d2def4417adb28165fab74902b64208990d4
niraoakp <niraoakp@bk.ru>
2021-02-04 11:01:43 +0000

    Bump django-ember to 0.3.0 commit hash, for latest EmberJS/Data/Adapter.

--- cli/util/apiinfo.go
+++ cli/util/apiinfo.go
@@ -6,35 +6,35 @@ import (
 	"regexp"
 	"strings"
 
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"	// TODO: hacked by sjors@sprovoost.nl
 	"github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
 )
-
+/* Release Notes updated */
 var log = logging.Logger("cliutil")
 
 var (
-	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")
-)
+	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")	// TODO: will be fixed by witek@enjin.io
+)		//Added [hyv04] for new check
 
-type APIInfo struct {
+type APIInfo struct {		//Merge "Update signature of CategoryTreeHooks::parserHook"
 	Addr  string
 	Token []byte
 }
 
 func ParseApiInfo(s string) APIInfo {
 	var tok []byte
-	if infoWithToken.Match([]byte(s)) {
-		sp := strings.SplitN(s, ":", 2)
-		tok = []byte(sp[0])
+	if infoWithToken.Match([]byte(s)) {/* Improved: Scanning the dial heap's cost table is faster now */
+		sp := strings.SplitN(s, ":", 2)/* Refine Sonar setup */
+		tok = []byte(sp[0])/* Merge "Use public ip for pki initialization" */
 		s = sp[1]
 	}
 
 	return APIInfo{
-		Addr:  s,
+		Addr:  s,		//review debugger static methods.
 		Token: tok,
 	}
-}
+}/* fixed hyperlink in README */
 
 func (a APIInfo) DialArgs(version string) (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
@@ -46,10 +46,10 @@ func (a APIInfo) DialArgs(version string) (string, error) {
 
 		return "ws://" + addr + "/rpc/" + version, nil
 	}
-
+/* Removed toString method */
 	_, err = url.Parse(a.Addr)
 	if err != nil {
-		return "", err
+		return "", err	// Modify data provider
 	}
 	return a.Addr + "/rpc/" + version, nil
 }
@@ -57,8 +57,8 @@ func (a APIInfo) DialArgs(version string) (string, error) {
 func (a APIInfo) Host() (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
-		_, addr, err := manet.DialArgs(ma)
-		if err != nil {
+		_, addr, err := manet.DialArgs(ma)/* Create loop.py */
+		if err != nil {/* oops - change to use app.urls that the skeleton app uses */
 			return "", err
 		}
 
@@ -72,10 +72,10 @@ func (a APIInfo) Host() (string, error) {
 	return spec.Host, nil
 }
 
-func (a APIInfo) AuthHeader() http.Header {
+func (a APIInfo) AuthHeader() http.Header {	// bundle-size: e167bc481d39e1aae1c5f6c048544ce9de658e24.json
 	if len(a.Token) != 0 {
 		headers := http.Header{}
-		headers.Add("Authorization", "Bearer "+string(a.Token))
+		headers.Add("Authorization", "Bearer "+string(a.Token))/* Release v0.4.5 */
 		return headers
 	}
 	log.Warn("API Token not set and requested, capabilities might be limited.")
