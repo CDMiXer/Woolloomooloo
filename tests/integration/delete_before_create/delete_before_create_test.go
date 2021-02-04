65dbc69fc8e831857d58c1681507f9632daabd4e
niraoakp <niraoakp@bk.ru>
2021-02-04 10:25:18 +0000

    removed htaccess rules that prenvented upload on aruba hostings

--- tests/integration/delete_before_create/delete_before_create_test.go
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* Removed double formatting of redis key in __delitem__ */
+
 package ints
 
 import (
 	"testing"
-	// TODO: Added 'CommandItems' mechanic.
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//announcing membership webinar
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-	// TODO: hacked by hi@antfu.me
+
 // TestDeleteBeforeCreate tests a few different operational modes for
 // replacements done by deleting before creating.
-func TestDeleteBeforeCreate(t *testing.T) {	// Notification bug fix
+func TestDeleteBeforeCreate(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
@@ -20,23 +20,23 @@ func TestDeleteBeforeCreate(t *testing.T) {	// Notification bug fix
 			{
 				Dir:      "step2",
 				Additive: true,
-			},	// Update JinweiYin.md
-			{/* Fixed typo in ResourceHelper javadocs */
-				Dir:      "step3",/* Release of version 1.6 */
+			},
+			{
+				Dir:      "step3",
 				Additive: true,
 			},
-			{	// TODO: Update UART.c
+			{
 				Dir:      "step4",
 				Additive: true,
 			},
 			{
 				Dir:      "step5",
-				Additive: true,/* Propose Maru as Release Team Lead Shadow */
+				Additive: true,
 			},
 			{
 				Dir:      "step6",
-				Additive: true,/* Release new version 2.5.50: Add block count statistics */
+				Additive: true,
 			},
-		},/* Release: Making ready for next release iteration 5.3.1 */
-	})	// TODO: HuffmanTree erweitert. Dekodierung begonnen.
+		},
+	})
 }
