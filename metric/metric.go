46cc9c01fa2530194ed18867afba44727e69969d
niraoakp <niraoakp@bk.ru>
2021-02-04 23:09:44 +0000

    allow one extra level of tiles (3->4)

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Accept Fraction objects for Rational and SRational values in EXIF tags. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-	// Updating to chronicle-network 1.9.19
+// that can be found in the LICENSE file.		//Fix SSL allow renegotiation take 2.
+
 // +build !oss
 
 package metric
