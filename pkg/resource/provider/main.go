0a7ea3ac2ada21eb84929a4417fb3818f95b95c1
niraoakp <niraoakp@bk.ru>
2021-02-04 16:01:42 +0000

    cleanup + -n path

--- pkg/resource/provider/main.go
+++ pkg/resource/provider/main.go
@@ -3,36 +3,36 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Introduced QualifiedName to Xbase */
-//     http://www.apache.org/licenses/LICENSE-2.0/* Dejé funcionando el login facebook. */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update README.md with Release badge */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package provider
 
-import (		//import row
+import (
 	"flag"
-	"fmt"	// added CURL finding on linux
+	"fmt"
 
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
-		//Update copyright notices in all file comments
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"		//Enable LiveReload
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/rpcutil"
-	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"/* Merge "Update associate_floating_ip to use instance objs" */
+	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
 )
 
 // Tracing is the optional command line flag passed to this provider for configuring a  Zipkin-compatible tracing
 // endpoint
-var tracing string	// TODO: will be fixed by 13860583249@yeah.net
+var tracing string
 
 // Main is the typical entrypoint for a resource provider plugin.  Using it isn't required but can cut down
-// significantly on the amount of boilerplate necessary to fire up a new resource provider./* Release of eeacms/energy-union-frontend:1.7-beta.12 */
+// significantly on the amount of boilerplate necessary to fire up a new resource provider.
 func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderServer, error)) error {
 	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")
 	flag.Parse()
@@ -40,15 +40,15 @@ func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderSe
 	// Initialize loggers before going any further.
 	logging.InitLogging(false, 0, false)
 	cmdutil.InitTracing(name, name, tracing)
-/* Update Console-Command-Sleep.md */
+
 	// Read the non-flags args and connect to the engine.
 	args := flag.Args()
 	if len(args) == 0 {
 		return errors.New("fatal: could not connect to host RPC; missing argument")
-	}/* Merge branch 'network-september-release' into hostedWorkloads_PS */
+	}
 	host, err := NewHostClient(args[0])
 	if err != nil {
-		return errors.Errorf("fatal: could not connect to host RPC: %v", err)		//add uploader
+		return errors.Errorf("fatal: could not connect to host RPC: %v", err)
 	}
 
 	// Fire up a gRPC server, letting the kernel choose a free port for us.
@@ -70,9 +70,9 @@ func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderSe
 	fmt.Printf("%d\n", port)
 
 	// Finally, wait for the server to stop serving.
-	if err := <-done; err != nil {/* TODO Comment */
+	if err := <-done; err != nil {
 		return errors.Errorf("fatal: %v", err)
 	}
 
-	return nil		//0f3f53ae-2e5e-11e5-9284-b827eb9e62be
+	return nil
 }
