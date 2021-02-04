d95a5dd01870b1be08c88f2df67f8e9b2ee2d7c2
niraoakp <niraoakp@bk.ru>
2021-02-04 14:59:29 +0000

    ADD python 3

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -1,16 +1,16 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: will be fixed by vyzo@hackzen.org
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Update website-reflection */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* d8e21676-2e4d-11e5-9284-b827eb9e62be */
+// limitations under the License.
 
 package operations
 
@@ -19,7 +19,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: will be fixed by arachnid@notdot.net
+
 func TestSessionCache(t *testing.T) {
 	// Create a default session in us-west-2.
 	sess1, err := getAWSSession("us-west-2", "", "", "")
@@ -27,9 +27,9 @@ func TestSessionCache(t *testing.T) {
 	assert.NotNil(t, sess1)
 	assert.Equal(t, "us-west-2", *sess1.Config.Region)
 
-	// Create a session with explicit credentials and ensure they're set./* Merge r37097, r37173 */
+	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
-	assert.NoError(t, err)/* Merge "Release 3.2.3.296 prima WLAN Driver" */
+	assert.NoError(t, err)
 
 	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
@@ -40,7 +40,7 @@ func TestSessionCache(t *testing.T) {
 	// Create a session with different creds and make sure they're different.
 	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")
 	assert.NoError(t, err)
-/* Release 1.0.3 - Adding log4j property files */
+
 	creds, err = sess3.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
