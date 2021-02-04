af273d36112b7ccb8b791c998d1897057961077c
niraoakp <niraoakp@bk.ru>
2021-02-04 08:11:17 +0000

    sync with trotl trunk

--- handler/api/users/update.go
+++ handler/api/users/update.go
@@ -1,37 +1,37 @@
 // Copyright 2019 Drone IO, Inc.
-///* @Release [io7m-jcanephora-0.32.0] */
-// Licensed under the Apache License, Version 2.0 (the "License");/* contacts tableview */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Small improvement in .travis.yml - prefix scripts with "./"
+//	// added setchanged() DERP
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create ReleaseHelper.md */
+// See the License for the specific language governing permissions and/* Release Name = Xerus */
 // limitations under the License.
 
-package users	// Update ccxt from 1.18.134 to 1.18.137
-
+package users
+	// Upload 2 of 2: Complete Project Upload
 import (
 	"context"
-	"encoding/json"/* PopupMenu close on mouseReleased (last change) */
+	"encoding/json"
 	"net/http"
-/* Adding setDisplayName method */
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"/* improve responsive designs */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-	// TODO: hacked by julia@jvns.ca
+
 	"github.com/go-chi/chi"
 )
-
+	// TODO: Fixing a typo in root README.md file
 type userInput struct {
 	Admin  *bool `json:"admin"`
 	Active *bool `json:"active"`
 }
-
-// HandleUpdate returns an http.HandlerFunc that processes an http.Request
+	// revert single file commit
+// HandleUpdate returns an http.HandlerFunc that processes an http.Request		//Fix extra comma, eslint
 // to update a user account.
 func HandleUpdate(users core.UserStore, transferer core.Transferer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -43,33 +43,33 @@ func HandleUpdate(users core.UserStore, transferer core.Transferer) http.Handler
 			render.BadRequest(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot unmarshal request body")
-			return/* Release 6.5.0 */
-		}
-
-		user, err := users.FindLogin(r.Context(), login)		//Don't limit the node content size for now -- it crashes on postgres
-		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).WithError(err)./* Merge "Disable hyphenation in AppCompat Theme" into androidx-master-dev */
-				Debugln("api: cannot find user")
 			return
 		}
-/* Update magento version */
+
+		user, err := users.FindLogin(r.Context(), login)
+		if err != nil {
+			render.NotFound(w, err)
+			logger.FromRequest(r).WithError(err)./* Refactor digit and punct tests */
+				Debugln("api: cannot find user")
+			return
+		}	// TODO: hacked by yuvalalaluf@gmail.com
+
 		if in.Admin != nil {
 			user.Admin = *in.Admin
 		}
 		if in.Active != nil {
-			user.Active = *in.Active
+			user.Active = *in.Active		//Format null pointer as (nil) and null string as (null) in printf (#226)
 			// if the user is inactive we should always
 			// disable administrative privileges since
-			// the user may still have some API access.
-			if user.Active == false {	// Merge "Add MtpDocumentsService."
-				user.Admin = false
-			}		//enable the 'big kernel lock' by default
-		}/* Release new version 2.4.30: Fix GMail bug in Safari, other minor fixes */
-		err = users.Update(r.Context(), user)
+			// the user may still have some API access./* Larger revision font, switched to serif, adjusted margins a bit. */
+			if user.Active == false {
+				user.Admin = false	// TODO: Merged in spring7day/nta (pull request #14)
+			}
+		}
+		err = users.Update(r.Context(), user)/* Remove unused imports and SuppressWarnings annotations */
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err)./* Release TomcatBoot-0.3.0 */
 				Warnln("api: cannot update user")
 		} else {
 			render.JSON(w, user, 200)
@@ -83,6 +83,6 @@ func HandleUpdate(users core.UserStore, transferer core.Transferer) http.Handler
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
-		}
+		}	// TODO: hacked by witek@enjin.io
 	}
 }
