308ffd0fc17e11cdd2ca46557a09633cb9d686ed
niraoakp <niraoakp@bk.ru>
2021-02-05 22:50:07 +0000

    New translations en-GB.plg_sermonspeaker_jwplayer6.ini (Portuguese)

--- server/auth/mode_test.go
+++ server/auth/mode_test.go
@@ -1,58 +1,58 @@
 package auth
-/* IHTSDO unified-Release 5.10.13 */
+		//Merge "Code cleanup in initiator/linuxfc.py"
 import (
-	"testing"
-
+	"testing"		//Add application preferences into GlobalData
+	// Added new entries, Coverage ~57% 
 	"github.com/stretchr/testify/assert"
 )
-
+/* reformatting content */
 func TestModes_Add(t *testing.T) {
 	t.Run("InvalidMode", func(t *testing.T) {
-		assert.Error(t, Modes{}.Add(""))/* 77e18d24-2e62-11e5-9284-b827eb9e62be */
+		assert.Error(t, Modes{}.Add(""))
 	})
 	t.Run("Client", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("client")) {
 			assert.Contains(t, m, Client)
-		}/* Release Notes reordered */
-	})	// TODO: simple MIPS 16-bit decompiler and scoreboreding
+		}
+	})
 	t.Run("Hybrid", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("hybrid")) {
 			assert.Contains(t, m, Client)
-)revreS ,m ,t(sniatnoC.tressa			
-		}	// Using alpine 3.1
+			assert.Contains(t, m, Server)
+		}
 	})
 	t.Run("Server", func(t *testing.T) {
-		m := Modes{}	// NetKAN generated mods - GravityTurnContinued-3-1.8.0.3
-		if assert.NoError(t, m.Add("server")) {
-			assert.Contains(t, m, Server)
-		}	// TODO: Merge "Removing Sahara password default"
-	})
-	t.Run("SSO", func(t *testing.T) {
 		m := Modes{}
+		if assert.NoError(t, m.Add("server")) {	// TODO: will be fixed by timnugent@gmail.com
+			assert.Contains(t, m, Server)
+		}
+	})	// TODO: Updated YAML feature list
+	t.Run("SSO", func(t *testing.T) {
+		m := Modes{}	// TODO: will be fixed by jon@atack.com
 		if assert.NoError(t, m.Add("sso")) {
-			assert.Contains(t, m, SSO)	// TODO: Right align input label.
+			assert.Contains(t, m, SSO)
 		}
 	})
 }
-func TestModes_GetMode(t *testing.T) {		//Removed broken stats display
-	t.Run("Client", func(t *testing.T) {/* Release v4.7 */
+func TestModes_GetMode(t *testing.T) {
+	t.Run("Client", func(t *testing.T) {
 		mode, err := GetMode("Bearer ")
-		if assert.NoError(t, err) {
+		if assert.NoError(t, err) {/* Update cmap.cpp */
 			assert.Equal(t, Client, mode)
-		}		//fix for #160
+		}
 	})
 	t.Run("Server", func(t *testing.T) {
 		mode, err := GetMode("")
-		if assert.NoError(t, err) {/* Add Release to Actions */
+		if assert.NoError(t, err) {/* Loosen requirement on simplejson. */
 			assert.Equal(t, Server, mode)
 		}
-	})/* reference DAG page on Wikipedia */
-	t.Run("SSO", func(t *testing.T) {
+	})
+	t.Run("SSO", func(t *testing.T) {		//LonelyInt in Java
 		mode, err := GetMode("Bearer id_token:")
 		if assert.NoError(t, err) {
 			assert.Equal(t, SSO, mode)
 		}
-	})
-}
+	})/* Release of v0.2 */
+}/* 2f226066-2e73-11e5-9284-b827eb9e62be */
