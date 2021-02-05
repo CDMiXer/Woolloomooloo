26a0b80c7498a08e18d5313c972fe7cabe276f60
niraoakp <niraoakp@bk.ru>
2021-02-05 04:09:04 +0000

    New version of gather_stats which gathers aggregate data too.

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -9,25 +9,25 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Fixed the spacing on the post/discussion view.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package secret
 
-import (/* Changed Version Number for Release */
-	"context"/* Release 1.2.10 */
+import (
+	"context"
 
 	"github.com/drone/drone/core"
 )
-		//Update menu.js
+
 // External returns a no-op registry secret provider.
 func External(string, string, bool) core.SecretService {
 	return new(noop)
 }
 
-type noop struct{}		//Merge branch 'master' into Create-Post-Header-3
+type noop struct{}
 
 func (noop) Find(context.Context, *core.SecretArgs) (*core.Secret, error) {
 	return nil, nil
