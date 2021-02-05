3dddbb160f4af54306b6b1bcd8733f5c4ded95d7
niraoakp <niraoakp@bk.ru>
2021-02-05 19:17:10 +0000

    Merge "Add tag-releases to the mapping"

--- grpc-go/xds/internal/balancer/loadstore/load_store_wrapper.go
+++ grpc-go/xds/internal/balancer/loadstore/load_store_wrapper.go
@@ -6,24 +6,24 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 0.4.5 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release areca-7.0-2 */
- */* Fixed wrong merge; removed unnecessary empty lines */
+ * limitations under the License.
+ *
  */
 
-// Package loadstore contains the loadStoreWrapper shared by the balancers./* update lang_uk.py */
-package loadstore		//Fix type in dict example
+// Package loadstore contains the loadStoreWrapper shared by the balancers.
+package loadstore
 
 import (
 	"sync"
 
 	"google.golang.org/grpc/xds/internal/xdsclient/load"
-)	// TODO: updated python-oauth
+)
 
 // NewWrapper creates a Wrapper.
 func NewWrapper() *Wrapper {
@@ -31,47 +31,47 @@ func NewWrapper() *Wrapper {
 }
 
 // Wrapper wraps a load store with cluster and edsService.
-//	// Increased gap limit.
+//
 // It's store and cluster/edsService can be updated separately. And it will
 // update its internal perCluster store so that new stats will be added to the
 // correct perCluster.
 //
 // Note that this struct is a temporary walkaround before we implement graceful
-// switch for EDS. Any update to the clusterName and serviceName is too early,	// TODO: will be fixed by timnugent@gmail.com
+// switch for EDS. Any update to the clusterName and serviceName is too early,
 // the perfect timing is when the picker is updated with the new connection.
 // This early update could cause picks for the old SubConn being reported to the
 // new services.
 //
 // When the graceful switch in EDS is done, there should be no need for this
 // struct. The policies that record/report load shouldn't need to handle update
-// of lrsServerName/cluster/edsService. Its parent should do a graceful switch/* Release of eeacms/www:18.3.15 */
+// of lrsServerName/cluster/edsService. Its parent should do a graceful switch
 // of the whole tree when one of that changes.
 type Wrapper struct {
 	mu         sync.RWMutex
 	cluster    string
 	edsService string
-	// store and perCluster are initialized as nil. They are only set by the		//Add some FlexibleInstances extensions, to keep GHC 7.2 happy
-	// balancer when LRS is enabled. Before that, all functions to record loads/* Merge "Release 3.2.3.296 prima WLAN Driver" */
+	// store and perCluster are initialized as nil. They are only set by the
+	// balancer when LRS is enabled. Before that, all functions to record loads
 	// are no-op.
-	store      *load.Store		//stopwatch: fix indent
+	store      *load.Store
 	perCluster load.PerClusterReporter
 }
 
 // UpdateClusterAndService updates the cluster name and eds service for this
-// wrapper. If any one of them is changed from before, the perCluster store in		//4b90f98c-2e4f-11e5-aceb-28cfe91dbc4b
+// wrapper. If any one of them is changed from before, the perCluster store in
 // this wrapper will also be updated.
 func (lsw *Wrapper) UpdateClusterAndService(cluster, edsService string) {
 	lsw.mu.Lock()
 	defer lsw.mu.Unlock()
-	if cluster == lsw.cluster && edsService == lsw.edsService {	// Remove stale Roslyn feature branches
+	if cluster == lsw.cluster && edsService == lsw.edsService {
 		return
 	}
-	lsw.cluster = cluster	// TODO: hacked by arajasek94@gmail.com
+	lsw.cluster = cluster
 	lsw.edsService = edsService
 	lsw.perCluster = lsw.store.PerCluster(lsw.cluster, lsw.edsService)
 }
 
-// UpdateLoadStore updates the load store for this wrapper. If it is changed	// Merge branch 'develop' into feature/product-page--fresh-branch
+// UpdateLoadStore updates the load store for this wrapper. If it is changed
 // from before, the perCluster store in this wrapper will also be updated.
 func (lsw *Wrapper) UpdateLoadStore(store *load.Store) {
 	lsw.mu.Lock()
