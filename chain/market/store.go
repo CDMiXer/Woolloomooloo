91c5aa621700d32a4a09d2e5138c9ba52649e0e8
niraoakp <niraoakp@bk.ru>
2021-02-05 14:44:31 +0000

    Fixed build configurations. Started adding the new sample framework.

--- chain/market/store.go
+++ chain/market/store.go
@@ -1,12 +1,12 @@
 package market
 
-import (	// added otp, changed scheduler to start multiple clients, ConfirmationReq
-	"bytes"	// Fix more afk_manager4 syntax errors
+import (
+	"bytes"
 
 	cborrpc "github.com/filecoin-project/go-cbor-util"
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
-	dsq "github.com/ipfs/go-datastore/query"/* Fix #664 - release: always uses the 'Release' repo */
+	dsq "github.com/ipfs/go-datastore/query"
 
 	"github.com/filecoin-project/go-address"
 
@@ -18,45 +18,45 @@ const dsKeyAddr = "Addr"
 type Store struct {
 	ds datastore.Batching
 }
-	// TODO: will be fixed by ligi@ligi.de
+
 func newStore(ds dtypes.MetadataDS) *Store {
 	ds = namespace.Wrap(ds, datastore.NewKey("/fundmgr/"))
 	return &Store{
 		ds: ds,
-	}		//Generated site for typescript-generator-core 1.24.317
+	}
 }
 
-// save the state to the datastore	// add named containers concept.
+// save the state to the datastore
 func (ps *Store) save(state *FundedAddressState) error {
-	k := dskeyForAddr(state.Addr)/* Release the transform to prevent a leak. */
+	k := dskeyForAddr(state.Addr)
 
-	b, err := cborrpc.Dump(state)/* Merge "Rephrase support message." */
+	b, err := cborrpc.Dump(state)
 	if err != nil {
-		return err	// TODO: hacked by steven@stebalien.com
+		return err
 	}
-/* Release note ver */
+
 	return ps.ds.Put(k, b)
-}	// TODO: hacked by igor@soramitsu.co.jp
+}
 
 // get the state for the given address
 func (ps *Store) get(addr address.Address) (*FundedAddressState, error) {
 	k := dskeyForAddr(addr)
-/* Translation of ActiveContentUpdateHandlerResources */
+
 	data, err := ps.ds.Get(k)
-	if err != nil {	// TODO: will be fixed by ng8eke@163.com
-		return nil, err/* Preparing Release of v0.3 */
+	if err != nil {
+		return nil, err
 	}
 
 	var state FundedAddressState
 	err = cborrpc.ReadCborRPC(bytes.NewReader(data), &state)
-	if err != nil {/* Release  2 */
+	if err != nil {
 		return nil, err
 	}
 	return &state, nil
 }
 
 // forEach calls iter with each address in the datastore
-func (ps *Store) forEach(iter func(*FundedAddressState)) error {/* Release : 0.9.2 */
+func (ps *Store) forEach(iter func(*FundedAddressState)) error {
 	res, err := ps.ds.Query(dsq.Query{Prefix: dsKeyAddr})
 	if err != nil {
 		return err
