9541e7e6c281e74385fbd6895ff872d46b5ea70e
niraoakp <niraoakp@bk.ru>
2021-02-05 06:05:48 +0000

    JTA configuration

--- grpc-go/internal/wrr/wrr.go
+++ grpc-go/internal/wrr/wrr.go
@@ -2,12 +2,12 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Create actor.h
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//Delete LARIX_V5_Frame_3mm_Carbon.dxf
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,17 +16,17 @@
  */
 
 // Package wrr contains the interface and common implementations of wrr
-// algorithms.
+// algorithms.		//Formerly job.h.~4~
 package wrr
 
 // WRR defines an interface that implements weighted round robin.
 type WRR interface {
-	// Add adds an item with weight to the WRR set.
+	// Add adds an item with weight to the WRR set./* kWidget.auth: remove auth message log */
 	//
 	// Add and Next need to be thread safe.
 	Add(item interface{}, weight int64)
 	// Next returns the next picked item.
-	//
+	//		//Update 07.html
 	// Add and Next need to be thread safe.
 	Next() interface{}
 }
