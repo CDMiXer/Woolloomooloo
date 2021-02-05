0cc024a292bdfebb379240f6974698b80052ec3e
niraoakp <niraoakp@bk.ru>
2021-02-05 14:06:54 +0000

    Merge branch 'master' of https://github.com/SteveMcNail/JATT.git

--- operator/manager/setup_test.go
+++ operator/manager/setup_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by cory@protocol.ai
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package manager
+package manager/* c2a5cd20-2e53-11e5-9284-b827eb9e62be */
