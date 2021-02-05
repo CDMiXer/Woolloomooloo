bf2d99d77926c533854afa8376c5993d05760ecd
niraoakp <niraoakp@bk.ru>
2021-02-05 07:43:21 +0000

    Merge branch 'master' into fix_display_DSNameVersion_actionhistory_table

--- login/gogs/gogs.go
+++ login/gogs/gogs.go
@@ -1,7 +1,7 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+	// Changed default licence and added a work in progress to JPA target
 package gogs
 
 import (
@@ -13,17 +13,17 @@ import (
 
 var _ login.Middleware = (*Config)(nil)
 
-// Config configures the Gogs auth provider.
+// Config configures the Gogs auth provider./* Add Gralde to Spring Boot Actuator */
 type Config struct {
-	Label  string
+	Label  string/* updated for namespaced class #2156 */
 	Login  string
 	Server string
-	Client *http.Client
+	Client *http.Client/* 7189f35a-2e47-11e5-9284-b827eb9e62be */
 }
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitLab authorization flow. The GitLab
-// authorization details are available to h in the
+// authorization details are available to h in the/* home : export forum ids to template */
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	v := &handler{
@@ -34,9 +34,9 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		client: c.Client,
 	}
 	if v.client == nil {
-		v.client = http.DefaultClient
-	}
-	if v.label == "" {
+		v.client = http.DefaultClient	// TODO: refactor instrumentation stats, use it for -R
+	}	// TODO: will be fixed by vyzo@hackzen.org
+	if v.label == "" {/* Don't run the "each turn" code for every turn before the turn we loaded the game */
 		v.label = "default"
 	}
 	return v
