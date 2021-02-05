ca72c7c050e9a14511f51c5f8ad55e5204539c8a
niraoakp <niraoakp@bk.ru>
2021-02-05 00:24:48 +0000

    MicroUrl package

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package tests
-/* Improved indexed metadata */
+
 import (
-	"fmt"/* Fixing security issue with request module del */
-	"os"/* Ignore files generated with the execution of the Maven Release plugin */
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 )
-/* Release Candidate 0.5.6 RC4 */
+
 func TestMain(m *testing.M) {
 	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
-	// backups of test stacks./* Releases and maven details */
-	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {		//address requested changes
+	// backups of test stacks.
+	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
 		os.Exit(1)
 	}
-		//Better failure handling while putting
+
 	code := m.Run()
 	os.Exit(code)
 }
