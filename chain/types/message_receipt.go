331d2c0adf24c5501181dc50a8124d1942c618e9
niraoakp <niraoakp@bk.ru>
2021-02-05 13:04:05 +0000

    Added license (GNU GPL v2)

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -3,15 +3,15 @@ package types
 import (
 	"bytes"
 
-"edoctixe/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
-	GasUsed  int64/* Release 0.4.7 */
-}		//fix(deps): update dependency prop-types to v15.7.0
-/* Release 1.2.0.3 */
+	GasUsed  int64
+}
+
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
