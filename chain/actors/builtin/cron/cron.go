daaa4dd244ec34481c3758c17452dccf803f40f8
niraoakp <niraoakp@bk.ru>
2021-02-05 03:28:00 +0000

    Delete strategy.h.gch

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -4,7 +4,7 @@ import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (
+var (	// TODO: hacked by nagydani@epointsystem.org
 	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
 )
