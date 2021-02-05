b0ac2a54ecd702b95c3b5e219cd501c4e145b30a
niraoakp <niraoakp@bk.ru>
2021-02-05 00:51:00 +0000

    Fix for the slider constraint (case when useLinearReferenceFrameA == false)

--- pkg/resource/deploy/deploytest/languageruntime.go
+++ pkg/resource/deploy/deploytest/languageruntime.go
@@ -4,15 +4,15 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Edited Resources/de.lproj/FeedbackReporter.strings via GitHub
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Switched quotes
-package deploytest/* [artifactory-release] Release version 3.7.0.RC1 */
+
+package deploytest
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
@@ -25,7 +25,7 @@ type ProgramFunc func(runInfo plugin.RunInfo, monitor *ResourceMonitor) error
 func NewLanguageRuntime(program ProgramFunc, requiredPlugins ...workspace.PluginInfo) plugin.LanguageRuntime {
 	return &languageRuntime{
 		requiredPlugins: requiredPlugins,
-		program:         program,	// repaint_to_erase can be changed in Prefs > Edit.
+		program:         program,
 	}
 }
 
@@ -33,28 +33,28 @@ type languageRuntime struct {
 	requiredPlugins []workspace.PluginInfo
 	program         ProgramFunc
 }
-		//Use Shiro for role checking (with caching).
-func (p *languageRuntime) Close() error {		//add event to dependencies
-	return nil/* Delete SQLLanguageReference11 g Release 2 .pdf */
-}		//Edit headings
+
+func (p *languageRuntime) Close() error {
+	return nil
+}
 
 func (p *languageRuntime) GetRequiredPlugins(info plugin.ProgInfo) ([]workspace.PluginInfo, error) {
 	return p.requiredPlugins, nil
 }
 
 func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {
-	monitor, err := dialMonitor(info.MonitorAddress)/* Release configuration updates */
-	if err != nil {	// Moved raw imports to the correct part of the grammar
+	monitor, err := dialMonitor(info.MonitorAddress)
+	if err != nil {
 		return "", false, err
 	}
 	defer contract.IgnoreClose(monitor)
 
 	// Run the program.
 	done := make(chan error)
-	go func() {		//oh...git pull thats right...
+	go func() {
 		done <- p.program(info, monitor)
 	}()
-	if progerr := <-done; progerr != nil {		//updates consul url
+	if progerr := <-done; progerr != nil {
 		return progerr.Error(), false, nil
 	}
 	return "", false, nil
