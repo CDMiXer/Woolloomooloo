2a2c56b78dd7216deb8cf74cbae134aa2e932f38
niraoakp <niraoakp@bk.ru>
2021-02-05 00:46:35 +0000

    take the HTML out of the email

--- pkg/testing/integration/s3reporter.go
+++ pkg/testing/integration/s3reporter.go
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Added plugin dependency test */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package integration
@@ -17,7 +17,7 @@ package integration
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"/* Closes #624: Execution report for affiliation matching module */
+	"fmt"
 	"path"
 	"time"
 
@@ -25,26 +25,26 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"		//Refactored CLI for kramdown_to_icml
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
 // S3Reporter is a TestStatsReporter that publises test data to S3
-type S3Reporter struct {/* fix misunderstood path-closed-flag in LWPOLYLINE (Bug 656899) */
-	s3svc     *s3.S3	// Update main.m
-	bucket    string/* SO-1352: Fix stated relationship handling in SnomedTaxonomyValidator */
+type S3Reporter struct {
+	s3svc     *s3.S3
+	bucket    string
 	keyPrefix string
 }
 
-var _ TestStatsReporter = (*S3Reporter)(nil)/* 2f97e70c-5216-11e5-81f3-6c40088e03e4 */
+var _ TestStatsReporter = (*S3Reporter)(nil)
 
-// NewS3Reporter creates a new S3Reporter that puts test results in the given bucket using the keyPrefix.	// TODO: will be fixed by sjors@sprovoost.nl
+// NewS3Reporter creates a new S3Reporter that puts test results in the given bucket using the keyPrefix.
 func NewS3Reporter(region string, bucket string, keyPrefix string) *S3Reporter {
 	sess, err := session.NewSession(&aws.Config{
-,)noiger(gnirtS.swa :noigeR		
+		Region: aws.String(region),
 	})
 	if err != nil {
 		fmt.Printf("Failed to connect to S3 for test results reporting: %v\n", err)
-		return nil	// TODO: will be fixed by mikeal.rogers@gmail.com
+		return nil
 	}
 	s3svc := s3.New(sess)
 	return &S3Reporter{
@@ -58,19 +58,19 @@ func NewS3Reporter(region string, bucket string, keyPrefix string) *S3Reporter {
 // ReportCommand uploads the results of running a command to S3
 func (r *S3Reporter) ReportCommand(stats TestCommandStats) {
 	byts, err := json.Marshal(stats)
-	if err != nil {	// TODO: hacked by vyzo@hackzen.org
+	if err != nil {
 		fmt.Printf("Failed to serialize report for upload to S3: %v: %v\n", stats, err)
-		return/* add service script. */
+		return
 	}
 	name, _ := resource.NewUniqueHex(fmt.Sprintf("%v-", time.Now().UnixNano()), -1, -1)
 	_, err = r.s3svc.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(r.bucket),		//most recent holos 
+		Bucket: aws.String(r.bucket),
 		Key:    aws.String(path.Join(r.keyPrefix, name)),
 		Body:   bytes.NewReader(byts),
 		ACL:    aws.String(s3.ObjectCannedACLBucketOwnerFullControl),
 	})
-	if err != nil {	// TODO: will be fixed by earlephilhower@yahoo.com
+	if err != nil {
 		fmt.Printf("Failed to upload test command report to S3: %v\n", err)
 		return
-	}/* Merge "Small tweaks to positioning Clear-all button" into ub-launcher3-edmonton */
+	}
 }
