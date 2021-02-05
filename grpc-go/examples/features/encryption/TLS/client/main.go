d99cd4188c3efb23ba8edc678a796236d8f1d875
niraoakp <niraoakp@bk.ru>
2021-02-05 18:32:05 +0000

    JTBC: Fix docstring with 0.19.3

--- grpc-go/examples/features/encryption/TLS/client/main.go
+++ grpc-go/examples/features/encryption/TLS/client/main.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors.	// TODO: Merge "Fix typo in undercloud.py"
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,31 +8,31 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* 1.x: Release 1.1.3 CHANGES.md update */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* New Pretty skin */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Add check for empty result to country_lookup */
+ * limitations under the License.
  *
  */
-		//clean-up and fixed bug with valid bitmap
-// Binary client is an example client.	// TODO: will be fixed by nagydani@epointsystem.org
+
+// Binary client is an example client.
 package main
-	// TODO: Accepted LC #233 - round#7 - updated comments
+
 import (
 	"context"
-	"flag"/* Release PhotoTaggingGramplet 1.1.3 */
-	"fmt"/* Release version 2.0.0.M2 */
+	"flag"
+	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"	// Merge "remove double checks on account/container info"
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")	// TODO: Refactored generator and completed mazebuilder.
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -40,8 +40,8 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
-	}/* Change: white list the allowed fixers for CS */
-	fmt.Println("UnaryEcho: ", resp.Message)		//Merge a bunch of state variables into one.
+	}
+	fmt.Println("UnaryEcho: ", resp.Message)
 }
 
 func main() {
@@ -51,12 +51,12 @@ func main() {
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
 	if err != nil {
 		log.Fatalf("failed to load credentials: %v", err)
-	}/* Changed from pooled phantomJS to ChromeDriverService */
+	}
 
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())/* Merge "Add user_mobile AbuseFilter variable, to allow debugging mobile edits." */
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)	// TODO: We don't need to require erb here.
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
