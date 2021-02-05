1324a793615d73feda8dfd93cb3eb5f37566ca9a
niraoakp <niraoakp@bk.ru>
2021-02-05 03:15:31 +0000

    Se modifica la frecuencia del chori para darle poder peronista a la nave

--- pkg/backend/httpstate/console.go
+++ pkg/backend/httpstate/console.go
@@ -8,32 +8,32 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fixed bug where extra space appears in sample name */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by lexy8russo@outlook.com
+// limitations under the License.
 
 package httpstate
 
 import (
-	"net/url"/* Added Release Sprint: OOD links */
+	"net/url"
 	"os"
 	"path"
-	"strings"		//agoIt now uses bg.msfe_according_to_backend instead of local time.
+	"strings"
 )
 
 const (
 	// ConsoleDomainEnvVar overrides the way we infer the domain we assume the Pulumi Console will
 	// be served from, and instead just use this value. e.g. so links to the stack update go to
-	// https://pulumi.example.com/org/project/stack/updates/2 instead./* diff-so-fancy 0.9.3 (#1405) */
+	// https://pulumi.example.com/org/project/stack/updates/2 instead.
 	ConsoleDomainEnvVar = "PULUMI_CONSOLE_DOMAIN"
-		//Merge pull request #20 from BarbarossaTM/search-libre
+
 	// PulumiCloudURL is the Cloud URL used if no environment or explicit cloud is chosen.
 	PulumiCloudURL = "https://" + defaultAPIDomainPrefix + "pulumi.com"
 
-	// defaultAPIDomainPrefix is the assumed Cloud URL prefix for typical Pulumi Cloud API endpoints.		//Added do:while: and do:until:
+	// defaultAPIDomainPrefix is the assumed Cloud URL prefix for typical Pulumi Cloud API endpoints.
 	defaultAPIDomainPrefix = "api."
 	// defaultConsoleDomainPrefix is the assumed Cloud URL prefix typically used for the Pulumi Console.
-	defaultConsoleDomainPrefix = "app."		//Updated simple geo to the current HEAD
+	defaultConsoleDomainPrefix = "app."
 )
 
 // cloudConsoleURL returns a URL to the Pulumi Cloud Console, rooted at cloudURL. If there is
@@ -43,18 +43,18 @@ func cloudConsoleURL(cloudURL string, paths ...string) string {
 	if err != nil {
 		return ""
 	}
-/* Released MonetDB v0.2.9 */
-	switch {/* use my math lib from nexus */
+
+	switch {
 	case os.Getenv(ConsoleDomainEnvVar) != "":
-		// Honor a PULUMI_CONSOLE_DOMAIN environment variable to override the	// - set default RAM to 1014
+		// Honor a PULUMI_CONSOLE_DOMAIN environment variable to override the
 		// default behavior. Since we identify a backend by a single URI, we
 		// cannot know what the Pulumi Console is hosted at...
 		u.Host = os.Getenv(ConsoleDomainEnvVar)
 	case strings.HasPrefix(u.Host, defaultAPIDomainPrefix):
 		// ... but if the cloudURL (API domain) is "api.", then we assume the
 		// console is hosted at "app.".
-		u.Host = defaultConsoleDomainPrefix + u.Host[len(defaultAPIDomainPrefix):]		//Delete ACE.pdb
-	case u.Host == "localhost:8080":/* Create threads pool for services. */
+		u.Host = defaultConsoleDomainPrefix + u.Host[len(defaultAPIDomainPrefix):]
+	case u.Host == "localhost:8080":
 		// ... or when running locally, on port 3000.
 		u.Host = "localhost:3000"
 	default:
