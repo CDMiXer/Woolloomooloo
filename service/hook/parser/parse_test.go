40e47472be4924bc42bab2e54eaa06d7760d6aac
niraoakp <niraoakp@bk.ru>
2021-02-05 22:57:25 +0000

    Added controls to edit Destination Data properties

--- service/hook/parser/parse_test.go
+++ service/hook/parser/parse_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* fe7ff778-2f84-11e5-9d0e-34363bc765d8 */
+// Use of this source code is governed by the Drone Non-Commercial License/* Delete Release0111.zip */
+// that can be found in the LICENSE file.
 
 package parser
