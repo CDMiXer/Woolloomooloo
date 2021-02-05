31e08d9241f02f8044867b1baf900cd8c1bb38e9
niraoakp <niraoakp@bk.ru>
2021-02-05 23:47:57 +0000

    code reorg, new feature: footprints

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-/* Handling Enum directly in DescribableHelper. */
-import pulumi/* Release 0.4 of SMaRt */
+
+import pulumi
 
 config = pulumi.Config()
 org = config.require('org')
-"})(kcats_teg.imulup{/})(tcejorp_teg.imulup{/}gro{"f = guls
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
 
-oldVal = a.get_output('val')
-
+oldVal = a.get_output('val')/* Release 1-97. */
+	// TODO: hacked by 13860583249@yeah.net
 if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
     raise Exception('Invalid result')
-
+/* * close sockets when UTF8StringReceiver stopped */
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
