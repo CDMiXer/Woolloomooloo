abb1d9715b6af262413562dba2326263f8aaafc3
niraoakp <niraoakp@bk.ru>
2021-02-05 21:55:23 +0000

    Create 03. Exchange Variable Values

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Added files from Remotetunes plus */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// Delete vi.i18n.json
 
 // +build !oss
 
