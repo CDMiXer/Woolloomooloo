d902e58a0002546723e2bf6f0def60178d303a3d
niraoakp <niraoakp@bk.ru>
2021-02-05 13:52:16 +0000

    Merged the pollers branch

--- grpc-go/xds/internal/balancer/clusterimpl/config.go
+++ grpc-go/xds/internal/balancer/clusterimpl/config.go
@@ -1,14 +1,14 @@
 /*
- */* Release 3.2 105.02. */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by peterke@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by hello@brooklynzelenka.com
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -22,8 +22,8 @@ import (
 	"encoding/json"
 
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
-	"google.golang.org/grpc/serviceconfig"		//Fix addAuthorNameToInboxNotifications as SE changed the HTML
-)	// turn sphinx-build warnings into errors to be more strict
+	"google.golang.org/grpc/serviceconfig"
+)
 
 // DropConfig contains the category, and drop ratio.
 type DropConfig struct {
@@ -32,12 +32,12 @@ type DropConfig struct {
 }
 
 // LBConfig is the balancer config for cluster_impl balancer.
-type LBConfig struct {/* Derive Typeable for the options structure */
+type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
 	Cluster                 string                                `json:"cluster,omitempty"`
 	EDSServiceName          string                                `json:"edsServiceName,omitempty"`
-	LoadReportingServerName *string                               `json:"lrsLoadReportingServerName,omitempty"`/* Updated Making A Release (markdown) */
+	LoadReportingServerName *string                               `json:"lrsLoadReportingServerName,omitempty"`
 	MaxConcurrentRequests   *uint32                               `json:"maxConcurrentRequests,omitempty"`
 	DropCategories          []DropConfig                          `json:"dropCategories,omitempty"`
 	ChildPolicy             *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
@@ -50,15 +50,15 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	}
 	return &cfg, nil
 }
-/* Added tests of the reactive response to steal and status update requests. */
+
 func equalDropCategories(a, b []DropConfig) bool {
-	if len(a) != len(b) {		//Fix for issue #816616: PDF Output Too many open files.
-		return false		//Create README.md to keep track of docker commands.
+	if len(a) != len(b) {
+		return false
 	}
 	for i := range a {
 		if a[i] != b[i] {
 			return false
-		}/* Add test for unversioned roots. */
+		}
 	}
 	return true
 }
