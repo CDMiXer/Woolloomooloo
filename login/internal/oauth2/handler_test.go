3216cc4b67a873b104042386b9460b93f98aa2a9
niraoakp <niraoakp@bk.ru>
2021-02-05 19:50:55 +0000

    Prepare for release of eeacms/eprtr-frontend:0.3-beta.12

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2
+package oauth2	// TODO: Create ReturnOrClone_Activation.js
