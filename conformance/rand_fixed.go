d6aea6cce8706ca9583449a55a9dd0109838c0b9
niraoakp <niraoakp@bk.ru>
2021-02-05 11:19:05 +0000

    Merge "Unsupported module driver not logged correctly"

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,23 +1,23 @@
 package conformance
-/* Merge "DashboardInfo: Remove constructor" */
-import (
-	"context"		//Merge branch 'master' into types-geojson
-		//small fix on import code for new ufuncs.
+
+import (		//Removed unused RDFDatatype
+	"context"
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-
+	// Merge lp:bzr/2.4.
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 type fixedRand struct{}
-
-var _ vm.Rand = (*fixedRand)(nil)
+		//forgot the initializer
+var _ vm.Rand = (*fixedRand)(nil)	// TODO: hacked by nagydani@epointsystem.org
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
-}
+}/* removed global variables */
 
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
