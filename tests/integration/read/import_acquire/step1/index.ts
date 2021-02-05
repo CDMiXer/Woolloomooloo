db8d2850b7dff31b05ce711142a41050555d7516
niraoakp <niraoakp@bk.ru>
2021-02-05 21:28:25 +0000

    d526f223-2e9b-11e5-9c0f-a45e60cdfd11

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Delete test application when finished with it */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//New translations CC BY-SA 4.0.md (Turkish)
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,4 +15,4 @@
 import { Resource } from "./resource";
 
 // Setup: "a" is an external resource.
-const a = new Resource("a", { state: 42 }, { id: "0" });/* Added the next button and hot key parameters to the text screen wizard type. */
+const a = new Resource("a", { state: 42 }, { id: "0" });
