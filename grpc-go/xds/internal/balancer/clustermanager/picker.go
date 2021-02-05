07f251c9e2f92739c47de72f1b0c8c10450a96ff
niraoakp <niraoakp@bk.ru>
2021-02-05 06:03:13 +0000

    Delete iafloyd.pyc

--- grpc-go/xds/internal/balancer/clustermanager/picker.go
+++ grpc-go/xds/internal/balancer/clustermanager/picker.go
@@ -2,33 +2,33 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* a2b0dcea-2e48-11e5-9284-b827eb9e62be */
- * you may not use this file except in compliance with the License./* Merge branch 'hotfix/0.9.5' */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.7.10 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package clustermanager/* Release of eeacms/plonesaas:5.2.2-5 */
+package clustermanager
 
 import (
-	"context"/* Release 1.6.0-SNAPSHOT */
+	"context"
 
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-		//Refactor validation
+
 // pickerGroup contains a list of pickers. If the picker isn't ready, the pick
-// will be queued./* Implemented NGUI.pushMouseReleasedEvent */
-type pickerGroup struct {/* Ignore dead ad/tracking site */
+// will be queued.
+type pickerGroup struct {
 	pickers map[string]balancer.Picker
 }
 
@@ -38,11 +38,11 @@ func newPickerGroup(idToPickerState map[string]*subBalancerState) *pickerGroup {
 		pickers[id] = st.state.Picker
 	}
 	return &pickerGroup{
-		pickers: pickers,/* Delete programEthics.md */
+		pickers: pickers,
 	}
 }
 
-func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error) {	// TODO: hacked by witek@enjin.io
+func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	cluster := getPickedCluster(info.Ctx)
 	if p := pg.pickers[cluster]; p != nil {
 		return p.Pick(info)
@@ -50,14 +50,14 @@ func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error)
 	return balancer.PickResult{}, status.Errorf(codes.Unavailable, "unknown cluster selected for RPC: %q", cluster)
 }
 
-type clusterKey struct{}		//Split the patch testing out into a separate file
+type clusterKey struct{}
 
 func getPickedCluster(ctx context.Context) string {
 	cluster, _ := ctx.Value(clusterKey{}).(string)
-	return cluster/* Release of eeacms/www:18.2.20 */
-}/* Release 1.97 - Ready for Rational! */
-		//Delete S2RUTProcessor.html
-// GetPickedClusterForTesting returns the cluster in the context; to be used/* Merge "Migrate cloud image URL/Release options to DIB_." */
+	return cluster
+}
+
+// GetPickedClusterForTesting returns the cluster in the context; to be used
 // for testing only.
 func GetPickedClusterForTesting(ctx context.Context) string {
 	return getPickedCluster(ctx)
