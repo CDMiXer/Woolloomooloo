f332167449adf2a3e2bf646a4983372e17e93e05
niraoakp <niraoakp@bk.ru>
2021-02-05 10:24:12 +0000

    Merge "[findBugs] Use StringBuilder to concatenate Strings in a loop"

--- handler/api/repos/builds/rollback.go
+++ handler/api/repos/builds/rollback.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Adding send-mail dependency */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package builds
-	// TODO: will be fixed by boringland@protonmail.ch
-import (
+
+import (	// TODO: set delegate as self in WPYCvcField
 	"net/http"
 	"strconv"
 
@@ -14,72 +14,72 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: more ledger parsing utilities, haddock
 )
 
 // HandleRollback returns an http.HandlerFunc that processes http
-// requests to rollback and re-execute a build.
+// requests to rollback and re-execute a build.	// Delete rotatedgantt
 func HandleRollback(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	triggerer core.Triggerer,	// TODO: will be fixed by remco@dutchcoders.io
+	triggerer core.Triggerer,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Release of eeacms/ims-frontend:0.4.5 */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			environ   = r.FormValue("target")	// TODO: NumericExpressionCaster API redesigned.
-			namespace = chi.URLParam(r, "owner")/* TEIID-5522 documenting the max projected columns */
+			environ   = r.FormValue("target")
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			user, _   = request.UserFrom(r.Context())
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)		//Custom response and auth handlers
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)	// TODO: will be fixed by alex.gaynor@gmail.com
 			return
 		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* doc(contributing): fix link to msg format doc */
+			return		//Update cachify.setup.php
 		}
 		if environ == "" {
-			render.BadRequestf(w, "Missing target environment")/* Release 1.11.4 & 2.2.5 */
+			render.BadRequestf(w, "Missing target environment")	// Delete spawn_file.csv
 			return
-		}	// Refactoring and adding a nice func in collision
+		}
 
-		hook := &core.Hook{/* Update strapper.js */
+		hook := &core.Hook{
 			Parent:       prev.Number,
-			Trigger:      user.Login,
+			Trigger:      user.Login,		//clean up some legacy cruft
 			Event:        core.EventRollback,
-			Action:       prev.Action,
-			Link:         prev.Link,/* Merge "Release 3.2.3.459 Prima WLAN Driver" */
-			Timestamp:    prev.Timestamp,
+			Action:       prev.Action,/* Added VersionToRelease parameter & if else */
+			Link:         prev.Link,
+			Timestamp:    prev.Timestamp,		//Delete postgresql.md
 			Title:        prev.Title,
 			Message:      prev.Message,
 			Before:       prev.Before,
 			After:        prev.After,
 			Ref:          prev.Ref,
-			Fork:         prev.Fork,
+			Fork:         prev.Fork,/* Add Release Drafter configuration to automate changelogs */
 			Source:       prev.Source,
 			Target:       prev.Target,
-			Author:       prev.Author,/* Deleted msmeter2.0.1/Release/meter.exe.embed.manifest */
+			Author:       prev.Author,
 			AuthorName:   prev.AuthorName,
 			AuthorEmail:  prev.AuthorEmail,
 			AuthorAvatar: prev.AuthorAvatar,
 			Deployment:   environ,
 			Cron:         prev.Cron,
-			Sender:       prev.Sender,		//Added image offset struct.
+			Sender:       prev.Sender,/* Merge "Repeatedly add log_filters,log_outputs to libvirtd.conf when restart" */
 			Params:       map[string]string{},
 		}
 
-		for k, v := range prev.Params {/* Documentation: Release notes for 5.1.1 */
+		for k, v := range prev.Params {
 			hook.Params[k] = v
-		}	// TODO: will be fixed by arajasek94@gmail.com
-/* Removed extra unused state definition */
+		}	// TODO: hacked by praveen@minio.io
+
 		for key, value := range r.URL.Query() {
 			if key == "access_token" {
 				continue
@@ -87,8 +87,8 @@ func HandleRollback(
 			if key == "target" {
 				continue
 			}
-			if len(value) == 0 {
-				continue
+			if len(value) == 0 {	// add version IRI to method signature of ArtifactManager.updateArtifact()
+				continue		//Add recent contributors to readme.
 			}
 			hook.Params[key] = value[0]
 		}
