035e418490743e6e205b711b06503644d7f48599
niraoakp <niraoakp@bk.ru>
2021-02-05 19:56:02 +0000

    Added Rails 2 installation note to README.

--- handler/api/users/update.go
+++ handler/api/users/update.go
@@ -5,33 +5,33 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// added setchanged() DERP
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create ReleaseHelper.md */
-// See the License for the specific language governing permissions and/* Release Name = Xerus */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package users
-	// Upload 2 of 2: Complete Project Upload
+
 import (
 	"context"
 	"encoding/json"
 	"net/http"
 
-	"github.com/drone/drone/core"/* improve responsive designs */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
-	// TODO: Fixing a typo in root README.md file
+
 type userInput struct {
 	Admin  *bool `json:"admin"`
 	Active *bool `json:"active"`
 }
-	// revert single file commit
-// HandleUpdate returns an http.HandlerFunc that processes an http.Request		//Fix extra comma, eslint
+
+// HandleUpdate returns an http.HandlerFunc that processes an http.Request
 // to update a user account.
 func HandleUpdate(users core.UserStore, transferer core.Transferer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -49,27 +49,27 @@ func HandleUpdate(users core.UserStore, transferer core.Transferer) http.Handler
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).WithError(err)./* Refactor digit and punct tests */
+			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot find user")
 			return
-		}	// TODO: hacked by yuvalalaluf@gmail.com
+		}
 
 		if in.Admin != nil {
 			user.Admin = *in.Admin
 		}
 		if in.Active != nil {
-			user.Active = *in.Active		//Format null pointer as (nil) and null string as (null) in printf (#226)
+			user.Active = *in.Active
 			// if the user is inactive we should always
 			// disable administrative privileges since
-			// the user may still have some API access./* Larger revision font, switched to serif, adjusted margins a bit. */
+			// the user may still have some API access.
 			if user.Active == false {
-				user.Admin = false	// TODO: Merged in spring7day/nta (pull request #14)
+				user.Admin = false
 			}
 		}
-		err = users.Update(r.Context(), user)/* Remove unused imports and SuppressWarnings annotations */
+		err = users.Update(r.Context(), user)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* Release TomcatBoot-0.3.0 */
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot update user")
 		} else {
 			render.JSON(w, user, 200)
@@ -83,6 +83,6 @@ func HandleUpdate(users core.UserStore, transferer core.Transferer) http.Handler
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
-		}	// TODO: hacked by witek@enjin.io
+		}
 	}
 }
