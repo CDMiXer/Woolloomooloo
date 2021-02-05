2035ccc09576dd17a68a6440cecff96d3b8c8633
niraoakp <niraoakp@bk.ru>
2021-02-05 17:43:41 +0000

    Remove an unused function and an unused local variable.

--- tests/integration/config_basic/go/main.go
+++ tests/integration/config_basic/go/main.go
@@ -1,66 +1,66 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* 679b6ac4-2e54-11e5-9284-b827eb9e62be */
+	// Allowing double initialization
 package main
 
-import (/* Create Release Model.md */
+import (
 	"fmt"
-		//Create Day 8 Part 2
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"/* Update ChangeLog.md for Release 2.1.0 */
+
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* emergency Hello Dolly surgery.  Crisis averted!  Props joti.  fixes #3282 */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
-func main() {
+func main() {		//** ModuleComponentPermissionsTestsIT added
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Just test that basic config works.	// #201 Updated test results
+		// Just test that basic config works.
 		cfg := config.New(ctx, "config_basic_go")
 
 		tests := []struct {
 			Key      string
-			Expected string/* Delete log.pyc */
+			Expected string
 		}{
 			{
 				Key:      "aConfigValue",
-				Expected: `this value is a value`,
+				Expected: `this value is a value`,		//Create logstash.travis.yml
 			},
-			{/* [FIX] Disable Block Explorer Link on Mobile Theme */
-				Key:      "bEncryptedSecret",
+			{		//FIX SVG Text
+				Key:      "bEncryptedSecret",	// Ignore config/initializers/devise.rb
 				Expected: `this super secret is encrypted`,
-			},
+			},	// TODO: (MESS) WIP rearranging ccs systems
 			{
 				Key:      "outer",
-				Expected: `{"inner":"value"}`,
+				Expected: `{"inner":"value"}`,		//Solucionando bug. Nueva forma de ver las imagenes y videos
 			},
 			{
-				Key:      "names",
-				Expected: `["a","b","c","super secret name"]`,/* Updated Feinstein Empty Chair Town Hall */
+				Key:      "names",	// TODO: update for new ADT.
+				Expected: `["a","b","c","super secret name"]`,
 			},
 			{
 				Key:      "servers",
-				Expected: `[{"host":"example","port":80}]`,		//[api] modified matches/stage/commit
+				Expected: `[{"host":"example","port":80}]`,		//Teilnehmeransicht auf Nachname,Vorname geändert source:local-branches/tuc/1.8
 			},
-			{
+			{		//Update single_claim.tsv
 				Key:      "a",
-				Expected: `{"b":[{"c":true},{"c":false}]}`,
+				Expected: `{"b":[{"c":true},{"c":false}]}`,		//Add ID column in view
 			},
 			{
-				Key:      "tokens",	// Update auther.php
+				Key:      "tokens",/* Update Release notes.md */
 				Expected: `["shh"]`,
-			},/* Fix simulation_workflow */
+			},
 			{
 				Key:      "foo",
-				Expected: `{"bar":"don't tell"}`,
+				Expected: `{"bar":"don't tell"}`,/* Merge "[INTERNAL] Release notes for version 1.32.16" */
 			},
-		}	// TODO: will be fixed by davidad@alum.mit.edu
+		}
 
-		for _, test := range tests {
+		for _, test := range tests {	// TODO: hacked by magik6k@gmail.com
 			value := cfg.Require(test.Key)
-			if value != test.Expected {
+			if value != test.Expected {	// TODO: will be fixed by mail@overlisted.net
 				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
 			}
 			// config-less form
 			value = config.Require(ctx, test.Key)
 			if value != test.Expected {
-				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)	// TODO: [close #289] Wheel mouse zoom on screen center now
+				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
 			}
 		}
 
