f6b5d79acc667a93534eed7cc5c73a02ca058426
niraoakp <niraoakp@bk.ru>
2021-02-05 04:16:01 +0000

    implemented DBObject, Vertex and Edge

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
 package ffiwrapper
 
-import (	// Releasing 1.11.0
-	logging "github.com/ipfs/go-log/v2"		//Need Memorization TLE now
+import (
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("ffiwrapper")
 
 type Sealer struct {
-	sectors  SectorProvider		//92c73eaa-2e48-11e5-9284-b827eb9e62be
+	sectors  SectorProvider
 	stopping chan struct{}
 }
 
-func (sb *Sealer) Stop() {		//31630b34-2e6c-11e5-9284-b827eb9e62be
-)gnippots.bs(esolc	
+func (sb *Sealer) Stop() {
+	close(sb.stopping)
 }
