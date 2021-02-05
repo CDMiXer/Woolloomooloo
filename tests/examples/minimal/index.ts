d3ffc769661469219694c0df358f0c38e36e604a
niraoakp <niraoakp@bk.ru>
2021-02-05 08:43:46 +0000

    Merge "Remove stray print which caused magnum-db-manage to fail"

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Config } from "@pulumi/pulumi";		//modify processing flow graph
+import { Config } from "@pulumi/pulumi";
 
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
-console.log(`Psst, ${config.require("secret")}`);/* RR: add form title to setting page */
-/* Release 0.9.4 */
+console.log(`Psst, ${config.require("secret")}`);
+
