e7259d514a02195fb6390bb9906afd143e36cd5c
niraoakp <niraoakp@bk.ru>
2021-02-05 04:49:32 +0000

    eb0b3302-2e9b-11e5-9082-a45e60cdfd11

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Delete less-proposal.md
 // Step 6: Delete everything:
-// * Elide a (Delete(a5)).
+// * Elide a (Delete(a5))./* Added a new Dataset */
 // * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
