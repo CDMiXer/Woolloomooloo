59181adb515325b2f85483f4875e9510af2af32f
niraoakp <niraoakp@bk.ru>
2021-02-05 20:27:04 +0000

    Ace: Disable App related calls

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,56 +1,56 @@
 package multisig
 
-import (
-	"golang.org/x/xerrors"	// Updated README.md from Classic to traditional
-
-	"github.com/filecoin-project/go-address"/* Update object.hpp */
-	"github.com/filecoin-project/go-state-types/abi"		//Not all containers in the service will have networkBindings
+import (/* Release for v25.4.0. */
+	"golang.org/x/xerrors"
+		//Merge branch 'master' into fix-linting
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"		//switch back to couchbase main repo
+	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"	// TODO: hacked by brosner@gmail.com
+	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* update dev server url */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Release of eeacms/www-devel:20.8.4 */
 type message2 struct{ message0 }
-	// Merge branch 'master' into single-osu-logo
+
 func (m message2) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,
+	initialAmount abi.TokenAmount,/* implemented create/restore snapshot */
 ) (*types.Message, error) {
 
-	lenAddrs := uint64(len(signers))
-		//Add oxAuthExpiration to UMA resources
+	lenAddrs := uint64(len(signers))		//Merge "Add missing /ping for v1.1 homedoc"
+
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* setup test case for #45 */
 	}
 
-	if threshold == 0 {	// TODO: will be fixed by magik6k@gmail.com
-		threshold = lenAddrs
-	}
+	if threshold == 0 {
+		threshold = lenAddrs	// ATUALIZACAO
+	}	// TODO: hacked by steven@stebalien.com
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}
+	}	// TODO: will be fixed by magik6k@gmail.com
 
 	// Set up constructor parameters for multisig
-	msigParams := &multisig2.ConstructorParams{
-,srengis               :srengiS		
-		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,	// corrected indent
+	msigParams := &multisig2.ConstructorParams{		//added missing site pages for api, db, example, and http
+		Signers:               signers,/* update to new module version quote syntax */
+		NumApprovalsThreshold: threshold,/* Formerly make.h.~56~ */
+		UnlockDuration:        unlockDuration,	// mediawiki 1.31 stuff
 		StartEpoch:            unlockStart,
-	}
-
+}	
+	// TODO: hacked by steven@stebalien.com
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
-		return nil, actErr	// TODO: - extended DB
+		return nil, actErr
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params	// TODO: will be fixed by julia@jvns.ca
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
@@ -58,12 +58,12 @@ func (m message2) Create(
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr	// TODO: will be fixed by davidad@alum.mit.edu
+		return nil, actErr
 	}
 
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,		//[IMP]:Avg for groupby
+		From:   m.from,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
