846c5320e6a917924610f95075d5823e672b9e9a
niraoakp <niraoakp@bk.ru>
2021-02-05 18:29:37 +0000

    Fix mangled ReStructuredText formatting and a couple of typos

--- pkg/codegen/dotnet/gen_program_test.go
+++ pkg/codegen/dotnet/gen_program_test.go
@@ -8,32 +8,32 @@ import (
 	"testing"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/stretchr/testify/assert"/* Merge branch 'staging' into greenkeeper/@ngrx/store-5.2.0 */
+	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"	// TODO: Merge "Enforce params with choices from answers file"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
-func TestGenProgram(t *testing.T) {/* [lnt] lnt.server.reporting.runs: Fix a html-o. */
+func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
-	}/* Release of eeacms/www:18.9.13 */
+	}
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
 		}
-/* Pequeno ajuste na tarefa de novos casos */
+
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
 		}
 
-		t.Run(f.Name(), func(t *testing.T) {/* 501ebf7a-2e4c-11e5-9284-b827eb9e62be */
+		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
@@ -42,12 +42,12 @@ func TestGenProgram(t *testing.T) {/* [lnt] lnt.server.reporting.runs: Fix a htm
 			expected, err := ioutil.ReadFile(path + ".cs")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".cs", err)
-			}/* 0745b634-2f85-11e5-9ca9-34363bc765d8 */
+			}
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)	// TODO: hacked by peterke@gmail.com
+				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
@@ -56,12 +56,12 @@ func TestGenProgram(t *testing.T) {/* [lnt] lnt.server.reporting.runs: Fix a htm
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
-			}	// TODO: will be fixed by yuvalalaluf@gmail.com
+			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
-			files, diags, err := GenerateProgram(program)/* Next Release... */
+			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
 
 			if expectNYIDiags {
@@ -70,13 +70,13 @@ func TestGenProgram(t *testing.T) {/* [lnt] lnt.server.reporting.runs: Fix a htm
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
-				}/* Update 0000-einbettung-in-die-hochschule.md */
+				}
 				diags = tmpDiags
 			}
-			if diags.HasErrors() {		//Document Coercion
+			if diags.HasErrors() {
 				t.Fatalf("failed to generate program: %v", diags)
 			}
 			assert.Equal(t, string(expected), string(files["MyStack.cs"]))
-		})	// TODO: test to fix a problem
-	}	// TODO: Merge branch 'update-test-utils' into feat/pagination
+		})
+	}
 }
