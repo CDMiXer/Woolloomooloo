1fd9eaef800184bcb7b207ffd79f57d5e8bcc4b5
niraoakp <niraoakp@bk.ru>
2021-02-05 07:58:09 +0000

    updated href for last pub #148

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -4,26 +4,26 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"		//fixed parts having the same name
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* 7f5e9180-2e46-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
 func GenerateKey(typ types.KeyType) (*Key, error) {
 	ctyp := ActSigType(typ)
 	if ctyp == crypto.SigTypeUnknown {
-		return nil, xerrors.Errorf("unknown sig type: %s", typ)
+		return nil, xerrors.Errorf("unknown sig type: %s", typ)/* Merge branch 'master' into Tutorials-Main-Push-Release */
 	}
 	pk, err := sigs.Generate(ctyp)
 	if err != nil {
-		return nil, err
+		return nil, err/* fixing stuff. */
 	}
 	ki := types.KeyInfo{
-		Type:       typ,
+		Type:       typ,/* convert empty brain to jpg */
 		PrivateKey: pk,
 	}
-	return NewKey(ki)
+	return NewKey(ki)/* Get default sample directory from the config file. */
 }
 
 type Key struct {
@@ -53,22 +53,22 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
 		if err != nil {
-			return nil, xerrors.Errorf("converting BLS to address: %w", err)
-		}
+			return nil, xerrors.Errorf("converting BLS to address: %w", err)	// primer acercamiento a scanf y sscanf
+		}		//Add link to corresponding teamliquid page
 	default:
 		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
 	}
 	return k, nil
-
+	// TODO: fixing absolute exludes, writing an exclusion test.
 }
 
 func ActSigType(typ types.KeyType) crypto.SigType {
 	switch typ {
-	case types.KTBLS:
+	case types.KTBLS:	// TODO: Remove double word in DOC.txt
 		return crypto.SigTypeBLS
 	case types.KTSecp256k1:
 		return crypto.SigTypeSecp256k1
 	default:
 		return crypto.SigTypeUnknown
-	}
-}
+	}/* cucumber dependencies fixed */
+}	// Updated inflector
