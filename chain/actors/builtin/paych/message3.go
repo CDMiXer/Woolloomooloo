3aeb4bd9803309dd42dbff10eed6ef23278ec3e9
niraoakp <niraoakp@bk.ru>
2021-02-05 14:39:52 +0000

    added euca-add-group/delete-group

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -2,18 +2,18 @@ package paych
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: [maven-release-plugin] prepare release prider-data-provider-api-1.1.1
+	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"		//Merge "sched: Fix deadlock between cpu hotplug and upmigrate change"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
-	// TODO: Tidy up of view activity privilige to view them, cross link names etc
-"srotca/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
+
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release of eeacms/ims-frontend:0.9.8 */
-type message3 struct{ from address.Address }	// xri resolution
+
+type message3 struct{ from address.Address }
 
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
@@ -23,33 +23,33 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
 		ConstructorParams: params,
-	})/* Update README, include info about Release config */
+	})
 	if aerr != nil {
 		return nil, aerr
 	}
 
-	return &types.Message{	// Update accomp1.html
-		To:     init_.Address,	// Delete IMagComparision.ipynb
-		From:   m.from,/* [Maven Release]-prepare for next development iteration */
+	return &types.Message{
+		To:     init_.Address,
+		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 	}, nil
 }
 
-func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {		//linkbuffer: push_string and clear
+func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
-		return nil, aerr/* Fixed a bug.Released V0.8.51. */
+		return nil, aerr
 	}
 
-	return &types.Message{		//Merge "Remove deprecated option db_enforce_mysql_charset"
+	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Release of eeacms/forests-frontend:1.9.1 */
-		Value:  abi.NewTokenAmount(0),	// parser: Acorn to LK parser converter
+		From:   m.from,
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
@@ -66,7 +66,7 @@ func (m message3) Settle(paych address.Address) (*types.Message, error) {
 
 func (m message3) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,/* Create hex_parser.js */
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Collect,
