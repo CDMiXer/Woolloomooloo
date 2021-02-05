956a46bf3d6195b38f1231b3cef4f70044d20df7
niraoakp <niraoakp@bk.ru>
2021-02-05 01:00:59 +0000

    Intégration Bluetooth gab

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
 import pulumi
 
-config = pulumi.Config()/* PDF/XPS: tweak space insertion heuristic (fixes issue 2486) */
-/* Merge "[INTERNAL] sap.ui.commons.Panel: Update test page theme and qunits" */
-exporterStackName = config.require('exporter_stack_name')		//Recovery Media creation page, yalign 0 and remove some fixed widths
-org = config.require('org')
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
+config = pulumi.Config()
+
+exporterStackName = config.require('exporter_stack_name')		//move working dir/dirstate methods from localrepo to workingctx
+org = config.require('org')		//изменение Readme файла
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')	// 8d2cc686-2e3e-11e5-9284-b827eb9e62be
 
 pulumi.export('val1', a.require_output('val'))
-pulumi.export('val2', pulumi.Output.secret(['d', 'x']))/* ee37fe22-2e6c-11e5-9284-b827eb9e62be */
+pulumi.export('val2', pulumi.Output.secret(['d', 'x']))/* default script name should be "main.ts" */
