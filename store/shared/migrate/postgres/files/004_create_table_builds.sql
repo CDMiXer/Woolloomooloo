cbfddddaf1a2625f94d7f77bd7ce2dd1f5df3441
niraoakp <niraoakp@bk.ru>
2021-02-05 10:14:25 +0000

    Update likebox

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,7 +1,7 @@
 -- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
- build_id            SERIAL PRIMARY KEY/* Release1.3.8 */
+ build_id            SERIAL PRIMARY KEY
 ,build_repo_id       INTEGER
 ,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
@@ -14,46 +14,46 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_link          VARCHAR(2000)
 ,build_timestamp     INTEGER
 ,build_title         VARCHAR(2000)
-,build_message       VARCHAR(2000)/* add link to repo with publication data */
+,build_message       VARCHAR(2000)
 ,build_before        VARCHAR(50)
-,build_after         VARCHAR(50)/* Release 0.44 */
-,build_ref           VARCHAR(500)
+,build_after         VARCHAR(50)
+,build_ref           VARCHAR(500)/* Official Release 1.7 */
 ,build_source_repo   VARCHAR(250)
 ,build_source        VARCHAR(500)
 ,build_target        VARCHAR(500)
-,build_author        VARCHAR(500)/* chore(package): update @types/execa to version 0.8.0 */
-,build_author_name   VARCHAR(500)/* Creando la clase Buffer que almacena los Items antes de guardarlos */
+,build_author        VARCHAR(500)
+,build_author_name   VARCHAR(500)
 ,build_author_email  VARCHAR(500)
 ,build_author_avatar VARCHAR(2000)
 ,build_sender        VARCHAR(500)
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(4000)
-,build_started       INTEGER/* f4739078-2e5b-11e5-9284-b827eb9e62be */
+,build_started       INTEGER
 ,build_finished      INTEGER
-,build_created       INTEGER
-,build_updated       INTEGER
+,build_created       INTEGER		//perl-bioperl-core : osx disabled (deps missing)
+,build_updated       INTEGER	// TODO: will be fixed by why@ipfs.io
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
--- name: create-index-builds-incomplete
+/* Release final 1.0.0  */
+-- name: create-index-builds-incomplete/* remove redundant specs of CatchAndRelease */
 
 CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
-		//trigger new build for ruby-head-clang (cd2eb1f)
--- name: create-index-builds-repo
 
+-- name: create-index-builds-repo
+		//ChartInit - modify definitions class in order to adapt to bar module
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-/* Release v5.18 */
+
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
-/* Merge branch 'master' into jpaImpl */
+
 -- name: create-index-builds-sender
 
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
 
-CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
+CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);	// TODO: will be fixed by 13860583249@yeah.net
