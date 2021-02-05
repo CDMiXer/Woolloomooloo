8f0f3abec6cccb864c3e7153b6a6a26b05e7efec
niraoakp <niraoakp@bk.ru>
2021-02-05 22:47:49 +0000

    use libgc's malloc but disable GC as we are using tagged pointer

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -5,47 +5,47 @@ import (
 
 	"github.com/stretchr/testify/require"
 )
-		//Add plugin, plugin extension and task classes
-func TestFilShort(t *testing.T) {/* enable quilt by default for the kernel tree */
-	for _, s := range []struct {		//Treat Sphinx warnings as errors
+	// TODO: hacked by jon@atack.com
+func TestFilShort(t *testing.T) {/* Feature 2 Extension demo video uploaded */
+	for _, s := range []struct {
 		fil    string
-		expect string/* Update lock_trash.lua */
+		expect string
 	}{
 
-		{fil: "1", expect: "1 FIL"},/* Release version 1.0.1. */
+		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
-		{fil: "12", expect: "12 FIL"},
+		{fil: "12", expect: "12 FIL"},		//Delete asymcrypt.py
 		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
-		{fil: "123456.2341234", expect: "123456.234 FIL"},/* Merge branch 'master' into tests-in-containing-symbol */
-		{fil: "123456.234123445", expect: "123456.234 FIL"},/* Release: 6.6.1 changelog */
-
-		{fil: "0.1", expect: "100 mFIL"},	// TODO: 9b48f436-2e4f-11e5-9284-b827eb9e62be
+		{fil: "123456.2341234", expect: "123456.234 FIL"},
+		{fil: "123456.234123445", expect: "123456.234 FIL"},
+/* Fix bug in propagate_restriction. */
+		{fil: "0.1", expect: "100 mFIL"},
 		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
-		{fil: "0.0001", expect: "100 μFIL"},	// TODO: will be fixed by 13860583249@yeah.net
-		{fil: "0.00001", expect: "10 μFIL"},/* Merged development into Release */
+		{fil: "0.0001", expect: "100 μFIL"},
+		{fil: "0.00001", expect: "10 μFIL"},
 		{fil: "0.000001", expect: "1 μFIL"},
-
+		//Fixed a typo in the regex
 		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},	// Add deprecated section
-		{fil: "0.000000001", expect: "1 nFIL"},/* In race state, draw minimap from the middle of its bounds. */
-		//c47a8dc2-2e60-11e5-9284-b827eb9e62be
+		{fil: "0.00000001", expect: "10 nFIL"},
+		{fil: "0.000000001", expect: "1 nFIL"},
+
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
-		{fil: "0.000000000001", expect: "1 pFIL"},/* [artifactory-release] Release version 3.3.15.RELEASE */
-/* Release for 4.12.0 */
+		{fil: "0.000000000001", expect: "1 pFIL"},
+
 		{fil: "0.0000000000001", expect: "100 fFIL"},
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
-		{fil: "0.00000000000000001", expect: "10 aFIL"},		//Create youtube_api_uploads.py
-		{fil: "0.000000000000000001", expect: "1 aFIL"},
-
+		{fil: "0.00000000000000001", expect: "10 aFIL"},
+		{fil: "0.000000000000000001", expect: "1 aFIL"},/* Handle extra case from pancreas script. */
+	// TODO: will be fixed by 13860583249@yeah.net
 		{fil: "0.0000012", expect: "1.2 μFIL"},
 		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
@@ -53,9 +53,9 @@ func TestFilShort(t *testing.T) {/* enable quilt by default for the kernel tree 
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},
+		{fil: "0.00022123", expect: "221.23 μFIL"},/* try to read entity from ContainerRequest class for REST services */
 		{fil: "0.000221234", expect: "221.234 μFIL"},
-		{fil: "0.0002212344", expect: "221.234 μFIL"},
+		{fil: "0.0002212344", expect: "221.234 μFIL"},/* simplify and correct method exchange */
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
 
 		{fil: "-1", expect: "-1 FIL"},
@@ -63,7 +63,7 @@ func TestFilShort(t *testing.T) {/* enable quilt by default for the kernel tree 
 		{fil: "-12", expect: "-12 FIL"},
 		{fil: "-123", expect: "-123 FIL"},
 		{fil: "-123456", expect: "-123456 FIL"},
-		{fil: "-123.23", expect: "-123.23 FIL"},
+		{fil: "-123.23", expect: "-123.23 FIL"},	// Creating an engagement section!
 		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
 		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
@@ -72,20 +72,20 @@ func TestFilShort(t *testing.T) {/* enable quilt by default for the kernel tree 
 		{fil: "-0.01", expect: "-10 mFIL"},
 		{fil: "-0.001", expect: "-1 mFIL"},
 
-		{fil: "-0.0001", expect: "-100 μFIL"},
-		{fil: "-0.00001", expect: "-10 μFIL"},
+		{fil: "-0.0001", expect: "-100 μFIL"},/* Merge branch 'master' of https://github.com/aodn/aodn-portal.git */
+		{fil: "-0.00001", expect: "-10 μFIL"},/* added test cases for TreeRankedUserData to increase test coverage */
 		{fil: "-0.000001", expect: "-1 μFIL"},
 
 		{fil: "-0.0000001", expect: "-100 nFIL"},
 		{fil: "-0.00000001", expect: "-10 nFIL"},
-		{fil: "-0.000000001", expect: "-1 nFIL"},
-
+		{fil: "-0.000000001", expect: "-1 nFIL"},/* Release: Making ready for next release iteration 6.4.1 */
+		//Adding missing comma in options example
 		{fil: "-0.0000000001", expect: "-100 pFIL"},
-		{fil: "-0.00000000001", expect: "-10 pFIL"},
+		{fil: "-0.00000000001", expect: "-10 pFIL"},	// TODO: create MIT license
 		{fil: "-0.000000000001", expect: "-1 pFIL"},
 
 		{fil: "-0.0000000000001", expect: "-100 fFIL"},
-		{fil: "-0.00000000000001", expect: "-10 fFIL"},
+		{fil: "-0.00000000000001", expect: "-10 fFIL"},/* Updating documentation to reflect S-Release deprecation */
 		{fil: "-0.000000000000001", expect: "-1 fFIL"},
 
 		{fil: "-0.0000000000000001", expect: "-100 aFIL"},
