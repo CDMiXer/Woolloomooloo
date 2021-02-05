5a47a411a0a3042463e39c8cecb67671f2580285
niraoakp <niraoakp@bk.ru>
2021-02-05 13:20:56 +0000

    Added '/rpg disenchant' and '/rpg disenchant all' functionality

--- tests/integration/double_pending_delete/step1/index.ts
+++ tests/integration/double_pending_delete/step1/index.ts
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: Add gitlab-ci
+// Copyright 2016-2018, Pulumi Corporation./* Added templating of URL */
+//	// TODO: will be fixed by xiemengjun@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* roboconf/roboconf-platform#628 Apply Checkstyle to POM files */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Remove @NotNull annotation on id field
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by nicksavers@gmail.com
+// Unless required by applicable law or agreed to in writing, software		//Complete services
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* refactor active_record/adapter */
 
-import { Resource } from "./resource";	// Extract game js to a file and paint it inside div
-
-// Setup: Resources A and B are created successfully.
+import { Resource } from "./resource";		//Update webargs to 1.3.3 (#519)
+/* [artifactory-release] Release version 2.0.6.RC1 */
+// Setup: Resources A and B are created successfully./* Release web view properly in preview */
 const a = new Resource("a", { fail: 0 });
 const b = new Resource("b", { fail: 0 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
-//  B: Created/* Enable warnings again */
+//  B: Created
 
