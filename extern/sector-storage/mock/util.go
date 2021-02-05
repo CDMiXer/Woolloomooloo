539d306215698b3e831776af8dbe452a13fcf772
niraoakp <niraoakp@bk.ru>
2021-02-05 15:24:54 +0000

    Fix page 404 when page_for_posts is empty. fixes #6539

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
-package mock
+package mock	// TODO: bf60c076-2e5c-11e5-9284-b827eb9e62be
 
 func CommDR(in []byte) (out [32]byte) {
 	for i, b := range in {
 		out[i] = ^b
 	}
-
+/* Release areca-7.2.17 */
 	return out
 }
