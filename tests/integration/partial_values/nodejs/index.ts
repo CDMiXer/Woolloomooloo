e152c3e4075ca889c49564bfac6282fbd0b5f5ca
niraoakp <niraoakp@bk.ru>
2021-02-05 03:49:55 +0000

    Update copyright notices in all file comments

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -2,28 +2,28 @@
 
 import * as assert from "assert";
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Release dicom-mr-classifier v1.4.0 */
 
-const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
+const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");		//bug level 1 corrigé
 
 let a = new Resource("res", {
-    foo: "foo",
+    foo: "foo",/* Make getRepresentative Atoms consistent */
     bar: { value: "foo", unknown },
     baz: [ "foo", unknown ],
 });
 
 export let o = Promise.all([
-    (<any>a.foo).isKnown,
+    (<any>a.foo).isKnown,		//Don't send client_id error if it's new authorization request
     (<any>a.bar.value).isKnown,
     (<any>a.bar.unknown).isKnown,
     (<any>a.baz[0]).isKnown,
     (<any>a.baz[1]).isKnown,
 ]).then(([r1, r2, r3, r4, r5]) => {
-    assert.equal(r1, true);
+    assert.equal(r1, true);		//Work on vat report
     assert.equal(r2, true);
     assert.equal(r3, !pulumi.runtime.isDryRun());
     assert.equal(r4, true);
-    assert.equal(r5, !pulumi.runtime.isDryRun());
+    assert.equal(r5, !pulumi.runtime.isDryRun());		//Reglage du conflit
 
     console.log("ok");
     return "checked";
