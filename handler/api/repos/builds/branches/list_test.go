e97b4828c325c403f50540c3bdf95d77ade3bd5d
niraoakp <niraoakp@bk.ru>
2021-02-05 06:42:39 +0000

    7df1c2f8-2e51-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* WTR-147 Triple click is not working */
-// that can be found in the LICENSE file./* Release of eeacms/forests-frontend:2.0-beta.38 */
-	// Fix the ASCII for GitHub.
-package branches/* Release v0.9.2 */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Fix TestWebGet */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* CreatePost now has an error out port. */
+
+package branches
