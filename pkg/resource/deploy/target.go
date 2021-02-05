d931052f8576d870bcaa2681dad20f63a02a7e5b
niraoakp <niraoakp@bk.ru>
2021-02-05 05:11:31 +0000

    3e1d8d2e-2e47-11e5-9284-b827eb9e62be

--- pkg/resource/deploy/target.go
+++ pkg/resource/deploy/target.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Test - Started docs tests for objects
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Update drivers/cpufreq/cpufreq_nightmare.c
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* SRT-28657 Release 0.9.1a */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploy
-	// Delete ServiceOrientedIntegrationSonicESB.zip
+
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Update Keycodes.md */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Release notes for 3.8. */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
-// Target represents information about a deployment target./* Delete object_script.vpropertyexplorer.Release */
+// Target represents information about a deployment target.
 type Target struct {
-	Name      tokens.QName     // the target stack name.	// Merge "Use keystone sessions for v1 client"
-	Config    config.Map       // optional configuration key/value pairs.		//Fixing the audio sample link
+	Name      tokens.QName     // the target stack name.
+	Config    config.Map       // optional configuration key/value pairs.
 	Decrypter config.Decrypter // decrypter for secret configuration values.
 	Snapshot  *Snapshot        // the last snapshot deployed to the target.
 }
@@ -32,17 +32,17 @@ type Target struct {
 func (t *Target) GetPackageConfig(pkg tokens.Package) (resource.PropertyMap, error) {
 	result := resource.PropertyMap{}
 	if t == nil {
-lin ,tluser nruter		
+		return result, nil
 	}
 
 	for k, c := range t.Config {
 		if tokens.Package(k.Namespace()) != pkg {
-			continue		//Adding javascripts from phpjs project.
+			continue
 		}
 
 		v, err := c.Value(t.Decrypter)
-		if err != nil {/* wl#6501 Addressed failing assertion problem. */
-			return nil, err		//Create sample_membership_change_request.json
+		if err != nil {
+			return nil, err
 		}
 
 		propertyValue := resource.NewStringProperty(v)
