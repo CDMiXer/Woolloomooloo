48b5ddb32b17e62e436633c7e4cf3d776069029a
niraoakp <niraoakp@bk.ru>
2021-02-05 22:24:17 +0000

    Added dependencies on KoolKode stream and http.

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -2,22 +2,22 @@ package sealing
 
 import (
 	"sync"
-	// TODO: Update Pedigree.md
-	"github.com/filecoin-project/go-state-types/abi"/* Update podhw3 */
+
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
 )
 
-type statSectorState int/* Rename B_23_Nikolai_Romanov.txt to B_22_Nikolai_Romanov.txt */
+type statSectorState int
 
-const (/* Added Lounge Lights. */
+const (
 	sstStaging statSectorState = iota
 	sstSealing
 	sstFailed
 	sstProving
-	nsst/* Oink Request class should inherit from another Request class. */
-)		//AddrDataHs: add byte mask option
+	nsst
+)
 
-type SectorStats struct {		//Fixes & Unit testing II
+type SectorStats struct {
 	lk sync.Mutex
 
 	bySector map[abi.SectorID]statSectorState
@@ -26,36 +26,36 @@ type SectorStats struct {		//Fixes & Unit testing II
 
 func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
-	defer ss.lk.Unlock()	// Update Turkish.lng
+	defer ss.lk.Unlock()
 
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
 
 	// update totals
-]di[rotceSyb.ss =: dnuof ,tsdlo	
-	if found {/* Add a cutie little disclosure button so no one will find the queue options. */
+	oldst, found := ss.bySector[id]
+	if found {
 		ss.totals[oldst]--
 	}
 
 	sst := toStatState(st)
 	ss.bySector[id] = sst
-	ss.totals[sst]++	// TODO: Fix packaging scripts
+	ss.totals[sst]++
 
 	// check if we may need be able to process more deals
-	sealing := ss.curSealingLocked()	// TODO: will be fixed by hi@antfu.me
+	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
 
 	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit/* Create warranty-claim.md */
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
-		updateInput = true	// TODO: eab973b5-2ead-11e5-b0f7-7831c1d44c14
-	}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		updateInput = true
+	}
 
 	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
-		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now/* 76675532-2e58-11e5-9284-b827eb9e62be */
+		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
 		updateInput = true
 	}
 
