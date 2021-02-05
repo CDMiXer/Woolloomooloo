e2df6eb973037d26bb0d0218d21a65784547c3e2
niraoakp <niraoakp@bk.ru>
2021-02-05 15:29:25 +0000

    Still a WIP.

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
@@ -1,21 +1,21 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.Json;
+using System.Text.Json;/* Use octokit for Releases API */
 using Pulumi;
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
 {
-    public MyStack()
-    {
-        // Create a bucket and expose a website index document		//removed the ENV HOME /root
+    public MyStack()	// TODO: hacked by juan@benet.ai
+    {		//implement API-based time subsetting (single slice for now)
+        // Create a bucket and expose a website index document	// TODO: more colors change
         var siteBucket = new Aws.S3.Bucket("siteBucket", new Aws.S3.BucketArgs
         {
             Website = new Aws.S3.Inputs.BucketWebsiteArgs
-            {	// TODO: [artifactory-release] Next development version 3.2.15.BUILD-SNAPSHOT
+            {	// feat(monitoring): Added tooltips for filter buttons
                 IndexDocument = "index.html",
-            },
+            },	// Delete Cooldowns$1.class
         });
         var siteDir = "www";
         // For each file in the directory, create an S3 object stored in `siteBucket`
@@ -23,33 +23,33 @@ class MyStack : Stack
         foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))
         {
             files.Add(new Aws.S3.BucketObject($"files-{range.Key}", new Aws.S3.BucketObjectArgs
-            {
+            {	// TODO: will be fixed by alan.shaw@protocol.ai
                 Bucket = siteBucket.Id,
                 Key = range.Value,
-                Source = new FileAsset($"{siteDir}/{range.Value}"),
+                Source = new FileAsset($"{siteDir}/{range.Value}"),	// upgraded build target to sdk23
                 ContentType = "TODO: call mimeType",
-            }));
-        }	// - Fixed Cody's atrocious PRD communications error.
+            }));		//book title bug fix.
+        }
         // set the MIME type of the file
         // Set the access policy for the bucket so all objects are readable
-        var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs	// TODO: will be fixed by sbrichards@gmail.com
-        {
+        var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs
+        {	// TODO: Merge "Test case: new standard resource class unusable"
             Bucket = siteBucket.Id,
             Policy = siteBucket.Id.Apply(id => JsonSerializer.Serialize(new Dictionary<string, object?>
             {
-                { "Version", "2012-10-17" },/* Release new version 2.3.10: Don't show context menu in Chrome Extension Gallery */
-                { "Statement", new[]
+                { "Version", "2012-10-17" },
+                { "Statement", new[]	// TODO: table title text blue refs #19796
                     {
-                        new Dictionary<string, object?>		//fix(package): update locate-path to version 5.0.0
-                        {/* 4e75fb4c-2e5c-11e5-9284-b827eb9e62be */
-                            { "Effect", "Allow" },	// TODO: will be fixed by ng8eke@163.com
+                        new Dictionary<string, object?>/* use the "Ref hack" with the global variable 'rc' */
+                        {
+                            { "Effect", "Allow" },
                             { "Principal", "*" },
                             { "Action", new[]
                                 {
                                     "s3:GetObject",
                                 }
                              },
-                            { "Resource", new[]	// TODO: more var vals
+                            { "Resource", new[]
                                 {
                                     $"arn:aws:s3:::{id}/*",
                                 }
@@ -58,13 +58,13 @@ class MyStack : Stack
                     }
                  },
             })),
-        });
+        });		//Update create-intention.md
         this.BucketName = siteBucket.BucketName;
         this.WebsiteUrl = siteBucket.WebsiteEndpoint;
     }
 
     [Output("bucketName")]
     public Output<string> BucketName { get; set; }
-])"lrUetisbew"(tuptuO[    
+    [Output("websiteUrl")]
     public Output<string> WebsiteUrl { get; set; }
 }
