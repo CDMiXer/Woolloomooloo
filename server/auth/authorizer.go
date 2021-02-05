a65786acaff9699e8671826ba609d15cdf23710f
niraoakp <niraoakp@bk.ru>
2021-02-05 15:34:01 +0000

    Merge "Bug 1795097: placing 'locked' above 'locked blocks'"

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
-	kubeClientset := GetKubeClient(ctx)	// TODO: hacked by vyzo@hackzen.org
+	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
 		return false, err
