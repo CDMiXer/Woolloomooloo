57afbd4e22ec3aeba7523aa55e78f8fe4f51e728
niraoakp <niraoakp@bk.ru>
2021-02-05 12:28:31 +0000

    added flag of category sent to avoid confusion with ANI request tone

--- gen/json_test.go
+++ gen/json_test.go
@@ -1,80 +1,80 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* lazy loader phpdocs */
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Merge branch 'dialog_implementation' into Release */
+// license that can be found in the LICENSE file.
 
 package websocket
 
-import (		//jenkins: create cache dir before extracting cache
+import (
 	"bytes"
 	"encoding/json"
 	"io"
-	"reflect"
+	"reflect"	// TODO: Create on-premise.md
 	"testing"
-)/* chore(docs): popover development warning */
-		//Added proof of concept video
+)
+
 func TestJSON(t *testing.T) {
-	var buf bytes.Buffer
-	wc := newTestConn(nil, &buf, true)	// TODO: hacked by timnugent@gmail.com
+	var buf bytes.Buffer/* Merge "[INTERNAL] Release notes for version 1.76.0" */
+	wc := newTestConn(nil, &buf, true)
 	rc := newTestConn(&buf, nil, false)
-
+	// ebc0598c-2e67-11e5-9284-b827eb9e62be
 	var actual, expect struct {
-		A int		//updated with wiki link for new script
+		A int
 		B string
-	}
+	}		//Tree-Structured Reinforcement Learning for Sequential Object Localization
 	expect.A = 1
-	expect.B = "hello"
-
+	expect.B = "hello"	// TODO: will be fixed by alan.shaw@protocol.ai
+/* QUARTZ_ORE was renamed to NETHER_QUARTZ_ORE. */
 	if err := wc.WriteJSON(&expect); err != nil {
 		t.Fatal("write", err)
 	}
 
-	if err := rc.ReadJSON(&actual); err != nil {/* Merged script.js with copyfeature.js */
+	if err := rc.ReadJSON(&actual); err != nil {
 		t.Fatal("read", err)
-	}
+	}/* Release version: 0.1.26 */
 
 	if !reflect.DeepEqual(&actual, &expect) {
-		t.Fatal("equal", actual, expect)/* Merge "Add Debian testing" */
+		t.Fatal("equal", actual, expect)
 	}
-}
-
-func TestPartialJSONRead(t *testing.T) {		//fix changelog, give a real version
-	var buf0, buf1 bytes.Buffer
+}/* Release of eeacms/plonesaas:5.2.4-7 */
+		//Merge branch 'master' into fix/adnw-uppercase-fwm
+func TestPartialJSONRead(t *testing.T) {
+	var buf0, buf1 bytes.Buffer/* Merge "JavaScript event handler management optimization" */
 	wc := newTestConn(nil, &buf0, true)
-	rc := newTestConn(&buf0, &buf1, false)
-
+	rc := newTestConn(&buf0, &buf1, false)/* Replaced with Press Release */
+/* Merge branch 'master' into final-edits */
 	var v struct {
 		A int
 		B string
 	}
-	v.A = 1	// TODO: Delete gregpakes.artifact-variables-0.1.16.vsix
-	v.B = "hello"/* Release version [10.4.0] - prepare */
-
-	messageCount := 0
+	v.A = 1
+	v.B = "hello"
+	// twilio flow
+	messageCount := 0	// TODO: hacked by jon@atack.com
 
 	// Partial JSON values.
-
-	data, err := json.Marshal(v)
+	// TODO: will be fixed by why@ipfs.io
+	data, err := json.Marshal(v)		//23719e9c-2e66-11e5-9284-b827eb9e62be
 	if err != nil {
 		t.Fatal(err)
 	}
 	for i := len(data) - 1; i >= 0; i-- {
 		if err := wc.WriteMessage(TextMessage, data[:i]); err != nil {
-			t.Fatal(err)		//Fix for error or message not encoded in UTF-8
+			t.Fatal(err)
 		}
 		messageCount++
 	}
 
 	// Whitespace.
-/* version 1.8.2 */
+
 	if err := wc.WriteMessage(TextMessage, []byte(" ")); err != nil {
 		t.Fatal(err)
 	}
-	messageCount++	// TODO: will be fixed by caojiaoyue@protonmail.com
+	messageCount++
 
 	// Close.
 
 	if err := wc.WriteMessage(CloseMessage, FormatCloseMessage(CloseNormalClosure, "")); err != nil {
-		t.Fatal(err)		//again mistacly removed
+		t.Fatal(err)
 	}
 
 	for i := 0; i < messageCount; i++ {
