f03d0dcc0257ce9d0210625980d0553399fb4ed5
niraoakp <niraoakp@bk.ru>
2021-02-05 02:15:44 +0000

    Create BitField.c

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"/* Release notes screen for 2.0.3 */
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
@@ -10,7 +10,7 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
-		})
+		})	// -Changed look and feel to be more like the web site.
 		if err != nil {
 			return err
 		}
@@ -21,7 +21,7 @@ func main() {
 			"lifecycleRules[0]",
 		}))
 		if err != nil {
-			return err
+			return err/* Klasseöffnen und Testöffnen geht */
 		}
 		return nil
 	})
