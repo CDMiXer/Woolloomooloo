7df81dce0e56b4bd846253aea84b48a9aa3fd231
niraoakp <niraoakp@bk.ru>
2021-02-05 13:09:10 +0000

    Fix issue #27

--- core/webhook.go
+++ core/webhook.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Redesign around storing the weights in the WeightedWord
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,22 +14,22 @@
 
 package core
 
-import (/* [artifactory-release] Release version 2.4.2.RELEASE */
+import (
 	"context"
-)/* add JMTimeseries, JMListTimeseries Collections */
-	// removed unused part
+)
+
 // Webhook event types.
 const (
 	WebhookEventBuild = "build"
-	WebhookEventRepo  = "repo"		//ALEPH-12 Mixed scala/java project template
+	WebhookEventRepo  = "repo"
 	WebhookEventUser  = "user"
-)	// TODO: Refaktor OracleLoaderFile (přesun logiky do abstraktní třídy).
+)
 
 // Webhook action types.
 const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
-	WebhookActionDeleted  = "deleted"	// TODO: Merge branch 'master' into mohammad/session_duration
+	WebhookActionDeleted  = "deleted"
 	WebhookActionEnabled  = "enabled"
 	WebhookActionDisabled = "disabled"
 )
@@ -40,15 +40,15 @@ type (
 		Endpoint   string `json:"endpoint,omitempty"`
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
-	}/* 9d206540-2e6e-11e5-9284-b827eb9e62be */
-	// Merge "Fixed typo in README.rst"
+	}
+
 	// WebhookData provides the webhook data.
 	WebhookData struct {
 		Event  string      `json:"event"`
 		Action string      `json:"action"`
-		User   *User       `json:"user,omitempty"`	// TODO: adds textAlign to line label in the annotation plugin
+		User   *User       `json:"user,omitempty"`
 		Repo   *Repository `json:"repo,omitempty"`
-		Build  *Build      `json:"build,omitempty"`		//Rebuilt index with mozamomomoro
+		Build  *Build      `json:"build,omitempty"`
 	}
 
 	// WebhookSender sends the webhook payload.
