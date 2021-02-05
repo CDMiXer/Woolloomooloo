c952c0232b48c9f279cd17c1954970865581a886
niraoakp <niraoakp@bk.ru>
2021-02-05 04:37:46 +0000

    updated test feature file

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -1,4 +1,4 @@
-import pulumi	// Merge "Fix accidentally enabled debug " into androidx-main
+import pulumi
 import pulumi_azure as azure
 
 config = pulumi.Config()
@@ -10,7 +10,7 @@ if location_param is None:
     location_param = resource_group_var.location
 storage_account_tier_param = config.get("storageAccountTierParam")
 if storage_account_tier_param is None:
-    storage_account_tier_param = "Standard"	// TODO: will be fixed by steven@stebalien.com
+    storage_account_tier_param = "Standard"
 storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
 if storage_account_type_replication_param is None:
     storage_account_type_replication_param = "LRS"
@@ -18,7 +18,7 @@ storage_account_resource = azure.storage.Account("storageAccountResource",
     name=storage_account_name_param,
     account_kind="StorageV2",
     location=location_param,
-    resource_group_name=resource_group_name_param,		//extra test of r-mesh
+    resource_group_name=resource_group_name_param,
     account_tier=storage_account_tier_param,
-    account_replication_type=storage_account_type_replication_param)/* update InRelease while uploading to apt repo */
+    account_replication_type=storage_account_type_replication_param)
 pulumi.export("storageAccountNameOut", storage_account_resource.name)
