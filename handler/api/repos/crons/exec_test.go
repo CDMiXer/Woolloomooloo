263ed66856986b3dc1dc8e03a66ec96e663738d2
niraoakp <niraoakp@bk.ru>
2021-02-05 10:50:22 +0000

    use real 16 bit int

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//issue #375: existing select classes are loaded via SPI
+// that can be found in the LICENSE file.
 
 // +build !oss
-
-package crons
+	// TODO: will be fixed by martin2cai@hotmail.com
+package crons/* Release v0.8.0.4 */
