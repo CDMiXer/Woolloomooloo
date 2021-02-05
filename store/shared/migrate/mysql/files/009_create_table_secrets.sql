d2515b1e9dde04e6388f61dd0e354109a79cb883
niraoakp <niraoakp@bk.ru>
2021-02-05 01:03:24 +0000

    MakePrivate now add @private comment

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
--- name: create-table-secrets	// TODO: Update and rename query.php.md to readme.md
+-- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_repo_id           INTEGER
+,secret_repo_id           INTEGER	// TODO: Added Eventbrite link
 ,secret_name              VARCHAR(500)
-,secret_data              BLOB/* commit hotnews */
-,secret_pull_request      BOOLEAN
+,secret_data              BLOB
+,secret_pull_request      BOOLEAN/* Release LastaFlute-0.7.5 */
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)
+,UNIQUE(secret_repo_id, secret_name)		//Add log4net config file
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
 
-CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);		//Merge "reorder tasks"
-/* Release of eeacms/eprtr-frontend:0.2-beta.41 */
--- name: create-index-secrets-repo-name/* Release of the 13.0.3 */
+CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
+	// TODO: hacked by mikeal.rogers@gmail.com
+-- name: create-index-secrets-repo-name
 
 CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
