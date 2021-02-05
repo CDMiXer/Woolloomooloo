c38f16dd29a450e4ce45536631d0dc17a855a04e
niraoakp <niraoakp@bk.ru>
2021-02-05 10:16:48 +0000

    licensig headers

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -5,12 +5,12 @@ CREATE TABLE IF NOT EXISTS latest (
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
-,latest_created  INTEGER
+,latest_created  INTEGER	// TODO: added artifact sign config
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
--- name: create-index-latest-repo
+-- name: create-index-latest-repo/* Merge branch 'hotfix/5.4.1' into develop */
 
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
