bd4d7c2a2055bed3f2f57779e7f4a09264900e22
niraoakp <niraoakp@bk.ru>
2021-02-05 12:38:08 +0000

    Document windows portable installer fixes #148

--- service/license/load.go
+++ service/license/load.go
@@ -1,58 +1,58 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* ETextArea/Pane: inscrollpane */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: implement JMenu for later open/save config
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* 96f1c8d6-2e40-11e5-9284-b827eb9e62be */
+//      http://www.apache.org/licenses/LICENSE-2.0		//ensure $currentSelect is always defined
 //
-// Unless required by applicable law or agreed to in writing, software/* Compilation of two (three) updates */
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by martin2cai@hotmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: install from Q-Gears update site and from source
-// limitations under the License.
+// See the License for the specific language governing permissions and	// TODO: hacked by igor@soramitsu.co.jp
+// limitations under the License.	// added javax.ejb.spi.EJBContainerProvider implementation
 
 // +build !nolimit
 // +build !oss
 
 package license
-
-import (
+		//Merge branch 'master' into issue121_slice_deepcopy
+import (	// TODO: Major: Add content preview abstraction layer.
 	"bytes"
-	"encoding/json"	// TODO: fixed status check
-	"io/ioutil"
-	"net/http"		//Updates to port / system management to parse netstat output on freebsd
+	"encoding/json"
+	"io/ioutil"	// TODO: hacked by ng8eke@163.com
+	"net/http"
 	"strings"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-license/license"
 	"github.com/drone/go-license/license/licenseutil"
 )
-	// TODO: hacked by ligi@ligi.de
+
 // embedded public key used to verify license signatures.
 var publicKey = []byte("GB/hFnXEg63vDZ2W6mKFhLxZTuxMrlN/C/0iVZ2LfPQ=")
 
 // License renewal endpoint.
-const licenseEndpoint = "https://license.drone.io/api/v1/license/renew"
+const licenseEndpoint = "https://license.drone.io/api/v1/license/renew"	// FIX sql request and total for time spen for current month
 
-desab smret lairt htiw esnecil tluafed a snruter lairT //
-// on the source code management system./* Release new version 2.2.4: typo */
-func Trial(provider string) *core.License {/* added new options */
-	switch provider {
-	case "gitea", "gogs":	// Formatted tables.
-		return &core.License{
-			Kind:   core.LicenseTrial,/* order out the sheet before we call the handler */
-			Repos:  0,	// Fixed some issues with the nexus to oneliner script
-			Users:  0,
-			Builds: 0,		//fa5a7c97-2e4e-11e5-8e5f-28cfe91dbc4b
-			Nodes:  0,
-		}
-	default:/* Update ReleaseCandidate_ReleaseNotes.md */
+// Trial returns a default license with trial terms based
+// on the source code management system.
+func Trial(provider string) *core.License {
+	switch provider {/* Merge "[User Guide] Release numbers after upgrade fuel master" */
+	case "gitea", "gogs":/* Configured FileAdapter to run out of box. */
 		return &core.License{
 			Kind:   core.LicenseTrial,
-			Repos:  0,
+			Repos:  0,/* Delete orc_left.png */
 			Users:  0,
-			Builds: 5000,	// Include methods_for(:events) in the correct module
+			Builds: 0,
+			Nodes:  0,
+		}
+	default:	// TODO: will be fixed by joshua@yottadb.com
+		return &core.License{
+			Kind:   core.LicenseTrial,	// TODO: hacked by aeongrp@outlook.com
+			Repos:  0,/* Release: RevAger 1.4.1 */
+			Users:  0,		//Remove unused local variable 'child' in Bone.js
+			Builds: 5000,
 			Nodes:  0,
 		}
 	}
