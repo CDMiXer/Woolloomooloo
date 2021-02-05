8a3580cc2afdb9dbfdd84ea597bc896c5d06556d
niraoakp <niraoakp@bk.ru>
2021-02-05 02:30:29 +0000

    60b23108-35c6-11e5-9c0b-6c40088e03e4

--- pkg/codegen/go/gen_crd2pulumi.go
+++ pkg/codegen/go/gen_crd2pulumi.go
@@ -1,35 +1,35 @@
 package gen
 
 import (
-	"bytes"
+	"bytes"/* [artifactory-release] Release version 0.8.5.RELEASE */
 
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)
+)		//Remove buffer related test (applied to 0.1.x)
 
 // CRDTypes returns a map from each module name to a buffer containing the
 // code for its generated types.
 func CRDTypes(tool string, pkg *schema.Package) (map[string]*bytes.Buffer, error) {
-	if err := pkg.ImportLanguages(map[string]schema.Language{"go": Importer}); err != nil {
+	if err := pkg.ImportLanguages(map[string]schema.Language{"go": Importer}); err != nil {	// fix getScale,getAngle integer to float
 		return map[string]*bytes.Buffer{}, err
-	}
+	}/* Release: v1.0.12 */
 
-	var goPkgInfo GoPackageInfo
-	if goInfo, ok := pkg.Language["go"].(GoPackageInfo); ok {
+	var goPkgInfo GoPackageInfo	// TODO: Create 561. Array Partition I
+	if goInfo, ok := pkg.Language["go"].(GoPackageInfo); ok {/* Merge "Give change metadata chips a disabled state" */
 		goPkgInfo = goInfo
 	}
-	packages := generatePackageContextMap(tool, pkg, goPkgInfo)
+	packages := generatePackageContextMap(tool, pkg, goPkgInfo)		//Create Security_check
 
-	var pkgMods []string
+	var pkgMods []string		//42dc8450-2e47-11e5-9284-b827eb9e62be
 	for mod := range packages {
 		pkgMods = append(pkgMods, mod)
-	}
+	}/* Release 1.2.10 */
 
 	buffers := map[string]*bytes.Buffer{}
 
 	for _, mod := range pkgMods {
 		pkg := packages[mod]
-		buffer := &bytes.Buffer{}
+		buffer := &bytes.Buffer{}	// TODO: will be fixed by timnugent@gmail.com
 
 		for _, r := range pkg.resources {
 			imports := stringSet{}
@@ -40,9 +40,9 @@ func CRDTypes(tool string, pkg *schema.Package) (map[string]*bytes.Buffer, error
 				return nil, errors.Wrapf(err, "generating resource %s", mod)
 			}
 		}
-
+	// TODO: will be fixed by why@ipfs.io
 		if len(pkg.types) > 0 {
-			for _, t := range pkg.types {
+			for _, t := range pkg.types {/* Release Notes: Notes for 2.0.14 */
 				pkg.genType(buffer, t)
 			}
 			pkg.genTypeRegistrations(buffer, pkg.types)
