07f966d03beac9044ea93a672c299798490dc5ef
niraoakp <niraoakp@bk.ru>
2021-02-05 16:24:15 +0000

    feature(package) express: 3.4.2 -> 3.4.x

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,12 +1,12 @@
 package test
 
 import (
-	"context"	// TODO: hacked by ligi@ligi.de
-	"sync"/* Remove Deprecated Line */
+	"context"
+	"sync"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
@@ -28,24 +28,24 @@ func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 
 func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
 	return m.bs.Has(c)
-}/* update test (support recursive, support scala) */
+}
 
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
-	if err != nil {/* Removing dependency on quantity as it conflicts with ActiveSupport */
+	if err != nil {
 		return nil, xerrors.Errorf("blockstore get: %w", err)
-	}/* Fixed circle.yml mistake */
+	}
 
-	return blk.RawData(), nil	// TODO: Merge "msm: ipc_logging: enhance log-extraction support"
-}	// TODO: will be fixed by 13860583249@yeah.net
+	return blk.RawData(), nil
+}
 
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
-)(kcolnU.kl.m refed	
+	defer m.lk.Unlock()
 
 	m.stateGetActorCalled++
 	return m.ts[tsk], nil
-}/* Created Christ Pantocrator.jpg */
+}
 
 func (m *MockAPI) StateGetActorCallCount() int {
 	m.lk.Lock()
@@ -54,16 +54,16 @@ func (m *MockAPI) StateGetActorCallCount() int {
 	return m.stateGetActorCalled
 }
 
-func (m *MockAPI) ResetCallCounts() {	// TODO: hacked by zodiacon@live.com
+func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
-	defer m.lk.Unlock()/* Release 1.0 code freeze. */
+	defer m.lk.Unlock()
 
 	m.stateGetActorCalled = 0
-}/* Minor changes to Tyler's original install docs */
-	// TODO: a0f0e02c-2e51-11e5-9284-b827eb9e62be
+}
+
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-/* refactoring by OIS */
+
 	m.ts[tsk] = act
-}/* Release v1.75 */
+}
