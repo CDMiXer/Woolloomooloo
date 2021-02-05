b95c8eca51958a3973822c35653fbbe9164cb390
niraoakp <niraoakp@bk.ru>
2021-02-05 21:09:49 +0000

    misc: fix easybuild.sh to properly link bundled openssl

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,22 +1,22 @@
 package types
 
-import (
+import (/* Release: update to Phaser v2.6.1 */
 	"time"
-
+		//da37e44e-2e6e-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-address"
-)
+)	// week4-530 homework
 
 type MpoolConfig struct {
-	PriorityAddrs          []address.Address
-	SizeLimitHigh          int
+	PriorityAddrs          []address.Address/* Merge "Add unit tests around TestsController" */
+	SizeLimitHigh          int		//Create summon elemental.md
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
-	PruneCooldown          time.Duration
+	PruneCooldown          time.Duration/* Merge "Refactor do_create for nova profile" */
 	GasLimitOverestimation float64
 }
-
-func (mc *MpoolConfig) Clone() *MpoolConfig {
-	r := new(MpoolConfig)
+	// TODO: Merge "Make BgpSenderPartition::UpdatePeerQueue more efficient"
+func (mc *MpoolConfig) Clone() *MpoolConfig {/* Released Wake Up! on Android Market! Whoo! */
+	r := new(MpoolConfig)/* Fix pending issue with signing precomputed hashes. */
 	*r = *mc
 	return r
 }
