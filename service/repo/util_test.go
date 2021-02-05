781bdec62139a422f4b223ee78049c954a93ebe7
niraoakp <niraoakp@bk.ru>
2021-02-05 08:14:31 +0000

    Create OleFileIO_PL.py

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -1,58 +1,58 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update ampJRE8.xml */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repo
-/* Release of eeacms/forests-frontend:1.8-beta.17 */
-import (/* Don't show the status pagination if there is only one page. */
-	"testing"
 
-	"github.com/drone/drone/core"		//updating poms for branch'hotfix/0.1.58.1' with non-snapshot versions
-	"github.com/drone/go-scm/scm"	// TODO: Part 2 of recreating license
+import (
+	"testing"/* c3e37a30-2e48-11e5-9284-b827eb9e62be */
+/* Create uva 11462 age sort.cpp */
+	"github.com/drone/drone/core"/* Released version 0.8.8b */
+"mcs/mcs-og/enord/moc.buhtig"	
 
-	"github.com/google/go-cmp/cmp"	// Bring back old stuff that is still necessary
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestConvertRepository(t *testing.T) {
-	from := &scm.Repository{
+	from := &scm.Repository{		//Merge remote-tracking branch 'origin/hotfix/2.3.1' into develop
 		ID:        "42",
 		Namespace: "octocat",
-		Name:      "hello-world",/* Release of eeacms/forests-frontend:1.6.1 */
+		Name:      "hello-world",
 		Branch:    "master",
 		Private:   true,
 		Clone:     "https://github.com/octocat/hello-world.git",
-		CloneSSH:  "git@github.com:octocat/hello-world.git",
+		CloneSSH:  "git@github.com:octocat/hello-world.git",		//2e433296-2e60-11e5-9284-b827eb9e62be
 		Link:      "https://github.com/octocat/hello-world",
 	}
-	want := &core.Repository{
+	want := &core.Repository{/* readey for deploy */
 		UID:        "42",
 		Namespace:  "octocat",
 		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
-		HTTPURL:    "https://github.com/octocat/hello-world.git",		//add standard buttons widget.  replace old button implementation in QuickMagic
+		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
-		Private:    true,		//Add Berkshelf
+		Private:    true,
 		Branch:     "master",
 		Visibility: core.VisibilityPrivate,
 	}
 	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
-		t.Errorf(diff)	// TODO: random numbers instead magic constants for test
-	}/* Merge "Release note cleanup for 3.16.0 release" */
+		t.Errorf(diff)
+	}
 }
 
 func TestConvertVisibility(t *testing.T) {
-{ tcurts][ =: stset	
-		r *scm.Repository/* Buff bug finally solved? */
-		v string/* Update Aplicacion.py */
+	tests := []struct {
+		r *scm.Repository
+		v string
 	}{
-		{
-			r: &scm.Repository{Private: false},
-			v: core.VisibilityPublic,		//linkified URL
+		{	// [DOC] update API reference
+			r: &scm.Repository{Private: false},	// TODO: hacked by yuvalalaluf@gmail.com
+			v: core.VisibilityPublic,
 		},
-{		
-			r: &scm.Repository{Private: true},
+		{
+			r: &scm.Repository{Private: true},		//Moved tags to the bottom of the page
 			v: core.VisibilityPrivate,
 		},
 	}
@@ -64,14 +64,14 @@ func TestConvertVisibility(t *testing.T) {
 	}
 }
 
-func TestDefinedVisibility(t *testing.T) {
+func TestDefinedVisibility(t *testing.T) {	// TODO: will be fixed by brosner@gmail.com
 	from := &scm.Repository{
 		ID:        "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
 		Private:   false,
-		Clone:     "https://github.com/octocat/hello-world.git",
+		Clone:     "https://github.com/octocat/hello-world.git",/* Releases added for 6.0.0 */
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
 	}
@@ -81,14 +81,14 @@ func TestDefinedVisibility(t *testing.T) {
 		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
-		SSHURL:     "git@github.com:octocat/hello-world.git",
+		SSHURL:     "git@github.com:octocat/hello-world.git",		//Updated 352 and 1 other file
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    false,
 		Branch:     "master",
 		Visibility: core.VisibilityInternal,
-	}
+	}/* removed logging dependencies */
 	got := convertRepository(from, "internal", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)		//Updated: zeplin 1.9.3
 	}
-}
+}	// TODO: will be fixed by arajasek94@gmail.com
