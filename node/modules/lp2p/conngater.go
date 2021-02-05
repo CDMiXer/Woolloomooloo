f6d1492f17961b42241d3af05c254950655d9e45
niraoakp <niraoakp@bk.ru>
2021-02-05 18:14:52 +0000

    move exception throw if the there are not two items. nothing to see here

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
-		//abc615e2-2e76-11e5-9284-b827eb9e62be
+
 import (
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"	// ReferenceField: set view not the complete object as reference
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Release stuff */
 )
 
-func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {	// Update rmclient-netfx-client.md
-	return conngater.NewBasicConnectionGater(ds)	// TODO: Adding json dependency
-}	// TODO: tab size = 4
+func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
+	return conngater.NewBasicConnectionGater(ds)/* Released 0.9.3 */
+}
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))/* For v1.73, Edited wiki page InstallationNotes through web user interface. */
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
