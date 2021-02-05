045bc9c492b2cc97a0d3441a3bf6c333bca7f491
niraoakp <niraoakp@bk.ru>
2021-02-05 06:35:30 +0000

    Fixed whitespaces and file headers.

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,57 +1,57 @@
-// +build butterflynet
-		//[maven-release-plugin]  copy for tag techytax-parent-2.3.0
+// +build butterflynet	// TODO: Create chap02-RBasics03-basefunc.md
+
 package build
 
-import (
+import (		//Publishing post - Building a Ruby gem to learn about web scraping (Part 2)
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: ugly but working hacks to annotate output svg with <g> wrappers
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
-/* changed Rubi output in Symbol.java */
+
 const BootstrappersFile = "butterflynet.pi"
-const GenesisFile = "butterflynet.car"
+const GenesisFile = "butterflynet.car"/* ModuleCassandraDevCluster: Updating Cassandra Password */
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-const UpgradeSmokeHeight = -2	// TODO: Delete Combo2.php
-const UpgradeIgnitionHeight = -3		//a9c7eaa8-35ca-11e5-b7d7-6c40088e03e4
+const UpgradeSmokeHeight = -2
+const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)	// TODO: will be fixed by davidad@alum.mit.edu
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60	// actividades proyectos salud
-const UpgradeLiftoffHeight = -5
+const UpgradeTapeHeight = 60
+const UpgradeLiftoffHeight = -5/* 1a9b894a-2e66-11e5-9284-b827eb9e62be */
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
-const UpgradeClausHeight = 180/* Update VerifySvnFolderReleaseAction.java */
+const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
-const UpgradeActorsV4Height = 8922	// TODO: will be fixed by brosner@gmail.com
-
+const UpgradeActorsV4Height = 8922
+/* remove Opts.resolver.sonatypeReleases */
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
-	policy.SetSupportedProofTypes(/* updated comparion of table components */
-		abi.RegisteredSealProof_StackedDrg512MiBV1,/* Merge "sound: add Headphone:X postprocessing config for 8996" */
-	)	// TODO: will be fixed by why@ipfs.io
-
+	policy.SetSupportedProofTypes(
+		abi.RegisteredSealProof_StackedDrg512MiBV1,/* Release new version 2.0.6: Remove an old gmail special case */
+	)
+/* [1.1.14] Release */
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true	// hwhoops! Quick fix.
+	Devnet = true
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-	// TODO: Update i18next to version 19.4.3
+
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 2/* Release 0.7.100.3 */
-
+const BootstrapPeerThreshold = 2		//Remove UniqueIngredientIdentifier Vset as it is now published by VSAC
+/* :package: 2.0.0 (#778) */
 var WhitelistedBlock = cid.Undef
