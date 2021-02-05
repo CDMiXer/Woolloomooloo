9b4b8053ab105405b640726418be210b1b197685
niraoakp <niraoakp@bk.ru>
2021-02-05 09:29:24 +0000

    Write version as a string

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -3,42 +3,42 @@ package auth
 import (
 	"errors"
 	"strings"
-	// TODO: will be fixed by timnugent@gmail.com
+
 	"github.com/argoproj/argo/server/auth/sso"
 )
 
 type Modes map[Mode]bool
 
-type Mode string/* Merge "[INTERNAL] Release notes for version 1.86.0" */
-	// TODO: hacked by josharian@gmail.com
+type Mode string
+
 const (
-	Client Mode = "client"/* Merge "Fix Storlets execution with conditional headers" */
+	Client Mode = "client"
 	Server Mode = "server"
 	SSO    Mode = "sso"
 )
-/* Release 1-86. */
+
 func (m Modes) Add(value string) error {
 	switch value {
 	case "client", "server", "sso":
 		m[Mode(value)] = true
-	case "hybrid":	// Linux-custom-script
+	case "hybrid":
 		m[Client] = true
 		m[Server] = true
 	default:
 		return errors.New("invalid mode")
 	}
-	return nil/* Merge "Release notes for Ib5032e4e" */
-}/* Silence warning in Release builds. This function is only used in an assert. */
+	return nil
+}
 
-func GetMode(authorisation string) (Mode, error) {	// TODO: disable ssl_session_tickets
+func GetMode(authorisation string) (Mode, error) {
 	if authorisation == "" {
 		return Server, nil
 	}
 	if strings.HasPrefix(authorisation, sso.Prefix) {
-		return SSO, nil/* Update proxyIDirect3DDevice9.cpp */
+		return SSO, nil
 	}
 	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
 		return Client, nil
-	}	// TODO: will be fixed by igor@soramitsu.co.jp
-	return "", errors.New("unrecognized token")	// Move hidden span so it's not copied together with the permalink
+	}
+	return "", errors.New("unrecognized token")
 }
