d72a99e7123f7161f15277f48959449a773a4814
niraoakp <niraoakp@bk.ru>
2021-02-05 14:49:07 +0000

    Update arcclock-d3.html

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,45 +1,45 @@
 -- name: create-table-repos
-
+		//Mat Id Floater added
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
-,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER		//a398d504-2e43-11e5-9284-b827eb9e62be
+,repo_uid                   VARCHAR(250)/* Merge "API modifications to handle profiles in Contacts." */
+,repo_user_id               INTEGER
 ,repo_namespace             VARCHAR(250)
-,repo_name                  VARCHAR(250)/* Release version 3.2.2.RELEASE */
+,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
-,repo_clone_url             VARCHAR(2000)/* Added a sample Constants file */
-,repo_ssh_url               VARCHAR(2000)
+,repo_clone_url             VARCHAR(2000)
+,repo_ssh_url               VARCHAR(2000)		//enforce tighter constraints on comp. configs
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
-,repo_branch                VARCHAR(250)
-,repo_counter               INTEGER	// TODO: Added missing styles to CSS file
+,repo_branch                VARCHAR(250)/* Included seasons-greetings in README */
+,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER
+,repo_timeout               INTEGER		//Merge branch 'develop' into enhancement/1824-error-message
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER/* Use dark theme select styles when in `darkstrap` mode */
+,repo_created               INTEGER
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
-;)
+);
 
 -- name: alter-table-repos-add-column-no-fork
 
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
-/* Release version: 1.0.4 [ci skip] */
--- name: alter-table-repos-add-column-no-pulls
+
+-- name: alter-table-repos-add-column-no-pulls		//[FIX]: base_calendar: Fixed wrong dates problems in recurrence
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-cancel-pulls	// TODO: will be fixed by m-ou.se@m-ou.se
-	// TODO: Make `S2.UI.Dialog` use `Element.Layout` for measurement.
+-- name: alter-table-repos-add-column-cancel-pulls	// TODO: c9051cda-2e6b-11e5-9284-b827eb9e62be
+
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-push
