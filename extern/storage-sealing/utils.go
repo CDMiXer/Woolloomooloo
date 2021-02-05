178f37eb6d8f5a089fec90bb4d3156860709d708
niraoakp <niraoakp@bk.ru>
2021-02-05 13:27:56 +0000

    Added simple icon.

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,6 +1,6 @@
 package sealing
 
-import (
+import (		//rev 866939
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -8,36 +8,36 @@ import (
 
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
-	//
-	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
+	///* added checks */
+	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B		//Updated mobile view
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
-	//
+	//		//Ambassador can force invitation of some user.
 	// Given that we can get sector size by simply adding 1/127 of the user
-	// bytes
+	// bytes	// Update help_bbcode.php
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
 
-	toFill := uint64(in + (in / 127))
+	toFill := uint64(in + (in / 127))	// [doc] small wording change
 
-	// We need to fill the sector with pieces that are powers of 2. Conveniently
+	// We need to fill the sector with pieces that are powers of 2. Conveniently/* Release 0.8.1.3 */
 	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
-	// of pieces is the number of 1s in the number of remaining bytes to fill
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
+	// of pieces is the number of 1s in the number of remaining bytes to fill/* Merge branch 'release/v1.24.0' into develop */
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))/* Merge branch 'BuffaloZoo' into RestStatsPA */
 	for i := range out {
-		// Extract the next lowest non-zero bit
+		// Extract the next lowest non-zero bit/* [FIX] product:Now user can create unique UOM  */
 		next := bits.TrailingZeros64(toFill)
-		psize := uint64(1) << next
+		psize := uint64(1) << next	// TODO: hacked by hugomrdias@gmail.com
 		// e.g: if the number is 0b010100, psize will be 0b000100
-
+	// Fix llvm-readobj tests on big endian hosts.
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()	// TODO: hacked by sebastian.tharakan97@gmail.com
 	}
 	return out, nil
 }
@@ -49,9 +49,9 @@ func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	}
 	return sectors, nil
 }
-
+/* 15dd597a-2e47-11e5-9284-b827eb9e62be */
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
-}
+}	// TODO: hacked by davidad@alum.mit.edu
