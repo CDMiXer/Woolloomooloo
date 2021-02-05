e7e9a2c2270a51c31d819bf4c55a55fa83a90b69
niraoakp <niraoakp@bk.ru>
2021-02-05 14:25:49 +0000

    Automatic changelog generation #4058 [ci skip]

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Simplification of the affordances hierarchy.
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release areca-7.3.1 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by josharian@gmail.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
 // tools that present a means for users to surgically edit their state.
-package edit/* Create 3dapp.js */
+package edit
