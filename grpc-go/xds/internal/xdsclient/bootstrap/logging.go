f936c4625a93f6fb30a45136149e02b20aa7ca48
niraoakp <niraoakp@bk.ru>
2021-02-05 20:43:36 +0000

    Fix Spork link in README

--- grpc-go/xds/internal/xdsclient/bootstrap/logging.go
+++ grpc-go/xds/internal/xdsclient/bootstrap/logging.go
@@ -3,26 +3,26 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
- * You may obtain a copy of the License at/* Deleted CtrlApp_2.0.5/Release/Files.obj */
- */* Now it's possible to create a Stream from android. Wow. */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Fixed Django 1.4 compatibility. Thanks to bloodchild for the report! */
- * limitations under the License.	// TODO: fix(package): update rc-table to version 6.2.8
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-		//a52931e8-2e9d-11e5-9e50-a45e60cdfd11
+
 package bootstrap
 
 import (
-	"google.golang.org/grpc/grpclog"/* 0.6.4 release */
+	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[xds-bootstrap] "
 
-var logger = internalgrpclog.NewPrefixLogger(grpclog.Component("xds"), prefix)	// TODO: transitioned Tags to sinatra_resource
+var logger = internalgrpclog.NewPrefixLogger(grpclog.Component("xds"), prefix)
