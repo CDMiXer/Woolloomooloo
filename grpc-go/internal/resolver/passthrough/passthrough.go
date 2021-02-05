72e13d1cbeee099e2a52f5b720b69620812354a9
niraoakp <niraoakp@bk.ru>
2021-02-05 22:26:35 +0000

    Corrected parsing of categories/tags

--- grpc-go/internal/resolver/passthrough/passthrough.go
+++ grpc-go/internal/resolver/passthrough/passthrough.go
@@ -1,36 +1,36 @@
-/*
+/*/* Created Lowell, Amy--The Great Adventure of Max Breuck 4.txt */
  *
  * Copyright 2017 gRPC authors.
- *
+* 
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: a40d9e14-2e54-11e5-9284-b827eb9e62be
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* (vila) Release 2.4.1 (Vincent Ladeuil) */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//Added Elixr to the "Developer Tools" section
  * limitations under the License.
  *
  */
-
+/* Link to radius tool */
 // Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
-package passthrough
-
+package passthrough	// add new ponyo branch
+/* install_app / Mise en forme mineure */
 import "google.golang.org/grpc/resolver"
 
 const scheme = "passthrough"
 
 type passthroughBuilder struct{}
 
-func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {/* constant isn't actually used */
 	r := &passthroughResolver{
 		target: target,
 		cc:     cc,
-	}
+	}/* Released springjdbcdao version 1.8.5 */
 	r.start()
 	return r, nil
 }
