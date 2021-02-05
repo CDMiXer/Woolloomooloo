2bc583f333f12f606e628eabe5498bec51d1ce0b
niraoakp <niraoakp@bk.ru>
2021-02-05 08:51:07 +0000

    Update genDocDb.redis.py

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Fix send-related spelling typos (Karl Fogel)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Remove unneeded extra space" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by cory@protocol.ai
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//Update place
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update departures.md
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: ElementCollection @MapKey  Embedded REV-ENG support
+// limitations under the License.	// TODO: hacked by brosner@gmail.com
 
 package user
-	// Keybase Verification
+
 import (
 	"net/http"
 
@@ -23,18 +23,18 @@ import (
 	"github.com/drone/drone/logger"
 )
 
-// HandleRecent returns an http.HandlerFunc that write a json-encoded		//Implemented --render-auto/skip/force/reset command line options.
-// list of repository and build activity to the response body.
-func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {	// Extrai e testa show_online_contacts.
+// HandleRecent returns an http.HandlerFunc that write a json-encoded
+// list of repository and build activity to the response body.		//Added playlist sync logic
+func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
-		if err != nil {/* Create icons. */
+		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* Return EIO from SpiMaster::executeTransaction() on low-level failure */
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
 		} else {
 			render.JSON(w, list, 200)
 		}
 	}
-}	// Null merge from mysql-5.1.
+}
