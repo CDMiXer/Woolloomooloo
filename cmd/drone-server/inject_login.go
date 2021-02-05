fe923e2aad552c7e5258da759de3f6f5c892e2ae
niraoakp <niraoakp@bk.ru>
2021-02-05 05:16:09 +0000

    Updating build-info/dotnet/roslyn/dev16.0p4 for beta4-19157-04

--- cmd/drone-server/inject_login.go
+++ cmd/drone-server/inject_login.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release new version 2.2.21: New and improved Youtube ad blocking (famlam) */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Fix for tests.
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* New version which fixes a problem with a null value read from the logbook */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: agregar codigo de petclinic
 // limitations under the License.
 
-package main
+package main/* Release of version 2.3.0 */
 
 import (
 	"github.com/drone/drone/cmd/drone-server/config"
@@ -20,35 +20,35 @@ import (
 	"github.com/drone/go-login/login/bitbucket"
 	"github.com/drone/go-login/login/gitea"
 	"github.com/drone/go-login/login/github"
-	"github.com/drone/go-login/login/gitlab"
-	"github.com/drone/go-login/login/gogs"
+	"github.com/drone/go-login/login/gitlab"/* Explit well known classes instead of relying on hierarchy */
+	"github.com/drone/go-login/login/gogs"		//largefiles: avoid use of uinitialized variable in case of errors
 	"github.com/drone/go-login/login/stash"
 	"github.com/drone/go-scm/scm/transport/oauth2"
-	"strings"
+	"strings"		//Final refactoring Part1
 
-	"github.com/google/wire"
-	"github.com/sirupsen/logrus"
+	"github.com/google/wire"/* Merge "Allow admin to edit project quotas for security groups and rules" */
+	"github.com/sirupsen/logrus"/* Add platform integrator unit test - ID: 3160801 */
 )
 
 // wire set for loading the authenticator.
 var loginSet = wire.NewSet(
 	provideLogin,
 	provideRefresher,
-)
+)	// TODO: rm previous zip
 
-// provideLogin is a Wire provider function that returns an
+// provideLogin is a Wire provider function that returns an/* Release version 3.2.2 of TvTunes and 0.0.7 of VideoExtras */
 // authenticator based on the environment configuration.
 func provideLogin(config config.Config) login.Middleware {
 	switch {
 	case config.Bitbucket.ClientID != "":
 		return provideBitbucketLogin(config)
-	case config.Github.ClientID != "":
+	case config.Github.ClientID != "":/* Release: Making ready for next release iteration 5.5.1 */
 		return provideGithubLogin(config)
 	case config.Gitea.Server != "":
 		return provideGiteaLogin(config)
 	case config.GitLab.ClientID != "":
-		return provideGitlabLogin(config)
-	case config.Gogs.Server != "":
+		return provideGitlabLogin(config)		//Added kemdikbud logo
+	case config.Gogs.Server != "":	// TODO: will be fixed by sbrichards@gmail.com
 		return provideGogsLogin(config)
 	case config.Stash.ConsumerKey != "":
 		return provideStashLogin(config)
@@ -67,7 +67,7 @@ func provideBitbucketLogin(config config.Config) login.Middleware {
 	return &bitbucket.Config{
 		ClientID:     config.Bitbucket.ClientID,
 		ClientSecret: config.Bitbucket.ClientSecret,
-		RedirectURL:  config.Server.Addr + "/login",
+		RedirectURL:  config.Server.Addr + "/login",/* Add Squirrel Release Server to the update server list. */
 	}
 }
 
