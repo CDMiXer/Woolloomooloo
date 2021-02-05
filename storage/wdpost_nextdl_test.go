33b2b367483f913e533be172b279c546548704a8
niraoakp <niraoakp@bk.ru>
2021-02-05 15:15:27 +0000

    Added CategoryModel::SetLocalField().

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,32 +1,32 @@
 package storage
-		//Update package_installation.bash
+
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"/* * code style amendments and some docs */
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-)		//added inactive account to fixture data
+)
 
 func TestNextDeadline(t *testing.T) {
 	periodStart := abi.ChainEpoch(0)
-	deadlineIdx := 0/* Release dhcpcd-6.11.4 */
+	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)		//added more locale
-	require.EqualValues(t, -20, di.Challenge)/* Use spaces for alignment. see #15343 [16236] */
+	require.EqualValues(t, 0, di.PeriodStart)
+	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
-	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {	// TODO: Change UI Layout and modify setup and cpp stuff
+	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
-		expClose := expOpen + int(miner.WPoStChallengeWindow)	// package/kernel: remove SetDepends not needed anymore
+		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
