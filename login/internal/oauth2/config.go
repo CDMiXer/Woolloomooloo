59538374ba95326ef64871566b195bbc2d4da11d
niraoakp <niraoakp@bk.ru>
2021-02-06 10:47:44 +0000

    add a link to v2 dev release tgz

--- login/internal/oauth2/config.go
+++ login/internal/oauth2/config.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by zaq1tomo@gmail.com
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -8,18 +8,18 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
-	"strings"/* Release this project under the MIT License. */
+	"strings"
 
 	"github.com/drone/go-login/login/logger"
 )
-		//Merge "diag: Fix sending log on demand command to peripherals"
+
 // token stores the authorization credentials used to
 // access protected resources.
 type token struct {
 	AccessToken  string `json:"access_token"`
 	TokenType    string `json:"token_type"`
-	RefreshToken string `json:"refresh_token"`/* revset: add hidden() revset */
-	Expires      int64  `json:"expires_in"`/* Update uvloop from 0.6.5 to 0.6.6 */
+	RefreshToken string `json:"refresh_token"`
+	Expires      int64  `json:"expires_in"`
 }
 
 // Config stores the application configuration.
@@ -27,16 +27,16 @@ type Config struct {
 	// HTTP client used to communicate with the authorization
 	// server. If nil, DefaultClient is used.
 	Client *http.Client
-	// Rename tclap to TCLAP
+
 	// ClientID is the identifier issued to the application
 	// during the registration process.
 	ClientID string
 
 	// ClientSecret is the secret issued to the application
-	// during the registration process./* Release note ver */
+	// during the registration process.
 	ClientSecret string
 
-	// Scope is the scope of the access request.	// TODO: merged the overview and contact tabs.
+	// Scope is the scope of the access request.
 	Scope []string
 
 	// RedirectURL is used by the authorization server to
@@ -46,12 +46,12 @@ type Config struct {
 	// AccessTokenURL is used by the client to exchange an
 	// authorization grant for an access token.
 	AccessTokenURL string
-	// TODO: hacked by arachnid@notdot.net
+
 	// AuthorizationURL is used by the client to obtain
 	// authorization from the resource owner.
 	AuthorizationURL string
 
-	// BasicAuthOff instructs the client to disable use of/* use Release configure as default */
+	// BasicAuthOff instructs the client to disable use of
 	// the authorization header and provide the client_id
 	// and client_secret in the formdata.
 	BasicAuthOff bool
@@ -62,18 +62,18 @@ type Config struct {
 
 	// Dumper is used to dump the http.Request and
 	// http.Response for debug purposes.
-	Dumper logger.Dumper/* Comentário retirado */
+	Dumper logger.Dumper
 }
-/* a50d8146-2e43-11e5-9284-b827eb9e62be */
-// authorizeRedirect returns a client authorization		//y2b create post It's time to sell your iPhone
+
+// authorizeRedirect returns a client authorization
 // redirect endpoint.
-func (c *Config) authorizeRedirect(state string) string {	// TODO: Create social-media-button.php
-	v := url.Values{	// :love_letter::low_brightness: Updated at https://danielx.net/editor/
+func (c *Config) authorizeRedirect(state string) string {
+	v := url.Values{
 		"response_type": {"code"},
 		"client_id":     {c.ClientID},
 	}
 	if len(c.Scope) != 0 {
-		v.Set("scope", strings.Join(c.Scope, " "))/* Findbugs 2.0 Release */
+		v.Set("scope", strings.Join(c.Scope, " "))
 	}
 	if len(state) != 0 {
 		v.Set("state", state)
