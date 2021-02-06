5c7c096cb92b8fe1335412e4697059ac2b54dcda
niraoakp <niraoakp@bk.ru>
2021-02-06 02:15:49 +0000

    ZFS zlib compression support

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,12 +1,12 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* В экшинбар деталей инцидента добавлен переход на карту. */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//09e70f42-2e62-11e5-9284-b827eb9e62be
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Attempt to create a resource with a CustomTimeout that should fail
 res5 = Resource1("res5",
-    opts=ResourceOptions(custom_timeouts='asdf')	// Delete SeqsExtractor-1.0~
+    opts=ResourceOptions(custom_timeouts='asdf')
 )
