2d7545f5faf3019782d09ba8f685322edaec2848
niraoakp <niraoakp@bk.ru>
2021-02-06 08:52:23 +0000

    useful resources to find content

--- handler/api/user/sync.go
+++ handler/api/user/sync.go
@@ -7,11 +7,11 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release with jdk11 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Delete ReleaseNotes.md */
+
 package user
 
 import (
@@ -29,7 +29,7 @@ import (
 func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
-/* Convert babel.py indentation to spaces */
+
 		// performs asyncrhonous account synchronization.
 		// this requires long polling to determine when the
 		// sync is complete.
@@ -49,17 +49,17 @@ func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc
 		_, err := syncer.Sync(r.Context(), viewer)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* Delete btcprice2.py */
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot synchronize account")
 			return
 		}
 		list, err := repos.List(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* Task #3394: Merging changes made in LOFAR-Release-1_2 into trunk */
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot synchrnoize account")
 		} else {
 			render.JSON(w, list, 200)
-		}/* Released 1.6.1.9.2. */
+		}
 	}
 }
