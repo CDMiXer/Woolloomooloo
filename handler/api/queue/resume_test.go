cd1a521e86c375966c38414abacafbe29dc0969e
niraoakp <niraoakp@bk.ru>
2021-02-06 07:44:36 +0000

    get votes/ images / animation (sorta)

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* #756 make new annotation top level to avoid messing with '$' or '.' */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package queue
+	// New translations moderation.yml (Swedish, Finland)
+package queue	// TODO: Rename gocaraText.js to GocaraText.js
