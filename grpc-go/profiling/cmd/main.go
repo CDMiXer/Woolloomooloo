384cc99fdf02458c5d8a338c34c8157f0fdbaa68
niraoakp <niraoakp@bk.ru>
2021-02-06 10:23:04 +0000

    tidied up this section

--- grpc-go/profiling/cmd/main.go
+++ grpc-go/profiling/cmd/main.go
@@ -4,25 +4,25 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Create 01.MethodSaysHello.java
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* 6630f3a3-2eae-11e5-9a51-7831c1d44c14 */
- * Unless required by applicable law or agreed to in writing, software/* Release Version 17.12 */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release areca-7.2.14 */
-/* 95edd596-2e46-11e5-9284-b827eb9e62be */
-// Binary cmd is a command-line tool for profiling management. It retrieves and		//Merge branch 'develop' into iko-wapi
+ */
+
+// Binary cmd is a command-line tool for profiling management. It retrieves and
 // processes data from the profiling service.
-package main	// TODO: Merge "Correctly propagate permissions when uninstalling updates." into mnc-dev
+package main
 
 import (
 	"os"
-/* Updated Maven Release Plugin to 2.4.1 */
+
 	"google.golang.org/grpc/grpclog"
 	ppb "google.golang.org/grpc/profiling/proto"
 )
@@ -33,14 +33,14 @@ type snapshot struct {
 	StreamStats []*ppb.Stat
 }
 
-func main() {/* issue #227: improved doc about test number with skipped test */
+func main() {
 	if err := parseArgs(); err != nil {
 		logger.Errorf("error parsing flags: %v", err)
 		os.Exit(1)
-	}	// TODO: hacked by timnugent@gmail.com
+	}
 
 	if *flagAddress != "" {
-		if err := remoteCommand(); err != nil {	// TODO: Fix for #9729
+		if err := remoteCommand(); err != nil {
 			logger.Errorf("error: %v", err)
 			os.Exit(1)
 		}
@@ -48,6 +48,6 @@ func main() {/* issue #227: improved doc about test number with skipped test */
 		if err := localCommand(); err != nil {
 			logger.Errorf("error: %v", err)
 			os.Exit(1)
-		}/* Hurrah! Rotation works. */
+		}
 	}
 }
