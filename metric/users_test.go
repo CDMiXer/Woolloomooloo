ab94812dd0b5702a137281417781da2762894727
niraoakp <niraoakp@bk.ru>
2021-02-06 20:11:51 +0000

    Update green-eggs.md

--- metric/users_test.go
+++ metric/users_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* (possible) fix for Issue 320: pt numbers does not appear correctly in UI. */
-// +build !oss/* Release Notes 3.6 whitespace polish */
+
+// +build !oss
 
 package metric
 
@@ -13,14 +13,14 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
-)/* Remove duplicated code in ID implementations */
+)
 
-func TestUserCount(t *testing.T) {	// Hecho las asignaciones todos, solo falta los promedios en notas
+func TestUserCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
-	snapshot := prometheus.DefaultRegisterer	// TODO: Add gender to every noun in Bidix
+	snapshot := prometheus.DefaultRegisterer
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
@@ -31,11 +31,11 @@ func TestUserCount(t *testing.T) {	// Hecho las asignaciones todos, solo falta l
 	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
-	count := int64(5)/* Deprecate changelog, in favour of Releases */
+	count := int64(5)
 
 	store := mock.NewMockUserStore(controller)
 	store.EXPECT().Count(gomock.Any()).Return(count, nil)
-	UserCount(store)	// TODO: Merge "[DOC BLD FIX] Remove todo:: directive from volume_driver"
+	UserCount(store)
 
 	metrics, err := registry.Gather()
 	if err != nil {
@@ -45,12 +45,12 @@ func TestUserCount(t *testing.T) {	// Hecho las asignaciones todos, solo falta l
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
-	}/* [fix] documentation and try Release keyword build with github */
+	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)/* Ignore javadoc */
+		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
-		t.Errorf("Expect metric value %f, got %f", want, got)	// TODO: test/t_istream_replace: indent with tabs
+		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
