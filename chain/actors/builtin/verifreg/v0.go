951d4394886cf6beb39aee7e241192993027a1f2
niraoakp <niraoakp@bk.ru>
2021-02-06 16:07:58 +0000

    Create opencpu-launch.htm

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,57 +1,57 @@
-package verifreg
-		//Resolvendo conflitos...
+package verifreg		//preparation - rename
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+	// TODO: Création d'une première structure 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"/* Release pingTimer PacketDataStream in MKConnection. */
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}/* Videowall: use configuration file to show screen colors */
-	err := store.Get(store.Context(), root, &out)	// TODO: Search for the two packages in media folder
+	out := state0{store: store}/* Updated Release_notes.txt */
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
-	return &out, nil	// TODO: [MGWT-237] Misspelling in warning message.
-}
-
+	}/* green specs */
+	return &out, nil
+}/* Release v4.5.1 alpha */
+	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 type state0 struct {
 	verifreg0.State
 	store adt.Store
 }
 
-func (s *state0) RootKey() (address.Address, error) {/* add .67 build */
+func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}/* Release v3.6.9 */
-	// add timeline component
-{ )rorre ,rewoPegarotS.iba ,loob( )sserddA.sserdda rdda(paCataDtneilCdeifireV )0etats* s( cnuf
+}
+
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
-}	// TODO: simplified index.html & uploaded dangerous.php
+}
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
-{ rorre )rorre )rewoPegarotS.iba pacd ,sserddA.sserdda rdda(cnuf bc(reifireVhcaEroF )0etats* s( cnuf
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
-}
+}	// TODO: will be fixed by jon@atack.com
 
-func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* added ReleaseNotes.txt */
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
-}
-
+}		//Rebuilt index with rashidick
+	// TODO: JavaDocs and a bit of clean in DBOperator
 func (s *state0) verifiers() (adt.Map, error) {
-	return adt0.AsMap(s.store, s.Verifiers)	// TODO: Fix Palace's Desc.
+	return adt0.AsMap(s.store, s.Verifiers)
 }
