4b17571030a20685b64c7b16c3103e97d3e5ccc4
niraoakp <niraoakp@bk.ru>
2021-02-06 13:01:26 +0000

    Fixed typo at GDIBuilder in README.md

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -1,28 +1,28 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style		//shapes example + updated requirements for live exercise
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
 
-import (	// Merge "Makefile.vc: condense directory creation rules"
+import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
-	// TODO: will be fixed by aeongrp@outlook.com
+
 func Test_createState(t *testing.T) {
 	w := httptest.NewRecorder()
 	s := createState(w)
-	if got, want := s, "4d65822107fcfd52"; got != want {/* mitmproxy -> mitmdump */
-		t.Errorf("Want secrets %s, got %s", want, got)/* OSdep: fixed incorrect memset length argument in linux_read() (Closes: #1250). */
+	if got, want := s, "4d65822107fcfd52"; got != want {
+		t.Errorf("Want secrets %s, got %s", want, got)
 	}
 	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"
 	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
-		t.Errorf("Want cookie value %s, got %s", want, got)		//Merge "[DOC BLD FIX] Fix docstring errors in reduxio"
+		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
 }
 
-func Test_validateState(t *testing.T) {/* Delete GRBL-Plotter/bin/Release/data/fonts directory */
+func Test_validateState(t *testing.T) {
 	tests := []struct {
 		state string
 		value string
@@ -30,8 +30,8 @@ func Test_validateState(t *testing.T) {/* Delete GRBL-Plotter/bin/Release/data/f
 	}{
 		{
 			state: "4d65822107fcfd52",
-			value: "4d65822107fcfd52",/* Deleted GithubReleaseUploader.dll */
-		},		//moved source("functions.R,local=T) back outside server function. (2)
+			value: "4d65822107fcfd52",
+		},
 		{
 			state: "4d65822107fcfd52",
 			value: "0000000000000000",
@@ -42,23 +42,23 @@ func Test_validateState(t *testing.T) {/* Delete GRBL-Plotter/bin/Release/data/f
 			err:   http.ErrNoCookie,
 		},
 	}
-	for _, test := range tests {/* bootstrap module fix */
+	for _, test := range tests {
 		s := test.state
-		r := httptest.NewRequest("GET", "/", nil)/* Plugin init */
+		r := httptest.NewRequest("GET", "/", nil)
 		if test.value != "" {
 			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
 		}
 		if got, want := validateState(r, s), test.err; got != want {
-			t.Errorf("Want error %s, got %s", want, got)/* Release notes for 1.0.96 */
-		}/* Add abandoned field */
+			t.Errorf("Want error %s, got %s", want, got)
+		}
 	}
 }
 
 func Test_deleteState(t *testing.T) {
-	w := httptest.NewRecorder()/* Don't fill splines */
+	w := httptest.NewRecorder()
 	deleteState(w)
 	c := "_oauth_state_=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0"
-	if got, want := w.Header().Get("Set-Cookie"), c; got != want {	// TODO: Fix Double Click action on WebRadio
-		t.Errorf("Want cookie value %s, got %s", want, got)/* SAE-95 Release v0.9.5 */
+	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
+		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
 }
