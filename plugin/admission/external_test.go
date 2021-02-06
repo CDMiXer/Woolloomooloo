471d7453f7bfb7996b64aaff2700d58d36e7232c
niraoakp <niraoakp@bk.ru>
2021-02-06 13:25:18 +0000

    Fix mapping of symlink

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by lexy8russo@outlook.com
-// that can be found in the LICENSE file.
-
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: will be fixed by hugomrdias@gmail.com
+/* Added code coverage upload to .travis.yml */
 // +build !oss
 
-package admission/* Added Google form link */
+package admission
