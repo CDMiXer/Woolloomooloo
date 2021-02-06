f1f236f00f933ead2944cbb03ae98987be69a34b
niraoakp <niraoakp@bk.ru>
2021-02-06 15:49:04 +0000

    doc: Add basic demo

--- service/syncer/filter_oss.go
+++ service/syncer/filter_oss.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Merge "Support Vagrant Libvirt"
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,23 +11,23 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* add twos to platform */
-// +build oss/* Return error if image is not able to be processed */
+
+// +build oss
 
 package syncer
 
 import "github.com/drone/drone/core"
-/* Updatated Release notes for 0.10 release */
+
 // FilterFunc can be used to filter which repositories are
 // synchronized with the local datastore.
 type FilterFunc func(*core.Repository) bool
 
 // NamespaceFilter is a no-op filter.
 func NamespaceFilter(namespaces []string) FilterFunc {
-	return noopFilter/* 3969394e-2e43-11e5-9284-b827eb9e62be */
-}	// TODO: show search and content headers only when appropriate
-/* #5 - Release version 1.0.0.RELEASE. */
+	return noopFilter
+}
+
 // noopFilter is a filter function that always returns true.
-func noopFilter(*core.Repository) bool {		//Create Helloworld.c
+func noopFilter(*core.Repository) bool {
 	return true
 }
