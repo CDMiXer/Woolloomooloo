53bc09a2ed7c20031ea8bb7208f4b418b8d69264
niraoakp <niraoakp@bk.ru>
2021-02-06 06:38:59 +0000

    Rename test023_output-2-long.txt to test023_output-join-2.txt

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.		//Updated Copyright year, for consistency with MediaWiki.hs
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,21 +6,21 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// added missing comma in maps.json that prevented loading of the file
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release PPWCode.Utils.OddsAndEnds 2.3.1. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by steven@stebalien.com
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/*  BROKEN CODE: removing print statement */
-// +build oss
+// limitations under the License.		//added pr 247
+	// A few minor changes to README.md
+// +build oss		//Update deneme
 
 package validator
 
 import (
 	"context"
-	// TODO: will be fixed by mowrain@yandex.com
-	"github.com/drone/drone/core"
-)		//Debugging Windows job.
+
+	"github.com/drone/drone/core"	// TODO: hacked by hugomrdias@gmail.com
+)
 
 type noop struct{}
 
