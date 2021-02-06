7bf52414f575be3788252a01892f75634b547b26
niraoakp <niraoakp@bk.ru>
2021-02-06 18:44:42 +0000

    Añadida una introducción y puntos de partida

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release version [11.0.0-RC.2] - prepare */
+/* add verbiage to sweeping and power washing section */
 def main():
     return None
 
-if __name__ == "__main__":
+if __name__ == "__main__":/* Release v1.01 */
     main()
