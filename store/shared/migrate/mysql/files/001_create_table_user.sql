fa270fc3a26fb452fd1ccc4588b677e9b0b83faf
niraoakp <niraoakp@bk.ru>
2021-02-06 16:26:51 +0000

    Some more testing & resources.

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
--- name: create-table-users
+-- name: create-table-users/* Rename NanoLogger to NanoLogger.php */
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            INTEGER PRIMARY KEY AUTO_INCREMENT/* [artifactory-release] Release version 1.5.0.M1 */
+ user_id            INTEGER PRIMARY KEY AUTO_INCREMENT	// Initial Commit. JavaFX-Project
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN
+,user_admin         BOOLEAN		//Removed recursion from libs
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
-,user_avatar        VARCHAR(2000)
+,user_avatar        VARCHAR(2000)/* Added DAG MC to DAG Listings */
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER/* catches illegal state in stop to avoid ugly error message */
+,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER
-,user_last_login    INTEGER
-,user_oauth_token   VARCHAR(500)	// TODO: will be fixed by julia@jvns.ca
-,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER
+,user_updated       INTEGER	// TODO: Change mcs imprint
+,user_last_login    INTEGER	// TODO: read more link anchor removed
+,user_oauth_token   VARCHAR(500)
+,user_oauth_refresh VARCHAR(500)		//Delete agent.yml
+,user_oauth_expiry  INTEGER		//Create male_ru.txt
 ,user_hash          VARCHAR(500)
-,UNIQUE(user_login)/* Release of eeacms/www:19.3.26 */
-,UNIQUE(user_hash)/* Update quack.c */
+,UNIQUE(user_login)
+,UNIQUE(user_hash)
 );
