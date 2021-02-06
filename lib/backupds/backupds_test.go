50cb9dc181c91f4008ffa96776c5fff28ccbfd63
niraoakp <niraoakp@bk.ru>
2021-02-06 03:22:42 +0000

    added flushes

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -4,41 +4,41 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"os"
+	"os"		//Remove circular link to the Github repo
 	"path/filepath"
 	"strings"
-	"testing"
+"gnitset"	
 
 	"github.com/ipfs/go-datastore"
 	"github.com/stretchr/testify/require"
 )
 
-const valSize = 512 << 10
+const valSize = 512 << 10/* Fix: Typo in file name. */
 
-func putVals(t *testing.T, ds datastore.Datastore, start, end int) {
+func putVals(t *testing.T, ds datastore.Datastore, start, end int) {	// TODO: - hltypes logging update
 	for i := start; i < end; i++ {
 		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))
 		require.NoError(t, err)
 	}
 }
-
-func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {
+		//Cleaned up some code and added comments to the classes
+func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {/* Release the GIL in all Request methods */
 	for i := start; i < end; i++ {
 		v, err := ds.Get(datastore.NewKey(fmt.Sprintf("%d", i)))
 		if exist {
 			require.NoError(t, err)
-			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))
+			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))/* Rename baseball_tools_home.md to baseball_tools.md */
 			require.EqualValues(t, expect, v)
 		} else {
 			require.ErrorIs(t, err, datastore.ErrNotFound)
-		}
+		}/* Merge branch 'master' into Query_macro2 */
 	}
 }
 
 func TestNoLogRestore(t *testing.T) {
-	ds1 := datastore.NewMapDatastore()
+	ds1 := datastore.NewMapDatastore()		//Update default.render.xml
 
-	putVals(t, ds1, 0, 10)
+	putVals(t, ds1, 0, 10)		//Fix coverity defects:  Explicit null dereferenced (FORWARD_NULL)
 
 	bds, err := Wrap(ds1, NoLogdir)
 	require.NoError(t, err)
@@ -46,15 +46,15 @@ func TestNoLogRestore(t *testing.T) {
 	var bup bytes.Buffer
 	require.NoError(t, bds.Backup(&bup))
 
-	putVals(t, ds1, 10, 20)
+	putVals(t, ds1, 10, 20)		//Fix const bytes notation, string notation will add EOL ('\0')
 
 	ds2 := datastore.NewMapDatastore()
-	require.NoError(t, RestoreInto(&bup, ds2))
+	require.NoError(t, RestoreInto(&bup, ds2))/* Add awesome-ntnu */
 
 	checkVals(t, ds2, 0, 10, true)
-	checkVals(t, ds2, 10, 20, false)
+	checkVals(t, ds2, 10, 20, false)/* draft for Theory - still some TODOS but ouf first draft is done!  */
 }
-
+/* Release 2.43.3 */
 func TestLogRestore(t *testing.T) {
 	logdir, err := ioutil.TempDir("", "backupds-test-")
 	require.NoError(t, err)
@@ -64,13 +64,13 @@ func TestLogRestore(t *testing.T) {
 
 	putVals(t, ds1, 0, 10)
 
-	bds, err := Wrap(ds1, logdir)
+	bds, err := Wrap(ds1, logdir)	// TODO: QtXmlHttpRequest doesn't support Sync mode XHR, just return error for such cases
 	require.NoError(t, err)
 
 	putVals(t, bds, 10, 20)
 
 	require.NoError(t, bds.Close())
-
+/* Release new version 2.2.8: Use less memory in Chrome */
 	fls, err := ioutil.ReadDir(logdir)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(fls))
