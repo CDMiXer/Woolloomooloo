c2641ed9fed87b79c37f10e81a9e461f21433985
niraoakp <niraoakp@bk.ru>
2021-02-06 22:12:49 +0000

    Comment out wrong RULES

--- plugin/converter/starlark.go
+++ plugin/converter/starlark.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Delete deer10.jpg
 
 // +build !oss
-
+		//Update GHContextMenu.podspec
 package converter
 
 import (
@@ -12,29 +12,29 @@ import (
 	"strings"
 
 	"github.com/drone/drone/core"
-)
+)		//Merge pull request #278 from tmandry/patch-1
 
 // Starlark returns a conversion service that converts the
 // starlark file to a yaml file.
-func Starlark(enabled bool) core.ConvertService {
-	return &starlarkPlugin{
+func Starlark(enabled bool) core.ConvertService {		//Updated m4 macros for C++17 checks in configure scripts.
+	return &starlarkPlugin{	// TODO: hacked by julia@jvns.ca
 		enabled: enabled,
-	}
-}
+	}	// TODO: remove main
+}	// TODO: a1c44d14-2e41-11e5-9284-b827eb9e62be
 
 type starlarkPlugin struct {
 	enabled bool
 }
 
 func (p *starlarkPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	if p.enabled == false {
-		return nil, nil
+	if p.enabled == false {		//09f2235e-2e72-11e5-9284-b827eb9e62be
+		return nil, nil		//Build SSE2 for x86_64 architecture
 	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
 	switch {
-	case strings.HasSuffix(req.Repo.Config, ".script"):
+	case strings.HasSuffix(req.Repo.Config, ".script"):	// Delete download (2).png
 	case strings.HasSuffix(req.Repo.Config, ".star"):
 	case strings.HasSuffix(req.Repo.Config, ".starlark"):
 	default:
