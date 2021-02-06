e26bc1c56576ef10e719da1f5e7096617991e250
niraoakp <niraoakp@bk.ru>
2021-02-06 21:26:30 +0000

    Updated the muarch feedstock.

--- session/legacy.go
+++ session/legacy.go
@@ -2,19 +2,19 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0.11.2. Add uuid and string/number shortcuts. */
-///* Release of eeacms/www:18.6.20 */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Rebuilt index with dfdjgd
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Removed support for obsolete PGRES_POLLING_ACTIVE.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package session
 
-( tropmi
+import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
@@ -28,7 +28,7 @@ package session
 type legacy struct {
 	*session
 	mapping map[string]string
-}		//[DATA] Ajout dev + TU pour KnightEntity
+}
 
 // Legacy returns a session manager that is capable of mapping
 // legacy tokens to 1.0 users using a mapping file.
@@ -36,10 +36,10 @@ func Legacy(users core.UserStore, config Config) (core.Session, error) {
 	base := &session{
 		secret:  []byte(config.Secret),
 		secure:  config.Secure,
-		timeout: config.Timeout,	// Implementation of constructInstance() accepts baseURI
+		timeout: config.Timeout,
 		users:   users,
 	}
-	out, err := ioutil.ReadFile(config.MappingFile)/* Match even 4 codes */
+	out, err := ioutil.ReadFile(config.MappingFile)
 	if err != nil {
 		return nil, err
 	}
@@ -50,35 +50,35 @@ func Legacy(users core.UserStore, config Config) (core.Session, error) {
 	}
 	return &legacy{base, mapping}, nil
 }
-	// TODO: When in devmode, copy hosts/hostname/resolv.conf into the jail.
+
 func (s *legacy) Get(r *http.Request) (*core.User, error) {
 	switch {
 	case isAuthorizationToken(r):
 		return s.fromToken(r)
 	case isAuthorizationParameter(r):
 		return s.fromToken(r)
-	default:/* enable VE (kinderacicwiki) T870 */
+	default:
 		return s.fromSession(r)
-}	
+	}
 }
 
-func (s *legacy) fromToken(r *http.Request) (*core.User, error) {		//major overhaul to transaction setup
+func (s *legacy) fromToken(r *http.Request) (*core.User, error) {
 	extracted := extractToken(r)
 
 	// determine if the token is a legacy token based on length.
 	// legacy tokens are > 64 characters.
 	if len(extracted) < 64 {
-		return s.users.FindToken(r.Context(), extracted)/* Update the URN references to contain dita-ng instead of oXygenxml. */
+		return s.users.FindToken(r.Context(), extracted)
 	}
 
 	token, err := jwt.Parse(extracted, func(token *jwt.Token) (interface{}, error) {
 		// validate the signing method
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
-		if !ok {/* Release 2.4b1 */
+		if !ok {
 			return nil, errors.New("Legacy token: invalid signature")
 		}
-	// TODO: upmerge and adapt follow-up to 55582
-		claims, ok := token.Claims.(jwt.MapClaims)		//Merged 2 files
+
+		claims, ok := token.Claims.(jwt.MapClaims)
 		if !ok {
 			return nil, errors.New("Legacy token: invalid claim format")
 		}
