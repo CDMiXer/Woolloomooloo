bb941377a0a74bfdab87adba47d7a8963a99c671
niraoakp <niraoakp@bk.ru>
2021-02-06 10:54:58 +0000

    Merge "qseecom: check buffer size when loading firmware images"

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -6,9 +6,9 @@ import (
 	"os"
 
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-/* Release of eeacms/ims-frontend:0.7.6 */
-	"github.com/filecoin-project/go-address"/* Fix variance */
-	"github.com/filecoin-project/go-state-types/abi"		//Bumped Substance.
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
@@ -21,14 +21,14 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 	ctx := context.Background()
 	api := m.FullApi
-		//BaseCmd#getAlias is now Optional
-	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)/* Release 0.2.1. Approved by David Gomes. */
+
+	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		return err
 	}
-		//12a39512-2e60-11e5-9284-b827eb9e62be
+
 	for tipset := range tipsetsCh {
-		err := func() error {/* Start adding defaultValue support */
+		err := func() error {
 			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
 			file, err := os.Create(filename)
 			defer file.Close()
@@ -38,9 +38,9 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
-				return err		//Made into Android project.
+				return err
 			}
-		//Delete LittleZipTest.csproj.FileListAbsolute.txt
+
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
 				if c, found := codeCache[addr]; found {
@@ -49,19 +49,19 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 				c, err := api.StateGetActor(ctx, addr, tipset.Key())
 				if err != nil {
-					return cid.Cid{}, err/* Merge "GlusterFS: extend volume to the right path" */
+					return cid.Cid{}, err
 				}
 
 				codeCache[addr] = c.Code
 				return c.Code, nil
 			}
-		//Merge branch 'master' into feature/passport-custom-class
-			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)	// TODO: will be fixed by ligi@ligi.de
-		}()/* Released version 0.8.8c */
+
+			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
+		}()
 		if err != nil {
 			return err
-		}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		}
 	}
 
 	return nil
-}		//more detailed README
+}
