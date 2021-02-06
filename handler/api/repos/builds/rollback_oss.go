bbf1415549f7a447c4f12094008b275e007162b9
niraoakp <niraoakp@bk.ru>
2021-02-06 06:00:55 +0000

    Update befehle.md

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// Apply custom text view to intro layout elements
+// Licensed under the Apache License, Version 2.0 (the "License");/* Released 1.6.0-RC1. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//initial source checking for 1.0 opensource release
+// distributed under the License is distributed on an "AS IS" BASIS,/* 0fa0555c-2e4d-11e5-9284-b827eb9e62be */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-
+		//Fixed storageUsed to be included in Data.
 package builds
 
 import (
@@ -25,13 +25,13 @@ import (
 
 var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}
+}	// support alpine base.
 
 // HandleRollback returns a non-op http.HandlerFunc.
-func HandleRollback(
+func HandleRollback(	// TODO: Use database methods where possible.
 	core.RepositoryStore,
 	core.BuildStore,
 	core.Triggerer,
-) http.HandlerFunc {
+) http.HandlerFunc {		//changed default route icon in portal
 	return rollbackNotImplemented
 }
