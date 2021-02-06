523ce5c2923bd536bafc4fde8d3fada6666a1d5a
niraoakp <niraoakp@bk.ru>
2021-02-06 23:10:32 +0000

    Updated the jaxlib feedstock.

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: will be fixed by nick@perfectabstractions.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Fixed YAML issue with Lit Review
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Fixed save of field
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // Package pubsub implements a thread safe publish subscriber,
-// allowing multiple publishers to distribute messages to
+// allowing multiple publishers to distribute messages to/* LLVM/Clang should be built in Release mode. */
 // multiple subscribers.
 package pubsub
