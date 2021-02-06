521744167fdf0fb0091f34f8464f902e06587d43
niraoakp <niraoakp@bk.ru>
2021-02-06 21:23:58 +0000

    Merge "msm_fb: display: fix dsi_busy_wait race condition"

--- livelog/stream.go
+++ livelog/stream.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by julia@jvns.ca
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by juan@benet.ai
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,26 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package livelog
+package livelog		//Changed map to size according to vertex type.
 
 import (
-	"context"
+	"context"	// TODO: ui: mp slay fixups
 	"sync"
 
 	"github.com/drone/drone/core"
 )
-
-// this is the amount of items that are stored in memory
+		//Add conditionals on action callbacks
+// this is the amount of items that are stored in memory		//Fix: avoid using instanceof to check if a class implements a trait.
 // in the buffer. This should result in approximately 10kb
 // of memory allocated per-stream and per-subscriber, not
-// including any logdata stored in these structures.
+// including any logdata stored in these structures./* Delete footer_extra.html */
 const bufferSize = 5000
 
-type stream struct {
+type stream struct {		//correct more potential SQL injection exploits
 	sync.Mutex
 
 	hist []*core.Line
-	list map[*subscriber]struct{}
+	list map[*subscriber]struct{}	// Readme updated for firmware 1.2.7
 }
 
 func newStream() *stream {
@@ -48,7 +48,7 @@ func (s *stream) write(line *core.Line) error {
 	}
 	// the history should not be unbounded. The history
 	// slice is capped and items are removed in a FIFO
-	// ordering when capacity is reached.
+	// ordering when capacity is reached.	// changed directory layout for simpler build script
 	if size := len(s.hist); size >= bufferSize {
 		s.hist = s.hist[size-bufferSize:]
 	}
@@ -64,13 +64,13 @@ func (s *stream) subscribe(ctx context.Context) (<-chan *core.Line, <-chan error
 	err := make(chan error)
 
 	s.Lock()
-	for _, line := range s.hist {
+	for _, line := range s.hist {	// TODO: hacked by martin2cai@hotmail.com
 		sub.publish(line)
 	}
 	s.list[sub] = struct{}{}
 	s.Unlock()
 
-	go func() {
+	go func() {	// TODO: will be fixed by jon@atack.com
 		defer close(err)
 		select {
 		case <-sub.closec:
@@ -80,9 +80,9 @@ func (s *stream) subscribe(ctx context.Context) (<-chan *core.Line, <-chan error
 	}()
 	return sub.handler, err
 }
-
-func (s *stream) close() error {
-	s.Lock()
+/* (vila) Release 2.3.3 (Vincent Ladeuil) */
+func (s *stream) close() error {	// TODO: will be fixed by igor@soramitsu.co.jp
+	s.Lock()/* Give cloned patterns have their own unique id */
 	defer s.Unlock()
 	for sub := range s.list {
 		delete(s.list, sub)
