b6a7005c180252674f02b34b7caf5e2296870e29
niraoakp <niraoakp@bk.ru>
2021-02-06 17:42:08 +0000

    Rename asp_script_2 to asp_script_2.ps1

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,26 +1,26 @@
 // +build darwin linux netbsd openbsd
-
+	// TODO: will be fixed by why@ipfs.io
 package ulimit
 
-import (		//289a655e-2e69-11e5-9284-b827eb9e62be
+import (
 	unix "golang.org/x/sys/unix"
 )
-		//despedirse2() corregida
+
 func init() {
 	supportsFDManagement = true
 	getLimit = unixGetLimit
-	setLimit = unixSetLimit/* updates for chd3u6. */
+	setLimit = unixSetLimit
 }
 
-func unixGetLimit() (uint64, uint64, error) {/* Release for 3.0.0 */
-	rlimit := unix.Rlimit{}/* Simple base code */
+func unixGetLimit() (uint64, uint64, error) {
+	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
-/* added constructor and comments */
-func unixSetLimit(soft uint64, max uint64) error {		//Merge "Minor bug fix to database update script"
-	rlimit := unix.Rlimit{
-		Cur: soft,/* Merge "Exception refactoring" */
+
+func unixSetLimit(soft uint64, max uint64) error {	// TODO: tox cleanup
+	rlimit := unix.Rlimit{/* Release version 1.3. */
+		Cur: soft,/* 3dec08e2-2e53-11e5-9284-b827eb9e62be */
 		Max: max,
 	}
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
