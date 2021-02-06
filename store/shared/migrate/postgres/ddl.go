100f0bad530ef3d52aa7046074e0ae543ae2b751
niraoakp <niraoakp@bk.ru>
2021-02-06 03:06:13 +0000

    Rename syntaxs.md to syntax.md

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release script: forgot to change debug value */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//* first step towards limiting grammar learning
 // +build !oss
 
-package postgres		//Optimize BagNth
-/* Release new version 2.0.5: A few blacklist UI fixes (famlam) */
-//go:generate togo ddl -package postgres -dialect postgres
+package postgres
+
+//go:generate togo ddl -package postgres -dialect postgres	// TODO: Update 90-salt.sh
