61923f9e0f25ed011fe7f66a23112ed8f1838e79
niraoakp <niraoakp@bk.ru>
2021-02-06 22:46:57 +0000

    ede32154-327f-11e5-920a-9cf387a8033e

--- pkg/codegen/python/importer.go
+++ pkg/codegen/python/importer.go
@@ -1,16 +1,16 @@
-// Copyright 2016-2020, Pulumi Corporation./* simplified Savon::Resolver */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* 3.12.2 Release */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* SCMReleaser -> ActionTreeBuilder */
-.esneciL eht rednu snoitatimil //
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package python
 
@@ -21,15 +21,15 @@ import (
 )
 
 // Compatibility mode for Kubernetes 2.0 SDK
-const kubernetes20 = "kubernetes20"/* Changed "*" to "@a" to work better. */
+const kubernetes20 = "kubernetes20"
 
 // PropertyInfo tracks Python-specific information associated with properties in a package.
 type PropertyInfo struct {
 	MapCase bool `json:"mapCase,omitempty"`
 }
-	// TODO: hacked by mowrain@yandex.com
-// PackageInfo tracks Python-specific information associated with a package.	// TODO: fd596502-2e4f-11e5-9284-b827eb9e62be
-type PackageInfo struct {	// width is configurable
+
+// PackageInfo tracks Python-specific information associated with a package.
+type PackageInfo struct {
 	Requires map[string]string `json:"requires,omitempty"`
 	// Readme contains the text for the package's README.md files.
 	Readme string `json:"readme,omitempty"`
@@ -40,31 +40,31 @@ type PackageInfo struct {	// width is configurable
 	ModuleNameOverrides map[string]string `json:"moduleNameOverrides,omitempty"`
 	// Toggle compatibility mode for a specified target.
 	Compatibility string `json:"compatibility,omitempty"`
-	// Deprecated: This bool is no longer needed since all providers now use input/output classes./* Added Release Sprint: OOD links */
+	// Deprecated: This bool is no longer needed since all providers now use input/output classes.
 	UsesIOClasses bool `json:"usesIOClasses,omitempty"`
 	// Indicates whether the pulumiplugin.json file should be generated.
 	EmitPulumiPluginFile bool `json:"emitPulumiPluginFile,omitempty"`
 }
 
-// Importer implements schema.Language for Python.		//increase text-indent
-var Importer schema.Language = importer(0)/* Upgrade version number to 3.1.5 Release Candidate 2 */
+// Importer implements schema.Language for Python.
+var Importer schema.Language = importer(0)
 
 type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
-	return raw, nil		//a20438f8-35ca-11e5-bda5-6c40088e03e4
+	return raw, nil
 }
 
-// ImportPropertySpec decodes language-specific metadata associated with a Property./* Credit h0ng10 properly */
+// ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
 	var info PropertyInfo
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {	// TODO: removed the controller from the app.js for eventPage
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {
 		return nil, err
 	}
 	return info, nil
-}/* configure.ac : Release 0.1.8. */
-/* Release: fix project/version extract */
+}
+
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
