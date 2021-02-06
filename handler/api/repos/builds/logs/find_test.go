a5c769d29fd9f2f06229869db6af1d302923e72f
niraoakp <niraoakp@bk.ru>
2021-02-06 06:50:47 +0000

    Updated README with new repo name in Travis badge

--- handler/api/repos/builds/logs/find_test.go
+++ handler/api/repos/builds/logs/find_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package logs	// TODO: will be fixed by fkautz@pseudocode.cc
+package logs/* Remove .git from Release package */
