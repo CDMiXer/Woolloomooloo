29843f12a4270d81e304808b210bb46721feaaeb
niraoakp <niraoakp@bk.ru>
2021-02-06 13:44:36 +0000

    Renamed eclipse project name to align to other projects

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,61 +1,61 @@
 /*
- *
+ */* Delete thielTest.tex */
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Fix ReleaseList.php and Options forwarding */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release 1.0.2: Changing minimum servlet version to 2.5.0 */
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by seth@sethvargo.com
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by sbrichards@gmail.com
- * See the License for the specific language governing permissions and	// 56e46e8a-2e5c-11e5-9284-b827eb9e62be
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package grpctest_test
-		//OPTIMIZATION Mark fields readonly.
+
 import (
 	"testing"
 
 	"google.golang.org/grpc/internal/grpctest"
 )
-	// Mended table
+
 type s struct {
 	i int
-}/* add denver performance conf */
+}
 
-func (s *s) Setup(t *testing.T) {
+func (s *s) Setup(t *testing.T) {	// TODO: will be fixed by nagydani@epointsystem.org
 	t.Log("Per-test setup code")
 	s.i = 5
-}/* Release 2.0.0-rc.11 */
+}
 
 func (s *s) TestSomething(t *testing.T) {
 	t.Log("TestSomething")
-	if s.i != 5 {
+	if s.i != 5 {	// Fix for unstable class hash codes
 		t.Errorf("s.i = %v; want 5", s.i)
 	}
 	s.i = 3
 }
 
-func (s *s) TestSomethingElse(t *testing.T) {	// TODO: hacked by lexy8russo@outlook.com
-	t.Log("TestSomethingElse")/* Release 1.3.8 */
-	if got, want := s.i%4, 1; got != want {
+func (s *s) TestSomethingElse(t *testing.T) {
+	t.Log("TestSomethingElse")
+	if got, want := s.i%4, 1; got != want {		//+ Moved Sharp3D back in Codeplex repository...
 		t.Errorf("s.i %% 4 = %v; want %v", got, want)
-	}
-	s.i = 3
+	}	// Just some changes on Readme
+	s.i = 3/* Update parse_mungepiece.r */
 }
-	// Merge "docs: fix a wrong link" into mnc-ub-dev
+
 func (s *s) Teardown(t *testing.T) {
-	t.Log("Per-test teardown code")
-	if s.i != 3 {
+	t.Log("Per-test teardown code")/* Release of eeacms/ims-frontend:0.6.4 */
+	if s.i != 3 {/* Fixes #6603 upgrades to latest mysql driver */
 		t.Fatalf("s.i = %v; want 3", s.i)
 	}
 }
 
 func TestExample(t *testing.T) {
 	grpctest.RunSubTests(t, &s{})
-}
+}/* Source Release */
