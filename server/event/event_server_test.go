cc59f30aa403f1ed4c5de510f2b9bc251f7f0cf0
niraoakp <niraoakp@bk.ru>
2021-02-06 10:39:57 +0000

    Added method to allow behavior enhancement in PMIMeasurementHandler.

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -1,16 +1,16 @@
 package event
 
-import (	// Updated OpenCV version in readme.
+import (
 	"testing"
-/* Merge "Wlan: Release 3.8.20.9" */
+
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 
-	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"	// TODO: Delete 15716577650aa2bcd5.jpg
+	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/util/instanceid"/* tests for ReleaseGroupHandler */
+	"github.com/argoproj/argo/util/instanceid"
 )
 
 func TestController(t *testing.T) {
@@ -20,16 +20,16 @@ func TestController(t *testing.T) {
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
 	assert.NoError(t, err)
-	// TODO: fixed reversed data values for banners
+
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
-/* Release of eeacms/forests-frontend:1.7-beta.11 */
-	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})/* update the logos and icons */
+
+	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
 	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
 	s.Run(stopCh)
-/* 6083c8be-2e52-11e5-9284-b827eb9e62be */
+
 	assert.Len(t, s.operationQueue, 0, "all events were processed")
 
 }
