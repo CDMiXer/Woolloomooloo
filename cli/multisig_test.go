c84ffe27e479f59e72fbb3e5d3af32df7a3c159c
niraoakp <niraoakp@bk.ru>
2021-02-06 18:13:20 +0000

    Hungry practice and qualifying session keys.

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,12 +1,12 @@
 package cli
 
 import (
-	"context"		//Added adjustable color
-	"os"	// TODO: Fix English grammar mistake
-	"testing"	// 1780f840-2f85-11e5-bcb1-34363bc765d8
-	"time"/* 506fe884-2e6d-11e5-9284-b827eb9e62be */
-		//dynamic loading of video- and audio-decoder
-	clitest "github.com/filecoin-project/lotus/cli/test"
+	"context"/* moved real-time upload option down to bottom of storage related settings */
+	"os"/* 1c3fb610-2e50-11e5-9284-b827eb9e62be */
+	"testing"
+	"time"
+
+	clitest "github.com/filecoin-project/lotus/cli/test"		//upper case in Entered By
 )
 
 // TestMultisig does a basic test to exercise the multisig CLI
@@ -14,7 +14,7 @@ import (
 func TestMultisig(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-
+/* Add version tests */
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
