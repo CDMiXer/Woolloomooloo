7fc4edc7307d54e8d0fa5db4c9e233166c90313a
niraoakp <niraoakp@bk.ru>
2021-02-06 15:14:50 +0000

    Update kontena.yml reference (#674)

--- core/syncer.go
+++ core/syncer.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Ignore RubyMine project files
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,4 +19,4 @@ import "context"
 // Syncer synchronizes the account repository list.
 type Syncer interface {
 	Sync(context.Context, *User) (*Batch, error)
-}
+}	// Update python version
