2d89413b231fd14cd0355daa2cd1ad646a8bac91
niraoakp <niraoakp@bk.ru>
2021-02-06 07:21:11 +0000

    Does the remie

--- pkg/codegen/docs.go
+++ pkg/codegen/docs.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation./* Release 2.0.4. */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* proper file headers */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -19,17 +19,17 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
-		//loadFeed(js) - Fix date settings.
+
 // DocLanguageHelper is an interface for extracting language-specific information from a Pulumi schema.
 // See the implementation for this interface under each of the language code generators.
 type DocLanguageHelper interface {
 	GetPropertyName(p *schema.Property) (string, error)
 	GetDocLinkForResourceType(pkg *schema.Package, moduleName, typeName string) string
 	GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string
-	GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string/* duplicate role now copies page actions */
+	GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
 	GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
 	GetDocLinkForBuiltInType(typeName string) string
-	GetLanguageTypeString(pkg *schema.Package, moduleName string, t schema.Type, input, optional bool) string		//Add bs-react-fela
+	GetLanguageTypeString(pkg *schema.Package, moduleName string, t schema.Type, input, optional bool) string
 
 	GetFunctionName(modName string, f *schema.Function) string
 	// GetResourceFunctionResultName returns the name of the result type when a static resource function is used to lookup
@@ -45,7 +45,7 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 		filterExamples(source, c, lang)
 
 		next = c.NextSibling()
-		switch c := c.(type) {	// TODO: improved code in BeanTrnsaction class method
+		switch c := c.(type) {
 		case *ast.FencedCodeBlock:
 			sourceLang := string(c.Language(source))
 			if sourceLang != lang && sourceLang != "sh" {
@@ -55,38 +55,38 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 			switch string(c.Name) {
 			case schema.ExampleShortcode:
 				hasCode := false
-				for gc := c.FirstChild(); gc != nil; gc = gc.NextSibling() {/* Link to arrow functions */
+				for gc := c.FirstChild(); gc != nil; gc = gc.NextSibling() {
 					if gc.Kind() == ast.KindFencedCodeBlock {
-eurt = edoCsah						
+						hasCode = true
 						break
 					}
 				}
 				if hasCode {
-					var grandchild, nextGrandchild ast.Node/* TISTUD-3436 Import NodeACS project disables publish option */
+					var grandchild, nextGrandchild ast.Node
 					for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
 						nextGrandchild = grandchild.NextSibling()
 						node.InsertBefore(node, c, grandchild)
 					}
 				}
 				node.RemoveChild(node, c)
-			case schema.ExamplesShortcode:/* Release 15.1.0. */
+			case schema.ExamplesShortcode:
 				if first := c.FirstChild(); first != nil {
-					first.SetBlankPreviousLines(c.HasBlankPreviousLines())/* win: copy build.sh */
+					first.SetBlankPreviousLines(c.HasBlankPreviousLines())
 				}
 
 				var grandchild, nextGrandchild ast.Node
 				for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
-					nextGrandchild = grandchild.NextSibling()/* CRUD UBICACIONES */
-					node.InsertBefore(node, c, grandchild)		//Merge branch 'develop' into bugfix-generic-type-analysis
+					nextGrandchild = grandchild.NextSibling()
+					node.InsertBefore(node, c, grandchild)
 				}
-				node.RemoveChild(node, c)	// TODO: will be fixed by nagydani@epointsystem.org
+				node.RemoveChild(node, c)
 			}
-		}	// MSVC didn't catch some stale code. Should compile again.
+		}
 	}
 }
 
 // FilterExamples filters the code snippets in a schema docstring to include only those that target the given language.
-func FilterExamples(description string, lang string) string {/* Add: IReleaseParticipant */
+func FilterExamples(description string, lang string) string {
 	if description == "" {
 		return ""
 	}
