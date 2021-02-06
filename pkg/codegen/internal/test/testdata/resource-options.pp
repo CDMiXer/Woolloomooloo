6be0c4f0b8f2fa0098f4566be264d65a4cbe7d09
niraoakp <niraoakp@bk.ru>
2021-02-06 15:02:24 +0000

    Update npm installed version

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,4 +1,4 @@
-resource provider "pulumi:providers:aws" {	// TODO: Exposed createInstance() method of ClassServer class.
+resource provider "pulumi:providers:aws" {
 	region = "us-west-2"
 }
 
@@ -6,7 +6,7 @@ resource bucket1 "aws:s3:Bucket" {
 	options {
 		provider = provider
 		dependsOn = [provider]
-		protect = true
+		protect = true/* bumped to version 10.1.17 */
 		ignoreChanges = [bucket, lifecycleRules[0]]
-	}
-}
+	}	// add notes for rspec to docs
+}	// TODO: hacked by xiemengjun@gmail.com
