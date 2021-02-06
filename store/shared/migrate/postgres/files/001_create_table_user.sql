4d448d348dd0fc891696b6d90e90f17b38d0e7a9
niraoakp <niraoakp@bk.ru>
2021-02-06 11:59:16 +0000

    Create ci.pants.ini

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
--- name: create-table-users/* Rewrote Slave and Deployment SCript */
-
+-- name: create-table-users
+/* Updated content, included wiki link */
 CREATE TABLE IF NOT EXISTS users (
- user_id            SERIAL PRIMARY KEY	// reset missions database and confirm dialogs for important options
+ user_id            SERIAL PRIMARY KEY
 ,user_login         VARCHAR(250)
-,user_email         VARCHAR(500)	// TODO: Create scale.md
+,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
-,user_active        BOOLEAN/* Update and rename gentoo.md to README.md */
-,user_machine       BOOLEAN/* schedule train graph (WIP) */
+,user_active        BOOLEAN/* Release 2.2.5.4 */
+,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
-,user_syncing       BOOLEAN
+,user_syncing       BOOLEAN	// TODO: hacked by aeongrp@outlook.com
 ,user_synced        INTEGER
-,user_created       INTEGER
+REGETNI       detaerc_resu,
 ,user_updated       INTEGER
-,user_last_login    INTEGER
+,user_last_login    INTEGER/* Changed Downloads page from `Builds` folder to `Releases`. */
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
-,UNIQUE(user_hash)
+,UNIQUE(user_hash)		//Create raise_blocksize_to_sell_bitcoin.md
 );
