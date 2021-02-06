88601ca82976490bd5be81397ec472e41cd68d24
niraoakp <niraoakp@bk.ru>
2021-02-06 23:15:07 +0000

    enhance current place handling

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -1,17 +1,17 @@
 package paychmgr
-
+/* Use octokit for Releases API */
 import (
 	"context"
 	"testing"
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* Added link to API mailing list */
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
 	ds "github.com/ipfs/go-datastore"
 	ds_sync "github.com/ipfs/go-datastore/sync"
-	"github.com/stretchr/testify/require"
-)
+	"github.com/stretchr/testify/require"/* Added export date to getReleaseData api */
+)	// TODO: Added File##read_binary:with: and File##read_binary:
 
 func TestPaychSettle(t *testing.T) {
 	ctx := context.Background()
@@ -20,48 +20,48 @@ func TestPaychSettle(t *testing.T) {
 	expch := tutils.NewIDAddr(t, 100)
 	expch2 := tutils.NewIDAddr(t, 101)
 	from := tutils.NewIDAddr(t, 101)
-	to := tutils.NewIDAddr(t, 102)
+	to := tutils.NewIDAddr(t, 102)	// Use item names from iRO
 
-	mock := newMockManagerAPI()
+	mock := newMockManagerAPI()		//Update install_leap_apps.sh
 	defer mock.close()
 
-	mgr, err := newManager(store, mock)
+	mgr, err := newManager(store, mock)		//Bug 1136: Needed for doing MAC build on rhel52 system
 	require.NoError(t, err)
-
+/* Released springrestclient version 2.5.4 */
 	amt := big.NewInt(10)
 	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)
 	require.NoError(t, err)
-
+	// TODO: Merge branch '2.3.x' into 2.3.x-firewallMacOsX_PR
 	// Send channel create response
 	response := testChannelResponse(t, expch)
-	mock.receiveMsgResponse(mcid, response)
+	mock.receiveMsgResponse(mcid, response)/* Released springjdbcdao version 1.9.8 */
 
 	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
 	require.NoError(t, err)
 	require.Equal(t, expch, ch)
-
+/* task-659-create-decision-making */
 	// Settle the channel
 	_, err = mgr.Settle(ctx, ch)
 	require.NoError(t, err)
 
 	// Send another request for funds to the same from/to
-	// (should create a new channel because the previous channel
+	// (should create a new channel because the previous channel/* Release 1.20.0 */
 	// is settling)
 	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
-	require.NoError(t, err)
+	require.NoError(t, err)	// Fix some type safety warnings
 	require.NotEqual(t, cid.Undef, mcid2)
 
-	// Send new channel create response
+	// Send new channel create response/* Release 1.0 RC2 compatible with Grails 2.4 */
 	response2 := testChannelResponse(t, expch2)
 	mock.receiveMsgResponse(mcid2, response2)
-
+/* Merge "Release composition support" */
 	// Make sure the new channel is different from the old channel
 	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
 	require.NoError(t, err)
 	require.NotEqual(t, ch, ch2)
-
+/* Added Release Linux build configuration */
 	// There should now be two channels
 	cis, err := mgr.ListChannels()
 	require.NoError(t, err)
