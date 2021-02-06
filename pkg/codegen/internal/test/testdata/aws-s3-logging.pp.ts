de5cd79a70fdca26de1a49404229e77c9a7ba680
niraoakp <niraoakp@bk.ru>
2021-02-06 17:06:02 +0000

    [REM] compute_reduction validation

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-		//few MySQL Optimizations to char-server
+
 const logs = new aws.s3.Bucket("logs", {});
-const bucket = new aws.s3.Bucket("bucket", {loggings: [{/* Added warning about the tests' effect on rabbitmq */
+const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
-}]});/* Merge "Node group handling improved in the db module" */
+}]});
 export const targetBucket = bucket.loggings[0].targetBucket;
