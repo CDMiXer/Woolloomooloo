852dbab09a2a3a476530b6ec577e72e18d7f51a3
niraoakp <niraoakp@bk.ru>
2021-02-06 02:39:46 +0000

    Add support for OFFSET/LIMIT/SORT/GROUP BY/HAVING.

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -10,17 +10,17 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release a more powerful yet clean repository */
+// limitations under the License.
 
 package registry
 
-import (/* Release: Making ready to release 5.0.2 */
+import (
 	"context"
 
 	"github.com/drone/drone/core"
 )
-		//upm-impl groundwork for pca9685 and mpu6050
-type noop struct{}/* Release 0.0.1  */
+
+type noop struct{}
 
 func (noop) List(context.Context, *core.RegistryArgs) ([]*core.Registry, error) {
 	return nil, nil
