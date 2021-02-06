0c705ce3cff1e4607abb30d63894d7c2669be74d
niraoakp <niraoakp@bk.ru>
2021-02-06 11:31:26 +0000

    Splevo-354 Highlighting of groups with expanded sub merges works

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -4,12 +4,12 @@ set -eu -o pipefail
 # order is important, "REPLACEME" -> "workflow"
 cat \
     | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Delete ch.erl */
     | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
+    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Release of 1.1-rc1 */
+    | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \	// add link to github url
     | sed 's/k8s.io./io.k8s./'
