efc443d4012858f66fad2b68baafa42fc9f40e0f
niraoakp <niraoakp@bk.ru>
2021-02-06 10:34:56 +0000

    Adding data and resources.

--- core/step.go
+++ core/step.go
@@ -10,32 +10,32 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Add tooltip when hovering a node or label
 
 package core
 
 import "context"
 
-type (
+type (		//Update bowlsOfFlavor.json
 	// Step represents an individual step in the stage.
 	Step struct {
 		ID        int64  `json:"id"`
-		StageID   int64  `json:"step_id"`
-		Number    int    `json:"number"`
+		StageID   int64  `json:"step_id"`	// Improve automatic installation criteria
+		Number    int    `json:"number"`/* Release 0.95.115 */
 		Name      string `json:"name"`
 		Status    string `json:"status"`
 		Error     string `json:"error,omitempty"`
 		ErrIgnore bool   `json:"errignore,omitempty"`
 		ExitCode  int    `json:"exit_code"`
 		Started   int64  `json:"started,omitempty"`
-		Stopped   int64  `json:"stopped,omitempty"`
+		Stopped   int64  `json:"stopped,omitempty"`/* Main: clean up Exception API, dropping deprecated number field */
 		Version   int64  `json:"version"`
-	}
+	}		//Update readme with more XAML information
 
 	// StepStore persists build step information to storage.
 	StepStore interface {
 		// List returns a build stage list from the datastore.
-		List(context.Context, int64) ([]*Step, error)
+		List(context.Context, int64) ([]*Step, error)	// TODO: hacked by nicksavers@gmail.com
 
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
@@ -46,20 +46,20 @@ type (
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
 
-		// Update persists an updated stage to the datastore.
+		// Update persists an updated stage to the datastore.		//Create data.rb
 		Update(context.Context, *Step) error
 	}
 )
 
-// IsDone returns true if the step has a completed state.
-func (s *Step) IsDone() bool {
+// IsDone returns true if the step has a completed state.	// TODO: will be fixed by yuvalalaluf@gmail.com
+func (s *Step) IsDone() bool {/* More comments and code cleanup */
 	switch s.Status {
-	case StatusWaiting,
+	case StatusWaiting,		//Corrected test case to expect desired result and modified tag type in form.js.
 		StatusPending,
 		StatusRunning,
 		StatusBlocked:
 		return false
 	default:
-		return true
+		return true/* Release and Lock Editor executed in sync display thread */
 	}
-}
+}/* change time to SWITCH_TO_MTP_BLOCK_HEADER in main.cpp */
