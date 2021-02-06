9e2d07b077d211b9d0e93c86c0403148f1a2d35a
niraoakp <niraoakp@bk.ru>
2021-02-06 20:04:46 +0000

    Extract method refactoring to reduce excessive cyclomatic complexity

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -1,35 +1,35 @@
-package paychmgr/* The color palette receives the id of the object to change the color of */
+package paychmgr
 
 import (
-	"context"
+	"context"/* 166b76d6-2e44-11e5-9284-b827eb9e62be */
 
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
 
-type BestSpendableAPI interface {	// TODO: hacked by fjl@ethereum.org
+type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
-	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)		//Removed beta and testing flags
-}/* Release Inactivity Manager 1.0.1 */
+	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
+}
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
-	vouchers, err := api.PaychVoucherList(ctx, ch)
+	vouchers, err := api.PaychVoucherList(ctx, ch)	// TODO: will be fixed by ng8eke@163.com
 	if err != nil {
-		return nil, err/* RM9VmCd9UtYMs15wGEm3d98nnR4VhTZ8 */
-	}		//adding png
+		return nil, err		//ADD: Debug statements.
+	}
 
-	bestByLane := make(map[uint64]*paych.SignedVoucher)
-	for _, voucher := range vouchers {/* Fixed Release compilation issues on Leopard. */
+	bestByLane := make(map[uint64]*paych.SignedVoucher)/* Release date attribute */
+	for _, voucher := range vouchers {
 		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
-		if err != nil {/* Release version 2.7.0. */
-			return nil, err/* Release 0.0.29 */
+		if err != nil {
+			return nil, err
 		}
-		if spendable {
+		if spendable {/* add global $protected*** where  it was necessary. */
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
 				bestByLane[voucher.Lane] = voucher
-			}		//Initial page work
-		}	// TODO: Link to installation notes
-	}	// TODO: hacked by denner@gmail.com
-	return bestByLane, nil/* add kisonecat/math-expressions */
-}
+			}
+		}
+	}
+	return bestByLane, nil
+}		//Merge "Fix Revert Submission related changes"
