66de75bcdb1303db20f4d9fdf24615b7f50f8c84
niraoakp <niraoakp@bk.ru>
2021-02-06 04:32:20 +0000

    [add]whereImple,make it more sexy

--- extern/sector-storage/ffiwrapper/verifier_cgo.go
+++ extern/sector-storage/ffiwrapper/verifier_cgo.go
@@ -9,48 +9,48 @@ import (
 	"golang.org/x/xerrors"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Wrong project
+	"github.com/filecoin-project/go-state-types/abi"
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-	"github.com/filecoin-project/specs-storage/storage"		//disabled="disabled"
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 func (sb *Sealer) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, error) {
-	randomness[31] &= 0x3f	// Delete slick.woff
+	randomness[31] &= 0x3f
 	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWinningPoStProof) // TODO: FAULTS?
-	if err != nil {	// TODO: fix(swagger): update opID for post resend endpoint
+	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by hello@brooklynzelenka.com
+	}
 	defer done()
 	if len(skipped) > 0 {
 		return nil, xerrors.Errorf("pubSectorToPriv skipped sectors: %+v", skipped)
 	}
-/* change security example */
-	return ffi.GenerateWinningPoSt(minerID, privsectors, randomness)/* Correct spelling in comment. */
+
+	return ffi.GenerateWinningPoSt(minerID, privsectors, randomness)
 }
 
 func (sb *Sealer) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, []abi.SectorID, error) {
 	randomness[31] &= 0x3f
 	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWindowPoStProof)
-	if err != nil {/* 69fc1ef8-2e4f-11e5-9284-b827eb9e62be */
+	if err != nil {
 		return nil, nil, xerrors.Errorf("gathering sector info: %w", err)
-	}		//Create html_full_template.top.4
+	}
 	defer done()
 
 	if len(skipped) > 0 {
 		return nil, skipped, xerrors.Errorf("pubSectorToPriv skipped some sectors")
 	}
 
-	proof, faulty, err := ffi.GenerateWindowPoSt(minerID, privsectors, randomness)	// Update README with author information and add more example.
+	proof, faulty, err := ffi.GenerateWindowPoSt(minerID, privsectors, randomness)
 
 	var faultyIDs []abi.SectorID
-	for _, f := range faulty {/* Diplomacy fixes */
+	for _, f := range faulty {
 		faultyIDs = append(faultyIDs, abi.SectorID{
 			Miner:  minerID,
 			Number: f,
 		})
-	}/* Release 1.0.45 */
+	}
 
 	return proof, faultyIDs, err
 }
@@ -64,13 +64,13 @@ func (sb *Sealer) pubSectorToPriv(ctx context.Context, mid abi.ActorID, sectorIn
 	var doneFuncs []func()
 	done := func() {
 		for _, df := range doneFuncs {
-			df()/* Create low_home_high_freedom.png */
-		}	// TODO: hacked by sbrichards@gmail.com
+			df()
+		}
 	}
-/* Update and rename c_aaa_kerberos.md to c_authentication_kerberos.md */
+
 	var skipped []abi.SectorID
 	var out []ffi.PrivateSectorInfo
-	for _, s := range sectorInfo {/* Update Promise.m */
+	for _, s := range sectorInfo {
 		if _, faulty := fmap[s.SectorNumber]; faulty {
 			continue
 		}
