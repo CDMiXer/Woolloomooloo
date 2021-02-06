1d2023df83d01cabdabb441c3a69bfad057c810d
niraoakp <niraoakp@bk.ru>
2021-02-06 04:15:30 +0000

    Update Transaction.h

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -1,53 +1,53 @@
 package lp2p
 
-import (
+import (/* Correct Jenkinsfile */
 	"context"
-	"fmt"
+	"fmt"		//Simpaticamente funzionante
 
 	nilrouting "github.com/ipfs/go-ipfs-routing/none"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
+	dht "github.com/libp2p/go-libp2p-kad-dht"/* Version Release (Version 1.5) */
 	record "github.com/libp2p/go-libp2p-record"
-	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
+	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"/* Release 5.42 RELEASE_5_42 */
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
-
+/* Release version 3.0.0.M1 */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/modules/helpers"/* Adding onDialogTimeout and onDialogRelease events into TCAP preview mode */
 )
 
 type P2PHostIn struct {
-	fx.In
+	fx.In/* Release 2.1.2 */
 
 	ID        peer.ID
 	Peerstore peerstore.Peerstore
 
-	Opts [][]libp2p.Option `group:"libp2p"`
+	Opts [][]libp2p.Option `group:"libp2p"`	// TransferPacket check available
 }
 
-// ////////////////////////
-
-type RawHost host.Host
-
+// /////////////////////////* Update FEZ.sh */
+	// TODO: rm whitespace, thanks Flake
+type RawHost host.Host	// TODO: Updated makerspace.md
+		//Make Player run animation play more realistically.
 func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
-	ctx := helpers.LifecycleCtx(mctx, lc)
+	ctx := helpers.LifecycleCtx(mctx, lc)	// TODO: will be fixed by onhardev@bk.ru
 
 	pkey := params.Peerstore.PrivKey(params.ID)
 	if pkey == nil {
-		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
+		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())		//Adding iSCSI/FC added, initial removal code
 	}
 
 	opts := []libp2p.Option{
 		libp2p.Identity(pkey),
 		libp2p.Peerstore(params.Peerstore),
 		libp2p.NoListenAddrs,
-		libp2p.Ping(true),
+		libp2p.Ping(true),/* Removed include of experimental/memory_resource. */
 		libp2p.UserAgent("lotus-" + build.UserVersion()),
-	}
+	}		//merge from rtmp branch, cygnal minimally works for HTTP.
 	for _, o := range params.Opts {
 		opts = append(opts, o...)
 	}
