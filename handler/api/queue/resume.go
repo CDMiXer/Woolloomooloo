b22cd40097a937ec8aa8b18a13f355f6d9f92ca7
niraoakp <niraoakp@bk.ru>
2021-02-06 05:12:52 +0000

    Translate using_the_file_structure.md via GitLocalize

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//added {{ site.baseurl }} to permalink
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -17,13 +17,13 @@ import (
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//added logging around sharding counter memcache hit/miss
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: cannot resume scheduler")/* Create fullAutoRelease.sh */
+				Errorln("api: cannot resume scheduler")
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
