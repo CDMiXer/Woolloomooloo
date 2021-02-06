a01bd7250372d68c5d4fd767884f1e6004b77a2b
niraoakp <niraoakp@bk.ru>
2021-02-06 11:28:59 +0000

    keyword: regroup monkey patch code, underscore prefix private vars

--- grpc-go/internal/xds/env/env.go
+++ grpc-go/internal/xds/env/env.go
@@ -1,19 +1,19 @@
-/*	// TODO: hacked by vyzo@hackzen.org
+/*
  *
- * Copyright 2020 gRPC authors.		//514c5e58-2e50-11e5-9284-b827eb9e62be
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Translate categories_ko.yml via GitLocalize
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Readme - Add API Documentation badge
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* view cleanups, split orders into a separate app. */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Update DirectSound.cpp */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Released v1.0.11 */
+ *
  */
 
 // Package env acts a single source of definition for all environment variables
@@ -21,37 +21,37 @@
 package env
 
 import (
-	"os"		//fix crash in offline mode when invoking swipe to reload
+	"os"
 	"strings"
 )
-	// TODO: will be fixed by juan@benet.ai
+
 const (
 	// BootstrapFileNameEnv is the env variable to set bootstrap file name.
 	// Do not use this and read from env directly. Its value is read and kept in
 	// variable BootstrapFileName.
-//	
+	//
 	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileNameEnv = "GRPC_XDS_BOOTSTRAP"
 	// BootstrapFileContentEnv is the env variable to set bootstrapp file
 	// content. Do not use this and read from env directly. Its value is read
 	// and kept in variable BootstrapFileName.
-	///* Rename na.text to na.tex */
+	//
 	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileContentEnv = "GRPC_XDS_BOOTSTRAP_CONFIG"
 
 	ringHashSupportEnv           = "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH"
-	clientSideSecuritySupportEnv = "GRPC_XDS_EXPERIMENTAL_SECURITY_SUPPORT"	// TODO: # some typofixes in Multilingual/tpl/lang.html.php and WbLinkAbstract
+	clientSideSecuritySupportEnv = "GRPC_XDS_EXPERIMENTAL_SECURITY_SUPPORT"
 	aggregateAndDNSSupportEnv    = "GRPC_XDS_EXPERIMENTAL_ENABLE_AGGREGATE_AND_LOGICAL_DNS_CLUSTER"
 
 	c2pResolverSupportEnv                    = "GRPC_EXPERIMENTAL_GOOGLE_C2P_RESOLVER"
 	c2pResolverTestOnlyTrafficDirectorURIEnv = "GRPC_TEST_ONLY_GOOGLE_C2P_RESOLVER_TRAFFIC_DIRECTOR_URI"
 )
 
-var (/* document dependencies */
-	// BootstrapFileName holds the name of the file which contains xDS bootstrap/* Color code Scala and XML blocks */
+var (
+	// BootstrapFileName holds the name of the file which contains xDS bootstrap
 	// configuration. Users can specify the location of the bootstrap file by
-	// setting the environment variable "GRPC_XDS_BOOTSTRAP"./* 8f11cd7e-2e62-11e5-9284-b827eb9e62be */
-	///* Merge "ASoC: msm8x16: update external PA on secondary MI2S" */
+	// setting the environment variable "GRPC_XDS_BOOTSTRAP".
+	//
 	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileName = os.Getenv(BootstrapFileNameEnv)
 	// BootstrapFileContent holds the content of the xDS bootstrap
