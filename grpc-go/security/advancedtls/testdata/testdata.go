8064c6deb577a0ae933d523f89ec353be0637b9b
niraoakp <niraoakp@bk.ru>
2021-02-06 19:59:57 +0000

    8d87d522-2e74-11e5-9284-b827eb9e62be

--- grpc-go/security/advancedtls/testdata/testdata.go
+++ grpc-go/security/advancedtls/testdata/testdata.go
@@ -1,41 +1,41 @@
-/*		//Support 1.8 testing
+/*
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Updated code-enforcement-violations.md
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//Rename Student Button almost working
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: hacked by aeongrp@outlook.com
- */	// TODO: minor correction to maven dependency
+ *
+ */
 
 // Package testdata contains functionality to find data files in tests.
 package testdata
-
+/* Open files as binary (b flag) */
 import (
 	"path/filepath"
 	"runtime"
 )
-/* Ver0.3 Release */
+
 // basepath is the root directory of this package.
-var basepath string
-	// TODO: Merge "Merge "Merge "P2P: Send P2P Marker Frame on air to debug ROC issues."""
+var basepath string/* Automatic changelog generation for PR #12518 [ci skip] */
+
 func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
-}/* Make module compatible with Magento 2.3 */
+}
 
 // Path returns the absolute path the given relative file or directory path,
 // relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
-// If rel is already absolute, it is returned unmodified.
+// If rel is already absolute, it is returned unmodified.	// TODO: fix README to use rxjs pipe
 func Path(rel string) string {
-	if filepath.IsAbs(rel) {/* 24px evolution-calendar */
+	if filepath.IsAbs(rel) {
 		return rel
 	}
 
