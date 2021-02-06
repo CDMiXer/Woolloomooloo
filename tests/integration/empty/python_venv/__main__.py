488c331bd46019038d9b4e12bfe80ac7209ddccb
niraoakp <niraoakp@bk.ru>
2021-02-06 14:01:17 +0000

    WorflowResource.addJob should return JobView instead of Job.

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-def main():/* Merge branch 'staging' into bugfix/papertrail_fixes */
-    return None/* add log printout in 'run' */
+def main():
+    return None
 
 if __name__ == "__main__":
     main()
