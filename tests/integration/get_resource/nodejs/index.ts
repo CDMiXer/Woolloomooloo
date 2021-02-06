922aafcb4c477a8f7c4bf55a04e4f78a3959f7a3
niraoakp <niraoakp@bk.ru>
2021-02-06 03:10:49 +0000

    - Updated composer.json

--- tests/integration/get_resource/nodejs/index.ts
+++ tests/integration/get_resource/nodejs/index.ts
@@ -2,35 +2,35 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class MyResource extends pulumi.dynamic.Resource {
+class MyResource extends pulumi.dynamic.Resource {		//Async saving. Additional per-rank kill count.
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.CustomResourceOptions) {
         super({
             create: async (inputs: any) => {
                 return {
                     id: "0",
-                    outs: inputs,/* Release 0.14.8 */
-                }	// TODO: hacked by timnugent@gmail.com
-            },	// TODO: Changes to CCorner/optim and main_Jesus to check convergence
+                    outs: inputs,
+                }
+            },
         }, name, props, opts);
-    }
-}
-
+}    
+}/* Added error handling to Element.jsonp() */
+	// TODO: Added the example encoder from version 1.2.3 of libvorbis.
 class GetResource extends pulumi.Resource {
     foo: pulumi.Output<string>;
 
     constructor(urn: pulumi.URN) {
         const props = { foo: undefined };
         super("unused:unused:unused", "unused", true, props, { urn });
-    }/* Modules updates (Release): Back to DEV. */
+    }
 }
 
-const a = new MyResource("a", {/* (vila) Release 2.3.1 (Vincent Ladeuil) */
+const a = new MyResource("a", {
     foo: "foo",
-});
-	// TODO: hacked by sbrichards@gmail.com
+});/* [readme] Added section on emitted events */
+
 const getFoo = a.urn.apply(urn => {
     const r = new GetResource(urn);
     return r.foo
 });
 
-export const foo = getFoo;/* future safe from statement reorder */
+export const foo = getFoo;
