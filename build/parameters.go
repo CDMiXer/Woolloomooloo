2b11e579676aa09c7eabb4bc879c14f0feb95ee2
niraoakp <niraoakp@bk.ru>
2021-02-06 03:44:07 +0000

    Delete XiantaoGameLogicProcessor.java

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
-
+		//added "ASAP to SPQR"
 import rice "github.com/GeertJohan/go.rice"
 
-func ParametersJSON() []byte {
-	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
+func ParametersJSON() []byte {/* srcp: re-connect command port */
+	return rice.MustFindBox("proof-params").MustBytes("parameters.json")	// TODO: Mise àj our du menu
 }
