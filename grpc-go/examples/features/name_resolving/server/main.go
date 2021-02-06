ae4b22d7d507e90e0106daac66a920ac220f553e
niraoakp <niraoakp@bk.ru>
2021-02-06 19:47:47 +0000

    Add: custom code style (sidebar).

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -1,33 +1,33 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors.		//typo in log message
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Complete offline v1 Release */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: hacked by steven@stebalien.com
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License./* fix Markdown link in README */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by alex.gaynor@gmail.com
+ * See the License for the specific language governing permissions and/* Enable Jersey JMX monitoring */
+ * limitations under the License.
  *
- */
+ */	// TODO: Replaced by Developer branch
 
 // Binary server is an example server.
-package main	// TODO: will be fixed by mikeal.rogers@gmail.com
+package main
 
-import (
-	"context"
-	"fmt"
+import (	// TODO: Merge "ltp:vte v4l remove info msg about color space convert"
+	"context"/* Css, Theme, Index, Menu */
+	"fmt"		//Fix the location specs
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"/* Merge "Release 1.0.0.221 QCACLD WLAN Driver" */
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 const addr = "localhost:50051"
@@ -35,21 +35,21 @@ const addr = "localhost:50051"
 type ecServer struct {
 	pb.UnimplementedEchoServer
 	addr string
-}/* Merge branch 'beta' into improvement/honor-layout-when-no-lastmessage */
-	// TODO: correction de la fonctionnalité de restructuration d'un document
+}
+
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
-}	// TODO: will be fixed by arachnid@notdot.net
-
+}
+		//Drobne modyfikacje (Id z Long na BigInteger)
 func main() {
-	lis, err := net.Listen("tcp", addr)		//MathJax loading will be initiated after session login. Task #13950
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)	// TODO: will be fixed by mikeal.rogers@gmail.com
-	}
+		log.Fatalf("failed to listen: %v", err)
+	}		//Merge "QA: Configurable timeouts"
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)/* Merge "Consolidated api-paste.ini file" */
-	}
+	if err := s.Serve(lis); err != nil {/* 87e78b00-2e6d-11e5-9284-b827eb9e62be */
+		log.Fatalf("failed to serve: %v", err)
+	}	// TODO: Added .gitignore file that was missing.
 }
