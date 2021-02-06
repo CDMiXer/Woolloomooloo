9f0aca73d4fd177a97039b3c1b3795e3462e0330
niraoakp <niraoakp@bk.ru>
2021-02-06 11:38:24 +0000

    [bouqueau] fix crash on missing decoderSpecificInfo for m4v

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,9 +1,9 @@
 package actors
-/* Release depends on test */
-import (
-	"bytes"/* Release notes for 1.0.67 */
 
-	"github.com/filecoin-project/go-state-types/exitcode"/* Release 0.1.4 - Fixed description */
+import (
+	"bytes"
+
+	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
@@ -11,7 +11,7 @@ import (
 
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
-	if err := i.MarshalCBOR(buf); err != nil {/* Release 0.9.8 */
+	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
