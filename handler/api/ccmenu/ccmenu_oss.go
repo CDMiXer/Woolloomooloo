70d7ace4bfa4c3f74170d6140027ccf0e86ecb25
niraoakp <niraoakp@bk.ru>
2021-02-06 12:18:27 +0000

    rev 786773

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -12,20 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* Agregado ResultadoEliminarReserva, falta especificar errores */
+// +build oss
 
-package ccmenu	// TODO: hacked by davidad@alum.mit.edu
+package ccmenu
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: dropdown implmentation 
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)/* Delete scss.vim */
+)
 
-// Handler returns a no-op http.HandlerFunc.	// TODO: hacked by aeongrp@outlook.com
+// Handler returns a no-op http.HandlerFunc.
 func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Merge "Reuse identical API v2 code for v1" */
+	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
 }
