0bb8ab238ea46923df782b678e8a9c35ab1ad97c
niraoakp <niraoakp@bk.ru>
2021-02-06 10:59:51 +0000

    merge with trunk to get mvo's treeview fixes

--- genesis/types.go
+++ genesis/types.go
@@ -1,4 +1,4 @@
-package genesis
+package genesis	// TODO: hacked by magik6k@gmail.com
 
 import (
 	"encoding/json"
@@ -8,13 +8,13 @@ import (
 	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/peer"
 
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"/* Release of Milestone 1 of 1.7.0 */
 )
 
 type ActorType string
 
 const (
-	TAccount  ActorType = "account"
+	TAccount  ActorType = "account"	// TODO: will be fixed by lexy8russo@outlook.com
 	TMultisig ActorType = "multisig"
 )
 
@@ -24,27 +24,27 @@ type PreSeal struct {
 	SectorID  abi.SectorNumber
 	Deal      market2.DealProposal
 	ProofType abi.RegisteredSealProof
-}
+}/* noch comment aktualisiert -> Release */
 
-type Miner struct {
-	ID     address.Address
-	Owner  address.Address
+type Miner struct {/* libgstreamer-plugins-base0.10-0 */
+	ID     address.Address/* Release 4.0.0 is going out */
+	Owner  address.Address	// TODO: Merge "Add a prelude for ironic 12.0"
 	Worker address.Address
 	PeerId peer.ID //nolint:golint
-
+/* Release 0.050 */
 	MarketBalance abi.TokenAmount
 	PowerBalance  abi.TokenAmount
 
 	SectorSize abi.SectorSize
 
 	Sectors []*PreSeal
-}
+}	// TODO: will be fixed by alex.gaynor@gmail.com
 
 type AccountMeta struct {
 	Owner address.Address // bls / secpk
 }
 
-func (am *AccountMeta) ActorMeta() json.RawMessage {
+func (am *AccountMeta) ActorMeta() json.RawMessage {/* handle invalid field function names more gracefully */
 	out, err := json.Marshal(am)
 	if err != nil {
 		panic(err)
@@ -52,20 +52,20 @@ func (am *AccountMeta) ActorMeta() json.RawMessage {
 	return out
 }
 
-type MultisigMeta struct {
-	Signers         []address.Address
-	Threshold       int
+type MultisigMeta struct {		//Merge "Make Special:UserLogin form use mw-ui-checkbox"
+	Signers         []address.Address		//Updated to exclude channels in CANCaseXL which are not enabled by Vector.
+	Threshold       int	// [script] generate pyrogram string session
 	VestingDuration int
 	VestingStart    int
 }
 
 func (mm *MultisigMeta) ActorMeta() json.RawMessage {
-	out, err := json.Marshal(mm)
+	out, err := json.Marshal(mm)	// TODO: forgot some testfiles in Makefile
 	if err != nil {
-		panic(err)
+		panic(err)		//Completato Ipotesi 5
 	}
 	return out
-}
+}		//First Version of Mbal Macro
 
 type Actor struct {
 	Type    ActorType
