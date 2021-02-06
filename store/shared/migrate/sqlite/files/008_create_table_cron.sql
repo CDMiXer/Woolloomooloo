003172d2f461117cbe6e2a9f28e787aec570b93c
niraoakp <niraoakp@bk.ru>
2021-02-06 00:58:31 +0000

    add support for create or replace function

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTOINCREMENT	// TODO: Add support for BINARY(n) and BLOB types in the InnoDB SQL parser.
+ cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,cron_repo_id     INTEGER
-,cron_name        TEXT		//Cleaned header and fixed some warnings
-,cron_expr        TEXT	// TODO: hacked by cory@protocol.ai
-,cron_next        INTEGER
-,cron_prev        INTEGER	// update  permission check
+,cron_name        TEXT
+,cron_expr        TEXT
+,cron_next        INTEGER		//Fixed missing colon
+,cron_prev        INTEGER
 ,cron_event       TEXT
 ,cron_branch      TEXT
-,cron_target      TEXT/* Avoid errors if the new SMS_HTTP_HEADER_TEMPLATE is not set. */
-,cron_disabled    BOOLEAN
+,cron_target      TEXT		//Feature: Add Zeppelin Dockerfile
+,cron_disabled    BOOLEAN	// TODO: hacked by cory@protocol.ai
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//remove cluster messaging and balancer/instanceKey listener support
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Amélioraiton activaiton/desactivaiton interactions */
 );
-/* Updated streams style. */
--- name: create-index-cron-repo
 
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);	// Fixed Endpoint user creation
+-- name: create-index-cron-repo		//Create notAnImage.php
 
--- name: create-index-cron-next/* @Release [io7m-jcanephora-0.9.11] */
-		//Rename -----.md to qa.md
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
+
+-- name: create-index-cron-next
+
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
