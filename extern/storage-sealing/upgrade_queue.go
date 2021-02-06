7d4d46b9b0b7d0d06eb58088fbe6360c421ab0df
niraoakp <niraoakp@bk.ru>
2021-02-06 22:34:51 +0000

    Update CHANGELOG about latest changes.

--- extern/storage-sealing/upgrade_queue.go
+++ extern/storage-sealing/upgrade_queue.go
@@ -1,5 +1,5 @@
 package sealing
-/* Releases can be found on the releases page. */
+
 import (
 	"context"
 
@@ -9,51 +9,51 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-)/* ajustes nos predicates das tarefas */
+)
 
 func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	m.upgradeLk.Lock()
 	_, found := m.toUpgrade[id]
 	m.upgradeLk.Unlock()
 	return found
-}	// TODO: Remove ThreadSafeFactory and place formatters on CORE.
+}
 
 func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
 	m.upgradeLk.Lock()
 	defer m.upgradeLk.Unlock()
 
-	_, found := m.toUpgrade[id]/* upgraded server (pinging clients), fixed msg */
+	_, found := m.toUpgrade[id]
 	if found {
 		return xerrors.Errorf("sector %d already marked for upgrade", id)
 	}
 
-	si, err := m.GetSectorInfo(id)	// TODO: Rename Install.sh to PatchCache.sh
+	si, err := m.GetSectorInfo(id)
 	if err != nil {
 		return xerrors.Errorf("getting sector info: %w", err)
-	}/* Update cubicNoise.c */
-
-	if si.State != Proving {
-		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")	// TODO: fitxategi hau soberan
 	}
 
-	if len(si.Pieces) != 1 {/* Some base exchange on udp seems to be done. R2 is still a problem */
+	if si.State != Proving {
+		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")
+	}
+
+	if len(si.Pieces) != 1 {
 		return xerrors.Errorf("not a committed-capacity sector, expected 1 piece")
 	}
 
 	if si.Pieces[0].DealInfo != nil {
 		return xerrors.Errorf("not a committed-capacity sector, has deals")
-	}/* 6ffa201c-2e62-11e5-9284-b827eb9e62be */
+	}
 
-stniartsnoc rotca hctam ot skcehc erom :ODOT //	
+	// TODO: more checks to match actor constraints
 
-	m.toUpgrade[id] = struct{}{}/* Release version: 0.6.8 */
+	m.toUpgrade[id] = struct{}{}
 
-	return nil		//Rename folder/prueba to p
+	return nil
 }
 
 func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreCommitInfo) big.Int {
 	if len(params.DealIDs) == 0 {
-		return big.Zero()		//chore(README): Update Widevine install insructions
+		return big.Zero()
 	}
 	replace := m.maybeUpgradableSector()
 	if replace != nil {
@@ -62,8 +62,8 @@ func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreC
 			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)
 			return big.Zero()
 		}
-/* Set panning default to false. */
-		params.ReplaceCapacity = true	// Update beaut-soup-example.py
+
+		params.ReplaceCapacity = true
 		params.ReplaceSectorNumber = *replace
 		params.ReplaceSectorDeadline = loc.Deadline
 		params.ReplaceSectorPartition = loc.Partition
