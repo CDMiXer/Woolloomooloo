1f7e1a520bd1fa6fd410f70707b1d809b65ca2bc
niraoakp <niraoakp@bk.ru>
2021-02-06 19:55:34 +0000

    Server: Users not needed right now.

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Pass --no-daemon to gradle build invocation
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-	// Update plotSolarElev.py
+
 // Now, simply update the resource; this should work fine:
 let a = new Resource("eternal", { state: 2 }, { protect: true });
