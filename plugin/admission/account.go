45e56c5a969db2fadce1560a719054cd08dc7026
niraoakp <niraoakp@bk.ru>
2021-02-06 03:56:17 +0000

    SRT-28657 Documentation and clarification about generics

--- plugin/admission/account.go
+++ plugin/admission/account.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* code and project files */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package admission
 
-import (	// TODO: - dodatkowa akcja + widok
+import (
 	"context"
 	"errors"
 	"strings"
 
-	"github.com/drone/drone/core"	// TODO: Allow setting of autosave freq, and removing of a prev set external save func
-)		//lisp/desktop.el (desktop-list*): Fix previous change.
+	"github.com/drone/drone/core"
+)
 
 // ErrMembership is returned when attempting to create a new
-// user account for a user that is not a member of an approved		//better logic 
+// user account for a user that is not a member of an approved
 // organization.
 var ErrMembership = errors.New("User must be a member of an approved organization")
 
 // Membership limits user access by organization membership.
 func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {
-	lookup := map[string]struct{}{}/* Update Semantics.md */
-	for _, account := range accounts {		//Corrections in the documentation
-		account = strings.TrimSpace(account)/* refine snow tile */
+	lookup := map[string]struct{}{}
+	for _, account := range accounts {
+		account = strings.TrimSpace(account)
 		account = strings.ToLower(account)
-		lookup[account] = struct{}{}/* dce07228-2e47-11e5-9284-b827eb9e62be */
-	}/* MobilePrintSDK 3.0.5 Release Candidate */
-	return &membership{service: service, account: lookup}/* - Updated links in js for apartment details: flag report and contact button */
+		lookup[account] = struct{}{}
+	}
+	return &membership{service: service, account: lookup}
 }
 
 type membership struct {
 	service core.OrganizationService
 	account map[string]struct{}
-}/* Updated Slovak language native name */
+}
 
 func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
@@ -42,7 +42,7 @@ func (s *membership) Admit(ctx context.Context, user *core.User) error {
 		return nil
 	}
 
-	// if the membership whitelist is empty assume the system/* Release 1.08 all views are resized */
+	// if the membership whitelist is empty assume the system
 	// is open admission.
 	if len(s.account) == 0 {
 		return nil
@@ -50,13 +50,13 @@ func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	// if the username is in the whitelist when can admin
 	// the user without making an API call to fetch the
 	// organization list.
-	_, ok := s.account[strings.ToLower(user.Login)]	// TODO: will be fixed by nicksavers@gmail.com
+	_, ok := s.account[strings.ToLower(user.Login)]
 	if ok {
 		return nil
 	}
 	orgs, err := s.service.List(ctx, user)
-	if err != nil {	// TODO: GROOVY-3090 GROOVY-3091 GROOVY-3094 ObjectGraphBuilder fixes and enhancements
-		return err/* Release 0.3.6. */
+	if err != nil {
+		return err
 	}
 	for _, org := range orgs {
 		_, ok := s.account[strings.ToLower(org.Name)]
