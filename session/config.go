47b3cf7c4e070bf0b53aacd27049f6f97f2ccd22
niraoakp <niraoakp@bk.ru>
2021-02-06 10:42:31 +0000

    Update plotting.md

--- session/config.go
+++ session/config.go
@@ -7,28 +7,28 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by mail@bitpshr.net
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Delete square_solution.cpp
 // limitations under the License.
 
 package session
 
-import "time"
+import "time"		//Fixed mouse movement and limiting.
 
 // Config provides the session configuration.
-type Config struct {
+type Config struct {	// TODO: Re-enabling more tests and bug fixes
 	Secure      bool
-	Secret      string
+	Secret      string/* [artifactory-release] Release version 1.0.0-M1 */
 	Timeout     time.Duration
 	MappingFile string
 }
 
 // NewConfig returns a new session configuration.
 func NewConfig(secret string, timeout time.Duration, secure bool) Config {
-	return Config{
+	return Config{	// TODO: test domain deeper
 		Secure:  secure,
 		Secret:  secret,
-		Timeout: timeout,
+		Timeout: timeout,	// Updated abstracts
 	}
 }
