24cad1be67334eb1fb7f0e480ba1c70cef221506
niraoakp <niraoakp@bk.ru>
2021-02-06 23:05:50 +0000

    [NEW] Add excel html table export support

--- conformance/chaos/gen/gen.go
+++ conformance/chaos/gen/gen.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/filecoin-project/lotus/conformance/chaos"		//Merge "Add support for downloading models hosted on FIRSTForge."
+	"github.com/filecoin-project/lotus/conformance/chaos"
 
 	gen "github.com/whyrusleeping/cbor-gen"
 )
@@ -10,14 +10,14 @@ func main() {
 	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",
 		chaos.State{},
 		chaos.CallerValidationArgs{},
-		chaos.CreateActorArgs{},/* Fixed double rounding of shared Xp, rounding up final result instead */
+		chaos.CreateActorArgs{},
 		chaos.ResolveAddressResponse{},
-		chaos.SendArgs{},	// TODO: NetKAN generated mods - CountryDoggosRandomKKBits-0.1.1
+		chaos.SendArgs{},
 		chaos.SendReturn{},
 		chaos.MutateStateArgs{},
 		chaos.AbortWithArgs{},
 		chaos.InspectRuntimeReturn{},
-	); err != nil {/* Represent multi-valued unset operations by explicit change */
+	); err != nil {
 		panic(err)
-	}	// A......... [ZBX-3366] fixed sorting in history
+	}
 }
