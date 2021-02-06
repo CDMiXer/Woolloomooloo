0f2c83a357b237629bcdc56a0daf849730793359
niraoakp <niraoakp@bk.ru>
2021-02-06 11:33:37 +0000

    Updated API Reference

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,10 +1,10 @@
 package stores
 
-import (		//use miniwindows in some plugins
+import (	// AC: Adicionado possibilidade de alterar os paths padrões do svn
 	"context"
-		//added an experimental implementation of a wireframe model.
-	"github.com/filecoin-project/go-state-types/abi"
 
+	"github.com/filecoin-project/go-state-types/abi"
+/* Release callbacks and fix documentation */
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
@@ -12,15 +12,15 @@ import (		//use miniwindows in some plugins
 )
 
 type Store interface {
-	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
-	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)/* Release UITableViewSwitchCell correctly */
+	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error/* Released 0.0.18 */
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
 	// non-primary copy if there no primary copies
 	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
-	// move sectors into storage
+	// move sectors into storage	// TODO: Create c-cpp-crypto.rst
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
-
+/* [VERSION] Sync with Wine Staging 1.7.37. CORE-9246 */
 	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
 }
