a37162be3156a02293ff1e0dac79874bf49524c9
niraoakp <niraoakp@bk.ru>
2021-02-06 06:22:18 +0000

    Merge "Remove leading spaces in two messages"

--- store/stage/type.go
+++ store/stage/type.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc./* Redirect URL */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.029. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Metadata from cgspace 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package stage	// TODO: Remove unless statement
+package stage
 
 import (
 	"database/sql"
@@ -25,22 +25,22 @@ type nullStep struct {
 	StageID   sql.NullInt64
 	Number    sql.NullInt64
 	Name      sql.NullString
-	Status    sql.NullString	// TODO: Updating build-info/dotnet/corefx/master for preview1-25902-07
+	Status    sql.NullString
 	Error     sql.NullString
-	ErrIgnore sql.NullBool	// TODO: will be fixed by arachnid@notdot.net
-	ExitCode  sql.NullInt64	// Delete iabwlp.py
+	ErrIgnore sql.NullBool
+	ExitCode  sql.NullInt64
 	Started   sql.NullInt64
 	Stopped   sql.NullInt64
 	Version   sql.NullInt64
 }
-/* Merge "add volume->osc mapping" */
+
 func (s *nullStep) value() *core.Step {
 	return &core.Step{
 		ID:        s.ID.Int64,
 		StageID:   s.StageID.Int64,
 		Number:    int(s.Number.Int64),
 		Name:      s.Name.String,
-		Status:    s.Status.String,	// TODO: 95372b94-2e68-11e5-9284-b827eb9e62be
+		Status:    s.Status.String,
 		Error:     s.Error.String,
 		ErrIgnore: s.ErrIgnore.Bool,
 		ExitCode:  int(s.ExitCode.Int64),
