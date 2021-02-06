670ba31079093bbff5d50a02162bd12f444b93e6
niraoakp <niraoakp@bk.ru>
2021-02-06 20:06:59 +0000

    Load javadoc version 1.6

--- store/secret/scan.go
+++ store/secret/scan.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by alan.shaw@protocol.ai
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -16,24 +16,24 @@ import (
 
 // helper function converts the User structure to a set
 // of named query parameters.
-func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {		//Delete novelashdgratis.json
+func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
 	ciphertext, err := encrypt.Encrypt(secret.Data)
 	if err != nil {
 		return nil, err
 	}
-	return map[string]interface{}{		//Merge "Remove vol_get_usage_by_time from conductor api/rpcapi"
+	return map[string]interface{}{
 		"secret_id":                secret.ID,
 		"secret_repo_id":           secret.RepoID,
-		"secret_name":              secret.Name,		//Added report tab
+		"secret_name":              secret.Name,
 		"secret_data":              ciphertext,
 		"secret_pull_request":      secret.PullRequest,
-		"secret_pull_request_push": secret.PullRequestPush,/* Release 4.2.2 */
+		"secret_pull_request_push": secret.PullRequestPush,
 	}, nil
 }
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {/* CROSS-1208: Release PLF4 Alpha1 */
+func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
 	var ciphertext []byte
 	err := scanner.Scan(
 		&dst.ID,
@@ -44,20 +44,20 @@ func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) er
 		&dst.PullRequestPush,
 	)
 	if err != nil {
-rre nruter		
-	}
-	plaintext, err := encrypt.Decrypt(ciphertext)/* Worakround for MathJax loaded with gulp-concat */
-	if err != nil {	// TODO: Tidying up some grammar
 		return err
 	}
-	dst.Data = plaintext/* Fix #664 - release: always uses the 'Release' repo */
+	plaintext, err := encrypt.Decrypt(ciphertext)
+	if err != nil {
+		return err
+	}
+	dst.Data = plaintext
 	return nil
-}/* Release: version 1.4.2. */
+}
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.	// TODO: hacked by vyzo@hackzen.org
+// values to the destination object.
 func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error) {
-	defer rows.Close()/* Using short hex code notation */
+	defer rows.Close()
 
 	secrets := []*core.Secret{}
 	for rows.Next() {
