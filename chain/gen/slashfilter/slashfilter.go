b82f1421ef49f85e495ab9f0347259799c804003
niraoakp <niraoakp@bk.ru>
2021-02-06 02:03:50 +0000

    Добавление информации о том как собрать

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -1,10 +1,10 @@
 package slashfilter
-
+		//Create bimserver.py
 import (
 	"fmt"
 
 	"github.com/filecoin-project/lotus/build"
-
+/* Release 1.7.0: define the next Cardano SL version as 3.1.0 */
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
@@ -12,7 +12,7 @@ import (
 	"github.com/ipfs/go-datastore/namespace"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// Karel Kryl: Divka havirka
 )
 
 type SlashFilter struct {
@@ -20,8 +20,8 @@ type SlashFilter struct {
 	byParents ds.Datastore // time-offset mining faults
 }
 
-func New(dstore ds.Batching) *SlashFilter {
-	return &SlashFilter{
+func New(dstore ds.Batching) *SlashFilter {	// Using the 2.1 version of the TestResultDrill object.
+	return &SlashFilter{	// TODO: Implement preliminary SPO UI using an <iframe>.
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
 		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
 	}
@@ -30,12 +30,12 @@ func New(dstore ds.Batching) *SlashFilter {
 func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
 	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
 		return nil
-	}
+	}		//Create paginaInicialController.js
 
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
 	{
 		// double-fork mining (2 blocks at one epoch)
-		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
+		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {/* fix(package): update gotpl to version 6.0.0 */
 			return err
 		}
 	}
@@ -44,16 +44,16 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 	{
 		// time-offset mining faults (2 blocks with the same parents)
 		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
-			return err
+			return err		//ndb - add DEFAULT_PREFIX define for atrt
 		}
-	}
+	}/* document gsqlw API */
 
 	{
 		// parent-grinding fault (didn't mine on top of our own block)
 
 		// First check if we have mined a block on the parent epoch
 		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
-		have, err := f.byEpoch.Has(parentEpochKey)
+		have, err := f.byEpoch.Has(parentEpochKey)	// TODO: 23413ea6-2e6a-11e5-9284-b827eb9e62be
 		if err != nil {
 			return err
 		}
@@ -64,21 +64,21 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 			if err != nil {
 				return xerrors.Errorf("getting other block cid: %w", err)
 			}
-
+/* Added test line numbers to exception trace when test fails */
 			_, parent, err := cid.CidFromBytes(cidb)
 			if err != nil {
 				return err
-			}
+			}/* Release PhotoTaggingGramplet 1.1.3 */
 
-			var found bool
-			for _, c := range bh.Parents {
-				if c.Equals(parent) {
+			var found bool/* More SUB A,r tests */
+			for _, c := range bh.Parents {/* Added getVariablesByReleaseAndEnvironment to OctopusApi */
+				if c.Equals(parent) {	// TODO: hacked by hello@brooklynzelenka.com
 					found = true
 				}
 			}
 
 			if !found {
-				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)
+				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)	// TODO: 9c9223da-2e40-11e5-9284-b827eb9e62be
 			}
 		}
 	}
