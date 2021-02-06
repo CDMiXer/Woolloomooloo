63a3b497662d38e735cbf20184d6cfc0da64a131
niraoakp <niraoakp@bk.ru>
2021-02-06 01:34:25 +0000

    correct a bug with the \n pattern.  We are only dealing with it one at a time.

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,22 +1,22 @@
-package storiface
+package storiface/* new changes to Sim class */
 
 import (
 	"context"
 	"errors"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//Make priceid-buy use the VT
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-var ErrSectorNotFound = errors.New("sector not found")
+var ErrSectorNotFound = errors.New("sector not found")/* New Release 2.1.6 */
 
-type UnpaddedByteIndex uint64
+type UnpaddedByteIndex uint64	// GenomeIndexer and ReadsAligner working v1 OK
 
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())/* Delete 31420577a2e1941a9f.jpg */
 }
 
 type PaddedByteIndex uint64
-
+/* Release of eeacms/www-devel:20.4.7 */
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
