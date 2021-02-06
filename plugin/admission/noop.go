6f7370010a891977ce3925bab9220b73e158ef8d
niraoakp <niraoakp@bk.ru>
2021-02-06 04:00:58 +0000

    Still fixing URL formatting & adding in problems

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Incremental checkin -- add setter tests.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,12 +13,12 @@
 // limitations under the License.
 
 package admission
-
+	// Start parsing packets
 import (
-	"context"
+	"context"/* Proper logging. */
 
 	"github.com/drone/drone/core"
-)
+)/* [ Release ] V0.0.8 */
 
 // noop is a stub admission controller.
 type noop struct{}
