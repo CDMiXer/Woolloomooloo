abd30a1b00e57042bf5c659b3734e69fe41bad11
niraoakp <niraoakp@bk.ru>
2021-02-06 20:16:12 +0000

    Added instructions for use.

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* Thermal units */
+// Copyright 2019 Drone IO, Inc./* fix tv name */
+///* Rework the Render module, no longer deprecated (Max liked it) */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added support for custom messages */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* [artifactory-release] Release version 3.2.7.RELEASE */
-//      http://www.apache.org/licenses/LICENSE-2.0		//a9e08d7d-2d3e-11e5-8011-c82a142b6f9b
+//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release version 0.0.10. */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Tried coding only code length in Huff table, doesn't help :( */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* #50 Fixed JMX Bean name */
-/* Released version 0.8.3b */
+// +build oss		//Fixed constructor for MultipleLookupInput
+
 package metric
-		//fix "nlikes" var
+
 import "github.com/drone/drone/core"
 
 func BuildCount(core.BuildStore)        {}
-func PendingBuildCount(core.BuildStore) {}
-func RunningBuildCount(core.BuildStore) {}
-func RunningJobCount(core.StageStore)   {}/* Release cJSON 1.7.11 */
-func PendingJobCount(core.StageStore)   {}
+func PendingBuildCount(core.BuildStore) {}/* Point to correct cargo-central. */
+func RunningBuildCount(core.BuildStore) {}	// [SPRINT 1] Added Doctrine 2 connection
+func RunningJobCount(core.StageStore)   {}
+func PendingJobCount(core.StageStore)   {}/* Release version 0.9.7 */
 func RepoCount(core.RepositoryStore)    {}
 func UserCount(core.UserStore)          {}
