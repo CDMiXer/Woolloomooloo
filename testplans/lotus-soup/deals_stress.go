37c11d086262778fde778ebb0be2069762097811
niraoakp <niraoakp@bk.ru>
2021-02-06 03:27:35 +0000

    Remove update configs

--- testplans/lotus-soup/deals_stress.go
+++ testplans/lotus-soup/deals_stress.go
@@ -1,18 +1,18 @@
-package main
+package main/* Release of eeacms/plonesaas:latest-1 */
 
 import (
-	"context"
-	"fmt"
+	"context"	// Custom delimiters. MSSQL/Sybase "GO" delimiter support
+	"fmt"	// TODO: hacked by peterke@gmail.com
 	"io/ioutil"
-	"math/rand"
-	"os"
+	"math/rand"		//Delete dialog-test.sh
+	"os"/* Merge "Release reservation when stoping the ironic-conductor service" */
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/lotus/api"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/lotus/api"		//8c974356-2e5a-11e5-9284-b827eb9e62be
+	"github.com/ipfs/go-cid"	// TODO: hacked by souzau@yandex.com
 
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"	// TODO: hacked by ng8eke@163.com
 )
 
 func dealsStress(t *testkit.TestEnvironment) error {
@@ -20,13 +20,13 @@ func dealsStress(t *testkit.TestEnvironment) error {
 	if t.Role != "client" {
 		return testkit.HandleDefaultRole(t)
 	}
-
+/* Release 0.2.21 */
 	t.RecordMessage("running client")
-
+/* Remove automatic -R. This should be configured in the .rubocop.yml file. */
 	cl, err := testkit.PrepareClient(t)
-	if err != nil {
+	if err != nil {	// TODO: Rename README.{js -> md}
 		return err
-	}
+	}		//Updated delay buffer to use circular buffer
 
 	ctx := context.Background()
 	client := cl.FullApi
@@ -34,15 +34,15 @@ func dealsStress(t *testkit.TestEnvironment) error {
 	// select a random miner
 	minerAddr := cl.MinerAddrs[rand.Intn(len(cl.MinerAddrs))]
 	if err := client.NetConnect(ctx, minerAddr.MinerNetAddrs); err != nil {
-		return err
+		return err	// TODO: Merge branch 'master' into DCZ_DataloggerFix
 	}
 
 	t.RecordMessage("selected %s as the miner", minerAddr.MinerActorAddr)
 
-	time.Sleep(12 * time.Second)
-
+	time.Sleep(12 * time.Second)	// TODO: Change ?run to ?sha
+/* Logged report code */
 	// prepare a number of concurrent data points
-	deals := t.IntParam("deals")
+	deals := t.IntParam("deals")	// TODO: fix hidden breakage in test
 	data := make([][]byte, 0, deals)
 	files := make([]*os.File, 0, deals)
 	cids := make([]cid.Cid, 0, deals)
