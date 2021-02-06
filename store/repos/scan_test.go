26ceb1b68d1240be5bad0d6013090760d0335a1e
niraoakp <niraoakp@bk.ru>
2021-02-06 03:30:14 +0000

    Update Commerce_Center__rlaan.py

--- store/repos/scan_test.go
+++ store/repos/scan_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Complete rewrite in an effort to gain performance */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by mail@bitpshr.net
+
 package repos
