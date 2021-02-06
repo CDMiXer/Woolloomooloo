4d4d9bcbd80476aeb9f2e1fe0ce4c88cae21dec4
niraoakp <niraoakp@bk.ru>
2021-02-06 19:31:03 +0000

    expression tables and images tabs working - simplified code

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,42 +1,42 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: hacked by yuvalalaluf@gmail.com
-//		//Using local time in all calendar functions.
+// Copyright 2016-2018, Pulumi Corporation.
+///* Reorganized the source files to be more consistent between Core and Controls */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* 01851928-2e55-11e5-9284-b827eb9e62be */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release 0.4.24 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.		//commander 0.4.x is back for release
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* SB-671: testUpdateMetadataOnDeleteReleaseVersionDirectory fixed */
+// See the License for the specific language governing permissions and	// readme file has been added
+.esneciL eht rednu snoitatimil //
 
-package stack/* 0a9ccd6a-2e57-11e5-9284-b827eb9e62be */
-
+package stack
+	// rspec added to Gemfile
 import (
-	"io/ioutil"		//Update 3_receipttypes.markdown
+	"io/ioutil"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* diff view in local changes works */
 )
 
 func TestLoadV0Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
 	assert.NoError(t, err)
 
-	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
-	assert.NoError(t, err)		//222f0f62-2e64-11e5-9284-b827eb9e62be
+	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)	// TODO: Internal commit
+	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)
+	assert.Len(t, chk.Latest.Resources, 30)/* "wow" is "uau", "ah" is "ah". Yeesh */
 }
 
 func TestLoadV1Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
 	assert.NoError(t, err)
-
+/* DCC-24 skeleton code for Release Service  */
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
-	assert.NotNil(t, chk.Latest)	// TODO: will be fixed by brosner@gmail.com
-	assert.Len(t, chk.Latest.Resources, 30)/* Clarify DB reset [ci skip] */
-}
+	assert.NotNil(t, chk.Latest)
+	assert.Len(t, chk.Latest.Resources, 30)
+}/* Added support for new constructor of ProxyConfiguration */
