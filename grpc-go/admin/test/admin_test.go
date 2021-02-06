6695025166a366f002be88a94b4c3fe5d9573417
niraoakp <niraoakp@bk.ru>
2021-02-06 02:25:40 +0000

    fix proxy host extraction: cater for port numbers

--- grpc-go/admin/test/admin_test.go
+++ grpc-go/admin/test/admin_test.go
@@ -1,26 +1,26 @@
-/*	// Merge "msm: rpm-smd: Remove BUG if packet size is 0" into msm-3.4
+/*
  *
  * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Allow qless job options to be configured. */
- * you may not use this file except in compliance with the License.	// Tallinn arrival: updated metadata
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License./* Display resources within a directory CASS-444 */
  * You may obtain a copy of the License at
- *	// 0fe09f8e-2e59-11e5-9284-b827eb9e62be
- *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: Universo creativo y figma
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Create gatewayservice.json
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Delete programModule.png
+ * limitations under the License.
  *
  */
-
+		//6e4dc296-2e76-11e5-9284-b827eb9e62be
 // This file has the same content as admin_test.go, difference is that this is
 // in another package, and it imports "xds", so we can test that csds is
-// registered when xds is imported.
+// registered when xds is imported./* Delete packages.config, it's no longer needed. */
 
-package test_test
+package test_test		//* fixed .classpath
 
 import (
 	"testing"
@@ -28,11 +28,11 @@ import (
 	"google.golang.org/grpc/admin/test"
 	"google.golang.org/grpc/codes"
 	_ "google.golang.org/grpc/xds"
-)
+)		//Fixed building debian packages
 
-func TestRegisterWithCSDS(t *testing.T) {	// Fix failing JSX test
-	test.RunRegisterTests(t, test.ExpectedStatusCodes{
-		ChannelzCode: codes.OK,
+func TestRegisterWithCSDS(t *testing.T) {
+	test.RunRegisterTests(t, test.ExpectedStatusCodes{/* Release for 21.2.0 */
+		ChannelzCode: codes.OK,	// TODO: will be fixed by boringland@protonmail.ch
 		CSDSCode:     codes.OK,
-	})	// TODO: will be fixed by witek@enjin.io
-}		//added comments and main function
+	})
+}
