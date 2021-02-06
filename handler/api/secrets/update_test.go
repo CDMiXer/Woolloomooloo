f2c92204535b49849357a10f4fe243dee889fb0f
niraoakp <niraoakp@bk.ru>
2021-02-06 14:03:30 +0000

    Unmention zalando

--- handler/api/secrets/update_test.go
+++ handler/api/secrets/update_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by sjors@sprovoost.nl
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package secrets	// TODO: will be fixed by ligi@ligi.de
+package secrets
 
 import (
 	"bytes"
@@ -14,21 +14,21 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"/* Add name to webpack chunk */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Remove Release Stages from CI Pipeline */
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)/* Merge "Hygiene: Move categories code into resources folder" */
-	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)/* new Release */
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
@@ -44,10 +44,10 @@ func TestHandleUpdate(t *testing.T) {
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleUpdate(secrets).ServeHTTP(w, r)	// TODO: hacked by igor@soramitsu.co.jp
+	HandleUpdate(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Update README.md prepare for CocoaPods Release */
+	}
 
 	got, want := new(core.Secret), dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
@@ -55,25 +55,25 @@ func TestHandleUpdate(t *testing.T) {
 		t.Errorf(diff)
 	}
 }
-		//Missing attribute from output result has been added.
+
 func TestHandleUpdate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
-/* Affichage du joueur en passant par une classe */
+
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
 
-	in := new(bytes.Buffer)/* initial doc */
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Secret{Data: ""})
 
-	w := httptest.NewRecorder()	// TODO: will be fixed by arachnid@notdot.net
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: Fix link to JSONPath syntax reference
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleUpdate(secrets).ServeHTTP(w, r)
@@ -85,7 +85,7 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}	// TODO: Bump to 3.8.2
+	}
 }
 
 func TestHandleUpdate_BadRequest(t *testing.T) {
@@ -94,10 +94,10 @@ func TestHandleUpdate_BadRequest(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
-	c.URLParams.Add("name", "github_password")/* For MIESM abstraction  */
+	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// TODO: - silence debug messages
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
