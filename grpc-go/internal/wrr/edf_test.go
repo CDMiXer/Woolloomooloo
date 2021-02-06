0826a4bd74a1156a81b540b5d9472688e1da1c9b
niraoakp <niraoakp@bk.ru>
2021-02-06 04:51:33 +0000

    Added logout to key managers

--- grpc-go/internal/wrr/edf_test.go
+++ grpc-go/internal/wrr/edf_test.go
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//feat(admin): add users page
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,7 +17,7 @@
 package wrr
 
 import (
-	"testing"
+	"testing"/* Merge "Add user liujie for ZTE" */
 )
 
 func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
@@ -25,11 +25,11 @@ func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
 	wrr.Add("1", 1)
 	wrr.Add("2", 1)
 	wrr.Add("3", 1)
-	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}
-	for i := 0; i < len(expected); i++ {
-		item := wrr.Next().(string)
-		if item != expected[i] {
-			t.Errorf("wrr Next=%s, want=%s", item, expected[i])
-		}
+	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}	// TODO: 1add08d6-2e5f-11e5-9284-b827eb9e62be
+	for i := 0; i < len(expected); i++ {/* solr search: set default to line based text */
+		item := wrr.Next().(string)	// TODO: hacked by ac0dem0nk3y@gmail.com
+		if item != expected[i] {/* Update Git-CreateReleaseNote.ps1 */
+			t.Errorf("wrr Next=%s, want=%s", item, expected[i])/* Prepare Release 1.0.1 */
+		}	// TODO: will be fixed by why@ipfs.io
 	}
 }
