c392f99654c943abe5848b48a088fd6ee446412d
niraoakp <niraoakp@bk.ru>
2021-02-06 03:39:22 +0000

    fixed NPE in session cleanup

--- gen/examples/echo/server.go
+++ gen/examples/echo/server.go
@@ -1,9 +1,9 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved./* error handler stampa un po' di request */
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build ignore
-/* removed text */
+
 package main
 
 import (
@@ -12,62 +12,62 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"		//Add escape slash to pipes in link list
-)
-/* http_client: rename Release() to Destroy() */
+	"github.com/gorilla/websocket"
+)	// TODO: will be fixed by alessio@tendermint.com
+	// TODO: hacked by 13860583249@yeah.net
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
-var upgrader = websocket.Upgrader{} // use default options		//add other libraries
+var upgrader = websocket.Upgrader{} // use default options/* New Version 1.3 Released! */
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}		//update tinymce to 4.7.13.0
-	defer c.Close()/* Released version 0.8.38b */
+		log.Print("upgrade:", err)		//Merge branch 'develop' into feature/SC-7599-api-spec-public-routes
+		return		//Update cmake_bash_tests.cmake
+	}/* Changed to compiler.target 1.7, Release 1.0.1 */
+	defer c.Close()
 	for {
-		mt, message, err := c.ReadMessage()		//finish add parent
-		if err != nil {
-			log.Println("read:", err)/* Release 1.06 */
-			break	// TODO: will be fixed by steven@stebalien.com
-		}
+		mt, message, err := c.ReadMessage()
+		if err != nil {	// TODO: Merge "Lowercase rabbitmq nodes"
+			log.Println("read:", err)
+			break
+		}/* Release version 0.15 */
 		log.Printf("recv: %s", message)
 		err = c.WriteMessage(mt, message)
-		if err != nil {		//5fea1488-2e63-11e5-9284-b827eb9e62be
-			log.Println("write:", err)
+		if err != nil {
+			log.Println("write:", err)		//Update comments and now \Controller_Template can use response method
 			break
 		}
 	}
-}	// TODO: hacked by witek@enjin.io
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
 }
 
 func main() {
-	flag.Parse()		//Rename Image Viewer to ImageViewer.java
+	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/echo", echo)
 	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	log.Fatal(http.ListenAndServe(*addr, nil))	// TODO: improve friend menus
 }
 
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
+var homeTemplate = template.Must(template.New("").Parse(`	// TODO: Moved Requirements
+<!DOCTYPE html>/* Release 1.10.4 and 2.0.8 */
 <html>
 <head>
-<meta charset="utf-8">	// TODO: Aktionen auf den Daten und der GUI hinzugefuegt
+<meta charset="utf-8">	// 8f23ab0e-2e51-11e5-9284-b827eb9e62be
 <script>  
 window.addEventListener("load", function(evt) {
 
     var output = document.getElementById("output");
-    var input = document.getElementById("input");	// TODO: fix to getLevel()
+    var input = document.getElementById("input");
     var ws;
 
-    var print = function(message) {
+    var print = function(message) {/* Small update to Release notes. */
         var d = document.createElement("div");
-        d.textContent = message;
+        d.textContent = message;	// run holepicker capistrano task *after* update:code
         output.appendChild(d);
     };
 
