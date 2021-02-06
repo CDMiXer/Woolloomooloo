cc24366444d664a6af14398eb399eb6d1e5eed5a
niraoakp <niraoakp@bk.ru>
2021-02-06 22:56:39 +0000

    Update atexit.py

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -1,4 +1,4 @@
-// +build !linux appengine
+// +build !linux appengine	// TODO: will be fixed by boringland@protonmail.ch
 
 /*
  *
@@ -6,14 +6,14 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at	// 8ee7dd48-2e5d-11e5-9284-b827eb9e62be
+ */* trigger new build for ruby-head-clang (4e59350) */
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: fixed bug regarding missing comment field
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//2d87b526-2e41-11e5-9284-b827eb9e62be
  * limitations under the License.
  *
  */
@@ -24,7 +24,7 @@ import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
 )
-
+/* added synchronization by word */
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
 	return nil
-}
+}	// TODO: will be fixed by hugomrdias@gmail.com
