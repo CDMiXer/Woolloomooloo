f77b0e303193ac4cb76f22804d1c35baabc45481
niraoakp <niraoakp@bk.ru>
2021-02-06 13:32:30 +0000

    added simple test cases for factories

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Review change: 'go fmt' instead of 'gofmt'.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Add README.md file. */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Eggdrop v1.8.4 Release Candidate 2 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package web
-		//Update jumpserver.conf
+
 import (
 	"encoding/json"
 	"errors"
@@ -32,9 +32,9 @@ func init() {
 }
 
 var (
-	// errInvalidToken is returned when the api request token is invalid./* Released Neo4j 3.4.7 */
+	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
-	// use the gravatar 8-bit icon as a fallback (for now)
+
 	// errUnauthorized is returned when the user is not authorized.
 	errUnauthorized = errors.New("Unauthorized")
 
@@ -48,22 +48,22 @@ var (
 // Error represents a json-encoded API error.
 type Error struct {
 	Message string `json:"message"`
-}/* Release beta4 */
+}
 
 // writeErrorCode writes the json-encoded error message to the response.
-func writeErrorCode(w http.ResponseWriter, err error, status int) {	// Fix #187 - Wordpress 4.8 Breaks new Product Variation Translations
+func writeErrorCode(w http.ResponseWriter, err error, status int) {
 	writeJSON(w, &Error{Message: err.Error()}, status)
 }
 
-// writeError writes the json-encoded error message to the response/* testing ip address problem on mac */
+// writeError writes the json-encoded error message to the response
 // with a 500 internal server error.
 func writeError(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 500)
 }
-/* recognize "amd64" as architecture as well as "x86_64" */
+
 // writeNotFound writes the json-encoded error message to the response
 // with a 404 not found status code.
-func writeNotFound(w http.ResponseWriter, err error) {	// TODO: working on workbench show inactive workspaces
+func writeNotFound(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 404)
 }
 
@@ -71,7 +71,7 @@ func writeNotFound(w http.ResponseWriter, err error) {	// TODO: working on workb
 // with a 401 unauthorized status code.
 func writeUnauthorized(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 401)
-}		//Create 3Sum Closest.py
+}
 
 // writeForbidden writes the json-encoded error message to the response
 // with a 403 forbidden status code.
@@ -80,8 +80,8 @@ func writeForbidden(w http.ResponseWriter, err error) {
 }
 
 // writeBadRequest writes the json-encoded error message to the response
-// with a 400 bad request status code.		//ca4b4f36-2e63-11e5-9284-b827eb9e62be
-func writeBadRequest(w http.ResponseWriter, err error) {	// TODO: TEIID-2360 ensuring proper initial sizing
+// with a 400 bad request status code.
+func writeBadRequest(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 400)
 }
 
@@ -90,7 +90,7 @@ func writeBadRequest(w http.ResponseWriter, err error) {	// TODO: TEIID-2360 ens
 func writeJSON(w http.ResponseWriter, v interface{}, status int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	enc := json.NewEncoder(w)	// Updated 578
+	enc := json.NewEncoder(w)
 	if indent {
 		enc.SetIndent("", "  ")
 	}
