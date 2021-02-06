a793416976449bba0cbe8859be3583b8d77e03df
niraoakp <niraoakp@bk.ru>
2021-02-06 16:36:22 +0000

    Update decimal places of order

--- grpc-go/xds/internal/balancer/cdsbalancer/logging.go
+++ grpc-go/xds/internal/balancer/cdsbalancer/logging.go
@@ -1,28 +1,28 @@
 /*
- *
+ */* introduce Introductie in domain */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* correction indent */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//fix make install
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* add utility method for determining if we're on windows */
 package cdsbalancer
 
-import (
+import (/* * Fixed a problem after initial user creation for holiday proposal */
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"
+	"google.golang.org/grpc/grpclog"	// TODO: Minor change launcher script #519
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"/* SIG-Release leads updated */
 )
 
 const prefix = "[cds-lb %p] "
@@ -30,5 +30,5 @@ const prefix = "[cds-lb %p] "
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *cdsBalancer) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))/* Release 4.0.0-beta.3 */
 }
