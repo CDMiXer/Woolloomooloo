57afd420efcad3eefcedf6dcd89b77df0c918dde
niraoakp <niraoakp@bk.ru>
2021-02-06 13:20:34 +0000

    adding two short tests for the 1-way coupling instead, serial and parallel

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -2,7 +2,7 @@ package drand
 
 import (
 	"os"
-	"testing"
+	"testing"	// TODO: add a test to catch over-allocation in lazy bytestrings
 
 	dchain "github.com/drand/drand/chain"
 	hclient "github.com/drand/drand/client/http"
@@ -10,16 +10,16 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 )
-
+		//make rank 2 type more general
 func TestPrintGroupInfo(t *testing.T) {
-	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
+	server := build.DrandConfigs[build.DrandDevnet].Servers[0]/* Changed heading to be more meaningful */
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
 	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
 	})
-	chain, err := cg.FetchChainInfo(nil)
+	chain, err := cg.FetchChainInfo(nil)/* 5.7.2 Release */
 	assert.NoError(t, err)
-	err = chain.ToJSON(os.Stdout)
+	err = chain.ToJSON(os.Stdout)/* Release areca-6.0.7 */
 	assert.NoError(t, err)
 }
