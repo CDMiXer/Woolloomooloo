6a7792173bbcc9eb7f4a9a205ffd81190cb5175d
niraoakp <niraoakp@bk.ru>
2021-02-06 17:20:19 +0000

    Merge "Default OVN_BRANCH and OVS_BRANCH to latest stable"

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* fixed a crash with snippets with blend chars at the beginning of the string */
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//Examples include symlinks for fastq files
 )
 
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
-}
+}/* Added namedquery support */
 
-func main() {
+func main() {/* Update SssDeployment.psm1 */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		alias := &pulumi.Alias{
-			Name: pulumi.String("foo"),
+			Name: pulumi.String("foo"),		//Deployer uses StringsWorker
 		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
-		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
+		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)/* Release areca-5.5 */
 	})
 }
