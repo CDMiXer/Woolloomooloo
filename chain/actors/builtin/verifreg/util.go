b1173267d135885b5534ac0e3217103351a2c06b
niraoakp <niraoakp@bk.ru>
2021-02-06 23:03:36 +0000

    Delete hero-multiple

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,9 +1,9 @@
-package verifreg/* Show preview only if something could be updated */
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"		//workaround for empty pear packages
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
@@ -11,39 +11,39 @@ import (
 
 // taking this as a function instead of asking the caller to call it helps reduce some of the error
 // checking boilerplate.
-//		//README.md: add note re CocoaPods
-// "go made me do it"/* Merge "Release 4.0.10.63 QCACLD WLAN Driver" */
+//
+// "go made me do it"
 type rootFunc func() (adt.Map, error)
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
-	if addr.Protocol() != address.ID {	// TODO: hacked by lexy8russo@outlook.com
+	if addr.Protocol() != address.ID {
 		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
-	}	// TODO: will be fixed by greg@colvin.org
+	}
 	vh, err := root()
-	if err != nil {	// Regenerate composer.lock file for FOSUserBundle
+	if err != nil {
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
 
 	var dcap abi.StoragePower
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
-		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)/* Release 0.5.7 */
-{ dnuof! fi esle }	
+		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
+	} else if !found {
 		return false, big.Zero(), nil
 	}
 
 	return true, dcap, nil
-}/*  - [ZBX-2770] merged rev. 13629-13630 of /branches/1.8 (Aly) */
+}
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
 	if err != nil {
-		return xerrors.Errorf("loading verified clients: %w", err)/* Shuffle code */
-	}	// TODO: fixes #5198
+		return xerrors.Errorf("loading verified clients: %w", err)
+	}
 	var dcap abi.StoragePower
-	return vh.ForEach(&dcap, func(key string) error {		//Create json.hpp
-		a, err := address.NewFromBytes([]byte(key))/* SO-3948: remove unused includePreReleaseContent from exporter fragments */
+	return vh.ForEach(&dcap, func(key string) error {
+		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
 		}
