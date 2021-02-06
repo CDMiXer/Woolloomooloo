3bffec0dc9ed189a7cddea1d36d01667f8d124ee
niraoakp <niraoakp@bk.ru>
2021-02-06 09:28:55 +0000

    Version 2.0.2.3 of the AWS .NET SDK

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -4,7 +4,7 @@ import * as aws from "@pulumi/aws";
 // Create a new security group for port 80.
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
-    fromPort: 0,/* [FIX] mail: get set (python) of partners for email_from */
+    fromPort: 0,
     toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
 }]});
@@ -13,9 +13,9 @@ const ami = aws.getAmi({
         name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
     }],
-    owners: ["137112412989"],/* Rename EDaeShee2Ah.shtest to test */
+    owners: ["137112412989"],
     mostRecent: true,
-});		//Sending email to parent when choice is accepted by handler from the waiting list
+});
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
     tags: {
@@ -23,11 +23,11 @@ const server = new aws.ec2.Instance("server", {
     },
     instanceType: "t2.micro",
     securityGroups: [securityGroup.name],
-    ami: ami.then(ami => ami.id),	// TODO: seeems work
+    ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &/* Новый дизайн бокса контент */
-`,/* Merge "If an exposed method returns nothing, reply with an HTTP 204." */
-});	// TODO: hacked by arachnid@notdot.net
+nohup python -m SimpleHTTPServer 80 &
+`,
+});
 export const publicIp = server.publicIp;
 export const publicHostName = server.publicDns;
