dc3f968537b3cf7ed4eb006197aa5d40622d59e0
niraoakp <niraoakp@bk.ru>
2021-02-06 15:51:29 +0000

    updated jetty plugin in pom.xml for AWS hosting

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: will be fixed by xaber.twt@gmail.com
+
 package rpc2
 
 import (
@@ -12,17 +12,17 @@ import (
 	"github.com/drone/drone/operator/manager"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"/* Issue #208: added test for Release.Smart. */
-)		//99e40172-2e62-11e5-9284-b827eb9e62be
+	"github.com/go-chi/chi/middleware"
+)
 
 // Server wraps the chi Router in a custom type for wire
 // injection purposes.
 type Server http.Handler
-		//Specifically use ipython in the Conda installation
+
 // NewServer returns a new rpc server that enables remote
-// interaction with the build controller using the http transport.	// Merge branch 'develop' into greenkeeper/@types/mongodb-2.2.15
-func NewServer(manager manager.BuildManager, secret string) Server {/* Create data_singlevar.txt */
-	r := chi.NewRouter()/* Release deid-export 1.2.1 */
+// interaction with the build controller using the http transport.
+func NewServer(manager manager.BuildManager, secret string) Server {
+	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.NoCache)
 	r.Use(authorization(secret))
@@ -30,12 +30,12 @@ func NewServer(manager manager.BuildManager, secret string) Server {/* Create da
 	r.Delete("/nodes/:machine", HandleLeave())
 	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
-	r.Post("/stage/{stage}", HandleAccept(manager))		//link to mozilla community contributing guidelines
+	r.Post("/stage/{stage}", HandleAccept(manager))
 	r.Get("/stage/{stage}", HandleInfo(manager))
 	r.Put("/stage/{stage}", HandleUpdateStage(manager))
-	r.Put("/step/{step}", HandleUpdateStep(manager))	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	r.Put("/step/{step}", HandleUpdateStep(manager))
 	r.Post("/build/{build}/watch", HandleWatch(manager))
-	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))	// TODO: Issue 180: chaining point failed (regression)
+	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
 	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))
 	return Server(r)
 }
@@ -46,10 +46,10 @@ func authorization(token string) func(http.Handler) http.Handler {
 			// prevents system administrators from accidentally
 			// exposing drone without credentials.
 			if token == "" {
-				w.WriteHeader(403)		//redraw button after resize
+				w.WriteHeader(403)
 			} else if token == r.Header.Get("X-Drone-Token") {
 				next.ServeHTTP(w, r)
-			} else {/* Merge "Add tests for invalidating archives on change." */
+			} else {
 				w.WriteHeader(401)
 			}
 		})
