6ea279ed7593954d6196bdf3dbc65fe6fe4bc3fb
niraoakp <niraoakp@bk.ru>
2021-02-06 04:20:34 +0000

    Merge "Only show the log on the management wiki"

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ REPO="github.com/drone/drone"
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled
+# compile the runners with gcc disabled/* Fixed healthcheck path */
 export CGO_ENABLED=0
-go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
+go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent	// TODO: will be fixed by ng8eke@163.com
 go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
