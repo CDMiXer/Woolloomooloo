a58c227b208f93ec57a0c4cfd2e7ef3009facffa
niraoakp <niraoakp@bk.ru>
2021-02-06 19:13:54 +0000

    add multivariate difference uncertainty measure

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 75668e54-2e4a-11e5-9284-b827eb9e62be */
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
-;)4 ,"demood-ton"(ecruoseR wen = a tsnoc
-	// TODO: Use merge instead of assign to support < 2.4
-// "a" should still be in the checkpoint with its new value.
+const a = new Resource("not-doomed", 4);
+
+// "a" should still be in the checkpoint with its new value.		//Update and rename Index to Contents.md
