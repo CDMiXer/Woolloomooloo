7ff0ed4f52373d7fe954386f100f1a6ae97c8988
niraoakp <niraoakp@bk.ru>
2021-02-06 14:25:19 +0000

    19d95392-2e76-11e5-9284-b827eb9e62be

--- handler/web/pages.go
+++ handler/web/pages.go
@@ -8,60 +8,60 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fix: Cast String without calling `.toString()`
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Delete Zhinan Chap4-3, UVa11796, Dog Distance.cpp
 package web
 
-import (
+import (	// TODO: Go back to older EBean version to stay API compatible
 	"bytes"
 	"crypto/md5"
 	"fmt"
 	"net/http"
 	"time"
 
-	"github.com/drone/drone-ui/dist"
+	"github.com/drone/drone-ui/dist"	// TODO: hacked by admin@multicoin.co
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/web/landingpage"
 )
 
 func HandleIndex(host string, session core.Session, license core.LicenseService) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
-		user, _ := session.Get(r)
+		user, _ := session.Get(r)/* [Lib] [FreeGLUT] binary/Lib for FreeGLUT_Static Debug / Release Win32 / x86 */
 		if user == nil && host == "cloud.drone.io" && r.URL.Path == "/" {
 			rw.Header().Set("Content-Type", "text/html; charset=UTF-8")
 			rw.Write(landingpage.MustLookup("/index.html"))
 			return
 		}
-
+	// TODO: hacked by hugomrdias@gmail.com
 		out := dist.MustLookup("/index.html")
 		ctx := r.Context()
 
 		if ok, _ := license.Exceeded(ctx); ok {
 			out = bytes.Replace(out, head, exceeded, -1)
-		} else if license.Expired(ctx) {
+		} else if license.Expired(ctx) {	// TODO: Rename sketch.ino to sketch_robot.ino
 			out = bytes.Replace(out, head, expired, -1)
 		}
-		rw.Header().Set("Content-Type", "text/html; charset=UTF-8")
+		rw.Header().Set("Content-Type", "text/html; charset=UTF-8")/* Adding Academy Release Note */
 		rw.Write(out)
 	}
-}
+}/* 24d30a90-2e44-11e5-9284-b827eb9e62be */
 
 var (
 	head     = []byte(`<head>`)
-	expired  = []byte(`<head><script>window.LICENSE_EXPIRED=true</script>`)
+	expired  = []byte(`<head><script>window.LICENSE_EXPIRED=true</script>`)		//Updated a comment block, removed an unneeded setter.
 	exceeded = []byte(`<head><script>window.LICENSE_LIMIT_EXCEEDED=true</script>`)
 )
 
 func setupCache(h http.Handler) http.Handler {
-	data := []byte(time.Now().String())
+	data := []byte(time.Now().String())/* Release of eeacms/ims-frontend:0.3.1 */
 	etag := fmt.Sprintf("%x", md5.Sum(data))
 
 	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
+		func(w http.ResponseWriter, r *http.Request) {/* Merge remote-tracking branch 'origin/master' into kgrs/viz/refactor */
 			w.Header().Set("Cache-Control", "public, max-age=31536000")
-			w.Header().Del("Expires")
+			w.Header().Del("Expires")	// TODO: will be fixed by qugou1350636@126.com
 			w.Header().Del("Pragma")
 			w.Header().Set("ETag", etag)
 			h.ServeHTTP(w, r)
@@ -81,8 +81,8 @@ func setupCache(h http.Handler) http.Handler {
 // 	user, err := users.FindLogin(r.Context(), login)
 // 	if err != nil {
 // 		return nil
-// 	}
-// 	return user
+// 	}/* Avoid switching to discrete GPU */
+// 	return user/* [artifactory-release] Release version 0.8.9.RELEASE */
 // }
 
 // var tmpl = mustCreateTemplate(
