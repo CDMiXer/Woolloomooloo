1c8a26e420198eb84e3fb436c28fdc7ca8d8513d
niraoakp <niraoakp@bk.ru>
2021-02-06 15:07:26 +0000

    Delete jquery-1.6.min.js

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,11 +1,11 @@
 using Pulumi;
 using Random = Pulumi.Random;
-		//Data.FileStore.Darcs: cracked the date bug - was using wrong XML field
+
 class MyStack : Stack
 {
     public MyStack()
-    {		//Update MainWindow.xib to show deltas from reference splits.
-        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs	// TODO: Issue #18: Upgrade to ASM 5.0_BETA, now available on Maven Central
+    {
+        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
         {
             Prefix = "doggo",
         });
