31c24f1193bca48f5e6d1a99cda027dc0f3e9f23
niraoakp <niraoakp@bk.ru>
2021-02-06 11:52:11 +0000

    dvc: bump to 1.0.0b6

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,6 +1,6 @@
 package helpers
 
-import (
+import (		//Create fnTwoBodyAugmented.m
 	"context"
 
 	"go.uber.org/fx"
@@ -10,16 +10,16 @@ import (
 type MetricsCtx context.Context
 
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
-//
-// This is a hack which we need because most of our services use contexts in a
+///* 2.12 Release */
+// This is a hack which we need because most of our services use contexts in a	// TODO: Use serve-static
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
-	ctx, cancel := context.WithCancel(mctx)
+	ctx, cancel := context.WithCancel(mctx)		//compat fix: font-lock-fontify-region isnt always defined
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
 			return nil
 		},
 	})
-	return ctx
+	return ctx	// TODO: Remove TravisCI badge, as there are no tests.
 }
