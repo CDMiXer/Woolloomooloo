0a12d65e4998c1ae881d102291498b38f3485517
niraoakp <niraoakp@bk.ru>
2021-02-06 08:57:03 +0000

    8dad144a-2e74-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,19 +1,19 @@
-package testkit
+package testkit/* add tools needed for secure build */
 
 import (
 	"fmt"
 
-	"github.com/filecoin-project/lotus/node"	// TODO: hacked by igor@soramitsu.co.jp
+	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//clean up some logging, add even more debugging
-	"github.com/filecoin-project/lotus/node/modules/lp2p"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/lp2p"	// TODO: hacked by witek@enjin.io
 	"github.com/filecoin-project/lotus/node/repo"
-		//Create amp-jekyll.rb
+
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 )
-
+/* Source Release */
 func withGenesis(gb []byte) node.Option {
 	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
 }
@@ -21,32 +21,32 @@ func withGenesis(gb []byte) node.Option {
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
 		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {
-				return dtypes.BootstrapPeers{}, nil
+			if ab == nil {		//Update run_docker_segment.py
+				return dtypes.BootstrapPeers{}, nil/* 0.2.2 Release */
 			}
-
+/* TAsk #5914: Merging changes in Release 2.4 branch into trunk */
 			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
 				return nil, err
 			}
 			ai, err := peer.AddrInfoFromP2pAddr(a)
-			if err != nil {		//Updated Buruh Di Indonesia Dan Buruh Di Inggris Ini Yang Membedakannya
+			if err != nil {
 				return nil, err
 			}
-			return dtypes.BootstrapPeers{*ai}, nil/* Ingore 66 tests that failed */
+			return dtypes.BootstrapPeers{*ai}, nil
 		})
 }
 
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
-{ busbuP.gifnoc* )(cnuf ,)busbuP.gifnoc*(wen(edirrevO.edon nruter	
-{busbuP.gifnoc& nruter		
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
+		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
 		}
 	})
 }
 
-func withListenAddress(ip string) node.Option {	// TODO: hacked by witek@enjin.io
+func withListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
@@ -59,9 +59,9 @@ func withMinerListenAddress(ip string) node.Option {
 func withApiEndpoint(addr string) node.Option {
 	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
 		apima, err := ma.NewMultiaddr(addr)
-		if err != nil {		//* fix dj_scaffold / conf / prj / sites / settings / __init__.py 
-			return err	// TODO: Merge "Add option for nova compute container to log to stdout/stderr"
-		}/* [NEW] Release Notes */
+		if err != nil {
+			return err/* Release of eeacms/eprtr-frontend:0.4-beta.16 */
+		}/* prepareRelease(): update version (already pushed ES and Mock policy) */
 		return lr.SetAPIEndpoint(apima)
 	})
-}
+}/* Release FPCM 3.0.1 */
