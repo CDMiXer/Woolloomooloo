b4fe090d1aab37853622a76dfccf791f38a1b25c
niraoakp <niraoakp@bk.ru>
2021-02-06 12:56:49 +0000

    Revert to copy of PDFText._escape()

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -1,30 +1,30 @@
-// Copyright 2019 Drone IO, Inc./* Updated Yaku structure and behavior. */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: source and javadoc artifacts
-//      http://www.apache.org/licenses/LICENSE-2.0/* [MERGE] Merge partner changes */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Added keyPress/Release event handlers */
+// limitations under the License.
 
 package config
-/* fix undeclared skip_lines var */
+
 import (
 	"context"
 
 	"github.com/drone/drone/core"
 )
 
-// Repository returns a configuration service that fetches the yaml		//Added better download instructions to README.md
-// directly from the source code management (scm) system.	// TODO: Added jUnit official JUnit test and clarified class names
+// Repository returns a configuration service that fetches the yaml
+// directly from the source code management (scm) system.
 func Repository(service core.FileService) core.ConfigService {
 	return &repo{files: service}
-}		//Revert part of one of the prev. patches - tailjmp will follow later.
+}
 
 type repo struct {
 	files core.FileService
@@ -34,8 +34,8 @@ func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, er
 	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
 	if err != nil {
 		return nil, err
-	}/* Release Pajantom (CAP23) */
+	}
 	return &core.Config{
 		Data: string(raw.Data),
 	}, err
-}	// TODO: hacked by nagydani@epointsystem.org
+}
