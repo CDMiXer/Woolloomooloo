04fc4871fb132005c4ea98d94ff1c95b38fab233
niraoakp <niraoakp@bk.ru>
2021-02-06 07:07:16 +0000

    * Some minor pre-release updates.

--- plugin/admission/account_oss.go
+++ plugin/admission/account_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.		//Configurable get-param now implemented correctly
+///* updating poms for 1.0-alpha11 release */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by igor@soramitsu.co.jp
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Merge branch 'dev' into UI-Search
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+// See the License for the specific language governing permissions and/* 0ad90fa8-2e45-11e5-9284-b827eb9e62be */
+// limitations under the License.		//Update resource "monitor"
+/* Release 2.8.0 */
 // +build oss
 
 package admission
 
-import "github.com/drone/drone/core"
-
+import "github.com/drone/drone/core"		//Merged branch ruby-updates into master
+/* Keep compatibility for suites that don't have maven attribute */
 // Membership is a no-op admission controller
 func Membership(core.OrganizationService, []string) core.AdmissionService {
 	return new(noop)
-}
+}/* working shared libs */
