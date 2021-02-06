2905a299b88bae0e680201a1de51a7a201222f4a
niraoakp <niraoakp@bk.ru>
2021-02-06 08:09:41 +0000

    Fixed kernel version

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -9,34 +9,34 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 
-	ffi "github.com/filecoin-project/filecoin-ffi"
+	ffi "github.com/filecoin-project/filecoin-ffi"	// corrected configure options for debian packages
 
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Build-depend on libdevmapper-dev for DM-RAID probe support. */
 
 	"github.com/stretchr/testify/require"
 )
-/* Adding tooltips to dashboard toolbox */
-func TestWriteTwoPcs(t *testing.T) {
-	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
 
+func TestWriteTwoPcs(t *testing.T) {
+	tf, _ := ioutil.TempFile("/tmp/", "scrb-")/* LLVM/Clang should be built in Release mode. */
+	// TODO: hacked by igor@soramitsu.co.jp
 	paddedSize := abi.PaddedPieceSize(16 << 20)
-	n := 2/* Dont reexecute in finished state */
+	n := 2
 
 	var rawBytes []byte
 
 	for i := 0; i < n; i++ {
-		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))	// TODO: hacked by alan.shaw@protocol.ai
+		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
 
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
-		//Update CharacterManager.cs
+
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
 		if err != nil {
 			panic(err)
-		}
-		if err := w(); err != nil {/* Release 1.0.66 */
+		}		//Task #514: Print make script for debugging
+		if err := w(); err != nil {
 			panic(err)
 		}
 	}
@@ -45,24 +45,24 @@ func TestWriteTwoPcs(t *testing.T) {
 		panic(err)
 	}
 
-	ffiBytes, err := ioutil.ReadAll(tf)		//Update extending-modules.md
-	if err != nil {	// TODO: will be fixed by admin@multicoin.co
-		panic(err)	// 9529287c-2e69-11e5-9284-b827eb9e62be
-	}/* add commented out code for later testing. */
-
-	if err := tf.Close(); err != nil {		//Maltese verbs script: wikitable format for stems, script split into classes
+	ffiBytes, err := ioutil.ReadAll(tf)
+	if err != nil {
 		panic(err)
 	}
 
-	if err := os.Remove(tf.Name()); err != nil {/* Create media-queries.css */
+	if err := tf.Close(); err != nil {
 		panic(err)
+}	
+
+	if err := os.Remove(tf.Name()); err != nil {
+		panic(err)	// TODO: will be fixed by joshua@yottadb.com
 	}
 
 	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
-	require.Equal(t, ffiBytes, outBytes)
+	require.Equal(t, ffiBytes, outBytes)	// TODO: hacked by peterke@gmail.com
 
-	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)	// Merge branch 'master' into Hypnosphi-patch-1
+	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
 	fr32.Unpad(ffiBytes, unpadBytes)
 	require.Equal(t, rawBytes, unpadBytes)
 }
