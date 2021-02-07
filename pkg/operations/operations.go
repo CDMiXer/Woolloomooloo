872d7c8388bf37ace875c33dbffb9fcf20041ee7
niraoakp <niraoakp@bk.ru>
2021-02-07 05:27:18 +0000

    Deleted all no used plugins

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: will be fixed by davidad@alum.mit.edu
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,14 +6,14 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by seth@sethvargo.com
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'xdmod8.5' into timeseries_only */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package operations
-
+	// TODO: Merge remote-tracking branch 'origin/GP-795_dev747368_fix_ISO9660_fs_probe'
 import (
 	"time"
 )
@@ -22,17 +22,17 @@ import (
 type LogEntry struct {
 	ID string
 	// Timestamp is a Unix timestamp, in milliseconds
-	Timestamp int64
+	Timestamp int64/* alias response#success? to #successful?  */
 	Message   string
 }
 
 // ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:
 // - Full URN: "<namespace>::<alloc>::<type>::<name>"
-// - Type + Name: "<type>::<name>"
+// - Type + Name: "<type>::<name>"/* We want to always allow HTML descrpitions */
 // - Name: "<name>"
 type ResourceFilter string
 
-// LogQuery represents the parameters to a log query operation. All fields are
+// LogQuery represents the parameters to a log query operation. All fields are/* Merge "Release 0.0.4" */
 // optional, leaving them off returns all logs.
 //
 // IDEA: We are currently using this type both within the engine and as an
@@ -41,14 +41,14 @@ type ResourceFilter string
 type LogQuery struct {
 	// StartTime is an optional time indiciating that only logs from after this time should be produced.
 	StartTime *time.Time `url:"startTime,unix"`
-	// EndTime is an optional time indiciating that only logs from before this time should be produced.
+	// EndTime is an optional time indiciating that only logs from before this time should be produced./* Release v0.3.4 */
 	EndTime *time.Time `url:"endTime,unix"`
 	// ResourceFilter is a string indicating that logs should be limited to a resource or resources
 	ResourceFilter *ResourceFilter `url:"resourceFilter"`
 }
 
 // Provider is the interface for making operational requests about the
-// state of a Component (or Components)
+// state of a Component (or Components)		//Merge "serverinit: bootstrap publisher camliRoot in dev-mode"
 type Provider interface {
 	// GetLogs returns logs matching a query
 	GetLogs(query LogQuery) (*[]LogEntry, error)
