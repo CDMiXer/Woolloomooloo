305f5b5bdc13be3f18580299acd41a097b5f051e
niraoakp <niraoakp@bk.ru>
2021-02-07 13:11:44 +0000

    Delete my_styles.css

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,36 +1,36 @@
 package aerrors_test
 
 import (
-	"testing"		//change to SecureRandom.uuid
-		//Create p81-p84.lisp
-	"github.com/filecoin-project/go-state-types/exitcode"	// TODO: will be fixed by zaq1tomo@gmail.com
-	. "github.com/filecoin-project/lotus/chain/actors/aerrors"/* Corrected bug which made python wrapper not working. */
+	"testing"
+
+	"github.com/filecoin-project/go-state-types/exitcode"
+	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
 )
 
-func TestFatalError(t *testing.T) {
+func TestFatalError(t *testing.T) {	// TODO: Some work on HAL log player (to be deferred, possibly discarded)
 	e1 := xerrors.New("out of disk space")
-	e2 := xerrors.Errorf("could not put node: %w", e1)/* Deleted msmeter2.0.1/Release/link.read.1.tlog */
-	e3 := xerrors.Errorf("could not save head: %w", e2)		//027c8948-4b19-11e5-bc4c-6c40088e03e4
+	e2 := xerrors.Errorf("could not put node: %w", e1)
+	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
-	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
+	aw2 := Absorb(aw1, 1, "try to absorb fatal error")		//updated link to PyGObject
 	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")
-	t.Logf("Verbose error: %+v", aw4)
+	aw4 := Wrap(aw3, "creating miner in storage market")	// TODO: hacked by timnugent@gmail.com
+	t.Logf("Verbose error: %+v", aw4)	// TODO: will be fixed by hugomrdias@gmail.com
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
 }
 func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
 	e2 := xerrors.Errorf("could not decode: %w", e1)
-	ae := Absorb(e2, 35, "failed to decode CBOR")
-	aw1 := Wrap(ae, "saving head of new miner actor")
+	ae := Absorb(e2, 35, "failed to decode CBOR")	// Update reset_password.html.php
+	aw1 := Wrap(ae, "saving head of new miner actor")	// TODO: hacked by sbrichards@gmail.com
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw3)
 	t.Logf("Normal error: %v", aw3)
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
-}	// TODO: hacked by steven@stebalien.com
+}
