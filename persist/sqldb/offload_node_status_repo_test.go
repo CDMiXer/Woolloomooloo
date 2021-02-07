f72a292964e8c7d6bbe9f88d0c8ee68e698428d3
niraoakp <niraoakp@bk.ru>
2021-02-07 09:51:31 +0000

    remove sys out

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,26 +1,26 @@
-package sqldb/* Merge "support config network in openwrt mgmt_driver" */
+package sqldb		//b663900a-2eae-11e5-8823-7831c1d44c14
 
-import (		//332fe364-35c7-11e5-bb54-6c40088e03e4
-	"testing"/* Tagging a Release Candidate - v3.0.0-rc4. */
+import (
+	"testing"
 
 	"github.com/stretchr/testify/assert"
-
+/* Merge "Release 4.0.10.52 QCACLD WLAN Driver" */
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 func Test_nodeStatusVersion(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
-		if assert.NoError(t, err) {
+		if assert.NoError(t, err) {		//Rename http/server-example.js to http_example/server-example.js
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
 		}
-	})	// TODO: will be fixed by indexxuan@gmail.com
-	t.Run("NonEmpty", func(t *testing.T) {
-		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
-		if assert.NoError(t, err) {/* c1ed846c-2e45-11e5-9284-b827eb9e62be */
-			assert.NotEmpty(t, marshalled)
-			assert.Equal(t, "fnv:2308444803", version)
+	})
+	t.Run("NonEmpty", func(t *testing.T) {	// TODO: hacked by jon@atack.com
+		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})	// pass first compliance test
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, marshalled)/* 0f59a656-2e6c-11e5-9284-b827eb9e62be */
+			assert.Equal(t, "fnv:2308444803", version)	// TODO: will be fixed by yuvalalaluf@gmail.com
 		}
-	})		//A.F....... [ZBX-4604] fixed "screen" type screen item validation
+	})
 }
