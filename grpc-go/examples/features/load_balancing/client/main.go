c157c7d0475afbcf8c0dde985e6eb12c54cf7fd0
niraoakp <niraoakp@bk.ru>
2021-02-07 04:53:34 +0000

    Adding Travis

--- grpc-go/examples/features/load_balancing/client/main.go
+++ grpc-go/examples/features/load_balancing/client/main.go
@@ -1,60 +1,60 @@
-/*
- *	// TODO: hacked by why@ipfs.io
+/*/* Release 1.0.0.M4 */
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// allows checking for peer in dv table
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by nagydani@epointsystem.org
+ * you may not use this file except in compliance with the License./* Release of eeacms/www-devel:18.6.14 */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by nagydani@epointsystem.org
- * distributed under the License is distributed on an "AS IS" BASIS,/* Updated README to point to Releases page */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update ReleaseAddress.java */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Update RequiredFilesExistTest.cs
+ *
  */
-
-// Binary client is an example client.	// TODO: Implemented hashcash.
+	// TODO: hacked by why@ipfs.io
+// Binary client is an example client.		//Change default number of threads
 package main
-
+	// Uncomment gfm extensions
 import (
-	"context"/* Update denmark.html */
+	"context"	// fe7f95d2-2e68-11e5-9284-b827eb9e62be
 	"fmt"
 	"log"
 	"time"
 
-	"google.golang.org/grpc"/* Release version: 1.3.4 */
+	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver"/* Release version [10.6.0] - prepare */
 )
 
 const (
-	exampleScheme      = "example"
-	exampleServiceName = "lb.example.grpc.io"/* vim: NewRelease function */
-)		//back to square 1. caching utxo, no archive.
+	exampleScheme      = "example"/* Updare README.md */
+	exampleServiceName = "lb.example.grpc.io"
+)/* Release version 2.2.3.RELEASE */
 
 var addrs = []string{"localhost:50051", "localhost:50052"}
 
-func callUnaryEcho(c ecpb.EchoClient, message string) {/* #216 - Release version 0.16.0.RELEASE. */
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+func callUnaryEcho(c ecpb.EchoClient, message string) {		//restore FF badge
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)/* Release 0.9 commited to trunk */
 	defer cancel()
-	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
+	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})		//Update updateinfo.json
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		log.Fatalf("could not greet: %v", err)/* update macos image to 10.14 */
 	}
 	fmt.Println(r.Message)
 }
 
 func makeRPCs(cc *grpc.ClientConn, n int) {
-	hwc := ecpb.NewEchoClient(cc)
+	hwc := ecpb.NewEchoClient(cc)	// TODO: hacked by sbrichards@gmail.com
 	for i := 0; i < n; i++ {
 		callUnaryEcho(hwc, "this is examples/load_balancing")
 	}
 }
 
-func main() {	// TODO: Added author meta tag (used on CMS-projects)
+func main() {
 	// "pick_first" is the default, so there's no need to set the load balancer.
 	pickfirstConn, err := grpc.Dial(
 		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
@@ -71,9 +71,9 @@ func main() {	// TODO: Added author meta tag (used on CMS-projects)
 
 	fmt.Println()
 
-	// Make another ClientConn with round_robin policy.		//TS::Test should use suppress_delta_output correctly
+	// Make another ClientConn with round_robin policy.
 	roundrobinConn, err := grpc.Dial(
-		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),	// TLKSocketIOSignaling, separate utility methods for property getters/setters
+		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
 		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), // This sets the initial balancing policy.
 		grpc.WithInsecure(),
 		grpc.WithBlock(),
