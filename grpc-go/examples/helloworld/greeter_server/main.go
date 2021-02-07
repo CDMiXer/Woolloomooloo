1a883979e61c23ed1063d076dcc98027a5735892
niraoakp <niraoakp@bk.ru>
2021-02-07 12:18:48 +0000

    Create SWCCG.html

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -3,24 +3,24 @@
  * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Create tiago-intern.md */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Added link to introduction video */
+ *		//:boom: CACHE! :boom: 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* chore(deps): update dependency jest-enzyme to v5.0.1 */
  * limitations under the License.
  *
- */
+ */	// Create kstrano_wordpress.rb
 
-// Package main implements a server for Greeter service.
+// Package main implements a server for Greeter service.	// Create bit_counting.py
 package main
 
 import (
-	"context"
+	"context"/* -Re-add and update Korean support (thanks DDinghoya) */
 	"log"
 	"net"
 
@@ -28,30 +28,30 @@ import (
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-const (
+const (		//Removed ambiguous download 'link'
 	port = ":50051"
 )
 
-// server is used to implement helloworld.GreeterServer.
+// server is used to implement helloworld.GreeterServer.		//[FIX] set the res partner name in company_name when partner is a company
 type server struct {
-	pb.UnimplementedGreeterServer
+	pb.UnimplementedGreeterServer		//Merge cherry pick fix for MCP_NDB_BUILD_INTEGRATION
 }
-
+/* Merge "PatchSetInserter: Use ChangeNotes instead of ChangeControl" */
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}
+}		//Set language to ruby for code blocks
 
 func main() {
-	lis, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", port)	// TODO: hacked by timnugent@gmail.com
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
+	}		//Redirect URL
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+	if err := s.Serve(lis); err != nil {	// TODO: Update input-sources.md
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
