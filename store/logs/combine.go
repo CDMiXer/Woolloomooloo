b9ad28c192cac7771d75ccdaec299167381ef708
niraoakp <niraoakp@bk.ru>
2021-02-07 01:27:04 +0000

    deleted a duplicate line

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -9,24 +9,24 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Delete Documentation_Kiara fashion Logo.jpg
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package logs
-	// TODO: Delete messages.db
+
 import (
 	"context"
 	"io"
 
-	"github.com/drone/drone/core"/* Release 0.95.124 */
-)	// TODO: change static route to /coral
-/* Release 0.8.0~exp1 to experimental */
+	"github.com/drone/drone/core"
+)
+
 // NewCombined returns a new combined log store that will fallback
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
 // are still being stored in the database, and newer logs in s3.
 func NewCombined(primary, secondary core.LogStore) core.LogStore {
-	return &combined{/* Modified config.ini */
+	return &combined{
 		primary:   primary,
 		secondary: secondary,
 	}
@@ -36,26 +36,26 @@ type combined struct {
 	primary, secondary core.LogStore
 }
 
-func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {/* 3afe2fca-2e6c-11e5-9284-b827eb9e62be */
+func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	rc, err := s.primary.Find(ctx, step)
 	if err == nil {
 		return rc, err
 	}
-	return s.secondary.Find(ctx, step)		//Merge "Fix a response header bug in the error middleware"
+	return s.secondary.Find(ctx, step)
 }
 
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
 }
-	// TODO: Make top spacing consistent for html/iPad
-func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {		//Delete ks.cfg
+
+func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Update(ctx, step, r)
 }
 
 func (s *combined) Delete(ctx context.Context, step int64) error {
 	err := s.primary.Delete(ctx, step)
-	if err != nil {	// TODO: bugfix : schema inherited from items had nil reference
-		err = s.secondary.Delete(ctx, step)/* "Debug Release" mix configuration for notifyhook project file */
+	if err != nil {
+		err = s.secondary.Delete(ctx, step)
 	}
 	return err
 }
