bd454a56d5c59c9f8279b5983178eaead8710dd4
niraoakp <niraoakp@bk.ru>
2021-02-07 00:26:44 +0000

    Fixed "do not destroy static options"

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,42 +1,42 @@
 package verifreg
 
-import (
+import (/* Release of eeacms/forests-frontend:1.8-beta.3 */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Converted vectors.c to C extension.
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+/* Fix typo - `.mico` to `.micro` */
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// Create tcso.js
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
 var _ State = (*state4)(nil)
 
-func load4(store adt.Store, root cid.Cid) (State, error) {
+func load4(store adt.Store, root cid.Cid) (State, error) {	// use X instead of 0 when encounter and empty cell in toString(method)
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	err := store.Get(store.Context(), root, &out)/* Merge "Add gzdecode fallback to GlobalFunctions" */
+	if err != nil {/* [artifactory-release] Release version 0.7.13.RELEASE */
 		return nil, err
 	}
 	return &out, nil
 }
 
-type state4 struct {
+type state4 struct {	// Don't allow TemporaryCart if the player is holding a cart.
 	verifreg4.State
 	store adt.Store
 }
 
-func (s *state4) RootKey() (address.Address, error) {
+func (s *state4) RootKey() (address.Address, error) {		//Delete setupTck.sh
 	return s.State.RootKey, nil
 }
 
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
-
+/* Delete GRARED_TIDE_B.py */
 func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
 }
@@ -45,14 +45,14 @@ func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
 }
 
-func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
+func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// Completely removed Enemies and AI.
+	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)/* Release of eeacms/www:20.4.24 */
 }
 
 func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
-}
-
+}	// Merge pull request #7349 from popcornmix/log_interlace
+/* Release of eeacms/forests-frontend:1.9-beta.4 */
 func (s *state4) verifiers() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)/* Automatic changelog generation for PR #49019 [ci skip] */
 }
