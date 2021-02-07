b08a91cf3d31ef5867e0e792f7f4308eaa09a7cc
niraoakp <niraoakp@bk.ru>
2021-02-07 05:46:13 +0000

    Fixed CRC32 generator

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,53 +1,53 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 2.0rc2 */
 // that can be found in the LICENSE file.
-
+/* Release v5.6.0 */
 // +build !oss
-/* Delete friyo@surya */
+
 package collabs
 
 import (
 	"net/http"
-
+		//removed obsolete project
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Release v0.3.0 */
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)
+)/* projects files now stored more organized within specific project folder. */
 
 // HandleList returns an http.HandlerFunc that write a json-encoded
-// list of repository collaborators to the response body.	// TODO: hacked by martin2cai@hotmail.com
+// list of repository collaborators to the response body.	// [asan] use raw syscalls for open/close on linux to avoid being intercepted
 func HandleList(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,	// update quarry to auto-set its work-bounds y-size/offset properly
 	members core.PermStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* More prominent */
+			namespace = chi.URLParam(r, "owner")		//Remove maintenance warning from README
 			name      = chi.URLParam(r, "name")
 		)
-/* Release v1.0.1 */
-		repo, err := repos.FindName(r.Context(), namespace, name)
+
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: will be fixed by nick@perfectabstractions.com
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).	// Update cellbase-env.sh
-				WithError(err).
+			logger.FromRequest(r).
+				WithError(err)./* f4963ee6-2e49-11e5-9284-b827eb9e62be */
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
-		}
+		}		//Fix peerDependency of react
 		members, err := members.List(r.Context(), repo.UID)
-		if err != nil {
+		if err != nil {/* Update task.html.md */
 			render.InternalError(w, err)
-			logger.FromRequest(r)./* Update from Andrew Crapo */
+			logger.FromRequest(r)./* Fix running elevated tests. Release 0.6.2. */
 				WithError(err).
 				WithField("namespace", namespace).
-.)eman ,"eman"(dleiFhtiW				
+				WithField("name", name).
 				Warnln("api: cannot get member list")
-		} else {/* Renamed isChildlogicHandled to isChildLogicHandled */
+		} else {
 			render.JSON(w, members, 200)
-		}
+}		
 	}
 }
