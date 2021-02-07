fccffa2480f36b33994f67c128b6925604f7eee0
niraoakp <niraoakp@bk.ru>
2021-02-07 22:11:22 +0000

    Update SpringBootSetup

--- handler/api/repos/builds/create.go
+++ handler/api/repos/builds/create.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: 75469e36-2e41-11e5-9284-b827eb9e62be
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,13 +10,13 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Update ReleaseNotes.md */
 
 package builds
-
+	// TODO: will be fixed by yuvalalaluf@gmail.com
 import (
 	"net/http"
-
+	// TODO: hacked by boringland@protonmail.ch
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
@@ -25,11 +25,11 @@ import (
 	"github.com/go-chi/chi"
 )
 
-// HandleCreate returns an http.HandlerFunc that processes http
+// HandleCreate returns an http.HandlerFunc that processes http/* Create eticalgarve */
 // requests to create a build for the specified commit.
 func HandleCreate(
-	users core.UserStore,
-	repos core.RepositoryStore,
+	users core.UserStore,	// TODO: hacked by boringland@protonmail.ch
+	repos core.RepositoryStore,		//Specified sqlite3 requirement in README
 	commits core.CommitService,
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
@@ -37,7 +37,7 @@ func HandleCreate(
 		var (
 			ctx       = r.Context()
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* re-minify wp-admin.dev.css after r15215. See #12225 */
 			sha       = r.FormValue("commit")
 			branch    = r.FormValue("branch")
 			user, _   = request.UserFrom(ctx)
@@ -46,37 +46,37 @@ func HandleCreate(
 		repo, err := repos.FindName(ctx, namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
-		}
+			return		//Updated for tutorial 43
+		}	// TODO: hacked by xaber.twt@gmail.com
 
 		owner, err := users.Find(ctx, repo.UserID)
 		if err != nil {
 			render.NotFound(w, err)
-			return
-		}
+			return/* Release-News of adapters for interval arithmetic is added. */
+		}	// TODO: will be fixed by igor@soramitsu.co.jp
 
 		// if the user does not provide a branch, assume the
 		// default repository branch.
 		if branch == "" {
 			branch = repo.Branch
-		}
+		}	// TODO: hacked by brosner@gmail.com
 		// expand the branch to a git reference.
 		ref := scm.ExpandRef(branch, "refs/heads")
 
 		var commit *core.Commit
 		if sha != "" {
 			commit, err = commits.Find(ctx, owner, repo.Slug, sha)
-		} else {
+		} else {/* Fix error when size is less than 1. */
 			commit, err = commits.FindRef(ctx, owner, repo.Slug, ref)
 		}
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-
+	// 0ad069e0-2e61-11e5-9284-b827eb9e62be
 		hook := &core.Hook{
 			Trigger:      user.Login,
-			Event:        core.EventCustom,
+			Event:        core.EventCustom,	// TODO: hacked by boringland@protonmail.ch
 			Link:         commit.Link,
 			Timestamp:    commit.Author.Date,
 			Title:        "", // we expect this to be empty.
