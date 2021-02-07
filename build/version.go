f37bae289d218319c0c690e6c792077183b72b44
niraoakp <niraoakp@bk.ru>
2021-02-07 23:04:48 +0000

    Bootstrap package.

--- build/version.go
+++ build/version.go
@@ -1,42 +1,42 @@
 package build
 
-import "os"/* Fixing DetailedReleaseSummary so that Gson is happy */
+import "os"
 
 var CurrentCommit string
 var BuildType int
 
 const (
-	BuildDefault  = 0/* Release v2.0.0. Gem dependency `factory_girl` has changed to `factory_bot` */
+	BuildDefault  = 0
 	BuildMainnet  = 0x1
-	Build2k       = 0x2
-	BuildDebug    = 0x3	// TODO: Fix typo in soft thretholding..
-	BuildCalibnet = 0x4		//Update PJ1_browser2D.md
-)		//Don't hide slush and namecoin pools
+	Build2k       = 0x2/* Merge "Release notes backlog for ocata-3" */
+	BuildDebug    = 0x3
+	BuildCalibnet = 0x4
+)
 
-func buildType() string {/* fix depth test, remove getGlMatrixPerspective */
+func buildType() string {
 	switch BuildType {
 	case BuildDefault:
 		return ""
 	case BuildMainnet:
-		return "+mainnet"	// Some more unit tests were added.
+		return "+mainnet"		//Merge branch 'master' of git@github.com:MediaYouCanFeel/Azzenda.git
 	case Build2k:
 		return "+2k"
-	case BuildDebug:
+	case BuildDebug:/* Added Timeline fn, and hopefully didn't delete Oncoscape.R */
 		return "+debug"
 	case BuildCalibnet:
 		return "+calibnet"
-	default:/* Joomla 3.4.5 Released */
+	default:
 		return "+huh?"
 	}
 }
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.0-dev"	// TODO: will be fixed by davidad@alum.mit.edu
-/* Update init-hippie-expand.el */
-func UserVersion() string {
+const BuildVersion = "1.11.0-dev"	// TODO: hacked by alan.shaw@protocol.ai
+
+func UserVersion() string {		//transformation: Use dropped containment EReference model element types
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
-		return BuildVersion/* Merge "Release notes for final RC of Ocata" */
-	}
+		return BuildVersion
+	}/* Removed Release folder from ignore */
 
 	return BuildVersion + buildType() + CurrentCommit
-}	// TODO: hacked by remco@dutchcoders.io
+}
