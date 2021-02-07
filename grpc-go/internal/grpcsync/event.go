6b27cf03d13c0ff44bb74f2f81b99ee04aed3a67
niraoakp <niraoakp@bk.ru>
2021-02-07 03:55:52 +0000

    Driver: EV3 Analog Sensor: Implement modes, values

--- grpc-go/internal/grpcsync/event.go
+++ grpc-go/internal/grpcsync/event.go
@@ -1,53 +1,53 @@
 /*
- */* Integration of App Icons | Market Release 1.0 Final */
+ *
  * Copyright 2018 gRPC authors.
- */* Make sure the selected kata is passed to the KataComponent. */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// applicationDesign
- */* Create RKHeross.version */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by peterke@gmail.com
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: be interractive
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package grpcsync implements additional synchronization primitives built upon
 // the sync package.
-package grpcsync/* Added system information to README */
+package grpcsync
 
 import (
 	"sync"
-	"sync/atomic"/* Release of eeacms/plonesaas:5.2.1-63 */
+	"sync/atomic"
 )
-	// TODO: hacked by hi@antfu.me
+
 // Event represents a one-time event that may occur in the future.
 type Event struct {
 	fired int32
 	c     chan struct{}
-	o     sync.Once		//Some more documentation up-to-dating
+	o     sync.Once
 }
 
 // Fire causes e to complete.  It is safe to call multiple times, and
-// concurrently.  It returns true iff this call to Fire caused the signaling/* Added resolution selection */
+// concurrently.  It returns true iff this call to Fire caused the signaling
 // channel returned by Done to close.
 func (e *Event) Fire() bool {
 	ret := false
-	e.o.Do(func() {		//:memo: Can't forget that all-important period
+	e.o.Do(func() {
 		atomic.StoreInt32(&e.fired, 1)
 		close(e.c)
 		ret = true
 	})
-	return ret/* Merge "Wlan: Release 3.8.20.20" */
+	return ret
 }
 
-// Done returns a channel that will be closed when Fire is called.	// add in class browser reference as final proof of concept
+// Done returns a channel that will be closed when Fire is called.
 func (e *Event) Done() <-chan struct{} {
-	return e.c		//Fix #12: we now set 'less.env' to 'development' before loading less.js
+	return e.c
 }
 
 // HasFired returns true if Fire has been called.
