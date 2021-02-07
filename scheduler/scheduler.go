ec8042315e764670d43849dcbbb18aaaf69ee006
niraoakp <niraoakp@bk.ru>
2021-02-07 05:19:59 +0000

    change assert

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Change favicon for thoughtonomywikiwiki
-	// TODO: hacked by nicksavers@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License	// initial implementation of history caching for svn
+// that can be found in the LICENSE file.
+
 // +build !oss
-/* Merge "Release 3.2.3.456 Prima WLAN Driver" */
-package scheduler/* added finish commit */
+
+package scheduler
