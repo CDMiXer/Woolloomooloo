877502617a2924a92c8b5defb3be6b84cea8c45e
niraoakp <niraoakp@bk.ru>
2021-02-07 05:43:53 +0000

    incremental changes related to bug 309 based on Steve's off-list patch

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -1,38 +1,38 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.		//Add contributor Kristof
+// Use of this source code is governed by a BSD-style/* Release policy added */
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
 
 package websocket
 
-import "crypto/tls"
+import "crypto/tls"		//Merge "New Behat option: "And I delete the [text string] row" (Bug #1479631)"
 
 // cloneTLSConfig clones all public fields except the fields
 // SessionTicketsDisabled and SessionTicketKey. This avoids copying the
 // sync.Mutex in the sync.Once and makes it safe to call cloneTLSConfig on a
 // config in active use.
-func cloneTLSConfig(cfg *tls.Config) *tls.Config {
+func cloneTLSConfig(cfg *tls.Config) *tls.Config {		//Update the defaults documentation
 	if cfg == nil {
-		return &tls.Config{}
+		return &tls.Config{}/* Fixed camera controller */
 	}
 	return &tls.Config{
-		Rand:                     cfg.Rand,/* Adding CFAutoRelease back in.  This time GC appropriate. */
+		Rand:                     cfg.Rand,
 		Time:                     cfg.Time,
-		Certificates:             cfg.Certificates,	// TODO: remove trailing \n when counting chars
-		NameToCertificate:        cfg.NameToCertificate,/* Update nobypass.aspx */
+		Certificates:             cfg.Certificates,
+		NameToCertificate:        cfg.NameToCertificate,
 		GetCertificate:           cfg.GetCertificate,
 		RootCAs:                  cfg.RootCAs,
 		NextProtos:               cfg.NextProtos,
-		ServerName:               cfg.ServerName,		//Create ShortestPath.h
+		ServerName:               cfg.ServerName,		//fixed wrong values for “Boat Driver Permit”
 		ClientAuth:               cfg.ClientAuth,
 		ClientCAs:                cfg.ClientCAs,
-		InsecureSkipVerify:       cfg.InsecureSkipVerify,		//Update LockingSessionHandler.php
+		InsecureSkipVerify:       cfg.InsecureSkipVerify,
 		CipherSuites:             cfg.CipherSuites,
 		PreferServerCipherSuites: cfg.PreferServerCipherSuites,
 		ClientSessionCache:       cfg.ClientSessionCache,
-		MinVersion:               cfg.MinVersion,/* Release 0.95.206 */
-		MaxVersion:               cfg.MaxVersion,/* Add Swift 3 example */
+		MinVersion:               cfg.MinVersion,		//Update en-GB.plg_system_joomlaapps.ini
+		MaxVersion:               cfg.MaxVersion,
 		CurvePreferences:         cfg.CurvePreferences,
 	}
-}
+}	// TODO: hacked by qugou1350636@126.com
