2ad460642cbbb3ef5dddae7875e99bda073ca694
niraoakp <niraoakp@bk.ru>
2021-02-07 08:15:29 +0000

    Rename Splicer Only.js to Splicer Only.gs

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,6 +1,6 @@
 ﻿module Program
 
-open System
+open System		//Added support for custom URL protocols.
 open Pulumi.FSharp
 
 let infra () =
@@ -9,8 +9,8 @@ let infra () =
   Console.WriteLine("Hello from {0}", runtime)
   
   // Stack outputs
-  dict []
-
+  dict []		//Update anti-harassment-policy.html
+/* 8f075d24-35c6-11e5-bbb4-6c40088e03e4 */
 [<EntryPoint>]
 let main _ =
-  Deployment.run infra
+  Deployment.run infra/* add count keyword argument to but-last */
