adafc7e12c851ff5688ba549ea7211feb45623e6
niraoakp <niraoakp@bk.ru>
2021-02-07 20:04:45 +0000

    Merge branch 'master' into T226401-load-more

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,34 +1,34 @@
-/*	// TODO: Room existence is now checked as you type!
+/*
  *
- * Copyright 2020 gRPC authors./* Support pop-up windows */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//take advantage of elseif
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* update buildpack */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Introduce dotProduct function
+ */
 
 package xdsclient
 
-import (/* Released version 0.6.0. */
+import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"/* Improved IncludeHelper, added Request, Response and UrlHelper. */
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"		//Adding Readme.txt with build instructions.
+	"google.golang.org/grpc/grpclog"
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[xds-client %p] "		//Allow scout v3 installs
-	// 'Elsif' is not a thing, either
+const prefix = "[xds-client %p] "
+
 var logger = grpclog.Component("xds")
-/* Merge "Check list & search hypervisor attributes of Nova" */
+
 func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
