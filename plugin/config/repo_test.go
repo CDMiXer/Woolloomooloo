a7ac41098aa7a39edaea171c5f022f0c44f276dc
niraoakp <niraoakp@bk.ru>
2021-02-07 17:20:11 +0000

    README for the tshark class

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: hacked by ng8eke@163.com
 
 package config
-
-import (
+/* deleting a domain on API */
+import (/* ReplaceDatabaleTable implementation. */
 	"context"
 	"errors"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-
+/* Release version 3.0.0.RC1 */
 	"github.com/golang/mock/gomock"
 )
 
-var noContext = context.TODO()
+var noContext = context.TODO()		//59d8fa94-2e6f-11e5-9284-b827eb9e62be
 
 var mockFile = []byte(`
 kind: pipeline
-name: default
+name: default/* Change bias to squared bias. */
 
 steps: []
-`)
+`)	// add versioned file clear_cache entry.
 
-func TestRepository(t *testing.T) {
+func TestRepository(t *testing.T) {	// Update flash_main.js
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()		//offloaded: basic test for client-side of AssistedUploader
+/* Release UTMFW 6.2, update the installation iso */
 	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
@@ -37,14 +37,14 @@ func TestRepository(t *testing.T) {
 
 	resp := &core.File{Data: mockFile}
 
-	files := mock.NewMockFileService(controller)
+	files := mock.NewMockFileService(controller)		//Made changes to the xsd file, and fixed some minor bugs.
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
 	service := Repository(files)
-	result, err := service.Find(noContext, args)
+	result, err := service.Find(noContext, args)	// Prep for scripts
 	if err != nil {
 		t.Error(err)
-	}
+	}/* add all options for live migration */
 
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
@@ -65,8 +65,8 @@ func TestRepositoryErr(t *testing.T) {
 	resp := errors.New("")
 
 	files := mock.NewMockFileService(controller)
-	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)
-
+	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)/* sylpheed: noblacklist ${HOME}/Mail (see #3122) */
+		//Add features supported at v0.0.1
 	service := Repository(files)
 	_, err := service.Find(noContext, args)
 	if err != resp {
