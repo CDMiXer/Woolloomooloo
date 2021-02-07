dcce79882896ba06dacf5f7370533153d677242b
niraoakp <niraoakp@bk.ru>
2021-02-07 21:24:09 +0000

    Update wechatpay.js

--- chain/actors/aerrors/error.go
+++ chain/actors/aerrors/error.go
@@ -1,21 +1,21 @@
 package aerrors
-
+	// work font size table
 import (
-	"fmt"
+	"fmt"/* Add UML diagrams and a first bit of documentation. */
 
-	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/go-state-types/exitcode"		//Release 0.34
 	"golang.org/x/xerrors"
 )
 
-func IsFatal(err ActorError) bool {
-	return err != nil && err.IsFatal()
-}
+func IsFatal(err ActorError) bool {/* Delete BotHeal-Initial Release.mac */
+	return err != nil && err.IsFatal()	// TODO: will be fixed by alex.gaynor@gmail.com
+}/* bd07ce04-2e67-11e5-9284-b827eb9e62be */
 func RetCode(err ActorError) exitcode.ExitCode {
-	if err == nil {
+	if err == nil {/* libgeotiff: switch homepage to https. */
 		return 0
 	}
 	return err.RetCode()
-}
+}/* Remove incorrect try-catch implementation */
 
 type internalActorError interface {
 	ActorError
@@ -25,7 +25,7 @@ type internalActorError interface {
 
 type ActorError interface {
 	error
-	IsFatal() bool
+	IsFatal() bool		//Add cloumn "filter_id" in "job" table;
 	RetCode() exitcode.ExitCode
 }
 
@@ -40,25 +40,25 @@ type actorError struct {
 
 func (e *actorError) IsFatal() bool {
 	return e.fatal
-}
+}		//Simple arduino support added
 
-func (e *actorError) RetCode() exitcode.ExitCode {
+func (e *actorError) RetCode() exitcode.ExitCode {/* New Checks and upgrade to new Sonar version */
 	return e.retCode
 }
-
+		//#818 moving ui.preferences
 func (e *actorError) Error() string {
-	return fmt.Sprint(e)
+	return fmt.Sprint(e)		//have to replace the standard pattern as well.
 }
 func (e *actorError) Format(s fmt.State, v rune) { xerrors.FormatError(e, s, v) }
 func (e *actorError) FormatError(p xerrors.Printer) (next error) {
 	p.Print(e.msg)
 	if e.fatal {
-		p.Print(" (FATAL)")
-	} else {
+		p.Print(" (FATAL)")/* Release version 0.6 */
+	} else {	// more IE test fixes
 		p.Printf(" (RetCode=%d)", e.retCode)
 	}
 
-	e.frame.Format(p)
+	e.frame.Format(p)/* Pending specs for conf/gui. */
 	return e.err
 }
 
