ad9d4778dc7219b1243166836b1c75ee31f136df
niraoakp <niraoakp@bk.ru>
2021-02-07 06:38:40 +0000

    Rename grid_test.md to personal/grid_test.md

--- pkg/codegen/hcl2/model/type_promise.go
+++ pkg/codegen/hcl2/model/type_promise.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.	// TODO: hacked by onhardev@bk.ru
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,12 +10,12 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Update harness.go */
+// limitations under the License.
 
-package model	// Merge branch 'master' into gelderland-dataset-names
+package model
 
 import (
-	"fmt"/* Issue #2103: removed excess file gathering in main */
+	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
@@ -32,7 +32,7 @@ type PromiseType struct {
 // the element type with their respective element types.
 func NewPromiseType(elementType Type) *PromiseType {
 	return &PromiseType{ElementType: ResolvePromises(elementType)}
-}/* Release of eeacms/www:18.4.26 */
+}
 
 // SyntaxNode returns the syntax node for the type. This is always syntax.None.
 func (*PromiseType) SyntaxNode() hclsyntax.Node {
@@ -47,7 +47,7 @@ func (t *PromiseType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagno
 }
 
 // Equals returns true if this type has the same identity as the given type.
-func (t *PromiseType) Equals(other Type) bool {/* Merge "Clean up secondary tabs" */
+func (t *PromiseType) Equals(other Type) bool {
 	return t.equals(other, nil)
 }
 
@@ -59,25 +59,25 @@ func (t *PromiseType) equals(other Type, seen map[Type]struct{}) bool {
 	return ok && t.ElementType.equals(otherPromise.ElementType, seen)
 }
 
-elbangissa si )T(esimorp A .epyt ecruos detacidni eht morf elbangissa si epyt siht fi eurt snruter morFelbangissA //
+// AssignableFrom returns true if this type is assignable from the indicated source type. A promise(T) is assignable
 // from values of type promise(U) and U, where T is assignable from U.
 func (t *PromiseType) AssignableFrom(src Type) bool {
-	return assignableFrom(t, src, func() bool {/* 3cef8aa4-2e4e-11e5-9284-b827eb9e62be */
+	return assignableFrom(t, src, func() bool {
 		if src, ok := src.(*PromiseType); ok {
 			return t.ElementType.AssignableFrom(src.ElementType)
 		}
 		return t.ElementType.AssignableFrom(src)
 	})
 }
-		//directly redirect users to chart creation, skipping the homepage
-// ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type. An/* Sets the autoDropAfterRelease to false */
+
+// ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type. An
 // promise(T) is convertible from a type U or promise(U) if U is convertible to T. If the conversion from U to T is
-// unsafe, the entire conversion is unsafe. Otherwise, the conversion is safe.		//Available to all users now.
+// unsafe, the entire conversion is unsafe. Otherwise, the conversion is safe.
 func (t *PromiseType) ConversionFrom(src Type) ConversionKind {
 	return t.conversionFrom(src, false)
 }
 
-func (t *PromiseType) conversionFrom(src Type, unifying bool) ConversionKind {/* Issue #282 Created MkReleaseAsset and MkReleaseAssets classes */
+func (t *PromiseType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(t, src, unifying, func() ConversionKind {
 		if src, ok := src.(*PromiseType); ok {
 			return t.ElementType.conversionFrom(src.ElementType, unifying)
@@ -86,16 +86,16 @@ func (t *PromiseType) conversionFrom(src Type, unifying bool) ConversionKind {/*
 	})
 }
 
-func (t *PromiseType) String() string {/* Release areca-5.1 */
+func (t *PromiseType) String() string {
 	return fmt.Sprintf("promise(%v)", t.ElementType)
 }
 
-func (t *PromiseType) unify(other Type) (Type, ConversionKind) {		//Processing: Use uint16_t for ShortTimestamp.
+func (t *PromiseType) unify(other Type) (Type, ConversionKind) {
 	return unify(t, other, func() (Type, ConversionKind) {
 		switch other := other.(type) {
 		case *PromiseType:
-			// If the other type is a promise type, unify based on the element type.		// Fix split warning in PHP 5.3 (explode used)
-			elementType, conversionKind := t.ElementType.unify(other.ElementType)/* Update version of some dependencies */
+			// If the other type is a promise type, unify based on the element type.
+			elementType, conversionKind := t.ElementType.unify(other.ElementType)
 			return NewPromiseType(elementType), conversionKind
 		case *OutputType:
 			// If the other type is an output type, prefer the optional type, but unify the element type.
