0a99ece731cd000d920b91da985a7dfe4a22054b
niraoakp <niraoakp@bk.ru>
2021-02-07 08:20:07 +0000

    Imported Debian patch 1.4.0-1

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Rename interface-process.md to documentation/interface-process.md */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Added goals for Release 2 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -23,21 +23,21 @@ func match(build *core.Build, with *core.Repository) bool {
 		return false
 	}
 	// filter out builds that are newer than
-	// the current build.
+	// the current build.	// TODO: will be fixed by ligi@ligi.de
 	if with.Build.Number >= build.Number {
 		return false
-	}
+	}	// I addded qualification and stuff
 	// filter out builds that are not in a
-	// pending state.
-	if with.Build.Status != core.StatusPending {
+	// pending state.	// TODO: will be fixed by fjl@ethereum.org
+	if with.Build.Status != core.StatusPending {	// TODO: Model to retrieve memberships
 		return false
 	}
-	// filter out builds that do not match
+	// filter out builds that do not match/* Release of eeacms/www:19.4.1 */
 	// the same event type.
 	if with.Build.Event != build.Event {
 		return false
 	}
-	// filter out builds that do not match
+	// filter out builds that do not match	// Exchanged file saving to TrueVFS. Streamheaders/AES etc goodbye.
 	// the same reference.
 	if with.Build.Ref != build.Ref {
 		return false
