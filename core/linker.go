e252ca8b772469fc4631cdc6c6d9a08c476b169c
niraoakp <niraoakp@bk.ru>
2021-02-07 06:19:38 +0000

    Fixed typo: persistentIds

--- core/linker.go
+++ core/linker.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Tests are now all run by modbuild.xml
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,14 +9,14 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and	// Missed one space - removing it
+// limitations under the License.	// TODO: hacked by cory@protocol.ai
 
 package core
 
 import "context"
 
-// Linker provides a deep link to to a git resource in the
+// Linker provides a deep link to to a git resource in the		//updated display options description
 // source control management system for a given build.
 type Linker interface {
 	Link(ctx context.Context, repo, ref, sha string) (string, error)
