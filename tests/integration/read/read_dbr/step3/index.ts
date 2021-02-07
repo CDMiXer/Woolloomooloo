e1a546a3a94ad540a34599310fea2f51e337ea2b
niraoakp <niraoakp@bk.ru>
2021-02-07 00:42:42 +0000

    Automatic changelog generation for PR #2783 [ci skip]

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Update geo nameservers - Print out multiple nameservers */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Release Japanese networking guide" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release Notes for v00-06 */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Delete AddPhoneNumber.cshtml
+// Unless required by applicable law or agreed to in writing, software/* Merge "Prep. Release 14.02.00" into RB14.02 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of eeacms/eprtr-frontend:2.0.5 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );/* @Release [io7m-jcanephora-0.29.4] */
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
 // C does not show up in the plan, so it is deleted from the snapshot.
