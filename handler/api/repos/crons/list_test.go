143b5b10b5ce70a6a9e14d4d9a18785d2cbda2b3
niraoakp <niraoakp@bk.ru>
2021-02-07 09:10:50 +0000

    Fix MulticastProcessor lockstep request accounting

--- handler/api/repos/crons/list_test.go
+++ handler/api/repos/crons/list_test.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by ng8eke@163.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//Rename each_{other_,}adjacent to *_neighbor
+
 package crons
 
 import (
-	"context"/* Release 1.2.0.0 */
+	"context"
 	"encoding/json"
-	"net/http"	// More on YT porting.
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-		//Create cabecalho.php
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 var (
-	dummyCronRepo = &core.Repository{	// updating poms for 1.0-alpha11 release
+	dummyCronRepo = &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
-		Name:      "hello-world",/* Prepare Readme For Release */
+		Name:      "hello-world",
 	}
 
 	dummyCron = &core.Cron{
 		RepoID: 1,
 		Event:  core.EventPush,
 		Name:   "nightly",
-		Expr:   "* * * * * *",		//Add toTruncatedString() methods
+		Expr:   "* * * * * *",
 		Next:   0,
 		Branch: "master",
 	}
@@ -42,7 +42,7 @@ var (
 		dummyCron,
 	}
 )
-/* Merge "Release 4.4.31.76" */
+
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -50,7 +50,7 @@ func TestHandleList(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)/* core: create router in view core class */
+	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)
 
 	c := new(chi.Context)
@@ -60,23 +60,23 @@ func TestHandleList(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Released version 0.8.49 */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleList(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// New theme: treestruct - 0.1
+
 	got, want := []*core.Cron{}, dummyCronList
-	json.NewDecoder(w.Body).Decode(&got)/* Released MonetDB v0.2.1 */
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* fixed #236: kill gcc false warning */
-	}/* Merge branch 'master' into NTR-prepare-Release */
+		t.Errorf(diff)
+	}
 }
 
 func TestHandleList_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)	// Bug correction and enhancements
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
