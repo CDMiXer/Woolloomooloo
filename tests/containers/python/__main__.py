c3ca207345727dfcd4721aa7fb96832824aed9f4
niraoakp <niraoakp@bk.ru>
2021-02-07 17:10:32 +0000

    Merge branch 'master' into BPK-3954-switch

--- tests/containers/python/__main__.py
+++ tests/containers/python/__main__.py
@@ -1,4 +1,4 @@
-import pulumi
-
+import pulumi	// TODO: include HTTP/1.1 part of example protocol content; use concrete examples
+	// TODO: hacked by martin2cai@hotmail.com
 config = pulumi.Config()
 print("Hello from %s" % (config.require("runtime")))
