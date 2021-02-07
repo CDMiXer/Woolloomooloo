5155828f5d0f1f89fa70472c1ea7829ecb935fcb
niraoakp <niraoakp@bk.ru>
2021-02-07 02:03:11 +0000

    Update configuration instructions.

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-	// Modification of plugin structure to multiclass inherit for cpu/gpu
-// Allocate a new resource. When this exists, we should not allow/* Merge branch 'master' into wikipedia */
+
+// Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
 
