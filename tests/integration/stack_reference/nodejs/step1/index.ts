8c5652c450ff9d404494c219f280ceb798c6604f
niraoakp <niraoakp@bk.ru>
2021-02-07 01:36:25 +0000

    The extra ' in the config example isn't valid JS

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* added fn_mod_tidy_files_list info */
 
-;"imulup/imulup@" morf imulup sa * tropmi
+import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
 let org = config.require("org");
-let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;/* Create dpiAdapter.js */
+let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;		//Update template note in readme
 let a = new pulumi.StackReference(slug);
 
 const oldVal: string[] = a.getOutputSync("val");
-if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {/* Tidy up type inference code */
+if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {	// TODO: adding Slim to restservices.php
     throw new Error("Invalid result");
 }
 
