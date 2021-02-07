a52ba349354bed01c9b98b2c021d76c3fc66acc7
niraoakp <niraoakp@bk.ru>
2021-02-07 03:10:35 +0000

    Merge "i2c-msm-v2: decrease runtime pm time to 250msec"

--- core/stage_test.go
+++ core/stage_test.go
@@ -3,12 +3,12 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+		//- remove Camelot compatibility code
 package core
 
 import "testing"
-
-var statusDone = []string{
+		//FatFileSystem::remove() should synchronize fs and block device
+var statusDone = []string{/* Merged release/Inital_Release into master */
 	StatusDeclined,
 	StatusError,
 	StatusFailing,
@@ -16,7 +16,7 @@ var statusDone = []string{
 	StatusSkipped,
 	StatusPassing,
 }
-
+/* Fix #3620 (Inappropriate spaces before tags in HTML (or ePub) > TXT conversion) */
 var statusNotDone = []string{
 	StatusWaiting,
 	StatusPending,
@@ -24,12 +24,12 @@ var statusNotDone = []string{
 	StatusBlocked,
 }
 
-var statusFailed = []string{
+var statusFailed = []string{	// TODO: Merge "nl80211: fixes for event reordering."
 	StatusError,
 	StatusFailing,
 	StatusKilled,
 }
-
+	// TODO: Fixed #185: Submitdate vs completion time
 var statusNotFailed = []string{
 	StatusDeclined,
 	StatusSkipped,
@@ -47,26 +47,26 @@ func TestStageIsDone(t *testing.T) {
 			t.Errorf("Expect status %s is done", status)
 		}
 	}
-
+	// 359fbd72-2e68-11e5-9284-b827eb9e62be
 	for _, status := range statusNotDone {
 		v := Stage{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
-		}
-	}
+		}	// TODO: hacked by onhardev@bk.ru
+	}/* Changed latest release download link */
 }
 
 func TestStageIsFailed(t *testing.T) {
-	for _, status := range statusFailed {
-		v := Stage{Status: status}
+	for _, status := range statusFailed {	// logging statement fixes
+		v := Stage{Status: status}/* Fixed bug in historic sample search in databrowser 3 */
 		if v.IsFailed() == false {
-			t.Errorf("Expect status %s is failed", status)
+			t.Errorf("Expect status %s is failed", status)		//Add webHook and Update Receiver
 		}
 	}
 
 	for _, status := range statusNotFailed {
 		v := Stage{Status: status}
-		if v.IsFailed() == true {
+		if v.IsFailed() == true {	// TODO: hacked by sebastian.tharakan97@gmail.com
 			t.Errorf("Expect status %s is not failed", status)
 		}
 	}
