46e130615bdde51369eaebf082d5307058f9e2eb
niraoakp <niraoakp@bk.ru>
2021-02-07 10:44:56 +0000

    Release of eeacms/forests-frontend:1.8

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore	// TODO: hacked by sebastian.tharakan97@gmail.com
+// Package blockstore and subpackages contain most of the blockstore
 // implementations used by Lotus.
 //
 // Blockstores not ultimately constructed out of the building blocks in this
 // package may not work properly.
-//		//Use transform for up case conversion.
-// This package re-exports parts of the go-ipfs-blockstore package such that		//Adding rlite (a light-weight router)
+//
+// This package re-exports parts of the go-ipfs-blockstore package such that/* Release: Making ready for next release iteration 6.7.2 */
 // no other package needs to import it directly, for ergonomics and traceability.
-package blockstore/* remove migrations cause its stored in the menu array #132 */
+package blockstore
