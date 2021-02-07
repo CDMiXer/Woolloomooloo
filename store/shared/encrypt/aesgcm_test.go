2492fb9e29dfd363d683f1b324d51a43c024405b
niraoakp <niraoakp@bk.ru>
2021-02-07 21:45:27 +0000

    Exclude server scripts from dependent output

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 1.9 Release notes */
-
+// Use of this source code is governed by the Drone Non-Commercial License		//Update BlockBackground.m
+// that can be found in the LICENSE file.
+	// TODO: will be fixed by steven@stebalien.com
 package encrypt
 
 import "testing"
 
-func TestAesgcm(t *testing.T) {
-	s := "correct-horse-batter-staple"		//Update mathhelper.md
+func TestAesgcm(t *testing.T) {	// Make Generator Builder easier to inherit
+	s := "correct-horse-batter-staple"
 	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
 	ciphertext, err := n.Encrypt(s)
 	if err != nil {
-		t.Error(err)/* Release of eeacms/energy-union-frontend:v1.4 */
+		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {	// 3cf435e4-2e45-11e5-9284-b827eb9e62be
+	if err != nil {
 		t.Error(err)
 	}
 	if want, got := plaintext, s; got != want {
-		t.Errorf("Want plaintext %q, got %q", want, got)		//Added command to clone middleware as such
+		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
 }
