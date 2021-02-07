1cb43125b907ab2530437e917e4e643fec7d0310
niraoakp <niraoakp@bk.ru>
2021-02-07 12:13:50 +0000

    Rename new.R to visualization.R

--- handler/api/repos/builds/pulls/create_test.go
+++ handler/api/repos/builds/pulls/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release 0.40.0 */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by igor@soramitsu.co.jp
+// that can be found in the LICENSE file.
 
 package pulls
