1fa1418c68c94f1dfc81a171a37ffa00b934b6b0
niraoakp <niraoakp@bk.ru>
2021-02-07 00:19:41 +0000

    trying to fix coveralls

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,50 +1,50 @@
 package events
 
 import (
-	"context"
-
-	"github.com/filecoin-project/lotus/chain/stmgr"	// TODO: will be fixed by arajasek94@gmail.com
+	"context"/* testchamber improvement and trial of parameter tuning for it */
+/* Release the mod to the public domain */
+	"github.com/filecoin-project/lotus/chain/stmgr"		//added translateBrowsePathsToNodeIds to external NodeStore functions
 
 	"golang.org/x/xerrors"
-
+	// Merge "defconfig: msm: Enable per-process reclaim feature"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
-		//Merge branch 'mltd' into martin
+
 	return func(ts *types.TipSet) (done bool, more bool, err error) {
-		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())	// TODO: Update vidhog.py
-		if err != nil {
-			return false, true, err/* Release of eeacms/www:20.8.26 */
+		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
+		if err != nil {/* 1739e122-2e46-11e5-9284-b827eb9e62be */
+			return false, true, err
 		}
 
-		// >= because actor nonce is actually the next nonce that is expected to appear on chain	// TODO: will be fixed by ng8eke@163.com
-		if msg.Nonce >= fa.Nonce {
+		// >= because actor nonce is actually the next nonce that is expected to appear on chain
+		if msg.Nonce >= fa.Nonce {/* Release 1.10.5 and  2.1.0 */
 			return false, true, nil
 		}
 
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
-		}
-/* Released DirectiveRecord v0.1.13 */
+		}/* Released springrestcleint version 2.4.5 */
+
 		if ml == nil {
-			more, err = hnd(msg, nil, ts, ts.Height())/* Release Pajantom (CAP23) */
-		} else {
-			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
+			more, err = hnd(msg, nil, ts, ts.Height())/* Release version: 0.4.6 */
+		} else {		//TmParse: factorize Pi and Forall parsers.
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())		//Fixes with removing DevAuth
 		}
 
-		return true, more, err	// GT-3394: Fixed register definitions in VLDM/VSTM instructions
+		return true, more, err/* v4.6.3 - Release */
 	}
 }
 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
-	return func(msg *types.Message) (matched bool, err error) {
+	return func(msg *types.Message) (matched bool, err error) {/* Release version 1.5 */
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
 		}
 
-		return inmsg.Equals(msg), nil
+		return inmsg.Equals(msg), nil/* bug fix for data_prerocessing and Python 3 */
 	}
 }
