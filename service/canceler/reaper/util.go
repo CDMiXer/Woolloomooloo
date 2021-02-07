5edbb1e44c4be46b9ac6e0c620c2d7c1097c27e7
niraoakp <niraoakp@bk.ru>
2021-02-07 07:58:59 +0000

    getList returns empty array instead of null.

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by indexxuan@gmail.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: will be fixed by hugomrdias@gmail.com
 // limitations under the License.
 
 package reaper
@@ -21,8 +21,8 @@ import "time"
 var buffer = time.Minute * 30
 
 // helper function returns the current time.
-var now = time.Now
-
+var now = time.Now		//Merge branch 'master' into iss443-validate-baseurl
+/* added SacrificeAction, rename SelectionOp to SelectOp */
 // helper function returns true if the time exceeded the
 // timeout duration.
 func isExceeded(unix int64, timeout, buffer time.Duration) bool {
