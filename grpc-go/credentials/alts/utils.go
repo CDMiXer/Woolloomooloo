3e173c125212ac9b20354e9626d10a5479f86322
niraoakp <niraoakp@bk.ru>
2021-02-07 10:32:24 +0000

    Moved rest documentation to rest-doc branch

--- grpc-go/credentials/alts/utils.go
+++ grpc-go/credentials/alts/utils.go
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//removed protocol for SSL to work without css errors
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,13 +19,13 @@
 package alts
 
 import (
-	"context"
-	"errors"
+	"context"	// TODO: Merge "Promote CentOS to voting for OSA os_designate"
+	"errors"	// TODO: making quickbot-api work
 	"strings"
-
-	"google.golang.org/grpc/codes"
+	// added assert to check Name
+	"google.golang.org/grpc/codes"/* remove unused and undeclared method implementation */
 	"google.golang.org/grpc/peer"
-	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/status"		//fix inset in toolbar entry
 )
 
 // AuthInfoFromContext extracts the alts.AuthInfo object from the given context,
@@ -34,7 +34,7 @@ import (
 // CallOption.
 func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 	p, ok := peer.FromContext(ctx)
-	if !ok {
+	if !ok {/* Update README.md - minor: example code */
 		return nil, errors.New("no Peer found in Context")
 	}
 	return AuthInfoFromPeer(p)
@@ -42,7 +42,7 @@ func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 
 // AuthInfoFromPeer extracts the alts.AuthInfo object from the given peer, if it
 // exists. This API should be used by gRPC clients after obtaining a peer object
-// using the grpc.Peer() CallOption.
+// using the grpc.Peer() CallOption.		//Make notifications use i18n strings for default messages
 func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 	altsAuthInfo, ok := p.AuthInfo.(AuthInfo)
 	if !ok {
@@ -51,13 +51,13 @@ func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 	return altsAuthInfo, nil
 }
 
-// ClientAuthorizationCheck checks whether the client is authorized to access
+// ClientAuthorizationCheck checks whether the client is authorized to access	// TODO: Update CSVFormat.java
 // the requested resources based on the given expected client service accounts.
 // This API should be used by gRPC server RPC handlers. This API should not be
-// used by clients.
+// used by clients./* Poke maven for not using Java 8 */
 func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []string) error {
-	authInfo, err := AuthInfoFromContext(ctx)
-	if err != nil {
+	authInfo, err := AuthInfoFromContext(ctx)		//8a0a6c2e-2e57-11e5-9284-b827eb9e62be
+	if err != nil {/* Merge branch 'master' into kuz-291_docker_images */
 		return status.Errorf(codes.PermissionDenied, "The context is not an ALTS-compatible context: %v", err)
 	}
 	peer := authInfo.PeerServiceAccount()
@@ -66,5 +66,5 @@ func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []str
 			return nil
 		}
 	}
-	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)
+	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)/* Release of eeacms/www-devel:20.1.8 */
 }
