f718ad011f196b4b0e3fbfd34be21403a5342da4
niraoakp <niraoakp@bk.ru>
2021-02-07 12:37:59 +0000

    Imported Upstream version 5.01

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing/* OF-1182 remove Release News, expand Blog */
+package sealing
 
 import (
-	"io"
-		//Added note about uninstalling previous versions of MVC
+	"io"	// Fix 404 location
+
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
 type NullReader struct {
-	*io.LimitedReader	// Merge "[DM] Skiping an erroneous UT case which was causing DM build failure"
+	*io.LimitedReader
 }
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
-}
-
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {	// TODO: Last chains added and translated
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}/* [Tests] ensure `node` `v0.8` tests stay passing. */
+}/* PDB no longer gets generated when compiling OSOM Incident Source Release */
+		//update 2geom (rev. 1569)
 func (m NullReader) NullBytes() int64 {
 	return m.N
 }
