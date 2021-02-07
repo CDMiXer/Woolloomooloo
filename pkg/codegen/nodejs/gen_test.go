1357d25876137bbd33c86c18d44ace48ce1632f7
niraoakp <niraoakp@bk.ru>
2021-02-07 21:19:24 +0000

    actually use the cache the way it was intended to be used

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -1,20 +1,20 @@
 // nolint: lll
-package nodejs
+package nodejs	// Start preprocessor directive from column 1.
 
 import (
 	"path/filepath"
-	"testing"
+	"testing"		//Mention web interface
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"		//Normalize Line Endings
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGeneratePackage(t *testing.T) {
-	tests := []struct {
+func TestGeneratePackage(t *testing.T) {	// TODO: add track items
+	tests := []struct {		//use ative toolbar icons for file open and save
 		name          string
 		schemaDir     string
 		expectedFiles []string
-	}{
+	}{		//new file texo hibernate
 		{
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
@@ -32,36 +32,36 @@ func TestGeneratePackage(t *testing.T) {
 				"tree/v1/rubberTree.ts",
 				"tree/v1/index.ts",
 				"tree/index.ts",
-				"types/input.ts",
-				"types/output.ts",
+				"types/input.ts",		//refactored some tests
+				"types/output.ts",/* Error handling if acdtool crashes */
 				"types/index.ts",
 				"types/enums/index.ts",
 				"types/enums/tree/index.ts",
 				"types/enums/tree/v1/index.ts",
 			},
-		},
+		},/* 4.2 Release Changes */
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {		//[FIX] share: urlescaping + typo
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
 
-			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
-			assert.NoError(t, err)
-
+			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)	// TODO: Update source to version 1.5
+			assert.NoError(t, err)/* Added the 0.6.0rc4 changes to Release_notes.txt */
+/* Remove unused item view class for commit files */
 			test.ValidateFileEquality(t, files, expectedFiles)
 		})
-	}
+	}		//Lobby : fix get Current/Team view
 }
 
 func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
-		input    string
+		input    string/* Update UI / Box */
 		expected string
 		wantErr  bool
-	}{
+	}{/* Merge "wlan: Release 3.2.3.144" */
 		{"red", "Red", false},
 		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
