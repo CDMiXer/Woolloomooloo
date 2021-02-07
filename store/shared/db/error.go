92b92eecaba4b117e43c29ebd70f0576933f10ab
niraoakp <niraoakp@bk.ru>
2021-02-07 16:45:58 +0000

    Add a better default for the output format

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -4,19 +4,19 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Add script for fetching metadata from audio file
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: fix #2542: NPE when deleting list with all caches in it
+///* Changing alias */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 2.12.1 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Fix small issues with label names and generation
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package db
 
 import "errors"
-	// Include the fact that the SDK has 1.0.4 and 1.1.1 runtimes
-// ErrOptimisticLock is returned by if the struct being
+		//Update commands and premissions description
+// ErrOptimisticLock is returned by if the struct being		//Merge #257 `Fix the eventsource server for CORS`
 // modified has a Version field and the value is not equal
-// to the current value in the database
-var ErrOptimisticLock = errors.New("Optimistic Lock Error")/* Release: 1.0 */
+// to the current value in the database/* classical conditioning examples */
+var ErrOptimisticLock = errors.New("Optimistic Lock Error")
