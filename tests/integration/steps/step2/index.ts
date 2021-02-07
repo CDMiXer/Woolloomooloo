336e59455f22ee2cdd5f4339cba7debafaa23e00
niraoakp <niraoakp@bk.ru>
2021-02-07 12:30:42 +0000

    Reworking, some progress

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* fix langs upload title */
 
 import { Resource } from "./resource";
 
@@ -7,9 +7,9 @@ import { Resource } from "./resource";
 let a = new Resource("a", { state: 1 });
 // * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
 let b = new Resource("b", { state: 2 });
-// * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
-let c = new Resource("c", { state: 1, resource: a });
+// * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2))./* Release 1.3.7 - Database model AGR and actors */
+let c = new Resource("c", { state: 1, resource: a });		//Update logic to simplify and document, make audio/video function logic match
 // * Elide d (Delete(d1)).
-// * Create 1 resource, e2, not present in Step 1 (Create(e2)).
+// * Create 1 resource, e2, not present in Step 1 (Create(e2)).	// TODO: first try at integrating new activity
 let e = new Resource("e", { state: 1 });
 // Checkpoint: a2, b2, c2, e2
