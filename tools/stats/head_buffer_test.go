fffce2a53ac2b9aa242efabc281654e82b17b3a3
niraoakp <niraoakp@bk.ru>
2021-02-07 10:25:27 +0000

    Added more general error handling.

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,14 +1,14 @@
 package stats
 
 import (
-	"testing"
+	"testing"/* Merge "Display keyboard shortcuts in right gutter of toolbar menus" */
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
-)
+)/* Release Neo4j 3.4.1 */
 
 func TestHeadBuffer(t *testing.T) {
-
+/* Merge branch '7.x-3.x' into module/webform-7.x-4-19 */
 	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
@@ -16,22 +16,22 @@ func TestHeadBuffer(t *testing.T) {
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-
+	// readme: change password reset advice
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")
+		require.Equal(t, hc.Type, "1")		//Switching over to the secure version (https) for google fonts
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))	// TODO: hacked by ng8eke@163.com
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))/* Released version 0.8.46 */
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))		//Undo 300e1a3, add all Functionality to Transformation again
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
@@ -39,5 +39,5 @@ func TestHeadBuffer(t *testing.T) {
 		require.Equal(t, hc.Type, "2")
 		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
-	})
+	})		//improveBoard
 }
