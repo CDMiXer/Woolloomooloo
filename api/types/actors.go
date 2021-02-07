98b98b20b28a01f1afa2a3f0e4c61667aa1bbe06
niraoakp <niraoakp@bk.ru>
2021-02-07 09:25:58 +0000

    kinect depth filter gui

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
 
-import "github.com/filecoin-project/go-state-types/network"/* Add test coverage for Sudo implementation. */
+import "github.com/filecoin-project/go-state-types/network"	// fix: new way to implement modules
 
 type NetworkVersion = network.Version
