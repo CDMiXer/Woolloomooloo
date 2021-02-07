eb23580ac0ecf81a6b0f2cb2ba360d3d884588b2
niraoakp <niraoakp@bk.ru>
2021-02-07 10:59:47 +0000

    Merge branch 'master' into dev_27

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,31 +1,31 @@
-package genesis
+package genesis		//Merge "Improve `redfish` set-boot-device behaviour"
 
 import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
-/* Changed host list layout; refactoring */
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"
+	bstore "github.com/filecoin-project/lotus/blockstore"	// removing readOnly
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)	// TODO: Delete TempConv.java
 
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
 
 	cst := cbor.NewCborStore(bs)
 
-	statecid, err := cst.Put(context.TODO(), &st)
+	statecid, err := cst.Put(context.TODO(), &st)/* Added method for write page title to output */
 	if err != nil {
-		return nil, err/* Add simple watching to documentation */
-	}
-/* Merge branch 'master' into add_Mohamed_Gomaa */
+		return nil, err
+	}/* Delete kfctl_k8s_istio.yaml */
+
 	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
-	}
+	}/* Expired passwords: Release strings for translation */
 
 	return act, nil
-}	// TODO: hacked by remco@dutchcoders.io
+}
