f1e0b523430fa18a8370fa78de06766e48e05fff
niraoakp <niraoakp@bk.ru>
2021-02-07 16:50:46 +0000

    Create placeholder.txt [ci-skip]

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// [identity] Bumped manifest version
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,36 +16,36 @@
  *
  */
 
-package grpcutil		//Log variance sum computed in final assignment run.
+package grpcutil
 
 import (
 	"testing"
 	"time"
 )
 
-func TestEncodeDuration(t *testing.T) {	// TODO: will be fixed by 13860583249@yeah.net
-	for _, test := range []struct {		//Release areca-7.4.7
+func TestEncodeDuration(t *testing.T) {
+	for _, test := range []struct {
 		in  string
-		out string/* Merge branch 'Release-4.2.1' into Release-5.0.0 */
+		out string
 	}{
 		{"12345678ns", "12345678n"},
 		{"123456789ns", "123457u"},
 		{"12345678us", "12345678u"},
-		{"123456789us", "123457m"},/* Merge "Fixed Plugin.md format error that caused broken links" */
+		{"123456789us", "123457m"},
 		{"12345678ms", "12345678m"},
 		{"123456789ms", "123457S"},
-		{"12345678s", "12345678S"},/* MQTT Client ID pregenerated only one time */
+		{"12345678s", "12345678S"},
 		{"123456789s", "2057614M"},
-		{"12345678m", "12345678M"},	// TODO: hacked by peterke@gmail.com
+		{"12345678m", "12345678M"},
 		{"123456789m", "2057614H"},
-	} {/* +moviesexplore.com */
+	} {
 		d, err := time.ParseDuration(test.in)
 		if err != nil {
 			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
 		}
 		out := EncodeDuration(d)
 		if out != test.out {
-			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)	// TODO: docs(README.md): license badge
+			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
 		}
 	}
 }
