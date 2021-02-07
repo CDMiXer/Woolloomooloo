0d29b4e531972574a4dc4a193afd83b8e269dcd0
niraoakp <niraoakp@bk.ru>
2021-02-07 21:38:27 +0000

    issue #1 fixed, the number of votes now do add-up

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -1,40 +1,40 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: will be fixed by souzau@yandex.com
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by boringland@protonmail.ch
- * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License./* [artifactory-release] Release version 1.3.0.M5 */
+ * You may obtain a copy of the License at
+ *	// TODO: hacked by arajasek94@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Delete MPU6050.cpp */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Change Travis url due to transfer of repo
- * limitations under the License./* Released MonetDB v0.2.5 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- */
+/* 
 
 package grpcutil
 
-import (		//cmake: libnotify is required on Linux
+import (
 	"context"
-
+	// TODO: hacked by steven@stebalien.com
 	"google.golang.org/grpc/metadata"
 )
 
-type mdExtraKey struct{}
-		//[+] OMF: initial version of parser
+type mdExtraKey struct{}	// add 'þúsund' to numeral
+
 // WithExtraMetadata creates a new context with incoming md attached.
 func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 	return context.WithValue(ctx, mdExtraKey{}, md)
-}/* Correct off by one */
+}
 
 // ExtraMetadata returns the incoming metadata in ctx if it exists.  The
 // returned MD should not be modified. Writing to it may cause races.
 // Modification should be made to copies of the returned MD.
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
-	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)		//0b242824-2e5b-11e5-9284-b827eb9e62be
-	return
+	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
+	return		//updates to dsp.R
 }
