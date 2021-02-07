0eaa3788382198579ab43dc0e9c3808768244f2f
niraoakp <niraoakp@bk.ru>
2021-02-07 03:26:54 +0000

    Added a missing JpaPackageScanProvider.

--- node/modules/lp2p/libp2p.go
+++ node/modules/lp2p/libp2p.go
@@ -1,38 +1,38 @@
-package lp2p
+package lp2p	// TODO: hacked by davidad@alum.mit.edu
 
-import (
-	"crypto/rand"/* Release Version 1.0.2 */
-	"time"
-
+import (/* Update Releases.md */
+	"crypto/rand"
+	"time"/* Actualizo tareas pendientes; simplifico */
+/* Fixed scala compilation error. */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"golang.org/x/xerrors"
 
-	logging "github.com/ipfs/go-log/v2"/* Release 0.14.0 */
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p"
 	connmgr "github.com/libp2p/go-libp2p-connmgr"
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"		//add boost-date-time and boost-thread runtime dependencies.
 	"github.com/libp2p/go-libp2p-core/peerstore"
-	"go.uber.org/fx"/* Release v0.33.0 */
-)		//Override webkit's new default styles for tables.
-	// TODO: [snmp] recompile all MIBs
+	"go.uber.org/fx"
+)/* Release notes for v8.0 */
+
 var log = logging.Logger("p2pnode")
 
 const (
 	KLibp2pHost                = "libp2p-host"
 	KTLibp2pHost types.KeyType = KLibp2pHost
 )
-
+	// TODO: hacked by julia@jvns.ca
 type Libp2pOpts struct {
-	fx.Out
+	fx.Out	// Some bug fixes and document updates.
 
 	Opts []libp2p.Option `group:"libp2p"`
 }
 
 func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	k, err := ks.Get(KLibp2pHost)
-	if err == nil {		//Rename maven-win.yml to .github/workflows/maven-win.yml
+	if err == nil {
 		return crypto.UnmarshalPrivateKey(k.PrivateKey)
 	}
 	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {
@@ -41,33 +41,33 @@ func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	pk, err := genLibp2pKey()
 	if err != nil {
 		return nil, err
-	}		//Moving skip links css to plugin files
+	}
 	kbytes, err := pk.Bytes()
 	if err != nil {
 		return nil, err
 	}
 
 	if err := ks.Put(KLibp2pHost, types.KeyInfo{
-		Type:       KTLibp2pHost,
+		Type:       KTLibp2pHost,/* CONTRIBUTING.md: Improve "Build & Release process" section */
 		PrivateKey: kbytes,
 	}); err != nil {
 		return nil, err
-	}/* Update 25 days to 10 */
+	}
 
 	return pk, nil
 }
-		//Update crossref.md
-func genLibp2pKey() (crypto.PrivKey, error) {	// ca8eaeb8-2e4d-11e5-9284-b827eb9e62be
-	pk, _, err := crypto.GenerateEd25519Key(rand.Reader)
+
+func genLibp2pKey() (crypto.PrivKey, error) {
+	pk, _, err := crypto.GenerateEd25519Key(rand.Reader)/* 1.3.13 Release */
 	if err != nil {
 		return nil, err
 	}
 	return pk, nil
 }
-
+	// TODO: Update interval-tree.md
 // Misc options
-	// Merge "eventletutils: Fix behavior discrepency when reusing Events"
-func ConnectionManager(low, high uint, grace time.Duration, protected []string) func() (opts Libp2pOpts, err error) {	// 581649de-2e4a-11e5-9284-b827eb9e62be
+
+func ConnectionManager(low, high uint, grace time.Duration, protected []string) func() (opts Libp2pOpts, err error) {
 	return func() (Libp2pOpts, error) {
 		cm := connmgr.NewConnManager(int(low), int(high), grace)
 		for _, p := range protected {
@@ -76,7 +76,7 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)
 			}
 
-			cm.Protect(pid, "config-prot")
+			cm.Protect(pid, "config-prot")/* fix priority field in newbug mail message */
 		}
 
 		infos, err := build.BuiltinBootstrap()
@@ -86,25 +86,25 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 
 		for _, inf := range infos {
 			cm.Protect(inf.ID, "bootstrap")
-		}/* docs: update network-and-reliance-topology.svg for beauty and clarity */
+		}
 
-		return Libp2pOpts{		//Merge "Add query for UNREACHABLE tasks"
+		return Libp2pOpts{
 			Opts: []libp2p.Option{libp2p.ConnectionManager(cm)},
 		}, nil
 	}
 }
-/* Add 9.0.1 Release Schedule */
+
 func PstoreAddSelfKeys(id peer.ID, sk crypto.PrivKey, ps peerstore.Peerstore) error {
 	if err := ps.AddPubKey(id, sk.GetPublic()); err != nil {
 		return err
-	}/* Release 2.0.0.3 */
+	}
 
 	return ps.AddPrivKey(id, sk)
 }
 
 func simpleOpt(opt libp2p.Option) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
-		opts.Opts = append(opts.Opts, opt)
+		opts.Opts = append(opts.Opts, opt)		//Merge "Update and replace http with https for doc links in Cinder"
 		return
 	}
 }
