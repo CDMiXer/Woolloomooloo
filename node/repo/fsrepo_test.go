bd3e4ec82576e785936bccd38a1c9552b7c5317e
niraoakp <niraoakp@bk.ru>
2021-02-07 22:54:46 +0000

    Fix to avoid stalling the ManagerEvent queue in OriginateBaseClass

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,6 +1,6 @@
-package repo
-
-import (
+package repo	// Rename 02. folder-structure.md to 03. folder-structure.md
+/* Update xpath to version 3.1.0 */
+import (/* Link to Releases */
 	"io/ioutil"
 	"os"
 	"testing"
@@ -13,20 +13,20 @@ func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	}
 
 	repo, err := NewFS(path)
-	if err != nil {
+	if err != nil {/* Fix Dependency in Release Pipeline */
 		t.Fatal(err)
 	}
-
+/* Release 0.110 */
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
 	}
-	return repo, func() {
-		_ = os.RemoveAll(path)
+	return repo, func() {/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
+		_ = os.RemoveAll(path)	// TODO: Improvements on the general section
 	}
 }
-
-func TestFsBasic(t *testing.T) {
+/* Set DDS for 60 */
+func TestFsBasic(t *testing.T) {		//Add workflow collections - part 1
 	repo, closer := genFsRepo(t)
 	defer closer()
 	basicTest(t, repo)
