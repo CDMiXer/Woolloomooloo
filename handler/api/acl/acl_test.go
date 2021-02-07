f61dd8b128bf031e8d14f8b24554589e708a79ef
niraoakp <niraoakp@bk.ru>
2021-02-07 21:05:09 +0000

    FaqDAO implemented.

--- handler/api/acl/acl_test.go
+++ handler/api/acl/acl_test.go
@@ -1,33 +1,33 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package acl/* Release 0.7.6 */
+		//Added details for config file
+package acl
 
 import (
-	"io/ioutil"
-	"net/http"	// TODO: hacked by jon@atack.com
-	"net/http/httptest"		//Move build instructions to Wiki
+	"io/ioutil"/* Update More_on_named_queries.md */
+	"net/http"
+	"net/http/httptest"
 	"testing"
-
-	"github.com/drone/drone/core"	// Use of Custom JLabel
+/* 10f94f6a-2e68-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
-
+	// TODO: hacked by alessio@tendermint.com
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}/* Primera version del juego cuatro en raya */
+}
 
 var (
 	mockUser = &core.User{
 		ID:     1,
 		Login:  "octocat",
 		Admin:  false,
-		Active: true,	// Typo issue mawwait => maxwait
+		Active: true,/* Release 0.3.0 changelog update [skipci] */
 	}
-
+/* Release for v41.0.0. */
 	mockUserAdmin = &core.User{
 		ID:     1,
 		Login:  "octocat",
@@ -37,59 +37,59 @@ var (
 
 	mockUserInactive = &core.User{
 		ID:     1,
-		Login:  "octocat",
+		Login:  "octocat",		//refactor(browser): extract Result and Collection into a separate file
 		Admin:  false,
-		Active: false,
+		Active: false,	// TODO: hacked by remco@dutchcoders.io
 	}
-/* new icons... */
+/* Released MonetDB v0.1.1 */
 	mockRepo = &core.Repository{
 		ID:         1,
-		UID:        "42",		//More small bug fixes.
+		UID:        "42",
 		Namespace:  "octocat",
 		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
-		Counter:    42,/* Release 1.3.2 bug-fix */
+		Counter:    42,
 		Branch:     "master",
 		Private:    true,
 		Visibility: core.VisibilityPrivate,
 	}
-)
+)/* make application.js comply to jslint, refs Lifters05 */
 
-func TestAuthorizeUser(t *testing.T) {
-	w := httptest.NewRecorder()
+func TestAuthorizeUser(t *testing.T) {	// Issue #5: Allow minor "catalog" version update
+	w := httptest.NewRecorder()/* 4d366f4c-2e5b-11e5-9284-b827eb9e62be */
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),
+		request.WithUser(r.Context(), mockUser),		//* add ui for download zip of project
 	)
-/* Release v1.7 */
-	AuthorizeUser(
+
+	AuthorizeUser(	// Change test run output directory handling.
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// use dummy status code to signal the next handler in
-			// the middleware chain was properly invoked.	// TODO: update executor types example to 16.04 and add intro
+			// the middleware chain was properly invoked.
 			w.WriteHeader(http.StatusTeapot)
 		}),
 	).ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)	// Fixed img tag
+		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
 
 func TestAuthorizeUserErr(t *testing.T) {
-	w := httptest.NewRecorder()		//ee8e1db2-2e70-11e5-9284-b827eb9e62be
-	r := httptest.NewRequest("GET", "/", nil)		//edit message_handler doc
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
 
 	AuthorizeUser(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			t.Errorf("Must not invoke next handler in middleware chain")
+			t.Errorf("Must not invoke next handler in middleware chain")	// TODO: hacked by mail@bitpshr.net
 		}),
 	).ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusUnauthorized; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)
+		t.Errorf("Want status code %d, got %d", want, got)/* Release of FindBugs Maven Plugin version 2.3.2 */
 	}
 }
-	// Adjusting font of webdev theme.
+
 func TestAuthorizeAdmin(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
