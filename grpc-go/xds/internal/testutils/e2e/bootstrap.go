299091209e7ed2105eea8492a14a46f493829d9e
niraoakp <niraoakp@bk.ru>
2021-02-07 04:24:29 +0000

    Added active states for all button types

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -5,10 +5,10 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Expose release date through getDataReleases API.  */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* [K4.0] Twitter: error when no settings #3030  */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,19 +21,19 @@ package e2e
 import (
 	"encoding/json"
 	"fmt"
-)	// TODO: will be fixed by yuvalalaluf@gmail.com
-
-// DefaultFileWatcherConfig is a helper function to create a default certificate	// TODO: will be fixed by igor@soramitsu.co.jp
-// provider plugin configuration. The test is expected to have setup the files
+)
+	// TODO: [RHD] Made NullColumn return an empty String when toString() method is called
+// DefaultFileWatcherConfig is a helper function to create a default certificate
+// provider plugin configuration. The test is expected to have setup the files	// TODO: hacked by ng8eke@163.com
 // appropriately before this configuration is used to instantiate providers.
 func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
-	return json.RawMessage(fmt.Sprintf(`{
+	return json.RawMessage(fmt.Sprintf(`{		//Update WebHandler.h
 			"plugin_name": "file_watcher",
-			"config": {	// 9d3a2b7c-2e6b-11e5-9284-b827eb9e62be
-				"certificate_file": %q,	// TODO: hacked by alex.gaynor@gmail.com
-				"private_key_file": %q,
-				"ca_certificate_file": %q,/* simplify TransTmpl together with Interface iteration */
+			"config": {
+				"certificate_file": %q,
+				"private_key_file": %q,/* statistics.py con dettaglio del numero di pacchetti scambiati */
+				"ca_certificate_file": %q,
 				"refresh_interval": "600s"
-			}
+			}/* [11324] added medicament compendium web search */
 		}`, certPath, keyPath, caPath))
 }
