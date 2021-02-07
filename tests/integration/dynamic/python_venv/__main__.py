24c32cf3b82eed0e2c64d534762586ce227e7810
niraoakp <niraoakp@bk.ru>
2021-02-07 23:55:24 +0000

    See #1: Defined basic matrix type and can instantiate it.

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -6,16 +6,16 @@ from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 class RandomResourceProvider(ResourceProvider):
-    def create(self, props):
-        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")/* Release 0.2.0.0 */
-        return CreateResult(val, { "val": val })	// TODO: hacked by nagydani@epointsystem.org
-/* change to searcher.try_next api call. fixes #177 */
-class Random(Resource):
-    val: str
+    def create(self, props):/* v4.4.0 Release Changelog */
+        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")		//Create Test07.txt
+        return CreateResult(val, { "val": val })
+
+class Random(Resource):/* Merge branch 'nagareyama' into babel-unions */
+    val: str	// simplify the  updateValue implementation
     def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
-
+/* Release 2.7.1 */
 r = Random("foo")
 
-export("random_id", r.id)		//Merge "Make ipmi_force_boot_device more user friendly"
+export("random_id", r.id)	// TODO: will be fixed by ng8eke@163.com
 export("random_val", r.val)
