6ba68ab477093dafb190bf5f01c6d49d84bcd096
niraoakp <niraoakp@bk.ru>
2021-02-07 14:42:10 +0000

    Remove unused link from enums.md

--- handler/api/repos/collabs/list_test.go
+++ handler/api/repos/collabs/list_test.go
@@ -1,76 +1,76 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: [MOD] Disabled gpg-signing.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss		//Added ObjC-Flag and libbz2/libz
+/* Release of eeacms/www:19.9.28 */
+// +build !oss
 
 package collabs
-
-import (
-	"context"/* Release version 1.0.2. */
-	"encoding/json"	// TODO: Delete LulzPrediction.lua
+/* Release for 3.9.0 */
+import (/* cead60c2-2e4b-11e5-9284-b827eb9e62be */
+	"context"
+	"encoding/json"
 	"net/http"
-	"net/http/httptest"/* Do not deploy from master. */
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* updated the about page with new photo and updated links */
 	"github.com/drone/drone/mock"
-
+		//Delete blockrate.pdf
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-/* Eklenti ile ilgili bilgiler sayfası */
+
 var (
 	mockUser = &core.User{
 		ID:    1,
-		Login: "octocat",	// TODO: Create tr_TR.ini
+		Login: "octocat",
 	}
 
-	mockRepo = &core.Repository{/* Update EveryPay Android Release Process.md */
+	mockRepo = &core.Repository{
 		ID:        1,
 		UID:       "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
-	}	// TODO: will be fixed by alex.gaynor@gmail.com
+	}
 
-	mockMember = &core.Perm{
-		Read:  true,
+	mockMember = &core.Perm{/* Updates for phpBB 3.1.9 */
+		Read:  true,/* #36: added documentation to markdown help and Release Notes */
 		Write: true,
 		Admin: true,
 	}
 
-	mockMembers = []*core.Collaborator{		//column widths
-		{/* Release for 2.4.1 */
+	mockMembers = []*core.Collaborator{		//Merge branch 'master' into fix-observer-test
+		{
 			Login: "octocat",
 			Read:  true,
 			Write: true,
 			Admin: true,
 		},
-		{	// TODO: hacked by joshua@yottadb.com
-			Login: "spaceghost",	// TODO: Create 1010
-			Read:  true,	// find extrama. threshold??
+		{
+			Login: "spaceghost",	// TODO: will be fixed by peterke@gmail.com
+			Read:  true,
 			Write: true,
 			Admin: true,
 		},
 	}
 )
-
+/* Eliminate warning in Release-Asserts mode. No functionality change */
 func TestList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)	// kmk: Extended evalcall and evalcall2 with a return value, local .RETURN.
 	members.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockMembers, nil)
-
+/* Revert correction rampe */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* Release 1.0.0-CI00092 */
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
@@ -83,7 +83,7 @@ func TestList(t *testing.T) {
 
 	got, want := []*core.Collaborator{}, mockMembers
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* 0727bb08-2e71-11e5-9284-b827eb9e62be */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
@@ -93,10 +93,10 @@ func TestList_NotFoundError(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)		//Update doc PSAR under dev
+	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)	// reload daily challenge results on back button
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
