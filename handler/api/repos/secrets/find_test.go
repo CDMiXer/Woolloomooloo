c08af547fa768a65dd4a6986972f64c1c0ae4682
niraoakp <niraoakp@bk.ru>
2021-02-07 15:58:11 +0000

    Correção do delay para esconder delta

--- handler/api/repos/secrets/find_test.go
+++ handler/api/repos/secrets/find_test.go
@@ -11,30 +11,30 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"	// lower and upper bound types instead of only upper bound
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: hacked by fjl@ethereum.org
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"	// nsPowerDesc can be null
 )
-
+/* Task 3 Pre-Release Material */
 func TestHandleFind(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: will be fixed by 13860583249@yeah.net
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Commit of exercise 6.33. */
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
-	secrets := mock.NewMockSecretStore(controller)
+	secrets := mock.NewMockSecretStore(controller)/* updated sample share step */
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// TODO: will be fixed by zhen6939@gmail.com
 	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
@@ -43,7 +43,7 @@ func TestHandleFind(t *testing.T) {
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleFind(repos, secrets).ServeHTTP(w, r)
+	HandleFind(repos, secrets).ServeHTTP(w, r)	// Added Mahony AHRS application
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -60,13 +60,13 @@ func TestHandleFind_RepoNotFound(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)		//avoid duplicate module names
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")	// Added installation instructions to README
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
-
+/* updating a broken link */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
@@ -74,24 +74,24 @@ func TestHandleFind_RepoNotFound(t *testing.T) {
 	)
 
 	HandleFind(repos, nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {
+	if got, want := w.Code, http.StatusNotFound; want != got {/* Add 'saveCursorPosition' option */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound
+	got, want := new(errors.Error), errors.ErrNotFound		//Update data_nomenclatures.sql
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
-func TestHandleFind_SecretNotFound(t *testing.T) {
+func TestHandleFind_SecretNotFound(t *testing.T) {/* Delete object_script.vpropertyexplorer.Release */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-
+		//Fixed: Even if effects were disabled, extra debris was created during explosions
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(nil, errors.ErrNotFound)
 
