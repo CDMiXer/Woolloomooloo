6517e9b89ee2563e9860b9d0242004e4f4fe18da
niraoakp <niraoakp@bk.ru>
2021-02-07 16:16:53 +0000

    Fix formatting of license in pom.xml

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
 // Package sigs allows for signing, verifying signatures and key generation
-// using key types selected by package user.
+// using key types selected by package user.	// Throw a ConfigParseException when a JsonParseException is thrown
 //
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
-// For  support of Filecoin BLS import:/* Merged feature/menu into master */
+// For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
