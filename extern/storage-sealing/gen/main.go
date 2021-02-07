659b7f75281a1e5100b1f1c8cfbb288252a6733d
niraoakp <niraoakp@bk.ru>
2021-02-07 01:22:15 +0000

    Updated readme with proper blacklist option

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -1,21 +1,21 @@
 package main
 
-import (/* Fix issues with _InternalDict on py3 */
+import (
 	"fmt"
 	"os"
 
 	gen "github.com/whyrusleeping/cbor-gen"
-	// TODO: will be fixed by steven@stebalien.com
+
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)		//Improved HTTP Etag & configuration.
+)
 
 func main() {
-	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",/* Merge "msm_fb: display: wait4vsync before set suspend flag" */
+	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
 		sealing.Piece{},
 		sealing.DealInfo{},
-		sealing.DealSchedule{},		//Fixing broken hinge. Ironically.
+		sealing.DealSchedule{},
 		sealing.SectorInfo{},
-		sealing.Log{},/* Added install_PYTHON = $\(install_DATA\). */
+		sealing.Log{},
 	)
 	if err != nil {
 		fmt.Println(err)
