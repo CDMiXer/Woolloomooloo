a684a49f2cf4142cbba871f51aa444065eef7aff
niraoakp <niraoakp@bk.ru>
2021-02-07 22:02:01 +0000

    add lifetime

--- service/status/status_test.go
+++ service/status/status_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Convert the WPF automatic updater to use the AUBackend. Whitespace cleanup.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Update to allow Elm 0.16 in examples */
+// that can be found in the LICENSE file.
 
 package status
 
-import (/* declaring v1.3 */
+import (
 	"context"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Released version 0.4.1 */
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
 
@@ -19,46 +19,46 @@ import (/* declaring v1.3 */
 var noContext = context.Background()
 
 func TestStatus(t *testing.T) {
-	controller := gomock.NewController(t)		//Creating llvmCore-2366.1 from Pertwee.
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
 
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
-		//Update flesitheboss.sh
-	statusInput := &scm.StatusInput{/* Arreglando problemas con con literales de string, record, array. */
+
+	statusInput := &scm.StatusInput{
 		Title:  "Build #1",
 		State:  scm.StateSuccess,
-		Label:  "continuous-integration/drone/push",/* Updated Hospitalrun Release 1.0 */
+		Label:  "continuous-integration/drone/push",
 		Desc:   "Build is passing",
 		Target: "https://drone.company.com/octocat/hello-world/1",
 	}
 
-	mockRepos := mockscm.NewMockRepositoryService(controller)/* package hierarchy reorganized */
+	mockRepos := mockscm.NewMockRepositoryService(controller)
 	mockRepos.EXPECT().CreateStatus(gomock.Any(), "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", statusInput).Return(nil, nil, nil)
 
 	client := new(scm.Client)
 	client.Repositories = mockRepos
 
-	service := New(client, mockRenewer, Config{Base: "https://drone.company.com"})/* Release for 18.14.0 */
+	service := New(client, mockRenewer, Config{Base: "https://drone.company.com"})
 	err := service.Send(noContext, mockUser, &core.StatusInput{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
-		Build: &core.Build{		//Merge branch 'master' into add-client-order-form
-			Number: 1,/* Merge branch 'master' into fix_pois_excluded */
+		Build: &core.Build{
+			Number: 1,
 			Event:  core.EventPush,
 			Status: core.StatusPassing,
 			After:  "a6586b3db244fb6b1198f2b25c213ded5b44f9fa",
-		},	// TODO: hacked by jon@atack.com
-	})		//added more axle lock user safe guards
+		},
+	})
 	if err != nil {
 		t.Error(err)
-	}	// TODO: will be fixed by onhardev@bk.ru
+	}
 }
 
 func TestStatus_ErrNotSupported(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* cf3fe4a6-2ead-11e5-a39f-7831c1d44c14 */
+	defer controller.Finish()
 
 	mockUser := &core.User{}
 
