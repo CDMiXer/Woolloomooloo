bfa259bbc728a08acee882ff3f0cc93e71969df9
niraoakp <niraoakp@bk.ru>
2021-02-07 17:42:11 +0000

    añadido num entre los det (t1x)

--- cli/wait.go
+++ cli/wait.go
@@ -1,34 +1,34 @@
-package cli
-/* Release of eeacms/volto-starter-kit:0.3 */
-import (/* Merge "Release 3.2.3.278 prima WLAN Driver" */
+package cli	// #vim reduce syntastic error window size
+
+import (
 	"fmt"
 	"time"
-
+/* Merge branch 'release-next' into ReleaseNotes5.0_1 */
 	"github.com/urfave/cli/v2"
-)/* Release DBFlute-1.1.0-sp7 */
-
-var WaitApiCmd = &cli.Command{		//Fix for anatomy page table, rows with no MA term.
+)		//Refactoring image function
+	// TODO: Update release-notes-0.15.0.2.md
+var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
-			api, closer, err := GetFullNodeAPI(cctx)
-			if err != nil {	// TODO: Automatic changelog generation for PR #52231 [ci skip]
+			api, closer, err := GetFullNodeAPI(cctx)		//New line for composer command
+			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)
+				time.Sleep(time.Second)		//Change dao to match spring data CrudRepository
 				continue
 			}
 			defer closer()
 
-			ctx := ReqContext(cctx)		//0A02bISxcGTPPfpWFZMQlu0xMNWSVkSt
+			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
-			if err != nil {
+			if err != nil {/* Merge "Remove keys from filters option for profile-list" */
 				return err
 			}
-/* Rename nida.js to nida.sql */
-			return nil
-		}/* Relink some files */
+
+			return nil	// TODO: will be fixed by fjl@ethereum.org
+		}
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
 }
