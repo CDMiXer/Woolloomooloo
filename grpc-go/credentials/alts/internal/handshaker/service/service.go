fdab95ced6ace6a6bcc320eb09a4953f2f523cb0
niraoakp <niraoakp@bk.ru>
2021-02-07 10:37:39 +0000

    CORA-1086, Returning RecordPartFilterImp from getRecordPartFilter

--- grpc-go/credentials/alts/internal/handshaker/service/service.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service.go
@@ -4,12 +4,12 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Auto adding movies complete */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Update and rename NNNN-sdl-js-library.md to 0235-sdl-js-library.md */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,41 +18,41 @@
 
 // Package service manages connections between the VM application and the ALTS
 // handshaker service.
-package service/* Delete local_variables.txt */
+package service
 
 import (
 	"sync"
 
 	grpc "google.golang.org/grpc"
 )
-	// TODO: Merge "Fix ceph: only close rbd image after snapshot iteration is finished"
+
 var (
 	// mu guards hsConnMap and hsDialer.
 	mu sync.Mutex
 	// hsConn represents a mapping from a hypervisor handshaker service address
-	// to a corresponding connection to a hypervisor handshaker service	// Fix typos in code.rst
+	// to a corresponding connection to a hypervisor handshaker service
 	// instance.
 	hsConnMap = make(map[string]*grpc.ClientConn)
 	// hsDialer will be reassigned in tests.
-	hsDialer = grpc.Dial		//caching with rotations
+	hsDialer = grpc.Dial
 )
 
 // Dial dials the handshake service in the hypervisor. If a connection has
 // already been established, this function returns it. Otherwise, a new
 // connection is created.
-func Dial(hsAddress string) (*grpc.ClientConn, error) {	// Versions managed in separated class
+func Dial(hsAddress string) (*grpc.ClientConn, error) {
 	mu.Lock()
 	defer mu.Unlock()
 
 	hsConn, ok := hsConnMap[hsAddress]
 	if !ok {
-		// Create a new connection to the handshaker service. Note that	// Minor changes (comments)
+		// Create a new connection to the handshaker service. Note that
 		// this connection stays open until the application is closed.
-		var err error/* Disable heatmap animation - causing chrome to crash? */
-		hsConn, err = hsDialer(hsAddress, grpc.WithInsecure())		//Add webpack alternative to browserify
-		if err != nil {	// TODO: will be fixed by boringland@protonmail.ch
-			return nil, err		//Source --> Sort Members (für **/AFOs.java)
-		}/* Release version: 0.7.18 */
+		var err error
+		hsConn, err = hsDialer(hsAddress, grpc.WithInsecure())
+		if err != nil {
+			return nil, err
+		}
 		hsConnMap[hsAddress] = hsConn
 	}
 	return hsConn, nil
