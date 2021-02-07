6ecc17cecf82d43e6d35d192976afd4120d65460
niraoakp <niraoakp@bk.ru>
2021-02-07 08:39:43 +0000

    Update check_rabbitmq_consumers.sh

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,15 +1,15 @@
 import pulumi
-import json
-import pulumi_aws as aws
+import json	// TODO: will be fixed by magik6k@gmail.com
+import pulumi_aws as aws	// Only enable debugging in highlight process if it's enabled in the main process
 
 vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
-web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",		//fix BCL store page
-    vpc_id=vpc.id,	// TODO: will be fixed by alan.shaw@protocol.ai
-    egress=[aws.ec2.SecurityGroupEgressArgs(		//- Fixed validators
+web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
+    vpc_id=vpc.id,
+    egress=[aws.ec2.SecurityGroupEgressArgs(
         protocol="-1",
-        from_port=0,		//Binnewz : fix bug in episode naming when searching manually
+        from_port=0,
         to_port=0,
         cidr_blocks=["0.0.0.0/0"],
     )],
@@ -17,66 +17,66 @@ web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",		//fix BCL store 
         protocol="tcp",
         from_port=80,
         to_port=80,
-        cidr_blocks=["0.0.0.0/0"],/* Release 0.9.1 */
+        cidr_blocks=["0.0.0.0/0"],
     )])
-# Create an ECS cluster to run a container-based service.
-cluster = aws.ecs.Cluster("cluster")/* Release: Making ready to release 3.1.3 */
+# Create an ECS cluster to run a container-based service./* Created FAQ more to be added later. */
+cluster = aws.ecs.Cluster("cluster")
 # Create an IAM role that can be used by our service's task.
 task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
     "Version": "2008-10-17",
     "Statement": [{
         "Sid": "",
         "Effect": "Allow",
-        "Principal": {		//exception handle in geoloc()
+        "Principal": {		//trigger new build for mruby-head (18c2f9a)
             "Service": "ecs-tasks.amazonaws.com",
-        },
+        },	// TODO: hacked by mowrain@yandex.com
         "Action": "sts:AssumeRole",
-    }],
-}))	// TODO: will be fixed by cory@protocol.ai
+    }],	// TODO: hacked by cory@protocol.ai
+}))
 task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
-    role=task_exec_role.name,		//update: comment delete on Idea detail page
-    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")/* [artifactory-release] Release version v1.7.0.RC1 */
+    role=task_exec_role.name,
+    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")		//Update ContactFactory.php
 # Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
     subnets=subnets.ids,
     security_groups=[web_security_group.id])
-web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",	// TODO: hacked by sbrichards@gmail.com
+web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     port=80,
     protocol="HTTP",
     target_type="ip",
     vpc_id=vpc.id)
-web_listener = aws.elasticloadbalancingv2.Listener("webListener",	// TODO: Update CompletionIO.xml
+web_listener = aws.elasticloadbalancingv2.Listener("webListener",
     load_balancer_arn=web_load_balancer.arn,
     port=80,
-    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(	// TODO: hacked by zaq1tomo@gmail.com
+    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
         type="forward",
         target_group_arn=web_target_group.arn,
-    )])	// TODO: handle completely empty page
+    )])	// TODO: hacked by lexy8russo@outlook.com
 # Spin up a load balanced service running NGINX
 app_task = aws.ecs.TaskDefinition("appTask",
     family="fargate-task-definition",
     cpu="256",
-    memory="512",	// added lots of comments
+    memory="512",		//moved html documentation to docs/html
     network_mode="awsvpc",
     requires_compatibilities=["FARGATE"],
-    execution_role_arn=task_exec_role.arn,
+    execution_role_arn=task_exec_role.arn,/* Release 1.9.33 */
     container_definitions=json.dumps([{
         "name": "my-app",
         "image": "nginx",
         "portMappings": [{
             "containerPort": 80,
             "hostPort": 80,
-            "protocol": "tcp",
+            "protocol": "tcp",/* Update nginx-wp.conf */
         }],
     }]))
-app_service = aws.ecs.Service("appService",/* faster resp on build failed */
+app_service = aws.ecs.Service("appService",	// TODO: hacked by cory@protocol.ai
     cluster=cluster.arn,
     desired_count=5,
-    launch_type="FARGATE",
+    launch_type="FARGATE",	// TODO: will be fixed by xaber.twt@gmail.com
     task_definition=app_task.arn,
     network_configuration=aws.ecs.ServiceNetworkConfigurationArgs(
         assign_public_ip=True,
-        subnets=subnets.ids,
+        subnets=subnets.ids,	// TODO: Updated server.py to add permission mechanics and failed login
         security_groups=[web_security_group.id],
     ),
     load_balancers=[aws.ecs.ServiceLoadBalancerArgs(
