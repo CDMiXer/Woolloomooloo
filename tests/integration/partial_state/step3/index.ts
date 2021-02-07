8dec4f8203a4613e38341ff612bf8d2cafd80c05
niraoakp <niraoakp@bk.ru>
2021-02-07 12:40:31 +0000

    Acerto no insert conferências

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by boringland@protonmail.ch
+import { Resource } from "./resource";	// TODO: Add Node.js .gitignore
 
-// resource "not-doomed" is created successfully.		//Cleanup on extension field code.
-const a = new Resource("not-doomed", 5);/* Merge branch 'master' into greenkeeper/cssnano-4.0.5 */
+// resource "not-doomed" is created successfully.
+const a = new Resource("not-doomed", 5);
 
 // "a" should be in the checkpoint.
