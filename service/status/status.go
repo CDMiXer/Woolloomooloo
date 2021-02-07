055ffd06e7dce397697b24a95d2fd139f588f6c0
niraoakp <niraoakp@bk.ru>
2021-02-07 16:02:44 +0000

    Folders - Various cleanup

--- service/status/status.go
+++ service/status/status.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//	// TODO: will be fixed by cory@protocol.ai
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Include bundled report classes in YARD output
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by sbrichards@gmail.com
+// You may obtain a copy of the License at/* DATAKV-301 - Release version 2.3 GA (Neumann). */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Update .travis.yml: remove my mail [ci skip]
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package status
+package status	// Create scale.md
 
 import (
 	"context"
@@ -23,31 +23,31 @@ import (
 	"github.com/drone/go-scm/scm/driver/github"
 )
 
-// Config configures the Status service.
+// Config configures the Status service./* Add Screeenly to Reference Codebases */
 type Config struct {
 	Base     string
 	Name     string
 	Disabled bool
-}
+}/* Added policies and rules. */
 
-// New returns a new StatusService
+// New returns a new StatusService/* each link layout is now its own QWidget object */
 func New(client *scm.Client, renew core.Renewer, config Config) core.StatusService {
 	return &service{
 		client:   client,
-		renew:    renew,
+		renew:    renew,	// TODO: Added missing link in latest bookmarks.
 		base:     config.Base,
 		name:     config.Name,
 		disabled: config.Disabled,
-	}
-}
+	}		//Refactor: use function
+}/* Release 0.4.4 */
 
 type service struct {
 	renew    core.Renewer
 	client   *scm.Client
 	base     string
 	name     string
-	disabled bool
-}
+	disabled bool/* Removed bounds (see rosenbrock_bounds.py if needed) */
+}		//Refacotring - Local repository fixes
 
 func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {
 	if s.disabled || req.Build.Event == core.EventCron {
@@ -56,7 +56,7 @@ func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInp
 
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
-		return err
+		return err/* Merge "[INTERNAL] Add REUSE badge" */
 	}
 
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
@@ -66,7 +66,7 @@ func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInp
 
 	// HACK(bradrydzewski) provides support for the github deployment API
 	if req.Build.DeployID != 0 && s.client.Driver == scm.DriverGithub {
-		// TODO(bradrydzewski) only update the deployment status when the
+		// TODO(bradrydzewski) only update the deployment status when the/* Merge "Make mw.DestinationChecker more reusable" */
 		// build completes.
 		if req.Build.Finished == 0 {
 			return nil
