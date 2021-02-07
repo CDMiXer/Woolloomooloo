22f1505d7ca57be89fe575ad5130125cabdf5ff2
niraoakp <niraoakp@bk.ru>
2021-02-07 12:33:11 +0000

    Update angular.of.gardens.service.js

--- login/gitee/gitee.go
+++ login/gitee/gitee.go
@@ -1,17 +1,17 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// Correct a couple of flake8 warning
+
 package gitee
-		//Create Kafka.md
+
 import (
 	"net/http"
-	"strings"/* Release of eeacms/www-devel:19.1.22 */
+	"strings"
 
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
-/* MiniRelease2 hardware update, compatible with STM32F105 */
+
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures the Gitee auth provider.
@@ -19,26 +19,26 @@ type Config struct {
 	ClientID     string
 	ClientSecret string
 	RedirectURL  string
-	Server       string	// translation save
+	Server       string
 	Scope        []string
 	Client       *http.Client
 }
 
 // Handler returns a http.Handler that runs h at the
 // completion of the Gitee authorization flow. The Gitee
-// authorization details are available to h in the		//Rebuilt index with jayhung97724
+// authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
-		BasicAuthOff:     true,	// TODO: only include relevant paths for CI trigger
+		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
 		RedirectURL:      c.RedirectURL,
-		AccessTokenURL:   server + "/oauth/token",/* o Release version 1.0-beta-1 of webstart-maven-plugin. */
-		AuthorizationURL: server + "/oauth/authorize",/* Updated merchant api to work with spigot 1.13 */
-		Scope:            c.Scope,		//[FIX] Twig THEMESPATH.
+		AccessTokenURL:   server + "/oauth/token",
+		AuthorizationURL: server + "/oauth/authorize",
+		Scope:            c.Scope,
 	})
 }
 
