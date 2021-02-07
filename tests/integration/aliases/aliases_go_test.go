5b713af1a7682d6eecc1101a8d1e28c9fdeb3800
niraoakp <niraoakp@bk.ru>
2021-02-07 06:00:29 +0000

    Removes whitelist parameter and fixes testsuite name

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build go all
 
-package ints		//moved cucumber rails up out of the dummy app dependencies.
+package ints
 
-import (/* Update dependency styled-components to v3.4.10 */
+import (/* Both html files work together now. */
 	"path/filepath"
-	"testing"	// TODO: will be fixed by davidad@alum.mit.edu
+	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Release 1.0 Final extra :) features; */
-)/* Drop obsolete ip6int table. */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
 
-var dirs = []string{	// A few comments on dvm.dylan
-	"rename",
+var dirs = []string{
+	"rename",/* snappy/systemimage.go: remove dead code */
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
@@ -22,20 +22,20 @@ func TestGoAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("go", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{
+			integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: Add SVG images to tiles to explore misalignment
 				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
 					"github.com/pulumi/pulumi/sdk/v2",
-				},/* Add environments to config file and config class. */
+				},
 				Quick: true,
-				EditDirs: []integration.EditDir{
-{					
+				EditDirs: []integration.EditDir{		//Rename blogz/index.md to blog/index.md
+					{
 						Dir:             filepath.Join(d, "step2"),
-						ExpectNoChanges: true,
+						ExpectNoChanges: true,		//Delete Toolbox.pyt
 						Additive:        true,
 					},
 				},
 			})
 		})
-	}	// TODO: Merge branch 'master' into new-br
-}
+	}	// TODO: hacked by brosner@gmail.com
+}/* Release version 3.6.13 */
