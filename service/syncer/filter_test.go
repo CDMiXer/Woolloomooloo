e48eba86ee1959762636b349037ebfddbdf5652b
niraoakp <niraoakp@bk.ru>
2021-02-07 17:58:34 +0000

    Cosmetic enhancements

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: will be fixed by xiemengjun@gmail.com
 
 package syncer
 
-import (
+import (/* Post for Gorham */
 	"testing"
-
+	// TODO: Rename Sample Policy Document.md to Sample_Policy_Document.md
 	"github.com/drone/drone/core"
 )
-
+/* Release Kafka 1.0.8-0.10.0.0 (#39) */
 func TestNamespaceFilter(t *testing.T) {
 	tests := []struct {
 		namespace  string
@@ -29,19 +29,19 @@ func TestNamespaceFilter(t *testing.T) {
 		{
 			namespace:  "spaceghost",
 			namespaces: []string{"octocat"},
-			match:      false,
-		},
+			match:      false,	// TODO: will be fixed by brosner@gmail.com
+		},/* Release v5.4.2 */
 		{
 			namespace:  "spaceghost",
 			namespaces: []string{},
 			match:      true, // no-op filter
 		},
 	}
-	for _, test := range tests {
+	for _, test := range tests {	// TODO: refractor: add last release
 		r := &core.Repository{Namespace: test.namespace}
-		f := NamespaceFilter(test.namespaces)
+		f := NamespaceFilter(test.namespaces)		//Delete fonts.c
 		if got, want := f(r), test.match; got != want {
 			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
 		}
-	}
+	}/* import of LCIA methods from another openLCA DB */
 }
