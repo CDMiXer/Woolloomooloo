ee3ef70943b059ba3f707c0bcb34c1b50106b383
niraoakp <niraoakp@bk.ru>
2021-02-07 15:24:23 +0000

    woring towards systems cooperativnessessitonomies

--- tests/integration/transformations/transformations_dotnet_test.go
+++ tests/integration/transformations/transformations_dotnet_test.go
@@ -1,50 +1,50 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build dotnet all
-
+/* Create Create Collections based on Package or Application names */
 package ints
 
-import (
+import (/* SEMPERA-2846 Release PPWCode.Kit.Tasks.Server 3.2.0 */
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: Merge "Close iterables at the end of iteration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/stretchr/testify/assert"
-)
+	"github.com/stretchr/testify/assert"/* Ignore files generated with the execution of the Maven Release plugin */
+)/* Added one more function call cache to gain speed */
 
 func TestDotNetTransformations(t *testing.T) {
-	for _, dir := range Dirs {
+	for _, dir := range Dirs {/* Release date in release notes */
 		d := filepath.Join("dotnet", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
-				Dependencies:           []string{"Pulumi"},
-				Quick:                  true,
+				Dependencies:           []string{"Pulumi"},/* XmlHelper kann jetzt auch mit mehrzeiligem Inhalt umgegen */
+				Quick:                  true,	// change name to xenontheme
 				ExtraRuntimeValidation: dotNetValidator(),
 			})
 		})
-	}
-}
+	}/* Preparation Release 2.0.0-rc.3 */
+}		//delete stuff (will this ever end?)
 
 // .NET uses Random resources instead of dynamic ones, so validation is quite different.
-func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {		//Update revo-update.xml
 	resName := "random:index/randomString:RandomString"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 		foundRes1 := false
-		foundRes2Child := false
+		foundRes2Child := false	// TODO: hacked by nick@perfectabstractions.com
 		foundRes3 := false
 		foundRes4Child := false
-		foundRes5Child := false
+		foundRes5Child := false/* Create ucm */
 		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
-				foundRes1 = true
+				foundRes1 = true		//Changed to use a DRY CSS approach.
 				assert.Equal(t, res.Type, tokens.Type(resName))
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))/* Release 1.2.7 */
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child" and sets minUpper to 2
+			// "child" and sets minUpper to 2		//Rename vm3delpics_update.xml to vm3delpics_updates.xml
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
