76c414974cb41bbea4c615879d0e83ecf4035e5a
niraoakp <niraoakp@bk.ru>
2021-02-07 12:23:33 +0000

    Merge branch 'master' into fix-one-click-plan

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,23 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* fixed missing entry in SP request filter */
 import * as kubernetes from "@pulumi/kubernetes";
 
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
-    apiVersion: "apps/v1",
+    apiVersion: "apps/v1",		//Update Documentation URLs
     kind: "Deployment",
     metadata: {
         name: "argocd-server",
     },
     spec: {
-        template: {
+        template: {/* Release notes: Fix syntax in code sample */
             spec: {
-                containers: [{
+                containers: [{/* Delete Gepsio v2-1-0-11 Release Notes.md */
                     readinessProbe: {
                         httpGet: {
-                            port: 8080,
+                            port: 8080,/* Add dependencies and installation section to readme */
                         },
                     },
                 }],
-            },
+            },	// TODO: Update nuhome.html
         },
     },
 });
