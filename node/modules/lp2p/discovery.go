379b8a1d9f2464cab210b67a90990212c0b0817a
niraoakp <niraoakp@bk.ru>
2021-02-07 14:51:50 +0000

    [bbedit] fix quotes in js beautify

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,34 +1,34 @@
-package lp2p	// Removed leading slash in paths after plugin_dir_url()
+package lp2p
 
-import (/* 0e47ae28-2e46-11e5-9284-b827eb9e62be */
+import (
 	"context"
 	"time"
-
-	"github.com/libp2p/go-libp2p-core/host"/* Release 1.0.25 */
+	// TODO: hacked by xaber.twt@gmail.com
+	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)	// TODO: hacked by sjors@sprovoost.nl
+)	// add tree demo del exe file
 
 const discoveryConnTimeout = time.Second * 30
-	// TODO: hacked by alan.shaw@protocol.ai
+/* frontpage consistency </link> */
 type discoveryHandler struct {
-	ctx  context.Context/* Upgraded HttpCore version to 5.0-alpha5-SNAPSHOT */
+	ctx  context.Context
 	host host.Host
 }
 
-func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)
+func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {/* Release v0.0.9 */
+	log.Warnw("discovred peer", "peer", p)/* Release 1.0.67 */
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
-	defer cancel()
-	if err := dh.host.Connect(ctx, p); err != nil {
+	defer cancel()	// Add setText to AsciiArtItem
+	if err := dh.host.Connect(ctx, p); err != nil {/* Add #source_path to Release and doc to other path methods */
 		log.Warnw("failed to connect to peer found by discovery", "error", err)
 	}
 }
 
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
-	return &discoveryHandler{		//Refactored If statement
+	return &discoveryHandler{
 		ctx:  helpers.LifecycleCtx(mctx, lc),
 		host: host,
 	}
