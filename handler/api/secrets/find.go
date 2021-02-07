b4099f70fee0e7b292d00b65f712e86c9daa7e78
niraoakp <niraoakp@bk.ru>
2021-02-07 21:00:16 +0000

    [VFTP] Added one new access flag

--- handler/api/secrets/find.go
+++ handler/api/secrets/find.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Studio: Release version now saves its data into AppData. */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Release 1.0.0.95 QCACLD WLAN Driver" */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release 4.2.2 */
+
 package secrets
 
-import (
+import (	// TODO: hacked by boringland@protonmail.ch
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* (StackingContextPtr) : New */
-
-	"github.com/go-chi/chi"/* Changed time format for alarm table in plugin_customization.ini */
+	"github.com/drone/drone/handler/api/render"	// human-friendly timestamps for package and event lists
+	// TODO: hacked by arachnid@notdot.net
+	"github.com/go-chi/chi"
 )
-
-// HandleFind returns an http.HandlerFunc that writes json-encoded	// TODO: Move file en/README.md to en/README.adoc
-// secret details to the the response body.
+	// TODO: hacked by arajasek94@gmail.com
+// HandleFind returns an http.HandlerFunc that writes json-encoded/* Create Release Checklist template */
+// secret details to the the response body.		//NetKAN updated mod - ContractParser-9.0
 func HandleFind(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "namespace")		//fixed requirements version
+			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
 		secret, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Rewrite section ReleaseNotes in ReadMe.md. */
 		}
 		safe := secret.Copy()
-		render.JSON(w, safe, 200)/* Solution Release config will not use Release-IPP projects configs by default. */
+		render.JSON(w, safe, 200)
 	}
 }
