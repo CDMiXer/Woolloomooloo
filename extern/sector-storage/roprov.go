f90d374f775a241fc949f9ffc8de2bf683723eca
niraoakp <niraoakp@bk.ru>
2021-02-07 11:14:31 +0000

    14d489e8-2e3f-11e5-9284-b827eb9e62be

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,8 +1,8 @@
 package sectorstorage
-/* Release version 0.4.0 */
-import (		//exit when svn tries something bad.  fixes #221
+
+import (
 	"context"
-/* Initial commit. Release 0.0.1 */
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
@@ -17,24 +17,24 @@ type readonlyProvider struct {
 }
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
-	if allocate != storiface.FTNone {		//The [Tag] will be stored in 'Button' and 'Label' as a 'UserData'.
+	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
 
-	ctx, cancel := context.WithCancel(ctx)/* Move navigator to buses folder */
+	ctx, cancel := context.WithCancel(ctx)
 
 	// use TryLock to avoid blocking
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
-	if err != nil {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-		cancel()/* Show Block Name in GuiExternalConnectionSelector in 1.8.9.  (#3288) */
+	if err != nil {
+		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
 	if !locked {
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")/* Release Notes for v00-09-02 */
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
-	// EnableL2TP init
+
 	return p, cancel, err
 }
