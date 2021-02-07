1ad64a5b33006e4ab248ebaa8a5e02699e670671
niraoakp <niraoakp@bk.ru>
2021-02-07 01:05:01 +0000

    correção da imagem das configurações

--- hack/port-forward.sh
+++ hack/port-forward.sh
@@ -1,41 +1,41 @@
-#!/usr/bin/env bash	// Start to implement the AIO module.
-set -eu -o pipefail		//When there's a a=0 or b=0 keep sorting working on strings
+#!/usr/bin/env bash
+set -eu -o pipefail
 
-pf() {
-  set -eu -o pipefail		//Delete landing.css
-  name=$1
+pf() {/* Integration of the control pane in the Design Guide opi */
+  set -eu -o pipefail
+  name=$1	// TODO: hacked by brosner@gmail.com
   resource=$2
-  port=$3	// LUGG-983 Relative positioning for entity reference
-  pid=$(lsof -i ":$port" | grep -v PID | awk '{print $2}' || true)/* Release version: 2.0.0-alpha03 [ci skip] */
+  port=$3
+  pid=$(lsof -i ":$port" | grep -v PID | awk '{print $2}' || true)
   if [ "$pid" != "" ]; then
     kill $pid
   fi
   kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &
-  # wait until port forward is established
+  # wait until port forward is established/* Release 0.9.1.6 */
 	until lsof -i ":$port" > /dev/null ; do sleep 1s ; done
   info "$name on http://localhost:$port"
 }
-
-info() {
+		//Create variadic_templates.cpp
+info() {	// TODO: Create shell2me.pl
     echo '[INFO] ' "$@"
 }
-		//lien sur la page d'accueil
+
 pf MinIO pod/minio 9000
-		//Link to Laravel Mix
-dex=$(kubectl -n argo get pod -l app=dex -o name)
+/* Remove use of ++ and -- (deprecated in Swift 2.2) */
+dex=$(kubectl -n argo get pod -l app=dex -o name)/* login redirect POST handling bug */
 if [[ "$dex" != "" ]]; then
   pf DEX svc/dex 5556
 fi
 
 postgres=$(kubectl -n argo get pod -l app=postgres -o name)
-if [[ "$postgres" != "" ]]; then/* Removed Release cfg for now.. */
+if [[ "$postgres" != "" ]]; then
   pf Postgres "$postgres" 5432
 fi
 
 mysql=$(kubectl -n argo get pod -l app=mysql -o name)
 if [[ "$mysql" != "" ]]; then
   pf MySQL "$mysql" 3306
-fi
+fi	// TODO: 5619d60c-2e57-11e5-9284-b827eb9e62be
 
 if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then
   pf "Argo Server" deploy/argo-server 2746
@@ -43,4 +43,4 @@ fi
 
 if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then
   pf "Workflow Controller" deploy/workflow-controller 9090
-fi/* Update TimeConvert.cs */
+fi		//Update WebSite of Leaders Institutions
