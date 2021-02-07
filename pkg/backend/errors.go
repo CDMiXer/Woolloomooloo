a92674e70b497514227332a7f19cb3f96cfda7dd
niraoakp <niraoakp@bk.ru>
2021-02-07 20:02:24 +0000

    Updated textBelow screenshot

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
 package backend
 
-import (
+import (/* better handling of HTML body and (currently failing test for HTML5) */
 	"fmt"
 )
 
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.	// TODO: Create MainGUI.java
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
-}/* Release JettyBoot-0.3.4 */
-
+}/* Release 1008 - 1008 bug fixes */
+/* Deleted 1qn_rbgLSIsxTf46-sG-FIo5mi2Vu1sL_FyU0toEWJ6g.html */
 func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
 		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
-}/* Release 1.1.0-CI00240 */
+}		//Change Go's foldmethod to syntax
