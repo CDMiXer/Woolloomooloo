872c956dfbfe8b030ec416a9b92d6861c1ab994d
niraoakp <niraoakp@bk.ru>
2021-02-07 21:33:29 +0000

    Create Gruntfile.js

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,31 +1,31 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Fixed a few typos. Fixed formatting of list items. */
+
 package ints
-/* Released 2.1.0 version */
+
 import (
 	"testing"
-	// TODO: Cleanup error messages.
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//update schema for v2.0
 	"github.com/stretchr/testify/assert"
-)	// TODO: will be fixed by peterke@gmail.com
+)
 
-var Dirs = []string{	// TODO: hacked by ac0dem0nk3y@gmail.com
+var Dirs = []string{/* o Release version 1.0-beta-1 of webstart-maven-plugin. */
 	"simple",
-}	// TODO: [wrapNewGObject] ./gio/System/GIO/File/File.chs
+}	// TODO: Merge "Fix installing tempest plugins"
 
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {		//Remove a warning notice.
 	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
-	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* wykomentowana prośba */
+	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
 		foundRes4Child := false
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
-			// "res1" has a transformation which adds additionalSecretOutputs/* Updated xstream jar for website.  */
-			if res.URN.Name() == "res1" {
+			// "res1" has a transformation which adds additionalSecretOutputs/* Merge branch 'master' into ED-2239-legal-is-great-content-3 */
+			if res.URN.Name() == "res1" {/* Create meta.yaml for cysgp4 package */
 				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
@@ -39,27 +39,27 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
-			// "res3" is impacted by a global stack transformation which sets
+			// "res3" is impacted by a global stack transformation which sets/* [artifactory-release] Release version 3.8.0.RELEASE */
 			// optionalDefault to "stackDefault"
-			if res.URN.Name() == "res3" {		//Fix deprecation warning in get_repo_path/1
+			if res.URN.Name() == "res3" {
 				foundRes3 = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))	// TODO: hacked by steven@stebalien.com
-				optionalInput := res.Inputs["optionalInput"]
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))	// TODO: SPDX-compliant license in root level package.json
+				optionalInput := res.Inputs["optionalInput"]	// TODO: will be fixed by nagydani@epointsystem.org
 				assert.NotNil(t, optionalInput)
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res4" is impacted by two component parent transformations which set
-			// optionalDefault to "default1" and then "default2" and also a global stack		//Add install notes
+			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
 			// result should be "stackDefault".
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))		//[ETL]improve,put comments
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)/* Release Notes for v00-16 */
+				assert.NotNil(t, optionalInput)
 				assert.Equal(t, "stackDefault", optionalInput.(string))
-			}/* Release of eeacms/www:21.3.31 */
+			}
 			// "res5" modifies one of its children to depend on another of its children.
 			if res.URN.Name() == "res5-child1" {
 				foundRes5Child = true
@@ -77,8 +77,8 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 		}
 		assert.True(t, foundRes1)
 		assert.True(t, foundRes2Child)
-		assert.True(t, foundRes3)
-		assert.True(t, foundRes4Child)	// Implemented aspect weaver for after advices
-		assert.True(t, foundRes5Child)/* Release 1.2.3 */
+		assert.True(t, foundRes3)/* Release of Verion 1.3.3 */
+		assert.True(t, foundRes4Child)
+		assert.True(t, foundRes5Child)
 	}
 }
