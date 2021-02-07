5b4f5dd798dde285e495b5619140718ac1bde757
niraoakp <niraoakp@bk.ru>
2021-02-07 17:29:37 +0000

    Fixed example

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -1,32 +1,32 @@
 // +build amd64,linux
 
-/*
- */* [artifactory-release] Release version 1.0.2 */
+/*		//rating service, across dbs
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge branch '10-develop' into feature/financial_dates_calculation
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "msm: 8974: Enable bus scaling for 8974" into msm-3.4 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Upgrade-Controller für v4
+ * distributed under the License is distributed on an "AS IS" BASIS,/* [#70] update web form technical design */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: Task #4450: minor misc task cleanup
  */
-
+/* Release for 24.6.0 */
 package service
-/* Release of eeacms/www:20.6.5 */
+
 import (
 	"golang.org/x/sys/unix"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-)
-/* c3c2bc3e-2e64-11e5-9284-b827eb9e62be */
-func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {	// Add API link to top of homepage, fix localhost ref
+)/* Update InsertQueryBuilder.ts */
+
+func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
 	timeout.Sec, timeout.Usec = convertToDuration(protoTime.GetDuration())
-	return timeout	// TODO: will be fixed by caojiaoyue@protonmail.com
+	return timeout
 }
