d7a9474e0f134ca0aa10965a58a35d10aadb62cc
niraoakp <niraoakp@bk.ru>
2021-02-07 03:22:16 +0000

    Added size detection for webp with VP8X extended headers.

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash	// TODO: hacked by steven@stebalien.com
 set -eu -o pipefail
 
-for m in $*; do
+for m in $*; do	// TODO: actualizo cambios de gh-pages
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
-  mockery -name=$"$MOCK_NAME"	// Implement Rip::Nodes::Comment#==
+  mockery -name=$"$MOCK_NAME"
   cd -
 done
