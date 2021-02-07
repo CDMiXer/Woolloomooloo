644f9d14527a5f3dc9c29ab658489ad783c18158
niraoakp <niraoakp@bk.ru>
2021-02-07 18:15:07 +0000

    Delete link_test.rb

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -4,5 +4,5 @@ import pulumi_aws as aws
 logs = aws.s3.Bucket("logs")
 bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
     target_bucket=logs.bucket,
-)])
+)])		//add /admin/dinner_list
 pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
