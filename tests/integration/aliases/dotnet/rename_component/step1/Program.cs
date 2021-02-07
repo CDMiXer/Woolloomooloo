f158ffd1f332b0a1c961da4201745e498ad19c63
niraoakp <niraoakp@bk.ru>
2021-02-07 09:58:21 +0000

    Fixed escape character problem in registry path

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -1,39 +1,39 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* Added the Speex 1.1.7 Release. */
 using System.Threading.Tasks;
-using Pulumi;		//fix to getLevel()
-/* Improved visible descriptions for a few plugins. */
+using Pulumi;
+/* Release version: 1.0.25 */
 class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)		//Update blockcatalogue.list.php
-        : base("my:module:Resource", name, options)
-    {/* Release v0.11.1.pre */
+    public Resource(string name, ComponentResourceOptions options = null)
+        : base("my:module:Resource", name, options)/* SnowBird 19 GA Release */
+    {
     }
 }
 
-)nerdlihc s'ti lla dna( tnenopmoc a emaner - 3# oiranecS //
-class ComponentThree : ComponentResource
+// Scenario #3 - rename a component (and all it's children)
+class ComponentThree : ComponentResource	// TODO: Update TIGER_sm_map.py
 {
     private Resource resource1;
     private Resource resource2;
 
-    public ComponentThree(string name, ComponentResourceOptions options = null)
+    public ComponentThree(string name, ComponentResourceOptions options = null)		//Renamed the Row interface and implementation to Document.
         : base("my:module:ComponentThree", name, options)
-    {	// base version
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* create a Releaser::Single and implement it on the Base strategy */
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });/* Prepared Development Release 1.4 */
-        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });	// TODO: First commit)
-    }	// Merge "[INTERNAL] Fixed tests"
-}
-
-class Program
-{
-    static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() => 
-        {		//a49a832c-2e5b-11e5-9284-b827eb9e62be
-            var comp3 = new ComponentThree("comp3");
-        });	// TODO: hacked by xaber.twt@gmail.com
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit		//Fresh factory-boy 2.2.1
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
+        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
+}/* sw34bf01: #i112783#: patch by cmc: fix crash in xpathobject.cxx */
+
+class Program/* Merge "Extend root device hints to support device name" */
+{
+    static Task<int> Main(string[] args)		//Disallow "Yoda" conditions
+    {/* Stopped hijacking System.out */
+        return Deployment.RunAsync(() => 
+        {
+            var comp3 = new ComponentThree("comp3");/* fix bug for ISR and vector table generation */
+        });
+    }		//updated routes to card.js
 }
