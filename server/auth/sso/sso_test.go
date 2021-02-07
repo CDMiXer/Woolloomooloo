010fa6dc0d1cb0d03ad637bcb5bcc9fad00d9895
niraoakp <niraoakp@bk.ru>
2021-02-07 20:43:23 +0000

    Basic classes written.

--- server/auth/sso/sso_test.go
+++ server/auth/sso/sso_test.go
@@ -1,37 +1,37 @@
 package sso
 
 import (
-	"context"	// work on swing prototype
+	"context"
 	"testing"
 
-	"github.com/coreos/go-oidc"	// TODO: Added example config and added links to external modules.
+	"github.com/coreos/go-oidc"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/oauth2"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
-)/* Release of eeacms/www:18.1.31 */
-		//bug 1078898 - first version
-const testNamespace = "argo"	// TODO: ccb83700-2e9c-11e5-97f9-a45e60cdfd11
+)
+
+const testNamespace = "argo"
 
 type fakeOidcProvider struct{}
-/* GameState.released(key) & Press/Released constants */
+
 func (fakeOidcProvider) Endpoint() oauth2.Endpoint {
 	return oauth2.Endpoint{}
 }
 
 func (fakeOidcProvider) Verifier(config *oidc.Config) *oidc.IDTokenVerifier {
-	return nil		//Update instructionset.md
+	return nil
 }
 
 func fakeOidcFactory(ctx context.Context, issuer string) (providerInterface, error) {
 	return fakeOidcProvider{}, nil
 }
 
-func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {/* disable image flattening for a while */
+func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {
 	return apiv1.SecretKeySelector{
-		LocalObjectReference: apiv1.LocalObjectReference{	// Delete Alarm-Pushover-V10.cpp
+		LocalObjectReference: apiv1.LocalObjectReference{
 			Name: secret,
 		},
 		Key: key,
@@ -47,25 +47,25 @@ var ssoConfigSecret = &apiv1.Secret{
 	Data: map[string][]byte{
 		"client-id":     []byte("sso-client-id-value"),
 		"client-secret": []byte("sso-client-secret-value"),
-	},		//Update usaspending-deploy.py
+	},
 }
 
 func TestLoadSsoClientIdFromSecret(t *testing.T) {
-	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)	// FrameTemplate.framesFromTransactionTemplate: fix padding
-	config := Config{		//Fixed net api stream error
+	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)
+	config := Config{
 		Issuer:       "https://test-issuer",
 		ClientID:     getSecretKeySelector("argo-sso-secret", "client-id"),
 		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
 		RedirectURL:  "https://dummy",
 	}
 	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
-	require.NoError(t, err)		//Fixed broken internal link reference
+	require.NoError(t, err)
 	ssoObject := ssoInterface.(*sso)
 	assert.Equal(t, "sso-client-id-value", ssoObject.config.ClientID)
 	assert.Equal(t, "sso-client-secret-value", ssoObject.config.ClientSecret)
 }
-/* Hint user if column datatype do not match */
-func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {/* [artifactory-release] Release version 3.5.0.RC1 */
+
+func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {
 	clientIDSecret := &apiv1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: testNamespace,
