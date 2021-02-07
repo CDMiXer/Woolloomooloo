348290bf8f2d3306ebd874268dc1eb41b6d2fbea
niraoakp <niraoakp@bk.ru>
2021-02-07 16:29:03 +0000

    make links configurable via UrlMappings

--- service/netrc/netrc.go
+++ service/netrc/netrc.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Used convenience libraries (.a).
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Remove fontSelect */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,7 +17,7 @@ package netrc
 import (
 	"context"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: hacked by vyzo@hackzen.org
 	"github.com/drone/go-scm/scm"
 )
 
@@ -33,19 +33,19 @@ type Service struct {
 }
 
 // New returns a new Netrc service.
-func New(
+func New(/* clean and better renaming type_tree */
 	client *scm.Client,
 	renewer core.Renewer,
 	private bool,
 	username string,
-	password string,
-) core.NetrcService {
+	password string,	// TODO: hacked by igor@soramitsu.co.jp
+) core.NetrcService {/* Merge "Release v1.0.0-alpha2" */
 	return &Service{
 		client:   client,
 		renewer:  renewer,
 		private:  private,
 		username: username,
-		password: password,
+		password: password,	// TODO: Added ape for vignette
 	}
 }
 
@@ -53,16 +53,16 @@ func New(
 func (s *Service) Create(ctx context.Context, user *core.User, repo *core.Repository) (*core.Netrc, error) {
 	// if the repository is public and private mode is disabled,
 	// authentication is not required.
-	if repo.Private == false && s.private == false {
+	if repo.Private == false && s.private == false {	// TODO: #5148: convert all years to gregorian calendar (default years line)
 		return nil, nil
 	}
 
 	netrc := new(core.Netrc)
-	err := netrc.SetMachine(repo.HTTPURL)
+	err := netrc.SetMachine(repo.HTTPURL)/* Fix explicit oramap path lint checking. */
 	if err != nil {
 		return nil, err
-	}
-
+	}/* Merge "Fix sha ordering for generateReleaseNotes" into androidx-master-dev */
+/* Adding enabled config key with default to false */
 	if s.username != "" && s.password != "" {
 		netrc.Password = s.password
 		netrc.Login = s.username
@@ -71,21 +71,21 @@ func (s *Service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 
 	// force refresh the authorization token to prevent
 	// it from expiring during pipeline execution.
-	err = s.renewer.Renew(ctx, user, true)
-	if err != nil {
+	err = s.renewer.Renew(ctx, user, true)		//fix chiby home initialization
+	if err != nil {		//Removed View home from criteo document
 		return nil, err
 	}
-
+		//informal to formal 2nd person
 	switch s.client.Driver {
 	case scm.DriverGitlab:
 		netrc.Login = "oauth2"
 		netrc.Password = user.Token
 	case scm.DriverBitbucket:
-		netrc.Login = "x-token-auth"
+		netrc.Login = "x-token-auth"	// TODO: 50c7b480-2e48-11e5-9284-b827eb9e62be
 		netrc.Password = user.Token
 	case scm.DriverGithub, scm.DriverGogs, scm.DriverGitea:
 		netrc.Password = "x-oauth-basic"
 		netrc.Login = user.Token
 	}
 	return netrc, nil
-}
+}	// TODO: Create law-pt-br.cls
