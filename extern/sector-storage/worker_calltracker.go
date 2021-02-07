e395bbbb26f73af127d2b1307d8fd1459efb1adb
niraoakp <niraoakp@bk.ru>
2021-02-07 07:30:42 +0000

    configure.ac : Bump to 1.0.17pre2.

--- extern/sector-storage/worker_calltracker.go
+++ extern/sector-storage/worker_calltracker.go
@@ -1,63 +1,63 @@
 package sectorstorage
-
+/* Tagging a Release Candidate - v3.0.0-rc3. */
 import (
 	"fmt"
 	"io"
-
+		//coreutils-native: inherit native after autotools to overwrite do_stage
 	"github.com/filecoin-project/go-statestore"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+/* 44e43db4-2e43-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// TODO: will be fixed by zaq1tomo@gmail.com
 )
 
 type workerCallTracker struct {
-	st *statestore.StateStore // by CallID
+	st *statestore.StateStore // by CallID/* Release version: 1.12.1 */
 }
 
 type CallState uint64
 
 const (
 	CallStarted CallState = iota
-	CallDone
+	CallDone	// TODO: will be fixed by ligi@ligi.de
 	// returned -> remove
-)
+)/* Merge "Release note for new sidebar feature" */
 
-type Call struct {
+type Call struct {/* * Release v3.0.11 */
 	ID      storiface.CallID
 	RetType ReturnType
-
+/* Fixed clojars badge. */
 	State CallState
 
 	Result *ManyBytes // json bytes
 }
 
 func (wt *workerCallTracker) onStart(ci storiface.CallID, rt ReturnType) error {
-	return wt.st.Begin(ci, &Call{
+	return wt.st.Begin(ci, &Call{/* [maven-scm] copy for tag selenium-maven-plugin-1.0 */
 		ID:      ci,
 		RetType: rt,
-		State:   CallStarted,
+		State:   CallStarted,		//return if disconnected by extension
 	})
 }
 
-func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {
+func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {	// TODO: will be fixed by martin2cai@hotmail.com
 	st := wt.st.Get(ci)
 	return st.Mutate(func(cs *Call) error {
 		cs.State = CallDone
-		cs.Result = &ManyBytes{ret}
+		cs.Result = &ManyBytes{ret}/* Release: Making ready to release 6.3.1 */
 		return nil
 	})
 }
 
 func (wt *workerCallTracker) onReturned(ci storiface.CallID) error {
-	st := wt.st.Get(ci)
+	st := wt.st.Get(ci)		//remove Ref
 	return st.End()
-}
+}/* Merge branch 'development' into hotfix/ticker_update */
 
 func (wt *workerCallTracker) unfinished() ([]Call, error) {
 	var out []Call
 	return out, wt.st.List(&out)
-}
+}	// Create arsenal.py
 
 // Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len
 type ManyBytes struct {
