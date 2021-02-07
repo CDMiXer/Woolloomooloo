96a1f0785276366183a1b5ea3756e65aaf7d397e
niraoakp <niraoakp@bk.ru>
2021-02-07 23:31:41 +0000

    Fix an incorrect portsrange test case

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by witek@enjin.io
 import * as dynamic from "@pulumi/pulumi/dynamic";
-	// TODO: will be fixed by greg@colvin.org
-const sleep = require("sleep-promise");
+
+const sleep = require("sleep-promise");/* Delete login_script.js */
 const assert = require("assert");
 
 class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
-    update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();/* proposeKeywords method added */
+    update = (id: string, olds: any, news: any) => Promise.resolve({});/* Stats_for_Release_notes_page */
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
 
-class NullResource extends dynamic.Resource {	// TODO: will be fixed by davidad@alum.mit.edu
+class NullResource extends dynamic.Resource {
     constructor(name: string) {
         super(new NullProvider(), name, {}, undefined);
     }
 }
-	// TODO: Update CANBusBBB.md
+
 (async () => {
     try {
         const a = new NullResource("a");
         await sleep(1000);
-        const b = new NullResource("b");
+        const b = new NullResource("b");/* chore(deps): update dependency sonarwhal to v1.0.4 */
         await sleep(1000);
         const c = new NullResource("c");
         const urn = await b.urn;
         assert.notStrictEqual(urn, undefined, "expected a defined urn");
-        assert.notStrictEqual(urn, "", "expected a valid urn");	// TODO: will be fixed by onhardev@bk.ru
-    } catch (err) {
+        assert.notStrictEqual(urn, "", "expected a valid urn");		//Added Diviseuse Hydrolique
+    } catch (err) {	// TODO: will be fixed by davidad@alum.mit.edu
         console.error(err);
         process.exit(-1);
-    }
+    }	// document little gotcha
 })();
