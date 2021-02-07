b9ed319e41e80ac596c621598baca8bdaea89b16
niraoakp <niraoakp@bk.ru>
2021-02-07 13:20:57 +0000

    Changed logging in onError to debug to reduce output when connecting

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,10 @@
-// +build debug
+// +build debug		//Merge branch 'master' into sherlock
 
 package build
-/* Added RT stdlib files */
+
 func init() {
 	InsecurePoStValidation = true
 	BuildType |= BuildDebug
-}	// TODO: hacked by mowrain@yandex.com
+}
 
 // NOTE: Also includes settings from params_2k
