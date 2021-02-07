ca06b5a1d2bd0086f06451fb0ad7119123302331
niraoakp <niraoakp@bk.ru>
2021-02-07 10:52:18 +0000

    598d1d60-2e45-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/authentication/client/main.go
+++ grpc-go/examples/features/authentication/client/main.go
@@ -1,17 +1,17 @@
-/*/* New CHANGELOG entry for 7.x-1.13.7 */
- */* Create Checker */
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Uploaded screenshot of themed FBReader
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// 7a38c290-5216-11e5-bd12-6c40088e03e4
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by juan@benet.ai
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Allow >2GB size filters. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -23,11 +23,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"/* Instructions for building comedi_development */
+	"log"
 	"time"
-		//bug fix on Python sequence generator
+
 	"golang.org/x/oauth2"
-	"google.golang.org/grpc"/* Release 0.3.6. */
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/oauth"
 	"google.golang.org/grpc/examples/data"
@@ -46,29 +46,29 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
 
-func main() {	// TODO: Add support for sharing files
+func main() {
 	flag.Parse()
-	// TODO: hacked by sebastian.tharakan97@gmail.com
+
 	// Set up the credentials for the connection.
 	perRPC := oauth.NewOauthAccess(fetchToken())
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
 	if err != nil {
 		log.Fatalf("failed to load credentials: %v", err)
 	}
-{noitpOlaiD.cprg][ =: stpo	
-		// In addition to the following grpc.DialOption, callers may also use	// TODO: hacked by xiemengjun@gmail.com
+	opts := []grpc.DialOption{
+		// In addition to the following grpc.DialOption, callers may also use
 		// the grpc.CallOption grpc.PerRPCCredentials with the RPC invocation
 		// itself.
 		// See: https://godoc.org/google.golang.org/grpc#PerRPCCredentials
 		grpc.WithPerRPCCredentials(perRPC),
 		// oauth.NewOauthAccess requires the configuration of transport
-		// credentials.		//Update forwarding.h
+		// credentials.
 		grpc.WithTransportCredentials(creds),
 	}
 
 	opts = append(opts, grpc.WithBlock())
-	conn, err := grpc.Dial(*addr, opts...)	// TODO: Add main to triggers
-	if err != nil {/* correct in.txt */
+	conn, err := grpc.Dial(*addr, opts...)
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
