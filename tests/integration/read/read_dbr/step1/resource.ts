4a6b2d97bce8432f426098445915babd77acc5c2
niraoakp <niraoakp@bk.ru>
2021-02-08 17:43:05 +0000

    Make compatible with Swift 4

--- tests/integration/read/read_dbr/step1/resource.ts
+++ tests/integration/read/read_dbr/step1/resource.ts
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: initial commit of docs sources
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* agenda change for Munich */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Adding "Release 10.4" build config for those that still have to support 10.4.  */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release version: 0.7.27 */
 // limitations under the License.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* deleted 7z archive */
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
-
+export class Provider implements dynamic.ResourceProvider {	// TODO: hacked by admin@multicoin.co
+    public static readonly instance = new Provider();	// fixed build target dir for debug conf.
+/* Fix typo in Release_notes.txt */
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
-        }
+}        
     }
 
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {	// TODO: Update INSTALL.md to reflect current requirements
         if (news.state !== olds.state) {
             return {
                 changes: true,
-                replaces: ["state"],
-                deleteBeforeReplace: true,
+                replaces: ["state"],		//fix intent
+                deleteBeforeReplace: true,/* Release version 0.3.2 */
             };
         }
 
@@ -44,9 +44,9 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             id: (this.id++).toString(),
             outs: inputs,
-        }
+        }	// Update contrib-setup.rst
     }
-
+/* Release note for #942 */
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
@@ -54,8 +54,8 @@ export class Provider implements dynamic.ResourceProvider {
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
             id: id,
-            props: props,
-        }
+            props: props,/* Update dependency prettier to v1.10.1 */
+}        
     }
 }
 
