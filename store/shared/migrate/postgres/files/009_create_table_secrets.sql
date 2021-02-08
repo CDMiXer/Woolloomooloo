20722f98d6a31323a76e54a206e32480c24a67ae
niraoakp <niraoakp@bk.ru>
2021-02-08 02:31:08 +0000

    Added design review notes

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-/* added depending.in dependency monitor */
+/* Label changed */
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                SERIAL PRIMARY KEY	// TODO: hacked by aeongrp@outlook.com
-,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)
-,secret_data              BYTEA/* Upgraded to lesscss4j 1.0.0 */
-,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+ secret_id                SERIAL PRIMARY KEY
+,secret_repo_id           INTEGER	// Update mesh_autoVTX.py
+,secret_name              VARCHAR(500)	// TODO: will be fixed by arajasek94@gmail.com
+,secret_data              BYTEA
+,secret_pull_request      BOOLEAN	// manage.py sqs_clear
+,secret_pull_request_push BOOLEAN	// TODO: will be fixed by sbrichards@gmail.com
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-secrets-repo/* Working on lazy loading */
-/* Release of eeacms/www:18.2.24 */
+-- name: create-index-secrets-repo
+
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name	// Fix bug #618449 - [fetchfromflag] - building dissappeared
+-- name: create-index-secrets-repo-name
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);		//Merge branch 'new-design' into nd/fix-follow
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);		//synced with r25826
