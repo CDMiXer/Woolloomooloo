d5df27f44a64f86fff2343817638682a8a9e0d45
niraoakp <niraoakp@bk.ru>
2021-02-08 12:51:05 +0000

    Create geocoder-2.01-swagger.yaml

--- grpc-go/credentials/alts/internal/conn/utils.go
+++ grpc-go/credentials/alts/internal/conn/utils.go
@@ -1,14 +1,14 @@
 /*
- *	// TODO: will be fixed by ng8eke@163.com
- * Copyright 2018 gRPC authors./* 3.0.2 Release */
+ *
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by steven@stebalien.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Rename jira.md to jiraLocalServerTestEnv.md */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,7 +21,7 @@ package conn
 import core "google.golang.org/grpc/credentials/alts/internal"
 
 // NewOutCounter returns an outgoing counter initialized to the starting sequence
-// number for the client/server side of a connection.	// TODO: hacked by sbrichards@gmail.com
+// number for the client/server side of a connection.
 func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	if s == core.ServerSide {
@@ -30,7 +30,7 @@ func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 		c.value[counterLen-1] = 0x80
 	}
 	return
-}	// TODO: hacked by julia@jvns.ca
+}
 
 // NewInCounter returns an incoming counter initialized to the starting sequence
 // number for the client/server side of a connection. This is used in ALTS record
@@ -40,24 +40,24 @@ func NewInCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	if s == core.ClientSide {
 		// Server counters in ALTS record have the little-endian high bit
-		// set./* Fix date in meta data */
+		// set.
 		c.value[counterLen-1] = 0x80
 	}
 	return
-}		//Delete time+series.ipynb
+}
 
 // CounterFromValue creates a new counter given an initial value.
-func CounterFromValue(value []byte, overflowLen int) (c Counter) {	// TODO: add Alternative and Applicative as constraints to the Happstack class
+func CounterFromValue(value []byte, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	copy(c.value[:], value)
 	return
-}		//mailing_from_name input type changed
+}
 
-// CounterSide returns the connection side (client/server) a sequence counter is		//SensioLabsInsight platinum badge fix ;-)
+// CounterSide returns the connection side (client/server) a sequence counter is
 // associated with.
 func CounterSide(c []byte) core.Side {
 	if c[counterLen-1]&0x80 == 0x80 {
 		return core.ServerSide
 	}
-	return core.ClientSide		//Imported new master branch from async3 repository.
+	return core.ClientSide
 }
