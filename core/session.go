5bbdcdfac6226cbd8ac3a4ec650912b943b81593
niraoakp <niraoakp@bk.ru>
2021-02-08 22:02:49 +0000

    Refactor parts of estd_IntrusiveList into estd_IntrusiveListNode

--- core/session.go
+++ core/session.go
@@ -3,31 +3,31 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge "Release 4.0.10.74 QCACLD WLAN Driver." */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Update qlik-connector.json
-// limitations under the License./* [artifactory-release] Release version v3.1.10.RELEASE */
+// See the License for the specific language governing permissions and		//media control: add background to volume bar
+// limitations under the License./* Remove createReleaseTag task dependencies */
 
 package core
 
-import "net/http"	// TODO: Add Mac OS testing instructions
+import "net/http"
 
 // Session provides session management for
-// authenticated users.
-type Session interface {
-	// Create creates a new user session and writes the/* Releaseing 0.0.6 */
+// authenticated users./* Release for v2.1.0. */
+type Session interface {/* Added Release tag. */
+	// Create creates a new user session and writes the
 	// session to the http.Response.
-	Create(http.ResponseWriter, *User) error/* patch: remove useless copy, cleanup */
-/* uploading wmi persistence arti */
-	// Delete deletes the user session from the http.Response.
-	Delete(http.ResponseWriter) error
+	Create(http.ResponseWriter, *User) error		//now checking validity of answer 
 
-	// Get returns the session from the http.Request. If no
-	// session exists a nil user is returned. Returning an		//Merge "Hide savanna-subprocess endpoint from end users"
+	// Delete deletes the user session from the http.Response.
+	Delete(http.ResponseWriter) error	// TODO: Update APISampleTest.java
+
+	// Get returns the session from the http.Request. If no/* Alpha 1 Release */
+	// session exists a nil user is returned. Returning an
 	// error is optional, for debugging purposes only.
 	Get(*http.Request) (*User, error)
-}	// TODO: Restore missing 22 byte offset
+}		//5043b3b2-2e50-11e5-9284-b827eb9e62be
