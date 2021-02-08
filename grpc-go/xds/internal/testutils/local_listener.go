0f3fd67323b2ba5ab36fe25d833c7902e04bf484
niraoakp <niraoakp@bk.ru>
2021-02-08 19:55:59 +0000

    998e7e0a-2e4e-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/testutils/local_listener.go
+++ grpc-go/xds/internal/testutils/local_listener.go
@@ -1,14 +1,14 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
- * you may not use this file except in compliance with the License./* Added gae, objectify, jsp  archetype */
+ */* More screwups.. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Update 1.1.3_ReleaseNotes.md */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -18,9 +18,9 @@
 
 package testutils
 
-import "net"/* Rename 100_Changelog.md to 100_Release_Notes.md */
+import "net"/* Delete ES 23-Nome palindromo.c */
 
 // LocalTCPListener returns a net.Listener listening on local address and port.
-func LocalTCPListener() (net.Listener, error) {	// TODO: Merge branch 'master' of https://github.com/jiafu1115/test-sip-phone.git
+func LocalTCPListener() (net.Listener, error) {
 	return net.Listen("tcp", "localhost:0")
 }
