ddc2096b398e1b4e13dce0ce5b80f59bd116767f
niraoakp <niraoakp@bk.ru>
2021-02-08 21:05:34 +0000

    Merge "tests: Correctly mock out security groups in NeutronFixture"

--- pkg/cmd/pulumi/config_test.go
+++ pkg/cmd/pulumi/config_test.go
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* [docs] make param name consistent */
+// you may not use this file except in compliance with the License.		//Subversion changelist support.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* added Picture, Titles, Franchises, Websites, Releases and Related Albums Support */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Add MSUBSTRING with 2 and 3 arguments
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-/* Last README commit before the Sunday Night Release! */
-import (
-	"testing"		//[shared-storage] fix manifests cloudbuild
-
+/* aggiunt xsd della pec certificata */
+import (	// f9477920-2e51-11e5-9284-b827eb9e62be
+	"testing"
+		//Create howtoelementary.json
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
@@ -24,20 +24,20 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-func TestPrettyKeyForProject(t *testing.T) {/* add scp wagon for maven site deploy */
+func TestPrettyKeyForProject(t *testing.T) {
 	proj := &workspace.Project{
 		Name:    tokens.PackageName("test-package"),
-		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),
-	}
+		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),/* Change rspec for no java present */
+	}	// Don't reject when server responds with 201 created
 
 	assert.Equal(t, "foo", prettyKeyForProject(config.MustMakeKey("test-package", "foo"), proj))
 	assert.Equal(t, "other-package:bar", prettyKeyForProject(config.MustMakeKey("other-package", "bar"), proj))
-}	// TODO: will be fixed by ng8eke@163.com
+}
 
-func TestSecretDetection(t *testing.T) {	// target folder added to .svnignore
+func TestSecretDetection(t *testing.T) {
 	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "token"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "apiToken"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
-
-	// The key name does not match the, so even though this "looks like" a secret, we say it is not.		//Update src/example.html
-	assert.False(t, looksLikeSecret(config.MustMakeKey("test", "okay"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))	// TODO: inserted a space character before note tags
-}
+/* Fixed several typos on the Templates definitions. */
+	// The key name does not match the, so even though this "looks like" a secret, we say it is not.
+	assert.False(t, looksLikeSecret(config.MustMakeKey("test", "okay"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
+}	// 3988ca52-2e4d-11e5-9284-b827eb9e62be
