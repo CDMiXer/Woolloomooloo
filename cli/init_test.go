6a7522fae240886131064bc66473b72f9405bf76
niraoakp <niraoakp@bk.ru>
2021-02-08 12:37:04 +0000

    Fixed drag&drop handler when option is turned off

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
-package cli	// TODO: new mingw exe
-
-import (
+package cli
+/* Update openmetaanalysis.css */
+import (/* Updated the findspark feedstock. */
 	logging "github.com/ipfs/go-log/v2"
 )
 
-func init() {
+func init() {/* Release 0.2 */
 	logging.SetLogLevel("watchdog", "ERROR")
-}
+}	// TODO: Added videa.seznam.cz (seznam video)
