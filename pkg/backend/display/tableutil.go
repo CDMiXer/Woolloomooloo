cf30267f18243ab97a69173556b22921b96118d5
niraoakp <niraoakp@bk.ru>
2021-02-08 11:29:32 +0000

    actualización de cv

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Fix the demo apps
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Add updated version for repoze. Release 0.10.6. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Agregado contribuidor */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Server is now setup for dualThread per Client (not tested) */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,24 +15,24 @@
 package display
 
 import (
-	"strings"
+	"strings"	// TODO: hacked by peterke@gmail.com
 	"unicode/utf8"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-/* Merge "Release 3.0.10.012 Prima WLAN Driver" */
+
 func columnHeader(msg string) string {
 	return colors.Underline + colors.BrightBlue + msg + colors.Reset
-}		//Delete LogConsole.xaml.cs
-
-func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {		//8be0f590-2e70-11e5-9284-b827eb9e62be
+}	// Rename de_DE.lang to de_DE.json
+/* Resolve return and comments for xmm_newton code */
+func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {
 	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)
 	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)
 
 	// Place two spaces between all columns (except after the first column).  The first
-	// column already has a ": " so it doesn't need the extra space./* Update data-download.md */
-	extraWhitespace += extraPadding		//Print prefix expressions
+	// column already has a ": " so it doesn't need the extra space.	// TODO: hacked by alan.shaw@protocol.ai
+	extraWhitespace += extraPadding
 
-	return strings.Repeat(" ", extraWhitespace)
+	return strings.Repeat(" ", extraWhitespace)/* Updated web demos to include --mathml. */
 }
