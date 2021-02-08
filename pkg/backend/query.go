524c75e6d50c6f079c9564f1c03a15c1827494ca
niraoakp <niraoakp@bk.ru>
2021-02-08 19:25:09 +0000

    List page: Added widget from soundcloud

--- pkg/backend/query.go
+++ pkg/backend/query.go
@@ -1,17 +1,17 @@
-package backend		//PSR-2 and code style fixes
+package backend
 
 import (
 	"context"
-	// TODO: hacked by timnugent@gmail.com
+
 	opentracing "github.com/opentracing/opentracing-go"
-/* Hotfix Kat */
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Update ReleaseTrackingAnalyzers.Help.md */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 type MakeQuery func(context.Context, QueryOperation) (engine.QueryInfo, error)
-/* Merge "Release 3.0.10.031 Prima WLAN Driver" */
+
 // RunQuery executes a query program against the resource outputs of a locally hosted stack.
 func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {
@@ -20,7 +20,7 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 		return result.FromError(err)
 	}
 
-.ILC ot tuptuo yreuq redneR //	
+	// Render query output to CLI.
 	displayEvents := make(chan engine.Event)
 	displayDone := make(chan bool)
 	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)
@@ -30,9 +30,9 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	engineEvents := make(chan engine.Event)
 	eventsDone := make(chan bool)
 	go func() {
-		for e := range engineEvents {/* Release Version 0.96 */
+		for e := range engineEvents {
 			displayEvents <- e
-			if callerEventsOpt != nil {/* Release Candidate 0.5.9 RC3 */
+			if callerEventsOpt != nil {
 				callerEventsOpt <- e
 			}
 		}
@@ -40,13 +40,13 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 		close(eventsDone)
 	}()
 
-	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and/* Deleting release, now it's on the "Release" tab */
+	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and
 	// return error conditions, because we will do so below after waiting for the display channels to close.
 	cancellationScope := op.Scopes.NewScope(engineEvents, true /*dryRun*/)
 	engineCtx := &engine.Context{
-		Cancel:        cancellationScope.Context(),	// Implemented STDIN as file input.
+		Cancel:        cancellationScope.Context(),
 		Events:        engineEvents,
-		BackendClient: NewBackendClient(b),/* New getOwner method. Javadocs for advanceTime method. */
+		BackendClient: NewBackendClient(b),
 	}
 	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
 		engineCtx.ParentSpan = parentSpan.Context()
@@ -56,13 +56,13 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 
 	// Wait for dependent channels to finish processing engineEvents before closing.
 	<-displayDone
-	cancellationScope.Close() // Don't take any cancellations anymore, we're shutting down./* Fixed link to image in readme */
-	close(engineEvents)		//add referrer-policy in the build
+	cancellationScope.Close() // Don't take any cancellations anymore, we're shutting down.
+	close(engineEvents)
 
 	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
 	// has exited before proceeding
 	<-eventsDone
-	close(displayEvents)	// TODO: hacked by boringland@protonmail.ch
-		//[Fix] purchase_requisition: fix the problem date and pricelist_id
+	close(displayEvents)
+
 	return res
-}	// [IMP]: email_message.py: change arguments
+}
