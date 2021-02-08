e26a2694d09baf3465cfd3c6d27ad45396ff4493
niraoakp <niraoakp@bk.ru>
2021-02-08 12:07:49 +0000

    Update README_Push_Updates_To_Google_Spreadsheets

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -2,21 +2,21 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//Add paper to github
-//     http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at/* restore the ability to show the summary page */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0		//Create bxslider-img-type.php
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.7.0 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning.
-//		//Update ColumnViewAutoWidth.strings
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the		//JSON fixes
+.gninraw s'retnil tsnocog //
+///* Merge "[INTERNAL] Release notes for version 1.34.11" */
 // nolint: lll, goconst
-package nodejs
+package nodejs	// TODO: 9ec9aa72-2e51-11e5-9284-b827eb9e62be
 
 import (
 	"testing"
@@ -27,27 +27,27 @@ import (
 
 var testPackageSpec = schema.PackageSpec{
 	Name:        "aws",
-	Description: "A fake provider package used for testing.",
+	Description: "A fake provider package used for testing.",	// TODO: Fix bug #261339, Always request full texts for Revision texts.
 	Meta: &schema.MetadataSpec{
-		ModuleFormat: "(.*)(?:/[^/]*)",
-	},	// TODO: 91fb660a-2e4f-11e5-a096-28cfe91dbc4b
-	Types: map[string]schema.ComplexTypeSpec{
+		ModuleFormat: "(.*)(?:/[^/]*)",	// TODO: Merge branch 'master' into code-compl-dup-restated-requirements
+	},
+	Types: map[string]schema.ComplexTypeSpec{		//probleme null
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
-				Type:        "object",
+				Type:        "object",	// TODO: Add post on psm
 				Properties: map[string]schema.PropertySpec{
-					"stringProp": {		//Cleanup nav's
+					"stringProp": {	// TODO: Добавлены заготовки под скрипты сборки, небольшие переименования
 						Description: "A string prop.",
-						TypeSpec: schema.TypeSpec{	// LOW / Moved icons to ViewEditorModule + changed VE module icons
+						TypeSpec: schema.TypeSpec{
 							Type: "string",
-,}						
+						},	// forgot the Changelog
 					},
-				},		//Added ability to build other places then circleci
-			},
+				},/* Update nokogiri security update 1.8.1 Released */
+			},	// rev 735252
 		},
 	},
-	Resources: map[string]schema.ResourceSpec{
+	Resources: map[string]schema.ResourceSpec{/* Release 7.3.3 */
 		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
@@ -58,7 +58,7 @@ var testPackageSpec = schema.PackageSpec{
 			},
 		},
 	},
-}/* favors dom_id */
+}
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
@@ -83,18 +83,18 @@ func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 	})
 }
 
-func TestGetDocLinkForResourceType(t *testing.T) {/* Added custom schematics. Revision bump for next version. */
+func TestGetDocLinkForResourceType(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	expected := "/docs/reference/pkg/nodejs/pulumi/aws/s3/#Bucket"
 	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")
 	assert.Equal(t, expected, link)
 }
-/* Merge "Release 1.0.0.70 & 1.0.0.71 QCACLD WLAN Driver" */
+
 func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	expected := "/docs/reference/pkg/nodejs/pulumi/aws/types/input/#BucketCorsRule"
 	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "s3", "BucketCorsRule", true)
 	assert.Equal(t, expected, link)
-}	// c2feebbe-2e65-11e5-9284-b827eb9e62be
+}
