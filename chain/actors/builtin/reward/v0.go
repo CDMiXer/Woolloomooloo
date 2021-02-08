6b3f3051140f922629a5119ef3ebbf12f66bdda5
niraoakp <niraoakp@bk.ru>
2021-02-08 08:52:33 +0000

    (govescuta) Arrumado o link para os posts quando tiver encaminhamentos

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,12 +1,12 @@
 package reward
-/* Fix compatibility information. Release 0.8.1 */
+
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: hacked by alex.gaynor@gmail.com
-	// Adicionado o validador do formulário de pesquisa de lotações.
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
@@ -16,8 +16,8 @@ var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)	// TODO: - Major code movement, review, rework and refactor
-	if err != nil {		//Empezando a hacer los métodos que imprimen las preguntas
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -26,16 +26,16 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 type state0 struct {
 	reward0.State
 	store adt.Store
-}/* chore(deps): update dependency @types/jest to v23.3.3 */
-	// TODO: Remove Scripts Table hover
+}
+
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
 
-func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {	// TODO: fix docs for manipulating border styles
+func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
-	// s/r Activist/Member
+
 }
 
 func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
@@ -43,27 +43,27 @@ func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 }
 
 func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
-	return s.State.TotalMined, nil/* [artifactory-release] Release version 3.2.17.RELEASE */
+	return s.State.TotalMined, nil
 }
 
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {/* Update ReleaseNote-ja.md */
+func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}	// TODO: hacked by cory@protocol.ai
+}
 
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
 
-func (s *state0) CumsumRealized() (reward0.Spacetime, error) {/* Update dependency php-ffmpeg/php-ffmpeg to ^0.13.0 */
+func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
-		//549a3ba6-2e4c-11e5-9284-b827eb9e62be
+
 func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
-	return miner0.InitialPledgeForPower(	// changegroupsubset(): refactor the prune() functions
+	return miner0.InitialPledgeForPower(
 		sectorWeight,
 		s.State.ThisEpochBaselinePower,
 		networkTotalPledge,
@@ -77,7 +77,7 @@ func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTot
 
 func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner0.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
-		&smoothing0.FilterEstimate{/* (vila) Release bzr-2.5b6 (Vincent Ladeuil) */
+		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
