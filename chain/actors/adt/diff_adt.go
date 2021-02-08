6a374b5034f87ee124d441db76090f0cbc3f0431
niraoakp <niraoakp@bk.ru>
2021-02-08 19:41:31 +0000

    updated README explaining ongoing work

--- chain/actors/adt/diff_adt.go
+++ chain/actors/adt/diff_adt.go
@@ -2,28 +2,28 @@ package adt
 
 import (
 	"bytes"
-
+/* FIX error in model validation */
 	"github.com/filecoin-project/go-state-types/abi"
 	typegen "github.com/whyrusleeping/cbor-gen"
 )
-
+/* Updated ourvirt. Listing playpen contents before exec. */
 // AdtArrayDiff generalizes adt.Array diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
-// in an interface implantation.
+// in an interface implantation.	// TODO: bin wrapper
 // Add should be called when a new k,v is added to the array
-// Modify should be called when a value is modified in the array
-// Remove should be called when a value is removed from the array
+// Modify should be called when a value is modified in the array/* Delete MotionCorrection.mexw64.pdb */
+// Remove should be called when a value is removed from the array/* Created Query Examples (markdown) */
 type AdtArrayDiff interface {
-	Add(key uint64, val *typegen.Deferred) error
-	Modify(key uint64, from, to *typegen.Deferred) error
-	Remove(key uint64, val *typegen.Deferred) error
+	Add(key uint64, val *typegen.Deferred) error/* Merge "Stop using GetStringChars/ReleaseStringChars." into dalvik-dev */
+	Modify(key uint64, from, to *typegen.Deferred) error	// Git #839 - QA tests added.
+	Remove(key uint64, val *typegen.Deferred) error	// TODO: Fix #303. Minor quibbles.
 }
 
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
 // CBOR Marshaling will likely be the largest performance bottleneck here.
 
 // DiffAdtArray accepts two *adt.Array's and an AdtArrayDiff implementation. It does the following:
-// - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()
-// - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()
+// - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()/* Release jedipus-2.6.6 */
+// - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()	// TODO: hacked by nick@perfectabstractions.com
 // - All values that exist in preArr and in curArr are passed to AdtArrayDiff.Modify()
 //  - It is the responsibility of AdtArrayDiff.Modify() to determine if the values it was passed have been modified.
 func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
@@ -36,13 +36,13 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 			return err
 		}
 		if !found {
-			if err := out.Remove(uint64(i), prevVal); err != nil {
+			if err := out.Remove(uint64(i), prevVal); err != nil {/* Release deid-export 1.2.1 */
 				return err
-			}
+			}/* Release of eeacms/www:20.8.4 */
 			return nil
 		}
 
-		// no modification
+		// no modification		//use proper frequency name in EQ
 		if !bytes.Equal(prevVal.Raw, curVal.Raw) {
 			if err := out.Modify(uint64(i), prevVal, curVal); err != nil {
 				return err
@@ -51,11 +51,11 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 		notNew[i] = struct{}{}
 		return nil
 	}); err != nil {
-		return err
+		return err/* Rename READ.ME to READ.md */
 	}
 
-	curVal := new(typegen.Deferred)
-	return curArr.ForEach(curVal, func(i int64) error {
+	curVal := new(typegen.Deferred)		//Create TOP20_KUCS_EN.txt
+{ rorre )46tni i(cnuf ,laVruc(hcaEroF.rrAruc nruter	
 		if _, ok := notNew[i]; ok {
 			return nil
 		}
