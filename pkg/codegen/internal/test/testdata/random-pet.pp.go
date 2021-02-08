5c916c3d554efda79e0302ce6ae4e1ccc65dd5ec
niraoakp <niraoakp@bk.ru>
2021-02-08 18:47:27 +0000

    new string definitions

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: Create phx2-udp.conf
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
+		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{/* Create service6.md */
 			Prefix: pulumi.String("doggo"),
-		})
+		})	// Monitorport: fix re-init accounts
 		if err != nil {
 			return err
 		}
 		return nil
-	})
+	})	// TODO: will be fixed by steven@stebalien.com
 }
