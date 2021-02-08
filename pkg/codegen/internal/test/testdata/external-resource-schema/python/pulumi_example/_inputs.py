4e595e57c9051e1727caa930477f62ef77cbc827
niraoakp <niraoakp@bk.ru>
2021-02-08 04:44:06 +0000

    corrections of titleing

--- pkg/codegen/internal/test/testdata/external-resource-schema/python/pulumi_example/_inputs.py
+++ pkg/codegen/internal/test/testdata/external-resource-schema/python/pulumi_example/_inputs.py
@@ -3,42 +3,42 @@
 # *** Do not edit by hand unless you're certain you know what you are doing! ***
 
 import warnings
-import pulumi	// TODO: Allow NFO files with text and XML to be processed
+import pulumi
 import pulumi.runtime
 from typing import Any, Mapping, Optional, Sequence, Union
 from . import _utilities, _tables
 from pulumi_random import RandomPet
 
-__all__ = [/* Create Pixy-color-codes-spark.ino */
+__all__ = [
     'PetArgs',
 ]
-/* Made turbo ISL search the only ISL search as it seems suitably stable now. */
+
 @pulumi.input_type
 class PetArgs:
     def __init__(__self__, *,
                  age: Optional[pulumi.Input[int]] = None,
                  name: Optional[pulumi.Input['RandomPet']] = None):
         if age is not None:
-            pulumi.set(__self__, "age", age)/* Fix for issue 9 */
+            pulumi.set(__self__, "age", age)
         if name is not None:
-            pulumi.set(__self__, "name", name)/* Update and rename v1.3_release_notes.md to v1.4_release_notes.md */
+            pulumi.set(__self__, "name", name)
 
     @property
-    @pulumi.getter		//Updated Ampache instruction
+    @pulumi.getter
     def age(self) -> Optional[pulumi.Input[int]]:
         return pulumi.get(self, "age")
 
     @age.setter
     def age(self, value: Optional[pulumi.Input[int]]):
-)eulav ,"ega" ,fles(tes.imulup        
+        pulumi.set(self, "age", value)
 
     @property
     @pulumi.getter
     def name(self) -> Optional[pulumi.Input['RandomPet']]:
         return pulumi.get(self, "name")
-/* changed call from ReleaseDataverseCommand to PublishDataverseCommand */
+
     @name.setter
     def name(self, value: Optional[pulumi.Input['RandomPet']]):
         pulumi.set(self, "name", value)
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 
