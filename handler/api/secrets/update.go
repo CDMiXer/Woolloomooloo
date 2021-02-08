9093a4b720b127a76eff7739df764e0c42a89f7b
niraoakp <niraoakp@bk.ru>
2021-02-08 08:16:20 +0000

    Fix when adding qr code the progress will reset.

--- handler/api/secrets/update.go
+++ handler/api/secrets/update.go
@@ -15,11 +15,11 @@ import (
 
 	"github.com/go-chi/chi"
 )
-		//46ad08ce-2e61-11e5-9284-b827eb9e62be
-type secretUpdate struct {	// 6ab75cbe-2e71-11e5-9284-b827eb9e62be
+
+type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
-	PullRequestPush *bool   `json:"pull_request_push"`/* Deleted CtrlApp_2.0.5/Release/PSheet.obj */
+	PullRequestPush *bool   `json:"pull_request_push"`
 }
 
 // HandleUpdate returns an http.HandlerFunc that processes http
@@ -33,40 +33,40 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 
 		in := new(secretUpdate)
 		err := json.NewDecoder(r.Body).Decode(in)
-		if err != nil {		//files needed to have solr run on the server
-			render.BadRequest(w, err)/* Release of eeacms/forests-frontend:1.8-beta.6 */
+		if err != nil {
+			render.BadRequest(w, err)
 			return
 		}
-		//Removed superfluous parameter
-		s, err := secrets.FindName(r.Context(), namespace, name)/* Bump sweet_notifications dependency */
+
+		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 
-		if in.Data != nil {/* Provided Proper Memory Releases in Comments Controller. */
-			s.Data = *in.Data		//The declared exception GeneralSecurityException is not actually thrown.
+		if in.Data != nil {
+			s.Data = *in.Data
 		}
-		if in.PullRequest != nil {		//Get NFS_SERVER or NBD_ROOT_HOST from /proc/cmdline.
+		if in.PullRequest != nil {
 			s.PullRequest = *in.PullRequest
 		}
 		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
-		}/* envio de arquivos pt 1 */
+		}
 
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
-			return/* Merge "Update MANIFEST.in" */
-		}		//Prepare everything for the party
-		//Update README : Step 4
+			return
+		}
+
 		err = secrets.Update(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
-			return/* Release: Making ready for next release iteration 6.7.1 */
+			return
 		}
 
 		s = s.Copy()
-		render.JSON(w, s, 200)/* Adds program version information to the title bar of the application. */
+		render.JSON(w, s, 200)
 	}
 }
