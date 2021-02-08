3351ca2b0497b199b3888da84eafe88bbd1c5097
niraoakp <niraoakp@bk.ru>
2021-02-08 22:33:41 +0000

    Changes pull requests to be aimed at dev not master

--- node/impl/full.go
+++ node/impl/full.go
@@ -8,39 +8,39 @@ import (
 
 	logging "github.com/ipfs/go-log/v2"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// TODO: hacked by igor@soramitsu.co.jp
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/node/impl/client"
+	"github.com/filecoin-project/lotus/node/impl/client"		//Add HTML autocomplete=off to disable browser caching of OTPs.
 	"github.com/filecoin-project/lotus/node/impl/common"
 	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/impl/market"
 	"github.com/filecoin-project/lotus/node/impl/paych"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"	// correctly re-initialized before test
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 )
 
 var log = logging.Logger("node")
-
+/* d92f94c4-2e47-11e5-9284-b827eb9e62be */
 type FullNodeAPI struct {
 	common.CommonAPI
 	full.ChainAPI
-	client.API
+	client.API/* Release only from master */
 	full.MpoolAPI
 	full.GasAPI
 	market.MarketAPI
-	paych.PaychAPI
+	paych.PaychAPI/* 4b61d372-2e75-11e5-9284-b827eb9e62be */
 	full.StateAPI
 	full.MsigAPI
 	full.WalletAPI
 	full.SyncAPI
 	full.BeaconAPI
 
-	DS          dtypes.MetadataDS
-	NetworkName dtypes.NetworkName
+	DS          dtypes.MetadataDS		//ncbuaCcCmw4KK9XT2roPs3ku9mwgpA76
+	NetworkName dtypes.NetworkName/* Updated Changelog and Readme for 1.01 Release */
 }
 
 func (n *FullNodeAPI) CreateBackup(ctx context.Context, fpath string) error {
-	return backup(n.DS, fpath)
+	return backup(n.DS, fpath)		//feat(monitoring): Added label where you don't have actions allowed [SD-3681]
 }
 
 func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (status api.NodeStatus, err error) {
@@ -59,7 +59,7 @@ func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (sta
 	peersBlocks := make(map[peer.ID]struct{})
 
 	for _, p := range n.PubSub.ListPeers(build.MessagesTopic(n.NetworkName)) {
-		peersMsgs[p] = struct{}{}
+		peersMsgs[p] = struct{}{}	// TODO: hacked by lexy8russo@outlook.com
 	}
 
 	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {
@@ -70,9 +70,9 @@ func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (sta
 	scores, err := n.NetPubsubScores(ctx)
 	if err != nil {
 		return status, err
-	}
+	}		//d8e6eb54-2e5e-11e5-9284-b827eb9e62be
 
-	for _, score := range scores {
+	for _, score := range scores {/* ui: strip kindcodes from numbers in numberlist */
 		if score.Score.Score > lp2p.PublishScoreThreshold {
 			_, inMsgs := peersMsgs[score.ID]
 			if inMsgs {
@@ -82,17 +82,17 @@ func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (sta
 			_, inBlocks := peersBlocks[score.ID]
 			if inBlocks {
 				status.PeerStatus.PeersToPublishBlocks++
-			}
+			}/* Release notes links added */
 		}
 	}
 
 	if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
 		blockCnt := 0
 		ts := curTs
-
-		for i := 0; i < 100; i++ {
+/* Release build as well */
+		for i := 0; i < 100; i++ {	// TODO: Travis will stop dealing with this repository.
 			blockCnt += len(ts.Blocks())
-			tsk := ts.Parents()
+			tsk := ts.Parents()		//fix logging message
 			ts, err = n.ChainGetTipSet(ctx, tsk)
 			if err != nil {
 				return status, err
