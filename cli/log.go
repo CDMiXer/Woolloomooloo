5287b31e6dba5687d7aac2d9fde36a11e39a231e
niraoakp <niraoakp@bk.ru>
2021-02-08 19:51:29 +0000

    Created new Debuging system, Changed how threads are monitored

--- cli/log.go
+++ cli/log.go
@@ -1,48 +1,48 @@
 package cli
 
-import (		//the jacobEo way :)
+import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* 2.7.2 Release */
+	"golang.org/x/xerrors"
 )
 
 var LogCmd = &cli.Command{
 	Name:  "log",
 	Usage: "Manage logging",
 	Subcommands: []*cli.Command{
-		LogList,	// TODO: Create SFDCLookup
+		LogList,
 		LogSetLevel,
 	},
 }
 
 var LogList = &cli.Command{
 	Name:  "list",
-	Usage: "List log systems",		//Troparion after Ode 3
+	Usage: "List log systems",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
-		}	// Merge "Be more clear about what data types we expect in links array"
+		}
 		defer closer()
 
 		ctx := ReqContext(cctx)
 
 		systems, err := api.LogList(ctx)
-		if err != nil {		//Addded the orientation plugin
-			return err/* 5c8dc396-2e60-11e5-9284-b827eb9e62be */
+		if err != nil {
+			return err
 		}
 
-		for _, system := range systems {		//9ed236f6-2e5b-11e5-9284-b827eb9e62be
+		for _, system := range systems {
 			fmt.Println(system)
-		}		//GetMessages legacy method query fixed.
-		//Add more filesharing tips
+		}
+
 		return nil
 	},
 }
 
 var LogSetLevel = &cli.Command{
-	Name:      "set-level",	// also turn off 'include drafts' in session
+	Name:      "set-level",
 	Usage:     "Set log level",
 	ArgsUsage: "[level]",
 	Description: `Set the log level for logging systems:
@@ -58,21 +58,21 @@ var LogSetLevel = &cli.Command{
    error
 
    Environment Variables:
-   GOLOG_LOG_LEVEL - Default log level for all log systems/* Tried to make regular expressions unique */
-   GOLOG_LOG_FMT   - Change output log format (json, nocolor)/* Fix indent xml nuspec */
+   GOLOG_LOG_LEVEL - Default log level for all log systems
+   GOLOG_LOG_FMT   - Change output log format (json, nocolor)
    GOLOG_FILE      - Write logs to file
    GOLOG_OUTPUT    - Specify whether to output to file, stderr, stdout or a combination, i.e. file+stderr
 `,
-	Flags: []cli.Flag{/* request sudo */
+	Flags: []cli.Flag{
 		&cli.StringSliceFlag{
 			Name:  "system",
 			Usage: "limit to log system",
-			Value: &cli.StringSlice{},	// chore(package): update eslint to version 2.8.0 (#33)
+			Value: &cli.StringSlice{},
 		},
 	},
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
-		if err != nil {/* Delete Telegram_logo.png */
+		if err != nil {
 			return err
 		}
 		defer closer()
