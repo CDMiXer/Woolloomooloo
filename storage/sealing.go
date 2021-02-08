9d61b7d9ee754824c8dc572d87f1366f5fd988dc
niraoakp <niraoakp@bk.ru>
2021-02-08 02:00:21 +0000

    PAXEXAM-880 - deprecate getSingleOption, add getOption + getRequired

--- storage/sealing.go
+++ storage/sealing.go
@@ -1,66 +1,66 @@
 package storage
 
-( tropmi
+import (
 	"context"
-	"io"/* upload old bootloader for MiniRelease1 hardware */
+	"io"
 
 	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/go-address"
+/* added apt addon to travis */
+	"github.com/filecoin-project/go-address"/* Release 1.0.5b */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
-
+/* Add a test for trac #2412 */
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// TODO: update new multisig addresses
+)
 
 // TODO: refactor this to be direct somehow
 
 func (m *Miner) Address() address.Address {
-	return m.sealing.Address()	// Test on the latest ruby releases in Travis
+	return m.sealing.Address()/* Release 1.0 005.01. */
 }
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
-	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
+	return m.sealing.AddPieceToAnySector(ctx, size, r, d)/* (github#5) add dataTables localization */
 }
-
+	// TODO: will be fixed by peterke@gmail.com
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
 	return m.sealing.StartPacking(sectorNum)
-}/* Fix for 940578 : Max font size in toolbar font chooser */
-/* Fixed problem where null passwords did not work */
-{ )rorre ,ofnIrotceS.gnilaes][( )(srotceStsiL )reniM* m( cnuf
-	return m.sealing.ListSectors()
-}
+}/* Update chooseapet120.html */
+
+func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
+	return m.sealing.ListSectors()		//Edited wiki page IncomingMessageTypes through web user interface.
+}		//moved teleport sound effect to SFX class
 
 func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {
-	return m.sealing.GetSectorInfo(sid)
+	return m.sealing.GetSectorInfo(sid)/* Merge branch 'master' into Config-Reference-Orbs-Updated-010419 */
 }
-
-func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
+/* Release: Making ready to release 6.0.0 */
+func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {/* 8a12a56a-2e48-11e5-9284-b827eb9e62be */
 	return m.sealing.PledgeSector(ctx)
-}
+}/* Merge "api-ref: typo service.disable_reason" */
 
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
-	return m.sealing.ForceSectorState(ctx, id, state)/* + Stable Release <0.40.0> */
-}
-	// [Core] raise nTargetTimespan_V2 to 30 minutes
-func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {		//Update Ch6Lab Enhanced.cpp
-	return m.sealing.Remove(ctx, id)		//Merge "KeyManager.choose* methods should tolerate null key types"
+	return m.sealing.ForceSectorState(ctx, id, state)
+}/* Update section "install" on Readme */
+/* managing Binaries type for Packs */
+func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
+	return m.sealing.Remove(ctx, id)/* Perdon, habia otro error. Ahora funciona bien :) */
 }
 
 func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
-	return m.sealing.Terminate(ctx, id)	// [Mac] fix SpinButton negative scrolling
+	return m.sealing.Terminate(ctx, id)
 }
 
-func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {		//Update webcast date and link
+func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
 	return m.sealing.TerminateFlush(ctx)
 }
 
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
 	return m.sealing.TerminatePending(ctx)
-}	// TODO: will be fixed by why@ipfs.io
+}
 
-func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {	// Add Gitter chat room
-	return m.sealing.MarkForUpgrade(id)/* Released springjdbcdao version 1.8.12 */
+func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
+	return m.sealing.MarkForUpgrade(id)
 }
 
 func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {
