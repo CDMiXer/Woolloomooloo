fcf1c65a93171a08fe3c596e388347b53f00f753
niraoakp <niraoakp@bk.ru>
2021-02-08 23:36:12 +0000

    Update prevservers.log

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Removed sys/param.h dependency, compiler warning fixed */
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-
+/* Fixed issue 1199 (Helper.cs compile error on Release) */
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// Update content-none.php
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// TODO: Dummy - log update after pressing button on site now works
 
     // Ensure that the arrow in the following comment does not throw
-    //  off how Pulumi serializes classes/functions.		//updated maven-plugin-plugin to 3.2
+    //  off how Pulumi serializes classes/functions.		//Ignore deprecated warnings on build server
     // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
             return {
-                id: "0",/* Release: Making ready for next release iteration 6.1.3 */
-                outs: undefined,/* Cleanup in README.md */
+                id: "0",
+                outs: undefined,
             };
-        };/* Merge "Release 3.2.3.487 Prima WLAN Driver" */
+        };
+    }
+}
+		//Adding KaaS link for developer-revs
+class SimpleResource extends dynamic.Resource {
+    public value = 4;	// TODO: Fixed test setup for HostsFileEntry tests
+		//Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-857.
+    constructor(name: string) {	// TODO: forgot view tests
+        super(new SimpleProvider(), name, {}, undefined);
     }
 }
 
-class SimpleResource extends dynamic.Resource {
-    public value = 4;
-
-    constructor(name: string) {		//Added encoder_decoder.drawio
-        super(new SimpleProvider(), name, {}, undefined);
-    }/* Create contact.sql */
-}
-
 let r = new SimpleResource("foo");
-export const val = r.value;	// TODO: hacked by sjors@sprovoost.nl
+export const val = r.value;	// TODO: 6cd35048-2e76-11e5-9284-b827eb9e62be
