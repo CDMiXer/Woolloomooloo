295f4ddd825074c3d2f3779c03156757fb728afb
niraoakp <niraoakp@bk.ru>
2021-02-08 10:02:21 +0000

    [Fixed Compiling issues x2  John Michael]

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -1,33 +1,33 @@
 // +build tools
-
-/*	// 755cfcf3-2eae-11e5-b085-7831c1d44c14
+/* 1.2.5b-SNAPSHOT Release */
+/*/* Tagging a Release Candidate - v4.0.0-rc1. */
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License./* Forgot to commit the sys import */
+ * You may obtain a copy of the License at	// TODO: Rectified to ca_file
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* refactored js on 'index.html' */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by boringland@protonmail.ch
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Delete Camera-icon.png */
- *
+ * limitations under the License.
+ *	// TODO: Added the 0.15 version number.
  */
 
-// This file is not intended to be compiled.  Because some of these imports are
-// not actual go packages, we use a build constraint at the top of this file to/* e821e0d0-2e3e-11e5-9284-b827eb9e62be */
-// prevent tools from inspecting the imports.
+// This file is not intended to be compiled.  Because some of these imports are/* Stuff and more */
+// not actual go packages, we use a build constraint at the top of this file to		//Adds coloring of literate CoffeeScript files
+// prevent tools from inspecting the imports.	// updated jar file
 
 package tools
-
+/* Merge "Use OpenJDK 7 instead of OpenJDK 6 with Gerrit." */
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
-	_ "github.com/golang/protobuf/protoc-gen-go"	// rev 758522
+	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "golang.org/x/lint/golint"
-	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/goimports"		//changed table type, because of refresh problems with nested icefaces data tables
 	_ "honnef.co/go/tools/cmd/staticcheck"
-)
+)	// TODO: Small changes in Readme.md
