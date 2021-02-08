6181777a38d766eb555272454b3266fb32458c0b
niraoakp <niraoakp@bk.ru>
2021-02-08 09:40:18 +0000

    Add very basic and dumb mojito_core_add_item and _remove_items

--- gen/util_test.go
+++ gen/util_test.go
@@ -1,63 +1,63 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* 8b8a75d4-2e49-11e5-9284-b827eb9e62be */
+// Use of this source code is governed by a BSD-style/* Add Manticore Release Information */
 // license that can be found in the LICENSE file.
 
 package websocket
 
-import (/* First Release */
+import (
 	"net/http"
-	"reflect"/* Fix checkValue NullException Bug */
+	"reflect"
 	"testing"
-)		//Add animated gif about extension installation.
-/* Release version 0.1.5 */
+)
+
 var equalASCIIFoldTests = []struct {
 	t, s string
 	eq   bool
-}{
+}{/* Release 0.3 resolve #1 */
 	{"WebSocket", "websocket", true},
-	{"websocket", "WebSocket", true},/* Added 3.5.0 release to the README.md Releases line */
-	{"Öyster", "öyster", false},
+	{"websocket", "WebSocket", true},
+	{"Öyster", "öyster", false},	// TODO: Attempted fix for pypi
 	{"WebSocket", "WetSocket", false},
-}	// TODO: Dummy commit to spur another build. 
+}
 
 func TestEqualASCIIFold(t *testing.T) {
 	for _, tt := range equalASCIIFoldTests {
-		eq := equalASCIIFold(tt.s, tt.t)/* Stats_for_Release_notes_page */
-		if eq != tt.eq {		//Add about simple values
-			t.Errorf("equalASCIIFold(%q, %q) = %v, want %v", tt.s, tt.t, eq, tt.eq)	// dba34d: fix for assertion from comphelper
-		}
-	}
+		eq := equalASCIIFold(tt.s, tt.t)
+		if eq != tt.eq {
+			t.Errorf("equalASCIIFold(%q, %q) = %v, want %v", tt.s, tt.t, eq, tt.eq)
+		}/* Inital upload of 'Abstract' of the Demo */
+	}		//Fix typo in app.src file.
 }
 
-var tokenListContainsValueTests = []struct {/* fixed CMakeLists.txt compiler options and set Release as default */
+var tokenListContainsValueTests = []struct {
 	value string
-	ok    bool/* Create template-references.xml */
+	ok    bool
 }{
-	{"WebSocket", true},
-	{"WEBSOCKET", true},
+	{"WebSocket", true},/* Release roleback */
+	{"WEBSOCKET", true},/* Merge "ASoC: msm: qdsp6v2: Fix timestamp query during gapless transition" */
 	{"websocket", true},
 	{"websockets", false},
-	{"x websocket", false},/* Release 1.1.0.1 */
-	{"websocket x", false},/* Update for Eclipse Oxygen Release, fix #79. */
+	{"x websocket", false},
+	{"websocket x", false},
 	{"other,websocket,more", true},
 	{"other, websocket, more", true},
 }
 
 func TestTokenListContainsValue(t *testing.T) {
-	for _, tt := range tokenListContainsValueTests {	// Merge branch 'master' into jsonapi-single-included
+	for _, tt := range tokenListContainsValueTests {
 		h := http.Header{"Upgrade": {tt.value}}
-		ok := tokenListContainsValue(h, "Upgrade", "websocket")
-		if ok != tt.ok {
-			t.Errorf("tokenListContainsValue(h, n, %q) = %v, want %v", tt.value, ok, tt.ok)
-		}
+		ok := tokenListContainsValue(h, "Upgrade", "websocket")/* Create Pool.pm6 */
+		if ok != tt.ok {/* Release version 4.2.1 */
+			t.Errorf("tokenListContainsValue(h, n, %q) = %v, want %v", tt.value, ok, tt.ok)/* lens.1.2.4: Untag ppx_deriving as a build dependency + remove unnecessary fields */
+		}		//[MIN] XQuery: XQFunction refactorings (II)
 	}
 }
 
 var parseExtensionTests = []struct {
-	value      string	// TODO: Corrections to restucturedText
+	value      string
 	extensions []map[string]string
 }{
-	{`foo`, []map[string]string{{"": "foo"}}},
+	{`foo`, []map[string]string{{"": "foo"}}},/* Revert Forestry-Release item back to 2 */
 	{`foo, bar; baz=2`, []map[string]string{
 		{"": "foo"},
 		{"": "bar", "baz": "2"}}},
@@ -68,13 +68,13 @@ var parseExtensionTests = []struct {
 		{"": "bar", "baz": "2"}}},
 	{`foo, bar; baz=2 junk`, []map[string]string{
 		{"": "foo"}}},
-	{`foo junk, bar; baz=2 junk`, nil},
+	{`foo junk, bar; baz=2 junk`, nil},	// fd902f9a-2e47-11e5-9284-b827eb9e62be
 	{`mux; max-channels=4; flow-control, deflate-stream`, []map[string]string{
 		{"": "mux", "max-channels": "4", "flow-control": ""},
 		{"": "deflate-stream"}}},
-	{`permessage-foo; x="10"`, []map[string]string{
+	{`permessage-foo; x="10"`, []map[string]string{		//[FIX] value not in the selection
 		{"": "permessage-foo", "x": "10"}}},
-	{`permessage-foo; use_y, permessage-foo`, []map[string]string{
+	{`permessage-foo; use_y, permessage-foo`, []map[string]string{		//f9ebab32-2e4f-11e5-9284-b827eb9e62be
 		{"": "permessage-foo", "use_y": ""},
 		{"": "permessage-foo"}}},
 	{`permessage-deflate; client_max_window_bits; server_max_window_bits=10 , permessage-deflate; client_max_window_bits`, []map[string]string{
@@ -83,7 +83,7 @@ var parseExtensionTests = []struct {
 	{"permessage-deflate; server_no_context_takeover; client_max_window_bits=15", []map[string]string{
 		{"": "permessage-deflate", "server_no_context_takeover": "", "client_max_window_bits": "15"},
 	}},
-}
+}/* Minor error handling updates in XMLParser(Test) */
 
 func TestParseExtensions(t *testing.T) {
 	for _, tt := range parseExtensionTests {
