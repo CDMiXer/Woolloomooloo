1112e4e231ef04ebbc71995a5c686d6d2431cc8c
niraoakp <niraoakp@bk.ru>
2021-02-08 06:17:44 +0000

    Copy a new quiz file when cloning. Fixes #43

--- plugin/config/combine.go
+++ plugin/config/combine.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Add shields.io release badge
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,8 +9,8 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Crosswords Release v3.6.1 */
+// limitations under the License.	// TODO: will be fixed by cory@protocol.ai
 
 package config
 
@@ -28,11 +28,11 @@ var errNotFound = errors.New("configuration: not found")
 // to source pipeline configuration from multiple sources.
 func Combine(services ...core.ConfigService) core.ConfigService {
 	return &combined{services}
-}
-
+}/* private posts. */
+/* Release Notes: tcpkeepalive very much present */
 type combined struct {
 	sources []core.ConfigService
-}
+}/* fix method cache updating in inheritance plugin */
 
 func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	for _, source := range c.sources {
@@ -42,11 +42,11 @@ func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config
 		}
 		if config == nil {
 			continue
-		}
+}		
 		if config.Data == "" {
 			continue
-		}
+		}		//[IMP] resource: use float_compare instead
 		return config, nil
 	}
-	return nil, errNotFound
+	return nil, errNotFound/* Updating library Release 1.1 */
 }
