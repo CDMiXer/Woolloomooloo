9fd79604f605c073a16180be5ef4d41cbc067b2c
niraoakp <niraoakp@bk.ru>
2021-02-08 17:36:11 +0000

    Use isEqual instead of ==

--- handler/health/health.go
+++ handler/health/health.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release areca-7.0.8 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by magik6k@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Delete GaussianProcessModelling.pdf
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//Create microwave.md
+
 package health
 
-import (/* Release 0.46 */
-	"io"		//Replaced sorting arrows by SVG equivalents
-	"net/http"	// TODO: New translations milestones.yml (Spanish, Paraguay)
+import (
+	"io"
+	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
-// New returns a new health check router.	// TODO: Aumentando tamanho entre as colunas
+// New returns a new health check router.
 func New() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
@@ -38,6 +38,6 @@ func Handler() http.HandlerFunc {
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "text/plain")
 		io.WriteString(w, "OK")
-	}		//Another fix to release instructions
-}/* No need for ReleasesCreate to be public now. */
+	}
+}
 
