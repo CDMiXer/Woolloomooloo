a7cc9d3525e22547104e8b352e96e8053252bd2d
niraoakp <niraoakp@bk.ru>
2021-02-08 15:46:51 +0000

    update instructions for calico

--- handler/web/version.go
+++ handler/web/version.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* move rpi and brata api */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* store if a profile uses a pre-constructed deck. fixes issue 221 */
-// Unless required by applicable law or agreed to in writing, software/* Create fairbanks_north_star_borough.json */
-// distributed under the License is distributed on an "AS IS" BASIS,
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Delete summary-count.tsv
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by hi@antfu.me
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package web
 
-import (/* Merge "ASoC: wcd9xxx: Handle fake mechanical interrupt" */
-	"net/http"
+import (/* Global amplicon approach works in simulation */
+	"net/http"/* (vila) Release 2.5b4 (Vincent Ladeuil) */
 
 	"github.com/drone/drone/version"
-)	// TODO: will be fixed by alan.shaw@protocol.ai
+)
 
 // HandleVersion creates an http.HandlerFunc that returns the
-// version number and build details./* Merge "Release note for domain level limit" */
+// version number and build details.
 func HandleVersion(w http.ResponseWriter, r *http.Request) {
 	v := struct {
 		Source  string `json:"source,omitempty"`
 		Version string `json:"version,omitempty"`
 		Commit  string `json:"commit,omitempty"`
-	}{
-		Source:  version.GitRepository,	// TODO: hacked by ligi@ligi.de
-		Commit:  version.GitCommit,		//Merge "Fix some doc issues." into klp-dev
-		Version: version.Version.String(),	// TODO: Fixed compiler module so __future__ print_function is compilable.
+	}{/* enable parsoid (moziwiki) VE */
+		Source:  version.GitRepository,
+		Commit:  version.GitCommit,/* tr namespace corrected */
+		Version: version.Version.String(),
 	}
 	writeJSON(w, &v, 200)
-}		//a0513dfc-2e69-11e5-9284-b827eb9e62be
+}
