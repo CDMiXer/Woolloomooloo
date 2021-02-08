52664b0485cd2ad21bcc59110596a8466270e6d4
niraoakp <niraoakp@bk.ru>
2021-02-08 08:14:09 +0000

    Remove special case 'MetaData' in method 'createOrUpdate'

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* @Release [io7m-jcanephora-0.29.0] */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
