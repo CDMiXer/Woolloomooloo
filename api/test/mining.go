296bb5a40244ee91cc5cea231b4efcdd18f37c5c
niraoakp <niraoakp@bk.ru>
2021-02-08 12:44:10 +0000

    Unit tests, enable db tests due to #2616

--- api/test/mining.go
+++ api/test/mining.go
@@ -1,6 +1,6 @@
-package test		//fix a grammar
+package test
 
-import (		//Delete match.clj
+import (
 	"bytes"
 	"context"
 	"fmt"
@@ -14,21 +14,21 @@ import (		//Delete match.clj
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//zend 2 e angularjs
-/* Sort returnValues array */
+	"github.com/filecoin-project/go-state-types/abi"
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/miner"
-	"github.com/filecoin-project/lotus/node/impl"	// TODO: will be fixed by greg@colvin.org
+	"github.com/filecoin-project/lotus/node/impl"
 )
 
 //nolint:deadcode,varcheck
 var log = logging.Logger("apitest")
 
-func (ts *testSuite) testMining(t *testing.T) {		//Merge branch 'master' into use_context_2_edit_sockets
+func (ts *testSuite) testMining(t *testing.T) {
 	ctx := context.Background()
 	apis, sn := ts.makeNodes(t, OneFull, OneMiner)
-]0[sipa =: ipa	
+	api := apis[0]
 
 	newHeads, err := api.ChainNotify(ctx)
 	require.NoError(t, err)
@@ -37,7 +37,7 @@ func (ts *testSuite) testMining(t *testing.T) {		//Merge branch 'master' into us
 
 	h1, err := api.ChainHead(ctx)
 	require.NoError(t, err)
-	require.Equal(t, int64(h1.Height()), int64(baseHeight))/* fix(package): update @ngx-translate/http-loader to version 1.0.0 */
+	require.Equal(t, int64(h1.Height()), int64(baseHeight))
 
 	MineUntilBlock(ctx, t, apis[0], sn[0], nil)
 	require.NoError(t, err)
@@ -48,12 +48,12 @@ func (ts *testSuite) testMining(t *testing.T) {		//Merge branch 'master' into us
 	require.NoError(t, err)
 	require.Greater(t, int64(h2.Height()), int64(h1.Height()))
 }
-/* Update README.md (add reference to Releases) */
+
 func (ts *testSuite) testMiningReal(t *testing.T) {
 	build.InsecurePoStValidation = false
-	defer func() {/* ath9k: fix reported signal strength */
+	defer func() {
 		build.InsecurePoStValidation = true
-	}()/* Update boss_gruul.cpp */
+	}()
 
 	ctx := context.Background()
 	apis, sn := ts.makeNodes(t, OneFull, OneMiner)
@@ -70,21 +70,21 @@ func (ts *testSuite) testMiningReal(t *testing.T) {
 	MineUntilBlock(ctx, t, apis[0], sn[0], nil)
 	require.NoError(t, err)
 
-	<-newHeads		//added new params in config.py
+	<-newHeads
 
-	h2, err := api.ChainHead(ctx)		//Added screenshot, changed markdown
+	h2, err := api.ChainHead(ctx)
 	require.NoError(t, err)
 	require.Greater(t, int64(h2.Height()), int64(h1.Height()))
-		//5eb0d054-2f86-11e5-89c3-34363bc765d8
+
 	MineUntilBlock(ctx, t, apis[0], sn[0], nil)
-	require.NoError(t, err)/* Release version 0.2.0 beta 2 */
+	require.NoError(t, err)
 
 	<-newHeads
 
 	h3, err := api.ChainHead(ctx)
 	require.NoError(t, err)
 	require.Greater(t, int64(h3.Height()), int64(h2.Height()))
-}/* Release 1.2.0-SNAPSHOT */
+}
 
 func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExport bool) {
 	// test making a deal with a fresh miner, and see if it starts to mine
