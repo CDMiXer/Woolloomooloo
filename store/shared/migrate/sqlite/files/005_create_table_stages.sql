34582482cb212a36fe3266fa83a48b18f5b1b3d6
niraoakp <niraoakp@bk.ru>
2021-02-08 17:53:11 +0000

    test markdown file

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,17 +1,17 @@
 -- name: create-table-stages
-
-CREATE TABLE IF NOT EXISTS stages (
+	// TODO: django1.8 compat
+CREATE TABLE IF NOT EXISTS stages (	// move logdetail into CrashHandler.cpp
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
-,stage_number      INTEGER
+,stage_number      INTEGER		//Add namespace test
 ,stage_kind        TEXT
-,stage_type        TEXT
+,stage_type        TEXT	// set rm=true
 ,stage_name        TEXT
 ,stage_status      TEXT
-,stage_error       TEXT
+,stage_error       TEXT/* Release version 3.0.0.RC1 */
 ,stage_errignore   BOOLEAN
-,stage_exit_code   INTEGER
+,stage_exit_code   INTEGER/* Add support for 4.1-4.1.1 replays. Release Scelight 6.2.27. */
 ,stage_limit       INTEGER
 ,stage_os          TEXT
 ,stage_arch        TEXT
@@ -21,17 +21,17 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_started     INTEGER
 ,stage_stopped     INTEGER
 ,stage_created     INTEGER
-,stage_updated     INTEGER
+,stage_updated     INTEGER/* =add categories, add project_parameters */
 ,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
-,stage_depends_on  TEXT
+,stage_depends_on  TEXT		//weitere kleine Erweiterungen und HttpPostRequest.cs hinzugefügt
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
-,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
+,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE/* Added version to Maven plugins */
 );
 
--- name: create-index-stages-build
+-- name: create-index-stages-build/* Release 0.2.0. */
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
