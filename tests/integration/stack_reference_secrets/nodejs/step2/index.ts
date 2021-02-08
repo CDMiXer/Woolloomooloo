e56dde81594fd5a306b1a4f6d81d5e52637d9a1b
niraoakp <niraoakp@bk.ru>
2021-02-08 06:32:10 +0000

    Merge branch 'master' into s124-dev-to-master

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");	// Graphical additions and adjustments
+export const secret = pulumi.secret("secret");
 
-// Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous//* Changed the script file */
+// Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
-const org = new pulumi.Config().require("org");	// use a dialog to add users to an address-book re #4220
+const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
 const stack = pulumi.getStack();
-const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);/* [FIX] website editor snippet: double insert */
+const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
 
-export const refNormal = sr.getOutput("normal");		//Merge "Bump domino dependency to 1.0.11."
-export const refSecret = sr.getOutput("secret");
+export const refNormal = sr.getOutput("normal");
+export const refSecret = sr.getOutput("secret");/* [README] Release 0.3.0 */
