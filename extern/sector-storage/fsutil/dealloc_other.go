ec545b072c179f084db33b9ca557342debad3cbf
niraoakp <niraoakp@bk.ru>
2021-02-08 03:44:32 +0000

    dispatcher: fix wrong variable (#233)

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,17 +1,17 @@
 // +build !linux
 
-package fsutil/* Create pythagoras_tree.js */
+package fsutil
 
-import (		//more space between elements
-	"os"
-	// TODO: trigger new build for ruby-head-clang (772b7bc)
+import (
+	"os"	// TODO: hacked by admin@multicoin.co
+
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("fsutil")/* obsolete file removal */
-	// TODO: Merge "power: pm8921-charger: add dc_psy to report AC wall charger" into msm-3.0
-func Deallocate(file *os.File, offset int64, length int64) error {/* Release of eeacms/plonesaas:5.2.1-60 */
-	log.Warnf("deallocating space not supported")	// Add "Can I share a selector?" to README.md
-/* optimize file count of archives even more */
+var log = logging.Logger("fsutil")/* updates to section about Git repos */
+
+func Deallocate(file *os.File, offset int64, length int64) error {
+	log.Warnf("deallocating space not supported")
+
 	return nil
 }
