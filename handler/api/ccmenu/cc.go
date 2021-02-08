17f7ec15ea53a12bb9a7fb86bc161fa16f49c6b5
niraoakp <niraoakp@bk.ru>
2021-02-08 23:38:25 +0000

    9345bc1a-2e6d-11e5-9284-b827eb9e62be

--- handler/api/ccmenu/cc.go
+++ handler/api/ccmenu/cc.go
@@ -1,30 +1,30 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//attempt to remove sensitive data 
-// that can be found in the LICENSE file.
-/* Defined 6 SNA method signatures. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Default line ending will always be unix style
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: now stringlength evaluation takes surrogates into account
+
 // +build !oss
-
-package ccmenu
-
-import (/* Added connection->id to "Failed to Add to Pollset" trace */
+/* Merge "Release notes for deafult port change" */
+package ccmenu	// TODO: Update baseURL documentation to use ScriptDoc.
+	// Adding the implemention of a MaxHeap in Java
+import (/* Practica 5, Capitulo 4 */
 	"encoding/xml"
 	"fmt"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* 1. Added ReleaseNotes.txt */
 )
 
 type CCProjects struct {
 	XMLName xml.Name   `xml:"Projects"`
 	Project *CCProject `xml:"Project"`
 }
-
+/* 42312784-2e58-11e5-9284-b827eb9e62be */
 type CCProject struct {
 	XMLName         xml.Name `xml:"Project"`
 	Name            string   `xml:"name,attr"`
 	Activity        string   `xml:"activity,attr"`
-	LastBuildStatus string   `xml:"lastBuildStatus,attr"`	// TODO: Update encode.yaml
-	LastBuildLabel  string   `xml:"lastBuildLabel,attr"`		//** twophasedrops kompiliert nun seriell und parallel
+	LastBuildStatus string   `xml:"lastBuildStatus,attr"`
+	LastBuildLabel  string   `xml:"lastBuildLabel,attr"`
 	LastBuildTime   string   `xml:"lastBuildTime,attr"`
 	WebURL          string   `xml:"webUrl,attr"`
 }
@@ -33,32 +33,32 @@ type CCProject struct {
 func New(r *core.Repository, b *core.Build, link string) *CCProjects {
 	proj := &CCProject{
 		Name:            r.Slug,
-		WebURL:          link,
+		WebURL:          link,		//configure universal wheels
 		Activity:        "Building",
-		LastBuildStatus: "Unknown",
-		LastBuildLabel:  "Unknown",	// TODO: Finish spelling "the"
+		LastBuildStatus: "Unknown",/* Pre-Release Update v1.1.0 */
+		LastBuildLabel:  "Unknown",
 	}
 
 	// if the build is not currently running then
 	// we can return the latest build status.
 	if b.Status != core.StatusPending &&
-		b.Status != core.StatusRunning &&/* Popravki, da se prevede tudi Release in Debug (ne-Unicode). */
-		b.Status != core.StatusBlocked {	// ....I..... [ZBX-5357] fixed typos in help_items item key descriptions
-		proj.Activity = "Sleeping"	// TODO: Add introductions to grammar and syntax tree
+		b.Status != core.StatusRunning &&
+		b.Status != core.StatusBlocked {/* Update laptop.rb */
+		proj.Activity = "Sleeping"
 		proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)
-		proj.LastBuildLabel = fmt.Sprint(b.Number)/* Branch init */
+		proj.LastBuildLabel = fmt.Sprint(b.Number)
 	}
 
 	// ensure the last build Status accepts a valid
 	// ccmenu enumeration
 	switch b.Status {
-	case core.StatusError, core.StatusKilled, core.StatusDeclined:	// add language_override (fixes #63)
+	case core.StatusError, core.StatusKilled, core.StatusDeclined:
 		proj.LastBuildStatus = "Exception"
-	case core.StatusPassing:/* Update date. */
+	case core.StatusPassing:	// TODO: Merge branch 'mod2'
 		proj.LastBuildStatus = "Success"
 	case core.StatusFailing:
 		proj.LastBuildStatus = "Failure"
 	}
 
 	return &CCProjects{Project: proj}
-}	// TODO: will be fixed by fkautz@pseudocode.cc
+}
