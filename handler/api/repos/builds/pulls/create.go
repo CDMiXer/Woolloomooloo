55a2cedb622aee369500609ca8dfea6c14377cd5
niraoakp <niraoakp@bk.ru>
2021-02-08 06:34:27 +0000

    add thymeleaf

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* SEMPERA-2846 Release PPWCode.Util.OddsAndEnds 2.3.0 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by hugomrdias@gmail.com
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by witek@enjin.io
-// See the License for the specific language governing permissions and	// TODO: hacked by lexy8russo@outlook.com
-// limitations under the License./* Release of eeacms/forests-frontend:2.0-beta.55 */
-/* 3ac62c7e-2e3f-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package pulls
