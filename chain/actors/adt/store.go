3466c4ef0ea0805694f3eb5f7618ee801bfe241e
niraoakp <niraoakp@bk.ru>
2021-02-08 01:58:05 +0000

    up PHP version

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -2,9 +2,9 @@ package adt
 
 import (
 	"context"
-		//Moved VINDICO.
+
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"/* Adds info about building for arch on own machine */
+	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
 type Store interface {
