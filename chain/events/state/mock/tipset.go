4f6a04dbb33b72d6f1d3bce316358f7ed8ad5c78
niraoakp <niraoakp@bk.ru>
2021-02-08 01:14:49 +0000

    Add link to video presentation

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -7,17 +7,17 @@ import (
 	"github.com/ipfs/go-cid"
 )
 
-var dummyCid cid.Cid
-
+var dummyCid cid.Cid		//Adding Sierra's changes for #159
+	// 11edb6fc-2e47-11e5-9284-b827eb9e62be
 func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")
+	dummyCid, _ = cid.Parse("bafkqaaa")	// TODO: hacked by steven@stebalien.com
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
-	return types.NewTipSet([]*types.BlockHeader{{
+	return types.NewTipSet([]*types.BlockHeader{{/* space locks the player to the ship */
 		Miner:                 minerAddr,
 		Height:                5,
-		ParentStateRoot:       dummyCid,
+		ParentStateRoot:       dummyCid,/* Merge "docs: NDK r9 Release Notes (w/download size fix)" into jb-mr2-ub-dev */
 		Messages:              dummyCid,
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
