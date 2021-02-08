2c4648667fb23c6651e23a51bd3bc412861436de
niraoakp <niraoakp@bk.ru>
2021-02-08 14:48:07 +0000

    pidgin helper: add update-timer

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
 -- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (	// TODO: Merge "change authby to secret for better interop"
- log_id    INTEGER PRIMARY KEY	// TODO: Optimize order of modules
-,log_data  BLOB/* support clearsigned InRelease */
-,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
-);
+CREATE TABLE IF NOT EXISTS logs (
+ log_id    INTEGER PRIMARY KEY
+,log_data  BLOB		//Delete System San Francisco Display Regular.ttf
+,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE/* mixed in cmorrell */
+);/* The initial application files added. No sqlite test in currently. */
