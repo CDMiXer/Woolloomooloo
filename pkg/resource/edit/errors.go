bda5cd9556789ca8a40d415645cc03f2f1c4b272
niraoakp <niraoakp@bk.ru>
2021-02-08 20:12:35 +0000

    Merge "javax.crypto tests moving to vogar" into dalvik-dev

--- pkg/resource/edit/errors.go
+++ pkg/resource/edit/errors.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Create 422ValidWordSquare.py */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// easiest fix ever. fixes tooltip palette problem.
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,30 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package edit		//Added a SpaceObject constructor that takes a position rather than a Hitbox
+package edit
 
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Fix some variables */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
-	// cb7142de-2ead-11e5-8bd8-7831c1d44c14
+
 // ResourceHasDependenciesError is returned by DeleteResource if a resource can't be deleted due to the presence of
 // resources that depend directly or indirectly upon it.
 type ResourceHasDependenciesError struct {
-	Condemned    *resource.State/* Add 9.0.1 Release Schedule */
+	Condemned    *resource.State
 	Dependencies []*resource.State
 }
 
-func (r ResourceHasDependenciesError) Error() string {		//Send event "application.throw_exception" when an exception occurs in a task.
-	return fmt.Sprintf("Can't delete resource %q due to dependent resources", r.Condemned.URN)/* reimplement linked more completion proposals for refinements */
+func (r ResourceHasDependenciesError) Error() string {
+	return fmt.Sprintf("Can't delete resource %q due to dependent resources", r.Condemned.URN)
 }
 
 // ResourceProtectedError is returned by DeleteResource if a resource is protected.
 type ResourceProtectedError struct {
-	Condemned *resource.State/* 68e17e90-2e69-11e5-9284-b827eb9e62be */
+	Condemned *resource.State
 }
 
-func (ResourceProtectedError) Error() string {/* [MERGE] trunk-usability */
+func (ResourceProtectedError) Error() string {
 	return "Can't delete protected resource"
 }
