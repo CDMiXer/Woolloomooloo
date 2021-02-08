e0ddc8b69d1b70fcaf765e3c6746f2f4e3cb69a1
niraoakp <niraoakp@bk.ru>
2021-02-08 17:16:58 +0000

    adding a dependency on OpenGSE, Google's high performance servlet engine.

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by greg@colvin.org
-// file is deliberately empty./* Correcting set_trace() statement */
+
+// file is deliberately empty.	// TODO: add playlistReady test, fixed flash flag
 // The engine should delete "a" since it's in the checkpoint.
