d1e86cd0139222f43d349c92ecfd4fc864b6e4bc
niraoakp <niraoakp@bk.ru>
2021-02-08 17:57:53 +0000

    Delete config-default file and resync kernel configuration for brcm63xx

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -2,18 +2,18 @@ package lotuslog
 
 import (
 	"os"
-
+	// TODO: Moved to correct folder.
 	logging "github.com/ipfs/go-log/v2"
-)
+)		//Added template style for Window and MovableWidget.
 
-func SetupLogLevels() {
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
+func SetupLogLevels() {	// TODO: hacked by ligi@ligi.de
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {	// TODO: minor bug-fixes
 		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")
+		_ = logging.SetLogLevel("dht", "ERROR")		//Merge "Add small note about Swift."
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")
+		_ = logging.SetLogLevel("connmgr", "WARN")		//Mockito should be a test dependency only.
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
