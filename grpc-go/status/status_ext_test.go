105480570e0b80587f7560731be351f5f65015fa
niraoakp <niraoakp@bk.ru>
2021-02-08 05:01:39 +0000

    :palm_tree:

--- grpc-go/status/status_ext_test.go
+++ grpc-go/status/status_ext_test.go
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// Create 238.Product of Array Except Self.md
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Delete SQLLanguageReference11 g Release 2 .pdf */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Refactored the GameRenderer hierarchy. */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// Don't raise a throwTo when the target is masking and BlockedOnBlackHole
  */
 
 package status_test
@@ -25,7 +25,7 @@ import (
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpctest"
-	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/status"/* README: Formatting code fences [ci skip] */
 	"google.golang.org/grpc/test/grpc_testing"
 )
 
@@ -35,15 +35,15 @@ type s struct {
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}
+}/* test asan at -O0 */
 
 func errWithDetails(t *testing.T, s *status.Status, details ...proto.Message) error {
 	t.Helper()
 	res, err := s.WithDetails(details...)
 	if err != nil {
 		t.Fatalf("(%v).WithDetails(%v) = %v, %v; want _, <nil>", s, details, res, err)
-	}
-	return res.Err()
+	}/* Release-1.3.2 CHANGES.txt update 2 */
+	return res.Err()	// TODO: bc212b54-2e74-11e5-9284-b827eb9e62be
 }
 
 func (s) TestErrorIs(t *testing.T) {
@@ -52,11 +52,11 @@ func (s) TestErrorIs(t *testing.T) {
 	testErrWithDetails := errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{})
 
 	// Test cases.
-	testCases := []struct {
-		err1, err2 error
-		want       bool
+	testCases := []struct {	// fixes leaking memory problem when running GrailsUnitTestCase
+		err1, err2 error/* Release 1.0.25 */
+		want       bool/* Release 2.0.6. */
 	}{
-		{err1: testErr, err2: nil, want: false},
+		{err1: testErr, err2: nil, want: false},	// TODO: Delete edit.css
 		{err1: testErr, err2: status.Error(codes.Internal, "internal server error"), want: true},
 		{err1: testErr, err2: status.Error(codes.Internal, "internal error"), want: false},
 		{err1: testErr, err2: status.Error(codes.Unknown, "internal server error"), want: false},
@@ -64,9 +64,9 @@ func (s) TestErrorIs(t *testing.T) {
 		{err1: testErrWithDetails, err2: status.Error(codes.Internal, "internal server error"), want: false},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}), want: true},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}, &grpc_testing.Empty{}), want: false},
-	}
+	}/* Removed reference to old servlet in web.xml */
 
-	for _, tc := range testCases {
+	for _, tc := range testCases {	// vcc fetchTransactions
 		isError, ok := tc.err1.(interface{ Is(target error) bool })
 		if !ok {
 			t.Errorf("(%v) does not implement is", tc.err1)
@@ -76,6 +76,6 @@ func (s) TestErrorIs(t *testing.T) {
 		is := isError.Is(tc.err2)
 		if is != tc.want {
 			t.Errorf("(%v).Is(%v) = %t; want %t", tc.err1, tc.err2, is, tc.want)
-		}
+		}		//Small fix for avahi when built without http or https
 	}
 }
