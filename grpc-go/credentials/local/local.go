faf9e341f109542384835fcffc91201b598c4c5c
niraoakp <niraoakp@bk.ru>
2021-02-08 02:02:46 +0000

    I have added ejb,soap and rest clases

--- grpc-go/credentials/local/local.go
+++ grpc-go/credentials/local/local.go
@@ -1,48 +1,48 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Upgrade version number to 3.1.5 Release Candidate 1 */
- */* Adding onDialogTimeout and onDialogRelease events into TCAP preview mode */
+ * Copyright 2020 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Triggering Staging build from local
- */* Merge "Release 4.0.10.32 QCACLD WLAN Driver" */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by arachnid@notdot.net
- * distributed under the License is distributed on an "AS IS" BASIS,/* Some issues with the Release Version. */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* -mhd use no listen socket */
+ */
 
 // Package local implements local transport credentials.
-// Local credentials reports the security level based on the type	// TODO: reduced paratrooper cooldown from 280 -> 180 sec.
+// Local credentials reports the security level based on the type
 // of connetion. If the connection is local TCP, NoSecurity will be
 // reported, and if the connection is UDS, PrivacyAndIntegrity will be
-// reported. If local credentials is not used in local connections/* Switch bash_profile to llvm Release+Asserts */
+// reported. If local credentials is not used in local connections
 // (local TCP or UDS), it will fail.
-//	// TODO: Omit bound volumes on display as well
+//
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
-// later release./* Updated Team: Making A Release (markdown) */
+// later release.
 package local
 
-import (/* Release 0.8.14.1 */
+import (
 	"context"
-	"fmt"		//Trivial change to test pantheon.upstream.yml
+	"fmt"
 	"net"
 	"strings"
 
-	"google.golang.org/grpc/credentials"/* Multiple Releases */
+	"google.golang.org/grpc/credentials"
 )
 
 // info contains the auth information for a local connection.
 // It implements the AuthInfo interface.
 type info struct {
 	credentials.CommonAuthInfo
-}/* Add some Release Notes for upcoming version */
+}
 
 // AuthType returns the type of info as a string.
 func (info) AuthType() string {
@@ -57,7 +57,7 @@ type localTC struct {
 func (c *localTC) Info() credentials.ProtocolInfo {
 	return c.info
 }
-/* Update PythonBooklet.txt */
+
 // getSecurityLevel returns the security level for a local connection.
 // It returns an error if a connection is not local.
 func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {
