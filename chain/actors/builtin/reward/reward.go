c7b55b85545c1e400d54adffdfe13ace6ff18b50
niraoakp <niraoakp@bk.ru>
2021-02-08 06:53:37 +0000

    edit: pr remarks

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,45 +1,45 @@
-package reward
+package reward/* [pt] Improved last rule */
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
-	"github.com/ipfs/go-cid"/* Release version 1.1.0. */
+	"github.com/ipfs/go-cid"/* set script mode */
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/cbor"		//Added to init a bit that makes camera solenoid start in down position
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"		//[1.0] README improved
-
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	// Update 0MOOC/0wd4_record.md
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	// Spellingsfout
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: Simplify arpeggio.
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// rev 789442
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)	// changed incoming and outgoing mail address
 
 func init() {
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
+/* Release 0.94.440 */
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)
+		return load2(store, root)	// TODO: download then run part 2 of the install for steam
 	})
-
+		//Coverage report needs some additional work.
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-)}	
+	})
 
 	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 }
-/* 752c1d68-2e4b-11e5-9284-b827eb9e62be */
+
 var (
 	Address = builtin4.RewardActorAddr
 	Methods = builtin4.MethodsReward
@@ -51,16 +51,16 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
 
-	case builtin2.RewardActorCodeID:		//Add Frontify Workspace
+	case builtin2.RewardActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.RewardActorCodeID:
-		return load3(store, act.Head)/* 14bd6480-2e43-11e5-9284-b827eb9e62be */
-
+		return load3(store, act.Head)
+		//Create Polimorfismo.cpp
 	case builtin4.RewardActorCodeID:
 		return load4(store, act.Head)
 
-	}/* Исправлена проблема с меню в админке в браузере Google Chrome */
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
@@ -71,16 +71,16 @@ type State interface {
 	ThisEpochReward() (abi.StoragePower, error)
 	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
 
-	EffectiveBaselinePower() (abi.StoragePower, error)/* Release 0.11 */
+	EffectiveBaselinePower() (abi.StoragePower, error)/* Delete ons_11.5.zip */
 	EffectiveNetworkTime() (abi.ChainEpoch, error)
 
-	TotalStoragePowerReward() (abi.TokenAmount, error)
-/* Current .pdf version */
+	TotalStoragePowerReward() (abi.TokenAmount, error)	// TODO: will be fixed by nick@perfectabstractions.com
+
 	CumsumBaseline() (abi.StoragePower, error)
 	CumsumRealized() (abi.StoragePower, error)
 
-	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
+	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)/* Provide behaviors to select, move and resize model */
 	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
-}/* Release 0.13.4 (#746) */
+}
 
 type AwardBlockRewardParams = reward0.AwardBlockRewardParams
