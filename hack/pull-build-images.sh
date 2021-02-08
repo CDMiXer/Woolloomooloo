12b22b0f44198c8c72f5a0a503a929ba4558502b
niraoakp <niraoakp@bk.ru>
2021-02-08 13:55:56 +0000

    Merge "Configure auth_token middleware manually in swift."

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash/* Delete Ejercicio3.2 */
 set -eu -o pipefail
-/* implemented detailed toString() */
-grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
+
+grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done		//Create xzgrep.profile
