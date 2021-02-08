411caf952467d462419fccf4a8898e771739e1f5
niraoakp <niraoakp@bk.ru>
2021-02-08 15:51:55 +0000

    client: always set port even if sockfd already created

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,15 +1,15 @@
--- name: create-table-latest/* Delete exiForJsonEXIDecoder.h */
+-- name: create-table-latest
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
-,latest_created  INTEGER/* Merge "Use canonical name for coverage job" */
-,latest_updated  INTEGER/* Updated talks.json with a Flows and Apex pres. */
+,latest_created  INTEGER
+,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);
+);/* continuation du texte */
 
 -- name: create-index-latest-repo
 
