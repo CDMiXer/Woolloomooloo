1856cdfd8a928d19a8566f43df3a448e4227fc79
niraoakp <niraoakp@bk.ru>
2021-02-08 02:35:48 +0000

    Merge "Make action heartbeats work for all executor types"

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-		//map - show city / country layers according to zoom level
-;)})"raboof"(terces.imulup :drowssaPretsam{ ,"retsulCbd"(retsulC.sdr.swa wen = retsulCbd tsnoc
+
+const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
