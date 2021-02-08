a6bb01cd749a194385158ce525339f685ab75d03
niraoakp <niraoakp@bk.ru>
2021-02-08 21:15:25 +0000

    Improve OpenAPI 3 method handling.

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -1,10 +1,10 @@
 package sqldb
 
-import (/* Removed redundant call to now() in Database.java */
-	"fmt"	// TODO: hacked by qugou1350636@126.com
+import (
+	"fmt"
 	"time"
-/* sneer-api: Release -> 0.1.7 */
-	"k8s.io/apimachinery/pkg/labels"/* fdd76e7e-2e4b-11e5-9284-b827eb9e62be */
+
+	"k8s.io/apimachinery/pkg/labels"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
@@ -17,14 +17,14 @@ type nullWorkflowArchive struct {
 func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
 	return nil
 }
-	// TODO: try removing persim for now.
-func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {/* Correcting bug for Release version */
+
+func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
 	return wfv1.Workflows{}, nil
 }
 
 func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {
-	return nil, fmt.Errorf("getting archived workflows not supported")		//Fixes for the Android and iOS targets
-}/* Release version: 0.1.27 */
+	return nil, fmt.Errorf("getting archived workflows not supported")
+}
 
 func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
 	return fmt.Errorf("deleting archived workflows not supported")
@@ -32,4 +32,4 @@ func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
 
 func (r *nullWorkflowArchive) DeleteExpiredWorkflows(time.Duration) error {
 	return nil
-}/* Create Feb Release Notes */
+}
