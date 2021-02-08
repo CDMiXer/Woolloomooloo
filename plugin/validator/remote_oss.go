7d06668e1b279a7d3a608a4411d7f687252ed343
niraoakp <niraoakp@bk.ru>
2021-02-08 01:02:28 +0000

    Delete Scratch Instructions

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Adding python support and preliminary Doxygen scripts
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Delete daihoXX.png */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// Help Command is polished
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by ng8eke@163.com
+/* Release tag: 0.7.2. */
 // +build oss
 
 package validator
@@ -20,10 +20,10 @@ import (
 	"time"
 
 	"github.com/drone/drone/core"
-)		//testing the continuation cache
+)
 
-// Remote returns a conversion service that converts the	// TODO: receiveBitcoin dataflow improved
+// Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
-	return new(noop)
-}	// TODO: Create registration_completed.twig
+	return new(noop)/* Release areca-5.5.6 */
+}
