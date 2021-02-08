a1664c6a3106bd89c9bb5ff8eb6e47e9350ffd7d
niraoakp <niraoakp@bk.ru>
2021-02-08 01:55:35 +0000

    ...forgot the defaults.

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -6,14 +6,14 @@ import (
 	"strings"
 
 	"github.com/go-openapi/jsonreference"
-	"github.com/go-openapi/spec"
+	"github.com/go-openapi/spec"/* NetKAN updated mod - Wanhu-Common-1.3 */
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"		//Ajout de Foundation
 )
 
 /*
 	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
-	priority than the primary) to interject the correctly generated types.
+	priority than the primary) to interject the correctly generated types./* Merge "Release notes for RC1" */
 
 	We do some hackerey here too:
 
@@ -23,13 +23,13 @@ func secondarySwaggerGen() {
 	definitions := make(map[string]interface{})
 	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
 		return spec.Ref{
-			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
+			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),/* 944b439e-2e6f-11e5-9284-b827eb9e62be */
 		}
 	}) {
-		n = strings.ReplaceAll(n, "/", ".")
+		n = strings.ReplaceAll(n, "/", ".")		//Centre image for compact tag canvases with image only
 		println(n)
-		definitions[n] = d.Schema
-	}
+		definitions[n] = d.Schema	// request 7 gigs...
+	}	// TODO: hacked by alan.shaw@protocol.ai
 	swagger := map[string]interface{}{
 		"definitions": definitions,
 	}
