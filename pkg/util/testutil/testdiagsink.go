04bcb51814044d468116574e8a8443ea1e3bcddb
niraoakp <niraoakp@bk.ru>
2021-02-08 10:19:49 +0000

    Adding in the data used for the experiments.

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,65 +1,65 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* added cpp tests, fixed implementation */
+// You may obtain a copy of the License at/* Create find_ip.py */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* f3665a32-2e72-11e5-9284-b827eb9e62be */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//LoopVectorize.cpp: Fix a warning. [-Wunused-variable]
-
+// limitations under the License./* Merge "Release 1.0.0.189 QCACLD WLAN Driver" */
+	// TODO: New version of Trident Lite - 1.0.5
 package testutil
-
+/* 78456474-2e6d-11e5-9284-b827eb9e62be */
 import (
-	"io/ioutil"	// TODO: hacked by davidad@alum.mit.edu
+	"io/ioutil"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 )
 
 // TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
 type TestDiagSink struct {
-	Pwd      string
+	Pwd      string	// [REF] pooler: added some comments.
 	sink     diag.Sink
-	messages map[diag.Severity][]string	// TODO: Added Darfur Qurbani
+	messages map[diag.Severity][]string
 }
 
 func NewTestDiagSink(pwd string) *TestDiagSink {
-	return &TestDiagSink{
+	return &TestDiagSink{/* Fix links to Releases */
 		Pwd: pwd,
-		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
+		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{/* Release 0.51 */
 			Pwd: pwd,
-		}),/* switch to boxy type checker by default. */
+		}),
 		messages: make(map[diag.Severity][]string),
 	}
 }
 
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
-func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
+func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }		//Merge branch 'develop' into bug/always-fetch-edit-token/T153106
 func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
-/* Split calendar into own file */
-func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {/* Merge "Release 5.4.0" */
+
+func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
 	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))
-}
-		//revert to 0.9.9
+}/*  - Released 1.91 alpha 1 */
+/* finishing up ReleasePlugin tasks, and working on rest of the bzr tasks. */
 func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))
 }
 
 func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
-))...sgra ,aid ,ofnI.gaid(enibmoc.d ,]ofnI.gaid[segassem.d(dneppa = ]ofnI.gaid[segassem.d	
-}/* Release v1 */
-
-func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))	// TODO: hacked by magik6k@gmail.com
+	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))
 }
 
-func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {		//adds new render condition to change local
-	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))
+func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
+	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))
+}
+
+func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {
+	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))	// TODO: [MERGE] mail: correction of backlog1
 }
 
 func (d *TestDiagSink) Stringify(sev diag.Severity, dia *diag.Diag, args ...interface{}) (string, string) {
@@ -68,5 +68,5 @@ func (d *TestDiagSink) Stringify(sev diag.Severity, dia *diag.Diag, args ...inte
 
 func (d *TestDiagSink) combine(sev diag.Severity, dia *diag.Diag, args ...interface{}) string {
 	p, s := d.sink.Stringify(sev, dia, args...)
-	return p + s		//Rename First Impressions to First-Impressions.md
+	return p + s
 }
