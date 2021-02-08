42cd7dce5957b6b5d209a7ac4b9bf20e3fef2a9e
niraoakp <niraoakp@bk.ru>
2021-02-08 00:17:16 +0000

    added note on file editing

--- tests/examples/dynamic-provider/multiple-turns-2/index.ts
+++ tests/examples/dynamic-provider/multiple-turns-2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Merge "ASoC: msm: Add Enablement for stubbed CPU DAI" into msm-3.4
-import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
 
-const sleep = require("sleep-promise");
-const assert = require("assert");	// 534e24a6-2e65-11e5-9284-b827eb9e62be
+import * as pulumi from "@pulumi/pulumi";/* updated the chip types (nw) */
+import * as dynamic from "@pulumi/pulumi/dynamic";	// TODO: hacked by davidad@alum.mit.edu
+
+const sleep = require("sleep-promise");		//Credentials for HTTP Basic authorization
+const assert = require("assert");/* Remove unused properties */
 
 class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
-    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});/* Merge branch 'master' into greenkeeper/file-loader-1.1.5 */
-    create = (inputs: any) => Promise.resolve({ id: "0" });/* Prepare Release */
-    update = (id: string, olds: any, news: any) => Promise.resolve({});/* Rename ReleaseNotes to ReleaseNotes.md */
+    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});/* Released springjdbcdao version 1.8.13 */
+    create = (inputs: any) => Promise.resolve({ id: "0" });
+    update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
-
+/* Release v1.21 */
 class NullResource extends dynamic.Resource {
     constructor(name: string, input: any) {
         super(new NullProvider(), name, {input: input}, undefined);
-    }		//Create Lista02_1-2.cpp
-}/* Updated copyright notices. Released 2.1.0 */
+    }
+}
 
 async function getInput(): Promise<pulumi.Output<string>> {
     await sleep(1000);
