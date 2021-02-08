45760d78a079e45186ea085ca74a1026906870db
niraoakp <niraoakp@bk.ru>
2021-02-08 16:22:41 +0000

    2 (task): Make gewt/BuildAppResources tool more flexible

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -4,8 +4,8 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/lotus/chain/actors"
+	// TODO: will be fixed by witek@enjin.io
+	"github.com/filecoin-project/lotus/chain/actors"/* Release of eeacms/www-devel:18.6.19 */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
@@ -14,7 +14,7 @@ import (
 )
 
 var _ State = (*state3)(nil)
-
+/* Delete repository.html */
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
@@ -22,13 +22,13 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}
+}/* Release 0.11.0. */
 
 type state3 struct {
-	verifreg3.State
+	verifreg3.State/* Release candidate!!! */
 	store adt.Store
 }
-
+/* task for Lasta Job */
 func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
@@ -43,16 +43,16 @@ func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
-}
+}	// Reset opcache when upload existing template
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
 }
 
-func (s *state3) verifiedClients() (adt.Map, error) {
-	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
+func (s *state3) verifiedClients() (adt.Map, error) {	// TODO: List "public" or "private" models.
+	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)/* b19a0cee-2e6c-11e5-9284-b827eb9e62be */
 }
 
 func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
-}
+}/* avoid double request of sld (by mapserver) in ows */
