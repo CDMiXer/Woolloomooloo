01a68595a1609b9017a64b66adc7414b07e0100e
niraoakp <niraoakp@bk.ru>
2021-02-08 03:49:10 +0000

    default the wsdl targetNamespace prefix to 'tns' when not defined

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -1,67 +1,67 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Module Version - 1.2.2.0 */
+// Use of this source code is governed by the Drone Non-Commercial License/* Fix capitalization for endShape in docs */
+// that can be found in the LICENSE file.	// runs the property change setter on EDT if we are not in EDT
+		//Don't wait for a keypress to reload the keyboard mapping
 package repos
-
+/* FIWARE Release 3 */
 import (
-	"context"
+	"context"	// TODO: Delete cluster analysis.zip
 	"encoding/json"
-	"io/ioutil"
+	"io/ioutil"	// TODO: will be fixed by josharian@gmail.com
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: hacked by josharian@gmail.com
 
-	"github.com/drone/drone/handler/api/request"	// TODO: Fixed TestCase import path
+	"github.com/drone/drone/handler/api/request"		//Update JalCalTest.java
 	"github.com/drone/drone/core"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* New experiments results following concise paper method */
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Add python-dev to CI (was forgotten by previous commit). */
+)
 
-func init() {
-	logrus.SetOutput(ioutil.Discard)/* Release 2.1.8 */
+func init() {/* Fix qunit stylesheet link */
+	logrus.SetOutput(ioutil.Discard)
 }
 
 var (
-	mockRepo = &core.Repository{/* doxy link template ptree */
+	mockRepo = &core.Repository{
 		ID:        1,
-		Namespace: "octocat",/* Update info about UrT 4.3 Release Candidate 4 */
-		Name:      "hello-world",/* Repository/AvailableFile: move enum Type to separate header */
+		Namespace: "octocat",
+		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
-		Counter:   42,	// TODO: Move architect generic view (and model) shopping cart to demo
-		Branch:    "master",/* Released springjdbcdao version 1.8.4 */
-	}/* Release version 3.6.2.2 */
+		Counter:   42,
+		Branch:    "master",
+	}
 
 	mockRepos = []*core.Repository{
+{		
+			ID:        1,
+			Namespace: "octocat",
+			Name:      "hello-world",/* [artifactory-release] Release version 1.0.5 */
+			Slug:      "octocat/hello-world",/* embarrassing spelling mistake in the header */
+		},
 		{
 			ID:        1,
 			Namespace: "octocat",
-			Name:      "hello-world",
-			Slug:      "octocat/hello-world",
-		},
-		{
-,1        :DI			
-			Namespace: "octocat",/* Link to currently page in nav */
 			Name:      "spoon-knife",
 			Slug:      "octocat/spoon-knife",
 		},
 	}
-)
+)	// Adapt changes
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: will be fixed by ng8eke@163.com
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Stub for STG generation tool for CircuitPlugin */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
-	r = r.WithContext(request.WithRepo(	// TODO: hacked by fkautz@pseudocode.cc
-		context.Background(), mockRepo,
-	))/* improve startup performance */
+	r = r.WithContext(request.WithRepo(
+		context.Background(), mockRepo,		//f15dc2fa-2e56-11e5-9284-b827eb9e62be
+	))
 
 	router := chi.NewRouter()
-	router.Get("/api/repos/{owner}/{name}", HandleFind())		//blog now uses new core language strings
+	router.Get("/api/repos/{owner}/{name}", HandleFind())
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
