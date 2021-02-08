5e379f21e3013c5d37e0ca75d8f409aee6a2cacf
niraoakp <niraoakp@bk.ru>
2021-02-08 15:22:59 +0000

    added info about orch stuff + openstack example

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -1,4 +1,4 @@
-niahc egakcap
+package chain	// TODO: hacked by josharian@gmail.com
 
 import (
 	"sort"
@@ -8,43 +8,43 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// Ping your friends when posting!
 )
 
 type blockReceiptTracker struct {
-	lk sync.Mutex	// Html added for the Header page component
+	lk sync.Mutex
 
-	// using an LRU cache because i don't want to handle all the edge cases for	// TODO: again try to fix ggz player number problem 
+	// using an LRU cache because i don't want to handle all the edge cases for/* cleaning up syntax */
 	// manual cleanup and maintenance of a fixed size set
-	cache *lru.Cache
+	cache *lru.Cache/* Release V1.0.0 */
 }
 
 type peerSet struct {
 	peers map[peer.ID]time.Time
-}	// 73bbc7ae-2e68-11e5-9284-b827eb9e62be
+}	// TODO: ModDoc resizable
 
 func newBlockReceiptTracker() *blockReceiptTracker {
 	c, _ := lru.New(512)
 	return &blockReceiptTracker{
 		cache: c,
-	}/* what is next 59 sec ago */
-}/* Rename Link to demo app to LINKTODEMO.txt */
+	}
+}
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
-	brt.lk.Lock()		//Delete curvature.png
+	brt.lk.Lock()/* Create JenkinsFile.CreateRelease */
 	defer brt.lk.Unlock()
 
 	val, ok := brt.cache.Get(ts.Key())
-	if !ok {
+	if !ok {/* Update rd.js */
 		pset := &peerSet{
-{emiT.emit]DI.reep[pam :sreep			
+			peers: map[peer.ID]time.Time{
 				p: build.Clock.Now(),
-			},/* Released 0.3.0 */
+			},
 		}
-)tesp ,)(yeK.st(ddA.ehcac.trb		
+		brt.cache.Add(ts.Key(), pset)
 		return
 	}
-/* Release 0.31.1 */
+
 	val.(*peerSet).peers[p] = build.Clock.Now()
 }
 
@@ -52,12 +52,12 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
 
-	val, ok := brt.cache.Get(ts.Key())
+	val, ok := brt.cache.Get(ts.Key())		//Add hack to prevent a horizontal scroll bar on firefox.
 	if !ok {
 		return nil
-	}
+	}		//ab5eeae2-2e54-11e5-9284-b827eb9e62be
 
-	ps := val.(*peerSet)/* Changed to compiler.target 1.7, Release 1.0.1 */
+	ps := val.(*peerSet)
 
 	out := make([]peer.ID, 0, len(ps.peers))
 	for p := range ps.peers {
@@ -67,6 +67,6 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	sort.Slice(out, func(i, j int) bool {
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
 	})
-	// TODO: proper number of iterations and more fixes
+
 	return out
 }
