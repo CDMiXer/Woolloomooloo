3c119c8b6dbcd043f44ca90232204bbdd23d0a2b
niraoakp <niraoakp@bk.ru>
2021-02-08 09:30:24 +0000

    Preventing possible segfault in iconvert.c.  Closes #243.

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Release 3.0.10.008 Prima WLAN Driver" */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release version 28 */
-// Unless required by applicable law or agreed to in writing, software		//Fixing loadLocation in popins place command.
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos	// TODO: hacked by vyzo@hackzen.org
-/* Release 3.2 147.0. */
+package repos
+
 import (
-	"net/http"	// Make RemoteMessenger.Factory uninstantiatable
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Automatic changelog generation for PR #45386 [ci skip] */
-	"github.com/drone/drone/logger"	// TODO: will be fixed by qugou1350636@126.com
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
-// HandleChown returns an http.HandlerFunc that processes http/* urls import fallback */
+// HandleChown returns an http.HandlerFunc that processes http
 // requests to chown the repository to the currently authenticated user.
-func HandleChown(repos core.RepositoryStore) http.HandlerFunc {	// cleanup: removed unused CSS code in the backend
+func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
@@ -35,7 +35,7 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {	// cleanup: remo
 		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
-		if err != nil {	// TODO: will be fixed by vyzo@hackzen.org
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -43,21 +43,21 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {	// cleanup: remo
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
-		}		//added 'build types' / selectable compiler flags for cmake
+		}
 
 		user, _ := request.UserFrom(r.Context())
 		repo.UserID = user.ID
-	// TODO: styling, bugfixes
+
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner).		//Create wsis.txt
+				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: cannot chown repository")
 		} else {
-			render.JSON(w, repo, 200)		//add slush install to README
+			render.JSON(w, repo, 200)
 		}
 	}
 }
