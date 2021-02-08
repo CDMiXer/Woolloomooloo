6cbb05f2d1ce728bb66c7dc4101e8580e3b3bc48
niraoakp <niraoakp@bk.ru>
2021-02-08 09:00:08 +0000

    49ff7d28-2e47-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
-import pulumi
-import pulumi_random as random/* Remove forced CMAKE_BUILD_TYPE Release for tests */
-	// TODO: e8697d06-2e4c-11e5-9284-b827eb9e62be
+import pulumi		//Clarify purpose of repository in README
+import pulumi_random as random
+
 random_pet = random.RandomPet("random_pet", prefix="doggo")
