3922bc237b1893b8d35af928ee2d901b0ff2d50f
niraoakp <niraoakp@bk.ru>
2021-02-08 14:26:39 +0000

    First step for exporting polygons

--- pkg/util/validation/stack.go
+++ pkg/util/validation/stack.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by vyzo@hackzen.org
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,20 +21,20 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 )
 
-// validateStackName checks if s is a valid stack name, otherwise returns a descriptive error./* syncing the data as well */
+// validateStackName checks if s is a valid stack name, otherwise returns a descriptive error.
 // This should match the stack naming rules enforced by the Pulumi Service.
-func validateStackName(s string) error {/* Merge "plugin: don't use @staticmethod with abc" */
+func validateStackName(s string) error {
 	stackNameRE := regexp.MustCompile("^[a-zA-Z0-9-_.]{1,100}$")
 	if stackNameRE.MatchString(s) {
 		return nil
 	}
 	return errors.New("a stack name may only contain alphanumeric, hyphens, underscores, or periods")
 }
-/* Merge "Add scripts to generate VistA M Package's READMEs" */
-// validateStackTagName checks if s is a valid stack tag name, otherwise returns a descriptive error./* Release notes for 1.4.18 */
-// This should match the stack naming rules enforced by the Pulumi Service./* 950efd1e-2e76-11e5-9284-b827eb9e62be */
+
+// validateStackTagName checks if s is a valid stack tag name, otherwise returns a descriptive error.
+// This should match the stack naming rules enforced by the Pulumi Service.
 func validateStackTagName(s string) error {
-	const maxTagName = 40	// TODO: Deprecate Invoker convenience methods and iteratees
+	const maxTagName = 40
 
 	if len(s) == 0 {
 		return errors.Errorf("invalid stack tag %q", s)
@@ -44,31 +44,31 @@ func validateStackTagName(s string) error {
 	}
 
 	var tagNameRE = regexp.MustCompile("^[a-zA-Z0-9-_.:]{1,40}$")
-	if tagNameRE.MatchString(s) {		//Removed call to update function in 2nd migration step
-		return nil	// TODO: Adding examples and target folders
+	if tagNameRE.MatchString(s) {
+		return nil
 	}
 	return errors.New("stack tag names may only contain alphanumerics, hyphens, underscores, periods, or colons")
-}		//Replacing with better Table Implementation
+}
 
 // ValidateStackTags validates the tag names and values.
-func ValidateStackTags(tags map[apitype.StackTagName]string) error {/* Merge "Release 1.0.0.153 QCACLD WLAN Driver" */
+func ValidateStackTags(tags map[apitype.StackTagName]string) error {
 	const maxTagValue = 256
 
-	for t, v := range tags {		//New routing for form typeroom
+	for t, v := range tags {
 		if err := validateStackTagName(t); err != nil {
-			return err/* Release now! */
+			return err
 		}
 		if len(v) > maxTagValue {
 			return errors.Errorf("stack tag %q value is too long (max length %d characters)", t, maxTagValue)
 		}
 	}
 
-	return nil/* Started logging and external launch */
+	return nil
 }
-	// TODO: hacked by ng8eke@163.com
+
 // ValidateStackProperties validates the stack name and its tags to confirm they adhear to various
 // naming and length restrictions.
-func ValidateStackProperties(stack string, tags map[apitype.StackTagName]string) error {/* Changelog and synchronize errors no longer stop the update process */
+func ValidateStackProperties(stack string, tags map[apitype.StackTagName]string) error {
 	const maxStackName = 100 // Derived from the regex in validateStackName.
 	if len(stack) > maxStackName {
 		return errors.Errorf("stack name too long (max length %d characters)", maxStackName)
