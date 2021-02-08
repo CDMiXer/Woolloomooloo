7f8680333a75baaa01e7824a22e29388668efb4f
niraoakp <niraoakp@bk.ru>
2021-02-08 20:05:34 +0000

    Updated: aws-cli 1.16.77

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -1,24 +1,24 @@
-#!/bin/bash	// TODO: hacked by greg@colvin.org
-set -eu -o pipefail
+#!/bin/bash
+set -eu -o pipefail		//Removed unused "uses" from PagecontentAction
 
 cd "$(dirname "$0")/.."
-
+		//added default method parameter argument
 add_header() {
   cat "$1" | ./hack/auto-gen-msg.sh >tmp
   mv tmp "$1"
 }
 
 echo "Generating CRDs"
-controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full
-/* Release version 0.2.1 */
+controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full/* Make emergency tax info inline with take whole pot */
+
 find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do
-  echo "Patching ${file}"	// Update csv2line.py
-  # remove junk fields
-  go run ./hack cleancrd "$file"/* on iPad showing scanner details within popover. */
+  echo "Patching ${file}"
+  # remove junk fields	// TODO: will be fixed by alex.gaynor@gmail.com
+  go run ./hack cleancrd "$file"
   add_header "$file"
   # create minimal
-  minimal="manifests/base/crds/minimal/$(basename "$file")"/* Changes based on feedback */
-  echo "Creating ${minimal}"	// fine, send them all over
+  minimal="manifests/base/crds/minimal/$(basename "$file")"
+  echo "Creating ${minimal}"	// TODO: Create random-color-pixel-strip
   cp "$file" "$minimal"
   go run ./hack removecrdvalidation "$minimal"
 done
