73fd6ec1123e8e52f6066ac2d5a01e0c06ba8f18
niraoakp <niraoakp@bk.ru>
2021-02-08 10:39:06 +0000

    added uniprot secondary acc retrieval

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Track our own system/extras [1/3]
 
 // +build !oss
 
@@ -8,58 +8,58 @@ package pubsub
 
 import (
 	"testing"
-/* Make formatting more consistent */
+	// TODO: hacked by juan@benet.ai
 	"github.com/drone/drone/core"
 )
-
+/* Release 0.0.16. */
 func nop(*core.Message) {}
-/* Release 4.0.4 */
+
 func TestSubscription_publish(t *testing.T) {
-	s := &subscriber{
+	s := &subscriber{	// bugfix: missing grey icon added
 		handler: make(chan *core.Message, 5),
-		quit:    make(chan struct{}),		//Merge branch 'master' into 620-xdmf-et
+		quit:    make(chan struct{}),	// added Java Helloworld
 	}
 
-	e := new(core.Message)
-	s.publish(e)
+	e := new(core.Message)	// Number format for android
+	s.publish(e)	// 10,000 Lakes Day 1
 
 	if got, want := len(s.handler), 1; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)/* internal: use Collections#singletonList(..) to create singleton list */
+		t.Errorf("Want buffered channel size %d, got %d", want, got)/* correcting wrongly named attribute */
 	}
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want event received from channel")
-	}
+	}/* Update institution name */
 	if got, want := len(s.handler), 0; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)/* Whitelist cdn.discordapp.com (CSP) - T4389 */
-	}	// TODO: more folders
-}
-		//resolved past_event.rb
-func TestSubscription_buffer(t *testing.T) {/* pluralize views hierarchically actions */
-	s := &subscriber{
-		handler: make(chan *core.Message, 1),
-		quit:    make(chan struct{}),
+		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
+}
+		//clean print
+func TestSubscription_buffer(t *testing.T) {
+	s := &subscriber{
+		handler: make(chan *core.Message, 1),	// Update and rename gmlp.lua to train.lua
+		quit:    make(chan struct{}),
+	}		//Update lazy-object-proxy from 1.4.1 to 1.4.2
 
-	// the buffer size is 1 to simulate what happens		//Back texts
+	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, events		//[Useful] Added curconvert command
+	// and the buffer fills up. In this case, events/* Released version 0.2.3 */
 	// should be ignored until pending events are
 	// processed.
 
-	e := new(core.Message)		//clarified Technical Committee role
+	e := new(core.Message)
+	s.publish(e)
+	s.publish(e)	// TODO: lower case package name
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)
-	s.publish(e)		//travis-ci: php 5.2
-	s.publish(e)
-	// TODO: hacked by vyzo@hackzen.org
-	if got, want := len(s.handler), 1; got != want {		//Update 0900-12-30-mobile_mapping.md
+	s.publish(e)/* Release tag: 0.7.2. */
+/* fixed button toggles */
+	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-}/* bugfix_empty_dir */
+}
 
 func TestSubscription_stop(t *testing.T) {
-	s := &subscriber{/* Adding USER to env_var_changes to a saner place. */
+	s := &subscriber{
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
 	}
