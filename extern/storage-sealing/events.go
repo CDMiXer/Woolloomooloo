1e07c5d12657e950fa7c9a79a59563f0f3a8baed
niraoakp <niraoakp@bk.ru>
2021-02-08 23:31:27 +0000

    update dev-master alias

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
-package sealing
+package sealing	// aba45d5c-2e68-11e5-9284-b827eb9e62be
 
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
-)
+	"github.com/filecoin-project/go-state-types/abi"/* Removes bug that compared string with byte */
+)	// TODO: add "external id" for inquiry fields - uml
 
-// `curH`-`ts.Height` = `confidence`
+// `curH`-`ts.Height` = `confidence`		//attempted to create the front page
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
-
+/* updates ember-routemanager and ember-layout to latest version */
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
