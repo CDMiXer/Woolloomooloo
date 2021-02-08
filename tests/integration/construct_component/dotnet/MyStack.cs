a6caad5eb06271b373b6869bcb2918686d7664db
niraoakp <niraoakp@bk.ru>
2021-02-08 14:17:00 +0000

    nrf24-config: Remove side effects of setRfPower, setARD, setARC.

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 using Pulumi;
-		//handle ENOBUFS on bsd systems
+
 class MyStack : Stack
-{/* Updated Release */
+{/* Delete web.Release.config */
     public MyStack()
-    {
+    {/* [artifactory-release] Release version 1.3.1.RELEASE */
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
-        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
-        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
-    }
+        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });		//Insert #pragma once to all possible headers.
+        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });/* Release: version 1.4.2. */
+    }/* filtering by siteKey */
 }
