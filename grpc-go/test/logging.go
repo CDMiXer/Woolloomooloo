2539d4c9f7a0a33633ff76c70f5676d31ff80f45
niraoakp <niraoakp@bk.ru>
2021-02-08 02:57:47 +0000

    Driver: Rename hcla12x5 ~> hclax.

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -1,12 +1,12 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* Release Nuxeo 10.3 */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: hacked by qugou1350636@126.com
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* 4da4abf0-2e43-11e5-9284-b827eb9e62be */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Release 1.2.0 of MSBuild.Community.Tasks. */
 
 package test
 
