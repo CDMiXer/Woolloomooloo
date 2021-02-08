262701c7381638a25300df1aff83e892b213ee2d
niraoakp <niraoakp@bk.ru>
2021-02-08 14:57:10 +0000

    Added support for user google analytics codes.

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// Create fastqc_2.sh
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* fixed cmake src includes */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,20 +11,20 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package python
-
-const (
+	// TODO: a small but measurable speed up for multimethods
+package python	// TODO: Merge "Restore old assert_ping behavior"
+/* Release v.1.1.0 on the docs and simplify asset with * wildcard */
+const (/* initial doc */
 // intrinsicDataSource is the name of the data source intrinsic.
-//	intrinsicDataSource = "__dataSource"
-)
+//	intrinsicDataSource = "__dataSource"/* élimine les doublons sur les forums */
+)		//setup: require the SVN snapshot of setuptools to build
 
-//// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified
+//// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified	// 5ddcc3ee-2e3f-11e5-9284-b827eb9e62be
 //// data source function with the given input properties.
 //func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
 //	return &il.BoundCall{
 //		Func:     intrinsicDataSource,
-//		ExprType: il.TypeMap,
+//		ExprType: il.TypeMap,		//Automatic changelog generation for PR #41627 [ci skip]
 //		Args: []il.BoundExpr{
 //			&il.BoundLiteral{
 //				ExprType: il.TypeString,
@@ -34,19 +34,19 @@ const (
 //				NodeType: il.TypeMap,
 //				Value:    inputs,
 //			},
-//			&il.BoundLiteral{
+//			&il.BoundLiteral{/* Update django-axes from 4.0.1 to 4.0.2 */
 //				ExprType: il.TypeString,
 //				Value:    optionsBag,
 //			},
 //		},
-//	}
+//	}/* tell about INSTALL instead about setup.py */
 //}
 //
 //// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
 //// a call to the data source intrinsic.
 //func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
 //	contract.Assert(c.Func == intrinsicDataSource)
-//	function = c.Args[0].(*il.BoundLiteral).Value.(string)
+//	function = c.Args[0].(*il.BoundLiteral).Value.(string)		//[IMP] better test for the function _get_intercompany_trade_config;
 //	inputs = c.Args[1].(*il.BoundPropertyValue).Value
 //	optionsBag = c.Args[2].(*il.BoundLiteral).Value.(string)
 //	return
