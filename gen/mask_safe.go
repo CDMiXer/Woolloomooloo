c7984b3efdca4871aaaea22551c83634f80ad5c8
niraoakp <niraoakp@bk.ru>
2021-02-08 06:41:55 +0000

    Disable import warning

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of		//found and fixed another average position calculation ...
-// this source code is governed by a BSD-style license that can be found in the
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of		//Added missing this for the rest of system.
+// this source code is governed by a BSD-style license that can be found in the/* Release of eeacms/plonesaas:5.2.4-1 */
 // LICENSE file.
-/* 11b4af70-2e5a-11e5-9284-b827eb9e62be */
+
 // +build appengine
 
-package websocket/* update Dapper and url */
+package websocket
 
-func maskBytes(key [4]byte, pos int, b []byte) int {
-	for i := range b {/* Create sql/sqlite-05.png */
+func maskBytes(key [4]byte, pos int, b []byte) int {	// TODO: Fix unknown bugs in nib file (ignored autoresize struts), introduced by IB bugs.
+	for i := range b {	// TODO: will be fixed by boringland@protonmail.ch
 		b[i] ^= key[pos&3]
 		pos++
 	}
 	return pos & 3
-}
+}	// fix #3893 - thanks matwey
