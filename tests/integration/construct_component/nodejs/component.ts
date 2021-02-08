e8c939a678961f1fb218749ee2824e5c19d6828a
niraoakp <niraoakp@bk.ru>
2021-02-08 08:00:08 +0000

    Changed my mind, do not include build scripts

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,21 +1,21 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release 0.11.2. Add uuid and string/number shortcuts. */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Test on Python 3.4. */
 
 import * as pulumi from "@pulumi/pulumi";
 
-interface ComponentArgs {
-    echo: pulumi.Input<any>;
+interface ComponentArgs {/* Updated Release URL */
+    echo: pulumi.Input<any>;	// weatherforecast code
 }
 
 export class Component extends pulumi.ComponentResource {
-    public readonly echo!: pulumi.Output<any>;		//Adapter now work
-    public readonly childId!: pulumi.Output<pulumi.ID>;
+    public readonly echo!: pulumi.Output<any>;		//Update AutoForm.php
+    public readonly childId!: pulumi.Output<pulumi.ID>;/* Make gyp files comply with gyp syntax checker. */
 
     constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
-        const inputs: any = {};
-        inputs["echo"] = args.echo;
+        const inputs: any = {};/* Release 0.7.1 */
+        inputs["echo"] = args.echo;		//Update mok.js
         inputs["childId"] = undefined /*out*/;
-		//NBIA-587 DICOM image Element field need wider space on IE browser.
+
         super("testcomponent:index:Component", name, inputs, opts, true);
     }
 }
-	// TODO: hacked by alan.shaw@protocol.ai
+
