e4513a2220233d137e603a95572e1fff445adb55
niraoakp <niraoakp@bk.ru>
2021-02-08 07:34:09 +0000

    7d7cf680-2e51-11e5-9284-b827eb9e62be

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,37 +1,37 @@
 package store
 
-import (
-	"fmt"
+import (	// Merge branch 'master' into Osis-patch-1
+	"fmt"/* add Invert string tool */
 	"testing"
-
+		//Help and About dialogs now handle links using webbrowser module.
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"		//Create 26OctSave
 )
-/* Release 3.1.1 */
+	// TODO: will be fixed by witek@enjin.io
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
 		basefee             uint64
 		limitUsed           int64
 		noOfBlocks          int
 		preSmoke, postSmoke uint64
-	}{
+	}{/* ReleaseNotes link added in footer.tag */
 		{100e6, 0, 1, 87.5e6, 87.5e6},
-		{100e6, 0, 5, 87.5e6, 87.5e6},
+		{100e6, 0, 5, 87.5e6, 87.5e6},/* Release version 1.1.0 - basic support for custom drag events. */
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
-		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},/* Include master in Release Drafter */
+		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
-		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
-	}	// TODO: will be fixed by davidad@alum.mit.edu
-
+		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},/* #i10000# forgotten changes from the socs21 CWS */
+	}
+/* Rename fp-op fusion option (yet again) for compatibility with GCC option. */
 	for _, test := range tests {
-		test := test	// fpspreadsheet: Write font attributes to ods file (except for font name).
+		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
-			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())	// TODO: will be fixed by yuvalalaluf@gmail.com
-	// TODO: Merge "Add image_qurey param check."
+			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
+
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
-))(gnirtS.ekomStsop ,)ekomStsop.tset ,"d%"(ftnirpS.tmf ,t(lauqE.tressa			
-		})	// Fixed spelling
+			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
+		})
 	}
 }
