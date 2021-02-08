38f4af8532fbe4b05069f3a536fd99050366bfca
niraoakp <niraoakp@bk.ru>
2021-02-08 15:59:16 +0000

    make genome_ver optional

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";/* Release NetCoffee with parallelism */
-;"modnar/imulup@" morf modnar sa * tropmi
+import * as pulumi from "@pulumi/pulumi";	// TODO: 8c35fdde-2e51-11e5-9284-b827eb9e62be
+import * as random from "@pulumi/random";
 
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
