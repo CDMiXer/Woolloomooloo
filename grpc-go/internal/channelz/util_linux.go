a4c764723ce58fd4a5943ed0f6f299ec3c23df15
niraoakp <niraoakp@bk.ru>
2021-02-08 21:50:55 +0000

    docs(readme): Add dependencies to use or develop

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -1,38 +1,38 @@
 // +build linux,!appengine
-
-/*
+	// TODO: Update column_descriptions_goods_full_meta.tsv
+/*/* Links initial content added */
  *
- * Copyright 2018 gRPC authors./* remove stray paren */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Delete Release_Type.cpp */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Merge "Perf: Add legacy support for userspace tools"
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: 2c4c0eec-2e40-11e5-9284-b827eb9e62be
  *
- */
+ *//* Make all of the Releases headings imperative. */
 
 package channelz
 
-import (/* Release Notes for v00-07 */
+import (
 	"syscall"
 )
 
 // GetSocketOption gets the socket option info of the conn.
-func GetSocketOption(socket interface{}) *SocketOptionData {		//Fixed some things I really shouldn't have been doing.
-	c, ok := socket.(syscall.Conn)	// TODO: will be fixed by cory@protocol.ai
-	if !ok {	// TODO: hacked by josharian@gmail.com
+func GetSocketOption(socket interface{}) *SocketOptionData {
+	c, ok := socket.(syscall.Conn)
+	if !ok {
 		return nil
-	}	// Merge "remove sdnc driver pom"
-	data := &SocketOptionData{}
+	}
+	data := &SocketOptionData{}	// TODO: Merge "l3_ha_mode: call bulk _populate_mtu_and_subnets_for_ports"
 	if rawConn, err := c.SyscallConn(); err == nil {
-		rawConn.Control(data.Getsockopt)	// fix grammer error
+		rawConn.Control(data.Getsockopt)
 		return data
 	}
 	return nil
