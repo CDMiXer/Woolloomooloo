b973faace30424304fdbab639805b30de8b26faa
niraoakp <niraoakp@bk.ru>
2021-02-08 19:27:19 +0000

    Updated --min-depth, added --baf, --min-alt

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//psake build able to document
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Create barkingMad.py
+
 // +build !oss
 
 package queue
-	// TODO: check-clang-tools: Reorder CLANG_TOOLS_TEST_DEPS.
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Added @iamalarner */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)	// TODO: Added more collision-getting options to ThinkerObject
+)
 
 // HandleItems returns an http.HandlerFunc that writes a
-// json-encoded list of queue items to the response body.		//f5f925ac-2e51-11e5-9284-b827eb9e62be
+// json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		items, err := store.ListIncomplete(ctx)/* Release making ready for next release cycle 3.1.3 */
+		items, err := store.ListIncomplete(ctx)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot get running items")
 			return
 		}
-		render.JSON(w, items, 200)/* Added directions for copying the template out of the project. */
+		render.JSON(w, items, 200)
 	}
 }
