ddc2f3eb6c857066c9565d503aefddcb9fea121c
niraoakp <niraoakp@bk.ru>
2021-02-08 17:48:05 +0000

    Adopted, due removal of TargetGroup2 class.

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// First version of energy implementation in the mod
-//		//double check mail files for deletion
+// You may obtain a copy of the License at
+//	// TODO: Merge "dev: gcdb: update truly wvga command mode panel header"
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* 7982ff66-2e65-11e5-9284-b827eb9e62be */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fix spectating players being able to use Rattle and Jingle Dress
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by cory@protocol.ai
+// limitations under the License./* Changed the Changelog message. Hope it works. #Release */
 
 package deploys
