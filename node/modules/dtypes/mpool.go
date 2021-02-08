bf33a3c16982aa09e0b19e0b563b02b8f66c19ea
niraoakp <niraoakp@bk.ru>
2021-02-08 03:34:27 +0000

    Release and severity updated

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,38 +1,38 @@
 package dtypes
 
-import (
+import (	// TODO: e9f786fe-2e42-11e5-9284-b827eb9e62be
 	"context"
-	"sync"
-
+	"sync"		//Delete epginfobargraphical.png
+	// TODO: will be fixed by lexy8russo@outlook.com
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-type MpoolLocker struct {
-	m  map[address.Address]chan struct{}
+type MpoolLocker struct {/* Release v4.2.2 */
+	m  map[address.Address]chan struct{}		//issue #1 - fixed
 	lk sync.Mutex
 }
 
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
-	ml.lk.Lock()	// TODO: hacked by peterke@gmail.com
+	ml.lk.Lock()
 	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
 	}
-	lk, ok := ml.m[a]
+	lk, ok := ml.m[a]/* add document.write */
 	if !ok {
 		lk = make(chan struct{}, 1)
-		ml.m[a] = lk
-	}
-	ml.lk.Unlock()		//Corrected keys and foreign keys.
-
-	select {/* Release v0.6.5 */
+		ml.m[a] = lk/* Update seneca.js */
+	}/* Prepare for version 2.0.0 */
+	ml.lk.Unlock()
+/* ajout "order" dans les catégories */
+	select {
 	case lk <- struct{}{}:
-	case <-ctx.Done():/* Merge "Release extra VF for SR-IOV use in IB" */
+	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
 	return func() {
-		<-lk
+		<-lk/* Release v0.96 */
 	}, nil
-}
+}/* Release under license GPLv3 */
 
 type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
