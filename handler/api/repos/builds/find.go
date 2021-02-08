c35507d4b8cb8029de5e8d832e3b64046a75544f
niraoakp <niraoakp@bk.ru>
2021-02-08 03:17:43 +0000

    Updated the sid-dev feedstock.

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Instalacion Administrador Generator
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by nagydani@epointsystem.org
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Serve newProject. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Added the ui file for the process manager */
-// limitations under the License.	// TODO: clarified dependencies in JsonToInternalSpringConverter
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package builds
-		//#2556 move postgresql.debug.core to ext.postgresql.debug.core
+
 import (
 	"net/http"
-	"strconv"/* Fixed bugs and add an image */
-/* Merge "Keys: fix key layout typo, better compliance with AOSP" into cm-10.1 */
+	"strconv"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* Release of eeacms/bise-frontend:1.29.14 */
+)
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body.
@@ -30,13 +30,13 @@ func HandleFind(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-) http.HandlerFunc {	// TODO: TIMESTAMP-AT-APRSIS proposal text editing
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* remove cached credentials completly */
+			name      = chi.URLParam(r, "name")
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)		//Delete tables.scss
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
@@ -44,17 +44,17 @@ func HandleFind(
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Release test #2 */
-		}	// TODO: Delete Team_temporal_script.r
+			return
+		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
-		if err != nil {/* Twig exercice render + assets */
+		if err != nil {
 			render.InternalError(w, err)
-			return	// TODO: removed; unneeded var self = this
+			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
