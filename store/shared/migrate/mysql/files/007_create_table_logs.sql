d1a78d3e6864b01aefe541c4339eefdac1738f60
niraoakp <niraoakp@bk.ru>
2021-02-08 00:02:43 +0000

    Create arithmeticExpression.py

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,5 +1,5 @@
--- name: create-table-logs
-
+-- name: create-table-logs	// writing changes to wallet
+		//Updated to use current timestep's delta
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
 ,log_data  MEDIUMBLOB
