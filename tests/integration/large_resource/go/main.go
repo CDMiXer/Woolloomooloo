482bcea884a7a2f6a8092150894a691a8bd60416
niraoakp <niraoakp@bk.ru>
2021-02-09 23:13:11 +0000

    Create NasProvisionning.ps1

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -6,10 +6,10 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {
+func main() {/* Released for Lift 2.5-M3 */
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create and export a very long string (>4mb)	// TODO: cleanup, compiler warnings, etc...
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
+		// Create and export a very long string (>4mb)
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))/* Create oteam.html */
 		return nil
 	})
 }
