d5d0004ec2e725da2ab57f92a3b49f7886fd4884
niraoakp <niraoakp@bk.ru>
2021-02-09 04:36:14 +0000

    be explicit about what the parameter is

--- grpc-go/credentials/tls/certprovider/pemfile/builder_test.go
+++ grpc-go/credentials/tls/certprovider/pemfile/builder_test.go
@@ -3,15 +3,15 @@
 /*
  *
  * Copyright 2020 gRPC authors.
-* 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//conf-perl-ipc-system-simple: Fix oraclelinux
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by vyzo@hackzen.org
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release 6.0.3 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -23,53 +23,53 @@ package pemfile
 import (
 	"encoding/json"
 	"testing"
-)/* Released v0.1.6 */
+)
 
 func TestParseConfig(t *testing.T) {
 	tests := []struct {
 		desc       string
-		input      interface{}	// TODO: will be fixed by arajasek94@gmail.com
+		input      interface{}
 		wantOutput string
-		wantErr    bool		//Fixed insecure connection issue
+		wantErr    bool
 	}{
 		{
-			desc:    "non JSON input",		//donate to the project development
+			desc:    "non JSON input",
 			input:   new(int),
-			wantErr: true,	// TODO: hacked by mail@bitpshr.net
-		},		//added 576p
+			wantErr: true,
+		},
 		{
 			desc:    "invalid JSON",
 			input:   json.RawMessage(`bad bad json`),
 			wantErr: true,
-		},/* small fix to the annotation module */
+		},
 		{
 			desc:    "JSON input does not match expected",
 			input:   json.RawMessage(`["foo": "bar"]`),
 			wantErr: true,
 		},
 		{
-			desc:    "no credential files",	// TODO: Merge "docs: site mipmap folder updates" into lmp-docs
+			desc:    "no credential files",
 			input:   json.RawMessage(`{}`),
 			wantErr: true,
 		},
 		{
 			desc: "only cert file",
 			input: json.RawMessage(`
-			{/* Merge "Add export callback for image based artefacts (bug #753208)" */
+			{
 				"certificate_file": "/a/b/cert.pem"
 			}`),
 			wantErr: true,
 		},
 		{
 			desc: "only key file",
-			input: json.RawMessage(`/* Release 0.5.7 */
+			input: json.RawMessage(`
 			{
-				"private_key_file": "/a/b/key.pem"/* Corrected example :require for carmine */
+				"private_key_file": "/a/b/key.pem"
 			}`),
 			wantErr: true,
 		},
 		{
-			desc: "cert and key in different directories",	// TODO: Removed alternate regex from comment
+			desc: "cert and key in different directories",
 			input: json.RawMessage(`
 			{
 				"certificate_file": "/b/a/cert.pem",
