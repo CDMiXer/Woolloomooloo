70ffe96a08e159853c2ea8f546df4d2b4194608f
niraoakp <niraoakp@bk.ru>
2021-02-09 11:34:03 +0000

    New shell package for shell scripts

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,20 +1,20 @@
 package genesis
 
 import (
-	"context"
+	"context"		//Convert ClassLoaderHandlers to use the Java ServiceLoader framework.
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin"/* Released 1.6.4. */
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"		//Merge "Bug 1821267: Submission Email and Submission Report"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"
+	bstore "github.com/filecoin-project/lotus/blockstore"/* Adds links to Heroku and Travis to README.md */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
-
+/* Merge "Release 3.2.3.352 Prima WLAN Driver" */
 	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
@@ -23,7 +23,7 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
 		Head:    stcid,
-		Nonce:   0,
-		Balance: types.NewInt(0),
+		Nonce:   0,		//Update JME. Use new method to clear processors.
+		Balance: types.NewInt(0),	// TODO: theme : removing mdb-* theme files
 	}, nil
-}
+}/* Add reference to Wikipedia article and Krautsevich's interview */
