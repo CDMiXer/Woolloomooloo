3693450f8781d598907fb8e15304637771da473b
niraoakp <niraoakp@bk.ru>
2021-02-09 08:14:37 +0000

    Updated composer.phar version.

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -2,8 +2,8 @@
 
 import { Resource } from "./resource";
 
-// Step 2: Reverse the dependency graph./* Release for v46.1.0. */
-// Checkpoint dependency graph: b -> a	// TODO: will be fixed by xaber.twt@gmail.com
-const b = new Resource("b", { state: 2 })		//Fixed missing constructor.
+// Step 2: Reverse the dependency graph.
+// Checkpoint dependency graph: b -> a/* fixes radio button */
+const b = new Resource("b", { state: 2 })/* [git] update gitignore */
 const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
