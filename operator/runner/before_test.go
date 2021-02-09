e36b880ea792251eb54dbee71340019877f99c1f
niraoakp <niraoakp@bk.ru>
2021-02-09 20:49:33 +0000

    Disable warnings in RSpec

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release jedipus-2.6.35 */
 // that can be found in the LICENSE file.
 
 package runner
