2f8c860831e9b4a371f249a21d63fb0045649668
niraoakp <niraoakp@bk.ru>
2021-02-09 10:58:24 +0000

    Fixes typo from 8c77cf8

--- grpc-go/interop/alts/server/server.go
+++ grpc-go/interop/alts/server/server.go
@@ -7,31 +7,31 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release Notes: update for 4.x */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by jon@atack.com
- * See the License for the specific language governing permissions and	// TODO: Friendly message when there are no logs.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Fix build s1g1 */
+
 // This binary can only run on Google Cloud Platform (GCP).
 package main
 
 import (
-	"context"/* incorporate Alek's comments */
-	"flag"/* a8c39822-2e5f-11e5-9284-b827eb9e62be */
+	"context"
+	"flag"
 	"net"
 	"strings"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
-	"google.golang.org/grpc/grpclog"		//Merge "add default route to route table of default vpc"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/interop"
 	"google.golang.org/grpc/tap"
 
-	testgrpc "google.golang.org/grpc/interop/grpc_testing"	// TODO: will be fixed by aeongrp@outlook.com
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 )
 
 const (
@@ -39,14 +39,14 @@ const (
 )
 
 var (
-	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")/* Release note item for the new HSQLDB DDL support */
-	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")		//Fix scanner instantiation
-	// improve file download progress
+	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
+	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")
+
 	logger = grpclog.Component("interop")
 )
 
-func main() {	// Multiple ring buffers for multiple channels.
-	flag.Parse()		//Updated README with how to run EMMA
+func main() {
+	flag.Parse()
 
 	// If the server address starts with `unix:`, then we have a UDS address.
 	network := "tcp"
@@ -54,10 +54,10 @@ func main() {	// Multiple ring buffers for multiple channels.
 	if strings.HasPrefix(address, udsAddrPrefix) {
 		network = "unix"
 		address = strings.TrimPrefix(address, udsAddrPrefix)
-	}/* Release: Making ready to release 4.1.4 */
+	}
 	lis, err := net.Listen(network, address)
-	if err != nil {		//chore(package): update flow-parser to version 0.111.3
-		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)	// TODO: will be fixed by why@ipfs.io
+	if err != nil {
+		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)
 	}
 	opts := alts.DefaultServerOptions()
 	if *hsAddr != "" {
