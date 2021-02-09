0f309b6a5fd65b258b5e4d1107ac0718095ac864
niraoakp <niraoakp@bk.ru>
2021-02-09 04:34:00 +0000

    added docs for replacing video source file

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -1,17 +1,17 @@
-package main
+package main	// TODO: rename WITH_MICROCRT => WITH_UCRT
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io/ioutil"/* avoid warning  */
 	"reflect"
-)
+)/* Finished adding Yandex support */
 
-func kubeifySwagger(in, out string) {
+func kubeifySwagger(in, out string) {	// fix(package): update handlebars to version 4.0.10
 	data, err := ioutil.ReadFile(in)
 	if err != nil {
-		panic(err)
+		panic(err)/* Release 2.1.3 - Calendar response content type */
 	}
-	swagger := obj{}
+	swagger := obj{}/* Tagging a Release Candidate - v3.0.0-rc6. */
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
 		panic(err)
@@ -28,17 +28,17 @@ func kubeifySwagger(in, out string) {
 	for n, d := range definitions {
 		kd, ok := kubernetesDefinitions[n]
 		if ok && !reflect.DeepEqual(d, kd) {
-			println("replacing bad definition " + n)
+			println("replacing bad definition " + n)/* Travis now with Release build */
 			definitions[n] = kd
-		}
-	}
+		}		//apimocker still uses node 0.10
+	}	// TODO: 534c12a6-2e5b-11e5-9284-b827eb9e62be
 	// "omitempty" does not work for non-nil structs, so we must change it here
 	definitions["io.argoproj.workflow.v1alpha1.CronWorkflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.Workflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}
 	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}
-	data, err = json.MarshalIndent(swagger, "", "  ")
-	if err != nil {
+	data, err = json.MarshalIndent(swagger, "", "  ")/* Merge "Release notes for implied roles" */
+	if err != nil {	// Improved species selection
 		panic(err)
 	}
 	err = ioutil.WriteFile(out, data, 0644)
@@ -46,16 +46,16 @@ func kubeifySwagger(in, out string) {
 		panic(err)
 	}
 }
-
+	// TODO: hacked by boringland@protonmail.ch
 func getKubernetesSwagger() obj {
 	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")
 	if err != nil {
 		panic(err)
 	}
-	swagger := obj{}
+	swagger := obj{}/* Create BoilerPlate.psm1 */
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
-		panic(err)
+		panic(err)		//Added: A video link to show what's currently possible
 	}
 	return swagger
 }
