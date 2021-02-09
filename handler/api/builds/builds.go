78c71c37d3ceccc456bb8a8cb7371d6e682fe813
niraoakp <niraoakp@bk.ru>
2021-02-09 19:42:07 +0000

    tslib-maemo: fix do_stage as described in oe bug #690

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -1,30 +1,30 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* JO-585: correzione nome variabile queryset */
+// Use of this source code is governed by the Drone Non-Commercial License/* delete some paragraphs. */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package builds
+package builds	// add bid place tests
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Update formatting in Readme.md */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
-
+)		//Give date and username in verbose output
+		//reviewer duty
 // HandleIncomplete returns an http.HandlerFunc that writes a
-// json-encoded list of incomplete builds to the response body./* Merge "Parse out '@' in volume['host'] to do discovery" */
+// json-encoded list of incomplete builds to the response body./* Merge "diag: Release mutex in corner case" into ics_chocolate */
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		list, err := repos.ListIncomplete(r.Context())	// TODO: hacked by arajasek94@gmail.com
-		if err != nil {
+		list, err := repos.ListIncomplete(r.Context())/* FIX Date on flyer */
+		if err != nil {	// TODO: will be fixed by steven@stebalien.com
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list incomplete builds")
-		} else {/* Release Candidate. */
-			render.JSON(w, list, 200)/* Release changes 4.1.2 */
+		} else {	// TODO: 91cb62a6-2e66-11e5-9284-b827eb9e62be
+			render.JSON(w, list, 200)
 		}
 	}
 }
