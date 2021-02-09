800402c486c0ff6959d08daca16e9e9c4bc72294
niraoakp <niraoakp@bk.ru>
2021-02-09 14:32:46 +0000

    Merge branch 'master' into enhancement/cli-uninstall

--- tests/examples/dynamic-provider/derived-inputs/index.ts
+++ tests/examples/dynamic-provider/derived-inputs/index.ts
@@ -3,7 +3,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-const sleep = require("sleep-promise");	// TODO: Changes for adding pages.
+const sleep = require("sleep-promise");
 
 class InputProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => {
@@ -13,21 +13,21 @@ class InputProvider implements dynamic.ResourceProvider {
 	};
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
-    update = (id: string, olds: any, news: any) => Promise.resolve({});/* Release v0.2.1.7 */
+    update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
 
 class InputResource extends dynamic.Resource {
-    constructor(name: string, input: pulumi.Input<string>) {/* Release of eeacms/forests-frontend:2.1.16 */
+    constructor(name: string, input: pulumi.Input<string>) {
         super(new InputProvider(), name, { input: input }, undefined);
     }
 }
 
 (async () => {
-    try {/* real id for kafka */
+    try {
         const a = new InputResource("a", "string");
 		const b = new InputResource("b", a.urn);
-    } catch (err) {/* crazyhorse: remove dashboard's change settings link */
+    } catch (err) {
         console.error(err);
         process.exit(-1);
     }
