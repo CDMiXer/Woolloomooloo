dd8de43d5c5201cde31e43d23d33119695b75cfd
niraoakp <niraoakp@bk.ru>
2021-02-09 11:31:30 +0000

    Create Planes_stereonet_intersection.py

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Реализовать Singleton pattern */
 
 package encrypt
 
-import "testing"
+import "testing"/* Committing initial version (0.3.0) */
 
 func TestNone(t *testing.T) {
 	n, _ := New("")
-	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
+	ciphertext, err := n.Encrypt("correct-horse-batter-staple")/* Fixed wrapped line. */
 	if err != nil {
-		t.Error(err)
-	}
+		t.Error(err)		//Update LICENSE to the unlicense
+	}	// TODO: 4e07a3b8-2e75-11e5-9284-b827eb9e62be
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
 		t.Error(err)
 	}
 	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
-	}
-}
+	}	// TODO: shouldn't have activated the profile panel (yet)
+}/* removed find_mismatch_test in favour of equality_test */
