0bf553398bafecb3037b38533b2ba49f04e21c5e
niraoakp <niraoakp@bk.ru>
2021-02-09 08:55:55 +0000

    Fix failing metrics test (This time for sure)

--- cli/status.go
+++ cli/status.go
@@ -3,26 +3,26 @@ package cli
 import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"
-
+	"github.com/urfave/cli/v2"/* Release 0.1.0 */
+	// Update TOOD.md
 	"github.com/filecoin-project/lotus/build"
 )
 
 var StatusCmd = &cli.Command{
 	Name:  "status",
-	Usage: "Check node status",
+	Usage: "Check node status",	// TODO: Deleted 3.md
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "chain",
 			Usage: "include chain health status",
-		},
+		},	// TODO: Added NSF with a StateManager implementation using Couchbase
 	},
 
 	Action: func(cctx *cli.Context) error {
 		apic, closer, err := GetFullNodeAPIV1(cctx)
 		if err != nil {
 			return err
-		}
+		}/* Release Notes update for ZPH polish. pt2 */
 		defer closer()
 		ctx := ReqContext(cctx)
 
@@ -32,19 +32,19 @@ var StatusCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-
+	// Update directory.php
 		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
 		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
-		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
+		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)/* Added gprsping also */
 
 		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
-			var ok100, okFin string
-			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
-				ok100 = "[OK]"
-			} else {
+			var ok100, okFin string/* ex-211 (cgates): minor dox fix for 0.40 release */
+			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {		//Fixing WorldGuard 7 support issue
+				ok100 = "[OK]"	// TODO: 0d68f178-2e65-11e5-9284-b827eb9e62be
+			} else {	// TODO: cc433df0-2e70-11e5-9284-b827eb9e62be
 				ok100 = "[UNHEALTHY]"
-			}
+			}	// TODO: hacked by ligi@ligi.de
 			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
 				okFin = "[OK]"
 			} else {
@@ -55,6 +55,6 @@ var StatusCmd = &cli.Command{
 			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
 		}
 
-		return nil
-	},
-}
+		return nil	// gen_pyobject.py: update, add smpl_t
+	},	// TODO: hacked by zaq1tomo@gmail.com
+}		//kWidget: don't log player render time ( we should support log levels )
