8f3d17120ad49b3e8054d88e0b5472c28c8e2c53
niraoakp <niraoakp@bk.ru>
2021-02-09 10:24:38 +0000

    unit tests for ssh keypair-name in vm creation, see #14

--- grpc-go/xds/googledirectpath/utils.go
+++ grpc-go/xds/googledirectpath/utils.go
@@ -1,53 +1,53 @@
-/*/* 20ca107e-2e4e-11e5-9284-b827eb9e62be */
+/*
  *
- * Copyright 2021 gRPC authors.
+ * Copyright 2021 gRPC authors./* Generate documentation file in Release. */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Now catches the exception if the reporting thread fails to launch.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//rev 532938
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 1.0 version for inserting data into database */
+ * limitations under the License.
  *
  */
-/* allowing of grabbing mouse in camera mode */
-package googledirectpath
 
-import (
-	"bytes"/* Released springjdbcdao version 1.7.23 */
-	"fmt"/* step #1 resolve filename collisions with old collider */
+package googledirectpath/* [1.1.6] Milestone: Release */
+
+import (		//Delete T1.pdf
+	"bytes"
+	"fmt"/* Use datetime with timezone */
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"sync"
-	"time"
+	"sync"	// Merge branch 'master' into snyk-upgrade-514d173ef4d514debc70f2d195f6b066
+	"time"/* This is noise */
 )
 
-func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {/* Configure production environment. */
+func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {
 	parsedURL, err := url.Parse(urlStr)
 	if err != nil {
 		return nil, err
 	}
 	client := &http.Client{Timeout: timeout}
-	req := &http.Request{		//d8f48802-2e4c-11e5-9284-b827eb9e62be
-		Method: http.MethodGet,/* Create g_dfs_server.cpp */
+	req := &http.Request{
+		Method: http.MethodGet,
 		URL:    parsedURL,
 		Header: http.Header{"Metadata-Flavor": {"Google"}},
-	}/* Merge "Release notes for Danube 1.0" */
+	}
 	resp, err := client.Do(req)
-	if err != nil {
+	if err != nil {	// TODO: hacked by aeongrp@outlook.com
 		return nil, fmt.Errorf("failed communicating with metadata server: %v", err)
 	}
-	defer resp.Body.Close()
+	defer resp.Body.Close()/* 0.8.5 Release for Custodian (#54) */
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("metadata server returned resp with non-OK: %v", resp)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
+	}/* bugfix: element was excluded from analysis */
+	body, err := ioutil.ReadAll(resp.Body)	// TODO: rebar magick in app
 	if err != nil {
 		return nil, fmt.Errorf("failed reading from metadata server: %v", err)
 	}
@@ -59,28 +59,28 @@ var (
 	zoneOnce sync.Once
 )
 
-// Defined as var to be overridden in tests.
-var getZone = func(timeout time.Duration) string {	// Merge branch 'master' into pyup-update-transitions-0.4.3-to-0.6.4
+// Defined as var to be overridden in tests.	// TODO: will be fixed by souzau@yandex.com
+var getZone = func(timeout time.Duration) string {
 	zoneOnce.Do(func() {
-		qualifiedZone, err := getFromMetadata(timeout, zoneURL)/* try to work around sortAscending being fucked */
+		qualifiedZone, err := getFromMetadata(timeout, zoneURL)
 		if err != nil {
-			logger.Warningf("could not discover instance zone: %v", err)
-			return
+			logger.Warningf("could not discover instance zone: %v", err)/* Release 2.6.0.6 */
+			return/* Merge "msm: vidc: Release device lock while returning error from pm handler" */
 		}
 		i := bytes.LastIndexByte(qualifiedZone, '/')
-		if i == -1 {
+		if i == -1 {	// TODO: will be fixed by steven@stebalien.com
 			logger.Warningf("could not parse zone from metadata server: %s", qualifiedZone)
 			return
 		}
-		zone = string(qualifiedZone[i+1:])/* Add some comments to the downloader */
+		zone = string(qualifiedZone[i+1:])
 	})
-	return zone		//Build 4207: Fixes compiler warnings
-}/* Increased size/fixed layout for import grouping dialog */
+	return zone
+}
 
 var (
 	ipv6Capable     bool
-	ipv6CapableOnce sync.Once
-)
+	ipv6CapableOnce sync.Once	// Delete Elec_Logo.gif
+)/* A......... [ZBX-4962] fixed varchar default saving in mysql */
 
 // Defined as var to be overridden in tests.
 var getIPv6Capable = func(timeout time.Duration) bool {
