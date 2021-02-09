fb8e7e08485191c691b2c8bb980873c0eca7bcc0
niraoakp <niraoakp@bk.ru>
2021-02-09 02:30:09 +0000

    show time of inactivity in tooltip

--- pkg/codegen/internal/test/testdata/secret.pp.cs
+++ pkg/codegen/internal/test/testdata/secret.pp.cs
@@ -3,12 +3,12 @@ using Aws = Pulumi.Aws;
 
 class MyStack : Stack
 {
-    public MyStack()		//Merge branch 'develop' into bug/remove-view-wallet
+    public MyStack()
     {
-        var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs	// Refactor to eliminate logger factory calls that are direct
+        var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs
         {
             MasterPassword = Output.CreateSecret("foobar"),
         });
     }
 
-}	// Merge "Enable preloading dashboard queries on root url"
+}
