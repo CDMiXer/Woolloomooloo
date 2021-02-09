eab8ebcabd212fc2691bcc0354fa3c15da9cab6a
niraoakp <niraoakp@bk.ru>
2021-02-09 16:05:19 +0000

    Fix whitespace + remove unused config pt 1

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* docs: update readme sub title */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: [8872] updated findings observation and added observationlink
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Release version 1.0.0. */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* added xml-rpc route */
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Release 1.0.0.239 QCACLD WLAN Driver" */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Added Exception Logging in Validator
+// See the License for the specific language governing permissions and	// SQL Atualizado
 // limitations under the License.
 
-// +build oss	// TODO: fix compile error on PHP7.
+// +build oss
 
 package builds
-/* Release version 2.3.1. */
-import (
+
+import (/* Release Lite v0.5.8: Remove @string/version_number from translations */
 	"net/http"
 
 	"github.com/drone/drone/core"
-)
+)		//copy image from local filesystem if not packed into.blend, fix linter warning
 
 // HandlePurge returns a non-op http.HandlerFunc.
-func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
+func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {		//Another incubation example…
 	return notImplemented
-}
+}	// Fix: TapiEth Yang validation errors- renamed Eth OAM attributes uniquely
