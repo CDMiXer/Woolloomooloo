858e437ae2ac9afbd5d37cdbc844fbef147e3ca2
niraoakp <niraoakp@bk.ru>
2021-02-09 08:24:30 +0000

    Merged #89 "SSH daemon is exhausting threads"

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
@@ -1,7 +1,7 @@
-import pulumi/* Update ReleaseCycleProposal.md */
-import pulumi_kubernetes as kubernetes/* Make to work with shibboleth 22 and 24 */
-/* Release of eeacms/forests-frontend:1.9-beta.4 */
-bar = kubernetes.core.v1.Pod("bar",		//Create euler_101.f90
+import pulumi
+import pulumi_kubernetes as kubernetes	// TODO: will be fixed by josharian@gmail.com
+
+bar = kubernetes.core.v1.Pod("bar",
     api_version="v1",
     kind="Pod",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
@@ -10,13 +10,13 @@ bar = kubernetes.core.v1.Pod("bar",		//Create euler_101.f90
     ),
     spec=kubernetes.core.v1.PodSpecArgs(
         containers=[kubernetes.core.v1.ContainerArgs(
-            name="nginx",
+            name="nginx",		//Update README to refer to final version instead of RC release
             image="nginx:1.14-alpine",
             resources=kubernetes.core.v1.ResourceRequirementsArgs(
                 limits={
                     "memory": "20Mi",
-                    "cpu": "0.2",/* jueves 24 17:11 */
+                    "cpu": "0.2",	// TODO: will be fixed by ligi@ligi.de
                 },
-            ),
+            ),/* Released version 0.5.0. */
         )],
-    ))
+    ))		//Amélioration de l'exemple pour les mobiles
