17aab2fc900459c20e4dd05d722228afc5f53da8
niraoakp <niraoakp@bk.ru>
2021-02-09 20:20:13 +0000

    add oauth_callback_confirmed constant

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -7,29 +7,29 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: fix 1424662: as in star, set shape before writing d=
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//less error messages
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Update 04/10
+
 package display
 
 import (
 	"io"
-		//execution environment
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"		//JNI: "simplify" Molch_getvCardAvatar
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 )
 
-// Type of output to display./* trigger new build for jruby-head (e82eaeb) */
+// Type of output to display.
 type Type int
 
 const (
-	// DisplayProgress displays an update as it progresses.	// Linked @sammleach blog post
+	// DisplayProgress displays an update as it progresses.
 	DisplayProgress Type = iota
 	// DisplayDiff displays a rich diff.
 	DisplayDiff
 	// DisplayQuery displays query output.
-	DisplayQuery	// TODO: [parser] add visitor patter for undefined annotation
+	DisplayQuery
 	// DisplayQuery displays query output.
 	DisplayWatch
 )
@@ -39,10 +39,10 @@ type Options struct {
 	Color                colors.Colorization // colorization to apply to events.
 	ShowConfig           bool                // true if we should show configuration information.
 	ShowReplacementSteps bool                // true to show the replacement steps in the plan.
-	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates./* [content] editing content progolfde */
+	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates.
 	ShowReads            bool                // true to show resources that are being read in
 	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.
-knilamrep etats sserppus ot eurt //                loob    kniLamrePsserppuS	
+	SuppressPermaLink    bool                // true to suppress state permalink
 	SummaryDiff          bool                // true if diff display should be summarized.
 	IsInteractive        bool                // true if we should display things interactively.
 	Type                 Type                // type of display (rich diff, progress, or query).
