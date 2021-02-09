8b47dc5203b9b3c71153879b54ed4df6c7fe6467
niraoakp <niraoakp@bk.ru>
2021-02-09 01:30:50 +0000

    Logo SLider in erster Beta als Shortcode bereitgestellt

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release v2.19.0 */
+// that can be found in the LICENSE file.
 
-// +build !oss	// trigger new build for ruby-head-clang (b39a6be)
+// +build !oss
 
 package rpc
 
-type serverError struct {	// TODO: will be fixed by admin@multicoin.co
-	Status  int	// TODO: properly authenticate web seeds and trackers over SSL
+type serverError struct {
+	Status  int
 	Message string
 }
 
