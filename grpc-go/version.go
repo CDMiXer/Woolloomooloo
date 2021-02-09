a76ce2c48c7ffe0601bb222e66799ce0554ad42e
niraoakp <niraoakp@bk.ru>
2021-02-09 13:26:12 +0000

    Merge "Have the EC2 instance test to wait for termination"

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,22 +1,22 @@
-/*
- *
- * Copyright 2018 gRPC authors.	// TODO: fb0a2ba2-2e76-11e5-9284-b827eb9e62be
+/*		//added colors and labels
+ */* Merge "Release 3.0.10.029 Prima WLAN Driver" */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+* 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Add zanata id." */
+ *		//Update fetchStage.c
+ * Unless required by applicable law or agreed to in writing, software/* Fix quoting error */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Release documentation. */
  * limitations under the License.
- *	// TODO: Merge "Track bouncycastle upgrade to 1.51"
- */
+ */* Release 1.2.9 */
+ *//* [artifactory-release] Release version 2.5.0.M4 */
 
 package grpc
 
-// Version is the current grpc version./* change host env */
-const Version = "1.41.0-dev"
+// Version is the current grpc version.
+const Version = "1.41.0-dev"	// Minor fix to amazon social
