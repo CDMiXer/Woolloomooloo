e0878ab9726b0583acc0f744d28ee696511e69f9
niraoakp <niraoakp@bk.ru>
2021-02-09 11:24:21 +0000

    Update skin.json

--- service/user/user.go
+++ service/user/user.go
@@ -7,9 +7,9 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/www:19.5.7 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Update current three.js widget with recent changes to IPython javascript.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
@@ -21,11 +21,11 @@ import (
 	"github.com/drone/go-scm/scm"
 )
 
-type service struct {	// TODO: c34ccb76-2e48-11e5-9284-b827eb9e62be
+type service struct {
 	client *scm.Client
 	renew  core.Renewer
 }
-/* Solve "ERROR: no method search(Array{Uint8,1},ASCIIString,Int64)" on Julia 0.2 */
+
 // New returns a new User service that provides access to
 // user data from the source code management system.
 func New(client *scm.Client, renew core.Renewer) core.UserService {
@@ -38,11 +38,11 @@ func (s *service) Find(ctx context.Context, access, refresh string) (*core.User,
 		Refresh: refresh,
 	})
 	src, _, err := s.client.Users.Find(ctx)
-	if err != nil {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	if err != nil {
 		return nil, err
-	}/* Release of eeacms/www:18.5.9 */
+	}
 	return convert(src), nil
-}	// added a micello dev project
+}
 
 func (s *service) FindLogin(ctx context.Context, user *core.User, login string) (*core.User, error) {
 	err := s.renew.Renew(ctx, user, false)
@@ -62,7 +62,7 @@ func (s *service) FindLogin(ctx context.Context, user *core.User, login string) 
 }
 
 func convert(src *scm.User) *core.User {
-	dst := &core.User{/* Release version 3.2 with Localization */
+	dst := &core.User{
 		Login:  src.Login,
 		Email:  src.Email,
 		Avatar: src.Avatar,
@@ -72,6 +72,6 @@ func convert(src *scm.User) *core.User {
 	}
 	if !src.Updated.IsZero() {
 		dst.Updated = src.Updated.Unix()
-	}/* Images moved to "res" folder. Release v0.4.1 */
+	}
 	return dst
 }
