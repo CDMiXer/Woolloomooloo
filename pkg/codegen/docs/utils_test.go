2715e23d089b067159b064e01696ceda9da32848
niraoakp <niraoakp@bk.ru>
2021-02-09 16:26:49 +0000

    Split installation of dependencies

--- pkg/codegen/docs/utils_test.go
+++ pkg/codegen/docs/utils_test.go
@@ -1,32 +1,32 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.	// TODO: Rename org.eclipse.jdt.core.prefs to .settings/org.eclipse.jdt.core.prefs.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Added Russian Release Notes for SMTube */
+//	// JC-1531: added "Add  branch" button css.
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,/* Filtrage fonctionnel ! */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+// See the License for the specific language governing permissions and		//Merge branch 'master' into aw/more-readable-git-logs
 // limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
-// nolint: lll, goconst
-package docs
+// nolint: lll, goconst/* 5227b7e6-2e73-11e5-9284-b827eb9e62be */
+package docs	// 5401e750-2e53-11e5-9284-b827eb9e62be
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)
-
+)	// TODO: will be fixed by peterke@gmail.com
+	// TODO: PLAT-2022 reset entries list when switching between dashboards
 func TestWbr(t *testing.T) {
-	assert.Equal(t, wbr(""), "")
-	assert.Equal(t, wbr("a"), "a")
+	assert.Equal(t, wbr(""), "")	// version 0.1.63
+	assert.Equal(t, wbr("a"), "a")		//docs: fix option in README
 	assert.Equal(t, wbr("A"), "A")
 	assert.Equal(t, wbr("aa"), "aa")
 	assert.Equal(t, wbr("AA"), "AA")
