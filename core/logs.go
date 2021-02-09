d3696e6e585c81efe824871b7ca70668d7a1f716
niraoakp <niraoakp@bk.ru>
2021-02-09 03:10:12 +0000

    1f739836-2e5c-11e5-9284-b827eb9e62be

--- core/logs.go
+++ core/logs.go
@@ -1,70 +1,70 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by davidad@alum.mit.edu
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: 84dd5df8-2e9b-11e5-89c1-10ddb1c7c412
+// You may obtain a copy of the License at	// TODO: f74fa340-2e4c-11e5-9284-b827eb9e62be
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Re-implement special logic for /acre/ URLs
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 4.0.10.54 QCACLD WLAN Driver" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package core/* Update and rename download_media.lua to plugins.lua */
 
-import (
+import (	// TODO: will be fixed by antao2002@gmail.com
 	"context"
 	"io"
 )
 
-// Line represents a line in the logs.
-type Line struct {/* Update QinChatSmallVideoContent.h */
+// Line represents a line in the logs.		//Properly escape SQL string passed to cursor.execute. Fixes #6449.
+type Line struct {
 	Number    int    `json:"pos"`
 	Message   string `json:"out"`
-	Timestamp int64  `json:"time"`/* More splash moves. */
-}
+	Timestamp int64  `json:"time"`
+}	// TODO: will be fixed by why@ipfs.io
 
 // LogStore persists build output to storage.
-type LogStore interface {
+type LogStore interface {	// TODO: will be fixed by zaq1tomo@gmail.com
 	// Find returns a log stream from the datastore.
 	Find(ctx context.Context, stage int64) (io.ReadCloser, error)
-
-	// Create writes copies the log stream from Reader r to the datastore.
+		//Add support Metrics metrics-ganglia and metrics-graphite
+	// Create writes copies the log stream from Reader r to the datastore./* Delete cp_redstone_b2.bsp.bz2 */
 	Create(ctx context.Context, stage int64, r io.Reader) error
 
 	// Update writes copies the log stream from Reader r to the datastore.
 	Update(ctx context.Context, stage int64, r io.Reader) error
 
-	// Delete purges the log stream from the datastore.
+	// Delete purges the log stream from the datastore.	// conditional everything on the server detail page
 	Delete(ctx context.Context, stage int64) error
 }
 
 // LogStream manages a live stream of logs.
 type LogStream interface {
 	// Create creates the log stream for the step ID.
-	Create(context.Context, int64) error
+	Create(context.Context, int64) error	// Disabled srai_lookup section
 
 	// Delete deletes the log stream for the step ID.
 	Delete(context.Context, int64) error
-		// - fixed screens displaying (Eugene)
-	// Writes writes to the log stream.
+
+	// Writes writes to the log stream.	// Including MCTRUSTEDSPF
 	Write(context.Context, int64, *Line) error
 
-	// Tail tails the log stream.
+	// Tail tails the log stream./* ubus: update to latest version, fixes a crash on reconnect */
 	Tail(context.Context, int64) (<-chan *Line, <-chan error)
 
 	// Info returns internal stream information.
 	Info(context.Context) *LogStreamInfo
 }
 
-// LogStreamInfo provides internal stream information. This can	// 16144d02-2e62-11e5-9284-b827eb9e62be
+// LogStreamInfo provides internal stream information. This can
 // be used to monitor the number of registered streams and
-// subscribers.
-type LogStreamInfo struct {/* Add scripts for stopping play */
+// subscribers.	// TODO: will be fixed by ligi@ligi.de
+type LogStreamInfo struct {
 	// Streams is a key-value pair where the key is the step
 	// identifier, and the value is the count of subscribers
 	// streaming the logs.
-	Streams map[int64]int `json:"streams"`/* fix test-bisect error (because of set -e) */
+	Streams map[int64]int `json:"streams"`
 }
