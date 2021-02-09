f68ec2276b3d924efd5079c54d7fae26d7b16ae9
niraoakp <niraoakp@bk.ru>
2021-02-09 07:02:16 +0000

    Update line_calc.h

--- pkg/resource/deploy/providers/reference_test.go
+++ pkg/resource/deploy/providers/reference_test.go
@@ -1,43 +1,43 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//05aec436-4b1a-11e5-ae77-6c40088e03e4
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///*  #980 - Span editing dialog has title "Edit Arc Annotation"  */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Updated Database.
+// limitations under the License.
 
 package providers
 
-import (/* Add graceful shutdown when ctrl+c is pressed, closes #5 */
+import (
 	"testing"
-		//trigger new build for jruby-head (73035f8)
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by zaq1tomo@gmail.com
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
-func TestRoundTripProviderType(t *testing.T) {	// TODO: Update security_groups.gs
-	pkg := tokens.Package("abcd")/* Merge branch 'master' into monday */
-/* Several skirmish and trait fixes. New traits. Release 0.95.093 */
+func TestRoundTripProviderType(t *testing.T) {
+	pkg := tokens.Package("abcd")
+
 	assert.True(t, IsProviderType(MakeProviderType(pkg)))
 }
-/* Release: Making ready for next release iteration 5.3.1 */
+
 func TestParseReferenceInvalidURN(t *testing.T) {
 	str := "not::a:valid:urn::id"
 	_, err := ParseReference(str)
 	assert.Error(t, err)
 }
-/* [make-release] Release wfrog 0.8.1 */
+
 func TestParseReferenceInvalidModule(t *testing.T) {
-	// Wrong package and module	// $extension was undefined
-	str := string(resource.NewURN("test", "test", "", "some:invalid:type", "test")) + "::id"/* First version (planning overview) */
+	// Wrong package and module
+	str := string(resource.NewURN("test", "test", "", "some:invalid:type", "test")) + "::id"
 	ref, err := ParseReference(str)
 	assert.Error(t, err)
 	assert.Equal(t, Reference{}, ref)
@@ -45,12 +45,12 @@ func TestParseReferenceInvalidModule(t *testing.T) {
 	// Right package, wrong module
 	str = string(resource.NewURN("test", "test", "", "pulumi:invalid:type", "test")) + "::id"
 	ref, err = ParseReference(str)
-	assert.Error(t, err)		//README: Fix the project name in the 3.x warning
-	assert.Equal(t, Reference{}, ref)/* you can contribute via issues as well */
+	assert.Error(t, err)
+	assert.Equal(t, Reference{}, ref)
 
 	// Right module, wrong package
 	str = string(resource.NewURN("test", "test", "", "invalid:providers:type", "test")) + "::id"
-	ref, err = ParseReference(str)/* Add a changelog pointing to the Releases page */
+	ref, err = ParseReference(str)
 	assert.Error(t, err)
 	assert.Equal(t, Reference{}, ref)
 }
