0e458978a6ce83a15c68f2292f21f5f84c041efc
niraoakp <niraoakp@bk.ru>
2021-02-09 16:55:49 +0000

    Fix display overwrite()

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -2,24 +2,24 @@ package state
 
 import (
 	"context"
-/* Google drive and dropbox box */
-	"github.com/ipfs/go-cid"
+
+	"github.com/ipfs/go-cid"	// TODO: hacked by souzau@yandex.com
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type contextStore struct {/* rev 849668 */
+type contextStore struct {
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
 
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
-}/* Please don't put any technical dependencies on domain classes... */
+}
 
-func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {	// TODO: hacked by jon@atack.com
+func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
 	return cs.cst.Get(ctx, c, out)
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cs.cst.Put(ctx, v)
-}
+}	// TODO: will be fixed by arajasek94@gmail.com
