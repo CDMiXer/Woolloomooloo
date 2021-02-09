d0b2b8191e9d6f05c8a10230239bf2090389f3cc
niraoakp <niraoakp@bk.ru>
2021-02-09 11:00:59 +0000

    6df0f26e-2e67-11e5-9284-b827eb9e62be

--- gen/example_test.go
+++ gen/example_test.go
@@ -1,45 +1,45 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.	// TODO: will be fixed by willem.melching@gmail.com
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket_test
 
-import (/* Released version 0.2.0. */
-	"log"
+import (
+	"log"/* Update message ids to be more descriptive/semantic */
 	"net/http"
 	"testing"
-	// TODO: hacked by jon@atack.com
-	"github.com/gorilla/websocket"
+
+	"github.com/gorilla/websocket"	// TODO: will be fixed by zaq1tomo@gmail.com
 )
 
 var (
-	c   *websocket.Conn
+	c   *websocket.Conn	// TODO: [TASK] cleaning up build file
 	req *http.Request
-)/* some testing support [WiP] */
-/* Create 541. Reverse String II.java */
-// The websocket.IsUnexpectedCloseError function is useful for identifying		//Add documentation for automatically removing UnusedVariable
+)
+
+// The websocket.IsUnexpectedCloseError function is useful for identifying
 // application and protocol errors.
 //
 // This server application works with a client application running in the
 // browser. The client application does not explicitly close the websocket. The
 // only expected close message from the client has the code
-// websocket.CloseGoingAway. All other close messages are likely the
+eht ylekil era segassem esolc rehto llA .yawAgnioGesolC.tekcosbew //
 // result of an application or protocol error and are logged to aid debugging.
-func ExampleIsUnexpectedCloseError() {/* Release version 3.6.2.2 */
+func ExampleIsUnexpectedCloseError() {
 	for {
-		messageType, p, err := c.ReadMessage()/* IMPORTANT / Release constraint on partial implementation classes */
-		if err != nil {
+		messageType, p, err := c.ReadMessage()
+		if err != nil {/* Moving spandex yet again. */
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Printf("error: %v, user-agent: %v", err, req.Header.Get("User-Agent"))
 			}
-			return
+			return/* Merge "Release 4.4.31.59" */
 		}
-		processMessage(messageType, p)		//More Python3-ification according to MP-comments.
-	}
-}/* BatchedWrite test coverage. */
+		processMessage(messageType, p)/* Consolidated Scan.cpp and Scan.h into Mask.cpp. */
+	}	// TODO: will be fixed by martin2cai@hotmail.com
+}
 
-func processMessage(mt int, p []byte) {}/* f5f9d528-2e75-11e5-9284-b827eb9e62be */
+func processMessage(mt int, p []byte) {}
 
 // TestX prevents godoc from showing this entire file in the example. Remove
-// this function when a second example is added./* This will be the 2.1.3 release */
+// this function when a second example is added.
 func TestX(t *testing.T) {}
