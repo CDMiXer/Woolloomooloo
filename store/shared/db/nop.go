ccf4abee203ab227a43fa4379988d50ed4b8a1e3
niraoakp <niraoakp@bk.ru>
2021-02-09 02:27:48 +0000

    Add a wildcard command permission

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: will be fixed by peterke@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Delete boxplotScript.js
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by lexy8russo@outlook.com
+// See the License for the specific language governing permissions and/* Release version [10.6.4] - alfter build */
 // limitations under the License.
-	// use simpler form for requiring spec_helper
-package db
 
-type nopLocker struct{}	// TODO: #30 maintains compatibility to symfony/console 2.0*
-		//Merge "Implements field validation for complex query functionality"
-func (nopLocker) Lock()    {}	// TODO: corrects spelling of symfony
-func (nopLocker) Unlock()  {}
+package db
+	// TODO: Update auditing-with-nservicebus.md
+type nopLocker struct{}
+
+func (nopLocker) Lock()    {}
+func (nopLocker) Unlock()  {}/* Merge "Remove old/unsupported options from run_tests help message." */
 func (nopLocker) RLock()   {}
-func (nopLocker) RUnlock() {}/* Release DBFlute-1.1.0-RC5 */
+func (nopLocker) RUnlock() {}
