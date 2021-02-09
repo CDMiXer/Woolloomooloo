ce34d3723cb7636dfdf7d560d9658aebcada585d
niraoakp <niraoakp@bk.ru>
2021-02-09 09:38:16 +0000

    some potential bugs from findbugs (veqryn)

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release v4.0 */
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("doomed", 4);		//swt.xygraph: fix an annotation bug that freezes xygraph
+const a = new Resource("doomed", 4);
 
 // "a" should still be in the checkpoint with its new value.
