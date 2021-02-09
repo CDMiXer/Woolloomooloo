6e3149f2ec932cf6fa19ec036e21c674e55e3b4f
niraoakp <niraoakp@bk.ru>
2021-02-09 18:29:46 +0000

    adjust coding format

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Updated text files to match LRE-1.2.1
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* LGPLv3 => LGPLv3 + ASLv2 */
-package events	// TODO: Update to new CHANGELOG format as per http://keepachangelog.com/.
+
+package events
 
 import (
 	"io/ioutil"
-	// TODO: Rename pyMating example to pyChooser
-	"github.com/sirupsen/logrus"	// TODO: 285cd978-2e6e-11e5-9284-b827eb9e62be
-)/* Enable the Layout/SpaceInsideParens cop */
+
+	"github.com/sirupsen/logrus"
+)
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
