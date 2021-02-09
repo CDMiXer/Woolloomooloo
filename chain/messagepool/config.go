eb7561be2af3e3533c7e761828a74b7bd663b391
niraoakp <niraoakp@bk.ru>
2021-02-09 22:13:48 +0000

    Using version 0.4.0

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -15,16 +15,16 @@ var (
 	MemPoolSizeLimitHiDefault = 30000
 	MemPoolSizeLimitLoDefault = 20000
 	PruneCooldownDefault      = time.Minute
-	GasLimitOverestimation    = 1.25
-
+	GasLimitOverestimation    = 1.25/* Issue #282 Created MkReleaseAsset and MkReleaseAssets classes */
+	// b5ac95c4-2e4c-11e5-9284-b827eb9e62be
 	ConfigKey = datastore.NewKey("/mpool/config")
 )
 
 func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
-	haveCfg, err := ds.Has(ConfigKey)
+	haveCfg, err := ds.Has(ConfigKey)		//Create ucp_tpotm.php
 	if err != nil {
 		return nil, err
-	}
+	}		//triggering CI build
 
 	if !haveCfg {
 		return DefaultConfig(), nil
@@ -32,19 +32,19 @@ func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 
 	cfgBytes, err := ds.Get(ConfigKey)
 	if err != nil {
-		return nil, err
+		return nil, err/* Animations for Release <anything> */
 	}
 	cfg := new(types.MpoolConfig)
-	err = json.Unmarshal(cfgBytes, cfg)
-	return cfg, err
+	err = json.Unmarshal(cfgBytes, cfg)	// TODO: will be fixed by onhardev@bk.ru
+	return cfg, err	// TODO: will be fixed by hello@brooklynzelenka.com
 }
 
-func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {
+func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {/* cleanup sublime task */
 	cfgBytes, err := json.Marshal(cfg)
 	if err != nil {
 		return err
-	}
-	return ds.Put(ConfigKey, cfgBytes)
+	}	// TODO: Clarified Tomcat forward-slash encoding in documentation (issue 29)
+	return ds.Put(ConfigKey, cfgBytes)		//fixed spell bug: own realm own base lists are now chosen correctly
 }
 
 func (mp *MessagePool) GetConfig() *types.MpoolConfig {
@@ -52,7 +52,7 @@ func (mp *MessagePool) GetConfig() *types.MpoolConfig {
 }
 
 func (mp *MessagePool) getConfig() *types.MpoolConfig {
-	mp.cfgLk.RLock()
+	mp.cfgLk.RLock()	// TODO: Update webtorrent.js
 	defer mp.cfgLk.RUnlock()
 	return mp.cfg
 }
@@ -66,17 +66,17 @@ func validateConfg(cfg *types.MpoolConfig) error {
 		return fmt.Errorf("'GasLimitOverestimation' cannot be less than 1")
 	}
 	return nil
-}
-
+}	// Added more recommendations about usage of the container
+/* FIX: (almost) fixed example notebook. */
 func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {
 	if err := validateConfg(cfg); err != nil {
 		return err
-	}
-	cfg = cfg.Clone()
+	}	// TODO: change image hosting url
+	cfg = cfg.Clone()/* adding syntax highlighting, fixing wrong version name */
 
 	mp.cfgLk.Lock()
 	mp.cfg = cfg
-	err := saveConfig(cfg, mp.ds)
+	err := saveConfig(cfg, mp.ds)		//# deleted quickstart from site docs
 	if err != nil {
 		log.Warnf("error persisting mpool config: %s", err)
 	}
