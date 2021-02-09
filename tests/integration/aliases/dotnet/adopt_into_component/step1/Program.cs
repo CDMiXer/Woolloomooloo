dc2c11a587902efbec3af4c24479d04288fb5f59
niraoakp <niraoakp@bk.ru>
2021-02-09 21:11:21 +0000

    Create combination-sum-iv.cpp

--- tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+		//Fix oxTrust API RS client inum
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource
+class Resource : ComponentResource/* Released 1.5.3. */
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }
+    }/* Create example_discurses.yaml */
 }
-
+/* Create jsAimGrp.py */
 // Scenario #2 - adopt a resource into a component
 class Component : ComponentResource
 {
@@ -23,28 +23,28 @@ class Component : ComponentResource
 // Scenario 3: adopt this resource into a new parent.
 class Component2 : ComponentResource
 {
-    public Component2(string name, ComponentResourceOptions options = null) 
+    public Component2(string name, ComponentResourceOptions options = null) /* Use linux line endings for kscript launcher */
         : base("my:module:Component2", name, options)
     {        
-    }
+    }	// Update zadanie7.c
 }
-
+	// TODO: return posData
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-// in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent.
+// in the next step to be parented by this.  Make sure that works with an opts with no parent	// 7114a482-2e70-11e5-9284-b827eb9e62be
+// versus an opts with a parent.	// TODO: Delete YieldCurveConvention1.png
 
 class Component3 : ComponentResource
-{
+{/* Remove all references to ‘MiniMock’ library. */
     public Component3(string name, ComponentResourceOptions options = null) 
-        : base("my:module:Component3", name, options)
+        : base("my:module:Component3", name, options)	// TODO: Merge branch 'master' of https://github.com/ch4mpy/hadoop2.git
     {        
-        new Component2(name + "-child", options);
-    }
+        new Component2(name + "-child", options);/* Updated openssl version requirement */
+    }/* Release v3.7.1 */
 }
 
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 : ComponentResource
-{
+{	// TODO: will be fixed by nicksavers@gmail.com
     public Component4(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component4", name, options)
     {        
@@ -52,7 +52,7 @@ class Component4 : ComponentResource
 }
 
 
-class Program
+class Program/* Release of eeacms/www:20.10.27 */
 {
     static Task<int> Main(string[] args)
     {
