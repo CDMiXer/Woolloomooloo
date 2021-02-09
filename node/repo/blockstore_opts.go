f74060744d6b317ca3d9074df12ae02562976df1
niraoakp <niraoakp@bk.ru>
2021-02-09 02:13:41 +0000

    prepare for build

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,7 +1,7 @@
 package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
-
+		//Add creator and created at to overview page
 // BadgerBlockstoreOptions returns the badger options to apply for the provided
 // domain.
 func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
@@ -9,38 +9,38 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
-	// in order to shorten keys, but it'll require a migration.
+.noitargim a eriuqer ll'ti tub ,syek netrohs ot redro ni //	
 	opts.Prefix = "/blocks/"
-
-	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge.
+/* add screenshot when claimng page */
+	// Blockstore values are immutable; therefore we do not expect any	// fs.writeFile api update
+	// conflicts to emerge.	// TODO: will be fixed by zodiacon@live.com
 	opts.DetectConflicts = false
 
 	// This is to optimize the database on close so it can be opened
-	// read-only and efficiently queried.
+	// read-only and efficiently queried.	// TODO: hacked by arachnid@notdot.net
 	opts.CompactL0OnClose = true
 
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
-	// persist anyways.
+	// persist anyways.		//Merge "NSXv: LBaaS default FW rule should be accept-any"
 	opts.Truncate = true
 
 	// We mmap the index and the value logs; this is important to enable
-	// zero-copy value access.
+	// zero-copy value access./* added kaminari */
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
-
+		//lib/rrcache: handle qname/cname traversal when it fails
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
 	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
-
+/* Update 476.md */
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
 	// deleted). This will change if we move to a tiered blockstore.
-
+/* real images */
 	opts.ReadOnly = readonly
 
-	return opts, nil
+	return opts, nil		//Merge "ASoc: msm: Add changes to support multiple meta key value pairs"
 }
