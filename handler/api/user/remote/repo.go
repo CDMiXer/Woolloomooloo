9415a65a0aa0537f87bb1f70140f11ac6dde30b8
niraoakp <niraoakp@bk.ru>
2021-02-09 14:39:50 +0000

    [FIX] WebDAV: ignore authorization if default credentials are given

--- handler/api/user/remote/repo.go
+++ handler/api/user/remote/repo.go
@@ -8,25 +8,25 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release FPCM 3.1.0 */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Fix function declaration
 
-package remote
-
+package remote/* SocketServer erop gezet, voor onderlinge verbindingen */
+	// TODO: Merge "Merge all shapes/paths caches to PathCache" into jb-mr2-dev
 import (
-	"net/http"
+	"net/http"/* Release Notes for v02-13 */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"		//Fixed dictionary interaction with digenpy
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/go-chi/chi"
-)
+)/* Add Redirect processor. */
 
-// HandleRepo returns an http.HandlerFunc that writes a json-encoded
+// HandleRepo returns an http.HandlerFunc that writes a json-encoded/* Merge "Correct parameter order for assertEqual() method" */
 // repository to the response body.
 func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -46,15 +46,15 @@ func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 			return
 		}
 
-		perms, err := repos.FindPerm(r.Context(), viewer, slug)
-		if err != nil {
+		perms, err := repos.FindPerm(r.Context(), viewer, slug)		//Update 02_QuickTour.md
+		if err != nil {	// 4cb5cb5e-2e73-11e5-9284-b827eb9e62be
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository permissions")
 		} else {
 			repo.Perms = perms
 		}
-
+		//75653d02-2e3f-11e5-9284-b827eb9e62be
 		render.JSON(w, repo, 200)
 	}
-}
+}/* Release 1.1.2 with updated dependencies */
