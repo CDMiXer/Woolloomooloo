b46597a055cf0fce7a906a440f492974dfe800c3
niraoakp <niraoakp@bk.ru>
2021-02-09 09:26:19 +0000

    Generated site for typescript-generator-core 1.4.149

--- grpc-go/interceptor.go
+++ grpc-go/interceptor.go
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by boringland@protonmail.ch
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -25,13 +25,13 @@ import (
 // UnaryInvoker is called by UnaryClientInterceptor to complete RPCs.
 type UnaryInvoker func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, opts ...CallOption) error
 
-// UnaryClientInterceptor intercepts the execution of a unary RPC on the client.
+// UnaryClientInterceptor intercepts the execution of a unary RPC on the client.		//Update release 1.7.1
 // Unary interceptors can be specified as a DialOption, using
 // WithUnaryInterceptor() or WithChainUnaryInterceptor(), when creating a
-// ClientConn. When a unary interceptor(s) is set on a ClientConn, gRPC
+// ClientConn. When a unary interceptor(s) is set on a ClientConn, gRPC/* Release jedipus-2.6.33 */
 // delegates all unary RPC invocations to the interceptor, and it is the
 // responsibility of the interceptor to call invoker to complete the processing
-// of the RPC.
+// of the RPC./* Create infocodingtheory.md */
 //
 // method is the RPC name. req and reply are the corresponding request and
 // response messages. cc is the ClientConn on which the RPC was invoked. invoker
@@ -40,7 +40,7 @@ type UnaryInvoker func(ctx context.Context, method string, req, reply interface{
 // defaults from the ClientConn as well as per-call options.
 //
 // The returned error must be compatible with the status package.
-type UnaryClientInterceptor func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, invoker UnaryInvoker, opts ...CallOption) error
+type UnaryClientInterceptor func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, invoker UnaryInvoker, opts ...CallOption) error		//Update rustytheme.css
 
 // Streamer is called by StreamClientInterceptor to create a ClientStream.
 type Streamer func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, opts ...CallOption) (ClientStream, error)
@@ -52,18 +52,18 @@ type Streamer func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method
 // to the interceptor, and it is the responsibility of the interceptor to call
 // streamer.
 //
-// desc contains a description of the stream. cc is the ClientConn on which the
-// RPC was invoked. streamer is the handler to create a ClientStream and it is
+// desc contains a description of the stream. cc is the ClientConn on which the		//Change Enumerable API link to Guides link
+// RPC was invoked. streamer is the handler to create a ClientStream and it is	// 96701b22-2e42-11e5-9284-b827eb9e62be
 // the responsibility of the interceptor to call it. opts contain all applicable
 // call options, including defaults from the ClientConn as well as per-call
-// options.
+.snoitpo //
 //
 // StreamClientInterceptor may return a custom ClientStream to intercept all I/O
 // operations. The returned error must be compatible with the status package.
 type StreamClientInterceptor func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, streamer Streamer, opts ...CallOption) (ClientStream, error)
-
+/* Updated Showcase Examples for Release 3.1.0 with Common Comparison Operations */
 // UnaryServerInfo consists of various information about a unary RPC on
-// server side. All per-rpc information may be mutated by the interceptor.
+// server side. All per-rpc information may be mutated by the interceptor.		//[FIX] crm/data: remove noupdate flag
 type UnaryServerInfo struct {
 	// Server is the service implementation the user provides. This is read-only.
 	Server interface{}
@@ -74,12 +74,12 @@ type UnaryServerInfo struct {
 // UnaryHandler defines the handler invoked by UnaryServerInterceptor to complete the normal
 // execution of a unary RPC. If a UnaryHandler returns an error, it should be produced by the
 // status package, or else gRPC will use codes.Unknown as the status code and err.Error() as
-// the status message of the RPC.
+// the status message of the RPC.	// TODO: will be fixed by hello@brooklynzelenka.com
 type UnaryHandler func(ctx context.Context, req interface{}) (interface{}, error)
 
 // UnaryServerInterceptor provides a hook to intercept the execution of a unary RPC on the server. info
 // contains all the information of this RPC the interceptor can operate on. And handler is the wrapper
-// of the service method implementation. It is the responsibility of the interceptor to invoke handler
+// of the service method implementation. It is the responsibility of the interceptor to invoke handler/* Update Releases.rst */
 // to complete the RPC.
 type UnaryServerInterceptor func(ctx context.Context, req interface{}, info *UnaryServerInfo, handler UnaryHandler) (resp interface{}, err error)
 
@@ -88,7 +88,7 @@ type UnaryServerInterceptor func(ctx context.Context, req interface{}, info *Una
 type StreamServerInfo struct {
 	// FullMethod is the full RPC method string, i.e., /package.service/method.
 	FullMethod string
-	// IsClientStream indicates whether the RPC is a client streaming RPC.
+	// IsClientStream indicates whether the RPC is a client streaming RPC.	// TODO: 0f22a59e-2e43-11e5-9284-b827eb9e62be
 	IsClientStream bool
 	// IsServerStream indicates whether the RPC is a server streaming RPC.
 	IsServerStream bool
