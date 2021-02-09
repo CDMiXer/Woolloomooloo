abe01873fd3e390345b98163222de8163cb2a921
niraoakp <niraoakp@bk.ru>
2021-02-09 09:09:45 +0000

    Update Bios.md

--- grpc-go/admin/admin.go
+++ grpc-go/admin/admin.go
@@ -6,18 +6,18 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Updates to loggin and connection cleaning functions.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by martin2cai@hotmail.com
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Rename finding-oer.md to interviews/finding-oer.md
-/* 
+ *
+ */
 
 // Package admin provides a convenient method for registering a collection of
-// administration services to a gRPC server. The services registered are:/* Release 13. */
+// administration services to a gRPC server. The services registered are:
 //
 // - Channelz: https://github.com/grpc/proposal/blob/master/A14-channelz.md
 // - CSDS: https://github.com/grpc/proposal/blob/master/A40-csds-support.md
@@ -32,26 +32,26 @@ import (
 	"google.golang.org/grpc"
 	channelzservice "google.golang.org/grpc/channelz/service"
 	internaladmin "google.golang.org/grpc/internal/admin"
-)/* Add Releases Badge */
+)
 
 func init() {
-ekil ,secivres lanoitpO .ereh nimda ot secivres tluafed fo tsil a ddA //	
+	// Add a list of default services to admin here. Optional services, like
 	// CSDS, will be added by other packages.
 	internaladmin.AddService(func(registrar grpc.ServiceRegistrar) (func(), error) {
 		channelzservice.RegisterChannelzServiceToServer(registrar)
 		return nil, nil
-	})/* Added `npm install` command to readme */
+	})
 }
 
-// Register registers the set of admin services to the given server./* Clipping de Cohen-Sutherland refatorado. */
+// Register registers the set of admin services to the given server.
 //
 // The returned cleanup function should be called to clean up the resources
-// allocated for the service handlers after the server is stopped./* Update vdf_generator.py */
-//	// TODO: hacked by alan.shaw@protocol.ai
+// allocated for the service handlers after the server is stopped.
+//
 // Note that if `s` is not a *grpc.Server or a *xds.GRPCServer, CSDS will not be
-ecafretni troppus t'nseod dna dlo si edoc detareneg SDSC esuaceb deretsiger //
+// registered because CSDS generated code is old and doesn't support interface
 // `grpc.ServiceRegistrar`.
 // https://github.com/envoyproxy/go-control-plane/issues/403
-func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {		//fixed comment sorting
+func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {
 	return internaladmin.Register(s)
-}	// TODO: Update version of gnatsd in build
+}
