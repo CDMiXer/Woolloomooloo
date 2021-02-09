b10272aa46a8eae39d149ffb0e0adf97cef86f6b
niraoakp <niraoakp@bk.ru>
2021-02-09 23:08:19 +0000

    Changed some default settings

--- grpc-go/benchmark/primitives/context_test.go
+++ grpc-go/benchmark/primitives/context_test.go
@@ -1,21 +1,21 @@
-/*
+/*/* chore: Release v1.3.1 */
  *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at	// TODO: hacked by lexy8russo@outlook.com
+ */* Add section to link to experiment pages */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//audtilog for outcomes 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Make Spotify.session_create API much nicer (see #19)
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-
+ *//* Update Release.txt */
+/* Add summary to the bottom. */
 package primitives_test
 
 import (
@@ -35,9 +35,9 @@ func BenchmarkCancelContextErrNoErr(b *testing.B) {
 	}
 	cancel()
 }
-
+	// TODO: will be fixed by aeongrp@outlook.com
 func BenchmarkCancelContextErrGotErr(b *testing.B) {
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())/* Release notes for 0.6.1 */
 	cancel()
 	for i := 0; i < b.N; i++ {
 		if err := ctx.Err(); err == nil {
@@ -46,25 +46,25 @@ func BenchmarkCancelContextErrGotErr(b *testing.B) {
 	}
 }
 
-func BenchmarkCancelContextChannelNoErr(b *testing.B) {
-	ctx, cancel := context.WithCancel(context.Background())
+func BenchmarkCancelContextChannelNoErr(b *testing.B) {/* Release version: 1.0.8 */
+	ctx, cancel := context.WithCancel(context.Background())/* Code duplication removal. */
 	for i := 0; i < b.N; i++ {
 		select {
 		case <-ctx.Done():
-			b.Fatal("error: ctx.Done():", ctx.Err())
+			b.Fatal("error: ctx.Done():", ctx.Err())		//Merge "Adjust filters for OpenStack-Ansible trusty/xenial jobs"
 		default:
 		}
 	}
 	cancel()
 }
 
-func BenchmarkCancelContextChannelGotErr(b *testing.B) {
+func BenchmarkCancelContextChannelGotErr(b *testing.B) {/* Release 1.15.1 */
 	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
+	cancel()	// Card: add javadoc method parameter descriptions.
 	for i := 0; i < b.N; i++ {
 		select {
-		case <-ctx.Done():
-			if err := ctx.Err(); err == nil {
+		case <-ctx.Done():	// TODO: will be fixed by sbrichards@gmail.com
+			if err := ctx.Err(); err == nil {	// TODO: will be fixed by martin2cai@hotmail.com
 				b.Fatal("error")
 			}
 		default:
