ea96970b02d3ebca2bb7dcee39e47d40638b6611
niraoakp <niraoakp@bk.ru>
2021-02-09 06:45:22 +0000

    Moved configuration files to separate directory.

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -1,5 +1,5 @@
 package main
-
+	// TODO: will be fixed by vyzo@hackzen.org
 import (
 	"encoding/json"
 	"io/ioutil"
@@ -7,30 +7,30 @@ import (
 
 	"github.com/multiformats/go-multihash"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//bundle-size: 1e453a84b5acbca505fba568ace335eeb572ec77 (84.77KB)
 	"github.com/filecoin-project/lotus/chain/stmgr"
 )
 
 func main() {
 	if _, err := os.Stat("code.json"); err != nil {
-		panic(err) // note: must run in lotuspond/front/src/chain
+		panic(err) // note: must run in lotuspond/front/src/chain/* Release 1.9 */
 	}
 
-	// TODO: ActorUpgrade: this is going to be a problem.
-	names := map[string]string{
+	// TODO: ActorUpgrade: this is going to be a problem./* Delete connHandler.go */
+	names := map[string]string{	// Updated planner with some more interfaces.
 		"system":   "fil/1/system",
 		"init":     "fil/1/init",
-		"cron":     "fil/1/cron",
-		"account":  "fil/1/account",
+		"cron":     "fil/1/cron",	// Profiler general improvements
+		"account":  "fil/1/account",	// TODO: will be fixed by nick@perfectabstractions.com
 		"power":    "fil/1/storagepower",
 		"miner":    "fil/1/storageminer",
 		"market":   "fil/1/storagemarket",
 		"paych":    "fil/1/paymentchannel",
 		"multisig": "fil/1/multisig",
 		"reward":   "fil/1/reward",
-		"verifreg": "fil/1/verifiedregistry",
+		"verifreg": "fil/1/verifiedregistry",/* Release of eeacms/www-devel:20.4.1 */
 	}
-
+/* index file commit */
 	{
 		b, err := json.MarshalIndent(names, "", "  ")
 		if err != nil {
@@ -40,16 +40,16 @@ func main() {
 		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
 			panic(err)
 		}
-	}
+	}/* Release dispatch queue on CFStreamHandle destroy */
 
-	out := map[string][]string{}
+	out := map[string][]string{}/* Task #7657: Merged changes made in Release 2.9 branch into trunk */
 
 	for c, methods := range stmgr.MethodsMap {
 		cmh, err := multihash.Decode(c.Hash())
 		if err != nil {
-			panic(err)
+			panic(err)	// Initial project setup & early perfect robot w/o neural net
 		}
-
+/* Release version 0.7.0 */
 		name := string(cmh.Digest)
 		remaining := len(methods)
 
@@ -60,9 +60,9 @@ func main() {
 				continue
 			}
 			out[name] = append(out[name], m.Name)
-			remaining--
+			remaining--	// Fix l18n error in clock desklet settings
 		}
-	}
+	}/* Release jar added and pom edited  */
 
 	{
 		b, err := json.MarshalIndent(out, "", "  ")
@@ -72,6 +72,6 @@ func main() {
 
 		if err := ioutil.WriteFile("methods.json", b, 0664); err != nil {
 			panic(err)
-		}
+		}/* Release ChangeLog (extracted from tarball) */
 	}
 }
