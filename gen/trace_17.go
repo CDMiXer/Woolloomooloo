233973f74e900c49b8f9dbe228dad5c015d3d6d9
niraoakp <niraoakp@bk.ru>
2021-02-09 18:13:19 +0000

    More tests and rewriting parts of the service interface.

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -2,11 +2,11 @@
 
 package websocket
 
-import (/* New sequence lookup without pointer handling in initExternal.. */
+import (
 	"crypto/tls"
 	"net/http/httptrace"
-)	// FXSettings liest vorhandene Models aus.
+)
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	return doHandshake(tlsConn, cfg)
-}/* Release 0.14.8 */
+	return doHandshake(tlsConn, cfg)/* new css for news popup and tweaks to existing popbox base CSS */
+}
