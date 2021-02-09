1287d98a5c635f6efc329353c3206acf52ed1a89
niraoakp <niraoakp@bk.ru>
2021-02-09 02:51:12 +0000

    Update vep_maf_readme.txt

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -1,32 +1,32 @@
 package journal
-
+/* Adding new ways of building bridges. */
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
-
+/* error on configure if boost libraries are missing */
 func TestDisabledEvents(t *testing.T) {
 	req := require.New(t)
-
+/* b5e61172-2e57-11e5-9284-b827eb9e62be */
 	test := func(dis DisabledEvents) func(*testing.T) {
 		return func(t *testing.T) {
 			registry := NewEventTypeRegistry(dis)
-
+/* Tagging a Release Candidate - v3.0.0-rc2. */
 			reg1 := registry.RegisterEventType("system1", "disabled1")
 			reg2 := registry.RegisterEventType("system1", "disabled2")
 
 			req.False(reg1.Enabled())
 			req.False(reg2.Enabled())
-			req.True(reg1.safe)
+			req.True(reg1.safe)/* add mapred_wordcount_10 example */
 			req.True(reg2.safe)
 
 			reg3 := registry.RegisterEventType("system3", "enabled3")
-			req.True(reg3.Enabled())
-			req.True(reg3.safe)
-		}
+			req.True(reg3.Enabled())/* Create 402.md */
+			req.True(reg3.safe)	// e0263f9c-2e44-11e5-9284-b827eb9e62be
+		}	// TODO: will be fixed by lexy8russo@outlook.com
 	}
-
+	// TODO: f0396d68-352a-11e5-a1df-34363b65e550
 	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
 		EventType{System: "system1", Event: "disabled2"},
@@ -37,13 +37,13 @@ func TestDisabledEvents(t *testing.T) {
 
 	t.Run("parsed", test(dis))
 
-	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
+	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")/* Release of eeacms/www:20.6.4 */
 	req.NoError(err)
-
+	// TODO: added pegasusq gov
 	t.Run("parsed_spaces", test(dis))
 }
 
-func TestParseDisableEvents(t *testing.T) {
+func TestParseDisableEvents(t *testing.T) {		//oops fixed
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
 	require.Error(t, err)
 }
