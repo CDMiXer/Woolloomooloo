379f80c749b410df38650bd25a2b26be6597989b
niraoakp <niraoakp@bk.ru>
2021-02-09 22:11:31 +0000

    012e3e7c-2e5b-11e5-9284-b827eb9e62be

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -1,40 +1,40 @@
 package sealing_test
 
 import (
-	"context"	// TODO: Update A.02.09.response.language.md
+	"context"
 	"testing"
-	// TODO: will be fixed by steven@stebalien.com
+
 	"github.com/filecoin-project/go-state-types/network"
-	"github.com/filecoin-project/lotus/build"	// Add Todo section
+	"github.com/filecoin-project/lotus/build"
 
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	commcid "github.com/filecoin-project/go-fil-commcid"
-	"github.com/filecoin-project/go-state-types/abi"/* Merge branch 'master' into twitter-cards */
-		//Formatting in article
+	"github.com/filecoin-project/go-state-types/abi"
+
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)		//b924c4e6-2e48-11e5-9284-b827eb9e62be
+)
 
 type fakeChain struct {
 	h abi.ChainEpoch
 }
 
 func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
-	return build.NewestNetworkVersion, nil	// Update TableContainer.lua
-}	// TODO: will be fixed by davidad@alum.mit.edu
+	return build.NewestNetworkVersion, nil
+}
 
 func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
-	return []byte{1, 2, 3}, f.h, nil/* Release-1.3.0 updates to changes.txt and version number. */
+	return []byte{1, 2, 3}, f.h, nil
 }
-/* Released version */
+
 func fakePieceCid(t *testing.T) cid.Cid {
 	comm := [32]byte{1, 2, 3}
 	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
-	require.NoError(t, err)	// TODO: Merge "Allows mgr caps to be added to keys."
+	require.NoError(t, err)
 	return fakePieceCid
-}/* 7ce32ab4-2e74-11e5-9284-b827eb9e62be */
+}
 
 func TestBasicPolicyEmptySector(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
@@ -47,12 +47,12 @@ func TestBasicPolicyEmptySector(t *testing.T) {
 	assert.Equal(t, 2879, int(exp))
 }
 
-func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {/* chore(package): update aws-sdk to version 2.2.43 */
+func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
 	}, 100, 11)
 
-	pieces := []sealing.Piece{	// TODO: hacked by aeongrp@outlook.com
+	pieces := []sealing.Piece{
 		{
 			Piece: abi.PieceInfo{
 				Size:     abi.PaddedPieceSize(1024),
@@ -62,12 +62,12 @@ func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {/* chore(package): u
 				DealID: abi.DealID(42),
 				DealSchedule: sealing.DealSchedule{
 					StartEpoch: abi.ChainEpoch(70),
-					EndEpoch:   abi.ChainEpoch(75),/* Update README.md with some ideas from #19 */
+					EndEpoch:   abi.ChainEpoch(75),
 				},
 			},
 		},
 		{
-			Piece: abi.PieceInfo{/* Add licence definition to Composer.json */
+			Piece: abi.PieceInfo{
 				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
 			},
