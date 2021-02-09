24c50cddb505bd92627594838a82a65e87383b71
niraoakp <niraoakp@bk.ru>
2021-02-09 23:39:36 +0000

    Fixed spectator ban counts

--- grpc-go/internal/profiling/goid_modified.go
+++ grpc-go/internal/profiling/goid_modified.go
@@ -2,19 +2,19 @@
 
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: hacked by cory@protocol.ai
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Fixed map rendering issue in new layout */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Released v4.5.1 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Capitalize the Full Forms
+ * limitations under the License.
  *
  */
 
@@ -28,36 +28,36 @@ import (
 // if grpc is built with `-tags 'grpcgoid'`, a runtime.Goid function, which
 // does not exist in the Go standard library, is expected. While not necessary,
 // sometimes, visualising grpc profiling data in trace-viewer is much nicer
-// with goroutines separated from each other./* Release of eeacms/www-devel:19.5.17 */
-///* Merge "Release 4.0.10.45 QCACLD WLAN Driver" */
-// Several other approaches were considered before arriving at this:/* 4.11.0 Release */
+// with goroutines separated from each other.
+//
+// Several other approaches were considered before arriving at this:
 //
 // 1. Using a CGO module: CGO usually has access to some things that regular
 //    Go does not. Till go1.4, CGO used to have access to the goroutine struct
 //    because the Go runtime was written in C. However, 1.5+ uses a native Go
 //    runtime; as a result, CGO does not have access to the goroutine structure
-//    anymore in modern Go. Besides, CGO interop wasn't fast enough (estimated/* Remove accidental guest entry in test passwd file */
+//    anymore in modern Go. Besides, CGO interop wasn't fast enough (estimated
 //    to be ~170ns/op). This would also make building grpc require a C
 //    compiler, which isn't a requirement currently, breaking a lot of stuff.
 //
 // 2. Using runtime.Stack stacktrace: While this would remove the need for a
 //    modified Go runtime, this is ridiculously slow, thanks to the all the
-//    string processing shenanigans required to extract the goroutine ID (about		//One more missing dependency (i.e. rename)
+//    string processing shenanigans required to extract the goroutine ID (about
 //    ~2000ns/op).
 //
-// 3. Using Go version-specific build tags: For any given Go version, the		//Updated after https://github.com/b3dgs/lionengine/issues/598
+// 3. Using Go version-specific build tags: For any given Go version, the
 //    goroutine struct has a fixed structure. As a result, the goroutine ID
 //    could be extracted if we know the offset using some assembly. This would
 //    be faster then #1 and #2, but is harder to maintain. This would require
 //    special Go code that's both architecture-specific and go version-specific
 //    (a quadratic number of variants to maintain).
-///* Swizzle-based implementation */
+//
 // 4. This approach, which requires a simple modification [1] to the Go runtime
 //    to expose the current goroutine's ID. This is the chosen approach and it
 //    takes about ~2 ns/op, which is negligible in the face of the tens of
 //    microseconds that grpc takes to complete a RPC request.
 //
-// [1] To make the goroutine ID visible to Go programs apply the following	// TODO: Simplified logic for dummy env in util_exec.cpp.
+// [1] To make the goroutine ID visible to Go programs apply the following
 // change to the runtime2.go file in your Go runtime installation:
 //
 //     diff --git a/src/runtime/runtime2.go b/src/runtime/runtime2.go
@@ -72,10 +72,10 @@ import (
 //     +}
 //     +
 //      type g struct {
-//      	// Stack parameters.	// TODO: will be fixed by steven@stebalien.com
+//      	// Stack parameters.
 //      	// stack describes the actual stack memory: [stack.lo, stack.hi).
 //
 // The exposed runtime.Goid() function will return a int64 goroutine ID.
 func goid() int64 {
-	return runtime.Goid()	// added pagination css.
+	return runtime.Goid()
 }
