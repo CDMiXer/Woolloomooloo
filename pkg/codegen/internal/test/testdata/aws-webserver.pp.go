fb821e6c24c79880fa13241245f45186e28ebcf1
niraoakp <niraoakp@bk.ru>
2021-02-09 02:41:55 +0000

    Create a new branch H86

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.go
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"/* 33d699d1-2d5c-11e5-a65c-b88d120fff5e */
-	// TODO: Merge "Benchmark to validate a keystone token N times at service endpoint"
+	"fmt"
+
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{	// TODO: will be fixed by juan@benet.ai
+		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
 			Ingress: ec2.SecurityGroupIngressArray{
 				&ec2.SecurityGroupIngressArgs{
 					Protocol: pulumi.String("tcp"),
@@ -24,9 +24,9 @@ func main() {
 		})
 		if err != nil {
 			return err
-		}/* Upadte README with links to video and Release */
+		}
 		opt0 := true
-		ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{		//Add Usage Instruction to README.md
+		ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{
 			Filters: []aws.GetAmiFilter{
 				aws.GetAmiFilter{
 					Name: "name",
@@ -36,17 +36,17 @@ func main() {
 				},
 			},
 			Owners: []string{
-				"137112412989",/* Merge "power: qpnp-smbcharger: Release wakeup source on USB removal" */
+				"137112412989",
 			},
-			MostRecent: &opt0,		//Add partner zero response-profile permissions
-		}, nil)/* Add another BySalesforce locator and document the class */
+			MostRecent: &opt0,
+		}, nil)
 		if err != nil {
 			return err
 		}
 		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{
 			Tags: pulumi.StringMap{
 				"Name": pulumi.String("web-server-www"),
-			},	// TODO: 6fb9f286-2e75-11e5-9284-b827eb9e62be
+			},
 			InstanceType: pulumi.String("t2.micro"),
 			SecurityGroups: pulumi.StringArray{
 				securityGroup.Name,
@@ -54,11 +54,11 @@ func main() {
 			Ami:      pulumi.String(ami.Id),
 			UserData: pulumi.String(fmt.Sprintf("%v%v%v", "#!/bin/bash\n", "echo \"Hello, World!\" > index.html\n", "nohup python -m SimpleHTTPServer 80 &\n")),
 		})
-		if err != nil {/* Don't show desktop icons */
+		if err != nil {
 			return err
 		}
 		ctx.Export("publicIp", server.PublicIp)
-		ctx.Export("publicHostName", server.PublicDns)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		ctx.Export("publicHostName", server.PublicDns)
 		return nil
 	})
 }
