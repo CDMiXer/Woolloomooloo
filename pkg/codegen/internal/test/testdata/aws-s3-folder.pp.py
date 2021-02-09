b32c4bf1687223183435950a6b3ab1da01b7db96
niraoakp <niraoakp@bk.ru>
2021-02-09 20:17:58 +0000

    Why isn't this loading in HTTPS? cmonBruh

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,33 +1,33 @@
 import pulumi
-import json	// Created leahsterncover.jpg
+import json
 import os
 import pulumi_aws as aws
 
-# Create a bucket and expose a website index document/* JS-Fehler gefixt, der den IE zum Aufgeben brachte */
-site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
+# Create a bucket and expose a website index document
+site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(/* 57c07ad8-2e66-11e5-9284-b827eb9e62be */
     index_document="index.html",
-))/* Released code under the MIT License */
-site_dir = "www"
+))
+site_dir = "www"	// TODO: Delete BOWModel_v_3
 # For each file in the directory, create an S3 object stored in `siteBucket`
-files = []	// TODO: hacked by timnugent@gmail.com
-for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
-    files.append(aws.s3.BucketObject(f"files-{range['key']}",
+files = []
+for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:/* Renaming and deleting terminologies */
+    files.append(aws.s3.BucketObject(f"files-{range['key']}",/* Release v0.4.0.pre */
         bucket=site_bucket.id,
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
         content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
-bucket_policy = aws.s3.BucketPolicy("bucketPolicy",		//Fixed debug macro to accept only format string
+bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
     bucket=site_bucket.id,
-    policy=site_bucket.id.apply(lambda id: json.dumps({		//Add some notes on the puppet setup to README.
+    policy=site_bucket.id.apply(lambda id: json.dumps({/* 8e78247c-2e52-11e5-9284-b827eb9e62be */
         "Version": "2012-10-17",
         "Statement": [{
             "Effect": "Allow",
             "Principal": "*",
-            "Action": ["s3:GetObject"],/* Release the mod to the public domain */
+            "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
-    })))	// No need to schedule in the current run loop
+    })))
 pulumi.export("bucketName", site_bucket.bucket)
-pulumi.export("websiteUrl", site_bucket.website_endpoint)
+pulumi.export("websiteUrl", site_bucket.website_endpoint)	// TODO: Add markers so the tests are readable.
