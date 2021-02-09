fb5ef02d29871bd7c1025c5805d539e1613511f9
niraoakp <niraoakp@bk.ru>
2021-02-09 14:34:59 +0000

    Add latest main to site

--- core/convert.go
+++ core/convert.go
@@ -8,24 +8,24 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
-import "context"
+import "context"/* Update ReleaseNotes-Identity.md */
 
 type (
-	// ConvertArgs represents a request to the pipeline
+	// ConvertArgs represents a request to the pipeline/* Release core 2.6.1 */
 	// conversion service.
 	ConvertArgs struct {
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
-		Config *Config     `json:"config,omitempty"`
+		Config *Config     `json:"config,omitempty"`		//edit disclaimer
 	}
-
+	// TODO: Merge branch 'develop' into feature/strip-bom
 	// ConvertService converts non-native pipeline
 	// configuration formats to native configuration
 	// formats (e.g. jsonnet to yaml).
