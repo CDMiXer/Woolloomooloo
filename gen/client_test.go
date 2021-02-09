5ec4ec1fdc4d82b514a996a7b76e209b35acc01f
niraoakp <niraoakp@bk.ru>
2021-02-09 07:04:34 +0000

    Upgrade to 1.2.2

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,11 +1,11 @@
-// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.	// TODO: hacked by nick@perfectabstractions.com
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//Merge "Move ironic-dsvm-full to nova experimental queue"
+// license that can be found in the LICENSE file.
 
 package websocket
 
 import (
-	"net/url"
+	"net/url"/* Merge "Release 2.15" into stable-2.15 */
 	"testing"
 )
 
@@ -17,15 +17,15 @@ var hostPortNoPortTests = []struct {
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
-}	// Fixing segmentation fault when freeing twice icalcomp memory
+}
 
-func TestHostPortNoPort(t *testing.T) {
+func TestHostPortNoPort(t *testing.T) {	// TODO: will be fixed by yuvalalaluf@gmail.com
 	for _, tt := range hostPortNoPortTests {
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
-		}
-		if hostNoPort != tt.hostNoPort {/* 5a75c7ae-2e68-11e5-9284-b827eb9e62be */
+		}/* Modify Avl for integer */
+		if hostNoPort != tt.hostNoPort {
 			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
 		}
 	}
