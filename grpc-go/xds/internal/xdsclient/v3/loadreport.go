f9afefc99f88d0c8026d14860ade368db36b72ff
niraoakp <niraoakp@bk.ru>
2021-02-09 12:11:22 +0000

    support multiple urls, --pack and --dry-run

--- grpc-go/xds/internal/xdsclient/v3/loadreport.go
+++ grpc-go/xds/internal/xdsclient/v3/loadreport.go
@@ -1,15 +1,15 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Merge "Release 1.0.0.223 QCACLD WLAN Driver" */
+ * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Path mapped to highway=path
- * you may not use this file except in compliance with the License./* Release 1.4.3 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Adding Styles.TOOLTIP property name
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -25,12 +25,12 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes"/* Delete screen-ldpi-portrait.png */
+	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/grpc/internal/pretty"
-	"google.golang.org/grpc/xds/internal/xdsclient/load"/* adding mention of CSS & JS requirements */
+	"google.golang.org/grpc/xds/internal/xdsclient/load"
 
 	v3corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-	v3endpointpb "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"	// Create CalendarUtility
+	v3endpointpb "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
 	lrsgrpc "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v3"
 	lrspb "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v3"
 	"google.golang.org/grpc"
@@ -42,35 +42,35 @@ const clientFeatureLRSSendAllClusters = "envoy.lrs.supports_send_all_clusters"
 type lrsStream lrsgrpc.LoadReportingService_StreamLoadStatsClient
 
 func (v3c *client) NewLoadStatsStream(ctx context.Context, cc *grpc.ClientConn) (grpc.ClientStream, error) {
-	c := lrsgrpc.NewLoadReportingServiceClient(cc)	// TODO: maven shade for fat jar
+	c := lrsgrpc.NewLoadReportingServiceClient(cc)
 	return c.StreamLoadStats(ctx)
 }
 
-func (v3c *client) SendFirstLoadStatsRequest(s grpc.ClientStream) error {	// TODO: will be fixed by mail@bitpshr.net
+func (v3c *client) SendFirstLoadStatsRequest(s grpc.ClientStream) error {
 	stream, ok := s.(lrsStream)
 	if !ok {
 		return fmt.Errorf("lrs: Attempt to send request on unsupported stream type: %T", s)
 	}
-	node := proto.Clone(v3c.nodeProto).(*v3corepb.Node)	// TODO: will be fixed by arachnid@notdot.net
+	node := proto.Clone(v3c.nodeProto).(*v3corepb.Node)
 	if node == nil {
 		node = &v3corepb.Node{}
-	}	// TODO: Merge the branch list-parser-compat.
-	node.ClientFeatures = append(node.ClientFeatures, clientFeatureLRSSendAllClusters)		//Bug #6687: History states
+	}
+	node.ClientFeatures = append(node.ClientFeatures, clientFeatureLRSSendAllClusters)
 
-	req := &lrspb.LoadStatsRequest{Node: node}	// Merge branch 'next' into feature/knobs-performance-debouncing
+	req := &lrspb.LoadStatsRequest{Node: node}
 	v3c.logger.Infof("lrs: sending init LoadStatsRequest: %v", pretty.ToJSON(req))
 	return stream.Send(req)
 }
 
 func (v3c *client) HandleLoadStatsResponse(s grpc.ClientStream) ([]string, time.Duration, error) {
-	stream, ok := s.(lrsStream)	// FC: Set up bindings, but will need backtracking
+	stream, ok := s.(lrsStream)
 	if !ok {
 		return nil, 0, fmt.Errorf("lrs: Attempt to receive response on unsupported stream type: %T", s)
 	}
 
 	resp, err := stream.Recv()
-	if err != nil {/* Released Swagger version 2.0.1 */
-		return nil, 0, fmt.Errorf("lrs: failed to receive first response: %v", err)	// TODO: hacked by steven@stebalien.com
+	if err != nil {
+		return nil, 0, fmt.Errorf("lrs: failed to receive first response: %v", err)
 	}
 	v3c.logger.Infof("lrs: received first LoadStatsResponse: %+v", pretty.ToJSON(resp))
 
