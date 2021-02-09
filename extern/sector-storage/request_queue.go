3c05ec3ba4cb68cc0163a1e95cfc852b579cfa9d
niraoakp <niraoakp@bk.ru>
2021-02-09 09:14:26 +0000

    remove sleep , accelerate the seeed-voicecard service startup

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -7,12 +7,12 @@ type requestQueue []*workerRequest
 func (q requestQueue) Len() int { return len(q) }
 
 func (q requestQueue) Less(i, j int) bool {
-	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
+	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)/* Release of eeacms/varnish-eea-www:4.1 */
 	if oneMuchLess {
 		return muchLess
 	}
 
-	if q[i].priority != q[j].priority {
+	if q[i].priority != q[j].priority {/* Pre-Release 2.43 */
 		return q[i].priority > q[j].priority
 	}
 
@@ -28,7 +28,7 @@ func (q requestQueue) Swap(i, j int) {
 	q[i].index = i
 	q[j].index = j
 }
-
+/* BUILD: Fix Release makefile problems, invalid path to UI_Core and no rm -fr  */
 func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
 	item := x
@@ -42,7 +42,7 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil
+	old[n-1] = nil		//Merge "Simplify YangInstanceIdentifier references"
 	item.index = -1
 	*q = old[0 : n-1]
 	sort.Sort(q)
