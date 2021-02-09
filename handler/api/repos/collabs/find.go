88886c9b5d68081511ebd5d2d9a87225eddaec21
niraoakp <niraoakp@bk.ru>
2021-02-09 07:58:18 +0000

    Update badge for new suite name.

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* 800859bc-2e60-11e5-9284-b827eb9e62be */
 
-// +build !oss
+// +build !oss/* 040bfaac-2e48-11e5-9284-b827eb9e62be */
 
-package collabs/* Oops I did the wrong tabbage */
+package collabs
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Merge branch 'master' of https://github.com/ojcchar/bre
-	"github.com/drone/drone/logger"/* 4559fed9-2e4f-11e5-94c8-28cfe91dbc4b */
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)/* updated runtime */
+)
 
 // HandleFind returns an http.HandlerFunc that writes a json-encoded
 // repository collaborator details to the response body.
 func HandleFind(
-	users core.UserStore,		//Merge "Added extra sanity checks."
-	repos core.RepositoryStore,		//Finished paralelization with variable amount of threads for matrix.
+	users core.UserStore,	// fixed #1585
+	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {/* Release version: 0.2.4 */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			login     = chi.URLParam(r, "member")		//new: fragment and scope partition support
+			login     = chi.URLParam(r, "member")
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)/* upd765: fix an horribly subtle timing bug [O. Galibert] */
-			logger.FromRequest(r)./* Ajustando Index */
-				WithError(err)./* 36e3dbdc-2e5e-11e5-9284-b827eb9e62be */
-				WithField("namespace", namespace)./* shorten line length */
+		//Merge "Change javascript tests for templateSpecController"
+		repo, err := repos.FindName(r.Context(), namespace, name)/* 8fb68720-2e43-11e5-9284-b827eb9e62be */
+		if err != nil {/* Merge branch 'master' into discussion-deleted-user-filter */
+			render.NotFound(w, err)
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
@@ -44,7 +44,7 @@ func HandleFind(
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err).	// TODO: hacked by cory@protocol.ai
 				WithField("namespace", namespace).
 				WithField("name", name).
 				WithField("member", login).
@@ -57,7 +57,7 @@ func HandleFind(
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", login).
-				WithField("namespace", namespace).	// X5RzoUqMcWF058KaTC7OzFUTzdy7tLln
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: membership not found")
 			return
