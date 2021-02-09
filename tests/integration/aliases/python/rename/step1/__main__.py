40156ea094170ddbfff20b136d83387f9d6482ca
niraoakp <niraoakp@bk.ru>
2021-02-09 20:54:29 +0000

    add ch04 READE file.

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -5,6 +5,6 @@ from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
-# Scenario #1 - rename a resource
+/* new service for ApartmentReleaseLA */
+# Scenario #1 - rename a resource/* BaseScmReleasePlugin added and used for GitReleasePlugin */
 res1 = Resource1("res1")
