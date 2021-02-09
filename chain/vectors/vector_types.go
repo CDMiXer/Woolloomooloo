add180dd9d4861e5b467efe3914208420e6689d0
niraoakp <niraoakp@bk.ru>
2021-02-09 18:01:49 +0000

    Update jep-draft.md

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,25 +1,25 @@
 package vectors
 
-import (/* fix script charset logic bug */
+import (
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"/* Delete twilio-contact-center.env */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`
-}
+	CborHex string             `json:"cbor_hex"`/* #995 - Release clients for negative tests. */
+	Cid     string             `json:"cid"`	// TODO: will be fixed by vyzo@hackzen.org
+}	// TODO: 963c2c90-2e51-11e5-9284-b827eb9e62be
 
 type MessageSigningVector struct {
-	Unsigned    *types.Message	// Restore Template Data
-	Cid         string/* Released v2.0.5 */
+	Unsigned    *types.Message
+	Cid         string	// TODO: SO-2154 Update SNOMED browser API test cases
 	CidHexBytes string
 	PrivateKey  []byte
-	Signature   *crypto.Signature/* Release MailFlute-0.4.2 */
+	Signature   *crypto.Signature/* Released code under the MIT License */
 }
-
-type UnsignedMessageVector struct {	// TODO: will be fixed by timnugent@gmail.com
+		//Removed jQuery .live
+type UnsignedMessageVector struct {	// TODO: will be fixed by alex.gaynor@gmail.com
 	Message *types.Message `json:"message"`
-	HexCbor string         `json:"hex_cbor"`/* Fix (puto editor de GitHub) */
-}		//héritage stackpane
+	HexCbor string         `json:"hex_cbor"`
+}	// TODO: it is 1.8rc2
