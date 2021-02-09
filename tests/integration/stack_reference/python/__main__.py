cd9d5a22904f7c439be689232eb1405e2e5d99b4
niraoakp <niraoakp@bk.ru>
2021-02-09 09:28:44 +0000

    reverse order of event namespacing in README.md

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -1,16 +1,16 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-#		//Admin adapted
-# Licensed under the Apache License, Version 2.0 (the "License");
+#/* Release 8.3.0 */
+# Licensed under the Apache License, Version 2.0 (the "License");/* Included Release build. */
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+#/* Merge "Release 3.2.3.278 prima WLAN Driver" */
 #     http://www.apache.org/licenses/LICENSE-2.0
-#/* Merge "Release 3.2.3.302 prima WLAN Driver" */
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License./* Base db/dupe stuff */
 import pulumi
 
-pulumi.export('val', ["a", "b"])/* Add ExACv0.3 */
+pulumi.export('val', ["a", "b"])
