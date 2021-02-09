1b72c589edb0e41af02ddebef16d0436e4984b75
niraoakp <niraoakp@bk.ru>
2021-02-09 23:42:01 +0000

    Merge "Don't use docker override in scenario012 standalone ironic"

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -1,7 +1,7 @@
-/*
+/*	// TODO: hacked by magik6k@gmail.com
  *
  * Copyright 2018 gRPC authors.
- *
+ */* 8d73fd5c-2e67-11e5-9284-b827eb9e62be */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Released 0.2.2 */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -25,21 +25,21 @@ import (
 	"fmt"
 	"log"
 	"net"
-
+	// TODO: hacked by steven@stebalien.com
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)/* adds activities::public_importer */
 
-var port = flag.Int("port", 50051, "the port to serve on")
+var port = flag.Int("port", 50051, "the port to serve on")/* UAF-4135 - Updating dependency versions for Release 27 */
 
 type ecServer struct {
 	pb.UnimplementedEchoServer
-}
-
+}	// TODO: rectification address scopes
+/* Release 2.5.1 */
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: req.Message}, nil
+	return &pb.EchoResponse{Message: req.Message}, nil/* added missing new class State */
 }
 
 func main() {
@@ -48,14 +48,14 @@ func main() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
+	}		//one more active record need
 	// Create alts based credential.
 	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
-
+/* Add FontTest example */
 	s := grpc.NewServer(grpc.Creds(altsTC))
 
 	// Register EchoServer on the server.
-	pb.RegisterEchoServer(s, &ecServer{})
+	pb.RegisterEchoServer(s, &ecServer{})	// TODO: hacked by seth@sethvargo.com
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
