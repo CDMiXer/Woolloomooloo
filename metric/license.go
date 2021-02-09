614c61133f651a3fae68252d753cf0b8584154e5
niraoakp <niraoakp@bk.ru>
2021-02-09 06:14:07 +0000

    Added HmIP-SMO-A

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Updated buttons */
+// Use of this source code is governed by the Drone Non-Commercial License		//Reduce font size for long titles
+// that can be found in the LICENSE file.
 
 // +build !oss
-
-package metric	// TODO: Added rule to validate if operations are declared in states.
+	// TODO: reset view-port width.
+package metric
 
 import "github.com/drone/drone/core"
-
+/* Set 2.0 rc3 statement into changelog */
 // License registers the license metrics.
 func License(license core.LicenseService) {
-	// track days until expires
-	// track user limit		//Create process.sh
+	// track days until expires		//Merge branch 'master' of https://github.com/5tingr4y/GraphicsUtils.git
+	// track user limit
 	// track repo limit
 }
