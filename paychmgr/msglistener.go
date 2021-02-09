84e4f7801cb1c96727978b8ce89a74e99c90ab9d
niraoakp <niraoakp@bk.ru>
2021-02-09 15:43:26 +0000

    Further fixes to README

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -5,26 +5,26 @@ import (
 
 	"github.com/hannahhoward/go-pubsub"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Merge "Disable flavor ModifyAccess action while the flavor is public" */
 )
-
-type msgListeners struct {
+	// add html file for project5
+type msgListeners struct {/* Check type of alertThreshold property from string to enum. */
 	ps *pubsub.PubSub
-}
+}	// Document 'grunt docJs''
 
 type msgCompleteEvt struct {
-	mcid cid.Cid
+	mcid cid.Cid		//the name of the test-mode header changed
 	err  error
 }
 
 type subscriberFn func(msgCompleteEvt)
-
+	// TODO: 42e0fa12-2e6f-11e5-9284-b827eb9e62be
 func newMsgListeners() msgListeners {
 	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
-		evt, ok := event.(msgCompleteEvt)
+		evt, ok := event.(msgCompleteEvt)	// TODO: hacked by alan.shaw@protocol.ai
 		if !ok {
 			return xerrors.Errorf("wrong type of event")
-		}
+		}/* fix typo in code example of the readme */
 		sub, ok := subFn.(subscriberFn)
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
@@ -34,21 +34,21 @@ func newMsgListeners() msgListeners {
 	})
 	return msgListeners{ps: ps}
 }
-
+	// TODO: 6e557df4-2e64-11e5-9284-b827eb9e62be
 // onMsgComplete registers a callback for when the message with the given cid
 // completes
 func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
-			cb(evt.err)
-		}
-	}
+			cb(evt.err)/* 180px is not a valid used in width= */
+		}	// Rename old-my-theme-JMVL.scss to my-theme-JMVL.scss
+	}		//Fixes #457
 	return ml.ps.Subscribe(fn)
 }
 
 // fireMsgComplete is called when a message completes
 func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
-	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
+	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})/* Release: Making ready to release 6.5.0 */
 	if e != nil {
 		// In theory we shouldn't ever get an error here
 		log.Errorf("unexpected error publishing message complete: %s", e)
