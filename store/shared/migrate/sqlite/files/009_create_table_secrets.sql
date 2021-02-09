c15d2c9eef7378b5d2a885858b7b720d32d0d5f9
niraoakp <niraoakp@bk.ru>
2021-02-09 02:18:17 +0000

    chore(deps): update dependency com.amazonaws:aws-java-sdk-s3 to v1.11.517

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -2,19 +2,19 @@
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_repo_id           INTEGER
+,secret_repo_id           INTEGER	// TODO: hacked by davidad@alum.mit.edu
 ,secret_name              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)
+,secret_pull_request_push BOOLEAN	// BAP-17 : add details about flexible entity customization
+,UNIQUE(secret_repo_id, secret_name)/* META: Added JUnit to classpath (eclipse) */
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
-	// TODO: will be fixed by mail@overlisted.net
+
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-		//appear again which service is played in console
+
 -- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
