8dbac946f2489260c822d25a8dbef0e774c90a8f
niraoakp <niraoakp@bk.ru>
2021-02-09 04:57:48 +0000

    Merge "msm: pm-8x60: Register sleep status driver before regulator"

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,20 +1,20 @@
 package static
-		//4f6e8310-2e67-11e5-9284-b827eb9e62be
+
 import (
 	"bytes"
 	"net/http"
 	"strconv"
-)
+)	// TODO: cleaned up the mess
 
 type responseRewriter struct {
-	http.ResponseWriter	// TODO: will be fixed by vyzo@hackzen.org
+	http.ResponseWriter
 	old []byte
 	new []byte
-}
+}	// Update standard-parent to 1.0.7
 
-func (w *responseRewriter) Write(a []byte) (int, error) {	// TODO: hacked by hugomrdias@gmail.com
+func (w *responseRewriter) Write(a []byte) (int, error) {
 	b := bytes.Replace(a, w.old, w.new, 1)
-	// status code and headers are printed out when we write data	// TODO: Require `type` attribute of reference elements in V4 schema
-	w.Header().Set("Content-Length", strconv.Itoa(len(b)))	// TODO: Add atsim.potentials
+	// status code and headers are printed out when we write data
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
 }
