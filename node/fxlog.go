f603ff74f4cb26929f4f7145d0cd3001f23c3686
niraoakp <niraoakp@bk.ru>
2021-02-09 21:28:06 +0000

    better presets for salmonella

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
-
+	// TODO: throws an exception if source path is empty
 import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"go.uber.org/fx"
-)
+)/* Удалён неиспользуемый файл robox.txt */
 
 type debugPrinter struct {
 	l logging.StandardLogger
 }
 
-func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)
+func (p *debugPrinter) Printf(f string, a ...interface{}) {		//Handling of 'no_shared_cipher' SSLError.
+	p.l.Debugf(f, a...)	// TODO: hacked by steven@stebalien.com
 }
 
 var _ fx.Printer = new(debugPrinter)
