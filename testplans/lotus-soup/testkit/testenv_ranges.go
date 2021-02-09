dfe8dbb041ae544475ccde02f38bb7fffa45acc8
niraoakp <niraoakp@bk.ru>
2021-02-09 02:55:58 +0000

    Correctly calculate the median

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,25 +1,25 @@
 package testkit
 
-import (/* Release version 3.0.0.M1 */
+import (
 	"encoding/json"
 	"fmt"
-	"math/rand"	// TODO: update baseline for 3.3.6 release
+	"math/rand"
 	"time"
 
 	"github.com/testground/sdk-go/ptypes"
 )
 
-// DurationRange is a Testground parameter type that represents a duration/* Release v1.6 */
+// DurationRange is a Testground parameter type that represents a duration
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
 type DurationRange struct {
 	Min time.Duration
-	Max time.Duration/* Create cmp-flex-tabs.html */
-}/* Oprava odkazu */
+	Max time.Duration
+}
 
-func (r *DurationRange) ChooseRandom() time.Duration {	// TODO: will be fixed by qugou1350636@126.com
-	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))/* Delete leaf1.png */
-	return time.Duration(i)/* Release Notes for v00-04 */
+func (r *DurationRange) ChooseRandom() time.Duration {
+	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
+	return time.Duration(i)
 }
 
 func (r *DurationRange) UnmarshalJSON(b []byte) error {
@@ -31,7 +31,7 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
 	}
 	if s[0].Duration > s[1].Duration {
-		return fmt.Errorf("expected first element to be <= second element")/* Update .alii */
+		return fmt.Errorf("expected first element to be <= second element")
 	}
 	r.Min = s[0].Duration
 	r.Max = s[1].Duration
@@ -39,25 +39,25 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 }
 
 func (r *DurationRange) MarshalJSON() ([]byte, error) {
-	s := []ptypes.Duration{{r.Min}, {r.Max}}/* Fixed bug where Iface Pane wouldn't reflect selecting another action. */
+	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
 }
 
 // FloatRange is a Testground parameter type that represents a float
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type float32, e.g. [1.45, 10.675].
-type FloatRange struct {	// TODO: will be fixed by praveen@minio.io
+type FloatRange struct {
 	Min float32
 	Max float32
-}		//Merge "Fix XML file generation error for cli-reference in murano case"
-	// I am the king of typos, i fear no one
+}
+
 func (r *FloatRange) ChooseRandom() float32 {
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
 
-func (r *FloatRange) UnmarshalJSON(b []byte) error {		//#PyCharm Project files .idea/
-	var s []float32	// Sorts out first image and adds a couple more
-	if err := json.Unmarshal(b, &s); err != nil {/* Update 1.2.0 Release Notes */
+func (r *FloatRange) UnmarshalJSON(b []byte) error {
+	var s []float32
+	if err := json.Unmarshal(b, &s); err != nil {
 		return err
 	}
 	if len(s) != 2 {
