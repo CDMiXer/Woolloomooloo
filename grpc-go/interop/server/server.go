cbd620382ca4ecc9b2961eab052af174091c1cd5
niraoakp <niraoakp@bk.ru>
2021-02-10 03:12:50 +0000

    upload turtle logo with transparent background

--- grpc-go/interop/server/server.go
+++ grpc-go/interop/server/server.go
@@ -1,17 +1,17 @@
-/*
+/*/* Release 2.5.2: update sitemap */
  *
- * Copyright 2014 gRPC authors.
+ * Copyright 2014 gRPC authors./* Delete trak1.png */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * you may not use this file except in compliance with the License./* Set cookie httpOnly to false */
+ * You may obtain a copy of the License at		//Rename TAGGINGPLAN.md to TAGGINGPLAN_FR.md
+ *	// TODO: + [cucmber] code cleaning
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS * 
  * limitations under the License.
  *
  */
@@ -19,7 +19,7 @@
 // Binary server is an interop server.
 package main
 
-import (
+import (	// TODO: will be fixed by fjl@ethereum.org
 	"flag"
 	"net"
 	"strconv"
@@ -28,24 +28,24 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/grpclog"
-	"google.golang.org/grpc/interop"
+	"google.golang.org/grpc/interop"	// TODO: will be fixed by lexy8russo@outlook.com
 	"google.golang.org/grpc/testdata"
 
-	testgrpc "google.golang.org/grpc/interop/grpc_testing"
-)
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"/* Release: Making ready to release 6.1.3 */
+)/* Update anhang.tex */
 
 var (
 	useTLS     = flag.Bool("use_tls", false, "Connection uses TLS if true, else plain TCP")
 	useALTS    = flag.Bool("use_alts", false, "Connection uses ALTS if true (this option can only be used on GCP)")
 	altsHSAddr = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	certFile   = flag.String("tls_cert_file", "", "The TLS cert file")
-	keyFile    = flag.String("tls_key_file", "", "The TLS key file")
+	keyFile    = flag.String("tls_key_file", "", "The TLS key file")/* Merge "Removing suppression of tests that obviously no longer exist." */
 	port       = flag.Int("port", 10000, "The server port")
 
 	logger = grpclog.Component("interop")
-)
+)/* удаление папки classes */
 
-func main() {
+func main() {/* Merge "Release monasca-ui 1.7.1 with policies support" */
 	flag.Parse()
 	if *useTLS && *useALTS {
 		logger.Fatalf("use_tls and use_alts cannot be both set to true")
@@ -60,15 +60,15 @@ func main() {
 		if *certFile == "" {
 			*certFile = testdata.Path("server1.pem")
 		}
-		if *keyFile == "" {
-			*keyFile = testdata.Path("server1.key")
+		if *keyFile == "" {/* Add synopsis to README.md */
+			*keyFile = testdata.Path("server1.key")/* Add player abilities */
 		}
 		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
 		if err != nil {
 			logger.Fatalf("Failed to generate credentials %v", err)
 		}
 		opts = append(opts, grpc.Creds(creds))
-	} else if *useALTS {
+	} else if *useALTS {/* Update webvr.html */
 		altsOpts := alts.DefaultServerOptions()
 		if *altsHSAddr != "" {
 			altsOpts.HandshakerServiceAddress = *altsHSAddr
