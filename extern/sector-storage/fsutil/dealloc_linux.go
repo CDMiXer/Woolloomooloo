a3702e929a6007a4abc3a9db7c41d518bc6e253b
niraoakp <niraoakp@bk.ru>
2021-02-10 08:28:35 +0000

    hgweb: make stripes in file log with CSS

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,28 +1,28 @@
-package fsutil/* Release 0.13.2 */
+package fsutil
 
 import (
 	"os"
 	"syscall"
 
-	logging "github.com/ipfs/go-log/v2"	// TODO: will be fixed by earlephilhower@yahoo.com
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("fsutil")
+		//52f1d732-2e5b-11e5-9284-b827eb9e62be
+const FallocFlPunchHole = 0x02 // linux/falloc.h/* Update predictions.c */
 
-const FallocFlPunchHole = 0x02 // linux/falloc.h
-		//Merge branch 'master' into greenkeeper-del-2.2.2
-func Deallocate(file *os.File, offset int64, length int64) error {/* Delete exactNNs.cpp */
+func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
 		return nil
-	}
+	}		//Rename gw2tips to gw2tips.html
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
 	if errno, ok := err.(syscall.Errno); ok {
-		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
+		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {/* AppVeyor: Publishing artifacts to GitHub Releases. */
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
-			err = nil // log and ignore/* Release Version 1.1.0 */
+			err = nil // log and ignore
 		}
 	}
-
+		//Didn't realize there was a spec covering the version number
 	return err
 }
