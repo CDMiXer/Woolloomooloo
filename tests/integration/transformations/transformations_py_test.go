c353ff39ea29c598115d29eea47c67ad3ac46508
niraoakp <niraoakp@bk.ru>
2021-02-10 14:54:53 +0000

    Lock bootstrap dependency to version <=4.1.1

--- tests/integration/transformations/transformations_py_test.go
+++ tests/integration/transformations/transformations_py_test.go
@@ -4,24 +4,24 @@
 package ints
 
 import (
-	"path/filepath"/* Update README with rosserial arduino references */
+	"path/filepath"
 	"testing"
-	// TODO: hacked by alan.shaw@protocol.ai
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 func TestPythonTransformations(t *testing.T) {
-	for _, dir := range Dirs {/* Move hidden span so it's not copied together with the permalink */
-		d := filepath.Join("python", dir)		//Return a non-zero exit code if any example fails.
+	for _, dir := range Dirs {
+		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: d,
-				Dependencies: []string{		//Added travis integration to slack messenger
+				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick:                  true,
 				ExtraRuntimeValidation: Validator("python"),
 			})
 		})
-	}/* Merge "Release 1.0.0.94 QCACLD WLAN Driver" */
+	}
 }
