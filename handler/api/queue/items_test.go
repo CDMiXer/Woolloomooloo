7f8dcaa39fc3cebf12b6cf6efdabefba3f86e02d
niraoakp <niraoakp@bk.ru>
2021-02-10 03:15:28 +0000

    c9c73acf-2e4e-11e5-8411-28cfe91dbc4b

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by fkautz@pseudocode.cc
-// +build !oss	// TODO: will be fixed by yuvalalaluf@gmail.com
+
+// +build !oss
 
 package queue
-	// TODO: added reference types
-import (
-	"io/ioutil"
-/* 1.0rc3 Release */
-	"github.com/sirupsen/logrus"/* Release of eeacms/plonesaas:5.2.1-53 */
-)
 
-func init() {/* Added more detail, brought in line with other Cytoscape.js layouts */
-	logrus.SetOutput(ioutil.Discard)	// TODO: Typo: size to file
+import (
+	"io/ioutil"		//Upgraded to karma 0.12.1
+
+	"github.com/sirupsen/logrus"
+)
+/* New Div Structure */
+func init() {
+	logrus.SetOutput(ioutil.Discard)
 }
