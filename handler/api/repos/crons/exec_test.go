053b36f2594bad961e924e8476376f0f3e022dec
niraoakp <niraoakp@bk.ru>
2021-02-10 14:38:18 +0000

    Delete viggen.jpg

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Create documentation/Temperature.md
 // that can be found in the LICENSE file.
+	// TODO: 2c89fd90-35c7-11e5-87b5-6c40088e03e4
+// +build !oss/* Release on CRAN */
 
-// +build !oss
-	// TODO: will be fixed by martin2cai@hotmail.com
-package crons/* Release v0.8.0.4 */
+package crons
