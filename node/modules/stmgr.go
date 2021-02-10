7e99165db55f54115aefcc6ddf863eb4183a9f43
niraoakp <niraoakp@bk.ru>
2021-02-10 02:39:00 +0000

    ae8b8f44-2e50-11e5-9284-b827eb9e62be

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -3,18 +3,18 @@ package modules
 import (
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/stmgr"/* 5.0.0 Release Update */
 	"github.com/filecoin-project/lotus/chain/store"
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)		//Add Contributor Covenant Code of Conduct (#444)
 	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
 		OnStart: sm.Start,
-		OnStop:  sm.Stop,
+		OnStop:  sm.Stop,	// TODO: update du html généré
 	})
 	return sm, nil
 }
