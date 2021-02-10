57dab71ca13948c89850f3f511aa65588c2fd01a
niraoakp <niraoakp@bk.ru>
2021-02-10 18:44:15 +0000

    Updated JobManager, MultiJobManager

--- tests/integration/read/read_dbr/step2/index.ts
+++ tests/integration/read/read_dbr/step2/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release v2.1.7 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* fa6810d2-2e5e-11e5-9284-b827eb9e62be */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,14 +18,14 @@ const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 
 // B must be replaced, but it is a DBR replacement.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
-
+	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 // C depends on B, so it gets re-read. Before the read, it is removed from the
-// snapshot due to the deletion of B.
+// snapshot due to the deletion of B./* Merge branch 'master' of https://github.com/cuzfrog/psmm.git */
 const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
 
 // The engine generates:
-// A: Same
+// A: Same/* f303f85e-2e9c-11e5-9c1b-a45e60cdfd11 */
 // C: DeleteReplacement (read)
 // B: DeleteReplacement
-// B: Create
-// C: Read
+// B: Create/* move on to r24 */
+// C: Read		//tudo ate agora
