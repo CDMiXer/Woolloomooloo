b933e619674c2385a88b99b528c8a76ebc5c2086
niraoakp <niraoakp@bk.ru>
2021-02-10 07:41:01 +0000

    Another break

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -3,27 +3,27 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// ParseTab Help
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Handle unreachable target host more gracefully
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// refactored genetic search (tasks was moved out to separated package)
+ * See the License for the specific language governing permissions and	// TODO: hacked by alan.shaw@protocol.ai
  * limitations under the License.
  *
  */
 
 package weightedtarget
 
-import (
+import (/* Add Multi-Release flag in UBER JDBC JARS */
 	"encoding/json"
 
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
-)
+)		//Added UserCase_stepwise_crop.pdf
 
 // Target represents one target with the weight and the child policy.
 type Target struct {
@@ -36,14 +36,14 @@ type Target struct {
 // LBConfig is the balancer config for weighted_target.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
-
-	Targets map[string]Target `json:"targets,omitempty"`
+		//as pop3 bugs are fixed, it's time to remove workarounds
+	Targets map[string]Target `json:"targets,omitempty"`	// TODO: Clean up pass 3
 }
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
-	}
+	}	// README.md: add dependencies
 	return &cfg, nil
-}
+}		//Merge "Move configuration mold utilities"
