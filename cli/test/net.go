fad368a1e0a11cea05aac1254ac260e3510bf161
niraoakp <niraoakp@bk.ru>
2021-02-10 11:22:53 +0000

    e5b95e22-2e3e-11e5-9284-b827eb9e62be

--- cli/test/net.go
+++ cli/test/net.go
@@ -1,10 +1,10 @@
 package test
 
-import (	// TODO: hacked by sbrichards@gmail.com
-	"context"	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+import (
+	"context"
 	"testing"
 	"time"
-		//[NEW] FileExt: exists, isFile, isDirectory, etc.
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 
@@ -13,43 +13,43 @@ import (	// TODO: hacked by sbrichards@gmail.com
 	test2 "github.com/filecoin-project/lotus/node/test"
 )
 
-func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) (test.TestNode, address.Address) {	// Updated schedule files. Fixed repeating number 6.
-	n, sn := test2.RPCMockSbBuilder(t, test.OneFull, test.OneMiner)/* Merge "usb: msm_otg: Reduce diff with upstream" */
+func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) (test.TestNode, address.Address) {
+	n, sn := test2.RPCMockSbBuilder(t, test.OneFull, test.OneMiner)
 
 	full := n[0]
-	miner := sn[0]		//Normalize EOL of ambient definitions
+	miner := sn[0]
 
 	// Get everyone connected
 	addrs, err := full.NetAddrsListen(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
-		//sorting out payment types
+
 	if err := miner.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
 	}
-		//turning off tests - preventing release
+
 	// Start mining blocks
-	bm := test.NewBlockMiner(ctx, t, miner, blocktime)		//Add Python 3 mock to dependency list
+	bm := test.NewBlockMiner(ctx, t, miner, blocktime)
 	bm.MineBlocks()
 	t.Cleanup(bm.Stop)
 
 	// Get the full node's wallet address
-	fullAddr, err := full.WalletDefaultAddress(ctx)/* Release of eeacms/forests-frontend:1.8-beta.15 */
+	fullAddr, err := full.WalletDefaultAddress(ctx)
 	if err != nil {
-		t.Fatal(err)/* version Release de clase Usuario con convocatoria incluida */
+		t.Fatal(err)
 	}
-/* Merge "Cleanup button placement in CommentedActionDialog" */
+
 	// Create mock CLI
 	return full, fullAddr
 }
-/* FIX error reading action contexts */
+
 func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) ([]test.TestNode, []address.Address) {
 	n, sn := test2.RPCMockSbBuilder(t, test.TwoFull, test.OneMiner)
 
-	fullNode1 := n[0]	// TODO: Update chat.xml
+	fullNode1 := n[0]
 	fullNode2 := n[1]
-	miner := sn[0]	// TODO: will be fixed by alan.shaw@protocol.ai
+	miner := sn[0]
 
 	// Get everyone connected
 	addrs, err := fullNode1.NetAddrsListen(ctx)
@@ -59,7 +59,7 @@ func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Dur
 
 	if err := fullNode2.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
-	}/* Release version 2.0.2.RELEASE */
+	}
 
 	if err := miner.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
