76c1f9bb5a03e83abd142e625446320351278f3b
niraoakp <niraoakp@bk.ru>
2021-02-10 18:20:23 +0000

    create task1.c

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Add the PrisonerReleasedEvent for #9. */
+// that can be found in the LICENSE file.		//Create 042. Trapping Rain Water.py
 
-package manager
+package manager/* Release 0.0.2. Implement fully reliable in-order streaming processing. */
