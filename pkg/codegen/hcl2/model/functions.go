50b62f4281a05616a3015d604044390901bc7d1e
niraoakp <niraoakp@bk.ru>
2021-02-10 21:28:32 +0000

    Setting leaf distance

--- pkg/codegen/hcl2/model/functions.go
+++ pkg/codegen/hcl2/model/functions.go
@@ -1,44 +1,44 @@
-// Copyright 2016-2020, Pulumi Corporation./* Updated changelog with #5, #10 */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by nicksavers@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Added CSSClass */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "wlan: Release 3.2.3.115" */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update Release#banner to support commenting */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package model
-/* Merge "Release 1.0.0.243 QCACLD WLAN Driver" */
+
 import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"/* Update agira.md */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
-/* Released 1.0.0-beta-1 */
+
 // FunctionSignature represents a possibly-type-polymorphic function signature.
 type FunctionSignature interface {
-	// GetSignature returns the static signature for the function when invoked with the given arguments.	// Merge "Support installing the openstack collection from a local location"
-	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)/* Correção mínima em Release */
+	// GetSignature returns the static signature for the function when invoked with the given arguments.
+	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
 }
 
 // Parameter represents a single function parameter.
 type Parameter struct {
-	Name string // The name of the parameter./* First version of yammer fetcher based on spring-social-yammer */
-	Type Type   // The type of the parameter.	// TODO: fixed Issue 282 : second try
+	Name string // The name of the parameter.
+	Type Type   // The type of the parameter.
 }
 
 // StaticFunctionSignature records the parameters and return type of a function.
 type StaticFunctionSignature struct {
 	// The function's fixed parameters.
-	Parameters []Parameter/* Add translation value iteration helper function */
-	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be		//Create geopandas_overlays.ipynb
+	Parameters []Parameter
+	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be
 	// assignable to this parameter.
-	VarargsParameter *Parameter/* ษฏฎ ฆียยนพะ */
+	VarargsParameter *Parameter
 	// The return type of the function.
 	ReturnType Type
 }
