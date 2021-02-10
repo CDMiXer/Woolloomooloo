95063dc405ee6f29e9d215bbdab10cbea52b5814
niraoakp <niraoakp@bk.ru>
2021-02-10 06:06:19 +0000

    Correct typo. Fixes #329. Thanks to @kniebremser.

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -1,24 +1,24 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
-using System.Threading.Tasks;	// Merge "Add ssh patch context manager"
-using Pulumi;/* Release references to shared Dee models when a place goes offline. */
-/* First Release Fixes */
-class Resource : ComponentResource
+/* added separate NodeView display */
+using System.Threading.Tasks;
+using Pulumi;
+		//get rid of unused variables.
+class Resource : ComponentResource	// TODO: will be fixed by alan.shaw@protocol.ai
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}	// issue #40: minimal grid-extension version
+}
 
-class Program	// Add default to --debug-flag
+class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
         {
             // Scenario #1 - rename a resource
-            var res1 = new Resource("res1");
+            var res1 = new Resource("res1");		//ProAI - Various bug fixes and move units by unit type (redrum)
         });
     }
 }
