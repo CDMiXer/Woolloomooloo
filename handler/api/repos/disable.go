0e2c3d241610bf6e4b50d38c7fd15ac662175cca
niraoakp <niraoakp@bk.ru>
2021-02-10 11:00:49 +0000

    Update travis to using node v4.0

--- handler/api/repos/disable.go
+++ handler/api/repos/disable.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-///* 5.7.1 Release */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by hello@brooklynzelenka.com
-//	// TODO: Fix index_alloc returns INDEX_NONE handling
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,17 +19,17 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"	// Rename Value.value to Value.name.
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
-// HandleDisable returns an http.HandlerFunc that processes http		//Merge "Added list for Content team"
+// HandleDisable returns an http.HandlerFunc that processes http
 // requests to disable a repository in the system.
 func HandleDisable(
 	repos core.RepositoryStore,
 	sender core.WebhookSender,
-) http.HandlerFunc {/* [CI-SKIP] Fix jenkins url in pom */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
@@ -37,7 +37,7 @@ func HandleDisable(
 		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
-		if err != nil {/* Update xRect.m comments. */
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -48,18 +48,18 @@ func HandleDisable(
 		}
 		repo.Active = false
 		err = repos.Update(r.Context(), repo)
-		if err != nil {/* Fix bugs /serverconfig APIs */
-			render.InternalError(w, err)/* Changed time format for alarm table in plugin_customization.ini */
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
 				Warnln("api: cannot update repository")
-			return/* Release v4.3 */
+			return
 		}
 
 		action := core.WebhookActionDisabled
-		if r.FormValue("remove") == "true" {/* Release sim_launcher dependency */
+		if r.FormValue("remove") == "true" {
 			action = core.WebhookActionDeleted
 			err = repos.Delete(r.Context(), repo)
 			if err != nil {
@@ -68,7 +68,7 @@ func HandleDisable(
 					WithError(err).
 					WithField("namespace", owner).
 					WithField("name", name).
-					Warnln("api: cannot delete repository")/* Add a way to put threads in an architecture based variable */
+					Warnln("api: cannot delete repository")
 				return
 			}
 		}
@@ -77,14 +77,14 @@ func HandleDisable(
 			Event:  core.WebhookEventRepo,
 			Action: action,
 			Repo:   repo,
-		})	// TODO: will be fixed by mowrain@yandex.com
+		})
 		if err != nil {
-			logger.FromRequest(r).		//setup.sh: disable lastlog(8)
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
 				Warnln("api: cannot send webhook")
-		}/* 30b097dc-2f67-11e5-85c1-6c40088e03e4 */
+		}
 
 		render.JSON(w, repo, 200)
 	}
