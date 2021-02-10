1bc12ae4c6251251ed21a46db0f5ce5ed776df12
niraoakp <niraoakp@bk.ru>
2021-02-10 20:56:35 +0000

    Merge "Avoid use of deprecated commands in lenovo driver"

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by alex.gaynor@gmail.com
+// that can be found in the LICENSE file.		//Update AAChartModel.h
 
 // +build !oss
 
 package crons
 
 import (
-	"net/http"/* Added documentation for unit testing. */
-		//Update ndslabs.yaml
-	"github.com/drone/drone/core"/* First Version of Disaggregation Module */
-	"github.com/drone/drone/handler/api/render"
+	"net/http"
 
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+/* Refactor KakakuEmpty Class */
 	"github.com/go-chi/chi"
-)		//Update rdpModule.js
+)
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of cron jobs to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-	crons core.CronStore,/* Added utility method in InputComponent for input labels  */
+	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* Added description, creator and assists */
+			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
@@ -32,10 +32,10 @@ func HandleList(
 			return
 		}
 		list, err := crons.List(r.Context(), repo.ID)
-		if err != nil {/* Merge "Pass interface model to validation" */
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		render.JSON(w, list, 200)
-	}	// TODO: cleaner, but still the right...
+	}
 }
