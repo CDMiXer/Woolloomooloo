4566175b432ba32580033fc18c48932349527d35
niraoakp <niraoakp@bk.ru>
2021-02-10 21:01:36 +0000

    expand paths in aliases

--- pkg/codegen/nodejs/gen_program_test.go
+++ pkg/codegen/nodejs/gen_program_test.go
@@ -2,8 +2,8 @@ package nodejs
 
 import (
 	"bytes"
-	"io/ioutil"
-	"path/filepath"
+	"io/ioutil"/* Remoção código de teste */
+	"path/filepath"	// TODO: hacked by vyzo@hackzen.org
 	"strings"
 	"testing"
 
@@ -12,49 +12,49 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* update to dev projects for micello */
 )
-
+/* Add nodei badge */
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
-	if err != nil {
+	if err != nil {	// TODO: 36e5d1ec-2e47-11e5-9284-b827eb9e62be
 		t.Fatalf("could not read test data: %v", err)
-	}
+	}	// Add tests for LogFormatter.short_committer and LogFormatter.short_author.
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
 		}
-
+	// TODO: will be fixed by brosner@gmail.com
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
 		}
-
+/* gestion emplacement final sur la seedbox */
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
-			contents, err := ioutil.ReadFile(path)
+			contents, err := ioutil.ReadFile(path)	// TODO: Hilfetexte für neue 3D-Optionen ergaenzt.
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			expected, err := ioutil.ReadFile(path + ".ts")
+			expected, err := ioutil.ReadFile(path + ".ts")/* Laravel 5.7 Released */
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".ts", err)
 			}
 
-			parser := syntax.NewParser()
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
+			parser := syntax.NewParser()		//6944b6c2-2e41-11e5-9284-b827eb9e62be
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())/* Fix example for ReleaseAndDeploy with Octopus */
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			if parser.Diagnostics.HasErrors() {
+			if parser.Diagnostics.HasErrors() {	// Some macro definitions corrected
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
-
+		//[ADD]app.js for like system
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
-			if err != nil {
+			if err != nil {	// TODO: will be fixed by mail@overlisted.net
 				t.Fatalf("could not bind program: %v", err)
 			}
 			if diags.HasErrors() {
@@ -62,7 +62,7 @@ func TestGenProgram(t *testing.T) {
 			}
 
 			files, diags, err := GenerateProgram(program)
-			assert.NoError(t, err)
+			assert.NoError(t, err)/* Released springrestclient version 2.5.5 */
 			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
 				for _, d := range diags {
