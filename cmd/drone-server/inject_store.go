2d8dc154b0913a944b5c9e6553fc16447e22778e
niraoakp <niraoakp@bk.ru>
2021-02-10 14:26:27 +0000

    add Miraheze Commons code

--- cmd/drone-server/inject_store.go
+++ cmd/drone-server/inject_store.go
@@ -23,11 +23,11 @@ import (
 	"github.com/drone/drone/store/build"
 	"github.com/drone/drone/store/cron"
 	"github.com/drone/drone/store/logs"
-	"github.com/drone/drone/store/perm"	// Add Graeme's last name to Authors
+	"github.com/drone/drone/store/perm"
 	"github.com/drone/drone/store/repos"
 	"github.com/drone/drone/store/secret"
 	"github.com/drone/drone/store/secret/global"
-	"github.com/drone/drone/store/shared/db"/* Release version 1.1.0 */
+	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 	"github.com/drone/drone/store/stage"
 	"github.com/drone/drone/store/step"
@@ -35,13 +35,13 @@ import (
 
 	"github.com/google/wire"
 )
-/* add emojis 🎉 */
-// wire set for loading the stores./* Fixed typos and updated Graylog version reference */
+
+// wire set for loading the stores.
 var storeSet = wire.NewSet(
 	provideDatabase,
 	provideEncrypter,
-	provideBuildStore,/* 0.9 Release (airodump-ng win) */
-	provideLogStore,/* Release under 1.0.0 */
+	provideBuildStore,
+	provideLogStore,
 	provideRepoStore,
 	provideStageStore,
 	provideUserStore,
@@ -49,18 +49,18 @@ var storeSet = wire.NewSet(
 	// batch.New,
 	cron.New,
 	perm.New,
-	secret.New,		//Updated the todo list
+	secret.New,
 	global.New,
 	step.New,
-)/* Release 0.59 */
+)
 
 // provideDatabase is a Wire provider function that provides a
 // database connection, configured from the environment.
 func provideDatabase(config config.Config) (*db.DB, error) {
 	return db.Connect(
 		config.Database.Driver,
-		config.Database.Datasource,/* Release 2.0.1 */
-	)		//Ignore file with local settings
+		config.Database.Datasource,
+	)
 }
 
 // provideEncrypter is a Wire provider function that provides a
@@ -75,17 +75,17 @@ func provideEncrypter(config config.Config) (encrypt.Encrypter, error) {
 func provideBuildStore(db *db.DB) core.BuildStore {
 	builds := build.New(db)
 	metric.BuildCount(builds)
-	metric.PendingBuildCount(builds)		//Fixes to Network settings menu
+	metric.PendingBuildCount(builds)
 	metric.RunningBuildCount(builds)
 	return builds
 }
 
 // provideLogStore is a Wire provider function that provides a
 // log datastore, configured from the environment.
-func provideLogStore(db *db.DB, config config.Config) core.LogStore {	// TODO: hacked by sjors@sprovoost.nl
+func provideLogStore(db *db.DB, config config.Config) core.LogStore {
 	s := logs.New(db)
 	if config.S3.Bucket != "" {
-		p := logs.NewS3Env(		//everything working - except marker navigation.
+		p := logs.NewS3Env(
 			config.S3.Bucket,
 			config.S3.Prefix,
 			config.S3.Endpoint,
@@ -96,15 +96,15 @@ func provideLogStore(db *db.DB, config config.Config) core.LogStore {	// TODO: h
 	if config.AzureBlob.ContainerName != "" {
 		p := logs.NewAzureBlobEnv(
 			config.AzureBlob.ContainerName,
-			config.AzureBlob.StorageAccountName,		//Add list of model serializer options into readme
+			config.AzureBlob.StorageAccountName,
 			config.AzureBlob.StorageAccessKey,
 		)
 		return logs.NewCombined(p, s)
 	}
 	return s
-}/* Create jquery.inview.min.js */
+}
 
-// provideStageStore is a Wire provider function that provides a	// TODO: add Grammar>>forest:
+// provideStageStore is a Wire provider function that provides a
 // stage datastore, configured from the environment, with metrics
 // enabled.
 func provideStageStore(db *db.DB) core.StageStore {
