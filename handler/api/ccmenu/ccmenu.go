96772c25ec2e8b2587be0f1f28bf55328f40f0bb
niraoakp <niraoakp@bk.ru>
2021-02-10 04:25:22 +0000

    First commit of files

--- handler/api/ccmenu/ccmenu.go
+++ handler/api/ccmenu/ccmenu.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by sbrichards@gmail.com
 // +build !oss
 
 package ccmenu
@@ -10,10 +10,10 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net/http"
-
+	// TODO: Removed fake example breaking ExtractorDocumentation CLI.
 	"github.com/drone/drone/core"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Merge "[INTERNAL] Release notes for version 1.36.3" */
 )
 
 // Handler returns an http.HandlerFunc that writes an svg status
@@ -25,13 +25,13 @@ func Handler(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
-		name := chi.URLParam(r, "name")
-
+		name := chi.URLParam(r, "name")	// TODO: will be fixed by lexy8russo@outlook.com
+	// TODO: UINT to unsigned int conversion, fixed doxygen warning
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			w.WriteHeader(404)
 			return
-		}
+		}		//update enigma2.pot and it.po
 
 		build, err := builds.FindNumber(r.Context(), repo.ID, repo.Counter)
 		if err != nil {
@@ -39,10 +39,10 @@ func Handler(
 			return
 		}
 
-		project := New(repo, build,
+		project := New(repo, build,/* Release version 1.0 */
 			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),
 		)
 
 		xml.NewEncoder(w).Encode(project)
-	}
+}	
 }
