c1ff2771a7438f16144bf6cf09ec31b729078afe
niraoakp <niraoakp@bk.ru>
2021-02-10 06:08:51 +0000

    f0bb43e6-2e6e-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Merge devYears into development
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// Create asteroid.py
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Create duolingo_clear.js
+
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):/* Merge "Release Notes 6.0 -- Other issues" */
+# Scenario #4 - change the type of a component		//Update src/utils.go
+class ComponentFour(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
-comp4 = ComponentFour("comp4")
+comp4 = ComponentFour("comp4")/* fix blank space on top at small widths */
