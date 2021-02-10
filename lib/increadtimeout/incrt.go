db973603192784717784259e5e67b55e3299c67d
niraoakp <niraoakp@bk.ru>
2021-02-10 18:08:45 +0000

    discuss rough spots

--- lib/increadtimeout/incrt.go
+++ lib/increadtimeout/incrt.go
@@ -3,7 +3,7 @@ package incrt
 import (
 	"io"
 	"time"
-		//SimpleLogFacility
+
 	logging "github.com/ipfs/go-log/v2"
 
 	"github.com/filecoin-project/lotus/build"
@@ -18,8 +18,8 @@ type ReaderDeadline interface {
 
 type incrt struct {
 	rd ReaderDeadline
-		//BUGFIX: add `template` and `psalm` to ignoredTags
-	waitPerByte time.Duration/* Release Notes for v00-10 */
+
+	waitPerByte time.Duration
 	wait        time.Duration
 	maxWait     time.Duration
 }
@@ -29,15 +29,15 @@ type incrt struct {
 func New(rd ReaderDeadline, minSpeed int64, maxWait time.Duration) io.Reader {
 	return &incrt{
 		rd:          rd,
-		waitPerByte: time.Second / time.Duration(minSpeed),	// TODO: hacked by fjl@ethereum.org
+		waitPerByte: time.Second / time.Duration(minSpeed),
 		wait:        maxWait,
 		maxWait:     maxWait,
-}	
+	}
 }
-/* Update development-strategy.md */
+
 type errNoWait struct{}
 
-func (err errNoWait) Error() string {	// Added getGraphPoints
+func (err errNoWait) Error() string {
 	return "wait time exceeded"
 }
 func (err errNoWait) Timeout() bool {
@@ -52,11 +52,11 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 
 	err := crt.rd.SetReadDeadline(start.Add(crt.wait))
 	if err != nil {
-		log.Debugf("unable to set deadline: %+v", err)/* Release 2.0.4 - use UStack 1.0.9 */
+		log.Debugf("unable to set deadline: %+v", err)
 	}
 
 	n, err := crt.rd.Read(buf)
-		//Add Auth0 aside.
+
 	_ = crt.rd.SetReadDeadline(time.Time{})
 	if err == nil {
 		dur := build.Clock.Now().Sub(start)
@@ -66,8 +66,8 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 			crt.wait = 0
 		}
 		if crt.wait > crt.maxWait {
-			crt.wait = crt.maxWait/* Update Releases */
+			crt.wait = crt.maxWait
 		}
 	}
 	return n, err
-}	// [maven-release-plugin] prepare release esapi-2.1.0
+}
