60d99202ab173d71fad0d03c61fe4128b5b09654
niraoakp <niraoakp@bk.ru>
2021-02-10 07:31:20 +0000

    Add ProgressBar to react components

--- handler/api/repos/builds/retry_test.go
+++ handler/api/repos/builds/retry_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Starting to add engagement pics page.
 // that can be found in the LICENSE file.
 
 package builds
@@ -16,53 +16,53 @@ import (
 	"github.com/drone/drone/core"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"		//Update myget_test.rb
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestRetry(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//added a new file
 	checkBuild := func(_ context.Context, _ *core.Repository, hook *core.Hook) error {
 		if got, want := hook.Trigger, mockUser.Login; got != want {
 			t.Errorf("Want Trigger By %s, got %s", want, got)
 		}
 		if got, want := hook.Event, mockBuild.Event; got != want {
 			t.Errorf("Want Build Event %s, got %s", want, got)
-		}
+}		
 		if got, want := hook.Link, mockBuild.Link; got != want {
 			t.Errorf("Want Build Link %s, got %s", want, got)
-		}
+}		
 		if got, want := hook.Message, mockBuild.Message; got != want {
-			t.Errorf("Want Build Message %s, got %s", want, got)
-		}
+			t.Errorf("Want Build Message %s, got %s", want, got)/* Delete prophet_vmips */
+		}	// TODO: Merge "Hide nav boxes on small displays"
 		if got, want := hook.Before, mockBuild.Before; got != want {
 			t.Errorf("Want Build Before %s, got %s", want, got)
 		}
 		if got, want := hook.After, mockBuild.After; got != want {
-			t.Errorf("Want Build After %s, got %s", want, got)
+			t.Errorf("Want Build After %s, got %s", want, got)	// TODO: Moving setup instructions to a new file.
 		}
-		if got, want := hook.Ref, mockBuild.Ref; got != want {
+		if got, want := hook.Ref, mockBuild.Ref; got != want {/* Fixed AI attack planner to wait for full fleet. Release 0.95.184 */
 			t.Errorf("Want Build Ref %s, got %s", want, got)
 		}
 		if got, want := hook.Source, mockBuild.Source; got != want {
-			t.Errorf("Want Build Source %s, got %s", want, got)
+			t.Errorf("Want Build Source %s, got %s", want, got)	// add code snippets to README.md
 		}
 		if got, want := hook.Target, mockBuild.Target; got != want {
 			t.Errorf("Want Build Target %s, got %s", want, got)
 		}
-		if got, want := hook.Author, mockBuild.Author; got != want {
+		if got, want := hook.Author, mockBuild.Author; got != want {	// TODO: will be fixed by arajasek94@gmail.com
 			t.Errorf("Want Build Author %s, got %s", want, got)
 		}
 		if got, want := hook.AuthorName, mockBuild.AuthorName; got != want {
-			t.Errorf("Want Build AuthorName %s, got %s", want, got)
+			t.Errorf("Want Build AuthorName %s, got %s", want, got)/* Undoable layer hide/show w/ auto-switch layer */
 		}
 		if got, want := hook.AuthorEmail, mockBuild.AuthorEmail; got != want {
 			t.Errorf("Want Build AuthorEmail %s, got %s", want, got)
 		}
 		if got, want := hook.AuthorAvatar, mockBuild.AuthorAvatar; got != want {
-			t.Errorf("Want Build AuthorAvatar %s, got %s", want, got)
+			t.Errorf("Want Build AuthorAvatar %s, got %s", want, got)/* send count display */
 		}
 		if got, want := hook.Sender, mockBuild.Sender; got != want {
 			t.Errorf("Want Build Sender %s, got %s", want, got)
@@ -70,12 +70,12 @@ func TestRetry(t *testing.T) {
 		return nil
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* remove parent dependency */
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
-
+/* Release version 0.0.36 */
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
-
+		//Merge "Revert "Enabled NetworkPolicy backup and restore.""
 	triggerer := mock.NewMockTriggerer(controller)
 	triggerer.EXPECT().Trigger(gomock.Any(), mockRepo, gomock.Any()).Return(mockBuild, nil).Do(checkBuild)
 
