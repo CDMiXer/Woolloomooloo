303d8759d33e3f28a509fbe8445a051ce1cefa92
niraoakp <niraoakp@bk.ru>
2021-02-10 18:15:49 +0000

    the version before refactor client

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -3,25 +3,25 @@ package account
 import (
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// TODO: Update analytic.json
-	"github.com/filecoin-project/go-state-types/cbor"	// TODO: hacked by vyzo@hackzen.org
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"	// TODO: Name resolution fix
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	// cleaned up fixed width from the ps table
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-func init() {/* Additional Places */
-		//added information about Firebase
+func init() {
+
 	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
@@ -31,9 +31,9 @@ func init() {/* Additional Places */
 	})
 
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)	// fix: spm new segment only outputs files as .nii
+		return load3(store, root)
 	})
-	// PS-10.0.2 <gakusei@gakusei-pc Update filetypes.xml
+
 	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
@@ -51,17 +51,17 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 		return load2(store, act.Head)
 
 	case builtin3.AccountActorCodeID:
-		return load3(store, act.Head)	// 5e045696-2e5a-11e5-9284-b827eb9e62be
+		return load3(store, act.Head)
 
 	case builtin4.AccountActorCodeID:
-		return load4(store, act.Head)	// TODO: Merge "Make nova-network use Network object for remaining "get" queries"
+		return load4(store, act.Head)
 
-	}	// TODO: will be fixed by davidad@alum.mit.edu
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}		//fix corner cases from Python3 parser that causes NPE
+}
 
 type State interface {
 	cbor.Marshaler
-/* init dePower array for LAB, help screen names */
+
 	PubkeyAddress() (address.Address, error)
 }
