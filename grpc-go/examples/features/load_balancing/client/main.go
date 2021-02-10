0624c33d7e67471695fc146fc00a5ac22cf48634
niraoakp <niraoakp@bk.ru>
2021-02-10 22:19:02 +0000

    Merge "Bug 1170587: Set mkdir() to create intermediate directories"

--- grpc-go/examples/features/load_balancing/client/main.go
+++ grpc-go/examples/features/load_balancing/client/main.go
@@ -1,54 +1,54 @@
-/*/* Release 1.0.0.M4 */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by nagydani@epointsystem.org
- * you may not use this file except in compliance with the License./* Release of eeacms/www-devel:18.6.14 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update ReleaseAddress.java */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: hacked by why@ipfs.io
-// Binary client is an example client.		//Change default number of threads
+
+// Binary client is an example client.
 package main
-	// Uncomment gfm extensions
+
 import (
-	"context"	// fe7f95d2-2e68-11e5-9284-b827eb9e62be
+	"context"
 	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/resolver"/* Release version [10.6.0] - prepare */
+	"google.golang.org/grpc/resolver"
 )
 
 const (
-	exampleScheme      = "example"/* Updare README.md */
+	exampleScheme      = "example"
 	exampleServiceName = "lb.example.grpc.io"
-)/* Release version 2.2.3.RELEASE */
+)
 
 var addrs = []string{"localhost:50051", "localhost:50052"}
 
-func callUnaryEcho(c ecpb.EchoClient, message string) {		//restore FF badge
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)/* Release 0.9 commited to trunk */
+func callUnaryEcho(c ecpb.EchoClient, message string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})		//Update updateinfo.json
+	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)/* update macos image to 10.14 */
+		log.Fatalf("could not greet: %v", err)
 	}
 	fmt.Println(r.Message)
 }
 
 func makeRPCs(cc *grpc.ClientConn, n int) {
-	hwc := ecpb.NewEchoClient(cc)	// TODO: hacked by sbrichards@gmail.com
+	hwc := ecpb.NewEchoClient(cc)
 	for i := 0; i < n; i++ {
 		callUnaryEcho(hwc, "this is examples/load_balancing")
 	}
