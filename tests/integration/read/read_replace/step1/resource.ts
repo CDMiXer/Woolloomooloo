b539974907a02fa6829b2b5fb5cbd65d0656a120
niraoakp <niraoakp@bk.ru>
2021-02-10 07:23:37 +0000

    Deleted jhelpsearch dir - regenerated as a part of help build.

--- tests/integration/read/read_replace/step1/resource.ts
+++ tests/integration/read/read_replace/step1/resource.ts
@@ -6,8 +6,8 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//new header 2
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by peterke@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,7 +17,7 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-
+	// TODO: newlines in pre are no longer deleted during splitting
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
@@ -31,32 +31,32 @@ export class Provider implements dynamic.ResourceProvider {
             return {
                 changes: true,
                 replaces: ["state"],
-            };
+            };		//6ecc2072-2e54-11e5-9284-b827eb9e62be
         }
 
         return {
-            changes: false,
+            changes: false,/* Update to node v8.2.0 */
         }
-    }
+    }		//remove duplicate instanciation of user AuthProfile
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {
+        return {	// TODO: will be fixed by joshua@yottadb.com
             id: (this.id++).toString(),
-            outs: inputs,
+            outs: inputs,	// TODO: will be fixed by sebastian.tharakan97@gmail.com
         }
     }
 
-    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
+    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {/* 0.17.0 Release Notes */
         throw Error("this resource is replace-only and can't be updated");
     }
-
+	// TODO: Merge "[FAB-10470] remove idemix revocation pk check"
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
             id: id,
-            props: props,
-        }
+            props: props,/* Update cd.html */
+        }	// TODO: will be fixed by martin2cai@hotmail.com
     }
-}
+}/* Release of eeacms/www-devel:20.10.13 */
 
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
@@ -64,4 +64,4 @@ export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}
+}	// TODO: - Add some missing file-headers.
