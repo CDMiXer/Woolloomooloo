8e79b015e8fae4410b526cf1d76fde8597788150
niraoakp <niraoakp@bk.ru>
2021-02-10 21:18:50 +0000

    kldecoder autotooled

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
-/* Updated content, included wiki link */
-CREATE TABLE IF NOT EXISTS users (
- user_id            SERIAL PRIMARY KEY
+
+CREATE TABLE IF NOT EXISTS users (/* Release v10.34 (r/vinylscratch quick fix) */
+ user_id            SERIAL PRIMARY KEY/* fix bem lint failures */
 ,user_login         VARCHAR(250)
-,user_email         VARCHAR(500)
+,user_email         VARCHAR(500)/* Widget: Release surface if root window is NULL. */
 ,user_admin         BOOLEAN
-,user_active        BOOLEAN/* Release 2.2.5.4 */
+,user_active        BOOLEAN
 ,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
-,user_syncing       BOOLEAN	// TODO: hacked by aeongrp@outlook.com
+,user_syncing       BOOLEAN/* Release 2.0: multi accounts, overdraft risk assessment */
 ,user_synced        INTEGER
-REGETNI       detaerc_resu,
+,user_created       INTEGER/* update lu.cpp */
 ,user_updated       INTEGER
-,user_last_login    INTEGER/* Changed Downloads page from `Builds` folder to `Releases`. */
+,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
-,user_hash          VARCHAR(500)
+,user_hash          VARCHAR(500)	// filebox : 65%
 ,UNIQUE(user_login)
-,UNIQUE(user_hash)		//Create raise_blocksize_to_sell_bitcoin.md
+,UNIQUE(user_hash)
 );
