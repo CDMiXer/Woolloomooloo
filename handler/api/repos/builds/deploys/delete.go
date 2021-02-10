bbe48e22d056a3d9f9dc57a9b76eaa2e4eee1a6a
niraoakp <niraoakp@bk.ru>
2021-02-10 02:27:02 +0000

    Changed Bind() on didCreateDocumentProvider to Export() to make it virtual.

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,62 +1,62 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Merge "cosmetics: shorten long line"
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Require JSON in webpay_errors to run by itself
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//final images?
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Improve error message, props simonwheatley, fixes #8397 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Changed Month of Release */
+// See the License for the specific language governing permissions and	// TODO: Don't overeat
 // limitations under the License.
 
 package deploys
 
 import (
-	"net/http"
+	"net/http"/* Add Nordic Venture Family to organizations list */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//updated boat site link
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-
-	"github.com/go-chi/chi"	// TODO: hacked by steven@stebalien.com
+		//no need to specify scm:tag
+	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
-func HandleDelete(
-	repos core.RepositoryStore,		//02140fe6-2e43-11e5-9284-b827eb9e62be
+func HandleDelete(/* Update scriptOne.js */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Use sync queue instead of PushService */
-			name      = chi.URLParam(r, "name")/* Create sync.yml */
-			target    = chi.URLParam(r, "*")
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")
+			target    = chi.URLParam(r, "*")/* 4.0.7 Release changes */
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* avoid bugs if subclasses not loaded in .removeSubClass */
+		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r)./* 0.7 Release */
 				WithError(err).
-				WithField("namespace", namespace).	// TODO: Merge branch 'staging' into mandrill-subaccount
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}
+		}/* ed85ec24-2e70-11e5-9284-b827eb9e62be */
 
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
-		if err != nil {/* 5.2.4 Release */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* Released springjdbcdao version 1.7.17 */
 				WithField("namespace", namespace).
-				WithField("name", name)./* Remove download stats badge */
+				WithField("name", name).
 				Debugln("api: cannot delete deployment")
 		} else {
-			w.WriteHeader(http.StatusNoContent)/* Event/Poll/Queue: include cleanup */
+			w.WriteHeader(http.StatusNoContent)	// TODO: hacked by mikeal.rogers@gmail.com
 		}
 	}
-}	// TODO: [engine,builder] collection method now accepts rootname options
+}		//.travis.yml: Install Swig and Python to test builds of Python bindings as well.
