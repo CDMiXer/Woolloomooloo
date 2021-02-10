37f303420ad9d4c0f22c0937869fad43012915e9
niraoakp <niraoakp@bk.ru>
2021-02-10 14:52:25 +0000

    Update sec_04.tex

--- grpc-go/examples/helloworld/greeter_client/main.go
+++ grpc-go/examples/helloworld/greeter_client/main.go
@@ -1,28 +1,28 @@
 /*
- */* Base definitions for communication via spray */
+ *
  * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release 1-129. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release v12.0.0 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Adding the functionality to process the processor results, improved comments.
+ *
  */
 
-// Package main implements a client for Greeter service.	// Fixed string assignment bug
+// Package main implements a client for Greeter service.
 package main
-/* ášši is g3, and progress has begun. */
+
 import (
 	"context"
 	"log"
-	"os"	// TODO: Add service_id as a configurable parameter
+	"os"
 	"time"
 
 	"google.golang.org/grpc"
@@ -31,17 +31,17 @@ import (
 
 const (
 	address     = "localhost:50051"
-	defaultName = "world"/* scatter done */
+	defaultName = "world"
 )
-		//b8544452-2e47-11e5-9284-b827eb9e62be
-func main() {	// TODO: Modificacion del POM
+
+func main() {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()	// TODO: Allow more memory for Jacoco.
-	c := pb.NewGreeterClient(conn)/* Made some fields private */
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
 	name := defaultName
@@ -49,10 +49,10 @@ func main() {	// TODO: Modificacion del POM
 		name = os.Args[1]
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()		//Update card_search.py
+	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
-	}		//Delete Home640x1136.jpg
+	}
 	log.Printf("Greeting: %s", r.GetMessage())
 }
