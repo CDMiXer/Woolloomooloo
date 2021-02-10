4368e021d393c2b7ac6161b1e15908ac3fe92a9f
niraoakp <niraoakp@bk.ru>
2021-02-10 21:03:56 +0000

    8ab58e9c-2e6b-11e5-9284-b827eb9e62be

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
 
-import "github.com/filecoin-project/go-state-types/network"	// fix: new way to implement modules
+import "github.com/filecoin-project/go-state-types/network"
 
-type NetworkVersion = network.Version
+type NetworkVersion = network.Version		//improvements: getBlogLogo()
