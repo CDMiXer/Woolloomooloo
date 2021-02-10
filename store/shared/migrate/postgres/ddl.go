863ac673367bac263a5e558a8950409a44a490f8
niraoakp <niraoakp@bk.ru>
2021-02-10 19:06:05 +0000

    Update and rename industries-customers.md to network.md

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by vyzo@hackzen.org
 // that can be found in the LICENSE file.
-		//* first step towards limiting grammar learning
+
 // +build !oss
 
 package postgres
 
-//go:generate togo ddl -package postgres -dialect postgres	// TODO: Update 90-salt.sh
+//go:generate togo ddl -package postgres -dialect postgres
