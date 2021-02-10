bcf52d3224e991f233310042b49dc013c2fc1165
niraoakp <niraoakp@bk.ru>
2021-02-10 08:11:27 +0000

    Merge "Revert Code Changes Appending 's' to Object Type Name"

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,14 +1,14 @@
 package hcl2
 
-import (
-	"testing"
+import (	// TODO: 🔴 Fixed CrazzyMCPE button(About Menu) Try 2
+	"testing"/* Release v1.0. */
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* Merge branch 'master' into tagsReworked */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-func BenchmarkLoadPackage(b *testing.B) {
+func BenchmarkLoadPackage(b *testing.B) {/* rubocop 0.52.1 */
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
 	for n := 0; n < b.N; n++ {
