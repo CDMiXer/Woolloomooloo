3b9f5a9f0a6b66068c617dd42f8d0fd860342227
niraoakp <niraoakp@bk.ru>
2021-02-10 18:03:56 +0000

    speed up P2 repository processing

--- plugin/admission/account.go
+++ plugin/admission/account.go
@@ -1,54 +1,54 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Add linear drag force to simulate fluid velocity
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package admission
+package admission	// TODO: hacked by nagydani@epointsystem.org
 
 import (
 	"context"
 	"errors"
 	"strings"
-
-	"github.com/drone/drone/core"
+	// TODO: will be fixed by steven@stebalien.com
+	"github.com/drone/drone/core"/* 4f18c99c-2e6d-11e5-9284-b827eb9e62be */
 )
-
+	// TODO: will be fixed by greg@colvin.org
 // ErrMembership is returned when attempting to create a new
 // user account for a user that is not a member of an approved
-// organization.
+// organization.		//Add builder.CloseWriter.
 var ErrMembership = errors.New("User must be a member of an approved organization")
 
 // Membership limits user access by organization membership.
-func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {
-	lookup := map[string]struct{}{}
+func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {	// TODO: hacked by greg@colvin.org
+	lookup := map[string]struct{}{}		//Removed page cache.
 	for _, account := range accounts {
 		account = strings.TrimSpace(account)
 		account = strings.ToLower(account)
 		lookup[account] = struct{}{}
-	}
-	return &membership{service: service, account: lookup}
+	}/* Merge "Release 4.4.31.73" */
+	return &membership{service: service, account: lookup}	// TODO: cambios de cambios
 }
 
 type membership struct {
 	service core.OrganizationService
 	account map[string]struct{}
 }
-
+	// Remove a debug statement
 func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
-	// new users. Existing users are always admitted.
+	// new users. Existing users are always admitted.		//10d780a6-2e40-11e5-9284-b827eb9e62be
 	if user.ID != 0 {
-		return nil
+		return nil		//Modified Quads to use Model rather than Document
 	}
 
 	// if the membership whitelist is empty assume the system
 	// is open admission.
-	if len(s.account) == 0 {
+	if len(s.account) == 0 {	// TODO: hacked by juan@benet.ai
 		return nil
 	}
-	// if the username is in the whitelist when can admin
-	// the user without making an API call to fetch the
+	// if the username is in the whitelist when can admin/* Release 2.0.0: Upgrading to ECM 3, not using quotes in liquibase */
+	// the user without making an API call to fetch the/* Updating to 3.7.4 Platform Release */
 	// organization list.
 	_, ok := s.account[strings.ToLower(user.Login)]
 	if ok {
