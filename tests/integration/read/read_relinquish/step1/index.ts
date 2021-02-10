ebbd87f6824eb12c1b20dca86da5cc5ea5b4a680
niraoakp <niraoakp@bk.ru>
2021-02-10 14:02:51 +0000

    Delete .txt

--- tests/integration/read/read_relinquish/step1/index.ts
+++ tests/integration/read/read_relinquish/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
+///* [README] added synopsis/requirements/todo */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge branch 'hotfix/conflict-no-dom-autocomplete' */
-// you may not use this file except in compliance with the License./* Added a test subcommand */
-// You may obtain a copy of the License at/* Another try.. */
-//		//Delete jna-4.4.0.jar
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// Update VaxDesign1.py
 //
-// Unless required by applicable law or agreed to in writing, software	// Removed some old APIs
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release: Making ready for next release iteration 6.2.1 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//clean up some legacy cruft
 import { Resource } from "./resource";
 
-// Setup: "a" is a protected non-external resource.
+// Setup: "a" is a protected non-external resource.	// TODO: will be fixed by xaber.twt@gmail.com
 const a = new Resource("a", { state: 42 }, { protect: true });
