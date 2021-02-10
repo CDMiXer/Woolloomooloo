ff3a654582594635d3b7ddc90812a23ee3ee8cef
niraoakp <niraoakp@bk.ru>
2021-02-10 19:31:02 +0000

    Minor naming correction

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,37 +1,37 @@
 package sqldb
 
 import (
-	"fmt"
+	"fmt"	// TODO: Merge "Add datastore-version-show to OSC"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+)/* Organize listeners to be cleaner */
 
 var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
-var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
+var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")/* Release 0.2.21 */
 
 type explosiveOffloadNodeStatusRepo struct {
-}
+}/* Manifest refactoring */
 
 func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
-}/* Add Multi-Release flag in UBER JDBC JARS */
-		//Merge "Update zone_migration comment"
-func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {		//Added address variable to start script
+}		//Fix build after r44818 
+
+func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
 	return "", OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
+	return nil, OffloadNotSupportedError		//Made the code neater. Seeing it killed me a little inside.
+}
+	// TODO: will be fixed by yuvalalaluf@gmail.com
+func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
 }
-/* Rename build.sh to build_Release.sh */
-func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
-	return nil, OffloadNotSupportedError/* 4.1.6 beta 7 Release changes  */
-}		//Create DownloadingIntersect.md
-
-func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {/* Release of eeacms/varnish-eea-www:21.1.18 */
+/* one more active record need */
+func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
 	return OffloadNotSupportedError
 }
-/* Release notes for 1.0.81 */
+
 func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
-	return nil, OffloadNotSupportedError	// Delete dpu
-}/* Updated: aws-cli 1.16.74 */
+	return nil, OffloadNotSupportedError
+}
