1dfabc0d5b7ca5e96af11fdac565b973210daf2c
niraoakp <niraoakp@bk.ru>
2021-02-10 16:34:53 +0000

    removed oracle/mssql drivers that dosen't uses and make problems...

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -1,12 +1,12 @@
-# Copyright 2020, Pulumi Corporation.  All rights reserved./* Renamed "Latest Release" to "Download" */
+# Copyright 2020, Pulumi Corporation.  All rights reserved.
 
 import pulumi
-/* 04601bd0-2e40-11e5-9284-b827eb9e62be */
+
 config = pulumi.Config()
 org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
-	// TODO: will be fixed by souzau@yandex.com
+
 got_err = False
 
 try:
