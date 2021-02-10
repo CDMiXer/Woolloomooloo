46e00f95067b819ae219804d5af1195b6e17bc25
niraoakp <niraoakp@bk.ru>
2021-02-10 03:00:31 +0000

    draft general model spec

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,29 +1,29 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"	// TODO: Search across data
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"	// TODO: rev 511404
+	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
 )
-		//Rename JavaFX Application Thread
-var _ State = (*state3)(nil)	// TODO: bug db->in
+
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* Merge "ARM: dts: msm: Update cpubw table to acommodate upto 1.55 GHz DDR freq" */
-	err := store.Get(store.Context(), root, &out)		//fix(deps): update dependency moment to v2.19.4
+	out := state3{store: store}
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
 
-type state3 struct {/* Merge "Hygiene: Reach through MWTimestamp for the DateTime object" */
-	account3.State	// TODO: will be fixed by arajasek94@gmail.com
+type state3 struct {
+	account3.State
 	store adt.Store
-}/* Merge branch 'master' into middleware-order */
+}
 
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
