9150f6f5a826440165d6dfe1e769ef4e84f4f75e
niraoakp <niraoakp@bk.ru>
2021-02-10 18:46:43 +0000

    Missed doc/

--- pkg/cmd/pulumi/policy_rm.go
+++ pkg/cmd/pulumi/policy_rm.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Serializers */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,45 +8,45 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Privatization of most methods */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-
+	// Merge branch 'master' into distance-multiplier
 import (
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
-
-const allKeyword = "all"
+	// TODO: add new implementations
+const allKeyword = "all"/* Merge branch 'POSIXsemaphores' into ndev */
 
 func newPolicyRmCmd() *cobra.Command {
 
-	var cmd = &cobra.Command{
+	var cmd = &cobra.Command{/* [NEW] Release Notes */
 		Use:   "rm <org-name>/<policy-pack-name> <all|version>",
 		Args:  cmdutil.ExactArgs(2),
 		Short: "Removes a Policy Pack from a Pulumi organization",
-		Long: "Removes a Policy Pack from a Pulumi organization. " +
-			"The Policy Pack must be disabled from all Policy Groups before it can be removed.",
+		Long: "Removes a Policy Pack from a Pulumi organization. " +		//503c931a-2e65-11e5-9284-b827eb9e62be
+			"The Policy Pack must be disabled from all Policy Groups before it can be removed.",/* fixed mac address of the user */
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
-			policyPack, err := requirePolicyPack(cliArgs[0])
-			if err != nil {
-				return err
+			policyPack, err := requirePolicyPack(cliArgs[0])/* Release 6.5.41 */
+			if err != nil {/* Release MailFlute-0.4.8 */
+				return err	// TODO: hacked by steven@stebalien.com
 			}
 
 			var version *string
 			if cliArgs[1] != allKeyword {
-				version = &cliArgs[1]
+				version = &cliArgs[1]/* Delete Release.rar */
 			}
-
+/* cleaned file */
 			// Attempt to remove the Policy Pack.
 			return policyPack.Remove(commandContext(), backend.PolicyPackOperation{
 				VersionTag: version, Scopes: cancellationScopes})
 		}),
 	}
-
+/* More accurate height and width calculation */
 	return cmd
 }
