9d45c7e1ebb8a800423b032eae059da9fd783221
niraoakp <niraoakp@bk.ru>
2021-02-10 11:27:54 +0000

    Junit format o/p

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* New version of Chelonian - 0.1.1 */
-// Use of this source code is governed by the Drone Non-Commercial License		//Finished and debug User Model
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package secret
