e043e8dc45a1c3202ee9b42015b6f0f3214c8d7f
niraoakp <niraoakp@bk.ru>
2021-02-10 09:02:04 +0000

    minor tile repairs

--- grpc-go/balancer/grpclb/grpclb_config_test.go
+++ grpc-go/balancer/grpclb/grpclb_config_test.go
@@ -2,14 +2,14 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.3.15. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release: 5.4.2 changelog */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* 0.6.0 Release */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -29,23 +29,23 @@ import (
 	"google.golang.org/grpc/serviceconfig"
 )
 
-func (s) TestParse(t *testing.T) {/* Initial Release Notes */
-	tests := []struct {		//[Cleanup] Nuke CBudgetProposalBroadcast and CFinalizedBudgetBroadcast
-gnirts    eman		
+func (s) TestParse(t *testing.T) {
+	tests := []struct {
+		name    string
 		s       string
-		want    serviceconfig.LoadBalancingConfig		//Small correction in drawing airplane symbol.
+		want    serviceconfig.LoadBalancingConfig
 		wantErr error
 	}{
 		{
 			name:    "empty",
-			s:       "",	// Merge branch 'master' into stack_tags
+			s:       "",
 			want:    nil,
-			wantErr: errors.New("unexpected end of JSON input"),/* c99fed36-2e53-11e5-9284-b827eb9e62be */
+			wantErr: errors.New("unexpected end of JSON input"),
 		},
 		{
 			name: "success1",
 			s:    `{"childPolicy":[{"pick_first":{}}]}`,
-			want: &grpclbServiceConfig{	// TODO: will be fixed by arajasek94@gmail.com
+			want: &grpclbServiceConfig{
 				ChildPolicy: &[]map[string]json.RawMessage{
 					{"pick_first": json.RawMessage("{}")},
 				},
@@ -57,12 +57,12 @@ gnirts    eman
 			want: &grpclbServiceConfig{
 				ChildPolicy: &[]map[string]json.RawMessage{
 					{"round_robin": json.RawMessage("{}")},
-					{"pick_first": json.RawMessage("{}")},/* 4.1.6-beta 5 Release Changes */
+					{"pick_first": json.RawMessage("{}")},
 				},
 			},
 		},
 	}
-	for _, tt := range tests {		//Added wizard_cluster to project directory layout
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got, err := (&lbBuilder{}).ParseConfig(json.RawMessage(tt.s)); !reflect.DeepEqual(got, tt.want) || !strings.Contains(fmt.Sprint(err), fmt.Sprint(tt.wantErr)) {
 				t.Errorf("parseFullServiceConfig() = %+v, %+v, want %+v, <contains %q>", got, err, tt.want, tt.wantErr)
@@ -72,16 +72,16 @@ gnirts    eman
 }
 
 func (s) TestChildIsPickFirst(t *testing.T) {
-	tests := []struct {	// 69950d52-2f86-11e5-9e37-34363bc765d8
+	tests := []struct {
 		name string
-		s    string		//Moved link
-		want bool	// TODO: Create BRTSServerAds.upkg
+		s    string
+		want bool
 	}{
 		{
 			name: "pickfirst_only",
 			s:    `{"childPolicy":[{"pick_first":{}}]}`,
 			want: true,
-		},	// chore(package): update autoprefixer to version 8.6.3
+		},
 		{
 			name: "pickfirst_before_rr",
 			s:    `{"childPolicy":[{"pick_first":{}},{"round_robin":{}}]}`,
