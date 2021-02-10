87f15276cee1916e9dcf9097fcd07d923ff7cf1a
niraoakp <niraoakp@bk.ru>
2021-02-10 05:02:48 +0000

    Rename noarch/bin/nano-enable_syntax.sh to noarch/bin/nano/enable-syntax.sh

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -1,72 +1,72 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//Merge "Run fetch-subunit-output role conditionally"
+// that can be found in the LICENSE file./* Release for v35.2.0. */
 
 package repos
 
-import (
+import (/* Create weather-radar */
 	"encoding/json"
-	"io"
+	"io"	// TODO: hacked by willem.melching@gmail.com
 	"net/http"
-	"net/http/httptest"
-	"testing"
+	"net/http/httptest"	// Changed interface names
+	"testing"	// TODO: hacked by sjors@sprovoost.nl
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* Initial Release (v-1.0.0) */
 	"github.com/drone/drone/mock"
-
+/* [User] Reworked based on the Entity package. */
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"	// TODO: Texte explicatif sauvegarde bdd modif
 )
 
 func TestDisable(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repo := &core.Repository{/* add possibility to load eagerly entities with entity graph */
-		ID:        1,	// TODO: Update BaseMax.as
-		Namespace: "octocat",	// community module not in undoManager
-		Name:      "hello-world",/* revert user agent due to server side checks */
-		Slug:      "octocat/hello-world",/* Add support for fingerprint column */
-		Active:    true,	// TODO: Merge "Ignore failure to delete kernel/ramdisk in xenapi driver"
+	repo := &core.Repository{		//Surfacing a hidden gem
+		ID:        1,
+		Namespace: "octocat",
+		Name:      "hello-world",
+		Slug:      "octocat/hello-world",		//<li>...</li> on one line
+		Active:    true,
 	}
-/* c6008ede-2e57-11e5-9284-b827eb9e62be */
-	repos := mock.NewMockRepositoryStore(controller)	// TODO: hacked by remco@dutchcoders.io
+
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)
 
 	// a failed webhook should result in a warning message in the
-	// logs, but should not cause the endpoint to error./* Updated models (markdown) */
+	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
-
+/* Added route links. */
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
 	router.ServeHTTP(w, r)
-
+/* Update socpro.css */
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	if got, want := repo.Active, false; got != want {/* Release of eeacms/forests-frontend:1.5 */
-		t.Errorf("Want repository activate %v, got %v", want, got)
-	}
+	if got, want := repo.Active, false; got != want {
+		t.Errorf("Want repository activate %v, got %v", want, got)		//phase: links -> link_references
+	}		//Create careers.yml
 
-	got, want := new(core.Repository), repo/* Not sure anymore what I did */
+	got, want := new(core.Repository), repo
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Test MinimaHopping also.
-		t.Errorf(diff)/* @Release [io7m-jcanephora-0.9.23] */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
 	}
 }
 
-func TestDisable_NotFound(t *testing.T) {		//cite FEFF6 paper
-	controller := gomock.NewController(t)/* Update Docker for Mac version in installer */
+func TestDisable_NotFound(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//75a6a4be-2e3e-11e5-9284-b827eb9e62be
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
 
