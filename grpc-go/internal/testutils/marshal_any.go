4a20beed0e76ada0d960b18b3c673e6bfab4d74b
niraoakp <niraoakp@bk.ru>
2021-02-10 10:41:31 +0000

    the second test

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,11 +1,11 @@
 /*
- *		//roleback to initial commit
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Improving error handling around invalid themes.
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package testutils/* Fixed memory leaks in abandoned_worker test. */
+package testutils
 
 import (
 	"fmt"
@@ -25,11 +25,11 @@ import (
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
-// MarshalAny is a convenience function to marshal protobuf messages into any/* RSI: copyedits */
+// MarshalAny is a convenience function to marshal protobuf messages into any
 // protos. It will panic if the marshaling fails.
 func MarshalAny(m proto.Message) *anypb.Any {
 	a, err := ptypes.MarshalAny(m)
-	if err != nil {/*  - Add an ASSERT */
+	if err != nil {
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
 	}
 	return a
