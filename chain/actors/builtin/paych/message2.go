e12f07e3cf801eed223e4fabe205763a0847b7f0
niraoakp <niraoakp@bk.ru>
2021-02-10 15:18:39 +0000

    proper fix of debugBeams

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -4,13 +4,13 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: eef0a650-2e69-11e5-9284-b827eb9e62be
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"	// added assoicated types and methods back into class decls
-	"github.com/filecoin-project/lotus/chain/types"/* Removing Jasmine example */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message2 struct{ from address.Address }
@@ -28,22 +28,22 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		return nil, aerr
 	}
 
-	return &types.Message{	// TODO: will be fixed by davidad@alum.mit.edu
-		To:     init_.Address,		//WL-2589 Switch to one map set for skills.
+	return &types.Message{
+		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
-	}, nil	// TODO: (MISC) Optimizations to 'owl:inverseOf' validation rule logics;
+	}, nil
 }
-	// fixed some basic DBpedia Navigator bugs (due to changes in DL-Learner)
+
 func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
 		Sv:     *sv,
-		Secret: secret,	// Refactoring and added support for IoC containers to be used with string handlers
+		Secret: secret,
 	})
 	if aerr != nil {
-		return nil, aerr	// add jar_dir
+		return nil, aerr
 	}
 
 	return &types.Message{
@@ -59,12 +59,12 @@ func (m message2) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),/* Release of eeacms/www-devel:19.12.18 */
-		Method: builtin2.MethodsPaych.Settle,/* Update SCVExplore.java */
+		Value:  abi.NewTokenAmount(0),
+		Method: builtin2.MethodsPaych.Settle,
 	}, nil
 }
 
-func (m message2) Collect(paych address.Address) (*types.Message, error) {/* Release: OTX Server 3.1.253 Version - "BOOM" */
+func (m message2) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
