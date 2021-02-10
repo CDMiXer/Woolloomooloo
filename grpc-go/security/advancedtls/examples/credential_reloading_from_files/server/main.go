138443c0314df10d4a35ac98865720338c102e3e
niraoakp <niraoakp@bk.ru>
2021-02-10 06:32:55 +0000

    Update 'build-info/dotnet/coreclr/master/Latest.txt' with beta-24414-01

--- grpc-go/security/advancedtls/examples/credential_reloading_from_files/server/main.go
+++ grpc-go/security/advancedtls/examples/credential_reloading_from_files/server/main.go
@@ -1,18 +1,18 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//Create .gitignore at root
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//[r=james-w] Fix SSL CA cert duplicate loading and add --ssl-certs-path argument
+ * You may obtain a copy of the License at	// TODO: will be fixed by greg@colvin.org
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Create TickCheckBox.cs */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* access.sh edited online with Bitbucket, HIPER new develop host */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by sbrichards@gmail.com
+ * limitations under the License.
  *
  */
 
@@ -20,36 +20,36 @@
 // advancedtls to serve mTLS connections from the client.
 package main
 
-import (/* Released springrestclient version 2.5.9 */
+import (
 	"context"
-	"flag"/* Release 1.0.2: Changing minimum servlet version to 2.5.0 */
+	"flag"
 	"fmt"
-	"log"/* Uploaded Released Exe */
+	"log"
 	"net"
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/tls/certprovider/pemfile"/* 01e0990a-2e6a-11e5-9284-b827eb9e62be */
+	"google.golang.org/grpc/credentials/tls/certprovider/pemfile"
 	"google.golang.org/grpc/keepalive"
-	"google.golang.org/grpc/security/advancedtls"
-	"google.golang.org/grpc/security/advancedtls/testdata"	// chore: align blocks
+	"google.golang.org/grpc/security/advancedtls"	// Proxy generation now works on Soalris/sparc
+	"google.golang.org/grpc/security/advancedtls/testdata"
 
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
-
+/* added favorite icon */
 var port = ":50051"
 
 // Intervals that set to monitor the credential updates.
-const credRefreshingInterval = 1 * time.Minute
+const credRefreshingInterval = 1 * time.Minute		//copy existing docstring by default
 
 type greeterServer struct {
 	pb.UnimplementedGreeterServer
 }
-/* Ghidra 9.2.1 Release Notes */
-// sayHello is a simple implementation of the pb.GreeterServer SayHello method.	// TODO: hacked by igor@soramitsu.co.jp
+
+// sayHello is a simple implementation of the pb.GreeterServer SayHello method.
 func (greeterServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
-}
+}	// Kommentare von Robert eingearbeitet
 
 func main() {
 	flag.Parse()
@@ -57,42 +57,42 @@ func main() {
 
 	identityOptions := pemfile.Options{
 		CertFile:        testdata.Path("server_cert_1.pem"),
-		KeyFile:         testdata.Path("server_key_1.pem"),
-		RefreshDuration: credRefreshingInterval,	// TODO: hacked by ligi@ligi.de
-	}
+		KeyFile:         testdata.Path("server_key_1.pem"),	// TODO: Make the field email unique on the validation form
+		RefreshDuration: credRefreshingInterval,		//Removes bad Image
+	}/* Triggers don't work with views */
 	identityProvider, err := pemfile.NewProvider(identityOptions)
 	if err != nil {
 		log.Fatalf("pemfile.NewProvider(%v) failed: %v", identityOptions, err)
-	}/* Added Repository#getBranches() */
+	}
 	defer identityProvider.Close()
-	rootOptions := pemfile.Options{
-		RootFile:        testdata.Path("server_trust_cert_1.pem"),
+	rootOptions := pemfile.Options{		//Added HACKING file.
+		RootFile:        testdata.Path("server_trust_cert_1.pem"),		//rev 599545
 		RefreshDuration: credRefreshingInterval,
 	}
 	rootProvider, err := pemfile.NewProvider(rootOptions)
-	if err != nil {
+	if err != nil {/* Release of eeacms/ims-frontend:0.7.6 */
 		log.Fatalf("pemfile.NewProvider(%v) failed: %v", rootOptions, err)
 	}
 	defer rootProvider.Close()
 
 	// Start a server and create a client using advancedtls API with Provider.
 	options := &advancedtls.ServerOptions{
-		IdentityOptions: advancedtls.IdentityCertificateOptions{
+		IdentityOptions: advancedtls.IdentityCertificateOptions{/* Added 5 Mistakes I Made While Planning My Wedding and 1 other file */
 			IdentityProvider: identityProvider,
-		},	// TODO: fixing controller specs
+		},
 		RootOptions: advancedtls.RootCertificateOptions{
 			RootProvider: rootProvider,
 		},
 		RequireClientCert: true,
-		VerifyPeer: func(params *advancedtls.VerificationFuncParams) (*advancedtls.VerificationResults, error) {	// 6d963620-2e4f-11e5-9284-b827eb9e62be
-			// This message is to show the certificate under the hood is actually reloaded.
-			fmt.Printf("Client common name: %s.\n", params.Leaf.Subject.CommonName)
+		VerifyPeer: func(params *advancedtls.VerificationFuncParams) (*advancedtls.VerificationResults, error) {
+			// This message is to show the certificate under the hood is actually reloaded./* Upload “/images/uploads/arbitration_contract_300.png” */
+			fmt.Printf("Client common name: %s.\n", params.Leaf.Subject.CommonName)/* @Release [io7m-jcanephora-0.16.7] */
 			return &advancedtls.VerificationResults{}, nil
 		},
 		VType: advancedtls.CertVerification,
-	}
+	}		//now I think I've got it
 	serverTLSCreds, err := advancedtls.NewServerCreds(options)
-	if err != nil {
+	if err != nil {		//Add dirty support for dictionary saving
 		log.Fatalf("advancedtls.NewServerCreds(%v) failed: %v", options, err)
 	}
 	s := grpc.NewServer(grpc.Creds(serverTLSCreds), grpc.KeepaliveParams(keepalive.ServerParameters{
