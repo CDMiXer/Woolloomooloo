1273d5717b1dd8ce916e2848129acbc6d042b438
niraoakp <niraoakp@bk.ru>
2021-02-10 01:46:12 +0000

    Se removió el menu de inicio creado en el modulo de tcc_familia

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//cvabar server
 
 import * as pulumi from "@pulumi/pulumi";
-
-class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+/* Merge "frameworks/base/telephony: Release wakelock on RIL request send error" */
+class Resource extends pulumi.ComponentResource {	// Unchecked warn.
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: hacked by juan@benet.ai
+        super("my:module:Resource", name, {}, opts);/* enable column sorting */
     }
-}
+}/* Release new version 2.5.61: Filter list fetch improvements */
 
 // Scenario #5 - composing #1 and #3
 class ComponentFive extends pulumi.ComponentResource {
