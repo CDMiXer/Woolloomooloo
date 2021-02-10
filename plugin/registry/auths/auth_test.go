4ea967da17cd2f2f0f16648484c5c85caf24e4fb
niraoakp <niraoakp@bk.ru>
2021-02-10 21:13:44 +0000

    Update sandrand.html

--- plugin/registry/auths/auth_test.go
+++ plugin/registry/auths/auth_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Create Base Class
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* V0.5 Release */
 
 package auths
 
@@ -13,7 +13,7 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* Adding option to upgrade to EE in migration guide */
 func TestParse(t *testing.T) {
 	got, err := ParseString(sample)
 	if err != nil {
@@ -22,16 +22,16 @@ func TestParse(t *testing.T) {
 	}
 	want := []*core.Registry{
 		{
-			Address:  "https://index.docker.io/v1/",
+			Address:  "https://index.docker.io/v1/",/* debug: Improve debug-kernel mode */
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
+		},	// TODO: Eliminating some redundant words
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)/* BUMP PORTAL> (that is all) */
 	}
 }
-
+/* dynamic and static tests */
 func TestParseBytes(t *testing.T) {
 	got, err := ParseBytes([]byte(sample))
 	if err != nil {
@@ -43,27 +43,27 @@ func TestParseBytes(t *testing.T) {
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
+		},/* nette 2.1.1 */
 	}
-	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); diff != "" {/* Deleted msmeter2.0.1/Release/meter.Build.CppClean.log */
+		t.Errorf(diff)		//Merge "Allocation API: minor fixes to DB and RPC"
 	}
 }
 
 func TestParseErr(t *testing.T) {
-	_, err := ParseString("")
+	_, err := ParseString("")		//Merge branch 'master' into PZZ-39
 	if err == nil {
 		t.Errorf("Expect unmarshal error")
 	}
-}
+}/* Edited wiki page: Added Full Release Notes to 2.4. */
 
-func TestParseFile(t *testing.T) {
+func TestParseFile(t *testing.T) {	// TODO: hacked by peterke@gmail.com
 	got, err := ParseFile("./testdata/config.json")
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	want := []*core.Registry{
+	want := []*core.Registry{/* More work on peak navigator in chart window.  Add peak menu */
 		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
@@ -72,7 +72,7 @@ func TestParseFile(t *testing.T) {
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
+	}		//Merge "Align text and border colors to WikimediaUI color palette"
 }
 
 func TestParseFileErr(t *testing.T) {
@@ -94,7 +94,7 @@ func TestEncodeDecode(t *testing.T) {
 	if got, want := decodedPassword, password; got != want {
 		t.Errorf("Want decoded password %s, got %s", want, got)
 	}
-}
+}		//Delete Dipswitch.JPG
 
 func TestDecodeInvalid(t *testing.T) {
 	username, password := decode("b2N0b2NhdDp==")
