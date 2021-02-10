0c2c098e58fd7876ec03ca285f750ce38be4e6fa
niraoakp <niraoakp@bk.ru>
2021-02-10 08:23:46 +0000

    177abee8-2e5c-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* Fix Release build */
+/* Release notes for v8.0 */
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):/* typo in ReleaseController */
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive(ComponentResource):
-    def __init__(self, name, opts=None):
+class ComponentFive(ComponentResource):/* Release version 0.7 */
+    def __init__(self, name, opts=None):		//Adds Gitter links and documentation
         super().__init__("my:module:ComponentFive", name, None, opts)
-        res1 = Resource1("otherchildrenamed", ResourceOptions(		//Even distribution of RawDataFiles to sub tasks
-            parent=self,
+        res1 = Resource1("otherchildrenamed", ResourceOptions(
+            parent=self,	// Fixing issue with nested container element from the clipboard lists.
             aliases=[Alias(name="otherchild", parent=self)]))
 
 comp5 = ComponentFive("newcomp5", ResourceOptions(
