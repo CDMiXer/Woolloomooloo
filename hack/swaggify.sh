954804046cdf3f4530a4656144e47ff915f162ae
niraoakp <niraoakp@bk.ru>
2021-02-10 20:20:16 +0000

    Rewrite biicode build script in Python and simplify

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -3,13 +3,13 @@ set -eu -o pipefail
 
 # order is important, "REPLACEME" -> "workflow"
 cat \
-    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Delete ch.erl */
+    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \	// Teleport is working now again.
+    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \		//Test for dispose damaged book
     | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Release of 1.1-rc1 */
-    | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \	// add link to github url
-    | sed 's/k8s.io./io.k8s./'
+    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
+    | sed 's/k8s.io./io.k8s./'	// TODO: will be fixed by hello@brooklynzelenka.com
