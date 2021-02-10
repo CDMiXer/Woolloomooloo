1f277c341e128dcbf84cf922d224e9955c77b244
niraoakp <niraoakp@bk.ru>
2021-02-10 11:20:35 +0000

    Update min Node version

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
-"use strict";/* Merge branch 'master' into correcciones */
+"use strict";
 const pulumi = require("@pulumi/pulumi");
 const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
