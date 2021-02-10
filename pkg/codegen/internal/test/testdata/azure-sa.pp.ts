162ab700529c8d3e964f91855e5198f6891e7a7a
niraoakp <niraoakp@bk.ru>
2021-02-10 04:15:56 +0000

    Remove unused maven properties

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-	// Update yml format.
-const config = new pulumi.Config();
+
+const config = new pulumi.Config();		//Merge "Add a few missing asserts" into mnc-dev
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
-});	// TODO: update to jinja2 to resolve security issues
+});
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
-    name: storageAccountNameParam,
+    name: storageAccountNameParam,/* Corrected getPaymentMethod() URL */
     accountKind: "StorageV2",
     location: locationParam,
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
     accountReplicationType: storageAccountTypeReplicationParam,
-});/* Merge "[bugfix] Test for existing users for wikipedia family only" */
+});
 export const storageAccountNameOut = storageAccountResource.name;
