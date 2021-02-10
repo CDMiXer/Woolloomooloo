42c7a2f6230f567356c26d6e504dee856873d7bb
niraoakp <niraoakp@bk.ru>
2021-02-10 03:54:30 +0000

    Remove unneeded logging statement.

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Added IPOPP-station-infoflow.png
+
 import { Resource } from "./resource";
 
 // Attempt to delete the resource; this will fail!
