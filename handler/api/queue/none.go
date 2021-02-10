d06ef8ea57a7b8673f42e69d50b240af922fac90
niraoakp <niraoakp@bk.ru>
2021-02-10 08:04:06 +0000

    Add Philio PAN08

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* chore: normalize comments */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,31 +8,31 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: get rid of even more warnings
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package queue
-
+package queue		//Liens en markdown
+	// Update terminalManagement
 import (
-	"net/http"
+	"net/http"/* 0ae037c0-2e6c-11e5-9284-b827eb9e62be */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Merge branch 'next' into patch-3
 	"github.com/drone/drone/handler/api/render"
-)
+)		//Update to v1.4.1
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-
+		//changed up generator
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandlePause(core.Scheduler) http.HandlerFunc {
-	return notImplemented
+func HandlePause(core.Scheduler) http.HandlerFunc {	// TODO: Create sb_rwjs.gzip
+	return notImplemented		//Add in a simple handler for push, needs refinement
 }
 
 func HandleResume(core.Scheduler) http.HandlerFunc {
