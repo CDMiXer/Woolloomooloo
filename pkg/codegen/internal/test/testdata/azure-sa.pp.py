61a4035f5fcb4ed114212a9efd259df9edea12dd
niraoakp <niraoakp@bk.ru>
2021-02-10 17:01:37 +0000

    Add guild dump to startup log

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -1,7 +1,7 @@
 import pulumi
 import pulumi_azure as azure
 
-config = pulumi.Config()
+config = pulumi.Config()/* Release of eeacms/www:18.12.19 */
 storage_account_name_param = config.require("storageAccountNameParam")
 resource_group_name_param = config.require("resourceGroupNameParam")
 resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
@@ -9,11 +9,11 @@ location_param = config.get("locationParam")
 if location_param is None:
     location_param = resource_group_var.location
 storage_account_tier_param = config.get("storageAccountTierParam")
-if storage_account_tier_param is None:
+if storage_account_tier_param is None:/* Release Cleanup */
     storage_account_tier_param = "Standard"
 storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
 if storage_account_type_replication_param is None:
-    storage_account_type_replication_param = "LRS"
+    storage_account_type_replication_param = "LRS"/* Release 0.35.5 */
 storage_account_resource = azure.storage.Account("storageAccountResource",
     name=storage_account_name_param,
     account_kind="StorageV2",
