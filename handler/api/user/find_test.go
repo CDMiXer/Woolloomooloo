decc6fa65d5119e76d4fa02e9203742f18d0c0a1
niraoakp <niraoakp@bk.ru>
2021-02-10 04:43:32 +0000

    Create Source-Code.md

--- handler/api/user/find_test.go
+++ handler/api/user/find_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by steven@stebalien.com
-// that can be found in the LICENSE file.		//* Changed locale of join course link
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package user
-/* Merge "Reincorporate autoincremented numbering for links without display text" */
-import (/* Merge branch 'develop' into 6.0-multijournal */
-	"encoding/json"
-	"net/http/httptest"
-	"testing"/* fixed sensio/generator-bundle to 2.1.x-dev */
 
-	"github.com/drone/drone/handler/api/request"/* Add Release Notes to README */
+import (
+	"encoding/json"	// TODO: hacked by igor@soramitsu.co.jp
+	"net/http/httptest"
+	"testing"
+
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
-	// TODO: Update pip from 19.1.1 to 19.2
-	"github.com/google/go-cmp/cmp"
-)
+/* Release v1.76 */
+	"github.com/google/go-cmp/cmp"/* Updated app version */
+)	// TODO: will be fixed by aeongrp@outlook.com
 
 func TestFind(t *testing.T) {
 	mockUser := &core.User{
@@ -22,19 +22,19 @@ func TestFind(t *testing.T) {
 	}
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/api/user", nil)
+	r := httptest.NewRequest("GET", "/api/user", nil)		//Fix haddock 'Module' label for Data.InputStream
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)	// TODO: Voice control
+	)
 
-	HandleFind()(w, r)	// Update algoliasearch-rails to version 1.24.1
+	HandleFind()(w, r)	// TODO: hacked by lexy8russo@outlook.com
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}		//Rename zsh_alias to zsh_aliases
-
-	got, want := &core.User{}, mockUser
-	json.NewDecoder(w.Body).Decode(got)	// 2 .JPG to .jpg
+	}
+		//clarified header in readme and updated taxonomy
+	got, want := &core.User{}, mockUser	// TODO: A little code simplification.
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
-}	// TODO: Create Exemplo8.10.cs
+	}		//add epoll tcp
+}
