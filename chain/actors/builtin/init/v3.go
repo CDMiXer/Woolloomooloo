0e29de345f1417d311ac843c06fdadcd7946a9ed
niraoakp <niraoakp@bk.ru>
2021-02-10 18:37:14 +0000

    NetKAN added mod - SemiSaturatableRW-2-2.1.2

--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -1,18 +1,18 @@
 package init
 
-import (		//paslaugų valdymas
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* [artifactory-release] Release version 1.2.1.RELEASE */
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-/* fixed posting in sidebar */
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"	// updated to properly position the activity indicator before the text
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
@@ -20,14 +20,14 @@ var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)	// TODO: Merge "layout/tripleo: run upgrade jobs on puppet-tripleo"
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}/* Release 1.18.0 */
+}
 
-type state3 struct {/* Released 1.1.1 with a fixed MANIFEST.MF. */
+type state3 struct {
 	init3.State
 	store adt.Store
 }
@@ -35,25 +35,25 @@ type state3 struct {/* Released 1.1.1 with a fixed MANIFEST.MF. */
 func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
-	// TODO: will be fixed by mikeal.rogers@gmail.com
+
 func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
 
 func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
-	if err != nil {/* Release 2.6.1 (close #13) */
-		return err	// TODO: will be fixed by arachnid@notdot.net
+	if err != nil {
+		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err	// TODO: will be fixed by arachnid@notdot.net
-		}	// Improved the example for artifact usage
+			return err
+		}
 		return cb(abi.ActorID(actorID), addr)
-	})/* chore(package): update fastify to version 0.27.0 */
-}/* Release of eeacms/forests-frontend:2.0-beta.44 */
+	})
+}
 
 func (s *state3) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
@@ -64,7 +64,7 @@ func (s *state3) SetNetworkName(name string) error {
 	return nil
 }
 
-func (s *state3) Remove(addrs ...address.Address) (err error) {		//Rename DragonKiller to DragonKiller.java
+func (s *state3) Remove(addrs ...address.Address) (err error) {
 	m, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
 		return err
@@ -73,7 +73,7 @@ func (s *state3) Remove(addrs ...address.Address) (err error) {		//Rename Dragon
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
-	}/* final code review amend */
+	}
 	amr, err := m.Root()
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
