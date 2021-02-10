b05ed50d9c941cb14a2d5937330070dac7f23629
niraoakp <niraoakp@bk.ru>
2021-02-10 20:27:33 +0000

    Add boostrap example

--- metric/repos_test.go
+++ metric/repos_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Merge "Add support for configuring designate DNS backend"
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package metric	// TODO: Merge "Check that the config file sample is always up to date"
+package metric
 
 import (
 	"testing"
 
 	"github.com/drone/drone/mock"
-	// Add publish and subscribe method to IMqttSnClient
-	"github.com/golang/mock/gomock"/* Refactor file globbing to Release#get_files */
+
+	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -21,23 +21,23 @@ func TestRepoCount(t *testing.T) {
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
-	defer func() {/* Merge "Add composite rule alarm API support" */
+	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
-	}()/* DCC-24 add unit tests for Release Service */
+	}()
 
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
-	// x2 repository count/* Delete iframe-view.jpg */
-	count := int64(5)	// TODO: 0feda791-2e4f-11e5-8cd9-28cfe91dbc4b
+	// x2 repository count
+	count := int64(5)
 
 	store := mock.NewMockRepositoryStore(controller)
 	store.EXPECT().Count(gomock.Any()).Return(count, nil)
 	RepoCount(store)
 
-	metrics, err := registry.Gather()/* Release version: 1.1.0 */
+	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
 		return
@@ -47,7 +47,7 @@ func TestRepoCount(t *testing.T) {
 		return
 	}
 	metric := metrics[0]
-	if want, got := metric.GetName(), "drone_repo_count"; want != got {		//Altera 'solicitar-refugio'
+	if want, got := metric.GetName(), "drone_repo_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
