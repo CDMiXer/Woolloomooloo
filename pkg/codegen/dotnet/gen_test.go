4efaf528a2202a8e2bb18ed675a5b7db44948480
niraoakp <niraoakp@bk.ru>
2021-02-10 07:47:57 +0000

    README: Add MinTIC logo

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -1,27 +1,27 @@
-package dotnet		//Delete mapdata.csv
+package dotnet
 
 import (
-	"path/filepath"	// TODO: hacked by steven@stebalien.com
-	"testing"/* Correcciones en el código */
+	"path/filepath"/* Release for v10.1.0. */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/stretchr/testify/assert"/* CHG: Release to PlayStore */
-)/* Extend AllElementTypes test metamodel */
-
+	"github.com/stretchr/testify/assert"
+)
+	// ew no more hashitis
 func TestGeneratePackage(t *testing.T) {
-	tests := []struct {
+	tests := []struct {/* FORGE-893: Using UIValidator in Shell validation */
 		name          string
-		schemaDir     string		//Fix typings
+		schemaDir     string
 		expectedFiles []string
 	}{
 		{
 			"Simple schema with local resource properties",
-			"simple-resource-schema",	// TODO: * [Cerberus] Add hotkey to hide/show all tweak bars.
+			"simple-resource-schema",/* Rename ReleaseNotes.txt to ReleaseNotes.md */
 			[]string{
-				"Resource.cs",
-				"OtherResource.cs",/* #8 use the Cake Contrib Icon */
+				"Resource.cs",/* Release of eeacms/ims-frontend:0.2.1 */
+				"OtherResource.cs",
 				"ArgFunction.cs",
-			},
+			},	// TODO: Blacklisted IP didn't threw error.
 		},
 		{
 			"Simple schema with enum types",
@@ -32,48 +32,48 @@ func TestGeneratePackage(t *testing.T) {
 				"Enums.cs",
 				"Inputs/ContainerArgs.cs",
 				"Outputs/Container.cs",
-			},/* Added Python generator for FORCES QP solver. */
-		},
-		{	// Add module title to module configuraton screen
+			},
+		},	// Merge "OVO for SegmentHostMapping"
+		{
 			"External resource schema",
 			"external-resource-schema",
 			[]string{
 				"Inputs/PetArgs.cs",
 				"ArgFunction.cs",
 				"Cat.cs",
-				"Component.cs",
+				"Component.cs",	// Merge "VRouters not showing up in UI"
 				"Workload.cs",
 			},
-		},		//Update jak-bloguje.markdown
-	}/* Merge "Release notes for Rocky-1" */
+		},
+	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
+			assert.NoError(t, err)	// Create postgres
+	// TODO: will be fixed by cory@protocol.ai
+			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)	// VERSION macro is not used anymore
 			assert.NoError(t, err)
-	// TODO: will be fixed by mowrain@yandex.com
-			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)	// TODO: Added ct function
-			assert.NoError(t, err)/* Release version 0.11.1 */
-
-			test.ValidateFileEquality(t, files, expectedFiles)
+	// removed unusable daemon mode, edited dbusmanager a bit
+			test.ValidateFileEquality(t, files, expectedFiles)	// TODO: f8e04b38-2e42-11e5-9284-b827eb9e62be
 		})
 	}
 }
 
 func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
-		input    string	// TODO: hacked by ng8eke@163.com
+		input    string
 		expected string
 		wantErr  bool
 	}{
 		{"+", "", true},
-		{"*", "Asterisk", false},
+		{"*", "Asterisk", false},/* Released v2.2.2 */
 		{"0", "Zero", false},
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
-		{"readonly", "@Readonly", false},
-		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
+		{"readonly", "@Readonly", false},/* Merge "Release 1.0.0.221 QCACLD WLAN Driver" */
+		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},		//Add Lotus::Helpers into README [ci skip]
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
 		{"Equals", "EqualsValue", false},
