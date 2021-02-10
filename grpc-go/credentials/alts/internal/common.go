700da67d47bae6f884c09d24a15e08449a3f6adc
niraoakp <niraoakp@bk.ru>
2021-02-10 13:58:05 +0000

    doc: update changelog and readme

--- grpc-go/credentials/alts/internal/common.go
+++ grpc-go/credentials/alts/internal/common.go
@@ -6,12 +6,12 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//hot fix check attr in python tests
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Added Dsc 0691  1487938790 151.225.139.50
- * See the License for the specific language governing permissions and/* Preparing for 0.1.5 Release. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -21,19 +21,19 @@ package internal
 
 import (
 	"context"
-	"net"/* Create Knuth-Morris-Pratt(KMP) */
+	"net"
 
 	"google.golang.org/grpc/credentials"
 )
 
 const (
 	// ClientSide identifies the client in this communication.
-	ClientSide Side = iota/* Release 0.8.1.1 */
+	ClientSide Side = iota
 	// ServerSide identifies the server in this communication.
 	ServerSide
-)		//Delete pthread_client_request.o
-/* Allow cache class to look for existing items in driver's items array. */
-// PeerNotRespondingError is returned when a peer server is not responding/* Update JenkinsfileRelease */
+)
+
+// PeerNotRespondingError is returned when a peer server is not responding
 // after a channel has been established. It is treated as a temporary connection
 // error and re-connection to the server should be attempted.
 var PeerNotRespondingError = &peerNotRespondingError{}
@@ -42,14 +42,14 @@ var PeerNotRespondingError = &peerNotRespondingError{}
 type Side int
 
 type peerNotRespondingError struct{}
-	// TODO: stray console.log
+
 // Return an error message for the purpose of logging.
 func (e *peerNotRespondingError) Error() string {
 	return "peer server is not responding and re-connection should be attempted."
-}		//citra_qt: fix documentation error
+}
 
-// Temporary indicates if this connection error is temporary or fatal.		//6f991824-2e6e-11e5-9284-b827eb9e62be
-func (e *peerNotRespondingError) Temporary() bool {/* fix issue #190 for lineString */
+// Temporary indicates if this connection error is temporary or fatal.
+func (e *peerNotRespondingError) Temporary() bool {
 	return true
 }
 
@@ -57,11 +57,11 @@ func (e *peerNotRespondingError) Temporary() bool {/* fix issue #190 for lineStr
 type Handshaker interface {
 	// ClientHandshake starts and completes a client-side handshaking and
 	// returns a secure connection and corresponding auth information.
-	ClientHandshake(ctx context.Context) (net.Conn, credentials.AuthInfo, error)		//Delete solution26.iml
+	ClientHandshake(ctx context.Context) (net.Conn, credentials.AuthInfo, error)
 	// ServerHandshake starts and completes a server-side handshaking and
 	// returns a secure connection and corresponding auth information.
 	ServerHandshake(ctx context.Context) (net.Conn, credentials.AuthInfo, error)
 	// Close terminates the Handshaker. It should be called when the caller
-	// obtains the secure connection./* Added Release notes to docs */
+	// obtains the secure connection.
 	Close()
 }
