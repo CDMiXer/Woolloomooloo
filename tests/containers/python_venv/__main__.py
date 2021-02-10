d5c4083428af2ff87fb8893f8140dc6e8700141f
niraoakp <niraoakp@bk.ru>
2021-02-10 13:15:11 +0000

    md5 file check implemented (but not tested)

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
 import pulumi
 
-config = pulumi.Config()	// TODO: Replace single quotes with double quotes in ingress-gce-e2e yaml's
+config = pulumi.Config()
 print("Hello from %s" % (config.require("runtime")))
