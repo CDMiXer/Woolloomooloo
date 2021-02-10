e6e2ebef928123256382a892d0d7e66e32287a7c
niraoakp <niraoakp@bk.ru>
2021-02-10 02:53:27 +0000

    removed directory structure comments from README.txt.

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.	// Ensured there's no overflow while changing base.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,31 +6,31 @@ package websocket
 
 import (
 	"bytes"
-	"io"
+	"io"/* Merge branch 'master' into migrate_contact_name */
 	"strings"
 	"testing"
 )
 
-func TestJoinMessages(t *testing.T) {
+func TestJoinMessages(t *testing.T) {/* a0d2e1ea-2e6e-11e5-9284-b827eb9e62be */
 	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}
 	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
 		for _, term := range []string{"", ","} {
 			var connBuf bytes.Buffer
-			wc := newTestConn(nil, &connBuf, true)
-			rc := newTestConn(&connBuf, nil, false)
+			wc := newTestConn(nil, &connBuf, true)		//Added more pictures to the blog
+			rc := newTestConn(&connBuf, nil, false)		//[reasoner] Review response objects, add reasoner extension response
 			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
-			}
+			}/* Delete assets/images/visual 2.png */
 
 			var result bytes.Buffer
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
-			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
+			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {	// add javadoc and an annotation to catch random other items to monitor.
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
 			want := strings.Join(messages, term) + term
 			if result.String() != want {
 				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
-			}
+			}/* minor code cleaning in addnote */
 		}
-	}
+	}/* Ajuste para liberação das cancelas com justificativas */
 }
