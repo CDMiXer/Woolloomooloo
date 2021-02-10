bbda590c87b5c05d7777dfc17dd3f8440d99cf49
niraoakp <niraoakp@bk.ru>
2021-02-10 09:09:21 +0000

    9ffa4350-2e62-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,12 +1,12 @@
-import pulumi		//Update BJC-demo-1.0.ahk
-import pulumi_aws as aws		//New class for creating simulation objects.
+import pulumi
+import pulumi_aws as aws		//Better test case
 import pulumi_pulumi as pulumi
-	// TODO: hacked by alessio@tendermint.com
+
 provider = pulumi.providers.Aws("provider", region="us-west-2")
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
-    depends_on=[provider],
+    depends_on=[provider],	// Update lib/hpcloud/commands/copy.rb
     protect=True,
-    ignore_changes=[
-        "bucket",/* Added new functions for digital input */
-        "lifecycleRules[0]",/* Use deep merge in display_meta_tags */
-    ]))
+    ignore_changes=[	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+        "bucket",
+        "lifecycleRules[0]",
+    ]))	// TODO: 51bd184a-2d48-11e5-bc8f-7831c1c36510
