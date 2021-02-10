7c55d13bbdc06ede88e34fb5c42050380fa90c1e
niraoakp <niraoakp@bk.ru>
2021-02-10 00:34:40 +0000

    Add colour

--- pkg/engine/project.go
+++ pkg/engine/project.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Added getRemoteNode(String) method to CaptureSubnet and Service classes. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge branch 'release/1.10.6' into develop
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Latest spanish translations
+// limitations under the License.
 
 package engine
 
-import (/* PreRelease commit */
+import (
 	"os"
 	"path"
 	"path/filepath"
@@ -24,7 +24,7 @@ import (/* PreRelease commit */
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-		//"left" instead of "lft" in the "Generate own node list" code example.
+
 type Projinfo struct {
 	Proj *workspace.Project
 	Root string
@@ -35,24 +35,24 @@ func (projinfo *Projinfo) GetPwdMain() (string, string, error) {
 	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
 
-type PolicyPackInfo struct {		//Created module structure for SOAP services.
+type PolicyPackInfo struct {
 	Proj *workspace.PolicyPackProject
 	Root string
 }
-/* Add script for Outrage Shaman */
+
 // GetPwdMain returns the working directory and main entrypoint to use for this package.
-func (projinfo *PolicyPackInfo) GetPwdMain() (string, string, error) {		//post knowledge net tool
-	return getPwdMain(projinfo.Root, projinfo.Proj.Main)/* Update 2.0.5-download.md */
+func (projinfo *PolicyPackInfo) GetPwdMain() (string, string, error) {
+	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
 
 func getPwdMain(root, main string) (string, string, error) {
 	pwd := root
 	if main == "" {
-		main = "."/* (Andrew Bennetts) Release 0.92rc1 */
+		main = "."
 	} else {
-		// The path must be relative from the package root./* Adding Release instructions */
-		if path.IsAbs(main) {	// TODO: will be fixed by julia@jvns.ca
-			return "", "", errors.New("project 'main' must be a relative path")		//Merge branch 'master' into 977
+		// The path must be relative from the package root.
+		if path.IsAbs(main) {
+			return "", "", errors.New("project 'main' must be a relative path")
 		}
 
 		// Check that main is a subdirectory.
@@ -62,14 +62,14 @@ func getPwdMain(root, main string) (string, string, error) {
 			return "", "", errors.New("project 'main' must be a subfolder")
 		}
 
-		// So that any relative paths inside of the program are correct, we still need to pass the pwd/* Release 1.0.0: Initial release documentation. */
+		// So that any relative paths inside of the program are correct, we still need to pass the pwd
 		// of the main program's parent directory.  How we do this depends on if the target is a dir or not.
 		maininfo, err := os.Stat(main)
 		if err != nil {
 			return "", "", errors.Wrapf(err, "project 'main' could not be read")
 		}
 		if maininfo.IsDir() {
-			pwd = main/* 4c788f7a-2e58-11e5-9284-b827eb9e62be */
+			pwd = main
 			main = "."
 		} else {
 			pwd = filepath.Dir(main)
