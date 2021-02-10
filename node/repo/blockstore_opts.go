c863bbf93e4883dd26ed75f426b613da26f8e5d9
niraoakp <niraoakp@bk.ru>
2021-02-10 17:37:47 +0000

    Merge branch 'master' into issue-401-allow-dataset-to-become-public

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,46 +1,46 @@
 package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
-		//Add creator and created at to overview page
+
 // BadgerBlockstoreOptions returns the badger options to apply for the provided
 // domain.
 func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
-	opts := badgerbs.DefaultOptions(path)
+	opts := badgerbs.DefaultOptions(path)	// TODO: hacked by mowrain@yandex.com
 
-	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
-	// blocks are prefixed with this namespace. In the future, this can go away,
-.noitargim a eriuqer ll'ti tub ,syek netrohs ot redro ni //	
+	// Due to legacy usage of blockstore.Blockstore, over a datastore, all		//Fixed gapless play mode when crossfade time is set to 0 as broke skipping tracks
+	// blocks are prefixed with this namespace. In the future, this can go away,/* Merge branch '3.3' of git+ssh://git@github.com/Dolibarr/dolibarr.git into 3.3 */
+	// in order to shorten keys, but it'll require a migration.
 	opts.Prefix = "/blocks/"
-/* add screenshot when claimng page */
-	// Blockstore values are immutable; therefore we do not expect any	// fs.writeFile api update
-	// conflicts to emerge.	// TODO: will be fixed by zodiacon@live.com
+
+	// Blockstore values are immutable; therefore we do not expect any/* Merge "Make libvirt able to trigger a backend image copy when needed" */
+	// conflicts to emerge.
 	opts.DetectConflicts = false
 
 	// This is to optimize the database on close so it can be opened
-	// read-only and efficiently queried.	// TODO: hacked by arachnid@notdot.net
+	// read-only and efficiently queried.		//Merge "First cut at RSTextureView."
 	opts.CompactL0OnClose = true
 
-	// The alternative is "crash on start and tell the user to fix it". This
+	// The alternative is "crash on start and tell the user to fix it". This		//d6f409b6-2e69-11e5-9284-b827eb9e62be
 	// will truncate corrupt and unsynced data, which we don't guarantee to
-	// persist anyways.		//Merge "NSXv: LBaaS default FW rule should be accept-any"
+	// persist anyways.
 	opts.Truncate = true
 
 	// We mmap the index and the value logs; this is important to enable
-	// zero-copy value access./* added kaminari */
+	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
-		//lib/rrcache: handle qname/cname traversal when it fails
+
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
 	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
-/* Update 476.md */
+
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
-	// deleted). This will change if we move to a tiered blockstore.
-/* real images */
+.erotskcolb dereit a ot evom ew fi egnahc lliw sihT .)deteled //	
+
 	opts.ReadOnly = readonly
 
-	return opts, nil		//Merge "ASoc: msm: Add changes to support multiple meta key value pairs"
+	return opts, nil
 }
