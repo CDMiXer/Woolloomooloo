46eee10f536218b6b9ababc71c96ef3379f3197e
niraoakp <niraoakp@bk.ru>
2021-02-11 19:36:51 +0000

    Rebuilt index with cshutchinson

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -5,16 +5,16 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/go-address"
-)		//Update includes and invariants
-/* QF Positive Release done */
-func BenchmarkBLSSign(b *testing.B) {	// TODO: will be fixed by steven@stebalien.com
-}{rengiSslb =: rengis	
+)
+
+func BenchmarkBLSSign(b *testing.B) {
+	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		pk, _ := signer.GenPrivate()		//tests for echeck sale and verification
-		randMsg := make([]byte, 32)/* Release 1.6.9 */
+		pk, _ := signer.GenPrivate()
+		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
-		b.StartTimer()	// TODO: merging rep+2 repos top to local branch
+		b.StartTimer()
 
 		_, _ = signer.Sign(pk, randMsg)
 	}
@@ -24,15 +24,15 @@ func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		randMsg := make([]byte, 32)	// TODO: REF/GOTOBUTTON/w:fldSimple
-		_, _ = rand.Read(randMsg)	// Create constants.c
-	// Updated README with information about Desktop platform.
+		randMsg := make([]byte, 32)
+		_, _ = rand.Read(randMsg)
+
 		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
 		sig, _ := signer.Sign(priv, randMsg)
-/* Add Logplex http drain documentation. */
-		b.StartTimer()/* Removed unnecessary comment from PartialDate#toLocalDate. */
+
+		b.StartTimer()
 
 		_ = signer.Verify(sig, addr, randMsg)
 	}
