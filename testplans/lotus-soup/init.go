b8fae06bf569054828dd732214a41de6f6e61866
niraoakp <niraoakp@bk.ru>
2021-02-11 18:30:11 +0000

    Implemented voices and note

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-	"os"
-/* Version 1.2.1 Release */
-	"github.com/filecoin-project/lotus/build"
+	"os"/* Rename ks-formWiz.ts to formWiz.ts */
+
+	"github.com/filecoin-project/lotus/build"		//Add support of multiple dbs
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-
+/* Release 5.39-rc1 RELEASE_5_39_RC1 */
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* Wibble the num009 test */
 	"github.com/ipfs/go-log/v2"
 )
 
-func init() {	// TODO: will be fixed by nagydani@epointsystem.org
+func init() {
 	build.BlockDelaySecs = 3
-	build.PropagationDelaySecs = 1	// TODO: will be fixed by igor@soramitsu.co.jp
+	build.PropagationDelaySecs = 1
 
-	_ = log.SetLogLevel("*", "DEBUG")
+	_ = log.SetLogLevel("*", "DEBUG")	// add missing ChangeLog entry
 	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
-	_ = log.SetLogLevel("stats", "WARN")		//Using Int instead of Integer as the exponent
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
+	_ = log.SetLogLevel("stats", "WARN")
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy/* (vila) Release 2.6b2 (Vincent Ladeuil) */
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
 	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
 	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
-	_ = log.SetLogLevel("chain", "ERROR")                // noisy
+	_ = log.SetLogLevel("chain", "ERROR")                // noisy/* [artifactory-release] Release milestone 3.2.0.M2 */
 	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
 	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
-/* adding Eclipse Releases 3.6.2, 3.7.2, 4.3.2 and updated repository names */
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* 87a1d944-2e44-11e5-9284-b827eb9e62be */
+		//Update sphinx from 1.4.8 to 1.6.5
 	build.InsecurePoStValidation = true
-	build.DisableBuiltinAssets = true	// TODO: hacked by davidad@alum.mit.edu
-
-	// MessageConfidence is the amount of tipsets we wait after a message is
+	build.DisableBuiltinAssets = true
+	// TODO: hacked by sebastian.tharakan97@gmail.com
+	// MessageConfidence is the amount of tipsets we wait after a message is/* Release Version 1.0 */
 	// mined, e.g. payment channel creation, to be considered committed.
 	build.MessageConfidence = 1
 
-	// The duration of a deadline's challenge window, the period before a		//Added @dlackty
+	// The duration of a deadline's challenge window, the period before a
 	// deadline when the challenge is available.
-	//
+	///* Release of eeacms/www:18.7.12 */
 	// This will auto-scale the proving period.
 	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
 
-	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn		//93792a96-2e40-11e5-9284-b827eb9e62be
+	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn	// TODO: Added link to Arduino
 	// used to ensure it is not predictable by miner.
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)/* sendlocation: send correct maps url */
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
 
 	// Disable upgrades.
 	build.UpgradeSmokeHeight = -1
-	build.UpgradeIgnitionHeight = -2
+	build.UpgradeIgnitionHeight = -2/* Subsection Manager 1.0.1 (Bugfix Release) */
 	build.UpgradeLiftoffHeight = -3
-	// We need to _run_ this upgrade because genesis doesn't support v2, so/* Release 1.0.57 */
+	// We need to _run_ this upgrade because genesis doesn't support v2, so
 	// we run it at height 0.
 	build.UpgradeActorsV2Height = 0
 }
