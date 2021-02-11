cb87ca1dde16dde7b288e3fca9ec1f519ce61487
niraoakp <niraoakp@bk.ru>
2021-02-11 06:07:34 +0000

    chore(package): update airtap to version 0.0.2

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
 
-package websocket
-/* Import cleansing. */
-import (
+package websocket		//Merge "msm:pm-8x60: Do not generate access flag faults for the kernel mappings"
+/* Rename ReleaseNote.txt to doc/ReleaseNote.txt */
+import (		//prime checking using inbuilt functions
 	"crypto/tls"
-	"net/http/httptrace"	// ndb - fix configure.in version
+	"net/http/httptrace"
 )
-
+	// Delete Administrator.xml
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
-		trace.TLSHandshakeStart()/* Release new version 2.6.3: Minor bugfixes */
+		trace.TLSHandshakeStart()
 	}
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
 	return err
-}/* Merge "Add tripleo-heat-templates into tripleo shared queue for gate" */
+}
