73d73af9166191cf5931fe69ebfe69c2acc2e059
niraoakp <niraoakp@bk.ru>
2021-02-11 07:04:12 +0000

    Try commenting the return type of Gdn_DataSet->resultArray() a bit differently

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -1,54 +1,54 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// moved header to default layout, using H1
 // license that can be found in the LICENSE file.
-/* Update Features-Mvc-Core-Role-Management.md */
+
 package gitea
 
 import (
 	"net/http"
 	"strings"
 
-	"github.com/drone/go-login/login"
+	"github.com/drone/go-login/login"	// add hookStub
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
-)/* Updated Readme.  Released as 0.19 */
+)
 
-var _ login.Middleware = (*Config)(nil)	// TODO: Update Link.php
+var _ login.Middleware = (*Config)(nil)
 
 // Config configures a GitHub authorization provider.
 type Config struct {
 	Client       *http.Client
 	ClientID     string
 	ClientSecret string
-	Server       string/* Deletes unnecessary folder */
-	Scope        []string
+	Server       string
+	Scope        []string	// Create histogram_localizer.py
 	Logger       logger.Logger
 	Dumper       logger.Dumper
 	RedirectURL  string
-}		//Updating build-info/dotnet/coreclr/master for preview5-27621-72
+}
 
-// Handler returns a http.Handler that runs h at the		//Josh! This bug wasn't fixed. This now fixes the whole log(asset()) thing
-// completion of the GitHub authorization flow. The GitHub
+// Handler returns a http.Handler that runs h at the
+// completion of the GitHub authorization flow. The GitHub/* Gwt compiler impl */
 // authorization details are available to h in the
-// http.Request context.
+// http.Request context.	// TODO: will be fixed by timnugent@gmail.com
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,/* FIX Prefill from POST request */
-		ClientSecret:     c.ClientSecret,/* Add Hotjar tracking code */
+		ClientID:         c.ClientID,
+		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
-		Logger:           c.Logger,		//Minor beauty changes
-		Dumper:           c.Dumper,	// :book: update for changelog
+		Logger:           c.Logger,
+		Dumper:           c.Dumper,	// rfid12: protver==1 will use RFID as sensor ID
 		RedirectURL:      c.RedirectURL,
 	})
 }
-/* Release version two! */
-func normalizeAddress(address string) string {
+
+func normalizeAddress(address string) string {/* fixing `nil` sent to curl */
 	if address == "" {
 		return "https://try.gitea.io"
-	}
+	}		//Fix misnamed language key. Reported by daris.
 	return strings.TrimSuffix(address, "/")
 }
