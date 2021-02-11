818b56f7536f422e8cbd4f5fc483a968912cd63c
niraoakp <niraoakp@bk.ru>
2021-02-11 19:03:15 +0000

    Fixed installer and force notations

--- handler/api/repos/secrets/find.go
+++ handler/api/repos/secrets/find.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: changed links
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* SS2: Updated Mission Manager for 151-160 Missions */
-
-package secrets		//Add updating CRM to sales process
+// +build !oss
+	// Fix userAdded
+package secrets
 
 import (
 	"net/http"
@@ -12,25 +12,25 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"/* Release version tag */
+	"github.com/go-chi/chi"
 )
-
+/* Released 0.1.5 version */
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // secret details to the the response body.
-func HandleFind(
+func HandleFind(	// TODO: Removed deprecated wx call
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by igor@soramitsu.co.jp
+			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by nick@perfectabstractions.com
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)		//OPT: text field styling
-			return		//CSS updates for UKBMS
+			render.NotFound(w, err)
+			return
 		}
 		result, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
