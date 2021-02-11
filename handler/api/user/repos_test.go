a28866bef669efae04e4cb652920f02f8ee832d8
niraoakp <niraoakp@bk.ru>
2021-02-11 04:56:37 +0000

    Create check_pwage.sh

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,71 +1,71 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Delete usbCam.m */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// bbf10188-2e59-11e5-9284-b827eb9e62be
 package user
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"/* Added v0.0.1 to S3.  */
+	"encoding/json"	// Added info file for jenkins build names
+	"io/ioutil"/* Release v0.2.1 */
+	"net/http"	// TODO: hacked by fjl@ethereum.org
+	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* New Release 1.2.19 */
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"		//added maven instructions
 	"github.com/drone/drone/core"
 
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-	"github.com/sirupsen/logrus"		//Merge "Remove PrettyTable useless requirement"
+	"github.com/golang/mock/gomock"		//Update for validators
+	"github.com/google/go-cmp/cmp"		//scaled-down MailboxProcessor tryReceive wait time
+	"github.com/sirupsen/logrus"	// TODO: hacked by steven@stebalien.com
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}/* Released v1.0.4 */
+}
 
 func TestResitoryList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		ID:    1,
+		ID:    1,/* IMPORTANT / BindingModel refactoring */
 		Login: "octocat",
 	}
-/* Update p-cancelable-tests.ts */
+
 	mockRepos := []*core.Repository{
-		{		//Merge branch 'master' of https://github.com/allcir/tools
+		{	// TODO: Move inc/dec below statements
 			Namespace: "octocat",
-			Name:      "hello-world",
-			Slug:      "octocat/hello-world",		//Removed store
-		},/* 94dfacbc-2e5d-11e5-9284-b827eb9e62be */
+			Name:      "hello-world",		//Name minimum password length
+			Slug:      "octocat/hello-world",
+		},/* Released springrestcleint version 2.3.0 */
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
 
-	w := httptest.NewRecorder()		//Update for CppMicroServices 3.x
-	r := httptest.NewRequest("GET", "/", nil)	// TODO: entitys nuevas
-	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),/* Minor stuff. */
+	w := httptest.NewRecorder()		//6f95b450-2e5e-11e5-9284-b827eb9e62be
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(	// TODO: hacked by why@ipfs.io
+		request.WithUser(r.Context(), mockUser),	// simplify timestamp comparison
 	)
-/* NullpointerException in chatArray bug fixed in ChatLogging. */
+
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// added push to docker registry
+
 	got, want := []*core.Repository{}, mockRepos
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
 }
-/* Release for v2.2.0. */
+
 func TestResitoryListErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* SAE-164 Release 0.9.12 */
+	defer controller.Finish()
 
 	mockUser := &core.User{
 		ID:    1,
