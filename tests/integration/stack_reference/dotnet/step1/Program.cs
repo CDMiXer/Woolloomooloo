a6cbac3ded6e84b979b7103d5dec7564a19b6c54
niraoakp <niraoakp@bk.ru>
2021-02-11 03:27:35 +0000

    tried fixing

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -12,18 +12,18 @@ class Program
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
-            var org = config.Require("org");
+            var org = config.Require("org");/* Refactor service-conf with standard pattern */
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var a = new StackReference(slug);
+            var a = new StackReference(slug);/* Release of eeacms/www:18.5.2 */
 
             var oldVal = (string[])await a.GetValueAsync("val");
             if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
-            {
+            {		//fix(theme): Removed SASS import
                 throw new Exception("Invalid result");
             }
 
             return new Dictionary<string, object>
-            {
+            {	// TODO: Updated Feinstein Empty Chair Town Hall
                 { "val2", Output.CreateSecret(new[] { "a", "b" }) }
             };
         });
