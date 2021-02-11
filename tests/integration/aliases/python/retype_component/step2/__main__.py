b38fdac882d9879ee47733b4194ca6432ce6ffba
niraoakp <niraoakp@bk.ru>
2021-02-11 18:20:44 +0000

    Delete C++20.h

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,14 +1,14 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import copy	// TODO: will be fixed by aeongrp@outlook.com
+import copy
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// Merge "Wait the wsrep_ready to be ON in mariadb"
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):/* ARMv5 bot in Release mode */
         super().__init__("my:module:Resource", name, None, opts)
-		//Create .hrconcept
-# Scenario #4 - change the type of a component/* Remove forward slashes from Font Paths */
+
+# Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
     def __init__(self, name, opts=ResourceOptions()):
         # Add an alias that references the old type of this resource...
@@ -17,13 +17,13 @@ class ComponentFour(ComponentResource):
             for alias in opts.aliases:
                 aliases.append(alias)
 
-        # ..and then make the super call with the new type of this resource and the added alias./* STL: Slice editor is using consts (not hardcoded anymore) */
+        # ..and then make the super call with the new type of this resource and the added alias.
         opts_copy = copy.copy(opts)
-        opts_copy.aliases = aliases
+        opts_copy.aliases = aliases/* Added migrate Pending transactions function */
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
 
-        # The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        # to.		//Fixed compilation on windows.
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))
+        # The child resource will also pick up an implicit alias due to the new type of the component it is parented/* Create EasyDB class */
+        # to.
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))		//Merge branch 'master' into newsphinxwarnings
 
 comp4 = ComponentFour("comp4")
