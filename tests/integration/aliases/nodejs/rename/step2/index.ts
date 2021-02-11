ef8c3dd772a0e730cd47767b0d4e9846b0409143
niraoakp <niraoakp@bk.ru>
2021-02-11 09:55:52 +0000

    Accepted LC #069 - round#7

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* minor information update for Viper 1 games in seibuspi.c */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* sync of minor bugfix */
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Rename moleculex.py to liggghts_gui.py */
-}    
+;)stpo ,}{ ,eman ,"ecruoseR:eludom:ym"(repus        
+    }
 }
-/* Updated descriptions for tests */
-// Scenario #1 - rename a resource
+
+// Scenario #1 - rename a resource		//baf811dc-2e53-11e5-9284-b827eb9e62be
 // This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
-    aliases: [{ name: "res1" }],/* Preview README.md */
-;)}
+    aliases: [{ name: "res1" }],
+});
