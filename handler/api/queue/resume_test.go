e6bf32f5522b3f4aa9af6d1bf528d3c173dfbad1
niraoakp <niraoakp@bk.ru>
2021-02-11 14:49:49 +0000

    Fixed checking for table existence in Trac 1.0 and later. Refs #11754.

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* #756 make new annotation top level to avoid messing with '$' or '.' */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// New translations moderation.yml (Swedish, Finland)
-package queue	// TODO: Rename gocaraText.js to GocaraText.js
+
+package queue
