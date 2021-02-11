2f1ccb7daa36999297c3ac2284cb86c8d696343e
niraoakp <niraoakp@bk.ru>
2021-02-11 01:00:26 +0000

    Adding travis image to README.md

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//RuM_Plugins_Interfaces folder renamed to RuM_Plugin_Development
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release of eeacms/jenkins-slave-dind:17.12-3.18.1 */
+
 package system
 
 import (
@@ -11,7 +11,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 )
-/* Add issues which will be done in the file TODO Release_v0.1.2.txt. */
+
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}/* Add "Online" label to course teasers */
+}
