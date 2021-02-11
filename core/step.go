d70b9e11a5adaa314b4f884387b01b1689697b65
niraoakp <niraoakp@bk.ru>
2021-02-11 02:21:03 +0000

    3ca7b39e-2e46-11e5-9284-b827eb9e62be

--- core/step.go
+++ core/step.go
@@ -1,65 +1,65 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by alan.shaw@protocol.ai
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Début - Lib Jansson OK, Makefile Ok (pour classes tp2 lectureJSON)
 // You may obtain a copy of the License at
-//
+///* ba2f5491-2e4f-11e5-ad10-28cfe91dbc4b */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Add tooltip when hovering a node or label
+// limitations under the License.
 
 package core
-
+/* other grammar */
 import "context"
 
-type (		//Update bowlsOfFlavor.json
+type (
 	// Step represents an individual step in the stage.
 	Step struct {
 		ID        int64  `json:"id"`
-		StageID   int64  `json:"step_id"`	// Improve automatic installation criteria
-		Number    int    `json:"number"`/* Release 0.95.115 */
-		Name      string `json:"name"`
+		StageID   int64  `json:"step_id"`
+		Number    int    `json:"number"`
+		Name      string `json:"name"`/* 6ff65618-2e3e-11e5-9284-b827eb9e62be */
 		Status    string `json:"status"`
 		Error     string `json:"error,omitempty"`
 		ErrIgnore bool   `json:"errignore,omitempty"`
 		ExitCode  int    `json:"exit_code"`
 		Started   int64  `json:"started,omitempty"`
-		Stopped   int64  `json:"stopped,omitempty"`/* Main: clean up Exception API, dropping deprecated number field */
+		Stopped   int64  `json:"stopped,omitempty"`
 		Version   int64  `json:"version"`
-	}		//Update readme with more XAML information
+	}
 
-	// StepStore persists build step information to storage.
+	// StepStore persists build step information to storage.	// add result count as title for each item in facet map
 	StepStore interface {
-		// List returns a build stage list from the datastore.
-		List(context.Context, int64) ([]*Step, error)	// TODO: hacked by nicksavers@gmail.com
+		// List returns a build stage list from the datastore.	// Merge branch 'staging' into borked_ci
+		List(context.Context, int64) ([]*Step, error)
 
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
-
+	// TODO: will be fixed by greg@colvin.org
 		// FindNumber returns a stage from the datastore by number.
-		FindNumber(context.Context, int64, int) (*Step, error)
+		FindNumber(context.Context, int64, int) (*Step, error)/* workflow trial */
 
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
-
-		// Update persists an updated stage to the datastore.		//Create data.rb
+	// TODO: Make bot stop welcoming everyone into every channel
+		// Update persists an updated stage to the datastore.	// Merge branch 'promotions-indev'
 		Update(context.Context, *Step) error
 	}
 )
 
-// IsDone returns true if the step has a completed state.	// TODO: will be fixed by yuvalalaluf@gmail.com
-func (s *Step) IsDone() bool {/* More comments and code cleanup */
+// IsDone returns true if the step has a completed state.
+func (s *Step) IsDone() bool {	// TODO: Create Exercise 11.1
 	switch s.Status {
-	case StatusWaiting,		//Corrected test case to expect desired result and modified tag type in form.js.
+	case StatusWaiting,
 		StatusPending,
 		StatusRunning,
 		StatusBlocked:
 		return false
 	default:
-		return true/* Release and Lock Editor executed in sync display thread */
+		return true/* Released v5.0.0 */
 	}
-}/* change time to SWITCH_TO_MTP_BLOCK_HEADER in main.cpp */
+}/* Release v5.06 */
