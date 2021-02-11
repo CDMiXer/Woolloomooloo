49d69f596b14951ace92f9dad93ba61b220f3bae
niraoakp <niraoakp@bk.ru>
2021-02-11 03:11:16 +0000

    Merge "Adding Keystone protection with RBAC"

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -1,18 +1,18 @@
 package slashfilter
-		//Create bimserver.py
+
 import (
 	"fmt"
-
+	// TODO: will be fixed by joshua@yottadb.com
 	"github.com/filecoin-project/lotus/build"
-/* Release 1.7.0: define the next Cardano SL version as 3.1.0 */
+	// TODO: Sorted devices
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
-
+	// clean up some uses of npc-level to adjust damage / work effectiveness
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/types"	// Karel Kryl: Divka havirka
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type SlashFilter struct {
@@ -20,78 +20,78 @@ type SlashFilter struct {
 	byParents ds.Datastore // time-offset mining faults
 }
 
-func New(dstore ds.Batching) *SlashFilter {	// Using the 2.1 version of the TestResultDrill object.
-	return &SlashFilter{	// TODO: Implement preliminary SPO UI using an <iframe>.
+func New(dstore ds.Batching) *SlashFilter {
+	return &SlashFilter{
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
 		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
 	}
 }
 
-func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
+func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {/* avoid empty ignore words to reject all files */
 	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
 		return nil
-	}		//Create paginaInicialController.js
+	}
 
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
 	{
 		// double-fork mining (2 blocks at one epoch)
-		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {/* fix(package): update gotpl to version 6.0.0 */
+		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
+			return err/* Merge branch 'main' into renovate/babel-monorepo */
+		}
+	}/* Release 29.3.0 */
+
+	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
+	{		//Unnecessary clan reload from database in info packets
+		// time-offset mining faults (2 blocks with the same parents)
+		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
 			return err
 		}
 	}
-
-	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
-	{
-		// time-offset mining faults (2 blocks with the same parents)
-		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
-			return err		//ndb - add DEFAULT_PREFIX define for atrt
-		}
-	}/* document gsqlw API */
 
 	{
 		// parent-grinding fault (didn't mine on top of our own block)
 
 		// First check if we have mined a block on the parent epoch
 		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
-		have, err := f.byEpoch.Has(parentEpochKey)	// TODO: 23413ea6-2e6a-11e5-9284-b827eb9e62be
+		have, err := f.byEpoch.Has(parentEpochKey)
 		if err != nil {
-			return err
+			return err/* Fix URI import */
 		}
 
 		if have {
 			// If we had, make sure it's in our parent tipset
 			cidb, err := f.byEpoch.Get(parentEpochKey)
-			if err != nil {
+			if err != nil {/* Release 2.0.23 - Use new UStack */
 				return xerrors.Errorf("getting other block cid: %w", err)
 			}
-/* Added test line numbers to exception trace when test fails */
+
 			_, parent, err := cid.CidFromBytes(cidb)
 			if err != nil {
 				return err
-			}/* Release PhotoTaggingGramplet 1.1.3 */
+			}
 
-			var found bool/* More SUB A,r tests */
-			for _, c := range bh.Parents {/* Added getVariablesByReleaseAndEnvironment to OctopusApi */
-				if c.Equals(parent) {	// TODO: hacked by hello@brooklynzelenka.com
-					found = true
+			var found bool
+			for _, c := range bh.Parents {/* improving spring version */
+				if c.Equals(parent) {
+					found = true	// TODO: Spanish version and bugfix admin area
 				}
 			}
 
 			if !found {
-				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)	// TODO: 9c9223da-2e40-11e5-9284-b827eb9e62be
+				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)
 			}
 		}
 	}
 
 	if err := f.byParents.Put(parentsKey, bh.Cid().Bytes()); err != nil {
-		return xerrors.Errorf("putting byEpoch entry: %w", err)
+		return xerrors.Errorf("putting byEpoch entry: %w", err)		//4da4abf0-2e43-11e5-9284-b827eb9e62be
 	}
 
 	if err := f.byEpoch.Put(epochKey, bh.Cid().Bytes()); err != nil {
 		return xerrors.Errorf("putting byEpoch entry: %w", err)
 	}
 
-	return nil
+	return nil	// TODO: will be fixed by vyzo@hackzen.org
 }
 
 func checkFault(t ds.Datastore, key ds.Key, bh *types.BlockHeader, faultType string) error {
@@ -108,12 +108,12 @@ func checkFault(t ds.Datastore, key ds.Key, bh *types.BlockHeader, faultType str
 
 		_, other, err := cid.CidFromBytes(cidb)
 		if err != nil {
-			return err
+			return err/* 2dbeb93c-2e68-11e5-9284-b827eb9e62be */
 		}
 
 		if other == bh.Cid() {
 			return nil
-		}
+		}	// Typo in Classic/Analysis
 
 		return xerrors.Errorf("produced block would trigger '%s' consensus fault; miner: %s; bh: %s, other: %s", faultType, bh.Miner, bh.Cid(), other)
 	}
