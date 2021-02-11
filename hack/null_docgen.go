a70119cf52157e4176be82970d7556dc2485f109
niraoakp <niraoakp@bk.ru>
2021-02-11 18:04:11 +0000

    removing dead bw code

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -2,6 +2,6 @@
 
 package main
 
-func generateDocs() {
+func generateDocs() {	// TODO: hacked by martin2cai@hotmail.com
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
 }
