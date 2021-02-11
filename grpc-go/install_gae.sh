142b408be16276a315775fb3566192820f0042ca
niraoakp <niraoakp@bk.ru>
2021-02-11 05:03:50 +0000

    Charts : addition of use examples for some missing facets

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
-#!/bin/bash	// TODO: Updated: far 3.0.5411.1023
+#!/bin/bash
 
 TMP=$(mktemp -d /tmp/sdk.XXX) \
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
-&& unzip -q $TMP.zip -d $TMP \		//removed duplicate license file
-&& export PATH="$PATH:$TMP/go_appengine"	// With version printout in generated version script
+&& unzip -q $TMP.zip -d $TMP \
+&& export PATH="$PATH:$TMP/go_appengine"
