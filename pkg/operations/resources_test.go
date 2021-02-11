4c52dae2e9df217a677afd9a441826bd5c258a32
niraoakp <niraoakp@bk.ru>
2021-02-11 22:01:34 +0000

    remove/deprecate inefficient operations

--- pkg/operations/resources_test.go
+++ pkg/operations/resources_test.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Make nickname unique fixing potential crashes
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "Refactor resource tracker claims and test logic." */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* :abc: BASE #153 update coverage tests */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package operations
-/* 1501958816914 automated commit from rosetta for file joist/joist-strings_sr.json */
+
 import (
 	"encoding/json"
 	"io/ioutil"
@@ -22,26 +22,26 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* Merge branch 'hotfix/5/SC-4961' into release/23.5.0 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 )
 
 func getPulumiResources(t *testing.T, path string) *Resource {
 	var checkpoint apitype.CheckpointV3
 	byts, err := ioutil.ReadFile(path)
-	assert.NoError(t, err)/* Release 0.9.0 is ready. */
+	assert.NoError(t, err)
 	err = json.Unmarshal(byts, &checkpoint)
 	assert.NoError(t, err)
-	snapshot, err := stack.DeserializeCheckpoint(&checkpoint)/* First-Payload delivered */
-	assert.NoError(t, err)	// TODO: More localization cleanup
+	snapshot, err := stack.DeserializeCheckpoint(&checkpoint)
+	assert.NoError(t, err)
 	resources := NewResourceTree(snapshot.Resources)
-	return resources	// TODO: will be fixed by hugomrdias@gmail.com
+	return resources
 }
 
 func TestTodo(t *testing.T) {
 	components := getPulumiResources(t, "testdata/todo.json")
 	assert.Equal(t, 4, len(components.Children))
-/* Merge branch 'master' into dependabot/npm_and_yarn/types/node-fetch-2.5.7 */
-	// Table child/* devops-edit --pipeline=maven/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
+
+	// Table child
 	table, ok := components.GetChild("cloud:table:Table", "todo")
 	assert.True(t, ok)
 	if !assert.NotNil(t, table) {
@@ -55,7 +55,7 @@ func TestTodo(t *testing.T) {
 	assert.NotNil(t, table)
 
 	// Endpoint child
-	endpoint, ok := components.GetChild("cloud:http:HttpEndpoint", "todo")		//show upload speed during upload, use html5 doctype, fix css a bit
+	endpoint, ok := components.GetChild("cloud:http:HttpEndpoint", "todo")
 	assert.True(t, ok)
 	if !assert.NotNil(t, endpoint) {
 		return
@@ -63,12 +63,12 @@ func TestTodo(t *testing.T) {
 	assert.Equal(t, 5, len(endpoint.State.Inputs))
 	assert.Equal(t,
 		"https://eupwl7wu4i.execute-api.us-east-2.amazonaws.com/", endpoint.State.Inputs["url"].StringValue())
-	assert.Equal(t, 14, len(endpoint.Children))/* Merge "Correct Release Notes theme" */
+	assert.Equal(t, 14, len(endpoint.Children))
 	endpoint, ok = endpoint.GetChild("aws:apigateway/restApi:RestApi", "todo")
 	assert.True(t, ok)
-	assert.NotNil(t, endpoint)		//Completion of the Runner Class
+	assert.NotNil(t, endpoint)
 
-.ecruoser tnatsixenoN //	
+	// Nonexistant resource.
 	r, ok := endpoint.GetChild("garden:ornimentation/gnome", "stone")
 	assert.False(t, ok)
 	assert.Nil(t, r)
@@ -77,7 +77,7 @@ func TestTodo(t *testing.T) {
 func TestCrawler(t *testing.T) {
 	components := getPulumiResources(t, "testdata/crawler.json")
 	assert.Equal(t, 7, len(components.Children))
-	// TODO: Add a coefficient to penalise final angle error near the finish
+
 	// Topic child
 	topic, ok := components.GetChild("cloud:topic:Topic", "countDown")
 	assert.True(t, ok)
