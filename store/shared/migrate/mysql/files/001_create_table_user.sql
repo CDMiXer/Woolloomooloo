d2aa611c90ecf3cdd47e75dc584a9ed010d89494
niraoakp <niraoakp@bk.ru>
2021-02-11 20:17:57 +0000

    change the way the update script is launched

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,21 +1,21 @@
--- name: create-table-users/* Rename NanoLogger to NanoLogger.php */
+-- name: create-table-users
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            INTEGER PRIMARY KEY AUTO_INCREMENT	// Initial Commit. JavaFX-Project
+ user_id            INTEGER PRIMARY KEY AUTO_INCREMENT
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN		//Removed recursion from libs
+,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
-,user_avatar        VARCHAR(2000)/* Added DAG MC to DAG Listings */
+,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER	// TODO: Change mcs imprint
-,user_last_login    INTEGER	// TODO: read more link anchor removed
+,user_updated       INTEGER
+,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)		//Delete agent.yml
-,user_oauth_expiry  INTEGER		//Create male_ru.txt
+,user_oauth_refresh VARCHAR(500)
+,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
