d50dd99fa5bed002ea0edf0e302a1b1062118dde
niraoakp <niraoakp@bk.ru>
2021-02-11 07:54:36 +0000

    Delete Trigonometry.java

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main	// add port info to readme
+package main
 
-import (	// time counter add complete
+import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
@@ -17,38 +17,38 @@ type FooComponent struct {
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {/* Release 2.0 enhancements. */
+	if err != nil {
 		return nil, err
-	}	// Prevent invalid values
-	return fooRes, nil	// TODO: will be fixed by vyzo@hackzen.org
+	}
+	return fooRes, nil
 }
-	// Merge "Fix crash when trying to save a page with a colon"
-// Scenario #4 - change the type of a component	// TODO: Implement new_suggestion view
-func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {/* 7a8c2106-2e71-11e5-9284-b827eb9e62be */
+
+// Scenario #4 - change the type of a component
+func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	alias := &pulumi.Alias{
 		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
 	}
-	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})	// Mention PHPUnit 8
-	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)		//bs "bosanski jezik" translation #15673. Author: mujo074. 
+	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
+	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
 	if err != nil {
-		return nil, err/* Clear 0x036, 0x0B6, 0x128 */
+		return nil, err
 	}
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
-	}	// adding config for extensions in keys
+	}
 	return fooComp, nil
-}	// [axl_io] removed unused variable in serial port enumerator on mac
+}
 
-func main() {	// TODO: hacked by lexy8russo@outlook.com
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp4")
-		if err != nil {/* Update README.md with more screenshots */
+		if err != nil {
 			return err
 		}
-	// ClassificationTest example added
+
 		return nil
 	})
 }
