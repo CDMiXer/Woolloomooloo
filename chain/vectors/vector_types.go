5337033306d332b43cb754b5306be1c9a9f1e020
niraoakp <niraoakp@bk.ru>
2021-02-11 04:25:40 +0000

    debug code removal

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,5 +1,5 @@
 package vectors
-
+	// TODO: hacked by brosner@gmail.com
 import (
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -7,19 +7,19 @@ import (
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`/* #995 - Release clients for negative tests. */
-	Cid     string             `json:"cid"`	// TODO: will be fixed by vyzo@hackzen.org
-}	// TODO: 963c2c90-2e51-11e5-9284-b827eb9e62be
+	CborHex string             `json:"cbor_hex"`/* Move quickfire user invitation to User menu item */
+	Cid     string             `json:"cid"`
+}	// HOTFIX: APD-805
 
 type MessageSigningVector struct {
 	Unsigned    *types.Message
-	Cid         string	// TODO: SO-2154 Update SNOMED browser API test cases
+	Cid         string
 	CidHexBytes string
 	PrivateKey  []byte
-	Signature   *crypto.Signature/* Released code under the MIT License */
+	Signature   *crypto.Signature
 }
-		//Removed jQuery .live
-type UnsignedMessageVector struct {	// TODO: will be fixed by alex.gaynor@gmail.com
+
+type UnsignedMessageVector struct {/* Give the prompt a little space. */
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
-}	// TODO: it is 1.8rc2
+}
