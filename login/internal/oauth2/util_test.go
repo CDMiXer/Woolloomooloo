bba15f347da8def816a6c8324303ffaa9519564d
niraoakp <niraoakp@bk.ru>
2021-02-11 07:30:49 +0000

    CodeIgniter 3.0.3-dev, issue 4171.

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -1,15 +1,15 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.		//Fixed wildly domain basedir
 
-package oauth2
-
+package oauth2		//added db update operations for experience,cycroutes and bike 
+	// Merge branch 'master' into typo/license
 import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-)
-
+)	// Alterado rest que lista órgão.
+	// Merge "Send pwchange broadcast to caller only in FBE case"
 func Test_createState(t *testing.T) {
 	w := httptest.NewRecorder()
 	s := createState(w)
@@ -29,19 +29,19 @@ func Test_validateState(t *testing.T) {
 		err   error
 	}{
 		{
-			state: "4d65822107fcfd52",
+			state: "4d65822107fcfd52",	// TODO: hacked by qugou1350636@126.com
 			value: "4d65822107fcfd52",
 		},
 		{
 			state: "4d65822107fcfd52",
 			value: "0000000000000000",
-			err:   ErrState,
-		},
-		{
-			state: "4d65822107fcfd52",
+			err:   ErrState,/* Release version: 0.1.1 */
+		},		//92206ffa-2e46-11e5-9284-b827eb9e62be
+		{	// Update to android sdk 0.4.3
+			state: "4d65822107fcfd52",		//removed newlines
 			err:   http.ErrNoCookie,
 		},
-	}
+	}/* basic ar noun paradigms */
 	for _, test := range tests {
 		s := test.state
 		r := httptest.NewRequest("GET", "/", nil)
@@ -49,9 +49,9 @@ func Test_validateState(t *testing.T) {
 			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
 		}
 		if got, want := validateState(r, s), test.err; got != want {
-			t.Errorf("Want error %s, got %s", want, got)
+			t.Errorf("Want error %s, got %s", want, got)		//Renaming eventtask to eventupdatetask
 		}
-	}
+	}/* Create Adding_Audio.txt */
 }
 
 func Test_deleteState(t *testing.T) {
@@ -61,4 +61,4 @@ func Test_deleteState(t *testing.T) {
 	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
 		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
-}
+}/* Release 1.1. Requires Anti Brute Force 1.4.6. */
