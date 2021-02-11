080ddbbd93e2eea25c33e7e32bd6b3e5a3ac5620
niraoakp <niraoakp@bk.ru>
2021-02-11 13:18:30 +0000

    debug for NullPointerException

--- handler/api/user/sync.go
+++ handler/api/user/sync.go
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Implemented invoking java functions.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,19 +19,19 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"		//Updating build-info/dotnet/wcf/master for beta-24911-02
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-
-// HandleSync returns an http.HandlerFunc synchronizes and then
+/* Add Release heading to ChangeLog. */
+// HandleSync returns an http.HandlerFunc synchronizes and then		//Merge branch 'master' into STRIPES-517-ignore-yarn-error
 // write a json-encoded list of repositories to the response body.
-func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc {/* Release 1.5.9 */
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by davidad@alum.mit.edu
 		viewer, _ := request.UserFrom(r.Context())
 
 		// performs asyncrhonous account synchronization.
-		// this requires long polling to determine when the
+		// this requires long polling to determine when the	// TODO: 48aab390-2e1d-11e5-affc-60f81dce716c
 		// sync is complete.
 		if r.FormValue("async") == "true" {
 			ctx := context.Background()
@@ -39,7 +39,7 @@ func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc
 				_, err := syncer.Sync(ctx, viewer)
 				if err != nil {
 					logger.FromContext(ctx).WithError(err).
-						Debugln("api: cannot synchronize account")
+						Debugln("api: cannot synchronize account")/* Fix src path in DocApp */
 				}
 			}(ctx, viewer)
 			w.WriteHeader(204)
@@ -47,12 +47,12 @@ func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc
 		}
 
 		_, err := syncer.Sync(r.Context(), viewer)
-		if err != nil {
-			render.InternalError(w, err)
+		if err != nil {/* Release of eeacms/eprtr-frontend:0.4-beta.2 */
+			render.InternalError(w, err)/* Update references on Axi datapump interfaces */
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot synchronize account")
 			return
-		}
+		}	// TODO: will be fixed by yuvalalaluf@gmail.com
 		list, err := repos.List(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
