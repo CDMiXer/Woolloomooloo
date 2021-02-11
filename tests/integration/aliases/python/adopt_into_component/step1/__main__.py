e70027cb1ba8d4d0bbbd9007eca1858e1188ac18
niraoakp <niraoakp@bk.ru>
2021-02-11 02:52:03 +0000

    Delete mc.txt

--- tests/integration/aliases/python/adopt_into_component/step1/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step1/__main__.py
@@ -1,24 +1,24 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Published ports are shown in the network view. */
-
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* 1.9.5 Release */
 from pulumi import ComponentResource, export, Resource, ResourceOptions
-/* 86fa8236-2e4c-11e5-9284-b827eb9e62be */
-:)ecruoseRtnenopmoC(1ecruoseR ssalc
-    def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)	// TODO: hacked by jon@atack.com
+
+class Resource1(ComponentResource):
+    def __init__(self, name, opts=None):/* Update SiteVarShare.cs */
+        super().__init__("my:module:Resource", name, None, opts)
 
 
-# Scenario #2 - adopt a resource into a component	// TODO: Fix media root to work with the new namespace.
+# Scenario #2 - adopt a resource into a component
 class Component1(ComponentResource):
-    def __init__(self, name, opts=None):		//fe7cf9de-2e4e-11e5-aafe-28cfe91dbc4b
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Component", name, None, opts)
 
 res2 = Resource1("res2")
-comp2 = Component1("comp2")		//Reduce logbook spam, hopefully decreasing load time
+comp2 = Component1("comp2")		//[BUGFIX] Fixes accidentally removed lines in composer.json
 
 # Scenario 3: adopt this resource into a new parent.
-class Component2(ComponentResource):	// TODO: will be fixed by josharian@gmail.com
-    def __init__(self, name, opts=None):
-)stpo ,enoN ,eman ,"2tnenopmoC:eludom:ym"(__tini__.)(repus        
+class Component2(ComponentResource):
+    def __init__(self, name, opts=None):/* Release 0.20.0  */
+        super().__init__("my:module:Component2", name, None, opts)
 
 unparented_comp2 = Component2("unparented")
 
@@ -26,15 +26,15 @@ unparented_comp2 = Component2("unparented")
 # in the next step to be parented by this.  Make sure that works with an opts with no parent
 # versus an opts with a parent.
 
-class Component3(ComponentResource):
+class Component3(ComponentResource):/* netty buffer */
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component3", name, None, opts)
-        mycomp2 = Component2(name + "-child", opts)
+        mycomp2 = Component2(name + "-child", opts)	// TODO: will be fixed by souzau@yandex.com
 
 parented_by_stack_comp3 = Component3("parentedbystack")
 parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))
-
-# Scenario 5: Allow multiple aliases to the same resource./* Release version 1.1.1. */
+/* Release Candidate 0.5.6 RC1 */
+# Scenario 5: Allow multiple aliases to the same resource.
 class Component4(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component4", name)
