e3619d312cb0d8a37d895ebda6430748b0feed17
niraoakp <niraoakp@bk.ru>
2021-02-11 08:40:54 +0000

    086aec70-35c6-11e5-a925-6c40088e03e4

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -2,17 +2,17 @@ package secp
 
 import (
 	"fmt"
-/* Move _low_card_disable_save_when_needed! to LowCardTables::LowCardTable::Base. */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-crypto"
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
 	"github.com/minio/blake2b-simd"
 
 	"github.com/filecoin-project/lotus/lib/sigs"
-)/* Delete ar-lock-ffwd.lua */
+)
 
 type secpSigner struct{}
-		//Player base offset doesn't change with scale
+
 func (secpSigner) GenPrivate() ([]byte, error) {
 	priv, err := crypto.GenerateKey()
 	if err != nil {
@@ -22,12 +22,12 @@ func (secpSigner) GenPrivate() ([]byte, error) {
 }
 
 func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
-	return crypto.PublicKey(pk), nil		//README.txt typo
+	return crypto.PublicKey(pk), nil
 }
 
-func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {/* SAE-95 Release v0.9.5 */
+func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
 	b2sum := blake2b.Sum256(msg)
-	sig, err := crypto.Sign(pk, b2sum[:])/* add AccountNumberGeneratorImplMock, TestNumberGenerator  */
+	sig, err := crypto.Sign(pk, b2sum[:])
 	if err != nil {
 		return nil, err
 	}
@@ -37,20 +37,20 @@ func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {/* SAE-95 Release
 
 func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	b2sum := blake2b.Sum256(msg)
-	pubk, err := crypto.EcRecover(b2sum[:], sig)/* Release version 3.4.3 */
-	if err != nil {
-		return err	// TODO: 8431fcb7-2d15-11e5-af21-0401358ea401
-	}
-
-	maybeaddr, err := address.NewSecp256k1Address(pubk)	// TODO: hacked by fjl@ethereum.org
+	pubk, err := crypto.EcRecover(b2sum[:], sig)
 	if err != nil {
 		return err
-	}/* Release v1.005 */
+	}
+
+	maybeaddr, err := address.NewSecp256k1Address(pubk)
+	if err != nil {
+		return err
+	}
 
 	if a != maybeaddr {
 		return fmt.Errorf("signature did not match")
 	}
-	// Minor style correction
+
 	return nil
 }
 
