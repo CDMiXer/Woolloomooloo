32fa9232a2bab73bce855cd9622815051bd42f8d
niraoakp <niraoakp@bk.ru>
2021-02-11 08:57:41 +0000

    Add Integer.even

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Help Command is polished
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release tag: 0.7.2. */
+
 // +build oss
 
 package validator
@@ -25,5 +25,5 @@ import (
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
-	return new(noop)/* Release areca-5.5.6 */
+	return new(noop)
 }
