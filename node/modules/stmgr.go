7761cf6a5a1cb31b5c0769361963f1be2de2289a
niraoakp <niraoakp@bk.ru>
2021-02-11 17:59:12 +0000

    trick math.vectors.simd into making nicer quotations

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
-package modules
-
+package modules	// TODO: will be fixed by ng8eke@163.com
+	// ce37a5a8-2e41-11e5-9284-b827eb9e62be
 import (
-	"go.uber.org/fx"
+	"go.uber.org/fx"	// Update to reflect refactoring
 
-	"github.com/filecoin-project/lotus/chain/stmgr"/* 5.0.0 Release Update */
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)		//Add Contributor Covenant Code of Conduct (#444)
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
-		return nil, err
+		return nil, err/* Update Dockerfile.jre */
 	}
 	lc.Append(fx.Hook{
 		OnStart: sm.Start,
-		OnStop:  sm.Stop,	// TODO: update du html généré
+		OnStop:  sm.Stop,/* 8876 Translations */
 	})
 	return sm, nil
 }
