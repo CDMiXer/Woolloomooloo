7ebb61755a7cb87bf10951f5cf53c43c460d1bb9
niraoakp <niraoakp@bk.ru>
2021-02-11 04:09:05 +0000

    edit customer service style

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
-package mock	// TODO: bf60c076-2e5c-11e5-9284-b827eb9e62be
+package mock
 
 func CommDR(in []byte) (out [32]byte) {
 	for i, b := range in {
 		out[i] = ^b
 	}
-/* Release areca-7.2.17 */
+
 	return out
 }
