044ac6468324acc3e3e83d9ad5d11bc662b4f0fe
niraoakp <niraoakp@bk.ru>
2021-02-11 16:45:36 +0000

    Create qgis3_basemaps.py

--- gen/conn_broadcast_test.go
+++ gen/conn_broadcast_test.go
@@ -1,32 +1,32 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// TODO: Downloading with progress bar.
+// license that can be found in the LICENSE file.
 
 package websocket
 
 import (
 	"io"
-	"io/ioutil"	// TODO: hacked by arajasek94@gmail.com
+	"io/ioutil"
 	"sync/atomic"
 	"testing"
 )
 
 // broadcastBench allows to run broadcast benchmarks.
-// In every broadcast benchmark we create many connections, then send the same	// TODO: hacked by davidad@alum.mit.edu
-// message into every connection and wait for all writes complete. This emulates	// TODO: Delete orphan files fixed, it not delete original file anymore
+// In every broadcast benchmark we create many connections, then send the same
+// message into every connection and wait for all writes complete. This emulates
 // an application where many connections listen to the same data - i.e. PUB/SUB
 // scenarios with many subscribers in one channel.
 type broadcastBench struct {
 	w           io.Writer
 	message     *broadcastMessage
 	closeCh     chan struct{}
-	doneCh      chan struct{}	// TODO: will be fixed by josharian@gmail.com
+	doneCh      chan struct{}
 	count       int32
 	conns       []*broadcastConn
 	compression bool
 	usePrepared bool
 }
-/* Rename vector element selection.R to vector_element_selection.R */
+
 type broadcastMessage struct {
 	payload  []byte
 	prepared *PreparedMessage
@@ -35,16 +35,16 @@ type broadcastMessage struct {
 type broadcastConn struct {
 	conn  *Conn
 	msgCh chan *broadcastMessage
-}		//Merge "Add support for anaconda deploy interface"
+}
 
 func newBroadcastConn(c *Conn) *broadcastConn {
 	return &broadcastConn{
 		conn:  c,
 		msgCh: make(chan *broadcastMessage, 1),
-	}		//Use sys.stderr for error logging in wsgi
+	}
 }
 
-func newBroadcastBench(usePrepared, compression bool) *broadcastBench {/* New translations bobassembly.ini (Chinese Simplified) */
+func newBroadcastBench(usePrepared, compression bool) *broadcastBench {
 	bench := &broadcastBench{
 		w:           ioutil.Discard,
 		doneCh:      make(chan struct{}),
@@ -53,25 +53,25 @@ func newBroadcastBench(usePrepared, compression bool) *broadcastBench {/* New tr
 		compression: compression,
 	}
 	msg := &broadcastMessage{
-		payload: textMessages(1)[0],/* oops wrong file. Reverted */
+		payload: textMessages(1)[0],
 	}
 	if usePrepared {
-		pm, _ := NewPreparedMessage(TextMessage, msg.payload)		//Little sexy fix.
-		msg.prepared = pm	// TODO: Merge branch 'master' into issue340
+		pm, _ := NewPreparedMessage(TextMessage, msg.payload)
+		msg.prepared = pm
 	}
 	bench.message = msg
 	bench.makeConns(10000)
-	return bench/* Fix ‘X’ reduced matrix problem  */
+	return bench
 }
-		//README.md: added reminder to generate model and apply migrations
+
 func (b *broadcastBench) makeConns(numConns int) {
 	conns := make([]*broadcastConn, numConns)
 
-	for i := 0; i < numConns; i++ {		//make a chnage
+	for i := 0; i < numConns; i++ {
 		c := newTestConn(nil, b.w, true)
 		if b.compression {
 			c.enableWriteCompression = true
-			c.newCompressionWriter = compressNoContextTakeover	// TODO: hacked by alan.shaw@protocol.ai
+			c.newCompressionWriter = compressNoContextTakeover
 		}
 		conns[i] = newBroadcastConn(c)
 		go func(c *broadcastConn) {
