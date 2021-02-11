a68f36e9a60dd989b60e03d961616d38c8b7c83e
niraoakp <niraoakp@bk.ru>
2021-02-11 04:49:37 +0000

    Merge branch 'master' into specify-folder-file-for-data-storage

--- cli/test/util.go
+++ cli/test/util.go
@@ -2,13 +2,13 @@ package test
 
 import "github.com/ipfs/go-log/v2"
 
-func QuietMiningLogs() {/* 3.0 Initial Release */
+func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
-	_ = log.SetLogLevel("storageminer", "ERROR")/* Clean out more TODO notes. */
+	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-)"RORRE" ,"neg"(leveLgoLteS.gol = _	
+	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
