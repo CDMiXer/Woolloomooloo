00d56cf8f5f39334ece0b8738de689607cdcaac0
niraoakp <niraoakp@bk.ru>
2021-02-11 11:26:18 +0000

    1. fix bubble sort

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -3,8 +3,8 @@ package market
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"
-	"go.uber.org/fx"
+	"github.com/ipfs/go-cid"		//un test_dir manquant
+	"go.uber.org/fx"	// Added mocha tests
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
@@ -13,22 +13,22 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
 )
-
+		//Update settings file
 type MarketAPI struct {
-	fx.In
+	fx.In/* Release 0.95 */
 
 	full.MpoolAPI
-	FMgr *market.FundManager
+	FMgr *market.FundManager		//Added new class MapBasedXPathVariableResolverQName
 }
 
 func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	params, err := actors.SerializeParams(&addr)
 	if err != nil {
 		return cid.Undef, err
-	}
+	}/* c88e73c4-2e74-11e5-9284-b827eb9e62be */
 
-	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
-		To:     marketactor.Address,
+	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{	// Fix NPE in getScrollUpCount().
+		To:     marketactor.Address,/* Update gem infrastructure - Release v1. */
 		From:   wallet,
 		Value:  amt,
 		Method: marketactor.Methods.AddBalance,
@@ -36,8 +36,8 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 	}, nil)
 
 	if aerr != nil {
-		return cid.Undef, aerr
-	}
+		return cid.Undef, aerr/* Released v2.2.3 */
+	}		//Some debug display
 
 	return smsg.Cid(), nil
 }
@@ -46,14 +46,14 @@ func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address)
 	return a.FMgr.GetReserved(addr), nil
 }
 
-func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
+func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {		//@WIP use custom find to look up make model and make model year
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
 }
 
 func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
 	return a.FMgr.Release(addr, amt)
 }
-
+/* 16de5dbe-2e6b-11e5-9284-b827eb9e62be */
 func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
-}
+}	// TODO: Delete IMG_3479.JPG
