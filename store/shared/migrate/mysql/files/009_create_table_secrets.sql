aacdfaae7e4e45c6f8e14d51d86aab38aadd7735
niraoakp <niraoakp@bk.ru>
2021-02-11 02:01:33 +0000

    Rename hha.py to test.py

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -2,19 +2,19 @@
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_repo_id           INTEGER	// TODO: Added Eventbrite link
+,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN/* Release LastaFlute-0.7.5 */
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)		//Add log4net config file
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,UNIQUE(secret_repo_id, secret_name)/* Added executable and game data */
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//#1 zeienko07: The project was created.
 );
 
 -- name: create-index-secrets-repo
-
+	// TODO: will be fixed by mail@overlisted.net
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
-	// TODO: hacked by mikeal.rogers@gmail.com
+	// Create devops-in-general
 -- name: create-index-secrets-repo-name
 
 CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
