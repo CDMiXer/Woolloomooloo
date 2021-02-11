aea2ef967522daa14aace1b72f903beb12d91a7a
niraoakp <niraoakp@bk.ru>
2021-02-11 23:39:57 +0000

    Include config files pypi package

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,7 +1,7 @@
-package build
-
+package build	// appveyor artifacts debug
+		//Create qbin.py
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Merge "Changed quoting function for oracleDB."
 )
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
