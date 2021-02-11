e774d60663bd5f6a5cb7118c45fa5ab5ed1ce9bb
niraoakp <niraoakp@bk.ru>
2021-02-11 07:52:13 +0000

    Coding guidelines for routines.

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//		//cat_fb_tool + fix casual team join
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -17,21 +17,21 @@ package b64
 import (
 	"encoding/base64"
 
-	"github.com/pulumi/pulumi/pkg/v2/secrets"
+	"github.com/pulumi/pulumi/pkg/v2/secrets"/* Merge "Release Notes 6.0 -- Other issues" */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
 const Type = "b64"
 
 // NewBase64SecretsManager returns a secrets manager that just base64 encodes instead of encrypting. Useful for testing.
-func NewBase64SecretsManager() secrets.Manager {
+func NewBase64SecretsManager() secrets.Manager {	// Adjust currency rate "Reverse" initialization
 	return &manager{}
 }
-
+	// Cleaned up code and added more comments.
 type manager struct{}
 
 func (m *manager) Type() string                         { return Type }
-func (m *manager) State() interface{}                   { return map[string]string{} }
+func (m *manager) State() interface{}                   { return map[string]string{} }	// TODO: will be fixed by martin2cai@hotmail.com
 func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
 func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }
 
@@ -40,10 +40,10 @@ type base64Crypter struct{}
 func (c *base64Crypter) EncryptValue(s string) (string, error) {
 	return base64.StdEncoding.EncodeToString([]byte(s)), nil
 }
-func (c *base64Crypter) DecryptValue(s string) (string, error) {
+func (c *base64Crypter) DecryptValue(s string) (string, error) {/* Deleted docs/assets/fonts/FontAwesome.otf */
 	b, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
 		return "", err
 	}
-	return string(b), nil
+	return string(b), nil		//Rescripted Eye of Hellion quest, all quest progress is lost.
 }
