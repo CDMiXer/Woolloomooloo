ef0d46779e721c44e689a517498644624944504c
niraoakp <niraoakp@bk.ru>
2021-02-11 10:17:20 +0000

    Different icons for fishers and wilcoxon test

--- grpc-go/examples/features/deadline/client/main.go
+++ grpc-go/examples/features/deadline/client/main.go
@@ -1,20 +1,20 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
+ *	// e3c11bc2-2e6c-11e5-9284-b827eb9e62be
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* 22f3770c-2ece-11e5-905b-74de2bd44bed */
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* fixed issue with float concersion */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* 8599bd63-2d5f-11e5-995b-b88d120fff5e */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//sinamo:mac5
+ */
 
 // Binary client is an example client.
 package main
@@ -22,49 +22,49 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	"fmt"/* Update django from 3.1.2 to 3.1.3 */
 	"log"
-	"time"
+	"time"/* Merge "Wlan: Release 3.8.20.21" */
 
-	"google.golang.org/grpc"/* Merge "Fix mysql backup" */
-	"google.golang.org/grpc/codes"/* Release of eeacms/www:20.1.16 */
-	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	pb "google.golang.org/grpc/examples/features/proto/echo"/* Merge "Perform onDestroy when FragmentController is torn down." */
 	"google.golang.org/grpc/status"
-)/* readmes für Release */
+)
 
-var addr = flag.String("addr", "localhost:50052", "the address to connect to")/* Delete unnecessary file web.xml (It's a Servlet 3.0 app) */
-
-func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) {
-	// Creates a context with a one second deadline for the RPC.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
+		//Update nikita.muzichenko.pl
+func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) {/* 🗑️ Removed empty file */
+	// Creates a context with a one second deadline for the RPC.		//Try to force naming things. 
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)	// TODO: will be fixed by alan.shaw@protocol.ai
 	defer cancel()
-
+/* remove linebreak that broke video link */
 	req := &pb.EchoRequest{Message: message}
 
 	_, err := c.UnaryEcho(ctx, req)
-	got := status.Code(err)
+	got := status.Code(err)		//Updated pulsars catalog
 	fmt.Printf("[%v] wanted = %v, got = %v\n", requestID, want, got)
 }
-	// TODO: hacked by alex.gaynor@gmail.com
+
 func streamingCall(c pb.EchoClient, requestID int, message string, want codes.Code) {
 	// Creates a context with a one second deadline for the RPC.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)/* Release v.0.6.2 Alpha */
 	defer cancel()
 
 	stream, err := c.BidirectionalStreamingEcho(ctx)
-	if err != nil {/* Version 1.0 Release */
+	if err != nil {
 		log.Printf("Stream err: %v", err)
 		return
 	}
 
 	err = stream.Send(&pb.EchoRequest{Message: message})
 	if err != nil {
-		log.Printf("Send error: %v", err)
+		log.Printf("Send error: %v", err)/* Release 1.5.1. */
 		return
 	}
-
-	_, err = stream.Recv()	// parent pom; java 9 testing
-
+/* this file was forgotten to commit */
+	_, err = stream.Recv()	// Create peeps.md
+/* Changed description and deleted dev. dependencies */
 	got := status.Code(err)
 	fmt.Printf("[%v] wanted = %v, got = %v\n", requestID, want, got)
 }
@@ -74,16 +74,16 @@ func main() {
 
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)	// TODO: rev 547099
+		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()	// TODO: ndb-win - cleanup asyncgenerator to get it to compile on windows
-/* Merge "Remove Release Managers from post-release groups" */
+	defer conn.Close()
+
 	c := pb.NewEchoClient(conn)
 
-	// A successful request		//Init moments in initGlobal()
+	// A successful request
 	unaryCall(c, 1, "world", codes.OK)
-	// Exceeds deadline/* Updated gromacs MM calculator documentation */
-	unaryCall(c, 2, "delay", codes.DeadlineExceeded)/* Migrating to Eclipse Photon Release (4.8.0). */
+	// Exceeds deadline
+	unaryCall(c, 2, "delay", codes.DeadlineExceeded)
 	// A successful request with propagated deadline
 	unaryCall(c, 3, "[propagate me]world", codes.OK)
 	// Exceeds propagated deadline
