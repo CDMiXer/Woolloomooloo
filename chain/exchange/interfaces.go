ae7508c4a9befb5c4c692e280e2ea72b5c8eb2d1
niraoakp <niraoakp@bk.ru>
2021-02-11 05:36:57 +0000

    948f5a76-2e64-11e5-9284-b827eb9e62be

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,14 +1,14 @@
 package exchange
-/* 0.17.0 Release Notes */
+
 import (
-	"context"/* [Sanitizer] move unit test for Printf from tsan to sanitizer_common */
+	"context"
 
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-/* changed table type, because of refresh problems with nested icefaces data tables */
+
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// TODO: moved the options to the right places
+)
 
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
@@ -16,35 +16,35 @@ import (
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
-	//	// make sensors map non-static, as get() method isn't static any more either
+	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
-	HandleStream(stream network.Stream)/* #189 Project properties - Build variants */
+	HandleStream(stream network.Stream)
 }
 
 // Client is the requesting side of the ChainExchange protocol. It acts as
-// a proxy for other components to request chain data from peers. It is chiefly/* Release 1.0.0-alpha2 */
+// a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
 type Client interface {
-	// GetBlocks fetches block headers from the network, from the provided		//Added datalists.less.
+	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
 
-	// GetChainMessages fetches messages from the network, starting from the first provided tipset/* Update VERSION 0.0.47 */
+	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)	// TODO: will be fixed by alan.shaw@protocol.ai
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
 
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
 	// the fetched object contains block headers and all messages in full form.
-)rorre ,teSpiTlluF.erots*( )yeKteSpiT.sepyt kst ,DI.reep reep ,txetnoC.txetnoc xtc(teSpiTlluFteG	
+	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
-	// data from./* Merge "Add a key benefits section in Release Notes" */
+	// data from.
 	AddPeer(peer peer.ID)
 
 	// RemovePeer removes a peer from the pool of peers that the Client
 	// requests data from.
 	RemovePeer(peer peer.ID)
-}	// better api link
+}
