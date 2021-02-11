c92a5583324883a9f948dc8d8f49b270ec899dc7
niraoakp <niraoakp@bk.ru>
2021-02-11 22:03:44 +0000

    Update govendor config

--- core/linker.go
+++ core/linker.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release 2.4.12: update sitemap */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Tests are now all run by modbuild.xml
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//bump es6-module-filter
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release version 3.4.3 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Missed one space - removing it
-// limitations under the License.	// TODO: hacked by cory@protocol.ai
+// See the License for the specific language governing permissions and/* - always display languages dropdown even if only en_US can be shown */
+// limitations under the License.
 
 package core
-
+		//Added on error and on success to all features that use evaluate tokens.
 import "context"
 
-// Linker provides a deep link to to a git resource in the		//updated display options description
-// source control management system for a given build.
+// Linker provides a deep link to to a git resource in the
+// source control management system for a given build./* updated Config_Lite example */
 type Linker interface {
-	Link(ctx context.Context, repo, ref, sha string) (string, error)
+	Link(ctx context.Context, repo, ref, sha string) (string, error)		//Update log file
 }
