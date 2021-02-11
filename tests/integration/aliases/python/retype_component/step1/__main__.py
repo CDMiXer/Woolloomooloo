53a7702714e179bd471d9001a2b93f3bbe690806
niraoakp <niraoakp@bk.ru>
2021-02-11 15:56:15 +0000

    Merge "Hidden checkboxes in Availability Zones table"

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Create duolingo_clear.js
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
@@ -6,10 +6,10 @@ class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #4 - change the type of a component		//Update src/utils.go
+# Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
-comp4 = ComponentFour("comp4")/* fix blank space on top at small widths */
+comp4 = ComponentFour("comp4")
