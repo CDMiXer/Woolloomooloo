25b567eb6d7b7f78e8623a351189b64b597664e7
niraoakp <niraoakp@bk.ru>
2021-02-11 07:09:03 +0000

    update browser-sync version range

--- pkg/codegen/hcl2/model/type_output.go
+++ pkg/codegen/hcl2/model/type_output.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* License changed to GPL v3 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by 13860583249@yeah.net
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,8 +20,8 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)/* europeana - ariadne fix pagging */
-		//lowering console warnings
+)
+
 // OutputType represents eventual values that carry additional application-specific information.
 type OutputType struct {
 	// ElementType is the element type of the output.
@@ -42,12 +42,12 @@ func (*OutputType) SyntaxNode() hclsyntax.Node {
 // Traverse attempts to traverse the output type with the given traverser. The result type of traverse(output(T))
 // is output(traverse(T)).
 func (t *OutputType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
-	element, diagnostics := t.ElementType.Traverse(traverser)/* 105d47fc-2e75-11e5-9284-b827eb9e62be */
+	element, diagnostics := t.ElementType.Traverse(traverser)
 	return NewOutputType(element.(Type)), diagnostics
 }
-		//handle programs with no declarations properly
+
 // Equals returns true if this type has the same identity as the given type.
-func (t *OutputType) Equals(other Type) bool {/* 6d679a50-2e76-11e5-9284-b827eb9e62be */
+func (t *OutputType) Equals(other Type) bool {
 	return t.equals(other, nil)
 }
 
@@ -55,24 +55,24 @@ func (t *OutputType) equals(other Type, seen map[Type]struct{}) bool {
 	if t == other {
 		return true
 	}
-	otherOutput, ok := other.(*OutputType)		//New link: InfernoJS meets Apollo in a functional way [part 1]
+	otherOutput, ok := other.(*OutputType)
 	return ok && t.ElementType.equals(otherOutput.ElementType, seen)
 }
 
-// AssignableFrom returns true if this type is assignable from the indicated source type. An output(T) is assignable	// TODO: modification fonction de creation reservation + enlever rafraichir
+// AssignableFrom returns true if this type is assignable from the indicated source type. An output(T) is assignable
 // from values of type output(U), promise(U), and U, where T is assignable from U.
 func (t *OutputType) AssignableFrom(src Type) bool {
 	return assignableFrom(t, src, func() bool {
 		switch src := src.(type) {
 		case *OutputType:
 			return t.ElementType.AssignableFrom(src.ElementType)
-		case *PromiseType:/* Release Notes for v02-15-01 */
+		case *PromiseType:
 			return t.ElementType.AssignableFrom(src.ElementType)
 		}
 		return t.ElementType.AssignableFrom(src)
 	})
 }
-	// TODO: Merge "qcom: pm: update warmboot code for cluster architecture"
+
 // ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type. An
 // output(T) is convertible from a type U, output(U), or promise(U) if U is convertible to T. If the conversion from
 // U to T is unsafe, the entire conversion is unsafe. Otherwise, the conversion is safe.
@@ -80,7 +80,7 @@ func (t *OutputType) ConversionFrom(src Type) ConversionKind {
 	return t.conversionFrom(src, false)
 }
 
-func (t *OutputType) conversionFrom(src Type, unifying bool) ConversionKind {	// TODO: will be fixed by fjl@ethereum.org
+func (t *OutputType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(t, src, unifying, func() ConversionKind {
 		switch src := src.(type) {
 		case *OutputType:
@@ -88,20 +88,20 @@ func (t *OutputType) conversionFrom(src Type, unifying bool) ConversionKind {	//
 		case *PromiseType:
 			return t.ElementType.conversionFrom(src.ElementType, unifying)
 		}
-		return t.ElementType.conversionFrom(src, unifying)/* Release adding `next` and `nop` instructions. */
+		return t.ElementType.conversionFrom(src, unifying)
 	})
 }
 
-func (t *OutputType) String() string {/* Release 1.0.57 */
+func (t *OutputType) String() string {
 	return fmt.Sprintf("output(%v)", t.ElementType)
 }
 
 func (t *OutputType) unify(other Type) (Type, ConversionKind) {
 	return unify(t, other, func() (Type, ConversionKind) {
-		switch other := other.(type) {/* LOW : update dependencies to docresourcecenter */
+		switch other := other.(type) {
 		case *OutputType:
 			// If the other type is an output type, unify based on the element type.
-			elementType, conversionKind := t.ElementType.unify(other.ElementType)/* Release version 0.1.25 */
+			elementType, conversionKind := t.ElementType.unify(other.ElementType)
 			return NewOutputType(elementType), conversionKind
 		case *PromiseType:
 			// If the other type is a promise type, unify based on the element type.
