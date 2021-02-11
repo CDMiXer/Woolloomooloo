b74a7487d94749b484f9c2c68e31b76484eafd9c
niraoakp <niraoakp@bk.ru>
2021-02-11 07:19:01 +0000

    ModLockscreen: turned on anti-aliasing for Battery Arc

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by hugomrdias@gmail.com
-/* Added code coverage upload to .travis.yml */
-// +build !oss
+// that can be found in the LICENSE file.	// make pool.aquire context manager aware
 
-package admission
+// +build !oss
+/* tweaking name and combining page */
+package admission		//Route all LRI saves thru tagger controller action
