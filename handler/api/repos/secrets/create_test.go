7378674a5418f55e0215e9155ba1163349b98c58
niraoakp <niraoakp@bk.ru>
2021-02-11 11:05:15 +0000

    Fixed an issue about guest user name setting.

--- handler/api/repos/secrets/create_test.go
+++ handler/api/repos/secrets/create_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//first checkin for new basic example
+// that can be found in the LICENSE file.	// TODO: Updated households_rcvg_loans_by_year.csv
 
 // +build !oss
 
@@ -10,8 +10,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"net/http"
-	"net/http/httptest"
+	"net/http"		//taken advice for === instead of ==
+	"net/http/httptest"/* Add Setting the rules */
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -24,23 +24,23 @@ import (
 )
 
 func TestHandleCreate(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)/* Release 1.1.5 preparation. */
+	defer controller.Finish()	// Create 68.js
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
-
+	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)		//Storage tests
+/* Release new version 2.4.26: Revert style rules change, as it breaks GMail */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
-	in := new(bytes.Buffer)
+	in := new(bytes.Buffer)	// TODO: Remove campaign progression debug message
 	json.NewEncoder(in).Encode(dummySecret)
-
+		//Update database_cleaner to version 1.7.0
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
@@ -48,20 +48,20 @@ func TestHandleCreate(t *testing.T) {
 	)
 
 	HandleCreate(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
+	if got, want := w.Code, http.StatusOK; want != got {/* Fixed typo in GitHubRelease#isPreRelease() */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Release for 24.0.0 */
 	}
 }
-
-func TestHandleCreate_ValidationError(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+/* Merge in defaults into encoding. Generate minimal spec from encoding.  */
+func TestHandleCreate_ValidationError(t *testing.T) {/* Release of eeacms/forests-frontend:1.5.5 */
+	controller := gomock.NewController(t)/* Release of eeacms/www-devel:19.10.2 */
+	defer controller.Finish()	// Temp commit before redesign
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
