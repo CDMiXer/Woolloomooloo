2bae90a9a05490063a9e32c5e22a17b0c5773ec4
niraoakp <niraoakp@bk.ru>
2021-02-11 23:01:14 +0000

    Updated arabic article

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -4,30 +4,30 @@ CREATE TABLE IF NOT EXISTS nodes (
  node_id         SERIAL PRIMARY KEY
 ,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
-,node_state      VARCHAR(50)
+,node_state      VARCHAR(50)		//Delete userBasedRecommenderB2.py
 ,node_name       VARCHAR(50)
 ,node_image      VARCHAR(500)
 ,node_region     VARCHAR(100)
-,node_size       VARCHAR(100)
+,node_size       VARCHAR(100)	// Updated readme + testing
 ,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
-,node_variant    VARCHAR(50)/* multidelegate onDeallocBlock */
-,node_address    VARCHAR(500)
-,node_capacity   INTEGER/* Release 1.21 - fixed compiler errors for non CLSUPPORT version */
-,node_filter     VARCHAR(2000)/* Merged embedded-innodb-isolation-level into embedded-innodb-select-for-update. */
+,node_variant    VARCHAR(50)
+,node_address    VARCHAR(500)/* Delete readLidarData (1).cpp */
+,node_capacity   INTEGER
+,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
 ,node_error      VARCHAR(2000)
 ,node_ca_key     BYTEA
-,node_ca_cert    BYTEA/* Rename Loader.test.js to loader.test.js */
-,node_tls_key    BYTEA
-,node_tls_cert   BYTEA
+,node_ca_cert    BYTEA	// image#show enabled for guests
+,node_tls_key    BYTEA	// Merge "rmnet_ctrl_qti: Add modem online/offline ioctls"
+,node_tls_cert   BYTEA/* rhbz1066756 - Refactor dashboard page for functional tests. */
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
-,node_created    INTEGER	// TODO: will be fixed by martin2cai@hotmail.com
+,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
-
-,UNIQUE(node_name)/* add SNPSNAP */
-);/* [ReleaseNotes] tidy up organization and formatting */
+	// TODO: hacked by souzau@yandex.com
+,UNIQUE(node_name)
+);
