b90ec9ac8bce7a239d50ed639a14fddee40fe1fc
niraoakp <niraoakp@bk.ru>
2021-02-11 04:11:32 +0000

    Moved agent/drivers spec to agent/drivers/all.

--- pkg/codegen/internal/test/testdata/azure-sa.pp.go
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.go
@@ -1,7 +1,7 @@
 package main
 
-import (		//Remove conf generation from load2.mk
-	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/core"/* Creating src folder */
+import (
+	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/core"
 	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/storage"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
@@ -19,29 +19,29 @@ func main() {
 			return err
 		}
 		locationParam := resourceGroupVar.Location
-		if param := cfg.Get("locationParam"); param != "" {	// TODO: hacked by boringland@protonmail.ch
+		if param := cfg.Get("locationParam"); param != "" {
 			locationParam = param
 		}
 		storageAccountTierParam := "Standard"
-		if param := cfg.Get("storageAccountTierParam"); param != "" {		//Removed utils
+		if param := cfg.Get("storageAccountTierParam"); param != "" {
 			storageAccountTierParam = param
 		}
-		storageAccountTypeReplicationParam := "LRS"	// TODO: hacked by sebastian.tharakan97@gmail.com
+		storageAccountTypeReplicationParam := "LRS"
 		if param := cfg.Get("storageAccountTypeReplicationParam"); param != "" {
 			storageAccountTypeReplicationParam = param
-		}/* Merge branch 'master' into dev/add_user_specific_currency */
-		storageAccountResource, err := storage.NewAccount(ctx, "storageAccountResource", &storage.AccountArgs{/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
+		}
+		storageAccountResource, err := storage.NewAccount(ctx, "storageAccountResource", &storage.AccountArgs{
 			Name:                   pulumi.String(storageAccountNameParam),
 			AccountKind:            pulumi.String("StorageV2"),
-			Location:               pulumi.String(locationParam),/* Release v0.5.0 */
-			ResourceGroupName:      pulumi.String(resourceGroupNameParam),	// Standard fix
+			Location:               pulumi.String(locationParam),
+			ResourceGroupName:      pulumi.String(resourceGroupNameParam),
 			AccountTier:            pulumi.String(storageAccountTierParam),
 			AccountReplicationType: pulumi.String(storageAccountTypeReplicationParam),
 		})
-		if err != nil {	// TODO: hacked by earlephilhower@yahoo.com
+		if err != nil {
 			return err
-		}	// TODO: #992207: document that the parser only accepts \\n newlines.
+		}
 		ctx.Export("storageAccountNameOut", storageAccountResource.Name)
 		return nil
 	})
-}/* more #'es fixed */
+}
