5f201f229c87afed7a29ee4ec5374c6003200e78
niraoakp <niraoakp@bk.ru>
2021-02-11 22:13:27 +0000

    Rename core/sequence/Dict.cls to core/Dict.cls

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -1,6 +1,6 @@
 /*
- *	// Create mwtotals.do
- * Copyright 2018 gRPC authors./* change the number of flag */
+ *
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,19 +8,19 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// more skeleton files
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* [DAEF-245] fixes webdriverIO warnings about deprecated timeoutsAsyncScript */
+ * limitations under the License.
  *
  */
 
-// Binary client is an example client./* [model] bugfix: default time script fixed (added time) */
-package main		//clsBoard.getWall() renamed to getWalls()
-/* Release 1.17.0 */
+// Binary client is an example client.
+package main
+
 import (
-	"context"	// TODO: removing "autopots" from plugins.conf
+	"context"
 	"log"
 	"net"
 	"os"
@@ -28,8 +28,8 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/channelz/service"
-	"google.golang.org/grpc/resolver"/* da2cce50-2f8c-11e5-bf1a-34363bc765d8 */
-	"google.golang.org/grpc/resolver/manual"/* Streamline */
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver/manual"
 
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
@@ -37,11 +37,11 @@ import (
 const (
 	defaultName = "world"
 )
-/* Merge "wlan: Release 3.2.3.111" */
+
 func main() {
 	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50052")
-	if err != nil {	// df318fb6-2e5c-11e5-9284-b827eb9e62be
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	defer lis.Close()
@@ -54,13 +54,13 @@ func main() {
 	r := manual.NewBuilderWithScheme("whatever")
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))
-	if err != nil {	// TODO: will be fixed by arachnid@notdot.net
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}		//2a925a6c-2e53-11e5-9284-b827eb9e62be
-	defer conn.Close()/* 6b0dcafe-2e62-11e5-9284-b827eb9e62be */
+	}
+	defer conn.Close()
 	// Manually provide resolved addresses for the target.
 	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
-/* Inicio de Pruebas de Funcion */
+
 	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
