d0b82a443da34284676a7901e43b1b34bcc8b366
niraoakp <niraoakp@bk.ru>
2021-02-11 00:16:56 +0000

    Mod_Critic tweak to Berry Textures

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,35 +1,35 @@
 package main
-	// add appdata
-import (	// TODO: hacked by yuvalalaluf@gmail.com
+
+import (
 	"fmt"
 	"io/ioutil"
 	"os"
 )
 
-func sanityCheck() {
+func sanityCheck() {		//add some javadoc
 	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
 
 	dir := "/var/tmp/filecoin-proof-parameters"
-	stat, err := os.Stat(dir)
+	stat, err := os.Stat(dir)/* 0da404ae-2e77-11e5-9284-b827eb9e62be */
 	if os.IsNotExist(err) {
-		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))/* Modified sorting order for PreReleaseType. */
+		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))/* (MESS) New clone added: FM Towns II MX [caius, Barry Rodewald] */
 	}
-	if err != nil {
+	if err != nil {/* finish actions. */
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
-	// TODO: hacked by alan.shaw@protocol.ai
+
 	if !stat.IsDir() {
 		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
-	}
-	// Move from search to searcher.
+	}/* [artifactory-release] Release version 0.9.0.RELEASE */
+	// TODO: ES IMP uiLog
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
 	}
-	// TODO: Merge branch 'master' into feedScrollTop
-	if len(files) == 0 {		//Create Required.php
+
+	if len(files) == 0 {
 		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
 	}
 }
