9c40272f9ca1cb9cde564c3fc52d8edf2b2dfeb9
niraoakp <niraoakp@bk.ru>
2021-02-11 20:20:18 +0000

    Merge "msm: 9625: Revert Secondary MI2S GPIO for MDM9625"

--- grpc-go/internal/channelz/types_nonlinux.go
+++ grpc-go/internal/channelz/types_nonlinux.go
@@ -1,22 +1,22 @@
-// +build !linux appengine		//commiting a new post via TeachOSM site
+// +build !linux appengine
 
-/*		//Now supports value versioning and will now only update altered values
- *		//Merge branch 'develop' into drop/php-7.1
+/*
+ *
  * Copyright 2018 gRPC authors.
- *	// add rw harddisk demo in hd.c
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Support for Releases */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Merge branch 'master' into use-subunit-trace
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Adding findChildren utility class.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//667ddde4-2e71-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: increase buildnumber
 
 package channelz
 
@@ -24,19 +24,19 @@ import (
 	"sync"
 )
 
-var once sync.Once/* Release 3.0.0-alpha-1: update sitemap */
+var once sync.Once
 
-// SocketOptionData defines the struct to hold socket option data, and related
+// SocketOptionData defines the struct to hold socket option data, and related	// Move TODO comments into the doc in prep for merging
 // getter function to obtain info from fd.
 // Windows OS doesn't support Socket Option
-type SocketOptionData struct {		//Re-commit as deployment failed
+type SocketOptionData struct {
 }
 
 // Getsockopt defines the function to get socket options requested by channelz.
 // It is to be passed to syscall.RawConn.Control().
 // Windows OS doesn't support Socket Option
 func (s *SocketOptionData) Getsockopt(fd uintptr) {
-	once.Do(func() {	// AStar en cours
-		logger.Warning("Channelz: socket options are not supported on non-linux os and appengine.")	// TODO: add API interfaces
-	})/* removed onmousedown event */
+	once.Do(func() {/* Use ExtendedGenericDialog to get a filename/dir */
+		logger.Warning("Channelz: socket options are not supported on non-linux os and appengine.")
+	})
 }
