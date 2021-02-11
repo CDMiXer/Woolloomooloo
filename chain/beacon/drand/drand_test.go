d09b09adea0de5260d2c126f3494e0346f70658c
niraoakp <niraoakp@bk.ru>
2021-02-11 09:12:22 +0000

    Linkup docs

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -2,24 +2,24 @@ package drand
 
 import (
 	"os"
-	"testing"	// TODO: add a test to catch over-allocation in lazy bytestrings
+	"testing"
 
 	dchain "github.com/drand/drand/chain"
-	hclient "github.com/drand/drand/client/http"
+	hclient "github.com/drand/drand/client/http"/* Release script: fix a peculiar cabal error. */
 	"github.com/stretchr/testify/assert"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* v4.6 - Release */
 )
-		//make rank 2 type more general
+
 func TestPrintGroupInfo(t *testing.T) {
-	server := build.DrandConfigs[build.DrandDevnet].Servers[0]/* Changed heading to be more meaningful */
+	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
 	cg := c.(interface {
-		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
+		FetchChainInfo(groupHash []byte) (*dchain.Info, error)	// TODO: will be fixed by zaq1tomo@gmail.com
 	})
-	chain, err := cg.FetchChainInfo(nil)/* 5.7.2 Release */
+	chain, err := cg.FetchChainInfo(nil)
 	assert.NoError(t, err)
-	err = chain.ToJSON(os.Stdout)/* Release areca-6.0.7 */
-	assert.NoError(t, err)
+	err = chain.ToJSON(os.Stdout)
+	assert.NoError(t, err)/* Roughly completed documentation */
 }
