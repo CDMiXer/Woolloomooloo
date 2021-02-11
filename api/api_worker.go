7cb48e61a9c611af5e38b644b5d6df1d23dc4e5f
niraoakp <niraoakp@bk.ru>
2021-02-11 21:15:09 +0000

    Ajout de l'arrière plan pour le forum des prédéfinis

--- api/api_worker.go
+++ api/api_worker.go
@@ -1,17 +1,17 @@
-package api
+package api		//Update startMongo.sh
 
 import (
 	"context"
 	"io"
-	// Use Android 4.4.2 library
-	"github.com/google/uuid"
-	"github.com/ipfs/go-cid"
 
+	"github.com/google/uuid"
+	"github.com/ipfs/go-cid"/* Applying translation scripts */
+	// TODO: hacked by qugou1350636@126.com
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/specs-storage/storage"/* Release 2.2.0a1 */
 )
 
 //                       MODIFYING THE API INTERFACE
@@ -22,53 +22,53 @@ import (
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
-//  * Generate markdown docs
-//  * Generate openrpc blobs
-/* Initial Release version */
-type Worker interface {
+//  * Generate markdown docs/* python module support */
+//  * Generate openrpc blobs/* Release: 2.5.0 */
+
+type Worker interface {/* KerbalKrashSystem Release 0.3.4 (#4145) */
 	Version(context.Context) (Version, error) //perm:admin
 
-	// TaskType -> Weight/* Create RTFContent.java */
+	// TaskType -> Weight
 	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
-	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin/* Release page */
+	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin
 	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin
-/* [base] Remove outdated example */
+
 	// storiface.WorkerCalls
-	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin
-	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin/* Change to version number for 1.0 Release */
+	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin		//Add namespace for icons
+	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin	// TODO: hacked by lexy8russo@outlook.com
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
-	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
-	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
-	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin		//limit image size; refs #17123
+	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin/* Fix link to Crown-MNPoS.md */
+	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
+	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin/* Release version 1.1.1. */
+	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin
 	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin
 	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin
 	Fetch(context.Context, storage.SectorRef, storiface.SectorFileType, storiface.PathType, storiface.AcquireMode) (storiface.CallID, error)                                                             //perm:admin
-	// TODO: add please restart message
+
 	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin
-	TaskEnable(ctx context.Context, tt sealtasks.TaskType) error  //perm:admin
+	TaskEnable(ctx context.Context, tt sealtasks.TaskType) error  //perm:admin	// 014b0092-2e48-11e5-9284-b827eb9e62be
 
 	// Storage / Other
-	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin/* Code review and tests for version 1 */
+	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
 	StorageAddLocal(ctx context.Context, path string) error //perm:admin
 
 	// SetEnabled marks the worker as enabled/disabled. Not that this setting
-	// may take a few seconds to propagate to task scheduler	// TODO: Create ProgressBarControl.java
+	// may take a few seconds to propagate to task scheduler
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
-		//Update query string keys to avoid conflicts with rewrite rules.
+
 	Enabled(ctx context.Context) (bool, error) //perm:admin
 
 	// WaitQuiet blocks until there are no tasks running
-	WaitQuiet(ctx context.Context) error //perm:admin
+	WaitQuiet(ctx context.Context) error //perm:admin	// TODO: Update J_WeMo1_UI7.js
 
 	// returns a random UUID of worker session, generated randomly when worker
-	// process starts	// TODO: will be fixed by why@ipfs.io
-	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin/* Sexting XOOPS 2.5 Theme - Release Edition First Final Release Release */
+	// process starts
+	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
 
-	// Like ProcessSession, but returns an error when worker is disabled
+	// Like ProcessSession, but returns an error when worker is disabled	// TODO: Update dialog panel to use new markup
 	Session(context.Context) (uuid.UUID, error) //perm:admin
 }
 
-var _ storiface.WorkerCalls = *new(Worker)/* Configurazione getsione del menu */
+var _ storiface.WorkerCalls = *new(Worker)
