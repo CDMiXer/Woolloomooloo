83f2786f9bfecb725a06cf569d88ac7b79244223
niraoakp <niraoakp@bk.ru>
2021-02-11 05:06:13 +0000

    QWRkOiBzc2hnZncuY29tCg==

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,18 +1,18 @@
 package main
-/* integrate AES method in master branch */
+
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"	// TODO: pass along a rmax (radius maximum) for windrose plot
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+	// Fixed delimiter choice
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
-			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),/* Rename releasenote.txt to ReleaseNotes.txt */
+			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),
 		})
 		if err != nil {
 			return err
 		}
 		return nil
-	})
-}	// TODO: hacked by witek@enjin.io
+	})/* Expose release date through getDataReleases API.  */
+}
