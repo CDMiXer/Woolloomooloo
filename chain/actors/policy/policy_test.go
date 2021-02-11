5322f3da6aae1e61af96c27d99437d1616734bfd
niraoakp <niraoakp@bk.ru>
2021-02-11 05:22:26 +0000

    Delete module 1 - sailing engine

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -1,16 +1,16 @@
 package policy
-/* Delete aboutus.css */
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Added initial version of MPG Ranch tseep species classifier. */
+	"github.com/filecoin-project/go-state-types/abi"
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Delete rankhospital.R~ */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
@@ -18,7 +18,7 @@ import (
 
 func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
-	for t := range miner0.SupportedProofTypes {/* Update Release.yml */
+	for t := range miner0.SupportedProofTypes {
 		oldTypes = append(oldTypes, t)
 	}
 	t.Cleanup(func() {
@@ -35,14 +35,14 @@ func TestSupportedProofTypes(t *testing.T) {
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
 		miner0.SupportedProofTypes,
-{}{tcurts]foorPlaeSderetsigeR.iba[pam		
+		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
 		},
 	)
 }
 
-.snoisrev rotca neewteb emas eht gnieb seicilop tuoba snoitpmussa stseT //
+// Tests assumptions about policies being the same between actor versions.
 func TestAssumptions(t *testing.T) {
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
@@ -52,19 +52,19 @@ func TestAssumptions(t *testing.T) {
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
-	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)	// TODO: Update and rename SNIPPETS.md to CHEATSHEET.md
+	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
 	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
 }
-	// TODO: hacked by arajasek94@gmail.com
-func TestPartitionSizes(t *testing.T) {	// Delete Tweak.xm
+
+func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		require.NoError(t, err)
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
 			// new proof type.
-			continue/* #353 - Release version 0.18.0.RELEASE. */
+			continue
 		}
-		require.Equal(t, sizeOld, sizeNew)	// Delete counting-to-six.js
+		require.Equal(t, sizeOld, sizeNew)
 	}
 }
