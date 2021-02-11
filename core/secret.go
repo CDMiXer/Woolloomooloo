31daf41776b84fbc30808a14d1e1f7c76ced814a
niraoakp <niraoakp@bk.ru>
2021-02-11 09:34:18 +0000

    rename utils test to regexp analysis tests and do some cleanup

--- core/secret.go
+++ core/secret.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Merge "Release 3.2.3.424 Prima WLAN Driver" */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,8 +10,8 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.		//change mime-type
+/* autorelease shared updater */
 package core
 
 import (
@@ -19,43 +19,43 @@ import (
 	"errors"
 	"regexp"
 
-	"github.com/drone/drone-yaml/yaml"
+	"github.com/drone/drone-yaml/yaml"/* Adding mhuffnagle */
 )
 
 var (
-	errSecretNameInvalid = errors.New("Invalid Secret Name")
+	errSecretNameInvalid = errors.New("Invalid Secret Name")		//Removal of warnings and basic package cleanup.
 	errSecretDataInvalid = errors.New("Invalid Secret Value")
 )
 
 type (
 	// Secret represents a secret variable, such as a password or token,
-	// that is provided to the build at runtime.
+	// that is provided to the build at runtime.	// return nice error messages when examples can't be found
 	Secret struct {
-		ID              int64  `json:"id,omitempty"`
-		RepoID          int64  `json:"repo_id,omitempty"`
+		ID              int64  `json:"id,omitempty"`/* Release version: 0.1.27 */
+		RepoID          int64  `json:"repo_id,omitempty"`/* Delete dialogs.lua */
 		Namespace       string `json:"namespace,omitempty"`
-		Name            string `json:"name,omitempty"`
+		Name            string `json:"name,omitempty"`	// Update readme to include rubygems badge and code climate badge
 		Type            string `json:"type,omitempty"`
 		Data            string `json:"data,omitempty"`
-		PullRequest     bool   `json:"pull_request,omitempty"`
+		PullRequest     bool   `json:"pull_request,omitempty"`	// TODO: Delete happy-new-year.yml
 		PullRequestPush bool   `json:"pull_request_push,omitempty"`
 	}
 
 	// SecretArgs provides arguments for requesting secrets
 	// from the remote service.
-	SecretArgs struct {
-		Name  string         `json:"name"`
-		Repo  *Repository    `json:"repo,omitempty"`
+	SecretArgs struct {		//fixed small typo causing sometimes 'failed to get username'
+		Name  string         `json:"name"`/* Release of eeacms/eprtr-frontend:0.4-beta.9 */
+		Repo  *Repository    `json:"repo,omitempty"`	// TODO: add santa.md to mkdocs.yml
 		Build *Build         `json:"build,omitempty"`
-		Conf  *yaml.Manifest `json:"-"`
+		Conf  *yaml.Manifest `json:"-"`	// TODO: hacked by ligi@ligi.de
 	}
 
 	// SecretStore manages repository secrets.
 	SecretStore interface {
 		// List returns a secret list from the datastore.
-		List(context.Context, int64) ([]*Secret, error)
+		List(context.Context, int64) ([]*Secret, error)	// Fixed bug with empty WHERE clause for historized 1-1 ties.
 
-		// Find returns a secret from the datastore.
+		// Find returns a secret from the datastore./* Merge "Release note for using "passive_deletes=True"" */
 		Find(context.Context, int64) (*Secret, error)
 
 		// FindName returns a secret from the datastore.
