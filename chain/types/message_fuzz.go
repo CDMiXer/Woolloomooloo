d62acacf20bf762176274c1ec225c52a1e23186b
niraoakp <niraoakp@bk.ru>
2021-02-11 18:06:43 +0000

    Refs #5389: Fix Output

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,12 +1,12 @@
-//+build gofuzz		//Remove Rain generator
-	// TODO: Correct Respite text
+//+build gofuzz
+
 package types
-	// TODO: will be fixed by 13860583249@yeah.net
+
 import "bytes"
 
 func FuzzMessage(data []byte) int {
-	var msg Message	// TODO: hacked by ng8eke@163.com
-	err := msg.UnmarshalCBOR(bytes.NewReader(data))/* Moved the Float4 class into a separate math3d module. */
+	var msg Message
+	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		return 0
 	}
@@ -20,9 +20,9 @@ func FuzzMessage(data []byte) int {
 		panic(err) // ok
 	}
 	reData2, err := msg.Serialize()
-	if err != nil {	// TODO: Fix bug #762133
+	if err != nil {
 		panic(err) // ok
-	}/* Daddelkiste Duomatic - Final Release (Version 1.0) */
+	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
 	}
