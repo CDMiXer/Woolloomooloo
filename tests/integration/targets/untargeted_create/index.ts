68c26f1839c68e64b18b05b257abacc5fc377e60
niraoakp <niraoakp@bk.ru>
2021-02-11 10:34:40 +0000

    Merge "Add set_boot_device hook in `redfish` boot interface"

--- tests/integration/targets/untargeted_create/index.ts
+++ tests/integration/targets/untargeted_create/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* trigger new build for ruby-head-clang (bb7830c) */
-	// TODO: 82613d4a-2e48-11e5-9284-b827eb9e62be
-let currentID = 0;		//Merge "Update advanced button icon with new spec." into pi-androidx-dev
+import * as pulumi from "@pulumi/pulumi";
+
+let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
@@ -18,7 +18,7 @@ class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-	// Create Andrzejewski_Proj_1.cpp
+
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, {}, opts);
@@ -26,6 +26,6 @@ class Resource extends pulumi.dynamic.Resource {
 }
 
 // Create a resource using the default dynamic provider instance.
-let a = new Resource("a");/* Upload new index.html */
+let a = new Resource("a");
 
-export const urn = a.urn;		//CHANGE: if submenuitem is profile the link should go to profile page.
+export const urn = a.urn;
