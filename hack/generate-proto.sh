5af774a96582775e0ffabad95a4337f47d03ce43
niraoakp <niraoakp@bk.ru>
2021-02-11 13:16:09 +0000

    Do not log on DEBUG

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,25 +1,25 @@
-#!/bin/bash/* Release 2.29.3 */
-set -eux -o pipefail	// TODO: will be fixed by ng8eke@163.com
+#!/bin/bash
+set -eux -o pipefail	// TODO: set debugging to off by default in sql install script
 
-go mod vendor/* Add the file */
-	// TODO: Delete switchery.min.js
+go mod vendor
+
 ${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
   --proto-import ./vendor
-	// TODO: new pending order
+
 for f in $(find pkg -name '*.proto'); do
-  protoc \
-    -I /usr/local/include \		//Merge "Upgrade version minor hardcodes to v2.2.x"
-    -I . \	// TODO: will be fixed by martin2cai@hotmail.com
+  protoc \/* 0.19.3: Maintenance Release (close #58) */
+    -I /usr/local/include \
+    -I . \
     -I ./vendor \
     -I ${GOPATH}/src \
     -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
     -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
     --gogofast_out=plugins=grpc:${GOPATH}/src \
     --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
-    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
+    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \		//https-only alb -> webfleet
     $f
 done
 
