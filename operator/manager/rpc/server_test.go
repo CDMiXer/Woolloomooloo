6c7592416ba8bfa92fa0d3718f13fae88a21b4e4
niraoakp <niraoakp@bk.ru>
2021-02-11 00:43:48 +0000

    fixed missing rights for trial objects

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "EditText notifies the IME when a suggestion is picked." */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by steven@stebalien.com
+
 // +build !oss
 
 package rpc
