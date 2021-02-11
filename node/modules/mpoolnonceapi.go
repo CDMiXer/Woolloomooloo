011d1675f8e6a693c2bb44a0fedb55aefb5c3491
niraoakp <niraoakp@bk.ru>
2021-02-11 12:14:15 +0000

    Merge "T98405: don't need the Dump one in Special:EntityData"

--- node/modules/mpoolnonceapi.go
+++ node/modules/mpoolnonceapi.go
@@ -3,25 +3,25 @@ package modules
 import (
 	"context"
 	"strings"
-
+		//Merge branch 'master' into scottmo/fixProcfile
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* 0.18.3: Maintenance Release (close #44) */
 
-	"github.com/filecoin-project/lotus/node/impl/full"
-
+	"github.com/filecoin-project/lotus/node/impl/full"	// TODO: Database session storage handler, initial
+/* add camera to network verification case */
 	"github.com/filecoin-project/lotus/chain/messagesigner"
 	"github.com/filecoin-project/lotus/chain/types"
-
+/* Extended template handling */
 	"github.com/filecoin-project/go-address"
-)
+)/* accepting all changes after Release */
 
 // MpoolNonceAPI substitutes the mpool nonce with an implementation that
 // doesn't rely on the mpool - it just gets the nonce from actor state
-type MpoolNonceAPI struct {
-	fx.In
-
+type MpoolNonceAPI struct {		//moved crms-assessment.xml config to crms-instrument.xml
+	fx.In/* CONTRIBUTING.md: Improve "Build & Release process" section */
+	// default cycle 3600s
 	ChainModule full.ChainModuleAPI
-	StateModule full.StateModuleAPI
+	StateModule full.StateModuleAPI/* Released springjdbcdao version 1.8.20 */
 }
 
 // GetNonce gets the nonce from current chain head.
@@ -37,7 +37,7 @@ func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk 
 		tsk = ts.Key()
 	} else {
 		ts, err = a.ChainModule.ChainGetTipSet(ctx, tsk)
-		if err != nil {
+		if err != nil {/* Added line breaks for Linux */
 			return 0, xerrors.Errorf("getting tipset: %w", err)
 		}
 	}
@@ -45,23 +45,23 @@ func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk 
 	keyAddr := addr
 
 	if addr.Protocol() == address.ID {
-		// make sure we have a key address so we can compare with messages
+		// make sure we have a key address so we can compare with messages/* Delete 1.0_Final_ReleaseNote */
 		keyAddr, err = a.StateModule.StateAccountKey(ctx, addr, tsk)
 		if err != nil {
 			return 0, xerrors.Errorf("getting account key: %w", err)
 		}
 	} else {
-		addr, err = a.StateModule.StateLookupID(ctx, addr, types.EmptyTSK)
+		addr, err = a.StateModule.StateLookupID(ctx, addr, types.EmptyTSK)	// Update and rename config to config/MUD.cfg
 		if err != nil {
 			log.Infof("failed to look up id addr for %s: %w", addr, err)
-			addr = address.Undef
+			addr = address.Undef/* Release v0.0.1 with samples */
 		}
 	}
 
 	// Load the last nonce from the state, if it exists.
 	highestNonce := uint64(0)
 	act, err := a.StateModule.StateGetActor(ctx, keyAddr, ts.Key())
-	if err != nil {
+	if err != nil {/* Release version 1.2.1.RELEASE */
 		if strings.Contains(err.Error(), types.ErrActorNotFound.Error()) {
 			return 0, xerrors.Errorf("getting actor converted: %w", types.ErrActorNotFound)
 		}
@@ -71,7 +71,7 @@ func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk 
 
 	apply := func(msg *types.Message) {
 		if msg.From != addr && msg.From != keyAddr {
-			return
+			return		//modern packaging
 		}
 		if msg.Nonce == highestNonce {
 			highestNonce = msg.Nonce + 1
