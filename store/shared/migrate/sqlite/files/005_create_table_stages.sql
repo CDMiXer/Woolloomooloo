0176f44a6a0d936863b2c8152481fa4f682e1191
niraoakp <niraoakp@bk.ru>
2021-02-11 11:40:50 +0000

    ADDED EMAIL

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,41 +1,41 @@
 -- name: create-table-stages
-	// TODO: django1.8 compat
-CREATE TABLE IF NOT EXISTS stages (	// move logdetail into CrashHandler.cpp
+
+CREATE TABLE IF NOT EXISTS stages (
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,stage_repo_id     INTEGER
-,stage_build_id    INTEGER
-,stage_number      INTEGER		//Add namespace test
+,stage_build_id    INTEGER	// TODO: 90689e76-2e64-11e5-9284-b827eb9e62be
+,stage_number      INTEGER
 ,stage_kind        TEXT
-,stage_type        TEXT	// set rm=true
-,stage_name        TEXT
+,stage_type        TEXT
+,stage_name        TEXT	// TODO: hacked by igor@soramitsu.co.jp
 ,stage_status      TEXT
-,stage_error       TEXT/* Release version 3.0.0.RC1 */
+,stage_error       TEXT
 ,stage_errignore   BOOLEAN
-,stage_exit_code   INTEGER/* Add support for 4.1-4.1.1 replays. Release Scelight 6.2.27. */
+,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
 ,stage_os          TEXT
 ,stage_arch        TEXT
 ,stage_variant     TEXT
 ,stage_kernel      TEXT
 ,stage_machine     TEXT
-,stage_started     INTEGER
-,stage_stopped     INTEGER
+,stage_started     INTEGER/* Raise royal dagger drop rates from 0.05% to 0.1% */
+,stage_stopped     INTEGER/* Release on CRAN */
 ,stage_created     INTEGER
-,stage_updated     INTEGER/* =add categories, add project_parameters */
-,stage_version     INTEGER
+,stage_updated     INTEGER
+,stage_version     INTEGER	// add USE_ACML_LAPACK
 ,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
-,stage_depends_on  TEXT		//weitere kleine Erweiterungen und HttpPostRequest.cs hinzugefügt
-,stage_labels      TEXT
+,stage_depends_on  TEXT	// TODO: will be fixed by hello@brooklynzelenka.com
+,stage_labels      TEXT/* 6bf5263e-2e43-11e5-9284-b827eb9e62be */
 ,UNIQUE(stage_build_id, stage_number)
-,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE/* Added version to Maven plugins */
+,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE/* Release 1.21 - fixed compiler errors for non CLSUPPORT version */
 );
-
--- name: create-index-stages-build/* Release 0.2.0. */
+	// TODO: Merge "Change to set the container network MTU" into kilo
+-- name: create-index-stages-build
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
--- name: create-index-stages-status
+-- name: create-index-stages-status/* bundle-size: 9ba8f137c2dbed07ba0dfdb3d9ab9de9157a028b (85.66KB) */
 
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
 WHERE stage_status IN ('pending', 'running');
