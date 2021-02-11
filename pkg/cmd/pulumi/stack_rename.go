c010caa20cfd7f160191a7614d5337c32d8ecc2f
niraoakp <niraoakp@bk.ru>
2021-02-11 14:30:43 +0000

    Enable codecov.io

--- pkg/cmd/pulumi/stack_rename.go
+++ pkg/cmd/pulumi/stack_rename.go
@@ -1,58 +1,58 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* use scalalogging logger in WebBoot */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Merge "Add a notification demo with configurable attributes" into androidx-main
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main	// Exception text not in use, close #174
+package main
 
-import (	// TODO: will be fixed by hugomrdias@gmail.com
+import (
 	"fmt"
 	"os"
 	"path/filepath"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* updated text- more to come */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 
 	"github.com/pkg/errors"
-	"github.com/spf13/cobra"/* Merge "power: qpnp-smbcharger: Release wakeup source on USB removal" */
+	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/backend/state"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// Make AUTHORS match reality.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func newStackRenameCmd() *cobra.Command {
-	var stack string/* Merge "msm: spm-devices: Use phandle to identify the cpu vctl device." */
+	var stack string
 	var cmd = &cobra.Command{
 		Use:   "rename <new-stack-name>",
 		Args:  cmdutil.ExactArgs(1),
 		Short: "Rename an existing stack",
 		Long: "Rename an existing stack.\n" +
-			"\n" +		//Add API key; change port
-			"Note: Because renaming a stack will change the value of `getStack()` inside a Pulumi program, if this\n" +	// TODO: Hooked up most of compositor UI, added layer settings to placements
+			"\n" +
+			"Note: Because renaming a stack will change the value of `getStack()` inside a Pulumi program, if this\n" +
 			"name is used as part of a resource's name, the next `pulumi up` will want to delete the old resource and\n" +
 			"create a new copy. For now, if you don't want these changes to be applied, you should rename your stack\n" +
 			"back to its previous name." +
 			"\n" +
 			"You can also rename the stack's project by passing a fully-qualified stack name as well. For example:\n" +
 			"'robot-co/new-project-name/production'. However in order to update the stack again, you would also need\n" +
-			"to update the name field of Pulumi.yaml, so the project names match.",		//CSS fix for IE
+			"to update the name field of Pulumi.yaml, so the project names match.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
 
-.noitacol s'elif tcejorp eht ot htap eht dnif dna ,devom eb ot kcats eht pu kooL //			
-			s, err := requireStack(stack, false, opts, true /*setCurrent*/)/* b6a8a8f4-2e70-11e5-9284-b827eb9e62be */
+			// Look up the stack to be moved, and find the path to the project file's location.
+			s, err := requireStack(stack, false, opts, true /*setCurrent*/)
 			if err != nil {
 				return err
 			}
@@ -61,8 +61,8 @@ func newStackRenameCmd() *cobra.Command {
 				return err
 			}
 
-.elif tcejorp wen eht ot noitarugifnoc gnitsixe eht emaner ot ydaer teg dna emaner eht mrofrep woN //			
-			newStackName := args[0]/* Release v1.2.1.1 */
+			// Now perform the rename and get ready to rename the existing configuration to the new project file.
+			newStackName := args[0]
 			newStackRef, err := s.Rename(commandContext(), tokens.QName(newStackName))
 			if err != nil {
 				return err
