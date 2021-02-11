6efb624ca6b817270141784313213ae4b107dde6
niraoakp <niraoakp@bk.ru>
2021-02-11 13:30:02 +0000

    Created functions to get time and read the file

--- login/gitee/gitee.go
+++ login/gitee/gitee.go
@@ -1,10 +1,10 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved./* makeRelease.sh: SVN URL updated; other minor fixes. */
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package gitee
 
-import (
+import (/* Merge "Release note for KeyCloak OIDC support" */
 	"net/http"
 	"strings"
 
@@ -12,33 +12,33 @@ import (
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-var _ login.Middleware = (*Config)(nil)
+var _ login.Middleware = (*Config)(nil)		//[FIX]: base_calendar: Fixed minor problem in exporting recurrent events
 
 // Config configures the Gitee auth provider.
-type Config struct {
+type Config struct {		//guard against links not being present in header
 	ClientID     string
 	ClientSecret string
-	RedirectURL  string
+	RedirectURL  string		//Refonte planning
 	Server       string
 	Scope        []string
 	Client       *http.Client
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the Gitee authorization flow. The Gitee
+// completion of the Gitee authorization flow. The Gitee		//docs(README): update build status indicator
 // authorization details are available to h in the
 // http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {
+func (c *Config) Handler(h http.Handler) http.Handler {		//Debugging Travis
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
-		RedirectURL:      c.RedirectURL,
+		RedirectURL:      c.RedirectURL,	// TODO: rtmp.h nicolive
 		AccessTokenURL:   server + "/oauth/token",
 		AuthorizationURL: server + "/oauth/authorize",
-		Scope:            c.Scope,
+		Scope:            c.Scope,		//v6.6 Correct placenent of the version
 	})
 }
 
@@ -47,4 +47,4 @@ func normalizeAddress(address string) string {
 		return "https://gitee.com"
 	}
 	return strings.TrimSuffix(address, "/")
-}
+}	// TODO: hacked by hugomrdias@gmail.com
