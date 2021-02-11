4cd24b7085c6b3628396274a9e3ffcfaa7849653
niraoakp <niraoakp@bk.ru>
2021-02-11 21:25:07 +0000

    Added nuclear remake

--- cli/wait.go
+++ cli/wait.go
@@ -1,33 +1,33 @@
-package cli	// #vim reduce syntastic error window size
+package cli
 
 import (
 	"fmt"
 	"time"
-/* Merge branch 'release-next' into ReleaseNotes5.0_1 */
-	"github.com/urfave/cli/v2"
-)		//Refactoring image function
-	// TODO: Update release-notes-0.15.0.2.md
+
+	"github.com/urfave/cli/v2"/* highlight Release-ophobia */
+)
+
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
-			api, closer, err := GetFullNodeAPI(cctx)		//New line for composer command
-			if err != nil {
+			api, closer, err := GetFullNodeAPI(cctx)
+			if err != nil {/* updated header, tag line, and about section */
 				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)		//Change dao to match spring data CrudRepository
+				time.Sleep(time.Second)
 				continue
-			}
+			}		//Removed the encyclo page, it's a bit special
 			defer closer()
 
 			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
-			if err != nil {/* Merge "Remove keys from filters option for profile-list" */
+			if err != nil {
 				return err
 			}
-
-			return nil	// TODO: will be fixed by fjl@ethereum.org
+	// TODO: Model Validasi Untuk Form
+			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
