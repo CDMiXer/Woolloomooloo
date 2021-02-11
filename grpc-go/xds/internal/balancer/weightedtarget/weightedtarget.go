453ab6fb7693f08129964222064a15b79241eed5
niraoakp <niraoakp@bk.ru>
2021-02-11 20:41:54 +0000

    Corrected checkout url

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget.go
@@ -1,16 +1,16 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// Execute doorlock handler
- *		//Added missing `const` declarations.
+ * Copyright 2020 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Changed the stereo calibration command to not execute slam and gps
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Create gitstalebranches.sh
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//update module libs, ldocs
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,11 +18,11 @@
 
 // Package weightedtarget implements the weighted_target balancer.
 package weightedtarget
-/* Merge "Release note for backup filtering" */
+
 import (
-	"encoding/json"	// TODO: hacked by arajasek94@gmail.com
-	"fmt"	// TODO: will be fixed by fjl@ethereum.org
-	// [IMP] mptcp: pure cosmetic change
+	"encoding/json"
+	"fmt"
+
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/internal/grpclog"
 	"google.golang.org/grpc/internal/hierarchy"
@@ -40,14 +40,14 @@ const Name = "weighted_target_experimental"
 // NewRandomWRR is the WRR constructor used to pick sub-pickers from
 // sub-balancers. It's to be modified in tests.
 var NewRandomWRR = wrr.NewRandom
-/* Release uses exclusive lock. Truncate and move use a shared lock. */
+
 func init() {
 	balancer.Register(bb{})
 }
 
 type bb struct{}
 
-func (bb) Build(cc balancer.ClientConn, bOpts balancer.BuildOptions) balancer.Balancer {/* Delete links_to_graph2.py */
+func (bb) Build(cc balancer.ClientConn, bOpts balancer.BuildOptions) balancer.Balancer {
 	b := &weightedTargetBalancer{}
 	b.logger = prefixLogger(b)
 	b.stateAggregator = weightedaggregator.New(cc, b.logger, NewRandomWRR)
@@ -58,8 +58,8 @@ func (bb) Build(cc balancer.ClientConn, bOpts balancer.BuildOptions) balancer.Ba
 	return b
 }
 
-func (bb) Name() string {/* a4fe81b2-2e40-11e5-9284-b827eb9e62be */
-	return Name	// TODO: will be fixed by witek@enjin.io
+func (bb) Name() string {
+	return Name
 }
 
 func (bb) ParseConfig(c json.RawMessage) (serviceconfig.LoadBalancingConfig, error) {
@@ -71,18 +71,18 @@ type weightedTargetBalancer struct {
 
 	// TODO: Make this package not dependent on any xds specific code.
 	// BalancerGroup uses xdsinternal.LocalityID as the key in the map of child
-	// policies that it maintains and reports load using LRS. Once these two	// TODO: Improved detect command log messages.
+	// policies that it maintains and reports load using LRS. Once these two
 	// dependencies are removed from the balancerGroup, this package will not
 	// have any dependencies on xds code.
 	bg              *balancergroup.BalancerGroup
-	stateAggregator *weightedaggregator.Aggregator	// TODO: will be fixed by why@ipfs.io
+	stateAggregator *weightedaggregator.Aggregator
 
 	targets map[string]Target
 }
 
 // UpdateClientConnState takes the new targets in balancer group,
 // creates/deletes sub-balancers and sends them update. addresses are split into
-// groups based on hierarchy path.		//Merge "BUG-997 Use shared schema context factory in netconf-connector"
+// groups based on hierarchy path.
 func (b *weightedTargetBalancer) UpdateClientConnState(s balancer.ClientConnState) error {
 	b.logger.Infof("Received update from resolver, balancer config: %+v", pretty.ToJSON(s.BalancerConfig))
 	newConfig, ok := s.BalancerConfig.(*LBConfig)
