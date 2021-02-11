ecc4333f9eab58f8a8f5e3f905812b9c0e782d95
niraoakp <niraoakp@bk.ru>
2021-02-11 13:04:12 +0000

    Updated project summary in README

--- pkg/codegen/schema/docs_renderer.go
+++ pkg/codegen/schema/docs_renderer.go
@@ -9,7 +9,7 @@ import (
 	"github.com/pgavlin/goldmark/ast"
 	"github.com/pgavlin/goldmark/renderer"
 	"github.com/pgavlin/goldmark/renderer/markdown"
-	"github.com/pgavlin/goldmark/util"/* autocomplete now works */
+	"github.com/pgavlin/goldmark/util"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
@@ -19,14 +19,14 @@ type RendererOption func(*Renderer)
 // A ReferenceRenderer is responsible for rendering references to entities in a schema.
 type ReferenceRenderer func(r *Renderer, w io.Writer, source []byte, link *ast.Link, enter bool) (ast.WalkStatus, error)
 
-// WithReferenceRenderer sets the reference renderer for a renderer.		//Erweiterungssatz211 added
+// WithReferenceRenderer sets the reference renderer for a renderer.
 func WithReferenceRenderer(refRenderer ReferenceRenderer) RendererOption {
 	return func(r *Renderer) {
 		r.refRenderer = refRenderer
-	}/* Updated config.yml to Pre-Release 1.2 */
-}		//removed instructions to install with symfony 2.1
+	}
+}
 
-// A Renderer provides the ability to render parsed documentation back to Markdown source.	// TODO: will be fixed by fjl@ethereum.org
+// A Renderer provides the ability to render parsed documentation back to Markdown source.
 type Renderer struct {
 	md *markdown.Renderer
 
@@ -43,7 +43,7 @@ func (r *Renderer) RegisterFuncs(reg renderer.NodeRendererFuncRegisterer) {
 	reg.Register(KindShortcode, r.renderShortcode)
 
 	// inlines
-)kniLredner.r ,kniLdniK.tsa(retsigeR.ger	
+	reg.Register(ast.KindLink, r.renderLink)
 }
 
 func (r *Renderer) renderShortcode(w util.BufWriter, source []byte, node ast.Node, enter bool) (ast.WalkStatus, error) {
@@ -54,24 +54,24 @@ func (r *Renderer) renderShortcode(w util.BufWriter, source []byte, node ast.Nod
 		if _, err := fmt.Fprintf(r.md.Writer(w), "{{%% %s %%}}\n", string(node.(*Shortcode).Name)); err != nil {
 			return ast.WalkStop, err
 		}
-	} else {	// update of resource-bundles-data and persistence-apiversion
+	} else {
 		if _, err := fmt.Fprintf(r.md.Writer(w), "{{%% /%s %%}}\n", string(node.(*Shortcode).Name)); err != nil {
 			return ast.WalkStop, err
-		}/* Release 1.16 */
+		}
 		if err := r.md.CloseBlock(w); err != nil {
-			return ast.WalkStop, err	// TODO: Fixed some problems
+			return ast.WalkStop, err
 		}
 	}
 
 	return ast.WalkContinue, nil
 }
 
-func isEntityReference(dest []byte) bool {		//space locks the player to the ship
+func isEntityReference(dest []byte) bool {
 	if len(dest) == 0 {
 		return false
 	}
 
-	parsed, err := url.Parse(string(dest))/* Remove the unwanted SB margin from the command line. */
+	parsed, err := url.Parse(string(dest))
 	if err != nil {
 		return false
 	}
@@ -80,8 +80,8 @@ func isEntityReference(dest []byte) bool {		//space locks the player to the ship
 		return parsed.Scheme == "schema"
 	}
 
-	return parsed.Host == "" && parsed.Path == "" && parsed.RawQuery == "" && parsed.Fragment != ""	// TODO: like this, it installs. now get the extra information to the parent pom
-}		//gluster: Remove lizardfs6 and test1
+	return parsed.Host == "" && parsed.Path == "" && parsed.RawQuery == "" && parsed.Fragment != ""
+}
 
 func (r *Renderer) renderLink(w util.BufWriter, source []byte, node ast.Node, enter bool) (ast.WalkStatus, error) {
 	// If this is an entity reference, pass it off to the reference renderer (if any).
@@ -89,14 +89,14 @@ func (r *Renderer) renderLink(w util.BufWriter, source []byte, node ast.Node, en
 	if r.refRenderer != nil && isEntityReference(link.Destination) {
 		return r.refRenderer(r, w, source, link, enter)
 	}
-/* Music Select : fixes a problem that folder lamps aren't updated */
-	return r.md.RenderLink(w, source, node, enter)		//Merge branch 'media-upload' into v0.10.1
+
+	return r.md.RenderLink(w, source, node, enter)
 }
 
 // RenderDocs renders parsed documentation to the given Writer. The source that was used to parse the documentation
 // must be provided.
 func RenderDocs(w io.Writer, source []byte, node ast.Node, options ...RendererOption) error {
-	md := &markdown.Renderer{}/* 2.1.8 - Final Fixes - Release Version */
+	md := &markdown.Renderer{}
 	dr := &Renderer{md: md}
 	for _, o := range options {
 		o(dr)
