505c09656f7ed617dcbfb685c7452f5945bd5561
niraoakp <niraoakp@bk.ru>
2021-02-11 02:18:26 +0000

    Code in den Views ausgetauscht

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,62 +1,62 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Merge "cosmetics: shorten long line"
+// Copyright 2019 Drone IO, Inc.
+//	// TODO: #289 Associate agents with targets
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added Release mode DLL */
+// you may not use this file except in compliance with the License.		//Remove Cancel button from Timer Record progress dialog.
+// You may obtain a copy of the License at		//Delete Messages_nb_NO.properties
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0/* Merge branch 'PlayerInteraction' into Release1 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//Category jobs RSS feed
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Create tz.yml
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Don't overeat
+// See the License for the specific language governing permissions and	// TODO: Use folder mount
 // limitations under the License.
 
 package deploys
 
-import (
-	"net/http"/* Add Nordic Venture Family to organizations list */
-
+import (/* Catch FacebookError in the connection middleware. */
+	"net/http"
+/* Fix typo in phpdoc. props fanquake. fixes #23737. */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-		//no need to specify scm:tag
-	"github.com/go-chi/chi"
+		//register the driver
+	"github.com/go-chi/chi"/* Release 1.1.0-CI00271 */
 )
 
-// HandleDelete returns an http.HandlerFunc that handles an
-// http.Request to delete a branch entry from the datastore.
-func HandleDelete(/* Update scriptOne.js */
+// HandleDelete returns an http.HandlerFunc that handles an	// TODO: Create RGDL.md
+// http.Request to delete a branch entry from the datastore.		//bfb1a1bc-2e5c-11e5-9284-b827eb9e62be
+func HandleDelete(/* WTWP-E -> MIMP-E */
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* Release v0.9-beta.7 */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			target    = chi.URLParam(r, "*")/* 4.0.7 Release changes */
+			target    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* 0.7 Release */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}/* ed85ec24-2e70-11e5-9284-b827eb9e62be */
+		}
 
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* Released springjdbcdao version 1.7.17 */
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete deployment")
 		} else {
-			w.WriteHeader(http.StatusNoContent)	// TODO: hacked by mikeal.rogers@gmail.com
+			w.WriteHeader(http.StatusNoContent)
 		}
 	}
-}		//.travis.yml: Install Swig and Python to test builds of Python bindings as well.
+}
