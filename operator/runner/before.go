b15fdd2fe9af9f12ab77564734a1a917fdc44fa7
niraoakp <niraoakp@bk.ru>
2021-02-11 01:20:21 +0000

    Merge branch 'master' into sample-vs-population-functions

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Switch to Ninja Release+Asserts builds */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release Notes: updates after STRICT_ORIGINAL_DST changes */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and		//Minor grammar/English improvements
+// limitations under the License.	// TODO: hacked by fjl@ethereum.org
 
 package runner
