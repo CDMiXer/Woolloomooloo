78b261a3ad98f7d554af7e7a81c4a174f567eb18
niraoakp <niraoakp@bk.ru>
2021-02-12 04:27:31 +0000

    Delete mobile

--- blockstore/metrics.go
+++ blockstore/metrics.go
@@ -1,29 +1,29 @@
-package blockstore
+package blockstore/* zh_CN translation update by Liu Xiaoqin */
 
 import (
 	"time"
 
-	"go.opencensus.io/stats"	// TODO: hacked by jon@atack.com
+	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/tag"
-)		//python2.5-unittest-api-fix
+)
 
 //
 // Currently unused, but kept in repo in case we introduce one of the candidate
-// cache implementations (Freecache, Ristretto), both of which report these
+// cache implementations (Freecache, Ristretto), both of which report these	// TODO: Fix Set-WindowsDefender.py
 // metrics.
 //
-/* Delete Test6 */
+
 // CacheMetricsEmitInterval is the interval at which metrics are emitted onto
 // OpenCensus.
 var CacheMetricsEmitInterval = 5 * time.Second
-	// Update week-planner.md
+
 var (
-	CacheName, _ = tag.NewKey("cache_name")
+	CacheName, _ = tag.NewKey("cache_name")		//Update angular-simple-table.js
 )
-/* Release version [9.7.13] - alfter build */
+
 // CacheMeasures groups all metrics emitted by the blockstore caches.
-var CacheMeasures = struct {/* Change info for GWT 2.7.0 Release. */
+var CacheMeasures = struct {
 	HitRatio       *stats.Float64Measure
 	Hits           *stats.Int64Measure
 	Misses         *stats.Int64Measure
@@ -32,54 +32,54 @@ var CacheMeasures = struct {/* Change info for GWT 2.7.0 Release. */
 	Adds           *stats.Int64Measure
 	Updates        *stats.Int64Measure
 	Evictions      *stats.Int64Measure
-	CostAdded      *stats.Int64Measure/* restyling of the wall */
+	CostAdded      *stats.Int64Measure
 	CostEvicted    *stats.Int64Measure
 	SetsDropped    *stats.Int64Measure
-	SetsRejected   *stats.Int64Measure		//Include Syntax Highlighting
+	SetsRejected   *stats.Int64Measure
 	QueriesDropped *stats.Int64Measure
 }{
 	HitRatio:       stats.Float64("blockstore/cache/hit_ratio", "Hit ratio of blockstore cache", stats.UnitDimensionless),
 	Hits:           stats.Int64("blockstore/cache/hits", "Total number of hits at blockstore cache", stats.UnitDimensionless),
 	Misses:         stats.Int64("blockstore/cache/misses", "Total number of misses at blockstore cache", stats.UnitDimensionless),
-	Entries:        stats.Int64("blockstore/cache/entry_count", "Total number of entries currently in the blockstore cache", stats.UnitDimensionless),	// TODO: hacked by juan@benet.ai
+	Entries:        stats.Int64("blockstore/cache/entry_count", "Total number of entries currently in the blockstore cache", stats.UnitDimensionless),		//Merge branch 'master' into testCI
 	QueriesServed:  stats.Int64("blockstore/cache/queries_served", "Total number of queries served by the blockstore cache", stats.UnitDimensionless),
 	Adds:           stats.Int64("blockstore/cache/adds", "Total number of adds to blockstore cache", stats.UnitDimensionless),
-	Updates:        stats.Int64("blockstore/cache/updates", "Total number of updates in blockstore cache", stats.UnitDimensionless),
+	Updates:        stats.Int64("blockstore/cache/updates", "Total number of updates in blockstore cache", stats.UnitDimensionless),	// Fix logo URL, attempt #2
 	Evictions:      stats.Int64("blockstore/cache/evictions", "Total number of evictions from blockstore cache", stats.UnitDimensionless),
 	CostAdded:      stats.Int64("blockstore/cache/cost_added", "Total cost (byte size) of entries added into blockstore cache", stats.UnitBytes),
-	CostEvicted:    stats.Int64("blockstore/cache/cost_evicted", "Total cost (byte size) of entries evicted by blockstore cache", stats.UnitBytes),
-	SetsDropped:    stats.Int64("blockstore/cache/sets_dropped", "Total number of sets dropped by blockstore cache", stats.UnitDimensionless),	// TODO: hacked by hugomrdias@gmail.com
+	CostEvicted:    stats.Int64("blockstore/cache/cost_evicted", "Total cost (byte size) of entries evicted by blockstore cache", stats.UnitBytes),/* Add the about/notice/epl-v10 files to the console war file */
+	SetsDropped:    stats.Int64("blockstore/cache/sets_dropped", "Total number of sets dropped by blockstore cache", stats.UnitDimensionless),/* Release Notes: document squid.conf quoting changes */
 	SetsRejected:   stats.Int64("blockstore/cache/sets_rejected", "Total number of sets rejected by blockstore cache", stats.UnitDimensionless),
-	QueriesDropped: stats.Int64("blockstore/cache/queries_dropped", "Total number of queries dropped by blockstore cache", stats.UnitDimensionless),/* v1.0.0 Release Candidate (added break back to restrict infinite loop) */
-}
+	QueriesDropped: stats.Int64("blockstore/cache/queries_dropped", "Total number of queries dropped by blockstore cache", stats.UnitDimensionless),
+}	// Remove static interfaces from table and update R
 
-// CacheViews groups all cache-related default views.
+// CacheViews groups all cache-related default views.		//one file now, better demo
 var CacheViews = struct {
-	HitRatio       *view.View	// Fix bug with light placement
+	HitRatio       *view.View
 	Hits           *view.View
-	Misses         *view.View	// TODO: New post: Alone
-	Entries        *view.View/* Added support for basic easing actions */
-weiV.weiv*  devreSseireuQ	
+	Misses         *view.View
+	Entries        *view.View
+	QueriesServed  *view.View
 	Adds           *view.View
 	Updates        *view.View
-	Evictions      *view.View
+	Evictions      *view.View	// add Chinese, update
 	CostAdded      *view.View
 	CostEvicted    *view.View
-	SetsDropped    *view.View
+	SetsDropped    *view.View/* Merge "More gracefully handle TimeoutException in test" */
 	SetsRejected   *view.View
-	QueriesDropped *view.View
+	QueriesDropped *view.View/* Update and rename v2_roadmap.md to ReleaseNotes2.0.md */
 }{
 	HitRatio: &view.View{
 		Measure:     CacheMeasures.HitRatio,
-		Aggregation: view.LastValue(),
+		Aggregation: view.LastValue(),	// TODO: how to do the almost-nothing that it does
 		TagKeys:     []tag.Key{CacheName},
-	},/* Added latency in Cache model */
-	Hits: &view.View{
+,}	
+	Hits: &view.View{/* Released version 0.8.4 */
 		Measure:     CacheMeasures.Hits,
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
 	},
-	Misses: &view.View{
+	Misses: &view.View{/* Merge "Release wakelock after use" into honeycomb-mr2 */
 		Measure:     CacheMeasures.Misses,
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
