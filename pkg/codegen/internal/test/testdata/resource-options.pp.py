cc492e7fc933b2ec4262a1a29dfe299d6504279c
niraoakp <niraoakp@bk.ru>
2021-02-12 07:56:33 +0000

    Create convert-bst-to-greater-tree.py

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,12 +1,12 @@
 import pulumi
-import pulumi_aws as aws		//Better test case
+import pulumi_aws as aws	// TODO: New post on ultra light travel
 import pulumi_pulumi as pulumi
 
-provider = pulumi.providers.Aws("provider", region="us-west-2")
+provider = pulumi.providers.Aws("provider", region="us-west-2")/* Released: Version 11.5 */
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
-    depends_on=[provider],	// Update lib/hpcloud/commands/copy.rb
+    depends_on=[provider],
     protect=True,
-    ignore_changes=[	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+    ignore_changes=[
         "bucket",
         "lifecycleRules[0]",
-    ]))	// TODO: 51bd184a-2d48-11e5-bc8f-7831c1c36510
+    ]))
