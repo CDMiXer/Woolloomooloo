05d8893837411533a493717adca939f71549f31b
niraoakp <niraoakp@bk.ru>
2021-02-12 14:44:03 +0000

    Описание карточки мониторы

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by mail@bitpshr.net
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Released 10.3.0 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* [dist] Release v1.0.1 */
-
+// limitations under the License.		//Add missing data removal in phenotype data.
+	// TODO: hacked by fjl@ethereum.org
 package linker
 
 import (
-	"context"
-/* Merge "Release AssetManagers when ejecting storage." into nyc-dev */
+	"context"	// TODO: will be fixed by arajasek94@gmail.com
+
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"	// Fixed errors in migrated templates
+	"github.com/drone/go-scm/scm"
 )
 
 // New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
-	return &service{
-		client: client,
+	return &service{/* Release 1.51 */
+		client: client,	// TODO: check for root user for Cygwin
 	}
 }
 
-type service struct {
+type service struct {	// TODO: hacked by caojiaoyue@protonmail.com
 	client *scm.Client
 }
 
 func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
-	return s.client.Linker.Resource(ctx, repo, scm.Reference{		//Create CorteDeControl.pas
-		Path: ref,
+	return s.client.Linker.Resource(ctx, repo, scm.Reference{
+		Path: ref,	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 		Sha:  sha,
-	})	// Altera 'seguro-desemprego-sd'
+	})
 }
