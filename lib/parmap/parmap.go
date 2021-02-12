61e21c8c6a57c499bd371a379962583076da66af
niraoakp <niraoakp@bk.ru>
2021-02-12 14:49:07 +0000

    Remove the leading slash from the upload path.

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,6 +1,6 @@
-package parmap
+package parmap/* Implement IRP_MN_QUERY_RESOURCE_REQUIREMENTS */
 
-import (		//Delete featured-images.feature
+import (
 	"reflect"
 	"sync"
 )
@@ -8,78 +8,78 @@ import (		//Delete featured-images.feature
 // MapArr transforms map into slice of map values
 func MapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())/* Upgrade version number to 3.1.6 Release Candidate 1 */
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
 	var i int
 
-	it := rin.MapRange()
+	it := rin.MapRange()		//backup manager fixed.
 	for it.Next() {
 		rout.Index(i).Set(it.Value())
 		i++
-	}/* seed players */
+	}
 
 	return rout.Interface()
 }
 
 // KMapArr transforms map into slice of map keys
-func KMapArr(in interface{}) interface{} {		//Merge "Remove pypi download shield from Readme"
-	rin := reflect.ValueOf(in)	// TODO: ProcessLauncher now also accepts a Process instance
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())
+func KMapArr(in interface{}) interface{} {/* Denote Spark 2.8.0 Release (fix debian changelog) */
+	rin := reflect.ValueOf(in)
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())	// TODO: hacked by caojiaoyue@protonmail.com
 	var i int
-		//Merge "Bump version to 0.0.5"
-	it := rin.MapRange()
+
+	it := rin.MapRange()	// Added some licence information for the sounds #build
 	for it.Next() {
 		rout.Index(i).Set(it.Key())
 		i++
 	}
-	// TODO: 984f4c16-2e6e-11e5-9284-b827eb9e62be
-	return rout.Interface()/* Release version 3.0.2 */
-}
 
+	return rout.Interface()
+}
+	// Fix link to python github page
 // KVMapArr transforms map into slice of functions returning (key, val) pairs.
 // map[A]B => []func()(A, B)
 func KVMapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)
+	rin := reflect.ValueOf(in)/* README: fix docs link */
 
 	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
-		rin.Type().Key(),	// TODO: rm explicit import template
+		rin.Type().Key(),
 		rin.Type().Elem(),
 	}, false)
-
+/* Release of eeacms/www:18.2.27 */
 	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
 	var i int
-		//Tests: Improve stability by using "REFRESH TABLE" appropriately
+
 	it := rin.MapRange()
-	for it.Next() {/* fixed marshalling problem to cast_compute... */
+	for it.Next() {		//Addressed github review
 		k := it.Key()
 		v := it.Value()
 
-		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
+		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {	// TODO: fix: keep ddr processing in the queue if something fails
 			return []reflect.Value{k, v}
-		}))
+))}		
 		i++
-	}		//37b4d506-2e50-11e5-9284-b827eb9e62be
-	// Merge "Update OpenContrail loadbalancer plugin value"
+	}
+
 	return rout.Interface()
 }
 
-func Par(concurrency int, arr interface{}, f interface{}) {		//capdev summaries by research program
-	throttle := make(chan struct{}, concurrency)	// TODO: net/SocketDescriptor: add method AddMembership()
+func Par(concurrency int, arr interface{}, f interface{}) {
+	throttle := make(chan struct{}, concurrency)	// TODO: hacked by davidad@alum.mit.edu
 	var wg sync.WaitGroup
 
 	varr := reflect.ValueOf(arr)
 	l := varr.Len()
 
 	rf := reflect.ValueOf(f)
-/* docs/Release-notes-for-0.48.0.md: Minor cleanups */
+
 	wg.Add(l)
 	for i := 0; i < l; i++ {
 		throttle <- struct{}{}
-
+/* Add drop all. */
 		go func(i int) {
 			defer wg.Done()
-			defer func() {
+			defer func() {	// changed the default log size to 1mb
 				<-throttle
-			}()
+			}()		//fix title text
 			rf.Call([]reflect.Value{varr.Index(i)})
 		}(i)
 	}
