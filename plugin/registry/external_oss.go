e52df98919253351959aa2c7690c05ec7caff29f
niraoakp <niraoakp@bk.ru>
2021-02-12 05:06:49 +0000

    Fix incorrect test template link and typo

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc./* Update page-content.php */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge branch 'master' into maximized */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Added NMI sound ACK mechanism to tecmo.c driver [Angelo Salese]
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Release notes for 1.0.94 */
+
 package registry
 
 import "github.com/drone/drone/core"
 
 // External returns a no-op registry credential provider.
 func External(string, string, bool) core.RegistryService {
-	return new(noop)/* Released version 0.8.2c */
-}/* Rename Adafruit_SSD1306.h to Adafruit_SSD1306_32pixel.h */
+	return new(noop)
+}
