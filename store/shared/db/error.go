230b84d767de970f58cd9d09d75c92b52d5681db
niraoakp <niraoakp@bk.ru>
2021-02-12 21:03:58 +0000

    Add /api/is/<status> route - List servers according to status

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -4,10 +4,10 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: fix #2542: NPE when deleting list with all caches in it
-///* Changing alias */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 2.12.1 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,8 +15,8 @@
 package db
 
 import "errors"
-		//Update commands and premissions description
-// ErrOptimisticLock is returned by if the struct being		//Merge #257 `Fix the eventsource server for CORS`
+
+// ErrOptimisticLock is returned by if the struct being
 // modified has a Version field and the value is not equal
-// to the current value in the database/* classical conditioning examples */
+// to the current value in the database
 var ErrOptimisticLock = errors.New("Optimistic Lock Error")
