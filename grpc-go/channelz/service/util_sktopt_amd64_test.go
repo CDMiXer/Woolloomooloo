a41cefff6cbf9dcd168dea18451680eabec253ad
niraoakp <niraoakp@bk.ru>
2021-02-12 14:56:45 +0000

    Rebuilt index with sahilpurav

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -1,6 +1,6 @@
 // +build amd64,linux
 
-/*		//rating service, across dbs
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -11,19 +11,19 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* [#70] update web form technical design */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Task #4450: minor misc task cleanup
+ *
  */
-/* Release for 24.6.0 */
+
 package service
 
 import (
 	"golang.org/x/sys/unix"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-)/* Update InsertQueryBuilder.ts */
+)
 
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
