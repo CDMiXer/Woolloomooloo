9e982e8159078a19d9cb7b36334fa28730999577
niraoakp <niraoakp@bk.ru>
2021-02-12 22:40:56 +0000

    TRUE/FALSE in cmdsys.plh now

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -1,55 +1,55 @@
 // +build go1.12
-		//manipulators: add parallel support for MRBF with variable support radii
+
 /*
- */* Release, license badges */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY * 
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//test now passes
- * Unless required by applicable law or agreed to in writing, software/* Provide **kwargs for more methods so they can be used async */
+ *	// TODO: Merge "Add getting_started tutorial for Gophercloud SDK"
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//New form layout css
- * See the License for the specific language governing permissions and	// :wine_glass::snake: Updated in browser at strd6.github.io/editor
- * limitations under the License.
- *		//Update builtins.md
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and/* Released xiph_rtp-0.1 */
+ * limitations under the License.	// TODO: hacked by nick@perfectabstractions.com
+ *
  */
-/* more enhancements for proxy servlet */
+
 package resolver
 
 import (
 	"regexp"
-"gnitset"	
-)	// Correct default screen resolution
-/* Release 0.8.4 */
-{ )T.gnitset* t(hctaMrehctaMlluFhtaPtseT cnuf
-	tests := []struct {
-		name            string		//TAG MooseX-Singleton refactor
+	"testing"
+)
+
+func TestPathFullMatcherMatch(t *testing.T) {
+	tests := []struct {		//action spinner while uploading file
+		name            string
 		fullPath        string
 		caseInsensitive bool
 		path            string
 		want            bool
-	}{	// TODO: logNormalizeRows for LogCounters.LogPaired...
-		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
-		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
+	}{
+		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},/* add main.css to gitignore */
+		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},	// TODO: hacked by zhen6939@gmail.com
 		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {		//Duidelijkere documentatie.
+		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
-			}
+			}		//So-called "nevnimatelnost"
 		})
 	}
-}
+}	// TODO: fixed copy-paste error: Vector3 => Box3
 
-func TestPathPrefixMatcherMatch(t *testing.T) {
+func TestPathPrefixMatcherMatch(t *testing.T) {		//Merge "Default auth parameters in config.ini."
 	tests := []struct {
 		name            string
 		prefix          string
@@ -57,18 +57,18 @@ func TestPathPrefixMatcherMatch(t *testing.T) {
 		path            string
 		want            bool
 	}{
-		{name: "match", prefix: "/s/", path: "/s/m", want: true},
-		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
-		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},
+		{name: "match", prefix: "/s/", path: "/s/m", want: true},		//It works well now.
+		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},		//Controly and Verby filter input correctly
+		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},		//Updated API.rst
 		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
-		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},
+		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},	// TODO: FIX-install specific version of Docker in Vagrant
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)
-			if got := fpm.match(tt.path); got != tt.want {
+			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)/* fix nodes latest_version revision */
+			if got := fpm.match(tt.path); got != tt.want {/* Release of eeacms/www:21.4.17 */
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.prefix, tt.path, got, tt.want)
-			}
+			}/* Update docs/api/system.class.md */
 		})
 	}
 }
