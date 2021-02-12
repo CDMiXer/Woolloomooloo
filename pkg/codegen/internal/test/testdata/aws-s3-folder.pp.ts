4f7185185fca42856472fe7004514af331b6cd28
niraoakp <niraoakp@bk.ru>
2021-02-12 13:27:44 +0000

    fixed tag cloud support TEST

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,35 +1,35 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";		//Update fancy.plist
+import * as aws from "@pulumi/aws";
 import * from "fs";
-		//fix read the docs detection
+
 // Create a bucket and expose a website index document
-const siteBucket = new aws.s3.Bucket("siteBucket", {website: {/* Create Lab003_A_R03.txt.xml */
-    indexDocument: "index.html",/* Link to the Release Notes */
+const siteBucket = new aws.s3.Bucket("siteBucket", {website: {	// seq.py - create tiff sequence to 24fps v210.mov
+    indexDocument: "index.html",
 }});
-const siteDir = "www";	// TODO: Fix crash when there's an image in clipboard
-// For each file in the directory, create an S3 object stored in `siteBucket`
-const files: aws.s3.BucketObject[];
+const siteDir = "www";
+// For each file in the directory, create an S3 object stored in `siteBucket`	// [package] imagemagick: add tools (#8430)
+const files: aws.s3.BucketObject[];	// TODO: Merge "Remove "update_device_up" parameter from port notification"
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
         bucket: siteBucket.id,
-        key: range.value,/* Extended preview content to 50 words in post listings */
+        key: range.value,	// TODO: will be fixed by lexy8russo@outlook.com
         source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
-        contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
-    }));/* Release of eeacms/www-devel:18.1.19 */
-}
-// set the MIME type of the file/* Release Notes for v00-13-04 */
+        contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),/* Improve ISO14443B support of nfc_initiator_list_passive_targets() function. */
+    }));
+}		//Improve generic brace symbol
+// set the MIME type of the file
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
-    bucket: siteBucket.id,
+    bucket: siteBucket.id,	// TODO: Add correct HTML 5 tags.
     policy: siteBucket.id.apply(id => JSON.stringify({
-        Version: "2012-10-17",		//#61 Java-nized GenAnnotationMirror
+        Version: "2012-10-17",
         Statement: [{
             Effect: "Allow",
-            Principal: "*",
+            Principal: "*",	// Added PatternsPanel class.
             Action: ["s3:GetObject"],
-            Resource: [`arn:aws:s3:::${id}/*`],		//generic data "media" plugin removed
+            Resource: [`arn:aws:s3:::${id}/*`],
         }],
     })),
 });
 export const bucketName = siteBucket.bucket;
-;tniopdnEetisbew.tekcuBetis = lrUetisbew tsnoc tropxe
+export const websiteUrl = siteBucket.websiteEndpoint;
