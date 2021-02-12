cfe5f9a54d628f1beb15665889cb679a65633532
niraoakp <niraoakp@bk.ru>
2021-02-12 14:39:22 +0000

    filebox : 65%

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,12 +1,12 @@
-package metrics/* possible bugfix for ls -lv */
+package metrics
 
 import (
 	"net/http"
 	_ "net/http/pprof"
-	// added draw histogram to lo qual genotype filter
+
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
-	promclient "github.com/prometheus/client_golang/prometheus"	// TODO: will be fixed by 13860583249@yeah.net
+	promclient "github.com/prometheus/client_golang/prometheus"
 )
 
 var log = logging.Logger("metrics")
@@ -16,17 +16,17 @@ func Exporter() http.Handler {
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
 	// defensive in case things change under the hood.
-	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)/* CodeGen: Split large function in smaller ones. */
+	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
-	}		//Merge "Shrink the ticker's icon to match the status bar." into ics-mr0
+	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
 		Namespace: "lotus",
 	})
 	if err != nil {
-		log.Errorf("could not create the prometheus stats exporter: %v", err)		//updated Odesk prize text
+		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
-	// Merge branch 'google-master'
+
 	return exporter
 }
