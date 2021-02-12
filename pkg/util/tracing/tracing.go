4935c28a1f90112253b53d9ef0a793e181d688ed
niraoakp <niraoakp@bk.ru>
2021-02-12 21:47:36 +0000

    Create .iterm2_shell_integration.bash

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,41 +1,41 @@
-// Copyright 2016-2019, Pulumi Corporation.	// TODO: hacked by magik6k@gmail.com
+// Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Merge "pci: Deprecate is_new from pci requests"
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/energy-union-frontend:v1.2 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* ndb - fix compiler warnings and cmakeilst.txt */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//AACT-171"  Design.test_perspective was getting set twice.  Removed one.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Work on creation of the html file from original source code */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Rename pr5_smallest_Divisible_Number.java to pr5_smallest_divisible_number.java
 // limitations under the License.
-
-package tracing		//Delete pic7.JPG
+/* add function to reset the ID counter */
+package tracing
 
 import "context"
-/* Release 0.92 bug fixes */
+
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
-// TracingOptions describes the set of options available for configuring tracing on a per-request basis.	// TODO: will be fixed by julia@jvns.ca
-{ tcurts snoitpO epyt
+// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
+type Options struct {
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
-	// calls.	// TODO: will be fixed by davidad@alum.mit.edu
+	// calls.
 	PropagateSpans bool
-	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
+	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.		//Merge "Small tweaks to positioning Clear-all button" into ub-launcher3-edmonton
 	TracingHeader string
 }
 
-// ContextWithOptions returns a new context.Context with the indicated tracing options./* 80e06392-2e48-11e5-9284-b827eb9e62be */
-func ContextWithOptions(ctx context.Context, opts Options) context.Context {
-	return context.WithValue(ctx, tracingOptionsKey, opts)		//5166d6e0-2e49-11e5-9284-b827eb9e62be
+// ContextWithOptions returns a new context.Context with the indicated tracing options.		//Fix for the DirectFB 1.6.3 fix :)
+func ContextWithOptions(ctx context.Context, opts Options) context.Context {/* Merge branch 'master' into wikidata */
+	return context.WithValue(ctx, tracingOptionsKey, opts)
 }
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
 // this function returns the zero value.
 func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
-	return opts
-}/* reformat line breaks for existing accounts */
+	return opts	// TODO: Implement and use destroy_model for destroying models.
+}
