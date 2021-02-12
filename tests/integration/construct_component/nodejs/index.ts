e390c2cda81cfe1c1bd5bde482fd6fb48f5364ca
niraoakp <niraoakp@bk.ru>
2021-02-12 05:38:20 +0000

    [Fix] base/res: Fix typo

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+		//License code update
 import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
-
+		//refactoring: simpler local configuration
