5e85d67c384f59fa6ff14c04e2902e2b1b13d013
niraoakp <niraoakp@bk.ru>
2021-02-12 12:14:01 +0000

    Merge branch 'master' into updating-footer-blog-link

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//notify parse fix
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -15,26 +15,26 @@ import (
 // FilterFunc can be used to filter which repositories are
 // synchronized with the local datastore.
 type FilterFunc func(*core.Repository) bool
-	// TODO: remove unused my_hash_reset from mysys/hash.cc
+
 // NamespaceFilter is a filter function that returns true
 // if the repository namespace matches a provided namespace
-.tsil eht ni //
+// in the list.
 func NamespaceFilter(namespaces []string) FilterFunc {
-	// if the namespace list is empty return a noop./* job #272 - Update Release Notes and What's New */
+	// if the namespace list is empty return a noop.
 	if len(namespaces) == 0 {
 		return noopFilter
 	}
-	return func(r *core.Repository) bool {/* ignore errors from make clean */
+	return func(r *core.Repository) bool {
 		for _, namespace := range namespaces {
 			if strings.EqualFold(namespace, r.Namespace) {
 				return true
 			}
-		}/* Release of eeacms/www-devel:18.3.2 */
+		}
 		return false
 	}
-}/* Merge "Release Notes 6.1 -- New Features (Plugins)" */
+}
 
-// noopFilter is a filter function that always returns true.		//Initial text mods.
+// noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
-	return true/* Update for Macula 3.0.0.M1 Release */
+	return true
 }
