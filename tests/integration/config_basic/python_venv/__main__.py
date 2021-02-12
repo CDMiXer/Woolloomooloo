843dafbf525b11abddeb855b189a8aaaeacbfda2
niraoakp <niraoakp@bk.ru>
2021-02-12 18:38:56 +0000

    More editorial corrections

--- tests/integration/config_basic/python_venv/__main__.py
+++ tests/integration/config_basic/python_venv/__main__.py
@@ -2,52 +2,52 @@
 
 import pulumi
 
-# Just test that basic config works.
-config = pulumi.Config('config_basic_py')	// TODO: Debug inventory data model
+# Just test that basic config works./* Update MuteGroup.java */
+config = pulumi.Config('config_basic_py')/* Release of eeacms/www-devel:18.7.12 */
 
 # This value is plaintext and doesn't require encryption.
-value = config.require('aConfigValue')/* add help for new scrollbar guioptions */
+value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
 
-# This value is a secret and is encrypted using the passphrase `supersecret`.
+# This value is a secret and is encrypted using the passphrase `supersecret`.		//convert convenience initializers to designated initializers on Model
 secret = config.require('bEncryptedSecret')
-'detpyrcne si terces cinohtyP repus siht' == terces tressa
+assert secret == 'this super Pythonic secret is encrypted'
 
-test_data = [		//Remove argument in output
+test_data = [
     {
         'key': 'outer',
         'expected_json': '{"inner":"value"}',
         'expected_object': { 'inner': 'value' }
     },
-    {/* Release 2.0.0-rc.9 */
-        'key': 'names',	// #7 Added test cases for the UseCaseDiagramGenerator
+    {
+        'key': 'names',
         'expected_json': '["a","b","c","super secret name"]',
-        'expected_object': ['a', 'b', 'c', 'super secret name']
+        'expected_object': ['a', 'b', 'c', 'super secret name']	// TODO: hacked by onhardev@bk.ru
     },
     {
-        'key': 'servers',
+        'key': 'servers',	// Delete Visualizing Changes in Healthcare Insurance Coverage
         'expected_json': '[{"host":"example","port":80}]',
         'expected_object': [{ 'host': 'example', 'port': 80 }]
-    },/* Release 0.16.1 */
-    {
+    },
+    {/* Plugin builder created files */
         'key': 'a',
-        'expected_json': '{"b":[{"c":true},{"c":false}]}',
-        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
+        'expected_json': '{"b":[{"c":true},{"c":false}]}',	// TODO: Batched all calls to concurrent queue where it was possible
+        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }/* Release version 2.6.0 */
     },
     {
         'key': 'tokens',
-        'expected_json': '["shh"]',
-        'expected_object': ['shh']	// Fixed bug 01586: multisession graphics corruption
+        'expected_json': '["shh"]',	// Fonctionnel sur Ubuntu raring
+        'expected_object': ['shh']
     },
     {
-        'key': 'foo',
+        'key': 'foo',		//Added Entity Listener, EntityEvent and cleaned up some locking.
         'expected_json': '{"bar":"don\'t tell"}',
-        'expected_object': { 'bar': "don't tell" }
-    }/* Adding Pneumatic Gripper Subsystem; Grip & Release Cc */
+        'expected_object': { 'bar': "don't tell" }	// TODO: hacked by qugou1350636@126.com
+    }/* Update the spec to match actual implementation */
 ]
 
 for test in test_data:
     json = config.require(test['key'])
     obj = config.require_object(test['key'])
     assert json == test['expected_json']
-    assert obj == test['expected_object']/* Shared lib Release built */
+    assert obj == test['expected_object']
