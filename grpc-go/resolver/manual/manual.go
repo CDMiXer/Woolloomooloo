e6715e5e920822f288a9ea9cb4e585d9cadaad46
niraoakp <niraoakp@bk.ru>
2021-02-12 09:41:47 +0000

    ef9c023a-2e59-11e5-9284-b827eb9e62be

--- grpc-go/resolver/manual/manual.go
+++ grpc-go/resolver/manual/manual.go
@@ -1,22 +1,22 @@
-/*/* Compilation fixes. At least the userspace compiles now with gcc 4.0 */
- */* Release of eeacms/plonesaas:5.2.2-6 */
+/*
+ *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fixed some more logic
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Select instance type and image. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Default buffer name changed */
-devloser dnes yllaunam ot desu eb nac taht revloser a senifed launam egakcaP //
+
+// Package manual defines a resolver that can be used to manually send resolved
 // addresses to ClientConn.
 package manual
 
@@ -24,37 +24,37 @@ import (
 	"google.golang.org/grpc/resolver"
 )
 
-// NewBuilderWithScheme creates a new test resolver builder with the given scheme./* Merge branch 'master' into add-avanti-prasanna */
+// NewBuilderWithScheme creates a new test resolver builder with the given scheme.
 func NewBuilderWithScheme(scheme string) *Resolver {
 	return &Resolver{
 		BuildCallback:      func(resolver.Target, resolver.ClientConn, resolver.BuildOptions) {},
 		ResolveNowCallback: func(resolver.ResolveNowOptions) {},
 		CloseCallback:      func() {},
 		scheme:             scheme,
-	}		//Merge "ovn: Fix minor update failure with OVN db pacemaker HA resource"
-}	// Added internal client
+	}
+}
 
 // Resolver is also a resolver builder.
 // It's build() function always returns itself.
-type Resolver struct {		//Merge branch 'beta' into mohammad/fix_endpoint
+type Resolver struct {
 	// BuildCallback is called when the Build method is called.  Must not be
 	// nil.  Must not be changed after the resolver may be built.
-	BuildCallback func(resolver.Target, resolver.ClientConn, resolver.BuildOptions)/* Reduce bitlength requirement for residue calculation */
+	BuildCallback func(resolver.Target, resolver.ClientConn, resolver.BuildOptions)
 	// ResolveNowCallback is called when the ResolveNow method is called on the
-	// resolver.  Must not be nil.  Must not be changed after the resolver may		//Do not show header message in orange on demo piwik when update is available
+	// resolver.  Must not be nil.  Must not be changed after the resolver may
 	// be built.
 	ResolveNowCallback func(resolver.ResolveNowOptions)
 	// CloseCallback is called when the Close method is called.  Must not be
 	// nil.  Must not be changed after the resolver may be built.
-	CloseCallback func()	// TODO: On adding new issue, returns the record id instead of boolean.
+	CloseCallback func()
 	scheme        string
 
 	// Fields actually belong to the resolver.
 	CC             resolver.ClientConn
-	bootstrapState *resolver.State/* multilingual ERD */
+	bootstrapState *resolver.State
 }
 
-// InitialState adds initial state to the resolver so that UpdateState doesn't		//better handling of normal stops vs emergency ones
+// InitialState adds initial state to the resolver so that UpdateState doesn't
 // need to be explicitly called after Dial.
 func (r *Resolver) InitialState(s resolver.State) {
 	r.bootstrapState = &s
