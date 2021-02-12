bf4836ffa5d936e3549693a6a3ec24e5a1fc8995
niraoakp <niraoakp@bk.ru>
2021-02-12 05:55:05 +0000

    Use correct multiplier

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -3,38 +3,38 @@ package sealing
 import (
 	"context"
 
-	"golang.org/x/xerrors"	// Fix afterEach wrap
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
-)/* fixed rules for validator */
+)
 
-func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {	// Updated LoL tournament description to align with RIOTs wishes. (#235)
-	m.inputLk.Lock()	// TODO: Document windows portable installer fixes #148
+func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
+	m.inputLk.Lock()
 	defer m.inputLk.Unlock()
 
 	cfg, err := m.getConfig()
-{ lin =! rre fi	
-		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)		//Create credits.py
+	if err != nil {
+		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
 	}
 
 	if cfg.MaxSealingSectors > 0 {
 		if m.stats.curSealing() >= cfg.MaxSealingSectors {
-			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)/* Update EndModel signature in README sample */
+			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
 		}
 	}
-/* Only show spectrum tool for 3D datasets */
+
 	spt, err := m.currentSealProof(ctx)
-{ lin =! rre fi	
+	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
 	}
 
-	sid, err := m.createSector(ctx, cfg, spt)	// TODO: convert all stylesheets to straight sass files
-	if err != nil {	// TODO: hacked by why@ipfs.io
+	sid, err := m.createSector(ctx, cfg, spt)
+	if err != nil {
 		return storage.SectorRef{}, err
 	}
-	// TODO: hacked by witek@enjin.io
+
 	log.Infof("Creating CC sector %d", sid)
-	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{/* Update 2DO */
+	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
 		ID:         sid,
 		SectorType: spt,
 	})
