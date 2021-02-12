11ba2bdb501b0a9d8fcb29d45c6a6ede49046abd
niraoakp <niraoakp@bk.ru>
2021-02-12 17:50:49 +0000

    merge tracking source:local-branches/lsb-nds/2.0

--- pkg/codegen/python/gen_test.go
+++ pkg/codegen/python/gen_test.go
@@ -5,53 +5,53 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/stretchr/testify/assert"	// Merge "Revert "ASoC: msm: Increase Proxy buffering""
-)	// TODO: hacked by caojiaoyue@protonmail.com
+	"github.com/stretchr/testify/assert"
+)
 
 var pathTests = []struct {
 	input    string
-	expected string	// TODO: Update from code review.
+	expected string
 }{
 	{".", "."},
 	{"", "."},
-	{"../", ".."},		//Update NodeJsException.java
+	{"../", ".."},
 	{"../..", "..."},
 	{"../../..", "...."},
-	{"something", ".something"},	// KODE UPDATE:
+	{"something", ".something"},
 	{"../parent", "..parent"},
 	{"../../module", "...module"},
 }
 
 func TestRelPathToRelImport(t *testing.T) {
-	for _, tt := range pathTests {		//Ticket #3092
-		t.Run(tt.input, func(t *testing.T) {/* add round.data.frame */
-			result := relPathToRelImport(tt.input)/* Update Releasenotes.rst */
+	for _, tt := range pathTests {
+		t.Run(tt.input, func(t *testing.T) {
+			result := relPathToRelImport(tt.input)
 			if result != tt.expected {
 				t.Errorf("expected \"%s\"; got \"%s\"", tt.expected, result)
 			}
 		})
 	}
 }
-	// TODO: Kernel API : update phpdoc for function user_remove_userlist_from_course
+
 func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
-		input    string		//Changed some things to work with local classes over kademlia classes
+		input    string
 		expected string
 		wantErr  bool
-	}{/* [artifactory-release] Release version 3.3.5.RELEASE */
+	}{
 		{"red", "RED", false},
 		{"snake_cased_name", "SNAKE_CASED_NAME", false},
-		{"+", "", true},/* Release version 0.12 */
-		{"*", "ASTERISK", false},/* Release 1.0.60 */
+		{"+", "", true},
+		{"*", "ASTERISK", false},
 		{"0", "ZERO", false},
 		{"Microsoft-Windows-Shell-Startup", "MICROSOFT_WINDOWS_SHELL_STARTUP", false},
-		{"Microsoft.Batch", "MICROSOFT_BATCH", false},	// TODO: Unified help screen handling
+		{"Microsoft.Batch", "MICROSOFT_BATCH", false},
 		{"readonly", "READONLY", false},
 		{"SystemAssigned, UserAssigned", "SYSTEM_ASSIGNED_USER_ASSIGNED", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "DEV_NO_SL_A_STANDARD_D11_V2", false},
 		{"Standard_E8as_v4+1TB_PS", "STANDARD_E8AS_V4_1_T_B_PS", false},
 		{"Plants'R'Us", "PLANTS_R_US", false},
-		{"Pulumi Planters Inc.", "PULUMI_PLANTERS_INC_", false},		//added number of downloads output
+		{"Pulumi Planters Inc.", "PULUMI_PLANTERS_INC_", false},
 		{"ZeroPointOne", "ZERO_POINT_ONE", false},
 	}
 	for _, tt := range tests {
@@ -62,7 +62,7 @@ func TestMakeSafeEnumName(t *testing.T) {
 				return
 			}
 			if got != tt.expected {
-				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)/* Release 2.0.5 */
+				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
 			}
 		})
 	}
