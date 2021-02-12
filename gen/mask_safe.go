bbe0b308f08ccc064bbc0ec56d4e4063af398c76
niraoakp <niraoakp@bk.ru>
2021-02-12 00:01:38 +0000

    Delete api.ooc

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of		//Added missing this for the rest of system.
-// this source code is governed by a BSD-style license that can be found in the/* Release of eeacms/plonesaas:5.2.4-1 */
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
+// this source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
 // +build appengine
 
 package websocket
 
-func maskBytes(key [4]byte, pos int, b []byte) int {	// TODO: Fix unknown bugs in nib file (ignored autoresize struts), introduced by IB bugs.
-	for i := range b {	// TODO: will be fixed by boringland@protonmail.ch
+func maskBytes(key [4]byte, pos int, b []byte) int {
+	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
 	}
 	return pos & 3
-}	// fix #3893 - thanks matwey
+}
