eb4b9575f5b74c7ddd7234513ad6daf1cce0b1cb
niraoakp <niraoakp@bk.ru>
2021-02-12 16:34:32 +0000

    Merge "Fix a merge problem in VMAX driver"

--- grpc-go/xds/internal/balancer/clusterimpl/logging.go
+++ grpc-go/xds/internal/balancer/clusterimpl/logging.go
@@ -7,11 +7,11 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* added approximation of real distributions idea */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Runner to generate images of the Acton data.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -21,14 +21,14 @@ package clusterimpl
 import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"/* Release 2.1.10 - fix JSON param filter */
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"/* ndb is under storage/ now */
+	"google.golang.org/grpc/grpclog"
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[xds-cluster-impl-lb %p] "
 
-var logger = grpclog.Component("xds")		//forgot the encrypt function
-/* Convert quick_reply.tpl's line endings to unix; fix the check boxes */
+var logger = grpclog.Component("xds")
+
 func prefixLogger(p *clusterImplBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
