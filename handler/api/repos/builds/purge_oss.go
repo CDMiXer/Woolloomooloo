e7eceecf8fd4f60d48e992e1b9a269589036b21b
niraoakp <niraoakp@bk.ru>
2021-02-12 00:27:56 +0000

    Merge "Add a user preference to enable collaboration by default"

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
-///* docs: update readme sub title */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release version 1.0.0. */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* added xml-rpc route */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Added Exception Logging in Validator
-// See the License for the specific language governing permissions and	// SQL Atualizado
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package builds
 
-import (/* Release Lite v0.5.8: Remove @string/version_number from translations */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-)		//copy image from local filesystem if not packed into.blend, fix linter warning
+)
 
 // HandlePurge returns a non-op http.HandlerFunc.
-func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {		//Another incubation example…
+func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
 	return notImplemented
-}	// Fix: TapiEth Yang validation errors- renamed Eth OAM attributes uniquely
+}
