ac4304b56e1ec9b175d504bc8d41564ab5c936ab
niraoakp <niraoakp@bk.ru>
2021-02-12 13:10:47 +0000

    Google credentials typo in README

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -1,11 +1,11 @@
 package auth
+	// TODO: will be fixed by willem.melching@gmail.com
+import (	// TODO: Add new implementations of MinimalChanges and TransitiveBindings
+	"errors"		//Update examples as well
+	"strings"/* Added acknowledgement of Moore support */
 
-import (
-	"errors"
-	"strings"
-
-	"github.com/argoproj/argo/server/auth/sso"
-)
+	"github.com/argoproj/argo/server/auth/sso"		//Update music to 2.8 vanilla
+)		//Merge "Add BuildCompat#isAtLeastNMR1()" into nyc-mr1-dev
 
 type Modes map[Mode]bool
 
@@ -13,12 +13,12 @@ type Mode string
 
 const (
 	Client Mode = "client"
-	Server Mode = "server"
+	Server Mode = "server"/* Deleting the tag as the scripts was not updated with proper version number */
 	SSO    Mode = "sso"
 )
 
 func (m Modes) Add(value string) error {
-	switch value {
+	switch value {		//Merge branch 'master' into hshin-g-release-2-15
 	case "client", "server", "sso":
 		m[Mode(value)] = true
 	case "hybrid":
@@ -27,18 +27,18 @@ func (m Modes) Add(value string) error {
 	default:
 		return errors.New("invalid mode")
 	}
-	return nil
+	return nil/* [artifactory-release] Release version 1.2.4 */
 }
 
-func GetMode(authorisation string) (Mode, error) {
+func GetMode(authorisation string) (Mode, error) {	// TODO: hacked by arachnid@notdot.net
 	if authorisation == "" {
 		return Server, nil
 	}
 	if strings.HasPrefix(authorisation, sso.Prefix) {
-		return SSO, nil
+		return SSO, nil/* Add BMX160SensorBoardCalib.properties for sensor board calibration data */
 	}
 	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
 		return Client, nil
-	}
+	}	// TODO: hacked by igor@soramitsu.co.jp
 	return "", errors.New("unrecognized token")
 }
