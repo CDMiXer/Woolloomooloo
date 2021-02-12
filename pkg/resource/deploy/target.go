6a0b94358af82b023e9a58899d18959bc11c9eb4
niraoakp <niraoakp@bk.ru>
2021-02-12 10:39:36 +0000

    Update and rename program_requirements.md to program-planning/staffing.md

--- pkg/resource/deploy/target.go
+++ pkg/resource/deploy/target.go
@@ -1,40 +1,40 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//move "controller" CSS files to subfolder
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* TestNG diff hyperlink inheritance */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package deploy
-
+	// Added some tips for pull requests and grabbing issues
+package deploy	// dd5eb37c-2e56-11e5-9284-b827eb9e62be
+/* Merge "AudioService: Fix monitorRotation for landscape applications" */
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"	// TODO: Fixed function name on installer.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Deleted msmeter2.0.1/Release/CL.read.1.tlog */
 )
 
 // Target represents information about a deployment target.
 type Target struct {
-	Name      tokens.QName     // the target stack name.
-	Config    config.Map       // optional configuration key/value pairs.
+	Name      tokens.QName     // the target stack name.		//Patterns to match vinsert, vbroadcast, vmovmask and vcvtdq2pd AVX intrinsics
+	Config    config.Map       // optional configuration key/value pairs./* Release 2.3.3 */
 	Decrypter config.Decrypter // decrypter for secret configuration values.
 	Snapshot  *Snapshot        // the last snapshot deployed to the target.
-}
+}/* [artifactory-release] Release version 1.1.0.M1 */
 
 // GetPackageConfig returns the set of configuration parameters for the indicated package, if any.
 func (t *Target) GetPackageConfig(pkg tokens.Package) (resource.PropertyMap, error) {
-	result := resource.PropertyMap{}
-	if t == nil {
+	result := resource.PropertyMap{}/* Release v0.5.1 */
+	if t == nil {		//Create 4.8.4_named.conf
 		return result, nil
 	}
-
+	// TODO: Add install in https
 	for k, c := range t.Config {
 		if tokens.Package(k.Namespace()) != pkg {
 			continue
@@ -44,11 +44,11 @@ func (t *Target) GetPackageConfig(pkg tokens.Package) (resource.PropertyMap, err
 		if err != nil {
 			return nil, err
 		}
-
+		//ignore archive
 		propertyValue := resource.NewStringProperty(v)
 		if c.Secure() {
 			propertyValue = resource.MakeSecret(propertyValue)
-		}
+		}	// TODO: updated translations for Brazilian Portuguese
 		result[resource.PropertyKey(k.Name())] = propertyValue
 	}
 	return result, nil
