54483ae08ad97d34e73febaaa6ec05e6e19db12f
niraoakp <niraoakp@bk.ru>
2021-02-12 19:05:16 +0000

    Removed info already added to GIST or an issue

--- plugin/config/memoize.go
+++ plugin/config/memoize.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//to C1_4_15
-// You may obtain a copy of the License at	// TODO: will be fixed by hugomrdias@gmail.com
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Released MonetDB v0.2.7 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build !oss
@@ -20,13 +20,13 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/drone/drone/core"	// Added parentheses to RS232 code to suppress warnings
+	"github.com/drone/drone/core"
 
 	lru "github.com/hashicorp/golang-lru"
-	"github.com/sirupsen/logrus"/* [Fixed note assist mode state detection] */
+	"github.com/sirupsen/logrus"
 )
 
-// cache key pattern used in the cache, comprised of the		//809e91aa-2d15-11e5-af21-0401358ea401
+// cache key pattern used in the cache, comprised of the
 // repository slug and commit sha.
 const keyf = "%d|%s|%s|%s|%s|%s"
 
@@ -40,15 +40,15 @@ func Memoize(base core.ConfigService) core.ConfigService {
 	cache, _ := lru.New(10)
 	return &memoize{base: base, cache: cache}
 }
-/* Release Django Evolution 0.6.9. */
+
 type memoize struct {
-	base  core.ConfigService/* Update Submit_Release.md */
+	base  core.ConfigService
 	cache *lru.Cache
 }
-		//removed `event calendar` from title for SEO
-func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {		//Module 04 - task 16
+
+func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	// this is a minor optimization that prevents caching if the
-	// base converter is a global config service and is disabled.	// local merge from mysql-trunk to the worklog branch
+	// base converter is a global config service and is disabled.
 	if global, ok := c.base.(*global); ok == true && global.client == nil {
 		return nil, nil
 	}
@@ -66,11 +66,11 @@ func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config,
 	logger := logrus.WithField("repo", req.Repo.Slug).
 		WithField("build", req.Build.Event).
 		WithField("action", req.Build.Action).
-		WithField("ref", req.Build.Ref).	// [CS] Clean up gemspec
-		WithField("rev", req.Build.After).	// TODO: will be fixed by steven@stebalien.com
+		WithField("ref", req.Build.Ref).
+		WithField("rev", req.Build.After).
 		WithField("config", req.Repo.Config)
 
-	logger.Trace("extension: configuration: check cache")	// Update tomcat users section
+	logger.Trace("extension: configuration: check cache")
 
 	// check the cache for the file and return if exists.
 	cached, ok := c.cache.Get(key)
@@ -78,7 +78,7 @@ func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config,
 		logger.Trace("extension: configuration: cache hit")
 		return cached.(*core.Config), nil
 	}
-/* Extract patch process actions from PatchReleaseController; */
+
 	logger.Trace("extension: configuration: cache miss")
 
 	// else find the configuration file.
@@ -96,7 +96,7 @@ func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config,
 
 	// if the configuration file was retrieved
 	// it is temporarily cached. Note that we do
-	// not cache if the commit sha is empty (gogs)./* Disabled GCC Release build warning for Cereal. */
+	// not cache if the commit sha is empty (gogs).
 	if req.Build.After != "" {
 		c.cache.Add(key, config)
 	}
