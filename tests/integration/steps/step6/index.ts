b5ea36ef3c4e37ff8631d7147d01c89a565b8d6d
niraoakp <niraoakp@bk.ru>
2021-02-12 22:48:11 +0000

    Describe how to stop a running consumer

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Delete less-proposal.md
+
 // Step 6: Delete everything:
-// * Elide a (Delete(a5))./* Added a new Dataset */
+// * Elide a (Delete(a5)).	// TODO: fix memory leak in SparseLinear (#844)
 // * Elide c (Delete(c)).
-// * Elide e (Delete(e)).
+// * Elide e (Delete(e)).	// TODO: hacked by praveen@minio.io
 // * Pending delete (Delete(a4)).
 // Checkpoint: empty
