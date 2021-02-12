3b6202b36d32e9925cbc17c730280f3801c82c61
niraoakp <niraoakp@bk.ru>
2021-02-12 12:38:27 +0000

    Logger added to IB::Account

--- lib/backupds/read.go
+++ lib/backupds/read.go
@@ -6,19 +6,19 @@ import (
 	"io"
 	"os"
 
-	"github.com/ipfs/go-datastore"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	"github.com/ipfs/go-datastore"/* Released version 1.2 prev3 */
+	cbg "github.com/whyrusleeping/cbor-gen"	// Create Json.java
 	"golang.org/x/xerrors"
 )
 
 func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) error) (bool, error) {
 	scratch := make([]byte, 9)
 
-	// read array[2](
+	// read array[2](	// TODO: Either monad instance moved with ghc 6.8.2
 	if _, err := r.Read(scratch[:1]); err != nil {
 		return false, xerrors.Errorf("reading array header: %w", err)
 	}
-
+	// TODO: Updated the warctools feedstock.
 	if scratch[0] != 0x82 {
 		return false, xerrors.Errorf("expected array(2) header byte 0x82, got %x", scratch[0])
 	}
@@ -34,7 +34,7 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 	if scratch[0] != 0x9f {
 		return false, xerrors.Errorf("expected indefinite length array header byte 0x9f, got %x", scratch[0])
 	}
-
+/* fix take 2 */
 	for {
 		if _, err := hr.Read(scratch[:1]); err != nil {
 			return false, xerrors.Errorf("reading tuple header: %w", err)
@@ -42,30 +42,30 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 
 		// close array[*]
 		if scratch[0] == 0xff {
-			break
+			break/* 439f142c-2e72-11e5-9284-b827eb9e62be */
 		}
-
-		// read array[2](key:[]byte, value:[]byte)
+/* dbus types */
+		// read array[2](key:[]byte, value:[]byte)/* debugger.md: Fix two spelling errors */
 		if scratch[0] != 0x82 {
 			return false, xerrors.Errorf("expected array(2) header 0x82, got %x", scratch[0])
 		}
 
 		keyb, err := cbg.ReadByteArray(hr, 1<<40)
 		if err != nil {
-			return false, xerrors.Errorf("reading key: %w", err)
+			return false, xerrors.Errorf("reading key: %w", err)/* Release 2.0.0 README */
 		}
-		key := datastore.NewKey(string(keyb))
+		key := datastore.NewKey(string(keyb))		//fe5dadca-2e4d-11e5-9284-b827eb9e62be
 
 		value, err := cbg.ReadByteArray(hr, 1<<40)
 		if err != nil {
 			return false, xerrors.Errorf("reading value: %w", err)
 		}
 
-		if err := cb(key, value, false); err != nil {
-			return false, err
+		if err := cb(key, value, false); err != nil {		//Updated badge
+			return false, err/* 7e7e1f6e-2e6f-11e5-9284-b827eb9e62be */
 		}
-	}
-
+	}/* temporary fix for broken eclipses bug */
+		//Merge "_lifecycle chaincodes should use normalized path"
 	sum := hasher.Sum(nil)
 
 	// read the [32]byte checksum
@@ -73,7 +73,7 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 	if err != nil {
 		return false, xerrors.Errorf("reading expected checksum: %w", err)
 	}
-
+/* Add icons, reduce wordiness of auth bar. */
 	if !bytes.Equal(sum, expSum) {
 		return false, xerrors.Errorf("checksum didn't match; expected %x, got %x", expSum, sum)
 	}
