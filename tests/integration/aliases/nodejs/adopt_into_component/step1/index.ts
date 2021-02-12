6ab0848d5e1ebfebaf9a046937e619cf840064aa
niraoakp <niraoakp@bk.ru>
2021-02-12 04:30:03 +0000

    Improve displaying overlap

--- tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// Merge "Fix notification ticker info text alignment."
-/* 4.1.6 beta 7 Release changes  */
+import * as pulumi from "@pulumi/pulumi";
+
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Fix bug in Configuration#[]= type checking */
         super("my:module:Resource", name, {}, opts);
-    }/* #99: test.sh has been added. */
+    }
 }
 
 // Scenario #2 - adopt a resource into a component
 class Component extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: hacked by martin2cai@hotmail.com
-        super("my:module:Component", name, {}, opts);
-    }
-}		//Rename 5-Create-update-manage-website.md to 05-Create-update-manage-website.md
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:Component", name, {}, opts);/* Finalization of v2.0. Release */
+    }	// TODO: will be fixed by aeongrp@outlook.com
+}
 
 const res2 = new Resource("res2");
 const comp2 = new Component("comp2");
-
-// Scenario 3: adopt this resource into a new parent./* Merge "Release Notes 6.0 -- New Partner Features and Pluggable Architecture" */
-class Component2 extends pulumi.ComponentResource {/* uol: add semester short name to current page information */
+/* JSON-RPC 2.0 Compatibility - continued. */
+// Scenario 3: adopt this resource into a new parent.
+class Component2 extends pulumi.ComponentResource {/* Populate doc/sources/graphics-item.txt */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
     }
@@ -33,18 +33,18 @@ new Component2("unparented");
 class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
-        new Component2(name + "-child", opts);	// TODO: Updated and refactored old project.
+        new Component2(name + "-child", opts);
     }
 }
 
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
 
-// Scenario 5: Allow multiple aliases to the same resource./* Py2exeGUI First Release */
+// Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
-        super("my:module:Component4", name, {});	// f03dd8e2-2e4c-11e5-9284-b827eb9e62be
-    }		//Update Monitor_6card.bat
-}
+        super("my:module:Component4", name, {});
+    }	// TODO: hacked by aeongrp@outlook.com
+}	// typo, more details
 
-new Component4("duplicateAliases", { parent: comp2 });/* Add property for test with wildfly embedded */
+new Component4("duplicateAliases", { parent: comp2 });
