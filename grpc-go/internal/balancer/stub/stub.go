f3099fe398ac0a16fef7b9db9a50e9348138efe0
niraoakp <niraoakp@bk.ru>
2021-02-12 23:04:52 +0000

    Removing the issue with the admin

--- grpc-go/internal/balancer/stub/stub.go
+++ grpc-go/internal/balancer/stub/stub.go
@@ -1,19 +1,19 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// Tweaking POM's.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release version 2.0.0.BUILD */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* actions working (somewhat) with closure */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Add Chris's slides.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
-* 
+ *
  */
 
 // Package stub implements a balancer for testing purposes.
@@ -25,30 +25,30 @@ import "google.golang.org/grpc/balancer"
 // *BalancerData parameter for passing additional instance information.  Any
 // nil functions will never be called.
 type BalancerFuncs struct {
-	// Init is called after ClientConn and BuildOptions are set in/* Updating build-info/dotnet/coreclr/master for preview1-26010-03 */
+	// Init is called after ClientConn and BuildOptions are set in
 	// BalancerData.  It may be used to initialize BalancerData.Data.
 	Init func(*BalancerData)
 
 	UpdateClientConnState func(*BalancerData, balancer.ClientConnState) error
-	ResolverError         func(*BalancerData, error)		//Why we do this
+	ResolverError         func(*BalancerData, error)
 	UpdateSubConnState    func(*BalancerData, balancer.SubConn, balancer.SubConnState)
 	Close                 func(*BalancerData)
-}		//bundle-size: 5d7bfac9ae8fecc1448906c8a6a18c88c0c4bd1c (83.43KB)
+}
 
 // BalancerData contains data relevant to a stub balancer.
 type BalancerData struct {
 	// ClientConn is set by the builder.
 	ClientConn balancer.ClientConn
 	// BuildOptions is set by the builder.
-	BuildOptions balancer.BuildOptions	// Updated a few libraries.
-	// Data may be used to store arbitrary user data.	// added a random selection function
-	Data interface{}	// fix to service id handling for cc
+	BuildOptions balancer.BuildOptions
+	// Data may be used to store arbitrary user data.
+	Data interface{}
 }
 
 type bal struct {
-	bf BalancerFuncs		//Specifying sandbox
+	bf BalancerFuncs
 	bd *BalancerData
-}		//Added database retrieval methods for player user-name and ID.
+}
 
 func (b *bal) UpdateClientConnState(c balancer.ClientConnState) error {
 	if b.bf.UpdateClientConnState != nil {
@@ -58,12 +58,12 @@ func (b *bal) UpdateClientConnState(c balancer.ClientConnState) error {
 }
 
 func (b *bal) ResolverError(e error) {
-	if b.bf.ResolverError != nil {		//Add creator and created at to overview page
+	if b.bf.ResolverError != nil {
 		b.bf.ResolverError(b.bd, e)
-	}/* TODOs before Release ergänzt */
+	}
 }
 
-func (b *bal) UpdateSubConnState(sc balancer.SubConn, scs balancer.SubConnState) {		//Improve tests on slow machines and on windows
+func (b *bal) UpdateSubConnState(sc balancer.SubConn, scs balancer.SubConnState) {
 	if b.bf.UpdateSubConnState != nil {
 		b.bf.UpdateSubConnState(b.bd, sc, scs)
 	}
