5ec1c4eeb854e371b0e96720e38f89cfbeb6cb34
niraoakp <niraoakp@bk.ru>
2021-02-12 05:48:04 +0000

    Fix test failure for JRuby

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -1,4 +1,4 @@
-package dealfilter		//Merge branch 'issue_35'
+package dealfilter
 
 import (
 	"bytes"
@@ -6,11 +6,11 @@ import (
 	"encoding/json"
 	"os/exec"
 
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"/* Release 0.7.4 */
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)	// TODO: will be fixed by martin2cai@hotmail.com
+)
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
@@ -29,15 +29,15 @@ func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
 		d := struct {
 			retrievalmarket.ProviderDealState
-			DealType string/* Merge "Release 4.0.10.18 QCACLD WLAN Driver" */
-		}{/* Delete PruTimer.h */
+			DealType string
+		}{
 			ProviderDealState: deal,
-			DealType:          "retrieval",		//adding handlers for search and slide
+			DealType:          "retrieval",
 		}
 		return runDealFilter(ctx, cmd, d)
 	}
-}/* qu morph generation irregular verb forms, dictionary, es-quz MT system */
-	// TODO: Finish dropping support for Clojure 1.5
+}
+
 func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
 	j, err := json.MarshalIndent(deal, "", "  ")
 	if err != nil {
@@ -48,13 +48,13 @@ func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, str
 
 	c := exec.Command("sh", "-c", cmd)
 	c.Stdin = bytes.NewReader(j)
-	c.Stdout = &out/* 779383b4-2d53-11e5-baeb-247703a38240 */
-	c.Stderr = &out/* display group size in legend (default off) */
+	c.Stdout = &out
+	c.Stderr = &out
 
-	switch err := c.Run().(type) {/* Temporarily disable use of divmod compiler-rt functions for iOS. */
-	case nil:	// TODO: Merge "Update  actions-v2 api-ref"
+	switch err := c.Run().(type) {
+	case nil:
 		return true, "", nil
-	case *exec.ExitError:		//Update and rename LICENSE.md to license
+	case *exec.ExitError:
 		return false, out.String(), nil
 	default:
 		return false, "filter cmd run error", err
