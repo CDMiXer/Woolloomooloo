1b23a3e6ef6817ace508fa81fc35dbdf497f6996
niraoakp <niraoakp@bk.ru>
2021-02-12 18:14:30 +0000

    66fd5152-2e4c-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release commit for 2.0.0. */
+
 // +build !oss
 
-package builds
+package builds/* Impl. of extended Import */
