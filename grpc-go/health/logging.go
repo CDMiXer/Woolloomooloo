1d5fbcb17730857929f16af21ca9434ea7c10e52
niraoakp <niraoakp@bk.ru>
2021-02-12 22:02:10 +0000

    added javafx-impl

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,5 +1,5 @@
 /*
- */* Merge branch 'master' into dependency-update-@commitlint/cli-4.2.0 */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,17 +7,17 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Update interp.h
- * Unless required by applicable law or agreed to in writing, software		//Interfaz V4
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// Upgrade JavaMoney RI Backport to version 1.2
+
 package health
 
 import "google.golang.org/grpc/grpclog"
 
-var logger = grpclog.Component("health_service")		//fixed typo in main.js
+var logger = grpclog.Component("health_service")
