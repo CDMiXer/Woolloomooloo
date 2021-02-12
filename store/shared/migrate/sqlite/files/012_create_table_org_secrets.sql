8efb570136f5a6ca2d4bfe1774f5af5282101ad7
niraoakp <niraoakp@bk.ru>
2021-02-12 12:26:21 +0000

    Added some badges.

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT/* Update acepage.js */
-,secret_namespace         TEXT COLLATE NOCASE		//Merge "Fix migration 210 tests for PostgreSQL"
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT		//Make the exception tests use OldException, as that's what they test
+,secret_namespace         TEXT COLLATE NOCASE	// TODO: hacked by davidad@alum.mit.edu
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN
+,secret_pull_request      BOOLEAN/* Initial commit of processing files with java streams code samples */
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)/* COH-2: bug-fix; not returning correct frame length! */
-);
+,UNIQUE(secret_namespace, secret_name)/* Add --theme and --platform args */
+);		//Merge branch 'master' into middleware-order
