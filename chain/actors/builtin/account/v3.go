1336ebf907f19555a52d70edfe26144fbe515c6c
niraoakp <niraoakp@bk.ru>
2021-02-12 02:55:08 +0000

    Updating the register at 200727_093843

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,13 +1,13 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: will be fixed by 13860583249@yeah.net
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
-)
+)	// Add readYaml mock.
 
 var _ State = (*state3)(nil)
 
@@ -16,14 +16,14 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}		//Removed unixodbc-dev package
 	return &out, nil
 }
 
 type state3 struct {
-	account3.State
+	account3.State	// TODO: hacked by davidad@alum.mit.edu
 	store adt.Store
-}
+}	// Ajout des methodes a la classe source
 
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
