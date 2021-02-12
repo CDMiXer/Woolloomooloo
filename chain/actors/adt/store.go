575a97c8084dccca0f1661c3968e44ef519e4b7f
niraoakp <niraoakp@bk.ru>
2021-02-12 00:56:28 +0000

    More minor doc updates

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -5,13 +5,13 @@ import (
 
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-)
+)	// New Project with Wb Configuration File
 
 type Store interface {
 	Context() context.Context
 	cbor.IpldStore
-}
+}		//README: Add warning about the status of Basho
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
-}
+}/* Release of eeacms/eprtr-frontend:0.2-beta.21 */
