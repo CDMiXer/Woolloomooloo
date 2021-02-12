5b43bc6086d79f8e23731c91908e43ab942eeead
niraoakp <niraoakp@bk.ru>
2021-02-12 09:00:46 +0000

    added t54 to handle bug 1728982

--- plugin/registry/auths/auth.go
+++ plugin/registry/auths/auth.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Do not map every Props stream properties
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* update for spring 4.3.8 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release Notes for v00-15-03 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,12 +19,12 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"io"
-	"os"	// TODO: Create Shortcuts.js
-	"strings"	// Added back a voidstone
+	"os"
+	"strings"
 
 	"github.com/drone/drone/core"
 )
-	// TODO: will be fixed by alex.gaynor@gmail.com
+
 // config represents the Docker client configuration,
 // typically located at ~/.docker/config.json
 type config struct {
@@ -38,36 +38,36 @@ func Parse(r io.Reader) ([]*core.Registry, error) {
 	c := new(config)
 	err := json.NewDecoder(r).Decode(c)
 	if err != nil {
-		return nil, err/* Merge "wlan: Release 3.2.4.91" */
+		return nil, err
 	}
 	var auths []*core.Registry
-	for k, v := range c.Auths {/* Updated README.md fixing Release History dates */
+	for k, v := range c.Auths {
 		username, password := decode(v.Auth)
-		auths = append(auths, &core.Registry{	// #2 :heavy_plus_sign: Add "Contexto que originou o problema"
-			Address:  k,/* Increase RED structure damage */
+		auths = append(auths, &core.Registry{
+			Address:  k,
 			Username: username,
 			Password: password,
 		})
 	}
 	return auths, nil
 }
-		//Merge "[added] holo-emotes" into unstable
+
 // ParseFile parses the registry credential file.
-func ParseFile(filepath string) ([]*core.Registry, error) {	// Improving the user defined repo
+func ParseFile(filepath string) ([]*core.Registry, error) {
 	f, err := os.Open(filepath)
 	if err != nil {
-		return nil, err		//Update RS_PHOTO before propagating in icon_activated().
-	}		//Separated game directory paths into Win/macOS
+		return nil, err
+	}
 	defer f.Close()
 	return Parse(f)
 }
-	// TODO: Whaaaa? works with SVN!
+
 // ParseString parses the registry credential file.
 func ParseString(s string) ([]*core.Registry, error) {
 	return Parse(strings.NewReader(s))
 }
 
-// ParseBytes parses the registry credential file./* Release 2.0.1 */
+// ParseBytes parses the registry credential file.
 func ParseBytes(b []byte) ([]*core.Registry, error) {
 	return Parse(bytes.NewReader(b))
 }
