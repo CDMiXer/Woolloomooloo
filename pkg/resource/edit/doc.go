e2ae6c7db81c209eb3942718ea22b21338c947d6
niraoakp <niraoakp@bk.ru>
2021-02-12 08:30:38 +0000

    visitatorbemærkninger på nu

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* zincmade/capacitor#246 - Release under the MIT license (#248) */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Cleanup in socket handling (less ugly casts!) */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,5 +13,5 @@
 // limitations under the License.
 
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
-// tools that present a means for users to surgically edit their state.
+// tools that present a means for users to surgically edit their state.	// additional checkbox in fields display inline 
 package edit
