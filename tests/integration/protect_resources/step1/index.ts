447af55d6038ee25d7cf966b04bb1258857c1a79
niraoakp <niraoakp@bk.ru>
2021-02-12 03:58:36 +0000

    chore(package): update devDependency semantic-release to version 15.11.0

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: deep copy and deep compare implemented
-import { Resource } from "./resource";
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//add docs for context of callbacks
 
+import { Resource } from "./resource";
+		//close pipes
 // Allocate a resource and protect it:
-let a = new Resource("eternal", { state: 1 }, { protect: true });
+let a = new Resource("eternal", { state: 1 }, { protect: true });/* Uprava vzhledu */
