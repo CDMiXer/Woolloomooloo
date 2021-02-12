7868f5f603608f045ef7acec9a45c66ff914311c
niraoakp <niraoakp@bk.ru>
2021-02-12 08:37:59 +0000

    launcher: removed unnecessary app_root definition

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS orgsecrets (
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BLOB
+,secret_data              BLOB		//Minor fix to amazon social
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
