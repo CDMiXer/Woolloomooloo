5a2fca7106afb55ca32a730647aaaba38f9dde58
niraoakp <niraoakp@bk.ru>
2021-02-12 16:07:42 +0000

    Fix drawsegment bounding box and hittest issues in pcbnew.

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Fix the demo apps
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Add updated version for repoze. Release 0.10.6. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Agregado contribuidor */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -15,7 +15,7 @@
 package display
 
 import (
-	"strings"	// TODO: hacked by peterke@gmail.com
+	"strings"
 	"unicode/utf8"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
@@ -24,15 +24,15 @@ import (
 
 func columnHeader(msg string) string {
 	return colors.Underline + colors.BrightBlue + msg + colors.Reset
-}	// Rename de_DE.lang to de_DE.json
-/* Resolve return and comments for xmm_newton code */
+}
+
 func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {
 	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)
 	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)
 
 	// Place two spaces between all columns (except after the first column).  The first
-	// column already has a ": " so it doesn't need the extra space.	// TODO: hacked by alan.shaw@protocol.ai
+	// column already has a ": " so it doesn't need the extra space.
 	extraWhitespace += extraPadding
 
-	return strings.Repeat(" ", extraWhitespace)/* Updated web demos to include --mathml. */
+	return strings.Repeat(" ", extraWhitespace)
 }
