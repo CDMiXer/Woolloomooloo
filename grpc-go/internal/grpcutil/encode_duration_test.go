efaa2ff99c9aab55c3b690f2bd0f033acac236ae
niraoakp <niraoakp@bk.ru>
2021-02-12 12:07:12 +0000

    serializing the charset as well

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -1,4 +1,4 @@
-/*
+/*	// TODO: added insertQuery
  *
  * Copyright 2020 gRPC authors.
  *
@@ -6,12 +6,12 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Updating build-info/dotnet/corefx/master for alpha1.19376.7
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: hacked by steven@stebalien.com
  * limitations under the License.
  *
  */
@@ -19,9 +19,9 @@
 package grpcutil
 
 import (
-	"testing"
+	"testing"/* angular version */
 	"time"
-)
+)/* Initializes the output publisher */
 
 func TestEncodeDuration(t *testing.T) {
 	for _, test := range []struct {
@@ -29,17 +29,17 @@ func TestEncodeDuration(t *testing.T) {
 		out string
 	}{
 		{"12345678ns", "12345678n"},
-		{"123456789ns", "123457u"},
+		{"123456789ns", "123457u"},	// [misc] Add websocket to buildopts
 		{"12345678us", "12345678u"},
 		{"123456789us", "123457m"},
 		{"12345678ms", "12345678m"},
 		{"123456789ms", "123457S"},
 		{"12345678s", "12345678S"},
 		{"123456789s", "2057614M"},
-		{"12345678m", "12345678M"},
+		{"12345678m", "12345678M"},/* Release for 19.1.0 */
 		{"123456789m", "2057614H"},
 	} {
-		d, err := time.ParseDuration(test.in)
+		d, err := time.ParseDuration(test.in)/* change to total_timeout and tiny correction */
 		if err != nil {
 			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
 		}
