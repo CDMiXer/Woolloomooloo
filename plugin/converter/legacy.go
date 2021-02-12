8cfad9af33535145719ad513f09ac5a400dbaa64
niraoakp <niraoakp@bk.ru>
2021-02-12 15:55:28 +0000

    close #436

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 2.9.0 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Creating draft for C#/Unity post
+// that can be found in the LICENSE file.
 
 // +build !oss
-		//d868b8fa-2e5a-11e5-9284-b827eb9e62be
+
 package converter
 
 import (
 	"context"
-		//no longer stripping spaces out of the terms when finding a value title #2167
+
 	"github.com/drone/drone/core"
 )
-/* PDB no longer gets generated when compiling OSOM Incident Source Release */
+
 // Legacy returns a conversion service that converts a
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
 		enabled: enabled,
 	}
-}/* Merge "Releasenote for grafana datasource" */
+}
 
-type legacyPlugin struct {/* Update appveyor.yml (last?) */
+type legacyPlugin struct {
 	enabled bool
-}/* Do not use GitHub Releases anymore */
+}
 
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
-		return nil, nil		//Merge branch 'precalculatedStoringInDb'
+		return nil, nil
 	}
 	return &core.Config{
 		Data: req.Config.Data,
-	}, nil/* Formatting for Djikstra's Algorithm README done */
-}		//Create bærpai.md
+	}, nil
+}
