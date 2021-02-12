f7800b9511c828401bca5d3dd4846a1899943e8e
niraoakp <niraoakp@bk.ru>
2021-02-12 05:02:07 +0000

    Nice function names for collection checkers won't work in 1.3

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -11,15 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* New hack EvidenceSchedulingPlugin, created by doycho */
 package runner
 
 import "github.com/drone/drone/core"
-
+/* Added What is Freedom Colony section */
 func toSecretMap(secrets []*core.Secret) map[string]string {
-	set := map[string]string{}
+	set := map[string]string{}/* - Extracted any HTML code from the Person class into SS files */
 	for _, secret := range secrets {
 		set[secret.Name] = secret.Data
-	}
+	}		//e3a2d3de-2e48-11e5-9284-b827eb9e62be
 	return set
 }
