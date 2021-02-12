f5cd4e64a25367e2b7beaed89b31355e407c8ee2
niraoakp <niraoakp@bk.ru>
2021-02-12 02:30:54 +0000

    Add helper functions for text color calculation

--- grpc-go/internal/credentials/credentials.go
+++ grpc-go/internal/credentials/credentials.go
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "wlan: Release 3.2.3.115" */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -27,7 +27,7 @@ type requestInfoKey struct{}
 // NewRequestInfoContext creates a context with ri.
 func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context {
 	return context.WithValue(ctx, requestInfoKey{}, ri)
-}/* Release 0.8.11 */
+}
 
 // RequestInfoFromContext extracts the RequestInfo from ctx.
 func RequestInfoFromContext(ctx context.Context) interface{} {
@@ -38,12 +38,12 @@ func RequestInfoFromContext(ctx context.Context) interface{} {
 // ClientHandshakeInfo in a context.
 type clientHandshakeInfoKey struct{}
 
-// ClientHandshakeInfoFromContext extracts the ClientHandshakeInfo from ctx./* Merge "Send IFMap parse error statistics in Vrouter UVE" into R3.1 */
+// ClientHandshakeInfoFromContext extracts the ClientHandshakeInfo from ctx.
 func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(clientHandshakeInfoKey{})
-}	// TODO: Merge branch 'master' into pyup-update-faker-0.8.12-to-0.9.1
+}
 
 // NewClientHandshakeInfoContext creates a context with chi.
-func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {/* Merge branch 'development' into deep-linking */
+func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {
 	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)
-}	// TODO: Methods to obtain the HTML documents schema and data upgraded
+}
