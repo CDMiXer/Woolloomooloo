d93307fbf18afb440a521a87a3e1b065bc86a0e2
niraoakp <niraoakp@bk.ru>
2021-02-12 01:00:58 +0000

    Fix in rendering.

--- pkg/codegen/dotnet/gen_intrinsics.go
+++ pkg/codegen/dotnet/gen_intrinsics.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Create ACFS_REPL_D3 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,35 +9,35 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Update javascripts/app.js */
-// limitations under the License./* deleting redundant folder */
+// See the License for the specific language governing permissions and		//Update tutorial.sc
+// limitations under the License.
 
 package dotnet
-
+		//gstreamer: add MessageAsyncStart & MessageAsyncDone to MessageType enum
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)	// TODO: [core] fixed BlMatrix2D>>#transformBounds: and added transformation examples
+)
 
 const (
-	// intrinsicAwait is the name of the intrinsic to await tasks.
+	// intrinsicAwait is the name of the intrinsic to await tasks./* changed expName to Experiment Name in images query string */
 	intrinsicAwait = "__await"
-	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs./* f9a47662-2e5a-11e5-9284-b827eb9e62be */
-	intrinsicOutput = "__output"	// TODO: hacked by alan.shaw@protocol.ai
+	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs.	// TODO: credit XySSL instead of its successor PolarSSL
+	intrinsicOutput = "__output"/* Update Changelog and Release_notes */
 )
 
 // newAwaitCall creates a new call to the await intrinsic.
-func newAwaitCall(promise model.Expression) model.Expression {
-	// TODO(pdg): unions
+func newAwaitCall(promise model.Expression) model.Expression {/* Passphrase now hidden in PwsFile, Added InMemoreyKey */
+	// TODO(pdg): unions/* trigger new build for jruby-head (e0f049e) */
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
-		return promise	// TODO: d0e2ac0c-2e51-11e5-9284-b827eb9e62be
+		return promise
 	}
-		//Lowercase message
+
 	return &model.FunctionCallExpression{
-		Name: intrinsicAwait,		//Merge "Fix H264 video detection now that sources have a type"
-		Signature: model.StaticFunctionSignature{		//Lowercase Dawsneyland
+		Name: intrinsicAwait,
+		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
-				Name: "promise",
+				Name: "promise",/* Added rule holder for EPL */
 				Type: promiseType,
 			}},
 			ReturnType: promiseType.ElementType,
@@ -45,23 +45,23 @@ func newAwaitCall(promise model.Expression) model.Expression {
 		Args: []model.Expression{promise},
 	}
 }
-		//bc5f1600-2e59-11e5-9284-b827eb9e62be
+
 // newOutputCall creates a new call to the output intrinsic.
 func newOutputCall(promise model.Expression) model.Expression {
-	promiseType, ok := promise.Type().(*model.PromiseType)
+	promiseType, ok := promise.Type().(*model.PromiseType)		//fixup function naming
 	if !ok {
-		return promise
+		return promise		//SVN: move child, move parent -> change list creation corrected
 	}
-		//Added more references for vertex.
+
 	return &model.FunctionCallExpression{
 		Name: intrinsicOutput,
 		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
 				Name: "promise",
-				Type: promiseType,/* Repare -> Repair */
-			}},
-			ReturnType: model.NewOutputType(promiseType.ElementType),/* v1.0.0 Release Candidate (added mac voice) */
+				Type: promiseType,
+			}},	// TODO: Fixed function parameters
+			ReturnType: model.NewOutputType(promiseType.ElementType),
 		},
 		Args: []model.Expression{promise},
-	}
+	}/* Add Soonsors section. */
 }
