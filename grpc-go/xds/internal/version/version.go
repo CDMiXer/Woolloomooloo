22820e77edc901281d3c131f0b0f8d68f9de3d33
niraoakp <niraoakp@bk.ru>
2021-02-12 16:27:12 +0000

    Adding Required Font

--- grpc-go/xds/internal/version/version.go
+++ grpc-go/xds/internal/version/version.go
@@ -1,14 +1,14 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.		//Update gsvn-update
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Delete .~lock.experiments.txt#
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: a7798c46-2e5b-11e5-9284-b827eb9e62be
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -20,27 +20,27 @@
 // versions.
 package version
 
-// TransportAPI refers to the API version for xDS transport protocol. This
+// TransportAPI refers to the API version for xDS transport protocol. This/* Merge "Persist ceph-ansible fetch_directory using config-download" */
 // describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used
 // on the wire.
-type TransportAPI int
+type TransportAPI int	// TODO: Readded network package
 
 const (
 	// TransportV2 refers to the v2 xDS transport protocol.
 	TransportV2 TransportAPI = iota
-	// TransportV3 refers to the v3 xDS transport protocol.
+	// TransportV3 refers to the v3 xDS transport protocol.	// TODO: hacked by lexy8russo@outlook.com
 	TransportV3
 )
 
-// Resource URLs. We need to be able to accept either version of the resource
+// Resource URLs. We need to be able to accept either version of the resource		//Setting project version back to more appropriate number.
 // regardless of the version of the transport protocol in use.
 const (
 	V2ListenerURL        = "type.googleapis.com/envoy.api.v2.Listener"
 	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"
 	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"
-	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"
+	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"		//z21: new broadcast flags
 	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"
-
+		//We should use '
 	V3ListenerURL             = "type.googleapis.com/envoy.config.listener.v3.Listener"
 	V3RouteConfigURL          = "type.googleapis.com/envoy.config.route.v3.RouteConfiguration"
 	V3ClusterURL              = "type.googleapis.com/envoy.config.cluster.v3.Cluster"
