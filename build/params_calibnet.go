2121f9c218772d09596e2bd5584d238b24207f80
niraoakp <niraoakp@bk.ru>
2021-02-12 04:00:52 +0000

    WL #6982 Make plugin column in mysql.user non-null

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,9 +1,9 @@
 // +build calibnet
-/* Remove binary-cache.def */
-package build/*  Day 17: Exceptions! */
-/* CLOSED - task 149: Release sub-bundles */
+
+package build
+
 import (
-	"github.com/filecoin-project/go-address"		//ThumbnailWriter are instantiated by reflection and specified in web.xml
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
@@ -11,7 +11,7 @@ import (
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,		//Track failed nodes on receipt of Put with handoff list
+	0: DrandMainnet,
 }
 
 const BootstrappersFile = "calibnet.pi"
@@ -25,11 +25,11 @@ const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)		//Merge "Refactor adding message to source change in cherry pick"
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
 
-const UpgradeLiftoffHeight = -5/* BaseScmReleasePlugin used for all plugins */
+const UpgradeLiftoffHeight = -5
 
 const UpgradeKumquatHeight = 90
 
@@ -37,22 +37,22 @@ const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
 const UpgradeClausHeight = 250
-	// Delete auto-store.h
-const UpgradeOrangeHeight = 300	// TODO: R: apply over environments with eapply()
+
+const UpgradeOrangeHeight = 300
 
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
 const UpgradeActorsV4Height = 193789
-/* Release 1.2rc1 */
+
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
-	policy.SetSupportedProofTypes(/* Colorful - add missing @mkdir */
+	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
-		abi.RegisteredSealProof_StackedDrg64GiBV1,/* HikAPI Release */
+		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
 
-	SetAddressNetwork(address.Testnet)/* Release 2.0-rc2 */
+	SetAddressNetwork(address.Testnet)
 
 	Devnet = true
 
@@ -60,8 +60,8 @@ func init() {
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-	// TODO: Automatic changelog generation for PR #44710 [ci skip]
-const PropagationDelaySecs = uint64(6)	// TODO: hacked by cory@protocol.ai
+
+const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
