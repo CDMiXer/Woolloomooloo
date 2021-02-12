0baf8fb61410dcde9aceba2e9db7ff1119e6cf35
niraoakp <niraoakp@bk.ru>
2021-02-12 00:18:24 +0000

    CWS gnumake3: remove ilib incase dll is not created

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -4,7 +4,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
-/* added WelcomeFragment to the fragments array (drawer) */
+
 type resources struct {
 	kubeClient kubernetes.Interface
 	namespace  string
@@ -12,8 +12,8 @@ type resources struct {
 
 func (r resources) GetSecret(name, key string) (string, error) {
 	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
-	if err != nil {/* Back to .net 3.5 */
-		return "", err/* Rename edge_chambers_type_-2.svg to edge_chambers_outline_0_0.svg */
+	if err != nil {
+		return "", err
 	}
 	return string(secret.Data[key]), nil
 }
