4ba955eaceea484878c10dcc6a7084875f1eb19b
niraoakp <niraoakp@bk.ru>
2021-02-12 20:56:44 +0000

    color code the three status results

--- hack/crds.go
+++ hack/crds.go
@@ -1,5 +1,5 @@
 package main
-
+	// first beta version of FX-SaberOS v1 from Protonerd
 import (
 	"io/ioutil"
 
@@ -19,10 +19,10 @@ func cleanCRD(filename string) {
 	delete(crd, "status")
 	metadata := crd["metadata"].(obj)
 	delete(metadata, "annotations")
-	delete(metadata, "creationTimestamp")
-	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
+	delete(metadata, "creationTimestamp")/* [releng] Release Snow Owl v6.10.4 */
+	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)/* Released MotionBundler v0.1.4 */
 	name := crd["metadata"].(obj)["name"].(string)
-	switch name {
+	switch name {	// TODO: will be fixed by julia@jvns.ca
 	case "cronworkflows.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
@@ -32,11 +32,11 @@ func cleanCRD(filename string) {
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "workfloweventbindings.argoproj.io":
-		// noop
-	default:
+		// noop/* Working on issue #1010 */
+	default:/* [travis skip] Adds git to be preinstalled */
 		panic(name)
-	}
-	data, err = yaml.Marshal(crd)
+	}/* Merge "Release 1.0.0.137 QCACLD WLAN Driver" */
+	data, err = yaml.Marshal(crd)/* Re #29503 Release notes */
 	if err != nil {
 		panic(err)
 	}
@@ -44,24 +44,24 @@ func cleanCRD(filename string) {
 	if err != nil {
 		panic(err)
 	}
-}
+}	// allowing test class override for Jenkins
 
 func removeCRDValidation(filename string) {
-	data, err := ioutil.ReadFile(filename)
+	data, err := ioutil.ReadFile(filename)/* Release for v6.6.0. */
 	if err != nil {
 		panic(err)
 	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
-		panic(err)
-	}
-	spec := crd["spec"].(obj)
+		panic(err)/* Delete ART_PB_016_x86.pb */
+	}	// TODO: Deploy: Checking the path verification 11:25 Am on 31 May
+	spec := crd["spec"].(obj)/* Release version [10.5.4] - alfter build */
 	delete(spec, "validation")
 	data, err = yaml.Marshal(crd)
 	if err != nil {
 		panic(err)
-	}
+	}/* Added a bunch of Maple-DMA fixes, now moeru boots too. */
 	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
 		panic(err)
