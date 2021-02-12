1b7987cc5f9f58bddf7ae6e2e19374a958adc81a
niraoakp <niraoakp@bk.ru>
2021-02-12 07:42:29 +0000

    Adding seo title section list

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
-
-import (
+	// TODO: hacked by caojiaoyue@protonmail.com
+import (/* API comment on properties with weak refs. */
 	"os"
-	// TODO: Moved to correct folder.
-	logging "github.com/ipfs/go-log/v2"
-)		//Added template style for Window and MovableWidget.
 
-func SetupLogLevels() {	// TODO: hacked by ligi@ligi.de
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {	// TODO: minor bug-fixes
+	logging "github.com/ipfs/go-log/v2"
+)
+
+func SetupLogLevels() {	// TODO: hacked by seth@sethvargo.com
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {	// TODO: hacked by why@ipfs.io
 		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")		//Merge "Add small note about Swift."
+		_ = logging.SetLogLevel("dht", "ERROR")		//fixed missing NCN-> in welcome.php
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")		//Mockito should be a test dependency only.
+		_ = logging.SetLogLevel("connmgr", "WARN")/* +Releases added and first public release committed. */
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
-}
+}	// TODO: add guide to readme
