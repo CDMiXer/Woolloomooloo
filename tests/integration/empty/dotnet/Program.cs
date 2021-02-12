99b20abacc201d66fac4628c4677fcde2a8f6203
niraoakp <niraoakp@bk.ru>
2021-02-12 08:49:42 +0000

    update README with non Object example

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-		//Picker: Fix tracebacks in threads
+
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
-{		//2NEsSBxbBeBVLoa7dPHJFemCOR5ywVjz
+{
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() => {});	// Minor enhancements on remove script
+        return Deployment.RunAsync(() => {});
     }
 }
