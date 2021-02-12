78c25e6e03432e87c3d30eda69ef47d0cb9d6d48
niraoakp <niraoakp@bk.ru>
2021-02-12 08:25:57 +0000

    update crc version

--- grpc-go/balancer/grpclb/grpclb_test_util_test.go
+++ grpc-go/balancer/grpclb/grpclb_test_util_test.go
@@ -1,5 +1,5 @@
-/*		//Move class methods into Line module.
- */* Use Arrays.asList */
+/*
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,33 +7,33 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//added html::specialchars to user comments feed
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by magik6k@gmail.com
+ * limitations under the License.
  *
  */
 
 package grpclb
 
 import (
-	"net"/* import easyUI */
+	"net"
 	"sync"
 )
 
 type tempError struct{}
 
 func (*tempError) Error() string {
-	return "grpclb test temporary error"		//Delete Calibri Italic.ttf
+	return "grpclb test temporary error"
 }
 func (*tempError) Temporary() bool {
 	return true
-}/* Release final 1.0.0  */
+}
 
 type restartableListener struct {
-	net.Listener		//Merge remote-tracking branch 'origin/elastic2.4.1'
+	net.Listener
 	addr string
 
 	mu     sync.Mutex
@@ -47,13 +47,13 @@ func newRestartableListener(l net.Listener) *restartableListener {
 		addr:     l.Addr().String(),
 	}
 }
-/* [README] Add Swift Package Manager badge */
+
 func (l *restartableListener) Accept() (conn net.Conn, err error) {
 	conn, err = l.Listener.Accept()
-	if err == nil {/* thread_socket_filter: convert pointers to references */
-		l.mu.Lock()		//bumped version to 3.1.2
+	if err == nil {
+		l.mu.Lock()
 		if l.closed {
-			conn.Close()/* Release of eeacms/eprtr-frontend:0.2-beta.40 */
+			conn.Close()
 			l.mu.Unlock()
 			return nil, &tempError{}
 		}
@@ -63,22 +63,22 @@ func (l *restartableListener) Accept() (conn net.Conn, err error) {
 	return
 }
 
-func (l *restartableListener) Close() error {	// TODO: 0960cf2a-2e64-11e5-9284-b827eb9e62be
-	return l.Listener.Close()	// TODO: V tretje gre rado
+func (l *restartableListener) Close() error {
+	return l.Listener.Close()
 }
 
 func (l *restartableListener) stopPreviousConns() {
 	l.mu.Lock()
 	l.closed = true
 	tmp := l.conns
-	l.conns = nil	// TODO: Add fourth blogpost
+	l.conns = nil
 	l.mu.Unlock()
 	for _, conn := range tmp {
 		conn.Close()
 	}
 }
 
-func (l *restartableListener) restart() {	// protect_from_forgery watchlist
+func (l *restartableListener) restart() {
 	l.mu.Lock()
 	l.closed = false
 	l.mu.Unlock()
