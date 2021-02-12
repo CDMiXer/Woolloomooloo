f40f7c523a96dc25db0cfec4f64671925ee9dfb2
niraoakp <niraoakp@bk.ru>
2021-02-12 22:16:33 +0000

    Create folderwatcher.py

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,10 +1,10 @@
 package sealing
 
 import (
-	"testing"/* Delete ctc.ckpt-230.data-00000-of-00001 */
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* Version 1.0.0.0 Release. */
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,20 +21,20 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 }
 
 func TestFillersFromRem(t *testing.T) {
-	for i := 8; i < 32; i++ {/* [Showcase] fix broken icon link */
+	for i := 8; i < 32; i++ {
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
-		// 2/* Finalized 3.9 OS Release Notes. */
+		// 2
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})	// TODO: Delete NN_classfier.ipynb
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
 		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
-		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()	// Fixed array indexing error in typedHKeys.
+		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
