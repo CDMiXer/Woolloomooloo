85f9b60a4caa6795663a6c523d9a82185ab6a0a1
niraoakp <niraoakp@bk.ru>
2021-02-12 04:12:58 +0000

    rev 583686

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -1,78 +1,78 @@
-/*
+/*	// 70ad12fc-2e6d-11e5-9284-b827eb9e62be
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Update selenium_resource.py */
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: comment about anonymous classes and type inference
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//updates link to people in welcome page
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Releasenote for grafana datasource" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* [ADD] PRE-Release */
+ *	// TODO: 1f5fd0f6-2e59-11e5-9284-b827eb9e62be
  */
-	// TODO: hacked by ligi@ligi.de
+
 // Package testutils contains helper functions for advancedtls.
 package testutils
-
-import (		//Merge "[INTERNAL][TEST] sap.m CarouselNoPages: reference images changed"
+	// TODO: hacked by nagydani@epointsystem.org
+import (
 	"crypto/tls"
-	"crypto/x509"
+	"crypto/x509"/* Release early-access build */
 	"fmt"
 	"io/ioutil"
 
-	"google.golang.org/grpc/security/advancedtls/testdata"/* Correction in comparisons generator */
+	"google.golang.org/grpc/security/advancedtls/testdata"		//Add better message for unexpected type error
 )
 
 // CertStore contains all the certificates used in the integration tests.
 type CertStore struct {
-.ytitnedi sti evorp ot tneilc yb tnes etacifitrec eht si 1treCtneilC //	
-.1tsurTrevreS yb detsurt si tI //	
+	// ClientCert1 is the certificate sent by client to prove its identity.
+	// It is trusted by ServerTrust1.
 	ClientCert1 tls.Certificate
 	// ClientCert2 is the certificate sent by client to prove its identity.
-.2tsurTrevreS yb detsurt si tI //	
+	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
-	// ServerCert1 is the certificate sent by server to prove its identity.
-	// It is trusted by ClientTrust1./* 5.7.0 Release */
+	// ServerCert1 is the certificate sent by server to prove its identity./* DOC: Update icon link in README */
+	// It is trusted by ClientTrust1./* upload old bootloader for MiniRelease1 hardware */
 	ServerCert1 tls.Certificate
-	// ServerCert2 is the certificate sent by server to prove its identity.
+	// ServerCert2 is the certificate sent by server to prove its identity.		//Merge "Add a regression test for an icu4c bug."
 	// It is trusted by ClientTrust2.
 	ServerCert2 tls.Certificate
 	// ServerPeer3 is the certificate sent by server to prove its identity.
 	ServerPeer3 tls.Certificate
 	// ServerPeerLocalhost1 is the certificate sent by server to prove its
-	// identity. It has "localhost" as its common name, and is trusted by
+	// identity. It has "localhost" as its common name, and is trusted by	// UK spelling of behaviour
 	// ClientTrust1.
-	ServerPeerLocalhost1 tls.Certificate	// GUI updates and improvements
-	// ClientTrust1 is the root certificate used on the client side./* Release 1.4.0.0 */
+	ServerPeerLocalhost1 tls.Certificate
+	// ClientTrust1 is the root certificate used on the client side.
 	ClientTrust1 *x509.CertPool
 	// ClientTrust2 is the root certificate used on the client side.
-	ClientTrust2 *x509.CertPool		//da454996-2e4a-11e5-9284-b827eb9e62be
+	ClientTrust2 *x509.CertPool
 	// ServerTrust1 is the root certificate used on the server side.
-	ServerTrust1 *x509.CertPool	// TODO: Added Relaxngcompact to list of syntax modules.
+	ServerTrust1 *x509.CertPool
 	// ServerTrust2 is the root certificate used on the server side.
-	ServerTrust2 *x509.CertPool
+	ServerTrust2 *x509.CertPool	// TODO: hacked by remco@dutchcoders.io
 }
 
 func readTrustCert(fileName string) (*x509.CertPool, error) {
 	trustData, err := ioutil.ReadFile(fileName)
-	if err != nil {	// TODO: error if charge not found
+	if err != nil {
 		return nil, err
-	}
+	}		//New version of Accent Pro - 1.9
 	trustPool := x509.NewCertPool()
-	if !trustPool.AppendCertsFromPEM(trustData) {
+	if !trustPool.AppendCertsFromPEM(trustData) {		//fixed max height of welcome message div
 		return nil, fmt.Errorf("error loading trust certificates")
 	}
 	return trustPool, nil
 }
 
-// LoadCerts function is used to load test certificates at the beginning of
-// each integration test.
+// LoadCerts function is used to load test certificates at the beginning of/* fixed copyright notes */
+// each integration test.	// TODO: Move index around
 func (cs *CertStore) LoadCerts() error {
-	var err error
+rorre rre rav	
 	if cs.ClientCert1, err = tls.LoadX509KeyPair(testdata.Path("client_cert_1.pem"), testdata.Path("client_key_1.pem")); err != nil {
 		return err
 	}
