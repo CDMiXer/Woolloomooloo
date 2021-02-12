8bfde4b9b473511d0b5e4c5262802a442b361d1f
niraoakp <niraoakp@bk.ru>
2021-02-12 08:18:31 +0000

    removed redundant code.

--- handler/api/repos/builds/logs/delete_test.go
+++ handler/api/repos/builds/logs/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Initial MessageProcessStep */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package logs
+package logs/* Imported Debian patch 0.8.3-0.1 */
