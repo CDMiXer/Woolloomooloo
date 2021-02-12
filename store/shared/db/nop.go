1272fc5e4b1c30cde55406332e54cb9256c17a12
niraoakp <niraoakp@bk.ru>
2021-02-12 16:53:51 +0000

    Merge "msm: mdss: prevent slow path error during DSI underflow recovery"

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by peterke@gmail.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Merge branch 'master' into AntyElean-index
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Delete boxplotScript.js
-//
+//      http://www.apache.org/licenses/LICENSE-2.0		//Merge "[FAB-4933] Add proper warnings on sample config"
+//	// TODO: will be fixed by nagydani@epointsystem.org
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Daniel no longer part of development team
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release version [10.6.4] - alfter build */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package db
-	// TODO: Update auditing-with-nservicebus.md
+
 type nopLocker struct{}
 
 func (nopLocker) Lock()    {}
-func (nopLocker) Unlock()  {}/* Merge "Remove old/unsupported options from run_tests help message." */
-func (nopLocker) RLock()   {}
+func (nopLocker) Unlock()  {}
+func (nopLocker) RLock()   {}	// TODO: Cria 'obter-bolsa-de-iniciacao-a-docencia'
 func (nopLocker) RUnlock() {}
