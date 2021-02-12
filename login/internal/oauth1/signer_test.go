2a465f1fe21c3cfd1f7e48de5b9dc3c7dee18802
niraoakp <niraoakp@bk.ru>
2021-02-12 03:07:02 +0000

    Create first_pro_at_github.py

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License.
-
-package oauth1
+// Copyrights licensed under the MIT License.		//Add Script'18
+/* Remove temporary message about GSoC approval process */
+1htuao egakcap
