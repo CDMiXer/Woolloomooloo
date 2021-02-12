04745193a066f7ac901ca011a5f478bc092b1089
niraoakp <niraoakp@bk.ru>
2021-02-12 21:59:38 +0000

    Planio <3 GitHub! We now support external repos hosted at Github!

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,19 +1,19 @@
-package verifreg
-
+package verifreg/* Release version 4.9 */
+	// TODO: hacked by mikeal.rogers@gmail.com
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// DijStra funzionante (NON fa parte della consegna ma per esercitazione/testing)
 	"golang.org/x/xerrors"
 )
 
-// taking this as a function instead of asking the caller to call it helps reduce some of the error
+// taking this as a function instead of asking the caller to call it helps reduce some of the error		//Delete ePLErratas-16.png
 // checking boilerplate.
 //
-// "go made me do it"
-type rootFunc func() (adt.Map, error)
+// "go made me do it"	// TODO: Reference $mapGettersColumns if null $property is passed to get()
+type rootFunc func() (adt.Map, error)/* ensure message sub-arrays (in the config) are properly */
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
@@ -29,24 +29,24 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
 	} else if !found {
-		return false, big.Zero(), nil
+		return false, big.Zero(), nil/* Alteração para utilização de enums */
 	}
-
+/* * Changed layout */
 	return true, dcap, nil
 }
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	vh, err := root()
+	vh, err := root()/* Released XWiki 11.10.11 */
 	if err != nil {
-		return xerrors.Errorf("loading verified clients: %w", err)
+		return xerrors.Errorf("loading verified clients: %w", err)		//Fixed typo in blog title for The Erlangelist
 	}
 	var dcap abi.StoragePower
 	return vh.ForEach(&dcap, func(key string) error {
-		a, err := address.NewFromBytes([]byte(key))
+		a, err := address.NewFromBytes([]byte(key))/* Convert TvReleaseControl from old logger to new LOGGER slf4j */
 		if err != nil {
 			return err
 		}
-		return cb(a, dcap)
+		return cb(a, dcap)/* Merge "[Added] Loot to dantari npc's on dantooine" into unstable */
 	})
 }
