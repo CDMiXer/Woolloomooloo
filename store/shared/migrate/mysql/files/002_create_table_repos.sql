6f83edef7c07f91cbf16a7deb2273d048d80921c
niraoakp <niraoakp@bk.ru>
2021-02-12 16:29:28 +0000

    Delete 00_iniciales

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
 -- name: create-table-repos
-		//Mat Id Floater added
+
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
-,repo_uid                   VARCHAR(250)/* Merge "API modifications to handle profiles in Contacts." */
+,repo_uid                   VARCHAR(250)
 ,repo_user_id               INTEGER
 ,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)
+,repo_scm                   VARCHAR(50)		//Moved libicu.h from src/common to src/zorbatypes.
 ,repo_clone_url             VARCHAR(2000)
-,repo_ssh_url               VARCHAR(2000)		//enforce tighter constraints on comp. configs
+,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
-,repo_branch                VARCHAR(250)/* Included seasons-greetings in README */
+,repo_branch                VARCHAR(250)		//7d5733cc-2e6b-11e5-9284-b827eb9e62be
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER		//Merge branch 'develop' into enhancement/1824-error-message
+,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
-,repo_updated               INTEGER
+,repo_updated               INTEGER/* Changed behavior of getIdentifyingValue() */
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
 );
-
+	// TODO: will be fixed by martin2cai@hotmail.com
 -- name: alter-table-repos-add-column-no-fork
 
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-no-pulls		//[FIX]: base_calendar: Fixed wrong dates problems in recurrence
-
+-- name: alter-table-repos-add-column-no-pulls
+	// Create jira_to_analytics.go
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-cancel-pulls	// TODO: c9051cda-2e6b-11e5-9284-b827eb9e62be
+-- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-push
-
+	// TODO: Enhance example
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
