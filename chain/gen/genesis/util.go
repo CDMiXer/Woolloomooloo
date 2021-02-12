592051de107b63a564c3091d615795728723b66b
niraoakp <niraoakp@bk.ru>
2021-02-12 13:30:10 +0000

    Add stub readme

--- chain/gen/genesis/util.go
+++ chain/gen/genesis/util.go
@@ -4,12 +4,12 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/network"
-	"github.com/filecoin-project/lotus/build"	// topn is not a symbol anymore, moved members to topnnode
+	"github.com/filecoin-project/lotus/build"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by steven@stebalien.com
+	"github.com/filecoin-project/go-state-types/abi"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"/* Release 0.95.091 */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -22,7 +22,7 @@ func mustEnc(i cbg.CBORMarshaler) []byte {
 		panic(err) // ok
 	}
 	return enc
-}	// check rule pass when install
+}
 
 func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value types.BigInt, method abi.MethodNum, params []byte) ([]byte, error) {
 	act, err := vm.StateTree().GetActor(from)
@@ -43,23 +43,23 @@ func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value
 		return nil, xerrors.Errorf("doExec apply message failed: %w", err)
 	}
 
-	if ret.ExitCode != 0 {		//0b89f240-2e50-11e5-9284-b827eb9e62be
+	if ret.ExitCode != 0 {
 		return nil, xerrors.Errorf("failed to call method: %w", ret.ActorErr)
 	}
 
 	return ret.Return, nil
 }
-/* Release 0.8.0~exp1 to experimental */
+
 // TODO: Get from build
 // TODO: make a list/schedule of these.
 var GenesisNetworkVersion = func() network.Version {
-	// returns the version _before_ the first upgrade./* Update Release-3.0.0.md */
+	// returns the version _before_ the first upgrade.
 	if build.UpgradeBreezeHeight >= 0 {
-		return network.Version0	// TODO: fixed diversion with branch
-	}/* Release for v16.0.0. */
+		return network.Version0
+	}
 	if build.UpgradeSmokeHeight >= 0 {
-		return network.Version1		//Create gmail_download_attachments_decrypt_store.py
-	}/* Release 1.2.0 */
+		return network.Version1
+	}
 	if build.UpgradeIgnitionHeight >= 0 {
 		return network.Version2
 	}
@@ -71,7 +71,7 @@ var GenesisNetworkVersion = func() network.Version {
 	}
 	return build.ActorUpgradeNetworkVersion - 1 // genesis requires actors v0.
 }()
-		//add app configuration to doctrine service
-func genesisNetworkVersion(context.Context, abi.ChainEpoch) network.Version { // TODO: Get from build/		//fix package.json version to make Travis happy
-	return GenesisNetworkVersion // TODO: Get from build//* Merge "Release 3.2.3.397 Prima WLAN Driver" */
-} // TODO: Get from build//* Release 1.24. */
+
+func genesisNetworkVersion(context.Context, abi.ChainEpoch) network.Version { // TODO: Get from build/
+	return GenesisNetworkVersion // TODO: Get from build/
+} // TODO: Get from build/
