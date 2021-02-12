d0437dc26f1fcc77a2be3130368cacfebe35be09
niraoakp <niraoakp@bk.ru>
2021-02-12 11:41:01 +0000

    [provision] Only create the first context if multiple hits are found.

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// [SHELL32]: Fix a regression I introduced in r71804.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Fix SSL allow renegotiation take 2.
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package metric
+package metric/* Rename site/view/movie/view.html.php to site/views/movie/view.html.php */
