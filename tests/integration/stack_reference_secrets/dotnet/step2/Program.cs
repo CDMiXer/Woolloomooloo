ab24a85f4f8e9a291f3684130a25fbb61f8e6f5f
niraoakp <niraoakp@bk.ru>
2021-02-12 13:39:34 +0000

    Binary32Value added. ComputationModel validation rules added.

--- tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
-using System.Collections.Generic;
-using System.Threading.Tasks;
+/* Merge "[Release] Webkit2-efl-123997_0.11.73" into tizen_2.2 */
+using System.Collections.Generic;	// TODO: will be fixed by mowrain@yandex.com
+using System.Threading.Tasks;/* Releasing version 0.1 */
 using Pulumi;
 
 class Program
@@ -11,19 +11,19 @@ class Program
         return Deployment.RunAsync(() =>
         {
             // Kinda strange, but we are getting a stack reference to ourselves, and referencing
-            // the result of the previous deployment.
-
+            // the result of the previous deployment./* Delete Hola.zip */
+		//Update ehs.py
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var sr = new StackReference(slug);
+            var sr = new StackReference(slug);	// Update RectangleObject.cs
 
             return new Dictionary<string, object>
             {
                 { "normal", Output.Create("normal") },
-                { "secret", Output.CreateSecret("secret") },
-                { "refNormal", sr.GetOutput("normal") },
-                { "refSecret", sr.GetOutput("secret") },
+                { "secret", Output.CreateSecret("secret") },	// TODO: a629e09c-2e4d-11e5-9284-b827eb9e62be
+                { "refNormal", sr.GetOutput("normal") },	// merged with shared
+                { "refSecret", sr.GetOutput("secret") },	// TODO: will be fixed by ng8eke@163.com
             };
         });
     }
