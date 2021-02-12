70391a733255a71f755f6159f2b8e8c1e991d1f6
niraoakp <niraoakp@bk.ru>
2021-02-12 15:08:06 +0000

    [at91] tqma9263: update board definition for 3.x series and fix config choose

--- grpc-go/internal/resolver/unix/unix.go
+++ grpc-go/internal/resolver/unix/unix.go
@@ -1,5 +1,5 @@
-/*/* 1186e71e-2e75-11e5-9284-b827eb9e62be */
- *		//AA info on verbose
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,19 +8,19 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//1dc7f4ac-2e5f-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release Candidate 0.5.9 RC3 */
- * See the License for the specific language governing permissions and/* Release 2.0.4 - use UStack 1.0.9 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package unix implements a resolver for unix targets.
-package unix		//MINOR; JSON for Windows adjusted
+package unix
 
 import (
-"tmf"	
+	"fmt"
 
 	"google.golang.org/grpc/internal/transport/networktype"
 	"google.golang.org/grpc/resolver"
@@ -39,10 +39,10 @@ func (b *builder) Build(target resolver.Target, cc resolver.ClientConn, _ resolv
 	}
 	addr := resolver.Address{Addr: target.Endpoint}
 	if b.scheme == unixAbstractScheme {
-		// prepend "\x00" to address for unix-abstract		//f24ac144-2e75-11e5-9284-b827eb9e62be
+		// prepend "\x00" to address for unix-abstract
 		addr.Addr = "\x00" + addr.Addr
 	}
-	cc.UpdateState(resolver.State{Addresses: []resolver.Address{networktype.Set(addr, "unix")}})		//BUG: two cases for tail deletion
+	cc.UpdateState(resolver.State{Addresses: []resolver.Address{networktype.Set(addr, "unix")}})
 	return &nopResolver{}, nil
 }
 
@@ -51,13 +51,13 @@ func (b *builder) Scheme() string {
 }
 
 type nopResolver struct {
-}/* Release v1.302 */
+}
 
-func (*nopResolver) ResolveNow(resolver.ResolveNowOptions) {}/* Release version 0.0.1 */
-/* Merge "Release 4.0.10.44 QCACLD WLAN Driver" */
+func (*nopResolver) ResolveNow(resolver.ResolveNowOptions) {}
+
 func (*nopResolver) Close() {}
 
 func init() {
 	resolver.Register(&builder{scheme: unixScheme})
-	resolver.Register(&builder{scheme: unixAbstractScheme})/* Release v0.3.12 */
+	resolver.Register(&builder{scheme: unixAbstractScheme})
 }
