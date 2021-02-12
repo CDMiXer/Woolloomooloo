07dd4c9a2fca123d1ee420479fac9240c3920e72
niraoakp <niraoakp@bk.ru>
2021-02-12 15:24:52 +0000

    Merge remote-tracking branch 'origin/GP-795_dev747368_fix_ISO9660_fs_probe'

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -5,19 +5,19 @@ import * as pulumi from "@pulumi/pulumi";
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }/* switch showcase to log() */
+    }
 }
 
-)nerdlihc s'ti lla dna( tnenopmoc a emaner - 3# oiranecS //
+// Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
     resource2: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Avoid unnecessary object allocations. */
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, {parent: this});
         this.resource2 = new Resource("otherchild", {parent: this});
     }
 }
-const comp3 = new ComponentThree("comp3");/* Create getRelease.Rd */
+const comp3 = new ComponentThree("comp3");
