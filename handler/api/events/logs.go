31576e69eb7543979e674e80b5ad407bebaa707c
niraoakp <niraoakp@bk.ru>
2021-02-12 16:44:03 +0000

    `Write MOAR Tests`?

--- handler/api/events/logs.go
+++ handler/api/events/logs.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Update - jwt auth
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: 31d6e28c-2e6e-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* no more line crazyness */
 package events
 
-import (
+import (	// git info added to manifest
 	"context"
 	"encoding/json"
 	"io"
@@ -32,29 +32,29 @@ import (
 // to the http.Response in an event stream format.
 func HandleLogStream(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,	// Added Trait for Gone presentations.
 	stages core.StageStore,
 	steps core.StepStore,
 	stream core.LogStream,
-) http.HandlerFunc {
+) http.HandlerFunc {/* Delete Window.png */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			namespace = chi.URLParam(r, "owner")	// TODO: hacked by zaq1tomo@gmail.com
+			name      = chi.URLParam(r, "name")/* Update calltoactionbar.jsx */
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
-			return
+			return		//Merge branch 'master' into cjb-get-posts-array
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)		//Update doc/01-mvc/README.md
 			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* Release: Making ready to release 5.8.1 */
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
@@ -68,21 +68,21 @@ func HandleLogStream(
 			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
-		if err != nil {
+		if err != nil {/* Release 3.2 095.02. */
 			render.NotFound(w, err)
 			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
-		if err != nil {
+		if err != nil {/* Merge "Release 1.0.0.87 QCACLD WLAN Driver" */
 			render.NotFound(w, err)
 			return
-		}
+		}	// TODO: Bintray script extracted, Gradle wrapper upgraded to 3.2.1
 
 		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
-		h.Set("Cache-Control", "no-cache")
+		h.Set("Cache-Control", "no-cache")/* 1.3.12 Release */
 		h.Set("Connection", "keep-alive")
-		h.Set("X-Accel-Buffering", "no")
+		h.Set("X-Accel-Buffering", "no")		//Style the project search based on the command panel css.
 
 		f, ok := w.(http.Flusher)
 		if !ok {
