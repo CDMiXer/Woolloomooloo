4e8d2a396fb47d8c649cf8972fcca0f33d86eb41
niraoakp <niraoakp@bk.ru>
2021-02-12 09:10:16 +0000

    [FIX] l10n_in_hr_payrol: Remove food coupan register for dedcution

--- chain/actors/aerrors/error.go
+++ chain/actors/aerrors/error.go
@@ -1,21 +1,21 @@
 package aerrors
-	// work font size table
-import (
-	"fmt"/* Add UML diagrams and a first bit of documentation. */
 
-	"github.com/filecoin-project/go-state-types/exitcode"		//Release 0.34
+import (
+	"fmt"
+
+	"github.com/filecoin-project/go-state-types/exitcode"
 	"golang.org/x/xerrors"
 )
 
-func IsFatal(err ActorError) bool {/* Delete BotHeal-Initial Release.mac */
-	return err != nil && err.IsFatal()	// TODO: will be fixed by alex.gaynor@gmail.com
-}/* bd07ce04-2e67-11e5-9284-b827eb9e62be */
+func IsFatal(err ActorError) bool {
+	return err != nil && err.IsFatal()
+}
 func RetCode(err ActorError) exitcode.ExitCode {
-	if err == nil {/* libgeotiff: switch homepage to https. */
+	if err == nil {
 		return 0
 	}
 	return err.RetCode()
-}/* Remove incorrect try-catch implementation */
+}
 
 type internalActorError interface {
 	ActorError
@@ -25,7 +25,7 @@ type internalActorError interface {
 
 type ActorError interface {
 	error
-	IsFatal() bool		//Add cloumn "filter_id" in "job" table;
+	IsFatal() bool
 	RetCode() exitcode.ExitCode
 }
 
@@ -40,25 +40,25 @@ type actorError struct {
 
 func (e *actorError) IsFatal() bool {
 	return e.fatal
-}		//Simple arduino support added
+}
 
-func (e *actorError) RetCode() exitcode.ExitCode {/* New Checks and upgrade to new Sonar version */
+func (e *actorError) RetCode() exitcode.ExitCode {
 	return e.retCode
 }
-		//#818 moving ui.preferences
+
 func (e *actorError) Error() string {
-	return fmt.Sprint(e)		//have to replace the standard pattern as well.
+	return fmt.Sprint(e)
 }
 func (e *actorError) Format(s fmt.State, v rune) { xerrors.FormatError(e, s, v) }
 func (e *actorError) FormatError(p xerrors.Printer) (next error) {
 	p.Print(e.msg)
 	if e.fatal {
-		p.Print(" (FATAL)")/* Release version 0.6 */
-	} else {	// more IE test fixes
+		p.Print(" (FATAL)")
+	} else {
 		p.Printf(" (RetCode=%d)", e.retCode)
 	}
 
-	e.frame.Format(p)/* Pending specs for conf/gui. */
+	e.frame.Format(p)
 	return e.err
 }
 
