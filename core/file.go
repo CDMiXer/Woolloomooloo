3c67d9464b50cca86a40aa8e193acab75ad1148f
niraoakp <niraoakp@bk.ru>
2021-02-12 08:56:20 +0000

    FIxed link

--- core/file.go
+++ core/file.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
-//		//add maven-eclipse-plunin.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Version upgrade to 1.0.1
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,13 +14,13 @@ erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
 
 package core
 
-import "context"	// Updated the vic feedstock.
+import "context"
 
 type (
 	// File represents the raw file contents in the remote
 	// version control system.
 	File struct {
-etyb][ ataD		
+		Data []byte
 		Hash []byte
 	}
 
@@ -30,7 +30,7 @@ etyb][ ataD
 	FileArgs struct {
 		Commit string
 		Ref    string
-	}	// TODO: hacked by zaq1tomo@gmail.com
+	}
 
 	// FileService provides access to contents of files in
 	// the remote source code management service (e.g. GitHub).
