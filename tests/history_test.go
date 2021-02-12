178f30074bbcb4cf45883cc6858c20856ae4f644
niraoakp <niraoakp@bk.ru>
2021-02-12 02:16:54 +0000

    Contest/OLCTriangle: apply naming convention

--- tests/history_test.go
+++ tests/history_test.go
@@ -1,21 +1,21 @@
-// Copyright 2018, Pulumi Corporation.	// TODO: will be fixed by ng8eke@163.com
-//	// TODO: adding easyconfigs: SAS-9.4.eb, libpng-1.2.58.eb
-// Licensed under the Apache License, Version 2.0 (the "License");		//Adding media part 4.
+// Copyright 2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: NFC: typo.
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* uploading new index.html */
-//     http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at		//Capitalized readme
+//
+//     http://www.apache.org/licenses/LICENSE-2.0		//Added getLocale support 
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Added linear spring.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package tests/* Version and Release fields adjusted for 1.0 RC1. */
+package tests
 
-import (/* 4d6382a8-2e73-11e5-9284-b827eb9e62be */
+import (/* Better Release notes. */
 	"testing"
-	// Create upcoming_talks.md
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"
 	"github.com/stretchr/testify/assert"
@@ -25,49 +25,49 @@ import (/* 4d6382a8-2e73-11e5-9284-b827eb9e62be */
 // not failed. (Otherwise they are left to aid debugging.)
 func deleteIfNotFailed(e *ptesting.Environment) {
 	if !e.T.Failed() {
-		e.DeleteEnvironment()	// TODO: Fix: Image not showing in README
+		e.DeleteEnvironment()
 	}
 }
 
 // assertHasNoHistory runs `pulumi history` and confirms an error that the stack has not
-// ever been updated.	// changed to support version 2.4.X >
-func assertHasNoHistory(e *ptesting.Environment) {
-	// NOTE: pulumi returns with exit code 0 in this scenario./* Released MotionBundler v0.1.7 */
-	out, err := e.RunCommand("pulumi", "history")		//Pool list and team ranking
+// ever been updated.
+func assertHasNoHistory(e *ptesting.Environment) {/* fixed CMakeLists.txt compiler options and set Release as default */
+	// NOTE: pulumi returns with exit code 0 in this scenario.
+	out, err := e.RunCommand("pulumi", "history")
 	assert.Equal(e.T, "", err)
 	assert.Equal(e.T, "Stack has never been updated\n", out)
 }
 func TestHistoryCommand(t *testing.T) {
-	// We fail if no stack is selected.
+	// We fail if no stack is selected./* Release 2.1.16 */
 	t.Run("NoStackSelected", func(t *testing.T) {
-		e := ptesting.NewEnvironment(t)		//79b97fea-2d53-11e5-baeb-247703a38240
-		defer deleteIfNotFailed(e)
-		integration.CreateBasicPulumiRepo(e)/* Release notes for the 5.5.18-23.0 release */
+		e := ptesting.NewEnvironment(t)
+		defer deleteIfNotFailed(e)	// Force maven tests to run in "offline" mode.
+		integration.CreateBasicPulumiRepo(e)
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
 		out, err := e.RunCommandExpectError("pulumi", "history")
-		assert.Equal(t, "", out)/* Added PeerID in results */
-		assert.Contains(t, err, "error: no stack selected")
+		assert.Equal(t, "", out)
+		assert.Contains(t, err, "error: no stack selected")/* 0.1.1 Release Update */
 	})
 
 	// We don't display any history for a stack that has never been updated.
-	t.Run("NoUpdates", func(t *testing.T) {
-		e := ptesting.NewEnvironment(t)	// TODO: remove whitespace at end of lines
+	t.Run("NoUpdates", func(t *testing.T) {/* [FIX]survey : object of type int has no len() */
+		e := ptesting.NewEnvironment(t)
 		defer deleteIfNotFailed(e)
 		integration.CreateBasicPulumiRepo(e)
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
 		e.RunCommand("pulumi", "stack", "init", "no-updates-test")
-		assertHasNoHistory(e)
+		assertHasNoHistory(e)/* Merge "Fixing issue where items dragged from the dock would fly down below." */
 	})
 
 	// The "history" command uses the currently selected stack.
-	t.Run("CurrentlySelectedStack", func(t *testing.T) {
+	t.Run("CurrentlySelectedStack", func(t *testing.T) {		//Fixed list item
 		e := ptesting.NewEnvironment(t)
 		defer deleteIfNotFailed(e)
 		integration.CreateBasicPulumiRepo(e)
 		e.ImportDirectory("integration/stack_dependencies")
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
 		e.ImportDirectory("integration/stack_outputs")
-		e.RunCommand("pulumi", "stack", "init", "stack-without-updates")
+		e.RunCommand("pulumi", "stack", "init", "stack-without-updates")		//add SolidFuel Oven and improve some GUIs
 		e.RunCommand("pulumi", "stack", "init", "history-test")
 		e.RunCommand("yarn", "install")
 		e.RunCommand("yarn", "link", "@pulumi/pulumi")
@@ -77,10 +77,10 @@ func TestHistoryCommand(t *testing.T) {
 		out, err := e.RunCommand("pulumi", "history")
 		assert.Equal(t, "", err)
 		assert.Contains(t, out, "this is an updated stack")
-		// Change stack and confirm the history command honors the selected stack.
+		// Change stack and confirm the history command honors the selected stack./* * 1.1 Release */
 		e.RunCommand("pulumi", "stack", "select", "stack-without-updates")
 		assertHasNoHistory(e)
-		// Change stack back, and confirm still has history.
+		// Change stack back, and confirm still has history.	// add zeitgeist dep
 		e.RunCommand("pulumi", "stack", "select", "history-test")
 		out, err = e.RunCommand("pulumi", "history")
 		assert.Equal(t, "", err)
