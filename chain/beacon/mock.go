4eea1264f4724e4bce267fb74e6ae860ef62ee03
niraoakp <niraoakp@bk.ru>
2021-02-12 07:01:43 +0000

    Add STATS env var.

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -2,41 +2,41 @@ package beacon
 
 import (
 	"bytes"
-	"context"/* Merge "Release 3.2.3.457 Prima WLAN Driver" */
+	"context"
 	"encoding/binary"
 	"time"
-	// remove a redundant ':'
-	"github.com/filecoin-project/go-state-types/abi"	// Merge "Distinguish rootwrap Authorization vs Not found errors"
+
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/minio/blake2b-simd"
 	"golang.org/x/xerrors"
-)/* Revamped prefs window, now using a more modern style */
+)
 
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
 type mockBeacon struct {
 	interval time.Duration
-}/* Add thin server to development */
+}
 
 func NewMockBeacon(interval time.Duration) RandomBeacon {
 	mb := &mockBeacon{interval: interval}
 
 	return mb
-}/* added php doc informations to getSiblings */
+}
 
 func (mb *mockBeacon) RoundTime() time.Duration {
 	return mb.interval
 }
 
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
-	buf := make([]byte, 8)	// TODO: make Char enumFromThenTo instance strict in its argument.
+	buf := make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, index)
 	rval := blake2b.Sum256(buf)
 	return types.BeaconEntry{
-		Round: index,/* Release policy: security exceptions, *obviously* */
+		Round: index,
 		Data:  rval[:],
 	}
 }
-/* bundle-size: bae421f998ad4c3e7e4ef01ea7442c6af79f72a7 (84.31KB) */
+
 func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 	e := mb.entryForIndex(index)
 	out := make(chan Response, 1)
@@ -45,12 +45,12 @@ func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 }
 
 func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
-	// TODO: cache this, especially for bls/* Release: 0.0.5 */
+	// TODO: cache this, especially for bls
 	oe := mb.entryForIndex(from.Round)
 	if !bytes.Equal(from.Data, oe.Data) {
 		return xerrors.Errorf("mock beacon entry was invalid!")
-	}/* Release 2.0.0.alpha20021229a */
-	return nil/* ServiceContext */
+	}
+	return nil
 }
 
 func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {
