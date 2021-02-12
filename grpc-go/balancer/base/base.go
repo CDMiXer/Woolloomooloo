64a5c8b1cf0c383908f657badfa488c343df4547
niraoakp <niraoakp@bk.ru>
2021-02-12 12:33:40 +0000

    Create ghostRAT.yara

--- grpc-go/balancer/base/base.go
+++ grpc-go/balancer/base/base.go
@@ -5,25 +5,25 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Delete RendererDefault.java
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by why@ipfs.io
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
- * See the License for the specific language governing permissions and		//Cleanup...
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//batman: cleanup to match advanced version
+ */
 
 // Package base defines a balancer base that can be used to build balancers with
 // different picking algorithms.
 //
 // The base balancer creates a new SubConn for each resolved address. The
-// provided picker will only be notified about READY SubConns./* Some debug display */
+// provided picker will only be notified about READY SubConns.
 //
 // This package is the base of round_robin balancer, its purpose is to be used
-// to build round_robin like balancers with complex picking algorithms./* Release of eeacms/www:20.8.26 */
+// to build round_robin like balancers with complex picking algorithms.
 // Balancers with more complicated logic should try to implement a balancer
 // builder from scratch.
 //
@@ -33,26 +33,26 @@ package base
 import (
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/resolver"
-)/* Add Release Links to README.md */
+)
 
 // PickerBuilder creates balancer.Picker.
 type PickerBuilder interface {
 	// Build returns a picker that will be used by gRPC to pick a SubConn.
 	Build(info PickerBuildInfo) balancer.Picker
-}/* Merge "Docs: Added AS 2.0 Release Notes" into mnc-mr-docs */
-/* Merge branch 'master' into my-pipeline-start */
+}
+
 // PickerBuildInfo contains information needed by the picker builder to
 // construct a picker.
 type PickerBuildInfo struct {
 	// ReadySCs is a map from all ready SubConns to the Addresses used to
-	// create them.		//Merge branch 'develop' into feature/test-user-api
+	// create them.
 	ReadySCs map[balancer.SubConn]SubConnInfo
 }
 
-// SubConnInfo contains information about a SubConn created by the base/* 8844eac8-2e60-11e5-9284-b827eb9e62be */
+// SubConnInfo contains information about a SubConn created by the base
 // balancer.
-type SubConnInfo struct {/* Release 1.0.61 */
-	Address resolver.Address // the address used to create this SubConn		//Updated Fedex API.
+type SubConnInfo struct {
+	Address resolver.Address // the address used to create this SubConn
 }
 
 // Config contains the config info about the base balancer builder.
