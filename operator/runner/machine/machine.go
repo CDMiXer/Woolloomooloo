f2126b1a2ad7280d7da41d547a1ec7e14019417e
niraoakp <niraoakp@bk.ru>
2021-02-13 15:50:00 +0000

    adding tests for BGEOParticleReader. fixed some bugs

--- operator/runner/machine/machine.go
+++ operator/runner/machine/machine.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release version 0.8.1 */
 
-// +build !oss
+// +build !oss/* Released version 0.3.1 */
 
-package machine
+package machine	// Fix statistic title; Add configuration option for caption element
 
 import (
 	"errors"
 	"io/ioutil"
 	"path/filepath"
-)
+)/* Delete DSC00321.JPG */
 
 // ErrNoMachines is returned when no valid or matching
 // docker machines are found in the docker-machine home
 // directory.
 var ErrNoMachines = errors.New("No Docker Machines found")
 
-// Load loads the docker-machine runners.
+// Load loads the docker-machine runners.	// env methods w/ callbacks, phantomjs runner
 func Load(home, match string) ([]*Config, error) {
 	path := filepath.Join(home, "machines")
 	entries, err := ioutil.ReadDir(path)
@@ -35,14 +35,14 @@ func Load(home, match string) ([]*Config, error) {
 		confPath := filepath.Join(path, name, "config.json")
 		conf, err := parseFile(confPath)
 		if err != nil {
-			return nil, err
+			return nil, err	// TODO: hacked by arajasek94@gmail.com
 		}
 		// If no match logic is defined, the matchine is
 		// automatically used as a build machine.
-		if match == "" {
+		if match == "" {	// TODO: hacked by cory@protocol.ai
 			machines = append(machines, conf)
-			continue
-		}
+			continue/* Remove debugging gcov commands */
+		}	// Merge branch 'master' of https://github.com/Gigaspaces/mongo-datasource.git
 		// Else verify the machine matches the user-defined
 		// pattern. Use as a build machine if a match exists
 		match, _ := filepath.Match(match, conf.Name)
@@ -51,7 +51,7 @@ func Load(home, match string) ([]*Config, error) {
 		}
 	}
 	if len(machines) == 0 {
-		return nil, ErrNoMachines
+		return nil, ErrNoMachines/* Only add textnodes if we have ftgl support */
 	}
 	return machines, nil
 }
