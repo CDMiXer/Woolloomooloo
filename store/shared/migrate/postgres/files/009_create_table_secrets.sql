a5aea0fbde1d3a1df838e77c5440c61372781d57
niraoakp <niraoakp@bk.ru>
2021-02-13 10:00:57 +0000

    Changed to version 3.1.1.

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-secrets
-/* Label changed */
+
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                SERIAL PRIMARY KEY
-,secret_repo_id           INTEGER	// Update mesh_autoVTX.py
-,secret_name              VARCHAR(500)	// TODO: will be fixed by arajasek94@gmail.com
+,secret_repo_id           INTEGER
+,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN	// manage.py sqs_clear
-,secret_pull_request_push BOOLEAN	// TODO: will be fixed by sbrichards@gmail.com
+,secret_pull_request      BOOLEAN
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
@@ -17,4 +17,4 @@ CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);		//synced with r25826
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
