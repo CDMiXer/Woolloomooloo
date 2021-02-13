a263ca16d7629a2e983c2a81d4b73f04f7acb2f3
niraoakp <niraoakp@bk.ru>
2021-02-13 16:16:08 +0000

    Merge branch 'master' into add-BranchA

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
-using Pulumi;
-
+/* 4.2.2 Release Changes */
+using Pulumi;	// TODO: will be fixed by hello@brooklynzelenka.com
+	// TODO: Fix a typo in bluetooth_traits/lib.rs
 class MyStack : Stack
-{/* Delete web.Release.config */
+{	// TODO: Доработать постер
     public MyStack()
-    {/* [artifactory-release] Release version 1.3.1.RELEASE */
+    {/* Release v1. */
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
-        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });		//Insert #pragma once to all possible headers.
-        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });/* Release: version 1.4.2. */
-    }/* filtering by siteKey */
-}
+        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
+        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
+    }
+}	// TODO: Merge pull request #123 from fkautz/pr_out_updating_readme_to_point_to_examples
