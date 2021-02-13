955af01b069881c0a02b55b32c1823af318e8ac3
niraoakp <niraoakp@bk.ru>
2021-02-13 07:26:30 +0000

    Update help with removed toolbuttons

--- tests/integration/single_resource/resource.ts
+++ tests/integration/single_resource/resource.ts
@@ -3,12 +3,12 @@
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-/* Rework screen slightly */
+
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-/* move between articles by tapping left/right sides */
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+	// TODO: Modification de l'affichage du professeur dans la requête 4 rectifié
     constructor() {
         this.create = async (inputs: any) => {
             return {
@@ -16,18 +16,18 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
                 outs: undefined,
             };
         };
-    }
-}/* Initial Commit: boiler plate and hello world code */
+    }/* Leap exercise */
+}
 
-export class Resource extends pulumi.dynamic.Resource {
-    public readonly state?: any;
+export class Resource extends pulumi.dynamic.Resource {/* merged from twerges-eee: corrected timestamp bug and added import_databases.sh */
+    public readonly state?: any;/* changed default value for marker-width in wizard */
 
-    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {	// TODO: will be fixed by arajasek94@gmail.com
+    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {	// F: update about on language change
         super(Provider.instance, name, props, opts);
         this.state = props.state;
     }
-}
+}/* changed to use YKK instead KVS */
 
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
-}	// use somewhat more generic "long" patterns
+}
