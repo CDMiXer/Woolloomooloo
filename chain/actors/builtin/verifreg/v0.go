f18d3b90483c7afeb470dea5e23534bcc01b4976
niraoakp <niraoakp@bk.ru>
2021-02-13 13:01:49 +0000

    Add DAA, IATO, STIG; Fix SP 800-53

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,30 +1,30 @@
-package verifreg		//preparation - rename
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	// TODO: Création d'une première structure 
+/* Release 3.1.2. */
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
-)
+)/* Merge "[INTERNAL] Release notes for version 1.78.0" */
 
 var _ State = (*state0)(nil)
-
+/* Added GDPR Section to iOS ads article. */
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}/* Updated Release_notes.txt */
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
-		return nil, err
-	}/* green specs */
+	if err != nil {	// TODO: some duplicates removed
+		return nil, err	// TODO: - added  lastConfigChange and maxFolderDepth
+	}
 	return &out, nil
-}/* Release v4.5.1 alpha */
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+}
+
 type state0 struct {
-	verifreg0.State
+	verifreg0.State/* DEV issues */
 	store adt.Store
 }
 
@@ -32,26 +32,26 @@ func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Update postfix.main.cf */
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
-}
-
+}	// TODO: will be fixed by mail@overlisted.net
+/* [checkup] store data/1542298214274394354-check.json [ci skip] */
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {		//fixed CONST problem
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
-}	// TODO: will be fixed by jon@atack.com
-
-func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
+}
+/* Release v10.33 */
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {		//error php unit
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)/* Major checkin after animation added 2. */
 }
 
-func (s *state0) verifiedClients() (adt.Map, error) {
+func (s *state0) verifiedClients() (adt.Map, error) {/* Merge "Remove debian-jessie from nodepool" */
 	return adt0.AsMap(s.store, s.VerifiedClients)
-}		//Rebuilt index with rashidick
-	// TODO: JavaDocs and a bit of clean in DBOperator
+}
+
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
 }
