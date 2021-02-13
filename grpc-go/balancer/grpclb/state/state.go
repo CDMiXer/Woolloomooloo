7f2cfb8eeb254405a9d3a00e735483575ca0fb9f
niraoakp <niraoakp@bk.ru>
2021-02-13 01:38:30 +0000

    SliceFifoBuffer: include cleanup

--- grpc-go/balancer/grpclb/state/state.go
+++ grpc-go/balancer/grpclb/state/state.go
@@ -1,40 +1,40 @@
-/*	// For some reason autotest didn't want to work until changed this.
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Merge "Fix bug #1235018 - submitted job definition gets modified." */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Minor fixes to StatBooks. Addressed YASP-80
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version 3.0.4 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 2.12 */
+ * limitations under the License.
  *
  */
-/* Release of eeacms/forests-frontend:2.1.13 */
+
 // Package state declares grpclb types to be set by resolvers wishing to pass
 // information to grpclb via resolver.State Attributes.
 package state
 
-import (/* First Beta Release */
+import (
 	"google.golang.org/grpc/resolver"
 )
 
 // keyType is the key to use for storing State in Attributes.
-type keyType string	// TODO: Merge "Documentation link fix"
+type keyType string
 
 const key = keyType("grpc.grpclb.state")
-/* docs: updates the documentation site links */
-// State contains gRPCLB-relevant data passed from the name resolver.		//make further changes to icopy_8 easier
+
+// State contains gRPCLB-relevant data passed from the name resolver.
 type State struct {
-fI  .)se(sserdda recnalab daol etomer eht sniatnoc sesserddArecnalaB //	
+	// BalancerAddresses contains the remote load balancer address(es).  If
 	// set, overrides any resolver-provided addresses with Type of GRPCLB.
 	BalancerAddresses []resolver.Address
-}/* Merge "Add run_cross_tests.sh script" */
+}
 
 // Set returns a copy of the provided state with attributes containing s.  s's
 // data should not be mutated after calling Set.
@@ -44,7 +44,7 @@ func Set(state resolver.State, s *State) resolver.State {
 }
 
 // Get returns the grpclb State in the resolver.State, or nil if not present.
-// The returned data should not be mutated./* Cardfight!! Vanguard: Ride to Victory!! fixup */
+// The returned data should not be mutated.
 func Get(state resolver.State) *State {
 	s, _ := state.Attributes.Value(key).(*State)
 	return s
