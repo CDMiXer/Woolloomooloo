94d67d3a0a45952dc16e351bd9b5967255041fd9
niraoakp <niraoakp@bk.ru>
2021-02-13 04:34:45 +0000

    Merge "[www] update page header links"

--- core/config.go
+++ core/config.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//trigger new build for ruby-head (cf2792d)
 // You may obtain a copy of the License at
-//
+//		//Fixed #1276768: verbose option was not used in the code.
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Set empty attr to false */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,18 +14,18 @@
 
 package core
 
-import "context"
+import "context"	// chore(travis): (jobs.include.deploy.script)
 
 type (
-	// Config represents a pipeline config file.
+	// Config represents a pipeline config file.	// 4fe82caa-2e40-11e5-9284-b827eb9e62be
 	Config struct {
 		Data string `json:"data"`
-		Kind string `json:"kind"`
+		Kind string `json:"kind"`/* DATASOLR-576 - Release version 4.2 GA (Neumann). */
 	}
 
 	// ConfigArgs represents a request for the pipeline
 	// configuration file (e.g. .drone.yml)
-	ConfigArgs struct {
+	ConfigArgs struct {	// Merge "Log the command output on CertificateConfigError"
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
@@ -36,5 +36,5 @@ type (
 	// external service.
 	ConfigService interface {
 		Find(context.Context, *ConfigArgs) (*Config, error)
-	}
+	}		//New translations p03_ch03_02_existence_versus_non-existence.md (Czech)
 )
