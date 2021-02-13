d4114323e7afafaf13e77ad7a19f1b2242f900c1
niraoakp <niraoakp@bk.ru>
2021-02-13 09:17:52 +0000

    Merge branch 'master' into aw/more-readable-git-logs

--- grpc-go/examples/features/encryption/ALTS/client/main.go
+++ grpc-go/examples/features/encryption/ALTS/client/main.go
@@ -6,43 +6,43 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Add CORS configuration to HandiNAVI
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release of eeacms/www-devel:19.1.17 */
- * limitations under the License.	// TODO: Extract acoustic model logic
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // Binary client is an example client.
 package main
-	// TODO: hacked by steven@stebalien.com
+
 import (
 	"context"
-	"flag"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"flag"
 	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"		//941af11e-2e40-11e5-9284-b827eb9e62be
+	"google.golang.org/grpc/credentials/alts"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")	// TODO: fix justification to unsubmition.
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})	// Update Paidverts Helper.user.js
+	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
-	// TODO: hacked by alex.gaynor@gmail.com
+
 func main() {
 	flag.Parse()
 
