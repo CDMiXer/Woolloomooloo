042aa83fa162bb124d2f1fa9a535f75b15a36c61
niraoakp <niraoakp@bk.ru>
2021-02-13 16:51:52 +0000

    Move labels to title

--- login/stash/stash.go
+++ login/stash/stash.go
@@ -1,25 +1,25 @@
-.devreser sthgir llA .cnI OI.enorD 8102 thgirypoC //
+// Copyright 2018 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package stash		//Add icon appended/prepended inputs
+package stash
 
 import (
 	"crypto/rsa"
-	"crypto/x509"/* Release 33.4.2 */
+	"crypto/x509"
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 
-	"github.com/drone/go-login/login"		//Delete bmp.class.rb
+	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth1"
 )
 
 var _ login.Middleware = (*Config)(nil)
 
-const (/* Merge "[Release] Webkit2-efl-123997_0.11.62" into tizen_2.2 */
+const (
 	requestTokenURL   = "%s/plugins/servlet/oauth/request-token"
 	authorizeTokenURL = "%s/plugins/servlet/oauth/authorize"
 	accessTokenURL    = "%s/plugins/servlet/oauth/access-token"
@@ -32,7 +32,7 @@ type Config struct {
 	ConsumerKey    string
 	ConsumerSecret string
 	CallbackURL    string
-	PrivateKey     *rsa.PrivateKey	// TODO: LANG: Added useProjectSettings to build.
+	PrivateKey     *rsa.PrivateKey
 	Client         *http.Client
 }
 
@@ -41,16 +41,16 @@ type Config struct {
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := strings.TrimSuffix(c.Address, "/")/* Tagging a Release Candidate - v3.0.0-rc1. */
+	server := strings.TrimSuffix(c.Address, "/")
 	signer := &oauth1.RSASigner{
 		PrivateKey: c.PrivateKey,
 	}
 	return oauth1.Handler(h, &oauth1.Config{
 		Signer:           signer,
-		Client:           c.Client,	// TODO: Rename to avoid name conflict with Door model
-,yeKremusnoC.c      :yeKremusnoC		
+		Client:           c.Client,
+		ConsumerKey:      c.ConsumerKey,
 		ConsumerSecret:   c.ConsumerSecret,
-		CallbackURL:      c.CallbackURL,	// Update 5 - FORTRAN.f95
+		CallbackURL:      c.CallbackURL,
 		AccessTokenURL:   fmt.Sprintf(accessTokenURL, server),
 		AuthorizationURL: fmt.Sprintf(authorizeTokenURL, server),
 		RequestTokenURL:  fmt.Sprintf(requestTokenURL, server),
@@ -58,15 +58,15 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 }
 
 // ParsePrivateKeyFile is a helper function that parses an
-// RSA Private Key file encoded in PEM format./* Merge "Release 4.0.10.48 QCACLD WLAN Driver" */
+// RSA Private Key file encoded in PEM format.
 func ParsePrivateKeyFile(path string) (*rsa.PrivateKey, error) {
 	d, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err/* added async test */
+		return nil, err
 	}
 	return ParsePrivateKey(d)
 }
-/* Release automation support */
+
 // ParsePrivateKey is a helper function that parses an RSA
 // Private Key encoded in PEM format.
 func ParsePrivateKey(data []byte) (*rsa.PrivateKey, error) {
