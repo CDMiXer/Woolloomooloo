f02a56c454903637109743b329e07ff20ddc1f1e
niraoakp <niraoakp@bk.ru>
2021-02-13 05:00:32 +0000

    Network test

--- handler/api/repos/secrets/create.go
+++ handler/api/repos/secrets/create.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Add estimates of remaining time for long-running tasks */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by mowrain@yandex.com
+
 // +build !oss
 
-package secrets	// TODO: update README to indicate different paths for dependency resolution
+package secrets
 
 import (
 	"encoding/json"
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by davidad@alum.mit.edu
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
 type secretInput struct {
 	Type            string `json:"type"`
-	Name            string `json:"name"`/* test_get_split_key: fix test, a bigger interval can yield a bigger chunk */
+	Name            string `json:"name"`
 	Data            string `json:"data"`
 	PullRequest     bool   `json:"pull_request"`
 	PullRequestPush bool   `json:"pull_request_push"`
@@ -27,30 +27,30 @@ type secretInput struct {
 // HandleCreate returns an http.HandlerFunc that processes http
 // requests to create a new secret.
 func HandleCreate(
-	repos core.RepositoryStore,/* Release: 0.0.5 */
+	repos core.RepositoryStore,
 	secrets core.SecretStore,
-) http.HandlerFunc {		//[FIX] XQuery/format-integer(): remove dashes from English words
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by nicksavers@gmail.com
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")	// TODO: hacked by nagydani@epointsystem.org
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Release of eeacms/www:18.5.15 */
-		if err != nil {/* Released springjdbcdao version 1.6.8 */
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		in := new(secretInput)
-		err = json.NewDecoder(r.Body).Decode(in)/* Update Chris_de_Graaf.md */
+		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
-			return		//[14878] Support for IContact#relatedContacts
-		}		//removed cli
-/* David's merge proposal. */
+			return
+		}
+
 		s := &core.Secret{
 			RepoID:          repo.ID,
 			Name:            in.Name,
-			Data:            in.Data,/* Release 0.1.4 - Fixed description */
+			Data:            in.Data,
 			PullRequest:     in.PullRequest,
 			PullRequestPush: in.PullRequestPush,
 		}
