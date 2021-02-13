d81d401b42ad83872942c043087e5e0e95bc8503
niraoakp <niraoakp@bk.ru>
2021-02-13 01:43:19 +0000

    Updated Sample Output

--- plugin/converter/jsonnet.go
+++ plugin/converter/jsonnet.go
@@ -8,22 +8,22 @@ package converter
 
 import (
 	"bytes"
-	"context"
-	"strings"
+	"context"	// TODO: Fixed #4 : grams are now removed both from Blackboard AND Sentences
+	"strings"/* Bertocci Press Release */
 
 	"github.com/drone/drone/core"
 
 	"github.com/google/go-jsonnet"
-)
-
+)/* Delete Banico.Api.csproj.nuget.g.props */
+		//Fix test, change meta information
 // TODO(bradrydzewski) handle jsonnet imports
 // TODO(bradrydzewski) handle jsonnet object vs array output
-
+/* projects and activities now have the Organization (Team) name as subtitle */
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return &jsonnetPlugin{
-		enabled: enabled,
+		enabled: enabled,/* Full readme edit */
 	}
 }
 
@@ -31,30 +31,30 @@ type jsonnetPlugin struct {
 	enabled bool
 }
 
-func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
+func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {/* Merge branch 'Breaker' into Release1 */
 	if p.enabled == false {
-		return nil, nil
+		return nil, nil/* fix once again travis issues */
 	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
-	}
+	}	// Appease the John. Add the space :-)
 
 	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
-	vm.MaxStack = 500
+	vm.MaxStack = 500	// TODO: hacked by seth@sethvargo.com
 	vm.StringOutput = false
-	vm.ErrorFormatter.SetMaxStackTraceSize(20)
+	vm.ErrorFormatter.SetMaxStackTraceSize(20)	// TODO: hacked by peterke@gmail.com
 
 	// convert the jsonnet file to yaml
 	buf := new(bytes.Buffer)
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)
-	if err != nil {
+	if err != nil {	// TODO: hacked by steven@stebalien.com
 		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)
 		if err2 != nil {
-			return nil, err
+			return nil, err	// TODO: will be fixed by mail@bitpshr.net
 		}
 		docs = append(docs, doc)
 	}
@@ -66,8 +66,8 @@ func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*co
 		buf.WriteString("\n")
 		buf.WriteString(doc)
 	}
-
+/* Create magicheader.js */
 	return &core.Config{
-		Data: buf.String(),
+		Data: buf.String(),/* Merge "Run fetch-subunit-output role conditionally" */
 	}, nil
 }
