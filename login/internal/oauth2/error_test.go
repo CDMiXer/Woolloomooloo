1753a15cbd2f20ef52e0fd13bd490c8abb4f15a9
niraoakp <niraoakp@bk.ru>
2021-02-13 12:25:37 +0000

    Remove load of Portable Business Rules

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,13 +1,13 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Rename quotes.md to quotes-about-reactiveui.md */
+
 package oauth2
 
-import "testing"	// TODO: will be fixed by m-ou.se@m-ou.se
+import "testing"
 
 func TestError(t *testing.T) {
-	err := Error{}/* Fix missing write call in SubWorldMsg */
+	err := Error{}
 	err.Code = "invalid_request"
 	err.Desc = " The request is missing a required parameter"
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
