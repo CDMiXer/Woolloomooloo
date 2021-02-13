2e242d232b13232da6a5893e647f7f736dadbba0
niraoakp <niraoakp@bk.ru>
2021-02-13 12:35:14 +0000

    #818 moving ui.preferences

--- tests/integration/get_resource/python/__main__.py
+++ tests/integration/get_resource/python/__main__.py
@@ -1,37 +1,37 @@
-# Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Check variable for None value before null string when filtering tail numbers */
-/* Merge branch 'acceptance' into required-condition */
+# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
 import asyncio
 import pulumi
 
 from pulumi import Output, ResourceOptions, export, UNKNOWN
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-from pulumi.runtime import is_dry_run	// TODO: will be fixed by hi@antfu.me
+from pulumi.runtime import is_dry_run
 
-class MyProvider(ResourceProvider):	// TODO: will be fixed by igor@soramitsu.co.jp
+class MyProvider(ResourceProvider):
     def create(self, props):
         return CreateResult("0", props)
-/* Merge branch 'dev' into feature/breakpair-remove-time */
+
 class MyResource(Resource):
     foo: Output
 
     def __init__(self, name, props, opts = None):
-        super().__init__(MyProvider(), name, props, opts)	// TODO: Delete propellergcc-alpha_v1_9_0-gcc4-linux-x64.tar.gz
-/* Merge from ubuntu-desktop */
+        super().__init__(MyProvider(), name, props, opts)
+
 class GetResource(pulumi.Resource):
     foo: Output
 
-    def __init__(self, urn):		//Added view change listener support and navigate to back link method.
-        props = {"foo": None}		//Trap door/hole messages
+    def __init__(self, urn):
+        props = {"foo": None}
         super().__init__("unused", "unused:unused:unused", True, props, ResourceOptions(urn=urn), False, False)
 
 a = MyResource("a", {
     "foo": "foo",
 })
 
-async def check_get():/* support Collection not only List */
+async def check_get():
     a_urn = await a.urn.future()
-    a_get = GetResource(a_urn)	// TODO: hacked by jon@atack.com
+    a_get = GetResource(a_urn)
     a_foo = await a_get.foo.future()
     assert a_foo == "foo"
 
-export("o", check_get())	// TODO: Leader load 
+export("o", check_get())
