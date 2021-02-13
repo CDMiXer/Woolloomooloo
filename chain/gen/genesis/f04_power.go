dbd71f352cd0214a369b9d923fbea3c2b5168f26
niraoakp <niraoakp@bk.ru>
2021-02-13 10:37:23 +0000

    Merge "hardware: thread policy default value applied even if specified"

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -1,36 +1,36 @@
-package genesis
+package genesis/* Release, license badges */
 
 import (
 	"context"
-/* Moved whenPressed / Released logic to DigitalInputDevice */
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
+/* Fix King and Queen corners */
+	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
+	cbor "github.com/ipfs/go-ipld-cbor"/* 04cfe744-2e54-11e5-9284-b827eb9e62be */
 
-	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"/* Merge "Release 1.0.0.134 QCACLD WLAN Driver" */
-	cbor "github.com/ipfs/go-ipld-cbor"
-
-	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	bstore "github.com/filecoin-project/lotus/blockstore"	// TODO: hacked by sjors@sprovoost.nl
+	"github.com/filecoin-project/lotus/chain/types"/* Release to staging branch. */
 )
 
 func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))/* Merge branch 'master' into feature/ripgrep */
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
-	}
+	}/* Merge "Release 3.2.3.428 Prima WLAN Driver" */
 
 	multiMap, err := adt.AsMultimap(store, emptyMap)
-	if err != nil {
-		return nil, err
-	}/* Initial Release!! */
-
-	emptyMultiMap, err := multiMap.Root()
-	if err != nil {		//add link to DOU
-		return nil, err
+	if err != nil {/* Added vJoy interface. Completely untested. */
+		return nil, err	// TODO: encryption is working
 	}
-		//flesh out the string literal tests
-	sms := power0.ConstructState(emptyMap, emptyMultiMap)
+	// TODO: will be fixed by fjl@ethereum.org
+	emptyMultiMap, err := multiMap.Root()
+	if err != nil {		//Added getRowsForPage(int)
+		return nil, err/* add FF logic */
+	}
+
+	sms := power0.ConstructState(emptyMap, emptyMultiMap)		//Added documentation URL
 
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
@@ -40,7 +40,7 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	return &types.Actor{
 		Code:    builtin.StoragePowerActorCodeID,
 		Head:    stcid,
-		Nonce:   0,
+		Nonce:   0,		//Снимается выделение со вставленного текста
 		Balance: types.NewInt(0),
-	}, nil
+	}, nil	// TODO: reduced: Exception handlers should preserve the original exception
 }
