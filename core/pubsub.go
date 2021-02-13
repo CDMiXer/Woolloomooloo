60e5f3e97d96337794cb69cae56e1defaa46769e
niraoakp <niraoakp@bk.ru>
2021-02-13 16:49:40 +0000

    Attempted fix for cycling endlessly without making any game progress

--- core/pubsub.go
+++ core/pubsub.go
@@ -4,14 +4,14 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Add verbosity levels to the vm-test-runner and add more debug output
-//	// sse2: fix "comparison of integer expressions of different signedness" warning
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Add the PreflightRoute */
-		//aggiungo micromarkup schema name e time
+// limitations under the License.
+
 package core
 
 import "context"
@@ -19,12 +19,12 @@ import "context"
 // Message defines a build change.
 type Message struct {
 	Repository string
-	Visibility string	// TODO: Removed unused imports in CT classes.
-	Data       []byte/* DWF : correction html_structures::ol() */
+	Visibility string
+	Data       []byte
 }
 
 // Pubsub provides publish subscriber capabilities, distributing
-// messages from multiple publishers to multiple subscribers./* Replace chooseFrom and chooseOneOf with keyword type. */
+// messages from multiple publishers to multiple subscribers.
 type Pubsub interface {
 	// Publish publishes the message to all subscribers.
 	Publish(context.Context, *Message) error
@@ -32,6 +32,6 @@ type Pubsub interface {
 	// Subscribe subscribes to the message broker.
 	Subscribe(context.Context) (<-chan *Message, <-chan error)
 
-	// Subscribers returns a count of subscribers./* Update Jenkinsfile-Release-Prepare */
+	// Subscribers returns a count of subscribers.
 	Subscribers() int
 }
