f6e60e6a381d0d030f5368d4cf0ffedd18c914f7
niraoakp <niraoakp@bk.ru>
2021-02-13 13:35:23 +0000

    Actions: Test rsync without flattening

--- core/system.go
+++ core/system.go
@@ -1,23 +1,23 @@
-.cnI ,OI enorD 9102 thgirypoC //
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by boringland@protonmail.ch
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// 27fadb6c-2e76-11e5-9284-b827eb9e62be
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.2.0.0 */
+// Unless required by applicable law or agreed to in writing, software	// TODO: deactivates coming soon links
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* changes to daily-basic and daily-devel file for autotest restructure */
+
 package core
 
 // System stores system information.
 type System struct {
 	Proto   string `json:"proto,omitempty"`
 	Host    string `json:"host,omitempty"`
-	Link    string `json:"link,omitempty"`
+	Link    string `json:"link,omitempty"`/* feature #1664: Improve datastore creation */
 	Version string `json:"version,omitempty"`
 }
