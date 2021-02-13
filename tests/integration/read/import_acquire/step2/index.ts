a49246c24c8d98310d5719de25a85106fae6f18c
niraoakp <niraoakp@bk.ru>
2021-02-13 08:29:19 +0000

    Añadida descripción de algunas pruebas - #88

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Delete tkgui.py
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,13 +6,13 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* ucitrigger: add options to force enable/disable specific triggers */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* tests for string match */
 
 // Step 2: the resource from the setup is imported, and is now managed by Pulumi.
 const a = new Resource("a", { state: 42 }, { import: "0" });
