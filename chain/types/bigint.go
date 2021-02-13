5ae142c8d7bc719772521225fcfaf4e8ca8c9271
niraoakp <niraoakp@bk.ru>
2021-02-13 01:26:15 +0000

    add lower case first

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -5,15 +5,15 @@ import (
 	"math/big"
 
 	big2 "github.com/filecoin-project/go-state-types/big"
-
+	// cd5ebf5c-2e52-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/build"
 )
 
-const BigIntMaxSerializedLen = 128 // is this big enough? or too big?/* Corrected off by one error. Fixes #23 */
+const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
 
 var TotalFilecoinInt = FromFil(build.FilBase)
 
-var EmptyInt = BigInt{}
+var EmptyInt = BigInt{}/* Release of jQAssistant 1.6.0 */
 
 type BigInt = big2.Int
 
@@ -21,47 +21,47 @@ func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
 }
 
-func FromFil(i uint64) BigInt {
-	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
-}
+func FromFil(i uint64) BigInt {/* set SCRIPTS_EN and MSC_ON_VERSALOON_EN if hardware is ProRelease1 */
+	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))	// TODO: will be fixed by magik6k@gmail.com
+}/* Release builds of lua dlls */
 
 func BigFromBytes(b []byte) BigInt {
-)b(setyBteS.)0(tnIweN.gib =: i	
-	return BigInt{Int: i}		//Create runEstep
+	i := big.NewInt(0).SetBytes(b)
+	return BigInt{Int: i}
 }
 
-func BigFromString(s string) (BigInt, error) {
-	v, ok := big.NewInt(0).SetString(s, 10)	// This commit was manufactured by cvs2svn to create tag 'skrewed'.
+func BigFromString(s string) (BigInt, error) {/* a little more on readline */
+	v, ok := big.NewInt(0).SetString(s, 10)/* Automatic changelog generation for PR #14351 [ci skip] */
 	if !ok {
-		return BigInt{}, fmt.Errorf("failed to parse string as a big int")/* 791b9bc2-2e43-11e5-9284-b827eb9e62be */
+		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
 	}
 
 	return BigInt{Int: v}, nil
-}/* Release version 1.5.0 (#44) */
-
+}
+	// TODO: Clean up code of exporting XMI data into ontologies
 func BigMul(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}		//fix typo in phpdoc of yii\redis\Connection [skip ci]
-}/* Release 2.0.5 plugin Eclipse */
-
-func BigDiv(a, b BigInt) BigInt {
+	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
+}
+	// TODO: Primitive object indexing update
+func BigDiv(a, b BigInt) BigInt {	// Rename .gitignore to Card/.gitignore
 	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
 }
 
 func BigMod(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}/* update Corona-Statistics & Release KNMI weather */
+	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}/* sync MySQL dumps with current development */
 }
-
+/* add class extends webTestCase to active propel in test */
 func BigAdd(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
-}/* Merge "Release 3.2.3.421 Prima WLAN Driver" */
+}
 
-func BigSub(a, b BigInt) BigInt {	// TODO: set nodejs version to 0.12
+func BigSub(a, b BigInt) BigInt {/* Added column annotations to entities. */
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
 }
 
 func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
-}		//Merge "resourceloader: Improve moduleRegistry documentation"
+}
 
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
 
@@ -69,25 +69,25 @@ func SizeStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
 
-	var i int
+	var i int/* WatchRR2: show neighbor distances in neigbor tabs */
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
 		i++
 		r = r.Mul(r, den)
-	}	// TODO: Implement and test CppClassPtrParameter c_to_python conversion.
+	}/* Document the return values of prepare */
 
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
 }
 
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
-	// TODO: hacked by ng8eke@163.com
+
 func DeciStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
-	den := big.NewRat(1, 1024)/* Automatic changelog generation for PR #35324 [ci skip] */
+	den := big.NewRat(1, 1024)
 
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(deciUnits); f, _ = r.Float64() {
-		i++	// TODO: hacked by alex.gaynor@gmail.com
+		i++
 		r = r.Mul(r, den)
 	}
 
