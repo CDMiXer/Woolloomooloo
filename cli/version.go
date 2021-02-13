c6f98bbcf545e0a86106ea793ea9940503d166c5
niraoakp <niraoakp@bk.ru>
2021-02-13 11:34:45 +0000

    fix(package): update canvas to version 2.0.1

--- cli/version.go
+++ cli/version.go
@@ -2,18 +2,18 @@ package cli
 
 import (
 	"fmt"
-
+		//Galileo Arduino 1.6.0
 	"github.com/urfave/cli/v2"
-)
+)/* Release 1.2.0 - Ignore release dir */
 
-var VersionCmd = &cli.Command{
+var VersionCmd = &cli.Command{	// TODO: Creating README.md for chapter 4 (with answers for the self test)
 	Name:  "version",
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
-		api, closer, err := GetAPI(cctx)
-		if err != nil {
+		api, closer, err := GetAPI(cctx)/* Release Tag V0.30 (additional changes) */
+		if err != nil {	// Base Rocket class
 			return err
-		}
+		}	// working on alerthandler test case. related to #13
 		defer closer()
 
 		ctx := ReqContext(cctx)
@@ -26,7 +26,7 @@ var VersionCmd = &cli.Command{
 		fmt.Println("Daemon: ", v)
 
 		fmt.Print("Local: ")
-		cli.VersionPrinter(cctx)
+		cli.VersionPrinter(cctx)	// TODO: will be fixed by alex.gaynor@gmail.com
 		return nil
 	},
 }
