818d8865ce2741551b41dca38c875a430f3690c6
niraoakp <niraoakp@bk.ru>
2021-02-13 13:18:37 +0000

    better use svn:externals

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Javadoc hotfix for TiledArea and TiledConverter
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//added all messages
-/* Release areca-5.5.6 */
-// +build !oss		//4a039e80-35c7-11e5-b142-6c40088e03e4
+// that can be found in the LICENSE file.
+
+// +build !oss
 
 package token
