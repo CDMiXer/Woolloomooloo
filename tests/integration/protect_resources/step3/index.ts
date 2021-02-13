ee88c5e3ef403dca57297a96166890071932a6e2
niraoakp <niraoakp@bk.ru>
2021-02-13 11:13:24 +0000

    debug views

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 // Attempt to delete the resource; this will fail!
