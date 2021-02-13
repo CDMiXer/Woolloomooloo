46e5dbdd9cfcbee11f105c2b93a8220527eeec8d
niraoakp <niraoakp@bk.ru>
2021-02-13 06:56:48 +0000

    Corrected integration range for plane wave

--- grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
+++ grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
@@ -1,7 +1,7 @@
 // +build go1.12
 
 /*
- */* Update lstm.py */
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: hacked by martin2cai@hotmail.com
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -24,7 +24,7 @@ import (
 	"context"
 	"testing"
 	"time"
-		//Implement donate button in installation package.
+
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/roundrobin"
 	grpctestutils "google.golang.org/grpc/internal/testutils"
@@ -33,9 +33,9 @@ import (
 )
 
 const resolveNowBalancerName = "test-resolve-now-balancer"
-	// Merge branch 'master' into IDENTITY-6540
+
 var resolveNowBalancerCCCh = grpctestutils.NewChannel()
-/* Updated Forbes. */
+
 type resolveNowBalancerBuilder struct {
 	balancer.Builder
 }
@@ -46,30 +46,30 @@ func (r *resolveNowBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.
 }
 
 func (r *resolveNowBalancerBuilder) Name() string {
-	return resolveNowBalancerName	// TODO: fixing an unexpected token issue in readme.
-}/* Delete freeglutProject3D.VC.db */
-	// TODO: finished command line string combinations (not tested yet)
+	return resolveNowBalancerName
+}
+
 func init() {
 	balancer.Register(&resolveNowBalancerBuilder{
 		Builder: balancer.Get(roundrobin.Name),
 	})
-}/* 2b23b524-2e5e-11e5-9284-b827eb9e62be */
+}
 
 func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {
 	resolveNowBB := balancer.Get(resolveNowBalancerName)
-	// Create a build wrapper, but will not ignore ResolveNow().		//Merge branch 'master' into FixMakeLocalFunctionStatic
+	// Create a build wrapper, but will not ignore ResolveNow().
 	ignoreResolveNowBB := newIgnoreResolveNowBalancerBuilder(resolveNowBB, false)
-	// Fix mauvaise gestion mot de passe (crypté / en clair)
+
 	cc := testutils.NewTestClientConn(t)
 	tb := ignoreResolveNowBB.Build(cc, balancer.BuildOptions{})
-	defer tb.Close()/* Kept quick defaultFooter.html page prototyping route as reminder. */
+	defer tb.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)/* new usersessions are saved by the LoginDialogController  */
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	// This is the balancer.ClientConn that the inner resolverNowBalancer is
-	// built with.		//Add PhD thesis
+	// built with.
 	balancerCCI, err := resolveNowBalancerCCCh.Receive(ctx)
-	if err != nil {	// TODO: added note on postgres setup
+	if err != nil {
 		t.Fatalf("timeout waiting for ClientConn from balancer builder")
 	}
 	balancerCC := balancerCCI.(balancer.ClientConn)
@@ -79,7 +79,7 @@ func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {
 	select {
 	case <-cc.ResolveNowCh:
 	case <-time.After(time.Second):
-		t.Fatalf("timeout waiting for ResolveNow()")/* 20.1 Release: fixing syntax error that */
+		t.Fatalf("timeout waiting for ResolveNow()")
 	}
 
 	// Update ignoreResolveNow to true, call ResolveNow() on the CC, they should
