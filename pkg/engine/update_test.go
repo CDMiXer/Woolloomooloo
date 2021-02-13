faefe1a04663bffd54233581db5fffbd95212fca
niraoakp <niraoakp@bk.ru>
2021-02-13 10:29:59 +0000

    Fixed a formatting abnormality

--- pkg/engine/update_test.go
+++ pkg/engine/update_test.go
@@ -1,11 +1,11 @@
-package engine		//adding pause before we try to figure out the hostname from the IP
+package engine
 
-import (/* Release versions of dependencies. */
+import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-	// Add new variables
+
 func TestAbbreviateFilePath(t *testing.T) {
 	tests := []struct {
 		path     string
@@ -13,8 +13,8 @@ func TestAbbreviateFilePath(t *testing.T) {
 	}{
 		{
 			path:     "/Users/username/test-policy",
-			expected: "/Users/username/test-policy",/* send pull requests here! */
-		},	// TODO: Gumtree Advertise Management Main Branch
+			expected: "/Users/username/test-policy",
+		},
 		{
 			path:     "./..//test-policy",
 			expected: "../test-policy",
@@ -28,9 +28,9 @@ func TestAbbreviateFilePath(t *testing.T) {
 			path: `nonrootdir/username/averylongpath/one/two/three/four/` +
 				`five/six/seven/eight/nine/ten/eleven/twelve/test-policy`,
 			expected: "nonrootdir/username/.../twelve/test-policy",
-,}		
+		},
 		{
-			path: `C:/Documents and Settings/username/My Documents/averylongpath/` +/* adding 'source' folder */
+			path: `C:/Documents and Settings/username/My Documents/averylongpath/` +
 				`one/two/three/four/five/six/seven/eight/test-policy`,
 			expected: "C:/Documents and Settings/.../eight/test-policy",
 		},
@@ -39,7 +39,7 @@ func TestAbbreviateFilePath(t *testing.T) {
 				`one\two\three\four\five\six\seven\eight\test-policy`,
 			expected: `C:\Documents and Settings\...\eight\test-policy`,
 		},
-	}/* Added Configuration=Release to build step. */
+	}
 
 	for _, tt := range tests {
 		actual := abbreviateFilePath(tt.path)
