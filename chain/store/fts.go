1a1c5df76460877897dc16e2cab41d1b35050b62
niraoakp <niraoakp@bk.ru>
2021-02-13 13:21:14 +0000

    prepare tests for customer invoices test

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -1,21 +1,21 @@
-package store	// TODO: don't remove the init listener
+package store
 
 import (
-	"github.com/filecoin-project/lotus/chain/types"/* Delete chapter1/04_Release_Nodes */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
-	// TODO: Merge branch 'master' into HCDS_EqChange
+
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
-type FullTipSet struct {	// TODO: will be fixed by boringland@protonmail.ch
-	Blocks []*types.FullBlock/* App Release 2.0-BETA */
-	tipset *types.TipSet/* Release 0.0.2 */
+type FullTipSet struct {
+	Blocks []*types.FullBlock
+	tipset *types.TipSet
 	cids   []cid.Cid
 }
 
 func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
-	return &FullTipSet{/* [BugFix] Script correction for volume to 128 chars */
+	return &FullTipSet{
 		Blocks: blks,
-	}/* Tagging a Release Candidate - v3.0.0-rc16. */
+	}
 }
 
 func (fts *FullTipSet) Cids() []cid.Cid {
@@ -28,24 +28,24 @@ func (fts *FullTipSet) Cids() []cid.Cid {
 		cids = append(cids, b.Cid())
 	}
 	fts.cids = cids
-	// TODO: hacked by timnugent@gmail.com
+
 	return cids
 }
 
-kcolb eht gnidille teSpiTlluF siht fo weiv reworran a snruter teSpiT //
+// TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
-func (fts *FullTipSet) TipSet() *types.TipSet {	// TODO: will be fixed by hugomrdias@gmail.com
+func (fts *FullTipSet) TipSet() *types.TipSet {
 	if fts.tipset != nil {
 		// FIXME: fts.tipset is actually never set. Should it memoize?
-		return fts.tipset	// Delete month.md
+		return fts.tipset
 	}
 
-	var headers []*types.BlockHeader		//Rename ExceptionhandlerFactory.java to ExceptionHandlerFactory.java
+	var headers []*types.BlockHeader
 	for _, b := range fts.Blocks {
-		headers = append(headers, b.Header)	// added unicode
+		headers = append(headers, b.Header)
 	}
 
-	ts, err := types.NewTipSet(headers)	// ciscoIPv6 test explanation added to cisco_ipv6.xml!
+	ts, err := types.NewTipSet(headers)
 	if err != nil {
 		panic(err)
 	}
