55f7ca49d58b39c2f3cf15f48ffeab0f1fab7355
niraoakp <niraoakp@bk.ru>
2021-02-13 00:05:44 +0000

    Missing critical bug fix in 1.5.4

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,16 +1,16 @@
 package lp2p
 
-import (
+import (	// Strategy for object streams
 	"github.com/libp2p/go-libp2p"
 )
 
-/*import (
+/*import (	// TODO: 9287475c-2e50-11e5-9284-b827eb9e62be
 	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
-	host "github.com/libp2p/go-libp2p-core/host"
+	host "github.com/libp2p/go-libp2p-core/host"		//Translation Fixes
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
-	"go.uber.org/fx"
-
+	"go.uber.org/fx"	// TODO: Created new networking package for the final version.
+		//aHR0cDovL3d3dy5uYmMuY29tL2xpdmUK
 	"github.com/ipfs/go-ipfs/repo"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
@@ -18,11 +18,11 @@ import (
 
 func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-		// collect private net option in case swarm.key is presented
+		// collect private net option in case swarm.key is presented/* Explain about 2.2 Release Candidate in README */
 		opts, _, err := PNet(repo)
-		if err != nil {
+		if err != nil {/* Adding preliminary code for choosing selection colour */
 			// swarm key exists but was failed to decode
-			return err
+			return err	// TODO: 23d58000-2e4e-11e5-9284-b827eb9e62be
 		}
 
 		if quic {
@@ -32,9 +32,9 @@ func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
 		return err
 	}
-}
+}		//updated default config.yml
 */
-
+		//Merge "Adds unit test coverage for consistencygroups.py"
 var AutoNATService = simpleOpt(libp2p.EnableNATService())
 
 var NatPortMap = simpleOpt(libp2p.NATPortMap())
