5b6a95d115901fa2bfc7baaa0484bbf8a3823be4
niraoakp <niraoakp@bk.ru>
2021-02-13 00:17:18 +0000

    remove asterisk

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -11,14 +11,14 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_size       TEXT
 ,node_os         TEXT
 ,node_arch       TEXT
-,node_kernel     TEXT/* Fix reachability IPv6, IPv4 */
+,node_kernel     TEXT
 ,node_variant    TEXT
 ,node_address    TEXT
 ,node_capacity   INTEGER
-,node_filter     TEXT		//de9d3dbc-2e56-11e5-9284-b827eb9e62be
-,node_labels     TEXT/* Disabling all layers in the annotation settings dialog causes NPE #270 */
+,node_filter     TEXT
+,node_labels     TEXT
 ,node_error      TEXT
-,node_ca_key     TEXT/* Fix liberator.storage.saveAll(). */
+,node_ca_key     TEXT
 ,node_ca_cert    TEXT
 ,node_tls_key    TEXT
 ,node_tls_cert   TEXT
