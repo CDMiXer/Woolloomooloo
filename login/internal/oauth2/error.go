33f007167892623cc420c1bfabe6bdf94d7d4cc3
niraoakp <niraoakp@bk.ru>
2021-02-13 08:19:47 +0000

    * main: remove tz_file_path global variable;

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -1,22 +1,22 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by sbrichards@gmail.com
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2
+package oauth2/* merge trunk, these are old changes, conflict with sandbox-tests folder */
 
-import "errors"
-
+import "errors"/* Class or function definition yields its own value */
+		//Create dict.md
 // ErrState indicates the state is invalid.
-var ErrState = errors.New("Invalid state")
+var ErrState = errors.New("Invalid state")	// Update paper about WOLV's data service
 
 // Error represents a failed authorization request.
 type Error struct {
 	Code string `json:"error"`
 	Desc string `json:"error_description"`
-}
+}/* Update version number file to V3.0.W.PreRelease */
 
 // Error returns the string representation of an
 // authorization error.
 func (e *Error) Error() string {
 	return e.Code + ": " + e.Desc
-}
+}/* Add Release Note for 1.0.5. */
