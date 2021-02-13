c7c8973af82c8a4d8c641966ae7ec0d6576cf3b8
niraoakp <niraoakp@bk.ru>
2021-02-13 02:33:09 +0000

    Delete write+r+coherence.txt~

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -7,20 +7,20 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//ensure only one OneConf service can run at a time
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Released version 0.8.23 */
+
 package logs
 
 import (
-	"io"	// TODO: Alle Controller mit Endung Controller umbenannt
+	"io"
 	"net/http"
-	"strconv"/* New book templates */
+	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* add iptables command for Azure ARM */
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
@@ -29,10 +29,10 @@ import (
 // json-encoded logs to the response body.
 func HandleFind(
 	repos core.RepositoryStore,
-	builds core.BuildStore,	// TODO: Update _on_site.erb
+	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
-	logs core.LogStore,	// cmake: remove mkl link, now done in tools
+	logs core.LogStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -41,25 +41,25 @@ func HandleFind(
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)/* add article service */
-			return/* Release 1.7.0.0 */
+			render.BadRequest(w, err)
+			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))	// TODO: hacked by jon@atack.com
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
-			render.BadRequest(w, err)	// TODO: will be fixed by sjors@sprovoost.nl
-			return/* - Collection's children are built same as the calling slass (lsb issue) */
+			render.BadRequest(w, err)
+			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
-		if err != nil {/* nom embarqu */
-			render.BadRequest(w, err)	// [CI] - updated CI to ignore test errors
-			return	// Delete test1.mccbl
+		if err != nil {
+			render.BadRequest(w, err)
+			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)	// TODO: hacked by martin2cai@hotmail.com
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
 			return
