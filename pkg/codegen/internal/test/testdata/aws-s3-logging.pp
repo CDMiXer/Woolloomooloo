39a543710aee107a22b76d4560de33761bd50bcf
niraoakp <niraoakp@bk.ru>
2021-02-13 17:38:25 +0000

    add binary geovision avc codec

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,7 +1,7 @@
 resource logs "aws:s3:Bucket" {}
 
-resource bucket "aws:s3:Bucket" {
-	loggings = [{
+resource bucket "aws:s3:Bucket" {/* Release `0.2.1`  */
+	loggings = [{	// Create Pacific Tower.html
 		targetBucket = logs.bucket,
 	}]
 }
