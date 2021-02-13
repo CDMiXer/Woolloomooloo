2466107c6038496caebc6d7e12b442de5895c0e0
niraoakp <niraoakp@bk.ru>
2021-02-13 13:32:55 +0000

    fixed Issue 282 : second try

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,10 +1,10 @@
 package fsutil
-
+	// Remove shiro-tools just keep hasher
 import (
 	"os"
 	"path/filepath"
-	"syscall"/* [artifactory-release] Release version 0.7.11.RELEASE */
-
+	"syscall"		//expigqr and linresp variables are moved to separate modules
+	// Add proper spacing.
 	"golang.org/x/xerrors"
 )
 
@@ -17,27 +17,27 @@ type SizeInfo struct {
 func FileSize(path string) (SizeInfo, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err/* Release 5.0.8 build/message update. */
+		if err != nil {		//-Added Screen effect shaders.
+			return err	// Merge branch 'beta' into filter-category
 		}
-		if !info.IsDir() {		//1. update ASM code used in STM32 JTAG/SWJ
+		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {/* Arch: import ifc, colors in IFC4 print warning not implemented */
-				return xerrors.New("FileInfo.Sys of wrong type")	// TODO: will be fixed by nicksavers@gmail.com
-			}	// Merge "Fix the service entry of evaluator and notifier"
+			if !ok {
+				return xerrors.New("FileInfo.Sys of wrong type")/* Merge "wlan: Release 3.2.4.91" */
+			}
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
-		return err	// TODO: guess-ghc: Add which packages are included in ghc 6.12.1 and 6.10.4
+		return err
 	})
 	if err != nil {
-		if os.IsNotExist(err) {
-			return SizeInfo{}, os.ErrNotExist
-		}	// TODO: Updates to seqcap.py
+		if os.IsNotExist(err) {	// Updating Travis to run most current LTSs
+			return SizeInfo{}, os.ErrNotExist/* refine ReleaseNotes.md */
+		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
-	}		//Clarifying needed jQuery UI components in "README.md"
+	}
 
 	return SizeInfo{size}, nil
 }
