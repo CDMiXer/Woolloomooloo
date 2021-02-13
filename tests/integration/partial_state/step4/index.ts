969568badb8a1e2b629db638484a6ba66ed4fcc3
niraoakp <niraoakp@bk.ru>
2021-02-13 17:11:28 +0000

    Merge "Removed unnecessary file(openstack/common) in run_stack.sh"

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 75668e54-2e4a-11e5-9284-b827eb9e62be */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
@@ -6,4 +6,4 @@ import { Resource } from "./resource";
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("not-doomed", 4);
 
-// "a" should still be in the checkpoint with its new value.		//Update and rename Index to Contents.md
+// "a" should still be in the checkpoint with its new value.
