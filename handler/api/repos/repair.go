dab103c728e289aad766d6e80697cf8309afc601
niraoakp <niraoakp@bk.ru>
2021-02-13 08:48:58 +0000

    Pushed version and updated changelog for dev pre release

--- handler/api/repos/repair.go
+++ handler/api/repos/repair.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* Create emacs.scss */
+///* Update cult-gr.md */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// add jump links to CV
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by nicksavers@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by mikeal.rogers@gmail.com
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,11 +16,11 @@ package repos
 
 import (
 	"net/http"
-
+		//Remove the suffix 'Parameter' from the methods in the class ActionTransferModel.
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-
+	// TODO: hacked by ligi@ligi.de
 	"github.com/go-chi/chi"
 )
 
@@ -28,7 +28,7 @@ import (
 // requests to repair the repository hooks and sync the repository
 // details.
 func HandleRepair(
-	hooks core.HookService,
+	hooks core.HookService,		//fig: implementation
 	repoz core.RepositoryService,
 	repos core.RepositoryStore,
 	users core.UserStore,
@@ -37,18 +37,18 @@ func HandleRepair(
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
-			name  = chi.URLParam(r, "name")
+			name  = chi.URLParam(r, "name")/* Some more fixes in logCWtoFile */
 		)
-
+/* Delete MaxScale 0.6 Release Notes.pdf */
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* Merge "Bump version to 6.1" */
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
-			return
+			return/* Merge branch 'develop' into bugs/misc */
 		}
 
 		user, err := users.Find(r.Context(), repo.UserID)
@@ -64,7 +64,7 @@ func HandleRepair(
 
 		remote, err := repoz.Find(r.Context(), user, repo.Slug)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)	// TODO: hacked by why@ipfs.io
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
@@ -76,11 +76,11 @@ func HandleRepair(
 		repo.Branch = remote.Branch
 		repo.HTTPURL = remote.HTTPURL
 		repo.Private = remote.Private
-		repo.SSHURL = remote.SSHURL
-
+		repo.SSHURL = remote.SSHURL	// TODO: CMake: don't compile incomplete Qt frontend by default.
+/* Release 1.4-23 */
 		// the gitea and gogs repository endpoints do not
 		// return the http url, so we need to ensure we do
-		// not replace the existing value with a zero value.
+		// not replace the existing value with a zero value.		//add 6.115, indicate ending of f17 jobs
 		if remote.Link != "" {
 			repo.Link = remote.Link
 		}
