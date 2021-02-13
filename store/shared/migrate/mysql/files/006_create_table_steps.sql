32a3b5a5e09e5d447579439f3eded41702329003
niraoakp <niraoakp@bk.ru>
2021-02-13 10:27:32 +0000

    Merge branch 'master' of https://github.com/rathilavkesh/BlogRepository.git

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps	// Fix failure to unset midi record dumping when recording is shut off.
+-- name: create-table-steps	// TODO: change to using the ID based Org GH API
 
 CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,step_stage_id    INTEGER/* Merge branch 'master' into wv */
+ step_id          INTEGER PRIMARY KEY AUTO_INCREMENT		//Readme: Destructured import
+,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)		//Switch to importing ValidationError from django.core.exceptions.
+,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER	// TODO: GracefulExpress: patch res.writeHead, doc updates
+,step_started     INTEGER
 ,step_stopped     INTEGER
-,step_version     INTEGER	// select existing tag of class during #selectClass:
-,UNIQUE(step_stage_id, step_number)
+,step_version     INTEGER
+,UNIQUE(step_stage_id, step_number)/* Merge "Remove an unmatched rightparen" */
 );
 
--- name: create-index-steps-stage		//Implement JWT storage to handle autologin (within the JWT timelife)
+-- name: create-index-steps-stage
 
-CREATE INDEX ix_steps_stage ON steps (step_stage_id);/* Merge "Support fat-flow at VN level" */
+CREATE INDEX ix_steps_stage ON steps (step_stage_id);
