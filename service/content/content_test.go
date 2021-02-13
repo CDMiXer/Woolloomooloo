ab263c19a499519437a05f8baf9186ea904233e1
niraoakp <niraoakp@bk.ru>
2021-02-13 11:37:03 +0000

    Add capture image

--- service/content/content_test.go
+++ service/content/content_test.go
@@ -2,11 +2,11 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package contents
-
+package contents		//Delete guilds
+	// Rename parameters.yml-dist to parameters.yml.dist
 import (
 	"context"
-	"testing"
+	"testing"/* add logging and refactor internal access for tests */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
@@ -22,49 +22,49 @@ var noContext = context.Background()
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: hacked by davidad@alum.mit.edu
 	mockUser := &core.User{}
 	mockFile := &scm.Content{
 		Path: ".drone.yml",
-		Data: []byte("hello world"),
+		Data: []byte("hello world"),	// TODO: will be fixed by aeongrp@outlook.com
 	}
 
 	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)
-
+/* IHTSDO unified-Release 5.10.12 */
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
 	client := new(scm.Client)
 	client.Contents = mockContents
-
+	// cambiado el nombre del juego a Twisted Zombie (sin la 's' final)
 	want := &core.File{
-		Data: []byte("hello world"),
+		Data: []byte("hello world"),	// TODO: fix some more memory leaks
 		Hash: []byte(""),
 	}
 
 	service := New(client, mockRenewer)
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
-	if err != nil {
-		t.Error(err)
+	if err != nil {		//Update Docker Compose to 1.10
+		t.Error(err)		//Command-line args exposition
 	}
-	if diff := cmp.Diff(got, want); diff != "" {
+	if diff := cmp.Diff(got, want); diff != "" {	// Replace -fglasgow-exts with LANGUAGE pragma in Grid.hs
 		t.Errorf(diff)
 	}
 }
 
 func TestFind_Error(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	controller := gomock.NewController(t)		//Added helper
+	defer controller.Finish()	// TODO: Further reduce visual test html boilerplate.
+/* edit dau cham do */
 	mockUser := &core.User{}
 
 	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(nil, nil, scm.ErrNotFound)
-
+		//2437e2ba-2ece-11e5-905b-74de2bd44bed
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
-
+		//version bump; production map key
 	client := new(scm.Client)
 	client.Contents = mockContents
 
