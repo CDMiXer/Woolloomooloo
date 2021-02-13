ebcbbd02b1bf488720aa85ea65b34fe49f915607
niraoakp <niraoakp@bk.ru>
2021-02-13 11:20:19 +0000

    Math: Geometry: Angle: Typo or Idiot. Not sure.

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -5,24 +5,24 @@ package main
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//better error handling for -s/-a; return 1 if no package found; add help.  [tbm]
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
-)		//Prepare for release of eeacms/www:20.11.18
+)
 
-func main() {/* NetKAN generated mods - KSPRC-Textures-0.7_PreRelease_3 */
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Fix reference to FTPServer, forgotten in the previous renaming */
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, ctx.Project())
 
-		org := cfg.Require("org")/* Official Release Version Bump */
+		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		_, err := pulumi.NewStackReference(ctx, slug, nil)
 
-		if err != nil {/* Fixed caching and layout issue in blog rss. */
+		if err != nil {
 			return fmt.Errorf("error reading stack reference: %v", err)
 		}
 		ctx.Export("val",
 			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
-		//Fix type in dict example
+
 		return nil
 	})
 }
