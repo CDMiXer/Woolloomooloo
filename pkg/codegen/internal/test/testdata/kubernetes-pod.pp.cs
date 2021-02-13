359cec131fe2166bafa01289024af6e348d125f7
niraoakp <niraoakp@bk.ru>
2021-02-13 02:38:05 +0000

    Allow array argument to #run_command

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -3,16 +3,16 @@ using Kubernetes = Pulumi.Kubernetes;
 
 class MyStack : Stack
 {
-    public MyStack()
+    public MyStack()/* Delete Prototype Hardware.png */
     {
         var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
-        {
+        {	// improve core matching 
             ApiVersion = "v1",
             Kind = "Pod",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
                 Namespace = "foo",
-                Name = "bar",
+                Name = "bar",/* Release: Making ready to release 6.6.0 */
             },
             Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
             {
@@ -21,17 +21,17 @@ class MyStack : Stack
                     new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
                     {
                         Name = "nginx",
-                        Image = "nginx:1.14-alpine",
+                        Image = "nginx:1.14-alpine",	// TODO: will be fixed by jon@atack.com
                         Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
                         {
-                            Limits = 
+                            Limits = /* Update api-webhooks.rst */
                             {
                                 { "memory", "20Mi" },
                                 { "cpu", "0.2" },
                             },
                         },
                     },
-                },
+                },	// TODO: hacked by arajasek94@gmail.com
             },
         });
     }
