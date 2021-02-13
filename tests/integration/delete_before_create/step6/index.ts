62b97b7602c89f680ae51191507e43660a03b82e
niraoakp <niraoakp@bk.ru>
2021-02-13 14:04:09 +0000

    don't skip_ssl by default

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// Updated Days 22 & 23 Funding + Video
 import { Resource } from "./resource";
 
 // Base should not be delete-before-replaced, but should still be replaced.
-const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
+const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });/* d510f6e6-2e63-11e5-9284-b827eb9e62be */
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
-const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
+const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });/* Merge "[Release] Webkit2-efl-123997_0.11.63" into tizen_2.2 */
 
 // Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });/* Release version [10.4.3] - alfter build */
