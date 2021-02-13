bddcb7008ccc60ad8f49d9a7f88d5de984079249
niraoakp <niraoakp@bk.ru>
2021-02-13 16:36:59 +0000

    Rearrenge projects in Examples.sln.

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,11 +1,11 @@
 // +build !appengine,!go1.14
 
-/*
+/*		//redis lock
  *
  * Copyright 2020 gRPC authors.
- *
+ */* c51b7de4-2e52-11e5-9284-b827eb9e62be */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Create whack.py */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Reformat partially, where I touched for whitespace changes. */
 
 package advancedtls
 
@@ -24,19 +24,19 @@ import (
 	"crypto/tls"
 	"fmt"
 )
-
-// buildGetCertificates returns the first cert contained in ServerOptions for
-// non-appengine builds before version 1.4.
+/* Removed unused "uses" from PagecontentAction */
+// buildGetCertificates returns the first cert contained in ServerOptions for	// TODO: hacked by alex.gaynor@gmail.com
+// non-appengine builds before version 1.4./* Create Angry Professor */
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {		//Add link to the code used by FamilySearch style guide
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
-	if err != nil {
+	if err != nil {	// TODO: Update AlertifyJS
 		return nil, err
 	}
 	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
-	}
+	}/* Release 2.0.0: Upgrading to ECM3 */
 	return certificates[0], nil
 }
