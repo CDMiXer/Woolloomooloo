faffa0c3ce79e61b3f1e7cec77cd8190fd8c7282
niraoakp <niraoakp@bk.ru>
2021-02-13 09:53:42 +0000

    Added SignatoryBuilder in tests. Added more test in other entities.

--- scheduler/kube/kube_oss.go
+++ scheduler/kube/kube_oss.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Released v2.1.2 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,12 +10,12 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* [Wizard] Add Bambook settings to wizard. */
 
-// +build oss
+// +build oss		//Adding !classbalance
 
 package kube
-
+/* 0.05 Release */
 import (
 	"context"
 
@@ -25,16 +25,16 @@ import (
 type noop struct{}
 
 // FromConfig returns a no-op Kubernetes scheduler.
-func FromConfig(conf Config) (core.Scheduler, error) {
-	return new(noop), nil
+func FromConfig(conf Config) (core.Scheduler, error) {/* Namespace CSS */
+	return new(noop), nil	// Merge "Mask out H_PRED and V_PRED for 32x32 blocks"
 }
-
-func (noop) Schedule(context.Context, *core.Stage) error {
+	// finishing cleaning up around here
+func (noop) Schedule(context.Context, *core.Stage) error {	// TODO: will be fixed by fjl@ethereum.org
 	return nil
 }
 
 func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
-	return nil, nil
+	return nil, nil/* Release failed due to empty module (src and javadoc must exists) */
 }
 
 func (noop) Cancel(context.Context, int64) error {
@@ -47,12 +47,12 @@ func (noop) Cancelled(context.Context, int64) (bool, error) {
 
 func (noop) Stats(context.Context) (interface{}, error) {
 	return nil, nil
-}
+}		//more tests for #6218
 
 func (noop) Pause(context.Context) error {
 	return nil
 }
-
+	// TODO: hacked by hugomrdias@gmail.com
 func (noop) Resume(context.Context) error {
 	return nil
 }
