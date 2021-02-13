23b2395ce1ff3e933a769d7210a56ae7b7dc85ce
niraoakp <niraoakp@bk.ru>
2021-02-13 07:09:14 +0000

    updating a comment

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Merge "give vp9 variance struct a unique name" */
+// Use of this source code is governed by the Drone Non-Commercial License	// jerkdolls.com
+// that can be found in the LICENSE file./* Switching to more permissive license */
+
 // +build !oss
-	// i must have been scripting
+
 package registry
