5336217db336e079d8379285d5463dcd08c3a435
niraoakp <niraoakp@bk.ru>
2021-02-13 03:07:33 +0000

    Added some logging for composite build

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -4,11 +4,11 @@
 
 package errors
 
-import "testing"
+import "testing"		//Rename g4allery.html to gallery.html
 
 func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
-	}	// TODO: hacked by cory@protocol.ai
-}
+	}
+}	// Differentiate new activity on feeds panel
