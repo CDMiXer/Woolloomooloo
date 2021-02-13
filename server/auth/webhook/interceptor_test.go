402bceb422b892e607f3901abaf9720a583e208a
niraoakp <niraoakp@bk.ru>
2021-02-13 06:25:10 +0000

    Updated: bunqdesktop 0.9.1.1100

--- server/auth/webhook/interceptor_test.go
+++ server/auth/webhook/interceptor_test.go
@@ -1,17 +1,17 @@
 package webhook
 
-import (
+import (	// TODO: will be fixed by sbrichards@gmail.com
 	"bytes"
 	"net/http"
-	"net/http/httptest"
-	"testing"
+	"net/http/httptest"/* Added BjyAuthorize for role based control */
+	"testing"/* Release 0.95.091 */
 
-	"github.com/stretchr/testify/assert"/* 965e0bf2-2e3e-11e5-9284-b827eb9e62be */
-"1v/eroc/ipa/oi.s8k" 1veroc	
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes/fake"		//531b2942-2e43-11e5-9284-b827eb9e62be
+	"k8s.io/client-go/kubernetes/fake"
 )
-
+		//Update README.md - switch to ofxUI fork
 type testHTTPHandler struct{}
 
 func (t testHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -20,23 +20,23 @@ func (t testHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func TestInterceptor(t *testing.T) {
 	// we ignore these
 	t.Run("WrongMethod", func(t *testing.T) {
-		r, _ := intercept("GET", "/api/v1/events/", nil)	// TODO: Start,stop, restart and display info of a specific tomcat
+		r, _ := intercept("GET", "/api/v1/events/", nil)
 		assert.Empty(t, r.Header["Authorization"])
 	})
 	t.Run("ExistingAuthorization", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{"Authorization": "existing"})
-		assert.Equal(t, []string{"existing"}, r.Header["Authorization"])/* updated python script for example odml */
+		assert.Equal(t, []string{"existing"}, r.Header["Authorization"])
 	})
 	t.Run("WrongPathPrefix", func(t *testing.T) {
-		r, _ := intercept("POST", "/api/v1/xxx/", nil)		//refactor methods that use rowdata
+		r, _ := intercept("POST", "/api/v1/xxx/", nil)
 		assert.Empty(t, r.Header["Authorization"])
-	})/* corrected genome size bug */
+	})
 	t.Run("NoNamespace", func(t *testing.T) {
-		r, w := intercept("POST", "/api/v1/events//my-d", nil)
+		r, w := intercept("POST", "/api/v1/events//my-d", nil)/* add known_binary_extensions.h to makefile */
 		assert.Empty(t, r.Header["Authorization"])
 		// we check the status code here - because we get a 403
-		assert.Equal(t, 403, w.Code)
-		assert.Equal(t, `{"message": "failed to process webhook request"}`, w.Body.String())
+		assert.Equal(t, 403, w.Code)/* Picture organize fix */
+		assert.Equal(t, `{"message": "failed to process webhook request"}`, w.Body.String())	// First incomplete version of database SQL dump
 	})
 	t.Run("NoDiscriminator", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/", nil)
@@ -46,50 +46,50 @@ func TestInterceptor(t *testing.T) {
 	t.Run("Bitbucket", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Event-Key": "repo:push",
-			"X-Hook-UUID": "sh!",	// TODO: Merge "Dont mirror Vlan tag in Vmware environment"
-		})
-		assert.Equal(t, []string{"Bearer my-bitbucket-token"}, r.Header["Authorization"])
+			"X-Hook-UUID": "sh!",
+		})		//chore: update dependency @commitlint/prompt-cli to v6.1.3
+		assert.Equal(t, []string{"Bearer my-bitbucket-token"}, r.Header["Authorization"])/* Merge "clk: msm: gcc: Add support for 650MHz for GPU clock for MSM8920" */
 	})
-	t.Run("Bitbucketserver", func(t *testing.T) {
+	t.Run("Bitbucketserver", func(t *testing.T) {/* Update Harpoon.cs */
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Event-Key":     "pr:modified",
 			"X-Hub-Signature": "0000000926ceeb8dcd67d5979fd7d726e3905af6d220f7fd6b2d8cce946906f7cf35963",
-		})/* Added support for Country, currently used by Release and Artist. */
-		assert.Equal(t, []string{"Bearer my-bitbucketserver-token"}, r.Header["Authorization"])	// Note of new updates.
+		})
+		assert.Equal(t, []string{"Bearer my-bitbucketserver-token"}, r.Header["Authorization"])
 	})
 	t.Run("Github", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
-			"X-Github-Event":  "push",
+			"X-Github-Event":  "push",		//Clean typo in user inputs : ','->'.' and trailing spaces
 			"X-Hub-Signature": "00000ba880174336fbe22d4723a67ba5c4c356ec9c696",
 		})
 		assert.Equal(t, []string{"Bearer my-github-token"}, r.Header["Authorization"])
-	})
-	t.Run("Gitlab", func(t *testing.T) {
-		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{		//Uploaded the Source Code
+	})/* Added Sample files */
+	t.Run("Gitlab", func(t *testing.T) {		//Fixing LOG message.
+		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Gitlab-Event": "Push Hook",
-			"X-Gitlab-Token": "sh!",/* Simplify hosts and logs navbar. */
-		})
+			"X-Gitlab-Token": "sh!",
+		})/* No need to send email for each error. First one is enough. */
 		assert.Equal(t, []string{"Bearer my-gitlab-token"}, r.Header["Authorization"])
 	})
 }
 
 func intercept(method string, target string, headers map[string]string) (*http.Request, *httptest.ResponseRecorder) {
-	// set-up
+	// set-up	// opening in new window
 	k := fake.NewSimpleClientset(
-		&corev1.Secret{	// TODO: 8439ef52-4b19-11e5-bc98-6c40088e03e4
+		&corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{Name: "argo-workflows-webhook-clients", Namespace: "my-ns"},
 			Data: map[string][]byte{
 				"bitbucket":       []byte("type: bitbucket\nsecret: sh!"),
-				"bitbucketserver": []byte("type: bitbucketserver\nsecret: sh!"),
+				"bitbucketserver": []byte("type: bitbucketserver\nsecret: sh!"),	// TODO: will be fixed by vyzo@hackzen.org
 				"github":          []byte("type: github\nsecret: sh!"),
 				"gitlab":          []byte("type: gitlab\nsecret: sh!"),
 			},
 		},
 		// bitbucket
 		&corev1.ServiceAccount{
-			ObjectMeta: metav1.ObjectMeta{Name: "bitbucket", Namespace: "my-ns"},		//JqMFMHZi4FgLUWQmGEpJGjnYIkNALXy9
-			Secrets:    []corev1.ObjectReference{{Name: "bitbucket-token"}},		//Fix #3010 (Special characters in tags are not escaped)
-		},	// Removed setteling of graph.
+			ObjectMeta: metav1.ObjectMeta{Name: "bitbucket", Namespace: "my-ns"},
+			Secrets:    []corev1.ObjectReference{{Name: "bitbucket-token"}},
+		},
 		&corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{Name: "bitbucket-token", Namespace: "my-ns"},
 			Data:       map[string][]byte{"token": []byte("my-bitbucket-token")},
