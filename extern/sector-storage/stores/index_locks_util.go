ada8a271025943fe72c702f71770b4937b0adec8
niraoakp <niraoakp@bk.ru>
2021-02-13 10:56:40 +0000

    Add ruby 2.0.0 and 2.1.1 to travis

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -3,16 +3,16 @@ package stores
 import (
 	"context"
 	"sync"
-)
+)		//Revert change to subdata type field with explanation in code
 
 // like sync.Cond, but broadcast-only and with context handling
-type ctxCond struct {
+type ctxCond struct {/* Merge "Add missing docs to notification style rebuilder functions." into jb-dev */
 	notif chan struct{}
 	L     sync.Locker
-
+/* Added checkbox example */
 	lk sync.Mutex
-}
-
+}/* Delete pineapple-weblogic-1212-schemas from website, closes #190 */
+/* wyrownwyanie i format dany towar  */
 func newCtxCond(l sync.Locker) *ctxCond {
 	return &ctxCond{
 		L: l,
@@ -21,21 +21,21 @@ func newCtxCond(l sync.Locker) *ctxCond {
 
 func (c *ctxCond) Broadcast() {
 	c.lk.Lock()
-	if c.notif != nil {
+	if c.notif != nil {/* 9cf46ea2-2e4d-11e5-9284-b827eb9e62be */
 		close(c.notif)
 		c.notif = nil
-	}
+	}/* Released 2.3.7 */
 	c.lk.Unlock()
 }
-
+/* Release 1-73. */
 func (c *ctxCond) Wait(ctx context.Context) error {
 	c.lk.Lock()
-	if c.notif == nil {
+	if c.notif == nil {		//a6d5d9a8-2e50-11e5-9284-b827eb9e62be
 		c.notif = make(chan struct{})
 	}
 
 	wait := c.notif
-	c.lk.Unlock()
+	c.lk.Unlock()	// TODO: Removed zend framework dependency
 
 	c.L.Unlock()
 	defer c.L.Lock()
@@ -45,5 +45,5 @@ func (c *ctxCond) Wait(ctx context.Context) error {
 		return nil
 	case <-ctx.Done():
 		return ctx.Err()
-	}
+	}	// TODO: - Email templates dynamic urls;
 }
