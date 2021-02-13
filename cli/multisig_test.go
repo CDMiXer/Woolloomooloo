1515b877909dee9d950121da855b08edb045a4fd
niraoakp <niraoakp@bk.ru>
2021-02-13 17:26:19 +0000

    Fixed issue of Collections/Answers AllLinks for Zhihu new version

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,20 +1,20 @@
 package cli
 
 import (
-	"context"/* moved real-time upload option down to bottom of storage related settings */
-	"os"/* 1c3fb610-2e50-11e5-9284-b827eb9e62be */
+	"context"
+	"os"
 	"testing"
 	"time"
 
-	clitest "github.com/filecoin-project/lotus/cli/test"		//upper case in Entered By
+	clitest "github.com/filecoin-project/lotus/cli/test"
 )
 
 // TestMultisig does a basic test to exercise the multisig CLI
-// commands
+// commands	// TODO: hacked by ac0dem0nk3y@gmail.com
 func TestMultisig(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-/* Add version tests */
+
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
