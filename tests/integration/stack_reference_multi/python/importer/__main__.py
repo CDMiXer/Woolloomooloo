d54a91f9c6682c99cdb4f1a1d2ae40ba3013ccf0
niraoakp <niraoakp@bk.ru>
2021-02-13 05:29:32 +0000

    Delete nations1.jpg

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -2,9 +2,9 @@ import pulumi
 
 config = pulumi.Config()
 
-exporterStackName = config.require('exporter_stack_name')		//move working dir/dirstate methods from localrepo to workingctx
-org = config.require('org')		//изменение Readme файла
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')	// 8d2cc686-2e3e-11e5-9284-b827eb9e62be
+exporterStackName = config.require('exporter_stack_name')	// TODO: Update M003.md
+org = config.require('org')
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')	// DynamoDB added as a supported database
 
 pulumi.export('val1', a.require_output('val'))
-pulumi.export('val2', pulumi.Output.secret(['d', 'x']))/* default script name should be "main.ts" */
+pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
