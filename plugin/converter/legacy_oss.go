c10ef5da50b6feb390aafeddf7e86783e08553b6
niraoakp <niraoakp@bk.ru>
2021-02-13 15:09:40 +0000

    Implemented basic instances launch

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by juan@benet.ai
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,16 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by mail@bitpshr.net
+
 // +build oss
 
 package converter
-		//cof g and strag outsite class
+
 import (
 	"github.com/drone/drone/core"
 )
-	// Added automatical confirmation to conda downloads
-// Legacy returns a conversion service that converts the/* Delete lab8.c */
+
+// Legacy returns a conversion service that converts the
 // legacy 0.8 file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return new(noop)
