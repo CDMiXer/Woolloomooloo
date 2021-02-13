94709fd54eb7a6d835945a7f3ad1685fb9948190
niraoakp <niraoakp@bk.ru>
2021-02-13 11:22:49 +0000

    cleanup: removed unused files

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Generate butterflies properly
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Rename Delivery to DeliveryProfile. */
 
-// +build !oss
-
+// +build !oss/* phpdoc for query.php. Props jacobsantos. fixes #7663 */
+	// TODO: Remove handler for the lock button
 package crons
 
 import (
-	"context"		//Update README.md for OS X building instructions
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: will be fixed by ng8eke@163.com
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"		//f3b56768-2e49-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"	// Solve maze
-	"github.com/golang/mock/gomock"
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"		//Fixed bug that prevented update.php to happen correctly.
 	"github.com/google/go-cmp/cmp"
-)		//Allow different configurations for each sensor.
-
-func TestHandleFind(t *testing.T) {/* implemented inherits */
+)/* Released URB v0.1.5 */
+/* Release 2.0 final. */
+func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -32,46 +32,46 @@ func TestHandleFind(t *testing.T) {/* implemented inherits */
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 
-	c := new(chi.Context)/* update build: rmv old listview sample, new listview project */
+	c := new(chi.Context)	// TODO: hacked by why@ipfs.io
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")		//2b30ba62-2e54-11e5-9284-b827eb9e62be
+	c.URLParams.Add("cron", "nightly")
+/* Merge "Update versions after September 18th Release" into androidx-master-dev */
+	w := httptest.NewRecorder()		//README.md: Add personal website sjackman.ca
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//rename YAML file, DRY
+	)
+
+	HandleFind(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)		//Fixed date fields format & validation.
+	}/* removed deployment of war artifacts */
+
+	got, want := &core.Cron{}, dummyCron
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleFind_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+/* Threshold and boost saved again */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
+	// adicionei a classe secondary-header
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
-	HandleFind(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {		//Remove single bullet
-		t.Errorf("Want response code %d, got %d", want, got)		//change midnight handling
-	}
-
-	got, want := &core.Cron{}, dummyCron/* Release 0.5.11 */
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
-	}
-}
-		//Remove Loyalty and PowerToughness compareTo primitives.
-func TestHandleFind_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release Q5 */
-
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
-
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")		//More group implementation.
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* Add NDK Home to path */
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// Create documentation/GlanceInvisbleToAdmin.md
-	)	// TODO: hacked by steven@stebalien.com
 
 	HandleFind(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
