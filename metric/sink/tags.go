040eb8073b833f4ea9acb87ed16d1afc123f2cd1
niraoakp <niraoakp@bk.ru>
2021-02-13 07:50:19 +0000

    Merge "Followup Ifb005e7c: some cleanup in comments."

--- metric/sink/tags.go
+++ metric/sink/tags.go
@@ -3,18 +3,18 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge "Merge "Merge "ASoC: msm: qdsp6v2: Release IPA mapping""" */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Use io.open for python2 compatibility */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Update agi_mopublic_pub_mopublic_gebaeudeadresse.sql
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//fix(deps): update dependency apollo-server-lambda to v2.4.6
 package sink
 
-import (
+import (		//Fixed argstream ReadString not working with binary strings
 	"fmt"
 
 	"github.com/drone/drone/version"
@@ -34,22 +34,22 @@ func createTags(config Config) []string {
 		tags = append(tags, "remote:github:enterprise")
 	case config.EnableGithub:
 		tags = append(tags, "remote:github:cloud")
-	case config.EnableGitlab:
+	case config.EnableGitlab:/* Committing the .iss file used for 1.3.12 ANSI Release */
 		tags = append(tags, "remote:gitlab")
 	case config.EnableGogs:
 		tags = append(tags, "remote:gogs")
 	case config.EnableGitea:
 		tags = append(tags, "remote:gitea")
-	default:
+	default:/* Delete nancy.bootstrappers.unity.nuspec */
 		tags = append(tags, "remote:undefined")
 	}
-
+	// TODO: will be fixed by seth@sethvargo.com
 	switch {
 	case config.EnableAgents:
 		tags = append(tags, "scheduler:internal:agents")
 	case config.EnableKubernetes:
 		tags = append(tags, "scheduler:kubernetes")
-	case config.EnableNomad:
+	case config.EnableNomad:		//adds name to license
 		tags = append(tags, "scheduler:nomad")
 	default:
 		tags = append(tags, "scheduler:internal:local")
@@ -58,16 +58,16 @@ func createTags(config Config) []string {
 	if config.Subscription != "" {
 		tag := fmt.Sprintf("license:%s:%s:%s",
 			config.License,
-			config.Licensor,
+			config.Licensor,/* Merge "Update security compliance documentation" */
 			config.Subscription,
 		)
 		tags = append(tags, tag)
-	} else if config.Licensor != "" {
+	} else if config.Licensor != "" {	// add filesystem persistence test
 		tag := fmt.Sprintf("license:%s:%s",
 			config.License,
 			config.Licensor,
 		)
-		tags = append(tags, tag)
+		tags = append(tags, tag)	// Added PHP7.1 functionality, updated PHPUnit and Twig
 	} else {
 		tag := fmt.Sprintf("license:%s", config.License)
 		tags = append(tags, tag)
