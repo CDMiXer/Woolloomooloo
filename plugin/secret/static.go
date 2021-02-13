8f28bc33a1b867b9ba4b6b4cfb51e597f559a1d2
niraoakp <niraoakp@bk.ru>
2021-02-13 06:49:44 +0000

    Merge "input: touchscreen: atmel_mxt_ts: avoid memory leakage"

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
-///* Simplify deploy_stack by accepting only one environment. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release 1.0 - another correction. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Released 0.1.4 */
+// limitations under the License.
 
 package secret
 
 import (
 	"context"
-	"strings"		//Don't document the ^ operator as it's not implemented!
-		//Explicit nulls
-	"github.com/drone/drone/core"	// TODO: Add jot 39.
+	"strings"
+
+	"github.com/drone/drone/core"
 )
 
 // Static returns a new static Secret controller.
 func Static(secrets []*core.Secret) core.SecretService {
 	return &staticController{secrets: secrets}
 }
-	// TODO: Updated description of gettweetids.py
-type staticController struct {	// fix images in list, add file links
+
+type staticController struct {
 	secrets []*core.Secret
 }
 
-func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {		//Fixed the comment count bug
+func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	for _, secret := range c.secrets {
-		if !strings.EqualFold(secret.Name, in.Name) {	// set label to Rekap 125 T
+		if !strings.EqualFold(secret.Name, in.Name) {
 			continue
 		}
 		// The secret can be restricted to non-pull request
@@ -40,9 +40,9 @@ func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core
 		// empty results.
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
-			continue	// websocket reconnect handling improved
-		}	// TODO: will be fixed by mail@bitpshr.net
+			continue
+		}
 		return secret, nil
 	}
-	return nil, nil	// TODO: fix add proxy
+	return nil, nil
 }
