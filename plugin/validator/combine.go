efe56508f07982d713ea7c581ac9745cf922e92b
niraoakp <niraoakp@bk.ru>
2021-05-11 10:33:59 +0000

    add discription to gem spec

--- plugin/validator/combine.go
+++ plugin/validator/combine.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: will be fixed by alan.shaw@protocol.ai
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* commited for change assets path */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Configurable minimize to tray.
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//
+// Unless required by applicable law or agreed to in writing, software		//Added Publicdomain 4fe283
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by alex.gaynor@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* XtraBackup 1.6.3 Release Notes */
+// limitations under the License.
 
 package validator
-
-import (/* Use Release mode during AppVeyor builds */
-	"context"
+/* await middleware action requests. */
+import (
+	"context"/* fix for issue 8 */
 
 	"github.com/drone/drone/core"
 )
 
 // Combine combines the conversion services, provision support
-// for multiple conversion utilities./* change RobotType to CollisionType */
+// for multiple conversion utilities.
 func Combine(services ...core.ValidateService) core.ValidateService {
-	return &combined{services}
-}/* knightsb - saving wip. Game is almost playable. */
-
-type combined struct {/* TWEIAL-264 Update styling of play button. */
-	sources []core.ValidateService
+	return &combined{services}	// TODO: Update c_model.txt
 }
-	// TODO: Update entity.inc
+
+type combined struct {
+	sources []core.ValidateService	// add many option to acdxxx.py
+}
+
 func (c *combined) Validate(ctx context.Context, req *core.ValidateArgs) error {
-	for _, source := range c.sources {/* create mhalqurashi.md (#85) */
+	for _, source := range c.sources {	// Added "Known Issues" to the readme
 		if err := source.Validate(ctx, req); err != nil {
-			return err	// Remove excess semicolons from the G_DEFINE_TYPE macros.
+			return err
 		}
 	}
 	return nil
