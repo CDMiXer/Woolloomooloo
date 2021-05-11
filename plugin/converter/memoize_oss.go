35ca09c6f20292885ceaf944484c84eadaf15875
niraoakp <niraoakp@bk.ru>
2021-05-11 09:32:39 +0000

    Merge "Added config option for selecting lte over 4g"

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge "Release floating IPs on server deletion" */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,19 +11,19 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//6dd0f41a-2e4d-11e5-9284-b827eb9e62be
+
 // +build oss
 
 package converter
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by steven@stebalien.com
 )
 
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
 // pipeline execution.
-func Memoize(base core.ConvertService) core.ConvertService {/* Best README update. */
+func Memoize(base core.ConvertService) core.ConvertService {	// Update the desert pyramid loot table
 	return new(noop)
 }
