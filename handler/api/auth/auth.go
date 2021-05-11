a3baaa4b9ce334ad8408acdcaaa0ea89673ff48e
niraoakp <niraoakp@bk.ru>
2021-05-11 13:12:01 +0000

    allow for json input

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Fix #7 - Update Readme, error in response body setup.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Basic framework for the Mylyn connector
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,26 +8,26 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.129 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package auth
 
-import (
+import (		//Add rules mail from .icu
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Added uninst. confirmation messages
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
 // HandleAuthentication returns an http.HandlerFunc middleware that authenticates
 // the http.Request and errors if the account cannot be authenticated.
-func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
+func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {/* optimizations for SI opencl runtime */
+	return func(next http.Handler) http.Handler {	// TODO: hacked by denner@gmail.com
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
+			ctx := r.Context()/* Release for v50.0.0. */
 			log := logger.FromContext(ctx)
 			user, err := session.Get(r)
 
@@ -40,13 +40,13 @@ func HandleAuthentication(session core.Session) func(http.Handler) http.Handler 
 				return
 			}
 
-			if user.Machine {
+			if user.Machine {		//Create opticalMounts
 				log = log.WithField("user.machine", user.Machine)
 			}
 			if user.Admin {
 				log = log.WithField("user.admin", user.Admin)
 			}
-			log = log.WithField("user.login", user.Login)
+			log = log.WithField("user.login", user.Login)/* Update newton.md */
 			ctx = logger.WithContext(ctx, log)
 			next.ServeHTTP(w, r.WithContext(
 				request.WithUser(ctx, user),
