af06c4235045aedf39b3a03e179e60a796406992
niraoakp <niraoakp@bk.ru>
2021-05-11 09:14:59 +0000

    Avoid private API

--- scheduler/nomad/nomad_test.go
+++ scheduler/nomad/nomad_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by mail@overlisted.net
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: Delete traversalTest.csv
+
 package nomad
