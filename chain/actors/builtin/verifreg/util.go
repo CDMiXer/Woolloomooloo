b189b22f83dfe64ddc03bb54c69990369c68e597
niraoakp <niraoakp@bk.ru>
2021-05-11 12:29:12 +0000

    e02f4862-2e49-11e5-9284-b827eb9e62be

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,48 +1,48 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"		//Delete Kaggle - 2nd Place - Liu.pdf
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
-)/* Initial Release beta1 (development) */
-/* Implemented cache hash and several validations for angular app */
-// taking this as a function instead of asking the caller to call it helps reduce some of the error	// TODO: hacked by sebastian.tharakan97@gmail.com
-// checking boilerplate.	// Update filtering_SNPs_by_sample_coverage.R
+)
+
+// taking this as a function instead of asking the caller to call it helps reduce some of the error
+// checking boilerplate.
 //
 // "go made me do it"
 type rootFunc func() (adt.Map, error)
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
-	if addr.Protocol() != address.ID {/* Release 0.95.211 */
-		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")		//Merge branch 'master' into feat/homepage-chart-add-deaths
+	if addr.Protocol() != address.ID {
+		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
 	}
 	vh, err := root()
-	if err != nil {/* Added note about lack of unit tests. */
-		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)	// fixed Errors * )
+	if err != nil {
+		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
-		//Update EC-Cache.md
+
 	var dcap abi.StoragePower
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
-	} else if !found {		//aa449314-2e54-11e5-9284-b827eb9e62be
+	} else if !found {
 		return false, big.Zero(), nil
 	}
-/* Fixed a mistake in the comments */
+
 	return true, dcap, nil
 }
-	// TODO: hacked by earlephilhower@yahoo.com
+
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
 	if err != nil {
-		return xerrors.Errorf("loading verified clients: %w", err)/* Update Release_v1.0.ino */
+		return xerrors.Errorf("loading verified clients: %w", err)
 	}
-	var dcap abi.StoragePower	// TODO: 6ce207d4-2fa5-11e5-9804-00012e3d3f12
-	return vh.ForEach(&dcap, func(key string) error {/* Merge "msm: kgsl: Check for GPMU feature before requesting firmware" */
+	var dcap abi.StoragePower
+	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
