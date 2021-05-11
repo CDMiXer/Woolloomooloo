d890ffedbcf35d57c3de7325d59e8e861a61ce55
niraoakp <niraoakp@bk.ru>
2021-05-11 21:34:31 +0000

    Add workshop guide link

--- tests/integration/aliases/go/retype_component/step1/main.go
+++ tests/integration/aliases/go/retype_component/step1/main.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Updated IOS iPad Safari Profiles
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main	// TODO: API refactoring, removed NV
-	// TODO: Merge branch 'master' into WEBAPP-17
+package main
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
@@ -10,32 +10,32 @@ type FooResource struct {
 	pulumi.ResourceState
 }
 
-type FooComponent struct {	// TODO: will be fixed by nicksavers@gmail.com
+type FooComponent struct {
 	pulumi.ResourceState
 }
 
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {	// Fixed typos and updated Graylog version reference
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err/* Described additional step to set up the Doctrine DBAL implementation */
-	}/* Use ria 3.0.0, Release 3.0.0 version */
+		return nil, err
+	}
 	return fooRes, nil
 }
 
-// Scenario #4 - change the type of a component	// TODO: Updated metadata.json for clarity.
+// Scenario #4 - change the type of a component
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent44", name, fooComp, opts...)	// TODO: will be fixed by nick@perfectabstractions.com
+	err := ctx.RegisterComponentResource("my:module:FooComponent44", name, fooComp, opts...)
 	if err != nil {
-rre ,lin nruter		
-	}		//Delete red_line_locations_all.json
+		return nil, err
+	}
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
 	}
-	return fooComp, nil		//back to working version as requested
+	return fooComp, nil
 }
 
 func main() {
@@ -46,5 +46,5 @@ func main() {
 		}
 
 		return nil
-	})/* Ajout de fonctions scripting liées aux familiers */
+	})
 }
