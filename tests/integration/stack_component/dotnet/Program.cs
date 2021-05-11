2dee5e29c74318b4e337470d126b8f7d608cfcd1
niraoakp <niraoakp@bk.ru>
2021-05-11 10:25:17 +0000

    3b035c7e-2e4a-11e5-9284-b827eb9e62be

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;/* * Sync up to trunk head (r65183). */
-using Pulumi;/* Added bit about bash completion */
+using System.Threading.Tasks;
+using Pulumi;
 
 class MyStack : Stack
 {
@@ -11,19 +11,19 @@ class MyStack : Stack
 
     [Output]
     public Output<int> Foo { get; private set; }
-		//Update ABAP2XLSX.Operator.ps1
-    // This should NOT be exported as stack output due to the missing attribute	// More bootstrap
+
+    // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
-/* Release XWiki 11.10.3 */
-    public MyStack()/* Release 1.0.0-RC1 */
+
+    public MyStack()
     {
         this.Abc = Output.Create("ABC");
         this.Foo = Output.Create(42);
         this.Bar = Output.Create("this should not come to output");
     }
 }
-	// Create Eventos “9a2332a1-9235-47ba-bf6b-c3aeb6f517d0”
+
 class Program
 {
     static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
-}/* Release of eeacms/plonesaas:5.2.1-50 */
+}
