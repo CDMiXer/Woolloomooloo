f1112cd4369b0df125a550d569806de79d3e5650
niraoakp <niraoakp@bk.ru>
2021-05-11 21:25:25 +0000

    Housecleaning.

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -1,20 +1,20 @@
 package test
-/* Added a template for the ReleaseDrafter bot. */
+
 import (
 	"github.com/blang/semver"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
-func NewHost(schemaDirectoryPath string) plugin.Host {		//Scan controller
-	return deploytest.NewPluginHost(nil, nil, nil,		//Create CmdXColor.cs
+func NewHost(schemaDirectoryPath string) plugin.Host {
+	return deploytest.NewPluginHost(nil, nil, nil,
 		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return AWS(schemaDirectoryPath)
-		}),/* Merge branch 'develop' into td-fix-missiontrip-help */
-		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {	// TODO: Merge branch 'master' into fmtlibcubeprop
-			return Azure(schemaDirectoryPath)
 		}),
-		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
+		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {/* enable some tests. */
+			return Azure(schemaDirectoryPath)/* Updated xml enable/disable a layer. */
+		}),	// TODO: Remove outdated versionchanged entry
+		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {	// Use this to see what scapy does
 			return Random(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
