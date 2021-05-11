919528b258e1b8bc084abae8ccb2e250a2e29dd0
niraoakp <niraoakp@bk.ru>
2021-05-11 15:15:50 +0000

    filter past incomplete actions

--- pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
@@ -4,9 +4,9 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as utilities from "../../utilities";
 
-// Export members:
+// Export members:	// TODO: will be fixed by vyzo@hackzen.org
 export * from "./rubberTree";
-
+	// Push ecma to variabel and variabel to Call
 // Export enums:
 export * from "../../types/enums/tree/v1";
 
@@ -17,11 +17,11 @@ const _module = {
     version: utilities.getVersion(),
     construct: (name: string, type: string, urn: string): pulumi.Resource => {
         switch (type) {
-            case "plant-provider:tree/v1:RubberTree":
+            case "plant-provider:tree/v1:RubberTree":	// Move dockercfg and services first before pulling docker containers (#56)
                 return new RubberTree(name, <any>undefined, { urn })
             default:
                 throw new Error(`unknown resource type ${type}`);
         }
     },
-};
+};/* Release 0.92 bug fixes */
 pulumi.runtime.registerResourceModule("plant-provider", "tree/v1", _module)
