cc57c138962b4afe42db26f9ad42f7c887a5543d
niraoakp <niraoakp@bk.ru>
2021-05-11 10:55:27 +0000

    Update README w/ test url; wiki link; disable taco npm link

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Adapter for JUnit test cases
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// :palm_tree::rage: Updated in browser at strd6.github.io/editor
 import * as pulumi from "@pulumi/pulumi";
-	// fix a bug when write content to file in vsprog
+
 let config = new pulumi.Config();
-let org = config.require("org");
-let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
+let org = config.require("org");	// TODO: Fix spurious error in memory.limit (PR#13673), plus some cleanup of the docs
+let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;	// Typo fixed. Thank you @misabear
 let a = new pulumi.StackReference(slug);
 
 export const val = ["a", "b"];
