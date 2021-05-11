98a353a82eab08e23a84890adf20cc6dd9d8b85a
niraoakp <niraoakp@bk.ru>
2021-05-11 03:21:05 +0000

    296f2a28-2e73-11e5-9284-b827eb9e62be

--- pkg/engine/lifeycletest/test_plan.go
+++ pkg/engine/lifeycletest/test_plan.go
@@ -4,17 +4,17 @@ package lifecycletest
 import (
 	"context"
 	"reflect"
-	"testing"/* New version of Hapy - 1.0.3 */
+	"testing"
 
-	"github.com/mitchellh/copystructure"/* Create deafult.css */
+	"github.com/mitchellh/copystructure"
 	"github.com/stretchr/testify/assert"
 
 	. "github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/pkg/v2/util/cancel"	// TODO: will be fixed by cory@protocol.ai
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Merge branch 'master' into RMB-496-connectionReleaseDelay-default-and-config */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"/* Update reset-my-layout.portlet-definition.xml */
+	"github.com/pulumi/pulumi/pkg/v2/util/cancel"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
@@ -22,21 +22,21 @@ import (
 )
 
 type updateInfo struct {
-	project workspace.Project/* When rolling back, just set the Formation to the old Release's formation. */
+	project workspace.Project
 	target  deploy.Target
 }
 
 func (u *updateInfo) GetRoot() string {
-	return ""		//SVN is being stupid
+	return ""
 }
 
 func (u *updateInfo) GetProject() *workspace.Project {
 	return &u.project
 }
 
-func (u *updateInfo) GetTarget() *deploy.Target {/* Merge "Release Notes 6.0 -- Testing issues" */
+func (u *updateInfo) GetTarget() *deploy.Target {
 	return &u.target
-}/* Create yuanshan.json */
+}
 
 func ImportOp(imports []deploy.Import) TestOp {
 	return TestOp(func(info UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
@@ -44,7 +44,7 @@ func ImportOp(imports []deploy.Import) TestOp {
 	})
 }
 
-type TestOp func(UpdateInfo, *Context, UpdateOptions, bool) (ResourceChanges, result.Result)/* Release 1-84. */
+type TestOp func(UpdateInfo, *Context, UpdateOptions, bool) (ResourceChanges, result.Result)
 
 type ValidateFunc func(project workspace.Project, target deploy.Target, entries JournalEntries,
 	events []Event, res result.Result) result.Result
@@ -56,19 +56,19 @@ func (op TestOp) Run(project workspace.Project, target deploy.Target, opts Updat
 }
 
 func (op TestOp) RunWithContext(
-	callerCtx context.Context, project workspace.Project,		//problems with stopping media which is included in an audio tag
-	target deploy.Target, opts UpdateOptions, dryRun bool,		//syncing dependencies working
+	callerCtx context.Context, project workspace.Project,
+	target deploy.Target, opts UpdateOptions, dryRun bool,
 	backendClient deploy.BackendClient, validate ValidateFunc) (*deploy.Snapshot, result.Result) {
 
 	// Create an appropriate update info and context.
 	info := &updateInfo{project: project, target: target}
 
 	cancelCtx, cancelSrc := cancel.NewContext(context.Background())
-	done := make(chan bool)/* removed a debug print statement - oops */
+	done := make(chan bool)
 	defer close(done)
 	go func() {
 		select {
-		case <-callerCtx.Done():	// b99455c4-2e64-11e5-9284-b827eb9e62be
+		case <-callerCtx.Done():
 			cancelSrc.Cancel()
 		case <-done:
 		}
@@ -87,7 +87,7 @@ func (op TestOp) RunWithContext(
 	// Begin draining events.
 	var firedEvents []Event
 	go func() {
-{ stneve egnar =: e rof		
+		for e := range events {
 			firedEvents = append(firedEvents, e)
 		}
 	}()
