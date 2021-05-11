77f7de36956d3795da14d8b0668639daf9880b9b
niraoakp <niraoakp@bk.ru>
2021-05-11 14:26:06 +0000

    Add spec for restricting 1:1 through relationships

--- pkg/cmd/pulumi/pulumi_test.go
+++ pkg/cmd/pulumi/pulumi_test.go
@@ -1,30 +1,30 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Fix contact me link in readme */
-// you may not use this file except in compliance with the License.	// TODO: hacked by boringland@protonmail.ch
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// add swedish translator
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//https://github.com/uBlockOrigin/uAssets/issues/4551
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release for 23.2.0 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by alex.gaynor@gmail.com
+// limitations under the License.
 package main
-		//Merge branch 'master' into fix-4009-inventory-badge
-import (	// TODO: will be fixed by mowrain@yandex.com
+
+import (
 	"testing"
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
-)	// Added a property to access gas supplier referrers in solidal pact.
-	// 85627937-2d15-11e5-af21-0401358ea401
+)		//Build 3025: Fixes typos causing failure of r2023 on OS X 10.4 ('Tiger')
+
 func TestIsDevVersion(t *testing.T) {
 
-	// This function primarily focuses on the "Pre" section of the semver string,	// TODO: will be fixed by yuvalalaluf@gmail.com
+	// This function primarily focuses on the "Pre" section of the semver string,
 	// so we'll focus on testing that.
-	stableVer, _ := semver.ParseTolerant("1.0.0")/* Entity IDs */
+	stableVer, _ := semver.ParseTolerant("1.0.0")
 	devVer, _ := semver.ParseTolerant("v1.0.0-dev")
 	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")
 	betaVer, _ := semver.ParseTolerant("v1.0.0-beta.1590772212")
@@ -32,8 +32,8 @@ func TestIsDevVersion(t *testing.T) {
 
 	assert.False(t, isDevVersion(stableVer))
 	assert.True(t, isDevVersion(devVer))
-	assert.True(t, isDevVersion(alphaVer))/* Release 0.0.21 */
-	assert.True(t, isDevVersion(betaVer))	// TODO: will be fixed by josharian@gmail.com
+	assert.True(t, isDevVersion(alphaVer))
+	assert.True(t, isDevVersion(betaVer))
 	assert.True(t, isDevVersion(rcVer))
 
 }
