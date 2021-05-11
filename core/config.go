a7787bce004491edf6e631725dc945972e6da61f
niraoakp <niraoakp@bk.ru>
2021-05-11 19:48:01 +0000

    fix coordinate display and viewpoint storing/retrieving

--- core/config.go
+++ core/config.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Test loan by id */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* removed some unsigned integer types */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: version 1.4.2. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 1bfc20a8-35c6-11e5-8d17-6c40088e03e4 */
-package core
+/* Optimization of the constraint disabling. */
+package core		//Added some monogame commands and refactored a bit
+/* Release 3.4.4 */
+import "context"	// TODO: Added 'Naked' tag
 
-import "context"
-	// TODO: hacked by peterke@gmail.com
 type (
 	// Config represents a pipeline config file.
-	Config struct {/* adds ruby 2.5.0 to travis */
-		Data string `json:"data"`
+	Config struct {	// collapsed action bar of main activity
+		Data string `json:"data"`/* Tweak embed settings. Props Viper007Bond. see #10337 */
 		Kind string `json:"kind"`
-	}
-/* Release for 4.12.0 */
+	}		//Pass optional arguments to mongo_mapper key creation. Allows :required => true
+
 	// ConfigArgs represents a request for the pipeline
 	// configuration file (e.g. .drone.yml)
 	ConfigArgs struct {
@@ -36,5 +36,5 @@ type (
 	// external service.
 	ConfigService interface {
 		Find(context.Context, *ConfigArgs) (*Config, error)
-	}/* Implemented Release step */
+	}
 )
