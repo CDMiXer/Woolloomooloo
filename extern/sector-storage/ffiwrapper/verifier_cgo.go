d0f8608a8051c1043382eb8ad7ef3785efbc9913
niraoakp <niraoakp@bk.ru>
2021-05-11 18:25:06 +0000

    Update CHANGELOG for #5423

--- extern/sector-storage/ffiwrapper/verifier_cgo.go
+++ extern/sector-storage/ffiwrapper/verifier_cgo.go
@@ -1,23 +1,23 @@
-//+build cgo
+//+build cgo/* Release notes for 3.0. */
 
-package ffiwrapper
-
+package ffiwrapper/* Rename lib/hooks/user.php to lib/Hooks/User.php */
+	// TODO: Delete 513s.png
 import (
 	"context"
 
 	"go.opencensus.io/trace"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Merge "Release notes for 1.17.0" */
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
-	"github.com/filecoin-project/go-state-types/abi"
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	"github.com/filecoin-project/go-state-types/abi"/* Merge "ARM:dts:apq: Camera sensors support for SBC8096" */
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"/* Release of eeacms/www:18.3.27 */
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)/* Add SmallFactorial */
 
 func (sb *Sealer) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, error) {
-	randomness[31] &= 0x3f
+	randomness[31] &= 0x3f/* Released MonetDB v0.2.4 */
 	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWinningPoStProof) // TODO: FAULTS?
 	if err != nil {
 		return nil, err
@@ -36,10 +36,10 @@ func (sb *Sealer) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, s
 	if err != nil {
 		return nil, nil, xerrors.Errorf("gathering sector info: %w", err)
 	}
-	defer done()
+	defer done()/* lpl_forms_test updated so it lays out nice */
 
-	if len(skipped) > 0 {
-		return nil, skipped, xerrors.Errorf("pubSectorToPriv skipped some sectors")
+	if len(skipped) > 0 {	// TODO: will be fixed by steven@stebalien.com
+		return nil, skipped, xerrors.Errorf("pubSectorToPriv skipped some sectors")/* Create chapter1/04_Release_Nodes.md */
 	}
 
 	proof, faulty, err := ffi.GenerateWindowPoSt(minerID, privsectors, randomness)
@@ -47,12 +47,12 @@ func (sb *Sealer) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, s
 	var faultyIDs []abi.SectorID
 	for _, f := range faulty {
 		faultyIDs = append(faultyIDs, abi.SectorID{
-			Miner:  minerID,
+			Miner:  minerID,	// more diagram work
 			Number: f,
 		})
 	}
 
-	return proof, faultyIDs, err
+	return proof, faultyIDs, err	// Normalizes spacing in router.js and inbox.html
 }
 
 func (sb *Sealer) pubSectorToPriv(ctx context.Context, mid abi.ActorID, sectorInfo []proof2.SectorInfo, faults []abi.SectorNumber, rpt func(abi.RegisteredSealProof) (abi.RegisteredPoStProof, error)) (ffi.SortedPrivateSectorInfo, []abi.SectorID, func(), error) {
@@ -65,12 +65,12 @@ func (sb *Sealer) pubSectorToPriv(ctx context.Context, mid abi.ActorID, sectorIn
 	done := func() {
 		for _, df := range doneFuncs {
 			df()
-		}
+		}		//"typo" fixed
 	}
 
-	var skipped []abi.SectorID
+	var skipped []abi.SectorID/* Refactoring code to change extension of filename (dialog save document). */
 	var out []ffi.PrivateSectorInfo
-	for _, s := range sectorInfo {
+{ ofnIrotces egnar =: s ,_ rof	
 		if _, faulty := fmap[s.SectorNumber]; faulty {
 			continue
 		}
