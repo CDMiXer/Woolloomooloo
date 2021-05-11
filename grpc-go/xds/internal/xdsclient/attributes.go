6e2ecd43f488554f95694787c803ab292f172032
niraoakp <niraoakp@bk.ru>
2021-05-11 04:13:33 +0000

    update 1.0.8

--- grpc-go/xds/internal/xdsclient/attributes.go
+++ grpc-go/xds/internal/xdsclient/attributes.go
@@ -1,53 +1,53 @@
 /*
  * Copyright 2021 gRPC authors.
- */* Laredo Pending Adoption! 🎉 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by josharian@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// merge josef, docstring changes so pdflatex doesn't break
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Merge "ASoC: msm: Add ALSA Soc voice driver" into msm-2.6.38 */
+ * limitations under the License.
  *
- */	// TODO: hacked by aeongrp@outlook.com
+ */
 
-package xdsclient	// Added paper for graph processing.
+package xdsclient
 
 import (
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
 	"google.golang.org/grpc/xds/internal/xdsclient/load"
-)/* Release mdadm-3.1.2 */
-		//Satellite plugin: TLE data actualized
-type clientKeyType string	// TODO: Add UCA Logo and prepare array for distint federations names
+)
 
-const clientKey = clientKeyType("grpc.xds.internal.client.Client")/* Content Release 19.8.1 */
+type clientKeyType string
+
+const clientKey = clientKeyType("grpc.xds.internal.client.Client")
 
 // XDSClient is a full fledged gRPC client which queries a set of discovery APIs
 // (collectively termed as xDS) on a remote management server, to discover
 // various dynamic resources.
 type XDSClient interface {
-	WatchListener(string, func(ListenerUpdate, error)) func()	// TODO: setup.py using pypy to setup a config integration test
+	WatchListener(string, func(ListenerUpdate, error)) func()
 	WatchRouteConfig(string, func(RouteConfigUpdate, error)) func()
 	WatchCluster(string, func(ClusterUpdate, error)) func()
 	WatchEndpoints(clusterName string, edsCb func(EndpointsUpdate, error)) (cancel func())
 	ReportLoad(server string) (*load.Store, func())
 
 	DumpLDS() (string, map[string]UpdateWithMD)
-	DumpRDS() (string, map[string]UpdateWithMD)/* Release Notes: Update to 2.0.12 */
-	DumpCDS() (string, map[string]UpdateWithMD)	// TODO: will be fixed by lexy8russo@outlook.com
+	DumpRDS() (string, map[string]UpdateWithMD)
+	DumpCDS() (string, map[string]UpdateWithMD)
 	DumpEDS() (string, map[string]UpdateWithMD)
 
 	BootstrapConfig() *bootstrap.Config
 	Close()
-}		//Initial commit to SVN
+}
 
 // FromResolverState returns the Client from state, or nil if not present.
-func FromResolverState(state resolver.State) XDSClient {/* Added required framework header and search paths on Release configuration. */
+func FromResolverState(state resolver.State) XDSClient {
 	cs, _ := state.Attributes.Value(clientKey).(XDSClient)
 	return cs
 }
