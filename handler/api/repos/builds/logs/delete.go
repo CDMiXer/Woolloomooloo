2a14854468082baf835fbc0e471b658b4c249a42
niraoakp <niraoakp@bk.ru>
2021-05-11 20:24:27 +0000

    Update mytools.user.js

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,57 +1,57 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Fix enum validation failing on schema validation */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Edited arch/arm/mach-tegra/nvrm/core/common/nvrm_clocks_limits.c via GitHub
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Delete rfc.h
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: fix typo and added more one course
+// See the License for the specific language governing permissions and/* Add an fma TableGen node. */
 // limitations under the License.
-/* Create disparo */
-package logs
+
+package logs		//test: raise file timeout to 75ms
 
 import (
 	"net/http"
-	"strconv"	// Delete OL1coefficient055.txt
+	"strconv"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Update to Releasenotes for 2.1.4 */
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-	// TODO: will be fixed by sbrichards@gmail.com
+/* Merge branch 'master' into CCM-42-create-an-option-document-type */
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the logs./* 4.11.0 Release */
+// requests to delete the logs.
 func HandleDelete(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
-	logs core.LogStore,/* Changed NewRelease servlet config in order to make it available. */
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// Partial Update
+	logs core.LogStore,
+) http.HandlerFunc {/* Release 3.3.1 */
+	return func(w http.ResponseWriter, r *http.Request) {		//implement symbolic links
+		var (/* Release version 1.3.1 with layout bugfix */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)	// TODO: Update files via my NOKIA 3310
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
-{ lin =! rre fi		
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))/* [v0.0.1] Release Version 0.0.1. */
+		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
-			render.BadRequest(w, err)	// TODO: will be fixed by steven@stebalien.com
-			return
+			render.BadRequest(w, err)
+			return		//doc: add badges for travis and gitter
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
@@ -60,24 +60,24 @@ func HandleDelete(
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-)rre ,w(dnuoFtoN.redner			
-			return
-		}	// TODO: will be fixed by joshua@yottadb.com
-		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)	// TODO: will be fixed by yuvalalaluf@gmail.com
-		if err != nil {
-			render.NotFound(w, err)
-			return	// TODO: will be fixed by hugomrdias@gmail.com
-}		
-		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)/* Added .row to better bootstrap */
-		if err != nil {
 			render.NotFound(w, err)
 			return
+		}
+		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
+		if err != nil {		//Updated text on jobs.MD
+			render.NotFound(w, err)
+			return
+		}
+		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
+		if err != nil {
+			render.NotFound(w, err)
+			return/* Bug fix for #3468526: Initial read is repeated after COMET Timeout */
 		}
 		err = logs.Delete(r.Context(), step.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return	// move exception throw if the there are not two items. nothing to see here
 		}
 		w.WriteHeader(204)
-	}
+	}/* explicitly set path */
 }
