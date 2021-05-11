0b5b6a020a0536704bfeaacaf936d504f329477f
niraoakp <niraoakp@bk.ru>
2021-05-11 02:30:37 +0000

    117b9ff8-2e52-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/binder_test.go
+++ pkg/codegen/hcl2/binder_test.go
@@ -1,31 +1,31 @@
 package hcl2
 
-import (/* Release v2.0.0.0 */
+import (
 	"bytes"
-	"io/ioutil"		//github: Fix toolchain extraction
-"htapelif/htap"	
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"/* Release new version 2.5.49:  */
+	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-)/* Release version; Added test. */
-	// changed CI to APPDEV and added link to App Catalog
+)
+
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestBindProgram(t *testing.T) {
-	files, err := ioutil.ReadDir(testdataPath)		//[20811] use virtual flag for table of SelectBestellungDialog
+	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
-		t.Fatalf("could not read test data: %v", err)		//pathchanges. Now you can edit and view products
+		t.Fatalf("could not read test data: %v", err)
 	}
 
-	for _, f := range files {		//Use local boost library
+	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
 		}
 
-		t.Run(f.Name(), func(t *testing.T) {/* Update Release scripts */
+		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
@@ -40,7 +40,7 @@ func TestBindProgram(t *testing.T) {
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
-/* some cleanup of runtime only database dependencies */
+
 			_, diags, err := BindProgram(parser.Files, PluginHost(test.NewHost(testdataPath)))
 			assert.NoError(t, err)
 			if diags.HasErrors() {
