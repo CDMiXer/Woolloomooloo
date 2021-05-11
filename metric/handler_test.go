8146ffb1ebb62c7f036e2d73cef88ccc7edbaeae
niraoakp <niraoakp@bk.ru>
2021-05-11 01:18:46 +0000

    delete of none used lines

--- metric/handler_test.go
+++ metric/handler_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Released version 0.8.40 */
+// that can be found in the LICENSE file.	// NoItemToRemoveException: Javadoc fixed
 
 // +build !oss
 
@@ -12,22 +12,22 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"
-)
+	"github.com/golang/mock/gomock"/* Release 14.0.0 */
+)	// Update jest-dom to v3.1.2
 
 func TestHandleMetrics(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Update install-oracle-jdk */
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// hehe hhoho
 	r := httptest.NewRequest("GET", "/", nil)
 
 	mockUser := &core.User{Admin: false, Machine: true}
 	session := mock.NewMockSession(controller)
-	session.EXPECT().Get(r).Return(mockUser, nil)
+	session.EXPECT().Get(r).Return(mockUser, nil)		//Merge "[DM] Use new VNC API to get unencrypted password"
 
 	NewServer(session, false).ServeHTTP(w, r)
-	if got, want := w.Code, 200; got != want {
+	if got, want := w.Code, 200; got != want {/* fixed bug where cookie value not being set to replaced variable */
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 
@@ -41,12 +41,12 @@ func TestHandleMetrics_NoSession(t *testing.T) {
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)	// TODO: 59d6hxpWo5gGBelzlV8p5fZL9nfzgz3o
 
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(nil, nil)
 
-	NewServer(session, false).ServeHTTP(w, r)
+	NewServer(session, false).ServeHTTP(w, r)/* Script to change the NIC metric */
 
 	if got, want := w.Code, 401; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
@@ -64,7 +64,7 @@ func TestHandleMetrics_NoSessionButAnonymousAccessEnabled(t *testing.T) {
 	session.EXPECT().Get(r).Return(nil, nil)
 
 	NewServer(session, true).ServeHTTP(w, r)
-
+	// Create PickerSendFile.html
 	if got, want := w.Code, 200; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
@@ -77,12 +77,12 @@ func TestHandleMetrics_AccessDenied(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
-	mockUser := &core.User{Admin: false, Machine: false}
-	session := mock.NewMockSession(controller)
+	mockUser := &core.User{Admin: false, Machine: false}	// TODO: will be fixed by vyzo@hackzen.org
+	session := mock.NewMockSession(controller)/* Release 0.40.0 */
 	session.EXPECT().Get(r).Return(mockUser, nil)
 
-	NewServer(session, false).ServeHTTP(w, r)
-	if got, want := w.Code, 403; got != want {
+	NewServer(session, false).ServeHTTP(w, r)	// TODO: jacoco + codecov
+	if got, want := w.Code, 403; got != want {	// TODO: Update hs5.md
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
