b49f217ee8bf6def99d7256e8752b44a5077e104
niraoakp <niraoakp@bk.ru>
2021-05-11 15:36:22 +0000

    Add note about how to stream remotely, fixes #230

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,15 +1,15 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: Reduce auto completion list flicker by reusing the visualization
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2		//krauterbank image upload
+package oauth2	// TODO: hacked by 13860583249@yeah.net
 
-import "testing"
-	// Update UserModel.class.php
-func TestError(t *testing.T) {
+import "testing"/* Fixed a copy paste bug, by cloning objects before pasting */
+
+func TestError(t *testing.T) {/* Merge "Release 1.0.0.181 QCACLD WLAN Driver" */
 	err := Error{}
 	err.Code = "invalid_request"
-	err.Desc = " The request is missing a required parameter"
+	err.Desc = " The request is missing a required parameter"		//Added notes to double/ceiling on value coverage
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
 		t.Errorf("Want error message %q, got %q", want, got)
 	}
