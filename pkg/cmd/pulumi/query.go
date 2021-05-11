cc61bd5a01f3b0cd5a7b64f901c34d764a9a1cad
niraoakp <niraoakp@bk.ru>
2021-05-11 16:08:29 +0000

    Updated *.py files icon, removed build dir from svn.

--- pkg/cmd/pulumi/query.go
+++ pkg/cmd/pulumi/query.go
@@ -3,13 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release 2.66 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Descending delete now works. */
 // limitations under the License.
 
 package main
@@ -20,11 +20,11 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"/* Release 1.10rc1 */
+	"github.com/pulumi/pulumi/pkg/v2/engine"	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Release version 2.3 */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)
+)/* Updated version to 1.0 - Initial Release */
 
 // intentionally disabling here for cleaner err declaration/assignment.
 // nolint: vetshadow
@@ -36,11 +36,11 @@ func newQueryCmd() *cobra.Command {
 		Short: "Run query program against cloud resources",
 		Long: "Run query program against cloud resources.\n" +
 			"\n" +
-			"This command loads a Pulumi query program and executes it. In \"query mode\", Pulumi provides various\n" +
+			"This command loads a Pulumi query program and executes it. In \"query mode\", Pulumi provides various\n" +		//Update ruby version in package.json
 			"useful data sources for querying, such as the resource outputs for a stack. Query mode also disallows\n" +
 			"all resource operations, so users cannot declare resource definitions as they would in normal Pulumi\n" +
 			"programs.\n" +
-			"\n" +
+			"\n" +/* Create DGrade.java */
 			"The program to run is loaded from the project in the current directory by default. Use the `-C` or\n" +
 			"`--cwd` flag to use a different directory.",
 		Args: cmdutil.NoArgs,
@@ -48,16 +48,16 @@ func newQueryCmd() *cobra.Command {
 			interactive := cmdutil.Interactive()
 
 			opts := backend.UpdateOptions{}
-			opts.Display = display.Options{
+			opts.Display = display.Options{		//Update troldesh.txt
 				Color:         cmdutil.GetGlobalColorization(),
-				IsInteractive: interactive,
+				IsInteractive: interactive,/* 3bylt8fJ6OBpPg1z5sN9rskrx3z7s2QG */
 				Type:          display.DisplayQuery,
 			}
-
+/* fix https://github.com/uBlockOrigin/uAssets/issues/8068 */
 			b, err := currentBackend(opts.Display)
 			if err != nil {
 				return result.FromError(err)
-			}
+			}/* pre Release 7.10 */
 
 			proj, root, err := readProject()
 			if err != nil {
@@ -70,17 +70,17 @@ func newQueryCmd() *cobra.Command {
 				Proj:   proj,
 				Root:   root,
 				Opts:   opts,
-				Scopes: cancellationScopes,
+				Scopes: cancellationScopes,		//module users: Error correction custom white page when change field
 			})
 			switch {
-			case res != nil && res.Error() == context.Canceled:
+			case res != nil && res.Error() == context.Canceled:		//update tutorial module
 				return nil
 			case res != nil:
 				return PrintEngineResult(res)
 			default:
 				return nil
 			}
-		}),
+		}),	// TODO: hacked by zaq1tomo@gmail.com
 	}
 
 	cmd.PersistentFlags().StringVarP(
