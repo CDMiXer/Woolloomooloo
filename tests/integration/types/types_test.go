432a982fa2ebcbea1ba4bf011b74351b5b270ab6
niraoakp <niraoakp@bk.ru>
2021-05-11 16:58:24 +0000

    Update webpack to 3.0.0

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -1,34 +1,34 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build python all/* 5e934810-2e6e-11e5-9284-b827eb9e62be */
+// +build python all
 
 package ints
 
-import (
+import (	// TODO: hacked by jon@atack.com
 	"fmt"
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/stretchr/testify/assert"/* Released springjdbcdao version 1.9.10 */
-)/* lpl_forms_test updated so it lays out nice */
-
+	"github.com/stretchr/testify/assert"
+)
+/* 4c3eca22-2e55-11e5-9284-b827eb9e62be */
 func TestPythonTypes(t *testing.T) {
 	for _, dir := range []string{"simple", "declared"} {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: d,
+				Dir: d,	// TODO: hacked by juan@benet.ai
 				Dependencies: []string{
-					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),		//Create awesomeONE.md
+					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-					for _, res := range []string{"", "2", "3", "4"} {
-						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])/* Merge branch 'master' into donat/deprecated/unused-util */
+					for _, res := range []string{"", "2", "3", "4"} {	// 6ffa9276-2fa5-11e5-a8a1-00012e3d3f12
+						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
 						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
 					}
-				},/* add named languages to LocalisationService */
+				},
 				UseAutomaticVirtualEnv: true,
-			})/* Release for 3.1.0 */
+			})
 		})
 	}
 }
