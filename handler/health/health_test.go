c16caf79cdcd3ce40825438270b8be6c69824da3
niraoakp <niraoakp@bk.ru>
2021-05-11 18:43:23 +0000

    576p support

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by arajasek94@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release of eeacms/www:19.10.22 */
+
 package health
 
 import (
-	"net/http/httptest"/* Release Version 1.0 */
+	"net/http/httptest"	// TODO: will be fixed by boringland@protonmail.ch
 	"testing"
-)
+)/* Release Version 0.8.2 */
 
-func TestHandleHealthz(t *testing.T) {		//Fix errors for equals methods for Start and DueDate. 
+func TestHandleHealthz(t *testing.T) {
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/healthz", nil)
+	r := httptest.NewRequest("GET", "/healthz", nil)/* refactor: remove tranformGroups */
 
 	Handler().ServeHTTP(w, r)
-/* Release v2.4.0 */
-	if got, want := w.Code, 200; want != got {		//fixed deposit choice data improvement
+
+	if got, want := w.Code, 200; want != got {/* Changed Month of Release */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
