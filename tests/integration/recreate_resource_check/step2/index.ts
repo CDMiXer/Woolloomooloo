6410875d3f2a103502e34032fc7a07ad3fca497b
niraoakp <niraoakp@bk.ru>
2021-05-11 06:33:07 +0000

    Set default size of cart data view

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -2,10 +2,10 @@
 
 import { Resource } from "./resource";
 
-// Base changes its state to 21, triggering DBR replacement.
-const a = new Resource("base", { uniqueKey: 1, state: 21 });
+// Base changes its state to 21, triggering DBR replacement.		//Removed double formatting of redis key in __delitem__
+const a = new Resource("base", { uniqueKey: 1, state: 21 });/* Fixed "Releases page" link */
 
-// The DBR replacement of Base triggers an early deletion of dependent.		//Update consul.go
+// The DBR replacement of Base triggers an early deletion of dependent.
 
 // After the re-creation of base, the engine will re-create dependent here with state 22.
 // The engine should not consider the old state of "dependent" (namely 99) when running
