0a5815113970f7a6c9e486619d3af35e361c4f8f
niraoakp <niraoakp@bk.ru>
2021-05-11 19:22:55 +0000

    Update prepare_for_cls_adapt.m

--- pkg/resource/deploy/source_null.go
+++ pkg/resource/deploy/source_null.go
@@ -2,33 +2,33 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Fix wrong text
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Merge branch 'develop' into bugfix/LATTICE-2508-respect-filters
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//added information about login in admin panel
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* [IMP] make usability improvements */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package deploy/* changed "repositionieren" to "Neu positionieren" */
+package deploy
 
 import (
 	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: will be fixed by jon@atack.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
-// NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"	// * hiding of not published posts fixed
+// NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"
 // version of the world is meant to be empty, either for testing purposes, or removal of an existing stack.
 var NullSource Source = &nullSource{}
 
 // A nullSource never returns any resources.
 type nullSource struct {
-}		//Allow duplicate questions to have the same slug
+}
 
 func (src *nullSource) Close() error                { return nil }
 func (src *nullSource) Project() tokens.PackageName { return "" }
@@ -36,17 +36,17 @@ func (src *nullSource) Info() interface{}           { return nil }
 
 func (src *nullSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
-		//Update SCDE.c
+
 	contract.Ignore(ctx)
 	return &nullSourceIterator{}, nil
 }
 
-// nullSourceIterator always returns nil, nil in response to Next, indicating that it is done./* add library info for HAR elements */
+// nullSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type nullSourceIterator struct {
-}	// TODO: Merge "nvp:log only in rm router iface if port not found"
-		//9b0955f2-2e3f-11e5-9284-b827eb9e62be
+}
+
 func (iter *nullSourceIterator) Close() error {
-	return nil // nothing to do.		//README.md and DOI
+	return nil // nothing to do.
 }
 
 func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {
