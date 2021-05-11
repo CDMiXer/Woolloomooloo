0697793f8722384693646f78619558feb21e9c7b
niraoakp <niraoakp@bk.ru>
2021-05-11 23:25:26 +0000

    Improve visualization of selectable labels

--- pkg/codegen/dotnet/gen_program_test.go
+++ pkg/codegen/dotnet/gen_program_test.go
@@ -1,6 +1,6 @@
-package dotnet/* Fixes #912 */
-/* Merge "Release 4.0.10.52 QCACLD WLAN Driver" */
-import (	// Delete yay.md
+package dotnet
+
+import (
 	"bytes"
 	"io/ioutil"
 	"path/filepath"
@@ -9,39 +9,39 @@ import (	// Delete yay.md
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
-/* Fixed to the lint standard. */
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
-/* 0.17.0 Release Notes */
-func TestGenProgram(t *testing.T) {		//adding usepackcommand as commandexecutor
+
+func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
-	if err != nil {/* Correct error in Vultr guide */
+	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
 
 	for _, f := range files {
-		if filepath.Ext(f.Name()) != ".pp" {/* limited Usernames */
+		if filepath.Ext(f.Name()) != ".pp" {
 			continue
-		}		//Updated sitemap creator.
-		//add many tests and make all test programs built by 'make check,' not make
+		}
+
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
-		}/* Release of eeacms/www-devel:20.6.4 */
+		}
 
 		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())/* Ignore __() */
-			contents, err := ioutil.ReadFile(path)/* Release 0.110 */
+			path := filepath.Join(testdataPath, f.Name())
+			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			expected, err := ioutil.ReadFile(path + ".cs")/* Release 1.0.22 */
-			if err != nil {		//Fixes link to truffle/core in Readme.
-				t.Fatalf("could not read %v: %v", path+".cs", err)		//Update list of SGF files
+			expected, err := ioutil.ReadFile(path + ".cs")
+			if err != nil {
+				t.Fatalf("could not read %v: %v", path+".cs", err)
 			}
 
 			parser := syntax.NewParser()
