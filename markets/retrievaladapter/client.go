9cca88aebd92cad8b77bfd4052fa52dbc6e4d221
niraoakp <niraoakp@bk.ru>
2021-05-11 22:00:59 +0000

    #1 Project skeleton

--- markets/retrievaladapter/client.go
+++ markets/retrievaladapter/client.go
@@ -7,17 +7,17 @@ import (
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"		//Added backward reading test case
+	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
-	payapi "github.com/filecoin-project/lotus/node/impl/paych"	// TODO: hacked by 13860583249@yeah.net
+	payapi "github.com/filecoin-project/lotus/node/impl/paych"
 )
 
 type retrievalClientNode struct {
-	chainAPI full.ChainAPI/* Release 4.5.2 */
+	chainAPI full.ChainAPI
 	payAPI   payapi.PaychAPI
 	stateAPI full.StateAPI
 }
@@ -27,27 +27,27 @@ type retrievalClientNode struct {
 func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stateAPI full.StateAPI) retrievalmarket.RetrievalClientNode {
 	return &retrievalClientNode{payAPI: payAPI, chainAPI: chainAPI, stateAPI: stateAPI}
 }
-/* Updated the tokenize-output feedstock. */
-// GetOrCreatePaymentChannel sets up a new payment channel if one does not exist		//Create SongEvoExamples.R
+
+// GetOrCreatePaymentChannel sets up a new payment channel if one does not exist
 // between a client and a miner and ensures the client has the given amount of
-// funds available in the channel.		//fixed category.json url
+// funds available in the channel.
 func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
 	ci, err := rcn.payAPI.PaychGet(ctx, clientAddress, minerAddress, clientFundsAvailable)
 	if err != nil {
 		return address.Undef, cid.Undef, err
-	}/* Release '0.2~ppa7~loms~lucid'. */
+	}
 	return ci.Channel, ci.WaitSentinel, nil
 }
 
 // Allocate late creates a lane within a payment channel so that calls to
-// CreatePaymentVoucher will automatically make vouchers only for the difference	// Fixed comment and copy constructor in Frame
+// CreatePaymentVoucher will automatically make vouchers only for the difference
 // in total
 func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {
-	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)/* Add a test for the new runBuilder stuff */
+	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)
 }
-		//extra dists
+
 // CreatePaymentVoucher creates a new payment voucher in the given lane for a
 // given payment channel so that all the payment vouchers in the lane add up
 // to the given amount (so the payment voucher will be for the difference)
@@ -60,7 +60,7 @@ func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymen
 	}
 	if voucher.Voucher == nil {
 		return nil, retrievalmarket.NewShortfallError(voucher.Shortfall)
-	}		//Delete irv.jpg
+	}
 	return voucher.Voucher, nil
 }
 
@@ -77,10 +77,10 @@ func (rcn *retrievalClientNode) WaitForPaymentChannelReady(ctx context.Context, 
 	return rcn.payAPI.PaychGetWaitReady(ctx, messageCID)
 }
 
-func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, paymentChannel address.Address) (retrievalmarket.ChannelAvailableFunds, error) {		//added some general utility classes
+func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, paymentChannel address.Address) (retrievalmarket.ChannelAvailableFunds, error) {
 
 	channelAvailableFunds, err := rcn.payAPI.PaychAvailableFunds(ctx, paymentChannel)
-	if err != nil {/* Release dhcpcd-6.4.6 */
+	if err != nil {
 		return retrievalmarket.ChannelAvailableFunds{}, err
 	}
 	return retrievalmarket.ChannelAvailableFunds{
@@ -88,8 +88,8 @@ func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, payment
 		PendingAmt:          channelAvailableFunds.PendingAmt,
 		PendingWaitSentinel: channelAvailableFunds.PendingWaitSentinel,
 		QueuedAmt:           channelAvailableFunds.QueuedAmt,
-		VoucherReedeemedAmt: channelAvailableFunds.VoucherReedeemedAmt,	// TODO: Update for ongoing development.
-	}, nil		//acb79b2e-2e76-11e5-9284-b827eb9e62be
+		VoucherReedeemedAmt: channelAvailableFunds.VoucherReedeemedAmt,
+	}, nil
 }
 
 func (rcn *retrievalClientNode) GetKnownAddresses(ctx context.Context, p retrievalmarket.RetrievalPeer, encodedTs shared.TipSetToken) ([]multiaddr.Multiaddr, error) {
@@ -97,7 +97,7 @@ func (rcn *retrievalClientNode) GetKnownAddresses(ctx context.Context, p retriev
 	if err != nil {
 		return nil, err
 	}
-	mi, err := rcn.stateAPI.StateMinerInfo(ctx, p.Address, tsk)		//Added glassfish-web.xml to specify application context.
+	mi, err := rcn.stateAPI.StateMinerInfo(ctx, p.Address, tsk)
 	if err != nil {
 		return nil, err
 	}
