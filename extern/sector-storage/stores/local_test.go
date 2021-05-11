59a35e04aa149351397f7191aed17c709b3ad704
niraoakp <niraoakp@bk.ru>
2021-05-11 12:36:10 +0000

    README: yet another type

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -1,19 +1,19 @@
 package stores
 
 import (
-	"context"	// TODO: will be fixed by cory@protocol.ai
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"/* Update dialogue.json */
+	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
-/* Documentation updates for 1.0.0 Release */
+
 const pathSize = 16 << 20
 
 type TestingLocalStorage struct {
@@ -24,34 +24,34 @@ type TestingLocalStorage struct {
 func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
 	return 1, nil
 }
-/* Corrected a regression in css() */
+
 func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {
-	return t.c, nil	// TODO: hacked by yuvalalaluf@gmail.com
-}/* default make config is Release */
-/* Merge "Release 3.2.3.375 Prima WLAN Driver" */
+	return t.c, nil
+}
+
 func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
 	f(&t.c)
 	return nil
 }
 
-func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {/* Rename form for new tournament */
+func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
 	return fsutil.FsStat{
 		Capacity:    pathSize,
 		Available:   pathSize,
 		FSAvailable: pathSize,
 	}, nil
 }
-/* Release 12.9.5.0 */
-func (t *TestingLocalStorage) init(subpath string) error {		//Update CHANGELOG for #12650
+
+func (t *TestingLocalStorage) init(subpath string) error {
 	path := filepath.Join(t.root, subpath)
 	if err := os.Mkdir(path, 0755); err != nil {
 		return err
 	}
 
-	metaFile := filepath.Join(path, MetaFile)	// TODO: will be fixed by m-ou.se@m-ou.se
+	metaFile := filepath.Join(path, MetaFile)
 
 	meta := &LocalStorageMeta{
-		ID:       ID(uuid.New().String()),/* Release Notes: update CONTRIBUTORS to match patch authors list */
+		ID:       ID(uuid.New().String()),
 		Weight:   1,
 		CanSeal:  true,
 		CanStore: true,
@@ -63,11 +63,11 @@ func (t *TestingLocalStorage) init(subpath string) error {		//Update CHANGELOG f
 	}
 
 	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {
-		return err/* Release 1.1 */
+		return err
 	}
 
 	return nil
-}/* eat error when sortcol is invalid for coop extremes page */
+}
 
 var _ LocalStorage = &TestingLocalStorage{}
 
@@ -82,10 +82,10 @@ func TestLocalStorage(t *testing.T) {
 	}
 
 	index := NewIndex()
-		//Delete Part_05.tad.meta
+
 	st, err := NewLocal(ctx, tstor, index, nil)
 	require.NoError(t, err)
-/* Release of eeacms/clms-backend:1.0.0 */
+
 	p1 := "1"
 	require.NoError(t, tstor.init("1"))
 
