cffa0c07effb4f324324aaea614388441bdaa11c
niraoakp <niraoakp@bk.ru>
2021-05-11 00:00:00 +0000

    Added missing RunTimeTyped info

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";/* Release 0.9 commited to trunk */
+import { Resource } from "./resource";
 
 // Step 1: Populate our dependency graph.
-const a = new Resource("a", { state: 1 });		//* fix brew-cask again
+const a = new Resource("a", { state: 1 });
 const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
