72c1725d7cc0962f8436d0b35021f54826dc2731
niraoakp <niraoakp@bk.ru>
2021-05-11 04:44:30 +0000

    change infowindow template

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs
+-- name: create-table-logs		//NetKAN generated mods - KSPInterstellarExtended-1.25.9.1
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    SERIAL PRIMARY KEY
-,log_data  BYTEA	// Set up expectations for HtmlFromPdf methods.
-);
+,log_data  BYTEA
+);		//Merge "Add option to clear profile data to 'cmd package compile'" into nyc-dev
