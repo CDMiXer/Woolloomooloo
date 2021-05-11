4143b520e6e67678b1c50ae0d0b73e23b78f244e
niraoakp <niraoakp@bk.ru>
2021-05-11 23:18:19 +0000

    Autofocus search box

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
--- name: create-table-cron		//Update ExcelToForm.vb
+-- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          SERIAL PRIMARY KEY
-,cron_repo_id     INTEGER		//Create quote.php
-,cron_name        VARCHAR(50)
+ cron_id          SERIAL PRIMARY KEY/* IBH-83 Getting environment variable and passing it into the scan process */
+,cron_repo_id     INTEGER
+,cron_name        VARCHAR(50)	// TODO: will be fixed by ng8eke@163.com
 ,cron_expr        VARCHAR(50)
-,cron_next        INTEGER
+,cron_next        INTEGER/* ncdc_count: Use LazyOutputFormat */
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)
+,cron_branch      VARCHAR(250)/* Release 0.95.200: Crash & balance fixes. */
 ,cron_target      VARCHAR(250)
-,cron_disabled    BOOLEAN/* fix java version */
+,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
-,cron_updated     INTEGER
+,cron_updated     INTEGER	// TODO: hacked by mikeal.rogers@gmail.com
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);		//Cleanup in onBlockBreak()
 
 -- name: create-index-cron-repo
 
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next
-
+-- name: create-index-cron-next		//updated readme.md with wappservice
+/* [Automated] [zoren] New translations */
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
