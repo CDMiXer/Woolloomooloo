a61925aff96df52d9d6945685049bcd4bab7e37e
niraoakp <niraoakp@bk.ru>
2021-05-11 16:40:48 +0000

    Updated the libopusenc feedstock.

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -8,17 +8,17 @@ import (
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-
+/* Release v0.3.0. */
 func NoRelay() func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
 	}
-}
+}	// TODO: Merge "Added implementation for Delete Node Task"
 
 // TODO: should be use baseRouting or can we use higher level router here?
-func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
+func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {	// [FIX] : Remove the conflict
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
 		return nil, fmt.Errorf("no suitable routing for discovery")
