2b9b4b9a41057a1ad3f477748fe55c4ae9ed18db
niraoakp <niraoakp@bk.ru>
2021-05-11 15:10:36 +0000

    Adição para GnuPlot

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: uncommitting an erroneous revision
+// Use of this source code is governed by the Drone Non-Commercial License		//Linking to last years slides
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: will be fixed by martin2cai@hotmail.com
 package rpc2
