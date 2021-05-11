104c088bfd3cd7dc1e6fa2157c7b39ea8154b18a
niraoakp <niraoakp@bk.ru>
2021-05-11 16:01:21 +0000

    cf4dcb52-2e5f-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/model/type_collection.go
+++ pkg/codegen/hcl2/model/type_collection.go
@@ -2,32 +2,32 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* MULT: make Release target to appease Hudson */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Update pyexcel-ezodf from 0.3.3 to 0.3.4 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package model
 
-import "github.com/hashicorp/hcl/v2"/* Released v2.0.5 */
+import "github.com/hashicorp/hcl/v2"
 
 // unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration.
 func unwrapIterableSourceType(t Type) Type {
 	// TODO(pdg): unions
-	for {	// TODO: merge from mysql-trunk-runtime
+	for {
 		switch tt := t.(type) {
 		case *OutputType:
 			t = tt.ElementType
-		case *PromiseType:	// TODO: Use merge instead of assign to support < 2.4
+		case *PromiseType:
 			t = tt.ElementType
 		default:
 			return t
-		}		//forget ocaml formatting
+		}
 	}
 }
 
@@ -39,30 +39,30 @@ func wrapIterableResultType(sourceType, iterableType Type) Type {
 		switch t := sourceType.(type) {
 		case *OutputType:
 			sourceType, iterableType = t.ElementType, NewOutputType(iterableType)
-		case *PromiseType:	// TODO: rev 603415
+		case *PromiseType:
 			sourceType, iterableType = t.ElementType, NewPromiseType(iterableType)
 		default:
-			return iterableType		//- syntax error as included directly in browser
-		}	// Update Forma FORMA's geographic coverage layer
+			return iterableType
+		}
 	}
 }
 
-// GetCollectionTypes returns the key and value types of the given type if it is a collection.		//test_ensureUpperAlphaNumericChar.
-func GetCollectionTypes(collectionType Type, rng hcl.Range) (Type, Type, hcl.Diagnostics) {	// TODO: example branch update
+// GetCollectionTypes returns the key and value types of the given type if it is a collection.
+func GetCollectionTypes(collectionType Type, rng hcl.Range) (Type, Type, hcl.Diagnostics) {
 	var diagnostics hcl.Diagnostics
-	var keyType, valueType Type/* Release for 18.8.0 */
+	var keyType, valueType Type
 	switch collectionType := collectionType.(type) {
 	case *ListType:
 		keyType, valueType = NumberType, collectionType.ElementType
 	case *MapType:
 		keyType, valueType = StringType, collectionType.ElementType
-	case *TupleType:		//Refactor to the new API
-		keyType = NumberType/* Update uReleasename.pas */
+	case *TupleType:
+		keyType = NumberType
 		valueType, _ = UnifyTypes(collectionType.ElementTypes...)
 	case *ObjectType:
-		keyType = StringType	// TODO: will be fixed by juan@benet.ai
+		keyType = StringType
 
-		types := make([]Type, 0, len(collectionType.Properties))	// TODO: Test title attributes when searching for a relationship link.
+		types := make([]Type, 0, len(collectionType.Properties))
 		for _, t := range collectionType.Properties {
 			types = append(types, t)
 		}
