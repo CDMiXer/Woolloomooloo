47cc90e890905d365b4d2dba61f416eee03441ad
niraoakp <niraoakp@bk.ru>
2021-05-11 08:50:21 +0000

    printf format fix

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"os"
 
-	gen "github.com/whyrusleeping/cbor-gen"
+	gen "github.com/whyrusleeping/cbor-gen"	// comment new changes
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 func main() {
-	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
-		sealing.Piece{},
+	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",		//annotations for tucanoan|perry_priest1985
+		sealing.Piece{},	// TODO: hacked by peterke@gmail.com
 		sealing.DealInfo{},
 		sealing.DealSchedule{},
 		sealing.SectorInfo{},
