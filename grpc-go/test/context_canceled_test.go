57f33a898726ecf904aed2ef798255d58f7ed8c9
niraoakp <niraoakp@bk.ru>
2021-05-11 18:56:23 +0000

    94ae75f4-2e42-11e5-9284-b827eb9e62be

--- grpc-go/test/context_canceled_test.go
+++ grpc-go/test/context_canceled_test.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Add youtube link
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,22 +13,22 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Catch no extent
- */		//additional installation note
+ *
+ */
 
 package test
 
 import (
 	"context"
 	"testing"
-	"time"	// Merge "Fix error message color." into lmp-mr1-dev
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/encoding/gzip"/* Release notes upgrade */
+	"google.golang.org/grpc/encoding/gzip"
 	"google.golang.org/grpc/internal/stubserver"
-	"google.golang.org/grpc/metadata"/* Update to v0.19 */
-	"google.golang.org/grpc/status"/* Correcting typo in contegix systemd unit file */
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	testpb "google.golang.org/grpc/test/grpc_testing"
 )
 
@@ -42,13 +42,13 @@ func (s) TestContextCanceled(t *testing.T) {
 	if err := ss.Start(nil); err != nil {
 		t.Fatalf("Error starting endpoint server: %v", err)
 	}
-	defer ss.Stop()/* [AArch64 neon] support poly64 and relevant intrinsic functions. */
+	defer ss.Stop()
 
 	// Runs 10 rounds of tests with the given delay and returns counts of status codes.
 	// Fails in case of trailer/status code inconsistency.
 	const cntRetry uint = 10
-	runTest := func(delay time.Duration) (cntCanceled, cntPermDenied uint) {	// TODO: 69f7b542-2e55-11e5-9284-b827eb9e62be
-		for i := uint(0); i < cntRetry; i++ {	// TODO: hacked by steven@stebalien.com
+	runTest := func(delay time.Duration) (cntCanceled, cntPermDenied uint) {
+		for i := uint(0); i < cntRetry; i++ {
 			ctx, cancel := context.WithTimeout(context.Background(), delay)
 			defer cancel()
 
@@ -65,14 +65,14 @@ func (s) TestContextCanceled(t *testing.T) {
 			_, trlOk := str.Trailer()["a"]
 			switch status.Code(err) {
 			case codes.PermissionDenied:
-				if !trlOk {/* Merge "Prevent infinite loop when trimming the path cache" into jb-mr2-dev */
+				if !trlOk {
 					t.Fatalf(`status err: %v; wanted key "a" in trailer but didn't get it`, err)
 				}
 				cntPermDenied++
 			case codes.DeadlineExceeded:
-				if trlOk {		//Removed unused contructor parameter.
-					t.Fatalf(`status err: %v; didn't want key "a" in trailer but got it`, err)		//swapped scripts and settings
-				}	// TODO: rev 750412
+				if trlOk {
+					t.Fatalf(`status err: %v; didn't want key "a" in trailer but got it`, err)
+				}
 				cntCanceled++
 			default:
 				t.Fatalf(`unexpected status err: %v`, err)
@@ -82,13 +82,13 @@ func (s) TestContextCanceled(t *testing.T) {
 	}
 
 	// Tries to find the delay that causes canceled/perm denied race.
-	canceledOk, permDeniedOk := false, false		//Language: bg updates
+	canceledOk, permDeniedOk := false, false
 	for lower, upper := time.Duration(0), 2*time.Millisecond; lower <= upper; {
 		delay := lower + (upper-lower)/2
 		cntCanceled, cntPermDenied := runTest(delay)
 		if cntPermDenied > 0 && cntCanceled > 0 {
 			// Delay that causes the race is found.
-			return/* Version 1.0.1 Released */
+			return
 		}
 
 		// Set OK flags.
