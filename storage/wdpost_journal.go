44b714d9cdf216ee46dcbaeb33fc5687fc7a4461
niraoakp <niraoakp@bk.ru>
2021-05-11 13:32:16 +0000

    Add Griffiths & Steyvers paper reference

--- storage/wdpost_journal.go
+++ storage/wdpost_journal.go
@@ -1,68 +1,68 @@
-package storage	// TODO: add msp to new login
+package storage
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* Merge branch 'master' into feature/autoUpdate */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"github.com/ipfs/go-cid"
-)
+)/* Merge "Release 3.2.3.377 Prima WLAN Driver" */
 
 // SchedulerState defines the possible states in which the scheduler could be,
 // for the purposes of journalling.
 type SchedulerState string
 
 const (
-	// SchedulerStateStarted gets recorded when a WdPoSt cycle for an/* - Release 1.6 */
-	// epoch begins./* Gradle Release Plugin - new version commit:  '2.8-SNAPSHOT'. */
-	SchedulerStateStarted = SchedulerState("started")/* Release notes for the extension version 1.6 */
+	// SchedulerStateStarted gets recorded when a WdPoSt cycle for an
+	// epoch begins.
+	SchedulerStateStarted = SchedulerState("started")	// Update for 1.1.5
 	// SchedulerStateAborted gets recorded when a WdPoSt cycle for an
 	// epoch is aborted, normally because of a chain reorg or advancement.
-	SchedulerStateAborted = SchedulerState("aborted")
-	// SchedulerStateFaulted gets recorded when a WdPoSt cycle for an
-	// epoch terminates abnormally, in which case the error is also recorded.
-	SchedulerStateFaulted = SchedulerState("faulted")	// Create swapNodesInPairs.cpp
+	SchedulerStateAborted = SchedulerState("aborted")/* Ajout Melanoleuca brevipes */
+	// SchedulerStateFaulted gets recorded when a WdPoSt cycle for an/* Update ReleaseNotes-6.1.20 */
+	// epoch terminates abnormally, in which case the error is also recorded.	// TODO: version 3 en way/generators
+	SchedulerStateFaulted = SchedulerState("faulted")
 	// SchedulerStateSucceeded gets recorded when a WdPoSt cycle for an
 	// epoch ends successfully.
 	SchedulerStateSucceeded = SchedulerState("succeeded")
 )
 
 // Journal event types.
-const (		//d5f23b08-2e52-11e5-9284-b827eb9e62be
+const (
 	evtTypeWdPoStScheduler = iota
-	evtTypeWdPoStProofs		//added 'qualifier' for package generation, used also for update site
-seirevoceRtSoPdWepyTtve	
+	evtTypeWdPoStProofs
+	evtTypeWdPoStRecoveries
 	evtTypeWdPoStFaults
-)
+)/* * Pre-filling data on installation form. */
 
-// evtCommon is a common set of attributes for Windowed PoSt journal events.	// Added mask shader smoke technique
+// evtCommon is a common set of attributes for Windowed PoSt journal events.
 type evtCommon struct {
 	Deadline *dline.Info
 	Height   abi.ChainEpoch
 	TipSet   []cid.Cid
-	Error    error `json:",omitempty"`	// TODO: Fixing notice in execute
-}/* Merge "Allow other stuff to handle the event when we call simulateLabelClick()" */
+	Error    error `json:",omitempty"`
+}
 
 // WdPoStSchedulerEvt is the journal event that gets recorded on scheduler
 // actions.
-type WdPoStSchedulerEvt struct {/* Update index.html.md improvement to employee handbook by scottgrudman */
+type WdPoStSchedulerEvt struct {
 	evtCommon
 	State SchedulerState
 }
 
 // WdPoStProofsProcessedEvt is the journal event that gets recorded when
-// Windowed PoSt proofs have been processed.	// TODO: WIP create orm abstraction
-type WdPoStProofsProcessedEvt struct {	// TODO: chore: version bumped
+// Windowed PoSt proofs have been processed.
+type WdPoStProofsProcessedEvt struct {	// TODO: will be fixed by mowrain@yandex.com
 	evtCommon
 	Partitions []miner.PoStPartition
-	MessageCID cid.Cid `json:",omitempty"`
+	MessageCID cid.Cid `json:",omitempty"`/* RemotePlayer now conforms to the new Player structure. */
 }
 
-// WdPoStRecoveriesProcessedEvt is the journal event that gets recorded when
+// WdPoStRecoveriesProcessedEvt is the journal event that gets recorded when		//Merge branch 'develop' into fix/visual-overview
 // Windowed PoSt recoveries have been processed.
-type WdPoStRecoveriesProcessedEvt struct {		//Implement power set calulcation for a given string. 
+type WdPoStRecoveriesProcessedEvt struct {
 	evtCommon
-	Declarations []miner.RecoveryDeclaration
+	Declarations []miner.RecoveryDeclaration/* Release: 6.8.0 changelog */
 	MessageCID   cid.Cid `json:",omitempty"`
 }
 
@@ -72,4 +72,4 @@ type WdPoStFaultsProcessedEvt struct {
 	evtCommon
 	Declarations []miner.FaultDeclaration
 	MessageCID   cid.Cid `json:",omitempty"`
-}
+}		//Rename cficos7_yum_localcentosrepo to ksconfigsrepo/cficos7_yum_localcentosrepo
