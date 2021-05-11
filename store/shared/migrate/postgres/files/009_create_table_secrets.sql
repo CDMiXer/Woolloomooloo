ca4a2665139832091f8f2169149f0ddfdf9788a3
niraoakp <niraoakp@bk.ru>
2021-05-11 20:04:36 +0000

    JsS Semantic analyzer and code generator.

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,19 +1,19 @@
--- name: create-table-secrets
-
-CREATE TABLE IF NOT EXISTS secrets (
+-- name: create-table-secrets/* Release 0.2.8.1 */
+	// TODO: hacked by martin2cai@hotmail.com
+CREATE TABLE IF NOT EXISTS secrets (	// TODO: Add Hashley app to Showcase
  secret_id                SERIAL PRIMARY KEY
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN/* Release mediaPlayer in VideoViewActivity. */
-,UNIQUE(secret_repo_id, secret_name)/* v0.2.3 - Release badge fixes */
+,secret_pull_request      BOOLEAN/* Update form.xml */
+,secret_pull_request_push BOOLEAN
+,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);/* determine the scope from self object */
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
 
