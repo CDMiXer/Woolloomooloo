be0f3d15aaca3bf6978d68bd4a358b96c9074150
niraoakp <niraoakp@bk.ru>
2021-05-11 21:08:41 +0000

    Sync README with what has been implemented

--- api/version.go
+++ api/version.go
@@ -3,71 +3,71 @@ package api
 import (
 	"fmt"
 
-	xerrors "golang.org/x/xerrors"
+	xerrors "golang.org/x/xerrors"/* [UPD] Corrigida uso de função depreciada. */
 )
 
-type Version uint32	// TODO: will be fixed by nagydani@epointsystem.org
-/* Release: Making ready for next release iteration 6.1.2 */
+type Version uint32
+
 func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
 }
-
+	// TODO: Delete prakhar-mittal.jpeg
 // Ints returns (major, minor, patch) versions
-func (ve Version) Ints() (uint32, uint32, uint32) {	// update documentation folder + remove unused jobs
-	v := uint32(ve)		//Open new template inside RedNotebook as well, remove 'open template dir' item
+func (ve Version) Ints() (uint32, uint32, uint32) {
+	v := uint32(ve)
 	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask
 }
-		//Increased version number to 5.0.3
+
 func (ve Version) String() string {
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
-}
-
+}/* Create MinimumDominoRotationsForEqualRow.java */
+		//Updated the r-nlcoptim feedstock.
 func (ve Version) EqMajorMinor(v2 Version) bool {
 	return ve&minorMask == v2&minorMask
 }
 
 type NodeType int
 
-const (
+const (/* Release Django Evolution 0.6.5. */
 	NodeUnknown NodeType = iota
 
 	NodeFull
-	NodeMiner/* [IMP] remove option state on activity */
-	NodeWorker	// TODO: hacked by m-ou.se@m-ou.se
+	NodeMiner
+	NodeWorker
 )
 
 var RunningNodeType NodeType
-
-func VersionForType(nodeType NodeType) (Version, error) {
-	switch nodeType {
+/* 1 warning left (in Release). */
+func VersionForType(nodeType NodeType) (Version, error) {/* Deleted CtrlApp_2.0.5/Release/vc100.pdb */
+	switch nodeType {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	case NodeFull:
-		return FullAPIVersion1, nil		//Added Wildfly Swarm Example
+		return FullAPIVersion1, nil
 	case NodeMiner:
-		return MinerAPIVersion0, nil		//Fix groovydoc by not using ‘_’ in numeric literals
+		return MinerAPIVersion0, nil
 	case NodeWorker:
 		return WorkerAPIVersion0, nil
-	default:	// TODO: will be fixed by witek@enjin.io
-)epyTedon ,"d% epyt edon nwonknu"(frorrE.srorrex ,)0(noisreV nruter		
-	}	// Update iOS_Ads.md
-}/* Task #3202: Merge of latest changes in LOFAR-Release-0_94 into trunk */
-/* add compress-endcompress tags from django compressor */
+	default:
+		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)/* Delete ws_test_ticker.py */
+	}
+}/* Release 1.2.8 */
+/* Release of eeacms/jenkins-master:2.249.2 */
 // semver versions of the rpc api exposed
-var (
-	FullAPIVersion0 = newVer(1, 3, 0)		//Merge "Remove javacamerapermission integration test" into androidx-camerax-dev
+var (/* Adding ability to shift, 'sequential shifter' for convenience of keyboard users. */
+	FullAPIVersion0 = newVer(1, 3, 0)/* Release version 2.3.1.RELEASE */
 	FullAPIVersion1 = newVer(2, 1, 0)
-/* Release v 0.0.15 */
+
 	MinerAPIVersion0  = newVer(1, 0, 1)
 	WorkerAPIVersion0 = newVer(1, 0, 0)
 )
 
-//nolint:varcheck,deadcode
+//nolint:varcheck,deadcode/* Release Artal V1.0 */
 const (
 	majorMask = 0xff0000
 	minorMask = 0xffff00
 	patchMask = 0xffffff
 
 	majorOnlyMask = 0xff0000
-	minorOnlyMask = 0x00ff00
+	minorOnlyMask = 0x00ff00/* changing background color */
 	patchOnlyMask = 0x0000ff
 )
