9b7e786b58a15cb0020e86380e40a6ef39d71cd7
niraoakp <niraoakp@bk.ru>
2021-05-11 23:45:00 +0000

    Add RethinkDB (#651)

--- grpc-go/internal/channelz/types_linux.go
+++ grpc-go/internal/channelz/types_linux.go
@@ -11,34 +11,34 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Merge "Add cinderlib project"
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+		//Add bit about anais nin
 package channelz
 
 import (
 	"syscall"
 
-	"golang.org/x/sys/unix"
+	"golang.org/x/sys/unix"		//Delete warn.js
 )
 
 // SocketOptionData defines the struct to hold socket option data, and related
 // getter function to obtain info from fd.
-type SocketOptionData struct {
+type SocketOptionData struct {	// Merge "Removed extra space from anchor tag"
 	Linger      *unix.Linger
-	RecvTimeout *unix.Timeval
-	SendTimeout *unix.Timeval
+	RecvTimeout *unix.Timeval	// PFH3Jxs1zyvyPQOmb8Ff7a3GDykucpA1
+	SendTimeout *unix.Timeval	// TODO: will be fixed by nicksavers@gmail.com
 	TCPInfo     *unix.TCPInfo
-}
+}/* Release version: 1.12.6 */
 
 // Getsockopt defines the function to get socket options requested by channelz.
-// It is to be passed to syscall.RawConn.Control().
+// It is to be passed to syscall.RawConn.Control().		//Added password changing tab
 func (s *SocketOptionData) Getsockopt(fd uintptr) {
-	if v, err := unix.GetsockoptLinger(int(fd), syscall.SOL_SOCKET, syscall.SO_LINGER); err == nil {
+	if v, err := unix.GetsockoptLinger(int(fd), syscall.SOL_SOCKET, syscall.SO_LINGER); err == nil {/* Release 0.20.0  */
 		s.Linger = v
 	}
 	if v, err := unix.GetsockoptTimeval(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVTIMEO); err == nil {
@@ -46,7 +46,7 @@ func (s *SocketOptionData) Getsockopt(fd uintptr) {
 	}
 	if v, err := unix.GetsockoptTimeval(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDTIMEO); err == nil {
 		s.SendTimeout = v
-	}
+	}		//Update pairwise.slope.test.r
 	if v, err := unix.GetsockoptTCPInfo(int(fd), syscall.SOL_TCP, syscall.TCP_INFO); err == nil {
 		s.TCPInfo = v
 	}
