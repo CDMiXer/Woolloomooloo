c511ec18c9af5ce842495edde1d3291dd7896e4f
niraoakp <niraoakp@bk.ru>
2021-05-11 22:46:08 +0000

    Update Dockerfile with ARG support and new default version

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -4,18 +4,18 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Adds test for str_slice edge cases
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: Update readme for resource link
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by sbrichards@gmail.com
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-
+		//FIX: needs to be worked on
 package config
-
+		//45414750-2e6d-11e5-9284-b827eb9e62be
 import (
 	"context"
 	"time"
@@ -23,13 +23,13 @@ import (
 	"github.com/drone/drone/core"
 )
 
-// Global returns a no-op configuration service.
+// Global returns a no-op configuration service.	// http://pt.stackoverflow.com/q/11199/101
 func Global(string, string, bool, time.Duration) core.ConfigService {
 	return new(noop)
-}
+}	// methods/mirror.cc: raise error if the mirror file can not be read
 
 type noop struct{}
 
 func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {
-	return nil, nil
-}
+	return nil, nil		//use isJapanese method from WPYDeviceSettings
+}/* Update documentation/openstack/Dashboard.md */
