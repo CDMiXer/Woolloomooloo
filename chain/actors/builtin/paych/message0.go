1393b4dbcfaa4d2ea61254fc85d19822e255aa16
niraoakp <niraoakp@bk.ru>
2021-05-11 12:46:52 +0000

    Link box interface

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -2,47 +2,47 @@ package paych
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Added js and css files.
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"	// include icon/count of times seen for repeat performers
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* [CHANGELOG] Release 0.1.0 */
+
 type message0 struct{ from address.Address }
 
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {		//Merge branch 'develop' into feature/remove-old-predict-routes
+	if aerr != nil {
 		return nil, aerr
 	}
-	enc, aerr := actors.SerializeParams(&init0.ExecParams{/* Release for v6.5.0. */
-		CodeCID:           builtin0.PaymentChannelActorCodeID,	// Implement VFCAP_FLIP for vo_vdpau.
+	enc, aerr := actors.SerializeParams(&init0.ExecParams{
+		CodeCID:           builtin0.PaymentChannelActorCodeID,
 		ConstructorParams: params,
-	})	// TODO: hacked by vyzo@hackzen.org
+	})
 	if aerr != nil {
-		return nil, aerr/* #180 - Release version 1.7.0 RC1 (Gosling). */
-	}		//Ajout getDestination
+		return nil, aerr
+	}
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
-		Method: builtin0.MethodsInit.Exec,/* Changed Python invocation to python3 */
-		Params: enc,/* ReleaseNotes: try to fix links */
-	}, nil	// TODO: Updated the r-htmltable feedstock.
-}/* Fixed crash in event targets */
-		//Adds bulleted list
+		Method: builtin0.MethodsInit.Exec,
+		Params: enc,
+	}, nil
+}
+
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{		//Merge "Fix displaying of devref for TestModelsMigrations"
+	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {/* @Release [io7m-jcanephora-0.9.1] */
+	if aerr != nil {
 		return nil, aerr
 	}
 
