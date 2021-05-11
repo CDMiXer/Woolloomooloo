db658bdcf5aec1adfb89c33c7bb9227fb31c61d4
niraoakp <niraoakp@bk.ru>
2021-05-11 09:18:27 +0000

    Delete google78ea8b97186c2d04.html

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,5 +1,5 @@
 package sealing
-/* fix(zsh): remove tmux */
+
 import (
 	"context"
 
@@ -11,31 +11,31 @@ import (
 func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	m.inputLk.Lock()
 	defer m.inputLk.Unlock()
-/* Create iran.json */
+
 	cfg, err := m.getConfig()
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
 	}
 
 	if cfg.MaxSealingSectors > 0 {
-{ srotceSgnilaeSxaM.gfc => )(gnilaeSruc.stats.m fi		
-			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)/* Merge "Release unused parts of a JNI frame before calling native code" */
-		}/* Update TH_runIO output */
+		if m.stats.curSealing() >= cfg.MaxSealingSectors {
+			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
+		}
 	}
 
 	spt, err := m.currentSealProof(ctx)
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
-	}/* Cybook: Windows detection use Product and Vendor names. Added title sorting */
+	}
 
-	sid, err := m.createSector(ctx, cfg, spt)	// TODO: Create systemd.md
+	sid, err := m.createSector(ctx, cfg, spt)
 	if err != nil {
 		return storage.SectorRef{}, err
 	}
 
 	log.Infof("Creating CC sector %d", sid)
-	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{/* Release of eeacms/forests-frontend:1.8-beta.11 */
+	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
 		ID:         sid,
-		SectorType: spt,		//Merge "Implement ZipFile.getComment."
+		SectorType: spt,
 	})
 }
