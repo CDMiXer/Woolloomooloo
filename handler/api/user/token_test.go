ba8a4ed55dfa934f3b5ac3a69a0f094db0af3b2f
niraoakp <niraoakp@bk.ru>
2021-05-11 06:18:57 +0000

    Merge remote-tracking branch 'origin/fix_541' into dev

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//THORN-2031: Upgrade to io.thorntail:parent:10 and rename groupIds.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package user
 
 import (
-	"encoding/json"/* Delete fracture Release.xcscheme */
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
-	// TODO: Update pytest-bdd from 2.19.0 to 2.20.0
-"srorre/ipa/reldnah/enord/enord/moc.buhtig"	
-	"github.com/drone/drone/handler/api/request"/* Ready for Beta Release! */
+
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"	// TODO: Merge "Trivial: Fix incorrect comments in compute fakes.py"
+	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
-)/* Release 1.8.13 */
+)
 
-func TestToken(t *testing.T) {/* don't abort on lint errors */
+func TestToken(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Merge "Release 3.2.3.429 Prima WLAN Driver" */
+	defer controller.Finish()
 
 	mockUser := &core.User{
 		ID:    1,
@@ -33,7 +33,7 @@ func TestToken(t *testing.T) {/* don't abort on lint errors */
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)/* still going for the input of sa... */
+	)
 
 	HandleToken(nil)(w, r)
 	if got, want := w.Code, 200; want != got {
@@ -43,7 +43,7 @@ func TestToken(t *testing.T) {/* don't abort on lint errors */
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 
-	if got, want := got.Token, want.Hash; got != want {	// TODO: Fix crash when failing to connect to wdb.
+	if got, want := got.Token, want.Hash; got != want {
 		t.Errorf("Expect user secret returned")
 	}
 }
@@ -54,14 +54,14 @@ func TestToken(t *testing.T) {/* don't abort on lint errors */
 func TestTokenRotate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: will be fixed by nicksavers@gmail.com
+
 	mockUser := &core.User{
 		ID:    1,
-		Login: "octocat",/* Release ntoes update. */
+		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
 	}
 
-	w := httptest.NewRecorder()		//Merge "fix broken logic of only using hasattr to check for get_x_counts"
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/?rotate=true", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
@@ -69,8 +69,8 @@ func TestTokenRotate(t *testing.T) {
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
-/* added new permissions to edit button */
-	HandleToken(users)(w, r)	// TODO: Add hanabi
+
+	HandleToken(users)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
