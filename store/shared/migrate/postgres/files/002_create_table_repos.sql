126cb5fc7b53d88ec688cb7a80017ffc227614d6
niraoakp <niraoakp@bk.ru>
2021-05-11 08:25:15 +0000

    update to ESR78

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
 -- name: create-table-repos
-
+/* Release note for #942 */
 CREATE TABLE IF NOT EXISTS repos (
- repo_id                    SERIAL PRIMARY KEY/* mapred client: atomic tgz upload */
-,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER
+ repo_id                    SERIAL PRIMARY KEY		//fix(CI): install build pre-requisites
+,repo_uid                   VARCHAR(250)/* Release version 3.1.1.RELEASE */
+,repo_user_id               INTEGER/* Update pom and config file for First Release 1.0 */
 ,repo_namespace             VARCHAR(250)
-,repo_name                  VARCHAR(250)
+,repo_name                  VARCHAR(250)/* Release 0.2.0 \o/. */
 ,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)
+,repo_scm                   VARCHAR(50)	// TODO: hacked by why@ipfs.io
 ,repo_clone_url             VARCHAR(2000)
-,repo_ssh_url               VARCHAR(2000)
+,repo_ssh_url               VARCHAR(2000)	// Interpretando a transformação springn.
 ,repo_html_url              VARCHAR(2000)
-,repo_active                BOOLEAN/* try to use grid */
-,repo_private               BOOLEAN/* Rename bootstrap.min.css to bootstrap.min.css.new */
-,repo_visibility            VARCHAR(50)		//Merge branch 'master' into dependabot/pip/app/coverage-5.5
+,repo_active                BOOLEAN
+,repo_private               BOOLEAN
+,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
-,repo_config                VARCHAR(500)/* Merge "Release 3.2.3.455 Prima WLAN Driver" */
-,repo_timeout               INTEGER	// TODO: will be fixed by mikeal.rogers@gmail.com
+,repo_config                VARCHAR(500)
+,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER	// TODO: hacked by souzau@yandex.com
-,repo_updated               INTEGER/* Release version 4.0.0.M3 */
+,repo_created               INTEGER	// TODO: Acrescentado configuração de e-mail de notificação
+,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
-,repo_secret                VARCHAR(50)
-,UNIQUE(repo_slug)/* rev 561380 */
-,UNIQUE(repo_uid)/* notes on big o notation, design patterns, postgres and sql */
+,repo_secret                VARCHAR(50)/* Release areca-7.4.1 */
+,UNIQUE(repo_slug)
+,UNIQUE(repo_uid)
 );
 
 -- name: alter-table-repos-add-column-no-fork
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;	// TODO: certifi v0.0.4
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-cancel-pulls/* Now really fix typo */
+-- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
-
+/* Release version 4.0.0.12. */
 -- name: alter-table-repos-add-column-cancel-push
-
+/* Fix release version in ReleaseNote */
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
