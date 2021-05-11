601b1ec9d3edf05d2ed78cf92f93d7bd5a42b034
niraoakp <niraoakp@bk.ru>
2021-05-11 08:21:38 +0000

    Fixed tests to account for failing slash in charmworld-url

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !nolimit
+// +build !nolimit	// TODO: hacked by yuvalalaluf@gmail.com
 // +build !oss
 
 package license
