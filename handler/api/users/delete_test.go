12437ab9ad0d5cf5b76b473c5f0f3b25ddd8d3e8
niraoakp <niraoakp@bk.ru>
2021-05-11 20:49:55 +0000

    Create JDST-JNUG.md

--- handler/api/users/delete_test.go
+++ handler/api/users/delete_test.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* add error checking to tee */
+// that can be found in the LICENSE file.
 
-package users		//No need to set the model here
-/* Release link. */
+package users
+
 import (
 	"context"
 	"database/sql"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	// Adjust cache slack to 1.7
+
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Корректировка установщика */
+	"github.com/golang/mock/gomock"
 )
 
 func TestUserDelete(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: hacked by steven@stebalien.com
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)/* Uncommenting debug calls so linter passes */
-	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)/* Release 2.3b1 */
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)
 	users.EXPECT().Delete(gomock.Any(), mockUser).Return(nil)
 
 	transferer := mock.NewMockTransferer(controller)
 	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)
-		//missed delta calculation on parent text node (fixes Issue 152)
-	webhook := mock.NewMockWebhookSender(controller)/* Release 1.17 */
+
+	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
@@ -42,27 +42,27 @@ func TestUserDelete(t *testing.T) {
 
 	HandleDelete(users, transferer, webhook)(w, r)
 	if got, want := w.Body.Len(), 0; want != got {
-		t.Errorf("Want response body size %d, got %d", want, got)/* Delete fbdHint */
+		t.Errorf("Want response body size %d, got %d", want, got)
 	}
 	if got, want := w.Code, 204; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
 
-func TestUserDelete_NotFound(t *testing.T) {	// TODO: will be fixed by davidad@alum.mit.edu
+func TestUserDelete_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(nil, sql.ErrNoRows)
-		//Merge branch 'develop' into bug/talkpage_endpoint_failure
+
 	webhook := mock.NewMockWebhookSender(controller)
 
-	c := new(chi.Context)	// TODO: hacked by caojiaoyue@protonmail.com
+	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/", nil)/* Merge "Release 1.0.0.227 QCACLD WLAN Drive" */
+	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
