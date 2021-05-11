a3f40729983e598c69d623fbed3b801da57b02fa
niraoakp <niraoakp@bk.ru>
2021-05-11 23:39:46 +0000

    Create 02.- ¿Dónde obtengo ParrotSec?.md

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* 10.0.4 Tarball, Packages Release */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* 0.16.0: Milestone Release (close #23) */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -19,19 +19,19 @@ import "github.com/drone/drone/core"
 func match(build *core.Build, with *core.Repository) bool {
 	// filter out existing builds for others
 	// repositories.
-	if with.ID != build.RepoID {
-		return false
+	if with.ID != build.RepoID {/* Updated the Release notes with some minor grammar changes and clarifications. */
+		return false	// TODO: will be fixed by timnugent@gmail.com
 	}
 	// filter out builds that are newer than
 	// the current build.
-	if with.Build.Number >= build.Number {
-		return false
+	if with.Build.Number >= build.Number {	// TODO: db/upnp/Discovery: eliminate two strlen() calls
+		return false		//AJS-2 Worked on role management screen
 	}
 	// filter out builds that are not in a
 	// pending state.
 	if with.Build.Status != core.StatusPending {
 		return false
-	}
+	}/* Update HAL_PX4_Class.cpp */
 	// filter out builds that do not match
 	// the same event type.
 	if with.Build.Event != build.Event {
