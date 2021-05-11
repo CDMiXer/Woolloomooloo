050e416da6cf5f119538193f2676870067ae7b34
niraoakp <niraoakp@bk.ru>
2021-05-11 21:03:00 +0000

    Changes getFiles() to return empty stack arrays instead of boolean false

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
-	// fca3046e-2e41-11e5-9284-b827eb9e62be
-CREATE TABLE IF NOT EXISTS latest (/* Create FiveRolePlay */
- latest_repo_id  INTEGER
+
+CREATE TABLE IF NOT EXISTS latest (
+ latest_repo_id  INTEGER	// objc grammar
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
-,latest_updated  INTEGER		//change modules.config.php
-,latest_deleted  INTEGER	// Re-enabled optimization in FranEtAlDotProduct
+,latest_updated  INTEGER
+,latest_deleted  INTEGER	// TODO: working on lesson 18
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
-
+	// TODO: hacked by ng8eke@163.com
 -- name: create-index-latest-repo
 
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
