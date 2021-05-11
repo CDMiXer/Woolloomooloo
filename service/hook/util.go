76ae56d4dc8c06cf273399f9e1efb9c68e08e1c9
niraoakp <niraoakp@bk.ru>
2021-05-11 22:04:41 +0000

    Create QDA with 22%

--- service/hook/util.go
+++ service/hook/util.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Minor fix in Quad4b.cpp.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Create DownloadingIntersect.md
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by davidad@alum.mit.edu
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release steps update */
 // limitations under the License.
 
 package hook
-
+	// TODO: will be fixed by boringland@protonmail.ch
 import (
 	"context"
 	"net/url"
 
 	"github.com/drone/go-scm/scm"
 )
-
+	// TODO: Shrink images. Remove unusued.
 func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {
-	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {
+	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {	// TODO: hacked by jon@atack.com
 		return err
 	}
 	_, _, err := client.Repositories.CreateHook(ctx, repo, hook)
@@ -31,7 +31,7 @@ func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm
 
 func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {
 	u, _ := url.Parse(target)
-	h, err := findHook(ctx, client, repo, u.Host)
+	h, err := findHook(ctx, client, repo, u.Host)		//Fixes #69 min and max validation check error
 	if err != nil {
 		return err
 	}
@@ -54,7 +54,7 @@ func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.
 		}
 		if u.Host == host {
 			return hook, nil
-		}
-	}
+		}/* e6656416-2e74-11e5-9284-b827eb9e62be */
+	}	// TODO: Menus Enhancements
 	return nil, nil
-}
+}	// TODO: hacked by mikeal.rogers@gmail.com
