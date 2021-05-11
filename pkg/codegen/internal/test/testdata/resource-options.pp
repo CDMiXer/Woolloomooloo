ca4e23bd767ea19bf8eb3c1d4ca2df3549ea2637
niraoakp <niraoakp@bk.ru>
2021-05-11 03:46:21 +0000

    rev 741702

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,8 +1,8 @@
 resource provider "pulumi:providers:aws" {
-	region = "us-west-2"
-}
-
-resource bucket1 "aws:s3:Bucket" {
+	region = "us-west-2"	// TODO: Merge branch 'master' into feature/volume-retrieval
+}/* Fixed bugs where strings were not written correctly */
+	// TODO: Complete description of the method
+resource bucket1 "aws:s3:Bucket" {/* Remove unneeded structure info. */
 	options {
 		provider = provider
 		dependsOn = [provider]
