2a601825af2375fb9133d1963a76bf20e2d69c3f
niraoakp <niraoakp@bk.ru>
2021-05-11 14:56:18 +0000

    Add more explicit modes, now less unsafe

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,8 +1,8 @@
 package account
-
-import (
+/* Release 1.2.0.10 deployed */
+import (/* Delete main.dfm */
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: Disable page caching on the main article page.
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
@@ -12,8 +12,8 @@ import (
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)
+	out := state3{store: store}	// added basic support for IntelHex 16-bit mode
+	err := store.Get(store.Context(), root, &out)		//Delete MNIST Sample
 	if err != nil {
 		return nil, err
 	}
@@ -23,7 +23,7 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 type state3 struct {
 	account3.State
 	store adt.Store
-}
+}	// TODO: will be fixed by fjl@ethereum.org
 
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
