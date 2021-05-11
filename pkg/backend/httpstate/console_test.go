8328d93224e7673f61593a3db88d72f27b388f4c
niraoakp <niraoakp@bk.ru>
2021-05-11 16:17:25 +0000

    Ring identifier middleware takes Router, not routes

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation./* fix up the image references */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge branch 'master' into mtageld-dev */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Rename Reflection 0 to reflection0.md */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Clarified option merging
 // limitations under the License.
 package httpstate
-	// Add rasp project link in README
+
 import (
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-		//Implement used-file detection, so open, unused files no longer interfere
+
 func TestConsoleURL(t *testing.T) {
 	t.Run("HonorEnvVar", func(t *testing.T) {
 		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
@@ -34,31 +34,31 @@ func TestConsoleURL(t *testing.T) {
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 
 		// Unset the variable, confirm the "standard behavior" where we
-		// replace "api." with "app.".		//Add public List<Info> listaAmigosDestacados()
-		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")/* Released v0.1.11 (closes #142) */
+		// replace "api." with "app.".		//make ADEngineBlock work with cocoapods
+		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")
 		assert.Equal(t,
-			"https://app.pulumi.contoso.com/1/2",
+			"https://app.pulumi.contoso.com/1/2",/* Release 4.0.2dev */
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
-	})
+	})	// TODO: will be fixed by alan.shaw@protocol.ai
 
 	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
-		assert.Equal(t,/* Released version 0.9.0 */
+		assert.Equal(t,
 			"https://app.pulumi.com/pulumi-bot/my-stack",
 			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
 
 		assert.Equal(t,
-			"http://app.pulumi.example.com/pulumi-bot/my-stack",
+			"http://app.pulumi.example.com/pulumi-bot/my-stack",/* Release making ready for next release cycle 3.1.3 */
 			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
-	})	// Rename vedtægter.md to 5_vedtægter.md
+	})
 
 	t.Run("LocalDevelopment", func(t *testing.T) {
 		assert.Equal(t,
 			"http://localhost:3000/pulumi-bot/my-stack",
 			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))
-	})
+	})	// TODO: About dialog moves to an activity.
 
 	t.Run("ConsoleDomainUnknown", func(t *testing.T) {
 		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))
-		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))	// Moved source descriptors to a mapping file
-	})
-}
+		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))
+	})	// TODO: Change in describing terms for being newly arrived
+}	// TODO: Parser, ParserState
