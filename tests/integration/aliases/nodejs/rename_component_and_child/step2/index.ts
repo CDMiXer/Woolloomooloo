1b443b576919678972960745086b34e8fb654b3c
niraoakp <niraoakp@bk.ru>
2021-05-11 02:03:24 +0000

    Add $options handling to filter_by_view

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* rev 621921 */
-import * as pulumi from "@pulumi/pulumi";	// TODO: Update collectfast from 1.0.0 to 1.1.0
-/* changed the dconf key name to avoid confusion */
+
+import * as pulumi from "@pulumi/pulumi";
+
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}/* Release of eeacms/www-devel:19.12.10 */
+}
 
-// Scenario #5 - composing #1 and #3/* [artifactory-release] Release version 1.2.0.M2 */
+// Scenario #5 - composing #1 and #3
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Created new electron project */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
