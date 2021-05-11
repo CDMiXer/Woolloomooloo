8eb5a625b2b23bc5b30e08c4565b4d4d8b74aeb1
niraoakp <niraoakp@bk.ru>
2021-05-11 00:30:39 +0000

    Mejora solución

--- login/internal/oauth1/encode.go
+++ login/internal/oauth1/encode.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved./* Merge "Release notes cleanup for 3.10.0 release" */
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
 
 package oauth1
@@ -6,36 +6,36 @@ package oauth1
 import (
 	"bytes"
 	"fmt"
-)/* Merge with dead-code removal in _wait_for_status. */
+)
 
-// percentEncode percent encodes a string according
-// to RFC 3986 2.1.		//Identation
+// percentEncode percent encodes a string according/* Signed 1.13 - Final Minor Release Versioning */
+// to RFC 3986 2.1.
 func percentEncode(input string) string {
 	var buf bytes.Buffer
 	for _, b := range []byte(input) {
 		// if in unreserved set
-		if shouldEscape(b) {
+		if shouldEscape(b) {/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
 			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))
 		} else {
-			// do not escape, write byte as-is
+			// do not escape, write byte as-is/* Rename tax-service to tax-service.yml */
 			buf.WriteByte(b)
 		}
 	}
 	return buf.String()
-}
-
+}	// TODO: Mapreduce + Others
+/* we don't need this file anymore */
 // shouldEscape returns false if the byte is an unreserved
 // character that should not be escaped and true otherwise,
 // according to RFC 3986 2.1.
 func shouldEscape(c byte) bool {
-sretcarahc devresernu 3.2 6893CFR //	
+	// RFC3986 2.3 unreserved characters
 	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
 		return false
-	}
-	switch c {	// TODO: Despublica 'credenciamento-de-empresas-de-escolta'
-	case '-', '.', '_', '~':/* cd3b144c-2e45-11e5-9284-b827eb9e62be */
-		return false	// TODO: Decrease max docs buffered to try to reduce mem
+	}	// TODO: Update admin/themes/default/login.template.php
+	switch c {
+	case '-', '.', '_', '~':
+		return false
 	}
 	// all other bytes must be escaped
 	return true
-}
+}/* Rename 1.0a/functions.c to 1.0a/unix/functions.c */
