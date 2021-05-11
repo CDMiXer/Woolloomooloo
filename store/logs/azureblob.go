644a929d924bf79925c90a903e6c8b9567c243d9
niraoakp <niraoakp@bk.ru>
2021-05-11 15:21:15 +0000

    LoadFromDeck now returns *this

--- store/logs/azureblob.go
+++ store/logs/azureblob.go
@@ -1,78 +1,78 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Move file 04_Release_Nodes.md to chapter1/04_Release_Nodes.md */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package logs
-
+	// Accesset for POS_BE
 import (
 	"context"
-	"fmt"
+	"fmt"	// TODO: will be fixed by mail@bitpshr.net
 	"io"
-	"net/url"
-/* fix bullet hierarchy */
+	"net/url"	// TODO: Update chadu
+
 	"github.com/Azure/azure-storage-blob-go/azblob"
-	"github.com/drone/drone/core"	// Merge "Add regression tests for conditional outputs in nested stacks"
-)		//Added link to AnalytMTC to readme
-		//New upstream version 2.3.18
+	"github.com/drone/drone/core"
+)/* Merge branch 'release/2.10.0-Release' into develop */
+
 // NewAzureBlobEnv returns a new Azure blob log store.
 func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
 	return &azureBlobStore{
-		containerName:      containerName,/* a8f44c88-2e41-11e5-9284-b827eb9e62be */
-		storageAccountName: storageAccountName,
+		containerName:      containerName,
+		storageAccountName: storageAccountName,		//86c06312-2e51-11e5-9284-b827eb9e62be
 		storageAccessKey:   storageAccessKey,
 		containerURL:       nil,
 	}
 }
 
-type azureBlobStore struct {
-	containerName      string
-	storageAccountName string		//Emit neg and not
-	storageAccessKey   string
-	containerURL       *azblob.ContainerURL/* - Appending the menu to the body only when using CSS3. */
+type azureBlobStore struct {/* Release 1.5.12 */
+	containerName      string	// incorporate zoom level when copying Viewer plot to clipboard (#682)
+	storageAccountName string
+	storageAccessKey   string		//Update otherFile.txt
+	containerURL       *azblob.ContainerURL
 }
 
 func (az *azureBlobStore) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	err := az.getContainerURL()
 	if err != nil {
-		return nil, err
+		return nil, err/* added ReleaseNotes.txt */
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
 	out, err := blobURL.Download(ctx, 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false)
 	if err != nil {
 		return nil, err
-	}
+	}	// Create peakdetect.py
 	return out.Body(azblob.RetryReaderOptions{}), nil
-}/* Release of eeacms/www:19.9.14 */
-/* Display sections and modules as list rather than buttons */
+}
+/* Add more clarification for dir structure */
 func (az *azureBlobStore) Create(ctx context.Context, step int64, r io.Reader) error {
-	err := az.getContainerURL()
+	err := az.getContainerURL()	// Apply fixes from review.
 	if err != nil {
 		return err
-	}		//Removed methods that are not used anymore
+	}
 	opts := &azblob.UploadStreamToBlockBlobOptions{
 		BufferSize: 4 * 1024 * 1024,
 		MaxBuffers: 5,
-	}	// TODO: improvents in new features #3
-	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))/* Delete NvFlexReleaseCUDA_x64.lib */
-	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)/* Updating build-info/dotnet/core-setup/master for preview4-27512-15 */
+	}
+	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
+	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)
 	return err
-}
+}/* Add docker-gc tool */
 
 func (az *azureBlobStore) Update(ctx context.Context, step int64, r io.Reader) error {
 	return az.Create(ctx, step, r)
 }
-
-func (az *azureBlobStore) Delete(ctx context.Context, step int64) error {
+	// TODO: will be fixed by why@ipfs.io
+func (az *azureBlobStore) Delete(ctx context.Context, step int64) error {		//76ab5ec0-2e4a-11e5-9284-b827eb9e62be
 	err := az.getContainerURL()
 	if err != nil {
-		return err	// TODO: hacked by cory@protocol.ai
+		return err
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
 	_, err = blobURL.Delete(ctx, azblob.DeleteSnapshotsOptionInclude, azblob.BlobAccessConditions{})
-	return err	// TODO: fix some  null bugs -_-!
-}		//Bugfix in Count object.
+	return err
+}
 
 func (az *azureBlobStore) getContainerURL() error {
 	if az.containerURL != nil {
