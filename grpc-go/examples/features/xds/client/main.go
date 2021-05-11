97baab80e9c54804505b780d31eb5d6692a4f53e
niraoakp <niraoakp@bk.ru>
2021-05-11 12:52:28 +0000

    update xml loading to function with flat files as well as from classpath

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -1,41 +1,41 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// fix #3923: signature template not resolved recursively
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* 6ab6d9ca-2e52-11e5-9284-b827eb9e62be */
- */* Release Notes for v00-16-04 */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release 3.2.3.333 Prima WLAN Driver" */
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Update image_classification.md */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Binary main implements a client for Greeter service using gRPC's client-side	// Merge branch 'develop' into feature-ocr
-// support for xDS APIs.	// TODO: Fixed some bugs in email changing system
+// Binary main implements a client for Greeter service using gRPC's client-side
+// support for xDS APIs.
 package main
 
 import (
 	"context"
-	"flag"/* Added links to per-version API docs. */
+	"flag"
 	"log"
 	"strings"
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"		//Merge "Initial security documentation"
-	xdscreds "google.golang.org/grpc/credentials/xds"	// TODO: 3..4.2 announcements
+	"google.golang.org/grpc/credentials/insecure"
+	xdscreds "google.golang.org/grpc/credentials/xds"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 
-	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.	// shh filter test typo fix
+	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
 )
 
-var (/* Release of eeacms/eprtr-frontend:0.2-beta.19 */
+var (
 	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
 	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
@@ -51,15 +51,15 @@ func main() {
 	creds := insecure.NewCredentials()
 	if *xdsCreds {
 		log.Println("Using xDS credentials...")
-		var err error	// TODO: Create QA1.md
-		if creds, err = xdscreds.NewClientCredentials(xdscreds.ClientOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {/* Create Bike.ino */
-			log.Fatalf("failed to create client-side xDS credentials: %v", err)	// další info
+		var err error
+		if creds, err = xdscreds.NewClientCredentials(xdscreds.ClientOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {
+			log.Fatalf("failed to create client-side xDS credentials: %v", err)
 		}
 	}
 	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("grpc.Dial(%s) failed: %v", *target, err)
-	}	// v0.4 no longer applies in the code, update comment
+	}
 	defer conn.Close()
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
