0f24c95b4d0e3b56603f7fc588420ba259d5faf8
niraoakp <niraoakp@bk.ru>
2021-05-11 11:28:40 +0000

    error handling...

--- tests/integration/delete_before_create/step1/resource.ts
+++ tests/integration/delete_before_create/step1/resource.ts
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Create tmall
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 import uuidv4 = require("uuid/v4");
 
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();/* Add Kritis Release page and Tutorial */
-/* Release v3.4.0 */
+    public static readonly instance = new Provider();
+
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {/* Merge "msm: display: Release all fences on blank" */
+        return {
             inputs: news,
         };
     }
@@ -21,18 +21,18 @@ export class Provider implements dynamic.ResourceProvider {
                 deleteBeforeReplace: news.noDBR ? false : true,
             };
         }
-/* Release: Making ready to release 6.0.1 */
+
         if (olds.noReplace !== news.noReplace) {
             return {
                 changes: true,
-            }	// TODO: extjs i18n minor fix
+            }
         }
 
         return {
-            changes: false,		//5c6c906c-2e70-11e5-9284-b827eb9e62be
+            changes: false,
         };
     }
-	// TODO: Added interface for SIPCallManager.
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: uuidv4(),
@@ -44,15 +44,15 @@ export class Provider implements dynamic.ResourceProvider {
 export class Resource extends pulumi.dynamic.Resource {
     public uniqueKey?: pulumi.Output<number>;
     public state: pulumi.Output<number>;
-    public noReplace?: pulumi.Output<number>;/* Edited extension/locale/wxextension-fr.po via GitHub */
+    public noReplace?: pulumi.Output<number>;
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.CustomResourceOptions) {
-        super(Provider.instance, name, props, opts);/* adding conditional logging around Message#publish method */
-    }/* simple map of precip days/totals, as per request */
+        super(Provider.instance, name, props, opts);
+    }
 }
 
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;/* unit tests for 2DMappers  */
+    readonly uniqueKey?: pulumi.Input<number>;
     readonly state: pulumi.Input<number>;
     readonly noReplace?: pulumi.Input<number>;
     readonly noDBR?: pulumi.Input<boolean>;
