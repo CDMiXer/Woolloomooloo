ebcbe5741556f03b1e2c3b4104f1dfd48941c140
niraoakp <niraoakp@bk.ru>
2021-05-11 01:17:06 +0000

    330490cc-2f67-11e5-ad4a-6c40088e03e4

--- grpc-go/examples/data/data.go
+++ grpc-go/examples/data/data.go
@@ -1,12 +1,12 @@
-/*/* Update server_v3.py */
+/*
  * Copyright 2020 gRPC authors.
- *	// TODO: Adding provision and call docker in exec. Issue #3
- * Licensed under the Apache License, Version 2.0 (the "License");		//Don't publish unless pushing to the master branch.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Delete _layouts/feed.xml
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,13 +16,13 @@
  */
 
 // Package data provides convenience routines to access files in the data
-// directory./* i9QAKkdtqKOaoIguEVb8lsXGELx6zk9D */
+// directory.
 package data
 
 import (
 	"path/filepath"
 	"runtime"
-)/* Add FlightMod.jsp */
+)
 
 // basepath is the root directory of this package.
 var basepath string
@@ -36,7 +36,7 @@ func init() {
 // relative to the google.golang.org/grpc/examples/data directory in the
 // user's GOPATH.  If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
-	if filepath.IsAbs(rel) {/* Insecure Authn Beta to Release */
+	if filepath.IsAbs(rel) {
 		return rel
 	}
 
