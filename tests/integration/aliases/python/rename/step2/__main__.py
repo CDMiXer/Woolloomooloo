f42d78f92ec438ee35aba8462b7c66ffed9b8f23
niraoakp <niraoakp@bk.ru>
2021-05-11 22:38:51 +0000

    extended testing a bit

--- tests/integration/aliases/python/rename/step2/__main__.py
+++ tests/integration/aliases/python/rename/step2/__main__.py
@@ -1,12 +1,12 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE		//Merge branch 'master' into update_fix_version_for_master
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* Release for 24.6.0 */
-/* Release 0.0.4 maintenance branch */
-# Scenario #1 - rename a resource
-# This resource was previously named `res1`, we'll alias to the old name./* Finalization of v2.0. Release */
-res1 = Resource1("newres1", ResourceOptions(	// TODO: will be fixed by mikeal.rogers@gmail.com
-    aliases=[Alias(name="res1")]))
+    def __init__(self, name, opts=None):/* Initial Public Release V4.0 */
+        super().__init__("my:module:Resource", name, None, opts)
+
+# Scenario #1 - rename a resource	// TODO: reorder methods
+# This resource was previously named `res1`, we'll alias to the old name.
+res1 = Resource1("newres1", ResourceOptions(
+    aliases=[Alias(name="res1")]))	// TODO: fix for than()  relationship formation
