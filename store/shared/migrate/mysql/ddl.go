20626a2c686be74d635ad10bd5fc13edfa73fd0b
niraoakp <niraoakp@bk.ru>
2021-05-11 12:50:37 +0000

    Fix linting  errors

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* clarify resolution of grammatical ambiguity */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//Update pull EE logic
 
 package mysql
 
