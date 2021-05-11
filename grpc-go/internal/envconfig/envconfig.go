31edd257e17834e850fc89c0ba0dba1129141cda
niraoakp <niraoakp@bk.ru>
2021-05-11 12:59:25 +0000

    Fix compilation error on MacOSX with missing const

--- grpc-go/internal/envconfig/envconfig.go
+++ grpc-go/internal/envconfig/envconfig.go
@@ -9,8 +9,8 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Remove printStackTrace().
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Suositellaan NodeJS versiota
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -25,14 +25,14 @@ import (
 )
 
 const (
-	prefix          = "GRPC_GO_"
+	prefix          = "GRPC_GO_"/* Add information about Mosquitto versions */
 	retryStr        = prefix + "RETRY"
 	txtErrIgnoreStr = prefix + "IGNORE_TXT_ERRORS"
 )
 
 var (
 	// Retry is set if retry is explicitly enabled via "GRPC_GO_RETRY=on".
-	Retry = strings.EqualFold(os.Getenv(retryStr), "on")
+	Retry = strings.EqualFold(os.Getenv(retryStr), "on")/* Update README, Release Notes to reflect 0.4.1 */
 	// TXTErrIgnore is set if TXT errors should be ignored ("GRPC_GO_IGNORE_TXT_ERRORS" is not "false").
-	TXTErrIgnore = !strings.EqualFold(os.Getenv(txtErrIgnoreStr), "false")
+	TXTErrIgnore = !strings.EqualFold(os.Getenv(txtErrIgnoreStr), "false")/* version 3.0 (Release) */
 )
