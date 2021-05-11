00472dc86393e2c3600d6ec998f0d852d21d53a9
niraoakp <niraoakp@bk.ru>
2021-05-11 07:58:12 +0000

    script for autorestarting

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,16 +1,16 @@
 -- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
- perm_user_id  INTEGER/* Implement asynchronous write net frames into DB */
-,perm_repo_uid VARCHAR(250)		//Get icons for branding also from Icons subdirectory
+ perm_user_id  INTEGER
+,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
-,PRIMARY KEY(perm_user_id, perm_repo_uid)/* Release of eeacms/www-devel:18.3.15 */
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE	// TODO: will be fixed by nick@perfectabstractions.com
+,PRIMARY KEY(perm_user_id, perm_repo_uid)
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
