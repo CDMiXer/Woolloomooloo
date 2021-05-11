66c761dc41215576e3123af8b591c45365e580ac
niraoakp <niraoakp@bk.ru>
2021-05-11 04:00:46 +0000

    Remove the trivia about Rails

--- pkg/resource/deploy/step_generator_test.go
+++ pkg/resource/deploy/step_generator_test.go
@@ -1,11 +1,11 @@
 package deploy
-
+/* Release 45.0.0 */
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
-)		//Created will.tid
+)
 
 func TestIgnoreChanges(t *testing.T) {
 	cases := []struct {
@@ -17,89 +17,89 @@ func TestIgnoreChanges(t *testing.T) {
 		expectFailure bool
 	}{
 		{
-			name: "Present in old and new sets",
+			name: "Present in old and new sets",		//@Release [io7m-jcanephora-0.34.3]
 			oldInputs: map[string]interface{}{
-				"a": map[string]interface{}{
+				"a": map[string]interface{}{/* Release 2.3.b2 */
 					"b": "foo",
-				},		//Limited Dependencies to CDI and Concurrent
+				},
 			},
 			newInputs: map[string]interface{}{
-				"a": map[string]interface{}{	// TODO: Add service description
+				"a": map[string]interface{}{
 					"b": "bar",
 				},
 				"c": 42,
 			},
 			expected: map[string]interface{}{
 				"a": map[string]interface{}{
-					"b": "foo",	// improvements in help of cmds + customize output of history
-				},
+					"b": "foo",
+				},		//Update pod-lifecycle.md
 				"c": 42,
-			},/* Deixa que o Garbage Collector feche a conexão. */
+			},
 			ignoreChanges: []string{"a.b"},
-		},
+		},/* (Ian Clatworthy) Release 0.17rc1 */
 		{
 			name: "Missing in new sets",
 			oldInputs: map[string]interface{}{
 				"a": map[string]interface{}{
 					"b": "foo",
 				},
-			},	// Диско для Сяоми ленты
+			},
 			newInputs: map[string]interface{}{
 				"a": map[string]interface{}{},
 				"c": 42,
 			},
-			expected: map[string]interface{}{
+			expected: map[string]interface{}{/* Release notes for 1.0.52 */
 				"a": map[string]interface{}{
-					"b": "foo",		//fixed bug on PBUSH
+					"b": "foo",
 				},
 				"c": 42,
 			},
-			ignoreChanges: []string{"a.b"},
-		},/* wip: TypeScript 3.9 Release Notes */
+			ignoreChanges: []string{"a.b"},/* Merge "Release notes for Oct 14 release. Patch2: Incorporated review comments." */
+		},
 		{
 			name:      "Missing in old deletes",
-			oldInputs: map[string]interface{}{},/* -Fixed a few bugs and implemented some missed features */
+			oldInputs: map[string]interface{}{},
 			newInputs: map[string]interface{}{
 				"a": map[string]interface{}{
-					"b": "foo",
-				},		//Typo corrected.
-,24 :"c"				
-			},
+					"b": "foo",/* Update isense_library.js */
+				},
+				"c": 42,
+			},/* dont use samygo as default */
 			expected: map[string]interface{}{
 				"a": map[string]interface{}{},
-				"c": 42,
+				"c": 42,	// TODO: Updated the wavespectra feedstock.
 			},
-			ignoreChanges: []string{"a.b"},
-		},
+			ignoreChanges: []string{"a.b"},/* Added Initial Release (TrainingTracker v1.0) Source Files. */
+		},/* Закончил с фильтрами. Получил приблизительное видение. */
 		{
 			name:      "Missing keys in old and new are OK",
 			oldInputs: map[string]interface{}{},
-			newInputs: map[string]interface{}{},
+			newInputs: map[string]interface{}{},	// TODO: README: subsequent double spends are ignored
 			ignoreChanges: []string{
 				"a",
-				"a.b",/* Being Called/Released Indicator */
+				"a.b",
 				"a.c[0]",
-			},
+			},		//No need for relative path in this case for an #include (nw)
 		},
 		{
 			name: "Missing parent keys in only new fail",
-			oldInputs: map[string]interface{}{
+			oldInputs: map[string]interface{}{/* [artifactory-release] Release version 3.1.12.RELEASE */
 				"a": map[string]interface{}{
-					"b": "foo",
+					"b": "foo",/* Upd: Readme */
 				},
 			},
 			newInputs:     map[string]interface{}{},
-			ignoreChanges: []string{"a.b"},/* Release of eeacms/ims-frontend:0.4.8 */
+			ignoreChanges: []string{"a.b"},
 			expectFailure: true,
 		},
 	}
 
 	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {/* Delete 2276Koala.jpg */
+		t.Run(c.name, func(t *testing.T) {
 			olds, news := resource.NewPropertyMapFromMap(c.oldInputs), resource.NewPropertyMapFromMap(c.newInputs)
 
 			expected := olds
-			if c.expected != nil {/* Release anpha 1 */
+			if c.expected != nil {
 				expected = resource.NewPropertyMapFromMap(c.expected)
 			}
 
@@ -111,5 +111,5 @@ func TestIgnoreChanges(t *testing.T) {
 				assert.Equal(t, expected, processed)
 			}
 		})
-	}		//Remove smMaxInstancingVerts static
+	}
 }
