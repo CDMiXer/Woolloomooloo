ef0c492ee5dca642274327017a1585f892952b0a
niraoakp <niraoakp@bk.ru>
2021-05-11 12:43:31 +0000

    Added all tool classes from DominionPicker and FuelTracker

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//modified hardcoding of userId (6) by using attr currUserId
-// Use of this source code is governed by the Drone Non-Commercial License/* translation I phase established */
-// that can be found in the LICENSE file./* Merge "[INTERNAL][FIX] sap.ui.integration: Escape card placeholders in editor" */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package validator
-	// Merge branch 'develop' into reportporta/reportportal#170
-import (/* v0.2.4 Release information */
+
+import (
 	"context"
 	"errors"
 	"testing"
@@ -13,14 +13,14 @@ import (/* v0.2.4 Release information */
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)/* Release 1.2.0 publicando en Repositorio Central */
+)
 
 var noContext = context.Background()
-	// Change binary output name.
+
 var mockFile = `
 kind: pipeline
 type: docker
-name: testing/* strfsong: merge several song_tag_locale() calls */
+name: testing
 `
 
 func TestCombine(t *testing.T) {
@@ -33,12 +33,12 @@ func TestCombine(t *testing.T) {
 		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
 	}
-/* Update TLS-for-Local-IoT.md */
-	service := mock.NewMockValidateService(controller)/* Release 1-95. */
+
+	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, args).Return(nil)
 
-	err := Combine(service).Validate(noContext, args)	// TODO: Merge "Update docs on SignalStrength.getLevel" into mnc-dev
-	if err != nil {/* Release 0.6.4 Alpha */
+	err := Combine(service).Validate(noContext, args)
+	if err != nil {
 		t.Error(err)
 	}
 }
@@ -46,12 +46,12 @@ func TestCombine(t *testing.T) {
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release 0.95 */
-	resp := errors.New("")		//[IMP] config deb package
+
+	resp := errors.New("")
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, nil).Return(resp)
 
-	err := Combine(service).Validate(noContext, nil)		//Fix yet more koa texture quirks
+	err := Combine(service).Validate(noContext, nil)
 	if err != resp {
 		t.Errorf("expected convert service error")
 	}
