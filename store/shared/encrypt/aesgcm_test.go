8aba38911a0b4b44305de7275a7ba1e2acb7c6b2
niraoakp <niraoakp@bk.ru>
2021-05-11 05:15:10 +0000

    Create Onboard.podspec

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -2,22 +2,22 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package encrypt	// TODO: removing java 1.6 method calls (String.isEmpty())
-	// TODO: hacked by alan.shaw@protocol.ai
+package encrypt
+
 import "testing"
-		//Fix FB event ID for bikes vs cars
+
 func TestAesgcm(t *testing.T) {
 	s := "correct-horse-batter-staple"
-	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")/* Update Orchard-1-9.Release-Notes.markdown */
-	ciphertext, err := n.Encrypt(s)/* #76180 - Add flex-wrap to flex-flow */
-	if err != nil {/* Add npc_talk hook */
-		t.Error(err)
-	}
+	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
+	ciphertext, err := n.Encrypt(s)
+	if err != nil {
+		t.Error(err)		//Update roster.js
+	}/* Rename folder first sort method */
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {	// TODO: added specific id for zoom warning and prevent from dispatch it.
+	if err != nil {	// Get rid of unnecessary Buffer.from() and inline function
 		t.Error(err)
 	}
 	if want, got := plaintext, s; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
-	}/* blank line removed */
-}
+	}
+}	// TODO: hacked by fjl@ethereum.org
