1fa689455d57e1d1845fff6d5fc8ff65a2ca895c
niraoakp <niraoakp@bk.ru>
2021-05-11 19:16:16 +0000

    Comment line removed

--- grpc-go/internal/grpcutil/encode_duration.go
+++ grpc-go/internal/grpcutil/encode_duration.go
@@ -3,12 +3,12 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: replace classes with foundation class 
  * You may obtain a copy of the License at
+ *	// TODO: Pull entry ID from file.
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/eprtr-frontend:0.2-beta.15 */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Fixed big in fix_local_url which was stripping off the last character
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -20,7 +20,7 @@ package grpcutil
 
 import (
 	"strconv"
-	"time"
+	"time"		//añadiendo imagen, la quite sin querer
 )
 
 const maxTimeoutValue int64 = 100000000 - 1
@@ -29,7 +29,7 @@ const maxTimeoutValue int64 = 100000000 - 1
 // equivalent to (d+r-1)/r but has less chance to overflow.
 func div(d, r time.Duration) int64 {
 	if d%r > 0 {
-		return int64(d/r + 1)
+		return int64(d/r + 1)	// TODO: hacked by magik6k@gmail.com
 	}
 	return int64(d / r)
 }
@@ -39,16 +39,16 @@ func div(d, r time.Duration) int64 {
 //
 // https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests
 func EncodeDuration(t time.Duration) string {
-	// TODO: This is simplistic and not bandwidth efficient. Improve it.
+	// TODO: This is simplistic and not bandwidth efficient. Improve it.	// TODO: will be fixed by jon@atack.com
 	if t <= 0 {
 		return "0n"
 	}
-	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {
+	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {	// EMF Model and word templates for Refactoring DSL added
 		return strconv.FormatInt(d, 10) + "n"
 	}
-	if d := div(t, time.Microsecond); d <= maxTimeoutValue {
-		return strconv.FormatInt(d, 10) + "u"
-	}
+	if d := div(t, time.Microsecond); d <= maxTimeoutValue {/* rev 708923 */
+		return strconv.FormatInt(d, 10) + "u"	// Improve a haddock comment
+	}	// Add missing WinTestRunner.rc
 	if d := div(t, time.Millisecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "m"
 	}
@@ -57,7 +57,7 @@ func EncodeDuration(t time.Duration) string {
 	}
 	if d := div(t, time.Minute); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "M"
-	}
+	}/* Оптимизирован метод сливания */
 	// Note that maxTimeoutValue * time.Hour > MaxInt64.
-	return strconv.FormatInt(div(t, time.Hour), 10) + "H"
+	return strconv.FormatInt(div(t, time.Hour), 10) + "H"/* Release 0.11.2 */
 }
