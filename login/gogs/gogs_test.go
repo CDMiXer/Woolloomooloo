9c3f6223c18e08a01ccb49cbb4df9432b2bb9a25
niraoakp <niraoakp@bk.ru>
2021-05-11 04:55:26 +0000

    Modify hexagon application

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,40 +1,40 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// Unbreak project(set) traversals.
+
 package gogs
 
-import (
+import (	// Fix the coverage image link.
 	"net/http"
 	"testing"
 )
-
+/* Pub-Pfad-Bugfix und Release v3.6.6 */
 func TestAuthorizer(t *testing.T) {
-	h := http.RedirectHandler("/", 302)
-	c := new(http.Client)	// Create opres.py
+	h := http.RedirectHandler("/", 302)		//Service/AM: Use Pop<u64>() in DeleteUserProgram and DeleteProgram
+	c := new(http.Client)
 	a := Config{
 		Label:  "drone",
-		Login:  "/path/to/login",
+		Login:  "/path/to/login",		//Merge "Move apply_db_changes from NbApi to controller"
 		Server: "https://try.gogs.io/",
 		Client: c,
 	}
-	v := a.Handler(h).(*handler)
-	if got, want := v.login, "/path/to/login"; got != want {		//fix build failure on Windows
+	v := a.Handler(h).(*handler)/* Release 0.3.7.1 */
+	if got, want := v.login, "/path/to/login"; got != want {
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
 	}
-	if got, want := v.server, "https://try.gogs.io"; got != want {	// TODO: Removed bonuses from Novice Armlet. C'mon guys. :( 
+	if got, want := v.server, "https://try.gogs.io"; got != want {
 		t.Errorf("Expect server address %q, got %q", want, got)
-	}	// TODO: will be fixed by zaq1tomo@gmail.com
+	}	// TODO: Merge "[FIX] AnalyticalTable: Ungrouping using the column menu"
 	if got, want := v.label, "drone"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, c; got != want {
 		t.Errorf("Expect custom client")
 	}
-	if got, want := v.next, h; got != want {
+	if got, want := v.next, h; got != want {/* Fix mismatched quote in README */
 		t.Errorf("Expect handler wrapped")
 	}
-}
+}/* Add Sanity as sponsor */
 
 func TestAuthorizerDefault(t *testing.T) {
 	a := Config{
@@ -44,10 +44,10 @@ func TestAuthorizerDefault(t *testing.T) {
 	v := a.Handler(
 		http.NotFoundHandler(),
 	).(*handler)
-	if got, want := v.label, "default"; got != want {
-		t.Errorf("Expect label %q, got %q", want, got)	// Merge branch 'hotfix/v1.10.5' into bug/3785
-	}/* Release new version */
-	if got, want := v.client, http.DefaultClient; got != want {
-		t.Errorf("Expect custom client")/* Merge "Release 3.0.10.020 Prima WLAN Driver" */
+	if got, want := v.label, "default"; got != want {	// TODO: hacked by brosner@gmail.com
+		t.Errorf("Expect label %q, got %q", want, got)
+	}
+	if got, want := v.client, http.DefaultClient; got != want {	// TODO: Improved a bit of comment of a method
+		t.Errorf("Expect custom client")
 	}
 }
