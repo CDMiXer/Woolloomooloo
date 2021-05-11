278e05c6f72baf9c81075b13b31a0870266325e3
niraoakp <niraoakp@bk.ru>
2021-05-11 13:39:33 +0000

    68196bdc-2e53-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/rubberTree.ts
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/rubberTree.ts
@@ -3,7 +3,7 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { input as inputs, output as outputs, enums } from "../../types";
-import * as utilities from "../../utilities";/* Made the containers parcelable */
+import * as utilities from "../../utilities";
 
 export class RubberTree extends pulumi.CustomResource {
     /**
@@ -14,25 +14,25 @@ export class RubberTree extends pulumi.CustomResource {
      * @param id The _unique_ provider ID of the resource to lookup.
      * @param opts Optional settings to control the behavior of the CustomResource.
      */
-    public static get(name: string, id: pulumi.Input<pulumi.ID>, opts?: pulumi.CustomResourceOptions): RubberTree {	// TODO: 86c12b5a-2e40-11e5-9284-b827eb9e62be
+    public static get(name: string, id: pulumi.Input<pulumi.ID>, opts?: pulumi.CustomResourceOptions): RubberTree {
         return new RubberTree(name, undefined as any, { ...opts, id: id });
-    }		//Add init debug support to command line
+    }
 
     /** @internal */
     public static readonly __pulumiType = 'plant-provider:tree/v1:RubberTree';
 
-    /**/* Merge "Sanity-check paths of files to be restored" into jb-mr2-dev */
+    /**
      * Returns true if the given object is an instance of RubberTree.  This is designed to work even
-     * when multiple copies of the Pulumi SDK have been loaded into the same process.	// TODO: hacked by sebastian.tharakan97@gmail.com
-     */		//Fix contact me link in readme
+     * when multiple copies of the Pulumi SDK have been loaded into the same process.
+     */
     public static isInstance(obj: any): obj is RubberTree {
-        if (obj === undefined || obj === null) {/* Merge "Decode inheritance project IDs before use" */
+        if (obj === undefined || obj === null) {
             return false;
         }
         return obj['__pulumiType'] === RubberTree.__pulumiType;
     }
 
-    public readonly container!: pulumi.Output<outputs.Container | undefined>;/* Always make status lowercase. Fix uppercase status NOT_COMPLIANT. refs #26787 */
+    public readonly container!: pulumi.Output<outputs.Container | undefined>;
     public readonly farm!: pulumi.Output<enums.tree.v1.Farm | string | undefined>;
     public readonly type!: pulumi.Output<enums.tree.v1.RubberTreeVariety>;
 
@@ -41,10 +41,10 @@ export class RubberTree extends pulumi.CustomResource {
      *
      * @param name The _unique_ name of the resource.
      * @param args The arguments to use to populate this resource's properties.
-     * @param opts A bag of options that control this resource's behavior.		//use formtastic instead of simple_form
+     * @param opts A bag of options that control this resource's behavior.
      */
     constructor(name: string, args: RubberTreeArgs, opts?: pulumi.CustomResourceOptions) {
-        let inputs: pulumi.Inputs = {};/* Release notes for 1.0.71 */
+        let inputs: pulumi.Inputs = {};
         if (!(opts && opts.id)) {
             if ((!args || args.type === undefined) && !(opts && opts.urn)) {
                 throw new Error("Missing required property 'type'");
@@ -59,19 +59,19 @@ export class RubberTree extends pulumi.CustomResource {
         }
         if (!opts) {
             opts = {}
-        }	// 5cdec5de-2e5b-11e5-9284-b827eb9e62be
+        }
 
         if (!opts.version) {
             opts.version = utilities.getVersion();
         }
         super(RubberTree.__pulumiType, name, inputs, opts);
     }
-}	// TODO: will be fixed by remco@dutchcoders.io
+}
 
 /**
- * The set of arguments for constructing a RubberTree resource./* [IMP] Release Name */
+ * The set of arguments for constructing a RubberTree resource.
  */
-export interface RubberTreeArgs {	// TODO: Contactos en cuentas gestionadas
+export interface RubberTreeArgs {
     readonly container?: pulumi.Input<inputs.Container>;
     readonly farm?: pulumi.Input<enums.tree.v1.Farm | string>;
     readonly type: pulumi.Input<enums.tree.v1.RubberTreeVariety>;
