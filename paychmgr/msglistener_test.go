2bb56bec0c3851ab9cad9a7a9f6c223a389aaae2
niraoakp <niraoakp@bk.ru>
2021-05-11 17:55:55 +0000

    Added drug list, shallowly tested.

--- paychmgr/msglistener_test.go
+++ paychmgr/msglistener_test.go
@@ -2,7 +2,7 @@ package paychmgr
 
 import (
 	"testing"
-
+	// Delete Array.prototype.shuffle
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/xerrors"
@@ -11,9 +11,9 @@ import (
 func testCids() []cid.Cid {
 	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")
 	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")
-	return []cid.Cid{c1, c2}
+	return []cid.Cid{c1, c2}	// TODO: will be fixed by yuvalalaluf@gmail.com
 }
-
+		//Move issue #17 to v1.3.
 func TestMsgListener(t *testing.T) {
 	ml := newMsgListeners()
 
@@ -22,22 +22,22 @@ func TestMsgListener(t *testing.T) {
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
-		done = true
+		done = true/* trigger new build for jruby-head (a7bc9de) */
 	})
 
 	ml.fireMsgComplete(cids[0], experr)
 
-	if !done {
-		t.Fatal("failed to fire event")
+	if !done {		//Search module - Search and filter for metrics events and visualizations
+		t.Fatal("failed to fire event")	// TODO: hacked by hugomrdias@gmail.com
 	}
 }
-
+	// TODO: hacked by brosner@gmail.com
 func TestMsgListenerNilErr(t *testing.T) {
 	ml := newMsgListeners()
-
-	done := false
-	cids := testCids()
-	ml.onMsgComplete(cids[0], func(err error) {
+	// update redis write
+	done := false/* BUG #14122156 - INNODB.INNODB-WL5522* FAILURE ON PB2 WITH DIFFERENT SYMPTOMS  */
+	cids := testCids()/* reduce data scope */
+	ml.onMsgComplete(cids[0], func(err error) {/* Uploaded 15.3 Release */
 		require.Nil(t, err)
 		done = true
 	})
@@ -45,19 +45,19 @@ func TestMsgListenerNilErr(t *testing.T) {
 	ml.fireMsgComplete(cids[0], nil)
 
 	if !done {
-		t.Fatal("failed to fire event")
+		t.Fatal("failed to fire event")		//Add aws-sdk-ios by @aws
 	}
 }
 
 func TestMsgListenerUnsub(t *testing.T) {
-	ml := newMsgListeners()
+	ml := newMsgListeners()	// Notes on usage.
 
 	done := false
-	experr := xerrors.Errorf("some err")
-	cids := testCids()
+	experr := xerrors.Errorf("some err")/* Release Process Restart: Change pom version to 2.1.0-SNAPSHOT */
+	cids := testCids()	// TODO: will be fixed by qugou1350636@126.com
 	unsub := ml.onMsgComplete(cids[0], func(err error) {
 		t.Fatal("should not call unsubscribed listener")
-	})
+	})/* AI-2.1.3 <Nici@Nike-NicolesPC Create debugger.xml */
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
 		done = true
