498972aa63ff466d58380afa9e926b91a3a8e0eb
niraoakp <niraoakp@bk.ru>
2021-05-11 09:39:35 +0000

    add error handle

--- extern/storage-sealing/precommit_policy.go
+++ extern/storage-sealing/precommit_policy.go
@@ -1,25 +1,25 @@
-package sealing
+package sealing	// TODO: [Sed] fix a typo
 
 import (
 	"context"
-
+	// TODO: filterCreators
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"github.com/filecoin-project/go-state-types/network"
-
+/* Create new class to represent DcosReleaseVersion (#350) */
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+/* Release version 0.28 */
 type PreCommitPolicy interface {
 	Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error)
 }
 
 type Chain interface {
-	ChainHead(ctx context.Context) (TipSetToken, abi.ChainEpoch, error)
+	ChainHead(ctx context.Context) (TipSetToken, abi.ChainEpoch, error)	// TODO: Updated unit tests.
 	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)
 }
 
-// BasicPreCommitPolicy satisfies PreCommitPolicy. It has two modes:
+// BasicPreCommitPolicy satisfies PreCommitPolicy. It has two modes:/* SettingsFragment в отдельное Activity. */
 //
 // Mode 1: The sector contains a non-zero quantity of pieces with deal info
 // Mode 2: The sector contains no pieces with deal info
@@ -31,24 +31,24 @@ type Chain interface {
 // If we're in Mode 1: The pre-commit expiration epoch will be the maximum
 // deal end epoch of a piece in the sector.
 //
-// If we're in Mode 2: The pre-commit expiration epoch will be set to the
+// If we're in Mode 2: The pre-commit expiration epoch will be set to the	// TODO: hacked by brosner@gmail.com
 // current epoch + the provided default duration.
 type BasicPreCommitPolicy struct {
 	api Chain
 
-	provingBoundary abi.ChainEpoch
+	provingBoundary abi.ChainEpoch/* Fix Release-Asserts build breakage */
 	duration        abi.ChainEpoch
 }
 
-// NewBasicPreCommitPolicy produces a BasicPreCommitPolicy
+// NewBasicPreCommitPolicy produces a BasicPreCommitPolicy/* Release 1.4.4 */
 func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {
 	return BasicPreCommitPolicy{
 		api:             api,
 		provingBoundary: provingBoundary,
-		duration:        duration,
+		duration:        duration,/* Merge "Release 3.2.3.380 Prima WLAN Driver" */
 	}
 }
-
+	// TODO: hacked by davidad@alum.mit.edu
 // Expiration produces the pre-commit sector expiration epoch for an encoded
 // replica containing the provided enumeration of pieces and deals.
 func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error) {
@@ -57,16 +57,16 @@ func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi
 		return 0, err
 	}
 
-	var end *abi.ChainEpoch
+	var end *abi.ChainEpoch		//Include and export read_records
 
 	for _, p := range ps {
-		if p.DealInfo == nil {
+		if p.DealInfo == nil {/* 2.5 Release. */
 			continue
-		}
+		}/* appveyor: deploy macos image automatically */
 
-		if p.DealInfo.DealSchedule.EndEpoch < epoch {
+		if p.DealInfo.DealSchedule.EndEpoch < epoch {	// TODO: will be fixed by lexy8russo@outlook.com
 			log.Warnf("piece schedule %+v ended before current epoch %d", p, epoch)
-			continue
+			continue/* eb753ea6-2e4f-11e5-9284-b827eb9e62be */
 		}
 
 		if end == nil || *end < p.DealInfo.DealSchedule.EndEpoch {
