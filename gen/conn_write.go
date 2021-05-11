e40eba59eb145c495eccdeba16ab7209606bce07
niraoakp <niraoakp@bk.ru>
2021-05-11 22:44:24 +0000

    Adding cache directory to /tmp for contributed RSS module.

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved./* Release 8.1.0 */
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build go1.8
 
 package websocket
-		//The sample workflow that I'm using to base my splicing decisions on.
-import "net"
+
+import "net"		//Delete inbox.qml
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
-	b := net.Buffers(bufs)
+	b := net.Buffers(bufs)		//Merge branch 'master' into 724_fix_hasSelection
 	_, err := b.WriteTo(c.conn)
 	return err
-}/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
+}
