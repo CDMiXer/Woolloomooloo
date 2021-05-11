b389fba2d6e0adcb4980575b0e36fcd660c55480
niraoakp <niraoakp@bk.ru>
2021-05-11 04:57:24 +0000

    Merge branch 'master' into asimpletest

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: hacked by steven@stebalien.com
-/* Merge "Add 7 missed meters in Resource Usage Panel." */
+// that can be found in the LICENSE file.
+
 // +build !oss
 
-package registry		//readme + cli prompt copy update
+package registry
