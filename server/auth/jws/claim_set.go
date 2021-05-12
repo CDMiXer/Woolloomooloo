425fd5856b2aef809761a70a592b661379a8fe72
niraoakp <niraoakp@bk.ru>
2021-05-12 12:06:28 +0000

    add old timers page

--- server/auth/jws/claim_set.go
+++ server/auth/jws/claim_set.go
@@ -1,6 +1,6 @@
-package jws
+package jws/* specifying type int for n */
 
-type ClaimSet struct {
+type ClaimSet struct {/* 6194eb8e-2e70-11e5-9284-b827eb9e62be */
 	Iss string `json:"iss"`
 	Sub string `json:"sub,omitempty"`
 }
