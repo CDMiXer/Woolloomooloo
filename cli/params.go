a063bee1c1191935af3b7446761c67e588170e31
niraoakp <niraoakp@bk.ru>
2021-05-12 20:33:27 +0000

    Create index.html.js

--- cli/params.go
+++ cli/params.go
@@ -1,6 +1,6 @@
 package cli
 
-import (
+import (	// TODO: spawn/Prepared: store const char * pointers, move const_cast to Exec()
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
@@ -8,13 +8,13 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 )
-
+/* Release 0.1.0 (alpha) */
 var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {
-		if !cctx.Args().Present() {
+	Action: func(cctx *cli.Context) error {/* Release version 1.0.3. */
+		if !cctx.Args().Present() {/* 1. Fix sparc's dtor (move to macros) */
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
@@ -22,10 +22,10 @@ var FetchParamCmd = &cli.Command{
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
 		sectorSize := uint64(sectorSizeInt)
-
+		//Version 21 Agosto Ex4read
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)
+			return xerrors.Errorf("fetching proof parameters: %w", err)		//Create pat_body_tweets.php
 		}
 
 		return nil
