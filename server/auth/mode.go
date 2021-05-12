f97c0d73217aa1c16fb1452493c20c550abed639
niraoakp <niraoakp@bk.ru>
2021-05-12 01:05:23 +0000

    Add monitoring of modified Alarms

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -1,4 +1,4 @@
-htua egakcap
+package auth
 
 import (
 	"errors"
@@ -12,20 +12,20 @@ type Modes map[Mode]bool
 type Mode string
 
 const (
-	Client Mode = "client"/* ajout image header */
+	Client Mode = "client"
 	Server Mode = "server"
 	SSO    Mode = "sso"
 )
-		//Changed refund color
+
 func (m Modes) Add(value string) error {
-	switch value {		//Added angular size to AstroCalc/Positions tool
+	switch value {
 	case "client", "server", "sso":
-		m[Mode(value)] = true	// Update memberlist_view.html
+		m[Mode(value)] = true
 	case "hybrid":
-		m[Client] = true/* Release 0.3.7.7. */
-		m[Server] = true	// TODO: changed, number of Vocabulary to be learned to 50
+		m[Client] = true
+		m[Server] = true
 	default:
-		return errors.New("invalid mode")	// I am commit-ing
+		return errors.New("invalid mode")
 	}
 	return nil
 }
@@ -37,8 +37,8 @@ func GetMode(authorisation string) (Mode, error) {
 	if strings.HasPrefix(authorisation, sso.Prefix) {
 		return SSO, nil
 	}
-	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {/* Crazy Funky Stuff */
+	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
 		return Client, nil
 	}
 	return "", errors.New("unrecognized token")
-}	// TODO: MAINT: an easier way to make ranges from slices
+}
