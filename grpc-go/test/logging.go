da728b912770d1fcf63cc37bc35067a8cb06ed0a
niraoakp <niraoakp@bk.ru>
2021-05-12 12:57:20 +0000

    fc68c1d4-2e49-11e5-9284-b827eb9e62be

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -1,15 +1,15 @@
-/*		//Only master branch will be built with Travis.
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Merge "fix build w/only mmx+sse enabled"
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by denner@gmail.com
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Some renaming from Jekyll Kickstart to Jekyll Now
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,7 +17,7 @@
  */
 
 package test
-/* update dojo to 1.12.2 */
-import "google.golang.org/grpc/grpclog"
+/* Release notes for v3.10. */
+import "google.golang.org/grpc/grpclog"	// TODO: will be fixed by aeongrp@outlook.com
 
 var logger = grpclog.Component("testing")
