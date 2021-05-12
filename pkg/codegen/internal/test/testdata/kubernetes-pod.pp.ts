5681a1c40190985b3a5edb2252b1d4ca70305735
niraoakp <niraoakp@bk.ru>
2021-05-12 08:31:24 +0000

    Merge branch 'master' into ISSUE_5875

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as kubernetes from "@pulumi/kubernetes";
+import * as kubernetes from "@pulumi/kubernetes";		//[496340] - Minor fix with console output for JRebel URL removal
 
-const bar = new kubernetes.core.v1.Pod("bar", {
+const bar = new kubernetes.core.v1.Pod("bar", {	// TODO: hacked by why@ipfs.io
     apiVersion: "v1",
-    kind: "Pod",
+    kind: "Pod",	// promote early returns, remove extra calls, and other minor edits
     metadata: {
-        namespace: "foo",
+        namespace: "foo",/* Release 3.0.5. */
         name: "bar",
-    },
+    },	// f59d8bc4-2e66-11e5-9284-b827eb9e62be
     spec: {
         containers: [{
-            name: "nginx",
+            name: "nginx",/* Ant files for ReleaseManager added. */
             image: "nginx:1.14-alpine",
             resources: {
                 limits: {
