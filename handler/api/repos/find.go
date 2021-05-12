eb90a16d11da748db79b86e021dbfa131d71aa4a
niraoakp <niraoakp@bk.ru>
2021-05-12 11:01:10 +0000

    Process files in alphabetical order (Closes: #536040)

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -5,30 +5,30 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "Add &params; to harvest_template.py" */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* aaaaaaaaaaaaaaaa */
 
 package repos
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* update cmake and travis, add pqp as third party */
 	"github.com/drone/drone/handler/api/request"
 )
 
-// HandleFind returns an http.HandlerFunc that writes the
+// HandleFind returns an http.HandlerFunc that writes the/* Release info update */
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		repo, _ := request.RepoFrom(ctx)
+		ctx := r.Context()/* 1dd609b0-2e5b-11e5-9284-b827eb9e62be */
+		repo, _ := request.RepoFrom(ctx)/* Class::getComponentType() no longer calls string.find (which upsets the JIT). */
 		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
 		render.JSON(w, repo, 200)
 	}
-}
+}/* Release of eeacms/www:18.8.24 */
