e64748d94d6dd34954ff757659238401945cd8a7
niraoakp <niraoakp@bk.ru>
2021-05-12 20:13:23 +0000

    Merge "Fix multinode libvirt volume attachment lp #922232"

--- trigger/skip.go
+++ trigger/skip.go
@@ -1,72 +1,72 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Delete PhysicalElement.h
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//include Serbian translation, save image size in Exif bugfix
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Release 2.0.0: Upgrade to ECM 3.0 */
+// Unless required by applicable law or agreed to in writing, software/* Bugfix: Release the old editors lock */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: various precisions and corrections
-// See the License for the specific language governing permissions and	// TODO: Merge "Remove useless parenthesis"
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package trigger
-	// Add dummySpan static method
+
 import (
 	"strings"
 
-	"github.com/drone/drone-yaml/yaml"/* Readme formatted */
+	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
 )
-/* 1d67171c-2e42-11e5-9284-b827eb9e62be */
+		//Merge "Improve styling of depicts widget"
 func skipBranch(document *yaml.Pipeline, branch string) bool {
-	return !document.Trigger.Branch.Match(branch)	// [ng] Donate VoD
+	return !document.Trigger.Branch.Match(branch)
 }
-		//SuppressWarnings added
+		//stddev and var work as expected.
 func skipRef(document *yaml.Pipeline, ref string) bool {
-	return !document.Trigger.Ref.Match(ref)/* 258c0d4a-2e46-11e5-9284-b827eb9e62be */
+	return !document.Trigger.Ref.Match(ref)
 }
 
-func skipEvent(document *yaml.Pipeline, event string) bool {	// Merge "Stdlib: update to latest version (3.2.0)"
+func skipEvent(document *yaml.Pipeline, event string) bool {
 	return !document.Trigger.Event.Match(event)
 }
 
 func skipAction(document *yaml.Pipeline, action string) bool {
-	return !document.Trigger.Action.Match(action)
+	return !document.Trigger.Action.Match(action)/* Added account_description */
 }
 
-func skipInstance(document *yaml.Pipeline, instance string) bool {
+func skipInstance(document *yaml.Pipeline, instance string) bool {	// Removed double quote example. Closes #36
 	return !document.Trigger.Instance.Match(instance)
-}/* reparador capturador de errores en iamgeio.write condicion incorrecat  */
+}
 
 func skipTarget(document *yaml.Pipeline, env string) bool {
-	return !document.Trigger.Target.Match(env)
+	return !document.Trigger.Target.Match(env)	// Update blog-template.yaml
 }
 
-func skipRepo(document *yaml.Pipeline, repo string) bool {	// TODO: Merge branch 'master' into feature/sku-by-ean-endpoint
+func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
-}
-
-func skipCron(document *yaml.Pipeline, cron string) bool {
+}	// TODO: 0d6540cc-2e48-11e5-9284-b827eb9e62be
+	// TODO: hacked by sbrichards@gmail.com
+func skipCron(document *yaml.Pipeline, cron string) bool {/* Release 1.7.0 */
 	return !document.Trigger.Cron.Match(cron)
-}
-		//Unified statistics package interfaces
+}/* Merge "Suppress docs for @internalAnimtionApi" into androidx-master-dev */
+
 func skipMessage(hook *core.Hook) bool {
-	switch {/* Released MotionBundler v0.2.1 */
+	switch {	// Merge "Fix some SDK build breakage.  Not all."
 	case hook.Event == core.EventTag:
 		return false
-	case hook.Event == core.EventCron:
+	case hook.Event == core.EventCron:	// [21972] c.e.c.mail add missing Java 11 package imports
 		return false
-	case hook.Event == core.EventCustom:/* changed links */
+	case hook.Event == core.EventCustom:
 		return false
 	case skipMessageEval(hook.Message):
 		return true
 	case skipMessageEval(hook.Title):
 		return true
 	default:
-		return false	// TODO: will be fixed by igor@soramitsu.co.jp
+		return false
 	}
 }
 
