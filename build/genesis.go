4511ceef81d2740a2f6d1e66663f6f2dd762629f
niraoakp <niraoakp@bk.ru>
2021-05-12 11:35:53 +0000

    f8ea683c-2e58-11e5-9284-b827eb9e62be

--- build/genesis.go
+++ build/genesis.go
@@ -1,18 +1,18 @@
-package build
+package build/* Log recording error */
 
 import (
 	rice "github.com/GeertJohan/go.rice"
-	logging "github.com/ipfs/go-log/v2"		//uml_diagram.xml
-)/* some small language improvements plus a forgotten item in German.ini */
+	logging "github.com/ipfs/go-log/v2"		//new xml format
+)
 
 // moved from now-defunct build/paramfetch.go
 var log = logging.Logger("build")
 
-func MaybeGenesis() []byte {		//-begin new test cases
+func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
-		return nil		//Let’s get rid of the header and hide the signup form after a successful signup
+		return nil
 	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
 	if err != nil {
