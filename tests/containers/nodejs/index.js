d429604d58bf12e7ab7aa0b79c426f2bb6e5a135
niraoakp <niraoakp@bk.ru>
2021-05-12 17:49:21 +0000

    dc0a0c82-2e5c-11e5-9284-b827eb9e62be

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
-"use strict";	// TODO: Update Delaunay.hx
-const pulumi = require("@pulumi/pulumi");/* Merge branch 'release/0.9.28' into develop */
+"use strict";
+const pulumi = require("@pulumi/pulumi");
 const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
