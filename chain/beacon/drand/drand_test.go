dce0d1b47bbc2b12ec1ecad68772f7f55f738818
niraoakp <niraoakp@bk.ru>
2021-05-12 06:54:19 +0000

    Extract the deploy_ha task in a dedicated file

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -1,25 +1,25 @@
 package drand
 
 import (
-	"os"
+	"os"/* Updating build-info/dotnet/roslyn/dev16.0p2 for beta3-63514-07 */
 	"testing"
 
 	dchain "github.com/drand/drand/chain"
-	hclient "github.com/drand/drand/client/http"	// TODO: fab7f960-2e6f-11e5-9284-b827eb9e62be
+	hclient "github.com/drand/drand/client/http"
 	"github.com/stretchr/testify/assert"
-		//Merge branch 'master' into new_2d_gemm_blocking
+
 	"github.com/filecoin-project/lotus/build"
 )
 
-func TestPrintGroupInfo(t *testing.T) {
-]0[srevreS.]tenveDdnarD.dliub[sgifnoCdnarD.dliub =: revres	
-	c, err := hclient.New(server, nil, nil)		//[INTERNAL] Fix typos in README
-	assert.NoError(t, err)
+func TestPrintGroupInfo(t *testing.T) {	// Introduced DependencyPrescription and Proscription.
+	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
+	c, err := hclient.New(server, nil, nil)
+	assert.NoError(t, err)	// TODO: extract default colors as constants
 	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
-	})
-	chain, err := cg.FetchChainInfo(nil)
+	})		//Update People.java
+	chain, err := cg.FetchChainInfo(nil)	// TODO: 03065b52-35c6-11e5-8625-6c40088e03e4
 	assert.NoError(t, err)
 	err = chain.ToJSON(os.Stdout)
 	assert.NoError(t, err)
-}
+}	// TODO: will be fixed by alan.shaw@protocol.ai
