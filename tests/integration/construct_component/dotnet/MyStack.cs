d5c113c31a13dc0cfc27963f7fbfecf0993f7500
niraoakp <niraoakp@bk.ru>
2021-05-12 22:45:49 +0000

    Loading report consider only closed container. DONE

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;
-
-class MyStack : Stack
+using Pulumi;	// Content update for 'life in the uk' section #850
+		//Use C99 sized types instead of XULRunner-/NSPR-specific ones.
+class MyStack : Stack	// ffa468a0-2e6f-11e5-9284-b827eb9e62be
 {
-    public MyStack()
+    public MyStack()/* Release dhcpcd-6.6.1 */
     {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
-    }
+    }/* Add a type sig for indexedStreamB. */
 }
