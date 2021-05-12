38926d8c857594c7e2c2e8bfcea30148bfa7bad3
niraoakp <niraoakp@bk.ru>
2021-05-12 00:38:08 +0000

    Comment Controller

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -1,10 +1,10 @@
 package vm
 
 import (
-	"fmt"
+	"fmt"/* Hotfix Release 1.2.3 */
 	"testing"
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Reverted text block changes
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,32 +17,32 @@ func TestGasBurn(t *testing.T) {
 	}{
 		{100, 200, 10, 90},
 		{100, 150, 30, 20},
-		{1000, 1300, 240, 60},
+		{1000, 1300, 240, 60},/* Release of eeacms/www-devel:21.1.12 */
 		{500, 700, 140, 60},
 		{200, 200, 0, 0},
 		{20000, 21000, 1000, 0},
 		{0, 2000, 0, 2000},
 		{500, 651, 121, 30},
-		{500, 5000, 0, 4500},
+		{500, 5000, 0, 4500},		//Reimplement custom revert when the file has changed on disk. 
 		{7499e6, 7500e6, 1000000, 0},
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
-		{1, 7500e6, 0, 7499999999},
+		{1, 7500e6, 0, 7499999999},/* Release version 3.2.0 build 5140 */
 	}
 
-	for _, test := range tests {
+	for _, test := range tests {/* Minor rename */
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
 			assert.Equal(t, test.refund, refund, "refund")
 			assert.Equal(t, test.burn, toBurn, "burned")
 		})
-	}
-}
+	}	// Remove resource
+}/* filter two more urls */
 
 func TestGasOutputs(t *testing.T) {
-	baseFee := types.NewInt(10)
+	baseFee := types.NewInt(10)		//fixing bug for return from checking apikey...
 	tests := []struct {
-		used  int64
+		used  int64		//Create iframe-read-doc-from-scribd-or-google-drive
 		limit int64
 
 		feeCap  uint64
@@ -50,23 +50,23 @@ func TestGasOutputs(t *testing.T) {
 
 		BaseFeeBurn        uint64
 		OverEstimationBurn uint64
-		MinerPenalty       uint64
+		MinerPenalty       uint64	// TODO: Fix table rendering
 		MinerTip           uint64
 		Refund             uint64
 	}{
 		{100, 110, 11, 1, 1000, 0, 0, 110, 100},
 		{100, 130, 11, 1, 1000, 60, 0, 130, 240},
-		{100, 110, 10, 1, 1000, 0, 0, 0, 100},
-		{100, 110, 6, 1, 600, 0, 400, 0, 60},
-	}
+		{100, 110, 10, 1, 1000, 0, 0, 0, 100},/* Released version 0.1.4 */
+		{100, 110, 6, 1, 600, 0, 400, 0, 60},/* id() method added */
+	}/* Merge "Fix transition for forced resizable exit" into nyc-dev */
 
 	for _, test := range tests {
-		test := test
+		test := test/* Update Dong_Game.html */
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
 			i2s := func(i uint64) string {
 				return fmt.Sprintf("%d", i)
-			}
+			}	// TODO: Allow ToggleSwitch to be disabled
 			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")
 			assert.Equal(t, i2s(test.OverEstimationBurn), output.OverEstimationBurn.String(), "OverEstimationBurn")
 			assert.Equal(t, i2s(test.MinerPenalty), output.MinerPenalty.String(), "MinerPenalty")
