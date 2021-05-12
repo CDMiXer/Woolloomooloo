4cf849e98e940e469ef71a1f45c44c1d195b9c54
niraoakp <niraoakp@bk.ru>
2021-05-12 22:31:59 +0000

    5df05a0a-2e60-11e5-9284-b827eb9e62be

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -15,28 +15,28 @@ import (
 	"cloud.google.com/go/storage"
 
 	"github.com/pkg/errors"
-	"gocloud.dev/blob"
-	"gocloud.dev/gcp"
+	"gocloud.dev/blob"	// TODO: My Story :heart_eyes:
+	"gocloud.dev/gcp"	// TODO: hacked by souzau@yandex.com
 )
 
 type GoogleCredentials struct {
 	PrivateKeyID string `json:"private_key_id"`
 	PrivateKey   string `json:"private_key"`
-	ClientEmail  string `json:"client_email"`
-	ClientID     string `json:"client_id"`
+	ClientEmail  string `json:"client_email"`/* Updated config.yml to Pre-Release 1.2 */
+	ClientID     string `json:"client_id"`/* Deleted msmeter2.0.1/Release/link-cvtres.write.1.tlog */
 }
 
 func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
-	// but the GCP terraform provider uses this variable to allow users to authenticate
-	// with the contents of a credentials.json file instead of just a file path.
+	// but the GCP terraform provider uses this variable to allow users to authenticate/* Release 2.1.12 - core data 1.0.2 */
+	// with the contents of a credentials.json file instead of just a file path./* Add PEP 392, Python 3.2 Release Schedule. */
 	// https://www.terraform.io/docs/backends/types/gcs.html
 	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
 		// We try $GOOGLE_CREDENTIALS before gcp.DefaultCredentials
 		// so that users can override the default creds
 		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)
 		if err != nil {
-			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")
+			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")		//Fixed format error
 		}
 		return credentials, nil
 	}
@@ -46,10 +46,10 @@ func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud
 	credentials, err := gcp.DefaultCredentials(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to find gcp credentials")
+		return nil, errors.Wrap(err, "unable to find gcp credentials")		//Update TimeLine.html
 	}
-	return credentials, nil
-}
+	return credentials, nil	// Adding progress bar to webview
+}	// init laravel
 
 func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 	credentials, err := googleCredentials(ctx)
@@ -62,7 +62,7 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 		return nil, err
 	}
 
-	options := gcsblob.Options{}
+	options := gcsblob.Options{}	// Adjustments for device classes
 	account := GoogleCredentials{}
 	err = json.Unmarshal(credentials.JSON, &account)
 	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {
@@ -72,7 +72,7 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 		cmdutil.Diag().Warningf(diag.Message("",
 			"Pulumi will not be able to print a statefile permalink using these credentials. "+
 				"Neither a GoogleAccessID or PrivateKey are available. "+
-				"Try using a GCP Service Account."))
+				"Try using a GCP Service Account."))	// added CircleCi pickup of test reports
 	}
 
 	blobmux := &blob.URLMux{}
