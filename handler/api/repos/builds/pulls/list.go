0fcf5eabd1a84f884cbe2e5d0e75775b49c033c4
niraoakp <niraoakp@bk.ru>
2021-05-12 03:54:00 +0000

    add support for parts, and empty-names

--- handler/api/repos/builds/pulls/list.go
+++ handler/api/repos/builds/pulls/list.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Update, the site is fixed now!
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release version 0.1, with the test project */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Factorize the class method
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package pulls
 
-import (
-	"net/http"
+import (	// TODO: will be fixed by vyzo@hackzen.org
+	"net/http"		//Board_ID usage implemented in BE
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* Released version 0.4 Beta */
 
 	"github.com/go-chi/chi"
 )
@@ -31,28 +31,28 @@ func HandleList(
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* 2e642648-2e58-11e5-9284-b827eb9e62be */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {	// TODO: hacked by aeongrp@outlook.com
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err).		//Update to Readme.
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-
+/* Release of eeacms/energy-union-frontend:1.7-beta.0 */
 		results, err := builds.LatestPulls(r.Context(), repo.ID)
 		if err != nil {
-			render.InternalError(w, err)
-			logger.FromRequest(r).
+			render.InternalError(w, err)/* hapus 404 not found */
+			logger.FromRequest(r)./* - Working on agreements */
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name)./* Update dependency react-flip-move to v3.0.3 */
 				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
