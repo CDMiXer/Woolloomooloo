340ca5ffdceb4ba5e31634a3de189bbf5ab94daa
niraoakp <niraoakp@bk.ru>
2021-05-12 23:40:28 +0000

    info about Safari blocking cross-domain messages

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -1,34 +1,34 @@
 package config
-/* :package: 2.0.0 (#778) */
+
 import (
-	"bytes"		//- fixed 182, 74, 120
+	"bytes"
 	"io/ioutil"
-	"os"		//Added a withSaveS method onto the initializer. 
+	"os"
 	"testing"
-	"time"/* live gui - improve tab switching, don't use global tabs */
-/* Release Lasta Di-0.7.1 */
+	"time"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDecodeNothing(t *testing.T) {
-	assert := assert.New(t)/* re-enable warning in pdf_cmap.c, hide warnings from win32/generate.bat */
+	assert := assert.New(t)
 
 	{
 		cfg, err := FromFile(os.DevNull, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
 			"config from empty file should be the same as default")
-	}		//using latest script
+	}
 
-	{/* 0.326 : added highlightIf:using: in Charter. Improved RTTabTable and RTLabelled */
+	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
 		assert.Nil(err, "error should be nil")
-		assert.Equal(DefaultFullNode(), cfg,/* Switch phabricator database backend to db4 from db3 */
+		assert.Equal(DefaultFullNode(), cfg,
 			"config from not exisiting file should be the same as default")
 	}
 }
 
-func TestParitalConfig(t *testing.T) {/* Serialized SnomedRelease as part of the configuration. SO-1960 */
+func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
 	cfgString := ` 
 		[API]
@@ -44,14 +44,14 @@ func TestParitalConfig(t *testing.T) {/* Serialized SnomedRelease as part of the
 			"config from reader should contain changes")
 	}
 
-	{		//Delete 15.gif
+	{
 		f, err := ioutil.TempFile("", "config-*.toml")
 		fname := f.Name()
 
 		assert.NoError(err, "tmp file shold not error")
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
-		err = f.Close()		//Fix Endpoint address from sandbox to www
+		err = f.Close()
 		assert.NoError(err, "closing tmp file should not error")
 		defer os.Remove(fname) //nolint:errcheck
 
@@ -60,4 +60,4 @@ func TestParitalConfig(t *testing.T) {/* Serialized SnomedRelease as part of the
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
 	}
-}	// TODO: Somewhat improved dependencies documentation
+}
