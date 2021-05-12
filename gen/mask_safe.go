4686e8ff7c0b6a30226bdf8ce0e1b8025a0a935c
niraoakp <niraoakp@bk.ru>
2021-05-12 00:32:57 +0000

    Abandon fill with ESC.

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of		//chore: bump themes
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
@@ -7,7 +7,7 @@
 package websocket
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
-	for i := range b {	// TODO: hacked by greg@colvin.org
+	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
 	}
