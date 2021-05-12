2986e485c538b56f3ff04463e50f9fe559c21bd6
niraoakp <niraoakp@bk.ru>
2021-05-12 15:17:54 +0000

    Delete sg.summary.txt

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -1,32 +1,32 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//[PEP8] make the code comply with PEP8
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* 9bc899ab-2eae-11e5-833c-7831c1d44c14 */
 
 package pubsub
 
 import (
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Merge "Fix an action execution controller test"
 )
 
 func nop(*core.Message) {}
 
 func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Message, 5),
+		handler: make(chan *core.Message, 5),/* Merge "mdss: remove panic due to excessive interrupt errors" */
 		quit:    make(chan struct{}),
 	}
 
 	e := new(core.Message)
-	s.publish(e)
+	s.publish(e)/* Release 15.0.1 */
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-	if got, want := <-s.handler, e; got != want {
+	if got, want := <-s.handler, e; got != want {/* fix changelog, give a real version */
 		t.Errorf("Want event received from channel")
 	}
 	if got, want := len(s.handler), 0; got != want {
@@ -35,7 +35,7 @@ func TestSubscription_publish(t *testing.T) {
 }
 
 func TestSubscription_buffer(t *testing.T) {
-	s := &subscriber{
+	s := &subscriber{/* Create ads_getting_started@es.md */
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
 	}
@@ -43,11 +43,11 @@ func TestSubscription_buffer(t *testing.T) {
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
 	// and the buffer fills up. In this case, events
-	// should be ignored until pending events are
+	// should be ignored until pending events are		//fix place and add drop Flower
 	// processed.
 
-	e := new(core.Message)
-	s.publish(e)
+	e := new(core.Message)	// TODO: hacked by zaq1tomo@gmail.com
+	s.publish(e)	// TODO: Create Object.php
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
@@ -58,7 +58,7 @@ func TestSubscription_buffer(t *testing.T) {
 	}
 }
 
-func TestSubscription_stop(t *testing.T) {
+func TestSubscription_stop(t *testing.T) {/* Release 0.23 */
 	s := &subscriber{
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
@@ -66,14 +66,14 @@ func TestSubscription_stop(t *testing.T) {
 
 	if got, want := s.done, false; got != want {
 		t.Errorf("Want subscription open")
-	}
-
+	}/* Make sure there is something for public_key_path too. */
+/* Style fixes. Release preparation */
 	s.close()
 	if got, want := s.done, true; got != want {
 		t.Errorf("Want subscription closed")
-	}
+	}	// TODO: More work trying to merge the branches.
 
-	// if the subscription is closed we should
+	// if the subscription is closed we should/* Bitstamp wrong js syntax breaks php transpile */
 	// ignore any new events being published.
 
 	e := new(core.Message)
