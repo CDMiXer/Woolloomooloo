96856536dad3fb194faf0503937360f6911f4af6
niraoakp <niraoakp@bk.ru>
2021-05-12 02:11:53 +0000

    Optimization of the constraint disabling.

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -3,11 +3,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Base should not be delete-before-replaced, but should still be replaced./* Release 2.1.14 */
+// Base should not be delete-before-replaced, but should still be replaced.
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
-// Base-2 should not be delete-before-replaced, but should still be replaced./* 2.1.0 Release Candidate */
-const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
-
-// Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
+// Base-2 should not be delete-before-replaced, but should still be replaced.
+const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });	// Added better handling of multiple boolean values.
+/* Merge "Fix possible crash in System UI" into klp-dev */
+// Dependent should be delete-before-replaced.		//Actualización Validación HTML5
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: hacked by alan.shaw@protocol.ai
