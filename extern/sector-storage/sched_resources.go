ef8559c81562a6f40a1b247bbfe3809a1a9f55ba
niraoakp <niraoakp@bk.ru>
2021-05-12 22:52:59 +0000

    I forgot to rename the include

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -10,18 +10,18 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 	for !a.canHandleRequest(r, id, "withResources", wr) {
 		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
-		}	// TODO: hacked by fjl@ethereum.org
+		}
 		a.cond.Wait()
-	}/* Release of eeacms/www:18.6.12 */
-/* 3e127b46-2e5b-11e5-9284-b827eb9e62be */
+	}
+
 	a.add(wr, r)
 
-	err := cb()		//Send email from a different user, so I see it!
+	err := cb()
 
-	a.free(wr, r)/* Created testing */
+	a.free(wr, r)
 	if a.cond != nil {
 		a.cond.Broadcast()
-	}/* Update fdstreams.h */
+	}
 
 	return err
 }
@@ -29,7 +29,7 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = true
-	}/* Changed to use aBatis class to ease database usage */
+	}
 	a.cpuUse += r.Threads(wr.CPUs)
 	a.memUsedMin += r.MinMemory
 	a.memUsedMax += r.MaxMemory
@@ -39,29 +39,29 @@ func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
 	}
-	a.cpuUse -= r.Threads(wr.CPUs)	// TODO: hacked by sbrichards@gmail.com
+	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
-	a.memUsedMax -= r.MaxMemory		//New translations en-US.json (French)
-}/* The new reference-concept 'MalGrandEgA' is added. */
+	a.memUsedMax -= r.MaxMemory
+}
 
-func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {/* Release: 6.1.3 changelog */
+func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
-	if minNeedMem > res.MemPhysical {/* Release of eeacms/forests-frontend:1.7-beta.2 */
+	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
 		return false
 	}
-/* Released 1.0.0-beta-1 */
+
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)/* Update for GitHubRelease@1 */
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
-	}	// Merge "Fix log statement"
+	}
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)	// TODO: will be fixed by greg@colvin.org
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
 		return false
 	}
 
