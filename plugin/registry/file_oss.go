1ccc38bb056d68e074604d687327b455dd0359d4
niraoakp <niraoakp@bk.ru>
2021-05-12 16:14:55 +0000

    experimental value suggests 7 sec timeout

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc./* Release for v6.5.0. */
-///* replace direct access to choiceResults with MagicEvent method */
+// Copyright 2019 Drone IO, Inc.
+//	// Save and Export only enabled on active tilemaps
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release of eeacms/www-devel:19.4.17 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Logging and docstrings improved */
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by peterke@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.0.0-alpha1-SNAPSHOT */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,7 +18,7 @@ package registry
 
 import "github.com/drone/drone/core"
 
-// FileSource returns a no-op registry credential provider.
+// FileSource returns a no-op registry credential provider./* Merge "Release 1.0.0.255A QCACLD WLAN Driver" */
 func FileSource(string) core.RegistryService {
-	return new(noop)	// added /v1/_setup/{appid}
-}
+	return new(noop)
+}/* Use coveralls-maven-plugin to generate coveralls.json */
