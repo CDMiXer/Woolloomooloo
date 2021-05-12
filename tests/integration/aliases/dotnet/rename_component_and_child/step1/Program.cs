2fc02a1f0ea65a17319b5d55fead9a3de680ee14
niraoakp <niraoakp@bk.ru>
2021-05-12 20:09:42 +0000

    Allow to run under Jetty

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -7,29 +7,29 @@ class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {/* Release of eeacms/www-devel:19.1.31 */
-    }
+    {		//Interpretador v1.0
+    }	// TODO: Fix OSGI-related tests
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive : ComponentResource
+class ComponentFive : ComponentResource		//e774b16c-2e66-11e5-9284-b827eb9e62be
 {
     private Resource resource;
-
+/* 7c64806c-2e4a-11e5-9284-b827eb9e62be */
     public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-		//51d4513c-2e4b-11e5-9284-b827eb9e62be
+/* - fixed Release_Win32 build path in xalutil */
 class Program
 {
     static Task<int> Main(string[] args)
-    {
+{    
         return Deployment.RunAsync(() => 
-        {/* Merge branch 'master' into Release/v1.2.1 */
-            var comp5 = new ComponentFive("comp5");/* Release of eeacms/www:20.3.2 */
+        {
+            var comp5 = new ComponentFive("comp5");
         });
     }
 }
