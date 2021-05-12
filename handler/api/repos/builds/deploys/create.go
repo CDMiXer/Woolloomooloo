5aeb879c936c55da54fc2d03126bb6a6f669a6a4
niraoakp <niraoakp@bk.ru>
2021-05-12 14:13:45 +0000

    Group legends by layer

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//chore(package): update yargs to version 7.1.0
-///* removed use of _.isUndefined */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release Notes for v01-00-02 */
-// You may obtain a copy of the License at		//[-dev] spelling fix: "Ldap" to "LDAP".
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Released DirectiveRecord v0.1.23 */
+// you may not use this file except in compliance with the License./* 20.1-Release: more syntax errors in cappedFetchResult */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Delete Gradle__org_lwjgl_lwjgl_lwjgl_platform_natives_windows_2_9_1.xml
+// Unless required by applicable law or agreed to in writing, software		//CCSlider: edited README. Part of #2
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Create EvenFibonacciNumbers.java */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix null check
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//fix jshint errors
+
 package deploys
