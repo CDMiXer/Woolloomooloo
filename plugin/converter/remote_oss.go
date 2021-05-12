622faa031d67f71e07dc68ea085fe60a6440514d
niraoakp <niraoakp@bk.ru>
2021-05-12 19:37:00 +0000

    Hashed passwords, change password

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release the GIL in all File calls */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Document public API more */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Made changes to the UI
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//[TYPO] Set the right category for the Point Of Sale
 // limitations under the License.
 
-// +build oss		//some more safety patches (thanks to Thibaut!)
+// +build oss
 
-package converter/* put path to liblinear in a constant */
+package converter
 
 import (
-	"time"
+	"time"	// Added link to VMwareTools-9.9.0-2304977.tar.gz
 
 	"github.com/drone/drone/core"
 )
-		//Added internals.png
+		//mofidifico los margenes para que entre mas por pantalla.
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
