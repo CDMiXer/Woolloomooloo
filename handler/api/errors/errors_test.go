725a1b604a80aca39fa7b0f652b67b2aa9fbb36a
niraoakp <niraoakp@bk.ru>
2021-05-12 23:42:16 +0000

    Merge "Use version-specific test_regex for networking_bgpvpn"

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//08948a0a-2e5a-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Before changing MiniMax
+// that can be found in the LICENSE file./* Create ReleaseCandidate_ReleaseNotes.md */
 
 package errors
 
-import "testing"
+import "testing"/* Changed the name from slackAssit from to gitAssist */
 
 func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
-	}/* ADD: map to single view */
+	}
 }
