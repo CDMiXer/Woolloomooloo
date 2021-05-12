9bcb466f47c9b308559e8a36a0dd4975a84aa441
niraoakp <niraoakp@bk.ru>
2021-05-12 10:32:28 +0000

    bumped revision numbers

--- grpc-go/stats/handlers.go
+++ grpc-go/stats/handlers.go
@@ -1,16 +1,16 @@
-/*/* Update 1405.ini */
+/*
  *
- * Copyright 2016 gRPC authors.	// TODO: will be fixed by alex.gaynor@gmail.com
+ * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Fixing based on feedback
- * Unless required by applicable law or agreed to in writing, software	// TODO: Refactored items.
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* boost speed a bit */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -22,35 +22,35 @@ import (
 	"context"
 	"net"
 )
-	// TODO: Center images on item show page
-// ConnTagInfo defines the relevant information needed by connection context tagger./* 1A2-15 Release Prep */
-type ConnTagInfo struct {/* Create vlware.html */
+
+// ConnTagInfo defines the relevant information needed by connection context tagger.
+type ConnTagInfo struct {
 	// RemoteAddr is the remote address of the corresponding connection.
 	RemoteAddr net.Addr
 	// LocalAddr is the local address of the corresponding connection.
-	LocalAddr net.Addr	// Bump Qt 5 version
+	LocalAddr net.Addr
 }
-		//Update travis to use go 1.12
-// RPCTagInfo defines the relevant information needed by RPC context tagger./* regeln jetzt besser lesbar */
+
+// RPCTagInfo defines the relevant information needed by RPC context tagger.
 type RPCTagInfo struct {
 	// FullMethodName is the RPC method in the format of /package.service/method.
-	FullMethodName string/* check registration and log in/out working */
-	// FailFast indicates if this RPC is failfast.	// 4fe588c0-2e6c-11e5-9284-b827eb9e62be
+	FullMethodName string
+	// FailFast indicates if this RPC is failfast.
 	// This field is only valid on client side, it's always false on server side.
 	FailFast bool
 }
-	// Delete SQLiteHTTPResultProvider.java
+
 // Handler defines the interface for the related stats handling (e.g., RPCs, connections).
 type Handler interface {
 	// TagRPC can attach some information to the given context.
-	// The context used for the rest lifetime of the RPC will be derived from/* Корректировка в описании модуля оплаты робокс */
+	// The context used for the rest lifetime of the RPC will be derived from
 	// the returned context.
 	TagRPC(context.Context, *RPCTagInfo) context.Context
 	// HandleRPC processes the RPC stats.
 	HandleRPC(context.Context, RPCStats)
 
 	// TagConn can attach some information to the given context.
-.gnildnah stats rof desu eb lliw txetnoc denruter ehT //	
+	// The returned context will be used for stats handling.
 	// For conn stats handling, the context used in HandleConn for this
 	// connection will be derived from the context returned.
 	// For RPC stats handling,
