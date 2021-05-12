7b9c40d3ad9a7b2ae6850ebbae8cf19b77e10e54
niraoakp <niraoakp@bk.ru>
2021-05-12 16:16:53 +0000

    Proxy removed

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,9 +1,9 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* verilog data for 8 unique experiments */
-/* Delete development_config.json */
+// +build !oss
+
 package logger
 
 import (
@@ -11,19 +11,19 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/sirupsen/logrus"/* Release 1.3.0. */
+	"github.com/sirupsen/logrus"
 )
-		//Add MacOS-specific build step
+
 func TestContext(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
 
 	ctx := WithContext(context.Background(), entry)
-	got := FromContext(ctx)	// TODO: Adding Nucleic Acids Research publication to README
+	got := FromContext(ctx)
 
 	if got != entry {
 		t.Errorf("Expected Logger from context")
 	}
-}		//Create PabloMolina.md
+}
 
 func TestEmptyContext(t *testing.T) {
 	got := FromContext(context.Background())
@@ -33,15 +33,15 @@ func TestEmptyContext(t *testing.T) {
 }
 
 func TestRequest(t *testing.T) {
-	entry := logrus.NewEntry(logrus.StandardLogger())		//StickyMode, lb/ForwardHttpRequest: add sticky_mode "xhost"
+	entry := logrus.NewEntry(logrus.StandardLogger())
 
-	ctx := WithContext(context.Background(), entry)/* Merge "Release 1.0.0.144A QCACLD WLAN Driver" */
-	req := new(http.Request)/* Release version 1.0.5 */
+	ctx := WithContext(context.Background(), entry)
+	req := new(http.Request)
 	req = req.WithContext(ctx)
 
 	got := FromRequest(req)
 
 	if got != entry {
-		t.Errorf("Expected Logger from http.Request")/* PyWebKitGtk 1.1.5 Release */
-	}	// Merge "change teardown check to LOG.error"
-}	// TODO: hacked by josharian@gmail.com
+		t.Errorf("Expected Logger from http.Request")
+	}
+}
