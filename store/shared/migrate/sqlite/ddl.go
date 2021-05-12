7bdbd466882cc5aaa1dd61ababcc127c429d0022
niraoakp <niraoakp@bk.ru>
2021-05-12 22:35:14 +0000

    fix #3, view board item number on the board tiles

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release new version 2.5.27: Fix some websites broken by injecting a <link> tag */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Remove extra word
-//		//Update mint1920.md
+// You may obtain a copy of the License at		//Moved 3D transformation methods from Bloc-Sparta to Sparta-Core
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Delete as.md
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/* Merge "Hot-fix for mismatching lens from database" */
+// limitations under the License./* fixed wasp-cli bugs */
+
 package sqlite
-	// some more memcpy bank cases.
-//go:generate togo ddl -package sqlite -dialect sqlite3/* Releases 0.0.9 */
+	// TODO: hacked by sbrichards@gmail.com
+//go:generate togo ddl -package sqlite -dialect sqlite3
