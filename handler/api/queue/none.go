a510e51ce248397ff9ac2a66a187e7c601030bfe
niraoakp <niraoakp@bk.ru>
2021-05-12 21:36:01 +0000

    Use JSON3 with no conflict

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* Release 1.1.0-CI00230 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,21 +8,21 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// adding choices
+// limitations under the License.
 
-// +build oss		//fix for ClassCastException
-	// TODO: will be fixed by nagydani@epointsystem.org
-eueuq egakcap
-		//Update register_binary_fuzzy.py
+// +build oss
+
+package queue
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)		//Merge "Update pypi description"
-	// TODO: Merge branch 'master' into fix_ff_keyevents
+)
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
@@ -36,5 +36,5 @@ func HandlePause(core.Scheduler) http.HandlerFunc {
 }
 
 func HandleResume(core.Scheduler) http.HandlerFunc {
-	return notImplemented/* Release of eeacms/plonesaas:5.2.1-61 */
+	return notImplemented
 }
