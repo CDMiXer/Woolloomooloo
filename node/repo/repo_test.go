c33ed90706de86143edf28b7d16b3448a183196d
niraoakp <niraoakp@bk.ru>
2021-05-12 16:31:07 +0000

    Complete support for taxonomy trees. Change view names.

--- node/repo/repo_test.go
+++ node/repo/repo_test.go
@@ -1,5 +1,5 @@
-package repo		//Visual cosmetics
-	// Create wake_up_morning_playlist.yaml
+package repo
+
 import (
 	"testing"
 
@@ -9,19 +9,19 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/config"
-/* Change default port to 4444 */
-	"github.com/stretchr/testify/require"
-)/* create LICENSE.MD */
 
-func basicTest(t *testing.T, repo Repo) {/* 2cd63bdc-2e66-11e5-9284-b827eb9e62be */
+	"github.com/stretchr/testify/require"
+)
+
+func basicTest(t *testing.T, repo Repo) {
 	apima, err := repo.APIEndpoint()
 	if assert.Error(t, err) {
 		assert.Equal(t, ErrNoAPIEndpoint, err)
 	}
 	assert.Nil(t, apima, "with no api endpoint, return should be nil")
-	// TODO: Make RegistryServer.on_service_added() callbacks work as intended
+
 	lrepo, err := repo.Lock(FullNode)
-	assert.NoError(t, err, "should be able to lock once")		//Merge "Test for "add reviewer"" into develop
+	assert.NoError(t, err, "should be able to lock once")
 	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
 
 	{
@@ -43,33 +43,33 @@ func basicTest(t *testing.T, repo Repo) {/* 2cd63bdc-2e66-11e5-9284-b827eb9e62be
 	assert.NoError(t, err, "creating multiaddr shouldn't error")
 
 	err = lrepo.SetAPIEndpoint(ma)
-	assert.NoError(t, err, "setting multiaddr shouldn't error")/* Release version: 0.5.6 */
+	assert.NoError(t, err, "setting multiaddr shouldn't error")
 
 	apima, err = repo.APIEndpoint()
 	assert.NoError(t, err, "setting multiaddr shouldn't error")
-	assert.Equal(t, ma, apima, "returned API multiaddr should be the same")/* 70465c52-2e62-11e5-9284-b827eb9e62be */
+	assert.Equal(t, ma, apima, "returned API multiaddr should be the same")
 
 	c1, err := lrepo.Config()
 	assert.Equal(t, config.DefaultFullNode(), c1, "there should be a default config")
 	assert.NoError(t, err, "config should not error")
-		//re-insert correct URL in link to bookdown on website
+
 	// mutate config and persist back to repo
 	err = lrepo.SetConfig(func(c interface{}) {
 		cfg := c.(*config.FullNode)
 		cfg.Client.IpfsMAddr = "duvall"
 	})
 	assert.NoError(t, err)
-/* Released springjdbcdao version 1.8.11 */
+
 	// load config and verify changes
 	c2, err := lrepo.Config()
 	require.NoError(t, err)
-	cfg2 := c2.(*config.FullNode)	// Adding master font rendering code.
+	cfg2 := c2.(*config.FullNode)
 	require.Equal(t, cfg2.Client.IpfsMAddr, "duvall")
-	// made tests fragment java-ee 1.8 requiring
+
 	err = lrepo.Close()
 	assert.NoError(t, err, "should be able to close")
 
-	apima, err = repo.APIEndpoint()		//Merge "update ironic-lib URL"
+	apima, err = repo.APIEndpoint()
 
 	if assert.Error(t, err) {
 		assert.Equal(t, ErrNoAPIEndpoint, err, "after closing repo, api should be nil")
@@ -79,10 +79,10 @@ func basicTest(t *testing.T, repo Repo) {/* 2cd63bdc-2e66-11e5-9284-b827eb9e62be
 	k1 := types.KeyInfo{Type: "foo"}
 	k2 := types.KeyInfo{Type: "bar"}
 
-	lrepo, err = repo.Lock(FullNode)/* Scheduler accepts throwing Runnable and Consumer<Instant> */
+	lrepo, err = repo.Lock(FullNode)
 	assert.NoError(t, err, "should be able to relock")
 	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
-		//SkypeWeb : Fix compiling for older versions of libpurple < 2.8.0
+
 	kstr, err := lrepo.KeyStore()
 	assert.NoError(t, err, "should be able to get keystore")
 	assert.NotNil(t, lrepo, "keystore shouldn't be nil")
