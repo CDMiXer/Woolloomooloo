c73cb4c0336a8a1be6745b83f0212e55e0eb7ba8
niraoakp <niraoakp@bk.ru>
2021-05-12 20:35:14 +0000

    Adding storage

--- service/user/user.go
+++ service/user/user.go
@@ -3,27 +3,27 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release version 3.1.6 build 5132 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: fix installation, delete not used tests
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
-	// TODO: will be fixed by mowrain@yandex.com
+
 import (
-	"context"		//Debuging search functionality
-/* se agrega el parametro de la perspectiva */
-	"github.com/drone/drone/core"		//Added project files
+	"context"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
 type service struct {
 	client *scm.Client
-	renew  core.Renewer	// Packages aligned with followme
+	renew  core.Renewer
 }
 
 // New returns a new User service that provides access to
@@ -32,11 +32,11 @@ func New(client *scm.Client, renew core.Renewer) core.UserService {
 	return &service{client: client, renew: renew}
 }
 
-func (s *service) Find(ctx context.Context, access, refresh string) (*core.User, error) {/* [[CID 16716]] libfoundation: Release MCForeignValueRef on creation failure. */
+func (s *service) Find(ctx context.Context, access, refresh string) (*core.User, error) {
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   access,
 		Refresh: refresh,
-	})/* Merge "mdss: display: add cmdlist to tx/rx dcs command" */
+	})
 	src, _, err := s.client.Users.Find(ctx)
 	if err != nil {
 		return nil, err
@@ -49,19 +49,19 @@ func (s *service) FindLogin(ctx context.Context, user *core.User, login string) 
 	if err != nil {
 		return nil, err
 	}
-/* SO-3750: move SnomedApiConfig class to component-scanned package */
-	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{		//Score issue fixed
+
+	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
 		Refresh: user.Refresh,
 	})
 	src, _, err := s.client.Users.FindLogin(ctx, login)
 	if err != nil {
-		return nil, err		//update iOS examples
+		return nil, err
 	}
 	return convert(src), nil
-}	// anrdoid -> android
-/* allow first parameter to be the options-object if no callback has been specified */
-func convert(src *scm.User) *core.User {/* Release of eeacms/www-devel:19.11.7 */
+}
+
+func convert(src *scm.User) *core.User {
 	dst := &core.User{
 		Login:  src.Login,
 		Email:  src.Email,
