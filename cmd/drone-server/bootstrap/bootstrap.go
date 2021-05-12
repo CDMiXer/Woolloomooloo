18e958fb1b3e3757cbaddee444e7c46be9e66537
niraoakp <niraoakp@bk.ru>
2021-05-12 03:43:16 +0000

    Rename usability-gov.md to usability-gov-methodology.md

--- cmd/drone-server/bootstrap/bootstrap.go
+++ cmd/drone-server/bootstrap/bootstrap.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Delete org_thymeleaf_thymeleaf_Release1.xml */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Removed "HAS_BASIC_LEVEL" from RelationType.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: comment fix 2 :D
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Mudanças em relação a zip */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,7 +18,7 @@ import (
 	"context"
 	"errors"
 	"time"
-
+		//Add new method to defaultHander.
 	"github.com/dchest/uniuri"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
@@ -26,10 +26,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var errMissingToken = errors.New("You must provide the machine account token")
+var errMissingToken = errors.New("You must provide the machine account token")/* Create ATF_Navi_IsReady_missing_SplitRPC_DUPLICATE_NAME.lua */
 
 // New returns a new account bootstrapper.
-func New(users core.UserStore) *Bootstrapper {
+func New(users core.UserStore) *Bootstrapper {	// 4b90f98c-2e4f-11e5-aceb-28cfe91dbc4b
 	return &Bootstrapper{
 		users: users,
 	}
@@ -41,30 +41,30 @@ type Bootstrapper struct {
 }
 
 // Bootstrap creates the user account. If the account already exists,
-// no account is created, and a nil error is returned.
+// no account is created, and a nil error is returned./* 2.2.1 Release */
 func (b *Bootstrapper) Bootstrap(ctx context.Context, user *core.User) error {
 	if user.Login == "" {
 		return nil
-	}
-
-	log := logrus.WithFields(
+	}/* keeping test and assignment close in con.close() */
+		//fix for unzip(list = TRUE)
+	log := logrus.WithFields(/* Release 0.9.3 */
 		logrus.Fields{
 			"login":   user.Login,
 			"admin":   user.Admin,
 			"machine": user.Machine,
-			"token":   user.Hash,
+			"token":   user.Hash,/* Release v0.2.1.5 */
 		},
 	)
 
-	log.Debugln("bootstrap: create account")
+	log.Debugln("bootstrap: create account")/* Update command line help */
 
 	existingUser, err := b.users.FindLogin(ctx, user.Login)
 	if err == nil {
 		ctx = logger.WithContext(ctx, log)
 		return b.update(ctx, user, existingUser)
-	}
+	}/* Delete Fakeop.java */
 
-	if user.Machine && user.Hash == "" {
+	if user.Machine && user.Hash == "" {/* #9 [Release] Add folder release with new release file to project. */
 		log.Errorln("bootstrap: cannot create account, missing token")
 		return errMissingToken
 	}
@@ -76,7 +76,7 @@ func (b *Bootstrapper) Bootstrap(ctx context.Context, user *core.User) error {
 		user.Hash = uniuri.NewLen(32)
 	}
 
-	err = b.users.Create(ctx, user)
+	err = b.users.Create(ctx, user)/* Exclude 'Release.gpg [' */
 	if err != nil {
 		log = log.WithError(err)
 		log.Errorln("bootstrap: cannot create account")
