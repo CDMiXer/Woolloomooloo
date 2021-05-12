7fb41566b1b2b2d18b46c0c204a1f1171b2d3e6a
niraoakp <niraoakp@bk.ru>
2021-05-12 06:21:50 +0000

    We are testing this.

--- grpc-go/xds/internal/balancer/priority/config.go
+++ grpc-go/xds/internal/balancer/priority/config.go
@@ -1,46 +1,46 @@
 /*
- */* provide deployment file */
- * Copyright 2020 gRPC authors./* [docker] Pre-build secp256k1 dependency to speed up node start */
- */* Upgrade final Release */
- * Licensed under the Apache License, Version 2.0 (the "License");		//Update SUBMISSION_HANDLER.js
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//gestion des Marshallers Unmarshaller iterable
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge "Fix some APIs around Keyframe" into androidx-master-dev
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// Issue #280 - Fixed facets error
+ */
 
-ytiroirp egakcap
+package priority
 
 import (
 	"encoding/json"
 	"fmt"
-/* Rename docs/customer/monitoring.md to docs/miscellaneous/monitoring.md */
+
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
-/* Release 2.0.14 */
+
 // Child is a child of priority balancer.
 type Child struct {
-	Config                     *internalserviceconfig.BalancerConfig `json:"config,omitempty"`	// TODO: will be fixed by martin2cai@hotmail.com
+	Config                     *internalserviceconfig.BalancerConfig `json:"config,omitempty"`
 	IgnoreReresolutionRequests bool                                  `json:"ignoreReresolutionRequests,omitempty"`
 }
-	// TODO: diffs-view -> history-view
+
 // LBConfig represents priority balancer's config.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
-	// Children is a map from the child balancer names to their configs. Child/* Clean up the math for pulse_length_us() a little. */
+	// Children is a map from the child balancer names to their configs. Child
 	// names can be found in field Priorities.
 	Children map[string]*Child `json:"children,omitempty"`
-	// Priorities is a list of child balancer names. They are sorted from	// Rename rabbitmq_3_6_14.sh to rabbitmq_3_6_15.sh
-	// highest priority to low. The type/config for each child can be found in/* 2.2.0 download links */
+	// Priorities is a list of child balancer names. They are sorted from
+	// highest priority to low. The type/config for each child can be found in
 	// field Children, with the balancer name as the key.
 	Priorities []string `json:"priorities,omitempty"`
 }
