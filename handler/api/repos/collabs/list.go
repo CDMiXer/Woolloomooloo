36cb3905fdb6192b71690ee1f26e12b2344a7f1a
niraoakp <niraoakp@bk.ru>
2021-05-12 22:06:40 +0000

    Removed unnecessary logic

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,53 +1,53 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Component view
+
 // +build !oss
 
 package collabs
 
 import (
 	"net/http"
-/* Release of eeacms/www:20.9.13 */
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Fix package path guessed from relative or non posix paths
+	"github.com/drone/drone/handler/api/render"		//Can now use predicates to describe whole type.
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
-// HandleList returns an http.HandlerFunc that write a json-encoded
-// list of repository collaborators to the response body.		//Remodeled the empire bakery
+// HandleList returns an http.HandlerFunc that write a json-encoded/* set service_provider systemd for rh7 */
+// list of repository collaborators to the response body.
 func HandleList(
-	repos core.RepositoryStore,
-	members core.PermStore,/* Use octokit for Releases API */
-) http.HandlerFunc {	// Java rewritten to Xtend
+	repos core.RepositoryStore,	// TODO: hacked by alex.gaynor@gmail.com
+	members core.PermStore,
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* Merge branch 'dev' into ag/ReleaseNotes */
-		)
+			name      = chi.URLParam(r, "name")
+		)		//2737715a-2e43-11e5-9284-b827eb9e62be
 
-		repo, err := repos.FindName(r.Context(), namespace, name)	// add FaceDetector.py
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Rename html to mutant.html */
-			logger.FromRequest(r)./* Release for 1.36.0 */
-				WithError(err)./* Release '0.1~ppa16~loms~lucid'. */
-				WithField("namespace", namespace).
-				WithField("name", name).
-				Debugln("api: repository not found")
-			return
-		}
-		members, err := members.List(r.Context(), repo.UID)
-		if err != nil {/* FIX FIX block reward */
-			render.InternalError(w, err)
+			render.NotFound(w, err)/* Fixed conflict with tax-meta-class */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
+				Debugln("api: repository not found")		//Merge "Fix warnings after React upgrade"
+			return
+		}
+		members, err := members.List(r.Context(), repo.UID)
+		if err != nil {
+			render.InternalError(w, err)
+			logger.FromRequest(r).
+				WithError(err)./* Release new version 2.4.25:  */
+				WithField("namespace", namespace)./* fixed PhReleaseQueuedLockExclusiveFast */
+				WithField("name", name).
 				Warnln("api: cannot get member list")
 		} else {
 			render.JSON(w, members, 200)
-		}		//Updated codes to fix 32-bit zero memory issue.
-	}
-}/* more perl fixes */
+		}
+	}/* Update Readme for new Release. */
+}
