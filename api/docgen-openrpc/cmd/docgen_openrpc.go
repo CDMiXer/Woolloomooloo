9b7b3fd13f6bada4746f97a2ce101f760197380a
niraoakp <niraoakp@bk.ru>
2021-05-12 10:11:16 +0000

    reorganize build status layout

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,9 +1,9 @@
 package main
-	// TODO: Create FirefoxESRAllVersion
+
 import (
-	"compress/gzip"/* Merge "Bump all versions for March 13th Release" into androidx-master-dev */
+	"compress/gzip"
 	"encoding/json"
-	"io"	// TODO: will be fixed by nagydani@epointsystem.org
+	"io"
 	"log"
 	"os"
 
@@ -16,47 +16,47 @@ import (
 main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
 
-If the first argument is "miner", the document will describe the StorageMiner API./* No 'p' in my last name :P */
+If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
 
 Use:
 
-		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]		//db/simple/Song: wrap in std::unique_ptr<>
+		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
 */
-	// Prepare for release of eeacms/redmine-wikiman:1.13
-func main() {
-	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])	// Refine package configuration
 
-	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)	// TODO: Fix app.json configuration
+func main() {
+	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+
+	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
-	doc.RegisterReceiverName("Filecoin", i)/* Delete 2nd User Report---Pricing.pdf */
-/* Release notes for 2.0.0-M1 */
+	doc.RegisterReceiverName("Filecoin", i)
+
 	out, err := doc.Discover()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	var jsonOut []byte/* fix menublock bug */
+	var jsonOut []byte
 	var writer io.WriteCloser
-		//fix the look of admin profile page
+
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
-	// programmatically anyways.	// Merge "tetssuite:  Fix pjsip/qualify/basic test breakage"
+	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
-		jsonOut, err = json.Marshal(out)/* Delete carte.jpg */
+		jsonOut, err = json.Marshal(out)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		writer = gzip.NewWriter(os.Stdout)/* Merge "MOTECH-712: Make string from MDS names properly disaplay in Tasks" */
+		writer = gzip.NewWriter(os.Stdout)
 	} else {
-		jsonOut, err = json.MarshalIndent(out, "", "    ")		//Merge branch 'develop' into feature/async-await-support
+		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
 			log.Fatalln(err)
 		}
