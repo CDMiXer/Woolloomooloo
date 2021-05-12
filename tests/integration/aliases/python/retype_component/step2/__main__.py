85caeb9d49d2adad4eca2f4de9b65a07f493f3dc
niraoakp <niraoakp@bk.ru>
2021-05-12 07:01:36 +0000

    there are no global contexts

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,12 +1,12 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Delete sitesearch.xml
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import copy
-
+		//Schema do SQL do banco de dados newsicop limpo, sem registros.
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)	// TODO: hacked by witek@enjin.io
+    def __init__(self, name, opts=None):		//[FIX] tools.email_send: never close the request's cursor
+        super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
@@ -15,15 +15,15 @@ class ComponentFour(ComponentResource):
         aliases = [Alias(type_="my:module:ComponentFour")]
         if opts.aliases is not None:
             for alias in opts.aliases:
-                aliases.append(alias)	// TODO: hacked by juan@benet.ai
+                aliases.append(alias)
 
-        # ..and then make the super call with the new type of this resource and the added alias./* Released 2.2.4 */
+        # ..and then make the super call with the new type of this resource and the added alias.	// TODO: hacked by zaq1tomo@gmail.com
         opts_copy = copy.copy(opts)
-        opts_copy.aliases = aliases
+        opts_copy.aliases = aliases	// TODO: will be fixed by brosner@gmail.com
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
 
         # The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        # to.	// TODO: will be fixed by arajasek94@gmail.com
+        # to.
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
-comp4 = ComponentFour("comp4")	// TODO: Fixed chain rule of LMA.
+comp4 = ComponentFour("comp4")
