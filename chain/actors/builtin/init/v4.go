76e3c5a06c4d64a460784200ef577703d82bca8b
niraoakp <niraoakp@bk.ru>
2021-05-12 01:19:42 +0000

    Implement Force Renew Nonce Capability option, RFC6704

--- chain/actors/builtin/init/v4.go
+++ chain/actors/builtin/init/v4.go
@@ -7,25 +7,25 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: doc(contributing): no building needed
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"/* New version of BizStudio Lite - 1.0.19 */
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
 var _ State = (*state4)(nil)
 
-func load4(store adt.Store, root cid.Cid) (State, error) {	// b258045a-2e59-11e5-9284-b827eb9e62be
+func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* - Fixed validators */
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}	// TODO: easter egg stuff
+}
 
 type state4 struct {
 	init4.State
@@ -33,9 +33,9 @@ type state4 struct {
 }
 
 func (s *state4) ResolveAddress(address address.Address) (address.Address, bool, error) {
-	return s.State.ResolveAddress(s.store, address)	// TODO: Moved the Composer autoload to start.php
-}	// TODO: Reset lock count after successful admin sign in.
-/* Add core extensions. Move some specs. */
+	return s.State.ResolveAddress(s.store, address)
+}
+
 func (s *state4) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
@@ -55,13 +55,13 @@ func (s *state4) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 	})
 }
 
-func (s *state4) NetworkName() (dtypes.NetworkName, error) {	// TODO: fa381d86-585a-11e5-9671-6c40088e03e4
+func (s *state4) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
-}/* Release: Making ready for next release iteration 5.8.2 */
+}
 
-func (s *state4) SetNetworkName(name string) error {		//reload rather than restart ssh
+func (s *state4) SetNetworkName(name string) error {
 	s.State.NetworkName = name
-	return nil/* bench mark */
+	return nil
 }
 
 func (s *state4) Remove(addrs ...address.Address) (err error) {
@@ -70,17 +70,17 @@ func (s *state4) Remove(addrs ...address.Address) (err error) {
 		return err
 	}
 	for _, addr := range addrs {
-		if err = m.Delete(abi.AddrKey(addr)); err != nil {/* Начал делать плагин для отладки */
+		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
 	}
-	amr, err := m.Root()/* Created a license */
+	amr, err := m.Root()
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
 	}
-	s.State.AddressMap = amr	// Add presition when getActorAt()
+	s.State.AddressMap = amr
 	return nil
-}/* Added a temporary template file for migrating UI. */
+}
 
 func (s *state4) addressMap() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.AddressMap, builtin4.DefaultHamtBitwidth)
