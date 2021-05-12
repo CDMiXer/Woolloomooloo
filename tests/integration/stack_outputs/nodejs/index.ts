6a17e265c3f96a9a0961707dc2fde46e04e74474
niraoakp <niraoakp@bk.ru>
2021-05-12 00:52:15 +0000

    fix missing dependencies in package json

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by juan@benet.ai
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-export let xyz = "ABC";/* IndexSequence creation fully reorganized (work in progress) */
-export let foo = 42;/* Updated RUN THE EXPORT (markdown) */
+export let xyz = "ABC";
+export let foo = 42;
