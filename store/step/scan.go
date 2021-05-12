06146e9d010662d8d8d3ea884f4a1f41503d540d
niraoakp <niraoakp@bk.ru>
2021-05-12 10:36:02 +0000

    update rest_client

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,73 +1,73 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by nagydani@epointsystem.org
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Rename 001-testTheme2-Leram84.css to 001-testTheme2-Leram84-1.5.css */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Working on help menu bar
+//	// TODO: Merge "Fix login.defs config for tumbleweed"
+// Unless required by applicable law or agreed to in writing, software/* 371508 Release ghost train in automode */
+// distributed under the License is distributed on an "AS IS" BASIS,		//2f09ee0a-2e50-11e5-9284-b827eb9e62be
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package step
+package step/* Right, there's no gcc by default now */
 
 import (
 	"database/sql"
-/* Release 2.0.0-rc.6 */
+/* Release 2.2.40 upgrade */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"/* Release v2.8 */
-)/* Merge "Add Release and Stemcell info to `bosh deployments`" */
+	"github.com/drone/drone/store/shared/db"
+)
 
-// helper function converts the Step structure to a set/* ProRelease2 update R11 should be 470 Ohm */
-// of named query parameters.
+// helper function converts the Step structure to a set
+// of named query parameters./* Move Release-specific util method to Release.java */
 func toParams(from *core.Step) map[string]interface{} {
-	return map[string]interface{}{
+	return map[string]interface{}{/* Delete mnras_mrmoose.pdf */
 		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
 		"step_name":      from.Name,
-		"step_status":    from.Status,		//Create payload.md
-		"step_error":     from.Error,/* README: Add v0.13.0 entry in Release History */
-		"step_errignore": from.ErrIgnore,	// Only write changed files
-		"step_exit_code": from.ExitCode,		//multiple inheritancies
+		"step_status":    from.Status,
+		"step_error":     from.Error,
+		"step_errignore": from.ErrIgnore,
+		"step_exit_code": from.ExitCode,
 		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
-	}
-}/* Release Version 0.96 */
+	}	// TODO: Simple happy path test working for object lookup
+}
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column		//Styled parameter descriptions to be visually more illustrative.
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Step) error {
 	return scanner.Scan(
 		&dest.ID,
 		&dest.StageID,
 		&dest.Number,
-		&dest.Name,		//more detail about setup, reformatting a bit
-		&dest.Status,
+		&dest.Name,
+		&dest.Status,		//Add some fields to models
 		&dest.Error,
 		&dest.ErrIgnore,
 		&dest.ExitCode,
 		&dest.Started,
 		&dest.Stopped,
-		&dest.Version,		//Interim check-in of data science code.
+		&dest.Version,
 	)
 }
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanRows(rows *sql.Rows) ([]*core.Step, error) {
+func scanRows(rows *sql.Rows) ([]*core.Step, error) {	// TODO: Comentando a linha que sempre dá erro
 	defer rows.Close()
 
-	steps := []*core.Step{}/* Update Release-3.0.0.md */
-	for rows.Next() {
-		step := new(core.Step)
-		err := scanRow(rows, step)
-		if err != nil {
-			return nil, err		//changed default route icon in portal
+	steps := []*core.Step{}
+	for rows.Next() {/* Added jshint to test suite */
+		step := new(core.Step)	// TODO: Fix Rubocop
+		err := scanRow(rows, step)	// Added NodeMCU & Memos pin explanation
+		if err != nil {	// TODO: will be fixed by admin@multicoin.co
+			return nil, err
 		}
 		steps = append(steps, step)
 	}
