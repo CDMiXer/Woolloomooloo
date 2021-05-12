f855e86e9952e0b94f0dcbb757a6d674073082e9
niraoakp <niraoakp@bk.ru>
2021-05-12 14:06:27 +0000

    fix instance checks

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release 3.1.2. */
-		//des entites html toujours, mais des 'caron' cette fois ^^
-// +build !oss		//some fixes for departures and admissions
+// that can be found in the LICENSE file.
 
-package rpc/* Delete version.json */
+// +build !oss
+
+package rpc
