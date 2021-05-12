31eb891a46d25093e35c2c000b0f4d6bf7b4907c
niraoakp <niraoakp@bk.ru>
2021-05-12 03:32:36 +0000

    Save file date when compressing a file

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -1,32 +1,32 @@
-using Pulumi;/* Upgrade final Release */
-using Azure = Pulumi.Azure;
+using Pulumi;
+using Azure = Pulumi.Azure;/* Add euro/pound/yen and fix non centered symbol */
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        var config = new Config();
+        var config = new Config();/* Use a transitionSlot to track from where the item is being unequipped. */
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
         var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
         {
             Name = resourceGroupNameParam,
         }));
-        var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);/* Windwalker - Initial Release */
+        var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
         var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
         var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
-        var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs/* Release 6.5.41 */
-        {
+        var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
+        {	// TODO: Redesigned TM for big screens, improved shortcuts, added missiog waitFor() cals
             Name = storageAccountNameParam,
             AccountKind = "StorageV2",
             Location = locationParam,
-            ResourceGroupName = resourceGroupNameParam,/* Add xvfb image */
+            ResourceGroupName = resourceGroupNameParam,
             AccountTier = storageAccountTierParam,
             AccountReplicationType = storageAccountTypeReplicationParam,
         });
-        this.StorageAccountNameOut = storageAccountResource.Name;
-    }
+        this.StorageAccountNameOut = storageAccountResource.Name;/* WikiExtrasPlugin/0.13.1: Release 0.13.1 */
+    }	// TODO: added legal stuff.
 
     [Output("storageAccountNameOut")]
-    public Output<string> StorageAccountNameOut { get; set; }		//[FIX] sale: overwrite _make_invoice and get section_id in sale_crm module
+    public Output<string> StorageAccountNameOut { get; set; }
 }
