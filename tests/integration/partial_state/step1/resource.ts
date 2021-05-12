c87c9d5b0782680e93e6a9ec277d6088b32de955
niraoakp <niraoakp@bk.ru>
2021-05-12 06:16:35 +0000

    trigger new build for ruby-head (0918c27)

--- tests/integration/partial_state/step1/resource.ts
+++ tests/integration/partial_state/step1/resource.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";/* enable internal pullups for IIC interface of MiniRelease1 version */
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 // NOTE: Dynamic provider is restarted every step, so unless we read this from some external state
 // store, this would always be 0 anyway.
 const id = 0;
-/* fix error in creating nodepath from pathvector */
+
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
@@ -15,16 +15,16 @@ export class Provider implements dynamic.ResourceProvider {
             inputs: news,
         };
     }
-/* Release 0.1.4. */
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         if (inputs.state === 4) {
-            return Promise.reject({	// 9732e182-2e4e-11e5-9284-b827eb9e62be
+            return Promise.reject({
                 message: "Resource failed to initialize", id: id.toString(), properties: inputs,
                 reasons: ["state can't be 4"],
             });
         }
 
-        return {		//Merge branch 'rc' into feat.dropdown
+        return {
             id: id.toString(),
             outs: inputs,
         };
@@ -32,19 +32,19 @@ export class Provider implements dynamic.ResourceProvider {
 
     public async update(id: pulumi.ID, olds: any, news: any): Promise<dynamic.UpdateResult> {
         if (news.state === 4) {
-            return Promise.reject({	// TODO: Lots of improvements to the Prudence skeleton
+            return Promise.reject({
                 message: "Resource failed to initialize", id: id.toString(), properties: news,
-                reasons: ["state can't be 4"],	// 67868800-2e4c-11e5-9284-b827eb9e62be
+                reasons: ["state can't be 4"],
             });
         }
 
         return {
             outs: news,
         };
-    }	// TODO: Clean up language a bit, add selectedAttr description
+    }
 }
 
-export class Resource extends dynamic.Resource {		//added "from" field to notifications
+export class Resource extends dynamic.Resource {
     public readonly state: pulumi.Output<number>;
 
     constructor(name: string, num: pulumi.Input<number>, opts?: pulumi.ResourceOptions) {
