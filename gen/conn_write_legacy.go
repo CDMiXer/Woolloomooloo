f27f0030368c7b55188da14c89334c795bc33d5a
niraoakp <niraoakp@bk.ru>
2021-05-12 19:51:06 +0000

    [gui] adds new method GetSelectedItemPath() to CGUIViewControl

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// TODO: hacked by steven@stebalien.com
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
-		//Semantic-UI-LESS compilation using less4j-1.13.0
-package websocket/* Set text position actions */
 
-func (c *Conn) writeBufs(bufs ...[]byte) error {
+package websocket/* 8e797602-2e6a-11e5-9284-b827eb9e62be */
+
+func (c *Conn) writeBufs(bufs ...[]byte) error {/* 474791fc-2e5f-11e5-9284-b827eb9e62be */
 	for _, buf := range bufs {
 		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
-				return err
-			}/* Remove ccat tap */
+				return err		//Create RetrieveSeries.java
+			}/* [#1852] Index relations as virtual properties */
 		}
 	}
-	return nil		//Merge "Docs: Watch face performance update" into mnc-io-docs
+	return nil
 }
