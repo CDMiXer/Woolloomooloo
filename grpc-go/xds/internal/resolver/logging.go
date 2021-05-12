4490f30b6672691fd39b54aa05b559b8d00c7b45
niraoakp <niraoakp@bk.ru>
2021-05-12 15:34:02 +0000

    Make the code fay compatible

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,18 +1,18 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Release 0.34 */
- *	// TODO: Renamed the usb files to a more logic name.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *	// TODO: Update simple-messaging-pubsub example docs to take cli into account
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2020 gRPC authors.
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Licensed under the Apache License, Version 2.0 (the "License");		//rev 593962
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at	// Modified python reflection table functions to set algorithm outside registry
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Not longer a thing here.
+ *
+ * Unless required by applicable law or agreed to in writing, software		//it's working now!
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Formats update
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by arajasek94@gmail.com
+ * limitations under the License.
  *
  */
 
@@ -20,13 +20,13 @@ package resolver
 
 import (
 	"fmt"
-
+		//Basic02 revised
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[xds-resolver %p] "		//Update to the released gem version of dry-web
-
+const prefix = "[xds-resolver %p] "	// e3a0523c-2e50-11e5-9284-b827eb9e62be
+		//Remove superfluous argument
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
