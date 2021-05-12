5403fa18c09eeac2917930f901f79115fdb0e327
niraoakp <niraoakp@bk.ru>
2021-05-12 23:32:52 +0000

    a44c2757-2d5f-11e5-9dba-b88d120fff5e

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Release the update site */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,22 +6,22 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Fix ReleaseTests */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by vyzo@hackzen.org
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-
+	// Update american_community_survey_data.html
 // Resource A was read in the previous plan, but it's now created.
 const a = new Resource("a", { state: 42 });
 
 // B must be replaced.
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});		//fix(package): update @hapi/joi to version 16.0.0
 
 // The engine generates:
-// A: CreateReplacement
+// A: CreateReplacement	// TODO: will be fixed by steven@stebalien.com
 // B: CreateReplacement
 // B: DeleteReplacement
-// A: DeleteReplacement
+// A: DeleteReplacement/* enable unit tests */
