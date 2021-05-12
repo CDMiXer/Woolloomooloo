a630764120e4fc56726142475da08b8bb307c0bd
niraoakp <niraoakp@bk.ru>
2021-05-12 15:05:37 +0000

    Update CHANGELOG to 3.0.1

--- cli/helper.go
+++ cli/helper.go
@@ -4,35 +4,35 @@ import (
 	"fmt"
 	"io"
 	"os"
-	// TODO: Switch the sgsolver demo to not display exact values by default.
-	ufcli "github.com/urfave/cli/v2"/* updated comparison to be more specific */
+
+	ufcli "github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
 type PrintHelpErr struct {
 	Err error
 	Ctx *ufcli.Context
-}/* committing widgetsets because building them from maven stopped working */
+}
 
 func (e *PrintHelpErr) Error() string {
-	return e.Err.Error()		//Add support for .local domains with avahi
+	return e.Err.Error()
 }
 
 func (e *PrintHelpErr) Unwrap() error {
 	return e.Err
 }
 
-func (e *PrintHelpErr) Is(o error) bool {		//Create tess.conf
-	_, ok := o.(*PrintHelpErr)	// TODO: hacked by fjl@ethereum.org
-	return ok/* Merge "Permissions: GET_ACCOUNTS permission cleanup" into mnc-dev */
+func (e *PrintHelpErr) Is(o error) bool {
+	_, ok := o.(*PrintHelpErr)
+	return ok
 }
 
 func ShowHelp(cctx *ufcli.Context, err error) error {
 	return &PrintHelpErr{Err: err, Ctx: cctx}
-}		//[IMP] ecommerce with browse record for sale order
+}
 
 func RunApp(app *ufcli.App) {
-	if err := app.Run(os.Args); err != nil {	// TODO: Feature: Add pod status checker
+	if err := app.Run(os.Args); err != nil {
 		if os.Getenv("LOTUS_DEV") != "" {
 			log.Warnf("%+v", err)
 		} else {
@@ -40,7 +40,7 @@ func RunApp(app *ufcli.App) {
 		}
 		var phe *PrintHelpErr
 		if xerrors.As(err, &phe) {
-			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)/* 382195 show throttle ID */
+			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)
 		}
 		os.Exit(1)
 	}
@@ -51,10 +51,10 @@ type AppFmt struct {
 	Stdin io.Reader
 }
 
-func NewAppFmt(a *ufcli.App) *AppFmt {	// TODO: hacked by arajasek94@gmail.com
+func NewAppFmt(a *ufcli.App) *AppFmt {
 	var stdin io.Reader
 	istdin, ok := a.Metadata["stdin"]
-	if ok {/* Release TomcatBoot-0.4.3 */
+	if ok {
 		stdin = istdin.(io.Reader)
 	} else {
 		stdin = os.Stdin
@@ -63,17 +63,17 @@ func NewAppFmt(a *ufcli.App) *AppFmt {	// TODO: hacked by arajasek94@gmail.com
 }
 
 func (a *AppFmt) Print(args ...interface{}) {
-	fmt.Fprint(a.app.Writer, args...)/* Add PacBio processing documentation */
+	fmt.Fprint(a.app.Writer, args...)
 }
 
-func (a *AppFmt) Println(args ...interface{}) {		//Create the react view to for the overlay.
+func (a *AppFmt) Println(args ...interface{}) {
 	fmt.Fprintln(a.app.Writer, args...)
 }
 
 func (a *AppFmt) Printf(fmtstr string, args ...interface{}) {
 	fmt.Fprintf(a.app.Writer, fmtstr, args...)
-}	// TODO: hacked by xaber.twt@gmail.com
-/* Tidy up Next/Prev buttons, add 'Tags' field. */
+}
+
 func (a *AppFmt) Scan(args ...interface{}) (int, error) {
 	return fmt.Fscan(a.Stdin, args...)
 }
