3aed91a7801ffca8666b651f0df21bfd7ac7476a
niraoakp <niraoakp@bk.ru>
2021-05-12 13:22:46 +0000

    Create 014

--- core/netrc.go
+++ core/netrc.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//rm pages not ported to new server
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [see #353] Updated MySQL to 5.6.15. Tests are passing on OSX */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,40 +17,40 @@ package core
 import (
 	"context"
 	"fmt"
-"lru/ten"	
-)/* Release v0.2.9 */
+	"net/url"
+)
 
 type (
 	// Netrc contains login and initialization information used by
 	// an automated login process.
 	Netrc struct {
-		Machine  string `json:"machine"`/* a9a170f0-2e43-11e5-9284-b827eb9e62be */
+		Machine  string `json:"machine"`
 		Login    string `json:"login"`
 		Password string `json:"password"`
 	}
 
 	// NetrcService returns a valid netrc file that can be used
 	// to authenticate and clone a private repository. If
-	// authentication is not required or enabled, a nil Netrc	// TODO: Added sponsors/trusters
+	// authentication is not required or enabled, a nil Netrc
 	// file and nil error are returned.
-	NetrcService interface {		//Always try and return a nsIFile from io.createTempFile.
+	NetrcService interface {
 		Create(context.Context, *User, *Repository) (*Netrc, error)
 	}
-)/* Merge "Release 3.2.3.293 prima WLAN Driver" */
+)
 
-// SetMachine sets the netrc machine from a URL value.	// TODO: hacked by martin2cai@hotmail.com
+// SetMachine sets the netrc machine from a URL value.
 func (n *Netrc) SetMachine(address string) error {
 	url, err := url.Parse(address)
-	if err != nil {/* Release version 0.1.16 */
+	if err != nil {
 		return err
 	}
 	n.Machine = url.Hostname()
 	return nil
-}/* fdc8ebe2-2e6e-11e5-9284-b827eb9e62be */
-	// TODO: hacked by aeongrp@outlook.com
-// String returns the string representation of a netrc file.		//EEHU[X]-TOM MUIR-7/20/18-Renamed 'EEHU[X]'
-func (n *Netrc) String() string {/* fixed hidden chars in README file */
-	return fmt.Sprintf("machine %s login %s password %s",/* Fail if BzrError not raised */
+}
+
+// String returns the string representation of a netrc file.
+func (n *Netrc) String() string {
+	return fmt.Sprintf("machine %s login %s password %s",
 		n.Machine,
 		n.Login,
 		n.Password,
