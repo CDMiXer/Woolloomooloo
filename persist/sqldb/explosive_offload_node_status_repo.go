a3626fbee4923f4c50ec41aeef2dee1114540d42
niraoakp <niraoakp@bk.ru>
2021-05-12 18:03:35 +0000

    reformat the readme file

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,8 +1,8 @@
-package sqldb/* Merge "Release notes for RC1" */
+package sqldb
 
 import (
 	"fmt"
-		//Delete Pojo.class
+
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
@@ -14,9 +14,9 @@ type explosiveOffloadNodeStatusRepo struct {
 
 func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
-}	// New multi-label form working
+}
 
-func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {		//Added some data members to file and chunk and the main method
+func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
 	return "", OffloadNotSupportedError
 }
 
@@ -24,13 +24,13 @@ func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error)
 	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {/* Release Candidate 0.5.9 RC1 */
+func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
-}/* Preparing for a lot of breaking changes */
+}
 
 func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
 	return OffloadNotSupportedError
-}/* Backport new features to master. */
+}
 
 func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
 	return nil, OffloadNotSupportedError
