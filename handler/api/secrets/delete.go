014af258411f7099c46df4fb5d657ec02d22dad8
niraoakp <niraoakp@bk.ru>
2021-05-12 08:51:16 +0000

    add VILISTEXTUM dep to install instructions

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -2,32 +2,32 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
-package secrets
+// +build !oss/* VersaloonPro Release3 update, add a connector for TVCC and TVREF */
+	// TODO: will be fixed by aeongrp@outlook.com
+package secrets/* oops, here's the Changelog */
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* Updated Readme for 4.0 Release Candidate 1 */
 	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
-func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
+func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {	// TODO: will be fixed by timnugent@gmail.com
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* Create LICENSE.md containing MIT License. */
 		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}	// TODO: hacked by aeongrp@outlook.com
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
