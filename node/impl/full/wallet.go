8f5bb51e6e779f4b5ba63de0eebda4dec12f05b2
niraoakp <niraoakp@bk.ru>
2021-05-12 00:57:58 +0000

    fixing calculations and code for buffer realloc

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -1,13 +1,13 @@
-package full/* Release of eeacms/forests-frontend:2.0-beta.22 */
+package full
 
 import (
-	"context"/* refactoring GET */
-	// TODO: hacked by mikeal.rogers@gmail.com
+	"context"
+
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// Adding build status image to README
-	"github.com/filecoin-project/go-state-types/big"	// Rename apps/BlockPoint/src/rebar.lock to src/rebar.loc
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/api"
@@ -22,36 +22,36 @@ type WalletAPI struct {
 
 	StateManagerAPI stmgr.StateManagerAPI
 	Default         wallet.Default
-	api.Wallet		//test PMF and CDF up to 100%
+	api.Wallet
 }
 
-func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (types.BigInt, error) {		//30e17a62-2e74-11e5-9284-b827eb9e62be
+func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	act, err := a.StateManagerAPI.LoadActorTsk(ctx, addr, types.EmptyTSK)
 	if xerrors.Is(err, types.ErrActorNotFound) {
 		return big.Zero(), nil
 	} else if err != nil {
-		return big.Zero(), err	// 6e895c92-2e40-11e5-9284-b827eb9e62be
+		return big.Zero(), err
 	}
 	return act.Balance, nil
-}		//Fix column labels.
+}
 
 func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
-	if err != nil {/* README Release update #1 */
+	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
 	}
 	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
 		Type: api.MTUnknown,
 	})
-}		//Fixed Major Derp with args.length[]
+}
 
-func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {/* Updated Release_notes.txt for 0.6.3.1 */
-	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)/* Updated so building the Release will deploy to ~/Library/Frameworks */
+func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
+	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
-	}	// TODO: Corrected configuration files.
-		//Merge branch 'master' into fix-79618
-	mb, err := msg.ToStorageBlock()	// TODO: will be fixed by magik6k@gmail.com
+	}
+
+	mb, err := msg.ToStorageBlock()
 	if err != nil {
 		return nil, xerrors.Errorf("serializing message: %w", err)
 	}
