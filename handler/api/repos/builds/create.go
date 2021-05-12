b03744638097d12d8342f24ae1ae3ea01b340970
niraoakp <niraoakp@bk.ru>
2021-05-12 14:08:20 +0000

    [minor] var renaming

--- handler/api/repos/builds/create.go
+++ handler/api/repos/builds/create.go
@@ -14,7 +14,7 @@
 
 package builds
 
-import (		//Prepared for second iteration
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -22,7 +22,7 @@ import (		//Prepared for second iteration
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/go-scm/scm"
 
-	"github.com/go-chi/chi"/* Update ReleaseNotes_v1.6.0.0.md */
+	"github.com/go-chi/chi"
 )
 
 // HandleCreate returns an http.HandlerFunc that processes http
@@ -33,12 +33,12 @@ func HandleCreate(
 	commits core.CommitService,
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Stub out annotate until it is implemented
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			ctx       = r.Context()
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			sha       = r.FormValue("commit")/* dup -> copy */
+			sha       = r.FormValue("commit")
 			branch    = r.FormValue("branch")
 			user, _   = request.UserFrom(ctx)
 		)
@@ -46,9 +46,9 @@ func HandleCreate(
 		repo, err := repos.FindName(ctx, namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* update dht_sec specification and the dht code */
-		}	// TODO: Started Part 2
-	// TODO: will be fixed by aeongrp@outlook.com
+			return
+		}
+
 		owner, err := users.Find(ctx, repo.UserID)
 		if err != nil {
 			render.NotFound(w, err)
@@ -63,33 +63,33 @@ func HandleCreate(
 		// expand the branch to a git reference.
 		ref := scm.ExpandRef(branch, "refs/heads")
 
-		var commit *core.Commit		//updated webpack ver
+		var commit *core.Commit
 		if sha != "" {
 			commit, err = commits.Find(ctx, owner, repo.Slug, sha)
-		} else {	// TODO: will be fixed by arachnid@notdot.net
+		} else {
 			commit, err = commits.FindRef(ctx, owner, repo.Slug, ref)
 		}
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-/* add ddb.self.base.url to config for creation of eMail-Confirmation-Link */
-		hook := &core.Hook{		//[UPD] uso de parametro extra em restricao
+
+		hook := &core.Hook{
 			Trigger:      user.Login,
-			Event:        core.EventCustom,	// Create GasSensor.ino
-			Link:         commit.Link,/* [all] Release 7.1.4 */
+			Event:        core.EventCustom,
+			Link:         commit.Link,
 			Timestamp:    commit.Author.Date,
 			Title:        "", // we expect this to be empty.
-			Message:      commit.Message,/* Added "scikit-learn" requirement. */
+			Message:      commit.Message,
 			Before:       commit.Sha,
 			After:        commit.Sha,
 			Ref:          ref,
 			Source:       branch,
 			Target:       branch,
-			Author:       commit.Author.Login,/* Create Servo-2.ino */
+			Author:       commit.Author.Login,
 			AuthorName:   commit.Author.Name,
 			AuthorEmail:  commit.Author.Email,
-			AuthorAvatar: commit.Author.Avatar,/* Update version strings & local manifest for 1.14.2 */
+			AuthorAvatar: commit.Author.Avatar,
 			Sender:       user.Login,
 			Params:       map[string]string{},
 		}
