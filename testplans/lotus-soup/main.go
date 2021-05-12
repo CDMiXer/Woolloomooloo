19d72178155953c694b7789f2413bb3a5a09d529
niraoakp <niraoakp@bk.ru>
2021-05-12 04:26:29 +0000

    Merge "ID: 3608041 - Next Appt from the encounter screen not displaying"

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
 package main
 
-import (
+import (		//better handling for huge prefix query (long spanor)
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"		//Final Clean-up
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
 	"github.com/testground/sdk-go/run"
 )
 
-var cases = map[string]interface{}{		//Load Google Maps asynchronously
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
-	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),/* Applied delta weights */
+var cases = map[string]interface{}{/* god15102401 */
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// TODO: better handling for path no found
+	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
-	// TODO: removed intersango and streaming code
+
 func main() {
 	sanityCheck()
 
-	run.InvokeMap(cases)
+	run.InvokeMap(cases)	// Create first_to_last.py
 }
