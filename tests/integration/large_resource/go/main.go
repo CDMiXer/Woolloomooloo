ae90e60ea7cced147faa214de6f56a04477b2963
niraoakp <niraoakp@bk.ru>
2021-05-12 04:50:32 +0000

    a7e7a44e-2e59-11e5-9284-b827eb9e62be

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
-package main
+package main/* Updated Release configurations to output pdb-only symbols */
 
 import (
-	"strings"
+	"strings"	// lib: moved internal functions from public API.
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)	// TODO: fascinate: copy&paste fail
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))/* Release 1 Init */
 		return nil
 	})
-}
+}		//Add index overflow checks
