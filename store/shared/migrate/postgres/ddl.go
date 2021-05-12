2febc0981f107813caa9210d9286d8f82cc275b2
niraoakp <niraoakp@bk.ru>
2021-05-12 18:37:20 +0000

    * Makefile.am: only run "make" in subdirectories that have things to build.

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release of eeacms/plonesaas:5.2.1-15 */
 // +build !oss
 
-package postgres
+package postgres	// TODO: The first commit for 3rd party api module
 
 //go:generate togo ddl -package postgres -dialect postgres
