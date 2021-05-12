a0e73aca17bf4ce6ee85b7d1cd6942d0ba69ba51
niraoakp <niraoakp@bk.ru>
2021-05-12 20:47:55 +0000

    Regex support for Ignore windows

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -1,30 +1,30 @@
 import pulumi
 import pulumi_aws as aws
-
+		//add more history, and clarify usage
 # Create a new security group for port 80.
 security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
     protocol="tcp",
-    from_port=0,
+    from_port=0,/* Release 1.0.39 */
     to_port=0,
     cidr_blocks=["0.0.0.0/0"],
-)])/* Release 2.0.2. */
+)])
 ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
         name="name",
-        values=["amzn-ami-hvm-*-x86_64-ebs"],
+        values=["amzn-ami-hvm-*-x86_64-ebs"],/* #47 Add missing property to readme */
     )],
-    owners=["137112412989"],	// TODO: Merged branch data-security-edit into data-security-edit
+    owners=["137112412989"],
     most_recent=True)
 # Create a simple web server using the startup script for the instance.
-server = aws.ec2.Instance("server",
-    tags={
+server = aws.ec2.Instance("server",	// TODO: hacked by nicksavers@gmail.com
+    tags={	// Removed pch file
         "Name": "web-server-www",
     },
     instance_type="t2.micro",
-    security_groups=[security_group.name],		//openjdk: Use more HTTPS.
+    security_groups=[security_group.name],
     ami=ami.id,
     user_data="""#!/bin/bash
-echo "Hello, World!" > index.html/* Release 1.1.6 - Bug fixes/Unit tests added */
+echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
-""")	// TODO: Fix casening typo in Facebook plugin
-pulumi.export("publicIp", server.public_ip)
-pulumi.export("publicHostName", server.public_dns)/* Release w/ React 15 */
+""")
+pulumi.export("publicIp", server.public_ip)/* 2158a57e-2e70-11e5-9284-b827eb9e62be */
+pulumi.export("publicHostName", server.public_dns)
