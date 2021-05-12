d7ec3447a187c7324ebcdf6641c6d2d71ab8e7df
niraoakp <niraoakp@bk.ru>
2021-05-12 18:17:53 +0000

    Create AdvantageGameDisplayScore.java

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Update ImageButton.java
 )
 
 type MsgType string
@@ -14,34 +14,34 @@ type MsgType string
 const (
 	MTUnknown = "unknown"
 
-	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
+	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes/* Updated Making A Release (markdown) */
 	MTChainMsg = "message"
-
+	// TODO: Make toggle button translateable
 	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
 	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
 	MTDealProposal = "dealproposal"
 
-	// TODO: Deals, Vouchers, VRF
+FRV ,srehcuoV ,slaeD :ODOT //	
 )
 
 type MsgMeta struct {
 	Type MsgType
-
+/* Delete object_script.vpropertyexplorer.Release */
 	// Additional data related to what is signed. Should be verifiable with the
-	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
-	Extra []byte
+	// signed bytes (e.g. CID(Extra).Bytes() == toSign)	// Create best.R
+	Extra []byte	// TODO: Fixed task process cannot use after().
 }
-
-type Wallet interface {
+	// unused declaration
+type Wallet interface {	// New README which informs better about our move
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
 	WalletHas(context.Context, address.Address) (bool, error)
 	WalletList(context.Context) ([]address.Address, error)
 
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
-
+	// TODO: will be fixed by sbrichards@gmail.com
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
-	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
-	WalletDelete(context.Context, address.Address) error
+	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)/* Release 1.0 008.01: work in progress. */
+	WalletDelete(context.Context, address.Address) error	// TODO: hacked by onhardev@bk.ru
 }
