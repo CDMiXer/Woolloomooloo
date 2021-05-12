504175fd27ec6851687f761fa2108a7d96990beb
niraoakp <niraoakp@bk.ru>
2021-05-12 04:34:05 +0000

    UI tests, release notes, promotional.docx

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* cambios orden del dia */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,43 +8,43 @@ import (
 	"context"
 	"encoding/json"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: hacked by fjl@ethereum.org
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"
-
+	"github.com/drone/drone/mock"		//Chapters: fixes #291 layout fixes for overflow true vertical layout
+	"github.com/drone/drone/core"		//Update fill_col.py
+	// TODO: hacked by vyzo@hackzen.org
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-)
+	"github.com/google/go-cmp/cmp"/* Merge "[INTERNAL] Release notes for version 1.36.1" */
+)/* Release of eeacms/apache-eea-www:5.1 */
 
-func TestChown(t *testing.T) {
-	controller := gomock.NewController(t)
+func TestChown(t *testing.T) {/* Merge branch 'master' into feature/rest-api-message-read-receipts */
+	controller := gomock.NewController(t)/* Finished up storeConcepts() method in Concepts.java. */
 	defer controller.Finish()
-
+		//c441ed3e-2e68-11e5-9284-b827eb9e62be
 	user := &core.User{
 		ID: 42,
 	}
 	repo := &core.Repository{
-		ID:     1,
+		ID:     1,	// Move exo-sync files into subfolder
 		UserID: 1,
 	}
 
 	checkChown := func(_ context.Context, updated *core.Repository) error {
 		if got, want := updated.UserID, user.ID; got != want {
 			t.Errorf("Want repository owner updated to %d, got %d", want, got)
-		}
+		}/* remove yggdrasilwiki config as wiki is being deleted */
 		return nil
 	}
-
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
+	// Sparql writter 
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: will be fixed by cory@protocol.ai
+	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)	// TODO: LbsoNrehcVYGWvet1RrOizMyFAQt5pYV
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")		//7e236b8e-2e68-11e5-9284-b827eb9e62be
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
