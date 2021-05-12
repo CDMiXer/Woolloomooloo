bfd57b08f345fe656ba6eb5c7c453df93e2a257c
niraoakp <niraoakp@bk.ru>
2021-05-12 12:48:33 +0000

    replaced by BasicFileSys

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
-
-import (	// Merge branch 'master' into feature/emoji-custom
-	"go.uber.org/fx"		//Log restarting due to FFmpeg errors
+/* Issue 110, custom frontend events and some minor fixes */
+import (
+	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"/* Delete some more code */
+	"github.com/filecoin-project/lotus/chain/store"
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)/* Embed @Ghosh's uiGradient in order to create those interpolated gradients */
 	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
-		OnStart: sm.Start,
+		OnStart: sm.Start,		//ZAPI-203: Missing imgapiUrl config variable
 		OnStop:  sm.Stop,
-	})		//Merge "Disable Ceilometer API by default on undercloud"
-	return sm, nil
-}
+	})
+	return sm, nil		//Added simple fixed int list
+}/* Редактирование текста: рефакторинг системы создания элементов. */
