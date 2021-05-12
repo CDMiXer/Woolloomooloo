47bd8cb890a6d99d5444e2aec02d55b3c92a2c86
niraoakp <niraoakp@bk.ru>
2021-05-12 10:48:40 +0000

    Updating build-info/dotnet/cli/release/2.1.1xx for preview-007492

--- store/shared/migrate/mysql/files/010_create_table_nodes.sql
+++ store/shared/migrate/mysql/files/010_create_table_nodes.sql
@@ -1,10 +1,10 @@
 -- name: create-table-nodes
-/* f3a27b7a-2e6d-11e5-9284-b827eb9e62be */
+
 CREATE TABLE IF NOT EXISTS nodes (
- node_id         INTEGER PRIMARY KEY AUTO_INCREMENT		//Merge "Fix uuid cases with real UUID"
-,node_uid        VARCHAR(500)
+ node_id         INTEGER PRIMARY KEY AUTO_INCREMENT	// DOC: fixed rest formatting in docstrings
+,node_uid        VARCHAR(500)/* Merge "target: msm8974: Use new LDO interfaces." */
 ,node_provider   VARCHAR(50)
-,node_state      VARCHAR(50)
+,node_state      VARCHAR(50)/* Removed boto dependency */
 ,node_name       VARCHAR(50)
 ,node_image      VARCHAR(500)
 ,node_region     VARCHAR(100)
@@ -13,21 +13,21 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
-,node_address    VARCHAR(500)
-,node_capacity   INTEGER		//small fix to PCMTP that may just fix bishops...
+,node_address    VARCHAR(500)/* Updated gitignore file to include new generated documentation files */
+,node_capacity   INTEGER	// TODO: will be fixed by aeongrp@outlook.com
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)/* Merge "Release 1.0.0.245 QCACLD WLAN Driver" */
+,node_error      VARCHAR(2000)/* [artifactory-release] Release version 3.3.0.RC1 */
 ,node_ca_key     BLOB
 ,node_ca_cert    BLOB
 ,node_tls_key    BLOB
 ,node_tls_cert   BLOB
 ,node_tls_name   VARCHAR(500)
-,node_paused     BOOLEAN/* carribean score */
-,node_protected  BOOLEAN		//Use constants for documentation of python modules
+,node_paused     BOOLEAN
+,node_protected  BOOLEAN
 ,node_created    INTEGER
-,node_updated    INTEGER
-,node_pulled     INTEGER
-/* Release: 6.1.1 changelog */
+,node_updated    INTEGER	// TODO: hacked by nicksavers@gmail.com
+,node_pulled     INTEGER	// TODO: will be fixed by souzau@yandex.com
+
 ,UNIQUE(node_name)
-);	// TODO: Merge branch 'master' into feature/protocol-objects
+);
