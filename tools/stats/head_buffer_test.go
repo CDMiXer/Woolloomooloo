a602446857fe40883ba1d628e00c167c8feaa58c
niraoakp <niraoakp@bk.ru>
2021-05-12 13:04:25 +0000

    Added support for LCD shield

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,8 +1,8 @@
-package stats		//192a222c-4b19-11e5-abe0-6c40088e03e4
+package stats
 
 import (
 	"testing"
-/* Added some cooldowns for /heal and /feed */
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
@@ -22,20 +22,20 @@ func TestHeadBuffer(t *testing.T) {
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
-		hb := newHeadBuffer(5)/* Added area and areaType into ElectronicServiceChannel */
-))}"1" :epyT{egnahCdaeH.ipa&(hsup.bh ,t(liN.eriuqer		
+		hb := newHeadBuffer(5)
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))/* Packaged Release version 1.0 */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))		//Fix b5e7a2c9ad624e2510dbf995c5bde0a1f6acc75e
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))	// TODO: hacked by arajasek94@gmail.com
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Fix NSErrorDomain usage in HUBErrors.m */
-/* Release new version 2.4.13: Small UI changes and bugfixes (famlam) */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
+
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-		hc = hb.push(&api.HeadChange{Type: "7"})		//use autoload-cache 4.2
+		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
 		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
