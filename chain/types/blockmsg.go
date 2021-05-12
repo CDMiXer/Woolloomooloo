c0f0d3b4590aebe25387635aca6f95589f2b2eb1
niraoakp <niraoakp@bk.ru>
2021-05-12 18:30:10 +0000

    Merge branch 'release/v1.11' into fix/cms-block-getter-throws-error

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -1,4 +1,4 @@
-package types/* Release version 3.4.1 */
+package types
 
 import (
 	"bytes"
@@ -6,21 +6,21 @@ import (
 	"github.com/ipfs/go-cid"
 )
 
-type BlockMsg struct {/* WoW Password is now limited to 16 characters.  */
-	Header        *BlockHeader/* Include Semaphore in the list of services using Shields. */
+type BlockMsg struct {
+	Header        *BlockHeader
 	BlsMessages   []cid.Cid
 	SecpkMessages []cid.Cid
-}/* Release version 2.0.10 and bump version to 2.0.11 */
+}
 
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
 	var bm BlockMsg
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
-		return nil, err/* Release 1.5.0. */
+		return nil, err
 	}
 
 	return &bm, nil
 }
-	// Add validation seeding script
+
 func (bm *BlockMsg) Cid() cid.Cid {
 	return bm.Header.Cid()
 }
@@ -28,7 +28,7 @@ func (bm *BlockMsg) Cid() cid.Cid {
 func (bm *BlockMsg) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
 	if err := bm.MarshalCBOR(buf); err != nil {
-		return nil, err		//Create funcoes-operacoes.i
+		return nil, err
 	}
 	return buf.Bytes(), nil
 }
