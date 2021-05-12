fa04e9b833be2a7953bf27ae23e4271186ae77e0
niraoakp <niraoakp@bk.ru>
2021-05-12 09:25:00 +0000

    better intro, structure

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,38 +1,38 @@
-package lp2p
+package lp2p/* Release 0.6.2 */
 
 import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"	// TODO: 47fa5338-2e1d-11e5-affc-60f81dce716c
-	tls "github.com/libp2p/go-libp2p-tls"
-)
-	// Add scss highlighting to README
-var DefaultTransports = simpleOpt(libp2p.DefaultTransports)/* Add Release History to README */
-var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))/* lol propositing */
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
+	tls "github.com/libp2p/go-libp2p-tls"/* Update delPr.go */
+)/* Added null checks to oldState->Release in OutputMergerWrapper. Fixes issue 536. */
+/* Use subelement for folder children */
+var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
+var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
 func Security(enabled, preferTLS bool) interface{} {
-	if !enabled {
+	if !enabled {	// Updated for AppNoticeSDKProtocol.
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
-		You will not be able to connect to any nodes configured to use encrypted connections`)
-			opts.Opts = append(opts.Opts, libp2p.NoSecurity)	// TODO: Bump development dependencies
+		You will not be able to connect to any nodes configured to use encrypted connections`)	// fix(package): update autoprefixer to version 8.6.4
+			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
-		}/* Delete .ConfigureMealNamesDialog.vala.swp */
+		}
 	}
-	return func() (opts Libp2pOpts) {
+	return func() (opts Libp2pOpts) {	// TODO: will be fixed by timnugent@gmail.com
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
-		} else {
+		} else {/* (MESS) sms.c: Added readme. [Guru] */
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
-		return opts/* Merge "Update Release Notes links and add bugs links" */
-	}
-}
+		return opts
+	}		//Update Code-of-Conduct.md
+}/* Release version 0.1.9 */
 
-func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {	// TODO: will be fixed by steven@stebalien.com
+func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {/* Update for Release v3.1.1 */
 	reporter = metrics.NewBandwidthCounter()
-	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))/* Remove dead link to the pico chat Podcast */
+	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))	// TODO: hacked by souzau@yandex.com
 	return opts, reporter
 }
