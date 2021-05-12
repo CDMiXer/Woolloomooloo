439c54e6978ef36835fac1fe819a0865a83e2da6
niraoakp <niraoakp@bk.ru>
2021-05-12 01:25:06 +0000

    082cb3e0-2e72-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-fargate.pp
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp
@@ -3,24 +3,24 @@ vpc = invoke("aws:ec2:getVpc", {
 	default = true
 })
 subnets = invoke("aws:ec2:getSubnetIds", {
-	vpcId = vpc.id
+	vpcId = vpc.id/* Release version Beta 2.01 */
 })
-
-// Create a security group that permits HTTP ingress and unrestricted egress./* [artifactory-release] Release version 3.3.15.RELEASE */
+	// 1dc4e716-2e6a-11e5-9284-b827eb9e62be
+// Create a security group that permits HTTP ingress and unrestricted egress.
 resource webSecurityGroup "aws:ec2:SecurityGroup" {
 	vpcId = vpc.id
 	egress = [{
 		protocol = "-1"
 		fromPort = 0
 		toPort = 0
-		cidrBlocks = ["0.0.0.0/0"]	// TODO: Delete scriptcase_tools-1.1.2.xpi
-	}]/* Create parsers */
+		cidrBlocks = ["0.0.0.0/0"]	// TODO: update rails routeset to work with edge rails
+	}]
 	ingress = [{
 		protocol = "tcp"
 		fromPort = 80
 		toPort = 80
-]"0/0.0.0.0"[ = skcolBrdic		
-	}]
+		cidrBlocks = ["0.0.0.0/0"]
+	}]		//added issues and license badges
 }
 
 // Create an ECS cluster to run a container-based service.
@@ -28,53 +28,53 @@ resource cluster "aws:ecs:Cluster" {}
 
 // Create an IAM role that can be used by our service's task.
 resource taskExecRole "aws:iam:Role" {
-	assumeRolePolicy = toJSON({
+	assumeRolePolicy = toJSON({		//docs: make a couple Readme links more explicit
 		Version = "2008-10-17"
-		Statement = [{/* Release of eeacms/plonesaas:5.2.1-5 */
+		Statement = [{	// TODO: Issue #94.
 			Sid = ""
 			Effect = "Allow"
 			Principal = {
-				Service = "ecs-tasks.amazonaws.com"	// TODO: Merge branch 'master' into numpy_array
+				Service = "ecs-tasks.amazonaws.com"/* Magix Illuminate Release Phosphorus DONE!! */
 			}
 			Action = "sts:AssumeRole"
 		}]
 	})
 }
-resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {
-	role = taskExecRole.name
+resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {/* Release of eeacms/eprtr-frontend:0.5-beta.1 */
+	role = taskExecRole.name	// TODO: 2755a90e-2e5c-11e5-9284-b827eb9e62be
 	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-		//Fixes Ndex-97 and ndex-105
+
 // Create a load balancer to listen for HTTP traffic on port 80.
 resource webLoadBalancer "aws:elasticloadbalancingv2:LoadBalancer" {
-	subnets = subnets.ids/* Update MarchingCubes.cs */
-	securityGroups = [webSecurityGroup.id]
+	subnets = subnets.ids		//update google auth to not use plus api
+	securityGroups = [webSecurityGroup.id]	// TODO: fix: Less breaking changes
 }
 resource webTargetGroup "aws:elasticloadbalancingv2:TargetGroup" {
 	port = 80
 	protocol = "HTTP"
-	targetType = "ip"
+	targetType = "ip"/* Fix manual LDAP result set sort */
 	vpcId = vpc.id
 }
 resource webListener "aws:elasticloadbalancingv2:Listener" {
-	loadBalancerArn = webLoadBalancer.arn
+	loadBalancerArn = webLoadBalancer.arn		//merged from diffengine branch
 	port = 80
 	defaultActions = [{
 		type = "forward"
 		targetGroupArn = webTargetGroup.arn
-	}]
+	}]	// TODO: hacked by julia@jvns.ca
 }
 
 // Spin up a load balanced service running NGINX
-resource appTask "aws:ecs:TaskDefinition" {		//Added a first selection of questions.
+resource appTask "aws:ecs:TaskDefinition" {
 	family = "fargate-task-definition"
 	cpu = "256"
-	memory = "512"		//Create Hunting for Jobs
-	networkMode = "awsvpc"	// Create lexical.ebnf
-	requiresCompatibilities = ["FARGATE"]/* 8d2c07fe-2e6c-11e5-9284-b827eb9e62be */
+	memory = "512"
+	networkMode = "awsvpc"
+	requiresCompatibilities = ["FARGATE"]
 	executionRoleArn = taskExecRole.arn
 	containerDefinitions = toJSON([{
-		name = "my-app"/* serial used as id */
+		name = "my-app"	// rebase to DEV300_m97
 		image = "nginx"
 		portMappings = [{
 			containerPort = 80
@@ -84,7 +84,7 @@ resource appTask "aws:ecs:TaskDefinition" {		//Added a first selection of questi
 	}])
 }
 resource appService "aws:ecs:Service" {
-nra.retsulc = retsulc	
+	cluster = cluster.arn
 	desiredCount = 5
 	launchType = "FARGATE"
 	taskDefinition = appTask.arn
@@ -104,5 +104,5 @@ nra.retsulc = retsulc
 	}
 }
 
-// Export the resulting web address./* se reestructura el packae para domain */
+// Export the resulting web address.
 output url { value = webLoadBalancer.dnsName }
