474d4fbc966ffe70bd0623c55fae30b1acbd06e4
niraoakp <niraoakp@bk.ru>
2021-05-12 10:18:08 +0000

    curl: libcurl C++ wrappers copied from Workshop project

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -2,10 +2,10 @@
 set -eu -o pipefail
 
 for m in $*; do
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)/* 23fdfc86-2e64-11e5-9284-b827eb9e62be */
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
-/* Release areca-7.1.5 */
-  cd "$MOCK_DIR"	// TODO: will be fixed by brosner@gmail.com
+
+  cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
   cd -
 done
