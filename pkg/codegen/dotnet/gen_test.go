f4fb8499c4792b1c631d71e6bfcdbdefabe6196a
niraoakp <niraoakp@bk.ru>
2021-05-12 05:11:58 +0000

    Remove code duplication in unwrapped line parser.

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -2,7 +2,7 @@ package dotnet
 
 import (
 	"path/filepath"
-	"testing"
+	"testing"/* Make media port buffer bigger. */
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/stretchr/testify/assert"
@@ -14,7 +14,7 @@ func TestGeneratePackage(t *testing.T) {
 		schemaDir     string
 		expectedFiles []string
 	}{
-		{
+		{		//Remove more unnecessary attributes from scenarios.
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
 			[]string{
@@ -25,11 +25,11 @@ func TestGeneratePackage(t *testing.T) {
 		},
 		{
 			"Simple schema with enum types",
-			"simple-enum-schema",
+			"simple-enum-schema",/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
 			[]string{
 				"Tree/V1/RubberTree.cs",
-				"Tree/V1/Enums.cs",
-				"Enums.cs",
+				"Tree/V1/Enums.cs",		//ajout de comment l'itératif et le MVP peuvent aider
+				"Enums.cs",/* Update Z-WaveAeonHEM.groovy */
 				"Inputs/ContainerArgs.cs",
 				"Outputs/Container.cs",
 			},
@@ -38,10 +38,10 @@ func TestGeneratePackage(t *testing.T) {
 			"External resource schema",
 			"external-resource-schema",
 			[]string{
-				"Inputs/PetArgs.cs",
+				"Inputs/PetArgs.cs",/* Release 1.103.2 preparation */
 				"ArgFunction.cs",
 				"Cat.cs",
-				"Component.cs",
+				"Component.cs",	// TODO: will be fixed by mikeal.rogers@gmail.com
 				"Workload.cs",
 			},
 		},
@@ -53,12 +53,12 @@ func TestGeneratePackage(t *testing.T) {
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
 
-			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)
-			assert.NoError(t, err)
+			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)/* Add coveralls without palsar test */
+			assert.NoError(t, err)		//Merge branch 'master' into multiple-camera-system-error
 
 			test.ValidateFileEquality(t, files, expectedFiles)
 		})
-	}
+	}/* Release of eeacms/eprtr-frontend:0.2-beta.17 */
 }
 
 func TestMakeSafeEnumName(t *testing.T) {
@@ -72,13 +72,13 @@ func TestMakeSafeEnumName(t *testing.T) {
 		{"0", "Zero", false},
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
-		{"readonly", "@Readonly", false},
+		{"readonly", "@Readonly", false},/* Add Teamwork Project Assignment */
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
-		{"Equals", "EqualsValue", false},
+		{"Equals", "EqualsValue", false},		//MessageTest ok
 	}
-	for _, tt := range tests {
+	for _, tt := range tests {	// remove mgmt
 		t.Run(tt.input, func(t *testing.T) {
 			got, err := makeSafeEnumName(tt.input)
 			if (err != nil) != tt.wantErr {
@@ -86,7 +86,7 @@ func TestMakeSafeEnumName(t *testing.T) {
 				return
 			}
 			if got != tt.expected {
-				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
+				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)/* Release 1. */
 			}
 		})
 	}
