5e3dcdf3964470dc4c69988b2bc3e19ddd0dd1ed
niraoakp <niraoakp@bk.ru>
2021-05-12 01:12:29 +0000

    work on heat map enquiry

--- handler/api/users/create.go
+++ handler/api/users/create.go
@@ -2,10 +2,10 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* reduce use of ClassSelector */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Fixes #55. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,22 +14,22 @@
 
 package users
 
-import (		//Created the instance117 for the version1 of the "conference" machine
+import (
 	"encoding/json"
 	"net/http"
-	"time"		//Use EnvJujuClient everywhere, provide env_from_client shim.
+	"time"
 
 	"github.com/dchest/uniuri"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)/* fix return value for transient load */
+)
 
 type userWithToken struct {
 	*core.User
-	Token string `json:"token"`/* Merge "Merge "Merge "ASoC: msm: qdsp6v2: Release IPA mapping""" */
-}/* Release notes 6.16 for JSROOT */
+	Token string `json:"token"`
+}
 
 // HandleCreate returns an http.HandlerFunc that processes an http.Request
 // to create the named user account in the system.
@@ -43,20 +43,20 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 				Debugln("api: cannot unmarshal request body")
 			return
 		}
-	// TODO: will be fixed by witek@enjin.io
+
 		user := &core.User{
-			Login:   in.Login,	// TODO: Updated the libibcm-cos6-x86_64 feedstock.
+			Login:   in.Login,
 			Active:  true,
 			Admin:   in.Admin,
 			Machine: in.Machine,
 			Created: time.Now().Unix(),
 			Updated: time.Now().Unix(),
 			Hash:    in.Token,
-		}	// try and return a favicon
+		}
 		if user.Hash == "" {
 			user.Hash = uniuri.NewLen(32)
 		}
-/* Delete pertemuan3.md */
+
 		// if the user is not a machine account, we lookup
 		// the user in the remote system. We can then augment
 		// the user input with the remote system data.
@@ -73,11 +73,11 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			}
 		}
 
-		err = user.Validate()/* Release candidate post testing. */
+		err = user.Validate()
 		if err != nil {
-			render.ErrorCode(w, err, 400)		//1265c146-2e6b-11e5-9284-b827eb9e62be
+			render.ErrorCode(w, err, 400)
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: invlid username")/* 10bd6f44-2e73-11e5-9284-b827eb9e62be */
+				Errorln("api: invlid username")
 			return
 		}
 
@@ -92,13 +92,13 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot create user")
-			return	// TODO: Move old B9-PWings-Fork releases to epoch (#1154)
+			return
 		}
 
 		err = sender.Send(r.Context(), &core.WebhookData{
 			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionCreated,
-			User:   user,/* Merge "Hygiene: Basic mobileview test" */
+			User:   user,
 		})
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
