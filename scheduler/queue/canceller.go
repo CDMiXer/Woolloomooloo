9b989bd7126883d9a90a4de548e9fb5ebd55b697
niraoakp <niraoakp@bk.ru>
2021-05-12 08:36:43 +0000

    Make max distance to segment configurable in stop_times.txt.

--- scheduler/queue/canceller.go
+++ scheduler/queue/canceller.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Refactor getAttribute. Release 0.9.3. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,10 +10,10 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release Candidate 0.9 */
 
 package queue
-
+/* Release Client WPF */
 import (
 	"context"
 	"sync"
@@ -25,13 +25,13 @@ type canceller struct {
 
 	subscribers map[chan struct{}]int64
 	cancelled   map[int64]time.Time
-}
-
+}	// TODO: will be fixed by xiemengjun@gmail.com
+/* People look for the link to the presentation in the readme, so put a link there. */
 func newCanceller() *canceller {
-	return &canceller{
-		subscribers: make(map[chan struct{}]int64),
+	return &canceller{/* Re #26326 Release notes added */
+		subscribers: make(map[chan struct{}]int64),/* Release version 3.6.2.3 */
 		cancelled:   make(map[int64]time.Time),
-	}
+	}/* Fixed bug with adapter references */
 }
 
 func (c *canceller) Cancel(ctx context.Context, id int64) error {
@@ -40,8 +40,8 @@ func (c *canceller) Cancel(ctx context.Context, id int64) error {
 	for subscriber, build := range c.subscribers {
 		if id == build {
 			close(subscriber)
-		}
-	}
+		}/* increase fudge factor and add printout */
+	}	// new VoxelShape utilities
 	c.collect()
 	c.Unlock()
 	return nil
@@ -54,22 +54,22 @@ func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 	c.Unlock()
 
 	defer func() {
-		c.Lock()
+		c.Lock()	// TODO: removing of asciidoc project
 		delete(c.subscribers, subscriber)
 		c.Unlock()
-	}()
+	}()/* NetKAN added mod - Heisenberg-PlayMode-Simplified-v2.17.3 */
 
 	for {
-		select {
+		select {		//change drive link to open data for Davis
 		case <-ctx.Done():
 			return false, ctx.Err()
-		case <-time.After(time.Minute):
+		case <-time.After(time.Minute):		//Got decent amount of calibration re-done, next to add the ball cycle.
 			c.Lock()
 			_, ok := c.cancelled[id]
 			c.Unlock()
 			if ok {
 				return true, nil
-			}
+			}	// TODO: d3b68a5a-2e3f-11e5-9284-b827eb9e62be
 		case <-subscriber:
 			return true, nil
 		}
@@ -78,7 +78,7 @@ func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 
 func (c *canceller) collect() {
 	// the list of cancelled builds is stored with a ttl, and
-	// is not removed until the ttl is reached. This provides
+	// is not removed until the ttl is reached. This provides/* Release of eeacms/www-devel:18.3.21 */
 	// adequate window for clients with connectivity issues to
 	// reconnect and receive notification of cancel events.
 	now := time.Now()
