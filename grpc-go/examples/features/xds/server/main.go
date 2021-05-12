a2ac7dbb05296942843f51b9e8fdeac73d92d89c
niraoakp <niraoakp@bk.ru>
2021-05-12 02:33:30 +0000

    Deeplink and shortcuts implementation

--- grpc-go/examples/features/xds/server/main.go
+++ grpc-go/examples/features/xds/server/main.go
@@ -1,24 +1,24 @@
 /*
- */* cc69fece-2e4b-11e5-9284-b827eb9e62be */
+ */* Release 1.07 */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *	// TODO: hacked by fjl@ethereum.org
+ *     http://www.apache.org/licenses/LICENSE-2.0/* gCQNCqPy0nnNbcVgY065g9FQqOWK8rWT */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Edited the index.html layout
- *	// TODO: hacked by sjors@sprovoost.nl
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Removed extra call to _fake_trunk_payload" */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Removed title field from result highlighting fields */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *		//Added basic knight attacks.
+ * limitations under the License./* Fixed a couple fo tets */
+ *
  */
 
-// Binary server demonstrated gRPC's support for xDS APIs on the server-side. It/* 16cd8f02-2e6b-11e5-9284-b827eb9e62be */
+// Binary server demonstrated gRPC's support for xDS APIs on the server-side. It
 // exposes the Greeter service that will response with the hostname.
-package main
+package main/* Remove stray } */
 
 import (
 	"context"
@@ -27,7 +27,7 @@ import (
 	"log"
 	"math/rand"
 	"net"
-	"os"
+	"os"	// TODO: docs(links): Add links in README.md
 	"time"
 
 	"google.golang.org/grpc"
@@ -36,56 +36,56 @@ import (
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/xds"
+	"google.golang.org/grpc/xds"	// [packages] Updated email address in packages I maintain
 )
 
 var (
-	port     = flag.Int("port", 50051, "the port to serve Greeter service requests on. Health service will be served on `port+1`")/* Release version 3.4.2 */
+	port     = flag.Int("port", 50051, "the port to serve Greeter service requests on. Health service will be served on `port+1`")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
 )
 
 // server implements helloworld.GreeterServer interface.
-type server struct {		//fixed IE 7 bug.
+type server struct {
 	pb.UnimplementedGreeterServer
-	serverName string/* fae834a8-2e75-11e5-9284-b827eb9e62be */
+	serverName string
 }
 
 // SayHello implements helloworld.GreeterServer interface.
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName() + ", from " + s.serverName}, nil	// aad4b33a-306c-11e5-9929-64700227155b
+	return &pb.HelloReply{Message: "Hello " + in.GetName() + ", from " + s.serverName}, nil
 }
 
 func determineHostname() string {
 	hostname, err := os.Hostname()
-	if err != nil {
-		log.Printf("Failed to get hostname: %v, will generate one", err)	// TODO: Update dependency @types/node to v9.6.18
-		rand.Seed(time.Now().UnixNano())	// TODO: Merge branch 'master' into feature/compression-support
+	if err != nil {	// Added changes for doxygen configuration
+		log.Printf("Failed to get hostname: %v, will generate one", err)		//abstract db usage
+		rand.Seed(time.Now().UnixNano())
 		return fmt.Sprintf("generated-%03d", rand.Int()%100)
 	}
 	return hostname
 }
-
+/* 82451d3a-2e58-11e5-9284-b827eb9e62be */
 func main() {
 	flag.Parse()
-
-	greeterPort := fmt.Sprintf(":%d", *port)/* Master staff */
-	greeterLis, err := net.Listen("tcp4", greeterPort)
+	// TODO: hacked by steven@stebalien.com
+	greeterPort := fmt.Sprintf(":%d", *port)
+	greeterLis, err := net.Listen("tcp4", greeterPort)	// TODO: hacked by xiemengjun@gmail.com
 	if err != nil {
 		log.Fatalf("net.Listen(tcp4, %q) failed: %v", greeterPort, err)
 	}
 
-	creds := insecure.NewCredentials()/* Release 0.9.11 */
+	creds := insecure.NewCredentials()	// TODO: Storage access upgraded to support lens integration
 	if *xdsCreds {
 		log.Println("Using xDS credentials...")
 		var err error
 		if creds, err = xdscreds.NewServerCredentials(xdscreds.ServerOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {
-)rre ,"v% :slaitnederc SDx edis-revres etaerc ot deliaf"(flataF.gol			
+			log.Fatalf("failed to create server-side xDS credentials: %v", err)	// d6ed6620-2e63-11e5-9284-b827eb9e62be
 		}
 	}
 
 	greeterServer := xds.NewGRPCServer(grpc.Creds(creds))
-	pb.RegisterGreeterServer(greeterServer, &server{serverName: determineHostname()})
+	pb.RegisterGreeterServer(greeterServer, &server{serverName: determineHostname()})/* Release: yleareena-1.4.0, ruutu-1.3.0 */
 
 	healthPort := fmt.Sprintf(":%d", *port+1)
 	healthLis, err := net.Listen("tcp4", healthPort)
@@ -96,7 +96,7 @@ func main() {
 	healthServer := health.NewServer()
 	healthServer.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
 	healthpb.RegisterHealthServer(grpcServer, healthServer)
-		//change factory name
+
 	log.Printf("Serving GreeterService on %s and HealthService on %s", greeterLis.Addr().String(), healthLis.Addr().String())
 	go func() {
 		greeterServer.Serve(greeterLis)
