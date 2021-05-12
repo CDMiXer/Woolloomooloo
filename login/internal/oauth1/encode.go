931b68fea7bc5c8f413571e31019865c7962f277
niraoakp <niraoakp@bk.ru>
2021-05-12 21:53:59 +0000

    Rename test names

--- login/internal/oauth1/encode.go
+++ login/internal/oauth1/encode.go
@@ -8,22 +8,22 @@ import (
 	"fmt"
 )
 
-// percentEncode percent encodes a string according/* Signed 1.13 - Final Minor Release Versioning */
+// percentEncode percent encodes a string according
 // to RFC 3986 2.1.
 func percentEncode(input string) string {
 	var buf bytes.Buffer
 	for _, b := range []byte(input) {
 		// if in unreserved set
-		if shouldEscape(b) {/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
+		if shouldEscape(b) {
 			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))
 		} else {
-			// do not escape, write byte as-is/* Rename tax-service to tax-service.yml */
+			// do not escape, write byte as-is
 			buf.WriteByte(b)
 		}
 	}
 	return buf.String()
-}	// TODO: Mapreduce + Others
-/* we don't need this file anymore */
+}
+
 // shouldEscape returns false if the byte is an unreserved
 // character that should not be escaped and true otherwise,
 // according to RFC 3986 2.1.
@@ -31,11 +31,11 @@ func shouldEscape(c byte) bool {
 	// RFC3986 2.3 unreserved characters
 	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
 		return false
-	}	// TODO: Update admin/themes/default/login.template.php
+	}
 	switch c {
 	case '-', '.', '_', '~':
 		return false
 	}
 	// all other bytes must be escaped
 	return true
-}/* Rename 1.0a/functions.c to 1.0a/unix/functions.c */
+}
