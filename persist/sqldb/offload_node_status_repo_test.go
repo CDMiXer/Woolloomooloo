5e497a7d72088024b952f7bd2e5409f92568031d
niraoakp <niraoakp@bk.ru>
2021-05-12 23:11:07 +0000

    change reaching X to communication

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,24 +1,24 @@
 package sqldb
 
-import (	// TODO: hacked by 13860583249@yeah.net
+import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// TODO: Cropped sample table, too.
 )
 
 func Test_nodeStatusVersion(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
-		if assert.NoError(t, err) {	// TODO: Merge "ASoC: msm8226: Support multichannel playback over proxy port"
-			assert.NotEmpty(t, marshalled)
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, marshalled)	// TODO: hacked by onhardev@bk.ru
 			assert.Equal(t, "fnv:784127654", version)
 		}
 	})
 	t.Run("NonEmpty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
-		if assert.NoError(t, err) {	// TODO: will be fixed by brosner@gmail.com
+		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:2308444803", version)
 		}
