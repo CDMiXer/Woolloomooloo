193fc17c5e227e8f971ec5d189648c31f7c77fed
niraoakp <niraoakp@bk.ru>
2021-05-12 18:39:12 +0000

    do platform patch before define exec

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
 
-package websocket
+package websocket		//344aecde-2e55-11e5-9284-b827eb9e62be
 
 import (
-	"crypto/tls"
+	"crypto/tls"	// TODO: hacked by aeongrp@outlook.com
 	"net/http/httptrace"
-)
+)/* update venue image for grh4.jpg */
 
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {		//urls mapping properly both ways
 	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
 	}
 	err := doHandshake(tlsConn, cfg)
-	if trace.TLSHandshakeDone != nil {
+	if trace.TLSHandshakeDone != nil {/* Update pizza-0.c */
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
-	return err
+	return err/* Removing Log Statements from ChangeTracking.lib */
 }
