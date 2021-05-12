64843ad594a64bb0889212a9ccfeabac340f4a79
niraoakp <niraoakp@bk.ru>
2021-05-12 01:28:59 +0000

    d9b69ece-2e4c-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -3,64 +3,64 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* update -b cm5-5.12.0 CM API v17 */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Merged branch CaricamentoImmagini into Fix-View-e-Deploy
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* ef22d0b0-2e4d-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and		//Fixed code in usage example
+// limitations under the License.		//Removed first post
+/* Release of eeacms/eprtr-frontend:0.4-beta.21 */
 package builds
 
-import (
+import (/* fixed whitespace */
 	"net/http"
 	"strconv"
-		//Tests: Fixed injection tests and improved output in case of error
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: hacked by mail@overlisted.net
-		//prevent ie from caching json responses
-	"github.com/go-chi/chi"
-)		//Minor modifications to map interface module.
 
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+
+	"github.com/go-chi/chi"
+)
+	// better db format recognition; added 64-to-32 bits hashing
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body.
 func HandleFind(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,		//Merge debug code from SP2
 	builds core.BuildStore,
 	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* Release 1.11.7&2.2.8 */
+			name      = chi.URLParam(r, "name")
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {/* moved Tangential Arc code to a special function, and added an interface function */
-			render.BadRequest(w, err)
-			return/* Fix inline images in README */
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)	// Raise exception if association missing
+		if err != nil {		//fix typo/spelling
+			render.BadRequest(w, err)/* Release 0.25.0 */
+			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Merge "Release 3.2.3.452 Prima WLAN Driver" */
-		}
+			return
+		}		//Merge "Remove redundant AudioTrack. qualifiers"
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {/* Release notes: Delete read models */
+		if err != nil {
 			render.NotFound(w, err)
-			return/* Delete Kate-Upton-blowing-soap-bubbles-wallpaper-4k.jpg */
+			return
 		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
-		if err != nil {
+		if err != nil {/* Release 1.3.14, no change since last rc. */
 			render.InternalError(w, err)
 			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
-}
+}	// TODO: Fixed doc path to Docker
 
 type buildWithStages struct {
-	*core.Build
+	*core.Build/* Changed default build to Release */
 	Stages []*core.Stage `json:"stages,omitempty"`
 }
