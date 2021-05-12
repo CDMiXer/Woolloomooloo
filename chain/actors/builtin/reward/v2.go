897fd73e9642ee38068f4c62e76993bb6821001f
niraoakp <niraoakp@bk.ru>
2021-05-12 11:06:31 +0000

    variable entfernt

--- chain/actors/builtin/reward/v2.go
+++ chain/actors/builtin/reward/v2.go
@@ -1,4 +1,4 @@
-package reward	// TODO: hacked by why@ipfs.io
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,7 +13,7 @@ import (
 )
 
 var _ State = (*state2)(nil)
-		//Merge "#3320 Buttons for saving document information error out "
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
@@ -21,30 +21,30 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}/* Moved implementations to own package */
-		//Merge branch 'develop' into issue/18-external-feeds
+}
+
 type state2 struct {
 	reward2.State
-	store adt.Store/* Daddelkiste Duomatic - Final Release (Version 1.0) */
-}/* Updating sample plot for Schreiber Ulam map kernel width 0.2 */
+	store adt.Store
+}
 
 func (s *state2) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil/* Release 0.2.3.4 */
-}	// TODO: will be fixed by magik6k@gmail.com
+	return s.State.ThisEpochReward, nil
+}
 
 func (s *state2) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
-		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,/* Release Tag V0.50 */
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
-}/* Fixing of sccp text suit - delay increasing */
-
-func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {	// Merge "Use dynamic timer for cluster status polling"
-	return s.State.ThisEpochBaselinePower, nil/* ea32f6a4-2e55-11e5-9284-b827eb9e62be */
 }
-/* more PWM power saving; fix nunchuck stuff */
+
+func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {
+	return s.State.ThisEpochBaselinePower, nil
+}
+
 func (s *state2) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
 }
@@ -55,8 +55,8 @@ func (s *state2) EffectiveBaselinePower() (abi.StoragePower, error) {
 
 func (s *state2) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}/* Create sum.html */
-/* - Fix a few DxEng APIs without proper calling convention (spotted by Stefan). */
+}
+
 func (s *state2) CumsumBaseline() (reward2.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
