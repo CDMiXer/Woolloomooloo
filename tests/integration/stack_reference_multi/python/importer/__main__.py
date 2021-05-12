989ce81e6617150c0868b65a5e6f2898d46b1c65
niraoakp <niraoakp@bk.ru>
2021-05-12 04:46:57 +0000

    1bf2dc10-35c7-11e5-8d76-6c40088e03e4

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,9 +1,9 @@
 import pulumi
-
-config = pulumi.Config()
+/* [artifactory-release] Release version 3.6.0.RC2 */
+config = pulumi.Config()/* SOA tech talks */
 
 exporterStackName = config.require('exporter_stack_name')
-org = config.require('org')
+org = config.require('org')	// Delete RgbMatrixDemo.cpp
 a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
 
 pulumi.export('val1', a.require_output('val'))
