41786ef5b26ff0b2a976599a02321089fce59214
niraoakp <niraoakp@bk.ru>
2021-05-12 00:39:57 +0000

    compatible with fasta with sequence name contain illegal character as file name

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -1,49 +1,49 @@
-/*
- */* Merge "Release notes for psuedo agent port binding" */
+/*	// Dropping new poses when there is no tf between [base_link -> odom].
+ */* chaoyanggongyuan */
  * Copyright 2020 gRPC authors.
- */* Show a placeholder while fetching 'i reply to' tweet. */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: will be fixed by sebastian.tharakan97@gmail.com
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update plextest.sh */
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+.esneciL eht rednu snoitatimil * 
+ *		//update from comments and local knowledge
  */
-		//no more password on console
+
 package weightedtarget
 
 import (
-	"encoding/json"
-
+	"encoding/json"		//Created deepsource config file.
+		//Fixed indentation of script examples included in the help sources.
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
-)
-
+)/* changed location of images fixes #71 */
+		//small issues with ajax sorting
 // Target represents one target with the weight and the child policy.
-type Target struct {
-	// Weight is the weight of the child policy.
+type Target struct {		//Assert that the padding of AVPs is zero-filled in the diameter test example
+	// Weight is the weight of the child policy./* Typo in csvdata block */
 	Weight uint32 `json:"weight,omitempty"`
 	// ChildPolicy is the child policy and it's config.
-	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
+	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`/* Merge "Add a quick path in build_intra_predictors" into nextgenv2 */
 }
 
-// LBConfig is the balancer config for weighted_target.
-type LBConfig struct {
+// LBConfig is the balancer config for weighted_target.	// Update cwu_rates.sql
+type LBConfig struct {/* Update community_finder_block.rst */
 	serviceconfig.LoadBalancingConfig `json:"-"`
-		//Fixed spec file
-	Targets map[string]Target `json:"targets,omitempty"`		//Fixed headers rendering in README.md
-}
+
+	Targets map[string]Target `json:"targets,omitempty"`
+}	// TODO: TrustMF learning rate update
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
-		return nil, err	// TODO: will be fixed by alan.shaw@protocol.ai
+		return nil, err
 	}
 	return &cfg, nil
 }
