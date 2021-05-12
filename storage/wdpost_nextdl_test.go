37882326e0c2bc06bc333c16c13bc431f6001525
niraoakp <niraoakp@bk.ru>
2021-05-12 09:03:28 +0000

    Add i18n messages for Japanese.

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,16 +1,16 @@
 package storage
-
-import (
+/* Changed HTTP version dep to HTTP >= 3000.0 && < 3000.1 */
+import (	// TODO: 929f16a6-2e6a-11e5-9284-b827eb9e62be
 	"testing"
-
+		//use lazy to avoid duplicate utxo warmup
 	"github.com/stretchr/testify/require"
-
+/* #204 Initial migration of 'tg-date-range-criterion' (DOM part and behavior). */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* cirrus release: new release created for release/0.1.6 */
 )
 
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)
+	periodStart := abi.ChainEpoch(0)	// TODO: Clean up data structure sizes and add Text to ringbuffer.
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
 
@@ -28,9 +28,9 @@ func TestNextDeadline(t *testing.T) {
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)/* 0.8.5 Release for Custodian (#54) */
 		require.EqualValues(t, deadlineIdx, di.Index)
-		require.EqualValues(t, expPeriodStart, di.PeriodStart)
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)		//make some things not fall over on local function definitions
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
