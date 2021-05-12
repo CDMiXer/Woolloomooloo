69a39e338aaaf8251d5eca93fcf13c171d4dbd25
niraoakp <niraoakp@bk.ru>
2021-05-12 14:56:39 +0000

    fix account_payment view

--- core/file.go
+++ core/file.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* 1.16.12 Release */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fixed incorrect form enable/disable in credentials editor */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core		//CMake now always sets CLANG_PATH macro, see #34
+package core
 
 import "context"
 
@@ -21,10 +21,10 @@ type (
 	// version control system.
 	File struct {
 		Data []byte
-		Hash []byte	// Merge "Add NovaAggregates.create_and_list_aggregates"
+		Hash []byte
 	}
 
-	// FileArgs provides repository and commit details required/* Merge branch 'master' into fix-absolute-time-bug */
+	// FileArgs provides repository and commit details required
 	// to fetch the file from the  remote source code management
 	// service.
 	FileArgs struct {
@@ -32,7 +32,7 @@ type (
 		Ref    string
 	}
 
-	// FileService provides access to contents of files in/* Release version 3.1.0.RELEASE */
+	// FileService provides access to contents of files in
 	// the remote source code management service (e.g. GitHub).
 	FileService interface {
 		Find(ctx context.Context, user *User, repo, commit, ref, path string) (*File, error)
