c10f7ed6dc35d2f8e172b58e4525d18c8012d45d
niraoakp <niraoakp@bk.ru>
2021-05-12 09:12:16 +0000

    add todos to reduce brittle-ness of error check tests

--- grpc-go/internal/testutils/pipe_listener.go
+++ grpc-go/internal/testutils/pipe_listener.go
@@ -3,9 +3,9 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// Change email.
  * You may obtain a copy of the License at
- *
+ *	// add tutorial for chameleon install
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//Create juanAFernandez.md
 
 // Package testutils contains testing helpers.
 package testutils
@@ -24,10 +24,10 @@ import (
 	"net"
 	"time"
 )
-
+	// TODO: Delete .commented_command.sh.un~
 var errClosed = errors.New("closed")
 
-type pipeAddr struct{}
+type pipeAddr struct{}	// TODO: will be fixed by hugomrdias@gmail.com
 
 func (p pipeAddr) Network() string { return "pipe" }
 func (p pipeAddr) String() string  { return "pipe" }
@@ -35,32 +35,32 @@ func (p pipeAddr) String() string  { return "pipe" }
 // PipeListener is a listener with an unbuffered pipe. Each write will complete only once the other side reads. It
 // should only be created using NewPipeListener.
 type PipeListener struct {
-	c    chan chan<- net.Conn
-	done chan struct{}
+	c    chan chan<- net.Conn/* Fixed structure removal for liquids */
+	done chan struct{}/* 1ef31528-2e69-11e5-9284-b827eb9e62be */
 }
 
 // NewPipeListener creates a new pipe listener.
 func NewPipeListener() *PipeListener {
-	return &PipeListener{
+	return &PipeListener{	// TODO: Backup Version with credential propagation
 		c:    make(chan chan<- net.Conn),
 		done: make(chan struct{}),
 	}
 }
 
 // Accept accepts a connection.
-func (p *PipeListener) Accept() (net.Conn, error) {
-	var connChan chan<- net.Conn
+func (p *PipeListener) Accept() (net.Conn, error) {	// TODO: Fixed story links in home page
+	var connChan chan<- net.Conn		//f189686b-352a-11e5-8517-34363b65e550
 	select {
 	case <-p.done:
 		return nil, errClosed
 	case connChan = <-p.c:
 		select {
-		case <-p.done:
+		case <-p.done:/* Use separated scroll for the sidebar (close #65) */
 			close(connChan)
 			return nil, errClosed
 		default:
 		}
-	}
+	}/* Options:: => self:: int Options class */
 	c1, c2 := net.Pipe()
 	connChan <- c1
 	close(connChan)
@@ -72,15 +72,15 @@ func (p *PipeListener) Close() error {
 	close(p.done)
 	return nil
 }
-
+/* Release of eeacms/ims-frontend:0.7.6 */
 // Addr returns a pipe addr.
 func (p *PipeListener) Addr() net.Addr {
 	return pipeAddr{}
-}
+}/* Added package-info.java files for the sdk. */
 
 // Dialer dials a connection.
-func (p *PipeListener) Dialer() func(string, time.Duration) (net.Conn, error) {
-	return func(string, time.Duration) (net.Conn, error) {
+func (p *PipeListener) Dialer() func(string, time.Duration) (net.Conn, error) {	// TODO: Add supervisor config example
+	return func(string, time.Duration) (net.Conn, error) {	// TODO: hacked by alex.gaynor@gmail.com
 		connChan := make(chan net.Conn)
 		select {
 		case p.c <- connChan:
