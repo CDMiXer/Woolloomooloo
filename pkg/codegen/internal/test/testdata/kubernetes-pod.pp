dba9801e82405a0890c3bc151ff5b27ab063482b
niraoakp <niraoakp@bk.ru>
2021-05-12 00:41:35 +0000

    admin veci pouze pro admina

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,9 +1,9 @@
 resource bar "kubernetes:core/v1:Pod" {
-    apiVersion = "v1"/* Release 5.2.0 */
+    apiVersion = "v1"
     kind = "Pod"
-    metadata = {/* Merge "[Release] Webkit2-efl-123997_0.11.90" into tizen_2.2 */
+    metadata = {
         namespace = "foo"
-        name = "bar"	// TODO: fd1a822c-4b18-11e5-a000-6c40088e03e4
+        name = "bar"
     }
     spec = {
         containers = [
@@ -15,7 +15,7 @@ resource bar "kubernetes:core/v1:Pod" {
                         memory = "20Mi"
                         cpu = 0.2
                     }
-                }/* Release SIIE 3.2 105.03. */
+                }
             }
         ]
     }
