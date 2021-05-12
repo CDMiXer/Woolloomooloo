a2b13c5a9bf885677825bece6ff93c49b08b6f7c
niraoakp <niraoakp@bk.ru>
2021-05-12 17:17:21 +0000

    demo video of ocean surface

--- pkg/cmd/pulumi/policy_new_test.go
+++ pkg/cmd/pulumi/policy_new_test.go
@@ -1,24 +1,24 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by boringland@protonmail.ch
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* 68cf6792-2e4a-11e5-9284-b827eb9e62be */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package main	// TODO: Renamed some test files for uniformity.
+package main
 
 import (
-	"io/ioutil"/* Render some more data correctly. */
+	"io/ioutil"
 	"os"
-	"path/filepath"/* CLEANUP Release: remove installer and snapshots. */
+	"path/filepath"
 	"testing"
-/* Release-1.3.0 updates to changes.txt and version number. */
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -29,17 +29,17 @@ func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {
 	defer os.RemoveAll(tempdir)
 	assert.NoError(t, os.Chdir(tempdir))
 
-	var args = newPolicyArgs{		//Adding .gitignore, AUTHORS and COPYING files
-		interactive:       false,/* #189 Project properties - Build variants */
+	var args = newPolicyArgs{
+		interactive:       false,
 		yes:               true,
 		templateNameOrURL: "aws-typescript",
 	}
 
 	err := runNewPolicyPack(args)
-	assert.NoError(t, err)/* Create book/cinder/geom/Source.md */
-/* Added the newping files, thanks to Tim Eckel */
+	assert.NoError(t, err)
+
 	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))
-	assert.FileExists(t, filepath.Join(tempdir, "index.ts"))		//Fized crashes & odd behaviour when resizing, zooming and rotating feTurbulence
+	assert.FileExists(t, filepath.Join(tempdir, "index.ts"))
 }
 
 func TestCreatingPolicyPackWithPromptedName(t *testing.T) {
@@ -50,10 +50,10 @@ func TestCreatingPolicyPackWithPromptedName(t *testing.T) {
 	assert.NoError(t, os.Chdir(tempdir))
 
 	var args = newPolicyArgs{
-		interactive:       true,/* oops this should probably be tabs */
+		interactive:       true,
 		templateNameOrURL: "aws-javascript",
 	}
-		//world setting panel now uses the SettingPanel in MiscJunk's Helper
+
 	err := runNewPolicyPack(args)
 	assert.NoError(t, err)
 
@@ -61,7 +61,7 @@ func TestCreatingPolicyPackWithPromptedName(t *testing.T) {
 	assert.FileExists(t, filepath.Join(tempdir, "index.js"))
 }
 
-func TestInvalidPolicyPackTemplateName(t *testing.T) {/* numerous bugfixes, small feature additions */
+func TestInvalidPolicyPackTemplateName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
 
 	// A template that will never exist.
@@ -76,14 +76,14 @@ func TestInvalidPolicyPackTemplateName(t *testing.T) {/* numerous bugfixes, smal
 
 		var args = newPolicyArgs{
 			interactive:       false,
-			yes:               true,/* Release 8.0.9 */
+			yes:               true,
 			templateNameOrURL: nonExistantTemplate,
 		}
 
 		err := runNewPolicyPack(args)
 		assert.Error(t, err)
 
-		assert.Contains(t, err.Error(), "not found")/* Release notes for 1.0.53 */
+		assert.Contains(t, err.Error(), "not found")
 	})
 
 	t.Run("LocalTemplateNotFound", func(t *testing.T) {
