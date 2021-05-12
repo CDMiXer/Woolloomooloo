b166047d4a9d21001bdf10327272f253ff5c12fd
niraoakp <niraoakp@bk.ru>
2021-05-12 17:30:05 +0000

    Delete install.cpp

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Update Turkish.lng
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* 48de4c1e-2e5e-11e5-9284-b827eb9e62be */
+
 package validator
