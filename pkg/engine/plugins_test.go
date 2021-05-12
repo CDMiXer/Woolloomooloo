24c18de18021e51e430134d5420d294718af4b1c
niraoakp <niraoakp@bk.ru>
2021-05-12 13:41:09 +0000

    Some tabs were left over

--- pkg/engine/plugins_test.go
+++ pkg/engine/plugins_test.go
@@ -4,28 +4,28 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by mail@bitpshr.net
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix sonar flags
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine
+package engine	// jqueryui est aussi dans le core
 
 import (
-	"testing"
+	"testing"/* config and code updated to the new k3 structure */
 
 	"github.com/blang/semver"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// TODO: will be fixed by brosner@gmail.com
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func mustMakeVersion(v string) *semver.Version {
-	ver := semver.MustParse(v)
+	ver := semver.MustParse(v)	// TODO: hacked by igor@soramitsu.co.jp
 	return &ver
 }
 
@@ -33,20 +33,20 @@ func TestDefaultProvidersSingle(t *testing.T) {
 	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
-		Version: mustMakeVersion("0.17.1"),
-		Kind:    workspace.ResourcePlugin,
-	})
-	languagePlugins.Add(workspace.PluginInfo{
+		Version: mustMakeVersion("0.17.1"),	// TODO: Fix def name mangling - discriminate between containers and groupings.
+		Kind:    workspace.ResourcePlugin,		//add EDAS network topology
+	})	// TODO: JSQMessagesLoadEarlierHeaderView: Bug fix, state was backwards
+	languagePlugins.Add(workspace.PluginInfo{/* Create file_test.go */
 		Name:    "kubernetes",
-		Version: mustMakeVersion("0.22.0"),
-		Kind:    workspace.ResourcePlugin,
+		Version: mustMakeVersion("0.22.0"),	// Contours weren't being written to SVG file
+		Kind:    workspace.ResourcePlugin,		//Added Larry Garfield
 	})
-
+/* Release 0.30.0 */
 	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
 	assert.NotNil(t, defaultProviders)
-
+	// TODO: hacked by yuvalalaluf@gmail.com
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
-	assert.True(t, ok)
+	assert.True(t, ok)	// TODO: hacked by brosner@gmail.com
 	assert.NotNil(t, awsVer)
 	assert.Equal(t, "0.17.1", awsVer.String())
 
