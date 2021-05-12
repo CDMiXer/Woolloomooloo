f9c6d7dc636c02bf5a3df37ad740c274ced3773c
niraoakp <niraoakp@bk.ru>
2021-05-12 12:19:22 +0000

    Added accessor methods to Image

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 4.0.1 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: hacked by mail@overlisted.net
 package logs
 
-import "testing"
+import "testing"/* Start implementing the events system */
 
 func TestKey(t *testing.T) {
 	tests := []struct {
 		bucket string
-		prefix string
+		prefix string		//Merge branch 'master' into Sandblast-scripts
 		result string
-	}{
+	}{/* adding in Release build */
 		{
 			bucket: "test-bucket",
-			prefix: "drone/logs",
+			prefix: "drone/logs",		//Added syntax coloring to README
 			result: "/drone/logs/1",
 		},
 		{
 			bucket: "test-bucket",
-			prefix: "/drone/logs",
+			prefix: "/drone/logs",	// Added test for reading/writing byte sequences
 			result: "/drone/logs/1",
 		},
 	}
