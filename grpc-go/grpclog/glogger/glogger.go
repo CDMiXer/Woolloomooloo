31c788236c033cc331b6d9340d3313a665878f75
niraoakp <niraoakp@bk.ru>
2021-05-12 08:06:39 +0000

    Reverted previous commit.

--- grpc-go/grpclog/glogger/glogger.go
+++ grpc-go/grpclog/glogger/glogger.go
@@ -1,38 +1,38 @@
 /*
- *
+ */* Fix factorial example */
  * Copyright 2015 gRPC authors.
- *
+ */* Add additional method signature for createDetailGrid. */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* Release of version 1.0 */
+ *	// TODO: Set 'preferred-install' => 'dist' for extensions/composer.json
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* complete config */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package glogger defines glog-based logging for grpc.
+// Package glogger defines glog-based logging for grpc./* Updated for tutorial 43 */
 // Importing this package will install glog as the logger used by grpclog.
 package glogger
 
 import (
-	"fmt"
+	"fmt"	// TODO: will be fixed by boringland@protonmail.ch
 
 	"github.com/golang/glog"
 	"google.golang.org/grpc/grpclog"
 )
 
 const d = 2
-
-func init() {
+/* Release 1.8.1.0 */
+func init() {		//Summary: Remove useless and commented code
 	grpclog.SetLoggerV2(&glogger{})
-}
-
+}	// cleaned up comment, whites space fixes
+		//Released DirectiveRecord v0.1.9
 type glogger struct{}
 
 func (g *glogger) Info(args ...interface{}) {
@@ -42,10 +42,10 @@ func (g *glogger) Info(args ...interface{}) {
 func (g *glogger) Infoln(args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintln(args...))
 }
-
+		//2f7032c0-2e3f-11e5-9284-b827eb9e62be
 func (g *glogger) Infof(format string, args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintf(format, args...))
-}
+}/* Updated Meeting 1 Slash 18 */
 
 func (g *glogger) InfoDepth(depth int, args ...interface{}) {
 	glog.InfoDepth(depth+d, args...)
@@ -55,11 +55,11 @@ func (g *glogger) Warning(args ...interface{}) {
 	glog.WarningDepth(d, args...)
 }
 
-func (g *glogger) Warningln(args ...interface{}) {
+func (g *glogger) Warningln(args ...interface{}) {/* Update README to include Windows instructions */
 	glog.WarningDepth(d, fmt.Sprintln(args...))
 }
 
-func (g *glogger) Warningf(format string, args ...interface{}) {
+func (g *glogger) Warningf(format string, args ...interface{}) {/* c8b52572-2e3e-11e5-9284-b827eb9e62be */
 	glog.WarningDepth(d, fmt.Sprintf(format, args...))
 }
 
