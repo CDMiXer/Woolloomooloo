251d7d7ed22e4d084f0ad0a70bead0cec90e4630
niraoakp <niraoakp@bk.ru>
2021-05-12 17:31:48 +0000

    Заготовки для расчётов

--- handler/api/user/sync.go
+++ handler/api/user/sync.go
@@ -6,11 +6,11 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Link xcopy to wine library, as it is using wine debug macros
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added a plain string example
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Finished implementation for DefaultGene */
+// limitations under the License.
 
 package user
 
@@ -18,48 +18,48 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/drone/drone/core"	// Add support for installing over TCP
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)	// implemenation with logger and processes
+)
 
 // HandleSync returns an http.HandlerFunc synchronizes and then
 // write a json-encoded list of repositories to the response body.
 func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Merge "Release 3.2.3.478 Prima WLAN Driver" */
+	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 
 		// performs asyncrhonous account synchronization.
-		// this requires long polling to determine when the/* now building Release config of premake */
+		// this requires long polling to determine when the
 		// sync is complete.
-		if r.FormValue("async") == "true" {/* comments on MSVC build environment */
+		if r.FormValue("async") == "true" {
 			ctx := context.Background()
 			go func(ctx context.Context, viewer *core.User) {
 				_, err := syncer.Sync(ctx, viewer)
 				if err != nil {
-					logger.FromContext(ctx).WithError(err).	// TODO: d18b7f52-4b19-11e5-99a1-6c40088e03e4
+					logger.FromContext(ctx).WithError(err).
 						Debugln("api: cannot synchronize account")
 				}
-			}(ctx, viewer)		//#6 [ Forgotten translations ] Check HTML tags
+			}(ctx, viewer)
 			w.WriteHeader(204)
 			return
 		}
-		//exersize about freemarker
+
 		_, err := syncer.Sync(r.Context(), viewer)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).	// TODO: hacked by cory@protocol.ai
-				Warnln("api: cannot synchronize account")/* Modified some build settings to make Release configuration actually work. */
+			logger.FromRequest(r).WithError(err).
+				Warnln("api: cannot synchronize account")
 			return
 		}
 		list, err := repos.List(r.Context(), viewer.ID)
-		if err != nil {	// 667e869a-2fbb-11e5-9f8c-64700227155b
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot synchrnoize account")
 		} else {
-			render.JSON(w, list, 200)		//Update markdown formatting (again).
+			render.JSON(w, list, 200)
 		}
 	}
 }
