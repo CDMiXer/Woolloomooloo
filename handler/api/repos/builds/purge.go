b7e802b884dec26fad2edbc6ab691f72413b5352
niraoakp <niraoakp@bk.ru>
2021-05-13 03:15:50 +0000

    added branch to build status

--- handler/api/repos/builds/purge.go
+++ handler/api/repos/builds/purge.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Start to migrate the brew library to a definition
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//3b51af08-2e56-11e5-9284-b827eb9e62be
+
 // +build !oss
 
 package builds
@@ -10,8 +10,8 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/drone/drone/core"/* Names and encapsulation */
-	"github.com/drone/drone/handler/api/render"	// TODO: Create Enigma_Main.py
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
@@ -23,23 +23,23 @@ func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.Handle
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			before    = r.FormValue("before")/* Added a trait that provides some unit testing helpers. */
-		)	// TODO: Define conda env
-		number, err := strconv.ParseInt(before, 10, 64)/* Removed reference to unused hidden sort fields. */
+			before    = r.FormValue("before")
+		)
+		number, err := strconv.ParseInt(before, 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
-			return/* Release Auth::register fix */
+			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: hacked by 13860583249@yeah.net
+			render.NotFound(w, err)
 			return
 		}
 		err = builds.Purge(r.Context(), repo.ID, number)
 		if err != nil {
 			render.InternalError(w, err)
-			return	// TODO: hacked by mowrain@yandex.com
+			return
 		}
-		w.WriteHeader(http.StatusNoContent)/* @Release [io7m-jcanephora-0.23.5] */
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
