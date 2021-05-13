f6deb4c3fd973a219b29d42b977dc50bc828ac92
niraoakp <niraoakp@bk.ru>
2021-05-13 13:45:59 +0000

    Removed an unnecessary uncertain records filter.

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,11 +1,11 @@
--- name: create-table-secrets/* Release 0.2.8.1 */
-	// TODO: hacked by martin2cai@hotmail.com
-CREATE TABLE IF NOT EXISTS secrets (	// TODO: Add Hashley app to Showcase
+-- name: create-table-secrets
+
+CREATE TABLE IF NOT EXISTS secrets (		//Create save_session_to_tmpfs.sh
  secret_id                SERIAL PRIMARY KEY
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN/* Update form.xml */
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS secrets (	// TODO: Add Hashley app to Showcase
 
 -- name: create-index-secrets-repo
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);	// TODO: Feature: Added minCount for facets as an optional setup property
 
 -- name: create-index-secrets-repo-name
-
+		//Merge "Update troubleshooting text for custom IPA images"
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
