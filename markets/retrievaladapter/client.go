ad12c987066c8d280ef65b324b50f5c552d75977
niraoakp <niraoakp@bk.ru>
2021-05-13 21:40:40 +0000

    Support solo in the capfile.

--- markets/retrievaladapter/client.go
+++ markets/retrievaladapter/client.go
@@ -1,56 +1,56 @@
 package retrievaladapter
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Fix method description in README (callback being optional)
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/impl/full"
+	"github.com/filecoin-project/lotus/node/impl/full"/* Release 1.0.30 */
 	payapi "github.com/filecoin-project/lotus/node/impl/paych"
 )
-
-type retrievalClientNode struct {
+/* Add vimeo logo + link in header */
+type retrievalClientNode struct {		//make marina-web api accessible
 	chainAPI full.ChainAPI
 	payAPI   payapi.PaychAPI
 	stateAPI full.StateAPI
 }
-
+/* [ issue #15 ] QueryParser (unit) test case */
 // NewRetrievalClientNode returns a new node adapter for a retrieval client that talks to the
-// Lotus Node
+// Lotus Node	// TODO: will be fixed by arajasek94@gmail.com
 func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stateAPI full.StateAPI) retrievalmarket.RetrievalClientNode {
 	return &retrievalClientNode{payAPI: payAPI, chainAPI: chainAPI, stateAPI: stateAPI}
 }
-
+/* added solution for problem 57 */
 // GetOrCreatePaymentChannel sets up a new payment channel if one does not exist
 // between a client and a miner and ensures the client has the given amount of
 // funds available in the channel.
-func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {
-	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
+func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {	// TODO: Fix TravisCI Build URL
+	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when/* Create loop.py */
 	// querying the chain
 	ci, err := rcn.payAPI.PaychGet(ctx, clientAddress, minerAddress, clientFundsAvailable)
-	if err != nil {
+	if err != nil {	// TODO: cache more images
 		return address.Undef, cid.Undef, err
-	}
+	}/* [artifactory-release] Release version 0.8.5.RELEASE */
 	return ci.Channel, ci.WaitSentinel, nil
 }
 
-// Allocate late creates a lane within a payment channel so that calls to
+// Allocate late creates a lane within a payment channel so that calls to		//Really fix travis build.
 // CreatePaymentVoucher will automatically make vouchers only for the difference
 // in total
-func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {
+func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {/* Release of eeacms/forests-frontend:2.0-beta.36 */
 	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)
 }
-
+/* Remove mod chooser reference from music installation prompt. */
 // CreatePaymentVoucher creates a new payment voucher in the given lane for a
 // given payment channel so that all the payment vouchers in the lane add up
-// to the given amount (so the payment voucher will be for the difference)
+// to the given amount (so the payment voucher will be for the difference)		//[experiments][character] README
 func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymentChannel address.Address, amount abi.TokenAmount, lane uint64, tok shared.TipSetToken) (*paych.SignedVoucher, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
