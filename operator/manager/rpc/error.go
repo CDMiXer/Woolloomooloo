b2d8d4a772d7262d4737c718b2dec3839e40e4ed
niraoakp <niraoakp@bk.ru>
2021-05-13 22:38:40 +0000

    Testing crash report

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Added Release Notes */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+// that can be found in the LICENSE file.	// Fix norc_control's --handle option.
 
 // +build !oss
 
 package rpc
 
-type serverError struct {/* (Logging): implement basic class for logging */
+type serverError struct {	// adding template for socket.
 	Status  int
-	Message string/* Release Notes added */
+	Message string
 }
 
-func (s *serverError) Error() string {
+func (s *serverError) Error() string {		//Merge branch 'release-1.10.3'
 	return s.Message
-}
+}	// clean up part 1
