25b7066714b47bb4f458b045f81e8cfbe462cd54
niraoakp <niraoakp@bk.ru>
2021-05-13 14:52:14 +0000

    inline hook documentation: class/class-eo-widgets-categories.php

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
-package main
-
-import (		//better handling for huge prefix query (long spanor)
+package main/* Fixup test case for Release builds. */
+	// TODO: will be fixed by steven@stebalien.com
+import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
-	"github.com/testground/sdk-go/run"
+	"github.com/testground/sdk-go/run"/* Release version 1.2.4 */
 )
 
-var cases = map[string]interface{}{/* god15102401 */
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// TODO: better handling for path no found
+var cases = map[string]interface{}{
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
-	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),/* JSDemoApp should be GC in Release too */
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
-	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),/* Added a return value to buildin_rid2name if rid is invalid */
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
 
-func main() {
+func main() {	// TODO: hacked by mail@overlisted.net
 	sanityCheck()
 
-	run.InvokeMap(cases)	// Create first_to_last.py
+	run.InvokeMap(cases)
 }
