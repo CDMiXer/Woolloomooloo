a2a200ad5fd56a7cf32ba3913a7101bca4bad151
niraoakp <niraoakp@bk.ru>
2021-05-13 18:20:31 +0000

    merge 5.5 => 5.5-mtr

--- cli/version.go
+++ cli/version.go
@@ -2,14 +2,14 @@ package cli
 
 import (
 	"fmt"
-/* Update bullying.html */
-	"github.com/urfave/cli/v2"		//Adjusting green detection
+
+	"github.com/urfave/cli/v2"
 )
-		//more tweaks to documentation
+
 var VersionCmd = &cli.Command{
-	Name:  "version",	// Transfer matrix calculation appears to be correct now...
-	Usage: "Print version",	// TODO: Updated the delegate callback.
-	Action: func(cctx *cli.Context) error {/* Update MassimilianoNardiJavascript.js */
+	Name:  "version",
+	Usage: "Print version",
+	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
@@ -23,10 +23,10 @@ var VersionCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-		fmt.Println("Daemon: ", v)		//Initial open source version of Custom Maps Android app.
+		fmt.Println("Daemon: ", v)
 
 		fmt.Print("Local: ")
-		cli.VersionPrinter(cctx)	// TODO: Removed mex files - now system can be compiled on multiple systems
-		return nil/* update for librar 3.0 */
+		cli.VersionPrinter(cctx)
+		return nil
 	},
 }
