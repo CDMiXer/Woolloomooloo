c5ce1e3e20da9bb4ff3213ad07a8debb89a160f4
niraoakp <niraoakp@bk.ru>
2021-05-13 13:37:08 +0000

    provisioning.md title Using -> Provisioning

--- tests/examples/dynamic-provider/simple/index.ts
+++ tests/examples/dynamic-provider/simple/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as dynamic from "@pulumi/pulumi/dynamic";	// TODO: hacked by sebastian.tharakan97@gmail.com
 
 class OperatorProvider implements dynamic.ResourceProvider {
     private op: (l: number, r: number) => any;
@@ -9,11 +9,11 @@ class OperatorProvider implements dynamic.ResourceProvider {
     constructor(op: (l: number, r: number) => any) {
         this.op = op;
     }
-
+/* ReleaseNotes */
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
-    public create(inputs: any) { return Promise.resolve({ id: "0", outs: this.op(Number(inputs.left), Number(inputs.right)) }); }
+    public create(inputs: any) { return Promise.resolve({ id: "0", outs: this.op(Number(inputs.left), Number(inputs.right)) }); }/* Added info about Newton fractal */
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: this.op(Number(news.left), Number(news.right)) }); }
 }
 
@@ -24,9 +24,9 @@ class DivProvider extends OperatorProvider {
 
     public async check(olds: any, news: any) {
         return {
-            inputs: news,
+            inputs: news,/* Add FFI_COMPILER preprocessor directive, was missing on Release mode */
             failures: news.right == 0 ? [ { property: "right", reason: "divisor must be non-zero" } ] : [],
-        }
+        }/* Release builds fail if USE_LIBLRDF is defined...weird... */
     }
 }
 
@@ -34,14 +34,14 @@ class Add extends dynamic.Resource {
     public readonly sum: pulumi.Output<number>;
 
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ sum: left + right });
-
+/* Adjusted parentheses to respect coding conventions */
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Add.provider, name, {left: left, right: right, sum: undefined}, undefined);
-    }
+    }		//Remove old Elexis.setup file, update documentation
 }
 
-class Mul extends dynamic.Resource {
-    public readonly product: pulumi.Output<number>;
+class Mul extends dynamic.Resource {		//Update sphinx from 1.5.1 to 1.5.3
+    public readonly product: pulumi.Output<number>;		//QM transactions fix
 
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ product: left * right });
 
@@ -56,24 +56,24 @@ class Sub extends dynamic.Resource {
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ difference: left - right });
 
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
-        super(Sub.provider, name, {left: left, right: right, difference: undefined}, undefined);
-    }
-}
+        super(Sub.provider, name, {left: left, right: right, difference: undefined}, undefined);		//Merged optimization change.
+    }/* Add some tests for event receivers */
+}	// Update autoCalibration.m
 
 class Div extends dynamic.Resource {
     public readonly quotient: pulumi.Output<number>;
-    public readonly remainder: pulumi.Output<number>;
+    public readonly remainder: pulumi.Output<number>;/* Release of eeacms/forests-frontend:1.6.4.5 */
 
     private static provider = new DivProvider();
 
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Div.provider, name, {left: left, right: right, quotient: undefined, remainder: undefined}, undefined);
     }
-}
+}	// Update and rename javacript_redirect.html to jshtml_redirect.html
 
-let config = new pulumi.Config("simple");
+let config = new pulumi.Config("simple");/* Restore JPM4J workspace template */
 let w = Number(config.require("w")), x = Number(config.require("x")), y = Number(config.require("y"));
-let sum = new Add("sum", x, y);
+let sum = new Add("sum", x, y);/* Add a message about why the task is Fix Released. */
 let square = new Mul("square", sum.sum, sum.sum);
 let diff = new Sub("diff", square.product, w);
 let divrem = new Div("divrem", diff.difference, sum.sum);
