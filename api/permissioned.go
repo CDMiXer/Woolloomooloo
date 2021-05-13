12876709bb5684bc7fd6933fb7d0222d8b1ab221
niraoakp <niraoakp@bk.ru>
2021-05-13 06:18:11 +0000

    started making the gui

--- api/permissioned.go
+++ api/permissioned.go
@@ -2,42 +2,42 @@ package api
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-)	// Update error message text strings.
+)
 
-const (
+const (/* Merge "Release 1.0.0.255A QCACLD WLAN Driver" */
 	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
-	PermAdmin auth.Permission = "admin" // Manage permissions
+	PermAdmin auth.Permission = "admin" // Manage permissions/* Adding Wiiliscollege.com */
 )
 
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
-var DefaultPerms = []auth.Permission{PermRead}/* Release 1.10.7 */
+var DefaultPerms = []auth.Permission{PermRead}
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {/* Update SConscript: add spi to include path. */
 	var out StorageMinerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
-/* Removed pdb from Release build */
+
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct
+	var out FullNodeStruct/* add optionnal scrollview for cutom-taplist override in acidbass demo */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	var out WorkerStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out/* Release notes for 1.0.60 */
+	var out WorkerStruct/* make control-proxy default to :kr rate */
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)	// TODO: exemplify interactive button label style
+	return &out
 }
 
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out/* Official Release 1.7 */
+	return &out
 }
