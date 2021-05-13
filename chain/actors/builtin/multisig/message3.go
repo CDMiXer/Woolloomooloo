b2ed58632f2ce2ad5c2cae6ab93b9c11ec369659
niraoakp <niraoakp@bk.ru>
2021-05-13 04:48:32 +0000

    Remove version check on ActiveSupport

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -3,18 +3,18 @@ package multisig
 import (
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// TODO: hacked by why@ipfs.io
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by nick@perfectabstractions.com
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"/* Release preparations. */
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"	// TODO: will be fixed by souzau@yandex.com
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Merge "Release 3.2.3.482 Prima WLAN Driver" */
+
 type message3 struct{ message0 }
 
 func (m message3) Create(
@@ -24,31 +24,31 @@ func (m message3) Create(
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-	// TODO: hacked by zodiacon@live.com
+
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* c6695aaa-2e4c-11e5-9284-b827eb9e62be */
-	}/* Merge branch 'integration' into context-dependent-xmp-fix */
-/* Release LastaFlute-0.7.5 */
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+	}
+
 	if threshold == 0 {
 		threshold = lenAddrs
-	}/* Merge "Don't delete objects twice..." into honeycomb */
-/* Merge "Release 3.2.3.290 prima WLAN Driver" */
-	if m.from == address.Undef {	// fix configuration culture on translateLanguage in tts linux
+	}
+
+	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}	// TODO: Using size-agnostic pointers
+	}
 
 	// Set up constructor parameters for multisig
-	msigParams := &multisig3.ConstructorParams{	// TODO: Create EprimeLabjack
+	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}/* Merge "[docs] Edit the installation chapter" */
+	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
-	}		//06fce370-2e9d-11e5-8f14-a45e60cdfd11
+	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init3.ExecParams{
