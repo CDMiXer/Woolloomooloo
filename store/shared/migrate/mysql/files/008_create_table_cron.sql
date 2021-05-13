67ca9d27ceef6cbdaf2bda57099c9c67ed456d03
niraoakp <niraoakp@bk.ru>
2021-05-13 02:50:34 +0000

    Create NSObject+PRAttributes.m

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-
-CREATE TABLE IF NOT EXISTS cron (/* [PRE-25] dev sync */
- cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,cron_repo_id     INTEGER/* * wfrog builder for win-Release (1.0.1) */
-,cron_name        VARCHAR(50)
+/* we need to setup the vm sandbox first, then connect via ssh */
+CREATE TABLE IF NOT EXISTS cron (
+ cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT	// Changed BorderLayout for MainMenu.java
+,cron_repo_id     INTEGER
+,cron_name        VARCHAR(50)/* (John Arbash Meinel) Release 0.12rc1 */
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
-,cron_event       VARCHAR(50)		//Add some more details to the configuration doc
+,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
-,cron_disabled    BOOLEAN
-,cron_created     INTEGER/* Ensure db is migrated before running features */
-,cron_updated     INTEGER
+,cron_disabled    BOOLEAN/* 9ee745f4-2e52-11e5-9284-b827eb9e62be */
+,cron_created     INTEGER
+,cron_updated     INTEGER/* Refactoring for Release, part 1 of ... */
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
-	// TODO: Don’t queue rallypoint move if MoveIntoWorld:false. Fixes #5576.
-CREATE INDEX ix_cron_repo ON cron (cron_repo_id);/* Merge "Bug 63800: Call handleArgs before GeneratorFactory" */
+/* Release note update. */
+CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next
-
-CREATE INDEX ix_cron_next ON cron (cron_next);	// TODO: hacked by igor@soramitsu.co.jp
+-- name: create-index-cron-next		//Merge "Fix potential race condition in lbaas v2 logic"
+/* Deleted CtrlApp_2.0.5/Release/Files.obj */
+CREATE INDEX ix_cron_next ON cron (cron_next);
