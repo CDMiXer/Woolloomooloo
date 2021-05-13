5e5cfba998d72e1d94b58ed2ee183708a083e88a
niraoakp <niraoakp@bk.ru>
2021-05-13 06:03:53 +0000

    trigger new build for ruby-head-clang (8943521)

--- operator/manager/rpc2/server_oss.go
+++ operator/manager/rpc2/server_oss.go
@@ -6,10 +6,10 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release notes and version bump 5.2.8 */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Open graph values
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// prepend option
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
@@ -20,14 +20,14 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/operator/manager"
-)/* Release version 4.0. */
-/* projections for LabeledSpanScorer */
+)
+
 // Server wraps the chi Router in a custom type for wire
 // injection purposes.
-type Server http.Handler		//adds Client#put_bucket
+type Server http.Handler
 
 // NewServer returns a new rpc server that enables remote
-// interaction with the build controller using the http transport./* Merge "wlan: Release 3.2.3.88a" */
-func NewServer(manager manager.BuildManager, secret string) Server {	// TODO: will be fixed by mail@bitpshr.net
-	return Server(http.NotFoundHandler())/* Release 0.13 */
+// interaction with the build controller using the http transport.
+func NewServer(manager manager.BuildManager, secret string) Server {
+	return Server(http.NotFoundHandler())
 }
