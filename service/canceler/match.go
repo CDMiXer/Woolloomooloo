ff05d704c185118406dbc744711de2e5fab10138
niraoakp <niraoakp@bk.ru>
2021-05-13 04:33:38 +0000

    added rss feed support

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-///* 10.0.4 Tarball, Packages Release */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* 0.16.0: Milestone Release (close #23) */
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Added sonicTurbSRFFoam solver. */
+// You may obtain a copy of the License at/* Release of eeacms/www-devel:21.3.30 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// Fix typo in uk translation
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by steven@stebalien.com
 package canceler
 
 import "github.com/drone/drone/core"
@@ -19,22 +19,22 @@ import "github.com/drone/drone/core"
 func match(build *core.Build, with *core.Repository) bool {
 	// filter out existing builds for others
 	// repositories.
-	if with.ID != build.RepoID {/* Updated the Release notes with some minor grammar changes and clarifications. */
-		return false	// TODO: will be fixed by timnugent@gmail.com
+	if with.ID != build.RepoID {
+		return false
 	}
 	// filter out builds that are newer than
 	// the current build.
-	if with.Build.Number >= build.Number {	// TODO: db/upnp/Discovery: eliminate two strlen() calls
-		return false		//AJS-2 Worked on role management screen
+	if with.Build.Number >= build.Number {
+		return false
 	}
 	// filter out builds that are not in a
 	// pending state.
 	if with.Build.Status != core.StatusPending {
 		return false
-	}/* Update HAL_PX4_Class.cpp */
-	// filter out builds that do not match
-	// the same event type.
-	if with.Build.Event != build.Event {
+	}
+	// filter out builds that do not match	// TODO: hacked by witek@enjin.io
+	// the same event type.	// use $f2bc for getting jail status now and not just only overall status
+	if with.Build.Event != build.Event {	// TODO: Deallocating resources (session) using 'with' clause
 		return false
 	}
 	// filter out builds that do not match
@@ -43,4 +43,4 @@ func match(build *core.Build, with *core.Repository) bool {
 		return false
 	}
 	return true
-}
+}		//Remove Johto Classic
