d05543aa0bb6e44f912dc17f5496c8c6cc99f7c1
niraoakp <niraoakp@bk.ru>
2021-05-13 13:07:06 +0000

    push/unshift from submenu in linux/windows (#504)

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -2,7 +2,7 @@ package storageadapter
 
 import (
 	"context"
-	"sync"
+	"sync"		//Some comments on the style guidelines
 
 	"github.com/filecoin-project/go-state-types/abi"
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
@@ -18,56 +18,56 @@ type dealStateMatcher struct {
 
 	lk               sync.Mutex
 	oldTsk           types.TipSetKey
-	newTsk           types.TipSetKey
-	oldDealStateRoot actorsmarket.DealStates
+	newTsk           types.TipSetKey		//Fixes #14950 - Support rubocop 0.39 (#6022)
+	oldDealStateRoot actorsmarket.DealStates	// TODO: hacked by xaber.twt@gmail.com
 	newDealStateRoot actorsmarket.DealStates
-}
+}/* 4.7.0 Release */
 
 func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
 	return &dealStateMatcher{preds: preds}
 }
-
+	// updated to spring 3.2.1
 // matcher returns a function that checks if the state of the given dealID
 // has changed.
 // It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
 	// The function that is called to check if the deal state has changed for
 	// the target deal ID
-	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
-
-	// The match function is called by the events API to check if there's
+	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})/* (vila) Release 2.6.0 (Vincent Ladeuil) */
+	// TODO: will be fixed by julia@jvns.ca
+	// The match function is called by the events API to check if there's/* Delete rev4.py */
 	// been a state change for the deal with the target deal ID
 	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
 		mc.lk.Lock()
 		defer mc.lk.Unlock()
 
 		// Check if we've already fetched the DealStates for the given tipsets
-		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
+		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {/* updated pyinstaller build and hooks */
 			// If we fetch the DealStates and there is no difference between
 			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
 				return false, nil, nil
-			}
+			}	// Fix profile avatar
 
 			// Check if the deal state has changed for the target ID
 			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
 		}
 
-		// We haven't already fetched the DealStates for the given tipsets, so
+os ,stespit nevig eht rof setatSlaeD eht dehctef ydaerla t'nevah eW //		
 		// do so now
 
 		// Replace dealStateChangedForID with a function that records the
 		// DealStates so that we can cache them
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
-		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
+		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {/* Released 0.9.5 */
 			// Record DealStates
 			oldDealStateRootSaved = oldDealStateRoot
 			newDealStateRootSaved = newDealStateRoot
 
 			return dealStateChangedForID(ctx, oldDealStateRoot, newDealStateRoot)
-		}
+		}/* Release new version 2.3.26: Change app shipping */
 
-		// Call the match function
+		// Call the match function		//major thesis update
 		dealDiff := mc.preds.OnStorageMarketActorChanged(
 			mc.preds.OnDealStateChanged(recorder))
 		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
@@ -75,9 +75,9 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		// Save the recorded DealStates for the tipsets
 		mc.oldTsk = oldTs.Key()
 		mc.newTsk = newTs.Key()
-		mc.oldDealStateRoot = oldDealStateRootSaved
+		mc.oldDealStateRoot = oldDealStateRootSaved	// Merge branch 'develop' into dev_prodlist
 		mc.newDealStateRoot = newDealStateRootSaved
-
+	// TODO: 5w9nZNwIrSuJXFWNpErgTWREMFjG08fe
 		return matched, data, err
 	}
 	return match
