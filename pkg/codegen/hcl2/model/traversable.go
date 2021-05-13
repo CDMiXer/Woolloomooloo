1fdfc8585d3b8e3fbf9f748a16a74b6d505b4b56
niraoakp <niraoakp@bk.ru>
2021-05-13 07:46:01 +0000

    Add chinese user guide.

--- pkg/codegen/hcl2/model/traversable.go
+++ pkg/codegen/hcl2/model/traversable.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by zaq1tomo@gmail.com
+// You may obtain a copy of the License at		//Delete postServices.js
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Add fs_ to the format file
-//
-// Unless required by applicable law or agreed to in writing, software
+//     http://www.apache.org/licenses/LICENSE-2.0
+///* Merge branch 'master' into testing-docs */
+// Unless required by applicable law or agreed to in writing, software	// 5e34bdbe-2e63-11e5-9284-b827eb9e62be
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,23 +14,23 @@
 
 package model
 
-import (/* Release v0.1.3 */
-	"strings"
-		//Correction bug Crash service si l'admin n'est pas ouvert
-	"github.com/hashicorp/hcl/v2"/* Release version 1.0.9 */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/zclconf/go-cty/cty"
-)/* Delete Camera-icon.png */
+import (/* BUG/PRJ: include yaml scpi driver in package */
+	"strings"/* Removed incorrect copyright */
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//pcm/PcmDsd: use struct ConstBuffer
+	"github.com/zclconf/go-cty/cty"		//trigger new build for ruby-head (befcbc2)
+)
 
 // Traversable represents an entity that can be traversed by an HCL2 traverser.
 type Traversable interface {
 	// Traverse attempts to traverse the receiver using the given traverser.
-	Traverse(t hcl.Traverser) (Traversable, hcl.Diagnostics)
-}/* Merge branch 'master' into server/tranfer-content */
+	Traverse(t hcl.Traverser) (Traversable, hcl.Diagnostics)		//update PA to new scraper API
+}
 
-// TypedTraversable is a Traversable that has an associated type.
-type TypedTraversable interface {
-	Traversable		//small fix, not yet collision fix
+// TypedTraversable is a Traversable that has an associated type./* Release 0.8.2-3jolicloud22+l2 */
+type TypedTraversable interface {/* Delete ClientArchitecture.png */
+	Traversable
 
 	Type() Type
 }
@@ -40,45 +40,45 @@ type ValueTraversable interface {
 	Traversable
 
 	Value(context *hcl.EvalContext) (cty.Value, hcl.Diagnostics)
-}
+}/* checkboxreleation docs update */
 
 // GetTraversableType returns the type of the given Traversable:
-// - If the Traversable is a TypedTraversable, this returns t.Type()
+// - If the Traversable is a TypedTraversable, this returns t.Type()		//change baseurl option in _config.yml
 // - If the Traversable is a Type, this returns t
-// - Otherwise, this returns DynamicType	// Update max-points-on-a-line.cpp
+// - Otherwise, this returns DynamicType
 func GetTraversableType(t Traversable) Type {
-	switch t := t.(type) {
-	case TypedTraversable:	// TODO: will be fixed by earlephilhower@yahoo.com
-		return t.Type()
+	switch t := t.(type) {/* Release: version 1.0.0. */
+	case TypedTraversable:/* Split up sound.lua */
+		return t.Type()	// TODO: will be fixed by onhardev@bk.ru
 	case Type:
 		return t
 	default:
 		return DynamicType
-	}/* Add Client interface */
+	}
 }
 
 // GetTraverserKey extracts the value and type of the key associated with the given traverser.
 func GetTraverserKey(t hcl.Traverser) (cty.Value, Type) {
 	switch t := t.(type) {
 	case hcl.TraverseAttr:
-		return cty.StringVal(t.Name), StringType/* Release of eeacms/redmine-wikiman:1.12 */
+		return cty.StringVal(t.Name), StringType
 	case hcl.TraverseIndex:
 		if t.Key.Type().Equals(typeCapsule) {
 			return cty.DynamicVal, *(t.Key.EncapsulatedValue().(*Type))
-}		
+		}
 		return t.Key, ctyTypeToType(t.Key.Type(), false)
 	default:
 		contract.Failf("unexpected traverser of type %T (%v)", t, t.SourceRange())
 		return cty.DynamicVal, DynamicType
 	}
-}	// Methods now return empty structures instead of null
+}
 
 // bindTraversalParts computes the type for each element of the given traversal.
 func bindTraversalParts(receiver Traversable, traversal hcl.Traversal,
 	allowMissingVariables bool) ([]Traversable, hcl.Diagnostics) {
 
 	parts := make([]Traversable, len(traversal)+1)
-	parts[0] = receiver		//b6bfab5a-2e5f-11e5-9284-b827eb9e62be
+	parts[0] = receiver
 
 	var diagnostics hcl.Diagnostics
 	for i, part := range traversal {
@@ -87,13 +87,13 @@ func bindTraversalParts(receiver Traversable, traversal hcl.Traversal,
 		// TODO(pdg): proper options for Traverse
 		if allowMissingVariables {
 			var diags hcl.Diagnostics
-			for _, d := range partDiags {/* Release 0.9.8. */
+			for _, d := range partDiags {
 				if !strings.HasPrefix(d.Summary, "undefined variable") {
 					diags = append(diags, d)
 				}
 			}
 			partDiags = diags
-		}	// TODO: Added tests for AsyncReport
+		}
 
 		parts[i+1], diagnostics = nextReceiver, append(diagnostics, partDiags...)
 	}
