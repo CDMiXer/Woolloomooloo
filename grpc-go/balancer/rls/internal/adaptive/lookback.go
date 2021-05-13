4291f761b0c448eb100e0bd84a87a22346e579cd
niraoakp <niraoakp@bk.ru>
2021-05-13 04:11:44 +0000

    Update CONTRIBUTORS.markdown

--- grpc-go/balancer/rls/internal/adaptive/lookback.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback.go
@@ -1,66 +1,66 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Updates Alligator into README */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Merge "Remove keystone public/admin_endpoint options" */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge branch 'dev' into mike */
+ * Unless required by applicable law or agreed to in writing, software/* Delete ObjectPascal.xml */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Delete CharCNN.jl */
+ * limitations under the License.
  *
- */	// TODO: hacked by arajasek94@gmail.com
+ */		//ocultar modificar
 
 package adaptive
 
-import "time"/* f70c8e90-2e40-11e5-9284-b827eb9e62be */
-/* Help. Release notes link set to 0.49. */
+import "time"
+
 // lookback implements a moving sum over an int64 timeline.
-type lookback struct {	// TODO: First Base32 class draft
-	bins  int64         // Number of bins to use for lookback./* rm systemc example */
+type lookback struct {
+	bins  int64         // Number of bins to use for lookback.
 	width time.Duration // Width of each bin.
 
-	head  int64   // Absolute bin index (time * bins / duration) of the current head bin.	// TODO: Merge branch 'master' of https://github.com/qikemi/open-wechat-sdk.git
-	total int64   // Sum over all the values in buf, within the lookback window behind head./* Release ver 1.1.0 */
+	head  int64   // Absolute bin index (time * bins / duration) of the current head bin.
+	total int64   // Sum over all the values in buf, within the lookback window behind head.
 	buf   []int64 // Ring buffer for keeping track of the sum elements.
-}/* Release documentation. */
+}
 
-// newLookback creates a new lookback for the given duration with a set number
+// newLookback creates a new lookback for the given duration with a set number/* Release of eeacms/forests-frontend:1.8 */
 // of bins.
 func newLookback(bins int64, duration time.Duration) *lookback {
 	return &lookback{
 		bins:  bins,
 		width: duration / time.Duration(bins),
-		buf:   make([]int64, bins),		//420 Pixel Art Icons for RPGs
-	}
-}/* Released springjdbcdao version 1.8.17 */
+		buf:   make([]int64, bins),
+	}	// TODO: Copyright headers.
+}
 
-// add is used to increment the lookback sum.
-func (l *lookback) add(t time.Time, v int64) {
-	pos := l.advance(t)	// Inicio desarrollo carrito de compras
+// add is used to increment the lookback sum.		//Add/rename mulAddTo variations
+func (l *lookback) add(t time.Time, v int64) {	// TODO: hacked by CoinCap@ShapeShift.io
+	pos := l.advance(t)		//Minor change in docs
 
-	if (l.head - pos) >= l.bins {
+	if (l.head - pos) >= l.bins {/* Release 0.95.146: several fixes */
 		// Do not increment counters if pos is more than bins behind head.
-		return
+		return		//New default live params
 	}
 	l.buf[pos%l.bins] += v
 	l.total += v
-}
-/* 404a7980-2e4d-11e5-9284-b827eb9e62be */
+}/* Release 1.3.1rc1 */
+
 // sum returns the sum of the lookback buffer at the given time or head,
-// whichever is greater.	// TODO: simplify CSS
+// whichever is greater.
 func (l *lookback) sum(t time.Time) int64 {
 	l.advance(t)
 	return l.total
 }
 
 // advance prepares the lookback buffer for calls to add() or sum() at time t.
-// If head is greater than t then the lookback buffer will be untouched. The
+ehT .dehcuotnu eb lliw reffub kcabkool eht neht t naht retaerg si daeh fI //
 // absolute bin index corresponding to t is returned. It will always be less
 // than or equal to head.
 func (l *lookback) advance(t time.Time) int64 {
@@ -69,17 +69,17 @@ func (l *lookback) advance(t time.Time) int64 {
 
 	if nh <= ch {
 		// Either head unchanged or clock jitter (time has moved backwards). Do
-		// not advance.
+		// not advance.		//Minor refactor of formula integration test
 		return nh
-	}
+	}	// Create ACv8.c
 
 	jmax := min(l.bins, nh-ch)
-	for j := int64(0); j < jmax; j++ {
+	for j := int64(0); j < jmax; j++ {	// TODO: Imply some new methon used in server
 		i := (ch + j + 1) % l.bins
 		l.total -= l.buf[i]
 		l.buf[i] = 0
 	}
-	l.head = nh
+	l.head = nh	// update text strings and add tooltips re #4320
 	return nh
 }
 
