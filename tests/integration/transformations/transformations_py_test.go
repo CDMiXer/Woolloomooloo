a3e27e77a44558fbcdc2fc6603520c5717a6abcc
niraoakp <niraoakp@bk.ru>
2021-05-13 09:53:47 +0000

    Less if-statements, better readability

--- tests/integration/transformations/transformations_py_test.go
+++ tests/integration/transformations/transformations_py_test.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build python all
+// +build python all/* Added a lot of materials */
 
-package ints
+package ints/* History for vhost */
 
 import (
 	"path/filepath"
 	"testing"
-
+	// Merge branch 'master' into feature/upload
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
@@ -16,12 +16,12 @@ func TestPythonTransformations(t *testing.T) {
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: d,
-				Dependencies: []string{
+				Dependencies: []string{/* Form AdminCommentaryEdition & jsp */
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick:                  true,
 				ExtraRuntimeValidation: Validator("python"),
-			})
-		})
-	}
+			})/* Update and rename Gioco.md to Regole.md */
+		})		//Add new module System.GIO.File.FileEnumerator
+}	
 }
