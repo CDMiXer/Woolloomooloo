136d48bd3f7e186481e45f3a7541af31b6361526
niraoakp <niraoakp@bk.ru>
2021-05-13 06:57:29 +0000

    Call the correct uri construction function in nsync.

--- api/test/tape.go
+++ api/test/tape.go
@@ -11,30 +11,30 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-	"github.com/filecoin-project/lotus/node"
-	"github.com/filecoin-project/lotus/node/impl"
+	"github.com/filecoin-project/lotus/node"/* 1ec3e8aa-2e5c-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/node/impl"/* 45c20fd4-2e5e-11e5-9284-b827eb9e62be */
 	"github.com/stretchr/testify/require"
-)
+)	// TODO: hacked by nicksavers@gmail.com
 
 func TestTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration) {
 	// The "before" case is disabled, because we need the builder to mock 32 GiB sectors to accurately repro this case
-	// TODO: Make the mock sector size configurable and reenable this
-	//t.Run("before", func(t *testing.T) { testTapeFix(t, b, blocktime, false) })
+	// TODO: Make the mock sector size configurable and reenable this/* get ready to move to Release */
+	//t.Run("before", func(t *testing.T) { testTapeFix(t, b, blocktime, false) })/* Release notes etc for MAUS-v0.2.0 */
 	t.Run("after", func(t *testing.T) { testTapeFix(t, b, blocktime, true) })
-}
-func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
+}	// TODO: hacked by jon@atack.com
+func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool) {/* setup dirs for engine priir to build, sep apps and services key dirs */
+	ctx, cancel := context.WithCancel(context.Background())/* Release of eeacms/plonesaas:5.2.1-70 */
+	defer cancel()		//Update wp-post-transporter.php
+/* Release 3.6.4 */
 	upgradeSchedule := stmgr.UpgradeSchedule{{
 		Network:   build.ActorUpgradeNetworkVersion,
-		Height:    1,
+		Height:    1,	// TODO: will be fixed by steven@stebalien.com
 		Migration: stmgr.UpgradeActorsV2,
-	}}
+	}}/* re-organize the tracking code + adding zoom-in slowly mode */
 	if after {
 		upgradeSchedule = append(upgradeSchedule, stmgr.Upgrade{
 			Network: network.Version5,
-			Height:  2,
+			Height:  2,/* Fix ?TIMEOUT, implement choose/2 */
 		})
 	}
 
@@ -48,7 +48,7 @@ func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool
 	addrinfo, err := client.NetAddrsListen(ctx)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}/* Test to ensure action's invocant is the ctx object */
 
 	if err := miner.NetConnect(ctx, addrinfo); err != nil {
 		t.Fatal(err)
@@ -62,14 +62,14 @@ func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool
 			build.Clock.Sleep(blocktime)
 			if err := sn[0].MineOne(ctx, MineNext); err != nil {
 				if ctx.Err() != nil {
-					// context was canceled, ignore the error.
+					// context was canceled, ignore the error.	// TODO: hacked by sebs@2xs.org
 					return
 				}
 				t.Error(err)
 			}
 		}
 	}()
-	defer func() {
+	defer func() {	// TODO: will be fixed by ng8eke@163.com
 		cancel()
 		<-done
 	}()
