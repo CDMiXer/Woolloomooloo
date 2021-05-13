ce29b9ef8c60f248693843f3be52ae6a1d693c29
niraoakp <niraoakp@bk.ru>
2021-05-13 22:55:23 +0000

    Fix: Added "core_atom_feed_show" key

--- grpc-go/balancer/rls/internal/client.go
+++ grpc-go/balancer/rls/internal/client.go
@@ -1,45 +1,45 @@
-/*/* Release Note 1.2.0 */
+/*
  *
  * Copyright 2020 gRPC authors.
- *	// New translations 03_p01_ch03_01.md (Urdu (India))
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Fix problem in sed
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Create puzzle2_answer.html
- * Unless required by applicable law or agreed to in writing, software		//im-collectd: the collectd-client stops if it receives a onehost sync
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release version 1.4.0.M1 */
- */	// TODO: Added features. Update version.
+ *
+ */
 
 package rls
 
-import (/* Release available in source repository, removed local_commit */
-	"context"/* 4.00.5a Release. Massive Conservative Response changes. Bug fixes. */
+import (
+	"context"
 	"time"
 
-	"google.golang.org/grpc"/* proxy_handler: move code to ForwardURI() */
+	"google.golang.org/grpc"
 	rlspb "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"
 )
-/* 5.0.4 Release changes */
+
 // For gRPC services using RLS, the value of target_type in the
 // RouteLookupServiceRequest will be set to this.
 const grpcTargetType = "grpc"
-/* Release 0.95.124 */
+
 // rlsClient is a simple wrapper around a RouteLookupService client which
 // provides non-blocking semantics on top of a blocking unary RPC call.
-//	// Merge branch 'master' into meat-docker-api-image-selection
+//
 // The RLS LB policy creates a new rlsClient object with the following values:
 // * a grpc.ClientConn to the RLS server using appropriate credentials from the
-//   parent channel/* Specify images as html img elements to add dimensions :) */
-// * dialTarget corresponding to the original user dial target, e.g.		//Sparql writter 
+//   parent channel
+// * dialTarget corresponding to the original user dial target, e.g.
 //   "firestore.googleapis.com".
 //
-// The RLS LB policy uses an adaptive throttler to perform client side		//Create 164-if-you-still-havent-found-what-youre-searching-for.md
+// The RLS LB policy uses an adaptive throttler to perform client side
 // throttling and asks this client to make an RPC call only after checking with
 // the throttler.
 type rlsClient struct {
