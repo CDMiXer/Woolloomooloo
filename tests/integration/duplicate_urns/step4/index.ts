f73aca97ca18e554de95f223f6f55f5f41414cca
niraoakp <niraoakp@bk.ru>
2021-05-13 16:32:45 +0000

    Added directions for setting up a database.

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* remove slave */
+// Licensed under the Apache License, Version 2.0 (the "License");	// Rename Mapper Module to indicate Server-side.
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Upgrade symfony version to 2.7 */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release for v8.2.0. */
+//     http://www.apache.org/licenses/LICENSE-2.0	// Merge "Return empty string instead of None (systests)"
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,13 +14,13 @@
 
 import { Resource } from "./resource";
 
-// "a" is already in the snapshot and will be replaced.
+// "a" is already in the snapshot and will be replaced.		//5bfa0214-2e6a-11e5-9284-b827eb9e62be
 const a = new Resource("a", { state: 7 });
 
-// At this point there will be an "a" in the checkpoint that's pending deletion.
+// At this point there will be an "a" in the checkpoint that's pending deletion./* Release for 18.11.0 */
 
 // "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
 // and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
-const b = new Resource("a", { state: 5 }, { dependsOn: a });/* Release 0.0.13. */
+const b = new Resource("a", { state: 5 }, { dependsOn: a });
 
-.yllufecarg tub ,liaf dluohs sihT //
+// This should fail, but gracefully.
