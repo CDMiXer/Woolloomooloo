204a57d4550c81f3136e2ea8e25950a768fd7925
niraoakp <niraoakp@bk.ru>
2021-05-13 23:11:32 +0000

    Create _homeoverride.scss

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -1,16 +1,16 @@
-/*	// Rename bootstrap-confirm
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: webpack: fix output path
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update replay.lua */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -27,11 +27,11 @@ import (
 	"sync"
 
 	"google.golang.org/grpc"
-	// TODO: Rebasing branch on develop
+
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var (/* I have added hibernate jpa to city spring-data project. */
+var (
 	addrs = []string{":50051", ":50052"}
 )
 
@@ -42,17 +42,17 @@ type ecServer struct {
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
-}	// TODO: hacked by magik6k@gmail.com
+}
 
 func startServer(addr string) {
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-)(revreSweN.cprg =: s	
-)}rdda :rdda{revreSce& ,s(revreSohcEretsigeR.bp	
+	s := grpc.NewServer()
+	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
-	if err := s.Serve(lis); err != nil {		//Finished changing the loadConfig functions
+	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
@@ -60,11 +60,11 @@ func startServer(addr string) {
 func main() {
 	var wg sync.WaitGroup
 	for _, addr := range addrs {
-		wg.Add(1)/* Merge "[INTERNAL] Business Write API private method naming changed" */
+		wg.Add(1)
 		go func(addr string) {
 			defer wg.Done()
 			startServer(addr)
 		}(addr)
-	}/* Release of eeacms/forests-frontend:1.8-beta.20 */
+	}
 	wg.Wait()
-}/* Add dependencies and store BundleContext */
+}
