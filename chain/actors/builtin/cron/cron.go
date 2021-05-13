354dd07e0c5d9db307bfe0fd9c2a09bdf333de8a
niraoakp <niraoakp@bk.ru>
2021-05-13 09:37:41 +0000

    Name Correction

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -4,7 +4,7 @@ import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (/* Minor changes. Release 1.5.1. */
+var (
 	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
 )
