28ed1e000f47b5ad505bd14013a3914b981e1867
niraoakp <niraoakp@bk.ru>
2021-05-13 09:39:38 +0000

    Update metadata and prepare release

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//zb metainfo updates
 
 import * as pulumi from "@pulumi/pulumi";
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// TODO: hacked by alessio@tendermint.com
 
     constructor() {
         this.create = async (inputs: any) => {
-            return {
-                id: "0",
+            return {		//Update message_media_downloadable_image.py
+                id: "0",	// Expanding flags for weather
                 outs: undefined,
             };
         };
     }
 }
 
-class Resource extends pulumi.dynamic.Resource {
+class Resource extends pulumi.dynamic.Resource {/* Release 0.9.0. */
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);
+        super(Provider.instance, name, {}, opts);	// TODO: will be fixed by hugomrdias@gmail.com
     }
 }
 
