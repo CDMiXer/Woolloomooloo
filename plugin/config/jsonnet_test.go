b3ccc8c305569ed8f8cf1a3c0f025e69defb01b5
niraoakp <niraoakp@bk.ru>
2021-05-13 12:22:57 +0000

    0c033330-2e50-11e5-9284-b827eb9e62be

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//trigger new build for ruby-head-clang (64e2285)
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Remove redundant header from checker result. WIP#12 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package config	// TODO: Fixed links in report page
+package config
