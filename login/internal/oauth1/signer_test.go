ff040ac63852288e384cad42e02f6cccb62017fe
niraoakp <niraoakp@bk.ru>
2021-05-13 13:42:25 +0000

    Merge branch 'feature/64572' into develop

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved.
+// Copyright (c) 2015 Dalton Hubble. All rights reserved./* adding removal of fragments in outputPosChanged, debugging facilities added too */
 // Copyrights licensed under the MIT License.
-
-package oauth1
+/* Release 2.4b4 */
+package oauth1	// TODO: hacked by indexxuan@gmail.com
