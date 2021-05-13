1045c7773f42d03d9f4b647ef9d7f717c28b2fb4
niraoakp <niraoakp@bk.ru>
2021-05-13 16:29:16 +0000

    [RELEASE]merging 'feature-OA-81' into 'dev'

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Fixed rollback of traces movement.
+
 import { Resource } from "./resource";
 
-// Allocate a new resource. When this exists, we should not allow/* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
-// the stack holding it to be `rm`'d without `--force`./* Release 1.2.0-beta8 */
+// Allocate a new resource. When this exists, we should not allow/* Added some features for GUI. */
+// the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
 
 export let o = a.state;
