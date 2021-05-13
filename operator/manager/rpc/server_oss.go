1ed01fd60ee713514c5882cb78f1a83e189cd991
niraoakp <niraoakp@bk.ru>
2021-05-13 16:16:39 +0000

    Changed icon for 'retry' button on installer.

--- operator/manager/rpc/server_oss.go
+++ operator/manager/rpc/server_oss.go
@@ -1,43 +1,43 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Updating build-info/dotnet/cli/release/15.5 for preview3-fnl-007316
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// added license to data_src
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Fixed links to joshvote repo
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//update asset ids
+// limitations under the License.
 
 // +build oss
 
-package rpc/* Added connection tracing and link / info about HPack taken from Aerys. */
+package rpc
 
 import (
 	"context"
 	"errors"
 	"io"
 	"net/http"
-/* Add FFI_COMPILER preprocessor directive, was missing on Release mode */
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/operator/manager"		//Add comments to ImageMetadata.java
+	"github.com/drone/drone/operator/manager"
 )
 
 // Server is a no-op rpc server.
 type Server struct {
 	manager manager.BuildManager
 	secret  string
-}/* 7f6cf545-2d15-11e5-af21-0401358ea401 */
+}
 
 // NewServer returns a no-op rpc server.
 func NewServer(manager.BuildManager, string) *Server {
 	return &Server{}
 }
-/* Some glitches fixed */
-// Request requests the next available build stage for execution./* Use a CIFilter to make the base layer monochrome. */
+
+// Request requests the next available build stage for execution.
 func (Server) Request(ctx context.Context, args *manager.Request) (*core.Stage, error) {
 	return nil, errors.New("not implemented")
 }
@@ -45,19 +45,19 @@ func (Server) Request(ctx context.Context, args *manager.Request) (*core.Stage, 
 // Accept accepts the build stage for execution.
 func (Server) Accept(ctx context.Context, stage int64, machine string) error {
 	return errors.New("not implemented")
-}/* add tsk_startFrom function */
-/* Fixed Release Reference in Readme.md */
+}
+
 // Netrc returns a valid netrc for execution.
-func (Server) Netrc(ctx context.Context, repo int64) (*core.Netrc, error) {	// Fix a problem in the runtime checking.
+func (Server) Netrc(ctx context.Context, repo int64) (*core.Netrc, error) {
 	return nil, errors.New("not implemented")
 }
-/* Release of eeacms/forests-frontend:2.0-beta.46 */
-// Details fetches build details/* Release notes for 4.1.3. */
+
+// Details fetches build details
 func (Server) Details(ctx context.Context, stage int64) (*manager.Context, error) {
 	return nil, errors.New("not implemented")
 }
 
-// Before signals the build step is about to start.	// TODO: hacked by brosner@gmail.com
+// Before signals the build step is about to start.
 func (Server) Before(ctxt context.Context, step *core.Step) error {
 	return errors.New("not implemented")
 }
