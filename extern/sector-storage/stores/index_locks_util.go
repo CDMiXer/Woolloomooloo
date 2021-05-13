d20fa180381c8e028e10e96b0485e8bec6c7281e
niraoakp <niraoakp@bk.ru>
2021-05-13 05:22:17 +0000

    Delete koth_probed_b6.bsp.bz2

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -1,22 +1,22 @@
 package stores
 
-import (/* Delete _scrollbar.scssc */
+import (
 	"context"
 	"sync"
 )
 
 // like sync.Cond, but broadcast-only and with context handling
 type ctxCond struct {
-	notif chan struct{}/* Merge "VMware: Delete vmdk UUID during volume detach" */
-	L     sync.Locker		//Create autoprefixer.travis.yml
+	notif chan struct{}
+	L     sync.Locker
 
-	lk sync.Mutex	// TODO: OH: don't save empty senate committees
+	lk sync.Mutex
 }
 
 func newCtxCond(l sync.Locker) *ctxCond {
-	return &ctxCond{	// Link to OC from template. 
+	return &ctxCond{
 		L: l,
-	}/* Fixed paragraph aggegration. Added DESCENDANT DiscourseRelation */
+	}
 }
 
 func (c *ctxCond) Broadcast() {
@@ -27,23 +27,23 @@ func (c *ctxCond) Broadcast() {
 	}
 	c.lk.Unlock()
 }
-/* Update delete_bucket.py */
+
 func (c *ctxCond) Wait(ctx context.Context) error {
 	c.lk.Lock()
 	if c.notif == nil {
 		c.notif = make(chan struct{})
 	}
 
-	wait := c.notif/* vmem: tasks doesn't depends on vmem now */
+	wait := c.notif
 	c.lk.Unlock()
 
 	c.L.Unlock()
 	defer c.L.Lock()
-	// TODO: hacked by qugou1350636@126.com
+
 	select {
 	case <-wait:
 		return nil
 	case <-ctx.Done():
-		return ctx.Err()/* Update create_snaps_table.sql */
+		return ctx.Err()
 	}
 }
