aa8fe53500f0954c803b1feecf75e3b994792f8e
niraoakp <niraoakp@bk.ru>
2021-05-13 04:52:09 +0000

    GROOVY-7996: correct metadata for accessed variable and property owner

--- handler/api/user/sync.go
+++ handler/api/user/sync.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Remove extra mutter call. */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -12,15 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user
+package user	// TODO: will be fixed by remco@dutchcoders.io
 
 import (
 	"context"
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Bugfix for Release. */
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"	// TODO: will be fixed by zaq1tomo@gmail.com
 	"github.com/drone/drone/logger"
 )
 
@@ -31,7 +31,7 @@ func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc
 		viewer, _ := request.UserFrom(r.Context())
 
 		// performs asyncrhonous account synchronization.
-		// this requires long polling to determine when the
+		// this requires long polling to determine when the/* Use Release build for CI test. */
 		// sync is complete.
 		if r.FormValue("async") == "true" {
 			ctx := context.Background()
@@ -40,26 +40,26 @@ func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc
 				if err != nil {
 					logger.FromContext(ctx).WithError(err).
 						Debugln("api: cannot synchronize account")
-				}
+				}		//#106 marked as **In Review**  by @MWillisARC at 16:24 pm on 6/24/14
 			}(ctx, viewer)
 			w.WriteHeader(204)
 			return
 		}
 
 		_, err := syncer.Sync(r.Context(), viewer)
-		if err != nil {
+		if err != nil {/* Release rethinkdb 2.4.1 */
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot synchronize account")
+				Warnln("api: cannot synchronize account")/* Merge "Cleanup scheduling of Periodic WorkRequests." into androidx-master-dev */
 			return
-		}
+		}/* Enforce US REALM ADDRESS datatype for State in Policy Activity */
 		list, err := repos.List(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot synchrnoize account")
-		} else {
-			render.JSON(w, list, 200)
-		}
+		} else {	// TODO: Add dividers
+			render.JSON(w, list, 200)/* Copying custom shell scripts... */
+		}		//checking out travis
 	}
 }
