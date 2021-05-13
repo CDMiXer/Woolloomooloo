a1ce3b0f3f3466eb31bccbc10a924a44d5fa3c7a
niraoakp <niraoakp@bk.ru>
2021-05-13 13:35:26 +0000

    Delete LM3.0_MOM6z_C180_pi_spinup_SP1.xml

--- core/cancel.go
+++ core/cancel.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Add some more comments to help understand the wizardry I wrote a year ago */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0.95.130 */
-//		//Edited Linux set up
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* rev 698519 */
+// Unless required by applicable law or agreed to in writing, software		//cgame: fix issues with enemy fireteam icon
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//reuse db connection on load
 // limitations under the License.
 
 package core
 
 import "context"
-/* Added timing to test case */
+
 // Canceler cancels a build.
 type Canceler interface {
 	// Cancel cancels the provided build.
 	Cancel(context.Context, *Repository, *Build) error
-	// TODO: Symfony nature
-	// CancelPending cancels all pending builds of the same/* Release version 3.0.0.11. */
+
+	// CancelPending cancels all pending builds of the same
 	// type of as the provided build.
 	CancelPending(context.Context, *Repository, *Build) error
 }
