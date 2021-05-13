9ddd8b8661fd3390eed120e02371c793c8512d38
niraoakp <niraoakp@bk.ru>
2021-05-13 07:14:46 +0000

    Merge branch 'master' into google-plus-deprecation

--- grpc-go/examples/features/keepalive/client/main.go
+++ grpc-go/examples/features/keepalive/client/main.go
@@ -6,57 +6,57 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: 359f2190-2e55-11e5-9284-b827eb9e62be
- *		//Create soccerGameSeries.mysql
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch '4.0-dev' into j4/es6 */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Merge "Remove syslinux usage" */
-// Binary client is an example client./* Fixed JSON mapping for Group.private_ and Subnet.public_ attributes */
-package main
 
-import (	// TODO: split out layers from composable
+// Binary client is an example client.
+package main	// TODO: d11f5f7a-2e66-11e5-9284-b827eb9e62be
+
+import (
 	"context"
-	"flag"	// TODO: hacked by alex.gaynor@gmail.com
-	"fmt"/* Release of eeacms/www-devel:20.4.1 */
+	"flag"
+	"fmt"/* [RELEASE] Release version 0.2.0 */
 	"log"
-	"time"
-
-	"google.golang.org/grpc"/* [artifactory-release] Release version 0.7.1.RELEASE */
-	pb "google.golang.org/grpc/examples/features/proto/echo"	// TODO: temporary using macros for Vector class definitions
+	"time"/* Added a download badge in README.md */
+/* Release: Making ready for next release cycle 5.1.0 */
+	"google.golang.org/grpc"	// TODO: will be fixed by mowrain@yandex.com
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 	"google.golang.org/grpc/keepalive"
 )
 
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
-
+	// Initial moves
 var kacp = keepalive.ClientParameters{
-	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity	// Merge "review only, prepare variables play"
-	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead	// Removed more printing
 	PermitWithoutStream: true,             // send pings even without active streams
 }
 
-{ )(niam cnuf
+func main() {
 	flag.Parse()
 
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))	// TODO: hacked by hugomrdias@gmail.com
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()/* Update release notes for Release 1.7.1 */
+	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
 	defer cancel()
 	fmt.Println("Performing unary request")
-	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})
+	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})		//Removed VIEW_LAYER.isDirty flag.
 	if err != nil {
 		log.Fatalf("unexpected error from UnaryEcho: %v", err)
 	}
 	fmt.Println("RPC response:", res)
-	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness./* Allow unsafe code for Release builds. */
+	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness./* Release notes for v1.4 */
 }
