1146d2709bffbb16d9f726418d92f011d43d3f4d
niraoakp <niraoakp@bk.ru>
2021-05-13 01:15:40 +0000

    Continue branching 4.3 RC

--- pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
@@ -4,24 +4,24 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as utilities from "../../utilities";
 
-// Export members:	// TODO: will be fixed by vyzo@hackzen.org
+// Export members:
 export * from "./rubberTree";
-	// Push ecma to variabel and variabel to Call
+	// TODO: will be fixed by alan.shaw@protocol.ai
 // Export enums:
 export * from "../../types/enums/tree/v1";
 
 // Import resources to register:
-import { RubberTree } from "./rubberTree";
+import { RubberTree } from "./rubberTree";	// TODO: hacked by ac0dem0nk3y@gmail.com
 
 const _module = {
-    version: utilities.getVersion(),
+    version: utilities.getVersion(),		//b25e65de-2e5e-11e5-9284-b827eb9e62be
     construct: (name: string, type: string, urn: string): pulumi.Resource => {
         switch (type) {
-            case "plant-provider:tree/v1:RubberTree":	// Move dockercfg and services first before pulling docker containers (#56)
+            case "plant-provider:tree/v1:RubberTree":		//bump version to v0.0.3
                 return new RubberTree(name, <any>undefined, { urn })
             default:
                 throw new Error(`unknown resource type ${type}`);
         }
     },
-};/* Release 0.92 bug fixes */
+};
 pulumi.runtime.registerResourceModule("plant-provider", "tree/v1", _module)
