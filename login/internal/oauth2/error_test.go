8dac676ac9c769209a456ebc4c1d9bb740f0eb05
niraoakp <niraoakp@bk.ru>
2021-05-13 23:31:12 +0000

    update version in text of README.md

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2	// TODO: hacked by 13860583249@yeah.net
+package oauth2
 
-import "testing"/* Fixed a copy paste bug, by cloning objects before pasting */
+import "testing"/* Merge branch 'develop' into fix/connections */
 
-func TestError(t *testing.T) {/* Merge "Release 1.0.0.181 QCACLD WLAN Driver" */
-	err := Error{}
+func TestError(t *testing.T) {
+	err := Error{}	// TODO: Added default implementation for Component and ExperimentalParticipant
 	err.Code = "invalid_request"
-	err.Desc = " The request is missing a required parameter"		//Added notes to double/ceiling on value coverage
+	err.Desc = " The request is missing a required parameter"
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
 		t.Errorf("Want error message %q, got %q", want, got)
 	}
