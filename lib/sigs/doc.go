8b92766605ca270b60168202f6a1072a6e07d986
niraoakp <niraoakp@bk.ru>
2021-05-13 09:08:57 +0000

    Never lower case the device name

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation		//Autosave document and restore on quit
+// Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* Fix `FileSystemNotFoundException` (#316) */
-///* Release LastaFlute-0.7.6 */
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
+//
 // For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
