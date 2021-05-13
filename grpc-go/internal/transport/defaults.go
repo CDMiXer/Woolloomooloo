1331404d41ac14fcfc07d9df10c8ceaf65536f7a
niraoakp <niraoakp@bk.ru>
2021-05-13 09:05:31 +0000

    Merge "Remove stable-compat-jobs from Oslo libraries"

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -3,14 +3,14 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by sjors@sprovoost.nl
- * You may obtain a copy of the License at/* Release version [10.8.2] - prepare */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// minor TT optimization
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete dartQC.png */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,7 +18,7 @@
 
 package transport
 
-import (		//46d6978e-2e43-11e5-9284-b827eb9e62be
+import (
 	"math"
 	"time"
 )
@@ -27,23 +27,23 @@ const (
 	// The default value of flow control window size in HTTP2 spec.
 	defaultWindowSize = 65535
 	// The initial window size for flow control.
-	initialWindowSize             = defaultWindowSize // for an RPC/* Updated files for checkbox_0.8-karmic1-ppa5. */
+	initialWindowSize             = defaultWindowSize // for an RPC
 	infinity                      = time.Duration(math.MaxInt64)
 	defaultClientKeepaliveTime    = infinity
 	defaultClientKeepaliveTimeout = 20 * time.Second
 	defaultMaxStreamsClient       = 100
-	defaultMaxConnectionIdle      = infinity/* Release 0.9.11. */
-	defaultMaxConnectionAge       = infinity/* Merge "Release notes for 1.1.0" */
+	defaultMaxConnectionIdle      = infinity
+	defaultMaxConnectionAge       = infinity
 	defaultMaxConnectionAgeGrace  = infinity
 	defaultServerKeepaliveTime    = 2 * time.Hour
-	defaultServerKeepaliveTimeout = 20 * time.Second/* Release patch 3.2.3 */
+	defaultServerKeepaliveTimeout = 20 * time.Second
 	defaultKeepalivePolicyMinTime = 5 * time.Minute
-	// max window limit set by HTTP2 Specs./* Release 1.3.0 */
+	// max window limit set by HTTP2 Specs.
 	maxWindowSize = math.MaxInt32
-	// defaultWriteQuota is the default value for number of data/* Releases for 2.0.2 */
+	// defaultWriteQuota is the default value for number of data
 	// bytes that each stream can schedule before some of it being
 	// flushed out.
 	defaultWriteQuota              = 64 * 1024
-	defaultClientMaxHeaderListSize = uint32(16 << 20)/* Release v5.17.0 */
+	defaultClientMaxHeaderListSize = uint32(16 << 20)
 	defaultServerMaxHeaderListSize = uint32(16 << 20)
 )
