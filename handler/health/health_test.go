264a8c51ab48f41bec286b05d463b371b77a516b
niraoakp <niraoakp@bk.ru>
2021-05-13 04:13:38 +0000

    this buf is not gixed

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release version 3.4.6 */
 package health
 
 import (
-	"net/http/httptest"	// TODO: will be fixed by boringland@protonmail.ch
+	"net/http/httptest"
 	"testing"
-)/* Release Version 0.8.2 */
+)
 
-func TestHandleHealthz(t *testing.T) {
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/healthz", nil)/* refactor: remove tranformGroups */
+func TestHandleHealthz(t *testing.T) {/* DOC Release: enhanced procedure */
+	w := httptest.NewRecorder()/* Release: Making ready to release 3.1.2 */
+	r := httptest.NewRequest("GET", "/healthz", nil)
 
 	Handler().ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {/* Changed Month of Release */
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)		//fixed to compile
 	}
 }
