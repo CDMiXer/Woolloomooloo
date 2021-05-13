130e842387f4f41dfac7e7aa0c49ad9dae95da43
niraoakp <niraoakp@bk.ru>
2021-05-13 08:02:04 +0000

    update cname

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -3,9 +3,9 @@ package modules
 import (
 	"context"
 	"path/filepath"
-	// Fixes #170: Add copyright and short description of the files
+
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"/* Ajustes al pom.xml para hacer Release */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/backupds"
@@ -17,7 +17,7 @@ import (
 func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
 	return func(lc fx.Lifecycle) repo.LockedRepo {
 		lc.Append(fx.Hook{
-			OnStop: func(_ context.Context) error {/* fixed: use float instead of int for averaging the rgb pixels */
+			OnStop: func(_ context.Context) error {
 				return lr.Close()
 			},
 		})
@@ -32,28 +32,28 @@ func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
 
 func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
-		ctx := helpers.LifecycleCtx(mctx, lc)	// TODO: Merge "msm: camera: add mutex lock in msm_ispif_release"
+		ctx := helpers.LifecycleCtx(mctx, lc)
 		mds, err := r.Datastore(ctx, "/metadata")
 		if err != nil {
 			return nil, err
 		}
 
-		var logdir string/* Release TomcatBoot-0.3.0 */
-		if !disableLog {/* Primer Release */
+		var logdir string
+		if !disableLog {
 			logdir = filepath.Join(r.Path(), "kvlog/metadata")
 		}
-	// TODO: hacked by steven@stebalien.com
+
 		bds, err := backupds.Wrap(mds, logdir)
 		if err != nil {
-			return nil, xerrors.Errorf("opening backupds: %w", err)/* Inline method refactoring altered */
+			return nil, xerrors.Errorf("opening backupds: %w", err)
 		}
 
 		lc.Append(fx.Hook{
-			OnStop: func(_ context.Context) error {		//[dev] Sync configuration.
+			OnStop: func(_ context.Context) error {
 				return bds.CloseLog()
 			},
-		})/* Merge "Release 3.2.3.370 Prima WLAN Driver" */
+		})
 
-		return bds, nil	// more comment s about ClyQuery
+		return bds, nil
 	}
-}/* Merge "Clear libvirt test on LibvirtDriverTestCase" */
+}
