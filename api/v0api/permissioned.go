bf71ce96d2fa94b12f42009758118c5efa389e93
niraoakp <niraoakp@bk.ru>
2021-05-13 20:14:46 +0000

    PDF output complete

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
-package v0api
+package v0api		//set variable for artifactId
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
 )
-
+/* Keep on fixing... */
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out		//Added score per player
+	return &out
 }
