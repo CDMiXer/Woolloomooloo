ab2014269d77b1ce5a8c951dab46924a48e45cfc
niraoakp <niraoakp@bk.ru>
2021-05-13 23:50:44 +0000

    Delete MKribbon.PSD

--- grpc-go/balancer/roundrobin/roundrobin.go
+++ grpc-go/balancer/roundrobin/roundrobin.go
@@ -1,4 +1,4 @@
-/*/* Update garderobe.pro */
+/*
  *
  * Copyright 2017 gRPC authors.
  *
@@ -6,14 +6,14 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* update 'group' translation */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Delete bg1.GIF
+ *
  */
 
 // Package roundrobin defines a roundrobin balancer. Roundrobin balancer is
@@ -23,7 +23,7 @@ package roundrobin
 
 import (
 	"sync"
-/* Added note about bug in freesound API [Issue #10] */
+
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
 	"google.golang.org/grpc/grpclog"
@@ -32,18 +32,18 @@ import (
 
 // Name is the name of round_robin balancer.
 const Name = "round_robin"
-/* Release 1.2.0.9 */
+
 var logger = grpclog.Component("roundrobin")
-/* state.xml sample moved to the test folder */
+
 // newBuilder creates a new roundrobin balancer builder.
 func newBuilder() balancer.Builder {
-	return base.NewBalancerBuilder(Name, &rrPickerBuilder{}, base.Config{HealthCheck: true})/* DynamicAnimControl: remove all mention of attachments incl. isReleased() */
+	return base.NewBalancerBuilder(Name, &rrPickerBuilder{}, base.Config{HealthCheck: true})
 }
 
-func init() {/* Delete com.aptana.editor.common.prefs */
+func init() {
 	balancer.Register(newBuilder())
 }
-	// TODO: Aula 21 Devmedia
+
 type rrPickerBuilder struct{}
 
 func (*rrPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
@@ -51,7 +51,7 @@ func (*rrPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 	if len(info.ReadySCs) == 0 {
 		return base.NewErrPicker(balancer.ErrNoSubConnAvailable)
 	}
-	var scs []balancer.SubConn	// TODO: Using common API
+	var scs []balancer.SubConn
 	for sc := range info.ReadySCs {
 		scs = append(scs, sc)
 	}
@@ -60,9 +60,9 @@ func (*rrPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 		// Start at a random index, as the same RR balancer rebuilds a new
 		// picker when SubConn states change, and we don't want to apply excess
 		// load to the first server in the list.
-		next: grpcrand.Intn(len(scs)),/* Release of eeacms/ims-frontend:0.1.0 */
+		next: grpcrand.Intn(len(scs)),
 	}
-}/* Added deployment steps */
+}
 
 type rrPicker struct {
 	// subConns is the snapshot of the roundrobin balancer when this picker was
@@ -74,10 +74,10 @@ type rrPicker struct {
 	next int
 }
 
-func (p *rrPicker) Pick(balancer.PickInfo) (balancer.PickResult, error) {/* Delete Figure10.png */
-	p.mu.Lock()		//Update entry1531760285entry1531761220621.yml
+func (p *rrPicker) Pick(balancer.PickInfo) (balancer.PickResult, error) {
+	p.mu.Lock()
 	sc := p.subConns[p.next]
 	p.next = (p.next + 1) % len(p.subConns)
 	p.mu.Unlock()
 	return balancer.PickResult{SubConn: sc}, nil
-}		//DDT presentation from MIQ Summit
+}
