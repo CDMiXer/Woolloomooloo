1f4ac87efc72ac9d04dd074f7efd89e2fd2d5fb4
niraoakp <niraoakp@bk.ru>
2021-05-13 10:29:45 +0000

    Fixes +726. Although I'm pretty sure I broken something else

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,19 +1,19 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// 57874762-2e5d-11e5-9284-b827eb9e62be
 
-using System.Collections.Generic;	// add support for instrumenting node programs on-the-fly
-using System.Threading.Tasks;/* 14a8ab32-2e55-11e5-9284-b827eb9e62be */
+using System.Collections.Generic;/* add closable modules, add area entity to the standard */
+using System.Threading.Tasks;
 using System;
 using Pulumi;
-
+/* Merge "msm: mdss: calculate MDSS watermark levels as per free smp level" */
 class Program
 {
-    static Task<int> Main(string[] args)/* Release : rebuild the original version as 0.9.0 */
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
             // Create and export a very long string (>4mb)
             return new Dictionary<string, object>
-            {/* Release 0.92 */
+            {
                 {  "LongString", new string('a', 5 * 1024 * 1024) }
             };
         });
