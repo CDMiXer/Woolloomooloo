1d4ac61692dd2e5529b1672f735eee7bd0bad24e
niraoakp <niraoakp@bk.ru>
2021-05-13 04:01:18 +0000

    Updated incorrect link in readme

--- handler/api/repos/builds/deploys/delete_test.go
+++ handler/api/repos/builds/deploys/delete_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package deploys	// Rebuilt index with faisalm1991
+package deploys
