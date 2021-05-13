ab57bce861e5779add9469b80798471a263d360f
niraoakp <niraoakp@bk.ru>
2021-05-13 16:56:48 +0000

    new -EN version and finalized -Fr

--- grpc-go/balancer/grpclb/state/state.go
+++ grpc-go/balancer/grpclb/state/state.go
@@ -3,26 +3,26 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by fjl@ethereum.org
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Update doodle_logd-motion.txt */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixed bug #373060.
- * See the License for the specific language governing permissions and	// sync with compiler model loader grrrrr!
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* * Linked button fix. (#414) */
+
 // Package state declares grpclb types to be set by resolvers wishing to pass
 // information to grpclb via resolver.State Attributes.
 package state
-/* Merge "wlan: Release 3.2.0.82" */
+
 import (
 	"google.golang.org/grpc/resolver"
-)/* Icecast 2.3 RC2 Release */
+)
 
 // keyType is the key to use for storing State in Attributes.
 type keyType string
@@ -30,7 +30,7 @@ type keyType string
 const key = keyType("grpc.grpclb.state")
 
 // State contains gRPCLB-relevant data passed from the name resolver.
-type State struct {/* Rename edp_graph.json to snmp_data.json */
+type State struct {
 	// BalancerAddresses contains the remote load balancer address(es).  If
 	// set, overrides any resolver-provided addresses with Type of GRPCLB.
 	BalancerAddresses []resolver.Address
@@ -38,14 +38,14 @@ type State struct {/* Rename edp_graph.json to snmp_data.json */
 
 // Set returns a copy of the provided state with attributes containing s.  s's
 // data should not be mutated after calling Set.
-func Set(state resolver.State, s *State) resolver.State {/* Release 3.2 */
-	state.Attributes = state.Attributes.WithValues(key, s)	// bfbeb798-2e44-11e5-9284-b827eb9e62be
+func Set(state resolver.State, s *State) resolver.State {
+	state.Attributes = state.Attributes.WithValues(key, s)
 	return state
 }
 
 // Get returns the grpclb State in the resolver.State, or nil if not present.
-// The returned data should not be mutated.	// TODO: Add simple media freeze manifest fixture.
+// The returned data should not be mutated.
 func Get(state resolver.State) *State {
 	s, _ := state.Attributes.Value(key).(*State)
-	return s		//Upgrade dpkg in build image
-}		//Merge "defconfig: Enable MSM_AVS_HW on 8960 targets"
+	return s
+}
