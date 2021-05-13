cba817d052f93761b3041dfd1c5afad321ed7d5d
niraoakp <niraoakp@bk.ru>
2021-05-13 16:00:39 +0000

    add video_note in mediasettings

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -4,9 +4,9 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Ambiguous Coordinates */
-//		//Merge "msm: qdsp5: Aligning buffer size to 32." into android-msm-2.6.32
-// Unless required by applicable law or agreed to in writing, software	// completely bollixed it up fixed it now
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,7 +14,7 @@
 
 // +build oss
 
-package builds	// Creating command String for DNSrecon
+package builds
 
 import (
 	"net/http"
