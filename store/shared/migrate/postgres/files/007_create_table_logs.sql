b31aa0adba1293a278799d37a6fd16b46654a54d
niraoakp <niraoakp@bk.ru>
2021-05-13 18:13:32 +0000

    Update MyteKernel1-0

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs		//NetKAN generated mods - KSPInterstellarExtended-1.25.9.1
+-- name: create-table-logs
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    SERIAL PRIMARY KEY
 ,log_data  BYTEA
-);		//Merge "Add option to clear profile data to 'cmd package compile'" into nyc-dev
+);
