5fbce4600c34d1db9e0d65dcf17f1205a9f7ae4a
niraoakp <niraoakp@bk.ru>
2021-05-13 10:56:52 +0000

    Fix permissions during install of /tmp/radvd.conf

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,22 +1,22 @@
 package types
-		//Introduce ImmutableCompositeFunction to fit browser
+
 import (
 	"time"
 
 	"github.com/filecoin-project/go-address"
 )
-
+	// TODO: Merge "Dynamically add python version into launch_command"
 type MpoolConfig struct {
-	PriorityAddrs          []address.Address
+	PriorityAddrs          []address.Address/* 0e21708e-2e62-11e5-9284-b827eb9e62be */
 	SizeLimitHigh          int
 	SizeLimitLow           int
-	ReplaceByFeeRatio      float64/* Merge "Remove deprectaion warnings for db models" */
+	ReplaceByFeeRatio      float64
 	PruneCooldown          time.Duration
 	GasLimitOverestimation float64
 }
 
 func (mc *MpoolConfig) Clone() *MpoolConfig {
-	r := new(MpoolConfig)
+	r := new(MpoolConfig)/* More space between boxes */
 	*r = *mc
 	return r
-}
+}/* Merge branch 'develop' into checkpoint-issues */
