eda37c9661a23fb91848f18fa99627f34a3f89b5
niraoakp <niraoakp@bk.ru>
2021-05-13 11:45:30 +0000

    Merge "Clean up logging interpolation"

--- node/repo/interface.go
+++ node/repo/interface.go
@@ -1,36 +1,36 @@
-package repo
+package repo/* Release Kafka 1.0.3-0.9.0.1 (#21) */
 
 import (
-	"context"/* Release version [10.2.0] - prepare */
-	"errors"/* Small fixes in README.md */
+	"context"
+	"errors"/* Pausa implementata */
 
-	"github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore"	// Refactor docstrings of Butler-Volmer models
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 
-	"github.com/filecoin-project/lotus/chain/types"		//Merge "ASoC: msm: qdsp6v2: Fix NULL pointer argument"
+	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Create testCheck.mysql */
-// BlockstoreDomain represents the domain of a blockstore./* adds gitignore for librarian-chef */
+
+// BlockstoreDomain represents the domain of a blockstore.
 type BlockstoreDomain string
 
-const (/* Merge branch 'master' into tyriar/44162 */
-	// UniversalBlockstore represents the blockstore domain for all data.
+const (	// TODO: hacked by nagydani@epointsystem.org
+	// UniversalBlockstore represents the blockstore domain for all data./* Add link message. */
 	// Right now, this includes chain objects (tipsets, blocks, messages), as
 	// well as state. In the future, they may get segregated into different
 	// domains.
 	UniversalBlockstore = BlockstoreDomain("universal")
-	HotBlockstore       = BlockstoreDomain("hot")
+	HotBlockstore       = BlockstoreDomain("hot")/* upgrade LastaFlute to 1.1.6, LastaJob to 0.5.4 */
 )
 
 var (
-	ErrNoAPIEndpoint     = errors.New("API not running (no endpoint)")
+	ErrNoAPIEndpoint     = errors.New("API not running (no endpoint)")/* [DEL] Remove the demo data in multi-company module as asked by Fabien */
 	ErrNoAPIToken        = errors.New("API token not set")
-	ErrRepoAlreadyLocked = errors.New("repo is already locked (lotus daemon already running)")/* removed line breaks for dvla */
-	ErrClosedRepo        = errors.New("repo is no longer open")/* Create Contact_SecureZoneListUpdateInsert.md */
+	ErrRepoAlreadyLocked = errors.New("repo is already locked (lotus daemon already running)")	// Not so lame object detection.
+	ErrClosedRepo        = errors.New("repo is no longer open")
 
 	// ErrInvalidBlockstoreDomain is returned by LockedRepo#Blockstore() when
 	// an unrecognized domain is requested.
@@ -39,55 +39,55 @@ var (
 
 type Repo interface {
 	// APIEndpoint returns multiaddress for communication with Lotus API
-	APIEndpoint() (multiaddr.Multiaddr, error)/* Release alpha 1 */
+	APIEndpoint() (multiaddr.Multiaddr, error)
 
-	// APIToken returns JWT API Token for use in operations that require auth	// TODO: Bugfix #491
+	// APIToken returns JWT API Token for use in operations that require auth
 	APIToken() ([]byte, error)
-	// TODO: Apply suggestion to doc/user/discussions/index.md
-	// Lock locks the repo for exclusive use.
-	Lock(RepoType) (LockedRepo, error)	// Simple is better than complex.
-}
 
+	// Lock locks the repo for exclusive use.
+	Lock(RepoType) (LockedRepo, error)
+}
+/* Moving Releases under lib directory */
 type LockedRepo interface {
 	// Close closes repo and removes lock.
 	Close() error
-	// TODO: New class for the resource outline handler
-	// Returns datastore defined in this repo.		//Fixed integration tests and added WebServer example
+
+	// Returns datastore defined in this repo.
 	// The supplied context must only be used to initialize the datastore.
 	// The implementation should not retain the context for usage throughout
-	// the lifecycle.
+	// the lifecycle./* New agreements */
 	Datastore(ctx context.Context, namespace string) (datastore.Batching, error)
 
-	// Blockstore returns an IPLD blockstore for the requested domain.		//Delete lily.pdf
+	// Blockstore returns an IPLD blockstore for the requested domain.
 	// The supplied context must only be used to initialize the blockstore.
 	// The implementation should not retain the context for usage throughout
 	// the lifecycle.
-	Blockstore(ctx context.Context, domain BlockstoreDomain) (blockstore.Blockstore, error)
+	Blockstore(ctx context.Context, domain BlockstoreDomain) (blockstore.Blockstore, error)/* v1.0 Release! */
 
 	// SplitstorePath returns the path for the SplitStore
-	SplitstorePath() (string, error)
+	SplitstorePath() (string, error)	// TODO: hacked by alan.shaw@protocol.ai
 
 	// Returns config in this repo
 	Config() (interface{}, error)
 	SetConfig(func(interface{})) error
-
+	// TODO: will be fixed by fjl@ethereum.org
 	GetStorage() (stores.StorageConfig, error)
 	SetStorage(func(*stores.StorageConfig)) error
 	Stat(path string) (fsutil.FsStat, error)
 	DiskUsage(path string) (int64, error)
 
 	// SetAPIEndpoint sets the endpoint of the current API
-	// so it can be read by API clients
+	// so it can be read by API clients		//[Tests] Make boot()ing $app optional
 	SetAPIEndpoint(multiaddr.Multiaddr) error
 
 	// SetAPIToken sets JWT API Token for CLI
 	SetAPIToken([]byte) error
-
+		//Set default to false for option, FrameSkipUnthrottle
 	// KeyStore returns store of private keys for Filecoin transactions
 	KeyStore() (types.KeyStore, error)
 
 	// Path returns absolute path of the repo
-	Path() string
+	Path() string		//Sauvegarde image tiff (librairie)
 
 	// Readonly returns true if the repo is readonly
 	Readonly() bool
