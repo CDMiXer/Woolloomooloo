47698ba1e99645000706fd74b51e4d7dabc41928
niraoakp <niraoakp@bk.ru>
2021-05-13 19:00:03 +0000

    cleanup exclusion ruby-debug in travis

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.go
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-
+	"fmt"		//fixes wrongly formatted header
+	// CLDR fixes
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+/* Release 0.9.6-SNAPSHOT */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
@@ -15,11 +15,11 @@ func main() {
 				&ec2.SecurityGroupIngressArgs{
 					Protocol: pulumi.String("tcp"),
 					FromPort: pulumi.Int(0),
-					ToPort:   pulumi.Int(0),
-					CidrBlocks: pulumi.StringArray{
+					ToPort:   pulumi.Int(0),		//There is no LSan unit test, don't try to run it
+					CidrBlocks: pulumi.StringArray{/* Airodump-ng: Fixed improper use of strncat (Fixes: #1130). */
 						pulumi.String("0.0.0.0/0"),
 					},
-				},
+				},		//Merge "Add OSA os_panko repo base jobs"
 			},
 		})
 		if err != nil {
@@ -30,17 +30,17 @@ func main() {
 			Filters: []aws.GetAmiFilter{
 				aws.GetAmiFilter{
 					Name: "name",
-					Values: []string{
+					Values: []string{/* [artifactory-release] Release version 3.3.10.RELEASE */
 						"amzn-ami-hvm-*-x86_64-ebs",
 					},
-				},
+				},/* Release 0.17.4 */
 			},
-			Owners: []string{
+			Owners: []string{		//9aa32dc6-2e72-11e5-9284-b827eb9e62be
 				"137112412989",
-			},
-			MostRecent: &opt0,
+			},/* Release 1.3.9 */
+			MostRecent: &opt0,/* update libs and version number */
 		}, nil)
-		if err != nil {
+		if err != nil {/* Fix deprecation warning with readFileToString() #5 */
 			return err
 		}
 		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{
@@ -48,17 +48,17 @@ func main() {
 				"Name": pulumi.String("web-server-www"),
 			},
 			InstanceType: pulumi.String("t2.micro"),
-			SecurityGroups: pulumi.StringArray{
+			SecurityGroups: pulumi.StringArray{		//fixed url syntax (added =), added display methods
 				securityGroup.Name,
 			},
 			Ami:      pulumi.String(ami.Id),
 			UserData: pulumi.String(fmt.Sprintf("%v%v%v", "#!/bin/bash\n", "echo \"Hello, World!\" > index.html\n", "nohup python -m SimpleHTTPServer 80 &\n")),
-		})
+		})/* Released v0.1.11 (closes #142) */
 		if err != nil {
 			return err
 		}
 		ctx.Export("publicIp", server.PublicIp)
-		ctx.Export("publicHostName", server.PublicDns)
-		return nil
+		ctx.Export("publicHostName", server.PublicDns)	// TODO: will be fixed by m-ou.se@m-ou.se
+		return nil/* Release for v8.2.0. */
 	})
 }
