4ffddf9250ef8665d1b2e4687326a083231dbe28
niraoakp <niraoakp@bk.ru>
2021-05-13 04:50:27 +0000

    Improved notifications: Notify user only if there are new releases

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -6,14 +6,14 @@ import (
 
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Release 11.1 */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+/* Release AppIntro 4.2.3 */
 	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 )
 
@@ -22,28 +22,28 @@ var _ State = (*state2)(nil)
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* Documentation and website update. Release 1.2.0. */
 		return nil, err
 	}
 	return &out, nil
 }
-
+		//Add cerulean theme
 type state2 struct {
-	msig2.State
+	msig2.State/* remplacement parameters.dist.yml */
 	store adt.Store
-}
+}/* #172 Release preparation for ANB */
 
 func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
-}
+}	// TODO: Delete DSC00321.JPG
 
 func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
-}
+}	// TODO: used es6 features for infix operator, aka exponent
 
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
-}
+}/* Patch externalFile qualified */
 
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
@@ -53,18 +53,18 @@ func (s *state2) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
 
-func (s *state2) Signers() ([]address.Address, error) {
+func (s *state2) Signers() ([]address.Address, error) {		//We edit meeting in this template rather than add
 	return s.State.Signers, nil
 }
 
 func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
-	if err != nil {
+	if err != nil {	// TODO: Some adjustments of brightness of the code understanding.
 		return err
 	}
-	var out msig2.Transaction
+	var out msig2.Transaction		//User-Model: SQL-Injections verhindern (bisher nur load-Methode)
 	return arr.ForEach(&out, func(key string) error {
-		txid, n := binary.Varint([]byte(key))
+		txid, n := binary.Varint([]byte(key))/* fix(package): update mongoose to version 5.4.4 */
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
@@ -75,7 +75,7 @@ func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 func (s *state2) PendingTxnChanged(other State) (bool, error) {
 	other2, ok := other.(*state2)
 	if !ok {
-		// treat an upgrade as a change, always
+		// treat an upgrade as a change, always	// TODO: will be fixed by davidad@alum.mit.edu
 		return true, nil
 	}
 	return !s.State.PendingTxns.Equals(other2.PendingTxns), nil
@@ -85,7 +85,7 @@ func (s *state2) transactions() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.PendingTxns)
 }
 
-func (s *state2) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
+func (s *state2) decodeTransaction(val *cbg.Deferred) (Transaction, error) {/* Release 1.3.23 */
 	var tx msig2.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
