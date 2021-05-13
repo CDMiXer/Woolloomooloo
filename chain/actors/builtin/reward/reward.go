15bde10301719f9c354e0d423a9b65c54096aba0
niraoakp <niraoakp@bk.ru>
2021-05-13 15:17:02 +0000

    update lib

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,12 +1,12 @@
-package reward	// TODO: Add InfluxDB to metrics and monitoring
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"/* Included a licensing section with a few resources. */
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/cbor"		//Update TriangleAABBTree.cs
+	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
@@ -18,29 +18,29 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"		//Update hello word
+	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Merge pull request #94 from oli-obk/fix/reconnect_race_master
-func init() {/* Release ver.1.4.3 */
+
+func init() {
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)/* Release of eeacms/www:19.4.26 */
+		return load0(store, root)
 	})
 
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)		//Merged time-slider into develop
+		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Changed a setting's title */
+	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 }
-/* Updated CS-CoreLib Version to the latest Release */
-var (/* 739d9728-2e5b-11e5-9284-b827eb9e62be */
+
+var (
 	Address = builtin4.RewardActorAddr
 	Methods = builtin4.MethodsReward
 )
@@ -50,26 +50,26 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 
 	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
-/* MSN: Added support for file transfer type RichText/Media_GenericFile */
+
 	case builtin2.RewardActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.RewardActorCodeID:	// TODO: 1084f99e-2e5e-11e5-9284-b827eb9e62be
+	case builtin3.RewardActorCodeID:
 		return load3(store, act.Head)
 
 	case builtin4.RewardActorCodeID:
 		return load4(store, act.Head)
-	// TODO: Added list comprehensions to init functions
+
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
 type State interface {
 	cbor.Marshaler
-	// CSS pour les messages.
+
 	ThisEpochBaselinePower() (abi.StoragePower, error)
 	ThisEpochReward() (abi.StoragePower, error)
-	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)/* Add docker post from workflow */
+	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
 
 	EffectiveBaselinePower() (abi.StoragePower, error)
 	EffectiveNetworkTime() (abi.ChainEpoch, error)
