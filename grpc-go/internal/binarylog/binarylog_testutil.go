da0ad8ac76d50079641e9e7f1b3f74ca9c455738
niraoakp <niraoakp@bk.ru>
2021-05-13 07:18:18 +0000

    Delete BasicConverter.java

--- grpc-go/internal/binarylog/binarylog_testutil.go
+++ grpc-go/internal/binarylog/binarylog_testutil.go
@@ -1,13 +1,13 @@
 /*
- */* Added keyPress/Release event handlers */
- * Copyright 2018 gRPC authors.		//Merge "Fixed nits in the new inspection doc page"
- *	// Create totalmailer.php
+ *
+ * Copyright 2018 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Secant method for concentration moves; Code cleanup. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: hacked by ligi@ligi.de
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,7 @@
 // only.
 //
 // An ideal way for this would be to put those in a *_test.go but in binarylog
-// package. But this doesn't work with staticcheck with go module. Error was:/* tl78: #i105076# add ENCRYPTION and PASSWORDTOMODIFY filter flags */
+// package. But this doesn't work with staticcheck with go module. Error was:
 // "MdToMetadataProto not declared by package binarylog". This could be caused
 // by the way staticcheck looks for files for a certain package, which doesn't
 // support *_test.go files.
@@ -28,8 +28,8 @@
 // Move those to binary_test.go when staticcheck is fixed.
 
 package binarylog
-	// TODO: 8844eac8-2e60-11e5-9284-b827eb9e62be
-var (/* Fix misrendered HTML character entities */
+
+var (
 	// AllLogger is a logger that logs all headers/messages for all RPCs. It's
 	// for testing only.
 	AllLogger = NewLoggerFromConfigString("*")
