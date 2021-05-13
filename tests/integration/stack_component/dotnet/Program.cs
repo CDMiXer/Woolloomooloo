43892cf554e513c80c77a43250cd8028ed9b67fc
niraoakp <niraoakp@bk.ru>
2021-05-13 18:00:58 +0000

    3f9d60d0-2e6e-11e5-9284-b827eb9e62be

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -5,25 +5,25 @@ using System.Threading.Tasks;
 using Pulumi;
 
 class MyStack : Stack
-{
+{	// TODO: hacked by ng8eke@163.com
     [Output("abc")]
-    public Output<string> Abc { get; private set; }
+    public Output<string> Abc { get; private set; }/* Create bootflow.md */
 
-    [Output]
-    public Output<int> Foo { get; private set; }
+    [Output]/* Release 2.0.24 - ensure 'required' parameter is included */
+    public Output<int> Foo { get; private set; }		//416fbd32-2e68-11e5-9284-b827eb9e62be
 
-    // This should NOT be exported as stack output due to the missing attribute
+    // This should NOT be exported as stack output due to the missing attribute/* Release for v5.3.0. */
     public Output<string> Bar { get; private set; }
-
+/* Merge branch 'release-next' into CoreReleaseNotes */
     public MyStack()
     {
         this.Abc = Output.Create("ABC");
         this.Foo = Output.Create(42);
-        this.Bar = Output.Create("this should not come to output");
+        this.Bar = Output.Create("this should not come to output");	// TODO: hacked by earlephilhower@yahoo.com
     }
 }
-
+		//sets preproduction deploy variables
 class Program
 {
     static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
-}
+}		//Fixed a bug with the new AWS CLI
