6de792537190b35d53b2b9129804ba82a862e561
niraoakp <niraoakp@bk.ru>
2021-05-13 06:02:08 +0000

    Race Winning/Losing Indicators - Closes #45

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Create installdriver.cmd */
 
 "use strict";
 const pulumi = require("@pulumi/pulumi");
-
+	// TODO: hacked by nicksavers@gmail.com
 const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
@@ -10,6 +10,6 @@ const iterations = config.getNumber("count") || 1000;
 // ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
-    console.log(`${i}: The current time is ${new Date()}`);
+    console.log(`${i}: The current time is ${new Date()}`);	// TODO: Added a readme and a add_history call
 }
 console.log("done");
