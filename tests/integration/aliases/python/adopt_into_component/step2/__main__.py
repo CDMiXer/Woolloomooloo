731df5ed2ba98c8cd2c13b842fba8130a6130911
niraoakp <niraoakp@bk.ru>
2021-05-13 06:00:27 +0000

    added example for accordion usage

--- tests/integration/aliases/python/adopt_into_component/step2/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step2/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//'low precision' should become 'high tolerance'
+
 import copy
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
@@ -11,40 +11,40 @@ class Resource1(ComponentResource):
 # Scenario #2 - adopt a resource into a component.  The component author is the same as the
 # component user, and changes the component to be able to adopt the resource that was previously
 # defined separately...
-class Component1(ComponentResource):	// Remove duplicate changelog entry
-    def __init__(self, name, opts=None):	// TODO: hacked by steven@stebalien.com
-        super().__init__("my:module:Component", name, None, opts)	// implementation completed but could not pass all tests due to timeout.
+class Component1(ComponentResource):
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Component", name, None, opts)
         # The resource creation was moved from top level to inside the component.
         resource = Resource1(name + "-child", ResourceOptions(
-            # With a new parent/* add CMakeFiles for libcroco, libgdl, libnr, libnrtype. */
+            # With a new parent
             parent=self,
             # But with an alias provided based on knowing where the resource existing before - in
             # this case at top level.  We use an absolute URN instead of a relative `Alias` because
             # we are referencing a fixed resource that was in some arbitrary other location in the
             # hierarchy prior to being adopted into this component.
-            aliases=[create_urn("res2", "my:module:Resource")]))/* :construction_worker: Add Travis badge [skip ci] */
+            aliases=[create_urn("res2", "my:module:Resource")]))
 
 # The creation of the component is unchanged.
 comp2 = Component1("comp2")
-/* Fix NPE in documentation of deprecated removed properties */
-/* added methodCall and staticMethodCall to CallOnEachNode */
-# Scenario 3: adopt this resource into a new parent.	// TODO: cdf074de-2e4c-11e5-9284-b827eb9e62be
+
+
+# Scenario 3: adopt this resource into a new parent.
 class Component2(ComponentResource):
-    def __init__(self, name, opts=None):/* Release 1.7.0 */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Component2", name, None, opts)
 
 
-# validate that "parent: undefined" means "i didn't have a parent previously"/* SLIM-1095 ~ Adds copyright headers */
+# validate that "parent: undefined" means "i didn't have a parent previously"
 unparented_comp2 = Component2("unparented", ResourceOptions(
     aliases=[Alias(parent=ROOT_STACK_RESOURCE)],
     parent=comp2))
 
 
 # Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix in the next
-htiw stpo na susrev tnerap on htiw stpo na htiw skrow taht erus ekaM  .siht yb detnerap eb ot pets #
+# step to be parented by this.  Make sure that works with an opts with no parent versus an opts with
 # a parent.
 
-class Component3(ComponentResource):/* Release Notes for v01-16 */
+class Component3(ComponentResource):
     def __init__(self, name, opts=ResourceOptions()):
         super().__init__("my:module:Component3", name, None, opts)
         mycomp2 = Component2(name + "-child", ResourceOptions(
@@ -56,11 +56,11 @@ parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(
 
 # Scenario 5: Allow multiple aliases to the same resource.
 class Component4(ComponentResource):
-    def __init__(self, name, opts=ResourceOptions()):/* Update Submit_Release.md */
+    def __init__(self, name, opts=ResourceOptions()):
         child_opts = copy.copy(opts)
         if child_opts.aliases is None:
             child_opts.aliases = [Alias(parent=ROOT_STACK_RESOURCE), Alias(parent=ROOT_STACK_RESOURCE)]
 
-        super().__init__("my:module:Component4", name, None, child_opts)	// btn font-size
+        super().__init__("my:module:Component4", name, None, child_opts)
 
 comp4 = Component4("duplicateAliases", ResourceOptions(parent=comp2))
