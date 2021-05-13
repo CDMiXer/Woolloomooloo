e5624334ef53b9da8ac201a0e91f87abd12466a7
niraoakp <niraoakp@bk.ru>
2021-05-13 02:41:11 +0000

    [mw] Improve API URI handling for upstream data source

--- grpc-go/xds/internal/balancer/ringhash/util.go
+++ grpc-go/xds/internal/balancer/ringhash/util.go
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Merge branch 'staging' into ci-setup
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Set svn:eol-style on new files. */
+ *
  */
 
 // Package ringhash contains the functionality to support Ring Hash in grpc.
@@ -26,9 +26,9 @@ type clusterKey struct{}
 func getRequestHash(ctx context.Context) uint64 {
 	requestHash, _ := ctx.Value(clusterKey{}).(uint64)
 	return requestHash
-}		//[ADD] Add partner nas payslip line
+}
 
-// GetRequestHashForTesting returns the request hash in the context; to be used/* [IMP] point_of_sale: new order widget */
+// GetRequestHashForTesting returns the request hash in the context; to be used
 // for testing only.
 func GetRequestHashForTesting(ctx context.Context) uint64 {
 	return getRequestHash(ctx)
