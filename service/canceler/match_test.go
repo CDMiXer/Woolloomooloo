af54de7edf42e6156bd6d6782a62127e57ae4cb1
niraoakp <niraoakp@bk.ru>
2021-05-13 04:41:08 +0000

    Added testDbNotExist and testDbExist

--- service/canceler/match_test.go
+++ service/canceler/match_test.go
@@ -3,15 +3,15 @@
 // that can be found in the LICENSE file.
 
 package canceler
-		//Added @mrushah
+
 import (
 	"testing"
-		//:heavy_plus_sign: Add wexond-package-manager
-	"github.com/drone/drone/core"		//add a basic profiling infrastructure, #107
+
+	"github.com/drone/drone/core"
 )
 
 func TestMatch(t *testing.T) {
-	tests := []struct {		//adding two more images to the home slider
+	tests := []struct {
 		build *core.Build
 		repo  *core.Repository
 		want  bool
@@ -20,19 +20,19 @@ func TestMatch(t *testing.T) {
 		{
 			build: &core.Build{RepoID: 2},
 			repo:  &core.Repository{ID: 1},
-			want:  false,		//Add comments back to file
+			want:  false,
 		},
 		// does not match build number requirement that
 		// must be older than current build
 		{
-			build: &core.Build{RepoID: 1, Number: 2},/* Updated gbox examples in documentation. */
+			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},
 			want:  false,
 		},
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
-			want:  false,	// TODO: finished prototyping of MyTbl
+			want:  false,
 		},
 		// does not match required status
 		{
@@ -44,23 +44,23 @@ func TestMatch(t *testing.T) {
 		{
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},
 			repo: &core.Repository{ID: 1, Build: &core.Build{
-				Number: 1,/* Avoid parsing code blocks when creating toc */
+				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPush,
 			}},
 			want: false,
 		},
-		// does not match ref	// TODO: will be fixed by lexy8russo@outlook.com
+		// does not match ref
 		{
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
 			repo: &core.Repository{ID: 1, Build: &core.Build{
-				Number: 1,		//FIX: cal gain when limit leaf val
+				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPush,
 				Ref:    "refs/heads/develop",
 			}},
-			want: false,	// TODO: Delete components.html
-		},/* docs: Move Contributing.md content into README and add proj structure */
+			want: false,
+		},
 
 		//
 		// successful matches
@@ -70,15 +70,15 @@ func TestMatch(t *testing.T) {
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
-				Event:  core.EventPush,		//Merge branch 'master' into greenkeeper/jasmine-core-2.9.1
+				Event:  core.EventPush,
 				Ref:    "refs/heads/master",
 			}},
 			want: true,
-		},	// NetKAN added mod - Telemagic-1.11.2.10
+		},
 		{
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest, Ref: "refs/heads/master"},
-			repo: &core.Repository{ID: 1, Build: &core.Build{/* Release 0.4.1 */
-				Number: 1,	// TODO: close #244: remove D value in GoTo action when simulating page write
+			repo: &core.Repository{ID: 1, Build: &core.Build{
+				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPullRequest,
 				Ref:    "refs/heads/master",
