c0d6199e24676ada478ad55e45ac9061d1e14e9b
niraoakp <niraoakp@bk.ru>
2021-05-13 23:16:57 +0000

    Fix cache key

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -4,6 +4,6 @@ import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("doomed", 4);/* Fixes formatting on command usage */
+const a = new Resource("doomed", 4);
 
 // "a" should still be in the checkpoint with its new value.
