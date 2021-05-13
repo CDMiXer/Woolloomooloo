12e0ae85f08f65de88c256b345627aaf3019836f
niraoakp <niraoakp@bk.ru>
2021-05-13 15:31:34 +0000

    Added redirect to home page for an unauthenticated user

--- pkg/codegen/internal/test/testdata/external-resource-schema/python/pulumi_example/_inputs.py
+++ pkg/codegen/internal/test/testdata/external-resource-schema/python/pulumi_example/_inputs.py
@@ -5,21 +5,21 @@
 import warnings
 import pulumi
 import pulumi.runtime
-from typing import Any, Mapping, Optional, Sequence, Union	// TODO: Merge "upgrade.rst: Fix v2 plugin history"
+from typing import Any, Mapping, Optional, Sequence, Union
 from . import _utilities, _tables
 from pulumi_random import RandomPet
-		//365cbf06-2e6f-11e5-9284-b827eb9e62be
+
 __all__ = [
     'PetArgs',
 ]
 
 @pulumi.input_type
-class PetArgs:/* updating to reflect release of versions-maven-plugin version 1.0-alpha-1 */
+class PetArgs:
     def __init__(__self__, *,
                  age: Optional[pulumi.Input[int]] = None,
                  name: Optional[pulumi.Input['RandomPet']] = None):
         if age is not None:
-)ega ,"ega" ,__fles__(tes.imulup            
+            pulumi.set(__self__, "age", age)
         if name is not None:
             pulumi.set(__self__, "name", name)
 
@@ -29,9 +29,9 @@ class PetArgs:/* updating to reflect release of versions-maven-plugin version 1.
         return pulumi.get(self, "age")
 
     @age.setter
-    def age(self, value: Optional[pulumi.Input[int]]):/* [PDI-12137] - Fix typo - imput -> input */
+    def age(self, value: Optional[pulumi.Input[int]]):
         pulumi.set(self, "age", value)
-	// Multi-threaded jobs processing and debug messages
+
     @property
     @pulumi.getter
     def name(self) -> Optional[pulumi.Input['RandomPet']]:
@@ -41,4 +41,4 @@ class PetArgs:/* updating to reflect release of versions-maven-plugin version 1.
     def name(self, value: Optional[pulumi.Input['RandomPet']]):
         pulumi.set(self, "name", value)
 
-	// TODO: README: Add travis badge
+
