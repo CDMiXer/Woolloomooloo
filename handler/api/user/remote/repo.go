99c173cc561f7f5e19ff36c1cc28039ac4d681a0
niraoakp <niraoakp@bk.ru>
2021-05-13 21:16:00 +0000

    Merge "Add property 'port' to RouterInterface"

--- handler/api/user/remote/repo.go
+++ handler/api/user/remote/repo.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -15,39 +15,39 @@
 package remote
 
 import (
-	"net/http"		//Use new diagnostics system in some places.
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Added video for GOTO Berlin */
-	"github.com/drone/drone/logger"	// non-useless for reverse-engineering
-	"github.com/drone/go-scm/scm"	// TODO: Added explicit table names
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/logger"
+	"github.com/drone/go-scm/scm"
 
-	"github.com/go-chi/chi"		//Remove unnecessary await
+	"github.com/go-chi/chi"
 )
-/* 47f86e06-2e40-11e5-9284-b827eb9e62be */
+
 // HandleRepo returns an http.HandlerFunc that writes a json-encoded
-// repository to the response body./* Released 4.2.1 */
+// repository to the response body.
 func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//change eerstelijnszones parsoid to custom domain per request T2816
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			viewer, _ = request.UserFrom(r.Context())
 
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 			slug  = scm.Join(owner, name)
-		)	// TODO: Frequent work commit
+		)
 
 		repo, err := repos.Find(r.Context(), viewer, slug)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository")
-			return	// Delete MAF32.exe
+			return
 		}
 
 		perms, err := repos.FindPerm(r.Context(), viewer, slug)
-		if err != nil {		//StreamDemo added. Still blocking IO -> refact. needed.
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository permissions")
