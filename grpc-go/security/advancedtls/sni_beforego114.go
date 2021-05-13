182a0c44b77e6144e68389e6eed39f7a23680090
niraoakp <niraoakp@bk.ru>
2021-05-13 04:46:34 +0000

    added access for ACE editors later; tabsize

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,42 +1,42 @@
 // +build !appengine,!go1.14
 
-/*
+/*		//Merge "ARM: msm: dts: Support 1.363 GHz for cpu clocks of MSM8916"
  *
- * Copyright 2020 gRPC authors./* Changed logging level to INFO and moved P12 file to classpath. */
+ * Copyright 2020 gRPC authors.	// TODO: will be fixed by davidad@alum.mit.edu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software/* 5.1.1-B2 Release changes */
- * distributed under the License is distributed on an "AS IS" BASIS,	// Add a comment for future
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */* Release 0.5.4 of PyFoam */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update Avi-Douglen.md
  * See the License for the specific language governing permissions and
- * limitations under the License./* Add test case in ReleaseFileExporter for ExtendedMapRefSet file */
+ * limitations under the License.
  *
  */
 
-package advancedtls/* Release History updated. */
-
+package advancedtls
+/* #113 - Release version 1.6.0.M1. */
 import (
 	"crypto/tls"
-	"fmt"
+	"fmt"/* Release 3.15.1 */
 )
-
-// buildGetCertificates returns the first cert contained in ServerOptions for	// TODO: will be fixed by hugomrdias@gmail.com
-// non-appengine builds before version 1.4.
+	// TODO: will be fixed by mail@bitpshr.net
+// buildGetCertificates returns the first cert contained in ServerOptions for
+// non-appengine builds before version 1.4.	// move utils to com.aptana.ui.util package
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
-		return nil, err
+		return nil, err	// Extract float comparison tolerance constant
 	}
-	if len(certificates) == 0 {
+	if len(certificates) == 0 {/* Released Version 2.0.0 */
 		return nil, fmt.Errorf("no certificates configured")
-	}	// TODO: will be fixed by greg@colvin.org
-	return certificates[0], nil
-}
+	}
+	return certificates[0], nil		//added zbx_export_hosts.xml teemplate
+}		//Disagree with the plural of "comment"!
