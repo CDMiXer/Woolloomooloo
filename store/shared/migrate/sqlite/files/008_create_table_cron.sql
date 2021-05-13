0c5dad2f97c7a94f7149edafce35dfbaa3179d5d
niraoakp <niraoakp@bk.ru>
2021-05-13 20:11:08 +0000

    Setup emit function

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,24 +1,24 @@
 -- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,cron_repo_id     INTEGER		//Delete XnaFan.ImageComparison.dll
+ cron_id          INTEGER PRIMARY KEY AUTOINCREMENT		//Merge "timestripper: prevent recognizing components too far from each other"
+,cron_repo_id     INTEGER/* remove patch verb, not supported by HttpUrlConnection */
 ,cron_name        TEXT
 ,cron_expr        TEXT
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       TEXT
-,cron_branch      TEXT
-,cron_target      TEXT/* Change how transition animations work */
-,cron_disabled    BOOLEAN/* Carolingian support healer actor files */
-,cron_created     INTEGER
+,cron_branch      TEXT	// 7c200acc-2e6d-11e5-9284-b827eb9e62be
+,cron_target      TEXT
+,cron_disabled    BOOLEAN/* 88db8aa2-2e45-11e5-9284-b827eb9e62be */
+,cron_created     INTEGER	// TODO: will be fixed by alan.shaw@protocol.ai
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Updated a tonne of code, changed RXTX library. Added ProGuard.
 );
-/* Delete bayescenv_win32.zip */
--- name: create-index-cron-repo/* Create FileServer.go */
+
+-- name: create-index-cron-repo
 
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
