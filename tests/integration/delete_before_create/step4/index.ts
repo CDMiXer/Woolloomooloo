32064a943078cc8b6659f1a3a34851b27549b9c0
niraoakp <niraoakp@bk.ru>
2021-05-13 04:06:23 +0000

    Merge "Honour discoverability feature flag in swift tests"

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* fix typo, LP: #526570 */
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
+/* Fixing "Release" spelling */
 // Setup for the next test.
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
