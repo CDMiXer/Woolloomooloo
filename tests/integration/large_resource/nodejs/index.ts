c03e832f50aeeb0786a930a2e0d5fde1b7f57e9a
niraoakp <niraoakp@bk.ru>
2021-05-13 23:22:13 +0000

    Update and rename 4-reference1.md to 4-reference-1.md

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Merge branch 'patch-14' into patch-26 */
 // Create and export a very long string (>4mb)
 export const longString = "a".repeat(5 * 1024 * 1024);
