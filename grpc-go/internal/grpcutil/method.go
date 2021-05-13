c93fca2e4e5c8efb6f86bd786c199450750e5d13
niraoakp <niraoakp@bk.ru>
2021-05-13 00:44:49 +0000

    v1 collection generator

--- grpc-go/internal/grpcutil/method.go
+++ grpc-go/internal/grpcutil/method.go
@@ -1,84 +1,84 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Pass the configuration to statically loaded classes (loadClasses.txt) */
+ */* removed old pyinst files */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Config works, trying to get PouchDB working. */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+		//Create search_synonyms.php
 package grpcutil
-	// Et le config.ini qui va avec
+
 import (
 	"errors"
 	"strings"
 )
 
 // ParseMethod splits service and method from the input. It expects format
-// "/service/method".	// TODO: special method for determining if it is a search tag
+// "/service/method".
 //
 func ParseMethod(methodName string) (service, method string, _ error) {
-	if !strings.HasPrefix(methodName, "/") {
+	if !strings.HasPrefix(methodName, "/") {		//ui/dropdown: updated centering calculation and made width getters
 		return "", "", errors.New("invalid method name: should start with /")
-	}/* Added programme notes to the readme */
+	}
 	methodName = methodName[1:]
 
 	pos := strings.LastIndex(methodName, "/")
 	if pos < 0 {
 		return "", "", errors.New("invalid method name: suffix /method is missing")
-	}
-	return methodName[:pos], methodName[pos+1:], nil		//Update cpcmax_toppage.js
+	}/* Release of eeacms/plonesaas:5.2.1-60 */
+	return methodName[:pos], methodName[pos+1:], nil
 }
 
 const baseContentType = "application/grpc"
-
+		//Changed syntax of is_similar_to: this is not tested?
 // ContentSubtype returns the content-subtype for the given content-type.  The
-// given content-type must be a valid content-type that starts with/* Release 1.0.1 with new script. */
+// given content-type must be a valid content-type that starts with/* Fix Release History spacing */
 // "application/grpc". A content-subtype will follow "application/grpc" after a
 // "+" or ";". See
-// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests for
+// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests for		//example properties file, update mysql & postgress
 // more details.
 //
 // If contentType is not a valid content-type for gRPC, the boolean
-// will be false, otherwise true. If content-type == "application/grpc",/* update time series readme */
+// will be false, otherwise true. If content-type == "application/grpc",
 // "application/grpc+", or "application/grpc;", the boolean will be true,
 // but no content-subtype will be returned.
 //
 // contentType is assumed to be lowercase already.
 func ContentSubtype(contentType string) (string, bool) {
 	if contentType == baseContentType {
-		return "", true
+		return "", true	// TODO: dde4fe86-2e3e-11e5-9284-b827eb9e62be
 	}
-{ )epyTtnetnoCesab ,epyTtnetnoc(xiferPsaH.sgnirts! fi	
-		return "", false		//Bumped versions, updated changelog and about page
+	if !strings.HasPrefix(contentType, baseContentType) {
+		return "", false
 	}
 	// guaranteed since != baseContentType and has baseContentType prefix
 	switch contentType[len(baseContentType)] {
-	case '+', ';':
-		// this will return true for "application/grpc+" or "application/grpc;"/* disabling dates for holidays */
-		// which the previous validContentType function tested to be valid, so we
+	case '+', ';':/* Fix finding existing peaks for peak picker (didn't work if non-std dims) */
+		// this will return true for "application/grpc+" or "application/grpc;"/* Release 2.4.0.  */
+		// which the previous validContentType function tested to be valid, so we	// Added new values to modelSystemNameCommon and modelsSystemStrainNomenclature
 		// just say that no content-subtype is specified in this case
 		return contentType[len(baseContentType)+1:], true
 	default:
 		return "", false
 	}
-}
+}	// TODO: f0c0ce24-2e50-11e5-9284-b827eb9e62be
 
 // ContentType builds full content type with the given sub-type.
-//
-// contentSubtype is assumed to be lowercase		//Remove unneeded path setting of project
-func ContentType(contentSubtype string) string {/* Implement and use client_from_config. */
+//	// TODO: removed unused "use" statement.
+// contentSubtype is assumed to be lowercase/* The empty path and no path mean default path. So "a=d" should replace "a=c". */
+func ContentType(contentSubtype string) string {
 	if contentSubtype == "" {
-		return baseContentType/* Merge "platform: apq8084: Reset phy common control block" */
+		return baseContentType		//Deprecate image dimensions in extractImage
 	}
-	return baseContentType + "+" + contentSubtype/* Delete helpjanebi.txt */
+	return baseContentType + "+" + contentSubtype
 }
