dfa858935d393d7bd076770540676a2b59fc2ad7
niraoakp <niraoakp@bk.ru>
2021-05-13 20:48:55 +0000

    attempting to establish an interface

--- handler/api/repos/builds/logs/find_test.go
+++ handler/api/repos/builds/logs/find_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Add a roundtrip test with nastily formatted but valid Python code */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Add debug build target for eclipse.
 
 package logs
