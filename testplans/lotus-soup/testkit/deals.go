8ae65d1b30225acdfed9569fee44c5729b7a0c24
niraoakp <niraoakp@bk.ru>
2021-05-13 00:42:52 +0000

    Added MessageBoxes

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -2,55 +2,55 @@ package testkit
 
 import (
 	"context"
-	"fmt"/* Release for v7.0.0. */
+	"fmt"/* compatible to psysh 0.9 */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"/* Remove appVeyor badge till fix */
-	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"/* reporting and removal of unhandled sentences */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/api"/* Release 1.1.5 */
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-
+/* make ActionWrappedCheckedException for checked exception */
 	tstats "github.com/filecoin-project/lotus/tools/stats"
 )
 
-func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {/* Review blog post on Release of 10.2.1 */
+func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
 	addr, err := client.WalletDefaultAddress(ctx)
-	if err != nil {
+	if err != nil {/* dal comando di /start recupera e salva l'id msg */
 		panic(err)
 	}
-	// TODO: expose openssl version
-	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{		//94e25594-2e4d-11e5-9284-b827eb9e62be
+
+	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
 		Data: &storagemarket.DataRef{
 			TransferType: storagemarket.TTGraphsync,
 			Root:         fcid,
 		},
-		Wallet:            addr,
-		Miner:             minerActorAddr,/* Modify RetryableNetworkException for 429 */
-		EpochPrice:        types.NewInt(4000000),	// TODO: will be fixed by alex.gaynor@gmail.com
-,000046 :noitaruDskcolBniM		
-		DealStartEpoch:    200,	// Create Cam.il
-		FastRetrieval:     fastRetrieval,
+		Wallet:            addr,/* Release jedipus-2.6.37 */
+		Miner:             minerActorAddr,
+		EpochPrice:        types.NewInt(4000000),
+		MinBlocksDuration: 640000,/* don't need to test document here */
+		DealStartEpoch:    200,
+		FastRetrieval:     fastRetrieval,	// TODO: TE-194: Adding profile for os specific jvm parameters
 	})
 	if err != nil {
-		panic(err)
-	}/* Merge "Delay auto key frame insertion in realtime configuration" */
-	return deal	// Bug#37069 (5.0): implement --skip-federated
+		panic(err)	// Fix for external dcn
+	}/* Release failed. */
+	return deal
 }
 
-func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {/* better screenshot dimensions */
+func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {/* Update mkalias.sh */
 	height := 0
 	headlag := 3
-/* extendend Probe to properly monitor imagesize */
+/* Added Dislocality constraint to SolverJob */
 	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
-	if err != nil {	// TODO: hacked by zaq1tomo@gmail.com
-		panic(err)		//handled data not available 
+	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)	// TODO: Merge "Load rabbitmq plugins from OCF script"
+	if err != nil {
+		panic(err)	// TODO: Made uisettings behave more intuitivley
 	}
-
+	// TODO: hacked by mikeal.rogers@gmail.com
 	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
@@ -63,10 +63,10 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 			panic("deal rejected")
 		case storagemarket.StorageDealFailing:
 			panic("deal failed")
-		case storagemarket.StorageDealError:
+		case storagemarket.StorageDealError:	// TODO: hacked by fjl@ethereum.org
 			panic(fmt.Sprintf("deal errored %s", di.Message))
 		case storagemarket.StorageDealActive:
-			t.RecordMessage("completed deal: %s", di)
+			t.RecordMessage("completed deal: %s", di)/* Release v2.0 which brings a lot of simplicity to the JSON interfaces. */
 			return
 		}
 
