6deafeb932389ac16e67ebd68ead4cab0eb1aa78
niraoakp <niraoakp@bk.ru>
2021-05-13 18:31:32 +0000

    Merge remote-tracking branch 'michalmac/dvrp' into michalmac_master

--- handler/api/users/create_test.go
+++ handler/api/users/create_test.go
@@ -1,58 +1,58 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Log4J version update
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 1. Alguns ajustes e formatação no destrutor da classe ResourceManager; */
+// that can be found in the LICENSE file.
 
-package users
-	// TODO: will be fixed by vyzo@hackzen.org
+package users		//Automatic changelog generation for PR #51029 [ci skip]
+
 import (
 	"bytes"
-	"context"/* Official Release Archives */
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// Added mention of daemon.get_method_list
+	"github.com/drone/drone/handler/api/errors"/* #198 fixed */
+	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Add more screenshots.
+	"github.com/google/go-cmp/cmp"	// TODO: Paint tiles with empire color, first step of #28.
 )
 
-func TestCreate(t *testing.T) {
-	controller := gomock.NewController(t)/* Add a loading indicator, and muck with styles a little. */
+func TestCreate(t *testing.T) {/* Update ReleaseManual.md */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Worth noting request :json so that _post and _put work.
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
+	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {/* Improve `Release History` formating */
 		if got, want := in.Login, "octocat"; got != want {
 			t.Errorf("Want user login %s, got %s", want, got)
 		}
-		if in.Hash == "" {
+		if in.Hash == "" {/* Initial commit for travis support */
 			t.Errorf("Expect user secert generated")
 		}
 		return nil
 	})
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	service := mock.NewMockUserService(controller)
-	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "octocat").Return(nil, errors.New("not found"))/* Release for 23.5.1 */
-		//Updated matrix table
+	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "octocat").Return(nil, errors.New("not found"))
+
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.User{Login: "octocat"})
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", in)
-/* Release of eeacms/eprtr-frontend:0.3-beta.22 */
-	HandleCreate(users, service, webhook)(w, r)
+
+	HandleCreate(users, service, webhook)(w, r)/* Release Notes: 3.3 updates */
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	out := new(core.User)
-	json.NewDecoder(w.Body).Decode(out)/* room_member: fix 3 typos */
+	out := new(core.User)/* added link to my stackoverflow page */
+	json.NewDecoder(w.Body).Decode(out)
 	if got, want := out.Login, "octocat"; got != want {
 		t.Errorf("Want user login %s, got %s", want, got)
 	}
@@ -61,28 +61,28 @@ func TestCreate(t *testing.T) {
 	}
 	if got := out.Created; got == 0 {
 		t.Errorf("Want user created set to current unix timestamp, got %v", got)
-	}
-	if got := out.Updated; got == 0 {/* Release notes 8.0.3 */
-		t.Errorf("Want user updated set to current unix timestamp, got %v", got)
+	}/* Merge "bug 1128:POM Restructuring for Automated Release" */
+	if got := out.Updated; got == 0 {/* Release v1.6 */
+		t.Errorf("Want user updated set to current unix timestamp, got %v", got)/* Release new version 2.5.27: Fix some websites broken by injecting a <link> tag */
 	}
 }
 
-func TestCreate_CorrectName(t *testing.T) {
-	controller := gomock.NewController(t)
+func TestCreate_CorrectName(t *testing.T) {	// trim more nob.dix all cats at +99%
+	controller := gomock.NewController(t)/* Release as "GOV.UK Design System CI" */
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)/* Update axis-1.tcl */
+	users := mock.NewMockUserStore(controller)/* small fix to export plugin */
 	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
 		if got, want := in.Login, "octocat"; got != want {
 			t.Errorf("Want user login %s, got %s", want, got)
 		}
 		if got, want := in.Email, "octocat@github.com"; got != want {
-			t.Errorf("Want user email %s, got %s", want, got)		//Alteração botão remover
-		}/* Release v6.4 */
+			t.Errorf("Want user email %s, got %s", want, got)
+		}
 		if in.Hash == "" {
 			t.Errorf("Expect user secert generated")
 		}
-		return nil	// Create jQueryUIToAF
+		return nil
 	})
 
 	webhook := mock.NewMockWebhookSender(controller)
