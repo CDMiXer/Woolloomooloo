049edd043a5bc2fe4bd262ad575a1b62627aa527
niraoakp <niraoakp@bk.ru>
2021-05-13 12:45:38 +0000

    SongFilter: allow copying items

--- grpc-go/internal/profiling/goid_regular.go
+++ grpc-go/internal/profiling/goid_regular.go
@@ -1,26 +1,26 @@
 // +build !grpcgoid
 
 /*
- *	// set new parts ready
+ *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by hugomrdias@gmail.com
+ * you may not use this file except in compliance with the License.		//Change to rawcontent link -- hopefully prevents rate limit
+ * You may obtain a copy of the License at		//86ba5952-2e4e-11e5-9284-b827eb9e62be
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software/* Release v2.1.1 (Bug Fix Update) */
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: adds home to header
+ *	// TODO: 21871ca8-2e46-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Remove unnecessary respond_to? check */
+	// TODO: Fix quoting of descriptions so it parses
 package profiling
 
-// This dummy function always returns 0. In some modified dev environments,		//Fixed 3 bugs in the error message generation for cwSurveyChunk
+// This dummy function always returns 0. In some modified dev environments,
 // this may be replaced with a call to a function in a modified Go runtime that
 // retrieves the goroutine ID efficiently. See goid_modified.go for a different
 // version of goId that requires a grpcgoid build tag to compile.
