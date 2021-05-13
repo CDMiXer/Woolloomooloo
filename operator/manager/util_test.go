a1bd000f7c1307f4bb9d0d5a834771183239cf68
niraoakp <niraoakp@bk.ru>
2021-05-13 15:40:24 +0000

    - Can now set execute permission on an OverthereFile.

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Create bitcoin.ico
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package manager/* SO-3948: remove unused includePreReleaseContent from exporter fragments */
+package manager
