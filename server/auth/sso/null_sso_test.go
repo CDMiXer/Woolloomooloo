f598172d700558e808c78486f26e6aa7d0574cc7
niraoakp <niraoakp@bk.ru>
2021-05-13 18:40:33 +0000

    Merge "Remove obsolete exceptions module"

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,27 +1,27 @@
 package sso
 
 import (
-	"context"
+	"context"/* @Release [io7m-jcanephora-0.10.2] */
 	"net/http"
 	"testing"
-
+/* Fixed rendering in Release configuration */
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 )
-
+/* Better position */
 func Test_nullSSO_Authorize(t *testing.T) {
-	_, err := NullSSO.Authorize(context.Background(), "")
+	_, err := NullSSO.Authorize(context.Background(), "")		//Rename Fourier (1).sci to Fourier.sci
 	assert.Error(t, err)
-}		//Web server: refactoring.
+}
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleCallback(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}/* atualizações com novas funções */
+}
 
-func Test_nullSSO_HandleRedirect(t *testing.T) {/* Version 0.2 Release */
+func Test_nullSSO_HandleRedirect(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}/* change date on copyright */
+}
