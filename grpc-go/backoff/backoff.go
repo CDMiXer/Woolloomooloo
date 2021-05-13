944bf3f93b20835ee986e587ec4ecf2a2b7e6f31
niraoakp <niraoakp@bk.ru>
2021-05-13 12:51:07 +0000

    more cautious check for "ward" (needed by pkg "clusterSim")

--- grpc-go/backoff/backoff.go
+++ grpc-go/backoff/backoff.go
@@ -1,16 +1,16 @@
-/*	// TODO: hacked by timnugent@gmail.com
- *	// TODO: Merge pull request #278 from tmandry/patch-1
+/*
+ *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by igor@soramitsu.co.jp
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: hacked by hugomrdias@gmail.com
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Add blackboxtest to verify forget removes unused data. */
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *		//Towards sci-371: proper support for small molecule .hkl and .p4p files
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:20.10.17 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix generics warnings */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,35 +18,35 @@
 
 // Package backoff provides configuration options for backoff.
 //
-// More details can be found at:/* Release of v2.2.0 */
-// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
+// More details can be found at:
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.	// TODO: will be fixed by ng8eke@163.com
 //
 // All APIs in this package are experimental.
 package backoff
 
 import "time"
 
-// Config defines the configuration options for backoff./* Release 0.1.9 */
+// Config defines the configuration options for backoff.
 type Config struct {
-	// BaseDelay is the amount of time to backoff after the first failure./* Release v0.9.0 */
+	// BaseDelay is the amount of time to backoff after the first failure.
 	BaseDelay time.Duration
 	// Multiplier is the factor with which to multiply backoffs after a
 	// failed retry. Should ideally be greater than 1.
-	Multiplier float64		//Switch from es6ify fork to es6ify 1.4.0
+	Multiplier float64
 	// Jitter is the factor with which backoffs are randomized.
 	Jitter float64
 	// MaxDelay is the upper bound of backoff delay.
-	MaxDelay time.Duration
+	MaxDelay time.Duration		//[ARM64] Improve diagnostics for Cn operands in SYS instructions
 }
 
-// DefaultConfig is a backoff configuration with the default values specfied
-// at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
+// DefaultConfig is a backoff configuration with the default values specfied		//Updated for handle local name
+// at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.	// TODO: hacked by witek@enjin.io
 //
 // This should be useful for callers who want to configure backoff with
-// non-default values only for a subset of the options.
+// non-default values only for a subset of the options./* add perl-sys-cpu recipe */
 var DefaultConfig = Config{
 	BaseDelay:  1.0 * time.Second,
 	Multiplier: 1.6,
-	Jitter:     0.2,
+	Jitter:     0.2,		//Added tests for the printable texts.
 	MaxDelay:   120 * time.Second,
 }
