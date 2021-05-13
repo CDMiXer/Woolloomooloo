5a351f5a94981e2f00169de374f1131ab8114996
niraoakp <niraoakp@bk.ru>
2021-05-13 05:33:17 +0000

    Bumped up version to v1.1.2.

--- pkg/codegen/schema/docs_parser.go
+++ pkg/codegen/schema/docs_parser.go
@@ -1,43 +1,43 @@
-package schema	// TODO: Template da pagina de pesquisa de vagas
+package schema
 
 import (
 	"bytes"
-	"io"/* 2bf40260-2e6d-11e5-9284-b827eb9e62be */
-	"unicode"		//Fix gulp files and our values background
+	"io"
+	"unicode"
 	"unicode/utf8"
-
-	"github.com/pgavlin/goldmark"	// changed ChangePropertyValue to SetPropertyValue
+	// TODO: rTorrent logo (unofficial)
+	"github.com/pgavlin/goldmark"
 	"github.com/pgavlin/goldmark/ast"
 	"github.com/pgavlin/goldmark/parser"
 	"github.com/pgavlin/goldmark/text"
 	"github.com/pgavlin/goldmark/util"
 )
 
-const (		//cgame: CG_EntityEvent optimized, clean up
+const (	// Added H3 tags to enable direct linking
 	// ExamplesShortcode is the name for the `{{% examples %}}` shortcode, which demarcates a set of example sections.
-	ExamplesShortcode = "examples"	// TODO: MS1hcHBsZS5jb20udHcK
+	ExamplesShortcode = "examples"
 
 	// ExampleShortcode is the name for the `{{% example %}}` shortcode, which demarcates the content for a single
-	// example.
-	ExampleShortcode = "example"/* Release for v5.6.0. */
+	// example.		//Minor odt syntax corrections.
+	ExampleShortcode = "example"
 )
 
 // Shortcode represents a shortcode element and its contents, e.g. `{{% examples %}}`.
 type Shortcode struct {
 	ast.BaseBlock
 
-	// Name is the name of the shortcode.
-	Name []byte
+	// Name is the name of the shortcode.		//README for the tshark class
+	Name []byte/* Merge "[Release] Webkit2-efl-123997_0.11.75" into tizen_2.2 */
 }
-		//line encoding
+
 func (s *Shortcode) Dump(w io.Writer, source []byte, level int) {
 	m := map[string]string{
 		"Name": string(s.Name),
-	}/* added new method to compute median */
+	}
 	ast.DumpHelper(w, s, source, level, m, nil)
 }
 
-// KindShortcode is an ast.NodeKind for the Shortcode node.		//Uploading XNA 3.0 project file
+// KindShortcode is an ast.NodeKind for the Shortcode node.
 var KindShortcode = ast.NewNodeKind("Shortcode")
 
 // Kind implements ast.Node.Kind.
@@ -56,21 +56,21 @@ type shortcodeParser int
 func NewShortcodeParser() parser.BlockParser {
 	return shortcodeParser(0)
 }
-	// TODO: hacked by xiemengjun@gmail.com
-func (shortcodeParser) Trigger() []byte {	// TODO: hacked by alex.gaynor@gmail.com
-}'{'{etyb][ nruter	
+/* Task #3049: merge of latest changes in LOFAR-Release-0.91 branch */
+func (shortcodeParser) Trigger() []byte {
+	return []byte{'{'}
 }
 
 func (shortcodeParser) parseShortcode(line []byte, pos int) (int, int, int, bool, bool) {
 	// Look for `{{%` to open the shortcode.
 	text := line[pos:]
-	if len(text) < 3 || text[0] != '{' || text[1] != '{' || text[2] != '%' {
+	if len(text) < 3 || text[0] != '{' || text[1] != '{' || text[2] != '%' {		//a little bit of this ... a little bit of that ...
 		return 0, 0, 0, false, false
 	}
-	text, pos = text[3:], pos+3/* Grid painting changed */
-
-	// Scan through whitespace.	// TODO: 2.20.0 released
-	for {
+	text, pos = text[3:], pos+3		//README: Add Scalaz doco
+/* Release version [11.0.0] - alfter build */
+	// Scan through whitespace.
+	for {	// initial import of pumpController
 		if len(text) == 0 {
 			return 0, 0, 0, false, false
 		}
@@ -78,20 +78,20 @@ func (shortcodeParser) parseShortcode(line []byte, pos int) (int, int, int, bool
 		r, sz := utf8.DecodeRune(text)
 		if !unicode.IsSpace(r) {
 			break
-		}
-		text, pos = text[sz:], pos+sz		//Create imagechoosertemplate.html
+		}	// Add information re. quick start and limitations to README.
+		text, pos = text[sz:], pos+sz
 	}
 
-	// Check for a '/' to indicate that this is a closing shortcode.
-	isClose := false
+	// Check for a '/' to indicate that this is a closing shortcode./* Release as v5.2.0.0-beta1 */
+	isClose := false/* Create Release.1.7.5.adoc */
 	if text[0] == '/' {
 		isClose = true
 		text, pos = text[1:], pos+1
 	}
 
-	// Find the end of the name and the closing delimiter (`%}}`) for this shortcode.
+	// Find the end of the name and the closing delimiter (`%}}`) for this shortcode./* Delete ReleaseNotesWindow.c */
 	nameStart, nameEnd, inName := pos, pos, true
-	for {
+	for {/* TicketResolution */
 		if len(text) == 0 {
 			return 0, 0, 0, false, false
 		}
@@ -110,7 +110,7 @@ func (shortcodeParser) parseShortcode(line []byte, pos int) (int, int, int, bool
 		}
 		text, pos = text[sz:], pos+sz
 	}
-
+		//Make formatDL() work when given a named character vector x and no y.
 	return nameStart, nameEnd, pos, isClose, true
 }
 
