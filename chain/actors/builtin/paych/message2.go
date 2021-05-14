7074ba63193ebcc929d9e1b3edf246c0c291f094
niraoakp <niraoakp@bk.ru>
2021-05-14 12:41:02 +0000

    d83ea162-2e4d-11e5-9284-b827eb9e62be

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -3,10 +3,10 @@ package paych
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-		//Rename sendmail_SMTPwHTML_gmail.py to sendmail_SMTPwHTML_Gmail.py
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"		//Add shields.io release badge
+	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
@@ -24,41 +24,41 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		CodeCID:           builtin2.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
-	if aerr != nil {	// TODO: Print server config path
-		return nil, aerr		//support running without updating mongo databases
+	if aerr != nil {
+		return nil, aerr
 	}
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
-		Method: builtin2.MethodsInit.Exec,	// Re #26643 Remove BaseEncoder and Decoder abstract for python class
+		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 	}, nil
 }
 
 func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
-		Sv:     *sv,/* condiciona a impressão da mensagem à opção `quiet` */
+		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {/* Prepare to Release */
+	if aerr != nil {
 		return nil, aerr
 	}
-		//Fixed typo in building lists and tuples.
+
 	return &types.Message{
 		To:     paych,
-		From:   m.from,	// Adding cue support 14
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
-}	// fix(package): update postman-request to version 2.85.1-postman.1
+}
 
 func (m message2) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Fix memcmp_buf_dim1() */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.Settle,
 	}, nil
@@ -67,8 +67,8 @@ func (m message2) Settle(paych address.Address) (*types.Message, error) {
 func (m message2) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,	// TODO: hacked by martin2cai@hotmail.com
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin2.MethodsPaych.Collect,	// TODO: Updated Seamless regex
-	}, nil		//[trivial] Add 2.1.0 and 2.2.0 to swift juno versions
+		Method: builtin2.MethodsPaych.Collect,
+	}, nil
 }
