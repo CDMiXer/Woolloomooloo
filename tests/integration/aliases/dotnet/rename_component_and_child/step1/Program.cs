94ab192d6c04d67bbb0da3b3347811aed87b435e
niraoakp <niraoakp@bk.ru>
2021-05-14 00:46:37 +0000

    catbox wiki changes

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -7,29 +7,29 @@ class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {		//Interpretador v1.0
-    }	// TODO: Fix OSGI-related tests
+    {
+    }/* Add publish to git. Release 0.9.1. */
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive : ComponentResource		//e774b16c-2e66-11e5-9284-b827eb9e62be
+class ComponentFive : ComponentResource
 {
     private Resource resource;
-/* 7c64806c-2e4a-11e5-9284-b827eb9e62be */
-    public ComponentFive(string name, ComponentResourceOptions options = null)
+
+    public ComponentFive(string name, ComponentResourceOptions options = null)/* Delete Login.cs */
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-/* - fixed Release_Win32 build path in xalutil */
+
 class Program
 {
     static Task<int> Main(string[] args)
-{    
+    {
         return Deployment.RunAsync(() => 
         {
             var comp5 = new ComponentFive("comp5");
-        });
+        });/* Delete 150_A1_Brechbuehler_Bieri_Eggmann_Menzel.pptx */
     }
 }
