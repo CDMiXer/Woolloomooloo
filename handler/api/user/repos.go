d8e6b28e6eb29e50dbeeb154e20f108d0781eb8e
niraoakp <niraoakp@bk.ru>
2021-05-14 12:05:12 +0000

    #714: MapTileRastered can set custom raster line number.

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone IO, Inc.		//fix link target to self
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc.
+//	// TODO: remove randDouble test case
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update README.md / add badges
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge "Wlan: Release 3.8.20.7" */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// blogpage requires PollNY
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 852d737a-2e4a-11e5-9284-b827eb9e62be */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* (MESS) ibm5170: Keyboard WIP. (nw) */
 
-package user
+package user/* Release 2.4.0 (close #7) */
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"	// TODO: Update Setting up development workspace.md
-	"github.com/drone/drone/logger"	// adding set project definition
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/logger"/* Rename git.hs to git.sh */
 )
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
@@ -33,15 +33,15 @@ func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
 		var err error
 		if r.FormValue("latest") != "true" {
 			list, err = repos.List(r.Context(), viewer.ID)
-		} else {	// TODO: refactoring pager BILLRUN-38
+		} else {	// Just regularize the naming of some palette colors
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
-		}/* Better Release notes. */
-		if err != nil {
+		}/* Fix missing newline in permission explanation */
+		if err != nil {/* Release 4.2.2 */
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list repositories")
-		} else {
+		} else {/* Release ChildExecutor after the channel was closed. See #173 */
 			render.JSON(w, list, 200)
-		}
+		}/* Change the arena in the FactoryStrategy to a ref to an ArenaInterface. */
 	}
 }
