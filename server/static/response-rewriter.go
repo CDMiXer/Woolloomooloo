49b39b7c6a66deaf06dd88f38c5dddf3a8dfcafe
niraoakp <niraoakp@bk.ru>
2021-05-14 04:21:29 +0000

    fix require statement

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,20 +1,20 @@
-package static/* c64ebc86-2e47-11e5-9284-b827eb9e62be */
-	// TODO: Just checking the change
-import (/* Release of eeacms/eprtr-frontend:0.4-beta.18 */
+package static
+
+import (
 	"bytes"
 	"net/http"
 	"strconv"
 )
-	// TODO: will be fixed by mail@bitpshr.net
+
 type responseRewriter struct {
-	http.ResponseWriter/* Release version 1.2.0.RC2 */
+	http.ResponseWriter
 	old []byte
 	new []byte
 }
 
-func (w *responseRewriter) Write(a []byte) (int, error) {	// TODO: tried to fiy an issue with the composite pattern
+func (w *responseRewriter) Write(a []byte) (int, error) {
 	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
-	return w.ResponseWriter.Write(b)	// TODO: Добавлена хронология версий
+	return w.ResponseWriter.Write(b)
 }
