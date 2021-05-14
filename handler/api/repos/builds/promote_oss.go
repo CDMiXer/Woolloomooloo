1630657acf495c914596da662d3066108dbf8394
niraoakp <niraoakp@bk.ru>
2021-05-14 04:16:00 +0000

    Delete splithalf_script.m

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -1,37 +1,37 @@
-// Copyright 2019 Drone IO, Inc./* Release of eeacms/bise-backend:v10.0.28 */
-//	// TODO: Don’t add a default mode of '0' to search params
+// Copyright 2019 Drone IO, Inc./* Merge branch 'develop' into FOGL-1797 */
+///* Release of eeacms/clms-frontend:1.0.5 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Released LockOMotion v0.1.1 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Deleted CtrlApp_2.0.5/Release/CtrlApp.pch */
 // +build oss
 
 package builds
-
+/* Release 0.3 version */
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release 1.3.1. */
+	"github.com/drone/drone/core"/* Reformat: firmware-misc-nonfree : Breaks: firmware-ralink */
+	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by brosner@gmail.com
 	render.NotImplemented(w, render.ErrNotImplemented)
-}
+}	// Merge "Adjust RESTAPIs convert-config w/suggests from SL"
 
-// HandlePromote returns a non-op http.HandlerFunc.	// TODO: hacked by nagydani@epointsystem.org
+// HandlePromote returns a non-op http.HandlerFunc.
 func HandlePromote(
 	core.RepositoryStore,
 	core.BuildStore,
 	core.Triggerer,
 ) http.HandlerFunc {
-	return notImplemented
+	return notImplemented/* Update minikeypad.xml */
 }
