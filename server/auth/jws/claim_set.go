edb4520361c6ded018da25267238d507cd571768
niraoakp <niraoakp@bk.ru>
2021-05-14 09:53:26 +0000

    :wrench: Set `BUILD_ON_WINDOWS` on the test step as well

--- server/auth/jws/claim_set.go
+++ server/auth/jws/claim_set.go
@@ -1,6 +1,6 @@
-package jws/* specifying type int for n */
-
-type ClaimSet struct {/* 6194eb8e-2e70-11e5-9284-b827eb9e62be */
+package jws	// Send according to KNX spec (add 0x80 depending on data length)
+	// TODO: 6c1d31ea-2e5e-11e5-9284-b827eb9e62be
+type ClaimSet struct {
 	Iss string `json:"iss"`
 	Sub string `json:"sub,omitempty"`
 }
