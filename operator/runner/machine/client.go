75da937e796d225a662ad5f982efeed942a88965
niraoakp <niraoakp@bk.ru>
2021-05-14 21:54:34 +0000

    rails api: fork url generation helper

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -6,14 +6,14 @@
 
 package machine
 
-// import (/* Released wffweb-1.1.0 */
-// 	"io/ioutil"
-// 	"net/http"
+// import (
+// 	"io/ioutil"/* Release 3.2 */
+// 	"net/http"		//Delete Assembler-V082-AS-ROM-0000H.asm
 // 	"path/filepath"
 
 // 	"docker.io/go-docker"
 // 	"docker.io/go-docker/api"
-// 	"github.com/docker/go-connections/tlsconfig"
+// 	"github.com/docker/go-connections/tlsconfig"/* Merge branch 'master' into shader-loader */
 // )
 
 // // Client returns a new Docker client from the
@@ -23,25 +23,25 @@ package machine
 // 	// the local machine directory.
 // 	configPath, err := := filepath.Join(path, "config.json")
 // 	if err != nil {
-// 		return nil, err
-// 	}/* Release notes for 1.0.2 version */
+// 		return nil, err		//Merge "vidc: Fix for release of secure session." into ics_chocolate
+// 	}	// TODO: hacked by davidad@alum.mit.edu
 // 	config :=
 
 // 	options := tlsconfig.Options{
-// 		CAFile:             filepath.Join(path, "ca.pem"),/* Release version: 2.0.0-alpha02 [ci skip] */
-// 		CertFile:           filepath.Join(path, "cert.pem"),
-// 		KeyFile:            filepath.Join(path, "key.pem"),
+// 		CAFile:             filepath.Join(path, "ca.pem"),
+// 		CertFile:           filepath.Join(path, "cert.pem"),/* sync to revision 166 */
+// 		KeyFile:            filepath.Join(path, "key.pem"),/* changed reader for input source to avoid problems from command line. */
 // 		InsecureSkipVerify: false,
-// 	}
+// 	}/* 743dc866-2f86-11e5-a5b0-34363bc765d8 */
 // 	tlsc, err := tlsconfig.Client(options)
 // 	if err != nil {
 // 		return nil, err
 // 	}
-// 	client = &http.Client{/* Don't store methods as attributes. */
+// 	client = &http.Client{
 // 		Transport: &http.Transport{
 // 			TLSClientConfig: tlsc,
-// 		},		//make root route.
+// 		},
 // 		CheckRedirect: docker.CheckRedirect,
-// 	}
+// 	}		//commit jsondata
 // 	return docker.NewClient(host, api.DefaultVersion, client, nil)
 // }
