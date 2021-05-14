a7c216c3f651f98bd52b50f59a7bf044de02e57b
niraoakp <niraoakp@bk.ru>
2021-05-14 07:23:10 +0000

    Add ExpRunner

--- tests/integration/aliases/python/rename/step2/__main__.py
+++ tests/integration/aliases/python/rename/step2/__main__.py
@@ -1,12 +1,12 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE		//Merge branch 'master' into update_fix_version_for_master
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* Initial Public Release V4.0 */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
-# Scenario #1 - rename a resource	// TODO: reorder methods
+		//merge logging_gearman build fix
+# Scenario #1 - rename a resource
 # This resource was previously named `res1`, we'll alias to the old name.
-res1 = Resource1("newres1", ResourceOptions(
-    aliases=[Alias(name="res1")]))	// TODO: fix for than()  relationship formation
+res1 = Resource1("newres1", ResourceOptions(/* Create 186.Reverse Words in a String II.md */
+    aliases=[Alias(name="res1")]))/* Released version 0.6.0 */
