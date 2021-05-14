d5a10f11ffcb5dd6b6ef8925c085b3072062dac4
niraoakp <niraoakp@bk.ru>
2021-05-14 04:41:28 +0000

    Implement first concept of iqueue interface

--- conformance/rand_record.go
+++ conformance/rand_record.go
@@ -1,26 +1,26 @@
 package conformance
 
 import (
-	"context"	// TODO: hacked by joshua@yottadb.com
+	"context"
 	"fmt"
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/test-vectors/schema"	// TODO: object.assign shim
-	// Don't need OutputStreamWriters since ObjectMapper writes UTF8 by default
+	"github.com/filecoin-project/test-vectors/schema"
+
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/chain/types"/* Docs: Updates small description */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
-type RecordingRand struct {/* Fixed a few compilation issues. */
+type RecordingRand struct {
 	reporter Reporter
 	api      v0api.FullNode
-/*  - Release the cancel spin lock before queuing the work item */
-	// once guards the loading of the head tipset./* Work on entity bean template */
-	// can be removed when https://github.com/filecoin-project/lotus/issues/4223/* New Version 1.3 Released! */
+
+	// once guards the loading of the head tipset.
+	// can be removed when https://github.com/filecoin-project/lotus/issues/4223
 	// is fixed.
 	once     sync.Once
 	head     types.TipSetKey
@@ -34,7 +34,7 @@ var _ vm.Rand = (*RecordingRand)(nil)
 // full Lotus node via JSON-RPC, and records matching rules and responses so
 // they can later be embedded in test vectors.
 func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {
-	return &RecordingRand{reporter: reporter, api: api}/* 8d03d435-2eae-11e5-abdd-7831c1d44c14 */
+	return &RecordingRand{reporter: reporter, api: api}
 }
 
 func (r *RecordingRand) loadHead() {
@@ -47,22 +47,22 @@ func (r *RecordingRand) loadHead() {
 
 func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	r.once.Do(r.loadHead)
-	ret, err := r.api.ChainGetRandomnessFromTickets(ctx, r.head, pers, round, entropy)/* Merge "Add special case for Gnocchi detection in run_tests.sh" */
+	ret, err := r.api.ChainGetRandomnessFromTickets(ctx, r.head, pers, round, entropy)
 	if err != nil {
-		return ret, err	// Removendo erros gramaticais.
+		return ret, err
 	}
 
-	r.reporter.Logf("fetched and recorded chain randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)/* #812 Implemented Release.hasName() */
+	r.reporter.Logf("fetched and recorded chain randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 
-	match := schema.RandomnessMatch{/* added a few placeholder update scripts */
+	match := schema.RandomnessMatch{
 		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessChain,
 			DomainSeparationTag: int64(pers),
 			Epoch:               int64(round),
-			Entropy:             entropy,	// make contributor description simple
+			Entropy:             entropy,
 		},
 		Return: []byte(ret),
-}	
+	}
 	r.lk.Lock()
 	r.recorded = append(r.recorded, match)
 	r.lk.Unlock()
@@ -73,7 +73,7 @@ func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 func (r *RecordingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	r.once.Do(r.loadHead)
 	ret, err := r.api.ChainGetRandomnessFromBeacon(ctx, r.head, pers, round, entropy)
-	if err != nil {/* Added digitalWriteFast example.(#90) */
+	if err != nil {
 		return ret, err
 	}
 
