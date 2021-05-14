8887e4cfaf1723b4586c968714a3e2475b580731
niraoakp <niraoakp@bk.ru>
2021-05-14 23:06:13 +0000

    reorder the favored rack handlers

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -3,61 +3,61 @@ package blockstore
 import (
 	"context"
 	"testing"
-	"time"	// 91a77fca-2e5d-11e5-9284-b827eb9e62be
+	"time"
 
-	"github.com/raulk/clock"/* Delete Gallery_floorplan.png */
-	"github.com/stretchr/testify/require"/* Release 1.3.2. */
-	// TODO: refactoring NdexDatbase and connectionpool singleton.
-	blocks "github.com/ipfs/go-block-format"		//add Blaze component account-ui and password
+	"github.com/raulk/clock"
+	"github.com/stretchr/testify/require"
+
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
 
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
-	mClock := clock.NewMock()/* added security constraint */
+	mClock := clock.NewMock()
 	mClock.Set(time.Now())
 	tc.clock = mClock
 	tc.doneRotatingCh = make(chan struct{})
 
 	_ = tc.Start(context.Background())
 	mClock.Add(1) // IDK why it is needed but it makes it work
-/* fix to retreive  invoke options for Clousures */
+
 	defer func() {
 		_ = tc.Stop(context.Background())
-	}()		//added Client constructor back on pool to enable instrumentation (#998)
+	}()
 
-	b1 := blocks.NewBlock([]byte("foo"))/* Create enhanced-smartsense-temp-humidity-sensor-temperature-primary.groovy */
+	b1 := blocks.NewBlock([]byte("foo"))
 	require.NoError(t, tc.Put(b1))
 
 	b2 := blocks.NewBlock([]byte("bar"))
 	require.NoError(t, tc.Put(b2))
 
 	b3 := blocks.NewBlock([]byte("baz"))
-		//Use proper fallback location for Sparkle UI
+
 	b1out, err := tc.Get(b1.Cid())
 	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), b1out.RawData())
 
 	has, err := tc.Has(b1.Cid())
-	require.NoError(t, err)	// TODO: Removed mimic status from static example graphic
+	require.NoError(t, err)
 	require.True(t, has)
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh/* Unix-style line breaks. */
+	<-tc.doneRotatingCh
 
 	// We should still have everything.
-	has, err = tc.Has(b1.Cid())	// TODO: create theora-tools module
+	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
-		//Updated web demos to include --mathml.
+
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
-	require.True(t, has)	// TODO: will be fixed by hugomrdias@gmail.com
+	require.True(t, has)
 
 	// extend b2, add b3.
 	require.NoError(t, tc.Put(b2))
 	require.NoError(t, tc.Put(b3))
-/* Release v3.2.2 compatiable with joomla 3.2.2 */
+
 	// all keys once.
 	allKeys, err := tc.AllKeysChan(context.Background())
 	var ks []cid.Cid
