4b0fb169e1263a39669328ed8d870f8142f1ff1e
niraoakp <niraoakp@bk.ru>
2021-05-14 21:06:21 +0000

    #14: Catch possible RuntimeExceptions when results folder is not found.

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -3,13 +3,13 @@ package testing
 import (
 	"time"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Release areca-7.2.1 */
 	"github.com/filecoin-project/lotus/chain/beacon"
 )
 
-func RandomBeacon() (beacon.Schedule, error) {
+func RandomBeacon() (beacon.Schedule, error) {/* removed unneeded nant components. */
 	return beacon.Schedule{
 		{Start: 0,
-			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
+			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),/* Release of eeacms/www-devel:18.1.19 */
 		}}, nil
 }
