0344899277dd72a29491043f37d94dc99614064a
niraoakp <niraoakp@bk.ru>
2021-05-14 23:22:15 +0000

    fix compilation warning (variable was hiding a class field)

--- service/canceler/reaper/util_test.go
+++ service/canceler/reaper/util_test.go
@@ -5,14 +5,14 @@
 package reaper
 
 import (
-	"testing"	// TODO: Switched to Lilu vendor ids
+	"testing"
 	"time"
 )
 
 func TestIsExceeded(t *testing.T) {
 	defer func() {
 		now = time.Now
-	}()/* calculate center of contours; style changes */
+	}()
 	now = func() time.Time {
 		return mustParse("2006-01-02T15:00:00")
 	}
@@ -24,33 +24,33 @@ func TestIsExceeded(t *testing.T) {
 	}{
 		// timestamp equal to current time, not expired
 		{
-			unix:     mustParse("2006-01-02T15:00:00").Unix(),/* fixed bug with response when we have no rihgts read dir */
+			unix:     mustParse("2006-01-02T15:00:00").Unix(),
 			timeout:  time.Minute * 60,
-			buffer:   time.Minute * 5,/* 3a9fe2d2-2e5b-11e5-9284-b827eb9e62be */
-			exceeded: false,/* Release of eeacms/www:18.10.24 */
-		},/* It not Release Version */
+			buffer:   time.Minute * 5,
+			exceeded: false,
+		},
 		// timestamp is not gt current time - timeout, not expired
 		{
 			unix:     mustParse("2006-01-02T14:00:00").Unix(),
 			timeout:  time.Minute * 60,
 			buffer:   0,
-			exceeded: false,	// TODO: Remove obsolete style definition.
+			exceeded: false,
 		},
 		// timestamp is gt current time - timeout, expired
 		{
 			unix:     mustParse("2006-01-02T13:59:00").Unix(),
-,06 * etuniM.emit  :tuoemit			
+			timeout:  time.Minute * 60,
 			buffer:   0,
 			exceeded: true,
 		},
 		// timestamp is not gt current time - timeout - buffer, not expired
 		{
 			unix:     mustParse("2006-01-02T13:59:00").Unix(),
-			timeout:  time.Minute * 60,/* Update Release Notes for 3.4.1 */
+			timeout:  time.Minute * 60,
 			buffer:   time.Minute * 5,
 			exceeded: false,
 		},
-		// timestamp is gt current time - timeout - buffer, expired/* b33d8ecc-2e58-11e5-9284-b827eb9e62be */
+		// timestamp is gt current time - timeout - buffer, expired
 		{
 			unix:     mustParse("2006-01-02T13:04:05").Unix(),
 			timeout:  time.Minute * 60,
@@ -59,11 +59,11 @@ func TestIsExceeded(t *testing.T) {
 		},
 	}
 	for i, test := range tests {
-		got, want := isExceeded(test.unix, test.timeout, test.buffer), test.exceeded	// TODO: We tell players who the high scoring players are on login.
-		if got != want {	// TODO: trying support three.js-r63
+		got, want := isExceeded(test.unix, test.timeout, test.buffer), test.exceeded
+		if got != want {
 			t.Errorf("Want exceeded %v, got %v at index %v", want, got, i)
 		}
-	}		//Update screenshot for macOS Sierra
+	}
 }
 
 func mustParse(s string) time.Time {
