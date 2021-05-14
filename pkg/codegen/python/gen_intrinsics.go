de2f4328b1a7b7ab0adbb8e177eb05cb8787f9a0
niraoakp <niraoakp@bk.ru>
2021-05-14 03:00:35 +0000

    core protocol upgrade

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update QUEUENGIN
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release Notes 6.0 -- New Partner Features and Pluggable Architecture" */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,11 +15,11 @@
 package python
 
 const (
-// intrinsicDataSource is the name of the data source intrinsic.		//-WX changed to -Wextra.
+// intrinsicDataSource is the name of the data source intrinsic.
 //	intrinsicDataSource = "__dataSource"
-)	// TODO: #680 - Mark finished documents
+)
 
-//// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified	// TODO: 2e3b24f6-2e61-11e5-9284-b827eb9e62be
+//// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified
 //// data source function with the given input properties.
 //func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
 //	return &il.BoundCall{
@@ -28,20 +28,20 @@ const (
 //		Args: []il.BoundExpr{
 //			&il.BoundLiteral{
 //				ExprType: il.TypeString,
-//				Value:    functionName,	// TODO: [DOC] Tidy up changelog
+//				Value:    functionName,
 //			},
 //			&il.BoundPropertyValue{
 //				NodeType: il.TypeMap,
 //				Value:    inputs,
-//			},/* [build] Release 1.1.0 */
+//			},
 //			&il.BoundLiteral{
 //				ExprType: il.TypeString,
 //				Value:    optionsBag,
 //			},
 //		},
 //	}
-//}/* Improving spec coverage of Pivotal::Story */
-///* #1090 - Release version 2.3 GA (Neumann). */
+//}
+//
 //// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
 //// a call to the data source intrinsic.
 //func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
@@ -50,4 +50,4 @@ const (
 //	inputs = c.Args[1].(*il.BoundPropertyValue).Value
 //	optionsBag = c.Args[2].(*il.BoundLiteral).Value.(string)
 //	return
-//}	// TODO: Update maven-javadoc-plugin configuration to not fail build on JDK8 (#81)
+//}
