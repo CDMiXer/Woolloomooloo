90a43010274f47336c089803b8b91d7c77c7ce14
niraoakp <niraoakp@bk.ru>
2021-05-14 09:39:24 +0000

    Create windows_recovery.png

--- handler/api/repos/crons/update_test.go
+++ handler/api/repos/crons/update_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by souzau@yandex.com
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//neater comments
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -8,74 +8,74 @@ package crons
 
 import (
 	"bytes"
-	"context"/* 758ed136-2d53-11e5-baeb-247703a38240 */
-	"encoding/json"		//transfer complete
-	"net/http"
+	"context"/* Phpinfo n'était pas capitalisé (Francky)  */
+	"encoding/json"
+	"net/http"		//Publish post on Jekyll and RVM
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"/* [TOOLS-121] Show "No releases for visible projects" in dropdown Release filter */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-/* clean up the scan results */
-	"github.com/go-chi/chi"		//fixed test for 1.4
+	"github.com/drone/drone/mock"	// TODO: KexHandler -> Negotiator
+/* Release instead of reedem. */
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* Release of eeacms/eprtr-frontend:0.4-beta.16 */
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockCron := new(core.Cron)	// Rename sitemap (2).xml to sitemap.xml
+	mockCron := new(core.Cron)
 	*mockCron = *dummyCron
-	mockCron.Disabled = false/* [bug] don't interpolate strings before calling gettext() */
-	mockCron.Branch = "develop"	// TODO: will be fixed by brosner@gmail.com
+	mockCron.Disabled = false/* Merge "Release 1.0.0.178 QCACLD WLAN Driver." */
+	mockCron.Branch = "develop"	// TODO: will be fixed by mail@bitpshr.net
 	mockCron.Target = "staging"
-/* Release 0.2.24 */
+/* Merge kpi-quest/master */
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)
-	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)
-/* Merge "supprt keycloak first stage" */
+	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)	// TODO: Delete IMG_3474.JPG
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// Update sudu
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
-	in := new(bytes.Buffer)
+	in := new(bytes.Buffer)/* (Robert Collins) Release bzr 0.15 RC 1 */
 	json.NewEncoder(in).Encode(mockCron)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", in)
-	r = r.WithContext(	// TODO: will be fixed by mowrain@yandex.com
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//Initial version of espec_ast
+	r = r.WithContext(/* fix: [github] Release type no needed :) */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* [QUARKS-124] add tests for Deadtime */
+
 	HandleUpdate(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-)tog ,tnaw ,"d% tog ,d% edoc esnopser tnaW"(frorrE.t		
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Cron{}, mockCron
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {		//* fixed drupal 7 user_authenticate
 		t.Errorf(diff)
 	}
 }
-
+/* Create viewedByScroll.js */
 func TestHandleUpdate_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)/* simply triangle in VAO/VBO */
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// TODO: will be fixed by willem.melching@gmail.com
 	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
