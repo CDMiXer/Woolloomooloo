2f916ab055b399494cd4a92438c813f99b575eaf
niraoakp <niraoakp@bk.ru>
2021-05-14 02:01:41 +0000

    Merge "Remove tox envirnoment for pypy"

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -9,60 +9,60 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/go-state-types/network"
-	// TODO: will be fixed by arajasek94@gmail.com
-	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: hacked by boringland@protonmail.ch
-	"github.com/filecoin-project/lotus/chain/types"
-)
 
-//                       MODIFYING THE API INTERFACE/* Merge "[FIX] core/StashedControlSupport: unstash with owner component" */
-//	// TODO: Remove WBW in bundle classes
+	"github.com/filecoin-project/lotus/api"	// TODO: will be fixed by souzau@yandex.com
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/types"
+)	// TODO: hacked by hello@brooklynzelenka.com
+		//added one line!
+//                       MODIFYING THE API INTERFACE
+//
 // NOTE: This is the V0 (Stable) API - when adding methods to this interface,
-// you'll need to make sure they are also present on the V1 (Unstable) API	// TODO: hacked by seth@sethvargo.com
+// you'll need to make sure they are also present on the V1 (Unstable) API
 //
 // This API is implemented in `v1_wrapper.go` as a compatibility layer backed
 // by the V1 api
-//
-// When adding / changing methods in this file:	// chore(deps): update dependency @types/sequelize to v4.27.37
+//		//Fix axioms tooltip
+// When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
 //  * Generate proxy structs
-//  * Generate mocks		//Merge "Do not include offline nodes in repo connectivity check"
+//  * Generate mocks
 //  * Generate markdown docs
-//  * Generate openrpc blobs	// TODO: will be fixed by nick@perfectabstractions.com
-
+//  * Generate openrpc blobs
+/* rev 627894 */
 type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
 	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)
 	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
 	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
-)rorre ,teSpiT.sepyt*( )yeKteSpiT.sepyt kst ,hcopEniahC.iba h ,txetnoC.txetnoc xtc(thgieHyBteSpiTteGniahC	
+	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
-	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)		//Added script to delete old s3 buckets to allow tests to pass again.
+	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
-	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
+	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)/* (vila) Release 2.3b1 (Vincent Ladeuil) */
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
-	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)		//The source code for the SwissMonitor service.
-	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
+	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
+	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)/* Add Release Branch */
 	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
 	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
-	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)		//Allow cache to be disabled.
+	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)
 	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
-	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)
-	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)/* Fixed a reporting problem in the channel tests */
+	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)		//psutil is used by the exporter jobs.
+	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)/* 0.202 : RTElement and RTEdge can now be fixed */
 	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
-	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)/* Create new-blog-by-github-pages-jekyll-theme.md */
+	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
 	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)
-	WalletBalance(context.Context, address.Address) (types.BigInt, error)	// TODO: hacked by martin2cai@hotmail.com
+	WalletBalance(context.Context, address.Address) (types.BigInt, error)
 }
-
+		//bluetooth sensor manager works and can connect bluetooth devices
 var _ Gateway = *new(FullNode)
