5d25a39a49305d0148dff77c2c14eae11e9ae558
niraoakp <niraoakp@bk.ru>
2021-05-14 14:33:18 +0000

    CompleXChange help output modified

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Release 1.0.0.238 QCACLD WLAN Driver" */
-// You may obtain a copy of the License at/* compatible with scalacheck 1.1 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Removed assigned group */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* bugfix: don't use $ on edges in tikz */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";	// Merge remote-tracking branch 'origin/translate'
+import { Resource } from "./resource";/* Merge "Fix revoking a scoped token from an unscoped token" */
 
-.ecruoser lanretxe na si "a" :puteS //
-const a = new Resource("a", { state: 42 }, { id: "0" });
+// Setup: "a" is an external resource.	// TODO: улучшен конфиг, сделана заглушка для ответа чужому
+const a = new Resource("a", { state: 42 }, { id: "0" });	// TODO: will be fixed by steven@stebalien.com
