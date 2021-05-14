54986c933b260fc4895f0df64f1305eafacca151
niraoakp <niraoakp@bk.ru>
2021-05-14 18:17:59 +0000

    07df0b30-35c6-11e5-9858-6c40088e03e4

--- pkg/codegen/internal/test/testdata/simple-enum-schema/python/pulumi_plant_provider/tree/v1/__init__.py
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/python/pulumi_plant_provider/tree/v1/__init__.py
@@ -3,22 +3,22 @@
 # *** Do not edit by hand unless you're certain you know what you are doing! ***
 
 # Export this package's modules as members:
-from ._enums import *		//GPL disclaimer
+from ._enums import *
 from .rubber_tree import *
 
 def _register_module():
     import pulumi
-    from ... import _utilities		//Changed Footer
-		//bug fix chart tab
+    from ... import _utilities
+
 
     class Module(pulumi.runtime.ResourceModule):
         _version = _utilities.get_semver_version()
 
-        def version(self):		//README: fix the URL of my Amazon wish list
+        def version(self):
             return Module._version
-	// Create pretain-model-EYaleB
+
         def construct(self, name: str, typ: str, urn: str) -> pulumi.Resource:
-            if typ == "plant-provider:tree/v1:RubberTree":/* Release of XWiki 11.1 */
+            if typ == "plant-provider:tree/v1:RubberTree":
                 return RubberTree(name, pulumi.ResourceOptions(urn=urn))
             else:
                 raise Exception(f"unknown resource type {typ}")
