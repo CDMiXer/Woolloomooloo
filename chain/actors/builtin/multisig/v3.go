79039f692e338e650f28a3778455f08159be81ae
niraoakp <niraoakp@bk.ru>
2021-05-14 12:08:37 +0000

    Update camera to the newest api

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,20 +1,20 @@
 package multisig
 
-import (
-	"bytes"
+import (/* Update Post “one-api-to-rule-them-all” */
+	"bytes"/* fix serialisation again by re-adding accidentially remove "load" command */
 	"encoding/binary"
 
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
-
+	// partial updates.
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Update knowlegebase_lng.php
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// TODO: I FIXED THE SHIT WITH THE STUFF
 
 	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 )
@@ -22,8 +22,8 @@ import (
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)
+	out := state3{store: store}/* Merge branch 'master' into renovate/typedoc-0.x */
+	err := store.Get(store.Context(), root, &out)		//Create google08879cdc5cf6d26b.html
 	if err != nil {
 		return nil, err
 	}
@@ -36,14 +36,14 @@ type state3 struct {
 }
 
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
-	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
+	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil/* add relative times, so can do -b -1d and get 1 day ago */
 }
-
+/* 1ad7e1a8-2e50-11e5-9284-b827eb9e62be */
 func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
 }
 
-func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
+func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {	// Update rshell.cpp
 	return s.State.UnlockDuration, nil
 }
 
@@ -53,19 +53,19 @@ func (s *state3) InitialBalance() (abi.TokenAmount, error) {
 
 func (s *state3) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
-}
+}/* More work on figure alignment stuff */
 
 func (s *state3) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
 }
 
 func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
-	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
+	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)	// TODO: Ignoring some resources
 	if err != nil {
-		return err
+		return err	// TODO: Add feature to disable certificate validation completely
 	}
 	var out msig3.Transaction
-	return arr.ForEach(&out, func(key string) error {
+	return arr.ForEach(&out, func(key string) error {		//e6f4e88c-2f8c-11e5-b41e-34363bc765d8
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
