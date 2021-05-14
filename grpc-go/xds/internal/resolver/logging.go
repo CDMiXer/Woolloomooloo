fd2db6dc721f206fd64316ba976d5e551a2d34bd
niraoakp <niraoakp@bk.ru>
2021-05-14 14:40:34 +0000

    7d40de7a-2e47-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,16 +1,16 @@
 /*
+ *		//8f648746-2e74-11e5-9284-b827eb9e62be
+ * Copyright 2020 gRPC authors.	// TODO: hacked by peterke@gmail.com
  *
- * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");		//rev 593962
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Modified python reflection table functions to set algorithm outside registry
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Not longer a thing here.
- *
- * Unless required by applicable law or agreed to in writing, software		//it's working now!
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */* drop_duplicates and entropy (#6) */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Formats update
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,14 +20,14 @@ package resolver
 
 import (
 	"fmt"
-		//Basic02 revised
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[xds-resolver %p] "	// e3a0523c-2e50-11e5-9284-b827eb9e62be
-		//Remove superfluous argument
-var logger = grpclog.Component("xds")
+const prefix = "[xds-resolver %p] "
+
+var logger = grpclog.Component("xds")/* lingering env */
 
 func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
