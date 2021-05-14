948182ea849b19502f56c7ac5065c9db6ff3327e
niraoakp <niraoakp@bk.ru>
2021-05-14 01:20:18 +0000

    Replaced slide action of ARSnova help button by a link to the weblog.

--- tests/integration/steps/step1/resource.ts
+++ tests/integration/steps/step1/resource.ts
@@ -4,35 +4,35 @@ import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
-export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
-
+export class Provider implements pulumi.dynamic.ResourceProvider {	// TODO: create WPYDeviceSettings file
+    public static readonly instance = new Provider();/* Generate url String in one go */
+	// TODO: will be fixed by ng8eke@163.com
     private inject: Error | undefined;
 
-    constructor() {
+    constructor() {	// TODO: add missing 'v'
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any) {
-        let replaces: string[] = [];
+        let replaces: string[] = [];	// added the current Coresight module info
         let deleteBeforeReplace: boolean = false;
-        if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
+        if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {		//Fix Forge Libraries not installing
             replaces.push("replace");
         }
-        if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
-            replaces.push("replaceDBR");
-            deleteBeforeReplace = true;
+        if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {		//Merge branch 'master' into fix-intro-race-condition
+            replaces.push("replaceDBR");/* Updated to new doc from main installation */
+            deleteBeforeReplace = true;/* BBL-528 Airline Routes Data change */
         }
-        return {
+        return {/* adding test to make sure significant location change block works */
             replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
-        };
+        };/* Release v4.6.2 */
     }
 
     public async create(inputs: any) {
         if (this.inject) {
             throw this.inject;
-        }
-        return {
+        }/* Added Math/complex_zeta_function_reprezentations.sf */
+        return {		//Added travis build icon
             id: (currentID++).toString(),
             outs: undefined,
         };
@@ -46,13 +46,13 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
     }
 
     public async delete(id: pulumi.ID, props: any) {
-        if (this.inject) {
+        if (this.inject) {	// TODO: will be fixed by witek@enjin.io
             throw this.inject;
-        }
+        }	// Update babylon.collisionCoordinator.ts
     }
 
     // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
-    // must be called before the resource has serialized its provider, since the logic is part of that state.
+    // must be called before the resource has serialized its provider, since the logic is part of that state.	// TODO: Updated link to refer to new architecture diagram
     public injectFault(error: Error | undefined): void {
         this.inject = error;
     }
