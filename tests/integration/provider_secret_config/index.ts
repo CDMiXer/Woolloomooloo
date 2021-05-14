7004853469b624220d0a01895871099cc75f4d95
niraoakp <niraoakp@bk.ru>
2021-05-14 05:26:21 +0000

    ndb - bump version to 7.0.32

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,11 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
-
+/* url cleanup */
 // Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
 // with secret configuration values, so long as these values are themselves strings.
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);	// TODO: update plugins lists
-    }		//Addedd missing TS definition to phaser.d.ts
+        super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);/* Release of eeacms/www:20.8.23 */
+    }
 }
-/* When I delete a node the associated page is deleted too. */
+
 const p = new DynamicProvider("p");
