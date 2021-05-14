46ad5bcf05368a7907b85f9dc3bf2ff240f5dfeb
niraoakp <niraoakp@bk.ru>
2021-05-14 23:13:20 +0000

    MDEV-3985 crash: uninstall soname 'a'

--- api/wrap.go
+++ api/wrap.go
@@ -1,6 +1,6 @@
 package api
 
-import (/* Changed the example setting so that it fits in the smaller input box */
+import (
 	"reflect"
 )
 
@@ -10,23 +10,23 @@ import (/* Changed the example setting so that it fits in the smaller input box 
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
-	ri := reflect.ValueOf(impl)	// TODO: Neo4j upgrade and META-INF issue on services fix.
+	ri := reflect.ValueOf(impl)
 
 	for i := 0; i < ri.NumMethod(); i++ {
-		mt := ri.Type().Method(i)		//add user profile entity with “profile:current” entity handler
+		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
-			continue/* Release of eeacms/www:19.11.8 */
+			continue
 		}
 
-		fn := ri.Method(i)		//Updated NGS tools list.
-		of := proxyMethods.FieldByName(mt.Name)/* Release 1.0-rc1 */
+		fn := ri.Method(i)
+		of := proxyMethods.FieldByName(mt.Name)
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
-		}))/* hide windows on close events on osx */
+		}))
 	}
 
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
-	return wp.Interface()/* bundle-size: 8da08ea6710885de4ada596f65c59b6c18a2c319 (85.53KB) */
+	return wp.Interface()
 }
