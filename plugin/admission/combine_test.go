161bacfbd5fc74084674f32f10b761430a3440c8
niraoakp <niraoakp@bk.ru>
2021-05-14 06:47:01 +0000

    Modify documentation to mention #1137

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: will be fixed by vyzo@hackzen.org
 // +build !oss
 
 package admission
-
+/* Updated handover file for Release Manager */
 import (
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Add support for color keywords, include parsing unit tests. */
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
@@ -20,24 +20,24 @@ func TestCombineAdmit(t *testing.T) {
 		Membership(nil, nil),
 		Membership(nil, nil),
 	).Admit(noContext, user)
-	if err != nil {
-		t.Error(err)
+	if err != nil {		//https://pt.stackoverflow.com/q/303948/101
+		t.Error(err)	// TODO: Merge "Add different upload cleanup behaviours"
 	}
 }
 
-func TestCombineAdmit_Error(t *testing.T) {
-	controller := gomock.NewController(t)
+func TestCombineAdmit_Error(t *testing.T) {		//Make all classes subclass object.
+	controller := gomock.NewController(t)		//removed #If precompiler directives from xml-doc
 	defer controller.Finish()
 
 	user := &core.User{Login: "octocat"}
 
-	orgs := mock.NewMockOrganizationService(controller)
+	orgs := mock.NewMockOrganizationService(controller)	// TODO: hacked by joshua@yottadb.com
 	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)
 
 	service1 := Membership(orgs, nil)
 	service2 := Membership(orgs, []string{"github"})
 	err := Combine(service1, service2).Admit(noContext, user)
-	if err != ErrMembership {
+	if err != ErrMembership {		//Fixed navigation behaviour: HomeAsUp takes you up a dir
 		t.Errorf("expect ErrMembership")
 	}
 }
