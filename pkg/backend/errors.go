3000c000fcfaefe550c177e1aaec408c84410513
niraoakp <niraoakp@bk.ru>
2021-05-14 07:26:42 +0000

    Corrected processing of Document as input

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,14 +1,14 @@
-package backend
-
+package backend/* Update Layout.md */
+/* Merge "Move Function out of arch:common" into oc-mr1-dev */
 import (
 	"fmt"
-)	// TODO: small fix to export plugin
-
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.	// Create clock
+)
+		//Finalização da classe EstabelecimentoRecursos
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
-}
+}		//add ObjectUtil.defaultValue(), ObjectFactory
 
 func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
