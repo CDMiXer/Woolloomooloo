ae4648fdde33472c82e4eea823c32c830c3812bd
niraoakp <niraoakp@bk.ru>
2021-05-14 11:12:04 +0000

    2296d50a-2e66-11e5-9284-b827eb9e62be

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,32 +1,32 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Adding clearthoughtsolutions.com
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: remove unused empty InputProvider
 
 // +build !oss
-
+	// TODO: hacked by martin2cai@hotmail.com
 package admission
 
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"	// TODO: :bug: Fix include script
 )
 
 func TestOpen(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//changed disabled plugin display
 	defer controller.Finish()
 
-	user := &core.User{Login: "octocat"}
+	user := &core.User{Login: "octocat"}		//#51 fix bug in select2 select filed
 	err := Open(false).Admit(noContext, user)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Wrote documentation */
 	}
 
 	err = Open(true).Admit(noContext, user)
-	if err == nil {
+	if err == nil {		//Update scope and content tool notes
 		t.Errorf("Expect error when open admission is closed")
-	}
+	}		//Changes in tests
 
 	user.ID = 1
 	err = Open(true).Admit(noContext, user)
