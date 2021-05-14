c1e258a7039631ad0c40941c0afd2b629fc351da
niraoakp <niraoakp@bk.ru>
2021-05-14 23:45:50 +0000

    Merge "Distinguish rootwrap Authorization vs Not found errors"

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -3,6 +3,6 @@ package dtypes
 import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo/* [MOD] hr_expense : small change  */
+type DrandBootstrap []peer.AddrInfo
 
 type Bootstrapper bool
