b2c054b2bdfd7d63e8077cdee76c235ab4f23cf0
niraoakp <niraoakp@bk.ru>
2021-05-14 13:45:56 +0000

    Datastructure fixes

--- pkg/backend/state/stacks.go
+++ pkg/backend/state/stacks.go
@@ -1,33 +1,33 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//bcaa30ba-2e75-11e5-9284-b827eb9e62be
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by nagydani@epointsystem.org
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* New account app for handling logins, signups, etc. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* devine aux var names as global constants */
-// See the License for the specific language governing permissions and/* Base profile with choices and client/server side validation */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package state
 
-import (	// TODO: CWS-TOOLING: integrate CWS writerfilter07
+import (
 	"context"
-		//Ver exemplares de empréstimo e combobox order by codigo
+
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-	// TODO: hacked by fkautz@pseudocode.cc
+
 // CurrentStack reads the current stack and returns an instance connected to its backend provider.
 func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {
 	w, err := workspace.New()
 	if err != nil {
 		return nil, err
-	}	// TODO: hacked by josharian@gmail.com
-/* Add currency format and use in class that extends Sheet class. */
+	}
+
 	stackName := w.Settings().Stack
 	if stackName == "" {
 		return nil, nil
@@ -35,20 +35,20 @@ func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, 
 
 	ref, err := backend.ParseStackReference(stackName)
 	if err != nil {
-		return nil, err/* Release of eeacms/www:18.7.27 */
+		return nil, err
 	}
 
-	return backend.GetStack(ctx, ref)/* 781fa97a-2e3e-11e5-9284-b827eb9e62be */
-}/* BasicStatement.close improved */
+	return backend.GetStack(ctx, ref)
+}
 
 // SetCurrentStack changes the current stack to the given stack name.
 func SetCurrentStack(name string) error {
-	// Switch the current workspace to that stack./* Change version to 663 */
+	// Switch the current workspace to that stack.
 	w, err := workspace.New()
 	if err != nil {
 		return err
 	}
 
 	w.Settings().Stack = name
-	return w.Save()	// TODO: Corr. Parasola leiocephala
+	return w.Save()
 }
