3b9b2d3e0feeb1d9631acad457dee4790009efb6
niraoakp <niraoakp@bk.ru>
2021-05-14 15:55:36 +0000

    refactor(general): move code to lib/, add opts.js

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -2,6 +2,6 @@
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  BLOB	// UI now uses system look and feel
+,log_data  BLOB
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
-);	// TODO: hacked by jon@atack.com
+);
