bdf5c6ef21e8536f87a0ebd941fc0b001a6c22e9
niraoakp <niraoakp@bk.ru>
2021-05-14 05:19:19 +0000

    illegal character fix

--- grpc-go/xds/internal/balancer/priority/logging.go
+++ grpc-go/xds/internal/balancer/priority/logging.go
@@ -5,18 +5,18 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* bot now builds an azc when it has not yet build one */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// e006bd8a-2e4d-11e5-9284-b827eb9e62be
+ * limitations under the License.
  *
  */
 
-package priority/* Release of eeacms/bise-frontend:1.29.15 */
+package priority
 
 import (
 	"fmt"
@@ -24,7 +24,7 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-/* d7756c placeholder, upd7759.c doesn't support upd7756 yet */
+
 const prefix = "[priority-lb %p] "
 
 var logger = grpclog.Component("xds")
