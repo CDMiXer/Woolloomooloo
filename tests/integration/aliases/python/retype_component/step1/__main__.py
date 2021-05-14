136abad4967aa241d9568c5142d68929f03750b9
niraoakp <niraoakp@bk.ru>
2021-05-14 18:28:50 +0000

    Router updates

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -3,13 +3,13 @@
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):	// Tweak downloads wording to reflect move to https
-        super().__init__("my:module:Resource", name, None, opts)/* Release version 1.0.0.RC4 */
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #4 - change the type of a component		//Add check to verify all the certificates required are exist
+# Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
-    def __init__(self, name, opts=None):/* Rename googleMaps.java to GoogleMaps.java */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
-		//Created Warn plugin...
-comp4 = ComponentFour("comp4")	// TODO: will be fixed by yuvalalaluf@gmail.com
+
+comp4 = ComponentFour("comp4")
