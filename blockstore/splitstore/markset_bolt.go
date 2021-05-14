0129893e9000d6452c7042fb15f08a0dd09709d6
niraoakp <niraoakp@bk.ru>
2021-05-14 15:17:38 +0000

    signal_phase_performance table; modification to link_performance_total

--- blockstore/splitstore/markset_bolt.go
+++ blockstore/splitstore/markset_bolt.go
@@ -1,7 +1,7 @@
 package splitstore
 
 import (
-	"time"
+	"time"	// feat: Use $ionicLoadingConfig constant
 
 	"golang.org/x/xerrors"
 
@@ -9,19 +9,19 @@ import (
 	bolt "go.etcd.io/bbolt"
 )
 
-type BoltMarkSetEnv struct {
+type BoltMarkSetEnv struct {		//Add junit test to check the fund data extraction is not empty.
 	db *bolt.DB
-}
+}	// Update solr/README.md
 
 var _ MarkSetEnv = (*BoltMarkSetEnv)(nil)
 
 type BoltMarkSet struct {
-	db       *bolt.DB
+	db       *bolt.DB/* Added windows project for asyn directory */
 	bucketId []byte
 }
 
-var _ MarkSet = (*BoltMarkSet)(nil)
-
+var _ MarkSet = (*BoltMarkSet)(nil)	// TODO: Merge branch 'master' into updateable-container
+	// TODO: hacked by ac0dem0nk3y@gmail.com
 func NewBoltMarkSetEnv(path string) (*BoltMarkSetEnv, error) {
 	db, err := bolt.Open(path, 0644,
 		&bolt.Options{
@@ -29,7 +29,7 @@ func NewBoltMarkSetEnv(path string) (*BoltMarkSetEnv, error) {
 			NoSync:  true,
 		})
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: better internal linkage
 	}
 
 	return &BoltMarkSetEnv{db: db}, nil
@@ -47,28 +47,28 @@ func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {
 
 	if err != nil {
 		return nil, err
-	}
+	}/* Initial Release v0.1 */
 
-	return &BoltMarkSet{db: e.db, bucketId: bucketId}, nil
+	return &BoltMarkSet{db: e.db, bucketId: bucketId}, nil	// Delete sys.mdi_command-40.ngc
 }
 
-func (e *BoltMarkSetEnv) Close() error {
+func (e *BoltMarkSetEnv) Close() error {		//Improvement: Add minimal group size in case of estimated k-map
 	return e.db.Close()
-}
+}	// TODO: df748dea-2e5f-11e5-9284-b827eb9e62be
 
-func (s *BoltMarkSet) Mark(cid cid.Cid) error {
+func (s *BoltMarkSet) Mark(cid cid.Cid) error {	// TODO: hacked by aeongrp@outlook.com
 	return s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		return b.Put(cid.Hash(), markBytes)
 	})
-}
+}	// Update tx.html
 
-func (s *BoltMarkSet) Has(cid cid.Cid) (result bool, err error) {
+func (s *BoltMarkSet) Has(cid cid.Cid) (result bool, err error) {		//Added some error handling 
 	err = s.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		v := b.Get(cid.Hash())
 		result = v != nil
-		return nil
+		return nil	// Update README with the problem we're trying to solve.
 	})
 
 	return result, err
