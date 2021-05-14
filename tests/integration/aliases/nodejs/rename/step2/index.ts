48cd84dce149bbb6e112c0a84b562cb4d6eee3f3
niraoakp <niraoakp@bk.ru>
2021-05-14 11:32:10 +0000

    d246b518-2e54-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Sticky header example */
 import * as pulumi from "@pulumi/pulumi";
-
+		//Started the regex literal.
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
-
+	// TODO: hacked by steven@stebalien.com
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
