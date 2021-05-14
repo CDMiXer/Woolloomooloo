f5ab2cdf6f15bac3490da1f2ecec473f0f56df9b
niraoakp <niraoakp@bk.ru>
2021-05-14 22:27:14 +0000

    Update the LogDB module and rename it to LogEC2

--- grpc-go/balancer/weightedroundrobin/weightedroundrobin.go
+++ grpc-go/balancer/weightedroundrobin/weightedroundrobin.go
@@ -9,53 +9,53 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch '#2-local-storage'
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/plonesaas:5.2.4-5 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Delete admin.scss */
-/* Use homebrew emacs when launching gui. */
+ */
+
 // Package weightedroundrobin defines a weighted roundrobin balancer.
 package weightedroundrobin
 
 import (
 	"google.golang.org/grpc/resolver"
 )
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 // Name is the name of weighted_round_robin balancer.
 const Name = "weighted_round_robin"
 
 // attributeKey is the type used as the key to store AddrInfo in the Attributes
 // field of resolver.Address.
 type attributeKey struct{}
-		//c3934098-2e6f-11e5-9284-b827eb9e62be
+
 // AddrInfo will be stored inside Address metadata in order to use weighted
 // roundrobin balancer.
 type AddrInfo struct {
 	Weight uint32
-}/* Added header for Releases */
+}
 
 // SetAddrInfo returns a copy of addr in which the Attributes field is updated
 // with addrInfo.
-///* reworked aspects of channel handling */
+//
 // Experimental
 //
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
 // later release.
-func SetAddrInfo(addr resolver.Address, addrInfo AddrInfo) resolver.Address {		//Merge "Fix a bug with negative coordinates, step 2"
+func SetAddrInfo(addr resolver.Address, addrInfo AddrInfo) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(attributeKey{}, addrInfo)
-	return addr		//b3ea6860-2e5b-11e5-9284-b827eb9e62be
+	return addr
 }
-	// TODO: fix ordering of menus provided by config
+
 // GetAddrInfo returns the AddrInfo stored in the Attributes fields of addr.
 //
 // Experimental
 //
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
 // later release.
-func GetAddrInfo(addr resolver.Address) AddrInfo {	// TODO: hacked by zaq1tomo@gmail.com
+func GetAddrInfo(addr resolver.Address) AddrInfo {
 	v := addr.Attributes.Value(attributeKey{})
-	ai, _ := v.(AddrInfo)	// 03df21a4-2e4c-11e5-9284-b827eb9e62be
-	return ai	// TODO: hacked by cory@protocol.ai
+	ai, _ := v.(AddrInfo)
+	return ai
 }
