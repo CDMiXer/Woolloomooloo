13f515b3014b51b05ae6c5e4aa615d00ce4471b7
niraoakp <niraoakp@bk.ru>
2021-05-14 19:47:35 +0000

    ddbaae32-2e44-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
-/* Release 0.035. Added volume control to options dialog */
-CREATE TABLE IF NOT EXISTS logs (/* Task #4714: Merge changes and fixes from LOFAR-Release-1_16 into trunk */
+
+CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
 ,log_data  MEDIUMBLOB
 );
