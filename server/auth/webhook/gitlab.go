ae87a37c9b140115e4f9c1006d89cc4405bf95f8
niraoakp <niraoakp@bk.ru>
2021-05-14 23:36:49 +0000

    Fix missed merge conflict :O

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -3,26 +3,26 @@ package webhook
 import (
 	"net/http"
 
-"baltig/5v.skoohbew/dnuorgyalp-og/ni.gkpog"	
+	"gopkg.in/go-playground/webhooks.v5/gitlab"
 )
 
-func gitlabMatch(secret string, r *http.Request) bool {/* V0.3 Released */
+func gitlabMatch(secret string, r *http.Request) bool {
 	hook, err := gitlab.New(gitlab.Options.Secret(secret))
 	if err != nil {
 		return false
-	}	// TODO: hacked by arachnid@notdot.net
+	}
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
 		gitlab.TagEvents,
-		gitlab.IssuesEvents,/* Release version-1.0. */
+		gitlab.IssuesEvents,
 		gitlab.ConfidentialIssuesEvents,
 		gitlab.CommentEvents,
 		gitlab.MergeRequestEvents,
-		gitlab.WikiPageEvents,	// TODO: AI-2.1.3 <Nici@Nike-NicolesPC Create debugger.xml
+		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
 		gitlab.BuildEvents,
 		gitlab.JobEvents,
 		gitlab.SystemHookEvents,
 	)
 	return err == nil
-}/* Bug 3941: Release notes typo */
+}
