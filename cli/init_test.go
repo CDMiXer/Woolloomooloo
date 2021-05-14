68d88443171e51b882d9cf9b27372927689ea586
niraoakp <niraoakp@bk.ru>
2021-05-14 19:23:22 +0000

    Limit vector norms for non-sigmoid losses

--- cli/init_test.go
+++ cli/init_test.go
@@ -3,7 +3,7 @@ package cli
 import (
 	logging "github.com/ipfs/go-log/v2"
 )
-
+	// TODO: will be fixed by mail@overlisted.net
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
-}/* Pass key code in callback function */
+}/* Release dhcpcd-6.4.2 */
