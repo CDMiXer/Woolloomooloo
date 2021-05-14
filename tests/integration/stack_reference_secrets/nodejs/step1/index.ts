4142baf970839342dc83e22efb41572f9d8e0b02
niraoakp <niraoakp@bk.ru>
2021-05-14 18:58:15 +0000

    Altera 'corrigir-dimensoes-de-imovel-da-uniao'

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
-export const normal = pulumi.output("normal");/* Release 1.0.2 with Fallback Picture Component, first version. */
+export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
 
