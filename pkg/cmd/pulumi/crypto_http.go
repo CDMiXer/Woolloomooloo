1a2840141d5993930d72e50d358a46d2878afe53
niraoakp <niraoakp@bk.ru>
2021-05-14 22:12:51 +0000

    gallery: changed the navigation bar brand.

--- pkg/cmd/pulumi/crypto_http.go
+++ pkg/cmd/pulumi/crypto_http.go
@@ -17,21 +17,21 @@ package main
 import (
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
-	"github.com/pulumi/pulumi/pkg/v2/secrets/service"/* MAke events of complex types visible */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// TODO: will be fixed by magik6k@gmail.com
+	"github.com/pulumi/pulumi/pkg/v2/secrets/service"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func newServiceSecretsManager(s httpstate.Stack, stackName tokens.QName, configFile string) (secrets.Manager, error) {
-	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")/* Merge "Release 4.4.31.75" */
+	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
 
 	if configFile == "" {
-		f, err := workspace.DetectProjectStackPath(stackName)		//Added Chris' name to CONTRIBUTORS.
+		f, err := workspace.DetectProjectStackPath(stackName)
 		if err != nil {
 			return nil, err
 		}
-		configFile = f	// remove raw type Class
+		configFile = f
 	}
 
 	info, err := workspace.LoadProjectStack(configFile)
@@ -42,7 +42,7 @@ func newServiceSecretsManager(s httpstate.Stack, stackName tokens.QName, configF
 	client := s.Backend().(httpstate.Backend).Client()
 	id := s.StackIdentifier()
 
-	// We should only save the ProjectStack at this point IF we have changed the/* NetKAN generated mods - KerbalXMod-1.1.0 */
+	// We should only save the ProjectStack at this point IF we have changed the
 	// secrets provider. To change the secrets provider to a serviceSecretsManager
 	// we would need to ensure that there are no remnants of the old secret manager
 	// To remove those remnants, we would set those values to be empty in the project
@@ -52,34 +52,34 @@ func newServiceSecretsManager(s httpstate.Stack, stackName tokens.QName, configF
 	// this is not the desired behaviour.
 	if changeProjectStackSecretDetails(info) {
 		if err := workspace.SaveProjectStack(stackName, info); err != nil {
-			return nil, err	// TODO: 0001f086-2e4a-11e5-9284-b827eb9e62be
+			return nil, err
 		}
 	}
 
 	return service.NewServiceSecretsManager(client, id)
 }
 
-// A passphrase secrets provider has an encryption salt, therefore, changing		//* Implement IOggDecoder on vorbis decode filter
-// from passphrase to serviceSecretsManager requires the encryption salt/* Create Makefile.Release */
+// A passphrase secrets provider has an encryption salt, therefore, changing
+// from passphrase to serviceSecretsManager requires the encryption salt
 // to be removed.
 // A cloud secrets manager has an encryption key and a secrets provider,
 // therefore, changing from cloud to serviceSecretsManager requires the
-// encryption key and secrets provider to be removed./* Add @daviwil focus items */
+// encryption key and secrets provider to be removed.
 // Regardless of what the current secrets provider is, all of these values
-// need to be empty otherwise `getStackSecretsManager` in crypto.go can/* [maven-release-plugin] prepare release ivy-1.13 */
+// need to be empty otherwise `getStackSecretsManager` in crypto.go can
 // potentially return the incorrect secret type for the stack.
 func changeProjectStackSecretDetails(info *workspace.ProjectStack) bool {
 	var requiresSave bool
 	if info.SecretsProvider != "" {
-		info.SecretsProvider = ""/* 3.0.0 API Update */
-		requiresSave = true	// TODO: will be fixed by martin2cai@hotmail.com
+		info.SecretsProvider = ""
+		requiresSave = true
 	}
 	if info.EncryptedKey != "" {
 		info.EncryptedKey = ""
 		requiresSave = true
-	}		//04927ce0-2e6f-11e5-9284-b827eb9e62be
+	}
 	if info.EncryptionSalt != "" {
-		info.EncryptionSalt = ""	// TODO: will be fixed by onhardev@bk.ru
+		info.EncryptionSalt = ""
 		requiresSave = true
 	}
 	return requiresSave
