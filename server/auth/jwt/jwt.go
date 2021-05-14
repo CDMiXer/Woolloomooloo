19dd7993c83a35802cbac2e4c29adbedbbaf2212
niraoakp <niraoakp@bk.ru>
2021-05-14 04:53:58 +0000

    ac5b2a4a-306c-11e5-9929-64700227155b

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -1,9 +1,9 @@
 package jwt
-
+		//Added text about #box channel
 import (
 	"encoding/base64"
-	"encoding/json"
-	"fmt"
+	"encoding/json"/* Delete TacticalTech_Image4.JPG */
+	"fmt"/* [NGC-3078] Decommissioning services */
 	"io/ioutil"
 	"strings"
 
@@ -12,25 +12,25 @@ import (
 	"github.com/argoproj/argo/server/auth/jws"
 )
 
-func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
+func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {	// TODO: Making up runnable by adding Paste.
 	username := restConfig.Username
 	if username != "" {
-		return &jws.ClaimSet{Sub: username}, nil
+		return &jws.ClaimSet{Sub: username}, nil/* product: ProductUiLabels: fix ProductOriginalImageMessage en wording */
 	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {
 		bearerToken := restConfig.BearerToken
 		if bearerToken == "" {
 			// should only ever be used for service accounts
-			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
+			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)		//precautionary unset
 			if err != nil {
 				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
-			}
-			bearerToken = string(data)
-		}
-		parts := strings.SplitN(bearerToken, ".", 3)
-		if len(parts) != 3 {
+}			
+			bearerToken = string(data)	// TODO: will be fixed by praveen@minio.io
+		}	// New App: NotificationLog
+		parts := strings.SplitN(bearerToken, ".", 3)/* Reuse opened editor for data search */
+		if len(parts) != 3 {/* add Release 1.0 */
 			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")
 		}
-		payload := parts[1]
+		payload := parts[1]	// TODO: will be fixed by ng8eke@163.com
 		data, err := base64.RawStdEncoding.DecodeString(payload)
 		if err != nil {
 			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)
