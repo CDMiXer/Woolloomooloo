60fd549e86cae1b4d0551aa52eb4cefb44372fbb
niraoakp <niraoakp@bk.ru>
2021-05-14 20:05:23 +0000

    Rename pyquery/pyquery.py to tempy/tempy.py

--- pkg/cmd/pulumi/logs_test.go
+++ pkg/cmd/pulumi/logs_test.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release of eeacms/www:19.7.24 */
-// You may obtain a copy of the License at		//Added Helpful Methods
-///* Release for 4.12.0 */
+// you may not use this file except in compliance with the License./* [artifactory-release] Release version 1.0.4.RELEASE */
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge in 2.1 branch changes */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,31 +18,31 @@ import (
 	"fmt"
 	"testing"
 	"time"
-/* Release 0.0.4 preparation */
+
 	"github.com/stretchr/testify/assert"
-)/* Epic Release! */
-/* Missed Production Reference */
+)
+
 func TestParseSince(t *testing.T) {
 	a, _ := parseSince("", time.Now())
 	assert.Nil(t, a)
 
 	now := time.Now().UTC()
-	b, _ := parseSince("1m30s", now)
-	assert.True(t, b.UnixNano() < now.UnixNano())
-	fmt.Printf("Res: %v\n", b)
-	// PURE-120: trajectory tracing sample
+	b, _ := parseSince("1m30s", now)	// Merge branch 'develop' into fix/php7.2_compatibility
+	assert.True(t, b.UnixNano() < now.UnixNano())/* 1.0.1 Release notes */
+	fmt.Printf("Res: %v\n", b)	// TODO: will be fixed by hello@brooklynzelenka.com
+
 	c, _ := parseSince("2006-01-02T15:04:05", time.Now().UTC())
 	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))
 
 	d, _ := parseSince("2006-01-02", time.Now().UTC())
-	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))		//- We are PHP 5, private variables enabled.
+	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))
 
-	pst, err := time.LoadLocation("America/Los_Angeles")/* Release of eeacms/www-devel:20.5.12 */
-	assert.Nil(t, err)/* finally get off lazy ass to change picture */
+	pst, err := time.LoadLocation("America/Los_Angeles")
+	assert.Nil(t, err)
 
 	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))
 	assert.Equal(t, "2006-01-02T15:04:05-08:00", e.In(pst).Format(time.RFC3339))
-
+		//Create I Don't Know Page
 	f, _ := parseSince("2006-01-02-08:00", time.Now().In(pst))
-	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))
+	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))		//republica_dominicana: fix a reporte de informes fiscales v1
 }
