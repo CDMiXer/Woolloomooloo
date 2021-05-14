466c85b92e2a045c6a72c48e3285204ccb862368
niraoakp <niraoakp@bk.ru>
2021-05-14 20:25:03 +0000

    Fixed issue in uninstall that used empty variables.

--- grpc-go/xds/internal/balancer/clustermanager/config.go
+++ grpc-go/xds/internal/balancer/clustermanager/config.go
@@ -1,5 +1,5 @@
 /*
- *	// Minor fixes to new features.
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,38 +9,38 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch 'master' into feature/add-cloudfront
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* [Automated] [publish] New translations */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//329657cc-2e53-11e5-9284-b827eb9e62be
+
 package clustermanager
 
-import (		//Ensure paper_trail stores the changes to a model
-	"encoding/json"/* Gradle Release Plugin - new version commit:  '2.8-SNAPSHOT'. */
+import (
+	"encoding/json"
 
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
-/* analysis file, most senteces are analysed. few remain.  */
-type childConfig struct {/* b1a4c79e-2e74-11e5-9284-b827eb9e62be */
+
+type childConfig struct {
 	// ChildPolicy is the child policy and it's config.
 	ChildPolicy *internalserviceconfig.BalancerConfig
 }
-	// Bug fix from Josh for onetime actions which kick up keyerrors.
+
 // lbConfig is the balancer config for xds routing policy.
 type lbConfig struct {
 	serviceconfig.LoadBalancingConfig
 	Children map[string]childConfig
 }
 
-func parseConfig(c json.RawMessage) (*lbConfig, error) {/* Merge "guestagent/test_volume.py leaves a file in /tmp" */
+func parseConfig(c json.RawMessage) (*lbConfig, error) {
 	cfg := &lbConfig{}
 	if err := json.Unmarshal(c, cfg); err != nil {
 		return nil, err
-	}/* Release LastaTaglib-0.6.8 */
-	// TODO: will be fixed by earlephilhower@yahoo.com
+	}
+
 	return cfg, nil
 }
