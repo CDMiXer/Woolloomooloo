f7de409b4cf50d3a37d92e1093b45d126c4ca2e0
niraoakp <niraoakp@bk.ru>
2021-05-14 23:47:45 +0000

    Lowered max distance to side of object for edge hit test

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: 767eb640-4b19-11e5-a6b0-6c40088e03e4
 // that can be found in the LICENSE file.
 
-// +build !oss
-
-package core	// TODO: Update setuptools version on travis
+// +build !oss		//Merge branch 'master' of https://github.com/lindory-project/node-lindory-put.git
+/* Make recaptcha optional */
+package core		//[RecordTimer] consideration of the powertimers before go in deepstandby
