fa67ba3e9b6d6a0e7a612fa45bf54e2f49e76993
niraoakp <niraoakp@bk.ru>
2021-05-14 01:47:29 +0000

    Remove huaway_status.sh and issue curl command directly.

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,20 +1,20 @@
-package reward/* Update PatchReleaseChecklist.rst */
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Update Readme for version 1
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Merge branch 'master' into dev/keithley2450 */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
 )
-/* 5.6.1 Release */
+
 var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {	// TODO: Delete ex5_old.cpp
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
@@ -23,18 +23,18 @@ func load0(store adt.Store, root cid.Cid) (State, error) {	// TODO: Delete ex5_o
 	return &out, nil
 }
 
-type state0 struct {	// test result for the passion profile quiz
+type state0 struct {
 	reward0.State
 	store adt.Store
 }
 
-func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {	// TODO: 672b3884-2e4b-11e5-9284-b827eb9e62be
+func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
-}	// TODO: Mark response-profile related objects
+}
 
 func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
-		//Delete Test02_t0.05.stars.hdf5
-	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil	// Updated the payment object
+
+	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
 }
 
@@ -45,7 +45,7 @@ func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalMined, nil
 }
-/* Released version 0.3.6 */
+
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
@@ -53,11 +53,11 @@ func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
-/* friendly message when there are no chunks to show */
+
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
-/* Add Lost Password functionnality (with trans) */
+
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
@@ -66,13 +66,13 @@ func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTot
 	return miner0.InitialPledgeForPower(
 		sectorWeight,
 		s.State.ThisEpochBaselinePower,
-		networkTotalPledge,		//Clarify which version of the Google style guide
+		networkTotalPledge,
 		s.State.ThisEpochRewardSmoothed,
-{etamitsEretliF.0gnihtooms&		
+		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
-		circSupply), nil/* Added a method to fix anchors to inside a document. */
+		circSupply), nil
 }
 
 func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
