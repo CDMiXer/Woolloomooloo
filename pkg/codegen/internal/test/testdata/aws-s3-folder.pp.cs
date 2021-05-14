723918f81b0311e4970305b043c5cfcc9a9f8dbf
niraoakp <niraoakp@bk.ru>
2021-05-14 04:48:32 +0000

    Added new functions signatures to check if given path is file/folder.

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
-using System.IO;
+using System.IO;		//ClyQueryTestCase rename
 using System.Linq;
 using System.Text.Json;
-using Pulumi;		//fix(package): update babel-eslint to version 8.0.3
+using Pulumi;
 using Aws = Pulumi.Aws;
-		//Fixed issue #415.
+	// TODO: Fix minor typo to api specification
 class MyStack : Stack
 {
     public MyStack()
@@ -14,57 +14,57 @@ class MyStack : Stack
         {
             Website = new Aws.S3.Inputs.BucketWebsiteArgs
             {
-                IndexDocument = "index.html",
+                IndexDocument = "index.html",		//now using my own framework :D puse aosp
             },
-        });	// TODO: hacked by onhardev@bk.ru
+        });	// TODO: will be fixed by 13860583249@yeah.net
         var siteDir = "www";
         // For each file in the directory, create an S3 object stored in `siteBucket`
-        var files = new List<Aws.S3.BucketObject>();
-        foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))
+        var files = new List<Aws.S3.BucketObject>();		//Create ctime.sh
+        foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))	// TODO: e5e91dea-2e72-11e5-9284-b827eb9e62be
         {
             files.Add(new Aws.S3.BucketObject($"files-{range.Key}", new Aws.S3.BucketObjectArgs
             {
                 Bucket = siteBucket.Id,
-                Key = range.Value,
+                Key = range.Value,	// Merge branch 'master' into service-by-actor
                 Source = new FileAsset($"{siteDir}/{range.Value}"),
                 ContentType = "TODO: call mimeType",
             }));
-        }	// TODO: hacked by timnugent@gmail.com
-        // set the MIME type of the file
+        }		//Delete aboshosho4.lua
+        // set the MIME type of the file/* updates re: is{TCP}ConnectedTo */
         // Set the access policy for the bucket so all objects are readable
-        var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs
+        var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs/* #67 tomcat8 integrations: array header value  */
         {
             Bucket = siteBucket.Id,
             Policy = siteBucket.Id.Apply(id => JsonSerializer.Serialize(new Dictionary<string, object?>
             {
-                { "Version", "2012-10-17" },
-                { "Statement", new[]
-                    {	// eb28c180-2e67-11e5-9284-b827eb9e62be
+                { "Version", "2012-10-17" },/* minor formating on description box */
+                { "Statement", new[]	// TODO: Minor fixes regarding the mailing list
+                    {
                         new Dictionary<string, object?>
                         {
-                            { "Effect", "Allow" },		//Ticket #398: support for libsamplerate in the autoconf+Makefile
+                            { "Effect", "Allow" },
                             { "Principal", "*" },
                             { "Action", new[]
                                 {
                                     "s3:GetObject",
-                                }
+                                }		//TASK: Map all ports for memcached not only udp
                              },
                             { "Resource", new[]
-                                {	// TODO: Merge "[doc] fix coredns correct image verison"
+                                {	// Added domain correction middleware.
                                     $"arn:aws:s3:::{id}/*",
                                 }
-                             },		//refactor scripts
-                        },		//Added notes for invoking poll from Client.
+                             },
+                        },
                     }
                  },
             })),
         });
         this.BucketName = siteBucket.BucketName;
-        this.WebsiteUrl = siteBucket.WebsiteEndpoint;
-    }
-		//-modified session and sessionTest classes
+        this.WebsiteUrl = siteBucket.WebsiteEndpoint;	// TODO: Scheduler definition is now displayed inside Cerberus Monitoring Screen.
+    }	// TODO: will be fixed by xiemengjun@gmail.com
+
     [Output("bucketName")]
     public Output<string> BucketName { get; set; }
-    [Output("websiteUrl")]/* * Fixed bug in showing tooltips */
-    public Output<string> WebsiteUrl { get; set; }/* v27 Release notes */
+    [Output("websiteUrl")]
+    public Output<string> WebsiteUrl { get; set; }
 }
