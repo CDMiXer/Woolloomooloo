1968a602d743934e0a761d2705a7160d20a86f13
niraoakp <niraoakp@bk.ru>
2021-05-14 03:58:43 +0000

    Fix typo: 'filered' → 'filtered'. (#784)

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,7 +1,7 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"		//added basic functionality for viewing changelogs
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
@@ -10,41 +10,41 @@ import (
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
-)		//Preparing Changelog for Release
-	// Author: Christian Rupp
+)
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-)tuo& ,toor ,)(txetnoC.erots(teG.erots =: rre	
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* Deleted some PDF events. */
-	}	// Created loop-guard.png
+		return nil, err
+	}
 	return &out, nil
 }
-/* using bonndan/ReleaseManager instead of RMT fork */
+
 type state0 struct {
 	verifreg0.State
-	store adt.Store	// TODO: Removed ".py" from documentation #11
+	store adt.Store
 }
 
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* ProRelease3 hardware update for pullup on RESET line of screen */
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
-}	// Delete Bosresume.pdf
+}
 
-func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {	// TODO: Merge "GPFS CES: Fix bugs related to access rules not found"
+func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
-}/* Update kunka.html */
+}
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
-func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// TODO: hacked by brosner@gmail.com
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
@@ -52,6 +52,6 @@ func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
 
-func (s *state0) verifiers() (adt.Map, error) {		//Delete castiglione_pescaia_tombino_guess.html
+func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
 }
