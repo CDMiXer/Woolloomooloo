c37562969379edfaa94f0a4d1b6d7f846944c722
niraoakp <niraoakp@bk.ru>
2021-05-14 14:16:57 +0000

    Fixes TableMerge table size set problem

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
-sepyt egakcap
+package types
 
 import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
 )
-		//Link to the docs branch
+
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
 	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
-	Nonce   uint64	// TODO: hacked by m-ou.se@m-ou.se
+	Nonce   uint64
 	Balance BigInt
 }
