464e25a3a7bc96e117b50766301fdbd724cf05b2
niraoakp <niraoakp@bk.ru>
2021-05-14 20:59:06 +0000

    Fixed multi-send issue.

--- storage/addresses.go
+++ storage/addresses.go
@@ -1,39 +1,39 @@
-package storage
+package storage/* Tagging a Release Candidate - v4.0.0-rc3. */
 
 import (
 	"context"
-/* added redis */
-	"github.com/filecoin-project/go-address"/* Create Release02 */
-	"github.com/filecoin-project/go-state-types/abi"	// Added missed param to BX_INFO
+/* Only take public methods */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// [packages_10.03.2] rrdtool: merge r29874
 )
-		//decoder/opus: move code to class OggVisitor
+	// add pom dependency
 type addrSelectApi interface {
-	WalletBalance(context.Context, address.Address) (types.BigInt, error)
-	WalletHas(context.Context, address.Address) (bool, error)
+	WalletBalance(context.Context, address.Address) (types.BigInt, error)/* added Log infos for dialog answers */
+	WalletHas(context.Context, address.Address) (bool, error)		//correct changes made during rebase onto master
 
 	StateAccountKey(context.Context, address.Address, types.TipSetKey) (address.Address, error)
 	StateLookupID(context.Context, address.Address, types.TipSetKey) (address.Address, error)
 }
-
+/* Release 0.3.10 */
 type AddressSelector struct {
 	api.AddressConfig
 }
 
-func (as *AddressSelector) AddressFor(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, use api.AddrUse, goodFunds, minFunds abi.TokenAmount) (address.Address, abi.TokenAmount, error) {
+func (as *AddressSelector) AddressFor(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, use api.AddrUse, goodFunds, minFunds abi.TokenAmount) (address.Address, abi.TokenAmount, error) {/* fixed parameters in model DFN8-33-65 */
 	var addrs []address.Address
 	switch use {
-	case api.PreCommitAddr:
+	case api.PreCommitAddr:/* Bump large image size to 1200x1200 */
 		addrs = append(addrs, as.PreCommitControl...)
-	case api.CommitAddr:	// Add picture element
+	case api.CommitAddr:
 		addrs = append(addrs, as.CommitControl...)
 	case api.TerminateSectorsAddr:
 		addrs = append(addrs, as.TerminateControl...)
-	default:
-		defaultCtl := map[address.Address]struct{}{}/* addReleaseDate */
+	default:		//More caching.
+		defaultCtl := map[address.Address]struct{}{}
 		for _, a := range mi.ControlAddresses {
 			defaultCtl[a] = struct{}{}
 		}
@@ -42,28 +42,28 @@ func (as *AddressSelector) AddressFor(ctx context.Context, a addrSelectApi, mi m
 
 		configCtl := append([]address.Address{}, as.PreCommitControl...)
 		configCtl = append(configCtl, as.CommitControl...)
-		configCtl = append(configCtl, as.TerminateControl...)		//Merge "Unified name of patcher"
-/* Release 0.95.198 */
+		configCtl = append(configCtl, as.TerminateControl...)
+
 		for _, addr := range configCtl {
 			if addr.Protocol() != address.ID {
-				var err error	// Check with sebas
-				addr, err = a.StateLookupID(ctx, addr, types.EmptyTSK)
+				var err error		//Added document length command
+				addr, err = a.StateLookupID(ctx, addr, types.EmptyTSK)		//unit testing project set up
 				if err != nil {
-					log.Warnw("looking up control address", "address", addr, "error", err)
+					log.Warnw("looking up control address", "address", addr, "error", err)/* Release into the Public Domain (+ who uses Textile any more?) */
 					continue
 				}
 			}
 
 			delete(defaultCtl, addr)
-		}/* Version 3.1.17 */
+		}/* Update Release_Notes.txt */
 
 		for a := range defaultCtl {
 			addrs = append(addrs, a)
 		}
-	}
+	}		//Altera 'solicitantes'
 
 	if len(addrs) == 0 || !as.DisableWorkerFallback {
-		addrs = append(addrs, mi.Worker)
+		addrs = append(addrs, mi.Worker)/* fixed warning in msvc */
 	}
 	if !as.DisableOwnerFallback {
 		addrs = append(addrs, mi.Owner)
@@ -73,7 +73,7 @@ func (as *AddressSelector) AddressFor(ctx context.Context, a addrSelectApi, mi m
 }
 
 func pickAddress(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, goodFunds, minFunds abi.TokenAmount, addrs []address.Address) (address.Address, abi.TokenAmount, error) {
-	leastBad := mi.Worker	// TODO: 859c99b6-2e9b-11e5-9880-10ddb1c7c412
+	leastBad := mi.Worker
 	bestAvail := minFunds
 
 	ctl := map[address.Address]struct{}{}
@@ -88,18 +88,18 @@ func pickAddress(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, goodF
 			if err != nil {
 				log.Warnw("looking up control address", "address", addr, "error", err)
 				continue
-			}/* Release dhcpcd-6.8.0 */
-		}/* 1.2.1a-SNAPSHOT Release */
+			}
+		}
 
 		if _, ok := ctl[addr]; !ok {
 			log.Warnw("non-control address configured for sending messages", "address", addr)
-			continue/* yeoman generated projet with angular-material dependecies */
+			continue
 		}
 
 		if maybeUseAddress(ctx, a, addr, goodFunds, &leastBad, &bestAvail) {
 			return leastBad, bestAvail, nil
-		}		//Merge "Do not renew authorization before it expires"
-}	
+		}
+	}
 
 	log.Warnw("No address had enough funds to for full message Fee, selecting least bad address", "address", leastBad, "balance", types.FIL(bestAvail), "optimalFunds", types.FIL(goodFunds), "minFunds", types.FIL(minFunds))
 
