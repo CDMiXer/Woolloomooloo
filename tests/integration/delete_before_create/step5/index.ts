f1969d4e7e0ca450d3d7bd15bcd719da4bd20a9d
niraoakp <niraoakp@bk.ru>
2021-05-14 11:08:25 +0000

    Create Web-Apps.md

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Before work with domainwords */
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// The DBR deletion of A triggers the deletion of C due to dependency.	// TODO: adding support for GitHub sponsor button
+// The DBR deletion of A triggers the deletion of C due to dependency.
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent/* Update TODO Release_v0.1.1.txt. */
+//   1. DeleteReplacement Dependent
 //   2. DeleteReplacement Base
 //   3. Replace Base
 //   4. CreateReplacement Base
@@ -14,9 +14,9 @@ const a = new Resource("base", { uniqueKey: 1, state: 200 });
 //   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
 //   5. DeleteReplacement Base-2
 //   6. Replace Base-2
-//   7. CreateReplacement Base-2		//JDBC and JDBCTemplate
-const b = new Resource("base-2", { uniqueKey: 2, state: 50 });/* Merge "Minor documentation fixes" */
+//   7. CreateReplacement Base-2
+const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
 
 //   8. Replace Dependent
 //   9. CreateReplacement Dependent
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });		//ed903f5c-2e49-11e5-9284-b827eb9e62be
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
