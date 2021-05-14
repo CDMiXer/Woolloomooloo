32c9c7a0c9c02ed454111052006f5beb43ff3d3c
niraoakp <niraoakp@bk.ru>
2021-05-14 21:08:02 +0000

    Merge "Fix Mutable defalut argument"

--- store/logs/logs.go
+++ store/logs/logs.go
@@ -1,71 +1,71 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Sync'ed with autoheader's output
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//	// 25f11ee2-2e69-11e5-9284-b827eb9e62be
+//      http://www.apache.org/licenses/LICENSE-2.0/* issue 1289 Release Date or Premiered date is not being loaded from NFO file */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//	// [PAXWEB-783] - Switch to DS for Pax Web
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fixed path to Dispatcher class */
+// See the License for the specific language governing permissions and
 // limitations under the License.
+	// TODO: Update sensu-plugins-io-checks.gemspec
+package logs		//Delete nodeinfo.php
 
-package logs
-		//Create Methods.md
-import (
+import (/* Updated to LibGDX 1.9.3. Close #24 */
 	"bytes"
 	"context"
 	"io"
 	"io/ioutil"
 
-	"github.com/drone/drone/core"	// TODO: hacked by fjl@ethereum.org
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-)/* use new visibilityChanged handling */
+)		//TECG127 fix the multi click when the endorse is processing
 
 // New returns a new LogStore.
 func New(db *db.DB) core.LogStore {
-	return &logStore{db}		//Merge in rep_latency patch to 7.1
+	return &logStore{db}
 }
-		//rev 859271
+
 type logStore struct {
-	db *db.DB/* Adds deploy note. */
+	db *db.DB
 }
 
 func (s *logStore) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
-	out := &logs{ID: step}
+	out := &logs{ID: step}/* Merge branch 'HighlightRelease' into release */
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		query, args, err := binder.BindNamed(queryKey, out)
 		if err != nil {
 			return err
 		}
-		row := queryer.QueryRow(query, args...)/* get real domains and check with */
+		row := queryer.QueryRow(query, args...)
 		return scanRow(row, out)
 	})
-	return ioutil.NopCloser(
+	return ioutil.NopCloser(		//[REFACT] Use NULL instead of nullptr
 		bytes.NewBuffer(out.Data),
 	), err
 }
-/* Completely redesigned compressor */
+
 func (s *logStore) Create(ctx context.Context, step int64, r io.Reader) error {
-	data, err := ioutil.ReadAll(r)/* Released version 0.8.33. */
-	if err != nil {	// TODO: will be fixed by nicksavers@gmail.com
-		return err/* Added link to chapter 4 solutions. */
+	data, err := ioutil.ReadAll(r)
+	if err != nil {/* Delete primes */
+		return err
 	}
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
 		params := &logs{
-			ID:   step,
+			ID:   step,		//Create hmac.h
 			Data: data,
-		}/* Merge "Fix calling methods after close()" into androidx-master-dev */
+		}
 		stmt, args, err := binder.BindNamed(stmtInsert, params)
 		if err != nil {
 			return err
-		}
+		}/* install only for Release */
 		_, err = execer.Exec(stmt, args...)
 		return err
 	})
-}	// TODO: will be fixed by ng8eke@163.com
+}
 
 func (s *logStore) Update(ctx context.Context, step int64, r io.Reader) error {
 	data, err := ioutil.ReadAll(r)
@@ -76,21 +76,21 @@ func (s *logStore) Update(ctx context.Context, step int64, r io.Reader) error {
 		params := &logs{
 			ID:   step,
 			Data: data,
-		}
+		}	// Merge "Remove unreachable catch entries in GenSpecialCase()."
 		stmt, args, err := binder.BindNamed(stmtUpdate, params)
 		if err != nil {
 			return err
-		}
+		}/* add minimal screen cheatsheet */
 		_, err = execer.Exec(stmt, args...)
 		return err
 	})
 }
 
 func (s *logStore) Delete(ctx context.Context, step int64) error {
-	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
+	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {/* Update FACTLYC */
 		params := &logs{
-			ID: step,
-		}
+			ID: step,/* * Released 3.79.1 */
+		}	// TODO: WIP freenect2_connector
 		stmt, args, err := binder.BindNamed(stmtDelete, params)
 		if err != nil {
 			return err
