28e59e3e8b481356edcf5125de4634eb96f45650
niraoakp <niraoakp@bk.ru>
2021-05-14 15:28:23 +0000

    Merge "Add error handling when Swift is not installed"

--- tests/integration/steps/step5/index.ts
+++ tests/integration/steps/step5/index.ts
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: d54c9cd4-2e3e-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Provider, Resource } from "./resource";
-
+/* Update lp_vs_mip.md */
 // Step 5: Fail during an update:
 // * Create 1 resource, a5, with a property different than the a4 in Step 4, requiring replacement
-//   (CreateReplacement(a5), Update(c4=>c5), DeleteReplaced(a4))./* [FEATURE] Add Release date for SSDT */
+//   (CreateReplacement(a5), Update(c4=>c5), DeleteReplaced(a4)).		//Rename rails_app_deployment.md to rails/app_deployment.md
 let a = new Resource("a", { state: 1, replace: 2 });
 // * Inject a fault into the Update(c4=>c5), such that we never delete a4 (and it goes onto the checkpoint list).
 // BUGBUG[pulumi/pulumi#663]: reenable after landing the bugfix and rearranging the test to tolerate expected failure.
-// Provider.instance.injectFault(new Error("intentional update failure during step 4"));		//change name of nested function for the sake of error messages
+// Provider.instance.injectFault(new Error("intentional update failure during step 4"));
 let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });
 let e = new Resource("e", { state: 1 });
-// Checkpoint: a5, c4, e4; pending delete: a4
+// Checkpoint: a5, c4, e4; pending delete: a4/* Add system log monitor */
