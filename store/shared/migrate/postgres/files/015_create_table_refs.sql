d2201ea224f747ebe3b07fbd518ea7667e6701b1
niraoakp <niraoakp@bk.ru>
2021-05-14 08:49:16 +0000

    DakrorLauncher implementation

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
-
+/* Release type and status should be in lower case. (#2489) */
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER		//added more documentation, for completness and clarity
-,latest_build_id INTEGER		//Create file PG_OtherTitles-model.pdf
-,latest_type     VARCHAR(50)
+ latest_repo_id  INTEGER
+,latest_build_id INTEGER
+,latest_type     VARCHAR(50)	// Went full inception
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER/* Merge "Bring back (abandoned) support for Cinder/NFS" */
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+,latest_deleted  INTEGER/* * Changed layout */
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// TODO: will be fixed by 13860583249@yeah.net
 );
 
 -- name: create-index-latest-repo
-		//Create tor_detect
-CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);		//MAJOR: Terminal#getSize() implemented
+
+CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
