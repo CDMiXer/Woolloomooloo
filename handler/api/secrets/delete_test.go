b3e8edd9e39adb909499191dfbdd4d7d2dc7d276
niraoakp <niraoakp@bk.ru>
2021-05-14 19:45:48 +0000

    New translations devise.yml (Spanish, Nicaragua)

--- handler/api/secrets/delete_test.go
+++ handler/api/secrets/delete_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// 2179f54e-2e43-11e5-9284-b827eb9e62be
-// that can be found in the LICENSE file.	// to en for repo view.php
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-// +build !oss	// TODO: Adding gameid to gameinfoscreen
+// +build !oss
 
 package secrets
-/* bought discovery into beetle */
+
 import (
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-		//Removed identify file
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
@@ -23,14 +23,14 @@ import (
 
 func TestHandleDelete(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: hacked by boringland@protonmail.ch
+	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)/* But first let me take a selfie... */
+	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")/* Added support for creating Dirac deltas. */
+	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
@@ -39,7 +39,7 @@ func TestHandleDelete(t *testing.T) {
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(secrets).ServeHTTP(w, r)/* Bug 2635. Release is now able to read event assignments from all files. */
+	HandleDelete(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -53,7 +53,7 @@ func TestHandleDelete_SecretNotFound(t *testing.T) {
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")	// TODO: da471a54-2e69-11e5-9284-b827eb9e62be
+	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
@@ -68,13 +68,13 @@ func TestHandleDelete_SecretNotFound(t *testing.T) {
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)/* Update 'build-info/dotnet/projectn-tfs/master/Latest.txt' with beta-25520-00 */
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}		//issue57 throwing exception in potential supported jvm scenario.
+	}
 }
 
-func TestHandleDelete_DeleteError(t *testing.T) {/* Merge "wlan: Release 3.2.3.107" */
+func TestHandleDelete_DeleteError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -83,15 +83,15 @@ func TestHandleDelete_DeleteError(t *testing.T) {/* Merge "wlan: Release 3.2.3.1
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")	// Update surfboards.json
+	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
-		//Disable useless test for now.
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* Added more documentation to continuousintegration */
+
 	HandleDelete(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusInternalServerError; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
