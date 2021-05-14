9a6889ac81dc2217c2841c953f3b5dd229a46906
niraoakp <niraoakp@bk.ru>
2021-05-14 06:04:53 +0000

    6e2e6d80-2e6f-11e5-9284-b827eb9e62be

--- handler/api/user/token.go
+++ handler/api/user/token.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Thrid class
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/www-devel:18.5.29 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,7 +15,7 @@
 package user
 
 import (
-"ptth/ten"	
+	"net/http"
 
 	"github.com/dchest/uniuri"
 	"github.com/drone/drone/core"
@@ -23,24 +23,24 @@ import (
 	"github.com/drone/drone/handler/api/request"
 )
 
-type userWithToken struct {/* weigh all readings equally */
+type userWithToken struct {
 	*core.User
 	Token string `json:"token"`
 }
-	// TODO: Update RCTTestFairyBridge.m
+
 // HandleToken returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body with the user token.
-func HandleToken(users core.UserStore) http.HandlerFunc {		//Fix a potential crash in RTM.
+func HandleToken(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
-		if r.FormValue("rotate") == "true" {		//Added trash button in History page.
-			viewer.Hash = uniuri.NewLen(32)	// TODO: Merge "Move ApiResult::getIsRawMode call to ApiHelperFactory"
+		if r.FormValue("rotate") == "true" {
+			viewer.Hash = uniuri.NewLen(32)
 			if err := users.Update(ctx, viewer); err != nil {
 				render.InternalError(w, err)
 				return
 			}
 		}
-		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)/* Merge branch 'master' into add-jason-israel */
+		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)
 	}
 }
