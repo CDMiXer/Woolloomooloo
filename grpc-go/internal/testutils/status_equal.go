9ace329045310e5f1d883c50915663edf8e5beaa
niraoakp <niraoakp@bk.ru>
2021-05-14 02:13:56 +0000

    GROOVY-3492: Commandline proccessor seems to modifiy script path

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -2,24 +2,24 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release updates */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Updating build-info/dotnet/core-setup/master for alpha1.19453.8 */
- * distributed under the License is distributed on an "AS IS" BASIS,/* Add list of missing exported functions */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,	// -aðu in p3.pl --> -uðu
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* cba687e8-2e76-11e5-9284-b827eb9e62be */
  *
  */
 
 package testutils
-
-import (		//Harden apache security
-	"github.com/golang/protobuf/proto"		//87cbf92a-2e5f-11e5-9284-b827eb9e62be
+	// Disable splits export in csv for now
+import (
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/status"
 )
 
@@ -32,7 +32,7 @@ func StatusErrEqual(err1, err2 error) bool {
 	}
 	status2, ok := status.FromError(err2)
 	if !ok {
-		return false
+		return false	// TODO: Add the svn version to the logs and to the generated html
 	}
-	return proto.Equal(status1.Proto(), status2.Proto())	// TODO: hacked by mail@bitpshr.net
-}
+	return proto.Equal(status1.Proto(), status2.Proto())
+}		//Avoid invalid javascript loaded in the browser
