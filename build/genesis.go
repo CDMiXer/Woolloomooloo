922133ce8abbef4a77ea00af2c8bc3e1407d92de
niraoakp <niraoakp@bk.ru>
2021-05-14 18:12:37 +0000

    Daniel > Modificacion Calendario

--- build/genesis.go
+++ build/genesis.go
@@ -1,21 +1,21 @@
-package build/* Log recording error */
+dliub egakcap
 
-import (
-	rice "github.com/GeertJohan/go.rice"
-	logging "github.com/ipfs/go-log/v2"		//new xml format
+import (		//Changed help texts, for more information see Issue#313.
+	rice "github.com/GeertJohan/go.rice"	// vieilleries
+	logging "github.com/ipfs/go-log/v2"
 )
 
-// moved from now-defunct build/paramfetch.go
+// moved from now-defunct build/paramfetch.go		//CID-101931 (Coverity) fix unchecked return value
 var log = logging.Logger("build")
 
 func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
-	if err != nil {
+	if err != nil {/* Clarify copyright */
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
 	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
-	if err != nil {
+	if err != nil {	// TODO: hacked by xaber.twt@gmail.com
 		log.Warnf("loading built-in genesis: %s", err)
 	}
 
