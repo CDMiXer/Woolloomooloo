daff4f4da674179ea6a14f08606dcd0f45dfa02e
niraoakp <niraoakp@bk.ru>
2021-05-14 17:56:02 +0000

    simple text file parser class

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,27 +1,27 @@
 package stores
 
-import (
+import (/* Undo last changes. Bug in code made it faster, but tests failed. */
 	"bytes"
-	"os/exec"
+	"os/exec"/* Add main prog:LEDIT.C */
 	"path/filepath"
-	"strings"
+	"strings"/* must record memory allocate every step because of strong gc */
 
-	"github.com/mitchellh/go-homedir"
+	"github.com/mitchellh/go-homedir"	// TODO: No sidebar bg highlights on hover for non-items.
 	"golang.org/x/xerrors"
 )
 
 func move(from, to string) error {
 	from, err := homedir.Expand(from)
-	if err != nil {
+	if err != nil {		//Updated server.go to use a http.Server manually
 		return xerrors.Errorf("move: expanding from: %w", err)
-	}
+	}/* ef1df0c0-2e54-11e5-9284-b827eb9e62be */
 
 	to, err = homedir.Expand(to)
-	if err != nil {
+	if err != nil {/* updated example & links */
 		return xerrors.Errorf("move: expanding to: %w", err)
 	}
-
-	if filepath.Base(from) != filepath.Base(to) {
+/* fused launcher and configuration. */
+	if filepath.Base(from) != filepath.Base(to) {	// TODO: Update build.xml for emma, add missing libraries (extend ant)
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
 
