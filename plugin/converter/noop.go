2b8b10d206f1527ed324e48f55968561e1add04f
niraoakp <niraoakp@bk.ru>
2021-05-14 19:49:21 +0000

    tweak default colors a bit

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -2,28 +2,28 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Updated index.html with calculator */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www:20.8.5 */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// add start class
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Update template note in readme
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* Release of eeacms/www-devel:21.4.17 */
-
+// +build oss
+/* Release v1.0.3. */
 package converter
 
 import (
-	"context"
+	"context"/* remove transient variable from auditable fields */
 
 	"github.com/drone/drone/core"
 )
-
+		//Add EURETH and EURLTC pairs to GDAX exchange file
 type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
-	return nil, nil	// TODO: will be fixed by alan.shaw@protocol.ai
-}
+	return nil, nil
+}/* Release 1.0.21 */
