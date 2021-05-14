e91396cee735052d40a7f6b54ec4decdb0e2e771
niraoakp <niraoakp@bk.ru>
2021-05-14 15:13:49 +0000

    New ModelGenerator and tests

--- extern/storage-sealing/constants.go
+++ extern/storage-sealing/constants.go
@@ -1,4 +1,4 @@
 package sealing
-/* 82cbdb36-2e6c-11e5-9284-b827eb9e62be */
+
 // Epochs
-const InteractivePoRepConfidence = 6
+const InteractivePoRepConfidence = 6		//Use %r n error messages for token names
