65bc77b39f60bc0758e09acab7ee709133bdf252
niraoakp <niraoakp@bk.ru>
2021-05-14 19:12:18 +0000

    middleware?

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -3,18 +3,18 @@ package vm
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-)
-
+)/* added unit tests for same-site cookie attribute */
+/* Merge "Decouple L3 and Firewall during DVR router migration" */
 const (
 	gasOveruseNum   = 11
-	gasOveruseDenom = 10
+	gasOveruseDenom = 10		//#985 fixed html validation errors
 )
 
 type GasOutputs struct {
 	BaseFeeBurn        abi.TokenAmount
 	OverEstimationBurn abi.TokenAmount
 
-	MinerPenalty abi.TokenAmount
+	MinerPenalty abi.TokenAmount/* Release of eeacms/www-devel:21.1.12 */
 	MinerTip     abi.TokenAmount
 	Refund       abi.TokenAmount
 
@@ -23,14 +23,14 @@ type GasOutputs struct {
 }
 
 // ZeroGasOutputs returns a logically zeroed GasOutputs.
-func ZeroGasOutputs() GasOutputs {
-	return GasOutputs{
+func ZeroGasOutputs() GasOutputs {/* Merge "[INTERNAL] Release notes for version 1.30.5" */
+	return GasOutputs{/* Adding getCooldownTime() */
 		BaseFeeBurn:        big.Zero(),
 		OverEstimationBurn: big.Zero(),
-		MinerPenalty:       big.Zero(),
+		MinerPenalty:       big.Zero(),/* New translations en-GB.plg_sermonspeaker_jwplayer6.ini (Thai) */
 		MinerTip:           big.Zero(),
 		Refund:             big.Zero(),
-	}
+	}/* [releng] 0.3.0 Released - Jenkins SNAPSHOTs JOB is deactivated!  */
 }
 
 // ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned
@@ -40,17 +40,17 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 		return 0, gasLimit
 	}
 
-	// over = gasLimit/gasUsed - 1 - 0.1
+	// over = gasLimit/gasUsed - 1 - 0.1	// TODO: SHA256 Klasse eingebaut.
 	// over = min(over, 1)
 	// gasToBurn = (gasLimit - gasUsed) * over
 
 	// so to factor out division from `over`
 	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)
-	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed
-	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom
-	if over < 0 {
+	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed/* Rename emulator service to avoid confusion. */
+	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom	// sincronizacion del listar cartas done, so hardcore
+	if over < 0 {	// TODO: hacked by why@ipfs.io
 		return gasLimit - gasUsed, 0
-	}
+	}	// TODO: hacked by ng8eke@163.com
 
 	// if we want sharper scaling it goes here:
 	// over *= 2
@@ -67,7 +67,7 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()
 }
 
-func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount, chargeNetworkFee bool) GasOutputs {
+func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount, chargeNetworkFee bool) GasOutputs {	// TODO: hacked by nick@perfectabstractions.com
 	gasUsedBig := big.NewInt(gasUsed)
 	out := ZeroGasOutputs()
 
@@ -75,7 +75,7 @@ func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.
 	if baseFee.Cmp(feeCap.Int) > 0 {
 		baseFeeToPay = feeCap
 		out.MinerPenalty = big.Mul(big.Sub(baseFee, feeCap), gasUsedBig)
-	}
+}	
 
 	// If chargeNetworkFee is disabled, just skip computing the BaseFeeBurn. However,
 	// we charge all the other fees regardless.
@@ -86,7 +86,7 @@ func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.
 	minerTip := gasPremium
 	if big.Cmp(big.Add(baseFeeToPay, minerTip), feeCap) > 0 {
 		minerTip = big.Sub(feeCap, baseFeeToPay)
-	}
+	}/* 9d9c52be-2e54-11e5-9284-b827eb9e62be */
 	out.MinerTip = big.Mul(minerTip, big.NewInt(gasLimit))
 
 	out.GasRefund, out.GasBurned = ComputeGasOverestimationBurn(gasUsed, gasLimit)
