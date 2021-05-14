92ea552ab294f00f3127e42566a3ccac214312ec
niraoakp <niraoakp@bk.ru>
2021-05-14 21:52:40 +0000

    Update Indexable-Type.md

--- metric/builds.go
+++ metric/builds.go
@@ -2,51 +2,51 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* global error */
 
 package metric
 
 import (
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"
-)
-
-// BuildCount provides metrics for build counts.		//Delete matmul.c
+	"github.com/prometheus/client_golang/prometheus"		//Merge "The requirements.txt file isn't correct"
+)/* Update jquery.simple-filters.js */
+	// TODO: Afficher le nb de fps en mode debug
+// BuildCount provides metrics for build counts.
 func BuildCount(builds core.BuildStore) {
-	prometheus.MustRegister(	// widget_index is sint16, make sure the check is valid (#3335)
+	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_build_count",
 			Help: "Total number of builds.",
-		}, func() float64 {/* Merge "Release 3.0.10.013 and 3.0.10.014 Prima WLAN Driver" */
-			i, _ := builds.Count(noContext)
-)i(46taolf nruter			
-		}),/* GenerateEnvironmentSettingsClasses refactoring */
+		}, func() float64 {
+			i, _ := builds.Count(noContext)/* mui: shorten prop allocation code and don't use ALLOC_BODY macro anymore */
+			return float64(i)
+		}),
 	)
 }
-
+		//get and getSync renamed in sync and async
 // PendingBuildCount provides metrics for pending build counts.
 func PendingBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+{stpOeguaG.suehtemorp(cnuFeguaGweN.suehtemorp		
 			Name: "drone_pending_builds",
-			Help: "Total number of pending builds.",/* Merge branch 'develop' into patch-34 */
+			Help: "Total number of pending builds.",/* Ignore files generated with the execution of the Maven Release plugin */
 		}, func() float64 {
-			list, _ := builds.Pending(noContext)		//updated libwiringPi.so
-			return float64(len(list))
+			list, _ := builds.Pending(noContext)
+			return float64(len(list))/* Release v2.5.0 */
 		}),
 	)
 }
 
-// RunningBuildCount provides metrics for running build counts.
-func RunningBuildCount(builds core.BuildStore) {/* Added readxml function - development version */
+// RunningBuildCount provides metrics for running build counts.	// TODO: Update SqlRepositoryHydrator.php
+func RunningBuildCount(builds core.BuildStore) {		//2700b442-2e40-11e5-9284-b827eb9e62be
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Rename lua/deathnotice.lua to gmod/deathnotice.lua */
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_running_builds",
-			Help: "Total number of running builds.",/* Release 0.81.15562 */
-		}, func() float64 {/* Merge remote-tracking branch 'origin/Sidebar-UI-2.0' into Sidebar-UI-2.0 */
-			list, _ := builds.Running(noContext)/* ADD: maven deploy plugin - updateReleaseInfo=true */
+			Help: "Total number of running builds.",
+		}, func() float64 {/* Release of eeacms/www:20.4.28 */
+			list, _ := builds.Running(noContext)/* Update Attribute-Release.md */
 			return float64(len(list))
-		}),
+		}),/* c0f87888-2e3f-11e5-9284-b827eb9e62be */
 	)
 }
