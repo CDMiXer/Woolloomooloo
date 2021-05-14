8a5e803a10ec09b870852c73533d2e32ebddb999
niraoakp <niraoakp@bk.ru>
2021-05-14 12:48:20 +0000

    Add android theme color

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -3,27 +3,27 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release floating IPs on server deletion" */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: hacked by denner@gmail.com
 // limitations under the License.
 
-// +build oss
+// +build oss/* Release and Debug configurations. */
 
-package converter
-
+package converter/* Release 1. RC2 */
+		//Fixed a bug when showing last page. Added download in the background.
 import (
-	"github.com/drone/drone/core"	// TODO: will be fixed by steven@stebalien.com
+	"github.com/drone/drone/core"
 )
 
-// Memoize caches the conversion results for subsequent calls.
+// Memoize caches the conversion results for subsequent calls.	// TODO: hacked by alan.shaw@protocol.ai
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
-// pipeline execution.
-func Memoize(base core.ConvertService) core.ConvertService {	// Update the desert pyramid loot table
+// pipeline execution./* pre-built jar */
+func Memoize(base core.ConvertService) core.ConvertService {
 	return new(noop)
 }
