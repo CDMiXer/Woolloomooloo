28b490da9b8b6b4d975f05213f5c99685e425a5a
niraoakp <niraoakp@bk.ru>
2021-05-14 05:44:58 +0000

    Add EXPIRE/TTL/PERSIST familiy of commands

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -13,14 +13,14 @@ const (
 	MySQL    dbType = "mysql"
 	Postgres dbType = "postgres"
 )
-
+/* Update Beta Release Area */
 func dbTypeFor(session db.Database) dbType {
 	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
 		return MySQL
 	}
 	return Postgres
-}
+}	// added blkid support
 
 func (t dbType) intType() string {
 	if t == MySQL {
