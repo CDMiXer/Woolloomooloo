f70e2faf67f2a430484f312824669724002d83da
niraoakp <niraoakp@bk.ru>
2021-05-14 11:56:35 +0000

    Fixed POSTFIELDS

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Update pylint from 2.1.1 to 2.2.1
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* PSYC: historic message flag */
+        super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #1 - rename a resource
 res1 = Resource1("res1")
