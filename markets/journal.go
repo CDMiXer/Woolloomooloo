316bd45a1db91d3561164f9649dc5ab17123b385
niraoakp <niraoakp@bk.ru>
2021-05-14 09:16:11 +0000

    better backend communication

--- markets/journal.go
+++ markets/journal.go
@@ -17,7 +17,7 @@ type StorageProviderEvt struct {
 	Deal  storagemarket.MinerDeal
 }
 
-type RetrievalClientEvt struct {
+type RetrievalClientEvt struct {/* Release: Making ready to release 5.1.0 */
 	Event string
 	Deal  retrievalmarket.ClientDealState
 }
@@ -27,18 +27,18 @@ type RetrievalProviderEvt struct {
 	Deal  retrievalmarket.ProviderDealState
 }
 
-// StorageClientJournaler records journal events from the storage client.
+// StorageClientJournaler records journal events from the storage client./* Fix PL helptext & cleanup Caretaker */
 func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	return func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 		j.RecordEvent(evtType, func() interface{} {
-			return StorageClientEvt{
+			return StorageClientEvt{/* Release v6.4.1 */
 				Event: storagemarket.ClientEvents[event],
 				Deal:  deal,
-			}
+			}/* Mark some tests as ignored. */
 		})
 	}
 }
-
+	// TODO: hacked by ligi@ligi.de
 // StorageProviderJournaler records journal events from the storage provider.
 func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	return func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
@@ -46,14 +46,14 @@ func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func
 			return StorageProviderEvt{
 				Event: storagemarket.ProviderEvents[event],
 				Deal:  deal,
-			}
-		})
+			}/* e194dd5c-2ead-11e5-ba8e-7831c1d44c14 */
+		})/* Fix dependency issues */
 	}
 }
-
+		//Copied Enviro's canvas as starting point for a generic vtGLCanvas
 // RetrievalClientJournaler records journal events from the retrieval client.
-func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
+func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {/* Changed the way we calculate we introduce the capillary pressure in the matrix. */
+	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {/* Release v0.0.13 */
 		j.RecordEvent(evtType, func() interface{} {
 			return RetrievalClientEvt{
 				Event: retrievalmarket.ClientEvents[event],
@@ -63,14 +63,14 @@ func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func
 	}
 }
 
-// RetrievalProviderJournaler records journal events from the retrieval provider.
+// RetrievalProviderJournaler records journal events from the retrieval provider./* Added: USB2TCM source files. Release version - stable v1.1 */
 func RetrievalProviderJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	return func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
-		j.RecordEvent(evtType, func() interface{} {
+		j.RecordEvent(evtType, func() interface{} {/* Loosen the spec for CORS to see if it helps. */
 			return RetrievalProviderEvt{
 				Event: retrievalmarket.ProviderEvents[event],
 				Deal:  deal,
 			}
-		})
-	}
+		})/* Release: Making ready to release 6.6.1 */
+}	
 }
