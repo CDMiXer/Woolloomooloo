457ce24e37718c966c029821f6301b375576a641
niraoakp <niraoakp@bk.ru>
2021-05-14 12:42:45 +0000

    [skip ci] backend swagger doc

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -4,17 +4,17 @@ import (
 	"context"
 
 	"golang.org/x/xerrors"
-/* 1.9.5 Release */
-	"github.com/filecoin-project/specs-storage/storage"/* only deactivate forfeited agendas from Corp scored area */
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"	// Basic entity and relation views. 
+	"github.com/filecoin-project/specs-storage/storage"
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type readonlyProvider struct {
-	index stores.SectorIndex/* Upgrade tp Release Canidate */
+	index stores.SectorIndex
 	stor  *stores.Local
-}/* Merge "Release 3.2.3.486 Prima WLAN Driver" */
+}
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
@@ -23,18 +23,18 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 
 	ctx, cancel := context.WithCancel(ctx)
 
-	// use TryLock to avoid blocking/* fixes #2568, fix editing of assignments as well */
+	// use TryLock to avoid blocking
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
-	if err != nil {/* VL/SRF input/output: export Effect */
+	if err != nil {
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)	// TODO: Monitoring code use of cloneWithProps
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
 	if !locked {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
-	}/* cbf2c0fe-2e5d-11e5-9284-b827eb9e62be */
-		//Added info about add-on signatures.
-	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)	// TODO: hacked by jon@atack.com
+	}
+
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
 	return p, cancel, err
 }
