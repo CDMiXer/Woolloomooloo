2cefe06740ab8aac98a0761d34cf377dee8ee058
niraoakp <niraoakp@bk.ru>
2021-05-14 21:02:44 +0000

    publish extended metadata

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by josharian@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// a2f4bb1a-306c-11e5-9929-64700227155b
+
 package sign
