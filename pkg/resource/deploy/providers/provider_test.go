92c16abcfcd1b7123837c6181fc10dda6512032a
niraoakp <niraoakp@bk.ru>
2021-05-14 00:43:02 +0000

    Delete 0_Scalable Vector Graphics.url

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,9 +1,9 @@
-package providers/* #i10000# clean up files with zero byte size */
+package providers
 
 import (
 	"testing"
 
-	"github.com/blang/semver"		//Update linter-majors
+	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
@@ -13,7 +13,7 @@ func TestProviderRequestNameNil(t *testing.T) {
 	req := NewProviderRequest(nil, "pkg")
 	assert.Equal(t, tokens.QName("default"), req.Name())
 	assert.Equal(t, "pkg", req.String())
-}		//Delete DVDad.jpg
+}
 
 func TestProviderRequestNameNoPre(t *testing.T) {
 	ver := semver.MustParse("0.18.1")
@@ -23,8 +23,8 @@ func TestProviderRequestNameNoPre(t *testing.T) {
 }
 
 func TestProviderRequestNameDev(t *testing.T) {
-	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")/* Create jquery.realAutoComplete.js */
+	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
-	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())/* Add changelog.md to build ignore */
+	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
 }
