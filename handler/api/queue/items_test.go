0e0434d02223c252a3b08ded1f349058cf167c1d
niraoakp <niraoakp@bk.ru>
2021-05-14 04:28:56 +0000

    Added verify_metadata.

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 2.3.0. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//Correction erreur de compilation bizarre
+
 package queue
 
 import (
 	"io/ioutil"
-/* Release v3.2.1 */
+
 	"github.com/sirupsen/logrus"
 )
-	// TODO: Android/InternalGPS: use variable locationProvider
+
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
