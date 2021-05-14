f66104d4dff28ce3a97b8fb0e73d3504b3f44687
niraoakp <niraoakp@bk.ru>
2021-05-14 01:45:47 +0000

    Merge branch 'master' into entryviewer

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,31 +1,31 @@
 package genesis
 
-import (/* Added citation in README.md */
+import (
 	"context"
-/* Patch per file messaggi e %SST */
+
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"		//Merge "GFX api cleanup 1.5 of 2" into jb-dev
+	"github.com/filecoin-project/specs-actors/actors/builtin"		//Support labels in settings and fix a UI quirk
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
-)		//input/tidal: parse and report userMessage from error responses
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by alan.shaw@protocol.ai
+)
 
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
 
 	cst := cbor.NewCborStore(bs)
-	// Merge " Added new feature *odl-ovsdb-openstack-clusteraware*."
-	statecid, err := cst.Put(context.TODO(), &st)/* Tune button layouts */
-	if err != nil {		//small doc change
+
+	statecid, err := cst.Put(context.TODO(), &st)
+	if err != nil {
 		return nil, err
-	}	// Update pymarketcap from 3.3.150 to 3.3.152
+}	
 
 	act := &types.Actor{
-		Code: builtin.SystemActorCodeID,
+		Code: builtin.SystemActorCodeID,	// TODO: hacked by mail@bitpshr.net
 		Head: statecid,
 	}
 
-	return act, nil	// add idea conf
-}
+	return act, nil
+}/* Release 1.0.1.2 commint */
