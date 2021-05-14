921f01f8a8f4528d039b210445d0e0b4c128c9bb
niraoakp <niraoakp@bk.ru>
2021-05-14 21:13:19 +0000

    move of not yet proven to be advantaguous operator to fullinternalexperience

--- handler/api/repos/builds/logs/delete_test.go
+++ handler/api/repos/builds/logs/delete_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package logs
+package logs/* Fix another spot where this test varied for a Release build. */
