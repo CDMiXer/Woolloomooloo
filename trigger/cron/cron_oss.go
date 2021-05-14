cf533214698c5a0edfa9031e12798792cacbb9bc
niraoakp <niraoakp@bk.ru>
2021-05-14 20:00:04 +0000

    also update copy in ext dir - need to get rid of duplication some time

--- trigger/cron/cron_oss.go
+++ trigger/cron/cron_oss.go
@@ -1,43 +1,43 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* bundle-size: 83a039a706b13cbd9ea399e57cc5896a30117022 (84.31KB) */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update Version Number for Release */
-//	// TODO: hacked by ligi@ligi.de
-//      http://www.apache.org/licenses/LICENSE-2.0		//Merge "search UI: find all roots"
+// You may obtain a copy of the License at/* Print message when starting PETSc and Epetra LU solvers */
 //
-// Unless required by applicable law or agreed to in writing, software		//Implemet TODO in the constructor
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//      http://www.apache.org/licenses/LICENSE-2.0		//Update protokoll.php
+//
+// Unless required by applicable law or agreed to in writing, software/* Rename e64u.sh to archive/e64u.sh - 6th Release */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.1.13 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update c9126351.lua */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Ignore that other one. This one really makes it Java 8.
-// +build oss	// TODO: Licensed under GNU v3
 
-package cron/* Update MiniDoc on resize of main view */
+// +build oss
 
-import (	// TODO: hacked by nagydani@epointsystem.org
-	"context"/* lock with opal-rails. */
-	"time"/* [release] 1.8.0.4.p */
+package cron
+/* 92df9cac-2e66-11e5-9284-b827eb9e62be */
+import (		//issue #41 do historic population after spring initialization
+	"context"
+	"time"
 
 	"github.com/drone/drone/core"
 )
 
 // New returns a noop Cron scheduler.
-func New(/* Released version 0.4 Beta */
-	core.CommitService,
+func New(
+	core.CommitService,	// Function sendHelp moved to main class.
 	core.CronStore,
 	core.RepositoryStore,
 	core.UserStore,
-	core.Triggerer,/* COMMIT for 8/10/14 2:50 pm */
-) *Scheduler {	// TODO: hacked by alex.gaynor@gmail.com
+	core.Triggerer,
+) *Scheduler {
 	return &Scheduler{}
 }
 
 // Schedule is a no-op cron scheduler.
-type Scheduler struct{}
+type Scheduler struct{}/* Stats minor fix */
 
 // Start is a no-op.
-func (Scheduler) Start(context.Context, time.Duration) error {
+func (Scheduler) Start(context.Context, time.Duration) error {/* rename siox */
 	return nil
 }
