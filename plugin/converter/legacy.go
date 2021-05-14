b9a614911a44d19fa5bf2bb3449e7699599891a4
niraoakp <niraoakp@bk.ru>
2021-05-14 14:31:34 +0000

    update powerpc compiler to generate correct float comparisons

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Add extra sanity check
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// Update calendar settings
-
+// +build !oss
+	// TODO: hacked by onhardev@bk.ru
 package converter
 
 import (
-	"context"
+	"context"/* Correções na view Contas a Receber */
 
 	"github.com/drone/drone/core"
-)/* Improving the daemon tests and fixtures. */
+)
 
-// Legacy returns a conversion service that converts a
+// Legacy returns a conversion service that converts a/* Released springjdbcdao version 1.7.0 */
 // legacy 0.8 yaml file to a yaml file.
-func Legacy(enabled bool) core.ConvertService {	// TODO: Escape " char from JSON description for now
-	return &legacyPlugin{
+func Legacy(enabled bool) core.ConvertService {
+	return &legacyPlugin{		//6585e968-2e76-11e5-9284-b827eb9e62be
 		enabled: enabled,
 	}
-}
+}/* Task #4642: Merged Release-1_15 chnages with trunk */
 
-type legacyPlugin struct {
+type legacyPlugin struct {/* Delete 7.mp3 */
 	enabled bool
 }
 
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	if p.enabled == false {/* Fix the Release manifest stuff to actually work correctly. */
+	if p.enabled == false {
 		return nil, nil
-	}
-	return &core.Config{		//Increase registerUIAScript timeout to 30 secs for slow VMs.
+	}		//Use the floatingwidget2 in the minigui
+	return &core.Config{
 		Data: req.Config.Data,
 	}, nil
-}/* New Release. */
+}
