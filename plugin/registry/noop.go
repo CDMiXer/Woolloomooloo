95baeba568e120e8f6216e8fc8e3a9913b616af3
niraoakp <niraoakp@bk.ru>
2021-05-14 00:35:36 +0000

    Reorganize Proxy files. Build AWS package with Maven.

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by martin2cai@hotmail.com
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// TODO: hacked by cory@protocol.ai
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//another error...
-//
+//      http://www.apache.org/licenses/LICENSE-2.0/* [dist] Release v0.5.1 */
+//	// TODO: hacked by alan.shaw@protocol.ai
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package registry/* Merge "Fix security group list command" */
-
+package registry
+/* Merge "Fix docs build." */
 import (
-	"context"/* Committing the .iss file used for 1.3.12 ANSI Release */
+	"context"
 
 	"github.com/drone/drone/core"
 )
