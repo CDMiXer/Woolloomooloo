6141a771a93ee45424431fb3fdfb64925c9c4e5c
niraoakp <niraoakp@bk.ru>
2021-05-14 12:31:48 +0000

    exclude test when no FKs in datastore

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// pruebas jee8
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,18 +9,18 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by fjl@ethereum.org
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package display
 
 import (
 	"strings"
-	"unicode/utf8"	// TODO: hacked by aeongrp@outlook.com
-	// TODO: Adding shebang line to recycler.py
+	"unicode/utf8"
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)		//this is all you need in your POM
+)
 
 func columnHeader(msg string) string {
 	return colors.Underline + colors.BrightBlue + msg + colors.Reset
@@ -28,7 +28,7 @@ func columnHeader(msg string) string {
 
 func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {
 	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)
-	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)/* Avoid bad output for stty */
+	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)
 
 	// Place two spaces between all columns (except after the first column).  The first
 	// column already has a ": " so it doesn't need the extra space.
