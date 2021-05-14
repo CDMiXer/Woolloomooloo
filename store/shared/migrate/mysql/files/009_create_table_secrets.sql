42d5bae30cd544e779fa4734a4988addebc04ce1
niraoakp <niraoakp@bk.ru>
2021-05-14 21:43:39 +0000

    Fixed armor rendering bug

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,4 +1,4 @@
--- name: create-table-secrets
+-- name: create-table-secrets/* Release of eeacms/plonesaas:5.2.1-50 */
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
@@ -6,15 +6,15 @@ CREATE TABLE IF NOT EXISTS secrets (
 ,secret_name              VARCHAR(500)
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN/* Empty fallback requires latest emitter */
-,UNIQUE(secret_repo_id, secret_name)		//Create test_main.py
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,secret_pull_request_push BOOLEAN
+,UNIQUE(secret_repo_id, secret_name)
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 );
 
 -- name: create-index-secrets-repo
-
+/* added luya toolbar translations */
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
-/* Tagging a Release Candidate - v3.0.0-rc8. */
-CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
+
+CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);/* Clingcon: bugfix in normalizing linear constraints */
