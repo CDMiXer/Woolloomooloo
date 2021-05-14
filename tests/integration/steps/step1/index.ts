04f2b37bd3106b16a290525028ef0f673a56ed5f
niraoakp <niraoakp@bk.ru>
2021-05-14 22:30:43 +0000

    Update smile_examples_inference.json

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -3,9 +3,9 @@
 import { Resource } from "./resource";
 
 // Step 1: Populate the world:
-// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.		//fix missing ()
 let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
 let c = new Resource("c", { state: 1, resource: a });
-let d = new Resource("d", { state: 1 });
+let d = new Resource("d", { state: 1 });/* Update p1-06.html */
 // Checkpoint: a1, b1, c1, d1
