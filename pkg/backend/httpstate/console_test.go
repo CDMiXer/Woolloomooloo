6272044e0dcefe135084b1edfa2416f26b0a85ee
niraoakp <niraoakp@bk.ru>
2021-05-14 16:11:36 +0000

    Adding macdeploy script to make stand alone app under mac OSX

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//16021818-2e5c-11e5-9284-b827eb9e62be
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge branch 'master' into mtageld-dev */
-//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Rename Reflection 0 to reflection0.md */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Fix off-by-one bug in stripping terminator from string */
+//
+// Unless required by applicable law or agreed to in writing, software	// TODO: aniadido horario de checkout
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Clarified option merging
+dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
 // limitations under the License.
 package httpstate
 
@@ -23,23 +23,23 @@ import (
 func TestConsoleURL(t *testing.T) {
 	t.Run("HonorEnvVar", func(t *testing.T) {
 		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
-		defer func() {
+		defer func() {		//keep it simple for now will add back later
 			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)
 		}()
 
-		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.
+		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.	// TODO: hacked by lexy8russo@outlook.com
 		os.Setenv("PULUMI_CONSOLE_DOMAIN", "pulumi-console.contoso.com")
 		assert.Equal(t,
 			"https://pulumi-console.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 
 		// Unset the variable, confirm the "standard behavior" where we
-		// replace "api." with "app.".		//make ADEngineBlock work with cocoapods
+		// replace "api." with "app.".
 		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")
 		assert.Equal(t,
-			"https://app.pulumi.contoso.com/1/2",/* Release 4.0.2dev */
+			"https://app.pulumi.contoso.com/1/2",	// TODO: Merge "Work around elasticsearch error"
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
-	})	// TODO: will be fixed by alan.shaw@protocol.ai
+	})
 
 	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
 		assert.Equal(t,
@@ -47,18 +47,18 @@ func TestConsoleURL(t *testing.T) {
 			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
 
 		assert.Equal(t,
-			"http://app.pulumi.example.com/pulumi-bot/my-stack",/* Release making ready for next release cycle 3.1.3 */
+			"http://app.pulumi.example.com/pulumi-bot/my-stack",
 			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
-	})
+	})	// TODO: [TRAVIS] Try to fix docker run invocation
 
-	t.Run("LocalDevelopment", func(t *testing.T) {
+	t.Run("LocalDevelopment", func(t *testing.T) {	// TODO: Add support for "not" operator in mixin guard conditions.
 		assert.Equal(t,
-			"http://localhost:3000/pulumi-bot/my-stack",
+			"http://localhost:3000/pulumi-bot/my-stack",	// GBX and GBP currencies (SF bug 1712966)
 			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))
-	})	// TODO: About dialog moves to an activity.
+	})	// TODO: hacked by hello@brooklynzelenka.com
 
 	t.Run("ConsoleDomainUnknown", func(t *testing.T) {
 		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))
 		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))
-	})	// TODO: Change in describing terms for being newly arrived
-}	// TODO: Parser, ParserState
+	})
+}/* Release note updated for V1.0.2 */
