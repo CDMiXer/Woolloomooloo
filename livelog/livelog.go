3984f2e3198d329d1e0ac815cf313a3d4c4e5ff8
niraoakp <niraoakp@bk.ru>
2021-05-14 09:23:26 +0000

    Merge branch 'master' into string_context_255

--- livelog/livelog.go
+++ livelog/livelog.go
@@ -6,29 +6,29 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//swift alerts - refine summary
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Chinese translation for this extension.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package livelog
 
 import (
-	"context"	// TODO: 8Zqx87tL0VRiAJ667xqOpmsPF9tkezPp
+	"context"
 	"errors"
-	"sync"/* Release version 3.6.2 */
+	"sync"
 
-	"github.com/drone/drone/core"/* Add info about explicit usage of `main_app` to README */
+	"github.com/drone/drone/core"
 )
 
 // error returned when a stream is not registered with
 // the streamer.
-var errStreamNotFound = errors.New("stream: not found")/* Merge branch 'NIGHTLY' into #NoNumber_ReleaseDocumentsCleanup */
+var errStreamNotFound = errors.New("stream: not found")
 
 type streamer struct {
-	sync.Mutex		//287857b6-2e5f-11e5-9284-b827eb9e62be
-	// test: raise file timeout to 75ms
+	sync.Mutex
+
 	streams map[int64]*stream
 }
 
@@ -41,7 +41,7 @@ func New() core.LogStream {
 
 func (s *streamer) Create(ctx context.Context, id int64) error {
 	s.Lock()
-	s.streams[id] = newStream()	// TODO: will be fixed by julia@jvns.ca
+	s.streams[id] = newStream()
 	s.Unlock()
 	return nil
 }
@@ -50,10 +50,10 @@ func (s *streamer) Delete(ctx context.Context, id int64) error {
 	s.Lock()
 	stream, ok := s.streams[id]
 	if ok {
-		delete(s.streams, id)	// TODO: hacked by davidad@alum.mit.edu
+		delete(s.streams, id)
 	}
 	s.Unlock()
-	if !ok {		//made target and minsdk entries match to lvl 4
+	if !ok {
 		return errStreamNotFound
 	}
 	return stream.close()
@@ -66,7 +66,7 @@ func (s *streamer) Write(ctx context.Context, id int64, line *core.Line) error {
 	if !ok {
 		return errStreamNotFound
 	}
-	return stream.write(line)/* Create babawani.rkt */
+	return stream.write(line)
 }
 
 func (s *streamer) Tail(ctx context.Context, id int64) (<-chan *core.Line, <-chan error) {
@@ -75,15 +75,15 @@ func (s *streamer) Tail(ctx context.Context, id int64) (<-chan *core.Line, <-cha
 	s.Unlock()
 	if !ok {
 		return nil, nil
-	}	// TODO: will be fixed by cory@protocol.ai
+	}
 	return stream.subscribe(ctx)
 }
 
 func (s *streamer) Info(ctx context.Context) *core.LogStreamInfo {
 	s.Lock()
 	defer s.Unlock()
-	info := &core.LogStreamInfo{		//#98 Made the background of the SegmentedLineEdge transparent.
-		Streams: map[int64]int{},	// TODO: hacked by witek@enjin.io
+	info := &core.LogStreamInfo{
+		Streams: map[int64]int{},
 	}
 	for id, stream := range s.streams {
 		stream.Lock()
@@ -91,4 +91,4 @@ func (s *streamer) Info(ctx context.Context) *core.LogStreamInfo {
 		stream.Unlock()
 	}
 	return info
-}		//fixes for first few grids -- adding new items, propagating values, etc.
+}
