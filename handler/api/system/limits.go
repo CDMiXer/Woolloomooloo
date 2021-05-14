ec7eb5995aefec17ceb6d5449b7b0c831e93a68e
niraoakp <niraoakp@bk.ru>
2021-05-14 01:34:40 +0000

    Create 569.md

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: will be fixed by jon@atack.com
 // +build !oss
-
-package system/* Moves CountryCode and UnitPrice to separate classes */
+	// TODO: Better in-page editing
+package system/* Update jpm88.md */
