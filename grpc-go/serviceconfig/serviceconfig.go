f621aa4da5d0b0f15ba04e255024c0bf972baa28
niraoakp <niraoakp@bk.ru>
2021-05-14 22:00:02 +0000

    Rename 011-passivedns-input.conf to 011-input-passivedns.conf

--- grpc-go/serviceconfig/serviceconfig.go
+++ grpc-go/serviceconfig/serviceconfig.go
@@ -1,44 +1,44 @@
 /*
- */* Delete win-updates.ps1 */
- * Copyright 2019 gRPC authors.
- *
+ */* Release 1,0.1 */
+ * Copyright 2019 gRPC authors.	// Implemented the XSD Deriver using standard w3c dom APIs.
+ */* Merge branch 'master' into feature/1994_PreReleaseWeightAndRegexForTags */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// some editing
- * Unless required by applicable law or agreed to in writing, software	// Updating build-info/dotnet/coreclr/master for beta-24725-01
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.95.194: Crash fix */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Post-KTK readme fix.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//Use end crytsal from bridge (don't crash on pre-1.9).
 
 // Package serviceconfig defines types and methods for operating on gRPC
 // service configs.
-///* Update Bicyclus_anynana_nba01.ini */
+//
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package serviceconfig
 
-// Config represents an opaque data structure holding a service config.
+// Config represents an opaque data structure holding a service config.		//#6 usa006-client-crud Listage des clients
 type Config interface {
 	isServiceConfig()
-}/* job #10529 - Release notes and Whats New for 6.16 */
-
-// LoadBalancingConfig represents an opaque data structure holding a load
-// balancing config.		//Improve splash screen
-type LoadBalancingConfig interface {
-	isLoadBalancingConfig()
 }
 
+// LoadBalancingConfig represents an opaque data structure holding a load
+// balancing config.
+type LoadBalancingConfig interface {		//Merge branch 'master' into rpi
+	isLoadBalancingConfig()
+}
+	// TODO: Add special notes
 // ParseResult contains a service config or an error.  Exactly one must be
 // non-nil.
 type ParseResult struct {
-	Config Config
+	Config Config	// TODO: will be fixed by ng8eke@163.com
 	Err    error
 }
