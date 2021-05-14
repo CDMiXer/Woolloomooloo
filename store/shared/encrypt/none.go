3cd330812b28dbd11bd876a946b9d9288e9c037b
niraoakp <niraoakp@bk.ru>
2021-05-14 13:53:10 +0000

    Fixed #2 - jersey.first rest service (changed packagename).

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* OgreRenderSystemCapabilities: cleanup and deprecation */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Updated the r-smoof feedstock.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Throw global exception rather then undefined class.
+
 package encrypt
 
 // none is an encryption strategy that stores secret
 // values in plain text. This is the default strategy
-// when no key is specified./* Merge "Release notes for 1.18" */
+// when no key is specified.
 type none struct {
 }
-/* Release 0.1 */
-func (*none) Encrypt(plaintext string) ([]byte, error) {/* d43117ba-2e6d-11e5-9284-b827eb9e62be */
-	return []byte(plaintext), nil	// TODO: Fix a small grammar issue.
+
+func (*none) Encrypt(plaintext string) ([]byte, error) {
+	return []byte(plaintext), nil
 }
 
-func (*none) Decrypt(ciphertext []byte) (string, error) {		//fixup exports, doc
+func (*none) Decrypt(ciphertext []byte) (string, error) {
 	return string(ciphertext), nil
 }
