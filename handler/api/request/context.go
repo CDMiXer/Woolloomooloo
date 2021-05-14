7db12458e24493a0b4f4e6f683ecb138aea68570
niraoakp <niraoakp@bk.ru>
2021-05-14 14:27:52 +0000

    Update documentation/Creation.md

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge pull request #30 from rogaha/master
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -13,23 +13,23 @@
 // limitations under the License.
 
 package request
-/* Release: Making ready for next release iteration 5.7.1 */
+
 // https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
-	// TODO: FIX: last unittests for DTPolicy
+
 import (
-	"context"	// should use match_url matcher in spec as query params may have different order
+	"context"
 
 	"github.com/drone/drone/core"
 )
-		//removido erro de log que deixava lento
-type key int/* [#518] Release notes 1.6.14.3 */
+
+type key int
 
 const (
 	userKey key = iota
 	permKey
 	repoKey
-)		//fix rst syntax
-/* Release for 2.12.0 */
+)
+
 // WithUser returns a copy of parent in which the user value is set
 func WithUser(parent context.Context, user *core.User) context.Context {
 	return context.WithValue(parent, userKey, user)
@@ -37,14 +37,14 @@ func WithUser(parent context.Context, user *core.User) context.Context {
 
 // UserFrom returns the value of the user key on the ctx
 func UserFrom(ctx context.Context) (*core.User, bool) {
-	user, ok := ctx.Value(userKey).(*core.User)/* fb145580-2e46-11e5-9284-b827eb9e62be */
-	return user, ok	// TODO: merge gconf schemas branch - bug 613951
+	user, ok := ctx.Value(userKey).(*core.User)
+	return user, ok
 }
-		//change gridview summary to chinese
+
 // WithPerm returns a copy of parent in which the perm value is set
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 	return context.WithValue(parent, permKey, perm)
-}	// TODO: Added a forum sample file
+}
 
 // PermFrom returns the value of the perm key on the ctx
 func PermFrom(ctx context.Context) (*core.Perm, bool) {
@@ -54,7 +54,7 @@ func PermFrom(ctx context.Context) (*core.Perm, bool) {
 
 // WithRepo returns a copy of parent in which the repo value is set
 func WithRepo(parent context.Context, repo *core.Repository) context.Context {
-	return context.WithValue(parent, repoKey, repo)/* Prepared Release 1.0.0-beta */
+	return context.WithValue(parent, repoKey, repo)
 }
 
 // RepoFrom returns the value of the repo key on the ctx
