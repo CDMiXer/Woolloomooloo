f5670a48c4088872f4edc6b6ebaf270d4862e740
niraoakp <niraoakp@bk.ru>
2021-05-14 23:20:30 +0000

    Remove re-generated IDEA files

--- handler/api/repos/builds/pulls/list.go
+++ handler/api/repos/builds/pulls/list.go
@@ -1,59 +1,59 @@
-// Copyright 2019 Drone IO, Inc.
-//	// Update, the site is fixed now!
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Release version 0.1, with the test project */
-//      http://www.apache.org/licenses/LICENSE-2.0
+// Copyright 2019 Drone IO, Inc./* trigger option:select as soon as the reminders view renders */
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Factorize the class method
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//configure.ac : Use  instead of .
+// You may obtain a copy of the License at
+///* hot fix for lightbox */
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* * Codelite Release configuration set up */
+// Unless required by applicable law or agreed to in writing, software	// TODO: Linting Modifications
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Improving motion event converters
 
 package pulls
 
-import (	// TODO: will be fixed by vyzo@hackzen.org
-	"net/http"		//Board_ID usage implemented in BE
-
-	"github.com/drone/drone/core"
+import (		//Add .watchr file
+	"net/http"
+/* Correção mínima em Release */
+	"github.com/drone/drone/core"/* Corrected TypeScript definitions */
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Released version 0.4 Beta */
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
-func HandleList(
+func HandleList(		//-make tests less verbose if they pass, also remove dependency on src/plugins/
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* 2e642648-2e58-11e5-9284-b827eb9e62be */
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: hacked by aeongrp@outlook.com
-			render.NotFound(w, err)
+		if err != nil {	// TODO: hacked by ng8eke@163.com
+			render.NotFound(w, err)/* 81cf0d9e-2d15-11e5-af21-0401358ea401 */
 			logger.FromRequest(r).
-				WithError(err).		//Update to Readme.
+				WithError(err).	// TODO: Merge "[FAB-13469] consistently use 127.0.0.1"
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-/* Release of eeacms/energy-union-frontend:1.7-beta.0 */
+
 		results, err := builds.LatestPulls(r.Context(), repo.ID)
 		if err != nil {
-			render.InternalError(w, err)/* hapus 404 not found */
-			logger.FromRequest(r)./* - Working on agreements */
+			render.InternalError(w, err)
+			logger.FromRequest(r).	// TODO: will be fixed by fjl@ethereum.org
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name)./* Update dependency react-flip-move to v3.0.3 */
-				Debugln("api: cannot list builds")
+				WithField("name", name)./* Test: Fix NPE on parsing Byte values when executing via PG */
+				Debugln("api: cannot list builds")/* Release 3.6.0 */
 		} else {
 			render.JSON(w, results, 200)
 		}
