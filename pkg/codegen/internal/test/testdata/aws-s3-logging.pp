b5685f1276eec795a021a788a03c6834ab3c16a1
niraoakp <niraoakp@bk.ru>
2021-05-14 22:09:14 +0000

    1394304c-2e67-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}
+resource logs "aws:s3:Bucket" {}		//f9c0ea8e-2e4d-11e5-9284-b827eb9e62be
 
 resource bucket "aws:s3:Bucket" {
-	loggings = [{
-		targetBucket = logs.bucket,		//Don't try to acquire lock if we do not have a source anymore.
-	}]/* Merge "Release 4.0.10.80 QCACLD WLAN Driver" */
+	loggings = [{	// Issue 256: No versions in svn trunk yet.
+		targetBucket = logs.bucket,
+	}]
 }
 
 output targetBucket {
-	value = bucket.loggings[0].targetBucket	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	value = bucket.loggings[0].targetBucket/* Release version: 0.6.7 */
 }
