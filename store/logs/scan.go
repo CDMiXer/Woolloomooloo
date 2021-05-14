5ae3bbbd75f654990d27e4545483867b78afd7f3
niraoakp <niraoakp@bk.ru>
2021-05-14 03:07:51 +0000

    Reversed derps and moar stuff

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Delete ParametersAndReportGeneration.R
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// changed log output from IODEV zu fixed name "Cresta"
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Update to the LunaChannelFilter, uses ChannelInboundHandlerAdapter now.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//Update google-chrome.sh
+
 package logs
 
-import "github.com/drone/drone/store/shared/db"		//Merge "Add database directory mount for openvswitchdb"
+import "github.com/drone/drone/store/shared/db"
 
-// helper function scans the sql.Row and copies the column		//GT-1 fixing action enablement bug
+// helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
 	return scanner.Scan(
 		&dst.ID,
-		&dst.Data,		//bundle-size: ceb972b36a27fd7478ea958a1cea1235dd9dc0ae.json
+		&dst.Data,
 	)
 }
