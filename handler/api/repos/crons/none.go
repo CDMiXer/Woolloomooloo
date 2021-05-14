9a7675f73c139b398b772a4f982f4b2ec1461353
niraoakp <niraoakp@bk.ru>
2021-05-14 20:17:52 +0000

    New signal added for cancel button

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release: Making ready for next release iteration 6.1.2 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// Added year of consolidation to faolex document model
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -13,41 +13,41 @@
 // limitations under the License.
 
 // +build oss
-
+		//c4c12712-2e67-11e5-9284-b827eb9e62be
 package crons
-
+/* Release version: 1.2.0.5 */
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-
+	// TODO: Changed arraylist objects to sets
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-
-func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
+		//Seeds: rend la sortie par défaut plus concise
+func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {	// TODO: Deleted Ny design och bildspråk
 	return notImplemented
 }
 
-func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented
+func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* Create 'Branch test' file */
+	return notImplemented/* These can go now */
 }
-
+	// TODO: Dangling pointer fix
 func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented/* Release notes are updated for version 0.3.2 */
 }
 
 func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
-
+/* SearchAsyncOperation: aboutToRun -> running */
 func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented	// TODO: working version of intersection and wrapper
 }
 
 func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,
-	core.CommitService, core.Triggerer) http.HandlerFunc {
+	core.CommitService, core.Triggerer) http.HandlerFunc {		//-define gnsrecord plugin for DNS
 	return notImplemented
 }
