e86bfe6e094e7e0317dfa372d40b41482ba74930
niraoakp <niraoakp@bk.ru>
2021-05-14 18:25:10 +0000

    added new conversion templates

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved./* Release 8.1.0 */
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,10 +6,10 @@
 
 package websocket
 
-import "net"		//Delete inbox.qml
+import "net"
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
-	b := net.Buffers(bufs)		//Merge branch 'master' into 724_fix_hasSelection
+	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
 	return err
 }
