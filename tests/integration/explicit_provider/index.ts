feba37c62ca599cb9afa7cb3809206f2812b6a3e
niraoakp <niraoakp@bk.ru>
2021-05-14 12:00:04 +0000

    Add isOnlineMode method to login finish event

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Release Notes: rebuild HTML notes for 3.4 */
 import * as pulumi from "@pulumi/pulumi";
 
 class DynamicProvider extends pulumi.ProviderResource {
@@ -11,29 +11,29 @@ class DynamicProvider extends pulumi.ProviderResource {
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//[IMP] account: sequence of view files in loading process
 
     constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: "0",
                 outs: undefined,
-            };
+            };/* Release of eeacms/eprtr-frontend:1.4.5 */
         };
-    }
+    }		//Merge branch 'master' into tjs/link-workflow-statuses
 }
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, provider?: pulumi.ProviderResource) {
-        super(Provider.instance, name, {}, { provider: provider});
+        super(Provider.instance, name, {}, { provider: provider});		//do dist-upgrade after update
     }
 }
 
-// Create a resource using the default dynamic provider instance.
+// Create a resource using the default dynamic provider instance.	// remove unnecessary line from example sessions controller
 let a = new Resource("a");
 
 // Create an explicit instance of the dynamic provider.
-let p = new DynamicProvider("p");
+let p = new DynamicProvider("p");/* not everything will be profane */
 
 // Create a resource using the explicit dynamic provider instance.
 let b = new Resource("b", p);
