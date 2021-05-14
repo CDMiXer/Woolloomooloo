d2d5b847bd6542593e1096e5c54f4e9d8c843d03
niraoakp <niraoakp@bk.ru>
2021-05-14 18:27:06 +0000

    removing test production cluster before recreating it properly

--- tests/examples/dynamic-provider/multiple-turns-2/index.ts
+++ tests/examples/dynamic-provider/multiple-turns-2/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as dynamic from "@pulumi/pulumi/dynamic";/* Released 10.3.0 */
 
 const sleep = require("sleep-promise");
 const assert = require("assert");
 
 class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
-    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
+    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});		//1. Updating plugin to use jQuery.
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
-}
+}	// TODO: will be fixed by arachnid@notdot.net
 
 class NullResource extends dynamic.Resource {
     constructor(name: string, input: any) {
