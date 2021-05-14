a394f6985f5af588d6ccfc6666425e6bfe50cb2b
niraoakp <niraoakp@bk.ru>
2021-05-14 17:32:18 +0000

    Return EPerm for not found machines

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure";	// TODO: hacked by boringland@protonmail.ch
+import * as azure from "@pulumi/azure";
 
-const config = new pulumi.Config();	// Update html-parser.js
+const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
-    name: resourceGroupNameParam,/* Clean up art contest less/coffee and add links to entrants. */
+    name: resourceGroupNameParam,
 });
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
-const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";		//Accuracy update
+const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
     accountKind: "StorageV2",
-    location: locationParam,/* doc: add Pimple in credits */
+    location: locationParam,
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
-    accountReplicationType: storageAccountTypeReplicationParam,/* Release notes for v0.13.2 */
+    accountReplicationType: storageAccountTypeReplicationParam,
 });
 export const storageAccountNameOut = storageAccountResource.name;
