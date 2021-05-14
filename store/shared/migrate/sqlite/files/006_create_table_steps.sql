0b1c4d2a33f6cce1c949c5b94325e8211d114b4f
niraoakp <niraoakp@bk.ru>
2021-05-14 06:26:57 +0000

    show_group/user_projectcontent removed (unused file)

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,21 +1,21 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,step_stage_id    INTEGER/* Rebuilt index with githubmin */
+CREATE TABLE IF NOT EXISTS steps (	// TODO: hacked by sebastian.tharakan97@gmail.com
+ step_id          INTEGER PRIMARY KEY AUTOINCREMENT	// TODO: hacked by timnugent@gmail.com
+,step_stage_id    INTEGER/* Added missing modifications to ReleaseNotes. */
 ,step_number      INTEGER
 ,step_name        TEXT
-,step_status      TEXT		//1.0-rc4 tag.
+,step_status      TEXT/* Release '0.1~ppa12~loms~lucid'. */
 ,step_error       TEXT
-,step_errignore   BOOLEAN
-,step_exit_code   INTEGER/* Release notes for 1.0.24 */
+,step_errignore   BOOLEAN		//Update RS_PHOTO before propagating in icon_activated().
+,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER
+,step_stopped     INTEGER/* loading up translations */
 ,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)
-,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE	// TODO: will be fixed by remco@dutchcoders.io
-);/* More touch-friendly controls, closes #19 */
+,UNIQUE(step_stage_id, step_number)		//[:reply_object]
+,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
+);/* [Release] Version bump. */
 
--- name: create-index-steps-stage	// TODO: will be fixed by qugou1350636@126.com
-
+-- name: create-index-steps-stage
+		//underscores ended
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
