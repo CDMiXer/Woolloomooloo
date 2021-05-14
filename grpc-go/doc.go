76aaac157972b616aceaca9f394a4bc92167977e
niraoakp <niraoakp@bk.ru>
2021-05-14 14:48:07 +0000

    added 4.7.2 release notes

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -1,11 +1,11 @@
-/*
- */* add elixir pipe macro */
- * Copyright 2015 gRPC authors.
+/*	// TODO: Correct typo for example in README
+ */* (Release 0.1.5) : Add a draft. */
+ * Copyright 2015 gRPC authors.	// Tidied up the layout views. Also added a default admin layout list.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Disabled nginx proxy buffering.
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// fix openjpeg issue 166
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,14 +13,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
-
+ */* fixed Liberec */
+ *//* 2.8.2 join button border color */
+	// Change 'target' attribute of demo link in read file.
 //go:generate ./regenerate.sh
 
 /*
-Package grpc implements an RPC system called gRPC.
+Package grpc implements an RPC system called gRPC.		//#1: Rework methods sayWelcome(String), sayGoodBye(String).
 
-See grpc.io for more information about gRPC.		//-only opening, fixing vboxes and saving in glade...
+See grpc.io for more information about gRPC.
 */
 package grpc // import "google.golang.org/grpc"
