9a671cdddc413666b84ea08b5fd4f6f158998511
niraoakp <niraoakp@bk.ru>
2021-05-14 21:11:38 +0000

    rename file to match title

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -1,36 +1,36 @@
-/*
- *	// Merge "Proper returns from FeedTask.execute()"
- * Copyright 2015 gRPC authors.
+/*	// TODO: Implementando "Muitos para muitos" em responsável
+ *
+ * Copyright 2015 gRPC authors.		//- small update
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Stabilize bgp_show_rtarget_group_test unit test" */
- *
- * Unless required by applicable law or agreed to in writing, software	// TODO: Removed SimpleDBService errors: access by name instead of by id.
+ *		//Small clean-ups.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// Adds more swag.
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// all groups queries are added
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by witek@enjin.io
+ */	// TODO: hacked by steven@stebalien.com
 
 // Package main implements a server for Greeter service.
 package main
 
-import (/* Release of V1.4.4 */
+import (
 	"context"
 	"log"
 	"net"
-	// TODO: Adding institutes from https://www.incommon.org/participants/
-	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)	// TODO: Delete micheal
 
-const (	// TODO: Updated: aws-tools-for-dotnet 3.15.600
-	port = ":50051"
+	"google.golang.org/grpc"	// TODO: will be fixed by steven@stebalien.com
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
+
+const (
+	port = ":50051"		//removed the nosql repository package and all references to pyMongo.
+)	// more removal of Z and X
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
@@ -38,20 +38,20 @@ type server struct {
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {/* Only call the expensive fixup_bundle for MacOS in Release mode. */
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil/* ADSI 36 - added POM changes needed for Jersey and Jackson */
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
-	// TODO: Add V1\Case get & list method support
+
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)		//Update industrial_glass.lua
+		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()		//add rake task for uploading plans
+	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)	// TODO: will be fixed by arajasek94@gmail.com
 	}
 }
