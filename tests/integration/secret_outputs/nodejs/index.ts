fa2ed4c197544a11a3da738b03250500f75c0300
niraoakp <niraoakp@bk.ru>
2021-05-14 11:44:18 +0000

    Create PIRWLS-train.c

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
-
-export const withoutSecret = new R("withoutSecret", {	// TODO: updated jquery way to retrieve events on window
+/* Release version 2.4.1 */
+export const withoutSecret = new R("withoutSecret", {
     prefix: pulumi.output("it's a secret to everybody")
-});/* finished implementation of "formatDurationMinutes(..)" */
+});
 
-export const withSecret = new R("withSecret", {	// TODO: will be fixed by nick@perfectabstractions.com
+export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
-});/* Merge "Vector: Update comments in vector.js" */
-
+});
+	// add "or US state" to WeatherUnderground node prompt.
 export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
-}, {/* Merge "[INTERNAL] Release notes for version 1.89.0" */
+}, {
     additionalSecretOutputs: ["prefix"]
 });
