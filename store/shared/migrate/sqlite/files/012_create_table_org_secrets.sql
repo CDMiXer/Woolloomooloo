0f7af82d7cf883de6f22997a453b8d4f85d45f75
niraoakp <niraoakp@bk.ru>
2021-05-14 19:01:48 +0000

    Fixes SnmpWriteControl.

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,7 +1,7 @@
 -- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT		//Upgrade excon to latest.
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS orgsecrets (
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);
+);	// Fixing use of exeext
