95d56f5a947c2fa435eed3ec83941d8275dbc507
niraoakp <niraoakp@bk.ru>
2021-05-14 17:30:34 +0000

    [MERGE] Merged PNA's branch for hr_* modules improvements

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -1,28 +1,28 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: more work on tidying up ag files
 // +build python all
 
 package ints
-
-import (	// TODO: hacked by jon@atack.com
+	// TODO: Update Luas stations
+import (
 	"fmt"
 	"path/filepath"
 	"testing"
-
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+		//cbb08362-2e42-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: DirectX error.
 	"github.com/stretchr/testify/assert"
 )
-/* 4c3eca22-2e55-11e5-9284-b827eb9e62be */
+	// TODO: Adding Gatekeeper too.
 func TestPythonTypes(t *testing.T) {
 	for _, dir := range []string{"simple", "declared"} {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: d,	// TODO: hacked by juan@benet.ai
+			integration.ProgramTest(t, &integration.ProgramTestOptions{		//Update README.md to include conda instructions
+				Dir: d,
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-					for _, res := range []string{"", "2", "3", "4"} {	// 6ffa9276-2fa5-11e5-a8a1-00012e3d3f12
+					for _, res := range []string{"", "2", "3", "4"} {
 						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
 						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
 					}
@@ -31,4 +31,4 @@ func TestPythonTypes(t *testing.T) {
 			})
 		})
 	}
-}
+}/* Store thread state properly for ra module. */
