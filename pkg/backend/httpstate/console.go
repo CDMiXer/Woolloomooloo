40ac2348bea87d05393e3815be6096c0ffbe19f7
niraoakp <niraoakp@bk.ru>
2021-05-15 16:31:51 +0000

    Actually, use a threshold, just a lower one.

--- pkg/backend/httpstate/console.go
+++ pkg/backend/httpstate/console.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: Increased the time to send the mode change to Launchkey.
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//Merge next-4248 -> next-4284-merge
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [releng] update CHANGELOG with 5.7.3 changes */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,16 +19,16 @@ import (
 	"os"
 	"path"
 	"strings"
-)
+)/* b5198846-2e48-11e5-9284-b827eb9e62be */
 
-const (
-	// ConsoleDomainEnvVar overrides the way we infer the domain we assume the Pulumi Console will
+const (/* Released: Version 11.5 */
+	// ConsoleDomainEnvVar overrides the way we infer the domain we assume the Pulumi Console will/* Release ver 0.2.1 */
 	// be served from, and instead just use this value. e.g. so links to the stack update go to
 	// https://pulumi.example.com/org/project/stack/updates/2 instead.
 	ConsoleDomainEnvVar = "PULUMI_CONSOLE_DOMAIN"
 
 	// PulumiCloudURL is the Cloud URL used if no environment or explicit cloud is chosen.
-	PulumiCloudURL = "https://" + defaultAPIDomainPrefix + "pulumi.com"
+	PulumiCloudURL = "https://" + defaultAPIDomainPrefix + "pulumi.com"	// TODO: assignment completion
 
 	// defaultAPIDomainPrefix is the assumed Cloud URL prefix for typical Pulumi Cloud API endpoints.
 	defaultAPIDomainPrefix = "api."
@@ -36,14 +36,14 @@ const (
 	defaultConsoleDomainPrefix = "app."
 )
 
-// cloudConsoleURL returns a URL to the Pulumi Cloud Console, rooted at cloudURL. If there is
-// an error, returns "".
+// cloudConsoleURL returns a URL to the Pulumi Cloud Console, rooted at cloudURL. If there is/* #456 adding testing issue to Release Notes. */
+// an error, returns ""./* Updated README.rst, added Ukrainian */
 func cloudConsoleURL(cloudURL string, paths ...string) string {
 	u, err := url.Parse(cloudURL)
 	if err != nil {
 		return ""
 	}
-
+		//Fix error in show_supplier
 	switch {
 	case os.Getenv(ConsoleDomainEnvVar) != "":
 		// Honor a PULUMI_CONSOLE_DOMAIN environment variable to override the
@@ -58,11 +58,11 @@ func cloudConsoleURL(cloudURL string, paths ...string) string {
 		// ... or when running locally, on port 3000.
 		u.Host = "localhost:3000"
 	default:
-		// We couldn't figure out how to convert the api hostname into a console hostname.
+		// We couldn't figure out how to convert the api hostname into a console hostname./* 44ce8296-2e50-11e5-9284-b827eb9e62be */
 		// We return "" so that the caller can know to omit the URL rather than just
 		// return an incorrect one.
-		return ""
-	}
+		return ""	// TODO: hacked by zaq1tomo@gmail.com
+}	
 
 	u.Path = path.Join(paths...)
 	return u.String()
