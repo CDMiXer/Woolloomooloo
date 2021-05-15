477298041e162ee663b31268b4535dd7e80d1854
niraoakp <niraoakp@bk.ru>
2021-05-15 23:45:24 +0000

    Old unit tests removed

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
 package metrics
 
-import (	// TODO: Set the statusbar style in style.qss
+import (
 	"net/http"
-	_ "net/http/pprof"
+	_ "net/http/pprof"	// TODO: Update artigo_postgresql_regexp_replace_cpf.sql
 
-	"contrib.go.opencensus.io/exporter/prometheus"
+	"contrib.go.opencensus.io/exporter/prometheus"	// Windows Build README
 	logging "github.com/ipfs/go-log/v2"
-	promclient "github.com/prometheus/client_golang/prometheus"
+	promclient "github.com/prometheus/client_golang/prometheus"		//Update Phrases-01.md
 )
-/* Release 1.10.6 */
-var log = logging.Logger("metrics")
 
-func Exporter() http.Handler {/* pick me up and some changes on checkout */
+var log = logging.Logger("metrics")
+/* Merge "[INTERNAL] Release notes for version 1.40.3" */
+func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
-	// OpenCensus exporter expects a concrete *Registry. The concrete type of/* chore(package): update oauth-sign to version 0.9.0 */
+	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
-	// defensive in case things change under the hood.	// TODO: will be fixed by alan.shaw@protocol.ai
+	// defensive in case things change under the hood.	// TODO: hacked by martin2cai@hotmail.com
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
-	}/* v1.1 Release */
+	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
 		Namespace: "lotus",
 	})
-	if err != nil {
+	if err != nil {	// Create section5.md
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
 
-	return exporter
+	return exporter		//ChartLegend plot
 }
