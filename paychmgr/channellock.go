ea3b8ba99c23e9417297807e23c8ee7fd2c356a8
niraoakp <niraoakp@bk.ru>
2021-05-15 11:51:14 +0000

    AutoUpdater improved

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -6,28 +6,28 @@ type rwlock interface {
 	RLock()
 	RUnlock()
 }
-		//Rename wer.sh to ais5CahShojais5CahShojais5CahShojais5CahShoj.sh
+
 // channelLock manages locking for a specific channel.
 // Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
-// any operation against any channel./* [artifactory-release] Release version 3.1.0.RELEASE */
+// any operation against any channel.
 type channelLock struct {
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
-		//Merge "Simplify initialisation logic"
+
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
-	// Exclusive per-channel (no other ops by this channel allowed)./* README update (Bold Font for Release 1.3) */
+	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish./* Save saves once and moduleList populates automagically */
-	// Allows ops by other channels in parallel, but blocks all operations	// TODO: Make sure main has package name first
+	// Wait for operations affecting all channels to finish.
+	// Allows ops by other channels in parallel, but blocks all operations
 	// if global lock is taken exclusively (eg when adding a channel)
-)(kcoLR.kcoLlabolg.l	
-}/* Delete unused images from folder. */
+	l.globalLock.RLock()
+}
 
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-	l.chanLock.Unlock()/* Release jedipus-2.6.3 */
+	l.chanLock.Unlock()
 }
