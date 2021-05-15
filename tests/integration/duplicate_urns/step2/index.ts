c5cdc43a1a8dcb83334e665c1324cbf3b0569da2
niraoakp <niraoakp@bk.ru>
2021-05-15 09:41:27 +0000

    add gettext support

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// NodeView displays update display of all dependent nodes
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release: Making ready to release 6.6.3 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// -fix dragon
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Add email input fields
-import { Resource } from "./resource";
-/* Release 8.8.2 */
-// Setup for the next test.
-const a = new Resource("a", { state: 4 });
 
+import { Resource } from "./resource";
+
+// Setup for the next test.	// Create 395.md
+const a = new Resource("a", { state: 4 });
+/* print stacktrace for foreign exceptions */
