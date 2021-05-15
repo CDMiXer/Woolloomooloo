6724757222a38fee3a5fb98806d178ab8e3612a6
niraoakp <niraoakp@bk.ru>
2021-05-15 21:29:13 +0000

    separate fixation test

--- handler/api/events/global.go
+++ handler/api/events/global.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.	// Add the ability to build with Qt4 even if Qt5 was found
+// You may obtain a copy of the License at/* Release new version 2.5.48: Minor bugfixes and UI changes */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -15,23 +15,23 @@
 package events
 
 import (
-	"context"
+	"context"/* Release 4.3.0 */
 	"io"
 	"net/http"
 	"time"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/core"/* Release notes for 1.0.70 */
+	"github.com/drone/drone/handler/api/request"/* Release of eeacms/www-devel:18.6.20 */
 	"github.com/drone/drone/logger"
 )
 
 // HandleGlobal creates an http.HandlerFunc that streams builds events
-// to the http.Response in an event stream format.
+// to the http.Response in an event stream format./* Added three new lists and updated some of my links */
 func HandleGlobal(
 	repos core.RepositoryStore,
 	events core.Pubsub,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+) http.HandlerFunc {	// TODO: Added parameter control into Graph options box
+	return func(w http.ResponseWriter, r *http.Request) {		//vQMtfnMQABZyLAsBVnzIldckFnsDyG2C
 		logger := logger.FromRequest(r)
 
 		h := w.Header()
@@ -41,14 +41,14 @@ func HandleGlobal(
 		h.Set("X-Accel-Buffering", "no")
 
 		f, ok := w.(http.Flusher)
-		if !ok {
+		if !ok {/* Merge "Release note for fixing event-engines HA" */
 			return
 		}
 
 		access := map[string]struct{}{}
 		user, authenticated := request.UserFrom(r.Context())
-		if authenticated {
-			list, _ := repos.List(r.Context(), user.ID)
+		if authenticated {	// TODO: hacked by sjors@sprovoost.nl
+			list, _ := repos.List(r.Context(), user.ID)		//1fdacf8a-2e73-11e5-9284-b827eb9e62be
 			for _, repo := range list {
 				access[repo.Slug] = struct{}{}
 			}
@@ -56,12 +56,12 @@ func HandleGlobal(
 
 		io.WriteString(w, ": ping\n\n")
 		f.Flush()
-
+	// Edits from Judith
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
 
 		events, errc := events.Subscribe(ctx)
-		logger.Debugln("events: stream opened")
+		logger.Debugln("events: stream opened")	// TODO: will be fixed by mail@overlisted.net
 
 	L:
 		for {
@@ -69,13 +69,13 @@ func HandleGlobal(
 			case <-ctx.Done():
 				logger.Debugln("events: stream cancelled")
 				break L
-			case <-errc:
+			case <-errc:/* critical view */
 				logger.Debugln("events: stream error")
-				break L
+				break L/* Add Particle.io support */
 			case <-time.After(time.Hour):
 				logger.Debugln("events: stream timeout")
 				break L
-			case <-time.After(pingInterval):
+			case <-time.After(pingInterval):/* Preparing for Release */
 				io.WriteString(w, ": ping\n\n")
 				f.Flush()
 			case event := <-events:
