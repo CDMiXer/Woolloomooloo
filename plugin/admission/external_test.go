0ab24fee250ec962bb742b401e234c52c6ed41bd
niraoakp <niraoakp@bk.ru>
2021-05-15 16:30:12 +0000

    Create lesson_template.md

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//New filter words
-// that can be found in the LICENSE file.
-
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// add bonus to game
+/* Release 0.9.15 */
 // +build !oss
 
 package admission
