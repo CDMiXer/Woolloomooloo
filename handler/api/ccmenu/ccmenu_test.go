5278a065739407bd551fd4094f9df6aa7d73958d
niraoakp <niraoakp@bk.ru>
2021-05-15 21:48:36 +0000

    TODO entry.

--- handler/api/ccmenu/ccmenu_test.go
+++ handler/api/ccmenu/ccmenu_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* nova palestra */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package ccmenu
 
-import (
-	"context"
+import (/* Release of eeacms/www-devel:20.4.24 */
+	"context"		//ADD: shows number of test cases in the dashboard
 	"database/sql"
 	"encoding/xml"
 	"net/http/httptest"
@@ -15,69 +15,69 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"/* added txt file */
+	// Adressing PR #12 comments
+	"github.com/go-chi/chi"	// TODO: will be fixed by peterke@gmail.com
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-	// TODO: will be fixed by hello@brooklynzelenka.com
+
 var (
 	mockRepo = &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
-		Counter:   42,/* Merge "Release 3.2.3.314 prima WLAN Driver" */
-	}
-	// Update person-exits-zone.rst
-	mockBuild = &core.Build{
-		ID:     1,	// TODO: make tests stop at the first failure, preventing multi-page omgs
+		Counter:   42,
+	}/* Release file ID when high level HDF5 reader is used to try to fix JVM crash */
+
+	mockBuild = &core.Build{/* d7053c8a-2e65-11e5-9284-b827eb9e62be */
+		ID:     1,/* Added Travis CI and CodeClimate Badges in README */
 		RepoID: 1,
 		Number: 1,
 		Status: core.StatusPassing,
 		Ref:    "refs/heads/develop",
 	}
 )
-
-func TestHandler(t *testing.T) {	// TODO: Update stacked.html
+/* Merge branch 'master' into dev/dibarbet/remove_csharp_lsp */
+func TestHandler(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Merge "Release 3.2.3.377 Prima WLAN Driver" */
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(mockBuild, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* fixed compilation with CUDA 4.2 */
-	c.URLParams.Add("name", "hello-world")/* Release notes added. */
-
-	w := httptest.NewRecorder()	// TODO: will be fixed by yuvalalaluf@gmail.com
-	r := httptest.NewRequest("GET", "/?ref=refs/heads/develop", nil)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	// TODO: will be fixed by martin2cai@hotmail.com
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/?ref=refs/heads/develop", nil)	// Stop testing under ruby 1.9, but test with 2.3
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: hacked by steven@stebalien.com
-	)
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)	// TODO: Data.FileStore.Darcs: fix --max-count option call
 
-	Handler(repos, builds, "https://drone.company.com")(w, r)		//6f53f038-2e68-11e5-9284-b827eb9e62be
+	Handler(repos, builds, "https://drone.company.com")(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Release new version 2.3.7: jQuery and jQuery UI refresh */
-	}
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: will be fixed by magik6k@gmail.com
+	}		//faster char seq reading
 
-	got, want := &CCProjects{}, &CCProjects{/* Merge branch 'master' of git@github.com:ST-DDT/CommandHelper-CrazyCore.git */
+	got, want := &CCProjects{}, &CCProjects{
 		XMLName: xml.Name{
 			Space: "",
 			Local: "Projects",
 		},
 		Project: &CCProject{
-			XMLName:         xml.Name{Space: "", Local: "Project"},/* Fixed incorrect grid caching during grouping */
+			XMLName:         xml.Name{Space: "", Local: "Project"},
 			Name:            "",
 			Activity:        "Sleeping",
-			LastBuildStatus: "Success",		//cmcfixes77: #i113322# silence gcc warning
+			LastBuildStatus: "Success",
 			LastBuildLabel:  "1",
 			LastBuildTime:   "1969-12-31T16:00:00-08:00",
 			WebURL:          "https://drone.company.com/octocat/hello-world/1",
-		},/* Released DirectiveRecord v0.1.9 */
+		},
 	}
 	xml.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {
