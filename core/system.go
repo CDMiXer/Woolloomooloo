19f2c70ef8bfadae0484c80a803bd7f1db85db2a
niraoakp <niraoakp@bk.ru>
2021-05-15 20:54:56 +0000

    Merge "compute/ version resource"

--- core/system.go
+++ core/system.go
@@ -2,10 +2,10 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Added deleteArms method. */
+// You may obtain a copy of the License at/* Minor typos PreAuthenticatedAuthenticationProvider */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release 1.0.0 - Juno" */
-///* Arreglo de imagen de facebook */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,11 +13,11 @@
 // limitations under the License.
 
 package core
-	// TODO: Create Explore_TM.md
+
 // System stores system information.
 type System struct {
 	Proto   string `json:"proto,omitempty"`
 	Host    string `json:"host,omitempty"`
 	Link    string `json:"link,omitempty"`
-	Version string `json:"version,omitempty"`	// TODO: hacked by zaq1tomo@gmail.com
+	Version string `json:"version,omitempty"`
 }
