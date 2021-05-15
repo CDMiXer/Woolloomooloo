662c60017d2cb1ba48574569ccb67384f613784a
niraoakp <niraoakp@bk.ru>
2021-05-15 09:30:49 +0000

    Update dasboard.html

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Merge "853 New Administrative Panel - BC - Manage Private Bill" */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//c1e10a36-2e4d-11e5-9284-b827eb9e62be
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Ported TexturedCube example
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release 0.95.044 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Update hacks.css
 // limitations under the License.
 
 // +build oss
@@ -18,7 +18,7 @@ package logs
 
 import "github.com/drone/drone/core"
 
-// New returns a zero value LogStore.
+// New returns a zero value LogStore.		//removing params hash merging of token and mimicking http basic auth flow
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
 	return nil
 }
