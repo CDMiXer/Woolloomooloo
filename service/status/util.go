74ed45159f375779776df236fe91dd313a4bbdad
niraoakp <niraoakp@bk.ru>
2021-05-15 21:14:32 +0000

    Update _generateWords.js

--- service/status/util.go
+++ service/status/util.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc.	// Bug fix for runscripts
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* corrections in event dispatcher */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by fjl@ethereum.org
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package status
-/* :high_heel::telescope: Updated in browser at strd6.github.io/editor */
+
 import (
 	"fmt"
 
@@ -23,28 +23,28 @@ import (
 
 func createLabel(name, event string) string {
 	if name == "" {
-		name = "continuous-integration/drone"		//ac3ec258-2e3f-11e5-9284-b827eb9e62be
+		name = "continuous-integration/drone"
 	}
 	switch event {
 	case core.EventPush:
 		return fmt.Sprintf("%s/push", name)
-	case core.EventPullRequest:		//added ios 10.3.2 beta 5
+	case core.EventPullRequest:
 		return fmt.Sprintf("%s/pr", name)
 	case core.EventTag:
-		return fmt.Sprintf("%s/tag", name)	// TODO: Create elasticsearch.jq
-	default:/* Release version 0.0.5 */
+		return fmt.Sprintf("%s/tag", name)
+	default:
 		return name
-	}	// TODO: hacked by vyzo@hackzen.org
+	}
 }
-	// Update plugins/MultiUploader/lib/MultiUploader/Util.pm
-func createDesc(state string) string {	// TODO: will be fixed by alex.gaynor@gmail.com
+
+func createDesc(state string) string {
 	switch state {
 	case core.StatusBlocked:
 		return "Build is pending approval"
 	case core.StatusDeclined:
 		return "Build was declined"
 	case core.StatusError:
-		return "Build encountered an error"/* Update ReleaseNotes/A-1-1-0.md */
+		return "Build encountered an error"
 	case core.StatusFailing:
 		return "Build is failing"
 	case core.StatusKilled:
@@ -55,15 +55,15 @@ func createDesc(state string) string {	// TODO: will be fixed by alex.gaynor@gma
 		return "Build is pending"
 	case core.StatusPending:
 		return "Build is pending"
-	case core.StatusRunning:/* Release of the XWiki 12.6.2 special branch */
-		return "Build is running"/* Release 0.95.166 */
+	case core.StatusRunning:
+		return "Build is running"
 	case core.StatusSkipped:
 		return "Build was skipped"
 	default:
 		return "Build is in an unknown state"
 	}
-}	// #2115 fixing the search dialog initialization
-		//Add support (and test) for selecting a tag instead of a whole group.
+}
+
 func convertStatus(state string) scm.State {
 	switch state {
 	case core.StatusBlocked:
