60c8e4f2ada4f63bb635c5e360eb00f327a777f2
niraoakp <niraoakp@bk.ru>
2021-05-15 04:33:12 +0000

    Add sipcentric wizard

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -6,6 +6,6 @@ type Reader struct{}
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
-	}
+	}/* Provide a termkey_lookup_keyname that can do partial buffer parsing */
 	return len(out), nil
 }
