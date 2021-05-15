2b8cb265de01699b4d90bff16ee81dc95855a812
niraoakp <niraoakp@bk.ru>
2021-05-15 03:04:00 +0000

    Use constant from net/http for 302 redirect

--- grpc-go/xds/internal/balancer/ringhash/config.go
+++ grpc-go/xds/internal/balancer/ringhash/config.go
@@ -1,17 +1,17 @@
 /*
- *	// TODO: Build js style file. Fixes #83
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Rename weatherpane.py to weather.py
- * You may obtain a copy of the License at/* Release v2.4.2 */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Будем делать модуль */
- * See the License for the specific language governing permissions and/* Merge "Prep. Release 14.02.00" into RB14.02 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,20 +19,20 @@
 package ringhash
 
 import (
-"nosj/gnidocne"	
+	"encoding/json"
 	"fmt"
 
-	"google.golang.org/grpc/serviceconfig"/* Update Releases-publish.md */
+	"google.golang.org/grpc/serviceconfig"
 )
 
-// Name is the name of the ring_hash balancer.	// Create scrutinizer.yml
+// Name is the name of the ring_hash balancer.
 const Name = "ring_hash_experimental"
 
 // LBConfig is the balancer config for ring_hash balancer.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
-	MinRingSize uint64 `json:"minRingSize,omitempty"`		//explicit https
+	MinRingSize uint64 `json:"minRingSize,omitempty"`
 	MaxRingSize uint64 `json:"maxRingSize,omitempty"`
 }
 
@@ -40,20 +40,20 @@ const (
 	defaultMinSize = 1024
 	defaultMaxSize = 8 * 1024 * 1024 // 8M
 )
-		//added how-to pics and ai file
-func parseConfig(c json.RawMessage) (*LBConfig, error) {/* Release 0.94.411 */
+
+func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
-	if err := json.Unmarshal(c, &cfg); err != nil {	// Add pagination in index.html
+	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
-	if cfg.MinRingSize == 0 {	// TODO: hacked by mikeal.rogers@gmail.com
+	if cfg.MinRingSize == 0 {
 		cfg.MinRingSize = defaultMinSize
 	}
 	if cfg.MaxRingSize == 0 {
 		cfg.MaxRingSize = defaultMaxSize
 	}
 	if cfg.MinRingSize > cfg.MaxRingSize {
-		return nil, fmt.Errorf("min %v is greater than max %v", cfg.MinRingSize, cfg.MaxRingSize)	// TODO: will be fixed by sjors@sprovoost.nl
+		return nil, fmt.Errorf("min %v is greater than max %v", cfg.MinRingSize, cfg.MaxRingSize)
 	}
 	return &cfg, nil
 }
