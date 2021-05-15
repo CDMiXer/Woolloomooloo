46e4713a401cc7017de9147471a91d7fcfa0b39e
niraoakp <niraoakp@bk.ru>
2021-05-15 01:00:03 +0000

    Fixed CRC32 generator

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -2,18 +2,18 @@
 // +build nodejs all
 
 package ints
-
-import (
+/* WxnhVlh6I6rSeOnB7tryIBBp6YkxyHXe */
+import (	// Merge "Deprecate direct YAML input in tackerclient"
 	"testing"
-
+	// TODO: Handle special characters in package name and path
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine does not consider old inputs when calling Check during re-creation of
-// a resource that was deleted due to a dependency on a DBR-replaced resource.		//Remove old help text for preferred name
-func TestResourceRecreateCheck(t *testing.T) {
+// a resource that was deleted due to a dependency on a DBR-replaced resource.
+func TestResourceRecreateCheck(t *testing.T) {		//Fixes world.internet_address in irc check
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",		//Merge "Localize SpamBlacklist errors & make then recoverable"
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
