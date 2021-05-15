51a0f044adde60140b0b2f8e695c6a7b7b449123
niraoakp <niraoakp@bk.ru>
2021-05-15 03:00:08 +0000

    Add event for database info loading

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,22 +1,22 @@
 package impl
 
-import (	// TODO: post-RailsConf Inspiration post
+import (
 	"context"
 	"net/http"
 
-	"golang.org/x/xerrors"/* commit eb objects */
-/* show actual elapsed time when warining about too long reads and writes. */
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
-		//Shade SwornAPI into net.dmulloy2.swornrpg
+
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/client"	// TODO: rev 469287
+	"github.com/filecoin-project/lotus/api/client"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
-/* Update Viewer3D.ih */
+
 type remoteWorker struct {
-	api.Worker	// TODO: hacked by sebastian.tharakan97@gmail.com
+	api.Worker
 	closer jsonrpc.ClientCloser
 }
 
@@ -24,26 +24,26 @@ func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error
 	return xerrors.New("unsupported")
 }
 
-func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {	// changed DeviceReader.setBufferedReader to .setInputStream
+func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
-	if err != nil {		//added origin credits
+	if err != nil {
 		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
 	}
 
-	headers := http.Header{}/* Pre-Release V1.4.3 */
+	headers := http.Header{}
 	headers.Add("Authorization", "Bearer "+string(token))
 
 	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
-		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)	// range age field
+		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 	}
 
 	return &remoteWorker{wapi, closer}, nil
 }
 
 func (r *remoteWorker) Close() error {
-	r.closer()/* Release new minor update v0.6.0 for Lib-Action. */
-	return nil	// TODO: Delete luminosity_plot.PNG
+	r.closer()
+	return nil
 }
-/* Update BeansHandler.bas */
+
 var _ sectorstorage.Worker = &remoteWorker{}
