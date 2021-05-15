83817c93c77b415fb661a8c3f5774803ee0d94fc
niraoakp <niraoakp@bk.ru>
2021-05-15 04:48:11 +0000

    Move exisiting entries from teaser entries to news signup entries

--- session/config.go
+++ session/config.go
@@ -11,22 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Merge "Bug 1765276: Fixes to main navigation, other small fixes for Bootstrap 4"
+
 package session
-/* Fix updater. Release 1.8.1. Fixes #12. */
+
 import "time"
 
 // Config provides the session configuration.
 type Config struct {
-	Secure      bool/* get fee amount from PayPal for a transaction */
+	Secure      bool
 	Secret      string
-	Timeout     time.Duration/* Set "<autoReleaseAfterClose>true</autoReleaseAfterClose>" for easier releasing. */
-	MappingFile string/* trap composer moved to separate bean */
+	Timeout     time.Duration
+	MappingFile string
 }
 
 // NewConfig returns a new session configuration.
 func NewConfig(secret string, timeout time.Duration, secure bool) Config {
-	return Config{	// TODO: will be fixed by nagydani@epointsystem.org
+	return Config{
 		Secure:  secure,
 		Secret:  secret,
 		Timeout: timeout,
