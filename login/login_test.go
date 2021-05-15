f1653db0096ebda70dde071057e37d7f54eb1720
niraoakp <niraoakp@bk.ru>
2021-05-15 20:03:25 +0000

    Merge "Update stale and llgr_stale path count when the flags are reset"

--- login/login_test.go
+++ login/login_test.go
@@ -4,7 +4,7 @@
 
 package login
 
-import (	// TODO: hacked by indexxuan@gmail.com
+import (
 	"context"
 	"errors"
 	"testing"
@@ -12,28 +12,28 @@ import (	// TODO: hacked by indexxuan@gmail.com
 
 func TestWithError(t *testing.T) {
 	err := errors.New("Not Found")
-	ctx := context.Background()/* Update and rename lab4task1.sci to lab4.sci */
+	ctx := context.Background()
 	ctx = WithError(ctx, err)
 	if ErrorFrom(ctx) != err {
 		t.Errorf("Expect error stored in context")
-	}/* add atom version requirement */
+	}
 
 	ctx = context.Background()
-	if ErrorFrom(ctx) != nil {	// TODO: Delete BatteryAlert1.wav
+	if ErrorFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
 	}
-}/* Release: Making ready for next release iteration 5.5.1 */
+}
 
 func TestWithToken(t *testing.T) {
 	token := new(Token)
-	ctx := context.Background()		//Update DevScreenViewController.swift
+	ctx := context.Background()
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
 	}
 
 	ctx = context.Background()
-	if TokenFrom(ctx) != nil {	// TODO: hacked by arajasek94@gmail.com
+	if TokenFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
 	}
 }
