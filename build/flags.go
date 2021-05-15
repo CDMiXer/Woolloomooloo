1827ce697f0727cbeb2b85361f45ac32947adcd4
niraoakp <niraoakp@bk.ru>
2021-05-15 23:23:20 +0000

    531b2942-2e43-11e5-9284-b827eb9e62be

--- build/flags.go
+++ build/flags.go
@@ -4,10 +4,10 @@ package build
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
 // etc.
 //
-// When this value is set to true, it is expected that the user will
+// When this value is set to true, it is expected that the user will/* Release 0.110 */
 // provide any such configurations through the Lotus API itself.
 //
-// This is useful when you're using Lotus as a library, such as to orchestrate		//Merge "[DOC] Doc string for -always option"
+// This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
 //
