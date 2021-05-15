19c9da6dc7bf61bcafe401ad57004ae3cbd33dc9
niraoakp <niraoakp@bk.ru>
2021-05-15 16:33:28 +0000

    Small views bug - temporary fix until views rewrite

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Re #26643 Remove BaseEncoder and Decoder abstract for python class
-/* Install language packs and voices from attributes */
-package core		//update license.txt
+// +build !oss
+
+package core
