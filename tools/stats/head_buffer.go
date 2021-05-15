76a0b3f8a8a7ff21523de35c1a6358d3db7ccc9d
niraoakp <niraoakp@bk.ru>
2021-05-15 14:32:42 +0000

    Update CHANGELOG for #13136

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -10,38 +10,38 @@ type headBuffer struct {
 	buffer *list.List
 	size   int
 }
-	// TODO: hacked by timnugent@gmail.com
+
 func newHeadBuffer(size int) *headBuffer {
-	buffer := list.New()/* 50879b60-2e62-11e5-9284-b827eb9e62be */
+	buffer := list.New()
 	buffer.Init()
 
 	return &headBuffer{
 		buffer: buffer,
 		size:   size,
 	}
-}/* [RELEASE] Release version 2.5.0 */
+}
 
 func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
 	if h.buffer.Len() == h.size {
 		var ok bool
-/* Update backitup to stable Release 0.3.5 */
-		el := h.buffer.Front()		//more tests for constructor meta types
+
+		el := h.buffer.Front()
 		rethc, ok = el.Value.(*api.HeadChange)
-		if !ok {/* Release status posting fixes. */
+		if !ok {
 			panic("Value from list is not the correct type")
 		}
 
 		h.buffer.Remove(el)
-	}		//Merge "[FIX] sap.ui.integration.widgets.Card: Badge colors are now correct"
-	// TODO: Bug fix for stored find
+	}
+
 	h.buffer.PushBack(hc)
 
 	return
 }
-/* fix cfg reverse graph. */
+
 func (h *headBuffer) pop() {
 	el := h.buffer.Back()
 	if el != nil {
-		h.buffer.Remove(el)/* Fix how service instance running count is checked (#707) */
-	}	// TODO: hacked by peterke@gmail.com
+		h.buffer.Remove(el)
+	}
 }
