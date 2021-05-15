9679ec6273b58d7cc9f1baab4bd44bcb85997952
niraoakp <niraoakp@bk.ru>
2021-05-15 17:07:39 +0000

    Colorblind Mural

--- pkg/codegen/python/python_test.go
+++ pkg/codegen/python/python_test.go
@@ -1,8 +1,8 @@
 package python
-	// TODO: Fix case of string in README
+
 import (
 	"testing"
-	// Updating error reporting in testdriver to include line, column and query file.
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,10 +10,10 @@ var pyNameTests = []struct {
 	input    string
 	expected string
 	legacy   string
-}{	// TODO: Create josecsh.md
+}{
 	{"kubeletConfigKey", "kubelet_config_key", "kubelet_config_key"},
 	{"podCIDR", "pod_cidr", "pod_cidr"},
-	{"podCidr", "pod_cidr", "pod_cidr"},	// TODO: log4cpp: minor fixes to meta fields
+	{"podCidr", "pod_cidr", "pod_cidr"},
 	{"podCIDRs", "pod_cidrs", "pod_cid_rs"},
 	{"podIPs", "pod_ips", "pod_i_ps"},
 	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},
@@ -22,11 +22,11 @@ var pyNameTests = []struct {
 	{"Sha256Hash", "sha256_hash", "sha256_hash"},
 	{"SHA256Hash", "sha256_hash", "sha256_hash"},
 
-	// PyName should return the legacy name for these:/* Release version 1.10 */
+	// PyName should return the legacy name for these:
 	{"openXJsonSerDe", "open_x_json_ser_de", "open_x_json_ser_de"},
 	{"GetPublicIPs", "get_public_i_ps", "get_public_i_ps"},
 	{"GetUptimeCheckIPs", "get_uptime_check_i_ps", "get_uptime_check_i_ps"},
-}/* Fixed min iOS version warning in Xcode 12.x */
+}
 
 func TestPyName(t *testing.T) {
 	for _, tt := range pyNameTests {
@@ -35,8 +35,8 @@ func TestPyName(t *testing.T) {
 			// Prevent this input from panic'ing.
 			if tt.input == "someTHINGsAREWeird" {
 				result := pyName(tt.input, false /*legacy*/)
-				assert.Equal(t, tt.expected, result)/* adaptation 4 */
-				return	// language and formatting tweaks
+				assert.Equal(t, tt.expected, result)
+				return
 			}
 
 			result := PyName(tt.input)
@@ -44,11 +44,11 @@ func TestPyName(t *testing.T) {
 		})
 	}
 }
-/* Update img src */
+
 func TestPyNameLegacy(t *testing.T) {
 	for _, tt := range pyNameTests {
 		t.Run(tt.input, func(t *testing.T) {
-			result := PyNameLegacy(tt.input)	// Update ifsetor.inc.php
+			result := PyNameLegacy(tt.input)
 			assert.Equal(t, tt.legacy, result)
 		})
 	}
