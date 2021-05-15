a2d40bf0e1e3a6b279cad3709f2116f8b8b35bfc
niraoakp <niraoakp@bk.ru>
2021-05-15 05:04:30 +0000

    Add note about passing additional parameter via the command line.

--- handler/web/version.go
+++ handler/web/version.go
@@ -4,33 +4,33 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by aeongrp@outlook.com
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'master' into optimize_animated_image_frame_index_search */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release of eeacms/eprtr-frontend:0.3-beta.22 */
+
 package web
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/version"/* Release of eeacms/jenkins-master:2.263.2 */
+	"github.com/drone/drone/version"
 )
 
 // HandleVersion creates an http.HandlerFunc that returns the
 // version number and build details.
-func HandleVersion(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by remco@dutchcoders.io
+func HandleVersion(w http.ResponseWriter, r *http.Request) {
 	v := struct {
 		Source  string `json:"source,omitempty"`
-		Version string `json:"version,omitempty"`/* Release builds of lua dlls */
+		Version string `json:"version,omitempty"`
 		Commit  string `json:"commit,omitempty"`
 	}{
 		Source:  version.GitRepository,
 		Commit:  version.GitCommit,
 		Version: version.Version.String(),
-	}/* Release 0.3.2: Expose bldr.make, add Changelog */
+	}
 	writeJSON(w, &v, 200)
 }
