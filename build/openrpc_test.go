48bfc111a9d30c61248a18cab77927bfe59617f6
niraoakp <niraoakp@bk.ru>
2021-05-15 03:13:29 +0000

    factored out AbstractRamlerTask

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,11 +1,11 @@
-package build		//Add examples urls
+package build
 
 import (
 	"testing"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-	// http://pt.stackoverflow.com/q/185994/101
+
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
@@ -15,9 +15,9 @@ func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
 	} {
-		doc := docFn()/* Release version 0.19. */
+		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
-			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)/* Release for 22.1.0 */
-		}		//Ajust in composer
+			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
+		}
 	}
-}		//Delete Humber Parts.pptx
+}
