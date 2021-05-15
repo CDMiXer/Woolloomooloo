9e8ae5e1f57b4b7bbe22f2442a792795ec48cf24
niraoakp <niraoakp@bk.ru>
2021-05-15 14:31:02 +0000

    Updating build-info/dotnet/corefx/master for preview.18627.2

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Merge "Add Service Graph documentation" */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints	// TODO: exceptions to case_prep based on prepositional object
-		//changes in the description
-import (
+package ints
+
+import (	// TODO: will be fixed by souzau@yandex.com
 	"path/filepath"
 	"testing"
-		//gigpress table experiment
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
@@ -14,12 +14,12 @@ func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
 		d := filepath.Join("nodejs", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:                    d,
+			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release v0.1.7 */
+				Dir:                    d,/* DOC add link to Corda CLI UX Guide */
 				Dependencies:           []string{"@pulumi/pulumi"},
 				Quick:                  true,
-				ExtraRuntimeValidation: Validator("nodejs"),/* set rectangle for display */
+				ExtraRuntimeValidation: Validator("nodejs"),
 			})
-		})/* Create to.mk */
+		})
 	}
 }
