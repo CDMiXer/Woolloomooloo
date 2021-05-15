9d142d1488d8d893ca214ed00b32c9e2eef04347
niraoakp <niraoakp@bk.ru>
2021-05-15 04:15:06 +0000

    Fixed TernaryState

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -5,18 +5,18 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release version 3.1.0.M1 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* reverse to old code */
+// limitations under the License.
 
 // +build nolimit
 // +build oss
 
-package license	// TODO: hacked by timnugent@gmail.com
-/* Fixes Issue #33 */
+package license
+
 import (
 	"github.com/drone/drone/core"
 )
