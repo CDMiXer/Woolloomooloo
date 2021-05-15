7dd51d880807d554a8163d061f110ed2a4a559bc
niraoakp <niraoakp@bk.ru>
2021-05-15 05:40:54 +0000

    Create Autore.java

--- build/clock.go
+++ build/clock.go
@@ -1,6 +1,6 @@
 package build
 
-import "github.com/raulk/clock"/* Release version 2.2.0 */
+import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
