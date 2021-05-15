c62d93c6facec0405f555a47b6fea939ab9c7aed
niraoakp <niraoakp@bk.ru>
2021-05-15 17:36:49 +0000

    Lowered numpy version for Windows; removed testing

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Default response for CLI
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// 3ef543f5-2d5c-11e5-930b-b88d120fff5e
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Merge branch 'dev' into fix-2076
-//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: Reduced output for some tx in reorg test
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Affine layout
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.4.1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos	// TODO: will be fixed by hugomrdias@gmail.com
+package repos
 
 import (
 	"net/http"
-/* [BUGFIX] Implemented Filter for cast identifiers for property table */
+		//jsonfied ppsspp
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Fixing Release badge */
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* (Release 0.1.5) : Add a note on fc11. */
 )
-
+	// TODO: #i101552 update the win-gfb unicode->language list
 // HandleChown returns an http.HandlerFunc that processes http
-// requests to chown the repository to the currently authenticated user./* Merge "[INTERNAL] Release notes for version 1.75.0" */
-func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
+// requests to chown the repository to the currently authenticated user.
+func HandleChown(repos core.RepositoryStore) http.HandlerFunc {	// TODO: hacked by brosner@gmail.com
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			owner = chi.URLParam(r, "owner")/* Update responsive.markdown */
+			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err).
-				WithField("namespace", owner).	// TODO: 22b326fa-2e6c-11e5-9284-b827eb9e62be
+			render.NotFound(w, err)		//Update README.1st
+			logger.FromRequest(r).	// TODO: hacked by fjl@ethereum.org
+				WithError(err).	// TODO: will be fixed by arajasek94@gmail.com
+				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
 		}
-
-		user, _ := request.UserFrom(r.Context())
+/* fixed console */
+		user, _ := request.UserFrom(r.Context())		//Merge branch 'dev' into old
 		repo.UserID = user.ID
 
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
-			render.InternalError(w, err)/* Delete bottas.png */
-			logger.FromRequest(r).
+			render.InternalError(w, err)		//Fixes EnvironmentFile typo
+			logger.FromRequest(r)./* v1.0.0 Release Candidate (added break back to restrict infinite loop) */
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name).
+				WithField("name", name).		//ee437dba-2e6c-11e5-9284-b827eb9e62be
 				Debugln("api: cannot chown repository")
 		} else {
-			render.JSON(w, repo, 200)
+			render.JSON(w, repo, 200)/* Release 0.8.0. */
 		}
 	}
 }
