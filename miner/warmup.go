dbecd45653d1b741a6293f3c7042c5d5b03c1458
niraoakp <niraoakp@bk.ru>
2021-05-15 21:56:10 +0000

    Merge branch 'developer' into ruishang

--- miner/warmup.go
+++ miner/warmup.go
@@ -1,31 +1,31 @@
 package miner
-/* 4.1.6-beta10 Release Changes */
+
 import (
 	"context"
 	"crypto/rand"
-	"math"	// TODO: cvts rolling/nonrolling merge loop
+	"math"
 	"time"
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-bitfield"		//43f6aa44-2e6c-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by why@ipfs.io
+	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Don't use the version number in the path for pbutils.
+
 func (m *Miner) winPoStWarmup(ctx context.Context) error {
 	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
-	if err != nil {/* Release 2.0.5 Final Version */
+	if err != nil {
 		return xerrors.Errorf("getting deadlines: %w", err)
 	}
 
 	var sector abi.SectorNumber = math.MaxUint64
-/* Release 0.5.2 */
+
 out:
-	for dlIdx := range deadlines {	// TODO: in response to #142
+	for dlIdx := range deadlines {
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
 		if err != nil {
 			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)
@@ -37,12 +37,12 @@ out:
 				continue
 			}
 			if err != nil {
-				return err	// TODO: hacked by martin2cai@hotmail.com
-			}	// kernel: update module names and add new config symbols for linux 3.3
+				return err
+			}
 
-			sector = abi.SectorNumber(b)	// Use 1.0.1 for parent pom.
-			break out	// TODO: will be fixed by qugou1350636@126.com
-		}	// TODO: hacked by mikeal.rogers@gmail.com
+			sector = abi.SectorNumber(b)
+			break out
+		}
 	}
 
 	if sector == math.MaxUint64 {
@@ -56,11 +56,11 @@ out:
 	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)
 	_, _ = rand.Read(r)
 
-	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)/* add donation buttons */
+	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
 	if err != nil {
 		return xerrors.Errorf("getting sector info: %w", err)
 	}
-/* fix https://github.com/AdguardTeam/AdguardFilters/issues/66614 */
+
 	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
 		{
 			SealProof:    si.SealProof,
@@ -70,7 +70,7 @@ out:
 	}, r)
 	if err != nil {
 		return xerrors.Errorf("failed to compute proof: %w", err)
-	}	// TODO: hacked by 13860583249@yeah.net
+	}
 
 	log.Infow("winning PoSt warmup successful", "took", time.Now().Sub(start))
 	return nil
