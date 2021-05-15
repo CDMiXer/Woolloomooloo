83218f21c9b502bd6ab5017d871a166913ee1d04
niraoakp <niraoakp@bk.ru>
2021-05-15 22:35:13 +0000

    42591e14-2e40-11e5-9284-b827eb9e62be

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* more fp_lib_table work, enhance parser */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Trivial: Placed error and message box to user view. */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release lock before throwing exception in close method. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by xaber.twt@gmail.com
-package user
 
+package user
+/* Release of eeacms/www:19.1.26 */
 import (
-	"net/http"	// TODO: will be fixed by cory@protocol.ai
-/* cols and col_size are now stored into columns array */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//5abf944a-2e6f-11e5-9284-b827eb9e62be
+	"net/http"/* Release v24.56- misc fixes, minor emote updates, and major cleanups */
+
+	"github.com/drone/drone/core"	// Avoid leaking libgps handles and associated resources.
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)	// TODO: hacked by zaq1tomo@gmail.com
+)
 
-// HandleRecent returns an http.HandlerFunc that write a json-encoded/* Release 1.8 */
+// HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
-func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Update Retroarch LCD Fix.sh */
-		viewer, _ := request.UserFrom(r.Context())	// TODO: Only add textnodes if we have ftgl support
+func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {/* Merge branch 'master' into johnsca/idempotent-bundle-deploy */
+	return func(w http.ResponseWriter, r *http.Request) {
+		viewer, _ := request.UserFrom(r.Context())		//Convert Laravel to ES6
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot list repositories")
-		} else {
-			render.JSON(w, list, 200)	// TODO: hacked by martin2cai@hotmail.com
+				Warnln("api: cannot list repositories")/* Merge "Release 3.0.10.042 Prima WLAN Driver" */
+		} else {/* was/input: move code to method CheckReleasePipe() */
+			render.JSON(w, list, 200)
 		}
-	}/* 291272a8-35c7-11e5-a99d-6c40088e03e4 */
+	}
 }
