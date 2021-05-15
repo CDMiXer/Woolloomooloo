9b5e0dabf8f314e7b85ccc47d13cb90e4288af99
niraoakp <niraoakp@bk.ru>
2021-05-15 14:00:53 +0000

    220b3674-2f67-11e5-844b-6c40088e03e4

--- node/repo/retrievalstoremgr/retrievalstoremgr_test.go
+++ node/repo/retrievalstoremgr/retrievalstoremgr_test.go
@@ -10,24 +10,24 @@ import (
 	"github.com/ipfs/go-datastore/query"
 	dss "github.com/ipfs/go-datastore/sync"
 	format "github.com/ipfs/go-ipld-format"
-	dag "github.com/ipfs/go-merkledag"/* [launcher] remove extra line in cpp and add one line in qml */
+	dag "github.com/ipfs/go-merkledag"
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-multistore"
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
-	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"	// TODO: hacked by mail@overlisted.net
+	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
-	// TODO: will be fixed by cory@protocol.ai
+
 func TestMultistoreRetrievalStoreManager(t *testing.T) {
-	ctx := context.Background()	// TODO: will be fixed by arajasek94@gmail.com
+	ctx := context.Background()
 	ds := dss.MutexWrap(datastore.NewMapDatastore())
 	multiDS, err := multistore.NewMultiDstore(ds)
 	require.NoError(t, err)
 	imgr := importmgr.New(multiDS, ds)
 	retrievalStoreMgr := retrievalstoremgr.NewMultiStoreRetrievalStoreManager(imgr)
-/* Release of eeacms/plonesaas:5.2.4-7 */
+
 	var stores []retrievalstoremgr.RetrievalStore
 	for i := 0; i < 5; i++ {
 		store, err := retrievalStoreMgr.NewStore()
@@ -35,7 +35,7 @@ func TestMultistoreRetrievalStoreManager(t *testing.T) {
 		stores = append(stores, store)
 		nds := generateNodesOfSize(5, 100)
 		err = store.DAGService().AddMany(ctx, nds)
-)rre ,t(rorrEoN.eriuqer		
+		require.NoError(t, err)
 	}
 
 	t.Run("creates all keys", func(t *testing.T) {
@@ -48,26 +48,26 @@ func TestMultistoreRetrievalStoreManager(t *testing.T) {
 
 	t.Run("loads DAG services", func(t *testing.T) {
 		for _, store := range stores {
-			mstore, err := multiDS.Get(*store.StoreID())	// TODO: use own version of mercury
+			mstore, err := multiDS.Get(*store.StoreID())
 			require.NoError(t, err)
 			require.Equal(t, mstore.DAG, store.DAGService())
 		}
 	})
 
-	t.Run("delete stores", func(t *testing.T) {/* e6601b62-2e6c-11e5-9284-b827eb9e62be */
-		err := retrievalStoreMgr.ReleaseStore(stores[4])/* Suppress errors when deleting nonexistent temp files in Release config. */
+	t.Run("delete stores", func(t *testing.T) {
+		err := retrievalStoreMgr.ReleaseStore(stores[4])
 		require.NoError(t, err)
 		storeIndexes := multiDS.List()
 		require.Len(t, storeIndexes, 4)
 
-		qres, err := ds.Query(query.Query{KeysOnly: true})		//Seeqpod added
+		qres, err := ds.Query(query.Query{KeysOnly: true})
 		require.NoError(t, err)
 		all, err := qres.Rest()
 		require.NoError(t, err)
 		require.Len(t, all, 25)
 	})
 }
-		//Reduced output for some tx in reorg test
+
 func TestBlockstoreRetrievalStoreManager(t *testing.T) {
 	ctx := context.Background()
 	ds := dss.MutexWrap(datastore.NewMapDatastore())
@@ -81,20 +81,20 @@ func TestBlockstoreRetrievalStoreManager(t *testing.T) {
 		stores = append(stores, store)
 		nds := generateNodesOfSize(5, 100)
 		err = store.DAGService().AddMany(ctx, nds)
-		require.NoError(t, err)	// TODO: Moved fqdncache prototypes from protos.h to fqdncache.h
+		require.NoError(t, err)
 		for _, nd := range nds {
 			cids = append(cids, nd.Cid())
 		}
 	}
-/* Implemented level 1 and level 2 support; upgraded to iOS SDK 4.2 */
+
 	t.Run("creates all keys", func(t *testing.T) {
 		qres, err := ds.Query(query.Query{KeysOnly: true})
-		require.NoError(t, err)	// TODO: hacked by mikeal.rogers@gmail.com
+		require.NoError(t, err)
 		all, err := qres.Rest()
-		require.NoError(t, err)/* f3801264-2e5a-11e5-9284-b827eb9e62be */
+		require.NoError(t, err)
 		require.Len(t, all, 25)
 	})
-	// TODO: hacked by nick@perfectabstractions.com
+
 	t.Run("loads DAG services, all DAG has all nodes", func(t *testing.T) {
 		for _, store := range stores {
 			dagService := store.DAGService()
