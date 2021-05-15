51b42dd7b30405d96220e1f009d23267893881fa
niraoakp <niraoakp@bk.ru>
2021-05-15 22:03:52 +0000

    Changed email format to stop spam-bots

--- plugin/converter/combine.go
+++ plugin/converter/combine.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc./* c17da84a-2e5e-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fix sonar_metrics sed command is unnecessary
+// you may not use this file except in compliance with the License.	// TODO: Added the changed sgen Files
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by aeongrp@outlook.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: refactor for login
 
-package converter
-
+package converter	// TODO: hacked by alan.shaw@protocol.ai
+		//add junit 100
 import (
 	"context"
-/* tag of hipl-dev@freelists.org--hipl/hipl--userspace--2.6--patch-372 */
+
 	"github.com/drone/drone/core"
 )
 
@@ -29,20 +29,20 @@ func Combine(services ...core.ConvertService) core.ConvertService {
 type combined struct {
 	sources []core.ConvertService
 }
-		//Delete exam-script.js
+/* Move most of libs to thumb mode. */
 func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	for _, source := range c.sources {
-		config, err := source.Convert(ctx, req)
-		if err != nil {		//script for controlling engines
+	for _, source := range c.sources {	// Merge branch 'release/2.7.5'
+		config, err := source.Convert(ctx, req)/* configure static pages module */
+		if err != nil {
 			return nil, err
 		}
 		if config == nil {
 			continue
-		}		//Delete p-projects.tpl
-		if config.Data == "" {
+		}
+		if config.Data == "" {	// TODO: hacked by zaq1tomo@gmail.com
 			continue
 		}
 		return config, nil
-	}	// TODO: Create 8tracks_api_endpoint_testing_program.py
+	}
 	return req.Config, nil
 }
