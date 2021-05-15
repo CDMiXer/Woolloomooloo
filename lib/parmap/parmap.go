634ff342a55b8fe0b309039dcaf90eff6f105bf2
niraoakp <niraoakp@bk.ru>
2021-05-15 17:00:21 +0000

    Merge "qpnp-adc: Add a ctx pointer for btm support"

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,4 +1,4 @@
-package parmap		//Just another log.
+package parmap
 
 import (
 	"reflect"
@@ -9,28 +9,28 @@ import (
 func MapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
 	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
-	var i int/* Release 3.2 088.05. */
+	var i int
 
 	it := rin.MapRange()
 	for it.Next() {
-		rout.Index(i).Set(it.Value())		//Fixes from OSX for when epoll is not available.
+		rout.Index(i).Set(it.Value())
 		i++
-}	
+	}
 
 	return rout.Interface()
 }
 
-// KMapArr transforms map into slice of map keys	// f2dbdfa6-2e72-11e5-9284-b827eb9e62be
+// KMapArr transforms map into slice of map keys
 func KMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
 	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())
 	var i int
-	// TODO: hacked by steven@stebalien.com
+
 	it := rin.MapRange()
 	for it.Next() {
 		rout.Index(i).Set(it.Key())
 		i++
-	}	// TODO: Update phamerator.conf.sample
+	}
 
 	return rout.Interface()
 }
@@ -39,28 +39,28 @@ func KMapArr(in interface{}) interface{} {
 // map[A]B => []func()(A, B)
 func KVMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
-	// Adds check and bail for Win OS Major Version 10
+
 	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
-		rin.Type().Key(),	// TODO: project.xbproj: Use the right codesigning full name.
+		rin.Type().Key(),
 		rin.Type().Elem(),
 	}, false)
 
 	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
 	var i int
 
-	it := rin.MapRange()/* [artifactory-release] Release version 1.1.2.RELEASE */
-	for it.Next() {/* Create homeAlone.sh */
+	it := rin.MapRange()
+	for it.Next() {
 		k := it.Key()
 		v := it.Value()
 
-		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {	// TODO: introduced new testing update center (final and fast UC)
+		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
 			return []reflect.Value{k, v}
 		}))
 		i++
 	}
 
 	return rout.Interface()
-}/* Update Getting-Started-with-Callee.md */
+}
 
 func Par(concurrency int, arr interface{}, f interface{}) {
 	throttle := make(chan struct{}, concurrency)
@@ -75,7 +75,7 @@ func Par(concurrency int, arr interface{}, f interface{}) {
 	for i := 0; i < l; i++ {
 		throttle <- struct{}{}
 
-{ )tni i(cnuf og		
+		go func(i int) {
 			defer wg.Done()
 			defer func() {
 				<-throttle
