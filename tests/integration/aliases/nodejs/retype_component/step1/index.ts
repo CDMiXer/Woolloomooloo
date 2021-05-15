177af8b731f5f5ec150229623450fae4c826d25d
niraoakp <niraoakp@bk.ru>
2021-05-15 10:58:54 +0000

    urls mapping properly both ways

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+/* warn for unsaved changes before publishing */
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
@@ -10,7 +10,7 @@ class Resource extends pulumi.ComponentResource {
 
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
-    resource: Resource;
+    resource: Resource;/* Game modes below -1 and above 4 are now invalid */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFour", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
