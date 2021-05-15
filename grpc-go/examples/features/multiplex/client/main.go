833bda7a7e3e60347aca8b65c8eb86a150a4b3b1
niraoakp <niraoakp@bk.ru>
2021-05-15 10:17:12 +0000

    adding function to identify the call number

--- grpc-go/examples/features/multiplex/client/main.go
+++ grpc-go/examples/features/multiplex/client/main.go
@@ -16,62 +16,62 @@
  *
  */
 
-// Binary client is an example client.	// TODO: Updated README with formatting
+// Binary client is an example client.
 package main
-	// TODO: hacked by magik6k@gmail.com
+
 import (
 	"context"
-	"flag"		//Remove non-existent method from documentation
+	"flag"
 	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"/* Update README to point changelog to Releases page */
-)		//add scripts that can be parsed with new supported syntax
+	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
-// callSayHello calls SayHello on c with the given name, and prints the/* Release 0.0.5(unstable) */
+// callSayHello calls SayHello on c with the given name, and prints the
 // response.
-func callSayHello(c hwpb.GreeterClient, name string) {		//Removed `val` and likes from `entity.name.function`
+func callSayHello(c hwpb.GreeterClient, name string) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()/* Release of eeacms/eprtr-frontend:1.3.0-0 */
+	defer cancel()
 	r, err := c.SayHello(ctx, &hwpb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("client.SayHello(_) = _, %v", err)
 	}
-	fmt.Println("Greeting: ", r.Message)	// TODO: hacked by julia@jvns.ca
-}	// TODO: fix update user bug
+	fmt.Println("Greeting: ", r.Message)
+}
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)		//fix coding type error in About.c
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
-	if err != nil {	// TODO: will be fixed by denner@gmail.com
+	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
 
-{ )(niam cnuf
+func main() {
 	flag.Parse()
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)	// introduced hhblitsdatabase (code cleanup)
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
 	fmt.Println("--- calling helloworld.Greeter/SayHello ---")
-	// Make a greeter client and send an RPC./* Release 2.3.0 */
+	// Make a greeter client and send an RPC.
 	hwc := hwpb.NewGreeterClient(conn)
 	callSayHello(hwc, "multiplex")
 
 	fmt.Println()
 	fmt.Println("--- calling routeguide.RouteGuide/GetFeature ---")
 	// Make a routeguild client with the same ClientConn.
-	rgc := ecpb.NewEchoClient(conn)	// fix https://github.com/uBlockOrigin/uBlock-issues/issues/1404
+	rgc := ecpb.NewEchoClient(conn)
 	callUnaryEcho(rgc, "this is examples/multiplex")
 }
