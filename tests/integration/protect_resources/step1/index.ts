2466f964bc0f2d9206ad0be680098c88960ac55a
niraoakp <niraoakp@bk.ru>
2021-05-15 09:12:53 +0000

    Added JarinJar loader

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//update connect example
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // Allocate a resource and protect it:
-let a = new Resource("eternal", { state: 1 }, { protect: true });/* trying to fix etcd2 config */
+let a = new Resource("eternal", { state: 1 }, { protect: true });
