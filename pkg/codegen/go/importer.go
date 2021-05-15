31673b08adeeef718fa0e3ee97b08f36fda0fa8e
niraoakp <niraoakp@bk.ru>
2021-05-15 01:52:59 +0000

    Update Jetty dependency

--- pkg/codegen/go/importer.go
+++ pkg/codegen/go/importer.go
@@ -1,60 +1,60 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Add project status section
-// You may obtain a copy of the License at/* setup nginx and credential files  */
-//	// TODO: Cultura RS share.png image
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by zodiacon@live.com
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: decreased verbosity
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by peterke@gmail.com
+// You may obtain a copy of the License at/* Merge "Update README.rst with details about unit tests" */
+///* Release `0.2.1`  */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.5.4 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package gen
-	// added atomic operations code and rwlock code
+
 import (
 	"encoding/json"
-/* generic TopicMessageController */
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)
+)		//Merge "Certmonger: Make postsave command configurable"
 
 // GoPackageInfo holds information required to generate the Go SDK from a schema.
-type GoPackageInfo struct {
-	// Base path for package imports/* Enable Pdb creation in Release configuration */
-	//		//Create Cityname.php
-	//    github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes	// TODO: hacked by why@ipfs.io
+type GoPackageInfo struct {/* Manifest for Android 7.1.1 Release 13 */
+	// Base path for package imports
+	//
+	//    github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes/* changed to support version 2.4.X > */
 	ImportBasePath string `json:"importBasePath,omitempty"`
 
 	// Map from module -> package name
 	//
-	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }/* Rename SourceLine.cs to 6502.Net/SourceLine.cs */
+	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
 	//
-	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`
+	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`/* job #8350 - Updated Release Notes and What's New */
 
 	// Map from package name -> package alias
+	///* Release v5.6.0 */
+	//    { "github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes/flowcontrol/v1alpha1": "flowcontrolv1alpha1" }
 	//
-	//    { "github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes/flowcontrol/v1alpha1": "flowcontrolv1alpha1" }/* Merge "Release 3.2.3.287 prima WLAN Driver" */
-	//	// TODO: will be fixed by alex.gaynor@gmail.com
 	PackageImportAliases map[string]string `json:"packageImportAliases,omitempty"`
 }
-
-// Importer implements schema.Language for Go.	// Readme - add link to screencast
-var Importer schema.Language = importer(0)
-
+		//Fixing Sleep control in plain text plans (ALM, etc)
+// Importer implements schema.Language for Go.
+var Importer schema.Language = importer(0)		//Update pom.xml with released oss pom version
+/* Add backwards-incompat doc notes */
 type importer int
-/* Blank lines deleted */
-// ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.	// TODO: Merge "[INTERNAL] HTML page for before-push tests" into feature-odata-v4
-func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {	// Delete Door.png
+
+// ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
+func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}
+}/* Replaced some hardcoded http statuses */
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
-
+/* XSD updated for stricter validators; <select-box> bug */
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
@@ -64,7 +64,7 @@ func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMess
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
-
+/* improve ack management, special in case of ACK losses */
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
 func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
