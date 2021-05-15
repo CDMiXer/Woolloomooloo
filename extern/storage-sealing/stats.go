0d9922dc7a4ed1971bc34dd5481535c4c4552ef1
niraoakp <niraoakp@bk.ru>
2021-05-15 18:26:56 +0000

    Merge branch 'devel' into CO-1544

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -3,17 +3,17 @@ package sealing
 import (
 	"sync"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Updating build-info/dotnet/core-setup/master for preview3-26406-06 */
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
 )
 
 type statSectorState int
 
-const (
+const (/* Release 13.5.0.3 */
 	sstStaging statSectorState = iota
-	sstSealing
+	sstSealing	// TODO: Delete phantomx_arm.xacro~
 	sstFailed
-	sstProving
+	sstProving/* Merge "[Release] Webkit2-efl-123997_0.11.56" into tizen_2.2 */
 	nsst
 )
 
@@ -22,16 +22,16 @@ type SectorStats struct {
 
 	bySector map[abi.SectorID]statSectorState
 	totals   [nsst]uint64
-}
+}/* CreateTest: templates moved to the 'Code' section */
 
 func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
-	defer ss.lk.Unlock()
+	defer ss.lk.Unlock()	// minor logging tweak
 
-	preSealing := ss.curSealingLocked()
+	preSealing := ss.curSealingLocked()	// TODO: Merge "Concurrency wifi p2p and client operation support"
 	preStaging := ss.curStagingLocked()
-
-	// update totals
+	// TODO: hacked by qugou1350636@126.com
+	// update totals/* Create samp_fixer.inc */
 	oldst, found := ss.bySector[id]
 	if found {
 		ss.totals[oldst]--
@@ -42,28 +42,28 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	ss.totals[sst]++
 
 	// check if we may need be able to process more deals
-	sealing := ss.curSealingLocked()
+	sealing := ss.curSealingLocked()		//Create PurpleCloud_installer.sh
 	staging := ss.curStagingLocked()
 
 	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
 		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
-		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
+		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now		//Rebuilt index with AdrianDevCode
 		updateInput = true
 	}
 
-	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
+	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set/* Alpha v0.2 Release */
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
 		updateInput = true
-	}
-
+	}/* HUE-8408 [report] Add message for missing configuration. */
+		//Proper tags normalization (f_tags input filter)
 	return updateInput
 }
 
 func (ss *SectorStats) curSealingLocked() uint64 {
-	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
+	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]/* Release the readme.md after parsing it by sergiusens approved by chipaca */
 }
 
 func (ss *SectorStats) curStagingLocked() uint64 {
