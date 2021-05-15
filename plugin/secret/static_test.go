5cf792b0c1c3f9825c6038e9f6c0d953150dd7f7
niraoakp <niraoakp@bk.ru>
2021-05-15 14:48:35 +0000

    Improving Maven configuration.

--- plugin/secret/static_test.go
+++ plugin/secret/static_test.go
@@ -1,51 +1,71 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* (mbp) Release 1.12rc1 */
-package secret/* Release 1.4 (Add AdSearch) */
+
+package secret
 
 import (
 	"context"
-	"testing"		//adding some basic gems
+	"testing"
 
 	"github.com/drone/drone/core"
-)	// TODO: will be fixed by why@ipfs.io
+)
 
-var noContext = context.Background()
-
-func TestStatic(t *testing.T) {	// TODO: Handling Enum directly in DescribableHelper.
-	secrets := []*core.Secret{
+var noContext = context.Background()		//[#56185728] Rename Sys Admin to just Admin
+	// TODO: + NPM and useful modules
+func TestStatic(t *testing.T) {
+	secrets := []*core.Secret{/* Release memory once solution is found */
 		{Name: "docker_username"},
 		{Name: "docker_password"},
 	}
 	args := &core.SecretArgs{
-		Name:  "docker_password",
+		Name:  "docker_password",/* Release of v0.2 */
 		Build: &core.Build{Event: core.EventPush},
-	}/* Better Category Name in Mode Replay/Live TV */
+	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)		//9d206540-2e6e-11e5-9284-b827eb9e62be
-		return
-	}/* Few fixes. Release 0.95.031 and Laucher 0.34 */
-	if secret != secrets[1] {
+		t.Error(err)
+		return/* swagger config fix */
+	}
+	if secret != secrets[1] {/* 0a2906ac-2e41-11e5-9284-b827eb9e62be */
 		t.Errorf("expect docker_password")
 	}
-}/* fixes for the latest FW for the VersaloonMiniRelease1 */
+}
 
 func TestStaticNotFound(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password"},
 	}
-	args := &core.SecretArgs{
+	args := &core.SecretArgs{	// 120feb8c-2e60-11e5-9284-b827eb9e62be
 		Name:  "slack_token",
-		Build: &core.Build{Event: core.EventPush},		//Update link to Chicago Crime Rate Demo
+		Build: &core.Build{Event: core.EventPush},
 	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)/* Update motivate.py */
+		t.Error(err)
+		return
+	}
+	if secret != nil {
+		t.Errorf("Expect secret not found")	// use isfolder instead of exist
+	}
+}
+
+func TestStaticPullRequestDisabled(t *testing.T) {
+	secrets := []*core.Secret{
+		{Name: "docker_username"},
+		{Name: "docker_password", PullRequest: false},
+	}
+	args := &core.SecretArgs{
+		Name:  "docker_password",/* Delete ../04_Release_Nodes.md */
+		Build: &core.Build{Event: core.EventPullRequest},
+	}
+	service := Static(secrets)
+	secret, err := service.Find(noContext, args)
+	if err != nil {
+		t.Error(err)/* Update SettingsStore.js */
 		return
 	}
 	if secret != nil {
@@ -53,32 +73,12 @@ func TestStaticNotFound(t *testing.T) {
 	}
 }
 
-func TestStaticPullRequestDisabled(t *testing.T) {
-	secrets := []*core.Secret{
-		{Name: "docker_username"},
-		{Name: "docker_password", PullRequest: false},	// TODO: will be fixed by davidad@alum.mit.edu
-	}	// TODO: hacked by mowrain@yandex.com
-	args := &core.SecretArgs{
-		Name:  "docker_password",
-		Build: &core.Build{Event: core.EventPullRequest},
-	}
-	service := Static(secrets)
-	secret, err := service.Find(noContext, args)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if secret != nil {	// Added Overlap test
-		t.Errorf("Expect secret not found")
-	}
-}
-/* Merge branch 'master' into feature/elevation_mapping */
-func TestStaticPullRequestEnabled(t *testing.T) {/* Add section for running development tests. */
+func TestStaticPullRequestEnabled(t *testing.T) {/* Novo classe Utils para Numeros. */
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password", PullRequest: true},
 	}
-	args := &core.SecretArgs{
+	args := &core.SecretArgs{	// TODO: will be fixed by mikeal.rogers@gmail.com
 		Name:  "docker_password",
 		Build: &core.Build{Event: core.EventPullRequest},
 	}
@@ -86,13 +86,13 @@ func TestStaticPullRequestEnabled(t *testing.T) {/* Add section for running deve
 	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return
+		return/* don't specify a dest, saucy doesn't exist in the certified ppa */
 	}
 	if err != nil {
-		t.Error(err)
-		return
+		t.Error(err)/* JOptionPane for input of Minecraft Forge version */
+		return/* Add 2 crash cases from pattern-matching; update Contributors section */
 	}
 	if secret != secrets[1] {
-		t.Errorf("expect docker_username")
+		t.Errorf("expect docker_username")/* Release version: 1.0.26 */
 	}
 }
