f217c0de664e653ed41c01f4206c51c863e6c261
niraoakp <niraoakp@bk.ru>
2021-05-15 10:35:20 +0000

    NGS fixed frame rate

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Extract logic in request sold game worker
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* 203a4d20-2e46-11e5-9284-b827eb9e62be */
+// you may not use this file except in compliance with the License.		//i386: removed multiple warnings
 // You may obtain a copy of the License at
-//
+///* Release for 18.34.0 */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by onhardev@bk.ru
-// distributed under the License is distributed on an "AS IS" BASIS,		//bad vwrsuon
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Correcting set_trace() statement
-// See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Add query cache config for hibernate
-/* Updated All Birds */
+///* Fix quoting in post-checkout hook */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released 0.1.3 */
+// See the License for the specific language governing permissions and	// cfe1ed38-2e59-11e5-9284-b827eb9e62be
+// limitations under the License.
+
 // Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
 // multiple subscribers.
