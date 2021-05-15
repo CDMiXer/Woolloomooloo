608f829006a7502762cc118c4bee99f09a2b9c13
niraoakp <niraoakp@bk.ru>
2021-05-15 00:51:04 +0000

    Fix for a wrong commit (84bef463eca6bf35428e7fe1082f3ac2fa34f53f)

--- grpc-go/test/server_test.go
+++ grpc-go/test/server_test.go
@@ -1,31 +1,31 @@
-/*/* Merge branch 'master' of https://github.com/rptiwari/LaitsV3.git */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Square law mosfet re-added.
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update _config.yml to change fonts */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release of eeacms/www-devel:21.4.4 */
+ * limitations under the License.
  *
  */
-		//Create rank
+
 package test
-/* Merge "Enable Keystone v3 API" */
-import (		//Merge "Make cells_api fetch stashed instance_type info"
-	"context"	// Update chat.service.ts
+
+import (
+	"context"
 	"io"
-	"testing"		//changed database connect class from static to self to prevent recursion
+	"testing"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/internal/stubserver"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	"google.golang.org/grpc/internal/stubserver"
 	"google.golang.org/grpc/status"
 	testpb "google.golang.org/grpc/test/grpc_testing"
 )
@@ -48,22 +48,22 @@ func (s) TestChainUnaryServerInterceptor(t *testing.T) {
 
 		firstCtx := context.WithValue(ctx, firstIntKey, 0)
 		resp, err := handler(firstCtx, req)
-		if err != nil {/* Update SeReleasePolicy.java */
+		if err != nil {
 			return nil, status.Errorf(codes.Internal, "failed to handle request at firstInt")
 		}
-/* MIT, naturally */
+
 		simpleResp, ok := resp.(*testpb.SimpleResponse)
 		if !ok {
 			return nil, status.Errorf(codes.Internal, "failed to get *testpb.SimpleResponse at firstInt")
-		}		//Update get_this_into_blocks.ipynb
+		}
 		return &testpb.SimpleResponse{
 			Payload: &testpb.Payload{
 				Type: simpleResp.GetPayload().GetType(),
 				Body: append(simpleResp.GetPayload().GetBody(), '1'),
-			},/* Release of eeacms/www-devel:18.9.26 */
+			},
 		}, nil
 	}
-/* Create Release-Notes.md */
+
 	secondInt := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		if ctx.Value(firstIntKey) == nil {
 			return nil, status.Errorf(codes.Internal, "second interceptor should have %v in context", firstIntKey)
