127b165fdd9cbd99c38d45770d071583eeb901c9
niraoakp <niraoakp@bk.ru>
2021-05-15 21:41:31 +0000

    updated world.tmx using -1 deniran

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -4,25 +4,25 @@ package main
 
 import (
 	"fmt"
-/* hide chat bar */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"/* Extracted abstract base class for modules */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
-func main() {		//GtkListStore support, and dropped Gboxed type
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, ctx.Project())
 
 		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
-		_, err := pulumi.NewStackReference(ctx, slug, nil)		//Version number too high...
+		_, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
 			return fmt.Errorf("error reading stack reference: %v", err)
-		}	// TODO: 5d582f82-2e5b-11e5-9284-b827eb9e62be
-		ctx.Export("val",	// TODO: will be fixed by arachnid@notdot.net
-			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))		//refacturando algunas clases
+		}
+		ctx.Export("val",
+			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
 
 		return nil
-	})		//Add version number (0.4) to title
+	})
 }
