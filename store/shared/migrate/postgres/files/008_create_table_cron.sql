cdd0fedeb6b175b37bbbbcafa26684ae241947cb
niraoakp <niraoakp@bk.ru>
2021-05-15 08:07:29 +0000

    Update index route default name

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          SERIAL PRIMARY KEY/* IBH-83 Getting environment variable and passing it into the scan process */
+ cron_id          SERIAL PRIMARY KEY
 ,cron_repo_id     INTEGER
-,cron_name        VARCHAR(50)	// TODO: will be fixed by ng8eke@163.com
+,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
-,cron_next        INTEGER/* ncdc_count: Use LazyOutputFormat */
+,cron_next        INTEGER
 ,cron_prev        INTEGER
-,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)/* Release 0.95.200: Crash & balance fixes. */
+,cron_event       VARCHAR(50)/* Update symfony-security-roles-vs-voters.md */
+,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER
-,cron_updated     INTEGER	// TODO: hacked by mikeal.rogers@gmail.com
+,cron_created     INTEGER	// neue plättchen dezenter hervorheben
+,cron_updated     INTEGER
 ,cron_version     INTEGER
-,UNIQUE(cron_repo_id, cron_name)
+,UNIQUE(cron_repo_id, cron_name)/* remove top margin on delete button */
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);		//Cleanup in onBlockBreak()
+);
 
 -- name: create-index-cron-repo
 
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
+		//Updated performance counter doc (closes #43)
+-- name: create-index-cron-next
 
--- name: create-index-cron-next		//updated readme.md with wappservice
-/* [Automated] [zoren] New translations */
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);/* Adapt to recent changes to upstream request */
