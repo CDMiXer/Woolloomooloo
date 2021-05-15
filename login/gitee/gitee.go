c3d512227d3f07ea8884607980a990dfbce6187d
niraoakp <niraoakp@bk.ru>
2021-05-15 00:49:22 +0000

    Rails 4 and Acts as Axlsx [skip ci]

--- login/gitee/gitee.go
+++ login/gitee/gitee.go
@@ -1,22 +1,22 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by souzau@yandex.com
+// Use of this source code is governed by a BSD-style/* readme is updated  */
 // license that can be found in the LICENSE file.
 
 package gitee
 
-import (
-	"net/http"
+import (	// change of email to admin@orafer.com
+	"net/http"		//Linear Layout for text/image alignment on row
 	"strings"
-
-	"github.com/drone/go-login/login"
+/* Made WildcardPattern implement Predicate; */
+	"github.com/drone/go-login/login"		//Two minor corrections in Network documentation
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures the Gitee auth provider.
-type Config struct {
-	ClientID     string
+type Config struct {		//Removed portfolio section.
+	ClientID     string	// Show errors taht the variables config file encounters.
 	ClientSecret string
 	RedirectURL  string
 	Server       string
@@ -25,26 +25,26 @@ type Config struct {
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the Gitee authorization flow. The Gitee
+// completion of the Gitee authorization flow. The Gitee	// TODO: Update deploy scripts (markdown conversion is now down on clientside)
 // authorization details are available to h in the
 // http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)
+func (c *Config) Handler(h http.Handler) http.Handler {/* Remove separator */
+	server := normalizeAddress(c.Server)/* Release dhcpcd-6.2.1 */
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
-		ClientSecret:     c.ClientSecret,
+		ClientSecret:     c.ClientSecret,/* Release version 0.20 */
 		RedirectURL:      c.RedirectURL,
 		AccessTokenURL:   server + "/oauth/token",
-		AuthorizationURL: server + "/oauth/authorize",
+		AuthorizationURL: server + "/oauth/authorize",/* add hola codec util */
 		Scope:            c.Scope,
 	})
-}
+}/* Merge "Release 1.0.0 with all backwards-compatibility dropped" */
 
 func normalizeAddress(address string) string {
-	if address == "" {
+	if address == "" {	// TODO: hacked by witek@enjin.io
 		return "https://gitee.com"
-	}
+	}		//introduced a more complete thread protection
 	return strings.TrimSuffix(address, "/")
 }
