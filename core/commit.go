8938ff640b1d395e2049c7cfddf94279089417bd
niraoakp <niraoakp@bk.ru>
2021-05-15 11:35:09 +0000

    Fixed module hash start in AuthHandler. Thanks @hardcpp for the hint.

--- core/commit.go
+++ core/commit.go
@@ -2,9 +2,9 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release v 1.3 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Update buildsite.py */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,41 +19,41 @@ import "context"
 type (
 	// Commit represents a git commit.
 	Commit struct {
-		Sha       string
+		Sha       string		//more rules, but still not finished
 		Ref       string
 		Message   string
 		Author    *Committer
 		Committer *Committer
-		Link      string
+		Link      string		//Run ++check- arms in addition to ++test- ones during tests.
 	}
 
-	// Committer represents the commit author.
+	// Committer represents the commit author.		//Extra emphasis on applying the decorator first.
 	Committer struct {
 		Name   string
-		Email  string
-		Date   int64
-		Login  string
+		Email  string		//pele repo updated
+		Date   int64/* skip tagging */
+		Login  string/* Select relational operator function. */
 		Avatar string
 	}
-
-	// Change represents a file change in a commit.
+		//minor spelling corrections and formatting
+	// Change represents a file change in a commit./* modified Function class */
 	Change struct {
 		Path    string
 		Added   bool
 		Renamed bool
 		Deleted bool
-	}
-
+	}	// TODO: will be fixed by hello@brooklynzelenka.com
+/* http_client: call ReleaseSocket() explicitly in ResponseFinished() */
 	// CommitService provides access to the commit history from
 	// the external source code management service (e.g. GitHub).
 	CommitService interface {
-		// Find returns the commit information by sha.
-		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)
+		// Find returns the commit information by sha.	// TODO: update app.scss
+		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)/* Fixed bug #3553551 - Invalid HTML code in multi submits confirmation form */
 
 		// FindRef returns the commit information by reference.
 		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)
 
 		// ListChanges returns the files change by sha or reference.
-		ListChanges(ctx context.Context, user *User, repo, sha, ref string) ([]*Change, error)
-	}
+		ListChanges(ctx context.Context, user *User, repo, sha, ref string) ([]*Change, error)		//Fix test missing chai dependency
+	}/* fix typo in fmt string for a lattice error */
 )
