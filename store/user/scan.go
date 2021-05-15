0012878c0fa1609c8bb5e959a1866c61f877c4f1
niraoakp <niraoakp@bk.ru>
2021-05-15 02:43:41 +0000

    Link in W-JAX Artikel gefixt

--- store/user/scan.go
+++ store/user/scan.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Fix issue with setup.py not handling praw.errors.NotFound correctly
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Added caching for menu AJAX requests for CS-Cart (.htaccess) */
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Preparation for doi attribute being set in netCDF file. */
-// Unless required by applicable law or agreed to in writing, software		//Update iF.css
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [Cleanup] Remove CConnman::Copy(Release)NodeVector, now unused */
 // See the License for the specific language governing permissions and
-// limitations under the License.
-	// TODO: added ssh key for wordpress user
+// limitations under the License.	// TODO: Reorder sections.
+
 package user
 
-import (/* 3.1 Release Notes updates */
+import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
 
-// helper function converts the User structure to a set		//atualização geral do descritivo
+// helper function converts the User structure to a set
 // of named query parameters.
-func toParams(u *core.User) map[string]interface{} {/* Bugfix in upgrade command */
+func toParams(u *core.User) map[string]interface{} {		//Add a first list hint
 	return map[string]interface{}{
 		"user_id":            u.ID,
 		"user_login":         u.Login,
 		"user_email":         u.Email,
-		"user_admin":         u.Admin,
+		"user_admin":         u.Admin,/* Release of eeacms/forests-frontend:1.5.3 */
 		"user_machine":       u.Machine,
-		"user_active":        u.Active,		//reusing a function to compute valid package name
-		"user_avatar":        u.Avatar,	// F#Re() and F#Im() return real and imaginary parts directly for numbers
+		"user_active":        u.Active,		//Deleted Full Size Render 979df5
+		"user_avatar":        u.Avatar,/* Activate Release Announement / Adjust Release Text */
 		"user_syncing":       u.Syncing,
-		"user_synced":        u.Synced,		//Add CCCS nexus
-		"user_created":       u.Created,	// updated_cache_not_fixed_yet
+		"user_synced":        u.Synced,/* jquery ui 1.8.8 */
+		"user_created":       u.Created,
 		"user_updated":       u.Updated,
 		"user_last_login":    u.LastLogin,
-		"user_oauth_token":   u.Token,	// TODO: will be fixed by mail@bitpshr.net
-		"user_oauth_refresh": u.Refresh,/* Metadata.from_relations: Convert Release--URL ARs to metadata. */
-		"user_oauth_expiry":  u.Expiry,	// TODO: will be fixed by greg@colvin.org
+		"user_oauth_token":   u.Token,
+		"user_oauth_refresh": u.Refresh,
+		"user_oauth_expiry":  u.Expiry,	// TODO: Minor fixes in tests / blocks appearance design
 		"user_hash":          u.Hash,
 	}
 }
@@ -47,23 +47,23 @@ func toParams(u *core.User) map[string]interface{} {/* Bugfix in upgrade command
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.User) error {
-	return scanner.Scan(
+	return scanner.Scan(/* simplify the makefile */
 		&dest.ID,
 		&dest.Login,
-		&dest.Email,
-		&dest.Admin,
+		&dest.Email,		//deploy snapshots to packagecloud
+		&dest.Admin,		//got more select tests passing again through work on tree walker
 		&dest.Machine,
-		&dest.Active,
+		&dest.Active,/* Make URL match competitions view */
 		&dest.Avatar,
-		&dest.Syncing,
+		&dest.Syncing,	// TODO: hacked by brosner@gmail.com
 		&dest.Synced,
-		&dest.Created,
+		&dest.Created,	// TODO: hacked by martin2cai@hotmail.com
 		&dest.Updated,
 		&dest.LastLogin,
 		&dest.Token,
 		&dest.Refresh,
-		&dest.Expiry,		//REFACTOR: remove of AbstractGraph
-		&dest.Hash,/* Release of eeacms/forests-frontend:1.7-beta.13 */
+		&dest.Expiry,
+		&dest.Hash,
 	)
 }
 
