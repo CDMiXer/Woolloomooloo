f652d5cf5e5b8604e8535cc9a6a2b11ede1bb1be
niraoakp <niraoakp@bk.ru>
2021-05-15 19:47:43 +0000

    refactoring: fix name of test case

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,21 +1,21 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package config/* corrections in the computation of the likelihood. */
+package config
 
 import (
 	"context"
 	"errors"
 	"testing"
-
+/* First Beta Release */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
 
-var noContext = context.TODO()		//Merge "Revert "Removing unnecessary casts to int64_t.""
+var noContext = context.TODO()
 
 var mockFile = []byte(`
 kind: pipeline
@@ -23,12 +23,12 @@ name: default
 
 steps: []
 `)
-
+/* Values added! */
 func TestRepository(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: How to ReadWrite SPIFlash with FlashROM and HydraBus documentation/tutorial
-	args := &core.ConfigArgs{
+
+	args := &core.ConfigArgs{	// TODO: fix input fields use inputTextArea
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
@@ -37,38 +37,38 @@ func TestRepository(t *testing.T) {
 
 	resp := &core.File{Data: mockFile}
 
-	files := mock.NewMockFileService(controller)
+	files := mock.NewMockFileService(controller)/* Release: version 1.4.2. */
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
-/* Update README; mention dvipng, and reorder items to make more sense */
-	service := Repository(files)
-	result, err := service.Find(noContext, args)
+
+	service := Repository(files)	// websites.factorcode: add screenshots
+	result, err := service.Find(noContext, args)		//Bugfix in book view (changing tags on page load)
 	if err != nil {
-		t.Error(err)
-	}		//Updating build-info/dotnet/core-setup/master for alpha.1.19563.6
-	// TODO: N2nLocaleMag null form value bug fixed
-	if result.Data != string(resp.Data) {		//Rename 0461.Hamming Distance.py to 0461_Hamming Distance.py
+		t.Error(err)/* 419afbb3-2e9c-11e5-9bf1-a45e60cdfd11 */
+	}/* fix order of Releaser#list_releases */
+
+	if result.Data != string(resp.Data) {/* Merge branch '0.7' into 0.7.0 */
 		t.Errorf("unexpected file contents")
-	}/* Release PPWCode.Util.AppConfigTemplate version 2.0.1 */
+	}
 }
-	// Update ProceduralFairings (#3985)
+	// TODO: will be fixed by nagydani@epointsystem.org
 func TestRepositoryErr(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* 657c070c-2e73-11e5-9284-b827eb9e62be */
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{
-		User:   &core.User{Login: "octocat"},		//Adapted GpuPacker to new Context structure
+	args := &core.ConfigArgs{	// TODO: hacked by ligi@ligi.de
+		User:   &core.User{Login: "octocat"},		//QueryQuickview: refinements to use outside of Gramps Gtk, eg testing
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build:  &core.Build{After: "6d144de7"},	// TODO: hacked by why@ipfs.io
+		Build:  &core.Build{After: "6d144de7"},/* Release v5.12 */
 		Config: nil,
-	}	// TODO: opening 5.133
+	}
 
 	resp := errors.New("")
 
 	files := mock.NewMockFileService(controller)
-	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)	// TODO: merged anti-crash branch
+	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)
 
-	service := Repository(files)
-	_, err := service.Find(noContext, args)
+	service := Repository(files)	// Seventeenth Lesson
+	_, err := service.Find(noContext, args)/* Merge branch 'master' into fix-taiko-proxies */
 	if err != resp {
 		t.Errorf("expect error returned from file service")
 	}
