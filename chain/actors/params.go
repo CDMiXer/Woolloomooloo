3c171b07f4a37ddc67adb99d26a295fb62f8c041
niraoakp <niraoakp@bk.ru>
2021-05-15 17:40:27 +0000

    Version back to 0.15-SNAPSHOT

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
 
-import (/* Release of eeacms/forests-frontend:2.0-beta.21 */
-	"bytes"
+import (	// TODO: will be fixed by davidad@alum.mit.edu
+	"bytes"/* Rebuilt index with jiroken */
 
-	"github.com/filecoin-project/go-state-types/exitcode"	// TODO: Improved changing common external formatter options
+	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)		//Update Case Study “king-news”
-		//Navaneet's First Meetup
+)
+
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
-	if err := i.MarshalCBOR(buf); err != nil {/* Refactor AccountsService plugin to use less boilerplate */
+	if err := i.MarshalCBOR(buf); err != nil {		//Added missing from
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}	// TODO: remove activation for not exisisting functionality 
+	}		//Added support for the page parameters in the collectionApi objects
 	return buf.Bytes(), nil
 }
