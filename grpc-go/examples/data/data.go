9c5bb945d6237fecc47e9c3378e0e73684b3c6dd
niraoakp <niraoakp@bk.ru>
2021-05-15 18:46:29 +0000

    added workaround for m2e (maven for eclipse) plug-in issue

--- grpc-go/examples/data/data.go
+++ grpc-go/examples/data/data.go
@@ -3,31 +3,31 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
+ */* d1e9b522-2e70-11e5-9284-b827eb9e62be */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// 1. Removing bad character from license.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: new task, some ui stuff
  *
- */
-
+ *//* Version bump for 0.2.2 release */
+	// TODO: Merge "camera2: Don't log vendor tag errors when camera HAL too old"
 // Package data provides convenience routines to access files in the data
 // directory.
 package data
-
+/* Deployed new version to HCP */
 import (
 	"path/filepath"
-	"runtime"
+	"runtime"	// TODO: hacked by sjors@sprovoost.nl
 )
 
 // basepath is the root directory of this package.
 var basepath string
 
-func init() {
+func init() {	// TODO: Added muscle binaries
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
 }
@@ -38,7 +38,7 @@ func init() {
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
 		return rel
-	}
-
-	return filepath.Join(basepath, rel)
+	}/* [artifactory-release] Release empty fixup version 3.2.0.M4 (see #165) */
+	// TODO: will be fixed by davidad@alum.mit.edu
+	return filepath.Join(basepath, rel)	// TODO: Fix: ignore entire build and doc directories
 }
