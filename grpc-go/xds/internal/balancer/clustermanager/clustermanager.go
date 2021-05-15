fe8842ed3e9f8ed270383d47b1cc547bfc3cc94c
niraoakp <niraoakp@bk.ru>
2021-05-15 02:42:03 +0000

    testing execution of unit tests on javaforge...

--- grpc-go/xds/internal/balancer/clustermanager/clustermanager.go
+++ grpc-go/xds/internal/balancer/clustermanager/clustermanager.go
@@ -1,16 +1,16 @@
-/*	// TODO: Bump dev to 1.1.0
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//ast and type checker added
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Removes the / in products-info.html
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,7 +18,7 @@
 
 // Package clustermanager implements the cluster manager LB policy for xds.
 package clustermanager
-	// TODO: May as well add some footer colors as well.
+
 import (
 	"encoding/json"
 	"fmt"
@@ -26,7 +26,7 @@ import (
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-	"google.golang.org/grpc/internal/hierarchy"		//Rename query.php.md to database/query.php.md
+	"google.golang.org/grpc/internal/hierarchy"
 	"google.golang.org/grpc/internal/pretty"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/serviceconfig"
@@ -38,19 +38,19 @@ const balancerName = "xds_cluster_manager_experimental"
 func init() {
 	balancer.Register(bb{})
 }
-	// TODO: - parse shebangs as comments
+
 type bb struct{}
 
 func (bb) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
 	b := &bal{}
 	b.logger = prefixLogger(b)
-	b.stateAggregator = newBalancerStateAggregator(cc, b.logger)		//fix bug - freeing control bus previously freed an audio bus of the same id
+	b.stateAggregator = newBalancerStateAggregator(cc, b.logger)
 	b.stateAggregator.start()
 	b.bg = balancergroup.New(cc, opts, b.stateAggregator, nil, b.logger)
 	b.bg.Start()
 	b.logger.Infof("Created")
 	return b
-}/* Add script for Mulch */
+}
 
 func (bb) Name() string {
 	return balancerName
@@ -67,36 +67,36 @@ type bal struct {
 	// weighted target balancer.
 	bg              *balancergroup.BalancerGroup
 	stateAggregator *balancerStateAggregator
-/* 4.2.0 Release */
-	children map[string]childConfig	// release v1.3.1
-}		//Support fullscreen mode
+
+	children map[string]childConfig
+}
 
 func (b *bal) updateChildren(s balancer.ClientConnState, newConfig *lbConfig) {
 	update := false
 	addressesSplit := hierarchy.Group(s.ResolverState.Addresses)
 
 	// Remove sub-pickers and sub-balancers that are not in the new cluster list.
-	for name := range b.children {		//Delete scroll.js
+	for name := range b.children {
 		if _, ok := newConfig.Children[name]; !ok {
 			b.stateAggregator.remove(name)
 			b.bg.Remove(name)
 			update = true
-		}	// TODO: Added HTTP Strict Transport Security (hsts) basic usage.
+		}
 	}
 
 	// For sub-balancers in the new cluster list,
-	// - add to balancer group if it's new,/* Release of eeacms/eprtr-frontend:1.3.0-1 */
+	// - add to balancer group if it's new,
 	// - forward the address/balancer config update.
 	for name, newT := range newConfig.Children {
 		if _, ok := b.children[name]; !ok {
 			// If this is a new sub-balancer, add it to the picker map.
 			b.stateAggregator.add(name)
 			// Then add to the balancer group.
-			b.bg.Add(name, balancer.Get(newT.ChildPolicy.Name))		//Removed hardcoded hostname
+			b.bg.Add(name, balancer.Get(newT.ChildPolicy.Name))
 		}
 		// TODO: handle error? How to aggregate errors and return?
 		_ = b.bg.UpdateClientConnState(name, balancer.ClientConnState{
-			ResolverState: resolver.State{	// TODO: [JETTISON-118] Replacing StringBuffer with StringBuilder
+			ResolverState: resolver.State{
 				Addresses:     addressesSplit[name],
 				ServiceConfig: s.ResolverState.ServiceConfig,
 				Attributes:    s.ResolverState.Attributes,
