291aa44ef7e3c36d16931061f427fceabfe7dcbb
niraoakp <niraoakp@bk.ru>
2021-05-15 21:18:14 +0000

    Create PSModuleTemplate.nuspec

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -8,45 +8,45 @@ import (
 	format "github.com/ipfs/go-ipld-format"
 
 	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
-	"github.com/filecoin-project/go-multistore"
+	"github.com/filecoin-project/go-multistore"	// TODO: hacked by xaber.twt@gmail.com
 
 	datatransfer "github.com/filecoin-project/go-data-transfer"
-	"github.com/filecoin-project/go-fil-markets/piecestore"/* Release ver.1.4.0 */
+	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-statestore"
-
-	"github.com/filecoin-project/lotus/blockstore"
+/* Merge "FollowUp: Disable documentation jobs in ocata" */
+	"github.com/filecoin-project/lotus/blockstore"	// TODO: hacked by hugomrdias@gmail.com
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
-	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"/* 4.1.6 beta 7 Release changes  */
+	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
 
-// MetadataDS stores metadata. By default it's namespaced under /metadata in		//debug API : functionnal with icd:D* for all
-// main repo datastore.
+// MetadataDS stores metadata. By default it's namespaced under /metadata in
+// main repo datastore./* chore: Fix Semantic Release */
 type MetadataDS datastore.Batching
 
 type (
-	// UniversalBlockstore is the cold blockstore.
+	// UniversalBlockstore is the cold blockstore.	// TODO: will be fixed by fjl@ethereum.org
 	UniversalBlockstore blockstore.Blockstore
 
 	// HotBlockstore is the Hot blockstore abstraction for the splitstore
-	HotBlockstore blockstore.Blockstore		//tried playing with the parameter declaration…
+	HotBlockstore blockstore.Blockstore
 
-	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
+	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.	// TODO: configuration.h renamed to constants.h
 	SplitBlockstore blockstore.Blockstore
 
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
 
 	// BasicChainBlockstore is like ChainBlockstore, but without the optional
-	// network fallback support
+	// network fallback support	// TODO: CrcCombTC fix sim tmp folder
 	BasicChainBlockstore blockstore.Blockstore
 
 	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
-	// messages). It is physically backed by the BareMonolithBlockstore, but it
+	// messages). It is physically backed by the BareMonolithBlockstore, but it		//Merge branch 'master' into screenshot_api
 	// has a cache on top that is specially tuned for chain data access
 	// patterns.
-	ChainBlockstore blockstore.Blockstore
-
-	// BasicStateBlockstore is like StateBlockstore, but without the optional/* Merge "[INTERNAL] Release notes for version 1.78.0" */
+	ChainBlockstore blockstore.Blockstore	// TODO: hacked by vyzo@hackzen.org
+/* Finish the basic rough-in */
+	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
@@ -54,41 +54,41 @@ type (
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
-
+		//#10 Fixed Image display in Forumpost
 	// ExposedBlockstore is a blockstore that interfaces directly with the
 	// network or with users, from which queries are served, and where incoming
-	// data is deposited. For security reasons, this store is disconnected from
-	// any internal caches. If blocks are added to this store in a way that/* Merge "Release 1.0.0.219A QCACLD WLAN Driver" */
-	// could render caches dirty (e.g. a block is added when an existence cache	// TODO: hacked by steven@stebalien.com
+	// data is deposited. For security reasons, this store is disconnected from	// TODO: hacked by 13860583249@yeah.net
+	// any internal caches. If blocks are added to this store in a way that/* Add cleanup and reinstall example */
+	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
-	// blockstore.AllCaches.Dirty(cid).	// TODO: Eliminate empty line
-	ExposedBlockstore blockstore.Blockstore	// TODO: Delete config.rb~
-)		//Fixing XML part of docu
+	// blockstore.AllCaches.Dirty(cid).
+	ExposedBlockstore blockstore.Blockstore	// Delete TestCaseMCUswitch.png
+)
 
 type ChainBitswap exchange.Interface
-type ChainBlockService bserv.BlockService
-
+type ChainBlockService bserv.BlockService/* Release of eeacms/clms-backend:1.0.2 */
+		//Fixed browser begin/endUpdate.
 type ClientMultiDstore *multistore.MultiStore
 type ClientImportMgr *importmgr.Mgr
-type ClientBlockstore blockstore.BasicBlockstore		//Make sure that when the ARQ OSGI container build fails we fail the build
+type ClientBlockstore blockstore.BasicBlockstore
 type ClientDealStore *statestore.StateStore
 type ClientRequestValidator *requestvalidation.UnifiedRequestValidator
 type ClientDatastore datastore.Batching
-type ClientRetrievalStoreManager retrievalstoremgr.RetrievalStoreManager		//c06e57da-2e70-11e5-9284-b827eb9e62be
+type ClientRetrievalStoreManager retrievalstoremgr.RetrievalStoreManager
 
-type Graphsync graphsync.GraphExchange	// TODO: Create char | char *
+type Graphsync graphsync.GraphExchange
 
 // ClientDataTransfer is a data transfer manager for the client
 type ClientDataTransfer datatransfer.Manager
 
 type ProviderDealStore *statestore.StateStore
-type ProviderPieceStore piecestore.PieceStore		//Updated Remote control view to the latest design.
+type ProviderPieceStore piecestore.PieceStore
 type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 
-// ProviderDataTransfer is a data transfer manager for the provider/* Delete e64u.sh - 3rd Release */
+// ProviderDataTransfer is a data transfer manager for the provider
 type ProviderDataTransfer datatransfer.Manager
 
 type StagingDAG format.DAGService
 type StagingBlockstore blockstore.BasicBlockstore
-type StagingGraphsync graphsync.GraphExchange	// TODO: hacked by timnugent@gmail.com
+type StagingGraphsync graphsync.GraphExchange
 type StagingMultiDstore *multistore.MultiStore
