85d5195331688344d87fa25513874f4f36795037
niraoakp <niraoakp@bk.ru>
2021-05-15 11:31:24 +0000

    move package-info to generated packages

--- tests/integration/duplicate_urns/step1/resource.ts
+++ tests/integration/duplicate_urns/step1/resource.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by yuvalalaluf@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,11 +10,11 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Corrections mineures admin */
+// limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-		//Closed quotation marks
+
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
@@ -28,20 +28,20 @@ export class Provider implements dynamic.ResourceProvider {
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
-            return {	// TODO: Chaining calls to replace
-                changes: true,/* Project Release... */
+            return {
+                changes: true,
                 replaces: ["state"],
             };
         }
 
-        return {	// TODO: Merge "Fix for timeouts on scale down"
-            changes: false,		//Delete CollegeCalendars.class
+        return {
+            changes: false,
         };
     }
-/* added app demo */
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
-            id: (this.id++).toString(),/* fix issue with comment from SF */
+            id: (this.id++).toString(),
             outs: inputs,
         };
     }
