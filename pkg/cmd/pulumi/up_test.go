ccbfe21e6afd63a85965fe7c9f4e5dfecebcb56c
niraoakp <niraoakp@bk.ru>
2021-05-15 10:49:38 +0000

    Pin flake8-mutable to latest version 1.2.0

--- pkg/cmd/pulumi/up_test.go
+++ pkg/cmd/pulumi/up_test.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//		//Fixed Whitespace in Widgets
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by yuvalalaluf@gmail.com
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Default child role. */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,35 +21,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidatePolicyPackConfig(t *testing.T) {
+func TestValidatePolicyPackConfig(t *testing.T) {/* Rename ReleaseNotes to ReleaseNotes.md */
 	var tests = []struct {
 		PolicyPackPaths       []string
 		PolicyPackConfigPaths []string
 		ExpectError           bool
 	}{
-		{
+		{	// Separate the controller code that sets the locale
 			PolicyPackPaths:       nil,
 			PolicyPackConfigPaths: nil,
 			ExpectError:           false,
-		},
+		},	// TODO: Fix 32/64-bit confusion on Solaris 10 x86.  Patch from Oliver Jowett.
 		{
 			PolicyPackPaths:       []string{},
-			PolicyPackConfigPaths: []string{},
-			ExpectError:           false,
+			PolicyPackConfigPaths: []string{},	// chore(package): update @types/webpack to version 3.8.0
+			ExpectError:           false,	// TODO: will be fixed by hugomrdias@gmail.com
 		},
 		{
 			PolicyPackPaths:       []string{"foo"},
 			PolicyPackConfigPaths: []string{},
 			ExpectError:           false,
-		},
+		},/* readme: update thanks */
 		{
-			PolicyPackPaths:       []string{"foo", "bar"},
+			PolicyPackPaths:       []string{"foo", "bar"},/* 553697f8-2e6f-11e5-9284-b827eb9e62be */
 			PolicyPackConfigPaths: []string{},
 			ExpectError:           false,
-		},
+		},	// TODO: hacked by souzau@yandex.com
 		{
 			PolicyPackPaths:       []string{"foo"},
-			PolicyPackConfigPaths: []string{"foo"},
+			PolicyPackConfigPaths: []string{"foo"},	// View/AppUsers/add.ctp: submit button
 			ExpectError:           false,
 		},
 		{
@@ -61,13 +61,13 @@ func TestValidatePolicyPackConfig(t *testing.T) {
 			PolicyPackPaths:       []string{"foo", "bar"},
 			PolicyPackConfigPaths: []string{"foo"},
 			ExpectError:           true,
-		},
+		},	// TODO: Use dark color theme in FolioReaderHighlightList when in night mode
 		{
-			PolicyPackPaths:       []string{},
+			PolicyPackPaths:       []string{},	// TODO: will be fixed by witek@enjin.io
 			PolicyPackConfigPaths: []string{"foo"},
-			ExpectError:           true,
+			ExpectError:           true,	// Move initialization of thread's stack to Scheduler::add()
 		},
-		{
+		{/* Release of eeacms/energy-union-frontend:v1.3 */
 			PolicyPackPaths:       []string{"foo"},
 			PolicyPackConfigPaths: []string{"foo", "bar"},
 			ExpectError:           true,
