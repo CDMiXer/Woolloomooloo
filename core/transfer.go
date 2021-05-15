7c862a429376796179fbb4c9008340104ef3387f
niraoakp <niraoakp@bk.ru>
2021-05-15 21:05:42 +0000

    run mojo also needs the debug additional params

--- core/transfer.go
+++ core/transfer.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Improved diagram test
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Update 1.2.0 Release Notes */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by igor@soramitsu.co.jp
+// you may not use this file except in compliance with the License./* widget-registry: convert to C++ */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: INSERT...ON DUPLICATE u pumpy
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 package core
-	// WS-11.0.3 <RIia@Ria-HP Create github_settings.xml
+/* fix integration autocomplete string type */
 import "context"
-
-// Transferer handles transfering repository ownership from one	// TODO: Add more info on Overwatch graphics settings
+/* Add check for NULL in Release */
+// Transferer handles transfering repository ownership from one	// more padding for aliados in carousel
 // user to another user account.
 type Transferer interface {
 	Transfer(ctx context.Context, user *User) error
