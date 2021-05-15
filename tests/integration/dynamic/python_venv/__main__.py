601aa7fe0b217faca160441e879b582bcafc46d5
niraoakp <niraoakp@bk.ru>
2021-05-15 22:22:13 +0000

    Kills/Items/Secrets percentage reported wrong if too high

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -9,13 +9,13 @@ class RandomResourceProvider(ResourceProvider):
     def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
-	// chore(package): update devDependency semantic-release to version 15.11.0
-class Random(Resource):	// TODO: hacked by witek@enjin.io
+
+class Random(Resource):
     val: str
     def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
-r = Random("foo")/* Release: Making ready for next release iteration 6.5.2 */
-/* Prepare Release 0.5.6 */
+r = Random("foo")
+
 export("random_id", r.id)
 export("random_val", r.val)
