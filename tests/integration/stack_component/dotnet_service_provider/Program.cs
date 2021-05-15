02d8bff3e95bd38aa34b85638c62769d71e0e970
niraoakp <niraoakp@bk.ru>
2021-05-15 10:55:12 +0000

    move alignment entry point

--- tests/integration/stack_component/dotnet_service_provider/Program.cs
+++ tests/integration/stack_component/dotnet_service_provider/Program.cs
@@ -1,53 +1,53 @@
-﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//remove xdebug config copy
+﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
-class MyStack : Stack/* Improving README to fit Callisto Release */
-{	// Update S2LoadBalancer.cpp
-    [Output("abc")]
+class MyStack : Stack	// TODO: Update overstroming.rst
+{
+    [Output("abc")]/* Release 3.2.1 */
     public Output<string> Abc { get; private set; }
 
-    [Output]/* Merge branch 'ReleasePreparation' into RS_19432_ExSubDocument */
+    [Output]
     public Output<int> Foo { get; private set; }
 
     // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
-/* Release build of launcher-mac (static link, upx packed) */
+
     public MyStack(Dependency dependency)
     {
         this.Abc = Output.Create(dependency.Abc);
         this.Foo = Output.Create(dependency.Foo);
-        this.Bar = Output.Create(dependency.Bar);
-    }/* added a very simple, partial, Network Manager wrapper */
-}		//Tidy up code and rename project
+        this.Bar = Output.Create(dependency.Bar);	// TODO: Vim: update bundled plugins.
+    }
+}
 
-class Program
+class Program/* GROOVY-3264 : Merge tweak from trunk (cs15094) */
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync<MyStack>(new SampleServiceProvider());
-    }	// All file endings to Unix \LF.
-}
-
+    }
+}	// Merge branch 'azure-pipelines' into masterintoAzure
+/* IHTSDO ms-Release 4.7.4 */
 class Dependency
-{/* Navigation-related changes (2). */
+{
     public string Abc { get; set; } = "ABC";
     public int Foo { get; set; } = 42;
-    public string Bar { get; set; } = "this should not come to output";	// TODO: Remove the source snap-indicator when ungrabbing
+    public string Bar { get; set; } = "this should not come to output";
 }
 
 class SampleServiceProvider : IServiceProvider
 {
-    public object GetService(Type serviceType)
+    public object GetService(Type serviceType)		//twig extension bugfix - while there is no journal role for user
     {
-        if (serviceType == typeof(MyStack))
+        if (serviceType == typeof(MyStack))/* Update CHANGELOG for #10242 */
         {
             return new MyStack(new Dependency()); 
         }
-
-        return null;/* Release candidate 1. */
+/* Rename dokumenter to _dokumenter */
+        return null;
     }
 }
