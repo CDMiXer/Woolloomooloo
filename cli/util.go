e2aa045eddf1bab0f59ff77ed9ace3ea6cc5a995
niraoakp <niraoakp@bk.ru>
2021-05-15 01:40:32 +0000

    Readability improvements to random byte swapper

--- cli/util.go
+++ cli/util.go
@@ -4,22 +4,22 @@ import (
 	"context"
 	"fmt"
 	"time"
-
+		//Fix gallery with default thumbnailPosition
 	"github.com/hako/durafmt"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* A better RSS Reader */
-	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/build"
+
+	"github.com/filecoin-project/lotus/api/v0api"	// TODO: Add string for Settings title #2
+	"github.com/filecoin-project/lotus/build"		//Add Database class.
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
-func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
+	// TODO: Fixed crash in imageloader when feed had no image
+func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {/* refactor: extract methods. */
 	var headers []*types.BlockHeader
-	for _, c := range vals {/* Make dagger modules more consistent (#16) */
-		blkc, err := cid.Decode(c)
-		if err != nil {	// TODO: GitHub -> GitLab
+	for _, c := range vals {
+		blkc, err := cid.Decode(c)	// TODO: will be fixed by alex.gaynor@gmail.com
+		if err != nil {
 			return nil, err
 		}
 
@@ -32,17 +32,17 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 	}
 
 	return types.NewTipSet(headers)
-}/* Create JANE_CASEY_SLOW_03.yaml */
+}
 
 func EpochTime(curr, e abi.ChainEpoch) string {
 	switch {
 	case curr > e:
 		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
-		return fmt.Sprintf("%d (now)", e)		//Rebuilt index with Mahammad8564
-	case curr < e:
+		return fmt.Sprintf("%d (now)", e)
+	case curr < e:/* manual finish of release loop */
 		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
-	}		//Merge "Refactor add nodes operation in cluster action"
+	}
 
 	panic("math broke")
-}
+}	// Update minutes_8.yml
