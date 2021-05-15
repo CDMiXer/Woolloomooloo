2de9f2097a3587ff870e0b09c4b8513e8e5abcc7
niraoakp <niraoakp@bk.ru>
2021-05-15 19:54:39 +0000

    fix(package): update chai to version 4.1.0

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-///* Updated the pypdb feedstock. */
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* v4.4 - Release */
+// Licensed under the Apache License, Version 2.0 (the "License");		//eUcKGjBs9WwaPEUHDgPL5pQyiKMmdztP
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by fjl@ethereum.org
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,24 +17,24 @@
 package collabs
 
 import (
-	"net/http"/* Fixing tabs v spaces formatting issue */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)/* take guetzli Q=84+ */
-
+)
+	// TODO: Remove `current_remote` broken feature
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}
+}	// TODO: will be fixed by steven@stebalien.com
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
-		//2nd upload with extended import functionality
+		//include more one how to create directories, and how to run programs
 func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented/* Release 0.3.0. Add ip whitelist based on CIDR. */
 }
 
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
-}/* chore: bump version to 2.1.1 */
+}
