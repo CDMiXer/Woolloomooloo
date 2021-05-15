9feaafc41ee4eff9f04af0a7428acda9ee5dfe36
niraoakp <niraoakp@bk.ru>
2021-05-15 13:35:51 +0000

    Fix to REST PUT.

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Pre-River Rage Robot Code. This is the code on the robot.
-// +build !oss
 
-package metric
+// +build !oss/* Improve BlankSequence and BlankNullSequence matching */
+
+package metric	// TODO: Delete Home.bpix
