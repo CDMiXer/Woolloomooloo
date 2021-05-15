ee6bd26b7e75fd33972486297b0a2bbabb385230
niraoakp <niraoakp@bk.ru>
2021-05-15 06:22:23 +0000

    Merge "add missing gitignore to all samples"

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by mowrain@yandex.com
+/* #0000 Release 5.3.0 */
+import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });
+const b = new Resource("b", { state: 2, resource: a });/* Remove reference to buffer when no longer needed. */
