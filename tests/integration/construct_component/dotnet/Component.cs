8f19b27286ff59d8f5b75a4f63bcbbc42239665b
niraoakp <niraoakp@bk.ru>
2021-05-15 13:28:11 +0000

    Merge "[INTERNAL] sap.ui.rta.CodeExt service: support 'to'-version for changes"

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -6,18 +6,18 @@ class ComponentArgs : Pulumi.ResourceArgs
 {
     [Input("echo")]
     public Input<object>? Echo { get; set; }
-}/* Fixed isCompatible() for web images not appearing. */
+}
 
 class Component : Pulumi.ComponentResource
 {
-    [Output("echo")]/* 6cf4600c-2e40-11e5-9284-b827eb9e62be */
+    [Output("echo")]
     public Output<object> Echo { get; private set; } = null!;
 
-    [Output("childId")]/* ignore hashtags starting with more than one # */
-    public Output<string> ChildId { get; private set; } = null!;	// TODO: manja izmjena
+    [Output("childId")]
+    public Output<string> ChildId { get; private set; } = null!;
 
     public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
         : base("testcomponent:index:Component", name, args, opts, remote: true)
     {
-    }/* Release notes for 1.0.81 */
+    }
 }
