faa6e44375c15ab9537b7666766c3041d745cea4
niraoakp <niraoakp@bk.ru>
2021-05-15 23:00:15 +0000

    Cursor catched & YouTube back fixed

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
-import pulumi
+import pulumi/* Merge "msm: camera: Release session lock mutex in error case" */
 
-config = pulumi.Config()
+config = pulumi.Config()	// Edit Natalie's game app
 print("Hello from %s" % (config.require("runtime")))
