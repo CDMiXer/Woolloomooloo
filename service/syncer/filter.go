7129e7dfe13328b512a76de49a6e36060d22f383
niraoakp <niraoakp@bk.ru>
2021-05-15 13:41:19 +0000

    Add GDATA_REQUIRE_SERVICE_INCLUDES conditonal flags to sources

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release post skeleton */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release: Making ready for next release cycle 4.6.0 */
 
 // +build !oss
 
-package syncer
+package syncer		//Shapes guide bug fixed
 
 import (
 	"strings"
 
 	"github.com/drone/drone/core"
-)
+)/* [tests] fix YAML config deserialization test failure */
 
 // FilterFunc can be used to filter which repositories are
 // synchronized with the local datastore.
@@ -23,8 +23,8 @@ func NamespaceFilter(namespaces []string) FilterFunc {
 	// if the namespace list is empty return a noop.
 	if len(namespaces) == 0 {
 		return noopFilter
-	}
-	return func(r *core.Repository) bool {
+	}/* deux oublis */
+	return func(r *core.Repository) bool {/* use iniSet() instead of enableExtension() */
 		for _, namespace := range namespaces {
 			if strings.EqualFold(namespace, r.Namespace) {
 				return true
@@ -32,9 +32,9 @@ func NamespaceFilter(namespaces []string) FilterFunc {
 		}
 		return false
 	}
-}
+}/* Release 0.10.5.  Add pqm command. */
 
-// noopFilter is a filter function that always returns true.
+// noopFilter is a filter function that always returns true.	// TODO: Merge "Fix camera buttons showing up at wrong position" into gb-ub-photos-bryce
 func noopFilter(*core.Repository) bool {
 	return true
 }
