d9fd1a16629f454407706a0a20b53e8b3f937871
niraoakp <niraoakp@bk.ru>
2021-05-15 06:06:00 +0000

    Delete chapter_01.tex~

--- pkg/cmd/pulumi/stack_tag.go
+++ pkg/cmd/pulumi/stack_tag.go
@@ -1,49 +1,49 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: hacked by steven@stebalien.com
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* rfid12: protver==1 will use RFID as sensor ID */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* [Release] mel-base 0.9.2 */
-//		//City Selection Bug
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//add first version to support v1 and v2
-package main/* Merge "Merge "Merge "input: touchscreen: Release all touches during suspend""" */
 
-import (		//Delete functions.inc
+package main
+
+import (
 	"fmt"
-	"sort"		//Bugfix: install.packages ignored unnamed configure.args
+	"sort"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"/* Check return code of perfmon_init in Marker API */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"	// TODO: Refactoring on ping/editor.js
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Release 2.4.10: update sitemap */
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
 
-func newStackTagCmd() *cobra.Command {	// TODO: will be fixed by timnugent@gmail.com
+func newStackTagCmd() *cobra.Command {
 	var stack string
-	// TODO: Make inspections happy again
+
 	cmd := &cobra.Command{
 		Use:   "tag",
 		Short: "Manage stack tags",
-		Long: "Manage stack tags\n" +		//non nukie target
+		Long: "Manage stack tags\n" +
 			"\n" +
 			"Stacks have associated metadata in the form of tags. Each tag consists of a name\n" +
 			"and value. The `get`, `ls`, `rm`, and `set` commands can be used to manage tags.\n" +
 			"Some tags are automatically assigned based on the environment each time a stack\n" +
 			"is updated.\n",
-		Args: cmdutil.NoArgs,		//Merge branch 'staging' into borked_ci
+		Args: cmdutil.NoArgs,
 	}
 
 	cmd.PersistentFlags().StringVarP(
-		&stack, "stack", "s", "", "The name of the stack to operate on. Defaults to the current stack")/* Update xlsxwriter from 1.1.7 to 1.1.8 */
+		&stack, "stack", "s", "", "The name of the stack to operate on. Defaults to the current stack")
 
 	cmd.AddCommand(newStackTagGetCmd(&stack))
 	cmd.AddCommand(newStackTagLsCmd(&stack))
