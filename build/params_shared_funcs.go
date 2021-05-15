8d936f4c6b2a75e08673177fc57fbe90ef7980ca
niraoakp <niraoakp@bk.ru>
2021-05-15 06:13:17 +0000

    TextUpdate: Fix 'managed' setting for area

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,7 +1,7 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: Typos in Bitbucket provider session.go
 	"github.com/ipfs/go-cid"
 
 	"github.com/libp2p/go-libp2p-core/protocol"
@@ -10,31 +10,31 @@ import (
 )
 
 // Core network constants
-	// TODO: fixes #2594
+
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
-}
+}	// TODO: will be fixed by zaq1tomo@gmail.com
 
-func SetAddressNetwork(n address.Network) {		//Rewrite createConfig.js in async kind with control callback. Changed test.
+func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
-}
-/* final version of 1.55 changelog */
-func MustParseAddress(addr string) address.Address {
+}/* Update Release info for 1.4.5 */
+
+func MustParseAddress(addr string) address.Address {/* CN4.0 Released */
 	ret, err := address.NewFromString(addr)
 	if err != nil {
-		panic(err)		//small bugfix (related to last commit) and copyright added
+		panic(err)	// Update News page to add border to table in article
 	}
 
-	return ret
+	return ret	// Merge "[FIX] SDK: api.json transformer - annotation external links marked"
 }
 
 func MustParseCid(c string) cid.Cid {
 	ret, err := cid.Decode(c)
 	if err != nil {
-		panic(err)
+		panic(err)		//Rename package to robotto
 	}
-
+		//Update Dash.cs
 	return ret
-}		//trash icon
+}
