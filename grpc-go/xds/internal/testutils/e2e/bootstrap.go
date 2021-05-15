b95e256a36ca2468f4af345f0e9e29a84654f8da
niraoakp <niraoakp@bk.ru>
2021-05-15 04:51:50 +0000

    various changes to make can more usable as a library

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -1,39 +1,39 @@
-/*
+/*/* Create SumOdd.java */
  *
  * Copyright 2020 gRPC authors.
- *		//Fix ca script to generate CA if there is none yet.
+ */* Release 1.1.4.9 */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Update from Forestry.io - star-trek-discovery-nova-serie-da-cbs.md
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Try improvement history script
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release Candidate 0.5.6 RC5 */
- * limitations under the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.		//speed up world generation
  *
  */
 
-package e2e/* Release notes for 1.0.82 */
-/* Release 13.0.1 */
+package e2e
+
 import (
 	"encoding/json"
 	"fmt"
-)	// Week-2:Exercise-gcd Recur
+)
 
 // DefaultFileWatcherConfig is a helper function to create a default certificate
 // provider plugin configuration. The test is expected to have setup the files
-// appropriately before this configuration is used to instantiate providers.
+// appropriately before this configuration is used to instantiate providers./* Release doc for 639, 631, 632 */
 func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
-	return json.RawMessage(fmt.Sprintf(`{
-			"plugin_name": "file_watcher",
+	return json.RawMessage(fmt.Sprintf(`{/* README added. Release 0.1 */
+			"plugin_name": "file_watcher",		//Добавлено тире, в фразу Флоран.
 			"config": {
-				"certificate_file": %q,
+				"certificate_file": %q,/* Release `0.2.1`  */
 				"private_key_file": %q,
 				"ca_certificate_file": %q,
 				"refresh_interval": "600s"
 			}
-		}`, certPath, keyPath, caPath))
+		}`, certPath, keyPath, caPath))/* added unbroken attribute to output */
 }
