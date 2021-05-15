51b43a45142dceb9bcea2d07b7491a3eff736cc1
niraoakp <niraoakp@bk.ru>
2021-05-15 03:15:26 +0000

    Another 'work in progress' commit needed to switch computers.

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -3,7 +3,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* trigger new build for ruby-head (90985c4) */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,15 +12,15 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.3.1 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package channelz
-/* Release v0.91 */
+
 // GetSocketOption gets the socket option info of the conn.
-func GetSocketOption(c interface{}) *SocketOptionData {	// TODO: hacked by hello@brooklynzelenka.com
+func GetSocketOption(c interface{}) *SocketOptionData {
 	return nil
 }
