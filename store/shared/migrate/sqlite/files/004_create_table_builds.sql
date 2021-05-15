d563332dcadbcba90700fc5b9f88f68303c2f9c4
niraoakp <niraoakp@bk.ru>
2021-05-15 11:16:49 +0000

    Merge "Enable consoleauth service during ec2 tests"

--- store/shared/migrate/sqlite/files/004_create_table_builds.sql
+++ store/shared/migrate/sqlite/files/004_create_table_builds.sql
@@ -1,12 +1,12 @@
--- name: create-table-builds		//test 404 page with video
+-- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
  build_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,build_repo_id       INTEGER
 ,build_trigger       TEXT
 ,build_number        INTEGER
-,build_parent        INTEGER	// Merge branch 'master' into bl-rd
-,build_status        TEXT/* Merge branch 'master' into ChangesNews */
+,build_parent        INTEGER
+,build_status        TEXT
 ,build_error         TEXT
 ,build_event         TEXT
 ,build_action        TEXT
@@ -15,15 +15,15 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_title         TEXT
 ,build_message       TEXT
 ,build_before        TEXT
-,build_after         TEXT/* Update app.h */
-,build_ref           TEXT/* Update target definitions following the KNIME 3.6 Release */
+,build_after         TEXT
+,build_ref           TEXT
 ,build_source_repo   TEXT
 ,build_source        TEXT
 ,build_target        TEXT
 ,build_author        TEXT
 ,build_author_name   TEXT
-TXET  liame_rohtua_dliub,
-,build_author_avatar TEXT	// add getEdges
+,build_author_email  TEXT
+,build_author_avatar TEXT
 ,build_sender        TEXT
 ,build_deploy        TEXT
 ,build_params        TEXT
@@ -39,7 +39,7 @@ TXET  liame_rohtua_dliub,
 -- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-	// TODO: will be fixed by peterke@gmail.com
+
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
@@ -48,7 +48,7 @@ CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
 
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
--- name: create-index-builds-ref	// TODO: Merge "b/5453320 Clear new repeat settings if user cancels change" into ics-mr1
+-- name: create-index-builds-ref
 
 CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
 
