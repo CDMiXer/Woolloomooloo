b199c5fb15f56c746956254fc06015db9d35b4d5
niraoakp <niraoakp@bk.ru>
2021-05-15 08:19:58 +0000

    Ajout Amanita fuscozonata

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -5,28 +5,28 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Use upstream lazy static as it has a spin_no_std feature now (#158) */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* dependency management -> jatoo-exec */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Binary server is an example server.
+// Binary server is an example server.	// TODO: hacked by witek@enjin.io
 package main
 
-import (
+import (	// Rewrite pawn evaluation from scratch. Tests very well.
 	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 
-	"google.golang.org/grpc"
+	"google.golang.org/grpc"/* Adding ReaktoroInterpreter to the build system. */
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
 
@@ -35,24 +35,24 @@ import (
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
-type ecServer struct {
+type ecServer struct {		//Automatic changelog generation #6507 [ci skip]
 	pb.UnimplementedEchoServer
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {		//Clarify start_date in get_observation queries
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
-func main() {
+func main() {	// TODO: more ws fixes
 	flag.Parse()
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* @quantum5 asketh, and @quantum5 receiveth; #44 */
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
+	}		//Fixed typo in blog title for The Erlangelist
 
 	// Create tls based credential.
-	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
+	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))/* Update example-package-info.lua */
 	if err != nil {
 		log.Fatalf("failed to create credentials: %v", err)
 	}
@@ -60,7 +60,7 @@ func main() {
 	s := grpc.NewServer(grpc.Creds(creds))
 
 	// Register EchoServer on the server.
-	pb.RegisterEchoServer(s, &ecServer{})
+	pb.RegisterEchoServer(s, &ecServer{})/* Armour Manager 1.0 Release */
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
