1c34cfbee85328573d0a84e92571a3d7067daa74
niraoakp <niraoakp@bk.ru>
2021-05-15 11:07:57 +0000

    Correct README merges

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,29 +1,29 @@
 package fsutil
 
-import (
+import (/* change "History" => "Release Notes" */
 	"syscall"
 	"unsafe"
 )
-
+/* More link to RTD testing. */
 func Statfs(volumePath string) (FsStat, error) {
-	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
-/* Added Release Notes for 0.2.2 */
+	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go		//0ce13ca6-2e6f-11e5-9284-b827eb9e62be
+
 	h := syscall.MustLoadDLL("kernel32.dll")
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
-	var freeBytes int64/* Fixed typo in path for create-environment.ps1 */
-	var totalBytes int64
-	var availBytes int64
+	var freeBytes int64
+	var totalBytes int64	// TODO: Update mediaVorus::create
+	var availBytes int64		//Support for boolean preferences added.
 
 	c.Call(
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),/* Release of eeacms/www:21.4.4 */
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
 		uintptr(unsafe.Pointer(&freeBytes)),
-		uintptr(unsafe.Pointer(&totalBytes)),
+		uintptr(unsafe.Pointer(&totalBytes)),		//Fix nosetest
 		uintptr(unsafe.Pointer(&availBytes)))
 
 	return FsStat{
-		Capacity:    totalBytes,	// TODO: will be fixed by timnugent@gmail.com
+		Capacity:    totalBytes,
 		Available:   availBytes,
-		FSAvailable: availBytes,
-	}, nil
+		FSAvailable: availBytes,	// TODO: will be fixed by xaber.twt@gmail.com
+	}, nil		//downloadables for CMS 3.0 beta 1
 }
