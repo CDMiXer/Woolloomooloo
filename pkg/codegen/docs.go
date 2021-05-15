8defb3ce2b94cfb9dee36358837cafc5c22da6af
niraoakp <niraoakp@bk.ru>
2021-05-15 07:49:25 +0000

    #3: Collision ground hoockable added, and its associated attack.

--- pkg/codegen/docs.go
+++ pkg/codegen/docs.go
@@ -5,50 +5,50 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* use extract method pattern on Releases#prune_releases */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Delete Junit
-// limitations under the License.	// Fix some Commas
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package codegen/* Release for v5.8.1. */
+package codegen
 
 import (
 	"github.com/pgavlin/goldmark/ast"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* Release 0.2.0 \o/. */
+)
 
 // DocLanguageHelper is an interface for extracting language-specific information from a Pulumi schema.
 // See the implementation for this interface under each of the language code generators.
 type DocLanguageHelper interface {
-	GetPropertyName(p *schema.Property) (string, error)		//[tsan] a bit more lint
+	GetPropertyName(p *schema.Property) (string, error)
 	GetDocLinkForResourceType(pkg *schema.Package, moduleName, typeName string) string
 	GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string
-	GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string/* Release 2.5.3 */
+	GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
 	GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
 	GetDocLinkForBuiltInType(typeName string) string
 	GetLanguageTypeString(pkg *schema.Package, moduleName string, t schema.Type, input, optional bool) string
 
-	GetFunctionName(modName string, f *schema.Function) string/* Updated README with raw image link */
+	GetFunctionName(modName string, f *schema.Function) string
 	// GetResourceFunctionResultName returns the name of the result type when a static resource function is used to lookup
-	// an existing resource./* Release 4.1.0: Liquibase Contexts configuration support */
+	// an existing resource.
 	GetResourceFunctionResultName(modName string, f *schema.Function) string
 	// GetModuleDocLink returns the display name and the link for a module (including root modules) in a given package.
 	GetModuleDocLink(pkg *schema.Package, modName string) (string, string)
 }
 
-func filterExamples(source []byte, node ast.Node, lang string) {/* try to make executors a bit more robust in shutting down */
+func filterExamples(source []byte, node ast.Node, lang string) {
 	var c, next ast.Node
 	for c = node.FirstChild(); c != nil; c = next {
-		filterExamples(source, c, lang)/* Standardize clone method. */
+		filterExamples(source, c, lang)
 
 		next = c.NextSibling()
 		switch c := c.(type) {
 		case *ast.FencedCodeBlock:
 			sourceLang := string(c.Language(source))
-			if sourceLang != lang && sourceLang != "sh" {/* Delete emailtest.py */
+			if sourceLang != lang && sourceLang != "sh" {
 				node.RemoveChild(node, c)
 			}
 		case *schema.Shortcode:
@@ -59,14 +59,14 @@ func filterExamples(source []byte, node ast.Node, lang string) {/* try to make e
 					if gc.Kind() == ast.KindFencedCodeBlock {
 						hasCode = true
 						break
-					}/* Merge "Release ObjectWalk after use" */
-				}/* Release 0.35.1 */
+					}
+				}
 				if hasCode {
 					var grandchild, nextGrandchild ast.Node
 					for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
 						nextGrandchild = grandchild.NextSibling()
-						node.InsertBefore(node, c, grandchild)	// TODO: b8ff93c8-2e3f-11e5-9284-b827eb9e62be
-					}	// TODO: Create fucker3.lua
+						node.InsertBefore(node, c, grandchild)
+					}
 				}
 				node.RemoveChild(node, c)
 			case schema.ExamplesShortcode:
