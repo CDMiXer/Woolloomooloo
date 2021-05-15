909628293b2e1b3304eef9029fbe2fafa863552b
niraoakp <niraoakp@bk.ru>
2021-05-15 19:26:09 +0000

    3ea69966-2e47-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -1,15 +1,15 @@
 using Pulumi;
-using Aws = Pulumi.Aws;/* Release version 1.4.0. */
-/* added GenerateTasksInRelease action. */
+using Aws = Pulumi.Aws;
+
 class MyStack : Stack
 {
-    public MyStack()/* ef9ee0a4-2e45-11e5-9284-b827eb9e62be */
-    {	// TODO: more work on getting it compiling again
-        // Create a new security group for port 80./* Release 2.0.6. */
+    public MyStack()
+    {
+        // Create a new security group for port 80.
         var securityGroup = new Aws.Ec2.SecurityGroup("securityGroup", new Aws.Ec2.SecurityGroupArgs
         {
             Ingress = 
-            {/* Release 2.0.0: Upgrading to new liquibase-ext-osgi pattern */
+            {
                 new Aws.Ec2.Inputs.SecurityGroupIngressArgs
                 {
                     Protocol = "tcp",
@@ -29,11 +29,11 @@ class MyStack : Stack
                 new Aws.Inputs.GetAmiFilterArgs
                 {
                     Name = "name",
-                    Values = 	// Create FinePrint.version
-                    {	// TODO: will be fixed by boringland@protonmail.ch
+                    Values = 
+                    {
                         "amzn-ami-hvm-*-x86_64-ebs",
                     },
-                },/* Change original MiniRelease2 to ProRelease1 */
+                },
             },
             Owners = 
             {
@@ -41,7 +41,7 @@ class MyStack : Stack
             },
             MostRecent = true,
         }));
-        // Create a simple web server using the startup script for the instance./* Create mbed_Client_Release_Note_16_03.md */
+        // Create a simple web server using the startup script for the instance.
         var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
         {
             Tags = 
@@ -54,17 +54,17 @@ class MyStack : Stack
                 securityGroup.Name,
             },
             Ami = ami.Apply(ami => ami.Id),
-            UserData = @"#!/bin/bash	// TODO: will be fixed by ligi@ligi.de
-echo ""Hello, World!"" > index.html	// TODO: Travis -Xmx4g
+            UserData = @"#!/bin/bash
+echo ""Hello, World!"" > index.html
 nohup python -m SimpleHTTPServer 80 &
 ",
-        });/* Merge branch 'master' into ines */
+        });
         this.PublicIp = server.PublicIp;
         this.PublicHostName = server.PublicDns;
     }
 
-    [Output("publicIp")]/* New Release 1.2.19 */
+    [Output("publicIp")]
     public Output<string> PublicIp { get; set; }
-    [Output("publicHostName")]	// - fixed: typos
+    [Output("publicHostName")]
     public Output<string> PublicHostName { get; set; }
-}/* Release nodes for TVirtualX.h change */
+}
