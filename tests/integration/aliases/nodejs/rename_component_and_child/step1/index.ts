cc0d41ea6bc55f51f8cc010d5accbce0c6b083df
niraoakp <niraoakp@bk.ru>
2021-05-15 10:33:28 +0000

    387ca230-2e59-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// Fix rack_cors setup
+import * as pulumi from "@pulumi/pulumi";
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: 6bdfc966-2e60-11e5-9284-b827eb9e62be
-		//Improve README a bit
 class Resource extends pulumi.ComponentResource {
-{ )snoitpOecruoseRtnenopmoC.imulup :?stpo ,gnirts :eman(rotcurtsnoc    
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
@@ -11,9 +11,9 @@ class Resource extends pulumi.ComponentResource {
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);/* Update sibyl.py */
-        this.resource = new Resource("otherchild", {parent: this});
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Implemented: RFXCom, sTypeTH13/sTypeTH14
+        super("my:module:ComponentFive", name, {}, opts);
+        this.resource = new Resource("otherchild", {parent: this});		//Added delay to Restore
     }
-}	// TODO: cosmetic changes to OSIS RTF filter output
+}
 const comp5 = new ComponentFive("comp5");
