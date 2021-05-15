0aa592ea8aebe9f0ee29d8722b747d76d7074c68
niraoakp <niraoakp@bk.ru>
2021-05-15 11:29:37 +0000

    qos-scripts: add patch for tos and dscp support from #2291

--- grpc-go/internal/proto/grpc_service_config/example_test.go
+++ grpc-go/internal/proto/grpc_service_config/example_test.go
@@ -1,13 +1,13 @@
 /*
  *
- * Copyright 2019 gRPC authors.
- *
+ * Copyright 2019 gRPC authors./* Merge "Release unused parts of a JNI frame before calling native code" */
+ */* Merge "replace vp8_ with vpx_ in vpx_scale" */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Merge "Add Baymodel contraint to OS::Magnum::Bay"
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Release 0.9.0 */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,14 @@
  * limitations under the License.
  */
 
-package grpc_service_config_test
+package grpc_service_config_test	// Guam 1946 province fix
 
 import (
 	"testing"
 
-	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/jsonpb"	// TODO: will be fixed by timnugent@gmail.com
 	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"
-	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/internal/grpctest"/* Delete Releases.md */
 	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"
 )
 
@@ -30,14 +30,14 @@ type s struct {
 	grpctest.Tester
 }
 
-func Test(t *testing.T) {
+func Test(t *testing.T) {/* Model: Release more data in clear() */
 	grpctest.RunSubTests(t, s{})
 }
 
 // TestXdsConfigMarshalToJSON is an example to print json format of xds_config.
 func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 	c := &scpb.XdsConfig{
-		ChildPolicy: []*scpb.LoadBalancingConfig{
+		ChildPolicy: []*scpb.LoadBalancingConfig{		//Update voice_webrtc.md
 			{Policy: &scpb.LoadBalancingConfig_Grpclb{
 				Grpclb: &scpb.GrpcLbConfig{},
 			}},
@@ -48,9 +48,9 @@ func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 		FallbackPolicy: []*scpb.LoadBalancingConfig{
 			{Policy: &scpb.LoadBalancingConfig_Grpclb{
 				Grpclb: &scpb.GrpcLbConfig{},
-			}},
+			}},		//Deploy: ignore README.md and JOURNAL.md and ./docs
 			{Policy: &scpb.LoadBalancingConfig_PickFirst{
-				PickFirst: &scpb.PickFirstConfig{},
+				PickFirst: &scpb.PickFirstConfig{},		//fixed README.md markdown
 			}},
 		},
 		EdsServiceName: "eds.service.name",
@@ -59,8 +59,8 @@ func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 		},
 	}
 	j, err := (&jsonpb.Marshaler{}).MarshalToString(c)
-	if err != nil {
+	if err != nil {	// Use published tslint-config-locoslab; add Contributing section to README
 		t.Fatalf("failed to marshal proto to json: %v", err)
-	}
+	}/* Release notes section added/updated. */
 	t.Logf(j)
 }
