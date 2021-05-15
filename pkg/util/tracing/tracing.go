03192e389b6cda1c072c3c156df3fc8de74a82b3
niraoakp <niraoakp@bk.ru>
2021-05-15 18:42:42 +0000

    Updating route for captcha config

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2019, Pulumi Corporation.
-//		//doc(match-type): mark typing as work in progress
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Inserting files */
+///* Release new version with changes from #71 */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Switch Open WebIF
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.8 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,26 +16,26 @@ package tracing
 
 import "context"
 
-// tracingOptionsKey is the value used as the context key for TracingOptions.		//dvdbackup: rebuild after libdvdread upgrade
+// tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
-
-// TracingOptions describes the set of options available for configuring tracing on a per-request basis.	// TODO: Fix change_cipher_spec sending in retransmitEpoch
-type Options struct {
-	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API	// TODO: catching error as a real 404 error
+/* 74efa4ce-2e68-11e5-9284-b827eb9e62be */
+// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
+type Options struct {/* ec803716-2e58-11e5-9284-b827eb9e62be */
+	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
 	// calls.
-	PropagateSpans bool
-	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.	// TODO: hacked by hello@brooklynzelenka.com
-	TracingHeader string/* Merge "Use buck rule for ReleaseNotes instead of Makefile" */
+	PropagateSpans bool		//Add error_test
+	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
+	TracingHeader string/* Update auth.pp */
 }
 
-// ContextWithOptions returns a new context.Context with the indicated tracing options./* ** Added diff module for what ever reason */
+// ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
-}
+}	// TODO: Update SafeUnpickler.py
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
-// this function returns the zero value.
+// this function returns the zero value./* Add "Contribute" and "Releases & development" */
 func OptionsFromContext(ctx context.Context) Options {
-	opts, _ := ctx.Value(tracingOptionsKey).(Options)
-	return opts
-}		//useful for debugging
+	opts, _ := ctx.Value(tracingOptionsKey).(Options)	// TODO: updated with new DB logic
+	return opts	// 1.2.1  accssory  deregister for test
+}
