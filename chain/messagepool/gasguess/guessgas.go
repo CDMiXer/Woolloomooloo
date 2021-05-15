ed778c64205c4acd3b4ef899c768bcbdb8902459
niraoakp <niraoakp@bk.ru>
2021-05-15 20:17:42 +0000

    Create 04-05-17-understand-programming.markdown

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -1,5 +1,5 @@
 package gasguess
-
+/* Merge "Wlan: Release 3.8.20.11" */
 import (
 	"context"
 
@@ -9,8 +9,8 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"/* Merge "Release 4.0.10.60 QCACLD WLAN Driver" */
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by ng8eke@163.com
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
@@ -21,47 +21,47 @@ type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types
 const failedGasGuessRatio = 0.5
 const failedGasGuessMax = 25_000_000
 
-const MinGas = 1298450
+const MinGas = 1298450/* Removed packages "gimp gmusicbrowser rhythmbox" */
 const MaxGas = 1600271356
 
-type CostKey struct {
+type CostKey struct {	// TODO: will be fixed by 13860583249@yeah.net
 	Code cid.Cid
 	M    abi.MethodNum
-}
+}		//Delete Java-mybatis.html
 
 var Costs = map[CostKey]int64{
 	{builtin0.InitActorCodeID, 2}:          8916753,
 	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
-	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
-	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
-	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
+	{builtin0.StorageMinerActorCodeID, 4}:  2315133,/* Release TomcatBoot-0.3.3 */
+	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,		//98028ec4-2e4d-11e5-9284-b827eb9e62be
+	{builtin0.StorageMinerActorCodeID, 6}:  22864493,	// Corrected spelling, fixed section links
 	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
 	{builtin0.StorageMinerActorCodeID, 10}: 23008274,
 	{builtin0.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin0.StorageMinerActorCodeID, 14}: 566356835,
-	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
+	{builtin0.StorageMinerActorCodeID, 16}: 5325185,/* Merge "Scroll down to show some new categories when they turn up" */
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
-	// TODO: Just reuse v0 values for now, this isn't actually used
+	// TODO: Just reuse v0 values for now, this isn't actually used	// c767647e-2e65-11e5-9284-b827eb9e62be
 	{builtin2.InitActorCodeID, 2}:          8916753,
 	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
-	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
+	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,/* Released 6.0 */
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
-	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
+	{builtin2.StorageMinerActorCodeID, 7}:  142002419,		//I18n::OneSky, (not "Onesky")
 	{builtin2.StorageMinerActorCodeID, 10}: 23008274,
-	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
+	{builtin2.StorageMinerActorCodeID, 11}: 19303178,/* Denote Spark 2.8.0 Release */
 	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
-	{builtin2.StorageMinerActorCodeID, 16}: 5325185,
+	{builtin2.StorageMinerActorCodeID, 16}: 5325185,/* Release 2.4.12: update sitemap */
 	{builtin2.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin2.StoragePowerActorCodeID, 2}:  23600956,
 }
 
 func failedGuess(msg *types.SignedMessage) int64 {
 	guess := int64(float64(msg.Message.GasLimit) * failedGasGuessRatio)
-	if guess > failedGasGuessMax {
+	if guess > failedGasGuessMax {/* Release connections for Rails 4+ */
 		guess = failedGasGuessMax
 	}
 	return guess
