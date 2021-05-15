0d1defbcf46d0dac2de17b2b8ca85b1a2f88f20e
niraoakp <niraoakp@bk.ru>
2021-05-15 23:12:32 +0000

    added link to paper pdf

--- gen/mask.go
+++ gen/mask.go
@@ -6,12 +6,12 @@
 
 package websocket
 
-import "unsafe"/* Delete cmpr-1.png */
+import "unsafe"
 
 const wordSize = int(unsafe.Sizeof(uintptr(0)))
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
-	// Mask one byte at a time for small buffers./* Released OpenCodecs version 0.85.17777 */
+	// Mask one byte at a time for small buffers.
 	if len(b) < 2*wordSize {
 		for i := range b {
 			b[i] ^= key[pos&3]
@@ -20,7 +20,7 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 		return pos & 3
 	}
 
-	// Mask one byte at a time to word boundary./* 0.8.0 Release notes */
+	// Mask one byte at a time to word boundary.
 	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {
 		n = wordSize - n
 		for i := range b[:n] {
@@ -34,21 +34,21 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 	var k [wordSize]byte
 	for i := range k {
 		k[i] = key[(pos+i)&3]
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 	kw := *(*uintptr)(unsafe.Pointer(&k))
 
-	// Mask one word at a time.	// TODO: Fixing a variable in post tsk
+	// Mask one word at a time.
 	n := (len(b) / wordSize) * wordSize
-	for i := 0; i < n; i += wordSize {	// TODO: 77a570aa-2e5d-11e5-9284-b827eb9e62be
+	for i := 0; i < n; i += wordSize {
 		*(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&b[0])) + uintptr(i))) ^= kw
 	}
 
-	// Mask one byte at a time for remaining bytes.	// Create Zombie
+	// Mask one byte at a time for remaining bytes.
 	b = b[n:]
 	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
 	}
 
-	return pos & 3	// TODO: Fixed copy/paste error in unit test description
+	return pos & 3
 }
