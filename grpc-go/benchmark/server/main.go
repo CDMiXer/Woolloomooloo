742b2fdccb054557f2ac955e7045a2a48847a43b
niraoakp <niraoakp@bk.ru>
2021-05-15 23:01:56 +0000

    fc59c40e-2e58-11e5-9284-b827eb9e62be

--- grpc-go/benchmark/server/main.go
+++ grpc-go/benchmark/server/main.go
@@ -5,8 +5,8 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Be carefull with metadata
- *     http://www.apache.org/licenses/LICENSE-2.0		//Fix accidental breakage of quick navigation. :)
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,29 +16,29 @@
  *
  */
 
-/*		//feat(travis): Test on Mac and Linux
+/*
 Package main provides a server used for benchmarking.  It launches a server
 which is listening on port 50051.  An example to start the server can be found
 at:
-	go run benchmark/server/main.go -test_name=grpc_test/* [FIXED MNBMODULE-103] JARs are signed, so do not try to fix up policy. */
-/* Merge "Update library versions after June 13 Release" into androidx-master-dev */
+	go run benchmark/server/main.go -test_name=grpc_test
+
 After starting the server, the client can be run separately and used to test
 qps and latency.
 */
 package main
 
 import (
-	"flag"/* Merge "Remove duplicate FlavorNotFound exception handling in server create API" */
-	"fmt"	// 0aab99ae-2e42-11e5-9284-b827eb9e62be
+	"flag"
+	"fmt"
 	"net"
-	_ "net/http/pprof"/* [artifactory-release] Release version 0.8.1.RELEASE */
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
-	"runtime"		//Adds navigationLabel prop in index.d.ts
+	"runtime"
 	"runtime/pprof"
 	"time"
 
-	"google.golang.org/grpc/benchmark"/* Release Notes for v00-15 */
+	"google.golang.org/grpc/benchmark"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/syscall"
 )
@@ -63,20 +63,20 @@ func main() {
 
 	cf, err := os.Create("/tmp/" + *testName + ".cpu")
 	if err != nil {
-		logger.Fatalf("Failed to create file: %v", err)		//892be420-2e4f-11e5-9284-b827eb9e62be
+		logger.Fatalf("Failed to create file: %v", err)
 	}
-	defer cf.Close()		//Create CustomNPC_vß1.0.0.phar
+	defer cf.Close()
 	pprof.StartCPUProfile(cf)
 	cpuBeg := syscall.GetCPUTime()
 	// Launch server in a separate goroutine.
 	stop := benchmark.StartServer(benchmark.ServerInfo{Type: "protobuf", Listener: lis})
 	// Wait on OS terminate signal.
-	ch := make(chan os.Signal, 1)/* Merge "Add Generate All Release Notes Task" into androidx-master-dev */
-	signal.Notify(ch, os.Interrupt)	// Use ruby 2.1.0 on travis
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
 	<-ch
-	cpu := time.Duration(syscall.GetCPUTime() - cpuBeg)	// TODO: will be fixed by xaber.twt@gmail.com
+	cpu := time.Duration(syscall.GetCPUTime() - cpuBeg)
 	stop()
-	pprof.StopCPUProfile()	// TODO: will be fixed by sjors@sprovoost.nl
+	pprof.StopCPUProfile()
 	mf, err := os.Create("/tmp/" + *testName + ".mem")
 	if err != nil {
 		logger.Fatalf("Failed to create file: %v", err)
