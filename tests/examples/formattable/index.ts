0cdd5a9db598884f2bddbcd5e1458635ce690cd5
niraoakp <niraoakp@bk.ru>
2021-05-15 06:26:02 +0000

    Merge "Don't call AbstractRevision::getContent unless when needed"

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 console.log("Here's some format goo: %s, %v, %q, %d!");
-console.error("Here's some format goo: %s, %v, %q, %d!");	// TODO: will be fixed by peterke@gmail.com
+console.error("Here's some format goo: %s, %v, %q, %d!");
