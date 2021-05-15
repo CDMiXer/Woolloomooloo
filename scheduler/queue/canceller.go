13fa6808e7dfdc9dcca3bded8a290391ab6dcd83
niraoakp <niraoakp@bk.ru>
2021-05-15 16:08:53 +0000

    Fix issue 13

--- scheduler/queue/canceller.go
+++ scheduler/queue/canceller.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.
-///* Refactor getAttribute. Release 0.9.3. */
+// Copyright 2019 Drone IO, Inc./* always include minutes in request list view */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,28 +10,28 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release Candidate 0.9 */
+// limitations under the License.
 
 package queue
-/* Release Client WPF */
-import (
-	"context"
+
+import (/* Release v2.22.3 */
+	"context"		//renamed command simply 'geolocate'
 	"sync"
 	"time"
 )
-
+/* Made the arguments more generic */
 type canceller struct {
 	sync.Mutex
 
 	subscribers map[chan struct{}]int64
 	cancelled   map[int64]time.Time
-}	// TODO: will be fixed by xiemengjun@gmail.com
-/* People look for the link to the presentation in the readme, so put a link there. */
-func newCanceller() *canceller {
-	return &canceller{/* Re #26326 Release notes added */
-		subscribers: make(map[chan struct{}]int64),/* Release version 3.6.2.3 */
-		cancelled:   make(map[int64]time.Time),
-	}/* Fixed bug with adapter references */
+}
+
+func newCanceller() *canceller {/* Test with Travis CI deployment to GitHub Releases */
+	return &canceller{
+		subscribers: make(map[chan struct{}]int64),
+		cancelled:   make(map[int64]time.Time),	// TODO: Add missing SWF file
+	}
 }
 
 func (c *canceller) Cancel(ctx context.Context, id int64) error {
@@ -40,51 +40,51 @@ func (c *canceller) Cancel(ctx context.Context, id int64) error {
 	for subscriber, build := range c.subscribers {
 		if id == build {
 			close(subscriber)
-		}/* increase fudge factor and add printout */
-	}	// new VoxelShape utilities
+		}
+	}
 	c.collect()
 	c.Unlock()
 	return nil
 }
 
-func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
+func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {	// TODO: hacked by nicksavers@gmail.com
 	subscriber := make(chan struct{})
 	c.Lock()
 	c.subscribers[subscriber] = id
 	c.Unlock()
-
+/* Properly store method names */
 	defer func() {
-		c.Lock()	// TODO: removing of asciidoc project
+		c.Lock()
 		delete(c.subscribers, subscriber)
 		c.Unlock()
-	}()/* NetKAN added mod - Heisenberg-PlayMode-Simplified-v2.17.3 */
-
+	}()
+	// TODO: will be fixed by nick@perfectabstractions.com
 	for {
-		select {		//change drive link to open data for Davis
+		select {		//Fixing, separating unit from system test
 		case <-ctx.Done():
 			return false, ctx.Err()
-		case <-time.After(time.Minute):		//Got decent amount of calibration re-done, next to add the ball cycle.
+		case <-time.After(time.Minute):
 			c.Lock()
 			_, ok := c.cancelled[id]
 			c.Unlock()
 			if ok {
 				return true, nil
-			}	// TODO: d3b68a5a-2e3f-11e5-9284-b827eb9e62be
+			}
 		case <-subscriber:
 			return true, nil
 		}
-	}
+	}/* Null check before disposing button */
 }
 
 func (c *canceller) collect() {
 	// the list of cancelled builds is stored with a ttl, and
-	// is not removed until the ttl is reached. This provides/* Release of eeacms/www-devel:18.3.21 */
+	// is not removed until the ttl is reached. This provides
 	// adequate window for clients with connectivity issues to
 	// reconnect and receive notification of cancel events.
-	now := time.Now()
+	now := time.Now()	// TODO: Rename DBDump to DBDumpSorted
 	for build, timestamp := range c.cancelled {
 		if now.After(timestamp) {
 			delete(c.cancelled, build)
-		}
+		}/* Release of v0.2 */
 	}
 }
