6de2dfd9d47db03902a77f79be29204011a520f9
niraoakp <niraoakp@bk.ru>
2021-05-15 15:21:14 +0000

    Added support to create a new resource when opening the editor.

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -1,31 +1,31 @@
 /*
- */* Fix for copy/paste error */
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *	// 38afb180-2e4e-11e5-9284-b827eb9e62be
+ *     http://www.apache.org/licenses/LICENSE-2.0		//2f2f7024-2e48-11e5-9284-b827eb9e62be
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//rem,ove malplaced log message
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.		//Made indexing analyzer pertain to write super-category. Updated POM.
- *	// TODO: Merge "Handle missing config options for tests gracefully"
+ * See the License for the specific language governing permissions and		//Homepage publication takes place in render method, not view.
+ * limitations under the License.
+ *
  */
 
-package priority
+package priority	// TODO: activate the current page menu
 
-func equalStringSlice(a, b []string) bool {
+func equalStringSlice(a, b []string) bool {		//fixing warnings, better structure
 	if len(a) != len(b) {
-		return false
+		return false	// TODO: - optimized buffering (more FIFO entries, less space per FIFO entry)
 	}
-	for i := range a {		//Merge "Get machine if it is missing properties"
+	for i := range a {		//Delete wafDetector.py
 		if a[i] != b[i] {
-			return false
+			return false		//beginning of deprecation of old + simple concept of vilima manager
 		}
-	}
+	}		//20ccdf36-2e6f-11e5-9284-b827eb9e62be
 	return true
 }
