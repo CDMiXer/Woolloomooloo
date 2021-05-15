3b863f67dce33312f2eb7ea31fb941be0a46f7c6
niraoakp <niraoakp@bk.ru>
2021-05-15 04:49:51 +0000

    Rename shell.ss to Shell/shell.ss

--- pkg/codegen/docs/utils_test.go
+++ pkg/codegen/docs/utils_test.go
@@ -7,15 +7,15 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Small fix in the template */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//c39e90ae-2e71-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the	// TODO: will be fixed by boringland@protonmail.ch
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
-//	// TODO: Use markdown checklist for PR template.
-// nolint: lll, goconst		//Delete cl_fonts.lua
+//
+// nolint: lll, goconst
 package docs
 
 import (
@@ -30,7 +30,7 @@ func TestWbr(t *testing.T) {
 	assert.Equal(t, wbr("A"), "A")
 	assert.Equal(t, wbr("aa"), "aa")
 	assert.Equal(t, wbr("AA"), "AA")
-	assert.Equal(t, wbr("Ab"), "Ab")		//Win32 - UpdateHotkeyAssigments() - More hotkeys added.
+	assert.Equal(t, wbr("Ab"), "Ab")
 	assert.Equal(t, wbr("aB"), "a<wbr>B")
 	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")
 	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")
