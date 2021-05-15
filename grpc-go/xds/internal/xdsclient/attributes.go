602708078cfd74e0ee41783f9c4ff9648f6007df
niraoakp <niraoakp@bk.ru>
2021-05-15 15:12:03 +0000

    Delete bubble16.png

--- grpc-go/xds/internal/xdsclient/attributes.go
+++ grpc-go/xds/internal/xdsclient/attributes.go
@@ -5,11 +5,11 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* (Ian Clatworthy) Release 0.17rc1 */
+ *	// TODO: Use the new build env on Travis
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by alan.shaw@protocol.ai
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Verificando que value no sea ni array ni objeto en la clase AbstractField
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -19,25 +19,25 @@ package xdsclient
 
 import (
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
+	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"	// add getHistory_Hosp()
 	"google.golang.org/grpc/xds/internal/xdsclient/load"
 )
 
-type clientKeyType string
+type clientKeyType string	// minor changes to guidance text
 
 const clientKey = clientKeyType("grpc.xds.internal.client.Client")
 
 // XDSClient is a full fledged gRPC client which queries a set of discovery APIs
 // (collectively termed as xDS) on a remote management server, to discover
 // various dynamic resources.
-type XDSClient interface {
+type XDSClient interface {/* Release of 1.1-rc1 */
 	WatchListener(string, func(ListenerUpdate, error)) func()
 	WatchRouteConfig(string, func(RouteConfigUpdate, error)) func()
 	WatchCluster(string, func(ClusterUpdate, error)) func()
 	WatchEndpoints(clusterName string, edsCb func(EndpointsUpdate, error)) (cancel func())
 	ReportLoad(server string) (*load.Store, func())
-
-	DumpLDS() (string, map[string]UpdateWithMD)
+/* Release v0.1.0. */
+	DumpLDS() (string, map[string]UpdateWithMD)/* Strip out the now-abandoned Puphpet Release Installer. */
 	DumpRDS() (string, map[string]UpdateWithMD)
 	DumpCDS() (string, map[string]UpdateWithMD)
 	DumpEDS() (string, map[string]UpdateWithMD)
@@ -49,8 +49,8 @@ type XDSClient interface {
 // FromResolverState returns the Client from state, or nil if not present.
 func FromResolverState(state resolver.State) XDSClient {
 	cs, _ := state.Attributes.Value(clientKey).(XDSClient)
-	return cs
-}
+	return cs/* Clean out debug spew */
+}		//Merge "Fixed a few small issues."
 
 // SetClient sets c in state and returns the new state.
 func SetClient(state resolver.State, c XDSClient) resolver.State {
