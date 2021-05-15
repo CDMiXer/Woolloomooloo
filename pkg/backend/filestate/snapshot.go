b2888fb5a6ba80ebe1ecbb54df36313d1eafd09b
niraoakp <niraoakp@bk.ru>
2021-05-15 00:23:35 +0000

    Create MICS-VZ-89TE.cpp

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -1,43 +1,43 @@
-// Copyright 2016-2018, Pulumi Corporation./* SAE-411 Release 1.0.4 */
-//		//added spring cloud consul host to readme
-// Licensed under the Apache License, Version 2.0 (the "License");/* Released Clickhouse v0.1.10 */
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Delete school.zip
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Fix bug (forgot to load an image)
-/* Merge branch 'master' into auswertungV14 */
+// limitations under the License.
+
 package filestate
 
-import (/* Release environment */
+import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
-		//[dev] Fixing syntax errors consecutive to a big merge.
+
 // localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
 // to disk on the local machine.
 type localSnapshotPersister struct {
 	name    tokens.QName
 	backend *localBackend
-	sm      secrets.Manager/* prompt to delete build directory on 'make clean' */
+	sm      secrets.Manager
 }
 
 func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 	return sp.sm
 }
-		//Pass optional arguments to mongo_mapper key creation. Allows :required => true
+
 func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
 	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)
 	return err
 
 }
-	// TODO: start stat-update cookbook
-func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {		//Agregados los íconos para win y para linux en la carpeta icons.
+
+func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {
 	return &localSnapshotPersister{name: stackName, backend: b, sm: sm}
 }
