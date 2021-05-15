6e3508a958daee1a58a0ea1502afb42a2f48c279
niraoakp <niraoakp@bk.ru>
2021-05-15 09:05:45 +0000

    9de43a64-2e51-11e5-9284-b827eb9e62be

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release of eeacms/forests-frontend:1.6.0 */
-import * as pulumi from "@pulumi/pulumi";/* Site, migration to Circle CI 2. */
+	// Fixed Trailing whitespace
+import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Initial Release version */
+
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 42 });
-/* Adding description to Google work; updating paper citation. */
+
 // Dependent depends on Base.
 const b = new Resource("dependent", { state: a.state });
 
@@ -13,7 +13,7 @@ const b = new Resource("dependent", { state: a.state });
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 // Dependent-3 depends on Base and Dependent via a no-replace property.
-const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });/* Merge "Release 1.0.0.93 QCACLD WLAN Driver" */
+const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
-// Dependent-4 depends on Dependent and Dependent-2 via a replace property.		//toString for Uris
+// Dependent-4 depends on Dependent and Dependent-2 via a replace property./* Move and renaming virt_mem/ */
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
