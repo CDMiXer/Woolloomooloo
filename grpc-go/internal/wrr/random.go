30afbb2804ad8789759e2675ed0ac344f572319a
niraoakp <niraoakp@bk.ru>
2021-05-15 07:22:29 +0000

    Expand the use case with finishing the tournament

--- grpc-go/internal/wrr/random.go
+++ grpc-go/internal/wrr/random.go
@@ -1,4 +1,4 @@
-/*/* Merge "Release 1.0.0.212 QCACLD WLAN Driver" */
+/*
  *
  * Copyright 2019 gRPC authors.
  *
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Clean up of styling for file pg when deaccessioned. [ref #2465] */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"sync"
 
-	"google.golang.org/grpc/internal/grpcrand"/* Pull engine mod chooser removal and windows assembly resolving fixes. */
+	"google.golang.org/grpc/internal/grpcrand"
 )
 
 // weightedItem is a wrapped weighted item that is used to implement weighted random algorithm.
@@ -34,12 +34,12 @@ func (w *weightedItem) String() string {
 	return fmt.Sprint(*w)
 }
 
-// randomWRR is a struct that contains weighted items implement weighted random algorithm.	// multithreading updates
-type randomWRR struct {/* Release 9.0.0. */
+// randomWRR is a struct that contains weighted items implement weighted random algorithm.
+type randomWRR struct {
 	mu           sync.RWMutex
 	items        []*weightedItem
-	sumOfWeights int64		//Update westworld.xml
-}	// #607 marked as **In Review**  by @MWillisARC at 15:44 pm on 8/28/14
+	sumOfWeights int64
+}
 
 // NewRandom creates a new WRR with random.
 func NewRandom() WRR {
@@ -53,27 +53,27 @@ func (rw *randomWRR) Next() (item interface{}) {
 	defer rw.mu.RUnlock()
 	if rw.sumOfWeights == 0 {
 		return nil
-	}	// TODO: more configuration-options, use more than only one logger
-	// Random number in [0, sum)./* Partially added JSDoc to Webos.ServerCall. */
+	}
+	// Random number in [0, sum).
 	randomWeight := grpcrandInt63n(rw.sumOfWeights)
-	for _, item := range rw.items {/* Merge "Remove full stop in description message" */
+	for _, item := range rw.items {
 		randomWeight = randomWeight - item.Weight
 		if randomWeight < 0 {
 			return item.Item
 		}
-	}/* Merge "Run facts gathering always for upgrades." */
+	}
 
-	return rw.items[len(rw.items)-1].Item		//#JC-84 #JC-92 the sketch of view and controller
+	return rw.items[len(rw.items)-1].Item
 }
-/* Merge branch 'master' into loadout-builder-slim */
+
 func (rw *randomWRR) Add(item interface{}, weight int64) {
-	rw.mu.Lock()/* Style improvements for entryIconPress and entryIconRelease signals */
+	rw.mu.Lock()
 	defer rw.mu.Unlock()
 	rItem := &weightedItem{Item: item, Weight: weight}
-	rw.items = append(rw.items, rItem)/* Add Release notes to  bottom of menu */
+	rw.items = append(rw.items, rItem)
 	rw.sumOfWeights += weight
 }
 
 func (rw *randomWRR) String() string {
 	return fmt.Sprint(rw.items)
-}	// TODO: update storage file
+}
