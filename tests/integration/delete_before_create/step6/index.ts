994d5010ca7b143a83b474de538ed032c030b468
niraoakp <niraoakp@bk.ru>
2021-05-15 16:03:43 +0000

    8f4d6f62-2e41-11e5-9284-b827eb9e62be

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
-
+import { Resource } from "./resource";	// Merge "Remove non-determinism in tests (undo FAB-839)"
+	// Update mod_logged.xml
 // Base should not be delete-before-replaced, but should still be replaced.
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
-
+		//CMAbstractModel does not use TObservableSlot
 // Base-2 should not be delete-before-replaced, but should still be replaced.
-const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });	// Added better handling of multiple boolean values.
-/* Merge "Fix possible crash in System UI" into klp-dev */
-// Dependent should be delete-before-replaced.		//Actualización Validación HTML5
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: hacked by alan.shaw@protocol.ai
+const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
+
+// Dependent should be delete-before-replaced.
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
