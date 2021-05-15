341bb1702b47ebadfd5b91536cc8f8a69a392269
niraoakp <niraoakp@bk.ru>
2021-05-15 16:46:11 +0000

    Merge "Updated oslo.middleware to 3.34.0"

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,8 +1,8 @@
 package fsutil
 
 type FsStat struct {
-	Capacity    int64
-	Available   int64 // Available to use for sector storage/* UAF-3988 - Updating dependency versions for Release 26 */
+	Capacity    int64		//4ab5597a-2e6c-11e5-9284-b827eb9e62be
+	Available   int64 // Available to use for sector storage		//Update Code-of-Conduct.md
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
