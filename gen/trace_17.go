67753e1eebb27e41aa506fa15bf10c49b52e0081
niraoakp <niraoakp@bk.ru>
2021-05-15 12:28:59 +0000

    Merge branch 'master' into VecCross

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
 // +build !go1.8
 
 package websocket
-/* fix packaging tag */
+
 import (
 	"crypto/tls"
 	"net/http/httptrace"
-)	// TODO: hacked by magik6k@gmail.com
+)
 
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {	// TODO: Fixed the AMI id in the configuration file.
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	return doHandshake(tlsConn, cfg)
 }
