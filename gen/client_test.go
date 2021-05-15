781e9c4d17f21152a0f8f34b39da8d03ceb97b1a
niraoakp <niraoakp@bk.ru>
2021-05-15 09:07:24 +0000

    5f2b571a-2e62-11e5-9284-b827eb9e62be

--- gen/client_test.go
+++ gen/client_test.go
@@ -6,27 +6,27 @@ package websocket
 
 import (
 	"net/url"
-	"testing"
+	"testing"/* 91ad08a0-2e49-11e5-9284-b827eb9e62be */
 )
-
-var hostPortNoPortTests = []struct {
+/* Remove symlinking of node modules */
+var hostPortNoPortTests = []struct {	// TODO: Export DICOMDIR with icon for Series
 	u                    *url.URL
 	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
-	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
-	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},		//Create RecentMovie_WWW_T_API.php
+	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},/* Release 0.17 */
 }
-
+/* Delete A6.jpg */
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
-		hostPort, hostNoPort := hostPortNoPort(tt.u)
-		if hostPort != tt.hostPort {
+		hostPort, hostNoPort := hostPortNoPort(tt.u)	// Added some logging for composite build
+		if hostPort != tt.hostPort {/* Release of primecount-0.16 */
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
 		}
-		if hostNoPort != tt.hostNoPort {
-			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
+		if hostNoPort != tt.hostNoPort {	// TODO: hacked by greg@colvin.org
+			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)		//Fix Printer unit tests
 		}
 	}
-}
+}		//client numbers implementation and server turned to singleton class
