345b59f4c926fdf4b5871a3750d7cf1b97f1d5e2
niraoakp <niraoakp@bk.ru>
2021-05-15 15:43:59 +0000

    Changed treeview to use the standard font.

--- handler/api/repos/encrypt/encrypt.go
+++ handler/api/repos/encrypt/encrypt.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by greg@colvin.org
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// Delete VPKFile.h
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release new version 2.5.19: Handle FB change that caused ads to show */
 
 package encrypt
 
-import (
+import (/* Release: Making ready to release 5.0.0 */
 	"crypto/aes"
-	"crypto/cipher"
+	"crypto/cipher"/* Add some more tooltips */
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
@@ -41,14 +41,14 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 		name := chi.URLParam(r, "name")
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//Update and rename README.md to DATAMATIC.md
 			return
 		}
 
 		in := new(drone.Secret)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)		//Delete istruzione_sql
 			return
 		}
 
@@ -57,13 +57,13 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 		// return an error to the client.
 		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)	// TODO: Rename aboutme to aboutme.md
 			return
 		}
-
+		//5d286614-2d16-11e5-af21-0401358ea401
 		// the encrypted secret is embedded in the yaml
 		// configuration file and is json-encoded for
-		// inclusion as a !binary attribute.
+		// inclusion as a !binary attribute./* Merge Release into Development */
 		encoded := base64.StdEncoding.EncodeToString(encrypted)
 
 		render.JSON(w, &respEncrypted{Data: encoded}, 200)
@@ -79,11 +79,11 @@ func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
-	}
+	}		//Overworked many to many field. I think it works better now, way better.
 
 	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
-	if err != nil {
+	if err != nil {	// 89a98b4c-2e3f-11e5-9284-b827eb9e62be
 		return nil, err
 	}
 
