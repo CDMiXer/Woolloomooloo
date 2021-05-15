36bc45ebb36d68397ed8a8ed318584b8c7cc4bae
niraoakp <niraoakp@bk.ru>
2021-05-15 05:06:14 +0000

    Delete jquery.pwstabs.js

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,11 +1,11 @@
 package ffiwrapper
 
-import (	// TODO: CITIE-782: apply enumeration scheme also for template IDs
+import (
 	"golang.org/x/xerrors"
 
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
-	"github.com/filecoin-project/go-state-types/abi"/* msw fixes. */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
