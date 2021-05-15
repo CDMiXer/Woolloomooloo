a4629cb2a3dc04c31c86a9d35f7b84b385e05455
niraoakp <niraoakp@bk.ru>
2021-05-15 07:07:55 +0000

    Renamed repository

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,14 +1,14 @@
 package utils
 
-import (	// TODO: will be fixed by why@ipfs.io
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
-	// Added instructions for use.
+
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"	// TODO: will be fixed by timnugent@gmail.com
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
 
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
@@ -16,12 +16,12 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
-			return storagemarket.StorageProviderInfo{}/* Tagged Release 2.1 */
+			return storagemarket.StorageProviderInfo{}
 		}
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
-	return storagemarket.StorageProviderInfo{/* Moved Change Log to Releases page. */
+	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
@@ -29,7 +29,7 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		Addrs:      multiaddrs,
 	}
 }
-		//Update CHANGELOG for #2684
+
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
