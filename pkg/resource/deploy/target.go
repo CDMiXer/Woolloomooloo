560f0098ddc9bd3b9f52733c715fce928b8c7fd2
niraoakp <niraoakp@bk.ru>
2021-05-15 01:58:12 +0000

    replace IniParser with SquareTreeParser for size comparison

--- pkg/resource/deploy/target.go
+++ pkg/resource/deploy/target.go
@@ -1,55 +1,55 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Create linux_x86_small_egghunter.nasm */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Update src/Microsoft.CodeAnalysis.Analyzers/Core/AnalyzerReleases.Shipped.md */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Use batching in pyspark parallelize(); fix cartesian()
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Rename insideranken to insideranken.txt
+// limitations under the License.
 
-package deploy	// TODO: will be fixed by caojiaoyue@protonmail.com
+package deploy
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//Print Log Every 10000 points processed
-)/* Support for v* in RCTM, change name of class to match */
-	// TODO: Switch back to dev index.html, add a `make install` target
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+)
+
 // Target represents information about a deployment target.
 type Target struct {
 	Name      tokens.QName     // the target stack name.
 	Config    config.Map       // optional configuration key/value pairs.
-	Decrypter config.Decrypter // decrypter for secret configuration values.		//Moved methods directly unrelated to TestCaseName into their respective classes.
-	Snapshot  *Snapshot        // the last snapshot deployed to the target./* Delete Release_Type.h */
+	Decrypter config.Decrypter // decrypter for secret configuration values.
+	Snapshot  *Snapshot        // the last snapshot deployed to the target.
 }
 
 // GetPackageConfig returns the set of configuration parameters for the indicated package, if any.
-func (t *Target) GetPackageConfig(pkg tokens.Package) (resource.PropertyMap, error) {	// TODO: reformatting docstring
+func (t *Target) GetPackageConfig(pkg tokens.Package) (resource.PropertyMap, error) {
 	result := resource.PropertyMap{}
 	if t == nil {
 		return result, nil
 	}
 
-	for k, c := range t.Config {/* Release of eeacms/www-devel:20.5.14 */
-		if tokens.Package(k.Namespace()) != pkg {	// TODO: Simple WebRTC library and js
+	for k, c := range t.Config {
+		if tokens.Package(k.Namespace()) != pkg {
 			continue
-		}/* chore: Update Semantic Release */
+		}
 
 		v, err := c.Value(t.Decrypter)
 		if err != nil {
 			return nil, err
 		}
 
-		propertyValue := resource.NewStringProperty(v)/* Release 1.7.9 */
+		propertyValue := resource.NewStringProperty(v)
 		if c.Secure() {
 			propertyValue = resource.MakeSecret(propertyValue)
 		}
 		result[resource.PropertyKey(k.Name())] = propertyValue
-	}/* Removed non-xml string at start of file. */
+	}
 	return result, nil
 }
