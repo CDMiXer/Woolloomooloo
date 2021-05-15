a012f0652df3e61043c3a039c633e268bd80f511
niraoakp <niraoakp@bk.ru>
2021-05-15 06:29:28 +0000

    cc6e0e00-2e51-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// :palm_tree::rage: Updated in browser at strd6.github.io/editor
+	// TODO: Major bug fix on mtcp_safe_pipe() (wasn't actually setting the incoming array)
 import * as pulumi from "@pulumi/pulumi";
-
-let config = new pulumi.Config();
-let org = config.require("org");	// TODO: Fix spurious error in memory.limit (PR#13673), plus some cleanup of the docs
-let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;	// Typo fixed. Thank you @misabear
+/* Release 1.10rc1 */
+let config = new pulumi.Config();/* Release v2.5.1 */
+let org = config.require("org");
+let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
-export const val = ["a", "b"];
+export const val = ["a", "b"];		//Comment line back in
