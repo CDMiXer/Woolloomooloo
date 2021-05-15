dfedbdd0102ec01d6bd05611c1fe4da54aec74b7
niraoakp <niraoakp@bk.ru>
2021-05-15 06:15:01 +0000

    Remove solarized

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail		//Add live support
-
-echo "# This is an auto-generated file. DO NOT EDIT"
+set -eu -o pipefail
+	// TODO: update to Kim Maltman suggestion
+echo "# This is an auto-generated file. DO NOT EDIT"/* Release v*.+.0 */
 cat
