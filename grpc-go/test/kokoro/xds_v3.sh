84fb36cffa6903542617c196be384536fda5b05f
niraoakp <niraoakp@bk.ru>
2021-05-15 05:11:52 +0000

    Adding back allowing custom.css on per template basis for lists.

--- grpc-go/test/kokoro/xds_v3.sh
+++ grpc-go/test/kokoro/xds_v3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh	// TODO: Merge branch 'master' into dependabot/nuget/AWSSDK.SQS-3.3.102.38
+XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh
