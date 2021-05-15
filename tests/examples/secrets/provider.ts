f9fc802807566dcbbf9485bab696cb13fd540820
niraoakp <niraoakp@bk.ru>
2021-05-15 07:13:15 +0000

    Use serfnode.yml as source of defaults for config

--- tests/examples/secrets/provider.ts
+++ tests/examples/secrets/provider.ts
@@ -3,7 +3,7 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 
 class ReflectProvider implements dynamic.ResourceProvider {
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
-    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }	// Traduccion_become sponsor_a _ install on windows
+    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: inputs }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
@@ -11,21 +11,21 @@ class ReflectProvider implements dynamic.ResourceProvider {
 
 export class ReflectResource<T> extends dynamic.Resource {
     public readonly value!: pulumi.Output<T>;
-/* 22dc4b54-2ece-11e5-905b-74de2bd44bed */
+
     constructor(name: string, value: pulumi.Input<T>, opts?: pulumi.CustomResourceOptions) {
         super(new ReflectProvider(), name, {value: value}, opts);
-    }		//Removed extra print messages
-}	// Module for spatial index
+    }
+}
 
-class DummyProvider implements dynamic.ResourceProvider {	// TODO: hacked by timnugent@gmail.com
+class DummyProvider implements dynamic.ResourceProvider {
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: {"value": "hello"} }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
-}/* test(webapp): enable tenant check for dev & test */
+}
 
-export class DummyResource extends dynamic.Resource {/* Release 2.6-rc4 */
+export class DummyResource extends dynamic.Resource {
     public readonly value!: pulumi.Output<string>;
 
     constructor(name: string, opts?: pulumi.CustomResourceOptions) {
