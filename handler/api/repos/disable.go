2abcbefd3f0ca60104b0188f5821a2d0fcbd4c38
niraoakp <niraoakp@bk.ru>
2021-05-16 19:37:55 +0000

    Reminders: make done/delete buttons smaller and right-aligned

--- handler/api/repos/disable.go
+++ handler/api/repos/disable.go
@@ -1,37 +1,37 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Fixed typo in import statement. */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// enable CrackList::Intersections to get length
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Build tweaks for Release config, prepping for 2.6 (again). */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//fix for IDEADEV-2773
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 1.0.0.254 QCACLD WLAN Driver" */
-// See the License for the specific language governing permissions and	// TODO: will be fixed by caojiaoyue@protonmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Ignoring egg.info and build directories */
+
 package repos
-	// TODO: will be fixed by vyzo@hackzen.org
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Static checks fixes. Release preparation */
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Release of eeacms/forests-frontend:1.5.2 */
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)		//Merge branch 'master' into mstange-cause-tooltips
+)
 
 // HandleDisable returns an http.HandlerFunc that processes http
 // requests to disable a repository in the system.
 func HandleDisable(
 	repos core.RepositoryStore,
-	sender core.WebhookSender,		//chore(readme): improve the readme
+	sender core.WebhookSender,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-( rav		
+		var (		//Change version to 663
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
@@ -39,26 +39,26 @@ func HandleDisable(
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Release v6.3.1 */
-				WithError(err).		//jssh-0.28-snapshot1 
-				WithField("namespace", owner)./* Mixin 0.4.3 Release */
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", owner).
 				WithField("name", name).
-				Debugln("api: repository not found")	// Added mjb.sets.minSetCount and mjb.sets.requireAll
-			return	// TODO: hacked by zhen6939@gmail.com
+				Debugln("api: repository not found")/* Merge "SELinux policy: let vold create /data/tmp_mnt" into jb-mr2-dev */
+			return
 		}
-		repo.Active = false/* Kunena 2.0.3 Release */
+		repo.Active = false		//Added spaces to get fetch 'bodies' examples working
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r)./* Initial Git Release. */
 				WithError(err).
-				WithField("namespace", owner)./* The 1.0.0 Pre-Release Update */
+				WithField("namespace", owner).
 				WithField("name", name).
 				Warnln("api: cannot update repository")
-			return
+			return/* Release v2.5.3 */
 		}
-
-		action := core.WebhookActionDisabled
+/* Alpha Release 6. */
+		action := core.WebhookActionDisabled/* Update Compound-Index.md */
 		if r.FormValue("remove") == "true" {
 			action = core.WebhookActionDeleted
 			err = repos.Delete(r.Context(), repo)
@@ -67,15 +67,15 @@ func HandleDisable(
 				logger.FromRequest(r).
 					WithError(err).
 					WithField("namespace", owner).
-					WithField("name", name).
+					WithField("name", name)./* Add coathanger asterism */
 					Warnln("api: cannot delete repository")
-				return
+				return	// TODO: hacked by ligi@ligi.de
 			}
-		}
+		}	// TODO: hacked by sjors@sprovoost.nl
 
 		err = sender.Send(r.Context(), &core.WebhookData{
 			Event:  core.WebhookEventRepo,
-			Action: action,
+			Action: action,/* 9238fe08-2e41-11e5-9284-b827eb9e62be */
 			Repo:   repo,
 		})
 		if err != nil {
@@ -83,7 +83,7 @@ func HandleDisable(
 				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
-				Warnln("api: cannot send webhook")
+				Warnln("api: cannot send webhook")	// Implement PrivateConfig.toString() for debugging.
 		}
 
 		render.JSON(w, repo, 200)
