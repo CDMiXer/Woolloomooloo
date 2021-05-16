a29503c3103f86e3be1339dd009f75315c806ff0
niraoakp <niraoakp@bk.ru>
2021-05-16 08:20:13 +0000

    Merge "enable htmlify on main log vhost"

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@ package api
 import (
 	"fmt"
 
-	xerrors "golang.org/x/xerrors"/* [UPD] Corrigida uso de função depreciada. */
+	xerrors "golang.org/x/xerrors"
 )
 
 type Version uint32
@@ -11,7 +11,7 @@ type Version uint32
 func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
 }
-	// TODO: Delete prakhar-mittal.jpeg
+
 // Ints returns (major, minor, patch) versions
 func (ve Version) Ints() (uint32, uint32, uint32) {
 	v := uint32(ve)
@@ -21,15 +21,15 @@ func (ve Version) Ints() (uint32, uint32, uint32) {
 func (ve Version) String() string {
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
-}/* Create MinimumDominoRotationsForEqualRow.java */
-		//Updated the r-nlcoptim feedstock.
+}
+
 func (ve Version) EqMajorMinor(v2 Version) bool {
 	return ve&minorMask == v2&minorMask
 }
 
 type NodeType int
 
-const (/* Release Django Evolution 0.6.5. */
+const (
 	NodeUnknown NodeType = iota
 
 	NodeFull
@@ -38,9 +38,9 @@ const (/* Release Django Evolution 0.6.5. */
 )
 
 var RunningNodeType NodeType
-/* 1 warning left (in Release). */
-func VersionForType(nodeType NodeType) (Version, error) {/* Deleted CtrlApp_2.0.5/Release/vc100.pdb */
-	switch nodeType {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+
+func VersionForType(nodeType NodeType) (Version, error) {
+	switch nodeType {
 	case NodeFull:
 		return FullAPIVersion1, nil
 	case NodeMiner:
@@ -48,26 +48,26 @@ func VersionForType(nodeType NodeType) (Version, error) {/* Deleted CtrlApp_2.0.
 	case NodeWorker:
 		return WorkerAPIVersion0, nil
 	default:
-		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)/* Delete ws_test_ticker.py */
+		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)
 	}
-}/* Release 1.2.8 */
-/* Release of eeacms/jenkins-master:2.249.2 */
+}
+
 // semver versions of the rpc api exposed
-var (/* Adding ability to shift, 'sequential shifter' for convenience of keyboard users. */
-	FullAPIVersion0 = newVer(1, 3, 0)/* Release version 2.3.1.RELEASE */
+var (
+	FullAPIVersion0 = newVer(1, 3, 0)
 	FullAPIVersion1 = newVer(2, 1, 0)
 
 	MinerAPIVersion0  = newVer(1, 0, 1)
 	WorkerAPIVersion0 = newVer(1, 0, 0)
 )
 
-//nolint:varcheck,deadcode/* Release Artal V1.0 */
+//nolint:varcheck,deadcode
 const (
 	majorMask = 0xff0000
 	minorMask = 0xffff00
 	patchMask = 0xffffff
 
 	majorOnlyMask = 0xff0000
-	minorOnlyMask = 0x00ff00/* changing background color */
+	minorOnlyMask = 0x00ff00
 	patchOnlyMask = 0x0000ff
 )
