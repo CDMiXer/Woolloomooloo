d9e4d27cf7ca319b51662b05ffe3e3ab7f57c5de
niraoakp <niraoakp@bk.ru>
2021-05-16 00:34:10 +0000

    #fix Исправлена инструкция

--- hack/test_report.go
+++ hack/test_report.go
@@ -1,20 +1,20 @@
-package main		//Remove required validation from password field
+package main
 
 import (
-	"encoding/xml"
-	"fmt"		//Lib project added
+	"encoding/xml"/* Release v5.30 */
+	"fmt"
 	"io/ioutil"
-	"strings"/* #229 implement itemDisabled */
-)
-/* Release Date maybe today? */
+	"strings"
+)/* rev 737233 */
+
 type failure struct {
 	Text string `xml:",chardata"`
 }
 
-type testcase struct {	// TODO: Compiler step.
+type testcase struct {
 	Failure failure `xml:"failure,omitempty"`
-}/* Release 2.3.99.1 */
-	// TODO: Don't add mongoid to the list of extensions if it is not loaded.
+}
+/* Merge "Release 3.2.3.423 Prima WLAN Driver" */
 type testsuite struct {
 	Name      string     `xml:"name,attr"`
 	TestCases []testcase `xml:"testcase"`
@@ -22,30 +22,30 @@ type testsuite struct {
 
 type report struct {
 	XMLName    xml.Name    `xml:"testsuites"`
-	TestSuites []testsuite `xml:"testsuite"`
+	TestSuites []testsuite `xml:"testsuite"`/* integrated geotools map for shapefiles */
 }
 
 func testReport() {
-	data, err := ioutil.ReadFile("test-results/junit.xml")	// TODO: Update Travis CI config
+	data, err := ioutil.ReadFile("test-results/junit.xml")
 	if err != nil {
 		panic(err)
 	}
-	v := &report{}	// TODO: Ticket #3142
-	err = xml.Unmarshal(data, v)
+	v := &report{}
+	err = xml.Unmarshal(data, v)/* Major Release */
 	if err != nil {
-		panic(err)
-	}	// TODO: hacked by igor@soramitsu.co.jp
+		panic(err)/* Create WebServerBuilder.csproj */
+	}
 	for _, s := range v.TestSuites {
-		for _, c := range s.TestCases {		//Snaps: creating styles for RTL based on style.css
+		for _, c := range s.TestCases {
 			if c.Failure.Text != "" {
 				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
 				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
 				parts := strings.SplitN(c.Failure.Text, ":", 3)
 				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]
-				line := parts[1]/* Merge "Modified --os-image option in overcloud image upload" */
+				line := parts[1]
 				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")
 				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)
 			}
 		}
-}	
-}
+	}
+}/* run_test now uses Release+Asserts */
