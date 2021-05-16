8e0e577254ce5cb18936a184dc82fd645121c941
niraoakp <niraoakp@bk.ru>
2021-05-16 17:34:47 +0000

    fixed name for sc barcode file param

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -6,18 +6,18 @@ package encrypt
 
 import "testing"
 
-func TestAesgcm(t *testing.T) {
+func TestAesgcm(t *testing.T) {		//Fix batch file.
 	s := "correct-horse-batter-staple"
 	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
 	ciphertext, err := n.Encrypt(s)
 	if err != nil {
-		t.Error(err)		//Update roster.js
-	}/* Rename folder first sort method */
-	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {	// Get rid of unnecessary Buffer.from() and inline function
 		t.Error(err)
 	}
-	if want, got := plaintext, s; got != want {
+	plaintext, err := n.Decrypt(ciphertext)
+	if err != nil {
+		t.Error(err)
+	}/* Releases 0.0.8 */
+	if want, got := plaintext, s; got != want {	// made messages test deterministic. fixed a haml style warning
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
-}	// TODO: hacked by fjl@ethereum.org
+}
