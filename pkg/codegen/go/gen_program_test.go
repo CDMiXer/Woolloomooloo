095206b203e15ea14745e3b97d8b513500fb8107
niraoakp <niraoakp@bk.ru>
2021-05-16 19:48:52 +0000

    QtApp: libpng and zlib removed

--- pkg/codegen/go/gen_program_test.go
+++ pkg/codegen/go/gen_program_test.go
@@ -1,18 +1,18 @@
 package gen
-/* fix for middle feet being too short */
+
 import (
 	"bytes"
 	"io/ioutil"
-	"path/filepath"/* Release 1 of the MAR library */
+	"path/filepath"
 	"testing"
-		//Update entryDetailADV_test.go
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model/format"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* package: update readable-stream */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
@@ -21,11 +21,11 @@ func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
-	}/* cc5e90ae-2e49-11e5-9284-b827eb9e62be */
+	}
 
 	for _, f := range files {
-		if filepath.Ext(f.Name()) != ".pp" {/* 957ae42e-2e59-11e5-9284-b827eb9e62be */
-			continue	// Update gollum.gemspec
+		if filepath.Ext(f.Name()) != ".pp" {
+			continue
 		}
 
 		t.Run(f.Name(), func(t *testing.T) {
@@ -41,16 +41,16 @@ func TestGenProgram(t *testing.T) {
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
-			if err != nil {		//feature #2798: Add SAVING IMAGE state
+			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}		//adding meta description/titles
+			}
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
-			if err != nil {/* require local_dir for Releaser as well */
-				t.Fatalf("could not bind program: %v", err)		//a1f308b4-2e4d-11e5-9284-b827eb9e62be
+			if err != nil {
+				t.Fatalf("could not bind program: %v", err)
 			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
@@ -63,18 +63,18 @@ func TestGenProgram(t *testing.T) {
 			}
 			assert.Equal(t, string(expected), string(files["main.go"]))
 		})
-	}/* MarkFlip Release 2 */
-}	// ee7f5ad0-2e4c-11e5-9284-b827eb9e62be
+	}
+}
 
 func TestCollectImports(t *testing.T) {
 	g := newTestGenerator(t, "aws-s3-logging.pp")
 	pulumiImports := codegen.NewStringSet()
-	stdImports := codegen.NewStringSet()/* Fix readme Drive link */
-	g.collectImports(g.program, stdImports, pulumiImports)/* Release v1.007 */
+	stdImports := codegen.NewStringSet()
+	g.collectImports(g.program, stdImports, pulumiImports)
 	stdVals := stdImports.SortedValues()
 	pulumiVals := pulumiImports.SortedValues()
 	assert.Equal(t, 0, len(stdVals))
-	assert.Equal(t, 1, len(pulumiVals))	// TODO: ajout du default pour ntp
+	assert.Equal(t, 1, len(pulumiVals))
 	assert.Equal(t, "\"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3\"", pulumiVals[0])
 }
 
