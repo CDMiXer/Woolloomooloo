c262f46c8b4014aec8e850c02238230a4786ab06
niraoakp <niraoakp@bk.ru>
2021-05-16 03:26:20 +0000

    #44 Changed the data store interface.

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -1,16 +1,16 @@
 package testkit
 
 import (
-	"bytes"
+	"bytes"/* Update for mobile slides */
 	"context"
 	"errors"
-	"fmt"
-	"io/ioutil"
-	"os"
+	"fmt"		//use Arrays.sort to sort plane
+	"io/ioutil"/* Release 1 Estaciones */
+	"os"		//Minor changes for lib/Thread.
 	"path/filepath"
 	"time"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//Don't use the version number in the path for pbutils.
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
 	ipld "github.com/ipfs/go-ipld-format"
@@ -18,8 +18,8 @@ import (
 	dstest "github.com/ipfs/go-merkledag/test"
 	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
-)
-
+)/* Release 2.4.0 */
+/* Clarify ngrok deployment */
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
 	t1 := time.Now()
 	offers, err := client.ClientFindData(ctx, fcid, nil)
@@ -28,28 +28,28 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	}
 	for _, o := range offers {
 		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
-	}
-	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
-
+	}	// TODO: Fix minor inaccuracy
+	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))	// Updated readme with proper blacklist option
+/* Release version 3.6.2.3 */
 	if len(offers) < 1 {
-		panic("no offers")
+		panic("no offers")		//Increase registerUIAScript timeout to 30 secs for slow VMs.
 	}
 
-	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
+	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")		//Code sample corrections for Template Strings
 	if err != nil {
 		panic(err)
-	}
-	defer os.RemoveAll(rpath)
+	}		//add translations for any to languages
+	defer os.RemoveAll(rpath)	// TODO: hacked by greg@colvin.org
 
 	caddr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
-		return err
+		return err		//Create placeholder.2
 	}
 
 	ref := &api.FileRef{
 		Path:  filepath.Join(rpath, "ret"),
 		IsCAR: carExport,
-	}
+	}/* added learngitbranching.js.org */
 	t1 = time.Now()
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
