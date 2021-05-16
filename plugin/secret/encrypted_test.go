83181e01ea99aae40e28dea8576e858ed84d65fe
niraoakp <niraoakp@bk.ru>
2021-05-16 10:04:11 +0000

    * default sort order changed to date

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by sbrichards@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,5 +11,5 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Implement IRP_MN_QUERY_RESOURCE_REQUIREMENTS
+
 package secret
