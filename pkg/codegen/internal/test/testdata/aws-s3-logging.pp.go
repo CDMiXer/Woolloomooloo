08f28469a1d51875badae7f7d0f8e0c2f947a3c1
niraoakp <niraoakp@bk.ru>
2021-05-16 03:06:41 +0000

    Automatic changelog generation for PR #10153 [ci skip]

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"/* entity data changes */
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: hacked by cory@protocol.ai
-		logs, err := s3.NewBucket(ctx, "logs", nil)/* añadido nuevo topic a kafka */
-		if err != nil {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		logs, err := s3.NewBucket(ctx, "logs", nil)
+		if err != nil {		//Update TwitchStreams.t
 			return err
-		}		//Add ENV to environmental variable
-		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{
-			Loggings: s3.BucketLoggingArray{	// TODO: Fix broken GitHub URL in package.json.
+		}
+		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{/* Update and rename ajax-form-tracking.html to ajax-event-tracking-jquery.html */
+			Loggings: s3.BucketLoggingArray{
 				&s3.BucketLoggingArgs{
 					TargetBucket: logs.Bucket,
 				},
-			},	// TODO: will be fixed by boringland@protonmail.ch
+			},
 		})
 		if err != nil {
-			return err
-		}	// TODO: Stop if no internet connection
+			return err	// TODO: hacked by mikeal.rogers@gmail.com
+		}/* fixed JSON setter issue with Dates */
 		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
 			return loggings[0].TargetBucket, nil
 		}).(pulumi.StringOutput))
