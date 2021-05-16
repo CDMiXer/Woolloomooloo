c813ba965091f2fde3f9f4076e5dc977ca4e7c93
niraoakp <niraoakp@bk.ru>
2021-05-16 01:57:10 +0000

    updated for sending subscription emails

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,29 +1,29 @@
 package account
-/* Release 1.2.0.10 deployed */
-import (/* Delete main.dfm */
-	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"	// TODO: Disable page caching on the main article page.
+
+import (
+	"github.com/filecoin-project/go-address"/* Merge "[INTERNAL] Release notes for version 1.28.20" */
+	"github.com/ipfs/go-cid"		//value for --dir must not exist on disk
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
-)
+)/* Add README.md file. */
 
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}	// added basic support for IntelHex 16-bit mode
-	err := store.Get(store.Context(), root, &out)		//Delete MNIST Sample
+	out := state3{store: store}
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-
+/* ny firat  commit */
 type state3 struct {
 	account3.State
 	store adt.Store
-}	// TODO: will be fixed by fjl@ethereum.org
+}
 
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
