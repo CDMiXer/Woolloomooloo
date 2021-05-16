5b67828c692c0f92131f03ead0aa7a0c05573503
niraoakp <niraoakp@bk.ru>
2021-05-16 23:21:09 +0000

    New suchwow

--- core/sched.go
+++ core/sched.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc.		//Nova release estruturas-de-dados-livro-v0.3.0.pdf
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Improve locking isolation in config.
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Added solution for leetCode - Search for a Range */
-// Unless required by applicable law or agreed to in writing, software		//Merged feature/cli-uploader into develop
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Make the tests work after metadata changes */
+// limitations under the License.
 
 package core
 
 import "context"
-/* Remove unused $delNx */
-// Filter provides filter criteria to limit stages requested	// TODO: Added the current work directory to classpath while running kikaha
+
+// Filter provides filter criteria to limit stages requested
 // from the scheduler.
 type Filter struct {
 	Kind    string
@@ -24,27 +24,27 @@ type Filter struct {
 	OS      string
 	Arch    string
 	Kernel  string
-	Variant string		//Delete asmcrypto.min.js
+	Variant string
 	Labels  map[string]string
 }
 
 // Scheduler schedules Build stages for execution.
-type Scheduler interface {/* Format Release notes for Direct Geometry */
+type Scheduler interface {
 	// Schedule schedules the stage for execution.
 	Schedule(context.Context, *Stage) error
 
-	// Request requests the next stage scheduled for execution./* Merge "[INTERNAL] Release notes for version 1.72.0" */
+	// Request requests the next stage scheduled for execution.
 	Request(context.Context, Filter) (*Stage, error)
 
-	// Cancel cancels scheduled or running jobs associated		//Added IOException to the handling of NotFound.
+	// Cancel cancels scheduled or running jobs associated
 	// with the parent build ID.
 	Cancel(context.Context, int64) error
 
 	// Cancelled blocks and listens for a cancellation event and
 	// returns true if the build has been cancelled.
-	Cancelled(context.Context, int64) (bool, error)/* 2254176e-2e44-11e5-9284-b827eb9e62be */
+	Cancelled(context.Context, int64) (bool, error)
 
-	// Pause pauses the scheduler and prevents new pipelines	// Fixed Issue 20.
+	// Pause pauses the scheduler and prevents new pipelines
 	// from being scheduled for execution.
 	Pause(context.Context) error
 
@@ -55,4 +55,4 @@ type Scheduler interface {/* Format Release notes for Direct Geometry */
 	// Stats provides statistics for underlying scheduler. The
 	// data format is scheduler-specific.
 	Stats(context.Context) (interface{}, error)
-}/* added nexus staging plugin to autoRelease */
+}
