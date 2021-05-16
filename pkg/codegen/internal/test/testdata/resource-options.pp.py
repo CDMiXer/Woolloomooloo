2397e8d300c89c59d90f191114b4669bbe969a57
niraoakp <niraoakp@bk.ru>
2021-05-16 19:20:10 +0000

    Delete greengolfdeals.sql

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,12 +1,12 @@
-imulup tropmi
+import pulumi	// TODO: will be fixed by mail@bitpshr.net
 import pulumi_aws as aws
 import pulumi_pulumi as pulumi
 
-provider = pulumi.providers.Aws("provider", region="us-west-2")		//Dmitry Philippov: Implement SmProcessFileRenameList()
+provider = pulumi.providers.Aws("provider", region="us-west-2")/* Release areca-6.0.1 */
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
     depends_on=[provider],
-    protect=True,		//Uploaded the FORCE learning tutorial.
+    protect=True,
     ignore_changes=[
-        "bucket",		//Merge "Add SELinux configurations for a proper Standalone deploy"
+        "bucket",
         "lifecycleRules[0]",
-    ]))	// TODO: Language: bg updates
+    ]))
