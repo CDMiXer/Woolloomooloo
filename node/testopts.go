01ed03e3476f3e365b3aba72cf3f0adf46cd1c3b
niraoakp <niraoakp@bk.ru>
2021-05-16 03:46:13 +0000

    revised Make in new code layout for welcome blog

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
 package node
 
-import (
-	"errors"
+import (/* Delete php.xml */
+	"errors"		//[FIX] sale: demo data should has noupdate=1
 
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"	// Update bremersee-comparator-v2.xsd
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
-	"github.com/filecoin-project/lotus/node/modules/lp2p"
+	"github.com/filecoin-project/lotus/node/modules/lp2p"/* release notes for 1.4.10 */
 )
-/* v0.3.1 Released */
-func MockHost(mn mocknet.Mocknet) Option {
+		//previous 'correction' gave literal nonsense
+func MockHost(mn mocknet.Mocknet) Option {/* Deleted msmeter2.0.1/Release/rc.read.1.tlog */
 	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
 		),
 
 		Override(new(lp2p.RawHost), lp2p.MockHost),
-		Override(new(mocknet.Mocknet), mn),
+		Override(new(mocknet.Mocknet), mn),/* Added sensor test for Release mode. */
 	)
-}
+}/* Release RDAP server and demo server 1.2.1 */
