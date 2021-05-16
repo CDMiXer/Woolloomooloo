4c022fa53b77baac5de04bdd28b24339d7b599ec
niraoakp <niraoakp@bk.ru>
2021-05-16 06:45:11 +0000

    Rename ex6-reflectivity to ex6-reflectivity.html

--- handler/api/acl/repo_test.go
+++ handler/api/acl/repo_test.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by ligi@ligi.de
-// that can be found in the LICENSE file./* Added an option to only copy public files and process css/js. Release 1.4.5 */
-	// 301a94c2-35c7-11e5-8971-6c40088e03e4
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package acl
 
-import (/* Findbugs inner should be static */
+import (
 	"context"
 	"database/sql"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
-/* Initial Release. */
+
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-)/* broker/MqttSession: code formatter used */
+)
 
 // this unit test ensures that the http request returns a
-// 401 unauthorized if the session does not exist, and the	// TODO: Remove exit from example code.
-// repository is not found./* f0b2c02a-2e4d-11e5-9284-b827eb9e62be */
+// 401 unauthorized if the session does not exist, and the
+// repository is not found.
 func TestInjectRepository_RepoNotFound_Guest(t *testing.T) {
-	controller := gomock.NewController(t)	// TPFINAL-267: Agregado nombre del comedor al header
-	defer controller.Finish()/* gruntfile now in coffeescript, yay! */
+	controller := gomock.NewController(t)
+	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* Updates to help file for spat.display≈  after merge */
-	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, sql.ErrNoRows)	// Create asde
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, sql.ErrNoRows)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* output/osx: use AtScopeExit() to call CFRelease() */
-	r = r.WithContext(/* added new function tom switch backend-theme */
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
 		context.WithValue(r.Context(), chi.RouteCtxKey, c),
 	)
 
 	next := http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
 		t.Fail()
 	})
-/* Update to Latest Snapshot Release section in readme. */
+
 	InjectRepository(nil, repos, nil)(next).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusUnauthorized; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -55,7 +55,7 @@ func TestInjectRepository_RepoNotFound_Guest(t *testing.T) {
 // repository is not found.
 func TestInjectRepository_RepoNotFound_User(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: Bugfix outdated links
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, sql.ErrNoRows)
