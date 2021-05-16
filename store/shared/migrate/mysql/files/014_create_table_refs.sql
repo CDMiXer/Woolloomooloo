475c0f3b3b3f9ff49f22a9b8d337030f128065cd
niraoakp <niraoakp@bk.ru>
2021-05-16 06:25:22 +0000

    Enable lint checks for k9mail-library

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
 
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER	// objc grammar
+ latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER	// TODO: working on lesson 18
+,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
-	// TODO: hacked by ng8eke@163.com
--- name: create-index-latest-repo
 
+-- name: create-index-latest-repo
+/* Started extracting ph-servlet */
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
