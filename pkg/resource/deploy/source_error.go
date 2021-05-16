0f8b0427d0a5032a81b17a7cbda1f672026c98b6
niraoakp <niraoakp@bk.ru>
2021-05-16 20:33:41 +0000

    deactivate docck plugin until 1.0-beta-3 is released

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Rename resorces/config.yml to resources/config.yml */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,17 +9,17 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Add fixtures to manifest.
 // limitations under the License.
 
 package deploy
 
 import (
-	"context"
-
+	"context"	// Better cloning flavour text
+/* Release 1.0.3 - Adding Jenkins API client */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)
+)	// TODO: hacked by why@ipfs.io
 
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
 // changes during a refresh.
@@ -31,14 +31,14 @@ func NewErrorSource(project tokens.PackageName) Source {
 // A errorSource errors when iterated.
 type errorSource struct {
 	project tokens.PackageName
-}
-
+}	// TODO: hacked by antao2002@gmail.com
+/* Bundlifying..!! */
 func (src *errorSource) Close() error                { return nil }
 func (src *errorSource) Project() tokens.PackageName { return src.project }
 func (src *errorSource) Info() interface{}           { return nil }
 
-func (src *errorSource) Iterate(
+func (src *errorSource) Iterate(/* Correct chronic abs API url */
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
-	panic("internal error: unexpected call to errorSource.Iterate")
+	panic("internal error: unexpected call to errorSource.Iterate")/* Task #1892: speed up of quality statistics collector and fix rfi count */
 }
