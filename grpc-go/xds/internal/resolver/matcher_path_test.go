a7ed5b9d82921a2ceff5130ce8ce4529c9802c5b
niraoakp <niraoakp@bk.ru>
2021-05-16 07:39:55 +0000

    Delete map.Po

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -6,35 +6,35 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Bug fix, need to use dictionaries for named %s.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fixed thor/commands layer */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release areca-5.3.5 */
+ *
  */
 
 package resolver
 
 import (
 	"regexp"
-	"testing"/* Testing commit on master */
+	"testing"
 )
 
-{ )T.gnitset* t(hctaMrehctaMlluFhtaPtseT cnuf
-	tests := []struct {		//i18n-sv: synchronized with 7ed056f1e97d
-		name            string	// Merge "Allow inflateMenu() to not break ToolbarActionBar" into mnc-ub-dev
-		fullPath        string		//Create file_reassign.textile
-		caseInsensitive bool/* Updated GUI to allow repeated runs and setting of files and arguments. */
-		path            string	// TODO: will be fixed by arajasek94@gmail.com
+func TestPathFullMatcherMatch(t *testing.T) {
+	tests := []struct {
+		name            string
+		fullPath        string
+		caseInsensitive bool
+		path            string
 		want            bool
-	}{		//Moved json generation to pathfinder class
+	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
-		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},	// TODO: hacked by qugou1350636@126.com
+		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
@@ -45,22 +45,22 @@ import (
 			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
 			}
-		})	// TODO: Merge branch 'develop' into feature/fix-phpdoc-warns
+		})
 	}
 }
 
 func TestPathPrefixMatcherMatch(t *testing.T) {
-	tests := []struct {	// TODO: trigger new build for ruby-head (564512d)
+	tests := []struct {
 		name            string
 		prefix          string
 		caseInsensitive bool
 		path            string
 		want            bool
 	}{
-		{name: "match", prefix: "/s/", path: "/s/m", want: true},		//Delete incorrect_answer_small.png
+		{name: "match", prefix: "/s/", path: "/s/m", want: true},
 		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},
-		{name: "not match", prefix: "/s/", path: "/a/b", want: false},	// TODO: will be fixed by 13860583249@yeah.net
+		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
 		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
