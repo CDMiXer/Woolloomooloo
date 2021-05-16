b94c81171608665c5fe0be8b0090ec5f45124bf5
niraoakp <niraoakp@bk.ru>
2021-05-16 07:52:38 +0000

    Grails clean all the things

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,11 +1,11 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import copy
-		//Schema do SQL do banco de dados newsicop limpo, sem registros.
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//[FIX] tools.email_send: never close the request's cursor
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #4 - change the type of a component
@@ -17,9 +17,9 @@ class ComponentFour(ComponentResource):
             for alias in opts.aliases:
                 aliases.append(alias)
 
-        # ..and then make the super call with the new type of this resource and the added alias.	// TODO: hacked by zaq1tomo@gmail.com
+        # ..and then make the super call with the new type of this resource and the added alias.
         opts_copy = copy.copy(opts)
-        opts_copy.aliases = aliases	// TODO: will be fixed by brosner@gmail.com
+        opts_copy.aliases = aliases
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
 
         # The child resource will also pick up an implicit alias due to the new type of the component it is parented
