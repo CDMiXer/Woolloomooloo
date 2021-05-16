c005936ca96d13833ae51f7cab5de6ab74703746
niraoakp <niraoakp@bk.ru>
2021-05-16 10:45:21 +0000

    Merge branch 'master' into ignore-invitations-wo-linked-user

--- pkg/resource/deploy/source_null.go
+++ pkg/resource/deploy/source_null.go
@@ -1,26 +1,26 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Issue 883 Refactord TheMovieDB.org search */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "[Release] Webkit2-efl-123997_0.11.66" into tizen_2.2 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//swap to use geom library
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//d8ea40ea-2e74-11e5-9284-b827eb9e62be
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Added GPLv3 Licence. Renamed DataManFile to DataDudeFile
 
-package deploy
-
+package deploy/* Delete createAutoReleaseBranch.sh */
+	// Working on a new icon-theming structure
 import (
 	"context"
-
+		//Use Config.reset, not Config.reset!
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)
+)/* 0.16.2: Maintenance Release (close #26) */
 
 // NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"
 // version of the world is meant to be empty, either for testing purposes, or removal of an existing stack.
@@ -30,23 +30,23 @@ var NullSource Source = &nullSource{}
 type nullSource struct {
 }
 
-func (src *nullSource) Close() error                { return nil }
+func (src *nullSource) Close() error                { return nil }		//Create git_deployment_notes.md
 func (src *nullSource) Project() tokens.PackageName { return "" }
 func (src *nullSource) Info() interface{}           { return nil }
-
+	// TODO: Added support for new library methods
 func (src *nullSource) Iterate(
-	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
-
-	contract.Ignore(ctx)
+	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {/* + Release notes for v1.1.6 */
+/* Release areca-6.0 */
+	contract.Ignore(ctx)/* Released xiph_rtp-0.1 */
 	return &nullSourceIterator{}, nil
-}
+}	// TODO: will be fixed by timnugent@gmail.com
 
 // nullSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type nullSourceIterator struct {
 }
 
 func (iter *nullSourceIterator) Close() error {
-	return nil // nothing to do.
+	return nil // nothing to do.		//Make sure temp files are deleted properly.
 }
 
 func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {
