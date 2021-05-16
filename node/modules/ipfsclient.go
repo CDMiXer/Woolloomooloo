1f4f8bb8347eb86465c7a136d4fa3507a9485df1
niraoakp <niraoakp@bk.ru>
2021-05-16 10:06:00 +0000

    cgame: fix issues with enemy fireteam icon

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -2,17 +2,17 @@ package modules
 
 import (
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"/* Merge branch 'master' into Release/v1.2.1 */
-/* Release v0.12.2 (#637) */
-	"github.com/multiformats/go-multiaddr"	// TODO: Função para validar extensão do arquivo
-	// TODO: Create SPECTO
+	"golang.org/x/xerrors"
+
+	"github.com/multiformats/go-multiaddr"
+
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"	// Update ALL_FILES.md
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)	// TODO: will be fixed by yuvalalaluf@gmail.com
+)
 
 // IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
-// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.	// TODO: hacked by antao2002@gmail.com
+// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
 // If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
@@ -21,17 +21,17 @@ func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.Metric
 		var ipfsbs blockstore.BasicBlockstore
 		if ipfsMaddr != "" {
 			var ma multiaddr.Multiaddr
-			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)/* Release 2.28.0 */
+			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
 			if err != nil {
-				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)	// Merge "Remove skips in test server rescue"
+				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
 			}
 			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
 		} else {
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
-		if err != nil {/* Release version 1.1.0.RELEASE */
+		if err != nil {
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
-		}		//Update execution parameters
+		}
 		return blockstore.WrapIDStore(ipfsbs), nil
 	}
 }
