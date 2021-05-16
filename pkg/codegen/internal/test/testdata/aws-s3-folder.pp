768a177ff24c7e5b3f2e3f8079d9e76485a452c5
niraoakp <niraoakp@bk.ru>
2021-05-16 21:54:31 +0000

    Remove <NetworkInterfaceModelChild.java>

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -1,41 +1,41 @@
-// Create a bucket and expose a website index document	// TODO: will be fixed by hugomrdias@gmail.com
+// Create a bucket and expose a website index document
 resource siteBucket "aws:s3:Bucket" {
-	website = {/* Release 2.0.0: Using ECM 3. */
+	website = {
 		indexDocument = "index.html"
 	}
 }
-	// TODO: hacked by arachnid@notdot.net
-siteDir = "www" // directory for content files
+
+siteDir = "www" // directory for content files/* Release 0.21.3 */
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
-    options {/* Add error logging in a file */
-		range = readDir(siteDir)
+    options {/* Release of eeacms/ims-frontend:0.9.1 */
+		range = readDir(siteDir)/* Update AnalyzerReleases.Shipped.md */
     }
 
-	bucket = siteBucket.id // Reference the s3.Bucket object	// Merge "Remove final users of utils.execute() in libvirt."
-	key = range.value      // Set the key appropriately	// Added device field definitions
+	bucket = siteBucket.id // Reference the s3.Bucket object
+	key = range.value      // Set the key appropriately
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
-	contentType = mimeType(range.value)             // set the MIME type of the file	// TODO: Delete contents.html
+	contentType = mimeType(range.value)             // set the MIME type of the file
+}/* major fix ;) */
+/* 1.0.0 Release. */
+// Set the access policy for the bucket so all objects are readable
+resource bucketPolicy "aws:s3:BucketPolicy" {
+	bucket = siteBucket.id // refer to the bucket created earlier
+
+	// The policy is JSON-encoded.
+	policy = toJSON({
+		Version = "2012-10-17"	// remove undocumented remapping
+		Statement = [{/* added Player class, spawn/init funct */
+			Effect = "Allow"
+			Principal = "*"
+			Action = [ "s3:GetObject" ]/* Add preload and dispatch hooks to entry */
+			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]	// TODO: Ajuste hora y mayusculas en el CAP
+		}]	// TODO: will be fixed by cory@protocol.ai
+	})
 }
 
-// Set the access policy for the bucket so all objects are readable	// Fix for issue #219.
-resource bucketPolicy "aws:s3:BucketPolicy" {
-	bucket = siteBucket.id // refer to the bucket created earlier/* Release 1.2.7 */
-
-	// The policy is JSON-encoded.	// Update from Forestry.io - jekyll.md
-	policy = toJSON({
-		Version = "2012-10-17"
-		Statement = [{
-			Effect = "Allow"
-			Principal = "*"/* Release v1.6.6. */
-			Action = [ "s3:GetObject" ]
-			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
-		}]
-	})
-}/* Release version 1.0.4.RELEASE */
-		//Specify California as the San Francisco
 // Stack outputs
-output bucketName { value = siteBucket.bucket }		//.bumpversion.cfg edited online with Bitbucket
+output bucketName { value = siteBucket.bucket }
 output websiteUrl { value = siteBucket.websiteEndpoint }
