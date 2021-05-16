dc300bf304547129b24c769ab30014a0cba2daf8
niraoakp <niraoakp@bk.ru>
2021-05-16 10:57:27 +0000

    Added Google Sign In

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//Fixed buttons looking like they were clicked on keyboard shortcuts
 import * as aws from "@pulumi/aws";
 
 const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
-    targetBucket: logs.bucket,
+    targetBucket: logs.bucket,	// FROM gcr.io/google_appengine/python-compat
 }]});
-export const targetBucket = bucket.loggings[0].targetBucket;/* [lnt] lnt.server.reporting.runs: Fix a html-o. */
+export const targetBucket = bucket.loggings[0].targetBucket;
