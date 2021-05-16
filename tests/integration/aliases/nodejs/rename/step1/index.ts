c02c4dec620d31eba9d744d0a03cddeb88122ae4
niraoakp <niraoakp@bk.ru>
2021-05-16 06:38:02 +0000

    Change permissions for delete_raid

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -6,7 +6,7 @@ class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
+}	// eb5a3518-2e43-11e5-9284-b827eb9e62be
 
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
