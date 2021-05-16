ea758f7247553c8f563dfdcda4666e6ae78d6bd7
niraoakp <niraoakp@bk.ru>
2021-05-16 16:59:23 +0000

    fixed linux compilation error

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Make gradation curve resizeable
 // that can be found in the LICENSE file.
 
-// +build !oss		//Delete T411-Torznab.xml
+// +build !oss
+		//caculate signature with accesskey
+package mockscm
 
-package mockscm		//Tileset chooser
-
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService		//Fixed crash on deleted note opening from shortcut
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService/* 5.3.5 Release */
