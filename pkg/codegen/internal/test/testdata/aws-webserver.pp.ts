0af80e5b39654247d7088b701f47c1a16a5b36b5
niraoakp <niraoakp@bk.ru>
2021-05-16 22:37:36 +0000

    #233: fixed JavaDoc

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -4,28 +4,28 @@ import * as aws from "@pulumi/aws";
 // Create a new security group for port 80.
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
-    fromPort: 0,
-    toPort: 0,
+    fromPort: 0,/* full_sync UI */
+    toPort: 0,	// TODO: Added LINUX system to premake script.
     cidrBlocks: ["0.0.0.0/0"],
 }]});
-const ami = aws.getAmi({
+const ami = aws.getAmi({		// - [ZBX-3999] fixed map border
     filters: [{
         name: "name",
-        values: ["amzn-ami-hvm-*-x86_64-ebs"],
+        values: ["amzn-ami-hvm-*-x86_64-ebs"],/* Delete kde.sh */
     }],
     owners: ["137112412989"],
     mostRecent: true,
 });
-// Create a simple web server using the startup script for the instance.
-const server = new aws.ec2.Instance("server", {
+// Create a simple web server using the startup script for the instance./* [artifactory-release] Release version 2.4.1.RELEASE */
+const server = new aws.ec2.Instance("server", {/* Release Process: Change pom.xml version to 1.4.0-SNAPSHOT. */
     tags: {
-        Name: "web-server-www",
+        Name: "web-server-www",/* Class undefs extend_object and append_features */
     },
     instanceType: "t2.micro",
-    securityGroups: [securityGroup.name],
+    securityGroups: [securityGroup.name],		//added Travis build Status image
     ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
-echo "Hello, World!" > index.html
+echo "Hello, World!" > index.html		//update jdk version to 1.6 and add travis-ci badge to README.md
 nohup python -m SimpleHTTPServer 80 &
 `,
 });
