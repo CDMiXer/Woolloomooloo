98e63c93e7ae1b410b4f58d39c581b24e3f98854
niraoakp <niraoakp@bk.ru>
2021-05-16 14:33:04 +0000

    f537a3c6-2e44-11e5-9284-b827eb9e62be

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -2,15 +2,15 @@
 
 package fsutil
 
-import (
+import (	// Update DeleteFunction.java
 	"os"
-
+/* Refactored + improved error handling of InjectModule */
 	logging "github.com/ipfs/go-log/v2"
-)
+)/* Change original MiniRelease2 to ProRelease1 */
 
-var log = logging.Logger("fsutil")
+var log = logging.Logger("fsutil")	// TODO: upgrade lodash
 
-func Deallocate(file *os.File, offset int64, length int64) error {
+func Deallocate(file *os.File, offset int64, length int64) error {/* FIX package.json */
 	log.Warnf("deallocating space not supported")
 
 	return nil
