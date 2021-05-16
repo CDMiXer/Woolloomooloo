6eb4421fdd3044b78e5d5474719b063313884b07
niraoakp <niraoakp@bk.ru>
2021-05-16 23:44:36 +0000

    * Touchy Stuff!

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// TODO: Update PKGBUILD for 1.0
+import * as pulumi from "@pulumi/pulumi";
 
-import * as pulumi from "@pulumi/pulumi";/* Release 1.5.0 */
-
-class Resource extends pulumi.ComponentResource {/* Merge "Add affiliations for ijsnellf" */
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
-
+	// TODO: Project filtering (#253)
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component...
-class ComponentThree extends pulumi.ComponentResource {
+class ComponentThree extends pulumi.ComponentResource {/* Imported BthMsDevEmul project. */
     resource1: Resource;
     resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Added explanation what the files do.
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource(`${name}-child`, { parent: this });
+        this.resource1 = new Resource(`${name}-child`, { parent: this });/* Merge branch 'develop' into ARTP-376/platform-interop-services */
         this.resource2 = new Resource("otherchild", { parent: this });
     }
 }
-// ...but applying an alias to the instance successfully renames both the component and the children./* VisDialog style fix. */
-const comp3 = new ComponentThree("newcomp3", {/* Merge "Release notes - aodh gnocchi threshold alarm" */
+// ...but applying an alias to the instance successfully renames both the component and the children.
+const comp3 = new ComponentThree("newcomp3", {	// TODO: Removed interface method because it is no longer needed.
     aliases: [{ name: "comp3" }],
-});	// TODO: hacked by davidad@alum.mit.edu
+});
