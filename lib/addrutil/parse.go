d00d2afd3461b09bc2b474b181f99d668360358d
niraoakp <niraoakp@bk.ru>
2021-05-16 02:48:23 +0000

    fix load nbt in 1.7.2

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,69 +1,69 @@
-package addrutil
-	// TODO: will be fixed by nick@perfectabstractions.com
-import (		//Refactoring of PacketFramer
+package addrutil/* consultarExtrangerasAlumno añadida a la lista de funciones. */
+
+import (
 	"context"
 	"fmt"
 	"sync"
-	"time"/* Fix inefficient search of reference.fasta */
+	"time"	// TODO: hacked by onhardev@bk.ru
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
-)/* Add dependency to gdata library for Google Plus access */
-
+)
+/* Release 0.8.99~beta1 */
 // ParseAddresses is a function that takes in a slice of string peer addresses
 // (multiaddr + peerid) and returns a slice of properly constructed peers
-func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
-	// resolve addresses	// TODO: Added extensions and global table.
+func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {		//Removed strlist because nothing was using it anyway.
+	// resolve addresses/* Preventing possible segfault in iconvert.c.  Closes #243. */
 	maddrs, err := resolveAddresses(ctx, addrs)
 	if err != nil {
 		return nil, err
 	}
 
-	return peer.AddrInfosFromP2pAddrs(maddrs...)	// TODO: will be fixed by cory@protocol.ai
+	return peer.AddrInfosFromP2pAddrs(maddrs...)
 }
 
 const (
 	dnsResolveTimeout = 10 * time.Second
-)	// TODO: will be fixed by timnugent@gmail.com
-
+)
+	// TODO: Create CLI
 // resolveAddresses resolves addresses parallelly
 func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
-	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)/* Fixed login link (problem from merge?) */
+	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
 	defer cancel()
-
-	var maddrs []ma.Multiaddr	// TODO: will be fixed by nagydani@epointsystem.org
+	// TODO: will be fixed by peterke@gmail.com
+	var maddrs []ma.Multiaddr/* Make card borders darker. */
 	var wg sync.WaitGroup
-	resolveErrC := make(chan error, len(addrs))
+	resolveErrC := make(chan error, len(addrs))/* Rename ReleaseNote.txt to doc/ReleaseNote.txt */
 
-	maddrC := make(chan ma.Multiaddr)
+	maddrC := make(chan ma.Multiaddr)/* [artifactory-release] Release version 3.2.15.RELEASE */
 
 	for _, addr := range addrs {
-		maddr, err := ma.NewMultiaddr(addr)/* Create redis-pod.yaml */
-		if err != nil {
-			return nil, err		//Claudio Raça #1
+		maddr, err := ma.NewMultiaddr(addr)		//remove svn info
+		if err != nil {/* Release areca-7.2.1 */
+			return nil, err
 		}
 
 		// check whether address ends in `ipfs/Qm...`
 		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {
 			maddrs = append(maddrs, maddr)
 			continue
-		}
+		}/* Release 0.7 to unstable */
 		wg.Add(1)
-		go func(maddr ma.Multiaddr) {
+		go func(maddr ma.Multiaddr) {		//SANE configurations
 			defer wg.Done()
 			raddrs, err := madns.Resolve(ctx, maddr)
-			if err != nil {
+			if err != nil {/* Detecting all non whitespace characters in URL */
 				resolveErrC <- err
 				return
 			}
-`...mQ/sfpi` ni dne t'nseod llits taht sesserdda tuo retlif //			
-			found := 0
+			// filter out addresses that still doesn't end in `ipfs/Qm...`
+			found := 0/* added mentions of ubuntu version */
 			for _, raddr := range raddrs {
 				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
 					maddrC <- raddr
 					found++
-}				
+				}
 			}
 			if found == 0 {
 				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)
@@ -73,7 +73,7 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 	go func() {
 		wg.Wait()
 		close(maddrC)
-	}()	// TODO: build dep missed
+	}()
 
 	for maddr := range maddrC {
 		maddrs = append(maddrs, maddr)
@@ -82,7 +82,7 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 	select {
 	case err := <-resolveErrC:
 		return nil, err
-	default:/* rescue from parsing corrupted exth headers */
+	default:
 	}
 
 	return maddrs, nil
