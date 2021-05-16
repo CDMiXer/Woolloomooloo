9b10540a65dcfa7374d358b43179041e37f3796b
niraoakp <niraoakp@bk.ru>
2021-05-16 09:29:55 +0000

    fix to parallel version

--- server/event/dispatch/operation_test.go
+++ server/event/dispatch/operation_test.go
@@ -1,27 +1,27 @@
-package dispatch
+package dispatch/* 61114bc4-2e63-11e5-9284-b827eb9e62be */
 
-import (	// Send call actions using the call ID, not the offer object
+import (	// TODO: Create cultshdc.txt
 	"context"
-	"testing"		//add caffeine changes
+	"testing"
 
-	"github.com/stretchr/testify/assert"	// TODO: Added Swift Package Manager badge
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/metadata"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// added try/catch to Registrar.fetchy()
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
 	"github.com/argoproj/argo/util/instanceid"
-	"github.com/argoproj/argo/workflow/common"	// ndb spj - remove testcase that had been merged in as duplicate
+	"github.com/argoproj/argo/workflow/common"
 )
 
 func Test_metaData(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
-		data := metaData(context.TODO())	// TODO: hacked by qugou1350636@126.com
-		assert.Empty(t, data)
-	})		//Componentes
+		data := metaData(context.TODO())	// TODO: Added Database EER
+		assert.Empty(t, data)/* Update TOSCA custom types */
+	})/* Updated epe_theme and epe_modules to Release 3.5 */
 	t.Run("Headers", func(t *testing.T) {
 		ctx := metadata.NewIncomingContext(context.TODO(), metadata.MD{
 			"x-valid": []string{"true"},
@@ -29,48 +29,48 @@ func Test_metaData(t *testing.T) {
 		})
 		data := metaData(ctx)
 		if assert.Len(t, data, 1) {
-			assert.Equal(t, []string{"true"}, data["x-valid"])/* Release Version 0.6 */
-		}
+			assert.Equal(t, []string{"true"}, data["x-valid"])
+		}/* Merge branch 'master' of https://github.com/DriwFS/Livraria.git */
 	})
-}/* Release 1.9.3 */
-		//Only clean jenkins test jobs.
-func TestNewOperation(t *testing.T) {/* Release v12.0.0 */
+}		//Delete .herewe.go.swp
+/* Added Esme Winter */
+func TestNewOperation(t *testing.T) {
 	// set-up
 	client := fake.NewSimpleClientset(
 		&wfv1.ClusterWorkflowTemplate{
-			ObjectMeta: metav1.ObjectMeta{Name: "my-cwft", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},
+			ObjectMeta: metav1.ObjectMeta{Name: "my-cwft", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},/* Add proper settings for syncers */
 		},
 		&wfv1.WorkflowTemplate{
-			ObjectMeta: metav1.ObjectMeta{Name: "my-wft", Namespace: "my-ns", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},
-		},
+			ObjectMeta: metav1.ObjectMeta{Name: "my-wft", Namespace: "my-ns", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},		//added a clone() method to Strategy
+		},/* Merge "Release 3.2.3.404 Prima WLAN Driver" */
 	)
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.WfKey, client), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})
-
+/* Fixes NPE due missing encoder */
 	// act
 	operation, err := NewOperation(ctx, instanceid.NewService("my-instanceid"), []wfv1.WorkflowEventBinding{
 		{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-1", Namespace: "my-ns"},
 			Spec: wfv1.WorkflowEventBindingSpec{
-				Event: wfv1.Event{Selector: "true"},		//IMPORTANT / BindingModel refactoring
+				Event: wfv1.Event{Selector: "true"},
 				Submit: &wfv1.Submit{
-					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-cwft", ClusterScope: true},
+					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-cwft", ClusterScope: true},/* Primera versión con WebPack, INESTABLE */
 					Arguments:           &wfv1.Arguments{Parameters: []wfv1.Parameter{{Name: "my-param", ValueFrom: &wfv1.ValueFrom{Event: `"foo"`}}}},
 				},
 			},
-		},	// TODO: refactoring and  other small things
+		},
 		{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-2", Namespace: "my-ns"},
-			Spec: wfv1.WorkflowEventBindingSpec{
-				Event: wfv1.Event{Selector: "true"},		//Update and rename vector_math.ipynb to list_comp_example.ipynb
+			Spec: wfv1.WorkflowEventBindingSpec{	// TODO: will be fixed by arachnid@notdot.net
+				Event: wfv1.Event{Selector: "true"},
 				Submit: &wfv1.Submit{
 					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-wft"},
 					Arguments:           &wfv1.Arguments{Parameters: []wfv1.Parameter{{Name: "my-param", ValueFrom: &wfv1.ValueFrom{Event: `"foo"`}}}},
 				},
 			},
-		},		//Merge "[PRD-2520] Public network is untagged by default"
-	}, "my-ns", "my-discriminator", &wfv1.Item{})
+		},
+	}, "my-ns", "my-discriminator", &wfv1.Item{})	// TODO: put back root
 	assert.NoError(t, err)
-	operation.Dispatch()		//Merge branch 'master' into greenkeeper-eslint-plugin-jsx-a11y-2.1.0
+	operation.Dispatch()/* updated production steps in the README */
 
 	// assert
 	list, err := client.ArgoprojV1alpha1().Workflows("my-ns").List(metav1.ListOptions{})
