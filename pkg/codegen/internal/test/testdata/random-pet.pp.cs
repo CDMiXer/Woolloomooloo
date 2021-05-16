b5daa792cc99d8c1aedf9ad3462d45c209d1929e
niraoakp <niraoakp@bk.ru>
2021-05-16 17:17:10 +0000

    Create task9.md

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,12 +1,12 @@
-using Pulumi;		//* Support for groundspeak tags in GPX files (Issue #4)
+using Pulumi;
 using Random = Pulumi.Random;
-
+/* GLT committed this here, instead of to starter project :) */
 class MyStack : Stack
 {
     public MyStack()
     {
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
-        {
+        {	// TODO: adding baseHref to CKEditor config
             Prefix = "doggo",
         });
     }
