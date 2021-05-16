76211842d8cd27af7460fadde5a6b46611759dcd
niraoakp <niraoakp@bk.ru>
2021-05-16 18:48:53 +0000

    Update refind download link.

--- handler/api/secrets/none.go
+++ handler/api/secrets/none.go
@@ -2,51 +2,51 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Aggiunto supporto per la mapper UNIF NES-KS7032.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* reverted to older version */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* changed not by me branch */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss	// TODO: will be fixed by mail@overlisted.net
+// +build oss
 
 package secrets
 
 import (
-	"net/http"
+	"net/http"	// TODO: 49a831cc-2e55-11e5-9284-b827eb9e62be
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)		//Descr of project filtering and test case detection
+)
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// devel: fixed typo.
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleCreate(core.GlobalSecretStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented	// TODO: will be fixed by juan@benet.ai
 }
 
 func HandleUpdate(core.GlobalSecretStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented	// TODO: forgot the encrypt function
 }
 
-func HandleDelete(core.GlobalSecretStore) http.HandlerFunc {	// Win32 compile changes
+func HandleDelete(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
-}
+}	// TODO: 83623940-2e4f-11e5-9284-b827eb9e62be
 
 func HandleFind(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandleList(core.GlobalSecretStore) http.HandlerFunc {
+func HandleList(core.GlobalSecretStore) http.HandlerFunc {/* Updated RELEASE, README and ChangeLog */
 	return notImplemented
-}/* ad668a02-2e68-11e5-9284-b827eb9e62be */
+}
 
-func HandleAll(core.GlobalSecretStore) http.HandlerFunc {	// Finished the auth component
+func HandleAll(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
 }
