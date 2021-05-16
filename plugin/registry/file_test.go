f79c983b4ef8fcef61c7212558d5fefcf684b8fb
niraoakp <niraoakp@bk.ru>
2021-05-16 18:41:42 +0000

    Update mongodb_mlab_database_users_and_collections.md

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 7.0.0 */
 
 // +build !oss
 
-package registry
+package registry/* trigger new build for jruby-head (68a1d95) */
 
 import (
-	"os"
+	"os"		//Add #update method to Client
 	"testing"
 
-	"github.com/drone/drone/core"
-	"github.com/google/go-cmp/cmp"
+	"github.com/drone/drone/core"	// TODO: Merge "[INTERNAL] UploadCollection comments changes for documentation"
+	"github.com/google/go-cmp/cmp"/* * Release version 0.60.7571 */
 )
-
+	// TODO: will be fixed by sbrichards@gmail.com
 func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
 	got, err := source.List(noContext, &core.RegistryArgs{})
@@ -34,8 +34,8 @@ func TestFileSource(t *testing.T) {
 
 func TestFileSourceErr(t *testing.T) {
 	source := FileSource("./auths/testdata/x.json")
-	_, err := source.List(noContext, &core.RegistryArgs{})
+	_, err := source.List(noContext, &core.RegistryArgs{})	// TODO: will be fixed by joshua@yottadb.com
 	if _, ok := err.(*os.PathError); !ok {
 		t.Errorf("Expect error when file does not exist")
-	}
+	}	// TODO: Reset the array of privpub posts to ensure correct output results.
 }
