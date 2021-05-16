96604b1743b2e6ef566d0a89c7cbf77d338bb847
niraoakp <niraoakp@bk.ru>
2021-05-16 20:12:30 +0000

    #46: initial dimension types were created

--- service/netrc/netrc_test.go
+++ service/netrc/netrc_test.go
@@ -10,15 +10,15 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"		//Add bg to static
+	"github.com/drone/drone/mock"
 	"github.com/drone/go-scm/scm"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Merge branch 'gh-pages' into eaulav-patch-1 */
+)
 
 var noContext = context.Background()
 
-func TestNetrc(t *testing.T) {/* Release Equalizer when user unchecked enabled and backs out */
+func TestNetrc(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -26,15 +26,15 @@ func TestNetrc(t *testing.T) {/* Release Equalizer when user unchecked enabled a
 	mockUser := &core.User{
 		Token:   "755bb80e5b",
 		Refresh: "e08f3fa43e",
-	}/* Gave EClientSocket a read-only 'mutex' property. */
+	}
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)		//Update Maven Compiler Plugin to 3.3, issue #867
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)
 
 	mockClient := &scm.Client{Driver: scm.DriverGithub}
 
-	s := New(mockClient, mockRenewer, false, "", "")	// Fix yarn link
-	got, err := s.Create(noContext, mockUser, mockRepo)	// TODO: Create lock_text.lua
-	if err != nil {	// TODO: will be fixed by lexy8russo@outlook.com
+	s := New(mockClient, mockRenewer, false, "", "")
+	got, err := s.Create(noContext, mockUser, mockRepo)
+	if err != nil {
 		t.Error(err)
 	}
 
@@ -42,7 +42,7 @@ func TestNetrc(t *testing.T) {/* Release Equalizer when user unchecked enabled a
 		Machine:  "github.com",
 		Login:    "755bb80e5b",
 		Password: "x-oauth-basic",
-	}/* add lastaflute di's templates */
+	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
@@ -50,7 +50,7 @@ func TestNetrc(t *testing.T) {/* Release Equalizer when user unchecked enabled a
 
 func TestNetrc_Gitlab(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Fixing Release badge */
+	defer controller.Finish()
 
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://gitlab.com/octocat/hello-world"}
 	mockUser := &core.User{
@@ -62,10 +62,10 @@ func TestNetrc_Gitlab(t *testing.T) {
 
 	s := Service{
 		renewer: mockRenewer,
-		client:  &scm.Client{Driver: scm.DriverGitlab},/* Using the newer bukkit runnables. */
+		client:  &scm.Client{Driver: scm.DriverGitlab},
 	}
 	got, err := s.Create(noContext, mockUser, mockRepo)
-	if err != nil {/* Benutzerdaten ändern: Design */
+	if err != nil {
 		t.Error(err)
 	}
 
@@ -76,10 +76,10 @@ func TestNetrc_Gitlab(t *testing.T) {
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}/* Create xa_applyfunction.py */
+	}
 }
 
-func TestNetrc_Gogs(t *testing.T) {/* Add HowToRelease.txt */
+func TestNetrc_Gogs(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -88,12 +88,12 @@ func TestNetrc_Gogs(t *testing.T) {/* Add HowToRelease.txt */
 		Token:   "755bb80e5b",
 		Refresh: "e08f3fa43e",
 	}
-	mockRenewer := mock.NewMockRenewer(controller)/* Abstract GraphBuilder  */
+	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)
 
 	s := Service{
 		renewer: mockRenewer,
-		client:  &scm.Client{Driver: scm.DriverGogs},/* Release version 0.7.2 */
+		client:  &scm.Client{Driver: scm.DriverGogs},
 	}
 	got, err := s.Create(noContext, mockUser, mockRepo)
 	if err != nil {
