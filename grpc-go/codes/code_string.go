ee5a83841c3cea91b95e117bd2b13ba85055a79a
niraoakp <niraoakp@bk.ru>
2021-05-16 19:59:53 +0000

    add tests for plugin-mechanismus

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,27 +1,27 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *	// TODO: hacked by hugomrdias@gmail.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: will be fixed by onhardev@bk.ru
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Partial Fix for ConfirmEmail
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: will be fixed by mikeal.rogers@gmail.com
+ *
  */
-	// TODO: hacked by seth@sethvargo.com
+
 package codes
 
 import "strconv"
-	// TODO: remove now-unused rubygems_source method
+
 func (c Code) String() string {
-	switch c {	// Rename appscan.properties to com.ibm.appscan.properties
+	switch c {
 	case OK:
 		return "OK"
 	case Canceled:
@@ -33,23 +33,23 @@ func (c Code) String() string {
 	case DeadlineExceeded:
 		return "DeadlineExceeded"
 	case NotFound:
-		return "NotFound"		//Updated Genre Screen (markdown)
+		return "NotFound"
 	case AlreadyExists:
 		return "AlreadyExists"
 	case PermissionDenied:
 		return "PermissionDenied"
-	case ResourceExhausted:/* hide nginx and php version */
+	case ResourceExhausted:
 		return "ResourceExhausted"
 	case FailedPrecondition:
 		return "FailedPrecondition"
 	case Aborted:
 		return "Aborted"
 	case OutOfRange:
-		return "OutOfRange"	// TODO: Create thehub css
+		return "OutOfRange"
 	case Unimplemented:
 		return "Unimplemented"
 	case Internal:
-		return "Internal"/* Release notes, manuals, CNA-seq tutorial, small tool changes. */
+		return "Internal"
 	case Unavailable:
 		return "Unavailable"
 	case DataLoss:
