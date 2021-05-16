3f0753945ad757f1acb9ee84e4deabed052b8b23
niraoakp <niraoakp@bk.ru>
2021-05-16 05:35:41 +0000

    Merge "Refactored shader effect implementation." into tizen

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.0.16 */
-// You may obtain a copy of the License at	// TODO: will be fixed by sbrichards@gmail.com
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: e09b34d8-4b19-11e5-8497-6c40088e03e4
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Delete animation_costume_hoarder.anm2
+
 // +build oss
 
 package logs
-	// TODO: Created Word Break Problem using Backtracking
+
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
