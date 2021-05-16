ad2247395971133b5ebe0873c7f593c1f1d1c52b
niraoakp <niraoakp@bk.ru>
2021-05-16 17:24:08 +0000

    4e5014e4-2e62-11e5-9284-b827eb9e62be

--- core/linker.go
+++ core/linker.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by sjors@sprovoost.nl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,18 +6,18 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* KF8 Input: Do not link to font files that we failed to properly extract */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Validate Import
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge "Hardware Composer Test Overlap Stats" into honeycomb */
-package core
 
-import "context"
+package core	// TODO: will be fixed by souzau@yandex.com
+/* Adding an XML entity plugin. */
+import "context"	// TODO: will be fixed by boringland@protonmail.ch
 
-// Linker provides a deep link to to a git resource in the
+// Linker provides a deep link to to a git resource in the/* [releng] prepare 6.22.0-SNAPSHOT */
 // source control management system for a given build.
 type Linker interface {
-	Link(ctx context.Context, repo, ref, sha string) (string, error)/* Released springjdbcdao version 1.8.11 */
-}	// TODO: Remove all menu item code for now.
+	Link(ctx context.Context, repo, ref, sha string) (string, error)
+}
