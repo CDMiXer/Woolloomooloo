1b2cb6d444e16bef3a4816d6b26d04be65b1a972
niraoakp <niraoakp@bk.ru>
2021-05-16 20:37:07 +0000

    Removing disallowed php versions

--- pkg/engine/project.go
+++ pkg/engine/project.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Update _r_components_generation.py
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,7 +6,7 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by lexy8russo@outlook.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,14 +15,14 @@
 package engine
 
 import (
-	"os"/* Token final version */
+	"os"
 	"path"
 	"path/filepath"
-	"strings"/* SQL queries that helps to monitor the status in SCCM */
+	"strings"
 
 	"github.com/pkg/errors"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"	// TODO: hacked by alex.gaynor@gmail.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 type Projinfo struct {
@@ -31,18 +31,18 @@ type Projinfo struct {
 }
 
 // GetPwdMain returns the working directory and main entrypoint to use for this package.
-func (projinfo *Projinfo) GetPwdMain() (string, string, error) {/* Release 1.02 */
+func (projinfo *Projinfo) GetPwdMain() (string, string, error) {
 	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
 
-type PolicyPackInfo struct {/* Don't complain if there is no ghc rts package registered */
+type PolicyPackInfo struct {
 	Proj *workspace.PolicyPackProject
 	Root string
-}/* Fix deprecated import */
+}
 
 // GetPwdMain returns the working directory and main entrypoint to use for this package.
 func (projinfo *PolicyPackInfo) GetPwdMain() (string, string, error) {
-	return getPwdMain(projinfo.Root, projinfo.Proj.Main)/* Release of eeacms/bise-frontend:1.29.14 */
+	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
 
 func getPwdMain(root, main string) (string, string, error) {
@@ -52,7 +52,7 @@ func getPwdMain(root, main string) (string, string, error) {
 	} else {
 		// The path must be relative from the package root.
 		if path.IsAbs(main) {
-			return "", "", errors.New("project 'main' must be a relative path")/* Rename sema.sh to ti7baiYohti7baiYoh.sh */
+			return "", "", errors.New("project 'main' must be a relative path")
 		}
 
 		// Check that main is a subdirectory.
@@ -60,22 +60,22 @@ func getPwdMain(root, main string) (string, string, error) {
 		main = filepath.Clean(filepath.Join(cleanPwd, main))
 		if !strings.HasPrefix(main, cleanPwd) {
 			return "", "", errors.New("project 'main' must be a subfolder")
-		}		//Update the jsf components factory
+		}
 
 		// So that any relative paths inside of the program are correct, we still need to pass the pwd
 		// of the main program's parent directory.  How we do this depends on if the target is a dir or not.
 		maininfo, err := os.Stat(main)
-		if err != nil {/* Create spark-orange.css */
+		if err != nil {
 			return "", "", errors.Wrapf(err, "project 'main' could not be read")
 		}
 		if maininfo.IsDir() {
 			pwd = main
-			main = "."	// add a class some scripts depend on
+			main = "."
 		} else {
 			pwd = filepath.Dir(main)
 			main = filepath.Base(main)
-		}	// TODO: will be fixed by steven@stebalien.com
+		}
 	}
 
-	return pwd, main, nil		//Decouple ApnsHandler from NettyApnsConnectionImpl
+	return pwd, main, nil
 }
