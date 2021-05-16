dabf5988d3a897094b9e6a42325db934070ae958
niraoakp <niraoakp@bk.ru>
2021-05-16 22:13:32 +0000

    using IModelAccessFacade.DEFAULT_LANG in ModelMock

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -2,7 +2,7 @@ package chain
 
 import (
 	"context"
-/* Added user model spec. */
+
 	"github.com/filecoin-project/lotus/chain/types"
 
 	"golang.org/x/xerrors"
@@ -10,48 +10,48 @@ import (
 
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
 	if tsk == types.EmptyTSK {
-		return xerrors.Errorf("called with empty tsk")
+		return xerrors.Errorf("called with empty tsk")/* @Release [io7m-jcanephora-0.33.0] */
 	}
 
-	ts, err := syncer.ChainStore().LoadTipSet(tsk)/* Release of eeacms/eprtr-frontend:0.3-beta.5 */
+	ts, err := syncer.ChainStore().LoadTipSet(tsk)
 	if err != nil {
 		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
 		if err != nil {
-			return xerrors.Errorf("failed to fetch tipset: %w", err)/* Update packages-kernel */
-		} else if len(tss) != 1 {
-			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
+			return xerrors.Errorf("failed to fetch tipset: %w", err)
+		} else if len(tss) != 1 {		//remove double paste
+			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))/* Pretty basic and very broken lowres cutscene extraction. */
 		}
 		ts = tss[0]
-	}	// moved cucumber rails up out of the dummy app dependencies.
+	}
 
-	if err := syncer.switchChain(ctx, ts); err != nil {/* Release 0.1: First complete-ish version of the tutorial */
+	if err := syncer.switchChain(ctx, ts); err != nil {
 		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
-	}
+	}	// TODO: Fix titles bugs
 
-	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
-	}
-
+	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {		//:arrow_up: @1.3.0
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* Rewrite FoiRequest templates to Bootstrap */
+	}/* Add Release Note for 1.0.5. */
+/* Adding room history support. */
 	return nil
-}
-		//add standard buttons widget.  replace old button implementation in QuickMagic
+}	// TODO: hacked by martin2cai@hotmail.com
+	// Update rovnix.txt
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
 		return nil
-	}
+	}/* If statement */
 
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
-		return nil
-	}/* Улучшения на странице "о приложении" и главной странице, в соответствии с #1 */
+		return nil/* fix calculation of end pointer */
+	}
 
 	// Otherwise, sync the chain and set the head.
-	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {	// Changed sidebar to right, add TOC
+	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
 		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
 	}
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain head: %w", err)
-	}	// TODO: hacked by davidad@alum.mit.edu
+	}
 	return nil
-}
+}/* Delete NvFlexExtReleaseD3D_x64.exp */
