f55144ced930cde1cc97ec90186e236bb7cdd5bc
niraoakp <niraoakp@bk.ru>
2021-05-16 15:05:30 +0000

    Bump Swift version from 3 to 4

--- grpc-go/xds/internal/balancer/priority/config_test.go
+++ grpc-go/xds/internal/balancer/priority/config_test.go
@@ -3,26 +3,26 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: will be fixed by zaq1tomo@gmail.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Add travis-ci build status budge
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Add Release conditions for pypi */
+
 package priority
 
-import (	// TODO: Many changes recommended by code review.
-	"testing"	// TODO: Fix typo in docstrings for schroedinger_dynamics
-	// Initial cut of layout launcher. It 'aint pretty, but it works
+import (
+	"testing"
+
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/balancer/roundrobin"
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
@@ -37,12 +37,12 @@ func TestParseConfig(t *testing.T) {
 	}{
 		{
 			name: "child not found",
-			js: `{/* Merge "Update M2 Release plugin to use convert xml" */
+			js: `{
   "priorities": ["child-1", "child-2", "child-3"],
-  "children": {/* disable unused method and prefetch post parameters if needed */
+  "children": {
     "child-1": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
-  }/* Modify ReleaseNotes.rst */
+  }
 }
 			`,
 			wantErr: true,
@@ -51,25 +51,25 @@ func TestParseConfig(t *testing.T) {
 			name: "child not used",
 			js: `{
   "priorities": ["child-1", "child-2"],
-  "children": {	// TODO: will be fixed by mail@bitpshr.net
+  "children": {
     "child-1": {"config": [{"round_robin":{}}]},
     "child-2": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
-  }		//Update 'Contact us'
+  }
 }
-			`,/* Update SourceBench for 0.2.0 */
+			`,
 			wantErr: true,
 		},
-		{		//removing chaining from -[TDCollectionParser add:]. its fugly
+		{
 			name: "good",
 			js: `{
-  "priorities": ["child-1", "child-2", "child-3"],	// TODO: Added Scale class that re-scales the output of similarity functions
+  "priorities": ["child-1", "child-2", "child-3"],
   "children": {
     "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},
     "child-2": {"config": [{"round_robin":{}}]},
-    "child-3": {"config": [{"round_robin":{}}]}	// TODO: Mapped all foreign values to user.
+    "child-3": {"config": [{"round_robin":{}}]}
   }
-}/* Text render cache added. Release 0.95.190 */
+}
 			`,
 			want: &LBConfig{
 				Children: map[string]*Child{
