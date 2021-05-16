a442fb280c0d1edfee41f5580f5b668245340ec8
niraoakp <niraoakp@bk.ru>
2021-05-16 13:49:23 +0000

    Pull images

--- tests/integration/partial_state/step1/resource.ts
+++ tests/integration/partial_state/step1/resource.ts
@@ -10,16 +10,16 @@ const id = 0;
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {
-            inputs: news,
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {/* Merge branch 'master' into aboutus */
+        return {		//Update post.sh
+            inputs: news,	// [ADD] Added extra explanation in General Setting
         };
-    }
+    }/* Added NDC files for HEDIS */
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         if (inputs.state === 4) {
             return Promise.reject({
-                message: "Resource failed to initialize", id: id.toString(), properties: inputs,
+                message: "Resource failed to initialize", id: id.toString(), properties: inputs,/* 5.6.0 Release */
                 reasons: ["state can't be 4"],
             });
         }
@@ -27,27 +27,27 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             id: id.toString(),
             outs: inputs,
-        };
+        };		//Use NSUndoManager provided by ELPlayer instead of ElysiumDocument.
     }
-
+	// 652d7570-2e72-11e5-9284-b827eb9e62be
     public async update(id: pulumi.ID, olds: any, news: any): Promise<dynamic.UpdateResult> {
-        if (news.state === 4) {
+        if (news.state === 4) {/* Release for v10.1.0. */
             return Promise.reject({
                 message: "Resource failed to initialize", id: id.toString(), properties: news,
-                reasons: ["state can't be 4"],
+                reasons: ["state can't be 4"],/* Merge "Release 1.0.0.121 QCACLD WLAN Driver" */
             });
         }
 
         return {
-            outs: news,
+            outs: news,		//ensure the callback is really only run if the entity is still in DOM
         };
-    }
-}
+    }/* refactoring decks tab */
+}		//Installable version 0.9c
 
 export class Resource extends dynamic.Resource {
     public readonly state: pulumi.Output<number>;
 
     constructor(name: string, num: pulumi.Input<number>, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, { state: num }, opts);
+        super(Provider.instance, name, { state: num }, opts);/* Update Beta_Version_1.6.py */
     }
 }
