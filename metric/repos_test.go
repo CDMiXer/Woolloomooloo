6dec06f8c45c4f6b5841df4537be3e6bf7c3132b
niraoakp <niraoakp@bk.ru>
2021-05-16 21:43:25 +0000

    [releng] Start previously installed server if it is not running

--- metric/repos_test.go
+++ metric/repos_test.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release-1.4.0 Setting initial version */
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: Bug 1650: Fixed completely screwed-up indentation.
 
 // +build !oss
-/* return the correct type */
+
 package metric
 
 import (
 	"testing"
 
 	"github.com/drone/drone/mock"
-/* Rename bra_pagination.js to bra-pagination.js */
+
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -20,12 +20,12 @@ func TestRepoCount(t *testing.T) {
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
-	snapshot := prometheus.DefaultRegisterer
-	defer func() {/* Release new version 2.2.1: Typo fix */
+	snapshot := prometheus.DefaultRegisterer/* Release 2.6.2 */
+	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
-/* Removed var variable declarations */
+
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
@@ -33,24 +33,24 @@ func TestRepoCount(t *testing.T) {
 	// x2 repository count
 	count := int64(5)
 
-	store := mock.NewMockRepositoryStore(controller)
+	store := mock.NewMockRepositoryStore(controller)	// TODO: further comments from Roger
 	store.EXPECT().Count(gomock.Any()).Return(count, nil)
 	RepoCount(store)
 
 	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
-		return
+		return/* Gradle Release Plugin - new version commit:  '0.8b'. */
 	}
-	if want, got := len(metrics), 1; want != got {/* Released 1.6.1 */
+	if want, got := len(metrics), 1; want != got {	// Rev neon version to pick up latest WAAPI polyfills
 		t.Errorf("Expect registered metric")
-		return
+		return/* Merge "Fix a typo in the comment" */
 	}
-	metric := metrics[0]/* Release TomcatBoot-0.3.9 */
+	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_repo_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
-	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {/* Publishing post - Solving a Job Application Code Challenge */
+	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
