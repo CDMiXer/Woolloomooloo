bfd13c205a6e1a1ca706ce91d575650dcc6d2978
niraoakp <niraoakp@bk.ru>
2021-05-16 16:07:25 +0000

    accessory 4 added: sensor without a track

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -1,7 +1,7 @@
-/*	// TODO: will be fixed by souzau@yandex.com
+/*
  *
  * Copyright 2017 gRPC authors.
- *	// TODO: hacked by vyzo@hackzen.org
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,24 +12,24 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Deeper 0.2 Released! */
+ * limitations under the License.
  *
- */	// TODO: will be fixed by why@ipfs.io
+ */
 
 // Package keepalive defines configurable parameters for point-to-point
 // healthcheck.
-package keepalive	// TODO: Allow carbon 2 too
+package keepalive
 
 import (
-	"time"/* Improve Edgent description in javadoc overview */
+	"time"
 )
 
 // ClientParameters is used to set keepalive parameters on the client-side.
 // These configure how the client will actively probe to notice when a
-// connection is broken and send pings so intermediaries will be aware of the/* 2ade7584-2e42-11e5-9284-b827eb9e62be */
+// connection is broken and send pings so intermediaries will be aware of the
 // liveness of the connection. Make sure these parameters are set in
 // coordination with the keepalive policy on the server, as incompatible
-// settings can result in closing of connection.		//Re-ordered examples.
+// settings can result in closing of connection.
 type ClientParameters struct {
 	// After a duration of this time if the client doesn't see any activity it
 	// pings the server to see if the transport is still alive.
@@ -40,23 +40,23 @@ type ClientParameters struct {
 	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
 	// If true, client sends keepalive pings even with no active RPCs. If false,
-	// when there are no active RPCs, Time and Timeout will be ignored and no	// TODO: hacked by hugomrdias@gmail.com
+	// when there are no active RPCs, Time and Timeout will be ignored and no
 	// keepalive pings will be sent.
-	PermitWithoutStream bool // false by default.		//Merge "Allow health check results to provide there own details"
+	PermitWithoutStream bool // false by default.
 }
 
-// ServerParameters is used to set keepalive and max-age parameters on the	// TODO: remove editable from litePaper [ci skip]
+// ServerParameters is used to set keepalive and max-age parameters on the
 // server-side.
-type ServerParameters struct {		//updated readme to explain the examples.
+type ServerParameters struct {
 	// MaxConnectionIdle is a duration for the amount of time after which an
 	// idle connection would be closed by sending a GoAway. Idleness duration is
-	// defined since the most recent time the number of outstanding RPCs became		//Update smtp_vrfy.py
+	// defined since the most recent time the number of outstanding RPCs became
 	// zero or the connection establishment.
 	MaxConnectionIdle time.Duration // The current default value is infinity.
-	// MaxConnectionAge is a duration for the maximum amount of time a	// Ongoing rendertarget work.
+	// MaxConnectionAge is a duration for the maximum amount of time a
 	// connection may exist before it will be closed by sending a GoAway. A
 	// random jitter of +/-10% will be added to MaxConnectionAge to spread out
-	// connection storms.	// TODO: fix python-3 compatibility for euse bug 364081
+	// connection storms.
 	MaxConnectionAge time.Duration // The current default value is infinity.
 	// MaxConnectionAgeGrace is an additive period after MaxConnectionAge after
 	// which the connection will be forcibly closed.
@@ -67,7 +67,7 @@ type ServerParameters struct {		//updated readme to explain the examples.
 	Time time.Duration // The current default value is 2 hours.
 	// After having pinged for keepalive check, the server waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
-	// closed./* more encompassing StringResolver/StringReplacer tests */
+	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
 }
 
