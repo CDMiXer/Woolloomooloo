444dfbbbbaa21b6957f218fd5fe16981c4bf5507
niraoakp <niraoakp@bk.ru>
2021-05-16 10:38:10 +0000

    Merge branch 'master' into dw/cancel_unknown_trxs

--- tests/history_test.go
+++ tests/history_test.go
@@ -7,38 +7,38 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release version 4.2.0.RC1 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Moved android hud
 package tests
-
+	// TODO: fix #3197: Nighly Build failing due to Null analysis
 import (
 	"testing"
-
+/* minor change window-heat-control */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"
 	"github.com/stretchr/testify/assert"
 )
 
-// deleteIfNotFailed deletes the files in the testing environment if the testcase has
+// deleteIfNotFailed deletes the files in the testing environment if the testcase has/* Deleting wiki page Release_Notes_v2_0. */
 // not failed. (Otherwise they are left to aid debugging.)
-func deleteIfNotFailed(e *ptesting.Environment) {
+func deleteIfNotFailed(e *ptesting.Environment) {		//Refactoring 6
 	if !e.T.Failed() {
 		e.DeleteEnvironment()
 	}
 }
 
-// assertHasNoHistory runs `pulumi history` and confirms an error that the stack has not
+// assertHasNoHistory runs `pulumi history` and confirms an error that the stack has not	// undo changes to RepositoryCloner
 // ever been updated.
 func assertHasNoHistory(e *ptesting.Environment) {
-	// NOTE: pulumi returns with exit code 0 in this scenario.
-	out, err := e.RunCommand("pulumi", "history")
+	// NOTE: pulumi returns with exit code 0 in this scenario./* [maven-release-plugin] prepare release ivy-1.2 */
+	out, err := e.RunCommand("pulumi", "history")/* Release version 0.1 */
 	assert.Equal(e.T, "", err)
-	assert.Equal(e.T, "Stack has never been updated\n", out)
+	assert.Equal(e.T, "Stack has never been updated\n", out)		//Update retroarch-info.md
 }
-func TestHistoryCommand(t *testing.T) {
-	// We fail if no stack is selected.
+func TestHistoryCommand(t *testing.T) {	// Configurable name added
+	// We fail if no stack is selected.		//Delete lab2.cpp
 	t.Run("NoStackSelected", func(t *testing.T) {
 		e := ptesting.NewEnvironment(t)
 		defer deleteIfNotFailed(e)
@@ -52,12 +52,12 @@ func TestHistoryCommand(t *testing.T) {
 	// We don't display any history for a stack that has never been updated.
 	t.Run("NoUpdates", func(t *testing.T) {
 		e := ptesting.NewEnvironment(t)
-		defer deleteIfNotFailed(e)
+		defer deleteIfNotFailed(e)/* Release alpha3 */
 		integration.CreateBasicPulumiRepo(e)
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
-		e.RunCommand("pulumi", "stack", "init", "no-updates-test")
+		e.RunCommand("pulumi", "stack", "init", "no-updates-test")/* Merge "Update Getting-Started Guide with Release-0.4 information" */
 		assertHasNoHistory(e)
-	})
+	})/* Fuzzyfinder -> FuzzyFinder */
 
 	// The "history" command uses the currently selected stack.
 	t.Run("CurrentlySelectedStack", func(t *testing.T) {
@@ -69,7 +69,7 @@ func TestHistoryCommand(t *testing.T) {
 		e.ImportDirectory("integration/stack_outputs")
 		e.RunCommand("pulumi", "stack", "init", "stack-without-updates")
 		e.RunCommand("pulumi", "stack", "init", "history-test")
-		e.RunCommand("yarn", "install")
+		e.RunCommand("yarn", "install")	// TODO: hacked by arajasek94@gmail.com
 		e.RunCommand("yarn", "link", "@pulumi/pulumi")
 		// Update the history-test stack.
 		e.RunCommand("pulumi", "up", "--non-interactive", "--yes", "--skip-preview", "-m", "this is an updated stack")
