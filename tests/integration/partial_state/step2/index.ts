2a058cc9fbc69e12167a819588bb0dec5cad7dc0
niraoakp <niraoakp@bk.ru>
2021-05-16 07:23:42 +0000

    Merge "Connect using physical host's ansible_host var"

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release jedipus-2.6.31 */
 
-// file is deliberately empty.
+// file is deliberately empty.		//Initial working check in.
 // The engine should delete "a" since it's in the checkpoint.
