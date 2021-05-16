521c1b552393bede009c91a742fd011753c48fcf
niraoakp <niraoakp@bk.ru>
2021-05-16 03:10:29 +0000

    Add nnrc_imp to the tests.

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
 import pulumi
 
-# Create a very long string (>4mb)/* Right old wrongs. */
+# Create a very long string (>4mb)
 long_string = "a" * 5 * 1024 * 1025
-/* Merge "Release 3.0.10.017 Prima WLAN Driver" */
+
 # Export the name of the bucket
 pulumi.export("long_string",  long_string)
