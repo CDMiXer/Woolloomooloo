bd0314fa6a47738f137393d1949a6553f57d19dd
niraoakp <niraoakp@bk.ru>
2021-05-16 23:28:41 +0000

    Merge "tests: fix unworking debug output"

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,11 +1,11 @@
 // +build calibnet
 
-package build		//Merge "Config driver: use "True" instead of "always""
+package build
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: will be fixed by magik6k@gmail.com
+	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
@@ -13,10 +13,10 @@ import (
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
-/* Release: 5.8.1 changelog */
+
 const BootstrappersFile = "calibnet.pi"
 const GenesisFile = "calibnet.car"
-		//Added sorting example
+
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 
@@ -25,13 +25,13 @@ const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)/* Add Release Notes section */
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
 
 const UpgradeLiftoffHeight = -5
 
-const UpgradeKumquatHeight = 90/* workflows: no need to set up ruby on ubuntu */
+const UpgradeKumquatHeight = 90
 
 const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
@@ -44,23 +44,23 @@ const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
 const UpgradeActorsV4Height = 193789
-		//Don't set Quick Photo as featured image
-func init() {/* Released springjdbcdao version 1.8.17 */
+
+func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
 	policy.SetSupportedProofTypes(
-		abi.RegisteredSealProof_StackedDrg32GiBV1,		//Delete rpi_gateway.py
+		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)/* Merge "[INTERNAL] Release notes for version 1.28.24" */
-		//added eax api
-	SetAddressNetwork(address.Testnet)/* using new third-party directory layout */
+	)
+
+	SetAddressNetwork(address.Testnet)
 
 	Devnet = true
-/* Added nodemon .monitor file to .gitignore */
+
 	BuildType = BuildCalibnet
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-/* add SO source for snippet */
+
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
