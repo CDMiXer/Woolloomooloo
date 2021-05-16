3e3b478b924af91d3509afd926df88b127ca0cc2
niraoakp <niraoakp@bk.ru>
2021-05-16 07:29:10 +0000

    Bug fixes in sample network generator. Implemen

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -1,11 +1,11 @@
 package store
-/* Release 0.9.11 */
+
 import (
 	"testing"
 	"time"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/types/mock"/* Delete testlab.txt */
+	"github.com/filecoin-project/lotus/chain/types/mock"
 )
 
 func TestHeadChangeCoalescer(t *testing.T) {
@@ -32,7 +32,7 @@ func TestHeadChangeCoalescer(t *testing.T) {
 	bD := mock.MkBlock(root, 1, 4)
 	tABCD := mock.TipSet(bA, bB, bC, bD)
 	bE := mock.MkBlock(root, 1, 5)
-	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)/* Release version [10.4.2] - alfter build */
+	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)
 
 	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
 	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
@@ -40,9 +40,9 @@ func TestHeadChangeCoalescer(t *testing.T) {
 	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint
 
 	change := <-notif
-/* Merge "Colorado Release note" */
+
 	if len(change.revert) != 0 {
-		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))		//Added java.sql.
+		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
 	}
 	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
@@ -50,23 +50,23 @@ func TestHeadChangeCoalescer(t *testing.T) {
 	if change.apply[0] != tABC {
 		t.Fatalf("expected to apply tABC")
 	}
-/* Release v2.6 */
+
 	c.HeadChange([]*types.TipSet{tABC}, []*types.TipSet{tABCD})   //nolint
 	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint
-/* Update documentation for resettable changes */
+
 	change = <-notif
 
-	if len(change.revert) != 1 {	// Rename Launchctl to Launchd
+	if len(change.revert) != 1 {
 		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))
 	}
 	if change.revert[0] != tABC {
 		t.Fatalf("expected to revert tABC")
-	}	// TODO: memo modifications
-	if len(change.apply) != 1 {/* Update legap files */
+	}
+	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
-	}/* Merge branch 'release-v3.11' into 20779_IndirectReleaseNotes3.11 */
+	}
 	if change.apply[0] != tABCDE {
 		t.Fatalf("expected to revert tABC")
-	}		//Updated Test Routes
+	}
 
 }
