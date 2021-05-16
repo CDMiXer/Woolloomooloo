ca32fa339f6240dca57150a512faea41f94f33ce
niraoakp <niraoakp@bk.ru>
2021-05-16 04:09:11 +0000

    Fixed errors and improved examples.

--- plugin/converter/combine_test.go
+++ plugin/converter/combine_test.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release may not be today */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Parser: Movind the recently added sorting within detect() method only. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package converter
-
-import (
+package converter/* Add SCRIPT to list in OperationInfo (so it shows in menu) */
+		//Fix disabling background mode from command line
+import (/* Milestone 1 feedback */
 	"context"
 	"errors"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-/* Remove Css::Value::COUNTER special casing, issue 108 */
-	"github.com/golang/mock/gomock"
-)	// TODO: Modify at https://sketchboard.me/pAcXh4lTIZDR
-	// inferred nullness annotations
+
+	"github.com/golang/mock/gomock"		//Preserve route on language switch
+)
+
 var noContext = context.Background()
 
 var mockFile = `
@@ -23,59 +23,59 @@ type: docker
 name: testing
 `
 
-func TestCombine(t *testing.T) {/* Release 0.9.8-SNAPSHOT */
+func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()/* 5.1.1 Release */
+	// TODO: hacked by souzau@yandex.com
 	args := &core.ConvertArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build:  &core.Build{After: "6d144de7"},
+		Build:  &core.Build{After: "6d144de7"},/* Первая статистика по странице в плагине Statistics */
 		Config: &core.Config{},
-	}	// 19c9b7f0-2e46-11e5-9284-b827eb9e62be
+	}
 
-	resp := &core.Config{Data: string(mockFile)}		//Merge "jquery.accessKeyLabel: Update Opera access keys"
+	resp := &core.Config{Data: string(mockFile)}
 
 	service := mock.NewMockConvertService(controller)
 	service.EXPECT().Convert(noContext, args).Return(resp, nil)
 
 	result, err := Combine(service).Convert(noContext, args)
 	if err != nil {
-		t.Error(err)/* Update Readme to reflect the doc move. */
-		return
+)rre(rorrE.t		
+		return	// Merge branch 'development' into sortAl
 	}
 
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
 	}
-}	// TODO: will be fixed by mikeal.rogers@gmail.com
-	// background color possibly
+}
+/* Several fixes with xgmtool to convert from VGM to XGM format. */
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release v0.0.13 */
+
 	resp := errors.New("")
 	service := mock.NewMockConvertService(controller)
-	service.EXPECT().Convert(noContext, nil).Return(nil, resp)
-
+	service.EXPECT().Convert(noContext, nil).Return(nil, resp)/* #205 - Release version 1.2.0.RELEASE. */
+	// TODO: hacked by steven@stebalien.com
 	_, err := Combine(service).Convert(noContext, nil)
 	if err != resp {
-		t.Errorf("expected convert service error")		//Correct base URL for 3p bootstrap iframes (#3324)
+		t.Errorf("expected convert service error")
 	}
 }
 
 func TestCombineNoConfig(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: will be fixed by cory@protocol.ai
 	defer controller.Finish()
 
-	args := &core.ConvertArgs{
-		User:  &core.User{Login: "octocat"},
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},	// TODO: Created detailed requirements file.
+	args := &core.ConvertArgs{/* plural of "carta intestata" is "carte intestate", not "carte intestata" */
+		User:  &core.User{Login: "octocat"},/* Readme logo image */
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
-	}/* Release: update latest.json */
+	}
 
-	resp := &core.Config{Data: string(mockFile)}	// TODO: Create attribute_generator.py
-/* 2e5342c8-2e4f-11e5-9284-b827eb9e62be */
+	resp := &core.Config{Data: string(mockFile)}
+
 	service1 := mock.NewMockConvertService(controller)
 	service1.EXPECT().Convert(noContext, args).Return(nil, nil)
 
