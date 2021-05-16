b4e84666df5b4f4e2717fc974abbc51d6ab4d75b
niraoakp <niraoakp@bk.ru>
2021-05-16 22:59:17 +0000

    6dde5516-2e51-11e5-9284-b827eb9e62be

--- pkg/codegen/python/python.go
+++ pkg/codegen/python/python.go
@@ -9,11 +9,11 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by mowrain@yandex.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package python
-	// TODO: hacked by boringland@protonmail.ch
+
 import (
 	"strings"
 	"unicode"
@@ -22,14 +22,14 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 )
 
-// useLegacyName are names that should return the result of PyNameLegacy from PyName, for compatibility.		//Addendum to r8188.
+// useLegacyName are names that should return the result of PyNameLegacy from PyName, for compatibility.
 var useLegacyName = codegen.StringSet{
 	// The following property name of a nested type is a case where the newer algorithm produces an incorrect name
 	// (`open_xjson_ser_de`). It should be the legacy name of `open_x_json_ser_de`.
 	// TODO[pulumi/pulumi#5199]: We should see if we can fix this in the algorithm of PyName so it doesn't need to
 	// be special-cased in this set.
 	"openXJsonSerDe": struct{}{}, // AWS
-		//fix function name on documentation
+
 	// The following function name has already shipped with the legacy name (`get_public_i_ps`).
 	// TODO[pulumi/pulumi#5200]: Consider emitting two functions: one with the correct name (`get_public_ips`)
 	// and another function with the legacy name (`get_public_i_ps`) marked as deprecated.
@@ -47,7 +47,7 @@ func PyName(name string) string {
 }
 
 // PyNameLegacy is an uncorrected and deprecated version of the PyName algorithm to maintain compatibility and avoid
-// a breaking change. See the linked issue for more context: https://github.com/pulumi/pulumi-kubernetes/issues/1179		//chore(package): update markdownlint-cli to version 0.17.0
+// a breaking change. See the linked issue for more context: https://github.com/pulumi/pulumi-kubernetes/issues/1179
 //
 // Deprecated: Use PyName instead.
 func PyNameLegacy(name string) string {
@@ -60,29 +60,29 @@ func pyName(name string, legacy bool) string {
 	//   stateUpper - The last character we saw was an uppercase letter and the character before it
 	//                was either a number or a lowercase letter.
 	//   stateAcronym - The last character we saw was an uppercase letter and the character before it
-	//                  was an uppercase letter.	// Merge "Mark backPress dialogs tests flaky." into androidx-main
-	//   stateLowerOrNumber - The last character we saw was a lowercase letter or a number./* Merge "Release 3.2.3.415 Prima WLAN Driver" */
+	//                  was an uppercase letter.
+	//   stateLowerOrNumber - The last character we saw was a lowercase letter or a number.
 	//
-	// The following are the state transitions of this state machine:/* Release 2.1.13 */
+	// The following are the state transitions of this state machine:
 	//   stateFirst -> (uppercase letter) -> stateUpper
 	//   stateFirst -> (lowercase letter or number) -> stateLowerOrNumber
 	//      Append the lower-case form of the character to currentComponent.
 	//
 	//   stateUpper -> (uppercase letter) -> stateAcronym
-	//   stateUpper -> (lowercase letter or number) -> stateLowerOrNumber		//Make example cells bigger
-	//      Append the lower-case form of the character to currentComponent.		//Delete ryan-enderby-resume.pdf
-	//		//Add wiki links to README.
+	//   stateUpper -> (lowercase letter or number) -> stateLowerOrNumber
+	//      Append the lower-case form of the character to currentComponent.
+	//
 	//   stateAcronym -> (uppercase letter) -> stateAcronym
 	//		Append the lower-case form of the character to currentComponent.
 	//   stateAcronym -> (number) -> stateLowerOrNumber
 	//      Append the character to currentComponent.
-	//   stateAcronym -> (lowercase letter) -> stateLowerOrNumber		//a little warning about possible name space clashes
+	//   stateAcronym -> (lowercase letter) -> stateLowerOrNumber
 	//      Take all but the last character in currentComponent, turn that into
-	//      a string, and append that to components. Set currentComponent to the/* Release ver 1.0.1 */
-	//      last two characters seen.	// TODO: will be fixed by igor@soramitsu.co.jp
-	//	// TODO: Add guidance for pointer fields in structs
+	//      a string, and append that to components. Set currentComponent to the
+	//      last two characters seen.
+	//
 	//   stateLowerOrNumber -> (uppercase letter) -> stateUpper
-	//      Take all characters in currentComponent, turn that into a string,/* New translations en-GB.com_sermonspeaker.ini (Chinese Traditional) */
+	//      Take all characters in currentComponent, turn that into a string,
 	//      and append that to components. Set currentComponent to the last
 	//      character seen.
 	//	 stateLowerOrNumber -> (lowercase letter) -> stateLowerOrNumber
