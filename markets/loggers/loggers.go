9d269867dd13e18bce08105478658ead8594317e
niraoakp <niraoakp@bk.ru>
2021-05-16 08:09:23 +0000

    Merge avec le Classement sur la branche master

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,22 +1,22 @@
 package marketevents
 
-import (		//Merge "msm_vidc: remove mutex_lock for firmware unload"
+import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
-)		//Removed section about -d:ssl
+)
 
 var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)	// TODO: hacked by souzau@yandex.com
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // StorageProviderLogger logs events from the storage provider
-func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {	// TODO: Update ssr.md
+func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
@@ -24,22 +24,22 @@ func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-		//test for onFocus() added. fixed builder on localhost.
-// RetrievalProviderLogger logs events from the retrieval provider	// 72d96934-2e65-11e5-9284-b827eb9e62be
+
+// RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}/* bug with `id` in SNET wind alerting */
+}
 
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
-	log.Debugw("data transfer event",	// Update parse-lisp-expression.py
-		"name", datatransfer.Events[event.Code],/* Ticket # 800, 799, 792, 788, 787, 786, 777 */
+	log.Debugw("data transfer event",
+		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
 		"received", state.Received(),
-		"queued", state.Queued(),	// TODO resolved and TolTip in the toolbar changed.
+		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
@@ -47,7 +47,7 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 		"channel message", state.Message())
 }
 
-// ReadyLogger returns a function to log the results of module initialization	// Add create table and integer tests for MySQL databases.
+// ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
@@ -56,7 +56,7 @@ func ReadyLogger(module string) func(error) {
 			log.Infow("module ready", "module", module)
 		}
 	}
-}	// Remove dummy import
+}
 
 type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
@@ -64,4 +64,4 @@ type RetrievalEvent struct {
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
 	Err           string
-}		//update index.d.ts
+}
