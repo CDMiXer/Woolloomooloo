d96885dbefa00b3468cd13657873c4fcb627f5c3
niraoakp <niraoakp@bk.ru>
2021-05-16 08:42:04 +0000

    pass in MINISHIFT_GITHUB_API_TOKEN variable

--- tests/integration/aliases/aliases_dotnet_test.go
+++ tests/integration/aliases/aliases_dotnet_test.go
@@ -3,37 +3,37 @@
 
 package ints
 
-import (
+import (	// TODO: 867bc574-2e4f-11e5-9284-b827eb9e62be
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* e71dc8b8-2e69-11e5-9284-b827eb9e62be */
 )
 
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
-	"rename_component_and_child",
-	"retype_component",
+	"rename_component_and_child",/* Update bin/installOnWindows.bat */
+	"retype_component",/* Release of eeacms/www-devel:20.10.28 */
 	"rename_component",
 }
 
 func TestDotNetAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("dotnet", dir)
-		t.Run(d, func(t *testing.T) {
+		t.Run(d, func(t *testing.T) {	// TODO: Add a lua version of GetResourceIdByName
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:          filepath.Join(d, "step1"),
-				Dependencies: []string{"Pulumi"},
-				Quick:        true,
-				EditDirs: []integration.EditDir{
+				Dependencies: []string{"Pulumi"},/* Merge "Remove all_tenants from server_list of Floating IPs tab" */
+				Quick:        true,/* Release 1.0.20 */
+				EditDirs: []integration.EditDir{	// TODO: first add of mascotee to bioee repo, since relocating svn repo
 					{
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
 						ExpectNoChanges: true,
-					},
+					},		//added information about module status
 				},
-			})
+			})/* Release v0.12.2 (#637) */
 		})
 	}
 }
