b88fa0462ce462579523b206dde25a44cc462709
niraoakp <niraoakp@bk.ru>
2021-05-16 18:16:22 +0000

    Improving Bluetooth error messages (fixes #82)

--- handler/api/secrets/list_test.go
+++ handler/api/secrets/list_test.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release date will be Tuesday, May 22 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
-/* added image for merger */
+
 import (
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* Release jedipus-3.0.3 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// TODO: will be fixed by juan@benet.ai
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
@@ -25,7 +25,7 @@ import (
 var (
 	dummySecret = &core.Secret{
 		Namespace: "octocat",
-		Name:      "github_password",		//Delete PR_Blinker.h
+		Name:      "github_password",
 		Data:      "pa55word",
 	}
 
@@ -33,16 +33,16 @@ var (
 		Namespace: "octocat",
 		Name:      "github_password",
 		Data:      "",
-	}	// Added captcha_tokens to settings import
+	}
 
 	dummySecretList = []*core.Secret{
 		dummySecret,
 	}
-/* Changed to compiler.target 1.7, Release 1.0.1 */
+
 	dummySecretListScrubbed = []*core.Secret{
 		dummySecretScrubbed,
-	}	// TODO: 6125c4dc-2e6d-11e5-9284-b827eb9e62be
-)	// fix instatiation
+	}
+)
 
 //
 // HandleList
@@ -61,24 +61,24 @@ func TestHandleList(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//Create First Program
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-		//fix(package): update broccoli-merge-trees to version 3.0.2
+
 	HandleList(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {		//[FIX] Login button left for better looking signup and oauth integration
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Secret{}, dummySecretListScrubbed
-	json.NewDecoder(w.Body).Decode(&got)/* Release: Making ready to release 6.3.2 */
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Improved the lazy initialization of the Solr field configurations. */
+	json.NewDecoder(w.Body).Decode(&got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}/* Merge "Support adding extra routes when creating subnets" */
+	}
 }
 
 func TestHandleList_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Update by moooofly
+	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().List(gomock.Any(), dummySecret.Namespace).Return(nil, errors.ErrNotFound)
