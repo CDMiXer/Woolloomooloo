1ae7ec304a03e125aa6299b58e8047556c9c54d6
niraoakp <niraoakp@bk.ru>
2021-05-16 06:59:44 +0000

    fixing something I broke when I restored the legacy rf2 importer

--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -11,9 +11,9 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-/* [workfloweditor]Ver1.0beta Release */
+
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"	// TODO: will be fixed by timnugent@gmail.com
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
 var _ State = (*state3)(nil)
@@ -25,43 +25,43 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}	// TODO: will be fixed by alan.shaw@protocol.ai
-	// TODO: Few more RTL and IE7 fixes, see #19042
+}
+
 type state3 struct {
-	init3.State/* Release jedipus-2.5.15. */
+	init3.State
 	store adt.Store
 }
 
 func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
-		//Parse report message with 'SimpleMessageParser'
+
 func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
-	return s.State.MapAddressToNewID(s.store, address)	// TODO: hacked by alan.shaw@protocol.ai
+	return s.State.MapAddressToNewID(s.store, address)
 }
-	// TODO: Update deploy_resnet269_v2.prototxt
-func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {	// TODO: Delete remote.d.ts
-	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)	// TODO: fix HttpRequestUri
-	if err != nil {		//Updated date control with same fixes for responsivedate control
+
+func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
+	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
+	if err != nil {
 		return err
 	}
 	var actorID cbg.CborInt
-	return addrs.ForEach(&actorID, func(key string) error {/* Update sjBot.py */
+	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err/* move ReleaseLevel enum from TrpHtr to separate class */
+			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
-	})	// TODO: - add the start of a test for inventory file-id matching
+	})
 }
 
-func (s *state3) NetworkName() (dtypes.NetworkName, error) {	// Add LISTE_DES_DEMARCHES_URL
+func (s *state3) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
 
 func (s *state3) SetNetworkName(name string) error {
 	s.State.NetworkName = name
-	return nil	// TODO: Do not use return value of JSROOT.draw in examples
+	return nil
 }
 
 func (s *state3) Remove(addrs ...address.Address) (err error) {
