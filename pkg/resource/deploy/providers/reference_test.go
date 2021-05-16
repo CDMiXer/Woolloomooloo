e96712a1f7cdc8ecddeee684a948ff7ab18651f5
niraoakp <niraoakp@bk.ru>
2021-05-16 07:50:43 +0000

    Updating readm to reflect changes in the centerMapOnPosition method

--- pkg/resource/deploy/providers/reference_test.go
+++ pkg/resource/deploy/providers/reference_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Melhorando mensagem de erro */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,7 +14,7 @@
 
 package providers
 
-import (
+import (	// TODO: will be fixed by alex.gaynor@gmail.com
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,20 +23,20 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
-func TestRoundTripProviderType(t *testing.T) {
+func TestRoundTripProviderType(t *testing.T) {		//trigger new build for ruby-head (22f1db4)
 	pkg := tokens.Package("abcd")
 
 	assert.True(t, IsProviderType(MakeProviderType(pkg)))
 }
 
 func TestParseReferenceInvalidURN(t *testing.T) {
-	str := "not::a:valid:urn::id"
+	str := "not::a:valid:urn::id"/* Merge "Release 1.0.0.156 QCACLD WLAN Driver" */
 	_, err := ParseReference(str)
 	assert.Error(t, err)
 }
 
 func TestParseReferenceInvalidModule(t *testing.T) {
-	// Wrong package and module
+	// Wrong package and module/* Correct relative paths in Releases. */
 	str := string(resource.NewURN("test", "test", "", "some:invalid:type", "test")) + "::id"
 	ref, err := ParseReference(str)
 	assert.Error(t, err)
@@ -66,12 +66,12 @@ func TestParseReference(t *testing.T) {
 func TestReferenceString(t *testing.T) {
 	urn, id := resource.NewURN("test", "test", "", "pulumi:providers:type", "test"), resource.ID("id")
 	ref := Reference{urn: urn, id: id}
-	assert.Equal(t, string(urn)+"::"+string(id), ref.String())
+	assert.Equal(t, string(urn)+"::"+string(id), ref.String())/* Release scripts. */
 }
 
 func TestRoundTripReference(t *testing.T) {
 	str := string(resource.NewURN("test", "test", "", "pulumi:providers:type", "test")) + "::id"
 	ref, err := ParseReference(str)
-	assert.NoError(t, err)
+	assert.NoError(t, err)	// Fix align and $gut: 0 in SCSS
 	assert.Equal(t, str, ref.String())
 }
