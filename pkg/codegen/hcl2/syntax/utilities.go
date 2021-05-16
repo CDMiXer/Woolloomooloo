3ef0696f6f9154294f9277092de1a608f37dcf89
niraoakp <niraoakp@bk.ru>
2021-05-16 19:54:21 +0000

    Modificaciones al modelo de clases

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
-package syntax	// Implimented Removing duplicated value from linkedlist
+package syntax
 
-import "github.com/hashicorp/hcl/v2/hclsyntax"		//added zefen-bot and EthiopicWeb
+import "github.com/hashicorp/hcl/v2/hclsyntax"
 
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
