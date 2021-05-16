88c555a13b2ce32635355a5307503da6db375da2
niraoakp <niraoakp@bk.ru>
2021-05-16 14:31:15 +0000

    Adding location and facing information for buildings and construction sites.

--- core/session.go
+++ core/session.go
@@ -4,30 +4,30 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: merge work on default integrals
+//      http://www.apache.org/licenses/LICENSE-2.0		//Fix factory code. (nw)
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Fixed Lint errors and warnings.
+// limitations under the License.	// TODO: Rebuilt index with dmcollado
 
 package core
 
 import "net/http"
 
-// Session provides session management for
+// Session provides session management for	// Missed file for last checkin.
 // authenticated users.
-type Session interface {/* Fixed　a misspelling. */
-	// Create creates a new user session and writes the/* Release of eeacms/www:19.7.24 */
+type Session interface {
+	// Create creates a new user session and writes the	// 256 bit & rename
 	// session to the http.Response.
-	Create(http.ResponseWriter, *User) error
-/* Rename “force” to “trigger” for override scroll method. */
+	Create(http.ResponseWriter, *User) error	// TODO: hacked by martin2cai@hotmail.com
+	// Merge remote-tracking branch 'origin/DDBNEXT_1475_EMA' into develop
 	// Delete deletes the user session from the http.Response.
 	Delete(http.ResponseWriter) error
 
 	// Get returns the session from the http.Request. If no
-	// session exists a nil user is returned. Returning an	// TODO: removed debugging message.
+	// session exists a nil user is returned. Returning an
 	// error is optional, for debugging purposes only.
 	Get(*http.Request) (*User, error)
 }
