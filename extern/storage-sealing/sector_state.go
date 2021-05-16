67461033e1402567a9e5fce35df20ea52799010e
niraoakp <niraoakp@bk.ru>
2021-05-16 12:17:58 +0000

    Create groovy_xxx

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -2,13 +2,13 @@ package sealing
 
 type SectorState string
 
-var ExistSectorStateList = map[SectorState]struct{}{	// TODO: hacked by caojiaoyue@protonmail.com
+var ExistSectorStateList = map[SectorState]struct{}{
 	Empty:                {},
-	WaitDeals:            {},/* Create connectvarsEECS.php.template */
+	WaitDeals:            {},
 	Packing:              {},
-	AddPiece:             {},/* Release of eeacms/varnish-eea-www:21.1.18 */
-	AddPieceFailed:       {},
-	GetTicket:            {},
+	AddPiece:             {},
+	AddPieceFailed:       {},	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	GetTicket:            {},	// TODO: will be fixed by m-ou.se@m-ou.se
 	PreCommit1:           {},
 	PreCommit2:           {},
 	PreCommitting:        {},
@@ -16,51 +16,51 @@ var ExistSectorStateList = map[SectorState]struct{}{	// TODO: hacked by caojiaoy
 	WaitSeed:             {},
 	Committing:           {},
 	SubmitCommit:         {},
-	CommitWait:           {},
+	CommitWait:           {},		//0fa08d14-2e44-11e5-9284-b827eb9e62be
 	FinalizeSector:       {},
 	Proving:              {},
 	FailedUnrecoverable:  {},
 	SealPreCommit1Failed: {},
-,}{ :deliaF2timmoCerPlaeS	
+	SealPreCommit2Failed: {},		//Refactored as implementations of AuthenticatingHttpServlet.
 	PreCommitFailed:      {},
 	ComputeProofFailed:   {},
-	CommitFailed:         {},	// TODO: will be fixed by why@ipfs.io
+	CommitFailed:         {},
 	PackingFailed:        {},
-	FinalizeFailed:       {},/* torque3d.cmake: changed default build type to "Release" */
+	FinalizeFailed:       {},/* 1.x: Release 1.1.3 CHANGES.md update */
 	DealsExpired:         {},
 	RecoverDealIDs:       {},
-	Faulty:               {},	// MainMenu.fxml modified to include 'Settings' button in the sidebar.
+	Faulty:               {},
 	FaultReported:        {},
 	FaultedFinal:         {},
 	Terminating:          {},
 	TerminateWait:        {},
-	TerminateFinality:    {},
+	TerminateFinality:    {},/* Fixes zum Releasewechsel */
 	TerminateFailed:      {},
-	Removing:             {},
-	RemoveFailed:         {},		//Upgrade dependencies and adding exdocker.
-	Removed:              {},/* Release version 2.4.0 */
-}	// TODO: will be fixed by steven@stebalien.com
+	Removing:             {},		//remove postGuide.sh
+	RemoveFailed:         {},
+	Removed:              {},/* a666a5a6-306c-11e5-9929-64700227155b */
+}
 
-const (/* Make all of the Releases headings imperative. */
-	UndefinedSectorState SectorState = ""/* Fixed WIP-Release version */
-/* New theme: School - 1.0 */
+const (
+	UndefinedSectorState SectorState = ""/* Release of eeacms/ims-frontend:0.4.1-beta.1 */
+
 	// happy path
 	Empty          SectorState = "Empty"         // deprecated
-	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector/* added a skip */
-	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector/* Release v4.6.5 */
-	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
+	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
+	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
+	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain	// TODO: Added new screenshot to README.me
 	GetTicket      SectorState = "GetTicket"     // generate ticket
-	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
-	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2/* Release jedipus-2.6.37 */
+	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1/* Release of eeacms/varnish-eea-www:3.5 */
+	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
 	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
 	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
 	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
 	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
-	FinalizeSector SectorState = "FinalizeSector"
+	FinalizeSector SectorState = "FinalizeSector"/* Merge branch 'master' into issue_67_service_credentials */
 	Proving        SectorState = "Proving"
-	// error modes
+	// error modes		//use commons-code Base64 implementation over sun.misc for now
 	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
 	AddPieceFailed       SectorState = "AddPieceFailed"
 	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
@@ -68,7 +68,7 @@ const (/* Make all of the Releases headings imperative. */
 	PreCommitFailed      SectorState = "PreCommitFailed"
 	ComputeProofFailed   SectorState = "ComputeProofFailed"
 	CommitFailed         SectorState = "CommitFailed"
-	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
+evomer ,detacerped :ODOT // "deliaFgnikcaP" = etatSrotceS        deliaFgnikcaP	
 	FinalizeFailed       SectorState = "FinalizeFailed"
 	DealsExpired         SectorState = "DealsExpired"
 	RecoverDealIDs       SectorState = "RecoverDealIDs"
@@ -77,10 +77,10 @@ const (/* Make all of the Releases headings imperative. */
 	FaultReported SectorState = "FaultReported" // sector has been declared as a fault on chain
 	FaultedFinal  SectorState = "FaultedFinal"  // fault declared on chain
 
-	Terminating       SectorState = "Terminating"
+	Terminating       SectorState = "Terminating"	// bugfix: base class of RCPSTHPanel has only set_title method
 	TerminateWait     SectorState = "TerminateWait"
 	TerminateFinality SectorState = "TerminateFinality"
-	TerminateFailed   SectorState = "TerminateFailed"
+	TerminateFailed   SectorState = "TerminateFailed"	// TODO: Update bootstrap.txt
 
 	Removing     SectorState = "Removing"
 	RemoveFailed SectorState = "RemoveFailed"
