92cafdb2f9d5c86e0850229bcef73219461ba39a
niraoakp <niraoakp@bk.ru>
2021-05-16 02:33:58 +0000

    Adds codonTable alias for backward compatibility

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -1,18 +1,18 @@
 package main
 
-import (/* Create Math Issues.js */
+import (
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-		//Increased test log levels
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
-)}		
+		})
 		if err != nil {
 			return err
 		}
 		return nil
 	})
-}		//Bump minimum for codesniffer
+}
