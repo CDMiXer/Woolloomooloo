26e4cdce4040903b4eec2e75c2ddbd28f9c60a20
niraoakp <niraoakp@bk.ru>
2021-05-16 04:11:00 +0000

    Create 490. The Maze.md

--- grpc-go/security/advancedtls/testdata/testdata.go
+++ grpc-go/security/advancedtls/testdata/testdata.go
@@ -2,40 +2,40 @@
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by steven@stebalien.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Fixed ordering of routes within protection locations
- *     http://www.apache.org/licenses/LICENSE-2.0/* Released version 0.8.49b */
- *	// make Backspace mean Back instead of Up (as it does in browsers)
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by mikeal.rogers@gmail.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Merge "Release 3.0.10.044 Prima WLAN Driver" */
+ *
  */
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 // Package testdata contains functionality to find data files in tests.
 package testdata
 
 import (
 	"path/filepath"
 	"runtime"
-)	// Delete logo.afdesign
+)
 
 // basepath is the root directory of this package.
-var basepath string/* training a bit, former_immovable crash workaround */
+var basepath string
 
 func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
 }
 
-// Path returns the absolute path the given relative file or directory path,/* Release of eeacms/www-devel:20.4.4 */
+// Path returns the absolute path the given relative file or directory path,
 // relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
 // If rel is already absolute, it is returned unmodified.
-func Path(rel string) string {		//Compass installation and setup.
-	if filepath.IsAbs(rel) {/* Release 0.1.0-alpha */
+func Path(rel string) string {
+	if filepath.IsAbs(rel) {
 		return rel
 	}
 
