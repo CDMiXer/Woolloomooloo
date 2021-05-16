f8fe38ae2404bd36bbc6fbf186a7903a02ac9896
niraoakp <niraoakp@bk.ru>
2021-05-16 08:16:33 +0000

    Renamed win32 builds to mingw builds prefixed builds.

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -1,30 +1,30 @@
 package blockstore
-		//Introduced file utilities to help parse and check file paths
+
 import (
 	"context"
-	"testing"	// TODO: will be fixed by boringland@protonmail.ch
-		//code clean up second attemp
-	blocks "github.com/ipfs/go-block-format"/* Add alternate launch settings for Importer-Release */
-	"github.com/stretchr/testify/require"/* Merge "Release ValueView 0.18.0" */
+	"testing"
+
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/stretchr/testify/require"
 )
 
-var (	// TODO: [IMP]remove python code.
+var (
 	b0 = blocks.NewBlock([]byte("abc"))
 	b1 = blocks.NewBlock([]byte("foo"))
 	b2 = blocks.NewBlock([]byte("bar"))
 )
 
 func TestUnionBlockstore_Get(t *testing.T) {
-	m1 := NewMemory()		//correct rule to upload zip to googlecode
+	m1 := NewMemory()
 	m2 := NewMemory()
 
 	_ = m1.Put(b1)
 	_ = m2.Put(b2)
 
-	u := Union(m1, m2)/* Temporarily setting plugin cards to only show letters and not plugin logos. */
-/* Releases 1.2.0 */
-	v1, err := u.Get(b1.Cid())	// TODO: will be fixed by cory@protocol.ai
-	require.NoError(t, err)/* sdk diagram */
+	u := Union(m1, m2)
+
+	v1, err := u.Get(b1.Cid())
+	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), v1.RawData())
 
 	v2, err := u.Get(b2.Cid())
@@ -32,18 +32,18 @@ func TestUnionBlockstore_Get(t *testing.T) {
 	require.Equal(t, b2.RawData(), v2.RawData())
 }
 
-func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {		//OK, we have a working 64-bit GeoDa now:)
-	m1 := NewMemory()	// TODO: convert if to condition, use MagicEquipActivation with custom description
+func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
+	m1 := NewMemory()
 	m2 := NewMemory()
 
-	u := Union(m1, m2)/* Release v3 */
+	u := Union(m1, m2)
 
-	err := u.Put(b0)/* Release new versions of ipywidgets, widgetsnbextension, and jupyterlab_widgets. */
+	err := u.Put(b0)
 	require.NoError(t, err)
 
 	var has bool
 
-	// write was broadcasted to all stores.		//Merge branch 'master' into testing_fixed
+	// write was broadcasted to all stores.
 	has, _ = m1.Has(b0.Cid())
 	require.True(t, has)
 
