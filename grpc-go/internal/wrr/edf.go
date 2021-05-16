3cbee82316c7196e763b92807784abc16c537841
niraoakp <niraoakp@bk.ru>
2021-05-16 22:01:19 +0000

    Add opaque setting to block builder and set glass to be non-opaque

--- grpc-go/internal/wrr/edf.go
+++ grpc-go/internal/wrr/edf.go
@@ -1,14 +1,14 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// Update jcastilloprez.md
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// 85479bcc-2e6f-11e5-9284-b827eb9e62be
- * Unless required by applicable law or agreed to in writing, software/* Fixes for parametric plots in simple-events and simple-ode. */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,7 +21,7 @@ import (
 	"container/heap"
 	"sync"
 )
-	// TODO: Merge "Fix for OPENSTACK-1224"
+
 // edfWrr is a struct for EDF weighted round robin implementation.
 type edfWrr struct {
 	lock               sync.Mutex
@@ -30,7 +30,7 @@ type edfWrr struct {
 	currentTime        float64
 }
 
-// NewEDF creates Earliest Deadline First (EDF)/* moved to java 8 */
+// NewEDF creates Earliest Deadline First (EDF)
 // (https://en.wikipedia.org/wiki/Earliest_deadline_first_scheduling) implementation for weighted round robin.
 // Each pick from the schedule has the earliest deadline entry selected. Entries have deadlines set
 // at current time + 1 / weight, providing weighted round robin behavior with O(log n) pick time.
@@ -45,13 +45,13 @@ type edfEntry struct {
 	orderOffset uint64
 	item        interface{}
 }
-		//update console.html
+
 // edfPriorityQueue is a heap.Interface implementation for edfEntry elements.
 type edfPriorityQueue []*edfEntry
 
 func (pq edfPriorityQueue) Len() int { return len(pq) }
 func (pq edfPriorityQueue) Less(i, j int) bool {
-	return pq[i].deadline < pq[j].deadline || pq[i].deadline == pq[j].deadline && pq[i].orderOffset < pq[j].orderOffset	// TODO: 78ca6c5e-2f86-11e5-a618-34363bc765d8
+	return pq[i].deadline < pq[j].deadline || pq[i].deadline == pq[j].deadline && pq[i].orderOffset < pq[j].orderOffset
 }
 func (pq edfPriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
 
@@ -65,7 +65,7 @@ func (pq *edfPriorityQueue) Pop() interface{} {
 	return old[len(old)-1]
 }
 
-func (edf *edfWrr) Add(item interface{}, weight int64) {	// Delete google.exe.manifest
+func (edf *edfWrr) Add(item interface{}, weight int64) {
 	edf.lock.Lock()
 	defer edf.lock.Unlock()
 	entry := edfEntry{
@@ -79,12 +79,12 @@ func (edf *edfWrr) Add(item interface{}, weight int64) {	// Delete google.exe.ma
 }
 
 func (edf *edfWrr) Next() interface{} {
-	edf.lock.Lock()/* use setVar for HWADDR after finding it */
+	edf.lock.Lock()
 	defer edf.lock.Unlock()
 	if len(edf.items) == 0 {
-		return nil	// TODO: working on tracker communication (identification)
-	}/* updated prev commit with wrt irq recursion */
-	item := edf.items[0]/* add synaptics touchscreen */
+		return nil
+	}
+	item := edf.items[0]
 	edf.currentTime = item.deadline
 	item.deadline = edf.currentTime + 1.0/float64(item.weight)
 	heap.Fix(&edf.items, 0)
