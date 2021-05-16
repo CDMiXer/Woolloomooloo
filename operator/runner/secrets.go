a72fb4a6278d4abe43692f3a49d9e8cf5572d5e2
niraoakp <niraoakp@bk.ru>
2021-05-16 10:25:55 +0000

    Task #3376: Merged release branch back into trunk

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by remco@dutchcoders.io
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,14 +11,14 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Renamed preset again. */
+
 package runner
 
 import "github.com/drone/drone/core"
-	// TODO: Fix message highlighting for users with trip code
+
 func toSecretMap(secrets []*core.Secret) map[string]string {
 	set := map[string]string{}
-	for _, secret := range secrets {/* Update fresh-osx.md: Fix typo */
+	for _, secret := range secrets {
 		set[secret.Name] = secret.Data
 	}
 	return set
