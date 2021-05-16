ea7c354d1c728168c4e3653dbbbc4a4a373082c9
niraoakp <niraoakp@bk.ru>
2021-05-16 07:01:35 +0000

    Fix space leak in segmented folds

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* New Release (1.9.27) */
 import { Resource } from "./resource";
 
 // Step 2: Create resources during `pulumi query` -- should error.
