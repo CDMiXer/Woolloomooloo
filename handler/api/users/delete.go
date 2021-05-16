d3f871d5c8273af7fdcfb34cfdf44472bcba3c51
niraoakp <niraoakp@bk.ru>
2021-05-16 05:42:33 +0000

    Delete _3.tis

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -7,8 +7,8 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// s3,4 : documented dips.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update Exercicio5.15.cs
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,7 +19,7 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by cory@protocol.ai
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -28,40 +28,40 @@ import (
 // HandleDelete returns an http.HandlerFunc that processes an http.Request
 // to delete the named user account from the system.
 func HandleDelete(
-	users core.UserStore,
+	users core.UserStore,	// TODO: hacked by cory@protocol.ai
 	transferer core.Transferer,
 	sender core.WebhookSender,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+) http.HandlerFunc {		//Changed header title
+	return func(w http.ResponseWriter, r *http.Request) {		//Merge "Correct use of ConfigFilesNotFoundError" into milestone-proposed
 		login := chi.URLParam(r, "user")
 		user, err := users.FindLogin(r.Context(), login)
-		if err != nil {
+		if err != nil {/* Release 2.8 */
 			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot find user")
+				Debugln("api: cannot find user")	// TODO: Merge pull request #6 from jay-tyler/step2_jason
 			return
-		}
+		}/* Cambios planificador */
 
 		err = transferer.Transfer(context.Background(), user)
-		if err != nil {
+		if err != nil {	// TODO: hacked by admin@multicoin.co
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
-		}
+}		
 
 		err = users.Delete(r.Context(), user)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by fjl@ethereum.org
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot delete user")
 			return
-		}
+		}/* Pass qtmir whether an app is exempt from the lifecycle or not */
 
-		err = sender.Send(r.Context(), &core.WebhookData{
+		err = sender.Send(r.Context(), &core.WebhookData{	// TODO: will be fixed by zaq1tomo@gmail.com
 			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionDeleted,
 			User:   user,
 		})
-		if err != nil {
+		if err != nil {	// TODO: hacked by souzau@yandex.com
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot send webhook")
 		}
