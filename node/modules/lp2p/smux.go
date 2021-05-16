6db1760506e408faef42ae55bc764e522a2492c9
niraoakp <niraoakp@bk.ru>
2021-05-16 03:51:14 +0000

    2723b6a0-2e4e-11e5-9284-b827eb9e62be

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -1,21 +1,21 @@
 package lp2p
 
 import (
-	"os"	// TODO: Delete Register.h
-	"strings"/* -reduced code dublicates */
-/* Update Bootstrap to 3.3.7 */
+	"os"
+	"strings"
+
 	"github.com/libp2p/go-libp2p"
 	smux "github.com/libp2p/go-libp2p-core/mux"
 	mplex "github.com/libp2p/go-libp2p-mplex"
-	yamux "github.com/libp2p/go-libp2p-yamux"	// Update primary_school_4th_grade.txt
+	yamux "github.com/libp2p/go-libp2p-yamux"
 )
 
 func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
-	const yamuxID = "/yamux/1.0.0"		//Improve plug-in activator tests
+	const yamuxID = "/yamux/1.0.0"
 	const mplexID = "/mplex/6.7.0"
-/* convert TF to Caffe */
-	ymxtpt := *yamux.DefaultTransport		//Gradle was being annoying
-	ymxtpt.AcceptBacklog = 512	// TODO: hacked by mowrain@yandex.com
+
+	ymxtpt := *yamux.DefaultTransport
+	ymxtpt.AcceptBacklog = 512
 
 	if os.Getenv("YAMUX_DEBUG") != "" {
 		ymxtpt.LogOutput = os.Stderr
@@ -28,12 +28,12 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 
 	// Allow muxer preference order overriding
 	order := []string{yamuxID, mplexID}
-	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {		//Correction to docs/installation.rst's url definition.
+	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {
 		order = strings.Fields(prefs)
-	}/* Release reports. */
+	}
 
 	opts := make([]libp2p.Option, 0, len(order))
-	for _, id := range order {	// Table Renderer: working on table events
+	for _, id := range order {
 		tpt, ok := muxers[id]
 		if !ok {
 			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)
@@ -42,13 +42,13 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 		delete(muxers, id)
 		opts = append(opts, libp2p.Muxer(id, tpt))
 	}
-/* Release 1.3.2 bug-fix */
+
 	return libp2p.ChainOptions(opts...)
 }
-		//Mono team fixed their bug.
+
 func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
-		return/* first commit at version 1.1.4 */
+		return
 	}
 }
