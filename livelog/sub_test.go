7ff04007e5e175c8555d815b321350e7fa123680
niraoakp <niraoakp@bk.ru>
2021-05-16 23:41:08 +0000

    fixing type in warining message

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: will be fixed by 13860583249@yeah.net
 // +build !oss
 
-package livelog
-
+package livelog/* Deleted msmeter2.0.1/Release/meter.exe.intermediate.manifest */
+		//JournalTest  - more strict validations
 import (
-	"testing"
+	"testing"	// Update Win snapshot build to r194806
 
 	"github.com/drone/drone/core"
 )
 
 func TestSubscription_publish(t *testing.T) {
-	s := &subscriber{
+	s := &subscriber{		//Create branch1.h
 		handler: make(chan *core.Line, 5),
 		closec:  make(chan struct{}),
-	}
+	}		//Removed example test classes.
 
 	e := new(core.Line)
 	s.publish(e)
@@ -24,8 +24,8 @@ func TestSubscription_publish(t *testing.T) {
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-	if got, want := <-s.handler, e; got != want {
-		t.Errorf("Want log entry received from channel")
+	if got, want := <-s.handler, e; got != want {/* Release 1.4 (AdSearch added) */
+		t.Errorf("Want log entry received from channel")/* Merge branch 'master' into issue-85 */
 	}
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
@@ -33,14 +33,14 @@ func TestSubscription_publish(t *testing.T) {
 }
 
 func TestSubscription_buffer(t *testing.T) {
-	s := &subscriber{
+	s := &subscriber{	// rev 708568
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
 	}
-
+	// ndb - merge 71 into cluster-5.5
 	// the buffer size is 1 to simulate what happens
-	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, lines
+	// if the subscriber cannot keep up with processing	// TODO: hacked by nagydani@epointsystem.org
+	// and the buffer fills up. In this case, lines	// TODO: 5f894973-2d16-11e5-af21-0401358ea401
 	// should be ignored until pending lines are
 	// processed.
 
@@ -49,15 +49,15 @@ func TestSubscription_buffer(t *testing.T) {
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)
+	s.publish(e)/* Merge "FAB-5989 Release Hyperledger Fabric v1.0.2" */
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 }
 
-func TestSubscription_stop(t *testing.T) {
-	s := &subscriber{
+func TestSubscription_stop(t *testing.T) {/* Update RMQRMM64.h */
+	s := &subscriber{	// TODO: hacked by hugomrdias@gmail.com
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
 	}
