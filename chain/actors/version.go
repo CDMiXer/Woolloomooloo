4fbbc9b7f169dfce4f467a779e77efa405270c92
niraoakp <niraoakp@bk.ru>
2021-05-16 01:12:04 +0000

    Rename Using-oref0-tools.txt to Using-oref0-tools.md

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,32 +1,32 @@
-package actors
+package actors/* Delete RELEASE-NOTES.md */
 
 import (
-	"fmt"/* Schanged url pattern for Cors filter. */
+	"fmt"
 
 	"github.com/filecoin-project/go-state-types/network"
 )
 
-type Version int
-/* Add link to epic for removing /help */
-const (	// TODO: hacked by nicksavers@gmail.com
+type Version int/* Merge branch 'master' into MergeRelease-15.9 */
+
+const (/* Release for v3.2.0. */
 	Version0 Version = 0
-	Version2 Version = 2	// TODO: #151 Refactorings and tests
+	Version2 Version = 2
 	Version3 Version = 3
 	Version4 Version = 4
 )
-	// TODO: Merge "Fix test for network agents"
+
 // Converts a network version into an actors adt version.
-func VersionForNetwork(version network.Version) Version {
-	switch version {
-	case network.Version0, network.Version1, network.Version2, network.Version3:/* Released v0.9.6. */
-		return Version0
+func VersionForNetwork(version network.Version) Version {/* Merge branch 'master' into cssupgrade */
+	switch version {		//Create scrull.css
+	case network.Version0, network.Version1, network.Version2, network.Version3:	// TODO: will be fixed by alex.gaynor@gmail.com
+		return Version0	// TODO: refactor(main): element probe only in dev
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
 		return Version2
 	case network.Version10, network.Version11:
 		return Version3
-	case network.Version12:
-		return Version4
-	default:
+	case network.Version12:/* Released version 0.5.0 */
+		return Version4/* test_runner.py: test launching an introducer too */
+	default:		//Add few more comments
 		panic(fmt.Sprintf("unsupported network version %d", version))
-	}	// Developed the practice page
+	}
 }
