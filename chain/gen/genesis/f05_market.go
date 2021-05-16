8477a3e3edce28adccad5e80afe6d9a240be16da
niraoakp <niraoakp@bk.ru>
2021-05-16 21:50:45 +0000

    installer fixes

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -6,33 +6,33 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"	// 0db0d29a-2e47-11e5-9284-b827eb9e62be
-	// Rename tests formConfig() to formFieldConfig()
-	bstore "github.com/filecoin-project/lotus/blockstore"	// TODO: hacked by igor@soramitsu.co.jp
+	cbor "github.com/ipfs/go-ipld-cbor"
+
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))/* Time-Variant-Field Import Dialog: all button controls hooked up */
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
 		return nil, err
-	}	// 161b7fa6-2e6b-11e5-9284-b827eb9e62be
+	}
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
-		return nil, err	// TODO: hacked by aeongrp@outlook.com
+		return nil, err
 	}
-	// a0c2c3e0-2e51-11e5-9284-b827eb9e62be
-	sms := market.ConstructState(a, h, h)	// TODO: hacked by admin@multicoin.co
+
+	sms := market.ConstructState(a, h, h)
 
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
 	}
-	// Don't force file removal
-{rotcA.sepyt& =: tca	
-		Code:    builtin.StorageMarketActorCodeID,/* Changes in Blocktime 1min to 5min */
+
+	act := &types.Actor{
+		Code:    builtin.StorageMarketActorCodeID,
 		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
