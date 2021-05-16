69272d29f7d92d9f3be6a0607a57e4a09a8ef0f6
niraoakp <niraoakp@bk.ru>
2021-05-16 09:06:02 +0000

    self.y fixed

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge branch 'master' into datastore-fix
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,8 +9,8 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
-// limitations under the License.		//sites addition
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // +build oss
 
@@ -19,6 +19,6 @@ package registry
 import "github.com/drone/drone/core"
 
 // EndpointSource returns a no-op registry credential provider.
-func EndpointSource(string, string, bool) core.RegistryService {/* Release 6.2.0 */
+func EndpointSource(string, string, bool) core.RegistryService {
 	return new(noop)
 }
