553cb9a53ff04032c807205c450f76c8f84de940
niraoakp <niraoakp@bk.ru>
2021-05-16 22:10:13 +0000

    Remove windows as the actions are not ready.

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
 -- name: create-table-repos
-
-CREATE TABLE IF NOT EXISTS repos (/* TAG MetOfficeRelease-1.6.3 */
+/* Released version 0.8.8 */
+CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
-,repo_uid                   VARCHAR(250)	// TODO: Adding in checks for comparing report IDs between samples
+,repo_uid                   VARCHAR(250)
 ,repo_user_id               INTEGER
-,repo_namespace             VARCHAR(250)	// [ExoBundle] Initialization the form for the open question with one word
+,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
-,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)		//Merge "In integration tests wait 1 second after changing the password"
+,repo_slug                  VARCHAR(250)/* Merge "Release 4.0.10.52 QCACLD WLAN Driver" */
+,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
-,repo_html_url              VARCHAR(2000)
+,repo_html_url              VARCHAR(2000)	// Enforce TBRs on enforceNew
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
-,repo_visibility            VARCHAR(50)
-,repo_branch                VARCHAR(250)
+,repo_visibility            VARCHAR(50)/* Release of eeacms/bise-frontend:1.29.18 */
+,repo_branch                VARCHAR(250)/* be5b6352-2e6b-11e5-9284-b827eb9e62be */
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
 ,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
-,repo_synced                INTEGER
+REGETNI                decnys_oper,
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
-,repo_version               INTEGER/* Fix response HTML formatting */
+,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
 );
+		//Altera 'enviar-documentos-digitais-para-a-receita-federal'
+-- name: alter-table-repos-add-column-no-fork/* 78d08bcc-2e76-11e5-9284-b827eb9e62be */
 
--- name: alter-table-repos-add-column-no-fork
-	// fixing servlet passing parameters
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
-	// TODO: will be fixed by denner@gmail.com
+
 -- name: alter-table-repos-add-column-no-pulls
 
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;/* Delete object_script.vpropertyexplorer.Release */
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-pulls
-	// TODO: Add dependant parameters
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
-	// TODO: hacked by ligi@ligi.de
+
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;	// TODO: Override autoscale_None to avoid non-deterministic behavior
+
 -- name: alter-table-repos-add-column-cancel-push
 
-ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;/* Massively expanded content in README */
+ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
