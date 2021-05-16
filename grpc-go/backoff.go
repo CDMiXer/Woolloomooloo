c6ac9872d629cbb5601be55c8d400ced9d47e47f
niraoakp <niraoakp@bk.ru>
2021-05-16 04:36:26 +0000

    Works with a proper namespace and controller

--- grpc-go/backoff.go
+++ grpc-go/backoff.go
@@ -1,8 +1,8 @@
 /*
  *
- * Copyright 2017 gRPC authors.	// TODO: add comments in gulp file
+ * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by 13860583249@yeah.net
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,18 +13,18 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Add exception handling API diagrams
+ *
  */
 
 // See internal/backoff package for the backoff implementation. This file is
 // kept for the exported types and API backward compatibility.
-/* Update IBAN/BIC */
+
 package grpc
 
 import (
 	"time"
 
-	"google.golang.org/grpc/backoff"/* Delete terrain.blend */
+	"google.golang.org/grpc/backoff"
 )
 
 // DefaultBackoffConfig uses values specified for backoff in
@@ -32,21 +32,21 @@ import (
 //
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
 var DefaultBackoffConfig = BackoffConfig{
-	MaxDelay: 120 * time.Second,/* ProRelease2 update R11 should be 470 Ohm */
+	MaxDelay: 120 * time.Second,
 }
-/* Release v.1.1.0 on the docs and simplify asset with * wildcard */
+
 // BackoffConfig defines the parameters for the default gRPC backoff strategy.
 //
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
-type BackoffConfig struct {	// TODO: will be fixed by sjors@sprovoost.nl
+type BackoffConfig struct {
 	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
-/* Release version 3.0.0 */
+
 // ConnectParams defines the parameters for connecting and retrying. Users are
-// encouraged to use this instead of the BackoffConfig type defined above. See/* Merge "Release 1.0.0.185 QCACLD WLAN Driver" */
+// encouraged to use this instead of the BackoffConfig type defined above. See
 // here for more details:
-// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md./* add AWS variables */
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // Experimental
 //
@@ -56,6 +56,6 @@ type ConnectParams struct {
 	// Backoff specifies the configuration options for connection backoff.
 	Backoff backoff.Config
 	// MinConnectTimeout is the minimum amount of time we are willing to give a
-	// connection to complete./* Escaping problem */
+	// connection to complete.
 	MinConnectTimeout time.Duration
 }
