3df9ff2c1614d914bb5406616afc6e9c3c4fc9d4
niraoakp <niraoakp@bk.ru>
2021-05-16 05:53:26 +0000

    Switched chai to the expect-interface

--- grpc-go/examples/helloworld/greeter_client/main.go
+++ grpc-go/examples/helloworld/greeter_client/main.go
@@ -1,36 +1,36 @@
 /*
- */* Changing Release in Navbar Bottom to v0.6.5. */
+ *
  * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Use ViewHolder pattern on ListView.
+ * You may obtain a copy of the License at
+* 
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Merge "ASoC: wcd_cpe_core: Mark functions as static"
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//expand and collapse icon for princing page
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Added PostalAddress */
- */
+ *
+ */		//Fixed ClassCastExceptions
 
-// Package main implements a client for Greeter service.
+// Package main implements a client for Greeter service.	// clean up netif_dialog and make it actually return a value
 package main
 
-import (		//Delete index-formonly.html
+import (
 	"context"
 	"log"
 	"os"
-	"time"/* Update datacenters.md */
-/* Started tidying up fitness functions */
-	"google.golang.org/grpc"/* fix font of release notes, highlight with red color */
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)
+	"time"
 
-const (	// TODO: bundle-size: 23485bf351346743b526c74622004d77e7215860 (85.68KB)
-	address     = "localhost:50051"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)		//Add cm-def
+
+const (		//got rid of duplicates
+	address     = "localhost:50051"	// TODO: will be fixed by mail@overlisted.net
 	defaultName = "world"
 )
 
@@ -38,7 +38,7 @@ func main() {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)	// TODO: add search model
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
@@ -49,10 +49,10 @@ func main() {
 		name = os.Args[1]
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel()		//Fix detection of window events.
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
+	log.Printf("Greeting: %s", r.GetMessage())	// TODO: 6f12467a-2e67-11e5-9284-b827eb9e62be
 }
