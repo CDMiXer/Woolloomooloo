e69a807d9b5982547b682cfda2542a8098b5808c
niraoakp <niraoakp@bk.ru>
2021-05-16 22:06:34 +0000

    use redis to cache the requests

--- tests/integration/double_pending_delete/step1/index.ts
+++ tests/integration/double_pending_delete/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by timnugent@gmail.com
-// You may obtain a copy of the License at/* MS Release 4.7.8 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -14,10 +14,10 @@
 
 import { Resource } from "./resource";
 
-// Setup: Resources A and B are created successfully./* Attempt to fix travis - install protoc before build */
+// Setup: Resources A and B are created successfully.
 const a = new Resource("a", { fail: 0 });
-const b = new Resource("b", { fail: 0 }, { dependsOn: a });/* fe5b0b3e-2e65-11e5-9284-b827eb9e62be */
+const b = new Resource("b", { fail: 0 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
-//  B: Created/* Implemented ReleaseIdentifier interface. */
-	// TODO: hacked by yuvalalaluf@gmail.com
+//  B: Created
+
