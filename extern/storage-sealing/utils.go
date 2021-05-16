ba14ff9822e723fed0e8a4a50e0faf359c445140
niraoakp <niraoakp@bk.ru>
2021-05-16 08:53:00 +0000

    Outline basic usage in README

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -2,49 +2,49 @@ package sealing
 
 import (
 	"math/bits"
-	// TODO: will be fixed by witek@enjin.io
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
-	//
-	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B/* doc (pom): comment and clear up pom structure */
+//	
+	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
-	//	// TODO: Modified some documentation and also some of the tests in the ideal module.
-	// Given that we can get sector size by simply adding 1/127 of the user
-	// bytes
 	//
-	// (we convert to sector bytes as they are nice round binary numbers)	// TODO: hacked by steven@stebalien.com
-/* Release 0.0.12 */
-	toFill := uint64(in + (in / 127))/* Updated Releases (markdown) */
+	// Given that we can get sector size by simply adding 1/127 of the user
+	// bytes	// Create 671. Second Minimum Node In a Binary Tree
+	//
+	// (we convert to sector bytes as they are nice round binary numbers)/* modifica package da configuration a properties */
+/* Start adding loading code from a file */
+	toFill := uint64(in + (in / 127))
 
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
-rebmun taht snaem osla tI .rotces eht llif ot deen ew sezis eceip eht lla //	
-	// of pieces is the number of 1s in the number of remaining bytes to fill/* Update globalize.d.ts */
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))/* Merge branch 'master' into pyup-update-flake8-print-3.1.0-to-3.1.1 */
+	// all the piece sizes we need to fill the sector. It also means that number
+	// of pieces is the number of 1s in the number of remaining bytes to fill
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))	// TODO: will be fixed by fkautz@pseudocode.cc
 	for i := range out {
-		// Extract the next lowest non-zero bit		//Merge branch 'master' into feature/remove-modding-discussions-user-colour
+		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(toFill)
-		psize := uint64(1) << next/* Release 1.0.0.1 */
+		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the	// TODO: hacked by sebastian.tharakan97@gmail.com
-		// next bit
+		// set that bit to 0 by XORing it, so the next iteration looks at the	// TODO: Create tutorial_visualize_matrix_plnkr.md
+		// next bit	// TODO: hacked by mail@bitpshr.net
 		toFill ^= psize
-
+/* Update file WebObjCaption-model.pdf */
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}/* Move schema files to a separate module and a better package. */
+	}
 	return out, nil
-}/* updated/added apis and created APIs sample project */
-/* fix config macro name for timer client. */
+}	// Added some debug to at least get some info of the situation.
+
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
-	if err := m.sectors.List(&sectors); err != nil {		//d30d7774-2e4b-11e5-9284-b827eb9e62be
+	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
 	}
 	return sectors, nil
