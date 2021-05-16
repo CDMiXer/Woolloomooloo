8589d524682ea14fe1108eca397a5929db538b94
niraoakp <niraoakp@bk.ru>
2021-05-16 09:20:57 +0000

    Add travis configurations

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Add controller generator spec
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Clarify name of label
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
