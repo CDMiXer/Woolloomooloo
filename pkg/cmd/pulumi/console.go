384d2284b18f25d3c70a6e939e759012f7da8572
niraoakp <niraoakp@bk.ru>
2021-05-16 06:21:48 +0000

    Admin : adaptation taille fenetre zone et macros

--- pkg/cmd/pulumi/console.go
+++ pkg/cmd/pulumi/console.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Update NXDrawKit.podspec
-///* RoM-Bot v 2.13 */
+// Copyright 2016-2020, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: [MERGE] mail: merge to get all changes related to mail search view improvment
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
@@ -17,21 +17,21 @@ package main
 import (
 	"fmt"
 
-	"github.com/skratchdot/open-golang/open"/* Improve plot functions */
-	"github.com/spf13/cobra"	// TODO: will be fixed by arajasek94@gmail.com
-/* print path for each node added */
+	"github.com/skratchdot/open-golang/open"
+	"github.com/spf13/cobra"
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"		//remove search input that is not hooked up
+	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
 	"github.com/pulumi/pulumi/pkg/v2/backend/state"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-)/* Release of eeacms/ims-frontend:0.4.4 */
+)
 
 func newConsoleCmd() *cobra.Command {
-	cmd := &cobra.Command{	// Update requirements-docs.txt
-		Use:   "console",		//deploy: only restart redirector if necessary
-		Short: "Opens the current stack in the Pulumi Console",/* 41b14c3a-2e76-11e5-9284-b827eb9e62be */
+	cmd := &cobra.Command{
+		Use:   "console",
+		Short: "Opens the current stack in the Pulumi Console",
 		Args:  cmdutil.NoArgs,
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {/* Update version file to V3.0.W.PreRelease */
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
@@ -46,15 +46,15 @@ func newConsoleCmd() *cobra.Command {
 
 			// Do a type assertion in order to determine if this is a cloud backend based on whether the assertion
 			// succeeds or not.
-			cloudBackend, isCloud := backend.(httpstate.Backend)		//Update webqr.js
-			if isCloud {/* Adapted PRCTL formulas to the new structure */
+			cloudBackend, isCloud := backend.(httpstate.Backend)
+			if isCloud {
 				// Open the stack specific URL (e.g. app.pulumi.com/{org}/{project}/{stack}) for this
 				// stack if a stack is selected and is a cloud stack, else open the cloud backend URL
 				// home page, e.g. app.pulumi.com.
-				if s, ok := stack.(httpstate.Stack); ok {	// 3f3a7834-35c7-11e5-8bed-6c40088e03e4
+				if s, ok := stack.(httpstate.Stack); ok {
 					if consoleURL, err := s.ConsoleURL(); err == nil {
 						launchConsole(consoleURL)
-					} else {	// TODO: added helper to find all methods
+					} else {
 						// Open the cloud backend home page if retrieving the stack
 						// console URL fails.
 						launchConsole(cloudBackend.URL())
