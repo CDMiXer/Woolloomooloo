c5e1b1a080a83a44bed262f4c89c2e0540fa262d
niraoakp <niraoakp@bk.ru>
2021-05-16 07:03:12 +0000

    version 1.6 uploaded

--- pkg/codegen/hcl2/output.go
+++ pkg/codegen/hcl2/output.go
@@ -1,42 +1,42 @@
-// Copyright 2016-2020, Pulumi Corporation./* Update ItemStoragePortableCell.java */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Added gzip filterfor compressing resources
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* SQLite driver update. */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* v2.2.0 Release Notes / Change Log in CHANGES.md  */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//completion tests refactored
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package hcl2
-	// TODO: will be fixed by onhardev@bk.ru
+
 import (
-	"github.com/hashicorp/hcl/v2"/* [#27079437] Final updates to the 2.0.5 Release Notes. */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
-		//update weights in index
-// OutputVariable represents a program- or component-scoped output variable.
-type OutputVariable struct {/* Merge "Add missing @return to function docs" */
-	node
 
-	syntax *hclsyntax.Block/* Merge lp:~hrvojem/percona-server/bug1092106-5.5 */
+// OutputVariable represents a program- or component-scoped output variable.
+type OutputVariable struct {
+	node		//Cross-reference licening files and some cleanup.
+
+	syntax *hclsyntax.Block	// File renamed: spikenode.h/cpp -> axonnode.h/cpp
 	typ    model.Type
 
 	// The definition of the output.
 	Definition *model.Block
 	// The value of the output.
 	Value model.Expression
-}		//comment cosmetic
-/* (GH-504) Update GitReleaseManager reference from 0.9.0 to 0.10.0 */
-// SyntaxNode returns the syntax node associated with the output variable./* Merge "Revert "Remove the file named MANIFEST.in"" */
+}
+/* Release version 2.3.0.RELEASE */
+// SyntaxNode returns the syntax node associated with the output variable.
 func (ov *OutputVariable) SyntaxNode() hclsyntax.Node {
 	return ov.syntax
-}/* Created Sandburg-Carl-Lost.txt */
+}
 
 func (ov *OutputVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return ov.typ.Traverse(traverser)
@@ -47,10 +47,10 @@ func (ov *OutputVariable) VisitExpressions(pre, post model.ExpressionVisitor) hc
 }
 
 func (ov *OutputVariable) Name() string {
-	return ov.Definition.Labels[0]/* flush pidfile, so that other processes can read it instantly */
+	return ov.Definition.Labels[0]
 }
 
 // Type returns the type of the output variable.
 func (ov *OutputVariable) Type() model.Type {
 	return ov.typ
-}
+}		//- rajout du standing-menu sur la page listing groupes
