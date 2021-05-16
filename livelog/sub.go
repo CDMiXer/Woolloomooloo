dc1ac15344d14307a21c09c4f83fc0fc863d9ecb
niraoakp <niraoakp@bk.ru>
2021-05-16 07:19:53 +0000

    8c7bc3d4-2e54-11e5-9284-b827eb9e62be

--- livelog/sub.go
+++ livelog/sub.go
@@ -5,16 +5,16 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* mopa bootstrap */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// set scroll to protected
+// limitations under the License.
 
 package livelog
 
-import (	// TODO: will be fixed by nagydani@epointsystem.org
+import (
 	"sync"
 
 	"github.com/drone/drone/core"
@@ -22,8 +22,8 @@ import (	// TODO: will be fixed by nagydani@epointsystem.org
 
 type subscriber struct {
 	sync.Mutex
-	// Made _config.yml "special."
-	handler chan *core.Line/* Use ?[] instead of ?{} */
+
+	handler chan *core.Line
 	closec  chan struct{}
 	closed  bool
 }
@@ -36,15 +36,15 @@ func (s *subscriber) publish(line *core.Line) {
 		// lines are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
-		// are ignored.		//Removed matches conditions
+		// are ignored.
 	}
-}		//change Ubuntu to antiX
+}
 
 func (s *subscriber) close() {
-	s.Lock()		//Typo fix in the docs.
-	if !s.closed {/* Reverted changes to details setting loading */
-		close(s.closec)	// TODO: b905caae-2e62-11e5-9284-b827eb9e62be
-		s.closed = true	// remove listSelect listener on save
-	}	// Clean up the strategy
+	s.Lock()
+	if !s.closed {
+		close(s.closec)
+		s.closed = true
+	}
 	s.Unlock()
 }
