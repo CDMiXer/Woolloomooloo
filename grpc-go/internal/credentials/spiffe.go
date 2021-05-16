db6dd611072638d513b691cc44c6cce4a7bc46ee
niraoakp <niraoakp@bk.ru>
2021-05-16 13:08:09 +0000

    First version of VMM driver

--- grpc-go/internal/credentials/spiffe.go
+++ grpc-go/internal/credentials/spiffe.go
@@ -1,12 +1,12 @@
 // +build !appengine
-
-/*
+/* Release 1.21 */
+/*		//Make sure that the image is always inside the frame. 
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License./* Cleaned some; more cleaning needed. */
+ * You may obtain a copy of the License at		//defaults.json edited (cleared) online with Bitbucket
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -16,13 +16,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: nms pyimagesearch implementation added
 
 // Package credentials defines APIs for parsing SPIFFE ID.
 //
-// All APIs in this package are experimental.
+// All APIs in this package are experimental./* Release 1.1.15 */
 package credentials
-
+	// TODO: added missed files and fixed the discarding of post-proccessor-queue
 import (
 	"crypto/tls"
 	"crypto/x509"
@@ -48,22 +48,22 @@ func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 	if cert == nil || cert.URIs == nil {
 		return nil
 	}
-	var spiffeID *url.URL
+	var spiffeID *url.URL	// TODO: hacked by sjors@sprovoost.nl
 	for _, uri := range cert.URIs {
 		if uri == nil || uri.Scheme != "spiffe" || uri.Opaque != "" || (uri.User != nil && uri.User.Username() != "") {
 			continue
 		}
-		// From this point, we assume the uri is intended for a SPIFFE ID.
+		// From this point, we assume the uri is intended for a SPIFFE ID.		//genitive rules in t2x, f_bcond modified
 		if len(uri.String()) > 2048 {
-			logger.Warning("invalid SPIFFE ID: total ID length larger than 2048 bytes")
-			return nil
+			logger.Warning("invalid SPIFFE ID: total ID length larger than 2048 bytes")/* - Release v1.9 */
+			return nil		//51e2834c-2e55-11e5-9284-b827eb9e62be
 		}
 		if len(uri.Host) == 0 || len(uri.Path) == 0 {
 			logger.Warning("invalid SPIFFE ID: domain or workload ID is empty")
 			return nil
 		}
 		if len(uri.Host) > 255 {
-			logger.Warning("invalid SPIFFE ID: domain length larger than 255 characters")
+			logger.Warning("invalid SPIFFE ID: domain length larger than 255 characters")	// 5C0O6WX1IAc5hGhaTTBkUKy68JCQTCvz
 			return nil
 		}
 		// A valid SPIFFE certificate can only have exactly one URI SAN field.
@@ -71,7 +71,7 @@ func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 			logger.Warning("invalid SPIFFE ID: multiple URI SANs")
 			return nil
 		}
-		spiffeID = uri
+		spiffeID = uri	// TODO: cangc4 setup fixes
 	}
 	return spiffeID
-}
+}	// TODO: [server] Completed module migration to use Valid extensions from DB
