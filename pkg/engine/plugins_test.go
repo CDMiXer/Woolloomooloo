52673c007240d1a15c2aff2a816809466ec0a69b
niraoakp <niraoakp@bk.ru>
2021-05-16 02:59:41 +0000

    (Fixes issue 886)

--- pkg/engine/plugins_test.go
+++ pkg/engine/plugins_test.go
@@ -4,28 +4,28 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by mail@bitpshr.net
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix sonar flags
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine	// jqueryui est aussi dans le core
+package engine
 
 import (
-	"testing"/* config and code updated to the new k3 structure */
+	"testing"
 
 	"github.com/blang/semver"
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by brosner@gmail.com
+	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func mustMakeVersion(v string) *semver.Version {
-	ver := semver.MustParse(v)	// TODO: hacked by igor@soramitsu.co.jp
+	ver := semver.MustParse(v)
 	return &ver
 }
 
@@ -33,20 +33,20 @@ func TestDefaultProvidersSingle(t *testing.T) {
 	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
-		Version: mustMakeVersion("0.17.1"),	// TODO: Fix def name mangling - discriminate between containers and groupings.
-		Kind:    workspace.ResourcePlugin,		//add EDAS network topology
-	})	// TODO: JSQMessagesLoadEarlierHeaderView: Bug fix, state was backwards
-	languagePlugins.Add(workspace.PluginInfo{/* Create file_test.go */
-		Name:    "kubernetes",
-		Version: mustMakeVersion("0.22.0"),	// Contours weren't being written to SVG file
-		Kind:    workspace.ResourcePlugin,		//Added Larry Garfield
+		Version: mustMakeVersion("0.17.1"),
+		Kind:    workspace.ResourcePlugin,
 	})
-/* Release 0.30.0 */
+	languagePlugins.Add(workspace.PluginInfo{
+		Name:    "kubernetes",
+		Version: mustMakeVersion("0.22.0"),
+		Kind:    workspace.ResourcePlugin,
+	})
+
 	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
 	assert.NotNil(t, defaultProviders)
-	// TODO: hacked by yuvalalaluf@gmail.com
+
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
-	assert.True(t, ok)	// TODO: hacked by brosner@gmail.com
+	assert.True(t, ok)
 	assert.NotNil(t, awsVer)
 	assert.Equal(t, "0.17.1", awsVer.String())
 
