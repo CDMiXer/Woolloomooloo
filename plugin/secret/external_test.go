47016e701b42e04499519d9d0d5c1e487dfb0830
niraoakp <niraoakp@bk.ru>
2021-05-16 00:50:18 +0000

    Update ISERVER_LDAP_BATCH_UPD.scp

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Release of eeacms/jenkins-slave-eea:3.21 */
 
 package secret
