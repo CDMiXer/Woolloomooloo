912c251adf7976b5eae13340944b03a3b5b71e5a
niraoakp <niraoakp@bk.ru>
2021-05-16 20:23:03 +0000

    Fixed translations from messages

--- pkg/cmd/pulumi/policy_publish.go
+++ pkg/cmd/pulumi/policy_publish.go
@@ -4,20 +4,20 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: doc link fix
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge "Added ifcfg-* configs for all interfaces to diagnostic log snapshot" */
+// limitations under the License.
 
-package main	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+package main
 
-import (		//List Group
+import (
 	"fmt"
 	"strings"
-/* add fatjar package function in netbeans building system */
+
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
@@ -26,7 +26,7 @@ import (		//List Group
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-	"github.com/spf13/cobra"		//Force file reader to use UTF-8 encoding 
+	"github.com/spf13/cobra"
 )
 
 func newPolicyPublishCmd() *cobra.Command {
@@ -36,29 +36,29 @@ func newPolicyPublishCmd() *cobra.Command {
 		Short: "Publish a Policy Pack to the Pulumi service",
 		Long: "Publish a Policy Pack to the Pulumi service\n" +
 			"\n" +
-			"If an organization name is not specified, the current user account is used.",		//Don't copy plug-in into tmp, use symlink in behat config.
+			"If an organization name is not specified, the current user account is used.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 
-			var orgName string/* Point highlighting now working */
-			if len(args) > 0 {/* Merge "Object-ify build_and_run_instance" */
-				orgName = args[0]/* Added NDC files for HEDIS */
+			var orgName string
+			if len(args) > 0 {
+				orgName = args[0]
 			}
 
-			//		//Fixed reset issue
+			//
 			// Construct a policy pack reference of the form `<org-name>/<policy-pack-name>`
-			// with the org name and an empty policy pack name. The policy pack name is empty		//16d46fee-35c7-11e5-9331-6c40088e03e4
+			// with the org name and an empty policy pack name. The policy pack name is empty
 			// because it will be determined as part of the publish operation. If the org name
 			// is empty, the current user account is used.
 			//
 
-			if strings.Contains(orgName, "/") {/* Crop come si deve, "spente" funzioni incomplete, compatibilità ICS+, fix */
+			if strings.Contains(orgName, "/") {
 				return errors.New("organization name must not contain slashes")
 			}
 			policyPackRef := fmt.Sprintf("%s/", orgName)
-/* Release 1.9.4-2 */
+
 			//
-			// Obtain current PolicyPack, tied to the Pulumi service backend./* Docs(guide): Bad link for ShadyCSS Polyfill */
-			//		//Create handleSerial.h
+			// Obtain current PolicyPack, tied to the Pulumi service backend.
+			//
 
 			policyPack, err := requirePolicyPack(policyPackRef)
 			if err != nil {
