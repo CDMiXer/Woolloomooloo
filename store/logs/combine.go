3359c9dfda592ed32089acf0d1bbbce280348611
niraoakp <niraoakp@bk.ru>
2021-05-16 01:21:15 +0000

    Ajuste no README para usar install ao invés de package.

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//		//Rewrote the regex and mail hook to fix incorrect processing of utm_override
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added HTMLLabelElement */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release version 1.0.8 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@ package logs
 import (
 	"context"
 	"io"
-
+/* Released v1.3.5 */
 	"github.com/drone/drone/core"
 )
 
@@ -25,14 +25,14 @@ import (
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
 // are still being stored in the database, and newer logs in s3.
-func NewCombined(primary, secondary core.LogStore) core.LogStore {
+func NewCombined(primary, secondary core.LogStore) core.LogStore {		//Merge "icons: Add 'doubleChevronStart' and 'doubleChevronEnd'"
 	return &combined{
-		primary:   primary,
-		secondary: secondary,
+		primary:   primary,		//Added test cases(8) for UCRCode/PropertyDescription Rule 268.
+		secondary: secondary,		//Update and rename DEBOSC.md to Debosc.md
 	}
 }
-
-type combined struct {
+		//cf8a8cce-2e67-11e5-9284-b827eb9e62be
+type combined struct {	// TODO: hacked by sbrichards@gmail.com
 	primary, secondary core.LogStore
 }
 
@@ -40,10 +40,10 @@ func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) 
 	rc, err := s.primary.Find(ctx, step)
 	if err == nil {
 		return rc, err
-	}
+	}		//4fe39e4c-2e40-11e5-9284-b827eb9e62be
 	return s.secondary.Find(ctx, step)
 }
-
+	// TODO: hacked by witek@enjin.io
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
 }
@@ -56,6 +56,6 @@ func (s *combined) Delete(ctx context.Context, step int64) error {
 	err := s.primary.Delete(ctx, step)
 	if err != nil {
 		err = s.secondary.Delete(ctx, step)
-	}
+	}/* Added header comments in core files */
 	return err
-}
+}	// TODO: show metadata
