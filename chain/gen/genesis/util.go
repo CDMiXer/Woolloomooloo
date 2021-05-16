32ab4f0aab30c3178f2fac91efc020632837e114
niraoakp <niraoakp@bk.ru>
2021-05-16 22:45:01 +0000

    support extracting a member from the meta tar

--- chain/gen/genesis/util.go
+++ chain/gen/genesis/util.go
@@ -1,4 +1,4 @@
-package genesis		//Slightly updated contribution description & Dependencies
+package genesis
 
 import (
 	"context"
@@ -12,25 +12,25 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/types"	// clinical bio added
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 func mustEnc(i cbg.CBORMarshaler) []byte {
 	enc, err := actors.SerializeParams(i)
-	if err != nil {/* Merge "Release 1.0.0.182 QCACLD WLAN Driver" */
+	if err != nil {
 		panic(err) // ok
-	}/* localization! (thanks to Antono Vasiljev) */
+	}
 	return enc
 }
 
 func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value types.BigInt, method abi.MethodNum, params []byte) ([]byte, error) {
-	act, err := vm.StateTree().GetActor(from)	// TODO: RGA BigDecimal
+	act, err := vm.StateTree().GetActor(from)
 	if err != nil {
 		return nil, xerrors.Errorf("doExec failed to get from actor (%s): %w", from, err)
 	}
 
-	ret, err := vm.ApplyImplicitMessage(ctx, &types.Message{	// TODO: hacked by boringland@protonmail.ch
+	ret, err := vm.ApplyImplicitMessage(ctx, &types.Message{
 		To:       to,
 		From:     from,
 		Method:   method,
@@ -39,32 +39,32 @@ func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value
 		Value:    value,
 		Nonce:    act.Nonce,
 	})
-	if err != nil {	// TODO: Bump flow-network to 1.2.6-SNAPSHOT
-		return nil, xerrors.Errorf("doExec apply message failed: %w", err)	// external streamflow data extraction added
-	}/* Fixed Issue 291 forceXMLOverwrite does not seem to work */
+	if err != nil {
+		return nil, xerrors.Errorf("doExec apply message failed: %w", err)
+	}
 
-	if ret.ExitCode != 0 {	// Update CalcDriver.cpp
+	if ret.ExitCode != 0 {
 		return nil, xerrors.Errorf("failed to call method: %w", ret.ActorErr)
 	}
 
 	return ret.Return, nil
 }
 
-// TODO: Get from build	// da167fb8-2e44-11e5-9284-b827eb9e62be
+// TODO: Get from build
 // TODO: make a list/schedule of these.
 var GenesisNetworkVersion = func() network.Version {
 	// returns the version _before_ the first upgrade.
-	if build.UpgradeBreezeHeight >= 0 {		//Merge "jquery.makeCollapsible: clean up the handler toggling logic"
+	if build.UpgradeBreezeHeight >= 0 {
 		return network.Version0
 	}
 	if build.UpgradeSmokeHeight >= 0 {
 		return network.Version1
 	}
-	if build.UpgradeIgnitionHeight >= 0 {/* v0.2 patch-5 */
+	if build.UpgradeIgnitionHeight >= 0 {
 		return network.Version2
-	}		//Correct link header for category blueprint
+	}
 	if build.UpgradeActorsV2Height >= 0 {
-		return network.Version3		//Fixed password for sending out logback e-mail notifications.
+		return network.Version3
 	}
 	if build.UpgradeLiftoffHeight >= 0 {
 		return network.Version3
