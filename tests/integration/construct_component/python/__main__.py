dbf0e1569b42a1418619cb6a24248bc930734946
niraoakp <niraoakp@bk.ru>
2021-05-16 05:14:08 +0000

    Updated AmazingResources list with new section for Swift tips & tricks

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from component import Component/* use constant instead of hard coded number. */
+from component import Component
 
-component_a = Component("a", echo=42)	// TODO: will be fixed by witek@enjin.io
+component_a = Component("a", echo=42)
 component_b = Component("b", echo=component_a.echo)
-component_c = Component("c", echo=component_a.childId)/* Release v0.22. */
+component_c = Component("c", echo=component_a.childId)
