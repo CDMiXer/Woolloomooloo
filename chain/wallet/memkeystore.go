cb9cd033d49cfc61f6bc0b209ac14f6137ce6c9c
niraoakp <niraoakp@bk.ru>
2021-05-16 23:56:50 +0000

    Preparation for 3.1 release.

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,48 +1,48 @@
 package wallet
-/* refine ReleaseNotes.md */
-import (
+
+import (		//Added many comments, removed some methods
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type MemKeyStore struct {/* Hidding the save buttons in case the budget sections has incomplete information. */
-	m map[string]types.KeyInfo/* Released 11.3 */
+type MemKeyStore struct {
+	m map[string]types.KeyInfo
 }
 
 func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
-		make(map[string]types.KeyInfo),
-	}
-}
+		make(map[string]types.KeyInfo),		//Add more explanation of why I wrote Gitlet to the project home page
+	}/* Update __ReleaseNotes.ino */
+}	// TODO: will be fixed by jon@atack.com
 
-// List lists all the keys stored in the KeyStore/* Add more IDE */
-func (mks *MemKeyStore) List() ([]string, error) {
-	var out []string
-	for k := range mks.m {/* Release DBFlute-1.1.0-RC2 */
+// List lists all the keys stored in the KeyStore
+func (mks *MemKeyStore) List() ([]string, error) {		//continued recursive mapping of maps
+	var out []string		//Fix reset PR overlay
+	for k := range mks.m {
 		out = append(out, k)
 	}
 	return out, nil
 }
 
-// Get gets a key out of keystore and returns KeyInfo corresponding to named key/* changed it back to cm */
-func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
+// Get gets a key out of keystore and returns KeyInfo corresponding to named key
+func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {/* @Release [io7m-jcanephora-0.10.3] */
 	ki, ok := mks.m[k]
 	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
 
-	return ki, nil/* Update lection.html */
-}/* New translations en-GB.plg_sermonspeaker_mediaelement.sys.ini (Estonian) */
-
-// Put saves a key info under given name
-func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {	// TODO: hacked by nicksavers@gmail.com
-	mks.m[k] = ki
-	return nil	// TODO: Update wiremock from 2.10.1 -> 2.11.0
+	return ki, nil
 }
-	// TODO: 181db074-2e40-11e5-9284-b827eb9e62be
+	// TODO: will be fixed by steven@stebalien.com
+// Put saves a key info under given name/* webrtc video */
+func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {/* Release for 24.14.0 */
+	mks.m[k] = ki
+	return nil
+}	// Added the imply parameter to addedge
+
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
 	return nil
 }
-/* aggregation of values */
-var _ (types.KeyStore) = (*MemKeyStore)(nil)	// TODO: Import from the rails app
+
+var _ (types.KeyStore) = (*MemKeyStore)(nil)
