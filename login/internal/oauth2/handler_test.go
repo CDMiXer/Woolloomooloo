89cf738054bd623dca21942304426d87116195c7
niraoakp <niraoakp@bk.ru>
2021-05-16 23:14:00 +0000

    add scmchangelog-maven-plugin

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-		//7890f168-2e64-11e5-9284-b827eb9e62be
+
 package oauth2
