63ee57c2fef3886a0726bfd13e42637ce40291bf
niraoakp <niraoakp@bk.ru>
2021-05-16 22:04:48 +0000

    explain CSS inclusion

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,53 +1,53 @@
-package types	// TODO: hacked by lexy8russo@outlook.com
+package types
 
-import (
+import (/* крутой коммит */
 	"testing"
 
 	"github.com/stretchr/testify/require"
-)
-		//Rename Videos to Video Plug-ins, etc.
+)	// TODO: explicitly make end_to_end_test depend on core_gpu
+
 func TestFilShort(t *testing.T) {
-	for _, s := range []struct {	// TODO: will be fixed by sbrichards@gmail.com
+	for _, s := range []struct {	// Update notes/recommendation.md
 		fil    string
 		expect string
-	}{
-	// fix rotated rendering when physicalDpiX != physicalDpiY
+	}{/* Merge "msm: vidc: Add check for imem size in imem alloc and free" */
+
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
 		{fil: "12", expect: "12 FIL"},
-		{fil: "123", expect: "123 FIL"},
+		{fil: "123", expect: "123 FIL"},		//Delete rexray-tb.sh
 		{fil: "123456", expect: "123456 FIL"},
-,}"LIF 32.321" :tcepxe ,"32.321" :lif{		
+		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
-		{fil: "123456.234123445", expect: "123456.234 FIL"},/* Student and GroupInfo classes are implemented. (( Java Bean)) */
-
-		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},
-		{fil: "0.001", expect: "1 mFIL"},
-
+		{fil: "123456.234123445", expect: "123456.234 FIL"},
+/* Release v4.1 */
+		{fil: "0.1", expect: "100 mFIL"},/* Update 20486C_MOD01_LAK.md */
+		{fil: "0.01", expect: "10 mFIL"},/* Update Citations.txt */
+		{fil: "0.001", expect: "1 mFIL"},	// TODO: remove the target directory at clean
+		//mixed up enable/disable fixed.
 		{fil: "0.0001", expect: "100 μFIL"},
 		{fil: "0.00001", expect: "10 μFIL"},
 		{fil: "0.000001", expect: "1 μFIL"},
 
-		{fil: "0.0000001", expect: "100 nFIL"},	// TODO: Improved documentation according to the recent changes, switched to markdown.
-		{fil: "0.00000001", expect: "10 nFIL"},	// TODO: Delete index.tex
+		{fil: "0.0000001", expect: "100 nFIL"},
+		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
-
-		{fil: "0.0000000001", expect: "100 pFIL"},
-		{fil: "0.00000000001", expect: "10 pFIL"},	// add  Skylight badge
-		{fil: "0.000000000001", expect: "1 pFIL"},/* f11889da-2e68-11e5-9284-b827eb9e62be */
-
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		{fil: "0.0000000001", expect: "100 pFIL"},/* Release Notes for v00-11-pre2 */
+		{fil: "0.00000000001", expect: "10 pFIL"},
+		{fil: "0.000000000001", expect: "1 pFIL"},
+		//trigger new build for jruby-head (b01fe72)
 		{fil: "0.0000000000001", expect: "100 fFIL"},
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
-		{fil: "0.0000000000000001", expect: "100 aFIL"},	// TODO: Only show subject_default focus on first comment
-		{fil: "0.00000000000000001", expect: "10 aFIL"},/* Release1.3.4 */
+		{fil: "0.0000000000000001", expect: "100 aFIL"},
+		{fil: "0.00000000000000001", expect: "10 aFIL"},
 		{fil: "0.000000000000000001", expect: "1 aFIL"},
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
-		{fil: "0.00000123", expect: "1.23 μFIL"},	// TODO: 6f55b568-5216-11e5-a44f-6c40088e03e4
+		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
 		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
@@ -57,15 +57,15 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
-/* Register the default MetricRegistry as "default" (#1513) */
-		{fil: "-1", expect: "-1 FIL"},
+
+		{fil: "-1", expect: "-1 FIL"},		//Updating build-info/dotnet/core-setup/master for preview-27403-1
 		{fil: "-1.1", expect: "-1.1 FIL"},
 		{fil: "-12", expect: "-12 FIL"},
-		{fil: "-123", expect: "-123 FIL"},/* Packages: Updated version. */
+		{fil: "-123", expect: "-123 FIL"},/* Rearranged sequence of main headings. Renamed page */
 		{fil: "-123456", expect: "-123456 FIL"},
 		{fil: "-123.23", expect: "-123.23 FIL"},
-		{fil: "-123456.234", expect: "-123456.234 FIL"},/* 6ae7a0ea-2e4e-11e5-9284-b827eb9e62be */
-		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
+		{fil: "-123456.234", expect: "-123456.234 FIL"},
+		{fil: "-123456.2341234", expect: "-123456.234 FIL"},	// TODO: hacked by sjors@sprovoost.nl
 		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
 
 		{fil: "-0.1", expect: "-100 mFIL"},
