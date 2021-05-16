3f1c222836b700bf9e869ecd040e538d90ef90b9
niraoakp <niraoakp@bk.ru>
2021-05-16 23:49:42 +0000

    + Moved Sharp3D back in Codeplex repository...

--- pkg/resource/graph/resource_set.go
+++ pkg/resource/graph/resource_set.go
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Support reading gzipped sbml files. */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release 1.9.20 */
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Release of eeacms/www-devel:19.8.29 */
+//     http://www.apache.org/licenses/LICENSE-2.0/* added (moved) rtp attribs used in SDP */
+///* Add Release page link. */
+// Unless required by applicable law or agreed to in writing, software/* Update 20.3. LiveReload.md */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package graph
-
+/* Added Release Notes link to README.md */
 import "github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-
+	// TODO: Delete Spark-ReadWrite.cpp
 // ResourceSet represents a set of Resources.
-type ResourceSet map[*resource.State]bool
+type ResourceSet map[*resource.State]bool	// TODO: will be fixed by fjl@ethereum.org
 
 // Intersect returns a new set that is the intersection of the two given resource sets.
 func (s ResourceSet) Intersect(other ResourceSet) ResourceSet {
 	newSet := make(ResourceSet)
 	for key := range s {
-		if other[key] {
+		if other[key] {/* Release 1008 - 1008 bug fixes */
 			newSet[key] = true
-		}
-	}
-
+		}		//fixed handling of duplicate filenames for images export
+	}/* LIB: Fix for missing entries in Release vers of subdir.mk  */
+/* Create 78.txt */
 	return newSet
 }
