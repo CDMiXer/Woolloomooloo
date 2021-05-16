221dd42ed5d4a87c948be24d321e9c7167eb3b0e
niraoakp <niraoakp@bk.ru>
2021-05-16 10:02:23 +0000

    Change the text "Resource Page" to be "Resource"

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 9018249c-2e4f-11e5-9284-b827eb9e62be */
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// Major update.
 
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
-// Dependent depends on Base with state 99.
+// Dependent depends on Base with state 99.	// fixed the wtf formatting
 const b = new Resource("dependent", { state: a.state });
