5e385fdb1b66ec48abb18e4e6c0af17e43947ed9
niraoakp <niraoakp@bk.ru>
2021-05-16 02:27:09 +0000

    Change request mapping for session controller

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,18 +1,18 @@
 package main
-
-import (/* growing_buffer: add method Release() */
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"	// TODO: 2a1321a6-2e5e-11e5-9284-b827eb9e62be
+	// TODO: Added constant EXPECTED_TOKENS_LENGTH for readability.
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {		//pt-dialog: changed CV to CV-Box for Wiki references
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
 			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),
 		})
 		if err != nil {
 			return err
-		}	// Updated forest to use species biomass method.
+		}
 		return nil
-)}	
-}
+	})
+}	// TODO: will be fixed by sebastian.tharakan97@gmail.com
