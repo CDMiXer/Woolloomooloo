9f1b34f49760477aa23821b4bec278e8cd4038ed
niraoakp <niraoakp@bk.ru>
2021-05-16 18:17:59 +0000

    NXP-14388: Fix the unbind menu update

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//update rows in chunks spec to also test TSQL syntax
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//Merge "Put the HTML attribute whitelist closer to HTML5"
+import { Resource } from "./resource";
 
-// The changing of a.state causes base to be DBR replaced. This in turn
+// The changing of a.state causes base to be DBR replaced. This in turn	// Create greetingcardsres.php
 // causes the deletion of b and e eagerly. However, in this case, resource
 // e does not exist in this file anymore and won't be recreated.
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent-2/* Release Notes for v01-13 */
+//   1. DeleteReplacement Dependent-2
 //   2. DeleteReplacement Dependent
-//   3. DeleteReplacement Base		//Render latest version SVG from Clojars
+//   3. DeleteReplacement Base
 //   4. Replace Base
 //   5. CreateReplacement Base
-const a = new Resource("base", { uniqueKey: 1, state: 100 });
+const a = new Resource("base", { uniqueKey: 1, state: 100 });		//Add support for clang 3.2 and newer gcc compilers for Imageworks builds
 
 //   6. Replace Dependent
-//   7. CreateReplacement Dependent/* feat(measure): New mg/cm2 Weight Per Area measures for Multiplex */
+//   7. CreateReplacement Dependent
 const b = new Resource("dependent", { state: a.state });
 
 //   Done. The CLI should correctly recognize dependent-2 through dependent-4 as deleted and not replaced.
