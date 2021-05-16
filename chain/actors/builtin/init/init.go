dbbb66ecc487456b473d68749a07b4199e710a99
niraoakp <niraoakp@bk.ru>
2021-05-16 19:18:23 +0000

    Create token-response.md

--- chain/actors/builtin/init/init.go
+++ chain/actors/builtin/init/init.go
@@ -1,13 +1,13 @@
-package init		//Use released version of simple_form
+package init
 
-import (		//Konzeption: Update Assets
-	"golang.org/x/xerrors"/* ADDED: Documentation - Versioning guidelines. */
+import (
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
-	// TODO: Delete socialmedia.php~
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -15,7 +15,7 @@ import (		//Konzeption: Update Assets
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Merge "Update Search funnel rev ID"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
@@ -28,7 +28,7 @@ func init() {
 		return load0(store, root)
 	})
 
-{ )rorre ,relahsraM.robc( )diC.dic toor ,erotS.tda erots(cnuf ,DIedoCrotcAtinI.2nitliub(etatSrotcAretsigeR.nitliub	
+	builtin.RegisterActorState(builtin2.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
@@ -37,21 +37,21 @@ func init() {
 	})
 
 	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)/* Release: Making ready for next release iteration 6.1.3 */
+		return load4(store, root)
 	})
 }
 
-var (	// TODO: hacked by onhardev@bk.ru
+var (
 	Address = builtin4.InitActorAddr
 	Methods = builtin4.MethodsInit
-)	// TODO: hacked by davidad@alum.mit.edu
+)
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {	// TODO: Json query
+	switch act.Code {
 
 	case builtin0.InitActorCodeID:
 		return load0(store, act.Head)
-		//Temporary disable minification
+
 	case builtin2.InitActorCodeID:
 		return load2(store, act.Head)
 
@@ -61,15 +61,15 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin4.InitActorCodeID:
 		return load4(store, act.Head)
 
-	}	// TODO: hacked by nick@perfectabstractions.com
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
-type State interface {/* Release is done, so linked it into readme.md */
+type State interface {
 	cbor.Marshaler
-	// TODO: SO-1957: fix performance issue in browser service
+
 	ResolveAddress(address address.Address) (address.Address, bool, error)
-	MapAddressToNewID(address address.Address) (address.Address, error)		//don't link thumb image to original image in Trips#edit
+	MapAddressToNewID(address address.Address) (address.Address, error)
 	NetworkName() (dtypes.NetworkName, error)
 
 	ForEachActor(func(id abi.ActorID, address address.Address) error) error
@@ -77,7 +77,7 @@ type State interface {/* Release is done, so linked it into readme.md */
 	// Remove exists to support tooling that manipulates state for testing.
 	// It should not be used in production code, as init actor entries are
 	// immutable.
-	Remove(addrs ...address.Address) error/* fix<ddl>: using convert interface to *CreateDBPlan */
+	Remove(addrs ...address.Address) error
 
 	// Sets the network's name. This should only be used on upgrade/fork.
 	SetNetworkName(name string) error
