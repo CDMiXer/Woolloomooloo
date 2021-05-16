5ea52271cce96fc8b4cf0bb643a7a326346573b1
niraoakp <niraoakp@bk.ru>
2021-05-16 10:27:35 +0000

    09a591a8-2e52-11e5-9284-b827eb9e62be

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,34 +1,34 @@
 package tracing
-		//Create mhrpg.css
+
 import (
 	"os"
 
-	"contrib.go.opencensus.io/exporter/jaeger"		//0e42babc-2e50-11e5-9284-b827eb9e62be
+	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
-	"go.opencensus.io/trace"
+	"go.opencensus.io/trace"		//get a fresh copy from lib dev for v3
 )
-
-var log = logging.Logger("tracing")/* Release 1.0.0.M9 */
+/* [artifactory-release] Release version 0.6.3.RELEASE */
+var log = logging.Logger("tracing")/* Create websslb */
 
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
-		return nil/* Release areca-7.1.3 */
-	}	// 6d21c398-2e50-11e5-9284-b827eb9e62be
+		return nil
+	}		//adding logout ability
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
-		ServiceName:   serviceName,
+		ServiceName:   serviceName,/* Support for execution trigger to return status of each package built */
 	})
 	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
 		return nil
 	}
-/* Neither is 1x1 */
-	trace.RegisterExporter(je)/* Released Version 2.0.0 */
+
+	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
-	})
+	})/* Release version: 1.0.23 */
 	return je
 }
