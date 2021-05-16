df5214b0041b4632aea36ca3b5fba50109d71b20
niraoakp <niraoakp@bk.ru>
2021-05-16 10:36:14 +0000

    4adff05c-2f86-11e5-a1b5-34363bc765d8

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-;"ecruoser/." morf } ecruoseR { tropmi
-	// TODO: global:ASSET_URL global:ASSET_URI global:THEME_ASSET_URL global:THEME_ASSET_URI
-// Next, just unprotect the resource:		//fetch latest.jpg from beecam
+import { Resource } from "./resource";
+
+// Next, just unprotect the resource:
 let a = new Resource("eternal", { state: 2 }, { protect: false });
