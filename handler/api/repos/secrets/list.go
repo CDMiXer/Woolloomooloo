97236becff184960551825c5bbb6611e85bbbd36
niraoakp <niraoakp@bk.ru>
2021-05-16 10:29:18 +0000

    [PDI-4325] repaired byte-to-string conversion

--- handler/api/repos/secrets/list.go
+++ handler/api/repos/secrets/list.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: 1705180c-2e6e-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: Update builddata.txt
+
 package secrets
-	// TODO: #56 - Save during sync
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-		//eccad8ea-2e42-11e5-9284-b827eb9e62be
-	"github.com/go-chi/chi"/* Fix backup replication age calculation */
+
+	"github.com/go-chi/chi"
 )
-/* Release Notes 3.5 */
+
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-	secrets core.SecretStore,		//chore(deps): update zrrrzzt/tfk-api-postnummer:latest docker digest to f729ac0
+	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)/* 8c9aef22-2e4d-11e5-9284-b827eb9e62be */
-		if err != nil {	// Add preliminary README
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {
 			render.NotFound(w, err)
-			return	// TODO: hacked by remco@dutchcoders.io
-		}/* ctest -C Release */
+			return
+		}
 		list, err := secrets.List(r.Context(), repo.ID)
 		if err != nil {
 			render.NotFound(w, err)
@@ -41,7 +41,7 @@ func HandleList(
 		secrets := []*core.Secret{}
 		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
-}		
+		}
 		render.JSON(w, secrets, 200)
 	}
 }
