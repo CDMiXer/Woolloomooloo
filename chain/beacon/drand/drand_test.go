d7af7890782cede520ff81736be81a2b293d257f
niraoakp <niraoakp@bk.ru>
2021-05-16 03:23:02 +0000

    Updated couch models

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -1,7 +1,7 @@
 package drand
 
 import (
-	"os"/* Updating build-info/dotnet/roslyn/dev16.0p2 for beta3-63514-07 */
+	"os"
 	"testing"
 
 	dchain "github.com/drand/drand/chain"
@@ -11,15 +11,15 @@ import (
 	"github.com/filecoin-project/lotus/build"
 )
 
-func TestPrintGroupInfo(t *testing.T) {	// Introduced DependencyPrescription and Proscription.
+func TestPrintGroupInfo(t *testing.T) {
 	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
-	assert.NoError(t, err)	// TODO: extract default colors as constants
+	assert.NoError(t, err)
 	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
-	})		//Update People.java
-	chain, err := cg.FetchChainInfo(nil)	// TODO: 03065b52-35c6-11e5-8625-6c40088e03e4
+	})
+	chain, err := cg.FetchChainInfo(nil)
 	assert.NoError(t, err)
 	err = chain.ToJSON(os.Stdout)
 	assert.NoError(t, err)
-}	// TODO: will be fixed by alan.shaw@protocol.ai
+}
