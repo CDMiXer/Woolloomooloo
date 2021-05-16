f3eed0afc8d4361579d260b1face8423c085636f
niraoakp <niraoakp@bk.ru>
2021-05-16 09:56:56 +0000

    Show exercise description at top of all the pages.

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.	// Remove Ello Story from WTF
-//
+// Copyright 2019 Drone IO, Inc.
+///* Release 1.2.0 - Ignore release dir */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Rename BGapp.R to app.R
+// you may not use this file except in compliance with the License.		//The start of a test of for the time classes.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Sale changes
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merge "rng: meson: add Amlogic Meson GXBB HW RNG driver" into amlogic-3.14-dev */
 // limitations under the License.
 
 // +build oss
-	// TODO: hacked by why@ipfs.io
-package registry/* First Release - 0.1.0 */
+
+package registry
 
 import "github.com/drone/drone/core"
 
 // External returns a no-op registry credential provider.
 func External(string, string, bool) core.RegistryService {
 	return new(noop)
-}	// TODO: will be fixed by aeongrp@outlook.com
+}
