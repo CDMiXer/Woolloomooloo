c44eebf7333d74bcdc0f0fc8195c67428cefac0d
niraoakp <niraoakp@bk.ru>
2021-05-16 14:38:36 +0000

    Merge "Make deletion policy work for CLUSTER_DEL_NODES action"

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -7,5 +7,5 @@ type ansiSQLChange string
 
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
 	_, err := session.Exec(string(s))
-	return err
+	return err/* BUG: return element from all operator functions */
 }
