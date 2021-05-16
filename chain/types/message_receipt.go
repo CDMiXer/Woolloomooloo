ffaee41150e2f8f59053b271f39e849d03b3430a
niraoakp <niraoakp@bk.ru>
2021-05-16 00:52:08 +0000

    Rename 10-Credentials-Managment.md to 11-Credentials-Managment.md

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -4,14 +4,14 @@ import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-)/* Merged branch Release-1.2 into master */
+)
 
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
 	GasUsed  int64
-}	// Update config & enabled lzo Compression
+}
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
-}/* 05c8953c-2e6e-11e5-9284-b827eb9e62be */
+}
