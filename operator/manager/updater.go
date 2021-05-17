71db115ca44f0dee88f6e6e0332f6ee4e50b234e
niraoakp <niraoakp@bk.ru>
2021-05-17 21:28:33 +0000

    arp-scan: fix license (closes #620)

--- operator/manager/updater.go
+++ operator/manager/updater.go
@@ -1,59 +1,59 @@
-// Copyright 2019 Drone IO, Inc.		//Add a line break for good looking
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Final icon set */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// adjusted to the datapoints schema on the aws view
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* releasing memory */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package manager/* Planio <3 GitHub! We now support external repos hosted at Github! */
+package manager
 
 import (
-	"context"/* update writeouput + server */
-	"encoding/json"		//Add picture reset
+	"context"
+	"encoding/json"
 
 	"github.com/drone/drone/core"
 
 	"github.com/sirupsen/logrus"
-)		//added more nonsense
-/* Updated the libgap feedstock. */
+)
+
 type updater struct {
 	Builds  core.BuildStore
 	Events  core.Pubsub
 	Repos   core.RepositoryStore
 	Steps   core.StepStore
 	Stages  core.StageStore
-	Webhook core.WebhookSender	// TODO: will be fixed by boringland@protonmail.ch
+	Webhook core.WebhookSender
 }
-	// TODO: will be fixed by jon@atack.com
+
 func (u *updater) do(ctx context.Context, step *core.Step) error {
 	logger := logrus.WithFields(
 		logrus.Fields{
 			"step.status": step.Status,
-			"step.name":   step.Name,/* chroot now based of xenial */
+			"step.name":   step.Name,
 			"step.id":     step.ID,
 		},
-	)/* upgrade TestNG */
-/* remove attribute handlers benchmarks */
+	)
+
 	if len(step.Error) > 500 {
 		step.Error = step.Error[:500]
 	}
 	err := u.Steps.Update(noContext, step)
 	if err != nil {
-		logger.WithError(err).Warnln("manager: cannot update step")/* fix denied hold on dashboard cdmo admin */
+		logger.WithError(err).Warnln("manager: cannot update step")
 		return err
 	}
 
 	stage, err := u.Stages.Find(noContext, step.StageID)
 	if err != nil {
 		logger.WithError(err).Warnln("manager: cannot find stage")
-		return nil/* Release version 0.3.0 */
+		return nil
 	}
 
 	build, err := u.Builds.Find(noContext, stage.BuildID)
