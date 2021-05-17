9027d96ce53f98ccc4122056609c470abcdd363e
niraoakp <niraoakp@bk.ru>
2021-05-17 15:32:58 +0000

    HttpClient updated.

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
 
 import (
-	logging "github.com/ipfs/go-log/v2"		//- additional/changing documentation
+	logging "github.com/ipfs/go-log/v2"/* 0.20.7: Maintenance Release (close #86) */
 
 	"go.uber.org/fx"
-)
+)		//Add Docker main site
 
 type debugPrinter struct {
-	l logging.StandardLogger	// ComboBox: Add setSelectedItem() and changes to options dialog.
+	l logging.StandardLogger
 }
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)
-}/* Released 2.3.0 official */
+	p.l.Debugf(f, a...)	// TODO: Delete landing-page-background2.jpg
+}
 
 var _ fx.Printer = new(debugPrinter)
