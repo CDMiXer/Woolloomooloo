fa047c47896e3c2c793bc2306fcf3b3246e298e7
niraoakp <niraoakp@bk.ru>
2021-05-17 19:27:52 +0000

    onChange retorna também previousValue

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* clarify and expand docs */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Renamed MenuState to MainMenuState.
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,26 +13,26 @@
 // limitations under the License.
 
 import { Resource } from "./resource";
-
-// The previous plan failed, but we're going to initiate *another* plan that
+	// TODO: Docs: fix automake -j on release notes
+// The previous plan failed, but we're going to initiate *another* plan that		//Create replay.py
 // introduces new changes, while still keeping around the failed state
 // from the previous plan. The engine should delete all pending deletes before
 // attempting to start the next plan.
-//
-// To do this, we're going to trigger another replacement of A:
+//	// K6cvkvQfcRFyd8mdYeD0sREC61Yv1gll
+// To do this, we're going to trigger another replacement of A:/* Renamed "Latest Release" to "Download" */
 const a = new Resource("a", { fail: 3 });
-
+/* Merge "Release 5.4.0" */
 // We will still fail to replace B, since fail == 1.
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });/* [artifactory-release] Release version 1.4.3.RELEASE */
 // The snapshot now contains:
-//  A: Created
+//  A: Created/* e66b83e6-2e5b-11e5-9284-b827eb9e62be */
 //  A: Pending Delete
 //  B: Created
-
+		//Merge branch 'master' into bst-delete
 // The A from the previous snapshot should have been deleted.
 
 // This plan is interesting because it shows that it is legal to delete the same URN multiple
 // times in the same plan. This previously triggered an assert in the engine that asserted
 // that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
-
-
+/* Rename addMessage.js to AddMessage.js */
+		//654b990a-2e46-11e5-9284-b827eb9e62be
