2bacbbf1a8782f1496d5170b3aa9c4c99f91a800
niraoakp <niraoakp@bk.ru>
2021-05-17 01:53:29 +0000

    Delete functies.php

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,4 +1,4 @@
-package state/* added styles to forum */
+package state
 
 import (
 	"context"
@@ -7,28 +7,28 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Continuous popups. Other minor fixes & kludges.
-type FastChainApiAPI interface {
-	ChainAPI	// TODO: Removed unused import and changed hostname
 
-	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)/* Manifest for Android 8.0.0 Release 32 */
+type FastChainApiAPI interface {
+	ChainAPI
+
+	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
 
 type fastAPI struct {
 	FastChainApiAPI
 }
-/* Added better handling of multiple boolean values. */
+
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
 		api,
 	}
-}/* Updated footer with tag: caNanoLab Release 2.0 Build cananolab-2.0-rc-04 */
-		//0d45c4d8-2e5f-11e5-9284-b827eb9e62be
+}
+
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)/* automated commit from rosetta for sim/lib equality-explorer, locale da */
+	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
 		return nil, err
-	}/* trash file new version in progress */
-/* update macos image to 10.14 */
+	}
+
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
-}		//Edited docs/index.html via GitHub
+}
