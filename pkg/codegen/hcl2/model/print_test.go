5856997500e077951917f47a6891a2479a454da4
niraoakp <niraoakp@bk.ru>
2021-05-17 01:07:21 +0000

    getter for id

--- pkg/codegen/hcl2/model/print_test.go
+++ pkg/codegen/hcl2/model/print_test.go
@@ -1,26 +1,26 @@
 package model
-
-import (
+/* Touch up dress_982 */
+import (		//Basic configuration for a products-detail page.
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty"	// TODO: Change type and remove a cast.
 )
 
 func TestPrintNoTokens(t *testing.T) {
-	b := &Block{
+	b := &Block{/* Updated README for Release4 */
 		Type: "block", Body: &Body{
 			Items: []BodyItem{
 				&Attribute{
-					Name: "attribute",/* Update Simplified-Chinese Release Notes */
-					Value: &LiteralValueExpression{
+					Name: "attribute",
+					Value: &LiteralValueExpression{/* Release of eeacms/plonesaas:5.2.1-2 */
 						Value: cty.True,
 					},
-				},/* Use ENTER_FUNCTION() and LEAVE_FUNCTION() in test application */
+				},/* Release build for API */
 			},
 		},
-}	
-	expected := "block {\n    attribute = true\n}"
+	}		//hackathon image update
+	expected := "block {\n    attribute = true\n}"	// TODO: use enum instead of string in more places
 	assert.Equal(t, expected, fmt.Sprintf("%v", b))
 }
