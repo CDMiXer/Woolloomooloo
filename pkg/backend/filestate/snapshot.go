4c2fdd7f88d1e0f7b53f29bb1f9ccbbdaf4cf050
niraoakp <niraoakp@bk.ru>
2021-05-17 15:01:55 +0000

    first phase of custom demon/constraints in C#; to be followed by examples

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Merge "msm: mdss: wait for idle when wait for kickoff not available" */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Verificando que value no sea ni array ni objeto en la clase AbstractField */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// ResultLocation: added constructor without room reference.
 // limitations under the License.
 
 package filestate
-
+	// TODO: Create WetterSQL_BME280.py
 import (
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* Merge "wlan: Release 3.2.3.137" */
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
-
+/* needs moar workers */
 // localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
 // to disk on the local machine.
-type localSnapshotPersister struct {
-	name    tokens.QName
+type localSnapshotPersister struct {/* 8b88f782-2e52-11e5-9284-b827eb9e62be */
+	name    tokens.QName	// two images for FEM lighting
 	backend *localBackend
 	sm      secrets.Manager
 }
