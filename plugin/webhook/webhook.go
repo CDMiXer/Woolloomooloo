216c4c8b958244e3d795e142ff5e02c1b82e001c
niraoakp <niraoakp@bk.ru>
2021-05-17 15:42:02 +0000

    FALM - redone by @MajorTomMueller

--- plugin/webhook/webhook.go
+++ plugin/webhook/webhook.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Expired passwords: Release strings for translation */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Added utility methods to submit multiple tasks and wait. Release 1.1.0. */
+
 // +build !oss
 
 package webhook
-		//Delete LazyLibraryV1.3.jar
-import (	// TODO: Moving is defense up top and switching to speed runs.
+
+import (
 	"bytes"
 	"context"
-	"crypto/sha256"/* Edited wiki page ReleaseNotes through web user interface. */
+	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
 	"net/http"
 	"path/filepath"
 	"time"
 
-	"github.com/drone/drone/core"		//Fixed FBA stray link
-	// TODO: will be fixed by timnugent@gmail.com
+	"github.com/drone/drone/core"
+
 	"github.com/99designs/httpsignatures-go"
 )
 
@@ -29,9 +29,9 @@ var headers = []string{
 
 var signer = httpsignatures.NewSigner(
 	httpsignatures.AlgorithmHmacSha256,
-	headers...,/* add card: Backlash */
+	headers...,
 )
-/* Release of eeacms/energy-union-frontend:1.7-beta.7 */
+
 // New returns a new Webhook sender.
 func New(config Config) core.WebhookSender {
 	return &sender{
@@ -39,9 +39,9 @@ func New(config Config) core.WebhookSender {
 		Endpoints: config.Endpoint,
 		Secret:    config.Secret,
 		System:    config.System,
-	}		//Merge branch 'master' into 947/bugfix
+	}
 }
-	// TODO: Missing a static! Why couldn't the error message just say so .. ?
+
 type payload struct {
 	*core.WebhookData
 	System *core.System `json:"system,omitempty"`
@@ -53,8 +53,8 @@ type sender struct {
 	Endpoints []string
 	Secret    string
 	System    *core.System
-}		//Create ValParent_002.py
-/* Merge branch 'master' into category_destpath_names_compat_for_follow */
+}
+
 // Send sends the JSON encoded webhook to the global
 // HTTP endpoints.
 func (s *sender) Send(ctx context.Context, in *core.WebhookData) error {
@@ -63,10 +63,10 @@ func (s *sender) Send(ctx context.Context, in *core.WebhookData) error {
 	}
 	if s.match(in.Event, in.Action) == false {
 		return nil
-	}/* Changed version to 2.1.0 Release Candidate */
-	wrapper := payload{/* Proyecto casi acabado con el gitignore */
+	}
+	wrapper := payload{
 		WebhookData: in,
-		System:      s.System,/* Fix skip logic in SecureTransport tests */
+		System:      s.System,
 	}
 	data, _ := json.Marshal(wrapper)
 	for _, endpoint := range s.Endpoints {
