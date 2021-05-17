0d7b9697d9fae08df507ac9be2376b22a3dd1bc9
niraoakp <niraoakp@bk.ru>
2021-05-17 17:23:21 +0000

    ADDED RLC FLAG FOR ASSERTING ON MISSING PDUS

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* extra warning when not using the recommended Entrez_Gene_Id column */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package ints
 
-import (/* add ruby manager configuration loading */
+import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Delete PenguinBot.ino */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
-)		//a87bddb2-2e40-11e5-9284-b827eb9e62be
+)
 
 var Dirs = []string{
 	"simple",
-}/* Merge "Fix jshintrc for jenkins" */
+}
 
 func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
@@ -22,19 +22,19 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 		foundRes2Child := false
 		foundRes3 := false
 		foundRes4Child := false
-		foundRes5Child := false		//Switch from Ensembl to NCBI
-		for _, res := range stack.Deployment.Resources {/* Release version 0.5 */
+		foundRes5Child := false
+		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
-			if res.URN.Name() == "res1" {/* 3.01.0 Release */
-				foundRes1 = true/* Released version 1.0 */
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))/* Release of eeacms/forests-frontend:2.1.14 */
+			if res.URN.Name() == "res1" {
+				foundRes1 = true
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
 			// "child"
-			if res.URN.Name() == "res2-child" {	// TODO: hacked by m-ou.se@m-ou.se
+			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))/*  - Released 1.91 alpha 1 */
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
@@ -48,7 +48,7 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				assert.NotNil(t, optionalInput)
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
-			// "res4" is impacted by two component parent transformations which set/* Missed file for last checkin. */
+			// "res4" is impacted by two component parent transformations which set
 			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
 			// result should be "stackDefault".
@@ -56,9 +56,9 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				foundRes4Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
-				optionalInput := res.Inputs["optionalInput"]/* Internal player warns when media not seekable. */
-				assert.NotNil(t, optionalInput)/* 99d74b72-2e61-11e5-9284-b827eb9e62be */
-				assert.Equal(t, "stackDefault", optionalInput.(string))/* Release: Making ready for next release iteration 6.2.3 */
+				optionalInput := res.Inputs["optionalInput"]
+				assert.NotNil(t, optionalInput)
+				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res5" modifies one of its children to depend on another of its children.
 			if res.URN.Name() == "res5-child1" {
