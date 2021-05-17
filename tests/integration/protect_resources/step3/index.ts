74828632b4e989b60d1bb9940a65096e075120d9
niraoakp <niraoakp@bk.ru>
2021-05-17 16:47:49 +0000

    warnings cleanup on archived/suspended sites, fixes #12396

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Merge "Wlan: Release 3.8.20.9" */
-import { Resource } from "./resource";		//Update version mentioned in README
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Fixed composer require, also added doctrine versions 2.3 and 2.4. */
 
+import { Resource } from "./resource";
+	// TODO: will be fixed by arachnid@notdot.net
 // Attempt to delete the resource; this will fail!
