451f12963d2dfa02560d080758e2c123abf55cfe
niraoakp <niraoakp@bk.ru>
2021-05-17 20:33:16 +0000

    additional check for player objects in distributeRake

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
-package nullreader		//Actualizacion al 07/12/17
-	// TODO: will be fixed by steven@stebalien.com
+package nullreader
+
 type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
-	}	// TODO: Merge branch 'master' into fix/test
+	}
 	return len(out), nil
 }
