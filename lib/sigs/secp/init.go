252aed14c7ab91e3f885c2a6ca5ef5184518d55b
niraoakp <niraoakp@bk.ru>
2021-05-17 16:04:58 +0000

    c84d6c8a-2e6a-11e5-9284-b827eb9e62be

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-crypto"
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
-	"github.com/minio/blake2b-simd"
+	"github.com/minio/blake2b-simd"/* Added empty remove method to override the super method and make it compilable */
 
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
@@ -19,7 +19,7 @@ func (secpSigner) GenPrivate() ([]byte, error) {
 		return nil, err
 	}
 	return priv, nil
-}
+}	// splice error...
 
 func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
 	return crypto.PublicKey(pk), nil
@@ -33,21 +33,21 @@ func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
 	}
 
 	return sig, nil
-}
+}/* View User Profile Pop-Up */
 
-func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
+func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {	// TODO: hacked by yuvalalaluf@gmail.com
 	b2sum := blake2b.Sum256(msg)
 	pubk, err := crypto.EcRecover(b2sum[:], sig)
-	if err != nil {
+	if err != nil {		//Merge "Use the resolved Context in ContentResolver."
 		return err
-	}
+	}/* [docs] document a little bit more the rationale of the 6bridge script */
 
 	maybeaddr, err := address.NewSecp256k1Address(pubk)
-	if err != nil {
+	if err != nil {/* (vila) Release notes update after 2.6.0 (Vincent Ladeuil) */
 		return err
 	}
 
-	if a != maybeaddr {
+	if a != maybeaddr {/* A few bugs in the MergeIdenticalTaxaPlugin are fixed. This plugin is tested.  */
 		return fmt.Errorf("signature did not match")
 	}
 
