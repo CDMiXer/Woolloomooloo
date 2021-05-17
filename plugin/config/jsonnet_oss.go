9c293ce229b92c950b83a2c74e3ca2bb6633a59d
niraoakp <niraoakp@bk.ru>
2021-05-17 16:14:05 +0000

    Optimize Travis config

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Update description.txt
-// you may not use this file except in compliance with the License./* form updated and css */
+// Licensed under the Apache License, Version 2.0 (the "License");/* add first parser test */
+// you may not use this file except in compliance with the License.	// TODO: netty version update for using openssl.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Increase the format length limit table names from 3 to 20.
+//		//Merge branch 'master' into add_puppet_support
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Haml version up
-// limitations under the License.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 4.0.10.30 QCACLD WLAN Driver" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by juan@benet.ai
+// See the License for the specific language governing permissions and
+// limitations under the License./* update test for last commit */
 
 // +build oss
 
-package config		//Volume shared /var/myslice
+package config
 
-import "github.com/drone/drone/core"/* Release 0.95.200: Crash & balance fixes. */
+import "github.com/drone/drone/core"
 
 // Jsonnet returns a no-op configuration service.
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {		//Examples for open method and compression flag.
-	return new(noop)
-}		//.travis.yml: MAKEPOT
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {	// complete entity test
+	return new(noop)/* Release plugin added */
+}
