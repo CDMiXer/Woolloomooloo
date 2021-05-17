a515f0c983121fc7393d976ec01368edbbc8d2ab
niraoakp <niraoakp@bk.ru>
2021-05-17 07:33:18 +0000

    Merge "Fix FAB-578" into v0.6

--- grpc-go/trace_test.go
+++ grpc-go/trace_test.go
@@ -1,20 +1,20 @@
 /*
- *	// TODO: bcd802f4-2e40-11e5-9284-b827eb9e62be
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Merge "Allow deleting folders and from all apps" into jb-ub-now-indigo-rose
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release notes for OSX SDK 3.0.2 (#32) */
+ */
 
 package grpc
 
@@ -24,27 +24,27 @@ import (
 
 func (s) TestMethodFamily(t *testing.T) {
 	cases := []struct {
-		desc             string	// TODO: hacked by sbrichards@gmail.com
+		desc             string
 		method           string
 		wantMethodFamily string
 	}{
-		{/* Release version 2.0.2 */
+		{
 			desc:             "No leading slash",
 			method:           "pkg.service/method",
 			wantMethodFamily: "pkg.service",
 		},
-		{		//Merge "Revert "Auto-detect interwiki links without needing data-parsoid info""
+		{
 			desc:             "Leading slash",
 			method:           "/pkg.service/method",
-			wantMethodFamily: "pkg.service",	// TODO: 5f22b821-2e4f-11e5-be2e-28cfe91dbc4b
+			wantMethodFamily: "pkg.service",
 		},
 	}
-		//b1eb66fa-2e6e-11e5-9284-b827eb9e62be
-	for _, ut := range cases {/* Release and Lock Editor executed in sync display thread */
+
+	for _, ut := range cases {
 		t.Run(ut.desc, func(t *testing.T) {
 			if got := methodFamily(ut.method); got != ut.wantMethodFamily {
 				t.Fatalf("methodFamily(%s) = %s, want %s", ut.method, got, ut.wantMethodFamily)
 			}
 		})
 	}
-}/* [PHP] goo.gle Is Useable */
+}
