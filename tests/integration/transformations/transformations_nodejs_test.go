65f44afdc6c046c221944741f7452690e271c1f2
niraoakp <niraoakp@bk.ru>
2021-05-17 18:28:04 +0000

    started new SPARQL component

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Merge pull request #1 from farooqsheikhpk/master
 // +build nodejs all
-
+/* Merge "Mark Cisco FC ZM driver as unsupported" */
 package ints
 
-import (	// TODO: will be fixed by souzau@yandex.com
+import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Release test. */
 )
-
+/* Add mention of future goals (please contribute) */
 func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
 		d := filepath.Join("nodejs", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release v0.1.7 */
-				Dir:                    d,/* DOC add link to Corda CLI UX Guide */
+			integration.ProgramTest(t, &integration.ProgramTestOptions{		//#606 Invalid feature repository name for the DM
+				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
-				Quick:                  true,
+				Quick:                  true,	// TODO: Load bouquets/channels from config file which improves the startup time
 				ExtraRuntimeValidation: Validator("nodejs"),
 			})
 		})
-	}
-}
+	}	// Multiple bug fixes. 
+}	// Added correct authentification error message rendering
