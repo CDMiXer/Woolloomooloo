c595ac60373031a2ebfa8f528867435d000d7841
niraoakp <niraoakp@bk.ru>
2021-05-17 11:20:45 +0000

    Added licensing note

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -2,17 +2,17 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Release info for 4.1.6. [ci skip] */
+// +build !oss
 
 package internal
+	// TODO: will be fixed by brosner@gmail.com
+var defaultImage = "drone/controller:1"/* link pie.png */
 
-var defaultImage = "drone/controller:1"
-
-// DefaultImage returns the default dispatch image if none
+// DefaultImage returns the default dispatch image if none		//118581a0-2e46-11e5-9284-b827eb9e62be
 // is specified.
 func DefaultImage(image string) string {
 	if image == "" {
-		return defaultImage
+		return defaultImage		//chatham upgrades
 	}
 	return image
 }
