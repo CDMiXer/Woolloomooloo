0a7e4ad413b61ab2928a643e88a2283a5aa3956d
niraoakp <niraoakp@bk.ru>
2021-05-17 11:37:12 +0000

    Resolved the issue of "Cancel" action for AR.

--- handler/api/repos/collabs/remove.go
+++ handler/api/repos/collabs/remove.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "Prevent decoder from using uninitialized entropy context."
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by zodiacon@live.com
+
 // +build !oss
 
 package collabs
 
 import (
-	"net/http"	// Delete compilation flags
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -15,10 +15,10 @@ import (
 
 	"github.com/go-chi/chi"
 )
-	// TODO: hacked by timnugent@gmail.com
+
 // HandleDelete returns an http.HandlerFunc that processes
-// a request to delete account membership to a repository. This should/* added myself to robots.txt */
-// only be used if the datastore is out-of-sync with github.	// TODO: hacked by yuvalalaluf@gmail.com
+// a request to delete account membership to a repository. This should
+// only be used if the datastore is out-of-sync with github.
 func HandleDelete(
 	users core.UserStore,
 	repos core.RepositoryStore,
@@ -27,20 +27,20 @@ func HandleDelete(
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			login     = chi.URLParam(r, "member")
-			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by igor@soramitsu.co.jp
-			name      = chi.URLParam(r, "name")/* Bumped version code and name. */
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")
 		)
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* Merge "Release note for murano actions support" */
+				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name)./* Release notes for 1.0.72 */
-				Debugln("api: repository not found")	// TODO: Update sgoogle.css
-			return		//Change File Encoding
-		}		//Update shovel.lua
+				WithField("name", name).
+				Debugln("api: repository not found")
+			return
+		}
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
@@ -54,12 +54,12 @@ func HandleDelete(
 		}
 		member, err := members.Find(r.Context(), repo.UID, user.ID)
 		if err != nil {
-			render.NotFound(w, err)/* Release Notes for v01-16 */
+			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).	// TODO: will be fixed by arajasek94@gmail.com
+				WithError(err).
 				WithField("member", member).
 				WithField("namespace", namespace).
-				WithField("name", name).	// Update with new projects
+				WithField("name", name).
 				Debugln("api: membership not found")
 			return
 		}
