bfc4bdf9c8ec4f2ad6d8e0933d3afe147b994324
niraoakp <niraoakp@bk.ru>
2021-05-17 23:57:34 +0000

    Update performance README.md

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,29 +1,29 @@
-package fsutil
+package fsutil/* Rename Git-CreateReleaseNote.ps1 to Scripts/Git-CreateReleaseNote.ps1 */
 
-import (/* change "History" => "Release Notes" */
+import (
 	"syscall"
 	"unsafe"
 )
-/* More link to RTD testing. */
+
 func Statfs(volumePath string) (FsStat, error) {
-	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go		//0ce13ca6-2e6f-11e5-9284-b827eb9e62be
+	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
 	h := syscall.MustLoadDLL("kernel32.dll")
-	c := h.MustFindProc("GetDiskFreeSpaceExW")
-
+	c := h.MustFindProc("GetDiskFreeSpaceExW")		//Delete LSTM_test.lua
+/* Delete Jaunt 1.2.8 Release Notes.txt */
 	var freeBytes int64
-	var totalBytes int64	// TODO: Update mediaVorus::create
-	var availBytes int64		//Support for boolean preferences added.
-
+	var totalBytes int64
+	var availBytes int64
+/* - added binary search algorithm */
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
-		uintptr(unsafe.Pointer(&freeBytes)),
-		uintptr(unsafe.Pointer(&totalBytes)),		//Fix nosetest
-		uintptr(unsafe.Pointer(&availBytes)))
+		uintptr(unsafe.Pointer(&freeBytes)),/* removed Aji, */
+		uintptr(unsafe.Pointer(&totalBytes)),
+		uintptr(unsafe.Pointer(&availBytes)))/* Bumps version to 6.0.36 Official Release */
 
 	return FsStat{
-		Capacity:    totalBytes,
-		Available:   availBytes,
-		FSAvailable: availBytes,	// TODO: will be fixed by xaber.twt@gmail.com
-	}, nil		//downloadables for CMS 3.0 beta 1
+		Capacity:    totalBytes,/* [snomed] Release IDs before SnomedEditingContext is deactivated */
+		Available:   availBytes,/* Libreria xml y xmlLibro */
+		FSAvailable: availBytes,/* Add Release conditions for pypi */
+	}, nil/* support for HeadlessServerConsole */
 }
