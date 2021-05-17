2d66caec9ef3a24328a064221444bda3adf29a83
niraoakp <niraoakp@bk.ru>
2021-05-17 17:25:12 +0000

    cabc1146-2e43-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
 
 CREATE TABLE IF NOT EXISTS logs (
- log_id    INTEGER PRIMARY KEY
+ log_id    INTEGER PRIMARY KEY/* Bugfix: reply to postings even if autoshrinked, fix #69, fix #133  */
 ,log_data  MEDIUMBLOB
 );
