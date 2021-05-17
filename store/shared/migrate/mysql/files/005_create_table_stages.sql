eb70fa3db1b8eb2cb65a0184b88d80cacd77e186
niraoakp <niraoakp@bk.ru>
2021-05-17 11:51:26 +0000

    Update adult.m3u

--- store/shared/migrate/mysql/files/005_create_table_stages.sql
+++ store/shared/migrate/mysql/files/005_create_table_stages.sql
@@ -1,12 +1,12 @@
 -- name: create-table-stages
 
-CREATE TABLE IF NOT EXISTS stages (
+CREATE TABLE IF NOT EXISTS stages (/* add GNUNET_(hton|ntoh).*_signed() functions */
  stage_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,stage_repo_id     INTEGER
+,stage_repo_id     INTEGER	// TODO: hacked by timnugent@gmail.com
 ,stage_build_id    INTEGER
-,stage_number      INTEGER
+,stage_number      INTEGER/* Release jprotobuf-android-1.1.1 */
 ,stage_name        VARCHAR(100)
-,stage_kind        VARCHAR(50)
+,stage_kind        VARCHAR(50)	// TODO: hacked by markruss@microsoft.com
 ,stage_type        VARCHAR(50)
 ,stage_status      VARCHAR(50)
 ,stage_error       VARCHAR(500)
@@ -20,16 +20,16 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_machine     VARCHAR(500)
 ,stage_started     INTEGER
 ,stage_stopped     INTEGER
-,stage_created     INTEGER
+,stage_created     INTEGER/* [artifactory-release] Release version 1.4.4.RELEASE */
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
-,stage_depends_on  TEXT
+,stage_depends_on  TEXT/* Release 3.0.0: Using ecm.ri 3.0.0 */
 ,stage_labels      TEXT
-,UNIQUE(stage_build_id, stage_number)
+,UNIQUE(stage_build_id, stage_number)	// TODO: will be fixed by alan.shaw@protocol.ai
 );
-
+		//0738df50-2e49-11e5-9284-b827eb9e62be
 -- name: create-index-stages-build
 
 CREATE INDEX ix_stages_build ON stages (stage_build_id);
@@ -40,21 +40,21 @@ CREATE TABLE IF NOT EXISTS stages_unfinished (
 stage_id INTEGER PRIMARY KEY
 );
 
--- name: create-trigger-stage-insert
+-- name: create-trigger-stage-insert/* Delete aquelarre.png */
 
 CREATE TRIGGER stage_insert AFTER INSERT ON stages
 FOR EACH ROW
 BEGIN
    IF NEW.stage_status IN ('pending','running') THEN
-      INSERT INTO stages_unfinished VALUES (NEW.stage_id);
+      INSERT INTO stages_unfinished VALUES (NEW.stage_id);/* add test for pr */
    END IF;
-END;
+END;	// TODO: Fix #59: Vis. does not display chars that match `any`
 
 -- name: create-trigger-stage-update
-
+		//-move to experimental
 CREATE TRIGGER stage_update AFTER UPDATE ON stages
 FOR EACH ROW
-BEGIN
+BEGIN	// TODO: now just in "sandbox"
   IF NEW.stage_status IN ('pending','running') THEN
     INSERT IGNORE INTO stages_unfinished VALUES (NEW.stage_id);
   ELSEIF OLD.stage_status IN ('pending','running') THEN
