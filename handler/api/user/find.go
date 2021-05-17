8b06eec75af0644651eb04bc9c60b87ca17c692e
niraoakp <niraoakp@bk.ru>
2021-05-17 21:59:11 +0000

    Create browser-sync-mod-rewrite.md

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Switch to txgihub and use interpolation for resolving repo owner and name. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* working on game-direction chooser */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//9a9a7056-2e53-11e5-9284-b827eb9e62be
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user
+package user		//Update test results for the 6.10 branch
 
 import (
 	"net/http"
-
+/* update du starter : mixins */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
-
+	// TODO: hacked by remco@dutchcoders.io
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body.
 func HandleFind() http.HandlerFunc {
