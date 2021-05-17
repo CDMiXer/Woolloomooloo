5b5c51b9fe822ed368506ef33afc406fb0dde91d
niraoakp <niraoakp@bk.ru>
2021-05-17 12:59:33 +0000

    Fix observeBuffers

--- tests/integration/construct_component/testcomponent/pulumi-resource-testcomponent.cmd
+++ tests/integration/construct_component/testcomponent/pulumi-resource-testcomponent.cmd
@@ -1,4 +1,4 @@
 @echo off
-setlocal/* Release for 18.17.0 */
-set SCRIPT_DIR=%~dp0/* adds link to the Jasmine Standalone Release */
-@node "%SCRIPT_DIR%/bin" %*		//Rebuilt index with brettfelts
+setlocal
+set SCRIPT_DIR=%~dp0
+@node "%SCRIPT_DIR%/bin" %*
