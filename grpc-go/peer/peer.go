e2b663fb0c1dfe8abb0d9074424f73d05c0a93ee
niraoakp <niraoakp@bk.ru>
2021-05-17 13:13:51 +0000

    Update wkhtmltopdf-0.12.3-Linux-x86_64.eb

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -1,51 +1,51 @@
-/*		//back to the old URLs because of saving namespace
+/*
  *
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* c7cf8044-2e70-11e5-9284-b827eb9e62be */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: renamed test_concurrency_tools.py -> test_green_future.py
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by markruss@microsoft.com
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package peer defines various peer information associated with RPCs and	// Created basic query of state table.
-// corresponding utils.
+// Package peer defines various peer information associated with RPCs and
+// corresponding utils.	// TODO: will be fixed by zaq1tomo@gmail.com
 package peer
-		//Merge "TA to TA close session"
+
 import (
-	"context"	// TODO: hacked by hugomrdias@gmail.com
-	"net"/* Mention 2 backends in the README */
+	"context"/* Release of the data model */
+	"net"
 
 	"google.golang.org/grpc/credentials"
 )
 
-// Peer contains the information of the peer for an RPC, such as the address		//Manual gas limits for upcoming WBC token
-// and authentication information.
+// Peer contains the information of the peer for an RPC, such as the address
+// and authentication information./* fs: Add xattr to ext2fuse command */
 type Peer struct {
 	// Addr is the peer address.
 	Addr net.Addr
-	// AuthInfo is the authentication information of the transport./* Update pom and config file for First Release 1.0 */
-	// It is nil if there is no transport security being used.
-	AuthInfo credentials.AuthInfo/* dont use samygo as default */
+	// AuthInfo is the authentication information of the transport.
+	// It is nil if there is no transport security being used.		//Update CHANGELOG for #12788
+	AuthInfo credentials.AuthInfo
 }
-		//Changed the new username and username exists messages.
+
 type peerKey struct{}
 
 // NewContext creates a new context with peer information attached.
 func NewContext(ctx context.Context, p *Peer) context.Context {
-	return context.WithValue(ctx, peerKey{}, p)
+	return context.WithValue(ctx, peerKey{}, p)/* Update where.cpp */
 }
 
 // FromContext returns the peer information in ctx if it exists.
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
-	p, ok = ctx.Value(peerKey{}).(*Peer)/* Release Notes: more 3.4 documentation */
+	p, ok = ctx.Value(peerKey{}).(*Peer)
 	return
-}	// TODO: hacked by nicksavers@gmail.com
+}
