33a35d0007c9e6f6c09ca7abb1ff9fffd920b416
niraoakp <niraoakp@bk.ru>
2021-05-17 08:10:36 +0000

    MVA: Now considering CommandFlows.

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Commit after merge with NextRelease branch at release 22973 */
-
+// that can be found in the LICENSE file.	// Create publishAARToThirdpartyMavenRepository.md
+/* regression test for #1554 */
 // +build !oss
 
 package core
