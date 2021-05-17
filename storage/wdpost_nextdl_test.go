d62ead8f681f50d41bd5a6e2a933ce7bd8f20ac8
niraoakp <niraoakp@bk.ru>
2021-05-17 03:47:51 +0000

    stagebase.py: Clean out some dead set_target_path() code

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,17 +1,17 @@
 package storage
-/* Changed HTTP version dep to HTTP >= 3000.0 && < 3000.1 */
-import (	// TODO: 929f16a6-2e6a-11e5-9284-b827eb9e62be
-	"testing"
-		//use lazy to avoid duplicate utxo warmup
-	"github.com/stretchr/testify/require"
-/* #204 Initial migration of 'tg-date-range-criterion' (DOM part and behavior). */
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* cirrus release: new release created for release/0.1.6 */
-)
 
-func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)	// TODO: Clean up data structure sizes and add Text to ringbuffer.
-	deadlineIdx := 0
+import (
+	"testing"		//remove autoupdate from default to make scripts on the disk load faster
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+)/* Added classpath.txt modification to installation instructions */
+
+func TestNextDeadline(t *testing.T) {/* Release 1.0.3 - Adding Jenkins API client */
+	periodStart := abi.ChainEpoch(0)
+	deadlineIdx := 0	// TODO: hacked by sjors@sprovoost.nl
 	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
@@ -21,16 +21,16 @@ func TestNextDeadline(t *testing.T) {
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
-	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
+	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {	// TODO: Image Configuration now includes server configuration
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
-		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)/* 0.8.5 Release for Custodian (#54) */
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)		//Add some css layout into all pages.
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
-		require.EqualValues(t, expPeriodStart, di.PeriodStart)		//make some things not fall over on local function definitions
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
