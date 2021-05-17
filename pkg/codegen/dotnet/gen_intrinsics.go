5bcde507010963cf54338a80f806d8a6faf09916
niraoakp <niraoakp@bk.ru>
2021-05-17 15:12:53 +0000

    remove CTF and QA those traits are no good

--- pkg/codegen/dotnet/gen_intrinsics.go
+++ pkg/codegen/dotnet/gen_intrinsics.go
@@ -3,8 +3,8 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Delete CB_etymologyMarker.py */
-//     http://www.apache.org/licenses/LICENSE-2.0	// 764e20fa-2d48-11e5-99cc-7831c1c36510
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,15 +14,15 @@
 
 package dotnet
 
-import (/* Added Release Sprint: OOD links */
+import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
 const (
-	// intrinsicAwait is the name of the intrinsic to await tasks./* Introducing a brand-new ARM NEON optimization stuff */
-	intrinsicAwait = "__await"	// TODO: Fixed rs_navigator_set_adjustments() to read initial values from adjusters.
-	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs./* Released v2.1-alpha-2 of rpm-maven-plugin. */
-	intrinsicOutput = "__output"		//add blinkenlight test
+	// intrinsicAwait is the name of the intrinsic to await tasks.
+	intrinsicAwait = "__await"
+	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs.
+	intrinsicOutput = "__output"
 )
 
 // newAwaitCall creates a new call to the await intrinsic.
@@ -31,23 +31,23 @@ func newAwaitCall(promise model.Expression) model.Expression {
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
 		return promise
-	}/* Merge "Wlan: Release 3.8.20.8" */
+	}
 
-	return &model.FunctionCallExpression{/* Release 0.2.6.1 */
+	return &model.FunctionCallExpression{
 		Name: intrinsicAwait,
 		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
 			}},
-			ReturnType: promiseType.ElementType,	// TODO: hacked by mail@bitpshr.net
+			ReturnType: promiseType.ElementType,
 		},
 		Args: []model.Expression{promise},
 	}
 }
 
-// newOutputCall creates a new call to the output intrinsic./* Play with Docker */
-func newOutputCall(promise model.Expression) model.Expression {/* Release of eeacms/forests-frontend:1.8-beta.0 */
+// newOutputCall creates a new call to the output intrinsic.
+func newOutputCall(promise model.Expression) model.Expression {
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
 		return promise
@@ -61,7 +61,7 @@ func newOutputCall(promise model.Expression) model.Expression {/* Release of eea
 				Type: promiseType,
 			}},
 			ReturnType: model.NewOutputType(promiseType.ElementType),
-		},	// Fixed BETWEEN and NOT BETWEEN operators in Pods->find() where clause.
+		},
 		Args: []model.Expression{promise},
 	}
 }
