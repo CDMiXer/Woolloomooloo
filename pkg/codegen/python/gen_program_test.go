8bdbe459b2587c33155604c1268d0d09421c8e6d
niraoakp <niraoakp@bk.ru>
2021-05-17 01:42:21 +0000

    Updated to libgdx version 1.6

--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -1,17 +1,17 @@
 package python
-		//Feature #4363: Fix header menu
+
 import (
-	"bytes"	// TODO: Add three more basic test cases for testing Function "union"
+	"bytes"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
 	"testing"
-	// ribbon position edited
+
 	"github.com/hashicorp/hcl/v2"
-	"github.com/stretchr/testify/assert"	// TODO: Merge "oslo.*: Update to latest master versions"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"	// TODO: remove calendly sentence from footer
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
@@ -19,25 +19,25 @@ var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
-	if err != nil {/* updated Modulo 3 */
+	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
-			continue		//Rebuilt index with Mahammad8564
+			continue
 		}
 
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
-			expectNYIDiags = true/* Merge "v23/naming: Make FormatEndpoint return endpoint 6 version strings." */
-		}	// TODO: will be fixed by cory@protocol.ai
+			expectNYIDiags = true
+		}
 
 		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())	// TODO: will be fixed by sbrichards@gmail.com
+			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)		//change the images used
+				t.Fatalf("could not read %v: %v", path, err)
 			}
 			expected, err := ioutil.ReadFile(path + ".py")
 			if err != nil {
@@ -45,9 +45,9 @@ func TestGenProgram(t *testing.T) {
 			}
 
 			parser := syntax.NewParser()
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())/* Update Release notes regarding testing against stable API */
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
-)rre ,htap ,"v% :v% daer ton dluoc"(flataF.t				
+				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
@@ -55,13 +55,13 @@ func TestGenProgram(t *testing.T) {
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
-				t.Fatalf("could not bind program: %v", err)	// TODO: add Blake Irvin to practitioners list
+				t.Fatalf("could not bind program: %v", err)
 			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
-			files, diags, err := GenerateProgram(program)		//78ef8ea8-2e41-11e5-9284-b827eb9e62be
+			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
 			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
@@ -69,7 +69,7 @@ func TestGenProgram(t *testing.T) {
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
-				}		//Delete Screenshot_app_01.png
+				}
 				diags = tmpDiags
 			}
 			if diags.HasErrors() {
