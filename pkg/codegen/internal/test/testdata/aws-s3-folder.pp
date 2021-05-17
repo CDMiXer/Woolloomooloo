75477cc86cd803f0ef9505aace8a558d39f2208a
niraoakp <niraoakp@bk.ru>
2021-05-17 08:50:01 +0000

    Updated min MPDN version

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -1,41 +1,41 @@
-// Create a bucket and expose a website index document
+// Create a bucket and expose a website index document		//added milliseconds to timestamp
 resource siteBucket "aws:s3:Bucket" {
 	website = {
 		indexDocument = "index.html"
 	}
 }
 
-siteDir = "www" // directory for content files/* Release 0.21.3 */
+siteDir = "www" // directory for content files
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
-    options {/* Release of eeacms/ims-frontend:0.9.1 */
-		range = readDir(siteDir)/* Update AnalyzerReleases.Shipped.md */
+    options {
+		range = readDir(siteDir)		//Resize fpfd128_impl_t struct to hold BCD products exactly.
     }
 
-	bucket = siteBucket.id // Reference the s3.Bucket object
+	bucket = siteBucket.id // Reference the s3.Bucket object	// TODO: move rails_ujs_fix to public section
 	key = range.value      // Set the key appropriately
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
-}/* major fix ;) */
-/* 1.0.0 Release. */
+}
+
 // Set the access policy for the bucket so all objects are readable
 resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
-
+/* Despublica 'intimacoes-diversas' */
 	// The policy is JSON-encoded.
 	policy = toJSON({
-		Version = "2012-10-17"	// remove undocumented remapping
-		Statement = [{/* added Player class, spawn/init funct */
+		Version = "2012-10-17"
+		Statement = [{
 			Effect = "Allow"
-			Principal = "*"
-			Action = [ "s3:GetObject" ]/* Add preload and dispatch hooks to entry */
-			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]	// TODO: Ajuste hora y mayusculas en el CAP
-		}]	// TODO: will be fixed by cory@protocol.ai
+			Principal = "*"		//HTTPRequest removes fragments from URIs before sending them
+			Action = [ "s3:GetObject" ]
+			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
+		}]
 	})
 }
 
 // Stack outputs
-output bucketName { value = siteBucket.bucket }
-output websiteUrl { value = siteBucket.websiteEndpoint }
+output bucketName { value = siteBucket.bucket }		//Changed loading the JSON Schemata from relative path to localhost:8080
+output websiteUrl { value = siteBucket.websiteEndpoint }/* Merge "Release 3.2.3.399 Prima WLAN Driver" */
