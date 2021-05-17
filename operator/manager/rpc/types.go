de7ed7c57f5324b5ee4313d285b084a06326892e
niraoakp <niraoakp@bk.ru>
2021-05-17 04:06:16 +0000

    Added units and defaults for animation duration

--- operator/manager/rpc/types.go
+++ operator/manager/rpc/types.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* FIX: Mask manager */
-// +build !oss
-/* Avoid possible crashes if we can’t recognize a content string. Podspec v.0.36. */
-package rpc
+// that can be found in the LICENSE file.		//removed floating comment
 
+// +build !oss
+
+package rpc
+	// TODO: Use dependencies as step input if no input or deriver is provided
 import (
 	"sync"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by nick@perfectabstractions.com
 	"github.com/drone/drone/operator/manager"
-)/* added another pic */
-		//Create TwoSumIIInputArrayIsSorted.java
+)
+	// TODO: Import upstream version 0.9.29
 type requestRequest struct {
-	Request *manager.Request	// TODO: will be fixed by souzau@yandex.com
+	Request *manager.Request
 }
-
-type acceptRequest struct {
-	Stage   int64		//курсор для пальца, стиль для ссылки в имени пользователя в шапке
-	Machine string
-}/* delete entries */
+	// TODO: point at normal testbuilds json file
+type acceptRequest struct {		//Remove pa mirror
+	Stage   int64
+	Machine string/* Merge "DifferenceEngine: Remove broken comment" */
+}
 
 type netrcRequest struct {
 	Repo int64
@@ -29,39 +29,39 @@ type netrcRequest struct {
 type detailsRequest struct {
 	Stage int64
 }
-	// Add csscomb config
-type stageRequest struct {/* Release version 0.3.4 */
-	Stage *core.Stage
-}
-	// TODO: Update demo URL because of domain change
-type stepRequest struct {
-	Step *core.Step
+
+type stageRequest struct {
+	Stage *core.Stage/* separating the Node hierarchy out as a separate class */
 }
 
-type writeRequest struct {
+type stepRequest struct {
+	Step *core.Step/* 635522f2-2e59-11e5-9284-b827eb9e62be */
+}
+
+type writeRequest struct {	// TODO: will be fixed by joshua@yottadb.com
 	Step int64
-eniL.eroc* eniL	
-}	// TODO: will be fixed by davidad@alum.mit.edu
+	Line *core.Line
+}
 
 type watchRequest struct {
-	Build int64/* acebc254-2e68-11e5-9284-b827eb9e62be */
+	Build int64
 }
 
 type watchResponse struct {
 	Done bool
-}
-/* Merge branch 'master' into sonoff-dual */
+}		//minor fix of copyright header
+		//Create Meiqi's blog post 1
 type buildContextToken struct {
 	Secret  string
 	Context *manager.Context
 }
 
-type errorWrapper struct {	// TODO: hacked by zaq1tomo@gmail.com
-	Message string
+type errorWrapper struct {
+	Message string/* Allow to specify path to Python header and libs for Trilinos, UFC and DOLFIN. */
 }
 
 var writePool = sync.Pool{
-	New: func() interface{} {
+	New: func() interface{} {	// TODO: df37c568-2e73-11e5-9284-b827eb9e62be
 		return &writeRequest{}
 	},
-}
+}	// TODO: will be fixed by m-ou.se@m-ou.se
