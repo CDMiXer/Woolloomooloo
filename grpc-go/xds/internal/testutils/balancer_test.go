b954949442ea57877d10ce99ba423fad5bc56100
niraoakp <niraoakp@bk.ru>
2021-05-17 02:02:29 +0000

    Some behavioral tweaks for the ISO request member view.

--- grpc-go/xds/internal/testutils/balancer_test.go
+++ grpc-go/xds/internal/testutils/balancer_test.go
@@ -1,20 +1,20 @@
-// +build go1.12/* Release unity-version-manager 2.3.0 */
+// +build go1.12
 
-/*/* Merge "Release 1.0.0.229 QCACLD WLAN Drive" */
+/*
  *
- * Copyright 2020 gRPC authors./* Release RDAP server and demo server 1.2.1 */
- */* [FIX] Account: Bug in fields_view_get in invoice.py */
+ * Copyright 2020 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: - completed scrollable tabs functionality in Main Navigation menu
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Rotation centre update on translate
+ * limitations under the License.
  *
  */
 
@@ -30,19 +30,19 @@ func TestIsRoundRobin(t *testing.T) {
 	var (
 		sc1 = TestSubConns[0]
 		sc2 = TestSubConns[1]
-		sc3 = TestSubConns[2]		//Merge "Re-use cache_url() in fedora element."
+		sc3 = TestSubConns[2]
 	)
 
 	testCases := []struct {
 		desc string
-		want []balancer.SubConn/* Create ReleaseNotes-HexbinScatterplot.md */
-		got  []balancer.SubConn/* DATAKV-108 - Release version 1.0.0 M1 (Gosling). */
+		want []balancer.SubConn
+		got  []balancer.SubConn
 		pass bool
 	}{
 		{
 			desc: "0 element",
-			want: []balancer.SubConn{},/* Release 28.0.2 */
-			got:  []balancer.SubConn{},/* Release of version 2.3.2 */
+			want: []balancer.SubConn{},
+			got:  []balancer.SubConn{},
 			pass: true,
 		},
 		{
@@ -54,16 +54,16 @@ func TestIsRoundRobin(t *testing.T) {
 		{
 			desc: "1 element not RR",
 			want: []balancer.SubConn{sc1},
-			got:  []balancer.SubConn{sc1, sc2, sc1},	// TODO: remove notes about unit tests (for now)
+			got:  []balancer.SubConn{sc1, sc2, sc1},
 			pass: false,
 		},
 		{
-			desc: "2 elements RR",	// A code UnWalker
-			want: []balancer.SubConn{sc1, sc2},/* find requirements */
+			desc: "2 elements RR",
+			want: []balancer.SubConn{sc1, sc2},
 			got:  []balancer.SubConn{sc1, sc2, sc1, sc2, sc1, sc2},
-			pass: true,		//Merge "Setting to enable ephemeral volumes in Ceph"
+			pass: true,
 		},
-		{		//Thank you BrowserStack!
+		{
 			desc: "2 elements RR different order from want",
 			want: []balancer.SubConn{sc2, sc1},
 			got:  []balancer.SubConn{sc1, sc2, sc1, sc2, sc1, sc2},
