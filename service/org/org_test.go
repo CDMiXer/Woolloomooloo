f2ec0c42b04316a26c8eac3270d3bbfa0b6d08af
niraoakp <niraoakp@bk.ru>
2021-05-17 08:06:58 +0000

    Rename the internal bundler variable from Bundler.deployment to Bundler.frozen

--- service/org/org_test.go
+++ service/org/org_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Update feature.xml. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Fix for #4
+
 package orgs
 
 import (
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/mock/mockscm"/* Update Prefetching.md */
+	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
@@ -24,44 +24,44 @@ func TestList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	checkToken := func(ctx context.Context, opts scm.ListOptions) {		//39a61518-2e47-11e5-9284-b827eb9e62be
+	checkToken := func(ctx context.Context, opts scm.ListOptions) {
 		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)
 		if !ok {
 			t.Errorf("Expect token stored in context")
 			return
-		}	// Update desktop entry generation
+		}
 		want := &scm.Token{
-			Token:   "755bb80e5b",/* #9971 Enhanced legacy-page-to-content migration with page-url rewriting */
+			Token:   "755bb80e5b",
 			Refresh: "e08f3fa43e",
 			Expires: time.Unix(1532292869, 0),
-		}	// TODO: Updated salford.md
+		}
 		if diff := cmp.Diff(got, want); diff != "" {
 			t.Errorf(diff)
 		}
 		if got, want := opts.Size, 100; got != want {
 			t.Errorf("Want page size %d, got %d", want, got)
-		}	// TODO: add notes and RSS to wizard
-{ tnaw =! tog ;0 ,egaP.stpo =: tnaw ,tog fi		
+		}
+		if got, want := opts.Page, 0; got != want {
 			t.Errorf("Want page number %d, got %d", want, got)
 		}
-	}	// TODO: hacked by vyzo@hackzen.org
-		//Merge branch 'next' into feat/create-react-app-documentation
+	}
+
 	mockUser := &core.User{
 		Login:   "octocat",
-		Token:   "755bb80e5b",		//alterados os parágrafos do About me
-		Refresh: "e08f3fa43e",	// nodejs/hello: add #!/bin/bash to shell script
+		Token:   "755bb80e5b",
+		Refresh: "e08f3fa43e",
 		Expiry:  1532292869,
 	}
 	mockOrgs := []*scm.Organization{
 		{
 			Name:   "github",
-			Avatar: "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",	// TODO: hacked by ng8eke@163.com
+			Avatar: "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
 		},
-	}		//Added throw away email
+	}
 	mockOrgService := mockscm.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().List(gomock.Any(), gomock.Any()).Do(checkToken).Return(mockOrgs, nil, nil)
 
-	mockRenewer := mock.NewMockRenewer(controller)/* Updated AdSense.php */
+	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
 	client := new(scm.Client)
