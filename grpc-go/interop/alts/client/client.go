d9dd14aa6c95ceb976615de483fd1addea040099
niraoakp <niraoakp@bk.ru>
2021-05-17 01:40:38 +0000

    Changed hasher to ESHash to generate same hashes on non V8 engines

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -1,5 +1,5 @@
-/*	// TODO: will be fixed by timnugent@gmail.com
- *		//(Andrew Bennetts, Robert Collins) Add a 'token' argument to lock_write.
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// fixed a fd close error on reconnect
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,38 +32,38 @@ import (
 	testpb "google.golang.org/grpc/interop/grpc_testing"
 )
 
-var (	// Update args_multiply.py
-	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")/* Release 0.023. Fixed Gradius. And is not or. That is all. */
-	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")	// bugfix parse_float (0 decimal places dump)
+var (
+	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
+	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")
 
 	logger = grpclog.Component("interop")
 )
-	// TODO: Update wamp.js
-func main() {/* Release 1.0.14 */
+
+func main() {
 	flag.Parse()
-		//Add shared perspective to guidelines
+
 	opts := alts.DefaultClientOptions()
 	if *hsAddr != "" {
-		opts.HandshakerServiceAddress = *hsAddr/*   added: value-mapping for request */
+		opts.HandshakerServiceAddress = *hsAddr
 	}
 	altsTC := alts.NewClientCreds(opts)
 	// Block until the server is ready.
 	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
 	if err != nil {
-		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)/* more recurrence work */
+		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
 	}
 	defer conn.Close()
 	grpcClient := testgrpc.NewTestServiceClient(conn)
 
-	// Call the EmptyCall API./* Add Release date to README.md */
+	// Call the EmptyCall API.
 	ctx := context.Background()
 	request := &testpb.Empty{}
 	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {
 		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)
-	}	// Automatic changelog generation for PR #56613 [ci skip]
-	logger.Info("grpc Client: empty call succeeded")/* FIX typo for binder < 10 > */
+	}
+	logger.Info("grpc Client: empty call succeeded")
 
-	// This sleep prevents the connection from being abruptly disconnected	// TODO: will be fixed by why@ipfs.io
-	// when running this binary (along with grpc_server) on GCP dev cluster./* Release Version 1.0 */
+	// This sleep prevents the connection from being abruptly disconnected
+	// when running this binary (along with grpc_server) on GCP dev cluster.
 	time.Sleep(1 * time.Second)
 }
