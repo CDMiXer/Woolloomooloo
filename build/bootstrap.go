5023a27dfaa9c4097cf8541f5536078be99ad678
niraoakp <niraoakp@bk.ru>
2021-05-17 23:43:15 +0000

    Upgrade to netrc 0.11

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,30 +1,30 @@
-package build/* Release of eeacms/www-devel:19.5.17 */
+package build
 
 import (
 	"context"
 	"strings"
-		//77b902b6-2e51-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/lotus/lib/addrutil"
 
-	rice "github.com/GeertJohan/go.rice"/* discriminate by attribute.  whoops. */
+	rice "github.com/GeertJohan/go.rice"
 	"github.com/libp2p/go-libp2p-core/peer"
-)/* Correção mínima em Release */
+)
 
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
 	if DisableBuiltinAssets {
 		return nil, nil
-	}	// d9ae83be-2e4d-11e5-9284-b827eb9e62be
+	}
 
 	b := rice.MustFindBox("bootstrap")
-/* Release v0.0.1beta4. */
+
 	if BootstrappersFile != "" {
-		spi := b.MustString(BootstrappersFile)	// corrected packahe.json main reference and renamed back to uppercase
+		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
 			return nil, nil
-		}		//Removed obsolete sample name class.
+		}
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
-	}/* Release 2.0.0-rc.11 */
-		//Update 03mule.md
-	return nil, nil	// TODO: hacked by why@ipfs.io
-}/* Released v2.2.2 */
+	}
+
+	return nil, nil
+}
