36d66d008a7eced9cd28c4b2dfb8614dd1c1f6eb
niraoakp <niraoakp@bk.ru>
2021-05-17 02:11:16 +0000

    add theming params

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,16 +1,16 @@
 package sso
 
 import (
-	"context"/* Fix for setting Release points */
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/argoproj/argo/server/auth/jws"
 )
-	// TODO: Fixed sln file
-var NullSSO Interface = nullService{}/* 0.6.0 Release */
 
-type nullService struct{}		//merge from rtmp branch, cygnal minimally works for HTTP.
+var NullSSO Interface = nullService{}
+
+type nullService struct{}
 
 func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
@@ -19,7 +19,7 @@ func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
-	// TODO: transparent option (output as png)
+
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)/* Support JSONP in the API */
+	w.WriteHeader(http.StatusNotImplemented)
 }
