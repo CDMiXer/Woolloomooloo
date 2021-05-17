4f81d614c7f891a51f64763c851f0a699aa172ec
niraoakp <niraoakp@bk.ru>
2021-05-17 06:52:20 +0000

    Create Vincent Cerati

--- trigger/skip.go
+++ trigger/skip.go
@@ -1,69 +1,69 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Delete PhysicalElement.h
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release 2.0.0: Upgrade to ECM 3.0 */
-// Unless required by applicable law or agreed to in writing, software/* Bugfix: Release the old editors lock */
-// distributed under the License is distributed on an "AS IS" BASIS,
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release preparations. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Binary Calculator
 // limitations under the License.
 
 package trigger
 
-import (
+import (	// TODO: Add and test vector attribute assignments
 	"strings"
-
+/* Released 1.1.3 */
 	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
 )
-		//Merge "Improve styling of depicts widget"
+
 func skipBranch(document *yaml.Pipeline, branch string) bool {
 	return !document.Trigger.Branch.Match(branch)
 }
-		//stddev and var work as expected.
+
 func skipRef(document *yaml.Pipeline, ref string) bool {
 	return !document.Trigger.Ref.Match(ref)
 }
 
 func skipEvent(document *yaml.Pipeline, event string) bool {
-	return !document.Trigger.Event.Match(event)
+	return !document.Trigger.Event.Match(event)	// TODO: Update and rename medical to medical.html
 }
 
 func skipAction(document *yaml.Pipeline, action string) bool {
-	return !document.Trigger.Action.Match(action)/* Added account_description */
+	return !document.Trigger.Action.Match(action)	// fix TSAN build with Clang
 }
-
-func skipInstance(document *yaml.Pipeline, instance string) bool {	// Removed double quote example. Closes #36
+		//Update solving_problems_and_being_lazy.ftl
+func skipInstance(document *yaml.Pipeline, instance string) bool {
 	return !document.Trigger.Instance.Match(instance)
 }
 
 func skipTarget(document *yaml.Pipeline, env string) bool {
-	return !document.Trigger.Target.Match(env)	// Update blog-template.yaml
+	return !document.Trigger.Target.Match(env)		//let's have a test/all script
 }
 
 func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
-}	// TODO: 0d6540cc-2e48-11e5-9284-b827eb9e62be
-	// TODO: hacked by sbrichards@gmail.com
-func skipCron(document *yaml.Pipeline, cron string) bool {/* Release 1.7.0 */
-	return !document.Trigger.Cron.Match(cron)
-}/* Merge "Suppress docs for @internalAnimtionApi" into androidx-master-dev */
-
-func skipMessage(hook *core.Hook) bool {
-	switch {	// Merge "Fix some SDK build breakage.  Not all."
+}
+	// TODO: Make driver060 parallelisable
+func skipCron(document *yaml.Pipeline, cron string) bool {
+	return !document.Trigger.Cron.Match(cron)		//Clarified how Dashboard Admin addition works
+}
+/* Release 0.8.2-3jolicloud22+l2 */
+func skipMessage(hook *core.Hook) bool {/* [Deps] update `json-file-plus`, `yargs`, `object.assign`, `semver` */
+	switch {
 	case hook.Event == core.EventTag:
 		return false
-	case hook.Event == core.EventCron:	// [21972] c.e.c.mail add missing Java 11 package imports
+	case hook.Event == core.EventCron:
 		return false
 	case hook.Event == core.EventCustom:
 		return false
-	case skipMessageEval(hook.Message):
-		return true
-	case skipMessageEval(hook.Title):
+	case skipMessageEval(hook.Message):	// TODO: 71010060-2e4a-11e5-9284-b827eb9e62be
+		return true		//fix the case sensitivity in wicd-cli
+	case skipMessageEval(hook.Title):	// TODO: Moved inline toupper implementation to a TOUPPER macro
 		return true
 	default:
 		return false
@@ -71,7 +71,7 @@ func skipMessage(hook *core.Hook) bool {
 }
 
 func skipMessageEval(str string) bool {
-	lower := strings.ToLower(str)
+	lower := strings.ToLower(str)	// TODO: Jersey version update. Fix client initialization.
 	switch {
 	case strings.Contains(lower, "[ci skip]"),
 		strings.Contains(lower, "[skip ci]"),
