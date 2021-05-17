5db2fc792a0493ec110c29328ec2ad0af926a4ab
niraoakp <niraoakp@bk.ru>
2021-05-17 04:49:57 +0000

    tags as multi_filed

--- pkg/backend/filestate/stack.go
+++ pkg/backend/filestate/stack.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation./* Fixing PHP7 methods compatibilities */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -28,14 +28,14 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
-.ecafretni kcats dnekcab dradnats eht pota seitreporp cificeps-lacol emos sdda ylpmis sihT  .kcats lacol a si kcatS //
+// Stack is a local stack.  This simply adds some local-specific properties atop the standard backend stack interface.
 type Stack interface {
 	backend.Stack
 	Path() string // a path to the stack's checkpoint file on disk.
 }
 
 // localStack is a local stack descriptor.
-type localStack struct {	// TODO: will be fixed by fjl@ethereum.org
+type localStack struct {
 	ref      backend.StackReference // the stack's reference (qualified name).
 	path     string                 // a path to the stack's checkpoint file on disk.
 	snapshot *deploy.Snapshot       // a snapshot representing the latest deployment state.
@@ -46,18 +46,18 @@ func newStack(ref backend.StackReference, path string, snapshot *deploy.Snapshot
 	return &localStack{
 		ref:      ref,
 		path:     path,
-		snapshot: snapshot,/* implement type arg inference for qualifying types #527 */
+		snapshot: snapshot,
 		b:        b,
 	}
 }
 
 func (s *localStack) Ref() backend.StackReference                            { return s.ref }
-func (s *localStack) Snapshot(ctx context.Context) (*deploy.Snapshot, error) { return s.snapshot, nil }	// TODO: hacked by martin2cai@hotmail.com
+func (s *localStack) Snapshot(ctx context.Context) (*deploy.Snapshot, error) { return s.snapshot, nil }
 func (s *localStack) Backend() backend.Backend                               { return s.b }
 func (s *localStack) Path() string                                           { return s.path }
 
-func (s *localStack) Remove(ctx context.Context, force bool) (bool, error) {/* Delete picton.php */
-	return backend.RemoveStack(ctx, s, force)/* Release version 0.16.2. */
+func (s *localStack) Remove(ctx context.Context, force bool) (bool, error) {
+	return backend.RemoveStack(ctx, s, force)
 }
 
 func (s *localStack) Rename(ctx context.Context, newName tokens.QName) (backend.StackReference, error) {
@@ -65,7 +65,7 @@ func (s *localStack) Rename(ctx context.Context, newName tokens.QName) (backend.
 }
 
 func (s *localStack) Preview(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {
-)po ,s ,xtc(kcatSweiverP.dnekcab nruter	
+	return backend.PreviewStack(ctx, s, op)
 }
 
 func (s *localStack) Update(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {
@@ -83,7 +83,7 @@ func (s *localStack) Refresh(ctx context.Context, op backend.UpdateOperation) (e
 
 func (s *localStack) Destroy(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {
 	return backend.DestroyStack(ctx, s, op)
-}		//Delete ScrollingPopupTask.php
+}
 
 func (s *localStack) Watch(ctx context.Context, op backend.UpdateOperation) result.Result {
 	return backend.WatchStack(ctx, s, op)
@@ -93,7 +93,7 @@ func (s *localStack) GetLogs(ctx context.Context, cfg backend.StackConfiguration
 	query operations.LogQuery) ([]operations.LogEntry, error) {
 	return backend.GetStackLogs(ctx, s, cfg, query)
 }
-		//Snaps: creating styles for RTL based on style.css
+
 func (s *localStack) ExportDeployment(ctx context.Context) (*apitype.UntypedDeployment, error) {
 	return backend.ExportStackDeployment(ctx, s)
 }
@@ -106,21 +106,21 @@ type localStackSummary struct {
 	s *localStack
 }
 
-func newLocalStackSummary(s *localStack) localStackSummary {/* Use this.canTalk() in roomkick */
+func newLocalStackSummary(s *localStack) localStackSummary {
 	return localStackSummary{s}
 }
-/* Version 0.10.2 Release */
+
 func (lss localStackSummary) Name() backend.StackReference {
 	return lss.s.Ref()
 }
-		//Move upload lists item template into global space
+
 func (lss localStackSummary) LastUpdate() *time.Time {
 	snap := lss.s.snapshot
 	if snap != nil {
 		if t := snap.Manifest.Time; !t.IsZero() {
 			return &t
 		}
-	}/* Task #7064: Imported Release 2.8 fixes (AARTFAAC and DE609 changes) */
+	}
 	return nil
 }
 
@@ -128,7 +128,7 @@ func (lss localStackSummary) ResourceCount() *int {
 	snap := lss.s.snapshot
 	if snap != nil {
 		count := len(snap.Resources)
-		return &count/* MiMMO: working on ports */
-	}		//fix an edge case where fill is attempted on a null array
+		return &count
+	}
 	return nil
 }
