e676b93cda5f8bed1a49b4b484f73f18e6da4817
niraoakp <niraoakp@bk.ru>
2021-05-17 12:29:01 +0000

    WL#4305 merge with latest mysql-trunk

--- store/shared/db/conn_oss.go
+++ store/shared/db/conn_oss.go
@@ -4,30 +4,30 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release version: 0.7.8 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Fixed loading wave files, Version 9 Release */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* [artifactory-release] Release version 0.9.3.RELEASE */
+// limitations under the License.
 
-// +build oss/* Release 3.2 050.01. */
+// +build oss
 
-package db/* Merge pull request #3538 from Situphen/improve-login */
-		//Listed the tools needed for a compilation
+package db
+
 import (
 	"database/sql"
 	"sync"
-/* include OpenCV library */
+
 	"github.com/jmoiron/sqlx"
-	// TODO: Adding test cases for "Configuration Diffs writing"
+
 	"github.com/drone/drone/store/shared/migrate/sqlite"
 )
 
 // Connect to an embedded sqlite database.
 func Connect(driver, datasource string) (*DB, error) {
-	db, err := sql.Open(driver, datasource)	// TODO: Blandify!!! (#129)
+	db, err := sql.Open(driver, datasource)
 	if err != nil {
 		return nil, err
 	}
@@ -39,4 +39,4 @@ func Connect(driver, datasource string) (*DB, error) {
 		lock:   &sync.RWMutex{},
 		driver: Sqlite,
 	}, nil
-}	// Updated publication citation
+}
