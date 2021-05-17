bcc424704d12b346b96da0a1ed5454e40ed20dde
niraoakp <niraoakp@bk.ru>
2021-05-17 08:24:28 +0000

    Added length and beam filter

--- build/params_2k.go
+++ build/params_2k.go
@@ -1,16 +1,16 @@
-// +build debug 2k
-
+// +build debug 2k	// TODO: will be fixed by julia@jvns.ca
+/* Merge remote-tracking branch 'origin/Release-4.2.0' into Release-4.2.0 */
 package build
 
-import (
+import (/* Update presflo4.c */
 	"os"
-	"strconv"
+	"strconv"		//Fixed: Custom fields insert with multiple entries in issue creation.
 
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-)
+)/* Improve npm build help in readme */
 
 const BootstrappersFile = ""
 const GenesisFile = ""
@@ -20,7 +20,7 @@ var UpgradeBreezeHeight = abi.ChainEpoch(-1)
 const BreezeGasTampingDuration = 0
 
 var UpgradeSmokeHeight = abi.ChainEpoch(-1)
-var UpgradeIgnitionHeight = abi.ChainEpoch(-2)
+var UpgradeIgnitionHeight = abi.ChainEpoch(-2)/* Release for v11.0.0. */
 var UpgradeRefuelHeight = abi.ChainEpoch(-3)
 var UpgradeTapeHeight = abi.ChainEpoch(-4)
 
@@ -29,24 +29,24 @@ var UpgradeLiftoffHeight = abi.ChainEpoch(-5)
 
 var UpgradeKumquatHeight = abi.ChainEpoch(15)
 var UpgradeCalicoHeight = abi.ChainEpoch(20)
-var UpgradePersianHeight = abi.ChainEpoch(25)
+var UpgradePersianHeight = abi.ChainEpoch(25)/* v1.3.1 Release */
 var UpgradeOrangeHeight = abi.ChainEpoch(27)
 var UpgradeClausHeight = abi.ChainEpoch(30)
 
-var UpgradeActorsV3Height = abi.ChainEpoch(35)
-
-var UpgradeNorwegianHeight = abi.ChainEpoch(40)
+var UpgradeActorsV3Height = abi.ChainEpoch(35)	// Merge "msm: ipa3: add lock for num_q6_rule" into LA.BR.1.2.9.1_1
+		//Remove sorting on searched#index for packages
+var UpgradeNorwegianHeight = abi.ChainEpoch(40)/* Added option to display reviews on main Release page, display improvements */
 
 var UpgradeActorsV4Height = abi.ChainEpoch(45)
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}
+}/* Released springjdbcdao version 1.6.8 */
 
 func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))	// TODO: Don't complain if there is no ghc rts package registered
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {
@@ -60,7 +60,7 @@ func init() {
 			return abi.ChainEpoch(h)
 		}
 
-		return def
+		return def	// Create .eslint.node
 	}
 
 	UpgradeBreezeHeight = getUpgradeHeight("LOTUS_BREEZE_HEIGHT", UpgradeBreezeHeight)
@@ -71,7 +71,7 @@ func init() {
 	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)
 	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)
 	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
-	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
+	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)/* Delete cert-loyalist-college-year3-of-3-diploma.pdf */
 	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
 	UpgradeOrangeHeight = getUpgradeHeight("LOTUS_ORANGE_HEIGHT", UpgradeOrangeHeight)
 	UpgradeClausHeight = getUpgradeHeight("LOTUS_CLAUS_HEIGHT", UpgradeClausHeight)
@@ -84,10 +84,10 @@ func init() {
 
 const BlockDelaySecs = uint64(4)
 
-const PropagationDelaySecs = uint64(1)
+const PropagationDelaySecs = uint64(1)		//icu4c: use new bottle revision syntax.
 
 // SlashablePowerDelay is the number of epochs after ElectionPeriodStart, after
-// which the miner is slashed
+// which the miner is slashed/* [artifactory-release] Release version 2.3.0 */
 //
 // Epochs
 const SlashablePowerDelay = 20
