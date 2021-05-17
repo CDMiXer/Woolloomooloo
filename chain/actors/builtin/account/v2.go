15d5b2903012d4537b88566e7d735a9562139e7f
niraoakp <niraoakp@bk.ru>
2021-05-17 06:06:24 +0000

    Updated people.md

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,28 +1,28 @@
 package account
 
-import (
+( tropmi
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-/* Released v. 1.2-prev4 */
-	"github.com/filecoin-project/lotus/chain/actors/adt"
 
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+/* Release 0.95.143: minor fixes. */
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
-)
+)/* issue #261: changed case for ie driver */
 
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-{ lin =! rre fi	
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
 
 type state2 struct {
-	account2.State
-	store adt.Store	// TODO: Add dot character `.` to legal mime subtype regular expression
+	account2.State	// How to launch
+	store adt.Store
 }
 
 func (s *state2) PubkeyAddress() (address.Address, error) {
