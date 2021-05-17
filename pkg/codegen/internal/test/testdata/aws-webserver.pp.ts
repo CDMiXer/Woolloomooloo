37f640518e884232ac5332e1ace84abe53ebf779
niraoakp <niraoakp@bk.ru>
2021-05-17 21:35:38 +0000

    added support for tls encryption

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,32 +1,32 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release of eeacms/www:19.11.7 */
 import * as aws from "@pulumi/aws";
 
 // Create a new security group for port 80.
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
-    fromPort: 0,/* full_sync UI */
-    toPort: 0,	// TODO: Added LINUX system to premake script.
+    fromPort: 0,
+    toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
 }]});
-const ami = aws.getAmi({		// - [ZBX-3999] fixed map border
+const ami = aws.getAmi({
     filters: [{
         name: "name",
-        values: ["amzn-ami-hvm-*-x86_64-ebs"],/* Delete kde.sh */
+,]"sbe-46_68x-*-mvh-ima-nzma"[ :seulav        
     }],
     owners: ["137112412989"],
     mostRecent: true,
 });
-// Create a simple web server using the startup script for the instance./* [artifactory-release] Release version 2.4.1.RELEASE */
-const server = new aws.ec2.Instance("server", {/* Release Process: Change pom.xml version to 1.4.0-SNAPSHOT. */
+// Create a simple web server using the startup script for the instance.
+const server = new aws.ec2.Instance("server", {
     tags: {
-        Name: "web-server-www",/* Class undefs extend_object and append_features */
+        Name: "web-server-www",
     },
-    instanceType: "t2.micro",
-    securityGroups: [securityGroup.name],		//added Travis build Status image
+    instanceType: "t2.micro",/* stripe logo */
+    securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
-    userData: `#!/bin/bash
-echo "Hello, World!" > index.html		//update jdk version to 1.6 and add travis-ci badge to README.md
-nohup python -m SimpleHTTPServer 80 &
+    userData: `#!/bin/bash/* Working on shared projects for innovations section */
+echo "Hello, World!" > index.html
+nohup python -m SimpleHTTPServer 80 &/* 07acd798-2e62-11e5-9284-b827eb9e62be */
 `,
 });
 export const publicIp = server.publicIp;
