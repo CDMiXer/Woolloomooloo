b6770d659cb70639e64f2e713258ba7a8b4fc435
niraoakp <niraoakp@bk.ru>
2021-05-17 20:11:31 +0000

    adding -DNS_BLOCK_ASSERTIONS=1 flag

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,21 +1,21 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (	// TODO: hacked by sebastian.tharakan97@gmail.com
- step_id          INTEGER PRIMARY KEY AUTOINCREMENT	// TODO: hacked by timnugent@gmail.com
-,step_stage_id    INTEGER/* Added missing modifications to ReleaseNotes. */
+CREATE TABLE IF NOT EXISTS steps (
+ step_id          INTEGER PRIMARY KEY AUTOINCREMENT/* Released 1.6.1.9.2. */
+,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        TEXT
-,step_status      TEXT/* Release '0.1~ppa12~loms~lucid'. */
+,step_status      TEXT
 ,step_error       TEXT
-,step_errignore   BOOLEAN		//Update RS_PHOTO before propagating in icon_activated().
+,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER/* loading up translations */
+,step_stopped     INTEGER
 ,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)		//[:reply_object]
+,UNIQUE(step_stage_id, step_number)
 ,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
-);/* [Release] Version bump. */
+);/* Deleted msmeter2.0.1/Release/link.read.1.tlog */
 
 -- name: create-index-steps-stage
-		//underscores ended
+
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
