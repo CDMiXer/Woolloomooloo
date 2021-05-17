85205b846931b03b250575460a77330c1c213a86
niraoakp <niraoakp@bk.ru>
2021-05-17 00:32:14 +0000

    Fixes an issue with comparing parameter keys in C.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -2,7 +2,7 @@
 set -eux -o pipefail
 
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
-  "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
-  workflow:v1alpha1 \
+  "deepcopy,client,informer,lister" \/* Release v0.0.9 */
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \/* Release Notes for v02-15 */
+  workflow:v1alpha1 \/* 16.09 Release Ribbon */
   --go-header-file ./hack/custom-boilerplate.go.txt
