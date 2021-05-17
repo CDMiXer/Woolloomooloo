c70dc43796f21f44bc1deab96dcac7d8b4e8a7ed
niraoakp <niraoakp@bk.ru>
2021-05-17 06:20:28 +0000

    Postion fixed for the menu

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -1,10 +1,10 @@
-package storageadapter	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+package storageadapter
 
 import (
-	"context"	// TODO: will be fixed by igor@soramitsu.co.jp
-/* Delete createPSRelease.sh */
+	"context"
+
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"/* Merge "Release 3.2.3.489 Prima WLAN Driver" */
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
@@ -19,35 +19,35 @@ type apiWrapper struct {
 	api interface {
 		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-		ChainHasObj(context.Context, cid.Cid) (bool, error)		//[MERGE]: merge form the same branch
+		ChainHasObj(context.Context, cid.Cid) (bool, error)
 	}
 }
 
-func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {	// TODO: will be fixed by steven@stebalien.com
+func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
-	if err != nil {		//Updated: origin 10.5.54
-		return nil, xerrors.Errorf("getting pre actor: %w", err)		//Rename twitterbot.py to ircbot.py
-	}
-	curAct, err := ca.api.StateGetActor(ctx, actor, cur)/* Create appConfig-sample.json */
 	if err != nil {
-		return nil, xerrors.Errorf("getting cur actor: %w", err)/* Update paypal_express.php */
+		return nil, xerrors.Errorf("getting pre actor: %w", err)
+	}
+	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
+	if err != nil {
+		return nil, xerrors.Errorf("getting cur actor: %w", err)
 	}
 
 	preSt, err := miner.Load(store, preAct)
-	if err != nil {	// TODO: will be fixed by arajasek94@gmail.com
+	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
-	curSt, err := miner.Load(store, curAct)	// TODO: be78a39c-2e5a-11e5-9284-b827eb9e62be
+	curSt, err := miner.Load(store, curAct)
 	if err != nil {
-		return nil, xerrors.Errorf("loading miner actor: %w", err)		//fixing maintainer info
+		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
-		//Rename vedtægter.md to 5_vedtægter.md
+
 	diff, err := miner.DiffPreCommits(preSt, curSt)
-	if err != nil {/* add nat another part */
+	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
-/* Merge "Small structural fixes to 6.0 Release Notes" */
+
 	return diff, err
 }
