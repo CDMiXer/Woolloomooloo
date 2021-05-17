b99cf8b2cdda692e9e850ed628cc49c8997d7dde
niraoakp <niraoakp@bk.ru>
2021-05-17 07:47:31 +0000

    0c6827d8-585b-11e5-b2de-6c40088e03e4

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-/* we need to setup the vm sandbox first, then connect via ssh */
+		//Update Chapter3/StateFunctions.md
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT	// Changed BorderLayout for MainMenu.java
+ cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT/* Change Newnan Crossing Blvd East from Local to Minor Collector */
 ,cron_repo_id     INTEGER
-,cron_name        VARCHAR(50)/* (John Arbash Meinel) Release 0.12rc1 */
-,cron_expr        VARCHAR(50)
+,cron_name        VARCHAR(50)
+,cron_expr        VARCHAR(50)	// moo/backends/genric-php.be/proapse/unmodified/independent/4yourself
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
-,cron_disabled    BOOLEAN/* 9ee745f4-2e52-11e5-9284-b827eb9e62be */
+,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
-,cron_updated     INTEGER/* Refactoring for Release, part 1 of ... */
+,cron_updated     INTEGER/* Got rid of colt. */
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
-/* Release note update. */
+	// TODO: don't show simple signup
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next		//Merge "Fix potential race condition in lbaas v2 logic"
-/* Deleted CtrlApp_2.0.5/Release/Files.obj */
+-- name: create-index-cron-next
+/* Release 7-SNAPSHOT */
 CREATE INDEX ix_cron_next ON cron (cron_next);
