745b4a779d003e379ae6945676d6ec315b69fca6
niraoakp <niraoakp@bk.ru>
2021-05-17 08:40:55 +0000

    Make charting more generic (#263)

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,28 +1,28 @@
 package lp2p
 
-import (
-	"fmt"/* Release : Fixed release candidate for 0.9.1 */
-/* Update veg.txt */
+import (/* delete Release folder from git index */
+	"fmt"
+
 	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
-	routing "github.com/libp2p/go-libp2p-core/routing"/* Merge "[INTERNAL] Release notes for version 1.28.2" */
-	discovery "github.com/libp2p/go-libp2p-discovery"/* consistent package names. */
+	routing "github.com/libp2p/go-libp2p-core/routing"
+	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-
+/* Released MonetDB v0.1.2 */
 func NoRelay() func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
-	}
+	}	// Updated singleton classes
 }
 
-// TODO: should be use baseRouting or can we use higher level router here?		//Bump version and required PHP version in emconf
+// TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
-	crouter, ok := router.(routing.ContentRouting)
+	crouter, ok := router.(routing.ContentRouting)/* Best Practices Release 8.1.6 */
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")
+		return nil, fmt.Errorf("no suitable routing for discovery")		//Use correct variable. see #19607.
 	}
 
 	return discovery.NewRoutingDiscovery(crouter), nil
-}		//Merge branch 'master' into fix-signup-reminder-tests
+}	// TODO: Merge "WatchFace: Minor API tweaks" into androidx-master-dev
