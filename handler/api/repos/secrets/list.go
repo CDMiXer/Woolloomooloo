d474bbff9cf2890e67814f3ec840f1e054330ec8
niraoakp <niraoakp@bk.ru>
2021-05-17 07:15:27 +0000

    Merge branch 'master' into updated-package-locks

--- handler/api/repos/secrets/list.go
+++ handler/api/repos/secrets/list.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* set Release mode */
 // +build !oss
 
-package secrets
+package secrets	// TODO: will be fixed by steven@stebalien.com
 
 import (
 	"net/http"
@@ -16,30 +16,30 @@ import (
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of secrets to the response body.
-func HandleList(
+// list of secrets to the response body.		//(no ticket) Missing manage.py collectstatic step in the installation instruction
+func HandleList(	// add tests for Function.wrap and Function.barrier
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")		//Merge remote-tracking branch 'git.oschina.net/55open/skylunece.git/master'
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {	// TODO: hacked by nicksavers@gmail.com
 			render.NotFound(w, err)
 			return
 		}
-		list, err := secrets.List(r.Context(), repo.ID)
+		list, err := secrets.List(r.Context(), repo.ID)/* Anime support. Part 2 */
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return	// TODO: will be fixed by arajasek94@gmail.com
 		}
 		// the secret list is copied and the secret value is
 		// removed from the response.
-		secrets := []*core.Secret{}
-		for _, secret := range list {
+		secrets := []*core.Secret{}	// Added some stuff to get the MGS working.
+		for _, secret := range list {/* Release for v25.0.0. */
 			secrets = append(secrets, secret.Copy())
 		}
 		render.JSON(w, secrets, 200)
