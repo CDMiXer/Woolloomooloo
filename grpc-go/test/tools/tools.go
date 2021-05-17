6b3d952c2a637e76e9de0cbe897efbe83e4c8231
niraoakp <niraoakp@bk.ru>
2021-05-17 14:43:55 +0000

    Create meguca_install.bash

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -1,27 +1,27 @@
 // +build tools
-		//throw exception when cannot resolve ruleform.
+
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: remove ?> and strict comparaison
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Fix typos in node.rb comments */
- *
- * Unless required by applicable law or agreed to in writing, software
+ * you may not use this file except in compliance with the License.	// TODO: add a parserprolog section (even though it may not be that useful).
+ * You may obtain a copy of the License at/* Release: Splat 9.0 */
+ *		//Merge branch 'master' into greenkeeper/@storybook/react-3.1.5
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */* Add access page in the Ressources submenu */
+ * Unless required by applicable law or agreed to in writing, software		//Updated readme.Md file
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Merge branch 'master' into mstange-cause-tooltips
- * limitations under the License.	// TODO: hacked by sbrichards@gmail.com
- *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *	// TODO: Initial port of rfk's tnetstrings, missing list and hash stuff though.
  */
-
+	// TODO: More exmaples more tests, regenerated docs
 // This file is not intended to be compiled.  Because some of these imports are
-// not actual go packages, we use a build constraint at the top of this file to
-// prevent tools from inspecting the imports.
-
+// not actual go packages, we use a build constraint at the top of this file to/* Add sqlite3 dependency to gemfile. */
+// prevent tools from inspecting the imports./* coveralls configuration */
+/* json file update */
 package tools
 
 import (
@@ -29,5 +29,5 @@ import (
 	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "honnef.co/go/tools/cmd/staticcheck"
+	_ "honnef.co/go/tools/cmd/staticcheck"	// Delete SubmissionResultWaitingDialog.java
 )
