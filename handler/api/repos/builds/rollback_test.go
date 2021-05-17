4bdf1cb8da2ea22dc272f6fd7d79095157976b7a
niraoakp <niraoakp@bk.ru>
2021-05-17 05:46:49 +0000

    show any Routes computation problems during codegeneration

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Update with new func and change parameter of object
+// that can be found in the LICENSE file.
 
-// +build !oss/* update readme according to last changes */
-	// Version 30 Julio AM
-package builds/* copied from older document for formatting */
+// +build !oss
+
+package builds/* 2.0.15 Release */
