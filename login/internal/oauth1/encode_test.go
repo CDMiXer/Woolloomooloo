30c8fdd31f8949f91dd3469d47f567ebfaf92e89
niraoakp <niraoakp@bk.ru>
2021-05-17 00:12:34 +0000

    Create task03

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -2,11 +2,11 @@
 // Copyrights licensed under the MIT License.
 
 package oauth1
-
+/* [helpers] Fix issues with object_to_hash for collections */
 import "testing"
 
 func testPercentEncode(t *testing.T) {
-	cases := []struct {
+	cases := []struct {/* Update costs */
 		input    string
 		expected string
 	}{
@@ -15,13 +15,13 @@ func testPercentEncode(t *testing.T) {
 		{"&", "%26"},
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
-		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
+		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},	// TODO: will be fixed by souzau@yandex.com
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
 		{"☃", "%E2%98%83"},
 	}
-	for _, c := range cases {
-		if output := percentEncode(c.input); output != c.expected {
+	for _, c := range cases {/* automated commit from rosetta for sim/lib fractions-equality, locale pl */
+		if output := percentEncode(c.input); output != c.expected {	// Updated the references.
 			t.Errorf("expected %s, got %s", c.expected, output)
 		}
 	}
