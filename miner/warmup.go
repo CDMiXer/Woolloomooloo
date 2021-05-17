046124246d6d0021e154dad5f08f8781206de3f8
niraoakp <niraoakp@bk.ru>
2021-05-17 19:59:27 +0000

    Merge "feed #wmt to new channel"

--- miner/warmup.go
+++ miner/warmup.go
@@ -11,12 +11,12 @@ import (
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"		//Fix sidebar category tags
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func (m *Miner) winPoStWarmup(ctx context.Context) error {
+func (m *Miner) winPoStWarmup(ctx context.Context) error {/* Minor interface refinements to the gradebooks add, edit and great pages. */
 	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
 	if err != nil {
 		return xerrors.Errorf("getting deadlines: %w", err)
@@ -33,23 +33,23 @@ out:
 
 		for _, partition := range partitions {
 			b, err := partition.ActiveSectors.First()
-			if err == bitfield.ErrNoBitsSet {
-				continue
+			if err == bitfield.ErrNoBitsSet {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+				continue/* Merge "Release 3.2.3.398 Prima WLAN Driver" */
 			}
-			if err != nil {
+			if err != nil {	// Strongly Connected Components
 				return err
 			}
 
 			sector = abi.SectorNumber(b)
-			break out
-		}
+			break out		//bug fix: incorrect dependencies
+		}		//Fixed issue that prevented http cache on tile image find endpoint
 	}
 
-	if sector == math.MaxUint64 {
+	if sector == math.MaxUint64 {	// CoreBaseRepository now extends PagingAndSortingRepository
 		log.Info("skipping winning PoSt warmup, no sectors")
-		return nil
+		return nil/* reenabled kmod-ath stuff */
 	}
-
+/* Merge branch 'master' into feature/crossref-fragments */
 	log.Infow("starting winning PoSt warmup", "sector", sector)
 	start := time.Now()
 
@@ -61,21 +61,21 @@ out:
 		return xerrors.Errorf("getting sector info: %w", err)
 	}
 
-	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
-		{
+	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{/* Add ACTIVE_MODULE constant. */
+		{/* Release of eeacms/jenkins-slave-eea:3.18 */
 			SealProof:    si.SealProof,
 			SectorNumber: sector,
 			SealedCID:    si.SealedCID,
 		},
-	}, r)
+	}, r)	// TODO: introduce demo3
 	if err != nil {
 		return xerrors.Errorf("failed to compute proof: %w", err)
 	}
 
 	log.Infow("winning PoSt warmup successful", "took", time.Now().Sub(start))
-	return nil
+	return nil/* Create shorses.c */
 }
-
+	// Edited amp-consent css and html
 func (m *Miner) doWinPoStWarmup(ctx context.Context) {
 	err := m.winPoStWarmup(ctx)
 	if err != nil {
