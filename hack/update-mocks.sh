cb7392159426a9f7ed06a1ae242799debf9802b5
niraoakp <niraoakp@bk.ru>
2021-05-17 00:23:28 +0000

    update peoples who are concerned

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -5,7 +5,7 @@ for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
-  cd "$MOCK_DIR"
+  cd "$MOCK_DIR"	// back to origanal with prebuild
   mockery -name=$"$MOCK_NAME"
-  cd -
+  cd -		//Added missing language variable in Upload
 done
