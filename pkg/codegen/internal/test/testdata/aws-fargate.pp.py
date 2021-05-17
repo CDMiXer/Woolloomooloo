1baf8f3d69d91f2f584e404cd12a714763e0b189
niraoakp <niraoakp@bk.ru>
2021-05-17 23:53:50 +0000

    dc9947f0-2e41-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,4 +1,4 @@
-import pulumi	// TODO: will be fixed by davidad@alum.mit.edu
+import pulumi
 import json
 import pulumi_aws as aws
 
@@ -15,7 +15,7 @@ web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
     )],
     ingress=[aws.ec2.SecurityGroupIngressArgs(
         protocol="tcp",
-        from_port=80,	// Rename gongfuzuqiu.md to shaolinzuqiu.md
+        from_port=80,
         to_port=80,
         cidr_blocks=["0.0.0.0/0"],
     )])
@@ -24,17 +24,17 @@ cluster = aws.ecs.Cluster("cluster")
 # Create an IAM role that can be used by our service's task.
 task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
     "Version": "2008-10-17",
-    "Statement": [{	// GUI: Adjust AppDatadir (for linux)
+    "Statement": [{
         "Sid": "",
-        "Effect": "Allow",		//Association template eliminating likely nonexistant apps
+        "Effect": "Allow",
         "Principal": {
             "Service": "ecs-tasks.amazonaws.com",
         },
         "Action": "sts:AssumeRole",
     }],
-}))/* fix(package): update ytdl-core to version 0.15.0 */
+}))
 task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
-    role=task_exec_role.name,		//Merge "[INTERNAL] sap.f: eslint warnings fixed"
+    role=task_exec_role.name,
     policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
@@ -43,7 +43,7 @@ web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
 web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     port=80,
     protocol="HTTP",
-    target_type="ip",		//Give focus to newly created tasks
+    target_type="ip",
     vpc_id=vpc.id)
 web_listener = aws.elasticloadbalancingv2.Listener("webListener",
     load_balancer_arn=web_load_balancer.arn,
@@ -55,14 +55,14 @@ web_listener = aws.elasticloadbalancingv2.Listener("webListener",
 # Spin up a load balanced service running NGINX
 app_task = aws.ecs.TaskDefinition("appTask",
     family="fargate-task-definition",
-    cpu="256",/* 92a7639a-2e76-11e5-9284-b827eb9e62be */
-    memory="512",	// Merge "Merge feabc2df0b8bfc8e4508cfe4bc2d701491bc6fb2 on remote branch"
+    cpu="256",
+    memory="512",
     network_mode="awsvpc",
     requires_compatibilities=["FARGATE"],
     execution_role_arn=task_exec_role.arn,
     container_definitions=json.dumps([{
         "name": "my-app",
-        "image": "nginx",		//still cleaning up imports using spyder
+        "image": "nginx",
         "portMappings": [{
             "containerPort": 80,
             "hostPort": 80,
@@ -76,13 +76,13 @@ app_service = aws.ecs.Service("appService",
     task_definition=app_task.arn,
     network_configuration=aws.ecs.ServiceNetworkConfigurationArgs(
         assign_public_ip=True,
-        subnets=subnets.ids,/* New scikit-learn requirements */
+        subnets=subnets.ids,
         security_groups=[web_security_group.id],
     ),
     load_balancers=[aws.ecs.ServiceLoadBalancerArgs(
         target_group_arn=web_target_group.arn,
-        container_name="my-app",		//Fix adoc syntax issue from issue #1
+        container_name="my-app",
         container_port=80,
     )],
-    opts=pulumi.ResourceOptions(depends_on=[web_listener]))/* Alpha 0.6.3 Release */
+    opts=pulumi.ResourceOptions(depends_on=[web_listener]))
 pulumi.export("url", web_load_balancer.dns_name)
