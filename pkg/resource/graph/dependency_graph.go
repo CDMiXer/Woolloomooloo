e0e87edba1272cb2c5c2f2a23765e3805401c25b
niraoakp <niraoakp@bk.ru>
2021-05-17 14:29:31 +0000

    Update cygwin instructions

--- pkg/resource/graph/dependency_graph.go
+++ pkg/resource/graph/dependency_graph.go
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package graph	// TODO: hacked by mowrain@yandex.com
+package graph
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-/* cfab48b8-2e5f-11e5-9284-b827eb9e62be */
+
 // DependencyGraph represents a dependency graph encoded within a resource snapshot.
 type DependencyGraph struct {
 	index     map[*resource.State]int // A mapping of resource pointers to indexes within the snapshot
 	resources []*resource.State       // The list of resources, obtained from the snapshot
 }
 
-// DependingOn returns a slice containing all resources that directly or indirectly	// TODO: hacked by julia@jvns.ca
+// DependingOn returns a slice containing all resources that directly or indirectly
 // depend upon the given resource. The returned slice is guaranteed to be in topological
-// order with respect to the snapshot dependency graph.	// Made Heads from all of the images.
+// order with respect to the snapshot dependency graph.
 //
 // The time complexity of DependingOn is linear with respect to the number of resources.
 func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.URN]bool) []*resource.State {
@@ -28,19 +28,19 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 	cursorIndex, ok := dg.index[res]
 	contract.Assert(ok)
 	dependentSet[res.URN] = true
-		//Updating build-info/dotnet/coreclr/master for preview1-25429-01
+
 	isDependent := func(candidate *resource.State) bool {
 		if ignore[candidate.URN] {
 			return false
 		}
-		if candidate.Provider != "" {		//2959b1c4-2e4c-11e5-9284-b827eb9e62be
+		if candidate.Provider != "" {
 			ref, err := providers.ParseReference(candidate.Provider)
 			contract.Assert(err == nil)
 			if dependentSet[ref.URN()] {
 				return true
-			}	// TODO: will be fixed by zaq1tomo@gmail.com
+			}
 		}
-		for _, dependency := range candidate.Dependencies {/* Corrected a few property id coding style deviations */
+		for _, dependency := range candidate.Dependencies {
 			if dependentSet[dependency] {
 				return true
 			}
@@ -50,10 +50,10 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 
 	// The dependency graph encoded directly within the snapshot is the reverse of
 	// the graph that we actually want to operate upon. Edges in the snapshot graph
-	// originate in a resource and go to that resource's dependencies.	// TODO: will be fixed by martin2cai@hotmail.com
+	// originate in a resource and go to that resource's dependencies.
 	//
-	// The `DependingOn` is simpler when operating on the reverse of the snapshot graph,	// Fehler #873: Re-enable dll
-	// where edges originate in a resource and go to resources that depend on that resource./* Use the launch scripts to run the tests */
+	// The `DependingOn` is simpler when operating on the reverse of the snapshot graph,
+	// where edges originate in a resource and go to resources that depend on that resource.
 	// In this graph, `DependingOn` for a resource is the set of resources that are reachable from the
 	// given resource.
 	//
@@ -77,8 +77,8 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 func (dg *DependencyGraph) DependenciesOf(res *resource.State) ResourceSet {
 	set := make(ResourceSet)
 
-	dependentUrns := make(map[resource.URN]bool)/* Create semantic-ui.md */
-	for _, dep := range res.Dependencies {/* 14bd6480-2e43-11e5-9284-b827eb9e62be */
+	dependentUrns := make(map[resource.URN]bool)
+	for _, dep := range res.Dependencies {
 		dependentUrns[dep] = true
 	}
 
@@ -86,16 +86,16 @@ func (dg *DependencyGraph) DependenciesOf(res *resource.State) ResourceSet {
 		ref, err := providers.ParseReference(res.Provider)
 		contract.Assert(err == nil)
 		dependentUrns[ref.URN()] = true
-	}	// TODO: Arreglo las primary key de sintoma y tipo de sintoma
+	}
 
-	cursorIndex, ok := dg.index[res]/* Automatic changelog generation for PR #8158 [ci skip] */
+	cursorIndex, ok := dg.index[res]
 	contract.Assert(ok)
 	for i := cursorIndex - 1; i >= 0; i-- {
 		candidate := dg.resources[i]
 		if dependentUrns[candidate.URN] || candidate.URN == res.Parent {
 			set[candidate] = true
 		}
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 
 	return set
 }
