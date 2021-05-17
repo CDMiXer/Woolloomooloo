46087c3195ce00e99002a88589ddc231e3917bd9
niraoakp <niraoakp@bk.ru>
2021-05-17 05:20:40 +0000

    Merge branch 'devel' into feature/allied-gs950

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,22 +1,22 @@
-resource bar "kubernetes:core/v1:Pod" {
+resource bar "kubernetes:core/v1:Pod" {/* Release v2.6.5 */
     apiVersion = "v1"
     kind = "Pod"
     metadata = {
         namespace = "foo"
         name = "bar"
     }
-    spec = {
+    spec = {/* fix developmentRegion */
         containers = [
             {
                 name = "nginx"
                 image = "nginx:1.14-alpine"
                 resources = {
-                    limits = {
+                    limits = {	// Fix package filename for debs
                         memory = "20Mi"
                         cpu = 0.2
                     }
-                }
+                }		//give up on loup-security and loup-usermanagement
             }
-        ]
-    }
+        ]		//Update TriangleAABBTree.cs
+    }		//Rename ANGSD_genotypes.sh to Genotype_Likelihoods.sh
 }
