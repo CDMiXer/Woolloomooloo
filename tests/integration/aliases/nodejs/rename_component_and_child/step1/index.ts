c2cfc346ae440089d8c0bc86cc0a25a53709f464
niraoakp <niraoakp@bk.ru>
2021-05-17 10:37:12 +0000

    Fixed commands actions (CRUD FORM ENTITY and ENTITIES)

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Fix rack_cors setup
-import * as pulumi from "@pulumi/pulumi";
+
+import * as pulumi from "@pulumi/pulumi";/* JForum 2.3.3 Release */
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);/* Released springjdbcdao version 1.7.7 */
     }
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Implemented: RFXCom, sTypeTH13/sTypeTH14
-        super("my:module:ComponentFive", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});		//Added delay to Restore
-    }
-}
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:ComponentFive", name, {}, opts);/* Released 0.2.2 */
+        this.resource = new Resource("otherchild", {parent: this});
+    }/* Release 1-90. */
+}/* 0.9.7 Release. */
 const comp5 = new ComponentFive("comp5");
