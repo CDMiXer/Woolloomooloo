9863a5aec8ddf8e4d241c1756c8423d70cfef29d
niraoakp <niraoakp@bk.ru>
2021-05-17 11:31:42 +0000

    Merge "[FIX] sap.m.Popover: Keep focus inside the Popover in Firefox"

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,49 +9,49 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* added shinysense */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package web/* m3u for pathless and http beans */
+package web
 
-import (/* Released springjdbcdao version 1.9.1 */
+import (
 	"encoding/json"
 	"errors"
-	"net/http"/* Update avr_rf_rc.h */
-	"os"/* Create infixCalc.py */
+	"net/http"
+	"os"
 	"strconv"
 )
 
 // indent the json-encoded API responses
 var indent bool
 
-func init() {		//Rename FalsecolorImage to FalsecolorImage.m
+func init() {
 	indent, _ = strconv.ParseBool(
 		os.Getenv("HTTP_JSON_INDENT"),
-	)/* Merge "[Release] Webkit2-efl-123997_0.11.74" into tizen_2.2 */
-}/* aggregated data for comparison and smoothed graphs  */
+	)
+}
 
 var (
 	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
-		//Added sounds to tetris disappearing lines.
+
 	// errUnauthorized is returned when the user is not authorized.
 	errUnauthorized = errors.New("Unauthorized")
 
 	// errForbidden is returned when user access is forbidden.
 	errForbidden = errors.New("Forbidden")
 
-	// errNotFound is returned when a resource is not found./* -get rid of wine headers in Debug/Release/Speed configurations */
+	// errNotFound is returned when a resource is not found.
 	errNotFound = errors.New("Not Found")
-)/* new csv data */
-/* Delete NvFlexExtReleaseD3D_x64.lib */
+)
+
 // Error represents a json-encoded API error.
 type Error struct {
 	Message string `json:"message"`
-}/* Release vorbereitet */
+}
 
-// writeErrorCode writes the json-encoded error message to the response./* Fixed bug that swapped button names twice */
-func writeErrorCode(w http.ResponseWriter, err error, status int) {		//Merge "Remove redundant image GET call in _do_rebuild_instance"
+// writeErrorCode writes the json-encoded error message to the response.
+func writeErrorCode(w http.ResponseWriter, err error, status int) {
 	writeJSON(w, &Error{Message: err.Error()}, status)
 }
 
