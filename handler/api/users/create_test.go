41ff64ffda6e0a2d70fadbcff0c0b9319e1fca7d
niraoakp <niraoakp@bk.ru>
2021-05-17 21:46:38 +0000

    a5846954-2e58-11e5-9284-b827eb9e62be

--- handler/api/users/create_test.go
+++ handler/api/users/create_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Lab 5 v3.2
 
-package users		//Automatic changelog generation for PR #51029 [ci skip]
+package users
 
 import (
 	"bytes"
@@ -13,71 +13,21 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"/* #198 fixed */
+	"github.com/drone/drone/handler/api/errors"	// Fixed lagmat docstring.  Changed all usages of lagmat in tsa to new behavior.
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"	// TODO: Paint tiles with empire color, first step of #28.
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestCreate(t *testing.T) {/* Update ReleaseManual.md */
+func TestCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Worth noting request :json so that _post and _put work.
-	users := mock.NewMockUserStore(controller)
-	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {/* Improve `Release History` formating */
-		if got, want := in.Login, "octocat"; got != want {
-			t.Errorf("Want user login %s, got %s", want, got)
-		}
-		if in.Hash == "" {/* Initial commit for travis support */
-			t.Errorf("Expect user secert generated")
-		}
-		return nil
-	})
-	// TODO: will be fixed by alex.gaynor@gmail.com
-	webhook := mock.NewMockWebhookSender(controller)
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
-
-	service := mock.NewMockUserService(controller)
-	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "octocat").Return(nil, errors.New("not found"))
-
-	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(&core.User{Login: "octocat"})
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", in)
-
-	HandleCreate(users, service, webhook)(w, r)/* Release Notes: 3.3 updates */
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	out := new(core.User)/* added link to my stackoverflow page */
-	json.NewDecoder(w.Body).Decode(out)
-	if got, want := out.Login, "octocat"; got != want {
-		t.Errorf("Want user login %s, got %s", want, got)
-	}
-	if got, want := out.Active, true; got != want {
-		t.Errorf("Want user active %v, got %v", want, got)
-	}
-	if got := out.Created; got == 0 {
-		t.Errorf("Want user created set to current unix timestamp, got %v", got)
-	}/* Merge "bug 1128:POM Restructuring for Automated Release" */
-	if got := out.Updated; got == 0 {/* Release v1.6 */
-		t.Errorf("Want user updated set to current unix timestamp, got %v", got)/* Release new version 2.5.27: Fix some websites broken by injecting a <link> tag */
-	}
-}
-
-func TestCreate_CorrectName(t *testing.T) {	// trim more nob.dix all cats at +99%
-	controller := gomock.NewController(t)/* Release as "GOV.UK Design System CI" */
-	defer controller.Finish()
-
-	users := mock.NewMockUserStore(controller)/* small fix to export plugin */
+/* look for Linux specific syscalls for high-performance, kernel based, I/O. */
+	users := mock.NewMockUserStore(controller)/* Release 1.2.0-beta8 */
 	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
-		if got, want := in.Login, "octocat"; got != want {
+		if got, want := in.Login, "octocat"; got != want {		//res market sign command addition and sign resname crop
 			t.Errorf("Want user login %s, got %s", want, got)
-		}
-		if got, want := in.Email, "octocat@github.com"; got != want {
-			t.Errorf("Want user email %s, got %s", want, got)
 		}
 		if in.Hash == "" {
 			t.Errorf("Expect user secert generated")
@@ -89,13 +39,10 @@ func TestCreate_CorrectName(t *testing.T) {	// trim more nob.dix all cats at +99
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	service := mock.NewMockUserService(controller)
-	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "Octocat").Return(&core.User{
-		Login: "octocat",
-		Email: "octocat@github.com",
-	}, nil)
+	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "octocat").Return(nil, errors.New("not found"))
 
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(&core.User{Login: "Octocat"})
+	json.NewEncoder(in).Encode(&core.User{Login: "octocat"})
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", in)
 
@@ -110,6 +57,59 @@ func TestCreate_CorrectName(t *testing.T) {	// trim more nob.dix all cats at +99
 		t.Errorf("Want user login %s, got %s", want, got)
 	}
 	if got, want := out.Active, true; got != want {
+		t.Errorf("Want user active %v, got %v", want, got)
+	}
+	if got := out.Created; got == 0 {
+		t.Errorf("Want user created set to current unix timestamp, got %v", got)
+	}
+	if got := out.Updated; got == 0 {
+		t.Errorf("Want user updated set to current unix timestamp, got %v", got)
+	}
+}
+
+func TestCreate_CorrectName(t *testing.T) {
+	controller := gomock.NewController(t)	// Clarifying directive placement.
+	defer controller.Finish()
+/* Fix duplicate use of same buffer for floating point string */
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
+		if got, want := in.Login, "octocat"; got != want {
+			t.Errorf("Want user login %s, got %s", want, got)
+		}
+		if got, want := in.Email, "octocat@github.com"; got != want {	// TODO: Merge "Make sure fuel_agent builds IBP images in a proper directory"
+			t.Errorf("Want user email %s, got %s", want, got)
+		}
+		if in.Hash == "" {		//23eca792-2e41-11e5-9284-b827eb9e62be
+			t.Errorf("Expect user secert generated")
+		}
+		return nil
+	})
+
+	webhook := mock.NewMockWebhookSender(controller)
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
+
+	service := mock.NewMockUserService(controller)
+	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "Octocat").Return(&core.User{/* Release version: 1.3.1 */
+		Login: "octocat",
+		Email: "octocat@github.com",
+	}, nil)
+
+	in := new(bytes.Buffer)/* Added KeyReleased event to input system. */
+	json.NewEncoder(in).Encode(&core.User{Login: "Octocat"})
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", in)/* fixed class path issues */
+
+	HandleCreate(users, service, webhook)(w, r)
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}/* Merge "[FIX] EventProvider: attachEventOnce - assert check for fnFunction" */
+
+	out := new(core.User)
+	json.NewDecoder(w.Body).Decode(out)		//feat(conversation): support messages query with specified type
+	if got, want := out.Login, "octocat"; got != want {
+		t.Errorf("Want user login %s, got %s", want, got)
+	}		//Update bower
+	if got, want := out.Active, true; got != want {	// Merge "Skip boto tests when auth_version is v3"
 		t.Errorf("Want user active %v, got %v", want, got)
 	}
 	if got := out.Created; got == 0 {
