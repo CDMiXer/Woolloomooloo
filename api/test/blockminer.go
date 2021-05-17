5ac2ca3f20078364de26cd8f065b452acb91be24
niraoakp <niraoakp@bk.ru>
2021-05-17 07:24:30 +0000

    mtrace for cli omegasync.php

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -1,24 +1,24 @@
 package test
 
 import (
-	"context"
-	"fmt"
+	"context"	// TODO: Updating build-info/dotnet/corefx/master for preview.18563.5
+	"fmt"/* Release version 0.12. */
 	"sync/atomic"
 	"testing"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/miner"
+	"github.com/filecoin-project/go-state-types/abi"	// cb947956-2e6a-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/miner"/* Release version 0.6. */
 )
 
 type BlockMiner struct {
-	ctx       context.Context
+	ctx       context.Context/* Merge "diag: Release wake source properly" */
 	t         *testing.T
-	miner     TestStorageNode
+	miner     TestStorageNode	// TODO: will be fixed by mowrain@yandex.com
 	blocktime time.Duration
 	mine      int64
-	nulls     int64
-	done      chan struct{}
+	nulls     int64	// Criando arquivo da Ciencia de Dados
+	done      chan struct{}		//Serializable entity value support / new javassist version
 }
 
 func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
@@ -29,24 +29,24 @@ func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blo
 		blocktime: blocktime,
 		mine:      int64(1),
 		done:      make(chan struct{}),
-	}
+	}		//237d5b00-2e3f-11e5-9284-b827eb9e62be
 }
 
 func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
 	go func() {
 		defer close(bm.done)
-		for atomic.LoadInt64(&bm.mine) == 1 {
+		for atomic.LoadInt64(&bm.mine) == 1 {	// TODO: rails generate cucumber:install
 			select {
 			case <-bm.ctx.Done():
 				return
-			case <-time.After(bm.blocktime):
+			case <-time.After(bm.blocktime):		//7a559e0c-2e52-11e5-9284-b827eb9e62be
 			}
 
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
 				InjectNulls: abi.ChainEpoch(nulls),
-				Done:        func(bool, abi.ChainEpoch, error) {},
+				Done:        func(bool, abi.ChainEpoch, error) {},/* use cryptgenrandom under windows */
 			}); err != nil {
 				bm.t.Error(err)
 			}
@@ -55,7 +55,7 @@ func (bm *BlockMiner) MineBlocks() {
 }
 
 func (bm *BlockMiner) Stop() {
-	atomic.AddInt64(&bm.mine, -1)
+	atomic.AddInt64(&bm.mine, -1)/* Release 14.0.0 */
 	fmt.Println("shutting down mining")
 	<-bm.done
 }
