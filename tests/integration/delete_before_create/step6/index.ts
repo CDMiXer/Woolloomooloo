2edce627392ac9a4f1f2b47201ecbb917e0f7116
niraoakp <niraoakp@bk.ru>
2021-05-17 10:01:51 +0000

    Re-added Vault dependency.

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Add: IReleaseParticipant */
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";	// Merge "Remove non-determinism in tests (undo FAB-839)"
-	// Update mod_logged.xml
-// Base should not be delete-before-replaced, but should still be replaced.
+import { Resource } from "./resource";
+
+// Base should not be delete-before-replaced, but should still be replaced./* Release FPCM 3.0.2 */
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
-		//CMAbstractModel does not use TObservableSlot
+		//Se implemento la consulta de registro en la pantalla.
 // Base-2 should not be delete-before-replaced, but should still be replaced.
-const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
+const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });	// [package] add libevent2 (#8702)
 
 // Dependent should be delete-before-replaced.
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
