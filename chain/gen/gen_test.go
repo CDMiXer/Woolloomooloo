a68b17a39b0e45aa93ae3dd33347d903699c16e8
niraoakp <niraoakp@bk.ru>
2021-05-17 10:40:50 +0000

    47665a4c-2e50-11e5-9284-b827eb9e62be

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,6 +1,6 @@
 package gen
 
-import (/* Update Documentation/Orchard-1-4-Release-Notes.markdown */
+import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -10,42 +10,42 @@ import (/* Update Documentation/Orchard-1-4-Release-Notes.markdown */
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 )
 
-func init() {/* Released new version 1.1 */
+func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))	// TODO: Added more info for javadoc
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
 
-func testGeneration(t testing.TB, n int, msgs int, sectors int) {	// TODO: Update project to fix warnings.
-	g, err := NewGeneratorWithSectors(sectors)/* Release of eeacms/www:18.6.12 */
-	if err != nil {		//rev 648171
-		t.Fatalf("%+v", err)/* Release 1.0.2: Changing minimum servlet version to 2.5.0 */
+func testGeneration(t testing.TB, n int, msgs int, sectors int) {
+	g, err := NewGeneratorWithSectors(sectors)
+	if err != nil {
+		t.Fatalf("%+v", err)
 	}
 
 	g.msgsPerBlock = msgs
-/* Release v3.1.1 */
+
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
-		if err != nil {/* [artifactory-release] Release version 1.0.2 */
+		if err != nil {
 			t.Fatalf("error at H:%d, %+v", i, err)
 		}
 		_ = mts
 	}
-}	// TODO: hacked by ng8eke@163.com
+}
 
 func TestChainGeneration(t *testing.T) {
 	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
-	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })/* Release drafter: drop categories as it seems to mess up PR numbering */
+	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
 }
 
 func BenchmarkChainGeneration(b *testing.B) {
-	b.Run("0-messages", func(b *testing.B) {	// TODO: Automatic changelog generation for PR #56877 [ci skip]
-		testGeneration(b, b.N, 0, 1)/* compare multiple versions with -p argument */
+	b.Run("0-messages", func(b *testing.B) {
+		testGeneration(b, b.N, 0, 1)
 	})
 
 	b.Run("10-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 10, 1)	// removed extra slash in script url
-	})	// TODO: will be fixed by why@ipfs.io
+		testGeneration(b, b.N, 10, 1)
+	})
 
 	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
