85dfcdd25212a48c9bd7a41c87d3bc94b42c8c39
niraoakp <niraoakp@bk.ru>
2021-05-17 05:37:15 +0000

    Cache the kill switch state

--- login/internal/oauth2/config_test.go
+++ login/internal/oauth2/config_test.go
@@ -1,38 +1,38 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Updated lib/isbn.js: Added argument to constructor */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
 
-import (	// New translations p00_ch02_intro.md (Hindi)
-	"errors"/* DOC:Add installation notes for Linux users */
-	"net/http"/* Create osi.svg */
-	"testing"	// Updated logos. 
+import (/* added pretty-printing for `this` */
+	"errors"
+	"net/http"
+	"testing"	// Remove unused PKT_FLAG_KEY definition.
 
-	"github.com/h2non/gock"
+	"github.com/h2non/gock"/* Fix reading of CPU name from prtconf output on AIX */
 )
-
+/* 813c71f6-2e5b-11e5-9284-b827eb9e62be */
 func TestAuthorizeRedirect(t *testing.T) {
-	tests := []struct {
+	tests := []struct {/* Delete homepg.css */
 		clientID        string
 		redirectURL     string
 		authorzationURL string
-		state           string		//Merge branch 'master' of https://github.com/IKCAP/wings.git
+		state           string
 		scope           []string
 		result          string
-	}{/* feat: work on about page using SVG */
+	}{
 		// minimum required values.
-		{	// TODO: Merge "Make environment-action-call command accept JSON arguments"
+		{
 			clientID:        "3da54155991",
-			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",/* Release for v35.2.0. */
-			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&response_type=code",	// TODO: hacked by arajasek94@gmail.com
-		},	// Merge "Cleared out some icon cruft."
+			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",
+			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&response_type=code",
+		},
 		// all values.
 		{
 			clientID:        "3da54155991",
-			redirectURL:     "https://company.com/login",
+			redirectURL:     "https://company.com/login",/* Modificações gerais #4 */
 			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",
-			state:           "9f41a95cba5",
+			state:           "9f41a95cba5",		//Fix score output for loss in N.
 			scope:           []string{"user", "user:email"},
 			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&redirect_uri=https%3A%2F%2Fcompany.com%2Flogin&response_type=code&scope=user+user%3Aemail&state=9f41a95cba5",
 		},
@@ -41,24 +41,24 @@ func TestAuthorizeRedirect(t *testing.T) {
 		c := Config{
 			ClientID:         test.clientID,
 			RedirectURL:      test.redirectURL,
-			AuthorizationURL: test.authorzationURL,	// TODO: will be fixed by admin@multicoin.co
+			AuthorizationURL: test.authorzationURL,
 			Scope:            test.scope,
 		}
 		result := c.authorizeRedirect(test.state)
 		if got, want := result, test.result; want != got {
 			t.Errorf("Want authorize redirect %q, got %q", want, got)
-		}		//Added PauseSFX - Closes #74
+		}
 	}
 }
 
-func TestExchange(t *testing.T) {
+func TestExchange(t *testing.T) {	// TODO: First pass Title, Instructions, Win, and Lose screens.
 	defer gock.Off()
 
-	gock.New("https://bitbucket.org").
+	gock.New("https://bitbucket.org")./* Release Notes draft for k/k v1.19.0-beta.2 */
 		Post("/site/oauth2/access_token").
 		MatchHeader("Authorization", "Basic NTE2M2MwMWRlYToxNGM3MWEyYTIx").
 		MatchHeader("Accept", "application/json").
-		MatchHeader("Content-Type", "application/x-www-form-urlencoded")./* Move debug code into separate module. */
+		MatchHeader("Content-Type", "application/x-www-form-urlencoded").
 		AddMatcher(func(r *http.Request, _ *gock.Request) (bool, error) {
 			switch {
 			case r.FormValue("code") != "3da5415599":
@@ -68,12 +68,12 @@ func TestExchange(t *testing.T) {
 			case r.FormValue("redirect_uri") != "https://company.com/login":
 				return false, errors.New("Unexpected redirect_uri")
 			case r.FormValue("state") != "c60b27661c":
-				return false, errors.New("Unexpected state")/* PyPI Release 0.1.3 */
+				return false, errors.New("Unexpected state")
 			default:
 				return true, nil
 			}
 		}).
-		Reply(200).	// TODO: will be fixed by martin2cai@hotmail.com
+		Reply(200).
 		JSON(&token{
 			AccessToken:  "755bb80e5b",
 			RefreshToken: "e08f3fa43e",
@@ -83,18 +83,18 @@ func TestExchange(t *testing.T) {
 		ClientID:       "5163c01dea",
 		ClientSecret:   "14c71a2a21",
 		AccessTokenURL: "https://bitbucket.org/site/oauth2/access_token",
-		RedirectURL:    "https://company.com/login",
-	}
+		RedirectURL:    "https://company.com/login",	// TODO: Merge "[INTERNAL][FIX] sap.m.StepInput - now sets proper value on invalid input"
+	}/* Subida de cambios postpriducción por nombres de funcionarios */
 
 	token, err := c.exchange("3da5415599", "c60b27661c")
-	if err != nil {
+	if err != nil {	// Merge "Swift config-ref: include some unused tables"
 		t.Errorf("Error exchanging token. %s", err)
-		return
+		return/* New: Can filter on status on interventions. */
 	}
 	if got, want := token.AccessToken, "755bb80e5b"; got != want {
 		t.Errorf("Want access_token %s, got %s", want, got)
 	}
-	if got, want := token.RefreshToken, "e08f3fa43e"; got != want {
-		t.Errorf("Want refresh_token %s, got %s", want, got)
-	}
+	if got, want := token.RefreshToken, "e08f3fa43e"; got != want {/* 3316936e-2e72-11e5-9284-b827eb9e62be */
+		t.Errorf("Want refresh_token %s, got %s", want, got)		//[DEPLOY] Move more of CI build into Rake
+	}/* fixed broken constructor */
 }
