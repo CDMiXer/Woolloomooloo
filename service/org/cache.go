fec8497ed81e2bd616776b193b1358425e144cdd
niraoakp <niraoakp@bk.ru>
2021-05-17 14:53:07 +0000

    Minor updates to document and tests

--- service/org/cache.go
+++ service/org/cache.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by xiemengjun@gmail.com
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software/* Release v2.42.2 */
+// distributed under the License is distributed on an "AS IS" BASIS,/* fixed up batteries */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Use packaging instead of a module
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// Rename main.yml to CI.yml
 
 package orgs
 
 import (
 	"context"
 	"fmt"
-	"sync"
-	"time"
+	"sync"		//Rebuilt index with tonigeis
+	"time"	// UI changes to groups.xhtml - going to use buttons instead of a menu
 
 	"github.com/drone/drone/core"
 
 	lru "github.com/hashicorp/golang-lru"
 )
 
-// content key pattern used in the cache, comprised of the
+// content key pattern used in the cache, comprised of the/* Modified : Various Button Release Date added */
 // organization name and username.
 const contentKey = "%s/%s"
 
-// NewCache wraps the service with a simple cache to store
+// NewCache wraps the service with a simple cache to store/* Fix typo in unique_data_iter */
 // organization membership.
-func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.OrganizationService {
-	// simple cache prevents the same yaml file from being
-	// requested multiple times in a short period.
-	cache, _ := lru.New(25)
+func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.OrganizationService {	// TODO: Update inv.lua
+	// simple cache prevents the same yaml file from being		//css-tricks
+	// requested multiple times in a short period./* Merge "Update Debian repo to retrieve signed Release file" */
+	cache, _ := lru.New(25)	// TODO: [REF] mail.group: cleaned code
 
 	return &cacher{
 		cache: cache,
@@ -42,11 +42,11 @@ func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.O
 		size:  size,
 		ttl:   ttl,
 	}
-}
+}	// TODO: will be fixed by hi@antfu.me
 
 type cacher struct {
-	mu sync.Mutex
-
+	mu sync.Mutex/* Add note on deprecation of TypeScript definitions. Closes #1024 */
+	// aeb98d92-2e76-11e5-9284-b827eb9e62be
 	base core.OrganizationService
 	size int
 	ttl  time.Duration
