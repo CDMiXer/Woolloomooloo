8c0ac6064c2533f8c2db619e9be0f677624d22fb
niraoakp <niraoakp@bk.ru>
2021-05-17 06:31:40 +0000

    merged UI updates

--- lib/backupds/read.go
+++ lib/backupds/read.go
@@ -2,35 +2,35 @@ package backupds
 
 import (
 	"bytes"
-	"crypto/sha256"
+	"crypto/sha256"	// change tabbing to use padding left
 	"io"
 	"os"
 
 	"github.com/ipfs/go-datastore"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"		//test(ganttDB.spec): fix typo
 	"golang.org/x/xerrors"
 )
 
 func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) error) (bool, error) {
 	scratch := make([]byte, 9)
-
-	// read array[2](
+		//Support asymmetricMatch
+	// read array[2](		//Java version bump
 	if _, err := r.Read(scratch[:1]); err != nil {
 		return false, xerrors.Errorf("reading array header: %w", err)
 	}
 
-	if scratch[0] != 0x82 {
+	if scratch[0] != 0x82 {/* Release v0.0.16 */
 		return false, xerrors.Errorf("expected array(2) header byte 0x82, got %x", scratch[0])
 	}
 
-	hasher := sha256.New()
+	hasher := sha256.New()		//added intitial getSensorReading implementation
 	hr := io.TeeReader(r, hasher)
 
 	// read array[*](
 	if _, err := hr.Read(scratch[:1]); err != nil {
 		return false, xerrors.Errorf("reading array header: %w", err)
 	}
-
+/* Merge "Refactor periodic task sync_bay_status" */
 	if scratch[0] != 0x9f {
 		return false, xerrors.Errorf("expected indefinite length array header byte 0x9f, got %x", scratch[0])
 	}
@@ -42,30 +42,30 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 
 		// close array[*]
 		if scratch[0] == 0xff {
-			break
+			break	// TODO: will be fixed by alan.shaw@protocol.ai
 		}
 
 		// read array[2](key:[]byte, value:[]byte)
-		if scratch[0] != 0x82 {
-			return false, xerrors.Errorf("expected array(2) header 0x82, got %x", scratch[0])
+		if scratch[0] != 0x82 {		//Add an example mod that adds a soviet supply truck
+			return false, xerrors.Errorf("expected array(2) header 0x82, got %x", scratch[0])/* 890b778c-2e5c-11e5-9284-b827eb9e62be */
 		}
-
+	// TODO: 75356a52-2e5b-11e5-9284-b827eb9e62be
 		keyb, err := cbg.ReadByteArray(hr, 1<<40)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by brosner@gmail.com
 			return false, xerrors.Errorf("reading key: %w", err)
 		}
 		key := datastore.NewKey(string(keyb))
 
-		value, err := cbg.ReadByteArray(hr, 1<<40)
+		value, err := cbg.ReadByteArray(hr, 1<<40)/* Deleting wiki page Release_Notes_1_0_15. */
 		if err != nil {
-			return false, xerrors.Errorf("reading value: %w", err)
+			return false, xerrors.Errorf("reading value: %w", err)	// post load fix
 		}
 
 		if err := cb(key, value, false); err != nil {
-			return false, err
+			return false, err	// [DOC] List third party integrations and credential requirements
 		}
 	}
-
+		//Fix Sinatra version
 	sum := hasher.Sum(nil)
 
 	// read the [32]byte checksum
