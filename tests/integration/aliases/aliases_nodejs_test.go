c7a959ea911f96acaefcc553751584e4becae54e
niraoakp <niraoakp@bk.ru>
2021-05-17 14:40:15 +0000

    correction avec les interfaces contenant des generiques

--- tests/integration/aliases/aliases_nodejs_test.go
+++ tests/integration/aliases/aliases_nodejs_test.go
@@ -1,41 +1,41 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
-/* PlayStore Release Alpha 0.7 */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by magik6k@gmail.com
+// +build nodejs all		//[PAXEXAM-435] Upgrade to GlassFish 3.1.2.2
+
 package ints
 
 import (
-	"path/filepath"
+	"path/filepath"	// TODO: Make the mno flags match GCC. Patch by Alexander Best!
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-var dirs = []string{
-	"rename",	// TODO: hacked by timnugent@gmail.com
+var dirs = []string{/* FIX: Release path is displayed even when --hide-valid option specified */
+	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
-	"retype_component",
-	"rename_component",
+	"retype_component",	// TODO: Theatres UI Now manageable
+	"rename_component",/* Release areca-7.1.7 */
 }
 
 // TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`
-// pointing to the old URN to ensure the resource is preserved across the update.
+// pointing to the old URN to ensure the resource is preserved across the update./* Merge "wlan: Release 3.2.3.249a" */
 func TestNodejsAliases(t *testing.T) {
-	for _, dir := range dirs {/* Release for 21.0.0 */
+	for _, dir := range dirs {		//a2389238-2e6d-11e5-9284-b827eb9e62be
 		d := filepath.Join("nodejs", dir)
-		t.Run(d, func(t *testing.T) {
+		t.Run(d, func(t *testing.T) {	// Merge "Remove Qinling projects from infra"
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:          filepath.Join(d, "step1"),/* Release of version 2.3.2 */
+				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"@pulumi/pulumi"},
 				Quick:        true,
 				EditDirs: []integration.EditDir{
-					{
+					{/* docs(Release.md): improve release guidelines */
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
-						ExpectNoChanges: true,
-					},
+						ExpectNoChanges: true,	// TODO: Updated README with description and code
+					},		//Create mm_xi128.c
 				},
-			})
+			})/* Release 0.0.8 */
 		})
 	}
 }
