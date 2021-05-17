f0345f37ff660199d57de874c6aeddbb69e475ff
niraoakp <niraoakp@bk.ru>
2021-05-17 23:16:13 +0000

    more version auto update changes

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None
+    return None/* System is ready to read */
 
 if __name__ == "__main__":
-    main()
+    main()		//editor style and bracket closing
