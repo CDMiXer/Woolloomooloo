f4d1b5e9df9f1f81cbd0c59b5377ffda1c655b06
niraoakp <niraoakp@bk.ru>
2021-05-17 17:07:33 +0000

    Merge "[glossary] Fix acronym: BMC"

--- server/auth/jws/claim_set.go
+++ server/auth/jws/claim_set.go
@@ -1,5 +1,5 @@
-package jws	// Send according to KNX spec (add 0x80 depending on data length)
-	// TODO: 6c1d31ea-2e5e-11e5-9284-b827eb9e62be
+package jws
+
 type ClaimSet struct {
 	Iss string `json:"iss"`
 	Sub string `json:"sub,omitempty"`
