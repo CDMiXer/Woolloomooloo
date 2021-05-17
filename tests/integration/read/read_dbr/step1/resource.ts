3c852abd4dbb1c02ecab46b73dcdb7c17629474d
niraoakp <niraoakp@bk.ru>
2021-05-17 05:57:18 +0000

    no comm through obstacles, CMC and MC choose initial random quadrant

--- tests/integration/read/read_dbr/step1/resource.ts
+++ tests/integration/read/read_dbr/step1/resource.ts
@@ -1,31 +1,31 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release of eeacms/forests-frontend:2.0-beta.7 */
-//		//Delete prova1
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Even more typo fixing! */
-//		//Merge "Move wakelock release to handleMessage" into klp-modular-dev
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Improved ref to message ID stuff
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";	// Update bmp180_rpi.h
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-	// TODO: Merge "channels/pjsip: Add memcheck-delay-stop."
+
     private id: number = 0;
-/* UseListOrder: Create a struct around OrderMap, NFC */
+
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
         }
     }
-/* Added support for specifying commit-id for remote operations */
+
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
@@ -35,10 +35,10 @@ export class Provider implements dynamic.ResourceProvider {
             };
         }
 
-        return {	// Add a TODO test case
-            changes: false,		//Check that each reigon is a phyiscal area.
+        return {
+            changes: false,
         }
-    }		//Update Tengu.java
+    }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
@@ -49,13 +49,13 @@ export class Provider implements dynamic.ResourceProvider {
 
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
-}    
+    }
 
-    public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {		//copy sketch from wiki
+    public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
-            id: id,/* Added some more commenting. */
+            id: id,
             props: props,
-        }		//TestChangeProperty tests from test_requests_le.py were fixed for Py3
+        }
     }
 }
 
