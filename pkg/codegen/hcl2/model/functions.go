aecd453318dad68105ea0f69036a43bdd243cd4b
niraoakp <niraoakp@bk.ru>
2021-05-17 22:08:05 +0000

    Changed default logging behavior.

--- pkg/codegen/hcl2/model/functions.go
+++ pkg/codegen/hcl2/model/functions.go
@@ -1,44 +1,44 @@
-// Copyright 2016-2020, Pulumi Corporation.		//made delete cpyctors private
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 1.16 */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release chart 2.1.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//version and name issues for build corrected
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package model
 
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"		//Super if not found.
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
-/* TASK: Delete .travis.yml */
-// FunctionSignature represents a possibly-type-polymorphic function signature.	// TODO: hacked by nagydani@epointsystem.org
+
+// FunctionSignature represents a possibly-type-polymorphic function signature.
 type FunctionSignature interface {
 	// GetSignature returns the static signature for the function when invoked with the given arguments.
 	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
-}/* Merge branch 'master' into pyup-update-seaborn-0.8.1-to-0.9.0 */
+}
 
-// Parameter represents a single function parameter.	// TODO: form -> $form
+// Parameter represents a single function parameter.
 type Parameter struct {
 	Name string // The name of the parameter.
 	Type Type   // The type of the parameter.
 }
 
 // StaticFunctionSignature records the parameters and return type of a function.
-type StaticFunctionSignature struct {/* Update Scan.md */
-	// The function's fixed parameters./* Initial Release to Git */
+type StaticFunctionSignature struct {
+	// The function's fixed parameters.
 	Parameters []Parameter
 	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be
 	// assignable to this parameter.
-	VarargsParameter *Parameter		//Clear channel/server lists and rejoin channels on reconnect (fixes #14)
+	VarargsParameter *Parameter
 	// The return type of the function.
 	ReturnType Type
 }
@@ -63,13 +63,13 @@ type Function struct {
 }
 
 // NewFunction creates a new function with the given signature.
-func NewFunction(signature FunctionSignature) *Function {	// TODO: will be fixed by brosner@gmail.com
+func NewFunction(signature FunctionSignature) *Function {
 	return &Function{signature: signature}
 }
 
 // SyntaxNode returns the syntax node for the function, which is always syntax.None.
-func (f *Function) SyntaxNode() hclsyntax.Node {/* Release 9 - chef 14 or greater */
-	return syntax.None/* Add Release Branch */
+func (f *Function) SyntaxNode() hclsyntax.Node {
+	return syntax.None
 }
 
 // Traverse attempts to traverse the function definition. This will always fail: functions are not traversable.
@@ -78,6 +78,6 @@ func (f *Function) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnosti
 }
 
 // GetSignature returns the static signature of the function when it is invoked with the given arguments.
-func (f *Function) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {	// TODO: Hardcoded example values for array_rand().
+func (f *Function) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return f.signature.GetSignature(arguments)
 }
