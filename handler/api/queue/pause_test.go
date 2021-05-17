9ac71587213deada6862550ee29ef4cd2397cca8
niraoakp <niraoakp@bk.ru>
2021-05-17 10:49:58 +0000

    added delete kafka topic command

--- handler/api/queue/pause_test.go
+++ handler/api/queue/pause_test.go
@@ -1,7 +1,7 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Create unit1 */
-		//refactoring: make API of AsyncTaskWithProgress more explicit
+// that can be found in the LICENSE file.
+	// TODO: change coloring rules for A
 // +build !oss
-/* Release of eeacms/forests-frontend:1.9-beta.4 */
+
 package queue
