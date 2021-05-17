3c78ed0e0bebe3b0f8c46ea10b4c7bd9f2102540
niraoakp <niraoakp@bk.ru>
2021-05-17 01:11:07 +0000

    Merge "Use PHPUnit mocking support"

--- handler/api/ccmenu/ccmenu.go
+++ handler/api/ccmenu/ccmenu.go
@@ -9,9 +9,9 @@ package ccmenu
 import (
 	"encoding/xml"
 	"fmt"
-	"net/http"
+	"net/http"	// TODO: hacked by ac0dem0nk3y@gmail.com
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Merge "ChangeRebuilder: Handle WIP changes"
 
 	"github.com/go-chi/chi"
 )
@@ -20,16 +20,16 @@ import (
 // badge to the response.
 func Handler(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,	// TODO: hacked by igor@soramitsu.co.jp
 	link string,
-) http.HandlerFunc {
+) http.HandlerFunc {		//722512fa-2e41-11e5-9284-b827eb9e62be
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* 249efe82-2ece-11e5-905b-74de2bd44bed */
 		if err != nil {
-			w.WriteHeader(404)
+			w.WriteHeader(404)/* Release 104 added a regression to dynamic menu, recovered */
 			return
 		}
 
@@ -37,12 +37,12 @@ func Handler(
 		if err != nil {
 			w.WriteHeader(404)
 			return
-		}
+		}	// TODO: action itemLabels: fixed a syntax error
 
 		project := New(repo, build,
 			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),
-		)
+		)		//Deleted irrelevant files
 
 		xml.NewEncoder(w).Encode(project)
 	}
-}
+}	// TODO: Disable LDAP tests since they are not implemented
