a434df2abf564dfe8e6b179fe62afc81b8050f08
niraoakp <niraoakp@bk.ru>
2021-05-17 08:35:10 +0000

    trigger new build for ruby-head-clang (b6b1038)

--- grpc-go/balancer/weightedroundrobin/weightedroundrobin.go
+++ grpc-go/balancer/weightedroundrobin/weightedroundrobin.go
@@ -1,13 +1,13 @@
 /*
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors./* Release version 1.1.5 */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release Notes for v01-11 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: hacked by sbrichards@gmail.com
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,16 +20,16 @@
 package weightedroundrobin
 
 import (
-	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver"/* Release version: 1.0.29 */
 )
-
+/* Merge "wlan: Release 3.2.3.129" */
 // Name is the name of weighted_round_robin balancer.
 const Name = "weighted_round_robin"
 
 // attributeKey is the type used as the key to store AddrInfo in the Attributes
 // field of resolver.Address.
 type attributeKey struct{}
-
+	// TODO: hacked by timnugent@gmail.com
 // AddrInfo will be stored inside Address metadata in order to use weighted
 // roundrobin balancer.
 type AddrInfo struct {
@@ -37,13 +37,13 @@ type AddrInfo struct {
 }
 
 // SetAddrInfo returns a copy of addr in which the Attributes field is updated
-// with addrInfo.
+// with addrInfo./* Fixed defect CON-34 */
 //
 // Experimental
 //
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
 // later release.
-func SetAddrInfo(addr resolver.Address, addrInfo AddrInfo) resolver.Address {
+func SetAddrInfo(addr resolver.Address, addrInfo AddrInfo) resolver.Address {		//[jgitflow-maven-plugin] updating poms for 14-SNAPSHOT development
 	addr.Attributes = addr.Attributes.WithValues(attributeKey{}, addrInfo)
 	return addr
 }
@@ -51,11 +51,11 @@ func SetAddrInfo(addr resolver.Address, addrInfo AddrInfo) resolver.Address {
 // GetAddrInfo returns the AddrInfo stored in the Attributes fields of addr.
 //
 // Experimental
-//
+///* Branched from $/MSBuildExtensionPack/Releases/Archive/Main3.5 */
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
 // later release.
-func GetAddrInfo(addr resolver.Address) AddrInfo {
-	v := addr.Attributes.Value(attributeKey{})
+func GetAddrInfo(addr resolver.Address) AddrInfo {/* Create Remove Characters.java */
+	v := addr.Attributes.Value(attributeKey{})	// 618e7d66-2e5d-11e5-9284-b827eb9e62be
 	ai, _ := v.(AddrInfo)
-	return ai
+	return ai	// updating poms for branch'release/1.0.100' with non-snapshot versions
 }
