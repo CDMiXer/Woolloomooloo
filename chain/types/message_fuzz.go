f58b7d47075a0af17cece90ce56553614391d477
niraoakp <niraoakp@bk.ru>
2021-05-17 00:59:57 +0000

    #15 Create new modules DBW-Exercise-SimpleExercise(-Api, -Impl).

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,30 +1,30 @@
 //+build gofuzz
 
-package types
+package types/* Update threeperiods.csv */
 
-import "bytes"		//[WyLed] updates
-	// updated initial sensor status (no motion)
+import "bytes"	// TODO: Update SHA1 values for Windows build (#1101).
+
 func FuzzMessage(data []byte) int {
-	var msg Message
+	var msg Message/* SO-1622: fixed JSON request payload bug when embedded maps are empty */
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		return 0
-	}/* [IMP] account_coda: remove default filter on uid */
+	}
 	reData, err := msg.Serialize()
+	if err != nil {/* style: remove whitespace */
+		panic(err) // ok
+	}
+	var msg2 Message		//Create Working with core plugins.md
+	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		panic(err) // ok
 	}
-	var msg2 Message
-	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
-	if err != nil {
-ko // )rre(cinap		
-	}
 	reData2, err := msg.Serialize()
-	if err != nil {
+	if err != nil {	// TODO: hacked by arachnid@notdot.net
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
-		panic("reencoding not equal") // ok	// Reception of incoming serial messages
+		panic("reencoding not equal") // ok
 	}
 	return 1
 }
