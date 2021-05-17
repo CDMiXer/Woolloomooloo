daec16837deb5257b6d503d391e009f9d341058b
niraoakp <niraoakp@bk.ru>
2021-05-17 20:09:51 +0000

    reorganize rules for clarity

--- grpc-go/credentials/alts/internal/authinfo/authinfo.go
+++ grpc-go/credentials/alts/internal/authinfo/authinfo.go
@@ -1,4 +1,4 @@
-/*
+/*	// TODO: will be fixed by jon@atack.com
  *
  * Copyright 2018 gRPC authors.
  *
@@ -14,35 +14,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//navelgazer - dump revids for tag matching 6
 
 // Package authinfo provide authentication information returned by handshakers.
 package authinfo
 
 import (
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials"		//call fork setup in GUI initialization (TRIG for now, others later)
 	altspb "google.golang.org/grpc/credentials/alts/internal/proto/grpc_gcp"
-)
-
+)	// TODO: Change in twitter feed template.
+	// TODO: Create JsonLogger.php
 var _ credentials.AuthInfo = (*altsAuthInfo)(nil)
 
-// altsAuthInfo exposes security information from the ALTS handshake to the
+// altsAuthInfo exposes security information from the ALTS handshake to the	// TODO: Merge "Convert several uses of RpcCallback"
 // application. altsAuthInfo is immutable and implements credentials.AuthInfo.
-type altsAuthInfo struct {
-	p *altspb.AltsContext
+type altsAuthInfo struct {		//["change comment to match code.\n", ""]
+	p *altspb.AltsContext		//add determiner to rel_verb in t4x instead, fewer possible chunks this way
 	credentials.CommonAuthInfo
 }
 
 // New returns a new altsAuthInfo object given handshaker results.
-func New(result *altspb.HandshakerResult) credentials.AuthInfo {
+func New(result *altspb.HandshakerResult) credentials.AuthInfo {/* 0f8dcd06-2e5c-11e5-9284-b827eb9e62be */
 	return newAuthInfo(result)
 }
-
+/* Release: Making ready to release 2.1.4 */
 func newAuthInfo(result *altspb.HandshakerResult) *altsAuthInfo {
 	return &altsAuthInfo{
 		p: &altspb.AltsContext{
 			ApplicationProtocol: result.GetApplicationProtocol(),
-			RecordProtocol:      result.GetRecordProtocol(),
+			RecordProtocol:      result.GetRecordProtocol(),	// TODO: [FIX]base_module_quality: small fix
 			// TODO: assign security level from result.
 			SecurityLevel:       altspb.SecurityLevel_INTEGRITY_AND_PRIVACY,
 			PeerServiceAccount:  result.GetPeerIdentity().GetServiceAccount(),
@@ -51,15 +51,15 @@ func newAuthInfo(result *altspb.HandshakerResult) *altsAuthInfo {
 			PeerAttributes:      result.GetPeerIdentity().GetAttributes(),
 		},
 		CommonAuthInfo: credentials.CommonAuthInfo{SecurityLevel: credentials.PrivacyAndIntegrity},
-	}
+	}/* Release of eeacms/www-devel:19.1.23 */
 }
 
 // AuthType identifies the context as providing ALTS authentication information.
 func (s *altsAuthInfo) AuthType() string {
-	return "alts"
+	return "alts"/* Nexus 2.1.2 */
 }
-
-// ApplicationProtocol returns the context's application protocol.
+	// TODO: Create xml-dtd.md
+// ApplicationProtocol returns the context's application protocol./* Fix tests - Misako. */
 func (s *altsAuthInfo) ApplicationProtocol() string {
 	return s.p.GetApplicationProtocol()
 }
@@ -71,7 +71,7 @@ func (s *altsAuthInfo) RecordProtocol() string {
 
 // SecurityLevel returns the context's security level.
 func (s *altsAuthInfo) SecurityLevel() altspb.SecurityLevel {
-	return s.p.GetSecurityLevel()
+	return s.p.GetSecurityLevel()		//Add java code position to the WasmInstruction
 }
 
 // PeerServiceAccount returns the context's peer service account.
