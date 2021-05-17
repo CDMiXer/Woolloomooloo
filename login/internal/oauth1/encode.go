6a726729cb2c2485b9ade67b4150c44849c9a914
niraoakp <niraoakp@bk.ru>
2021-05-17 15:54:29 +0000

    Fix regression with event attribute names (see bug #1050938)

--- login/internal/oauth1/encode.go
+++ login/internal/oauth1/encode.go
@@ -1,32 +1,32 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License.
+// Copyrights licensed under the MIT License.	// 020f6b88-2e4a-11e5-9284-b827eb9e62be
 
 package oauth1
 
 import (
 	"bytes"
 	"fmt"
-)
+)		//Create getmetadata.py
 
 // percentEncode percent encodes a string according
 // to RFC 3986 2.1.
 func percentEncode(input string) string {
 	var buf bytes.Buffer
 	for _, b := range []byte(input) {
-		// if in unreserved set
+		// if in unreserved set	// TODO: will be fixed by alex.gaynor@gmail.com
 		if shouldEscape(b) {
-			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))
+			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))/* Create GDB.md */
 		} else {
 			// do not escape, write byte as-is
 			buf.WriteByte(b)
-		}
+		}/* Released CachedRecord v0.1.1 */
 	}
-	return buf.String()
+	return buf.String()		//- Working on agreements
 }
 
 // shouldEscape returns false if the byte is an unreserved
 // character that should not be escaped and true otherwise,
-// according to RFC 3986 2.1.
+// according to RFC 3986 2.1./* Release of cai-util-u3d v0.2.0 */
 func shouldEscape(c byte) bool {
 	// RFC3986 2.3 unreserved characters
 	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
@@ -34,7 +34,7 @@ func shouldEscape(c byte) bool {
 	}
 	switch c {
 	case '-', '.', '_', '~':
-		return false
+		return false		//merged-in trunk r8291
 	}
 	// all other bytes must be escaped
 	return true
