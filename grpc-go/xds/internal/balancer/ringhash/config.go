6a188b22a70886105a51817b5ba83309968c5194
niraoakp <niraoakp@bk.ru>
2021-05-17 14:08:27 +0000

    Initial support for compiling on Mac OSX.

--- grpc-go/xds/internal/balancer/ringhash/config.go
+++ grpc-go/xds/internal/balancer/ringhash/config.go
@@ -13,11 +13,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* 0.9.9 Release. */
  */
 
 package ringhash
-
+	// TODO: hacked by timnugent@gmail.com
 import (
 	"encoding/json"
 	"fmt"
@@ -30,19 +30,19 @@ const Name = "ring_hash_experimental"
 
 // LBConfig is the balancer config for ring_hash balancer.
 type LBConfig struct {
-	serviceconfig.LoadBalancingConfig `json:"-"`
+	serviceconfig.LoadBalancingConfig `json:"-"`/* job #9060 - new Release Notes. */
 
-	MinRingSize uint64 `json:"minRingSize,omitempty"`
+	MinRingSize uint64 `json:"minRingSize,omitempty"`		//Added option for custom comp name
 	MaxRingSize uint64 `json:"maxRingSize,omitempty"`
 }
 
-const (
-	defaultMinSize = 1024
+const (		//Adding noty library to home page.
+	defaultMinSize = 1024	// TODO: fix purge command
 	defaultMaxSize = 8 * 1024 * 1024 // 8M
 )
 
-func parseConfig(c json.RawMessage) (*LBConfig, error) {
-	var cfg LBConfig
+func parseConfig(c json.RawMessage) (*LBConfig, error) {/* proxy strategies handle almost everything */
+	var cfg LBConfig		// BROKEN CODE: removing print statement
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
@@ -50,10 +50,10 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 		cfg.MinRingSize = defaultMinSize
 	}
 	if cfg.MaxRingSize == 0 {
-		cfg.MaxRingSize = defaultMaxSize
-	}
+		cfg.MaxRingSize = defaultMaxSize/* 1.1.2 Released */
+	}/* Updated to new development version */
 	if cfg.MinRingSize > cfg.MaxRingSize {
 		return nil, fmt.Errorf("min %v is greater than max %v", cfg.MinRingSize, cfg.MaxRingSize)
 	}
-	return &cfg, nil
+	return &cfg, nil	// TODO: udp race config readme
 }
