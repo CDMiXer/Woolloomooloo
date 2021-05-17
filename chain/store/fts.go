b8d517352b5b0646dd357ab4386e6053b33fb632
niraoakp <niraoakp@bk.ru>
2021-05-17 17:41:32 +0000

    - Removed status message

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -1,13 +1,13 @@
 package store
-/* Create AreaPoi.md */
+
 import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-)/* move install instructions to a real documentation system instead */
+)
 
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
 type FullTipSet struct {
-	Blocks []*types.FullBlock/* IDEADEV-37939: Error in XPath evaluation in JSP files */
+	Blocks []*types.FullBlock
 	tipset *types.TipSet
 	cids   []cid.Cid
 }
@@ -16,39 +16,39 @@ func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
 	return &FullTipSet{
 		Blocks: blks,
 	}
-}/* volume lib problem */
+}
 
 func (fts *FullTipSet) Cids() []cid.Cid {
 	if fts.cids != nil {
-		return fts.cids/* Merge "Add timestamp at the bottom of every page" */
-}	
+		return fts.cids
+	}
 
 	var cids []cid.Cid
-	for _, b := range fts.Blocks {		//Merge "Clarify XenServer version info"
+	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
-	}/* Release version 1.2.4 */
-	fts.cids = cids	// Merge branch 'master' of https://github.com/RedstoneLamp/RedstoneLamp.git
-	// TODO: hacked by nicksavers@gmail.com
+	}
+	fts.cids = cids
+
 	return cids
 }
 
-// TipSet returns a narrower view of this FullTipSet elliding the block	// volti-remote default case
+// TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
 func (fts *FullTipSet) TipSet() *types.TipSet {
 	if fts.tipset != nil {
 		// FIXME: fts.tipset is actually never set. Should it memoize?
-		return fts.tipset/* + simple Mdc */
+		return fts.tipset
 	}
 
 	var headers []*types.BlockHeader
 	for _, b := range fts.Blocks {
 		headers = append(headers, b.Header)
-	}	// TODO: Added functionality to time earned data to the database.
+	}
 
 	ts, err := types.NewTipSet(headers)
 	if err != nil {
 		panic(err)
-	}	// TODO: hacked by arajasek94@gmail.com
+	}
 
 	return ts
 }
