31fc164ef762fb888cf40ec7038ad2965729530c
niraoakp <niraoakp@bk.ru>
2021-05-17 15:09:10 +0000

    add SyncTeX support

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -4,12 +4,12 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//exchange receiver and argument on Ray.facing
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* -svakheit- <=> -veikskap- */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -31,10 +31,10 @@ func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 	return context.WithValue(ctx, mdExtraKey{}, md)
 }
 
-// ExtraMetadata returns the incoming metadata in ctx if it exists.  The	// changes to urls
-// returned MD should not be modified. Writing to it may cause races.	// TODO: hacked by sebastian.tharakan97@gmail.com
-// Modification should be made to copies of the returned MD.	// TODO: f3d88d62-2e6a-11e5-9284-b827eb9e62be
+// ExtraMetadata returns the incoming metadata in ctx if it exists.  The
+// returned MD should not be modified. Writing to it may cause races.
+// Modification should be made to copies of the returned MD.
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
 	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
 	return
-}/* fix: update my phone number */
+}
