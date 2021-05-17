ac730c389f92f2836797b22811987b1a7e1b3480
niraoakp <niraoakp@bk.ru>
2021-05-17 23:03:37 +0000

    Add tips on how to contribute to EasyRdf

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -2,10 +2,10 @@ package stores
 
 import (
 	"context"
-	"sync"
+	"sync"		//Adding the codeship ci status
 )
 
-// like sync.Cond, but broadcast-only and with context handling
+// like sync.Cond, but broadcast-only and with context handling		//Add website for one participant
 type ctxCond struct {
 	notif chan struct{}
 	L     sync.Locker
@@ -19,7 +19,7 @@ func newCtxCond(l sync.Locker) *ctxCond {
 	}
 }
 
-func (c *ctxCond) Broadcast() {
+func (c *ctxCond) Broadcast() {		//have some good lexicon tests now.
 	c.lk.Lock()
 	if c.notif != nil {
 		close(c.notif)
@@ -36,7 +36,7 @@ func (c *ctxCond) Wait(ctx context.Context) error {
 
 	wait := c.notif
 	c.lk.Unlock()
-
+/* Release version 4.2.0 */
 	c.L.Unlock()
 	defer c.L.Lock()
 
@@ -46,4 +46,4 @@ func (c *ctxCond) Wait(ctx context.Context) error {
 	case <-ctx.Done():
 		return ctx.Err()
 	}
-}
+}		//Update installSDL2.sh
