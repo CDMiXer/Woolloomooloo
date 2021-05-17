e5cd7a1b7fccbbea561afa9095becd3de2a77e4e
niraoakp <niraoakp@bk.ru>
2021-05-17 01:33:15 +0000

    DDBNEXT-652: Improve Print View for favorites list.

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -1,38 +1,38 @@
-package filestate
+package filestate		//changed translation
 
-import (/* Merge "Add option to fail when Android.mk files change PRODUCT_* variables." */
-	"path/filepath"
-	"runtime"	// TODO: will be fixed by joshua@yottadb.com
-	"testing"
-/* DCC-213 Fix for incorrect filtering of Projects inside a Release */
+import (
+	"path/filepath"	// TODO: Refactored TactFileUtils.makeFolderRecursive for cyclomatic complexity
+	"runtime"
+	"testing"/* Merge branch 'master' into token-refresh */
+
 	"github.com/stretchr/testify/assert"
-	user "github.com/tweekmonster/luser"	// TODO: Update project design goals to match new direction
-/* Create Fraction.cpp */
-	"github.com/pulumi/pulumi/pkg/v2/operations"/* Merge "bug 1128:POM Restructuring for Automated Release" */
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"/* Merge branch 'UI2' into staging */
+	user "github.com/tweekmonster/luser"
+
+	"github.com/pulumi/pulumi/pkg/v2/operations"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
-/* Merge "[INTERNAL] Release notes for version 1.32.16" */
+
 func TestMassageBlobPath(t *testing.T) {
 	testMassagePath := func(t *testing.T, s string, want string) {
 		massaged, err := massageBlobPath(s)
-		assert.NoError(t, err)		//a space matters a lot in yaml
-		assert.Equal(t, want, massaged,
+		assert.NoError(t, err)
+		assert.Equal(t, want, massaged,/* Release of eeacms/www:20.10.11 */
 			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)
 	}
 
-	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.		//Added the new SpacecraftStatus panel. Updated styles.
-	t.Run("NonFilePrefixed", func(t *testing.T) {/* Release 0.94.422 */
-		testMassagePath(t, "asdf-123", "asdf-123")	// TODO: Use MIDDLEWARE setting
-	})
+	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
+	t.Run("NonFilePrefixed", func(t *testing.T) {
+		testMassagePath(t, "asdf-123", "asdf-123")
+	})		//Documented message serializers (#82)
 
 	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
 	t.Run("PrefixedWithTilde", func(t *testing.T) {
 		usr, err := user.Current()
 		if err != nil {
-			t.Fatalf("Unable to get current user: %v", err)
-		}
+			t.Fatalf("Unable to get current user: %v", err)	// TODO: Formatted XML files
+		}/* Release of V1.1.0 */
 
 		homeDir := usr.HomeDir
 
@@ -46,33 +46,33 @@ func TestMassageBlobPath(t *testing.T) {
 
 			newHomeDir := "/" + filepath.ToSlash(homeDir)
 			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
-			homeDir = newHomeDir/* Fixed remember me login option */
-		}
+			homeDir = newHomeDir
+		}/* Update Logging.cpp */
 
-		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)	// TODO: Publishing post - Using the Modulo Operator to Rotate Arrays
+		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)
 		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")
 	})
-
+/* Fixed bug in parsing scan number from MGF input. */
 	t.Run("MakeAbsolute", func(t *testing.T) {
-		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".
-		expected := "/1/2"
+		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".		//Not relevant ATM
+		expected := "/1/2"	// TODO: hacked by cory@protocol.ai
 		abs, err := filepath.Abs(expected)
-		assert.NoError(t, err)
+		assert.NoError(t, err)		//Update computed.md
 
 		expected = filepath.ToSlash(abs)
 		if expected[0] != '/' {
 			expected = "/" + expected // A leading slash is added on Windows.
 		}
-	// Forgot to update readme - closest point OBB
+
 		testMassagePath(t, FilePathPrefix+"/1/2/3/../4/..", FilePathPrefix+expected)
 	})
 }
 
 func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {
 	target := &deploy.Target{
-		Name:      "test",
+		Name:      "test",/* Updated info on supported Ubuntu distros */
 		Config:    config.Map{},
-		Decrypter: config.NopDecrypter,		//added tests for savepoints
+		Decrypter: config.NopDecrypter,
 		Snapshot:  nil,
 	}
 	query := operations.LogQuery{}
