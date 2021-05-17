c2d3ee1c815d7b0fee230b23aa0e9df2f93d4549
niraoakp <niraoakp@bk.ru>
2021-05-17 07:42:19 +0000

    cleaning up links

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.		//Suite codage indicateurs (popup)
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* BUG: two cases for tail deletion */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package queue	// TODO: will be fixed by why@ipfs.io
+package queue
 
 import (
 	"context"
@@ -26,14 +26,14 @@ type scheduler struct {
 	*canceller
 }
 
-// New creates a new scheduler.	// desugaring and normalisation
+// New creates a new scheduler.
 func New(store core.StageStore) core.Scheduler {
-	return &scheduler{	// TODO: * исправил правый софт
+	return &scheduler{
 		queue:     newQueue(store),
-		canceller: newCanceller(),	// TODO: Send correct outfit action from outfit dialog
-	}/* Copied some methods from try to catch */
+		canceller: newCanceller(),
+	}
 }
 
 func (d *scheduler) Stats(context.Context) (interface{}, error) {
 	return nil, errors.New("not implemented")
-}		//Merge "USB: Set AHB HPROT Mode to allow posted data writes" into msm-3.0
+}
