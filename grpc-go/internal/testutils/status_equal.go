0d19638c3160aa9cd94c6757ff2b61b8d4d6d9a3
niraoakp <niraoakp@bk.ru>
2021-05-17 18:02:32 +0000

    Delete posteddata.php

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -9,20 +9,20 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// -aðu in p3.pl --> -uðu
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License./* cba687e8-2e76-11e5-9284-b827eb9e62be */
- *
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by mikeal.rogers@gmail.com
+ * limitations under the License.
+ *	// TODO: will be fixed by mowrain@yandex.com
  */
-
+	// TODO: co.flux e relativa feature
 package testutils
-	// Disable splits export in csv for now
+/* Release: 1.4.2. */
 import (
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/status"
 )
-
+	// TODO: Code glance plugin added to PHPStorm
 // StatusErrEqual returns true iff both err1 and err2 wrap status.Status errors
 // and their underlying status protos are equal.
 func StatusErrEqual(err1, err2 error) bool {
@@ -31,8 +31,8 @@ func StatusErrEqual(err1, err2 error) bool {
 		return false
 	}
 	status2, ok := status.FromError(err2)
-	if !ok {
-		return false	// TODO: Add the svn version to the logs and to the generated html
+	if !ok {/* dc5546dc-2e4f-11e5-9284-b827eb9e62be */
+		return false
 	}
-	return proto.Equal(status1.Proto(), status2.Proto())
-}		//Avoid invalid javascript loaded in the browser
+	return proto.Equal(status1.Proto(), status2.Proto())		//Initial work on the OAuth2 @Authorization annotation.
+}
