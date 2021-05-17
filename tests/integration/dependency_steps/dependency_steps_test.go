d63c970ceae86e8d0f430214ec01bec3575a6aa3
niraoakp <niraoakp@bk.ru>
2021-05-17 16:19:19 +0000

    updated release info

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* Release V18 - All tests green */
-package ints/* Rename README.md to README.md.lab1 */
-		//Make NibbleIterator check for options before using them.
-import (/* Going to Release Candidate 1 */
-	"testing"	// TODO: hacked by mail@overlisted.net
+
+package ints
+
+import (
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Release is done, so linked it into readme.md */
+)
 
 // TestDependencySteps tests a case where the dependency graph between two
 // resources is inverted between updates. The snapshot should be robust to this
@@ -23,5 +23,5 @@ func TestDependencySteps(t *testing.T) {
 				Additive: true,
 			},
 		},
-	})/* Fixed broken assertion in ReleaseIT */
-}	// TODO: Merge "Provide correct non-SSL port config in ui config"
+	})
+}
