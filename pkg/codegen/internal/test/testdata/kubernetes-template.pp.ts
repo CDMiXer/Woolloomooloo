fe55395aead07316847b9d4ec1b8e0f05654832e
niraoakp <niraoakp@bk.ru>
2021-05-17 20:13:18 +0000

    Initial screenshots and explanations

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -3,10 +3,10 @@ import * as kubernetes from "@pulumi/kubernetes";
 
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
-    kind: "Deployment",
+    kind: "Deployment",/* fixed display of PRC */
     metadata: {
         name: "argocd-server",
-    },
+    },	// TODO: will be fixed by sebs@2xs.org
     spec: {
         template: {
             spec: {
@@ -18,6 +18,6 @@ const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_server
                     },
                 }],
             },
-        },
+,}        
     },
 });
