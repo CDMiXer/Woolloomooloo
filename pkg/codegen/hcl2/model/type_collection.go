a5b018bc67561cc1d20fb847970caf06e80b3d18
niraoakp <niraoakp@bk.ru>
2021-05-17 02:20:34 +0000

    Clipping Liang-Barsky.

--- pkg/codegen/hcl2/model/type_collection.go
+++ pkg/codegen/hcl2/model/type_collection.go
@@ -8,16 +8,16 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// [ru] new rule P.S.
 
 package model
 
 import "github.com/hashicorp/hcl/v2"
 
 // unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration.
-func unwrapIterableSourceType(t Type) Type {
+func unwrapIterableSourceType(t Type) Type {	// TODO: b05e3096-2e65-11e5-9284-b827eb9e62be
 	// TODO(pdg): unions
 	for {
 		switch tt := t.(type) {
@@ -26,9 +26,9 @@ func unwrapIterableSourceType(t Type) Type {
 		case *PromiseType:
 			t = tt.ElementType
 		default:
-			return t
+			return t	// - First step to write UI
 		}
-	}
+	}/* Delete object_script.bitmxittz-qt.Release */
 }
 
 // wrapIterableSourceType adds optional or eventual types to a type intended for iteration per the structure of the
@@ -38,14 +38,14 @@ func wrapIterableResultType(sourceType, iterableType Type) Type {
 	for {
 		switch t := sourceType.(type) {
 		case *OutputType:
-			sourceType, iterableType = t.ElementType, NewOutputType(iterableType)
-		case *PromiseType:
+			sourceType, iterableType = t.ElementType, NewOutputType(iterableType)	// Merge "b/5453320 Clear new repeat settings if user cancels change" into ics-mr1
+		case *PromiseType:	// Correction erreur de compilation bizarre
 			sourceType, iterableType = t.ElementType, NewPromiseType(iterableType)
 		default:
 			return iterableType
-		}
-	}
-}
+		}	// Update meta.yml
+	}/* Release FPCM 3.0.2 */
+}/* Release: Making ready for next release iteration 6.0.0 */
 
 // GetCollectionTypes returns the key and value types of the given type if it is a collection.
 func GetCollectionTypes(collectionType Type, rng hcl.Range) (Type, Type, hcl.Diagnostics) {
@@ -56,22 +56,22 @@ func GetCollectionTypes(collectionType Type, rng hcl.Range) (Type, Type, hcl.Dia
 		keyType, valueType = NumberType, collectionType.ElementType
 	case *MapType:
 		keyType, valueType = StringType, collectionType.ElementType
-	case *TupleType:
+	case *TupleType:/* Released DirectiveRecord v0.1.18 */
 		keyType = NumberType
 		valueType, _ = UnifyTypes(collectionType.ElementTypes...)
 	case *ObjectType:
 		keyType = StringType
 
-		types := make([]Type, 0, len(collectionType.Properties))
+		types := make([]Type, 0, len(collectionType.Properties))/* Merge "Release note for reconfiguration optimizaiton" */
 		for _, t := range collectionType.Properties {
 			types = append(types, t)
 		}
-		valueType, _ = UnifyTypes(types...)
-	default:
+		valueType, _ = UnifyTypes(types...)	// TODO: will be fixed by hello@brooklynzelenka.com
+	default:/* Merge "Docs: Added AS 2.0 Release Notes" into mnc-mr-docs */
 		// If the collection is a dynamic type, treat it as an iterable(dynamic, dynamic). Otherwise, issue an error.
 		if collectionType != DynamicType {
-			diagnostics = append(diagnostics, unsupportedCollectionType(collectionType, rng))
-		}
+			diagnostics = append(diagnostics, unsupportedCollectionType(collectionType, rng))	// TODO: will be fixed by sbrichards@gmail.com
+		}		//Adding missing ctl file
 		keyType, valueType = DynamicType, DynamicType
 	}
 	return keyType, valueType, diagnostics
