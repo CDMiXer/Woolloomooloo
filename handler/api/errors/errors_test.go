c35e3ad6c87cfd757952e8fcd4be32576a3facc4
niraoakp <niraoakp@bk.ru>
2021-05-17 14:38:32 +0000

    69dbbcf2-2e41-11e5-9284-b827eb9e62be

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Create ReleaseCandidate_ReleaseNotes.md */
+// that can be found in the LICENSE file.
 
 package errors
 
-import "testing"/* Changed the name from slackAssit from to gitAssist */
+import "testing"
 
 func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
