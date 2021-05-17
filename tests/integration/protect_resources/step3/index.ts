6d776ae14e5a10f87a0864f4ce57cbf7b0200730
niraoakp <niraoakp@bk.ru>
2021-05-17 03:22:52 +0000

    2ed0b7e8-2e73-11e5-9284-b827eb9e62be

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Resource } from "./resource";
+/* Merge "Wlan: Release 3.8.20.9" */
+import { Resource } from "./resource";		//Update version mentioned in README
 
 // Attempt to delete the resource; this will fail!
