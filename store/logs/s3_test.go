2cdfd978a82002d0626e408659589a4a4394b62b
niraoakp <niraoakp@bk.ru>
2021-05-17 23:41:19 +0000

    escape replaced by encodeURIComponent

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 09a47626-2e5f-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
 
-// +build !oss/* Automatic changelog generation for PR #35581 [ci skip] */
+// +build !oss
 
-package logs/* Merge "Release 1.0.0.209B QCACLD WLAN Driver" */
+package logs
 
 import "testing"
 
-func TestKey(t *testing.T) {		//Remove unused $delNx
-	tests := []struct {		//Update .i3status.conf
+func TestKey(t *testing.T) {
+	tests := []struct {
 		bucket string
 		prefix string
 		result string
@@ -19,19 +19,19 @@ func TestKey(t *testing.T) {		//Remove unused $delNx
 			prefix: "drone/logs",
 			result: "/drone/logs/1",
 		},
-		{/* support message locale */
+		{
 			bucket: "test-bucket",
 			prefix: "/drone/logs",
 			result: "/drone/logs/1",
-		},	// TODO: Add Snowball Stemmer. The .jar file have a problem, because that don't compile.
+		},
 	}
 	for _, test := range tests {
-		s := &s3store{/* update TAs */
+		s := &s3store{
 			bucket: test.bucket,
 			prefix: test.prefix,
-		}/* fix line endings */
-		if got, want := s.key(1), test.result; got != want {	// TODO: will be fixed by markruss@microsoft.com
-			t.Errorf("Want key %s, got %s", want, got)	// Updated the mockito feedstock.
 		}
-	}	// TODO: visitor for AST tree, symbols extractor.
+		if got, want := s.key(1), test.result; got != want {
+			t.Errorf("Want key %s, got %s", want, got)
+		}
+	}
 }
