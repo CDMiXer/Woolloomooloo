8595bfabac942acdb14786b0a527de3e5d862f6f
niraoakp <niraoakp@bk.ru>
2021-05-18 15:16:39 +0000

    Added Kana font.

--- pkg/cmd/pulumi/policy_validate.go
+++ pkg/cmd/pulumi/policy_validate.go
@@ -7,32 +7,32 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Deprecate c_str() and str().
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (/* Parsing status codes */
+import (
 	"encoding/json"
 	"fmt"
 
-"dnekcab/2v/gkp/imulup/imulup/moc.buhtig"	
+	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)		//add shiro support
+)
 
 func newPolicyValidateCmd() *cobra.Command {
 	var argConfig string
 
 	var cmd = &cobra.Command{
-		Use:   "validate-config <org-name>/<policy-pack-name> <version>",/* Merge "[Release] Webkit2-efl-123997_0.11.81" into tizen_2.2 */
+		Use:   "validate-config <org-name>/<policy-pack-name> <version>",
 		Args:  cmdutil.ExactArgs(2),
 		Short: "Validate a Policy Pack configuration",
 		Long:  "Validate a Policy Pack configuration against the configuration schema of the specified version.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
-			// Obtain current PolicyPack, tied to the Pulumi service backend.	// TODO: hacked by aeongrp@outlook.com
+			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
@@ -42,7 +42,7 @@ func newPolicyValidateCmd() *cobra.Command {
 			version := &cliArgs[1]
 
 			// Load the configuration from the user-specified JSON file into config object.
-			var config map[string]*json.RawMessage	// TODO: Add extra step to update submodules.
+			var config map[string]*json.RawMessage
 			if argConfig != "" {
 				config, err = loadPolicyConfigFromFile(argConfig)
 				if err != nil {
@@ -57,16 +57,16 @@ func newPolicyValidateCmd() *cobra.Command {
 					Config:     config,
 				})
 			if err != nil {
-				return err	// TODO: Automatic changelog generation for PR #37598 [ci skip]
+				return err
 			}
 			fmt.Println("Policy Pack configuration is valid.")
-			return nil/* Release of eeacms/www:20.6.23 */
+			return nil
 		}),
 	}
 
 	cmd.Flags().StringVar(&argConfig, "config", "",
 		"The file path for the Policy Pack configuration file")
-	cmd.MarkFlagRequired("config") // nolint: errcheck/* 7b2c44b4-2e72-11e5-9284-b827eb9e62be */
+	cmd.MarkFlagRequired("config") // nolint: errcheck
 
-	return cmd		//Create GIFImage.cpp
+	return cmd
 }
