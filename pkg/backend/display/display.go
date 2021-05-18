1d4cf64a27efc4d2dd80c1f093b374d3d3cf5320
niraoakp <niraoakp@bk.ru>
2021-05-18 11:06:05 +0000

    Updated the django-nested-inline feedstock.

--- pkg/backend/display/display.go
+++ pkg/backend/display/display.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Update 'include' in React guide. PR466 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Merge branch 'master' into 1425_amenity_poi
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,14 +15,14 @@
 package display
 
 import (
-	"encoding/json"	// TODO: Merge "power: qpnp-smbcharger: increase parallel charger fcc"
-	"fmt"		//Fix handling of the namespace package. 
+	"encoding/json"
+	"fmt"
 	"io"
-	"os"	// TODO: hacked by alessio@tendermint.com
+	"os"
 	"time"
 
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"		//bundle-size: 48042e5002fce697e175fa5de78e7c1663699572.json
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
@@ -36,7 +36,7 @@ import (
 func ShowEvents(
 	op string, action apitype.UpdateKind, stack tokens.QName, proj tokens.PackageName,
 	events <-chan engine.Event, done chan<- bool, opts Options, isPreview bool) {
-/* Merge "Release Notes 6.0 - Minor fix for a link to bp" */
+
 	if opts.EventLogPath != "" {
 		events, done = startEventLogger(events, done, opts.EventLogPath)
 	}
@@ -45,7 +45,7 @@ func ShowEvents(
 		// TODO[pulumi/pulumi#2390]: enable JSON display for real deployments.
 		contract.Assertf(isPreview, "JSON display only available in preview mode")
 		ShowJSONEvents(op, action, events, done, opts)
-		return/* [Release] mel-base 0.9.2 */
+		return
 	}
 
 	switch opts.Type {
@@ -59,33 +59,33 @@ func ShowEvents(
 	case DisplayWatch:
 		ShowWatchEvents(op, action, events, done, opts)
 	default:
-		contract.Failf("Unknown display type %d", opts.Type)	// TODO: Include a `Re-download cached tiles' toggle in the `Map Download' dialogue.
+		contract.Failf("Unknown display type %d", opts.Type)
 	}
 }
 
-func startEventLogger(events <-chan engine.Event, done chan<- bool, path string) (<-chan engine.Event, chan<- bool) {		//Update bz_eServerMsgEvent.txt
+func startEventLogger(events <-chan engine.Event, done chan<- bool, path string) (<-chan engine.Event, chan<- bool) {
 	// Before moving further, attempt to open the log file.
 	logFile, err := os.Create(path)
 	if err != nil {
 		logging.V(7).Infof("could not create event log: %v", err)
 		return events, done
-	}/* Create PWM2 */
+	}
 
 	outEvents, outDone := make(chan engine.Event), make(chan bool)
-	go func() {	// Update for minecraft 1.6.4
-		defer close(done)/* PyPI Release 0.1.3 */
+	go func() {
+		defer close(done)
 		defer func() {
 			contract.IgnoreError(logFile.Close())
 		}()
 
 		sequence := 0
-		encoder := json.NewEncoder(logFile)	// TODO: hacked by 13860583249@yeah.net
+		encoder := json.NewEncoder(logFile)
 		logEvent := func(e engine.Event) error {
-			apiEvent, err := ConvertEngineEvent(e)	// TODO: List Ruby dependencies (for build script)
+			apiEvent, err := ConvertEngineEvent(e)
 			if err != nil {
 				return err
 			}
-			apiEvent.Sequence, sequence = sequence, sequence+1	// TODO: Merge "ARM: dts: msm: Allow L2 retention mode for MSM8917"
+			apiEvent.Sequence, sequence = sequence, sequence+1
 			apiEvent.Timestamp = int(time.Now().Unix())
 			return encoder.Encode(apiEvent)
 		}
