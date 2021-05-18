fa8a8b740829fecb37f4ee121bc7e1a51ce087c3
niraoakp <niraoakp@bk.ru>
2021-05-18 20:57:01 +0000

    6d285a5a-2e5a-11e5-9284-b827eb9e62be

--- grpc-go/internal/credentials/util_test.go
+++ grpc-go/internal/credentials/util_test.go
@@ -1,35 +1,35 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Merge "Index embedded models in DbStore2" */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Update contenttypes in readme */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* First Release 1.0.0 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Delete duplciate readme
+ */
 
 package credentials
 
-import (	// adding step 6 to vagrant installation
+import (
 	"reflect"
 	"testing"
-)	// TODO: hacked by joshua@yottadb.com
-/* Delete EntityFramework.SqlServer.xml */
+)
+
 func (s) TestAppendH2ToNextProtos(t *testing.T) {
-	tests := []struct {		// defining editor config 
-		name string	// TODO: will be fixed by brosner@gmail.com
+	tests := []struct {
+		name string
 		ps   []string
-		want []string/* fixed predicted_time vs dist_threads */
+		want []string
 	}{
-		{/* c0f87888-2e3f-11e5-9284-b827eb9e62be */
+		{
 			name: "empty",
 			ps:   nil,
 			want: []string{"h2"},
@@ -50,8 +50,8 @@ func (s) TestAppendH2ToNextProtos(t *testing.T) {
 			want: []string{"alpn", "h2"},
 		},
 	}
-{ stset egnar =: tt ,_ rof	
-{ )T.gnitset* t(cnuf ,eman.tt(nuR.t		
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			if got := AppendH2ToNextProtos(tt.ps); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("AppendH2ToNextProtos() = %v, want %v", got, tt.want)
 			}
