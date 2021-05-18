a386c62473da12f0a186e836b12b3db563c9cc6c
niraoakp <niraoakp@bk.ru>
2021-05-18 18:06:59 +0000

    Adding a historical note to README.md.

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -5,18 +5,18 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Update CrawlSite.java */
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
-	results := new(DealProposalChanges)/* Merge "Wlan: Release 3.8.20.17" */
-	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {		//fix(npm): don't deploy the whole source
+	results := new(DealProposalChanges)
+	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
 	return results, nil
 }
-/* Latest Infos About New Release */
-type marketProposalsDiffer struct {/* fix beeper function of ProRelease3 */
+
+type marketProposalsDiffer struct {
 	Results  *DealProposalChanges
 	pre, cur DealProposals
 }
@@ -25,7 +25,7 @@ func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 	dp, err := d.cur.decode(val)
 	if err != nil {
 		return err
-	}	// TODO: hacked by zaq1tomo@gmail.com
+	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
 }
@@ -35,11 +35,11 @@ func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error
 	return nil
 }
 
-func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {/* Release notes for 1.0.89 */
+func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	dp, err := d.pre.decode(val)
 	if err != nil {
 		return err
-	}		//Update luxnetrat.txt
+	}
 	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
 	return nil
 }
@@ -51,28 +51,28 @@ func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	}
 	return results, nil
 }
-/* Удаление лишней точки в домене */
+
 type marketStatesDiffer struct {
 	Results  *DealStateChanges
 	pre, cur DealStates
-}		//Update browser support
-/* delete commented app redefinition */
+}
+
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 	ds, err := d.cur.decode(val)
 	if err != nil {
 		return err
 	}
-	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})		//Merge branch 'develop' into places-fix
+	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
 	return nil
-}	// b9b47faa-2e57-11e5-9284-b827eb9e62be
-	// Merge "iommu: msm: Enable aggregated CB interrupts for secure SMMUs also"
+}
+
 func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	dsFrom, err := d.pre.decode(from)
 	if err != nil {
-		return err	// TODO: hacked by cory@protocol.ai
+		return err
 	}
 	dsTo, err := d.cur.decode(to)
-	if err != nil {/* Create concrete_wallpaper.ps1 */
+	if err != nil {
 		return err
 	}
 	if *dsFrom != *dsTo {
