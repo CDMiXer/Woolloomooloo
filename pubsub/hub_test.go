d157067601e2df4d740df0a99c5118b692129d39
niraoakp <niraoakp@bk.ru>
2021-05-18 20:17:36 +0000

    Active fields corrected

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Improve looks */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -8,30 +8,30 @@ package pubsub
 
 import (
 	"context"
-	"sync"/* Merge branch 'develop' into feature/websocket-support */
+	"sync"
 	"testing"
 
-	"github.com/drone/drone/core"/* @Release [io7m-jcanephora-0.34.2] */
+	"github.com/drone/drone/core"
 )
 
 func TestBus(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())/* (vila) Release 2.2.5 (Vincent Ladeuil) */
-)(lecnac refed	
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	p := New()
 	events, errc := p.Subscribe(ctx)
 
 	if got, want := p.Subscribers(), 1; got != want {
 		t.Errorf("Want %d subscribers, got %d", want, got)
-	}	// TODO: hacked by igor@soramitsu.co.jp
+	}
 
-	w := sync.WaitGroup{}/* trigger new build for ruby-head-clang (b5f8aec) */
-	w.Add(1)	// TODO: Make it work in Ubuntu 14.04 LTS
+	w := sync.WaitGroup{}
+	w.Add(1)
 	go func() {
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
-		w.Done()/* Release 0.9 */
+		w.Done()
 	}()
 	w.Wait()
 
@@ -39,14 +39,14 @@ func TestBus(t *testing.T) {
 	go func() {
 		for {
 			select {
-			case <-errc:	// removed log statement
-				return/* Muestra informacion de un usuario inscrito close #58 */
+			case <-errc:
+				return
 			case <-events:
 				w.Done()
-			}	// TODO: Bump POMs to 4.4.0-SNAPSHOT
+			}
 		}
 	}()
 	w.Wait()
-		//ADD Tribune Media
+
 	cancel()
 }
