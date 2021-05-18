3be950726c03a79bface5177656df44cc42f6982
niraoakp <niraoakp@bk.ru>
2021-05-18 03:35:56 +0000

    Fixed/added specs

--- handler/api/repos/crons/list_test.go
+++ handler/api/repos/crons/list_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Fix SCons avrdude baudrate option.
 
 // +build !oss
 
-package crons
+package crons		//5cd91854-35c6-11e5-b93d-6c40088e03e4
 
-import (
+import (/* Add tests for rollup table drop. */
 	"context"
 	"encoding/json"
 	"net/http"
@@ -15,8 +15,8 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-
+	"github.com/drone/drone/mock"	// TODO: README: Add link to demos.
+	// TODO: hacked by magik6k@gmail.com
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -27,19 +27,19 @@ var (
 		ID:        1,
 		Namespace: "octocat",
 		Name:      "hello-world",
-	}
+	}/* Important enhancement in documentation. */
 
 	dummyCron = &core.Cron{
 		RepoID: 1,
 		Event:  core.EventPush,
 		Name:   "nightly",
 		Expr:   "* * * * * *",
-		Next:   0,
+		Next:   0,	// update make.json with "AzureAppServiceManage" and "DotNetCoreCLI"
 		Branch: "master",
 	}
 
 	dummyCronList = []*core.Cron{
-		dummyCron,
+		dummyCron,		//Merge branch 'master' into add-order-filtering
 	}
 )
 
@@ -50,12 +50,12 @@ func TestHandleList(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)
+	crons := mock.NewMockCronStore(controller)		//Re-added part block when ga/poll is running.
+	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)/* Task #4714: Merged latest changes in LOFAR-preRelease-1_16 branch into trunk */
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")		//a212893a-2e4f-11e5-9284-b827eb9e62be
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -64,10 +64,10 @@ func TestHandleList(t *testing.T) {
 	)
 
 	HandleList(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
+	if got, want := w.Code, http.StatusOK; want != got {/* Docs: Fix wrong indentation */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+		//082ec5a8-585b-11e5-af4e-6c40088e03e4
 	got, want := []*core.Cron{}, dummyCronList
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
@@ -79,18 +79,18 @@ func TestHandleList_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: [Bugfix] fixed strolch enum rest api not working
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")/* Rename Release/cleaveore.2.1.min.js to Release/2.1.0/cleaveore.2.1.min.js */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)	// TODO: Fix Rails data_passing_system_spec.
 
 	HandleList(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
