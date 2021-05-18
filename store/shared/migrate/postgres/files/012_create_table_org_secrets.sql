759481c2029376cdf4accc9ca01d84e881aeded8
niraoakp <niraoakp@bk.ru>
2021-05-18 22:46:12 +0000

    0ec72d30-2e67-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,11 +1,11 @@
 -- name: create-table-org-secrets
-
+	// TODO: [IMP] Docstring added
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
-,secret_name              VARCHAR(200)
+,secret_name              VARCHAR(200)/* Delete sphere.c */
 ,secret_type              VARCHAR(50)
-,secret_data              BYTEA/* Fixes TXT record parsing. */
+,secret_data              BYTEA	// Create resources.erb
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
