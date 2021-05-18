3d92d06f4482735d83dc1ace1f0077e779bd1c1f
niraoakp <niraoakp@bk.ru>
2021-05-18 12:35:02 +0000

    836f3dea-2e6b-11e5-9284-b827eb9e62be

--- pkg/codegen/docs.go
+++ pkg/codegen/docs.go
@@ -1,14 +1,14 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.		//Start of writeUDP and closeUDP
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by mail@bitpshr.net
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// v0.3, fix divide-by-zero, change tabs to space
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* 0f4bb9f0-2e6d-11e5-9284-b827eb9e62be */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* updated diagram and other stuff */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,7 +18,7 @@ import (
 	"github.com/pgavlin/goldmark/ast"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)
+)	// use isToken on permanent instead of getting card first
 
 // DocLanguageHelper is an interface for extracting language-specific information from a Pulumi schema.
 // See the implementation for this interface under each of the language code generators.
@@ -28,17 +28,17 @@ type DocLanguageHelper interface {
 	GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string
 	GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
 	GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
-	GetDocLinkForBuiltInType(typeName string) string
+	GetDocLinkForBuiltInType(typeName string) string		//fixed morph disamb
 	GetLanguageTypeString(pkg *schema.Package, moduleName string, t schema.Type, input, optional bool) string
 
 	GetFunctionName(modName string, f *schema.Function) string
-	// GetResourceFunctionResultName returns the name of the result type when a static resource function is used to lookup
+	// GetResourceFunctionResultName returns the name of the result type when a static resource function is used to lookup	// TODO: hacked by vyzo@hackzen.org
 	// an existing resource.
 	GetResourceFunctionResultName(modName string, f *schema.Function) string
 	// GetModuleDocLink returns the display name and the link for a module (including root modules) in a given package.
-	GetModuleDocLink(pkg *schema.Package, modName string) (string, string)
+	GetModuleDocLink(pkg *schema.Package, modName string) (string, string)/* Major Release before Site Dissemination */
 }
-
+		//Update auditing-with-nservicebus.md
 func filterExamples(source []byte, node ast.Node, lang string) {
 	var c, next ast.Node
 	for c = node.FirstChild(); c != nil; c = next {
@@ -49,12 +49,12 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 		case *ast.FencedCodeBlock:
 			sourceLang := string(c.Language(source))
 			if sourceLang != lang && sourceLang != "sh" {
-				node.RemoveChild(node, c)
-			}
+				node.RemoveChild(node, c)/* added getter for resolve Id */
+			}		//Search extensions in a different place. 
 		case *schema.Shortcode:
-			switch string(c.Name) {
+			switch string(c.Name) {/* Add toClass and getFieldDescriptor method to ast types */
 			case schema.ExampleShortcode:
-				hasCode := false
+				hasCode := false	// TODO: Update XGBRegressor.ipynb
 				for gc := c.FirstChild(); gc != nil; gc = gc.NextSibling() {
 					if gc.Kind() == ast.KindFencedCodeBlock {
 						hasCode = true
@@ -66,14 +66,14 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 					for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
 						nextGrandchild = grandchild.NextSibling()
 						node.InsertBefore(node, c, grandchild)
-					}
+					}/* Forgot to remove opt params from main Params struct */
 				}
 				node.RemoveChild(node, c)
 			case schema.ExamplesShortcode:
 				if first := c.FirstChild(); first != nil {
 					first.SetBlankPreviousLines(c.HasBlankPreviousLines())
 				}
-
+/* Merge "[www] update sitemap.xml" */
 				var grandchild, nextGrandchild ast.Node
 				for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
 					nextGrandchild = grandchild.NextSibling()
