1374269f8ed60d713fc6ca85684de272221d2503
niraoakp <niraoakp@bk.ru>
2021-05-18 15:47:55 +0000

    Aggiunti i video di Bologna

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by steven@stebalien.com
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Move stray closing p tag out of a translation. props chrisbliss18, fixes #13036. */
-// resource "not-doomed" is created successfully./* Hotfix Release 1.2.3 */
+
+// resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
-		//removed some now-unnecessary repositories
+
 // "a" should be in the checkpoint.
