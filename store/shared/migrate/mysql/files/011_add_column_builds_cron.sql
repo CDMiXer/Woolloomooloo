bde31d9b9aa38ff79391629b4c5c2f775e98dfe1
niraoakp <niraoakp@bk.ru>
2021-05-18 13:02:09 +0000

    Merge branch 'dev' into marcos-dev2

--- store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron
+-- name: alter-table-builds-add-column-cron/* #456 adding testing issue to Release Notes. */
 
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
