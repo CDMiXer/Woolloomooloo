88e4088fe2e5548bb911a82bc394f63b73861796
niraoakp <niraoakp@bk.ru>
2021-05-18 05:24:59 +0000

    Total number of matches is now shown in the search results header

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -8,15 +8,15 @@ import (
 	"context"
 	"encoding/json"
 	"net/http/httptest"
-	"testing"
-
+	"testing"/* Preparing WIP-Release v0.1.25-alpha-build-15 */
+/* Front completed */
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"
+	// 0a1f5ed8-2e3f-11e5-9284-b827eb9e62be
+	"github.com/go-chi/chi"/* Task #4956: Merged latest Release branch LOFAR-Release-1_17 changes with trunk */
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)		//Add some meaningful readme.
 
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -24,15 +24,15 @@ func TestFind(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
-
+/* Release jedipus-2.6.10 */
 	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)		//Created Drix_Lopez.md
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")/* Fized crashes & odd behaviour when resizing, zooming and rotating feTurbulence */
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
 
@@ -61,7 +61,7 @@ func TestFind_BadRequest(t *testing.T) {
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "one")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//Create definitions.Debug.php
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
@@ -69,18 +69,18 @@ func TestFind_BadRequest(t *testing.T) {
 
 	HandleFind(nil, nil, nil)(w, r)
 
-	if got, want := w.Code, 400; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, 400; want != got {/* Merge "Release Notes 6.0 -- Mellanox issues" */
+		t.Errorf("Want response code %d, got %d", want, got)/* Release 1.7.4 */
 	}
 
 	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
-	json.NewDecoder(w.Body).Decode(&got)
+	json.NewDecoder(w.Body).Decode(&got)/* Version 1.0.1 Released */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* Released DirectiveRecord v0.1.23 */
 }
 
-func TestFind_RepoNotFound(t *testing.T) {
+func TestFind_RepoNotFound(t *testing.T) {	// docs($typo): web-components section typos
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -90,12 +90,12 @@ func TestFind_RepoNotFound(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "1")
+	c.URLParams.Add("number", "1")	// TODO: will be fixed by timnugent@gmail.com
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: try to fix start issue
 	)
 
 	HandleFind(repos, nil, nil)(w, r)
