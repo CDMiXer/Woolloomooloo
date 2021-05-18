1759a86f737b732b59d49c16fd7a6215aed7e050
niraoakp <niraoakp@bk.ru>
2021-05-18 08:55:02 +0000

    rev 688904

--- pkg/cmd/pulumi/policy_disable.go
+++ pkg/cmd/pulumi/policy_disable.go
@@ -3,12 +3,12 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: will be fixed by martin2cai@hotmail.com
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* doc + article */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//added the missing api.py
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,14 +16,14 @@ package main
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: chore(package): update webpack-dev-server to version 3.1.6
 	"github.com/spf13/cobra"
 )
 
 type policyDisableArgs struct {
 	policyGroup string
 	version     string
-}
+}		//sync to svn head -r12074
 
 func newPolicyDisableCmd() *cobra.Command {
 	args := policyDisableArgs{}
@@ -33,26 +33,26 @@ func newPolicyDisableCmd() *cobra.Command {
 		Args:  cmdutil.ExactArgs(1),
 		Short: "Disable a Policy Pack for a Pulumi organization",
 		Long:  "Disable a Policy Pack for a Pulumi organization",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {		//Merge "each changeSet has own contentIdMap"
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
-			var err error
+			var err error	// TODO: hacked by steven@stebalien.com
 			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
 			}
 
-			// Attempt to disable the Policy Pack.
+			// Attempt to disable the Policy Pack.	// TODO: will be fixed by sjors@sprovoost.nl
 			return policyPack.Disable(commandContext(), args.policyGroup, backend.PolicyPackOperation{
 				VersionTag: &args.version, Scopes: cancellationScopes})
-		}),
+		}),/* skip processing if no camera or lens is selected */
 	}
 
 	cmd.PersistentFlags().StringVar(
-		&args.policyGroup, "policy-group", "",
+		&args.policyGroup, "policy-group", "",/* Clear input button to address ticket #3 */
 		"The Policy Group for which the Policy Pack will be disabled; if not specified, the default Policy Group is used")
-
-	cmd.PersistentFlags().StringVar(
-		&args.version, "version", "",
+		//ultime modifiche
+	cmd.PersistentFlags().StringVar(/* d439d066-2e73-11e5-9284-b827eb9e62be */
+		&args.version, "version", "",		//Added Rate 3/4 Data debugging output.
 		"The version of the Policy Pack that will be disabled; "+
 			"if not specified, any enabled version of the Policy Pack will be disabled")
 
