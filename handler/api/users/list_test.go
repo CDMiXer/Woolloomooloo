60c858a3683474694d991894d841e78f9283d570
niraoakp <niraoakp@bk.ru>
2021-05-18 15:29:10 +0000

    need to check for null

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* bring in paper-ui and polymer (extracted from zip because bower is a huge dep) */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package users
 
 import (
 	"database/sql"
-	"encoding/json"/* Merge branch 'develop' into feature/CC-1424 */
-	"net/http/httptest"	// TODO: Create hst-uninstall
+	"encoding/json"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"/* Release beta. */
+	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Rename cobaltbrew.py to cobaltbrew
-)/* Update line 59 in ProjectSummaryCreator */
+	"github.com/google/go-cmp/cmp"
+)
 
 var (
 	mockUser = &core.User{
 		ID:     1,
 		Login:  "octocat",
 		Email:  "octocat@github.com",
-		Admin:  false,/* import PIL more rebustly */
-		Active: true,		//e266bcec-2e75-11e5-9284-b827eb9e62be
+		Admin:  false,
+		Active: true,
 		Avatar: "https://avatars1.githubusercontent.com/u/583231",
 	}
 
@@ -35,32 +35,32 @@ var (
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Fix other OS crashing on STARTUPINFO
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
 
-	w := httptest.NewRecorder()		//c2b4d42a-2e47-11e5-9284-b827eb9e62be
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	h := HandleList(users)
 
-	h(w, r)/* PreRelease commit */
+	h(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Merge "Keep the request when throwing ConnectionException with curl_init_pooled" */
+	}
 
 	got, want := []*core.User{}, mockUserList
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}/* new Release */
+	}
 }
 
-func TestUserList_Err(t *testing.T) {		//update leap
+func TestUserList_Err(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Merge branch 'master' into add-aby-abraham-kal */
+	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)		//Merge "Fix --update-unexpected option if test result contains a dollar sign."
+	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
