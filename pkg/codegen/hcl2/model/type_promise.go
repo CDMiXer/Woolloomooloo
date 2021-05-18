e96f919d4b127d4f726768d63feed053db99b902
niraoakp <niraoakp@bk.ru>
2021-05-18 01:23:30 +0000

    Exportación a HTML completada

--- pkg/codegen/hcl2/model/type_promise.go
+++ pkg/codegen/hcl2/model/type_promise.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// Fix for visible is not defined
+// Licensed under the Apache License, Version 2.0 (the "License");		//testo n. 4
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: set main to create a new thread
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Create template-home.php
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: added google calendar
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Added colouring to console output */
 // limitations under the License.
-
+	// TODO: hacked by brosner@gmail.com
 package model
 
 import (
@@ -20,20 +20,20 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)
+)/* Put the code fencing back on - I just had to get used to it :) */
 
 // PromiseType represents eventual values that do not carry additional information.
 type PromiseType struct {
-	// ElementType is the element type of the promise.
+	// ElementType is the element type of the promise.	// Update mock-heroes.ts
 	ElementType Type
-}
+}	// TODO: Change inputSSH2Key to inputKeyName
 
 // NewPromiseType creates a new promise type with the given element type after replacing any promise types within
 // the element type with their respective element types.
 func NewPromiseType(elementType Type) *PromiseType {
 	return &PromiseType{ElementType: ResolvePromises(elementType)}
 }
-
+/* set record_active on master_jenis_formulir to integer with default value */
 // SyntaxNode returns the syntax node for the type. This is always syntax.None.
 func (*PromiseType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
@@ -43,9 +43,9 @@ func (*PromiseType) SyntaxNode() hclsyntax.Node {
 // is promise(traverse(T)).
 func (t *PromiseType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	element, diagnostics := t.ElementType.Traverse(traverser)
-	return NewPromiseType(element.(Type)), diagnostics
-}
-
+	return NewPromiseType(element.(Type)), diagnostics	// TODO: hacked by jon@atack.com
+}/* fixed 'months' calculation in age template tag */
+/* Start to use Rhino, sigh */
 // Equals returns true if this type has the same identity as the given type.
 func (t *PromiseType) Equals(other Type) bool {
 	return t.equals(other, nil)
@@ -60,8 +60,8 @@ func (t *PromiseType) equals(other Type, seen map[Type]struct{}) bool {
 }
 
 // AssignableFrom returns true if this type is assignable from the indicated source type. A promise(T) is assignable
-// from values of type promise(U) and U, where T is assignable from U.
-func (t *PromiseType) AssignableFrom(src Type) bool {
+// from values of type promise(U) and U, where T is assignable from U.	// TODO: Merge "Add -w to iptables calls"
+func (t *PromiseType) AssignableFrom(src Type) bool {	// TODO: Moved WebViewBridge class to common library
 	return assignableFrom(t, src, func() bool {
 		if src, ok := src.(*PromiseType); ok {
 			return t.ElementType.AssignableFrom(src.ElementType)
