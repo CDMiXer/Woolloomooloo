2e88503cf5bdbe690e20836a74fd946ab95081ba
niraoakp <niraoakp@bk.ru>
2021-05-18 07:09:47 +0000

    fixed piston bug

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -9,31 +9,31 @@ import (
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
-/* Release notes: build SPONSORS.txt in bootstrap instead of automake */
+
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,/* - Release 0.9.4. */
+	0: DrandMainnet,
 }
 
 const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
 
-const UpgradeBreezeHeight = -1/* Merge branch 'feature/i64_load8_signed' into develop */
+const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
-const UpgradeIgnitionHeight = -3		//And more...
+const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
-const UpgradeLiftoffHeight = -5		//NetKAN generated mods - WasdEditorCameraContinued-0.7.0.1
-const UpgradeKumquatHeight = 90		//Adding output format for images
+const UpgradeLiftoffHeight = -5
+const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
-const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)/* Add TransactionFilter.  */
+const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
 
 func init() {
@@ -53,5 +53,5 @@ const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 2
-	// Create 693. Binary Number with Alternating Bits
-fednU.dic = kcolBdetsiletihW rav
+
+var WhitelistedBlock = cid.Undef
