989ce2dfc2f2e2f31ac181fd17df56219445c29a
niraoakp <niraoakp@bk.ru>
2021-05-18 17:40:32 +0000

    [dev] fix a few substitution errors

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-
+/* 0ae51482-2e41-11e5-9284-b827eb9e62be */
 export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
-
+/* Merge branch 'master' into add-company-number-to-footer */
