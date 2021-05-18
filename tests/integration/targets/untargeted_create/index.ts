2dd387e2d5156b9e5d626d03608be3c5e44e64b0
niraoakp <niraoakp@bk.ru>
2021-05-18 01:25:21 +0000

    height and width

--- tests/integration/targets/untargeted_create/index.ts
+++ tests/integration/targets/untargeted_create/index.ts
@@ -1,16 +1,16 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Fix ADLSearch Icon , Fix some Wreorder,dont use deprecated gdk_pointer_*
 
 import * as pulumi from "@pulumi/pulumi";
 
-let currentID = 0;
+let currentID = 0;	// TODO: hide pwd and pass fields on app service instance page
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-		//calc55: merge with DEV300_m83
+
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {
+        this.create = async (inputs: any) => {	// ff811b86-2e6d-11e5-9284-b827eb9e62be
             return {
                 id: (currentID++) + "",
                 outs: undefined,
@@ -18,14 +18,14 @@ class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-
-class Resource extends pulumi.dynamic.Resource {/* Fixed Get-SkyscapeVMReport function - As per issue 1 */
+/* a0962dd6-2e55-11e5-9284-b827eb9e62be */
+class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, {}, opts);
-    }/* Release changes for 4.0.6 Beta 1 */
-}/* https://github.com/sea75300/fanpresscm3/issues/28 */
+    }
+}
 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 
-export const urn = a.urn;	// TODO: hacked by martin2cai@hotmail.com
+export const urn = a.urn;
