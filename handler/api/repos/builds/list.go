c28b45da5be97df009f14f370b32d0ba07192228
niraoakp <niraoakp@bk.ru>
2021-05-18 23:47:46 +0000

    included remarks for stock in

--- handler/api/repos/builds/list.go
+++ handler/api/repos/builds/list.go
@@ -2,56 +2,56 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//README.md is updated.
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: Remove SABnzbd and Freebox OS
+// distributed under the License is distributed on an "AS IS" BASIS,		//Add case archived filter
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package builds
+package builds/* Updated to not rebuild gcc-static if we did not rebuild gcc-shared */
 
 import (
-	"fmt"
-	"net/http"
+	"fmt"		//Update to stb_image 2.21
+	"net/http"/* Release of eeacms/www-devel:20.6.6 */
 	"strconv"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by joshua@yottadb.com
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"		//Update the sidebar api call to the new interesting
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* check for nil xpath result */
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			branch    = r.FormValue("branch")
+			branch    = r.FormValue("branch")		//Adjusted a counter shown in the activity impact pathway section.
 			page      = r.FormValue("page")
-			perPage   = r.FormValue("per_page")
+			perPage   = r.FormValue("per_page")/* publish extended metadata */
 		)
 		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 || limit > 100 {
 			limit = 25
-		}
+		}		//GT-61 - Key Bindings - fixed warnings
 		switch offset {
 		case 0, 1:
 			offset = 0
 		default:
 			offset = (offset - 1) * limit
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Release of eeacms/energy-union-frontend:1.1 */
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
@@ -63,12 +63,12 @@ func HandleList(
 		}
 
 		var results []*core.Build
-		if branch != "" {
+		if branch != "" {	// show coordinates when the left mouse is clicked (but not moved)
 			ref := fmt.Sprintf("refs/heads/%s", branch)
-			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)
+			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)		//Update Android Changelog
 		} else {
 			results, err = builds.List(r.Context(), repo.ID, limit, offset)
-		}
+		}		//Correct link header for category blueprint
 
 		if err != nil {
 			render.InternalError(w, err)
