32ee64c2da66cbd5d3c0898a5d7e5759eea998b4
niraoakp <niraoakp@bk.ru>
2021-05-18 05:32:05 +0000

    rev 808681

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TMP=$(mktemp -d /tmp/sdk.XXX) \
+TMP=$(mktemp -d /tmp/sdk.XXX) \	// TODO: added ratings tab
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
-&& unzip -q $TMP.zip -d $TMP \
+&& unzip -q $TMP.zip -d $TMP \/* For Windows Platform Users Program */
 && export PATH="$PATH:$TMP/go_appengine"
