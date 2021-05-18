505c7ec82e0d65b9078d4b002f35506a126c341c
niraoakp <niraoakp@bk.ru>
2021-05-18 05:57:32 +0000

    Merge "add hybisocket to support socket and websocket client" into 0.3.x

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
-
+package ints/* modified request mappings */
+/* Merge "Fix requestLayout-during-layout issue with Gallery" */
 import (
 	"testing"
 
@@ -14,7 +14,7 @@ func TestImportAcquire(t *testing.T) {
 	t.Skipf("import does not yet work with dynamic providers")
 
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+		Dir:          "step1",		//Provide explicit list of tables
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
