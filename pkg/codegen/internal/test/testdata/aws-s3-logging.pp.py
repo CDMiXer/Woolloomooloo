2738e248d700c280dbe9a71fa64c85f41eede114
niraoakp <niraoakp@bk.ru>
2021-05-18 00:31:36 +0000

    Capitalisation.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
-import pulumi/* 3bfee570-2e55-11e5-9284-b827eb9e62be */
+import pulumi
 import pulumi_aws as aws
 
 logs = aws.s3.Bucket("logs")
 bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
-    target_bucket=logs.bucket,	// TODO: hacked by nagydani@epointsystem.org
+    target_bucket=logs.bucket,
 )])
 pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
