10b5ba7c1eb1d9e16a19c603d65551c9441c26b5
niraoakp <niraoakp@bk.ru>
2021-05-18 23:40:43 +0000

    Merge "Remove MigrationPreCheckClientException"

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+	// Asking for observed agreement now a method of Agreement
 using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
-    {
+        : base("my:module:Resource", name, options)		//Angrier now
+    {/* added reference types */
     }
 }
 
@@ -15,25 +15,25 @@ class Resource : ComponentResource
 class ComponentThree : ComponentResource
 {
     private Resource resource1;
-    private Resource resource2;
+    private Resource resource2;/* Update bookList.jsp */
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
     {
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// TODO: will be fixed by mowrain@yandex.com
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// TODO: will be fixed by denner@gmail.com
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
 
 class Program
-{
-    static Task<int> Main(string[] args)
+{/* Update SCUI version */
+    static Task<int> Main(string[] args)	// orts freightanim position fix
     {
         return Deployment.RunAsync(() => 
         {
             var comp3 = new ComponentThree("comp3");
         });
     }
-}
+}	// TODO: hacked by arajasek94@gmail.com
