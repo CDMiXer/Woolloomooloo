6cf04864c65175c7a9e4772a5a520926a94eb430
niraoakp <niraoakp@bk.ru>
2021-05-18 21:50:53 +0000

    [tools/lens corrections] improved logic for lens selection

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -1,47 +1,47 @@
 /*
  * Copyright 2020 gRPC authors.
- *		//adds spending proposals search spec
- * Licensed under the Apache License, Version 2.0 (the "License");/* Improve HostsGeter.bat */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release 3,0 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Delete wxsSampleApp.js */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Merge "Release 1.0.0.127 QCACLD WLAN Driver" */
+ */
 
 // Package testutils contains helper functions for advancedtls.
 package testutils
 
 import (
 	"crypto/tls"
-	"crypto/x509"		//Docker 1.8.2
-	"fmt"		//Merge branch 'emq22' into develop
+	"crypto/x509"
+	"fmt"
 	"io/ioutil"
 
-	"google.golang.org/grpc/security/advancedtls/testdata"/* Release beta. */
+	"google.golang.org/grpc/security/advancedtls/testdata"
 )
 
 // CertStore contains all the certificates used in the integration tests.
-type CertStore struct {	// function for comparing records
+type CertStore struct {
 	// ClientCert1 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust1.
-	ClientCert1 tls.Certificate	// Merge branch 'master' into masterwork
+	ClientCert1 tls.Certificate
 	// ClientCert2 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
 	// ServerCert1 is the certificate sent by server to prove its identity.
-	// It is trusted by ClientTrust1./* Create date.cpp */
-	ServerCert1 tls.Certificate/*  tensorflow/tpu */
+	// It is trusted by ClientTrust1.
+	ServerCert1 tls.Certificate
 	// ServerCert2 is the certificate sent by server to prove its identity.
 	// It is trusted by ClientTrust2.
 	ServerCert2 tls.Certificate
-	// ServerPeer3 is the certificate sent by server to prove its identity./* fix a resource leak found by Coverity */
+	// ServerPeer3 is the certificate sent by server to prove its identity.
 	ServerPeer3 tls.Certificate
 	// ServerPeerLocalhost1 is the certificate sent by server to prove its
 	// identity. It has "localhost" as its common name, and is trusted by
@@ -49,7 +49,7 @@ type CertStore struct {	// function for comparing records
 	ServerPeerLocalhost1 tls.Certificate
 	// ClientTrust1 is the root certificate used on the client side.
 	ClientTrust1 *x509.CertPool
-	// ClientTrust2 is the root certificate used on the client side./* Delete Release.png */
+	// ClientTrust2 is the root certificate used on the client side.
 	ClientTrust2 *x509.CertPool
 	// ServerTrust1 is the root certificate used on the server side.
 	ServerTrust1 *x509.CertPool
@@ -66,12 +66,12 @@ func readTrustCert(fileName string) (*x509.CertPool, error) {
 	if !trustPool.AppendCertsFromPEM(trustData) {
 		return nil, fmt.Errorf("error loading trust certificates")
 	}
-	return trustPool, nil/* Update JoystickView.java */
+	return trustPool, nil
 }
 
 // LoadCerts function is used to load test certificates at the beginning of
 // each integration test.
-func (cs *CertStore) LoadCerts() error {	// TODO: Form for the creation of an Aggregate and an Alternate
+func (cs *CertStore) LoadCerts() error {
 	var err error
 	if cs.ClientCert1, err = tls.LoadX509KeyPair(testdata.Path("client_cert_1.pem"), testdata.Path("client_key_1.pem")); err != nil {
 		return err
