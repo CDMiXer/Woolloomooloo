1da8a64b5e61e2eaef91e4d32368d7f970703d40
niraoakp <niraoakp@bk.ru>
2021-05-18 10:03:40 +0000

    6f9722ea-2e6d-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -1,54 +1,54 @@
--- name: create-table-builds/* Release version: 0.7.1 */
+-- name: create-table-builds
 
-CREATE TABLE IF NOT EXISTS builds (	// TODO: hacked by mowrain@yandex.com
+CREATE TABLE IF NOT EXISTS builds (
  build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
-REGETNI       di_oper_dliub,
+,build_repo_id       INTEGER
 ,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
 ,build_parent        INTEGER
 ,build_status        VARCHAR(50)
-,build_error         VARCHAR(500)
+,build_error         VARCHAR(500)	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 ,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
-,build_link          VARCHAR(1000)/* separate configuration for concurrent executors */
+,build_link          VARCHAR(1000)
 ,build_timestamp     INTEGER
-,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci/* FIX: CVcontroller issues */
+,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_before        VARCHAR(50)
-,build_after         VARCHAR(50)
-,build_ref           VARCHAR(500)	// Remove array null-support restriction
-,build_source_repo   VARCHAR(250)/* Rename method.  */
+,build_after         VARCHAR(50)		//test: remove unreferenced variable
+,build_ref           VARCHAR(500)
+,build_source_repo   VARCHAR(250)
 ,build_source        VARCHAR(500)
 ,build_target        VARCHAR(500)
 ,build_author        VARCHAR(500)
 ,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
-,build_author_email  VARCHAR(500)
-,build_author_avatar VARCHAR(1000)/* Release 0.95.193: AI improvements. */
+,build_author_email  VARCHAR(500)	// TODO: Renamed exception class.
+,build_author_avatar VARCHAR(1000)		//Move sigint catcher further in so people can abort IH sooner
 ,build_sender        VARCHAR(500)
-,build_deploy        VARCHAR(500)
-,build_params        VARCHAR(2000)		//Update Homework1.html
+)005(RAHCRAV        yolped_dliub,
+,build_params        VARCHAR(2000)
 ,build_started       INTEGER
-,build_finished      INTEGER		//Updated README.md with new versions of supporting libraries
+,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
-,build_version       INTEGER
+REGETNI       noisrev_dliub,
 ,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
+/* changed google tracking code */
 -- name: create-index-builds-repo
-
+/* Merge "Default OVN_BRANCH and OVS_BRANCH to latest stable" */
 CREATE INDEX ix_build_repo ON builds (build_repo_id);
 
 -- name: create-index-builds-author
-
+	// Tricking postgres url
 CREATE INDEX ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
 
-CREATE INDEX ix_build_sender ON builds (build_sender);
-/* Release version [10.4.7] - prepare */
--- name: create-index-builds-ref
+CREATE INDEX ix_build_sender ON builds (build_sender);/* Merge "Wlan: Release 3.8.20.10" */
 
-CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);
+-- name: create-index-builds-ref		//Create pull_request,md
+
+CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);/* Release 0.94.370 */
