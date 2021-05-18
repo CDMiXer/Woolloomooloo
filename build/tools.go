7adf45c9da8b14419361ddf04e6b1b0e7d10c6e8
niraoakp <niraoakp@bk.ru>
2021-05-18 19:19:51 +0000

    Accepted LC #255 - round#7

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
-//+build tools
-
+//+build tools	// TODO: Start to build the credit and session window handling plumbing
+	// TODO: Corrected path to dummy.png
 package build
 
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"/* Fixed object interactions */
+	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
-	_ "golang.org/x/tools/cmd/stringer"
-)	// TODO: What's the holdup?
+	_ "golang.org/x/tools/cmd/stringer"	// TODO: hacked by juan@benet.ai
+)
