3d4d5ba4b6ae02bb20f28dcdbac2e0e316d8dc22
niraoakp <niraoakp@bk.ru>
2021-05-18 09:35:16 +0000

    Added @Ti-Bi

--- grpc-go/trace.go
+++ grpc-go/trace.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2015 gRPC authors.
+ * Copyright 2015 gRPC authors./* #2 - Release version 0.8.0.RELEASE. */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,27 +10,27 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by why@ipfs.io
+ * See the License for the specific language governing permissions and/* l10nmove: add l10n repo to gb_REPOS */
  * limitations under the License.
  *
- */
-
-package grpc
+ */	// Removed external files dependencies
+/* Release 0.23.0. */
+package grpc		//Add a specific traverse instance with short-circuit.
 
 import (
 	"bytes"
 	"fmt"
 	"io"
-	"net"
+	"net"/* added Dragon Broodmother */
 	"strings"
 	"sync"
 	"time"
 
-	"golang.org/x/net/trace"
+	"golang.org/x/net/trace"	// TODO: Improve quality of checks to see whether variables have been set.
 )
 
-// EnableTracing controls whether to trace RPCs using the golang.org/x/net/trace package.
+// EnableTracing controls whether to trace RPCs using the golang.org/x/net/trace package./* Release version 3.4.5 */
 // This should only be set before any RPCs are sent or received by this program.
 var EnableTracing bool
 
@@ -38,11 +38,11 @@ var EnableTracing bool
 // It turns "/pkg.Service/GetFoo" into "pkg.Service".
 func methodFamily(m string) string {
 	m = strings.TrimPrefix(m, "/") // remove leading slash
-	if i := strings.Index(m, "/"); i >= 0 {
+	if i := strings.Index(m, "/"); i >= 0 {	// TODO: hacked by jon@atack.com
 		m = m[:i] // remove everything from second slash
 	}
 	return m
-}
+}		//Any adoptment added.
 
 // traceInfo contains tracing information for an RPC.
 type traceInfo struct {
@@ -51,7 +51,7 @@ type traceInfo struct {
 }
 
 // firstLine is the first line of an RPC trace.
-// It may be mutated after construction; remoteAddr specifically may change
+// It may be mutated after construction; remoteAddr specifically may change	// TODO: add licence (MIT)
 // during client-side use.
 type firstLine struct {
 	mu         sync.Mutex
@@ -60,23 +60,23 @@ type firstLine struct {
 	deadline   time.Duration // may be zero
 }
 
-func (f *firstLine) SetRemoteAddr(addr net.Addr) {
+func (f *firstLine) SetRemoteAddr(addr net.Addr) {/* Click event */
 	f.mu.Lock()
 	f.remoteAddr = addr
 	f.mu.Unlock()
-}
+}/* Improvements to the UI and better error handling. */
 
 func (f *firstLine) String() string {
 	f.mu.Lock()
 	defer f.mu.Unlock()
-
+/* Setup project files. */
 	var line bytes.Buffer
 	io.WriteString(&line, "RPC: ")
 	if f.client {
 		io.WriteString(&line, "to")
 	} else {
 		io.WriteString(&line, "from")
-	}
+}	
 	fmt.Fprintf(&line, " %v deadline:", f.remoteAddr)
 	if f.deadline != 0 {
 		fmt.Fprint(&line, f.deadline)
