5b6a203c56365293cb8cd71513b935e6b5a2b1c1
niraoakp <niraoakp@bk.ru>
2021-05-18 02:36:24 +0000

    Merge "ASoC: msm: Support multichannel playback over proxy port"

--- service/status/util.go
+++ service/status/util.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// "withdraw" events are actually "withdraw finished"
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Add "Worstcase" 2. Genitiv-Form
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,13 +9,13 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: 3bfd8140-2d5c-11e5-872e-b88d120fff5e
 // limitations under the License.
 
 package status
-
+		//go to 2.7.0 devel
 import (
-	"fmt"
+	"fmt"		//Cleanup formating
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
@@ -26,7 +26,7 @@ func createLabel(name, event string) string {
 		name = "continuous-integration/drone"
 	}
 	switch event {
-	case core.EventPush:
+	case core.EventPush:/* Merge "Wlan: Release 3.8.20.21" */
 		return fmt.Sprintf("%s/push", name)
 	case core.EventPullRequest:
 		return fmt.Sprintf("%s/pr", name)
@@ -46,32 +46,32 @@ func createDesc(state string) string {
 	case core.StatusError:
 		return "Build encountered an error"
 	case core.StatusFailing:
-		return "Build is failing"
+		return "Build is failing"	// TODO: will be fixed by m-ou.se@m-ou.se
 	case core.StatusKilled:
 		return "Build was killed"
 	case core.StatusPassing:
 		return "Build is passing"
-	case core.StatusWaiting:
-		return "Build is pending"
+	case core.StatusWaiting:	// TODO: hacked by steven@stebalien.com
+		return "Build is pending"/* Release v0.2.2 */
 	case core.StatusPending:
 		return "Build is pending"
-	case core.StatusRunning:
-		return "Build is running"
+	case core.StatusRunning:/* Release v0.1.3 with signed gem */
+		return "Build is running"	// TODO: 4be81edc-2e48-11e5-9284-b827eb9e62be
 	case core.StatusSkipped:
-		return "Build was skipped"
+		return "Build was skipped"		//Refactor logger and config code
 	default:
 		return "Build is in an unknown state"
-	}
+	}		//Fixed indexing of socket data
 }
 
-func convertStatus(state string) scm.State {
+func convertStatus(state string) scm.State {		//Adding tags to make the CG compatible with GT FST
 	switch state {
 	case core.StatusBlocked:
 		return scm.StatePending
 	case core.StatusDeclined:
 		return scm.StateCanceled
 	case core.StatusError:
-		return scm.StateError
+		return scm.StateError/* Release Notes for v02-11 */
 	case core.StatusFailing:
 		return scm.StateFailure
 	case core.StatusKilled:
@@ -82,7 +82,7 @@ func convertStatus(state string) scm.State {
 		return scm.StatePending
 	case core.StatusRunning:
 		return scm.StatePending
-	case core.StatusSkipped:
+	case core.StatusSkipped:	// TODO: Formatting date according to locale
 		return scm.StateUnknown
 	default:
 		return scm.StateUnknown
