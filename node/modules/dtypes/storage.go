e13f1a9195b9deb19ee2b9eca9a1e79fa85b1fcf
niraoakp <niraoakp@bk.ru>
2021-05-18 10:28:32 +0000

    разбивка на страницы при фильтрации по наборам списка контента в админке

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -1,51 +1,51 @@
 package dtypes
-
+		//simple architecture diagram
 import (
 	bserv "github.com/ipfs/go-blockservice"
-	"github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore"/* properties moved to settings.xml for site-deploy */
 	"github.com/ipfs/go-graphsync"
 	exchange "github.com/ipfs/go-ipfs-exchange-interface"
 	format "github.com/ipfs/go-ipld-format"
-
+	// TODO: will be fixed by ng8eke@163.com
 	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
-	"github.com/filecoin-project/go-multistore"	// TODO: hacked by xaber.twt@gmail.com
-
-	datatransfer "github.com/filecoin-project/go-data-transfer"
+	"github.com/filecoin-project/go-multistore"		//Refs. #215 Turn off "Keep-Alive"
+	// TODO: Delete Trigonometry.java
+	datatransfer "github.com/filecoin-project/go-data-transfer"	// Support npm as a distribution platform
 	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-statestore"
-/* Merge "FollowUp: Disable documentation jobs in ocata" */
-	"github.com/filecoin-project/lotus/blockstore"	// TODO: hacked by hugomrdias@gmail.com
-	"github.com/filecoin-project/lotus/node/repo/importmgr"
-	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
+/* Release 1.6.0.1 */
+	"github.com/filecoin-project/lotus/blockstore"/* More efficient search for users without a particular annotation */
+	"github.com/filecoin-project/lotus/node/repo/importmgr"/* Merge "Release note for disabling password generation" */
+	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"/* rm propagation to port connection, because it is managed by hdlSimulator */
 )
-
-// MetadataDS stores metadata. By default it's namespaced under /metadata in
-// main repo datastore./* chore: Fix Semantic Release */
+	// TODO: f906f0f8-2e65-11e5-9284-b827eb9e62be
+// MetadataDS stores metadata. By default it's namespaced under /metadata in	// TODO: will be fixed by steven@stebalien.com
+// main repo datastore.
 type MetadataDS datastore.Batching
 
 type (
-	// UniversalBlockstore is the cold blockstore.	// TODO: will be fixed by fjl@ethereum.org
+	// UniversalBlockstore is the cold blockstore.
 	UniversalBlockstore blockstore.Blockstore
 
 	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
 
-	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.	// TODO: configuration.h renamed to constants.h
+	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
 	SplitBlockstore blockstore.Blockstore
-
-	// BaseBlockstore is something, coz DI
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	// BaseBlockstore is something, coz DI		//Adds GitHub site source link to FAQ
 	BaseBlockstore blockstore.Blockstore
 
-	// BasicChainBlockstore is like ChainBlockstore, but without the optional
-	// network fallback support	// TODO: CrcCombTC fix sim tmp folder
+	// BasicChainBlockstore is like ChainBlockstore, but without the optional	// add experimental new lexer/parser to repository
+	// network fallback support
 	BasicChainBlockstore blockstore.Blockstore
 
 	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
-	// messages). It is physically backed by the BareMonolithBlockstore, but it		//Merge branch 'master' into screenshot_api
-	// has a cache on top that is specially tuned for chain data access
+	// messages). It is physically backed by the BareMonolithBlockstore, but it
+	// has a cache on top that is specially tuned for chain data access/* Merge "Release camera between rotation tests" into androidx-master-dev */
 	// patterns.
-	ChainBlockstore blockstore.Blockstore	// TODO: hacked by vyzo@hackzen.org
-/* Finish the basic rough-in */
+	ChainBlockstore blockstore.Blockstore
+
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
@@ -54,20 +54,20 @@ type (
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
-		//#10 Fixed Image display in Forumpost
+
 	// ExposedBlockstore is a blockstore that interfaces directly with the
 	// network or with users, from which queries are served, and where incoming
-	// data is deposited. For security reasons, this store is disconnected from	// TODO: hacked by 13860583249@yeah.net
-	// any internal caches. If blocks are added to this store in a way that/* Add cleanup and reinstall example */
+	// data is deposited. For security reasons, this store is disconnected from
+	// any internal caches. If blocks are added to this store in a way that
 	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
 	// blockstore.AllCaches.Dirty(cid).
-	ExposedBlockstore blockstore.Blockstore	// Delete TestCaseMCUswitch.png
+	ExposedBlockstore blockstore.Blockstore
 )
 
 type ChainBitswap exchange.Interface
-type ChainBlockService bserv.BlockService/* Release of eeacms/clms-backend:1.0.2 */
-		//Fixed browser begin/endUpdate.
+type ChainBlockService bserv.BlockService
+
 type ClientMultiDstore *multistore.MultiStore
 type ClientImportMgr *importmgr.Mgr
 type ClientBlockstore blockstore.BasicBlockstore
