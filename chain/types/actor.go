6a357fe8b07b74d2d935cdb9b06f80e5dd7a2f01
niraoakp <niraoakp@bk.ru>
2021-05-18 16:18:45 +0000

    Add RSS feed for repository.

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -4,12 +4,12 @@ import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
-)
+)		//Rank import.
 
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// TODO: fichier test integration vidéo
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
