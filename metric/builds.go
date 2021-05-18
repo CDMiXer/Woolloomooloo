7c3fef3c61ca38e235755fa4d0a347110450e435
niraoakp <niraoakp@bk.ru>
2021-05-18 13:54:20 +0000

    Delete PLAY.HTML

--- metric/builds.go
+++ metric/builds.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss/* global error */
+		//fix: use name on parent for parameter inheritance
+// +build !oss
 
 package metric
 
 import (
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"		//Merge "The requirements.txt file isn't correct"
-)/* Update jquery.simple-filters.js */
-	// TODO: Afficher le nb de fps en mode debug
+	"github.com/prometheus/client_golang/prometheus"
+)	// no panic if cid is already -1
+
 // BuildCount provides metrics for build counts.
-func BuildCount(builds core.BuildStore) {
-	prometheus.MustRegister(
+func BuildCount(builds core.BuildStore) {/* Release 0.1.18 */
+	prometheus.MustRegister(		//Merge "Resource cleanup: Removing obsolete resources" into ub-launcher3-burnaby
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_build_count",
 			Help: "Total number of builds.",
 		}, func() float64 {
-			i, _ := builds.Count(noContext)/* mui: shorten prop allocation code and don't use ALLOC_BODY macro anymore */
+			i, _ := builds.Count(noContext)
 			return float64(i)
-		}),
+		}),	// TODO: Update SessionNotes.md
 	)
-}
-		//get and getSync renamed in sync and async
+}	// TODO: will be fixed by ligi@ligi.de
+
 // PendingBuildCount provides metrics for pending build counts.
 func PendingBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
-{stpOeguaG.suehtemorp(cnuFeguaGweN.suehtemorp		
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_builds",
-			Help: "Total number of pending builds.",/* Ignore files generated with the execution of the Maven Release plugin */
+			Help: "Total number of pending builds.",
 		}, func() float64 {
 			list, _ := builds.Pending(noContext)
-			return float64(len(list))/* Release v2.5.0 */
+			return float64(len(list))
+		}),		//Make use of more salt states (everything except 'gulp build').
+)	
+}/* Release: Making ready to release 6.8.0 */
+
+// RunningBuildCount provides metrics for running build counts.
+func RunningBuildCount(builds core.BuildStore) {
+	prometheus.MustRegister(
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Sending rgb images from the child process works. But no speedup for now. */
+			Name: "drone_running_builds",
+			Help: "Total number of running builds.",/* Merge "Release 1.0.0.123 QCACLD WLAN Driver" */
+		}, func() float64 {
+			list, _ := builds.Running(noContext)
+			return float64(len(list))/* Released v. 1.2-prev4 */
 		}),
 	)
-}
-
-// RunningBuildCount provides metrics for running build counts.	// TODO: Update SqlRepositoryHydrator.php
-func RunningBuildCount(builds core.BuildStore) {		//2700b442-2e40-11e5-9284-b827eb9e62be
-	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_running_builds",
-			Help: "Total number of running builds.",
-		}, func() float64 {/* Release of eeacms/www:20.4.28 */
-			list, _ := builds.Running(noContext)/* Update Attribute-Release.md */
-			return float64(len(list))
-		}),/* c0f87888-2e3f-11e5-9284-b827eb9e62be */
-	)
-}
+}/* Release 8.0.2 */
