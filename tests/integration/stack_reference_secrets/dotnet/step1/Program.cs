dfa66dc215d4f75018b121926d7d3d0bff21d1cf
niraoakp <niraoakp@bk.ru>
2021-05-18 21:11:06 +0000

    Fix net 1.1 build

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,10 +1,10 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// Update 11.txt
+/* Syntax highlight one block. Acknowledge str.format. */
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
-class Program
+class Program		//apache didnt like a url inside a url
 {
     static Task<int> Main(string[] args)
     {
