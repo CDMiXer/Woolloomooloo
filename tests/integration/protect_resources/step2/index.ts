a4d9a652c5be9422c3c853a90558f66aed8711c9
niraoakp <niraoakp@bk.ru>
2021-05-18 06:11:52 +0000

    Base para el ejercicio 36

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by why@ipfs.io
-import { Resource } from "./resource";
 
+import { Resource } from "./resource";
+		//Tag 1.5.1 (rebased version)
 // Now, simply update the resource; this should work fine:
 let a = new Resource("eternal", { state: 2 }, { protect: true });
