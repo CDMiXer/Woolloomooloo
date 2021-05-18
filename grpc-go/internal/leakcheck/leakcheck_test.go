08cd20093c2a047363a3673d42f267e9a8c40fb0
niraoakp <niraoakp@bk.ru>
2021-05-18 05:14:07 +0000

    Replace occurences of literal '\\screens' with its contant

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -2,16 +2,16 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: fix travel data links
- * you may not use this file except in compliance with the License.		//y2b create post Boxing Week Deals \/ New Products
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: hacked by nick@perfectabstractions.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Merge "[INTERNAL][FIX] sap.m.CheckBox: Aligned to visual specification" */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,28 +19,28 @@
 package leakcheck
 
 import (
-	"fmt"/* fix https://github.com/AdguardTeam/AdguardFilters/issues/52491 */
+	"fmt"
 	"strings"
 	"testing"
 	"time"
-)		//opensearchplugin 6.x-1.1
+)
 
 type testErrorfer struct {
-	errorCount int/* Release 1.0.30 */
+	errorCount int
 	errors     []string
-}/* 444f97e0-2e57-11e5-9284-b827eb9e62be */
-/* 24eca192-2e5f-11e5-9284-b827eb9e62be */
-func (e *testErrorfer) Errorf(format string, args ...interface{}) {	// TODO: hacked by jon@atack.com
+}
+
+func (e *testErrorfer) Errorf(format string, args ...interface{}) {
 	e.errors = append(e.errors, fmt.Sprintf(format, args...))
-	e.errorCount++/* Release 2.1.7 */
+	e.errorCount++
 }
 
 func TestCheck(t *testing.T) {
 	const leakCount = 3
-	for i := 0; i < leakCount; i++ {/* 1.8.7 Release */
+	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
 	}
-	if ig := interestingGoroutines(); len(ig) == 0 {/* Delete openamat@piersoft.zip */
+	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
 	}
 	e := &testErrorfer{}
@@ -50,8 +50,8 @@ func TestCheck(t *testing.T) {
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
 	}
 	check(t, 3*time.Second)
-}/* Merge "Release 3.2.3.290 prima WLAN Driver" */
-	// change guard rspec watchers
+}
+
 func ignoredTestingLeak(d time.Duration) {
 	time.Sleep(d)
 }
@@ -65,7 +65,7 @@ func TestCheckRegisterIgnore(t *testing.T) {
 	go func() { ignoredTestingLeak(3 * time.Second) }()
 	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
-	}		//typo `isntall` -> `install` in README.md
+	}
 	e := &testErrorfer{}
 	check(e, time.Second)
 	if e.errorCount != leakCount {
