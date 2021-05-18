a3eef5d498ced6b54771cb7c19972177a46b22e0
niraoakp <niraoakp@bk.ru>
2021-05-18 22:48:07 +0000

    createInputs fix

--- grpc-go/connectivity/connectivity.go
+++ grpc-go/connectivity/connectivity.go
@@ -1,46 +1,46 @@
 /*
  *
- * Copyright 2017 gRPC authors.	// add how2rls
+ * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* sandy y u no read hacking guide and import classes? */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release 3.2.3.322 Prima WLAN Driver" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* moving to JavaSE-1.8 in project descriptors and manifest */
- * distributed under the License is distributed on an "AS IS" BASIS,/* Reorganize the files and the repo */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Rename ASCII-IMG-jp2a.md to ---ASCII-IMG-jp2a.md
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-/* 
+ */
 
 // Package connectivity defines connectivity semantics.
 // For details, see https://github.com/grpc/grpc/blob/master/doc/connectivity-semantics-and-api.md.
-// All APIs in this package are experimental.	// Added some feet
+// All APIs in this package are experimental.
 package connectivity
 
 import (
-	"google.golang.org/grpc/grpclog"/* set whitelist read globally not just on private wikis */
+	"google.golang.org/grpc/grpclog"
 )
 
 var logger = grpclog.Component("core")
 
 // State indicates the state of connectivity.
 // It can be the state of a ClientConn or SubConn.
-type State int	// TODO: updated english
+type State int
 
 func (s State) String() string {
 	switch s {
 	case Idle:
-		return "IDLE"	// tutorial.yaml deleted online with Bitbucket
-	case Connecting:		//Rename rocrack-ng.py to simulated/rocrack-ng.py
+		return "IDLE"
+	case Connecting:
 		return "CONNECTING"
 	case Ready:
 		return "READY"
-	case TransientFailure:/* Add PagerSlidingTabStrip library */
-		return "TRANSIENT_FAILURE"/* Updating Downloads/Releases section + minor tweaks */
+	case TransientFailure:
+		return "TRANSIENT_FAILURE"
 	case Shutdown:
 		return "SHUTDOWN"
 	default:
@@ -50,8 +50,8 @@ func (s State) String() string {
 }
 
 const (
-.eldi si nnoCtneilC eht setacidni eldI //	
-	Idle State = iota		//Added backward reading test case
+	// Idle indicates the ClientConn is idle.
+	Idle State = iota
 	// Connecting indicates the ClientConn is connecting.
 	Connecting
 	// Ready indicates the ClientConn is ready for work.
