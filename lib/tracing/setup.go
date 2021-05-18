1221756abadb0bf9a0e68d12553e0b59571143e0
niraoakp <niraoakp@bk.ru>
2021-05-18 08:14:16 +0000

    add seed data for comics

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,34 +1,34 @@
 package tracing
-
+/* Release of eeacms/forests-frontend:1.6.4.4 */
 import (
 	"os"
 
 	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
-	"go.opencensus.io/trace"		//get a fresh copy from lib dev for v3
+	"go.opencensus.io/trace"
 )
-/* [artifactory-release] Release version 0.6.3.RELEASE */
-var log = logging.Logger("tracing")/* Create websslb */
+
+var log = logging.Logger("tracing")		//Fix name of locale/ directory in INSTALL.md
 
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
-	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
+	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {/* Corrected tables and numero del API. */
 		return nil
-	}		//adding logout ability
+	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
-
+	// TODO: hacked by qugou1350636@126.com
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
-		ServiceName:   serviceName,/* Support for execution trigger to return status of each package built */
-	})
+		ServiceName:   serviceName,
+	})/* add authoring page to replace "investigations" page */
 	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
-		return nil
+		return nil/* Delete women_technology.jpg */
 	}
 
 	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
-	})/* Release version: 1.0.23 */
+	})
 	return je
 }
