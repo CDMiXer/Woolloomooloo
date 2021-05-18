2808cae57b8daf9d81bd62bfbfeff0156fd268bc
niraoakp <niraoakp@bk.ru>
2021-05-18 02:34:32 +0000

    Created basic mule flow for querying and retrieving documents

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,14 +1,14 @@
-package backend/* Update Layout.md */
-/* Merge "Move Function out of arch:common" into oc-mr1-dev */
+package backend
+
 import (
 	"fmt"
 )
-		//Finalização da classe EstabelecimentoRecursos
+
 // ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
-}		//add ObjectUtil.defaultValue(), ObjectFactory
+}
 
 func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
