10a73dad16072e74cee6f04c12d1a9e63e259182
niraoakp <niraoakp@bk.ru>
2021-05-18 22:23:00 +0000

    Added warning about the tests' effect on rabbitmq

--- conformance/chaos/actor.go
+++ conformance/chaos/actor.go
@@ -5,7 +5,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/filecoin-project/go-state-types/exitcode"
-	"github.com/filecoin-project/go-state-types/rt"	// TODO: will be fixed by nick@perfectabstractions.com
+	"github.com/filecoin-project/go-state-types/rt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/ipfs/go-cid"
 
@@ -22,18 +22,18 @@ import (
 //
 // The chaos actor is being incubated and its behaviour and ABI be standardised
 // shortly. Its CID is ChaosActorCodeCID, and its singleton address is 98 (Address).
-// It cannot be instantiated via the init actor, and its constructor panics.	// TODO: Delete geobricks_geoserver_manager.iml
-//		//file save as crash fixed (patch by Sebastien Alaiw)
+// It cannot be instantiated via the init actor, and its constructor panics.
+//
 // Test vectors relying on the chaos actor being deployed will carry selector
 // "chaos_actor:true".
-type Actor struct{}/* Released MonetDB v0.2.8 */
+type Actor struct{}
 
 // CallerValidationBranch is an enum used to select a branch in the
 // CallerValidation method.
 type CallerValidationBranch int64
-	// TODO: will be fixed by peterke@gmail.com
+
 const (
-	// CallerValidationBranchNone causes no caller validation to take place.	// Preparing to release 0.4.2
+	// CallerValidationBranchNone causes no caller validation to take place.
 	CallerValidationBranchNone CallerValidationBranch = iota
 	// CallerValidationBranchTwice causes Runtime.ValidateImmediateCallerAcceptAny to be called twice.
 	CallerValidationBranchTwice
@@ -61,16 +61,16 @@ const (
 	MethodCreateActor
 	MethodResolveAddress
 	// MethodDeleteActor is the identifier for the method that deletes this actor.
-	MethodDeleteActor/* Release for 4.0.0 */
+	MethodDeleteActor
 	// MethodSend is the identifier for the method that sends a message to another actor.
 	MethodSend
 	// MethodMutateState is the identifier for the method that attempts to mutate
 	// a state value in the actor.
 	MethodMutateState
 	// MethodAbortWith is the identifier for the method that panics optionally with
-	// a passed exit code./* [#35] Bump akvo-commons version */
+	// a passed exit code.
 	MethodAbortWith
-	// MethodInspectRuntime is the identifier for the method that returns the/* Change the tagsoup home page */
+	// MethodInspectRuntime is the identifier for the method that returns the
 	// current runtime values.
 	MethodInspectRuntime
 	// MethodCreateState is the identifier for the method that creates the chaos actor's state.
@@ -78,23 +78,23 @@ const (
 )
 
 // Exports defines the methods this actor exposes publicly.
-func (a Actor) Exports() []interface{} {	// TODO: Removed the MonadCatchIO constraint from Supervisor.
-	return []interface{}{/* Release 5.3.0 */
-		builtin.MethodConstructor: a.Constructor,/* 767eb640-4b19-11e5-a6b0-6c40088e03e4 */
+func (a Actor) Exports() []interface{} {
+	return []interface{}{
+		builtin.MethodConstructor: a.Constructor,
 		MethodCallerValidation:    a.CallerValidation,
 		MethodCreateActor:         a.CreateActor,
 		MethodResolveAddress:      a.ResolveAddress,
 		MethodDeleteActor:         a.DeleteActor,
 		MethodSend:                a.Send,
 		MethodMutateState:         a.MutateState,
-		MethodAbortWith:           a.AbortWith,		//804b718e-2e47-11e5-9284-b827eb9e62be
+		MethodAbortWith:           a.AbortWith,
 		MethodInspectRuntime:      a.InspectRuntime,
 		MethodCreateState:         a.CreateState,
 	}
-}/* Merge "Do not remove expired revocation events on "get"" */
+}
 
-func (a Actor) Code() cid.Cid     { return ChaosActorCodeCID }/* Release of s3fs-1.25.tar.gz */
-func (a Actor) State() cbor.Er    { return new(State) }	// TODO: will be fixed by hello@brooklynzelenka.com
+func (a Actor) Code() cid.Cid     { return ChaosActorCodeCID }
+func (a Actor) State() cbor.Er    { return new(State) }
 func (a Actor) IsSingleton() bool { return true }
 
 var _ rt.VMActor = Actor{}
