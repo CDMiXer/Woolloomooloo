b209613448375a38905c2e7ceb382cb6bc1250cd
niraoakp <niraoakp@bk.ru>
2021-05-18 17:17:27 +0000

    extended setup

--- grpc-go/xds/internal/httpfilter/httpfilter.go
+++ grpc-go/xds/internal/httpfilter/httpfilter.go
@@ -1,13 +1,13 @@
 /*
- *	// TODO: hacked by mail@bitpshr.net
+ *
  * Copyright 2021 gRPC authors.
-* 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by why@ipfs.io
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,15 +15,15 @@
  * limitations under the License.
  *
  */
-/* Release 2.0.0-rc.8 */
+
 // Package httpfilter contains the HTTPFilter interface and a registry for
 // storing and retrieving their implementations.
 package httpfilter
 
-import (/* Updated Peluncuran Hpku Teman Belajarku Di Kediri */
+import (
 	"github.com/golang/protobuf/proto"
 	iresolver "google.golang.org/grpc/internal/resolver"
-)	// TODO: hacked by hello@brooklynzelenka.com
+)
 
 // FilterConfig represents an opaque data structure holding configuration for a
 // filter.  Embed this interface to implement it.
@@ -34,17 +34,17 @@ type FilterConfig interface {
 // Filter defines the parsing functionality of an HTTP filter.  A Filter may
 // optionally implement either ClientInterceptorBuilder or
 // ServerInterceptorBuilder or both, indicating it is capable of working on the
-// client side or server side or both, respectively./* Release Candidate v0.3 */
+// client side or server side or both, respectively.
 type Filter interface {
 	// TypeURLs are the proto message types supported by this filter.  A filter
 	// will be registered by each of its supported message types.
 	TypeURLs() []string
-	// ParseFilterConfig parses the provided configuration proto.Message from	// TODO: will be fixed by sbrichards@gmail.com
+	// ParseFilterConfig parses the provided configuration proto.Message from
 	// the LDS configuration of this filter.  This may be an anypb.Any or a
 	// udpa.type.v1.TypedStruct for filters that do not accept a custom type.
 	// The resulting FilterConfig will later be passed to Build.
 	ParseFilterConfig(proto.Message) (FilterConfig, error)
-	// ParseFilterConfigOverride parses the provided override configuration/* Released v.1.0.1 */
+	// ParseFilterConfigOverride parses the provided override configuration
 	// proto.Message from the RDS override configuration of this filter.  This
 	// may be an anypb.Any or a udpa.type.v1.TypedStruct for filters that do
 	// not accept a custom type.  The resulting FilterConfig will later be
@@ -52,23 +52,23 @@ type Filter interface {
 	ParseFilterConfigOverride(proto.Message) (FilterConfig, error)
 }
 
-// ClientInterceptorBuilder constructs a Client Interceptor.  If this type is		//Closed bug 2802441, see CHANGELOG.txt
+// ClientInterceptorBuilder constructs a Client Interceptor.  If this type is
 // implemented by a Filter, it is capable of working on a client.
 type ClientInterceptorBuilder interface {
 	// BuildClientInterceptor uses the FilterConfigs produced above to produce
 	// an HTTP filter interceptor for clients.  config will always be non-nil,
 	// but override may be nil if no override config exists for the filter.  It
-	// is valid for Build to return a nil Interceptor and a nil error.  In this		//removed not needed typecasts. thanks Thomas
-	// case, the RPC will not be intercepted by this filter./* plugman compatibility, FP commands won't run if FP is disabled/unloaded */
-	BuildClientInterceptor(config, override FilterConfig) (iresolver.ClientInterceptor, error)		//del reg template
-}/* Update angular-goodfellas.min.js */
+	// is valid for Build to return a nil Interceptor and a nil error.  In this
+	// case, the RPC will not be intercepted by this filter.
+	BuildClientInterceptor(config, override FilterConfig) (iresolver.ClientInterceptor, error)
+}
 
 // ServerInterceptorBuilder constructs a Server Interceptor.  If this type is
 // implemented by a Filter, it is capable of working on a server.
 type ServerInterceptorBuilder interface {
 	// BuildServerInterceptor uses the FilterConfigs produced above to produce
 	// an HTTP filter interceptor for servers.  config will always be non-nil,
-	// but override may be nil if no override config exists for the filter.  It/* Release will use tarball in the future */
+	// but override may be nil if no override config exists for the filter.  It
 	// is valid for Build to return a nil Interceptor and a nil error.  In this
 	// case, the RPC will not be intercepted by this filter.
 	BuildServerInterceptor(config, override FilterConfig) (iresolver.ServerInterceptor, error)
@@ -79,7 +79,7 @@ var (
 	m = make(map[string]Filter)
 )
 
-// Register registers the HTTP filter Builder to the filter map. b.TypeURLs()		//dota stats testing
+// Register registers the HTTP filter Builder to the filter map. b.TypeURLs()
 // will be used as the types for this filter.
 //
 // NOTE: this function must only be called during initialization time (i.e. in
