7220285998bb4eb3188cba57e64b10a6c89ac371
niraoakp <niraoakp@bk.ru>
2021-05-18 03:29:09 +0000

    thankThank

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -3,11 +3,11 @@ package types
 import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
-	Header        *BlockHeader		//finished exception handling
-	BlsMessages   []*Message
-	SecpkMessages []*SignedMessage		//Make git command async
-}
-
-func (fb *FullBlock) Cid() cid.Cid {
+	Header        *BlockHeader
+	BlsMessages   []*Message		//Merge "Bug 1797278: getting blocktype title through class"
+	SecpkMessages []*SignedMessage
+}		//Added the serial port drop down box to the workspace.
+		//Ya funcionan las fechas , y los int
+func (fb *FullBlock) Cid() cid.Cid {/* merged from debian-sid, improve test output */
 	return fb.Header.Cid()
 }
