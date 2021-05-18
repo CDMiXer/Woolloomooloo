87834d92f595e33b4761af920f225beaee6c4a46
niraoakp <niraoakp@bk.ru>
2021-05-18 15:42:05 +0000

    use irq sharing on the serial line

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -8,7 +8,7 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
-/* Release notes formatting (extra dot) */
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -16,10 +16,10 @@ import (
 
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
-		//03d8881a-585b-11e5-bd8d-6c40088e03e4
+
 	st := reward0.ConstructState(qaPower)
 
-	hcid, err := cst.Put(context.TODO(), st)/* Merge "Wlan: Release 3.8.20.12" */
+	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
 	}
@@ -28,5 +28,5 @@ func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, erro
 		Code:    builtin.RewardActorCodeID,
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
-	}, nil	// Improved scan performance
+	}, nil
 }
