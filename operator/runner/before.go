605368594c7f7766d4fae45a375f3a7d1e052040
niraoakp <niraoakp@bk.ru>
2021-05-18 18:40:49 +0000

    Very basic searching on item's description/name. Can extend later

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -4,12 +4,12 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release new version 2.4.18: Retire the app version (famlam) */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Add not so that these tests pass with pipefail enabled.
+// See the License for the specific language governing permissions and/* Fixed bug related to the output of the reset command */
+// limitations under the License./* Merge branch 'master' into T223948-media-player */
 
 package runner
