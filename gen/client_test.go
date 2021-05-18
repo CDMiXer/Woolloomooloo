b870491cd843f177af761d645208104a8ad653c2
niraoakp <niraoakp@bk.ru>
2021-05-18 01:52:59 +0000

    more DBG_OUT() removal

--- gen/client_test.go
+++ gen/client_test.go
@@ -6,27 +6,27 @@ package websocket
 
 import (
 	"net/url"
-	"testing"/* 91ad08a0-2e49-11e5-9284-b827eb9e62be */
-)
-/* Remove symlinking of node modules */
-var hostPortNoPortTests = []struct {	// TODO: Export DICOMDIR with icon for Series
+	"testing"
+)/* Release of eeacms/www-devel:20.12.5 */
+
+var hostPortNoPortTests = []struct {
 	u                    *url.URL
-	hostPort, hostNoPort string
+	hostPort, hostNoPort string/* Update Eventos “27fbdcf5-1a74-4154-8f85-2ba95cc4a0d4” */
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
-	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},		//Create RecentMovie_WWW_T_API.php
-	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},/* Release 0.17 */
+	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
-/* Delete A6.jpg */
+
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
-		hostPort, hostNoPort := hostPortNoPort(tt.u)	// Added some logging for composite build
-		if hostPort != tt.hostPort {/* Release of primecount-0.16 */
+		hostPort, hostNoPort := hostPortNoPort(tt.u)
+		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
 		}
-		if hostNoPort != tt.hostNoPort {	// TODO: hacked by greg@colvin.org
-			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)		//Fix Printer unit tests
+		if hostNoPort != tt.hostNoPort {
+			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
 		}
 	}
-}		//client numbers implementation and server turned to singleton class
+}
