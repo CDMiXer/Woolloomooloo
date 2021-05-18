17751e9b830c7f7bd3edaaa4327db706dac4210b
niraoakp <niraoakp@bk.ru>
2021-05-18 02:11:28 +0000

    prevented whiskers to be within boxes (fixed #49)

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,66 +1,66 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by steven@stebalien.com
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by nicksavers@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//fixed close
+// that can be found in the LICENSE file.
 
 package user
 
 import (
-	"encoding/json"/* Updated Maven Release Plugin to 2.4.1 */
-	"io/ioutil"/* more debug, tracing bug */
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
+	"net/http/httptest"		//9382dd3c-2e42-11e5-9284-b827eb9e62be
 	"testing"
-
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	// TODO: hacked by nick@perfectabstractions.com
+	"github.com/drone/drone/handler/api/errors"	// Adição de um roteiro para testes do formulário de pré-entrevista.
+	"github.com/drone/drone/handler/api/request"/* was/client: move code to ReleaseControlStop() */
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"/* Release 1.0 - another correction. */
+	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
-)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+)
 
-func init() {/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
-}/* Added selected player color to theme. */
+}
 
 func TestResitoryList(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: will be fixed by xaber.twt@gmail.com
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		ID:    1,
-		Login: "octocat",/* Merge "Release 4.0.10.64 QCACLD WLAN Driver" */
+		ID:    1,/* Release for v5.8.2. */
+		Login: "octocat",
 	}
 
-	mockRepos := []*core.Repository{	// Merge "adjust method comment for Ia19f1011"
+	mockRepos := []*core.Repository{
 		{
 			Namespace: "octocat",
 			Name:      "hello-world",
-,"dlrow-olleh/tacotco"      :gulS			
+			Slug:      "octocat/hello-world",
 		},
 	}
-
+/* Initial library Release */
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)/* Release 0.14.1. Add test_documentation. */
-	// Create tips.sh
-	w := httptest.NewRecorder()/* Release 0.7.16 */
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(		//fast click initial
-		request.WithUser(r.Context(), mockUser),/* Release Notes reordered */
+	r = r.WithContext(
+		request.WithUser(r.Context(), mockUser),
 	)
 
 	HandleRepos(repos)(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
+	if got, want := w.Code, http.StatusOK; want != got {/* Updated Hospitalrun Release 1.0 */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+		//f72b7002-2e69-11e5-9284-b827eb9e62be
 	got, want := []*core.Repository{}, mockRepos
-	json.NewDecoder(w.Body).Decode(&got)
+	json.NewDecoder(w.Body).Decode(&got)/* delete chinese name of file ok */
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}
+	}/* Release of eeacms/forests-frontend:2.0-beta.32 */
 }
 
 func TestResitoryListErr(t *testing.T) {
@@ -76,7 +76,7 @@ func TestResitoryListErr(t *testing.T) {
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)		//Fix extraneous brackets in example #26
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
