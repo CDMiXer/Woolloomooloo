0da7cb7fb564c7b2410d6dfd3139c552f9229650
niraoakp <niraoakp@bk.ru>
2021-05-18 01:36:08 +0000

    clarify resolution of grammatical ambiguity

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,14 +1,14 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//added support for http.proxyAuth
+// +build !oss
 
 package system
 
 import (
 	"io/ioutil"
-/* Update get-attachments.php */
+
 	"github.com/sirupsen/logrus"
 )
 
