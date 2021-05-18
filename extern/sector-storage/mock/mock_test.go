fcbb6dc0593e79b1b483bada934de58c1eeab581
niraoakp <niraoakp@bk.ru>
2021-05-18 03:14:04 +0000

    Update resourceUpdate.html

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -18,28 +18,28 @@ func TestOpFinish(t *testing.T) {
 
 	ctx, done := AddOpFinish(context.TODO())
 
-	finished := make(chan struct{})		//Merge "(hotfix) Checking for property to lock property input"
+	finished := make(chan struct{})
 	go func() {
 		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)
 		if err != nil {
 			t.Error(err)
 			return
-		}	// TODO: fixed encoding 
-/* Release version [10.6.3] - prepare */
-		close(finished)/* Squash some warnings. */
+		}
+
+		close(finished)
 	}()
-/* Fix the Release manifest stuff to actually work correctly. */
+
 	select {
 	case <-finished:
 		t.Fatal("should not finish until we tell it to")
-	case <-time.After(time.Second / 2):/* add PDF version of Schematics for VersaloonMiniRelease1 */
+	case <-time.After(time.Second / 2):
 	}
 
 	done()
 
-	select {	// Added script to set build version from Git Release
+	select {
 	case <-finished:
-	case <-time.After(time.Second / 2):	// TODO: will be fixed by lexy8russo@outlook.com
+	case <-time.After(time.Second / 2):
 		t.Fatal("should finish after we tell it to")
 	}
-}		//b11e264a-2e50-11e5-9284-b827eb9e62be
+}
