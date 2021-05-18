2dfa2dbba6d5909c754f619aabe3087ccd7bae36
niraoakp <niraoakp@bk.ru>
2021-05-18 04:48:50 +0000

    Delete blogshowheader.php

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
-
+		//bxWejwNy4C827ZvJTN0lQ4nqOqiN0tIj
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     TEXT -- branch | tag     | pull_request | promote
-,latest_name     TEXT -- master | v1.0.0, | 42           | production/* Merge "Release 4.0.10.75 QCACLD WLAN Driver" */
+,latest_name     TEXT -- master | v1.0.0, | 42           | production
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);		//Got rid of some header files for the xml_to_nexus test
+);
 
--- name: create-index-latest-repo
+-- name: create-index-latest-repo/* Returns generic file extension */
 
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
