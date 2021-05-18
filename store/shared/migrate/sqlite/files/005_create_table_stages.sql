307a5ba4ed25329583e9d507a0b76d9373b2ebcb
niraoakp <niraoakp@bk.ru>
2021-05-18 17:58:24 +0000

    Add upgrade instructions

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,41 +1,41 @@
 -- name: create-table-stages
 
-CREATE TABLE IF NOT EXISTS stages (	// TODO: will be fixed by mail@bitpshr.net
+CREATE TABLE IF NOT EXISTS stages (
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,stage_repo_id     INTEGER
+,stage_repo_id     INTEGER	// First attempt at test coverage via coveralls.io
 ,stage_build_id    INTEGER
-,stage_number      INTEGER
+,stage_number      INTEGER	// TODO: will be fixed by ng8eke@163.com
 ,stage_kind        TEXT
 ,stage_type        TEXT
 ,stage_name        TEXT
-,stage_status      TEXT
-,stage_error       TEXT/* Merge "Release 3.0.10.052 Prima WLAN Driver" */
-,stage_errignore   BOOLEAN
-,stage_exit_code   INTEGER		//change button caption if nomad is unreachable to 'Unknown'
-,stage_limit       INTEGER
+,stage_status      TEXT		//started creating basic inspectors and constructos
+,stage_error       TEXT
+,stage_errignore   BOOLEAN/* Update to match new org */
+,stage_exit_code   INTEGER
+,stage_limit       INTEGER		//getLevel added to paratree
 ,stage_os          TEXT
-,stage_arch        TEXT
+,stage_arch        TEXT/* Removed a little whitespace */
 ,stage_variant     TEXT
-,stage_kernel      TEXT	// Merge branch 'feature/delegates'
+,stage_kernel      TEXT
 ,stage_machine     TEXT
-,stage_started     INTEGER		//Update CHANGELOG for PR #2698 [skip ci]
-,stage_stopped     INTEGER
-,stage_created     INTEGER/* d289e626-2fbc-11e5-b64f-64700227155b */
+,stage_started     INTEGER
+,stage_stopped     INTEGER/* DATASOLR-257 - Release version 1.5.0.RELEASE (Gosling GA). */
+,stage_created     INTEGER
 ,stage_updated     INTEGER
-,stage_version     INTEGER	// Merge branch 'master' of https://github.com/bergmanlab/ngs_te_mapper.git
-,stage_on_success  BOOLEAN		//change currentTimeMillis() to nanoTime()
+,stage_version     INTEGER
+,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
-,UNIQUE(stage_build_id, stage_number)/* Update Hi.swift */
+,UNIQUE(stage_build_id, stage_number)
 ,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
 );
-	// TODO: will be fixed by arajasek94@gmail.com
+
 -- name: create-index-stages-build
-	// TODO: hacked by witek@enjin.io
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);	// Coordinator portal activation
+
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);/* Change the processor artifact ID from “processor” to “lightcyle-processor”. */
 
 -- name: create-index-stages-status
 
-CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)		//added update from game model.
+CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
 WHERE stage_status IN ('pending', 'running');
