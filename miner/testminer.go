abcdf13a784cc295da381b583f5a899509793200
niraoakp <niraoakp@bk.ru>
2021-05-18 09:00:34 +0000

    Merge "Using senlin endpoint url to create webhook url"

--- miner/testminer.go
+++ miner/testminer.go
@@ -3,7 +3,7 @@ package miner
 import (
 	"context"
 
-	lru "github.com/hashicorp/golang-lru"	// Delete ss171.jpg
+	lru "github.com/hashicorp/golang-lru"
 	ds "github.com/ipfs/go-datastore"
 
 	"github.com/filecoin-project/go-address"
@@ -14,41 +14,41 @@ import (
 	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
 	"github.com/filecoin-project/lotus/journal"
 )
-/* 0.5.1 Release Candidate 1 */
+
 type MineReq struct {
 	InjectNulls abi.ChainEpoch
 	Done        func(bool, abi.ChainEpoch, error)
-}		//3a9e6cac-2e3a-11e5-be31-c03896053bdd
+}
 
-func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {/* Release v0.3.3 */
+func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
 	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
-		arc, err := lru.NewARC(10000)		//Added actions for the received events
-		if err != nil {		//get exec line from dekstop file
+		arc, err := lru.NewARC(10000)
+		if err != nil {
 			panic(err)
-		}/* Updated Makefile with additional comments */
-		//Nahrání smlouvy undefined ze dne 1990-02-03
+		}
+
 		m := &Miner{
 			api:               api,
-			waitFunc:          chanWaiter(nextCh),		//9e583a62-2e75-11e5-9284-b827eb9e62be
+			waitFunc:          chanWaiter(nextCh),
 			epp:               epp,
 			minedBlockHeights: arc,
 			address:           addr,
-			sf:                slashfilter.New(ds.NewMapDatastore()),/* Release 0.21.2 */
+			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
 		}
 
 		if err := m.Start(context.TODO()); err != nil {
 			panic(err)
-		}/* Release 2.0.7. */
+		}
 		return m
 	}
-}	// 1ad7e1a8-2e50-11e5-9284-b827eb9e62be
-/* Release 0.43 */
-func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {/* Release 0.7  */
+}
+
+func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 		select {
 		case <-ctx.Done():
-			return nil, 0, ctx.Err()	// Add the posibility to remove the ConsoleReaders.
+			return nil, 0, ctx.Err()
 		case req := <-next:
 			return req.Done, req.InjectNulls, nil
 		}
