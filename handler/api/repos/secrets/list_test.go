931b30617584b904d66788e7371085afbe6003a8
niraoakp <niraoakp@bk.ru>
2021-05-18 17:19:07 +0000

    removal of Cli service integration of new api/cli with runtime

--- handler/api/repos/secrets/list_test.go
+++ handler/api/repos/secrets/list_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//New translations activerecord.yml (Spanish, El Salvador)
+
 // +build !oss
 
 package secrets
 
 import (
-	"context"/* 3.8.4 Release */
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -21,12 +21,12 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-/* Release 0.9.1.1 */
+
 var (
 	dummySecretRepo = &core.Repository{
 		ID:        1,
-		Namespace: "octocat",	// TODO: hacked by ligi@ligi.de
-		Name:      "hello-world",/* Modify parameters in README. */
+		Namespace: "octocat",
+		Name:      "hello-world",
 	}
 
 	dummySecret = &core.Secret{
@@ -50,40 +50,40 @@ var (
 	}
 )
 
-///* add ProRelease3 hardware */
+//
 // HandleList
-//		//Respect 'markdown.breaks' setting.
+//
 
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* Bugfix "block_min_correct" */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-	// added:  an option for linking locally
+
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().List(gomock.Any(), dummySecretRepo.ID).Return(dummySecretList, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Cleaning Up. Getting Ready for 1.1 Release */
+	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* separate tag releases, add release and snapshot command */
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleList(repos, secrets).ServeHTTP(w, r)/* add %{?dist} to Release */
-	if got, want := w.Code, http.StatusOK; want != got {	// TODO: hacked by aeongrp@outlook.com
+	HandleList(repos, secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Secret{}, dummySecretListScrubbed
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* fix email formatting */
-	}/* first omniauth tests */
+		t.Errorf(diff)
+	}
 }
 
 func TestHandleList_RepoNotFound(t *testing.T) {
@@ -91,7 +91,7 @@ func TestHandleList_RepoNotFound(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)/* Add RegisterRegion function to Marker API */
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
