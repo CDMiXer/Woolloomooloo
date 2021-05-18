89ae5c7f8b976035e8c47363e08fba6eaf773386
niraoakp <niraoakp@bk.ru>
2021-05-18 04:37:38 +0000

    tips & tricks with command line

--- server/auth/webhook/bitbucket.go
+++ server/auth/webhook/bitbucket.go
@@ -1,35 +1,35 @@
 package webhook
 
-import (
+import (		//[published]
 	"net/http"
 
 	"gopkg.in/go-playground/webhooks.v5/bitbucket"
 )
 
 func bitbucketMatch(secret string, r *http.Request) bool {
-	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))
+	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))/* Release v0.2.11 */
 	if err != nil {
 		return false
 	}
-	_, err = hook.Parse(r,
-		bitbucket.RepoPushEvent,
-		bitbucket.RepoForkEvent,
+	_, err = hook.Parse(r,/* Created from GithubApi 0.2528059396427125 */
+		bitbucket.RepoPushEvent,/* Fix bug in DiscussionModel->SetField() when called with an array. */
+		bitbucket.RepoForkEvent,/* V1.0 Release */
 		bitbucket.RepoUpdatedEvent,
 		bitbucket.RepoCommitCommentCreatedEvent,
 		bitbucket.RepoCommitStatusCreatedEvent,
-		bitbucket.RepoCommitStatusUpdatedEvent,
+		bitbucket.RepoCommitStatusUpdatedEvent,/* Delete helloPush.iml */
 		bitbucket.IssueCreatedEvent,
 		bitbucket.IssueUpdatedEvent,
 		bitbucket.IssueCommentCreatedEvent,
 		bitbucket.PullRequestCreatedEvent,
 		bitbucket.PullRequestUpdatedEvent,
-		bitbucket.PullRequestApprovedEvent,
+		bitbucket.PullRequestApprovedEvent,	// TODO: statechart tweaks
 		bitbucket.PullRequestUnapprovedEvent,
 		bitbucket.PullRequestMergedEvent,
-		bitbucket.PullRequestDeclinedEvent,
+		bitbucket.PullRequestDeclinedEvent,/* Release 0.94.421 */
 		bitbucket.PullRequestCommentCreatedEvent,
 		bitbucket.PullRequestCommentUpdatedEvent,
-		bitbucket.PullRequestCommentDeletedEvent,
+		bitbucket.PullRequestCommentDeletedEvent,	// TODO: hacked by greg@colvin.org
 	)
-	return err == nil
+	return err == nil/* Updates faq/faq.md */
 }
