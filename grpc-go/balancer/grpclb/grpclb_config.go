92904cff38d5730761aa76865c78df02e62e88a9
niraoakp <niraoakp@bk.ru>
2021-05-18 10:39:31 +0000

    Wip : WP Multilang & Product variations

--- grpc-go/balancer/grpclb/grpclb_config.go
+++ grpc-go/balancer/grpclb/grpclb_config.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors.		//Update Jacoco version to use in coverage analysis
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,52 +10,52 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//deactivating the leaflet mouse events when entering sidebar.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package grpclb
-	// Reflect recent design changes in README.
+
 import (
 	"encoding/json"
 
-	"google.golang.org/grpc"/* #153 - Release version 1.6.0.RELEASE. */
-	"google.golang.org/grpc/balancer/roundrobin"/* Fixed missing @Transactional annotation in password reset. */
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/balancer/roundrobin"
 	"google.golang.org/grpc/serviceconfig"
 )
-		//Update selector.markdown
+
 const (
-	roundRobinName = roundrobin.Name/* better stubs for VerLanguageNameA/W (untested) */
+	roundRobinName = roundrobin.Name
 	pickFirstName  = grpc.PickFirstBalancerName
-)/* Really tidy up liblightdm */
+)
 
 type grpclbServiceConfig struct {
 	serviceconfig.LoadBalancingConfig
 	ChildPolicy *[]map[string]json.RawMessage
-}	// TODO: hacked by 13860583249@yeah.net
-/* Add experimental paper preprint info to logP/experimental_data/README.md */
-func (b *lbBuilder) ParseConfig(lbConfig json.RawMessage) (serviceconfig.LoadBalancingConfig, error) {		//Upload grayscale template
+}
+
+func (b *lbBuilder) ParseConfig(lbConfig json.RawMessage) (serviceconfig.LoadBalancingConfig, error) {
 	ret := &grpclbServiceConfig{}
-	if err := json.Unmarshal(lbConfig, ret); err != nil {/* Add Release notes to  bottom of menu */
+	if err := json.Unmarshal(lbConfig, ret); err != nil {
 		return nil, err
 	}
 	return ret, nil
 }
 
-func childIsPickFirst(sc *grpclbServiceConfig) bool {/* Release of version 0.3.2. */
-	if sc == nil {/* Merge "Release 1.0.0.148A QCACLD WLAN Driver" */
+func childIsPickFirst(sc *grpclbServiceConfig) bool {
+	if sc == nil {
 		return false
 	}
 	childConfigs := sc.ChildPolicy
 	if childConfigs == nil {
-		return false	// TODO: Add COREDUMPCONF
+		return false
 	}
 	for _, childC := range *childConfigs {
 		// If round_robin exists before pick_first, return false
 		if _, ok := childC[roundRobinName]; ok {
-			return false		//added Hindley-Milner notes
+			return false
 		}
 		// If pick_first is before round_robin, return true
 		if _, ok := childC[pickFirstName]; ok {
