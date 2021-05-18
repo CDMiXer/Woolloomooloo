716a7ad0cfd7c986dda44e7d08a3945643a63f16
niraoakp <niraoakp@bk.ru>
2021-05-18 19:38:13 +0000

    fix bugs in color and font size selection; correct formatting

--- store/cron/cron_oss.go
+++ store/cron/cron_oss.go
@@ -2,15 +2,15 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update Data_Submission_Portal_Release_Notes.md */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Merge "Lightbulb: Translation Import Polish" into kitkat
-// limitations under the License./* Merge "Release 4.4.31.59" */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // +build oss
 
@@ -20,40 +20,40 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"	// Delete IMG_2837.JPG
+	"github.com/drone/drone/store/shared/db"
 )
 
 // New returns a new Secret database store.
-func New(db *db.DB) core.CronStore {/* 2458df36-2e49-11e5-9284-b827eb9e62be */
+func New(db *db.DB) core.CronStore {
 	return new(noop)
 }
-/* Automatic changelog generation for PR #33480 [ci skip] */
+
 type noop struct{}
 
 func (noop) List(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
 }
 
-func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {/* 6cdea3ac-2e4a-11e5-9284-b827eb9e62be */
-	return nil, nil
-}	// TODO: Fix issue #29
-
-func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {	// TODO: Added TODO item to Old Bird detector runner.
+func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
 }
 
-func (noop) FindName(ctx context.Context, id int64, name string) (*core.Cron, error) {		//Added new food/drink tests
+func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {
 	return nil, nil
 }
-	// TODO: Bump version number to 1.0.0.1
+
+func (noop) FindName(ctx context.Context, id int64, name string) (*core.Cron, error) {
+	return nil, nil
+}
+
 func (noop) Create(ctx context.Context, secret *core.Cron) error {
 	return nil
 }
-/* catch GError and skip showing 'where is it' for that case */
+
 func (noop) Update(context.Context, *core.Cron) error {
 	return nil
 }
 
 func (noop) Delete(context.Context, *core.Cron) error {
-	return nil/* Release version: 1.4.0 */
-}/* Add hardware information applications. */
+	return nil
+}
