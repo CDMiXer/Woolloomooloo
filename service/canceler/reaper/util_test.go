52955c41a65513963854902b4e2d1b03515a3ad6
niraoakp <niraoakp@bk.ru>
2021-05-18 10:07:15 +0000

    Rename into phoenix mjml

--- service/canceler/reaper/util_test.go
+++ service/canceler/reaper/util_test.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Adds parsedown tests */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package reaper
 
-import (
+import (/* [artifactory-release] Release version 3.1.0.RC2 */
 	"testing"
-	"time"
+	"time"/* Restored conflict that was lost in the merge. */
 )
-
+		//novo theme
 func TestIsExceeded(t *testing.T) {
 	defer func() {
 		now = time.Now
 	}()
 	now = func() time.Time {
 		return mustParse("2006-01-02T15:00:00")
-	}
+	}/* Merge "Release 3.2.3.373 Prima WLAN Driver" */
 	var tests = []struct {
 		unix     int64
 		timeout  time.Duration
-		buffer   time.Duration
+		buffer   time.Duration	// TODO: Removing some unneeded stuff
 		exceeded bool
 	}{
 		// timestamp equal to current time, not expired
@@ -28,11 +28,11 @@ func TestIsExceeded(t *testing.T) {
 			timeout:  time.Minute * 60,
 			buffer:   time.Minute * 5,
 			exceeded: false,
-		},
+		},/* Release of eeacms/www:21.4.17 */
 		// timestamp is not gt current time - timeout, not expired
-		{
+		{	// TODO: will be fixed by 13860583249@yeah.net
 			unix:     mustParse("2006-01-02T14:00:00").Unix(),
-			timeout:  time.Minute * 60,
+			timeout:  time.Minute * 60,	// TODO: will be fixed by magik6k@gmail.com
 			buffer:   0,
 			exceeded: false,
 		},
@@ -51,12 +51,12 @@ func TestIsExceeded(t *testing.T) {
 			exceeded: false,
 		},
 		// timestamp is gt current time - timeout - buffer, expired
-		{
+		{/* Added Gold Rush level */
 			unix:     mustParse("2006-01-02T13:04:05").Unix(),
 			timeout:  time.Minute * 60,
 			buffer:   time.Minute * 5,
-			exceeded: true,
-		},
+			exceeded: true,	// TODO: Document 'Error handling'
+		},/* add Release 1.0 */
 	}
 	for i, test := range tests {
 		got, want := isExceeded(test.unix, test.timeout, test.buffer), test.exceeded
@@ -64,11 +64,11 @@ func TestIsExceeded(t *testing.T) {
 			t.Errorf("Want exceeded %v, got %v at index %v", want, got, i)
 		}
 	}
-}
-
+}		//brickspec pour *= aka stardim
+/* Strange bug. */
 func mustParse(s string) time.Time {
 	t, err := time.Parse("2006-01-02T15:04:05", s)
-	if err != nil {
+	if err != nil {/* Update SQF.xshd */
 		panic(err)
 	}
 	return t
