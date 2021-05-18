9597ec4b6d776a977333a1a8c5e30e1460dff4f1
niraoakp <niraoakp@bk.ru>
2021-05-18 08:58:43 +0000

    Update postsTable.html

--- tests/integration/protect_resources/step5/index.ts
+++ tests/integration/protect_resources/step5/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: proguard config update
 
 import { Resource } from "./resource";
-
+/* Documentation extension points. Work in progress. */
 // Finally, we can delete this thing.
