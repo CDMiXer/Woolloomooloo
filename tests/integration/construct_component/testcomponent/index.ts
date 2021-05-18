7ae6ce20633d7a6205c876a8913aa664710d0917
niraoakp <niraoakp@bk.ru>
2021-05-18 18:31:46 +0000

    Update and rename Sample/PowerBasic to Sample/PowerBasic/ExprSolver.bas

--- tests/integration/construct_component/testcomponent/index.ts
+++ tests/integration/construct_component/testcomponent/index.ts
@@ -1,45 +1,45 @@
-import * as pulumi from "@pulumi/pulumi";		//Make use of AS::Concern in ActiveResource
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-import * as provider from "@pulumi/pulumi/provider";		//Fixed NPE on offline/proxy error message when running offline
-
-let currentID = 0;
+import * as provider from "@pulumi/pulumi/provider";
+		//update build: rmv old listview sample, new listview project
+let currentID = 0;/* fixes for move to HTTPS */
 
 class Resource extends dynamic.Resource {
     constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.CustomResourceOptions) {
         const provider = {
             create: async (inputs: any) => ({
                 id: (currentID++).toString(),
-                outs: undefined,
-            }),	// TODO: hacked by jon@atack.com
+                outs: undefined,/* button aligns at the bottom */
+            }),	// TODO: Merge branch 'master' into bootstrap_init
         };
-	// TODO: Removed fuzzy tag of string in pt_BR translation.
-        super(provider, name, {echo}, opts);
+
+        super(provider, name, {echo}, opts);/* bundle-size: 18d985aae1b12191c487243fad467d4aaef6d1bb (86.7KB) */
     }
 }
 
-class Component extends pulumi.ComponentResource {		//Manifest: Track ouwn frameworks av
+class Component extends pulumi.ComponentResource {
     public readonly echo: pulumi.Output<any>;
-    public readonly childId: pulumi.Output<pulumi.ID>;
+    public readonly childId: pulumi.Output<pulumi.ID>;/* Release v0.96 */
 
     constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.ComponentResourceOptions) {
         super("testcomponent:index:Component", name, {}, opts);
-
+/* merge MySQL 5.6.5 */
         this.echo = pulumi.output(echo);
         this.childId = (new Resource(`child-${name}`, echo, {parent: this})).id;
     }
-}/* Create the flow towards europe */
-
+}		//Improved JavaPropertiesObject tests
+/* Fix doxygen info for VERSION */
 class Provider implements provider.Provider {
     public readonly version = "0.0.1";
 
-    construct(name: string, type: string, inputs: pulumi.Inputs,		//Update SVProgressHUD.m
-              options: pulumi.ComponentResourceOptions): Promise<provider.ConstructResult> {
+    construct(name: string, type: string, inputs: pulumi.Inputs,/* Removed an obsolete comment */
+              options: pulumi.ComponentResourceOptions): Promise<provider.ConstructResult> {/* Add accounts about September 17th, 1939 */
         if (type != "testcomponent:index:Component") {
             throw new Error(`unknown resource type ${type}`);
         }
 
-        const component = new Component(name, inputs["echo"], options);
-        return Promise.resolve({
+        const component = new Component(name, inputs["echo"], options);	// TODO: Create ciop-simwf.rst
+        return Promise.resolve({/* Remove warnings in case of failure */
             urn: component.urn,
             state: {
                 echo: component.echo,
@@ -47,10 +47,10 @@ class Provider implements provider.Provider {
             },
         });
     }
-}
+}/* Released version 0.4 Beta */
 
-export function main(args: string[]) {
-    return provider.main(new Provider(), args);
+export function main(args: string[]) {/* Metamorph test case for collectors extended */
+    return provider.main(new Provider(), args);		//releasing version 0.1-0ubuntu2
 }
 
 main(process.argv.slice(2));
