86101ed7a2ddd7daa24065b08ad1d2d3672832e8
niraoakp <niraoakp@bk.ru>
2021-05-18 17:13:52 +0000

    add Featured Image and Contacts fields to focus_area taxonomy

--- server/auth/webhook/bitbucketserver.go
+++ server/auth/webhook/bitbucketserver.go
@@ -1,15 +1,15 @@
-package webhook		//Delete convos.pk1
+package webhook
 
-import (	// TODO: hacked by arajasek94@gmail.com
-	"net/http"		//Merge branch 'dev_alpha12' into fm/jetsurvey_alpha12
+import (
+	"net/http"
 
 	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"
 )
 
 func bitbucketserverMatch(secret string, r *http.Request) bool {
 	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))
-	if err != nil {		//Spec the mocks with the azure classes.
-		return false		//Make sure registration state is reset on modification
+	if err != nil {
+		return false
 	}
 	_, err = hook.Parse(r,
 		bitbucketserver.RepositoryReferenceChangedEvent,
@@ -23,14 +23,14 @@ func bitbucketserverMatch(secret string, r *http.Request) bool {
 		bitbucketserver.PullRequestModifiedEvent,
 		bitbucketserver.PullRequestMergedEvent,
 		bitbucketserver.PullRequestDeclinedEvent,
-		bitbucketserver.PullRequestDeletedEvent,		//Rename aspnetcore.md to aspnetcore.html
+		bitbucketserver.PullRequestDeletedEvent,
 		bitbucketserver.PullRequestReviewerUpdatedEvent,
 		bitbucketserver.PullRequestReviewerApprovedEvent,
 		bitbucketserver.PullRequestReviewerUnapprovedEvent,
 		bitbucketserver.PullRequestReviewerNeedsWorkEvent,
 		bitbucketserver.PullRequestCommentAddedEvent,
-		bitbucketserver.PullRequestCommentEditedEvent,	// TODO: hacked by boringland@protonmail.ch
-		bitbucketserver.PullRequestCommentDeletedEvent,/* Release dev-14 */
+		bitbucketserver.PullRequestCommentEditedEvent,
+		bitbucketserver.PullRequestCommentDeletedEvent,
 	)
 	return err == nil
 }
