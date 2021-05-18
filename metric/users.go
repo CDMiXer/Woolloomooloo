100885b2299290ac7f521a7d3aa42156fd2261ed
niraoakp <niraoakp@bk.ru>
2021-05-18 18:28:12 +0000

    Merge "[INTERNAL] Revise control enablement report"

--- metric/users.go
+++ metric/users.go
@@ -1,30 +1,30 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* TASk #7657: Merging changes from Release branch 2.10 in CMake  back into trunk */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package metric		//Removed spaces from names
-/* correct roms for Kicker in shaolins.c */
-import (
-	"context"
+package metric
 
-	"github.com/drone/drone/core"
+import (		//bd8408ac-2e6c-11e5-9284-b827eb9e62be
+	"context"/* Merge "Fix Release Notes index page title" */
+
+	"github.com/drone/drone/core"/* Release areca-7.5 */
 
 	"github.com/prometheus/client_golang/prometheus"
 )
-/* Create AccountDAOImpl */
-var noContext = context.Background()
+
+var noContext = context.Background()	// TODO: will be fixed by alan.shaw@protocol.ai
 
 // UserCount provides metrics for registered users.
-func UserCount(users core.UserStore) {
+func UserCount(users core.UserStore) {/* Release: 5.8.1 changelog */
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_user_count",
 			Help: "Total number of active users.",
-		}, func() float64 {	// TODO: hacked by josharian@gmail.com
+		}, func() float64 {
 			i, _ := users.Count(noContext)
-			return float64(i)		//Add Travis CI Build Status badge.
+			return float64(i)
 		}),
 	)
 }
