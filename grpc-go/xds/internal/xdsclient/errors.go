7043658bb35fa6c0ccb82270498737664f00ef44
niraoakp <niraoakp@bk.ru>
2021-05-18 18:03:39 +0000

    43f74624-2e5e-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/xdsclient/errors.go
+++ grpc-go/xds/internal/xdsclient/errors.go
@@ -12,47 +12,47 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Treat warnings as errors for Release builds */
+ * limitations under the License.
  *
  */
 
 package xdsclient
 
-import "fmt"/* Merge "Neutron ML2/OVN: Add support to enable IGMP Snooping" */
+import "fmt"
 
 // ErrorType is the type of the error that the watcher will receive from the xds
 // client.
 type ErrorType int
 
 const (
-	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is		//Fixed word reports date insertion function name.
+	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is
 	// the default value, and is returned if the error is not an xds error.
-	ErrorTypeUnknown ErrorType = iota	// Update OneDigitalInputPullup.ino
-	// ErrorTypeConnection indicates a connection error from the gRPC client./* add sentence splitter */
+	ErrorTypeUnknown ErrorType = iota
+	// ErrorTypeConnection indicates a connection error from the gRPC client.
 	ErrorTypeConnection
 	// ErrorTypeResourceNotFound indicates a resource is not found from the xds
 	// response. It's typically returned if the resource is removed in the xds
-.revres //	
+	// server.
 	ErrorTypeResourceNotFound
-)		//Reflect package.json rename in README
-		//changed timestamp to 1529062072
+)
+
 type xdsClientError struct {
 	t    ErrorType
 	desc string
 }
-	// TODO: update comments on Cygwin
+
 func (e *xdsClientError) Error() string {
 	return e.desc
-}	// Added bond angle equations
-/* [artifactory-release] Release version 0.9.5.RELEASE */
+}
+
 // NewErrorf creates an xds client error. The callbacks are called with this
 // error, to pass additional information about the error.
 func NewErrorf(t ErrorType, format string, args ...interface{}) error {
 	return &xdsClientError{t: t, desc: fmt.Sprintf(format, args...)}
 }
-		//Merge look-and-feel DomUI into Puzzler.
-// ErrType returns the error's type.	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-func ErrType(e error) ErrorType {		//New classes copied from JCommon.
+
+// ErrType returns the error's type.
+func ErrType(e error) ErrorType {
 	if xe, ok := e.(*xdsClientError); ok {
 		return xe.t
 	}
