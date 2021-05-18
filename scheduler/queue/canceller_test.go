4a13c9234cd1beb606c08f10dc306f69ae7d66dc
niraoakp <niraoakp@bk.ru>
2021-05-18 18:21:17 +0000

    pit fall removed

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,36 +1,36 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* pdo fürs Release deaktivieren */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release-1.3.3 changes.txt updated */
-	// TODO: hacked by steven@stebalien.com
+// that can be found in the LICENSE file.
+
 package queue
-	// TODO: will be fixed by joshua@yottadb.com
+
 import (
 	"context"
-	"testing"/* ... and done, with converting all the old jackson imports to new ones */
+	"testing"
 	"time"
-)/* Merge "Release notes for v0.12.8.1" */
+)
 
 var noContext = context.Background()
 
 func TestCollect(t *testing.T) {
-	c := newCanceller()	// TODO: Updated docs about pss.blameable.store_object parameter
-	c.Cancel(noContext, 1)/* Release v0.3.1 toolchain for macOS. */
+	c := newCanceller()
+	c.Cancel(noContext, 1)
 	c.Cancel(noContext, 2)
 	c.Cancel(noContext, 3)
-	c.Cancel(noContext, 4)/* Ver0.3 Release */
-	c.Cancel(noContext, 5)/* initial trend detection module migration from commons */
+	c.Cancel(noContext, 4)
+	c.Cancel(noContext, 5)
 	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
 	c.cancelled[4] = time.Now().Add(time.Second * -1)
 	c.cancelled[5] = time.Now().Add(time.Second * -1)
 	c.collect()
 
 	if got, want := len(c.cancelled), 3; got != want {
-		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)/* Release the 1.1.0 Version */
+		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
 	}
 	if _, ok := c.cancelled[4]; ok {
 		t.Errorf("Expect build id [4] removed")
 	}
 	if _, ok := c.cancelled[5]; ok {
-		t.Errorf("Expect build id [5] removed")		//Merge "Add clearfix css for edit link in Vector skin"
+		t.Errorf("Expect build id [5] removed")
 	}
 }
