ec6e7b5b37a488b73525de9e3510cb3161b964a1
niraoakp <niraoakp@bk.ru>
2021-05-18 10:30:27 +0000

    branches/1.8.2 version fix

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,33 +1,33 @@
 package full
-		//148a0bf0-2e5f-11e5-9284-b827eb9e62be
+
 import (
-	"context"	// Merge branch 'master' of https://github.com/Kahval/product-crawler
-	"fmt"	// TODO: show max HP
+	"context"		//bbcfa460-2e73-11e5-9284-b827eb9e62be
+	"fmt"		//Create scala_first_steps.adoc
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/beacon"
 	"github.com/filecoin-project/lotus/chain/types"
-	"go.uber.org/fx"/* Merge "Lowering zindex for spinners, so they don't appear above modal windows." */
-)	// TODO: hacked by igor@soramitsu.co.jp
-	// TODO: will be fixed by lexy8russo@outlook.com
+	"go.uber.org/fx"
+)
+
 type BeaconAPI struct {
-	fx.In
+	fx.In/* Merge "Release 1.0.0.108 QCACLD WLAN Driver" */
 
 	Beacon beacon.Schedule
 }
-/* Release of eeacms/www:18.10.11 */
-func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {/* added missing igf_session_class */
+
+func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
 	b := a.Beacon.BeaconForEpoch(epoch)
-	rr := b.MaxBeaconRoundForEpoch(epoch)
+	rr := b.MaxBeaconRoundForEpoch(epoch)	// TODO: Fixes minor typo in nulecule/spec/README.md
 	e := b.Entry(ctx, rr)
 
-	select {
+	select {/* Release 0.22.2. */
 	case be, ok := <-e:
 		if !ok {
 			return nil, fmt.Errorf("beacon get returned no value")
 		}
 		if be.Err != nil {
-			return nil, be.Err
+			return nil, be.Err/* Release of eeacms/www-devel:18.3.6 */
 		}
 		return &be.Entry, nil
 	case <-ctx.Done():
