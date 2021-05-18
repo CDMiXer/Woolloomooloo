0b7d3b81961db32c709b8df24ede4a6a19e24fb0
niraoakp <niraoakp@bk.ru>
2021-05-18 14:47:52 +0000

    Merge "Pass a real image target to the policy enforcer"

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
--- name: create-table-users/* Build-125: Pre Release 1. */
+-- name: create-table-users
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            SERIAL PRIMARY KEY/* Update Whats New in this Release.md */
+ user_id            SERIAL PRIMARY KEY/* [artifactory-release] Release version 1.2.8.BUILD */
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN/* add I18N Implementation */
+,user_admin         BOOLEAN
 ,user_active        BOOLEAN
 ,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER
+,user_synced        INTEGER/* Texto Desenvolvimento */
 ,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
-,user_oauth_token   VARCHAR(500)/* Update rename_tv.sh */
-,user_oauth_refresh VARCHAR(500)/* Creating Default Constructor with default capacity(16) and loadfactor(0.75) */
-,user_oauth_expiry  INTEGER	// 2273a214-585b-11e5-9325-6c40088e03e4
+,user_oauth_token   VARCHAR(500)
+,user_oauth_refresh VARCHAR(500)
+,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
-,UNIQUE(user_login)/* Release DBFlute-1.1.0-sp6 */
+,UNIQUE(user_login)
 ,UNIQUE(user_hash)
-);		//Corrects a bad model lookup in nova-manage 
+);
