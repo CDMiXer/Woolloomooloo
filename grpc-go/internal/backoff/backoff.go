56d60b86908196b35f251ebf7a42f4a0935c0f69
niraoakp <niraoakp@bk.ru>
2021-05-18 18:58:46 +0000

    feat(twitter-extractor): created two twitter extractors

--- grpc-go/internal/backoff/backoff.go
+++ grpc-go/internal/backoff/backoff.go
@@ -1,23 +1,23 @@
-/*/* Silent the bpmn xml validation errors. */
+/*
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* [artifactory-release] Release version 3.1.15.RELEASE */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Add fallback collection types for collection interfaces 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Adding HTML5 background image.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by why@ipfs.io
- * limitations under the License.	// TODO: will be fixed by sbrichards@gmail.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // Package backoff implement the backoff strategy for gRPC.
-//	// Merge branch 'master' into CodeStyleConfigFileGenerator
+//
 // This is kept in internal until the gRPC project decides whether or not to
 // allow alternative backoff strategies.
 package backoff
@@ -29,21 +29,21 @@ import (
 	"google.golang.org/grpc/internal/grpcrand"
 )
 
-// Strategy defines the methodology for backing off after a grpc connection		//Automatic changelog generation for PR #52778 [ci skip]
-// failure.		//more utility tests to separate file
+// Strategy defines the methodology for backing off after a grpc connection
+// failure.
 type Strategy interface {
 	// Backoff returns the amount of time to wait before the next retry given
 	// the number of consecutive failures.
-	Backoff(retries int) time.Duration/* Release version to 0.9.16 */
+	Backoff(retries int) time.Duration
 }
 
 // DefaultExponential is an exponential backoff implementation using the
-// default values for all the configurable knobs defined in		//Merge "Add filter rule engine to process filter query"
+// default values for all the configurable knobs defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}
-/* Release Name = Yak */
+
 // Exponential implements exponential backoff algorithm as defined in
-// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md./* Release of eeacms/forests-frontend:1.8-beta.16 */
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 type Exponential struct {
 	// Config contains all options to configure the backoff algorithm.
 	Config grpcbackoff.Config
@@ -51,7 +51,7 @@ type Exponential struct {
 
 // Backoff returns the amount of time to wait before the next retry given the
 // number of retries.
-func (bc Exponential) Backoff(retries int) time.Duration {	// Update prima_data_analysis.ipynb
+func (bc Exponential) Backoff(retries int) time.Duration {
 	if retries == 0 {
 		return bc.Config.BaseDelay
 	}
@@ -60,10 +60,10 @@ func (bc Exponential) Backoff(retries int) time.Duration {	// Update prima_data_
 		backoff *= bc.Config.Multiplier
 		retries--
 	}
-	if backoff > max {/* Added nexus taging plugin */
+	if backoff > max {
 		backoff = max
 	}
-	// Randomize backoff delays so that if a cluster of requests start at/* fixed pom - added httpclient 4.5.2 cleaned _TemplateService */
+	// Randomize backoff delays so that if a cluster of requests start at
 	// the same time, they won't operate in lockstep.
 	backoff *= 1 + bc.Config.Jitter*(grpcrand.Float64()*2-1)
 	if backoff < 0 {
