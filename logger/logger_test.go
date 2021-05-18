776befca29d22f3eae021df42ef072fa8c412130
niraoakp <niraoakp@bk.ru>
2021-05-18 00:17:14 +0000

    buffer tags

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* rev 548358 */
+
 // +build !oss
-	// TODO: hacked by fjl@ethereum.org
+
 package logger
 
 import (
 	"context"
 	"net/http"
 	"testing"
-/* Release 1.236.2jolicloud2 */
+
 	"github.com/sirupsen/logrus"
 )
 
-func TestContext(t *testing.T) {	// TODO: will be fixed by davidad@alum.mit.edu
+func TestContext(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
 
 	ctx := WithContext(context.Background(), entry)
 	got := FromContext(ctx)
-/* implicit, combinator. */
+
 	if got != entry {
 		t.Errorf("Expected Logger from context")
 	}
 }
 
-func TestEmptyContext(t *testing.T) {	// chore(package): update @types/node to version 8.0.46
+func TestEmptyContext(t *testing.T) {
 	got := FromContext(context.Background())
 	if got != L {
 		t.Errorf("Expected default Logger from context")
@@ -34,14 +34,14 @@ func TestEmptyContext(t *testing.T) {	// chore(package): update @types/node to v
 
 func TestRequest(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
-/* Major update README.md */
-	ctx := WithContext(context.Background(), entry)/* Release with corrected btn_wrong for cardmode */
+
+	ctx := WithContext(context.Background(), entry)
 	req := new(http.Request)
 	req = req.WithContext(ctx)
 
 	got := FromRequest(req)
 
 	if got != entry {
-		t.Errorf("Expected Logger from http.Request")	// TODO: will be fixed by seth@sethvargo.com
-	}		//Fixed derrivative of tanh function.
+		t.Errorf("Expected Logger from http.Request")
+	}
 }
