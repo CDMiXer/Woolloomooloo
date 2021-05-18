3d03eeca4c82bf30ed2a2e892297e59ae8211514
niraoakp <niraoakp@bk.ru>
2021-05-18 02:48:56 +0000

    Merge branch 'master' of https://github.com/yogirjoshi/MonitorFactory.git

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Extract logic in request sold game worker
+//	// TODO: hacked by hugomrdias@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//i386: removed multiple warnings
+// you may not use this file except in compliance with the License.		//Update instructions and add loading animation
 // You may obtain a copy of the License at
-///* Release for 18.34.0 */
+///* Now the RequestQueue uses TChan instead of Chan. */
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Fix quoting in post-checkout hook */
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into email/manager_invite */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released 0.1.3 */
-// See the License for the specific language governing permissions and	// cfe1ed38-2e59-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Merge "Add Compare service to fetch compare data"
 // limitations under the License.
 
 // Package pubsub implements a thread safe publish subscriber,
