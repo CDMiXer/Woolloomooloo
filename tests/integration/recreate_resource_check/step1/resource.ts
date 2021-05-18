095d8fbc893594883ad1a500491b755d715b0cd1
niraoakp <niraoakp@bk.ru>
2021-05-18 16:05:56 +0000

    id "Bahasa Indonesia" translation #15647. Author: adegun.

--- tests/integration/recreate_resource_check/step1/resource.ts
+++ tests/integration/recreate_resource_check/step1/resource.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Merge branch 'master' into CASSANDRA-20
+
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-export class Provider implements dynamic.ResourceProvider {	// TODO: will be fixed by fjl@ethereum.org
+export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private id: number = 0;
-/* Update install-statusengine.sh */
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {/* Support method creation from Constructors */
+
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         // When the engine re-creates a resource after it was deleted, it should
         // not pass the old (deleted) inputs to Check when re-creating.
-        ///* #6 [Release] Add folder release with new release file to project. */
+        //
         // This Check implementation fails the test if this happens.
-        if (olds.state === 99 && news.state === 22) {		//added update and reconfiguration by animation mode
+        if (olds.state === 99 && news.state === 22) {
             return {
-                inputs: news,		//e0623572-2e50-11e5-9284-b827eb9e62be
+                inputs: news,
                 failures: [
                     {
-                        property: "state",/* Merge "Release notes for implied roles" */
+                        property: "state",
                         reason: "engine did invalid comparison of old and new check inputs for recreated resource",
                     },
                 ],
@@ -34,7 +34,7 @@ export class Provider implements dynamic.ResourceProvider {	// TODO: will be fix
         if (olds.state !== news.state) {
             return {
                 changes: true,
-                replaces: ["state"],/* Vorbereitung Release 1.7.1 */
+                replaces: ["state"],
                 deleteBeforeReplace: true,
             };
         }
@@ -42,8 +42,8 @@ export class Provider implements dynamic.ResourceProvider {	// TODO: will be fix
         return {
             changes: false,
         };
-    }		//Reuploading blog landing
-	// TODO: Update the type matcher
+    }
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: (this.id++).toString(),
@@ -56,12 +56,12 @@ export class Resource extends pulumi.dynamic.Resource {
     public uniqueKey?: pulumi.Output<number>;
     public state: pulumi.Output<number>;
 
-    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {/* Release: 5.8.2 changelog */
+    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}		//Updated Book list, and added shelf to books.
+}
 
 export interface ResourceProps {
     readonly uniqueKey?: pulumi.Input<number>;
     readonly state: pulumi.Input<number>;
-}/* Release v12.0.0 */
+}
