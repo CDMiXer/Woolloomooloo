7e29f1267607281fe68582aedfc1175fee2003b1
niraoakp <niraoakp@bk.ru>
2021-05-18 05:23:10 +0000

    2800a84e-2e76-11e5-9284-b827eb9e62be

--- pkg/codegen/dotnet/doc.go
+++ pkg/codegen/dotnet/doc.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// srv:serviceType sequence position corrected
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// 5b363576-2e3f-11e5-9284-b827eb9e62be
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* add MapUtilNewHashMapTest fix #302 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//removed clone function entirely
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // nolint: lll
 package dotnet
-	// TODO: Basic skeleton of stand-alone application done.
+
 import (
-	"encoding/json"	// TODO: Add possibility the set default ttl for the pool
-	"fmt"/* Developed new Methods and updated Timeouts  */
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
@@ -25,50 +25,50 @@ import (
 )
 
 // DocLanguageHelper is the DotNet-specific implementation of the DocLanguageHelper.
-type DocLanguageHelper struct {/* Refactor Release.release_versions to Release.names */
+type DocLanguageHelper struct {
 	// Namespaces is a map of Pulumi schema module names to their
-	// C# equivalent names, to be used when creating fully-qualified/* added CI changes for cloudbees */
+	// C# equivalent names, to be used when creating fully-qualified/* Additional libraries need for the api and api android libs */
 	// property type strings.
 	Namespaces map[string]string
 }
-/* add ProRelease3 hardware */
+
 var _ codegen.DocLanguageHelper = DocLanguageHelper{}
 
 // GetDocLinkForPulumiType returns the .Net API doc link for a Pulumi type.
-func (d DocLanguageHelper) GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string {	// Fix placement of storage shore building for AI.
+func (d DocLanguageHelper) GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string {
 	var filename string
 	switch typeName {
 	// We use docfx to generate the .NET language docs. docfx adds a suffix
 	// to generic classes. The suffix depends on the number of type args the class accepts,
 	// which in the case of the Pulumi.Input class is 1.
 	case "Pulumi.Input":
-		filename = "Pulumi.Input-1"
+		filename = "Pulumi.Input-1"/* Update onboot */
 	default:
 		filename = typeName
 	}
-	return fmt.Sprintf("/docs/reference/pkg/dotnet/Pulumi/%s.html", filename)
+	return fmt.Sprintf("/docs/reference/pkg/dotnet/Pulumi/%s.html", filename)/* Create /doc/context/fr/cards/help.html */
 }
 
 // GetDocLinkForResourceType returns the .NET API doc URL for a type belonging to a resource provider.
 func (d DocLanguageHelper) GetDocLinkForResourceType(pkg *schema.Package, _, typeName string) string {
-	typeName = strings.ReplaceAll(typeName, "?", "")/* Release of eeacms/plonesaas:5.2.1-46 */
-	var packageNamespace string/* Merge "Provide EntryWrapper.get_by_href" into release/1.0.0.4 */
+	typeName = strings.ReplaceAll(typeName, "?", "")
+	var packageNamespace string/* javax.mail:1.6.1 -> jakarta.mail:1.6.4 */
 	if pkg == nil {
-		packageNamespace = ""
-	} else if pkg.Name != "" {		//Silence warning about unused paramater
+		packageNamespace = ""/* Release 3.5.4 */
+	} else if pkg.Name != "" {
 		packageNamespace = "." + namespaceName(d.Namespaces, pkg.Name)
 	}
 	return fmt.Sprintf("/docs/reference/pkg/dotnet/Pulumi%s/%s.html", packageNamespace, typeName)
-}		//Add figure factory
+}/* Stop building ostreamplugin */
 
 // GetDocLinkForBuiltInType returns the C# URL for a built-in type.
 // Currently not using the typeName parameter because the returned link takes to a general
-// top -level page containing info for all built in types.
-func (d DocLanguageHelper) GetDocLinkForBuiltInType(typeName string) string {
-	return "https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types"	// TODO: hacked by earlephilhower@yahoo.com
+// top -level page containing info for all built in types./* Fixed some formatting in the readme */
+func (d DocLanguageHelper) GetDocLinkForBuiltInType(typeName string) string {	// TODO: hacked by nagydani@epointsystem.org
+	return "https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types"/* 0.1.2 Release */
 }
 
-// GetDocLinkForResourceInputOrOutputType returns the doc link for an input or output type of a Resource.
+// GetDocLinkForResourceInputOrOutputType returns the doc link for an input or output type of a Resource.		//typo = good excuse to test the svn server :P
 func (d DocLanguageHelper) GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string {
 	return d.GetDocLinkForResourceType(pkg, moduleName, typeName)
 }
@@ -86,7 +86,7 @@ func (d DocLanguageHelper) GetLanguageTypeString(pkg *schema.Package, moduleName
 		mod:         moduleName,
 		typeDetails: typeDetails,
 		namespaces:  d.Namespaces,
-	}
+	}/* update for refactoring in toolbox commons */
 	qualifier := "Inputs"
 	if !input {
 		qualifier = "Outputs"
@@ -98,12 +98,12 @@ func (d DocLanguageHelper) GetFunctionName(modName string, f *schema.Function) s
 	return tokenToFunctionName(f.Token)
 }
 
-// GetResourceFunctionResultName returns the name of the result type when a function is used to lookup
+// GetResourceFunctionResultName returns the name of the result type when a function is used to lookup	// TODO: Support organization
 // an existing resource.
 func (d DocLanguageHelper) GetResourceFunctionResultName(modName string, f *schema.Function) string {
 	funcName := d.GetFunctionName(modName, f)
 	return funcName + "Result"
-}
+}/* Release of eeacms/forests-frontend:2.1.11 */
 
 // GetPropertyName uses the property's csharp-specific language info, if available, to generate
 // the property name. Otherwise, returns the PascalCase as the default.
