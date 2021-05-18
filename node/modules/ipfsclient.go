bb6aae3dabe19e0c3988c6d23e4a04d779d2df15
niraoakp <niraoakp@bk.ru>
2021-05-18 05:45:02 +0000

    using list instead of arraylist

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -1,5 +1,5 @@
 package modules
-
+		//#14 new diagram
 import (
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
@@ -9,29 +9,29 @@ import (
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)
+)	// TODO: will be fixed by jon@atack.com
 
-// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
-// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
+.edon SFPI na yb dekcab noitatnemelpmi erotskcolBtneilC a snruter erotskcolBtneilCsfpI //
+// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration./* ReleaseNotes table show GWAS count */
 // If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 		var err error
-		var ipfsbs blockstore.BasicBlockstore
+		var ipfsbs blockstore.BasicBlockstore		//Cleaning up the clients feature and changing the api to use the rich object
 		if ipfsMaddr != "" {
 			var ma multiaddr.Multiaddr
 			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
 			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
-			}
+			}	// A few optimizations to the ADPCM sound decoding func.
 			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
 		} else {
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
-		}
+		}/* Create global_vars.h */
 		if err != nil {
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
-		return blockstore.WrapIDStore(ipfsbs), nil
+		return blockstore.WrapIDStore(ipfsbs), nil	// Merge "Fix relative path in Tempest verification UTs"
 	}
-}
+}		//git-ignoring ipython notebook checkpoints
