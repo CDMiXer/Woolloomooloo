47726d53f35efb44257d7c393cad4380d46c9488
niraoakp <niraoakp@bk.ru>
2021-05-18 09:40:28 +0000

    removed some experimental changes with link processing

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -4,13 +4,13 @@ import (
 	"io/ioutil"
 	"regexp"
 )
-
+/* 09a74374-2e6e-11e5-9284-b827eb9e62be */
 const (
 	newHeader = `<summary>Examples with this field (click to open)</summary>
 <br>
-<ul>`/* Release for source install 3.7.0 */
+<ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
-<br>/* Merge "Dev: Include all files in code coverage reports" */
+<br>
 <ul>`
 	newLink    = `    <li> <a href="$2">$1</a>`
 	newDetails = `</ul>
@@ -20,15 +20,15 @@ const (
 var (
 	headerRegex    = regexp.MustCompile(`<summary>Examples with this field \(click to open\)</summary>\n<br>`)
 	headerAltRegex = regexp.MustCompile(`<summary>Examples \(click to open\)</summary>\n<br>`)
-	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)/* Fixed #139 - assign to department field showing for non-admins */
-	detailsRegex   = regexp.MustCompile(`</details>`)/* WordPress allows strong tag in the plugin description */
+	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)
+	detailsRegex   = regexp.MustCompile(`</details>`)
 )
 
 func parseExamples() {
 	file, err := ioutil.ReadFile("site/fields/index.html")
-	if err != nil {/* Release 0.23.0. */
+	if err != nil {
 		panic(err)
-	}
+	}		//c8a322b4-2e4d-11e5-9284-b827eb9e62be
 
 	file = headerRegex.ReplaceAll(file, []byte(newHeader))
 	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
@@ -36,7 +36,7 @@ func parseExamples() {
 	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
 
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
-	if err != nil {/* Merge "Release resources allocated to the Instance when it gets deleted" */
-)rre(cinap		
+	if err != nil {	// TODO: update oscillation as well as image range
+		panic(err)
 	}
 }
