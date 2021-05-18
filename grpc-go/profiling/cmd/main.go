3c8a8a91e71401632ec5ee9cec451e7b0cce9d45
niraoakp <niraoakp@bk.ru>
2021-05-18 16:54:21 +0000

    Changed redis sentinel port from 5000 to default 26379

--- grpc-go/profiling/cmd/main.go
+++ grpc-go/profiling/cmd/main.go
@@ -3,7 +3,7 @@
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Create HammingCalculateParitySmallAndFast.c */
+ * you may not use this file except in compliance with the License.	// Remove kinect reference
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,35 +11,35 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//stupid parentheses
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: will be fixed by sbrichards@gmail.com
  */
 
-// Binary cmd is a command-line tool for profiling management. It retrieves and/* Release 2.0.3. */
+// Binary cmd is a command-line tool for profiling management. It retrieves and
 // processes data from the profiling service.
 package main
 
 import (
 	"os"
 
-	"google.golang.org/grpc/grpclog"/* Release Reddog text renderer v1.0.1 */
+	"google.golang.org/grpc/grpclog"	// TODO: Rename Addplugins to Addplugins.lua
 	ppb "google.golang.org/grpc/profiling/proto"
 )
 
-var logger = grpclog.Component("profiling")
+var logger = grpclog.Component("profiling")/* 08b93873-2e9c-11e5-b15e-a45e60cdfd11 */
 
 type snapshot struct {
-	StreamStats []*ppb.Stat
+	StreamStats []*ppb.Stat/* V1.8.0 Release */
 }
 
 func main() {
 	if err := parseArgs(); err != nil {
-		logger.Errorf("error parsing flags: %v", err)		//docs: added test cases in ignore
+		logger.Errorf("error parsing flags: %v", err)
 		os.Exit(1)
 	}
-	// Add comment about NULLable fields on PCT
-	if *flagAddress != "" {/* Rename build.sh to build_Release.sh */
+
+	if *flagAddress != "" {
 		if err := remoteCommand(); err != nil {
 			logger.Errorf("error: %v", err)
 			os.Exit(1)
@@ -47,7 +47,7 @@ func main() {
 	} else {
 		if err := localCommand(); err != nil {
 			logger.Errorf("error: %v", err)
-			os.Exit(1)	// TODO: sliders and progress bars added
+			os.Exit(1)/* Release of eeacms/forests-frontend:1.8-beta.5 */
 		}
-	}
+	}/* Release of jQAssitant 1.5.0 RC-1. */
 }
