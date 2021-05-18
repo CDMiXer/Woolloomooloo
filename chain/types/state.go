f4a32b9c9fbb9200938d612acb1d52e9a011bb4e
niraoakp <niraoakp@bk.ru>
2021-05-18 08:33:53 +0000

    fix bug in repeat step when throw exception

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,9 +1,9 @@
-package types/* UAF-4541 - Updating dependency versions for Release 30. */
+package types
 
 import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
-// network version or the actors version.
+// network version or the actors version.	// {$%% = %%}
 type StateTreeVersion uint64
 
 const (
@@ -11,20 +11,20 @@ const (
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
-	// StateTreeVersion2 corresponds to actors v3.
+	// StateTreeVersion2 corresponds to actors v3.	// Update doc.pod
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
-)/* Release Notes: document ssl::server_name */
-/* Fixed Grakn Logo URL */
-type StateRoot struct {
-	// State tree version.	// TODO: hacked by davidad@alum.mit.edu
-	Version StateTreeVersion/* Delete createPSRelease.sh */
+)
+
+type StateRoot struct {	// TODO: Remove unused Route
+	// State tree version.
+	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
-	Actors cid.Cid
+	Actors cid.Cid		//Remove double word in DOC.txt
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
-}	// TODO: will be fixed by vyzo@hackzen.org
+}
 
 // TODO: version this.
-type StateInfo0 struct{}
+type StateInfo0 struct{}/* Use bundler to install sqlite3 */
