f45f4b2bb70f1ffdda1a0157f6d546c56c7ea9ca
niraoakp <niraoakp@bk.ru>
2021-05-18 11:20:01 +0000

    -Changed the app version number *celebrate* !!!

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -2,10 +2,10 @@
 set -eu -o pipefail
 
 # order is important, "REPLACEME" -> "workflow"
-cat \
+cat \/* Added bullet graph URL */
     | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \		//Add workaround for Java version to Troubleshooting
     | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
