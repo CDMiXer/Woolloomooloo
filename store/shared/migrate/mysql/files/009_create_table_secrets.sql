3b6934d3bed8e6583a02a312403e6e1fce14e9fc
niraoakp <niraoakp@bk.ru>
2021-05-18 22:39:01 +0000

    fix: SASS not CSS

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
--- name: create-table-secrets/* Release of eeacms/plonesaas:5.2.1-50 */
+-- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT	// Fixed discrepancies from creation of new git repo
 ,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)
+,secret_name              VARCHAR(500)/* improve coverage on path spec */
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Release 1.4.0. */
 );
 
 -- name: create-index-secrets-repo
-/* added luya toolbar translations */
+
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name
-
-CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);/* Clingcon: bugfix in normalizing linear constraints */
+-- name: create-index-secrets-repo-name		//With version printout in generated version script
+	// fix(package): update file-type to version 8.0.0
+CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
