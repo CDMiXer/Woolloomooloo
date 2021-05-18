d2ae9d205bbfe477518acf20d5db9a58a49fdcc8
niraoakp <niraoakp@bk.ru>
2021-05-18 07:22:08 +0000

    24df66f0-2e3f-11e5-9284-b827eb9e62be

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -eu -o pipefail
+set -eu -o pipefail/* Merge "tests: Collect info on failure of conn_tester" */
 
 for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
-
-  cd "$MOCK_DIR"	// back to origanal with prebuild
+		//1. fix xls filename in batches
+  cd "$MOCK_DIR"/* Released 3.0.2 */
   mockery -name=$"$MOCK_NAME"
-  cd -		//Added missing language variable in Upload
-done
+  cd -		//Move from go 1.4rc2 to 1.4 release
+done/* Removed archive 2 */
