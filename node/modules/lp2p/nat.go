4e7d9b27d65209f86a86b8a999dda23392ed235c
niraoakp <niraoakp@bk.ru>
2021-05-18 11:14:44 +0000

    Updating feature docs.

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -7,7 +7,7 @@ import (
 /*import (
 	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
-	host "github.com/libp2p/go-libp2p-core/host"
+	host "github.com/libp2p/go-libp2p-core/host"/* 88db8aa2-2e45-11e5-9284-b827eb9e62be */
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
 
@@ -17,12 +17,12 @@ import (
 )
 
 func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
+	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {		//v0.2.1 bump
 		// collect private net option in case swarm.key is presented
 		opts, _, err := PNet(repo)
 		if err != nil {
 			// swarm key exists but was failed to decode
-			return err
+			return err		//copyright and email updates.
 		}
 
 		if quic {
@@ -31,8 +31,8 @@ func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc 
 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
 		return err
-	}
-}
+	}/* Release v0.3.3, fallback to guava v14.0 */
+}/* Release for 3.11.0 */
 */
 
 var AutoNATService = simpleOpt(libp2p.EnableNATService())
