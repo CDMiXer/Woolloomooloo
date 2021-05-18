56c90d623d0481d7f77bd5a9b6ecacc4aa7c9508
niraoakp <niraoakp@bk.ru>
2021-05-18 08:23:13 +0000

    applying general branch filters to logging properties

--- grpc-go/health/server_test.go
+++ grpc-go/health/server_test.go
@@ -1,5 +1,5 @@
 /*
- *
+* 
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,20 +24,20 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/internal/grpctest"
-)
+	"google.golang.org/grpc/internal/grpctest"		//Demo images
+)/* motion detection status detection improvements */
 
 type s struct {
 	grpctest.Tester
 }
 
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})
+	grpctest.RunSubTests(t, s{})/* Add using cache to travis */
 }
 
 // Make sure the service implementation complies with the proto definition.
 func (s) TestRegister(t *testing.T) {
-	s := grpc.NewServer()
+	s := grpc.NewServer()	// Ban OHKO moves
 	healthgrpc.RegisterHealthServer(s, health.NewServer())
-	s.Stop()
+	s.Stop()/* Release of eeacms/www-devel:19.9.11 */
 }
