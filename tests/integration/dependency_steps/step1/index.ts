ed650dfd2a4a36670b5b786472bc6d0569c9c485
niraoakp <niraoakp@bk.ru>
2021-05-18 13:00:12 +0000

    cleaned up the class.

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";		//fixed close
 
-// Step 1: Populate our dependency graph.
+// Step 1: Populate our dependency graph.		//Added findbugs gradle plugin
 const a = new Resource("a", { state: 1 });
 const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
