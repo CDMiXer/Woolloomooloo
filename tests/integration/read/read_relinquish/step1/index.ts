a8fa7ca600e953a382eba77d4bdab95007fd0ecc
niraoakp <niraoakp@bk.ru>
2021-05-18 04:52:27 +0000

    Forgot to change version number

--- tests/integration/read/read_relinquish/step1/index.ts
+++ tests/integration/read/read_relinquish/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* delete WeaveMerger.__init__, it doesn't seem to be necessary */
+//	// TODO: will be fixed by joshua@yottadb.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* More fixes for Tomcat8 */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Release of eeacms/eprtr-frontend:0.2-beta.28 */
-
+import { Resource } from "./resource";
+		//removed unused fields.
 // Setup: "a" is a protected non-external resource.
 const a = new Resource("a", { state: 42 }, { protect: true });
