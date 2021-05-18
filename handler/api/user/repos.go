434ba7695c3d0fbdc30654985ae231c840917a4f
niraoakp <niraoakp@bk.ru>
2021-05-18 02:38:18 +0000

    Merge branch 'master' into feature/connectedapp-redux

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: remove randDouble test case
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update README.md / add badges
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Merge "Wlan: Release 3.8.20.7" */
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// blogpage requires PollNY
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: hacked by igor@soramitsu.co.jp
+// You may obtain a copy of the License at
+///* [IMP] account: General journal report update with osv memory (ref: PSI) */
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Updates for Release 8.1.1036 */
+// Unless required by applicable law or agreed to in writing, software/* All messages to user now do so via Zeus.ui */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 852d737a-2e4a-11e5-9284-b827eb9e62be */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* (MESS) ibm5170: Keyboard WIP. (nw) */
+// limitations under the License.
 
-package user/* Release 2.4.0 (close #7) */
+package user
 
 import (
-	"net/http"
+	"net/http"/* Released 4.0 alpha 4 */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//// AdminCartRulesController: wording.
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"/* Rename git.hs to git.sh */
+	"github.com/drone/drone/logger"
 )
 
-// HandleRepos returns an http.HandlerFunc that write a json-encoded
-// list of repositories to the response body.
+// HandleRepos returns an http.HandlerFunc that write a json-encoded	// TODO: will be fixed by joshua@yottadb.com
+// list of repositories to the response body./* Merge "Update ceilometer-agent-notification puppet scripts" */
 func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Release candidate! */
 		viewer, _ := request.UserFrom(r.Context())
 
-		var list []*core.Repository
-		var err error
+		var list []*core.Repository		//Create 02_01.sql
+		var err error	// TODO: hacked by fkautz@pseudocode.cc
 		if r.FormValue("latest") != "true" {
 			list, err = repos.List(r.Context(), viewer.ID)
-		} else {	// Just regularize the naming of some palette colors
+		} else {
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
-		}/* Fix missing newline in permission explanation */
-		if err != nil {/* Release 4.2.2 */
+		}
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list repositories")
-		} else {/* Release ChildExecutor after the channel was closed. See #173 */
+		} else {
 			render.JSON(w, list, 200)
-		}/* Change the arena in the FactoryStrategy to a ref to an ArenaInterface. */
-	}
+		}
+	}		//adding a bit of trouble shooting
 }
