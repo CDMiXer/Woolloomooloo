1baed08316b84df484a5a83177b7eb0215e25b6a
niraoakp <niraoakp@bk.ru>
2021-05-18 15:15:00 +0000

    p means paragraph, dah

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -4,19 +4,19 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: README: Use fenced code blocks with syntax highlighting
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and		//fixed loop again - check is on ForwardSolver now!
+// limitations under the License.	// TODO: Merge "Pulling out predictions into another row view." into ub-launcher3-burnaby
 
 // +build oss
 
 package admission
 
-import (
+import (/* Fixed functions' name in oscam.h/oscam.c */
 	"time"
 
 	"github.com/drone/drone/core"
