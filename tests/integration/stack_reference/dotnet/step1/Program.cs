530896426591732a32e2625a859bc1393f0921e8
niraoakp <niraoakp@bk.ru>
2021-05-18 13:47:08 +0000

    Updated test.ini with resetlines configuration

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -1,30 +1,30 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System;
-using System.Collections.Generic;	// TODO: hacked by nicksavers@gmail.com
-using System.Threading.Tasks;	// TODO: pupcamera: small gui tweak
+using System;	// adding rules with adjectives
+using System.Collections.Generic;/* Merge "Revert "Release 1.7 rc3"" */
+using System.Threading.Tasks;
 using Pulumi;
 
-class Program		//dafba540-2ead-11e5-ac44-7831c1d44c14
-{		//pe sphere VTK output: add angular velocity
+class Program
+{
     static Task<int> Main(string[] args)
-    {	// TODO: hacked by josharian@gmail.com
-        return Deployment.RunAsync(async () =>
+    {
+        return Deployment.RunAsync(async () =>/* REFACTOR: remove of AbstractGraph */
         {
-            var config = new Config();/* Remove Check Errors tool. */
+            var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
             var oldVal = (string[])await a.GetValueAsync("val");
-            if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")/* docs(readme): rename italian readme */
+            if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
             {
                 throw new Exception("Invalid result");
             }
 
             return new Dictionary<string, object>
-            {	// TODO: hacked by brosner@gmail.com
-                { "val2", Output.CreateSecret(new[] { "a", "b" }) }/* Release of eeacms/ims-frontend:0.3.2 */
+            {	// TODO: will be fixed by earlephilhower@yahoo.com
+                { "val2", Output.CreateSecret(new[] { "a", "b" }) }
             };
         });
     }
