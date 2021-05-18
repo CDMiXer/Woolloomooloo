5533d971ead9a8101a15c16991fde1d6642f9c68
niraoakp <niraoakp@bk.ru>
2021-05-18 17:54:54 +0000

    Update CHANGELOG for #5916

--- operator/manager/rpc2/server_oss.go
+++ operator/manager/rpc2/server_oss.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Code changes for #103 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www:21.3.31 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Raven-Releases */
 // +build oss
 
 package rpc2
@@ -20,7 +20,7 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/operator/manager"
-)
+)	// improved computed size of training file window
 
 // Server wraps the chi Router in a custom type for wire
 // injection purposes.
@@ -30,4 +30,4 @@ type Server http.Handler
 // interaction with the build controller using the http transport.
 func NewServer(manager manager.BuildManager, secret string) Server {
 	return Server(http.NotFoundHandler())
-}
+}/* fix old avatar code */
