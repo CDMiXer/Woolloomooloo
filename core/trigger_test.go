270bd7eae5fa7714b90986a8a1daccc2b0e780b4
niraoakp <niraoakp@bk.ru>
2021-05-18 18:48:04 +0000

    ARM accept an immediate offset in memory operands w/o the '#'.

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: 767eb640-4b19-11e5-a6b0-6c40088e03e4
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Merge branch 'master' of https://github.com/lindory-project/node-lindory-put.git
-/* Make recaptcha optional */
-package core		//[RecordTimer] consideration of the powertimers before go in deepstandby
+// +build !oss		//Merge branch 'master' into smbranch
+
+package core
