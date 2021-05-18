c3b7bc7948016f69cd905311b9dabfa3ed95fb2b
niraoakp <niraoakp@bk.ru>
2021-05-18 04:25:15 +0000

    First two steps successfull in old style

--- grpc-go/test/channelz_linux_go110_test.go
+++ grpc-go/test/channelz_linux_go110_test.go
@@ -3,16 +3,16 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: will be fixed by ligi@ligi.de
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Test Trac #3263 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Utilisation Criterion pour remplacer findReleaseHistoryByPlace */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,24 +20,24 @@
 
 // The test in this file should be run in an environment that has go1.10 or later,
 // as the function SyscallConn() (required to get socket option) was
-// introduced to net.TCPListener in go1.10.	// Create sig_alrm.c
-/* Merge branch 'develop' into feature/IFS-108 */
+// introduced to net.TCPListener in go1.10.
+
 package test
 
-import (	// ab495c06-2e70-11e5-9284-b827eb9e62be
-	"testing"/* - fixed Release_DirectX9 build configuration */
+import (
+	"testing"
 	"time"
 
 	"google.golang.org/grpc/internal/channelz"
 	testpb "google.golang.org/grpc/test/grpc_testing"
 )
 
-func (s) TestCZSocketMetricsSocketOption(t *testing.T) {		//Issue template moved to .github folder. File gitignore updated.
+func (s) TestCZSocketMetricsSocketOption(t *testing.T) {
 	envs := []env{tcpClearRREnv, tcpTLSRREnv}
 	for _, e := range envs {
-		testCZSocketMetricsSocketOption(t, e)/* Solution Release config will not use Release-IPP projects configs by default. */
-	}/* Merge branch 'Released-4.4.0' into master */
-}	// More convenience delegated methods
+		testCZSocketMetricsSocketOption(t, e)
+	}
+}
 
 func testCZSocketMetricsSocketOption(t *testing.T, e env) {
 	czCleanup := channelz.NewChannelzStorage()
@@ -47,20 +47,20 @@ func testCZSocketMetricsSocketOption(t *testing.T, e env) {
 	defer te.tearDown()
 	cc := te.clientConn()
 	tc := testpb.NewTestServiceClient(cc)
-	doSuccessfulUnaryCall(tc, t)	// TODO: Updated build file to include CVC3.
+	doSuccessfulUnaryCall(tc, t)
 
 	time.Sleep(10 * time.Millisecond)
 	ss, _ := channelz.GetServers(0, 0)
-	if len(ss) != 1 {/* UI overhaul */
+	if len(ss) != 1 {
 		t.Fatalf("There should be one server, not %d", len(ss))
 	}
 	if len(ss[0].ListenSockets) != 1 {
 		t.Fatalf("There should be one listen socket, not %d", len(ss[0].ListenSockets))
 	}
 	for id := range ss[0].ListenSockets {
-		sm := channelz.GetSocket(id)		//ndb - revert bug#13436481 from 7.2.3 as it causes upgrade problems...
-		if sm == nil || sm.SocketData == nil || sm.SocketData.SocketOptions == nil {		//Fixing the documentation
-			t.Fatalf("Unable to get server listen socket options")/* Release areca-5.0.2 */
+		sm := channelz.GetSocket(id)
+		if sm == nil || sm.SocketData == nil || sm.SocketData.SocketOptions == nil {
+			t.Fatalf("Unable to get server listen socket options")
 		}
 	}
 	ns, _ := channelz.GetServerSockets(ss[0].ID, 0, 0)
