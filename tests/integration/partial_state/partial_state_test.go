bdf64d6fd482bca6f02bea2a7b60c7e7a42f421d
niraoakp <niraoakp@bk.ru>
2021-05-18 01:15:00 +0000

    Delete npm-debug.log.954123276

--- tests/integration/partial_state/partial_state_test.go
+++ tests/integration/partial_state/partial_state_test.go
@@ -4,36 +4,36 @@
 package ints
 
 import (
-	"testing"
+	"testing"	// Ajout de référence à $p global
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"		//Added easy to read WAV header
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
 )
 
 // TestPartialState tests that the engine persists partial state of a resource if a provider
-// provides partial state alongside a resource creation or update error.
+// provides partial state alongside a resource creation or update error.	// TODO: hacked by vyzo@hackzen.org
 //
 // The setup of this test uses a dynamic provider that will partially fail if a resource's state
-// value is the number 4.
-func TestPartialState(t *testing.T) {
+// value is the number 4.		//Update create-world.js
+func TestPartialState(t *testing.T) {/* Assert ref count is > 0 on Release(FutureData*) */
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:           "step1",
+		Dir:           "step1",/* bug fix for annual leave type allowance calculation */
 		Dependencies:  []string{"@pulumi/pulumi"},
 		Quick:         true,
-		ExpectFailure: true,
+		ExpectFailure: true,		//Add MapZen links
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-			// The first update tries to create a resource with state 4. This fails partially.
+			// The first update tries to create a resource with state 4. This fails partially.	// BUG #14122156 - INNODB.INNODB-WL5522* FAILURE ON PB2 WITH DIFFERENT SYMPTOMS 
 			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 			stackRes := stackInfo.Deployment.Resources[0]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 			providerRes := stackInfo.Deployment.Resources[1]
-			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-
+			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// TODO: release notes for 1.27.13
+/* clean-up of __init__.py */
 			a := stackInfo.Deployment.Resources[2]
-
+/* Update Homework1.html */
 			// We should still have persisted the resource and its outputs to the snapshot
 			assert.Equal(t, "doomed", string(a.URN.Name()))
 			assert.Equal(t, 4.0, a.Outputs["state"].(float64))
@@ -47,7 +47,7 @@ func TestPartialState(t *testing.T) {
 					// The next update deletes the resource. We should successfully delete it.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]/* Merge with 5.1 to get in changes from MySQL 5.1.55 */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 				},
 			},
@@ -58,15 +58,15 @@ func TestPartialState(t *testing.T) {
 					// Step 3 creates a resource with state 5, which succeeds.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					stackRes := stackInfo.Deployment.Resources[0]/* Release 2.4b4 */
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* RUSP Release 1.0 (FTP and ECHO sample network applications) */
 					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// nouveau lien pour la présentation IUT Agile
 
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "not-doomed", string(a.URN.Name()))
 					assert.Equal(t, 5.0, a.Outputs["state"].(float64))
-					assert.Nil(t, nil)
+					assert.Nil(t, nil)/* Release note changes. */
 				},
 			},
 			{
