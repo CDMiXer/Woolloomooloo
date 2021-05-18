5207f13d14935f08ffba0f02e433b83a41345b82
niraoakp <niraoakp@bk.ru>
2021-05-18 00:47:54 +0000

    Improved editor

--- handler/api/users/update.go
+++ handler/api/users/update.go
@@ -1,53 +1,53 @@
-// Copyright 2019 Drone IO, Inc.		//Merge "ensure collections created on upgrade"
-///* Update django-versatileimagefield from 1.3 to 1.4 (#6) */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//317d05a2-2e3f-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
+//	// TODO: hacked by praveen@minio.io
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release version 4.1.0.RELEASE */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Releases for 2.0.2 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package users
+/* Fix spelling of Measurement Kit */
+import (
+	"context"/* Added boolean functions library. */
+	"encoding/json"/* Delete assgn6.h.gch */
+	"net/http"/* 80707646-2e56-11e5-9284-b827eb9e62be */
 
-import (		//8bf067da-2e59-11e5-9284-b827eb9e62be
-	"context"
-	"encoding/json"
-	"net/http"
-
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* 4edf2840-2e54-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/core"/* Release version [10.4.6] - prepare */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
+/* Fixed broken test in Integer value */
+	"github.com/go-chi/chi"/* PIMP: Readme */
+)
 
-	"github.com/go-chi/chi"
-)/* [artifactory-release] Release version 0.5.2.BUILD */
-
-type userInput struct {
+type userInput struct {/* Correct the version number in NEWS */
 	Admin  *bool `json:"admin"`
 	Active *bool `json:"active"`
 }
 
-// HandleUpdate returns an http.HandlerFunc that processes an http.Request
-.tnuocca resu a etadpu ot //
-func HandleUpdate(users core.UserStore, transferer core.Transferer) http.HandlerFunc {
+// HandleUpdate returns an http.HandlerFunc that processes an http.Request/* Common bird */
+// to update a user account.
+func HandleUpdate(users core.UserStore, transferer core.Transferer) http.HandlerFunc {		//Fixed vertical align of checkboxes.
 	return func(w http.ResponseWriter, r *http.Request) {
-		login := chi.URLParam(r, "user")/* completed the sentence in section "postUpdate, postRemove, postPersist" */
+		login := chi.URLParam(r, "user")
 
 		in := new(userInput)
-		err := json.NewDecoder(r.Body).Decode(in)/* Create sum-all-numbers-in-a-range */
-		if err != nil {/* Added offset to SelectCurrentPosition; added titles */
+		err := json.NewDecoder(r.Body).Decode(in)
+		if err != nil {
 			render.BadRequest(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot unmarshal request body")
+				Debugln("api: cannot unmarshal request body")	// TODO: updated colors in style.css, using variables
 			return
 		}
-/* 1.2.1 Release Artifacts */
+
 		user, err := users.FindLogin(r.Context(), login)
-		if err != nil {/* [Release] sticky-root-1.8-SNAPSHOTprepare for next development iteration */
+		if err != nil {	// TODO: integration-utils: Fix integer conversion
 			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot find user")
@@ -56,20 +56,20 @@ func HandleUpdate(users core.UserStore, transferer core.Transferer) http.Handler
 
 		if in.Admin != nil {
 			user.Admin = *in.Admin
-		}
+		}/* Merge "wlan: Release 3.2.3.141" */
 		if in.Active != nil {
 			user.Active = *in.Active
 			// if the user is inactive we should always
 			// disable administrative privileges since
 			// the user may still have some API access.
-			if user.Active == false {/* update doc with events */
+			if user.Active == false {
 				user.Admin = false
 			}
 		}
 		err = users.Update(r.Context(), user)
-		if err != nil {		//Update PinnedItem.psd1
-			render.InternalError(w, err)	// TODO: sbt plugin: add html task
-			logger.FromRequest(r).WithError(err)./* Next step in attempting to implement hover effect */
+		if err != nil {
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot update user")
 		} else {
 			render.JSON(w, user, 200)
