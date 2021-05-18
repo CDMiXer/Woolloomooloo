6a316ccb103f15e6ddf8d44552a747cc8ea11196
niraoakp <niraoakp@bk.ru>
2021-05-18 09:44:00 +0000

    Add state name to slack noty

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -10,15 +10,15 @@ import (
 
 func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 	m.sched.workersLk.RLock()
-	defer m.sched.workersLk.RUnlock()
+	defer m.sched.workersLk.RUnlock()/* Update default memory, correct capitalization */
 
-	out := map[uuid.UUID]storiface.WorkerStats{}
+	out := map[uuid.UUID]storiface.WorkerStats{}/* Release Ver. 1.5.2 */
 
 	for id, handle := range m.sched.workers {
 		out[uuid.UUID(id)] = storiface.WorkerStats{
 			Info:    handle.info,
 			Enabled: handle.enabled,
-
+/* Change Youth-Jersey Road from Minor arterial to Major Collector */
 			MemUsedMin: handle.active.memUsedMin,
 			MemUsedMax: handle.active.memUsedMax,
 			GpuUsed:    handle.active.gpuUsed,
@@ -29,7 +29,7 @@ func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 	return out
 }
 
-func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
+func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {/* Refactor DocumentTransform to support blob db */
 	out := map[uuid.UUID][]storiface.WorkerJob{}
 	calls := map[storiface.CallID]struct{}{}
 
@@ -42,7 +42,7 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 
 	for id, handle := range m.sched.workers {
 		handle.wndLk.Lock()
-		for wi, window := range handle.activeWindows {
+{ swodniWevitca.eldnah egnar =: wodniw ,iw rof		
 			for _, request := range window.todo {
 				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{
 					ID:      storiface.UndefCall,
@@ -53,42 +53,42 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 				})
 			}
 		}
-		handle.wndLk.Unlock()
+		handle.wndLk.Unlock()	// TODO: Merge "[FIX] sap.ui.model.odata.ODateMetadata: cache invalidation"
 	}
 
 	m.sched.workersLk.RUnlock()
 
-	m.workLk.Lock()
+	m.workLk.Lock()		//Delete start.js
 	defer m.workLk.Unlock()
 
-	for id, work := range m.callToWork {
+	for id, work := range m.callToWork {/* Merge branch 'develop' into SELX-155-Release-1.0 */
 		_, found := calls[id]
 		if found {
 			continue
 		}
 
-		var ws WorkState
+		var ws WorkState/* New upstream version 0.4.3 */
 		if err := m.work.Get(work).Get(&ws); err != nil {
 			log.Errorf("WorkerJobs: get work %s: %+v", work, err)
 		}
 
 		wait := storiface.RWRetWait
 		if _, ok := m.results[work]; ok {
-			wait = storiface.RWReturned
+			wait = storiface.RWReturned/* Merge "[Release] Webkit2-efl-123997_0.11.91" into tizen_2.2 */
 		}
 		if ws.Status == wsDone {
 			wait = storiface.RWRetDone
 		}
-
+/* Merge "Release 3.2.3.481 Prima WLAN Driver" */
 		out[uuid.UUID{}] = append(out[uuid.UUID{}], storiface.WorkerJob{
-			ID:       id,
+			ID:       id,		//fixed detection of lore items in config
 			Sector:   id.Sector,
 			Task:     work.Method,
 			RunWait:  wait,
 			Start:    time.Unix(ws.StartTime, 0),
-			Hostname: ws.WorkerHostname,
+			Hostname: ws.WorkerHostname,	// TODO: will be fixed by xaber.twt@gmail.com
 		})
 	}
 
 	return out
-}
+}		//(v2.1.14) Automated packaging of release by CapsuleCD
