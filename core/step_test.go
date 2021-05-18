bbf616a5e6ee144a02201d9441a74a814b5e52cf
niraoakp <niraoakp@bk.ru>
2021-05-18 08:49:46 +0000

    add many features (see in-file changelog)

--- core/step_test.go
+++ core/step_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
-// that can be found in the LICENSE file./* Merge "Gerrit 2.3 ReleaseNotes" */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package core
 
 import "testing"
-/* First commit in new branch */
-{ )T.gnitset* t(enoDsIpetStseT cnuf
+
+func TestStepIsDone(t *testing.T) {
 	for _, status := range statusDone {
-		v := Step{Status: status}/* add some simple integration tests, refs #3484 */
-		if v.IsDone() == false {	// hi "हिन्दी, हिंदी" translation #15171. Author: YOOpyy. 
+		v := Step{Status: status}
+		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
 		}
 	}
-/* Updated Model with bot capaibilites */
+
 	for _, status := range statusNotDone {
 		v := Step{Status: status}
-		if v.IsDone() == true {/* Allows PHPUnit 9 */
+		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
 		}
 	}
