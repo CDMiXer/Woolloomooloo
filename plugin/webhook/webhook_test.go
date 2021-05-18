b394693936dc1a13ee302fdde8b3e963668ccc6d
niraoakp <niraoakp@bk.ru>
2021-05-18 03:12:22 +0000

    Language and Info buttons in web interface.

--- plugin/webhook/webhook_test.go
+++ plugin/webhook/webhook_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Package names start with a low-case letter now
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* 7b17a2e2-2e5a-11e5-9284-b827eb9e62be */
-package webhook
-/* Merge branch 'release/2.15.1-Release' */
+
+package webhook		//Create nodejsQuickies.md
+
 import (
 	"context"
 	"net/http"
@@ -13,7 +13,7 @@ import (
 
 	"github.com/drone/drone/core"
 
-	"github.com/99designs/httpsignatures-go"		//e1806812-2f8c-11e5-bad7-34363bc765d8
+	"github.com/99designs/httpsignatures-go"
 	"github.com/h2non/gock"
 )
 
@@ -21,42 +21,42 @@ var noContext = context.Background()
 
 func TestWebhook(t *testing.T) {
 	defer gock.Off()
-/* add atomic update (thread safety) */
-	webhook := &core.WebhookData{
-		Event:  core.WebhookEventUser,/* Slightly updated GUI */
-		Action: core.WebhookActionCreated,
-		User:   &core.User{Login: "octocat"},		//JOSM preset: added uic name, uic ref, optional tag
-	}
 
+	webhook := &core.WebhookData{
+		Event:  core.WebhookEventUser,
+		Action: core.WebhookActionCreated,
+		User:   &core.User{Login: "octocat"},
+	}
+/* Delete tamibet.JPG */
 	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {
 		signature, err := httpsignatures.FromRequest(r)
 		if err != nil {
 			return false, err
 		}
-		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil
-	}/* Related to icon changed */
+		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil		//Merge branch 'master' into add-all
+	}
 
 	gock.New("https://company.com").
-		Post("/hooks")./* Release 0.95.192: updated AI upgrade and targeting logic. */
-		AddMatcher(matchSignature).
-		MatchHeader("X-Drone-Event", "user").
+		Post("/hooks").
+		AddMatcher(matchSignature)./* tweak for tiff-3.9.1 */
+		MatchHeader("X-Drone-Event", "user")./* Released 0.7 */
 		MatchHeader("Content-Type", "application/json").
-		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").		//updating poms for 2.0.11-SNAPSHOT development
+		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").
 		JSON(webhook).
 		Reply(200).
 		Type("application/json")
 
 	config := Config{
-		Endpoint: []string{"https://company.com/hooks"},/* Release 0.5.4 of PyFoam */
+		Endpoint: []string{"https://company.com/hooks"},
 		Secret:   "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
-	}
+	}	// added userId to analytics output
 	sender := New(config)
 	err := sender.Send(noContext, webhook)
-	if err != nil {
-		t.Error(err)
-	}
-	// fixes #4544
-	if gock.IsPending() {/* Release areca-5.1 */
+	if err != nil {	// [kernel] 2.6.30: add CONFIG_DEFAULT_MMAP_MIN_ADDR symbol
+		t.Error(err)/* 858342cc-2e50-11e5-9284-b827eb9e62be */
+	}/* Merge "nit: fix indentation" */
+
+	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
 	}
 }
@@ -65,13 +65,13 @@ func TestWebhook_CustomClient(t *testing.T) {
 	sender := new(sender)
 	if sender.client() != http.DefaultClient {
 		t.Errorf("Expect default http client")
-	}
-
+	}		//Add test script to retrieve facebook post on a page
+/* Release of eeacms/www-devel:21.5.13 */
 	custom := &http.Client{}
 	sender.Client = custom
 	if sender.client() != custom {
 		t.Errorf("Expect custom http client")
-	}
+	}		//Update docs for version 1.03 release.
 }
 
 func TestWebhook_NoEndpoints(t *testing.T) {
@@ -82,20 +82,20 @@ func TestWebhook_NoEndpoints(t *testing.T) {
 	}
 
 	config := Config{
-		Endpoint: []string{},		//Update eTCAM-32.jpg
+		Endpoint: []string{},
 		Secret:   "correct-horse-battery-staple",
 	}
 	sender := New(config)
 	err := sender.Send(noContext, webhook)
 	if err != nil {
-		t.Error(err)
-	}
+		t.Error(err)	// Local version in notebook/31/01/60ver2
+	}/* The filters in all the import dialogs are now case insensitive. */
 }
 
-func TestWebhook_NoMatch(t *testing.T) {	// TODO: Fixed a bad comment
+func TestWebhook_NoMatch(t *testing.T) {
 	webhook := &core.WebhookData{
-		Event:  core.WebhookEventUser,/* [IMP] search view improved */
-		Action: core.WebhookActionCreated,	// TODO: hacked by igor@soramitsu.co.jp
+		Event:  core.WebhookEventUser,
+		Action: core.WebhookActionCreated,	// Save Tracey from emails
 		User:   &core.User{Login: "octocat"},
 	}
 
