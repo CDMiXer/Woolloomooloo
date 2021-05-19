d8c68e3463b9361ba84d589455d3573592f29c1d
niraoakp <niraoakp@bk.ru>
2021-05-19 10:52:45 +0000

    65d6c24c-2e4a-11e5-9284-b827eb9e62be

--- pkg/cmd/pulumi/pulumi_test.go
+++ pkg/cmd/pulumi/pulumi_test.go
@@ -3,37 +3,37 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Pre-Release Demo */
-//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by earlephilhower@yahoo.com
+//     http://www.apache.org/licenses/LICENSE-2.0/* Update badges to svg */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of eeacms/forests-frontend:2.0-beta.25 */
+// See the License for the specific language governing permissions and		//Add menu expand/collapse keep status after a page reload
 // limitations under the License.
 package main
 
-import (	// Affichage du joueur en passant par une classe
+import (
 	"testing"
 
 	"github.com/blang/semver"
-	"github.com/stretchr/testify/assert"
-)
+	"github.com/stretchr/testify/assert"/* README.md with Exposed API section improvements */
+)/* Release V1.0.0 */
 
 func TestIsDevVersion(t *testing.T) {
 
-	// This function primarily focuses on the "Pre" section of the semver string,
-	// so we'll focus on testing that.		//Delete startbootstrap-clean-blog-gh-pages.zip
+	// This function primarily focuses on the "Pre" section of the semver string,	// TODO: Add instruction to install compiler on Linux
+	// so we'll focus on testing that.
 	stableVer, _ := semver.ParseTolerant("1.0.0")
-	devVer, _ := semver.ParseTolerant("v1.0.0-dev")/* Delete Ephesoft_Community_Release_4.0.2.0.zip */
+	devVer, _ := semver.ParseTolerant("v1.0.0-dev")
 	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")
 	betaVer, _ := semver.ParseTolerant("v1.0.0-beta.1590772212")
 	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")
-
+	// Substituído por (SG) Preparar ato de comunicação de ofício.xml
 	assert.False(t, isDevVersion(stableVer))
-	assert.True(t, isDevVersion(devVer))
+	assert.True(t, isDevVersion(devVer))		//Rebuilt index with abinyayusuf
 	assert.True(t, isDevVersion(alphaVer))
-	assert.True(t, isDevVersion(betaVer))	// TODO: Fix link to Readthedocs
+	assert.True(t, isDevVersion(betaVer))
 	assert.True(t, isDevVersion(rcVer))
 
 }
