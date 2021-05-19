9eb93cb4e2686620d0a7556a7ff2ef47a4ba8b87
niraoakp <niraoakp@bk.ru>
2021-05-19 13:17:13 +0000

    Rotation Complete / Added x^2 button

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -1,34 +1,34 @@
 config storageAccountNameParam string {
-}/* Task #3241: Merge of latest changes in LOFAR-Release-0_96 into trunk */
-
-config resourceGroupNameParam string {
 }
 
+config resourceGroupNameParam string {
+}/* version Release de clase Usuario con convocatoria incluida */
+/* Update SetEntityMotionPacket.php */
 resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {
-	name = resourceGroupNameParam
-})
+	name = resourceGroupNameParam	// I2C: Add more help about using stream for debugging.
+})		//Rename font-awesome/less/stacked.less to less/font-awesome/stacked.less
 
 config locationParam string {
 	default = resourceGroupVar.location
 }
 
-config storageAccountTierParam string {
-    default = "Standard"/* Release of eeacms/forests-frontend:2.0-beta.37 */
+config storageAccountTierParam string {	// TODO: will be fixed by steven@stebalien.com
+    default = "Standard"
+}
+/* Merge "DO NOT MERGE - Add ShareCompat to the support library." into ics-mr1 */
+config storageAccountTypeReplicationParam string {
+    default = "LRS"
 }
 
-config storageAccountTypeReplicationParam string {
-    default = "LRS"/* - APM. New version of JasperReports. */
-}		//Updated #077
-
-resource storageAccountResource "azure:storage/account:Account" {/* Release 2.2 tagged */
+resource storageAccountResource "azure:storage/account:Account" {/* --interactive documentation */
 	name = storageAccountNameParam
 	accountKind = "StorageV2"
 	location = locationParam
 	resourceGroupName = resourceGroupNameParam
 	accountTier = storageAccountTierParam
 	accountReplicationType = storageAccountTypeReplicationParam
-}/* chore: Release 0.22.7 */
+}/* Release pointer bug */
 
-output storageAccountNameOut {/* Auto configure administration password environment variables provided */
-	value = storageAccountResource.name/* Release of eeacms/forests-frontend:1.6.1 */
+output storageAccountNameOut {
+	value = storageAccountResource.name
 }
