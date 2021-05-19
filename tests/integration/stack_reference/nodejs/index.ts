30806b64265cf1dad572040afba9763545af2a49
niraoakp <niraoakp@bk.ru>
2021-05-19 17:37:14 +0000

    Made a change on github.com editor

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Major bug fix on mtcp_safe_pipe() (wasn't actually setting the incoming array)
+
 import * as pulumi from "@pulumi/pulumi";
-/* Release 1.10rc1 */
-let config = new pulumi.Config();/* Release v2.5.1 */
+
+let config = new pulumi.Config();/* Latest German translation */
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
-export const val = ["a", "b"];		//Comment line back in
+export const val = ["a", "b"];
