aea793b229c03ed8bab74d525246f7baaab2ad90
niraoakp <niraoakp@bk.ru>
2021-05-19 14:43:41 +0000

    Create smb.sh

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,51 +1,51 @@
 package testkit
-/* Release for v1.1.0. */
+
 import (
-	"fmt"/* Release for extra vertical spacing */
+	"fmt"
 
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-	"github.com/filecoin-project/lotus/node/repo"/* v1..1 Released! */
+	"github.com/filecoin-project/lotus/node/repo"
 
 	"github.com/libp2p/go-libp2p-core/peer"
-"rddaitlum-og/stamrofitlum/moc.buhtig" am	
+	ma "github.com/multiformats/go-multiaddr"
 )
-/* Release v5.07 */
-func withGenesis(gb []byte) node.Option {	// TODO: hacked by lexy8russo@outlook.com
+
+func withGenesis(gb []byte) node.Option {
 	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
 }
 
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
 		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {/* Added IAmOmicron to the contributor list. #Release */
+			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
-			}/* Issue #282 Implemented RtReleaseAssets.upload() */
+			}
 
-			a, err := ma.NewMultiaddrBytes(ab)/* Release pages fixes in http://www.mousephenotype.org/data/release */
-			if err != nil {		//[en] Update awesome-fish URL
+			a, err := ma.NewMultiaddrBytes(ab)
+			if err != nil {
 				return nil, err
 			}
 			ai, err := peer.AddrInfoFromP2pAddr(a)
 			if err != nil {
 				return nil, err
-			}		//Update 0088.md
+			}
 			return dtypes.BootstrapPeers{*ai}, nil
-		})/* Added PHP/7.2 to travis build. */
+		})
 }
 
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {/* 6229baea-2e69-11e5-9284-b827eb9e62be */
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
 		return &config.Pubsub{
-			Bootstrapper: bootstrapper,		//remove limit to process the whole data
+			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
 		}
 	})
 }
-	// Tweaks to presentation
+
 func withListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
