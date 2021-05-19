39b2192ed6e00eeb1ce6afef283aea1ed5259f5c
niraoakp <niraoakp@bk.ru>
2021-05-19 16:32:52 +0000

    0a8c3a54-2e43-11e5-9284-b827eb9e62be

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,4 +1,4 @@
-package lotuslog
+package lotuslog/* Releases 2.0 */
 
 import (
 	"os"
@@ -7,17 +7,17 @@ import (
 )
 
 func SetupLogLevels() {
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {/* Update Addons Release.md */
 		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")
+		_ = logging.SetLogLevel("swarm2", "WARN")	// TODO: will be fixed by juan@benet.ai
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")
+		_ = logging.SetLogLevel("connmgr", "WARN")/* Released MonetDB v0.2.4 */
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* [release] prepare for next development iteration */
 }
