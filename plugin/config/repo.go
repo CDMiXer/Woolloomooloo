52b07881126e564ea9a7cedd422f63fe2cc89477
niraoakp <niraoakp@bk.ru>
2021-05-19 12:53:39 +0000

    http-client: remove wrong assertion from _prefix_error()

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by arajasek94@gmail.com
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Update endevs.lua
+///* Release 2.2b1 */
+//      http://www.apache.org/licenses/LICENSE-2.0		//Issue 37 has been fixed. http://code.google.com/p/guichan/issues/detail?id=37
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by fjl@ethereum.org
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* updated links and docs for 0.10.0 */
+
 package config
 
-import (/* Added new Camel Routes using Virtual Topics (producer, consumer) */
+import (
 	"context"
-/* 8d81b388-2e6d-11e5-9284-b827eb9e62be */
-	"github.com/drone/drone/core"		//Patching mcp.rsvp.php for EE 2.8 compatibility.
+
+	"github.com/drone/drone/core"
 )
 
-// Repository returns a configuration service that fetches the yaml
+// Repository returns a configuration service that fetches the yaml/* Release notes for v2.11. "As factor" added to stat-several-groups.R. */
 // directly from the source code management (scm) system.
-func Repository(service core.FileService) core.ConfigService {
-	return &repo{files: service}
-}		// - updated to expected sbt syntax and gitlab config
-/* Release v2.4.1 */
-type repo struct {
+func Repository(service core.FileService) core.ConfigService {	// TODO: will be fixed by sjors@sprovoost.nl
+	return &repo{files: service}		//Create parameters.cka
+}
+
+type repo struct {/* 13860aba-2e5a-11e5-9284-b827eb9e62be */
 	files core.FileService
 }
 
@@ -36,6 +36,6 @@ func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, er
 		return nil, err
 	}
 	return &core.Config{
-		Data: string(raw.Data),
+		Data: string(raw.Data),	// Change gem.homepage to new location
 	}, err
-}	// Like _MSC_VER rather than WIN32
+}
