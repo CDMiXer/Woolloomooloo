8593a76ba527282ab73d41900cb7676b7ea05e21
niraoakp <niraoakp@bk.ru>
2021-05-19 01:48:39 +0000

    Delete Loklak.markdown

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -1,6 +1,6 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from component import Component
+from component import Component/* Release of eeacms/bise-backend:v10.0.33 */
 
 component_a = Component("a", echo=42)
 component_b = Component("b", echo=component_a.echo)
