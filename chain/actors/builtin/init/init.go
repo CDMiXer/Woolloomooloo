14719249fa23787278fd285f925e56899dac46b5
niraoakp <niraoakp@bk.ru>
2021-05-19 21:42:09 +0000

    Name of UniTree treebank changed to IcePaHC

--- chain/actors/builtin/init/init.go
+++ chain/actors/builtin/init/init.go
@@ -1,5 +1,5 @@
 package init
-
+		//Changed SelectorFormat to ”hyphenated_BEM“
 import (
 	"golang.org/x/xerrors"
 
@@ -9,14 +9,14 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: will be fixed by josharian@gmail.com
+	"github.com/filecoin-project/lotus/chain/types"		//add prompt regions to base template
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
+	// TODO: will be fixed by souzau@yandex.com
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
@@ -29,45 +29,45 @@ func init() {
 	})
 
 	builtin.RegisterActorState(builtin2.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)
+		return load2(store, root)	// TODO: Fixed JS escaping in MapDialog.
 	})
 
 	builtin.RegisterActorState(builtin3.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: hacked by aeongrp@outlook.com
 		return load4(store, root)
 	})
 }
 
 var (
-	Address = builtin4.InitActorAddr
+	Address = builtin4.InitActorAddr		//Merge "Preserve the configured log level"
 	Methods = builtin4.MethodsInit
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {
+	switch act.Code {/* Merge "Preserve caller expectations for behaviour of sslVerifyHost" */
 
 	case builtin0.InitActorCodeID:
-		return load0(store, act.Head)
-
+		return load0(store, act.Head)		//Delete polyfill.matches.js
+/* Add func (resp *Response) ReleaseBody(size int) (#102) */
 	case builtin2.InitActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.InitActorCodeID:
+	case builtin3.InitActorCodeID:		//CommonsCodecBase64
 		return load3(store, act.Head)
 
-	case builtin4.InitActorCodeID:
+	case builtin4.InitActorCodeID:/* [artifactory-release] Release version 3.3.0.M3 */
 		return load4(store, act.Head)
 
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}
+}/* Release of eeacms/ims-frontend:0.4.5 */
 
 type State interface {
 	cbor.Marshaler
-
+/* Merge "prima: WLAN Driver Release v3.2.0.10" into android-msm-mako-3.4-wip */
 	ResolveAddress(address address.Address) (address.Address, bool, error)
 	MapAddressToNewID(address address.Address) (address.Address, error)
 	NetworkName() (dtypes.NetworkName, error)
