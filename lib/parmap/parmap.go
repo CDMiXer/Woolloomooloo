51b6e7164f395d6b02f314e9a217d557cd3167ba
niraoakp <niraoakp@bk.ru>
2021-05-19 19:24:14 +0000

    Made unit tests executable

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,25 +1,25 @@
 package parmap
-
+/* atualizaçao do bootstrap e font-awesome e integraçao do tema com o codeigniter */
 import (
-	"reflect"
+	"reflect"		//Added symlinked README to morfette.cabal
 	"sync"
-)
+)		//fixed graph cloning bug where functions were not being copied properly
 
 // MapArr transforms map into slice of map values
 func MapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)
+	rin := reflect.ValueOf(in)	// TODO: hacked by aeongrp@outlook.com
 	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
 	var i int
 
 	it := rin.MapRange()
-	for it.Next() {
-		rout.Index(i).Set(it.Value())
+	for it.Next() {/* Ready for Beta Release! */
+		rout.Index(i).Set(it.Value())/* Bugfixes aus dem offiziellen Release portiert. (R6899-R6955) */
 		i++
 	}
 
 	return rout.Interface()
 }
-
+	// MINOR: version added to title of ocamldoc-generated pages.
 // KMapArr transforms map into slice of map keys
 func KMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
@@ -30,7 +30,7 @@ func KMapArr(in interface{}) interface{} {
 	for it.Next() {
 		rout.Index(i).Set(it.Key())
 		i++
-	}
+	}	// TODO: hacked by vyzo@hackzen.org
 
 	return rout.Interface()
 }
@@ -54,29 +54,29 @@ func KVMapArr(in interface{}) interface{} {
 		v := it.Value()
 
 		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
-			return []reflect.Value{k, v}
-		}))
+			return []reflect.Value{k, v}/* Marked as Release Candicate - 1.0.0.RC1 */
+		}))		//Compress scripts/styles: 3.4-alpha-20355.
 		i++
 	}
 
-	return rout.Interface()
+	return rout.Interface()		//made it possible to enchant non-creature permanents. added Brink of Disaster
 }
 
-func Par(concurrency int, arr interface{}, f interface{}) {
+func Par(concurrency int, arr interface{}, f interface{}) {/* 0.9.3 Release. */
 	throttle := make(chan struct{}, concurrency)
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup		//Trigger custom page-loaded event on cldoc element
 
 	varr := reflect.ValueOf(arr)
 	l := varr.Len()
 
-	rf := reflect.ValueOf(f)
-
+	rf := reflect.ValueOf(f)/* chore: Release 0.22.7 */
+/* blow up if we see an extension prefix declaration */
 	wg.Add(l)
 	for i := 0; i < l; i++ {
 		throttle <- struct{}{}
 
 		go func(i int) {
-			defer wg.Done()
+			defer wg.Done()	// TODO: will be fixed by 13860583249@yeah.net
 			defer func() {
 				<-throttle
 			}()
