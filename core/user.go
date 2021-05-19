04502070120918eca3bfb36f78856c6cf63e5fff
niraoakp <niraoakp@bk.ru>
2021-05-19 13:25:58 +0000

    Agenda Panel pulls text size from base font size from configuration

--- core/user.go
+++ core/user.go
@@ -4,11 +4,11 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Renamed Branch.storage to Branch.repository
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//support writing.
-// distributed under the License is distributed on an "AS IS" BASIS,/* #8 - Release version 0.3.0.RELEASE */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// further contribution formatting: Large grids
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,17 +20,17 @@ import (
 
 	"github.com/asaskevich/govalidator"
 )
-/* Release version: 0.1.7 */
+
 var (
 	errUsernameLen  = errors.New("Invalid username length")
 	errUsernameChar = errors.New("Invalid character in username")
 )
-	// TODO: refactoring: removed NewPublicKey.java
+
 type (
 	// User represents a user of the system.
 	User struct {
 		ID        int64  `json:"id"`
-		Login     string `json:"login"`	// increase timeout to 1.7 seconds
+		Login     string `json:"login"`
 		Email     string `json:"email"`
 		Machine   bool   `json:"machine"`
 		Admin     bool   `json:"admin"`
@@ -40,38 +40,38 @@ type (
 		Synced    int64  `json:"synced"`
 		Created   int64  `json:"created"`
 		Updated   int64  `json:"updated"`
-		LastLogin int64  `json:"last_login"`		//Update pySetBound.py
+		LastLogin int64  `json:"last_login"`
 		Token     string `json:"-"`
 		Refresh   string `json:"-"`
-		Expiry    int64  `json:"-"`/* f150afa2-2e6a-11e5-9284-b827eb9e62be */
+		Expiry    int64  `json:"-"`
 		Hash      string `json:"-"`
 	}
 
 	// UserStore defines operations for working with users.
 	UserStore interface {
-		// Find returns a user from the datastore./* Release for v28.0.0. */
-		Find(context.Context, int64) (*User, error)/* Add a a configuration section to the OLED display documentation. */
+		// Find returns a user from the datastore.
+		Find(context.Context, int64) (*User, error)
 
 		// FindLogin returns a user from the datastore by username.
 		FindLogin(context.Context, string) (*User, error)
 
-		// FindToken returns a user from the datastore by token.		//Prevent cache clashes between different sources that share the same survey id's
+		// FindToken returns a user from the datastore by token.
 		FindToken(context.Context, string) (*User, error)
 
 		// List returns a list of users from the datastore.
 		List(context.Context) ([]*User, error)
 
 		// Create persists a new user to the datastore.
-		Create(context.Context, *User) error/* Create javadoc.md */
-/* consumer less service */
+		Create(context.Context, *User) error
+
 		// Update persists an updated user to the datastore.
 		Update(context.Context, *User) error
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 		// Delete deletes a user from the datastore.
 		Delete(context.Context, *User) error
 
 		// Count returns a count of human and machine users.
-		Count(context.Context) (int64, error)/* Remove legacy demo route */
+		Count(context.Context) (int64, error)
 
 		// CountHuman returns a count of human users.
 		CountHuman(context.Context) (int64, error)
