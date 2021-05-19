95d430b88d336dbabce61736d1de98bf1bf76c62
niraoakp <niraoakp@bk.ru>
2021-05-19 07:10:53 +0000

    satellites: re-organize source to clear root of plugin tree

--- build/parameters.go
+++ build/parameters.go
@@ -4,4 +4,4 @@ import rice "github.com/GeertJohan/go.rice"
 
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}	// TODO: hacked by sebastian.tharakan97@gmail.com
+}/* TvTunes: Release of screensaver */
