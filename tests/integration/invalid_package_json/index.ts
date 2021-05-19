ab8e1c0da15878d5560bca77e642b21529e03d14
niraoakp <niraoakp@bk.ru>
2021-05-19 20:07:38 +0000

    Map is now sent as parameter and state is not reset after draw.

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -2,17 +2,17 @@
 
 import { Config } from "@pulumi/pulumi";
 import * as runtime from "@pulumi/pulumi/runtime"
-
+/* Privacy update w/ GDPR */
 (async function() {
     const config = new Config();
-
+		//Use notBefore as a date not a number
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
-    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that/* modify intro. */
     // behavior.
-    const deps = await runtime.computeCodePaths();
+    const deps = await runtime.computeCodePaths();		//b7932bc0-2e4f-11e5-9284-b827eb9e62be
 
     const actual = JSON.stringify([...deps.keys()].sort());
-    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
+    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";		//- dream details - resources table updates
 
     if (actual !== expected) {
         throw new Error(`Got '${actual}' expected '${expected}'`)
