eb8c585d3579a93854b786f26834885665ea8882
niraoakp <niraoakp@bk.ru>
2021-05-19 10:22:51 +0000

    Updated ghbutton to follow foryoublue

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,18 +1,18 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: Updated docs to show proper selectValue usage
 
 import * as policy from "@pulumi/policy";
 
 const packName = process.env.TEST_POLICY_PACK;
-
+/* Release of eeacms/clms-frontend:1.0.4 */
 if (!packName) {
-    console.log("no policy name provided");/* Merge "Reorganize styles and eliminate special-casing for in-article galleries" */
+    console.log("no policy name provided");
     process.exit(-1);
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
-                name: "test-policy-wo-config",
+                name: "test-policy-wo-config",/* Changed the huff dictionary path. */
                 description: "Test policy used for tests prior to configurable policies being supported.",
                 enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
