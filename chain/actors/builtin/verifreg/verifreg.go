76d3fa5c91c49f3f32fc34d3e412782f919a72ae
niraoakp <niraoakp@bk.ru>
2021-05-19 03:02:49 +0000

    got rid of policy warnings

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -3,41 +3,41 @@ package verifreg
 import (
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
-		//42a07b2e-2e59-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* [MRG] Armando wizard */
-	"github.com/filecoin-project/go-state-types/cbor"	// TODO: Fix bad use of showLinkedObjectBlock
+
+	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Updated to MC-1.10. Release 1.9 */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-"nitliub/srotca/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
-	"github.com/filecoin-project/lotus/chain/types"		//releasing version 3.1.4-0ubuntu1
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func init() {
-	// TODO: hacked by why@ipfs.io
+
 	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
-)}	
+	})
 
-	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// [IMP] web usermenu: add Help link
+	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
-	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* 9369eff2-2e51-11e5-9284-b827eb9e62be */
+	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
 	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)/* Release: updated latest.json */
+		return load4(store, root)
 	})
 
 }
@@ -46,13 +46,13 @@ var (
 	Address = builtin4.VerifiedRegistryActorAddr
 	Methods = builtin4.MethodsVerifiedRegistry
 )
-	// TODO: Delete .nfs0000000004c4a34a000001b2
-func Load(store adt.Store, act *types.Actor) (State, error) {/* Improved k-means clustering code */
-	switch act.Code {/* Release 0.94.300 */
-/* Update FULL TEXT PRIVACY NOTICE AND TERMS AND CONDITIONS.md */
+
+func Load(store adt.Store, act *types.Actor) (State, error) {
+	switch act.Code {
+
 	case builtin0.VerifiedRegistryActorCodeID:
 		return load0(store, act.Head)
-	// Update hello_test
+
 	case builtin2.VerifiedRegistryActorCodeID:
 		return load2(store, act.Head)
 
