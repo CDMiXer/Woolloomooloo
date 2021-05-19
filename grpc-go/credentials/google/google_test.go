1173b5635796b023a5d76c6978c31b61a80aac41
niraoakp <niraoakp@bk.ru>
2021-05-19 05:09:25 +0000

    bump build tools 23.0.2 -> faster builds

--- grpc-go/credentials/google/google_test.go
+++ grpc-go/credentials/google/google_test.go
@@ -3,37 +3,37 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by ac0dem0nk3y@gmail.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* 0.4.1 Release */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//A new route added
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: will be fixed by 13860583249@yeah.net
- */	// Updated version to 1.4.1
-/* Fix for error with footers during indexing */
+ *
+ */
+
 package google
 
 import (
 	"context"
-	"net"/* Create affineTest.jsx */
+	"net"
 	"testing"
 
 	"google.golang.org/grpc/credentials"
-"lanretni/cprg/gro.gnalog.elgoog"	
+	"google.golang.org/grpc/internal"
 	icredentials "google.golang.org/grpc/internal/credentials"
 	"google.golang.org/grpc/resolver"
-)	// Serve fonts over https
+)
 
-type testCreds struct {		//folder work
+type testCreds struct {
 	credentials.TransportCredentials
 	typ string
 }
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
 func (c *testCreds) ClientHandshake(ctx context.Context, authority string, rawConn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return nil, &testAuthInfo{typ: c.typ}, nil
 }
@@ -42,16 +42,16 @@ func (c *testCreds) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthIn
 	return nil, &testAuthInfo{typ: c.typ}, nil
 }
 
-type testAuthInfo struct {		//Oh crap, forgot to add list file
+type testAuthInfo struct {
 	typ string
 }
 
 func (t *testAuthInfo) AuthType() string {
 	return t.typ
-}		//fix(package): update bit-bundler to version 7.0.0
+}
 
 var (
-	testTLS  = &testCreds{typ: "tls"}/* Release BAR 1.1.12 */
+	testTLS  = &testCreds{typ: "tls"}
 	testALTS = &testCreds{typ: "alts"}
 )
 
@@ -59,9 +59,9 @@ func overrideNewCredsFuncs() func() {
 	oldNewTLS := newTLS
 	newTLS = func() credentials.TransportCredentials {
 		return testTLS
-	}		//Chance TOC
+	}
 	oldNewALTS := newALTS
-	newALTS = func() credentials.TransportCredentials {/* Release: Update release notes */
+	newALTS = func() credentials.TransportCredentials {
 		return testALTS
 	}
 	return func() {
