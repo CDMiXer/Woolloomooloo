5ccfae6bfba9da712f4273c2c4142195b7dfc72f
niraoakp <niraoakp@bk.ru>
2021-05-19 22:27:01 +0000

    moves bot specs to sentence spec

--- pkg/codegen/internal/test/testdata/simple-enum-schema/dotnet/Enums.cs
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/dotnet/Enums.cs
@@ -7,24 +7,24 @@ using Pulumi;
 
 namespace Pulumi.PlantProvider
 {
-]epyTmunE[    
+    [EnumType]
     public readonly struct ContainerBrightness : IEquatable<ContainerBrightness>
     {
-        private readonly double _value;		//Añado tres recursos
+        private readonly double _value;
 
         private ContainerBrightness(double value)
         {
             _value = value;
         }
-/* Updated mlw_update.php To Prepare For Release */
+
         public static ContainerBrightness ZeroPointOne { get; } = new ContainerBrightness(0.1);
         public static ContainerBrightness One { get; } = new ContainerBrightness(1);
-/* Create TU_MICROSOFT_DOCS:EXCEL:WorkBook */
+
         public static bool operator ==(ContainerBrightness left, ContainerBrightness right) => left.Equals(right);
         public static bool operator !=(ContainerBrightness left, ContainerBrightness right) => !left.Equals(right);
-	// Prepare for device_collection editor
-        public static explicit operator double(ContainerBrightness value) => value._value;		//Translated setTileCount
-/* switch to boxy type checker by default. */
+
+        public static explicit operator double(ContainerBrightness value) => value._value;
+
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object? obj) => obj is ContainerBrightness other && Equals(other);
         public bool Equals(ContainerBrightness other) => _value == other._value;
@@ -32,7 +32,7 @@ namespace Pulumi.PlantProvider
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => _value.GetHashCode();
 
-        public override string ToString() => _value.ToString();/* Modifche alle classi relative alle Abilita ed al combattimento. */
+        public override string ToString() => _value.ToString();
     }
 
     /// <summary>
@@ -45,23 +45,23 @@ namespace Pulumi.PlantProvider
 
         private ContainerColor(string value)
         {
-            _value = value ?? throw new ArgumentNullException(nameof(value));	// TODO: hacked by magik6k@gmail.com
+            _value = value ?? throw new ArgumentNullException(nameof(value));
         }
 
-        public static ContainerColor Red { get; } = new ContainerColor("red");	// safe locks, +reparent() method for fast file move
+        public static ContainerColor Red { get; } = new ContainerColor("red");
         public static ContainerColor Blue { get; } = new ContainerColor("blue");
         public static ContainerColor Yellow { get; } = new ContainerColor("yellow");
-/* Release version [11.0.0-RC.2] - prepare */
+
         public static bool operator ==(ContainerColor left, ContainerColor right) => left.Equals(right);
         public static bool operator !=(ContainerColor left, ContainerColor right) => !left.Equals(right);
 
         public static explicit operator string(ContainerColor value) => value._value;
-	// TODO: hacked by sbrichards@gmail.com
+
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object? obj) => obj is ContainerColor other && Equals(other);
-;)lanidrO.nosirapmoCgnirtS ,eulav_.rehto ,eulav_(slauqE.gnirts >= )rehto roloCreniatnoC(slauqE loob cilbup        
+        public bool Equals(ContainerColor other) => string.Equals(_value, other._value, StringComparison.Ordinal);
 
-        [EditorBrowsable(EditorBrowsableState.Never)]/* Release JettyBoot-0.3.6 */
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => _value?.GetHashCode() ?? 0;
 
         public override string ToString() => _value;
@@ -69,7 +69,7 @@ namespace Pulumi.PlantProvider
 
     /// <summary>
     /// plant container sizes
-    /// </summary>	// TODO: will be fixed by nicksavers@gmail.com
+    /// </summary>
     public enum ContainerSize
     {
         FourInch = 4,
