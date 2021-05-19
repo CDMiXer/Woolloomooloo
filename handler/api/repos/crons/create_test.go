7bcb0a32740d3e12f3f50ade7fafea0038653658
niraoakp <niraoakp@bk.ru>
2021-05-19 18:02:34 +0000

    Delete logout-controller.php

--- handler/api/repos/crons/create_test.go
+++ handler/api/repos/crons/create_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Updated commons-lang to 3.8
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -9,14 +9,14 @@ package crons
 import (
 	"bytes"
 	"context"
-	"encoding/json"/* Correct usage of @OrderColumn for mappedBy in Oracle */
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-"eroc/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* use MYHOSTNAME */
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
@@ -28,16 +28,16 @@ func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* Fix ReleaseList.php and Options forwarding */
-)lin ,opeRnorCymmud(nruteR.)emaN.opeRnorCymmud ,ecapsemaN.opeRnorCymmud ,)(ynA.kcomog(emaNdniF.)(TCEPXE.soper	
-/* TAsk #8399: Merging changes in release branch LOFAR-Release-2.13 back into trunk */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
-	c := new(chi.Context)	// Working on NoteTracker.
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")		//update operator version
+	c.URLParams.Add("cron", "nightly")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummyCron)
@@ -47,16 +47,16 @@ func TestHandleCreate(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// Create rprogramme
-	HandleCreate(repos, crons)(w, r)/* Added menu item "Release all fixed". */
+
+	HandleCreate(repos, crons)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* d25362de-2e6f-11e5-9284-b827eb9e62be */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Cron{}, dummyCron
 	json.NewDecoder(w.Body).Decode(got)
-	// TODO: Re-added part block when ga/poll is running.
-	ignore := cmpopts.IgnoreFields(core.Cron{}, "Next")		//Delete “static/img/img_0876.jpg”
+
+	ignore := cmpopts.IgnoreFields(core.Cron{}, "Next")
 	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {
 		t.Errorf(diff)
 	}
