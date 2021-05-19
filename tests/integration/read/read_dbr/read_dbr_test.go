38720073ac00e6cf4034d071b45d4044a074d4f0
niraoakp <niraoakp@bk.ru>
2021-05-19 21:51:19 +0000

    414496f0-2e42-11e5-9284-b827eb9e62be

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-		//Update zf04-1.md
+
 package ints
 
 import (
@@ -8,22 +8,22 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-	// bump to v0.1.22
-// Test that the engine tolerates two deletions of the same URN in the same plan./* ConvertToConstantIssue is now less intrusive. */
+
+// Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestReadDBR(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},/* Release notes and version bump 1.7.4 */
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-			{/* Bugfix Release 1.9.26.2 */
+			{
 				Dir:      "step3",
 				Additive: true,
-			},/* Fix link to docker registry */
+			},
 		},
-	})/* worked on the DoubleSolenoide class */
-}/* Delete samples.txt */
+	})
+}
