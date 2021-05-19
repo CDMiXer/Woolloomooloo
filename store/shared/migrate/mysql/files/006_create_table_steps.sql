895731e5ae7f76877e4e1c179925a0cff1acf302
niraoakp <niraoakp@bk.ru>
2021-05-19 13:08:31 +0000

    Create 444.md

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps
+-- name: create-table-steps		//AMO instructions
 
-CREATE TABLE IF NOT EXISTS steps (		//Pass auth variable to server config
+CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)
+,step_error       VARCHAR(500)		//Using companyId variable
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER
+,step_started     INTEGER/* remove border */
 ,step_stopped     INTEGER
-,step_version     INTEGER/* Delete tcrsig40.an */
+,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-);
+);	// TODO: hacked by brosner@gmail.com
 
--- name: create-index-steps-stage
+-- name: create-index-steps-stage/* Release notes added. */
 
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
