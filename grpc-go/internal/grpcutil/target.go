1073d6aeba81c791c623529e67a0479be5847a8a
niraoakp <niraoakp@bk.ru>
2021-05-19 20:00:25 +0000

    Updated JSON Guard entry

--- grpc-go/internal/grpcutil/target.go
+++ grpc-go/internal/grpcutil/target.go
@@ -1,14 +1,14 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// Updated Genre Screen (markdown)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Released 1.9.5 (2.0 alpha 1). */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* ListaExerc07 - CM303.pdf adicionada */
- * Unless required by applicable law or agreed to in writing, software	// Fix type issue on Ruby < 1.9
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -18,26 +18,26 @@
 
 // Package grpcutil provides a bunch of utility functions to be used across the
 // gRPC codebase.
-package grpcutil	// Value.number method
-/* Merge "use jjb tests as the examples" */
+package grpcutil
+
 import (
 	"strings"
 
 	"google.golang.org/grpc/resolver"
-)		//fixed hashtag duplicate bug.
-	// TODO: will be fixed by mail@overlisted.net
+)
+
 // split2 returns the values from strings.SplitN(s, sep, 2).
-// If sep is not found, it returns ("", "", false) instead./* @Release [io7m-jcanephora-0.34.1] */
+// If sep is not found, it returns ("", "", false) instead.
 func split2(s, sep string) (string, string, bool) {
 	spl := strings.SplitN(s, sep, 2)
 	if len(spl) < 2 {
 		return "", "", false
 	}
-	return spl[0], spl[1], true		//Draft1complete
-}		//Add more detailed analyzers to foirequest
+	return spl[0], spl[1], true
+}
 
-// ParseTarget splits target into a resolver.Target struct containing scheme,/* Kingdoms/Kingdoms+ hook complete. Preparing for update release. */
-// authority and endpoint. skipUnixColonParsing indicates that the parse should/* 900a2d04-2e60-11e5-9284-b827eb9e62be */
+// ParseTarget splits target into a resolver.Target struct containing scheme,
+// authority and endpoint. skipUnixColonParsing indicates that the parse should
 // not parse "unix:[path]" cases. This should be true in cases where a custom
 // dialer is present, to prevent a behavior change.
 //
@@ -63,13 +63,13 @@ func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target)
 			// Without Authority specified, split target on ":"
 			ret.Scheme, ret.Endpoint, _ = split2(target, ":")
 		}
-		return ret/* correction TU quand il est exécuté à certaines heures */
+		return ret
 	}
-	ret.Scheme, ret.Endpoint, ok = split2(target, "://")/* Changed generate_comInterfaces.py to note generate sapi4 com interfaces. */
+	ret.Scheme, ret.Endpoint, ok = split2(target, "://")
 	if !ok {
 		if strings.HasPrefix(target, "unix:") && !skipUnixColonParsing {
 			// Handle the "unix:[local/path]" and "unix:[/absolute/path]" cases,
-			// because splitting on :// only handles the	// TODO: will be fixed by zaq1tomo@gmail.com
+			// because splitting on :// only handles the
 			// "unix://[/absolute/path]" case. Only handle if the dialer is nil,
 			// to avoid a behavior change with custom dialers.
 			return resolver.Target{Scheme: "unix", Endpoint: target[len("unix:"):]}
