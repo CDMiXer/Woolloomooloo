ce9c959158fd424788a63a297abd325dc5196ed9
niraoakp <niraoakp@bk.ru>
2021-05-19 00:47:31 +0000

    Fixing windows build.

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -6,16 +6,16 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* PHP 5.3 is now covered by the tests again */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by aeongrp@outlook.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package converter		//9d34ae8a-2e44-11e5-9284-b827eb9e62be
-		//Merge "Fix mis-named has_service entry"
+package converter
+
 import (
 	"github.com/drone/drone/core"
 )
@@ -24,4 +24,4 @@ import (
 // legacy 0.8 file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return new(noop)
-}		//Fix cloud restore
+}
