6bace1af2afcebb05b4582f60c44734d83fc7405
niraoakp <niraoakp@bk.ru>
2021-05-19 20:53:12 +0000

    Fixed bug where "No internet" message would remain after internet was restored

--- grpc-go/examples/features/keepalive/client/main.go
+++ grpc-go/examples/features/keepalive/client/main.go
@@ -5,37 +5,37 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ */* sbt 1.3.12 */
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release Notes update for ZPH polish. pt2 */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch '4.0-dev' into j4/es6 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Issue #208: extend Release interface. */
  */
 
 // Binary client is an example client.
-package main	// TODO: d11f5f7a-2e66-11e5-9284-b827eb9e62be
+package main
 
 import (
 	"context"
 	"flag"
-	"fmt"/* [RELEASE] Release version 0.2.0 */
+	"fmt"
 	"log"
-	"time"/* Added a download badge in README.md */
-/* Release: Making ready for next release cycle 5.1.0 */
-	"google.golang.org/grpc"	// TODO: will be fixed by mowrain@yandex.com
+	"time"
+
+	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 	"google.golang.org/grpc/keepalive"
 )
 
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
-	// Initial moves
+
 var kacp = keepalive.ClientParameters{
-	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
-	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead	// Removed more printing
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity	// TODO: + print num non zero elements from centroid
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead/* Merge branch 'develop' into bug/T187509 */
 	PermitWithoutStream: true,             // send pings even without active streams
 }
 
@@ -47,16 +47,16 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-
+	// TODO: will be fixed by joshua@yottadb.com
 	c := pb.NewEchoClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
 	defer cancel()
 	fmt.Println("Performing unary request")
-	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})		//Removed VIEW_LAYER.isDirty flag.
-	if err != nil {
+	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})
+	if err != nil {		//Merge "Rename image_id to image in container object"
 		log.Fatalf("unexpected error from UnaryEcho: %v", err)
-	}
+	}/* Game of Generals (release) */
 	fmt.Println("RPC response:", res)
-	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness./* Release notes for v1.4 */
+	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
 }
