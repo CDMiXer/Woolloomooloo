7467b25f1e7a52702cc53a754a536cc423983cc3
niraoakp <niraoakp@bk.ru>
2021-05-19 10:01:31 +0000

    Added Android In-App Billing v3 license

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* maz kozitas */
 
-import (
+import (	// TODO: will be fixed by xiemengjun@gmail.com
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+/* Release notes 8.2.3 */
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
 }
 
-func main() {/* Release TomcatBoot-0.4.0 */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}
+		fooComponent := &FooComponent{}/* Release 1.1.9 */
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
 	})
-}
+}/* 577c790c-5216-11e5-b704-6c40088e03e4 */
