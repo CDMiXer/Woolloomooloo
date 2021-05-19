78b5161a46c395b151283a46bad95ffc9a907607
niraoakp <niraoakp@bk.ru>
2021-05-19 17:29:43 +0000

    Update Pcl.md

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -1,18 +1,18 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-		//Added a patternlab watch task
-using System.Threading.Tasks;/* Release v0.3.3.2 */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* corrected casing on GitHub */
+
+using System.Threading.Tasks;
 using Pulumi;
-	// Added buffer pool-related INFORMATION_SCHEMA table queries to PS-5.6 grammar
-class Resource : ComponentResource
-{	// Merge "Expose passthrough configuration in overcloud."
+
+class Resource : ComponentResource	// TODO: hacked by mikeal.rogers@gmail.com
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}
+}		//Exportación a HTML completada
 
 // Scenario #5 - composing #1 and #3
-class ComponentFive : ComponentResource
+class ComponentFive : ComponentResource/* Release 0.5.0-alpha3 */
 {
     private Resource resource;
 
@@ -24,18 +24,18 @@ class ComponentFive : ComponentResource
             Parent = this,
             Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
         });
-}    
+    }	// TODO: hacked by mikeal.rogers@gmail.com
 }
 
-class Program/* Last update of readme. I hope so. */
-{	// TODO: isShvMkjc3yvA0EMlbUvtPYDm2s0xzhN
-    static Task<int> Main(string[] args)/* Add Observer Pattern Demo */
+class Program		//port of Hubzillas code highlight feature
+{
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {	// TODO: Gave quick UI to table export and uncommented dropping of tables
-            var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions		//Merge branch 'master' into add-fran-mowinckel
-            {
-                Aliases = { new Alias { Name = "comp5" } },		//Change colorsheme
+        {		//Merge "FloatableElement: Replace superfluous class with general one"
+            var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
+            {/* security fix: found by @hamb and his friend */
+                Aliases = { new Alias { Name = "comp5" } },
             });
         });
     }
