fedc79be10042c3e3030f9b4f8140d64bb96eb4a
niraoakp <niraoakp@bk.ru>
2021-05-19 09:39:44 +0000

    APD-267: favorites page structure

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* e336b4d0-2e46-11e5-9284-b827eb9e62be */
 // +build !oss
 
 package secrets
 
 import (
-	"encoding/json"
+	"encoding/json"		//Update fetch_departamento.php
 	"net/http"
-	"net/http/httptest"
-	"testing"
-
-	"github.com/drone/drone/core"
+	"net/http/httptest"/* Only build DVB/ATSC plugins when building for DVB/ATSC. */
+	"testing"	// PROBCORE-821 styling to make it look prettier
+		//Indentation overflow correction
+	"github.com/drone/drone/core"	// TODO: 885ab70c-2e3f-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)/* Release 0.4--validateAndThrow(). */
 
 func TestHandleAll(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -36,7 +36,7 @@ func TestHandleAll(t *testing.T) {
 	}
 
 	got, want := []*core.Secret{}, dummySecretListScrubbed
-	json.NewDecoder(w.Body).Decode(&got)
+	json.NewDecoder(w.Body).Decode(&got)/* Merge "Release 4.0.10.46 QCACLD WLAN Driver" */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
@@ -57,7 +57,7 @@ func TestHandleAll_SecretListErr(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound
+	got, want := new(errors.Error), errors.ErrNotFound/* CHANGE: Asset Improvements */
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
