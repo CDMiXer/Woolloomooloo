a8d50d5b04f70f6dd59092f2d5418fb856b7dadb
niraoakp <niraoakp@bk.ru>
2021-05-19 19:27:55 +0000

    bumping travis to 1.11

--- store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron	// TODO: will be fixed by igor@soramitsu.co.jp
-	// TODO: Создали первый файл в GitHub
+-- name: alter-table-builds-add-column-cron
+	// 5dc14244-2e72-11e5-9284-b827eb9e62be
 ALTER TABLE builds ADD COLUMN build_cron TEXT NOT NULL DEFAULT '';
