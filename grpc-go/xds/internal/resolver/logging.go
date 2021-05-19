d63b51545d4d0b170ef70a1e82853b88172ed8e3
niraoakp <niraoakp@bk.ru>
2021-05-19 14:18:13 +0000

    Merge "Add user privacy documentation"

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,15 +1,15 @@
-/*
- *		//8f648746-2e74-11e5-9284-b827eb9e62be
- * Copyright 2020 gRPC authors.	// TODO: hacked by peterke@gmail.com
+/*/* Stop publishing */
+ */* Release 2.2.9 description */
+ * Copyright 2020 gRPC authors./* Release gubbins for Pathogen */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Release of eeacms/www-devel:18.3.14 */
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* drop_duplicates and entropy (#6) */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* bump django to 1.5 (compat with heroku) */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -26,8 +26,8 @@ import (
 )
 
 const prefix = "[xds-resolver %p] "
-
-var logger = grpclog.Component("xds")/* lingering env */
+/* fix for issue 704: Choose Target Player Highlight. */
+var logger = grpclog.Component("xds")
 
 func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
