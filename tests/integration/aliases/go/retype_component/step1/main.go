f3ed9f8cd9c2c649528f7aa3b9bebff4b5ae8eef
niraoakp <niraoakp@bk.ru>
2021-05-19 01:54:21 +0000

    VersionChecker Commit

--- tests/integration/aliases/go/retype_component/step1/main.go
+++ tests/integration/aliases/go/retype_component/step1/main.go
@@ -1,30 +1,30 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Re #26534 Release notes */
+
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)	// TODO: Execute Travis on 7.0
+)
 
 type FooResource struct {
-	pulumi.ResourceState		//added Open source section
-}
-
-type FooComponent struct {/* Added go test to wercker */
 	pulumi.ResourceState
 }
 
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {/* Release v1.15 */
-	fooRes := &FooResource{}/* Update MW_Launcher_0_7_1_RC1_Linux.sh */
+type FooComponent struct {
+	pulumi.ResourceState
+}
+
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
+	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {/* Release 2.1.0. */
+	if err != nil {
 		return nil, err
 	}
 	return fooRes, nil
 }
 
 // Scenario #4 - change the type of a component
-func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {		//Update share01-persistent-volume.yaml
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent44", name, fooComp, opts...)
 	if err != nil {
@@ -34,17 +34,17 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
-	}/* 07565b1c-2e52-11e5-9284-b827eb9e62be */
-	return fooComp, nil		//markdown improvements
-}/* Load home page content from Contentful */
+	}
+	return fooComp, nil
+}
 
-func main() {/* Rename vi-VN.yml to vi-vn.yml */
-	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: move dns.* to unmaintained
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp4")
-		if err != nil {	// TODO: hacked by xiemengjun@gmail.com
+		if err != nil {
 			return err
 		}
-/* Add gitlab badge */
-		return nil/* Fix code block in ReleaseNotes.md */
+
+		return nil
 	})
 }
