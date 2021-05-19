72b4f8f27682f447db274443502f7ff3aa704738
niraoakp <niraoakp@bk.ru>
2021-05-19 05:24:04 +0000

    Scaffold files from yo hedley.

--- tests/integration/get_resource/go/main.go
+++ tests/integration/get_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"reflect"
+	"reflect"/* 7deab0b0-2e71-11e5-9284-b827eb9e62be */
 
-	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
+	"github.com/pulumi/pulumi-random/sdk/v2/go/random"/* renamed smartapp */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type MyResource struct {
 	pulumi.ResourceState
-
+/* #308 - Release version 0.17.0.RELEASE. */
 	Length pulumi.IntOutput `pulumi:"length"`
 }
 
@@ -19,7 +19,7 @@ type MyResourceArgs struct{}
 func (MyResourceArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()
 }
-
+/* Create Release Date.txt */
 func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
 	var resource MyResource
 	err := ctx.RegisterResource("unused:unused:unused", "unused", &MyResourceArgs{}, &resource,
@@ -30,7 +30,7 @@ func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
 	return &resource, nil
 }
 
-func main() {
+func main() {/* Remove paths, start adding types */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		pet, err := random.NewRandomPet(ctx, "cat", &random.RandomPetArgs{
