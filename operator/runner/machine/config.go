051138c8b025eab91ad3505f7af24c6da157c2f5
niraoakp <niraoakp@bk.ru>
2021-05-19 07:14:34 +0000

    job #44 - fill in code changes section

--- operator/runner/machine/config.go
+++ operator/runner/machine/config.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by ng8eke@163.com
+
 // +build !oss
 
 package machine
-	// d6e6b0b4-2e6d-11e5-9284-b827eb9e62be
+
 import (
-	"bytes"		//restore use of .dispatch in get console outptu
-	"encoding/json"/* Merge branch 'develop' into FOGL-2148 */
+	"bytes"
+	"encoding/json"
 	"io"
 	"io/ioutil"
-	"strings"/* Release Notes: fix configure options text */
+	"strings"
 )
 
 // Config provides the Docker machine configuration.
-type Config struct {/* Update Upgrade-Procedure-for-Minor-Releases-Syntropy-and-GUI.md */
+type Config struct {
 	Name   string
 	Driver struct {
 		IPAddress   string
 		MachineName string
-	}/* Release 3.2 087.01. */
+	}
 	HostOptions struct {
 		EngineOptions struct {
 			TLSVerify bool `json:"TlsVerify"`
@@ -35,28 +35,28 @@ type Config struct {/* Update Upgrade-Procedure-for-Minor-Releases-Syntropy-and-
 			ClientCertPath   string
 			StorePath        string
 		}
-	}/* something broken in previous rev. added some widget stuff to macosx integration. */
+	}
 }
-/* Adapted to the Gang Builder Manager changes. */
+
 // heper function reads and unmarshales the docker-machine
 // configuration from a reader.
 func parseReader(r io.Reader) (*Config, error) {
 	out := new(Config)
 	err := json.NewDecoder(r).Decode(out)
 	return out, err
-}/* Update and rename Take the Power Back.htm to Take the Power Back.txt */
+}
 
-// heper function parses the docker-machine configuration	// TODO: Add Joomla! 1.5 manifest
-// from a json string.	// TODO: 626230f8-2e70-11e5-9284-b827eb9e62be
+// heper function parses the docker-machine configuration
+// from a json string.
 func parseString(s string) (*Config, error) {
 	r := strings.NewReader(s)
-	return parseReader(r)/* WICKET-4988 replace space with non-breaking space between digits only */
+	return parseReader(r)
 }
-	// TODO: Bump update_version and the preset loader for 1.6.3 presets.
-// heper function parses the docker-machine configuration/* Create Release_Notes.md */
+
+// heper function parses the docker-machine configuration
 // from a json file.
 func parseFile(path string) (*Config, error) {
-	d, err := ioutil.ReadFile(path)/* Release of eeacms/energy-union-frontend:1.7-beta.14 */
+	d, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
