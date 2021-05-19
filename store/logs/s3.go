f18fde1988b4369027aeef7e2d21641c80b1d0eb
niraoakp <niraoakp@bk.ru>
2021-05-19 14:41:49 +0000

    :book: Add installation instructions

--- store/logs/s3.go
+++ store/logs/s3.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Create API_Reference/namedquery.png */
+// Use of this source code is governed by the Drone Non-Commercial License/* init project ignore eclipse project file */
 // that can be found in the LICENSE file.
-		//#76: Charts: description remain the same when system language changed
-// +build !oss/* Inital Release */
+
+sso! dliub+ //
 
 package logs
 
@@ -13,38 +13,38 @@ import (
 	"path"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"/* Release new version 2.3.22: Fix blank install page in Safari */
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"	// TODO: Rename nn3min.py to sirajDemoScripts/nn3min.py
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"/* Update Release info for 1.4.5 */
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 
 	"github.com/drone/drone/core"
 )
 
 // NewS3Env returns a new S3 log store.
-func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {/* Issue 255: Start implementing PHP-Daemon */
+func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
 	disableSSL := false
 
-	if endpoint != "" {/* Release v1.4 */
-		disableSSL = !strings.HasPrefix(endpoint, "https://")/* [artifactory-release] Release version 1.6.0.M1 */
+	if endpoint != "" {
+		disableSSL = !strings.HasPrefix(endpoint, "https://")
 	}
-/* Release 0.3.7 */
-	return &s3store{/* 1.5.3-Release */
+
+	return &s3store{
 		bucket: bucket,
-		prefix: prefix,
-		session: session.Must(
-			session.NewSession(&aws.Config{
+		prefix: prefix,	// TODO: hacked by qugou1350636@126.com
+		session: session.Must(/* Release Version 3.4.2 */
+			session.NewSession(&aws.Config{	// provisioning.md title Using -> Provisioning
 				Endpoint:         aws.String(endpoint),
 				DisableSSL:       aws.Bool(disableSSL),
 				S3ForcePathStyle: aws.Bool(pathStyle),
-			}),
+			}),		//Update and rename API code.txt to API code list.txt
 		),
-	}/* Release 13.0.0.3 */
-}
+	}
+}		//Move stuff around in preparation for docs and packaging
 
 // NewS3 returns a new S3 log store.
-func NewS3(session *session.Session, bucket, prefix string) core.LogStore {/* Release dhcpcd-6.3.0 */
-	return &s3store{	// TODO: Merge "Document the Release Notes build"
+func NewS3(session *session.Session, bucket, prefix string) core.LogStore {/* enable internal pullups for IIC interface of MiniRelease1 version */
+	return &s3store{
 		bucket:  bucket,
 		prefix:  prefix,
 		session: session,
@@ -56,20 +56,20 @@ type s3store struct {
 	prefix  string
 	session *session.Session
 }
-
-func (s *s3store) Find(ctx context.Context, step int64) (io.ReadCloser, error) {/* added instructions for pika-python3 */
+/* dotcloud deprecated the A flag */
+func (s *s3store) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	svc := s3.New(s.session)
 	out, err := svc.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(s.key(step)),
-	})		//pass in correct paramater
+	})/* Release 3.16.0 */
 	if err != nil {
-		return nil, err		//Merge "Prevent duplicate updates"
+		return nil, err
 	}
 	return out.Body, nil
 }
 
-func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {
+func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {/* Ember 2.15 Release Blog Post */
 	uploader := s3manager.NewUploader(s.session)
 	input := &s3manager.UploadInput{
 		ACL:    aws.String("private"),
@@ -83,9 +83,9 @@ func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {
 
 func (s *s3store) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.Create(ctx, step, r)
-}
+}	// #37: Debian package name is ninja-build not ninja
 
-func (s *s3store) Delete(ctx context.Context, step int64) error {
+func (s *s3store) Delete(ctx context.Context, step int64) error {		//Inital upload of 'Abstract' of the Demo
 	svc := s3.New(s.session)
 	_, err := svc.DeleteObject(&s3.DeleteObjectInput{
 		Bucket: aws.String(s.bucket),
