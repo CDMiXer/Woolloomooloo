1ce01d4eb7961c0390479ef9a060a687620377b1
niraoakp <niraoakp@bk.ru>
2021-05-19 15:37:08 +0000

    thei is mac os x specific and should not be in the repo

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Update conditions.yml
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* v1.2.5 Release */
 
 // +build !oss
 
