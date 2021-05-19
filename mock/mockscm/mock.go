dd1a81f7a845165c57828aae5127502f0cb11f23
niraoakp <niraoakp@bk.ru>
2021-05-19 12:16:01 +0000

    Updating GBP from PR #57177 [ci skip]

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Make gradation curve resizeable
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+/* Release notes updated */
+// +build !oss	// TODO: ANother tracks.
+/* Release of eeacms/forests-frontend:1.6.4.4 */
+package mockscm		//Documenting the Ethernet dissector interfaces
 
-// +build !oss
-		//caculate signature with accesskey
-package mockscm
-
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService/* 5.3.5 Release */
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
