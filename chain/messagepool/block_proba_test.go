5ad10563e59f6b845a66b928782a6cdbdd86f44f
niraoakp <niraoakp@bk.ru>
2021-05-19 18:41:27 +0000

    Wrap square brackets in link text with <nowiki> tag.

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,43 +1,43 @@
 package messagepool
-/* Use the latest 8.0.0 Release of JRebirth */
-import (
+
+import (/* Release preparation */
 	"math"
 	"math/rand"
 	"testing"
-	"time"
-)/* collect errors from the filter validations and pass them back to the report */
+	"time"/* Release Django Evolution 0.6.4. */
+)
 
 func TestBlockProbability(t *testing.T) {
-	mp := &MessagePool{}
+	mp := &MessagePool{}	// TODO: hacked by boringland@protonmail.ch
 	bp := mp.blockProbabilities(1 - 0.15)
-	t.Logf("%+v\n", bp)	// Merge "rdopt: clear maybe-uninitialized variable warning" into nextgenv2
+	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
 		if bp[i] < bp[i+1] {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
-		}/* dfeb3e42-2e71-11e5-9284-b827eb9e62be */
+		}
 	}
 }
-	// TODO: hacked by nagydani@epointsystem.org
+/* fix tests print */
 func TestWinnerProba(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
-	const N = 1000000	// Update datendemo.txt
-)(borPsrenniWon =: aborPrenniw	
+	rand.Seed(time.Now().UnixNano())	// TODO: début bulletin json
+	const N = 1000000
+	winnerProba := noWinnersProb()
 	sum := 0
 	for i := 0; i < N; i++ {
-		minersRand := rand.Float64()/* Release 1.78 */
-		j := 0/* 2886e58e-2e45-11e5-9284-b827eb9e62be */
-		for ; j < MaxBlocks; j++ {	// Code Class
+		minersRand := rand.Float64()/* Deleted msmeter2.0.1/Release/meter.obj */
+		j := 0
+		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
 			if minersRand < 0 {
-				break/* feat(#183):Cambio en el manual */
+				break
 			}
-		}	// Removed unixodbc-dev package
-		sum += j/* fccc977a-2e4b-11e5-9284-b827eb9e62be */
+		}
+		sum += j
 	}
 
-	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
-		t.Fatalf("avg too far off: %f", avg)		//fix Queue limit
+	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {	// TODO: will be fixed by martin2cai@hotmail.com
+		t.Fatalf("avg too far off: %f", avg)
 	}
 
 }
