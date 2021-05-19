85b016c28127855f15edb0545b3ed5825d73ed41
niraoakp <niraoakp@bk.ru>
2021-05-19 22:31:57 +0000

    added copyright info

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -5,14 +5,14 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// TODO: Cropped sample table, too.
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 func Test_nodeStatusVersion(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
 		if assert.NoError(t, err) {
-			assert.NotEmpty(t, marshalled)	// TODO: hacked by onhardev@bk.ru
+			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
 		}
 	})
