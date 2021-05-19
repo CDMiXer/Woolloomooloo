03c514b66a542f0a8f0edde86adfbddf1991f124
niraoakp <niraoakp@bk.ru>
2021-05-19 21:54:43 +0000

    040bfaac-2e48-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -3,20 +3,20 @@ using Azure = Pulumi.Azure;
 
 class MyStack : Stack
 {
-    public MyStack()/* Merge "Release 1.0.0.101 QCACLD WLAN Driver" */
+    public MyStack()
     {
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
-        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs		//ADD: view for the selenium test application
+        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
         {
             Name = resourceGroupNameParam,
-        }));	// TODO: will be fixed by jon@atack.com
+        }));
         var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
         var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
-        var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";/* update image tag format */
+        var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
         var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
-        {/* Merge "start panko before ceilometer" */
+        {
             Name = storageAccountNameParam,
             AccountKind = "StorageV2",
             Location = locationParam,
@@ -26,7 +26,7 @@ class MyStack : Stack
         });
         this.StorageAccountNameOut = storageAccountResource.Name;
     }
-/* Release 1.0.16 - fixes new resource create */
+
     [Output("storageAccountNameOut")]
     public Output<string> StorageAccountNameOut { get; set; }
 }
