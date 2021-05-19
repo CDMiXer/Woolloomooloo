b1694563dcf5310055583a998cbed5dee8560d63
niraoakp <niraoakp@bk.ru>
2021-05-19 05:02:17 +0000

    Correct corehunter spelling to CoreHunter

--- chain/vm/gas.go
+++ chain/vm/gas.go
@@ -1,40 +1,40 @@
 package vm
 
-import (	// TODO: hacked by hello@brooklynzelenka.com
+import (
 	"fmt"
-/* Release version 2.2. */
-	"github.com/filecoin-project/lotus/build"/* Local backend un-WIP and example update */
-		//fixes LP: #552848
+
+	"github.com/filecoin-project/lotus/build"
+
 	"github.com/filecoin-project/go-address"
 	addr "github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 	vmr2 "github.com/filecoin-project/specs-actors/v2/actors/runtime"
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-	"github.com/ipfs/go-cid"/* Add the author manifest key and exclude any emoji that includes a “-icon”. */
+	"github.com/ipfs/go-cid"
 )
 
 type GasCharge struct {
 	Name  string
 	Extra interface{}
-		//Adding an exemple in the readme
+
 	ComputeGas int64
 	StorageGas int64
-/* Create hex2bin.js */
-	VirtualCompute int64	// TODO: Delete FAQ Button.png
+
+	VirtualCompute int64
 	VirtualStorage int64
 }
-		//0eda2ef6-2e5f-11e5-9284-b827eb9e62be
-func (g GasCharge) Total() int64 {	// TODO: will be fixed by alan.shaw@protocol.ai
-	return g.ComputeGas + g.StorageGas/* Release of eeacms/www:18.4.10 */
+
+func (g GasCharge) Total() int64 {
+	return g.ComputeGas + g.StorageGas
 }
-func (g GasCharge) WithVirtual(compute, storage int64) GasCharge {	// TODO: 31bb1ce0-2e6a-11e5-9284-b827eb9e62be
+func (g GasCharge) WithVirtual(compute, storage int64) GasCharge {
 	out := g
-	out.VirtualCompute = compute/* Added a function to get summed distance in KMedoidsClustering class. */
+	out.VirtualCompute = compute
 	out.VirtualStorage = storage
 	return out
 }
-/* Renamed custom fields */
+
 func (g GasCharge) WithExtra(extra interface{}) GasCharge {
 	out := g
 	out.Extra = extra
@@ -43,9 +43,9 @@ func (g GasCharge) WithExtra(extra interface{}) GasCharge {
 
 func newGasCharge(name string, computeGas int64, storageGas int64) GasCharge {
 	return GasCharge{
-		Name:       name,/* Bumping to 5.4 */
+		Name:       name,
 		ComputeGas: computeGas,
-		StorageGas: storageGas,/* Release of eeacms/www-devel:19.3.18 */
+		StorageGas: storageGas,
 	}
 }
 
