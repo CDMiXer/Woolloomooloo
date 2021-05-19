a8235b095906c8fe83e97af98e920a6703f124e2
niraoakp <niraoakp@bk.ru>
2021-05-19 02:58:59 +0000

    Fixed bug: edge number was not set for inverse edge

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,30 +1,30 @@
 package store
 
 import (
-	"fmt"		//fixed launcher badge counting
-	"testing"/* Release: 0.0.4 */
-
+	"fmt"
+	"testing"
+	// TODO: And moomesa too ...
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/assert"
-)	// TODO: 4fc342c4-2e71-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/types"/* ab51d7be-2e4d-11e5-9284-b827eb9e62be */
+	"github.com/stretchr/testify/assert"	// TODO: Create community-process.rst
+)
 
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
 		basefee             uint64
-		limitUsed           int64	// TODO: Merge branch 'new-design' into nd/image-proxy
-		noOfBlocks          int/* Merge branch 'master' into add-minimal-dispatcher */
-		preSmoke, postSmoke uint64	// TODO: will be fixed by aeongrp@outlook.com
+		limitUsed           int64
+		noOfBlocks          int
+		preSmoke, postSmoke uint64
 	}{
-		{100e6, 0, 1, 87.5e6, 87.5e6},/* Merge "Add experimental Manila LVM job with minimal services" */
+		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
-		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},/* Merge branch 'Pre-Release(Testing)' into master */
+		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
 
-	for _, test := range tests {		//NetKAN updated mod - TRPHire-0.6.10.1
+	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
