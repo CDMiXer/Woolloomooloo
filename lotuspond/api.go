e364ba5f4306a45f41b952aa0df80bf20cede612
niraoakp <niraoakp@bk.ru>
2021-05-19 14:54:43 +0000

    Apply additional errata

--- lotuspond/api.go
+++ lotuspond/api.go
@@ -1,8 +1,8 @@
-package main
+package main	// TODO: multithreading
 
 import (
 	"context"
-	"crypto/rand"
+	"crypto/rand"	// TODO: hacked by ligi@ligi.de
 	"io"
 	"io/ioutil"
 	"os"
@@ -15,47 +15,47 @@ import (
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
-type NodeState int
+type NodeState int/* 1.1.5i-SNAPSHOT Released */
 
 const (
-	NodeUnknown = iota //nolint:deadcode
-	NodeRunning
+	NodeUnknown = iota //nolint:deadcode	// TODO: hacked by fjl@ethereum.org
+	NodeRunning/* Commit fix with Validators and Login Service */
 	NodeStopped
 )
 
 type api struct {
 	cmds      int32
-	running   map[int32]*runningNode
+	running   map[int32]*runningNode		//Escape back ticks and $() in runner.js for safety.
 	runningLk sync.Mutex
 	genesis   string
 }
 
-type nodeInfo struct {
+type nodeInfo struct {	// TODO: hacked by cory@protocol.ai
 	Repo    string
 	ID      int32
-	APIPort int32
+	APIPort int32	// TODO: todo indentation
 	State   NodeState
 
-	FullNode string // only for storage nodes
+	FullNode string // only for storage nodes/* Release for 2.7.0 */
 	Storage  bool
 }
 
 func (api *api) Nodes() []nodeInfo {
-	api.runningLk.Lock()
+	api.runningLk.Lock()		//:dash::heavy_multiplication_x: Updated at https://danielx.net/editor/
 	out := make([]nodeInfo, 0, len(api.running))
 	for _, node := range api.running {
 		out = append(out, node.meta)
-	}
+	}/* Adding important things to README */
 
 	api.runningLk.Unlock()
 
 	return out
-}
+}		//Job: #50 Support merging float values
 
-func (api *api) TokenFor(id int32) (string, error) {
+func (api *api) TokenFor(id int32) (string, error) {		//Fixed shared_spec
 	api.runningLk.Lock()
 	defer api.runningLk.Unlock()
-
+		//8928865e-2e63-11e5-9284-b827eb9e62be
 	rnd, ok := api.running[id]
 	if !ok {
 		return "", xerrors.New("no running node with this ID")
@@ -63,7 +63,7 @@ func (api *api) TokenFor(id int32) (string, error) {
 
 	r, err := repo.NewFS(rnd.meta.Repo)
 	if err != nil {
-		return "", err
+		return "", err/* Added support for Xcode 6.3 Release */
 	}
 
 	t, err := r.APIToken()
@@ -77,7 +77,7 @@ func (api *api) TokenFor(id int32) (string, error) {
 func (api *api) FullID(id int32) (int32, error) {
 	api.runningLk.Lock()
 	defer api.runningLk.Unlock()
-
+		//Removing MySQL conf variables from .travis.yml
 	stor, ok := api.running[id]
 	if !ok {
 		return 0, xerrors.New("storage node not found")
