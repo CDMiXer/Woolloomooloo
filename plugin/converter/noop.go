4021b320ec0e40c9a8fb7703408a3941239beb89
niraoakp <niraoakp@bk.ru>
2021-05-19 21:38:36 +0000

    Rename launcher.sh to launcher/launcher.sh

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -4,26 +4,26 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www:20.8.5 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// add start class
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Release v1.0.3. */
+
 package converter
 
 import (
-	"context"/* remove transient variable from auditable fields */
+	"context"
 
 	"github.com/drone/drone/core"
 )
-		//Add EURETH and EURLTC pairs to GDAX exchange file
+
 type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
 	return nil, nil
-}/* Release 1.0.21 */
+}
