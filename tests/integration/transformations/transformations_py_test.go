34989940b734ee8c5f843146ebfee6ed33039f8f
niraoakp <niraoakp@bk.ru>
2021-05-19 06:51:18 +0000

    Typo in the 5.0.0 changelog

--- tests/integration/transformations/transformations_py_test.go
+++ tests/integration/transformations/transformations_py_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build python all/* Added a lot of materials */
+// +build python all
 
-package ints/* History for vhost */
+package ints
 
-import (
+import (/* Release notes for 3.4. */
 	"path/filepath"
 	"testing"
-	// Merge branch 'master' into feature/upload
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 func TestPythonTransformations(t *testing.T) {
 	for _, dir := range Dirs {
-		d := filepath.Join("python", dir)
+		d := filepath.Join("python", dir)	// Change all main titles to h2
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: d,
-				Dependencies: []string{/* Form AdminCommentaryEdition & jsp */
+				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick:                  true,
 				ExtraRuntimeValidation: Validator("python"),
-			})/* Update and rename Gioco.md to Regole.md */
-		})		//Add new module System.GIO.File.FileEnumerator
-}	
+			})
+		})
+	}
 }
