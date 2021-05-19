fcea81f1361f2a99400eb3d9a33f8b90ad9f5a4f
niraoakp <niraoakp@bk.ru>
2021-05-19 05:07:47 +0000

    WIP on leader election

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          SERIAL PRIMARY KEY
+ cron_id          SERIAL PRIMARY KEY/* [skip ci] update yii2-codeception version */
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
-,cron_event       VARCHAR(50)/* Update symfony-security-roles-vs-voters.md */
+,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER	// neue plättchen dezenter hervorheben
+,cron_created     INTEGER/* Begin removing systems Hungarian notation from public fields.  */
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
-,UNIQUE(cron_repo_id, cron_name)/* remove top margin on delete button */
+,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
 
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
-		//Updated performance counter doc (closes #43)
--- name: create-index-cron-next
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);/* Relax version constraint for upcoming Flow 6 */
 
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);/* Adapt to recent changes to upstream request */
+-- name: create-index-cron-next
+/* Release 0.8.0~exp3 */
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
