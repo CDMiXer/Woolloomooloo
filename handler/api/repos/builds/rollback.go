a8a007b6fce2ad53da049bf288f56bd9f1e9407d
niraoakp <niraoakp@bk.ru>
2021-05-19 10:54:25 +0000

    made example much smaller

--- handler/api/repos/builds/rollback.go
+++ handler/api/repos/builds/rollback.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Update cipmankVychodni.child.js */
-// +build !oss
+
+// +build !oss/* Deleted CtrlApp_2.0.5/Release/mt.command.1.tlog */
 
 package builds
-	// TODO: will be fixed by why@ipfs.io
+
 import (
 	"net/http"
-	"strconv"		//Use an updated Google Sat URL.
+	"strconv"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Add Releases and Cutting version documentation back in. */
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* #42 add and refactor the interface of getAllExecutors */
-
+	"github.com/drone/drone/handler/api/request"
+/* Fix autoconf build in libclang since r197075, (has been reverted in r197111). */
 	"github.com/go-chi/chi"
 )
-	// TODO: Follow-up to [3505]: use plural `scripts` in HDF for consistency.
+
 // HandleRollback returns an http.HandlerFunc that processes http
-// requests to rollback and re-execute a build./* Issue #511 Implemented some tests for MkReleaseAsset */
-func HandleRollback(
+// requests to rollback and re-execute a build.
+func HandleRollback(		//add file to cons
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* added javadoc for doPress and doRelease pattern for momentary button */
+	builds core.BuildStore,
 	triggerer core.Triggerer,
-) http.HandlerFunc {/* s/ReleasePart/ReleaseStep/g */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (		//Fixing no response bug
-			environ   = r.FormValue("target")
+		var (
+			environ   = r.FormValue("target")		//Fjerna regler som ikke gjelder apertium
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")		//Create LZ77_Output.txt
 			user, _   = request.UserFrom(r.Context())
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)/* Release 174 */
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}/* #3 - Release version 1.0.1.RELEASE. */
+		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		prev, err := builds.FindNumber(r.Context(), repo.ID, number)/* [skia] optimize fill painter to not autoRelease SkiaPaint */
+		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Limit the number of blog posts listed on the main page */
+			return
 		}
 		if environ == "" {
 			render.BadRequestf(w, "Missing target environment")
-			return
-		}/* Delete unused Maia prometheus-config.yaml */
-	// TODO: Update angular-daterangepicker-plus.min.js
+			return	// TODO: hacked by boringland@protonmail.ch
+		}
+
 		hook := &core.Hook{
 			Parent:       prev.Number,
 			Trigger:      user.Login,
-			Event:        core.EventRollback,
+			Event:        core.EventRollback,		//Icon improved & Indentation fixed
 			Action:       prev.Action,
-			Link:         prev.Link,/* Released 1.1.3 */
-			Timestamp:    prev.Timestamp,
+			Link:         prev.Link,
+			Timestamp:    prev.Timestamp,/* Fixed Welcome :D */
 			Title:        prev.Title,
 			Message:      prev.Message,
 			Before:       prev.Before,
@@ -73,13 +73,13 @@ func HandleRollback(
 			Deployment:   environ,
 			Cron:         prev.Cron,
 			Sender:       prev.Sender,
-			Params:       map[string]string{},
+			Params:       map[string]string{},/* Delete all_offline.png */
 		}
 
 		for k, v := range prev.Params {
 			hook.Params[k] = v
 		}
-
+		//Various updates to Phaser, and 1.5.4 PIXI
 		for key, value := range r.URL.Query() {
 			if key == "access_token" {
 				continue
@@ -96,8 +96,8 @@ func HandleRollback(
 		result, err := triggerer.Trigger(r.Context(), repo, hook)
 		if err != nil {
 			render.InternalError(w, err)
-		} else {
+		} else {	// TODO: Extra emphasis on applying the decorator first.
 			render.JSON(w, result, 200)
 		}
-	}
+	}/* [api] support Set values in Options */
 }
