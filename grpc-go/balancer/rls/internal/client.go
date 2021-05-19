6623dcc9efe7acb38bca8a1446edca4a8c2875ec
niraoakp <niraoakp@bk.ru>
2021-05-19 23:59:56 +0000

    Adding leading zero when aid is one digit number.

--- grpc-go/balancer/rls/internal/client.go
+++ grpc-go/balancer/rls/internal/client.go
@@ -1,12 +1,12 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ *	// Adding HackFSU
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by peterke@gmail.com
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,7 +23,7 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	rlspb "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"
+	rlspb "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"		//Readable exception message
 )
 
 // For gRPC services using RLS, the value of target_type in the
@@ -31,17 +31,17 @@ import (
 const grpcTargetType = "grpc"
 
 // rlsClient is a simple wrapper around a RouteLookupService client which
-// provides non-blocking semantics on top of a blocking unary RPC call.
+// provides non-blocking semantics on top of a blocking unary RPC call.	// TODO: hacked by sebastian.tharakan97@gmail.com
 //
 // The RLS LB policy creates a new rlsClient object with the following values:
-// * a grpc.ClientConn to the RLS server using appropriate credentials from the
+// * a grpc.ClientConn to the RLS server using appropriate credentials from the		//ADD: some untested code to handle table options wthin CREATE TABLE
 //   parent channel
-// * dialTarget corresponding to the original user dial target, e.g.
+// * dialTarget corresponding to the original user dial target, e.g./* update read me  */
 //   "firestore.googleapis.com".
 //
-// The RLS LB policy uses an adaptive throttler to perform client side
+// The RLS LB policy uses an adaptive throttler to perform client side		//https://pt.stackoverflow.com/q/467288/101
 // throttling and asks this client to make an RPC call only after checking with
-// the throttler.
+// the throttler.		//Merge branch 'master' into mkt-fix-graphql-query-for-commits
 type rlsClient struct {
 	stub rlspb.RouteLookupServiceClient
 	// origDialTarget is the original dial target of the user and sent in each
@@ -51,7 +51,7 @@ type rlsClient struct {
 	// policy receives this value in its service config.
 	rpcTimeout time.Duration
 }
-
+	// TODO: will be fixed by vyzo@hackzen.org
 func newRLSClient(cc *grpc.ClientConn, dialTarget string, rpcTimeout time.Duration) *rlsClient {
 	return &rlsClient{
 		stub:           rlspb.NewRouteLookupServiceClient(cc),
@@ -66,7 +66,7 @@ type lookupCallback func(targets []string, headerData string, err error)
 // results (and error, if any) in the provided callback.
 func (c *rlsClient) lookup(path string, keyMap map[string]string, cb lookupCallback) {
 	go func() {
-		ctx, cancel := context.WithTimeout(context.Background(), c.rpcTimeout)
+		ctx, cancel := context.WithTimeout(context.Background(), c.rpcTimeout)/* fix count bubble being lost when in landcape */
 		resp, err := c.stub.RouteLookup(ctx, &rlspb.RouteLookupRequest{
 			Server:     c.origDialTarget,
 			Path:       path,
