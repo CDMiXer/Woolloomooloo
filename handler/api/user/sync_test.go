bfb83f285f3b4fa4f52efc40d5bc5f075bc473f2
niraoakp <niraoakp@bk.ru>
2021-05-19 17:55:24 +0000

    ControlInputProcessor: Make leave the game when press Start and Select

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
-// Use of this source code is governed by the Drone Non-Commercial License	// Merge branch 'master' into require_time
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release of eeacms/www-devel:20.11.25 */
+
 package user
