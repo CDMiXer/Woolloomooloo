083902ad5c5f3f90e0f59214bfd85ea13a74896b
niraoakp <niraoakp@bk.ru>
2021-05-19 03:52:49 +0000

    Tidied up the fallback widgetset, ie removed it, still working

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -6,20 +6,20 @@ import (
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
-/* Fix chat sync issues */
+
 type ScoreKeeper struct {
 	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
-func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {		//[maven-release-plugin]  copy for tag jetty-project-7.0.0.1beta1
+func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
-	sk.scores = scores/* Changed REV11 description */
+	sk.scores = scores
 	sk.lk.Unlock()
 }
 
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {/* Update and rename novacoin-qt.install to doubloon-qt.install */
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
-	defer sk.lk.Unlock()		//Imported Debian patch 1.7.5-1ubuntu1
+	defer sk.lk.Unlock()
 	return sk.scores
 }
