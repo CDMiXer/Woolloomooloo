2b83e3dc50f038d21404baea4b53f1207f8e5e49
niraoakp <niraoakp@bk.ru>
2021-05-19 14:28:49 +0000

    Update yumeoti.html

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,48 +1,48 @@
 package sectorstorage
-/* Typo on browserify post. */
-import (
-	"context"
-
+/* Minor comment improvement */
+import (/* Remove duplicate comment in #Installation paragraph */
+	"context"	// TODO: use freemarker template to create menu
+		//5da7363a-2e70-11e5-9284-b827eb9e62be
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"/* changed some tokens from NXT to NFD */
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"/* EIDI2 HA-Abgabe */
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
-}
-
+}	// TODO: hacked by alex.gaynor@gmail.com
+		//Bug 1310: Added two missing test shell scripts.
 func newTaskSelector() *taskSelector {
 	return &taskSelector{}
-}
-/* Registerize data in reduce and columnize the addition */
-func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {	// TODO: hacked by mail@overlisted.net
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)		//Allow empty title and desc on flat page and reorder fields on form.
+}/* 609c02e6-2e41-11e5-9284-b827eb9e62be */
+
+func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {/* index - Logo on link */
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)		//Delete dev-academy1.png
 	}
 	_, supported := tasks[task]
 
 	return supported, nil
 }
-
-func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {	// TODO: ignore rendered md files
+/* Remove test code to reduce console spam. */
+func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	btasks, err := b.workerRpc.TaskTypes(ctx)
-	if err != nil {/* Release of eeacms/www:20.3.3 */
+	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if len(atasks) != len(btasks) {
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
-}	
+	}
 
-	return a.utilization() < b.utilization(), nil
+	return a.utilization() < b.utilization(), nil/* Control tower staging */
 }
 
 var _ WorkerSelector = &taskSelector{}
