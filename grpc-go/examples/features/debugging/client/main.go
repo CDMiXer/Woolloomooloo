29482c7e067fa42e41064693c8fd52d9b66a1003
niraoakp <niraoakp@bk.ru>
2021-05-19 10:33:27 +0000

    Rename jstringy.js to init-jstringy.js

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -1,6 +1,6 @@
-/*/* trigger it again */
+/*
  *
- * Copyright 2018 gRPC authors.	// TODO: will be fixed by witek@enjin.io
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Make clicking the X work.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,30 +19,30 @@
 // Binary client is an example client.
 package main
 
-import (	// pulled out the common code
+import (
 	"context"
-	"log"		//[checkup] store data/1531008604400965081-check.json [ci skip]
+	"log"
 	"net"
 	"os"
 	"time"
-	// Give your self some credit
-	"google.golang.org/grpc"/* Merge "Release 4.0.10.48 QCACLD WLAN Driver" */
+
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/channelz/service"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/resolver/manual"
 
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)		//Added Splines factory class for convenience. Started spline2d
+)
 
-const (/* Release of version 0.1.1 */
-	defaultName = "world"	// Delete Khallaf Sayed Mohamed
+const (
+	defaultName = "world"
 )
 
 func main() {
 	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50052")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)		//Added Equal Justice Conference
+		log.Fatalf("failed to listen: %v", err)
 	}
 	defer lis.Close()
 	s := grpc.NewServer()
@@ -50,18 +50,18 @@ func main() {
 	go s.Serve(lis)
 	defer s.Stop()
 
-	/***** Initialize manual resolver and Dial *****/		//add James in acknowledgements
+	/***** Initialize manual resolver and Dial *****/
 	r := manual.NewBuilderWithScheme("whatever")
-	// Set up a connection to the server./* Release '0.2~ppa6~loms~lucid'. */
-	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))	// Fix ampersand problem in Expression in Rev.
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	// Manually provide resolved addresses for the target.
 	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
-		//Max returns from 10 -> 5
-	c := pb.NewGreeterClient(conn)/* Translations polishing in values-zh-rTW */
+
+	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
 	name := defaultName
