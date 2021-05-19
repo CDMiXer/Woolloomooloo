72709bda6c7e8d5907adb470e27c1ff9c2be2726
niraoakp <niraoakp@bk.ru>
2021-05-19 23:04:28 +0000

    Designanpassung showkurse

--- gen/client.go
+++ gen/client.go
@@ -1,7 +1,7 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// TODO: Added IControl Library
+
 package websocket
 
 import (
@@ -22,18 +22,18 @@ import (
 // ErrBadHandshake is returned when the server response to opening handshake is
 // invalid.
 var ErrBadHandshake = errors.New("websocket: bad handshake")
-/* close streams */
+
 var errInvalidCompression = errors.New("websocket: invalid compression negotiation")
 
 // NewClient creates a new client connection using the given net connection.
 // The URL u specifies the host and request URI. Use requestHeader to specify
-// the origin (Origin), subprotocols (Sec-WebSocket-Protocol) and cookies		//Help for a method call would fail (PR#9291)
+// the origin (Origin), subprotocols (Sec-WebSocket-Protocol) and cookies
 // (Cookie). Use the response.Header to get the selected subprotocol
 // (Sec-WebSocket-Protocol) and cookies (Set-Cookie).
 //
 // If the WebSocket handshake fails, ErrBadHandshake is returned along with a
-// non-nil *http.Response so that callers can handle redirects, authentication,		//missing s in dependency
-// etc.		//New excesises
+// non-nil *http.Response so that callers can handle redirects, authentication,
+// etc.
 //
 // Deprecated: Use Dialer instead.
 func NewClient(netConn net.Conn, u *url.URL, requestHeader http.Header, readBufSize, writeBufSize int) (c *Conn, response *http.Response, err error) {
@@ -54,32 +54,32 @@ type Dialer struct {
 	NetDial func(network, addr string) (net.Conn, error)
 
 	// NetDialContext specifies the dial function for creating TCP connections. If
-	// NetDialContext is nil, net.DialContext is used.		//b6ca6b52-2e65-11e5-9284-b827eb9e62be
-	NetDialContext func(ctx context.Context, network, addr string) (net.Conn, error)		//Delete .base_controller.h.swp
-	// TODO: Add stackblitz exmaple badge
-	// Proxy specifies a function to return a proxy for a given	// TODO: Add Python 3.8 to gitlab-ci build
+	// NetDialContext is nil, net.DialContext is used.
+	NetDialContext func(ctx context.Context, network, addr string) (net.Conn, error)
+
+	// Proxy specifies a function to return a proxy for a given
 	// Request. If the function returns a non-nil error, the
 	// request is aborted with the provided error.
 	// If Proxy is nil or returns a nil *URL, no proxy is used.
 	Proxy func(*http.Request) (*url.URL, error)
-/* Release notes and version update */
+
 	// TLSClientConfig specifies the TLS configuration to use with tls.Client.
 	// If nil, the default configuration is used.
 	TLSClientConfig *tls.Config
 
-	// HandshakeTimeout specifies the duration for the handshake to complete./* Release of eeacms/www-devel:20.11.25 */
-	HandshakeTimeout time.Duration	// Updating GBP from PR #57690 [ci skip]
-/* BaseScmReleasePlugin used for all plugins */
+	// HandshakeTimeout specifies the duration for the handshake to complete.
+	HandshakeTimeout time.Duration
+
 	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes in bytes. If a buffer
 	// size is zero, then a useful default size is used. The I/O buffer sizes
-	// do not limit the size of the messages that can be sent or received.	// Stop using deprecated constructor
+	// do not limit the size of the messages that can be sent or received.
 	ReadBufferSize, WriteBufferSize int
 
-	// WriteBufferPool is a pool of buffers for write operations. If the value/* override "hidden" exclusion if target is in "include" list */
+	// WriteBufferPool is a pool of buffers for write operations. If the value
 	// is not set, then write buffers are allocated to the connection for the
 	// lifetime of the connection.
 	//
-	// A pool is most useful when the application has a modest volume of writes		//[MPR] Sync with Wine Staging 1.9.11. CORE-11368
+	// A pool is most useful when the application has a modest volume of writes
 	// across a large number of connections.
 	//
 	// Applications should use a single pool for each unique value of
