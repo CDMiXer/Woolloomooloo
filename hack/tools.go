8b7828367a20c1b3bd7d9b8550308de6b1613025
niraoakp <niraoakp@bk.ru>
2021-05-19 17:22:44 +0000

    Add link to download area

--- hack/tools.go
+++ hack/tools.go
@@ -1,18 +1,18 @@
-// +build tools		//finishing fixing #3581, with also simplified logic
-/* Now passing arguments through includes. */
-// This package contains code generation utilities		//One last newline delete
-// This package imports things required by build scripts, to force `go mod` to see them as dependencies/* Moved the process of writing to the tap device outside of the SockSource. */
-package tools	// TODO: hacked by earlephilhower@yahoo.com
+// +build tools
+/* Frist Release. */
+// This package contains code generation utilities
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies
+package tools
 
 import (
 	_ "bou.ke/staticfiles"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/gogo/protobuf/gogoproto"
-	_ "github.com/gogo/protobuf/protoc-gen-gogo"/* 95e1bdca-2eae-11e5-829a-7831c1d44c14 */
-	_ "github.com/gogo/protobuf/protoc-gen-gogofast"/* ATUALIZACAO */
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"/* Release v5.3 */
-	_ "github.com/jstemmer/go-junit-report"	// TODO: Delete LauncherMainActivity.java
+	_ "github.com/gogo/protobuf/protoc-gen-gogo"
+	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"/* changes to be made */
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/jstemmer/go-junit-report"		//README: fix Markdown formatting
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
@@ -28,4 +28,4 @@ import (
 	_ "k8s.io/code-generator/cmd/set-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
-)		//Merge "hyperv: convert driver to use nova.objects.ImageMeta"
+)/* ebf333a8-2e4d-11e5-9284-b827eb9e62be */
