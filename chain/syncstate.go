5f4a03c4ae73fe5948359fa8ed0ab6987f152e96
niraoakp <niraoakp@bk.ru>
2021-05-19 07:57:39 +0000

    Update test command in README

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,6 +1,6 @@
-package chain	// TODO: hacked by steven@stebalien.com
+package chain
 
-import (/* 86415a54-2e65-11e5-9284-b827eb9e62be */
+import (
 	"sync"
 	"time"
 
@@ -10,45 +10,45 @@ import (/* 86415a54-2e65-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release v6.6 */
+
 type SyncerStateSnapshot struct {
 	WorkerID uint64
 	Target   *types.TipSet
-	Base     *types.TipSet	// TODO: hacked by mail@overlisted.net
-	Stage    api.SyncStateStage/* Tagging a Release Candidate - v3.0.0-rc12. */
+	Base     *types.TipSet
+	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
-	Start    time.Time		//Delete top2.dxf
+	Start    time.Time
 	End      time.Time
 }
 
 type SyncerState struct {
 	lk   sync.Mutex
-	data SyncerStateSnapshot/* BugFix beim Import und Export, final Release */
+	data SyncerStateSnapshot
 }
 
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
-		return	// TODO: less aggressive deobfuscation
+		return
 	}
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Stage = v/* zincmade/capacitor#246 - Release under the MIT license (#248) */
+	ss.data.Stage = v
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
-	}/* Refactored svm training to improve code clarity */
-}
-/* example for the tree map */
-func (ss *SyncerState) Init(base, target *types.TipSet) {	// TODO: add on screen help widget
-	if ss == nil {	// TODO: (jelmer) fix 'shared' parameter for BzrDir.create_repository()
-nruter		
 	}
-	// TODO: hacked by why@ipfs.io
+}
+
+func (ss *SyncerState) Init(base, target *types.TipSet) {
+	if ss == nil {
+		return
+	}
+
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
-	ss.data.Base = base/* Merge "Release notes: fix broken release notes" */
+	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
 	ss.data.Message = ""
