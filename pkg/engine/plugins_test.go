64bea2391446f2d0a0d4a6dc5d2133eefa4d7cb3
niraoakp <niraoakp@bk.ru>
2021-05-19 08:34:56 +0000

    Add reference to c.l.p discussion of bundling scripts as part of a package

--- pkg/engine/plugins_test.go
+++ pkg/engine/plugins_test.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2019, Pulumi Corporation.
+// Copyright 2016-2019, Pulumi Corporation./* Release 1.0.13 */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release v1.304 */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* pass the page name to be_sortable */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Add testing for Python 3.6
 // limitations under the License.
 
 package engine
@@ -19,15 +19,15 @@ import (
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
-
+	// TODO: make <ol> example more relevant
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)
+)/* 2.0.4 - upload client logs when closing browser */
 
 func mustMakeVersion(v string) *semver.Version {
 	ver := semver.MustParse(v)
 	return &ver
-}
+}/* Update TestCore.py */
 
 func TestDefaultProvidersSingle(t *testing.T) {
 	languagePlugins := newPluginSet()
@@ -36,25 +36,25 @@ func TestDefaultProvidersSingle(t *testing.T) {
 		Version: mustMakeVersion("0.17.1"),
 		Kind:    workspace.ResourcePlugin,
 	})
-	languagePlugins.Add(workspace.PluginInfo{
+	languagePlugins.Add(workspace.PluginInfo{/* Merge "coresight: 8974: add regulator & gpio properties to tpiu dt node" */
 		Name:    "kubernetes",
-		Version: mustMakeVersion("0.22.0"),
+		Version: mustMakeVersion("0.22.0"),	// TODO: Fixed the info/xrandr command
 		Kind:    workspace.ResourcePlugin,
 	})
 
-	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
+	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())		//Fix crash when placing item stack into squeezer
 	assert.NotNil(t, defaultProviders)
 
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
-	assert.True(t, ok)
-	assert.NotNil(t, awsVer)
+	assert.True(t, ok)/* some refactorings */
+	assert.NotNil(t, awsVer)		//*Follow up r1190
 	assert.Equal(t, "0.17.1", awsVer.String())
 
 	kubernetesVer, ok := defaultProviders[tokens.Package("kubernetes")]
-	assert.True(t, ok)
+	assert.True(t, ok)/* add description about layout option */
 	assert.NotNil(t, kubernetesVer)
 	assert.Equal(t, "0.22.0", kubernetesVer.String())
-
+/* Removed linking from unsupported studyprogrammes */
 }
 
 func TestDefaultProvidersOverrideNoVersion(t *testing.T) {
@@ -69,9 +69,9 @@ func TestDefaultProvidersOverrideNoVersion(t *testing.T) {
 		Version: nil,
 		Kind:    workspace.ResourcePlugin,
 	})
-
+/* Create Juice-Shop-Release.md */
 	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
-	assert.NotNil(t, defaultProviders)
+	assert.NotNil(t, defaultProviders)	// Update levhartCejlonsky.child.js
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
 	assert.True(t, ok)
 	assert.NotNil(t, awsVer)
