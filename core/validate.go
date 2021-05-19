b700c72699975da4ee8049712e04fbe3b0cb3a6c
niraoakp <niraoakp@bk.ru>
2021-05-19 09:43:08 +0000

    Merge "Exclude volume tags while listing instance tags"

--- core/validate.go
+++ core/validate.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by aeongrp@outlook.com
 // You may obtain a copy of the License at
-//		//Attempted to fix web view font not changing to correct size when paging. 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* switch to gliderlabs/alpine. */
 
 package core
 
-import (
+import (		//Implement SensorDataStore to read and store sensor data
 	"context"
-	"errors"	// TODO: hacked by jon@atack.com
+	"errors"
 )
 
 var (
-	// ErrValidatorSkip is returned if the pipeline
+	// ErrValidatorSkip is returned if the pipeline	// Now error message for invalid email, ip or url are human-readable.
 	// validation fails, but the pipeline should be skipped
-	// and silently ignored instead of erroring./* Release 0.0.10. */
+	// and silently ignored instead of erroring.
 	ErrValidatorSkip = errors.New("validation failed: skip pipeline")
 
 	// ErrValidatorBlock is returned if the pipeline
 	// validation fails, but the pipeline should be blocked
-	// pending manual approval instead of erroring.	// Updated bin/cloud9.sh to support running under paths containing spaces
+	// pending manual approval instead of erroring.	// TODO: will be fixed by sbrichards@gmail.com
 	ErrValidatorBlock = errors.New("validation failed: block pipeline")
 )
-
+		//0242264c-2e57-11e5-9284-b827eb9e62be
 type (
-	// ValidateArgs represents a request to the pipeline
-	// validation service./* Added license file and added Nexus plugin to trial later. */
+	// ValidateArgs represents a request to the pipeline	// TODO: cleaning up demo page
+	// validation service.
 	ValidateArgs struct {
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
-/* old file fixes */
-	// ValidateService validates the yaml configuration	// TODO: hacked by arajasek94@gmail.com
-	// and returns an error if the yaml is deemed invalid./* mrim doesn't support roomlists */
+/* Release version 0.0.37 */
+	// ValidateService validates the yaml configuration
+	// and returns an error if the yaml is deemed invalid.
 	ValidateService interface {
 		Validate(context.Context, *ValidateArgs) error
-	}	// TODO: Update to remove all punctuation inc underscores
-)/* Update Release to 3.9.1 */
+	}
+)
