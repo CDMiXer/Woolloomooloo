e70541a68cb34425713ef872d10327856bcd36e9
niraoakp <niraoakp@bk.ru>
2021-05-19 09:32:19 +0000

    prepare to rx2

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -3,8 +3,8 @@ package sealing
 import (
 	"io"
 
-	"github.com/filecoin-project/go-state-types/abi"
-	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by arajasek94@gmail.com
+	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"	// TODO: hacked by sbrichards@gmail.com
 )
 
 type NullReader struct {
@@ -12,9 +12,9 @@ type NullReader struct {
 }
 
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}		//updating readme to reflect package name
 }
-
-func (m NullReader) NullBytes() int64 {
+	// TODO: will be fixed by ng8eke@163.com
+func (m NullReader) NullBytes() int64 {/* [1.1.6] Milestone: Release */
 	return m.N
-}
+}/* Release: 5.0.1 changelog */
