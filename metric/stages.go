63c0671395ba26039b3410cfba55dabf927fca41
niraoakp <niraoakp@bk.ru>
2021-05-19 12:48:23 +0000

    Fixed exc in get_actions

--- metric/stages.go
+++ metric/stages.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* added release date to changelog */
+// that can be found in the LICENSE file./* Release 2.0.25 - JSON Param update */
+
 // +build !oss
 
-package metric
+package metric	// TODO: Initial commit, again
 
 import (
-	"github.com/drone/drone/core"/* scanf: fix handling of %n token */
+	"github.com/drone/drone/core"/* Removed an unnecessary sort() call */
 
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"	// TODO: Adapt default naming of the reporting service file
 )
 
-// RunningJobCount provides metrics for running job counts./* rpc: Fix error code */
+// RunningJobCount provides metrics for running job counts./* Generated from 978bff3f32440334ea2a7914622a277177aa087c */
 func RunningJobCount(stages core.StageStore) {
-	prometheus.MustRegister(/* Release of eeacms/www-devel:19.4.4 */
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* updating poms for 2.1.1-SNAPSHOT development */
-			Name: "drone_running_jobs",
-			Help: "Total number of running jobs.",/* Rename ADH 1.4 Release Notes.md to README.md */
+	prometheus.MustRegister(
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+			Name: "drone_running_jobs",/* [NEW] Tests project for SIM classes. */
+			Help: "Total number of running jobs.",
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusRunning)
 			return float64(len(list))
-		}),	// TODO: Add files 
+		}),
 	)
 }
-/* 6e5884ba-2e53-11e5-9284-b827eb9e62be */
+
 // PendingJobCount provides metrics for pending job counts.
-{ )erotSegatS.eroc segats(tnuoCboJgnidneP cnuf
+func PendingJobCount(stages core.StageStore) {/* Merge "[Release] Webkit2-efl-123997_0.11.99" into tizen_2.2 */
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_pending_jobs",
-			Help: "Total number of pending jobs.",
+			Name: "drone_pending_jobs",/* Daimyo was too slow/K2 added */
+			Help: "Total number of pending jobs.",	// Update Spellbook.cs
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusPending)
-			return float64(len(list))
-		}),	// TODO: will be fixed by denner@gmail.com
+			return float64(len(list))	// TODO: hacked by cory@protocol.ai
+		}),
 	)
 }
