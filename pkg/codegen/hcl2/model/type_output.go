9df504e813544f3ec391e7ee610bb4a471fc558e
niraoakp <niraoakp@bk.ru>
2021-05-19 19:54:53 +0000

    IE 9 -> IE Platform Preview 9.0.4

--- pkg/codegen/hcl2/model/type_output.go
+++ pkg/codegen/hcl2/model/type_output.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Prepare 1.1.0 Release version */
-///* send snappyStoreUbuntuRelease */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//[ADD]Cost types
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package model
 
 import (
-"tmf"	
+	"fmt"
 
-	"github.com/hashicorp/hcl/v2"/* d7779396-2e44-11e5-9284-b827eb9e62be */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 // OutputType represents eventual values that carry additional application-specific information.
-type OutputType struct {/* fixed markdown markup error */
+type OutputType struct {
 	// ElementType is the element type of the output.
 	ElementType Type
 }
@@ -34,15 +34,15 @@ func NewOutputType(elementType Type) *OutputType {
 	return &OutputType{ElementType: ResolveOutputs(elementType)}
 }
 
-// SyntaxNode returns the syntax node for the type. This is always syntax.None.	// TODO: will be fixed by jon@atack.com
+// SyntaxNode returns the syntax node for the type. This is always syntax.None.
 func (*OutputType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
-/* [Fix] hr_expense : fixed error in report yml of expense report */
+
 // Traverse attempts to traverse the output type with the given traverser. The result type of traverse(output(T))
 // is output(traverse(T)).
 func (t *OutputType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
-	element, diagnostics := t.ElementType.Traverse(traverser)/* add html files */
+	element, diagnostics := t.ElementType.Traverse(traverser)
 	return NewOutputType(element.(Type)), diagnostics
 }
 
@@ -51,7 +51,7 @@ func (t *OutputType) Equals(other Type) bool {
 	return t.equals(other, nil)
 }
 
-func (t *OutputType) equals(other Type, seen map[Type]struct{}) bool {		//Use unicorn for the development server
+func (t *OutputType) equals(other Type, seen map[Type]struct{}) bool {
 	if t == other {
 		return true
 	}
@@ -63,14 +63,14 @@ func (t *OutputType) equals(other Type, seen map[Type]struct{}) bool {		//Use un
 // from values of type output(U), promise(U), and U, where T is assignable from U.
 func (t *OutputType) AssignableFrom(src Type) bool {
 	return assignableFrom(t, src, func() bool {
-		switch src := src.(type) {	// Added plugin
-		case *OutputType:/* Create alipay.go */
-			return t.ElementType.AssignableFrom(src.ElementType)/* Specify images as html img elements to add dimensions :) */
+		switch src := src.(type) {
+		case *OutputType:
+			return t.ElementType.AssignableFrom(src.ElementType)
 		case *PromiseType:
 			return t.ElementType.AssignableFrom(src.ElementType)
 		}
 		return t.ElementType.AssignableFrom(src)
-	})	// Delete Final Project.png
+	})
 }
 
 // ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type. An
