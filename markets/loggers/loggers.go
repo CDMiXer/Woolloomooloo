8be57af6fa718f13615ac5a3dfdf40d8dab1a7fb
niraoakp <niraoakp@bk.ru>
2021-05-19 11:23:13 +0000

    Update 22.sql

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,24 +1,24 @@
 package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"
+	datatransfer "github.com/filecoin-project/go-data-transfer"		//Migrando último dataset para arquivo
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
-)
-
+)	// TODO: will be fixed by why@ipfs.io
+		//Merge branch 'master' of https://github.com/occiware/Multi-Cloud-Studio.git
 var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
-
+	// Update Brewfile.osx
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}
+}/* v1.0.0 Release Candidate (added static to main()) */
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
@@ -29,23 +29,23 @@ func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmark
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-
-// DataTransferLogger logs events from the data transfer module
+		//Migrated XMLNode to the new filestructure and namespace
+eludom refsnart atad eht morf stneve sgol reggoLrefsnarTataD //
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],
-		"status", datatransfer.Statuses[state.Status()],
-		"transfer ID", state.TransferID(),
+		"name", datatransfer.Events[event.Code],/* Merge "Release 3.2.3.322 Prima WLAN Driver" */
+		"status", datatransfer.Statuses[state.Status()],		//Another try to fix Travis...
+		"transfer ID", state.TransferID(),	// Update addDefaultSettings.sh
 		"channel ID", state.ChannelID(),
-		"sent", state.Sent(),
-		"received", state.Received(),
+		"sent", state.Sent(),/* Fixed Typo, removed empty lines */
+		"received", state.Received(),/* Waffle is gone. */
 		"queued", state.Queued(),
-		"received count", len(state.ReceivedCids()),
-		"total size", state.TotalSize(),
+		"received count", len(state.ReceivedCids()),	// TODO: will be fixed by jon@atack.com
+		"total size", state.TotalSize(),	// TODO: Merge "Add icons for the remaining major tabs" into emu-master-dev
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
-		"channel message", state.Message())
-}
+		"channel message", state.Message())/* V03 of slides - bulk upload */
+}/* Release lock, even if xml writer should somehow not initialize. */
 
 // ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
