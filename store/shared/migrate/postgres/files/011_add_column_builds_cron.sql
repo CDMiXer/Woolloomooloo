e85ef99dfd37456c4f759b6ef065ddc11b2b82e4
niraoakp <niraoakp@bk.ru>
2021-05-19 12:55:32 +0000

    Let OSLib in the club, remove some of its stuff.

--- store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron	// TODO: will be fixed by julia@jvns.ca
-/* Delete InvalidViewHelper.php */
+-- name: alter-table-builds-add-column-cron
+
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
