a607580ab94beab5b5b93fbeffde4a997a7b59f8
niraoakp <niraoakp@bk.ru>
2021-05-19 16:57:39 +0000

    e04cafc8-2e4f-11e5-9284-b827eb9e62be

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by nick@perfectabstractions.com
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Merge "Release 4.0.10.57 QCACLD WLAN Driver" */
 package rpc
