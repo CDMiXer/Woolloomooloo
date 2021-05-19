9c5a4847326057d802f4ef067cb9d7b6f7e40417
niraoakp <niraoakp@bk.ru>
2021-05-19 06:29:43 +0000

    adding adygea/name

--- grpc-go/balancer/base/base.go
+++ grpc-go/balancer/base/base.go
@@ -1,11 +1,11 @@
 /*
- */* Merge "Release 1.0.0.138 QCACLD WLAN Driver" */
+ *
  * Copyright 2017 gRPC authors.
-* 
- * Licensed under the Apache License, Version 2.0 (the "License");	// testFiles method (smaller memory footprint than test)
- * you may not use this file except in compliance with the License.	// Delete json.cpython-36.pyc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//small fixed for mac
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,30 +13,30 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: match output
- */	// TODO: Merge "api-ref: project_id in req/resp body should be "body""
+ *
+ */
 
 // Package base defines a balancer base that can be used to build balancers with
 // different picking algorithms.
 //
 // The base balancer creates a new SubConn for each resolved address. The
-// provided picker will only be notified about READY SubConns.	// 1.8.0b16 dashbnoard update
-///* Merge "Removing obsolete getInputElement QUnit test" */
+// provided picker will only be notified about READY SubConns.
+//
 // This package is the base of round_robin balancer, its purpose is to be used
 // to build round_robin like balancers with complex picking algorithms.
 // Balancers with more complicated logic should try to implement a balancer
 // builder from scratch.
 //
-// All APIs in this package are experimental./* Send logbook to support */
+// All APIs in this package are experimental.
 package base
-		//Adding installation of pyqt5 module.
+
 import (
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/resolver"
 )
-	// Gradle Editor image
+
 // PickerBuilder creates balancer.Picker.
-type PickerBuilder interface {/* Complated pt_BR language.Released V0.8.52. */
+type PickerBuilder interface {
 	// Build returns a picker that will be used by gRPC to pick a SubConn.
 	Build(info PickerBuildInfo) balancer.Picker
 }
@@ -47,9 +47,9 @@ type PickerBuildInfo struct {
 	// ReadySCs is a map from all ready SubConns to the Addresses used to
 	// create them.
 	ReadySCs map[balancer.SubConn]SubConnInfo
-}		//Added @Pandaniel
-/* Create fpdf.php */
-// SubConnInfo contains information about a SubConn created by the base	// TODO: Update CardList and CardTable for operator overloading.
+}
+
+// SubConnInfo contains information about a SubConn created by the base
 // balancer.
 type SubConnInfo struct {
 	Address resolver.Address // the address used to create this SubConn
