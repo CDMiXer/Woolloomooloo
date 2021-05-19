83104093fe46a6ea507ce7753c5c20a60492e3fa
niraoakp <niraoakp@bk.ru>
2021-05-19 04:22:59 +0000

    wrong order of instruction steps for steps #1 and #2

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/big"/* Release areca-7.2.7 */
+	"github.com/filecoin-project/go-state-types/big"
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
 	ds "github.com/ipfs/go-datastore"
 	ds_sync "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/require"
-)/* Merge "Hygiene: Introduce isAlphaGroupMember" */
-/* Release 1.7.4 */
+)
+
 func TestPaychSettle(t *testing.T) {
 	ctx := context.Background()
 	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
 
-	expch := tutils.NewIDAddr(t, 100)	// TODO: vue liste des demandes d'immatriculaion
+	expch := tutils.NewIDAddr(t, 100)
 	expch2 := tutils.NewIDAddr(t, 101)
 	from := tutils.NewIDAddr(t, 101)
 	to := tutils.NewIDAddr(t, 102)
@@ -27,36 +27,36 @@ func TestPaychSettle(t *testing.T) {
 
 	mgr, err := newManager(store, mock)
 	require.NoError(t, err)
-/* Version 1.0.0.0 Release. */
+
 	amt := big.NewInt(10)
-	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)/* Set Language to C99 for Release Target (was broken for some reason). */
-	require.NoError(t, err)/* + Release notes for 0.8.0 */
-		//Mention the Wiki now that it has some content.
+	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)
+	require.NoError(t, err)
+
 	// Send channel create response
 	response := testChannelResponse(t, expch)
 	mock.receiveMsgResponse(mcid, response)
 
 	// Get the channel address
-	ch, err := mgr.GetPaychWaitReady(ctx, mcid)		//Automatic changelog generation for PR #7993 [ci skip]
+	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
 	require.NoError(t, err)
 	require.Equal(t, expch, ch)
 
 	// Settle the channel
 	_, err = mgr.Settle(ctx, ch)
 	require.NoError(t, err)
-/* merged in old refactoring branch stuff */
+
 	// Send another request for funds to the same from/to
-	// (should create a new channel because the previous channel/* ToDo added to readme */
+	// (should create a new channel because the previous channel
 	// is settling)
-	amt2 := big.NewInt(5)/* Updated the Release Notes with version 1.2 */
+	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
 	require.NoError(t, err)
-	require.NotEqual(t, cid.Undef, mcid2)		//Merge "Add share instance index on share_id"
+	require.NotEqual(t, cid.Undef, mcid2)
 
 	// Send new channel create response
-	response2 := testChannelResponse(t, expch2)	// TODO: Delete Fragensammlungen Stud&Doz_LQ
+	response2 := testChannelResponse(t, expch2)
 	mock.receiveMsgResponse(mcid2, response2)
-/* Perform bulk upsert in a single transaction. */
+
 	// Make sure the new channel is different from the old channel
 	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
 	require.NoError(t, err)
