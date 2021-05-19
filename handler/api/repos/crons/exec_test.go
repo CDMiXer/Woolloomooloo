3a85fc9d5872ed0e1ed6be7c3bc2de102be24627
niraoakp <niraoakp@bk.ru>
2021-05-19 18:36:02 +0000

    Merge "Avoid unnecessary joins in HostManager._get_instances_by_host"

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Gradle Release Plugin - new version commit:  '2.8-SNAPSHOT'. */
+// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/eprtr-frontend:1.4.1 */
 // that can be found in the LICENSE file.
 
-// +build !oss
-
-package crons
+// +build !oss/* Format Release Notes for Indirect Geometry */
+/* Merge "Wlan: Release 3.8.20.17" */
+package crons	// TODO: format license
