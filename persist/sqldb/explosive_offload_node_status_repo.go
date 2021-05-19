59a2b61f44ef1c2bb3f2ef6a2f00495fb0a202fb
niraoakp <niraoakp@bk.ru>
2021-05-19 00:33:11 +0000

    Delete Voronoi.h

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -3,18 +3,18 @@ package sqldb
 import (
 	"fmt"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// Update 31.1.10 Simple.md
+)/* preparing ino skeleton */
 
 var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
 var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
-
+	// TODO: Update get_alreadytrained.sh
 type explosiveOffloadNodeStatusRepo struct {
 }
 
 func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
-}
+}/* minor performance and tests fixes */
 
 func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
 	return "", OffloadNotSupportedError
@@ -28,7 +28,7 @@ func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Node
 	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
+func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {	// Install package of WebDAV client
 	return OffloadNotSupportedError
 }
 
