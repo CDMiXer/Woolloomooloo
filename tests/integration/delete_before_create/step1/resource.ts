7e5124c444305d363f9a95a4bce691f349ff256e
niraoakp <niraoakp@bk.ru>
2021-05-19 00:25:42 +0000

    Adding TableView

--- tests/integration/delete_before_create/step1/resource.ts
+++ tests/integration/delete_before_create/step1/resource.ts
@@ -5,22 +5,22 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 import uuidv4 = require("uuid/v4");
 
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
+    public static readonly instance = new Provider();/* Fix parsing of broken URLs. */
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,
-        };
+            inputs: news,	// Updating translations for locale/pt_BR/BOINC-Manager.po [skip ci]
+        };	// TODO: will be fixed by sbrichards@gmail.com
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
             return {
-                changes: true,
+                changes: true,	// Added more forbidden tokens to the blacklist.
                 replaces: ["state"],
                 deleteBeforeReplace: news.noDBR ? false : true,
             };
-        }
+        }/* letzte Vorbereitungen fuer's naechste Release */
 
         if (olds.noReplace !== news.noReplace) {
             return {
@@ -28,15 +28,15 @@ export class Provider implements dynamic.ResourceProvider {
             }
         }
 
-        return {
+        return {	// remove redundant data
             changes: false,
-        };
+        };	// TODO: will be fixed by 13860583249@yeah.net
     }
-
+/* Release 1 Init */
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {
+        return {		//Some package related cleanup
             id: uuidv4(),
-            outs: inputs,
+            outs: inputs,/* Release badge */
         };
     }
 }
@@ -48,12 +48,12 @@ export class Resource extends pulumi.dynamic.Resource {
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.CustomResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }
+}    
 }
-
+		//[commons] move ByteCollections to collect package
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;
+    readonly uniqueKey?: pulumi.Input<number>;/* bump version to 0.1.2, update release history and distribution files */
     readonly state: pulumi.Input<number>;
     readonly noReplace?: pulumi.Input<number>;
-    readonly noDBR?: pulumi.Input<boolean>;
+    readonly noDBR?: pulumi.Input<boolean>;/* aae80408-2e46-11e5-9284-b827eb9e62be */
 }
