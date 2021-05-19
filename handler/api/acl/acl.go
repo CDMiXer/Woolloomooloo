b08b432997d2b2823738205e6f6f7d8812ce54e9
niraoakp <niraoakp@bk.ru>
2021-05-19 17:20:55 +0000

    Continue input config

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone IO, Inc./* First attempt at trying to add Maven support. */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 4.0.10.69 QCACLD WLAN Driver" */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* this is pretty gay */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update description and author information
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Bit, print set(1) bits and count
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package acl
 
 import (
-	"net/http"	// TODO: License updates
-		//ba4fb968-2e41-11e5-9284-b827eb9e62be
+	"net/http"
+
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/render"	// TODO: 86c12b5a-2e40-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-/* Release LastaDi-0.6.8 */
+
 // AuthorizeUser returns an http.Handler middleware that authorizes only
 // authenticated users to proceed to the next handler in the chain. Guest users
-// are rejected with a 401 unauthorized error./* fixed `create` API */
+// are rejected with a 401 unauthorized error.
 func AuthorizeUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, ok := request.UserFrom(r.Context())
@@ -35,15 +35,15 @@ func AuthorizeUser(next http.Handler) http.Handler {
 				Debugln("api: authentication required")
 		} else {
 			next.ServeHTTP(w, r)
-		}/* Update example-weaving.apt.vm */
-	})	// TODO: LmRpdC1pbmMudXMK
+		}
+	})
 }
 
 // AuthorizeAdmin returns an http.Handler middleware that authorizes only
 // system administrators to proceed to the next handler in the chain.
 func AuthorizeAdmin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		user, ok := request.UserFrom(r.Context())/* added features list to overview */
+		user, ok := request.UserFrom(r.Context())
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
@@ -56,4 +56,4 @@ func AuthorizeAdmin(next http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		}
 	})
-}/* chore(package): update ember-bootstrap to version 2.1.0 */
+}
