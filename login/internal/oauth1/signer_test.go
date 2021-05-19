d29487fc8dd0705a517d106b895a4d18b2809e0e
niraoakp <niraoakp@bk.ru>
2021-05-19 11:14:22 +0000

    m3ua removeAllResources and process bad encoded MAPUserAbortInfo

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved./* adding removal of fragments in outputPosChanged, debugging facilities added too */
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
-/* Release 2.4b4 */
-package oauth1	// TODO: hacked by indexxuan@gmail.com
+
+package oauth1
