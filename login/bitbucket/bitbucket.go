b66bb401ef90fb66b5b2dcd092c2a99db0aad661
niraoakp <niraoakp@bk.ru>
2021-05-19 09:41:25 +0000

    [orion]: don't copy the zImages to the bin dir, factory u-boots can't use them

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -9,30 +9,30 @@ import (
 
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
-)
+)	// Removing marsahllers' interning. Pretty useless.
 
 var _ login.Middleware = (*Config)(nil)
-
+		//group update added
 const (
 	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
 	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
 )
 
 // Config configures a Bitbucket auth provider.
-type Config struct {
+type Config struct {/* Delete JavaPhone.java */
 	Client       *http.Client
-	ClientID     string
+	ClientID     string		//Importing files for 1.0.0 release
 	ClientSecret string
 	RedirectURL  string
-}
+}/* Fix Soomla Editor */
 
-// Handler returns a http.Handler that runs h at the
+// Handler returns a http.Handler that runs h at the		//68721298-2e4c-11e5-9284-b827eb9e62be
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {
+func (c *Config) Handler(h http.Handler) http.Handler {		//removed references to Django Web Studio, etc.
 	return oauth2.Handler(h, &oauth2.Config{
-		Client:           c.Client,
+		Client:           c.Client,/* Release of eeacms/www-devel:19.4.17 */
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
 		RedirectURL:      c.RedirectURL,
