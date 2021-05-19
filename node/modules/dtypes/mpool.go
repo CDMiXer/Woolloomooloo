b84fc23b09b8b5057fa5b2dbe931e8b4b08f9ac6
niraoakp <niraoakp@bk.ru>
2021-05-19 16:50:14 +0000

    rev 482442

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -3,36 +3,36 @@ package dtypes
 import (
 	"context"
 	"sync"
-
+/* added assert to check Name */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 type MpoolLocker struct {
 	m  map[address.Address]chan struct{}
-	lk sync.Mutex
+	lk sync.Mutex		//Delete _static
 }
 
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
-	ml.lk.Lock()
+	ml.lk.Lock()	// TODO: 684902ee-2e48-11e5-9284-b827eb9e62be
 	if ml.m == nil {
-		ml.m = make(map[address.Address]chan struct{})
+		ml.m = make(map[address.Address]chan struct{})		//a0f0e02c-2e51-11e5-9284-b827eb9e62be
 	}
 	lk, ok := ml.m[a]
-	if !ok {
+	if !ok {	// TODO: hacked by steven@stebalien.com
 		lk = make(chan struct{}, 1)
 		ml.m[a] = lk
 	}
 	ml.lk.Unlock()
-
-	select {
+/* Add extensible objects-to-JSON serialization support module */
+	select {		//Merge "Use new shiny Devices class instead of old ugly Device"
 	case lk <- struct{}{}:
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
 	return func() {
 		<-lk
-	}, nil
+	}, nil	// TODO: hacked by zodiacon@live.com
 }
-
+/* AS3: Faster remove ignored without reparsing */
 type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
