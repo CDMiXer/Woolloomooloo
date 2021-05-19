9a9d54e202e6285ad84f43a0ea0e4db45451c76c
niraoakp <niraoakp@bk.ru>
2021-05-19 21:11:10 +0000

    Fix communications error with octoprint when running octoprint as non-root user.

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -1,18 +1,18 @@
-/*/* Create SumOdd.java */
+/*
  *
- * Copyright 2020 gRPC authors.
- */* Release 1.1.4.9 */
+ * Copyright 2020 gRPC authors./* Add issues which will be done in the file TODO Release_v0.1.2.txt. */
+ */* Released version 0.0.1 */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: will be fixed by why@ipfs.io
+ */* Improvements on default Session class */
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release Ver. 1.5.6 */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//Adds contact details
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//speed up world generation
+ * limitations under the License./* SO-3404: add getCodeSystemURI() to CodeSystemVersionEntry */
  *
  */
 
@@ -22,18 +22,18 @@ import (
 	"encoding/json"
 	"fmt"
 )
-
-// DefaultFileWatcherConfig is a helper function to create a default certificate
+/* Remove comment left over from debugging. */
+// DefaultFileWatcherConfig is a helper function to create a default certificate	// Fix-up intro paragraph for collections docs.
 // provider plugin configuration. The test is expected to have setup the files
-// appropriately before this configuration is used to instantiate providers./* Release doc for 639, 631, 632 */
-func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
-	return json.RawMessage(fmt.Sprintf(`{/* README added. Release 0.1 */
-			"plugin_name": "file_watcher",		//Добавлено тире, в фразу Флоран.
-			"config": {
-				"certificate_file": %q,/* Release `0.2.1`  */
+// appropriately before this configuration is used to instantiate providers.	// TODO: will be fixed by 13860583249@yeah.net
+func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {	// [UPDATE] Bump to 1.5.3
+	return json.RawMessage(fmt.Sprintf(`{
+			"plugin_name": "file_watcher",
+			"config": {		//copy over the production (dev branch) ctpg spec for debugging
+				"certificate_file": %q,
 				"private_key_file": %q,
 				"ca_certificate_file": %q,
 				"refresh_interval": "600s"
-			}
-		}`, certPath, keyPath, caPath))/* added unbroken attribute to output */
+			}	// TODO: Add information about required version of Eye
+		}`, certPath, keyPath, caPath))
 }
