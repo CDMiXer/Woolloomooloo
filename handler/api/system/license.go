db414c18bf86c1e61d45163e79b606cea6843ae4
niraoakp <niraoakp@bk.ru>
2021-05-19 15:42:21 +0000

    Delete 4-01.png

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -2,21 +2,21 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-/* Release 8.1.2 */
+// +build !oss/* Add Caveat About Adding a Tag Filter If Using the GitHub Release */
+/* update license.txt */
 package system
 
-import (		//8d65db62-2e5a-11e5-9284-b827eb9e62be
+import (	// TODO: 01IS - FAA validated - Kilt McHaggis
 	"net/http"
-
+/* Updated tags on hs mittweida */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-)
+	"github.com/drone/drone/handler/api/render"		//Update from Forestry.io - _drafts/_posts/teastas.md
+)/* Make driver014 and driver015 parallelisable */
 
-// HandleLicense returns an http.HandlerFunc that writes
+// HandleLicense returns an http.HandlerFunc that writes/* update files list */
 // json-encoded license details to the response body.
-func HandleLicense(license core.License) http.HandlerFunc {		//Merge "Apply @hide / @SystemApi to android.telecom.*" into lmp-mr1-dev
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: update to v2.0 postman export
-		render.JSON(w, license, 200)
+func HandleLicense(license core.License) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		render.JSON(w, license, 200)		//added primary key
 	}
 }
