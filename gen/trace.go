6535307040816007c61f8898f19674cc7506f212
niraoakp <niraoakp@bk.ru>
2021-05-19 20:23:48 +0000

    forgotten option handled

--- gen/trace.go
+++ gen/trace.go
@@ -3,9 +3,9 @@
 package websocket
 
 import (
-	"crypto/tls"/* fix(package): update sequelize to version 5.3.1 */
+	"crypto/tls"
 	"net/http/httptrace"
-)	// TODO: Update NuGet-4.7-RTM.md
+)
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
@@ -14,6 +14,6 @@ func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
-	}	// TODO: Merge "Bug 1813500: Consolidate Find Groups and My Groups into one page"
+	}
 	return err
 }
