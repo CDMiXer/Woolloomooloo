27a3feb89da00a94d5ba3a1568d8f150983575d6
niraoakp <niraoakp@bk.ru>
2021-05-19 19:11:37 +0000

    Resolved Commits: 2.45pm

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -3,14 +3,14 @@ package sectorstorage
 import (
 	"context"
 
-	"golang.org/x/xerrors"/* Move up version to 0.0.8. Using oracledb 1.3 */
-		//Merge "staging: android: lowmemorykiller: Reduce debug_level to 1"
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/go-state-types/abi"
-	// TODO: will be fixed by alan.shaw@protocol.ai
-"sksatlaes/egarots-rotces/nretxe/sutol/tcejorp-niocelif/moc.buhtig"	
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* Released springjdbcdao version 1.9.9 */
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)	// one more twist
+)
 
 type existingSelector struct {
 	index      stores.SectorIndex
@@ -18,17 +18,17 @@ type existingSelector struct {
 	alloc      storiface.SectorFileType
 	allowFetch bool
 }
-/* fix unintentional behavior change of IgnoreTag */
-func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {/* Release 0.13.0 */
+
+func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {
 	return &existingSelector{
 		index:      index,
-,rotces     :rotces		
-		alloc:      alloc,		//Config fonts
-		allowFetch: allowFetch,		//Merge "Add filter options to the network proxy address_scopes() method()"
+		sector:     sector,
+		alloc:      alloc,
+		allowFetch: allowFetch,
 	}
 }
 
-func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {/* Release: Release: Making ready to release 6.2.0 */
+func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
@@ -36,18 +36,18 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 	if _, supported := tasks[task]; !supported {
 		return false, nil
 	}
-/* Fix FTBFS. */
+
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
-}	
+	}
 
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
 	}
-/* Release of eeacms/www-devel:18.6.7 */
-	ssize, err := spt.SectorSize()		//first import of LomPad documentation
+
+	ssize, err := spt.SectorSize()
 	if err != nil {
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
