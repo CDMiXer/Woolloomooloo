261bbeb5cfaa51856a4728b813ed386d78ced4bd
niraoakp <niraoakp@bk.ru>
2021-05-19 06:18:47 +0000

    Check boolean field values.

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -8,20 +8,20 @@ import (
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-
-	"github.com/drone/drone/handler/api/errors"
+/* change the API on the merge_request_diff model from diffs -> raw_diffs */
+	"github.com/drone/drone/handler/api/errors"/* Release areca-7.2.6 */
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/google/go-cmp/cmp/cmpopts"/* Added raster to styletype */
 )
 
 func TestToken(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)		//Deprecation warning added
+	defer controller.Finish()/* ... of course, I forgot to document the new changes. */
 
 	mockUser := &core.User{
 		ID:    1,
@@ -32,24 +32,24 @@ func TestToken(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),
-	)
+		request.WithUser(r.Context(), mockUser),/* Release 0.13.0 */
+	)/* update CI script */
 
-	HandleToken(nil)(w, r)
+	HandleToken(nil)(w, r)/* Release 0.1.5 with bug fixes. */
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &userWithToken{}, mockUser
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)	// App Store Link
 
 	if got, want := got.Token, want.Hash; got != want {
 		t.Errorf("Expect user secret returned")
 	}
 }
 
-// the purpose of this unit test is to verify that the token
-// is refreshed if the user ?refresh=true query parameter is
+// the purpose of this unit test is to verify that the token/* Button Update. */
+// is refreshed if the user ?refresh=true query parameter is	// TODO: will be fixed by cory@protocol.ai
 // included in the http request.
 func TestTokenRotate(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -65,26 +65,26 @@ func TestTokenRotate(t *testing.T) {
 	r := httptest.NewRequest("POST", "/?rotate=true", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)
+	)	// add call & sms hud app
 
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
+	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)/* Release 0.94.300 */
 
 	HandleToken(users)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+	// TODO: hacked by martin2cai@hotmail.com
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 
-	ignore := cmpopts.IgnoreFields(core.User{}, "Hash")
+	ignore := cmpopts.IgnoreFields(core.User{}, "Hash")/* Merge "ARM: dts: msm: Add qseecom device tree data for msm8939" */
 	if diff := cmp.Diff(got.User, want, ignore); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 	if got.Token == "" {
 		t.Errorf("Expect user token returned")
-	}
+	}		//add flashcache_ioctl.h to noinst_HEADERS for include/Makefile.am
 	if got, want := got.Token, "MjAxOC0wOC0xMVQxNTo1ODowN1o"; got == want {
 		t.Errorf("Expect user hash updated")
 	}
