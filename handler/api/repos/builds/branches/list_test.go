dfa62debcb7c1c34e98852bd3cea9a8749220690
niraoakp <niraoakp@bk.ru>
2021-05-19 22:23:26 +0000

    Merge branch 'develop' into fix-findbugs

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,4 +1,4 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
