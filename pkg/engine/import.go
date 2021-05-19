fc530205f7c6d2cd5cc4a38dfeaacf3ca9d7684e
niraoakp <niraoakp@bk.ru>
2021-05-19 22:33:39 +0000

    Merge "Fix receiving OVS events twice"

--- pkg/engine/import.go
+++ pkg/engine/import.go
@@ -1,52 +1,52 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Merge "ASoC: wcd: update cross connection logic for removal"
+// Copyright 2016-2020, Pulumi Corporation.		//Add Sinatra::NotFound to Airbrake ignored errors list.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* [artifactory-release] Release version 1.7.0.RELEASE */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Added Travis build
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: completed transition matrix
 // limitations under the License.
 
-package engine
+package engine/* Use Release build in CI */
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* Merge "Remove the check hasMultipleUsers in Settings." into mnc-dev */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Ajout fichiers Doctrine
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"	// TODO: [server] Merged fix for lp:670351
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"	// TODO: will be fixed by admin@multicoin.co
 )
-	// Create seqcurator.py
+/* Merge "BUG-582: expose QNameModule" */
 func Import(u UpdateInfo, ctx *Context, opts UpdateOptions, imports []deploy.Import,
 	dryRun bool) (ResourceChanges, result.Result) {
-/* Merge remote-tracking branch 'AIMS/UAT_Release6' */
-	contract.Require(u != nil, "u")
+
+	contract.Require(u != nil, "u")		//Merge "Upgrade from ELK6 to ELK7 FOSS release"
 	contract.Require(ctx != nil, "ctx")
 
-	defer func() { ctx.Events <- cancelEvent() }()
-
+	defer func() { ctx.Events <- cancelEvent() }()	// Remove signon-apparmor-password from upstream merger, it was a mistake.
+/* Made addons list constant. */
 	info, err := newDeploymentContext(u, "import", ctx.ParentSpan)
 	if err != nil {
 		return nil, result.FromError(err)
-	}/* [Release] Prepare release of first version 1.0.0 */
+	}
 	defer info.Close()
-
+	// TODO: adapt js to new xml layout
 	emitter, err := makeEventEmitter(ctx.Events, u)
-	if err != nil {/* Added script to set build version from Git Release */
+	if err != nil {/* Release version [10.6.2] - prepare */
 		return nil, result.FromError(err)
 	}
-	defer emitter.Close()/* Release 0.94.300 */
+	defer emitter.Close()
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newRefreshSource,
 		Events:        emitter,
-		Diag:          newEventSink(emitter, false),		//SHA256 Klasse eingebaut.
+		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
-		isImport:      true,
-		imports:       imports,/* added session and position update */
+		isImport:      true,	// TODO: switch to django-docker:1.3 container
+		imports:       imports,
 	}, dryRun)
 }
