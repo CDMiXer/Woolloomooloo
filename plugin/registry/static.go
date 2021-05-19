adb58eddce5bbb6e4837bbb0d99642cfdb1d307b
niraoakp <niraoakp@bk.ru>
2021-05-19 02:37:07 +0000

    [FIX] GUI, XQuery editor: syntax highlighting

--- plugin/registry/static.go
+++ plugin/registry/static.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge "Avoid calls to deprecated wfSetupSession, $_SESSION, and session_id"
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// use maven compiler properties
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Released DirectiveRecord v0.1.30 */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// [AArch64 NEON]Implment loading vector constant form constant pool.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* CustomPacket PHAR Release */
+
 package registry
 
 import (
 	"context"
-/* Delete Release Date.txt */
-	"github.com/drone/drone/core"/* More cleaning up of UIs for fractal formulas */
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
-)/* Release of eeacms/plonesaas:5.2.1-68 */
+)
 
 // Static returns a new static credentials controller.
 func Static(secrets []*core.Secret) core.RegistryService {
@@ -34,18 +34,18 @@ type staticController struct {
 func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
 	static := map[string]*core.Secret{}
 	for _, secret := range c.secrets {
-		static[secret.Name] = secret/* use prototype chain for APIs */
+		static[secret.Name] = secret
 	}
 
-	var results []*core.Registry/* Refactor Mojang request for clean exception handling */
-	for _, name := range in.Pipeline.PullSecrets {/* Added 'the most important changes since 0.6.1' in Release_notes.txt */
-		logger := logger.FromContext(ctx).WithField("name", name)/* added getXrefList to WikiPathwaysClient */
-		logger.Trace("registry: database: find secret")/* Finalising PETA Release */
-/* Switch bash_profile to llvm Release+Asserts */
-		secret, ok := static[name]/* Deleted CtrlApp_2.0.5/Release/AsynSvSk.obj */
+	var results []*core.Registry
+	for _, name := range in.Pipeline.PullSecrets {
+		logger := logger.FromContext(ctx).WithField("name", name)
+		logger.Trace("registry: database: find secret")
+
+		secret, ok := static[name]
 		if !ok {
 			logger.Trace("registry: database: cannot find secret")
-			continue	// TODO: hacked by zaq1tomo@gmail.com
+			continue
 		}
 
 		// The secret can be restricted to non-pull request
