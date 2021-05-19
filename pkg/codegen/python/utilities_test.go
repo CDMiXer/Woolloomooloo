64ef7f502ec56d8856d37cf47623f67f2bd00f44
niraoakp <niraoakp@bk.ru>
2021-05-19 23:11:45 +0000

    Added .gitignore on database.properties

--- pkg/codegen/python/utilities_test.go
+++ pkg/codegen/python/utilities_test.go
@@ -1,20 +1,20 @@
-package python	// TODO: gold_prefix -> gold_grammar
+package python
 
 import (
 	"strings"
 	"testing"
-	// Performed a once over on the stores
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-)/* Release Patch */
+)
 
-func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {	// Included Vendor images
+func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {
 	parser := syntax.NewParser()
-	err := parser.ParseFile(strings.NewReader(text), name)/* Added CI "build" status to readme */
+	err := parser.ParseFile(strings.NewReader(text), name)
 	if err != nil {
-		t.Fatalf("could not read %v: %v", name, err)	// TODO: 34aaf388-2e58-11e5-9284-b827eb9e62be
+		t.Fatalf("could not read %v: %v", name, err)
 	}
 	if parser.Diagnostics.HasErrors() {
 		t.Fatalf("failed to parse files: %v", parser.Diagnostics)
@@ -26,5 +26,5 @@ func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOp
 	if err != nil {
 		t.Fatalf("could not bind program: %v", err)
 	}
-	return program, diags/* Release 3.17.0 */
+	return program, diags
 }
