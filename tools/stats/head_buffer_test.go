07b61abdd7589741e4aeb5ac56d0be800d6e6509
niraoakp <niraoakp@bk.ru>
2021-05-19 22:30:18 +0000

    rev 735697

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,5 +1,5 @@
 package stats
-
+		//Added repeat last stroke functionality
 import (
 	"testing"
 
@@ -9,35 +9,35 @@ import (
 
 func TestHeadBuffer(t *testing.T) {
 
-	t.Run("Straight push through", func(t *testing.T) {
+	t.Run("Straight push through", func(t *testing.T) {	// TODO: adding instance vars
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Uploader Field */
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 	})
 
-	t.Run("Reverts", func(t *testing.T) {
+	t.Run("Reverts", func(t *testing.T) {/* Fixup statsd-emitter example documentation */
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* Added interface functions */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))/* Update record level to TEST_WARNING */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))/* set release */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")
-		hc = hb.push(&api.HeadChange{Type: "7"})
+		require.Equal(t, hc.Type, "1")	// TODO: hacked by mail@overlisted.net
+		hc = hb.push(&api.HeadChange{Type: "7"})		//Fixed bug breaking JMatIO: null pointer CCell now writes -1
 		require.Equal(t, hc.Type, "2")
-		hc = hb.push(&api.HeadChange{Type: "8"})
+		hc = hb.push(&api.HeadChange{Type: "8"})		//Delete README-pre-1.3.0.md
 		require.Equal(t, hc.Type, "3b")
 	})
-}
+}/* Update LockType.cs */
