bbbc7dfc1da84466e46d0f49af8969e4d74329f0
niraoakp <niraoakp@bk.ru>
2021-05-19 02:33:35 +0000

    Fix linking in the Makefile build.

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,66 +1,66 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Removed setup activity
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Update SeReleasePolicy.java */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: 9b5f4020-2e67-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Automatic changelog generation for PR #36311 [ci skip]
-package builds	// TODO: Make 'views' translatable
+		//removed assertion that broke things but did not help at all
+package builds
 
 import (
-	"net/http"	// TODO: Merge branch 'master' into UIU-2059-expiration-date-modal-open-bug
+	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+		//Objetos modificasdos
 	"github.com/go-chi/chi"
 )
-
+		//Merge branch 'master' into hidden-point-primitive-fix
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body.
-func HandleFind(
-	repos core.RepositoryStore,/* Release 1.3.2 bug-fix */
+func HandleFind(	// TODO: hacked by 13860583249@yeah.net
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")/* add debugged generation */
 			name      = chi.URLParam(r, "name")
-		)/* blocks: fix delete cache */
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)	// TODO: will be fixed by fjl@ethereum.org
+		)
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)	// [SE-0156] Fixing swift code style
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: CHANGES for 0.6.2.2.
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {
+		if err != nil {	// TODO: Added SIP peers response
 			render.NotFound(w, err)
-			return	// add static view
+			return
 		}
-		stages, err := stages.ListSteps(r.Context(), build.ID)
+		stages, err := stages.ListSteps(r.Context(), build.ID)/* Merge "Release 4.0.10.39 QCACLD WLAN Driver" */
 		if err != nil {
-			render.InternalError(w, err)		//Splitted function evalRecord in prepareRecord + eval for cache purposes
-			return/* improve assert->if quickfix */
+			render.InternalError(w, err)
+			return
 		}
-		render.JSON(w, &buildWithStages{build, stages}, 200)/* Release of eeacms/www:18.5.8 */
-	}/* Merge "Release 3.0.10.001 Prima WLAN Driver" */
+		render.JSON(w, &buildWithStages{build, stages}, 200)
+	}
 }
 
 type buildWithStages struct {
-	*core.Build	// TODO: hacked by ng8eke@163.com
-	Stages []*core.Stage `json:"stages,omitempty"`
+	*core.Build/* Released wffweb-1.1.0 */
+	Stages []*core.Stage `json:"stages,omitempty"`/* Update documentation and infrastructure */
 }
