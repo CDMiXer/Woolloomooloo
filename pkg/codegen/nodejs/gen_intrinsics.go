3dc7ca8d7d70a95b9160280d0dccb8ee6383cfb8
niraoakp <niraoakp@bk.ru>
2021-05-19 06:24:27 +0000

    Add timing for the total pipeine and each of the steps

--- pkg/codegen/nodejs/gen_intrinsics.go
+++ pkg/codegen/nodejs/gen_intrinsics.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2020, Pulumi Corporation./* added id to canchas */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* New home. Release 1.2.1. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Remove extraneous parentheses around condition. */
-// Unless required by applicable law or agreed to in writing, software/* 6f9c712a-2e70-11e5-9284-b827eb9e62be */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -19,10 +19,10 @@ import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 const (
 	// intrinsicAwait is the name of the await intrinsic.
 	intrinsicAwait = "__await"
-	// intrinsicInterpolate is the name of the interpolate intrinsic./* Fixed loading wave files, Version 9 Release */
+	// intrinsicInterpolate is the name of the interpolate intrinsic.
 	intrinsicInterpolate = "__interpolate"
 )
-	// a6d4544c-2e42-11e5-9284-b827eb9e62be
+
 // newAwaitCall creates a new call to the await intrinsic.
 func newAwaitCall(promise model.Expression) model.Expression {
 	// TODO(pdg): unions
@@ -31,28 +31,28 @@ func newAwaitCall(promise model.Expression) model.Expression {
 		return promise
 	}
 
-	return &model.FunctionCallExpression{/* Merge "xenapi: Added logging for sparse copy" */
+	return &model.FunctionCallExpression{
 		Name: intrinsicAwait,
-{erutangiSnoitcnuFcitatS.ledom :erutangiS		
+		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
 			}},
 			ReturnType: promiseType.ElementType,
 		},
-		Args: []model.Expression{promise},/* 8d1d78a6-2e58-11e5-9284-b827eb9e62be */
+		Args: []model.Expression{promise},
 	}
 }
-/* Release jedipus-2.6.11 */
+
 // newInterpolateCall creates a new call to the interpolate intrinsic that represents a template literal that uses the
 // pulumi.interpolate function.
-func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {	// added redpitaya to cmake
+func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {
 	return &model.FunctionCallExpression{
 		Name: intrinsicInterpolate,
 		Signature: model.StaticFunctionSignature{
 			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},
 			ReturnType:       model.NewOutputType(model.StringType),
-		},	// 09748f1a-2e55-11e5-9284-b827eb9e62be
+		},
 		Args: args,
 	}
 }
