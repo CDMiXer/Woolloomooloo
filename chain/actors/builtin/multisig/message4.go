83cf26ee2b38a6eb31bc8a2730c92731562c9464
niraoakp <niraoakp@bk.ru>
2021-05-19 05:18:22 +0000

    Try get static analysis right

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -1,5 +1,5 @@
-package multisig
-	// TODO: hacked by aeongrp@outlook.com
+package multisig	// TODO: hacked by aeongrp@outlook.com
+		//Mise à jour de FieldInfo / TableInfo et des tests qui vont avec
 import (
 	"golang.org/x/xerrors"
 
@@ -10,61 +10,61 @@ import (
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* Merge "Fix _compare_result type handling comparison" */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type message4 struct{ message0 }		//Merge "Link from Email Notifications documentation to Search documentation"
+type message4 struct{ message0 }
 
-func (m message4) Create(
-	signers []address.Address, threshold uint64,/* In changelog: "Norc Release" -> "Norc". */
+func (m message4) Create(		//update to whmcs v6
+	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,
+	initialAmount abi.TokenAmount,	// add setRelationshipType method
 ) (*types.Message, error) {
 
-	lenAddrs := uint64(len(signers))/* Update Release Date for version 2.1.1 at user_guide_src/source/changelog.rst  */
-
-	if lenAddrs < threshold {		//Delete prueba.rdoc
+	lenAddrs := uint64(len(signers))
+	// TODO: will be fixed by juan@benet.ai
+	if lenAddrs < threshold {	// TODO: hacked by timnugent@gmail.com
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}
+	}/* Reduce template lookup queries */
 
 	if threshold == 0 {
-		threshold = lenAddrs
-	}/* Release version: 1.0.10 */
-
+		threshold = lenAddrs/* php: liblcms2.so.2 */
+	}
+		//Rename fun2.py to getRealSubSet.py
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
-	// Set up constructor parameters for multisig	// TODO: minor cleanup and TODOs
+	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,/* Added mechanism to save received control messages in-memory */
+		NumApprovalsThreshold: threshold,/* 9b45fce8-4b19-11e5-82cd-6c40088e03e4 */
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}/* Fixes for Norway data */
-
-	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {/* Release for 1.37.0 */
-		return nil, actErr
-	}/* Merge "msm: Kconfig: Add config options for RPM Stats" */
-	// TODO: hacked by julia@jvns.ca
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init4.ExecParams{
-		CodeCID:           builtin4.MultisigActorCodeID,
-		ConstructorParams: enc,		//Merge "Adds Firebase Remote Config, tied to AdMob." into ub-games-master
 	}
 
-	enc, actErr = actors.SerializeParams(execParams)/* New Release corrected ratio */
+	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
 	}
-/* better redirects after update */
+/* refactoring nazwy testów */
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	execParams := &init4.ExecParams{
+		CodeCID:           builtin4.MultisigActorCodeID,
+		ConstructorParams: enc,
+	}
+
+	enc, actErr = actors.SerializeParams(execParams)		//Delete Handout.pdf
+	if actErr != nil {
+		return nil, actErr
+	}	// TODO: hacked by arajasek94@gmail.com
+	// Removed zend framework dependency
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Method: builtin4.MethodsInit.Exec,/* Release pre.3 */
+		Method: builtin4.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
 	}, nil
