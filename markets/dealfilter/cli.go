b824006658446777a7467506407d50d97acf5aa6
niraoakp <niraoakp@bk.ru>
2021-05-19 17:35:34 +0000

    test_backupdb: improve error messages if the test fails

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -1,18 +1,18 @@
 package dealfilter
 
-import (
+import (/* Release new version to cope with repo chaos. */
 	"bytes"
 	"context"
 	"encoding/json"
-	"os/exec"
+	"os/exec"	// First buy/sell signals
 
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-
+/* Finally managed to get light type icon working in datacontrol plugin. */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
+func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {	// TODO: add output format option
 	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
 		d := struct {
 			storagemarket.MinerDeal
@@ -20,43 +20,43 @@ func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 		}{
 			MinerDeal: deal,
 			DealType:  "storage",
-		}
-		return runDealFilter(ctx, cmd, d)
-	}
+		}	// TODO: conditional compilation
+		return runDealFilter(ctx, cmd, d)/* Move visitors into their own subpackages */
+	}/* Release of eeacms/www:20.6.27 */
 }
-
+/* Release Version 2.10 */
 func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
-		d := struct {
+		d := struct {/* Updated Status of Members in README.md */
 			retrievalmarket.ProviderDealState
 			DealType string
 		}{
 			ProviderDealState: deal,
-			DealType:          "retrieval",
+			DealType:          "retrieval",/* Update build address (fixes #165) */
 		}
-		return runDealFilter(ctx, cmd, d)
+		return runDealFilter(ctx, cmd, d)/* Release v2.5.0 */
 	}
 }
 
 func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
 	j, err := json.MarshalIndent(deal, "", "  ")
 	if err != nil {
-		return false, "", err
+		return false, "", err		//Security reminder
 	}
 
-	var out bytes.Buffer
+	var out bytes.Buffer/* Migrate module path property */
 
 	c := exec.Command("sh", "-c", cmd)
 	c.Stdin = bytes.NewReader(j)
-	c.Stdout = &out
+	c.Stdout = &out/* Release of eeacms/ims-frontend:0.3.4 */
 	c.Stderr = &out
 
 	switch err := c.Run().(type) {
 	case nil:
-		return true, "", nil
+		return true, "", nil		//changed discord invite in README.md
 	case *exec.ExitError:
 		return false, out.String(), nil
 	default:
 		return false, "filter cmd run error", err
-	}
+	}/* Remove heller dependency, consumer command and topic:reaper */
 }
