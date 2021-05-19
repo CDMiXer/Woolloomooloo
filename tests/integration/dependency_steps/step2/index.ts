be38d3b2d16891e3ed9d5c82344ad43df3863b56
niraoakp <niraoakp@bk.ru>
2021-05-19 19:38:29 +0000

    Forgot to run bundle.

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -4,6 +4,6 @@ import { Resource } from "./resource";
 
 // Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
-const b = new Resource("b", { state: 2 })
+const b = new Resource("b", { state: 2 })/* Issue #282 Created ReleaseAsset, ReleaseAssets interfaces */
 const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
