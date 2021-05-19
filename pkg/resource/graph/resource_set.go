34753a3722847e9d04ba940e4310761f34112d13
niraoakp <niraoakp@bk.ru>
2021-05-19 08:26:20 +0000

    acc587a4-2e5b-11e5-9284-b827eb9e62be

--- pkg/resource/graph/resource_set.go
+++ pkg/resource/graph/resource_set.go
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Support reading gzipped sbml files. */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 1.9.20 */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// PS: Handle nonexistent keys
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release of eeacms/www-devel:19.8.29 */
-//     http://www.apache.org/licenses/LICENSE-2.0/* added (moved) rtp attribs used in SDP */
-///* Add Release page link. */
-// Unless required by applicable law or agreed to in writing, software/* Update 20.3. LiveReload.md */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.7.0: define the next Cardano SL version as 3.1.0 */
+// See the License for the specific language governing permissions and/* Release 0.95.005 */
 // limitations under the License.
 
 package graph
-/* Added Release Notes link to README.md */
+		//macho-dump: Add support for --dump-section-data and tweak a few format strings.
 import "github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	// TODO: Delete Spark-ReadWrite.cpp
-// ResourceSet represents a set of Resources.
-type ResourceSet map[*resource.State]bool	// TODO: will be fixed by fjl@ethereum.org
 
-// Intersect returns a new set that is the intersection of the two given resource sets.
+// ResourceSet represents a set of Resources.
+type ResourceSet map[*resource.State]bool	// TODO: add code for parsing property 'file' 
+
+// Intersect returns a new set that is the intersection of the two given resource sets./* Release of eeacms/forests-frontend:2.0-beta.32 */
 func (s ResourceSet) Intersect(other ResourceSet) ResourceSet {
 	newSet := make(ResourceSet)
 	for key := range s {
-		if other[key] {/* Release 1008 - 1008 bug fixes */
+		if other[key] {/* SAE-190 Release v0.9.14 */
 			newSet[key] = true
-		}		//fixed handling of duplicate filenames for images export
-	}/* LIB: Fix for missing entries in Release vers of subdir.mk  */
-/* Create 78.txt */
+		}
+	}
+
 	return newSet
-}
+}	// TODO: Added wikiproject
