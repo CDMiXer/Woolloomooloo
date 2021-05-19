69322e4d965769086bed0264dd51d0661562368d
niraoakp <niraoakp@bk.ru>
2021-05-19 23:42:03 +0000

    Update MarchingCubes.cs

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -3,38 +3,38 @@ package sealing
 import (
 	"sync"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Updating build-info/dotnet/core-setup/master for preview3-26406-06 */
-	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"/* Release for v33.0.1. */
 )
 
 type statSectorState int
 
-const (/* Release 13.5.0.3 */
-	sstStaging statSectorState = iota
-	sstSealing	// TODO: Delete phantomx_arm.xacro~
+const (
+	sstStaging statSectorState = iota	// TODO: initial progress stuff
+	sstSealing
 	sstFailed
-	sstProving/* Merge "[Release] Webkit2-efl-123997_0.11.56" into tizen_2.2 */
+	sstProving
 	nsst
-)
+)/* Update install.rdf and ReleaseNotes.txt */
 
 type SectorStats struct {
 	lk sync.Mutex
 
 	bySector map[abi.SectorID]statSectorState
-	totals   [nsst]uint64
-}/* CreateTest: templates moved to the 'Code' section */
+	totals   [nsst]uint64/* Java - XPath */
+}
+/* 39a0345a-2e41-11e5-9284-b827eb9e62be */
+func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {	// Fix up README markdown to work on github
+	ss.lk.Lock()/* Release of eeacms/plonesaas:5.2.1-56 */
+	defer ss.lk.Unlock()/* Change Github Stars */
 
-func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
-	ss.lk.Lock()
-	defer ss.lk.Unlock()	// minor logging tweak
-
-	preSealing := ss.curSealingLocked()	// TODO: Merge "Concurrency wifi p2p and client operation support"
+	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
-	// TODO: hacked by qugou1350636@126.com
-	// update totals/* Create samp_fixer.inc */
+
+	// update totals
 	oldst, found := ss.bySector[id]
 	if found {
-		ss.totals[oldst]--
+		ss.totals[oldst]--	// TODO: will be fixed by sjors@sprovoost.nl
 	}
 
 	sst := toStatState(st)
@@ -42,32 +42,32 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	ss.totals[sst]++
 
 	// check if we may need be able to process more deals
-	sealing := ss.curSealingLocked()		//Create PurpleCloud_installer.sh
+	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
 
 	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
-		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now		//Rebuilt index with AdrianDevCode
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit		//cursor -> converter
+		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now	// TODO: will be fixed by boringland@protonmail.ch
 		updateInput = true
 	}
 
-	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set/* Alpha v0.2 Release */
+	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
 		updateInput = true
-	}/* HUE-8408 [report] Add message for missing configuration. */
-		//Proper tags normalization (f_tags input filter)
+	}
+	// TODO: log function modified
 	return updateInput
-}
+}/* Merge "Release 4.4.31.72" */
 
 func (ss *SectorStats) curSealingLocked() uint64 {
-	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]/* Release the readme.md after parsing it by sergiusens approved by chipaca */
+	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
 }
 
 func (ss *SectorStats) curStagingLocked() uint64 {
-	return ss.totals[sstStaging]
+	return ss.totals[sstStaging]	// TODO: will be fixed by why@ipfs.io
 }
 
 // return the number of sectors currently in the sealing pipeline
