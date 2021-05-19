ed89db4796107dfb00082026fe736dce53bd1a46
niraoakp <niraoakp@bk.ru>
2021-05-19 15:53:07 +0000

    First planning of repo structure in readme

--- grpc-go/examples/features/encryption/ALTS/client/main.go
+++ grpc-go/examples/features/encryption/ALTS/client/main.go
@@ -1,43 +1,43 @@
 /*
- *	// TODO: will be fixed by why@ipfs.io
- * Copyright 2018 gRPC authors.	// TODO: Add language-1c-bsl as project using grammar test
- *	// TODO: will be fixed by witek@enjin.io
+ *
+ * Copyright 2018 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Create gitkeys */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//So many slots! So many colors!
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//cleanup test_add_node_set
+ *
  */
 
-// Binary client is an example client.		//updated README for development mode
+// Binary client is an example client.
 package main
 
 import (
 	"context"
-	"flag"/* New Release info. */
+	"flag"
 	"fmt"
 	"log"
 	"time"
-	// Demo style changes
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"/* First Release - 0.1.0 */
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)		//converter fixes
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
-	if err != nil {/* Merge "[INTERNAL] sap.m.GenericTile: Remove using keyboard implemented" */
+	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
@@ -52,11 +52,11 @@ func main() {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)	// TODO: will be fixed by lexy8russo@outlook.com
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
-	// Make a echo client and send an RPC.	// Added outout example image
+	// Make a echo client and send an RPC.
 	rgc := ecpb.NewEchoClient(conn)
 	callUnaryEcho(rgc, "hello world")
 }
