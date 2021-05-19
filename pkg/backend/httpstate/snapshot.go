21872a76377d6578a75134a466c941e560d49a80
niraoakp <niraoakp@bk.ru>
2021-05-19 01:21:35 +0000

    default storage size set to 1

--- pkg/backend/httpstate/snapshot.go
+++ pkg/backend/httpstate/snapshot.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release the version 1.2.0 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Add Really-Smart-Mailbox.groovy to Repo
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,19 +16,19 @@ package httpstate
 
 import (
 	"context"
-/* Merge "Release 1.0.0.177 QCACLD WLAN Driver" */
-	"github.com/pkg/errors"/* Release: 0.95.006 */
-	"github.com/pulumi/pulumi/pkg/v2/backend"	// TODO: #2437 adding example editor which open files in phoebus
+
+	"github.com/pkg/errors"
+	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate/client"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"		//added pagination to the html renderer
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 )
 
 // cloudSnapshotPersister persists snapshots to the Pulumi service.
 type cloudSnapshotPersister struct {
 	context     context.Context         // The context to use for client requests.
-	update      client.UpdateIdentifier // The UpdateIdentifier for this update sequence.	// TODO: 87a3cb1e-2e62-11e5-9284-b827eb9e62be
+	update      client.UpdateIdentifier // The UpdateIdentifier for this update sequence.
 	tokenSource *tokenSource            // A token source for interacting with the service.
 	backend     *cloudBackend           // A backend for communicating with the service
 	sm          secrets.Manager
@@ -55,9 +55,9 @@ var _ backend.SnapshotPersister = (*cloudSnapshotPersister)(nil)
 func (cb *cloudBackend) newSnapshotPersister(ctx context.Context, update client.UpdateIdentifier,
 	tokenSource *tokenSource, sm secrets.Manager) *cloudSnapshotPersister {
 	return &cloudSnapshotPersister{
-		context:     ctx,/* Update get_alreadytrained.sh */
+		context:     ctx,
 		update:      update,
-		tokenSource: tokenSource,	// TODO: will be fixed by lexy8russo@outlook.com
+		tokenSource: tokenSource,
 		backend:     cb,
 		sm:          sm,
 	}
