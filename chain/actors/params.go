614aa9dc48cee4239fe3c3bbbb466372554a2c4a
niraoakp <niraoakp@bk.ru>
2021-05-20 01:24:42 +0000

    d07177f0-2fbc-11e5-b64f-64700227155b

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
 
-import (	// TODO: will be fixed by davidad@alum.mit.edu
-	"bytes"/* Rebuilt index with jiroken */
+import (/* Released v.1.1 */
+	"bytes"	// ICL12 projects: cleanup and move all common properties in common_icl12.props
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"		//edit oundur twitter username
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
-	buf := new(bytes.Buffer)
-	if err := i.MarshalCBOR(buf); err != nil {		//Added missing from
+	buf := new(bytes.Buffer)	// TODO: Justinfan Release
+	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}		//Added support for the page parameters in the collectionApi objects
+	}
 	return buf.Bytes(), nil
-}
+}/* SEMPERA-2846 Release PPWCode.Util.OddsAndEnds 2.3.0 */
