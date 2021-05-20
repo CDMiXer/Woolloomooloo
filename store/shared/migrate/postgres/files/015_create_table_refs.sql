6de21da9eed05105ea85d4d80ed974cf16650ffc
niraoakp <niraoakp@bk.ru>
2021-05-20 22:11:11 +0000

    Agregado código para widget de selección de archivos

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,14 +1,14 @@
 -- name: create-table-latest
-/* Release type and status should be in lower case. (#2489) */
+
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)	// Went full inception
+,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER/* * Changed layout */
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// TODO: will be fixed by 13860583249@yeah.net
+,latest_deleted  INTEGER
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
 -- name: create-index-latest-repo
