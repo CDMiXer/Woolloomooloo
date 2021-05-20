ad29fc9a8f868cca24c30e728a82cf2527939e28
niraoakp <niraoakp@bk.ru>
2021-05-20 08:17:04 +0000

    Correct Feature Request

--- handler/health/health.go
+++ handler/health/health.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.		//reorganize package path for mac addr editor.
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Change order of logos for Suluh-INOVASI branding */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,27 +11,27 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Create Valgrind suppression file for library memory issues. */
+
 package health
-		//C++ logo only
+
 import (
 	"io"
 	"net/http"
-		//Fix focus/blur issues with markable regions.
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
-		//Tweaked error message and removed assert.
+
 // New returns a new health check router.
 func New() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.NoCache)/* 23062400-2f67-11e5-8fcd-6c40088e03e4 */
+	r.Use(middleware.NoCache)
 	r.Handle("/", Handler())
-	return r/* Changed key for the "New" dialog to "RSS Update Feed". */
+	return r
 }
-	// TODO: Format dates for statistics
-// Handler creates an http.HandlerFunc that performs system/* Release Tag for version 2.3 */
+
+// Handler creates an http.HandlerFunc that performs system
 // healthchecks and returns 500 if the system is in an unhealthy state.
 func Handler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
