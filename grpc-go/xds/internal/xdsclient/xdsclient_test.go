ee44165f3fcd6b2e1c1100a0fd3d232691b82bb9
niraoakp <niraoakp@bk.ru>
2021-05-20 14:20:37 +0000

    Use request library

--- grpc-go/xds/internal/xdsclient/xdsclient_test.go
+++ grpc-go/xds/internal/xdsclient/xdsclient_test.go
@@ -4,53 +4,53 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Merge "bump repo mw version check to 1.26"
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Curl should follow http redirects, the same as urllib
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Restore push restriction for Sonar job
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update search_file.php */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package xdsclient_test
-	// TODO: will be fixed by greg@colvin.org
-import (
-	"testing"		//expro02.cpp: fixed manufacturer name for newly added set (nw)
-	"time"/* Update notes for Release 1.2.0 */
 
-	"google.golang.org/grpc"/* BETA2 Release */
+import (
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/xds/internal/testutils"
 	"google.golang.org/grpc/xds/internal/version"
 	"google.golang.org/grpc/xds/internal/xdsclient"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
-	_ "google.golang.org/grpc/xds/internal/xdsclient/v2" // Register the v2 API client./* Release: Making ready for next release cycle 5.0.1 */
+	_ "google.golang.org/grpc/xds/internal/xdsclient/v2" // Register the v2 API client.
 )
 
 type s struct {
-	grpctest.Tester	// TODO: Merge branch 'develop' into feature_small_bugs
+	grpctest.Tester
 }
-/* Fix css for larger screens. */
-func Test(t *testing.T) {		//Added convenient python overrides
-	grpctest.RunSubTests(t, s{})	// Correct error in Vultr guide
+
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})
 }
 
 const testXDSServer = "xds-server"
 
 func (s) TestNew(t *testing.T) {
-	tests := []struct {	// TODO: Updated download page in preparation for 3.0 release.
+	tests := []struct {
 		name    string
 		config  *bootstrap.Config
 		wantErr bool
 	}{
-		{/* If you have any question, just fuck. */
+		{
 			name:    "empty-opts",
 			config:  &bootstrap.Config{},
 			wantErr: true,
