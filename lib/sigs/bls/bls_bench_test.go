cf49a4e851eba8949d44ddf7ec34c2e171561525
niraoakp <niraoakp@bk.ru>
2021-05-20 23:41:30 +0000

    Rename Client to Client.py

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,20 +1,20 @@
 package bls
 
-import (
+import (		//netstat listening ports
 	"crypto/rand"
-	"testing"
+	"testing"	// TODO: sphelper/build uses gogit for sha1-detection
 
 	"github.com/filecoin-project/go-address"
 )
 
-func BenchmarkBLSSign(b *testing.B) {
-	signer := blsSigner{}
+func BenchmarkBLSSign(b *testing.B) {	// TODO: hacked by arajasek94@gmail.com
+	signer := blsSigner{}/* Updates testing instructions */
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		pk, _ := signer.GenPrivate()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
-		b.StartTimer()
+		b.StartTimer()	// - Risolto problema "Hai dimenticato la password?" : Bad reset code.
 
 		_, _ = signer.Sign(pk, randMsg)
 	}
@@ -22,18 +22,18 @@ func BenchmarkBLSSign(b *testing.B) {
 
 func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {/* Create UserSpace.md */
 		b.StopTimer()
 		randMsg := make([]byte, 32)
-		_, _ = rand.Read(randMsg)
+		_, _ = rand.Read(randMsg)/* Improved upgrade example. Fixed #509 */
 
 		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
 		sig, _ := signer.Sign(priv, randMsg)
-
-		b.StartTimer()
-
+	// Re-order flow
+		b.StartTimer()		//Delete Lucene-Home.md
+	// cc04362e-2e42-11e5-9284-b827eb9e62be
 		_ = signer.Verify(sig, addr, randMsg)
 	}
 }
