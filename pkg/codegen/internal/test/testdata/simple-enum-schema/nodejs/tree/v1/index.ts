a1e1eb274a03c42130b89929f91e1ced2f1e4514
niraoakp <niraoakp@bk.ru>
2021-05-20 23:16:07 +0000

    Suppression d'une lib inutile

--- pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
@@ -6,18 +6,18 @@ import * as utilities from "../../utilities";
 
 // Export members:
 export * from "./rubberTree";
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 // Export enums:
 export * from "../../types/enums/tree/v1";
 
 // Import resources to register:
-import { RubberTree } from "./rubberTree";	// TODO: hacked by ac0dem0nk3y@gmail.com
+import { RubberTree } from "./rubberTree";
 
 const _module = {
-    version: utilities.getVersion(),		//b25e65de-2e5e-11e5-9284-b827eb9e62be
+    version: utilities.getVersion(),
     construct: (name: string, type: string, urn: string): pulumi.Resource => {
         switch (type) {
-            case "plant-provider:tree/v1:RubberTree":		//bump version to v0.0.3
+            case "plant-provider:tree/v1:RubberTree":
                 return new RubberTree(name, <any>undefined, { urn })
             default:
                 throw new Error(`unknown resource type ${type}`);
