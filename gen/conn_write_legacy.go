c3a2c2f5b0d73bd6e6d118023e6c7065770b865f
niraoakp <niraoakp@bk.ru>
2021-05-20 16:21:13 +0000

    Create vminterface.py

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,6 +1,6 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Rough cut at Debates functionality. */
+// license that can be found in the LICENSE file.
 
 // +build !go1.8
 
