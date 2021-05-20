929efb34ef97eaa3fc18d61c416c4b697ee65542
niraoakp <niraoakp@bk.ru>
2021-05-20 19:42:24 +0000

    Add RxJava Operator

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,10 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
-/* url cleanup */
+
 // Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
 // with secret configuration values, so long as these values are themselves strings.
 class DynamicProvider extends pulumi.ProviderResource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);/* Release of eeacms/www:20.8.23 */
+    constructor(name: string, opts?: pulumi.ResourceOptions) {/* 0f3f53ae-2e5e-11e5-9284-b827eb9e62be */
+        super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
     }
 }
 
