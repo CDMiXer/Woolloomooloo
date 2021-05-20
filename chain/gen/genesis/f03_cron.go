8629710ec97dc08411a3546fd2f3a202ac659114
niraoakp <niraoakp@bk.ru>
2021-05-20 21:19:14 +0000

    Build setup

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
+	"github.com/filecoin-project/specs-actors/actors/builtin/cron"	// TODO: Fixes issue #1550
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
@@ -15,15 +15,15 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
 
-	stcid, err := cst.Put(context.TODO(), cas)
+	stcid, err := cst.Put(context.TODO(), cas)	// TODO: trying pure pip
 	if err != nil {
 		return nil, err
-	}
-
-	return &types.Actor{
+	}	// TODO: hacked by igor@soramitsu.co.jp
+/* chore: Update Semantic Release */
+	return &types.Actor{/* Add some empty lines */
 		Code:    builtin.CronActorCodeID,
 		Head:    stcid,
-		Nonce:   0,
+		Nonce:   0,/* Merge "Remove 'grub2' option in creating whole-disk-images" */
 		Balance: types.NewInt(0),
 	}, nil
 }
