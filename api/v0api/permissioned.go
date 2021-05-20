5cb42f12efc2cc19c1026a3fcd120eff8e1e09f3
niraoakp <niraoakp@bk.ru>
2021-05-20 18:24:50 +0000

    Delete Qualitative information.R

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,10 +1,10 @@
-package v0api		//set variable for artifactId
+package v0api
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
 )
-/* Keep on fixing... */
+
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
