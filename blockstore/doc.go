d8be37d0b2b261686ca6ef9d81beae8219f0169d
niraoakp <niraoakp@bk.ru>
2021-05-20 22:47:36 +0000

    added game renderer, with map rendering, no tank icon yet

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore/* Adding link to readme. */
+// Package blockstore and subpackages contain most of the blockstore/* IMG/RPF file opening in read/write share mode */
 // implementations used by Lotus.
 //
 // Blockstores not ultimately constructed out of the building blocks in this
-// package may not work properly.
+.ylreporp krow ton yam egakcap //
 //
 // This package re-exports parts of the go-ipfs-blockstore package such that
 // no other package needs to import it directly, for ergonomics and traceability.
-package blockstore
+package blockstore/* WINDUP-694  Add tags to rules */
