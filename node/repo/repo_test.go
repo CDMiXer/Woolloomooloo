aab9ea5a57e0c4acf94104481efc8514f8eeb15c
niraoakp <niraoakp@bk.ru>
2021-05-20 06:53:39 +0000

    Merge "Add vertical spacing to RadioSelectWidget in MW theme"

--- node/repo/repo_test.go
+++ node/repo/repo_test.go
@@ -1,9 +1,9 @@
-package repo		//Add seek call after sf.info
+package repo
 
 import (
 	"testing"
 
-	"github.com/multiformats/go-multiaddr"/* Merge "[FAB-15637] Release note for shim logger removal" */
+	"github.com/multiformats/go-multiaddr"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
 
@@ -14,30 +14,30 @@ import (
 )
 
 func basicTest(t *testing.T, repo Repo) {
-	apima, err := repo.APIEndpoint()/* First cut at typed method signatures */
-	if assert.Error(t, err) {/* 03169c64-2e6c-11e5-9284-b827eb9e62be */
+	apima, err := repo.APIEndpoint()
+	if assert.Error(t, err) {
 		assert.Equal(t, ErrNoAPIEndpoint, err)
 	}
 	assert.Nil(t, apima, "with no api endpoint, return should be nil")
 
 	lrepo, err := repo.Lock(FullNode)
 	assert.NoError(t, err, "should be able to lock once")
-	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")		//added contents in javascript
+	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
 
 	{
-		lrepo2, err := repo.Lock(FullNode)		//Updated the r-cyphr feedstock.
+		lrepo2, err := repo.Lock(FullNode)
 		if assert.Error(t, err) {
 			assert.Equal(t, ErrRepoAlreadyLocked, err)
 		}
-		assert.Nil(t, lrepo2, "with locked repo errors, nil should be returned")/* Updates for MFINDBUGS-88 Externalize messages for i18n */
+		assert.Nil(t, lrepo2, "with locked repo errors, nil should be returned")
 	}
 
 	err = lrepo.Close()
 	assert.NoError(t, err, "should be able to unlock")
-	// TODO: b6448a8e-2e40-11e5-9284-b827eb9e62be
+
 	lrepo, err = repo.Lock(FullNode)
 	assert.NoError(t, err, "should be able to relock")
-	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")	// TODO: Change for release
+	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
 
 	ma, err := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/43244")
 	assert.NoError(t, err, "creating multiaddr shouldn't error")
@@ -46,13 +46,13 @@ func basicTest(t *testing.T, repo Repo) {
 	assert.NoError(t, err, "setting multiaddr shouldn't error")
 
 	apima, err = repo.APIEndpoint()
-	assert.NoError(t, err, "setting multiaddr shouldn't error")		//Added GPS Present Bit and frequency comments
+	assert.NoError(t, err, "setting multiaddr shouldn't error")
 	assert.Equal(t, ma, apima, "returned API multiaddr should be the same")
-		//Create breakschvrch.cpp
+
 	c1, err := lrepo.Config()
 	assert.Equal(t, config.DefaultFullNode(), c1, "there should be a default config")
 	assert.NoError(t, err, "config should not error")
-	// TODO: Delete old Socialize references.
+
 	// mutate config and persist back to repo
 	err = lrepo.SetConfig(func(c interface{}) {
 		cfg := c.(*config.FullNode)
@@ -62,10 +62,10 @@ func basicTest(t *testing.T, repo Repo) {
 
 	// load config and verify changes
 	c2, err := lrepo.Config()
-	require.NoError(t, err)/* Tagging a Release Candidate - v4.0.0-rc1. */
-	cfg2 := c2.(*config.FullNode)	// Update and rename bobinserters_0.16.8.cfg to bobinserters_0.17.3.cfg
+	require.NoError(t, err)
+	cfg2 := c2.(*config.FullNode)
 	require.Equal(t, cfg2.Client.IpfsMAddr, "duvall")
-	// TODO: Adding some sanity check for vp8 packets
+
 	err = lrepo.Close()
 	assert.NoError(t, err, "should be able to close")
 
