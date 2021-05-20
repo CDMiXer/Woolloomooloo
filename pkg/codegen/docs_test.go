bc397d818dc0e8176ada318d3d3d6dddbf0714c4
niraoakp <niraoakp@bk.ru>
2021-05-20 12:12:12 +0000

    Fixed some issues with away message encoding.

--- pkg/codegen/docs_test.go
+++ pkg/codegen/docs_test.go
@@ -4,58 +4,58 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Update group size value
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Restrict sky-clouds group, avoids skydome picks.
-// limitations under the License.		//Fix bug when searchBar was active singleSelection
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package codegen
 
 import (
-	"testing"		//Revert prior and add note why we use strerror().
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 const codeFence = "```"
-		//v.233 Incremented version
+
 func TestFilterExamples(t *testing.T) {
 	tsCodeSnippet := `### Example 1
 ` + codeFence + `typescript
-import * as path from path;/* byte indication added; */
-/* Release v1.0.0-beta2 */
+import * as path from path;
+
 console.log("I am a console log statement in ts.");
 ` + codeFence
 
-	goCodeSnippet := `\n` + codeFence + `go/* [deployment] fix Release in textflow */
+	goCodeSnippet := `\n` + codeFence + `go
 import (
 	"fmt"
 	"strings"
-)/* Upgrade to Android 4.0.1.2, ABS 4.0 RC1, and roboguice 2.0b3 */
+)
 
 func fakeFunc() {
 	fmt.Print("Hi, I am a fake func!")
-}	// TODO: compile 1.6
+}
 ` + codeFence
 
-	leadingDescription := "This is a leading description for this resource."/* Released springrestcleint version 2.4.6 */
+	leadingDescription := "This is a leading description for this resource."
 	exampleShortCode := `{{% example %}}` + tsCodeSnippet + "\n" + goCodeSnippet + `{{% /example %}}`
 	description := leadingDescription + `
 {{% examples %}}` + exampleShortCode + `
-{{% /examples %}}`	// TODO: hacked by brosner@gmail.com
+{{% /examples %}}`
 
 	t.Run("ContainsRelevantCodeSnippet", func(t *testing.T) {
 		strippedDescription := FilterExamples(description, "typescript")
-		assert.NotEmpty(t, strippedDescription, "content could not be extracted")	// TODO: [IMP] set button class
+		assert.NotEmpty(t, strippedDescription, "content could not be extracted")
 		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
-	})/* Releases link for changelog */
+	})
 
 	// The above description does not contain a Python code snippet and because
 	// the description contains only one Example without any Python code snippet,
-	// we should expect an empty string in this test.	// TODO: Generated site for typescript-generator-maven-plugin 1.26.334
+	// we should expect an empty string in this test.
 	t.Run("DoesNotContainRelevantSnippet", func(t *testing.T) {
 		strippedDescription := FilterExamples(description, "python")
 		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
