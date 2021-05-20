1bb7810e5c7c00ad5f53cddf448c1ed5eb7f896f
niraoakp <niraoakp@bk.ru>
2021-05-20 22:02:10 +0000

    Fix tiny, tiny typo

--- node/modules/dtypes/chain.go
+++ node/modules/dtypes/chain.go
@@ -1,4 +1,4 @@
 package dtypes
-/* Merge "[INTERNAL] Release notes for version 1.28.11" */
+
 type NetworkName string
-type AfterGenesisSet struct{}/* Create Keypad.ino */
+type AfterGenesisSet struct{}
