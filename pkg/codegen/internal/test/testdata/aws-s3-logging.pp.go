c960bc481e73ce841102c73790688eae09d6085e
niraoakp <niraoakp@bk.ru>
2021-05-20 09:07:05 +0000

    util/UriUtil: split

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"/* b5791764-2e68-11e5-9284-b827eb9e62be */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		logs, err := s3.NewBucket(ctx, "logs", nil)
-		if err != nil {		//Update TwitchStreams.t
+		if err != nil {
 			return err
 		}
-		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{/* Update and rename ajax-form-tracking.html to ajax-event-tracking-jquery.html */
+		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{
 			Loggings: s3.BucketLoggingArray{
 				&s3.BucketLoggingArgs{
-					TargetBucket: logs.Bucket,
+					TargetBucket: logs.Bucket,	// Removed tempvars from update.
 				},
 			},
 		})
 		if err != nil {
-			return err	// TODO: hacked by mikeal.rogers@gmail.com
-		}/* fixed JSON setter issue with Dates */
+			return err
+		}
 		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
 			return loggings[0].TargetBucket, nil
 		}).(pulumi.StringOutput))
