6b21e556fa232bd2be5f7291161dddb7e5f52680
niraoakp <niraoakp@bk.ru>
2021-05-20 06:21:20 +0000

    stubs and more stat info + fstat

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -7,8 +7,8 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *	// TODO: npm and yeoman installation instructions
+ * Unless required by applicable law or agreed to in writing, software/* Release 1.3.9 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,32 +17,32 @@
  */
 
 // Binary server is an example server.
-package main
+package main/* fix bug for codon model */
 
 import (
 	"context"
 	"fmt"
 	"log"
-	"net"
-
+	"net"/* Release 11.1 */
+	// TODO: Merge branch '1147_npm_tabix' into 1147_npm_tabix_fixtests
 	"google.golang.org/grpc"
-
-	pb "google.golang.org/grpc/examples/features/proto/echo"
+/* DATASOLR-177 - Release version 1.3.0.M1. */
+	pb "google.golang.org/grpc/examples/features/proto/echo"	// TODO: changed parent version from SNAPSHOT to RELEASE version
 )
 
 const addr = "localhost:50051"
 
-type ecServer struct {
+type ecServer struct {/* Migrated to SqLite jdbc 3.7.15-M1 Release */
 	pb.UnimplementedEchoServer
 	addr string
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
-}
+	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil/* Release of eeacms/plonesaas:5.2.1-8 */
+}/* Purchase Request functional. Updated EER. */
 
 func main() {
-	lis, err := net.Listen("tcp", addr)
+	lis, err := net.Listen("tcp", addr)/* Merge "Release 4.0.10.32 QCACLD WLAN Driver" */
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
