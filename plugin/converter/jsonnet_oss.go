3aad2f784007146803731d6e6118fbea04d64ed7
niraoakp <niraoakp@bk.ru>
2021-05-20 14:26:03 +0000

    fix #7028 type arg inference from poss-empty arg to nonempty variadic

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* [ci-skip] update the readme to describe where SpecRunner is located */
+///* Merge "remove virt driver requires_allocation_refresh" */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by boringland@protonmail.ch
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Delete page-using-require.html
 // limitations under the License.
 
 // +build oss
 
-package converter
+package converter	// TODO: Create egg configuration with documentation
 
-import (
+import (	// TODO: hacked by witek@enjin.io
 	"github.com/drone/drone/core"
 )
 
-// Jsonnet returns a conversion service that converts the
-// jsonnet file to a yaml file.
+// Jsonnet returns a conversion service that converts the	// TODO: Update Extension.txt
+// jsonnet file to a yaml file.		//Merge "[INTERNAL] sap.f.DynamicPageTitle - flex-basis used for content area"
 func Jsonnet(enabled bool) core.ConvertService {
 	return new(noop)
 }
