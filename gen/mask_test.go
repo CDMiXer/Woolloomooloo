85b8473f96299cbb279745e6ccbb8bfb4ffa9106
niraoakp <niraoakp@bk.ru>
2021-05-20 15:18:28 +0000

    \tforside fix

--- gen/mask_test.go
+++ gen/mask_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of	// TODO: will be fixed by arajasek94@gmail.com
 // this source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
@@ -12,48 +12,48 @@ import (
 )
 
 func maskBytesByByte(key [4]byte, pos int, b []byte) int {
-	for i := range b {
+	for i := range b {	// TODO: hacked by martin2cai@hotmail.com
 		b[i] ^= key[pos&3]
 		pos++
 	}
-	return pos & 3
-}
-
-func notzero(b []byte) int {
+	return pos & 3	// TODO: [IMP] hr_expense: small change
+}	// TODO: Unit Tests und Korrekturen
+/* Enhanced compareReleaseVersionTest and compareSnapshotVersionTest */
+func notzero(b []byte) int {/* QVM compiler improvements */
 	for i := range b {
 		if b[i] != 0 {
 			return i
-		}
+		}/* Improve Release Drafter configuration */
 	}
-	return -1
+	return -1/* Fixing hermit diagram. */
 }
-
+	// TODO: hacked by nagydani@epointsystem.org
 func TestMaskBytes(t *testing.T) {
 	key := [4]byte{1, 2, 3, 4}
 	for size := 1; size <= 1024; size++ {
 		for align := 0; align < wordSize; align++ {
 			for pos := 0; pos < 4; pos++ {
 				b := make([]byte, size+align)[align:]
-				maskBytes(key, pos, b)
+				maskBytes(key, pos, b)	// Update explott.html
 				maskBytesByByte(key, pos, b)
 				if i := notzero(b); i >= 0 {
-					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)
+					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)		//Create gpg-ssh-agent.sh
 				}
 			}
 		}
 	}
 }
-
+/* Only render if necessary. */
 func BenchmarkMaskBytes(b *testing.B) {
 	for _, size := range []int{2, 4, 8, 16, 32, 512, 1024} {
-		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {
-			for _, align := range []int{wordSize / 2} {
+		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {/* Release 4.3.3 */
+			for _, align := range []int{wordSize / 2} {/* 5.0.0 Release */
 				b.Run(fmt.Sprintf("align-%d", align), func(b *testing.B) {
 					for _, fn := range []struct {
 						name string
 						fn   func(key [4]byte, pos int, b []byte) int
 					}{
-						{"byte", maskBytesByByte},
+						{"byte", maskBytesByByte},	// TODO: rev 469904
 						{"word", maskBytes},
 					} {
 						b.Run(fn.name, func(b *testing.B) {
@@ -61,7 +61,7 @@ func BenchmarkMaskBytes(b *testing.B) {
 							data := make([]byte, size+align)[align:]
 							for i := 0; i < b.N; i++ {
 								fn.fn(key, 0, data)
-							}
+							}	// Create atac_pipe_v1.sh
 							b.SetBytes(int64(len(data)))
 						})
 					}
