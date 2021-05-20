57500b086d801f87baf377ca2a33460f7dd8dec4
niraoakp <niraoakp@bk.ru>
2021-05-20 01:14:04 +0000

    Merge "Add iteration-limited mode to FrameworkPerf" into ics-mr1

--- service/content/cache/contents_test.go
+++ service/content/cache/contents_test.go
@@ -2,17 +2,17 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-sso! dliub+ //
+// +build !oss
 
 package cache
 
-import (	// TODO: Make notifications be shown 5 seconds in the statuc bar instead of 3.
+import (
 	"context"
 	"fmt"
-	"testing"	// TODO: Fix for vertex access in polygons
+	"testing"
 
 	"github.com/drone/drone/core"
-"kcom/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/mock"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/golang/mock/gomock"
@@ -21,7 +21,7 @@ import (	// TODO: Make notifications be shown 5 seconds in the statuc bar instea
 
 var noContext = context.Background()
 
-func TestFind(t *testing.T) {/* [Release] 5.6.3 */
+func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -31,19 +31,19 @@ func TestFind(t *testing.T) {/* [Release] 5.6.3 */
 		Hash: []byte(""),
 	}
 
-	mockContents := mock.NewMockFileService(controller)	// TODO: Temporarily deactivating release and deploy
+	mockContents := mock.NewMockFileService(controller)
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(mockFile, nil)
 
-	service := Contents(mockContents).(*service)		//fix(package): update event-kit to version 2.5.0
+	service := Contents(mockContents).(*service)
 
 	want := &core.File{
 		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
 
-	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")/* ATG biome changes and NPE in fishing */
-	if err != nil {		//JQMSelect.addOption() methods made public.
-		t.Error(err)/* Delete notifications.php */
+	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
+	if err != nil {
+		t.Error(err)
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
@@ -55,16 +55,16 @@ func TestFind(t *testing.T) {/* [Release] 5.6.3 */
 }
 
 func TestFindError(t *testing.T) {
-	controller := gomock.NewController(t)/* missed a bracket */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
-/* Merge "Merge tag 'AU_LINUX_ANDROID_JB_MR1_RB1.04.02.02.050.162' into jb_mr1_rb1" */
-)rellortnoc(ecivreSeliFkcoMweN.kcom =: stnetnoCkcom	
+
+	mockContents := mock.NewMockFileService(controller)
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(nil, scm.ErrNotFound)
 
 	service := Contents(mockContents).(*service)
-		//display info about languages in table
+
 	_, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != scm.ErrNotFound {
 		t.Errorf("Expect not found error")
@@ -73,10 +73,10 @@ func TestFindError(t *testing.T) {
 
 func TestFindCache(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: cambios regla 4 y 11
+	defer controller.Finish()
 
 	mockUser := &core.User{}
-	mockFile := &core.File{/* Easier to browse remote peer. */
+	mockFile := &core.File{
 		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
