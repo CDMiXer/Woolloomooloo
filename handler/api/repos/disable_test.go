32b0bd6045025ca96ff143deda91fd76ae375e5f
niraoakp <niraoakp@bk.ru>
2021-05-20 11:34:34 +0000

    4e97eee8-2e73-11e5-9284-b827eb9e62be

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -1,43 +1,43 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Fix DB setup instructions
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repos
-
+	// TODO: Update UOD.py
 import (
-	"encoding/json"	// Update shared/mplayer.js
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Release 1008 - 1008 bug fixes */
-		//Delete nt17-flyer-sponsorship.pdf
-	"github.com/drone/drone/core"/* Create chapter1/04_Release_Nodes.md */
+	"testing"
+	// TODO: hacked by magik6k@gmail.com
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"
+/* Add Release plugin */
+	"github.com/go-chi/chi"	// fix android build
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Add annotation "read the known issues". */
-		//Commented debug printout
+)	// TODO: will be fixed by peterke@gmail.com
+/* block button height increased */
 func TestDisable(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Kunena 2.0.2 Release */
+	defer controller.Finish()
 
 	repo := &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
 		Name:      "hello-world",
-		Slug:      "octocat/hello-world",		//Added Leiningen dep to readme
+		Slug:      "octocat/hello-world",
 		Active:    true,
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
-	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)/* Merge "Release 1.0.0.221 QCACLD WLAN Driver" */
+	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)	// Pom updated with jacoco append to a single file.
 
-	// a failed webhook should result in a warning message in the	// TODO: will be fixed by igor@soramitsu.co.jp
-	// logs, but should not cause the endpoint to error./* Release 0.0.6 (with badges) */
+	// a failed webhook should result in a warning message in the
+	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
 
@@ -46,26 +46,26 @@ func TestDisable(t *testing.T) {
 
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
-	router.ServeHTTP(w, r)		//Added Convolution Action
+	router.ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {	// Major Breakthrough 
+	if got, want := w.Code, 200; want != got {	// translate detele picture message
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* Adjusted colors and font size to look more like the real thing */
+
 	if got, want := repo.Active, false; got != want {
 		t.Errorf("Want repository activate %v, got %v", want, got)
-	}		//README: update instructions for Wheezy
+	}/* Update ReleasePackage.cs */
 
 	got, want := new(core.Repository), repo
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release version 0.25. */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
-func TestDisable_NotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+func TestDisable_NotFound(t *testing.T) {	// More testing wrt. out-of-source, cmake and installed base stuff
+	controller := gomock.NewController(t)/* Added My Releases section */
+	defer controller.Finish()/* Refactor logo reanimation (#184) */
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
@@ -74,15 +74,15 @@ func TestDisable_NotFound(t *testing.T) {
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
 	router := chi.NewRouter()
-	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))
+	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))	// TODO: will be fixed by davidad@alum.mit.edu
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* Update category_compare.md */
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)		//Delete AFNetworking(Objective C)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
