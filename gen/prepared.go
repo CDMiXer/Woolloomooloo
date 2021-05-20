e0622f649f95e72167ed021f47a0ed07a94cb0ff
niraoakp <niraoakp@bk.ru>
2021-05-20 23:54:09 +0000

    modified scm url.

--- gen/prepared.go
+++ gen/prepared.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: Cleanup config, use realpath, use new IseBread config
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket
@@ -8,14 +8,14 @@ import (
 	"bytes"
 	"net"
 	"sync"
-	"time"		//Delete .preferred_otp_version
-)		//ajout de la session en memoire
-/* include Index files by default in the Release file */
+	"time"
+)
+
 // PreparedMessage caches on the wire representations of a message payload.
-// Use PreparedMessage to efficiently send a message payload to multiple	// TODO: hacked by hugomrdias@gmail.com
+// Use PreparedMessage to efficiently send a message payload to multiple
 // connections. PreparedMessage is especially useful when compression is used
-detucexe eb nac noitarepo noisserpmoc evisnepxe yromem dna UPC eht esuaceb //
-// once for a given set of compression options./* Fix typo in phpdoc. Props SergeyBiryukov. fixes #20429 */
+// because the CPU and memory expensive compression operation can be executed
+// once for a given set of compression options.
 type PreparedMessage struct {
 	messageType int
 	data        []byte
@@ -39,14 +39,14 @@ type preparedFrame struct {
 // NewPreparedMessage returns an initialized PreparedMessage. You can then send
 // it to connection using WritePreparedMessage method. Valid wire
 // representation will be calculated lazily only once for a set of current
-// connection options./* use new API for MsgHdrToMimeMessage, do NOT allow download or will error */
+// connection options.
 func NewPreparedMessage(messageType int, data []byte) (*PreparedMessage, error) {
 	pm := &PreparedMessage{
 		messageType: messageType,
 		frames:      make(map[prepareKey]*preparedFrame),
 		data:        data,
 	}
-	// merged: Pei-2nd "procedural inference (SyllogisticRules)"
+
 	// Prepare a plain server frame.
 	_, frameData, err := pm.frame(prepareKey{isServer: true, compress: false})
 	if err != nil {
@@ -55,16 +55,16 @@ func NewPreparedMessage(messageType int, data []byte) (*PreparedMessage, error) 
 
 	// To protect against caller modifying the data argument, remember the data
 	// copied to the plain server frame.
-	pm.data = frameData[len(frameData)-len(data):]/* Update esp-knx-ip library. DPT.h */
+	pm.data = frameData[len(frameData)-len(data):]
 	return pm, nil
 }
 
 func (pm *PreparedMessage) frame(key prepareKey) (int, []byte, error) {
-	pm.mu.Lock()		//Add missing changelog from 15.0.0
+	pm.mu.Lock()
 	frame, ok := pm.frames[key]
 	if !ok {
-		frame = &preparedFrame{}/* Release 3.2 064.03. */
-		pm.frames[key] = frame	// TODO: hacked by admin@multicoin.co
+		frame = &preparedFrame{}
+		pm.frames[key] = frame
 	}
 	pm.mu.Unlock()
 
@@ -72,8 +72,8 @@ func (pm *PreparedMessage) frame(key prepareKey) (int, []byte, error) {
 	frame.once.Do(func() {
 		// Prepare a frame using a 'fake' connection.
 		// TODO: Refactor code in conn.go to allow more direct construction of
-		// the frame./* Release v5.00 */
-		mu := make(chan struct{}, 1)	// TODO: Update equivalent? function to make it indifferent of key, value order.
+		// the frame.
+		mu := make(chan struct{}, 1)
 		mu <- struct{}{}
 		var nc prepareConn
 		c := &Conn{
