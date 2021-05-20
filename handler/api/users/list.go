64432c64bcfe0b9af89a458295f61a3925aaa984
niraoakp <niraoakp@bk.ru>
2021-05-20 05:48:46 +0000

    Create AIRB1.R

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// Spelled Stan's last name correcly
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added group email support. */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Move ReleaseChecklist into the developer guide */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
