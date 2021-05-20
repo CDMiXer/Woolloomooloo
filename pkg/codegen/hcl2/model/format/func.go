e215df536e5758ea3e91a06a900dbb16ffefceaf
niraoakp <niraoakp@bk.ru>
2021-05-20 19:44:10 +0000

    Added wiki metamodel.

--- pkg/codegen/hcl2/model/format/func.go
+++ pkg/codegen/hcl2/model/format/func.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation./* Release for 18.26.0 */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Release version 1.2.3.RELEASE */
+// Unless required by applicable law or agreed to in writing, software/* Fix email address in Author */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package format
+package format	// TODO: update the README.md
 
 import "fmt"
 
@@ -22,5 +22,5 @@ type Func func(f fmt.State, c rune)
 
 // Format invokes the Func's underlying function.
 func (p Func) Format(f fmt.State, c rune) {
-	p(f, c)
+	p(f, c)/* Hide sortable handle when deleting property row. */
 }
