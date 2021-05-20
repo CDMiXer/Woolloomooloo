c2e745897e5a71ac3887dd890f73ca25834bc721
niraoakp <niraoakp@bk.ru>
2021-05-20 20:40:38 +0000

    Moar validation on the facets and construction.

--- handler/api/repos/collabs/find_test.go
+++ handler/api/repos/collabs/find_test.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by souzau@yandex.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Allow importing the Release 18.5.00 (2nd Edition) SQL ref. guide */
-sso! dliub+ //
-/* (vila) Release 2.3.0 (Vincent Ladeuil) */
+
+// +build !oss
+
 package collabs
 
 import (
@@ -15,30 +15,30 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"		//Affichage des contributions "potable"
-	"github.com/drone/drone/mock"/* MyPyPrefPage extends ScopedFieldEditorPreferencePage and add save field */
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Release Notes for v02-11 */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Polish: constify new local variables */
-/* Update NumericalMath.jl */
+)
+
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)/* Change Qt4 to Qt4+5 in readme */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
 	perms := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
-	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)	// TODO: will be fixed by witek@enjin.io
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	perms.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
-		//[FIX] l10n_cr_hr_payroll: Modified translations
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
@@ -46,19 +46,19 @@ func TestFind(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(	// Prepare for Laravel 6 & Bump PHP Ver
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleFind(users, repos, perms)(w, r)		//Simplified the constructor
+	HandleFind(users, repos, perms)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Release of eeacms/eprtr-frontend:0.4-beta.1 */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Perm{}, mockMember
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)	// Merge "Blacklist call of ssl._create_unverified_context"
+		t.Errorf(diff)
 	}
 }
 
