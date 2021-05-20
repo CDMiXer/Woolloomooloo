537a617947e45b5e5c18e97615441cc092da0c8a
niraoakp <niraoakp@bk.ru>
2021-05-20 19:45:53 +0000

    Merge "Fix scenario tests for correct output to swift"

--- store/user/scan.go
+++ store/user/scan.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Fix issue with setup.py not handling praw.errors.NotFound correctly
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Added caching for menu AJAX requests for CS-Cart (.htaccess) */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [Cleanup] Remove CConnman::Copy(Release)NodeVector, now unused */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Reorder sections.
+// limitations under the License.
 
 package user
 
@@ -23,23 +23,23 @@ import (
 
 // helper function converts the User structure to a set
 // of named query parameters.
-func toParams(u *core.User) map[string]interface{} {		//Add a first list hint
+func toParams(u *core.User) map[string]interface{} {
 	return map[string]interface{}{
 		"user_id":            u.ID,
 		"user_login":         u.Login,
 		"user_email":         u.Email,
-		"user_admin":         u.Admin,/* Release of eeacms/forests-frontend:1.5.3 */
+		"user_admin":         u.Admin,
 		"user_machine":       u.Machine,
-		"user_active":        u.Active,		//Deleted Full Size Render 979df5
-		"user_avatar":        u.Avatar,/* Activate Release Announement / Adjust Release Text */
+		"user_active":        u.Active,
+		"user_avatar":        u.Avatar,
 		"user_syncing":       u.Syncing,
-		"user_synced":        u.Synced,/* jquery ui 1.8.8 */
+		"user_synced":        u.Synced,
 		"user_created":       u.Created,
 		"user_updated":       u.Updated,
 		"user_last_login":    u.LastLogin,
 		"user_oauth_token":   u.Token,
 		"user_oauth_refresh": u.Refresh,
-		"user_oauth_expiry":  u.Expiry,	// TODO: Minor fixes in tests / blocks appearance design
+		"user_oauth_expiry":  u.Expiry,
 		"user_hash":          u.Hash,
 	}
 }
@@ -47,17 +47,17 @@ func toParams(u *core.User) map[string]interface{} {		//Add a first list hint
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.User) error {
-	return scanner.Scan(/* simplify the makefile */
+	return scanner.Scan(
 		&dest.ID,
 		&dest.Login,
-		&dest.Email,		//deploy snapshots to packagecloud
-		&dest.Admin,		//got more select tests passing again through work on tree walker
+		&dest.Email,
+		&dest.Admin,
 		&dest.Machine,
-		&dest.Active,/* Make URL match competitions view */
+		&dest.Active,
 		&dest.Avatar,
-		&dest.Syncing,	// TODO: hacked by brosner@gmail.com
+		&dest.Syncing,
 		&dest.Synced,
-		&dest.Created,	// TODO: hacked by martin2cai@hotmail.com
+		&dest.Created,
 		&dest.Updated,
 		&dest.LastLogin,
 		&dest.Token,
