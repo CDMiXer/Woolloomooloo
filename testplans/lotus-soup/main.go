9f98c6a352374b6287e5ab99d6b153ceb5443f3c
niraoakp <niraoakp@bk.ru>
2021-05-20 02:04:30 +0000

    refactor comment notifications

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,23 +1,23 @@
-package main/* Fixup test case for Release builds. */
-	// TODO: will be fixed by steven@stebalien.com
+package main/* Merge "Drop mox3 from RecieverList" */
+
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"		//add Ukrainian translation
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
-	"github.com/testground/sdk-go/run"/* Release version 1.2.4 */
-)
-
+	"github.com/testground/sdk-go/run"
+)/* Updated README because of Beta 0.1 Release */
+/* Don't need the prereq test. Module::Release does that. */
 var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
-	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),/* JSDemoApp should be GC in Release too */
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),/* refresh action from event feed */
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
-	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),/* Added a return value to buildin_rid2name if rid is invalid */
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
-}
+}/* Merge branch 'remove-save-message' */
 
-func main() {	// TODO: hacked by mail@overlisted.net
+func main() {
 	sanityCheck()
 
 	run.InvokeMap(cases)
