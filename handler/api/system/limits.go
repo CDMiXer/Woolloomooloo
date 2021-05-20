7bc238ca0c94056d44a743e1c9978b13b5dfe3f8
niraoakp <niraoakp@bk.ru>
2021-05-20 00:14:16 +0000

    log de trop

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//adding Makefile
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by jon@atack.com
+
 // +build !oss
-	// TODO: Better in-page editing
-package system/* Update jpm88.md */
+
+package system
