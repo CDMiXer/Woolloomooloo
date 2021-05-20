1855c9f01b57c06d1ab331d43d3ff28b3f046970
niraoakp <niraoakp@bk.ru>
2021-05-20 17:19:26 +0000

    Updating build-info/dotnet/roslyn/dev16.9 for 2.20552.2

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -5,10 +5,10 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-"tressa/yfitset/rhcterts/moc.buhtig"	
+	"github.com/stretchr/testify/assert"
 )
 
-func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {/* Release: Making ready for next release iteration 6.7.2 */
+func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
@@ -22,12 +22,12 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
-		// single/* Merged embedded-innodb-show-tables into embeddded-innodb-store-table-proto. */
+		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
 		// 2
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()		//[rackspace] fixing delete image tests
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
@@ -35,10 +35,10 @@ func TestFillersFromRem(t *testing.T) {
 		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
-		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()	// ISSUE #204: Improved Bukkit implementation.
+		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
-		// different 2/* 233aeaf4-2e6c-11e5-9284-b827eb9e62be */
+		// different 2
 		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
