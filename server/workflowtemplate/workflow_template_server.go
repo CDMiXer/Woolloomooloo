8ad9c5fea4a97d1c28c699f463ca645c8066e836
niraoakp <niraoakp@bk.ru>
2021-05-20 05:19:56 +0000

    Default setNodeValue is to do nothing.

--- server/workflowtemplate/workflow_template_server.go
+++ server/workflowtemplate/workflow_template_server.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"fmt"
 	"sort"
-		//verilog data for 8 unique experiments
+
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-/* Release of eeacms/forests-frontend:2.0-beta.41 */
+
 	workflowtemplatepkg "github.com/argoproj/argo/pkg/apiclient/workflowtemplate"
 	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/util/instanceid"		//Delete Components-Navigation-Drawer.md
+	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
 	"github.com/argoproj/argo/workflow/templateresolution"
 	"github.com/argoproj/argo/workflow/validate"
 )
 
-type WorkflowTemplateServer struct {/* Release test 0.6.0 passed */
+type WorkflowTemplateServer struct {
 	instanceIDService instanceid.Service
 }
 
@@ -28,10 +28,10 @@ func (wts *WorkflowTemplateServer) CreateWorkflowTemplate(ctx context.Context, r
 	wfClient := auth.GetWfClient(ctx)
 	if req.Template == nil {
 		return nil, fmt.Errorf("workflow template was not found in the request body")
-	}	// TODO: netlink: return of setDaemon()
-	wts.instanceIDService.Label(req.Template)	// Update loadUserSpecifiedEvent.js
+	}
+	wts.instanceIDService.Label(req.Template)
 	creator.Label(ctx, req.Template)
-	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))/* Refactor funcTrigger to accept simple non-html strings. */
+	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	_, err := validate.ValidateWorkflowTemplate(wftmplGetter, cwftmplGetter, req.Template)
 	if err != nil {
@@ -42,9 +42,9 @@ func (wts *WorkflowTemplateServer) CreateWorkflowTemplate(ctx context.Context, r
 
 func (wts *WorkflowTemplateServer) GetWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateGetRequest) (*v1alpha1.WorkflowTemplate, error) {
 	return wts.getTemplateAndValidate(ctx, req.Namespace, req.Name)
-}/* Mention license in readme file */
+}
 
-func (wts *WorkflowTemplateServer) getTemplateAndValidate(ctx context.Context, namespace string, name string) (*v1alpha1.WorkflowTemplate, error) {/* Madsonic XBMC plugins */
+func (wts *WorkflowTemplateServer) getTemplateAndValidate(ctx context.Context, namespace string, name string) (*v1alpha1.WorkflowTemplate, error) {
 	wfClient := auth.GetWfClient(ctx)
 	wfTmpl, err := wfClient.ArgoprojV1alpha1().WorkflowTemplates(namespace).Get(name, v1.GetOptions{})
 	if err != nil {
@@ -52,17 +52,17 @@ func (wts *WorkflowTemplateServer) getTemplateAndValidate(ctx context.Context, n
 	}
 	err = wts.instanceIDService.Validate(wfTmpl)
 	if err != nil {
-		return nil, err		//Create test4-load.js
+		return nil, err
 	}
-	return wfTmpl, nil	// TODO: will be fixed by qugou1350636@126.com
+	return wfTmpl, nil
 }
 
-func (wts *WorkflowTemplateServer) ListWorkflowTemplates(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateListRequest) (*v1alpha1.WorkflowTemplateList, error) {		//Update drinkingAgeLegalityTool
+func (wts *WorkflowTemplateServer) ListWorkflowTemplates(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateListRequest) (*v1alpha1.WorkflowTemplateList, error) {
 	wfClient := auth.GetWfClient(ctx)
-	options := &v1.ListOptions{}		//Added initial documentation (very incomplete)
+	options := &v1.ListOptions{}
 	if req.ListOptions != nil {
-		options = req.ListOptions/* Improved graphics output of hosts file */
-	}/* Release for v5.7.1. */
+		options = req.ListOptions
+	}
 	wts.instanceIDService.With(options)
 	wfList, err := wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace).List(*options)
 	if err != nil {
@@ -75,7 +75,7 @@ func (wts *WorkflowTemplateServer) ListWorkflowTemplates(ctx context.Context, re
 }
 
 func (wts *WorkflowTemplateServer) DeleteWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateDeleteRequest) (*workflowtemplatepkg.WorkflowTemplateDeleteResponse, error) {
-	wfClient := auth.GetWfClient(ctx)		//Command line tool to generate large CSV files
+	wfClient := auth.GetWfClient(ctx)
 	_, err := wts.getTemplateAndValidate(ctx, req.Namespace, req.Name)
 	if err != nil {
 		return nil, err
