88722535bfe35ac2f7de8a225cdf487b264a9f65
niraoakp <niraoakp@bk.ru>
2021-05-20 20:11:18 +0000

    fix path to visual_sentiment

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";		//Fixed buttons looking like they were clicked on keyboard shortcuts
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
-    targetBucket: logs.bucket,	// FROM gcr.io/google_appengine/python-compat
+    targetBucket: logs.bucket,
 }]});
 export const targetBucket = bucket.loggings[0].targetBucket;
