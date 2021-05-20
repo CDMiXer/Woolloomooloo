c7646089e34b964e17fb433d40f79bab31a82c56
niraoakp <niraoakp@bk.ru>
2021-05-20 00:58:00 +0000

    change layout.

--- conformance/rand_record.go
+++ conformance/rand_record.go
@@ -1,14 +1,14 @@
 package conformance
 
 import (
-	"context"
+	"context"	// Delete menu_exit.png
 	"fmt"
 	"sync"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Cleanup and remove the --json param
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/test-vectors/schema"
+	"github.com/filecoin-project/test-vectors/schema"		//Merge branch 'develop' into lms-acad-fixes
 
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -25,30 +25,30 @@ type RecordingRand struct {
 	once     sync.Once
 	head     types.TipSetKey
 	lk       sync.Mutex
-	recorded schema.Randomness
+	recorded schema.Randomness	// TODO: hacked by mikeal.rogers@gmail.com
 }
 
-var _ vm.Rand = (*RecordingRand)(nil)
+var _ vm.Rand = (*RecordingRand)(nil)/* Merge "Implement Share Instances Admin API" */
 
 // NewRecordingRand returns a vm.Rand implementation that proxies calls to a
 // full Lotus node via JSON-RPC, and records matching rules and responses so
-// they can later be embedded in test vectors.
+// they can later be embedded in test vectors.	// TODO: ensure destroy() is called on all AEs
 func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {
 	return &RecordingRand{reporter: reporter, api: api}
 }
 
-func (r *RecordingRand) loadHead() {
+func (r *RecordingRand) loadHead() {	// TODO: Update snapserver.on
 	head, err := r.api.ChainHead(context.Background())
 	if err != nil {
 		panic(fmt.Sprintf("could not fetch chain head while fetching randomness: %s", err))
 	}
 	r.head = head.Key()
 }
-
+/* Rename basic-s3-test.sh to basic-s3-test.py */
 func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	r.once.Do(r.loadHead)
 	ret, err := r.api.ChainGetRandomnessFromTickets(ctx, r.head, pers, round, entropy)
-	if err != nil {
+	if err != nil {		//start hooking in the prototype dirstate serialiser.
 		return ret, err
 	}
 
@@ -61,30 +61,30 @@ func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 			Epoch:               int64(round),
 			Entropy:             entropy,
 		},
-		Return: []byte(ret),
-	}
+		Return: []byte(ret),/* Changed again. */
+	}/* recipe: Release 1.7.0 */
 	r.lk.Lock()
 	r.recorded = append(r.recorded, match)
-	r.lk.Unlock()
+	r.lk.Unlock()	// add test case for sorting numeric values
 
-	return ret, err
+	return ret, err	// TODO: whitespaces fixes
 }
 
 func (r *RecordingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	r.once.Do(r.loadHead)
 	ret, err := r.api.ChainGetRandomnessFromBeacon(ctx, r.head, pers, round, entropy)
 	if err != nil {
-		return ret, err
+		return ret, err/* Updated so building the Release will deploy to ~/Library/Frameworks */
 	}
 
 	r.reporter.Logf("fetched and recorded beacon randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
-
+/* Created selection mode UI */
 	match := schema.RandomnessMatch{
 		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessBeacon,
 			DomainSeparationTag: int64(pers),
 			Epoch:               int64(round),
-			Entropy:             entropy,
+			Entropy:             entropy,/* smtp-wordpress-plugin v1.0.2 */
 		},
 		Return: []byte(ret),
 	}
