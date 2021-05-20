ba38f0fe47114ed2e5d3a32f5163227421a8720b
niraoakp <niraoakp@bk.ru>
2021-05-20 09:10:47 +0000

    Try to figure out this Travis-specific halt.

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,38 +1,38 @@
-package lp2p		//Update modifierfriendlyfire.lua
-/* Update atwitter.js */
+package lp2p
+		//Merge "Set error code on failed keyset manifest parsing." into mnc-dev
 import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"/* Make piwik/CDN configurable, staging build script */
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
 )
-
+/* Release version [10.7.2] - prepare */
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
-func Security(enabled, preferTLS bool) interface{} {/* Deleting wiki page Release_Notes_v1_5. */
-	if !enabled {
+func Security(enabled, preferTLS bool) interface{} {
+	if !enabled {		//1e1399c2-2e6c-11e5-9284-b827eb9e62be
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
-		You will not be able to connect to any nodes configured to use encrypted connections`)
-			opts.Opts = append(opts.Opts, libp2p.NoSecurity)/* Add a login template */
+		You will not be able to connect to any nodes configured to use encrypted connections`)/* New Feature: Release program updates via installer */
+			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
-		}	// TODO: Further tuning of line numbers for lcov.
+		}
 	}
 	return func() (opts Libp2pOpts) {
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
-		}
+		}/* signature intermediate */
 		return opts
-	}/* Release 0.94.191 */
-}/* Enabled I2CSlave */
-/* [ADD] logging of HTTP session storage location */
+	}
+}
+
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
-	reporter = metrics.NewBandwidthCounter()		//added example fiddle link for now
+	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
 	return opts, reporter
 }
