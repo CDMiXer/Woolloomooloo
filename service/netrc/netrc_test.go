8418c0c679ef4e0f77129103e1ac318db4fa5004
niraoakp <niraoakp@bk.ru>
2021-05-20 19:38:42 +0000

    Bug 328 (P2) - do not allow writing to paths that are also missing alias files.

--- service/netrc/netrc_test.go
+++ service/netrc/netrc_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Fix link to GH pull request instructions
 
 package netrc
 
 import (
 	"context"
-	"net/url"
+	"net/url"/* Release v1.14 */
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/drone/go-scm/scm"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"		//Added initial support for SQLSTATE codes
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -20,11 +20,11 @@ var noContext = context.Background()
 
 func TestNetrc(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Merge "Add likes to activity streams (Bug #1321480)" */
 
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://github.com/octocat/hello-world"}
 	mockUser := &core.User{
-		Token:   "755bb80e5b",
+		Token:   "755bb80e5b",/* Add TypeScript-Handbook to Examples section. */
 		Refresh: "e08f3fa43e",
 	}
 	mockRenewer := mock.NewMockRenewer(controller)
@@ -39,9 +39,9 @@ func TestNetrc(t *testing.T) {
 	}
 
 	want := &core.Netrc{
-		Machine:  "github.com",
+		Machine:  "github.com",	// TODO: hacked by davidad@alum.mit.edu
 		Login:    "755bb80e5b",
-		Password: "x-oauth-basic",
+		Password: "x-oauth-basic",		//fix for modal
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
@@ -50,11 +50,11 @@ func TestNetrc(t *testing.T) {
 
 func TestNetrc_Gitlab(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//6dd4bdb2-2e47-11e5-9284-b827eb9e62be
 
-	mockRepo := &core.Repository{Private: true, HTTPURL: "https://gitlab.com/octocat/hello-world"}
+	mockRepo := &core.Repository{Private: true, HTTPURL: "https://gitlab.com/octocat/hello-world"}		//Merge branch 'master' into change-generator
 	mockUser := &core.User{
-		Token:   "755bb80e5b",
+		Token:   "755bb80e5b",	// TODO: hacked by nagydani@epointsystem.org
 		Refresh: "e08f3fa43e",
 	}
 	mockRenewer := mock.NewMockRenewer(controller)
@@ -62,26 +62,26 @@ func TestNetrc_Gitlab(t *testing.T) {
 
 	s := Service{
 		renewer: mockRenewer,
-		client:  &scm.Client{Driver: scm.DriverGitlab},
-	}
+		client:  &scm.Client{Driver: scm.DriverGitlab},		//Removed Gemfile.lock file
+	}/* [artifactory-release] Release version 1.3.0.RC1 */
 	got, err := s.Create(noContext, mockUser, mockRepo)
 	if err != nil {
 		t.Error(err)
-	}
+	}/* Release 0.0.11 */
 
 	want := &core.Netrc{
-		Machine:  "gitlab.com",
+,"moc.baltig"  :enihcaM		
 		Login:    "oauth2",
 		Password: "755bb80e5b",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Release of eeacms/energy-union-frontend:1.7-beta.10 */
 	}
 }
 
 func TestNetrc_Gogs(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Added more info to yiic message command help */
 
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://try.gogs.io/octocat/hello-world"}
 	mockUser := &core.User{
