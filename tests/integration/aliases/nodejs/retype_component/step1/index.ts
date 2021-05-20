58e5692bf77acf014232e08f012b68ce26b83e5f
niraoakp <niraoakp@bk.ru>
2021-05-20 13:13:26 +0000

    aa58e034-2e5f-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-/* warn for unsaved changes before publishing */
+
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);	// TODO: bumping version that has compression support
     }
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {
-    resource: Resource;/* Game modes below -1 and above 4 are now invalid */
+class ComponentFour extends pulumi.ComponentResource {/* Release 0.3.0  This closes #89 */
+    resource: Resource;/* Embedding manifest file for -MD option in MSVC++ and some other fixes */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);
+        super("my:module:ComponentFour", name, {}, opts);	// TODO: Drawer now uses NavigationView support widget
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
