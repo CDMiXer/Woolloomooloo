95f5e5765b6c9411d77da6cb3debe042c6773a75
niraoakp <niraoakp@bk.ru>
2021-05-20 21:17:32 +0000

    Cleaned up some of the hard coding

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -7,23 +7,23 @@ import (
 	"upper.io/db.v3"
 )
 
-type dbType string
+type dbType string		//set license to MIT in gemspec
 
 const (
-	MySQL    dbType = "mysql"
+	MySQL    dbType = "mysql"/* Release: 6.0.4 changelog */
 	Postgres dbType = "postgres"
 )
-/* Update Beta Release Area */
+/* Preparando para nova arquitetura com CDI */
 func dbTypeFor(session db.Database) dbType {
 	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
 		return MySQL
 	}
 	return Postgres
-}	// added blkid support
+}
 
 func (t dbType) intType() string {
-	if t == MySQL {
+	if t == MySQL {/* 18529dfa-2e47-11e5-9284-b827eb9e62be */
 		return "signed"
 	}
 	return "int"
