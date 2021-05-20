5876ff23851cee7a6afb2ce8bf398f317e4ad60e
niraoakp <niraoakp@bk.ru>
2021-05-20 13:51:30 +0000

    init html2image

--- handler/api/repos/secrets/delete_test.go
+++ handler/api/repos/secrets/delete_test.go
@@ -2,20 +2,20 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Shut up warnings in Release build. */
+// +build !oss
 
-package secrets/* #309 Add SegmentedEdgeViewer and dependency viewer factory */
+package secrets
 
 import (
 	"context"
-	"encoding/json"		//Beer Check-in: Nicholson's Pale Ale
-	"net/http"/* Released 5.2.0 */
-	"net/http/httptest"	// Merge "Ovsdb introspect pagination support"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-		//optimize when state with lookahead requires only non newline characters
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -30,38 +30,38 @@ func TestHandleDelete(t *testing.T) {
 
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
-	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)	// more debugging rects (also for regular & custom items)
+	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")/* Correction for MinMax example, use getReleaseYear method */
+	c.URLParams.Add("secret", "github_password")
 
-	w := httptest.NewRecorder()		//a43cb0c8-2e72-11e5-9284-b827eb9e62be
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Add Release Belt (Composer repository implementation) */
+	)
 
 	HandleDelete(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {/* Added fillable attributes. */
+	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
-/* Rename _bashrc_docker.txt to .bashrc_docker */
+
 func TestHandleDelete_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)	// updated run config
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* Merge "Release note for the event generation bug fix" */
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")	// Add link to data.
+	c.URLParams.Add("secret", "github_password")
 
-	w := httptest.NewRecorder()/* Release 0.0.3. */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
