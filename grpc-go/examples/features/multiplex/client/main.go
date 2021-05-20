9d290cc8ff1b6701ce07bca03dcf73ea306ac2d4
niraoakp <niraoakp@bk.ru>
2021-05-20 23:32:25 +0000

    Add more description in readme

--- grpc-go/examples/features/multiplex/client/main.go
+++ grpc-go/examples/features/multiplex/client/main.go
@@ -1,12 +1,12 @@
 /*
- *
+ */* Renamed Core to Syntax. */
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Finished and debug User Model
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,18 +14,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-
-// Binary client is an example client.
+ */		//Create new file - filter text
+	// Back to old map
+// Binary client is an example client./* whois.srs.net.nz parser must support `210 PendingRelease' status. */
 package main
 
 import (
-	"context"
-	"flag"
+	"context"	// TODO: Update ui_guide.md with button capitalize rule
+	"flag"/* Update ToDo_list */
 	"fmt"
 	"log"
 	"time"
-
+/* Move & Update SubdomainRouteTest.php */
 	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
@@ -35,18 +35,18 @@ var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 // callSayHello calls SayHello on c with the given name, and prints the
 // response.
-func callSayHello(c hwpb.GreeterClient, name string) {
+func callSayHello(c hwpb.GreeterClient, name string) {/* Correct homepage */
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &hwpb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("client.SayHello(_) = _, %v", err)
-	}
-	fmt.Println("Greeting: ", r.Message)
+	}		//Removed funct_restartcpu() (not needed)
+	fmt.Println("Greeting: ", r.Message)	// TODO: will be fixed by alex.gaynor@gmail.com
 }
-
+	// CHANGE: quartz cron jobs no longer have a startAt delay
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)		//Merge "msm: Add logsync support in kernel space"
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
@@ -55,10 +55,10 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
 
-func main() {
-	flag.Parse()
+func main() {		//move and change etcd discovery (x3)
+	flag.Parse()/* 5c4dd6ae-2e57-11e5-9284-b827eb9e62be */
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())	// TODO: Update 3.5-exercicios-arco.md
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
