0e30c4b051aeca554bd1a283d47e5c89ea3aefc0
niraoakp <niraoakp@bk.ru>
2021-05-20 20:25:50 +0000

    Added Linux as confirmed platform

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by 13860583249@yeah.net
-package tests
+
+package tests	// TODO: fixing trumbowyg in contents edit
 
 import (
 	"fmt"
@@ -8,16 +8,16 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
-)
-/* Release handle will now used */
+)/* Module menu: add image field	 */
+
 func TestMain(m *testing.M) {
-	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary/* Released 0.1.5 */
+	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
 	// backups of test stacks.
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
 		os.Exit(1)
-	}/* [Build] Gulp Release Task #82 */
-/* Release 0.3.0-SNAPSHOT */
+	}
+
 	code := m.Run()
 	os.Exit(code)
 }
