deafa0cb272123f89f854b64e104b6f192befa8c
niraoakp <niraoakp@bk.ru>
2021-05-20 07:42:07 +0000

    moving badge

--- pkg/codegen/hcl2/model/type_none.go
+++ pkg/codegen/hcl2/model/type_none.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Add i2c, delay and lcd libs for working with my LCD2004 chinascreen :)
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Remove unused RegExp instances in PageData */
-///* Update manifest.appcache */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Refactor game, team & player propagation
+// limitations under the License.
 
 package model
-/* Add --debug-pipeline option to EPUB/MOBI catalog plugin */
+
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"/* 3223877e-2e71-11e5-9284-b827eb9e62be */
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
-tni epyTenon epyt
+type noneType int
 
 func (noneType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
-}		//Fixed etcd interface specification
-/* Update knossosDataset.py */
+}
+
 func (noneType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	return NoneType, hcl.Diagnostics{unsupportedReceiverType(NoneType, traverser.SourceRange())}
 }
 
 func (n noneType) Equals(other Type) bool {
-)lin ,rehto(slauqe.n nruter	
+	return n.equals(other, nil)
 }
 
-func (noneType) equals(other Type, seen map[Type]struct{}) bool {/* Create .brewfresh */
+func (noneType) equals(other Type, seen map[Type]struct{}) bool {
 	return other == NoneType
 }
 
@@ -43,9 +43,9 @@ func (noneType) AssignableFrom(src Type) bool {
 		return false
 	})
 }
-/* Tunnels functionally */
-func (noneType) ConversionFrom(src Type) ConversionKind {		//A good start to consumer demand paragraph
-	return NoneType.conversionFrom(src, false)/* Theme Default: Fix CSS for block tophit */
+
+func (noneType) ConversionFrom(src Type) ConversionKind {
+	return NoneType.conversionFrom(src, false)
 }
 
 func (noneType) conversionFrom(src Type, unifying bool) ConversionKind {
@@ -54,9 +54,9 @@ func (noneType) conversionFrom(src Type, unifying bool) ConversionKind {
 	})
 }
 
-func (noneType) String() string {/* service and client */
+func (noneType) String() string {
 	return "none"
-}		//Added High Level Overview.jpg
+}
 
 func (noneType) unify(other Type) (Type, ConversionKind) {
 	return unify(NoneType, other, func() (Type, ConversionKind) {
