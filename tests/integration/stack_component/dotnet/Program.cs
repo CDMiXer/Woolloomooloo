5738e73ef0b8512e755fa998051a9e5865a13936
niraoakp <niraoakp@bk.ru>
2021-05-20 03:49:24 +0000

    Create troubleshooting.html

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -1,29 +1,29 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* document_change: Improve the workflow */
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
-class MyStack : Stack
-{	// TODO: hacked by ng8eke@163.com
+class MyStack : Stack/* simplified assembly descriptor by removing unneeded include and exclude lists */
+{
     [Output("abc")]
-    public Output<string> Abc { get; private set; }/* Create bootflow.md */
+    public Output<string> Abc { get; private set; }
+/* Update .authinfo */
+    [Output]
+    public Output<int> Foo { get; private set; }
 
-    [Output]/* Release 2.0.24 - ensure 'required' parameter is included */
-    public Output<int> Foo { get; private set; }		//416fbd32-2e68-11e5-9284-b827eb9e62be
-
-    // This should NOT be exported as stack output due to the missing attribute/* Release for v5.3.0. */
+    // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
-/* Merge branch 'release-next' into CoreReleaseNotes */
-    public MyStack()
+
+    public MyStack()	// update plugin and AUs
     {
-        this.Abc = Output.Create("ABC");
+        this.Abc = Output.Create("ABC");/* Released version 0.4.0.beta.2 */
         this.Foo = Output.Create(42);
-        this.Bar = Output.Create("this should not come to output");	// TODO: hacked by earlephilhower@yahoo.com
-    }
+        this.Bar = Output.Create("this should not come to output");
+    }	// TODO: hacked by steven@stebalien.com
 }
-		//sets preproduction deploy variables
+
 class Program
 {
     static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
-}		//Fixed a bug with the new AWS CLI
+}
