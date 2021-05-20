c0282708164304d803271d4a5326a010d82275c7
niraoakp <niraoakp@bk.ru>
2021-05-20 20:45:58 +0000

    Fix for the global mute

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.		//[IMP]:base setup modules installation wizard
-///* Release areca-5.5.3 */
-// Licensed under the Apache License, Version 2.0 (the "License");		//site and readme change
-// you may not use this file except in compliance with the License./* Released springjdbcdao version 1.7.17 */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* test change for setting welcome messages */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Add StringLiteralUtil
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/eprtr-frontend:0.4-beta.11 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* lombokified */
+// limitations under the License.
 
-package deploys	// TODO: bbtpanel: layout corrections
+package deploys
 
 import (
 	"net/http"
@@ -27,26 +27,26 @@ import (
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
-	repos core.RepositoryStore,/* Update swiftlint file for naming changes */
-	builds core.BuildStore,		//Add radio lines to the manifest
+	repos core.RepositoryStore,
+	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Release version 2.0.0.RC2 */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)	// TODO: will be fixed by nagydani@epointsystem.org
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).	// TODO: will be fixed by nicksavers@gmail.com
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return	// Ajout d'un pseudo combat, plus autres minimodifs mineures
+			return
 		}
 
-		results, err := builds.LatestDeploys(r.Context(), repo.ID)/* Release areca-5.0.2 */
+		results, err := builds.LatestDeploys(r.Context(), repo.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
