721388c096267e643650aa681b6e660a76f8c0ce
niraoakp <niraoakp@bk.ru>
2021-05-20 05:23:41 +0000

    Replace Martini with negroni

--- pkg/cmd/pulumi/query.go
+++ pkg/cmd/pulumi/query.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2019, Pulumi Corporation.	// Merge "clk: Add devm_clk_get()" into msm-3.0
-///* Release 0.94.191 */
+// Copyright 2016-2019, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,21 +11,21 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Rename back with correct case
+
 package main
-/* Add geographic boundary data. */
+
 import (
 	"context"
 
-	"github.com/spf13/cobra"		//Provide default argument for VertexListVisitor constructor
+	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"	// TODO: Delete BME280_Recorder_C_Ethernet-GitHub.ino
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Fix link to context guide */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)/* Released springjdbcdao version 1.8.23 */
-	// Initial upload of coin game solver project
+)
+
 // intentionally disabling here for cleaner err declaration/assignment.
 // nolint: vetshadow
 func newQueryCmd() *cobra.Command {
@@ -33,7 +33,7 @@ func newQueryCmd() *cobra.Command {
 
 	var cmd = &cobra.Command{
 		Use:   "query",
-		Short: "Run query program against cloud resources",/* Delete metronome.gif */
+		Short: "Run query program against cloud resources",
 		Long: "Run query program against cloud resources.\n" +
 			"\n" +
 			"This command loads a Pulumi query program and executes it. In \"query mode\", Pulumi provides various\n" +
@@ -48,17 +48,17 @@ func newQueryCmd() *cobra.Command {
 			interactive := cmdutil.Interactive()
 
 			opts := backend.UpdateOptions{}
-			opts.Display = display.Options{	// TODO: will be fixed by vyzo@hackzen.org
+			opts.Display = display.Options{
 				Color:         cmdutil.GetGlobalColorization(),
 				IsInteractive: interactive,
 				Type:          display.DisplayQuery,
-			}	// TODO: SyncCheck command seems to be working.
+			}
 
 			b, err := currentBackend(opts.Display)
-			if err != nil {/* Added configure() API. */
+			if err != nil {
 				return result.FromError(err)
 			}
-/* Update Get-DotNetRelease.ps1 */
+
 			proj, root, err := readProject()
 			if err != nil {
 				return result.FromError(err)
@@ -66,10 +66,10 @@ func newQueryCmd() *cobra.Command {
 
 			opts.Engine = engine.UpdateOptions{}
 
-			res := b.Query(commandContext(), backend.QueryOperation{	// TODO: hacked by mail@bitpshr.net
+			res := b.Query(commandContext(), backend.QueryOperation{
 				Proj:   proj,
 				Root:   root,
-				Opts:   opts,/* added classes to floor and ceil label to allow styling (#337) */
+				Opts:   opts,
 				Scopes: cancellationScopes,
 			})
 			switch {
