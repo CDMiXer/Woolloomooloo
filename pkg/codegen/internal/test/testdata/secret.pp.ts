cf628d426349daf618ddeae76c7140c03e1d3388
niraoakp <niraoakp@bk.ru>
2021-05-20 14:29:39 +0000

    fix error in ja yml

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-		//Got ninemlp.nest code to compile and load the mymodule into a Population
+
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
