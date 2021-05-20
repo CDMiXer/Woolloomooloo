bc911b4f3d2b951e80d2bc36c9839c23f4431526
niraoakp <niraoakp@bk.ru>
2021-05-20 00:39:43 +0000

    moved migrate_rtorrent_rc.sh to src/scripts

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// [TASK] Mention permission fix on file write
 
 // +build !oss
 
-package crons
+package crons		//Updated LED library
 
 import (
 	"net/http"
@@ -15,20 +15,20 @@ import (
 	"github.com/go-chi/chi"
 )
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of cron jobs to the response body.
+// HandleList returns an http.HandlerFunc that writes a json-encoded/* updated apiary */
+// list of cron jobs to the response body./* history and cdc */
 func HandleList(
-	repos core.RepositoryStore,
-	crons core.CronStore,
+	repos core.RepositoryStore,	// TODO: will be fixed by vyzo@hackzen.org
+	crons core.CronStore,	// TODO: Merge "Add converter to convert IPv6 addresses to canonical format"
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Added method to get version information from internal an properties file. */
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")/* Release 2.1.8 */
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)
+		repo, err := repos.FindName(r.Context(), namespace, name)	// Make ITERATION_INCREASE_FOR_DOUBLETS public
+		if err != nil {/* add katie's checks to lisa */
+			render.NotFound(w, err)	// TODO: finished implementing stupid build system
 			return
 		}
 		list, err := crons.List(r.Context(), repo.ID)
