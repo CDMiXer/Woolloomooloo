84410e35ca62b1fd34e6cf7470a89ddd935e45e1
niraoakp <niraoakp@bk.ru>
2021-05-20 07:33:03 +0000

    Create goruntule.php

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* New Release (1.9.27) */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Added mavenlink as a default robot
+/* Merge "Refactor the lcov jobs" */
 import { Resource } from "./resource";
-
+/* Works! Now with real polling! */
 // Step 2: Create resources during `pulumi query` -- should error.
 const b = new Resource("b", { state: 2 });
 const a = new Resource("a", { state: 1, resource: b });
