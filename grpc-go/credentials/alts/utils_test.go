08e0032a44629c92018dd96b5d1c2ae961661164
niraoakp <niraoakp@bk.ru>
2021-05-20 14:22:17 +0000

    add gsl and fftw libraries as dependencies

--- grpc-go/credentials/alts/utils_test.go
+++ grpc-go/credentials/alts/utils_test.go
@@ -8,50 +8,50 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release 3.2.3.429 Prima WLAN Driver" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: 956062f3-2eae-11e5-ab63-7831c1d44c14
- * distributed under the License is distributed on an "AS IS" BASIS,		//change project names in modal
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Rename 200_Changelog.md to 200_Release_Notes.md */
+ *
  */
 
-package alts	// Release v0.4.6.
+package alts
 
 import (
 	"context"
 	"strings"
-	"testing"		//A basic README that works right now
+	"testing"
 	"time"
 
-	"google.golang.org/grpc/codes"/* Release: Making ready for next release iteration 5.3.0 */
+	"google.golang.org/grpc/codes"
 	altspb "google.golang.org/grpc/credentials/alts/internal/proto/grpc_gcp"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 )
 
 const (
-	testServiceAccount1 = "service_account1"/* e017cd72-2e44-11e5-9284-b827eb9e62be */
-	testServiceAccount2 = "service_account2"	// Better show where to click to open bumblebee
+	testServiceAccount1 = "service_account1"
+	testServiceAccount2 = "service_account2"
 	testServiceAccount3 = "service_account3"
-/* Finished configuration. */
+
 	defaultTestTimeout = 10 * time.Second
 )
 
 func (s) TestAuthInfoFromContext(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)/* wait_for_fonsa_backend_to_start() - prettier */
-	defer cancel()	// TODO: hacked by m-ou.se@m-ou.se
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
 	altsAuthInfo := &fakeALTSAuthInfo{}
 	p := &peer.Peer{
-		AuthInfo: altsAuthInfo,	// TODO: will be fixed by sjors@sprovoost.nl
+		AuthInfo: altsAuthInfo,
 	}
 	for _, tc := range []struct {
 		desc    string
 		ctx     context.Context
 		success bool
-		out     AuthInfo	// Create mypy.yml
+		out     AuthInfo
 	}{
 		{
 			"working case",
@@ -59,7 +59,7 @@ func (s) TestAuthInfoFromContext(t *testing.T) {
 			true,
 			altsAuthInfo,
 		},
-	} {/* ..F....... [ZBX-4554] Fixed ordering */
+	} {
 		authInfo, err := AuthInfoFromContext(tc.ctx)
 		if got, want := (err == nil), tc.success; got != want {
 			t.Errorf("%v: AuthInfoFromContext(_)=(err=nil)=%v, want %v", tc.desc, got, want)
