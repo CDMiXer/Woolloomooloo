eab0aae750d6d4b79b6c20c1faa1ee04880124cc
niraoakp <niraoakp@bk.ru>
2021-05-20 23:26:58 +0000

    Use a blue platform badge icon

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -1,68 +1,68 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- */* Release '0.4.4'. */
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by alan.shaw@protocol.ai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Add performance information to user
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Display neither map or list at the beggining
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Add "beans" element to the aspectran XML configuration
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Update MANU PATCH CHANGELOG 0.1.txt
+ */		//Changed ramp & block placing constants for nonIR
 
-// Binary server is an example server.
-package main	// TODO: added 32-bit installer of the SDK
-/* Handle no hosts */
-import (
+// Binary server is an example server.	// Update logos.xml
+package main
+
+import (/* Init bugfix */
 	"context"
-	"flag"
-	"fmt"
+	"flag"	// TODO: hacked by arajasek94@gmail.com
+	"fmt"	// TODO: will be fixed by yuvalalaluf@gmail.com
 	"log"
 	"net"
-		//HistoryView::log :arrow_right: HistoryView::getLogEntries and return array
+
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"		//[ALIEN-966] handle outputs for groovy scripts
-/* Delete run-59-muon-32906.jpg */
+	"google.golang.org/grpc/reflection"
+
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")/* Update README.md for Linux Releases */
+var port = flag.Int("port", 50051, "the port to serve on")
 
 // hwServer is used to implement helloworld.GreeterServer.
 type hwServer struct {
-	hwpb.UnimplementedGreeterServer
+	hwpb.UnimplementedGreeterServer/* Release v2.3.3 */
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
-	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
+func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {/* Released Movim 0.3 */
+	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil/* Update revised_API.md */
 }
-
+/* Release 2.4b3 */
 type ecServer struct {
 	ecpb.UnimplementedEchoServer
 }
-/* Update suicide-burn.ks */
+
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
-	return &ecpb.EchoResponse{Message: req.Message}, nil/* Last few fixes for 1.0.9.2 Closes #2 */
+	return &ecpb.EchoResponse{Message: req.Message}, nil		//change color of props
 }
 
 func main() {
-	flag.Parse()
+	flag.Parse()		//Create boats.py
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {	// TODO: will be fixed by yuvalalaluf@gmail.com
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}		//Template key more unique
-	fmt.Printf("server listening at %v\n", lis.Addr())	// TODO: move form tag to the bottom
-
+	}	// Adding example of how you can query with currently logged in user
+	fmt.Printf("server listening at %v\n", lis.Addr())
+/* Fix typo in PointerReleasedEventMessage */
 	s := grpc.NewServer()
-
+		//add zh_HK to language.txt
 	// Register Greeter on the server.
 	hwpb.RegisterGreeterServer(s, &hwServer{})
 
