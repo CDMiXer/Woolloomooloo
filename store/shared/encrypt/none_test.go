0bb512ae9a617270b225e6dafd916f02ce2dc338
niraoakp <niraoakp@bk.ru>
2021-05-20 09:59:47 +0000

    Pass [] optlist to gen_tcp port_command.

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Migrated to uima-tokens-regex 1.4 (term keyword replaced by rule) */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// Add top-level class diagram
 package encrypt
 
 import "testing"
@@ -9,7 +9,7 @@ import "testing"
 func TestNone(t *testing.T) {
 	n, _ := New("")
 	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
-	if err != nil {
+	if err != nil {/* Release of eeacms/www:18.5.9 */
 		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
