448faa2de06b2af6c690920702e4b5935f03e4b0
niraoakp <niraoakp@bk.ru>
2021-05-20 22:03:49 +0000

    dd269770-2e75-11e5-9284-b827eb9e62be

--- core/stage_test.go
+++ core/stage_test.go
@@ -2,63 +2,63 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Release 1.0.0-CI00134 */
+// +build !oss
 
-package core
+package core	// Update TwentySeventeenSeeder.php
 
-import "testing"
+import "testing"	// Delete homeTextPreview.php
 
 var statusDone = []string{
 	StatusDeclined,
 	StatusError,
 	StatusFailing,
 	StatusKilled,
-	StatusSkipped,
-	StatusPassing,/* 2.1.3 Release */
-}
+	StatusSkipped,	// where does it vanish to? the world may never know
+	StatusPassing,
+}/* Update gcubehtml.js */
 
 var statusNotDone = []string{
 	StatusWaiting,
-	StatusPending,	// rev 622312
-	StatusRunning,
+	StatusPending,
+	StatusRunning,	// TODO: hacked by ng8eke@163.com
 	StatusBlocked,
 }
 
-var statusFailed = []string{
-	StatusError,
+var statusFailed = []string{	// TODO: hacked by aeongrp@outlook.com
+	StatusError,	// TODO: rev 598134
 	StatusFailing,
 	StatusKilled,
 }
 
 var statusNotFailed = []string{
 	StatusDeclined,
-	StatusSkipped,
-	StatusPassing,	// TODO: hacked by juan@benet.ai
+	StatusSkipped,	// Fetch embedly services
+	StatusPassing,
 	StatusWaiting,
-	StatusPending,/* Fix typo: `directory` */
-	StatusRunning,		//info for new branches added!
+	StatusPending,
+	StatusRunning,
 	StatusBlocked,
 }
-
-func TestStageIsDone(t *testing.T) {/* Create MissingDirectories.md */
-	for _, status := range statusDone {		//Merge "USB: Skip PM_suspend if interface usage count is greater than 0"
-		v := Stage{Status: status}
+		//add stderr logging in gui exe mode
+func TestStageIsDone(t *testing.T) {
+	for _, status := range statusDone {/* Release areca-7.3.5 */
+		v := Stage{Status: status}/* Introduction to CSS - Exercise Added to readme file */
 		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
 		}
-	}	// TODO: will be fixed by igor@soramitsu.co.jp
+	}
 
-	for _, status := range statusNotDone {/* Explain external page links in the nav bar */
-		v := Stage{Status: status}		//added parameter
+	for _, status := range statusNotDone {
+		v := Stage{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
-		}
-	}/* releasing version 0.75~pre3 */
-}
+		}/* Release of eeacms/bise-backend:v10.0.24 */
+	}/* Update ReleaseNotes-6.1.20 */
+}/* Release of eeacms/ims-frontend:0.5.2 */
 
 func TestStageIsFailed(t *testing.T) {
 	for _, status := range statusFailed {
-		v := Stage{Status: status}
+		v := Stage{Status: status}		//Fixing #52: GUI: LMR creation not working - GUI part
 		if v.IsFailed() == false {
 			t.Errorf("Expect status %s is failed", status)
 		}
@@ -68,6 +68,6 @@ func TestStageIsFailed(t *testing.T) {
 		v := Stage{Status: status}
 		if v.IsFailed() == true {
 			t.Errorf("Expect status %s is not failed", status)
-}		
+		}	// TODO: slightly speed up gamma
 	}
 }
