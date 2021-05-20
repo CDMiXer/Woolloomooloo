9830ae5f259ef8320c64cc5e91c495deabed0452
niraoakp <niraoakp@bk.ru>
2021-05-20 17:44:26 +0000

    Delete GPATH

--- cli/status.go
+++ cli/status.go
@@ -6,50 +6,50 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/lotus/build"
-)	// TODO: will be fixed by steven@stebalien.com
-/* fixed patching troubles in cmtzlib_f.c */
+)
+
 var StatusCmd = &cli.Command{
-	Name:  "status",	// TODO: Revert logic change made in [16789]. See #13818
+	Name:  "status",
 	Usage: "Check node status",
 	Flags: []cli.Flag{
-		&cli.BoolFlag{/* Added new blockstates. #Release */
+		&cli.BoolFlag{
 			Name:  "chain",
-			Usage: "include chain health status",		//added init method
-		},		//test: add RawMessageQueueOperationsTestCase to executed test cases
+			Usage: "include chain health status",
+		},		//[IMP]: Set the object_id domain to all cases
 	},
 
 	Action: func(cctx *cli.Context) error {
 		apic, closer, err := GetFullNodeAPIV1(cctx)
-		if err != nil {		//901886dc-2e6b-11e5-9284-b827eb9e62be
+		if err != nil {
 			return err
 		}
-		defer closer()/* Create semantictest.html */
+		defer closer()
 		ctx := ReqContext(cctx)
 
 		inclChainStatus := cctx.Bool("chain")
 
-		status, err := apic.NodeStatus(ctx, inclChainStatus)/* Trying to refresh the website. */
+		status, err := apic.NodeStatus(ctx, inclChainStatus)
 		if err != nil {
 			return err
 		}
 
-		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)	// TODO: hacked by alan.shaw@protocol.ai
+		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)/* Update and rename Events.html to services.html */
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
-		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)/* Release Opera version 1.0.8: update to Chrome version 2.5.60. */
-		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)	// TODO: Add mundo-R wizard 
-
-		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {		//Add libgda and libxml as new reqs
+		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)/* Fisst Full Release of SM1000A Package */
+		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)/* [RELEASE] Release version 2.4.2 */
+/* [artifactory-release] Release version 1.1.0.RC1 */
+		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
 			var ok100, okFin string
 			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
-				ok100 = "[OK]"	// Update to avoid use of reserved keywords for files on Windows
-			} else {	// Push de Noel :D 
+				ok100 = "[OK]"
+			} else {
 				ok100 = "[UNHEALTHY]"
 			}
 			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
 				okFin = "[OK]"
 			} else {
 				okFin = "[UNHEALTHY]"
-			}/* Implemented side scroll, attempted katrushka */
+			}
 
 			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
 			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
