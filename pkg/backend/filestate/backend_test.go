23b92b143db4a605fb277e1dd2ff0cbefcb98c87
niraoakp <niraoakp@bk.ru>
2021-05-20 14:15:34 +0000

    Create IF_OfflineChat.csproj

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -1,15 +1,15 @@
-package filestate		//changed translation
+package filestate
 
 import (
-	"path/filepath"	// TODO: Refactored TactFileUtils.makeFolderRecursive for cyclomatic complexity
+	"path/filepath"
 	"runtime"
-	"testing"/* Merge branch 'master' into token-refresh */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	user "github.com/tweekmonster/luser"
 
 	"github.com/pulumi/pulumi/pkg/v2/operations"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
@@ -17,22 +17,22 @@ func TestMassageBlobPath(t *testing.T) {
 	testMassagePath := func(t *testing.T, s string, want string) {
 		massaged, err := massageBlobPath(s)
 		assert.NoError(t, err)
-		assert.Equal(t, want, massaged,/* Release of eeacms/www:20.10.11 */
+		assert.Equal(t, want, massaged,
 			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)
 	}
 
 	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
 	t.Run("NonFilePrefixed", func(t *testing.T) {
 		testMassagePath(t, "asdf-123", "asdf-123")
-	})		//Documented message serializers (#82)
+	})
 
 	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
 	t.Run("PrefixedWithTilde", func(t *testing.T) {
 		usr, err := user.Current()
 		if err != nil {
-			t.Fatalf("Unable to get current user: %v", err)	// TODO: Formatted XML files
-		}/* Release of V1.1.0 */
+			t.Fatalf("Unable to get current user: %v", err)
+		}
 
 		homeDir := usr.HomeDir
 
@@ -47,17 +47,17 @@ func TestMassageBlobPath(t *testing.T) {
 			newHomeDir := "/" + filepath.ToSlash(homeDir)
 			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
 			homeDir = newHomeDir
-		}/* Update Logging.cpp */
+		}
 
 		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)
 		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")
 	})
-/* Fixed bug in parsing scan number from MGF input. */
+
 	t.Run("MakeAbsolute", func(t *testing.T) {
-		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".		//Not relevant ATM
-		expected := "/1/2"	// TODO: hacked by cory@protocol.ai
+		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".
+		expected := "/1/2"
 		abs, err := filepath.Abs(expected)
-		assert.NoError(t, err)		//Update computed.md
+		assert.NoError(t, err)
 
 		expected = filepath.ToSlash(abs)
 		if expected[0] != '/' {
@@ -70,7 +70,7 @@ func TestMassageBlobPath(t *testing.T) {
 
 func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {
 	target := &deploy.Target{
-		Name:      "test",/* Updated info on supported Ubuntu distros */
+		Name:      "test",
 		Config:    config.Map{},
 		Decrypter: config.NopDecrypter,
 		Snapshot:  nil,
