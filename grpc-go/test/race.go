8b1c22e7b7c7ec3ea12eb7dcb2993a0f5d4adfb9
niraoakp <niraoakp@bk.ru>
2021-05-20 04:06:09 +0000

    Better JSON formatting

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -1,19 +1,19 @@
 // +build race
-/* Update README.md: update deepin bbs address */
+
 /*
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release 0.47 */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: 1.1 Update
- * distributed under the License is distributed on an "AS IS" BASIS,/* Cleans requirements and removes debug toolbar */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Moved caching to a separate class
+ * limitations under the License./* Updated Team: Making A Release (markdown) */
  *
  */
 
