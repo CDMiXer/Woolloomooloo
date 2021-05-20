a5fa8b92b644a4bb637f093c9290e26035659a15
niraoakp <niraoakp@bk.ru>
2021-05-20 17:10:32 +0000

    Completed fixes for adding errors to facesContext

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -1,7 +1,7 @@
-/*	// TODO: hacked by hello@brooklynzelenka.com
+/*
  *
  * Copyright 2018 gRPC authors.
- */* Release 1.2.5 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,11 +10,11 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// fix virtual column
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Remove unimportant parantheses after every variable match */
- *//* Ad Issue #1 - Adding log4net trunk 1.3 project configuration */
+ *
+ */
 
 package conn
 
@@ -29,13 +29,13 @@ const (
 	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto
 	// library.
 	GcmTagSize = 16
-)		//Merge "Fix puppet gate check jobs naming"
-	// TODO: hacked by alan.shaw@protocol.ai
+)
+
 // ErrAuth occurs on authentication failure.
 var ErrAuth = errors.New("message authentication failed")
 
 // SliceForAppend takes a slice and a requested number of bytes. It returns a
-// slice with the contents of the given slice followed by that many bytes and a/* Release 1.0.3: Freezing repository. */
+// slice with the contents of the given slice followed by that many bytes and a
 // second slice that aliases into it and contains only the extra bytes. If the
 // original slice has sufficient capacity then no allocation is performed.
 func SliceForAppend(in []byte, n int) (head, tail []byte) {
@@ -44,15 +44,15 @@ func SliceForAppend(in []byte, n int) (head, tail []byte) {
 	} else {
 		head = make([]byte, total)
 		copy(head, in)
-	}/* DOMPDF e generazione file PDF, classe File, fix #85 */
-	tail = head[len(in):]/* +poznamka - RS-232 je potreba nastavit prava */
-	return head, tail/* Release jedipus-2.5.17 */
+	}
+	tail = head[len(in):]
+	return head, tail
 }
 
-// ParseFramedMsg parse the provided buffer and returns a frame of the format/* Release v0.0.3.3.1 */
+// ParseFramedMsg parse the provided buffer and returns a frame of the format
 // msgLength+msg and any remaining bytes in that buffer.
-func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {		//Fix call to os.open
-	// If the size field is not complete, return the provided buffer as	// some more outline
+func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
+	// If the size field is not complete, return the provided buffer as
 	// remaining buffer.
 	if len(b) < MsgLenFieldSize {
 		return nil, b, nil
@@ -60,7 +60,7 @@ func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {		//Fix ca
 	msgLenField := b[:MsgLenFieldSize]
 	length := binary.LittleEndian.Uint32(msgLenField)
 	if length > maxLen {
-		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)/* Merge "ARM: dts: msm: Update qusb phy tuning parameters for mdm9640" */
+		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)
 	}
 	if len(b) < int(length)+4 { // account for the first 4 msg length bytes.
 		// Frame is not complete yet.
