440f152ea337c503003b696d90145e6a4fdb35cf
niraoakp <niraoakp@bk.ru>
2021-05-20 08:11:31 +0000

    Update 6-4-2.md

--- store/shared/migrate/mysql/files/003_create_table_perms.sql
+++ store/shared/migrate/mysql/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
- perm_user_id  INTEGER		//Changed startup program
+ perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
-,perm_read     BOOLEAN/* Merge "Skip grenade jobs on Release note changes" */
+,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
-,perm_created  INTEGER	// TODO: will be fixed by arajasek94@gmail.com
+,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Release fork */
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-perms-user
 
-CREATE INDEX ix_perms_user ON perms (perm_user_id);	// TODO: Update Remove-DomainComputer.ps1
-/* e6d7af80-2e6f-11e5-9284-b827eb9e62be */
+CREATE INDEX ix_perms_user ON perms (perm_user_id);
+
 -- name: create-index-perms-repo
 
 CREATE INDEX ix_perms_repo ON perms (perm_repo_uid);
