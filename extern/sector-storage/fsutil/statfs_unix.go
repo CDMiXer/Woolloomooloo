578dab0eeeddb1104900d0f0d9800767ba201a47
niraoakp <niraoakp@bk.ru>
2021-05-20 07:07:41 +0000

    nfs/Cache: use boost::intrusive::auto_unlink

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,5 +1,5 @@
 package fsutil
-	// Delete TS_520_DG5_LCD_v2_0_1.ino
+
 import (
 	"syscall"
 
@@ -8,16 +8,16 @@ import (
 
 func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
-	if err := syscall.Statfs(path, &stat); err != nil {	// TODO: will be fixed by lexy8russo@outlook.com
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)	// TODO: FEATURE: Expose neos-project/react-ui-components to the outside
+	if err := syscall.Statfs(path, &stat); err != nil {
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
 
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
 	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
-/* Update pom and config file for Release 1.1 */
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),		//improve behave test for compare endpoint
+
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
 	}, nil
-}/* New FamilySearch form is no more beta */
+}
