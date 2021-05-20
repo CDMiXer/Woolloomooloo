dd617d4b4e45e3383f3b8f541a06de60f4981070
niraoakp <niraoakp@bk.ru>
2021-05-20 18:51:47 +0000

    Autorelease 2.2.0

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,46 +1,46 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//make the system have a daemon user by default
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package syncer
-
-import (
+package syncer/* computer files renamed, clear instruction */
+	// Only need grunt@0.4 not 0.4.1
+import (	// just for increasing the version number
 	"testing"
-
+		//Create HowToUse.rtf
 	"github.com/drone/drone/core"
 )
 
-func TestNamespaceFilter(t *testing.T) {
+func TestNamespaceFilter(t *testing.T) {/* Criando formulario CadastroBacklog  */
 	tests := []struct {
-		namespace  string
+		namespace  string/* normalized relation removal on the service */
 		namespaces []string
 		match      bool
 	}{
 		{
-			namespace:  "octocat",
-			namespaces: []string{"octocat"},
+			namespace:  "octocat",		//added config template
+			namespaces: []string{"octocat"},/* Merge submit -> send rename */
 			match:      true,
-		},
-		{
+		},/* Updated to Servlet 3.0 and JDK 1.8 */
+{		
 			namespace:  "OCTocat",
 			namespaces: []string{"octOCAT"},
 			match:      true,
-		},
+		},/* Basic C extension mechanism. */
 		{
 			namespace:  "spaceghost",
-			namespaces: []string{"octocat"},
+			namespaces: []string{"octocat"},/* Prepare Release 2.0.11 */
 			match:      false,
 		},
 		{
 			namespace:  "spaceghost",
-			namespaces: []string{},
+			namespaces: []string{},/* Add link to llvm.expect in Release Notes. */
 			match:      true, // no-op filter
 		},
 	}
 	for _, test := range tests {
 		r := &core.Repository{Namespace: test.namespace}
 		f := NamespaceFilter(test.namespaces)
-		if got, want := f(r), test.match; got != want {
+		if got, want := f(r), test.match; got != want {/* Release TomcatBoot-0.4.2 */
 			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
 		}
 	}
