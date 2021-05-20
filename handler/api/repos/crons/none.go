b2d95ca9f6bed30971db033ba55a7b129b6ae916
niraoakp <niraoakp@bk.ru>
2021-05-20 18:42:57 +0000

    Added isOwner()

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -1,53 +1,53 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release: Making ready for next release iteration 6.1.2 */
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Added year of consolidation to faolex document model
+// You may obtain a copy of the License at	// TODO: Update sandbox model for the latest LyoD
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// merge bzr.dev r4042
+// distributed under the License is distributed on an "AS IS" BASIS,/* reset to Release build type */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* v1.0 Release - update changelog */
 // limitations under the License.
 
 // +build oss
-		//c4c12712-2e67-11e5-9284-b827eb9e62be
+
 package crons
-/* Release version: 1.2.0.5 */
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"		//Auf StudentenVerwalten.xhtml die Nachricht bei den Buttons ausgebessert
 )
-	// TODO: Changed arraylist objects to sets
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-		//Seeds: rend la sortie par défaut plus concise
-func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {	// TODO: Deleted Ny design och bildspråk
+
+func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
+	return notImplemented
+}		//Removed a reference to the wiki
+
+func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* Create 'Branch test' file */
-	return notImplemented/* These can go now */
-}
-	// TODO: Dangling pointer fix
 func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented/* Release notes are updated for version 0.3.2 */
+	return notImplemented
 }
 
 func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
-/* SearchAsyncOperation: aboutToRun -> running */
+
 func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented	// TODO: working version of intersection and wrapper
+	return notImplemented
 }
 
 func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,
-	core.CommitService, core.Triggerer) http.HandlerFunc {		//-define gnsrecord plugin for DNS
+	core.CommitService, core.Triggerer) http.HandlerFunc {
 	return notImplemented
 }
