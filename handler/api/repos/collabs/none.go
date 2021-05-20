efc935ed6d10f246595c001fbf0000ba3e3ffd0f
niraoakp <niraoakp@bk.ru>
2021-05-20 16:45:26 +0000

    Modified Zend Acl/Navigation to be extendable.

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
-///* v4.4 - Release */
-// Licensed under the Apache License, Version 2.0 (the "License");		//eUcKGjBs9WwaPEUHDgPL5pQyiKMmdztP
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release version: 0.7.1 */
+///* Release for 2.6.0 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by fjl@ethereum.org
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into movebrowserify */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www-devel:18.8.1 */
+// See the License for the specific language governing permissions and/* SlidePane fix and Release 0.7 */
 // limitations under the License.
 
-// +build oss
+// +build oss/* Create TechnologyStacks.adoc */
 
 package collabs
 
 import (
-	"net/http"
+	"net/http"/* Closes #20 cleanup */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* fs: Add fuse driver */
 	"github.com/drone/drone/handler/api/render"
 )
-	// TODO: Remove `current_remote` broken feature
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}	// TODO: will be fixed by steven@stebalien.com
+}
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented/* Set up Release */
 }
-		//include more one how to create directories, and how to run programs
-func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented/* Release 0.3.0. Add ip whitelist based on CIDR. */
+
+func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {	// TODO: will be fixed by boringland@protonmail.ch
+	return notImplemented
 }
 
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
