b19ae19ee72ef82870cf39a77e2df2cc1f7ac822
niraoakp <niraoakp@bk.ru>
2021-05-20 00:25:03 +0000

    Fixes to TOFSD - still not working tho

--- grpc-go/xds/internal/testutils/local_listener.go
+++ grpc-go/xds/internal/testutils/local_listener.go
@@ -1,26 +1,26 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//5c0a5dd4-2e57-11e5-9284-b827eb9e62be
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* updated source for syntactic parser integration */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// better output for mismatch breakdown table.
- * Unless required by applicable law or agreed to in writing, software		//adding content for arc
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Even more HKey tests
  * See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil * 
+ * limitations under the License.
  *
  */
 
 package testutils
-		//Turn off the pre-cert error persisting on public site.
+
 import "net"
 
-// LocalTCPListener returns a net.Listener listening on local address and port.	// TODO: Added ChangeAware::isNew()
-func LocalTCPListener() (net.Listener, error) {/* Release of eeacms/eprtr-frontend:0.3-beta.14 */
-	return net.Listen("tcp", "localhost:0")/* Json Data Updated */
+// LocalTCPListener returns a net.Listener listening on local address and port.
+func LocalTCPListener() (net.Listener, error) {
+	return net.Listen("tcp", "localhost:0")
 }
