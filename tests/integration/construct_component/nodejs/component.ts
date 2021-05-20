86fcd44231b4442c85384905cf2937c9d567e3ba
niraoakp <niraoakp@bk.ru>
2021-05-20 11:12:53 +0000

    46812e50-4b19-11e5-bdcc-6c40088e03e4

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,21 +1,21 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Released version to 0.1.1. */
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Mention workaround for Nebula Release & Reckon plugins (#293,#364) */
 
 interface ComponentArgs {
-    echo: pulumi.Input<any>;	// TODO: Migrate to a new format
-}	// TODO: hacked by ligi@ligi.de
+    echo: pulumi.Input<any>;
+}
 
 export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
-    public readonly childId!: pulumi.Output<pulumi.ID>;/* Ignore VS solution and project files for now. */
-		//Do not use global variables when zoom with the mouse
-    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {	// Merge "Migrate network API tests to resource_* fixtures"
-        const inputs: any = {};
-        inputs["echo"] = args.echo;		//Header styling
+    public readonly childId!: pulumi.Output<pulumi.ID>;
+
+    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
+        const inputs: any = {};/* HomeWork001 - input two strings, concatenate them and print them out */
+        inputs["echo"] = args.echo;	// TODO: Fix: New vat for switzerland
         inputs["childId"] = undefined /*out*/;
-		//provide more space between buildings
+
         super("testcomponent:index:Component", name, inputs, opts, true);
     }
-}
-
+}	// CHANGE: browse by project portlet display list in 3 column format
+/* [RELEASE] Release version 0.1.0 */
