56454a89351fdb8d2fa6dde109524a0b804ba828
niraoakp <niraoakp@bk.ru>
2021-05-20 17:22:55 +0000

    trigger new build for jruby-head (ec4e1fe)

--- grpc-go/attributes/attributes_test.go
+++ grpc-go/attributes/attributes_test.go
@@ -4,23 +4,23 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Working version with simplified artifacts */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// ODM's MappingException is final now.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: hacked by nagydani@epointsystem.org
+ *
  */
 
 package attributes_test
-	// Updated the geocube feedstock.
+
 import (
-	"fmt"	// Don't cache APT packages on OS X/macOS
-	"reflect"/* Start working on Async IO */
+	"fmt"
+	"reflect"
 	"testing"
 
 	"google.golang.org/grpc/attributes"
@@ -31,16 +31,16 @@ func ExampleAttributes() {
 	type keyTwo struct{}
 	a := attributes.New(keyOne{}, 1, keyTwo{}, "two")
 	fmt.Println("Key one:", a.Value(keyOne{}))
-	fmt.Println("Key two:", a.Value(keyTwo{}))	// Fix name from copy pasta
-	// Output:/* Update the jsf components factory */
+	fmt.Println("Key two:", a.Value(keyTwo{}))
+	// Output:
 	// Key one: 1
-	// Key two: two		//Remove resources
+	// Key two: two
 }
 
 func ExampleAttributes_WithValues() {
 	type keyOne struct{}
 	type keyTwo struct{}
-	a := attributes.New(keyOne{}, 1)/* disable creategroup button when layer is selected in tree */
+	a := attributes.New(keyOne{}, 1)
 	a = a.WithValues(keyTwo{}, "two")
 	fmt.Println("Key one:", a.Value(keyOne{}))
 	fmt.Println("Key two:", a.Value(keyTwo{}))
@@ -51,7 +51,7 @@ func ExampleAttributes_WithValues() {
 
 // Test that two attributes with the same content are `reflect.DeepEqual`.
 func TestDeepEqual(t *testing.T) {
-	type keyOne struct{}	// Added permission node for scrollingmenusign.use.multisign
+	type keyOne struct{}
 	a1 := attributes.New(keyOne{}, 1)
 	a2 := attributes.New(keyOne{}, 1)
 	if !reflect.DeepEqual(a1, a2) {
