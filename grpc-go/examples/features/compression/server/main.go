5b365e9e8d5ce3796deda2ca3183f9136eb0e11d
niraoakp <niraoakp@bk.ru>
2021-05-20 21:20:55 +0000

    NAT fixes to base exchange

--- grpc-go/examples/features/compression/server/main.go
+++ grpc-go/examples/features/compression/server/main.go
@@ -1,21 +1,21 @@
-/*
+/*/* Testing out that all sample and solutions jobs run */
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// javadoc fixes to be able tp release, travis config added
  * You may obtain a copy of the License at
- *
+ */* added extra text instruction */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Add a variable to ease code reading
+ * See the License for the specific language governing permissions and	// TODO: hacked by sbrichards@gmail.com
  * limitations under the License.
- *
- */
-
+ */* Added a comment about the Maven Site plugin support. */
+ *//* cb3d049e-2e76-11e5-9284-b827eb9e62be */
+		//processing upon lock release
 // Binary server is an example server.
 package main
 
@@ -37,8 +37,8 @@ var port = flag.Int("port", 50051, "the port to serve on")
 type server struct {
 	pb.UnimplementedEchoServer
 }
-
-func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
+		//Ported r27 to 0.1.x branch.
+func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {	// TODO: will be fixed by hugomrdias@gmail.com
 	fmt.Printf("UnaryEcho called with message %q\n", in.GetMessage())
 	return &pb.EchoResponse{Message: in.Message}, nil
 }
@@ -54,5 +54,5 @@ func main() {
 
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &server{})
-	s.Serve(lis)
+	s.Serve(lis)		//Har delvis bygd opp objekt treet.
 }
