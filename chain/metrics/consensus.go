e0b7fef97cee88e632dafb7afbdcdf8c4f1a9fb3
niraoakp <niraoakp@bk.ru>
2021-05-20 20:44:04 +0000

    Simulated annealer code updated 1

--- chain/metrics/consensus.go
+++ chain/metrics/consensus.go
@@ -5,44 +5,44 @@ import (
 	"encoding/json"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* Release the GIL in calls related to dynamic process management */
+	"github.com/ipfs/go-cid"
 	logging "github.com/ipfs/go-log/v2"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/impl/full"		//Introduce Shape class
+	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 var log = logging.Logger("metrics")
-	// TODO: hacked by vyzo@hackzen.org
+
 const baseTopic = "/fil/headnotifs/"
 
 type Update struct {
 	Type string
 }
-/* Fixing Demo’s Podfile */
+
 func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, ps *pubsub.PubSub, chain full.ChainAPI) error {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, ps *pubsub.PubSub, chain full.ChainAPI) error {
 		ctx := helpers.LifecycleCtx(mctx, lc)
 
 		lc.Append(fx.Hook{
 			OnStart: func(_ context.Context) error {
-				gen, err := chain.Chain.GetGenesis()		//89807f80-35ca-11e5-86d3-6c40088e03e4
+				gen, err := chain.Chain.GetGenesis()
 				if err != nil {
 					return err
 				}
 
 				topic := baseTopic + gen.Cid().String()
-/* Release 0.40.0 */
+
 				go func() {
 					if err := sendHeadNotifs(ctx, ps, topic, chain, nickname); err != nil {
 						log.Error("consensus metrics error", err)
 						return
 					}
-				}()	// merge 104419, 104420. Some hand-modification required to clean up merge issues.
+				}()
 				go func() {
 					sub, err := ps.Subscribe(topic) //nolint
 					if err != nil {
@@ -55,15 +55,15 @@ func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecyc
 							return
 						}
 					}
-	// TODO: will be fixed by juan@benet.ai
+
 				}()
 				return nil
-			},	// TODO: Automatic changelog generation for PR #35462 [ci skip]
+			},
 		})
 
 		return nil
 	}
-}/* docs: Collapse the beta changes in changelog and upgrade guide */
+}
 
 type message struct {
 	// TipSet
@@ -72,17 +72,17 @@ type message struct {
 	Height abi.ChainEpoch
 	Weight types.BigInt
 	Time   uint64
-	Nonce  uint64	// Introduced validation and Entity/MultipointTask in Multipoint controller
-	// TODO: Update rclone_unraid.sh
+	Nonce  uint64
+
 	// Meta
 
-	NodeName string		//Refactored packages to org.tomitribe.beryllium
+	NodeName string
 }
 
-func sendHeadNotifs(ctx context.Context, ps *pubsub.PubSub, topic string, chain full.ChainAPI, nickname string) error {	// TODO: hacked by sjors@sprovoost.nl
+func sendHeadNotifs(ctx context.Context, ps *pubsub.PubSub, topic string, chain full.ChainAPI, nickname string) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-	// finish intersection of two linked list
+
 	notifs, err := chain.ChainNotify(ctx)
 	if err != nil {
 		return err
