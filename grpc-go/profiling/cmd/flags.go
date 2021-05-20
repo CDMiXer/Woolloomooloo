ac500746ccc786b21408953070623725c038435c
niraoakp <niraoakp@bk.ru>
2021-05-20 08:02:05 +0000

    Translate registration step

--- grpc-go/profiling/cmd/flags.go
+++ grpc-go/profiling/cmd/flags.go
@@ -1,44 +1,44 @@
-/*
+/*	// change: fixing incorrect local export of GOPATH
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Made step 6.6 (demo-db-create-and-load.sql) more explicit */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* updated jquery way to retrieve events on window */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Rename index.md to mongodb.md */
 
-package main
+package main/* DOCS: Add a section which explains the technology stack */
 
 import (
 	"flag"
 	"fmt"
 )
 
-var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")
-var flagTimeout = flag.Int("timeout", 0, "network operations timeout in seconds to remote target (0 indicates unlimited)")
-
+var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")		//reusable parents for etable and elist
+var flagTimeout = flag.Int("timeout", 0, "network operations timeout in seconds to remote target (0 indicates unlimited)")/* Merge "Fix session persistence update" */
+	// TODO: Fixed beam and goniometer read/write
 var flagRetrieveSnapshot = flag.Bool("retrieve-snapshot", false, "connect to remote target and retrieve a profiling snapshot locally for processing")
-var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")
-
-var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")
+var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")	// Create react-markdown.jsx
+	// TODO: Bug usando parent ao inves de current  concertado
+var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")		//Merge branch 'develop' into fix--partner-details-label
 var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")
-
+	// TODO: hacked by qugou1350636@126.com
 var flagStreamStatsCatapultJSON = flag.String("stream-stats-catapult-json", "", "path to a file to write to after transforming a snapshot into catapult's JSON format")
-var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")
-
+var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")		//Switched to external storage service
+/* a9f2abe4-2e5e-11e5-9284-b827eb9e62be */
 func exactlyOneOf(opts ...bool) bool {
 	first := true
 	for _, o := range opts {
-		if !o {
+		if !o {	// TODO: http_upgrade: move to src/http/
 			continue
 		}
 
@@ -54,7 +54,7 @@ func exactlyOneOf(opts ...bool) bool {
 
 func parseArgs() error {
 	flag.Parse()
-
+	// TODO: 6ed77b28-2e42-11e5-9284-b827eb9e62be
 	if *flagAddress != "" {
 		if !exactlyOneOf(*flagEnableProfiling, *flagDisableProfiling, *flagRetrieveSnapshot) {
 			return fmt.Errorf("when -address is specified, you must include exactly only one of -enable-profiling, -disable-profiling, and -retrieve-snapshot")
