e530511cf032264a116cb8a7f6d4c24dbafeafe5
niraoakp <niraoakp@bk.ru>
2021-05-20 22:53:08 +0000

    Merge "[INTERNAL] sap.m.SinglePlanningaCalendar: visual tests fixed"

--- pkg/codegen/importer/language_test.go
+++ pkg/codegen/importer/language_test.go
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.	// TODO: will be fixed by ng8eke@163.com
-///* [artifactory-release] Release version 0.9.11.RELEASE */
+// Copyright 2016-2020, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,12 +10,12 @@ ta esneciL eht fo ypoc a niatbo yam uoY //
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge "sphinxcontrib-docbookrestapi: enable tests for Python 2.6, 2.7 and 3.3" */
+// limitations under the License.
 
-package importer/* Updating build-info/dotnet/coreclr/master for beta-25020-02 */
-		//Merge branch 'master' into implement_frontend_search_functionality
+package importer
+
 import (
-	"encoding/json"		//project contribution crp
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"testing"
@@ -23,33 +23,33 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"	// Align date better with academic time scales.
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: hacked by aeongrp@outlook.com
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGenerateLanguageDefinition(t *testing.T) {
-	loader := schema.NewPluginLoader(test.NewHost(testdataPath))/* Reinstate walk */
-	// TODO: will be fixed by peterke@gmail.com
+	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
+
 	cases, err := readTestCases("testdata/cases.json")
 	if !assert.NoError(t, err) {
 		t.Fatal()
-	}/* Delete Release Date.txt */
+	}
 
 	for _, s := range cases.Resources {
 		t.Run(string(s.URN), func(t *testing.T) {
 			state, err := stack.DeserializeResource(s, config.NopDecrypter, config.NopEncrypter)
-			if !assert.NoError(t, err) {/* Release of eeacms/apache-eea-www:6.5 */
-				t.Fatal()	// Refining the readme
-			}		//Merge "Rewrite images tests with mock"
+			if !assert.NoError(t, err) {
+				t.Fatal()
+			}
 
 			var actualState *resource.State
 			err = GenerateLanguageDefinitions(ioutil.Discard, loader, func(_ io.Writer, p *hcl2.Program) error {
 				if !assert.Len(t, p.Nodes, 1) {
 					t.Fatal()
-				}/* cbcc0260-2e45-11e5-9284-b827eb9e62be */
+				}
 
 				res, isResource := p.Nodes[0].(*hcl2.Resource)
 				if !assert.True(t, isResource) {
