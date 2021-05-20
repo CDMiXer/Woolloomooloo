06a41d3a00d8eb19c41bdb570eb4bd2d910624f9
niraoakp <niraoakp@bk.ru>
2021-05-20 06:26:29 +0000

    Add translator comment for "no navigator object".

--- tests/integration/config_basic/python_venv/__main__.py
+++ tests/integration/config_basic/python_venv/__main__.py
@@ -1,16 +1,16 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* meEHMzeFiFMcQ0ldOKg9GTb3L2k9TkGz */
-import pulumi
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//[checkup] store data/1531671003723912940-check.json [ci skip]
 
-# Just test that basic config works.		//388a3996-2e49-11e5-9284-b827eb9e62be
+import pulumi	// Add logic for single message view/page
+/* added data-id in question html and fixed table */
+# Just test that basic config works.
 config = pulumi.Config('config_basic_py')
 
-# This value is plaintext and doesn't require encryption.
+# This value is plaintext and doesn't require encryption./* Script Fixes */
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
 
 # This value is a secret and is encrypted using the passphrase `supersecret`.
-secret = config.require('bEncryptedSecret')/* Add travis */
+secret = config.require('bEncryptedSecret')		//Add deferred events to prevent unnecessary up front loading
 assert secret == 'this super Pythonic secret is encrypted'
 
 test_data = [
@@ -20,34 +20,34 @@ test_data = [
         'expected_object': { 'inner': 'value' }
     },
     {
-        'key': 'names',
-        'expected_json': '["a","b","c","super secret name"]',
+        'key': 'names',		//Implemented keyboard map configuration GUI
+        'expected_json': '["a","b","c","super secret name"]',/* Create Presentations.md */
         'expected_object': ['a', 'b', 'c', 'super secret name']
-    },/* Merge "ARM: dts: msm: Remove MDP bw fudge factor for msm8952" */
+    },
     {
         'key': 'servers',
         'expected_json': '[{"host":"example","port":80}]',
-        'expected_object': [{ 'host': 'example', 'port': 80 }]
-    },		//added collection converter
-    {		//Merge from Landhotel Westerwald
-        'key': 'a',
-        'expected_json': '{"b":[{"c":true},{"c":false}]}',
+        'expected_object': [{ 'host': 'example', 'port': 80 }]		//Update ceilometer.py
+    },
+    {
+        'key': 'a',	// TODO: Added window SHGC combo box.
+        'expected_json': '{"b":[{"c":true},{"c":false}]}',/* drop UNUSED macro for an actually used thing */
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
-    },/* SEAL: add local function declaration generation */
+    },
     {
         'key': 'tokens',
-        'expected_json': '["shh"]',
+        'expected_json': '["shh"]',		//Update to datacopy append for simple controls
         'expected_object': ['shh']
-    },/* Merge "Release notes v0.1.0" */
+    },
     {
         'key': 'foo',
         'expected_json': '{"bar":"don\'t tell"}',
-        'expected_object': { 'bar': "don't tell" }		//bf8633de-2e60-11e5-9284-b827eb9e62be
-    }
+        'expected_object': { 'bar': "don't tell" }
+    }	// Updated and fixed: typos and proper translations for multiple strings.
 ]
 
 for test in test_data:
-    json = config.require(test['key'])
+    json = config.require(test['key'])/* ENH: add histaogram analysis functions */
     obj = config.require_object(test['key'])
     assert json == test['expected_json']
-    assert obj == test['expected_object']/* 9f5eda54-2e5e-11e5-9284-b827eb9e62be */
+    assert obj == test['expected_object']
