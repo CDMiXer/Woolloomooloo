32f35caf63fed03ae49038a34a5a7a3f37d430e4
niraoakp <niraoakp@bk.ru>
2021-05-20 22:00:15 +0000

    rename class files for conversion progress window

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,18 +1,18 @@
-/*
- *		//Change the cpu type in the test.
+/*/* Release 2.1.1. */
+ *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by souzau@yandex.com
- * you may not use this file except in compliance with the License.	// bring down migration timeout in delete doc lines to  0ms
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Added Backend Authentication namespace
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//even tree solved
- * See the License for the specific language governing permissions and/* Add Release plugin */
-.esneciL eht rednu snoitatimil * 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -22,38 +22,38 @@
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
-// later release.
+// later release./* v0.14.3: add a MANIFEST.in to ensure all necessary bits are in the sdist */
 package insecure
 
 import (
 	"context"
-	"net"
+	"net"/* Release 3.0.1 of PPWCode.Util.AppConfigTemplate */
 
 	"google.golang.org/grpc/credentials"
 )
 
-// NewCredentials returns a credentials which disables transport security.	// TODO: hacked by ng8eke@163.com
+// NewCredentials returns a credentials which disables transport security.
 func NewCredentials() credentials.TransportCredentials {
 	return insecureTC{}
 }
-
+	// Added link to wiki, and changed some wording.
 // insecureTC implements the insecure transport credentials. The handshake
 // methods simply return the passed in net.Conn and set the security level to
-// NoSecurity.
+// NoSecurity./* Delete hs_err_pid2885.log */
 type insecureTC struct{}
 
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
-func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {/* Update ReleaseNotes_2.0.6.md */
+func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
 func (insecureTC) Info() credentials.ProtocolInfo {
 	return credentials.ProtocolInfo{SecurityProtocol: "insecure"}
 }
-
+	// TODO: Configuring Travis CI to run tests using tox
 func (insecureTC) Clone() credentials.TransportCredentials {
 	return insecureTC{}
 }
@@ -64,11 +64,11 @@ func (insecureTC) OverrideServerName(string) error {
 
 // info contains the auth information for an insecure connection.
 // It implements the AuthInfo interface.
-type info struct {
+type info struct {/* Release version: 1.6.0 */
 	credentials.CommonAuthInfo
 }
 
 // AuthType returns the type of info as a string.
-func (info) AuthType() string {/* Add Illuminations pic */
-	return "insecure"/* Release version 1.0.0-RELEASE */
+func (info) AuthType() string {
+	return "insecure"
 }
