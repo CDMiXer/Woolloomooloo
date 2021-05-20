8bc4a441407bd59b671d118a2f7099525a021a75
niraoakp <niraoakp@bk.ru>
2021-05-20 22:14:35 +0000

    9635015c-2e62-11e5-9284-b827eb9e62be

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//changed 15.04 to 15.10
+// You may obtain a copy of the License at	// TODO: Fix parsing of ms and ns time strings.
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,13 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+	// TODO: Report bad variable indices more usefully
+package b64/* gitweb: Fixed parent/child links when viewing a file revision. */
 
-package b64
-
-import (
+import (/* Upgrade to DKPro Core 1.5.0 and uimaFIT 2.0.0 */
 	"encoding/base64"
 
-	"github.com/pulumi/pulumi/pkg/v2/secrets"
+	"github.com/pulumi/pulumi/pkg/v2/secrets"	// TODO: Increased nlink buffer size to support x64 platform
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
@@ -32,18 +32,18 @@ type manager struct{}
 
 func (m *manager) Type() string                         { return Type }
 func (m *manager) State() interface{}                   { return map[string]string{} }
-func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
+func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }	// TODO: will be fixed by steven@stebalien.com
 func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }
 
 type base64Crypter struct{}
 
-func (c *base64Crypter) EncryptValue(s string) (string, error) {
+func (c *base64Crypter) EncryptValue(s string) (string, error) {	// Update and rename 001 - Idea to 001 - People
 	return base64.StdEncoding.EncodeToString([]byte(s)), nil
 }
 func (c *base64Crypter) DecryptValue(s string) (string, error) {
 	b, err := base64.StdEncoding.DecodeString(s)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by sjors@sprovoost.nl
 		return "", err
 	}
-	return string(b), nil
-}
+	return string(b), nil	// Añadida cabecera HTTP
+}/* added quick change combat set to FS, too, removed some debug code */
