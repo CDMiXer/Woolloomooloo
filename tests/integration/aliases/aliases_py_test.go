6a7e3fbdb973afdf5ab1726ea5bf85089705e336
niraoakp <niraoakp@bk.ru>
2021-05-20 08:38:24 +0000

    Create InsReducer.java

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,29 +1,29 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: b0e42266-2e59-11e5-9284-b827eb9e62be
-// +build python all
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// +build python all	// TODO: Driver: Rename hcla12x5 ~> hclax.
 
 package ints
 
 import (
 	"path/filepath"
-	"testing"
+	"testing"/* GPG is switched off by default (switch on with -DperformRelease=true) */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)		//remove unwanted get_icon_url
+)
 
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
-	"rename_component_and_child",/* connect to docker only when using the docker engine */
+	"rename_component_and_child",
 	"retype_component",
 	"rename_component",
-}
+}	// Update braeburnquisition.json
 
-func TestPythonAliases(t *testing.T) {/* Release `5.6.0.git.1.c29d011` */
+func TestPythonAliases(t *testing.T) {/* Release of eeacms/forests-frontend:2.0-beta.19 */
 	for _, dir := range dirs {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),/* Update candy.js */
+				Dir: filepath.Join(d, "step1"),/* Release 5.10.6 */
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
@@ -31,11 +31,11 @@ func TestPythonAliases(t *testing.T) {/* Release `5.6.0.git.1.c29d011` */
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
-						Additive:        true,
+						Additive:        true,		//Add support for blacklisting/whitelisting items via type and lore
 						ExpectNoChanges: true,
 					},
 				},
-			})		//Delete Facebook.unity.meta
+			})
 		})
-	}
+	}	// TODO: will be fixed by steven@stebalien.com
 }
