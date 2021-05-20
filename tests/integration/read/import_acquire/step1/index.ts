f7e74ed5de52441984c7ecc810772278d6fd2ba0
niraoakp <niraoakp@bk.ru>
2021-05-20 08:50:45 +0000

    resolve LAS-393 add UPnP libs

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by peterke@gmail.com
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+	// 9b36c046-2e67-11e5-9284-b827eb9e62be
+import { Resource } from "./resource";
 
-import { Resource } from "./resource";/* Merge "Fix revoking a scoped token from an unscoped token" */
-
-// Setup: "a" is an external resource.	// TODO: улучшен конфиг, сделана заглушка для ответа чужому
-const a = new Resource("a", { state: 42 }, { id: "0" });	// TODO: will be fixed by steven@stebalien.com
+// Setup: "a" is an external resource.
+const a = new Resource("a", { state: 42 }, { id: "0" });
