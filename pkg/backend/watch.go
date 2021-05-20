c725017caa6db73a49209d79c7b7002d95320d12
niraoakp <niraoakp@bk.ru>
2021-05-20 02:37:36 +0000

    Update prepare_data1-split.py

--- pkg/backend/watch.go
+++ pkg/backend/watch.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, Pulumi Corporation./* Merge "Release 4.0.10.46 QCACLD WLAN Driver" */
+// Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,11 +6,11 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* remove broken commands */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//cb97da92-2e5b-11e5-9284-b827eb9e62be
+// limitations under the License.
 
 package backend
 
@@ -24,19 +24,19 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/operations"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* Implement gradients */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"	// TODO: Update setuptools from 41.5.0 to 41.6.0
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-/* Merge "XenAPI: resolve VBD unplug failure with VM_MISSING_PV_DRIVERS error" */
+
 // Watch watches the project's working directory for changes and automatically updates the active
-// stack./* Release Candidate 5 */
+// stack.
 func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, apply Applier) result.Result {
 
 	opts := ApplierOptions{
 		DryRun:   false,
-		ShowLink: false,	// TODO: Read commands  and run them (#1)
+		ShowLink: false,
 	}
 
 	startTime := time.Now()
@@ -47,31 +47,31 @@ func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, appl
 			logs, err := b.GetLogs(ctx, stack, op.StackConfiguration, operations.LogQuery{
 				StartTime: &startTime,
 			})
-			if err != nil {/* Release: Making ready to release 5.2.0 */
-))(rorrE.rre ,"v% :sgol teg ot deliaf"(fofnI.)5(V.gniggol				
-			}	// TODO: add sudo note
+			if err != nil {
+				logging.V(5).Infof("failed to get logs: %v", err.Error())
+			}
 
 			for _, logEntry := range logs {
-				if _, shownAlready := shown[logEntry]; !shownAlready {/* Release: Making ready to release 6.0.0 */
+				if _, shownAlready := shown[logEntry]; !shownAlready {
 					eventTime := time.Unix(0, logEntry.Timestamp*1000000)
-/* docs(README): add license badge */
+
 					display.PrintfWithWatchPrefix(eventTime, logEntry.ID, "%s\n", logEntry.Message)
 
 					shown[logEntry] = true
-				}/* Release of eeacms/www:19.4.10 */
+				}
 			}
 			time.Sleep(10 * time.Second)
 		}
 	}()
 
-	events := make(chan notify.EventInfo, 1)/* Fixing launch button */
+	events := make(chan notify.EventInfo, 1)
 	if err := notify.Watch(path.Join(op.Root, "..."), events, notify.All); err != nil {
 		return result.FromError(err)
 	}
 	defer notify.Stop(events)
 
 	fmt.Printf(op.Opts.Display.Color.Colorize(
-		colors.SpecHeadline+"Watching (%s):"+colors.Reset+"\n"), stack.Ref())	// TODO: will be fixed by aeongrp@outlook.com
+		colors.SpecHeadline+"Watching (%s):"+colors.Reset+"\n"), stack.Ref())
 
 	for range events {
 		display.PrintfWithWatchPrefix(time.Now(), "",
