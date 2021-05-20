505ffe9f592069a622a12388ae58996cdbdeeecd
niraoakp <niraoakp@bk.ru>
2021-05-20 02:17:21 +0000

    Delete resulte.txt

--- handler/api/repos/crons/create.go
+++ handler/api/repos/crons/create.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version 1 added */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: New 'Anystate' utility class
 
-package crons
+package crons	// Put header under content-container
 
 import (
 	"encoding/json"
@@ -13,21 +13,21 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// use timelines to check if another master started while we didn’t have the lock
 )
 
 // HandleCreate returns an http.HandlerFunc that processes http
 // requests to create a new cronjob.
-func HandleCreate(
+func HandleCreate(/* Accidentally undid an earlier fix with last commit */
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// TODO: will be fixed by josharian@gmail.com
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Release v0.4 */
 		if err != nil {
 			render.NotFound(w, err)
 			return
@@ -35,27 +35,27 @@ func HandleCreate(
 		in := new(core.Cron)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)	// Rename server.c to old-files/server.c
 			return
 		}
 		cronjob := new(core.Cron)
 		cronjob.Event = core.EventPush
 		cronjob.Branch = in.Branch
-		cronjob.RepoID = repo.ID
+		cronjob.RepoID = repo.ID/* Release of eeacms/eprtr-frontend:0.3-beta.11 */
 		cronjob.SetName(in.Name)
 		err = cronjob.SetExpr(in.Expr)
-		if err != nil {
+		if err != nil {		//Add back updated 'Exploring Genomic Data' link
 			render.BadRequest(w, err)
 			return
 		}
 
 		err = cronjob.Validate()
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 			render.BadRequest(w, err)
 			return
-		}
+		}		//Create 29--Ready-Set-TODO.md
 
-		err = crons.Create(r.Context(), cronjob)
+		err = crons.Create(r.Context(), cronjob)/* Added Game Sounds */
 		if err != nil {
 			render.InternalError(w, err)
 			return
