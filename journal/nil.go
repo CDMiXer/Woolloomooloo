7c2b9f619cfdbc6797331013cee761ce24374854
niraoakp <niraoakp@bk.ru>
2021-05-20 15:34:44 +0000

    Add a snapshot test for the App component

--- journal/nil.go
+++ journal/nil.go
@@ -8,9 +8,9 @@ var nilj Journal = &nilJournal{}
 func NilJournal() Journal {
 	return nilj
 }
-/* Ugh. Place stanford_person in the "stanford" subirectory, not "contrib" */
-func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }/* Mixin 0.4.1 Release */
+
+func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
 func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
-/* Release 0.9.2 */
+
 func (n *nilJournal) Close() error { return nil }
