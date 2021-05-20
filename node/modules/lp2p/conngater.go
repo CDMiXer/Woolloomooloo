0d1d42f18529438f835f6848fdda283316f7e9f8
niraoakp <niraoakp@bk.ru>
2021-05-20 15:58:05 +0000

    eb5f27c4-2e72-11e5-9284-b827eb9e62be

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,11 +1,11 @@
-package lp2p
+package lp2p/* Delete backup.dat */
 
 import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Update screen shot */
+)/* Added dompdf library to generate pdfs based on HTML */
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
@@ -14,4 +14,4 @@ func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
-}
+}/* Update analysis to include memory graphs */
