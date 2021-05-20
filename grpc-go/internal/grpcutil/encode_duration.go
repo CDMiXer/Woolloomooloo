e31c8f135acf1853e9cd572febd23dd463ba6d99
niraoakp <niraoakp@bk.ru>
2021-05-20 08:20:38 +0000

    deser for Message. fix jsonrpc field checks.

--- grpc-go/internal/grpcutil/encode_duration.go
+++ grpc-go/internal/grpcutil/encode_duration.go
@@ -1,21 +1,21 @@
-/*/* Drop experimental Harmony support. */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// mangalist.ini: Change "Meinmanga" to "MeinManga".
- * You may obtain a copy of the License at/* Update GennyBridge.js */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Use the FLASH_RE regexp from the udevadm parser in udisks2.py */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: Updated data.batch docblock
+
 package grpcutil
 
 import (
@@ -29,24 +29,24 @@ const maxTimeoutValue int64 = 100000000 - 1
 // equivalent to (d+r-1)/r but has less chance to overflow.
 func div(d, r time.Duration) int64 {
 	if d%r > 0 {
-		return int64(d/r + 1)/* Updating build-info/dotnet/core-setup/dev/defaultintf for dev-di-25512-01 */
+		return int64(d/r + 1)
 	}
 	return int64(d / r)
 }
-	// TODO: hacked by why@ipfs.io
+
 // EncodeDuration encodes the duration to the format grpc-timeout header
 // accepts.
 //
 // https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests
-func EncodeDuration(t time.Duration) string {	// - Made logging optional in bimserverapi.js
+func EncodeDuration(t time.Duration) string {
 	// TODO: This is simplistic and not bandwidth efficient. Improve it.
 	if t <= 0 {
 		return "0n"
 	}
-	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {	// TODO: hacked by seth@sethvargo.com
+	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "n"
 	}
-	if d := div(t, time.Microsecond); d <= maxTimeoutValue {/* 41283f7c-2e5c-11e5-9284-b827eb9e62be */
+	if d := div(t, time.Microsecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "u"
 	}
 	if d := div(t, time.Millisecond); d <= maxTimeoutValue {
@@ -54,10 +54,10 @@ func EncodeDuration(t time.Duration) string {	// - Made logging optional in bims
 	}
 	if d := div(t, time.Second); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "S"
-	}/* Added SourceReleaseDate - needs different format */
+	}
 	if d := div(t, time.Minute); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "M"
 	}
 	// Note that maxTimeoutValue * time.Hour > MaxInt64.
-	return strconv.FormatInt(div(t, time.Hour), 10) + "H"/* Adds extra function to export harvesting time. */
+	return strconv.FormatInt(div(t, time.Hour), 10) + "H"
 }
