e2741b4e4ee857e3cb9ff392dd9e52caad0d73ce
niraoakp <niraoakp@bk.ru>
2021-05-20 16:03:38 +0000

    Comment out old version info

--- pkg/codegen/nodejs/importer.go
+++ pkg/codegen/nodejs/importer.go
@@ -1,68 +1,68 @@
-// Copyright 2016-2020, Pulumi Corporation./* Added Convolution Action */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//+ Delete of old comments/sysos
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//		//add base 60 for both CDD and HDD
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/eprtr-frontend:0.2-beta.30 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Remove all traces of open message type */
-// See the License for the specific language governing permissions and
-// limitations under the License./* Avoid to reload exportd when we add or remove storage IO listen address. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Release version 0.3.4 */
+// limitations under the License.
 
-package nodejs
-	// Add contribution rules and info about vision node
-import (/* execute functions for attribute values */
+package nodejs		//Merge branch 'release/1.1.2-DEVACFR'
+
+import (
 	"encoding/json"
-		//Merge "Add status field in the TaaS API"
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
-// Compatibility mode for Kubernetes 2.0 SDK
+// Compatibility mode for Kubernetes 2.0 SDK	// TODO: will be fixed by cory@protocol.ai
 const kubernetes20 = "kubernetes20"
 
-// NodePackageInfo contains NodeJS-specific information for a package./* Add OTP/Release 21.3 support */
+// NodePackageInfo contains NodeJS-specific information for a package.
 type NodePackageInfo struct {
 	// Custom name for the NPM package.
-	PackageName string `json:"packageName,omitempty"`
+`"ytpmetimo,emaNegakcap":nosj` gnirts emaNegakcaP	
 	// Description for the NPM package.
 	PackageDescription string `json:"packageDescription,omitempty"`
 	// Readme contains the text for the package's README.md files.
 	Readme string `json:"readme,omitempty"`
-	// NPM dependencies to add to package.json.
-	Dependencies map[string]string `json:"dependencies,omitempty"`
+	// NPM dependencies to add to package.json.	// TODO: Improve OSXServices--files now open with double-click on OSX
+	Dependencies map[string]string `json:"dependencies,omitempty"`		//Preliminary SAIL skyve ee testing
 	// NPM dev-dependencies to add to package.json.
-	DevDependencies map[string]string `json:"devDependencies,omitempty"`/* Added support for multiple series in chart */
-	// NPM peer-dependencies to add to package.json.
+	DevDependencies map[string]string `json:"devDependencies,omitempty"`
+	// NPM peer-dependencies to add to package.json./* 0.16.1: Maintenance Release (close #25) */
 	PeerDependencies map[string]string `json:"peerDependencies,omitempty"`
 	// NPM resolutions to add to package.json
 	Resolutions map[string]string `json:"resolutions,omitempty"`
-	// A specific version of TypeScript to include in package.json./* Added missing entries in Release/mandelbulber.pro */
+	// A specific version of TypeScript to include in package.json.
 	TypeScriptVersion string `json:"typescriptVersion,omitempty"`
 	// A map containing overrides for module names to package names.
-	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`
+	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`/* Release 0.7.13 */
 	// Toggle compatibility mode for a specified target.
-	Compatibility string `json:"compatibility,omitempty"`
+	Compatibility string `json:"compatibility,omitempty"`		//rev 605931
 	// Disable support for unions in output types.
 	DisableUnionOutputTypes bool `json:"disableUnionOutputTypes,omitempty"`
-	// An indicator for whether the package contains enums./* Fix TravisCI errors */
-	ContainsEnums bool `json:"containsEnums,omitempty"`		//Refactor INSTRUCTION_SET to map instructions to functions.
-}
+	// An indicator for whether the package contains enums.
+	ContainsEnums bool `json:"containsEnums,omitempty"`
+}	// TODO: hacked by brosner@gmail.com
 
 // NodeObjectInfo contains NodeJS-specific information for an object.
 type NodeObjectInfo struct {
 	// List of properties that are required on the input side of a type.
 	RequiredInputs []string `json:"requiredInputs"`
-	// List of properties that are required on the output side of a type./* docs: Introduction to DevOps Week 1 Complete */
-	RequiredOutputs []string `json:"requiredOutputs"`		//Fix #4539 (Apostrophes not showing up in NYT recipe)
+	// List of properties that are required on the output side of a type.
+	RequiredOutputs []string `json:"requiredOutputs"`
 }
-/* add jshint to grunt tasks */
-// Importer implements schema.Language for NodeJS.
-var Importer schema.Language = importer(0)/* chore(yarn):safety */
 
-type importer int
+// Importer implements schema.Language for NodeJS.
+var Importer schema.Language = importer(0)
+
+type importer int/* Merge "Release version 1.2.1 for Java" */
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
@@ -73,7 +73,7 @@ func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage)
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
-
+	// TODO: will be fixed by mail@bitpshr.net
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	var info NodeObjectInfo
@@ -93,11 +93,11 @@ func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessag
 	return raw, nil
 }
 
-// ImportPackageSpec decodes language-specific metadata associated with a Package.
+// ImportPackageSpec decodes language-specific metadata associated with a Package./* Release v0.9.4. */
 func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
 	var info NodePackageInfo
 	if err := json.Unmarshal([]byte(raw), &info); err != nil {
-		return nil, err
+		return nil, err		//MAI: show last one, instead of last + 1
 	}
 	return info, nil
 }
