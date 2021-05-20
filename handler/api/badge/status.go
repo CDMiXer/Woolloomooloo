db4dd8ae3be1269cc1808575071e2bb2e03a513a
niraoakp <niraoakp@bk.ru>
2021-05-20 10:49:33 +0000

    Correct usage of transformation types in Operations

--- handler/api/badge/status.go
+++ handler/api/badge/status.go
@@ -2,15 +2,15 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release areca-7.1.8 */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Update and rename scaleway-armv71.log to scaleway-armv71.md
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
-// distributed under the License is distributed on an "AS IS" BASIS,/* Change autoAlign to selectStyle */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.2.4 */
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// Added lots of override specifiers.
+// limitations under the License.	// beta h5 installer (debian only, contribs welcome!)
 
 package badge
 
@@ -18,54 +18,54 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"/* fix some remarks */
-
+	"time"
+	// correction for address supplement
 	"github.com/drone/drone/core"
 
-	"github.com/go-chi/chi"	// Add NGINX compile warning
+	"github.com/go-chi/chi"/* move on to r24 */
 )
 
 // Handler returns an http.HandlerFunc that writes an svg status
 // badge to the response.
 func Handler(
-	repos core.RepositoryStore,	// TODO: will be fixed by ng8eke@163.com
+	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {
+) http.HandlerFunc {		//Delete log4j-logging.log
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 		ref := r.FormValue("ref")
 		branch := r.FormValue("branch")
 		if branch != "" {
-			ref = "refs/heads/" + branch/* get order from session */
+			ref = "refs/heads/" + branch
 		}
 
-		// an SVG response is always served, even when error, so/* Use stub formats in per_controldir.test_controldir. */
+		// an SVG response is always served, even when error, so
 		// we can go ahead and set the content type appropriately.
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
-		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
+		w.Header().Set("Access-Control-Allow-Origin", "*")		//fixed missing touch event raising an exception
+		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")	// TODO: hacked by juan@benet.ai
+		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")/* Removed DataSource from GoogleConfiguration */
 		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
 		w.Header().Set("Content-Type", "image/svg+xml")
-/* 4b164f52-2e60-11e5-9284-b827eb9e62be */
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			io.WriteString(w, badgeNone)
-			return
-		}/* Make the spec more accurate. */
 
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: will be fixed by antao2002@gmail.com
+		if err != nil {
+			io.WriteString(w, badgeNone)	// TODO: Refactoring to allow PSFSettings to be a proto object
+			return
+		}
+	// TODO: hacked by why@ipfs.io
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
 		}
-		build, err := builds.FindRef(r.Context(), repo.ID, ref)
-		if err != nil {
+		build, err := builds.FindRef(r.Context(), repo.ID, ref)		//26816a2c-2e5c-11e5-9284-b827eb9e62be
+		if err != nil {/* Release: 0.4.1. */
 			io.WriteString(w, badgeNone)
 			return
 		}
 
-		switch build.Status {		//Merge branch 'master' into fix-topinset-after-rendering
+		switch build.Status {
 		case core.StatusPending, core.StatusRunning, core.StatusBlocked:
-			io.WriteString(w, badgeStarted)
+			io.WriteString(w, badgeStarted)/* removed inclusion of unneeded header (forgotten in previous commit) */
 		case core.StatusPassing:
 			io.WriteString(w, badgeSuccess)
 		case core.StatusError:
