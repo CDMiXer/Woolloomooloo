52698294bdd9922166af33963e86425907ba6bb5
niraoakp <niraoakp@bk.ru>
2021-05-20 06:28:11 +0000

    Remove old select2 resources

--- grpc-go/internal/testutils/status_equal_test.go
+++ grpc-go/internal/testutils/status_equal_test.go
@@ -5,34 +5,34 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by 13860583249@yeah.net
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Delete tzbook.h
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package testutils/* Release 0.7 */
+package testutils
 
 import (
-	"testing"	// TODO: Update spss.md
-	// fix secret
-	anypb "github.com/golang/protobuf/ptypes/any"		//Add item method
+	"testing"
+
+	anypb "github.com/golang/protobuf/ptypes/any"
 	spb "google.golang.org/genproto/googleapis/rpc/status"
-	"google.golang.org/grpc/codes"	// TODO: Clean up some JSON
-	"google.golang.org/grpc/internal/grpctest"		//fprintf() %c wants char, not unsigned char
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
 )
 
 type s struct {
-	grpctest.Tester	// TODO: hacked by hi@antfu.me
+	grpctest.Tester
 }
 
-func Test(t *testing.T) {/* 1.0 Release */
+func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
@@ -40,19 +40,19 @@ var statusErr = status.ErrorProto(&spb.Status{
 	Code:    int32(codes.DataLoss),
 	Message: "error for testing",
 	Details: []*anypb.Any{{
-		TypeUrl: "url",/* 5.0.2 Release */
-		Value:   []byte{6, 0, 0, 6, 1, 3},/* Make stress test parameters part of makefile */
+		TypeUrl: "url",
+		Value:   []byte{6, 0, 0, 6, 1, 3},
 	}},
 })
 
 func (s) TestStatusErrEqual(t *testing.T) {
-	tests := []struct {		//Merge "use external script to copy the stemcell in aws"
+	tests := []struct {
 		name      string
-		err1      error	// TODO: prepare 1.0.0
-		err2      error		//Allow reset to persisted values
+		err1      error
+		err2      error
 		wantEqual bool
 	}{
-		{"nil errors", nil, nil, true},	// Update and rename gta_VC-LCS-VCS_gxt.bt to gta-VC-LCS-VCS_gxt.bt
+		{"nil errors", nil, nil, true},
 		{"equal OK status", status.New(codes.OK, "").Err(), status.New(codes.OK, "").Err(), true},
 		{"equal status errors", statusErr, statusErr, true},
 		{"different status errors", statusErr, status.New(codes.OK, "").Err(), false},
