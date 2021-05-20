6ab5e80170ed6c0de68cc4c62589dde6b6dcacff
niraoakp <niraoakp@bk.ru>
2021-05-20 15:20:14 +0000

    Fixed creation of game configuration during game creation.

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Sticky header example */
+
 import * as pulumi from "@pulumi/pulumi";
-		//Started the regex literal.
+
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
-    }
+        super("my:module:Resource", name, {}, opts);	// TODO: hacked by remco@dutchcoders.io
+    }		//fixed bad octave in test case
 }
-	// TODO: hacked by steven@stebalien.com
+
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
-    aliases: [{ name: "res1" }],
+    aliases: [{ name: "res1" }],	// Now working on Linux
 });
