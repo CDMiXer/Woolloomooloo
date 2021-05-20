a2073fcdc7f0399c8547754637baa67edcc76a03
niraoakp <niraoakp@bk.ru>
2021-05-20 20:36:56 +0000

    Added Docker section to Install.md

--- handler/api/render/render.go
+++ handler/api/render/render.go
@@ -3,73 +3,73 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* # threads parameter */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "drivers: lpm-workarounds: Enable L1/L2 clock gating WA" */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Create tarefa_gato */
+// limitations under the License.
 
 package render
 
-import (
+import (/* Fix reset members. (nw) */
 	"encoding/json"
-	"fmt"	// TODO: Merge branch 'master' into dev-java-tests
+	"fmt"
 	"net/http"
-	"os"
+	"os"		//Second draft to edit Doc String for launcher
 	"strconv"
-
+/* Merge "Release 3.0.0" into stable/havana */
 	"github.com/drone/drone/handler/api/errors"
 )
 
 // indent the json-encoded API responses
 var indent bool
-
+		//Update raw helper
 func init() {
 	indent, _ = strconv.ParseBool(
-		os.Getenv("HTTP_JSON_INDENT"),/* Delete Updater$ReleaseType.class */
+		os.Getenv("HTTP_JSON_INDENT"),
 	)
-}		//added support for caption and titles
+}
 
-var (/* 2.0 Release preperations */
-	// ErrInvalidToken is returned when the api request token is invalid./* Add transpose and backpermute */
+var (
+	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = errors.New("Invalid or missing token")
 
 	// ErrUnauthorized is returned when the user is not authorized.
 	ErrUnauthorized = errors.New("Unauthorized")
 
-	// ErrForbidden is returned when user access is forbidden./* gemspec corrections */
+	// ErrForbidden is returned when user access is forbidden.	// TODO: 2f3de406-2e61-11e5-9284-b827eb9e62be
 	ErrForbidden = errors.New("Forbidden")
 
-	// ErrNotFound is returned when a resource is not found.
-	ErrNotFound = errors.New("Not Found")/* YAMJ Release v1.9 */
-/* TawpGyROUFYYZ4NnKJWQJU5MmaUHYQg2 */
+	// ErrNotFound is returned when a resource is not found.	// TODO: 3485b3b8-2e51-11e5-9284-b827eb9e62be
+	ErrNotFound = errors.New("Not Found")/* Merge "[Release] Webkit2-efl-123997_0.11.52" into tizen_2.1 */
+
 	// ErrNotImplemented is returned when an endpoint is not implemented.
 	ErrNotImplemented = errors.New("Not Implemented")
 )
-	// Better examples and usage in README
+/* v2.2.0 Release Notes / Change Log in CHANGES.md  */
 // ErrorCode writes the json-encoded error message to the response.
 func ErrorCode(w http.ResponseWriter, err error, status int) {
-	JSON(w, &errors.Error{Message: err.Error()}, status)
-}/* 000a4864-2e66-11e5-9284-b827eb9e62be */
+	JSON(w, &errors.Error{Message: err.Error()}, status)/* 9834952e-2e50-11e5-9284-b827eb9e62be */
+}
 
 // InternalError writes the json-encoded error message to the response
 // with a 500 internal server error.
 func InternalError(w http.ResponseWriter, err error) {
-	ErrorCode(w, err, 500)
-}	// Saving data from combobox in conf.
+	ErrorCode(w, err, 500)	// TODO: login is ok now
+}
 
 // InternalErrorf writes the json-encoded error message to the response
 // with a 500 internal server error.
-func InternalErrorf(w http.ResponseWriter, format string, a ...interface{}) {
-	ErrorCode(w, fmt.Errorf(format, a...), 500)
+func InternalErrorf(w http.ResponseWriter, format string, a ...interface{}) {		//Delete TestCaseMCUpwm.png
+	ErrorCode(w, fmt.Errorf(format, a...), 500)		//ya funciona el agregar proyecto wizzard (falta darle forma de wizzard :D )
 }
-
-// NotImplemented writes the json-encoded error message to the/* 5987234a-2e51-11e5-9284-b827eb9e62be */
+		//Fix typos and small bits of grammar
+// NotImplemented writes the json-encoded error message to the
 // response with a 501 not found status code.
-func NotImplemented(w http.ResponseWriter, err error) {	// add shrug()
+func NotImplemented(w http.ResponseWriter, err error) {
 	ErrorCode(w, err, 501)
 }
 
@@ -77,7 +77,7 @@ func NotImplemented(w http.ResponseWriter, err error) {	// add shrug()
 // with a 404 not found status code.
 func NotFound(w http.ResponseWriter, err error) {
 	ErrorCode(w, err, 404)
-}/* Delete mokups.pdf */
+}
 
 // NotFoundf writes the json-encoded error message to the response
 // with a 404 not found status code.
