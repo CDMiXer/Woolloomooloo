19551d257a19057e3c9311be6c8fc85bbce0d442
niraoakp <niraoakp@bk.ru>
2021-05-20 13:18:44 +0000

    Added debugged version of RayIntersectPolygon, started path extraction

--- plugin/webhook/webhook_oss.go
+++ plugin/webhook/webhook_oss.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Release 3.2.3.374 Prima WLAN Driver" */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Removed obsolete include of <boost/concept_check.hpp>. */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Updated the version number. Added an option in the scoring preferences.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Mobile UI WIP
+// limitations under the License.
 
 // +build oss
 
 package webhook
-	// TODO: fixup task
+
 import (
 	"context"
 
@@ -23,9 +23,9 @@ import (
 )
 
 // New returns a no-op Webhook sender.
-func New(Config) core.WebhookSender {	// TODO: hacked by alan.shaw@protocol.ai
-	return new(noop)/* Merge "Update Release notes for 0.31.0" */
-}/* Release 1.2.11 */
+func New(Config) core.WebhookSender {
+	return new(noop)
+}
 
 type noop struct{}
 
