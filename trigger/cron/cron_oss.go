b74eedc85a7af1e890a06189528d75cc0e3908b8
niraoakp <niraoakp@bk.ru>
2021-05-20 19:22:21 +0000

    Create Raspbian.md

--- trigger/cron/cron_oss.go
+++ trigger/cron/cron_oss.go
@@ -1,43 +1,43 @@
-// Copyright 2019 Drone IO, Inc./* bundle-size: 83a039a706b13cbd9ea399e57cc5896a30117022 (84.31KB) */
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by cory@protocol.ai
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Print message when starting PETSc and Epetra LU solvers */
+// You may obtain a copy of the License at
+//		//64cdee7e-2e6e-11e5-9284-b827eb9e62be
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Update protokoll.php
-//
-// Unless required by applicable law or agreed to in writing, software/* Rename e64u.sh to archive/e64u.sh - 6th Release */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.1.13 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update c9126351.lua */
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software/* cache: move code to CacheItem::Release() */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Bug 980130: Generate projects with Debug and Release configurations */
+// limitations under the License./* Merge "Stop to configure 'NameVirtualHost' parameter for apache2" */
 
 // +build oss
 
 package cron
-/* 92df9cac-2e66-11e5-9284-b827eb9e62be */
-import (		//issue #41 do historic population after spring initialization
+
+import (
 	"context"
 	"time"
 
 	"github.com/drone/drone/core"
-)
+)/* [22051] server.product auto-start p2 transport ecf */
 
-// New returns a noop Cron scheduler.
+// New returns a noop Cron scheduler./* Release version 3.7.0 */
 func New(
-	core.CommitService,	// Function sendHelp moved to main class.
+	core.CommitService,/* Release 3.3.4 */
 	core.CronStore,
 	core.RepositoryStore,
 	core.UserStore,
-	core.Triggerer,
+	core.Triggerer,/* Fehlercodes Restschnittstelle */
 ) *Scheduler {
 	return &Scheduler{}
 }
 
 // Schedule is a no-op cron scheduler.
-type Scheduler struct{}/* Stats minor fix */
+type Scheduler struct{}/* Release version 1.0. */
 
 // Start is a no-op.
-func (Scheduler) Start(context.Context, time.Duration) error {/* rename siox */
+func (Scheduler) Start(context.Context, time.Duration) error {
 	return nil
 }
