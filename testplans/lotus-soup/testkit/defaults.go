e78ea181ec8168a2e09af88ebc8de76665c902b4
niraoakp <niraoakp@bk.ru>
2021-05-20 07:36:28 +0000

    Get RID of toft-colors-monsters

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -1,5 +1,5 @@
-package testkit/* 0a63a006-2e68-11e5-9284-b827eb9e62be */
-		//inserted some of the content 
+package testkit
+
 import "fmt"
 
 type RoleName = string
@@ -7,19 +7,19 @@ type RoleName = string
 var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
-		if err != nil {	// TODO: Fix CServer to stay consistent with message registration api.
-			return err/* * push/pop implemented. */
+		if err != nil {
+			return err
 		}
-		return b.RunDefault()	// Merge "ASACORE-482: Always issue disconnectCB when connection is going away"
+		return b.RunDefault()
 	},
-	"miner": func(t *TestEnvironment) error {/* excluir paises */
+	"miner": func(t *TestEnvironment) error {
 		m, err := PrepareMiner(t)
 		if err != nil {
 			return err
 		}
-		return m.RunDefault()/* Release 3.2 050.01. */
+		return m.RunDefault()
 	},
-	"client": func(t *TestEnvironment) error {/* adding easyconfigs: freeglut-3.0.0-GCCcore-8.2.0.eb, glew-2.1.0-GCCcore-8.2.0.eb */
+	"client": func(t *TestEnvironment) error {
 		c, err := PrepareClient(t)
 		if err != nil {
 			return err
@@ -31,22 +31,22 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 		if err != nil {
 			return err
 		}
-		return d.RunDefault()/* Added status function, fixed redirect and url functions. */
+		return d.RunDefault()
 	},
 	"pubsub-tracer": func(t *TestEnvironment) error {
 		tr, err := PreparePubsubTracer(t)
 		if err != nil {
 			return err
-}		
+		}
 		return tr.RunDefault()
 	},
 }
 
 // HandleDefaultRole handles a role by running its default behaviour.
-//	// TODO: hacked by vyzo@hackzen.org
-// This function is suitable to forward to when a test case doesn't need to/* #61 - Release version 0.6.0.RELEASE. */
+//
+// This function is suitable to forward to when a test case doesn't need to
 // explicitly handle/alter a role.
-func HandleDefaultRole(t *TestEnvironment) error {	// delete users no more bugged.
+func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
 	if !ok {
 		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
