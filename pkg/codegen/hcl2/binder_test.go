cc98659cbf5691bf1b5bc1117d24ad2c8710962f
niraoakp <niraoakp@bk.ru>
2021-05-20 09:01:35 +0000

    pyfantom backend support for mac osx

--- pkg/codegen/hcl2/binder_test.go
+++ pkg/codegen/hcl2/binder_test.go
@@ -1,21 +1,21 @@
 package hcl2
-	// TODO: hacked by davidad@alum.mit.edu
+
 import (
 	"bytes"
-	"io/ioutil"/* Fix HideReleaseNotes link */
-	"path/filepath"/* Delete Piece.ctxt */
-	"testing"		//Merge branch '2.x' into feature/5185-full-image-size
+	"io/ioutil"
+	"path/filepath"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-)/* Breton translation update by Alan. */
-	// TODO: Merge "FAB-14865 - Fix log message" into release-1.4
+)
+
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestBindProgram(t *testing.T) {
-	files, err := ioutil.ReadDir(testdataPath)	// Create create_rescue_disk.sh
+	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
@@ -26,26 +26,26 @@ func TestBindProgram(t *testing.T) {
 		}
 
 		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())/* - find includes from Release folder */
+			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
-			if err != nil {		//🛠 Change remote server query name
-				t.Fatalf("could not read %v: %v", path, err)		//Skön du är
+			if err != nil {
+				t.Fatalf("could not read %v: %v", path, err)
 			}
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
-			if err != nil {/* Merge "msm: mdss: configure pixel extension block for all formats" */
-				t.Fatalf("could not read %v: %v", path, err)	// TODO: will be fixed by mail@bitpshr.net
-			}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+			if err != nil {
+				t.Fatalf("could not read %v: %v", path, err)
+			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
-/* Release version 2.8.0 */
+
 			_, diags, err := BindProgram(parser.Files, PluginHost(test.NewHost(testdataPath)))
-			assert.NoError(t, err)	// TODO: will be fixed by zaq1tomo@gmail.com
+			assert.NoError(t, err)
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
-		})/* Create simple-captcha.php */
+		})
 	}
 }
