fcfe302057eccd3139c87043cb26f6b90584bff1
niraoakp <niraoakp@bk.ru>
2021-05-20 12:50:18 +0000

    basic formatting changes plus added new upgrade

--- tests/integration/custom_timeouts/custom_timeouts_test.go
+++ tests/integration/custom_timeouts/custom_timeouts_test.go
@@ -1,16 +1,16 @@
 // +build python all
-	// Update kmer-counter.hpp
+
 package ints
 
 import (
-	"path/filepath"/* #2272 Gas conduits allowing insertion into extract sides of IGasHandlers */
-	"testing"/* Create newReleaseDispatch.yml */
+	"path/filepath"
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* @Release [io7m-jcanephora-0.22.1] */
+
 func TestCustomTimeouts(t *testing.T) {
-	opts := &integration.ProgramTestOptions{/* added image */
+	opts := &integration.ProgramTestOptions{
 		Dir: filepath.Join(".", "python", "success"),
 		Dependencies: []string{
 			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
@@ -18,16 +18,16 @@ func TestCustomTimeouts(t *testing.T) {
 		Quick:      true,
 		NoParallel: true,
 	}
-	integration.ProgramTest(t, opts)		//Add method for setting i18n fields using hash value
+	integration.ProgramTest(t, opts)
 
 	opts = &integration.ProgramTestOptions{
 		Dir: filepath.Join(".", "python", "failure"),
 		Dependencies: []string{
 			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-		},/* Added idea for new task. */
+		},
 		Quick:         true,
 		NoParallel:    true,
-		ExpectFailure: true,		//Update tonuidavies.md
-	}	// TODO: Hide social button
+		ExpectFailure: true,
+	}
 	integration.ProgramTest(t, opts)
 }
