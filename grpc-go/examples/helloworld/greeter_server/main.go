807ea32bf75486ef670431ae42e2b2d85bbac739
niraoakp <niraoakp@bk.ru>
2021-05-20 20:27:48 +0000

    update textures

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -1,43 +1,43 @@
-/*	// TODO: Implementando "Muitos para muitos" em responsável
+/*
  *
- * Copyright 2015 gRPC authors.		//- small update
+ * Copyright 2015 gRPC authors./* Add Release Drafter to GitHub Actions */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Update django from 2.0.10 to 2.0.12
  * You may obtain a copy of the License at
- *		//Small clean-ups.
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Adds more swag.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by steven@stebalien.com
+ */
 
 // Package main implements a server for Greeter service.
 package main
 
-import (
+import (/* Added link to 100 Potential Interview Questions */
 	"context"
 	"log"
 	"net"
 
-	"google.golang.org/grpc"	// TODO: will be fixed by steven@stebalien.com
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"/* Update cluster-container-schedule.md */
+)/* Release 5.1.1 */
 
 const (
-	port = ":50051"		//removed the nosql repository package and all references to pyMongo.
-)	// more removal of Z and X
-
+	port = ":50051"
+)
+	// TODO: Rodrigo Albornoz - MongoDb - Exercício 02 - Resolvido
 // server is used to implement helloworld.GreeterServer.
-type server struct {
+type server struct {		//Add Zen Habits, The Book by Leo Babauta.
 	pb.UnimplementedGreeterServer
 }
 
-// SayHello implements helloworld.GreeterServer
+// SayHello implements helloworld.GreeterServer/* 69a788b6-2e59-11e5-9284-b827eb9e62be */
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
@@ -45,13 +45,13 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 
 func main() {
 	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	if err != nil {/* Readme refinements */
+		log.Fatalf("failed to listen: %v", err)		//Merge "Replaced deprecated oslo_messaging_rabbit section"
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer()/* Released 3.0.1 */
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)	// TODO: will be fixed by arajasek94@gmail.com
+		log.Fatalf("failed to serve: %v", err)/* Improving README to fit Callisto Release */
 	}
 }
