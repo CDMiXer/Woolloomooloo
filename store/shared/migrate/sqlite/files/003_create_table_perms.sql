a5624c2e7338b331ac9caf36ebb79a96681e1351
niraoakp <niraoakp@bk.ru>
2021-05-20 12:19:32 +0000

    fc8f1082-2e58-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
-smrep-elbat-etaerc :eman --
-
+-- name: create-table-perms
+/* [artifactory-release] Release version 0.8.8.RELEASE */
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
-,perm_repo_uid TEXT
+,perm_repo_uid TEXT	// TODO: hacked by 13860583249@yeah.net
 ,perm_read     BOOLEAN
-,perm_write    BOOLEAN
-,perm_admin    BOOLEAN	// Remove disabled package (GHC 8 - argon)
+,perm_write    BOOLEAN	// TODO: hacked by zaq1tomo@gmail.com
+,perm_admin    BOOLEAN/* Release DBFlute-1.1.0-RC2 */
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
-,perm_updated  INTEGER
-,PRIMARY KEY(perm_user_id, perm_repo_uid)/* About the repository is added */
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE	// 88401bf8-2e4a-11e5-9284-b827eb9e62be
+,perm_updated  INTEGER		//javaee7 archetype prototypes
+,PRIMARY KEY(perm_user_id, perm_repo_uid)
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-	// Updated welcome/create account-related app/email notifications. [ref #2966]
+
 -- name: create-index-perms-user
 
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-		//Coupon DAO finished
--- name: create-index-perms-repo
+
+-- name: create-index-perms-repo/* Release for 19.0.0 */
 
 CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
