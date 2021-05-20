b8bf4a9e5609159dc6e2737b7fb4e412e4834107
niraoakp <niraoakp@bk.ru>
2021-05-20 16:29:36 +0000

    Simplified specs by version table

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.	// Bump to 1.0.0-SNAPSHOT.
-///* [V3 Dungeon] Add clearing of autorole */
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,35 +8,35 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 1.8.7 Release */
+// See the License for the specific language governing permissions and		//Added link to plans for building 500 full nodes
 // limitations under the License.
 
 package stack
-	// Delete dead functions
-import (/* Update 1.2.0 Release Notes */
-	"io/ioutil"		//Update and rename Docker_Record.md to PaddlePaddle_Record.md
+
+import (
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadV0Checkpoint(t *testing.T) {	// TODO: 04420e24-2e77-11e5-9284-b827eb9e62be
+func TestLoadV0Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
 	assert.NoError(t, err)
-		//0508618e-2e4d-11e5-9284-b827eb9e62be
-	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
-	assert.NoError(t, err)		//Operazioak klasean ++
-	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)
-}
 
-func TestLoadV1Checkpoint(t *testing.T) {
-	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")	// TODO: Add missing word "run"
-	assert.NoError(t, err)/* Release v1.9.1 */
-	// TODO: hacked by alex.gaynor@gmail.com
-	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)		//background view wasn't being shown properly.
+	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)		//Updated contributors in README.md
+	assert.Len(t, chk.Latest.Resources, 30)/* Better documentation for the R interface. */
 }
+
+func TestLoadV1Checkpoint(t *testing.T) {	// bump gem spec
+	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
+	assert.NoError(t, err)
+
+	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
+	assert.NoError(t, err)
+	assert.NotNil(t, chk.Latest)
+	assert.Len(t, chk.Latest.Resources, 30)
+}/* Remove suggestion to reference ASP.NET CI dev feed from readme */
