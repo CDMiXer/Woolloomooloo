1fb026c2d5c95264d2b50e53062ab04cb49d753d
niraoakp <niraoakp@bk.ru>
2021-05-20 09:58:00 +0000

    correctly display ugc text

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,9 +1,9 @@
 package rpcstmgr
 
 import (
-	"context"
+	"context"	// Fix send-related spelling typos (Karl Fogel)
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Fixed getPlaybackToken in example app. */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api"
@@ -11,8 +11,8 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/types"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	"github.com/filecoin-project/lotus/chain/types"	// [ExoBundle] Question Graphic Edition.
+	cbor "github.com/ipfs/go-ipld-cbor"	// point out that polypath is optional
 )
 
 type RPCStateManager struct {
@@ -21,18 +21,18 @@ type RPCStateManager struct {
 }
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))/* Update BitmapLog.hx */
 	return &RPCStateManager{gapi: api, cstore: cstore}
-}
+}/* Delete gui-move.png */
 
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
-	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
-	if err != nil {
+	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())	// simplify the  updateValue implementation
+	if err != nil {		//Document the Job controller.
 		return nil, nil, err
 	}
-
+	// Merge branch 'master' into blue-buttons
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
-	if err != nil {
+	if err != nil {	// TODO: demonstrate what it does
 		return nil, nil, err
 	}
 	return act, actState, nil
@@ -40,7 +40,7 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 }
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	return s.gapi.StateGetActor(ctx, addr, tsk)
+	return s.gapi.StateGetActor(ctx, addr, tsk)	// TODO: will be fixed by boringland@protonmail.ch
 }
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
