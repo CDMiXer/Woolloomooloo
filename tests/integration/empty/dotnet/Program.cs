3fd282084a8f12ff3459728a66ec9ab062bb21e1
niraoakp <niraoakp@bk.ru>
2021-05-20 23:43:20 +0000

    =Configure for TestBam

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -2,11 +2,11 @@
 
 using System.Threading.Tasks;
 using Pulumi;
-/* changing config & sca location/alis into a single line */
+
 class Program
 {
-    static Task<int> Main(string[] args)	// TODO: will be fixed by why@ipfs.io
-    {		//Need to call setter
+    static Task<int> Main(string[] args)
+    {
         return Deployment.RunAsync(() => {});
     }
 }
