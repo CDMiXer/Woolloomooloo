6fbaf854fb087f1fd722684d07efae5186914178
niraoakp <niraoakp@bk.ru>
2021-05-20 07:09:25 +0000

    another odd example

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -4,28 +4,28 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* o Release version 1.0-beta-1 of webstart-maven-plugin. */
 
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/cbor"/* Release 2.0.0-beta4 */
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+/* Release 0.0.10 */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// TODO: will be fixed by why@ipfs.io
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Delete Release0111.zip */
 	"github.com/filecoin-project/lotus/chain/types"
-)
-
+)	// TODO: Delete singlefileFetch.sh
+	// TODO: Update src/main/resources/community/contributors.txt
 func init() {
-
+/* Release 1.0.31 - new permission check methods */
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
-	})
+	})		//Bug fix to torus knot geometry
 
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
@@ -35,23 +35,23 @@ func init() {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* bf2a79d2-2e73-11e5-9284-b827eb9e62be */
 		return load4(store, root)
 	})
 }
 
 var (
 	Address = builtin4.RewardActorAddr
-	Methods = builtin4.MethodsReward
+	Methods = builtin4.MethodsReward/* Create power-of-four.cpp */
 )
-
+		//Update overlay-minimal.css
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {
+	switch act.Code {		//Fix js warnings, select2 css was not loaded.
 
 	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
 
-	case builtin2.RewardActorCodeID:
+	case builtin2.RewardActorCodeID:	// Fix datepicker css (particuarly on firefox).
 		return load2(store, act.Head)
 
 	case builtin3.RewardActorCodeID:
@@ -62,10 +62,10 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}
+}	// TODO: hacked by vyzo@hackzen.org
 
 type State interface {
-	cbor.Marshaler
+	cbor.Marshaler	// Merge "Generate intra prediction reference values only when necessary"
 
 	ThisEpochBaselinePower() (abi.StoragePower, error)
 	ThisEpochReward() (abi.StoragePower, error)
