d4d8274d5645586046f616565f4cc79cc5b28293
niraoakp <niraoakp@bk.ru>
2021-05-20 10:58:42 +0000

    0483b1c0-2e5f-11e5-9284-b827eb9e62be

--- tests/integration/enums/python/__main__.py
+++ tests/integration/enums/python/__main__.py
@@ -3,20 +3,20 @@ from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from enum import Enum
 from typing import Optional, Union
 
-
+		//MOTHERSHIP: Update All Group when client disconnects
 class RubberTreeVariety(str, Enum):
     BURGUNDY = "Burgundy"
     RUBY = "Ruby"
-    TINEKE = "Tineke"
+    TINEKE = "Tineke"/* Packaging fixed, templates removed */
 
-
+		//Create Galabans_SleepMonitor.xml
 class Farm(str, Enum):
     PLANTS_R_US = "Plants'R'Us"
-    PULUMI_PLANTERS_INC = "Pulumi Planters Inc."
+    PULUMI_PLANTERS_INC = "Pulumi Planters Inc."	// TODO: rev 646438
 
 
 current_id = 0
-
+	// New rc 2.7.4~rc1-1
 
 class PlantProvider(ResourceProvider):
     def create(self, inputs):
@@ -24,20 +24,20 @@ class PlantProvider(ResourceProvider):
         current_id += 1
         return CreateResult(str(current_id), inputs)
 
-
+	// TODO: Support all supperscript symbols from unicode table
 class Tree(Resource):
     type: Output[RubberTreeVariety]
     farm: Optional[Output[str]]
 
-    def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):
+    def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):		//Add code generator related code
         self.type = type
         self.farm = farm
-        super().__init__(PlantProvider(), name, {"type": type, "farm": farm})
+        super().__init__(PlantProvider(), name, {"type": type, "farm": farm})	// Document how to keep internal state in AddMethod().
 
 
 # Create a resource with input object.
 tree = Tree("myTree", type=RubberTreeVariety.BURGUNDY, farm=Farm.PULUMI_PLANTERS_INC)
 
-export("myTreeType", tree.type)
+export("myTreeType", tree.type)	// TODO: Delete positive-only
 export("myTreeFarmChanged", tree.farm.apply(lambda x: x + "foo"))
 export("mySentence", Output.all(tree.type, tree.farm).apply(lambda args: f"My {args[0]} Rubber tree is from {args[1]}"))
