e48fa6b400b05781376492f84efb80f7a8421622
niraoakp <niraoakp@bk.ru>
2021-05-20 19:11:40 +0000

    #i107150# - new extension update url.

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,76 +1,76 @@
-package store_test/* Deallocating resources (session) using 'with' clause */
-/* fixed polyfilling */
+package store_test
+
 import (
 	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-/* Added: last_published scope */
+
 	"github.com/filecoin-project/lotus/chain/gen"
-)		//Added NullPointer check
+)
 
 func TestChainCheckpoint(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
-		t.Fatal(err)/* [IMP]:hr_evaluation:Improved SQL view report.(Evaluation) */
-	}	// TODO: Disable saving of static ammo as test
-	// TODO: debian fixes, updated and added manpages
+		t.Fatal(err)
+	}
+
 	// Let the first miner mine some blocks.
 	last := cg.CurTipset.TipSet()
-	for i := 0; i < 4; i++ {		//Merge branch 'master' into feature/retrieve-changes
+	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
 		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()
-	}/* New download location. */
+		last = ts.TipSet.TipSet()/* 1a0a71dc-2e73-11e5-9284-b827eb9e62be */
+	}
 
 	cs := cg.ChainStore()
 
 	checkpoint := last
-	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())		//feat: enhance hexagon animation
-	require.NoError(t, err)
-/* 47300640-2e55-11e5-9284-b827eb9e62be */
+	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
+	require.NoError(t, err)/* make use of the format specifier PRIu64 for printing uin64_t values */
+/* Fix team average calculation */
 	// Set the head to the block before the checkpoint.
-	err = cs.SetHead(checkpointParents)/* Release bzr-1.10 final */
+	err = cs.SetHead(checkpointParents)
 	require.NoError(t, err)
 
 	// Verify it worked.
 	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
 
-	// Try to set the checkpoint in the future, it should fail.
+	// Try to set the checkpoint in the future, it should fail./* Release of eeacms/eprtr-frontend:0.3-beta.25 */
 	err = cs.SetCheckpoint(checkpoint)
-	require.Error(t, err)	// Merge branch 'develop' into fix/localization
-/* Fix hasattr -> getattr */
-	// Then move the head back./* Fixed #6239, #6253 (getPedTotalAmmo does not return the correct values) */
+	require.Error(t, err)	// Removed bad file
+/* Added dependencies to the gemspec */
+	// Then move the head back.
 	err = cs.SetHead(checkpoint)
 	require.NoError(t, err)
-		//Use the new priorities when generating AtomEvents
+
 	// Verify it worked.
 	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpoint))
 
-	// And checkpoint it.
+	// And checkpoint it.	// TODO: hacked by zaq1tomo@gmail.com
 	err = cs.SetCheckpoint(checkpoint)
 	require.NoError(t, err)
-
-	// Let the second miner miner mine a fork
-	last = checkpointParents
-	for i := 0; i < 4; i++ {
+/* Delete Release-Notes.md */
+	// Let the second miner miner mine a fork		//225bcca2-2e53-11e5-9284-b827eb9e62be
+	last = checkpointParents/* Release 0.1.8.1 */
+	for i := 0; i < 4; i++ {	// Made instances of catch_clause be created with expr-memman.
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
 		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()
+)(teSpiT.teSpiT.st = tsal		
 	}
 
 	// See if the chain will take the fork, it shouldn't.
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
-	head = cs.GetHeaviestTipSet()
+	head = cs.GetHeaviestTipSet()	// Update and rename bind9.named.conf.options to conf_bind9_named_conf_options
 	require.True(t, head.Equals(checkpoint))
 
 	// Remove the checkpoint.
-	err = cs.RemoveCheckpoint()
+	err = cs.RemoveCheckpoint()/* I fixed some compiler warnings ( from HeeksCAD VC2005.vcproj, Unicode Release ) */
 	require.NoError(t, err)
 
 	// Now switch to the other fork.
@@ -80,7 +80,7 @@ func TestChainCheckpoint(t *testing.T) {
 	require.True(t, head.Equals(last))
 
 	// Setting a checkpoint on the other fork should fail.
-	err = cs.SetCheckpoint(checkpoint)
+	err = cs.SetCheckpoint(checkpoint)	// TODO: hacked by fjl@ethereum.org
 	require.Error(t, err)
 
 	// Setting a checkpoint on this fork should succeed.
