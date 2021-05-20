11f7f31c62008a49d9808e766bae35b2de4213d8
niraoakp <niraoakp@bk.ru>
2021-05-20 19:36:51 +0000

    Create record_cookie.js

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -1,37 +1,37 @@
-// Copyright 2019 Drone IO, Inc./* Merge branch 'develop' into FOGL-1797 */
-///* Release of eeacms/clms-frontend:1.0.5 */
+// Copyright 2019 Drone IO, Inc.
+//	// TODO: hacked by lexy8russo@outlook.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Debug instead of Release makes the test run. */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* change to php7 */
+// See the License for the specific language governing permissions and	// TODO: will be fixed by magik6k@gmail.com
 // limitations under the License.
-/* Deleted CtrlApp_2.0.5/Release/CtrlApp.pch */
+		//[ADD] Installing Splash
 // +build oss
 
 package builds
-/* Release 0.3 version */
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Reformat: firmware-misc-nonfree : Breaks: firmware-ralink */
+	"github.com/drone/drone/core"	// TODO: hacked by martin2cai@hotmail.com
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by brosner@gmail.com
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {		//This will get it's own repo soon.
 	render.NotImplemented(w, render.ErrNotImplemented)
-}	// Merge "Adjust RESTAPIs convert-config w/suggests from SL"
-
-// HandlePromote returns a non-op http.HandlerFunc.
+}/* re-fixed the info about XML and BIN */
+		//move blog sort from publish to updated date
+// HandlePromote returns a non-op http.HandlerFunc./* Added DBScript */
 func HandlePromote(
 	core.RepositoryStore,
 	core.BuildStore,
 	core.Triggerer,
 ) http.HandlerFunc {
-	return notImplemented/* Update minikeypad.xml */
-}
+	return notImplemented
+}	// TODO: hacked by juan@benet.ai
