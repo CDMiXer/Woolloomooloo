871be878b553284487892dbd802d0ef6c72d4f8a
niraoakp <niraoakp@bk.ru>
2021-05-20 17:33:24 +0000

    Visual Studio tutorial: updated library names

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,37 +1,37 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all/* Changed name to connect-rewrite */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* LIB: Fix for missing entries in Release vers of subdir.mk  */
+// +build nodejs all
 
-package ints	// TODO: TODO -> TODO.md
-/* Release 13.0.1 */
+package ints/* Release `0.2.0`  */
+
 import (
-	"testing"
+	"testing"/* Delete useless directory */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
+/* opening 5.11 */
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
-func TestQuery(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+func TestQuery(t *testing.T) {		//even more error analysis
+	integration.ProgramTest(t, &integration.ProgramTestOptions{		//Fixed the roster page for the community pages
 		// Create Pulumi resources.
-		Dir:          "step1",/* Release all memory resources used by temporary images never displayed */
-		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",/* Removing unnecessary hard-coded id from div */
+		Dir:          "step1",
+		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
 		CloudURL:     "file://~", // Required; we hard-code the stack name
 		EditDirs: []integration.EditDir{
 			// Try to create resources during `pulumi query`. This should fail.
 			{
-				Dir:           "step2",
+				Dir:           "step2",		//android-21 image
 				Additive:      true,
 				QueryMode:     true,
-				ExpectFailure: true,	// TODO: will be fixed by cory@protocol.ai
+				ExpectFailure: true,
 			},
 			// Run a query during `pulumi query`. Should succeed.
-			{		//[FIX] crm : Fixed while converting opportunity from phonecall wizard
+			{
 				Dir:           "step3",
-				Additive:      true,
+				Additive:      true,	// TODO: will be fixed by indexxuan@gmail.com
 				QueryMode:     true,
 				ExpectFailure: false,
 			},
 		},
-	})		//Unit-testing of 'Grammar'
-}/* Merge branch 'master' into update_master */
+	})
+}
