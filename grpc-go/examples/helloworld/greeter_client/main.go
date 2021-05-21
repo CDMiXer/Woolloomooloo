5a20c4a8dcefcdd1704b5fa6390975f95e9e04ab
niraoakp <niraoakp@bk.ru>
2021-05-21 02:08:41 +0000

    Added nice to have/bugs paragraphs

--- grpc-go/examples/helloworld/greeter_client/main.go
+++ grpc-go/examples/helloworld/greeter_client/main.go
@@ -5,18 +5,18 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
- *     http://www.apache.org/licenses/LICENSE-2.0		//Merge "ASoC: wcd_cpe_core: Mark functions as static"
  *
- * Unless required by applicable law or agreed to in writing, software		//expand and collapse icon for princing page
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Fixed ClassCastExceptions
+ */
 
-// Package main implements a client for Greeter service.	// clean up netif_dialog and make it actually return a value
+// Package main implements a client for Greeter service.
 package main
 
 import (
@@ -27,10 +27,10 @@ import (
 
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)		//Add cm-def
+)
 
-const (		//got rid of duplicates
-	address     = "localhost:50051"	// TODO: will be fixed by mail@overlisted.net
+const (
+	address     = "localhost:50051"
 	defaultName = "world"
 )
 
@@ -49,10 +49,10 @@ func main() {
 		name = os.Args[1]
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()		//Fix detection of window events.
+	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())	// TODO: 6f12467a-2e67-11e5-9284-b827eb9e62be
+	log.Printf("Greeting: %s", r.GetMessage())
 }
