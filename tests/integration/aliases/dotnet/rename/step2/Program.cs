d40d7801e1b28edec66ed8d81d75e3ce4fc59bc8
niraoakp <niraoakp@bk.ru>
2021-05-21 19:59:38 +0000

    [fix] исправление маленькой проблемы со sweetalert

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -3,7 +3,7 @@
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource
+class Resource : ComponentResource/* Fixed link to WIP-Releases */
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
@@ -16,14 +16,14 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {
+        {		//Change git repo/org to this forks info.
             // Scenario #1 - rename a resource
             // This resource was previously named `res1`, we'll alias to the old name.
-            var res1 = new Resource("newres1",
+            var res1 = new Resource("newres1",		//Updated mcs/fixed completion unit tests.
                 new ComponentResourceOptions
                 {
                     Aliases = { new Alias { Name = "res1" } },
                 });
         });
     }
-}
+}	// Rowspan normalize only where row contains more than one cell
