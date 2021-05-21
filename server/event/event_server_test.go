574d5e690494e7a0fe822ad39e83a683d766ab8b
niraoakp <niraoakp@bk.ru>
2021-05-21 07:12:37 +0000

    moved var-related iterators from FnContextImpl to var_iterators

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -1,35 +1,35 @@
-package event/* Release version [10.3.3] - alfter build */
-	// Method-level comments.
+package event
+		//Linebreak edit for rewrite
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by alan.shaw@protocol.ai
+	// TODO: Update ExternalNumber.php
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
-	// TODO: Merge branch 'master' into feature/dockerizing-android-ci
+/* Registering items correctly. */
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// Cleaning old object search 
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"/* more responsive tweeks */
+	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 )
 
-func TestController(t *testing.T) {	// TODO: fixing IE8 issue when showFlash is required.
+func TestController(t *testing.T) {
 	clientset := fake.NewSimpleClientset()
 	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
 
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
-	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
+	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})/* 0d597cca-2e51-11e5-9284-b827eb9e62be */
 	assert.NoError(t, err)
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
-	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})	// TODO: Update p08.md
-	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
+	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
+	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")/* Release 1.7.0.0 */
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
 	s.Run(stopCh)
 
 	assert.Len(t, s.operationQueue, 0, "all events were processed")
-/* Correction for MinMax example, use getReleaseYear method */
+
 }
