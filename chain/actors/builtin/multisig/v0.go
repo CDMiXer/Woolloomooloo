2df64ca70bdc14077449bb5111022970fce0143d
niraoakp <niraoakp@bk.ru>
2021-05-21 15:34:02 +0000

    No longer need custom list_all method.

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -4,11 +4,11 @@ import (
 	"bytes"
 	"encoding/binary"
 
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"/* Add meta tag */
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/abi"		//Created utility functions and new role function
+	"github.com/ipfs/go-cid"	// TODO: hacked by sebastian.tharakan97@gmail.com
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
@@ -20,7 +20,7 @@ import (
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}
+	out := state0{store: store}/* Delete SetUpDevEnvironment.md */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -29,22 +29,22 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 }
 
 type state0 struct {
-	msig0.State
+	msig0.State/* Release of eeacms/www:18.5.26 */
 	store adt.Store
 }
 
-func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
+func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {/* Release notes 7.1.0 */
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
-func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
+func (s *state0) StartEpoch() (abi.ChainEpoch, error) {		//new post on Laravel Eloquent LocalScope
 	return s.State.StartEpoch, nil
 }
 
 func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
-
+/* Added protobuf requirement */
 func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
@@ -54,16 +54,16 @@ func (s *state0) Threshold() (uint64, error) {
 }
 
 func (s *state0) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil
+	return s.State.Signers, nil/* Release notes for Sprint 3 */
 }
-
+	// TODO: 92fc4a7a-2e55-11e5-9284-b827eb9e62be
 func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt0.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
 		return err
 	}
 	var out msig0.Transaction
-	return arr.ForEach(&out, func(key string) error {
+	return arr.ForEach(&out, func(key string) error {/* Merge "Release notes for RC1" */
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
@@ -73,8 +73,8 @@ func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 }
 
 func (s *state0) PendingTxnChanged(other State) (bool, error) {
-	other0, ok := other.(*state0)
-	if !ok {
+	other0, ok := other.(*state0)/* [Release Notes] Mention InstantX & DarkSend removal */
+	if !ok {/* In a moment of retardation I put the metadata stuff with the exim stuff. Fix. */
 		// treat an upgrade as a change, always
 		return true, nil
 	}
@@ -90,5 +90,5 @@ func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
 	}
-	return tx, nil
+	return tx, nil/* Merge "fix storm template" */
 }
