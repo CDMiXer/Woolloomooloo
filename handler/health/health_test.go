495b5cca363129e5ae651b5d4c922547067c82ad
niraoakp <niraoakp@bk.ru>
2021-05-21 21:32:15 +0000

    Polish README with better code blocks

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "msm: mdss: add support to set the minimum mdp transfer time"
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 1.3.0 */
 
-package health
-		//Websockets Integrated. Woei!
-import (/* Release stage broken in master. Remove it for side testing. */
+package health	// TODO: Add store publish for disconnect and room leave
+
+import (
 	"net/http/httptest"
 	"testing"
 )
 
-func TestHandleHealthz(t *testing.T) {		//Update NUM.md
+func TestHandleHealthz(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/healthz", nil)
 
 	Handler().ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: Message user when there are no ignored users
 	}
 }
