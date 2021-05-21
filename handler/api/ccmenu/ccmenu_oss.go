74d0f385ee3b0b571d454789cb5da0aeb49b6344
niraoakp <niraoakp@bk.ru>
2021-05-21 10:51:50 +0000

    Delete WindowsManager_tb.vhd

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: creation working
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//Refactored some minor quirks
 // You may obtain a copy of the License at
-//		//Delete drsaxjs.png
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* add Getting Started */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: New version of Modern Business - 1.0.6
+// distributed under the License is distributed on an "AS IS" BASIS,		//Prepare release 0.14.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Fix multienums not being indexed correctly
-
-// +build oss/* preparing for the new maven antlr3 plugin */
+// limitations under the License.
+	// TODO: More gitignores
+// +build oss
 
 package ccmenu
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"		//Adding support for framework on OS X
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"	// TODO: BRCD-1463 - No VAT is applied on a vatable service.
 )
-
+	// Delete mcat.sh
 // Handler returns a no-op http.HandlerFunc.
 func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Fix to Release notes - 190 problem */
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
-}
+}/* Release version 0.1.0 */
