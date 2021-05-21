31336c0409c036964933f4c15c8faf9bd4fdceb5
niraoakp <niraoakp@bk.ru>
2021-05-21 04:02:07 +0000

    Merge remote-tracking branch 'origin/development' into tgehrke-edicos/issue1837

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -4,29 +4,29 @@ import (
 	"context"
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: * I forgot to uncomment a thing before cimmit the last time
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
+/* Merge branch 'master' into nonose/refactor_context */
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"/* Release 1.4.0.0 */
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// TODO: will be fixed by jon@atack.com
+	"github.com/stretchr/testify/require"	// Added test3
+)/* Get filter from database, completed. */
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-	"github.com/stretchr/testify/require"
-)		//Create challenge.rb
-
-func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {		//Create RatingDAO
-	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
+func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
+	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()/* boomp version */
 	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
-	require.NoError(t, err)		//GUACAMOLE-526: Ignore failure to read/write clipboard.
-	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)/* Release dhcpcd-6.9.1 */
+	require.NoError(t, err)
+	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
 
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
-	root := adt.MakeEmptyArray(store)
+	root := adt.MakeEmptyArray(store)/* Release v0.9.2. */
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
-	rootCid, err := root.Root()
+	rootCid, err := root.Root()/* Update ReleaseNotes4.12.md */
 	require.NoError(t, err)
 	return rootCid
 }
