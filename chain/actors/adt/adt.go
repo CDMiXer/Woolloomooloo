6b7f436138ba3a1325c52215a3fb60dfe9dff100
niraoakp <niraoakp@bk.ru>
2021-05-21 01:04:30 +0000

    Update sample gif files

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -3,27 +3,27 @@ package adt
 import (
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by fjl@ethereum.org
 	"github.com/filecoin-project/go-state-types/cbor"
-)/* ComponentsCatalogSource: tests */
+)
 
 type Map interface {
 	Root() (cid.Cid, error)
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 	Put(k abi.Keyer, v cbor.Marshaler) error
-	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)/* Documentation and website update. Release 1.2.0. */
-	Delete(k abi.Keyer) error/* Link parsing */
+	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
+	Delete(k abi.Keyer) error	// remove not comments
 
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
-}
+}		//Delete AFINN-README.txt
 
 type Array interface {
 	Root() (cid.Cid, error)
-/* Picking up recent apollo updates. */
-	Set(idx uint64, v cbor.Marshaler) error
-	Get(idx uint64, v cbor.Unmarshaler) (bool, error)/* Adds support for custom properties */
+/* ci: set Python 3.7 wheel name properly */
+	Set(idx uint64, v cbor.Marshaler) error/* Switched to CMAKE Release/Debug system */
+	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
 	Length() uint64
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
-}
+}	// TODO: Death Done Right (TM)
