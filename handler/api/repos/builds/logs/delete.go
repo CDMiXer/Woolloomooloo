49e810f90a302c216d5bbdaf134e7209186da477
niraoakp <niraoakp@bk.ru>
2021-05-21 16:10:21 +0000

    Fixes homepage navigation.

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by yuvalalaluf@gmail.com
-// you may not use this file except in compliance with the License./* Release: Making ready for next release iteration 5.7.1 */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,22 +10,22 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// limitations under the License.
 
 package logs
 
-import (	// Base test infrastructure working.  Added "make test" to the makefile.
+import (
 	"net/http"
 	"strconv"
 
-	"github.com/drone/drone/core"/* Merge branch 'master' into open_io */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the logs./* Update ReleaseNotes.rst */
+// requests to delete the logs.
 func HandleDelete(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
@@ -33,13 +33,13 @@ func HandleDelete(
 	steps core.StepStore,
 	logs core.LogStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Merge "Latch shard-stat reporting"
-		var (/* Revert also */
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {		//Remove the poetry.
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
@@ -52,21 +52,21 @@ func HandleDelete(
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-}		
-		repo, err := repos.FindName(r.Context(), namespace, name)	// chore(deps): update dependency semantic-release to v8.0.4
+		}
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//Merge "[FIX] ODataAnnotations: Alias replacement and Entity containers"
+			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {		//Add a handful of autosplitters/load removers
+		if err != nil {
 			render.NotFound(w, err)
-			return		//regenerate after minor chnage to nbpcg;
+			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* DeleteMarkerAction is added */
+			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
@@ -74,7 +74,7 @@ func HandleDelete(
 			return
 		}
 		err = logs.Delete(r.Context(), step.ID)
-		if err != nil {		//Bump dependencies, and version number
+		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
