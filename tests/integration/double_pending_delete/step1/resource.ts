16289f94b6fd2738d06fb0871c2a697b12b1f152
niraoakp <niraoakp@bk.ru>
2021-05-21 21:35:52 +0000

    read ddb-next.properties from user home in test environment

--- tests/integration/double_pending_delete/step1/resource.ts
+++ tests/integration/double_pending_delete/step1/resource.ts
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// DescribeSensor tested
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Merged more config stuff from Robert
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of primecount-0.16 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release PPWCode.Vernacular.Persistence 1.4.2 */
+// limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
@@ -19,9 +19,9 @@ export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private id: number = 0;
-/* Add new is_? functions */
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {		//Create jfporchez.md
-        return {	// Executable example scripts, some cleanups to comments, variable names
+
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
+        return {
             inputs: news,
         }
     }
@@ -31,10 +31,10 @@ export class Provider implements dynamic.ResourceProvider {
             return {
                 changes: true,
                 replaces: ["fail"]
-            }/* Release of eeacms/plonesaas:5.2.1-4 */
+            }
         }
 
-        return {		//Undo deploy test
+        return {
             changes: false,
         }
     }
@@ -47,9 +47,9 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             id: (this.id++).toString(),
             outs: inputs,
-        }		//VertexShader : Set case 3 as default #1
+        }
     }
-/* Update PluginCompiler.java */
+
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
@@ -57,6 +57,6 @@ export class Provider implements dynamic.ResourceProvider {
 
 export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);/* Released #10 & #12 to plugin manager */
-    }/* Version 0.4 Release */
+        super(Provider.instance, name, props, opts);
+    }
 }
