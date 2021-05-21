f35c4c9f91dd9820e5cce757d70eae67d13f95a4
niraoakp <niraoakp@bk.ru>
2021-05-21 09:05:25 +0000

    Add method to create new collection and handle if SQLAlchemy throws exception

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Release 1.09 */
 
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("doomed", 4);
+const a = new Resource("doomed", 4);		//Update buildDebPackage.sh
 
 // "a" should still be in the checkpoint with its new value.
