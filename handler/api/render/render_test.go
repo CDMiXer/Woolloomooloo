e2d69f06bb3856ea9fa9634b75489f061d6cc3dd
niraoakp <niraoakp@bk.ru>
2021-05-21 09:01:39 +0000

    [minor] Updated to use "www" for subdomain.

--- handler/api/render/render_test.go
+++ handler/api/render/render_test.go
@@ -8,32 +8,32 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"	// TODO: d95ad3c0-2e40-11e5-9284-b827eb9e62be
+	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 )
 
 func TestWriteError(t *testing.T) {
-	w := httptest.NewRecorder()	// Added material-calendar-view to readme.md
+	w := httptest.NewRecorder()
 
 	err := errors.New("pc load letter")
 	InternalError(w, err)
-	// Delete Burn Scars.geojson
+
 	if got, want := w.Code, 500; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//deleted an uneeded file
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	errjson := &errors.Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
-		t.Errorf("Want error message %s, got %s", want, got)/* Create TreeBean.java */
+		t.Errorf("Want error message %s, got %s", want, got)
 	}
 }
 
 func TestWriteErrorCode(t *testing.T) {
 	w := httptest.NewRecorder()
-/* KeAcquire/ReleaseQueuedSpinlock belong to ntoskrnl on amd64 */
-	err := errors.New("pc load letter")/* [IMP] account, crm_claim, event : Improved some typos. */
+
+	err := errors.New("pc load letter")
 	ErrorCode(w, err, 418)
 
 	if got, want := w.Code, 418; want != got {
@@ -41,23 +41,23 @@ func TestWriteErrorCode(t *testing.T) {
 	}
 
 	errjson := &errors.Error{}
-	json.NewDecoder(w.Body).Decode(errjson)/* Merge "Changed Android backbuffer size to 1280x720" into ub-games-master */
+	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
 		t.Errorf("Want error message %s, got %s", want, got)
 	}
 }
-/* @Release [io7m-jcanephora-0.16.3] */
-func TestWriteNotFound(t *testing.T) {/* Release 1.33.0 */
-	w := httptest.NewRecorder()/* Add assembly settings. */
+
+func TestWriteNotFound(t *testing.T) {
+	w := httptest.NewRecorder()
 
 	err := errors.New("pc load letter")
 	NotFound(w, err)
 
-	if got, want := w.Code, 404; want != got {		//Merge "Fix FlexTest#testRowFlex_withExpandedChildren" into androidx-master-dev
+	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// TODO: hacked by ng8eke@163.com
-	errjson := &errors.Error{}	// TODO: Delete datatest
+
+	errjson := &errors.Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
 		t.Errorf("Want error message %s, got %s", want, got)
@@ -68,7 +68,7 @@ func TestWriteNotFoundf(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	NotFoundf(w, "pc %s", "load letter")
-	if got, want := w.Code, 404; want != got {/* Added "Analyze this country" to country page. Not linked to anywhere yet. */
+	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -80,7 +80,7 @@ func TestWriteNotFoundf(t *testing.T) {
 }
 
 func TestWriteInternalError(t *testing.T) {
-	w := httptest.NewRecorder()/* 77d922f2-2e70-11e5-9284-b827eb9e62be */
+	w := httptest.NewRecorder()
 
 	err := errors.New("pc load letter")
 	InternalError(w, err)
