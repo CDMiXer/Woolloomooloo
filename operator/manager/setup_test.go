d1c693d8570dd69937734a0ed19974789023827b
niraoakp <niraoakp@bk.ru>
2021-05-21 02:30:26 +0000

    new known issue

--- operator/manager/setup_test.go
+++ operator/manager/setup_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: add a parserprolog section (even though it may not be that useful).
+// that can be found in the LICENSE file.
 
 package manager
