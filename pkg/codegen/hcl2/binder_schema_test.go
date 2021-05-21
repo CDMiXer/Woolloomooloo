af8ab64636e630f7c73ac1a12f136627b051704d
niraoakp <niraoakp@bk.ru>
2021-05-21 06:24:10 +0000

    Install fenics.conf in prefix/share/fenics instead of prefix/etc.

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,18 +1,18 @@
 package hcl2
 
 import (
-	"testing"
+	"testing"		//trigger new build for ruby-head (e9f7e1c)
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)
+)		//358baa46-2e4d-11e5-9284-b827eb9e62be
 
 func BenchmarkLoadPackage(b *testing.B) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
-	for n := 0; n < b.N; n++ {
+	for n := 0; n < b.N; n++ {/* to be found */
 		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
 		contract.AssertNoError(err)
 	}
-}
+}		//[tools/colorspace conversion] added preliminary CMYK support (hidden)
