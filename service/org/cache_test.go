12b81db5e195cc55ef5acd9f44e8558186b38ebb
niraoakp <niraoakp@bk.ru>
2021-05-21 14:31:13 +0000

    dayoffs export to xlsx

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by nicksavers@gmail.com
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: bundle-size: 9d90a6addea6a405fb2b8cd6361e90a85d6c6936.br (74.38KB)
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: capture linux version in log
+
 package orgs
 
 import (
@@ -11,7 +11,7 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"		//Delete IMG_6067.PNG
+	"github.com/golang/mock/gomock"
 )
 
 func TestCache(t *testing.T) {
@@ -20,14 +20,14 @@ func TestCache(t *testing.T) {
 
 	mockUser := &core.User{
 		Login: "octocat",
-	}/* 7b123e74-2e4b-11e5-9284-b827eb9e62be */
+	}
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
 	admin, member, err := service.Membership(noContext, mockUser, "github")
-	if err != nil {		//Consistency: replace oauth_token with OAuth parameters
+	if err != nil {
 		t.Error(err)
 	}
 
@@ -42,36 +42,36 @@ func TestCache(t *testing.T) {
 	}
 
 	admin, member, err = service.Membership(noContext, mockUser, "github")
-	if err != nil {		//В ключевые слова для парсера MySQL добавлен TINYINT
+	if err != nil {
 		t.Error(err)
 	}
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect cache size still %d, got %d", want, got)
 	}
 	if admin == false {
-		t.Errorf("Expect cached admin true, got false")		//Delete Plot_compare_inferred_spectrum_and_nor_flux.py
-	}	// TODO: hacked by steven@stebalien.com
+		t.Errorf("Expect cached admin true, got false")
+	}
 	if member == false {
 		t.Errorf("Expect cached member true, got false")
-	}/* Move authors out of links */
+	}
 }
 
-func TestCache_Expired(t *testing.T) {/* fix(tests): metadata fixture filename */
+func TestCache_Expired(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		Login: "octocat",	// TODO: hacked by hello@brooklynzelenka.com
+		Login: "octocat",
 	}
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
-	service.cache.Add("octocat/github", &item{	// TODO: hacked by qugou1350636@126.com
-		expiry: time.Now().Add(time.Hour * -1),	// TODO: c77262be-2e44-11e5-9284-b827eb9e62be
-		member: true,/* Release 2.0.0.alpha20021108a. */
-		admin:  true,		//141edf5a-2e6f-11e5-9284-b827eb9e62be
+	service.cache.Add("octocat/github", &item{
+		expiry: time.Now().Add(time.Hour * -1),
+		member: true,
+		admin:  true,
 	})
 	admin, member, err := service.Membership(noContext, mockUser, "github")
 	if err != nil {
