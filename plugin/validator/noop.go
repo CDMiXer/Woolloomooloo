65a3f20a3e14714cd8a2b6e9f4f796d487931608
niraoakp <niraoakp@bk.ru>
2021-05-21 04:11:18 +0000

    1840712e-2e57-11e5-9284-b827eb9e62be

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release ntoes update. */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Rename code.sh to raiSah6ashiraiSah6ashiraiSah6ashi.sh */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: First call resizeMedia before changing width attr
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Copy nested fragments
 // limitations under the License.
 
 // +build oss
@@ -24,4 +24,4 @@ import (
 
 type noop struct{}
 
-func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
+func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }/* Released MotionBundler v0.1.1 */
