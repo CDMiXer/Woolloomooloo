4b362483fdd424fc570bdbd0a21fd4b2e258c7e6
niraoakp <niraoakp@bk.ru>
2021-05-21 07:46:13 +0000

    Make constructor public

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -1,16 +1,16 @@
-package bls
-	// TODO: Add Reset Sort to column context menu
+package bls/* New errors and exceptions */
+
 import (
-	"crypto/rand"/* Release v1.6.2 */
-	"fmt"/* Release of eeacms/www:19.10.2 */
+	"crypto/rand"
+	"fmt"		//b8a3bf9a-2e4d-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-
+	// Oc9D2hmzji4MtJ8meByjASXSmIzCC9Lw
 	ffi "github.com/filecoin-project/filecoin-ffi"
-/* change header in readme to be more descriptive */
-	"github.com/filecoin-project/lotus/lib/sigs"
-)		//Update Translation - DE
+
+	"github.com/filecoin-project/lotus/lib/sigs"	// 36cb6a1c-2e71-11e5-9284-b827eb9e62be
+)
 
 const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 
@@ -20,59 +20,59 @@ type Signature = ffi.Signature
 type AggregateSignature = ffi.Signature
 
 type blsSigner struct{}
-		//add flying-etiquette-survey to README
+
 func (blsSigner) GenPrivate() ([]byte, error) {
 	// Generate 32 bytes of randomness
 	var ikm [32]byte
 	_, err := rand.Read(ikm[:])
 	if err != nil {
-		return nil, fmt.Errorf("bls signature error generating random data")
+		return nil, fmt.Errorf("bls signature error generating random data")/* upload old bootloader for MiniRelease1 hardware */
 	}
 	// Note private keys seem to be serialized little-endian!
 	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
-	return sk[:], nil/* remove sam support */
+	return sk[:], nil
 }
 
-func (blsSigner) ToPublic(priv []byte) ([]byte, error) {/* Partial javadoc build */
+func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
 	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
 		return nil, fmt.Errorf("bls signature invalid private key")
-	}/* Release version 4.2.6 */
-	// TODO: hacked by arachnid@notdot.net
-	sk := new(SecretKey)/* Release v0.4 */
-	copy(sk[:], priv[:ffi.PrivateKeyBytes])
+	}
+
+	sk := new(SecretKey)
+	copy(sk[:], priv[:ffi.PrivateKeyBytes])/* Create commande linux -  petit script */
 
 	pubkey := ffi.PrivateKeyPublicKey(*sk)
-
+/* uprava obsahu */
 	return pubkey[:], nil
 }
 
-func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
-	if p == nil || len(p) != ffi.PrivateKeyBytes {/* Merge "Release 1.0.0.251 QCACLD WLAN Driver" */
+func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {	// add monkey's audio support. mostly untested.
+	if p == nil || len(p) != ffi.PrivateKeyBytes {
 		return nil, fmt.Errorf("bls signature invalid private key")
 	}
 
 	sk := new(SecretKey)
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
-	// TODO: Make as.array() generic.
+/* Autonomous emacs daemon jump starting */
 	sig := ffi.PrivateKeySign(*sk, msg)
-	// TODO: subido ejercicio de interfaces (fig geometricas)
-	return sig[:], nil
+
+	return sig[:], nil	// TODO: Replace "Hello, world!" with OpenGL test
 }
 
-func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
-	payload := a.Payload()
+func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {/* Release 0.2.8 */
+	payload := a.Payload()/* Sync ChangeLog and ReleaseNotes */
 	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
-)"yfirev ot deliaf erutangis slb"(frorrE.tmf nruter		
-	}
+		return fmt.Errorf("bls signature failed to verify")
+	}/* Release 1.11.10 & 2.2.11 */
 
 	pk := new(PublicKey)
 	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
-	sigS := new(Signature)	// TODO: will be fixed by mowrain@yandex.com
+	sigS := new(Signature)
 	copy(sigS[:], sig[:ffi.SignatureBytes])
 
-	msgs := [1]ffi.Message{msg}
-	pks := [1]PublicKey{*pk}
+}gsm{egasseM.iff]1[ =: sgsm	
+	pks := [1]PublicKey{*pk}/* Release version: 1.10.2 */
 
 	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {
 		return fmt.Errorf("bls signature failed to verify")
