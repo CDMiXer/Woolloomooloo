75c4434fc090e5d3ff331e9139260b7be3ea281d
niraoakp <niraoakp@bk.ru>
2021-05-21 03:25:43 +0000

    b8016ff0-2e58-11e5-9284-b827eb9e62be

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* fix typo, LP: #526570 */
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Fixing "Release" spelling */
-// Setup for the next test.
-const a = new Resource("base", { uniqueKey: 1, state: 100 });
+
+// Setup for the next test.	// Remove commented out travis config
+const a = new Resource("base", { uniqueKey: 1, state: 100 });/* further XQJ improvements */
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
