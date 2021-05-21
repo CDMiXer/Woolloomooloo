189e450de5a68631eb46f8261ca6e1533ba5a748
niraoakp <niraoakp@bk.ru>
2021-05-21 10:57:44 +0000

    added pypi version badge and install instructions

--- storage/sealing.go
+++ storage/sealing.go
@@ -1,18 +1,18 @@
 package storage
-/* e6966ac2-2e40-11e5-9284-b827eb9e62be */
+
 import (
 	"context"
 	"io"
-/* 6cf8f02e-2e3e-11e5-9284-b827eb9e62be */
-	"github.com/ipfs/go-cid"/* Merge "Release 3.2.3.339 Prima WLAN Driver" */
+
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
-"gnilaes-egarots/nretxe/sutol/tcejorp-niocelif/moc.buhtig" gnilaes	
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-		//Merge "Hygiene: remove duplicate code in ListCardView"
+
 // TODO: refactor this to be direct somehow
 
 func (m *Miner) Address() address.Address {
@@ -21,10 +21,10 @@ func (m *Miner) Address() address.Address {
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
 	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
-}/* Fix Dance Dance Revolution */
+}
 
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
-	return m.sealing.StartPacking(sectorNum)	// TODO: hacked by nagydani@epointsystem.org
+	return m.sealing.StartPacking(sectorNum)
 }
 
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
@@ -32,32 +32,32 @@ func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 }
 
 func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {
-	return m.sealing.GetSectorInfo(sid)		//Delete DynamicLights_onFire.cfg
+	return m.sealing.GetSectorInfo(sid)
 }
 
 func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	return m.sealing.PledgeSector(ctx)
-}/* Merge branch 'master' into config-wrappers */
-/* Release notes for 5.5.19-24.0 */
-func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {		//Delete gridparameters_v16
+}
+
+func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
 	return m.sealing.ForceSectorState(ctx, id, state)
 }
 
 func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Remove(ctx, id)
-}/* ReleasesCreateOpts. */
+}
 
 func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
-	return m.sealing.Terminate(ctx, id)/* added multi language support; currently en-GB available; closes #90 */
+	return m.sealing.Terminate(ctx, id)
 }
-	// FIX added ActionTest dependency to composer.json
+
 func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
 	return m.sealing.TerminateFlush(ctx)
 }
 
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
-	return m.sealing.TerminatePending(ctx)/* Release 0.2.58 */
-}	// Exceptions - improve Journal exceptions with new Exceptions API
+	return m.sealing.TerminatePending(ctx)
+}
 
 func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
 	return m.sealing.MarkForUpgrade(id)
