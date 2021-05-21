67da8057460c896c5d4dba38419c91359a678075
niraoakp <niraoakp@bk.ru>
2021-05-21 14:57:40 +0000

    Update mk_dirs.py

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2019, Pulumi Corporation.
-//
+///* Корректировка в проверке поля телефон на странице быстрого оформления заказа */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release new version with changes from #71 */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Switch Open WebIF
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,26 +16,26 @@ package tracing
 
 import "context"
 
-// tracingOptionsKey is the value used as the context key for TracingOptions.
+// tracingOptionsKey is the value used as the context key for TracingOptions./* Wip on listing jobs */
 var tracingOptionsKey struct{}
-/* 74efa4ce-2e68-11e5-9284-b827eb9e62be */
-// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
-type Options struct {/* ec803716-2e58-11e5-9284-b827eb9e62be */
+
+// TracingOptions describes the set of options available for configuring tracing on a per-request basis./* Moving the gun's default position */
+type Options struct {
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
 	// calls.
-	PropagateSpans bool		//Add error_test
-	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
-	TracingHeader string/* Update auth.pp */
+	PropagateSpans bool/* share base and hadoop key tabs */
+	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.		//Updated font installation process
+	TracingHeader string
 }
-
+		//a7a549ec-2e53-11e5-9284-b827eb9e62be
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
-}	// TODO: Update SafeUnpickler.py
+}
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
-// this function returns the zero value./* Add "Contribute" and "Releases & development" */
+// this function returns the zero value./* Add tidyhtml to wercker config. */
 func OptionsFromContext(ctx context.Context) Options {
-	opts, _ := ctx.Value(tracingOptionsKey).(Options)	// TODO: updated with new DB logic
-	return opts	// 1.2.1  accssory  deregister for test
+	opts, _ := ctx.Value(tracingOptionsKey).(Options)	// Fixed issues with xmbctrl.
+	return opts
 }
