c046c11b45e0aacab0def1d6fed5292818936f0f
niraoakp <niraoakp@bk.ru>
2021-05-21 16:08:26 +0000

    Add string indexing

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Merge "Clean the documentation for rspec-beaker"
 
 // +build !oss
-
-package scheduler
+		//Added link to the report to contact.md
+package scheduler	// Rebuilt index with anaethoss
