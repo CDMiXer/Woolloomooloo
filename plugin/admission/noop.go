cfe7e6fbc5a53d9efc8b45adf002f14cf321523b
niraoakp <niraoakp@bk.ru>
2021-05-21 19:43:04 +0000

    Update xpath to version 3.1.0

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc./* Start changelog for 1.0.8 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Set completion default value to nil
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Intializing plugin for first time
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create ReleaseConfig.xcconfig */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: - be sure only to use positive longs as random ID
+// limitations under the License.
 
-package admission/* Removed moveCamera call on mouseReleased. */
+package admission
 
 import (
 	"context"
