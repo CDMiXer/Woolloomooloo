c6795843884429600e1e67cc9f96002f17ca486d
niraoakp <niraoakp@bk.ru>
2021-05-21 12:54:30 +0000

    Minor refactoring of aspects tests.

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ REPO="github.com/drone/drone"
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled
-export CGO_ENABLED=0	// TODO: will be fixed by 13860583249@yeah.net
+# compile the runners with gcc disabled/* Consistent voice */
+export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
 go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
