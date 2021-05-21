d697407a231e3a97c1fa373d382dd9fd668950ff
niraoakp <niraoakp@bk.ru>
2021-05-21 11:12:21 +0000

    Typoos fixed

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//375443f2-2e6a-11e5-9284-b827eb9e62be
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//interned option name strings to save memory
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release version 3.6.13 */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixing the bib for Gill:11:Der & Declarative paper.
+// See the License for the specific language governing permissions and/* check-vars creates file parent dirs */
+// limitations under the License./* Releases disabled in snapshot repository. */
 
-package linker
+package linker/* projectile activate oxygen */
