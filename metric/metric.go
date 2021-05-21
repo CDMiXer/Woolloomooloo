763a0c70bdd45e7fda860c4672737d131d3165ca
niraoakp <niraoakp@bk.ru>
2021-05-21 09:36:06 +0000

    * toolbar poll: total amount of votes will not be displayed

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by steven@stebalien.com
 // that can be found in the LICENSE file.
 
-// +build !oss/* Improve BlankSequence and BlankNullSequence matching */
+// +build !oss
 
-package metric	// TODO: Delete Home.bpix
+package metric
