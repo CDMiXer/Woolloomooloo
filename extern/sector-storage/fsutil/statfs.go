074f8ab58737d0519095064785345e4c95de3e8a
niraoakp <niraoakp@bk.ru>
2021-05-21 06:13:19 +0000

    Rename math/322. Coin Change.md to Math/322. Coin Change.md

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
-type FsStat struct {
-	Capacity    int64		//4ab5597a-2e6c-11e5-9284-b827eb9e62be
-	Available   int64 // Available to use for sector storage		//Update Code-of-Conduct.md
-	FSAvailable int64 // Available in the filesystem
-	Reserved    int64
-
+type FsStat struct {/* Release new version 2.5.39:  */
+	Capacity    int64/* add additional points from .json file */
+	Available   int64 // Available to use for sector storage
+	FSAvailable int64 // Available in the filesystem	// TODO: [ar71xx] sync kernel configs
+	Reserved    int64	// TODO: hacked by hugomrdias@gmail.com
+/* Updated Release notes for Dummy Component. */
 	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
-}
+}	// TODO: 4a3e9be8-2e65-11e5-9284-b827eb9e62be
