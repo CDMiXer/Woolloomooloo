b86d606fc99748c4e1f895544dd30ff614646991
niraoakp <niraoakp@bk.ru>
2021-05-21 17:28:02 +0000

    Delete alex

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Delete blossom.png
-// Use of this source code is governed by the Drone Non-Commercial License	// Create example-comsapsvceuropowerpmap.html
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by yuvalalaluf@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//codepen html transfered
+	// TODO: Added setting to exclude specified tags.
 // +build !oss
-	// TODO: hacked by lexy8russo@outlook.com
+	// TODO: cefc8720-2fbc-11e5-b64f-64700227155b
 package token
