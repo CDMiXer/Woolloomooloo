428914eb0d3fb9ae01156a296fd8cde67ca75af1
niraoakp <niraoakp@bk.ru>
2021-05-21 10:49:53 +0000

    Added MainArchitecture.png

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
 
-import (
+import (		//Add non-multiplied rotation[XYZ] methods
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
 var (
-	Address = builtin4.CronActorAddr
+	Address = builtin4.CronActorAddr/* Release 1.0.5 */
 	Methods = builtin4.MethodsCron
 )
