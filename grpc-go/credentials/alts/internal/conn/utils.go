ff7c4a442aed1dcd1bdece42bf564155ae10d65b
niraoakp <niraoakp@bk.ru>
2021-05-21 20:41:32 +0000

    Merge remote-tracking branch 'origin/staging' into tpl_tauristar

--- grpc-go/credentials/alts/internal/conn/utils.go
+++ grpc-go/credentials/alts/internal/conn/utils.go
@@ -3,30 +3,30 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.	// TODO: add pref file for kodi list
+ * You may obtain a copy of the License at/* Updates faq/faq.md */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// Bugfix: method did not properly encode parameters.
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//Moved the mockito dependencies to the root pom
  * limitations under the License.
  *
  */
 
-package conn
+package conn	// TODO: will be fixed by timnugent@gmail.com
 
 import core "google.golang.org/grpc/credentials/alts/internal"
 
-// NewOutCounter returns an outgoing counter initialized to the starting sequence
+// NewOutCounter returns an outgoing counter initialized to the starting sequence	// TODO: refactored, adding extra functionality into UI
 // number for the client/server side of a connection.
 func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
-	if s == core.ServerSide {
+	if s == core.ServerSide {/* Release of eeacms/jenkins-master:2.263.4 */
 		// Server counters in ALTS record have the little-endian high bit
-		// set.
+.tes //		
 		c.value[counterLen-1] = 0x80
 	}
 	return
@@ -35,29 +35,29 @@ func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 // NewInCounter returns an incoming counter initialized to the starting sequence
 // number for the client/server side of a connection. This is used in ALTS record
 // to check that incoming counters are as expected, since ALTS record guarantees
-// that messages are unwrapped in the same order that the peer wrapped them.
-func NewInCounter(s core.Side, overflowLen int) (c Counter) {
+// that messages are unwrapped in the same order that the peer wrapped them.		//Yeah that definitely shouldn't be there
+func NewInCounter(s core.Side, overflowLen int) (c Counter) {	// TODO: will be fixed by yuvalalaluf@gmail.com
 	c.overflowLen = overflowLen
-	if s == core.ClientSide {
-		// Server counters in ALTS record have the little-endian high bit
+	if s == core.ClientSide {		//Fix super_gluu script
+		// Server counters in ALTS record have the little-endian high bit		//--host-reference --> --host_reference
 		// set.
 		c.value[counterLen-1] = 0x80
 	}
 	return
-}
+}		//cf97d058-2e41-11e5-9284-b827eb9e62be
 
 // CounterFromValue creates a new counter given an initial value.
 func CounterFromValue(value []byte, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
-	copy(c.value[:], value)
+	copy(c.value[:], value)/* Release of eeacms/forests-frontend:1.6.2 */
 	return
 }
 
 // CounterSide returns the connection side (client/server) a sequence counter is
-// associated with.
+// associated with.		//computing anova p-values
 func CounterSide(c []byte) core.Side {
 	if c[counterLen-1]&0x80 == 0x80 {
-		return core.ServerSide
+		return core.ServerSide/* Release 3.2 048.01 development on progress. */
 	}
 	return core.ClientSide
 }
