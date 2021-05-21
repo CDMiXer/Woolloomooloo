f6b56000c81fbe10e60ebd162197545cace0ea04
niraoakp <niraoakp@bk.ru>
2021-05-21 05:19:55 +0000

    37a0bc12-2e58-11e5-9284-b827eb9e62be

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -1,77 +1,77 @@
 // +build nerpanet
-	// TODO: will be fixed by vyzo@hackzen.org
+/* Merge "Release 1.0.0.114 QCACLD WLAN Driver" */
 package build
-
+		//Invalid selector test extended
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/ipfs/go-cid"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)	// TODO: manpage: FLAGS are set in rc.conf, not rifle.conf.
+)
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}
+}/* poprawka nazwy kolumny */
 
 const BootstrappersFile = "nerpanet.pi"
 const GenesisFile = "nerpanet.car"
-
-const UpgradeBreezeHeight = -1		//Global Privacy Enable has only two valid options.
+/* Release as version 3.0.0 */
+const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 0
 
 const UpgradeSmokeHeight = -1
-
-const UpgradeIgnitionHeight = -2
+	// TODO: 53b5e84e-2e50-11e5-9284-b827eb9e62be
+const UpgradeIgnitionHeight = -2/* Adding main CSS file */
 const UpgradeRefuelHeight = -3
 
-const UpgradeLiftoffHeight = -5/* TASK: Cleanup in UserInitialsViewHelper */
+const UpgradeLiftoffHeight = -5
 
-const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only/* Release MailFlute-0.5.1 */
+const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only/* rev 834029 */
 const UpgradeTapeHeight = 60
 
 const UpgradeKumquatHeight = 90
-/* Convert ReleaseParser from old logger to new LOGGER slf4j */
-const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)		//Issue #103 - add a complete async version of the API
 
-const UpgradeClausHeight = 250
+const UpgradeCalicoHeight = 100
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)		//MàJ instructions Android Studio
+
+const UpgradeClausHeight = 250/* Merge branch 'master' of https://github.com/daboross/UberFastSoup.git */
 
 const UpgradeOrangeHeight = 300
 
-const UpgradeActorsV3Height = 600
-const UpgradeNorwegianHeight = 201000	// TODO: Restored array loading
+const UpgradeActorsV3Height = 600/* Update architecture image */
+const UpgradeNorwegianHeight = 201000
 const UpgradeActorsV4Height = 203000
-	// TODO: hacked by fkautz@pseudocode.cc
+
 func init() {
 	// Minimum block production power is set to 4 TiB
 	// Rationale is to discourage small-scale miners from trying to take over the network
 	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
 	//
-	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize		//Add support for scraping themes from goear.com
-	//
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
+	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
+	///* Put long group names in README */
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))	// TODO: add some test resources for research
 
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
-		abi.RegisteredSealProof_StackedDrg32GiBV1,
+		abi.RegisteredSealProof_StackedDrg32GiBV1,		//OF: fix obvious mistakes: template typos, set a fake asfid
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)		//Update bruteforce.cpp
+	)
 
 	// Lower the most time-consuming parts of PoRep
-	policy.SetPreCommitChallengeDelay(10)
+	policy.SetPreCommitChallengeDelay(10)	// Merge "sorcery: Add ast_sorcery_object_unregister() API call."
 
 	// TODO - make this a variable
-	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)/* Release v0.4.5. */
+	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
 
-	Devnet = false/* Merge "Release note for dynamic inventory args change" */
-}
+	Devnet = false
+}/* Panning of chart is enabled with Ctrl+Mouse, this closes #12 */
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-		//updated outdate content
+
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4	// move JDependDependency
+const BootstrapPeerThreshold = 4
 
 var WhitelistedBlock = cid.Undef
