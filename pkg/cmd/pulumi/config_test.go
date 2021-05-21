95c27df25388da62fd14f247c8933c6211df508f
niraoakp <niraoakp@bk.ru>
2021-05-21 03:35:06 +0000

    Fixed, should work now.

--- pkg/cmd/pulumi/config_test.go
+++ pkg/cmd/pulumi/config_test.go
@@ -1,43 +1,43 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Added a couple of functions to the export list.
-// you may not use this file except in compliance with the License.		//fixed layout break point bug
-// You may obtain a copy of the License at/* Recommit: Fixed DAO and Model Classes */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by seth@sethvargo.com
+//     http://www.apache.org/licenses/LICENSE-2.0		//Merge "Improve UI for blacklist preference"
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Implementados métodos necessários para a conversão de DTOs.
-// See the License for the specific language governing permissions and/* add check for read failure */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Change spam all abilities from 15 to 5 minutes at the end of the game */
+
 package main
 
-import (
+import (/* Release version tag */
 	"testing"
-
+/* Modified README - Release Notes section */
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"		//Merge branch 'DDB-NEXT-Multipolygone_byId' into develop
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//Create code_2.py
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"		//pipeline.py: fix
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-	// TODO: 4b9f4cfe-2e5f-11e5-9284-b827eb9e62be
+
 func TestPrettyKeyForProject(t *testing.T) {
 	proj := &workspace.Project{
 		Name:    tokens.PackageName("test-package"),
 		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),
-	}/* Release Version 1.1.4 */
-/* job #8321 - Rework the message in the dialog. */
+	}
+	// Add query tests for dupe-types and enhanced-for
 	assert.Equal(t, "foo", prettyKeyForProject(config.MustMakeKey("test-package", "foo"), proj))
 	assert.Equal(t, "other-package:bar", prettyKeyForProject(config.MustMakeKey("other-package", "bar"), proj))
-}
+}/* HOTFIX: Change log level, change createReleaseData script */
 
-func TestSecretDetection(t *testing.T) {/* Remove dev testing code */
+func TestSecretDetection(t *testing.T) {
 	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "token"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "apiToken"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 
 	// The key name does not match the, so even though this "looks like" a secret, we say it is not.
-	assert.False(t, looksLikeSecret(config.MustMakeKey("test", "okay"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
+	assert.False(t, looksLikeSecret(config.MustMakeKey("test", "okay"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))	// TODO: Only latest OS X
 }
