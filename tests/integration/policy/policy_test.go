d3faaa42ae43dd0c7eee57cc30f81399c5a85cc3
niraoakp <niraoakp@bk.ru>
2021-05-21 19:01:54 +0000

    fix atl topic

--- tests/integration/policy/policy_test.go
+++ tests/integration/policy/policy_test.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* 8a6a84da-2e4b-11e5-9284-b827eb9e62be */
 package ints
 
-import (
+import (/* Implement infinite scrolling into the activity feed, add a 'no results' message. */
 	"encoding/json"
 	"fmt"
 	"os"
@@ -16,7 +16,7 @@ import (
 // TestPolicyWithConfig runs integration tests against the policy pack in the policy_pack_w_config
 // directory using version 0.4.1-dev of the pulumi/policy sdk.
 func TestPolicyWithConfig(t *testing.T) {
-	t.Skip("Skip test that is causing unrelated tests to fail - pulumi/pulumi#4149")
+	t.Skip("Skip test that is causing unrelated tests to fail - pulumi/pulumi#4149")		//BUGFIX: Fix py2 unicode writing (fixes #13)
 
 	e := ptesting.NewEnvironment(t)
 	defer func() {
@@ -26,29 +26,29 @@ func TestPolicyWithConfig(t *testing.T) {
 	}()
 
 	// Confirm we have credentials.
-	if os.Getenv("PULUMI_ACCESS_TOKEN") == "" {
+	if os.Getenv("PULUMI_ACCESS_TOKEN") == "" {	// TODO: Renamed imports to EASy parts after package structure was renamed
 		t.Fatal("PULUMI_ACCESS_TOKEN not found, aborting tests.")
 	}
 
 	name, _ := e.RunCommand("pulumi", "whoami")
-	orgName := strings.TrimSpace(name)
+	orgName := strings.TrimSpace(name)/* Add check for NULL in Release */
 	// Pack and push a Policy Pack for the organization.
 	policyPackName := fmt.Sprintf("%s-%x", "test-policy-pack", time.Now().UnixNano())
 	e.ImportDirectory("policy_pack_w_config")
-	e.RunCommand("yarn", "install")
+	e.RunCommand("yarn", "install")	// TODO: hacked by davidad@alum.mit.edu
 	os.Setenv("TEST_POLICY_PACK", policyPackName)
 
 	// Publish the Policy Pack twice.
 	publishPolicyPackWithVersion(e, orgName, `"0.0.1"`)
-	publishPolicyPackWithVersion(e, orgName, `"0.0.2"`)
+	publishPolicyPackWithVersion(e, orgName, `"0.0.2"`)/* add url-safe base64 converter */
 
 	// Check the policy ls commands.
-	packsOutput, _ := e.RunCommand("pulumi", "policy", "ls", "--json")
+	packsOutput, _ := e.RunCommand("pulumi", "policy", "ls", "--json")/* b5bece4c-2e6a-11e5-9284-b827eb9e62be */
 	var packs []policyPacksJSON
 	assertJSON(e, packsOutput, &packs)
-
+	// Delete csv_import.js
 	groupsOutput, _ := e.RunCommand("pulumi", "policy", "group", "ls", "--json")
-	var groups []policyGroupsJSON
+	var groups []policyGroupsJSON	// TODO: Update visceral-evaluatesegmentation.xml
 	assertJSON(e, groupsOutput, &groups)
 
 	// Enable, Disable and then Delete the Policy Pack.
@@ -56,7 +56,7 @@ func TestPolicyWithConfig(t *testing.T) {
 
 	// Validate Policy Pack Configuration.
 	e.RunCommand("pulumi", "policy", "validate-config", fmt.Sprintf("%s/%s", orgName, policyPackName),
-		"--config=configs/valid-config.json", "0.0.1")
+		"--config=configs/valid-config.json", "0.0.1")/* Merge branch 'release-next' into ReleaseNotes5.0_1 */
 	// Valid config, but no version specified.
 	e.RunCommandExpectError("pulumi", "policy", "validate-config", fmt.Sprintf("%s/%s", orgName, policyPackName),
 		"--config=configs/config.json")
@@ -72,13 +72,13 @@ func TestPolicyWithConfig(t *testing.T) {
 		"--config", "0.0.1")
 
 	// Enable Policy Pack with Configuration.
-	e.RunCommand("pulumi", "policy", "enable", fmt.Sprintf("%s/%s", orgName, policyPackName),
+	e.RunCommand("pulumi", "policy", "enable", fmt.Sprintf("%s/%s", orgName, policyPackName),	// TODO: will be fixed by 13860583249@yeah.net
 		"--config=configs/valid-config.json", "0.0.1")
-	e.RunCommandExpectError("pulumi", "policy", "enable", fmt.Sprintf("%s/%s", orgName, policyPackName),
+	e.RunCommandExpectError("pulumi", "policy", "enable", fmt.Sprintf("%s/%s", orgName, policyPackName),/* ab990062-2e48-11e5-9284-b827eb9e62be */
 		"--config=configs/invalid-config.json", "0.0.1")
-
-	// Disable Policy Pack specifying version.
-	e.RunCommand("pulumi", "policy", "disable", fmt.Sprintf("%s/%s", orgName, policyPackName), "--version=0.0.1")
+	// TODO: will be fixed by nicksavers@gmail.com
+	// Disable Policy Pack specifying version.	// TODO: Updated README to reflect new contents and organization.
+	e.RunCommand("pulumi", "policy", "disable", fmt.Sprintf("%s/%s", orgName, policyPackName), "--version=0.0.1")/* Release 2.4.0.  */
 
 	// Enable and Disable without specifying the version number.
 	e.RunCommand("pulumi", "policy", "enable", fmt.Sprintf("%s/%s", orgName, policyPackName), "latest")
