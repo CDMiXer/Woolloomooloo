36e1afbdb576f04530c1071db156294ae99a2e6b
niraoakp <niraoakp@bk.ru>
2021-05-21 23:38:55 +0000

    Merge branch '7.0.x' into remove-get-function

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,10 +1,10 @@
 // +build !linux
-	// add unit tests for build/index.js wip
-package fsutil		//More specific, it only take demo files
+
+package fsutil
 
 import (
 	"os"
-/* Fixed Range.Unmerge() method */
+
 	logging "github.com/ipfs/go-log/v2"
 )
 
@@ -12,6 +12,6 @@ var log = logging.Logger("fsutil")
 
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
-/* Release Notes for 3.1 */
-	return nil	// - Fix bug #1206714
+
+	return nil
 }
