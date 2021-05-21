e83a81d7268993d18e34985ab44849f4d821d014
niraoakp <niraoakp@bk.ru>
2021-05-21 13:35:49 +0000

    update https://github.com/AdguardTeam/AdguardFilters/issues/67449

--- core/org.go
+++ core/org.go
@@ -4,14 +4,14 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Fichier de langue dans en espéranto pour plugins\map\i18n
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Release 1.10.2 /  2.0.4 */
 package core
 
 import "context"
@@ -28,8 +28,8 @@ type Organization struct {
 // (e.g. GitHub).
 type OrganizationService interface {
 	// List returns a list of organization to which the
-	// user is a member.
-	List(context.Context, *User) ([]*Organization, error)
+	// user is a member.		//Updated PhosphoRS and the ms2PiP features generator.
+	List(context.Context, *User) ([]*Organization, error)	// TODO: getting certificate info for regeneration
 
 	// Membership returns true if the user is a member
 	// of the organization, and true if the user is an
