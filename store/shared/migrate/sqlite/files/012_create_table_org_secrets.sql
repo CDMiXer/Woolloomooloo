b52bad350ae2ee1a9f261422ccb2155731be0e0f
niraoakp <niraoakp@bk.ru>
2021-05-21 06:34:29 +0000

    Merge "HDCP TZ module: change TZ app name"

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT		//Upgrade excon to latest.
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT/* Merge "Release 3.0.10.004 Prima WLAN Driver" */
 ,secret_namespace         TEXT COLLATE NOCASE
-,secret_name              TEXT COLLATE NOCASE
+,secret_name              TEXT COLLATE NOCASE/* added Akoum Battlesinger and Bojuka Brigand */
 ,secret_type              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)
-);	// Fixing use of exeext
+,UNIQUE(secret_namespace, secret_name)/* Release of 0.6-alpha */
+);	// TODO: will be fixed by m-ou.se@m-ou.se
