01224299f2486f669d02eb8c75f0e92ff491e459
niraoakp <niraoakp@bk.ru>
2021-05-21 18:21:06 +0000

    Corrijo errores v2- Juan

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -1,24 +1,24 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// Create block-user-comments.php
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
-/* #JC-115 Added 'Back' button to edit topic form */
-class Resource : ComponentResource	// TODO: hacked by admin@multicoin.co
-{/* Updated meeting minutes with assignees and estimations. */
-    public Resource(string name, ComponentResourceOptions options = null)
+
+class Resource : ComponentResource		//BUG : java.sql.date extends java.util.date.
+{	// Create WINNF_FT_S_FPR_testcase.py
+    public Resource(string name, ComponentResourceOptions options = null)	// TODO: will be fixed by alex.gaynor@gmail.com
         : base("my:module:Resource", name, options)
-    {
+{    
     }
 }
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
 class Program
 {
     static Task<int> Main(string[] args)
-    {		//Set the "last aired date" in the current Locale
+    {		//Sep Update
         return Deployment.RunAsync(() => 
-        {		//fix up some config load / save stuff
+        {
             // Scenario #1 - rename a resource
-            var res1 = new Resource("res1");
-        });	// TODO: hacked by souzau@yandex.com
+            var res1 = new Resource("res1");	// UsuarioServicio
+        });
     }
-}		//Improving Project class.
+}
