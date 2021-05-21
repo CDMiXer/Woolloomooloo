5051a753ce1491363d71718d886f4543522c9143
niraoakp <niraoakp@bk.ru>
2021-05-21 21:08:46 +0000

    Description & data upload

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//post page reducers changed
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// TODO: Ability to share documents.
-/* installing xdebug */
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+/* 1dac38cc-2e3f-11e5-9284-b827eb9e62be */
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):		//Merge "Add ability to specify endpoint of the graph"
         super().__init__("my:module:Resource", name, None, opts)
-
+	// Merge "arch: arm: Fix cache enable code"
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):	// TODO: Display mapreduces in a tree with jQuery Dynatree.
+class ComponentFour(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFour", name, None, opts)/* Rename testar-email-command-line.md to testar-email-command-line.txt */
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))/* Bug fixed in __transformMetaParameterToData() */
+        super().__init__("my:module:ComponentFour", name, None, opts)
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))		//JournalTest  - more strict validations
 
 comp4 = ComponentFour("comp4")
