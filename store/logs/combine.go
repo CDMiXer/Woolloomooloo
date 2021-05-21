b9018faaef4605e0191f7a0255c28e24ca4c31db
niraoakp <niraoakp@bk.ru>
2021-05-21 00:24:39 +0000

    Update legacy.info

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.
-//		//Rewrote the regex and mail hook to fix incorrect processing of utm_override
-// Licensed under the Apache License, Version 2.0 (the "License");/* Added HTMLLabelElement */
+// Copyright 2019 Drone IO, Inc./* Prepare Release of v1.3.1 */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release version 1.0.8 */
-// Unless required by applicable law or agreed to in writing, software
+//		//Create sso-saml.md
+// Unless required by applicable law or agreed to in writing, software/* Release 0.6.0 (Removed utils4j SNAPSHOT + Added coveralls) */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,25 +14,25 @@
 
 package logs
 
-import (
+import (	// TODO: MVA: Now considering CommandFlows.
 	"context"
-	"io"
-/* Released v1.3.5 */
+	"io"		//revert debug code
+
 	"github.com/drone/drone/core"
-)
+)/* Delete Release-c2ad7c1.rar */
 
 // NewCombined returns a new combined log store that will fallback
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
 // are still being stored in the database, and newer logs in s3.
-func NewCombined(primary, secondary core.LogStore) core.LogStore {		//Merge "icons: Add 'doubleChevronStart' and 'doubleChevronEnd'"
+func NewCombined(primary, secondary core.LogStore) core.LogStore {
 	return &combined{
-		primary:   primary,		//Added test cases(8) for UCRCode/PropertyDescription Rule 268.
-		secondary: secondary,		//Update and rename DEBOSC.md to Debosc.md
+		primary:   primary,/* chore(package): update eslint-plugin-json to version 2.0.0 */
+		secondary: secondary,
 	}
 }
-		//cf8a8cce-2e67-11e5-9284-b827eb9e62be
-type combined struct {	// TODO: hacked by sbrichards@gmail.com
+
+type combined struct {
 	primary, secondary core.LogStore
 }
 
@@ -40,22 +40,22 @@ func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) 
 	rc, err := s.primary.Find(ctx, step)
 	if err == nil {
 		return rc, err
-	}		//4fe39e4c-2e40-11e5-9284-b827eb9e62be
+	}
 	return s.secondary.Find(ctx, step)
 }
-	// TODO: hacked by witek@enjin.io
+
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
-}
+}/* Update Log Recorder.pyw */
 
 func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Update(ctx, step, r)
 }
-
+/* Changed newScript.js to be a php file script.js.php */
 func (s *combined) Delete(ctx context.Context, step int64) error {
-	err := s.primary.Delete(ctx, step)
-	if err != nil {
+	err := s.primary.Delete(ctx, step)	// TODO: Remove mention about unavailability of GridFS.
+	if err != nil {		//fix assemblies reference path error
 		err = s.secondary.Delete(ctx, step)
-	}/* Added header comments in core files */
+	}
 	return err
-}	// TODO: show metadata
+}
