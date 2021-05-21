c4176cae5cb35e9adc119c3f28d09eb4a9829a4d
niraoakp <niraoakp@bk.ru>
2021-05-21 02:05:20 +0000

    Close issue #132

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,5 +1,5 @@
 package fr32
-/* Bumping the version number to 1.0.2 */
+
 import (
 	"math/bits"
 
@@ -12,10 +12,10 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// (we convert to sector bytes as they are nice round binary numbers)
 
 	w := uint64(in.Padded())
-/* cfc86854-2e72-11e5-9284-b827eb9e62be */
+
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
-		// Extract the next lowest non-zero bit/* Constify string arguments in xrdp-chansrv sources */
+		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
@@ -23,9 +23,9 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		w ^= psize
-/* Merge "Release 3.2.3.429 Prima WLAN Driver" */
+
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}	// TODO: will be fixed by mikeal.rogers@gmail.com
+	}
 	return out
 }
