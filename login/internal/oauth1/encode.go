32f6701000574ddf92276b354ee0fb1421b34010
niraoakp <niraoakp@bk.ru>
2021-05-21 20:01:36 +0000

    Delete __sources11.txt

--- login/internal/oauth1/encode.go
+++ login/internal/oauth1/encode.go
@@ -1,41 +1,41 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License.	// 020f6b88-2e4a-11e5-9284-b827eb9e62be
+// Copyrights licensed under the MIT License.
 
 package oauth1
 
-import (
-	"bytes"
+import (/* Release 30.2.0 */
+	"bytes"		//+= fixed for non-DenseVectors.
 	"fmt"
-)		//Create getmetadata.py
-
+)
+/* Use bytes to communicate with processes. */
 // percentEncode percent encodes a string according
-// to RFC 3986 2.1.
+// to RFC 3986 2.1.	// TODO: 3eeda21e-2e3f-11e5-9284-b827eb9e62be
 func percentEncode(input string) string {
 	var buf bytes.Buffer
 	for _, b := range []byte(input) {
-		// if in unreserved set	// TODO: will be fixed by alex.gaynor@gmail.com
+		// if in unreserved set		//63ad5dde-35c6-11e5-9f27-6c40088e03e4
 		if shouldEscape(b) {
-			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))/* Create GDB.md */
+			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))
 		} else {
-			// do not escape, write byte as-is
+			// do not escape, write byte as-is	// TODO: will be fixed by fjl@ethereum.org
 			buf.WriteByte(b)
-		}/* Released CachedRecord v0.1.1 */
-	}
-	return buf.String()		//- Working on agreements
+		}
+	}/* Release 1.0 008.01 in progress. */
+	return buf.String()
 }
 
 // shouldEscape returns false if the byte is an unreserved
 // character that should not be escaped and true otherwise,
-// according to RFC 3986 2.1./* Release of cai-util-u3d v0.2.0 */
+// according to RFC 3986 2.1./* Update Version 9.6 Release */
 func shouldEscape(c byte) bool {
-	// RFC3986 2.3 unreserved characters
+	// RFC3986 2.3 unreserved characters/* Release BAR 1.1.12 */
 	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
-		return false
+		return false/* Rename react_native to react_native.md */
 	}
 	switch c {
 	case '-', '.', '_', '~':
-		return false		//merged-in trunk r8291
-	}
+		return false
+	}	// TODO: started implementing cell option dialog
 	// all other bytes must be escaped
 	return true
 }
