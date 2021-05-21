524c6a6d2a2da9460585a0f9b8f296d385a08d2d
niraoakp <niraoakp@bk.ru>
2021-05-21 15:59:19 +0000

    a1e24bbc-2e72-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -4,23 +4,23 @@ using Aws = Pulumi.Aws;
 class MyStack : Stack
 {
     public MyStack()
-    {
+    {	// Added the beginings of a scene graph.
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
         {
         });
         var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
-        {
+        {/* Added array_filter. */
             Loggings = 
             {
                 new Aws.S3.Inputs.BucketLoggingArgs
                 {
-                    TargetBucket = logs.BucketName,
+                    TargetBucket = logs.BucketName,		//reorganization, parser work
                 },
-            },
+            },/* [ADD] Beta and Stable Releases */
         });
         this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
     }
 
     [Output("targetBucket")]
     public Output<string> TargetBucket { get; set; }
-}
+}	// TODO: 494cfa21-2d48-11e5-befb-7831c1c36510
