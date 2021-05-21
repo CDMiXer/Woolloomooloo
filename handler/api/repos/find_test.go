0173e499e6dc170964bbbc7621f659a268e5baa0
niraoakp <niraoakp@bk.ru>
2021-05-21 11:36:08 +0000

    Archive old examples

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Main document selection */
 package repos
 
-import (
+import (/* Create microwave.md */
 	"context"
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
-
+/* (tanner) Release 1.14rc2 */
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
 	"github.com/sirupsen/logrus"
@@ -18,9 +18,9 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)/* Release 2.0.1 */
 
-func init() {
+func init() {/* Task #1892: making sure not to load nans */
 	logrus.SetOutput(ioutil.Discard)
 }
 
@@ -30,22 +30,22 @@ var (
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
-		Counter:   42,
-		Branch:    "master",
+		Counter:   42,	// fix home page.
+		Branch:    "master",/* Pass 2. Still not compile. */
 	}
 
 	mockRepos = []*core.Repository{
 		{
 			ID:        1,
-			Namespace: "octocat",
+			Namespace: "octocat",/* sorting css a little */
 			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
-		},
+		},/* a607716e-2e49-11e5-9284-b827eb9e62be */
 		{
 			ID:        1,
 			Namespace: "octocat",
 			Name:      "spoon-knife",
-			Slug:      "octocat/spoon-knife",
+			Slug:      "octocat/spoon-knife",		//Merge branch 'master' into OhSixTwo_without_Identity
 		},
 	}
 )
@@ -59,18 +59,18 @@ func TestFind(t *testing.T) {
 	r = r.WithContext(request.WithRepo(
 		context.Background(), mockRepo,
 	))
-
-	router := chi.NewRouter()
+/* Merge "Release 3.2.3.392 Prima WLAN Driver" */
+	router := chi.NewRouter()	// TODO: gap-data 1.2.4 -- upgrade GAE SDK from 1.5.5 to 1.6.0
 	router.Get("/api/repos/{owner}/{name}", HandleFind())
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* Release notes updates for 1.1b10 (and some retcon). */
 	}
 
 	got, want := new(core.Repository), mockRepo
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)		//Minor changes in README
 	}
 }
