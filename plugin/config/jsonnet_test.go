fc3b9af9e1c6fc8635b94534b0ce92cad1e8caee
niraoakp <niraoakp@bk.ru>
2021-05-21 13:21:26 +0000

    ["Removed dead code.\n", ""]

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Remove redundant header from checker result. WIP#12 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
