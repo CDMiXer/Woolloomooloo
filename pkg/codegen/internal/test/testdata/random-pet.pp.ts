1f52c7071609f822d052ff4c73bbcab1a510a9dc
niraoakp <niraoakp@bk.ru>
2021-05-21 12:36:39 +0000

    add table and association for product feedback

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
-/* Create OPENSHIFT.md */
+
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
