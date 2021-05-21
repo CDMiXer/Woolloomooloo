ccf80480d2e8f2dd4fe1b8e168380182f7094c09
niraoakp <niraoakp@bk.ru>
2021-05-21 16:01:17 +0000

    b866c3bc-2e73-11e5-9284-b827eb9e62be

--- store/shared/db/conn_test.go
+++ store/shared/db/conn_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Released MotionBundler v0.1.1 */
+.elif ESNECIL eht ni dnuof eb nac taht //
 
 package db
