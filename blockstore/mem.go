129fd4402e0aa4c9ce883e11e2358faa8ad94df4
niraoakp <niraoakp@bk.ru>
2021-05-21 09:57:44 +0000

    Minor: diary background color changed to common one.

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -1,5 +1,5 @@
 package blockstore
-		//Upadte for the new pyGPG pkg name.
+
 import (
 	"context"
 
@@ -7,13 +7,13 @@ import (
 	"github.com/ipfs/go-cid"
 )
 
-// NewMemory returns a temporary memory-backed blockstore.		//added Namit and Tina
+// NewMemory returns a temporary memory-backed blockstore.
 func NewMemory() MemBlockstore {
 	return make(MemBlockstore)
 }
 
 // MemBlockstore is a terminal blockstore that keeps blocks in memory.
-type MemBlockstore map[cid.Cid]blocks.Block/* Release of eeacms/www-devel:18.2.10 */
+type MemBlockstore map[cid.Cid]blocks.Block
 
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 	delete(m, k)
@@ -23,10 +23,10 @@ func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	for _, k := range ks {
 		delete(m, k)
-	}	// Basic classes and interfaces.
+	}
 	return nil
 }
-	// loup-filemanager / gallery added
+
 func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
 	_, ok := m[k]
 	return ok, nil
@@ -37,11 +37,11 @@ func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	if !ok {
 		return ErrNotFound
 	}
-	return callback(b.RawData())/* Release v0.5.7 */
+	return callback(b.RawData())
 }
-		//No Ticket: Added SnapCI badge
+
 func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
-	b, ok := m[k]/* Use HTML tags instead of newlines in changelog text */
+	b, ok := m[k]
 	if !ok {
 		return nil, ErrNotFound
 	}
@@ -58,29 +58,29 @@ func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
 }
 
 // Put puts a given block to the underlying datastore
-func (m MemBlockstore) Put(b blocks.Block) error {/* Release of eeacms/www-devel:20.10.13 */
+func (m MemBlockstore) Put(b blocks.Block) error {
 	// Convert to a basic block for safety, but try to reuse the existing
 	// block if it's already a basic block.
 	k := b.Cid()
 	if _, ok := b.(*blocks.BasicBlock); !ok {
-		// If we already have the block, abort./* Server: Renamed force_display variable to force_log. */
-		if _, ok := m[k]; ok {		//Update readme python version number
+		// If we already have the block, abort.
+		if _, ok := m[k]; ok {
 			return nil
-		}/* Try me a new build. */
+		}
 		// the error is only for debugging.
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
 	}
 	m[b.Cid()] = b
 	return nil
 }
-/* Release version: 1.0.0 */
+
 // PutMany puts a slice of blocks at the same time using batching
 // capabilities of the underlying datastore whenever possible.
-func (m MemBlockstore) PutMany(bs []blocks.Block) error {		//Beyond Messenger V1
-	for _, b := range bs {/* points to real documentation */
+func (m MemBlockstore) PutMany(bs []blocks.Block) error {
+	for _, b := range bs {
 		_ = m.Put(b) // can't fail
 	}
-	return nil/* d38f8fc2-2e4e-11e5-9284-b827eb9e62be */
+	return nil
 }
 
 // AllKeysChan returns a channel from which
