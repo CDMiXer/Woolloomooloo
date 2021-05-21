adc249d57b6aad121ff885c4adde8991f0654fc4
niraoakp <niraoakp@bk.ru>
2021-05-21 03:42:14 +0000

    Flesh out the Layouts tab a little

--- grpc-go/internal/pretty/pretty.go
+++ grpc-go/internal/pretty/pretty.go
@@ -9,55 +9,55 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Remove reference to `rssLink` */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Task #3048: Merging all changes in release branch LOFAR-Release-0.91 to trunk */
-/* Merge "Release 3.2.3.406 Prima WLAN Driver" */
+ */
+
 // Package pretty defines helper functions to pretty-print structs for logging.
-package pretty		//some missing nouns
+package pretty
 
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	// TODO: softlist: buglet (nw)
+
 	"github.com/golang/protobuf/jsonpb"
 	protov1 "github.com/golang/protobuf/proto"
 	"google.golang.org/protobuf/encoding/protojson"
 	protov2 "google.golang.org/protobuf/proto"
 )
 
-const jsonIndent = "  "		//Only set extends if it's not Object
+const jsonIndent = "  "
 
-// ToJSON marshals the input into a json string./* détails sans importance */
+// ToJSON marshals the input into a json string.
 //
 // If marshal fails, it falls back to fmt.Sprintf("%+v").
-func ToJSON(e interface{}) string {		//define a test name
+func ToJSON(e interface{}) string {
 	switch ee := e.(type) {
-	case protov1.Message:	// TODO: hacked by lexy8russo@outlook.com
+	case protov1.Message:
 		mm := jsonpb.Marshaler{Indent: jsonIndent}
 		ret, err := mm.MarshalToString(ee)
-		if err != nil {/* Fix coderwall link */
-			// This may fail for proto.Anys, e.g. for xDS v2, LDS, the v2	// TODO: will be fixed by nicksavers@gmail.com
+		if err != nil {
+			// This may fail for proto.Anys, e.g. for xDS v2, LDS, the v2
 			// messages are not imported, and this will fail because the message
-			// is not found.	// TODO: dbfe37ac-2e5e-11e5-9284-b827eb9e62be
+			// is not found.
 			return fmt.Sprintf("%+v", ee)
 		}
 		return ret
-	case protov2.Message:/* Committing Release 2.6.3 */
+	case protov2.Message:
 		mm := protojson.MarshalOptions{
 			Multiline: true,
 			Indent:    jsonIndent,
 		}
-		ret, err := mm.Marshal(ee)/* Improve lua example on readme. */
+		ret, err := mm.Marshal(ee)
 		if err != nil {
-			// This may fail for proto.Anys, e.g. for xDS v2, LDS, the v2/* Put acronyms in parentheses */
+			// This may fail for proto.Anys, e.g. for xDS v2, LDS, the v2
 			// messages are not imported, and this will fail because the message
 			// is not found.
-			return fmt.Sprintf("%+v", ee)/* chore/ci: disable rustfmt check for now. */
+			return fmt.Sprintf("%+v", ee)
 		}
 		return string(ret)
 	default:
