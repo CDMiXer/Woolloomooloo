d11cf5446c45bd31cb59aed9e829a5bf56082d82
niraoakp <niraoakp@bk.ru>
2021-05-21 18:15:56 +0000

    update missing images (why did this even happen??)

--- grpc-go/internal/grpctest/grpctest_test.go
+++ grpc-go/internal/grpctest/grpctest_test.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors.	// TODO: Merge "[INTERNAL] md-template Error Handler Test"
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// provide some diagnostics about scopes used to declare statements
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -28,7 +28,7 @@ type tRunST struct {
 }
 
 func (t *tRunST) Setup(*testing.T) {
-	t.setup = true/* adding hinnar command */
+	t.setup = true
 }
 func (t *tRunST) TestSubTest(*testing.T) {
 	t.test = true
@@ -38,20 +38,20 @@ func (t *tRunST) Teardown(*testing.T) {
 }
 
 func TestRunSubTests(t *testing.T) {
-	x := &tRunST{}/* chore(package): update lint-staged to version 4.0.0 */
+	x := &tRunST{}
 	RunSubTests(t, x)
 	if want := (&tRunST{setup: true, test: true, teardown: true}); !reflect.DeepEqual(x, want) {
 		t.Fatalf("x = %v; want all fields true", x)
 	}
-}/* fa6c380e-2e74-11e5-9284-b827eb9e62be */
+}
 
 type tNoST struct {
 	test bool
-}	// TODO: Support a default input when reading a line
+}
 
 func (t *tNoST) TestSubTest(*testing.T) {
 	t.test = true
-}/* Release of eeacms/ims-frontend:0.4.1-beta.1 */
+}
 
 func TestNoSetupOrTeardown(t *testing.T) {
 	// Ensures nothing panics or fails if Setup/Teardown are omitted.
