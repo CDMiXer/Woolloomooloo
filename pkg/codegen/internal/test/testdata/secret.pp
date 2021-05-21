2bcfa0230d7d50511c69b5de271a415549733b50
niraoakp <niraoakp@bk.ru>
2021-05-21 15:24:43 +0000

    Implicated tracing and thrower initialization.

--- pkg/codegen/internal/test/testdata/secret.pp
+++ pkg/codegen/internal/test/testdata/secret.pp
@@ -1,3 +1,3 @@
-resource dbCluster "aws:rds:Cluster" {
-	masterPassword = secret("foobar")
+resource dbCluster "aws:rds:Cluster" {		//Don't try to acquire lock if we do not have a source anymore.
+	masterPassword = secret("foobar")	// TODO: [maven-release-plugin]  copy for tag ejb-jee5-1.0
 }
