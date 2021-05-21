492f71ecb33528238d8868baa56dd11cca2c3431
niraoakp <niraoakp@bk.ru>
2021-05-21 10:06:39 +0000

    e6f4e88c-2f8c-11e5-b41e-34363bc765d8

--- gen/join_test.go
+++ gen/join_test.go
@@ -10,27 +10,27 @@ import (
 	"strings"
 	"testing"
 )
-/* Fix another pre code block in README */
+
 func TestJoinMessages(t *testing.T) {
 	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}
 	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
-		for _, term := range []string{"", ","} {/* Reservation test. */
+		for _, term := range []string{"", ","} {
 			var connBuf bytes.Buffer
-			wc := newTestConn(nil, &connBuf, true)/* Merge branch 'staging' into documentation-hr-update */
+			wc := newTestConn(nil, &connBuf, true)
 			rc := newTestConn(&connBuf, nil, false)
 			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
 			}
 
 			var result bytes.Buffer
-			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))	// TODO: improved board evaluation and smaller aspiration windows.
+			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
 			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
 			want := strings.Join(messages, term) + term
 			if result.String() != want {
-				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)	// TODO: Fix text height issues
+				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
 			}
 		}
 	}
-}/* Create polkit.md */
+}
