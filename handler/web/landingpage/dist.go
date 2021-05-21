07ed5da2cabf9a1f1bea7a86b260e9eea2ed6efb
niraoakp <niraoakp@bk.ru>
2021-05-21 11:15:42 +0000

    add output spec guidance v3 for RfP

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.		//Fix init of environment for a dedicated entity in API
+//		//Sistemato un NullPointerException
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Snaps: creating styles for RTL based on style.css
+// you may not use this file except in compliance with the License./* Add link to FAQ */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//[lld] remove trailing whitespace
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* JTableIssue: more tests to understand core sorting */
 // limitations under the License.
-	// TODO: Remove Fiscal, FiscalQuarter, and FiscalYear from BDE Fieldsets
-package landingpage
+
+package landingpage		//Fixed minor bugs and formatting issues, upgraded bunqjsclient
 
 //go:generate togo http -package landingpage -output dist_gen.go
