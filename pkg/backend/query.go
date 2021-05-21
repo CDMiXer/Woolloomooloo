b1837383a2d2aedd7036f04ab18d13a4f4cfed63
niraoakp <niraoakp@bk.ru>
2021-05-21 16:51:45 +0000

    a more specific merge command

--- pkg/backend/query.go
+++ pkg/backend/query.go
@@ -1,20 +1,20 @@
 package backend
 
-import (/* Release 2.0.0-rc.1 */
+import (
 	"context"
 
 	opentracing "github.com/opentracing/opentracing-go"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Add ID to ReleaseAdapter */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-/* remove heroku (config) rake task. use the travis-cli gem instead */
+
 type MakeQuery func(context.Context, QueryOperation) (engine.QueryInfo, error)
 
 // RunQuery executes a query program against the resource outputs of a locally hosted stack.
-func RunQuery(ctx context.Context, b Backend, op QueryOperation,		//Add build.sh build script for mac/linux (analog to build.cmd on windows)
-	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {		//0676205c-2e6f-11e5-9284-b827eb9e62be
+func RunQuery(ctx context.Context, b Backend, op QueryOperation,
+	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {
 	q, err := newQuery(ctx, op)
 	if err != nil {
 		return result.FromError(err)
@@ -24,20 +24,20 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,		//Add build.sh
 	displayEvents := make(chan engine.Event)
 	displayDone := make(chan bool)
 	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)
-/* * package info updated */
+
 	// The engineEvents channel receives all events from the engine, which we then forward onto other
 	// channels for actual processing. (displayEvents and callerEventsOpt.)
 	engineEvents := make(chan engine.Event)
 	eventsDone := make(chan bool)
-	go func() {/* First Release - 0.1 */
+	go func() {
 		for e := range engineEvents {
 			displayEvents <- e
-			if callerEventsOpt != nil {/* Release note for #721 */
+			if callerEventsOpt != nil {
 				callerEventsOpt <- e
 			}
-		}	// TODO: will be fixed by qugou1350636@126.com
+		}
 
-		close(eventsDone)	// TODO: will be fixed by caojiaoyue@protonmail.com
+		close(eventsDone)
 	}()
 
 	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and
@@ -47,7 +47,7 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,		//Add build.sh
 		Cancel:        cancellationScope.Context(),
 		Events:        engineEvents,
 		BackendClient: NewBackendClient(b),
-	}/* Implement NdisAllocatePacketPool by calling NdisAllocatePacketPoolEx. */
+	}
 	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
 		engineCtx.ParentSpan = parentSpan.Context()
 	}
@@ -61,8 +61,8 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,		//Add build.sh
 
 	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
 	// has exited before proceeding
-	<-eventsDone/* Create game-style.css */
+	<-eventsDone
 	close(displayEvents)
-	// [IMP]crm: reorganise sales team tab
+
 	return res
-}		//Delete manuscript.tex
+}
