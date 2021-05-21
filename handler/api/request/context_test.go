f9e8ba8aa5177643d16fafd8911f0e20731a6bdb
niraoakp <niraoakp@bk.ru>
2021-05-21 12:18:16 +0000

    Update version in nsi

--- handler/api/request/context_test.go
+++ handler/api/request/context_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by arajasek94@gmail.com
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by steven@stebalien.com
-		//3c8fea14-2e5c-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file./* d6773c38-2e4b-11e5-9284-b827eb9e62be */
+
 package request
