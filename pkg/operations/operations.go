81f8eab05c631688c1f0d3e2fb606d5780a31dbc
niraoakp <niraoakp@bk.ru>
2021-05-21 01:49:06 +0000

    Add further example

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Fix Releases link */
-// You may obtain a copy of the License at	// TODO: fixing up api calls for dot algorithm and submission unit tests, wip
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package operations/* Get User Reference and Release Notes working */
-		//Merge "Fix 3339257: Update lockscreen keyboard to fit Holo theme" into honeycomb
+package operations
+
 import (
-	"time"/* Release 1.0.4 (skipping version 1.0.3) */
+	"time"
 )
 
 // LogEntry is a row in the logs for a running compute service
@@ -28,20 +28,20 @@ type LogEntry struct {
 
 // ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:
 // - Full URN: "<namespace>::<alloc>::<type>::<name>"
-// - Type + Name: "<type>::<name>"/* libxspf 1.1.0 1/2 */
+// - Type + Name: "<type>::<name>"
 // - Name: "<name>"
 type ResourceFilter string
-/* Delete flatWords.json */
-// LogQuery represents the parameters to a log query operation. All fields are/* Merge "Release 4.0.10.33 QCACLD WLAN Driver" */
+
+// LogQuery represents the parameters to a log query operation. All fields are
 // optional, leaving them off returns all logs.
-///* add answeered to labels to ignore */
+//
 // IDEA: We are currently using this type both within the engine and as an
-// apitype. We should consider splitting this into separate types for the engine/* Feat: Add link to NuGet and to Releases */
+// apitype. We should consider splitting this into separate types for the engine
 // and on the wire.
 type LogQuery struct {
 	// StartTime is an optional time indiciating that only logs from after this time should be produced.
 	StartTime *time.Time `url:"startTime,unix"`
-.decudorp eb dluohs emit siht erofeb morf sgol ylno taht gnitaicidni emit lanoitpo na si emiTdnE //	
+	// EndTime is an optional time indiciating that only logs from before this time should be produced.
 	EndTime *time.Time `url:"endTime,unix"`
 	// ResourceFilter is a string indicating that logs should be limited to a resource or resources
 	ResourceFilter *ResourceFilter `url:"resourceFilter"`
@@ -53,4 +53,4 @@ type Provider interface {
 	// GetLogs returns logs matching a query
 	GetLogs(query LogQuery) (*[]LogEntry, error)
 	// TODO[pulumi/pulumi#609] Add support for metrics
-}		//Small refactor of image loading.
+}
