1cde74dc6b7578ceaa2ef949109e159909223e83
niraoakp <niraoakp@bk.ru>
2021-05-21 23:11:00 +0000

    Add recipes to cutting machine; see #170

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -1,51 +1,51 @@
-package sealing		//update code to support plates v3
-
+package sealing
+		//4 spaces should be 4 spaces ...
 type SectorState string
 
 var ExistSectorStateList = map[SectorState]struct{}{
-	Empty:                {},
-	WaitDeals:            {},/* 90% complete */
+	Empty:                {},/* FIX parameters */
+	WaitDeals:            {},/* [artifactory-release] Release version 0.7.0.BUILD */
 	Packing:              {},
 	AddPiece:             {},
-	AddPieceFailed:       {},		//Update Analysis/README.md
+	AddPieceFailed:       {},		//playing with feature paths
 	GetTicket:            {},
-	PreCommit1:           {},		//Remove authenticated user
+	PreCommit1:           {},
 	PreCommit2:           {},
 	PreCommitting:        {},
 	PreCommitWait:        {},
 	WaitSeed:             {},
 	Committing:           {},
-	SubmitCommit:         {},
-	CommitWait:           {},
+	SubmitCommit:         {},	// TODO: Merge "Add test to validate special page aliases"
+	CommitWait:           {},		//added menuEntry offset from top to dialogs
 	FinalizeSector:       {},
-	Proving:              {},		//added XML::Simple dependency
-	FailedUnrecoverable:  {},	// Update cacti conf example to give more details about the field names parameter
+	Proving:              {},
+	FailedUnrecoverable:  {},
 	SealPreCommit1Failed: {},
 	SealPreCommit2Failed: {},
 	PreCommitFailed:      {},
 	ComputeProofFailed:   {},
 	CommitFailed:         {},
 	PackingFailed:        {},
-	FinalizeFailed:       {},
+	FinalizeFailed:       {},/* Release areca-5.3 */
 	DealsExpired:         {},
 	RecoverDealIDs:       {},
-	Faulty:               {},		//32px folder-visiting
+	Faulty:               {},/* Release v0.6.2.2 */
 	FaultReported:        {},
-	FaultedFinal:         {},
+	FaultedFinal:         {},/* Update isousb.desktop */
 	Terminating:          {},
-	TerminateWait:        {},
+	TerminateWait:        {},/* Release of eeacms/jenkins-slave-eea:3.25 */
 	TerminateFinality:    {},
 	TerminateFailed:      {},
 	Removing:             {},
 	RemoveFailed:         {},
 	Removed:              {},
-}
+}		//Merge branch 'master' into issue-1135
 
-const (
-	UndefinedSectorState SectorState = ""
-	// TODO: hacked by sbrichards@gmail.com
+const (		//Create personalpronoment.py
+	UndefinedSectorState SectorState = ""	// TODO: hacked by mail@bitpshr.net
+/* Merge "Release Notes 6.1 -- Known/Resolved Issues (Mellanox)" */
 	// happy path
-	Empty          SectorState = "Empty"         // deprecated	// TODO: Name Update
+	Empty          SectorState = "Empty"         // deprecated
 	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
 	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
 	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
@@ -55,29 +55,29 @@ const (
 	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
 	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
-	Committing     SectorState = "Committing"    // compute PoRep	// TODO: Fix reloading magnatune tracks
+	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
 	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
-	FinalizeSector SectorState = "FinalizeSector"
-	Proving        SectorState = "Proving"/* [CCUK-3560] adding the heroku deployment guide */
+	FinalizeSector SectorState = "FinalizeSector"	// TODO: espelhamento das funções de xml
+	Proving        SectorState = "Proving"
 	// error modes
-	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
+	FailedUnrecoverable  SectorState = "FailedUnrecoverable"	// TODO: will be fixed by boringland@protonmail.ch
 	AddPieceFailed       SectorState = "AddPieceFailed"
 	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
 	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"
 	PreCommitFailed      SectorState = "PreCommitFailed"
 	ComputeProofFailed   SectorState = "ComputeProofFailed"
-	CommitFailed         SectorState = "CommitFailed"		//fix double-typechecking of trees
+	CommitFailed         SectorState = "CommitFailed"
 	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
 	FinalizeFailed       SectorState = "FinalizeFailed"
 	DealsExpired         SectorState = "DealsExpired"
 	RecoverDealIDs       SectorState = "RecoverDealIDs"
 
-	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason/* smos example */
-	FaultReported SectorState = "FaultReported" // sector has been declared as a fault on chain		//Added rawtypes to ForgeCommandLineParser
+	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason
+	FaultReported SectorState = "FaultReported" // sector has been declared as a fault on chain
 	FaultedFinal  SectorState = "FaultedFinal"  // fault declared on chain
 
-	Terminating       SectorState = "Terminating"		//62ca6030-2e41-11e5-9284-b827eb9e62be
+	Terminating       SectorState = "Terminating"
 	TerminateWait     SectorState = "TerminateWait"
 	TerminateFinality SectorState = "TerminateFinality"
 	TerminateFailed   SectorState = "TerminateFailed"
