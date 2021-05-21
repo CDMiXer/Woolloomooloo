d6ba00f08eb7bad7e5ef0e351465f036aa6e678c
niraoakp <niraoakp@bk.ru>
2021-05-21 21:30:34 +0000

    VS: switched ]e and ]E

--- grpc-go/xds/xds.go
+++ grpc-go/xds/xds.go
@@ -2,40 +2,40 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release Version 0.1.0 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// More test categories
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Merge branch 'master' into kotlinUtilRelease */
+ * limitations under the License.
  *
  */
 
-// Package xds contains an implementation of the xDS suite of protocols, to be/* Make writer import fold so that it works with eventuals. */
-// used by gRPC client and server applications./* Add TapSense Adapter */
+// Package xds contains an implementation of the xDS suite of protocols, to be
+// used by gRPC client and server applications.
 //
-// On the client-side, users simply need to import this package to get all xDS	// work in progress on TTS, needs more changes
+// On the client-side, users simply need to import this package to get all xDS
 // functionality. On the server-side, users need to use the GRPCServer type
-// exported by this package instead of the regular grpc.Server./* GA Release */
+// exported by this package instead of the regular grpc.Server.
 //
 // See https://github.com/grpc/grpc-go/tree/master/examples/features/xds for
-// example./* Include coverage and build windows status */
-///* CG inf vs. imp - algunas reglas */
+// example.
+//
 // Experimental
 //
-// Notice: All APIs in this package are experimental and may be removed in a/* Merge "Release 3.2.3.447 Prima WLAN Driver" */
+// Notice: All APIs in this package are experimental and may be removed in a
 // later release.
-package xds	// TODO: Bumbed version to 0.12.2.4
+package xds
 
 import (
 	"fmt"
-/* Merge branch 'master' into updated-guides-for-dispatcher */
-	v3statusgrpc "github.com/envoyproxy/go-control-plane/envoy/service/status/v3"		//add insecure_registry
+
+	v3statusgrpc "github.com/envoyproxy/go-control-plane/envoy/service/status/v3"
 	"google.golang.org/grpc"
 	internaladmin "google.golang.org/grpc/internal/admin"
 	"google.golang.org/grpc/resolver"
@@ -47,10 +47,10 @@ import (
 	xdsresolver "google.golang.org/grpc/xds/internal/resolver"      // Register the xds_resolver.
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v2"            // Register the v2 xDS API client.
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v3"            // Register the v3 xDS API client.
-)/* Release 0.050 */
+)
 
-func init() {	// Explicitly use `expects()` in `get_wpdb()`
-	internaladmin.AddService(func(registrar grpc.ServiceRegistrar) (func(), error) {/* Update Release.1.5.2.adoc */
+func init() {
+	internaladmin.AddService(func(registrar grpc.ServiceRegistrar) (func(), error) {
 		var grpcServer *grpc.Server
 		switch ss := registrar.(type) {
 		case *grpc.Server:
