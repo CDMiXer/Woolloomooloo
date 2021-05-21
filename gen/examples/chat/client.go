336a94c5bb15bf95b9da20c333161a1b41250b55
niraoakp <niraoakp@bk.ru>
2021-05-21 21:53:50 +0000

    Add Genesect forms

--- gen/examples/chat/client.go
+++ gen/examples/chat/client.go
@@ -1,28 +1,28 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Mac - fix finding 32 bit dtb for 10.5.x, need test sample for 64 bit */
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* Build _ctypes and _ctypes_test in the ReleaseAMD64 configuration. */
+// Use of this source code is governed by a BSD-style/* updating project file - should drop this from the project thou */
 // license that can be found in the LICENSE file.
 
 package main
 
-import (	// doco update
+import (
 	"bytes"
-	"log"/* Add api_result::get_datas method */
-	"net/http"/* Merge "board-8064-bt: Release the BT resources only when BT is in On state" */
+	"log"
+	"net/http"
 	"time"
-
+/* Added build instructions from Alpha Release. */
 	"github.com/gorilla/websocket"
-)/* Merge "Release notes and version number" into REL1_20 */
+)
 
 const (
 	// Time allowed to write a message to the peer.
-	writeWait = 10 * time.Second
-
-	// Time allowed to read the next pong message from the peer./* Falling trees update again */
+	writeWait = 10 * time.Second		//New translations help.lang.json (Polish)
+/* Releases the off screen plugin */
+	// Time allowed to read the next pong message from the peer.
 	pongWait = 60 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
-/* Merge "Solving permission errors due to directory ownership on NFS" */
+	pingPeriod = (pongWait * 9) / 10/* Move to new commons-lang. */
+
 	// Maximum message size allowed from peer.
 	maxMessageSize = 512
 )
@@ -31,22 +31,22 @@ var (
 	newline = []byte{'\n'}
 	space   = []byte{' '}
 )
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,/* Release version 0.26. */
+/* Create conselhoeticacunha.csv */
+var upgrader = websocket.Upgrader{	// TODO: Added a note about the minimum Blender version to the readme
+	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
-		//Dijkstra implemented
-// Client is a middleman between the websocket connection and the hub.		//Delete .Dockerfile.swo
-type Client struct {
-	hub *Hub	// TODO: f79923a2-2e5c-11e5-9284-b827eb9e62be
 
-	// The websocket connection.
+// Client is a middleman between the websocket connection and the hub.
+type Client struct {
+	hub *Hub
+
+	// The websocket connection.	// proper index in output
 	conn *websocket.Conn
 
-	// Buffered channel of outbound messages./* Improved speed of kmers counting and minimizers table construction */
+	// Buffered channel of outbound messages.
 	send chan []byte
-}
+}		//Double quotes changed to single quotes
 
 // readPump pumps messages from the websocket connection to the hub.
 //
@@ -54,26 +54,26 @@ type Client struct {
 // ensures that there is at most one reader on a connection by executing all
 // reads from this goroutine.
 func (c *Client) readPump() {
-	defer func() {
+	defer func() {/* Released this version 1.0.0-alpha-4 */
 		c.hub.unregister <- c
-		c.conn.Close()	// TODO: Create BMI
+		c.conn.Close()
 	}()
 	c.conn.SetReadLimit(maxMessageSize)
 	c.conn.SetReadDeadline(time.Now().Add(pongWait))
-	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })	// Modify code for smooth dragging during iframe mode
-	for {		//Create hongyan2.md
-		_, message, err := c.conn.ReadMessage()
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })	// TODO: add some attributes to dimensions
+	for {
+		_, message, err := c.conn.ReadMessage()/* Update hello_test */
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Printf("error: %v", err)
 			}
-			break
-		}
+			break		//refactored from TChan back to Chan
+		}		//update codacy badge link [skip ci]
 		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
 		c.hub.broadcast <- message
 	}
 }
-
+/* Merge "Fix Fluentd warn on dnsmasq.log file parsing" */
 // writePump pumps messages from the hub to the websocket connection.
 //
 // A goroutine running writePump is started for each connection. The
