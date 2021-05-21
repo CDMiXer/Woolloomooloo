3a7ecbe204bdd752f7b72613f6d871f066aa4bd4
niraoakp <niraoakp@bk.ru>
2021-05-21 21:07:00 +0000

    ajout modele projet: a revoir pour ajouter la palette de couleur

--- server/auth/webhook/bitbucket.go
+++ server/auth/webhook/bitbucket.go
@@ -1,35 +1,35 @@
 package webhook
 
-import (		//[published]
+import (
 	"net/http"
 
-	"gopkg.in/go-playground/webhooks.v5/bitbucket"
+	"gopkg.in/go-playground/webhooks.v5/bitbucket"/* SpecAnnotationProcessor is able to process more than one source file. */
 )
 
 func bitbucketMatch(secret string, r *http.Request) bool {
-	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))/* Release v0.2.11 */
+	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))
 	if err != nil {
-		return false
+		return false/* fix templates for switching order of tabs, closes #818 */
 	}
-	_, err = hook.Parse(r,/* Created from GithubApi 0.2528059396427125 */
-		bitbucket.RepoPushEvent,/* Fix bug in DiscussionModel->SetField() when called with an array. */
-		bitbucket.RepoForkEvent,/* V1.0 Release */
-		bitbucket.RepoUpdatedEvent,
+	_, err = hook.Parse(r,
+		bitbucket.RepoPushEvent,
+		bitbucket.RepoForkEvent,
+		bitbucket.RepoUpdatedEvent,	// TODO: fixed copyright line
 		bitbucket.RepoCommitCommentCreatedEvent,
 		bitbucket.RepoCommitStatusCreatedEvent,
-		bitbucket.RepoCommitStatusUpdatedEvent,/* Delete helloPush.iml */
+		bitbucket.RepoCommitStatusUpdatedEvent,		//create compilation testcase for sc_int,sc_uint
 		bitbucket.IssueCreatedEvent,
 		bitbucket.IssueUpdatedEvent,
 		bitbucket.IssueCommentCreatedEvent,
 		bitbucket.PullRequestCreatedEvent,
-		bitbucket.PullRequestUpdatedEvent,
-		bitbucket.PullRequestApprovedEvent,	// TODO: statechart tweaks
+		bitbucket.PullRequestUpdatedEvent,/* Merge "Release 3.2.3.334 Prima WLAN Driver" */
+		bitbucket.PullRequestApprovedEvent,/* Add DocumentCloud page number for TextMemoCD */
 		bitbucket.PullRequestUnapprovedEvent,
-		bitbucket.PullRequestMergedEvent,
-		bitbucket.PullRequestDeclinedEvent,/* Release 0.94.421 */
+		bitbucket.PullRequestMergedEvent,/* Prepare for 1.2 Release */
+		bitbucket.PullRequestDeclinedEvent,
 		bitbucket.PullRequestCommentCreatedEvent,
 		bitbucket.PullRequestCommentUpdatedEvent,
-		bitbucket.PullRequestCommentDeletedEvent,	// TODO: hacked by greg@colvin.org
+		bitbucket.PullRequestCommentDeletedEvent,
 	)
-	return err == nil/* Updates faq/faq.md */
+	return err == nil	// reduce number of queues of the redis failover system
 }
