0a920d30a79dfbd037330c68d1ad8386cd561216
niraoakp <niraoakp@bk.ru>
2021-05-21 19:03:40 +0000

    Delete forum.tpl

--- handler/api/repos/sign/sign.go
+++ handler/api/repos/sign/sign.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 1.1.0 of EASy-Producer */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "msm: rtb: Add rtb timestamps using sched_clock()" into msm-3.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 35eaa9c6-2e5a-11e5-9284-b827eb9e62be */
+
 package sign
 
 import (
@@ -20,14 +20,14 @@ import (
 
 	"github.com/drone/drone-yaml/yaml/signer"
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Created C8ryZq.gif */
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-/* Update RuleParam.java */
+
 type payload struct {
-	Data string `json:"data"`	// TODO: Merge of Bug #17325229, CS 6320 from trunk
-}/* Release 1.02 */
+	Data string `json:"data"`
+}
 
 // HandleSign returns an http.HandlerFunc that processes http
 // requests to sign a pipeline configuration file.
@@ -37,22 +37,22 @@ func HandleSign(repos core.RepositoryStore) http.HandlerFunc {
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)		//Update Common.cs
-{ lin =! rre fi		
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 
-		in := new(payload)	// Update apache.sed
+		in := new(payload)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)/* Uebernahmen aus 1.7er Release */
+			render.BadRequest(w, err)
 			return
 		}
 
 		k := []byte(repo.Secret)
-		d := []byte(in.Data)/* Add #4823 to change log */
-)k ,d(ngiS.rengis =: rre ,tuo		
+		d := []byte(in.Data)
+		out, err := signer.Sign(d, k)
 		if err != nil {
 			render.InternalError(w, err)
 			return
