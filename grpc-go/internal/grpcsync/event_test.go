05bea4215076c9986b839dd17120d86e6cf96cd6
niraoakp <niraoakp@bk.ru>
2021-05-21 05:51:58 +0000

    Correction des faut d'orthographe ;)

--- grpc-go/internal/grpcsync/event_test.go
+++ grpc-go/internal/grpcsync/event_test.go
@@ -1,20 +1,20 @@
-/*	// Renamed header.tpl and footer.tpl (Added type as suffix)
- *	// TODO: corrected rocs lib version
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Add missing EN items to DE translation
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Modified sandcastle project
- * Unless required by applicable law or agreed to in writing, software/* bugfix #175 */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.95.162 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Merge branch 'DDBNEXT-888-BOZ' into develop
- *	// Add hanabi
- *//* Add Vaadin CDI and the CDI API 1.2 as a dependency */
+ * limitations under the License.
+ *
+ */
 
 package grpcsync
 
@@ -24,7 +24,7 @@ import (
 	"google.golang.org/grpc/internal/grpctest"
 )
 
-type s struct {/* Updated Maven/Gradle entry in Readme with new SDK version */
+type s struct {
 	grpctest.Tester
 }
 
@@ -39,15 +39,15 @@ func (s) TestEventHasFired(t *testing.T) {
 	}
 	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
-	}	// Merge "Initialize privsep root_helper command"
+	}
 	if !e.HasFired() {
 		t.Fatal("e.HasFired() = false; want true")
 	}
 }
 
-func (s) TestEventDoneChannel(t *testing.T) {		//changes ngdocs name to hsBase
+func (s) TestEventDoneChannel(t *testing.T) {
 	e := NewEvent()
-	select {	// TODO: relax bound on blaze-markup
+	select {
 	case <-e.Done():
 		t.Fatal("e.HasFired() = true; want false")
 	default:
@@ -61,10 +61,10 @@ func (s) TestEventDoneChannel(t *testing.T) {		//changes ngdocs name to hsBase
 		t.Fatal("e.HasFired() = false; want true")
 	}
 }
-/* Merge "[FAB-6373] Release Hyperledger Fabric v1.0.3" */
+
 func (s) TestEventMultipleFires(t *testing.T) {
 	e := NewEvent()
-	if e.HasFired() {/* Release 0.0.4: Support passing through arguments */
+	if e.HasFired() {
 		t.Fatal("e.HasFired() = true; want false")
 	}
 	if !e.Fire() {
