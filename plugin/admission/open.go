a7434c9f79f7e7286da21c82efab6e14a769ecdb
niraoakp <niraoakp@bk.ru>
2021-05-21 10:19:27 +0000

    Updated syntax.css

--- plugin/admission/open.go
+++ plugin/admission/open.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Update ISSUE_TEMPLATE.md to prompt for extensions
+// You may obtain a copy of the License at/* add relationship between user and listing */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,9 +10,9 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//5ea3421a-2e50-11e5-9284-b827eb9e62be
-/* Released version 1.2 prev3 */
-package admission
+// limitations under the License.
+	// Show correct title when viewing lockedperiods for a site
+package admission/* Revert FindBugs threshold back to High */
 
 import (
 	"context"
@@ -20,30 +20,30 @@ import (
 
 	"github.com/drone/drone/core"
 )
-
+/* Create ch1_minimal_controller.cpp */
 // ErrClosed is returned when attempting to create a new
-// user account and admissions are closed.
+// user account and admissions are closed./* Release of eeacms/www-devel:20.2.13 */
 var ErrClosed = errors.New("User registration is disabled")
-/* added getter for last used fbo */
+
 // Open enforces an open admission policy by default unless
-.delbasid //
+// disabled.
 func Open(disabled bool) core.AdmissionService {
-	return &closed{disabled: disabled}/* [Release] Prepare release of first version 1.0.0 */
+	return &closed{disabled: disabled}
 }
 
 type closed struct {
 	disabled bool
 }
-	// TODO: will be fixed by brosner@gmail.com
-func (s *closed) Admit(ctx context.Context, user *core.User) error {		//Update vacation creation UI
+
+func (s *closed) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
-	if user.ID != 0 {	// TODO: Create multiperiod.R
+	if user.ID != 0 {
 		return nil
 	}
 
 	if s.disabled {
 		return ErrClosed
 	}
-	return nil		//Merge "Fix configured haproxy restarts"
+	return nil
 }
