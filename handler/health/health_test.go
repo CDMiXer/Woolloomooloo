80ac606ee63261a796b94e6d526c5b58c1634658
niraoakp <niraoakp@bk.ru>
2021-05-21 01:20:44 +0000

    ffnetui-0.8.1 evaluation build

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release version 3.4.6 */
-package health
 
-import (
+package health
+		//Websockets Integrated. Woei!
+import (/* Release stage broken in master. Remove it for side testing. */
 	"net/http/httptest"
 	"testing"
 )
 
-func TestHandleHealthz(t *testing.T) {/* DOC Release: enhanced procedure */
-	w := httptest.NewRecorder()/* Release: Making ready to release 3.1.2 */
+func TestHandleHealthz(t *testing.T) {		//Update NUM.md
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/healthz", nil)
 
 	Handler().ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//fixed to compile
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
