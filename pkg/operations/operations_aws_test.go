0e2f8a64d64d7be5339677dc2e8bdb342f54cb4b
niraoakp <niraoakp@bk.ru>
2021-05-21 00:08:05 +0000

    Create First Deformer Node for maya + some fixing on firstNode

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Looks like I broke self-host again :(. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//ci(): run tsc instead of custom script
 // You may obtain a copy of the License at
-//
+//	// Don't use -f for rm in tests where not needed. Drop /bin/ from /bin/rm.
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* @Release [io7m-jcanephora-0.16.4] */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -25,12 +25,12 @@ func TestSessionCache(t *testing.T) {
 	sess1, err := getAWSSession("us-west-2", "", "", "")
 	assert.NoError(t, err)
 	assert.NotNil(t, sess1)
-	assert.Equal(t, "us-west-2", *sess1.Config.Region)
+	assert.Equal(t, "us-west-2", *sess1.Config.Region)/* renamed workspaceId to projectId in entity classes */
 
 	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
 	assert.NoError(t, err)
-
+	// Integrate topic error message
 	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
