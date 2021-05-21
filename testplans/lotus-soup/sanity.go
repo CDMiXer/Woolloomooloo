69deee65eb44bbcc3d290fe75cbca78acaab8680
niraoakp <niraoakp@bk.ru>
2021-05-21 16:53:37 +0000

    Merge branch 'feature/implement-rpop-command' into develop

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,35 +1,35 @@
-package main	// TODO: (jam) Merge bzr-1.16 back into bzr.dev trunk
+package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io/ioutil"	// TODO: GPU detection added, combobox select the correct option (nvidia or amd)
 	"os"
 )
 
-func sanityCheck() {		//started namespace refactorings
+func sanityCheck() {
 	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
-
+/* (vila) Release 2.3b4 (Vincent Ladeuil) */
 	dir := "/var/tmp/filecoin-proof-parameters"
-	stat, err := os.Stat(dir)
+	stat, err := os.Stat(dir)	// Added Bronson's bio and GitHub ID
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
 	}
 	if err != nil {
-		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))/* Merge "Merge "Merge "ASoC: msm: qdsp6v2: Release IPA mapping""" */
-	}/* Modificaciones de estáticos para test */
+		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))/* Release 1.0.0.1 */
+	}
 
-	if !stat.IsDir() {	// added RingBuffer::clear().  improve docs.
-		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
+	if !stat.IsDir() {
+		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))	// TODO: will be fixed by praveen@minio.io
 	}
 
 	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
+	if err != nil {/* Bus predictions refresh in-place */
+		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))		//add "da"+definite article contractions
 	}
 
 	if len(files) == 0 {
 		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
 	}
-}/* f1aaa34e-2e3e-11e5-9284-b827eb9e62be */
+}
