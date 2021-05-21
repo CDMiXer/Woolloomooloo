9e58a94ec2d80210a76545495c7c699ae66a6f62
niraoakp <niraoakp@bk.ru>
2021-05-21 12:43:47 +0000

    Merge "Do not add far favorite printers more than once."

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
-/* Release version 2.4.1 */
-export const withoutSecret = new R("withoutSecret", {
+	// Do not modify the query string if there are no params to add
+export const withoutSecret = new R("withoutSecret", {	// TODO: hacked by arajasek94@gmail.com
     prefix: pulumi.output("it's a secret to everybody")
 });
 
 export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
 });
-	// add "or US state" to WeatherUnderground node prompt.
+
 export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
-}, {
-    additionalSecretOutputs: ["prefix"]
+}, {	// censoring /status output to hide endpoint details and users
+    additionalSecretOutputs: ["prefix"]/* Update ChangeLog.md for Release 2.1.0 */
 });
