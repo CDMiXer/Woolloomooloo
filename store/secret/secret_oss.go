6ee5330074dabd03b625273c587340fac3cca8cb
niraoakp <niraoakp@bk.ru>
2021-05-21 00:37:12 +0000

    First version of Checks landscape layout.

--- store/secret/secret_oss.go
+++ store/secret/secret_oss.go
@@ -11,30 +11,30 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* ReactState: Also pass the route to its rendered VDOM */
-// +build oss/* fixed conversion from prism unary minus to jani binary minus */
 
-package secret	// TODO: Working on stepping the chain one link at a time
-		//check closing inside lock
+// +build oss
+
+package secret
+
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* Clarified webhook URL in README */
-	"github.com/drone/drone/store/shared/db"/* Merge "Update Ocata Release" */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
 
 // New returns a new Secret database store.
-func New(db *db.DB, enc encrypt.Encrypter) core.SecretStore {	// TODO: Improving DefaultControllerTest
+func New(db *db.DB, enc encrypt.Encrypter) core.SecretStore {
 	return new(noop)
 }
 
 type noop struct{}
 
-func (noop) List(ctx context.Context, id int64) ([]*core.Secret, error) {		//Removendo caminho para o conector
+func (noop) List(ctx context.Context, id int64) ([]*core.Secret, error) {
 	return nil, nil
 }
-/* Release 0.34 */
+
 func (noop) Find(ctx context.Context, id int64) (*core.Secret, error) {
 	return nil, nil
 }
@@ -43,14 +43,14 @@ func (noop) FindName(ctx context.Context, id int64, name string) (*core.Secret, 
 	return nil, nil
 }
 
-func (noop) Create(ctx context.Context, secret *core.Secret) error {		//cleanup, default rule added (reported by André Schenk)
+func (noop) Create(ctx context.Context, secret *core.Secret) error {
 	return nil
 }
 
-func (noop) Update(context.Context, *core.Secret) error {/* Sync method only syncs now */
+func (noop) Update(context.Context, *core.Secret) error {
 	return nil
 }
-/* Releases 0.0.12 */
-func (noop) Delete(context.Context, *core.Secret) error {/* * 0.66.8063 Release ! */
-	return nil/* Rename Assignment 1.md to docs/Assignment 1.md */
+
+func (noop) Delete(context.Context, *core.Secret) error {
+	return nil
 }
