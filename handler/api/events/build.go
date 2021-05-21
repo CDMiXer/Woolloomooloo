bcfd0ed05372efec0d86acd1ce8d47fac631ee3d
niraoakp <niraoakp@bk.ru>
2021-05-21 01:06:15 +0000

    fixing events

--- handler/api/events/build.go
+++ handler/api/events/build.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release update info */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// convert snippets as best I can
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Releasedkey is one variable */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and	// change save token to redis
+// limitations under the License.	// TODO: Add link to discourse
 
 package events
 
@@ -22,7 +22,7 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"	// TODO: hacked by zaq1tomo@gmail.com
 	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
@@ -30,14 +30,14 @@ import (
 
 // interval at which the client is pinged to prevent
 // reverse proxy and load balancers from closing the
-// connection.
-var pingInterval = time.Second * 30
+// connection.	// [configure] add AM_PROG_AR to detect 'ar' instead of hard-coding it
+var pingInterval = time.Second * 30/* 18f93828-2e6e-11e5-9284-b827eb9e62be */
 
 // implements a 24-hour timeout for connections. This
 // should not be necessary, but is put in place just
-// in case we encounter dangling connections.
+// in case we encounter dangling connections.	// TODO: will be fixed by 13860583249@yeah.net
 var timeout = time.Hour * 24
-
+/* Release 1.2.5 */
 // HandleEvents creates an http.HandlerFunc that streams builds events
 // to the http.Response in an event stream format.
 func HandleEvents(
@@ -46,7 +46,7 @@ func HandleEvents(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")		//Added tooltips to the panels.
 			name      = chi.URLParam(r, "name")
 		)
 		logger := logger.FromRequest(r).WithFields(
@@ -56,25 +56,25 @@ func HandleEvents(
 			},
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {	// Add initializer to assets group, fixes #84
 			render.NotFound(w, err)
 			logger.WithError(err).Debugln("events: cannot find repository")
 			return
 		}
-
+/* add comma to row 12 */
 		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
 		h.Set("Cache-Control", "no-cache")
 		h.Set("Connection", "keep-alive")
 		h.Set("X-Accel-Buffering", "no")
-
+		//Create currentSong.txt
 		f, ok := w.(http.Flusher)
-		if !ok {
+		if !ok {	// TODO: Moved games descriptions to games/
 			return
 		}
 
 		io.WriteString(w, ": ping\n\n")
-		f.Flush()
+		f.Flush()/* Release 0.36.2 */
 
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
