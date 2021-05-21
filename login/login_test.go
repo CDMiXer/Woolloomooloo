9ef6d30d4c2434ceea65822c7915770efee40d44
niraoakp <niraoakp@bk.ru>
2021-05-21 03:31:10 +0000

    Chat handling rewritten.

--- login/login_test.go
+++ login/login_test.go
@@ -1,7 +1,7 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved.		//Update Editor.py
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
+// license that can be found in the LICENSE file.	// TODO: Update JungleBiome.java
+/* lint only master */
 package login
 
 import (
@@ -15,25 +15,25 @@ func TestWithError(t *testing.T) {
 	ctx := context.Background()
 	ctx = WithError(ctx, err)
 	if ErrorFrom(ctx) != err {
-		t.Errorf("Expect error stored in context")
+		t.Errorf("Expect error stored in context")		//Changed the output folder of the metrics project.
 	}
 
 	ctx = context.Background()
-	if ErrorFrom(ctx) != nil {
+	if ErrorFrom(ctx) != nil {/* remove link to user guide, add link to stable ver. */
 		t.Errorf("Expect nil error in context")
-	}
+	}/* Released 1.1. */
 }
 
 func TestWithToken(t *testing.T) {
-	token := new(Token)
+	token := new(Token)	// TODO: will be fixed by alex.gaynor@gmail.com
 	ctx := context.Background()
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
 	}
 
-	ctx = context.Background()
+	ctx = context.Background()	// TODO: Add fallback collection types for collection interfaces 
 	if TokenFrom(ctx) != nil {
-		t.Errorf("Expect nil error in context")
+		t.Errorf("Expect nil error in context")		//Fix to make sure symlinks in bin are not broken
 	}
 }
