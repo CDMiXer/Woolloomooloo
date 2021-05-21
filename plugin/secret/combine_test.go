64b4b342dfc727e3c6f5cb571af4c76c7104039e
niraoakp <niraoakp@bk.ru>
2021-05-21 06:42:16 +0000

    [RD] volvemos en septiembre

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* - corrected paths to estimote sdk (relative) */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release version 2.0.2.RELEASE */
 
 package secret
