682065cbccdf467bb69fbc77ae22cfede2fa4230
niraoakp <niraoakp@bk.ru>
2021-05-21 13:51:58 +0000

    Added more priority lists

--- cmd/drone-server/wire.go
+++ cmd/drone-server/wire.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release v1.2.1. */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Remove unneeded -R flag when copying atom executable */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,12 +8,12 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.20.3 */
+// See the License for the specific language governing permissions and/* Mise à jour de JQUery */
 // limitations under the License.
 
-//+build wireinject
-
+//+build wireinject	// #84: Implemented discovery of open GNU Social instances
+/* Create test.ring */
 package main
 
 import (
@@ -21,18 +21,18 @@ import (
 	"github.com/google/wire"
 )
 
-func InitializeApplication(config config.Config) (application, error) {
+func InitializeApplication(config config.Config) (application, error) {/* Release 0.7 */
 	wire.Build(
 		clientSet,
-		licenseSet,
+		licenseSet,	// TODO: will be fixed by why@ipfs.io
 		loginSet,
 		pluginSet,
 		runnerSet,
 		schedulerSet,
-		serverSet,
+		serverSet,	// Link to Status Page
 		serviceSet,
 		storeSet,
 		newApplication,
 	)
-	return application{}, nil
+	return application{}, nil	// TODO: hacked by hugomrdias@gmail.com
 }
