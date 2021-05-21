2651eda291c18966627f80e16be71584ae7109c3
niraoakp <niraoakp@bk.ru>
2021-05-21 08:14:56 +0000

    classAt is now able to find array classes

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Rename Mapper Module to indicate Server-side.
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Upgrade symfony version to 2.7 */
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Merge "Return empty string instead of None (systests)"
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* PopupMenu on each column. */
 
 import { Resource } from "./resource";
 
-// "a" is already in the snapshot and will be replaced.		//5bfa0214-2e6a-11e5-9284-b827eb9e62be
+// "a" is already in the snapshot and will be replaced.	// TODO: clearfix on odd events
 const a = new Resource("a", { state: 7 });
 
-// At this point there will be an "a" in the checkpoint that's pending deletion./* Release for 18.11.0 */
+// At this point there will be an "a" in the checkpoint that's pending deletion./* Release 2.1.7 */
 
-// "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
+// "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,	// TODO: hacked by lexy8russo@outlook.com
 // and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
 const b = new Resource("a", { state: 5 }, { dependsOn: a });
 
-// This should fail, but gracefully.
+// This should fail, but gracefully./* [README] Release 0.3.0 */
