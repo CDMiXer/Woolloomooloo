aa2745dd95f4d84d41d4adbcc74f411077ec6db2
niraoakp <niraoakp@bk.ru>
2021-05-21 09:12:26 +0000

    Removed unnecessary color variable

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Kunde,Adresse,Mock
 
-// +build !oss
-	// TODO: hacked by onhardev@bk.ru
+// +build !oss	// Change 'Components' to lowercase
+	// TODO: hacked by davidad@alum.mit.edu
 package converter
 
 import (
-	"context"/* Correções na view Contas a Receber */
+	"context"
 
 	"github.com/drone/drone/core"
-)
+)/* Updated CS-CoreLib Version to the latest Release */
 
-// Legacy returns a conversion service that converts a/* Released springjdbcdao version 1.7.0 */
+// Legacy returns a conversion service that converts a/* Control Ingreso numeros y 2 decimales. */
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
-	return &legacyPlugin{		//6585e968-2e76-11e5-9284-b827eb9e62be
+	return &legacyPlugin{
 		enabled: enabled,
-	}
-}/* Task #4642: Merged Release-1_15 chnages with trunk */
-
-type legacyPlugin struct {/* Delete 7.mp3 */
+	}		//Disable landscape due to confliction
+}
+	// TODO: hacked by mikeal.rogers@gmail.com
+type legacyPlugin struct {
 	enabled bool
 }
 
-func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	if p.enabled == false {
+func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {	// same in svg
+	if p.enabled == false {	// TODO: hacked by fkautz@pseudocode.cc
 		return nil, nil
-	}		//Use the floatingwidget2 in the minigui
-	return &core.Config{
+	}
+	return &core.Config{/* lastfm loved fix 2 */
 		Data: req.Config.Data,
 	}, nil
 }
