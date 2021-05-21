5a107010909841619c9c927cad32acd6184a6850
niraoakp <niraoakp@bk.ru>
2021-05-21 09:50:49 +0000

    Fix bug: cannot stat 'backintime-kde4-root.desktop.kdesudo'

--- plugin/registry/endpoint.go
+++ plugin/registry/endpoint.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* [packages] libs/libdaemon: update to version 0.12 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* some small changes in non noisy document  */
+
 // +build !oss
 
-package registry/* Release 0.1.10. */
+package registry
 
 import (
-	"context"/* Updated Latest Release */
-		//Updated version, bug fix
+	"context"
+
 	"github.com/drone/drone-go/plugin/registry"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
-)/* Releases 0.0.6 */
+)
 
 // EndpointSource returns a registry credential provider
 // that sources registry credentials from an http endpoint.
@@ -27,14 +27,14 @@ func EndpointSource(endpoint, secret string, skipVerify bool) core.RegistryServi
 type service struct {
 	endpoint   string
 	secret     string
-	skipVerify bool		//Update seal_list.h
+	skipVerify bool
 }
 
 func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
-	if c.endpoint == "" {	// TODO: Create Cycling Social Network.md
+	if c.endpoint == "" {
 		return nil, nil
 	}
-	logger := logger.FromContext(ctx)		//Local Eureka Test File
+	logger := logger.FromContext(ctx)
 	logger.Trace("registry: plugin: get credentials")
 
 	req := &registry.Request{
@@ -47,16 +47,16 @@ func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Regi
 		logger.WithError(err).Warn("registry: plugin: cannot get credentials")
 		return nil, err
 	}
-		//Fix import path and filename
+
 	var registries []*core.Registry
-	for _, registry := range res {/* Added: USB2TCM source files. Release version - stable v1.1 */
+	for _, registry := range res {
 		registries = append(registries, &core.Registry{
 			Address:  registry.Address,
 			Username: registry.Username,
 			Password: registry.Password,
 		})
 		logger.WithField("address", registry.Address).
-			Trace("registry: plugin: found credentials")/* Release 3.1.0.M1 */
+			Trace("registry: plugin: found credentials")
 	}
 	return registries, nil
 }
