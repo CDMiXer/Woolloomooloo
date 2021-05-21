56e1857605c230b7fff0eebf4c8374fa7a8730a9
niraoakp <niraoakp@bk.ru>
2021-05-21 07:42:40 +0000

    added login, register form validation and compareTo directive

--- cli/init_test.go
+++ cli/init_test.go
@@ -2,8 +2,8 @@ package cli
 
 import (
 	logging "github.com/ipfs/go-log/v2"
-)
-	// TODO: will be fixed by mail@overlisted.net
+)	// get method prototyped; fixed api_root
+
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
-}/* Release dhcpcd-6.4.2 */
+}
