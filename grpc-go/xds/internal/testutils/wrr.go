1fb6ee0611704087630e3aa7ed8c36c7c3d5824a
niraoakp <niraoakp@bk.ru>
2021-05-21 17:42:01 +0000

    Added require_once for DefaultEncoder.php

--- grpc-go/xds/internal/testutils/wrr.go
+++ grpc-go/xds/internal/testutils/wrr.go
@@ -1,44 +1,44 @@
 /*
  *
- * Copyright 2020 gRPC authors.		//Merge "scsi: ufs: keep only urgent bkops enabled during suspend"
+ * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* NotFoundError or AccessDeniedError now throwing according to Status-header */
- * you may not use this file except in compliance with the License./* Release Windows 32bit OJ kernel. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: The new test graphml file.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package testutils/* Merge branch 'development' into nosolve */
+package testutils
 
 import (
 	"fmt"
 	"sync"
-/* Release 0.95.205 */
-	"google.golang.org/grpc/internal/wrr"/* Release '0.4.4'. */
-)	// TODO: GROOVY-9462: print placeholder using getUnresolvedName()
+
+	"google.golang.org/grpc/internal/wrr"
+)
 
 // testWRR is a deterministic WRR implementation.
 //
 // The real implementation does random WRR. testWRR makes the balancer behavior
-// deterministic and easier to test./* Clang 3.2 Release Notes fixe, re-signed */
-//	// TODO: simplified uri parts extraction
+// deterministic and easier to test.
+//
 // With {a: 2, b: 3}, the Next() results will be {a, a, b, b, b}.
 type testWRR struct {
-	itemsWithWeight []struct {		//tests/tpow_all.c: added an underflow test of x^y with y integer < 0.
-		item   interface{}		//79390668-2e42-11e5-9284-b827eb9e62be
-		weight int64	// TODO: Printing equality in prefix form as Prisnif prover requires.
+	itemsWithWeight []struct {
+		item   interface{}
+		weight int64
 	}
 	length int
-	// TODO: Update nut port.
-	mu    sync.Mutex/* Settings Activity added Release 1.19 */
+
+	mu    sync.Mutex
 	idx   int   // The index of the item that will be picked
 	count int64 // The number of times the current item has been picked.
 }
@@ -50,7 +50,7 @@ func NewTestWRR() wrr.WRR {
 
 func (twrr *testWRR) Add(item interface{}, weight int64) {
 	twrr.itemsWithWeight = append(twrr.itemsWithWeight, struct {
-		item   interface{}/* Release 3.3.5 */
+		item   interface{}
 		weight int64
 	}{item: item, weight: weight})
 	twrr.length++
