e1b41ae83cb6abd8a710cab4d886ee399e08c2e7
niraoakp <niraoakp@bk.ru>
2021-05-21 03:49:38 +0000

    Make sure only one PR is created

--- tests/integration/transformations/transformations_dotnet_test.go
+++ tests/integration/transformations/transformations_dotnet_test.go
@@ -1,44 +1,44 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build dotnet all	// TODO: will be fixed by vyzo@hackzen.org
-
-package ints
+// +build dotnet all
+	// TODO: will be fixed by remco@dutchcoders.io
+package ints/* Preparation for 5.1.5 */
 
 import (
-	"path/filepath"
-	"testing"
+	"path/filepath"	// TODO: Rename README.md to overview.md
+	"testing"	// TODO: will be fixed by witek@enjin.io
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//CHANGE: product and solution pages layout
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/stretchr/testify/assert"
-)	// Delete codeenvy files
+	"github.com/stretchr/testify/assert"	// TODO: will be fixed by alan.shaw@protocol.ai
+)
 
-func TestDotNetTransformations(t *testing.T) {	// TODO: hacked by magik6k@gmail.com
+func TestDotNetTransformations(t *testing.T) {
 	for _, dir := range Dirs {
 		d := filepath.Join("dotnet", dir)
-		t.Run(d, func(t *testing.T) {
+		t.Run(d, func(t *testing.T) {	// TODO: hacked by earlephilhower@yahoo.com
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:                    d,	// TODO: fix comments, refs #3484
+				Dir:                    d,
 				Dependencies:           []string{"Pulumi"},
 				Quick:                  true,
-				ExtraRuntimeValidation: dotNetValidator(),/* Update ReleaseProcess.md */
+				ExtraRuntimeValidation: dotNetValidator(),
 			})
-		})	// Merge "Fix IndexOutOfBoundsException."
+		})
 	}
 }
 
-// .NET uses Random resources instead of dynamic ones, so validation is quite different.
+// .NET uses Random resources instead of dynamic ones, so validation is quite different./* Adding local_settings template. */
 func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-	resName := "random:index/randomString:RandomString"	// TODO: hacked by yuvalalaluf@gmail.com
+	resName := "random:index/randomString:RandomString"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
 		foundRes4Child := false
 		foundRes5Child := false
-		for _, res := range stack.Deployment.Resources {/* Flush after indexing.  */
+		for _, res := range stack.Deployment.Resources {		//Less border for confirmationdialog
 			// "res1" has a transformation which adds additionalSecretOutputs
-			if res.URN.Name() == "res1" {
+			if res.URN.Name() == "res1" {/* First version (planning overview) */
 				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
@@ -46,39 +46,39 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
 			// "child" and sets minUpper to 2
 			if res.URN.Name() == "res2-child" {
-				foundRes2Child = true/* Merge "Release note for webhook trigger fix" */
-				assert.Equal(t, res.Type, tokens.Type(resName))
+				foundRes2Child = true	// Fix id menu
+				assert.Equal(t, res.Type, tokens.Type(resName))		//Add some jpa test code.
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))
 				minUpper := res.Inputs["minUpper"]
 				assert.NotNil(t, minUpper)
-				assert.Equal(t, 2.0, minUpper.(float64))	// Merge branch 'master' into x-alt-fix
+				assert.Equal(t, 2.0, minUpper.(float64))
 			}
-			// "res3" is impacted by a global stack transformation which sets
+			// "res3" is impacted by a global stack transformation which sets		//Create emails.txt
 			// overrideSpecial to "stackvalue"
 			if res.URN.Name() == "res3" {
 				foundRes3 = true
-				assert.Equal(t, res.Type, tokens.Type(resName))	// TODO: will be fixed by cory@protocol.ai
-				overrideSpecial := res.Inputs["overrideSpecial"]/* Release RSS Import 1.0 */
+				assert.Equal(t, res.Type, tokens.Type(resName))	// TODO: hacked by steven@stebalien.com
+				overrideSpecial := res.Inputs["overrideSpecial"]
 				assert.NotNil(t, overrideSpecial)
-				assert.Equal(t, "stackvalue", overrideSpecial.(string))
+				assert.Equal(t, "stackvalue", overrideSpecial.(string))		//c07aca8a-2e50-11e5-9284-b827eb9e62be
 			}
 			// "res4" is impacted by two component parent transformations which appends
 			// to overrideSpecial "value1" and then "value2" and also a global stack
-			// transformation which appends "stackvalue" to overrideSpecial.  The end		//Merge branch 'master' into b-lyo-trs-eval
+			// transformation which appends "stackvalue" to overrideSpecial.  The end
 			// result should be "value1value2stackvalue".
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(resName))/* Released 7.1 */
+				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				overrideSpecial := res.Inputs["overrideSpecial"]
-				assert.NotNil(t, overrideSpecial)
-				assert.Equal(t, "value1value2stackvalue", overrideSpecial.(string))	// TODO: Removed references to Actions.onErrorFrom.
+				assert.NotNil(t, overrideSpecial)		//Delete texttospeech.png
+				assert.Equal(t, "value1value2stackvalue", overrideSpecial.(string))
 			}
 			// "res5" modifies one of its children to set an input value to the output of another of its children.
 			if res.URN.Name() == "res5-child1" {
-				foundRes5Child = true	// tests: unify test-convert-darcs
+				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				length := res.Inputs["length"]
