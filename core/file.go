28ae1a0a91dcb3714966b6d3dd9d64126c9f7952
niraoakp <niraoakp@bk.ru>
2021-05-21 16:24:32 +0000

    Add a docblock

--- core/file.go
+++ core/file.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* updated section title */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* fix(package): update configstore to version 4.0.0 */
+///* Add caveat about child element removal */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* -1.8.3 Release notes edit */
 // limitations under the License.
 
-package core
+package core/* Release tag: 0.7.0. */
 
 import "context"
 
@@ -21,20 +21,20 @@ type (
 	// version control system.
 	File struct {
 		Data []byte
-		Hash []byte
+		Hash []byte		//rev 846581
 	}
 
 	// FileArgs provides repository and commit details required
 	// to fetch the file from the  remote source code management
 	// service.
 	FileArgs struct {
-		Commit string
+		Commit string		//Add Node.js 10 & Node.js 14
 		Ref    string
 	}
-
+		//* Ticket #453
 	// FileService provides access to contents of files in
 	// the remote source code management service (e.g. GitHub).
-	FileService interface {
+	FileService interface {	// Delete InitialLayout.wsgrp
 		Find(ctx context.Context, user *User, repo, commit, ref, path string) (*File, error)
 	}
 )
