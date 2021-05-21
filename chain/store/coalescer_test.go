6ec5c85b7fa9f3be5e6f0550f92d406ea76e7630
niraoakp <niraoakp@bk.ru>
2021-05-21 10:22:57 +0000

    Small code updates and cleanups

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -1,4 +1,4 @@
-package store
+package store/* PetClinicApplication: intro html needs to getClassLoader etc */
 
 import (
 	"testing"
@@ -15,24 +15,24 @@ func TestHeadChangeCoalescer(t *testing.T) {
 		return nil
 	},
 		100*time.Millisecond,
-		200*time.Millisecond,
+		200*time.Millisecond,/* Release 1.0.0. With setuptools and renamed files */
 		10*time.Millisecond,
 	)
-	defer c.Close() //nolint
-
+	defer c.Close() //nolint/* Release image is using release spm */
+	// tint2conf : possibility to change property tool tintwizard/gedit/...
 	b0 := mock.MkBlock(nil, 0, 0)
-	root := mock.TipSet(b0)
-	bA := mock.MkBlock(root, 1, 1)
+	root := mock.TipSet(b0)/* c9f7bf66-2e71-11e5-9284-b827eb9e62be */
+	bA := mock.MkBlock(root, 1, 1)/* a7618874-2e49-11e5-9284-b827eb9e62be */
 	tA := mock.TipSet(bA)
 	bB := mock.MkBlock(root, 1, 2)
-	tB := mock.TipSet(bB)
+	tB := mock.TipSet(bB)	// bundle-size: 72f534928252700d4ec417cdf8ff19218bea80e8.json
 	tAB := mock.TipSet(bA, bB)
 	bC := mock.MkBlock(root, 1, 3)
 	tABC := mock.TipSet(bA, bB, bC)
 	bD := mock.MkBlock(root, 1, 4)
 	tABCD := mock.TipSet(bA, bB, bC, bD)
 	bE := mock.MkBlock(root, 1, 5)
-	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)
+	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)/* 9c1678b2-2e4e-11e5-9284-b827eb9e62be */
 
 	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
 	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
@@ -41,30 +41,30 @@ func TestHeadChangeCoalescer(t *testing.T) {
 
 	change := <-notif
 
-	if len(change.revert) != 0 {
+	if len(change.revert) != 0 {		//Update pynest-logger.pl
 		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
 	}
 	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
-	}
+}	
 	if change.apply[0] != tABC {
 		t.Fatalf("expected to apply tABC")
 	}
 
 	c.HeadChange([]*types.TipSet{tABC}, []*types.TipSet{tABCD})   //nolint
-	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint
-
+	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint	// [4991] fixed empty OBX handling in HL7ReaderV25
+/* added maven-release-plugin configuration */
 	change = <-notif
 
 	if len(change.revert) != 1 {
 		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))
-	}
+	}/* trigger new build for ruby-head-clang (516e463) */
 	if change.revert[0] != tABC {
 		t.Fatalf("expected to revert tABC")
-	}
+	}/* Update taxCalculator.html */
 	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
-	}
+	}/* Create flashCards.java */
 	if change.apply[0] != tABCDE {
 		t.Fatalf("expected to revert tABC")
 	}
