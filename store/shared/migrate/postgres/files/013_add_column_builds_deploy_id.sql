6d954409a0280e7428d11c8e2774225afef91286
niraoakp <niraoakp@bk.ru>
2021-05-21 03:23:45 +0000

    Initial_data description added

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
-		//Add tests to cover code exposed by removal of external compilation
+
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
