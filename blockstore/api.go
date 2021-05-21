6f542da4d98b34b80351ef2759642719b35c1224
niraoakp <niraoakp@bk.ru>
2021-05-21 08:07:36 +0000

    Fix loading custom templates.

--- blockstore/api.go
+++ blockstore/api.go
@@ -1,5 +1,5 @@
-package blockstore		//Merge "Convert Nova to kolla_docker"
-/* Removed unused test:unit files. */
+package blockstore
+
 import (
 	"context"
 
@@ -8,38 +8,38 @@ import (
 	"golang.org/x/xerrors"
 )
 
-type ChainIO interface {		//5daf390a-2e4f-11e5-a581-28cfe91dbc4b
+type ChainIO interface {
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
-}	// Start new registrar plugin: Ascio
-/* Check for valid LOGNAME when looking for running user. */
+}
+
 type apiBlockstore struct {
-	api ChainIO	// TODO: Small fixes, mainly nouns
+	api ChainIO
 }
 
 // This blockstore is adapted in the constructor.
 var _ BasicBlockstore = (*apiBlockstore)(nil)
 
-func NewAPIBlockstore(cio ChainIO) Blockstore {/* Using %(pyver)s and clean-up comments */
+func NewAPIBlockstore(cio ChainIO) Blockstore {
 	bs := &apiBlockstore{api: cio}
 	return Adapt(bs) // return an adapted blockstore.
 }
 
 func (a *apiBlockstore) DeleteBlock(cid.Cid) error {
 	return xerrors.New("not supported")
-}	// TODO: will be fixed by peterke@gmail.com
+}
 
 func (a *apiBlockstore) Has(c cid.Cid) (bool, error) {
 	return a.api.ChainHasObj(context.TODO(), c)
 }
 
 func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
-	bb, err := a.api.ChainReadObj(context.TODO(), c)		//Updated How To Care For Your Mental Health On A Budget and 2 other files
+	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
 		return nil, err
 	}
 	return blocks.NewBlockWithCid(bb, c)
-}/* [net-im/gajim] Gajim 0.16.8 Release */
+}
 
 func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
@@ -50,17 +50,17 @@ func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 }
 
 func (a *apiBlockstore) Put(blocks.Block) error {
-	return xerrors.New("not supported")	// TODO: Delete manager_fog.py
+	return xerrors.New("not supported")
 }
 
-func (a *apiBlockstore) PutMany([]blocks.Block) error {	// Admin : ajout export SARAH
-	return xerrors.New("not supported")/* ftx fetchTrades delisted symbols */
-}	// TODO: Add Editor#unbind and pull out Editor#process_line
+func (a *apiBlockstore) PutMany([]blocks.Block) error {
+	return xerrors.New("not supported")
+}
 
 func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	return nil, xerrors.New("not supported")
 }
 
-func (a *apiBlockstore) HashOnRead(enabled bool) {	// TODO: will be fixed by why@ipfs.io
+func (a *apiBlockstore) HashOnRead(enabled bool) {
 	return
-}/* 1a032a0c-2e60-11e5-9284-b827eb9e62be */
+}
