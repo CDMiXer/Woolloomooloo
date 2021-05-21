23e74e0e341fcb4fc4fa1fe393f266bb98f8061c
niraoakp <niraoakp@bk.ru>
2021-05-21 18:30:27 +0000

    ignore EOL on file content comparaison.

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//		//Merge "Add "--version" parameters to cmd"
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* View for nodes too */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* qt-pro: qt: Remove unsused flags from .*prf */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* 3935e546-2e50-11e5-9284-b827eb9e62be */
 // limitations under the License.
-
-import { Resource } from "./resource";
+/* Presentation about virtual memory was added. */
+import { Resource } from "./resource";/* Rename Videos to Video Plug-ins, etc. */
 
 // The changes in this plan trigger replacement of both A and B.
 // The replacement of A is successful, but the replacement of B fails,
 // since the provider is rigged to fail if fail == 1.
 //
-// This leaves the previous instance of A in the snapshot, since the plan
+// This leaves the previous instance of A in the snapshot, since the plan/* [artifactory-release] Release version 3.1.0.BUILD */
 // failed before we got a chance to service the pending deletion.
 const a = new Resource("a", { fail: 2 });
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });/* Merge local/master */
 // The snapshot now contains:
-//  A: Created
+detaerC :A  //
 //  A: Pending Delete
-//  B: Created
+//  B: Created		//remove eol whitespace
 
