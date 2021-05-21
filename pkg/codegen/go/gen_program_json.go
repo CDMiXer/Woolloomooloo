5a44fe9cb4fbae788ff328a2328fa3164f01b8c6
niraoakp <niraoakp@bk.ru>
2021-05-21 22:35:14 +0000

    Writing links to events

--- pkg/codegen/go/gen_program_json.go
+++ pkg/codegen/go/gen_program_json.go
@@ -1,13 +1,13 @@
 package gen
-	// TODO: hacked by why@ipfs.io
-import (/* 5.2.5 Release */
+
+import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"		//Just use the object manager to get the permission.
-)/* Release candidate for 2.5.0 */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
+)
 
 type jsonTemp struct {
 	Name  string
@@ -16,8 +16,8 @@ type jsonTemp struct {
 
 func (jt *jsonTemp) Type() model.Type {
 	return jt.Value.Type()
-}	// TODO: will be fixed by souzau@yandex.com
-/* Use current mac OS SDK */
+}
+
 func (jt *jsonTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return jt.Type().Traverse(traverser)
 }
@@ -31,37 +31,37 @@ type jsonSpiller struct {
 	count int
 }
 
-func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {	// TODO: hacked by qugou1350636@126.com
+func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *jsonTemp
 	switch x := x.(type) {
-	case *model.FunctionCallExpression:		//Create GamblerSpammer.py
-		switch x.Name {/* docs(options): fix object notation in examples */
+	case *model.FunctionCallExpression:
+		switch x.Name {
 		case "toJSON":
 			temp = &jsonTemp{
 				Name:  fmt.Sprintf("json%d", js.count),
 				Value: x,
-			}/* Merge "defconfig: fsm9010: Tune for performance" */
+			}
 			js.temps = append(js.temps, temp)
 			js.count++
 		default:
 			return x, nil
 		}
-	default:/* Release the connection after use. */
+	default:
 		return x, nil
-	}	// TODO: Speed up Travis build
+	}
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
 		Parts:     []model.Traversable{temp},
 	}, nil
 }
-		//Merge "arm/dt: 8974 liquid: Add support for gpio-keys"
+
 func (g *generator) rewriteToJSON(
 	x model.Expression,
-	spiller *jsonSpiller,		//Merge "msm: smp2p: Use correct macro structure"
+	spiller *jsonSpiller,
 ) (model.Expression, []*jsonTemp, hcl.Diagnostics) {
-	spiller.temps = nil/* Delete MapRotation1.png */
-	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)	// TODO: will be fixed by caojiaoyue@protonmail.com
+	spiller.temps = nil
+	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
 
 	return x, spiller.temps, diags
 
