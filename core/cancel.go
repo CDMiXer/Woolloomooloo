4497271817dba3887970f273e2e2e0bf6e9e8956
niraoakp <niraoakp@bk.ru>
2021-05-21 07:25:03 +0000

    Change remote image source.

--- core/cancel.go
+++ core/cancel.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* Add some more comments to help understand the wizardry I wrote a year ago */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,10 +6,10 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//cgame: fix issues with enemy fireteam icon
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//reuse db connection on load
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
