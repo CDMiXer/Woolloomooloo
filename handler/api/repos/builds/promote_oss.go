6a26c732b2fe16029b2705cece00e4054cbaa511
niraoakp <niraoakp@bk.ru>
2021-05-21 23:19:44 +0000

    Merge branch 'master' into integration_test/883

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by lexy8russo@outlook.com
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Copyright 2019 Drone IO, Inc./* Rename .gitignore to _gitignor */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release 1.10.6 */
+// you may not use this file except in compliance with the License.	// TODO: hacked by lexy8russo@outlook.com
 // You may obtain a copy of the License at
-///* Debug instead of Release makes the test run. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* change to php7 */
-// See the License for the specific language governing permissions and	// TODO: will be fixed by magik6k@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 3.1 Screenshots */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//[ADD] Installing Splash
+/* Corrected typo in P.S. and added a :) */
 // +build oss
 
 package builds
@@ -19,19 +19,19 @@ package builds
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: hacked by martin2cai@hotmail.com
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {		//This will get it's own repo soon.
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}/* re-fixed the info about XML and BIN */
-		//move blog sort from publish to updated date
-// HandlePromote returns a non-op http.HandlerFunc./* Added DBScript */
+}/* Merge "Add metadata length check to volume-manage" */
+
+// HandlePromote returns a non-op http.HandlerFunc.
 func HandlePromote(
 	core.RepositoryStore,
 	core.BuildStore,
 	core.Triggerer,
 ) http.HandlerFunc {
 	return notImplemented
-}	// TODO: hacked by juan@benet.ai
+}
