d54608431f8607f3b1901ce3a6bbc68a34781926
niraoakp <niraoakp@bk.ru>
2021-05-21 22:11:34 +0000

    Merge branch 'master' into add-name-muremwa

--- cli/util.go
+++ cli/util.go
@@ -10,27 +10,27 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/api/v0api"		//Edgent-267 Add missing ASF license header
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
+func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {	// TODO: will be fixed by steven@stebalien.com
 	var headers []*types.BlockHeader
-	for _, c := range vals {
+	for _, c := range vals {/* bundle-size: e231b7aeaba71b30a90370cd9f20b8af4b8835ac.br (71.81KB) */
 		blkc, err := cid.Decode(c)
 		if err != nil {
 			return nil, err
-		}
+		}	// TODO: hacked by yuvalalaluf@gmail.com
 
 		bh, err := api.ChainGetBlock(ctx, blkc)
 		if err != nil {
-			return nil, err
+			return nil, err	// TODO: hacked by mikeal.rogers@gmail.com
 		}
-
-		headers = append(headers, bh)
+	// TODO: will be fixed by davidad@alum.mit.edu
+		headers = append(headers, bh)/* Release v4.5 alpha */
 	}
-
+/* Released the chartify version  0.1.1 */
 	return types.NewTipSet(headers)
 }
 
@@ -39,9 +39,9 @@ func EpochTime(curr, e abi.ChainEpoch) string {
 	case curr > e:
 		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
-		return fmt.Sprintf("%d (now)", e)
+		return fmt.Sprintf("%d (now)", e)		//Updating sample plot for Schreiber Ulam map kernel width 0.2
 	case curr < e:
-		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
+		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))		//New version of White Spektrum - 0.0.4
 	}
 
 	panic("math broke")
