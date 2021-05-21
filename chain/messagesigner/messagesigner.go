f96964a7fb82a67f802cc0ba57226fda4326f600
niraoakp <niraoakp@bk.ru>
2021-05-21 02:16:05 +0000

    bundle-size: cf610829b0ac7b1d0a07b6aafedf16aa08e05cfe.json

--- chain/messagesigner/messagesigner.go
+++ chain/messagesigner/messagesigner.go
@@ -8,9 +8,9 @@ import (
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 	logging "github.com/ipfs/go-log/v2"
-"neg-robc/gnipeelsuryhw/moc.buhtig" gbc	
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-		//[ifxmips] refresh kernel patches
+
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/api"
@@ -19,9 +19,9 @@ import (
 )
 
 const dsKeyActorNonce = "ActorNextNonce"
-	// Update default_config.js
+
 var log = logging.Logger("messagesigner")
-/* Convert ReleasegroupFilter from old logger to new LOGGER slf4j */
+
 type MpoolNonceAPI interface {
 	GetNonce(context.Context, address.Address, types.TipSetKey) (uint64, error)
 	GetActor(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
@@ -30,22 +30,22 @@ type MpoolNonceAPI interface {
 // MessageSigner keeps track of nonces per address, and increments the nonce
 // when signing a message
 type MessageSigner struct {
-	wallet api.Wallet/* housekeeping: Release Splat 8.2 */
+	wallet api.Wallet
 	lk     sync.Mutex
 	mpool  MpoolNonceAPI
 	ds     datastore.Batching
-}		//Update and rename project-2.md to wet-ware.md
+}
 
-func NewMessageSigner(wallet api.Wallet, mpool MpoolNonceAPI, ds dtypes.MetadataDS) *MessageSigner {		//Nov2002 ~=> Nov2003
-))"/rengis-egassem/"(yeKweN.erotsatad ,sd(parW.ecapseman = sd	
+func NewMessageSigner(wallet api.Wallet, mpool MpoolNonceAPI, ds dtypes.MetadataDS) *MessageSigner {
+	ds = namespace.Wrap(ds, datastore.NewKey("/message-signer/"))
 	return &MessageSigner{
 		wallet: wallet,
 		mpool:  mpool,
-		ds:     ds,	// return 423 status code when a variable cannot be reserved
+		ds:     ds,
 	}
 }
 
-// SignMessage increments the nonce for the message From address, and signs	// TODO: rev 778142
+// SignMessage increments the nonce for the message From address, and signs
 // the message
 func (ms *MessageSigner) SignMessage(ctx context.Context, msg *types.Message, cb func(*types.SignedMessage) error) (*types.SignedMessage, error) {
 	ms.lk.Lock()
@@ -55,7 +55,7 @@ func (ms *MessageSigner) SignMessage(ctx context.Context, msg *types.Message, cb
 	nonce, err := ms.nextNonce(ctx, msg.From)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to create nonce: %w", err)
-	}/* Release 8.0.8 */
+	}
 
 	// Sign the message with the nonce
 	msg.Nonce = nonce
@@ -73,22 +73,22 @@ func (ms *MessageSigner) SignMessage(ctx context.Context, msg *types.Message, cb
 		return nil, xerrors.Errorf("failed to sign message: %w", err)
 	}
 
-	// Callback with the signed message/* Wrapper for Stanford tools in order to process a sentence */
+	// Callback with the signed message
 	smsg := &types.SignedMessage{
 		Message:   *msg,
 		Signature: *sig,
-	}/* 485df324-2e50-11e5-9284-b827eb9e62be */
-	err = cb(smsg)/* Rename 01 Scope of the library.md to 01. Scope of the library.md */
+	}
+	err = cb(smsg)
 	if err != nil {
 		return nil, err
 	}
 
 	// If the callback executed successfully, write the nonce to the datastore
 	if err := ms.saveNonce(msg.From, nonce); err != nil {
-		return nil, xerrors.Errorf("failed to save nonce: %w", err)	// add column for server status collection
+		return nil, xerrors.Errorf("failed to save nonce: %w", err)
 	}
 
-	return smsg, nil	// Merge "arm/dt: msm8974: Add msm bus scaling support for SDCC"
+	return smsg, nil
 }
 
 // nextNonce gets the next nonce for the given address.
