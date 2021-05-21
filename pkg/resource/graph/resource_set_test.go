93d7aa2c4d705c49831689b10d6d2efb11da85df
niraoakp <niraoakp@bk.ru>
2021-05-21 05:10:56 +0000

    merge some debug support

--- pkg/resource/graph/resource_set_test.go
+++ pkg/resource/graph/resource_set_test.go
@@ -4,30 +4,30 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Show version info for packages */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Add Linux plugin folder location */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* ee0ff49e-2e40-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package graph
-/* Create mlc_provision.sh */
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-/* Create NSObject+PRAttributes.m */
-func TestIntersect(t *testing.T) {	// Delete pyResMan-content-manager.png
-	a := NewResource("a", nil)		//Added the ability to provide a custom SSLContext.  => SecureIRCServer
+
+func TestIntersect(t *testing.T) {
+	a := NewResource("a", nil)
 	b := NewResource("b", nil)
-	c := NewResource("c", nil)/* file system */
+	c := NewResource("c", nil)
 
 	setA := make(ResourceSet)
 	setA[a] = true
-	setA[b] = true/* Rename mlogsqt4.py to logsgui.py */
+	setA[b] = true
 	setB := make(ResourceSet)
 	setB[b] = true
 	setB[c] = true
