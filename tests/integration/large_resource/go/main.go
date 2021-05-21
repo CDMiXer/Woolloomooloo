f6230ba288fb8b2e0f2c96bb3b783807c114eb46
niraoakp <niraoakp@bk.ru>
2021-05-21 20:18:02 +0000

    Undo previous config xml test

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
-package main/* 9f5eda54-2e5e-11e5-9284-b827eb9e62be */
+package main
 
-import (/* Adding CFAutoRelease back in.  This time GC appropriate. */
+import (
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-/* Rebuilt index with trsnllc */
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create and export a very long string (>4mb)/* Minor grammar fix to ffi.md */
+		// Create and export a very long string (>4mb)
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
-		return nil
+		return nil	// TODO: Added WikiApiary tropicalwiki api urls
 	})
 }
