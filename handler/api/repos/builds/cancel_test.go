6cd01a4f22e234d5575e53d63106b48d0a826cdc
niraoakp <niraoakp@bk.ru>
2021-05-21 02:23:21 +0000

    CndWsgfUF0w5jAWIENDTcPATIFGCyNXX

--- handler/api/repos/builds/cancel_test.go
+++ handler/api/repos/builds/cancel_test.go
@@ -4,7 +4,7 @@
 
 package builds
 
-import (	// TODO: hacked by martin2cai@hotmail.com
+import (
 	"context"
 	"net/http/httptest"
 	"testing"
@@ -14,7 +14,7 @@ import (	// TODO: hacked by martin2cai@hotmail.com
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-)	// TODO: will be fixed by arachnid@notdot.net
+)
 
 func TestCancel(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -26,37 +26,37 @@ func TestCancel(t *testing.T) {
 			Status: core.StatusPending,
 			Steps: []*core.Step{
 				{Status: core.StatusPassing},
-				{Status: core.StatusPending},		//Appveyor: Install Python via conda and also install numpy
+				{Status: core.StatusPending},
 			},
-,}		
-	}	// [en] remove 4×4 from spelling.txt
+		},
+	}
 
-	mockBuildCopy := new(core.Build)	// TODO: will be fixed by why@ipfs.io
+	mockBuildCopy := new(core.Build)
 	*mockBuildCopy = *mockBuild
 
-	repos := mock.NewMockRepositoryStore(controller)		//Skip failing test
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)/* 0424ee3e-2e45-11e5-9284-b827eb9e62be */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuildCopy, nil)
 	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)
-	// TODO: 43824784-2e6d-11e5-9284-b827eb9e62be
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
-		//Prep changelog for release
+
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
-	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)/* Build 3421: Adds Czech translations */
+	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)
 
-	steps := mock.NewMockStepStore(controller)	// TODO: will be fixed by julia@jvns.ca
+	steps := mock.NewMockStepStore(controller)
 	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 	statusService := mock.NewMockStatusService(controller)
 	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
 
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 	scheduler := mock.NewMockScheduler(controller)
 	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)
 
@@ -64,8 +64,8 @@ func TestCancel(t *testing.T) {
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
-		//Fixed time conflict checking (still need tests) and schedule display
-)(redroceRweN.tsetptth =: w	
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
