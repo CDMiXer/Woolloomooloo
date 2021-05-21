9c25d9046c9254a40968dd5f21f55b7cd906d538
niraoakp <niraoakp@bk.ru>
2021-05-21 13:40:54 +0000

    updated readme.md to include more informatino

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,14 +1,14 @@
-package main
+package main/* 9f5eda54-2e5e-11e5-9284-b827eb9e62be */
 
-import (
+import (/* Adding CFAutoRelease back in.  This time GC appropriate. */
 	"strings"
-	// TODO: Improved game launcher
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* create example project */
 
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+/* Rebuilt index with trsnllc */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create and export a very long string (>4mb)		//gwt krise updated
+		// Create and export a very long string (>4mb)/* Minor grammar fix to ffi.md */
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
 		return nil
 	})
