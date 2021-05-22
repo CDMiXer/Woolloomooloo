378bc9a85ee6095a873d3576b346491f1acf83ad
niraoakp <niraoakp@bk.ru>
2021-05-22 16:58:57 +0000

    Create symbol.cpp

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Fixed composer require, also added doctrine versions 2.3 and 2.4. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-	// TODO: will be fixed by arachnid@notdot.net
+
 // Attempt to delete the resource; this will fail!
