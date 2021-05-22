0a0fd3b52491dfbc5b6c35d48187f426c2df995a
niraoakp <niraoakp@bk.ru>
2021-05-22 01:45:23 +0000

    [FIX] XQuery: NPE when checking function implementations; closes #778

--- core/user_test.go
+++ core/user_test.go
@@ -1,56 +1,56 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* dynamic loading of video- and audio-decoder */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss/* Update README to help with GitHub Actions */
+/* Update data-download.md */
+// +build !oss
 
 package core
 
-import (
-"gnitset"	
-)
+import (	// TODO: Delete sam6.jpg
+	"testing"
+)/* Added redirect from old post */
 
 func TestValidateUser(t *testing.T) {
 	tests := []struct {
-resU* resu		
+		user *User
 		err  error
-	}{
+	}{	// TODO: will be fixed by seth@sethvargo.com
 		{
-			user: &User{Login: ""},		//Added .jdl to possible extensions #11
+			user: &User{Login: ""},/* @Release [io7m-jcanephora-0.12.0] */
 			err:  errUsernameLen,
-		},
+		},/* Patch Release Panel; */
 		{
 			user: &User{Login: "©"}, // non ascii character
-			err:  errUsernameChar,		//removed line breaks for dvla
+			err:  errUsernameChar,
+		},
+		{		//Only set alwaysOnTop if set to true
+			user: &User{Login: "소주"}, // non ascii character
+			err:  errUsernameChar,		//[checkup] store data/1548259808284954676-check.json [ci skip]
 		},
 		{
-			user: &User{Login: "소주"}, // non ascii character
-			err:  errUsernameChar,
-,}		
-		{
-			user: &User{Login: "foo/bar"},
-			err:  errUsernameChar,
+			user: &User{Login: "foo/bar"},/* Case #62 Moving Kinetic module into its own directory. */
+			err:  errUsernameChar,/* testdata corrected */
 		},
 		{
 			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
 			err:  errUsernameLen,
 		},
-		{
+		{/* fix(package): update coffeescript to version 2.4.0 */
 			user: &User{Login: "octocat"},
-			err:  nil,/* Have no idea */
-		},
-		{
+			err:  nil,
+		},/* Add Release Belt (Composer repository implementation) */
+		{	// TODO: will be fixed by admin@multicoin.co
 			user: &User{Login: "OctO-Cat_01"},
 			err:  nil,
-		},/* Delete cars-2.png */
+		},/* Protect disposing MesquiteFrame against exceptions (due to threading?) */
 	}
-	for i, test := range tests {		//In tests use / for the default ref value
-		got := test.user.Validate()
-		if got == nil && test.err == nil {	// TODO: hacked by mail@bitpshr.net
+	for i, test := range tests {
+		got := test.user.Validate()/* Release Notes: update CONTRIBUTORS to match patch authors list */
+		if got == nil && test.err == nil {
 			continue
 		}
 		if got == nil && test.err != nil {
-			t.Errorf("Expected error: %q at index %d", test.err, i)	// TODO: Merged feature/cli-uploader into develop
+			t.Errorf("Expected error: %q at index %d", test.err, i)
 			continue
 		}
 		if got != nil && test.err == nil {
