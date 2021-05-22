aa7d441e953508caa56b09c64787a30487e87aed
niraoakp <niraoakp@bk.ru>
2021-05-22 10:05:13 +0000

    Merge "Show hovercard actions in submit requirement account chips"

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail/* Split up the SCL mixin to match the system one. */
-/* Subs: Added support for muxing ASS subs in MKV */
-echo "# This is an auto-generated file. DO NOT EDIT"
-cat/* Added NIST meta 3.13.6 */
+set -eu -o pipefail
+/* hook up tutorials schedule */
+echo "# This is an auto-generated file. DO NOT EDIT"/* Renamed ActionFact to Action */
+cat
