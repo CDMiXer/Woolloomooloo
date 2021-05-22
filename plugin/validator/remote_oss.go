65a903f3a7d00b8c2f060e73e04d5d7a672fbfce
niraoakp <niraoakp@bk.ru>
2021-05-22 14:11:34 +0000

    Change climbing day 1 from Tues -> Wed

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by cory@protocol.ai
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* fbcon uses all screen space */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Update AD.PartialEquilibriumApi.Tests.csproj
+
 // +build oss
 
 package validator
