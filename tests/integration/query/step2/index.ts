168ec62676b3e1d7056a529df65669b1625f819c
niraoakp <niraoakp@bk.ru>
2021-05-22 23:07:46 +0000

    changing default seed status for fetch pipeline seed method

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Added mavenlink as a default robot
-/* Merge "Refactor the lcov jobs" */
-import { Resource } from "./resource";
-/* Works! Now with real polling! */
-// Step 2: Create resources during `pulumi query` -- should error.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import { Resource } from "./resource";/* 6d9d4a80-2e42-11e5-9284-b827eb9e62be */
+
+// Step 2: Create resources during `pulumi query` -- should error./* Release Notes for v00-16-04 */
 const b = new Resource("b", { state: 2 });
 const a = new Resource("a", { state: 1, resource: b });
