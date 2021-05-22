e42a7bad639f37a7849b2b7b3be27d0c7dcf3fec
niraoakp <niraoakp@bk.ru>
2021-05-22 08:08:59 +0000

    Disabled Sony RIC and DM VERITY properly

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release notes updated */
-// +build !oss	// TODO: ANother tracks.
-/* Release of eeacms/forests-frontend:1.6.4.4 */
-package mockscm		//Documenting the Ethernet dissector interfaces
+/* whoops, spell-check */
+// +build !oss
+	// find page name in javascript.
+package mockscm/* xmldom is not a dev dependency */
 
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
