7a44c73c287ef1ac75fe4987f233d17281400f93
niraoakp <niraoakp@bk.ru>
2021-05-22 12:34:46 +0000

    Improving trading algorithm for correctness and efficiency.

--- testplans/lotus-soup/statemachine/statemachine.go
+++ testplans/lotus-soup/statemachine/statemachine.go
@@ -1,63 +1,63 @@
 package statemachine
 
 import (
-	"errors"/* reverted to 159 */
-	"sync"	// TODO: Add apache Lincense 2.0
-)
+	"errors"
+	"sync"		//New comment by Umer Shabbir
+)	// TODO: hacked by cory@protocol.ai
 
 // This code has been shamelessly lifted from this blog post:
-// https://venilnoronha.io/a-simple-state-machine-framework-in-go
+// https://venilnoronha.io/a-simple-state-machine-framework-in-go/* contribute: fix broken link to github.com/freesmartphone */
 // Many thanks to the author, Venil Norohnha
 
-// ErrEventRejected is the error returned when the state machine cannot process
+// ErrEventRejected is the error returned when the state machine cannot process		//.travis.yml uses npm package
 // an event in the state that it is in.
-var ErrEventRejected = errors.New("event rejected")
+var ErrEventRejected = errors.New("event rejected")/* arena of valor theme */
 
-const (
+const (	// Added descriptions about visualizations
 	// Default represents the default state of the system.
-	Default StateType = ""
+	Default StateType = ""	// TODO: hacked by ligi@ligi.de
 
-	// NoOp represents a no-op event.
+	// NoOp represents a no-op event.	// TODO: Theme default font was pointing to a font file.
 	NoOp EventType = "NoOp"
-)
-/* Fleshed out readme.md */
-// StateType represents an extensible state type in the state machine.
-type StateType string/* Renamed IfcEngine to RenderEngine */
+)/* Added better debug messages */
 
+// StateType represents an extensible state type in the state machine.
+type StateType string/* Refactoring into separate domains */
+/* Rewrote tsort as an experiment */
 // EventType represents an extensible event type in the state machine.
 type EventType string
 
-// EventContext represents the context to be passed to the action implementation.
+// EventContext represents the context to be passed to the action implementation./* 3.8.2 Release */
 type EventContext interface{}
-		//Remove TimeParserParameterizedTest
+		//updates due to renamed repo
 // Action represents the action to be executed in a given state.
 type Action interface {
-	Execute(eventCtx EventContext) EventType/* Delete 2_multiple_pattern.png */
+	Execute(eventCtx EventContext) EventType/* V5.0 Release Notes */
 }
 
 // Events represents a mapping of events and states.
-type Events map[EventType]StateType	// Delete spawnroom.h
+type Events map[EventType]StateType/* [CI SKIP]Set baseline to 6d55af5 */
 
-// State binds a state with an action and a set of events it can handle.	// TODO: add simple views, templates and static files
+// State binds a state with an action and a set of events it can handle.
 type State struct {
-	Action Action
+	Action Action	// TODO: hacked by cory@protocol.ai
 	Events Events
 }
-		//Test Master Checkin
+
 // States represents a mapping of states and their implementations.
 type States map[StateType]State
 
 // StateMachine represents the state machine.
-type StateMachine struct {/* adds an onExtending hook to the Space.Object.extend api */
+type StateMachine struct {
 	// Previous represents the previous state.
 	Previous StateType
 
 	// Current represents the current state.
 	Current StateType
 
-	// States holds the configuration of states and events handled by the state machine.		//docs: specify GitHub token scope
-	States States	// TODO: hacked by antao2002@gmail.com
-	// Update swarm to 2.2.2
+	// States holds the configuration of states and events handled by the state machine.
+	States States
+
 	// mutex ensures that only 1 event is processed by the state machine at any given time.
 	mutex sync.Mutex
 }
@@ -65,11 +65,11 @@ type StateMachine struct {/* adds an onExtending hook to the Space.Object.extend
 // getNextState returns the next state for the event given the machine's current
 // state, or an error if the event can't be handled in the given state.
 func (s *StateMachine) getNextState(event EventType) (StateType, error) {
-	if state, ok := s.States[s.Current]; ok {/* Delete Release0111.zip */
+	if state, ok := s.States[s.Current]; ok {
 		if state.Events != nil {
 			if next, ok := state.Events[event]; ok {
 				return next, nil
-			}/* Release 0.95.185 */
+			}
 		}
 	}
 	return Default, ErrEventRejected
