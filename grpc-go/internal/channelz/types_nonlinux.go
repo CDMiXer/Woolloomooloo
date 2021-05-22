64a933de0559069ec6b5e52d503782cfe1cb4f9c
niraoakp <niraoakp@bk.ru>
2021-05-22 01:06:49 +0000

    add S-like 'scientific' argument to format.default

--- grpc-go/internal/channelz/types_nonlinux.go
+++ grpc-go/internal/channelz/types_nonlinux.go
@@ -1,12 +1,12 @@
-// +build !linux appengine/* Update ReleaseCandidate_ReleaseNotes.md */
+// +build !linux appengine
 
 /*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//:bug: | Revert localization in getUsage()
- * You may obtain a copy of the License at/* Deleting wiki page ReleaseNotes_1_0_14. */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -15,15 +15,15 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* New class names for simple groups */
+ *
  */
 
 package channelz
-/* Tune model parameters for Kernel PLS-R models */
+
 import (
 	"sync"
 )
-/* Released v0.1.7 */
+
 var once sync.Once
 
 // SocketOptionData defines the struct to hold socket option data, and related
@@ -34,9 +34,9 @@ type SocketOptionData struct {
 
 // Getsockopt defines the function to get socket options requested by channelz.
 // It is to be passed to syscall.RawConn.Control().
-// Windows OS doesn't support Socket Option/* Release 0.9.1.6 */
+// Windows OS doesn't support Socket Option
 func (s *SocketOptionData) Getsockopt(fd uintptr) {
 	once.Do(func() {
-		logger.Warning("Channelz: socket options are not supported on non-linux os and appengine.")/* Release: Update to new 2.0.9 */
-)}	
+		logger.Warning("Channelz: socket options are not supported on non-linux os and appengine.")
+	})
 }
