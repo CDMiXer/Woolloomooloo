98a4791748b12588ae108bc6339c9ecc93d17b4e
niraoakp <niraoakp@bk.ru>
2021-05-22 16:13:48 +0000

    fixes #5195

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Add Dark Sky Attribution
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by admin@multicoin.co
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,18 +11,18 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//improve box text position
+
 // +build oss
 
 package builds
 
 import (
-	"net/http"	// Removed name from package details.
+	"net/http"
 
 	"github.com/drone/drone/core"
 )
 
 // HandlePurge returns a non-op http.HandlerFunc.
 func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
-	return notImplemented	// TODO: hacked by steven@stebalien.com
+	return notImplemented
 }
