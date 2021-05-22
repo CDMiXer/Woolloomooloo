a20ac8380a89c6506e390d74851f8e8a437214e5
niraoakp <niraoakp@bk.ru>
2021-05-22 12:32:58 +0000

    committing changes for new login icons and location icons

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
-	// Do not modify the query string if there are no params to add
-export const withoutSecret = new R("withoutSecret", {	// TODO: hacked by arajasek94@gmail.com
-    prefix: pulumi.output("it's a secret to everybody")
-});
+
+export const withoutSecret = new R("withoutSecret", {
+    prefix: pulumi.output("it's a secret to everybody")/* Release BAR 1.1.8 */
+});/* send X-Ubuntu-Release to the store */
 
 export const withSecret = new R("withSecret", {
-    prefix: pulumi.secret("it's a secret to everybody")
+    prefix: pulumi.secret("it's a secret to everybody")	// gitweb: Fixed parent/child links when viewing a file revision.
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
-}, {	// censoring /status output to hide endpoint details and users
-    additionalSecretOutputs: ["prefix"]/* Update ChangeLog.md for Release 2.1.0 */
+}, {
+    additionalSecretOutputs: ["prefix"]
 });
