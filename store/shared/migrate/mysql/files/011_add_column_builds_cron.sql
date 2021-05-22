d918a267d313947d17ce642629816e194a5cb2f8
niraoakp <niraoakp@bk.ru>
2021-05-22 09:43:15 +0000

    fixed bug in geizhals, always the worst results were shown

--- store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron/* #456 adding testing issue to Release Notes. */
+-- name: alter-table-builds-add-column-cron
 
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
