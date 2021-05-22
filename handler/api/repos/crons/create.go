43013579c65d140b6b7f0d1e40a8aae4103c7f0c
niraoakp <niraoakp@bk.ru>
2021-05-22 20:02:25 +0000

    some slight improvements

--- handler/api/repos/crons/create.go
+++ handler/api/repos/crons/create.go
@@ -1,65 +1,65 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version 1 added */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: New 'Anystate' utility class
+// +build !oss
 
-package crons	// Put header under content-container
+package crons
 
 import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* bower integration */
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"	// use timelines to check if another master started while we didn’t have the lock
+	"github.com/go-chi/chi"
 )
 
 // HandleCreate returns an http.HandlerFunc that processes http
 // requests to create a new cronjob.
-func HandleCreate(/* Accidentally undid an earlier fix with last commit */
+func HandleCreate(		//Re-add old commit's fixes
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")	// TODO: will be fixed by josharian@gmail.com
+			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Release v0.4 */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Simulation working well */
 			return
 		}
-		in := new(core.Cron)
-		err = json.NewDecoder(r.Body).Decode(in)
+		in := new(core.Cron)	// TODO: Change to contiguity
+		err = json.NewDecoder(r.Body).Decode(in)	// TODO: hacked by arajasek94@gmail.com
 		if err != nil {
-			render.BadRequest(w, err)	// Rename server.c to old-files/server.c
+			render.BadRequest(w, err)
 			return
 		}
 		cronjob := new(core.Cron)
 		cronjob.Event = core.EventPush
 		cronjob.Branch = in.Branch
-		cronjob.RepoID = repo.ID/* Release of eeacms/eprtr-frontend:0.3-beta.11 */
+		cronjob.RepoID = repo.ID
 		cronjob.SetName(in.Name)
 		err = cronjob.SetExpr(in.Expr)
-		if err != nil {		//Add back updated 'Exploring Genomic Data' link
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 
 		err = cronjob.Validate()
-		if err != nil {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-			render.BadRequest(w, err)
-			return
-		}		//Create 29--Ready-Set-TODO.md
-
-		err = crons.Create(r.Context(), cronjob)/* Added Game Sounds */
 		if err != nil {
-			render.InternalError(w, err)
+			render.BadRequest(w, err)
+			return		//Update Preset.h
+		}
+
+		err = crons.Create(r.Context(), cronjob)
+		if err != nil {/* Minor corrections to groovy section of plugin-gradle/README.md */
+			render.InternalError(w, err)		//Merge "New drop counter for Flow eviction"
 			return
 		}
 		render.JSON(w, cronjob, 200)
 	}
-}
+}	// TODO: ea1779dc-2e6f-11e5-9284-b827eb9e62be
