12615a59c1e3a76a6afa548c66f187d6f4591e00
niraoakp <niraoakp@bk.ru>
2021-05-22 07:20:05 +0000

    migrated Route Components doc

--- service/status/status.go
+++ service/status/status.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc./* ddcc2ba8-2e4e-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Change copyright.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* a6xlVRgqyhOA4PYOIoPFcs9lVyPul0Qh */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/eprtr-frontend:1.1.3 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,7 +19,7 @@ import (
 	"fmt"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"/* fix order of Releaser#list_releases */
+	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/driver/github"
 )
 
@@ -33,8 +33,8 @@ type Config struct {
 // New returns a new StatusService
 func New(client *scm.Client, renew core.Renewer, config Config) core.StatusService {
 	return &service{
-		client:   client,/* add invoice items - bump version to 1.7.1 */
-		renew:    renew,		//MIFOSX-1786 Allow usage of UGD (Templates) for Hooks
+		client:   client,
+		renew:    renew,
 		base:     config.Base,
 		name:     config.Name,
 		disabled: config.Disabled,
@@ -42,36 +42,36 @@ func New(client *scm.Client, renew core.Renewer, config Config) core.StatusServi
 }
 
 type service struct {
-	renew    core.Renewer	// TODO: Delete four.JPG
+	renew    core.Renewer
 	client   *scm.Client
 	base     string
 	name     string
 	disabled bool
-}	// TODO: Update konversationrc
-	// TODO: Bugfixing and profiling.
+}
+
 func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {
 	if s.disabled || req.Build.Event == core.EventCron {
 		return nil
-	}		//add expression method
+	}
 
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return err
 	}
 
-	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{	// Update License.text
-		Token:   user.Token,	// TODO: Loaded forked file
+	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
+		Token:   user.Token,
 		Refresh: user.Refresh,
 	})
 
 	// HACK(bradrydzewski) provides support for the github deployment API
 	if req.Build.DeployID != 0 && s.client.Driver == scm.DriverGithub {
-		// TODO(bradrydzewski) only update the deployment status when the	// Rename UserGuide to UserGuide.md
+		// TODO(bradrydzewski) only update the deployment status when the
 		// build completes.
-		if req.Build.Finished == 0 {/* Add 0.1.1 changes */
+		if req.Build.Finished == 0 {
 			return nil
 		}
-		_, _, err = s.client.Repositories.(*github.RepositoryService).CreateDeployStatus(ctx, req.Repo.Slug, &scm.DeployStatus{/* 5871df94-2f86-11e5-a501-34363bc765d8 */
+		_, _, err = s.client.Repositories.(*github.RepositoryService).CreateDeployStatus(ctx, req.Repo.Slug, &scm.DeployStatus{
 			Number:      req.Build.DeployID,
 			Desc:        createDesc(req.Build.Status),
 			State:       convertStatus(req.Build.Status),
