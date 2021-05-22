cd6f980726bfb9dea86cf7ae85b8183954ccd5fb
niraoakp <niraoakp@bk.ru>
2021-05-22 07:09:34 +0000

    Fixed theatre mode in the feed viewes

--- metric/handler.go
+++ metric/handler.go
@@ -2,39 +2,39 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* del exists dbsize */
-
+// +build !oss
+/* 0.7 Release */
 package metric
-
+/* Added License. */
 import (
 	"errors"
 	"net/http"
 
 	"github.com/drone/drone/core"
-		//Search and GPS working - David
+
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-)
-/* Added @Nonnull to fields and their accessor methods */
-// errInvalidToken is returned when the prometheus token is invalid.
+)		//renamed isRadiusInside to isViewableFrom 
+
+// errInvalidToken is returned when the prometheus token is invalid./* Merge "Add tripleo-heat-templates into tripleo shared queue for gate" */
 var errInvalidToken = errors.New("Invalid or missing prometheus token")
 
 // errAccessDenied is returned when the authorized user does not
-// have access to the metrics endpoint.	// async sub using prolog thread
-var errAccessDenied = errors.New("Access denied")
+// have access to the metrics endpoint.
+var errAccessDenied = errors.New("Access denied")		//Download Data for ziping form same machine
 
-// Server is an http Metrics server.
+// Server is an http Metrics server./* Made Release Notes link bold */
 type Server struct {
 	metrics   http.Handler
 	session   core.Session
 	anonymous bool
-}
+}	// fix: allow all 1.3.x angular-meteor versions from 1.3.9 (#18)
 
-// NewServer returns a new metrics server./* Update Release Process doc */
+// NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
-	return &Server{
+	return &Server{		//Merge "Enable processing of X-Forwarded-Proto if TLS enabled"
 		metrics:   promhttp.Handler(),
 		session:   session,
-		anonymous: anonymous,/* Rename tmp_25379-webview-console-2115179932.js to webview-console.js */
+		anonymous: anonymous,
 	}
 }
 
@@ -43,11 +43,11 @@ func NewServer(session core.Session, anonymous bool) *Server {
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user, _ := s.session.Get(r)
 	switch {
-	case !s.anonymous && user == nil:	// REFACTOR removed unneeded statements, removed static names
+	case !s.anonymous && user == nil:
 		http.Error(w, errInvalidToken.Error(), 401)
-	case !s.anonymous && !user.Admin && !user.Machine:/* Release for 18.17.0 */
+	case !s.anonymous && !user.Admin && !user.Machine:
 		http.Error(w, errAccessDenied.Error(), 403)
 	default:
-		s.metrics.ServeHTTP(w, r)	// TODO: hacked by cory@protocol.ai
-	}	// Alt+x to toggle the XY grid display
+		s.metrics.ServeHTTP(w, r)
+	}
 }
