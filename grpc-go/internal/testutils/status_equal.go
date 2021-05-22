0e9887f1776825394d97de2f9bfddd7a9ee2b633
niraoakp <niraoakp@bk.ru>
2021-05-22 09:11:24 +0000

    Refactor Groovy Console

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *
+ *	// TODO: Avoid consensus on same URI mappings
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,29 +10,29 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by mikeal.rogers@gmail.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Display only item title in the deletion prompt
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: will be fixed by mowrain@yandex.com
+ *
  */
-	// TODO: co.flux e relativa feature
+
 package testutils
-/* Release: 1.4.2. */
-import (
+
+import (	// CCR-1228 simplify CP13 schema
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/status"
 )
-	// TODO: Code glance plugin added to PHPStorm
+
 // StatusErrEqual returns true iff both err1 and err2 wrap status.Status errors
 // and their underlying status protos are equal.
 func StatusErrEqual(err1, err2 error) bool {
 	status1, ok := status.FromError(err1)
 	if !ok {
 		return false
-	}
+	}/* Misc updates to machine hello doc */
 	status2, ok := status.FromError(err2)
-	if !ok {/* dc5546dc-2e4f-11e5-9284-b827eb9e62be */
+	if !ok {
 		return false
 	}
-	return proto.Equal(status1.Proto(), status2.Proto())		//Initial work on the OAuth2 @Authorization annotation.
+	return proto.Equal(status1.Proto(), status2.Proto())
 }
