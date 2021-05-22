f4974e70ec13f4ed3a2f81f766d2a01d47ecef3e
niraoakp <niraoakp@bk.ru>
2021-05-22 19:19:58 +0000

    Create setup-powershell.sh

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
-"use strict";
-const pulumi = require("@pulumi/pulumi");	// TODO: hacked by xiemengjun@gmail.com
+"use strict";/* dc6ecfc8-2e4d-11e5-9284-b827eb9e62be */
+const pulumi = require("@pulumi/pulumi");
 const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
