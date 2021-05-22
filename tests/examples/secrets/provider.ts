576f671844597a18dc7b1315930174f6d5f4d626
niraoakp <niraoakp@bk.ru>
2021-05-22 15:57:18 +0000

    Raising version as releasing new version

--- tests/examples/secrets/provider.ts
+++ tests/examples/secrets/provider.ts
@@ -1,33 +1,33 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-/* changement titre */
+
 class ReflectProvider implements dynamic.ResourceProvider {
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: inputs }); }
-    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }/* Delete encoder.ino */
+    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
 }
-		//f1c4d06e-4b19-11e5-b01c-6c40088e03e4
+
 export class ReflectResource<T> extends dynamic.Resource {
-    public readonly value!: pulumi.Output<T>;		//d1a14988-585a-11e5-9d0b-6c40088e03e4
+    public readonly value!: pulumi.Output<T>;
 
     constructor(name: string, value: pulumi.Input<T>, opts?: pulumi.CustomResourceOptions) {
         super(new ReflectProvider(), name, {value: value}, opts);
     }
-}		//Merge "Actually start the systemd services in step3 of the major-upgrade step"
+}
 
 class DummyProvider implements dynamic.ResourceProvider {
-    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }/* - Released 1.0-alpha-8. */
+    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
-    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }/* Gem-specific README info */
+    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: {"value": "hello"} }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
 }
 
 export class DummyResource extends dynamic.Resource {
-    public readonly value!: pulumi.Output<string>;/* DATAGRAPH-573 - Prepare next development iteration. */
-		//Delete RunPipeline.sh
+    public readonly value!: pulumi.Output<string>;
+
     constructor(name: string, opts?: pulumi.CustomResourceOptions) {
         super(new DummyProvider(), name, {}, opts);
     }
