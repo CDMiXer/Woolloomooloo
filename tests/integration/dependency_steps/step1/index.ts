ac5ee7f64a45b9daeebc7f19db5ee590d695bf06
niraoakp <niraoakp@bk.ru>
2021-05-22 08:18:59 +0000

    Rebuilt index with prscllsb

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//fixed close
-
-// Step 1: Populate our dependency graph.		//Added findbugs gradle plugin
+import { Resource } from "./resource";
+		//Merge branch 'master' into #52/custom-color-picker-layout
+// Step 1: Populate our dependency graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });
+const b = new Resource("b", { state: 2, resource: a });	// TODO: Update OpenWisAuthorization.java
 // Dependency graph: b -> a
