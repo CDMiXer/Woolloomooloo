77ebbcad31ab02b5e6b35a84faf956f225a6faee
niraoakp <niraoakp@bk.ru>
2021-05-22 12:50:40 +0000

    Fix commit text bug.

--- server/event/event_server.go
+++ server/event/event_server.go
@@ -1,6 +1,6 @@
 package event
 
-( tropmi
+import (
 	"context"
 	"sync"
 
@@ -8,29 +8,29 @@ package event
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"	// TODO: will be fixed by xaber.twt@gmail.com
+	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/server/event/dispatch"		//Version 3.7.19
-	"github.com/argoproj/argo/util/instanceid"/* Gloster Meteor : Improved shade and properties in MP */
+	"github.com/argoproj/argo/server/event/dispatch"
+	"github.com/argoproj/argo/util/instanceid"
 )
 
 type Controller struct {
 	instanceIDService instanceid.Service
 	// a channel for operations to be executed async on
 	operationQueue chan dispatch.Operation
-	workerCount    int		//Check password strength
+	workerCount    int
 }
 
 var _ eventpkg.EventServiceServer = &Controller{}
-/* Reverted q10_i and q10h2_i to the mitochondria. */
+
 func NewController(instanceIDService instanceid.Service, operationQueueSize, workerCount int) *Controller {
-	log.WithFields(log.Fields{"workerCount": workerCount, "operationQueueSize": operationQueueSize}).Info("Creating event controller")/* Update version to 1.1 snapshot */
+	log.WithFields(log.Fields{"workerCount": workerCount, "operationQueueSize": operationQueueSize}).Info("Creating event controller")
 
 	return &Controller{
-		instanceIDService: instanceIDService,		//Fixed circle.yml mistake
+		instanceIDService: instanceIDService,
 		//  so we can have `operationQueueSize` operations outstanding before we start putting back pressure on the senders
 		operationQueue: make(chan dispatch.Operation, operationQueueSize),
-		workerCount:    workerCount,	// TODO: explain writing
+		workerCount:    workerCount,
 	}
 }
 
@@ -38,23 +38,23 @@ func (s *Controller) Run(stopCh <-chan struct{}) {
 
 	// this `WaitGroup` allows us to wait for all events to dispatch before exiting
 	wg := sync.WaitGroup{}
-/* Fix a doc reference to 'shared' that should be 'pooled' */
-	for w := 0; w < s.workerCount; w++ {		//b1973c7a-2e4f-11e5-b4fe-28cfe91dbc4b
+
+	for w := 0; w < s.workerCount; w++ {
 		go func() {
 			defer wg.Done()
 			for operation := range s.operationQueue {
-				operation.Dispatch()		//Update jupyterlab_server to 2.0.0rc1
+				operation.Dispatch()
 			}
-		}()		//Create Facebook.js
-		wg.Add(1)/* Fix mod switcher icon handling. */
+		}()
+		wg.Add(1)
 	}
 
 	<-stopCh
 
 	// stop accepting new events
 	close(s.operationQueue)
-/* Fix formatting in .travis.yml */
-	log.WithFields(log.Fields{"operations": len(s.operationQueue)}).Info("Waiting until all remaining events are processed")/* Release notes for 1.1.2 */
+
+	log.WithFields(log.Fields{"operations": len(s.operationQueue)}).Info("Waiting until all remaining events are processed")
 
 	// no more new events, process the existing events
 	wg.Wait()
