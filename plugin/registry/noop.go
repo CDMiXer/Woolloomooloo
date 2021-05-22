5cee82597de98ecba2bc61a6f59e0b1be17da1fd
niraoakp <niraoakp@bk.ru>
2021-05-22 15:02:00 +0000

    connect fix

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//JQMTabs improved.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,11 +17,11 @@ package registry
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* Updated MDHT Release. */
+	"github.com/drone/drone/core"
 )
 
 type noop struct{}
-/* Merge "Release 3.2.3.348 Prima WLAN Driver" */
+
 func (noop) List(context.Context, *core.RegistryArgs) ([]*core.Registry, error) {
 	return nil, nil
 }
