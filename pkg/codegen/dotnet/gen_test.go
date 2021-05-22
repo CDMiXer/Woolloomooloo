92723a42ccd82efefb001e7f8497211387cb4c0a
niraoakp <niraoakp@bk.ru>
2021-05-22 04:04:00 +0000

    Fixed build.properites

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -1,54 +1,54 @@
-package dotnet/* Update deployApp.groovy */
+package dotnet		//tidying up the navigation
 
-import (
-	"path/filepath"
-	"testing"/* Chore: update readme.md file for singlerestaurantcontainer */
-/* Release bzr-1.7.1 final */
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+import (/* Merge "[Release] Webkit2-efl-123997_0.11.40" into tizen_2.1 */
+	"path/filepath"	// TODO: will be fixed by cory@protocol.ai
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* Release 1.0! */
 	"github.com/stretchr/testify/assert"
 )
-
+	// TODO: Forgot to take out the log statement.
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
 		name          string
-		schemaDir     string
+		schemaDir     string/* change - for _ */
 		expectedFiles []string
-	}{
+	}{	// Update specs for noncapture regex
 		{
 			"Simple schema with local resource properties",
-			"simple-resource-schema",
+			"simple-resource-schema",		//rev 752421
 			[]string{
-				"Resource.cs",
-				"OtherResource.cs",	// File and dir context menu interface update
+				"Resource.cs",	// TODO: Remove unused endpoints
+				"OtherResource.cs",
 				"ArgFunction.cs",
 			},
-		},	// TODO: Improve markdown formatting
-		{	// TODO: hacked by witek@enjin.io
+		},
+		{
 			"Simple schema with enum types",
-			"simple-enum-schema",/* some fixes, some more test cases */
+			"simple-enum-schema",
 			[]string{
-				"Tree/V1/RubberTree.cs",
-				"Tree/V1/Enums.cs",	// TODO: will be fixed by brosner@gmail.com
+				"Tree/V1/RubberTree.cs",	// TODO: Corrected DEFAULT_CIRC_DESK translation.
+				"Tree/V1/Enums.cs",
 				"Enums.cs",
 				"Inputs/ContainerArgs.cs",
 				"Outputs/Container.cs",
-			},/* ReleaseID. */
-		},
+			},
+		},/* Release version 3.0.0.M4 */
 		{
 			"External resource schema",
-			"external-resource-schema",
+,"amehcs-ecruoser-lanretxe"			
 			[]string{
-				"Inputs/PetArgs.cs",		//Update filterworden.lua
+				"Inputs/PetArgs.cs",
 				"ArgFunction.cs",
 				"Cat.cs",
 				"Component.cs",
 				"Workload.cs",
 			},
 		},
-	}
+	}/* Removed use of FunctionalSourceSet from platformPlay */
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {	// TODO: Added favicon and encoding
+		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
@@ -56,13 +56,13 @@ func TestGeneratePackage(t *testing.T) {
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)
 			assert.NoError(t, err)
 
-			test.ValidateFileEquality(t, files, expectedFiles)/* Release jedipus-2.6.18 */
-		})
-	}	// TODO: Wider layout
-}		//Create week_db.php
+			test.ValidateFileEquality(t, files, expectedFiles)		//Exception renamed.
+		})/* Release new version 2.5.39:  */
+	}	// TODO: working on delete object
+}
 
 func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {/* Release jedipus-2.6.28 */
+	tests := []struct {
 		input    string
 		expected string
 		wantErr  bool
