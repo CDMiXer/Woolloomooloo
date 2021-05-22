fff5c71c753a54992e34244f3bb896688084a603
niraoakp <niraoakp@bk.ru>
2021-05-22 15:09:22 +0000

    :scroll: nit pickin

--- pkg/codegen/hcl2/syntax/parser.go
+++ pkg/codegen/hcl2/syntax/parser.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation./* make pool.aquire context manager aware */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Merge "Add test for compute API os-quota-class-sets"
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release DBFlute-1.1.0-sp5 */
 
 package syntax
 
@@ -21,40 +21,40 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
-
+/* Merge "Remove placeholder file from releasenotes/notes dir" */
 // File represents a single parsed HCL2 source file.
 type File struct {
 	Name   string          // The name of the file.
 	Body   *hclsyntax.Body // The body of the parsed file.
 	Bytes  []byte          // The raw bytes of the source file.
 	Tokens TokenMap        // A map from syntax nodes to token information.
-}
+}	// TODO: Unified the way e2e are executed
 
 // Parser is a parser for HCL2 source files.
 type Parser struct {
-	Files       []*File         // The parsed files.
+	Files       []*File         // The parsed files./* Update notes for Release 1.2.0 */
 	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.
 	tokens      tokenMap        // A map from syntax nodes to token information.
-}
+}	// TODO: hacked by indexxuan@gmail.com
 
 // NewParser creates a new HCL2 parser.
-func NewParser() *Parser {
+func NewParser() *Parser {	// TODO: hacked by vyzo@hackzen.org
 	return &Parser{tokens: tokenMap{}}
 }
-
+/* Released version 0.8.23 */
 // ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated
 // will be added to the parser's diagnostics.
 func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	src, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
-	}
+	}/* Update syntax/purpose_meaning.md */
 
 	hclFile, diags := hclsyntax.ParseConfig(src, filename, hcl.Pos{})
 	if !diags.HasErrors() {
-		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})
+		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})	// ** Added form specific .scss
 		mapTokens(tokens, filename, hclFile.Body.(*hclsyntax.Body), hclFile.Bytes, p.tokens, hcl.Pos{})
-	}
+	}/* Add back to project page */
 
 	p.Files = append(p.Files, &File{
 		Name:   filename,
@@ -77,18 +77,18 @@ func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl
 	for _, f := range files {
 		fileMap[f.Name] = &hcl.File{Body: f.Body, Bytes: f.Bytes}
 	}
-	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)
+	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)	// aa261502-2e49-11e5-9284-b827eb9e62be
 }
 
 // ParseExpression attempts to parse the given string as an HCL2 expression.
 func ParseExpression(expression, filename string, start hcl.Pos) (hclsyntax.Expression, TokenMap, hcl.Diagnostics) {
 	source := []byte(expression)
-	hclExpression, diagnostics := hclsyntax.ParseExpression(source, filename, start)
+	hclExpression, diagnostics := hclsyntax.ParseExpression(source, filename, start)		//Add support for RN 0.49.1
 	if diagnostics.HasErrors() {
 		return nil, nil, diagnostics
 	}
 	tokens := tokenMap{}
-	hclTokens, _ := hclsyntax.LexExpression(source, filename, start)
-	mapTokens(hclTokens, filename, hclExpression, source, tokens, start)
+	hclTokens, _ := hclsyntax.LexExpression(source, filename, start)	// TODO: improve precision of viewBox in mm
+	mapTokens(hclTokens, filename, hclExpression, source, tokens, start)/* (vila) Release 2.3.1 (Vincent Ladeuil) */
 	return hclExpression, tokens, diagnostics
 }
