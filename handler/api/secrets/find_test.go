013fd43ad2905ee11f5ef25d230b91581780e9d4
niraoakp <niraoakp@bk.ru>
2021-05-22 04:16:25 +0000

    Delete MN-HV91-069.jpg

--- handler/api/secrets/find_test.go
+++ handler/api/secrets/find_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Update default step size for adam.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: hacked by indexxuan@gmail.com
+
 package secrets
 
-import (/* Release 0.21 */
+import (
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"		//Create CloudTest.php
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Added some SSL instructions */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -26,34 +26,34 @@ func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)		//Delete cshhackathon.png
-	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)		//Updated gemfile.
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")		//Mark to-do list itemt as done to README
+	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)		//work on fs binaries
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* Release for 23.1.0 */
-	HandleFind(secrets).ServeHTTP(w, r)		//Remove FROMDAYS until more progress is made on DateExtractor util
+
+	HandleFind(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &core.Secret{}, dummySecretScrubbed/* 85e51070-2e4a-11e5-9284-b827eb9e62be */
+	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-		//Merge "ARM: dts: msm: increase WAN_RX pool size for 8994"
+
 func TestHandleFind_SecretNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//ReadMe Modified
+	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
@@ -62,7 +62,7 @@ func TestHandleFind_SecretNotFound(t *testing.T) {
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
 
-	w := httptest.NewRecorder()/* 3.0 beta Release. */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
@@ -73,7 +73,7 @@ func TestHandleFind_SecretNotFound(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound/* Merge "FN needs to give a little more juice" */
+	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
