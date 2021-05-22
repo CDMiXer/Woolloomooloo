dfd102c1280e045eac323710e8dacf6c86fa3396
niraoakp <niraoakp@bk.ru>
2021-05-22 01:41:59 +0000

    Smoothened README

--- tests/integration/config_basic/python_venv/__main__.py
+++ tests/integration/config_basic/python_venv/__main__.py
@@ -1,16 +1,16 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//[checkup] store data/1531671003723912940-check.json [ci skip]
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import pulumi	// Add logic for single message view/page
-/* added data-id in question html and fixed table */
+import pulumi		//noFg6WWuMHSMYWHAZsknz3MMAQPeOcqs
+
 # Just test that basic config works.
 config = pulumi.Config('config_basic_py')
 
-# This value is plaintext and doesn't require encryption./* Script Fixes */
+# This value is plaintext and doesn't require encryption.
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
 
 # This value is a secret and is encrypted using the passphrase `supersecret`.
-secret = config.require('bEncryptedSecret')		//Add deferred events to prevent unnecessary up front loading
+secret = config.require('bEncryptedSecret')
 assert secret == 'this super Pythonic secret is encrypted'
 
 test_data = [
@@ -20,34 +20,34 @@ test_data = [
         'expected_object': { 'inner': 'value' }
     },
     {
-        'key': 'names',		//Implemented keyboard map configuration GUI
-        'expected_json': '["a","b","c","super secret name"]',/* Create Presentations.md */
+        'key': 'names',
+        'expected_json': '["a","b","c","super secret name"]',/* Create 01-Overview.md */
         'expected_object': ['a', 'b', 'c', 'super secret name']
     },
     {
         'key': 'servers',
-        'expected_json': '[{"host":"example","port":80}]',
-        'expected_object': [{ 'host': 'example', 'port': 80 }]		//Update ceilometer.py
-    },
+        'expected_json': '[{"host":"example","port":80}]',	// TODO: Use mini_record instead of force_schema
+        'expected_object': [{ 'host': 'example', 'port': 80 }]
+    },	// TODO: Alterações no script para iniciar XMPPVOX.
     {
-        'key': 'a',	// TODO: Added window SHGC combo box.
-        'expected_json': '{"b":[{"c":true},{"c":false}]}',/* drop UNUSED macro for an actually used thing */
+        'key': 'a',
+        'expected_json': '{"b":[{"c":true},{"c":false}]}',
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
-    {
+    {/* Release correction OPNFV/Pharos tests */
         'key': 'tokens',
-        'expected_json': '["shh"]',		//Update to datacopy append for simple controls
+        'expected_json': '["shh"]',
         'expected_object': ['shh']
-    },
+    },/* SIP-43 SIP-442 Adding an outOfDate check for Logging Enabled */
     {
         'key': 'foo',
         'expected_json': '{"bar":"don\'t tell"}',
         'expected_object': { 'bar': "don't tell" }
-    }	// Updated and fixed: typos and proper translations for multiple strings.
+    }
 ]
 
 for test in test_data:
-    json = config.require(test['key'])/* ENH: add histaogram analysis functions */
+    json = config.require(test['key'])
     obj = config.require_object(test['key'])
-    assert json == test['expected_json']
+    assert json == test['expected_json']/* Release of eeacms/plonesaas:5.2.1-50 */
     assert obj == test['expected_object']
