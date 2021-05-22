91b1c276db39d186ebc8e335d662a036ad829745
niraoakp <niraoakp@bk.ru>
2021-05-22 19:27:28 +0000

    Update to use gif.

--- handler/api/repos/secrets/update.go
+++ handler/api/repos/secrets/update.go
@@ -9,63 +9,63 @@ package secrets
 import (
 	"encoding/json"
 	"net/http"
-		//part 2 of x
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)		//Added code to automatically scale up file limits
+)
 
 type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
 	PullRequestPush *bool   `json:"pull_request_push"`
 }
-	// TODO: hacked by alan.shaw@protocol.ai
+
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
 func HandleUpdate(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Release1.4.3 */
-		var (/* Added Release Sprint: OOD links */
-			namespace = chi.URLParam(r, "owner")	// rename file to match title
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
 		)
-/* learn-ws: commit soap-spring-cxf project */
+
 		in := new(secretUpdate)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-/* housekeeping: remove mention of sponsorship */
+
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		//Lich.cs: Opposition Tribe
-		s, err := secrets.FindName(r.Context(), repo.ID, secret)/* Release version 4.0.0 */
+
+		s, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
-			render.NotFound(w, err)/* Release: Making ready for next release iteration 6.8.0 */
+			render.NotFound(w, err)
 			return
-		}/* Release v0.12.3 (#663) */
-		//Update JVMHashJoinUtility.java
+		}
+
 		if in.Data != nil {
 			s.Data = *in.Data
 		}
 		if in.PullRequest != nil {
 			s.PullRequest = *in.PullRequest
 		}
-		if in.PullRequestPush != nil {	// Less shilling
+		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
 		}
 
 		err = s.Validate()
-		if err != nil {/* Release 1.0.1 of PPWCode.Util.AppConfigTemplate. */
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
@@ -78,5 +78,5 @@ func HandleUpdate(
 
 		s = s.Copy()
 		render.JSON(w, s, 200)
-	}		//Merge "[FAB-7132] Add couch indexes to chaincode install pkg"
+	}
 }
