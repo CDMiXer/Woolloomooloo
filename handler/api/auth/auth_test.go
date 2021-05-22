4c6bd263bee736f7c2a9880d2651df3b0bd399c3
niraoakp <niraoakp@bk.ru>
2021-05-22 03:35:06 +0000

    web-routes: basic test-suite for PathInfo

--- handler/api/auth/auth_test.go
+++ handler/api/auth/auth_test.go
@@ -1,71 +1,71 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 1.5.5 */
+
 package auth
 
 import (
 	"database/sql"
-	"io/ioutil"
+	"io/ioutil"		//chore: update lodash monorepo packages
 	"net/http"
-	"net/http/httptest"
+	"net/http/httptest"/* Release of 2.4.0 */
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/request"/* Equals, HashCode & toString() validation */
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/sirupsen/logrus"		//701a775a-2e5c-11e5-9284-b827eb9e62be
-
+	"github.com/sirupsen/logrus"
+/* correction (mauvais ref des pg) */
 	"github.com/golang/mock/gomock"
 )
 
-func init() {
-	logrus.SetOutput(ioutil.Discard)
-}
+func init() {		//Your updated config file
+	logrus.SetOutput(ioutil.Discard)		//add area of Cylider and Sphere
+}		//Fixing various warnings in Rest services
 
 func TestAuth(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Release Notes for v02-04-01 */
 	defer controller.Finish()
-
+/* escape replaced by encodeURIComponent */
 	mockUser := &core.User{
 		ID:      1,
-		Login:   "octocat",
-		Admin:   true,
+		Login:   "octocat",	// new Command features
+		Admin:   true,/* mod herobanner width */
 		Machine: true,
-		Hash:    "$2a$04$rR2VvGjM9iqAAoyLSE4IrexAlxDbIS3M5YKtj9ANs7vraki0ybYJq 197XXbZablx0RPQ8",/* Merge branch 'develop' into fix/proofreader-formatting */
+		Hash:    "$2a$04$rR2VvGjM9iqAAoyLSE4IrexAlxDbIS3M5YKtj9ANs7vraki0ybYJq 197XXbZablx0RPQ8",		//summary of our project, contributors, instructor
 	}
-		//adding better signature documentation
-	session := mock.NewMockSession(controller)
-	session.EXPECT().Get(gomock.Any()).Return(mockUser, nil)/* Merge "Add puppet-docker_registry project" */
-/* Create DESCRIP.MMS */
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/?access_token=VA.197XXbZablx0RPQ8", nil)/* fixes keyboard agent docs. Release of proscene-2.0.0-beta.1 */
+
+	session := mock.NewMockSession(controller)	// TODO: Add deprecated scheme for testing, filled in todos and added logic
+	session.EXPECT().Get(gomock.Any()).Return(mockUser, nil)
+
+	w := httptest.NewRecorder()/* Update dashboard dan laporan excel */
+	r := httptest.NewRequest("GET", "/?access_token=VA.197XXbZablx0RPQ8", nil)
 
 	HandleAuthentication(session)(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// use dummy status code to signal the next handler in
 			// the middleware chain was properly invoked.
 			w.WriteHeader(http.StatusTeapot)
-
+		//README: Add basic features list
 			// verify the user was added to the request context
-			if user, _ := request.UserFrom(r.Context()); user != mockUser {
+			if user, _ := request.UserFrom(r.Context()); user != mockUser {		//Update lmapireq
 				t.Errorf("Expect user in context")
-			}
+			}	// TODO: hacked by vyzo@hackzen.org
 		}),
 	).ServeHTTP(w, r)
 
-	if got, want := w.Code, http.StatusTeapot; got != want {/* Missing exception catch in BeanParameter. */
+	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
-/* Improved setup.py in order to upload it to pypi */
+
 func TestAuth_Guest(t *testing.T) {
-	controller := gomock.NewController(t)		//Fix fstab /boot dump option (1->0)
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	w := httptest.NewRecorder()/* Delete GCost.java */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-		//rev 497456
+
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(gomock.Any()).Return(nil, sql.ErrNoRows)
 
@@ -80,9 +80,9 @@ func TestAuth_Guest(t *testing.T) {
 				t.Errorf("Expect guest mode, no user in context")
 			}
 		}),
-	).ServeHTTP(w, r)		//Removing the email adresses of APE Founders. (AUTHORS)
-		//Fix for MT03739 backgamn: Access Violation after OK
-	if got, want := w.Code, http.StatusTeapot; got != want {/* [MJAVACC-71] JTB mojo fails to move output files to proper directory on Windows */
+	).ServeHTTP(w, r)
+
+	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
