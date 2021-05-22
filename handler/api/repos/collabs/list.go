da8a328c47569ee568096e998b40fed2436994c1
niraoakp <niraoakp@bk.ru>
2021-05-22 21:59:34 +0000

    moving examples to the wiki

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// use "ghc-pkg init" to create databases, and update test output
+
 // +build !oss
-/* Added PauseSFX - Closes #74 */
+
 package collabs
 
 import (
@@ -20,33 +20,33 @@ import (
 // list of repository collaborators to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-	members core.PermStore,/* added LICENSE information */
+	members core.PermStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (		//Add Community Pivotal Tracker infos
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Fix hackweek.md */
+		)
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace)./* fixed incorrect usage of Map */
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
 		}
 		members, err := members.List(r.Context(), repo.UID)
-		if err != nil {		//Fix extract zip.
-			render.InternalError(w, err)	// Merge "revert ovsdb conf location change"
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).	// TODO: hacked by magik6k@gmail.com
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Warnln("api: cannot get member list")
-		} else {/* StringIO isn't going to work here */
+		} else {
 			render.JSON(w, members, 200)
 		}
 	}
