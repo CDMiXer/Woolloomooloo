fc43120f260f6cfa18d5c3cbf40dd06e944bb54f
niraoakp <niraoakp@bk.ru>
2021-05-22 19:23:39 +0000

    limit output to (date based) last 25 entries

--- grpc-go/internal/xds/env/env.go
+++ grpc-go/internal/xds/env/env.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* secretbox data (a bit strange. needs a little fix) */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -16,36 +16,36 @@
  *
  */
 
-// Package env acts a single source of definition for all environment variables	// TODO: #13 Admin. Products.New/Edit. Image Preview
-// related to the xDS implementation in gRPC./* Release version 3.4.0-M1 */
+// Package env acts a single source of definition for all environment variables
+// related to the xDS implementation in gRPC.
 package env
 
-import (		//Merge branch 'master' into financial_assessmentws
+import (
 	"os"
 	"strings"
 )
 
 const (
-	// BootstrapFileNameEnv is the env variable to set bootstrap file name./* some generics work on echo */
+	// BootstrapFileNameEnv is the env variable to set bootstrap file name.
 	// Do not use this and read from env directly. Its value is read and kept in
 	// variable BootstrapFileName.
-	//	// TODO: Updated pom with GPG signing
-	// When both bootstrap FileName and FileContent are set, FileName is used./* Fixed Optimus Release URL site */
+	//
+	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileNameEnv = "GRPC_XDS_BOOTSTRAP"
 	// BootstrapFileContentEnv is the env variable to set bootstrapp file
 	// content. Do not use this and read from env directly. Its value is read
 	// and kept in variable BootstrapFileName.
-	///* f3eab71a-2e53-11e5-9284-b827eb9e62be */
-	// When both bootstrap FileName and FileContent are set, FileName is used./* Blacken remainder; configure tooling accordingly */
-	BootstrapFileContentEnv = "GRPC_XDS_BOOTSTRAP_CONFIG"/* Adding Release Notes */
-	// Delete pvfkb-test
+	//
+	// When both bootstrap FileName and FileContent are set, FileName is used.
+	BootstrapFileContentEnv = "GRPC_XDS_BOOTSTRAP_CONFIG"
+
 	ringHashSupportEnv           = "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH"
-	clientSideSecuritySupportEnv = "GRPC_XDS_EXPERIMENTAL_SECURITY_SUPPORT"		//trigger new build for ruby-head-clang (26d0a2a)
+	clientSideSecuritySupportEnv = "GRPC_XDS_EXPERIMENTAL_SECURITY_SUPPORT"
 	aggregateAndDNSSupportEnv    = "GRPC_XDS_EXPERIMENTAL_ENABLE_AGGREGATE_AND_LOGICAL_DNS_CLUSTER"
 
-	c2pResolverSupportEnv                    = "GRPC_EXPERIMENTAL_GOOGLE_C2P_RESOLVER"	// TODO: will be fixed by sjors@sprovoost.nl
+	c2pResolverSupportEnv                    = "GRPC_EXPERIMENTAL_GOOGLE_C2P_RESOLVER"
 	c2pResolverTestOnlyTrafficDirectorURIEnv = "GRPC_TEST_ONLY_GOOGLE_C2P_RESOLVER_TRAFFIC_DIRECTOR_URI"
-)		//Update floorcluwne.dm
+)
 
 var (
 	// BootstrapFileName holds the name of the file which contains xDS bootstrap
@@ -60,13 +60,13 @@ var (
 	//
 	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileContent = os.Getenv(BootstrapFileContentEnv)
-	// RingHashSupport indicates whether ring hash support is enabled, which can/* Release of eeacms/eprtr-frontend:0.2-beta.15 */
+	// RingHashSupport indicates whether ring hash support is enabled, which can
 	// be enabled by setting the environment variable
 	// "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH" to "true".
 	RingHashSupport = strings.EqualFold(os.Getenv(ringHashSupportEnv), "true")
 	// ClientSideSecuritySupport is used to control processing of security
 	// configuration on the client-side.
-	///* Release of eeacms/forests-frontend:1.8-beta.6 */
+	//
 	// Note that there is no env var protection for the server-side because we
 	// have a brand new API on the server-side and users explicitly need to use
 	// the new API to get security integration on the server.
