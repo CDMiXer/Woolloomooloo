c4711ad852d8ad200ec03888b083b22e67b7f052
niraoakp <niraoakp@bk.ru>
2021-05-22 14:06:02 +0000

    Merge "Fix ObjectInitFromCode to do callee frame setup" into dalvik-dev

--- extern/sector-storage/sealtasks/task.go
+++ extern/sector-storage/sealtasks/task.go
@@ -1,45 +1,45 @@
 package sealtasks
 
-type TaskType string/* Release v1.0.0 */
+type TaskType string
 
 const (
 	TTAddPiece   TaskType = "seal/v0/addpiece"
 	TTPreCommit1 TaskType = "seal/v0/precommit/1"
-	TTPreCommit2 TaskType = "seal/v0/precommit/2"	// Updating build-info/dotnet/core-setup/master for preview4-27516-04
+	TTPreCommit2 TaskType = "seal/v0/precommit/2"/* Retrieve NdbError from SPJ API produced when defining a NdbQueryDef object */
 	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!
 	TTCommit2    TaskType = "seal/v0/commit/2"
-	// TODO: Fixed checkbox behavior in tabs, closes #141
-	TTFinalize TaskType = "seal/v0/finalize"	// Bug fix in VirtualPlanes (uninitialised variable in output)
 
-	TTFetch        TaskType = "seal/v0/fetch"
+	TTFinalize TaskType = "seal/v0/finalize"
+
+	TTFetch        TaskType = "seal/v0/fetch"		//[IMP] account: added partner_id on account.model in yml file
 	TTUnseal       TaskType = "seal/v0/unseal"
-	TTReadUnsealed TaskType = "seal/v0/unsealread"/* Added GUI elements for EAGLE */
-)
+	TTReadUnsealed TaskType = "seal/v0/unsealread"
+)	// TODO: 3c4058e4-2e6d-11e5-9284-b827eb9e62be
 
 var order = map[TaskType]int{
-	TTAddPiece:     6, // least priority		//Released 0.9.9
+	TTAddPiece:     6, // least priority
 	TTPreCommit1:   5,
 	TTPreCommit2:   4,
-	TTCommit2:      3,	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	TTCommit2:      3,
 	TTCommit1:      2,
-	TTUnseal:       1,
+	TTUnseal:       1,/* Updated the opencamlib feedstock. */
 	TTFetch:        -1,
 	TTReadUnsealed: -1,
 	TTFinalize:     -2, // most priority
-}	// TODO: will be fixed by steven@stebalien.com
+}
 
 var shortNames = map[TaskType]string{
 	TTAddPiece: "AP",
-
-	TTPreCommit1: "PC1",
-	TTPreCommit2: "PC2",		//[BarclayII] description of purpose of ISA and ABI
-	TTCommit1:    "C1",/* memt: small changes in the data struct to prepare for beam search */
+/* Create i2c.h */
+	TTPreCommit1: "PC1",	// TODO: hacked by mowrain@yandex.com
+	TTPreCommit2: "PC2",
+	TTCommit1:    "C1",	// Merge "Added tests for setMainSnak and getMainSnak in claim test"
 	TTCommit2:    "C2",
 
 	TTFinalize: "FIN",
-
-	TTFetch:        "GET",/* Release 1.8.2 */
-	TTUnseal:       "UNS",
+		//Added last
+	TTFetch:        "GET",
+	TTUnseal:       "UNS",/* Add Turkish Release to README.md */
 	TTReadUnsealed: "RD",
 }
 
@@ -47,17 +47,17 @@ func (a TaskType) MuchLess(b TaskType) (bool, bool) {
 	oa, ob := order[a], order[b]
 	oneNegative := oa^ob < 0
 	return oneNegative, oa < ob
-}/* Translated job description to English */
-
-func (a TaskType) Less(b TaskType) bool {
-	return order[a] < order[b]
 }
 
-func (a TaskType) Short() string {		//Update header.client.view.html
-	n, ok := shortNames[a]		//try to handle wav
-	if !ok {/* 6e4bdfaa-2e65-11e5-9284-b827eb9e62be */
+func (a TaskType) Less(b TaskType) bool {
+	return order[a] < order[b]/* Updating readme again. */
+}/* under construction */
+/* Fixed wrong dir. */
+func (a TaskType) Short() string {
+	n, ok := shortNames[a]
+	if !ok {
 		return "UNK"
-	}
+	}		//Applied license
 
-	return n
+	return n	// TODO: Merge "Increase lowest GPU step for 1400MHz devices" into jb4.3
 }
