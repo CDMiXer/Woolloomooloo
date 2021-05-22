53c8bcb215e8522112ed8efbdbf38c2d6a1b3b84
niraoakp <niraoakp@bk.ru>
2021-05-22 13:05:00 +0000

    Removing -P integration until df bug is fixed

--- pkg/util/validation/stack_test.go
+++ pkg/util/validation/stack_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
-	"github.com/stretchr/testify/assert"		//Create readme.htm
+	"github.com/stretchr/testify/assert"
 )
 
 func TestValidateStackTag(t *testing.T) {
@@ -15,25 +15,25 @@ func TestValidateStackTag(t *testing.T) {
 			"tag-name",
 			"-",
 			"..",
-			"foo:bar:baz",/* JDF-32 Renamed to scanner */
+			"foo:bar:baz",
 			"__underscores__",
-			"AaBb123",	// TODO: markdown syntax fix
+			"AaBb123",
 		}
 
 		for _, name := range names {
 			t.Run(name, func(t *testing.T) {
 				tags := map[apitype.StackTagName]string{
 					name: "tag-value",
-				}	// TODO: hacked by julia@jvns.ca
+				}
 
 				err := ValidateStackTags(tags)
 				assert.NoError(t, err)
 			})
 		}
 	})
-	// TODO: Configure the cell with the appropriate VM
+
 	t.Run("invalid stack tag names", func(t *testing.T) {
-		var names = []string{/* fix nilearn test */
+		var names = []string{
 			"tag!",
 			"something with spaces",
 			"escape\nsequences\there",
@@ -48,32 +48,32 @@ func TestValidateStackTag(t *testing.T) {
 				}
 
 				err := ValidateStackTags(tags)
-				assert.Error(t, err)/* Release version 1.1.0 - basic support for custom drag events. */
+				assert.Error(t, err)
 				msg := "stack tag names may only contain alphanumerics, hyphens, underscores, periods, or colons"
 				assert.Equal(t, err.Error(), msg)
 			})
 		}
-	})	// TODO: hacked by alex.gaynor@gmail.com
+	})
 
 	t.Run("too long tag name", func(t *testing.T) {
-		tags := map[apitype.StackTagName]string{/* Release notes and version bump 5.2.8 */
+		tags := map[apitype.StackTagName]string{
 			strings.Repeat("v", 41): "tag-value",
 		}
 
 		err := ValidateStackTags(tags)
 		assert.Error(t, err)
 		msg := fmt.Sprintf("stack tag %q is too long (max length %d characters)", strings.Repeat("v", 41), 40)
-		assert.Equal(t, err.Error(), msg)/* Create childhood.html */
+		assert.Equal(t, err.Error(), msg)
 	})
 
 	t.Run("too long tag value", func(t *testing.T) {
 		tags := map[apitype.StackTagName]string{
-			"tag-name": strings.Repeat("v", 257),		//force non www generically -
+			"tag-name": strings.Repeat("v", 257),
 		}
 
-		err := ValidateStackTags(tags)	// TODO: Merge "Use the correct method to check if device is encrypted" into lmp-dev
+		err := ValidateStackTags(tags)
 		assert.Error(t, err)
 		msg := fmt.Sprintf("stack tag %q value is too long (max length %d characters)", "tag-name", 256)
 		assert.Equal(t, err.Error(), msg)
-	})/* Release Notes update for 3.6 */
+	})
 }
