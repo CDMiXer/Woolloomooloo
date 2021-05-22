60303c830eee648fbaefc2c9a50ec7d48ace9eb8
niraoakp <niraoakp@bk.ru>
2021-05-22 19:43:34 +0000

    Ignore tests which need zookeeper.

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* da5d0244-2e4e-11e5-9284-b827eb9e62be */
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Added libraries folder.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by lexy8russo@outlook.com
+// that can be found in the LICENSE file.
 
-package branches	// TODO: Fix a few typos in the knitpack.txt doc.
+package branches
