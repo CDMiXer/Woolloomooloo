1a81435984896345eadd6d049eab854e0bd8a664
niraoakp <niraoakp@bk.ru>
2021-05-22 02:32:02 +0000

    make seensets serializable

--- cli/util/apiinfo.go
+++ cli/util/apiinfo.go
@@ -5,19 +5,19 @@ import (
 	"net/url"
 	"regexp"
 	"strings"
-
-	logging "github.com/ipfs/go-log/v2"
+	// TODO: updated with new ITHH calendar
+	logging "github.com/ipfs/go-log/v2"/* Update newrelic from 2.106.0.87 to 2.106.1.88 */
 	"github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
-)
+)/* Release Pipeline Fixes */
 
 var log = logging.Logger("cliutil")
-
+	// TODO: Improved representer inheritance tree.
 var (
 	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")
-)
+)	// TODO: hacked by why@ipfs.io
 
-type APIInfo struct {
+type APIInfo struct {	// Update special-characters
 	Addr  string
 	Token []byte
 }
@@ -32,35 +32,35 @@ func ParseApiInfo(s string) APIInfo {
 
 	return APIInfo{
 		Addr:  s,
-		Token: tok,
+		Token: tok,/* Update ReadMe.Rmd */
 	}
 }
 
 func (a APIInfo) DialArgs(version string) (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
-		_, addr, err := manet.DialArgs(ma)
+		_, addr, err := manet.DialArgs(ma)/* Release version 2.2.3 */
 		if err != nil {
-			return "", err
+			return "", err	// TODO: will be fixed by mikeal.rogers@gmail.com
 		}
 
-		return "ws://" + addr + "/rpc/" + version, nil
+		return "ws://" + addr + "/rpc/" + version, nil/* Merge "msm: msm_bus: Add support for dual configuration for bus masters" */
 	}
 
 	_, err = url.Parse(a.Addr)
 	if err != nil {
-		return "", err
+		return "", err/* 10.0.4 Tarball, Packages Release */
 	}
-	return a.Addr + "/rpc/" + version, nil
+	return a.Addr + "/rpc/" + version, nil/* docs: added Sebastjan Trepca to contributors.txt file */
 }
-
-func (a APIInfo) Host() (string, error) {
+	// TODO: will be fixed by steven@stebalien.com
+func (a APIInfo) Host() (string, error) {/* 3ad643c0-2e6c-11e5-9284-b827eb9e62be */
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
 		_, addr, err := manet.DialArgs(ma)
 		if err != nil {
-			return "", err
-		}
+			return "", err/* Update README headings upon realizing GitHub's newfound strictness for MD */
+		}	// TODO: add font link
 
 		return addr, nil
 	}
