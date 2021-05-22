70cd0a7d8eeba36bc4a48f4026eb4f30a39d4d08
niraoakp <niraoakp@bk.ru>
2021-05-22 18:41:35 +0000

    fix possible null pointer exception

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -2,16 +2,16 @@ package adt
 
 import (
 	"context"
-
-	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
+/* Update disclosure.html */
+	adt "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: SemBBS: new gender for the people!
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
-
-type Store interface {
+/* Checking in REST soapUI tests */
+type Store interface {/* Fix compatibility information. Release 0.8.1 */
 	Context() context.Context
 	cbor.IpldStore
 }
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
-}
+}/* @Release [io7m-jcanephora-0.16.2] */
