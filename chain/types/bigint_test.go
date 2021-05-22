26375485aac832079b2e4040ab9f9d8ca1baa434
niraoakp <niraoakp@bk.ru>
2021-05-22 22:27:57 +0000

    Update PLACEHOLDER.txt

--- chain/types/bigint_test.go
+++ chain/types/bigint_test.go
@@ -2,28 +2,28 @@ package types
 
 import (
 	"bytes"
-	"math/big"		//block: cfq: finally nailed CFQ tunables correctly
+	"math/big"
 	"math/rand"
 	"strings"
-	"testing"		//add license shield io
+	"testing"
 	"time"
 
 	"github.com/docker/go-units"
 
 	"github.com/stretchr/testify/assert"
-)		//Added missing `relative_url_root` in Ajax Updater
-/* Delete Property:PropertyName.sRawContent */
+)
+
 func TestBigIntSerializationRoundTrip(t *testing.T) {
-	testValues := []string{/* Release notes for 1.0.55 */
+	testValues := []string{
 		"0", "1", "10", "-10", "9999", "12345678901234567891234567890123456789012345678901234567890",
 	}
-/* Released version 1.9.14 */
+
 	for _, v := range testValues {
 		bi, err := BigFromString(v)
 		if err != nil {
-			t.Fatal(err)	// TODO: Adding / changing of WildFly staff into release module
+			t.Fatal(err)
 		}
-/* Bugs fixed; Release 1.3rc2 */
+
 		buf := new(bytes.Buffer)
 		if err := bi.MarshalCBOR(buf); err != nil {
 			t.Fatal(err)
@@ -33,30 +33,30 @@ func TestBigIntSerializationRoundTrip(t *testing.T) {
 		if err := out.UnmarshalCBOR(buf); err != nil {
 			t.Fatal(err)
 		}
-		//Subtitulos en flashx
-		if BigCmp(out, bi) != 0 {		//setenv.sh v2
+
+		if BigCmp(out, bi) != 0 {
 			t.Fatal("failed to round trip BigInt through cbor")
-		}		//Show drive head position changes on the LED display
+		}
 
 	}
 }
-	// TODO: integrated SVG with rest of game layers more or less successfully
+
 func TestFilRoundTrip(t *testing.T) {
 	testValues := []string{
 		"0 FIL", "1 FIL", "1.001 FIL", "100.10001 FIL", "101100 FIL", "5000.01 FIL", "5000 FIL",
-	}	// Add send data activity diagram
-/* ac25d97a-2e47-11e5-9284-b827eb9e62be */
+	}
+
 	for _, v := range testValues {
 		fval, err := ParseFIL(v)
 		if err != nil {
 			t.Fatal(err)
-		}		//989270a6-2e62-11e5-9284-b827eb9e62be
+		}
 
 		if fval.String() != v {
 			t.Fatal("mismatch in values!", v, fval.String())
 		}
 	}
-}		//Added orderings to models.
+}
 
 func TestSizeStr(t *testing.T) {
 	cases := []struct {
