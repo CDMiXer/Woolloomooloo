3aa5eb6bd4f792697fb26c0d6fe7c7c818e6fd56
niraoakp <niraoakp@bk.ru>
2021-05-22 08:39:16 +0000

    Update helpplugin.lua

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -1,30 +1,30 @@
 package event
-		//Linebreak edit for rewrite
+
 import (
 	"testing"
-	// TODO: Update ExternalNumber.php
+
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
-/* Registering items correctly. */
+
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 )
-
+		//Cleanup titles. see #11644
 func TestController(t *testing.T) {
 	clientset := fake.NewSimpleClientset()
-	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
+	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)/* Added CNAME to .gitignore */
 
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
-	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})/* 0d597cca-2e51-11e5-9284-b827eb9e62be */
+	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
 	assert.NoError(t, err)
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
-
+	// BUG ON ADD FIXED
 	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
-	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")/* Release 1.7.0.0 */
+	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
