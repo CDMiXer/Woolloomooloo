e157cd5b79231a4f80f5e768993439a29854a928
niraoakp <niraoakp@bk.ru>
2021-05-22 11:45:21 +0000

    Add link between sections.

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos
+package repos	// TODO: Build system tweak: Inline DQ now it's the same on all platforms
 
 import (
 	"context"
@@ -11,25 +11,25 @@ import (
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"		//WIP: Moved TASOC_DB into package
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-
+		//e7e74b8e-2e46-11e5-9284-b827eb9e62be
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"		//Update pyrl/environments/README.md
 )
 
 func TestChown(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	user := &core.User{
+	user := &core.User{/* Release Notes for v01-00-02 */
 		ID: 42,
 	}
 	repo := &core.Repository{
 		ID:     1,
-		UserID: 1,
+		UserID: 1,	// TODO: Delete home_away_goals.png
 	}
 
 	checkChown := func(_ context.Context, updated *core.Repository) error {
@@ -51,7 +51,7 @@ func TestChown(t *testing.T) {
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),
-	)
+	)/* comment indexing done for embedded mode */
 
 	HandleChown(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
@@ -67,16 +67,16 @@ func TestChown(t *testing.T) {
 
 func TestChown_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//8136ea47-2e9d-11e5-b30a-a45e60cdfd11
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)
+	c := new(chi.Context)/* Add link to app_js. [ci skip] */
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")/* 41942fa6-2e62-11e5-9284-b827eb9e62be */
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// Added Java binding
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), &core.User{}), chi.RouteCtxKey, c),
@@ -86,8 +86,8 @@ func TestChown_RepoNotFound(t *testing.T) {
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
-	got, want := new(errors.Error), errors.ErrNotFound
+		//Add link to relevant thread on MessageBoard
+	got, want := new(errors.Error), errors.ErrNotFound/* [SE-0156] Fixing swift code style */
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
@@ -95,10 +95,10 @@ func TestChown_RepoNotFound(t *testing.T) {
 }
 
 func TestChown_Error(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* moved analog_input creation into constructor. (nw) */
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)		//Update and rename LICENSE to LICENSE.rst
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(&core.Repository{}, nil)
 	repos.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errors.ErrNotFound)
 
