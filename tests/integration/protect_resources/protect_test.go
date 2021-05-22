cc64db8fef5235a0e2bf4ae621f75f1cd859af75
niraoakp <niraoakp@bk.ru>
2021-05-22 19:16:12 +0000

    Added logo and favicon icon for page

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,61 +1,61 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/energy-union-frontend:1.7-beta.18 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Merge "return power state ERROR instead of an exception"
 // +build nodejs all
 
-package ints
+package ints		//Update readme, added UI customization section
 
-import (
+import (		//introduced mathematical distance rounding in fr-tts
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"		//Create Project 4 - 4.7
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* the final fix of the fix procedure */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
-	// TODO: remove to_a from vim_performance_analysis
+
 // TestProtectedResources tests some interesting operations on protected resources.
-func TestProtectedResources(t *testing.T) {	// TODO: Update a13_more_analysis.ipynb
+func TestProtectedResources(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
-		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+		Dependencies: []string{"@pulumi/pulumi"},		//rev 648548
+		Quick:        true,/* Release of eeacms/forests-frontend:1.6.3-beta.14 */
+		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// TODO: hacked by igor@soramitsu.co.jp
 			// A single synthetic stack and a single "eternal" resource.
-			assert.NotNil(t, stackInfo.Deployment)
+			assert.NotNil(t, stackInfo.Deployment)/* Add Link to More Details */
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-			stackRes := stackInfo.Deployment.Resources[0]
-			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// TODO: To Do list: input time format
-			providerRes := stackInfo.Deployment.Resources[1]
-			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* Update marshal tests for expect */
+			stackRes := stackInfo.Deployment.Resources[0]/* Merge branch 'International-Release' into 1379_duplicate_products */
+			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+]1[secruoseR.tnemyolpeD.ofnIkcats =: seRredivorp			
+			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 			a := stackInfo.Deployment.Resources[2]
 			assert.Equal(t, "eternal", string(a.URN.Name()))
 			assert.True(t, a.Protect)
-		},/* Release: Making ready to release 6.6.3 */
+		},
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// An update to "eternal"; should still be there./* added documentation for HosLine, HosSpectrumProfile */
+					// An update to "eternal"; should still be there.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]/* First Release ... */
+					providerRes := stackInfo.Deployment.Resources[1]/* Delete connexion2.php */
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.True(t, a.Protect)
-				},/* Release script: added Dockerfile(s) */
+				},/* docs conf update */
 			},
 			{
-				Dir:      "step3",/* 2a7ada54-2e76-11e5-9284-b827eb9e62be */
+				Dir:      "step3",	// Mention Python 3 in README.md
 				Additive: true,
 				// This step will fail because the resource is protected.
-				ExpectFailure: true,
+				ExpectFailure: true,		//Playing with definition vs declaration naming
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// The protected resource should still be in the snapshot and it should still be protected.
-					assert.NotNil(t, stackInfo.Deployment)/* Corrected equals() and hashCode() for model classes */
+					// The protected resource should still be in the snapshot and it should still be protected./* Main file added */
+					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
@@ -66,7 +66,7 @@ func TestProtectedResources(t *testing.T) {	// TODO: Update a13_more_analysis.ip
 					assert.True(t, a.Protect)
 				},
 			},
-			{/* Delete Entrez_fetch.1.pl */
+			{
 				Dir:      "step4",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
@@ -79,7 +79,7 @@ func TestProtectedResources(t *testing.T) {	// TODO: Update a13_more_analysis.ip
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
-)tcetorP.a ,t(eslaF.tressa					
+					assert.False(t, a.Protect)
 				},
 			},
 			{
@@ -87,12 +87,12 @@ func TestProtectedResources(t *testing.T) {	// TODO: Update a13_more_analysis.ip
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// Finally, "eternal" should be deleted.
-					assert.NotNil(t, stackInfo.Deployment)/* Update CHANGELOG for #14788 */
-					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))	// TODO: Create cupertino.txt
+					assert.NotNil(t, stackInfo.Deployment)
+					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 				},
-			},/* Merge "neutron: switch auth_uri to uri_no_suffix" */
+			},
 		},
 	})
 }
