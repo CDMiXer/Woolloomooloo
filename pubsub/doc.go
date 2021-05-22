d58d04be75c00a6d8882e0395163fe14b2a5d0d1
niraoakp <niraoakp@bk.ru>
2021-05-22 03:53:21 +0000

    wagon-ssh 2.10 -> 3.3.0.

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by hugomrdias@gmail.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update instructions and add loading animation
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Now the RequestQueue uses TChan instead of Chan. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into email/manager_invite */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Merge "Add Compare service to fetch compare data"
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // Package pubsub implements a thread safe publish subscriber,
