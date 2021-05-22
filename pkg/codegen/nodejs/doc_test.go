3a9d04eb702fc0d7f8c71359ede07db4c309a44f
niraoakp <niraoakp@bk.ru>
2021-05-22 22:38:44 +0000

    Simple event scheduler first draft

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,74 +1,74 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: update lesson_i for not enrolled courses
-// Licensed under the Apache License, Version 2.0 (the "License");/* PopupMenu close on mouseReleased (last change) */
-// you may not use this file except in compliance with the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Delete GP_Content_Seg_Input_File_092115_Full_Data_weights.csv */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by alex.gaynor@gmail.com
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Add a key benefits section in Release Notes" */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the/* [IMP] website snippet option: rename data-class into data-value */
+		//Rename compile-env.md to env.md
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
 // nolint: lll, goconst
-package nodejs	// [doc] small changes
-	// TODO: Timers now play sound regardless if they're visible
+package nodejs
+
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"/* Delete index452.html */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Merge branch '85' into 85
+
 var testPackageSpec = schema.PackageSpec{
 	Name:        "aws",
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
-	},	// TODO: hacked by ng8eke@163.com
-	Types: map[string]schema.ComplexTypeSpec{
-		"aws:s3/BucketCorsRule:BucketCorsRule": {/* Update CensoController.php */
+	},
+	Types: map[string]schema.ComplexTypeSpec{/* GameState.released(key) & Press/Released constants */
+		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
 						Description: "A string prop.",
-						TypeSpec: schema.TypeSpec{	// streszczenie
+						TypeSpec: schema.TypeSpec{	// TODO: hacked by souzau@yandex.com
 							Type: "string",
-						},
+						},	// Added correct user ID
 					},
-				},		//added note to build due to CI problems
+				},
 			},
 		},
 	},
 	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
-			InputProperties: map[string]schema.PropertySpec{
+			InputProperties: map[string]schema.PropertySpec{/* Trying remove .text */
 				"corsRules": {
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
 					},
 				},
 			},
-		},
+		},/* Update and rename 0000-00-00-Home.markdown to 0001-01-01-Home.markdown */
 	},
 }
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
-
+/* Fixed loading inventory of unavailable tech. Release 0.95.186 */
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
-	return pkg		//Added the shell meteor command
+	return pkg
 }
 
-func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
+func TestDocLinkGenerationForPulumiTypes(t *testing.T) {/* Release notes for 0.43 are no longer preliminary */
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
@@ -81,20 +81,20 @@ func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")
 		assert.Equal(t, expected, link)
 	})
-}
-/* Release of eeacms/www-devel:20.1.11 */
-func TestGetDocLinkForResourceType(t *testing.T) {
+}		//Improved formatting in readme.
+
+func TestGetDocLinkForResourceType(t *testing.T) {/* update to secure version of jackson-databind */
 	pkg := getTestPackage(t)
-	d := DocLanguageHelper{}/* Release version 0.0.10. */
+	d := DocLanguageHelper{}	// Adding space between hashtag and text
 	expected := "/docs/reference/pkg/nodejs/pulumi/aws/s3/#Bucket"
-	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")
+	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")	// TODO: will be fixed by sjors@sprovoost.nl
 	assert.Equal(t, expected, link)
 }
 
 func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
-	pkg := getTestPackage(t)
+	pkg := getTestPackage(t)		//Update lang-model-dataset.md
 	d := DocLanguageHelper{}
-	expected := "/docs/reference/pkg/nodejs/pulumi/aws/types/input/#BucketCorsRule"		//Update Preset.h
-	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "s3", "BucketCorsRule", true)		//8d0e2172-2e61-11e5-9284-b827eb9e62be
+	expected := "/docs/reference/pkg/nodejs/pulumi/aws/types/input/#BucketCorsRule"
+	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "s3", "BucketCorsRule", true)
 	assert.Equal(t, expected, link)
 }
