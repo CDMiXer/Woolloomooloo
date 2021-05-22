a07d2dea0764bacd7c748bf56d561c8b7c15ed88
niraoakp <niraoakp@bk.ru>
2021-05-22 18:32:54 +0000

    Several build/documentation related fixes.

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -3,13 +3,13 @@
 // that can be found in the LICENSE file.
 
 package manager
-/* mk: extbld: dont use download --continue */
+
 import (
-	"io/ioutil"	// TODO: Rename Motorola Device Manager.txt to Motorola Device Manager.MD
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)		//Merge "Add voting docs jobs to kuryr-tempest-plugin"
-}/* Release LastaFlute-0.7.4 */
+	logrus.SetOutput(ioutil.Discard)
+}
