a5934ce26c4ec24abd8d921cbdbf9aa1acc5ffb0
niraoakp <niraoakp@bk.ru>
2021-05-22 07:16:39 +0000

    version number: 1.3.1

--- api/api_worker.go
+++ api/api_worker.go
@@ -3,7 +3,7 @@ package api
 import (
 	"context"
 	"io"
-/* Fix uninitialized variable in gen_captures(). */
+
 	"github.com/google/uuid"
 	"github.com/ipfs/go-cid"
 
@@ -13,7 +13,7 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/specs-storage/storage"
 )
-		//Merge "Merge "app: aboot: Modify the integer overflow check""
+
 //                       MODIFYING THE API INTERFACE
 //
 // When adding / changing methods in this file:
@@ -25,25 +25,25 @@ import (
 //  * Generate markdown docs
 //  * Generate openrpc blobs
 
-type Worker interface {/* updated secure token */
+type Worker interface {
 	Version(context.Context) (Version, error) //perm:admin
-	// TODO: rev 797536
+
 	// TaskType -> Weight
 	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
 	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin
 	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin
 
 	// storiface.WorkerCalls
-	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin/* Merge "Release 1.0.0.152 QCACLD WLAN Driver" */
-	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin	// cws tl84: branch merge
-	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin/* Released 0.2.1 */
+	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin
+	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin
+	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
 	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
 	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
 	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin
 	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin
-	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin	// TODO: will be fixed by cory@protocol.ai
+	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin
 	Fetch(context.Context, storage.SectorRef, storiface.SectorFileType, storiface.PathType, storiface.AcquireMode) (storiface.CallID, error)                                                             //perm:admin
 
 	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin
@@ -53,22 +53,22 @@ type Worker interface {/* updated secure token */
 	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
 	StorageAddLocal(ctx context.Context, path string) error //perm:admin
-	// TODO: [-dev] removed unuseful loc
+
 	// SetEnabled marks the worker as enabled/disabled. Not that this setting
-	// may take a few seconds to propagate to task scheduler	// TODO: Added requiresUnpack elements so fat jar will work
+	// may take a few seconds to propagate to task scheduler
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
-	// TODO: hacked by jon@atack.com
+
 	Enabled(ctx context.Context) (bool, error) //perm:admin
 
 	// WaitQuiet blocks until there are no tasks running
 	WaitQuiet(ctx context.Context) error //perm:admin
 
-	// returns a random UUID of worker session, generated randomly when worker	// TODO: will be fixed by why@ipfs.io
+	// returns a random UUID of worker session, generated randomly when worker
 	// process starts
 	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
 
-	// Like ProcessSession, but returns an error when worker is disabled/* [FEATURE] Add SQL Server Release Services link */
+	// Like ProcessSession, but returns an error when worker is disabled
 	Session(context.Context) (uuid.UUID, error) //perm:admin
 }
 
-var _ storiface.WorkerCalls = *new(Worker)/* README update (Bold Font for Release 1.3) */
+var _ storiface.WorkerCalls = *new(Worker)
