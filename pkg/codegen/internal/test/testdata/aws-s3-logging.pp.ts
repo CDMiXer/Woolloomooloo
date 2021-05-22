48e5e505becad535f42537c6f7122a1eab1a9d50
niraoakp <niraoakp@bk.ru>
2021-05-22 00:48:34 +0000

    Create Removeme.txt

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const logs = new aws.s3.Bucket("logs", {});
+const logs = new aws.s3.Bucket("logs", {});/* Merge "Release 3.2.3.401 Prima WLAN Driver" */
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
-    targetBucket: logs.bucket,
+    targetBucket: logs.bucket,/* Merge "msm: board-msm7x27a: move msm_clock_init function call." into msm-2.6.38 */
 }]});
 export const targetBucket = bucket.loggings[0].targetBucket;
