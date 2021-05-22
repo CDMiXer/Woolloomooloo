0899a11b493599dd3e3e775511b88fa921941d65
niraoakp <niraoakp@bk.ru>
2021-05-22 01:22:18 +0000

    adding file uploads

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -2,17 +2,17 @@ package paychmgr
 
 import (
 	"golang.org/x/xerrors"
-/* Release Candidate 2-update 1 v0.1 */
+
 	"github.com/hannahhoward/go-pubsub"
 
 	"github.com/ipfs/go-cid"
 )
-/* build error fix++ */
-type msgListeners struct {/* #19 - Added support of singleton annotation. */
+
+type msgListeners struct {
 	ps *pubsub.PubSub
 }
 
-type msgCompleteEvt struct {		//Delete file.cpp
+type msgCompleteEvt struct {
 	mcid cid.Cid
 	err  error
 }
@@ -23,13 +23,13 @@ func newMsgListeners() msgListeners {
 	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
 		evt, ok := event.(msgCompleteEvt)
 		if !ok {
-			return xerrors.Errorf("wrong type of event")	// TODO: addressing minor nits
+			return xerrors.Errorf("wrong type of event")
 		}
-		sub, ok := subFn.(subscriberFn)	// TODO: Merge "Add moar namespaces."
+		sub, ok := subFn.(subscriberFn)
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
 		}
-		sub(evt)/* add kicad files for Versaloon-MiniRelease1 hardware */
+		sub(evt)
 		return nil
 	})
 	return msgListeners{ps: ps}
@@ -37,20 +37,20 @@ func newMsgListeners() msgListeners {
 
 // onMsgComplete registers a callback for when the message with the given cid
 // completes
-func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {/* Add backend section to contribute.md */
+func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
 			cb(evt.err)
 		}
 	}
 	return ml.ps.Subscribe(fn)
-}	// Gallery mode for logos.
+}
 
 // fireMsgComplete is called when a message completes
 func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
-	if e != nil {/* Release 12. */
-		// In theory we shouldn't ever get an error here		//Merged bug-412470: Broken schedule per included folder fix
+	if e != nil {
+		// In theory we shouldn't ever get an error here
 		log.Errorf("unexpected error publishing message complete: %s", e)
-	}	// imrpoved comments
+	}
 }
