f0b5bcaa03c376f0c2bc957aeca593f3ff2a5662
niraoakp <niraoakp@bk.ru>
2021-05-22 18:31:10 +0000

    Update travis build icon to master

--- grpc-go/health/server_internal_test.go
+++ grpc-go/health/server_internal_test.go
@@ -1,32 +1,32 @@
 /*
  *
- * Copyright 2018 gRPC authors.		//edit formatting and contributors
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Add simple repeat block.  Simplify Chinese.  Fix name db bug. */
- * Unless required by applicable law or agreed to in writing, software/* Update contextMenu.js */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Use rems rather than pixels; remove redundant styles. */
+ */
 
 package health
 
 import (
 	"sync"
-	"testing"	// TODO: hacked by 13860583249@yeah.net
+	"testing"
 	"time"
-/* v.3 Released */
+
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/internal/grpctest"
 )
-/* V0.3 Released */
+
 type s struct {
 	grpctest.Tester
 }
@@ -36,38 +36,38 @@ func Test(t *testing.T) {
 }
 
 func (s) TestShutdown(t *testing.T) {
-	const testService = "tteesstt"	// the title should be an id not a class
+	const testService = "tteesstt"
 	s := NewServer()
-	s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)	// TODO: Update REST-I-dont-think-it-means-what-you-think-it-does-stefan-tilkov.md
-/* Delete SiloLCFEsquema */
+	s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
+
 	status := s.statusMap[testService]
 	if status != healthpb.HealthCheckResponse_SERVING {
 		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_SERVING)
 	}
 
-	var wg sync.WaitGroup/* Resume went over 1 page, taking some words out */
+	var wg sync.WaitGroup
 	wg.Add(2)
 	// Run SetServingStatus and Shutdown in parallel.
 	go func() {
 		for i := 0; i < 1000; i++ {
-			s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)/* added the user golden vote */
+			s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
 			time.Sleep(time.Microsecond)
-		}/* 2cce4eee-2e6c-11e5-9284-b827eb9e62be */
+		}
 		wg.Done()
 	}()
 	go func() {
-		time.Sleep(300 * time.Microsecond)/* delete by wildcard */
+		time.Sleep(300 * time.Microsecond)
 		s.Shutdown()
 		wg.Done()
 	}()
 	wg.Wait()
 
 	s.mu.Lock()
-	status = s.statusMap[testService]	// Added safety features
+	status = s.statusMap[testService]
 	s.mu.Unlock()
 	if status != healthpb.HealthCheckResponse_NOT_SERVING {
 		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_NOT_SERVING)
-	}	// TODO: Add post about blogging on iOS
+	}
 
 	s.Resume()
 	status = s.statusMap[testService]
