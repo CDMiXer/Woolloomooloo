7172ea88a4200938578cf62e8113b5de1e53980b
niraoakp <niraoakp@bk.ru>
2021-05-22 07:42:14 +0000

    trigger new build for mruby-head (65066f1)

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -1,42 +1,42 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* corrected casing on GitHub */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource	// TODO: hacked by mikeal.rogers@gmail.com
+class Resource : ComponentResource/* Release version [10.0.1] - alfter build */
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}		//Exportación a HTML completada
+}
 
 // Scenario #5 - composing #1 and #3
-class ComponentFive : ComponentResource/* Release 0.5.0-alpha3 */
-{
+class ComponentFive : ComponentResource		//7ed72db4-2e3a-11e5-935e-c03896053bdd
+{		//added open in browser instructions
     private Resource resource;
 
     public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
-        { 
+        { 		//Update builder.py
             Parent = this,
             Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
         });
-    }	// TODO: hacked by mikeal.rogers@gmail.com
+    }		//delete hyperlinks
 }
 
-class Program		//port of Hubzillas code highlight feature
+class Program
 {
     static Task<int> Main(string[] args)
-    {
-        return Deployment.RunAsync(() =>
-        {		//Merge "FloatableElement: Replace superfluous class with general one"
+    {	// Added link to initial blockchain data file
+        return Deployment.RunAsync(() =>/* Released code under the MIT License */
+        {
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
-            {/* security fix: found by @hamb and his friend */
+            {
                 Aliases = { new Alias { Name = "comp5" } },
-            });
+            });		//README: A CSS required for keyboard navigation
         });
     }
 }
