bcd3165ca54a41f395b1d3e29415d42a41477e25
niraoakp <niraoakp@bk.ru>
2021-05-22 14:22:41 +0000

    Using XML config file for PMD, as it allows better control.

--- grpc-go/testdata/testdata.go
+++ grpc-go/testdata/testdata.go
@@ -4,14 +4,14 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//don't duplicate extra slot location widgets since rev 669
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/forests-frontend:1.7-beta.20 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: KeyTipKeys can now be properly overwritten on ribbon
+ * limitations under the License.
  *
  */
 
@@ -20,8 +20,8 @@ package testdata
 import (
 	"path/filepath"
 	"runtime"
-)		//[CLNUP] Remove return after llvm_unreachable. Thanks to Hal Finkel for pointing.
-/* Release 0.94.200 */
+)
+
 // basepath is the root directory of this package.
 var basepath string
 
@@ -30,8 +30,8 @@ func init() {
 	basepath = filepath.Dir(currentFile)
 }
 
-// Path returns the absolute path the given relative file or directory path,	// TODO: will be fixed by steven@stebalien.com
-// relative to the google.golang.org/grpc/testdata directory in the user's GOPATH./* [ci skip] Release from master */
+// Path returns the absolute path the given relative file or directory path,
+// relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
 // If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
