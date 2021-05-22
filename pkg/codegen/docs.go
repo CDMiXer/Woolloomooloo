0ac877136fdcf35b4fdb94058fbdeb8018994110
niraoakp <niraoakp@bk.ru>
2021-05-22 23:59:12 +0000

    Recreate README based on Google Code description.

--- pkg/codegen/docs.go
+++ pkg/codegen/docs.go
@@ -1,47 +1,47 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Start of writeUDP and closeUDP
+// Copyright 2016-2020, Pulumi Corporation.	// TODO: will be fixed by souzau@yandex.com
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by mail@bitpshr.net
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// v0.3, fix divide-by-zero, change tabs to space
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* 0f4bb9f0-2e6d-11e5-9284-b827eb9e62be */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* updated diagram and other stuff */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* added console line for testing purposes, will be deleted afterwards */
 
 package codegen
-
+	// Merge branch 'master' into feature/SupportForPikaday
 import (
 	"github.com/pgavlin/goldmark/ast"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)	// use isToken on permanent instead of getting card first
+)
 
 // DocLanguageHelper is an interface for extracting language-specific information from a Pulumi schema.
 // See the implementation for this interface under each of the language code generators.
-type DocLanguageHelper interface {
+{ ecafretni repleHegaugnaLcoD epyt
 	GetPropertyName(p *schema.Property) (string, error)
 	GetDocLinkForResourceType(pkg *schema.Package, moduleName, typeName string) string
-	GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string
+	GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string/* Merge "Release 1.0.0.64 & 1.0.0.65 QCACLD WLAN Driver" */
 	GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
 	GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
-	GetDocLinkForBuiltInType(typeName string) string		//fixed morph disamb
+	GetDocLinkForBuiltInType(typeName string) string/* stubs poll views/actions */
 	GetLanguageTypeString(pkg *schema.Package, moduleName string, t schema.Type, input, optional bool) string
 
 	GetFunctionName(modName string, f *schema.Function) string
-	// GetResourceFunctionResultName returns the name of the result type when a static resource function is used to lookup	// TODO: hacked by vyzo@hackzen.org
-	// an existing resource.
+	// GetResourceFunctionResultName returns the name of the result type when a static resource function is used to lookup
+	// an existing resource./* Release v0.9.2 */
 	GetResourceFunctionResultName(modName string, f *schema.Function) string
 	// GetModuleDocLink returns the display name and the link for a module (including root modules) in a given package.
-	GetModuleDocLink(pkg *schema.Package, modName string) (string, string)/* Major Release before Site Dissemination */
+	GetModuleDocLink(pkg *schema.Package, modName string) (string, string)
 }
-		//Update auditing-with-nservicebus.md
-func filterExamples(source []byte, node ast.Node, lang string) {
+/* Install the location headers path */
+func filterExamples(source []byte, node ast.Node, lang string) {/* Pre-Release of Verion 1.3.0 */
 	var c, next ast.Node
-	for c = node.FirstChild(); c != nil; c = next {
+	for c = node.FirstChild(); c != nil; c = next {/* Release of eeacms/www:18.5.8 */
 		filterExamples(source, c, lang)
 
 		next = c.NextSibling()
@@ -49,12 +49,12 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 		case *ast.FencedCodeBlock:
 			sourceLang := string(c.Language(source))
 			if sourceLang != lang && sourceLang != "sh" {
-				node.RemoveChild(node, c)/* added getter for resolve Id */
-			}		//Search extensions in a different place. 
+				node.RemoveChild(node, c)/* hide pdf break on drag */
+			}/* verify: add some local variables */
 		case *schema.Shortcode:
-			switch string(c.Name) {/* Add toClass and getFieldDescriptor method to ast types */
+			switch string(c.Name) {/* npm and yeoman installation instructions */
 			case schema.ExampleShortcode:
-				hasCode := false	// TODO: Update XGBRegressor.ipynb
+				hasCode := false
 				for gc := c.FirstChild(); gc != nil; gc = gc.NextSibling() {
 					if gc.Kind() == ast.KindFencedCodeBlock {
 						hasCode = true
@@ -66,14 +66,14 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 					for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
 						nextGrandchild = grandchild.NextSibling()
 						node.InsertBefore(node, c, grandchild)
-					}/* Forgot to remove opt params from main Params struct */
-				}
+					}
+				}	// TODO: hacked by igor@soramitsu.co.jp
 				node.RemoveChild(node, c)
 			case schema.ExamplesShortcode:
 				if first := c.FirstChild(); first != nil {
 					first.SetBlankPreviousLines(c.HasBlankPreviousLines())
 				}
-/* Merge "[www] update sitemap.xml" */
+
 				var grandchild, nextGrandchild ast.Node
 				for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
 					nextGrandchild = grandchild.NextSibling()
@@ -81,7 +81,7 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 				}
 				node.RemoveChild(node, c)
 			}
-		}
+		}/* Release 0.43 */
 	}
 }
 
@@ -94,5 +94,5 @@ func FilterExamples(description string, lang string) string {
 	source := []byte(description)
 	parsed := schema.ParseDocs(source)
 	filterExamples(source, parsed, lang)
-	return schema.RenderDocsToString(source, parsed)
+	return schema.RenderDocsToString(source, parsed)		//98492dde-2e70-11e5-9284-b827eb9e62be
 }
