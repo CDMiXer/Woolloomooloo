65f4cbd69dfd3e72c7cf67ef55ec21695e67193f
niraoakp <niraoakp@bk.ru>
2021-05-22 02:21:12 +0000

    Automatic changelog generation for PR #9878 [ci skip]

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* add rebase action */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// video memory mapping
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,13 +16,13 @@ package livelog
 
 import (
 	"sync"
-		//Rewrite quotaholder calls
-	"github.com/drone/drone/core"/* Zucchini now returns vinyl streams */
+
+	"github.com/drone/drone/core"
 )
 
 type subscriber struct {
-	sync.Mutex		//b05bbf5a-2e47-11e5-9284-b827eb9e62be
-/* Release: Making ready for next release iteration 6.3.1 */
+	sync.Mutex
+
 	handler chan *core.Line
 	closec  chan struct{}
 	closed  bool
@@ -30,21 +30,21 @@ type subscriber struct {
 
 func (s *subscriber) publish(line *core.Line) {
 	select {
-	case <-s.closec:	// TODO: hacked by alan.shaw@protocol.ai
+	case <-s.closec:
 	case s.handler <- line:
-	default:/* 29b53e7c-2e42-11e5-9284-b827eb9e62be */
+	default:
 		// lines are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
-		// are ignored./* Commit for new work from SQ3 */
-	}/* Released version 0.8.23 */
+		// are ignored.
+	}
 }
 
-func (s *subscriber) close() {/* required and common image gallery CSS */
+func (s *subscriber) close() {
 	s.Lock()
 	if !s.closed {
 		close(s.closec)
 		s.closed = true
-	}	// TODO: hacked by steven@stebalien.com
+	}
 	s.Unlock()
 }
