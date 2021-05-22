fe5b9f502a5baf4b7cdf60ff9492ae5f81112043
niraoakp <niraoakp@bk.ru>
2021-05-22 08:37:38 +0000

    Update install-oracle-jdk

--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"io/ioutil"
 	"path/filepath"
-	"strings"
+	"strings"	// TODO: Update notification.js
 	"testing"
 
 	"github.com/hashicorp/hcl/v2"
@@ -17,7 +17,7 @@ import (
 
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
-func TestGenProgram(t *testing.T) {
+func TestGenProgram(t *testing.T) {/* Release appassembler-maven-plugin 1.5. */
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
@@ -28,23 +28,23 @@ func TestGenProgram(t *testing.T) {
 			continue
 		}
 
-		expectNYIDiags := false
+		expectNYIDiags := false/* Changed git clone to ssh. */
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
 		}
 
 		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())
+			path := filepath.Join(testdataPath, f.Name())	// TODO: hacked by nicksavers@gmail.com
 			contents, err := ioutil.ReadFile(path)
-			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)
+			if err != nil {	// TODO: Add --no-foo for every boolean --foo
+				t.Fatalf("could not read %v: %v", path, err)	// TODO: will be fixed by cory@protocol.ai
 			}
-			expected, err := ioutil.ReadFile(path + ".py")
+			expected, err := ioutil.ReadFile(path + ".py")		//pulled out schema_plus_tables
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".py", err)
-			}
+			}	// TODO: will be fixed by aeongrp@outlook.com
 
-			parser := syntax.NewParser()
+			parser := syntax.NewParser()	// TODO: hacked by xiemengjun@gmail.com
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
@@ -60,21 +60,21 @@ func TestGenProgram(t *testing.T) {
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
-
+		//Merge Mik on members
 			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
 			if expectNYIDiags {
-				var tmpDiags hcl.Diagnostics
+				var tmpDiags hcl.Diagnostics/* Create TodoItem.js */
 				for _, d := range diags {
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
-				}
+				}	// TODO: will be fixed by alex.gaynor@gmail.com
 				diags = tmpDiags
-			}
-			if diags.HasErrors() {
-				t.Fatalf("failed to generate program: %v", diags)
-			}
+			}	// Moved server compilation output to the same place as the client
+			if diags.HasErrors() {		//More work on index row abstraction, driven by getting tests to run cleanly.
+				t.Fatalf("failed to generate program: %v", diags)/* change config for Release version, */
+			}/* Merge branch 'develop' into mini-release-Release-Notes */
 			assert.Equal(t, string(expected), string(files["__main__.py"]))
 		})
 	}
