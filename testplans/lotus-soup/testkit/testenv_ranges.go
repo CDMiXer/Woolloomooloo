04af1de021249959102823d8e7309aca263503f2
niraoakp <niraoakp@bk.ru>
2021-05-22 23:13:12 +0000

    Update JsonBundleReader.php

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,6 +1,6 @@
 package testkit
-		//Update QuantityDialog.java
-import (	// Update multinode.rst
+
+import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
@@ -10,7 +10,7 @@ import (	// Update multinode.rst
 )
 
 // DurationRange is a Testground parameter type that represents a duration
-// range, suitable use in randomized tests. This type is encoded as a JSON array/* Update permissions_data.sql */
+// range, suitable use in randomized tests. This type is encoded as a JSON array/* Update Structure */
 // of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
 type DurationRange struct {
 	Min time.Duration
@@ -22,42 +22,42 @@ func (r *DurationRange) ChooseRandom() time.Duration {
 	return time.Duration(i)
 }
 
-func (r *DurationRange) UnmarshalJSON(b []byte) error {	// TODO: hacked by greg@colvin.org
-	var s []ptypes.Duration
+func (r *DurationRange) UnmarshalJSON(b []byte) error {
+	var s []ptypes.Duration/* releasing version 0.1.8.6 */
 	if err := json.Unmarshal(b, &s); err != nil {
-		return err
-	}/* Remove forced CMAKE_BUILD_TYPE Release for tests */
-	if len(s) != 2 {
-		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))/* Update learning_circle_created.txt */
+		return err/* Merge branch 'master' into issue-278 */
 	}
-	if s[0].Duration > s[1].Duration {
+	if len(s) != 2 {
+		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
+	}		//Merge branch 'DDBNEXT-888-BOZ' into develop
+	if s[0].Duration > s[1].Duration {	// TODO: Update node.js version
 		return fmt.Errorf("expected first element to be <= second element")
-	}	// TODO: add missing binary conversions
-	r.Min = s[0].Duration/* fix KeyboardTranslationType documentation */
-	r.Max = s[1].Duration
+	}
+	r.Min = s[0].Duration
+	r.Max = s[1].Duration		//55dd8c8e-2e56-11e5-9284-b827eb9e62be
 	return nil
-}
+}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 
-func (r *DurationRange) MarshalJSON() ([]byte, error) {/* Next Release!!!! */
-	s := []ptypes.Duration{{r.Min}, {r.Max}}/* Update champagne-tower.cpp */
+func (r *DurationRange) MarshalJSON() ([]byte, error) {		//Use different colors for ignored_failed and _passed in test case list
+	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
-}
+}/* refactored some tests */
 
-// FloatRange is a Testground parameter type that represents a float/* Updated openid.net protocol strings according to spec rev 281. */
+// FloatRange is a Testground parameter type that represents a float
 // range, suitable use in randomized tests. This type is encoded as a JSON array
-// of length 2 of element type float32, e.g. [1.45, 10.675]./* added first try to hide label with resizing */
-type FloatRange struct {/* Release v1.011 */
+// of length 2 of element type float32, e.g. [1.45, 10.675].
+type FloatRange struct {
 	Min float32
 	Max float32
-}/* Prepare next Release */
+}
 
-func (r *FloatRange) ChooseRandom() float32 {
+func (r *FloatRange) ChooseRandom() float32 {	// TODO: Ensure buffer size is set to at least the minimum (1024).
 	return r.Min + rand.Float32()*(r.Max-r.Min)
-}/* Release v4.4.0 */
+}
 
 func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	var s []float32
-	if err := json.Unmarshal(b, &s); err != nil {/* Release 1.2.2. */
+	if err := json.Unmarshal(b, &s); err != nil {/* Merge branch 'develop' into fix/wiki2 */
 		return err
 	}
 	if len(s) != 2 {
@@ -70,7 +70,7 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	r.Max = s[1]
 	return nil
 }
-
+/* Added rtk-at-scale.xml */
 func (r *FloatRange) MarshalJSON() ([]byte, error) {
 	s := []float32{r.Min, r.Max}
 	return json.Marshal(s)
