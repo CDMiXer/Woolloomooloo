439992557e4f7da966b9d443d1c898106193e00c
niraoakp <niraoakp@bk.ru>
2021-05-22 05:57:17 +0000

    :bug: #21 APP melhoria documentação

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -1,38 +1,38 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//45e4a3f2-2e5c-11e5-9284-b827eb9e62be
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* clarify and expand docs */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Renamed MenuState to MainMenuState.
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//noch einen vergessen re #1414
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-	// TODO: Docs: fix automake -j on release notes
-// The previous plan failed, but we're going to initiate *another* plan that		//Create replay.py
+
+// The previous plan failed, but we're going to initiate *another* plan that
 // introduces new changes, while still keeping around the failed state
-// from the previous plan. The engine should delete all pending deletes before
+// from the previous plan. The engine should delete all pending deletes before	// Tag search implemented for backlog
 // attempting to start the next plan.
-//	// K6cvkvQfcRFyd8mdYeD0sREC61Yv1gll
-// To do this, we're going to trigger another replacement of A:/* Renamed "Latest Release" to "Download" */
+//
+// To do this, we're going to trigger another replacement of A:
 const a = new Resource("a", { fail: 3 });
-/* Merge "Release 5.4.0" */
+	// TODO: hacked by earlephilhower@yahoo.com
 // We will still fail to replace B, since fail == 1.
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });/* [artifactory-release] Release version 1.4.3.RELEASE */
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });		//The test data set got smaller for noise revision
 // The snapshot now contains:
-//  A: Created/* e66b83e6-2e5b-11e5-9284-b827eb9e62be */
+//  A: Created
 //  A: Pending Delete
 //  B: Created
-		//Merge branch 'master' into bst-delete
-// The A from the previous snapshot should have been deleted.
 
-// This plan is interesting because it shows that it is legal to delete the same URN multiple
-// times in the same plan. This previously triggered an assert in the engine that asserted
-// that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
-/* Rename addMessage.js to AddMessage.js */
-		//654b990a-2e46-11e5-9284-b827eb9e62be
+// The A from the previous snapshot should have been deleted.
+	// Move images to params.images
+// This plan is interesting because it shows that it is legal to delete the same URN multiple	// TODO: 122773e0-2e53-11e5-9284-b827eb9e62be
+// times in the same plan. This previously triggered an assert in the engine that asserted		//fix(package): update postcss-selector-parser to version 5.0.0
+// that this is impossible (https://github.com/pulumi/pulumi/issues/1503)		//Added Report about consumption of last days. 
+
+
