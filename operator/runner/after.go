4106c00ff372a9925b16eff45520080f9e7d9cd5
niraoakp <niraoakp@bk.ru>
2021-05-22 15:00:19 +0000

    Removed dependency (boost::algorithm::starts_with)

--- operator/runner/after.go
+++ operator/runner/after.go
@@ -5,10 +5,10 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* [extra] pp-trace - Add test for conditional callbacks. */
+// Unless required by applicable law or agreed to in writing, software/* Use query params as curl params array */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete plasma-desktop.kwinrule */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
