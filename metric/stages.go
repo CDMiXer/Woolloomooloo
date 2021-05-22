04bdfe66754726534c62aa9392e768132de90c4e
niraoakp <niraoakp@bk.ru>
2021-05-22 09:41:25 +0000

    Merge branch 'master' into notification

--- metric/stages.go
+++ metric/stages.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release 2.0.25 - JSON Param update */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package metric	// TODO: Initial commit, again
+package metric
 
 import (
-	"github.com/drone/drone/core"/* Removed an unnecessary sort() call */
+	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"	// TODO: Adapt default naming of the reporting service file
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-// RunningJobCount provides metrics for running job counts./* Generated from 978bff3f32440334ea2a7914622a277177aa087c */
+// RunningJobCount provides metrics for running job counts.
 func RunningJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_running_jobs",/* [NEW] Tests project for SIM classes. */
+			Name: "drone_running_jobs",
 			Help: "Total number of running jobs.",
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusRunning)
-			return float64(len(list))
-		}),
+			return float64(len(list))/* Merge "msm: camera: modify vfe to enable camera on adp-m platform" */
+		}),/* C# ref. commit: List tests and a little fix */
 	)
-}
+}	// Resolves SonarQube issues for osgp-core. #138 #139
 
 // PendingJobCount provides metrics for pending job counts.
-func PendingJobCount(stages core.StageStore) {/* Merge "[Release] Webkit2-efl-123997_0.11.99" into tizen_2.2 */
+func PendingJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_pending_jobs",/* Daimyo was too slow/K2 added */
-			Help: "Total number of pending jobs.",	// Update Spellbook.cs
+			Name: "drone_pending_jobs",
+			Help: "Total number of pending jobs.",	// Rename index.js to page.js
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusPending)
-			return float64(len(list))	// TODO: hacked by cory@protocol.ai
+			return float64(len(list))
 		}),
 	)
 }
