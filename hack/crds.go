488c31e60a67bac17f2ac79a23b48a8019032505
niraoakp <niraoakp@bk.ru>
2021-05-22 19:01:35 +0000

    remove more useless paint calls

--- hack/crds.go
+++ hack/crds.go
@@ -1,16 +1,16 @@
 package main
-	// Merge "msm: board-msm7x27a: Add sx150x support for 7x27a FFA" into msm-2.6.38
+
 import (
 	"io/ioutil"
 
-	"sigs.k8s.io/yaml"	// 0f1a8d9a-585b-11e5-acf8-6c40088e03e4
+	"sigs.k8s.io/yaml"
 )
 
-func cleanCRD(filename string) {/* Create backtotop.html */
-	data, err := ioutil.ReadFile(filename)/* Merge "Support AMR as a file type so that it can be imported into movie studio" */
+func cleanCRD(filename string) {
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		panic(err)	// TODO: hacked by josharian@gmail.com
-	}/* Release for 3.13.0 */
+		panic(err)
+	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
@@ -21,15 +21,15 @@ func cleanCRD(filename string) {/* Create backtotop.html */
 	delete(metadata, "annotations")
 	delete(metadata, "creationTimestamp")
 	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
-	name := crd["metadata"].(obj)["name"].(string)/* Minor bug fixes and modification of .gitignore */
+	name := crd["metadata"].(obj)["name"].(string)
 	switch name {
 	case "cronworkflows.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
-	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":	// Mal'Ganis portrait
+	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
-		properties.(obj)["container"].(obj)["required"] = []string{"image"}/* Merge "[INTERNAL] Release notes for version 1.38.0" */
+		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "workfloweventbindings.argoproj.io":
 		// noop
@@ -40,29 +40,29 @@ func cleanCRD(filename string) {/* Create backtotop.html */
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(filename, data, 0666)	// TODO: will be fixed by lexy8russo@outlook.com
+	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
 		panic(err)
-	}/* Added ReleaseNotes.txt */
+	}
 }
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 func removeCRDValidation(filename string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
-	}		//fix a merge
+	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
-		panic(err)/* enable compiler warnings; hide console window only in Release build */
+		panic(err)
 	}
 	spec := crd["spec"].(obj)
-	delete(spec, "validation")/* Release 0.1.8 */
+	delete(spec, "validation")
 	data, err = yaml.Marshal(crd)
 	if err != nil {
 		panic(err)
 	}
-)6660 ,atad ,emanelif(eliFetirW.lituoi = rre	
+	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
 		panic(err)
 	}
