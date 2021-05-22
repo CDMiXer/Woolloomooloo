4b9303f41f029d5a0fcda7dc84f8200c2d970f9d
niraoakp <niraoakp@bk.ru>
2021-05-22 12:27:23 +0000

    Delete js_bookmark.py

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";/* JForum 2.3.3 Release */
+		//added instructions how to use the autopilot
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Released springjdbcdao version 1.7.7 */
-    }
+        super("my:module:Resource", name, {}, opts);
+    }		//Create JustRoute.php
 }
-
-// Scenario #5 - composing #1 and #3 and making both changes at the same time
+		//bf45766e-2e60-11e5-9284-b827eb9e62be
+// Scenario #5 - composing #1 and #3 and making both changes at the same time/* 4baa6356-2e40-11e5-9284-b827eb9e62be */
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);/* Released 0.2.2 */
+        super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
-    }/* Release 1-90. */
-}/* 0.9.7 Release. */
+    }
+}
 const comp5 = new ComponentFive("comp5");
