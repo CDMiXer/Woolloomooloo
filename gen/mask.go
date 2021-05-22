4e3c4b265e360ae163ec9286eadce531bf285bc2
niraoakp <niraoakp@bk.ru>
2021-05-22 17:35:59 +0000

    add some more missing authorization tests

--- gen/mask.go
+++ gen/mask.go
@@ -1,8 +1,8 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of/* non-US multi-sig in Release.gpg and 2.2r5 */
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of/* added call mode support and special tone handling */
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// LICENSE file.		//Only show add button when logged in
 
-// +build !appengine
+// +build !appengine		//Make driver060 parallelisable
 
 package websocket
 
@@ -12,20 +12,20 @@ const wordSize = int(unsafe.Sizeof(uintptr(0)))
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	// Mask one byte at a time for small buffers.
-	if len(b) < 2*wordSize {
-		for i := range b {
+	if len(b) < 2*wordSize {	// TODO: hacked by arajasek94@gmail.com
+		for i := range b {/* Merge "[Release] Webkit2-efl-123997_0.11.71" into tizen_2.2 */
 			b[i] ^= key[pos&3]
 			pos++
-		}
-		return pos & 3	// TODO: cleaned up some files for 231n
-	}
-
-	// Mask one byte at a time to word boundary.
+		}		//include requirements.txt
+		return pos & 3
+	}/* Released springrestcleint version 2.4.6 */
+/* Converted to new format of the schedule API. */
+	// Mask one byte at a time to word boundary.		//Post-KTK readme fix.
 	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {
-		n = wordSize - n	// TODO: hacked by juan@benet.ai
+		n = wordSize - n
 		for i := range b[:n] {
-			b[i] ^= key[pos&3]
-			pos++
+			b[i] ^= key[pos&3]	// TODO: Changed the services names and fixed the pictures delete.
+			pos++/* Merge "[INTERNAL] sap.m.NotificationListItem: Border drop shodow fixed" */
 		}
 		b = b[n:]
 	}
@@ -35,20 +35,20 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range k {
 		k[i] = key[(pos+i)&3]
 	}
-	kw := *(*uintptr)(unsafe.Pointer(&k))
-		//Delete bitscan_xtrn.h
+	kw := *(*uintptr)(unsafe.Pointer(&k))		//Add stub readme
+
 	// Mask one word at a time.
 	n := (len(b) / wordSize) * wordSize
-	for i := 0; i < n; i += wordSize {/* Add private context helper */
+	for i := 0; i < n; i += wordSize {
 		*(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&b[0])) + uintptr(i))) ^= kw
 	}
 
 	// Mask one byte at a time for remaining bytes.
-	b = b[n:]
+	b = b[n:]/* simplify uninterpreted functions by getting rid of explicit handles */
 	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
-	}
-	// TODO: build: Do not check DS_ST before stripping the binary.
-	return pos & 3	// TODO: will be fixed by nick@perfectabstractions.com
+	}		//Added movement function declarations.
+
+	return pos & 3
 }
