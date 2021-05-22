fc734e1f3275ec20bdad2bf4d3bc9d06eba14169
niraoakp <niraoakp@bk.ru>
2021-05-22 14:28:05 +0000

    Merge "Use joined version of db.api calls"

--- core/trigger.go
+++ core/trigger.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 1.9.3.19 CommandLineParser */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -16,15 +16,15 @@ package core
 
 import "context"
 
-// Trigger types
+// Trigger types/* Update release notes for Release 1.7.1 */
 const (
-	TriggerHook = "@hook"
-	TriggerCron = "@cron"
+	TriggerHook = "@hook"/* SnomedRelease is passed down to the importer. SO-1960 */
+	TriggerCron = "@cron"		//8c53cf2c-35c6-11e5-bd66-6c40088e03e4
 )
 
-// Triggerer is responsible for triggering a Build from an
+// Triggerer is responsible for triggering a Build from an/* Release 2.7. */
 // incoming drone. If a build is skipped a nil value is
 // returned.
-type Triggerer interface {
-	Trigger(context.Context, *Repository, *Hook) (*Build, error)
+type Triggerer interface {		//fixed undefined verification token error
+	Trigger(context.Context, *Repository, *Hook) (*Build, error)	// TODO: will be fixed by arajasek94@gmail.com
 }
