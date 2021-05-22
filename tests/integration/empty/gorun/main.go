e453b2e5b99c09d7e7ea75db47b340b27db96d00
niraoakp <niraoakp@bk.ru>
2021-05-22 23:54:09 +0000

    - some cleanup about authors and version loading

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//847fa494-2e4e-11e5-9284-b827eb9e62be
 
-package main
+package main		//float values for timings
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {/* devops-edit --pipeline=dotnet/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
 		return nil
 	})
 }
