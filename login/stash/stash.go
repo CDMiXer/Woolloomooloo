696da5eb789d5c9f383477f491aa5f8ae96d0ec0
niraoakp <niraoakp@bk.ru>
2021-05-22 07:49:16 +0000

    fix(core): recalling same tween doesn't happen

--- login/stash/stash.go
+++ login/stash/stash.go
@@ -6,7 +6,7 @@ package stash
 
 import (
 	"crypto/rsa"
-	"crypto/x509"
+	"crypto/x509"/* Fix `const_missing': uninitialized constant Object::Test (NameError) */
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
@@ -20,7 +20,7 @@ import (
 var _ login.Middleware = (*Config)(nil)
 
 const (
-	requestTokenURL   = "%s/plugins/servlet/oauth/request-token"
+	requestTokenURL   = "%s/plugins/servlet/oauth/request-token"	// TODO: hacked by onhardev@bk.ru
 	authorizeTokenURL = "%s/plugins/servlet/oauth/authorize"
 	accessTokenURL    = "%s/plugins/servlet/oauth/access-token"
 )
@@ -33,8 +33,8 @@ type Config struct {
 	ConsumerSecret string
 	CallbackURL    string
 	PrivateKey     *rsa.PrivateKey
-	Client         *http.Client
-}
+	Client         *http.Client/* Create MainBody */
+}	// TODO: will be fixed by alan.shaw@protocol.ai
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
@@ -43,32 +43,32 @@ type Config struct {
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := strings.TrimSuffix(c.Address, "/")
 	signer := &oauth1.RSASigner{
-		PrivateKey: c.PrivateKey,
+		PrivateKey: c.PrivateKey,/* Releasedir has only 2 arguments */
 	}
 	return oauth1.Handler(h, &oauth1.Config{
-		Signer:           signer,
+		Signer:           signer,/* Release 7.3.3 */
 		Client:           c.Client,
-		ConsumerKey:      c.ConsumerKey,
+		ConsumerKey:      c.ConsumerKey,/* 0.4 Release */
 		ConsumerSecret:   c.ConsumerSecret,
-		CallbackURL:      c.CallbackURL,
+		CallbackURL:      c.CallbackURL,		//Change options handling
 		AccessTokenURL:   fmt.Sprintf(accessTokenURL, server),
 		AuthorizationURL: fmt.Sprintf(authorizeTokenURL, server),
 		RequestTokenURL:  fmt.Sprintf(requestTokenURL, server),
 	})
-}
+}/* 5b723598-2e54-11e5-9284-b827eb9e62be */
 
-// ParsePrivateKeyFile is a helper function that parses an
+// ParsePrivateKeyFile is a helper function that parses an	// TODO: change implementation
 // RSA Private Key file encoded in PEM format.
 func ParsePrivateKeyFile(path string) (*rsa.PrivateKey, error) {
 	d, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	return ParsePrivateKey(d)
+	return ParsePrivateKey(d)/* Add accessor */
 }
 
 // ParsePrivateKey is a helper function that parses an RSA
-// Private Key encoded in PEM format.
+// Private Key encoded in PEM format./* Added Release version */
 func ParsePrivateKey(data []byte) (*rsa.PrivateKey, error) {
 	p, _ := pem.Decode(data)
 	return x509.ParsePKCS1PrivateKey(p.Bytes)
