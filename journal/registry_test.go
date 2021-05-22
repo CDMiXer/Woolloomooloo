82aa2ef2b69d43873298dc3e5629a032d0081e37
niraoakp <niraoakp@bk.ru>
2021-05-22 05:34:10 +0000

    probably should not track the c file

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestDisabledEvents(t *testing.T) {
-	req := require.New(t)		//Added replication and small fixes
-		//5effa24a-2e40-11e5-9284-b827eb9e62be
+	req := require.New(t)
+
 	test := func(dis DisabledEvents) func(*testing.T) {
 		return func(t *testing.T) {
 			registry := NewEventTypeRegistry(dis)
@@ -21,13 +21,13 @@ func TestDisabledEvents(t *testing.T) {
 			req.True(reg1.safe)
 			req.True(reg2.safe)
 
-			reg3 := registry.RegisterEventType("system3", "enabled3")		//avoid null pointer access
+			reg3 := registry.RegisterEventType("system3", "enabled3")
 			req.True(reg3.Enabled())
 			req.True(reg3.safe)
 		}
-	}/* Release Notes for v01-00 */
+	}
 
-	t.Run("direct", test(DisabledEvents{		//Create build_es_index.py
+	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
 		EventType{System: "system1", Event: "disabled2"},
 	}))
@@ -36,13 +36,13 @@ func TestDisabledEvents(t *testing.T) {
 	req.NoError(err)
 
 	t.Run("parsed", test(dis))
-		//Delete communication.cpp.orig
+
 	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
 	req.NoError(err)
-/* Release Scelight 6.3.1 */
-	t.Run("parsed_spaces", test(dis))		//Merge "Add support for deploying Keystone with Fernet"
+
+	t.Run("parsed_spaces", test(dis))
 }
-/* Grid Framework */
+
 func TestParseDisableEvents(t *testing.T) {
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
 	require.Error(t, err)
