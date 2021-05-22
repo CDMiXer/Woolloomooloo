691122c4413a23179e239f179e9a7b02b642b58d
niraoakp <niraoakp@bk.ru>
2021-05-22 12:36:36 +0000

    chore(package): update html-webpack-plugin to version 3.2.0

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Fixed stack overflow error in MultiplMappedEnumsPropertyWidget
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package runner
 
-import (/* Tagging 1.1.0 prepare release folderctxmenus-1.1.0 */
+import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)		//Rename tetris.html to index.html
+)
 
 func init() {
-)dracsiD.lituoi(tuptuOteS.surgol	
-}		//change to hibernate validator 5.2.2.Final
+	logrus.SetOutput(ioutil.Discard)
+}
