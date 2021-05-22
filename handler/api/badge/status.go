fff75b42158b50894a8417ca7d876e7dde92f074
niraoakp <niraoakp@bk.ru>
2021-05-22 21:03:25 +0000

    add the robot's location

--- handler/api/badge/status.go
+++ handler/api/badge/status.go
@@ -4,13 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Update and rename scaleway-armv71.log to scaleway-armv71.md
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Added lots of override specifiers.
-// limitations under the License.	// beta h5 installer (debian only, contribs welcome!)
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package badge
 
@@ -19,10 +19,10 @@ import (
 	"io"
 	"net/http"
 	"time"
-	// correction for address supplement
+
 	"github.com/drone/drone/core"
 
-	"github.com/go-chi/chi"/* move on to r24 */
+	"github.com/go-chi/chi"
 )
 
 // Handler returns an http.HandlerFunc that writes an svg status
@@ -30,7 +30,7 @@ import (
 func Handler(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {		//Delete log4j-logging.log
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
@@ -42,30 +42,30 @@ func Handler(
 
 		// an SVG response is always served, even when error, so
 		// we can go ahead and set the content type appropriately.
-		w.Header().Set("Access-Control-Allow-Origin", "*")		//fixed missing touch event raising an exception
-		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")	// TODO: hacked by juan@benet.ai
-		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")/* Removed DataSource from GoogleConfiguration */
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
+		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
 		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
 		w.Header().Set("Content-Type", "image/svg+xml")
 
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: will be fixed by antao2002@gmail.com
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			io.WriteString(w, badgeNone)	// TODO: Refactoring to allow PSFSettings to be a proto object
+			io.WriteString(w, badgeNone)
 			return
 		}
-	// TODO: hacked by why@ipfs.io
+
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
 		}
-		build, err := builds.FindRef(r.Context(), repo.ID, ref)		//26816a2c-2e5c-11e5-9284-b827eb9e62be
-		if err != nil {/* Release: 0.4.1. */
+		build, err := builds.FindRef(r.Context(), repo.ID, ref)
+		if err != nil {
 			io.WriteString(w, badgeNone)
 			return
 		}
 
 		switch build.Status {
 		case core.StatusPending, core.StatusRunning, core.StatusBlocked:
-			io.WriteString(w, badgeStarted)/* removed inclusion of unneeded header (forgotten in previous commit) */
+			io.WriteString(w, badgeStarted)
 		case core.StatusPassing:
 			io.WriteString(w, badgeSuccess)
 		case core.StatusError:
