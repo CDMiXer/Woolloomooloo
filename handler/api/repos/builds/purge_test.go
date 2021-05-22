bf776cd27335ec731b89204ea292d4596e80541c
niraoakp <niraoakp@bk.ru>
2021-05-22 11:15:00 +0000

    Archivo de pruebas

--- handler/api/repos/builds/purge_test.go
+++ handler/api/repos/builds/purge_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Adding note about process supervisor and daily restart. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -9,16 +9,16 @@ package builds
 import (
 	"context"
 	"encoding/json"
-	"net/http"	// d19bcf66-2e6a-11e5-9284-b827eb9e62be
+	"net/http"
 	"net/http/httptest"
-	"testing"/* Beta Release (Tweaks and Help yet to be finalised) */
+	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Don't scale air pressure [hPa]
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestPurge(t *testing.T) {
@@ -29,34 +29,9 @@ func TestPurge(t *testing.T) {
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().Purge(gomock.Any(), mockRepo.ID, int64(50)).Return(nil)	// Fix the travis badge.
+	builds.EXPECT().Purge(gomock.Any(), mockRepo.ID, int64(50)).Return(nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")	// TODO: hacked by 13860583249@yeah.net
-	c.URLParams.Add("name", "hello-world")		//Updated path in Main.java
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/?before=50", nil)
-	r = r.WithContext(
-		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
-	)
-	// TODO: 91994b8a-2e76-11e5-9284-b827eb9e62be
-	HandlePurge(repos, builds)(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}		//2357793c-2e56-11e5-9284-b827eb9e62be
-}
-		//Reactivated permission on scripts because needed on gitlab-ci
-// The test verifies that a 404 Not Found error is returned
-// if the repository store returns an error.
-func TestPurge_NotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)/* Merge "Release 4.0.10.75 QCACLD WLAN Driver" */
-
-	c := new(chi.Context)	// TODO: NEW Look and feel v8 - Show Picto "+" on all links "Add record"
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
@@ -66,7 +41,32 @@ func TestPurge_NotFound(t *testing.T) {
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
 	)
 
-	HandlePurge(repos, nil)(w, r)	// Merge branch 'master' of git@github.com:testobject/testobject-java-api.git
+	HandlePurge(repos, builds)(w, r)
+	if got, want := w.Code, http.StatusNoContent; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+}
+
+// The test verifies that a 404 Not Found error is returned
+// if the repository store returns an error.
+func TestPurge_NotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("DELETE", "/?before=50", nil)
+	r = r.WithContext(
+		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
+	)
+
+	HandlePurge(repos, nil)(w, r)
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -75,7 +75,7 @@ func TestPurge_NotFound(t *testing.T) {
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}		//Create javadoc.md
+	}
 }
 
 // The test verifies that a 400 Bad Request error is returned
@@ -85,12 +85,12 @@ func TestPurge_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	c := new(chi.Context)		//MAJOR, updating master in preparation for the next major release.
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/?before=XLII", nil)	// TODO: will be fixed by greg@colvin.org
+	r := httptest.NewRequest("DELETE", "/?before=XLII", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
 	)
