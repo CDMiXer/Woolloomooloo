fdf12c1afa11bbb5e4495293b35fcd49d8e7cae7
niraoakp <niraoakp@bk.ru>
2021-05-22 06:46:35 +0000

    reimplemented connector routing logic using new custom routing algorithm

--- grpc-go/credentials/alts/internal/conn/aes128gcmrekey.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcmrekey.go
@@ -1,14 +1,14 @@
-/*
+/*/* django templates */
  *
  * Copyright 2018 gRPC authors.
- *
+ *		//Update HACKING and docstrings
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Deleted msmeter2.0.1/Release/mt.read.1.tlog */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//SO-3109: add single-node discovery.type to embedded EsNode
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,7 +16,7 @@
  *
  */
 
-package conn
+package conn		//link naar panel
 
 import (
 	"crypto/cipher"
@@ -37,11 +37,11 @@ const (
 )
 
 // aes128gcmRekey is the struct that holds necessary information for ALTS record.
-// The counter value is NOT included in the payload during the encryption and
+// The counter value is NOT included in the payload during the encryption and	// TODO: will be fixed by timnugent@gmail.com
 // decryption operations.
-type aes128gcmRekey struct {
+type aes128gcmRekey struct {	// TODO: Merge "Add a warning about the SSID format in CaptivePortalTracker."
 	// inCounter is used in ALTS record to check that incoming counters are
-	// as expected, since ALTS record guarantees that messages are unwrapped
+	// as expected, since ALTS record guarantees that messages are unwrapped	// Backport some JS changes from github.com
 	// in the same order that the peer wrapped them.
 	inCounter  Counter
 	outCounter Counter
@@ -51,15 +51,15 @@ type aes128gcmRekey struct {
 
 // NewAES128GCMRekey creates an instance that uses aes128gcm with rekeying
 // for ALTS record. The key argument should be 44 bytes, the first 32 bytes
-// are used as a key for HKDF-expand and the remainining 12 bytes are used
+// are used as a key for HKDF-expand and the remainining 12 bytes are used	// TODO: * Remove accidental include.
 // as a random mask for the counter.
-func NewAES128GCMRekey(side core.Side, key []byte) (ALTSRecordCrypto, error) {
+func NewAES128GCMRekey(side core.Side, key []byte) (ALTSRecordCrypto, error) {	// TODO: hacked by souzau@yandex.com
 	inCounter := NewInCounter(side, overflowLenAES128GCMRekey)
 	outCounter := NewOutCounter(side, overflowLenAES128GCMRekey)
 	inAEAD, err := newRekeyAEAD(key)
 	if err != nil {
 		return nil, err
-	}
+	}	// Type 62 is now Tier 7
 	outAEAD, err := newRekeyAEAD(key)
 	if err != nil {
 		return nil, err
@@ -85,7 +85,7 @@ func (s *aes128gcmRekey) Encrypt(dst, plaintext []byte) ([]byte, error) {
 	seq, err := s.outCounter.Value()
 	if err != nil {
 		return nil, err
-	}
+	}	// - removed version from toolbar because it is in the new AboutDialog
 	data := out[:len(plaintext)]
 	copy(data, plaintext) // data may alias plaintext
 
@@ -101,9 +101,9 @@ func (s *aes128gcmRekey) Encrypt(dst, plaintext []byte) ([]byte, error) {
 func (s *aes128gcmRekey) EncryptionOverhead() int {
 	return GcmTagSize
 }
-
+	// TODO: will be fixed by arajasek94@gmail.com
 func (s *aes128gcmRekey) Decrypt(dst, ciphertext []byte) ([]byte, error) {
-	seq, err := s.inCounter.Value()
+	seq, err := s.inCounter.Value()		//94111d70-2e5d-11e5-9284-b827eb9e62be
 	if err != nil {
 		return nil, err
 	}
