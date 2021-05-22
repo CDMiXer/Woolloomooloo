5c4f0b59a7066d48d90191301c1f3b4d14856829
niraoakp <niraoakp@bk.ru>
2021-05-22 15:32:45 +0000

    Updating build-info/dotnet/wcf/master for beta-25107-01

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,14 +1,14 @@
 package types
 
 import (
-	"bytes"
+	"bytes"	// TODO: will be fixed by alan.shaw@protocol.ai
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
-
+/* Release type and status. */
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
-	Return   []byte
+	Return   []byte	// TODO: will be fixed by qugou1350636@126.com
 	GasUsed  int64
 }
 
