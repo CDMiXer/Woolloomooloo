4626f10e24344401f690cb529c65049bbf85dc61
niraoakp <niraoakp@bk.ru>
2021-05-22 04:39:35 +0000

    Updated owner in mono.json

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Update integrate-your-tech@es.md
+// that can be found in the LICENSE file.	// Fixed Issue #2: Broken link to original project.
 
 // +build !oss
 
