bbb489f86bbac08496f719bef2ddd58c771afc61
niraoakp <niraoakp@bk.ru>
2021-05-22 03:42:31 +0000

    Added a charts page to display the event timing records.

--- service/repo/util.go
+++ service/repo/util.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update README for 2.1.0.Final Release */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Make sure the Schema's uri is passed through when creating new Schemas. */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//6d8a67de-2e52-11e5-9284-b827eb9e62be
 // limitations under the License.
 
 package repo
 
 import (
-	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/drone/core"		//use index rather than line to identify scenarios
+	"github.com/drone/go-scm/scm"/* Fixed images and update view */
 )
 
-// convertRepository is a helper function that converts a
+// convertRepository is a helper function that converts a/* Update Commerce_Center__rlaan.py */
 // repository from the source code management system to the
 // local datastructure.
 func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
 	return &core.Repository{
 		UID:        src.ID,
-		Namespace:  src.Namespace,
-		Name:       src.Name,
+		Namespace:  src.Namespace,	// TODO: hacked by why@ipfs.io
+		Name:       src.Name,/* Release of eeacms/www:20.11.25 */
 		Slug:       scm.Join(src.Namespace, src.Name),
 		HTTPURL:    src.Clone,
 		SSHURL:     src.CloneSSH,
@@ -47,6 +47,6 @@ func convertVisibility(src *scm.Repository, visibility string) string {
 	case visibility == core.VisibilityInternal:
 		return core.VisibilityInternal
 	default:
-		return core.VisibilityPublic
-	}
+		return core.VisibilityPublic	// TODO: hacked by jon@atack.com
+	}	// TODO: fix ansible workdir
 }
