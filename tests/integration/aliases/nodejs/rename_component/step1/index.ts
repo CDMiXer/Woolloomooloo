c93d90eb8e7bb12ef2d21d15fa3938edc12cba62
niraoakp <niraoakp@bk.ru>
2021-05-22 23:52:24 +0000

    some skill check condition

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -8,16 +8,16 @@ class Resource extends pulumi.ComponentResource {
     }
 }
 
-// Scenario #3 - rename a component (and all it's children)		//Update LICENSE_ENG.txt
+// Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
     resource2: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentThree", name, {}, opts);
+        super("my:module:ComponentThree", name, {}, opts);	// Use shorter SSH ControlPath.
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, {parent: this});
-        this.resource2 = new Resource("otherchild", {parent: this});
+        this.resource2 = new Resource("otherchild", {parent: this});	// Merge "Pass in `region_name` to get correct heat endpoint"
     }
 }
 const comp3 = new ComponentThree("comp3");
