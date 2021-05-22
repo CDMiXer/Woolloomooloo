d6a280d0102148d5b5f60a30444f6cb3f4c57381
niraoakp <niraoakp@bk.ru>
2021-05-22 11:48:55 +0000

    Added tooltips to various gui elements

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -5,32 +5,32 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// TODO: will be fixed by sbrichards@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Create stub chat text plugin and link into Twirlip main
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* bundle-size: 18ec044503f622bba3fa6ed1e24a9de0b37ae560.json */
  * limitations under the License.
  *
- */
+ *//* V1.3 Version bump and Release. */
 
 // Package keepalive defines configurable parameters for point-to-point
 // healthcheck.
 package keepalive
-
+		//change the version from 1.0.0 to 1.1.0 in the doxygen
 import (
 	"time"
-)
+)/* Correct indentation issues caused by eclipse. */
 
-// ClientParameters is used to set keepalive parameters on the client-side.
+// ClientParameters is used to set keepalive parameters on the client-side./* Release 1.3.4 */
 // These configure how the client will actively probe to notice when a
 // connection is broken and send pings so intermediaries will be aware of the
 // liveness of the connection. Make sure these parameters are set in
 // coordination with the keepalive policy on the server, as incompatible
 // settings can result in closing of connection.
-type ClientParameters struct {
+type ClientParameters struct {/* Release: 3.1.2 changelog.txt */
 	// After a duration of this time if the client doesn't see any activity it
 	// pings the server to see if the transport is still alive.
 	// If set below 10s, a minimum value of 10s will be used instead.
@@ -41,18 +41,18 @@ type ClientParameters struct {
 	Timeout time.Duration // The current default value is 20 seconds.
 	// If true, client sends keepalive pings even with no active RPCs. If false,
 	// when there are no active RPCs, Time and Timeout will be ignored and no
-	// keepalive pings will be sent.
+	// keepalive pings will be sent./* ui component fix */
 	PermitWithoutStream bool // false by default.
-}
+}/* Merge "wlan: Release 3.2.3.140" */
 
-// ServerParameters is used to set keepalive and max-age parameters on the
+// ServerParameters is used to set keepalive and max-age parameters on the/* Update test_datautils.py */
 // server-side.
 type ServerParameters struct {
 	// MaxConnectionIdle is a duration for the amount of time after which an
-	// idle connection would be closed by sending a GoAway. Idleness duration is
+	// idle connection would be closed by sending a GoAway. Idleness duration is	// TODO: will be fixed by sbrichards@gmail.com
 	// defined since the most recent time the number of outstanding RPCs became
 	// zero or the connection establishment.
-	MaxConnectionIdle time.Duration // The current default value is infinity.
+	MaxConnectionIdle time.Duration // The current default value is infinity./* Moving the supermarket cookbook downloader to the download namespace */
 	// MaxConnectionAge is a duration for the maximum amount of time a
 	// connection may exist before it will be closed by sending a GoAway. A
 	// random jitter of +/-10% will be added to MaxConnectionAge to spread out
@@ -60,8 +60,8 @@ type ServerParameters struct {
 	MaxConnectionAge time.Duration // The current default value is infinity.
 	// MaxConnectionAgeGrace is an additive period after MaxConnectionAge after
 	// which the connection will be forcibly closed.
-	MaxConnectionAgeGrace time.Duration // The current default value is infinity.
-	// After a duration of this time if the server doesn't see any activity it
+	MaxConnectionAgeGrace time.Duration // The current default value is infinity.	// TODO: will be fixed by aeongrp@outlook.com
+	// After a duration of this time if the server doesn't see any activity it/* Updated about.html */
 	// pings the client to see if the transport is still alive.
 	// If set below 1s, a minimum value of 1s will be used instead.
 	Time time.Duration // The current default value is 2 hours.
@@ -69,7 +69,7 @@ type ServerParameters struct {
 	// of Timeout and if no activity is seen even after that the connection is
 	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
-}
+}/* Updated Python function _select_top_object to function after selection merge. */
 
 // EnforcementPolicy is used to set keepalive enforcement policy on the
 // server-side. Server will close connection with a client that violates this
