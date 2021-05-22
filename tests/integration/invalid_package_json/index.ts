25066b99fdd012eb3cadada2906a54df9d0f96f5
niraoakp <niraoakp@bk.ru>
2021-05-22 20:50:54 +0000

    Tweak the docs a bit.

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: Fix #515: Userlist: Search doesn't show anything if page is out of range
 import { Config } from "@pulumi/pulumi";
 import * as runtime from "@pulumi/pulumi/runtime"
-/* Privacy update w/ GDPR */
+
 (async function() {
     const config = new Config();
-		//Use notBefore as a date not a number
+/* Merge branch 'master' into amam/add_award */
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
-    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that/* modify intro. */
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
     // behavior.
-    const deps = await runtime.computeCodePaths();		//b7932bc0-2e4f-11e5-9284-b827eb9e62be
+    const deps = await runtime.computeCodePaths();/* Fixed build issue for Release version after adding "c" api support */
 
     const actual = JSON.stringify([...deps.keys()].sort());
-    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";		//- dream details - resources table updates
+    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
 
     if (actual !== expected) {
         throw new Error(`Got '${actual}' expected '${expected}'`)
     }
-})()
+})()	// TODO: will be fixed by davidad@alum.mit.edu
