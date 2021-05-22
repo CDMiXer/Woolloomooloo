1c3c7d7946b0e6bdbb5673be9b85144468f9aadc
niraoakp <niraoakp@bk.ru>
2021-05-22 08:07:07 +0000

    Create max verstappen post

--- handler/api/secrets/update_test.go
+++ handler/api/secrets/update_test.go
@@ -1,21 +1,21 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Cache seen prefixes for configurable time period */
+// +build !oss
 
 package secrets
-	// TODO: will be fixed by mail@bitpshr.net
+
 import (
 	"bytes"
 	"context"
-	"encoding/json"/* Update to Releasenotes for 2.1.4 */
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"	// Delete LinuxCNC_M4-Dcs_5i25-7i77.desktop
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
@@ -25,29 +25,29 @@ import (
 
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Released version 0.8.27 */
+	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)	// TODO: Update ofApp.cpp
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
-		//Prepare release 1.0.5
+
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
 
-	in := new(bytes.Buffer)		//Improve console output from district-graphs.R
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
-/* Update FCE_prepare.m */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// Update GRE to Groovy 1.6
-	)	// TODO: Swapped out Jsoniter with Jackson. Slightly slower but easier to use.
-/* Plant distribution test passing */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
 	HandleUpdate(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* 0.1.1 Release. */
+	}
 
 	got, want := new(core.Secret), dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
@@ -59,8 +59,8 @@ func TestHandleUpdate(t *testing.T) {
 func TestHandleUpdate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* copyright notice */
-	secrets := mock.NewMockGlobalSecretStore(controller)		//hotfix: checkstyle errors in test case
+
+	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
 
 	c := new(chi.Context)
