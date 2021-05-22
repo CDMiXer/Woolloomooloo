dec1a8e2070deb642f1f0411964e53d6f490684f
niraoakp <niraoakp@bk.ru>
2021-05-22 13:21:28 +0000

    Rename q3_run.py to source/q3_run.py

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,24 +1,24 @@
-.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
-	// More efficient search for users without a particular annotation
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* add deleteReference, deletes system file and removes the file meta for it both */
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* fix duplication concept (use pool) */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
-    resource: Resource;	// Improved grid loop.
+    resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        // Add an alias that references the old type of this resource...
+        // Add an alias that references the old type of this resource.../* Release PBXIS-0.5.0-alpha1 */
         const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
         // ..and then make the super call with the new type of this resource and the added alias.
-        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });		//Upgraded version to 3.8.0
+        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
         // to.
         this.resource = new Resource("otherchild", { parent: this });
-    }	// TODO: iocore: allow I/O broker to be a separate process
-}
+    }
+}/* changed call from ReleaseDataverseCommand to PublishDataverseCommand */
 const comp4 = new ComponentFour("comp4");
