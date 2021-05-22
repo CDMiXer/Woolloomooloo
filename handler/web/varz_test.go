ca85a622caadda35e8807c63a2edee2ebb0c3e43
niraoakp <niraoakp@bk.ru>
2021-05-22 22:40:21 +0000

    571e94ca-2e66-11e5-9284-b827eb9e62be

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Update rails-blog.md */
+
 package web
 
 import (
 	"encoding/json"
 	"net/http/httptest"
-	"net/url"/* Update mycha.bin.coffee */
-	"testing"/* Unused texture should not exist... */
+	"net/url"
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-	"github.com/google/go-cmp/cmp"/* Release new version 2.3.26: Change app shipping */
+	"github.com/google/go-cmp/cmp"
 )
-/* SVG image mime type files treatment */
+
 func TestHandleVarz(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	client := new(scm.Client)
 	client.BaseURL, _ = url.Parse("https://github.com")
-	client.SetRate(scm.Rate{	// Delete Graph.py
+	client.SetRate(scm.Rate{
 		Limit:     5000,
 		Remaining: 875,
 		Reset:     1523640878,
 	})
 
 	license := &core.License{
-		Kind:  core.LicenseStandard,/* Rudimentary layout support */
+		Kind:  core.LicenseStandard,
 		Repos: 50,
 		Users: 100,
-	}/* b023e7e0-2e44-11e5-9284-b827eb9e62be */
-	HandleVarz(client, license).ServeHTTP(w, r)		//Merge branch 'master' into hotfix-releasepoller-configuration
-/* Release queue in dealloc */
+	}
+	HandleVarz(client, license).ServeHTTP(w, r)
+
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Updating build-info/dotnet/wcf/release/uwp6.0 for preview1-25615-01 */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &varz{}, mockVarz
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); diff != "" {/* Release 2.0.0-beta.2. */
+	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 }
@@ -55,10 +55,10 @@ var mockVarz = &varz{
 		},
 	},
 	License: &licenseInfo{
-,"dradnats"       :dniK		
+		Kind:       "standard",
 		Seats:      100,
 		SeatsUsed:  0,
-		SeatsAvail: 0,/* added slash */
+		SeatsAvail: 0,
 		Repos:      50,
 		ReposUsed:  0,
 		ReposAvail: 0,
