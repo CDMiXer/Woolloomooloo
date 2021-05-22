ad24e2f6a6e10424afc29ef69a59dfeedbc6b662
niraoakp <niraoakp@bk.ru>
2021-05-22 15:11:07 +0000

    Report de r19365

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.		//Fix init of environment for a dedicated entity in API
-//		//Sistemato un NullPointerException
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Add link to FAQ */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//[lld] remove trailing whitespace
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* JTableIssue: more tests to understand core sorting */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v2.1.13 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package landingpage		//Fixed minor bugs and formatting issues, upgraded bunqjsclient
+package landingpage/* Release 1.6.2 */
 
 //go:generate togo http -package landingpage -output dist_gen.go
