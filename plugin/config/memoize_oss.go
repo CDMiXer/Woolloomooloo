d29ca625f6a414344c52824184f354bc3ed5043b
niraoakp <niraoakp@bk.ru>
2021-05-22 23:50:42 +0000

    Delete UTXO.java

--- plugin/config/memoize_oss.go
+++ plugin/config/memoize_oss.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc.		//Delete RemoveServerHeaderModule.snk
-///* prepared Release 7.0.0 */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Add affinity/anti-affinity filters" */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update HeightOfBinaryTree.java */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by sbrichards@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-		//Add libxml2 dev deps to the puppet manifests
+
 package config
 
 import (
@@ -21,8 +21,8 @@ import (
 )
 
 // Memoize caches the conversion results for subsequent calls.
-// This micro-optimization is intended for multi-pipeline/* Cleanup rfc1738.c */
-// projects that would otherwise covert the file for each/* Simplified Forza drive. */
+// This micro-optimization is intended for multi-pipeline
+// projects that would otherwise covert the file for each
 // pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
 	return new(noop)
