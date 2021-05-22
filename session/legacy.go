82ad210d34fabae2289bf1f34381be366ee1f195
niraoakp <niraoakp@bk.ru>
2021-05-22 08:01:45 +0000

    [REF] pylint_vauxoo_light: Add doc to message W1201

--- session/legacy.go
+++ session/legacy.go
@@ -7,10 +7,10 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by seth@sethvargo.com
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:18.3.15 */
-// See the License for the specific language governing permissions and	// Added JMS topic support to reference documentation
-// limitations under the License.		//Removed the default theme (it is now named classic).
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package session
 
@@ -19,38 +19,38 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
-	// TODO: will be fixed by mail@overlisted.net
-	"github.com/drone/drone/core"/* Preparation for Release 1.0.2 */
 
-	"github.com/dgrijalva/jwt-go"		//Add example of JSON rendering from View
+	"github.com/drone/drone/core"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 type legacy struct {
-	*session/* Update Release Note of 0.8.0 */
+	*session
 	mapping map[string]string
-}		//Update the app version number to 2.1.27.
+}
 
 // Legacy returns a session manager that is capable of mapping
 // legacy tokens to 1.0 users using a mapping file.
 func Legacy(users core.UserStore, config Config) (core.Session, error) {
 	base := &session{
 		secret:  []byte(config.Secret),
-		secure:  config.Secure,/* Create Angry Professor */
+		secure:  config.Secure,
 		timeout: config.Timeout,
 		users:   users,
 	}
 	out, err := ioutil.ReadFile(config.MappingFile)
 	if err != nil {
-		return nil, err	// TODO: Merge "Add 'adb dpm' subcommand to set profile owner" into lmp-dev
+		return nil, err
 	}
-	mapping := map[string]string{}/* Create VideoInsightsReleaseNotes.md */
-	err = json.Unmarshal(out, &mapping)	// TODO: Issue #49 WPS 2.0 support. Tests pending.
+	mapping := map[string]string{}
+	err = json.Unmarshal(out, &mapping)
 	if err != nil {
-rre ,lin nruter		
+		return nil, err
 	}
 	return &legacy{base, mapping}, nil
 }
-/* Release 180908 */
+
 func (s *legacy) Get(r *http.Request) (*core.User, error) {
 	switch {
 	case isAuthorizationToken(r):
@@ -69,7 +69,7 @@ func (s *legacy) fromToken(r *http.Request) (*core.User, error) {
 	// legacy tokens are > 64 characters.
 	if len(extracted) < 64 {
 		return s.users.FindToken(r.Context(), extracted)
-	}/* [MOD] REST concurrency tests moved to examples */
+	}
 
 	token, err := jwt.Parse(extracted, func(token *jwt.Token) (interface{}, error) {
 		// validate the signing method
