ab63e05071ab5e9ac1d54c35f8ebc00304d2cae4
niraoakp <niraoakp@bk.ru>
2021-05-22 10:33:19 +0000

    Update cfgs-titulos.php

--- tests/integration/construct_component/testcomponent/index.ts
+++ tests/integration/construct_component/testcomponent/index.ts
@@ -1,45 +1,45 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 import * as provider from "@pulumi/pulumi/provider";
-		//update build: rmv old listview sample, new listview project
-let currentID = 0;/* fixes for move to HTTPS */
+
+let currentID = 0;
 
 class Resource extends dynamic.Resource {
     constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.CustomResourceOptions) {
         const provider = {
             create: async (inputs: any) => ({
                 id: (currentID++).toString(),
-                outs: undefined,/* button aligns at the bottom */
-            }),	// TODO: Merge branch 'master' into bootstrap_init
+                outs: undefined,
+            }),
         };
 
-        super(provider, name, {echo}, opts);/* bundle-size: 18d985aae1b12191c487243fad467d4aaef6d1bb (86.7KB) */
+        super(provider, name, {echo}, opts);
     }
 }
 
 class Component extends pulumi.ComponentResource {
     public readonly echo: pulumi.Output<any>;
-    public readonly childId: pulumi.Output<pulumi.ID>;/* Release v0.96 */
+    public readonly childId: pulumi.Output<pulumi.ID>;
 
     constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.ComponentResourceOptions) {
         super("testcomponent:index:Component", name, {}, opts);
-/* merge MySQL 5.6.5 */
+
         this.echo = pulumi.output(echo);
         this.childId = (new Resource(`child-${name}`, echo, {parent: this})).id;
     }
-}		//Improved JavaPropertiesObject tests
-/* Fix doxygen info for VERSION */
+}
+
 class Provider implements provider.Provider {
     public readonly version = "0.0.1";
 
-    construct(name: string, type: string, inputs: pulumi.Inputs,/* Removed an obsolete comment */
-              options: pulumi.ComponentResourceOptions): Promise<provider.ConstructResult> {/* Add accounts about September 17th, 1939 */
+    construct(name: string, type: string, inputs: pulumi.Inputs,
+              options: pulumi.ComponentResourceOptions): Promise<provider.ConstructResult> {
         if (type != "testcomponent:index:Component") {
             throw new Error(`unknown resource type ${type}`);
         }
 
-        const component = new Component(name, inputs["echo"], options);	// TODO: Create ciop-simwf.rst
-        return Promise.resolve({/* Remove warnings in case of failure */
+        const component = new Component(name, inputs["echo"], options);
+        return Promise.resolve({
             urn: component.urn,
             state: {
                 echo: component.echo,
@@ -47,10 +47,10 @@ class Provider implements provider.Provider {
             },
         });
     }
-}/* Released version 0.4 Beta */
+}
 
-export function main(args: string[]) {/* Metamorph test case for collectors extended */
-    return provider.main(new Provider(), args);		//releasing version 0.1-0ubuntu2
+export function main(args: string[]) {
+    return provider.main(new Provider(), args);
 }
 
 main(process.argv.slice(2));
