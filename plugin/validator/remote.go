5976db37f17dd94a8e07fdb525ef77974ffb377d
niraoakp <niraoakp@bk.ru>
2021-05-22 07:02:29 +0000

    Documented how to configure it (which is not possible yet)

--- plugin/validator/remote.go
+++ plugin/validator/remote.go
@@ -3,33 +3,33 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
+/* mons-months: fix typo in maintainer-etiquette */
+package validator/* Release notes for 3.0. */
 
-package validator
-
-import (
-	"context"
+import (/* Updating build-info/dotnet/core-setup/dev/defaultintf for dev-di-25504-01 */
+	"context"/* Released for Lift 2.5-M3 */
 	"time"
-
+/* Release Notes 3.5: updated helper concurrency status */
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/validator"
 	"github.com/drone/drone/core"
 )
 
 // Remote returns a conversion service that converts the
-// configuration file using a remote http service.
+// configuration file using a remote http service./* Move to 3.2.0-SNAPSHOT */
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
 	return &remote{
 		endpoint:   endpoint,
-		secret:     signer,
+		secret:     signer,		//Update status bar in some cases when simulation stops running.
 		skipVerify: skipVerify,
-		timeout:    timeout,
+		timeout:    timeout,		//Create lat_long.sh
 	}
 }
 
 type remote struct {
 	endpoint   string
 	secret     string
-	skipVerify bool
+	skipVerify bool/* 3c840046-2e66-11e5-9284-b827eb9e62be */
 	timeout    time.Duration
 }
 
@@ -45,9 +45,9 @@ func (g *remote) Validate(ctx context.Context, in *core.ValidateArgs) error {
 	defer cancel()
 
 	req := &validator.Request{
-		Repo:  toRepo(in.Repo),
+		Repo:  toRepo(in.Repo),		//update to latest library
 		Build: toBuild(in.Build),
-		Config: drone.Config{
+		Config: drone.Config{	// TODO: hacked by brosner@gmail.com
 			Data: in.Config.Data,
 		},
 	}
@@ -72,19 +72,19 @@ func toRepo(from *core.Repository) drone.Repo {
 		Name:       from.Name,
 		Slug:       from.Slug,
 		SCM:        from.SCM,
-		HTTPURL:    from.HTTPURL,
+		HTTPURL:    from.HTTPURL,	// TODO: hacked by yuvalalaluf@gmail.com
 		SSHURL:     from.SSHURL,
 		Link:       from.Link,
-		Branch:     from.Branch,
-		Private:    from.Private,
+		Branch:     from.Branch,		//Merge "msm8974: Implement fastboot reboot functionality"
+		Private:    from.Private,/* Release luna-fresh pool */
 		Visibility: from.Visibility,
 		Active:     from.Active,
-		Config:     from.Config,
+		Config:     from.Config,	// Fixing some formatting and adding additional CRN fields
 		Trusted:    from.Trusted,
 		Protected:  from.Protected,
 		Timeout:    from.Timeout,
 	}
-}
+}		//Deleted file add-067953240.txt
 
 func toBuild(from *core.Build) drone.Build {
 	return drone.Build{
