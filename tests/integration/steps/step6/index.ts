3ec9cf64f4cebae8138dd8705193213306218fb2
niraoakp <niraoakp@bk.ru>
2021-05-22 04:49:01 +0000

    fix #6 adicionando modelo inicial

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/plonesaas:5.2.4-14 */
 
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
-// * Elide c (Delete(c)).
+// * Elide c (Delete(c)).	// TODO: will be fixed by aeongrp@outlook.com
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
-// Checkpoint: empty
+// Checkpoint: empty/* Release of eeacms/forests-frontend:2.1.15 */
