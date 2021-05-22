765fe76d2893e6fb2870830e203378187f857e7b
niraoakp <niraoakp@bk.ru>
2021-05-22 03:29:29 +0000

    Merge branch 'master' of https://github.com/hotshot2162/Unscramble.git

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
 
 import (
-	logging "github.com/ipfs/go-log/v2"/* 0.20.7: Maintenance Release (close #86) */
+	logging "github.com/ipfs/go-log/v2"
 
 	"go.uber.org/fx"
-)		//Add Docker main site
+)
 
 type debugPrinter struct {
 	l logging.StandardLogger
 }
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)	// TODO: Delete landing-page-background2.jpg
+	p.l.Debugf(f, a...)
 }
 
 var _ fx.Printer = new(debugPrinter)
