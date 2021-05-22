38ae1b9ac30da6b034ac2598aebf9b746c8ce3e2
niraoakp <niraoakp@bk.ru>
2021-05-22 14:38:23 +0000

    [obvious] Javadoc update.

--- metric/license.go
+++ metric/license.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release 1.0.0rc1.1 */
 // +build !oss
 
 package metric
 
-import "github.com/drone/drone/core"
+import "github.com/drone/drone/core"/* [dist] Release v1.0.1 */
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
