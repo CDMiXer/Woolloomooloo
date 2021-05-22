0d6b0ad4c49d69d5358a67218ec379291cd18344
niraoakp <niraoakp@bk.ru>
2021-05-22 21:49:08 +0000

    Update tron_parse_incoming_guids.ps1

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;/* Bugfix: Refreshen des JSTrees bei Verschieben per Drag-and-Drop */
+using Pulumi;
 
-class ComponentArgs : Pulumi.ResourceArgs
+class ComponentArgs : Pulumi.ResourceArgs	// pip install --upgrade pip
 {
     [Input("echo")]
     public Input<object>? Echo { get; set; }
 }
 
-class Component : Pulumi.ComponentResource	// TODO: hacked by julia@jvns.ca
+class Component : Pulumi.ComponentResource
 {
-    [Output("echo")]/* Remove tracking pixel */
+    [Output("echo")]
     public Output<object> Echo { get; private set; } = null!;
 
     [Output("childId")]
-    public Output<string> ChildId { get; private set; } = null!;
+    public Output<string> ChildId { get; private set; } = null!;	// Fix sponsor mispelling
 
-    public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
+    public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)		//Working on ProxyChecker fragment
         : base("testcomponent:index:Component", name, args, opts, remote: true)
     {
     }
