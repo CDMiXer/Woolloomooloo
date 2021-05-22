feeeaca9d4e42699f81387ed00d14ab75b771f07
niraoakp <niraoakp@bk.ru>
2021-05-22 23:39:53 +0000

    Rename to bookmarks.

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash	// TODO: Rebuilt index with ShekharGH
 set -eu -o pipefail
-
-# order is important, "REPLACEME" -> "workflow"
-cat \/* Added bullet graph URL */
+	// Added location_header to request_helpers
+# order is important, "REPLACEME" -> "workflow"		//694a2326-2e5c-11e5-9284-b827eb9e62be
+cat \
     | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \		//Add workaround for Java version to Troubleshooting
-    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \/* preliminary support for volumes conversion. */
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
