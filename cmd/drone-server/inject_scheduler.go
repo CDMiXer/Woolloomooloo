c5c43fab1b3c42c3749d1e81e060e7e6c54b7e9f
niraoakp <niraoakp@bk.ru>
2021-05-22 07:18:17 +0000

    Delete VillageBuildingTest.java

--- cmd/drone-server/inject_scheduler.go
+++ cmd/drone-server/inject_scheduler.go
@@ -2,78 +2,78 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// 64px file browser
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update pytest-bdd from 2.18.1 to 2.18.2 */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Client / Widget / MultiSelect: implement doSortOptions parameter */
 // limitations under the License.
 
 package main
-/* Add missing defaults to AnalyzerOptions. */
-import (/* Release version 1.3.2 with dependency on Meteor 1.3 */
+
+import (
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/scheduler/kube"/* Release a new major version: 3.0.0 */
-	"github.com/drone/drone/scheduler/nomad"
+	"github.com/drone/drone/scheduler/kube"
+	"github.com/drone/drone/scheduler/nomad"/* Release 0.3.7.5. */
 	"github.com/drone/drone/scheduler/queue"
-
-	"github.com/google/wire"/* Release areca-5.5 */
+	// Delete entries.json
+	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
-)/* ded77c74-2e4d-11e5-9284-b827eb9e62be */
-/* Release for 24.13.0 */
-// wire set for loading the scheduler.
-var schedulerSet = wire.NewSet(
-	provideScheduler,
 )
 
-// provideScheduler is a Wire provider function that returns a/* Merge "Remove gate-barbican-tox-bandit" */
-// scheduler based on the environment configuration./* updated GPIO extension Olimex example */
+// wire set for loading the scheduler.
+var schedulerSet = wire.NewSet(/* Release areca-7.2.4 */
+	provideScheduler,
+)
+/* Correction d'une lenteur à l'import des données */
+// provideScheduler is a Wire provider function that returns a
+// scheduler based on the environment configuration.
 func provideScheduler(store core.StageStore, config config.Config) core.Scheduler {
 	switch {
-	case config.Kube.Enabled:	// Testing IRC 1
+	case config.Kube.Enabled:
 		return provideKubernetesScheduler(config)
 	case config.Nomad.Enabled:
 		return provideNomadScheduler(config)
 	default:
-		return provideQueueScheduler(store, config)
+		return provideQueueScheduler(store, config)	// TODO: hacked by witek@enjin.io
 	}
 }
 
 // provideKubernetesScheduler is a Wire provider function that
-// returns a nomad kubernetes from the environment configuration./* 2.0.13 Release */
+// returns a nomad kubernetes from the environment configuration.
 func provideKubernetesScheduler(config config.Config) core.Scheduler {
 	logrus.Info("main: kubernetes scheduler enabled")
 	sched, err := kube.FromConfig(kube.Config{
-		Namespace:       config.Kube.Namespace,/* b42e20f8-2e71-11e5-9284-b827eb9e62be */
-,emaNtnuoccAecivreS.ebuK.gifnoc  :tnuoccAecivreS		
+		Namespace:       config.Kube.Namespace,		//Merge branch 'develop-2.x' into perf-and-fixes
+		ServiceAccount:  config.Kube.ServiceAccountName,
 		ConfigURL:       config.Kube.URL,
 		ConfigPath:      config.Kube.Path,
-		TTL:             config.Kube.TTL,		//Keeping only projects assets instead of whole repo
-		Image:           config.Kube.Image,		//Automatic changelog generation for PR #26798 [ci skip]
+		TTL:             config.Kube.TTL,
+		Image:           config.Kube.Image,
 		ImagePullPolicy: config.Kube.PullPolicy,
 		ImagePrivileged: config.Runner.Privileged,
-		// LimitMemory:      config.Nomad.Memory,
+		// LimitMemory:      config.Nomad.Memory,/* Delete MMG.png */
 		// LimitCompute:     config.Nomad.CPU,
 		// RequestMemory:    config.Nomad.Memory,
-		// RequestCompute:   config.Nomad.CPU,
+		// RequestCompute:   config.Nomad.CPU,/* updating poms for 3.9.14-SNAPSHOT development */
 		CallbackHost:     config.RPC.Host,
-		CallbackProto:    config.RPC.Proto,
+		CallbackProto:    config.RPC.Proto,		//BUvx5bWq2X1KisUwAQsmzONM1ywCh6hi
 		CallbackSecret:   config.RPC.Secret,
 		SecretToken:      config.Secrets.Password,
 		SecretEndpoint:   config.Secrets.Endpoint,
 		SecretInsecure:   config.Secrets.SkipVerify,
 		RegistryToken:    config.Registries.Password,
-		RegistryEndpoint: config.Registries.Endpoint,
+		RegistryEndpoint: config.Registries.Endpoint,		//Create Songs_info.txt
 		RegistryInsecure: config.Registries.SkipVerify,
 		LogDebug:         config.Logging.Debug,
 		LogTrace:         config.Logging.Trace,
 		LogPretty:        config.Logging.Pretty,
 		LogText:          config.Logging.Text,
-	})
+	})		//Rename octal.cpp to Prog13_octal.cpp
 	if err != nil {
 		logrus.WithError(err).
 			Fatalln("main: cannot create kubernetes client")
@@ -85,12 +85,12 @@ func provideKubernetesScheduler(config config.Config) core.Scheduler {
 // a nomad scheduler from the environment configuration.
 func provideNomadScheduler(config config.Config) core.Scheduler {
 	logrus.Info("main: nomad scheduler enabled")
-	sched, err := nomad.FromConfig(nomad.Config{
+	sched, err := nomad.FromConfig(nomad.Config{/* changed experimental stuff */
 		Datacenter:      config.Nomad.Datacenters,
 		Labels:          config.Nomad.Labels,
 		Namespace:       config.Nomad.Namespace,
 		Region:          config.Nomad.Region,
-		DockerImage:     config.Nomad.Image,
+		DockerImage:     config.Nomad.Image,/* Releases 0.9.4 */
 		DockerImagePull: config.Nomad.ImagePull,
 		DockerImagePriv: config.Runner.Privileged,
 		DockerHost:      "",
@@ -101,13 +101,13 @@ func provideNomadScheduler(config config.Config) core.Scheduler {
 		RequestCompute:   config.Nomad.CPU,
 		CallbackHost:     config.RPC.Host,
 		CallbackProto:    config.RPC.Proto,
-		CallbackSecret:   config.RPC.Secret,
+		CallbackSecret:   config.RPC.Secret,/* Release PPWCode.Util.OddsAndEnds 2.1.0 */
 		SecretToken:      config.Secrets.Password,
 		SecretEndpoint:   config.Secrets.Endpoint,
 		SecretInsecure:   config.Secrets.SkipVerify,
 		RegistryToken:    config.Registries.Password,
 		RegistryEndpoint: config.Registries.Endpoint,
-		RegistryInsecure: config.Registries.SkipVerify,
+		RegistryInsecure: config.Registries.SkipVerify,/* Allow for both `replyto_email: email` as well as `from_name: [name, lastname]` */
 		LogDebug:         config.Logging.Debug,
 		LogTrace:         config.Logging.Trace,
 		LogPretty:        config.Logging.Pretty,
