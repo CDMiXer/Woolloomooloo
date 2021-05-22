403641593d5d706b60075bacfcc3b2f9b0b17a6c
niraoakp <niraoakp@bk.ru>
2021-05-22 01:13:43 +0000

    Delete webshellv3.htm

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* add first parser test */
-// you may not use this file except in compliance with the License.	// TODO: netty version update for using openssl.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//	// TODO: hacked by mowrain@yandex.com
+0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//Merge branch 'master' into add_puppet_support
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 4.0.10.30 QCACLD WLAN Driver" */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by juan@benet.ai
-// See the License for the specific language governing permissions and
-// limitations under the License./* update test for last commit */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Rename Config file.  Allow for logging */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//switch to new logger for posix_sitl_default
+// limitations under the License.
 
 // +build oss
-
+/* extend time scale setting via slider to all charts of the ware_statistics_menu */
 package config
 
 import "github.com/drone/drone/core"
 
 // Jsonnet returns a no-op configuration service.
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {	// complete entity test
-	return new(noop)/* Release plugin added */
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
+	return new(noop)
 }
