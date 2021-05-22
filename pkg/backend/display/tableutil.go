d3feff3892ddccde636bf1578a64f4ac0a4122bf
niraoakp <niraoakp@bk.ru>
2021-05-22 03:17:05 +0000

    Commit of new interactive example.

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -2,15 +2,15 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Align the two require in the README
-//	// TODO: Changed streams and config to not use resources
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Refactored main application logic into Cli module */
+// limitations under the License.
 
 package display
 
@@ -19,20 +19,20 @@ import (
 	"unicode/utf8"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: Merge branch 'vlc_logging_to_window'
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 func columnHeader(msg string) string {
 	return colors.Underline + colors.BrightBlue + msg + colors.Reset
 }
-/* Release 0.3.7.1 */
+
 func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {
-	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)/* nueva url de la bolsa de trabajo */
-	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)	// TODO: Create m2m.rb
+	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)
+	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)
 
 	// Place two spaces between all columns (except after the first column).  The first
 	// column already has a ": " so it doesn't need the extra space.
 	extraWhitespace += extraPadding
-/* Release 0.4.4 */
-	return strings.Repeat(" ", extraWhitespace)/* Create [HowTo] Opensubtitles.org subtitles register as a user.md */
-}	// TODO: Fix auto merged file + update release notes
+
+	return strings.Repeat(" ", extraWhitespace)
+}
