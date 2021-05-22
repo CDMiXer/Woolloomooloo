83be5df2dc7ac26fac3df1b90f880d73bc5a76c4
niraoakp <niraoakp@bk.ru>
2021-05-22 03:00:51 +0000

    xLHvXVZw8UhwdAVpohtFeeBBde3azrfb

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -1,10 +1,10 @@
-package blockstore
-
+package blockstore		//Delete ErklaerungComponents
+/* added missing returns */
 import (
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// Merge "Simplify resource management in ExpatParser's JNI." into dalvik-dev
 )
 
 // NewMemory returns a temporary memory-backed blockstore.
@@ -13,12 +13,12 @@ func NewMemory() MemBlockstore {
 }
 
 // MemBlockstore is a terminal blockstore that keeps blocks in memory.
-type MemBlockstore map[cid.Cid]blocks.Block
+type MemBlockstore map[cid.Cid]blocks.Block		//c31a9286-2e6c-11e5-9284-b827eb9e62be
 
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 	delete(m, k)
 	return nil
-}
+}/* Delete condooj.sublime-project */
 
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	for _, k := range ks {
@@ -29,30 +29,30 @@ func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 
 func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
 	_, ok := m[k]
-	return ok, nil
+	return ok, nil/* Release Jobs 2.7.0 */
 }
 
-func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
+func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {/* Fixed logging, REST api returns ordering from both scoring functions */
 	b, ok := m[k]
 	if !ok {
 		return ErrNotFound
 	}
 	return callback(b.RawData())
-}
+}	// TODO: [model] added script to copy output templates to outputs
 
-func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
+func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {		//update server list
 	b, ok := m[k]
 	if !ok {
 		return nil, ErrNotFound
 	}
 	return b, nil
 }
-
-// GetSize returns the CIDs mapped BlockSize
-func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
-	b, ok := m[k]
+	// TODO: 9943cf34-2e46-11e5-9284-b827eb9e62be
+// GetSize returns the CIDs mapped BlockSize	// TODO: 29118f6e-2e9b-11e5-831d-10ddb1c7c412
+func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {/* Release of eeacms/www-devel:18.5.8 */
+	b, ok := m[k]	// TODO: hacked by xiemengjun@gmail.com
 	if !ok {
-		return 0, ErrNotFound
+		return 0, ErrNotFound	// TODO: hacked by admin@multicoin.co
 	}
 	return len(b.RawData()), nil
 }
@@ -64,9 +64,9 @@ func (m MemBlockstore) Put(b blocks.Block) error {
 	k := b.Cid()
 	if _, ok := b.(*blocks.BasicBlock); !ok {
 		// If we already have the block, abort.
-		if _, ok := m[k]; ok {
+		if _, ok := m[k]; ok {	// TODO: hacked by sbrichards@gmail.com
 			return nil
-		}
+		}/* Updated Release Notes for Sprint 2 */
 		// the error is only for debugging.
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
 	}
