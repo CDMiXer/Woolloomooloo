d1517937ce9e48c4f03f3a2ee3878a5b7b1ac498
niraoakp <niraoakp@bk.ru>
2021-05-22 03:18:52 +0000

    Switch to ImmutableSet for implementing justifications + tweaks

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Create error.r
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./*  removing old content */
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly.
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent-4/* e3353aee-2e5d-11e5-9284-b827eb9e62be */
-//   2. DeleteReplacement Dependent		//Renamed the report html file 
+//   1. DeleteReplacement Dependent-4
+//   2. DeleteReplacement Dependent
 //   3. DeleteReplacement Base
-//   4. Replace Base/* Merge "Release 1.0.0.160 QCACLD WLAN Driver" */
+//   4. Replace Base
 //   5. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
@@ -17,11 +17,11 @@ const a = new Resource("base", { uniqueKey: 1, state: 99 });
 //   7. CreateReplacement Dependent
 const b = new Resource("dependent", { state: a.state });
 
-//   8. Update Dependent-2/* auto login in last login was OK */
+//   8. Update Dependent-2
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 //   9. Update Dependent-3
-const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });/* update ServerRelease task */
+const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });/* Cleanup lib/index */
 
 //  10. Replace Dependent-4
 //  11. CreateReplacement Dependent-4
