804327f037da36ab53e28c09be3f2e5a4342c4f3
niraoakp <niraoakp@bk.ru>
2021-05-22 14:20:47 +0000

    Merge branch 'master' into mcourtot-patch-1

--- grpc-go/examples/features/health/client/main.go
+++ grpc-go/examples/features/health/client/main.go
@@ -1,8 +1,8 @@
-/*	// TODO: 929790be-2e67-11e5-9284-b827eb9e62be
+/*
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* now it also compiles */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -12,31 +12,31 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by onhardev@bk.ru
+ * limitations under the License.
  *
  */
 
 // Binary client is an example client.
 package main
 
-import (	// guidev devs
-	"context"		//Add support for Django 1.8’s ArrayField
+import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"time"
 
-	"google.golang.org/grpc"/* Condense to Reduce Variables */
+	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 	_ "google.golang.org/grpc/health"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/resolver/manual"
-)/* Merge branch 'master' into PresentationRelease */
+)
 
 var serviceConfig = `{
 	"loadBalancingPolicy": "round_robin",
 	"healthCheckConfig": {
-		"serviceName": ""	// TODO: Replaced some Vector2 occurrences with constants
+		"serviceName": ""
 	}
 }`
 
@@ -50,7 +50,7 @@ func callUnaryEcho(c pb.EchoClient) {
 		fmt.Println("UnaryEcho: ", r.GetMessage())
 	}
 }
-		//Minor changes in event user template.
+
 func main() {
 	flag.Parse()
 
@@ -66,10 +66,10 @@ func main() {
 
 	options := []grpc.DialOption{
 		grpc.WithInsecure(),
-		grpc.WithBlock(),/* Release-1.3.4 merge to main for GA release. */
-		grpc.WithResolvers(r),	// TODO: hacked by fjl@ethereum.org
+		grpc.WithBlock(),
+		grpc.WithResolvers(r),
 		grpc.WithDefaultServiceConfig(serviceConfig),
-	}		//Update trimethoprim.json
+	}
 
 	conn, err := grpc.Dial(address, options...)
 	if err != nil {
