0b2fa62ddf9300f85b33b6b0c941288410a103a8
niraoakp <niraoakp@bk.ru>
2021-05-22 05:03:14 +0000

    RTSS: Lighting - there will never be specular input for per pixel lights

--- handler/api/repos/crons/find.go
+++ handler/api/repos/crons/find.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* sign added */
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Merge "msm: kgsl: Release process mutex appropriately to avoid deadlock" */
+
 package crons
 
-import (/* fix can't open plots when open ogr datasource directly */
-	"net/http"/* New translations bobrevamp.ini (Serbian (Cyrillic)) */
-/* Release of eeacms/www:20.5.26 */
-	"github.com/drone/drone/core"	// TODO: Fixed incorrect video link
+import (
+	"net/http"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
@@ -19,24 +19,24 @@ import (/* fix can't open plots when open ogr datasource directly */
 // cronjob details to the the response body.
 func HandleFind(
 	repos core.RepositoryStore,
-	crons core.CronStore,		//translated: How to install Arch Linux on VirtualBox
+	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// Added overlap_evaluation.xml
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
-		)	// TODO: will be fixed by nicksavers@gmail.com
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* [artifactory-release] Release version 0.7.13.RELEASE */
-			render.NotFound(w, err)
-			return		//Made loading screen 360
-		}
-		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)	// TODO: Fix factory code. (nw)
 		if err != nil {
 			render.NotFound(w, err)
-			return	// TODO: Fix iterator for empty results
-		}/*  Bug#12744991 - DECIMAL_ROUND(X,D) GIVES WRONG RESULTS WHEN D == N*(-9) */
+			return
+		}
+		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}
 		render.JSON(w, cronjob, 200)
-	}/* hello listener fix */
+	}
 }
