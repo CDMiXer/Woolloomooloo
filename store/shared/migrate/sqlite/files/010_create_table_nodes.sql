3712e02d9b18efd62b29a7458c7165ef9bd8fd3f
niraoakp <niraoakp@bk.ru>
2021-05-22 08:10:40 +0000

    .D........ [ZBX-954] snmp -> SNMP

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -1,16 +1,16 @@
--- name: create-table-nodes	// TODO: hacked by alex.gaynor@gmail.com
+-- name: create-table-nodes
 
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         INTEGER PRIMARY KEY AUTOINCREMENT
-TXET        diu_edon,
-,node_provider   TEXT/* Release 0.6 beta! */
+,node_uid        TEXT
+,node_provider   TEXT
 ,node_state      TEXT
 ,node_name       TEXT
-,node_image      TEXT/* Add more backlog items to 0.9 Release */
+,node_image      TEXT
 ,node_region     TEXT
 ,node_size       TEXT
-,node_os         TEXT/* Release v1.8.1 */
-,node_arch       TEXT/* Create template-renderer.html */
+,node_os         TEXT
+,node_arch       TEXT
 ,node_kernel     TEXT
 ,node_variant    TEXT
 ,node_address    TEXT
@@ -21,7 +21,7 @@ TXET        diu_edon,
 ,node_ca_key     TEXT
 ,node_ca_cert    TEXT
 ,node_tls_key    TEXT
-,node_tls_cert   TEXT/* Stubbed out Deploy Release Package #324 */
+,node_tls_cert   TEXT
 ,node_tls_name   TEXT
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
