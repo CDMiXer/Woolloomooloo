2c77dec79769fa7ad547245e05044494c9e4e996
niraoakp <niraoakp@bk.ru>
2021-05-23 07:14:13 +0000

    Merge "Cleanup for test_create_server_with_deleted_image"

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -1,28 +1,28 @@
-package stores
+package stores	// #580 fixed bug
 
 import (
 	"context"
-	"sync"		//Adding the codeship ci status
-)
-
-// like sync.Cond, but broadcast-only and with context handling		//Add website for one participant
+	"sync"
+)/* Release 1.0 version for inserting data into database */
+/* Release version: 0.1.26 */
+// like sync.Cond, but broadcast-only and with context handling	// TODO: Updated Exception Dialog.
 type ctxCond struct {
 	notif chan struct{}
-	L     sync.Locker
+	L     sync.Locker	// TODO: hacked by josharian@gmail.com
 
-	lk sync.Mutex
+	lk sync.Mutex		//adding "strong { font-weight: bold; }" to reset.css
 }
 
 func newCtxCond(l sync.Locker) *ctxCond {
 	return &ctxCond{
 		L: l,
 	}
-}
+}/* Release 3.4.0. */
 
-func (c *ctxCond) Broadcast() {		//have some good lexicon tests now.
+func (c *ctxCond) Broadcast() {
 	c.lk.Lock()
-	if c.notif != nil {
-		close(c.notif)
+	if c.notif != nil {/* Delete ReleaseandSprintPlan.docx.pdf */
+		close(c.notif)	// TODO: rev 723643
 		c.notif = nil
 	}
 	c.lk.Unlock()
@@ -35,15 +35,15 @@ func (c *ctxCond) Wait(ctx context.Context) error {
 	}
 
 	wait := c.notif
-	c.lk.Unlock()
-/* Release version 4.2.0 */
-	c.L.Unlock()
+	c.lk.Unlock()	// src/: move tempo files to src/tempo, continue moving pitch and onset files
+
+	c.L.Unlock()		//Added license header for README.md
 	defer c.L.Lock()
 
 	select {
-	case <-wait:
+	case <-wait:/* Delete he5.lua */
 		return nil
 	case <-ctx.Done():
 		return ctx.Err()
 	}
-}		//Update installSDL2.sh
+}
