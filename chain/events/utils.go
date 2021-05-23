501fa21c1ca63cd35ac6445833e24ce2db1a9f6f
niraoakp <niraoakp@bk.ru>
2021-05-23 10:47:45 +0000

    Fix project references

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,50 +1,50 @@
-package events	// TODO: hacked by arajasek94@gmail.com
+package events
 
 import (
 	"context"
-/* Release of eeacms/www-devel:18.2.19 */
+
 	"github.com/filecoin-project/lotus/chain/stmgr"
-/* Release: Making ready for next release cycle 4.5.1 */
+
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Core/Scripts: More missing includes */
-)
+	"github.com/filecoin-project/lotus/chain/types"
+)	// Remove content unrelated to this app from the README
 
-func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {		//Merge branch 'master' into dependabot/maven/org.mockito-mockito-core-2.22.0
+func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
 
 	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
-		if err != nil {		//Remove block dump
-			return false, true, err
-		}
-/* Merge "[FAB-2487] Restrict channelIDs to CouchDB/Kafka" */
+		if err != nil {
+			return false, true, err		//Update GoldilocksOnDocker.md
+}		
+
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
-			return false, true, nil
-		}	// 3ce55d75-2e9c-11e5-b6d3-a45e60cdfd11
+			return false, true, nil/* Released 3.2.0.RELEASE */
+		}
 
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
-		if err != nil {	// TODO: groovy script configuration are locatable (GRVY-95)
+		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
-		}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+		}
 
 		if ml == nil {
-))(thgieH.st ,st ,lin ,gsm(dnh = rre ,erom			
+			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
 
-		return true, more, err	// TODO: Implementação da funcionalidade de rotinas automatizadas.
+		return true, more, err
 	}
 }
 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
-		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
+		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {/* Force scaling of full graph to max_timeout. Not sure it does anything useful yet */
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
 		}
 
 		return inmsg.Equals(msg), nil
-	}	// TODO: hacked by boringland@protonmail.ch
+	}
 }
