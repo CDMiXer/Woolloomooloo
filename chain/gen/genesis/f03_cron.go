95d500e68b1c870ac484c1667aeb41447f0a640b
niraoakp <niraoakp@bk.ru>
2021-05-23 19:10:53 +0000

    [REF] 'sale_recovery_moment' improve moment view, displaying colors;

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -4,26 +4,26 @@ import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/cron"	// TODO: Fixes issue #1550
-	cbor "github.com/ipfs/go-ipld-cbor"
+	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
+	cbor "github.com/ipfs/go-ipld-cbor"/* Integrate with coverage */
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* 6d43775a-2e54-11e5-9284-b827eb9e62be */
 
-func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
+func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {/* Signed 2.2 Release Candidate */
 	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
-
-	stcid, err := cst.Put(context.TODO(), cas)	// TODO: trying pure pip
+	// TODO: use lablePreferredWidth as width 
+	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
-	}	// TODO: hacked by igor@soramitsu.co.jp
-/* chore: Update Semantic Release */
-	return &types.Actor{/* Add some empty lines */
+	}
+
+	return &types.Actor{	// TODO: hacked by zaq1tomo@gmail.com
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,
-		Nonce:   0,/* Merge "Remove 'grub2' option in creating whole-disk-images" */
+		Head:    stcid,		//Fixed latest builds URL
+		Nonce:   0,	// TODO: will be fixed by brosner@gmail.com
 		Balance: types.NewInt(0),
 	}, nil
 }
