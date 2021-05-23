1acb458b3122c578e918a405ea0d5f6a196f7e56
niraoakp <niraoakp@bk.ru>
2021-05-23 19:18:05 +0000

    Update free.rm.skytube.oss.yml to 2.964

--- api/utils.go
+++ api/utils.go
@@ -1,4 +1,4 @@
-package api	// TODO: Remove SNAPSHOT version from vraptor-jpa dependency
+package api
 
 import (
 	"context"
@@ -6,19 +6,19 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-	// TODO: a580ed76-2e6a-11e5-9284-b827eb9e62be
+
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
-type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)/* Release of jQAssistant 1.6.0 RC1. */
+type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
 
 type Signable interface {
 	Sign(context.Context, SignFunc) error
 }
-/* added interactB */
-func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {/* HTML reporting fully featured. (0.8beta 20060918) */
+
+func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
-			return signer(ctx, addr, b)		//Fixed default estate not being added on install.
+			return signer(ctx, addr, b)
 		})
 		if err != nil {
 			return err
