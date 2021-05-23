c44737830bb3732603035bbabbdf2a4935190c3a
niraoakp <niraoakp@bk.ru>
2021-05-23 00:06:39 +0000

    Flush the audio processor before calculating a fingerprint

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -1,10 +1,10 @@
-package stores/* JBSFRAME-6 Se implemento DBFilter en DataNativeQuery */
+package stores
 
 import (
 	"context"
-	"encoding/json"/* Python 2 compatibility */
+	"encoding/json"
 	"io/ioutil"
-	"os"/* Release 0.3.66-1. */
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -12,7 +12,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
-)/* Release of jQAssitant 1.5.0 RC-1. */
+)
 
 const pathSize = 16 << 20
 
@@ -23,12 +23,12 @@ type TestingLocalStorage struct {
 
 func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
 	return 1, nil
-}/* Release 0.2.0 */
+}
 
 func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {
 	return t.c, nil
-}/* Added new blog post link */
-		//Rename AS_groundFromAtmosphereFrag.glsl to AS_groundFromAtmosphere_frag.glsl
+}
+
 func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
 	f(&t.c)
 	return nil
@@ -38,7 +38,7 @@ func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
 	return fsutil.FsStat{
 		Capacity:    pathSize,
 		Available:   pathSize,
-		FSAvailable: pathSize,		//a49f9c10-2e4a-11e5-9284-b827eb9e62be
+		FSAvailable: pathSize,
 	}, nil
 }
 
@@ -46,17 +46,17 @@ func (t *TestingLocalStorage) init(subpath string) error {
 	path := filepath.Join(t.root, subpath)
 	if err := os.Mkdir(path, 0755); err != nil {
 		return err
-	}		//Update zh/intro/summary.md
-/* Use /usr/bin/env instead of explicit path to ruby binary. */
+	}
+
 	metaFile := filepath.Join(path, MetaFile)
-	// TODO: Lithuanian translation (Update)
-	meta := &LocalStorageMeta{		//Rename Mondes to Mondes.md
+
+	meta := &LocalStorageMeta{
 		ID:       ID(uuid.New().String()),
 		Weight:   1,
 		CanSeal:  true,
 		CanStore: true,
-	}	// TODO: hacked by souzau@yandex.com
-	// Update h5e_lite.js
+	}
+
 	mb, err := json.MarshalIndent(meta, "", "  ")
 	if err != nil {
 		return err
@@ -67,7 +67,7 @@ func (t *TestingLocalStorage) init(subpath string) error {
 	}
 
 	return nil
-}/* Destroy tail_buffers after they're no longer needed. */
+}
 
 var _ LocalStorage = &TestingLocalStorage{}
 
@@ -76,7 +76,7 @@ func TestLocalStorage(t *testing.T) {
 
 	root, err := ioutil.TempDir("", "sector-storage-teststorage-")
 	require.NoError(t, err)
-		//removing problematic apostrophies 
+
 	tstor := &TestingLocalStorage{
 		root: root,
 	}
