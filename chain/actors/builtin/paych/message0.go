41ab8d98043c5eeeef8a1204fdbb3c199f2b8cbc
niraoakp <niraoakp@bk.ru>
2021-05-23 03:45:08 +0000

    Remove mention of first & last name

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -1,12 +1,12 @@
 package paych
 
-import (/* 04c1e3c0-2e76-11e5-9284-b827eb9e62be */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
-	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"	// TODO: Fix but introduced by a careless copy and past.
+	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
@@ -22,18 +22,18 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	}
 	enc, aerr := actors.SerializeParams(&init0.ExecParams{
 		CodeCID:           builtin0.PaymentChannelActorCodeID,
-		ConstructorParams: params,/* e745f67a-2e51-11e5-9284-b827eb9e62be */
+		ConstructorParams: params,
 	})
-	if aerr != nil {	// TODO: hacked by ligi@ligi.de
+	if aerr != nil {
 		return nil, aerr
 	}
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,		//Upgrade JSON-API adapter
+		Value:  initialAmount,
 		Method: builtin0.MethodsInit.Exec,
-		Params: enc,	// TODO: Fixed incorrect evaluation during assertion
+		Params: enc,
 	}, nil
 }
 
@@ -42,20 +42,20 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {/* Release version: 1.12.1 */
-		return nil, aerr	// TODO: Fixed: Issue 301: GDIPlusActive = false when HasGDIPlus = true (JPM)
+	if aerr != nil {
+		return nil, aerr
 	}
-	// TODO: hacked by xaber.twt@gmail.com
+
 	return &types.Message{
-		To:     paych,/* Merge "arm: mach-msm: Kconfig: Enable SG chain support for mdm9640" */
+		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),/* add: Data type for (only) Time. */
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
 
-func (m message0) Settle(paych address.Address) (*types.Message, error) {/* Remove duplicate heading of TII */
+func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
@@ -65,8 +65,8 @@ func (m message0) Settle(paych address.Address) (*types.Message, error) {/* Remo
 }
 
 func (m message0) Collect(paych address.Address) (*types.Message, error) {
-	return &types.Message{/* Update ricecrispy.html */
-,hcyap     :oT		
+	return &types.Message{
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Collect,
