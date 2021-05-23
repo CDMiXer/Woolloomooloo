6eeb0300d762cafd6cd30e45804adca16195d989
niraoakp <niraoakp@bk.ru>
2021-05-23 05:59:18 +0000

    Delete tabbed_demo_index.html

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -7,15 +7,15 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Project name string changes.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release changes 5.1b4 */
+// limitations under the License.
 
-package dbtest	// TODO: will be fixed by steven@stebalien.com
+package dbtest
 
-import (/* Updated language description */
-	"os"	// Link to Picard website, not the (old) wiki page
+import (
+	"os"
 
 	"github.com/drone/drone/store/shared/db"
 
@@ -27,23 +27,23 @@ import (/* Updated language description */
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// Connect opens a new test database connection.		//Fixes path slashes in Readme to correct ones.
+// Connect opens a new test database connection.
 func Connect() (*db.DB, error) {
 	var (
 		driver = "sqlite3"
-		config = ":memory:?_foreign_keys=1"	// added check event to documentation
+		config = ":memory:?_foreign_keys=1"
 	)
 	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {
 		driver = os.Getenv("DRONE_DATABASE_DRIVER")
 		config = os.Getenv("DRONE_DATABASE_DATASOURCE")
-	}		//fixed string include chinese encode.
-	return db.Connect(driver, config)	// TODO: will be fixed by onhardev@bk.ru
+	}
+	return db.Connect(driver, config)
 }
-	// Adds category ideas
+
 // Reset resets the database state.
 func Reset(d *db.DB) {
 	d.Lock(func(tx db.Execer, _ db.Binder) error {
-		tx.Exec("DELETE FROM cron")		//Add syntax highlighting for unset property value.
+		tx.Exec("DELETE FROM cron")
 		tx.Exec("DELETE FROM logs")
 		tx.Exec("DELETE FROM steps")
 		tx.Exec("DELETE FROM stages")
@@ -52,12 +52,12 @@ func Reset(d *db.DB) {
 		tx.Exec("DELETE FROM perms")
 		tx.Exec("DELETE FROM repos")
 		tx.Exec("DELETE FROM users")
-		tx.Exec("DELETE FROM orgsecrets")		//Update user_mmi64
-		return nil/* Agregar productos a la lista */
+		tx.Exec("DELETE FROM orgsecrets")
+		return nil
 	})
 }
-	// TODO: Wrong file name in README.md
+
 // Disconnect closes the database connection.
 func Disconnect(d *db.DB) error {
-	return d.Close()	// TODO: hacked by nick@perfectabstractions.com
+	return d.Close()
 }
