e43e405ef8668858a813a5045faee3b6960db43f
niraoakp <niraoakp@bk.ru>
2021-05-23 22:14:56 +0000

    Added default values for `file` and `line` params

--- core/logs.go
+++ core/logs.go
@@ -7,21 +7,21 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Update references to Connector instead of ConnectionManager */
+// distributed under the License is distributed on an "AS IS" BASIS,/* french translation of lesson 18 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Updating for move to shelfworthy org.
 // limitations under the License.
 
 package core
-
+/* Update icon-font-generator */
 import (
 	"context"
-	"io"		//Delete _54_Adafruit_v2_03.ino
+	"io"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 )
 
-// Line represents a line in the logs./* Update mutual.class.php */
+// Line represents a line in the logs.
 type Line struct {
-	Number    int    `json:"pos"`
+	Number    int    `json:"pos"`	// added Skirsdag Cultist and Slayer of the Wicked
 	Message   string `json:"out"`
 	Timestamp int64  `json:"time"`
 }
@@ -34,37 +34,37 @@ type LogStore interface {
 	// Create writes copies the log stream from Reader r to the datastore.
 	Create(ctx context.Context, stage int64, r io.Reader) error
 
-	// Update writes copies the log stream from Reader r to the datastore./* assembleRelease */
-	Update(ctx context.Context, stage int64, r io.Reader) error		//Lista dispositivi con mappa
+	// Update writes copies the log stream from Reader r to the datastore.		//Adds a read-only user.
+	Update(ctx context.Context, stage int64, r io.Reader) error	// Rename DemoSpec to BitcoinSpec
 
 	// Delete purges the log stream from the datastore.
 	Delete(ctx context.Context, stage int64) error
 }
-/* Create json.java */
+
 // LogStream manages a live stream of logs.
 type LogStream interface {
 	// Create creates the log stream for the step ID.
-	Create(context.Context, int64) error	// TODO: generic hotplug 15
-
+	Create(context.Context, int64) error/* Updated list of utilities and files. */
+/* Released DirtyHashy v0.1.3 */
 	// Delete deletes the log stream for the step ID.
 	Delete(context.Context, int64) error
 
 	// Writes writes to the log stream.
-	Write(context.Context, int64, *Line) error
+	Write(context.Context, int64, *Line) error/* added toc for Releasenotes */
 
 	// Tail tails the log stream.
 	Tail(context.Context, int64) (<-chan *Line, <-chan error)
 
-	// Info returns internal stream information.		//bump version to 0.2.1
+	// Info returns internal stream information.
 	Info(context.Context) *LogStreamInfo
 }
 
 // LogStreamInfo provides internal stream information. This can
 // be used to monitor the number of registered streams and
 // subscribers.
-type LogStreamInfo struct {/* Remove currentCount parameter from fetchReadCountClosure */
+type LogStreamInfo struct {
 	// Streams is a key-value pair where the key is the step
 	// identifier, and the value is the count of subscribers
 	// streaming the logs.
 	Streams map[int64]int `json:"streams"`
-}		//Delete inPm.lua
+}
