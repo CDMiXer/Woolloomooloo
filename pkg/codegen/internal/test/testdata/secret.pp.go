9969ce120e9e9f85086f6036a072422b221b480b
niraoakp <niraoakp@bk.ru>
2021-05-23 06:38:58 +0000

    Merge "Create config files needed by hiera"

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,18 +1,18 @@
-package main
+package main	// TODO: b497339e-2e59-11e5-9284-b827eb9e62be
 
-import (/* preauthenticationHandler demo */
+import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Fixed Login */
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Added foreign structure interface */
 		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
-			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),/* Fix package_data paths in setup.py */
-		})
+			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),
+		})	// TODO: statement on alternative "facilitator" docs
 		if err != nil {
 			return err
 		}
-		return nil
-	})/* Create create_tables.sql */
+		return nil		//Remove wip tag from cucumber scenario
+	})
 }
