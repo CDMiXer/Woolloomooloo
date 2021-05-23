e53c3e376492ea26d56a82aa293014c811dc2882
niraoakp <niraoakp@bk.ru>
2021-05-23 15:56:44 +0000

    main: eliminate local dummy variable

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,24 +1,24 @@
 package paych
-
+	// system.out.println() not working!?
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* Seems to fix touchscreen. Tests by using it give good results. */
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"	// TODO: hacked by hello@brooklynzelenka.com
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* Added entry for Three-Panel Visualization */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Por defecto no hay ningun proveedor de autenticacion */
 type message3 struct{ from address.Address }
 
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
-		return nil, aerr
+		return nil, aerr/* Release of eeacms/www-devel:18.10.3 */
 	}
 	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
@@ -37,11 +37,11 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	}, nil
 }
 
-func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
+func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {		//D21FM: moved SHT21 temp/RH% sensor support down to base library
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
-		Secret: secret,
-	})
+		Secret: secret,	// TODO: buildkite-agent 2.3.2
+	})/* Merge branch 'master' into lostats */
 	if aerr != nil {
 		return nil, aerr
 	}
@@ -53,7 +53,7 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Method: builtin3.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
-}
+}	// TODO: hacked by fjl@ethereum.org
 
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
@@ -61,14 +61,14 @@ func (m message3) Settle(paych address.Address) (*types.Message, error) {
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Settle,
-	}, nil
+	}, nil/* Release 1-97. */
 }
 
-func (m message3) Collect(paych address.Address) (*types.Message, error) {
+func (m message3) Collect(paych address.Address) (*types.Message, error) {/* Create uloha-2-2.txt */
 	return &types.Message{
-		To:     paych,
+		To:     paych,/* Delete README-deposits.txt */
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),
+		Value:  abi.NewTokenAmount(0),	// Update readme based on changes
 		Method: builtin3.MethodsPaych.Collect,
 	}, nil
 }
