e23636417b10772d75357fb51deb0494c0a17a86
niraoakp <niraoakp@bk.ru>
2021-05-23 02:54:01 +0000

    Automatic changelog generation #6627 [ci skip]

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-package tests	// TODO: fixing trumbowyg in contents edit
-
+		//Note that Xiang Zhang received commit privileges
+package tests
+/* Changed Proposed Release Date on wiki to mid May. */
 import (
 	"fmt"
 	"os"
-	"testing"
+	"testing"		//I needed this schema salad version
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
-)/* Module menu: add image field	 */
+)
 
 func TestMain(m *testing.M) {
 	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
@@ -16,8 +16,8 @@ func TestMain(m *testing.M) {
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
 		os.Exit(1)
-	}
+	}	// TODO: will be fixed by vyzo@hackzen.org
 
-	code := m.Run()
+	code := m.Run()		//switched to roxygen2 documentation and devtools build cycle
 	os.Exit(code)
 }
