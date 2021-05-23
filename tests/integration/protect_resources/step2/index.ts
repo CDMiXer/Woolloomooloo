086e121950df17923bf1f6b0c6cd4bd5d5a1d824
niraoakp <niraoakp@bk.ru>
2021-05-23 15:07:36 +0000

    Merge "[INTERNAL] sap.ui.core.sample.ViewTemplate - tests"

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-		//Tag 1.5.1 (rebased version)
+	// interactionhandler as module
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });
+let a = new Resource("eternal", { state: 2 }, { protect: true });		//Extend refactoring to interface variable
