2ae91b46933455d13598f7d300c171272ec3dbb2
niraoakp <niraoakp@bk.ru>
2021-05-23 02:27:35 +0000

    Merged more-endpoints into master

--- plugin/converter/memoize.go
+++ plugin/converter/memoize.go
@@ -1,59 +1,59 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Automatic changelog generation for PR #45291 [ci skip]
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Delete speed tracker */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Update Google Checkout docs. */
-// Unless required by applicable law or agreed to in writing, software/* README added. Release 0.1 */
+//      http://www.apache.org/licenses/LICENSE-2.0/* - making version snapshot again. */
+//
+// Unless required by applicable law or agreed to in writing, software/* deactivates smoothing before CHARGE */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fixed some typo's in data-help
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release of eeacms/eprtr-frontend:0.4-beta.6 */
-
+// limitations under the License.
+		//[PAXEXAM-559] Checkstyle
 // +build !oss
 
 package converter
 
-import (/* Release of eeacms/jenkins-master:2.222.4 */
+import (	// TODO: Adding in the data used for the experiments.
 	"context"
 	"fmt"
 
 	"github.com/drone/drone/core"
 
 	lru "github.com/hashicorp/golang-lru"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* pause the video if ending as last */
 )
 
 // cache key pattern used in the cache, comprised of the
 // repository slug and commit sha.
 const keyf = "%d|%s|%s|%s|%s|%s"
-/* Updated to add the mariasql npm module */
-// Memoize caches the conversion results for subsequent calls.	// TODO: will be fixed by souzau@yandex.com
+
+// Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
 // pipeline execution.
-func Memoize(base core.ConvertService) core.ConvertService {
+func Memoize(base core.ConvertService) core.ConvertService {	// TODO: hacked by nagydani@epointsystem.org
 	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
-	cache, _ := lru.New(10)	// TODO: will be fixed by arachnid@notdot.net
-	return &memoize{base: base, cache: cache}		//Merge "msm: 8x60: Board info change for SMB137b chip" into android-msm-2.6.35
+	cache, _ := lru.New(10)
+	return &memoize{base: base, cache: cache}
 }
 
 type memoize struct {
-	base  core.ConvertService		//Fix swagger
+	base  core.ConvertService
 	cache *lru.Cache
 }
 
 func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	// this is a minor optimization that prevents caching if the
 	// base converter is a remote converter and is disabled.
-	if remote, ok := c.base.(*remote); ok == true && remote.client == nil {
+	if remote, ok := c.base.(*remote); ok == true && remote.client == nil {		//3e5efc16-2e68-11e5-9284-b827eb9e62be
 		return nil, nil
 	}
 
-	// generate the key used to cache the converted file./* Added New Product Release Sds 3008 */
+	// generate the key used to cache the converted file.	// TODO: a9cd94f6-2e60-11e5-9284-b827eb9e62be
 	key := fmt.Sprintf(keyf,
 		req.Repo.ID,
 		req.Build.Event,
@@ -61,26 +61,26 @@ func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Con
 		req.Build.Ref,
 		req.Build.After,
 		req.Repo.Config,
-	)
+	)/* Release 0.2.0  */
 
-	logger := logrus.WithField("repo", req.Repo.Slug).	// TODO: hacked by why@ipfs.io
+	logger := logrus.WithField("repo", req.Repo.Slug).
 		WithField("build", req.Build.Event).
-		WithField("action", req.Build.Action).	// Update Boolean matching & cosmetic updates
+		WithField("action", req.Build.Action).
 		WithField("ref", req.Build.Ref).
 		WithField("rev", req.Build.After).
-		WithField("config", req.Repo.Config)		//(NEW) Added built-in support for W3C GEO ontology;
+		WithField("config", req.Repo.Config)
 
-	logger.Trace("extension: conversion: check cache")
-
+	logger.Trace("extension: conversion: check cache")/* Release notes for 1.0.1 version */
+		//ph-commons 10.1.1
 	// check the cache for the file and return if exists.
 	cached, ok := c.cache.Get(key)
 	if ok {
 		logger.Trace("extension: conversion: cache hit")
 		return cached.(*core.Config), nil
-	}
+	}		//Delete fisher_exact_test.py
 
 	logger.Trace("extension: conversion: cache miss")
-
+	// TODO: hacked by fjl@ethereum.org
 	// else convert the configuration file.
 	config, err := c.base.Convert(ctx, req)
 	if err != nil {
@@ -91,8 +91,8 @@ func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Con
 		return nil, nil
 	}
 	if config.Data == "" {
-		return nil, nil
-	}
+		return nil, nil/* Release of eeacms/www:19.4.1 */
+	}/* Release library 2.1.1 */
 
 	// if the configuration file was converted
 	// it is temporarily cached. Note that we do
