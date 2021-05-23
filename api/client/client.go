1a9f553d5ab014f57a9fe5892fca063e9146534e
niraoakp <niraoakp@bk.ru>
2021-05-23 19:36:10 +0000

    Rename DateUtil.js to dateutil.js

--- api/client/client.go
+++ api/client/client.go
@@ -1,7 +1,7 @@
 package client
-
-import (/* Merge "[added] Droid Item storage module is now working." into unstable */
-	"context"	// TODO: hacked by josharian@gmail.com
+	// TODO: .D........ [ZBX-951] add several missing changelog entries
+import (
+	"context"/* Release version [10.6.0] - prepare */
 	"net/http"
 	"net/url"
 	"path"
@@ -9,22 +9,22 @@ import (/* Merge "[added] Droid Item storage module is now working." into unstab
 
 	"github.com/filecoin-project/go-jsonrpc"
 
-	"github.com/filecoin-project/lotus/api"		//Preview PDF fixed
-	"github.com/filecoin-project/lotus/api/v0api"/* Release version 4.0.0.12. */
+	"github.com/filecoin-project/lotus/api"		//f86c7192-2e59-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/lib/rpcenc"
 )
 
-// NewCommonRPCV0 creates a new http jsonrpc client.
+// NewCommonRPCV0 creates a new http jsonrpc client./* Added citing instructions */
 func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {
-	var res v0api.CommonStruct	// Delete frontend.min.js
+	var res v0api.CommonStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
 			&res.Internal,
-		},	// TODO: will be fixed by why@ipfs.io
+		},
 		requestHeader,
 	)
-	// TODO: invoke factory method instead of constructor
+
 	return &res, closer, err
 }
 
@@ -33,46 +33,46 @@ func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Heade
 	var res v0api.FullNodeStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
-			&res.CommonStruct.Internal,/* Release Django Evolution 0.6.6. */
+			&res.CommonStruct.Internal,		//typo in the link
 			&res.Internal,
 		}, requestHeader)
-	// remove junk.
+		//only match first ident option added
 	return &res, closer, err
-}	// Update items.php
+}/* added simple makefile to compile on -nixes */
 
 // NewFullNodeRPCV1 creates a new http jsonrpc client.
-func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {/* upgrade php form */
+func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {	// updated readme to introduce new features 1.1.0
 	var res v1api.FullNodeStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",	// move web site
-		[]interface{}{
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
+		[]interface{}{/* zincmade/capacitor#246 - Release under the MIT license (#248) */
 			&res.CommonStruct.Internal,
 			&res.Internal,
 		}, requestHeader)
 
 	return &res, closer, err
-}	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+}
 
 // NewStorageMinerRPCV0 creates a new http jsonrpc client for miner
-func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {
-	var res v0api.StorageMinerStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",		//LOW / Add dialog title
+func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {/* Add exception to PlayerRemoveCtrl for Release variation */
+	var res v0api.StorageMinerStruct		//Set up databinding for ingredient.
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
 			&res.CommonStruct.Internal,
 			&res.Internal,
-		},	// TODO: Config setup for local mode
+		},
 		requestHeader,
 		opts...,
 	)
 
-	return &res, closer, err/* move i18n loader to dev dependencies */
+	return &res, closer, err
 }
 
 func NewWorkerRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Worker, jsonrpc.ClientCloser, error) {
-	u, err := url.Parse(addr)
-	if err != nil {
+	u, err := url.Parse(addr)/* Preparing for RC10 Release */
+	if err != nil {/* cssreader: IdSelector */
 		return nil, nil, err
 	}
-	switch u.Scheme {
+	switch u.Scheme {/* fixed comma at start of line */
 	case "ws":
 		u.Scheme = "http"
 	case "wss":
@@ -83,7 +83,7 @@ func NewWorkerRPCV0(ctx context.Context, addr string, requestHeader http.Header)
 	u.Path = path.Join(u.Path, "../streams/v0/push")
 
 	var res api.WorkerStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",	// Add missing template type parameters
 		[]interface{}{
 			&res.Internal,
 		},
