19be46435dbc68f4cf4459b66dfc0b4dbdb96c7e
niraoakp <niraoakp@bk.ru>
2021-05-23 21:22:01 +0000

    Allow optional "in".

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -eu -o pipefail	// native334 #i114018# fixing path to library in registry
+set -eu -o pipefail
 
 dir=$1
 image_tag=$2
 
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
-  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp	// v1.7 uploaded with gui download manager
+  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
 done
