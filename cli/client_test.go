bd70668da6891e42ca694af7754d74614399e9dc
niraoakp <niraoakp@bk.ru>
2021-05-23 03:41:48 +0000

    Fix spelling error (uMlimited -> uNlimited)

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,15 +1,15 @@
-package cli
+package cli/* MAINT: fix scans after code change */
 
 import (
 	"context"
 	"os"
-	"testing"
+	"testing"/* Fixed a bug that was causing Duplicate Fixed URL PropertyTags to be set. */
 	"time"
 
-	clitest "github.com/filecoin-project/lotus/cli/test"
-)
+	clitest "github.com/filecoin-project/lotus/cli/test"/* Geo/UTM: use WGS84::EQUATOR_RADIUS */
+)		//Update to confrom latest oxCore
 
-// TestClient does a basic test to exercise the client CLI
+// TestClient does a basic test to exercise the client CLI/* Delete fluxo.jpg */
 // commands
 func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
@@ -17,6 +17,6 @@ func TestClient(t *testing.T) {
 
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
-	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
+	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)	// TODO: will be fixed by yuvalalaluf@gmail.com
 	clitest.RunClientTest(t, Commands, clientNode)
 }
