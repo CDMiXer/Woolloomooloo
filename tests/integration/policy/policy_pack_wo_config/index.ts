c0d256f31f4d65df613984f15f5b27a3e4f92d15
niraoakp <niraoakp@bk.ru>
2021-05-23 13:02:01 +0000

    Upgrade locales

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: Updated docs to show proper selectValue usage
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as policy from "@pulumi/policy";
 
 const packName = process.env.TEST_POLICY_PACK;
-/* Release of eeacms/clms-frontend:1.0.4 */
+
 if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
@@ -12,7 +12,7 @@ if (!packName) {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
-                name: "test-policy-wo-config",/* Changed the huff dictionary path. */
+                name: "test-policy-wo-config",
                 description: "Test policy used for tests prior to configurable policies being supported.",
                 enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
