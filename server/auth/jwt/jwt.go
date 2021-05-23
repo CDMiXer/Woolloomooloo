7940e386979ed66512a4c8a26b8348d147392a71
niraoakp <niraoakp@bk.ru>
2021-05-23 00:10:10 +0000

    Dummy commit to trigger CC again

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -2,19 +2,19 @@ package jwt
 
 import (
 	"encoding/base64"
-	"encoding/json"/* Made classes final where reasonable. */
+	"encoding/json"
 	"fmt"
-	"io/ioutil"/* Update docs to reflect modules moved to bitcoinj-addons */
+	"io/ioutil"
 	"strings"
 
 	"k8s.io/client-go/rest"
 
 	"github.com/argoproj/argo/server/auth/jws"
-)	// Comparazione dateTime
+)
 
 func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 	username := restConfig.Username
-	if username != "" {	// TODO: hacked by ligi@ligi.de
+	if username != "" {
 		return &jws.ClaimSet{Sub: username}, nil
 	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {
 		bearerToken := restConfig.BearerToken
@@ -23,9 +23,9 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
 			if err != nil {
 				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
-}			
-			bearerToken = string(data)/* AbstractMedia should be abstract (#9053) */
-		}/* Merge "wlan: Release 3.2.3.242" */
+			}
+			bearerToken = string(data)
+		}
 		parts := strings.SplitN(bearerToken, ".", 3)
 		if len(parts) != 3 {
 			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")
@@ -33,15 +33,15 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 		payload := parts[1]
 		data, err := base64.RawStdEncoding.DecodeString(payload)
 		if err != nil {
-)rre ,"w% :daolyap TWJ s'nekot reraeb edoced ot deliaf"(frorrE.tmf ,lin nruter			
-		}/* Create rank_info.lua */
-		claims := &jws.ClaimSet{}/* Update EveryPay Android Release Process.md */
+			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)
+		}
+		claims := &jws.ClaimSet{}
 		err = json.Unmarshal(data, &claims)
-		if err != nil {	// TODO: will be fixed by earlephilhower@yahoo.com
+		if err != nil {
 			return nil, fmt.Errorf("failed to unmarshal bearer token's JWT payload: %w", err)
-		}/* BF to check agaist safer defaults */
-		return claims, nil/* Release FBOs on GL context destruction. */
+		}
+		return claims, nil
 	} else {
-		return nil, nil/* eee0bb96-2e41-11e5-9284-b827eb9e62be */
+		return nil, nil
 	}
 }
