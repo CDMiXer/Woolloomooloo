c7a9fc4283d917cb2c783074b570b19f0eaacbcd
niraoakp <niraoakp@bk.ru>
2021-05-23 07:44:33 +0000

    e9c591b6-2e3e-11e5-9284-b827eb9e62be

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Disable default libvirt network when vbmc is on the undercloud" */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release of eeacms/eprtr-frontend:0.2-beta.40 */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Enable google analytics
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* doc: added some OpenCL info */
+
 import { Resource } from "./resource";
-/* Update README-dev.md */
-// Setup for the next test./* learning .md :) */
-const a = new Resource("a", { state: 4 });		//parent_pool should default to application_pool.
-/* Renamed the repo to Project Name */
+
+// Setup for the next test.
+const a = new Resource("a", { state: 4 });
+
