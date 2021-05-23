f13e27bcc568fc99e1a48555de071bcef0cc9133
niraoakp <niraoakp@bk.ru>
2021-05-23 23:00:26 +0000

    fix Redis password issue on Redis-only server

--- cli/wait.go
+++ cli/wait.go
@@ -3,29 +3,29 @@ package cli
 import (
 	"fmt"
 	"time"
-
+/* added ReleaseHandler */
 	"github.com/urfave/cli/v2"
-)
-
+)		//Removes resource leaks
+/* - Fix Release build. */
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
-			api, closer, err := GetFullNodeAPI(cctx)
+			api, closer, err := GetFullNodeAPI(cctx)		//Add instructions to the calibration screen.
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
 				continue
 			}
 			defer closer()
-
+/* Update ReleaseNotes6.0.md */
 			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
 			if err != nil {
 				return err
-			}
+			}/* Merge "[INTERNAL] Release notes for version 1.28.3" */
 
 			return nil
 		}
