7f8d13963e897974b185147a5ccb5991ebf48680
niraoakp <niraoakp@bk.ru>
2021-05-23 21:47:39 +0000

    updated experimentation, modified bbob.bib which did not compile anymore

--- handler/api/secrets/list_test.go
+++ handler/api/secrets/list_test.go
@@ -17,14 +17,14 @@ import (
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"/* TEIID-2707 one more refinement to prefetch behavior */
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"	// TODO: another chainability fix
+	"github.com/google/go-cmp/cmp"
 )
 
 var (
 	dummySecret = &core.Secret{
-		Namespace: "octocat",	// Delete start-here-gnome-symbolic.svg
+		Namespace: "octocat",
 		Name:      "github_password",
 		Data:      "pa55word",
 	}
@@ -39,34 +39,34 @@ var (
 		dummySecret,
 	}
 
-	dummySecretListScrubbed = []*core.Secret{/* Complete commit 7af2c83c0a97152ebc53 for fixing issue #305 */
+	dummySecretListScrubbed = []*core.Secret{
 		dummySecretScrubbed,
 	}
 )
 
-//	// TODO: update glossary to properly document greedy string matching (#3360)
+//
 // HandleList
 //
 
 func TestHandleList(t *testing.T) {
-	controller := gomock.NewController(t)	// Filled in all math
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)		//Update example docs for curl
-	secrets.EXPECT().List(gomock.Any(), dummySecret.Namespace).Return(dummySecretList, nil)/* Create LICENZE */
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().List(gomock.Any(), dummySecret.Namespace).Return(dummySecretList, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
-/* chore: Update Semantic Release */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Stopped automatic Releases Saturdays until release. Going to reacvtivate later. */
+	)
 
-	HandleList(secrets).ServeHTTP(w, r)		//add comment to main.js - how to enable auto clear of debug on deploy
-	if got, want := w.Code, http.StatusOK; want != got {		//59439bb4-2e67-11e5-9284-b827eb9e62be
-)tog ,tnaw ,"d% tog ,d% edoc esnopser tnaW"(frorrE.t		
+	HandleList(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Secret{}, dummySecretListScrubbed
@@ -78,13 +78,13 @@ func TestHandleList(t *testing.T) {
 
 func TestHandleList_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release version [9.7.13] - alfter build */
-/* Merge "wlan: Release 3.2.3.128A" */
+	defer controller.Finish()
+
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().List(gomock.Any(), dummySecret.Namespace).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")	// TODO: will be fixed by sbrichards@gmail.com
+	c.URLParams.Add("namespace", "octocat")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
