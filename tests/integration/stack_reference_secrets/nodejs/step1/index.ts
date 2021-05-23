eecc495a739aebeffee841602663e500963a64f3
niraoakp <niraoakp@bk.ru>
2021-05-23 14:17:00 +0000

    optim sparse

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-/* 0ae51482-2e41-11e5-9284-b827eb9e62be */
-export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");
-/* Merge branch 'master' into add-company-number-to-footer */
+
+export const normal = pulumi.output("normal");	// d824e1c8-2e66-11e5-9284-b827eb9e62be
+export const secret = pulumi.secret("secret");		//Update to provide a shortcut of the project
+		//Merge "Persist group cache by uuid"
