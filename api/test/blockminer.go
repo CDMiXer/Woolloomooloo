e53aec2b664b3b44df3bca42958a16989e9e06ab
niraoakp <niraoakp@bk.ru>
2021-05-23 12:17:20 +0000

    don't crash if dialog can't be dismissed

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -1,52 +1,52 @@
-package test
+package test	// TODO: will be fixed by xaber.twt@gmail.com
 
 import (
-	"context"	// TODO: Updating build-info/dotnet/corefx/master for preview.18563.5
-	"fmt"/* Release version 0.12. */
+	"context"
+	"fmt"/* Fixed formatting of Release Historiy in README */
 	"sync/atomic"
 	"testing"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"	// cb947956-2e6a-11e5-9284-b827eb9e62be
-	"github.com/filecoin-project/lotus/miner"/* Release version 0.6. */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/miner"
 )
-
+/* RUSP Release 1.0 (FTP and ECHO sample network applications) */
 type BlockMiner struct {
-	ctx       context.Context/* Merge "diag: Release wake source properly" */
+	ctx       context.Context
 	t         *testing.T
-	miner     TestStorageNode	// TODO: will be fixed by mowrain@yandex.com
+	miner     TestStorageNode
 	blocktime time.Duration
 	mine      int64
-	nulls     int64	// Criando arquivo da Ciencia de Dados
-	done      chan struct{}		//Serializable entity value support / new javassist version
+	nulls     int64
+	done      chan struct{}/* Merge "Release Notes 6.0 -- Mellanox issues" */
 }
 
-func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
-	return &BlockMiner{
+func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {	// Added engineApplyShaderToModel and engineRemoveShaderFromModel
+	return &BlockMiner{	// group update added
 		ctx:       ctx,
 		t:         t,
 		miner:     miner,
 		blocktime: blocktime,
 		mine:      int64(1),
-		done:      make(chan struct{}),
-	}		//237d5b00-2e3f-11e5-9284-b827eb9e62be
+		done:      make(chan struct{}),/* ae1c9a42-2e57-11e5-9284-b827eb9e62be */
+	}
 }
 
 func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
 	go func() {
 		defer close(bm.done)
-		for atomic.LoadInt64(&bm.mine) == 1 {	// TODO: rails generate cucumber:install
+		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
 			case <-bm.ctx.Done():
 				return
-			case <-time.After(bm.blocktime):		//7a559e0c-2e52-11e5-9284-b827eb9e62be
-			}
+			case <-time.After(bm.blocktime):		//Form for the creation of an Aggregate and an Alternate
+			}/* Remove extra console log */
 
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
 				InjectNulls: abi.ChainEpoch(nulls),
-				Done:        func(bool, abi.ChainEpoch, error) {},/* use cryptgenrandom under windows */
+				Done:        func(bool, abi.ChainEpoch, error) {},
 			}); err != nil {
 				bm.t.Error(err)
 			}
@@ -55,7 +55,7 @@ func (bm *BlockMiner) MineBlocks() {
 }
 
 func (bm *BlockMiner) Stop() {
-	atomic.AddInt64(&bm.mine, -1)/* Release 14.0.0 */
+	atomic.AddInt64(&bm.mine, -1)
 	fmt.Println("shutting down mining")
 	<-bm.done
 }
