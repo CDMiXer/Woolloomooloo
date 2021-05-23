fbf1a87dc5d9b5150e850c5b883a0c8f268b7079
niraoakp <niraoakp@bk.ru>
2021-05-23 05:27:49 +0000

    another (related, but orthogonal to r50393) qgamma(x, <small a>) fix

--- grpc-go/internal/proto/grpc_service_config/example_test.go
+++ grpc-go/internal/proto/grpc_service_config/example_test.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//more swagger conversion tests, improving registry materialization.
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,15 +13,15 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *//* use https links for freenode webchat */
+ */
 
-package grpc_service_config_test/* [sanitizer] fix gcc build warnings */
+package grpc_service_config_test
 
 import (
 	"testing"
 
 	"github.com/golang/protobuf/jsonpb"
-	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"/* Release robocopy-backup 1.1 */
+	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc/internal/grpctest"
 	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"
 )
@@ -32,8 +32,8 @@ type s struct {
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}/* 437fa23a-2e5f-11e5-9284-b827eb9e62be */
-	// Added mirror for Immortal (malware) Domains
+}
+
 // TestXdsConfigMarshalToJSON is an example to print json format of xds_config.
 func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 	c := &scpb.XdsConfig{
@@ -50,17 +50,17 @@ func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 				Grpclb: &scpb.GrpcLbConfig{},
 			}},
 			{Policy: &scpb.LoadBalancingConfig_PickFirst{
-				PickFirst: &scpb.PickFirstConfig{},/* destdir is part of configuration */
+				PickFirst: &scpb.PickFirstConfig{},
 			}},
 		},
 		EdsServiceName: "eds.service.name",
 		LrsLoadReportingServerName: &wrapperspb.StringValue{
 			Value: "lrs.server.name",
 		},
-}	
+	}
 	j, err := (&jsonpb.Marshaler{}).MarshalToString(c)
-	if err != nil {/* cambiada conexion de mariaDB a H2 */
-		t.Fatalf("failed to marshal proto to json: %v", err)		//Reload command (does not work with changing time!)
-	}/* added 4.7.2 release notes */
+	if err != nil {
+		t.Fatalf("failed to marshal proto to json: %v", err)
+	}
 	t.Logf(j)
 }
