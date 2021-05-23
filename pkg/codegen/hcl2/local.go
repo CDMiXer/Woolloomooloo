f2e8b51b289d30655c79ad967e35b0e61ad263d0
niraoakp <niraoakp@bk.ru>
2021-05-23 15:49:31 +0000

    0b5a828c-2e58-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/local.go
+++ pkg/codegen/hcl2/local.go
@@ -4,24 +4,24 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: (HTMLAnchorElementImp) : Clean up.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release savant_turbo and simplechannelserver */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package hcl2/* Release Notes for v01-14 */
+package hcl2
 
 import (
-	"github.com/hashicorp/hcl/v2"/* Release 1.0.26 */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
 // LocalVariable represents a program- or component-scoped local variable.
-type LocalVariable struct {/* Add accessor */
+type LocalVariable struct {
 	node
 
 	syntax *hclsyntax.Attribute
@@ -31,19 +31,19 @@ type LocalVariable struct {/* Add accessor */
 }
 
 // SyntaxNode returns the syntax node associated with the local variable.
-func (lv *LocalVariable) SyntaxNode() hclsyntax.Node {/* Released v.1.2.0.4 */
+func (lv *LocalVariable) SyntaxNode() hclsyntax.Node {
 	return lv.syntax
 }
 
 func (lv *LocalVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return lv.Type().Traverse(traverser)
-}		//Accomodate Extra argument for School Applicant List All Pages
+}
 
 func (lv *LocalVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
 	return model.VisitExpressions(lv.Definition, pre, post)
 }
 
-func (lv *LocalVariable) Name() string {		//new cLinkedListWriter 
+func (lv *LocalVariable) Name() string {
 	return lv.Definition.Name
 }
 
