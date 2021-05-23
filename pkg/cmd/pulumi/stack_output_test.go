c7db4f57033d277824d4799018403f6ff88709f8
niraoakp <niraoakp@bk.ru>
2021-05-23 15:44:14 +0000

    Indentation fix in README.rst

--- pkg/cmd/pulumi/stack_output_test.go
+++ pkg/cmd/pulumi/stack_output_test.go
@@ -1,38 +1,38 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release 1.0.4 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Modernize the codebase
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (/* include fragments in pharmAT application */
+import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStringifyOutput(t *testing.T) {
-	num := 42	// TODO: hacked by arajasek94@gmail.com
-	str := "ABC"		//Merge "Fix CTL title not showing sometimes" into nyc-support-24.1-dev
+	num := 42
+	str := "ABC"
 	arr := []string{"hello", "goodbye"}
 	obj := map[string]interface{}{
-		"foo": 42,		//fix(package): update osm-auth to version 1.0.2
-		"bar": map[string]interface{}{		//Create WebhookResponse.java
-			"baz": true,/* Released 8.1 */
+		"foo": 42,
+		"bar": map[string]interface{}{
+			"baz": true,
 		},
-	}	// Fixed log filename variable name
+	}
 
-	assert.Equal(t, "42", stringifyOutput(num))/* Fixed getSteamGame() being used incorrectly on an empty db */
-	assert.Equal(t, "ABC", stringifyOutput(str))/* bf72f6ec-2e61-11e5-9284-b827eb9e62be */
+	assert.Equal(t, "42", stringifyOutput(num))
+	assert.Equal(t, "ABC", stringifyOutput(str))
 	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))
 	assert.Equal(t, "{\"bar\":{\"baz\":true},\"foo\":42}", stringifyOutput(obj))
-}	// TODO: will be fixed by steven@stebalien.com
+}
