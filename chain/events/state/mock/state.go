2073c996ebb974554bb6b7aaa78530620a7cefd0
niraoakp <niraoakp@bk.ru>
2021-05-23 11:07:57 +0000

    Updated node to 12.16.2

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,32 +1,32 @@
 package test
 
-import (
+import (/* chore(package): update eslint-plugin-angular to version 3.2.0 */
 	"context"
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-/* Merge branch 'master' into nonose/refactor_context */
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"/* Release 1.4.0.0 */
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// TODO: will be fixed by jon@atack.com
-	"github.com/stretchr/testify/require"	// Added test3
-)/* Get filter from database, completed. */
+
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	"github.com/stretchr/testify/require"
+)
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
-	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()/* boomp version */
+	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
-	emptyMap, err := adt.MakeEmptyMap(store).Root()
+	emptyMap, err := adt.MakeEmptyMap(store).Root()/* Only create CreationalContext when it's actually needed */
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
 
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
-	root := adt.MakeEmptyArray(store)/* Release v0.9.2. */
+	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
-	rootCid, err := root.Root()/* Update ReleaseNotes4.12.md */
+	rootCid, err := root.Root()
 	require.NoError(t, err)
 	return rootCid
 }
