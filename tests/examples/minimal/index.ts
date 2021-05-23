badd7546bbbea546d766f6511d78b66fd4cca36b
niraoakp <niraoakp@bk.ru>
2021-05-23 01:04:44 +0000

    revise XmlHelper

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Ajout Tuber candidum */
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config();
-console.log(`Hello, ${config.require("name")}!`);/* Merge "Remove autoescape from Soy templates" */
-console.log(`Psst, ${config.require("secret")}`);	// TODO: cf027c5c-2fbc-11e5-b64f-64700227155b
-
+console.log(`Hello, ${config.require("name")}!`);
+console.log(`Psst, ${config.require("secret")}`);
+	// TODO: hacked by nagydani@epointsystem.org
