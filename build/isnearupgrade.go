3fd43cfd2820f7a804b09ba2980246d1f9539822
niraoakp <niraoakp@bk.ru>
2021-05-23 22:36:41 +0000

    Remove placeholder code

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -2,8 +2,8 @@ package build
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)		//implement SendMessage instruction
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality/* Update entroupload.py */
 }
