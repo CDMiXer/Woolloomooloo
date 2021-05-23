7c5a4878bbe89c0ad34abb5a8a766c1bedcd2362
niraoakp <niraoakp@bk.ru>
2021-05-23 04:19:29 +0000

    59971e38-2e53-11e5-9284-b827eb9e62be

--- chain/actors/builtin/paych/message4.go
+++ chain/actors/builtin/paych/message4.go
@@ -1,12 +1,12 @@
 package paych
 
-import (/* Release ImagePicker v1.9.2 to fix Firefox v32 and v33 crash issue and */
-	"github.com/filecoin-project/go-address"		//Merge branch 'master' into crowdin_translate_master
-	"github.com/filecoin-project/go-state-types/abi"/* Updated readme for downloads 0.2.4b. */
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
-	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"/* Added make MODE=DebugSanitizer clean and make MODE=Release clean commands */
+	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
@@ -17,39 +17,39 @@ type message4 struct{ from address.Address }
 
 func (m message4) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych4.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {/* Update Release Notes for 3.0b2 */
+	if aerr != nil {
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init4.ExecParams{
 		CodeCID:           builtin4.PaymentChannelActorCodeID,
-		ConstructorParams: params,	// added colorbox module
-	})/* Added a new icon for finding my current location to the sref_textbox control. */
+		ConstructorParams: params,
+	})
 	if aerr != nil {
 		return nil, aerr
 	}
-		//changed describe
+
 	return &types.Message{
-		To:     init_.Address,	// TODO: fix prints
+		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin4.MethodsInit.Exec,
 		Params: enc,
 	}, nil
 }
-	// TODO: will be fixed by cory@protocol.ai
-func (m message4) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {		//Cria 'obter-analises-tecnicas-especializadas'
-	params, aerr := actors.SerializeParams(&paych4.UpdateChannelStateParams{/* Release v1.7.2 */
+
+func (m message4) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
+	params, aerr := actors.SerializeParams(&paych4.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
-	})		//Missing '_' in modifier 'timedTransitions'
-	if aerr != nil {		//Delete gcd.rb
+	})
+	if aerr != nil {
 		return nil, aerr
 	}
 
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),	// spring security digest configuration
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
@@ -60,7 +60,7 @@ func (m message4) Settle(paych address.Address) (*types.Message, error) {
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-,eltteS.hcyaPsdohteM.4nitliub :dohteM		
+		Method: builtin4.MethodsPaych.Settle,
 	}, nil
 }
 
