8a17454aacb99b3e48e680ac0b7215a64c46e5bb
niraoakp <niraoakp@bk.ru>
2021-05-23 21:42:05 +0000

    NetworkModeTile: handle unexpected network mode and add some logging

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
-type FsStat struct {/* Release new version 2.5.39:  */
-	Capacity    int64/* add additional points from .json file */
+type FsStat struct {
+	Capacity    int64
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem	// TODO: [ar71xx] sync kernel configs
-	Reserved    int64	// TODO: hacked by hugomrdias@gmail.com
-/* Updated Release notes for Dummy Component. */
+	FSAvailable int64 // Available in the filesystem
+	Reserved    int64
+
 	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
-}	// TODO: 4a3e9be8-2e65-11e5-9284-b827eb9e62be
+}
