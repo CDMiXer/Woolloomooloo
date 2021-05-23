82c7aceb2124b77e4eaa8f20bdb7b34bc68b1524
niraoakp <niraoakp@bk.ru>
2021-05-23 09:47:00 +0000

    move convert_to_int_or_float to SortedSetCommandsMixin

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -4,23 +4,23 @@
 // +build !oss
 
 package rpc2
-		//custom comparator
-// Copyright 2019 Drone.IO Inc. All rights reserved./* test(use-allergy, use-patient-note): restore console.error after tests */
+
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 import (
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/operator/manager"	// TODO: hacked by josharian@gmail.com
-)	// TODO: hacked by why@ipfs.io
+	"github.com/drone/drone/operator/manager"
+)
 
 // details provides the runner with the build details and
 // includes all environment data required to execute the build.
 type details struct {
-	*manager.Context/* Update ReleaseNotes-6.2.2 */
+	*manager.Context
 	Netrc *core.Netrc `json:"netrc"`
 	Repo  *repositroy `json:"repository"`
 }
 
 // repository wraps a repository object to include the secret
-// when the repository is marshaled to json.	// Update CHANGELOG for #10435
+// when the repository is marshaled to json.
 type repositroy struct {
 	*core.Repository
 	Secret string `json:"secret"`
