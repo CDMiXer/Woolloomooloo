6e6e9a1b79251cdd340ffc90c383b6fff9c676d1
niraoakp <niraoakp@bk.ru>
2021-05-23 06:24:31 +0000

    Update Integrations/AWS-CloudTrail/CHANGELOG.md

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Issue 36: new Query Editor (used by Trac Connector)
+//	// Changes related to core refactoring form #1168
+// Licensed under the Apache License, Version 2.0 (the "License");/* created internal packages for server plugin */
+// you may not use this file except in compliance with the License./* Release 1.34 */
+// You may obtain a copy of the License at
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//changed 15.04 to 15.10
-// You may obtain a copy of the License at	// TODO: Fix parsing of ms and ns time strings.
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by jon@atack.com
+///* sighs in powershell environment variables */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Report bad variable indices more usefully
-package b64/* gitweb: Fixed parent/child links when viewing a file revision. */
 
-import (/* Upgrade to DKPro Core 1.5.0 and uimaFIT 2.0.0 */
+package b64
+
+import (
 	"encoding/base64"
 
-	"github.com/pulumi/pulumi/pkg/v2/secrets"	// TODO: Increased nlink buffer size to support x64 platform
+	"github.com/pulumi/pulumi/pkg/v2/secrets"		//Merge "fixed config checker for falsy values"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
@@ -32,18 +32,18 @@ type manager struct{}
 
 func (m *manager) Type() string                         { return Type }
 func (m *manager) State() interface{}                   { return map[string]string{} }
-func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }	// TODO: will be fixed by steven@stebalien.com
-func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }
+func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
+func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }/* show train weight in loco tab */
 
 type base64Crypter struct{}
 
-func (c *base64Crypter) EncryptValue(s string) (string, error) {	// Update and rename 001 - Idea to 001 - People
+func (c *base64Crypter) EncryptValue(s string) (string, error) {
 	return base64.StdEncoding.EncodeToString([]byte(s)), nil
 }
 func (c *base64Crypter) DecryptValue(s string) (string, error) {
 	b, err := base64.StdEncoding.DecodeString(s)
-	if err != nil {	// TODO: will be fixed by sjors@sprovoost.nl
+	if err != nil {	// TODO: Standard fix
 		return "", err
 	}
-	return string(b), nil	// Añadida cabecera HTTP
-}/* added quick change combat set to FS, too, removed some debug code */
+	return string(b), nil
+}
