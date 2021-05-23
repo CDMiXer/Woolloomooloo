bf70850e10f51c50f126e08e88240a6a9c52cf63
niraoakp <niraoakp@bk.ru>
2021-05-23 09:24:54 +0000

    Fix spelling of "dashboard" in new_relic_deploy

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: 8b34ac2e-35c6-11e5-85ff-6c40088e03e4
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Fix: makedev not declared on gcc8
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updating ChangeLog For 0.57 Alpha 2 Dev Release */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
