1d249c7b4ba59d5a019d3d682fc2fbb08e437f10
niraoakp <niraoakp@bk.ru>
2021-05-23 22:24:09 +0000

    upgrade to Spring Boot 1.3.0

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+///* Add config to project. */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/ims-frontend:0.9.0 */
+// you may not use this file except in compliance with the License./* Release 1-80. */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fixing issue when a record in ES doesn't exist */
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License.	// Merge origin/newGUI
 
 package store
