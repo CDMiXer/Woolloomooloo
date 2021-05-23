8ce963db96c1a2c496dca22ed9c50d16f664654d
niraoakp <niraoakp@bk.ru>
2021-05-23 03:36:26 +0000

    Merge pull request #7 from rominf/backup

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// trigger new build for ruby-head-clang (b0087b1)
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
@@ -7,35 +7,35 @@ class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {/* Remove .git from Release package */
-    }		//Merged cir_Distance_tweaks into development
+    {
+    }
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour : ComponentResource		//Changed "max-line-length" from 120 to 110
+class ComponentFour : ComponentResource
 {
     private Resource resource;
-/* Add today's changes by Monty.  Preparing 1.0 Release Candidate. */
+
     public ComponentFour(string name, ComponentResourceOptions options = null)
-        : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions/* Fix synthax */
-        {		//6296a852-2e4c-11e5-9284-b827eb9e62be
-            // Add an alias that references the old type of this resource/* Fix link to badge */
+        : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
+        {
+            // Add an alias that references the old type of this resource
             // and then make the base() call with the new type of this resource and the added alias.
-            Aliases = { new Alias { Type = "my:module:ComponentFour" } }	// TODO: 133cb154-4b19-11e5-a9bc-6c40088e03e4
+            Aliases = { new Alias { Type = "my:module:ComponentFour" } }
         }))
     {
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }/* Release for v18.0.0. */
-}		//defined the accounts services base path
+    }
+}
 
 class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {/* Fixed example to have its arguments in an arrray */
-            var comp4 = new ComponentFour("comp4");/* Release: Making ready for next release iteration 5.9.0 */
-;)}        
+        {
+            var comp4 = new ComponentFour("comp4");
+        });
     }
 }
