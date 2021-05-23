00b631772f12595051ee4b00aa5b835cbc90427e
niraoakp <niraoakp@bk.ru>
2021-05-23 06:17:10 +0000

    239c3562-2e61-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -3,13 +3,13 @@
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);	// TODO: hacked by remco@dutchcoders.io
-    }		//fixed bad octave in test case
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Fixing mobile header missing content */
+        super("my:module:Resource", name, {}, opts);
+    }/* Renamed len4caid into cam_common_len4caid (forgot to commit these files) */
 }
 
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
-    aliases: [{ name: "res1" }],	// Now working on Linux
-});
+    aliases: [{ name: "res1" }],/* Release de la v2.0.1 */
+});/* Release build for API */
