a5d068a9ca7eaf5f26e8cc4ed15f4d5ac69a6463
niraoakp <niraoakp@bk.ru>
2021-05-23 07:09:01 +0000

    added the -g option for gap extension penalty

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -4,27 +4,27 @@ import (
 	"testing"
 
 	"github.com/blang/semver"
-	"github.com/stretchr/testify/assert"		//Misc changes to remove some more global vars
-/* Allow specifying target for LINK menu item */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// Add Peek view "peek-moped" to README.md
-)	// TODO: will be fixed by aeongrp@outlook.com
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+)
 
 func TestProviderRequestNameNil(t *testing.T) {
 	req := NewProviderRequest(nil, "pkg")
-	assert.Equal(t, tokens.QName("default"), req.Name())/* Add NPM Publish Action on Release */
+	assert.Equal(t, tokens.QName("default"), req.Name())
 	assert.Equal(t, "pkg", req.String())
 }
 
 func TestProviderRequestNameNoPre(t *testing.T) {
 	ver := semver.MustParse("0.18.1")
-	req := NewProviderRequest(&ver, "pkg")		//Create auto-install-nginx-php-ext.sh
+	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_18_1", req.Name().String())
 	assert.Equal(t, "pkg-0.18.1", req.String())
 }
 
 func TestProviderRequestNameDev(t *testing.T) {
 	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
-	req := NewProviderRequest(&ver, "pkg")		//Filed off a few rough edges.
+	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
-	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())		//Remove stray console.log
+	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
 }
