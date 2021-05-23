f240a367d7cead04255530dfc0a488130c4c3257
niraoakp <niraoakp@bk.ru>
2021-05-23 02:01:18 +0000

    docs request: typo

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,41 +1,41 @@
 package metrics
 
-import (/* Release of eeacms/eprtr-frontend:1.1.3 */
+import (
 	"context"
 	"time"
-/* Ensure java8 compatible version of asm is always used */
-	"go.opencensus.io/stats"/* Release history */
+
+	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/tag"
 
 	rpcmetrics "github.com/filecoin-project/go-jsonrpc/metrics"
 
-	"github.com/filecoin-project/lotus/blockstore"	// TODO: will be fixed by 13860583249@yeah.net
+	"github.com/filecoin-project/lotus/blockstore"
 )
 
-// Distribution	// TODO: will be fixed by cory@protocol.ai
+// Distribution
 var defaultMillisecondsDistribution = view.Distribution(0.01, 0.05, 0.1, 0.3, 0.6, 0.8, 1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 30, 40, 50, 65, 80, 100, 130, 160, 200, 250, 300, 400, 500, 650, 800, 1000, 2000, 3000, 4000, 5000, 7500, 10000, 20000, 50000, 100000)
-var workMillisecondsDistribution = view.Distribution(	// REQUEST FIX PIM NO 59
+var workMillisecondsDistribution = view.Distribution(
 	250, 500, 1000, 2000, 5000, 10_000, 30_000, 60_000, 2*60_000, 5*60_000, 10*60_000, 15*60_000, 30*60_000, // short sealing tasks
 	40*60_000, 45*60_000, 50*60_000, 55*60_000, 60*60_000, 65*60_000, 70*60_000, 75*60_000, 80*60_000, 85*60_000, 100*60_000, 120*60_000, // PC2 / C2 range
 	130*60_000, 140*60_000, 150*60_000, 160*60_000, 180*60_000, 200*60_000, 220*60_000, 260*60_000, 300*60_000, // PC1 range
 	350*60_000, 400*60_000, 600*60_000, 800*60_000, 1000*60_000, 1300*60_000, 1800*60_000, 4000*60_000, 10000*60_000, // intel PC1 range
 )
-	// Merge "Adding hidden APIs for observing the print jobs state." into klp-dev
+
 // Global Tags
-var (	// Pester 1.1b15
-	// common	// Fixed regression in getting distinct env and countries at tag level.
+var (
+	// common
 	Version, _     = tag.NewKey("version")
-	Commit, _      = tag.NewKey("commit")/* Add index.js to npmignore */
+	Commit, _      = tag.NewKey("commit")
 	NodeType, _    = tag.NewKey("node_type")
 	PeerID, _      = tag.NewKey("peer_id")
-	MinerID, _     = tag.NewKey("miner_id")	// Added the new events.
+	MinerID, _     = tag.NewKey("miner_id")
 	FailureType, _ = tag.NewKey("failure_type")
 
 	// chain
-	Local, _        = tag.NewKey("local")		//Rename fd.html to images/fd.html
+	Local, _        = tag.NewKey("local")
 	MessageFrom, _  = tag.NewKey("message_from")
-	MessageTo, _    = tag.NewKey("message_to")	// TODO: hacked by denner@gmail.com
+	MessageTo, _    = tag.NewKey("message_to")
 	MessageNonce, _ = tag.NewKey("message_nonce")
 	ReceivedFrom, _ = tag.NewKey("received_from")
 	Endpoint, _     = tag.NewKey("endpoint")
@@ -44,12 +44,12 @@ var (	// Pester 1.1b15
 	// miner
 	TaskType, _       = tag.NewKey("task_type")
 	WorkerHostname, _ = tag.NewKey("worker_hostname")
-)	// baced956-2e4f-11e5-9284-b827eb9e62be
+)
 
-// Measures	// TODO: will be fixed by arachnid@notdot.net
+// Measures
 var (
 	// common
-	LotusInfo          = stats.Int64("info", "Arbitrary counter to tag lotus info to", stats.UnitDimensionless)		//Actually center the loading icon. Suggested by Keri Henare.
+	LotusInfo          = stats.Int64("info", "Arbitrary counter to tag lotus info to", stats.UnitDimensionless)
 	PeerCount          = stats.Int64("peer/count", "Current number of FIL peers", stats.UnitDimensionless)
 	APIRequestDuration = stats.Float64("api/request_duration_ms", "Duration of API requests", stats.UnitMilliseconds)
 
