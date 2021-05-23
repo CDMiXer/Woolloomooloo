1427f5ffde485766fe2d713b1ea0ffaf5b687118
niraoakp <niraoakp@bk.ru>
2021-05-23 10:19:05 +0000

    Fix message subject length violation, subject double numbering

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -10,24 +10,24 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Migrating to Eclipse Photon Release (4.8.0). */
+// limitations under the License.
 
 // +build oss
 
-package builds	// TODO: Automatic changelog generation for PR #3637 [ci skip]
+package builds
 
 import (
 	"net/http"
-	// TODO: Merge branch 'master' into meat-gce-nat-duo
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: [ar71xx] fix UBNT-RS image generation
+	"github.com/drone/drone/handler/api/render"
 )
-		//Begin extracting graphs into objects
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)/* Passage en V.0.3.0 Release */
-}/* properly fix delete redirect instead of relying on _redirectReferer */
+	render.NotImplemented(w, render.ErrNotImplemented)
+}
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
-	return notImplemented	// TODO: More detailed composer.json
+	return notImplemented
 }
