9435a5432f306ebd2b80a5059db2c1bd58c664f0
niraoakp <niraoakp@bk.ru>
2021-05-23 03:08:37 +0000

    Merge branch 'problem-1'

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -3,7 +3,7 @@ package adt
 import (
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by fjl@ethereum.org
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 )
 
@@ -12,18 +12,18 @@ type Map interface {
 
 	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
-	Delete(k abi.Keyer) error	// remove not comments
+	Delete(k abi.Keyer) error
 
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
-}		//Delete AFINN-README.txt
+}
 
 type Array interface {
 	Root() (cid.Cid, error)
-/* ci: set Python 3.7 wheel name properly */
-	Set(idx uint64, v cbor.Marshaler) error/* Switched to CMAKE Release/Debug system */
+
+	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
 	Length() uint64
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
-}	// TODO: Death Done Right (TM)
+}
