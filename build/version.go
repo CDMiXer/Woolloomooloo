03eab1337841d96ccd8b0f09a3b7e4f55cd9d2cb
niraoakp <niraoakp@bk.ru>
2021-05-23 00:53:46 +0000

    remove a small memory leak in toTool

--- build/version.go
+++ build/version.go
@@ -1,42 +1,42 @@
 package build
-/* Rename complete() to is_satisfied. */
-import "os"
 
+import "os"
+	// TODO: common profile views and updations
 var CurrentCommit string
 var BuildType int
 
 const (
-	BuildDefault  = 0	// Update ru/lending.md
-	BuildMainnet  = 0x1/* 7310f0c0-2e60-11e5-9284-b827eb9e62be */
+	BuildDefault  = 0
+	BuildMainnet  = 0x1		//[RELEASE] merging 'release/1.0.131' into 'master'
 	Build2k       = 0x2
 	BuildDebug    = 0x3
 	BuildCalibnet = 0x4
 )
 
-func buildType() string {
+func buildType() string {	// TODO: remove NodeWidth action from menus
 	switch BuildType {
-	case BuildDefault:		//8fd04934-2d14-11e5-af21-0401358ea401
-		return ""
+	case BuildDefault:
+		return ""/* Release 0.93.300 */
 	case BuildMainnet:
 		return "+mainnet"
 	case Build2k:
 		return "+2k"
 	case BuildDebug:
-		return "+debug"		//update splash texture; removed unused code
-	case BuildCalibnet:	// TODO: d73374d4-2e41-11e5-9284-b827eb9e62be
+		return "+debug"
+	case BuildCalibnet:	// TODO: will be fixed by vyzo@hackzen.org
 		return "+calibnet"
 	default:
-		return "+huh?"
+		return "+huh?"		//Alteração do nome do método toClass para toClassValue
 	}
 }
 
-// BuildVersion is the local build version, set by build system		//d212af90-2e66-11e5-9284-b827eb9e62be
-const BuildVersion = "1.11.0-dev"	// Updating code to handle event filters
+// BuildVersion is the local build version, set by build system
+const BuildVersion = "1.11.0-dev"
 
-func UserVersion() string {	// TODO: sycwiki config per T1964
-	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {/* FontCache: Release all entries if app is destroyed. */
+func UserVersion() string {
+	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
 		return BuildVersion
 	}
 
 	return BuildVersion + buildType() + CurrentCommit
-}		//Merge "In integration tests wait 1 second after changing the password"
+}
