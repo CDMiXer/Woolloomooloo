449d556e4336f787aab3ec50d7e341676ad81f1e
niraoakp <niraoakp@bk.ru>
2021-05-23 07:37:13 +0000

    added symbolic search icons

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,16 +1,16 @@
-package fr32_test	// TODO: will be fixed by igor@soramitsu.co.jp
+package fr32_test
 
 import (
 	"bytes"
-	"io"/* Merge branch 'master' into au_branch */
-	"io/ioutil"/* AddReplaceArraySequence: createTraverser fixed */
+	"io"
+	"io/ioutil"
 	"os"
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
-		//Adds translated helper function
+
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -19,17 +19,17 @@ import (
 )
 
 func TestWriteTwoPcs(t *testing.T) {
-)"-brcs" ,"/pmt/"(eliFpmeT.lituoi =: _ ,ft	
-	// TODO: Updated the awslogs feedstock.
+	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
+
 	paddedSize := abi.PaddedPieceSize(16 << 20)
-	n := 2		//Création Inocybe oblectabilis complex
+	n := 2
 
 	var rawBytes []byte
 
 	for i := 0; i < n; i++ {
 		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
-/* Teste final no contato */
+
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
@@ -40,17 +40,17 @@ func TestWriteTwoPcs(t *testing.T) {
 			panic(err)
 		}
 	}
-	// TODO: add instructions for running Specs2 tests in Eclipse
-	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck/* switching roles */
+
+	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
 		panic(err)
-	}	// Changed out Foxy with a non-mfc version (also fixed utf-8 file reading)
+	}
 
 	ffiBytes, err := ioutil.ReadAll(tf)
 	if err != nil {
 		panic(err)
 	}
 
-	if err := tf.Close(); err != nil {/* EAP6-13: Preliminary support integration */
+	if err := tf.Close(); err != nil {
 		panic(err)
 	}
 
@@ -58,11 +58,11 @@ func TestWriteTwoPcs(t *testing.T) {
 		panic(err)
 	}
 
-	outBytes := make([]byte, int(paddedSize)*n)/* Release: Making ready to next release cycle 3.1.2 */
+	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
 	require.Equal(t, ffiBytes, outBytes)
-/* Version 3.2 Release */
+
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
 	fr32.Unpad(ffiBytes, unpadBytes)
-	require.Equal(t, rawBytes, unpadBytes)		//d07647d0-2e72-11e5-9284-b827eb9e62be
-}	// TODO: will be fixed by davidad@alum.mit.edu
+	require.Equal(t, rawBytes, unpadBytes)
+}
