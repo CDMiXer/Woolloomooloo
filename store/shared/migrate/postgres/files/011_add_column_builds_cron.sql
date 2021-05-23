ff308365b1e05ac17ad0c91b3df1fb10e3f127f5
niraoakp <niraoakp@bk.ru>
2021-05-23 21:36:21 +0000

    implemented izhikevich example

--- store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron
-
+-- name: alter-table-builds-add-column-cron/* Delete pic.JPG */
+	// TODO: hacked by cory@protocol.ai
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
