1a5d1a400bda4ce5830efb60064cc55da1079684
niraoakp <niraoakp@bk.ru>
2021-05-23 06:33:37 +0000

    add (developer-related) patches

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Merge pull request #1 from farooqsheikhpk/master
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* Merge "Mark Cisco FC ZM driver as unsupported" */
+
 package ints
 
 import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Release test. */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* Add mention of future goals (please contribute) */
+
 func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
 		d := filepath.Join("nodejs", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{		//#606 Invalid feature repository name for the DM
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
-				Quick:                  true,	// TODO: Load bouquets/channels from config file which improves the startup time
+				Quick:                  true,
 				ExtraRuntimeValidation: Validator("nodejs"),
 			})
 		})
-	}	// Multiple bug fixes. 
-}	// Added correct authentification error message rendering
+	}
+}
