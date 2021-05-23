8c79773d1777c818878aa01aab8afde507a7b006
niraoakp <niraoakp@bk.ru>
2021-05-23 10:53:10 +0000

    FIX: removed most of the old tests

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -2,38 +2,38 @@ package rpcenc
 
 import (
 	"context"
-	"io"/* added a screenshot and updated readme.md */
+	"io"
 	"io/ioutil"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
-	"github.com/gorilla/mux"	// TODO: chatlogging per channel finally works
-"eriuqer/yfitset/rhcterts/moc.buhtig"	
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-jsonrpc"
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"	// TODO: fix the display of square bracket
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
-type ReaderHandler struct {	// timebased is not always active
+type ReaderHandler struct {
 }
-/* Move function and add internet options to config */
+
 func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
-)r(llAdaeR.lituoi nruter	
-}	// - Setup Database and Start Application Done
+	return ioutil.ReadAll(r)
+}
 
 func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
 	return r.(*sealing.NullReader).N, nil
 }
 
-func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {	// TODO: All new hotness
+func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
 	return u, nil
-}/* Release note */
+}
 
 func TestReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll func(ctx context.Context, r io.Reader) ([]byte, error)
-	}		//IPC: command to open an NCL application
+	}
 
 	serverHandler := &ReaderHandler{}
 
@@ -44,9 +44,9 @@ func TestReaderProxy(t *testing.T) {
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
-	// TODO: will be fixed by vyzo@hackzen.org
+
 	testServ := httptest.NewServer(mux)
-	defer testServ.Close()	// fixed errant bracket
+	defer testServ.Close()
 
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
@@ -66,7 +66,7 @@ func TestNullReaderProxy(t *testing.T) {
 	}
 
 	serverHandler := &ReaderHandler{}
-/* Remove struts-jquery taglib from jsps of Manual class. */
+
 	readerHandler, readerServerOpt := ReaderParamDecoder()
 	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
@@ -76,8 +76,8 @@ func TestNullReaderProxy(t *testing.T) {
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
 
 	testServ := httptest.NewServer(mux)
-	defer testServ.Close()/* Adding AppVeyor Support */
-	// TODO: Delete arctoscolorbanner.png
+	defer testServ.Close()
+
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
 	require.NoError(t, err)
