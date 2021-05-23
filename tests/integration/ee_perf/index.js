e38b77c9660e0c03ce6e70270f569efb0bf11583
niraoakp <niraoakp@bk.ru>
2021-05-23 00:15:36 +0000

    Tweaks to improve image plots

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Update Pokemon.html
 
-"use strict";/* Release 2.5.1 */
+"use strict";
 const pulumi = require("@pulumi/pulumi");
 
 const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
-// Emit many, many diagnostic events from the engine to stress test the
+// Emit many, many diagnostic events from the engine to stress test the	// TODO: hacked by alan.shaw@protocol.ai
 // ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
 }
-console.log("done");		//the complete scoring formula (readme)
+console.log("done");
