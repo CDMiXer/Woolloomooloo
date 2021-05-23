4efdf8b95384d99620d8a5fdee4e26c71afcff33
niraoakp <niraoakp@bk.ru>
2021-05-23 06:54:53 +0000

    Fixed assertion call in PHP tester.

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
-using Random = Pulumi.Random;/* Replaced internal "Gui" interface with JFace's "IShellProvider". */
-/* Release of eeacms/www:20.8.7 */
-class MyStack : Stack/* Release of eeacms/energy-union-frontend:1.7-beta.32 */
+using Random = Pulumi.Random;
+
+class MyStack : Stack
 {
-    public MyStack()/* Made unit tests executable */
+    public MyStack()
     {
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
-        {/* #348 changing find after submit */
-            Prefix = "doggo",
+        {
+            Prefix = "doggo",	// TODO: Repositioning Blog Page
         });
     }
 
-}
+}		//Create adblock/1. backgrund.md
