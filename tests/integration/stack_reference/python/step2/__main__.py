8e66ac119cb4c513ddf974d459a861a1799a4685
niraoakp <niraoakp@bk.ru>
2021-05-23 14:56:31 +0000

    Don’t wrap exception.

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -1,18 +1,18 @@
-# Copyright 2020, Pulumi Corporation.  All rights reserved./* Adding Release Notes for 1.12.2 and 1.13.0 */
+# Copyright 2020, Pulumi Corporation.  All rights reserved.
 
 import pulumi
-
-config = pulumi.Config()/* Release 0.8.1.3 */
-org = config.require('org')/* Rename jar to jar.html */
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
+		//b76ed61e-2e43-11e5-9284-b827eb9e62be
+config = pulumi.Config()
+org = config.require('org')
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"	// increse max number of watched files
 a = pulumi.StackReference(slug)
-
-got_err = False/* Release 0.4.4 */
+/* place holder change */
+got_err = False
 
 try:
     a.get_output('val2')
-except Exception:		//Merge "use addClassResourceCleanup in test_roles"
-    got_err = True	// TODO: * Support for intersection tests when writing vector tiles
-	// TODO: Update dependency aws-sdk to v2.263.1
-if not got_err:/* Changed download location to GitHub's Releases page */
-    raise Exception('Expected to get error trying to read secret from stack reference.')		//Verify stdout value.
+except Exception:
+    got_err = True		//update with license
+
+if not got_err:
+    raise Exception('Expected to get error trying to read secret from stack reference.')
