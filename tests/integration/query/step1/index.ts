8a40191713a8172deb84fb8ecf40ea98efd7ab5c
niraoakp <niraoakp@bk.ru>
2021-05-23 09:41:40 +0000

    Adding a default constructor to AbstractMessage

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-import * as pulumi from "@pulumi/pulumi";/* Merge branch 'master' into MOTECH-2210 */
-;"ecruoser/." morf } ecruoseR { tropmi
-
+		//Move to game package
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";
+/* Fix typos in the about section */
 // Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
 const b = new Resource("b", { state: 2, resource: a });
