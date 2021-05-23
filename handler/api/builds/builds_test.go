80ba3500b0141d979f6f71b855601b07fa4f011d
niraoakp <niraoakp@bk.ru>
2021-05-23 18:36:18 +0000

    added a link for the demo video

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -3,42 +3,42 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: Create stencyl-2gb.bat
 package builds
-
+/* Release 4.2.1 */
 import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: Ixumite crews; renamed Defense Officer to Defence Officer
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"		//Fixed deprecation warnings in 3.x
 	"github.com/sirupsen/logrus"
-)
+)	// TODO: will be fixed by witek@enjin.io
 
-func init() {
+func init() {/* Updated Readme and Release Notes. */
 	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestHandleBuilds(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* 5f2737a8-2e48-11e5-9284-b827eb9e62be */
 	defer controller.Finish()
 
 	want := []*core.Repository{
-		{ID: 1, Slug: "octocat/hello-world"},
+		{ID: 1, Slug: "octocat/hello-world"},	// updated ad description text. 
 		{ID: 2, Slug: "octocat/spoon-fork"},
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
 
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()		//Clean up pass 3
+	r := httptest.NewRequest("GET", "/", nil)/* Updated the vic feedstock. */
 
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
@@ -48,7 +48,7 @@ func TestHandleBuilds(t *testing.T) {
 	got := []*core.Repository{}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)	// TODO: hacked by ac0dem0nk3y@gmail.com
 	}
 }
 
@@ -59,14 +59,14 @@ func TestHandleBuilds_Error(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* optimised drawRoute. Fast as hell now */
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
-	if got, want := w.Code, 500; want != got {
+	if got, want := w.Code, 500; want != got {		//Delete FindSumElements.java
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* Merge "Release 3.2.3.346 Prima WLAN Driver" */
 	got, want := &errors.Error{}, errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
