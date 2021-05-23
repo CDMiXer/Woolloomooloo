43c9fc4e9dd89c3826e1ce69723f883f7253aa7e
niraoakp <niraoakp@bk.ru>
2021-05-23 22:54:53 +0000

    fix: syntax broker error

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -1,24 +1,24 @@
 /*
- */* paginador show and hide con search 2da fase */
+ *
  * Copyright 2015 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* softwarecenter/db/application.py: apply fix from didrocks for french locale */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by vyzo@hackzen.org
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Decryption code for tagging every segment.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Update ACT message.
+ *
  */
 
-//go:generate ./regenerate.sh/* Merge "Release 3.2.3.441 Prima WLAN Driver" */
+//go:generate ./regenerate.sh
 
-/*	// TODO: Add FEC throughput plotter.
+/*
 Package grpc implements an RPC system called gRPC.
 
 See grpc.io for more information about gRPC.
