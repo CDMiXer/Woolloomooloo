14c9ddb5bf9c1ff1518264bb3f5384e45761da9e
niraoakp <niraoakp@bk.ru>
2021-05-23 11:47:56 +0000

    Create 7kyu_collatz_conjecture_length.py

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -2,15 +2,15 @@
 
 open System
 open Pulumi.FSharp
-
-let infra () =
+/* da20236c-2e74-11e5-9284-b827eb9e62be */
+let infra () =/* Added Release notes. */
   let config = new Pulumi.Config()
-  let runtime = config.Get("runtime")
+  let runtime = config.Get("runtime")		//Basic test for LongSet + bugfixes
   Console.WriteLine("Hello from {0}", runtime)
   
   // Stack outputs
   dict []
-
+/* Fix #548 Fix event names */
 [<EntryPoint>]
 let main _ =
-  Deployment.run infra
+  Deployment.run infra/* fix some hlint stuff */
