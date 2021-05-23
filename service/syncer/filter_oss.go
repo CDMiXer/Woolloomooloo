40ebad7bb42cfbea9325a3db587563d09ab7170d
niraoakp <niraoakp@bk.ru>
2021-05-23 16:07:34 +0000

    [densematrix] reenable and fix broken check, to avoid aliasing in mv and mtv

--- service/syncer/filter_oss.go
+++ service/syncer/filter_oss.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: Convert dockerfiles to LF
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Delete Substance.java */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* process merge code */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+/* 0.42 bug fix */
+// +build oss		//Update README for v2.3.0
 
-// +build oss
+package syncer/* 0.16.0: Milestone Release (close #23) */
 
-package syncer/* Fix Int64.ushr */
+import "github.com/drone/drone/core"
 
-import "github.com/drone/drone/core"	// Removed unused member variable and its disembodied doc comment
-/* Merge "Add Release Notes and Architecture Docs" */
-// FilterFunc can be used to filter which repositories are
-// synchronized with the local datastore.
+// FilterFunc can be used to filter which repositories are/* Merge "[VNC config] User role should be case insensitive" */
+// synchronized with the local datastore./* Update Colin-McCallum.md */
 type FilterFunc func(*core.Repository) bool
-	// Fix a reload bug in Live Update, where data got slightly corrupted
+
 // NamespaceFilter is a no-op filter.
 func NamespaceFilter(namespaces []string) FilterFunc {
 	return noopFilter
-}
+}		//Hello Menu (simple) complete.
 
-// noopFilter is a filter function that always returns true./* Merge "Release 1.0.0.226 QCACLD WLAN Drive" */
-func noopFilter(*core.Repository) bool {	// TODO: Fixes issue 97.
+// noopFilter is a filter function that always returns true.
+func noopFilter(*core.Repository) bool {
 	return true
 }
