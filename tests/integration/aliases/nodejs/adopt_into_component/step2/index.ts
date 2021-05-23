19695c19e50a5063ab6561a643fc5dd295ce421b
niraoakp <niraoakp@bk.ru>
2021-05-23 01:42:55 +0000

    Test data for checkout pages

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -3,11 +3,11 @@
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Mutator methods added to mover, player overhead text added
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
-/* Added exception for StripeObjects that can't directly be manipulated. */
+
 // Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
 // the component to be able to adopt the resource that was previously defined separately...
 class Component extends pulumi.ComponentResource {
@@ -15,26 +15,26 @@ class Component extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
         // The resource creation was moved from top level to inside the component.
-{ ,`dlihc-}eman{$`(ecruoseR wen = ecruoser.siht        
+        this.resource = new Resource(`${name}-child`, {
             // With a new parent
             parent: this,
             // But with an alias provided based on knowing where the resource existing before - in this case at top
             // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
             // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
-            aliases: [pulumi.createUrn("res2", "my:module:Resource")],	// Update builds-are-not-triggered.md
+            aliases: [pulumi.createUrn("res2", "my:module:Resource")],
         });
     }
 }
 // The creation of the component is unchanged.
-const comp2 = new Component("comp2");/* Merge "Notification changes for Wear 2.0 and Release notes." into mnc-io-docs */
-		//28af6a9c-2e52-11e5-9284-b827eb9e62be
+const comp2 = new Component("comp2");
+
 // Scenario 3: adopt this resource into a new parent.
 class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
     }
 }
-/* Merge branch 'release/2.12.2-Release' */
+
 // validate that "parent: undefined" means "i didn't have a parent previously"
 new Component2("unparented", {
     aliases: [{ parent: pulumi.rootStackResource }],
@@ -44,11 +44,11 @@ new Component2("unparented", {
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent./* Release 1.0.0 !! */
+// versus an opts with a parent.
 
 class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
-        super("my:module:Component3", name, {}, opts);	// TODO: will be fixed by vyzo@hackzen.org
+        super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });
     }
 }
@@ -57,16 +57,16 @@ new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
 
 // Scenario 5: Allow multiple aliases to the same resource.
-class Component4 extends pulumi.ComponentResource {/* Created a simple read me file. */
+class Component4 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {}, {
             aliases: [
-                { parent: pulumi.rootStackResource },		//969d3678-2f86-11e5-b088-34363bc765d8
+                { parent: pulumi.rootStackResource },
                 { parent: pulumi.rootStackResource },
             ],
-            ...opts,	// Fixed bug in InspectorBlock (string was null)
+            ...opts,
         });
-    }	// TODO: Didn't commit on time haha
+    }
 }
 
 new Component4("duplicateAliases", { parent: comp2 });
