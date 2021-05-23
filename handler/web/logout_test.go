85448efc344a7b42f1db44d58a3f069571cf7b5e
niraoakp <niraoakp@bk.ru>
2021-05-23 13:49:36 +0000

    [CPORT]: We shouldn't be DPRINT1'ing from the library designed to DPRINT...

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//return this for setters
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "usb: misc: Resume mdm interface on receiving notification available cb" */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package web
-/* fast click initial */
+
 import (
-	"net/http/httptest"		//8e383be8-2e73-11e5-9284-b827eb9e62be
-	"testing"/* Release RDAP SQL provider 1.2.0 */
+	"net/http/httptest"/* website/docs: Add missing `end` to "Run Once or Always" example */
+	"testing"/* Using Release with debug info */
 )
 
 func TestLogout(t *testing.T) {
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/logout", nil)
-		//Merge "Move gr-file-list-constants to typescript"
-	HandleLogout().ServeHTTP(w, r)/* Version updated to 0.9 */
+	r := httptest.NewRequest("GET", "/logout", nil)	// TODO: hacked by greg@colvin.org
 
+	HandleLogout().ServeHTTP(w, r)
+		//Merge "Encode should be called only for strings"
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
-		t.Errorf("Want response code %q, got %q", want, got)	// TODO: Create  generate_category_pages.rb
+		t.Errorf("Want response code %q, got %q", want, got)/* Get operational scheduled task info */
 	}
 }
