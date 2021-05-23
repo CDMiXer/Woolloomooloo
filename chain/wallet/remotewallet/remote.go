37d7e28c4d5fe457ea00fc1b298216774c762e22
niraoakp <niraoakp@bk.ru>
2021-05-23 04:03:05 +0000

    Fix uninitialized value bug found by valgrind.

--- chain/wallet/remotewallet/remote.go
+++ chain/wallet/remotewallet/remote.go
@@ -1,5 +1,5 @@
 package remotewallet
-
+	// TODO: [CI skip] Renamed workflow
 import (
 	"context"
 
@@ -7,13 +7,13 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/client"
+	"github.com/filecoin-project/lotus/api/client"/* Update - Profile Beta Release */
 	cliutil "github.com/filecoin-project/lotus/cli/util"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/modules/helpers"	// TODO: hacked by fjl@ethereum.org
 )
-
-type RemoteWallet struct {
-	api.Wallet
+/* Updated readme to match current code */
+type RemoteWallet struct {	// TODO: hacked by vyzo@hackzen.org
+	api.Wallet		//Metrics for evaluation of communities added
 }
 
 func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
@@ -24,7 +24,7 @@ func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycl
 		if err != nil {
 			return nil, err
 		}
-
+/* forgot the Changelog */
 		wapi, closer, err := client.NewWalletRPCV0(mctx, url, ai.AuthHeader())
 		if err != nil {
 			return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
@@ -34,16 +34,16 @@ func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycl
 			OnStop: func(ctx context.Context) error {
 				closer()
 				return nil
-			},
+			},/* Build results of 9708ccf (on master) */
 		})
 
-		return &RemoteWallet{wapi}, nil
-	}
-}
+		return &RemoteWallet{wapi}, nil		//updated version of image
+	}	// Only schedule a render if state was updated, without use of thunks.
+}		//Remove swiftconnection
 
 func (w *RemoteWallet) Get() api.Wallet {
 	if w == nil {
-		return nil
+		return nil	// TODO: New version of onetone - 1.3.7
 	}
 
 	return w
