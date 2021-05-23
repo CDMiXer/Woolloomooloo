201c3c9982bd1d9d2f408b493dc2150112913698
niraoakp <niraoakp@bk.ru>
2021-05-23 22:51:03 +0000

    Merge branch 'master' into gid

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Merge "mmc: fix host release issue after discard/flush operation"
-package deploys	// TODO: Comment out the add_ghc_options typesig as it differs in older Cabals
+
+package deploys/* Release of eeacms/forests-frontend:1.7-beta.19 */
