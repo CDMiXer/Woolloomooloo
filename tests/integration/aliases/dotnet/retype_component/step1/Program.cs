7d032e4a43a250faec85b6bc4eb34970fa1f24d4
niraoakp <niraoakp@bk.ru>
2021-05-23 01:06:40 +0000

    Add GETS function

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,28 +1,28 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Delete base/Proyecto/RadStudio10.3/minicom/Win32/Release directory */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
-		//094b47d6-2e6e-11e5-9284-b827eb9e62be
+
 class Resource : ComponentResource
-{	// TODO: will be fixed by yuvalalaluf@gmail.com
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }/* Add Releases and Cutting version documentation back in. */
+    }
 }
-	// TODO: hacked by juan@benet.ai
+
 // Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
 {
-    private Resource resource;/* state: refactor micro unit watcher into function */
+    private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFour", name, options)/* Attempt to fix delay issue, UAT Release */
+        : base("my:module:ComponentFour", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
-}/* Release version 3.2 with Localization */
-		//Make subs support translationable
+}
+
 class Program
 {
     static Task<int> Main(string[] args)
@@ -30,6 +30,6 @@ class Program
         return Deployment.RunAsync(() => 
         {
             var comp4 = new ComponentFour("comp4");
-        });	// refaktoring
+        });
     }
 }
