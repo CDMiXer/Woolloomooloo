b6c67afec01ecc0a763148175a80fd6a0fca7137
niraoakp <niraoakp@bk.ru>
2021-05-23 19:07:26 +0000

    GeometryTypes needs master now

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// Merge "Log which repo zuul references are created on"
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main/* Make sure appstream metadata file passes appstream-util validate */
+package main
 
 import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
-)/* #indentation */
+)
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, ctx.Project())
 
-		org := cfg.Require("org")/* Improved the 'model' task to support the APP argument. */
-		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())		//include xpath-big.xml in installation
+		org := cfg.Require("org")
+		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		_, err := pulumi.NewStackReference(ctx, slug, nil)
-/* updated SCM for GIT & Maven Release */
+
 		if err != nil {
-			return fmt.Errorf("error reading stack reference: %v", err)/* Release of eeacms/www-devel:20.6.6 */
-		}	// TODO: Made contribution URL into a hyperlink
+			return fmt.Errorf("error reading stack reference: %v", err)
+		}
 		ctx.Export("val",
 			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
 
