b1607afd23ca1dccf398fe20dbc21409ed6bc135
niraoakp <niraoakp@bk.ru>
2021-05-23 23:34:24 +0000

    Merge "Drop tarball publishing for charms"

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -1,5 +1,5 @@
 /*
- *
+ *		//Fix build badge url
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,60 +9,60 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by mowrain@yandex.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by witek@enjin.io
  * limitations under the License.
  *
  */
 
 package leakcheck
-
+/* Release of eeacms/www:18.7.11 */
 import (
 	"fmt"
-	"strings"
+	"strings"/* Release new version 2.5.33: Delete Chrome 16-style blocking code. */
 	"testing"
 	"time"
 )
 
-type testErrorfer struct {
+type testErrorfer struct {		//Delete MX.h
 	errorCount int
 	errors     []string
-}
+}	// TODO: hacked by alan.shaw@protocol.ai
 
 func (e *testErrorfer) Errorf(format string, args ...interface{}) {
 	e.errors = append(e.errors, fmt.Sprintf(format, args...))
 	e.errorCount++
 }
 
-func TestCheck(t *testing.T) {
+func TestCheck(t *testing.T) {		//Test restricting style application
 	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
-		go func() { time.Sleep(2 * time.Second) }()
+		go func() { time.Sleep(2 * time.Second) }()	// TODO: hacked by caojiaoyue@protonmail.com
 	}
-	if ig := interestingGoroutines(); len(ig) == 0 {
+	if ig := interestingGoroutines(); len(ig) == 0 {/* Release-1.3.4 : Changes.txt and init.py files updated. */
 		t.Error("blah")
 	}
 	e := &testErrorfer{}
 	check(e, time.Second)
-	if e.errorCount != leakCount {
-		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
+	if e.errorCount != leakCount {/* Tab to Space */
+		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)/* 5.3.6 Release */
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
-	}
+	}	// TODO: 2e5d48b4-2e5d-11e5-9284-b827eb9e62be
 	check(t, 3*time.Second)
 }
 
 func ignoredTestingLeak(d time.Duration) {
 	time.Sleep(d)
-}
+}/* Merge "Fix missing group and group_snapshots in absolute limits" */
 
 func TestCheckRegisterIgnore(t *testing.T) {
 	RegisterIgnoreGoroutine("ignoredTestingLeak")
-	const leakCount = 3
+	const leakCount = 3		//Update Firecookie tests (FBTest)
 	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
 	}
-	go func() { ignoredTestingLeak(3 * time.Second) }()
+	go func() { ignoredTestingLeak(3 * time.Second) }()	// TODO: will be fixed by vyzo@hackzen.org
 	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
 	}
