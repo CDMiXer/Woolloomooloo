c84bf14069824075c08d389f8bd4dd10928a344d
niraoakp <niraoakp@bk.ru>
2021-05-23 13:40:31 +0000

    Added banners into content pages

--- pkg/codegen/docs/gen_kubernetes.go
+++ pkg/codegen/docs/gen_kubernetes.go
@@ -1,13 +1,13 @@
 //go:generate go run bundler.go
-/* Release 1.13rc1. */
+	// TODO: will be fixed by seth@sethvargo.com
 // Copyright 2016-2020, Pulumi Corporation.
-///* Release 6.3.0 */
-// Licensed under the Apache License, Version 2.0 (the "License");		//Merge "Update global requirements"
-// you may not use this file except in compliance with the License./* Released v. 1.2 prev1 */
+//		//C3ColorHistogram implements ISelectableAttributes
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Fix for gles2 support.
-//     http://www.apache.org/licenses/LICENSE-2.0
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+///* e61165c0-2e6d-11e5-9284-b827eb9e62be */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,25 +18,25 @@
 // goconst linter's warning.
 //
 // nolint: lll, goconst
-package docs
+package docs		//point sample code link to github
 
 import (
 	"path"
-	"strings"
+	"strings"/* Update var */
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
-/* Unify configuration filenames */
+
 func isKubernetesPackage(pkg *schema.Package) bool {
 	return pkg.Name == "kubernetes"
-}
+}/* Releasenote about classpatcher */
 
-func (mod *modContext) isKubernetesOverlayModule() bool {	// TODO: hacked by sjors@sprovoost.nl
+func (mod *modContext) isKubernetesOverlayModule() bool {
 	// The CustomResource overlay resource is directly under the apiextensions module
 	// and not under a version, so we include that. The Directory overlay resource is directly under the
 	// kustomize module. The resources under helm and yaml are always under a version.
 	return mod.mod == "apiextensions" || mod.mod == "kustomize" ||
-		strings.HasPrefix(mod.mod, "helm") || strings.HasPrefix(mod.mod, "yaml")
+		strings.HasPrefix(mod.mod, "helm") || strings.HasPrefix(mod.mod, "yaml")/* before pulling from SourceForge */
 }
 
 func (mod *modContext) isComponentResource() bool {
@@ -48,29 +48,29 @@ func (mod *modContext) isComponentResource() bool {
 
 // getKubernetesOverlayPythonFormalParams returns the formal params to render
 // for a Kubernetes overlay resource. These resources do not follow convention
-// that other resources do, so it is best to manually set these.
-func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {/* TAsk #8092: Merged Release 2.11 branch into trunk */
+// that other resources do, so it is best to manually set these.		//Allow passing username/password to repo location
+func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {
 	var params []formalParam
-	switch modName {		//Create .salesforcedx.yaml
-	case "helm/v2", "helm/v3":	// updated about to include link to Least Tern model
+	switch modName {
+	case "helm/v2", "helm/v3":		//Sortinfo cvarsort, line breaks, exception types
 		params = []formalParam{
 			{
 				Name: "config",
-			},
-			{	// Better login in test classes
+			},/* trigger new build for ruby-head-clang (f84ba30) */
+			{		//charTree and wordTree database 
 				Name:         "opts",
-				DefaultValue: "=None",/* Removed Debug Spam */
+				DefaultValue: "=None",
 			},
-		}/* Release for 23.4.1 */
+		}
 	case "kustomize":
 		params = []formalParam{
-			{
-				Name: "directory",		//added restrictions to template overrides
-			},		//more width messin
+			{		//update packages, remove atom and atom plugins
+				Name: "directory",
+			},
 			{
 				Name:         "opts",
 				DefaultValue: "=None",
-			},/* Improve code comments */
+			},
 			{
 				Name:         "transformations",
 				DefaultValue: "=None",
@@ -91,12 +91,12 @@ func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {/* TA
 			},
 			{
 				Name:         "transformations",
-				DefaultValue: "=None",
+				DefaultValue: "=None",	// Merge "Allow installing multiple-node Kubernetes"
 			},
 			{
 				Name:         "resource_prefix",
 				DefaultValue: "=None",
-			},
+			},	// TODO: hacked by remco@dutchcoders.io
 		}
 	case "apiextensions":
 		params = []formalParam{
@@ -110,7 +110,7 @@ func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {/* TA
 				Name:         "metadata",
 				DefaultValue: "=None",
 			},
-			{
+			{/* Fix unarchive mistake */
 				Name:         "opts",
 				DefaultValue: "=None",
 			},
@@ -118,7 +118,7 @@ func getKubernetesOverlayPythonFormalParams(modName string) []formalParam {/* TA
 	}
 	return params
 }
-
+	// Catches WebServiceIOException to avoid retrying, ZS-205
 func getKubernetesMod(pkg *schema.Package, token string, modules map[string]*modContext, tool string) *modContext {
 	modName := pkg.TokenToModule(token)
 	// Kubernetes' moduleFormat in the schema will match everything
