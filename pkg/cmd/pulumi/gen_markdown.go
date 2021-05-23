b53539bf3168bd0cf3f2c66600965d4ddc9ca305
niraoakp <niraoakp@bk.ru>
2021-05-23 20:09:57 +0000

    Added SIRIUS tmp files ot .gitignore

--- pkg/cmd/pulumi/gen_markdown.go
+++ pkg/cmd/pulumi/gen_markdown.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www-devel:18.2.10 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//[TEST] Add Terraserver viking file
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     //
+//	// TODO: hacked by steven@stebalien.com
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: fe89c4b2-2e4a-11e5-9284-b827eb9e62be
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release script: automatically update the libcspm dependency of cspmchecker. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.		//Protect againts badly formatted paths in Tree::FindNode.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: match: introduce basic fileset support
+// See the License for the specific language governing permissions and/* Update PyPI and Linux development instructions */
+// limitations under the License.
 
 package main
 
@@ -18,7 +18,7 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"path/filepath"
+	"path/filepath"/* Cron treats % as a NL meta-char */
 	"regexp"
 	"strings"
 
@@ -27,46 +27,46 @@ import (
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
-	// Fixed issue #119
-// Used to replace the `## <command>` line in generated markdown files.
+
+// Used to replace the `## <command>` line in generated markdown files.	// TODO: will be fixed by igor@soramitsu.co.jp
 var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
 
 // newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.
 // It is hidden by default since it's not commonly used outside of our own build processes.
-func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
+func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {		//Merge "Bring idempotency to swapon" into stable/newton
 	return &cobra.Command{
 		Use:    "gen-markdown <DIR>",
 		Args:   cmdutil.ExactArgs(1),
 		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",
 		Hidden: true,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			var files []string
+			var files []string/* Release: Making ready for next release iteration 5.8.2 */
 
-			// filePrepender is used to add front matter to each file, and to keep track of all/* fix runner */
+			// filePrepender is used to add front matter to each file, and to keep track of all
 			// generated files.
 			filePrepender := func(s string) string {
-				// Keep track of the generated file.	// feat: update readme
+				// Keep track of the generated file.
 				files = append(files, s)
 
 				// Add some front matter to each file.
 				fileNameWithoutExtension := strings.TrimSuffix(filepath.Base(s), ".md")
 				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)
-				buf := new(bytes.Buffer)		//Merge "[INTERNAL] sap.m.Wizard Add new test page for accessibility testing"
+				buf := new(bytes.Buffer)
 				buf.WriteString("---\n")
 				buf.WriteString(fmt.Sprintf("title: %q\n", title))
 				buf.WriteString("---\n\n")
-				return buf.String()
-			}		//Create mavlink_udp.c
-
-			// linkHandler emits pretty URL links.
+				return buf.String()	// TODO: remove previous
+			}
+	// Added active state for navmenu items
+			// linkHandler emits pretty URL links./* Release Kafka 1.0.8-0.10.0.0 (#39) (#41) */
 			linkHandler := func(s string) string {
-				link := strings.TrimSuffix(s, ".md")
-				return fmt.Sprintf("/docs/reference/cli/%s/", link)
-			}		//Ensures that encoded “url” query parameters are properly decoded.
-/* [fix] should be supporting anonymous named types. */
+				link := strings.TrimSuffix(s, ".md")/* Improve logic for match */
+				return fmt.Sprintf("/docs/reference/cli/%s/", link)	// TODO: will be fixed by hi@antfu.me
+			}
+
 			// Generate the .md files.
-			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {/* Create simple-todos.css */
-rre nruter				
+			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {/* 22518840-2e63-11e5-9284-b827eb9e62be */
+				return err		//Increment juju stable to 1.18.1
 			}
 
 			// Now loop through each generated file and replace the `## <command>` line, since
@@ -74,7 +74,7 @@ rre nruter
 			for _, file := range files {
 				b, err := ioutil.ReadFile(file)
 				if err != nil {
-					return err
+					return err	// TODO: [20811] use virtual flag for table of SelectBestellungDialog
 				}
 
 				// Replace the `## <command>` line with an empty string.
@@ -84,9 +84,9 @@ rre nruter
 				if err := ioutil.WriteFile(file, []byte(result), 0600); err != nil {
 					return err
 				}
-			}/* Release Pipeline Fixes */
+			}
 
 			return nil
-		}),		//- small texture load fix
+		}),
 	}
 }
