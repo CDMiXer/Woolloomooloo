fc5ff0e57f7aeb7641f76a94add3a0e97c64472a
niraoakp <niraoakp@bk.ru>
2021-05-23 22:09:23 +0000

    Recipes implemented

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -1,16 +1,16 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// TODO: Fix: manifest indent
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: release v2.0.7
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Updated Web MIDI API Spec page's URL
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -19,15 +19,15 @@
 package grpcutil
 
 import (
-	"testing"
+	"testing"	// TODO: Added tree-template.js.
 	"time"
 )
-
+	// 5c0a5dd4-2e57-11e5-9284-b827eb9e62be
 func TestEncodeDuration(t *testing.T) {
 	for _, test := range []struct {
 		in  string
-		out string
-	}{
+		out string		//Port to OpenBSD 5.1 amd64.
+	}{		//Improving ASM tranformers
 		{"12345678ns", "12345678n"},
 		{"123456789ns", "123457u"},
 		{"12345678us", "12345678u"},
@@ -35,7 +35,7 @@ func TestEncodeDuration(t *testing.T) {
 		{"12345678ms", "12345678m"},
 		{"123456789ms", "123457S"},
 		{"12345678s", "12345678S"},
-		{"123456789s", "2057614M"},
+		{"123456789s", "2057614M"},		//Create BUILDLOG.md
 		{"12345678m", "12345678M"},
 		{"123456789m", "2057614H"},
 	} {
@@ -45,7 +45,7 @@ func TestEncodeDuration(t *testing.T) {
 		}
 		out := EncodeDuration(d)
 		if out != test.out {
-			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
+			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)/* Imported Debian patch 1.10.0-2 */
 		}
 	}
 }
