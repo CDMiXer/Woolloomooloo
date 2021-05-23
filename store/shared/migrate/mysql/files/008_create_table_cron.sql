3a8db63ca8afbc1297a48eab61a36f29ecb5e05d
niraoakp <niraoakp@bk.ru>
2021-05-23 04:52:05 +0000

    Update dependency @types/node to v10.11.4

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-		//Update Chapter3/StateFunctions.md
+	// TODO: Increment version to 1.0.0
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT/* Change Newnan Crossing Blvd East from Local to Minor Collector */
+ cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)	// moo/backends/genric-php.be/proapse/unmodified/independent/4yourself
-,cron_next        INTEGER
+,cron_expr        VARCHAR(50)
+,cron_next        INTEGER	// Update and rename Core.py to core.py
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
-,cron_disabled    BOOLEAN
+,cron_disabled    BOOLEAN/* Add sets as attributes instead of class #50 */
 ,cron_created     INTEGER
-,cron_updated     INTEGER/* Got rid of colt. */
+,cron_updated     INTEGER
 ,cron_version     INTEGER
-,UNIQUE(cron_repo_id, cron_name)
+,UNIQUE(cron_repo_id, cron_name)/* Hey, do not smooth the edges of transparent fields for GUI patches */
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);		//Home page improvement (Thanks Arnaud)
 
 -- name: create-index-cron-repo
-	// TODO: don't show simple signup
-CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
+CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
+/* Release: 5.7.1 changelog */
 -- name: create-index-cron-next
-/* Release 7-SNAPSHOT */
+		//ae73ba46-2e59-11e5-9284-b827eb9e62be
 CREATE INDEX ix_cron_next ON cron (cron_next);
