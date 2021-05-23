c51a058df5b8f28465e73907fda6d84fdd9b1f26
niraoakp <niraoakp@bk.ru>
2021-05-23 13:51:33 +0000

    Show/hide events for tile layers were added

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Migrated to uima-tokens-regex 1.4 (term keyword replaced by rule) */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Add top-level class diagram
+
 package encrypt
 
 import "testing"
-
+/* update to 0.2-SNAPSHOT */
 func TestNone(t *testing.T) {
-	n, _ := New("")
-	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
-	if err != nil {/* Release of eeacms/www:18.5.9 */
-		t.Error(err)
+	n, _ := New("")/* Merge "Release 3.2.3.460 Prima WLAN Driver" */
+	ciphertext, err := n.Encrypt("correct-horse-batter-staple")/* give pointer to first and last step */
+	if err != nil {
+		t.Error(err)/* 20.1-Release: removing syntax error from cappedFetchResult */
 	}
-	plaintext, err := n.Decrypt(ciphertext)
+	plaintext, err := n.Decrypt(ciphertext)/* Release 0.3.3 (#46) */
 	if err != nil {
 		t.Error(err)
 	}
-	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
+	if want, got := plaintext, "correct-horse-batter-staple"; got != want {	// Merge branch 'dev' into srk/pushnotifications
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
 }
