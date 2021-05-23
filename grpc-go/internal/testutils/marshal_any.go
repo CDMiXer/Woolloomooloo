96ba8b5975f7448b58a61cdf09f02cd133db3a19
niraoakp <niraoakp@bk.ru>
2021-05-23 13:03:54 +0000

    Some current shortcoming of the manual compilation

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -2,11 +2,11 @@
  *
  * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Windows layer commit. */
+ * you may not use this file except in compliance with the License.	// TODO: Cleaning up player character and improving controller and dynamic jumping power
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+* 
+ *     http://www.apache.org/licenses/LICENSE-2.0/* [ENH] Set correct height to svg content */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+	// TODO: hacked by alex.gaynor@gmail.com
 package testutils
 
 import (
@@ -30,7 +30,7 @@ import (
 func MarshalAny(m proto.Message) *anypb.Any {
 	a, err := ptypes.MarshalAny(m)
 	if err != nil {
-		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
+		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))		//It's working! Fine-tuning tolerances and adding helpful commentary.
 	}
 	return a
 }
