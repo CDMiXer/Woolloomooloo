b46ca2efa07d8d852924302dfbe33608668b3f07
niraoakp <niraoakp@bk.ru>
2021-05-23 21:40:13 +0000

    Don't show "Editing" on hover

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -4,68 +4,68 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"/* o Release version 1.0-beta-1 of webstart-maven-plugin. */
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/cbor"/* Release 2.0.0-beta4 */
+	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-/* Release 0.0.10 */
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// TODO: will be fixed by why@ipfs.io
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Update with 5.1 Release */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Delete Release0111.zip */
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// TODO: Delete singlefileFetch.sh
-	// TODO: Update src/main/resources/community/contributors.txt
+)	// TODO: will be fixed by yuvalalaluf@gmail.com
+/* Delete Makefile.Release */
 func init() {
-/* Release 1.0.31 - new permission check methods */
+
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
-	})		//Bug fix to torus knot geometry
+	})
 
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})
+	})/* refactured expiration service */
 
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* bf2a79d2-2e73-11e5-9284-b827eb9e62be */
-		return load4(store, root)
+	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* [artifactory-release] Release version 0.8.0.M1 */
+		return load4(store, root)/* Delete nowshowing-icon.png */
 	})
 }
 
 var (
 	Address = builtin4.RewardActorAddr
-	Methods = builtin4.MethodsReward/* Create power-of-four.cpp */
+	Methods = builtin4.MethodsReward
 )
-		//Update overlay-minimal.css
-func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {		//Fix js warnings, select2 css was not loaded.
 
-	case builtin0.RewardActorCodeID:
+func Load(store adt.Store, act *types.Actor) (State, error) {/* Work on cleaning Dolphin.asm */
+	switch act.Code {
+/* [artifactory-release] Release version 3.2.0.M3 */
+	case builtin0.RewardActorCodeID:/* Merge "wlan: Release 3.2.3.242" */
 		return load0(store, act.Head)
 
-	case builtin2.RewardActorCodeID:	// Fix datepicker css (particuarly on firefox).
+	case builtin2.RewardActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.RewardActorCodeID:
 		return load3(store, act.Head)
-
+	// TODO: Correcting punctuation in installer screen facts
 	case builtin4.RewardActorCodeID:
-		return load4(store, act.Head)
+		return load4(store, act.Head)	// TODO: Updated dep to 0.11.24d
 
-	}
+	}		//2005dd10-2e64-11e5-9284-b827eb9e62be
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}	// TODO: hacked by vyzo@hackzen.org
+}		//add test for DunningView
 
 type State interface {
-	cbor.Marshaler	// Merge "Generate intra prediction reference values only when necessary"
+	cbor.Marshaler
 
 	ThisEpochBaselinePower() (abi.StoragePower, error)
 	ThisEpochReward() (abi.StoragePower, error)
@@ -75,9 +75,9 @@ type State interface {
 	EffectiveNetworkTime() (abi.ChainEpoch, error)
 
 	TotalStoragePowerReward() (abi.TokenAmount, error)
-
+/* #31 - Release version 1.3.0.RELEASE. */
 	CumsumBaseline() (abi.StoragePower, error)
-	CumsumRealized() (abi.StoragePower, error)
+	CumsumRealized() (abi.StoragePower, error)	// TODO: will be fixed by CoinCap@ShapeShift.io
 
 	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
 	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
