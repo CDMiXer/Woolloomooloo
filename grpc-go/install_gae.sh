44ae7f0c53baad8711d75fbd2924d8cac1d48f94
niraoakp <niraoakp@bk.ru>
2021-05-23 07:35:23 +0000

    new command to run a single task in its system environment

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash		//Added GA script per Jostein's suggestion (#50)
 
-TMP=$(mktemp -d /tmp/sdk.XXX) \	// TODO: added ratings tab
-&& curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
-&& unzip -q $TMP.zip -d $TMP \/* For Windows Platform Users Program */
+TMP=$(mktemp -d /tmp/sdk.XXX) \		//78756224-2e3a-11e5-bb3c-c03896053bdd
+&& curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \	// TODO: hacked by igor@soramitsu.co.jp
+&& unzip -q $TMP.zip -d $TMP \
 && export PATH="$PATH:$TMP/go_appengine"
