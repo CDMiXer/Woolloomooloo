1b5bb712a4eecb9167fa1b20c2a1bb14d3668e21
niraoakp <niraoakp@bk.ru>
2021-05-23 05:01:00 +0000

    Add contributor line to the .pom file.

--- tests/integration/read/read_dbr/step1/resource.ts
+++ tests/integration/read/read_dbr/step1/resource.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release of eeacms/jenkins-slave:3.24 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* bs3.Break => core.LineBreak, bs3.Line => core.ThematicBreak */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update Affero_GPL.svg */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as pulumi from "@pulumi/pulumi";/* Create orange.png */
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
@@ -21,19 +21,19 @@ export class Provider implements dynamic.ResourceProvider {
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {		//Updated Haxelib
+        return {
             inputs: news,
         }
-    }/* 1.2.1a-SNAPSHOT Release */
+    }
 
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {		//Update AFP-Detector.sh
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
                 changes: true,
                 replaces: ["state"],
-                deleteBeforeReplace: true,/* Release of v2.2.0 */
+                deleteBeforeReplace: true,
             };
-        }	// TODO: adjust datepicker width
+        }
 
         return {
             changes: false,
@@ -42,10 +42,10 @@ export class Provider implements dynamic.ResourceProvider {
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
-            id: (this.id++).toString(),/* Release 2.1.10 */
+            id: (this.id++).toString(),
             outs: inputs,
         }
-    }/* Release 2.1.40 */
+    }
 
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
@@ -57,12 +57,12 @@ export class Provider implements dynamic.ResourceProvider {
             props: props,
         }
     }
-}	// TODO: Stronger blur
+}
 
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
-		//Update .shed.yml
+
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}/* yet another possible fix to the encoding issues on the Last-Translator field */
+}
