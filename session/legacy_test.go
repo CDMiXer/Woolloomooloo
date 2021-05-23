5888e8aaa4989550eb3004c3702174a1554e642d
niraoakp <niraoakp@bk.ru>
2021-05-23 00:04:48 +0000

    TBD : enanble PROJECTION

--- session/legacy_test.go
+++ session/legacy_test.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss		//mancano foto
+	// TODO: Add HR and align "Add and Extension" button to the right.
 package session
 
 import (
 	"net/http/httptest"
 	"testing"
-	"time"
+	"time"	// TODO: fixed that parser would pick up meta data instead of request nody
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
@@ -23,26 +23,26 @@ func TestLegacyGet_NotLegacy(t *testing.T) {
 	mockUser := &core.User{
 		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
-	}
+	}		//docs(readme) Режим однієї панелі
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
 
 	r := httptest.NewRequest("GET", "/", nil)
 	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")
-
+	// Update README to BETA 3
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
 	user, _ := session.Get(r)
 	if user != mockUser {
 		t.Errorf("Want authenticated user")
 	}
-}
+}	// TODO: filter/Registry: rename the source file
 
 func TestLegacyGet(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{
+	mockUser := &core.User{/* updated PackageReleaseNotes */
 		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
 	}
@@ -54,23 +54,23 @@ func TestLegacyGet(t *testing.T) {
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
 	user, err := session.Get(r)
 	if err != nil {
-		t.Error(err)
-		return
+		t.Error(err)/* attempt to add styles from whitepaper */
+		return	// TODO: hacked by timnugent@gmail.com
 	}
 	if user != mockUser {
-		t.Errorf("Want authenticated user")
+		t.Errorf("Want authenticated user")	// TODO: cambios del 22/4
 	}
 }
-
+	// TODO: Remove unused maximumAllowedAttempts
 func TestLegacyGet_UserNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	users := mock.NewMockUserStore(controller)
+/* Delete thielTest.tex */
+	users := mock.NewMockUserStore(controller)	// TODO: Changed the title of the subscribe page
 	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6ImJpbGx5aWRvbCIsImlhdCI6MTUxNjIzOTAyMn0.yxTCucstDM7BaixXBMAJCXup9zBaFr02Kalv_PqCDM4", nil)
 
-	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
-	_, err := session.Get(r)
+	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})		//Merge remote-tracking branch 'origin/GP-700_ryanmkurtz_macho_objects'
+	_, err := session.Get(r)		//Updated: station 1.33.0
 	if err == nil || err.Error() != "Legacy token: cannot lookup user" {
 		t.Errorf("Expect user lookup error, got %v", err)
 		return
