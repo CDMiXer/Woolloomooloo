82b4a230e406ee621b63a9b2b2a9885e3207aab7
niraoakp <niraoakp@bk.ru>
2021-05-23 10:04:24 +0000

    Fix theme install location

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
-package build/* Merge "Release 3.2.3.371 Prima WLAN Driver" */
+package build
 
-// DisableBuiltinAssets disables the resolution of go.rice boxes that store/* Release v.0.0.1 */
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,	// TODO: Merge "ARM: dts: msm: disable charging only on 8994 CDPs"
-// etc.
-//
+// DisableBuiltinAssets disables the resolution of go.rice boxes that store
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
+// etc.	// TODO: hacked by timnugent@gmail.com
+///* Merge branch 'develop' into feature/IFS-108 */
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
-//		//DBFlute Engine: Oracle emergency option for too many procedures
+//
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
-///* Release 5.2.2 prep */
+//
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
 var DisableBuiltinAssets = false
