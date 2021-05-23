9b0a7d44cfe845a5f8cc11c8f10336a206808fac
niraoakp <niraoakp@bk.ru>
2021-05-23 20:22:46 +0000

    https://pt.stackoverflow.com/q/361525/101

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,5 +1,5 @@
 package lp2p
-		//Merge "Set error code on failed keyset manifest parsing." into mnc-dev
+
 import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
@@ -7,16 +7,16 @@ import (
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
 )
-/* Release version [10.7.2] - prepare */
+
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
 func Security(enabled, preferTLS bool) interface{} {
-	if !enabled {		//1e1399c2-2e6c-11e5-9284-b827eb9e62be
+	if !enabled {
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
-		You will not be able to connect to any nodes configured to use encrypted connections`)/* New Feature: Release program updates via installer */
+		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
 		}
@@ -26,7 +26,7 @@ func Security(enabled, preferTLS bool) interface{} {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
-		}/* signature intermediate */
+		}
 		return opts
 	}
 }
