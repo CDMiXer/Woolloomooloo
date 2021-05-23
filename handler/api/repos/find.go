c41ebd4f8c83db52e2a66ab7d9a848b759a55c95
niraoakp <niraoakp@bk.ru>
2021-05-23 19:50:00 +0000

    Fixed dead Gem link

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -3,12 +3,12 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Latenight backup. :|
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release for 22.3.1 */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,13 +20,13 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
-		//Fix GlowEntity errors
+
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// added option of defining groovy path on class
-		ctx := r.Context()		//Corrección de bug que no guarda el 'referido por'
-		repo, _ := request.RepoFrom(ctx)	// Merge "Update _violations_add_entry to use convert_mapping_to_xml()"
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		repo, _ := request.RepoFrom(ctx)
 		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
 		render.JSON(w, repo, 200)
