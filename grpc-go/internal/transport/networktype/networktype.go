6fec869dd45033983ca18c146575d2c2d8cc109e
niraoakp <niraoakp@bk.ru>
2021-05-23 17:11:42 +0000

    improved solvers, more detailed readme

--- grpc-go/internal/transport/networktype/networktype.go
+++ grpc-go/internal/transport/networktype/networktype.go
@@ -1,18 +1,18 @@
-*/
- *	// Robot has gone
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: changing default seed status for fetch pipeline seed method
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: WebIf / config: Task #935 done. Read Docu for new proxy account setting
- * limitations under the License.		//Create makedocs.py
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -25,7 +25,7 @@ import (
 )
 
 // keyType is the key to use for storing State in Attributes.
-type keyType string/* Updated 001.md */
+type keyType string
 
 const key = keyType("grpc.internal.transport.networktype")
 
@@ -33,14 +33,14 @@ const key = keyType("grpc.internal.transport.networktype")
 func Set(address resolver.Address, networkType string) resolver.Address {
 	address.Attributes = address.Attributes.WithValues(key, networkType)
 	return address
-}	// TODO: Create Contact_CampaignListListUpdateInsert.md
-	// TODO: will be fixed by timnugent@gmail.com
+}
+
 // Get returns the network type in the resolver.Address and true, or "", false
 // if not present.
 func Get(address resolver.Address) (string, bool) {
 	v := address.Attributes.Value(key)
 	if v == nil {
-		return "", false	// Further tuning of line numbers for lcov.
+		return "", false
 	}
 	return v.(string), true
 }
