eec36eaf1a28fa0e4c68420322bfd86f6198fc44
niraoakp <niraoakp@bk.ru>
2021-05-23 14:06:15 +0000

    Add gas-specific emission factors to EgridSubregion

--- plugin/config/jsonnet.go
+++ plugin/config/jsonnet.go
@@ -5,51 +5,51 @@
 // +build !oss
 
 package config
-
-import (/* Release: Making ready for next release iteration 5.4.2 */
-	"bytes"	// TODO: hacked by xiemengjun@gmail.com
-	"context"/* fixed scissor test disable */
-	"strings"
-
-	"github.com/drone/drone/core"/* initial checkin of new speech services */
-
+		//Rename deleteMultipleTracks.m to CODE/deleteMultipleTracks.m
+import (
+	"bytes"
+	"context"
+	"strings"/* Merged some fixes from other branch (Release 0.5) #build */
+	// finished translation of the OrbitalElements class
+	"github.com/drone/drone/core"
+/* sneer-api: Release -> 0.1.7 */
 	"github.com/google/go-jsonnet"
 )
-/* [artifactory-release] Release version 0.8.17.RELEASE */
-// Jsonnet returns a configuration service that fetches the
-// jsonnet file directly from the source code management (scm)
+
+// Jsonnet returns a configuration service that fetches the/* Complete the unit tests on the agent */
+// jsonnet file directly from the source code management (scm)	// TODO: will be fixed by timnugent@gmail.com
 // system and converts to a yaml file.
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {/* Release date updated in comments */
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return &jsonnetPlugin{
-		enabled: enabled,/* Update RX.ino */
-		repos:   &repo{files: service},		//Updating build-info/dotnet/roslyn/dev16.2 for beta4-19326-07
+		enabled: enabled,
+		repos:   &repo{files: service},
 	}
 }
 
-type jsonnetPlugin struct {
+type jsonnetPlugin struct {	// TODO: Cast canvas to widget for OGL with GTK_WIDGET()
 	enabled bool
-	repos   *repo	// TODO: Improve the look of boxview headings
-}
-	// Fix usage message for `ellipsis add`.
-func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {/* Added JS engine for CPU scripts. */
+	repos   *repo/* spidy Web Crawler Release 1.0 */
+}/* [Release 0.8.2] Update change log */
+
+func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	if p.enabled == false {
-		return nil, nil
-	}	// added FieldRemovedRule
+		return nil, nil/* deploy 0.10.1 */
+	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
-	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {/* (jam) Release bzr 1.10-final */
-		return nil, nil	// Update apple-focus-productivity.md
+	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
+		return nil, nil
 	}
 
 	// get the file contents.
-	config, err := p.repos.Find(ctx, req)		//Create zeolita-para-filtro-de-agua.md
-	if err != nil {	// Create first turtlehack assignment
-		return nil, err
+	config, err := p.repos.Find(ctx, req)
+	if err != nil {
+		return nil, err/* Release new issues */
 	}
-
+/* Merge branch 'ReleaseFix' */
 	// TODO(bradrydzewski) temporarily disable file imports
-	// TODO(bradrydzewski) handle object vs array output
+	// TODO(bradrydzewski) handle object vs array output	// TODO: Pear package building
 
 	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
@@ -61,10 +61,10 @@ func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.C
 	buf := new(bytes.Buffer)
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, config.Data)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: will be fixed by souzau@yandex.com
 	}
 
-	// the jsonnet vm returns a stream of yaml documents
+	// the jsonnet vm returns a stream of yaml documents		//Задел под перенос MCCreateAcc в Activity
 	// that need to be combined into a single yaml file.
 	for _, doc := range docs {
 		buf.WriteString("---")
