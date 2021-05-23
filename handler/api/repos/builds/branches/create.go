a1b0b344f76cfd7804366851ec407f8ec7475afd
niraoakp <niraoakp@bk.ru>
2021-05-23 08:27:58 +0000

    version 81.0.4044.17

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Image: improve animated gif detection
-///* Merge "Release 3.2.3.333 Prima WLAN Driver" */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release 2.6.2 */
-// you may not use this file except in compliance with the License./* New post: Test Blog Title */
-// You may obtain a copy of the License at/* Merge "Add logging fixture to integration tests" */
-//		//fix compiler errors for context class
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package branches	// Added active states for all button types
+package branches
