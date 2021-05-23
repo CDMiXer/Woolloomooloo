bde560e2ecb9ac3318cda55b6104b9af73bf1bd1
niraoakp <niraoakp@bk.ru>
2021-05-23 06:31:52 +0000

    Create spin.scss

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
-#!/bin/bash	// Merge "fix neutron-lib grafana dashboard"
+#!/bin/bash
 set -eux -o pipefail
 
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
-  "deepcopy,client,informer,lister" \/* updated README and POM.xml files to version 0.0.5-SNAPSHOT */
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \	// TODO: oledb32 update
+  "deepcopy,client,informer,lister" \
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
   workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
