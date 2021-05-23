5a5ce23b6719f262d840664f110797da1ee7d68d
niraoakp <niraoakp@bk.ru>
2021-05-23 18:08:20 +0000

    Added some information about flashing (linux)

--- handler/api/secrets/delete_test.go
+++ handler/api/secrets/delete_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Fixing type of fifo2.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -10,14 +10,14 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"		//Added a User-Agent to PirateBay searching.
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* Merged hotfixRelease_v1.4.0 into release_v1.4.0 */
-/* Release 061 */
+	"github.com/drone/drone/mock"
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"	// releasing version 0.64
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -26,19 +26,19 @@ func TestHandleDelete(t *testing.T) {
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)	// TODO: will be fixed by boringland@protonmail.ch
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
-		//[fix] runbot green
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// TODO: hacked by nick@perfectabstractions.com
+
 	HandleDelete(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -54,18 +54,18 @@ func TestHandleDelete_SecretNotFound(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
-	c.URLParams.Add("name", "github_password")/* WIP. implementing kite flag system */
-	// TODO: will be fixed by lexy8russo@outlook.com
-	w := httptest.NewRecorder()/* Release of eeacms/plonesaas:5.2.1-34 */
+	c.URLParams.Add("name", "github_password")
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(secrets).ServeHTTP(w, r)	// TODO: will be fixed by 13860583249@yeah.net
-	if got, want := w.Code, http.StatusNotFound; want != got {	// Update SeaMonkey-web-browser.desktop
-		t.Errorf("Want response code %d, got %d", want, got)		//simplify & performance turning 
-	}	// TODO: [Fix] base_report_designer: set default protocol
+	HandleDelete(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
@@ -77,7 +77,7 @@ func TestHandleDelete_SecretNotFound(t *testing.T) {
 func TestHandleDelete_DeleteError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Update requested by Ivan
+
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(errors.ErrNotFound)
