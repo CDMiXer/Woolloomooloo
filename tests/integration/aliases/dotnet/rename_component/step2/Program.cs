16f4269ec17e2be07def05f342a8fa60746831a0
niraoakp <niraoakp@bk.ru>
2021-05-23 04:01:23 +0000

    Add coveralls to requirements.txt

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* fixed sign button */
-using System.Threading.Tasks;/* Release for 4.5.0 */
+
+using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{	// Create Setting_up_Firebase_for_Android.md
+{
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)/* ee30defc-2e63-11e5-9284-b827eb9e62be */
+        : base("my:module:Resource", name, options)
     {
     }
 }
-	// fix: path to ideino
+
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component itself.
 class ComponentThree : ComponentResource
@@ -20,10 +20,10 @@ class ComponentThree : ComponentResource
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
-    {	// Merge "Switch nova console type from spice to novnc"
+    {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// Deleted _posts/faqs/1-where-are-they.md
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });	// TODO: Create Eventos “9a2332a1-9235-47ba-bf6b-c3aeb6f517d0”
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
@@ -34,9 +34,9 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {	// TODO: Update readme to trigger travis
+        {
             // Applying an alias to the instance successfully renames both the component and the children.
-            var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions		//chore(package): update @commitlint/cli to version 6.0.1
+            var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp3" } },
             });
