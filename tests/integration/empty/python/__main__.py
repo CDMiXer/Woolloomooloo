7df0991d32e62eedea42710444fd8efa472fd942
niraoakp <niraoakp@bk.ru>
2021-05-23 06:42:43 +0000

    Fix closeGap for side walls of cylinders and cones

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,6 +1,6 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Can now save algorithm via user interface input */
 
-def main():/* Bumped Version for Release */
+def main():
     return None
 
 if __name__ == "__main__":
