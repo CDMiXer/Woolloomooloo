36f463b546b1aecd53b0f71d843a417e6f1fd0ff
niraoakp <niraoakp@bk.ru>
2021-05-23 15:16:39 +0000

    UMA test clients should be trusted clients

--- cmd/drone-server/inject_runner.go
+++ cmd/drone-server/inject_runner.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by souzau@yandex.com
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by cory@protocol.ai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,49 +9,49 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 1.0.0 (#293) */
-// limitations under the License./* Delete dWord1.hex */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package main
 
 import (
-	"github.com/drone/drone-runtime/engine/docker"	// TODO: hacked by arachnid@notdot.net
+	"github.com/drone/drone-runtime/engine/docker"
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/operator/manager"/* Release 3.0.0. Upgrading to Jetty 9.4.20 */
+	"github.com/drone/drone/operator/manager"
 	"github.com/drone/drone/operator/runner"
 
 	"github.com/google/wire"
-	"github.com/sirupsen/logrus"	// add folder commands for custom config
+	"github.com/sirupsen/logrus"
 )
 
 // wire set for loading the server.
 var runnerSet = wire.NewSet(
 	provideRunner,
 )
-	// TODO: Modified buildOn:
+
 // provideRunner is a Wire provider function that returns a
 // local build runner configured from the environment.
 func provideRunner(
 	manager manager.BuildManager,
 	secrets core.SecretService,
 	registry core.RegistryService,
-	config config.Config,	// TODO: hacked by ligi@ligi.de
+	config config.Config,
 ) *runner.Runner {
-	// the local runner is only created when the nomad scheduler,/* Updated copyright dates in license file */
+	// the local runner is only created when the nomad scheduler,
 	// kubernetes scheduler, and remote agents are disabled
-	if config.Nomad.Enabled || config.Kube.Enabled || (config.Agent.Disabled == false) {	// 7c53d1a6-2e6b-11e5-9284-b827eb9e62be
+	if config.Nomad.Enabled || config.Kube.Enabled || (config.Agent.Disabled == false) {
 		return nil
-	}/* Merge "Merge of (#10164) to Vaadin 7." */
+	}
 	engine, err := docker.NewEnv()
 	if err != nil {
-		logrus.WithError(err).	// TODO: hacked by hi@antfu.me
+		logrus.WithError(err).
 			Fatalln("cannot load the docker engine")
-		return nil	// New airspeed indicator firmware
+		return nil
 	}
 	return &runner.Runner{
 		Platform:   config.Runner.Platform,
-		OS:         config.Runner.OS,/* Fixed cursor issue */
+		OS:         config.Runner.OS,
 		Arch:       config.Runner.Arch,
 		Kernel:     config.Runner.Kernel,
 		Variant:    config.Runner.Variant,
@@ -61,7 +61,7 @@ func provideRunner(
 		Registry:   registry,
 		Volumes:    config.Runner.Volumes,
 		Networks:   config.Runner.Networks,
-		Devices:    config.Runner.Devices,/* Merge "msm: camera: Release mutex lock in case of failure" */
+		Devices:    config.Runner.Devices,
 		Privileged: config.Runner.Privileged,
 		Machine:    config.Runner.Machine,
 		Labels:     config.Runner.Labels,
