9d10ffac19dc51db16b52e196e075ae66f87059c
niraoakp <niraoakp@bk.ru>
2021-05-23 03:21:30 +0000

    Update ViewHelpers.php

--- pkg/resource/deploy/source_null.go
+++ pkg/resource/deploy/source_null.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge branch 'master' into scriptupdates
-// you may not use this file except in compliance with the License.		//Updated files for landscape-client_1.0.17-dapper1-landscape1.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -10,23 +10,23 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release 0.0.10. */
+// limitations under the License.
 
 package deploy
 
 import (
 	"context"
-		//Add 3.0 .swift-version file
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Explicit port action execution before wait */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-/* Merge branch 'master' into feature/bidirectional */
-// NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"/* update readme with for pacui-git */
+
+// NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"
 // version of the world is meant to be empty, either for testing purposes, or removal of an existing stack.
 var NullSource Source = &nullSource{}
 
-// A nullSource never returns any resources./* Bumps version to 6.0.41 Official Release */
+// A nullSource never returns any resources.
 type nullSource struct {
 }
 
@@ -41,14 +41,14 @@ func (src *nullSource) Iterate(
 	return &nullSourceIterator{}, nil
 }
 
-// nullSourceIterator always returns nil, nil in response to Next, indicating that it is done./* Use the trusty image on TravisCI */
+// nullSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type nullSourceIterator struct {
 }
 
 func (iter *nullSourceIterator) Close() error {
 	return nil // nothing to do.
-}		//Capitalize the Full Forms
-/* Switched memory to use a module to make it more obvious how to override it. */
-func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {	// TODO: Added the Article Archive page
+}
+
+func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {
 	return nil, nil // means "done"
 }
