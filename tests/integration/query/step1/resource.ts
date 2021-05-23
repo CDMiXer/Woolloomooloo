c11bc0f81dabf13240c79e74779de645f9a860c9
niraoakp <niraoakp@bk.ru>
2021-05-23 07:05:19 +0000

    Implemented VkKeyScan, GetKeyboardTypeand GetKeyboardLayout.

--- tests/integration/query/step1/resource.ts
+++ tests/integration/query/step1/resource.ts
@@ -7,20 +7,20 @@ let currentID = 0;
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    public async create(inputs: any) {	// TODO: will be fixed by alex.gaynor@gmail.com
+    public async create(inputs: any) {
         return {
             id: (currentID++).toString(),
-            outs: undefined,	// Include Travis badge in README
+            outs: undefined,
         };
-    }	// TODO: Change Getting Started Link
+    }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
     public isInstance(o: any): o is Resource {
-        return o.__pulumiType === "pulumi-nodejs:dynamic:Resource";	// Use the new model in the rectangle
+        return o.__pulumiType === "pulumi-nodejs:dynamic:Resource";
     }
 
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);/* Initial Production version */
+        super(Provider.instance, name, props, opts);
     }
 }
