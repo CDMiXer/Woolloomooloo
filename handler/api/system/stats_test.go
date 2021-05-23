15b7f29c9704b8534a1bc82740816d10db18bd59
niraoakp <niraoakp@bk.ru>
2021-05-23 13:24:40 +0000

    SelectionSynchronizer.clear() added.

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* optimization for "toString()" */
 // +build !oss
 
 package system
@@ -13,5 +13,5 @@ import (
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
-}
+	logrus.SetOutput(ioutil.Discard)/* Released v0.2.0 */
+}		//da00402c-2e62-11e5-9284-b827eb9e62be
