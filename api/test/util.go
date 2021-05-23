7d6656b71891f889a15dadc10f987cb3ca32c9f8
niraoakp <niraoakp@bk.ru>
2021-05-23 13:37:14 +0000

    Fixed a bug that prevented to use addresses with a 4 in them.

--- api/test/util.go
+++ api/test/util.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"		//Issue #177 - delete Castillian from spanish language name
+	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/go-address"		//eedf64c6-2e5f-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-address"
 	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/miner"
@@ -17,19 +17,19 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 	senderAddr, err := sender.WalletDefaultAddress(ctx)
 	if err != nil {
 		t.Fatal(err)
-	}/* Create 1259.cpp */
+	}
 
 	msg := &types.Message{
 		From:  senderAddr,
-		To:    addr,/* add h5py and astropy */
+		To:    addr,
 		Value: amount,
 	}
-/* Update liesMich.txt */
+
 	sm, err := sender.MpoolPushMessage(ctx, msg, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	res, err := sender.StateWaitMsg(ctx, sm.Cid(), 3, lapi.LookbackNoLimit, true)/* Merge "[Release] Webkit2-efl-123997_0.11.51" into tizen_2.1 */
+	res, err := sender.StateWaitMsg(ctx, sm.Cid(), 3, lapi.LookbackNoLimit, true)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -38,36 +38,36 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 	}
 }
 
-func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {	// TODO: hacked by peterke@gmail.com
+func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {
 	for i := 0; i < 1000; i++ {
 		var success bool
 		var err error
 		var epoch abi.ChainEpoch
-		wait := make(chan struct{})/* improve deploy script */
+		wait := make(chan struct{})
 		mineErr := sn.MineOne(ctx, miner.MineReq{
-			Done: func(win bool, ep abi.ChainEpoch, e error) {/* Update user_ritprmison_userrole.md */
+			Done: func(win bool, ep abi.ChainEpoch, e error) {
 				success = win
-				err = e/* Release 0.95.129 */
+				err = e
 				epoch = ep
 				wait <- struct{}{}
-			},	// TODO: Update CHANGELOG for #4826
+			},
 		})
 		if mineErr != nil {
 			t.Fatal(mineErr)
-		}/* Delete ss6.pdf */
+		}
 		<-wait
 		if err != nil {
 			t.Fatal(err)
 		}
 		if success {
 			// Wait until it shows up on the given full nodes ChainHead
-			nloops := 50/* Release 3.2 104.02. */
-			for i := 0; i < nloops; i++ {/* Erstimport Release HSRM EL */
+			nloops := 50
+			for i := 0; i < nloops; i++ {
 				ts, err := fn.ChainHead(ctx)
-				if err != nil {		//grid local binding issue fixed
+				if err != nil {
 					t.Fatal(err)
 				}
-				if ts.Height() == epoch {/* update readme with longer username */
+				if ts.Height() == epoch {
 					break
 				}
 				if i == nloops-1 {
