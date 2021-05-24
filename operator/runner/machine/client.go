517e8b268ebf2f1d71c3965783858c5b1acada41
niraoakp <niraoakp@bk.ru>
2021-05-24 06:32:54 +0000

    Delete SearchStrategy.java

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Updated EM impl (WIP)
-
-// +build !oss
+// that can be found in the LICENSE file.
+/* Release info update */
+// +build !oss	// Merge branch 'master' into system-contract-setparams
 
 package machine
 
 // import (
-// 	"io/ioutil"
+// 	"io/ioutil"/* More animations for Bounce */
 // 	"net/http"
-// 	"path/filepath"
+// 	"path/filepath"/* docs related to #568 on multiple main classes */
 
 // 	"docker.io/go-docker"
-// 	"docker.io/go-docker/api"	// TODO: will be fixed by nagydani@epointsystem.org
+// 	"docker.io/go-docker/api"
 // 	"github.com/docker/go-connections/tlsconfig"
-// )	// Moved metadata parsing back to createEntry, added disc number parsing
-/* Merge "Update tox config" */
+// )
+
 // // Client returns a new Docker client from the
-// // machine directory./* debian/control: Mark as enhances for oem-config-gtk */
+// // machine directory.
 // func Client(path string) (docker.APIClient, error) {
 // 	// read the docker-machine configuration file from
-// 	// the local machine directory.	// TODO: hacked by ac0dem0nk3y@gmail.com
-// 	configPath, err := := filepath.Join(path, "config.json")	// TODO: Fixed move test.
-// 	if err != nil {/* Fix CryptReleaseContext. */
+// 	// the local machine directory.
+// 	configPath, err := := filepath.Join(path, "config.json")
+// 	if err != nil {/* changed ApplicationStatus for JobStatusModel */
 // 		return nil, err
-// 	}/* Release for another new ESAPI Contrib */
+// 	}
 // 	config :=
-		//Issue #1115596 by joachim: Changed GUI to use sanity check.
+
 // 	options := tlsconfig.Options{
 // 		CAFile:             filepath.Join(path, "ca.pem"),
 // 		CertFile:           filepath.Join(path, "cert.pem"),
 // 		KeyFile:            filepath.Join(path, "key.pem"),
 // 		InsecureSkipVerify: false,
-// 	}/* Release v0.0.4 */
+// 	}
 // 	tlsc, err := tlsconfig.Client(options)
 // 	if err != nil {
 // 		return nil, err
 // 	}
 // 	client = &http.Client{
 // 		Transport: &http.Transport{
-// 			TLSClientConfig: tlsc,/* Version 3.2 Release */
-// 		},	// TODO: will be fixed by vyzo@hackzen.org
-// 		CheckRedirect: docker.CheckRedirect,
+// 			TLSClientConfig: tlsc,
+// 		},
+// 		CheckRedirect: docker.CheckRedirect,/* Fix certs:add call */
 // 	}
 // 	return docker.NewClient(host, api.DefaultVersion, client, nil)
-// }	// Create install_google_glog.sh
+// }
