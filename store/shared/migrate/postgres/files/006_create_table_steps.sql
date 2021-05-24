951f01d13f5e4ac5f1f2bf780b4ce29587d0aff9
niraoakp <niraoakp@bk.ru>
2021-05-24 11:24:27 +0000

    Fixed git updater

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -4,17 +4,17 @@ CREATE TABLE IF NOT EXISTS steps (
  step_id          SERIAL PRIMARY KEY
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
-,step_name        VARCHAR(100)	// TODO: Merge "Grant HeifWriterTest read/write permission by rules" into pi-androidx-dev
+,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER		//Implementing additional methods to attach data to output file.
+,step_exit_code   INTEGER
 ,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-);/* CyFluxViz Release v0.88. */
-	// TODO: hacked by boringland@protonmail.ch
+);
+
 -- name: create-index-steps-stage
 
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
