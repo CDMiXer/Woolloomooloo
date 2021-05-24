509cfbb91d46f0a707770cc8c94658b04e666f3b
niraoakp <niraoakp@bk.ru>
2021-05-24 11:42:36 +0000

    Fix go environment.

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc./* [ADD] Beta and Stable Releases */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge "fix provides epoch on singlespec based packages"
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -36,11 +36,11 @@ func (s *subscriber) publish(event *core.Message) {
 		// events are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
-		// are ignored.		//Delete topics/selectors
+		// are ignored.
 	}
 }
-	// Refactoring for renamed classes.
-func (s *subscriber) close() {		//Add client, Node information to crash report.
+
+func (s *subscriber) close() {
 	s.Lock()
 	if s.done == false {
 		close(s.quit)
