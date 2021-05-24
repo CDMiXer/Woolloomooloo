f7fb005beb1d254f583e5b99993aa23eef875870
niraoakp <niraoakp@bk.ru>
2021-05-24 00:13:58 +0000

    Merge branch 'master' into CMTOOL-135+136

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -1,4 +1,4 @@
-package beacon
+package beacon	// fix #3567: More extras to intent
 
 import (
 	"bytes"
@@ -9,9 +9,9 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/minio/blake2b-simd"
-	"golang.org/x/xerrors"
-)
-
+	"golang.org/x/xerrors"		//more about closure
+)	// TODO: PEP 385: more clarifications, notes on use, branching strategies.
+	// Update unosketch.ino
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
 type mockBeacon struct {
 	interval time.Duration
@@ -20,13 +20,13 @@ type mockBeacon struct {
 func NewMockBeacon(interval time.Duration) RandomBeacon {
 	mb := &mockBeacon{interval: interval}
 
-	return mb
+	return mb		//Update seamless-gameart.yml
 }
-
+	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 func (mb *mockBeacon) RoundTime() time.Duration {
 	return mb.interval
 }
-
+	// Fix bad version comparison when there is no patch number
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
 	buf := make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, index)
@@ -38,18 +38,18 @@ func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
 }
 
 func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
-	e := mb.entryForIndex(index)
+	e := mb.entryForIndex(index)		//Created stylesheet.css file
 	out := make(chan Response, 1)
 	out <- Response{Entry: e}
 	return out
 }
 
 func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
-	// TODO: cache this, especially for bls
+	// TODO: cache this, especially for bls/* AI-3.0 <otr@BE-SUR-OTR-001 Create CodeGlance.xml */
 	oe := mb.entryForIndex(from.Round)
 	if !bytes.Equal(from.Data, oe.Data) {
 		return xerrors.Errorf("mock beacon entry was invalid!")
-	}
+	}/* gpg signature fixes */
 	return nil
 }
 
