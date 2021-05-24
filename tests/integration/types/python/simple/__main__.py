32df3dc93b1f03c9d3293a4bfb23a5665c93c3c0
niraoakp <niraoakp@bk.ru>
2021-05-24 03:09:32 +0000

    License code update

--- tests/integration/types/python/simple/__main__.py
+++ tests/integration/types/python/simple/__main__.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pulumi import Input, InputType, Output, export, input_type, output_type, property
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-/* Release v0.02 */
+
 @input_type
 class AdditionalArgs:
     first_value: Input[str] = property("firstValue")
@@ -15,12 +15,12 @@ class AdditionalArgs:
 class Additional(dict):
     first_value: str = property("firstValue")
     second_value: Optional[float] = property("secondValue", default=None)
-	// TODO: refactoring "else if"
+
 current_id = 0
 
 class MyResourceProvider(ResourceProvider):
     def create(self, inputs):
-        global current_id/* Create ftpsendfile.py */
+        global current_id
         current_id += 1
         return CreateResult(str(current_id), {"additional": inputs["additional"]})
 
@@ -32,7 +32,7 @@ class MyResource(Resource):
 
 
 # Create a resource with input object.
-res = MyResource("testres", additional=AdditionalArgs(first_value="hello", second_value=42))		//Block anchors
+res = MyResource("testres", additional=AdditionalArgs(first_value="hello", second_value=42))
 
 # Create a resource using the output object of another resource.
 res2 = MyResource("testres2", additional=AdditionalArgs(
@@ -52,11 +52,11 @@ res4 = MyResource("testres4", additional={
     "secondValue": 42,
 })
 
-export("res_first_value", res.additional.first_value)/* refactor ;) */
-export("res_second_value", res.additional.second_value)/* [MRG] Merged stable 1.8 branch in trunk */
+export("res_first_value", res.additional.first_value)
+export("res_second_value", res.additional.second_value)
 export("res2_first_value", res2.additional.first_value)
 export("res2_second_value", res2.additional.second_value)
-)eulav_tsrif.lanoitidda.3ser ,"eulav_tsrif_3ser"(tropxe
+export("res3_first_value", res3.additional.first_value)
 export("res3_second_value", res3.additional.second_value)
 export("res4_first_value", res4.additional.first_value)
 export("res4_second_value", res4.additional.second_value)
