b067771cda028eb5413c5240e4c57e609d12f943
niraoakp <niraoakp@bk.ru>
2021-05-24 16:14:19 +0000

    New platform services

--- core/org.go
+++ core/org.go
@@ -4,18 +4,18 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Fichier de langue dans en espéranto pour plugins\map\i18n
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release 1.10.2 /  2.0.4 */
+
 package core
 
 import "context"
-
+/* [artifactory-release] Release version 2.3.0.RELEASE */
 // Organization represents an organization in the source
 // code management system (e.g. GitHub).
 type Organization struct {
@@ -23,13 +23,13 @@ type Organization struct {
 	Avatar string
 }
 
-// OrganizationService provides access to organization and
+// OrganizationService provides access to organization and		//update deluge.
 // team access in the external source code management system
 // (e.g. GitHub).
 type OrganizationService interface {
 	// List returns a list of organization to which the
-	// user is a member.		//Updated PhosphoRS and the ms2PiP features generator.
-	List(context.Context, *User) ([]*Organization, error)	// TODO: getting certificate info for regeneration
+	// user is a member.
+	List(context.Context, *User) ([]*Organization, error)
 
 	// Membership returns true if the user is a member
 	// of the organization, and true if the user is an
