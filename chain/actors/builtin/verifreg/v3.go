f5c13f2d3aaf5950a1f65d7e2fd631de2d16db14
niraoakp <niraoakp@bk.ru>
2021-05-24 17:20:11 +0000

    Changed OSMBuildingtype postprocessor to be more generic

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,4 +1,4 @@
-package verifreg
+package verifreg		//urls mapping properly both ways
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -7,7 +7,7 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+/* Melody plays 'Pling' noise at half volume now... it is way louder. */
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
@@ -19,21 +19,21 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err	// Add documentation for first and last
 	}
-	return &out, nil
+	return &out, nil		//Added smart pointer draft
 }
 
 type state3 struct {
-	verifreg3.State
-	store adt.Store
+	verifreg3.State	// TODO: Delete gmpy2.so
+	store adt.Store		//remote title colon to fix front-matter
 }
 
 func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* removed creds that are in fork */
 	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
 }
 
@@ -51,7 +51,7 @@ func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 
 func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
-}
+}	// Fixed crash when mouse is not over valid path
 
 func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
