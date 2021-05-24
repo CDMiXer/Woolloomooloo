a4d70b6c7263e7d4256e10b9087a600e7ff70e30
niraoakp <niraoakp@bk.ru>
2021-05-24 08:48:09 +0000

    fixed typo in UIAlertView blocks example

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,57 +1,57 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-		//Create email_credentials.txt
+
 package ints
 
-import (	// TODO: HSeaRIWEOvJ7DTWoNE1cQ3axNU12egnx
+import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"	// TODO: Add upload to codecov.
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
-)/* 8f50e76e-2e46-11e5-9284-b827eb9e62be */
+)
 
-// Test that the engine tolerates two deletions of the same URN in the same plan./* Amends js so button can only be clicked once	 */
+// Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestDoublePendingDelete(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},/* It not Release Version */
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:           "step2",/* Create just-enough-clojure.clj */
-				Additive:      true,/* Update thesis_main.tex */
+				Dir:           "step2",
+				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
 
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))/* Release of 3.3.1 */
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "a", string(a.URN.Name()))/* added dbus config file */
-					assert.False(t, a.Delete)/* merge changeset 11050 from trunk */
+					assert.Equal(t, "a", string(a.URN.Name()))
+					assert.False(t, a.Delete)
 
 					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
 
-					b := stackInfo.Deployment.Resources[4]		//Added Armour Toughness to oldArmourStrength module
+					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
-					assert.False(t, b.Delete)		//Added button to map tab
+					assert.False(t, b.Delete)
 
-				},/* #208 - Release version 0.15.0.RELEASE. */
+				},
 			},
 			{
 				Dir:           "step3",
-				Additive:      true,		//update readme with for pacui-git
+				Additive:      true,
 				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Fix URL to Apache-2.0 license.
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// There is still only one pending delete resource in this snapshot.
 					assert.NotNil(t, stackInfo.Deployment)
 
