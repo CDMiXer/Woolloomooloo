bf92f486049d3790461be2250cdbdbf0e6d58b61
niraoakp <niraoakp@bk.ru>
2021-05-24 01:28:35 +0000

    Recommend block image syntax instead of inline image syntax

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,10 +1,10 @@
 package sealing
-
-import (
+	// TODO: Added/modified ...2String methods
+import (		//e2e08aa8-2e58-11e5-9284-b827eb9e62be
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by onhardev@bk.ru
-)		//javastravav3api#143 Add name to route meta
+	"github.com/filecoin-project/go-state-types/abi"
+)
 
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
@@ -12,4 +12,4 @@ type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
-}
+}/* moved composition logic out of UrlRule inside composition component. */
