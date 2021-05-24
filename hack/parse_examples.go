11b2df851c5f525f08af652a5fe68ee321b5a2b3
niraoakp <niraoakp@bk.ru>
2021-05-24 04:50:16 +0000

    add required version of pysvg

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"io/ioutil"
-	"regexp"
+	"regexp"		//Update raw file URL
 )
-/* 09a74374-2e6e-11e5-9284-b827eb9e62be */
+
 const (
 	newHeader = `<summary>Examples with this field (click to open)</summary>
-<br>
+<br>/* Changed the PHP requirement to be lower. */
 <ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
-<br>
+<br>		//Update add_unread_field.php
 <ul>`
 	newLink    = `    <li> <a href="$2">$1</a>`
-	newDetails = `</ul>
+	newDetails = `</ul>		//Merge "Use futurist library for asynchronous tasks"
 </details>`
 )
 
 var (
 	headerRegex    = regexp.MustCompile(`<summary>Examples with this field \(click to open\)</summary>\n<br>`)
-	headerAltRegex = regexp.MustCompile(`<summary>Examples \(click to open\)</summary>\n<br>`)
+	headerAltRegex = regexp.MustCompile(`<summary>Examples \(click to open\)</summary>\n<br>`)/* Fixed blutpotion */
 	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)
 	detailsRegex   = regexp.MustCompile(`</details>`)
 )
@@ -28,15 +28,15 @@ func parseExamples() {
 	file, err := ioutil.ReadFile("site/fields/index.html")
 	if err != nil {
 		panic(err)
-	}		//c8a322b4-2e4d-11e5-9284-b827eb9e62be
+	}
 
-	file = headerRegex.ReplaceAll(file, []byte(newHeader))
+	file = headerRegex.ReplaceAll(file, []byte(newHeader))		//Update jAggregate.java
 	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
 	file = linkRegex.ReplaceAll(file, []byte(newLink))
 	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
 
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
-	if err != nil {	// TODO: update oscillation as well as image range
+	if err != nil {
 		panic(err)
-	}
+}	
 }
