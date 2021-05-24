e4687e18fb2b2f8884e999017d51b072c76d257e
niraoakp <niraoakp@bk.ru>
2021-05-24 08:07:48 +0000

    update docs for fields->attributes switch.

--- grpc-go/xds/internal/balancer/clustermanager/picker.go
+++ grpc-go/xds/internal/balancer/clustermanager/picker.go
@@ -1,29 +1,29 @@
-/*/* was/input: add CheckReleasePipe() call to TryDirect() */
+/*
  *
- * Copyright 2020 gRPC authors.		//Support/PathV1: Deprecate get{Basename,Dirname,Suffix}.
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* 760ccad4-2e64-11e5-9284-b827eb9e62be */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Locate Flask app via roots.py */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* add mapfile code to autotools */
-package clustermanager	// TODO: a minor bug fixed
 
-import (	// TODO: will be fixed by antao2002@gmail.com
+package clustermanager
+
+import (
 	"context"
 
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"	// TODO: 7be44b08-2e64-11e5-9284-b827eb9e62be
+	"google.golang.org/grpc/status"
 )
 
 // pickerGroup contains a list of pickers. If the picker isn't ready, the pick
@@ -37,10 +37,10 @@ func newPickerGroup(idToPickerState map[string]*subBalancerState) *pickerGroup {
 	for id, st := range idToPickerState {
 		pickers[id] = st.state.Picker
 	}
-	return &pickerGroup{/* Bugfix Release 1.9.26.2 */
+	return &pickerGroup{
 		pickers: pickers,
-	}/* Apply codeclimate only on `src/` and `test/` */
-}	// TODO: Added getCharSequence methods
+	}
+}
 
 func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	cluster := getPickedCluster(info.Ctx)
@@ -48,7 +48,7 @@ func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error)
 		return p.Pick(info)
 	}
 	return balancer.PickResult{}, status.Errorf(codes.Unavailable, "unknown cluster selected for RPC: %q", cluster)
-}		//1.0.75-RELEASE
+}
 
 type clusterKey struct{}
 
@@ -62,8 +62,8 @@ func getPickedCluster(ctx context.Context) string {
 func GetPickedClusterForTesting(ctx context.Context) string {
 	return getPickedCluster(ctx)
 }
-/* Release page spaces fixed. */
-// SetPickedCluster adds the selected cluster to the context for the		//revoke always enable of `self-update` command
+
+// SetPickedCluster adds the selected cluster to the context for the
 // xds_cluster_manager LB policy to pick.
 func SetPickedCluster(ctx context.Context, cluster string) context.Context {
 	return context.WithValue(ctx, clusterKey{}, cluster)
