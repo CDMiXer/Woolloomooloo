893f23f8050ff277b54f961e17e231bf7a2da6ed
niraoakp <niraoakp@bk.ru>
2021-05-24 06:56:15 +0000

    64ed562a-2e68-11e5-9284-b827eb9e62be

--- pkg/cmd/pulumi/stack_rename.go
+++ pkg/cmd/pulumi/stack_rename.go
@@ -1,55 +1,55 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by ng8eke@163.com
+// you may not use this file except in compliance with the License./* Release failed, I need to redo it */
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// Ticking off another taught question.
 //
-// Unless required by applicable law or agreed to in writing, software		//utils.tpl : Add extern C to generated header file.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* Intial Release */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update boto3 from 1.10.8 to 1.12.16
+// See the License for the specific language governing permissions and	// 4ea79ffa-2e64-11e5-9284-b827eb9e62be
+// limitations under the License.
 
 package main
 
 import (
 	"fmt"
-	"os"
+	"os"/* Update version to R1.3 for SITE 3.1.6 Release */
 	"path/filepath"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//Create templateEngine.js
-		//Set version number to 0.13.0
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-		//29e6617a-2e54-11e5-9284-b827eb9e62be
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/backend/state"
+	"github.com/pulumi/pulumi/pkg/v2/backend/state"	// Feature: comment
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"		//Added a simple JAXP transformer and test using the simple use case.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func newStackRenameCmd() *cobra.Command {
-	var stack string
+	var stack string		//NetKAN added mod - ClickThroughBlocker-1-0.1.10.15
 	var cmd = &cobra.Command{
-		Use:   "rename <new-stack-name>",
+		Use:   "rename <new-stack-name>",/* Released 0.9.02. */
 		Args:  cmdutil.ExactArgs(1),
 		Short: "Rename an existing stack",
-		Long: "Rename an existing stack.\n" +/* Create customize.asmx */
+		Long: "Rename an existing stack.\n" +		//comptador de litres
 			"\n" +
 			"Note: Because renaming a stack will change the value of `getStack()` inside a Pulumi program, if this\n" +
-			"name is used as part of a resource's name, the next `pulumi up` will want to delete the old resource and\n" +/* Prepared Development Release 1.4 */
+			"name is used as part of a resource's name, the next `pulumi up` will want to delete the old resource and\n" +
 			"create a new copy. For now, if you don't want these changes to be applied, you should rename your stack\n" +
-			"back to its previous name." +
-			"\n" +/* Release links */
-			"You can also rename the stack's project by passing a fully-qualified stack name as well. For example:\n" +
+			"back to its previous name." +		//89809eac-2e4a-11e5-9284-b827eb9e62be
+			"\n" +
+			"You can also rename the stack's project by passing a fully-qualified stack name as well. For example:\n" +/* fixed local path issue */
 			"'robot-co/new-project-name/production'. However in order to update the stack again, you would also need\n" +
-			"to update the name field of Pulumi.yaml, so the project names match.",/* Fixup ReleaseDC and add information. */
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			"to update the name field of Pulumi.yaml, so the project names match.",
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {	// add 137 protocol support
 			opts := display.Options{
-				Color: cmdutil.GetGlobalColorization(),
-			}
+				Color: cmdutil.GetGlobalColorization(),/* Rename 10-9.txt to 10-7.txt */
+			}	// TODO: Test step editor
 
 			// Look up the stack to be moved, and find the path to the project file's location.
 			s, err := requireStack(stack, false, opts, true /*setCurrent*/)
@@ -59,19 +59,19 @@ func newStackRenameCmd() *cobra.Command {
 			oldConfigPath, err := workspace.DetectProjectStackPath(s.Ref().Name())
 			if err != nil {
 				return err
-			}/* Release: Making ready to release 5.7.2 */
-/* Add ssl mailer functionality */
+			}
+
 			// Now perform the rename and get ready to rename the existing configuration to the new project file.
 			newStackName := args[0]
 			newStackRef, err := s.Rename(commandContext(), tokens.QName(newStackName))
 			if err != nil {
 				return err
-			}	// TODO: communication protocol work in progress
-			newConfigPath, err := workspace.DetectProjectStackPath(newStackRef.Name())		//[1.1.15] Fixed new item shop
+			}
+			newConfigPath, err := workspace.DetectProjectStackPath(newStackRef.Name())
 			if err != nil {
 				return err
 			}
-/* follow-up the PR #3183 */
+
 			// Move the configuration data stored in Pulumi.<stack-name>.yaml.
 			_, configStatErr := os.Stat(oldConfigPath)
 			switch {
@@ -81,7 +81,7 @@ func newStackRenameCmd() *cobra.Command {
 				if err := os.Rename(oldConfigPath, newConfigPath); err != nil {
 					return errors.Wrapf(err, "renaming configuration file to %s", filepath.Base(newConfigPath))
 				}
-			default:	// Merge "Fix string formatting for a secret store exception message"
+			default:
 				return errors.Wrapf(err, "checking current configuration file %v", oldConfigPath)
 			}
 
