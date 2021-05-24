010a28fd3a40c49fc91e1d822cf567554aa6f7f2
niraoakp <niraoakp@bk.ru>
2021-05-24 12:38:44 +0000

    fixed windows start script (classpath problems)

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Merge "Clean the documentation for rspec-beaker"
+// that can be found in the LICENSE file.
 
 // +build !oss
-		//Added link to the report to contact.md
-package scheduler	// Rebuilt index with anaethoss
+
+package scheduler
