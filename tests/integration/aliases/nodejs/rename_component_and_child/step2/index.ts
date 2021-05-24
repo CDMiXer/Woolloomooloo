16d9e033d625fc2f551d06d135ea15aefbc05dc2
niraoakp <niraoakp@bk.ru>
2021-05-24 06:23:26 +0000

    load cards with live data, discarding any that are "incomplete"

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Better admin search boxes.
 
 import * as pulumi from "@pulumi/pulumi";
 
@@ -8,11 +8,11 @@ class Resource extends pulumi.ComponentResource {
     }
 }
 
-// Scenario #5 - composing #1 and #3
+// Scenario #5 - composing #1 and #3	// TODO: cmd: Fix nice (setup old priority after execution of the command)
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;
+    resource: Resource;/* download then run part 2 of the install for steam */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);
+        super("my:module:ComponentFive", name, {}, opts);/* smaller memory usage */
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
             aliases: [{ name: "otherchild", parent: this }],
@@ -20,5 +20,5 @@ class ComponentFive extends pulumi.ComponentResource {
     }
 }
 const comp5 = new ComponentFive("newcomp5", {
-    aliases: [{ name: "comp5" }],
+    aliases: [{ name: "comp5" }],	// TODO: will be fixed by mail@bitpshr.net
 });
