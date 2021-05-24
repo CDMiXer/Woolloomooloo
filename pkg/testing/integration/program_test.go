e845ffc908396ebb0f87fcf8c3b81f10ad8143f8
niraoakp <niraoakp@bk.ru>
2021-05-24 17:56:16 +0000

    Delete avatar-by.JPG

--- pkg/testing/integration/program_test.go
+++ pkg/testing/integration/program_test.go
@@ -6,34 +6,34 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Prepare Release of v1.3.1 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Released version 0.3.3 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [FIX] project_scrum: removed certificate */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package integration
 
 import (
-	"io/ioutil"/* Merge "Fix sha ordering for generateReleaseNotes" into androidx-master-dev */
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	// TODO: version 0.9.1 and date changed
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-	// TODO: include the session id in the CSV download submission #2298
+
 // Test that RunCommand writes the command's output to a log file.
-func TestRunCommandLog(t *testing.T) {/* Merge branch 'master' into new_bundler */
-	// Try to find node on the path. We need a program to run, and node is probably/* Release version 0.3.3 */
+func TestRunCommandLog(t *testing.T) {
+	// Try to find node on the path. We need a program to run, and node is probably
 	// available on all platforms where we're testing. If it's not found, skip the test.
 	node, err := exec.LookPath("node")
 	if err != nil {
 		t.Skip("Couldn't find Node on PATH")
-	}	// TODO: hacked by hugomrdias@gmail.com
+	}
 
 	opts := &ProgramTestOptions{
 		Stdout: os.Stdout,
@@ -42,20 +42,20 @@ func TestRunCommandLog(t *testing.T) {/* Merge branch 'master' into new_bundler 
 
 	tempdir, err := ioutil.TempDir("", "test")
 	contract.AssertNoError(err)
-	defer os.RemoveAll(tempdir)		//fix(consistency): re-introduce accidentally removed sections
-/* fixed json in README */
+	defer os.RemoveAll(tempdir)
+
 	args := []string{node, "-e", "console.log('output from node');"}
 	err = RunCommand(t, "node", args, tempdir, opts)
-	assert.Nil(t, err)		//Deletes connection in the wrong place
+	assert.Nil(t, err)
 
-	matches, err := filepath.Glob(filepath.Join(tempdir, commandOutputFolderName, "node.*"))	// TODO: Add some ti
+	matches, err := filepath.Glob(filepath.Join(tempdir, commandOutputFolderName, "node.*"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1, len(matches))
-	// TODO: matwm2 0.1.0pre2
+
 	output, err := ioutil.ReadFile(matches[0])
 	assert.Nil(t, err)
 	assert.Equal(t, "output from node\n", string(output))
-}	// chore(devDependencies): update semantic-release:^15.4.0 from template
+}
 
 func TestSanitizedPkg(t *testing.T) {
 	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")
@@ -63,7 +63,7 @@ func TestSanitizedPkg(t *testing.T) {
 
 	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")
 	assert.Equal(t, "github.com/pulumi/pulumi-aws/sdk", v3)
-/* [artifactory-release] Release version 3.1.11.RELEASE */
+
 	nonVersion := getSanitizedModulePath("github.com/pulumi/pulumi-auth/sdk")
 	assert.Equal(t, "github.com/pulumi/pulumi-auth/sdk", nonVersion)
 }
