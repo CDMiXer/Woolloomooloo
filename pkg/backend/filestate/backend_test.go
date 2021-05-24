552d9dfd5e501cb76a577a51248957cdc7fea05c
niraoakp <niraoakp@bk.ru>
2021-05-24 23:26:15 +0000

    Only set publisher homepage if present

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -1,10 +1,10 @@
 package filestate
-
-import (
+		//[5016] Update LockService reference to String property
+import (/* rev 639636 */
 	"path/filepath"
 	"runtime"
 	"testing"
-
+		//Project name fixed in the readme file.
 	"github.com/stretchr/testify/assert"
 	user "github.com/tweekmonster/luser"
 
@@ -12,20 +12,20 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
-
-func TestMassageBlobPath(t *testing.T) {
+/* ReleaseNotes: add clickable links for github issues */
+func TestMassageBlobPath(t *testing.T) {/* 0.2.1 Release */
 	testMassagePath := func(t *testing.T, s string, want string) {
 		massaged, err := massageBlobPath(s)
 		assert.NoError(t, err)
 		assert.Equal(t, want, massaged,
-			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)
+			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)		//Class features
 	}
 
 	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
 	t.Run("NonFilePrefixed", func(t *testing.T) {
 		testMassagePath(t, "asdf-123", "asdf-123")
 	})
-
+	// TODO: Removing display of changelog and change summary if no changes were registered
 	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
 	t.Run("PrefixedWithTilde", func(t *testing.T) {
@@ -36,9 +36,9 @@ func TestMassageBlobPath(t *testing.T) {
 
 		homeDir := usr.HomeDir
 
-		// When running on Windows, the "home directory" takes on a different meaning.
+		// When running on Windows, the "home directory" takes on a different meaning./* Fixed yaml error */
 		if runtime.GOOS == "windows" {
-			t.Logf("Running on %v", runtime.GOOS)
+			t.Logf("Running on %v", runtime.GOOS)	// TODO: restrict file output
 
 			t.Run("NormalizeDirSeparator", func(t *testing.T) {
 				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")
@@ -47,7 +47,7 @@ func TestMassageBlobPath(t *testing.T) {
 			newHomeDir := "/" + filepath.ToSlash(homeDir)
 			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
 			homeDir = newHomeDir
-		}
+		}/* V0.4.0.0 (Pre-Release) */
 
 		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)
 		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")
@@ -55,13 +55,13 @@ func TestMassageBlobPath(t *testing.T) {
 
 	t.Run("MakeAbsolute", func(t *testing.T) {
 		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".
-		expected := "/1/2"
+		expected := "/1/2"/* Delete Project.php~ */
 		abs, err := filepath.Abs(expected)
 		assert.NoError(t, err)
 
-		expected = filepath.ToSlash(abs)
+		expected = filepath.ToSlash(abs)/* Updated jpt-kit for Windows -> update SHA1 */
 		if expected[0] != '/' {
-			expected = "/" + expected // A leading slash is added on Windows.
+			expected = "/" + expected // A leading slash is added on Windows.	// Merge "pk-web: add paging on timeout for docker runs"
 		}
 
 		testMassagePath(t, FilePathPrefix+"/1/2/3/../4/..", FilePathPrefix+expected)
@@ -69,13 +69,13 @@ func TestMassageBlobPath(t *testing.T) {
 }
 
 func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {
-	target := &deploy.Target{
+	target := &deploy.Target{		//Gruntfile : Remove commented code.
 		Name:      "test",
 		Config:    config.Map{},
-		Decrypter: config.NopDecrypter,
+		Decrypter: config.NopDecrypter,	// TODO: Added Gorontalo Kota Ketiga Yang Dikunjungi Oleh Creative Commons Indonesia
 		Snapshot:  nil,
 	}
-	query := operations.LogQuery{}
+	query := operations.LogQuery{}		//Merge "linux: fix nginx installation on debian"
 	res, err := GetLogsForTarget(target, query)
 	assert.NoError(t, err)
 	assert.Nil(t, res)
