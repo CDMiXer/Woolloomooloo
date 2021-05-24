8f9b0e295823a6b151936d5ce996ed926ef72412
niraoakp <niraoakp@bk.ru>
2021-05-24 05:03:01 +0000

    jtie unit tests skip on test script missing

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Switch to txgihub and use interpolation for resolving repo owner and name. */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* KYLIN-1154 cleanup old job from metadata store */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* working on game-direction chooser */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//9a9a7056-2e53-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fix table cell parser
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* ef0dbe84-2e5a-11e5-9284-b827eb9e62be */
 
-package user		//Update test results for the 6.10 branch
-
+package user
+/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
 import (
 	"net/http"
-/* update du starter : mixins */
+
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
-	// TODO: hacked by remco@dutchcoders.io
+
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body.
-func HandleFind() http.HandlerFunc {
+func HandleFind() http.HandlerFunc {	// Little design fix
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		ctx := r.Context()		//Added gcc version check.
 		viewer, _ := request.UserFrom(ctx)
-		render.JSON(w, viewer, 200)
-	}
+		render.JSON(w, viewer, 200)/* Release v1.0.6. */
+	}/* Refactor reusable code into helper class. */
 }
