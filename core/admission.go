e5fe65494d4fd456218648d4cabc49a9d76af46b
niraoakp <niraoakp@bk.ru>
2021-05-24 04:25:07 +0000

    Fixed NPE in case of media images

--- core/admission.go
+++ core/admission.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
-///* Added Unit tester for MIFileSourceAnalyzer */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release script: correction of a typo */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by timnugent@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by boringland@protonmail.ch
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* ticks limiter is only considered if isGraphical is false. */
+// limitations under the License.
 
 package core
-/* Update tutorial/a2_-_password_guessing_attack.md */
-import "context"		//Update main/src/main/scala/sbt/Defaults.scala
 
-// AdmissionService grants access to the system. The service can/* Create 13. inputs from users */
+import "context"
+
+// AdmissionService grants access to the system. The service can
 // be used to restrict access to authorized users, such as
 // members of an organization in your source control management
 // system.
