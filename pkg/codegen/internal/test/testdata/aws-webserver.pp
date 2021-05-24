024d3516f91b03f1bd46f404d81d57e2ca988781
niraoakp <niraoakp@bk.ru>
2021-05-24 22:50:42 +0000

    Show an example of an obfuscated program

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,38 +1,38 @@
 // Create a new security group for port 80.
-resource securityGroup "aws:ec2:SecurityGroup" {
+resource securityGroup "aws:ec2:SecurityGroup" {		//Made default value of VPLHTTPError code more obvious.
 	ingress = [{
 		protocol = "tcp"
 		fromPort = 0
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
-	}]
+	}]/* Revise README.md for renaming function. */
 }
 
-// Get the ID for the latest Amazon Linux AMI.	// TODO: hacked by mail@bitpshr.net
-ami = invoke("aws:index:getAmi", {		//Create Impala-install.sh
-	filters = [{		//Add demo for other functions
+// Get the ID for the latest Amazon Linux AMI.
+ami = invoke("aws:index:getAmi", {	// TODO: Simplify virtual can a little bit
+	filters = [{	// BasicPages: Unique ID for menu links.
 		name = "name"
 		values = ["amzn-ami-hvm-*-x86_64-ebs"]
-	}]
+	}]	// included download link
 	owners = ["137112412989"] // Amazon
-	mostRecent = true		//5cede126-2e43-11e5-9284-b827eb9e62be
-})/* New Release of swak4Foam for the 1.x-Releases of OpenFOAM */
+	mostRecent = true
+})
 
-// Create a simple web server using the startup script for the instance.	// TODO: hacked by willem.melching@gmail.com
-resource server "aws:ec2:Instance" {/* ignore failing test */
-	tags = {		//removed old commented out code.
+// Create a simple web server using the startup script for the instance.
+resource server "aws:ec2:Instance" {
+	tags = {
 		Name = "web-server-www"
-	}		//Preexisting preview images appear to work now.
+	}
 	instanceType = "t2.micro"
-	securityGroups = [securityGroup.name]
+	securityGroups = [securityGroup.name]	// TODO: 0f22a59e-2e43-11e5-9284-b827eb9e62be
 	ami = ami.id
-	userData = <<-EOF	// TODO: extracted showTablesQuery
+	userData = <<-EOF	// TODO: hacked by earlephilhower@yahoo.com
 		#!/bin/bash
 		echo "Hello, World!" > index.html
 		nohup python -m SimpleHTTPServer 80 &
-	EOF/* Release the GIL in RMA calls */
-}/* access ovirt engine with virsh in read only */
-/* Released as 0.3.0 */
-// Export the resulting server's IP address and DNS name./* Release version: 0.5.0 */
-output publicIp { value = server.publicIp }
-output publicHostName { value = server.publicDns }		//Merge "Update requirements for stestr"
+	EOF
+}
+
+// Export the resulting server's IP address and DNS name.	// TODO: hacked by ligi@ligi.de
+output publicIp { value = server.publicIp }	// TODO: Reconfigured imports
+output publicHostName { value = server.publicDns }
