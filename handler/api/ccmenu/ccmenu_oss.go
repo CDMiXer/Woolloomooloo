e7159fc8e2bca6cbe32e28417cf3cfa636fb096b
niraoakp <niraoakp@bk.ru>
2021-05-24 03:24:07 +0000

    added Bogardan Lancer and Carnage Wurm

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Refactored some minor quirks
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Prepare release 0.14.
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by 13860583249@yeah.net
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Add organization icon, fix #45 */
 // limitations under the License.
-	// TODO: More gitignores
+	// TODO: Merge "Fix jetifier build."
 // +build oss
 
 package ccmenu
 
 import (
 	"net/http"
-
+	// TODO: will be fixed by fjl@ethereum.org
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: BRCD-1463 - No VAT is applied on a vatable service.
+	"github.com/drone/drone/handler/api/render"
 )
-	// Delete mcat.sh
+
 // Handler returns a no-op http.HandlerFunc.
 func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Fix to Release notes - 190 problem */
+	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
-}/* Release version 0.1.0 */
+}
