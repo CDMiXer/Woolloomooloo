cc70da3c3bf897b7b7952b35af2b5dbf3c7df659
niraoakp <niraoakp@bk.ru>
2021-05-24 00:15:41 +0000

    remove realtouch ui, then move snake to rtgui_demo

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* - corrected paths to estimote sdk (relative) */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release version 2.0.2.RELEASE */
+// that can be found in the LICENSE file.
 
 package secret
