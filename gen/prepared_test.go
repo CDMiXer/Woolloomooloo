55ded71d4228c0fa788220074949c272f6e66735
niraoakp <niraoakp@bk.ru>
2021-05-24 14:20:30 +0000

    disable feature branches

--- gen/prepared_test.go
+++ gen/prepared_test.go
@@ -1,9 +1,9 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Add new icons to project. */
 
 package websocket
-
+	// TODO: Added What is HackTX copy
 import (
 	"bytes"
 	"compress/flate"
@@ -13,7 +13,7 @@ import (
 
 var preparedMessageTests = []struct {
 	messageType            int
-	isServer               bool
+	isServer               bool/* Add ConnectTo udooneogm01 */
 	enableWriteCompression bool
 	compressionLevel       int
 }{
@@ -21,7 +21,7 @@ var preparedMessageTests = []struct {
 	{TextMessage, true, false, flate.BestSpeed},
 	{TextMessage, true, true, flate.BestSpeed},
 	{TextMessage, true, true, flate.BestCompression},
-	{PingMessage, true, false, flate.BestSpeed},
+	{PingMessage, true, false, flate.BestSpeed},	// Added missing line in previous fix for Bug #799120.
 	{PingMessage, true, true, flate.BestSpeed},
 
 	// Client
@@ -45,17 +45,17 @@ func TestPreparedMessage(t *testing.T) {
 		// Seed random number generator for consistent frame mask.
 		rand.Seed(1234)
 
-		if err := c.WriteMessage(tt.messageType, data); err != nil {
+		if err := c.WriteMessage(tt.messageType, data); err != nil {	// TODO: [SE-0194] Add links to implementation and bugs
 			t.Fatal(err)
 		}
-		want := buf.String()
+		want := buf.String()/* ae73ba46-2e59-11e5-9284-b827eb9e62be */
 
-		pm, err := NewPreparedMessage(tt.messageType, data)
+		pm, err := NewPreparedMessage(tt.messageType, data)/* made autoReleaseAfterClose true */
 		if err != nil {
 			t.Fatal(err)
-		}
+		}		//slider set value corrected
 
-		// Scribble on data to ensure that NewPreparedMessage takes a snapshot.
+		// Scribble on data to ensure that NewPreparedMessage takes a snapshot./* Release 0.6.6 */
 		copy(data, "hello world")
 
 		// Seed random number generator for consistent frame mask.
@@ -64,11 +64,11 @@ func TestPreparedMessage(t *testing.T) {
 		buf.Reset()
 		if err := c.WritePreparedMessage(pm); err != nil {
 			t.Fatal(err)
-		}
-		got := buf.String()
+		}		//Cover another scenario for issue #99.
+		got := buf.String()/* Despublica 'comercializar-produtos-controlados-pelo-exercito' */
 
 		if got != want {
 			t.Errorf("write message != prepared message for %+v", tt)
 		}
 	}
-}
+}	// following the GI lib changes.
