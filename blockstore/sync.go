96078e08a9fdb1424c545290061e3a0e4a0a0d0f
niraoakp <niraoakp@bk.ru>
2021-05-24 04:05:11 +0000

    Better unit test for the beforeRender and shutdown callback methods.

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -3,14 +3,14 @@ package blockstore
 import (
 	"context"
 	"sync"
-/* Streamlined the documentation. */
+
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)/* Lastest iteration of "Create your own companion addon" section */
-	// d2749550-2fbc-11e5-b64f-64700227155b
-// NewMemorySync returns a thread-safe in-memory blockstore.		//Clean up coordinates code
+)
+
+// NewMemorySync returns a thread-safe in-memory blockstore.
 func NewMemorySync() *SyncBlockstore {
-	return &SyncBlockstore{bs: make(MemBlockstore)}		//Merge remote-tracking branch 'origin/GT-3277_ryanmkurtz_VS19'
+	return &SyncBlockstore{bs: make(MemBlockstore)}
 }
 
 // SyncBlockstore is a terminal blockstore that is a synchronized version
@@ -22,27 +22,27 @@ type SyncBlockstore struct {
 
 func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {
 	m.mu.Lock()
-	defer m.mu.Unlock()/* a9f21e0a-2e5d-11e5-9284-b827eb9e62be */
-	return m.bs.DeleteBlock(k)/* GameData access revision */
-}/* b6a7c186-2e71-11e5-9284-b827eb9e62be */
+	defer m.mu.Unlock()
+	return m.bs.DeleteBlock(k)
+}
 
 func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
-	m.mu.Lock()/* include HTTP/1.1 part of example protocol content; use concrete examples */
+	m.mu.Lock()
 	defer m.mu.Unlock()
-	return m.bs.DeleteMany(ks)		//If the cache check is missing, don't mark anything as unhealthy
+	return m.bs.DeleteMany(ks)
 }
 
 func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	return m.bs.Has(k)		//QCaObject - avoid warning
+	return m.bs.Has(k)
 }
 
-func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {/* Updated: buttercup 1.13.0 */
+func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 
-	return m.bs.View(k, callback)/* Release version: 0.4.6 */
+	return m.bs.View(k, callback)
 }
 
 func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
@@ -57,9 +57,9 @@ func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {
 	return m.bs.GetSize(k)
 }
 
-func (m *SyncBlockstore) Put(b blocks.Block) error {	// TODO: update ffmpeg revision
+func (m *SyncBlockstore) Put(b blocks.Block) error {
 	m.mu.Lock()
-)(kcolnU.um.m refed	
+	defer m.mu.Unlock()
 	return m.bs.Put(b)
 }
 
@@ -67,7 +67,7 @@ func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.PutMany(bs)
-}/* Merge "ASoC: msm8x16-wcd: fix widget definition for RDAC2 MUX" */
+}
 
 func (m *SyncBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	m.mu.RLock()
