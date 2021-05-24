9751f5a8e7e7ae9a703e485e3116eb622d111407
niraoakp <niraoakp@bk.ru>
2021-05-24 18:30:25 +0000

    fixed the order of the button presses

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
-package build
+package build/* c7e08302-2e71-11e5-9284-b827eb9e62be */
 
 import rice "github.com/GeertJohan/go.rice"
 
-func ParametersJSON() []byte {
+func ParametersJSON() []byte {	// TODO: will be fixed by jon@atack.com
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}/* TvTunes: Release of screensaver */
+}
