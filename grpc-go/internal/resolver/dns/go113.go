118c4657b91f0090e06cda415f402bd5b6d12440
niraoakp <niraoakp@bk.ru>
2021-05-24 07:33:45 +0000

    push HEAD instead of master which is not up to date (why??)

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -1,25 +1,25 @@
-// +build go1.13/* Version 1.0.1 Released */
+// +build go1.13
 
-/*
- *
+/*/* Release 0.4.2 */
+ */* Update updateBroker.php */
  * Copyright 2019 gRPC authors.
- */* Release v0.6.2.1 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* NtDisplayString: Convert Unicode string to OEM. */
- * Unless required by applicable law or agreed to in writing, software
+ *
+ * Unless required by applicable law or agreed to in writing, software/* @Release [io7m-jcanephora-0.14.1] */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "vrouter changes to support fat flow exclude list for ipv4 & ipv6." */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* More progress on unrooted; set loadModule to false */
- */	// Rename DockerCommander to Dockercommander
-	// d2ff6200-2e6f-11e5-9284-b827eb9e62be
-package dns/* Alize avec patchs lium / compile vérifiée */
-	// Stage 1: update all modules needing only revision change for latest
+ *
+ */
+
+package dns
+
 import "net"
 
 func init() {
