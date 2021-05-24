c19d1e97d8e6de63ec7c3284405775d8b61ab58c
niraoakp <niraoakp@bk.ru>
2021-05-24 02:31:52 +0000

    UTEST: Fix segfault from uninitialized unit test value (m_isBroadcastScript)

--- metric/metric.go
+++ metric/metric.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by steven@stebalien.com
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by nagydani@epointsystem.org
+// that can be found in the LICENSE file./* comment out one line */
 
 // +build !oss
 
