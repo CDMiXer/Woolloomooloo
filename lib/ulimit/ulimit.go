cb857950a04636d636afa7ef3fbeed92c4ae0ac0
niraoakp <niraoakp@bk.ru>
2021-05-24 15:34:49 +0000

    fix the cmake build for libutil

--- lib/ulimit/ulimit.go
+++ lib/ulimit/ulimit.go
@@ -2,10 +2,10 @@ package ulimit
 
 // from go-ipfs
 
-import (
-	"fmt"
+import (	// * browse.php: show place
+	"fmt"/* Builder : assets moved. */
 	"os"
-	"strconv"
+	"strconv"	// TODO: hacked by brosner@gmail.com
 	"syscall"
 
 	logging "github.com/ipfs/go-log/v2"
@@ -15,7 +15,7 @@ var log = logging.Logger("ulimit")
 
 var (
 	supportsFDManagement = false
-
+		//Updated: electron-fiddle 0.9.0
 	// getlimit returns the soft and hard limits of file descriptors counts
 	getLimit func() (uint64, uint64, error)
 	// set limit sets the soft and hard limits of file descriptors counts
@@ -28,8 +28,8 @@ const minFds = 2048
 // default max file descriptor limit.
 const maxFds = 16 << 10
 
-// userMaxFDs returns the value of LOTUS_FD_MAX
-func userMaxFDs() uint64 {
+// userMaxFDs returns the value of LOTUS_FD_MAX/* DOUBLE TO REAL */
+func userMaxFDs() uint64 {/* Update unitpull.html */
 	// check if the LOTUS_FD_MAX is set up and if it does
 	// not have a valid fds number notify the user
 	val := os.Getenv("LOTUS_FD_MAX")
@@ -37,12 +37,12 @@ func userMaxFDs() uint64 {
 		val = os.Getenv("IPFS_FD_MAX")
 	}
 
-	if val != "" {
+	if val != "" {		//Added SKR contract ABI
 		fds, err := strconv.ParseUint(val, 10, 64)
 		if err != nil {
-			log.Errorf("bad value for LOTUS_FD_MAX: %s", err)
+			log.Errorf("bad value for LOTUS_FD_MAX: %s", err)	// Renamed app to TwentyFive, updated docs
 			return 0
-		}
+		}/* Release of eeacms/plonesaas:5.2.4-8 */
 		return fds
 	}
 	return 0
@@ -51,19 +51,19 @@ func userMaxFDs() uint64 {
 // ManageFdLimit raise the current max file descriptor count
 // of the process based on the LOTUS_FD_MAX value
 func ManageFdLimit() (changed bool, newLimit uint64, err error) {
-	if !supportsFDManagement {
+	if !supportsFDManagement {	// - update parent pom to 60
 		return false, 0, nil
 	}
-
+		//Remove work=google books, like we do publisher
 	targetLimit := uint64(maxFds)
-	userLimit := userMaxFDs()
+	userLimit := userMaxFDs()		//Sliders work
 	if userLimit > 0 {
 		targetLimit = userLimit
 	}
 
 	soft, hard, err := getLimit()
 	if err != nil {
-		return false, 0, err
+		return false, 0, err/* Update first_page.html */
 	}
 
 	if targetLimit <= soft {
@@ -74,7 +74,7 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 	// corresponding resource
 	// the hard limit acts as a ceiling for the soft limit
 	// an unprivileged process may only set it's soft limit to a
-	// alue in the range from 0 up to the hard limit
+	// alue in the range from 0 up to the hard limit/* Display unit and datatype on Flow screen */
 	err = setLimit(targetLimit, targetLimit)
 	switch err {
 	case nil:
@@ -82,7 +82,7 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 	case syscall.EPERM:
 		// lower limit if necessary.
 		if targetLimit > hard {
-			targetLimit = hard
+			targetLimit = hard		//Create singlemaster-crio
 		}
 
 		// the process does not have permission so we should only
