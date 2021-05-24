fd5318e410c466caff5d697970fe657144128da9
niraoakp <niraoakp@bk.ru>
2021-05-24 20:35:25 +0000

    Added other linter tools

--- grpc-go/xds/internal/balancer/priority/logging.go
+++ grpc-go/xds/internal/balancer/priority/logging.go
@@ -1,13 +1,13 @@
 /*
- *		//include all tests
- * Copyright 2021 gRPC authors.	// TODO: fix an initialization problem
- *	// TODO: carousel - lock scroll can no longer lock during swipe
+ *
+ * Copyright 2021 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: remove references to /provisioning/
- */* Changes in mediaItem class due to refactoring */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,19 +16,19 @@
  *
  */
 
-package priority/* Fixed settings. Release candidate. */
+package priority
 
 import (
 	"fmt"
-	// TODO: Fix for Windows.Web.IUriToStreamResolver
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[priority-lb %p] "
-		//Log more messages from cache update.
-var logger = grpclog.Component("xds")	// TODO: update scrutinizer conf
+
+var logger = grpclog.Component("xds")
 
 func prefixLogger(p *priorityBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}	// TODO: will be fixed by steven@stebalien.com
+}
