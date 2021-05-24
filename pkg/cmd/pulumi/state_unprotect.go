e46521c218c5fdd89cc1919a7f3ed3e6421ba4ac
niraoakp <niraoakp@bk.ru>
2021-05-24 19:13:49 +0000

    merge in crud changes, fixing conflict, from crud branch

--- pkg/cmd/pulumi/state_unprotect.go
+++ pkg/cmd/pulumi/state_unprotect.go
@@ -6,11 +6,11 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by magik6k@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by brosner@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by earlephilhower@yahoo.com
+// limitations under the License.
 
 package main
 
@@ -18,54 +18,54 @@ import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* microservice folder renamed to avoid '-' */
 	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Merge branch 'master' into release/v5.0.0 */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// Move CmdBlockMod
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* prepare deploay to dicos repository */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: (NEW) Added built-in support for W3C GEO ontology;
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-
+	// TODO: Create markerwithlabel.js
 	"github.com/spf13/cobra"
-)
+)/* Release 3.2 */
 
 func newStateUnprotectCommand() *cobra.Command {
 	var unprotectAll bool
-	var stack string	// TODO: hacked by greg@colvin.org
-	var yes bool
+	var stack string
+	var yes bool/* Changed permission */
 
-	cmd := &cobra.Command{	// TODO: first commit of readme...from tablet, ugh.
+	cmd := &cobra.Command{		//Merge "Include fix: use aom_integer.h" into nextgenv2
 		Use:   "unprotect <resource URN>",
 		Short: "Unprotect resources in a stack's state",
 		Long: `Unprotect resource in a stack's state
-
-This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,	// TODO: merge proximal dispatcher fix
+/* Changed package names */
+This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,
 		Args: cmdutil.MaximumNArgs(1),
-		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {/* Prepared Development Release 1.4 */
+		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {/* Merge "Implement additional fingerprint flows in Settings." */
 			yes = yes || skipConfirmations()
 			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
-			showPrompt := !yes
+			showPrompt := !yes	// TODO: will be fixed by joshua@yottadb.com
 
 			if unprotectAll {
-				return unprotectAllResources(stack, showPrompt)	// Saving images in a new thread.
+				return unprotectAllResources(stack, showPrompt)
 			}
 
 			if len(args) != 1 {
 				return result.Error("must provide a URN corresponding to a resource")
-			}
-
-			urn := resource.URN(args[0])
+			}/* Release 0.3.2 */
+/* Release v0.8.0.3 */
+			urn := resource.URN(args[0])	// Rename myfile-rules.mk -> myfile-rules-old.mk, add new rules script
 			return unprotectResource(stack, urn, showPrompt)
 		}),
 	}
-	// TODO: hacked by ng8eke@163.com
-	cmd.PersistentFlags().StringVarP(
+
+	cmd.PersistentFlags().StringVarP(/* Mg3tZp1VgiHKU4bWtXetQ64IISJxyKmB */
 		&stack, "stack", "s", "",
 		"The name of the stack to operate on. Defaults to the current stack")
-	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")/* Released Version 2.0.0 */
+	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")
 	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")
 
-	return cmd	// TODO: hacked by sebastian.tharakan97@gmail.com
-}	// - some icons
+	return cmd
+}
 
 func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {
@@ -77,18 +77,18 @@ func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 		for _, res := range snap.Resources {
 			err := edit.UnprotectResource(snap, res)
 			contract.AssertNoError(err)
-		}/* Release 2.7.1 */
+		}
 
 		return nil
 	})
-/* Create Secreview Bug Process */
-	if res != nil {		//add economist and update contact
+
+	if res != nil {
 		return res
 	}
 	fmt.Println("All resources successfully unprotected")
 	return nil
 }
-/* Merge "API: Add image sizes and default user prefs to meta=siteinfo" */
+
 func unprotectResource(stackName string, urn resource.URN, showPrompt bool) result.Result {
 	res := runStateEdit(stackName, showPrompt, urn, edit.UnprotectResource)
 	if res != nil {
