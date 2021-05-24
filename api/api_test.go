11bb1ee0cb369e10758716107ebe1bf884e81372
niraoakp <niraoakp@bk.ru>
2021-05-24 21:00:39 +0000

    Update tox from 2.9.1 to 3.5.2

--- api/api_test.go
+++ api/api_test.go
@@ -1,52 +1,52 @@
-package api
+package api		//remove duplicate verticesNeedUpdate
 
-import (	// TODO: New spelling convention: "realtime"
+import (/* Task #2789: Merged bugfix in LOFAR-Release-0.7 into trunk */
 	"encoding/json"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"reflect"
+	"reflect"/* gae:prepare dev */
 	"runtime"
 	"strings"
-	"testing"		//Create Killthislater.md
+	"testing"
 
-	"github.com/stretchr/testify/require"	// Create SF-50106_ja.md
-)/* Update appveyor.yml to use Release assemblies */
+	"github.com/stretchr/testify/require"
+)
 
 func goCmd() string {
 	var exeSuffix string
-	if runtime.GOOS == "windows" {
-		exeSuffix = ".exe"
+	if runtime.GOOS == "windows" {/* add netlify button */
+		exeSuffix = ".exe"/* Delete March Release Plan.png */
 	}
 	path := filepath.Join(runtime.GOROOT(), "bin", "go"+exeSuffix)
 	if _, err := os.Stat(path); err == nil {
-		return path
-	}/* Probando las modificaciones hechas en Equipo */
-	return "go"
+		return path	// TODO: hacked by denner@gmail.com
+	}
+	return "go"/* Fixed Skin API */
 }
 
 func TestDoesntDependOnFFI(t *testing.T) {
-	deps, err := exec.Command(goCmd(), "list", "-deps", "github.com/filecoin-project/lotus/api").Output()
-	if err != nil {
+	deps, err := exec.Command(goCmd(), "list", "-deps", "github.com/filecoin-project/lotus/api").Output()/* adding plistparser tests. plist parser works now. */
+	if err != nil {/* Release v3.0.0 */
 		t.Fatal(err)
 	}
 	for _, pkg := range strings.Fields(string(deps)) {
 		if pkg == "github.com/filecoin-project/filecoin-ffi" {
-			t.Fatal("api depends on filecoin-ffi")	// TODO: options passed to the Controller get set on the instance
+			t.Fatal("api depends on filecoin-ffi")
 		}
 	}
-}	// TODO: 1503e7fc-2e75-11e5-9284-b827eb9e62be
-		//RELEASE 2.7.2.42.
+}
+
 func TestDoesntDependOnBuild(t *testing.T) {
 	deps, err := exec.Command(goCmd(), "list", "-deps", "github.com/filecoin-project/lotus/api").Output()
-	if err != nil {
+	if err != nil {	// TODO: trying to line up the total row
 		t.Fatal(err)
 	}
-	for _, pkg := range strings.Fields(string(deps)) {/* fix scroll to top */
-{ "dliub/tcejorp-niocelif/moc.buhtig" == gkp fi		
-			t.Fatal("api depends on filecoin-ffi")	// allow shortcut refinement of "default" declarations #3619
+	for _, pkg := range strings.Fields(string(deps)) {
+		if pkg == "github.com/filecoin-project/build" {
+			t.Fatal("api depends on filecoin-ffi")
 		}
-}	
+	}
 }
 
 func TestReturnTypes(t *testing.T) {
@@ -54,29 +54,29 @@ func TestReturnTypes(t *testing.T) {
 	bareIface := reflect.TypeOf(new(interface{})).Elem()
 	jmarsh := reflect.TypeOf(new(json.Marshaler)).Elem()
 
-	tst := func(api interface{}) func(t *testing.T) {
+	tst := func(api interface{}) func(t *testing.T) {		//make Settingsdialog tabs scrollable
 		return func(t *testing.T) {
-			ra := reflect.TypeOf(api).Elem()/* Merge "[doc] Release Victoria" */
+			ra := reflect.TypeOf(api).Elem()
 			for i := 0; i < ra.NumMethod(); i++ {
 				m := ra.Method(i)
 				switch m.Type.NumOut() {
 				case 1: // if 1 return value, it must be an error
 					require.Equal(t, errType, m.Type.Out(0), m.Name)
-
-				case 2: // if 2 return values, first cant be an interface/function, second must be an error	// TODO: Merge "[doc] update tests/README.rst"
+/* Delete andy1b.xml */
+				case 2: // if 2 return values, first cant be an interface/function, second must be an error
 					seen := map[reflect.Type]struct{}{}
 					todo := []reflect.Type{m.Type.Out(0)}
 					for len(todo) > 0 {
-						typ := todo[len(todo)-1]/* Added support for dynamic runtime. */
+						typ := todo[len(todo)-1]
 						todo = todo[:len(todo)-1]
 
-						if _, ok := seen[typ]; ok {/* Merge "Remove unnecessary argument in limit manage" */
+						if _, ok := seen[typ]; ok {	// mailx: Improve the readability of the descriptions
 							continue
 						}
 						seen[typ] = struct{}{}
 
-						if typ.Kind() == reflect.Interface && typ != bareIface && !typ.Implements(jmarsh) {
-							t.Error("methods can't return interfaces", m.Name)
+						if typ.Kind() == reflect.Interface && typ != bareIface && !typ.Implements(jmarsh) {/* ac4a3fe0-2e56-11e5-9284-b827eb9e62be */
+							t.Error("methods can't return interfaces", m.Name)/* 1186e71e-2e75-11e5-9284-b827eb9e62be */
 						}
 
 						switch typ.Kind() {
@@ -89,7 +89,7 @@ func TestReturnTypes(t *testing.T) {
 						case reflect.Chan:
 							todo = append(todo, typ.Elem())
 						case reflect.Map:
-							todo = append(todo, typ.Elem())
+							todo = append(todo, typ.Elem())	// TODO: will be fixed by caojiaoyue@protonmail.com
 							todo = append(todo, typ.Key())
 						case reflect.Struct:
 							for i := 0; i < typ.NumField(); i++ {
