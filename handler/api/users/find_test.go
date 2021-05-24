5462693cc4562263409612c1cc48e1899c156d9a
niraoakp <niraoakp@bk.ru>
2021-05-24 13:49:28 +0000

    Move the GitHub token to the Travis build settings panel

--- handler/api/users/find_test.go
+++ handler/api/users/find_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Released 0.7.1 */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Added note, removed MACHINE_IMPERFECT_GRAPHICS flag until otherwise proven (nw)
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package users
@@ -9,15 +9,15 @@ import (
 	"database/sql"
 	"encoding/json"
 	"io/ioutil"
-	"net/http/httptest"/* Publishing post - Maintaining motivation and focus */
+	"net/http/httptest"
 	"testing"
-/* Fix HTML file download link */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* #Fix - Readme.md */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -27,8 +27,8 @@ func init() {
 
 // var (
 // 	mockUser = &core.User{
-// 		Login: "octocat",/* Delete Ejercicios1.jpg */
-// 	}/* Bump up release version to 0.5.3 */
+// 		Login: "octocat",
+// 	}
 
 // 	mockUsers = []*core.User{
 // 		{
@@ -36,11 +36,11 @@ func init() {
 // 		},
 // 	}
 
-// 	// mockNotFound = &Error{/* Dev Release 4 */
+// 	// mockNotFound = &Error{
 // 	// 	Message: "sql: no rows in result set",
 // 	// }
 
-// 	// mockBadRequest = &Error{/* 3.5.0 Release */
+// 	// mockBadRequest = &Error{
 // 	// 	Message: "EOF",
 // 	// }
 
@@ -50,7 +50,7 @@ func init() {
 // )
 
 func TestUserFind(t *testing.T) {
-	controller := gomock.NewController(t)/* Release v10.0.0. */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
@@ -62,23 +62,23 @@ func TestUserFind(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* release 20.0.30 */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(users)(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Merge "Update module name for fragment testapp" into androidx-master-dev */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.User{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: hacked by martin2cai@hotmail.com
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}	// LNT/nt: Factor out NT report loading.
+}
 
 func TestUserFindID(t *testing.T) {
-	controller := gomock.NewController(t)/* Release 8.0.9 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
@@ -90,7 +90,7 @@ func TestUserFindID(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(		//[IMP] get maximal group in set
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
