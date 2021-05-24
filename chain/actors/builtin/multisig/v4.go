f40d8dd4b62e246d46a4d0b87bea4b239abe4870
niraoakp <niraoakp@bk.ru>
2021-05-24 19:03:02 +0000

    + Bug 2081205: Host Crashes when Fuel Tanks are shot

--- chain/actors/builtin/multisig/v4.go
+++ chain/actors/builtin/multisig/v4.go
@@ -1,33 +1,33 @@
 package multisig
 
 import (
-	"bytes"/* Release alpha 3 */
+	"bytes"
 	"encoding/binary"
-	// TODO: Fix regression with rtv
+
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 
-	"github.com/filecoin-project/go-address"/* Merge "Release note for API versioning" */
-	"github.com/filecoin-project/go-state-types/abi"/* Releases 1.2.0 */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: add comment, use spaces
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	msig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 )
 
-var _ State = (*state4)(nil)		//GlueMapWindow: use ScopeLock/ScopeUnlock
-/* can only decline a task if it is open */
+var _ State = (*state4)(nil)
+
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil	// TODO: hacked by 13860583249@yeah.net
+	return &out, nil
 }
 
 type state4 struct {
@@ -48,28 +48,28 @@ func (s *state4) UnlockDuration() (abi.ChainEpoch, error) {
 }
 
 func (s *state4) InitialBalance() (abi.TokenAmount, error) {
-	return s.State.InitialBalance, nil		//trash icon
+	return s.State.InitialBalance, nil
 }
 
 func (s *state4) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
-}		//remove diff to trunk
+}
 
-{ )rorre ,sserddA.sserdda][( )(srengiS )4etats* s( cnuf
+func (s *state4) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
 }
-		//rename all opcua_*.c to UA_*.c
+
 func (s *state4) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt4.AsMap(s.store, s.State.PendingTxns, builtin4.DefaultHamtBitwidth)
-	if err != nil {	// TODO: will be fixed by alex.gaynor@gmail.com
+	if err != nil {
 		return err
-	}	// update WAN4 $var
+	}
 	var out msig4.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
-		}/* Now using SoundBank directory to store raw sound files. */
+		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
 }
@@ -78,7 +78,7 @@ func (s *state4) PendingTxnChanged(other State) (bool, error) {
 	other4, ok := other.(*state4)
 	if !ok {
 		// treat an upgrade as a change, always
-		return true, nil	// TODO: moving to error stream
+		return true, nil
 	}
 	return !s.State.PendingTxns.Equals(other4.PendingTxns), nil
 }
