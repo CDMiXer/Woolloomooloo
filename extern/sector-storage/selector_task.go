f99d9470f0d39d516abbb4710749b450489886d6
niraoakp <niraoakp@bk.ru>
2021-05-24 23:19:11 +0000

    282d2f5a-2e66-11e5-9284-b827eb9e62be

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,34 +1,34 @@
 package sectorstorage
-/* Minor comment improvement */
-import (/* Remove duplicate comment in #Installation paragraph */
-	"context"	// TODO: use freemarker template to create menu
-		//5da7363a-2e70-11e5-9284-b827eb9e62be
+
+import (
+	"context"
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"/* EIDI2 HA-Abgabe */
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
-}	// TODO: hacked by alex.gaynor@gmail.com
-		//Bug 1310: Added two missing test shell scripts.
+}
+
 func newTaskSelector() *taskSelector {
 	return &taskSelector{}
-}/* 609c02e6-2e41-11e5-9284-b827eb9e62be */
+}
 
-func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {/* index - Logo on link */
+func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)		//Delete dev-academy1.png
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	_, supported := tasks[task]
 
 	return supported, nil
 }
-/* Remove test code to reduce console spam. */
+
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
@@ -42,7 +42,7 @@ func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *work
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
 
-	return a.utilization() < b.utilization(), nil/* Control tower staging */
+	return a.utilization() < b.utilization(), nil
 }
 
 var _ WorkerSelector = &taskSelector{}
