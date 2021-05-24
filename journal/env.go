9aad99a800deeb91ab002ad18980540e603d6d2f
niraoakp <niraoakp@bk.ru>
2021-05-24 01:06:43 +0000

    connected TOGGLE_CHANNEL_FAVORITE (not sure why this was missed)

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
 
-import (		//rename db scripts
-	"os"/* Delete Data_Releases.rst */
+import (
+	"os"
 )
 
 // envJournalDisabledEvents is the environment variable through which disabled
-// journal events can be customized.		//Add android .apk
-const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"/* finish creation of borrower */
+// journal events can be customized.
+const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
 		}
-	}/* + OtlParallel execution model */
+	}
 	// fallback if env variable is not set, or if it failed to parse.
 	return DefaultDisabledEvents
-}		//Delete Book.php~
+}
