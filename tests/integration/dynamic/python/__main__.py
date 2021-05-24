3304cad3d360ef59f46bbe196125ac5015ff7532
niraoakp <niraoakp@bk.ru>
2021-05-24 07:25:03 +0000

    Update cu_facebook_activity.access.feature

--- tests/integration/dynamic/python/__main__.py
+++ tests/integration/dynamic/python/__main__.py
@@ -1,6 +1,6 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: 18 Sep feature is one-off of nexrad coverage before warnings
-	// TODO: Issue #24 - context.getNamespaceURI().
-import binascii		//Rename footboard_A/communication.ino to Main/Board/footboard_A/communication.ino
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import binascii
 import os
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
@@ -10,12 +10,12 @@ class RandomResourceProvider(ResourceProvider):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
 
-class Random(Resource):/* Add #copyWithAnnouncer (pair-programmed with Milton) */
+class Random(Resource):
     val: str
     def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
 r = Random("foo")
 
-export("random_id", r.id)		//config.template.php updated and install-debian.sh copying to config.php
+export("random_id", r.id)
 export("random_val", r.val)
