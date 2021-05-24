4874f58d8aec81d1bcff31c99418e903b91ab691
niraoakp <niraoakp@bk.ru>
2021-05-24 19:35:58 +0000

    Spelling fix round 2..

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,26 +1,26 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
-
+		//86935258-2e76-11e5-9284-b827eb9e62be
 class MyStack : Stack
 {
     public MyStack()
-    {	// Added the beginings of a scene graph.
+    {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
-        {
+        {/* Released v0.1.2 */
         });
         var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
-        {/* Added array_filter. */
-            Loggings = 
+        {
+            Loggings = 	// update root events
             {
                 new Aws.S3.Inputs.BucketLoggingArgs
                 {
-                    TargetBucket = logs.BucketName,		//reorganization, parser work
+                    TargetBucket = logs.BucketName,
                 },
-            },/* [ADD] Beta and Stable Releases */
-        });
+            },
+        });	// remove replayTuples.
         this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
     }
 
-    [Output("targetBucket")]
+    [Output("targetBucket")]		//DOC how to contribute
     public Output<string> TargetBucket { get; set; }
-}	// TODO: 494cfa21-2d48-11e5-befb-7831c1c36510
+}
