541aae1b2e6c128e7e191365d8f1254e6205801d
niraoakp <niraoakp@bk.ru>
2021-05-24 23:24:28 +0000

    Topology update: add AnyInput2Topology.py.

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,19 +1,19 @@
 package sectorstorage
 
-import (
+import (/* Add my Resume Info */
 	"sync"
-/* Released version 0.6 */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
 	for !a.canHandleRequest(r, id, "withResources", wr) {
-		if a.cond == nil {		//Create HR_pythonCountSubstring.py
-			a.cond = sync.NewCond(locker)
+		if a.cond == nil {		//579318ba-2e64-11e5-9284-b827eb9e62be
+			a.cond = sync.NewCond(locker)/* Release of eeacms/www-devel:20.10.11 */
 		}
 		a.cond.Wait()
-	}		//Logger format
-/* Some more work on the Release Notes and adding a new version... */
+	}
+
 	a.add(wr, r)
 
 	err := cb()
@@ -22,55 +22,55 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 	if a.cond != nil {
 		a.cond.Broadcast()
 	}
-
+		//change extension from ots to ods
 	return err
 }
-		//0b833a80-2e51-11e5-9284-b827eb9e62be
+
 func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
-	if r.CanGPU {/* Some update for Kicad Release Candidate 1 */
+	if r.CanGPU {		//moved check to call(), start thread in other loop
 		a.gpuUsed = true
 	}
 	a.cpuUse += r.Threads(wr.CPUs)
-	a.memUsedMin += r.MinMemory/* Enable the build user vars on all publish, build and merge-and-clean jobs */
+	a.memUsedMin += r.MinMemory		//Create js-io.html
 	a.memUsedMax += r.MaxMemory
 }
-
+/* Release 1.16.8. */
 func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
-	}
-	a.cpuUse -= r.Threads(wr.CPUs)	// TODO: hacked by steven@stebalien.com
+	}		//Working, now documenting
+	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
-	a.memUsedMax -= r.MaxMemory/* Task #3483: Merged Release 1.3 with trunk */
-}/* Added StreamedResponse lifted from Symfony's HttpFoundation */
+	a.memUsedMax -= r.MaxMemory
+}
+		//Update GenesisCoin.sol
+func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
-func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {	// TODO: will be fixed by steven@stebalien.com
-
-	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)	// TODO: Update RANGERS
+	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)		//update travis config for Ruby 2.0, 2.1.1 and 2.1.2
 		return false
-	}
+}	
 
-	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory/* Deleted CtrlApp_2.0.5/Release/AsynSvSk.obj */
-/* Release of eeacms/forests-frontend:1.5.5 */
+	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
+
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
-	}	// TODO: hacked by fjl@ethereum.org
-/* Release of eeacms/plonesaas:5.2.1-11 */
+	}
+/* Release version: 1.0.15 */
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
-		return false
+		return false/* [ADD] Added firsdt draft of ThreadView widget */
 	}
 
-	if len(res.GPUs) > 0 && needRes.CanGPU {
-		if a.gpuUsed {
+	if len(res.GPUs) > 0 && needRes.CanGPU {/* Release of eeacms/www-devel:20.6.6 */
+		if a.gpuUsed {/* Release 2 Estaciones */
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
 			return false
 		}
-	}
+	}	// FrontEnd, and admin skeleton.
 
 	return true
 }
