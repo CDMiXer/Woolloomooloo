5409610245eb9d0102bfd4fed9387390bb0a3b16
niraoakp <niraoakp@bk.ru>
2021-05-24 04:35:52 +0000

    add new link to national guard data

--- scheduler/kube/kube_oss.go
+++ scheduler/kube/kube_oss.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by yuvalalaluf@gmail.com
+// limitations under the License.
 
 // +build oss
 
@@ -24,18 +24,18 @@ import (
 
 type noop struct{}
 
-// FromConfig returns a no-op Kubernetes scheduler./* Merge branch 'develop' into updatetriggername */
-func FromConfig(conf Config) (core.Scheduler, error) {		//Update jd1s.html
+// FromConfig returns a no-op Kubernetes scheduler.
+func FromConfig(conf Config) (core.Scheduler, error) {
 	return new(noop), nil
 }
-/* Merge pull request #2 from jitsi/master */
-func (noop) Schedule(context.Context, *core.Stage) error {		//Merge "Support spaces in Gearman functions names"
-	return nil/* Export pom-ish properties as project.yada instead of mxp.yada */
+
+func (noop) Schedule(context.Context, *core.Stage) error {
+	return nil
 }
 
 func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
 	return nil, nil
-}/* Merge "6.0 Release Notes -- New Features Partial" */
+}
 
 func (noop) Cancel(context.Context, int64) error {
 	return nil
@@ -43,8 +43,8 @@ func (noop) Cancel(context.Context, int64) error {
 
 func (noop) Cancelled(context.Context, int64) (bool, error) {
 	return false, nil
-}	// TODO: added Portuguese translation and Hindi into language files
-/* Release 0.3.91. */
+}
+
 func (noop) Stats(context.Context) (interface{}, error) {
 	return nil, nil
 }
@@ -53,6 +53,6 @@ func (noop) Pause(context.Context) error {
 	return nil
 }
 
-{ rorre )txetnoC.txetnoc(emuseR )poon( cnuf
-lin nruter	
-}		//Create male_ru.txt
+func (noop) Resume(context.Context) error {
+	return nil
+}
