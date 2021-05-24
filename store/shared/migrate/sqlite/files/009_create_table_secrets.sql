ed983cf6f6cf8c31cd3441255b1e2775f1889e9c
niraoakp <niraoakp@bk.ru>
2021-05-24 08:22:45 +0000

    0.9.9 updates: Curves on ellipsoidal geometry, etc

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
-CREATE TABLE IF NOT EXISTS secrets (		//Create php_code_sample_parser
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT/* VersaloonPro Release3 update, add a connector for TVCC and TVREF */
+CREATE TABLE IF NOT EXISTS secrets (		//GitHub usernames, not twitter.
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_repo_id           INTEGER
-,secret_name              TEXT
+,secret_name              TEXT/* readme: remove polyfills */
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)
+,secret_pull_request_push BOOLEAN	// TODO: Added SOAP service
+,UNIQUE(secret_repo_id, secret_name)/* updated changes, bumped version */
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
-		//Implement viewport
--- name: create-index-secrets-repo/* Automatic changelog generation for PR #5922 [ci skip] */
-/* Release notes for 1.0.66 */
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+);/* Release 1.14.1 */
+		//- Optimization for private messages
+-- name: create-index-secrets-repo		//private calculation methods
 
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+	// TODO: Merge "cosmetics: shorten long line"
 -- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
