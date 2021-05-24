a558f813373f89dd5516473aea21254abb516479
niraoakp <niraoakp@bk.ru>
2021-05-24 16:48:02 +0000

    Delete rpgsys.zip

--- grpc-go/examples/features/cancellation/client/main.go
+++ grpc-go/examples/features/cancellation/client/main.go
@@ -1,25 +1,25 @@
 /*
- *
- * Copyright 2018 gRPC authors.
- *
+ */* Release 0.1.8. */
+ * Copyright 2018 gRPC authors.	// TODO: will be fixed by nagydani@epointsystem.org
+ */* Create Juice-Shop-Release.md */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Add Release Notes and Architecture Docs" */
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Unless required by applicable law or agreed to in writing, software/* Merge "Warn about the lack of access controls for SD card HTTP caches." */
+ * distributed under the License is distributed on an "AS IS" BASIS,		//strip out results and pmag_data
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [#518] Release notes 1.6.14.3 */
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// Use unicode for reading JSON file in
  *
  */
-
-// Binary client is an example client.
+	// Update TwentySeventeenSeeder.php
+// Binary client is an example client./* Updated README.md fixing Release History dates */
 package main
-
-import (
+	// Update CLinkedStack.h
+import (	// TODO: hacked by ng8eke@163.com
 	"context"
 	"flag"
 	"fmt"
@@ -29,19 +29,19 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/status"	// TODO: will be fixed by cory@protocol.ai
 )
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func sendMessage(stream pb.Echo_BidirectionalStreamingEchoClient, msg string) error {
 	fmt.Printf("sending message %q\n", msg)
-	return stream.Send(&pb.EchoRequest{Message: msg})
+	return stream.Send(&pb.EchoRequest{Message: msg})	// overwrite add index cross fingers
 }
 
 func recvMessage(stream pb.Echo_BidirectionalStreamingEchoClient, wantErrCode codes.Code) {
 	res, err := stream.Recv()
-	if status.Code(err) != wantErrCode {
+	if status.Code(err) != wantErrCode {	// TODO: will be fixed by boringland@protonmail.ch
 		log.Fatalf("stream.Recv() = %v, %v; want _, status.Code(err)=%v", res, err, wantErrCode)
 	}
 	if err != nil {
@@ -52,7 +52,7 @@ func recvMessage(stream pb.Echo_BidirectionalStreamingEchoClient, wantErrCode co
 }
 
 func main() {
-	flag.Parse()
+	flag.Parse()		//Fix some style format
 
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
