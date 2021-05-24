cc3eaa4e30112dad88feccbcf0541aca886e099c
niraoakp <niraoakp@bk.ru>
2021-05-24 22:52:33 +0000

    Factor out common base class AbstractTlsClient

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
-package lotuslog/* Releases 2.0 */
-
+package lotuslog
+/* Release 1.3.0.1 */
 import (
 	"os"
-
+		//Making unit tests more readable
 	logging "github.com/ipfs/go-log/v2"
 )
 
-func SetupLogLevels() {
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {/* Update Addons Release.md */
-		_ = logging.SetLogLevel("*", "INFO")
+func SetupLogLevels() {	// TODO: will be fixed by steven@stebalien.com
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
+		_ = logging.SetLogLevel("*", "INFO")/* moved constant back to GrailsScriptRunner */
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")	// TODO: will be fixed by juan@benet.ai
+		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")/* Released MonetDB v0.2.4 */
+		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* [release] prepare for next development iteration */
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")	// Make pkg-config dep a little less strict
 }
