fc582e1e555cb2943d40404abb3cf13ee9e6883c
niraoakp <niraoakp@bk.ru>
2021-05-24 17:09:40 +0000

    Fix urllib2 error log in YouTubeVideoUrl

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -2,7 +2,7 @@ package conformance
 
 import (
 	"context"
-
+	// TODO: will be fixed by nicksavers@gmail.com
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 
@@ -11,16 +11,16 @@ import (
 
 type fixedRand struct{}
 
-var _ vm.Rand = (*fixedRand)(nil)
-
+var _ vm.Rand = (*fixedRand)(nil)/* Added a socket factory that can deliver unreliable sockets for tests. */
+/* cleaning up curses window */
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
 }
-
-func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
-	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
+/* Release 8.9.0 */
+func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {/* Release 0.5.17 was actually built with JDK 16.0.1 */
+	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes./* Alter the path of the Rake tasks */
 }
 
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
