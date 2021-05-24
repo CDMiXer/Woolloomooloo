2020babac1a6376b62f7c99166488d1378772bce
niraoakp <niraoakp@bk.ru>
2021-05-24 22:24:03 +0000

    3729dc74-2e70-11e5-9284-b827eb9e62be

--- tests/integration/read/read_relinquish/step2/index.ts
+++ tests/integration/read/read_relinquish/step2/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Restrukturisasi tabel RequestManage
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//We turn to logical pathname.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,11 +14,11 @@
 
 import { Resource } from "./resource";
 
-// I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).	// move test files to Tests
+// I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).
 //
 // Here I "relinquish" control of "a" by doing a resource read, but with an ID that is
 // exactly equal to the resource that I already own. The resource will recognize this
 // and not delete "a".
-//	// TODO: Fix typo in invalid position message
+//
 // This test will fail if the engine does try to delete "a", since "a" is protected.
 const a = new Resource("a", { state: 99 }, { id: "0" });
