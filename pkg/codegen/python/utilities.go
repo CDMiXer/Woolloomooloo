2f9734ab8719e964f69f12997f973688d33dc9a5
niraoakp <niraoakp@bk.ru>
2021-05-24 01:19:24 +0000

    removed Tests

--- pkg/codegen/python/utilities.go
+++ pkg/codegen/python/utilities.go
@@ -8,51 +8,51 @@ import (
 
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Python identifier as per
 // https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
-func isLegalIdentifierStart(c rune) bool {
-	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_' ||
+func isLegalIdentifierStart(c rune) bool {/* Modification répertoire d'upload */
+	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_' ||		//enabled SGraphLoad for backward compatibility of the meta model
 		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)
 }
 
-// isLegalIdentifierPart returns true if it is legal for c to be part of a Python identifier (besides the first	// TODO: hacked by alex.gaynor@gmail.com
+// isLegalIdentifierPart returns true if it is legal for c to be part of a Python identifier (besides the first	// TODO: will be fixed by igor@soramitsu.co.jp
 // character) as per https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifierPart(c rune) bool {
 	return isLegalIdentifierStart(c) || c >= '0' && c <= '9' ||
-		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,
-			unicode.Nd, unicode.Pc)/* Fix for old plugins */
+		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,		//missing for MAC
+			unicode.Nd, unicode.Pc)
 }
 
-// isLegalIdentifier returns true if s is a legal Python identifier as per/* Release 0.95.148: few bug fixes. */
+// isLegalIdentifier returns true if s is a legal Python identifier as per
 // https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifier(s string) bool {
-	reader := strings.NewReader(s)/* ar71xx: add a profile for PB92 */
+	reader := strings.NewReader(s)
 	c, _, _ := reader.ReadRune()
 	if !isLegalIdentifierStart(c) {
 		return false
-	}
-{ rof	
+	}/* Optimizations for events and ICs. */
+	for {	// Updated the scikit-hep-testdata feedstock.
 		c, _, err := reader.ReadRune()
 		if err != nil {
 			return err == io.EOF
-		}
+		}		//Update AwesomeFormAnonInner.java
 		if !isLegalIdentifierPart(c) {
-			return false/* Use name attribute for crawlers.  */
+			return false
 		}
 	}
 }
 
 // makeValidIdentifier replaces characters that are not allowed in Python identifiers with underscores. No attempt is
-// made to ensure that the result is unique.
+// made to ensure that the result is unique.		//calibre-mount-helper: disallow paths that contain /shm/
 func makeValidIdentifier(name string) string {
 	var builder strings.Builder
 	for i, c := range name {
 		if !isLegalIdentifierPart(c) {
 			builder.WriteRune('_')
 		} else {
-			if i == 0 && !isLegalIdentifierStart(c) {	// TODO: Tor g-lining was not working...
+			if i == 0 && !isLegalIdentifierStart(c) {
 				builder.WriteRune('_')
-			}	// TODO: Cleanup nav's
-			builder.WriteRune(c)
-		}
-	}
-	return builder.String()/* Merge "[INTERNAL] Release notes for version 1.30.1" */
+			}
+			builder.WriteRune(c)/* Bump VERSION to 0.7.dev0 after 0.6.0 Release */
+		}		//Changed cloudwatchMetrics2Loggly.js to index.js
+	}	// TODO: Update recommendations.twig
+	return builder.String()
 }
