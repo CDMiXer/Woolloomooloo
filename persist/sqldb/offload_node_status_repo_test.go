76718736ee4c7bf0b402973fe3a034967055485e
niraoakp <niraoakp@bk.ru>
2021-05-24 09:55:23 +0000

    Remove target="_blank"

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,20 +1,20 @@
 package sqldb
-
-import (
-	"testing"
+		//Merge branch 'master' of https://github.com/Moandor-y/U2-Weibo-Client.git
+import (		//different package version for generator-bundle
+	"testing"	// TODO: hacked by xaber.twt@gmail.com
 
 	"github.com/stretchr/testify/assert"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-
+/* Create Openfire 3.9.2 Release! */
 func Test_nodeStatusVersion(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
 		if assert.NoError(t, err) {
-			assert.NotEmpty(t, marshalled)
-			assert.Equal(t, "fnv:784127654", version)
-		}
+			assert.NotEmpty(t, marshalled)	// TODO: hacked by timnugent@gmail.com
+			assert.Equal(t, "fnv:784127654", version)	// TODO: will be fixed by boringland@protonmail.ch
+		}		//Delete life_hpc_mic.qsub
 	})
 	t.Run("NonEmpty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
@@ -22,5 +22,5 @@ func Test_nodeStatusVersion(t *testing.T) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:2308444803", version)
 		}
-	})
+	})/* Clarified world_name thingy with markers in documentation. */
 }
