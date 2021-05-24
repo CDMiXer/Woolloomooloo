ceaebef71ed1f29ac8aa53a04bb0d2a374c4f4ee
niraoakp <niraoakp@bk.ru>
2021-05-24 14:22:23 +0000

    more fixes for subset handling in ERA5

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -3,13 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release version: 1.0.7 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by zaq1tomo@gmail.com
+// See the License for the specific language governing permissions and	// TODO: fixed article category color scheme
 // limitations under the License.
 
 package acl
@@ -26,32 +26,32 @@ import (
 // AuthorizeUser returns an http.Handler middleware that authorizes only
 // authenticated users to proceed to the next handler in the chain. Guest users
 // are rejected with a 401 unauthorized error.
-func AuthorizeUser(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func AuthorizeUser(next http.Handler) http.Handler {	// TODO: will be fixed by lexy8russo@outlook.com
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* Merge "Release 1.0.0.215 QCACLD WLAN Driver" */
 		_, ok := request.UserFrom(r.Context())
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
 				Debugln("api: authentication required")
 		} else {
-			next.ServeHTTP(w, r)
-		}
-	})
+			next.ServeHTTP(w, r)/* Merge branch 'dialog_implementation' into Release */
+		}	// TODO: Correct title in README.rst
+	})/* Merge "Fix V2 hypervisor server schema attribute" */
 }
 
 // AuthorizeAdmin returns an http.Handler middleware that authorizes only
-// system administrators to proceed to the next handler in the chain.
+// system administrators to proceed to the next handler in the chain.	// Fix to the Fact: Item MUST call expessly default methods
 func AuthorizeAdmin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user, ok := request.UserFrom(r.Context())
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
-			logger.FromRequest(r).
-				Debugln("api: authentication required")
+			logger.FromRequest(r).	// TODO: bug fix optional inports
+				Debugln("api: authentication required")	// TODO: 55df62a8-2e5a-11e5-9284-b827eb9e62be
 		} else if !user.Admin {
 			render.Forbidden(w, errors.ErrForbidden)
 			logger.FromRequest(r).
-				Debugln("api: administrative access required")
+				Debugln("api: administrative access required")		//Document windows portable installer fixes #148
 		} else {
 			next.ServeHTTP(w, r)
 		}
