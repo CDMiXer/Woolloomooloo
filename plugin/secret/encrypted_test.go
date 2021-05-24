4b7e970d300a3b5c06d4fac1dc51de708471f37c
niraoakp <niraoakp@bk.ru>
2021-05-24 15:05:48 +0000

    frame Child Window enhancement

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Delete page9.html
-// Licensed under the Apache License, Version 2.0 (the "License");	// Define a tipografia padrão do tema
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Now able to to call Engine Released */
+//      http://www.apache.org/licenses/LICENSE-2.0	// Create MainWindow.form
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Simplify fetch on individual request view
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software		//Updating @Optional annotation
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Updated pivot tables
 // See the License for the specific language governing permissions and
-// limitations under the License.		//yay! landing pages work
+// limitations under the License.
 
 package secret
