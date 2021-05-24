e21857a1f2ed6438246438701c8922ad16f4dae7
niraoakp <niraoakp@bk.ru>
2021-05-24 10:08:40 +0000

    change initial bitvector size.

--- handler/api/user/remote/repo.go
+++ handler/api/user/remote/repo.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* adding readme for cublas examples */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Create SObjectWrapperUtility */
 // You may obtain a copy of the License at
-//
+///* Update README.md so that it uses provided imports */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,35 +17,35 @@ package remote
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//added a notebook for RBMs and DBNs
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"	// TODO: extract block methods in rake_tasks, use "run action" hook on base
 
 	"github.com/go-chi/chi"
 )
 
-// HandleRepo returns an http.HandlerFunc that writes a json-encoded
-// repository to the response body.
+// HandleRepo returns an http.HandlerFunc that writes a json-encoded/* Fix Release and NexB steps in Jenkinsfile */
+// repository to the response body./* Content Release 19.8.1 */
 func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			viewer, _ = request.UserFrom(r.Context())
-
+/* moved phpunit.xml.dist */
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 			slug  = scm.Join(owner, name)
 		)
-
-		repo, err := repos.Find(r.Context(), viewer, slug)
+		//Started driver class and fixed other classes.
+		repo, err := repos.Find(r.Context(), viewer, slug)	// TODO: Add otherbot remove and fix formatting on watching
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot get remote repository")
+				Debugln("api: cannot get remote repository")/* DROOLS-1701 Support for FEEL fn definition (non-external, FEEL defined) */
 			return
-		}
-
+		}/* Release v1.46 */
+		//debug output for finding the travis problem
 		perms, err := repos.FindPerm(r.Context(), viewer, slug)
 		if err != nil {
 			render.InternalError(w, err)
