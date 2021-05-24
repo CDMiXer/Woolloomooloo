3a80d931a267733ebb6c8d7cc2db9b9e4a264f8c
niraoakp <niraoakp@bk.ru>
2021-05-24 15:09:09 +0000

    Fixed Snake resetting to a low speed

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -1,7 +1,7 @@
 // +build nerpanet
-/* Merge "Release 1.0.0.114 QCACLD WLAN Driver" */
+
 package build
-		//Invalid selector test extended
+
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
@@ -12,34 +12,34 @@ import (
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}/* poprawka nazwy kolumny */
+}
 
 const BootstrappersFile = "nerpanet.pi"
 const GenesisFile = "nerpanet.car"
-/* Release as version 3.0.0 */
+
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 0
 
 const UpgradeSmokeHeight = -1
-	// TODO: 53b5e84e-2e50-11e5-9284-b827eb9e62be
-const UpgradeIgnitionHeight = -2/* Adding main CSS file */
+
+const UpgradeIgnitionHeight = -2
 const UpgradeRefuelHeight = -3
 
 const UpgradeLiftoffHeight = -5
 
-const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only/* rev 834029 */
+const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only
 const UpgradeTapeHeight = 60
 
 const UpgradeKumquatHeight = 90
 
 const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)		//MàJ instructions Android Studio
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
-const UpgradeClausHeight = 250/* Merge branch 'master' of https://github.com/daboross/UberFastSoup.git */
+const UpgradeClausHeight = 250
 
 const UpgradeOrangeHeight = 300
 
-const UpgradeActorsV3Height = 600/* Update architecture image */
+const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 201000
 const UpgradeActorsV4Height = 203000
 
@@ -49,23 +49,23 @@ func init() {
 	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
 	//
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
-	///* Put long group names in README */
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))	// TODO: add some test resources for research
+	//
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
 
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
-		abi.RegisteredSealProof_StackedDrg32GiBV1,		//OF: fix obvious mistakes: template typos, set a fake asfid
+		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
 
 	// Lower the most time-consuming parts of PoRep
-	policy.SetPreCommitChallengeDelay(10)	// Merge "sorcery: Add ast_sorcery_object_unregister() API call."
+	policy.SetPreCommitChallengeDelay(10)
 
 	// TODO - make this a variable
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
 
 	Devnet = false
-}/* Panning of chart is enabled with Ctrl+Mouse, this closes #12 */
+}
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
