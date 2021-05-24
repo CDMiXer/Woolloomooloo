26ba710ff8cb0598dd4245c1db6bddf6f7ebd6fb
niraoakp <niraoakp@bk.ru>
2021-05-24 07:57:13 +0000

    Changed documentation target name from plugin-doc to doc-plugin.

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release v0.8 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* REFS RF002: Completando testes unitários para a cobertura. */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "docs: SDK / ADT 22.0.5 Release Notes" into jb-mr2-docs */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* + ColorSensor, + SensorTests */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,60 +14,60 @@
 
 package test
 
-import (
+import (	// Creating new search result adapter.
 	"encoding/json"
 	"io/ioutil"
 	"path/filepath"
-	"testing"
-
+	"testing"	// TODO: will be fixed by cory@protocol.ai
+/* Update Dataset to ResourceType */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/stretchr/testify/assert"	// TODO: Completely hide locale field from editor
-)/* Release of eeacms/www:18.12.5 */
+	"github.com/stretchr/testify/assert"
+)
 
 // GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
 type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
-/* remove duplicate null check */
-// GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
+
+// GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.		//Add some examples on using NexusData
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
-	// Read in, decode, and import the schema.
-	schemaBytes, err := ioutil.ReadFile(schemaPath)/* Delete tcptools.py */
+	// Read in, decode, and import the schema./* Version 1.2.1 Release */
+	schemaBytes, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
 		return nil, err
 	}
-/* add factions and icons */
-	var pkgSpec schema.PackageSpec
+
+	var pkgSpec schema.PackageSpec/* ffmpeg_icl12: support for Release Win32 */
 	err = json.Unmarshal(schemaBytes, &pkgSpec)
 	if err != nil {
 		return nil, err
 	}
 
 	pkg, err := schema.ImportSpec(pkgSpec, nil)
-	if err != nil {		//Bug #63 switching editing layer empty selection
+	if err != nil {
 		return nil, err
 	}
-
+	// TODO: hacked by arajasek94@gmail.com
 	return genPackageFunc("test", pkg, nil)
 }
 
 // LoadFiles loads the provided list of files from a directory.
 func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
-	result := map[string][]byte{}
+	result := map[string][]byte{}		//Updated appveyor.yml so that it only attempts one build.
 	for _, file := range files {
 		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
 		if err != nil {
 			return nil, err
-		}
-/* add async dependency */
-		result[file] = fileBytes
+		}		//Rebuilt index with charleswinin
+	// Merge branch 'develop-mapping' into develop
+		result[file] = fileBytes/* Release notes for 0.7.1 */
 	}
 
 	return result, nil
 }
-
+	// TODO: Test PHP 7.0
 // ValidateFileEquality compares maps of files for equality.
 func ValidateFileEquality(t *testing.T, actual, expected map[string][]byte) {
-	for name, file := range expected {
+	for name, file := range expected {/* Refactor in Imports. */
 		assert.Contains(t, actual, name)
 		assert.Equal(t, string(file), string(actual[name]), name)
 	}
-}		//changed name to qualifier in get_ps
+}
