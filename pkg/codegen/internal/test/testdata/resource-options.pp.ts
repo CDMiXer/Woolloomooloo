320c21078a8b2d1942e112e53368bdc90edbc73c
niraoakp <niraoakp@bk.ru>
2021-05-24 23:13:58 +0000

    882cb342-2e72-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,13 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-
+	// TODO: CloneHelper: must be able to handle uninitialized list fields
 const provider = new aws.Provider("provider", {region: "us-west-2"});
-const bucket1 = new aws.s3.Bucket("bucket1", {}, {
+const bucket1 = new aws.s3.Bucket("bucket1", {}, {/* 2b520e68-35c7-11e5-b6c2-6c40088e03e4 */
     provider: provider,
     dependsOn: [provider],
     protect: true,
     ignoreChanges: [
-        "bucket",
+        "bucket",/* Merge "[FIX] sap.ui.model.type.String: JSDoc for minLength is added" */
         "lifecycleRules[0]",
     ],
 });
