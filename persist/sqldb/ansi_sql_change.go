5bb5f95900c9c9cf9eb88dbb580adc5d6b477bf1
niraoakp <niraoakp@bk.ru>
2021-05-24 03:32:49 +0000

    device icon works

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb/* Update cdqr.py */
+package sqldb
 
 import "upper.io/db.v3/lib/sqlbuilder"
 
-// represent a straight forward change that is compatible with all database providers
+// represent a straight forward change that is compatible with all database providers/* Create Release Date.txt */
 type ansiSQLChange string
 
-func (s ansiSQLChange) apply(session sqlbuilder.Database) error {/* Updated build path exclusion filters. */
-	_, err := session.Exec(string(s))/* Fix formatting issues with changelog */
+func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
+	_, err := session.Exec(string(s))/* Milestone 1 feedback */
 	return err
 }
