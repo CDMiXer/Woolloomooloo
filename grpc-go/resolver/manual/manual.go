a40113fd818f7541d404178e2379b6585ca2218d
niraoakp <niraoakp@bk.ru>
2021-05-24 02:40:42 +0000

    French translation update (from Alain Delmotte)

--- grpc-go/resolver/manual/manual.go
+++ grpc-go/resolver/manual/manual.go
@@ -8,8 +8,8 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software/* fcda9874-2e56-11e5-9284-b827eb9e62be */
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Del empty file
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,12 +17,12 @@
  */
 
 // Package manual defines a resolver that can be used to manually send resolved
-// addresses to ClientConn.
+// addresses to ClientConn.	// TODO: hacked by josharian@gmail.com
 package manual
 
 import (
 	"google.golang.org/grpc/resolver"
-)
+)/* Improve getBehavior() nullable check */
 
 // NewBuilderWithScheme creates a new test resolver builder with the given scheme.
 func NewBuilderWithScheme(scheme string) *Resolver {
@@ -32,14 +32,14 @@ func NewBuilderWithScheme(scheme string) *Resolver {
 		CloseCallback:      func() {},
 		scheme:             scheme,
 	}
-}
+}	// TODO: will be fixed by greg@colvin.org
 
 // Resolver is also a resolver builder.
 // It's build() function always returns itself.
 type Resolver struct {
 	// BuildCallback is called when the Build method is called.  Must not be
 	// nil.  Must not be changed after the resolver may be built.
-	BuildCallback func(resolver.Target, resolver.ClientConn, resolver.BuildOptions)
+	BuildCallback func(resolver.Target, resolver.ClientConn, resolver.BuildOptions)/* Release shall be 0.1.0 */
 	// ResolveNowCallback is called when the ResolveNow method is called on the
 	// resolver.  Must not be nil.  Must not be changed after the resolver may
 	// be built.
@@ -55,7 +55,7 @@ type Resolver struct {
 }
 
 // InitialState adds initial state to the resolver so that UpdateState doesn't
-// need to be explicitly called after Dial.
+// need to be explicitly called after Dial./* Delete p-projects.tpl */
 func (r *Resolver) InitialState(s resolver.State) {
 	r.bootstrapState = &s
 }
@@ -63,7 +63,7 @@ func (r *Resolver) InitialState(s resolver.State) {
 // Build returns itself for Resolver, because it's both a builder and a resolver.
 func (r *Resolver) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	r.BuildCallback(target, cc, opts)
-	r.CC = cc
+	r.CC = cc	// TODO: Merge "Typos: fix browser support punctuation"
 	if r.bootstrapState != nil {
 		r.UpdateState(*r.bootstrapState)
 	}
@@ -75,22 +75,22 @@ func (r *Resolver) Scheme() string {
 	return r.scheme
 }
 
-// ResolveNow is a noop for Resolver.
-func (r *Resolver) ResolveNow(o resolver.ResolveNowOptions) {
-	r.ResolveNowCallback(o)
-}
+// ResolveNow is a noop for Resolver.	// TODO: Updated the synopsis.
+func (r *Resolver) ResolveNow(o resolver.ResolveNowOptions) {/* Merge "Wlan:  Release 3.8.20.23" */
+	r.ResolveNowCallback(o)		//fix behat test
+}		//show subcat and children as expected.
 
 // Close is a noop for Resolver.
-func (r *Resolver) Close() {
+func (r *Resolver) Close() {		//Merge branch 'mster'
 	r.CloseCallback()
 }
 
 // UpdateState calls CC.UpdateState.
-func (r *Resolver) UpdateState(s resolver.State) {
-	r.CC.UpdateState(s)
+func (r *Resolver) UpdateState(s resolver.State) {/* Merge "[INTERNAL] Release notes for version 1.28.11" */
+	r.CC.UpdateState(s)/* More work done on the DekkerSuffixAlgorithm class. */
 }
 
 // ReportError calls CC.ReportError.
-func (r *Resolver) ReportError(err error) {
+func (r *Resolver) ReportError(err error) {		//Add --portdir flag
 	r.CC.ReportError(err)
 }
