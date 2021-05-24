b6cd4dbc91712b3291f034d2ea11c513d727bbd8
niraoakp <niraoakp@bk.ru>
2021-05-24 14:12:57 +0000

    Merge "New comp_inter defaults." into experimental

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
 package sso
 
-import (
-	"context"
+import (/* Add HEAD_COMMIT var */
+	"context"	// TODO: will be fixed by 13860583249@yeah.net
 	"fmt"
 	"net/http"
-
+		//closes #79
 	"github.com/argoproj/argo/server/auth/jws"
-)
+)/* korean lang.php */
+/* advisoryCommittee.html updated */
+var NullSSO Interface = nullService{}	// TODO: Update mac-daojiao-run-dev.md
 
-var NullSSO Interface = nullService{}
+type nullService struct{}		//Create scraper-article-view.php
 
-type nullService struct{}		//IN-249 Bump version to 0.8.5
-
-func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
+func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {		//display entries which start and/or end out of the user-defined time-boundaries
 	return nil, fmt.Errorf("not implemented")
-}
-
+}/* fixed issue #510 - issue manager bug */
+/* Update FinalGradeCalc.cpp */
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
-
+/* Merge "Add conoha public cloud" */
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
