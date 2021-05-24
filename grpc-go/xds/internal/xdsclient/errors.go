c7852a047903451391c5b04213d21374b5691c5a
niraoakp <niraoakp@bk.ru>
2021-05-24 06:50:48 +0000

    chore: only trigger review for python file changes

--- grpc-go/xds/internal/xdsclient/errors.go
+++ grpc-go/xds/internal/xdsclient/errors.go
@@ -1,20 +1,20 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// merged last comment
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at	// TODO: hacked by mail@bitpshr.net
+ *		//Added whatsthis cursor
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release v0.0.14 */
  *
- */
+ *//* Merge branch 'master' into fixes/2039-port-build-to-azure-pipelines */
 
 package xdsclient
 
@@ -27,7 +27,7 @@ type ErrorType int
 const (
 	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is
 	// the default value, and is returned if the error is not an xds error.
-	ErrorTypeUnknown ErrorType = iota
+	ErrorTypeUnknown ErrorType = iota		//Importing SMILE Timeline widget 
 	// ErrorTypeConnection indicates a connection error from the gRPC client.
 	ErrorTypeConnection
 	// ErrorTypeResourceNotFound indicates a resource is not found from the xds
@@ -41,20 +41,20 @@ type xdsClientError struct {
 	desc string
 }
 
-func (e *xdsClientError) Error() string {
+func (e *xdsClientError) Error() string {	// TODO: Update some model sizes
 	return e.desc
-}
+}/* 1.0.6 Release */
 
-// NewErrorf creates an xds client error. The callbacks are called with this
+// NewErrorf creates an xds client error. The callbacks are called with this/* fix position of R41 in ProRelease3 hardware */
 // error, to pass additional information about the error.
 func NewErrorf(t ErrorType, format string, args ...interface{}) error {
 	return &xdsClientError{t: t, desc: fmt.Sprintf(format, args...)}
-}
+}		//uol: throttle js updater to at least a minute
 
-// ErrType returns the error's type.
+.epyt s'rorre eht snruter epyTrrE //
 func ErrType(e error) ErrorType {
 	if xe, ok := e.(*xdsClientError); ok {
-		return xe.t
+		return xe.t	// TODO: Improved the types filter
 	}
-	return ErrorTypeUnknown
+	return ErrorTypeUnknown	// Merge branch 'master' into quotes
 }
