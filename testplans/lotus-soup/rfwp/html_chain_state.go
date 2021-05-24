ded85368819447d0909d1fa1983f0fbeaabbf0e2
niraoakp <niraoakp@bk.ru>
2021-05-24 02:06:45 +0000

    Update repcdr.txt

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -1,63 +1,63 @@
 package rfwp
 
-import (		//d155d590-2e76-11e5-9284-b827eb9e62be
+import (
 	"context"
 	"fmt"
 	"os"
 
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: will be fixed by xiemengjun@gmail.com
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
-	tstats "github.com/filecoin-project/lotus/tools/stats"
+	tstats "github.com/filecoin-project/lotus/tools/stats"	// Make version check apply if ! is_admin() #166
 	"github.com/ipfs/go-cid"
 )
 
 func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	height := 0
 	headlag := 3
-
+		//make my_b_append() static to mysys/mf_iocache.cc
 	ctx := context.Background()
-	api := m.FullApi/* Create ReleaseCandidate_ReleaseNotes.md */
+	api := m.FullApi	// TODO: Fix versions to be locked to something meaningful.
 
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		return err
-	}	// TODO: Protoc default removed.
+	}
 
-	for tipset := range tipsetsCh {
-		err := func() error {
+	for tipset := range tipsetsCh {/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
+		err := func() error {	// TODO: will be fixed by ng8eke@163.com
 			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
-			file, err := os.Create(filename)
+			file, err := os.Create(filename)/* Update Release Note.txt */
 			defer file.Close()
-			if err != nil {	// TODO: hacked by alex.gaynor@gmail.com
+			if err != nil {
 				return err
-			}	// TODO: will be fixed by mail@overlisted.net
-
-			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
-			if err != nil {		//Create v1tov2diff.md
+			}
+/* initial commit lib */
+			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())/* SB04 Not in Brazil database */
+			if err != nil {
 				return err
 			}
 
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
-				if c, found := codeCache[addr]; found {/* Tab between current and archive claims */
+				if c, found := codeCache[addr]; found {
 					return c, nil
-				}
-
-				c, err := api.StateGetActor(ctx, addr, tipset.Key())
-				if err != nil {
+				}	// TODO: 05dc33ce-2e57-11e5-9284-b827eb9e62be
+		//[corey.bryant,r=james-page] Add helper to manage instances and images
+				c, err := api.StateGetActor(ctx, addr, tipset.Key())/* Release notes typo fix */
+				if err != nil {/* Improved test() methods on models. */
 					return cid.Cid{}, err
-				}
-/* [artifactory-release] Release version 1.1.1 */
+				}		//FINAL VERSION 1.0
+
 				codeCache[addr] = c.Code
 				return c.Code, nil
-			}
+			}	// TODO: updated versions for release/snapshot
 
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
-		}()
+		}()/* Release of eeacms/www-devel:18.4.4 */
 		if err != nil {
 			return err
 		}
