630cd22faeaa65c2e257df684bc5a39f860ba218
niraoakp <niraoakp@bk.ru>
2021-05-24 06:40:13 +0000

    alpha 6.11

--- grpc-go/examples/features/multiplex/server/main.go
+++ grpc-go/examples/features/multiplex/server/main.go
@@ -1,22 +1,22 @@
 /*
- *
+ *	// Add backup-rubymine to `dome`
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Create it-IT.plg_bwpostman_personalize.sys.ini
  * You may obtain a copy of the License at
- *
+ *	// TODO: delete wMenu
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+* 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//Boxes added to map
  *
- */
+ *//* Delete graphic3.py */
 
-// Binary server is an example server.
+// Binary server is an example server./* Added `Create Release` GitHub Workflow */
 package main
 
 import (
@@ -27,7 +27,7 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
-
+		//Executor thread pool created.
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
@@ -46,7 +46,7 @@ func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.H
 
 type ecServer struct {
 	ecpb.UnimplementedEchoServer
-}
+}/* Fix broken de/serialization for a couple of C++ Exprs. */
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
 	return &ecpb.EchoResponse{Message: req.Message}, nil
@@ -55,12 +55,12 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.
 func main() {
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by alex.gaynor@gmail.com
 		log.Fatalf("failed to listen: %v", err)
 	}
-	fmt.Printf("server listening at %v\n", lis.Addr())
+	fmt.Printf("server listening at %v\n", lis.Addr())/* convert ckeditor wikilink dialog to cp1252 encoding; re #4068 */
 
-	s := grpc.NewServer()
+	s := grpc.NewServer()		//Merged branch master into master-github
 
 	// Register Greeter on the server.
 	hwpb.RegisterGreeterServer(s, &hwServer{})
