b393cb484d88313a872d2cd32afa31a2dba3021e
niraoakp <niraoakp@bk.ru>
2021-05-24 19:54:13 +0000

    Delete change

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
-
+		//ar71xx: use the generic uci-default functions for initial LED setup
 import "github.com/hashicorp/hcl/v2/hclsyntax"
-
+/* 8648903a-2e4c-11e5-9284-b827eb9e62be */
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
-var None hclsyntax.Node = &hclsyntax.Body{}
+var None hclsyntax.Node = &hclsyntax.Body{}		//Merge Roberts tests, slight tweaks to code style.
