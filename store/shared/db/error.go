aea0132879923c7ea266d839df89fe1cc39a6c2c
niraoakp <niraoakp@bk.ru>
2021-05-24 07:39:22 +0000

    Delete Power BI Sample.ipynb

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc./* 81cf0e3b-2d15-11e5-af21-0401358ea401 */
-///* proper collision boxes */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Clean up readme intro
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release for v5.2.3. */
-//      http://www.apache.org/licenses/LICENSE-2.0	// Added the ability to reset a compass back to the spawn-point
-///* Release dhcpcd-6.6.3 */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Alternatív letöltés az Azure-ról
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software/* Delete NvFlexReleaseD3D_x64.dll */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package db
+package db		//Removes whitelist parameter and fixes testsuite name
 
 import "errors"
-
-// ErrOptimisticLock is returned by if the struct being
+/* Release version 0.12.0 */
+// ErrOptimisticLock is returned by if the struct being	// TODO: will be fixed by cory@protocol.ai
 // modified has a Version field and the value is not equal
 // to the current value in the database
-var ErrOptimisticLock = errors.New("Optimistic Lock Error")
+var ErrOptimisticLock = errors.New("Optimistic Lock Error")	// TODO: hacked by aeongrp@outlook.com
