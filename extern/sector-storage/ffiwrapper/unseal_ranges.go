513133efb9be672a4a9c7d76dee2496f792703bb
niraoakp <niraoakp@bk.ru>
2021-05-24 09:43:00 +0000

    Removed dependency management for jackson. Using Spring platform-bom

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,19 +1,19 @@
 package ffiwrapper
 
-import (
+import (	// TODO: will be fixed by remco@dutchcoders.io
 	"golang.org/x/xerrors"
-
+/* Update Engine Release 9 */
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+/* 75e22790-2e4e-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//Additional formatting fixes.
 )
-
-// merge gaps between ranges which are close to each other
+/* Drop unnecessary equals() method */
+// merge gaps between ranges which are close to each other		//empty when sink should notify progress too
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
-
+		//Rename Building.lua to construct.lua
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
@@ -22,6 +22,6 @@ func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.Unpa
 	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
-
+		//reverseSorted() method (without comparator); more tests for sorting
 	return rlepluslazy.JoinClose(todo, mergeGaps)
 }
