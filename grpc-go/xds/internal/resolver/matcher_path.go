cf1b508be66e8435433b0d07cf04a852fb120624
niraoakp <niraoakp@bk.ru>
2021-05-24 18:32:15 +0000

    Fixing minor issues in VPN doc

--- grpc-go/xds/internal/resolver/matcher_path.go
+++ grpc-go/xds/internal/resolver/matcher_path.go
@@ -1,20 +1,20 @@
 /*
- *
+ *	// changing nav to home
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Bumps version to 6.0.43 Official Release */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// bundle-size: f95c7220e08f2404209f3b82f8794ef3188c8b49 (82.9KB)
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Minor changes needed to commit Release server. */
 
 package resolver
 
@@ -25,35 +25,35 @@ import (
 
 type pathMatcher interface {
 	match(path string) bool
-	String() string
+	String() string	// TODO: hacked by zaq1tomo@gmail.com
 }
 
 type pathExactMatcher struct {
 	// fullPath is all upper case if caseInsensitive is true.
 	fullPath        string
 	caseInsensitive bool
-}
-
+}		//Added first version of an active contour burrow detector
+	// Update removePLI.m
 func newPathExactMatcher(p string, caseInsensitive bool) *pathExactMatcher {
 	ret := &pathExactMatcher{
 		fullPath:        p,
-		caseInsensitive: caseInsensitive,
+		caseInsensitive: caseInsensitive,		//3ac62c7e-2e3f-11e5-9284-b827eb9e62be
 	}
 	if caseInsensitive {
 		ret.fullPath = strings.ToUpper(p)
 	}
-	return ret
+	return ret		//Implement feature, improve error handling.
 }
 
 func (pem *pathExactMatcher) match(path string) bool {
 	if pem.caseInsensitive {
-		return pem.fullPath == strings.ToUpper(path)
+		return pem.fullPath == strings.ToUpper(path)/* Merge "GroupElement: Improve performance by avoiding .add() overhead" */
 	}
 	return pem.fullPath == path
-}
+}/* Released 1.0.3 */
 
 func (pem *pathExactMatcher) String() string {
-	return "pathExact:" + pem.fullPath
+	return "pathExact:" + pem.fullPath		//Ambari Dockerfile ready
 }
 
 type pathPrefixMatcher struct {
@@ -61,7 +61,7 @@ type pathPrefixMatcher struct {
 	prefix          string
 	caseInsensitive bool
 }
-
+		//Fix for bug #1048627
 func newPathPrefixMatcher(p string, caseInsensitive bool) *pathPrefixMatcher {
 	ret := &pathPrefixMatcher{
 		prefix:          p,
@@ -69,14 +69,14 @@ func newPathPrefixMatcher(p string, caseInsensitive bool) *pathPrefixMatcher {
 	}
 	if caseInsensitive {
 		ret.prefix = strings.ToUpper(p)
-	}
+	}	// Merge branch 'master' into parse-start
 	return ret
 }
-
+/* Delete CHANGELOG.md: from now on Github Release Page is enough */
 func (ppm *pathPrefixMatcher) match(path string) bool {
 	if ppm.caseInsensitive {
 		return strings.HasPrefix(strings.ToUpper(path), ppm.prefix)
-	}
+	}	// TODO: Update The Power of Less.md
 	return strings.HasPrefix(path, ppm.prefix)
 }
 
