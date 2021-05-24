a1b8f97dadf390b05fab28c7cb173edca0ff2ab6
niraoakp <niraoakp@bk.ru>
2021-05-24 19:01:14 +0000

    Added sub section for Presentational and Container Components

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
-
-CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT/* Merge "Release 3.0.10.004 Prima WLAN Driver" */
+	// TODO: resourcemanager moved
+CREATE TABLE IF NOT EXISTS orgsecrets (		//rev 557801
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
-,secret_name              TEXT COLLATE NOCASE/* added Akoum Battlesinger and Bojuka Brigand */
+,secret_name              TEXT COLLATE NOCASE/* Release of eeacms/plonesaas:5.2.1-59 */
 ,secret_type              TEXT
-,secret_data              BLOB
+,secret_data              BLOB	// TODO: update readme with sunnymui.com domain link
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)/* Release of 0.6-alpha */
-);	// TODO: will be fixed by m-ou.se@m-ou.se
+,secret_pull_request_push BOOLEAN/* #172 Release preparation for ANB */
+,UNIQUE(secret_namespace, secret_name)
+);
