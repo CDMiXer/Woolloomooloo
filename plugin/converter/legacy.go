db83eeddef6a4ecc20f0212eb4f5c8cbd9c22071
niraoakp <niraoakp@bk.ru>
2021-05-24 06:45:28 +0000

    notification delay option added

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Kunde,Adresse,Mock
+// that can be found in the LICENSE file.
 
-// +build !oss	// Change 'Components' to lowercase
-	// TODO: hacked by davidad@alum.mit.edu
+// +build !oss
+
 package converter
 
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-)/* Updated CS-CoreLib Version to the latest Release */
+)
 
-// Legacy returns a conversion service that converts a/* Control Ingreso numeros y 2 decimales. */
+// Legacy returns a conversion service that converts a	// Update readme links
 // legacy 0.8 yaml file to a yaml file.
-func Legacy(enabled bool) core.ConvertService {
-	return &legacyPlugin{
-		enabled: enabled,
-	}		//Disable landscape due to confliction
-}
-	// TODO: hacked by mikeal.rogers@gmail.com
+func Legacy(enabled bool) core.ConvertService {		//Use --noinput in django:syncdb
+	return &legacyPlugin{/* New: Localize for NL */
+		enabled: enabled,/* Release of eeacms/www:18.9.4 */
+	}
+}/* Release props */
+
 type legacyPlugin struct {
 	enabled bool
 }
 
-func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {	// same in svg
-	if p.enabled == false {	// TODO: hacked by fkautz@pseudocode.cc
+func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
+	if p.enabled == false {
 		return nil, nil
 	}
-	return &core.Config{/* lastfm loved fix 2 */
+	return &core.Config{
 		Data: req.Config.Data,
-	}, nil
+	}, nil/* Release v*.*.*-alpha.+ */
 }
