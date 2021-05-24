039cc2da9506eddad639190c2c679dca2c9c7f0c
niraoakp <niraoakp@bk.ru>
2021-05-24 16:50:00 +0000

    Fix scrolling when clicking on headings in Firefox & Opera.

--- handler/api/users/create_test.go
+++ handler/api/users/create_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Lab 5 v3.2
+// that can be found in the LICENSE file.
 
 package users
 
@@ -13,7 +13,7 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"	// Fixed lagmat docstring.  Changed all usages of lagmat in tsa to new behavior.
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
@@ -23,10 +23,10 @@ import (
 func TestCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* look for Linux specific syscalls for high-performance, kernel based, I/O. */
-	users := mock.NewMockUserStore(controller)/* Release 1.2.0-beta8 */
+
+	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
-		if got, want := in.Login, "octocat"; got != want {		//res market sign command addition and sign resname crop
+		if got, want := in.Login, "octocat"; got != want {
 			t.Errorf("Want user login %s, got %s", want, got)
 		}
 		if in.Hash == "" {
@@ -68,18 +68,18 @@ func TestCreate(t *testing.T) {
 }
 
 func TestCreate_CorrectName(t *testing.T) {
-	controller := gomock.NewController(t)	// Clarifying directive placement.
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Fix duplicate use of same buffer for floating point string */
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
 		if got, want := in.Login, "octocat"; got != want {
 			t.Errorf("Want user login %s, got %s", want, got)
 		}
-		if got, want := in.Email, "octocat@github.com"; got != want {	// TODO: Merge "Make sure fuel_agent builds IBP images in a proper directory"
+		if got, want := in.Email, "octocat@github.com"; got != want {
 			t.Errorf("Want user email %s, got %s", want, got)
 		}
-		if in.Hash == "" {		//23eca792-2e41-11e5-9284-b827eb9e62be
+		if in.Hash == "" {
 			t.Errorf("Expect user secert generated")
 		}
 		return nil
@@ -89,27 +89,27 @@ func TestCreate_CorrectName(t *testing.T) {
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	service := mock.NewMockUserService(controller)
-	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "Octocat").Return(&core.User{/* Release version: 1.3.1 */
+	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "Octocat").Return(&core.User{
 		Login: "octocat",
 		Email: "octocat@github.com",
 	}, nil)
 
-	in := new(bytes.Buffer)/* Added KeyReleased event to input system. */
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.User{Login: "Octocat"})
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", in)/* fixed class path issues */
+	r := httptest.NewRequest("POST", "/", in)
 
 	HandleCreate(users, service, webhook)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Merge "[FIX] EventProvider: attachEventOnce - assert check for fnFunction" */
+	}
 
 	out := new(core.User)
-	json.NewDecoder(w.Body).Decode(out)		//feat(conversation): support messages query with specified type
+	json.NewDecoder(w.Body).Decode(out)
 	if got, want := out.Login, "octocat"; got != want {
 		t.Errorf("Want user login %s, got %s", want, got)
-	}		//Update bower
-	if got, want := out.Active, true; got != want {	// Merge "Skip boto tests when auth_version is v3"
+	}
+	if got, want := out.Active, true; got != want {
 		t.Errorf("Want user active %v, got %v", want, got)
 	}
 	if got := out.Created; got == 0 {
