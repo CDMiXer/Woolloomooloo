3d8060fe1c701449a62f0fb5ceb41613b417ec16
niraoakp <niraoakp@bk.ru>
2021-05-24 03:22:18 +0000

    Work on Shephard exact ARMA likelihood.

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,12 +1,12 @@
-import pulumi
+import pulumi	// TODO: @abaril: Added a version output to log so we can determine which is running
 import pulumi_aws as aws
 import pulumi_pulumi as pulumi
-
+	// TODO: calling_interval_file
 provider = pulumi.providers.Aws("provider", region="us-west-2")
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
-    depends_on=[provider],		//Merge branch 'master' into kddimitrov/application-id-taken-from-packagejson
+    depends_on=[provider],
     protect=True,
-    ignore_changes=[/* added Privileges support. */
+    ignore_changes=[
         "bucket",
         "lifecycleRules[0]",
     ]))
