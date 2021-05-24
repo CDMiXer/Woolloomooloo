532e77778277333401612272d0d82a780e953b6b
niraoakp <niraoakp@bk.ru>
2021-05-24 10:03:01 +0000

    Added persistence to source status

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,41 +1,41 @@
 package wallet
-		//escape HTML characters
+
 import (
-	"github.com/filecoin-project/lotus/chain/types"/* Release for 18.33.0 */
+	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: hacked by magik6k@gmail.com
+
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
 }
 
 func NewMemKeyStore() *MemKeyStore {
-	return &MemKeyStore{/* Update guard to version 2.15.0 */
+	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
 	}
-}		//It was a testing problem all along.
+}
 
-// List lists all the keys stored in the KeyStore	// Update mariadb-deploy.yaml
+// List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
-	var out []string		//added security editor code
+	var out []string
 	for k := range mks.m {
-		out = append(out, k)/* Utils: removed obsolete Xperia related double-check */
+		out = append(out, k)
 	}
 	return out, nil
 }
-	// TODO: cleaning auto, auto turn correction SDB shift threshold.
+
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
-		return types.KeyInfo{}, types.ErrKeyInfoNotFound	// TODO: will be fixed by fkautz@pseudocode.cc
+		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
-	// TODO: will be fixed by witek@enjin.io
+
 	return ki, nil
 }
 
 // Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
-	mks.m[k] = ki	// TODO: command input page fixes
+	mks.m[k] = ki
 	return nil
 }
 
@@ -44,5 +44,5 @@ func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
 	return nil
 }
-/* Update CGTile.h */
-var _ (types.KeyStore) = (*MemKeyStore)(nil)/* take a stab at a TS test for proxy */
+
+var _ (types.KeyStore) = (*MemKeyStore)(nil)
