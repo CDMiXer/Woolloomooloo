462e46a5a458dbc00c58cc82da846f82a89d987a
niraoakp <niraoakp@bk.ru>
2021-05-24 05:26:11 +0000

    Merge "Fix checks for audio format."

--- core/perm.go
+++ core/perm.go
@@ -2,44 +2,44 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Update echo-api.md
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* minor fix on style fonts */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
-import "context"/* Update README Release History */
+import "context"
 
-type (/* Intento de tabulación en el archivo. */
-	// Perm represents an individuals repository	// TODO: hacked by xiemengjun@gmail.com
+type (
+	// Perm represents an individuals repository
 	// permission.
 	Perm struct {
 		UserID  int64  `db:"perm_user_id"  json:"-"`
 		RepoUID string `db:"perm_repo_uid" json:"-"`
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
-		Admin   bool   `db:"perm_admin"    json:"admin"`/* improving documentation for resolvers */
+		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"-"`
 		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
 	}
 
-	// Collaborator represents a project collaborator,		//Add retrieval of ingresses from k8s apiserver (#5)
+	// Collaborator represents a project collaborator,
 	// and provides the account and repository permissions
 	// details.
 	Collaborator struct {
 		UserID  int64  `db:"perm_user_id"  json:"user_id"`
 		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
 		Login   string `db:"user_login"    json:"login"`
-		Avatar  string `db:"user_avatar"   json:"avatar"`	// TODO: Don't abort shares scan if folder is non-existent
+		Avatar  string `db:"user_avatar"   json:"avatar"`
 		Read    bool   `db:"perm_read"     json:"read"`
-		Write   bool   `db:"perm_write"    json:"write"`/* Merge branch 'master' into add-gaurav */
+		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"synced"`
 		Created int64  `db:"perm_created"  json:"created"`
@@ -53,14 +53,14 @@ type (/* Intento de tabulación en el archivo. */
 		// datastore.
 		Find(ctx context.Context, repoUID string, userID int64) (*Perm, error)
 
-		// List returns a list of project members from the	// TODO: hacked by greg@colvin.org
+		// List returns a list of project members from the
 		// datastore.
-		List(ctx context.Context, repoUID string) ([]*Collaborator, error)	// Merge "[L10N] Update families files"
+		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
 
 		// Update persists an updated project member
-		// to the datastore.	// Merge branch 'master' into update-sofb
+		// to the datastore.
 		Update(context.Context, *Perm) error
-/* Release process, usage instructions */
+
 		// Delete deletes a project member from the
 		// datastore.
 		Delete(context.Context, *Perm) error
