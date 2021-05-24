57f9d50ee59c026ef7c15bdb0e23b1e4ca934349
niraoakp <niraoakp@bk.ru>
2021-05-24 15:18:20 +0000

    delete obsolete branch

--- grpc-go/examples/features/keepalive/client/main.go
+++ grpc-go/examples/features/keepalive/client/main.go
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* sbt 1.3.12 */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release Notes update for ZPH polish. pt2 */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Issue #208: extend Release interface. */
+ *
  */
 
 // Binary client is an example client.
@@ -34,8 +34,8 @@ import (
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
 var kacp = keepalive.ClientParameters{
-	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity	// TODO: + print num non zero elements from centroid
-	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead/* Merge branch 'develop' into bug/T187509 */
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
 	PermitWithoutStream: true,             // send pings even without active streams
 }
 
@@ -47,16 +47,16 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	// TODO: will be fixed by joshua@yottadb.com
+
 	c := pb.NewEchoClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
 	defer cancel()
 	fmt.Println("Performing unary request")
 	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})
-	if err != nil {		//Merge "Rename image_id to image in container object"
+	if err != nil {
 		log.Fatalf("unexpected error from UnaryEcho: %v", err)
-	}/* Game of Generals (release) */
+	}
 	fmt.Println("RPC response:", res)
 	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
 }
