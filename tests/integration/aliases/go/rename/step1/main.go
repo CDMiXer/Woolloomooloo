14a872bae1dc4b81f088191bb8831fabee23c392
niraoakp <niraoakp@bk.ru>
2021-05-24 05:12:14 +0000

    Avoid duplicate builds in PRs

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main/* maz kozitas */
+package main/* Add local state for folding items */
 
-import (	// TODO: will be fixed by xiemengjun@gmail.com
+import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
-/* Release notes 8.2.3 */
+)/* Release 0.93.450 */
+/* Changed project to generate XML documentation file on Release builds */
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
-}
-
-func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}/* Release 1.1.9 */
+}		//[library] Add missing attribute mapping from mfi to queue item
+/* Removed procedures and events from MultiModeLeg property sheet */
+func main() {/* Release version bump */
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Proper fix for swig support -- which was actually due to a bug with swig. */
+		fooComponent := &FooComponent{}
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
 	})
-}/* 577c790c-5216-11e5-b704-6c40088e03e4 */
+}
