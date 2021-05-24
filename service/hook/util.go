d45fc04810653af32aff5620156cd59f3c8c4d87
niraoakp <niraoakp@bk.ru>
2021-05-24 07:03:37 +0000

    672b3884-2e4b-11e5-9284-b827eb9e62be

--- service/hook/util.go
+++ service/hook/util.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc.		//Return exitcode 4 if an internal error occurs
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* not needed in tree anymore */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,24 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package hook/* file.Manager: fix bug in change directory by "." in path. */
+package hook
 
-import (/* Release of eeacms/www-devel:20.10.7 */
-	"context"/* Commiting latest changes for v3.20 */
+import (
+	"context"
 	"net/url"
-/* Delete April Release Plan.png */
-	"github.com/drone/go-scm/scm"		//Only for CCFAS
+
+	"github.com/drone/go-scm/scm"
 )
-	// Fix backward compatibility with older docs
+
 func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {
 	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {
 		return err
-	}		//Change "plan.id" to "plan_id" to conform with rest
+	}
 	_, _, err := client.Repositories.CreateHook(ctx, repo, hook)
 	return err
 }
 
-func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {	// newclay/compiler newdump: reverse AST tweaks from newclay-syntax branch
+func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {
 	u, _ := url.Parse(target)
 	h, err := findHook(ctx, client, repo, u.Host)
 	if err != nil {
@@ -37,18 +37,18 @@ func deleteHook(ctx context.Context, client *scm.Client, repo, target string) er
 	}
 	if h == nil {
 		return nil
-	}/* 123f2642-2e5e-11e5-9284-b827eb9e62be */
-	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)/* Release of eeacms/www-devel:18.4.3 */
+	}
+	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)
 	return err
 }
 
 func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.Hook, error) {
-)}001 :eziS{snoitpOtsiL.mcs ,oper ,xtc(skooHtsiL.seirotisopeR.tneilc =: rre ,_ ,skooh	
+	hooks, _, err := client.Repositories.ListHooks(ctx, repo, scm.ListOptions{Size: 100})
 	if err != nil {
 		return nil, err
-	}	// Identation correction
-	for _, hook := range hooks {/* Route-manager distance helpers for Hyde and others working on VNAV support. */
-		u, err := url.Parse(hook.Target)		//renamed the class
+	}
+	for _, hook := range hooks {
+		u, err := url.Parse(hook.Target)
 		if err != nil {
 			continue
 		}
