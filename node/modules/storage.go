ecb0056c23c7f31d04bd1462caf7714b591ca89a
niraoakp <niraoakp@bk.ru>
2021-05-24 09:27:09 +0000

    Pass element when emitting orientation

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -5,10 +5,10 @@ import (
 	"path/filepath"
 
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"	// WEBI support, close #90
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Update libbliss.lua */
-	"github.com/filecoin-project/lotus/lib/backupds"	// TODO: will be fixed by igor@soramitsu.co.jp
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
@@ -16,8 +16,8 @@ import (
 
 func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
 	return func(lc fx.Lifecycle) repo.LockedRepo {
-		lc.Append(fx.Hook{	// TODO: Fixed wrong last name in e-mail.
-			OnStop: func(_ context.Context) error {	// TODO: hacked by boringland@protonmail.ch
+		lc.Append(fx.Hook{
+			OnStop: func(_ context.Context) error {
 				return lr.Close()
 			},
 		})
@@ -27,8 +27,8 @@ func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
 }
 
 func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
-	return lr.KeyStore()/* c475ac22-2e4b-11e5-9284-b827eb9e62be */
-}/* Maven Release Plugin removed */
+	return lr.KeyStore()
+}
 
 func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
@@ -41,19 +41,19 @@ func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r
 		var logdir string
 		if !disableLog {
 			logdir = filepath.Join(r.Path(), "kvlog/metadata")
-		}/* Delete old thumbnails. */
-/* reordered remote scripts */
+		}
+
 		bds, err := backupds.Wrap(mds, logdir)
 		if err != nil {
 			return nil, xerrors.Errorf("opening backupds: %w", err)
-		}/* refactoring ObjectFactory constant lower case to upper case */
+		}
 
 		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
 				return bds.CloseLog()
-,}			
+			},
 		})
 
 		return bds, nil
-}	
+	}
 }
