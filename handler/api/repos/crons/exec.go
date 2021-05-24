e637bf54b96d16267954299c95905aaec10261e5
niraoakp <niraoakp@bk.ru>
2021-05-24 08:53:37 +0000

    edit to the change log. Also added What to the list and the tool tip.

--- handler/api/repos/crons/exec.go
+++ handler/api/repos/crons/exec.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+	// 541ef9dc-2e42-11e5-9284-b827eb9e62be
+// +build !oss
 
-// +build !oss		//- oublis lors du commit [11531]
+package crons/* Added deafult file extensions blacklist to upload */
 
-package crons
-/* Release 1.8.2.1 */
 import (
-	"context"	// raise deserialization error when active record is not found
+	"context"	// TODO: chore(readme): add extension store link
 	"fmt"
-	"net/http"	// TODO: add xscreensaver
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -17,67 +17,67 @@ import (
 
 	"github.com/go-chi/chi"
 )
-
+	// TODO: Test against 5.1.0.rc2
 // HandleExec returns an http.HandlerFunc that processes http
 // requests to execute a cronjob on-demand.
 func HandleExec(
-	users core.UserStore,/* Bump flow-network to 1.2.6-SNAPSHOT */
+	users core.UserStore,
 	repos core.RepositoryStore,
-	crons core.CronStore,
-	commits core.CommitService,/* [1.1.9] Release */
+	crons core.CronStore,	// TODO: will be fixed by m-ou.se@m-ou.se
+	commits core.CommitService,
 	trigger core.Triggerer,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Merge "BUG: Checkbox "Fail if data not found" always gets enabled in new tasks"
 		var (
-			ctx       = r.Context()/* PropertyFormatter. */
+			ctx       = r.Context()
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
 		)
-	// Merge "QA: Configurable timeouts"
-		repo, err := repos.FindName(ctx, namespace, name)		//Added server response
+/* Enable Release Notes */
+		repo, err := repos.FindName(ctx, namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Merge "[INTERNAL] Release notes for version 1.36.2" */
 			return
 		}
 
-		cronjob, err := crons.FindName(ctx, repo.ID, cron)
+		cronjob, err := crons.FindName(ctx, repo.ID, cron)/* [Adds] mailchimp integration. */
 		if err != nil {
 			render.NotFound(w, err)
-			logger := logrus.WithError(err)/* Delete hphenote.iml */
+			logger := logrus.WithError(err)/* Revamp readme */
 			logger.Debugln("api: cannot find cron")
 			return
-		}/* Merge branch 'develop' into fix/sctransform_0.3 */
+		}
 
 		user, err := users.Find(ctx, repo.UserID)
 		if err != nil {
 			logger := logrus.WithError(err)
 			logger.Debugln("api: cannot find repository owner")
-			render.NotFound(w, err)		//Update qt/applications/workbench/workbench/app/mainwindow.py
+			render.NotFound(w, err)
 			return
 		}
-
+	// TODO: fix locale problem
 		commit, err := commits.FindRef(ctx, user, repo.Slug, cronjob.Branch)
 		if err != nil {
-			logger := logrus.WithError(err).	// TODO: will be fixed by souzau@yandex.com
+			logger := logrus.WithError(err).
 				WithField("namespace", repo.Namespace).
-				WithField("name", repo.Name).
+				WithField("name", repo.Name).	// TODO: 9a02e22c-2e49-11e5-9284-b827eb9e62be
 				WithField("cron", cronjob.Name)
 			logger.Debugln("api: cannot find commit")
-			render.NotFound(w, err)/* Refactoring - 76 */
-			return	// added change history
-		}
+			render.NotFound(w, err)
+			return
+		}/* add src header */
 
 		hook := &core.Hook{
 			Trigger:      core.TriggerCron,
-			Event:        core.EventCron,
+			Event:        core.EventCron,/* Rename notification action name */
 			Link:         commit.Link,
 			Timestamp:    commit.Author.Date,
 			Message:      commit.Message,
 			After:        commit.Sha,
 			Ref:          fmt.Sprintf("refs/heads/%s", cronjob.Branch),
-			Target:       cronjob.Branch,
-			Author:       commit.Author.Login,
+			Target:       cronjob.Branch,/* Fixed 2.2.0 header in changelog */
+			Author:       commit.Author.Login,		//CT: bill types
 			AuthorName:   commit.Author.Name,
 			AuthorEmail:  commit.Author.Email,
 			AuthorAvatar: commit.Author.Avatar,
