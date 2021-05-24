aaae017eb70ee60c00d63927ad7460dd73aeff03
niraoakp <niraoakp@bk.ru>
2021-05-24 22:00:53 +0000

    Delete SoundingRockets.netkan

--- core/cancel.go
+++ core/cancel.go
@@ -2,20 +2,20 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// Fixed stack overflow error in MultiplMappedEnumsPropertyWidget
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release version 0.4.2 */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* d780c14e-2e5a-11e5-9284-b827eb9e62be */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: DateTimeField now accepts ‘onBlur’ and ‘name’ props
+// See the License for the specific language governing permissions and		//6d695c7a-2e56-11e5-9284-b827eb9e62be
 // limitations under the License.
 
 package core
 
 import "context"
-
+	// TODO: TaiwanChessBoard complete
 // Canceler cancels a build.
 type Canceler interface {
 	// Cancel cancels the provided build.
