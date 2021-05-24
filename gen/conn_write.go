44553780e1eeb16b6297d3621e6e67a7cf4f7ca3
niraoakp <niraoakp@bk.ru>
2021-05-24 13:16:42 +0000

    changed template colours to CAMH colours

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,14 +1,14 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* remove pch.hpp */
+
 // +build go1.8
-/* Added PageID to indexes in menuitems table. */
+
 package websocket
-/* Updated Releases_notes.txt */
+
 import "net"
 
-func (c *Conn) writeBufs(bufs ...[]byte) error {	// TODO: Move docker login in after_success to prevent early blockage
+func (c *Conn) writeBufs(bufs ...[]byte) error {
 	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
 	return err
