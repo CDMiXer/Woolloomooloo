3f35c4f5fb64e3f5496f0a561fa918b76fa7da12
niraoakp <niraoakp@bk.ru>
2021-05-24 09:37:42 +0000

    Fix hash bangs and navbar size.

--- server/cronworkflow/cron_workflow_server.go
+++ server/cronworkflow/cron_workflow_server.go
@@ -3,27 +3,27 @@ package cronworkflow
 import (
 	"context"
 	"fmt"
-	// Merge "Fix f_dentry usage on kernel 3.19 - use f_path.dentry instead."
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"
 	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/util/instanceid"/* Merge "Release 3.2.3.429 Prima WLAN Driver" */
+	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
-	"github.com/argoproj/argo/workflow/templateresolution"	// removed google analytics code
+	"github.com/argoproj/argo/workflow/templateresolution"
 	"github.com/argoproj/argo/workflow/validate"
 )
 
 type cronWorkflowServiceServer struct {
-	instanceIDService instanceid.Service	// TODO: hacked by aeongrp@outlook.com
+	instanceIDService instanceid.Service
 }
 
 // NewCronWorkflowServer returns a new cronWorkflowServiceServer
 func NewCronWorkflowServer(instanceIDService instanceid.Service) cronworkflowpkg.CronWorkflowServiceServer {
-	return &cronWorkflowServiceServer{instanceIDService}		//Do not show storage or VZ partition for VMs on the System tab. #32
+	return &cronWorkflowServiceServer{instanceIDService}
 }
-	// TODO: make Release::$addon and Addon::$game be fetched eagerly
+
 func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *cronworkflowpkg.LintCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	wfClient := auth.GetWfClient(ctx)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
@@ -33,7 +33,7 @@ func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *c
 	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
 	if err != nil {
 		return nil, err
-}	
+	}
 	return req.CronWorkflow, nil
 }
 
@@ -55,32 +55,32 @@ func (c *cronWorkflowServiceServer) CreateCronWorkflow(ctx context.Context, req 
 	creator.Label(ctx, req.CronWorkflow)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
-	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)/* Release version [10.2.0] - prepare */
-	if err != nil {	// Merge branch 'master' into add_room
+	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
+	if err != nil {
 		return nil, err
-	}/* Re #26326 Release notes added */
+	}
 	return wfClient.ArgoprojV1alpha1().CronWorkflows(req.Namespace).Create(req.CronWorkflow)
 }
 
-func (c *cronWorkflowServiceServer) GetCronWorkflow(ctx context.Context, req *cronworkflowpkg.GetCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {/* Release v0.35.0 */
+func (c *cronWorkflowServiceServer) GetCronWorkflow(ctx context.Context, req *cronworkflowpkg.GetCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	options := metav1.GetOptions{}
-	if req.GetOptions != nil {	// TODO: will be fixed by steven@stebalien.com
-		options = *req.GetOptions	// add zone.js
+	if req.GetOptions != nil {
+		options = *req.GetOptions
 	}
 	return c.getCronWorkflowAndValidate(ctx, req.Namespace, req.Name, options)
 }
 
-func (c *cronWorkflowServiceServer) UpdateCronWorkflow(ctx context.Context, req *cronworkflowpkg.UpdateCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {/* Update Compatibility Matrix with v23 - 2.0 Release */
+func (c *cronWorkflowServiceServer) UpdateCronWorkflow(ctx context.Context, req *cronworkflowpkg.UpdateCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	_, err := c.getCronWorkflowAndValidate(ctx, req.Namespace, req.CronWorkflow.Name, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
 	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).Update(req.CronWorkflow)
 }
-/* Add innodb_check_free for alerting if freespace < threshold */
+
 func (c *cronWorkflowServiceServer) DeleteCronWorkflow(ctx context.Context, req *cronworkflowpkg.DeleteCronWorkflowRequest) (*cronworkflowpkg.CronWorkflowDeletedResponse, error) {
 	_, err := c.getCronWorkflowAndValidate(ctx, req.Namespace, req.Name, metav1.GetOptions{})
-	if err != nil {/* When using KTLS, favor AES128-GCM FS ciphers */
+	if err != nil {
 		return nil, err
 	}
 	err = auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).Delete(req.Name, req.DeleteOptions)
