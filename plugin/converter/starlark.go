60ed77656cf9a6c363e89b4a45d9072f1d905079
niraoakp <niraoakp@bk.ru>
2021-05-24 22:37:53 +0000

    Make the two images on the homepage clickable hyperlinks.

--- plugin/converter/starlark.go
+++ plugin/converter/starlark.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//93fcc30c-2e71-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// Fix selector for setRows and setPlugins
 package converter
 
-import (
+import (	// TODO: hacked by alex.gaynor@gmail.com
 	"bytes"
-	"context"
+	"context"/* Add links to markdown versions of man pages */
 	"strings"
 
 	"github.com/drone/drone/core"
@@ -18,32 +18,32 @@ import (
 // starlark file to a yaml file.
 func Starlark(enabled bool) core.ConvertService {
 	return &starlarkPlugin{
-		enabled: enabled,
+		enabled: enabled,	// TODO: Test PHP7.1 but allow failures
 	}
 }
 
 type starlarkPlugin struct {
 	enabled bool
 }
-
+/* Add issue #18 to the TODO Release_v0.1.2.txt. */
 func (p *starlarkPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
-		return nil, nil
+		return nil, nil/* Delete java.json */
 	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
-	switch {
+	switch {	// TODO: Code and comments refactor
 	case strings.HasSuffix(req.Repo.Config, ".script"):
 	case strings.HasSuffix(req.Repo.Config, ".star"):
 	case strings.HasSuffix(req.Repo.Config, ".starlark"):
 	default:
-		return nil, nil
+		return nil, nil		//Fix renovate.json
 	}
 
 	// convert the starlark file to yaml
 	buf := new(bytes.Buffer)
-
+/* Delete Gallop.podspec */
 	return &core.Config{
 		Data: buf.String(),
 	}, nil
