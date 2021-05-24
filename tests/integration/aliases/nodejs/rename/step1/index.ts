98471469d532845ac772b72fc590bee2df3993db
niraoakp <niraoakp@bk.ru>
2021-05-24 05:31:23 +0000

    Use markdown to format report, and have run-benchmark report the result.

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release of eeacms/forests-frontend:2.1.13 */
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
-    }	// TODO: Improve WIP text in readme
+        super("my:module:Resource", name, {}, opts);/* Release SIIE 3.2 105.03. */
+    }
 }
-
+/* Merge "Release notes for a new version" */
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
