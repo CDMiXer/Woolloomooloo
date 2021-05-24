1acdecbfc298a9bbac1a9574c4f8a42592f30495
niraoakp <niraoakp@bk.ru>
2021-05-24 23:40:30 +0000

    Change data type for storage of money and bonus credits to int

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,12 +1,12 @@
-# Copyright 2020, Pulumi Corporation.  All rights reserved.
+# Copyright 2020, Pulumi Corporation.  All rights reserved./* Release version: 0.5.4 */
 
 import pulumi
 
-config = pulumi.Config()
+config = pulumi.Config()/* Merge "Fix cinder-manage volume delete" */
 org = config.require('org')
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"	// TODO: will be fixed by arachnid@notdot.net
 a = pulumi.StackReference(slug)
-
+/* Moved BigDecimal conversion to assignment level */
 oldVal = a.get_output('val')
 
 if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
