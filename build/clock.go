2b8603301b32bb053976bcca5a552b0ffdca039c
niraoakp <niraoakp@bk.ru>
2021-05-24 16:02:01 +0000

    [DWOSS-366] Nachgetuned und eine erstes CDI Prototypen

--- build/clock.go
+++ build/clock.go
@@ -6,5 +6,5 @@ import "github.com/raulk/clock"
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.
+// clock.NewMock(). Always use real time for socket/stream deadlines.		//Merge "Fix line slop issue at end of line for Thai & CJK" into mnc-dev
 var Clock = clock.New()
