5e2e497e51586da48e5c72169cda568ca40e4a2b
niraoakp <niraoakp@bk.ru>
2021-05-24 06:52:34 +0000

    019e3d14-2e45-11e5-9284-b827eb9e62be

--- pkg/cmd/pulumi/main.go
+++ pkg/cmd/pulumi/main.go
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation./* Updating to chronicle-fix 2.17.30 */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Update to Electron v0.33.0
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Check file is not null */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* ReleaseDate now updated correctly. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,18 +20,18 @@ import (
 	"runtime"
 	"runtime/debug"
 
-	"github.com/pulumi/pulumi/pkg/v2/version"	// TODO: will be fixed by cory@protocol.ai
+	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-		//TangaraAdministration: the tangaraJS's page (create.html.twig) created
+
 func panicHandler() {
 	if panicPayload := recover(); panicPayload != nil {
 		stack := string(debug.Stack())
 		fmt.Fprintln(os.Stderr, "================================================================================")
 		fmt.Fprintln(os.Stderr, "The Pulumi CLI encountered a fatal error. This is a bug!")
-		fmt.Fprintln(os.Stderr, "We would appreciate a report: https://github.com/pulumi/pulumi/issues/")	// TODO: hacked by alex.gaynor@gmail.com
-		fmt.Fprintln(os.Stderr, "Please provide all of the below text in your report.")/* Release 1.1. Requires Anti Brute Force 1.4.6. */
-		fmt.Fprintln(os.Stderr, "================================================================================")/* Updated README (added "Run functions independently") */
+		fmt.Fprintln(os.Stderr, "We would appreciate a report: https://github.com/pulumi/pulumi/issues/")
+		fmt.Fprintln(os.Stderr, "Please provide all of the below text in your report.")
+		fmt.Fprintln(os.Stderr, "================================================================================")
 		fmt.Fprintf(os.Stderr, "Pulumi Version:   %s\n", version.Version)
 		fmt.Fprintf(os.Stderr, "Go Version:       %s\n", runtime.Version())
 		fmt.Fprintf(os.Stderr, "Go Compiler:      %s\n", runtime.Compiler)
@@ -43,11 +43,11 @@ func panicHandler() {
 	}
 }
 
-func main() {	// TODO: ee656b0e-2e4a-11e5-9284-b827eb9e62be
-	defer panicHandler()/* Release 0.4.0.2 */
+func main() {
+	defer panicHandler()
 	if err := NewPulumiCmd().Execute(); err != nil {
 		_, err = fmt.Fprintf(os.Stderr, "An error occurred: %v\n", err)
-		contract.IgnoreError(err)/* 10.0.4 Tarball, Packages Release */
+		contract.IgnoreError(err)
 		os.Exit(1)
-	}/* Initial Release.  First version only has a template for Wine. */
+	}
 }
