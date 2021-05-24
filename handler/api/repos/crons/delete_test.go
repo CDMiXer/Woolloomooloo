5a078e51cd94ad78dfbdf7dda0c0dd0dca699fcd
niraoakp <niraoakp@bk.ru>
2021-05-24 07:53:53 +0000

    Create sw-ifr.html

--- handler/api/repos/crons/delete_test.go
+++ handler/api/repos/crons/delete_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* bb942f88-2e47-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -7,21 +7,21 @@
 package crons
 
 import (
-	"context"/* Release v6.3.1 */
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Update Additionally.md */
-/* Released v2.1-alpha-2 of rpm-maven-plugin. */
+	"testing"
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Add tests for addType and RemoveType for ComponentDefinitions */
+)
 
-func TestHandleDelete(t *testing.T) {	// TODO: will be fixed by sjors@sprovoost.nl
+func TestHandleDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -31,21 +31,21 @@ func TestHandleDelete(t *testing.T) {	// TODO: will be fixed by sjors@sprovoost.
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 	crons.EXPECT().Delete(gomock.Any(), dummyCron).Return(nil)
-	// TODO: Bump Frida to support Android 11 ART tracing
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
-		//removed extra `
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(	// TODO: Merge branch 'master' into k8s_upgrade
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(repos, crons).ServeHTTP(w, r)/* add PropertiesProvider tests */
+	HandleDelete(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//Set multiple icon images on frames
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
 
@@ -58,21 +58,21 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// fixed linear equation being cut off
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Release notes! */
+	)
 
 	HandleDelete(repos, nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {/* Released gem 2.1.3 */
+	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// TODO: hacked by witek@enjin.io
-	got, want := new(errors.Error), errors.ErrNotFound	// TODO: will be fixed by caojiaoyue@protonmail.com
+
+	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
