7837ea07a5c4f9cdd1a52b6ac8ec37a37384648c
niraoakp <niraoakp@bk.ru>
2021-05-24 02:38:56 +0000

    Update RunHPTopup.m

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -4,22 +4,22 @@ CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,cron_repo_id     INTEGER
 ,cron_name        TEXT
-,cron_expr        TEXT	// TODO: hacked by boringland@protonmail.ch
+,cron_expr        TEXT
 ,cron_next        INTEGER
-,cron_prev        INTEGER/* TextCommit */
+,cron_prev        INTEGER
 ,cron_event       TEXT
 ,cron_branch      TEXT
 ,cron_target      TEXT
-,cron_disabled    BOOLEAN		//8f882db6-2e44-11e5-9284-b827eb9e62be
+,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
-,UNIQUE(cron_repo_id, cron_name)/* ProcList Polish help */
+,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-cron-repo		//Create 08_05_DataGridImport
-/* Added load_file function */
+-- name: create-index-cron-repo
+
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
