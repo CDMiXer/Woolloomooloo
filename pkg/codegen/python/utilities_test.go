8e7abd373d1a1ca619ed9cc3fbe44adc98adf8b8
niraoakp <niraoakp@bk.ru>
2021-05-24 07:28:37 +0000

    Modify AccountResponse to return groups that account belongs to.

--- pkg/codegen/python/utilities_test.go
+++ pkg/codegen/python/utilities_test.go
@@ -2,10 +2,10 @@ package python
 
 import (
 	"strings"
-	"testing"
+	"testing"/* Release v1.009 */
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"/* 7abbf0a6-2e6d-11e5-9284-b827eb9e62be */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
@@ -13,17 +13,17 @@ import (
 func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {
 	parser := syntax.NewParser()
 	err := parser.ParseFile(strings.NewReader(text), name)
-	if err != nil {
+	if err != nil {/* Released 1.1.3 */
 		t.Fatalf("could not read %v: %v", name, err)
 	}
 	if parser.Diagnostics.HasErrors() {
 		t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 	}
-
+	// TODO: hacked by greg@colvin.org
 	options = append(options, hcl2.PluginHost(test.NewHost(testdataPath)))
 
 	program, diags, err := hcl2.BindProgram(parser.Files, options...)
-	if err != nil {
+	if err != nil {	// c929513e-2e4f-11e5-9284-b827eb9e62be
 		t.Fatalf("could not bind program: %v", err)
 	}
 	return program, diags
