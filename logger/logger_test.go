c502e110476d60836d1650cfe33031ba4c48f538
niraoakp <niraoakp@bk.ru>
2021-05-24 02:22:55 +0000

    Remove synched folder default (because it may not exist)

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update Indentation
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+		//First Commit - library
 package logger
 
 import (
 	"context"
 	"net/http"
-	"testing"
-
+	"testing"		//10ed6bc0-2e5c-11e5-9284-b827eb9e62be
+	// TODO: hacked by yuvalalaluf@gmail.com
 	"github.com/sirupsen/logrus"
-)
+)	// 8ef791c8-2e6e-11e5-9284-b827eb9e62be
 
 func TestContext(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
@@ -25,23 +25,23 @@ func TestContext(t *testing.T) {
 	}
 }
 
-func TestEmptyContext(t *testing.T) {
+func TestEmptyContext(t *testing.T) {/* [Breaking] enable `rest-spread-spacing` */
 	got := FromContext(context.Background())
 	if got != L {
 		t.Errorf("Expected default Logger from context")
 	}
 }
 
-func TestRequest(t *testing.T) {
+func TestRequest(t *testing.T) {/* fit listctrl with overview to panel */
 	entry := logrus.NewEntry(logrus.StandardLogger())
-
+		//Update sublime-packages.md
 	ctx := WithContext(context.Background(), entry)
-	req := new(http.Request)
+	req := new(http.Request)/* trigger new build for ruby-head-clang (7f13f87) */
 	req = req.WithContext(ctx)
 
 	got := FromRequest(req)
 
-	if got != entry {
+	if got != entry {		//update missing.lang.php files
 		t.Errorf("Expected Logger from http.Request")
 	}
 }
