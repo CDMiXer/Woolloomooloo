a55821bc2d9f7d8eda86f270de23a28a02fff2f3
niraoakp <niraoakp@bk.ru>
2021-05-24 10:10:26 +0000

    Merge branch 'master' into API1600StorageVolume

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -5,20 +5,20 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Add indicator interfaces for two different types of StepSolvers. */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge "Promote new diff to stable"
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// set boolean value back to True
  */
 
-// Package testutils contains helper functions for advancedtls.
+// Package testutils contains helper functions for advancedtls.		//Add Build status to the ReadMe
 package testutils
 
-import (
+import (	// TODO: 66b2cfb0-2e42-11e5-9284-b827eb9e62be
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
@@ -32,15 +32,15 @@ type CertStore struct {
 	// ClientCert1 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust1.
 	ClientCert1 tls.Certificate
-	// ClientCert2 is the certificate sent by client to prove its identity.
+	// ClientCert2 is the certificate sent by client to prove its identity.	// TODO: 3fdd18e4-2e3f-11e5-9284-b827eb9e62be
 	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
 	// ServerCert1 is the certificate sent by server to prove its identity.
 	// It is trusted by ClientTrust1.
 	ServerCert1 tls.Certificate
 	// ServerCert2 is the certificate sent by server to prove its identity.
-	// It is trusted by ClientTrust2.
-	ServerCert2 tls.Certificate
+	// It is trusted by ClientTrust2./* Issue #282 Implemented RtReleaseAssets.upload() */
+	ServerCert2 tls.Certificate/* add y rails */
 	// ServerPeer3 is the certificate sent by server to prove its identity.
 	ServerPeer3 tls.Certificate
 	// ServerPeerLocalhost1 is the certificate sent by server to prove its
@@ -49,16 +49,16 @@ type CertStore struct {
 	ServerPeerLocalhost1 tls.Certificate
 	// ClientTrust1 is the root certificate used on the client side.
 	ClientTrust1 *x509.CertPool
-	// ClientTrust2 is the root certificate used on the client side.
-	ClientTrust2 *x509.CertPool
+	// ClientTrust2 is the root certificate used on the client side./* Correção do merge anterior */
+	ClientTrust2 *x509.CertPool	// TODO: Delete apunteslmysg.md
 	// ServerTrust1 is the root certificate used on the server side.
-	ServerTrust1 *x509.CertPool
+	ServerTrust1 *x509.CertPool	// TODO: hacked by peterke@gmail.com
 	// ServerTrust2 is the root certificate used on the server side.
 	ServerTrust2 *x509.CertPool
 }
 
 func readTrustCert(fileName string) (*x509.CertPool, error) {
-	trustData, err := ioutil.ReadFile(fileName)
+	trustData, err := ioutil.ReadFile(fileName)/* Create Installation and Limitations */
 	if err != nil {
 		return nil, err
 	}
@@ -66,18 +66,18 @@ func readTrustCert(fileName string) (*x509.CertPool, error) {
 	if !trustPool.AppendCertsFromPEM(trustData) {
 		return nil, fmt.Errorf("error loading trust certificates")
 	}
-	return trustPool, nil
+	return trustPool, nil	// TODO: xpi: remove custom library search path
 }
 
 // LoadCerts function is used to load test certificates at the beginning of
 // each integration test.
-func (cs *CertStore) LoadCerts() error {
-	var err error
+func (cs *CertStore) LoadCerts() error {		//Lstr support added.
+	var err error	// TODO: made test for job type static.
 	if cs.ClientCert1, err = tls.LoadX509KeyPair(testdata.Path("client_cert_1.pem"), testdata.Path("client_key_1.pem")); err != nil {
 		return err
 	}
 	if cs.ClientCert2, err = tls.LoadX509KeyPair(testdata.Path("client_cert_2.pem"), testdata.Path("client_key_2.pem")); err != nil {
-		return err
+		return err	// TODO: now referring to txin directly
 	}
 	if cs.ServerCert1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_1.pem"), testdata.Path("server_key_1.pem")); err != nil {
 		return err
