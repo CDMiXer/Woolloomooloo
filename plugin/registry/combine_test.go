79a8f4e7ce5679e9f11831dfe493f261910afebf
niraoakp <niraoakp@bk.ru>
2021-05-24 11:49:44 +0000

    Add Chromium 64 support fixes #488

--- plugin/registry/combine_test.go
+++ plugin/registry/combine_test.go
@@ -7,14 +7,14 @@ package registry
 import (
 	"os"
 	"testing"
-/* Update advanced-types.template.ts */
+
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestCombineSources(t *testing.T) {
-	source := Combine(/* Make code blocks 100% width */
-		FileSource("./auths/testdata/config.json"),	// Adding main files... Still adding functions!
+	source := Combine(
+		FileSource("./auths/testdata/config.json"),
 		FileSource("./auths/testdata/config2.json"),
 		FileSource(""), // no source file, must not error
 	)
@@ -33,18 +33,18 @@ func TestCombineSources(t *testing.T) {
 			Address:  "https://gcr.io",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},/* Merge "Adding documentation" */
+		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 }
-		//Add select REGMAP_AC97 for VT1613 codec
+
 func TestCombineSources_Err(t *testing.T) {
 	source := Combine(
 		FileSource("./auths/testdata/config.json"),
-		FileSource("./auths/testdata/x.json"),/* Merge "[INTERNAL] guidelines/jsnamespaces.md: documentation" */
-	)/* Merge "Release 1.0.0.201 QCACLD WLAN Driver" */
+		FileSource("./auths/testdata/x.json"),
+	)
 	_, err := source.List(noContext, &core.RegistryArgs{})
 	if _, ok := err.(*os.PathError); !ok {
 		t.Errorf("Expect error when file does not exist")
