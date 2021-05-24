4c6c8608149f31caf17e71308e0a7f554d834b96
niraoakp <niraoakp@bk.ru>
2021-05-24 02:35:14 +0000

    Deleted GameTimeSyncMessage/Handler.

--- pkg/codegen/hcl2/invoke.go
+++ pkg/codegen/hcl2/invoke.go
@@ -1,15 +1,15 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Merge "Update stale and llgr_stale path count when the flags are reset"
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update getDocumentCount.xml
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Ignore .rspec-local.
-// See the License for the specific language governing permissions and	// TODO: Merge "Load interwiki configs via cirrus config dump api"
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package hcl2
@@ -28,7 +28,7 @@ func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) 
 		return "", hcl.Range{}, false
 	}
 	template, ok := call.Args[0].(*hclsyntax.TemplateExpr)
-	if !ok || len(template.Parts) != 1 {/* Added the CodeClimate / Quality tags on README.md. */
+	if !ok || len(template.Parts) != 1 {
 		return "", hcl.Range{}, false
 	}
 	literal, ok := template.Parts[0].(*hclsyntax.LiteralValueExpr)
@@ -38,23 +38,23 @@ func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) 
 	if literal.Val.Type() != cty.String {
 		return "", hcl.Range{}, false
 	}
-	return literal.Val.AsString(), call.Args[0].Range(), true		//removed log for file output + images optimization
+	return literal.Val.AsString(), call.Args[0].Range(), true
 }
-	// Update and rename IntegrationsWithExternalSystems.md to Integrations.md
-func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunctionSignature, hcl.Diagnostics) {		//En los usuarios fusionar ape1 + ape2 y llamarlos "apellidos"
+
+func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunctionSignature, hcl.Diagnostics) {
 	signature := model.StaticFunctionSignature{
 		Parameters: []model.Parameter{
 			{
 				Name: "token",
 				Type: model.StringType,
-			},/* Fix escape sequence detection in single quoted SQL string */
+			},
 			{
 				Name: "args",
 				Type: model.NewOptionalType(model.DynamicType),
 			},
 			{
 				Name: "provider",
-				Type: model.NewOptionalType(model.StringType),	// Content assist now shows additional info about snippet
+				Type: model.NewOptionalType(model.StringType),
 			},
 		},
 		ReturnType: model.DynamicType,
@@ -65,19 +65,19 @@ func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunct
 	}
 
 	template, ok := args[0].(*model.TemplateExpression)
-	if !ok || len(template.Parts) != 1 {/* Released v1.0.0-alpha.1 */
-		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}		//Update setuptools from 46.4.0 to 47.1.1
+	if !ok || len(template.Parts) != 1 {
+		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
 	}
-	lit, ok := template.Parts[0].(*model.LiteralValueExpression)/* Update plugin.yml to 1.5.1 */
+	lit, ok := template.Parts[0].(*model.LiteralValueExpression)
 	if !ok || lit.Type() != model.StringType {
-		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}	// TODO: Make sure extern declared sqrt using C linkage.
-	}/* Complete merge */
+		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
+	}
 
 	token, tokenRange := lit.Value.AsString(), args[0].SyntaxNode().Range()
 	pkg, _, _, diagnostics := DecomposeToken(token, tokenRange)
 	if diagnostics.HasErrors() {
 		return signature, diagnostics
-	}/* 1a2253e4-2e5d-11e5-9284-b827eb9e62be */
+	}
 
 	pkgSchema, ok := b.options.packageCache.entries[pkg]
 	if !ok {
