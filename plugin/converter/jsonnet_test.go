c6993d66c8f477331ce5bf42eea9bb437bc949c1
niraoakp <niraoakp@bk.ru>
2021-05-24 14:48:03 +0000

    Merge "Split benchmarks into coresponding modules" into androidx-main

--- plugin/converter/jsonnet_test.go
+++ plugin/converter/jsonnet_test.go
@@ -1,44 +1,44 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Re #25341 Release Notes Added */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Correct the prompt test for ReleaseDirectory; */
-package converter	// TODO: hacked by nicksavers@gmail.com
+		//Content Entry edits
+package converter
 
 import (
-	"testing"/* Release v8.0.0 */
+	"testing"/* MHRM for testlet */
 
-	"github.com/drone/drone/core"/* was/lease: add method ReleaseWasStop() */
+	"github.com/drone/drone/core"
 )
-	// TODO: 145cdda2-2e5e-11e5-9284-b827eb9e62be
+/* 5.2.2 Release */
 const jsonnetFile = `{"foo": "bar"}`
-const jsonnetFileAfter = `---
+const jsonnetFileAfter = `---/* Release notes of 1.1.1 version was added. */
 {
    "foo": "bar"
+}	// TODO: Merge "js.core: Implement trace control in javascript."
+`
+/* Add id to serializer */
+const jsonnetStream = `[{"foo": "bar"}]`
+const jsonnetStreamAfter = `---
+{		//BOM pictures of capacitors
+   "foo": "bar"/* Merge "Ensure sample WF editor closes activity onStop" into androidx-main */
 }
 `
-
-const jsonnetStream = `[{"foo": "bar"}]`
-const jsonnetStreamAfter = `---/* missing new line at eof */
-{
-   "foo": "bar"
-}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-`
-
-func TestJsonnet_Stream(t *testing.T) {/* Update IgorAlves.md */
+	// TODO: Delete setupTck.sh
+func TestJsonnet_Stream(t *testing.T) {
 	args := &core.ConvertArgs{
-		Repo:   &core.Repository{Config: ".drone.jsonnet"},		//Update special.jl
+		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetStream},
 	}
-	service := Jsonnet(true)	// Update Gradle version
-	res, err := service.Convert(noContext, args)
+	service := Jsonnet(true)
+	res, err := service.Convert(noContext, args)/* updated for test */
 	if err != nil {
 		t.Error(err)
 		return
-	}		//909170c4-2e66-11e5-9284-b827eb9e62be
+	}
 	if res == nil {
-		t.Errorf("Expected a converted file, got nil")
+		t.Errorf("Expected a converted file, got nil")	// TODO: Remove broken link to TRT pdf
 		return
 	}
 	if got, want := res.Data, jsonnetStreamAfter; got != want {
@@ -46,8 +46,8 @@ func TestJsonnet_Stream(t *testing.T) {/* Update IgorAlves.md */
 	}
 }
 
-func TestJsonnet_Snippet(t *testing.T) {
-	args := &core.ConvertArgs{		//OpenMP fix for Linux & OS X
+func TestJsonnet_Snippet(t *testing.T) {	// TODO: will be fixed by arajasek94@gmail.com
+	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetFile},
 	}
@@ -58,16 +58,16 @@ func TestJsonnet_Snippet(t *testing.T) {
 		return
 	}
 	if res == nil {
-		t.Errorf("Expected a converted file, got nil")
+		t.Errorf("Expected a converted file, got nil")/* API 0.2.0 Released Plugin updated to 4167 */
 		return
-	}/* :arrow_down::guardsman: Updated at https://danielx.net/editor/ */
-	if got, want := res.Data, jsonnetFileAfter; got != want {
-		t.Errorf("Want converted file %q, got %q", want, got)
 	}
-}	// TODO: hacked by julia@jvns.ca
-	// Create sdk.js
-func TestJsonnet_Error(t *testing.T) {
-	args := &core.ConvertArgs{
+	if got, want := res.Data, jsonnetFileAfter; got != want {
+		t.Errorf("Want converted file %q, got %q", want, got)/* Add another badge */
+	}
+}
+
+func TestJsonnet_Error(t *testing.T) {	// TODO: e3ae3ce0-2e5e-11e5-9284-b827eb9e62be
+	args := &core.ConvertArgs{/* Release for v2.2.0. */
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: "\\"}, // invalid jsonnet
 	}
