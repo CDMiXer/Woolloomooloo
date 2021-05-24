bc93da8c6d5d74bb36ed0543f2f8725993cb9759
niraoakp <niraoakp@bk.ru>
2021-05-24 19:52:24 +0000

    Merge "msm: clock-8x60: Add 27MHz rate gsbi_qup clocks" into msm-2.6.35

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -6,8 +6,8 @@ package ints
 import (
 	"path/filepath"
 	"testing"
-
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	// TODO: will be fixed by caojiaoyue@protonmail.com
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//Merge branch 'master' into bug/open-enrollments-no-service-167206215
 )
 
 var dirs = []string{
@@ -17,14 +17,14 @@ var dirs = []string{
 	"retype_component",
 	"rename_component",
 }
-
+/* b59e1622-2e63-11e5-9284-b827eb9e62be */
 func TestGoAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("go", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),
-				Dependencies: []string{
+				Dir: filepath.Join(d, "step1"),/* nunaliit2-js: Fixes to GridCanvas */
+				Dependencies: []string{/* Merge branch 'hotfix/3.0.1' into develop */
 					"github.com/pulumi/pulumi/sdk/v2",
 				},
 				Quick: true,
@@ -34,8 +34,8 @@ func TestGoAliases(t *testing.T) {
 						ExpectNoChanges: true,
 						Additive:        true,
 					},
-				},
+				},/* Alpha Release Nº1. */
 			})
 		})
 	}
-}
+}	// TODO: Deleted unneeded files
