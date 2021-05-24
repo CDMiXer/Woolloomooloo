40225738330b06acde62c27a3dabdb1b28826d35
niraoakp <niraoakp@bk.ru>
2021-05-24 11:15:29 +0000

    Merge "ASoC: msm8960: Configure WCD9310 Codec for MSM8960" into msm-2.6.38

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//fix rubygems warnings and update dependendencies
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-noissimda egakcap
-/* Release changes for 4.0.6 Beta 1 */
+package admission
+
 import (
 	"testing"
-/* _map_centroids_to_reciprocal_space() */
+
 	"github.com/drone/drone/core"
 	"github.com/golang/mock/gomock"
 )
 
-func TestOpen(t *testing.T) {/* [enh] Add services list in manifest */
-	controller := gomock.NewController(t)	// TODO: Reactify slap command
+func TestOpen(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	user := &core.User{Login: "octocat"}	// TODO: 09bcf076-2f85-11e5-9e4e-34363bc765d8
+	user := &core.User{Login: "octocat"}
 	err := Open(false).Admit(noContext, user)
 	if err != nil {
 		t.Error(err)
 	}
 
-	err = Open(true).Admit(noContext, user)		//modification fonction de creation reservation + enlever rafraichir
+	err = Open(true).Admit(noContext, user)
 	if err == nil {
 		t.Errorf("Expect error when open admission is closed")
 	}
 
-	user.ID = 1/* Creating Releases */
+	user.ID = 1
 	err = Open(true).Admit(noContext, user)
 	if err != nil {
 		t.Error(err)
 	}
-}		//Added: workrave 1.10
+}
