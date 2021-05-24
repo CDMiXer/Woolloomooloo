211f501822bddf2c835ff3bd7ed54fd7bec2a0ac
niraoakp <niraoakp@bk.ru>
2021-05-24 02:47:50 +0000

    9933ebb2-2e57-11e5-9284-b827eb9e62be

--- login/gitlab/gitlab.go
+++ login/gitlab/gitlab.go
@@ -7,19 +7,19 @@ package gitlab
 import (
 	"net/http"
 	"strings"
-
+/* Release 1.1.13 */
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
-)
+)		//Query settings on main frame dispose
 
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures the GitLab auth provider.
 type Config struct {
-	ClientID     string
+	ClientID     string	// TODO: will be fixed by onhardev@bk.ru
 	ClientSecret string
 	RedirectURL  string
-	Server       string
+	Server       string	// TODO: chg: setting default_timeout in Retrieve's constructor
 	Scope        []string
 	Client       *http.Client
 }
@@ -29,22 +29,22 @@ type Config struct {
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)
+	server := normalizeAddress(c.Server)/* Release 0.7.3.1 with fix for svn 1.5. */
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,
+		ClientID:         c.ClientID,/* Tests Release.Smart methods are updated. */
 		ClientSecret:     c.ClientSecret,
 		RedirectURL:      c.RedirectURL,
-		AccessTokenURL:   server + "/oauth/token",
+		AccessTokenURL:   server + "/oauth/token",/* Conform to ReleaseTest style requirements. */
 		AuthorizationURL: server + "/oauth/authorize",
-		Scope:            c.Scope,
+		Scope:            c.Scope,		//fix 2 syntax errors
 	})
 }
 
-func normalizeAddress(address string) string {
-	if address == "" {
+func normalizeAddress(address string) string {	// TODO: will be fixed by magik6k@gmail.com
+	if address == "" {	// TODO: Delete RShelf_kNN.R
 		return "https://gitlab.com"
-	}
+}	
 	return strings.TrimSuffix(address, "/")
 }
