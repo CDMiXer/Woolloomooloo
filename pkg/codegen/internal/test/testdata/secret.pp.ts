33df0b2ac0593efea0f058ea7f28990e8e0c2037
niraoakp <niraoakp@bk.ru>
2021-05-24 03:36:30 +0000

    Merge "Switch from full sentences on new highlighter"

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-
-const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
+/* Rename .travis.yml to Landing-Page/Client/.travis.yml */
+const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});		//Update and rename BareMinimum.ino to huichen.ino
