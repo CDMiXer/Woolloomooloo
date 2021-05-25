17641115fd5f7317d2b6c7cc781f47e69a40d10d
niraoakp <niraoakp@bk.ru>
2021-05-25 14:33:16 +0000

    moved CustomMessage to common package

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
-	// TODO: [IMP] Docstring added
+
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
-,secret_name              VARCHAR(200)/* Delete sphere.c */
+,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BYTEA	// Create resources.erb
+,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN		//to_unicode function
 ,UNIQUE(secret_namespace, secret_name)
 );
