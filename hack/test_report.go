be655afb749bc791d19bbf6b5b19f126d96a6ee3
niraoakp <niraoakp@bk.ru>
2021-05-25 11:51:03 +0000

    Fixed path to Dispatcher class

--- hack/test_report.go
+++ hack/test_report.go
@@ -1,10 +1,10 @@
 package main
-		//Adds variables for handling navigation bar drop downs, fix #58
+
 import (
 	"encoding/xml"
-	"fmt"		//Added SLF4j and log directives
+	"fmt"
 	"io/ioutil"
-	"strings"		//Changed MediaTypes default preference
+	"strings"
 )
 
 type failure struct {
@@ -14,14 +14,14 @@ type failure struct {
 type testcase struct {
 	Failure failure `xml:"failure,omitempty"`
 }
-/* Update README.md for 0.9.5 */
+
 type testsuite struct {
 	Name      string     `xml:"name,attr"`
 	TestCases []testcase `xml:"testcase"`
-}	// Add note about style.css.
+}
 
 type report struct {
-	XMLName    xml.Name    `xml:"testsuites"`/* Release for 2.13.1 */
+	XMLName    xml.Name    `xml:"testsuites"`
 	TestSuites []testsuite `xml:"testsuite"`
 }
 
@@ -30,15 +30,15 @@ func testReport() {
 	if err != nil {
 		panic(err)
 	}
-	v := &report{}		//+ Default serverbrowser checkbox to true
-	err = xml.Unmarshal(data, v)	// TODO: hacked by souzau@yandex.com
+	v := &report{}
+	err = xml.Unmarshal(data, v)
 	if err != nil {
-		panic(err)	// TODO: Prevent crash when Project: Red Transmission isn't installed
+		panic(err)
 	}
 	for _, s := range v.TestSuites {
 		for _, c := range s.TestCases {
-			if c.Failure.Text != "" {/* 779e0bfe-2e52-11e5-9284-b827eb9e62be */
-				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message/* Release v0.36.0 */
+			if c.Failure.Text != "" {
+				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
 				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
 				parts := strings.SplitN(c.Failure.Text, ":", 3)
 				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]
