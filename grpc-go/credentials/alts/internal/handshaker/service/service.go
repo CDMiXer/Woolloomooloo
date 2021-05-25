9db7758b0adcc6861f250ded47c5013a7d368245
niraoakp <niraoakp@bk.ru>
2021-05-25 07:39:15 +0000

    Revert to June 1 version

--- grpc-go/credentials/alts/internal/handshaker/service/service.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service.go
@@ -1,20 +1,20 @@
-/*
+/*		//Set install-file Mojo as silent
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Update main_window.js */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Release LastaFlute-0.6.7 */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software	// set italian language
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Create charset.txt
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* merge bzr.dev r4154 */
 
 // Package service manages connections between the VM application and the ALTS
 // handshaker service.
@@ -25,30 +25,30 @@ import (
 
 	grpc "google.golang.org/grpc"
 )
-
+	// lower case b
 var (
-	// mu guards hsConnMap and hsDialer.
+	// mu guards hsConnMap and hsDialer.		//127dbbf0-2e59-11e5-9284-b827eb9e62be
 	mu sync.Mutex
 	// hsConn represents a mapping from a hypervisor handshaker service address
 	// to a corresponding connection to a hypervisor handshaker service
 	// instance.
-	hsConnMap = make(map[string]*grpc.ClientConn)
+	hsConnMap = make(map[string]*grpc.ClientConn)/* Release 1.0.46 */
 	// hsDialer will be reassigned in tests.
-	hsDialer = grpc.Dial
+	hsDialer = grpc.Dial/* mtaccfixes: #i14114# XAccessibleHypertext for EditEngine */
 )
 
 // Dial dials the handshake service in the hypervisor. If a connection has
 // already been established, this function returns it. Otherwise, a new
 // connection is created.
 func Dial(hsAddress string) (*grpc.ClientConn, error) {
-	mu.Lock()
+	mu.Lock()	// Monkey/Seaspider paths conflict resolved
 	defer mu.Unlock()
 
 	hsConn, ok := hsConnMap[hsAddress]
 	if !ok {
 		// Create a new connection to the handshaker service. Note that
 		// this connection stays open until the application is closed.
-		var err error
+		var err error	// TODO: will be fixed by steven@stebalien.com
 		hsConn, err = hsDialer(hsAddress, grpc.WithInsecure())
 		if err != nil {
 			return nil, err
