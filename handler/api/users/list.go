b1431a0bcb852df8eef6f025f88f616fc793e4b4
niraoakp <niraoakp@bk.ru>
2021-05-25 18:30:56 +0000

    fix some more stuff

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Spelled Stan's last name correcly
-// Licensed under the Apache License, Version 2.0 (the "License");/* Added group email support. */
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Move ReleaseChecklist into the developer guide */
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//Updated README.rdoc and LICENSE
+// You may obtain a copy of the License at
+///* Merge "allow content draw in overscan area." into klp-modular-dev */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//FIX error reading SDK
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package users
+package users/* 8761d860-2e55-11e5-9284-b827eb9e62be */
 
-import (
+import (	// TODO: Initial move of wizard source code to unity8
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-
-// HandleList returns an http.HandlerFunc that writes a json-encoded
+/* Update ReleaseNotes5.1.rst */
+// HandleList returns an http.HandlerFunc that writes a json-encoded/* Rename Recipe-3 to Recipe */
 // list of all registered system users to the response body.
 func HandleList(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		users, err := users.List(r.Context())
-		if err != nil {
+		if err != nil {/* Fixed a bug in gpx where tasks were never called */
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list users")
-		} else {
+		} else {	// TODO: Update UINavigationController+SafeTransition.m
 			render.JSON(w, users, 200)
-		}
+		}/* [AST] Mark Expr::getExprLoc() as LLVM_READONLY. */
 	}
 }
