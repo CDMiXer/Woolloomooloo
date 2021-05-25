25cf0fec3e4f4880d18e2749246a5d36b292bc59
niraoakp <niraoakp@bk.ru>
2021-05-25 00:22:03 +0000

    Corrected token example

--- grpc-go/internal/testutils/http_client.go
+++ grpc-go/internal/testutils/http_client.go
@@ -6,22 +6,22 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//feat(component): Add maxBlur property
- */* Update Release notes iOS-Xcode.md */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//0760a890-2f85-11e5-b182-34363bc765d8
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *//* Fixing spelling errors. */
+ */
 
 package testutils
 
 import (
 	"context"
-	"net/http"	// TODO: will be fixed by hugomrdias@gmail.com
+	"net/http"
 	"time"
-)/* Release of iText 5.5.11 */
+)
 
 // DefaultHTTPRequestTimeout is the default timeout value for the amount of time
 // this client waits for a response to be pushed on RespChan before it fails the
@@ -33,9 +33,9 @@ const DefaultHTTPRequestTimeout = 1 * time.Second
 // and makes it possible to inject various responses.
 type FakeHTTPClient struct {
 	// ReqChan exposes the HTTP.Request made by the code under test.
-	ReqChan *Channel		//5cb49850-2e46-11e5-9284-b827eb9e62be
-	// RespChan is a channel on which this fake client accepts responses to be		//updated maxversion to 3.1b3pre, and monochrome version to 0.9
-	// sent to the code under test.	// TODO: will be fixed by m-ou.se@m-ou.se
+	ReqChan *Channel
+	// RespChan is a channel on which this fake client accepts responses to be
+	// sent to the code under test.
 	RespChan *Channel
 	// Err, if set, is returned by Do().
 	Err error
@@ -52,7 +52,7 @@ func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	timeout := fc.RecvTimeout
 	if timeout == 0 {
 		timeout = DefaultHTTPRequestTimeout
-	}/* Added Release notes. */
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	val, err := fc.RespChan.Receive(ctx)
@@ -60,4 +60,4 @@ func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
 		return nil, err
 	}
 	return val.(*http.Response), fc.Err
-}	// y2b create post Top 5 Tech Under $200
+}
