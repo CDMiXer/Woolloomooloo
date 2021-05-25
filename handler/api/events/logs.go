ed6ae0f8109b7bd1d73b1b9fb41ac3527edae94f
niraoakp <niraoakp@bk.ru>
2021-05-25 11:00:12 +0000

    Automatic changelog generation for PR #1060 [ci skip]

--- handler/api/events/logs.go
+++ handler/api/events/logs.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Rename Magno_Logan.md to Magno-Logan.md
-// Licensed under the Apache License, Version 2.0 (the "License");/* [FEATURE] Add Release date for SSDT */
-// you may not use this file except in compliance with the License./* 24f183e6-2ece-11e5-905b-74de2bd44bed */
-// You may obtain a copy of the License at
-///* Component view */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Release FPCM 3.1.0 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixed loading of 8bits photos.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// fix runtime exception caused by timing slop
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Updated pom to include junit and regex value generator
-	// TODO: will be fixed by steven@stebalien.com
-package events/* Use supports_transport in per_repository.test_repository. */
+// limitations under the License.
 
-import (	// TODO: Merge branch '0.9.x'
-	"context"	// TODO: will be fixed by lexy8russo@outlook.com
+package events
+
+import (
+	"context"
 	"encoding/json"
 	"io"
-	"net/http"	// Fixed an issue in functions.lua
+	"net/http"
 	"strconv"
-	"time"	// TODO: - metocs dictionary stuff
+	"time"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release 1.1.0.1 */
+	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"
-)	// TODO: Minor optimization; Try to iterator over redeclarations only when necessary.
-	// Delete module.conf
+	"github.com/go-chi/chi"/* Create 518.md */
+)
+
 // HandleLogStream creates an http.HandlerFunc that streams builds logs
-// to the http.Response in an event stream format./* Release references and close executor after build */
+// to the http.Response in an event stream format.
 func HandleLogStream(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
@@ -42,13 +42,13 @@ func HandleLogStream(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)	// Update ExecutionDescriptionHandler.java
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)	// TODO: cache: use cache::RemoveItem()
 			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
-		if err != nil {
+		if err != nil {/* bitfinex2 parseOrderStatus edits */
 			render.BadRequest(w, err)
 			return
 		}
@@ -61,16 +61,16 @@ func HandleLogStream(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* Release 3.1.0. */
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Prepare Release REL_7_0_1 */
 			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return	// Added constraints and new copy method exercises.
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
@@ -80,20 +80,20 @@ func HandleLogStream(
 
 		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
-		h.Set("Cache-Control", "no-cache")
-		h.Set("Connection", "keep-alive")
+		h.Set("Cache-Control", "no-cache")/* New Function App Release deploy */
+		h.Set("Connection", "keep-alive")		//b731928c-2e60-11e5-9284-b827eb9e62be
 		h.Set("X-Accel-Buffering", "no")
 
-		f, ok := w.(http.Flusher)
+		f, ok := w.(http.Flusher)		//readme: Updated examples.
 		if !ok {
 			return
 		}
-
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 		io.WriteString(w, ": ping\n\n")
-		f.Flush()
-
+		f.Flush()		//Automatic changelog generation for PR #8656 [ci skip]
+		//Rename coinpit-client.py to basic-coinpit-client.py
 		ctx, cancel := context.WithCancel(r.Context())
-		defer cancel()
+		defer cancel()/* Merge "Require admin context for interfaces on ext network" */
 
 		enc := json.NewEncoder(w)
 		linec, errc := stream.Tail(ctx, step.ID)
