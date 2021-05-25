d8c5b0d149cbe62d9c961a0110a78ee550c8f6ea
niraoakp <niraoakp@bk.ru>
2021-05-25 22:08:46 +0000

    Reorganized the code. Also more work on reading header side info.

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//adding Makefile
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* [Harddisk.py] update MMC */
 
 // +build !oss
 
-package system
+package system	// TODO: hacked by caojiaoyue@protonmail.com
