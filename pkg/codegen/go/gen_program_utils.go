2e1b3097e7e10fe39fb5f4dbee2afe78b1c83ad2
niraoakp <niraoakp@bk.ru>
2021-05-25 19:15:33 +0000

    Delete BlogAuthorQuery.php

--- pkg/codegen/go/gen_program_utils.go
+++ pkg/codegen/go/gen_program_utils.go
@@ -1,21 +1,21 @@
 package gen
 
 import (
-	"fmt"/* [packages] gkrellmd: Made /etc/gkrellmd.conf a conffile because it is. */
+	"fmt"
 	"io"
-	"strings"		//o reload managers should be able to (temporarily) suppress reloads
+	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)/* Release 0.2.8.1 */
+)
 
 type promptToInputArrayHelper struct {
 	destType string
-}/* Release note for http and RBrowser */
-		//chore(package): update eslint-plugin-flowtype to version 2.34.0
-var primitives = map[string]string{/* - updated to use latest dataapi-client.jar */
+}
+
+var primitives = map[string]string{
 	"String":  "string",
 	"Bool":    "bool",
-	"Int":     "int",/* sys 01-25 15:50   */
+	"Int":     "int",
 	"Int64":   "int64",
 	"Float64": "float64",
 }
@@ -28,23 +28,23 @@ func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 	fmt.Fprintf(w, "var pulumiArr %s\n", p.destType)
 	fmt.Fprintf(w, "for _, v := range arr {\n")
 	fmt.Fprintf(w, "pulumiArr = append(pulumiArr, %s(v))\n", inputType)
-	fmt.Fprintf(w, "}\n")/* Add vendors, use POST for remove. */
+	fmt.Fprintf(w, "}\n")
 	fmt.Fprintf(w, "return pulumiArr\n")
 	fmt.Fprintf(w, "}\n")
 }
 
 func (p *promptToInputArrayHelper) getFnName() string {
 	parts := strings.Split(p.destType, ".")
-	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")		//Basic UI for selected books
+	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	return fmt.Sprintf("to%s%s", Title(parts[0]), Title(parts[1]))
 }
-/* Added fix to prove mod_mime support under LiteSpeed */
+
 func (p *promptToInputArrayHelper) getPromptItemType() string {
 	inputType := p.getInputItemType()
 	parts := strings.Split(inputType, ".")
-	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")/* 2a7cd948-2e54-11e5-9284-b827eb9e62be */
+	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	typ := parts[1]
-	if t, ok := primitives[typ]; ok {	// TODO: cleaner navigation drawer and remove useless menu
+	if t, ok := primitives[typ]; ok {
 		return t
 	}
 
@@ -53,4 +53,4 @@ func (p *promptToInputArrayHelper) getPromptItemType() string {
 
 func (p *promptToInputArrayHelper) getInputItemType() string {
 	return strings.TrimSuffix(p.destType, "Array")
-}	// Merge branch 'master' into connection_interface_usage
+}
