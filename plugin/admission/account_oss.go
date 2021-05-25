2d551438c9d4823751efaad89921aa744cace4ff
niraoakp <niraoakp@bk.ru>
2021-05-25 07:53:40 +0000

    Arreglando formulas.

--- plugin/admission/account_oss.go
+++ plugin/admission/account_oss.go
@@ -3,11 +3,11 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// fix(project): Type definition of KeyValue is wrong
-//      http://www.apache.org/licenses/LICENSE-2.0	// fixed bug where checkboxReleation id have not been delivered closes #323
 //
-// Unless required by applicable law or agreed to in writing, software/* update link markdown */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update onlinestatus.md
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,5 +20,5 @@ import "github.com/drone/drone/core"
 
 // Membership is a no-op admission controller
 func Membership(core.OrganizationService, []string) core.AdmissionService {
-	return new(noop)		//REF: renamed some classes and put net code in net package
+	return new(noop)
 }
