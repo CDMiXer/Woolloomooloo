224cd1946aaaae0fc5fe1b3e9a27141b438c4ce2
niraoakp <niraoakp@bk.ru>
2021-05-25 16:28:09 +0000

    73992f2c-2e50-11e5-9284-b827eb9e62be

--- hack/recurl.sh
+++ hack/recurl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -eux -o pipefail/* The Unproductivity Release :D */
-/* Fix build issue for cloud-plugin-iam. */
-file=$1
-url=$2		//Create disable_hyperthreading.sh
+set -eux -o pipefail
 
-# loop forever	// TODO: Pcbnew: Allows an offset for SMD type (and CONNECTOR type)  pads.
+file=$1
+url=$2
+
+# loop forever
 while ! curl -L -o "$file" -- "$url" ;do
   echo "sleeping before trying again"
   sleep 10s
-done		//Create statistics_lab2_pb2.m
+done
 
 chmod +x "$file"
