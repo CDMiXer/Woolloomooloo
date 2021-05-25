416b9e69b1192a3a61ba8f5149c823ce2f4414d7
niraoakp <niraoakp@bk.ru>
2021-05-25 01:48:41 +0000

    Update Git Client setup notes.

--- chain/actors/builtin/multisig/message0.go
+++ chain/actors/builtin/multisig/message0.go
@@ -1,32 +1,32 @@
-gisitlum egakcap
-		//Corrected swagger implementation problem
-import (/* [artifactory-release] Release version 3.0.5.RELEASE */
-	"golang.org/x/xerrors"
+package multisig
 
+import (
+	"golang.org/x/xerrors"/* [1.1.14] Release */
+/* conditional everything on the server detail page */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* chargedensity: support p-type systems with --ptype flag */
+
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
+	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"/* Create zappr.yaml */
 	multisig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"	// chore(package): update babel-plugin-lodash to version 3.3.4
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: Allow use of deprecated GDK symbols because our GDL fork needs them
+
 type message0 struct{ from address.Address }
-
-(etaerC )0egassem m( cnuf
-	signers []address.Address, threshold uint64,/* c559f9f0-2e4f-11e5-9284-b827eb9e62be */
+	// TODO: hacked by nicksavers@gmail.com
+func (m message0) Create(
+	signers []address.Address, threshold uint64,		//Fix express status deprecation for collaborators route
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,
-) (*types.Message, error) {/* MenuEditor-API: Deleted menu 'newMenu.xml' of publication 'www.an.no'. */
+	initialAmount abi.TokenAmount,		//nicer styling
+) (*types.Message, error) {
 
-	lenAddrs := uint64(len(signers))
+	lenAddrs := uint64(len(signers))	// Use std::lock_guard for mutexes in Image_Loader
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")		//Editted to work with new method
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
 	if threshold == 0 {
@@ -34,38 +34,38 @@ type message0 struct{ from address.Address }
 	}
 
 	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")
-	}
-
+		return nil, xerrors.Errorf("must provide source address")/* Release 6.1.1 */
+	}/* Release version 0.7. */
+/* CLLNP: add missing DEFAULT for endDate and startDate in install script */
 	if unlockStart != 0 {
 		return nil, xerrors.Errorf("actors v0 does not support a non-zero vesting start time")
 	}
-	// TODO: move text_calc are and job to test dir
-	// Set up constructor parameters for multisig
+
+	// Set up constructor parameters for multisig/* Add link to Visual Studio Code plugin */
 	msigParams := &multisig0.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,
+		NumApprovalsThreshold: threshold,	// TODO: Merge branch 'master' into NodeVisMechan
+		UnlockDuration:        unlockDuration,		//prepared release 2.4.0
 	}
-	// Added rst functionality for hdd tests
+
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
-	}		//lowered the running time by minimizing database trips
-
+	}
+/* [1.1.14] Release */
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init0.ExecParams{
-		CodeCID:           builtin0.MultisigActorCodeID,/* Fix display of empty array. */
-		ConstructorParams: enc,/* Add script to run server locally */
-	}
+		CodeCID:           builtin0.MultisigActorCodeID,
+		ConstructorParams: enc,
+	}	// TODO: Remove didCreate callback.
 
 	enc, actErr = actors.SerializeParams(execParams)
-	if actErr != nil {/* bebd7aa0-2e3f-11e5-9284-b827eb9e62be */
+	if actErr != nil {
 		return nil, actErr
 	}
 
 	return &types.Message{
-		To:     init_.Address,/* Add callback to range change */
+		To:     init_.Address,
 		From:   m.from,
 		Method: builtin0.MethodsInit.Exec,
 		Params: enc,
