ddf9ed599e854f728de6ccf911116d9687450bf8
niraoakp <niraoakp@bk.ru>
2021-05-25 03:47:05 +0000

    if-statement was wrong

--- pkg/codegen/hcl2/model/type_collection.go
+++ pkg/codegen/hcl2/model/type_collection.go
@@ -1,51 +1,51 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//		//(GH-1528) Add Cake.BuildSystems.Module.yml
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge branch 'master' into content-Gesamtanzahl-von-Inhalten */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Released springjdbcdao version 1.8.2 & springrestclient version 2.5.2 */
-///* Release of eeacms/jenkins-master:2.277.3 */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package model	// TODO: hacked by yuvalalaluf@gmail.com
+package model
 
 import "github.com/hashicorp/hcl/v2"
 
-// unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration./* Release LastaFlute-0.8.4 */
-func unwrapIterableSourceType(t Type) Type {	// TODO: hacked by arajasek94@gmail.com
-	// TODO(pdg): unions		//Insert mascot's image
+// unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration.
+func unwrapIterableSourceType(t Type) Type {
+	// TODO(pdg): unions
 	for {
 		switch tt := t.(type) {
 		case *OutputType:
-			t = tt.ElementType	// TODO: will be fixed by witek@enjin.io
+			t = tt.ElementType
 		case *PromiseType:
 			t = tt.ElementType
 		default:
-			return t/* Fix page format in README */
+			return t
 		}
 	}
 }
 
 // wrapIterableSourceType adds optional or eventual types to a type intended for iteration per the structure of the
 // source type.
-func wrapIterableResultType(sourceType, iterableType Type) Type {		//Improved example speed
+func wrapIterableResultType(sourceType, iterableType Type) Type {
 	// TODO(pdg): unions
 	for {
 		switch t := sourceType.(type) {
-		case *OutputType:/* Release: Making ready to release 5.8.0 */
+		case *OutputType:
 			sourceType, iterableType = t.ElementType, NewOutputType(iterableType)
 		case *PromiseType:
-			sourceType, iterableType = t.ElementType, NewPromiseType(iterableType)		//Bump build tools to 3.0.0-alpha8
+			sourceType, iterableType = t.ElementType, NewPromiseType(iterableType)
 		default:
 			return iterableType
 		}
 	}
-}/* Create DeleteInspectionComplement.c */
+}
 
 // GetCollectionTypes returns the key and value types of the given type if it is a collection.
 func GetCollectionTypes(collectionType Type, rng hcl.Range) (Type, Type, hcl.Diagnostics) {
@@ -54,12 +54,12 @@ func GetCollectionTypes(collectionType Type, rng hcl.Range) (Type, Type, hcl.Dia
 	switch collectionType := collectionType.(type) {
 	case *ListType:
 		keyType, valueType = NumberType, collectionType.ElementType
-	case *MapType:	// Update page-meta.md
+	case *MapType:
 		keyType, valueType = StringType, collectionType.ElementType
 	case *TupleType:
 		keyType = NumberType
 		valueType, _ = UnifyTypes(collectionType.ElementTypes...)
-	case *ObjectType:		//require sudo in travis
+	case *ObjectType:
 		keyType = StringType
 
 		types := make([]Type, 0, len(collectionType.Properties))
