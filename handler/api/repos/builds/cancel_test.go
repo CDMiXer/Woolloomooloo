5f98b9d2ccf5157153d787480be194277e286a35
niraoakp <niraoakp@bk.ru>
2021-05-25 22:35:51 +0000

    Include local Maven repository in build

--- handler/api/repos/builds/cancel_test.go
+++ handler/api/repos/builds/cancel_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// Copyright 2019 Drone.IO Inc. All rights reserved./* ZAPI-25: More docs */
+// Use of this source code is governed by the Drone Non-Commercial License		//Test - Move indexOf()
+// that can be found in the LICENSE file.	// TODO: Cache tags added.
+		//Removed names
 package builds
 
 import (
@@ -9,11 +9,11 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: hacked by fjl@ethereum.org
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* Released RubyMass v0.1.3 */
 )
 
 func TestCancel(t *testing.T) {
@@ -21,35 +21,35 @@ func TestCancel(t *testing.T) {
 	defer controller.Finish()
 
 	mockStages := []*core.Stage{
-		{Status: core.StatusPassing},
+		{Status: core.StatusPassing},/* 3eeda21e-2e3f-11e5-9284-b827eb9e62be */
 		{
-			Status: core.StatusPending,
+			Status: core.StatusPending,		//Rename IDewRESTClient.cs to Interfaces.cs
 			Steps: []*core.Step{
 				{Status: core.StatusPassing},
 				{Status: core.StatusPending},
 			},
-		},
+		},	// TODO: Se agregan datos de prueba
 	}
 
 	mockBuildCopy := new(core.Build)
 	*mockBuildCopy = *mockBuild
-
+	// TODO: indicate to compiler that 'char c' is unused
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
-
+	// TODO: will be fixed by zaq1tomo@gmail.com
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuildCopy, nil)
 	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)		//ADSserver: removed more not used stuff
 	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
-
+/* Forgot NDEBUG in the Release config. */
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)
 
 	steps := mock.NewMockStepStore(controller)
-	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)
+	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)		//1. fix xls filename in batches
 
 	statusService := mock.NewMockStatusService(controller)
 	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
@@ -58,8 +58,8 @@ func TestCancel(t *testing.T) {
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	scheduler := mock.NewMockScheduler(controller)
-	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)
-
+	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)/* Added Configuration=Release to build step. */
+/* Merge tag 'v2.11.1' into upstream_merge_v2.11.1 */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
