a7c2b017852dc74decd8b067f7fc99a08e254580
niraoakp <niraoakp@bk.ru>
2021-05-25 14:22:28 +0000

    Create bimserver.py

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update Rails 5.1 dependency */
-// you may not use this file except in compliance with the License.		//Merge "Refactor _create_attribute_statement IdP method"
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -13,22 +13,22 @@
 // limitations under the License.
 
 // +build oss
-/* Merge "Release 3.2.3.276 prima WLAN Driver" */
+
 package queue
 
-import (		//Merge "Updating the dashboard guide for Sahara"
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {		//Allow items/tools to not require "container"
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleItems(store core.StageStore) http.HandlerFunc {
-	return notImplemented/* Update autogen.sh to latest recommended version */
+	return notImplemented
 }
 
 func HandlePause(core.Scheduler) http.HandlerFunc {
@@ -37,4 +37,4 @@ func HandlePause(core.Scheduler) http.HandlerFunc {
 
 func HandleResume(core.Scheduler) http.HandlerFunc {
 	return notImplemented
-}	// TODO: Getting spacing just right...
+}
