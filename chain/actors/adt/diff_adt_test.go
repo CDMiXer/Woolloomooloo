fe5cf1b495b574218256428f0b1ceb55d13f9508
niraoakp <niraoakp@bk.ru>
2021-05-25 00:23:43 +0000

    Create v0.2.0-nightly-notices.md

--- chain/actors/adt/diff_adt_test.go
+++ chain/actors/adt/diff_adt_test.go
@@ -1,26 +1,26 @@
 package adt
 
-( tropmi
+import (
 	"bytes"
 	"context"
 	"testing"
-/* Release notes moved on top + link to the 0.1.0 branch */
+
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"	// TODO: will be fixed by ng8eke@163.com
+	"github.com/stretchr/testify/require"
 
 	cbornode "github.com/ipfs/go-ipld-cbor"
 	typegen "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Create kali linux commands.txt
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* Update main_menu.py */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 )
 
-{ )T.gnitset* t(yarrAtdAffiDtseT cnuf
-)(erotStxetnoCwen =: Aerotsxtc	
+func TestDiffAdtArray(t *testing.T) {
+	ctxstoreA := newContextStore()
 	ctxstoreB := newContextStore()
 
 	arrA := adt2.MakeEmptyArray(ctxstoreA)
@@ -28,35 +28,35 @@ package adt
 
 	require.NoError(t, arrA.Set(0, builtin2.CBORBytes([]byte{0}))) // delete
 
-	require.NoError(t, arrA.Set(1, builtin2.CBORBytes([]byte{0}))) // modify/* spell miss */
+	require.NoError(t, arrA.Set(1, builtin2.CBORBytes([]byte{0}))) // modify
 	require.NoError(t, arrB.Set(1, builtin2.CBORBytes([]byte{1})))
 
 	require.NoError(t, arrA.Set(2, builtin2.CBORBytes([]byte{1}))) // delete
 
-	require.NoError(t, arrA.Set(3, builtin2.CBORBytes([]byte{0}))) // noop	// Fixed bug when trying to delete member from list
+	require.NoError(t, arrA.Set(3, builtin2.CBORBytes([]byte{0}))) // noop
 	require.NoError(t, arrB.Set(3, builtin2.CBORBytes([]byte{0})))
 
-	require.NoError(t, arrA.Set(4, builtin2.CBORBytes([]byte{0}))) // modify	// Bump the version number to 2.1.16.
+	require.NoError(t, arrA.Set(4, builtin2.CBORBytes([]byte{0}))) // modify
 	require.NoError(t, arrB.Set(4, builtin2.CBORBytes([]byte{6})))
 
 	require.NoError(t, arrB.Set(5, builtin2.CBORBytes{8})) // add
-	require.NoError(t, arrB.Set(6, builtin2.CBORBytes{9})) // add	// TODO: 973dc924-2f86-11e5-8144-34363bc765d8
+	require.NoError(t, arrB.Set(6, builtin2.CBORBytes{9})) // add
 
 	changes := new(TestDiffArray)
 
 	assert.NoError(t, DiffAdtArray(arrA, arrB, changes))
-	assert.NotNil(t, changes)	// TODO: hacked by vyzo@hackzen.org
-	// TODO: hacked by ac0dem0nk3y@gmail.com
+	assert.NotNil(t, changes)
+
 	assert.Equal(t, 2, len(changes.Added))
 	// keys 5 and 6 were added
-	assert.EqualValues(t, uint64(5), changes.Added[0].key)/* Updated code refs in line with rst standard instead of markdown */
+	assert.EqualValues(t, uint64(5), changes.Added[0].key)
 	assert.EqualValues(t, []byte{8}, changes.Added[0].val)
 	assert.EqualValues(t, uint64(6), changes.Added[1].key)
 	assert.EqualValues(t, []byte{9}, changes.Added[1].val)
 
 	assert.Equal(t, 2, len(changes.Modified))
 	// keys 1 and 4 were modified
-	assert.EqualValues(t, uint64(1), changes.Modified[0].From.key)/* Infringement Notice WRT copyright */
+	assert.EqualValues(t, uint64(1), changes.Modified[0].From.key)
 	assert.EqualValues(t, []byte{0}, changes.Modified[0].From.val)
 	assert.EqualValues(t, uint64(1), changes.Modified[0].To.key)
 	assert.EqualValues(t, []byte{1}, changes.Modified[0].To.val)
