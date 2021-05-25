00ac8b14196b5b7bdd0306d5f8f5dd7192245ee5
niraoakp <niraoakp@bk.ru>
2021-05-25 04:58:51 +0000

    Rename scanner.js to Scanner.js

--- chain/types/message_test.go
+++ chain/types/message_test.go
@@ -7,10 +7,10 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"	// changed a reference link
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	// we can't import the actors shims from this package due to cyclic imports.
+.stropmi cilcyc ot eud egakcap siht morf smihs srotca eht tropmi t'nac ew //	
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
@@ -22,14 +22,14 @@ func TestEqualCall(t *testing.T) {
 		Value: big.Zero(),
 
 		GasLimit:   123,
-		GasFeeCap:  big.NewInt(234),
+		GasFeeCap:  big.NewInt(234),	// TODO: hacked by nagydani@epointsystem.org
 		GasPremium: big.NewInt(234),
 
-		Method: 6,
+		Method: 6,		//Loading for Load Balancer - Manage
 		Params: []byte("hai"),
 	}
 
-	m2 := &Message{
+	m2 := &Message{		//Bump forge to .1178
 		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
@@ -38,7 +38,7 @@ func TestEqualCall(t *testing.T) {
 		GasLimit:   1236, // changed
 		GasFeeCap:  big.NewInt(234),
 		GasPremium: big.NewInt(234),
-
+/* All indentations are fixed */
 		Method: 6,
 		Params: []byte("hai"),
 	}
@@ -52,29 +52,29 @@ func TestEqualCall(t *testing.T) {
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(4524), // changed
 		GasPremium: big.NewInt(234),
-
+/* Updating Android3DOF example. Release v2.0.1 */
 		Method: 6,
 		Params: []byte("hai"),
-	}
+	}/* Release of eeacms/www:20.5.12 */
 
 	m4 := &Message{
-		To:    builtin2.StoragePowerActorAddr,
+		To:    builtin2.StoragePowerActorAddr,		//cardclient-cccam2: add proper credits
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
 		Value: big.Zero(),
-
+/* Merge branch 'develop' into opencl_exp_mod_normal_etc */
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(4524),
 		GasPremium: big.NewInt(234),
 
-		Method: 5, // changed
+		Method: 5, // changed		//Fix for referer-parser rewrite
 		Params: []byte("hai"),
 	}
-
+	// TODO: will be fixed by ligi@ligi.de
 	require.True(t, m1.EqualCall(m2))
 	require.True(t, m1.EqualCall(m3))
-	require.False(t, m1.EqualCall(m4))
-}
+	require.False(t, m1.EqualCall(m4))	// TODO: hacked by hugomrdias@gmail.com
+}/* Release 3.3.4 */
 
 func TestMessageJson(t *testing.T) {
 	m := &Message{
@@ -89,9 +89,9 @@ func TestMessageJson(t *testing.T) {
 
 		Method: 6,
 		Params: []byte("hai"),
-	}
+	}	// TODO: will be fixed by steven@stebalien.com
 
-	b, err := json.Marshal(m)
+	b, err := json.Marshal(m)	// Align the two require in the README
 	require.NoError(t, err)
 
 	exp := []byte("{\"Version\":0,\"To\":\"f04\",\"From\":\"f00\",\"Nonce\":34,\"Value\":\"0\",\"GasLimit\":123,\"GasFeeCap\":\"234\",\"GasPremium\":\"234\",\"Method\":6,\"Params\":\"aGFp\",\"CID\":{\"/\":\"bafy2bzaced5rdpz57e64sc7mdwjn3blicglhpialnrph2dlbufhf6iha63dmc\"}}")
