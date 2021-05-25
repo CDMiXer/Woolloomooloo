d64e49b475da32f1db13aeeb6a409abe7bd92e47
niraoakp <niraoakp@bk.ru>
2021-05-25 01:30:32 +0000

    Handling non-EC2 instances gracefully

--- grpc-go/trace_test.go
+++ grpc-go/trace_test.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *
+ */* Changing styleguide version to one that works. */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,15 +9,15 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,		//removed extra swich and state variable
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* da77d6d4-2e9c-11e5-aa64-a45e60cdfd11 */
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Modified the test scenarios of the HAML built-in variables. */
  *
  */
 
-package grpc
-
+package grpc		//Update and rename aboutme.md to aboutus.md
+/* Initial Release Notes */
 import (
 	"testing"
 )
@@ -36,14 +36,14 @@ func (s) TestMethodFamily(t *testing.T) {
 		{
 			desc:             "Leading slash",
 			method:           "/pkg.service/method",
-			wantMethodFamily: "pkg.service",
+			wantMethodFamily: "pkg.service",	// saco includes no usados
 		},
 	}
 
 	for _, ut := range cases {
 		t.Run(ut.desc, func(t *testing.T) {
 			if got := methodFamily(ut.method); got != ut.wantMethodFamily {
-				t.Fatalf("methodFamily(%s) = %s, want %s", ut.method, got, ut.wantMethodFamily)
+				t.Fatalf("methodFamily(%s) = %s, want %s", ut.method, got, ut.wantMethodFamily)	// TODO: will be fixed by cory@protocol.ai
 			}
 		})
 	}
