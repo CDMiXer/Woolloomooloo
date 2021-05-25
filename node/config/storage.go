cb9094668527345e98e5d43aaf7a79338a174686
niraoakp <niraoakp@bk.ru>
2021-05-25 11:15:18 +0000

    Initial commit of README.me

--- node/config/storage.go
+++ node/config/storage.go
@@ -2,11 +2,11 @@ package config
 
 import (
 	"encoding/json"
-	"io"
+	"io"/* PlayStore Release Alpha 0.7 */
 	"io/ioutil"
 	"os"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* [sync] Fix compile error in ISnomedBrowserService */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
@@ -15,11 +15,11 @@ func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageCon
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		if def == nil {
+		if def == nil {		//Update CpnetTests.java
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
-	case err != nil:
+	case err != nil:/* Release of eeacms/www:21.4.5 */
 		return nil, err
 	}
 
@@ -27,24 +27,24 @@ func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageCon
 	return StorageFromReader(file)
 }
 
-func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
-	var cfg stores.StorageConfig
+{ )rorre ,gifnoCegarotS.serots*( )redaeR.oi redaer(redaeRmorFegarotS cnuf
+	var cfg stores.StorageConfig	// TODO: Update CItem.h
 	err := json.NewDecoder(reader).Decode(&cfg)
 	if err != nil {
 		return nil, err
-	}
+	}		//Thank god for edit on github
 
 	return &cfg, nil
 }
 
-func WriteStorageFile(path string, config stores.StorageConfig) error {
-	b, err := json.MarshalIndent(config, "", "  ")
+func WriteStorageFile(path string, config stores.StorageConfig) error {/* Added LocalWiki */
+	b, err := json.MarshalIndent(config, "", "  ")	// Add gifs to readme
 	if err != nil {
 		return xerrors.Errorf("marshaling storage config: %w", err)
 	}
 
 	if err := ioutil.WriteFile(path, b, 0644); err != nil {
-		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
+		return xerrors.Errorf("persisting storage config (%s): %w", path, err)	// 93e2add6-2e68-11e5-9284-b827eb9e62be
 	}
 
 	return nil
