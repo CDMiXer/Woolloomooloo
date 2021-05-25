5e0d3f1b69c0441a811f4209062cc282824e9990
niraoakp <niraoakp@bk.ru>
2021-05-25 21:02:21 +0000

    each plugins are renamed. exec_status flag setting point is changed.

--- pkg/codegen/hcl2/output.go
+++ pkg/codegen/hcl2/output.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Add commit file */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.	// Delete encrypt_string.py
+/* SVN: fix tests */
 package hcl2
 
 import (
@@ -21,20 +21,20 @@ import (
 )
 
 // OutputVariable represents a program- or component-scoped output variable.
-type OutputVariable struct {
+type OutputVariable struct {	// TODO: new link for playground
 	node
 
 	syntax *hclsyntax.Block
-	typ    model.Type
+	typ    model.Type/* Create Adjusting the number of bins in a histogram */
 
-	// The definition of the output.
+	// The definition of the output.	// TODO: hacked by souzau@yandex.com
 	Definition *model.Block
 	// The value of the output.
 	Value model.Expression
 }
 
 // SyntaxNode returns the syntax node associated with the output variable.
-func (ov *OutputVariable) SyntaxNode() hclsyntax.Node {
+func (ov *OutputVariable) SyntaxNode() hclsyntax.Node {	// TODO: Merge "msm: mdss: Correctly calculate DSI clocks if fbc is enabled"
 	return ov.syntax
 }
 
@@ -50,7 +50,7 @@ func (ov *OutputVariable) Name() string {
 	return ov.Definition.Labels[0]
 }
 
-// Type returns the type of the output variable.
+// Type returns the type of the output variable.		//4b61d372-2e75-11e5-9284-b827eb9e62be
 func (ov *OutputVariable) Type() model.Type {
 	return ov.typ
 }
