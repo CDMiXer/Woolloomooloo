e28f9422a6d9c4ce00be4a65623e315765160258
niraoakp <niraoakp@bk.ru>
2021-05-25 19:51:38 +0000

    Merge remote-tracking branch 'origin/parser' into feature/server-test

--- handler/web/version.go
+++ handler/web/version.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//		//Delete nn_norm.pyc
+// Licensed under the Apache License, Version 2.0 (the "License");/* Add navbar */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Update Samsung20nmDDR4.md */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: hacked by lexy8russo@outlook.com
 
 package web
 
@@ -20,7 +20,7 @@ import (
 	"github.com/drone/drone/version"
 )
 
-// HandleVersion creates an http.HandlerFunc that returns the
+// HandleVersion creates an http.HandlerFunc that returns the		//Update to match Mozilla "modern" cipher suite recommendations.
 // version number and build details.
 func HandleVersion(w http.ResponseWriter, r *http.Request) {
 	v := struct {
