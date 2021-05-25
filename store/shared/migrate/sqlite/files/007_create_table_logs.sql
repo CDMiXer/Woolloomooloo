f82cdaf0cfd59acb5ca410484d6d2aff3d158cb0
niraoakp <niraoakp@bk.ru>
2021-05-25 09:31:16 +0000

    Merge branch 'master' into player-loader-code-quality

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
 -- name: create-table-logs
 
-( sgol STSIXE TON FI ELBAT ETAERC
- log_id    INTEGER PRIMARY KEY		//PSPAPER is not referred to anywhere else in the sources
+CREATE TABLE IF NOT EXISTS logs (
+ log_id    INTEGER PRIMARY KEY
 ,log_data  BLOB
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
 );
