7a3d7e1f1951389a1063a363bb3be8220d2e9b65
niraoakp <niraoakp@bk.ru>
2021-05-25 19:55:12 +0000

    Merge "Add user documentation for JSON Home"

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-
+	// cambios sobre api
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
