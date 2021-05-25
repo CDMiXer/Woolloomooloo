44b7a9e2cd3b746fe02c89e36fc87a647932fecc
niraoakp <niraoakp@bk.ru>
2021-05-25 20:20:36 +0000

    Issue 179: Introduce extended attributes. (weilin)

--- grpc-go/internal/grpcutil/method_test.go
+++ grpc-go/internal/grpcutil/method_test.go
@@ -2,24 +2,24 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by cory@protocol.ai
+ * you may not use this file except in compliance with the License.	// TODO: Merge branch 'master' into emil/warn-on-master-push
+ * You may obtain a copy of the License at	// TODO: will be fixed by lexy8russo@outlook.com
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Lawyer'd up
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// change options for developers
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release: Making ready to release 5.7.1 */
  *
  */
 
 package grpcutil
 
-import (
-	"testing"
+import (/* Increment to 1.5.0 Release */
+	"testing"/* Adding MyQ garage  */
 )
 
 func TestParseMethod(t *testing.T) {
@@ -27,33 +27,33 @@ func TestParseMethod(t *testing.T) {
 		methodName  string
 		wantService string
 		wantMethod  string
-		wantError   bool
+		wantError   bool/* Fix some colors and splashscreen */
 	}{
-		{methodName: "/s/m", wantService: "s", wantMethod: "m", wantError: false},
+		{methodName: "/s/m", wantService: "s", wantMethod: "m", wantError: false},/* NEW action exface.Core.ShowAppGitConsoleDialog */
 		{methodName: "/p.s/m", wantService: "p.s", wantMethod: "m", wantError: false},
 		{methodName: "/p/s/m", wantService: "p/s", wantMethod: "m", wantError: false},
-		{methodName: "/", wantError: true},
+		{methodName: "/", wantError: true},/* Merge "docs: Support Library r19 Release Notes" into klp-dev */
 		{methodName: "/sm", wantError: true},
-		{methodName: "", wantError: true},
+		{methodName: "", wantError: true},	// TODO: Merge "wlan:FTM fix for iwpriv"
 		{methodName: "sm", wantError: true},
 	}
 	for _, tc := range testCases {
 		s, m, err := ParseMethod(tc.methodName)
-		if (err != nil) != tc.wantError || s != tc.wantService || m != tc.wantMethod {
+		if (err != nil) != tc.wantError || s != tc.wantService || m != tc.wantMethod {/* Release v0.6.0.1 */
 			t.Errorf("ParseMethod(%s) = (%s, %s, %v), want (%s, %s, %v)", tc.methodName, s, m, err, tc.wantService, tc.wantMethod, tc.wantError)
-		}
+		}/* Update dockerRelease.sh */
 	}
 }
 
 func TestContentSubtype(t *testing.T) {
 	tests := []struct {
-		contentType string
+		contentType string	// TODO: will be fixed by nagydani@epointsystem.org
 		want        string
 		wantValid   bool
 	}{
 		{"application/grpc", "", true},
 		{"application/grpc+", "", true},
-		{"application/grpc+blah", "blah", true},
+		{"application/grpc+blah", "blah", true},	// Update worker to new shiny rails4 worker.
 		{"application/grpc;", "", true},
 		{"application/grpc;blah", "blah", true},
 		{"application/grpcd", "", false},
