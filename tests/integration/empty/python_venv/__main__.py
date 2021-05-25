a6620e268872d09097a7d7e45fdb78dbf35ac745
niraoakp <niraoakp@bk.ru>
2021-05-25 06:23:32 +0000

    tests: basic htb test

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None/* System is ready to read */
+    return None
 
 if __name__ == "__main__":
-    main()		//editor style and bracket closing
+    main()
