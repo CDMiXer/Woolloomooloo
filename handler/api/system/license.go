cb4c919356cd8d61dc9aaa5424eb4bbc095c8a49
niraoakp <niraoakp@bk.ru>
2021-05-25 03:52:13 +0000

    Update equacao_2_grau.c

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -2,21 +2,21 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Add Caveat About Adding a Tag Filter If Using the GitHub Release */
-/* update license.txt */
+// +build !oss
+
 package system
 
-import (	// TODO: 01IS - FAA validated - Kilt McHaggis
+import (
 	"net/http"
-/* Updated tags on hs mittweida */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Update from Forestry.io - _drafts/_posts/teastas.md
-)/* Make driver014 and driver015 parallelisable */
 
-// HandleLicense returns an http.HandlerFunc that writes/* update files list */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+)
+
+// HandleLicense returns an http.HandlerFunc that writes
 // json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		render.JSON(w, license, 200)		//added primary key
+		render.JSON(w, license, 200)
 	}
 }
