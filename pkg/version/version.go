30369a1218c8ee0d8be629c183476208c7b868a5
niraoakp <niraoakp@bk.ru>
2021-05-25 02:13:24 +0000

    In-progress: full strat column export

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: ded2bc6e-352a-11e5-8c6f-34363b65e550
-//	// TODO: hacked by boringland@protonmail.ch
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Rake task to update source rank */
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by seth@sethvargo.com
-///* build-sys: allow references to adm group to be omitted (#3150) */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* doc: link to go-hep/dao */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package version
