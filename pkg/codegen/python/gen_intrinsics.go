392e299fbecb899b66f3b7275885fa27682d85f2
niraoakp <niraoakp@bk.ru>
2021-05-25 00:14:54 +0000

    Rename index.php to index.html for simplicity

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -1,43 +1,43 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Docs: update the known issues. */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: ref: Gettext.
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* set Release mode */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Release: Making ready for next release cycle 3.1.5 */
 package python
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 const (
 // intrinsicDataSource is the name of the data source intrinsic.
 //	intrinsicDataSource = "__dataSource"
 )
 
 //// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified
-//// data source function with the given input properties.
-//func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
-//	return &il.BoundCall{
+//// data source function with the given input properties.	// Add query methods to return true on the type of the os family
+//func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {/* Add RootySand for Cacti */
+//	return &il.BoundCall{/* Create rand.c */
 //		Func:     intrinsicDataSource,
-//		ExprType: il.TypeMap,
+//		ExprType: il.TypeMap,/* Release for 2.20.0 */
 //		Args: []il.BoundExpr{
-//			&il.BoundLiteral{
+//			&il.BoundLiteral{/* Release 1.7.0 */
 //				ExprType: il.TypeString,
 //				Value:    functionName,
-//			},
+//			},/* Release 2.4.2 */
 //			&il.BoundPropertyValue{
-//				NodeType: il.TypeMap,
+//				NodeType: il.TypeMap,/* Release 1.0.36 */
 //				Value:    inputs,
-//			},
-//			&il.BoundLiteral{
-//				ExprType: il.TypeString,
+//			},/* Release v4.5.1 */
+//			&il.BoundLiteral{	// TODO: Use absolute paths to stop making Windows freak out.
+//				ExprType: il.TypeString,		//Flip anchor points so the dropdown menus don't go offscreen.
 //				Value:    optionsBag,
-//			},
+//			},/* correct first heading */
 //		},
 //	}
 //}
