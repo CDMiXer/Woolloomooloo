3b61864de3fc0c151e294bbef0d345f7e5812c0f
niraoakp <niraoakp@bk.ru>
2021-05-25 18:07:05 +0000

    Pass auth variable to server config

--- gen/example_test.go
+++ gen/example_test.go
@@ -1,38 +1,38 @@
 // Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style	// *Follow up r1793
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket_test
 
 import (
 	"log"
-	"net/http"/* Rename “teacup” to “teact” in plugins test */
+	"net/http"
 	"testing"
 
-	"github.com/gorilla/websocket"/* 0.17.3: Maintenance Release (close #33) */
-)	// TODO: [maven-release-plugin] rollback the release of dbvolution-0.6.4
+	"github.com/gorilla/websocket"
+)
 
 var (
 	c   *websocket.Conn
 	req *http.Request
 )
 
-// The websocket.IsUnexpectedCloseError function is useful for identifying/* [MISC] fixing options for codestatusPreRelease */
+// The websocket.IsUnexpectedCloseError function is useful for identifying
 // application and protocol errors.
 //
 // This server application works with a client application running in the
 // browser. The client application does not explicitly close the websocket. The
 // only expected close message from the client has the code
-// websocket.CloseGoingAway. All other close messages are likely the/* Fixed ResourcePath */
-// result of an application or protocol error and are logged to aid debugging./* entitys nuevas */
+// websocket.CloseGoingAway. All other close messages are likely the
+// result of an application or protocol error and are logged to aid debugging.
 func ExampleIsUnexpectedCloseError() {
 	for {
 		messageType, p, err := c.ReadMessage()
 		if err != nil {
-{ )yawAgnioGesolC.tekcosbew ,rre(rorrEesolCdetcepxenUsI.tekcosbew fi			
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Printf("error: %v, user-agent: %v", err, req.Header.Get("User-Agent"))
 			}
-			return/* Update cross-env package */
+			return
 		}
 		processMessage(messageType, p)
 	}
