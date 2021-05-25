68bafe1c744ae250f0d5dfe76270460d70a1781d
niraoakp <niraoakp@bk.ru>
2021-05-25 13:06:55 +0000

    0dcbf480-2e4c-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,41 +1,41 @@
--- name: create-table-stages
-
+-- name: create-table-stages/* Merge "Release notes for b1d215726e" */
+/* Release notes 7.1.11 */
 CREATE TABLE IF NOT EXISTS stages (
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,stage_repo_id     INTEGER	// First attempt at test coverage via coveralls.io
+,stage_repo_id     INTEGER		//distinct rendering of habtm
 ,stage_build_id    INTEGER
-,stage_number      INTEGER	// TODO: will be fixed by ng8eke@163.com
+,stage_number      INTEGER
 ,stage_kind        TEXT
-,stage_type        TEXT
+,stage_type        TEXT	// TODO: test video on 404 page
 ,stage_name        TEXT
-,stage_status      TEXT		//started creating basic inspectors and constructos
-,stage_error       TEXT
-,stage_errignore   BOOLEAN/* Update to match new org */
-,stage_exit_code   INTEGER
-,stage_limit       INTEGER		//getLevel added to paratree
-,stage_os          TEXT
-,stage_arch        TEXT/* Removed a little whitespace */
-,stage_variant     TEXT
+TXET      sutats_egats,
+,stage_error       TEXT/* Renamed .travil.yml to .travis.yml */
+,stage_errignore   BOOLEAN
+,stage_exit_code   INTEGER/* Added sort key to the document index. */
+,stage_limit       INTEGER		//85df36fc-2e75-11e5-9284-b827eb9e62be
+,stage_os          TEXT		//Use esc_attr() consistently in wp_dropdown_pages().
+,stage_arch        TEXT
+,stage_variant     TEXT/* Merge "Release note for Zaqar resource support" */
 ,stage_kernel      TEXT
 ,stage_machine     TEXT
-,stage_started     INTEGER
-,stage_stopped     INTEGER/* DATASOLR-257 - Release version 1.5.0.RELEASE (Gosling GA). */
+,stage_started     INTEGER/* Release version [10.5.2] - prepare */
+,stage_stopped     INTEGER
 ,stage_created     INTEGER
-,stage_updated     INTEGER
+,stage_updated     INTEGER	// fixed bug in avoid_readback disk cache eviction algorithm
 ,stage_version     INTEGER
-,stage_on_success  BOOLEAN
-,stage_on_failure  BOOLEAN
+,stage_on_success  BOOLEAN/* Delete prod.log */
+,stage_on_failure  BOOLEAN	// TODO: Change association to affiliation
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
 ,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
 );
-
+	// TODO: hacked by nick@perfectabstractions.com
 -- name: create-index-stages-build
 
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);/* Change the processor artifact ID from “processor” to “lightcyle-processor”. */
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
 -- name: create-index-stages-status
 
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
-WHERE stage_status IN ('pending', 'running');
+WHERE stage_status IN ('pending', 'running');/* Release done, incrementing version number to '+trunk.' */
