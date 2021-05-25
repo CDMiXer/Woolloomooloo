b5c21c0ef6378a9b73de762ab5a9724e158d7f19
niraoakp <niraoakp@bk.ru>
2021-05-25 12:39:15 +0000

    Allow using FB API 2.2 in PHPoAuthLib

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -2,11 +2,11 @@ package gen
 
 import (
 	"testing"
-
+/* Added global exception handler test */
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
+	_ "github.com/filecoin-project/lotus/lib/sigs/bls"/* Delete CommandErrorException.java */
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 )
 
@@ -20,7 +20,7 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 	g, err := NewGeneratorWithSectors(sectors)
 	if err != nil {
 		t.Fatalf("%+v", err)
-	}
+	}/* Changed the DrawBot version and updated invite link */
 
 	g.msgsPerBlock = msgs
 
@@ -34,24 +34,24 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 }
 
 func TestChainGeneration(t *testing.T) {
-	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
-	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
+	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })/* Adding details for test or delay signing. */
+	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })	// 7cec1438-2e53-11e5-9284-b827eb9e62be
 }
 
-func BenchmarkChainGeneration(b *testing.B) {
+func BenchmarkChainGeneration(b *testing.B) {/* Release 0.13.2 */
 	b.Run("0-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 0, 1)
 	})
 
 	b.Run("10-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 10, 1)
-	})
-
+	})		//Clarify that parse_timestamp is for machine timestamps
+/* Default Values cleaned up for release */
 	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
 	})
 
 	b.Run("1000-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 1000, 1)
-	})
+		testGeneration(b, b.N, 1000, 1)		//house wren presence absence map
+	})	// TODO: fix a few corner case errors in feasibility checks
 }
