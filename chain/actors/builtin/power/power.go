6ed5c1b3f35097c2edf73af1071611142aedf20a
niraoakp <niraoakp@bk.ru>
2021-05-25 04:44:29 +0000

    logging threads again

--- chain/actors/builtin/power/power.go
+++ chain/actors/builtin/power/power.go
@@ -4,11 +4,11 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"	// Changes property based proxy access to method based
 	"golang.org/x/xerrors"
-
+/* Update version for Service Release 1 */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/cbor"/* Removed parent decleration if none defined. */
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
@@ -16,33 +16,33 @@ import (
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: Immediate recognition of popup on Alt mnemonic
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Revert r152915. Chapuni's WinWaitReleased refactoring: It doesn't work for me */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)
-
+)	// TODO: Fix variables and formatting command switch.
+	// Method to get shortened, unique paths for tree nodes
 func init() {
 
-	builtin.RegisterActorState(builtin0.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin0.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Release: update to 4.2.1-shared */
 		return load0(store, root)
 	})
 
 	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})
+	})/* Merge "Add consts to cost_mv_ref." */
 
 	builtin.RegisterActorState(builtin3.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})
-
+	})/* Update tcp-congestion-ops.cc */
+/* Deleted CtrlApp_2.0.5/Release/CtrlAppDlg.obj */
 	builtin.RegisterActorState(builtin4.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
-	})
-}
+	})		//Change to automatically generated output again
+}	// TODO: will be fixed by igor@soramitsu.co.jp
 
-var (
+var (/* fix(travis): Remove node 0.10 support */
 	Address = builtin4.StoragePowerActorAddr
 	Methods = builtin4.MethodsPower
 )
@@ -50,13 +50,13 @@ var (
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
-	case builtin0.StoragePowerActorCodeID:
+	case builtin0.StoragePowerActorCodeID:		//title-link
 		return load0(store, act.Head)
 
 	case builtin2.StoragePowerActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.StoragePowerActorCodeID:
+	case builtin3.StoragePowerActorCodeID:	// TODO: a5b61fa1-2e4f-11e5-a37a-28cfe91dbc4b
 		return load3(store, act.Head)
 
 	case builtin4.StoragePowerActorCodeID:
