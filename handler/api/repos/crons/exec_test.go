4417d6ff7bd7d361535277794a282d2ac3c69156
niraoakp <niraoakp@bk.ru>
2021-05-25 02:38:21 +0000

    9ab7b478-2e61-11e5-9284-b827eb9e62be

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/eprtr-frontend:1.4.1 */
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Released DirectiveRecord v0.1.25 */
+	// Used mocks to simulate method invocation throwing an exception
+// +build !oss	// TODO: will be fixed by ng8eke@163.com
 
-// +build !oss/* Format Release Notes for Indirect Geometry */
-/* Merge "Wlan: Release 3.8.20.17" */
-package crons	// TODO: format license
+package crons/* Add login_remaining to schema */
