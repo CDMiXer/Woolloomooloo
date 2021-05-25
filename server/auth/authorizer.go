e2e07e0f4302a6a8e36577a08aa5f98c4d9b84b0
niraoakp <niraoakp@bk.ru>
2021-05-25 09:22:23 +0000

    Fixing of sccp text suit - delay increasing

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
 package auth
-/* Additional MC + Gendustry */
-import (
+		//Added new functions to start/stop productionsites to LuaMap and fixed the test.
+import (/* Synch patchlevel in Makefile w/ `Release' tag in spec file. */
 	"context"
-
+/* 1e14a024-2e62-11e5-9284-b827eb9e62be */
 	authUtil "github.com/argoproj/argo/util/auth"
-)/* Release of eeacms/eprtr-frontend:0.4-beta.7 */
-
+)/* Update secret_services.md */
+	// TODO: typo isstruct should be iscell
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
-		return false, err
+		return false, err	// TODO: will be fixed by vyzo@hackzen.org
 	}
 	return allowed, nil
 }
