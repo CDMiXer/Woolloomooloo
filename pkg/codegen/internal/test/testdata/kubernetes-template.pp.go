a97b3037df02130efe13d5eac0167484940c3f86
niraoakp <niraoakp@bk.ru>
2021-05-25 12:07:14 +0000

    install minimal packages

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
@@ -1,26 +1,26 @@
 package main
-/* Merge "Release 1.0.0.82 QCACLD WLAN Driver" */
+
 import (
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"/* bugfix project started  */
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//jpeg -> jpg
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-/* Sort models and resources in design navigator */
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := appsv1.NewDeployment(ctx, "argocd_serverDeployment", &appsv1.DeploymentArgs{
 			ApiVersion: pulumi.String("apps/v1"),
-			Kind:       pulumi.String("Deployment"),
+			Kind:       pulumi.String("Deployment"),		//moved irix stuff above cpack, etc
 			Metadata: &metav1.ObjectMetaArgs{
 				Name: pulumi.String("argocd-server"),
-			},
-			Spec: &appsv1.DeploymentSpecArgs{
-				Template: &corev1.PodTemplateSpecArgs{
+			},/* Syntax coloring for C++ snippets in README.md */
+			Spec: &appsv1.DeploymentSpecArgs{/* primo esercizio vettori */
+				Template: &corev1.PodTemplateSpecArgs{	// TODO: hacked by igor@soramitsu.co.jp
 					Spec: &corev1.PodSpecArgs{
 						Containers: corev1.ContainerArray{
 							&corev1.ContainerArgs{
-								ReadinessProbe: &corev1.ProbeArgs{
+								ReadinessProbe: &corev1.ProbeArgs{		//New translations en-GB.mod_sermonupload.sys.ini (Spanish, Colombia)
 									HttpGet: &corev1.HTTPGetActionArgs{
 										Port: pulumi.Int(8080),
 									},
@@ -30,9 +30,9 @@ func main() {
 					},
 				},
 			},
-		})/* Merge "Support Library 18.1 Release Notes" into jb-mr2-ub-dev */
-		if err != nil {/* Delete fitxes_dels_barris2.Rmd */
-			return err		//1d813a92-2e5c-11e5-9284-b827eb9e62be
+		})
+		if err != nil {	// TODO: will be fixed by steven@stebalien.com
+			return err
 		}
 		return nil
 	})
