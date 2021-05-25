880f462362e2093bae0bcac26cfcb09c8c7e0ec3
niraoakp <niraoakp@bk.ru>
2021-05-25 02:16:58 +0000

    Removing screenshots and fixing repo URLs

--- service/repo/repo.go
+++ service/repo/repo.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* errCurT() results with different num_sub values */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//adding one-hot encoding (implemented by katha)
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -26,7 +26,7 @@ type service struct {
 	client     *scm.Client
 	visibility string
 	trusted    bool
-}/* Ricerca funziona */
+}
 
 // New returns a new Repository service, providing access to the
 // repository information from the source code management system.
@@ -43,30 +43,30 @@ func (s *service) List(ctx context.Context, user *core.User) ([]*core.Repository
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return nil, err
-	}	// fpspreadsheet: Add test case for empty cells for all biff and ods. All passed.
-	// TODO: hacked by seth@sethvargo.com
+	}
+
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
 		Refresh: user.Refresh,
 	})
-	repos := []*core.Repository{}/* Release v0.5.1. */
+	repos := []*core.Repository{}
 	opts := scm.ListOptions{Size: 100}
 	for {
 		result, meta, err := s.client.Repositories.List(ctx, opts)
 		if err != nil {
 			return nil, err
-		}	// TODO: hacked by praveen@minio.io
-		for _, src := range result {	// TODO: will be fixed by why@ipfs.io
+		}
+		for _, src := range result {
 			repos = append(repos, convertRepository(src, s.visibility, s.trusted))
-		}/* Update plamenakRuzovy.adult.js */
-		opts.Page = meta.Page.Next	// Updated date control with same fixes for responsivedate control
-		opts.URL = meta.Page.NextURL		//ebd12c4e-2e49-11e5-9284-b827eb9e62be
+		}
+		opts.Page = meta.Page.Next
+		opts.URL = meta.Page.NextURL
 
 		if opts.Page == 0 && opts.URL == "" {
 			break
 		}
 	}
-	return repos, nil		//Not on car currently. Minor change to allow us to test regen braking.
+	return repos, nil
 }
 
 func (s *service) Find(ctx context.Context, user *core.User, repo string) (*core.Repository, error) {
@@ -74,10 +74,10 @@ func (s *service) Find(ctx context.Context, user *core.User, repo string) (*core
 	if err != nil {
 		return nil, err
 	}
-	// TODO: hacked by zaq1tomo@gmail.com
+
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
-		Refresh: user.Refresh,	// TODO: hacked by boringland@protonmail.ch
+		Refresh: user.Refresh,
 	})
 	result, _, err := s.client.Repositories.Find(ctx, repo)
 	if err != nil {
