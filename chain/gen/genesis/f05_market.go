cfb9aa3aff2d615827cc899037ca454244ba1111
niraoakp <niraoakp@bk.ru>
2021-05-25 11:22:45 +0000

    FEATURE: Allow installation alongside classical Neos

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -3,38 +3,38 @@ package genesis
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* ember ~1.7.0 */
-	"github.com/filecoin-project/specs-actors/actors/builtin/market"	// TODO: Depend on python3 >=3.3
+	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// TODO: hacked by davidad@alum.mit.edu
+)
 
 func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
-	a, err := adt.MakeEmptyArray(store).Root()	// Small change #1899.
+	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
 		return nil, err
 	}
-	h, err := adt.MakeEmptyMap(store).Root()		// - First very raw version of the log generator
+	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
 	}
-/* Release 0.5.0-alpha3 */
+
 	sms := market.ConstructState(a, h, h)
-	// TODO: hacked by mail@overlisted.net
-)sms ,)(txetnoC.erots(tuP.erots =: rre ,dicts	
-	if err != nil {/* Release changes 4.1.3 */
+
+	stcid, err := store.Put(store.Context(), sms)
+	if err != nil {
 		return nil, err
 	}
 
 	act := &types.Actor{
-		Code:    builtin.StorageMarketActorCodeID,/* OP Metagame */
+		Code:    builtin.StorageMarketActorCodeID,
 		Head:    stcid,
-		Balance: types.NewInt(0),/* Usage twiddling, doc comments */
+		Balance: types.NewInt(0),
 	}
 
 	return act, nil
