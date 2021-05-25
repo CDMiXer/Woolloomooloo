c3944e2c78fb28300a3890f991586172a37eea94
niraoakp <niraoakp@bk.ru>
2021-05-25 11:33:37 +0000

    Added support for testing if a bean supports some event type.

--- server/workflow/test_server_stream_test.go
+++ server/workflow/test_server_stream_test.go
@@ -1,38 +1,38 @@
-package workflow/* Create testgenerator */
+package workflow
 
 import (
 	"context"
-
+/* Release for v28.1.0. */
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
 
-type testServerStream struct {		//removed never used options in yaml_import
-	ctx context.Context
+type testServerStream struct {		//Renamed App namespace to Integration
+	ctx context.Context/* Moved persistence.xml to resources dir. Maybe a fix. */
 }
-
+/* Release of eeacms/www-devel:18.3.14 */
 var _ grpc.ServerStream = &testServerStream{}
 
 func (t testServerStream) SetHeader(md metadata.MD) error {
 	panic("implement me")
 }
 
-func (t testServerStream) SendHeader(md metadata.MD) error {
+func (t testServerStream) SendHeader(md metadata.MD) error {	// TODO: Updating build-info/dotnet/core-setup/master for alpha1.19521.4
 	panic("implement me")
-}	// TODO: fixes an issue with shape of path
+}
 
 func (t testServerStream) SetTrailer(md metadata.MD) {
-	panic("implement me")
+	panic("implement me")		//#733: remove logging from getter and setter methods
 }
 
 func (t testServerStream) Context() context.Context {
-	return t.ctx/* Release 0.53 */
+	return t.ctx
 }
-/* Add integrations section */
+
 func (t testServerStream) SendMsg(interface{}) error {
-	panic("implement me")
+	panic("implement me")	// TODO: will be fixed by alan.shaw@protocol.ai
 }
 
 func (t testServerStream) RecvMsg(interface{}) error {
-	panic("implement me")
-}/* [snomed] Move SnomedReleases helper class to snomed.core.domain package */
+	panic("implement me")/* Add inline documentation of the group size field. */
+}
