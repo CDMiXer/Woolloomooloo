f826d0dcb4cb8d923990b63e2625065932275523
niraoakp <niraoakp@bk.ru>
2021-05-25 19:08:01 +0000

    Implement SetStatic*Field

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints	// Delete WAAEexecutejil.java
 
 import (
 	"testing"
-
+	// Merge branch 'master' into negar/default_stake
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
-// TestDependencySteps tests a case where the dependency graph between two
+/* SortedIntrusiveForwardList: fix swap() function */
+// TestDependencySteps tests a case where the dependency graph between two/* fixed some typo's in data-help */
 // resources is inverted between updates. The snapshot should be robust to this
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
 func TestDependencySteps(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+	integration.ProgramTest(t, &integration.ProgramTestOptions{	// Expanded Javadoc for `Reflection`
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{
+{			
 				Dir:      "step2",
 				Additive: true,
-			},
+			},		//change guestion language
 		},
-	})
+	})	// TODO: will be fixed by alex.gaynor@gmail.com
 }
