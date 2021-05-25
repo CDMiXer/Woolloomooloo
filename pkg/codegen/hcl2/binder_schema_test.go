b7397b0b0ebaed00f45839124975b8f354e6f5a4
niraoakp <niraoakp@bk.ru>
2021-05-25 10:11:23 +0000

    Create Post “datacite’s-first-virtual-member-meetings”

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,18 +1,18 @@
 package hcl2
 
 import (
-	"testing"		//trigger new build for ruby-head (e9f7e1c)
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)		//358baa46-2e4d-11e5-9284-b827eb9e62be
-
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: will be fixed by why@ipfs.io
+)		//Update elk_start_script
+/* Release script updated. */
 func BenchmarkLoadPackage(b *testing.B) {
-	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
+	loader := schema.NewPluginLoader(test.NewHost(testdataPath))/* Log should now work in Node (i.e. without browser), but not tested */
 
-	for n := 0; n < b.N; n++ {/* to be found */
+	for n := 0; n < b.N; n++ {
 		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
 		contract.AssertNoError(err)
 	}
-}		//[tools/colorspace conversion] added preliminary CMYK support (hidden)
+}
