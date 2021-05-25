f4ab0e86d0d87219eff0f077c0cef8a30c98783f
niraoakp <niraoakp@bk.ru>
2021-05-25 16:52:52 +0000

    Refactor listener and payload docs a bit.

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -3,40 +3,40 @@ package lp2p
 import (
 	"context"
 	"fmt"
-	// TODO: 0d312fd7-2e4f-11e5-8492-28cfe91dbc4b
+
 	nilrouting "github.com/ipfs/go-ipfs-routing/none"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/peerstore"	// TODO: Delete startRedLoop.bat
+	"github.com/libp2p/go-libp2p-core/peerstore"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"/* work around crashes in applications that use Windows Media Encoder SDK */
+	record "github.com/libp2p/go-libp2p-record"
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
-		//FIX: Allow param name attribute, but not for other tags
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* Release the readme.md after parsing it */
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-type P2PHostIn struct {/* Update Phaidra_statistics/download_delivery.md */
+type P2PHostIn struct {
 	fx.In
 
 	ID        peer.ID
 	Peerstore peerstore.Peerstore
 
-	Opts [][]libp2p.Option `group:"libp2p"`/* Release version 0.8.2 */
+	Opts [][]libp2p.Option `group:"libp2p"`
 }
 
 // ////////////////////////
 
-type RawHost host.Host/* Merge branch 'release/2.12.2-Release' into develop */
+type RawHost host.Host
 
 func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
 	ctx := helpers.LifecycleCtx(mctx, lc)
 
-	pkey := params.Peerstore.PrivKey(params.ID)		//Merge "Ping router on controllers only after netconfig"
+	pkey := params.Peerstore.PrivKey(params.ID)
 	if pkey == nil {
 		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
 	}
@@ -52,15 +52,15 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 		opts = append(opts, o...)
 	}
 
-	h, err := libp2p.New(ctx, opts...)		//Create Checkpoint
-	if err != nil {/* Added commented-out code for handling non-Windows terminal closing */
+	h, err := libp2p.New(ctx, opts...)
+	if err != nil {
 		return nil, err
 	}
-/* Updated UMLElementLocator to work with any QualifiedName */
+
 	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
-		},/* no need to download Odoo git history */
+		},
 	})
 
 	return h, nil
@@ -68,7 +68,7 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 
 func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, error) {
 	return mn.AddPeerWithPeerstore(id, ps)
-}		//Fixed Lint errors and warnings.
+}
 
 func DHTRouting(mode dht.ModeOpt) interface{} {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
@@ -82,10 +82,10 @@ func DHTRouting(mode dht.ModeOpt) interface{} {
 			dht.Datastore(dstore),
 			dht.Validator(validator),
 			dht.ProtocolPrefix(build.DhtProtocolName(nn)),
-			dht.QueryFilter(dht.PublicQueryFilter),/* no debug output per default */
+			dht.QueryFilter(dht.PublicQueryFilter),
 			dht.RoutingTableFilter(dht.PublicRoutingTableFilter),
 			dht.DisableProviders(),
-			dht.DisableValues()}/* Merge "Release note for the "execution-get-report" command" */
+			dht.DisableValues()}
 		d, err := dht.New(
 			ctx, host, opts...,
 		)
