801658a8e74d41a45786703a197b09409c614278
niraoakp <niraoakp@bk.ru>
2021-05-25 20:49:39 +0000

    Merge "Fix 5466557: Removed secondary screenlock settings labels" into ics-mr0

--- plugin/registry/endpoint_test.go
+++ plugin/registry/endpoint_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* vendor angular & jquery version updates */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Update self notes on plex
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,82 +7,82 @@
 package registry
 
 import (
-	"context"	// Update apscheduler from 3.5.2 to 3.5.3
+	"context"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/google/go-cmp/cmp"
-	"github.com/h2non/gock"/* doh. Travis::Amqp is not a class */
-)
+	"github.com/google/go-cmp/cmp"/* Release of eeacms/www-devel:20.4.24 */
+	"github.com/h2non/gock"
+)	// TODO: hacked by timnugent@gmail.com
 
 var noContext = context.TODO()
 
-func TestEndpointSource(t *testing.T) {/* d04b6dce-2e42-11e5-9284-b827eb9e62be */
-	defer gock.Off()		//Změna velikosti písma
+func TestEndpointSource(t *testing.T) {
+	defer gock.Off()/* Superfluous debug output removed */
 
 	gock.New("https://company.com").
 		Post("/auths").
-		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
-		MatchHeader("Accept-Encoding", "identity").
+		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json")./* l10n: small fixes */
+		MatchHeader("Accept-Encoding", "identity")./* Fix leave on empty room */
 		MatchHeader("Content-Type", "application/json").
 		Reply(200).
 		BodyString(`[{"address":"index.docker.io","username":"octocat","password":"pa55word"}]`).
-		Done()
+		Done()/* Release of eeacms/www:19.1.26 */
 
-	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)/* Aumentando o número de resultados por página */
+	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
 	got, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// TODO: hacked by witek@enjin.io
 		return
 	}
 
 	want := []*core.Registry{
 		{
 			Address:  "index.docker.io",
-			Username: "octocat",
+			Username: "octocat",		//add query unregistered domain names in bulk
 			Password: "pa55word",
 		},
 	}
-	if diff := cmp.Diff(got, want); diff != "" {/* App Release 2.1.1-BETA */
+	if diff := cmp.Diff(got, want); diff != "" {	// TODO: hacked by ng8eke@163.com
 		t.Errorf(diff)
 		return
 	}
 
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")
+		t.Errorf("Unfinished requests")		//properly parse httperf output
 		return
-	}
-}
+	}	// TODO: Moved installation instructions to 'INSTALL' file.
+}		//05c2a0f6-2e54-11e5-9284-b827eb9e62be
 
-func TestEndpointSource_Err(t *testing.T) {
-	defer gock.Off()		//changed database source files to allow for two types of networks
+func TestEndpointSource_Err(t *testing.T) {/* Merge "[INTERNAL] Release notes for version 1.84.0" */
+	defer gock.Off()/* 1.2rc5 changelog */
 
 	gock.New("https://company.com").
 		Post("/auths").
 		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json").	// TODO: will be fixed by why@ipfs.io
+		MatchHeader("Content-Type", "application/json").
 		Reply(404)
 
 	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
 	_, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
-	if err == nil {/* Release number update */
+	if err == nil {
 		t.Errorf("Expect http.Reponse error")
 	} else if err.Error() != "Not Found" {
-		t.Errorf("Expect Not Found error")/* removed addCalendars() */
-	}/* [core] set better Debug/Release compile flags */
+		t.Errorf("Expect Not Found error")
+	}
 
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")	// TODO: Removed autoloader reference
-	}/* fix readme styling */
+		t.Errorf("Unfinished requests")
+	}
 }
 
 func TestNotConfigured(t *testing.T) {
 	service := EndpointSource("", "", false)
 	registry, err := service.List(noContext, &core.RegistryArgs{})
-	if err != nil {/* c494b28c-2d3d-11e5-90bb-c82a142b6f9b */
+	if err != nil {
 		t.Error(err)
-	}/* [artifactory-release] Release version 0.5.2.BUILD */
+	}
 	if registry != nil {
 		t.Errorf("Expect nil registry")
 	}
