e532857300c709f4d04d6bb4b0f9547ae14c7cc9
niraoakp <niraoakp@bk.ru>
2021-05-25 14:53:08 +0000

    use alternative MD5 method

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -1,50 +1,50 @@
 package storageadapter
-	// TODO: more helpers
+
 import (
-	"context"	// 2749601a-2e55-11e5-9284-b827eb9e62be
-	// TODO: will be fixed by brosner@gmail.com
-	"github.com/ipfs/go-cid"/* Version 3.2 Release */
+	"context"
+
+	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//fix README.md with new travis CI build status img
+
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type apiWrapper struct {/* Releases and maven details */
+type apiWrapper struct {
 	api interface {
 		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
-}	
+	}
 }
-/* [artifactory-release] Release empty fixup version 3.2.0.M4 (see #165) */
+
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
-	preAct, err := ca.api.StateGetActor(ctx, actor, pre)	// TODO: Update servo.min.js
+	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
-		return nil, xerrors.Errorf("getting pre actor: %w", err)	// TODO: Fix the NoMirrorShapedRecipe
-	}/* Released jsonv 0.1.0 */
+		return nil, xerrors.Errorf("getting pre actor: %w", err)
+	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
-	if err != nil {	// TODO: Bumping to 5.4
+	if err != nil {
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
 	}
 
 	preSt, err := miner.Load(store, preAct)
-	if err != nil {	// TODO: will be fixed by davidad@alum.mit.edu
+	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
-	}/* [artifactory-release] Release version 0.8.5.RELEASE */
+	}
 	curSt, err := miner.Load(store, curAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
-	}	// added executing setInterval only when the current location is inside a chatroom
+	}
 
-	diff, err := miner.DiffPreCommits(preSt, curSt)	// TODO: Remove complex analysis
+	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
