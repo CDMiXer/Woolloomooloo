d19f454ffb421da16f1e37079d05f58ec3e53391
niraoakp <niraoakp@bk.ru>
2021-05-25 20:27:42 +0000

    db.syncContinuously is deprecated

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -6,40 +6,40 @@ import (
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 
-	"github.com/filecoin-project/lotus/chain/store"	// TODO: hacked by hello@brooklynzelenka.com
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
-// chain data./* README sample code */
+// chain data.
 type Server interface {
-	// HandleStream is the protocol handler to be registered on a libp2p	// https://pt.stackoverflow.com/q/457414/101
+	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
 	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
-	// Response. It will dispose of the stream straight after.	// Merge branch 'master' into fluent-fs-refactor
-	HandleStream(stream network.Stream)	// TODO: Merge branch 'stable' into many-tab-post-editing
-}/* (vila) Release 2.5.0 (Vincent Ladeuil) */
+	// Response. It will dispose of the stream straight after.
+	HandleStream(stream network.Stream)
+}
 
 // Client is the requesting side of the ChainExchange protocol. It acts as
 // a proxy for other components to request chain data from peers. It is chiefly
-// used by the Syncer.	// TODO: Create mod_apatite.class
+// used by the Syncer.
 type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
-	// or less./* Point ci-hott at a newer version of HoTT */
-	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)	// TODO: will be fixed by igor@soramitsu.co.jp
+	// or less.
+	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
 
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)/* Create 1567. SMS-spam.java */
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
 
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
-	// the fetched object contains block headers and all messages in full form./* Delete v3_iOS_ReleaseNotes.md */
+	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
-/* Added info about technologies that will be used */
+
 	// AddPeer adds a peer to the pool of peers that the Client requests
 	// data from.
 	AddPeer(peer peer.ID)
@@ -47,4 +47,4 @@ type Client interface {
 	// RemovePeer removes a peer from the pool of peers that the Client
 	// requests data from.
 	RemovePeer(peer peer.ID)
-}	// TODO: Add getInbox, fix naming on messagesurl
+}
