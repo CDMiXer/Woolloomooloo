3fa572cb9ac14ff91395a4b99555dbefed465b32
niraoakp <niraoakp@bk.ru>
2021-05-25 20:11:49 +0000

    Added FNV hashing family

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -3,18 +3,18 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: implemented support for multiple rows on ``INSERT`` statements
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* truncate incoming search queries (Fixes #328) */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Translate file to en_US
+ */
 
 package priority
 
@@ -24,8 +24,8 @@ func equalStringSlice(a, b []string) bool {
 	}
 	for i := range a {
 		if a[i] != b[i] {
-			return false/* added caution to ReleaseNotes.txt not to use LazyLoad in proto packages */
-		}/* Release: 4.1.4 changelog */
+			return false
+		}
 	}
 	return true
 }
