0ac7794b3f5cc83e8213b8e1220a586d00a9248e
niraoakp <niraoakp@bk.ru>
2021-05-25 11:09:46 +0000

    Add readme doc for intents

--- core/transfer.go
+++ core/transfer.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.23 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge branch 'master' of github.com:AppStateESS/phpwebsite.git */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by witek@enjin.io
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-/* Update rutubex.php */
+
 // Transferer handles transfering repository ownership from one
 // user to another user account.
-type Transferer interface {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+type Transferer interface {
 	Transfer(ctx context.Context, user *User) error
 }
