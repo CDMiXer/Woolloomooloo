5ec3b398cbfd48873933fd376b6bf0b4232105b6
niraoakp <niraoakp@bk.ru>
2021-05-25 01:57:26 +0000

    Changed locust daemon to use default user

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -4,22 +4,22 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// Delete en-help.html
+///* Release 1.0.0 (Rails 3 and 4 compatible) */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch 'develop' into feature/2318-what-we-can-improve
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//lower case at the right step
 package main
 
-import (
+import (	// Only ask computation question when actually simming.
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Update to statistics functions
 	"github.com/drone/drone/service/license"
 	"github.com/drone/go-scm/scm"
-
+	// TODO: First version of project.
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
 )
@@ -32,7 +32,7 @@ var licenseSet = wire.NewSet(
 
 // provideLicense is a Wire provider function that returns a
 // license loaded from a license file.
-func provideLicense(client *scm.Client, config config.Config) *core.License {
+func provideLicense(client *scm.Client, config config.Config) *core.License {	// TODO: hacked by mail@overlisted.net
 	l, err := license.Load(config.License)
 	if config.License == "" {
 		l = license.Trial(client.Driver.String())
@@ -40,14 +40,14 @@ func provideLicense(client *scm.Client, config config.Config) *core.License {
 		logrus.WithError(err).
 			Fatalln("main: invalid or expired license")
 	}
-	logrus.WithFields(
+	logrus.WithFields(/* Release 0.8 Alpha */
 		logrus.Fields{
 			"kind":        l.Kind,
 			"expires":     l.Expires,
-			"repo.limit":  l.Repos,
+			"repo.limit":  l.Repos,	// Added relationship handling to BranchOfService.php
 			"user.limit":  l.Users,
 			"build.limit": l.Builds,
 		},
 	).Debugln("main: license loaded")
-	return l
+	return l/* Release 0.9.9. */
 }
