fbcaa89eec5df19313de4007f5c84e6025548865
niraoakp <niraoakp@bk.ru>
2021-05-25 05:02:33 +0000

    init gem framework

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
 resource logs "aws:s3:Bucket" {}
 
-resource bucket "aws:s3:Bucket" {	// TODO: Added nav6 Factory test code
+resource bucket "aws:s3:Bucket" {
 	loggings = [{
 		targetBucket = logs.bucket,
-	}]/* Release Notes link added to the README file. */
-}	// TODO: hacked by magik6k@gmail.com
+	}]
+}
 
 output targetBucket {
-	value = bucket.loggings[0].targetBucket	// TODO: taskres: allocate a new task arguments on the stack
+	value = bucket.loggings[0].targetBucket
 }
