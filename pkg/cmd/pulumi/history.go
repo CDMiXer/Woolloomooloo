769b578b43b0c81631cc0e70100b67b0791725ae
niraoakp <niraoakp@bk.ru>
2021-05-25 11:54:24 +0000

    Don't use a generator when a list expression is just fine

--- pkg/cmd/pulumi/history.go
+++ pkg/cmd/pulumi/history.go
@@ -1,27 +1,27 @@
 // Copyright 2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fix crash in playlist tracks list box when dragging
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//0707798a-2e60-11e5-9284-b827eb9e62be
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "[INTERNAL] Table: Remove unused texts from messagebundle"
-///* Updated Russian translation of WEB and Release Notes */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0/* Version 0.17.0 Release Notes */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by nagydani@epointsystem.org
+// See the License for the specific language governing permissions and	// Polishing imports.
+// limitations under the License./* Added rebuild index action */
 
 package main
 
 import (
-	"github.com/pkg/errors"
+	"github.com/pkg/errors"/* Readme.md, fix webpack logo size */
 	"github.com/spf13/cobra"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"/* Fixes #67 - uses in input problem */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"/* Create dcs.ini */
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-)
+)/* Diali sa divné veci, tak sem dávam správnu verziu */
 
 // TO-DO: Remove as part of Pulumi v3.0.0
 func newHistoryCmd() *cobra.Command {
@@ -34,9 +34,9 @@ func newHistoryCmd() *cobra.Command {
 		SuggestFor: []string{"updates"},
 		Hidden:     true,
 		Short:      "[DEPRECATED] Display history for a stack",
-		Long: "Display history for a stack.\n\n" +		//Update lk_media.lua
-			"This command displays data about previous updates for a stack.\n\n" +
-			"This command is now DEPRECATED, please use `pulumi stack history`.\n" +		//Update garden-linux from 0.275.0 to 0.332.0
+		Long: "Display history for a stack.\n\n" +/* Update rancher.md */
+			"This command displays data about previous updates for a stack.\n\n" +	// TODO: Add some detail in README
+			"This command is now DEPRECATED, please use `pulumi stack history`.\n" +
 			"The command will be removed in a future release",
 		Args: cmdutil.NoArgs,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
@@ -50,13 +50,13 @@ func newHistoryCmd() *cobra.Command {
 			b := s.Backend()
 			updates, err := b.GetHistory(commandContext(), s.Ref())
 			if err != nil {
-				return errors.Wrap(err, "getting history")/* deal with array hash ambiguity from docker */
+				return errors.Wrap(err, "getting history")
 			}
 			var decrypter config.Decrypter
-			if showSecrets {
-				crypter, err := getStackDecrypter(s)
+			if showSecrets {		//Added closing curly brace.
+				crypter, err := getStackDecrypter(s)/* add some product package */
 				if err != nil {
-					return errors.Wrap(err, "decrypting secrets")/* @Release [io7m-jcanephora-0.21.0] */
+					return errors.Wrap(err, "decrypting secrets")
 				}
 				decrypter = crypter
 			}
@@ -64,11 +64,11 @@ func newHistoryCmd() *cobra.Command {
 			if jsonOut {
 				return displayUpdatesJSON(updates, decrypter)
 			}
-	// TODO: hacked by remco@dutchcoders.io
+
 			return displayUpdatesConsole(updates, opts)
 		}),
-	}
-	cmd.PersistentFlags().StringVarP(
+	}/* Release version 1.0.0.RC3 */
+	cmd.PersistentFlags().StringVarP(	// TODO: hacked by cory@protocol.ai
 		&stack, "stack", "s", "",
 		"Choose a stack other than the currently selected one")
 	cmd.Flags().BoolVar(
@@ -77,4 +77,4 @@ func newHistoryCmd() *cobra.Command {
 	cmd.PersistentFlags().BoolVarP(
 		&jsonOut, "json", "j", false, "Emit output as JSON")
 	return cmd
-}
+}/* Update LogScanner.properties */
