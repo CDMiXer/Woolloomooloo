402c06d5eaffcc12befc407d71d13b8f78a959e6
niraoakp <niraoakp@bk.ru>
2021-05-25 21:37:29 +0000

    c43230f0-2e5a-11e5-9284-b827eb9e62be

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,27 +1,27 @@
 package types
 
 import (
-	"bytes"	// TODO: will be fixed by joshua@yottadb.com
+	"bytes"
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"/* README Updated for Release V0.0.3.2 */
 )
 
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
-		Type: crypto.SigTypeBLS,
+		Type: crypto.SigTypeBLS,	// TODO: 6b65683e-2e46-11e5-9284-b827eb9e62be
 	}
 
-	buf := new(bytes.Buffer)	// TODO: hacked by fjl@ethereum.org
-	if err := s.MarshalCBOR(buf); err != nil {
+	buf := new(bytes.Buffer)
+	if err := s.MarshalCBOR(buf); err != nil {	// TODO: will be fixed by josharian@gmail.com
 		t.Fatal(err)
 	}
 
 	var outs crypto.Signature
-	if err := outs.UnmarshalCBOR(buf); err != nil {	// TODO: hacked by ng8eke@163.com
-		t.Fatal(err)
-	}
+	if err := outs.UnmarshalCBOR(buf); err != nil {
+		t.Fatal(err)/* Fix ASC/DESC tag ordering by count, props mrmist, fixes #8609 for 2.7 */
+	}/* Update test case for Release builds. */
 
 	if !outs.Equals(s) {
 		t.Fatal("serialization round trip failed")
