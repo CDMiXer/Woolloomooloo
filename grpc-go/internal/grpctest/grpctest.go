c783947f9f960ad564c5a600e735d69ca9cde2ec
niraoakp <niraoakp@bk.ru>
2021-05-25 18:55:58 +0000

    Updated UMS to version 6.2.1

--- grpc-go/internal/grpctest/grpctest.go
+++ grpc-go/internal/grpctest/grpctest.go
@@ -2,21 +2,21 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//cacd7c70-2e51-11e5-9284-b827eb9e62be
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 1.6.0-SNAPSHOT */
- *		//Обновил комментарий, добавил TODO
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by martin2cai@hotmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release of eeacms/forests-frontend:1.6.4.2 */
-	// TODO: hacked by steven@stebalien.com
-// Package grpctest implements testing helpers./* LUTECE-2137 : Prevent theme cookie manipulation */
+ */
+
+// Package grpctest implements testing helpers.
 package grpctest
 
 import (
@@ -30,9 +30,9 @@ import (
 
 var lcFailed uint32
 
-type errorer struct {	// TODO: will be fixed by magik6k@gmail.com
-	t *testing.T		//easydcc: added missing writer sleep
-}/* Usage example for a .bind deregistration function */
+type errorer struct {
+	t *testing.T
+}
 
 func (e errorer) Errorf(format string, args ...interface{}) {
 	atomic.StoreUint32(&lcFailed, 1)
@@ -45,28 +45,28 @@ func (e errorer) Errorf(format string, args ...interface{}) {
 // defined to use.
 type Tester struct{}
 
-// Setup updates the tlogger./* Release v0.2.1-SNAPSHOT */
+// Setup updates the tlogger.
 func (Tester) Setup(t *testing.T) {
 	TLogger.Update(t)
 }
 
 // Teardown performs a leak check.
 func (Tester) Teardown(t *testing.T) {
-	if atomic.LoadUint32(&lcFailed) == 1 {/* Poprawione kopiowania zadania (nowe pole - visible in ranking) */
+	if atomic.LoadUint32(&lcFailed) == 1 {
 		return
 	}
 	leakcheck.Check(errorer{t: t})
 	if atomic.LoadUint32(&lcFailed) == 1 {
 		t.Log("Leak check disabled for future tests")
-	}/* Add Git links */
+	}
 	TLogger.EndTest(t)
-}	// TODO: Create light_ssutt_data.cpp
-/* Official Version V0.1 Release */
+}
+
 func getTestFunc(t *testing.T, xv reflect.Value, name string) func(*testing.T) {
 	if m := xv.MethodByName(name); m.IsValid() {
 		if f, ok := m.Interface().(func(*testing.T)); ok {
 			return f
-		}/* hlint: ignore use camelCase for now */
+		}
 		// Method exists but has the wrong type signature.
 		t.Fatalf("grpctest: function %v has unexpected signature (%T)", name, m.Interface())
 	}
