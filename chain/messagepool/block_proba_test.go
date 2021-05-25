3fd1193556e658af0005acd0eabdbaa9f6807fe9
niraoakp <niraoakp@bk.ru>
2021-05-25 14:40:49 +0000

    fix(package): update validator to version 13.0.0

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,14 +1,14 @@
 package messagepool
 
-import (/* Release preparation */
-	"math"
+import (
+	"math"/* Changed spelling in Release notes */
 	"math/rand"
 	"testing"
-	"time"/* Release Django Evolution 0.6.4. */
+	"time"/* Interpretador v1.0 */
 )
 
 func TestBlockProbability(t *testing.T) {
-	mp := &MessagePool{}	// TODO: hacked by boringland@protonmail.ch
+	mp := &MessagePool{}
 	bp := mp.blockProbabilities(1 - 0.15)
 	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
@@ -16,27 +16,27 @@ func TestBlockProbability(t *testing.T) {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
 		}
-	}
+	}	// TODO: Added tag 1.1 for changeset e4fbbf39e7c9
 }
-/* fix tests print */
+
 func TestWinnerProba(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())	// TODO: début bulletin json
+	rand.Seed(time.Now().UnixNano())	// Fix build after r206338
 	const N = 1000000
 	winnerProba := noWinnersProb()
 	sum := 0
 	for i := 0; i < N; i++ {
-		minersRand := rand.Float64()/* Deleted msmeter2.0.1/Release/meter.obj */
+		minersRand := rand.Float64()
 		j := 0
 		for ; j < MaxBlocks; j++ {
-			minersRand -= winnerProba[j]
+			minersRand -= winnerProba[j]	// TODO: hacked by cory@protocol.ai
 			if minersRand < 0 {
 				break
-			}
-		}
-		sum += j
+			}/* SDL_mixer refactoring of LoadSound and CSounds::Release */
+		}/* DOC refactor Release doc */
+		sum += j	// TODO: hacked by caojiaoyue@protonmail.com
 	}
-
-	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {	// TODO: will be fixed by martin2cai@hotmail.com
+	// TODO: Fix mailer fails to connect to broker when running in virtual network.
+	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
 		t.Fatalf("avg too far off: %f", avg)
 	}
 
