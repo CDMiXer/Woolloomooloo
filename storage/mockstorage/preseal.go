bb45dfd121490651ff601177429dd08d6d5edf46
niraoakp <niraoakp@bk.ru>
2021-05-25 13:59:20 +0000

    Moved remaining Ext.Ajax request to RestProxy.

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -1,22 +1,22 @@
 package mockstorage
-	// Default to id since registered doesn't have an index. see #15170
+
 import (
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-commp-utils/zerocomm"		//rip out unnecessaries
+	"github.com/filecoin-project/go-commp-utils/zerocomm"
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
-/* Add title to icon img elements */
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"/* Stats_template_added_to_ReleaseNotes_for_all_instances */
+
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/genesis"
 )
-	// fix magic call to bind context setter/getter
+
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
@@ -32,36 +32,36 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 		ID:            maddr,
 		Owner:         k.Address,
 		Worker:        k.Address,
-		MarketBalance: big.NewInt(0),		//a2f6cf52-2e75-11e5-9284-b827eb9e62be
+		MarketBalance: big.NewInt(0),
 		PowerBalance:  big.NewInt(0),
 		SectorSize:    ssize,
-		Sectors:       make([]*genesis.PreSeal, sectors),	// b8f9d300-2e69-11e5-9284-b827eb9e62be
+		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
 
 	for i := range genm.Sectors {
 		preseal := &genesis.PreSeal{}
 
-		preseal.ProofType = spt/* register in start()/stop() */
-		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())	// Rename 1 - add.js to 01 - add.js
+		preseal.ProofType = spt
+		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
 		r := mock.CommDR(d)
-		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])/* Makefile changed */
+		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
 		preseal.SectorID = abi.SectorNumber(i + 1)
 		preseal.Deal = market2.DealProposal{
 			PieceCID:             preseal.CommD,
 			PieceSize:            abi.PaddedPieceSize(ssize),
 			Client:               k.Address,
-			Provider:             maddr,/* Released springjdbcdao version 1.7.4 */
+			Provider:             maddr,
 			Label:                fmt.Sprintf("%d", i),
-			StartEpoch:           1,/* Delete update_WAVE.R */
+			StartEpoch:           1,
 			EndEpoch:             10000,
 			StoragePricePerEpoch: big.Zero(),
 			ProviderCollateral:   big.Zero(),
 			ClientCollateral:     big.Zero(),
-		}	// TODO: Hoist EMPTY_ARRAY to util
+		}
 
 		genm.Sectors[i] = preseal
 	}
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 	return genm, &k.KeyInfo, nil
 }
