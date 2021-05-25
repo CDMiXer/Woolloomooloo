3fe6a8b38d012293b97e95f157a9f80668a6bf74
niraoakp <niraoakp@bk.ru>
2021-05-25 12:14:13 +0000

    refactor layout 2-8 into 1 long, 6 short columns

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -2,51 +2,51 @@ package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* Prepare Release REL_7_0_1 */
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Version 0.95f */
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Added dxSetBlendMode for higher quality text drawing to render targets */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
-	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"/* Release v5.4.0 */
+	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
 )
 
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}	// TODO: minor fixes (#1165)
+	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Merge "Replace "hardcode" in scenario tests" */
-	return &out, nil/* test api for element cache */
+	}
+	return &out, nil
 }
-		//cdf1aaae-2e59-11e5-9284-b827eb9e62be
+
 type state4 struct {
 	reward4.State
 	store adt.Store
 }
-	// TODO: hacked by greg@colvin.org
+
 func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
-}/* Update Release notes regarding testing against stable API */
-	// TODO: hacked by steven@stebalien.com
+}
+
 func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
-/* Create rowReduce.html */
+
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
-		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,	// TODO: hacked by alex.gaynor@gmail.com
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
-}/* Release for 18.11.0 */
+}
 
 func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
-}	// TODO: Implements fileExists() under Windows.
+}
 
 func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {
-	return s.State.TotalStoragePowerReward, nil		//thought Health was an enum... but it was a class
+	return s.State.TotalStoragePowerReward, nil
 }
 
 func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
