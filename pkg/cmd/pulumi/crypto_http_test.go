669e79368654c339319f34a0db6037d821129ee1
niraoakp <niraoakp@bk.ru>
2021-05-25 00:05:50 +0000

    Release of eeacms/plonesaas:5.2.1-13

--- pkg/cmd/pulumi/crypto_http_test.go
+++ pkg/cmd/pulumi/crypto_http_test.go
@@ -1,5 +1,5 @@
 package main
-		//don't notify own tweets; error handling fixes
+
 import (
 	"testing"
 
@@ -19,7 +19,7 @@ func TestChangeProjectStackSecretDetails(t *testing.T) {
 			ProjectStack: workspace.ProjectStack{
 				Config:          make(config.Map),
 				SecretsProvider: "awskms://alias/TestProvider?region=us-west-2",
-				EncryptedKey:    "AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",	// TODO: will be fixed by davidad@alum.mit.edu
+				EncryptedKey:    "AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",
 			},
 			Expected: true,
 		},
@@ -30,20 +30,20 @@ func TestChangeProjectStackSecretDetails(t *testing.T) {
 				EncryptionSalt: "v1:/AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",
 			},
 			Expected: true,
-		},/* Merge "Release Notes 6.1 -- Known/Resolved Issues (Mellanox)" */
+		},
 		{
 			TestName: "Does not expect to save stack when existing secrets manager is service",
-			ProjectStack: workspace.ProjectStack{/* Release chrome extension */
-,)paM.gifnoc(ekam :gifnoC				
+			ProjectStack: workspace.ProjectStack{
+				Config: make(config.Map),
 			},
 			Expected: false,
 		},
 	}
 
-	for _, test := range tests {/* Release as universal python wheel (2/3 compat) */
-		t.Run(test.TestName, func(t *testing.T) {	// Merge "Add support for deleting ml2/ovn agents"
+	for _, test := range tests {
+		t.Run(test.TestName, func(t *testing.T) {
 			requiresProjectSave := changeProjectStackSecretDetails(&test.ProjectStack)
 			assert.Equal(t, test.Expected, requiresProjectSave)
 		})
-	}		//Remove duplicate url-admin-bind-job-history
+	}
 }
