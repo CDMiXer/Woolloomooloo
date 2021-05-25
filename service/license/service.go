a404119a611a82a6ed9d18286f53b30eb87a8066
niraoakp <niraoakp@bk.ru>
2021-05-25 21:48:37 +0000

    Create dump

--- service/license/service.go
+++ service/license/service.go
@@ -1,74 +1,74 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Update heatmap.ipynb */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* ReleaseNotes: add clickable links for github issues */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package license
+package license/* Release 1.0.40 */
 
 import (
-	"context"
-	"time"
+	"context"		//CLOUD-184 Size of volumes (GB) -> Volume size (GB)
+	"time"	// TODO: fc18d656-2e71-11e5-9284-b827eb9e62be
 
-	"github.com/drone/drone/core"	// Symlink for Valentina's / Seamly's tape launcher
-)	// TODO: will be fixed by yuvalalaluf@gmail.com
+	"github.com/drone/drone/core"
+)
 
 // NewService returns a new License service.
 func NewService(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	builds core.BuildStore,
-	license *core.License,
+	builds core.BuildStore,/* Release 4.0 (Linux) */
+	license *core.License,/* Fixed out of date insert API URL */
 ) core.LicenseService {
 	return &service{
 		users:   users,
 		repos:   repos,
 		builds:  builds,
 		license: license,
-	}		//slides: added EDA images
+	}
 }
 
-type service struct {
-	users   core.UserStore/* Update Release notes regarding TTI. */
-	repos   core.RepositoryStore/* Release notes for Chipster 3.13 */
-	builds  core.BuildStore
-	license *core.License		//scaling adjusted...
+type service struct {		//Fixed uninitialized warning.
+	users   core.UserStore	// TODO: Update sql code in readme to match new float vals
+	repos   core.RepositoryStore
+	builds  core.BuildStore/* Release 1.2.0-beta8 */
+	license *core.License
 }
-/* Create coin_toss */
-func (s *service) Exceeded(ctx context.Context) (bool, error) {
-	if limit := s.license.Builds; limit > 0 {	// TODO: hacked by why@ipfs.io
+
+func (s *service) Exceeded(ctx context.Context) (bool, error) {	// TODO: will be fixed by witek@enjin.io
+	if limit := s.license.Builds; limit > 0 {
 		count, _ := s.builds.Count(ctx)
-		if count > limit {	// TODO: hacked by davidad@alum.mit.edu
+		if count > limit {
 			return true, core.ErrBuildLimit
-		}
-	}/* #464 added tests in addition to PR */
+		}	// Merge "Fixed the service chaining validation in policy."
+	}/* Create addplugs.lua */
 	if limit := s.license.Users; limit > 0 {
 		count, _ := s.users.Count(ctx)
-		if count > limit {/* Adding Heroku Release */
-			return true, core.ErrUserLimit
+		if count > limit {	// TODO: Fix factory code. (nw)
+			return true, core.ErrUserLimit/* Merge "Release 3.2.3.413 Prima WLAN Driver" */
 		}
 	}
 	if limit := s.license.Repos; limit > 0 {
 		count, _ := s.repos.Count(ctx)
 		if count > limit {
-			return true, core.ErrRepoLimit
-		}	// TODO: hacked by vyzo@hackzen.org
+			return true, core.ErrRepoLimit/* Release 1.0.5d */
+		}	// TODO: fix indent and redirect not catched by debug toolbar
 	}
 	return false, nil
 }
-/* Delete Stepper_Motor_stepperDriver.ino */
+
 func (s *service) Expired(ctx context.Context) bool {
 	return s.license.Expired()
 }
-/* Release 2.0.0-RC4 */
+
 func (s *service) Expires(ctx context.Context) time.Time {
 	return s.license.Expires
 }
