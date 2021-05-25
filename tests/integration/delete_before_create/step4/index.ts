e5e6a3a6206dac9b94d99a32edde4508d431157e
niraoakp <niraoakp@bk.ru>
2021-05-25 15:57:45 +0000

    Collapsible contents (text block)

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Implement subset and superset checks. */
 
-// Setup for the next test.	// Remove commented out travis config
-const a = new Resource("base", { uniqueKey: 1, state: 100 });/* further XQJ improvements */
+// Setup for the next test.
+const a = new Resource("base", { uniqueKey: 1, state: 100 });		//Cleaning up debug outputs
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
