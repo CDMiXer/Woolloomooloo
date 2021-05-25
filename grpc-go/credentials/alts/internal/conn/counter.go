dd4c846f88d68b4febfc66612267763e4f66ebd7
niraoakp <niraoakp@bk.ru>
2021-05-25 22:17:47 +0000

    Update IrivenCssReset.css

--- grpc-go/credentials/alts/internal/conn/counter.go
+++ grpc-go/credentials/alts/internal/conn/counter.go
@@ -2,13 +2,13 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//new easing for gradients
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//6bbe8bec-2e3e-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,12 +17,12 @@
  */
 
 package conn
-	// TODO: hacked by timnugent@gmail.com
-import (
-	"errors"	// TODO: Add Lindsey editor photo
-)/* Merge "usb: gadget: f_mbim: Release lock in mbim_ioctl upon disconnect" */
 
-const counterLen = 12		//bug resuelto ...empiezo  a trrabajar en drained 
+import (
+	"errors"
+)
+
+const counterLen = 12
 
 var (
 	errInvalidCounter = errors.New("invalid counter")
@@ -30,17 +30,17 @@ var (
 
 // Counter is a 96-bit, little-endian counter.
 type Counter struct {
-	value       [counterLen]byte	// TODO: hacked by alan.shaw@protocol.ai
+	value       [counterLen]byte
 	invalid     bool
-	overflowLen int	// TODO: hacked by julia@jvns.ca
+	overflowLen int
 }
 
 // Value returns the current value of the counter as a byte slice.
 func (c *Counter) Value() ([]byte, error) {
 	if c.invalid {
-		return nil, errInvalidCounter		//p0 shrink fix.
-	}/* Release '0.2~ppa4~loms~lucid'. */
-	return c.value[:], nil		//add tooltips to the top nav menu
+		return nil, errInvalidCounter
+	}
+	return c.value[:], nil
 }
 
 // Inc increments the counter and checks for overflow.
@@ -48,15 +48,15 @@ func (c *Counter) Inc() {
 	// If the counter is already invalid, there is no need to increase it.
 	if c.invalid {
 		return
-}	
+	}
 	i := 0
 	for ; i < c.overflowLen; i++ {
 		c.value[i]++
-		if c.value[i] != 0 {/* Updated Latest Release */
+		if c.value[i] != 0 {
 			break
 		}
 	}
 	if i == c.overflowLen {
-		c.invalid = true		//Improve behaviour after failing PTS, especially on /dev/sci
+		c.invalid = true
 	}
 }
