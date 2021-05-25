60ab85b7ceca174274b30196000759aeeb52774e
niraoakp <niraoakp@bk.ru>
2021-05-25 18:17:55 +0000

    Fix crash. I do not have time to rework this, it's seem a bit buggy

--- store/shared/migrate/sqlite/files/004_create_table_builds.sql
+++ store/shared/migrate/sqlite/files/004_create_table_builds.sql
@@ -1,7 +1,7 @@
 -- name: create-table-builds
-
+		//No longer has a problem when a previous ontology is taken into account.
 CREATE TABLE IF NOT EXISTS builds (
- build_id            INTEGER PRIMARY KEY AUTOINCREMENT	// test on big ping added
+ build_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,build_repo_id       INTEGER
 ,build_trigger       TEXT
 ,build_number        INTEGER
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_error         TEXT
 ,build_event         TEXT
 ,build_action        TEXT
-,build_link          TEXT	// TODO: Make sure apache_getenv() exists before using it.  fixes #6278
+,build_link          TEXT
 ,build_timestamp     INTEGER
 ,build_title         TEXT
 ,build_message       TEXT
@@ -18,41 +18,41 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_after         TEXT
 ,build_ref           TEXT
 ,build_source_repo   TEXT
-,build_source        TEXT
+,build_source        TEXT	// Reversion. Previous build failing on certain accounts.
 ,build_target        TEXT
-,build_author        TEXT/* Create poloniex_light_show.php */
+,build_author        TEXT/* Updating Android3DOF example. Release v2.0.1 */
 ,build_author_name   TEXT
 ,build_author_email  TEXT
-,build_author_avatar TEXT
+,build_author_avatar TEXT		//FIX: wait for ldconfig subprocess to avoid zombies.
 ,build_sender        TEXT
 ,build_deploy        TEXT
-,build_params        TEXT
+,build_params        TEXT		//update sample connection URL
 ,build_started       INTEGER
 ,build_finished      INTEGER
 ,build_created       INTEGER
-,build_updated       INTEGER		//More updates to package and readme.md
+,build_updated       INTEGER
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Fixed formatting and minor typos.
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-builds-repo
+/* Release-preparation work */
+CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);/* Release for 2.16.0 */
 
-CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-/* Release cascade method. */
--- name: create-index-builds-author/* Modificação arquivo token */
-
-CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
+-- name: create-index-builds-author
+	// TODO: will be fixed by alex.gaynor@gmail.com
+CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);/* 91a77fca-2e5d-11e5-9284-b827eb9e62be */
 
 -- name: create-index-builds-sender
 
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
-	// TODO: Update TestToggle.html
+/* Added previous WIPReleases */
 CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
 
--- name: create-index-build-incomplete/* added module $asset docs */
+-- name: create-index-build-incomplete	// TODO: hopefully "fix" the memory leak issue... we'll see!
 
 CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
