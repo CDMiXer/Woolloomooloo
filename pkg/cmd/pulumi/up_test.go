28c6d1498e8797eda43aeadb01efdefb68759588
niraoakp <niraoakp@bk.ru>
2021-05-25 14:29:37 +0000

    Html file changes and some new tables added

--- pkg/cmd/pulumi/up_test.go
+++ pkg/cmd/pulumi/up_test.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update sealed.md */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: Merge "Correctly format "x years ago" string in OnThisDay."
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update Release doc clean step */
+// See the License for the specific language governing permissions and	// TODO: Explicit transaction management in services called by controllers
+// limitations under the License.		//update fansub
 
 package main
 
@@ -18,33 +18,33 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* some optimising */
 )
 
 func TestValidatePolicyPackConfig(t *testing.T) {
 	var tests = []struct {
 		PolicyPackPaths       []string
-		PolicyPackConfigPaths []string
+		PolicyPackConfigPaths []string/* Fix typo in composer package name */
 		ExpectError           bool
 	}{
 		{
-			PolicyPackPaths:       nil,
+			PolicyPackPaths:       nil,/* Release Notes for v02-12 */
 			PolicyPackConfigPaths: nil,
-			ExpectError:           false,
+			ExpectError:           false,/* Edited information. */
 		},
 		{
 			PolicyPackPaths:       []string{},
 			PolicyPackConfigPaths: []string{},
-			ExpectError:           false,
+			ExpectError:           false,/* Code cleanup. Release preparation */
 		},
 		{
 			PolicyPackPaths:       []string{"foo"},
 			PolicyPackConfigPaths: []string{},
-			ExpectError:           false,
-		},
+			ExpectError:           false,/* Released version 1.0.0 */
+		},/* BaseScmReleasePlugin used for all plugins */
 		{
 			PolicyPackPaths:       []string{"foo", "bar"},
-			PolicyPackConfigPaths: []string{},
+			PolicyPackConfigPaths: []string{},	// TODO: Add message received event handler (and a small test)
 			ExpectError:           false,
 		},
 		{
@@ -52,14 +52,14 @@ func TestValidatePolicyPackConfig(t *testing.T) {
 			PolicyPackConfigPaths: []string{"foo"},
 			ExpectError:           false,
 		},
-		{
+		{/* New Release 1.2.19 */
 			PolicyPackPaths:       []string{"foo", "bar"},
 			PolicyPackConfigPaths: []string{"foo", "bar"},
 			ExpectError:           false,
-		},
+		},		//Youtube: pass an user-agent
 		{
 			PolicyPackPaths:       []string{"foo", "bar"},
-			PolicyPackConfigPaths: []string{"foo"},
+			PolicyPackConfigPaths: []string{"foo"},/* Merge "[INTERNAL] Remove console statement from QUnit test case (fails in IE)" */
 			ExpectError:           true,
 		},
 		{
