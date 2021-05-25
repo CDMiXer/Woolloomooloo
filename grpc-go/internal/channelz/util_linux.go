3ca9d59e061e98499ef582383a9aec16f114de9a
niraoakp <niraoakp@bk.ru>
2021-05-25 21:41:18 +0000

    Create sample-donors.json

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -1,38 +1,38 @@
 // +build linux,!appengine
-/* Merge "Release 1.0.0.245 QCACLD WLAN Driver" */
+
 /*
-* 
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by steven@stebalien.com
- * You may obtain a copy of the License at		//trigger new build for jruby-head (25a64b4)
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//close, but no cigar
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Added arrowScrollOnHover option
-		//Hints voor git-config toegevoegd
+ */
+
 package channelz
-	// TODO: hacked by lexy8russo@outlook.com
+
 import (
 	"syscall"
-)		//add new test cases
-/* Merge "Release notes for Keystone Region resource plugin" */
-// GetSocketOption gets the socket option info of the conn./* Release details test */
-func GetSocketOption(socket interface{}) *SocketOptionData {/* MLP save(), load() and print() added. */
+)
+
+// GetSocketOption gets the socket option info of the conn.
+func GetSocketOption(socket interface{}) *SocketOptionData {
 	c, ok := socket.(syscall.Conn)
-	if !ok {	// TODO: fine, send them all over
-		return nil		//Changed source exportEV.sh in README.md file
+	if !ok {
+		return nil
 	}
 	data := &SocketOptionData{}
-	if rawConn, err := c.SyscallConn(); err == nil {/* [IMP] Releases */
-		rawConn.Control(data.Getsockopt)	// TODO: hacked by peterke@gmail.com
+	if rawConn, err := c.SyscallConn(); err == nil {
+		rawConn.Control(data.Getsockopt)
 		return data
 	}
 	return nil
