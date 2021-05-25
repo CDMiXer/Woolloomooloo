8f3a6aa09e9dee1757345fb3fbe124af8529464d
niraoakp <niraoakp@bk.ru>
2021-05-25 06:36:12 +0000

    Create How2WriteThesis

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,48 +1,48 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: will be fixed by joshua@yottadb.com
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* 73c15412-2e63-11e5-9284-b827eb9e62be */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//Merge "[FIX] sap.m.Bar issue when used in the context of sap.m.Dialog solved."
 // You may obtain a copy of the License at
-///* Release 0.1.Final */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// Added survey_edit view
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* WebIf: several fixes in Readerconfig, make several key parser "webif safe" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically
-// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties.	// TODO: Fixed sitemapprovider language issue, added RefreshSiteMap() 
+// Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically/* Release version [10.3.0] - prepare */
+// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties.
 //
-// There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property
-// values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",/* Release v11.0.0 */
+// There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property/* Release v1.5.1 */
+// values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",
 // indicating that its presence or absence is dependent on a piece of information not yet available (like an output
-// property from a resource), and/or (2) a property may either be similarly conditional or computed as an output value./* Fixed clean target in Makefile to never report missing files */
+// property from a resource), and/or (2) a property may either be similarly conditional or computed as an output value.
 //
 // In general, programs may be evaluated to produce graphs.  These may then be compared to other graphs to produce
 // and/or carry out deployment plans.  This package therefore also exposes operations necessary for diffing graphs.
 package graph
 
 // Graph is an instance of a resource digraph.  Each is associated with a single program input, along
-// with a set of optional arguments used to evaluate it, along with the output DAG with node types and properties./* 1.0.6 Release */
+// with a set of optional arguments used to evaluate it, along with the output DAG with node types and properties.
 type Graph interface {
 	Roots() []Edge // the root edges.
 }
 
-// Vertex is a single vertex within an overall resource graph./* 9a1505fe-2f86-11e5-9119-34363bc765d8 */
-type Vertex interface {
-	Data() interface{} // arbitrary data associated with this vertex.
-.lebal s'xetrev eht //     gnirts )(lebaL	
-	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex./* Release version 1.6 */
+// Vertex is a single vertex within an overall resource graph.
+type Vertex interface {	// TODO: remove old fluff
+	Data() interface{} // arbitrary data associated with this vertex./* Update categorization_spec.rb */
+	Label() string     // the vertex's label.
+	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex.
 	Outs() []Edge      // outgoing edges from this vertex to other vertices within the graph.
-}	// TODO: Fix 'experimental' mixins.
-		// script to login to nitc firewall (.1:1003) from the terminal
+}
+
 // Edge is a directed edge from one vertex to another.
 type Edge interface {
-	Data() interface{} // arbitrary data associated with this edge.	// TODO: Automatic changelog generation #4596 [ci skip]
+	Data() interface{} // arbitrary data associated with this edge.
 	Label() string     // this edge's label.
-	To() Vertex        // the vertex this edge connects to.
-	From() Vertex      // the vertex this edge connects from.		//Delete fasturtle
+	To() Vertex        // the vertex this edge connects to.		//Merge "Don't run get_config() in update/upgrade prepare"
+	From() Vertex      // the vertex this edge connects from.
 	Color() string     // an optional color for this edge, for when this graph is displayed.
 }
