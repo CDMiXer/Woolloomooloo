a89bab09c385432cdfefc630c81d35be18efd7d2
niraoakp <niraoakp@bk.ru>
2021-05-25 17:56:35 +0000

    handle space in group name

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
 
-import (/* Released v.1.1 */
-	"bytes"	// ICL12 projects: cleanup and move all common properties in common_icl12.props
+import (
+	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"		//edit oundur twitter username
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
-	buf := new(bytes.Buffer)	// TODO: Justinfan Release
+	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
-		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
+		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")/* Release Checklist > Bugs List  */
 	}
 	return buf.Bytes(), nil
-}/* SEMPERA-2846 Release PPWCode.Util.OddsAndEnds 2.3.0 */
+}
