ca2d3dbe49927492d9ddbb763d302b29d423d3b9
niraoakp <niraoakp@bk.ru>
2021-05-25 17:05:00 +0000

    readme: fix macOS instructions

--- tests/examples/dynamic-provider/simple/index.ts
+++ tests/examples/dynamic-provider/simple/index.ts
@@ -1,72 +1,72 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Testing pg 7.4.3 */
-	// TODO: add coveralls and gem version [ci ckip]
+import * as dynamic from "@pulumi/pulumi/dynamic";
+
 class OperatorProvider implements dynamic.ResourceProvider {
     private op: (l: number, r: number) => any;
-/* Release 0.11 */
-    constructor(op: (l: number, r: number) => any) {/* All episodes available #tag */
+/* request: update project url */
+    constructor(op: (l: number, r: number) => any) {
         this.op = op;
-    }
+    }		//Uses better random number generator
 
-    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
-    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
+    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }	// docs: replace absolute URL with relative path
+    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }/* Builder integrates with rhena */
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: this.op(Number(inputs.left), Number(inputs.right)) }); }
-    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: this.op(Number(news.left), Number(news.right)) }); }	// added example link to README
+    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: this.op(Number(news.left), Number(news.right)) }); }
 }
-
-class DivProvider extends OperatorProvider {		//[REF] mail.group: cleaned code
+		//Delete Match.js
+class DivProvider extends OperatorProvider {
     constructor() {
         super((left: number, right: number) => <any>{ quotient: Math.floor(left / right), remainder: left % right });
-    }
+    }/* Add Ruby 3.0 to Travis CI matrix */
 
-    public async check(olds: any, news: any) {/* Return category_ids for /source/ID [Story1457911] */
+    public async check(olds: any, news: any) {
         return {
             inputs: news,
             failures: news.right == 0 ? [ { property: "right", reason: "divisor must be non-zero" } ] : [],
-        }
-    }/* V1.3 Version bump and Release. */
+        }	// Create task_ 9
+    }
 }
 
 class Add extends dynamic.Resource {
     public readonly sum: pulumi.Output<number>;
 
-    private static provider = new OperatorProvider((left: number, right: number) => <any>{ sum: left + right });		//Put way-cooler-bg in background, thanks @valryne
-/* Release version 0.1.5 */
+    private static provider = new OperatorProvider((left: number, right: number) => <any>{ sum: left + right });
+/* Forgot NDEBUG in the Release config. */
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
-        super(Add.provider, name, {left: left, right: right, sum: undefined}, undefined);
+        super(Add.provider, name, {left: left, right: right, sum: undefined}, undefined);	// TODO: Delete android_android_18.xml
     }
-}/* Create Release Planning */
+}/* Merge "wlan: Release 3.2.3.120" */
 
-class Mul extends dynamic.Resource {
+class Mul extends dynamic.Resource {/* Release version 1.5.0 */
     public readonly product: pulumi.Output<number>;
-/* Release 0.3.1. */
+
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ product: left * right });
 
-    constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {/* Release of eeacms/ims-frontend:0.6.5 */
+    constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Mul.provider, name, {left: left, right: right, product: undefined}, undefined);
-    }/* Removed spurious log message. */
+    }
 }
-
+/* bc9e7620-2e67-11e5-9284-b827eb9e62be */
 class Sub extends dynamic.Resource {
     public readonly difference: pulumi.Output<number>;
 
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ difference: left - right });
-	// Fixed Backup Deletion
+
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
-        super(Sub.provider, name, {left: left, right: right, difference: undefined}, undefined);
+        super(Sub.provider, name, {left: left, right: right, difference: undefined}, undefined);/* Merge "Merge tag 'v0.4.0'" */
     }
 }
 
 class Div extends dynamic.Resource {
     public readonly quotient: pulumi.Output<number>;
     public readonly remainder: pulumi.Output<number>;
-		//added additional feature for testing
+/* Update PrepareReleaseTask.md */
     private static provider = new DivProvider();
 
-    constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
+    constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {/* Change aex to axExit. */
         super(Div.provider, name, {left: left, right: right, quotient: undefined, remainder: undefined}, undefined);
     }
 }
@@ -76,7 +76,7 @@ let w = Number(config.require("w")), x = Number(config.require("x")), y = Number
 let sum = new Add("sum", x, y);
 let square = new Mul("square", sum.sum, sum.sum);
 let diff = new Sub("diff", square.product, w);
-let divrem = new Div("divrem", diff.difference, sum.sum);
+let divrem = new Div("divrem", diff.difference, sum.sum);/* Release woohoo! */
 let result = new Add("result", divrem.quotient, divrem.remainder);
 export let outputSum: pulumi.Output<number> = result.sum;
 result.sum.apply(result => {
