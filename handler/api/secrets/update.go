a10f3a32a947ff33f2802b182f895dbd8404ceaf
niraoakp <niraoakp@bk.ru>
2021-05-25 12:55:59 +0000

    Create hw3.py

--- handler/api/secrets/update.go
+++ handler/api/secrets/update.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release bdm constraint source and dest type" */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -13,47 +13,47 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"		//Create rjec-mfsg.ini
-)		//project - add powerbuilder to c converter (initial)
-		//Exception text not in use, close #174
+	"github.com/go-chi/chi"
+)
+
 type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
 	PullRequestPush *bool   `json:"pull_request_push"`
-}/* Release v0.03 */
+}
 
 // HandleUpdate returns an http.HandlerFunc that processes http
-// requests to update a secret./* Merge "[doc] Release Victoria" */
-func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {/* Release 2.4.10: update sitemap */
+// requests to update a secret.
+func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Released 0.9.1 Beta */
+		var (
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
-/* Remove outdated and unnecessary Cookies mode */
+
 		in := new(secretUpdate)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}/* Merge "Release 1.0.0.209 QCACLD WLAN Driver" */
+		}
 
-		s, err := secrets.FindName(r.Context(), namespace, name)/* Merge "Release 3.2.3.460 Prima WLAN Driver" */
+		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-/* Existence of shelves should not prevent releases */
+
 		if in.Data != nil {
 			s.Data = *in.Data
 		}
 		if in.PullRequest != nil {
 			s.PullRequest = *in.PullRequest
-		}	// TODO: Fix broken Azure destroy_environment.
+		}
 		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
 		}
-	// a735ac74-2e5b-11e5-9284-b827eb9e62be
+
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
@@ -63,9 +63,9 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {/* Release 2
 		err = secrets.Update(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
-			return/* Release for the new V4MBike with the handlebar remote */
+			return
 		}
-/* Release 0.14.4 */
+
 		s = s.Copy()
 		render.JSON(w, s, 200)
 	}
