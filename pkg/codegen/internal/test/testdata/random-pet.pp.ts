b54c27d08fb51c1abbfbae28e1afe7e23a71fa17
niraoakp <niraoakp@bk.ru>
2021-05-25 16:51:11 +0000

    In Python, how to define a for loop inline?

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as random from "@pulumi/random";
+import * as random from "@pulumi/random";/* Merge "Release Notes 6.0 -- Other issues" */
 
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
