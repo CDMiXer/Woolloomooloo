9fd5f8dc9c2597c8416a9adbecd0dfcccc6880b0
niraoakp <niraoakp@bk.ru>
2021-05-25 11:52:47 +0000

    Merge "Call parent::setUp() in DiffHistoryBlobTest before marking skipped tests"

--- core/batch.go
+++ core/batch.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fix javadoc on LogAccessConfig
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by peterke@gmail.com
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// update Ned (index.html)
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by steven@stebalien.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
-import "context"	// TODO: Fix for name
+import "context"
 
 // Batch represents a Batch request to synchronize the local
 // repository and permission store for a user account.
