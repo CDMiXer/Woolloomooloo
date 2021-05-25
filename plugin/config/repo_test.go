2e139817995c8ffed1187397f5cf3f46bebd0212
niraoakp <niraoakp@bk.ru>
2021-05-25 18:03:38 +0000

    Delete pharma.md

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,58 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by hugomrdias@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package config
 
-import (/* Updated: far 3.0.5475.1172 */
-	"context"
+import (
+	"context"/* disable smooth rng */
 	"errors"
-	"testing"		//Create glaciacommands.js
-
-	"github.com/drone/drone/core"	// ADD test to select builder
+	"testing"
+/* scan-build: enable C++ support by default. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-
+		//need to add another section
 	"github.com/golang/mock/gomock"
-)
+)	// TODO: will be fixed by m-ou.se@m-ou.se
 
-var noContext = context.TODO()	// TODO: Merge branch 'development' into 20589-update-iceberg-to-062
+var noContext = context.TODO()
 
 var mockFile = []byte(`
 kind: pipeline
 name: default
-
+		//IMGAPI-296: Need to create amon probes for image creation failures
 steps: []
 `)
 
 func TestRepository(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-		//Informative exceptions here and there.
-	args := &core.ConfigArgs{
-		User:   &core.User{Login: "octocat"},
-		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},	// TODO: hacked by sebastian.tharakan97@gmail.com
-		Build:  &core.Build{After: "6d144de7"},
-		Config: nil,
-	}		//Create Pip_testbed.ino
-
-	resp := &core.File{Data: mockFile}
-
-	files := mock.NewMockFileService(controller)
-	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)/* [ADD] contact */
-
-	service := Repository(files)
-	result, err := service.Find(noContext, args)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if result.Data != string(resp.Data) {/* Resolves #2 */
-		t.Errorf("unexpected file contents")	// TODO: hacked by hi@antfu.me
-	}
-}
-
-func TestRepositoryErr(t *testing.T) {
-	controller := gomock.NewController(t)/* Add response status handling and new events. */
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
@@ -62,14 +35,41 @@ func TestRepositoryErr(t *testing.T) {
 		Config: nil,
 	}
 
-	resp := errors.New("")
-		//next neighbour
+	resp := &core.File{Data: mockFile}
+
+	files := mock.NewMockFileService(controller)
+	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
+
+	service := Repository(files)
+	result, err := service.Find(noContext, args)
+	if err != nil {
+		t.Error(err)
+	}
+	// TODO: will be fixed by lexy8russo@outlook.com
+	if result.Data != string(resp.Data) {
+		t.Errorf("unexpected file contents")	// TODO: resolve no scope
+	}
+}
+
+func TestRepositoryErr(t *testing.T) {	// TODO: hacked by why@ipfs.io
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	args := &core.ConfigArgs{	// TODO: hacked by fjl@ethereum.org
+		User:   &core.User{Login: "octocat"},
+		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Build:  &core.Build{After: "6d144de7"},
+		Config: nil,/* update basic example */
+	}/* Prepare Release 2.0.12 */
+
+	resp := errors.New("")/* describing attribute values should only query once upon multiple invocations */
+
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)
 
-	service := Repository(files)		//copyFile correction test
+	service := Repository(files)		//fixed bad test name
 	_, err := service.Find(noContext, args)
 	if err != resp {
 		t.Errorf("expect error returned from file service")
-	}
+	}		//[FIX] Issue #2064
 }
