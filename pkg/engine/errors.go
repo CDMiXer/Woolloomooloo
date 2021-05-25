5dd7757f66d41ecfbcd942c6998da16a7eeb4ace
niraoakp <niraoakp@bk.ru>
2021-05-25 14:51:20 +0000

    added url metadata tag

--- pkg/engine/errors.go
+++ pkg/engine/errors.go
@@ -3,16 +3,16 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release notes for 1.0.9 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Exclude log files from npm package
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine
+package engine		//Remove redundant qualifiers
 
 import (
 	"fmt"
@@ -22,19 +22,19 @@ import (
 
 //
 // This file contains type definitions for errors that can arise in the engine that the CLI layer would
-// like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them
+// like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them/* Release 0.3.1 */
 // and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()
 // implementation of these types.
 //
 
-// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.
+// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.		//Normalize filter implementation (a constrast enhancer) 
 // The most common reason why this happens is that this key is being decrypted in a stack that's not the same
 // one that encrypted it.
-type DecryptError struct {
+type DecryptError struct {	// TODO: Filter some more irrelevant characters from search query.
 	Key config.Key // The configuration key whose value couldn't be decrypted
 	Err error      // The error that occurred while decrypting
 }
-
+	// TODO: Merge branch 'develop' into feaute/ligthweight-headers-codec
 func (d DecryptError) Error() string {
 	return fmt.Sprintf("failed to decrypt configuration key '%s': %s", d.Key, d.Err.Error())
 }
