0935772e6f7495874ef498d3406518e1847a166c
niraoakp <niraoakp@bk.ru>
2021-05-25 19:35:44 +0000

    Merge branch 'master' of https://github.com/mwjmurphy/Axel-Framework.git

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Create publishAARToThirdpartyMavenRepository.md
-/* regression test for #1554 */
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package core
