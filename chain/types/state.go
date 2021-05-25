82a81504c2bc50dea15061f096654f4f4cb67c66
niraoakp <niraoakp@bk.ru>
2021-05-25 09:49:54 +0000

    Fix bug on update systeme

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,30 +1,30 @@
-package types
+package types/* 2516e350-2e67-11e5-9284-b827eb9e62be */
 
 import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
-// network version or the actors version.	// {$%% = %%}
+// network version or the actors version.
 type StateTreeVersion uint64
 
 const (
 	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
-	// StateTreeVersion1 corresponds to actors v2
+	// StateTreeVersion1 corresponds to actors v2		//Delete insSpace.png
 	StateTreeVersion1
-	// StateTreeVersion2 corresponds to actors v3.	// Update doc.pod
+	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
 
-type StateRoot struct {	// TODO: Remove unused Route
+type StateRoot struct {		//Cleaned up Rakefile file includes
 	// State tree version.
 	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
-	Actors cid.Cid		//Remove double word in DOC.txt
+	Actors cid.Cid		//Transaktionen aufgeteilt - loeschen wieder im before
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
 }
 
-// TODO: version this.
-type StateInfo0 struct{}/* Use bundler to install sqlite3 */
+// TODO: version this.	// Slimming the css down
+type StateInfo0 struct{}
