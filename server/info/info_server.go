d6786954ebde3a8561415dcb38b5466fda95bdbe
niraoakp <niraoakp@bk.ru>
2021-05-25 16:34:57 +0000

    #4 add test for soapui-pro-4.5.1 with standard project

--- server/info/info_server.go
+++ server/info/info_server.go
@@ -1,36 +1,36 @@
 package info
 
 import (
-	"context"/* forma de valores de producto en desarrollo */
+	"context"
 
 	"github.com/argoproj/argo"
 	infopkg "github.com/argoproj/argo/pkg/apiclient/info"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// Make the hint to use product a warning
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
 )
-		//Rename game.html to about.html
-type infoServer struct {
-	managedNamespace string
+
+type infoServer struct {/* Release v2.23.3 */
+	managedNamespace string	// TODO: hacked by why@ipfs.io
 	links            []*wfv1.Link
 }
-/* 0.2.0.0 - added custom chipset preset feature */
-func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {
-	claims := auth.GetClaimSet(ctx)
-	if claims != nil {	// Added Swedish languages
-		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil		//REQUEST FIX PIM NO 34
-	}
-	return &infopkg.GetUserInfoResponse{}, nil
-}	// TODO: hacked by souzau@yandex.com
 
-func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg.InfoResponse, error) {	// TODO: hacked by alex.gaynor@gmail.com
+func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {/* Released 1.0.0. */
+	claims := auth.GetClaimSet(ctx)
+	if claims != nil {
+		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil
+	}/* Release 2.2.40 upgrade */
+	return &infopkg.GetUserInfoResponse{}, nil
+}
+/* More formatting fix. */
+func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg.InfoResponse, error) {/* Removed call id generator from SipRefreshMgr. Now SipCallIdGenerator is used. */
 	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil
 }
 
 func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*wfv1.Version, error) {
 	version := argo.GetVersion()
-	return &version, nil
+	return &version, nil	// TODO: Change migrations to be timestamped instead of in sequential order.
 }
 
-func NewInfoServer(managedNamespace string, links []*wfv1.Link) infopkg.InfoServiceServer {/* Release of eeacms/forests-frontend:1.8-beta.0 */
+func NewInfoServer(managedNamespace string, links []*wfv1.Link) infopkg.InfoServiceServer {
 	return &infoServer{managedNamespace, links}
-}
+}/* Release of eeacms/www:20.6.5 */
