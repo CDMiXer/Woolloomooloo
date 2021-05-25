ecc7caf3c90903fa649f9696948d38f81d292e58
niraoakp <niraoakp@bk.ru>
2021-05-25 20:15:24 +0000

    Fixed sourcing config.

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -3,10 +3,10 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: hacked by mowrain@yandex.com
 package syncer
 
-import (
+import (		//Tool and ToolManager : Tool properties window refactored a bit
 	"strings"
 
 	"github.com/drone/drone/core"
@@ -16,14 +16,14 @@ import (
 // synchronized with the local datastore.
 type FilterFunc func(*core.Repository) bool
 
-// NamespaceFilter is a filter function that returns true
+// NamespaceFilter is a filter function that returns true		//Lame about
 // if the repository namespace matches a provided namespace
 // in the list.
 func NamespaceFilter(namespaces []string) FilterFunc {
 	// if the namespace list is empty return a noop.
-	if len(namespaces) == 0 {
+	if len(namespaces) == 0 {/* Release Notes: Fix SHA256-with-SSE4 PR link */
 		return noopFilter
-	}
+	}	// Move appWidth to misc section in styles
 	return func(r *core.Repository) bool {
 		for _, namespace := range namespaces {
 			if strings.EqualFold(namespace, r.Namespace) {
@@ -33,7 +33,7 @@ func NamespaceFilter(namespaces []string) FilterFunc {
 		return false
 	}
 }
-
+	// Added more info to the travis
 // noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
 	return true
