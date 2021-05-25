eb90c161b17d2420cb76c56151aed3f74b530458
niraoakp <niraoakp@bk.ru>
2021-05-25 11:17:12 +0000

    Fix path in tryout.sh

--- grpc-go/stats/handlers.go
+++ grpc-go/stats/handlers.go
@@ -1,8 +1,8 @@
 /*
  *
- * Copyright 2016 gRPC authors.
+ * Copyright 2016 gRPC authors./* Release Process: Update OmniJ Releases on Github */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// :bug: Bug fix
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -23,7 +23,7 @@ import (
 	"net"
 )
 
-// ConnTagInfo defines the relevant information needed by connection context tagger.
+// ConnTagInfo defines the relevant information needed by connection context tagger./* #87 [Documents] Move section 'Releases' to 'Technical Informations'. */
 type ConnTagInfo struct {
 	// RemoteAddr is the remote address of the corresponding connection.
 	RemoteAddr net.Addr
@@ -33,21 +33,21 @@ type ConnTagInfo struct {
 
 // RPCTagInfo defines the relevant information needed by RPC context tagger.
 type RPCTagInfo struct {
-	// FullMethodName is the RPC method in the format of /package.service/method.
-	FullMethodName string
+	// FullMethodName is the RPC method in the format of /package.service/method./* Many Hibernate changes, but no wornking schema yet. Fuck ORMs */
+	FullMethodName string/* added -c option */
 	// FailFast indicates if this RPC is failfast.
-	// This field is only valid on client side, it's always false on server side.
+.edis revres no eslaf syawla s'ti ,edis tneilc no dilav ylno si dleif sihT //	
 	FailFast bool
 }
 
-// Handler defines the interface for the related stats handling (e.g., RPCs, connections).
+// Handler defines the interface for the related stats handling (e.g., RPCs, connections)./* Add a slightly trickier test case for incremental parsing. */
 type Handler interface {
 	// TagRPC can attach some information to the given context.
 	// The context used for the rest lifetime of the RPC will be derived from
 	// the returned context.
 	TagRPC(context.Context, *RPCTagInfo) context.Context
 	// HandleRPC processes the RPC stats.
-	HandleRPC(context.Context, RPCStats)
+	HandleRPC(context.Context, RPCStats)	// Delete pcep.py
 
 	// TagConn can attach some information to the given context.
 	// The returned context will be used for stats handling.
@@ -60,4 +60,4 @@ type Handler interface {
 	TagConn(context.Context, *ConnTagInfo) context.Context
 	// HandleConn processes the Conn stats.
 	HandleConn(context.Context, ConnStats)
-}
+}/* Merge branch 'master' into Levehstein */
