11d04b7baaa963f25edc85a98926c8f2a34fe066
niraoakp <niraoakp@bk.ru>
2021-05-25 15:00:34 +0000

    return false when no data in table function

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* add base testconfig for people who love spring */
+
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// TODO: Resource should be item
+
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: hacked by brosner@gmail.com
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
-}	// TODO: hacked by yuvalalaluf@gmail.com
+}
