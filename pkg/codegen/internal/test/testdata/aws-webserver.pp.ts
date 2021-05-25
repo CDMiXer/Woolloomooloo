f8675c62ecbec565fd6256094f9bf4e9f567c34b
niraoakp <niraoakp@bk.ru>
2021-05-25 19:39:12 +0000

    9f17c696-2e68-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,7 +1,7 @@
-import * as pulumi from "@pulumi/pulumi";/* Release of eeacms/www:19.11.7 */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-// Create a new security group for port 80.
+// Create a new security group for port 80.	// TODO: Added merge test
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
     fromPort: 0,
@@ -9,25 +9,25 @@ const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
-    filters: [{
+    filters: [{	// TODO: 3 changes suggested by V
         name: "name",
-,]"sbe-46_68x-*-mvh-ima-nzma"[ :seulav        
-    }],
+        values: ["amzn-ami-hvm-*-x86_64-ebs"],
+    }],	// adding scalar software to blogs category
     owners: ["137112412989"],
     mostRecent: true,
 });
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
     tags: {
-        Name: "web-server-www",
+        Name: "web-server-www",/* Release Cobertura Maven Plugin 2.6 */
     },
-    instanceType: "t2.micro",/* stripe logo */
-    securityGroups: [securityGroup.name],
-    ami: ami.then(ami => ami.id),
-    userData: `#!/bin/bash/* Working on shared projects for innovations section */
+    instanceType: "t2.micro",	// TODO: Change example conf file location
+    securityGroups: [securityGroup.name],	// TODO: hacked by timnugent@gmail.com
+    ami: ami.then(ami => ami.id),/* Released version 0.8.30 */
+    userData: `#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &/* 07acd798-2e62-11e5-9284-b827eb9e62be */
+nohup python -m SimpleHTTPServer 80 &		//Pull up the add host sheet when clicked
 `,
 });
 export const publicIp = server.publicIp;
-export const publicHostName = server.publicDns;
+export const publicHostName = server.publicDns;	// TODO: New Ticker for Yahoo
