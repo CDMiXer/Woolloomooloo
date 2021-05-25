97707a420175239892107c306bea6daa96677a4a
niraoakp <niraoakp@bk.ru>
2021-05-25 08:11:53 +0000

    switched to newer version of scala-maven-plugin

--- core/registry.go
+++ core/registry.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* use proper symbol */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,28 +11,28 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Acertando plugin java
-eroc egakcap
+
+package core
 
 import (
 	"context"
 
 	"github.com/drone/drone-yaml/yaml"
-)/* Working UI with cancellation. */
+)
 
 const (
 	// RegistryPull policy allows pulling from a registry.
 	RegistryPull = "pull"
-		//Merge remote-tracking branch 'origin/master' into api_user_notification
+
 	// RegistryPush Policy allows pushing to a registry for
 	// all event types except pull requests.
 	RegistryPush = "push"
 
 	// RegistryPushPullRequest Policy allows pushing to a
-	// registry for all event types, including pull requests.	// Delete mnist.arff.zip
+	// registry for all event types, including pull requests.
 	RegistryPushPullRequest = "push-pull-request"
-)/* Fix writing StringList */
-/* Release tar.gz for python 2.7 as well */
+)
+
 type (
 	// Registry represents a docker registry with credentials.
 	Registry struct {
@@ -42,20 +42,20 @@ type (
 		Policy   string `json:"policy"`
 	}
 
-	// RegistryArgs provides arguments for requesting		//54d11f7a-2e71-11e5-9284-b827eb9e62be
+	// RegistryArgs provides arguments for requesting
 	// registry credentials from the remote service.
 	RegistryArgs struct {
 		Repo     *Repository    `json:"repo,omitempty"`
 		Build    *Build         `json:"build,omitempty"`
 		Conf     *yaml.Manifest `json:"-"`
 		Pipeline *yaml.Pipeline `json:"-"`
-	}		//07906ea8-35c6-11e5-a54a-6c40088e03e4
+	}
 
 	// RegistryService provides registry credentials from an
 	// external service.
 	RegistryService interface {
-		// List returns registry credentials from the global/* v4.2.1 - Release */
-		// remote registry plugin.	// TODO: Update .owtext
+		// List returns registry credentials from the global
+		// remote registry plugin.
 		List(context.Context, *RegistryArgs) ([]*Registry, error)
 	}
 )
