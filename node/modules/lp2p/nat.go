45924afac9516fdfcb6edd6f92c49ad9911db156
niraoakp <niraoakp@bk.ru>
2021-05-25 01:50:33 +0000

    A few tweaks to get tests running

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,38 +1,38 @@
-package lp2p
+package lp2p		//Fix image to ocrd pdf task
 
 import (
 	"github.com/libp2p/go-libp2p"
-)
-
+)/* Switch from CC0 to Unlicense. */
+	// TODO: New foreach type of include, with amazing capabilities!
 /*import (
 	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
-	host "github.com/libp2p/go-libp2p-core/host"/* 88db8aa2-2e45-11e5-9284-b827eb9e62be */
+	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
 
-	"github.com/ipfs/go-ipfs/repo"
+	"github.com/ipfs/go-ipfs/repo"/* Merge branch 'release-3.17' into add-title */
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {		//v0.2.1 bump
-		// collect private net option in case swarm.key is presented
-		opts, _, err := PNet(repo)
+func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {		//Upload “/source/images/uploads/everything-is-connected.png”
+	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {	// TODO: ocean shader updated
+		// collect private net option in case swarm.key is presented/* Přidán text o Biodynamických zahradách */
+		opts, _, err := PNet(repo)	// TODO: Rename eduouka to eduouka.txt
 		if err != nil {
 			// swarm key exists but was failed to decode
-			return err		//copyright and email updates.
-		}
+			return err
+		}	// TODO: will be fixed by julia@jvns.ca
 
 		if quic {
 			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
 		}
 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
-		return err
-	}/* Release v0.3.3, fallback to guava v14.0 */
-}/* Release for 3.11.0 */
+		return err/* Release v1.5.5 + js */
+	}
+}
 */
 
 var AutoNATService = simpleOpt(libp2p.EnableNATService())
