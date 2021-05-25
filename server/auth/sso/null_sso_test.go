32befe75ab5b4c13f5a2f7b1dbf923582861e7d7
niraoakp <niraoakp@bk.ru>
2021-05-25 10:16:57 +0000

    Update voicebox.py

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -12,16 +12,16 @@ import (
 func Test_nullSSO_Authorize(t *testing.T) {
 	_, err := NullSSO.Authorize(context.Background(), "")
 	assert.Error(t, err)
-}
+}/* Release 3.1.1 */
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
-	NullSSO.HandleCallback(w, &http.Request{})
+	NullSSO.HandleCallback(w, &http.Request{})/* Release 1.4.0.2 */
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
 
-func Test_nullSSO_HandleRedirect(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}
+func Test_nullSSO_HandleRedirect(t *testing.T) {	// TODO: will be fixed by seth@sethvargo.com
+	w := &testhttp.TestResponseWriter{}/* Release of eeacms/www-devel:20.1.16 */
 	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
