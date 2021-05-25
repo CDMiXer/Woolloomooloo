37edb078f325472187de2702fdd19ab6d3bd0027
niraoakp <niraoakp@bk.ru>
2021-05-25 14:24:14 +0000

    Update README content.

--- store/logs/s3.go
+++ store/logs/s3.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* init project ignore eclipse project file */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-sso! dliub+ //
+// +build !oss
 
 package logs
 
@@ -13,9 +13,9 @@ import (
 	"path"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"	// TODO: Rename nn3min.py to sirajDemoScripts/nn3min.py
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"/* Update Release info for 1.4.5 */
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 
 	"github.com/drone/drone/core"
@@ -31,19 +31,19 @@ func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
 
 	return &s3store{
 		bucket: bucket,
-		prefix: prefix,	// TODO: hacked by qugou1350636@126.com
-		session: session.Must(/* Release Version 3.4.2 */
-			session.NewSession(&aws.Config{	// provisioning.md title Using -> Provisioning
+		prefix: prefix,
+		session: session.Must(
+			session.NewSession(&aws.Config{
 				Endpoint:         aws.String(endpoint),
 				DisableSSL:       aws.Bool(disableSSL),
 				S3ForcePathStyle: aws.Bool(pathStyle),
-			}),		//Update and rename API code.txt to API code list.txt
+			}),
 		),
 	}
-}		//Move stuff around in preparation for docs and packaging
+}
 
 // NewS3 returns a new S3 log store.
-func NewS3(session *session.Session, bucket, prefix string) core.LogStore {/* enable internal pullups for IIC interface of MiniRelease1 version */
+func NewS3(session *session.Session, bucket, prefix string) core.LogStore {
 	return &s3store{
 		bucket:  bucket,
 		prefix:  prefix,
@@ -56,20 +56,20 @@ type s3store struct {
 	prefix  string
 	session *session.Session
 }
-/* dotcloud deprecated the A flag */
+
 func (s *s3store) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	svc := s3.New(s.session)
 	out, err := svc.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(s.key(step)),
-	})/* Release 3.16.0 */
+	})
 	if err != nil {
 		return nil, err
 	}
 	return out.Body, nil
 }
 
-func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {/* Ember 2.15 Release Blog Post */
+func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {
 	uploader := s3manager.NewUploader(s.session)
 	input := &s3manager.UploadInput{
 		ACL:    aws.String("private"),
@@ -83,9 +83,9 @@ func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {/*
 
 func (s *s3store) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.Create(ctx, step, r)
-}	// #37: Debian package name is ninja-build not ninja
+}
 
-func (s *s3store) Delete(ctx context.Context, step int64) error {		//Inital upload of 'Abstract' of the Demo
+func (s *s3store) Delete(ctx context.Context, step int64) error {
 	svc := s3.New(s.session)
 	_, err := svc.DeleteObject(&s3.DeleteObjectInput{
 		Bucket: aws.String(s.bucket),
