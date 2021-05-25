7649749b7f7a7febe5e8d08942c975e78981f6d8
niraoakp <niraoakp@bk.ru>
2021-05-25 19:09:51 +0000

    Merge "Fix the device_id and device_owner of gw"

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -1,4 +1,4 @@
-// Create a bucket and expose a website index document		//added milliseconds to timestamp
+// Create a bucket and expose a website index document
 resource siteBucket "aws:s3:Bucket" {
 	website = {
 		indexDocument = "index.html"
@@ -6,36 +6,36 @@ resource siteBucket "aws:s3:Bucket" {
 }
 
 siteDir = "www" // directory for content files
-
+/* Merge "unite parameters for MgmtDriver interfaces" */
 // For each file in the directory, create an S3 object stored in `siteBucket`
-resource files "aws:s3:BucketObject" {
+resource files "aws:s3:BucketObject" {/* Block repodata code to debug a completely random failure. */
     options {
-		range = readDir(siteDir)		//Resize fpfd128_impl_t struct to hold BCD products exactly.
-    }
+		range = readDir(siteDir)
+    }		//Merge "Update castellan to 0.12.0"
 
-	bucket = siteBucket.id // Reference the s3.Bucket object	// TODO: move rails_ujs_fix to public section
+	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
-}
-
-// Set the access policy for the bucket so all objects are readable
-resource bucketPolicy "aws:s3:BucketPolicy" {
+}/* Adjusting font of webdev theme. */
+/* V0.3 Released */
+// Set the access policy for the bucket so all objects are readable/* Added IA test file. */
+resource bucketPolicy "aws:s3:BucketPolicy" {		//More OSS/Sonatype tweaks.
 	bucket = siteBucket.id // refer to the bucket created earlier
-/* Despublica 'intimacoes-diversas' */
+
 	// The policy is JSON-encoded.
 	policy = toJSON({
 		Version = "2012-10-17"
-		Statement = [{
+		Statement = [{	// TODO: Actually instantiate TreeToIndex classes.
 			Effect = "Allow"
-			Principal = "*"		//HTTPRequest removes fragments from URIs before sending them
+			Principal = "*"	// TODO: hacked by why@ipfs.io
 			Action = [ "s3:GetObject" ]
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
-		}]
+		}]/* Release Version 1.0.2 */
 	})
 }
-
+/* Update 99_rubydebug.conf */
 // Stack outputs
-output bucketName { value = siteBucket.bucket }		//Changed loading the JSON Schemata from relative path to localhost:8080
-output websiteUrl { value = siteBucket.websiteEndpoint }/* Merge "Release 3.2.3.399 Prima WLAN Driver" */
+output bucketName { value = siteBucket.bucket }
+output websiteUrl { value = siteBucket.websiteEndpoint }
