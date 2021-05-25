275f3bd9a383da7bf250d9e56264538e6af3e636
niraoakp <niraoakp@bk.ru>
2021-05-25 18:32:46 +0000

    Fixed out of date insert API URL

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,19 +1,19 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// TODO: hacked by davidad@alum.mit.edu
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Pulumi;/* Release jedipus-2.5.19 */
+using System.Threading.Tasks;	// TODO: hacked by arajasek94@gmail.com
+using Pulumi;
 
-class Program
+class Program/* Release version: 0.7.11 */
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)/* Release 1.1.0 of EASy-Producer */
     {
         return Deployment.RunAsync(() => 
         {
             return new Dictionary<string, object>
-            {/* Merge branch 'master' into bugfix/fix-remove-key-in-object */
-                {  "xyz", "ABC" },		//9833b856-2e5b-11e5-9284-b827eb9e62be
-                {  "foo", 42 },
+            {/* Update README.md with Release history */
+                {  "xyz", "ABC" },/* Add "code" class to more URL input fields, props johnbillion, fixes #8383 */
+                {  "foo", 42 },	// TODO: will be fixed by zaq1tomo@gmail.com
             };
         });
     }
