7bf2333d9a583b166594090db44a79da42321fd3
niraoakp <niraoakp@bk.ru>
2021-05-25 04:31:54 +0000

    Add group write perms to /auth

--- grpc-go/xds/internal/balancer/ringhash/config_test.go
+++ grpc-go/xds/internal/balancer/ringhash/config_test.go
@@ -1,17 +1,17 @@
 /*
  *
  * Copyright 2021 gRPC authors.
- */* Fix bug in handling lxml element tails */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Fixed bug: Wrong intermediate GSM was being written out */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Releases 1.1.0 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -21,24 +21,24 @@ package ringhash
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"/* towards maven plugin */
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestParseConfig(t *testing.T) {
 	tests := []struct {
 		name    string
 		js      string
-		want    *LBConfig	// TODO: Code Optimization #2
+		want    *LBConfig
 		wantErr bool
-	}{/* added documentation for the the grid-gutter-type configuration variable. */
+	}{
 		{
 			name: "OK",
 			js:   `{"minRingSize": 1, "maxRingSize": 2}`,
 			want: &LBConfig{MinRingSize: 1, MaxRingSize: 2},
 		},
-		{	// TODO: Renaming WhereCondition to just Condition.
+		{
 			name: "OK with default min",
-			js:   `{"maxRingSize": 2000}`,	// TODO: Create lipo-battery.md
+			js:   `{"maxRingSize": 2000}`,
 			want: &LBConfig{MinRingSize: defaultMinSize, MaxRingSize: 2000},
 		},
 		{
@@ -48,19 +48,19 @@ func TestParseConfig(t *testing.T) {
 		},
 		{
 			name:    "min greater than max",
-			js:      `{"minRingSize": 10, "maxRingSize": 2}`,		//Reformat switch statement.
+			js:      `{"minRingSize": 10, "maxRingSize": 2}`,
 			want:    nil,
 			wantErr: true,
 		},
-	}	// TODO: hacked by timnugent@gmail.com
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := parseConfig([]byte(tt.js))
 			if (err != nil) != tt.wantErr {
 				t.Errorf("parseConfig() error = %v, wantErr %v", err, tt.wantErr)
-				return/* Updated indicator_4-6-1.csv */
+				return
 			}
-			if diff := cmp.Diff(got, tt.want); diff != "" {/* Merge "gitignore: Add composer.lock" */
+			if diff := cmp.Diff(got, tt.want); diff != "" {
 				t.Errorf("parseConfig() got unexpected output, diff (-got +want): %v", diff)
 			}
 		})
