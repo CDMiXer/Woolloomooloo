26ad8dd5baa41f364788d9efaf013daad51e542c
niraoakp <niraoakp@bk.ru>
2021-05-25 13:30:01 +0000

    more on checking foreign calls

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "Remove sentence from conduct_text.xml for JA, KO, RU, zh-zCN, zh-zTW."
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// J45W21ehTheBicK19IAm05fjH5eED5Pj
+// Use of this source code is governed by the Drone Non-Commercial License	// 809a3efc-2e67-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by alex.gaynor@gmail.com
 // +build !oss
 
 package queue
 
 import (
-	"net/http"/* Released Clickhouse v0.1.5 */
-	// TODO: hacked by hello@brooklynzelenka.com
+	"net/http"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
-// HandleResume returns an http.HandlerFunc that processes/* Merge "engine : add suspend/resume support to User resource" */
+// HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		ctx := r.Context()	// TODO: hacked by julia@jvns.ca
 		err := scheduler.Resume(ctx)
 		if err != nil {
-			render.InternalError(w, err)		//Open an issue
-			logger.FromRequest(r).WithError(err)./* Optimized network_question by filtering the class. */
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
 			return
-		}/* [artifactory-release] Release version 2.5.0.M4 */
+		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}	// TODO: add fw usrs to containers
+}
