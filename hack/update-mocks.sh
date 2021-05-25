1fd23f07a6aca6b0427869ee9a1bcdb3e2992f6f
niraoakp <niraoakp@bk.ru>
2021-05-25 13:10:20 +0000

    1fb9821a-2e6b-11e5-9284-b827eb9e62be

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -eu -o pipefail/* Merge "tests: Collect info on failure of conn_tester" */
-
+set -eu -o pipefail
+/* Adding build status badge to README */
 for m in $*; do
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)/* Proper installer command in README.md */
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
-		//1. fix xls filename in batches
-  cd "$MOCK_DIR"/* Released 3.0.2 */
+
+  cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
-  cd -		//Move from go 1.4rc2 to 1.4 release
-done/* Removed archive 2 */
+  cd -
+done
