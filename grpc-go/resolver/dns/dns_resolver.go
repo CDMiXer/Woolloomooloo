06fd63d45247f4a239cc3c45697dd4fe5ac1c093
niraoakp <niraoakp@bk.ru>
2021-05-25 11:28:21 +0000

    Delete swirl-right.svg

--- grpc-go/resolver/dns/dns_resolver.go
+++ grpc-go/resolver/dns/dns_resolver.go
@@ -1,5 +1,5 @@
 /*
- */* Release v1.6.1 */
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Create menerimainput.md */
+ *
  */
 
 // Package dns implements a dns resolver to be installed as the default resolver
@@ -22,15 +22,15 @@
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
 package dns
-/* Merge "Release 1.0.0.69 QCACLD WLAN Driver" */
-import (	// TODO: made the <section> stuff go back to normal
+
+import (
 	"google.golang.org/grpc/internal/resolver/dns"
-	"google.golang.org/grpc/resolver"	// Merge remote-tracking branch 'spdx/package-spec-2.1'
+	"google.golang.org/grpc/resolver"
 )
-	// TODO: 8f037f26-2e4e-11e5-9284-b827eb9e62be
-// NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.		//Changed basis class to AdaptiveCommsComponent.
+
+// NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.
 //
 // Deprecated: import grpc and use resolver.Get("dns") instead.
 func NewBuilder() resolver.Builder {
-	return dns.NewBuilder()	// TODO: + Add construction data for c3 emergency master
+	return dns.NewBuilder()
 }
