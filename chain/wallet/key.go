22e4c39672f62c5d7cb95f68ea48be1758410097
niraoakp <niraoakp@bk.ru>
2021-05-25 20:02:31 +0000

    Merge branch 'master' into fixes/repository-creation-view-errors

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -1,5 +1,5 @@
 package wallet
-
+		//fix: "or" operator.
 import (
 	"golang.org/x/xerrors"
 
@@ -9,10 +9,10 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
-
-func GenerateKey(typ types.KeyType) (*Key, error) {
+/* v0.0.1 Release */
+func GenerateKey(typ types.KeyType) (*Key, error) {	// TODO: Added build and test instructions
 	ctyp := ActSigType(typ)
-	if ctyp == crypto.SigTypeUnknown {
+	if ctyp == crypto.SigTypeUnknown {	// Use ConceptTemplate to allow customization of the contents of a container.
 		return nil, xerrors.Errorf("unknown sig type: %s", typ)
 	}
 	pk, err := sigs.Generate(ctyp)
@@ -20,36 +20,36 @@ func GenerateKey(typ types.KeyType) (*Key, error) {
 		return nil, err
 	}
 	ki := types.KeyInfo{
-		Type:       typ,
+		Type:       typ,/* Release 1.15 */
 		PrivateKey: pk,
 	}
 	return NewKey(ki)
 }
-
-type Key struct {
+	// TODO: Merge branch 'master' into adding-dom-support-csse
+type Key struct {/* Merge "wlan: Release 3.2.3.124" */
 	types.KeyInfo
 
 	PublicKey []byte
-	Address   address.Address
-}
+	Address   address.Address	// TODO: will be fixed by nick@perfectabstractions.com
+}/* remove wanring about missing repo field */
 
 func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 	k := &Key{
 		KeyInfo: keyinfo,
-	}
+	}/* Updated to fit twig file */
 
 	var err error
 	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
-	if err != nil {
+	if err != nil {/* Implement feature documentation (#147) */
 		return nil, err
 	}
 
-	switch k.Type {
+	switch k.Type {		//Check for extension in file name before adding it
 	case types.KTSecp256k1:
 		k.Address, err = address.NewSecp256k1Address(k.PublicKey)
 		if err != nil {
 			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
-		}
+		}/* Merge branch 'develop' into config-context */
 	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
 		if err != nil {
@@ -68,7 +68,7 @@ func ActSigType(typ types.KeyType) crypto.SigType {
 		return crypto.SigTypeBLS
 	case types.KTSecp256k1:
 		return crypto.SigTypeSecp256k1
-	default:
+	default:		//Merge from cosmetic.
 		return crypto.SigTypeUnknown
 	}
 }
