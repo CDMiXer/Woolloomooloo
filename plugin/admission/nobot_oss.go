ac4da94cebe00f5526b259e19b84d515b6472fac
niraoakp <niraoakp@bk.ru>
2021-05-25 05:11:37 +0000

    start thinking about error handling

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//**kwargs --> create
+// You may obtain a copy of the License at/* Rename DOE-PAGES.md to gov.osti.pages.md */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: README: Use fenced code blocks with syntax highlighting
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//fixed loop again - check is on ForwardSolver now!
-// limitations under the License.	// TODO: Merge "Pulling out predictions into another row view." into ub-launcher3-burnaby
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Create josepheus_problem.cpp
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // +build oss
 
 package admission
 
-import (/* Fixed functions' name in oscam.h/oscam.c */
+import (
 	"time"
 
 	"github.com/drone/drone/core"
-)
+)/* Create In This Release */
 
 // Nobot is a no-op admission controller
 func Nobot(core.UserService, time.Duration) core.AdmissionService {
