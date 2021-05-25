b5960fe42703067f77aa501b1600e4af5a39270d
niraoakp <niraoakp@bk.ru>
2021-05-25 11:24:40 +0000

    Update for mobile slides

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www:19.1.12 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release new version 2.4.18: Retire the app version (famlam) */
+//	// Merge "support events"
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Add not so that these tests pass with pipefail enabled.
-// See the License for the specific language governing permissions and/* Fixed bug related to the output of the reset command */
-// limitations under the License./* Merge branch 'master' into T223948-media-player */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package runner
+package runner		//up, Callback/timer
