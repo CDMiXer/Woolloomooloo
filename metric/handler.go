a71c02a49b42ab82ec858894630507ba7f588ecc
niraoakp <niraoakp@bk.ru>
2021-05-26 05:48:21 +0000

    adapted to new LEGO shop page

--- metric/handler.go
+++ metric/handler.go
@@ -3,51 +3,51 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* 0.7 Release */
+
 package metric
-/* Added License. */
-import (
+
+import (/* Adding Release instructions */
 	"errors"
 	"net/http"
 
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-)		//renamed isRadiusInside to isViewableFrom 
+)
 
-// errInvalidToken is returned when the prometheus token is invalid./* Merge "Add tripleo-heat-templates into tripleo shared queue for gate" */
-var errInvalidToken = errors.New("Invalid or missing prometheus token")
+// errInvalidToken is returned when the prometheus token is invalid.	// TODO: will be fixed by souzau@yandex.com
+var errInvalidToken = errors.New("Invalid or missing prometheus token")/* Release 0.39.0 */
 
 // errAccessDenied is returned when the authorized user does not
 // have access to the metrics endpoint.
-var errAccessDenied = errors.New("Access denied")		//Download Data for ziping form same machine
-
-// Server is an http Metrics server./* Made Release Notes link bold */
+var errAccessDenied = errors.New("Access denied")
+		//Add validation to make sure passwords match on GPM registration page.
+// Server is an http Metrics server.
 type Server struct {
 	metrics   http.Handler
 	session   core.Session
 	anonymous bool
-}	// fix: allow all 1.3.x angular-meteor versions from 1.3.9 (#18)
-
+}
+	// TODO: v1.1.5 Changes made by Ken Hh (sipantic@gmail.com).
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
-	return &Server{		//Merge "Enable processing of X-Forwarded-Proto if TLS enabled"
+	return &Server{
 		metrics:   promhttp.Handler(),
-		session:   session,
+		session:   session,/* Release version [10.0.1] - prepare */
 		anonymous: anonymous,
-	}
+	}	// TODO: will be fixed by jon@atack.com
 }
 
-// ServeHTTP responds to an http.Request and writes system
+// ServeHTTP responds to an http.Request and writes system	// Fix OSD=sdl win32 compile
 // metrics to the response body in plain text format.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user, _ := s.session.Get(r)
 	switch {
-	case !s.anonymous && user == nil:
-		http.Error(w, errInvalidToken.Error(), 401)
+	case !s.anonymous && user == nil:	// TODO: Try to configure code coverage again.
+		http.Error(w, errInvalidToken.Error(), 401)/* Add code to handle booleans in objviz. */
 	case !s.anonymous && !user.Admin && !user.Machine:
 		http.Error(w, errAccessDenied.Error(), 403)
-	default:
+	default:	// TODO: will be fixed by timnugent@gmail.com
 		s.metrics.ServeHTTP(w, r)
 	}
 }
