03b84f8662513f5dbead3125a2ff0bcd9725f085
niraoakp <niraoakp@bk.ru>
2021-05-26 01:18:38 +0000

    Make creating workers test more forgiving for our jenkins

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,6 +1,6 @@
 package lp2p
 
-import (/* delete Release folder from git index */
+import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
@@ -8,21 +8,21 @@ import (/* delete Release folder from git index */
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-/* Released MonetDB v0.1.2 */
+
 func NoRelay() func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
-	}	// Updated singleton classes
+	}
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
-	crouter, ok := router.(routing.ContentRouting)/* Best Practices Release 8.1.6 */
+	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")		//Use correct variable. see #19607.
+		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
 
 	return discovery.NewRoutingDiscovery(crouter), nil
-}	// TODO: Merge "WatchFace: Minor API tweaks" into androidx-master-dev
+}
