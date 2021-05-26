40475339b28152ec1e6462f868419b5d8b773b8b
niraoakp <niraoakp@bk.ru>
2021-05-26 23:27:40 +0000

    client: minor fix in client demo name generator

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update SPAdes home page link
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release: Making ready for next release iteration 6.0.3 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixed targetGroupId issue in background import.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build nolimit
 // +build !oss
 
-package license/* Updated Little Bites of Cocoa's link */
+package license
 
-import (	// TODO: hacked by souzau@yandex.com
-	"github.com/drone/drone/core"	// TODO: Rename Setup.css to SETup.css
+import (
+	"github.com/drone/drone/core"
 )
 
 // DefaultLicense is an empty license with no restrictions.
