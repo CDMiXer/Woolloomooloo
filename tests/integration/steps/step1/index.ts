77fabd380c11c2ce975652d56055c98df68f0f08
niraoakp <niraoakp@bk.ru>
2021-05-26 22:49:18 +0000

    v2.0.0 : Fixed issue #148

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -2,10 +2,10 @@
 
 import { Resource } from "./resource";
 
-// Step 1: Populate the world:
-// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
+// Step 1: Populate the world:		//Fixed Cereal compilation bug on Android
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property./* Allow to define auxiliary variables in global models. */
 let a = new Resource("a", { state: 1 });
-let b = new Resource("b", { state: 1 });
+let b = new Resource("b", { state: 1 });/* Перед обновлением. */
 let c = new Resource("c", { state: 1, resource: a });
 let d = new Resource("d", { state: 1 });
 // Checkpoint: a1, b1, c1, d1
