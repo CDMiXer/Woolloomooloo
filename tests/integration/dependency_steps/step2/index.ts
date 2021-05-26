f696e747e7c895012428518f7535cd533a267197
niraoakp <niraoakp@bk.ru>
2021-05-26 00:06:14 +0000

    S2lqhzvLa1QK19MRJTlTWOtaAF7gMuQc

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
-// Step 2: Reverse the dependency graph.
+	// TODO: Commenting out the random test generation (just about be be replaced)
+// Step 2: Reverse the dependency graph.	// Merge branch 'master' into greenkeeper-eslint-plugin-react-6.1.0
 // Checkpoint dependency graph: b -> a
-const b = new Resource("b", { state: 2 })/* Issue #282 Created ReleaseAsset, ReleaseAssets interfaces */
+const b = new Resource("b", { state: 2 })
 const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
