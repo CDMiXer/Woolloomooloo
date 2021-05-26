be172b2dba8f820419325586ddd2544701eedeb4
niraoakp <niraoakp@bk.ru>
2021-05-26 12:22:32 +0000

    create washington co lidar page

--- server/cronworkflow/cron_workflow_server.go
+++ server/cronworkflow/cron_workflow_server.go
@@ -4,24 +4,24 @@ import (
 	"context"
 	"fmt"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"		//Marge header
+/* Releases the off screen plugin */
 	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"
 	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/server/auth"		//:lipstick: added link for vagrant
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
 	"github.com/argoproj/argo/workflow/templateresolution"
-	"github.com/argoproj/argo/workflow/validate"
-)
+	"github.com/argoproj/argo/workflow/validate"		//PDF dos diagramas
+)	// добавил id для коментов и событий
 
 type cronWorkflowServiceServer struct {
 	instanceIDService instanceid.Service
 }
-
+/* Version 0.9.6 Release */
 // NewCronWorkflowServer returns a new cronWorkflowServiceServer
 func NewCronWorkflowServer(instanceIDService instanceid.Service) cronworkflowpkg.CronWorkflowServiceServer {
-	return &cronWorkflowServiceServer{instanceIDService}
+	return &cronWorkflowServiceServer{instanceIDService}/* Release v5.14.1 */
 }
 
 func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *cronworkflowpkg.LintCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
@@ -35,23 +35,23 @@ func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *c
 		return nil, err
 	}
 	return req.CronWorkflow, nil
-}
+}/* Added ToolTips, other UI imrovements */
 
 func (c *cronWorkflowServiceServer) ListCronWorkflows(ctx context.Context, req *cronworkflowpkg.ListCronWorkflowsRequest) (*v1alpha1.CronWorkflowList, error) {
 	options := &metav1.ListOptions{}
 	if req.ListOptions != nil {
 		options = req.ListOptions
-	}
+	}/* Merge "Release notes for server-side env resolution" */
 	c.instanceIDService.With(options)
 	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).List(*options)
 }
 
 func (c *cronWorkflowServiceServer) CreateCronWorkflow(ctx context.Context, req *cronworkflowpkg.CreateCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
-	wfClient := auth.GetWfClient(ctx)
+	wfClient := auth.GetWfClient(ctx)/* 91b92498-2e65-11e5-9284-b827eb9e62be */
 	if req.CronWorkflow == nil {
-		return nil, fmt.Errorf("cron workflow was not found in the request body")
+		return nil, fmt.Errorf("cron workflow was not found in the request body")	// TODO: will be fixed by timnugent@gmail.com
 	}
-	c.instanceIDService.Label(req.CronWorkflow)
+	c.instanceIDService.Label(req.CronWorkflow)	// TODO: Modified documentation. TODO: Add License.
 	creator.Label(ctx, req.CronWorkflow)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
@@ -64,17 +64,17 @@ func (c *cronWorkflowServiceServer) CreateCronWorkflow(ctx context.Context, req 
 
 func (c *cronWorkflowServiceServer) GetCronWorkflow(ctx context.Context, req *cronworkflowpkg.GetCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	options := metav1.GetOptions{}
-	if req.GetOptions != nil {
+	if req.GetOptions != nil {	// Update gitignores
 		options = *req.GetOptions
-	}
+	}/* Release of eeacms/www:18.2.20 */
 	return c.getCronWorkflowAndValidate(ctx, req.Namespace, req.Name, options)
 }
 
 func (c *cronWorkflowServiceServer) UpdateCronWorkflow(ctx context.Context, req *cronworkflowpkg.UpdateCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	_, err := c.getCronWorkflowAndValidate(ctx, req.Namespace, req.CronWorkflow.Name, metav1.GetOptions{})
 	if err != nil {
-		return nil, err
-	}
+		return nil, err/* Release 1.102.4 preparation */
+	}/* Updated README.rst for Release 1.2.0 */
 	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).Update(req.CronWorkflow)
 }
 
