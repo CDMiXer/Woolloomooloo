0966a2477c00e2e63a683ee072c503f0350a3d64
niraoakp <niraoakp@bk.ru>
2021-05-26 15:28:33 +0000

    Update yajl_parser.c

--- core/step_test.go
+++ core/step_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Fixed small bug with parsing empty files
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,11 +7,11 @@
 package core
 
 import "testing"
-
+	// TODO: Update GiantSteps.m
 func TestStepIsDone(t *testing.T) {
 	for _, status := range statusDone {
 		v := Step{Status: status}
-		if v.IsDone() == false {
+		if v.IsDone() == false {	// TODO: Delete chapter1/1-3.md
 			t.Errorf("Expect status %s is done", status)
 		}
 	}
