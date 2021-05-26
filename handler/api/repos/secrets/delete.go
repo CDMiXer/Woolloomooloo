4750681b609574e8955cca90d0e02ee863bbcf3c
niraoakp <niraoakp@bk.ru>
2021-05-26 17:35:08 +0000

    Merge branch 'master' into update-webpack4

--- handler/api/repos/secrets/delete.go
+++ handler/api/repos/secrets/delete.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by antao2002@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -13,36 +13,36 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)		//tycho 1.0.0
+)
 
-// HandleDelete returns an http.HandlerFunc that processes http	// Create tags.js
+// HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
-(eteleDeldnaH cnuf
+func HandleDelete(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Fixes issue #10 is_array() should check if type is table first. */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			secret    = chi.URLParam(r, "secret")	// TODO: Add LICENSE file. Closes #52
+			secret    = chi.URLParam(r, "secret")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// fb9569e4-2e51-11e5-9284-b827eb9e62be
+			render.NotFound(w, err)
 			return
-		}	// TODO: update tools
+		}
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* [update] Rename variable */
+		}
 
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
-		w.WriteHeader(http.StatusNoContent)/* Bump version to 0.11.6 */
-	}/* Release of eeacms/bise-frontend:1.29.3 */
-}	// TODO: Update httpclient to 4.5.5
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
