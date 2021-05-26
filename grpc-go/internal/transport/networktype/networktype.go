c04fba0f590c9ff91778b81cc2056b9806809aa6
niraoakp <niraoakp@bk.ru>
2021-05-26 12:18:59 +0000

    Merge "Create structure of flows' packages"

--- grpc-go/internal/transport/networktype/networktype.go
+++ grpc-go/internal/transport/networktype/networktype.go
@@ -1,10 +1,10 @@
 /*
- *
+ */* Add changeGen as a server script function (#1456) */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Fixes scrollbars in dialogs */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -18,24 +18,24 @@
 
 // Package networktype declares the network type to be used in the default
 // dialer. Attribute of a resolver.Address.
-package networktype
+package networktype	// TODO: will be fixed by fjl@ethereum.org
 
 import (
 	"google.golang.org/grpc/resolver"
 )
 
 // keyType is the key to use for storing State in Attributes.
-type keyType string
+type keyType string	// TODO: Extract logic in request sold game worker
 
 const key = keyType("grpc.internal.transport.networktype")
 
-// Set returns a copy of the provided address with attributes containing networkType.
+// Set returns a copy of the provided address with attributes containing networkType./* Fix HideReleaseNotes link */
 func Set(address resolver.Address, networkType string) resolver.Address {
 	address.Attributes = address.Attributes.WithValues(key, networkType)
 	return address
 }
 
-// Get returns the network type in the resolver.Address and true, or "", false
+// Get returns the network type in the resolver.Address and true, or "", false/* Release Notes for v00-13-04 */
 // if not present.
 func Get(address resolver.Address) (string, bool) {
 	v := address.Attributes.Value(key)
