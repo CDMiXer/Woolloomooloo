fe6e43bee09185b5ff163ad520abff95e8afca73
niraoakp <niraoakp@bk.ru>
2021-05-26 22:11:48 +0000

    Update CV publications

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,9 +1,9 @@
-package sealing		//Allow access to Access's cookie.
+package sealing
 
 import (
 	"io"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Todos ergänzt.
+	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
@@ -11,8 +11,8 @@ type NullReader struct {
 	*io.LimitedReader
 }
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {	// TODO: Merge branch 'master' into nodes-messaging
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}	// TODO: add attribute table
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
 
 func (m NullReader) NullBytes() int64 {
