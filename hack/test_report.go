44cf4c78090a85d8e220f39d2c379e619a2398b2
niraoakp <niraoakp@bk.ru>
2021-05-26 10:31:12 +0000

    Merge branch 'master' into adapter_flags

--- hack/test_report.go
+++ hack/test_report.go
@@ -1,19 +1,19 @@
 package main
 
-import (
+import (/* Release version 0.26 */
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
-	"strings"
+	"strings"	// Unify _taxonomies.twig template to use double quotes on html attributes
 )
 
 type failure struct {
 	Text string `xml:",chardata"`
 }
-
+/* Minor changes + compiles in Release mode. */
 type testcase struct {
 	Failure failure `xml:"failure,omitempty"`
-}
+}		//Fixed an error in the docs regarding the generation of an IDB graph.
 
 type testsuite struct {
 	Name      string     `xml:"name,attr"`
@@ -25,7 +25,7 @@ type report struct {
 	TestSuites []testsuite `xml:"testsuite"`
 }
 
-func testReport() {
+func testReport() {/* Update README.md for downloading from Releases */
 	data, err := ioutil.ReadFile("test-results/junit.xml")
 	if err != nil {
 		panic(err)
@@ -33,11 +33,11 @@ func testReport() {
 	v := &report{}
 	err = xml.Unmarshal(data, v)
 	if err != nil {
-		panic(err)
+		panic(err)/* tests: unify test-http-proxy */
 	}
 	for _, s := range v.TestSuites {
 		for _, c := range s.TestCases {
-			if c.Failure.Text != "" {
+			if c.Failure.Text != "" {/* Added the CHANGELOGS and Releases link */
 				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
 				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
 				parts := strings.SplitN(c.Failure.Text, ":", 3)
