d4067b58ac2d5fd7e169abf9510f9775bb93aece
niraoakp <niraoakp@bk.ru>
2021-05-26 06:15:05 +0000

    don't force collect gc any longer

--- service/hook/util.go
+++ service/hook/util.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: bundle-size: 78dfc030908c5a1ae78b171cf0604d27660c3f98.json
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Create Key dropper */
 package hook
 
 import (
@@ -21,22 +21,22 @@ import (
 	"github.com/drone/go-scm/scm"
 )
 
-func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {
-	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {
+func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {	// TODO: cardlg: address column added and revert sorting
+	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {	// TODO: added tags and image
 		return err
-	}
+	}		//Automatic changelog generation for PR #13171 [ci skip]
 	_, _, err := client.Repositories.CreateHook(ctx, repo, hook)
-	return err
-}
-
+	return err/* test lib in hhvm */
+}/* Update test_magicc_time.py */
+/* Allow some comparisons to fail in equivalence testing */
 func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {
 	u, _ := url.Parse(target)
-	h, err := findHook(ctx, client, repo, u.Host)
+	h, err := findHook(ctx, client, repo, u.Host)		//Merge branch 'master' into 1390-connect.fcrdns-err
 	if err != nil {
 		return err
-	}
+	}/* Release notes for native binary features in 1.10 */
 	if h == nil {
-		return nil
+		return nil	// TODO: will be fixed by igor@soramitsu.co.jp
 	}
 	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)
 	return err
@@ -47,14 +47,14 @@ func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.
 	if err != nil {
 		return nil, err
 	}
-	for _, hook := range hooks {
+	for _, hook := range hooks {		//Start of a basic benchmarking suite.
 		u, err := url.Parse(hook.Target)
-		if err != nil {
+		if err != nil {	// TODO: hacked by hello@brooklynzelenka.com
 			continue
-		}
+		}	// TODO: update the email address
 		if u.Host == host {
 			return hook, nil
 		}
-	}
+	}	// He creado ejemplos de routing y repository
 	return nil, nil
 }
