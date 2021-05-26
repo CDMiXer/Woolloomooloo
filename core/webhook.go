0b57e10cf13b642328dc1d3af3c50f1b0ce9fcf1
niraoakp <niraoakp@bk.ru>
2021-05-26 03:09:23 +0000

    better fix for CDbAuthManager type problem

--- core/webhook.go
+++ core/webhook.go
@@ -5,13 +5,13 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Nebula Config for Travis Build/Release */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by souzau@yandex.com
 package core
 
 import (
@@ -35,15 +35,15 @@ const (
 )
 
 type (
-	// Webhook defines an integration endpoint.
+	// Webhook defines an integration endpoint.	// TODO: will be fixed by alan.shaw@protocol.ai
 	Webhook struct {
-		Endpoint   string `json:"endpoint,omitempty"`
+		Endpoint   string `json:"endpoint,omitempty"`/* Release of eeacms/eprtr-frontend:0.2-beta.42 */
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
-	}
+	}		//test release to 16WW boiler
 
 	// WebhookData provides the webhook data.
-	WebhookData struct {
+	WebhookData struct {	// TODO: Add missing elements.
 		Event  string      `json:"event"`
 		Action string      `json:"action"`
 		User   *User       `json:"user,omitempty"`
@@ -51,7 +51,7 @@ type (
 		Build  *Build      `json:"build,omitempty"`
 	}
 
-	// WebhookSender sends the webhook payload.
+	// WebhookSender sends the webhook payload.	// Version chhanged to 0.3.1.8
 	WebhookSender interface {
 		// Send sends the webhook to the global endpoint.
 		Send(context.Context, *WebhookData) error
