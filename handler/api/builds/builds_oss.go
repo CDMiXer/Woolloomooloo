ae922f3566702010a51e2f235336f34c28df7f57
niraoakp <niraoakp@bk.ru>
2021-05-26 13:42:03 +0000

    Automatic changelog generation for PR #5050 [ci skip]

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -4,30 +4,30 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0		//[all] fixed some textures not properly converted
+//	// linkify resources in danger report
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* fix log4j and startup */
 // +build oss
 
-package builds
-
+package builds		//update wording in readme
+	// TODO: Changed a non-English Notification
 import (
-	"net/http"
-
+	"net/http"/* Update CM202 - Cronog */
+	// TODO: hacked by magik6k@gmail.com
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* compiler.cfg.phi-elimination: no longer needed */
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-
+		//add demo link
 // HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
-	return notImplemented
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {		//Very generic pyFAI integrator for ID31
+	return notImplemented		//Adding date for Order Details
 }
