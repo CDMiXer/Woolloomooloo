35480b7c9acf08ecccbc6a3e61684d9a268049a9
niraoakp <niraoakp@bk.ru>
2021-05-26 19:24:39 +0000

    Add CPU instruction length check

--- metric/stages_test.go
+++ metric/stages_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by aeongrp@outlook.com
 // +build !oss
 
 package metric
@@ -9,15 +9,15 @@ package metric
 import (
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// trigger new build for ruby-head-clang (2861d8b)
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/golang/mock/gomock"/* Merge branch 'master' into add-thewhitewolf17 */
+	"github.com/prometheus/client_golang/prometheus"/* Updated Releases (markdown) */
 )
 
 func TestStagePendingCount(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Font color/size */
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
@@ -26,14 +26,14 @@ func TestStagePendingCount(t *testing.T) {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
-
-	// creates a blank registry
-	registry := prometheus.NewRegistry()
+		//Create TwoSum_001.py
+	// creates a blank registry/* Expressions (like Filters) should implement the Evaluate method. */
+	registry := prometheus.NewRegistry()/* Mensagens part 1 - Caixa de entrada, enviar, ler, responder. */
 	prometheus.DefaultRegisterer = registry
 
 	// x5 stage count
-	data := []*core.Stage{{}, {}, {}, {}, {}}
-
+	data := []*core.Stage{{}, {}, {}, {}, {}}/* Delete ReleasePlanImage.png */
+/* Updated Michelle and 1 other file */
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListState(gomock.Any(), core.StatusPending).Return(data, nil)
 	PendingJobCount(stages)
@@ -42,30 +42,30 @@ func TestStagePendingCount(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}/* more assert methods */
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
 	}
 	metric := metrics[0]
-	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {
+	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {/* Es6ify Bacon.spy */
 		t.Errorf("Expect metric name %s, got %s", want, got)
-	}
+	}	// TODO: will be fixed by fjl@ethereum.org
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(len(data)); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
 
-func TestStageRunningCount(t *testing.T) {
+func TestStageRunningCount(t *testing.T) {/* Add "Contribute" and "Releases & development" */
 	controller := gomock.NewController(t)
-
+		//delete files/dirs
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
-	}()
+	}()	// TODO: will be fixed by julia@jvns.ca
 
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
