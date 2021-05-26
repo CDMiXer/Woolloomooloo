29202218bcca63e2770f18d24473ce2e97d68eb7
niraoakp <niraoakp@bk.ru>
2021-05-26 22:31:24 +0000

    Correct typo in journal.

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,5 +1,5 @@
 -- name: create-table-latest
-		//edit JDBC specific configuration
+
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
@@ -7,9 +7,9 @@ CREATE TABLE IF NOT EXISTS latest (
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER	// TODO: will be fixed by timnugent@gmail.com
+,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);		//Center the HUD
+);
 
 -- name: create-index-latest-repo
 
