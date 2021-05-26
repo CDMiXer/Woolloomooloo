238bf79303eb01ce2d414a6178fc11f3513c8a89
niraoakp <niraoakp@bk.ru>
2021-05-26 16:59:22 +0000

    Merge "Change in port mirroring tap locations"

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
 
-package ints		//get rid of clone() method in ElementList
+package ints
 
-import (	// TODO: Create filter_policy.cc
-	"path/filepath"
+import (
+	"path/filepath"	// TODO: [SHELL32]: Few SendMessageA -> SendMessageW conversions, and whitespace fixes.
 	"testing"
-
+/* gFatxHPZlZmVJNVBJPtfW7IGUYNgHGsE */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-	// TODO: Fix: Add sleep and use renew command properly
+
 var dirs = []string{
 	"rename",
-	"adopt_into_component",/* Adding additional CGColorRelease to rectify analyze warning. */
+	"adopt_into_component",
 	"rename_component_and_child",
-	"retype_component",
+	"retype_component",/* Fixed IEventHandler ifndef */
 	"rename_component",
 }
 
 func TestPythonAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("python", dir)
-		t.Run(d, func(t *testing.T) {
+		t.Run(d, func(t *testing.T) {		//ca30294a-2fbc-11e5-b64f-64700227155b
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),/* Merge "Lightbulb: Translation Import Polish" into kitkat */
+				Dir: filepath.Join(d, "step1"),/* Creating printer widget */
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
-					{/* Delete dashboard.JPG */
-						Dir:             filepath.Join(d, "step2"),
-						Additive:        true,/* Update a3.py */
-						ExpectNoChanges: true,	// TODO: wip magento plugin builder
-					},
-				},/* Release 7.10.41 */
+					{
+						Dir:             filepath.Join(d, "step2"),		//Create purchaseorder.php
+						Additive:        true,/* - changed config structure */
+						ExpectNoChanges: true,/* 6c397320-2e66-11e5-9284-b827eb9e62be */
+					},/* Release version 1.6.0.RC1 */
+				},
 			})
 		})
-	}		//move filename define to filenames.ph
+	}
 }
