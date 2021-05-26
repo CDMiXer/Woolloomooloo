fa73ba1d5173a01e86e1cf4c7b701d79228efef1
niraoakp <niraoakp@bk.ru>
2021-05-26 22:51:17 +0000

    DropDown values

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//update cell to use common lib method
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by praveen@minio.io
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: mods to results data for network search
 import { Resource } from "./resource";
 
-// The DBR deletion of A triggers the deletion of C due to dependency./* Merge branch 'master' into docs/migrations */
+// The DBR deletion of A triggers the deletion of C due to dependency.
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent	// Merge "Fix possible crash in System UI" into klp-dev
+//   1. DeleteReplacement Dependent
 //   2. DeleteReplacement Base
-//   3. Replace Base
-//   4. CreateReplacement Base/* Update Status FAQs for New Status Release */
+//   3. Replace Base		//prepare swagger.yaml for 0.22
+//   4. CreateReplacement Base	// TODO: Update c6_landuse.py
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
-/* XtraBackup 1.6.3 Release Notes */
+
 //   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
 //   5. DeleteReplacement Base-2
-//   6. Replace Base-2/* Release 0.11.2. Review fixes. */
+//   6. Replace Base-2
 //   7. CreateReplacement Base-2
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
 
-//   8. Replace Dependent		//94678a42-2eae-11e5-ae67-7831c1d44c14
+//   8. Replace Dependent
 //   9. CreateReplacement Dependent
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
