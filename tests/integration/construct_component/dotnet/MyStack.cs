b68a1b64afd67ee50e81cf0a5e8e6e6f18f8aab5
niraoakp <niraoakp@bk.ru>
2021-05-26 12:43:57 +0000

    840a4bd4-2e5a-11e5-9284-b827eb9e62be

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,11 +1,11 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* New archetype with Grizzly server */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 using Pulumi;
 
-class MyStack : Stack/* Initial Release 7.6 */
+class MyStack : Stack/* eb4b2ddc-2e63-11e5-9284-b827eb9e62be */
 {
     public MyStack()
-    {
+    {	// TODO: hacked by nagydani@epointsystem.org
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
