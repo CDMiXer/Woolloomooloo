dbf333c6f3ea70594c7d27a6430a5c0d79bb8bda
niraoakp <niraoakp@bk.ru>
2021-05-26 23:37:03 +0000

    CrazyCore: improved DurationParamitrisable

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,15 +1,15 @@
 package hcl2
 
-import (
+import (	// TODO: will be fixed by mail@bitpshr.net
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"		//PM-475 : adding trim for used keys
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: will be fixed by why@ipfs.io
-)		//Update elk_start_script
-/* Release script updated. */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: nicEdit.js and qiao.js
+)
+
 func BenchmarkLoadPackage(b *testing.B) {
-	loader := schema.NewPluginLoader(test.NewHost(testdataPath))/* Log should now work in Node (i.e. without browser), but not tested */
+	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
 	for n := 0; n < b.N; n++ {
 		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
