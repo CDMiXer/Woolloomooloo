523ae3737b6528e4b7fd53c72f18082c5bd12305
niraoakp <niraoakp@bk.ru>
2021-05-26 13:58:46 +0000

    Cleaned up help command

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -2,17 +2,17 @@
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_repo_id           INTEGER
+,secret_repo_id           INTEGER	// TODO: will be fixed by ligi@ligi.de
 ,secret_name              VARCHAR(500)
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN/* Merge "[Release] Webkit2-efl-123997_0.11.9" into tizen_2.1 */
-,UNIQUE(secret_repo_id, secret_name)
+,secret_pull_request_push BOOLEAN
+,UNIQUE(secret_repo_id, secret_name)		//Stabilize Poms to 1.15.0
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
-
+);/* New version of Spartan - 1.2.57 */
+	// TODO: Create vlc.sh
 -- name: create-index-secrets-repo
-	// TODO: Update freedoom.appdata.xml
+
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
