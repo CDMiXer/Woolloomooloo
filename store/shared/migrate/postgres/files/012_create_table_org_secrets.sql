ce42088b8b5c97e0322f27734ab736550aefad5e
niraoakp <niraoakp@bk.ru>
2021-05-26 21:47:45 +0000

    * output only mesh...

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                SERIAL PRIMARY KEY
+ secret_id                SERIAL PRIMARY KEY	// Update with new VPN address
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN		//to_unicode function
+,secret_pull_request      BOOLEAN	// TODO: Update URL to main pancancer_launcher readme
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
