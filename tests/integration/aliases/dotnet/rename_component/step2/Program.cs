99b7dfbd9410f623ee03659859b4c8513b822e0b
niraoakp <niraoakp@bk.ru>
2021-05-26 05:23:37 +0000

    Describe Featuretypes global cache

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -9,29 +9,29 @@ class Resource : ComponentResource
         : base("my:module:Resource", name, options)
     {
     }
-}
+}		//Clean up the file
 
 // Scenario #3 - rename a component (and all it's children)
-// No change to the component itself.
+// No change to the component itself.	// fixed some minor language bug with overlays
 class ComponentThree : ComponentResource
 {
-    private Resource resource1;
+    private Resource resource1;		//remove dada's code
     private Resource resource2;
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
-    {
+    {/* Release of eeacms/ims-frontend:0.4.1-beta.2 */
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
+    }		//add onResourceChange with testcase.
 }
 
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)/* [releng] Release v6.10.5 */
     {
         return Deployment.RunAsync(() =>
         {
