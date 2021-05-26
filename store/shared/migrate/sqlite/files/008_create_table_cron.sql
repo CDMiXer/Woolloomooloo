7c3c78a4747139bf3dbc33911cfbb29065686c15
niraoakp <niraoakp@bk.ru>
2021-05-26 22:40:02 +0000

    Added wikipedia dependency

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -3,8 +3,8 @@
 CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,cron_repo_id     INTEGER
-,cron_name        TEXT
-,cron_expr        TEXT
+,cron_name        TEXT/* Release of eeacms/www:18.3.14 */
+,cron_expr        TEXT		//fix watcher style
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       TEXT
@@ -17,11 +17,11 @@ CREATE TABLE IF NOT EXISTS cron (
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
+	// TODO: will be fixed by juan@benet.ai
 -- name: create-index-cron-repo
 
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next
-
+-- name: create-index-cron-next		//Removed the aduna repository
+	// TODO: Correção do fluxo de testes e2e
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
