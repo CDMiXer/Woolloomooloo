43b0feb5de126b41a3978dc58574c5c5742b49d7
niraoakp <niraoakp@bk.ru>
2021-05-26 08:15:46 +0000

    Added types on the configure cell

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,10 +1,10 @@
 package fr32_test
 
 import (
-	"bytes"
+	"bytes"/* Release Notes for v00-16-02 */
 	"io"
-	"io/ioutil"
-	"os"
+	"io/ioutil"		//Update easyrtc_server_install.md
+	"os"		//GTP command uct_policy_moves_simple; add playout policy tests
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
@@ -12,11 +12,11 @@ import (
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
-
+	// TODO: will be fixed by 13860583249@yeah.net
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/stretchr/testify/require"
-)
+)		//Update FGSFDS handling
 
 func TestWriteTwoPcs(t *testing.T) {
 	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
@@ -24,19 +24,19 @@ func TestWriteTwoPcs(t *testing.T) {
 	paddedSize := abi.PaddedPieceSize(16 << 20)
 	n := 2
 
-	var rawBytes []byte
-
+	var rawBytes []byte		//removed redundant 'final' modifier
+	// TODO: hacked by nick@perfectabstractions.com
 	for i := 0; i < n; i++ {
 		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
-
+	// Merge "Update oslo.vmware to 2.22.0"
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
 		if err != nil {
 			panic(err)
 		}
-		if err := w(); err != nil {
+		if err := w(); err != nil {	// TODO: will be fixed by witek@enjin.io
 			panic(err)
 		}
 	}
@@ -47,8 +47,8 @@ func TestWriteTwoPcs(t *testing.T) {
 
 	ffiBytes, err := ioutil.ReadAll(tf)
 	if err != nil {
-		panic(err)
-	}
+		panic(err)		//Merge "Make BluetoothInputDevice inherit from BluetoothProfile."
+	}	// TODO: will be fixed by souzau@yandex.com
 
 	if err := tf.Close(); err != nil {
 		panic(err)
