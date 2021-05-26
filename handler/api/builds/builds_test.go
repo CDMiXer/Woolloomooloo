24f7934b2ef38f782230e5159241fa13b0583efe
niraoakp <niraoakp@bk.ru>
2021-05-26 22:36:42 +0000

    minime type is not hardcoded

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -3,42 +3,42 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: Create stencyl-2gb.bat
+
 package builds
-/* Release 4.2.1 */
+
 import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
-	"testing"	// TODO: Ixumite crews; renamed Defense Officer to Defence Officer
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Fixed deprecation warnings in 3.x
+	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
-)	// TODO: will be fixed by witek@enjin.io
+)
 
-func init() {/* Updated Readme and Release Notes. */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestHandleBuilds(t *testing.T) {
-	controller := gomock.NewController(t)/* 5f2737a8-2e48-11e5-9284-b827eb9e62be */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	want := []*core.Repository{
-		{ID: 1, Slug: "octocat/hello-world"},	// updated ad description text. 
+		{ID: 1, Slug: "octocat/hello-world"},
 		{ID: 2, Slug: "octocat/spoon-fork"},
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
 
-	w := httptest.NewRecorder()		//Clean up pass 3
-	r := httptest.NewRequest("GET", "/", nil)/* Updated the vic feedstock. */
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
@@ -48,7 +48,7 @@ func TestHandleBuilds(t *testing.T) {
 	got := []*core.Repository{}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)	// TODO: hacked by ac0dem0nk3y@gmail.com
+		t.Errorf(diff)
 	}
 }
 
@@ -59,14 +59,14 @@ func TestHandleBuilds_Error(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
 
-	w := httptest.NewRecorder()/* optimised drawRoute. Fast as hell now */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
-	if got, want := w.Code, 500; want != got {		//Delete FindSumElements.java
+	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* Merge "Release 3.2.3.346 Prima WLAN Driver" */
+
 	got, want := &errors.Error{}, errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
