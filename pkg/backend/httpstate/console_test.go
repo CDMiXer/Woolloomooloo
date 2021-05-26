b61c196affc4448d237d4fb33560eff8928d4e5f
niraoakp <niraoakp@bk.ru>
2021-05-26 05:55:20 +0000

    fix f_bcond here too

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* fix reporting query */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release v.1.4.0 */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+///* changed to faster xml parser */
+// Unless required by applicable law or agreed to in writing, software/* 2bee376a-2e4d-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,/* dos 3.3 for convenience */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release of version 2.3.0 */
 package httpstate
 
 import (
@@ -18,16 +18,16 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)
+)/* Release version: 1.13.0 */
 
-func TestConsoleURL(t *testing.T) {
+func TestConsoleURL(t *testing.T) {	// TODO: hacked by arachnid@notdot.net
 	t.Run("HonorEnvVar", func(t *testing.T) {
 		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
 		defer func() {
 			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)
 		}()
 
-		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.
+		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.		//Add delete with guard/route
 		os.Setenv("PULUMI_CONSOLE_DOMAIN", "pulumi-console.contoso.com")
 		assert.Equal(t,
 			"https://pulumi-console.contoso.com/1/2",
@@ -35,25 +35,25 @@ func TestConsoleURL(t *testing.T) {
 
 		// Unset the variable, confirm the "standard behavior" where we
 		// replace "api." with "app.".
-		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")
+		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")/* Release Notes for v00-16-02 */
 		assert.Equal(t,
 			"https://app.pulumi.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 	})
-
+/* Moved method from StorageManager to MongoVariationStorage */
 	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
 		assert.Equal(t,
 			"https://app.pulumi.com/pulumi-bot/my-stack",
-			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
+			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))/* package: fix dependencies */
 
-		assert.Equal(t,
+		assert.Equal(t,		//New update.json
 			"http://app.pulumi.example.com/pulumi-bot/my-stack",
-			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
+			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))/* Local commit. Disabled JS validator (causing errors in Eclipse). */
 	})
 
-	t.Run("LocalDevelopment", func(t *testing.T) {
+	t.Run("LocalDevelopment", func(t *testing.T) {/* Typo and formatiing fixed */
 		assert.Equal(t,
-			"http://localhost:3000/pulumi-bot/my-stack",
+			"http://localhost:3000/pulumi-bot/my-stack",	// TODO: will be fixed by hugomrdias@gmail.com
 			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))
 	})
 
