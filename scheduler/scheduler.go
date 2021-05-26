ffe8031e01b5afac04b1441e415a947f4c418979
niraoakp <niraoakp@bk.ru>
2021-05-26 06:24:10 +0000

    TgT1eCXVLrBjfCu4JMDw5Is01sccBaYv

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//7eb26a68-2e50-11e5-9284-b827eb9e62be
 // +build !oss
 
-package scheduler
+package scheduler	// Ihopskrivning: "kött rätter" -> "kötträtter"
