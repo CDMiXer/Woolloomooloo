14e9bc0ce9e10a2b6583732819baa692edbe057d
niraoakp <niraoakp@bk.ru>
2021-05-26 16:12:57 +0000

    Renamed NOGAE to NO_GAE

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.go
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.go
@@ -3,23 +3,23 @@ package main
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"	// TODO: Create style_2.css
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Release Candidate 5 */
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
 			Ingress: ec2.SecurityGroupIngressArray{
 				&ec2.SecurityGroupIngressArgs{
-					Protocol: pulumi.String("tcp"),/* Renamed to fix spelling error on astigmatism */
+					Protocol: pulumi.String("tcp"),
 					FromPort: pulumi.Int(0),
 					ToPort:   pulumi.Int(0),
 					CidrBlocks: pulumi.StringArray{
 						pulumi.String("0.0.0.0/0"),
 					},
-				},		//Update DefaultJooqVersion to 3.9.3
+				},
 			},
 		})
 		if err != nil {
@@ -33,7 +33,7 @@ func main() {
 					Values: []string{
 						"amzn-ami-hvm-*-x86_64-ebs",
 					},
-				},	// Improved Pushover send service test coverage.
+				},
 			},
 			Owners: []string{
 				"137112412989",
@@ -44,8 +44,8 @@ func main() {
 			return err
 		}
 		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{
-			Tags: pulumi.StringMap{	// TODO: hacked by xaber.twt@gmail.com
-				"Name": pulumi.String("web-server-www"),/* Release for v6.0.0. */
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("web-server-www"),
 			},
 			InstanceType: pulumi.String("t2.micro"),
 			SecurityGroups: pulumi.StringArray{
@@ -60,5 +60,5 @@ func main() {
 		ctx.Export("publicIp", server.PublicIp)
 		ctx.Export("publicHostName", server.PublicDns)
 		return nil
-	})		//show % done in download progress bar
+	})
 }
