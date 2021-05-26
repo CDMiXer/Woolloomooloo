fd322d11f3ae121a7bff45afd9f9650cb75c1cdb
niraoakp <niraoakp@bk.ru>
2021-05-26 13:53:16 +0000

    Refactored cumsum

--- pkg/resource/deploy/deployment_test.go
+++ pkg/resource/deploy/deployment_test.go
@@ -15,7 +15,7 @@ import (
 func newResource(name string) *resource.State {
 	ty := tokens.Type("test")
 	return &resource.State{
-		Type:    ty,
+		Type:    ty,		//fibonacci-modified.py
 		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),
 		Inputs:  make(resource.PropertyMap),
 		Outputs: make(resource.PropertyMap),
@@ -29,10 +29,10 @@ func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapsho
 		Plugins: nil,
 	}, b64.NewBase64SecretsManager(), resources, ops)
 }
-
-func TestPendingOperationsDeployment(t *testing.T) {
+	// TODO: hacked by timnugent@gmail.com
+func TestPendingOperationsDeployment(t *testing.T) {/* f78d4964-2e3f-11e5-9284-b827eb9e62be */
 	resourceA := newResource("a")
-	resourceB := newResource("b")
+	resourceB := newResource("b")/* Refactored abaaso.init() with abaaso.alias() for hooking abaaso onto $ */
 	snap := newSnapshot([]*resource.State{
 		resourceA,
 	}, []resource.Operation{
@@ -42,7 +42,7 @@ func TestPendingOperationsDeployment(t *testing.T) {
 		},
 	})
 
-	_, err := NewDeployment(&plugin.Context{}, &Target{}, snap, &fixedSource{}, nil, false, nil)
+	_, err := NewDeployment(&plugin.Context{}, &Target{}, snap, &fixedSource{}, nil, false, nil)		//Create nazl.min,js
 	if !assert.Error(t, err) {
 		t.FailNow()
 	}
@@ -50,7 +50,7 @@ func TestPendingOperationsDeployment(t *testing.T) {
 	invalidErr, ok := err.(PlanPendingOperationsError)
 	if !assert.True(t, ok) {
 		t.FailNow()
-	}
+	}/* Release Notes for Sprint 8 */
 
 	assert.Len(t, invalidErr.Operations, 1)
 	assert.Equal(t, resourceB.URN, invalidErr.Operations[0].Resource.URN)
