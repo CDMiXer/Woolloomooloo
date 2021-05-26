d177dba8ac109313a64272be56e99ae9f03d5da2
niraoakp <niraoakp@bk.ru>
2021-05-26 05:57:09 +0000

    b1bfd034-2e56-11e5-9284-b827eb9e62be

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: Update extrafilter.conf
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Automatic changelog generation for PR #11592 [ci skip] */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Added Unit-Tests to Properties
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Removing no longer required IE specific CSS rules. */
-// limitations under the License.	// TODO: Rename unit-3/picturegallery.html to HTML/unit-3/picturegallery.html
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* FIX multi-user-cred SQL migration not working with orphaned user creds */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// +build oss/* CWS-TOOLING: integrate CWS solaris11 */
+// +build oss	// TODO: missing assertion
 
 package collabs
-		//implement nested inner class as closure instead so it can access the global def
-import (
+
+import (	// Add Resources: Icons for buttons
 	"net/http"
-		//fixed cropping polygon bug
+	// TODO: will be fixed by davidad@alum.mit.edu
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-/* Ajout Russula aeruginea */
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// TODO: hacked by praveen@minio.io
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
+func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {		//Minor package refactoring and added unit tests.
 	return notImplemented
 }
 
 func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
-
-func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {	// TODO: Delete runtest.py
+		//Improve output text when processing test log in pb2combinations tests.
+func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
