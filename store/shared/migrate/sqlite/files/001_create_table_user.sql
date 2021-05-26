26b9f98da9780e038554916b70df597855564d1c
niraoakp <niraoakp@bk.ru>
2021-05-26 09:09:01 +0000

    template:fix memory leak

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
--- name: create-table-users	// Rename uiframeworks to uiframeworks.md
+-- name: create-table-users/* Released springjdbcdao version 1.7.13 */
 
-CREATE TABLE IF NOT EXISTS users (
- user_id            INTEGER PRIMARY KEY AUTOINCREMENT/* Remove "beta" tag from microprofile */
-,user_login         TEXT COLLATE NOCASE	// TODO: hacked by ac0dem0nk3y@gmail.com
+CREATE TABLE IF NOT EXISTS users (		//update deployer and EthereumInstance
+ user_id            INTEGER PRIMARY KEY AUTOINCREMENT
+,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
 ,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
-,user_active        BOOLEAN/* Create motioncraft.py */
-,user_avatar        TEXT
+NAELOOB        evitca_resu,
+,user_avatar        TEXT	// minor change to display of period selectors
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER/* Release: 0.4.1. */
+,user_synced        INTEGER/* Clean to reduse memory of APK */
 ,user_created       INTEGER
-,user_updated       INTEGER
+,user_updated       INTEGER/* Released springjdbcdao version 1.7.6 */
 ,user_last_login    INTEGER
 ,user_oauth_token   TEXT
-,user_oauth_refresh TEXT
-,user_oauth_expiry  INTEGER
-,user_hash          TEXT
+,user_oauth_refresh TEXT	// Lab color space, closes #1133
+,user_oauth_expiry  INTEGER/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
+,user_hash          TEXT	// TODO: will be fixed by mail@bitpshr.net
 ,UNIQUE(user_login COLLATE NOCASE)
 ,UNIQUE(user_hash)
-);/* Fix build button URL */
+);
