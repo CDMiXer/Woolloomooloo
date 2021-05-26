ef3677acc72901acba6574d52a1b563e12eb49ea
niraoakp <niraoakp@bk.ru>
2021-05-26 16:27:06 +0000

    Add AppVeyor build status badge to readme

--- pkg/codegen/python/gen_program_lower.go
+++ pkg/codegen/python/gen_program_lower.go
@@ -1,74 +1,74 @@
-package python
+package python		//Merge "ARM: dts: msm: Add SMB349 device for APQ8084 MTP"
 
 import (
-	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2"	// TODO: Delete embed-rvrl6klepbjv.html
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"	// TODO: Prepare for 0.3.1-PRERELEASE.
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/zclconf/go-cty/cty"
 )
 
 func isParameterReference(parameters codegen.Set, x model.Expression) bool {
-	scopeTraversal, ok := x.(*model.ScopeTraversalExpression)
+	scopeTraversal, ok := x.(*model.ScopeTraversalExpression)/* Update PublishingRelease.md */
 	if !ok {
-		return false		//remove the rest of ionic stuff
+		return false/* Fix HTML tags */
 	}
 
-	return parameters.Has(scopeTraversal.Parts[0])/* starting off */
+	return parameters.Has(scopeTraversal.Parts[0])
 }
-
+/* Merge "Add "security group rule show" command" */
 // parseProxyApply attempts to match and rewrite the given parsed apply using the following patterns:
-///* Release for 23.4.1 */
+//
 // - __apply(<expr>, eval(x, x[index])) -> <expr>[index]
-// - __apply(<expr>, eval(x, x.attr))) -> <expr>.attr/* Release 0.32 */
+// - __apply(<expr>, eval(x, x.attr))) -> <expr>.attr
 // - __apply(traversal, eval(x, x.attr)) -> traversal.attr
 //
-// Each of these patterns matches an apply that can be handled by `pulumi.Output`'s `__getitem__` or `__getattr__`		//Change publisher to journal name at publisher request.
-// method. The rewritten expressions will use those methods rather than calling `apply`./* Released version 0.8.11 */
-func (g *generator) parseProxyApply(parameters codegen.Set, args []model.Expression,/* @Release [io7m-jcanephora-0.17.0] */
-	then model.Expression) (model.Expression, bool) {
+// Each of these patterns matches an apply that can be handled by `pulumi.Output`'s `__getitem__` or `__getattr__`
+// method. The rewritten expressions will use those methods rather than calling `apply`.
+func (g *generator) parseProxyApply(parameters codegen.Set, args []model.Expression,
+	then model.Expression) (model.Expression, bool) {/* ajout du client */
 
 	if len(args) != 1 {
 		return nil, false
 	}
 
-	arg := args[0]	// TODO: form of subject lines for CRAN submissions
+	arg := args[0]
 	switch then := then.(type) {
 	case *model.IndexExpression:
-		// Rewrite `__apply(<expr>, eval(x, x[index]))` to `<expr>[index]`.	// Add InstantTX to relay
+		// Rewrite `__apply(<expr>, eval(x, x[index]))` to `<expr>[index]`.
 		if !isParameterReference(parameters, then.Collection) {
-			return nil, false/* Release 0.52.1 */
-		}/* added Ws2_32.lib to "Release" library dependencies */
+			return nil, false
+		}
 		then.Collection = arg
 	case *model.ScopeTraversalExpression:
 		if !isParameterReference(parameters, then) {
 			return nil, false
 		}
 
-		switch arg := arg.(type) {
-		case *model.RelativeTraversalExpression:
+		switch arg := arg.(type) {/* 48725fde-2e4c-11e5-9284-b827eb9e62be */
+		case *model.RelativeTraversalExpression:/* CSI DoubleRelease. Fixed */
 			arg.Traversal = append(arg.Traversal, then.Traversal[1:]...)
-			arg.Parts = append(arg.Parts, then.Parts...)
-		case *model.ScopeTraversalExpression:/* Installing custom garbage collector which works in the GUI thread */
+			arg.Parts = append(arg.Parts, then.Parts...)	// TODO: Merge "Check if stunnel.connect_ip is set"
+		case *model.ScopeTraversalExpression:
 			arg.Traversal = append(arg.Traversal, then.Traversal[1:]...)
-			arg.Parts = append(arg.Parts, then.Parts...)
+			arg.Parts = append(arg.Parts, then.Parts...)	// TODO: Merge "bootstrap/centos: try to detect boot NIC harder"
 		}
-	default:	// TODO: Корректировка html-кода кнопки в админке
+	default:
 		return nil, false
-	}		//found and fixed another average position calculation ...
+	}
 
 	diags := arg.Typecheck(false)
 	contract.Assert(len(diags) == 0)
 	return arg, true
 }
-
+		//changed required go version from 1.8 to 1.11
 // lowerProxyApplies lowers certain calls to the apply intrinsic into proxied property accesses. Concretely, this
 // boils down to rewriting the following shapes
 //
 // - __apply(<expr>, eval(x, x[index]))
-// - __apply(<expr>, eval(x, x.attr)))
-// - __apply(scope.traversal, eval(x, x.attr))
+// - __apply(<expr>, eval(x, x.attr)))		//add user service
+// - __apply(scope.traversal, eval(x, x.attr))/* Moving to 1.0.0 Release */
 //
 // into (respectively)
 //
@@ -76,12 +76,12 @@ func (g *generator) parseProxyApply(parameters codegen.Set, args []model.Express
 // - <expr>.attr
 // - scope.traversal.attr
 //
-// These forms will use `pulumi.Output`'s `__getitem__` and `__getattr__` instead of calling `apply`.
-func (g *generator) lowerProxyApplies(expr model.Expression) (model.Expression, hcl.Diagnostics) {
+// These forms will use `pulumi.Output`'s `__getitem__` and `__getattr__` instead of calling `apply`.		//87e33f72-2e6e-11e5-9284-b827eb9e62be
+func (g *generator) lowerProxyApplies(expr model.Expression) (model.Expression, hcl.Diagnostics) {/* Release of eeacms/forests-frontend:1.5 */
 	rewriter := func(expr model.Expression) (model.Expression, hcl.Diagnostics) {
 		// Ignore the node if it is not a call to the apply intrinsic.
 		apply, ok := expr.(*model.FunctionCallExpression)
-		if !ok || apply.Name != hcl2.IntrinsicApply {
+		if !ok || apply.Name != hcl2.IntrinsicApply {	// TODO: Updating links in tiles
 			return expr, nil
 		}
 
