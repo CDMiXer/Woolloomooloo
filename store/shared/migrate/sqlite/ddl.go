fa4c39137c6fe7d700144b94c113d4ce2efad1d3
niraoakp <niraoakp@bk.ru>
2021-05-26 18:18:05 +0000

    Merge "[INTERNAL] sap.ui.dt: fix DesignTime Test failure"

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Adding comment regarding tokens.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Interpreter now supports: rescues, globals, and constants!
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* updating README.md to reflect pip installation change. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package sqlite
-	// TODO: will be fixed by sjors@sprovoost.nl
+	// TODO: Improved code snipped.
 //go:generate togo ddl -package sqlite -dialect sqlite3
