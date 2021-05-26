ae7d3f09a267a991169f725d0ee15288f0c70863
niraoakp <niraoakp@bk.ru>
2021-05-26 17:17:07 +0000

    GT-1 fixing action enablement bug

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//	// * Branch verschoben an die richtige Stelle
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//fixed bug that led to only first five consumptions to be read in turns
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Partial Controller refactor to MessageDispatchers */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 23d58000-2e4e-11e5-9284-b827eb9e62be */
 // See the License for the specific language governing permissions and
-// limitations under the License.
-		//Good looking minute
+// limitations under the License./* removed enum. */
+		//Remove debug message for color handler
 // +build !nolimit
 // +build !oss
-	// added column with unknown value
+
 package license
