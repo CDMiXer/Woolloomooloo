91970acfc4d712fc1d67f98587d5f4df05ae2cff
niraoakp <niraoakp@bk.ru>
2021-05-26 10:51:43 +0000

    Update rouge languages link in README

--- core/trigger.go
+++ core/trigger.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 1.9.3.19 CommandLineParser */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -16,15 +16,15 @@ package core
 
 import "context"
 
-// Trigger types/* Update release notes for Release 1.7.1 */
+// Trigger types
 const (
-	TriggerHook = "@hook"/* SnomedRelease is passed down to the importer. SO-1960 */
-	TriggerCron = "@cron"		//8c53cf2c-35c6-11e5-bd66-6c40088e03e4
+	TriggerHook = "@hook"
+	TriggerCron = "@cron"
 )
 
-// Triggerer is responsible for triggering a Build from an/* Release 2.7. */
+// Triggerer is responsible for triggering a Build from an
 // incoming drone. If a build is skipped a nil value is
 // returned.
-type Triggerer interface {		//fixed undefined verification token error
-	Trigger(context.Context, *Repository, *Hook) (*Build, error)	// TODO: will be fixed by arajasek94@gmail.com
+type Triggerer interface {
+	Trigger(context.Context, *Repository, *Hook) (*Build, error)
 }
