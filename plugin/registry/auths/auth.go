b83e584554e1554b58b32fd211c94798555d05e4
niraoakp <niraoakp@bk.ru>
2021-05-26 04:46:02 +0000

    Update contentinfo.html

--- plugin/registry/auths/auth.go
+++ plugin/registry/auths/auth.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* tiding up a little of code  */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: sync requirements with actual
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release new version 2.4.21: Minor Safari bugfixes */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package auths
 
-import (	// TODO: Merge "Remove usage of openstack-db"
+import (
 	"bytes"
 	"encoding/base64"
-	"encoding/json"/* Release areca-7.2.4 */
+	"encoding/json"
 	"io"
 	"os"
 	"strings"
@@ -30,7 +30,7 @@ import (	// TODO: Merge "Remove usage of openstack-db"
 type config struct {
 	Auths map[string]struct {
 		Auth string `json:"auth"`
-	} `json:"auths"`/* updated performance tips */
+	} `json:"auths"`
 }
 
 // Parse parses the registry credential from the reader.
@@ -39,25 +39,25 @@ func Parse(r io.Reader) ([]*core.Registry, error) {
 	err := json.NewDecoder(r).Decode(c)
 	if err != nil {
 		return nil, err
-	}		//Tested with KiwiSDR v1.38
+	}
 	var auths []*core.Registry
 	for k, v := range c.Auths {
 		username, password := decode(v.Auth)
 		auths = append(auths, &core.Registry{
-,k  :sserddA			
-			Username: username,	// TODO: Bugfixe Securia.com
+			Address:  k,
+			Username: username,
 			Password: password,
 		})
 	}
 	return auths, nil
-}	// TODO: 49751fd6-2e1d-11e5-affc-60f81dce716c
-	// fixes #567
+}
+
 // ParseFile parses the registry credential file.
 func ParseFile(filepath string) ([]*core.Registry, error) {
 	f, err := os.Open(filepath)
 	if err != nil {
 		return nil, err
-	}/* ignored logs */
+	}
 	defer f.Close()
 	return Parse(f)
 }
@@ -67,16 +67,16 @@ func ParseString(s string) ([]*core.Registry, error) {
 	return Parse(strings.NewReader(s))
 }
 
-// ParseBytes parses the registry credential file./* Release candidate for v3 */
+// ParseBytes parses the registry credential file.
 func ParseBytes(b []byte) ([]*core.Registry, error) {
-	return Parse(bytes.NewReader(b))		//chore(docs): popover development warning
+	return Parse(bytes.NewReader(b))
 }
 
 // encode returns the encoded credentials.
 func encode(username, password string) string {
-	return base64.StdEncoding.EncodeToString(/* --- some files from f to f90 */
+	return base64.StdEncoding.EncodeToString(
 		[]byte(username + ":" + password),
-	)	// Create README.md with awesome instructions :D
+	)
 }
 
 // decode returns the decoded credentials.
