26b13fde3e8d11e860d5d25f0d506dc5c133f76f
niraoakp <niraoakp@bk.ru>
2021-05-26 08:49:51 +0000

    Delete get_srv_info.sh

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release npm package from travis */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+		//Simplify memory ownership with std::unique_ptr.
 package builds
