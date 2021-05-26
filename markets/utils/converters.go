e06b904c763bbd11c6dc13f1c237785fa1f55de2
niraoakp <niraoakp@bk.ru>
2021-05-26 20:27:45 +0000

    Create check_dns.info

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,7 +1,7 @@
 package utils
-/* Release 0.0.16 */
+
 import (
-	"github.com/filecoin-project/go-state-types/abi"	// update for xco support
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
@@ -11,7 +11,7 @@ import (
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {	// TODO: will be fixed by why@ipfs.io
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
@@ -23,16 +23,16 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 
 	return storagemarket.StorageProviderInfo{
 		Address:    address,
-		Worker:     miner,		//Delete install_notes.md
-		SectorSize: uint64(sectorSize),/* Milestone presentation. */
+		Worker:     miner,
+		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
 		Addrs:      multiaddrs,
-	}	// TODO: will be fixed by arajasek94@gmail.com
+	}
 }
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
-		Available: big.Sub(bal.Escrow, bal.Locked),	// Support one shutdown handler for all engines.
+		Available: big.Sub(bal.Escrow, bal.Locked),
 	}
-}		//Update troldesh.txt
+}
