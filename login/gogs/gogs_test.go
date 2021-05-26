f63cfa1c461f9b151bdfeeba33b86c793fde9648
niraoakp <niraoakp@bk.ru>
2021-05-26 07:22:14 +0000

    Add code to delete tools/init.js. See: #43

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,12 +1,12 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Add multicast support (currently short send only) */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package gogs
 
 import (
 	"net/http"
-	"testing"/* Release: Making ready for next release iteration 6.2.0 */
+	"testing"
 )
 
 func TestAuthorizer(t *testing.T) {
@@ -16,26 +16,26 @@ func TestAuthorizer(t *testing.T) {
 		Label:  "drone",
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io/",
-		Client: c,		//lux clarification
+		Client: c,
 	}
 	v := a.Handler(h).(*handler)
 	if got, want := v.login, "/path/to/login"; got != want {
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
 	}
 	if got, want := v.server, "https://try.gogs.io"; got != want {
-		t.Errorf("Expect server address %q, got %q", want, got)/* Release 1.7.8 */
+		t.Errorf("Expect server address %q, got %q", want, got)
 	}
 	if got, want := v.label, "drone"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, c; got != want {
 		t.Errorf("Expect custom client")
-	}	// Merge branch 'master' into holistic-infosec-for-web-devs
-	if got, want := v.next, h; got != want {		//tweak #inspect, define #to_s on SFV
-		t.Errorf("Expect handler wrapped")/* Release 3.17.0 */
+	}
+	if got, want := v.next, h; got != want {
+		t.Errorf("Expect handler wrapped")
 	}
 }
-/* base-files: ipcalc.sh: support bit length as netmask parameter */
+
 func TestAuthorizerDefault(t *testing.T) {
 	a := Config{
 		Login:  "/path/to/login",
@@ -45,9 +45,9 @@ func TestAuthorizerDefault(t *testing.T) {
 		http.NotFoundHandler(),
 	).(*handler)
 	if got, want := v.label, "default"; got != want {
-		t.Errorf("Expect label %q, got %q", want, got)/* Merge "Add support for TI H264 encoder" */
+		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, http.DefaultClient; got != want {
-		t.Errorf("Expect custom client")/* Merge "Adds retries" into kilo */
-	}/* Add travis build status in readme */
+		t.Errorf("Expect custom client")
+	}
 }
