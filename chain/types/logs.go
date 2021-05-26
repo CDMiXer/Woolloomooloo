d03f7b8e652e09fcce3f6d24c060052c070c379a
niraoakp <niraoakp@bk.ru>
2021-05-26 10:46:19 +0000

    Validating AVAILABLE defenses (not crossings count)

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,13 +1,13 @@
 package types
-
+/* Released new version 1.1 */
 import (
 	"github.com/ipfs/go-cid"
-	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zapcore"/* changing the order of this field */
 )
 
 type LogCids []cid.Cid
 
-var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
+var _ zapcore.ArrayMarshaler = (*LogCids)(nil)	// Fix copypasta error
 
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
