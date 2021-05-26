ab304cce1c47c7e9eb6c4abf0396ec7d84e6f079
niraoakp <niraoakp@bk.ru>
2021-05-26 22:04:33 +0000

    file/folder operations by name using regex

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,32 +1,32 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* added a boolean matcher */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* KYLIN-1154 cleanup old job from metadata store */
+// Licensed under the Apache License, Version 2.0 (the "License");	// Added a test for the tile grid rendering system to prove it works.
+// you may not use this file except in compliance with the License.		//Create ggkk
 // You may obtain a copy of the License at
-//
+//		//Rename README.txt to LeapRTC.README.txt
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fix table cell parser
-// See the License for the specific language governing permissions and
-// limitations under the License./* ef0dbe84-2e5a-11e5-9284-b827eb9e62be */
+///* JSDemoApp should be GC in Release too */
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by jon@atack.com
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.94.364 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Changes pull requests to be aimed at dev not master
+// See the License for the specific language governing permissions and/* Add skeleton for the ReleaseUpgrader class */
+// limitations under the License.
 
 package user
-/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
+
 import (
 	"net/http"
-
-	"github.com/drone/drone/handler/api/render"
+/* document progress bar message */
+	"github.com/drone/drone/handler/api/render"		//Updated projects for new version
 	"github.com/drone/drone/handler/api/request"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body.
-func HandleFind() http.HandlerFunc {	// Little design fix
+func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()		//Added gcc version check.
+		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
-		render.JSON(w, viewer, 200)/* Release v1.0.6. */
-	}/* Refactor reusable code into helper class. */
+		render.JSON(w, viewer, 200)
+	}
 }
