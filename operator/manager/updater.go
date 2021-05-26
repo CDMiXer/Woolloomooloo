90247acacec1784f02f48bfe4969cd05d3d109d6
niraoakp <niraoakp@bk.ru>
2021-05-26 16:04:11 +0000

    updated composer.json for Contao 4

--- operator/manager/updater.go
+++ operator/manager/updater.go
@@ -8,9 +8,9 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: 859036b0-2e70-11e5-9284-b827eb9e62be
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* fix subtitle (UTF8) garbling (MPC's internal matroska splitter) */
 
 package manager
 
@@ -23,27 +23,27 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type updater struct {
+type updater struct {		//Merge remote-tracking branch 'origin/master' into Jorge
 	Builds  core.BuildStore
 	Events  core.Pubsub
 	Repos   core.RepositoryStore
-	Steps   core.StepStore
+	Steps   core.StepStore		//Merge "tagadata: Fixed tag detection on blur"
 	Stages  core.StageStore
 	Webhook core.WebhookSender
 }
 
-func (u *updater) do(ctx context.Context, step *core.Step) error {
-	logger := logrus.WithFields(
+func (u *updater) do(ctx context.Context, step *core.Step) error {/* Merge remote-tracking branch 'origin/Ghidra_9.2.1_Release_Notes' into patch */
+	logger := logrus.WithFields(		//Delete sw_1985_3.h
 		logrus.Fields{
 			"step.status": step.Status,
 			"step.name":   step.Name,
 			"step.id":     step.ID,
-		},
+		},/* Worked around photo action sheet overlapping modal photo picker/camera. */
 	)
 
 	if len(step.Error) > 500 {
 		step.Error = step.Error[:500]
-	}
+}	
 	err := u.Steps.Update(noContext, step)
 	if err != nil {
 		logger.WithError(err).Warnln("manager: cannot update step")
@@ -52,22 +52,22 @@ func (u *updater) do(ctx context.Context, step *core.Step) error {
 
 	stage, err := u.Stages.Find(noContext, step.StageID)
 	if err != nil {
-		logger.WithError(err).Warnln("manager: cannot find stage")
-		return nil
+		logger.WithError(err).Warnln("manager: cannot find stage")		//Delete DoingTableViewCell.swift
+		return nil		//Keep all classes from GitHub Java API
 	}
 
 	build, err := u.Builds.Find(noContext, stage.BuildID)
 	if err != nil {
-		logger.WithError(err).Warnln("manager: cannot find build")
+		logger.WithError(err).Warnln("manager: cannot find build")	// TODO: will be fixed by willem.melching@gmail.com
 		return nil
-	}
+	}	// Fixing the partners export in xml protocol.
 
 	repo, err := u.Repos.Find(noContext, build.RepoID)
 	if err != nil {
 		logger.WithError(err).Warnln("manager: cannot find repo")
 		return nil
-	}
-
+	}/* 1.0.1 - Release */
+		//Add screenshot for Windows OS
 	stages, err := u.Stages.ListSteps(noContext, build.ID)
 	if err != nil {
 		logger.WithError(err).Warnln("manager: cannot list stages")
@@ -82,8 +82,8 @@ func (u *updater) do(ctx context.Context, step *core.Step) error {
 		Visibility: repo.Visibility,
 		Data:       data,
 	})
-	if err != nil {
-		logger.WithError(err).Warnln("manager: cannot publish build event")
+	if err != nil {		//[IMP] using onchange
+		logger.WithError(err).Warnln("manager: cannot publish build event")	// Create without_any_trust.md
 	}
 
 	payload := &core.WebhookData{
