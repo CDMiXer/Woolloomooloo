5bd37c2e55947946b9fb1562222c72562c39969f
niraoakp <niraoakp@bk.ru>
2021-05-26 11:46:50 +0000

    Update trending_tester.yml

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,47 +1,47 @@
 package wallet
-
+	// TODO: changed default fetch interval to 60 seconds
 import (
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)		//Tweak to gammas. 
+	// TODO: hacked by ng8eke@163.com
+type MemKeyStore struct {/* Preparing for 2.0 GA Release */
+	m map[string]types.KeyInfo		//Fix LP #258124 and crash when copying LPEs
+}	// Updated changelog so generated deb is signed by Akiban build user.
 
-type MemKeyStore struct {
-	m map[string]types.KeyInfo
-}
-
-func NewMemKeyStore() *MemKeyStore {
-	return &MemKeyStore{
+func NewMemKeyStore() *MemKeyStore {		//Deleted file-master repo as it's not required anymore
+	return &MemKeyStore{/* Update MifareClassicValueBlock.ino */
 		make(map[string]types.KeyInfo),
 	}
 }
 
-// List lists all the keys stored in the KeyStore
-func (mks *MemKeyStore) List() ([]string, error) {
+// List lists all the keys stored in the KeyStore		//Update PinnedItem.psd1
+func (mks *MemKeyStore) List() ([]string, error) {	// TODO: Changes in the bankstatement views.
 	var out []string
 	for k := range mks.m {
 		out = append(out, k)
 	}
 	return out, nil
 }
-
+		//Create connect_no.svg
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
-	}
+	}/* Added the SWTableViewCell framework. */
 
 	return ki, nil
-}
+}/* add new script and update earth* scripts */
 
-// Put saves a key info under given name
+// Put saves a key info under given name/* --host-reference --> --host_reference */
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
-}
+}/* Release dev-15 */
 
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
-	delete(mks.m, k)
+	delete(mks.m, k)		//new api, in preparation for concurrent execution. Remove the example javascript.
 	return nil
 }
 
