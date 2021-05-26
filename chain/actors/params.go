3ffc1b6c96edff6ad226d9e76b05373d76d37608
niraoakp <niraoakp@bk.ru>
2021-05-26 04:58:41 +0000

    Adds 2.0.X to changelog

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
-
-import (
+/* Rename Release/cleaveore.2.1.js to Release/2.1.0/cleaveore.2.1.js */
+import (		//46b04942-2e72-11e5-9284-b827eb9e62be
 	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/go-state-types/exitcode"/* Change "History" => "Release Notes" */
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
-	buf := new(bytes.Buffer)
+	buf := new(bytes.Buffer)/* b661e20c-2e6b-11e5-9284-b827eb9e62be */
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
-		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")/* Release Checklist > Bugs List  */
+		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
 	return buf.Bytes(), nil
 }
