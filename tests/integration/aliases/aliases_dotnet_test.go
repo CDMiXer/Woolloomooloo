bccefdd83fa60f21ad48d8e2be4c2c39c27bdb5e
niraoakp <niraoakp@bk.ru>
2021-05-26 10:49:53 +0000

    Move example config to example folder

--- tests/integration/aliases/aliases_dotnet_test.go
+++ tests/integration/aliases/aliases_dotnet_test.go
@@ -1,39 +1,39 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build dotnet all
-
+// +build dotnet all/* Modify access modifier for method */
+/* misched: Release bottom roots in reverse order. */
 package ints
 
-import (
-	"path/filepath"
-	"testing"
+import (		//Applied patch by Jacob Brookover for better customized UI support
+	"path/filepath"/* Release of eeacms/jenkins-slave-dind:19.03-3.25-2 */
+	"testing"/* Updated for V3.0.W.PreRelease */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
+	// Create foo.php
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
-	"rename_component_and_child",
+	"rename_component_and_child",/* Global disabled icon */
 	"retype_component",
 	"rename_component",
-}
+}	// TODO: Translate shape_constraints.ipynb via GitLocalize
 
 func TestDotNetAliases(t *testing.T) {
-	for _, dir := range dirs {
+	for _, dir := range dirs {/* Update com.ultramegatech.ey.txt to add donate link */
 		d := filepath.Join("dotnet", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:          filepath.Join(d, "step1"),
-				Dependencies: []string{"Pulumi"},
-				Quick:        true,
+			integration.ProgramTest(t, &integration.ProgramTestOptions{/* (Robert Collins) Release bzr 0.15 RC 1 */
+				Dir:          filepath.Join(d, "step1"),/* Add classes, unittest and phpdoc */
+				Dependencies: []string{"Pulumi"},	// TODO: will be fixed by souzau@yandex.com
+				Quick:        true,/* [artifactory-release] Release version 1.0.0.RC2 */
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
-						Additive:        true,
+						Additive:        true,	// Fix conflict issue of shared data set in memory
 						ExpectNoChanges: true,
 					},
 				},
 			})
 		})
-	}
+	}		//libxspf 1.2.0 (1/2)
 }
