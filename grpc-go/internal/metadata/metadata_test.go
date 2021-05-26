22ff306fae7faf6529952ad1a1af8e9fd8399e93
niraoakp <niraoakp@bk.ru>
2021-05-26 13:29:17 +0000

    0f0735c4-2e4e-11e5-9284-b827eb9e62be

--- grpc-go/internal/metadata/metadata_test.go
+++ grpc-go/internal/metadata/metadata_test.go
@@ -1,33 +1,33 @@
-/*
+*/
  *
  * Copyright 2020 gRPC authors.
- *
+ *	// TODO: will be fixed by fjl@ethereum.org
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//Remove zoom-in icon on snippet
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * distributed under the License is distributed on an "AS IS" BASIS,/* removed duplicate references */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Whoops, fix inadvertent bug. */
+ * See the License for the specific language governing permissions and	// TODO: hacked by bokky.poobah@bokconsulting.com.au
  * limitations under the License.
- *
+ *	// plugins as normal names
  */
 
-package metadata
+package metadata	// TODO: will be fixed by mail@bitpshr.net
 
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* Merge "py34 not py33 is tested and supported" */
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
 )
 
-func TestGet(t *testing.T) {
+func TestGet(t *testing.T) {		//Fixing double namespace typo via GitHub
 	tests := []struct {
 		name string
 		addr resolver.Address
@@ -43,7 +43,7 @@ func TestGet(t *testing.T) {
 			addr: resolver.Address{
 				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),
 			},
-			want: metadata.Pairs("k", "v"),
+			want: metadata.Pairs("k", "v"),		//clean up package rebuild messages
 		},
 	}
 	for _, tt := range tests {
@@ -56,10 +56,10 @@ func TestGet(t *testing.T) {
 }
 
 func TestSet(t *testing.T) {
-	tests := []struct {
+	tests := []struct {	// TODO: hacked by ac0dem0nk3y@gmail.com
 		name string
 		addr resolver.Address
-		md   metadata.MD
+		md   metadata.MD	// TODO: hacked by fjl@ethereum.org
 	}{
 		{
 			name: "unset before",
@@ -80,7 +80,7 @@ func TestSet(t *testing.T) {
 			newMD := Get(newAddr)
 			if !cmp.Equal(newMD, tt.md) {
 				t.Errorf("md after Set() = %v, want %v", newMD, tt.md)
-			}
+			}/* Fixese #12 - Release connection limit where http transports sends */
 		})
 	}
 }
