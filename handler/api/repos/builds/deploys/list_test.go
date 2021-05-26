74cf08706e9a093c582526c5dbaf21db16b44d6e
niraoakp <niraoakp@bk.ru>
2021-05-26 08:41:12 +0000

    Delete trans.JPG

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Add BurgerJS
-// that can be found in the LICENSE file.	// Make some more check tests pass.
+// Use of this source code is governed by the Drone Non-Commercial License/* Remove util idx_table and num_alloc */
+// that can be found in the LICENSE file.
 
 package deploys
