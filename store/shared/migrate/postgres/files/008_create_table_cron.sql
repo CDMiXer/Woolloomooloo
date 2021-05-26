6dfd286fc9725cbaa309ffe26eb76c5d036a9797
niraoakp <niraoakp@bk.ru>
2021-05-26 01:45:14 +0000

    fiber stuff

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,9 +1,9 @@
 -- name: create-table-cron
 
-CREATE TABLE IF NOT EXISTS cron (
- cron_id          SERIAL PRIMARY KEY/* [skip ci] update yii2-codeception version */
+CREATE TABLE IF NOT EXISTS cron (	// Merge "Delete unnecessary groupadd in multinode-lab"
+ cron_id          SERIAL PRIMARY KEY/* Delete XPloadsion - XPloadsive Love [LDGM Release].mp3 */
 ,cron_repo_id     INTEGER
-,cron_name        VARCHAR(50)
+,cron_name        VARCHAR(50)	// TODO: Merge branch 'develop' into cithomas/tpondefaultsink
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER/* Begin removing systems Hungarian notation from public fields.  */
+,cron_created     INTEGER
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS cron (
 
 -- name: create-index-cron-repo
 
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);/* Relax version constraint for upcoming Flow 6 */
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
-/* Release 0.8.0~exp3 */
+
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
