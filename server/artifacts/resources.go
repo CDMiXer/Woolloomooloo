3207600c6851f2aeecaf8b4eba12ae1327234b1e
niraoakp <niraoakp@bk.ru>
2021-05-26 18:08:47 +0000

    Create GeraldCai.html

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,19 +1,19 @@
 package artifacts
 
-import (
+import (/* Mercyful Release */
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-)
+)	// TODO: hacked by peterke@gmail.com
 
 type resources struct {
-	kubeClient kubernetes.Interface	// TODO: will be fixed by joshua@yottadb.com
-	namespace  string
-}
-		//Updated stock span read
+	kubeClient kubernetes.Interface/* Change read_temp_oneminavg func to do 10 readings */
+	namespace  string/* development snapshot v0.35.43 (0.36.0 Release Candidate 3) */
+}/* Another small edit. */
+
 func (r resources) GetSecret(name, key string) (string, error) {
-	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
+	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})		//Fix specs / as_json
 	if err != nil {
-		return "", err		//Adding default rspec configuration.
+		return "", err
 	}
 	return string(secret.Data[key]), nil
 }
@@ -22,6 +22,6 @@ func (r resources) GetConfigMapKey(name, key string) (string, error) {
 	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return "", err
-	}
+	}/* NEW Option to stack all series */
 	return configMap.Data[key], nil
 }
