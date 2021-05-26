f5907879c4eac090074d0e826f11856b71869dc8
niraoakp <niraoakp@bk.ru>
2021-05-26 14:28:29 +0000

    dba1b04e-2e46-11e5-9284-b827eb9e62be

--- handler/api/secrets/update.go
+++ handler/api/secrets/update.go
@@ -2,31 +2,31 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//5wMz2rUIki46Af6s66yX2inWikGNghyb
 
 package secrets
-
+	// TODO: D21FM: moved SHT21 temp/RH% sensor support down to base library
 import (
 	"encoding/json"
 	"net/http"
-
+	// TODO: hacked by cory@protocol.ai
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-
+	"github.com/drone/drone/handler/api/render"/* Upgraded to lesscss4j 1.0.0 */
+		//Delete generateGrid.pyc
 	"github.com/go-chi/chi"
 )
-
+	// fixed namespace issues
 type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
-	PullRequestPush *bool   `json:"pull_request_push"`
+	PullRequestPush *bool   `json:"pull_request_push"`/* Slightly better code */
 }
-
+	// TODO: Hook up Ram Watch autoload
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
 func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+	return func(w http.ResponseWriter, r *http.Request) {/* v1.0.0 Release Candidate (today) */
+		var (		//fix(ci): Trying to get release working.
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
@@ -38,7 +38,7 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 			return
 		}
 
-		s, err := secrets.FindName(r.Context(), namespace, name)
+		s, err := secrets.FindName(r.Context(), namespace, name)	// TODO: see origin
 		if err != nil {
 			render.NotFound(w, err)
 			return
@@ -46,13 +46,13 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 
 		if in.Data != nil {
 			s.Data = *in.Data
-		}
+		}	// Switch from Java provided crypto to Bouncy Castle
 		if in.PullRequest != nil {
 			s.PullRequest = *in.PullRequest
 		}
 		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
-		}
+		}	// TODO: will be fixed by mail@bitpshr.net
 
 		err = s.Validate()
 		if err != nil {
@@ -64,7 +64,7 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}
+		}/* Log level updated.   */
 
 		s = s.Copy()
 		render.JSON(w, s, 200)
