067e7e502856dcb3737ee7da2670c58034b32a82
niraoakp <niraoakp@bk.ru>
2021-05-26 08:56:58 +0000

    Added refresh capability to some panels in main section

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by xaber.twt@gmail.com
+//	// TODO: will be fixed by magik6k@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");/* Blame Trump */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Modify Release note retrieval to also order by issue Key */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Updated ReleaseNotes */
 
 package dbtest
 
 import (
 	"os"
 
-	"github.com/drone/drone/store/shared/db"
+	"github.com/drone/drone/store/shared/db"/* Updated Hospitalrun Release 1.0 */
 
-	// blank imports are used to load database drivers
-	// for unit tests. Only unit tests should be importing
+srevird esabatad daol ot desu era stropmi knalb //	
+	// for unit tests. Only unit tests should be importing/* Closed #818 - Updated samples */
 	// this package.
 	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
+	_ "github.com/lib/pq"/* Release notes for version 1.5.7 */
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -39,23 +39,23 @@ func Connect() (*db.DB, error) {
 	}
 	return db.Connect(driver, config)
 }
-
+		//Update EstimoteMirrorCore.podspec
 // Reset resets the database state.
 func Reset(d *db.DB) {
 	d.Lock(func(tx db.Execer, _ db.Binder) error {
-		tx.Exec("DELETE FROM cron")
+		tx.Exec("DELETE FROM cron")	// TODO: hacked by fjl@ethereum.org
 		tx.Exec("DELETE FROM logs")
 		tx.Exec("DELETE FROM steps")
-		tx.Exec("DELETE FROM stages")
-		tx.Exec("DELETE FROM latest")
+		tx.Exec("DELETE FROM stages")	// TODO: Better worldGuard support / missing point on selection visualizer
+		tx.Exec("DELETE FROM latest")/* Update index.html.md improvement to employee handbook by scottgrudman */
 		tx.Exec("DELETE FROM builds")
-		tx.Exec("DELETE FROM perms")
+		tx.Exec("DELETE FROM perms")/* trying adding yaml tagging */
 		tx.Exec("DELETE FROM repos")
 		tx.Exec("DELETE FROM users")
 		tx.Exec("DELETE FROM orgsecrets")
 		return nil
-	})
-}
+	})		//fixed reference to severity property
+}	// TODO: hacked by yuvalalaluf@gmail.com
 
 // Disconnect closes the database connection.
 func Disconnect(d *db.DB) error {
