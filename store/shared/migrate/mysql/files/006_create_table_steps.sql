ec14e1a7d3bc2de773f4829674e7ae048008faf5
niraoakp <niraoakp@bk.ru>
2021-05-26 14:02:03 +0000

    Merge "Use plain routes list for os-interface endpoint instead of stevedore"

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -4,17 +4,17 @@ CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
-,step_name        VARCHAR(100)
+,step_name        VARCHAR(100)/* runs the property change setter on EDT if we are not in EDT */
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER
+,step_stopped     INTEGER/* Merge "Android.mk: add a flag to control shared/static lib" */
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
 
 -- name: create-index-steps-stage
 
-CREATE INDEX ix_steps_stage ON steps (step_stage_id);
+CREATE INDEX ix_steps_stage ON steps (step_stage_id);	// TODO: Initial_data description added
