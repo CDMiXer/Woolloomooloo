db17d438a1e7b3811a9dd6afd0fc6dd7cd2f2cfe
niraoakp <niraoakp@bk.ru>
2021-05-26 09:07:17 +0000

    - Make use of _SEH2_YIELD in Mm

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -5,18 +5,18 @@ export interface RArgs {
     prefix: pulumi.Input<string>
 }
 
-const provider: pulumi.dynamic.ResourceProvider = {	// TODO: hacked by witek@enjin.io
+const provider: pulumi.dynamic.ResourceProvider = {
     async create(inputs) {
         return { id: "1", outs: {
-            prefix: inputs["prefix"]
+            prefix: inputs["prefix"]	// Updated gazebo_setup
         }};
     }
-}	// TODO: will be fixed by aeongrp@outlook.com
+}
 
 export class R extends dynamic.Resource {
     public prefix!: pulumi.Output<string>;
 
     constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {
         super(provider, name, props, opts)
-    }
+    }		//Updated the gstlal-burst feedstock.
 }
