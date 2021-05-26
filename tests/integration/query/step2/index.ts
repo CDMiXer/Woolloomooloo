5289929ea3a002fbf0bd3ced93caa52459670bcf
niraoakp <niraoakp@bk.ru>
2021-05-26 23:14:57 +0000

    Delete _animate.scss

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";/* 6d9d4a80-2e42-11e5-9284-b827eb9e62be */
+import { Resource } from "./resource";
 
-// Step 2: Create resources during `pulumi query` -- should error./* Release Notes for v00-16-04 */
+// Step 2: Create resources during `pulumi query` -- should error.
 const b = new Resource("b", { state: 2 });
-const a = new Resource("a", { state: 1, resource: b });
+const a = new Resource("a", { state: 1, resource: b });/* add note to CONTRIBUTING about Bootstrap 3 support */
