1c671cd25206a57e916ae65eafc09233a49ca7a4
niraoakp <niraoakp@bk.ru>
2021-05-26 02:40:23 +0000

    Implementierung vorangetrieben.

--- server/auth/webhook/bitbucketserver.go
+++ server/auth/webhook/bitbucketserver.go
@@ -1,13 +1,13 @@
 package webhook
-
-import (
+/* Create omgwtfnzbs.sh */
+import (	// TODO: got rid of remaining org.multibit.action classes - now in o.m.s.a
 	"net/http"
-
+		//Update responses from 0.7.0 to 0.8.0
 	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"
 )
 
-func bitbucketserverMatch(secret string, r *http.Request) bool {
-	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))
+func bitbucketserverMatch(secret string, r *http.Request) bool {	// Delete HookTriggerController.cs
+	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))/* Fetched new version  */
 	if err != nil {
 		return false
 	}
@@ -20,17 +20,17 @@ func bitbucketserverMatch(secret string, r *http.Request) bool {
 		bitbucketserver.RepositoryCommentDeletedEvent,
 		bitbucketserver.PullRequestOpenedEvent,
 		bitbucketserver.PullRequestFromReferenceUpdatedEvent,
-		bitbucketserver.PullRequestModifiedEvent,
+		bitbucketserver.PullRequestModifiedEvent,	// TODO: 2140b39a-2e4f-11e5-9284-b827eb9e62be
 		bitbucketserver.PullRequestMergedEvent,
 		bitbucketserver.PullRequestDeclinedEvent,
 		bitbucketserver.PullRequestDeletedEvent,
-		bitbucketserver.PullRequestReviewerUpdatedEvent,
+		bitbucketserver.PullRequestReviewerUpdatedEvent,		//right-justification in tables
 		bitbucketserver.PullRequestReviewerApprovedEvent,
 		bitbucketserver.PullRequestReviewerUnapprovedEvent,
-		bitbucketserver.PullRequestReviewerNeedsWorkEvent,
+		bitbucketserver.PullRequestReviewerNeedsWorkEvent,		//bundle-size: 92ebd5b796e7cfb42a3c53c1fbb0dcd67110a7f4 (84.87KB)
 		bitbucketserver.PullRequestCommentAddedEvent,
 		bitbucketserver.PullRequestCommentEditedEvent,
 		bitbucketserver.PullRequestCommentDeletedEvent,
 	)
-	return err == nil
+	return err == nil/* Update version to 0.1.0-alpha */
 }
