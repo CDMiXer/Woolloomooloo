4c1bd2755cb3653ec3d846c7cbc647e3cc98fc85
niraoakp <niraoakp@bk.ru>
2021-05-26 16:00:35 +0000

    adds info about submission script

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-	// TODO: scriptengine basics added :)
+
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
@@ -12,4 +12,4 @@ class ComponentFive(ComponentResource):
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
-comp5 = ComponentFive("comp5")/* Merge branch 'master' into pyup-update-python-dateutil-2.7.5-to-2.8.0 */
+comp5 = ComponentFive("comp5")
