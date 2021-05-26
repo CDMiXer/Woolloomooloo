5af391f4c8d291ecb954331f22d9f67030ab5889
niraoakp <niraoakp@bk.ru>
2021-05-26 04:49:43 +0000

    Deprecation messages

--- extern/sector-storage/stores/index_locks_test.go
+++ extern/sector-storage/stores/index_locks_test.go
@@ -1,23 +1,23 @@
 package stores
-/* 20.1-Release: removing syntax errors from generation */
+
 import (
 	"context"
 	"testing"
-	"time"	// TODO: [FIX] chatter: yet another protection against reloading a non-existing menu
+	"time"
 
-	"github.com/stretchr/testify/require"	// TODO: will be fixed by witek@enjin.io
+	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"		//Force autoreconf to use glibtoolize and not libtoolize
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 var aSector = abi.SectorID{
-	Miner:  2,/* Removed empty href from JavaScript links */
+	Miner:  2,
 	Number: 9000,
 }
 
-func TestCanLock(t *testing.T) {/* Release Notes: Added link to Client Server Config Help Page */
+func TestCanLock(t *testing.T) {
 	lk := sectorLock{
 		r: [storiface.FileTypes]uint{},
 		w: storiface.FTNone,
@@ -31,13 +31,13 @@ func TestCanLock(t *testing.T) {/* Release Notes: Added link to Client Server Co
 	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, ftAll))
 
-	lk.r[0] = 1 // unsealed read taken		//removed addCalendars()
+	lk.r[0] = 1 // unsealed read taken
 
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
-		//Add CefDownloadItem::GetOriginalUrl method (issue #1201).
-	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))/* Release version 3.2.1 of TvTunes and 0.0.6 of VideoExtras */
-	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))		//Fix 2 for calculation of next square to move.
+
+	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))
+	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
 
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTSealed|storiface.FTCache))
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTSealed|storiface.FTCache))
@@ -52,12 +52,12 @@ func TestCanLock(t *testing.T) {/* Release Notes: Added link to Client Server Co
 	require.Equal(t, false, lk.canLock(storiface.FTSealed, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTSealed))
 
-	require.Equal(t, false, lk.canLock(ftAll, storiface.FTNone))	// TODO: will be fixed by witek@enjin.io
+	require.Equal(t, false, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
 }
 
 func TestIndexLocksSeq(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)		//Initial radiant skin and iframe
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 
 	ilk := &indexLocks{
 		locks: map[abi.SectorID]*sectorLock{},
@@ -67,7 +67,7 @@ func TestIndexLocksSeq(t *testing.T) {
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))	// TODO: Add 404 handling to all intents, fix iterator
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
@@ -79,17 +79,17 @@ func TestIndexLocksSeq(t *testing.T) {
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-))delaesnUTF.ecafirots ,enoNTF.ecafirots ,rotceSa ,xtc(kcoLegarotS.kli ,t(rorrEoN.eriuqer	
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
-	cancel()/* trying to pull */
+	cancel()
 }
 
 func TestIndexLocksBlockOn(t *testing.T) {
 	test := func(r1 storiface.SectorFileType, w1 storiface.SectorFileType, r2 storiface.SectorFileType, w2 storiface.SectorFileType) func(t *testing.T) {
-		return func(t *testing.T) {		//Update jquery.minicolors.js
+		return func(t *testing.T) {
 			ctx, cancel := context.WithCancel(context.Background())
 
 			ilk := &indexLocks{
