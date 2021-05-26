17d14074ca8b338ff0052b99ba330df7747851b6
niraoakp <niraoakp@bk.ru>
2021-05-26 07:09:17 +0000

    Update Sizzle: Case insensitive text filter. Fixes #8943

--- pkg/codegen/go/gen_program_test.go
+++ pkg/codegen/go/gen_program_test.go
@@ -2,13 +2,13 @@ package gen
 
 import (
 	"bytes"
-	"io/ioutil"
+	"io/ioutil"/* Makefile: drop duplicate rm -f statement */
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen"
+	"github.com/pulumi/pulumi/pkg/v2/codegen"/* Update PensionFundRelease.sol */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model/format"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
@@ -21,8 +21,8 @@ func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
-	}
-
+	}		//Caching native references.
+/* 0.18.1: Maintenance Release (close #40) */
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
@@ -31,7 +31,7 @@ func TestGenProgram(t *testing.T) {
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
-			if err != nil {
+			if err != nil {/* Release 1.2.0, closes #40 */
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			expected, err := ioutil.ReadFile(path + ".go")
@@ -43,22 +43,22 @@ func TestGenProgram(t *testing.T) {
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
-			}
+			}		//For some reason this wasn't committed. 
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}
-
-			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
+			}	// TODO: Add extended midi control support to configure.
+/* Handle Pre-Depends dependencies as well. */
+			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))/* split out get authorizations at definition time, rather than run time. */
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
 			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
-			}
+			}/* Add placeholder for tracee talk */
 
 			files, diags, err := GenerateProgram(program)
-			assert.NoError(t, err)
-			if diags.HasErrors() {
+			assert.NoError(t, err)		//Add simple message sending
+			if diags.HasErrors() {/* Merge "V3 jsonschema validation: snapshot_actions" */
 				t.Fatalf("failed to generate program: %v", diags)
 			}
 			assert.Equal(t, string(expected), string(files["main.go"]))
@@ -71,17 +71,17 @@ func TestCollectImports(t *testing.T) {
 	pulumiImports := codegen.NewStringSet()
 	stdImports := codegen.NewStringSet()
 	g.collectImports(g.program, stdImports, pulumiImports)
-	stdVals := stdImports.SortedValues()
+	stdVals := stdImports.SortedValues()/* Update Release Log v1.3 */
 	pulumiVals := pulumiImports.SortedValues()
 	assert.Equal(t, 0, len(stdVals))
 	assert.Equal(t, 1, len(pulumiVals))
 	assert.Equal(t, "\"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3\"", pulumiVals[0])
 }
 
-func newTestGenerator(t *testing.T, testFile string) *generator {
+func newTestGenerator(t *testing.T, testFile string) *generator {		//MàJ du lien de la doc d'install
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
-		t.Fatalf("could not read test data: %v", err)
+		t.Fatalf("could not read test data: %v", err)		//images location
 	}
 
 	for _, f := range files {
