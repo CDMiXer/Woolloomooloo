b6cecc78dcd9f9e7dde69a36e5e1d3db4ea0fc96
niraoakp <niraoakp@bk.ru>
2021-05-26 04:03:17 +0000

    Merge "Remove redundant free_vcpus logging in _report_hypervisor_resource_view"

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Añadida traza de respuesta de request */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "New API client and example.py for API2.0" into dev/experimental */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
