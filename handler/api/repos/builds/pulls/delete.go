4be7bc30018f3d2426320ea3911a6ce8582cad08
niraoakp <niraoakp@bk.ru>
2021-05-26 03:32:49 +0000

    Delete tcp_playback.o

--- handler/api/repos/builds/pulls/delete.go
+++ handler/api/repos/builds/pulls/delete.go
@@ -2,59 +2,59 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Remove useless prototypes
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Implemented Debug DLL and Release DLL configurations. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Released springjdbcdao version 1.9.14 */
 
 package pulls
 
-import (
+import (/* - UDP tests working */
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"	// combined metric graphs view
 	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
-	repos core.RepositoryStore,
-	builds core.BuildStore,
+	repos core.RepositoryStore,/* add local-options to not relevant content in diff for releasing */
+	builds core.BuildStore,		//Delete cron_log_config
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			number, _ = strconv.Atoi(chi.URLParam(r, "pull"))
-		)
+		)	// Moved packages
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//Adding QueueFragment and DnD support from apollo source code.
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")
+				Debugln("api: cannot find repository")		//Update AMD Ryzen timings
 			return
 		}
 
 		err = builds.DeletePull(r.Context(), repo.ID, number)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).	// Delete CORE MT 570 Beta 00.zip
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot delete pr")
+				Debugln("api: cannot delete pr")/* Merge "Change default ansible_ssh_user to "kolla"" */
 		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
