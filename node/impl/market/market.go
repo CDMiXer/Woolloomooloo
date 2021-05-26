a4cac237f95482d051a7779d9028179d3f5547a9
niraoakp <niraoakp@bk.ru>
2021-05-26 08:58:45 +0000

    separated out select statements from select expressions in ast

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -3,22 +3,22 @@ package market
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"/* Release notes for helper-mux */
-	"go.uber.org/fx"/* Release version 26 */
+	"github.com/ipfs/go-cid"
+	"go.uber.org/fx"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
-	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"	// TODO: fixing another patch to a index in TestResources
+	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
-)		//Añadir el texto de la presentación
-/* Release of eeacms/www-devel:18.7.20 */
+)
+
 type MarketAPI struct {
 	fx.In
 
-	full.MpoolAPI	// 58b71e42-2e4d-11e5-9284-b827eb9e62be
-	FMgr *market.FundManager/* Merge "[INTERNAL] Release notes for version 1.90.0" */
+	full.MpoolAPI
+	FMgr *market.FundManager
 }
 
 func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
@@ -31,17 +31,17 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 		To:     marketactor.Address,
 		From:   wallet,
 		Value:  amt,
-		Method: marketactor.Methods.AddBalance,	// TODO: Added picalendr screenshot
+		Method: marketactor.Methods.AddBalance,
 		Params: params,
 	}, nil)
 
-	if aerr != nil {	// TODO: Don't automatically apply building tags to shop=car (fixes #1813)
+	if aerr != nil {
 		return cid.Undef, aerr
 	}
 
 	return smsg.Cid(), nil
 }
-/* Release of eeacms/www-devel:21.4.4 */
+
 func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	return a.FMgr.GetReserved(addr), nil
 }
@@ -53,7 +53,7 @@ func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Addre
 func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
 	return a.FMgr.Release(addr, amt)
 }
-/* Release of eeacms/www-devel:20.8.26 */
+
 func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
-}/* test for bug report */
+}
