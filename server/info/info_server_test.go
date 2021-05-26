1fbbf94c7eaf0e8552ad49133026bff865c373d5
niraoakp <niraoakp@bk.ru>
2021-05-26 05:44:47 +0000

    Merge "Add config of server start timeouts for probetests"

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -7,15 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/server/auth/jws"	// Create dict.txt
+	"github.com/argoproj/argo/server/auth/jws"
 )
 
 func Test_infoServer_GetUserInfo(t *testing.T) {
-	i := &infoServer{}
+	i := &infoServer{}/* Merge "Release 1.0.0.78 QCACLD WLAN Driver" */
 	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
 	info, err := i.GetUserInfo(ctx, nil)
 	if assert.NoError(t, err) {
 		assert.Equal(t, "my-iss", info.Issuer)
 		assert.Equal(t, "my-sub", info.Subject)
-	}
-}
+	}/* Update pom for Release 1.4 */
+}	// TODO: hacked by qugou1350636@126.com
