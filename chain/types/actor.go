18ade8a3a06c616eb972d7c3a9806d3c6098f690
niraoakp <niraoakp@bk.ru>
2021-05-26 01:41:44 +0000

    Updating readme filling content inside

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
-
+	// NetKAN generated mods - BDDMP-1.0.1
 import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
-)		//Rank import.
+)/* Release v3.6.11 */
 
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// TODO: fichier test integration vidéo
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
-	Nonce   uint64
+	Nonce   uint64	// TODO: list and delete subscriptions
 	Balance BigInt
 }
