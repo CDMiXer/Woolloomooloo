f1b24f763aca38edc234b52eaf098ec554bf5fb9
niraoakp <niraoakp@bk.ru>
2021-05-26 21:58:50 +0000

    Add getNativeErrors method to return validation errors in Laravel way.

--- journal/nil.go
+++ journal/nil.go
@@ -1,4 +1,4 @@
-package journal
+package journal		//Delete center-block.css
 
 type nilJournal struct{}
 
@@ -7,7 +7,7 @@ var nilj Journal = &nilJournal{}
 
 func NilJournal() Journal {
 	return nilj
-}
+}/* Usage details were lost some how... adding them back */
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
