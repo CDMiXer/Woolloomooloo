58ac4bce736f1116184ec5eaca4ed230e629b6de
niraoakp <niraoakp@bk.ru>
2021-05-26 11:20:04 +0000

    remove protocol from url

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
-export const normal = pulumi.output("normal");	// d824e1c8-2e66-11e5-9284-b827eb9e62be
-export const secret = pulumi.secret("secret");		//Update to provide a shortcut of the project
-		//Merge "Persist group cache by uuid"
+export const normal = pulumi.output("normal");
+export const secret = pulumi.secret("secret");
+
