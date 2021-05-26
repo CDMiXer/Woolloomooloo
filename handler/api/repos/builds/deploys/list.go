1f8f1968690c6b577c7310e003dbc10b8dcd84d0
niraoakp <niraoakp@bk.ru>
2021-05-26 04:10:03 +0000

    add support for merged sourcemap

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// Added support for generating Hex encoded MD5 digests.
 // You may obtain a copy of the License at
-//
+//	// Update base2clock.ino
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* added breadth first search */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// jarek/electric2go
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,7 +16,7 @@ package deploys
 
 import (
 	"net/http"
-
+		//a573b1a2-2e50-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
@@ -26,7 +26,7 @@ import (
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
-func HandleList(
+func HandleList(/* Create bacpipe.sh */
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
@@ -34,21 +34,21 @@ func HandleList(
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)	// [doxygen] Took Sherm's suggestions "*an* inertial"
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name).	// Create notor.html
 				Debugln("api: cannot find repository")
 			return
 		}
 
-		results, err := builds.LatestDeploys(r.Context(), repo.ID)
+		results, err := builds.LatestDeploys(r.Context(), repo.ID)	// Упрощен алгоритм блокировки ssl по ip
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* Merge pull request #9 from FictitiousFrode/Release-4 */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
@@ -58,4 +58,4 @@ func HandleList(
 			render.JSON(w, results, 200)
 		}
 	}
-}
+}		//Merge "docs: Fix the circularReveal example." into lmp-docs
