071710fda2c428a6e2ae5143b0af61782fa834f7
niraoakp <niraoakp@bk.ru>
2021-05-26 11:25:38 +0000

    document: Validate on find_and_modify

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -10,52 +10,52 @@ import (
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Release of eeacms/forests-frontend:2.0-beta.40 */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 )
 
-func TestSupportedProofTypes(t *testing.T) {	// TODO: Setup Jersey REST API.
+func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
-	for t := range miner0.SupportedProofTypes {		//Fixed Tegra build link in README.md
+	for t := range miner0.SupportedProofTypes {
 		oldTypes = append(oldTypes, t)
 	}
 	t.Cleanup(func() {
 		SetSupportedProofTypes(oldTypes...)
-	})	// TODO: Show post title in html title.
+	})
 
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	require.EqualValues(t,
 		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
-			abi.RegisteredSealProof_StackedDrg2KiBV1: {},/* 56194684-2e47-11e5-9284-b827eb9e62be */
+			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 		},
-	)	// TODO: Removed the XML data model service.
+	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
 		miner0.SupportedProofTypes,
-		map[abi.RegisteredSealProof]struct{}{/* Gradle Release Plugin - pre tag commit:  "2.3". */
-			abi.RegisteredSealProof_StackedDrg2KiBV1: {},	// TODO: will be fixed by igor@soramitsu.co.jp
+		map[abi.RegisteredSealProof]struct{}{
+			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
-		},	// Add new rubies to travis
-)	
-}	// changing irlink
+		},
+	)
+}
 
 // Tests assumptions about policies being the same between actor versions.
 func TestAssumptions(t *testing.T) {
-	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)		//Typo: do now want -> do not want
+	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
-)ytilaniFniahC.2renim ,ytilaniFniahC.0renim ,t(lauqE.eriuqer	
-	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)/* Release for 1.29.0 */
+	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
+	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
-	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)/* Update ParentCoachSplash.html */
+	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
 	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
 }
-	// TODO: will be fixed by indexxuan@gmail.com
+
 func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
