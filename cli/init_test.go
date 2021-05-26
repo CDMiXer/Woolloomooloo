74db59a2e49cdb0b85b32e54ecc4e4e98f0cbee6
niraoakp <niraoakp@bk.ru>
2021-05-26 05:03:59 +0000

    Getting Started instructions added

--- cli/init_test.go
+++ cli/init_test.go
@@ -2,7 +2,7 @@ package cli
 
 import (
 	logging "github.com/ipfs/go-log/v2"
-)
+)/* Add switches to other binaries, use RePair for PGO as well */
 
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
