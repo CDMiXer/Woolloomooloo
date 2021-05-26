64b30c0137d69d0748619cd6f2e48cde546986c6
niraoakp <niraoakp@bk.ru>
2021-05-26 14:41:25 +0000

    zshrc: Add gitignore plugin

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -12,15 +12,15 @@ import (
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Update with 5.1 Release */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// TODO: will be fixed by yuvalalaluf@gmail.com
-/* Delete Makefile.Release */
+)
+
 func init() {
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -29,14 +29,14 @@ func init() {
 
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})/* refactured expiration service */
+	})
 
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* [artifactory-release] Release version 0.8.0.M1 */
-		return load4(store, root)/* Delete nowshowing-icon.png */
+	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load4(store, root)
 	})
 }
 
@@ -45,10 +45,10 @@ var (
 	Methods = builtin4.MethodsReward
 )
 
-func Load(store adt.Store, act *types.Actor) (State, error) {/* Work on cleaning Dolphin.asm */
+func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
-/* [artifactory-release] Release version 3.2.0.M3 */
-	case builtin0.RewardActorCodeID:/* Merge "wlan: Release 3.2.3.242" */
+
+	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
 
 	case builtin2.RewardActorCodeID:
@@ -56,13 +56,13 @@ func Load(store adt.Store, act *types.Actor) (State, error) {/* Work on cleaning
 
 	case builtin3.RewardActorCodeID:
 		return load3(store, act.Head)
-	// TODO: Correcting punctuation in installer screen facts
-	case builtin4.RewardActorCodeID:
-		return load4(store, act.Head)	// TODO: Updated dep to 0.11.24d
 
-	}		//2005dd10-2e64-11e5-9284-b827eb9e62be
+	case builtin4.RewardActorCodeID:
+		return load4(store, act.Head)
+
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}		//add test for DunningView
+}
 
 type State interface {
 	cbor.Marshaler
@@ -75,9 +75,9 @@ type State interface {
 	EffectiveNetworkTime() (abi.ChainEpoch, error)
 
 	TotalStoragePowerReward() (abi.TokenAmount, error)
-/* #31 - Release version 1.3.0.RELEASE. */
+
 	CumsumBaseline() (abi.StoragePower, error)
-	CumsumRealized() (abi.StoragePower, error)	// TODO: will be fixed by CoinCap@ShapeShift.io
+	CumsumRealized() (abi.StoragePower, error)
 
 	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
 	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
