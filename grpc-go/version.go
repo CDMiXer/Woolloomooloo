0168a265220eef0335393aa8cbf3734b72726a80
niraoakp <niraoakp@bk.ru>
2021-05-26 05:07:24 +0000

    Merge "Fix textlib 'file' exception regex"

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -2,21 +2,21 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// iPad fixes for HelpViewController
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Added "Model Details" frame.
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// reflecting that the leader going offline + election happens  test now works
- *	// TODO: declaration corrections
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Work on classic implementation
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.6.8 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: will be fixed by nicksavers@gmail.com
  *
- */
+ *//* better debug projects */
 
 package grpc
-
+	// jzip - add option to choose add current folder name or nto
 // Version is the current grpc version.
 const Version = "1.41.0-dev"
