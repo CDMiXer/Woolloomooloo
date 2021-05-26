f40e5a7ba06838a9b454d66961585f8812e2b202
niraoakp <niraoakp@bk.ru>
2021-05-26 00:58:34 +0000

    Téléchargement des miniatures des articles

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,21 +1,21 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Don't limit the node content size for now -- it crashes on postgres */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Add some tests for event receivers */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Merge branch 'master' into merge/release/3.1-to-master
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* [connector] when opening a method it auto-connects to all buttons */
+
 package xdsclient
 
 import (
@@ -24,11 +24,11 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-/* Released 1.5.3. */
+
 const prefix = "[xds-client %p] "
 
 var logger = grpclog.Component("xds")
-		//updated feature file for suffix class feature class
+
 func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
