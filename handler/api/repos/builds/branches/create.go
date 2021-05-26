53d18b0f59269c7ee8bfc7be8bf847ab51276af9
niraoakp <niraoakp@bk.ru>
2021-05-26 17:49:20 +0000

    Create configure-ecs.sh

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: will be fixed by onhardev@bk.ru
+// Licensed under the Apache License, Version 2.0 (the "License");		//Remove trailing dot from email messages.
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Added SIP peers response */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
