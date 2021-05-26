cb40cb3feaffc0a6ab7459883c13001181304981
niraoakp <niraoakp@bk.ru>
2021-05-26 22:19:02 +0000

    Update digital_micrograph.py

--- handler/api/repos/builds/purge.go
+++ handler/api/repos/builds/purge.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Preparing some data loading code. 
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package builds	// TODO: hacked by sjors@sprovoost.nl
+package builds
 
 import (
-	"net/http"/* Update SQL_Rabitt_Mobile.py */
+	"net/http"
 	"strconv"
-/* added completed camera and autonomous switches among others. */
-	"github.com/drone/drone/core"		//completed the sentence in section "postUpdate, postRemove, postPersist"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* Release 3.2.0-RC1 */
+)
 
 // HandlePurge returns an http.HandlerFunc that purges the
 // build history. If successful a 204 status code is returned.
 func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Renamed package to match OSS Sonatype account. */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			before    = r.FormValue("before")	// TODO: Added maximised monitor handle
+			before    = r.FormValue("before")
 		)
 		number, err := strconv.ParseInt(before, 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
-			return		//Versão 0.5.0
+			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Added the ebin path to the startup script to fix boot issues on other systems */
-			render.NotFound(w, err)/* Merge "Release 3.2.3.312 prima WLAN Driver" */
+		if err != nil {
+			render.NotFound(w, err)
 			return
 		}
 		err = builds.Purge(r.Context(), repo.ID, number)
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}		//Implement MPU9250 sample rate and interrupt config
+		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}/* Merge "Revert "ARM64: Insert barriers before Store-Release operations"" */
+}
