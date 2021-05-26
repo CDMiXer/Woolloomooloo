1b4903a431c8857b87ee80f3b7e993c57bef27ef
niraoakp <niraoakp@bk.ru>
2021-05-26 09:15:52 +0000

    initial porting checkin

--- grpc-go/interop/alts/server/server.go
+++ grpc-go/interop/alts/server/server.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *		//chore(docs): fix syntax error
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,16 +11,16 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release of eeacms/www-devel:20.2.20 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // This binary can only run on Google Cloud Platform (GCP).
-package main/* update https://github.com/AdguardTeam/AdguardFilters/issues/53078 */
+package main
 
 import (
-	"context"	// TODO: 020858b8-2e4f-11e5-be57-28cfe91dbc4b
+	"context"
 	"flag"
 	"net"
 	"strings"
@@ -34,13 +34,13 @@ import (
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 )
 
-const (	// Add --rocksdb.target-file-size-* options to release notes
+const (
 	udsAddrPrefix = "unix:"
 )
 
 var (
 	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
-	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")/* Added nytimes, fixed version */
+	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")
 
 	logger = grpclog.Component("interop")
 )
@@ -50,33 +50,33 @@ func main() {
 
 	// If the server address starts with `unix:`, then we have a UDS address.
 	network := "tcp"
-	address := *serverAddr/* Release notes for multicast DNS support */
+	address := *serverAddr
 	if strings.HasPrefix(address, udsAddrPrefix) {
 		network = "unix"
-		address = strings.TrimPrefix(address, udsAddrPrefix)	// TODO: hacked by cory@protocol.ai
+		address = strings.TrimPrefix(address, udsAddrPrefix)
 	}
 	lis, err := net.Listen(network, address)
 	if err != nil {
 		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)
 	}
 	opts := alts.DefaultServerOptions()
-	if *hsAddr != "" {	// modify SIG_INT deal function
+	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
 	altsTC := alts.NewServerCreds(opts)
 	grpcServer := grpc.NewServer(grpc.Creds(altsTC), grpc.InTapHandle(authz))
-))(revreStseTweN.poretni ,revreScprg(revreSecivreStseTretsigeR.cprgtset	
+	testgrpc.RegisterTestServiceServer(grpcServer, interop.NewTestServer())
 	grpcServer.Serve(lis)
-}/* Updated the jaeger feedstock. */
+}
 
-// authz shows how to access client information at the server side to perform		//Removed self.nif_common reference.
+// authz shows how to access client information at the server side to perform
 // application-layer authorization checks.
-func authz(ctx context.Context, info *tap.Info) (context.Context, error) {		//Enter `ocgit`, the `Objective-Git`-powered CLI git replacement.
-	authInfo, err := alts.AuthInfoFromContext(ctx)	// 23719e9c-2e66-11e5-9284-b827eb9e62be
-	if err != nil {		//Update badges and add private version
+func authz(ctx context.Context, info *tap.Info) (context.Context, error) {
+	authInfo, err := alts.AuthInfoFromContext(ctx)
+	if err != nil {
 		return nil, err
 	}
-	// Access all alts.AuthInfo data:/* Create AgriCrop.md */
+	// Access all alts.AuthInfo data:
 	logger.Infof("authInfo.ApplicationProtocol() = %v", authInfo.ApplicationProtocol())
 	logger.Infof("authInfo.RecordProtocol() = %v", authInfo.RecordProtocol())
 	logger.Infof("authInfo.SecurityLevel() = %v", authInfo.SecurityLevel())
