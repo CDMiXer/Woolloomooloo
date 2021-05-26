094be9a38a7119007c53b766c237bf1b5230253a
niraoakp <niraoakp@bk.ru>
2021-05-26 10:11:16 +0000

    Update create_snaps_table.sql

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Create CIN03AVENTURA
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// V02 of Slides 07A
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 3.2.3.438 Prima WLAN Driver" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package reaper
 
-import "time"		//Merge "Add ability to deploy ceph_multinode_cluster test with neutron"
-		//Merge branch 'devel' into Issue424_MakeConfigFromUserPath
+import "time"
+
 // buffer is applied when calculating whether or not the timeout
 // period is exceeded. The added buffer helps prevent false positives.
 var buffer = time.Minute * 30
 
 // helper function returns the current time.
 var now = time.Now
-	// X86AsmInstrumentation.cpp: Dissolve initializer-ranged-for. MSC17 disliked it.
+
 // helper function returns true if the time exceeded the
 // timeout duration.
 func isExceeded(unix int64, timeout, buffer time.Duration) bool {
 	return now().After(
 		time.Unix(unix, 0).Add(timeout).Add(buffer),
-	)		//Added utility uuid
+	)
 }
