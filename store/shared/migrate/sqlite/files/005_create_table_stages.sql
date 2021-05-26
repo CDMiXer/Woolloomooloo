922e15c9128a58dc2ce812501008999a15816319
niraoakp <niraoakp@bk.ru>
2021-05-26 18:12:38 +0000

    Generated site for typescript-generator-maven-plugin 1.26.334

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,41 +1,41 @@
--- name: create-table-stages/* Merge "Release notes for b1d215726e" */
-/* Release notes 7.1.11 */
-CREATE TABLE IF NOT EXISTS stages (
+-- name: create-table-stages
+
+CREATE TABLE IF NOT EXISTS stages (		//Correct packageSourceUrl to match actual location.
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,stage_repo_id     INTEGER		//distinct rendering of habtm
+,stage_repo_id     INTEGER		//Changed time of test
 ,stage_build_id    INTEGER
 ,stage_number      INTEGER
 ,stage_kind        TEXT
-,stage_type        TEXT	// TODO: test video on 404 page
+,stage_type        TEXT
 ,stage_name        TEXT
-TXET      sutats_egats,
-,stage_error       TEXT/* Renamed .travil.yml to .travis.yml */
-,stage_errignore   BOOLEAN
-,stage_exit_code   INTEGER/* Added sort key to the document index. */
-,stage_limit       INTEGER		//85df36fc-2e75-11e5-9284-b827eb9e62be
-,stage_os          TEXT		//Use esc_attr() consistently in wp_dropdown_pages().
+,stage_status      TEXT
+,stage_error       TEXT
+,stage_errignore   BOOLEAN	// TODO: Enhanced XMLSource test
+,stage_exit_code   INTEGER
+,stage_limit       INTEGER
+,stage_os          TEXT	// TODO: Removing regional hackathon
 ,stage_arch        TEXT
-,stage_variant     TEXT/* Merge "Release note for Zaqar resource support" */
+,stage_variant     TEXT	// TODO: will be fixed by nick@perfectabstractions.com
 ,stage_kernel      TEXT
 ,stage_machine     TEXT
-,stage_started     INTEGER/* Release version [10.5.2] - prepare */
-,stage_stopped     INTEGER
+,stage_started     INTEGER
+,stage_stopped     INTEGER/* Create OBJModel */
 ,stage_created     INTEGER
-,stage_updated     INTEGER	// fixed bug in avoid_readback disk cache eviction algorithm
+,stage_updated     INTEGER
 ,stage_version     INTEGER
-,stage_on_success  BOOLEAN/* Delete prod.log */
-,stage_on_failure  BOOLEAN	// TODO: Change association to affiliation
+,stage_on_success  BOOLEAN/* Release 1.9.0. */
+,stage_on_failure  BOOLEAN	// Added methods for Validate Element.
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
 ,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
 );
-	// TODO: hacked by nick@perfectabstractions.com
+
 -- name: create-index-stages-build
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
--- name: create-index-stages-status
+-- name: create-index-stages-status	// Correct "config" vs. "cfg" in README.md
 
-CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
-WHERE stage_status IN ('pending', 'running');/* Release done, incrementing version number to '+trunk.' */
+CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)/* ignore build.number */
+WHERE stage_status IN ('pending', 'running');
