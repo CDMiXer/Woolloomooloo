158a6632d19d671dc5147c11c3f3beca0822b10b
niraoakp <niraoakp@bk.ru>
2021-05-27 23:59:36 +0000

    Point to Create React App

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* optimization for "toString()" */
-// +build !oss
+// that can be found in the LICENSE file./* Release 1.17.0 */
+
+// +build !oss	// TODO: hacked by boringland@protonmail.ch
 
 package system
 
 import (
-	"io/ioutil"
+	"io/ioutil"/* Release of eeacms/bise-backend:v10.0.27 */
 
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)/* Released v0.2.0 */
-}		//da00402c-2e62-11e5-9284-b827eb9e62be
+	logrus.SetOutput(ioutil.Discard)
+}		//Fix Mystic skills double-casting at high ping
