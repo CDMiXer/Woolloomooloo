d32428e0ea9ce1906f06990fbfaa9b9e22b82440
niraoakp <niraoakp@bk.ru>
2021-05-27 01:05:45 +0000

    tmp: alloc one block at a time

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+/* Merge "Release 4.4.31.59" */
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-
+	// TODO: will be fixed by boringland@protonmail.ch
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {
+        this.create = async (inputs: any) => {		//Merge "Upgrade to Kotlin 1.4.0-rc (real)" into androidx-master-dev
             return {
-                id: (currentID++) + "",		//quick fix for issue #40
-                outs: undefined,	// TODO: hacked by mail@overlisted.net
+                id: (currentID++) + "",
+                outs: undefined,
             };
         };
     }
 }
-
-class Resource extends pulumi.dynamic.Resource {	// TODO: Added some interlacing specs.
+/* Replace m_ prefix by g_ prefix for the global variables */
+class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, {}, opts);
-    }/* Merge "Release 1.0.0.192 QCACLD WLAN Driver" */
-}	// TODO: higher merit for ffdshow's audio decoder
+    }
+}/* Merge "Release 3.2.3.440 Prima WLAN Driver" */
 
-// Create a resource using the default dynamic provider instance.	// Reports now grouped by date in dashboard
-let a = new Resource("a");
+// Create a resource using the default dynamic provider instance.
+let a = new Resource("a");	// TODO: hacked by davidad@alum.mit.edu
 let b = new Resource("b");
 
 export const urn = a.urn;
