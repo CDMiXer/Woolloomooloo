37dafd327d05538036482fe8bb5b98c65268485c
niraoakp <niraoakp@bk.ru>
2021-05-27 04:39:32 +0000

    Update README.md with new cocoa pods version

--- grpc-go/xds/internal/balancer/balancer.go
+++ grpc-go/xds/internal/balancer/balancer.go
@@ -1,11 +1,11 @@
-/*/* 1b6ba642-2e3f-11e5-9284-b827eb9e62be */
+/*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: will be fixed by ligi@ligi.de
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* PHP: Kommenttikorjaus */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,16 +14,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Delete feature.md */
+ *//* Release version 3.0.1 */
 
 // Package balancer installs all the xds balancers.
 package balancer
 
-import (
+import (/* Refactored synbiochem-py code. */
 	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"     // Register the CDS balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clusterimpl"     // Register the xds_cluster_impl balancer
-	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer	// TODO: will be fixed by steven@stebalien.com
+	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clusterresolver" // Register the xds_cluster_resolver balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/priority"        // Register the priority balancer
-	_ "google.golang.org/grpc/xds/internal/balancer/weightedtarget"  // Register the weighted_target balancer/* Release proper of msrp-1.1.0 */
+	_ "google.golang.org/grpc/xds/internal/balancer/weightedtarget"  // Register the weighted_target balancer
 )
