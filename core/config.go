e0cebe2d5dc071128ef0def322f8db26625cd67b
niraoakp <niraoakp@bk.ru>
2021-05-27 00:38:44 +0000

    TBS 3.8.0 beta + benches

--- core/config.go
+++ core/config.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www-devel:19.11.30 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,16 +8,16 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Nice icons! Use svgs instead of pngs where possible.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core		//optional pronunciation dataset
+package core
 
 import "context"
 
-type (		//Create InstallComponent.php
-	// Config represents a pipeline config file.		//rev 659889
+type (
+	// Config represents a pipeline config file.
 	Config struct {
 		Data string `json:"data"`
 		Kind string `json:"kind"`
@@ -26,15 +26,15 @@ type (		//Create InstallComponent.php
 	// ConfigArgs represents a request for the pipeline
 	// configuration file (e.g. .drone.yml)
 	ConfigArgs struct {
-		User   *User       `json:"-"`/* Release 1.16.0 */
+		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
-		Build  *Build      `json:"build,omitempty"`		//535a3cb8-2e43-11e5-9284-b827eb9e62be
+		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
 
-	// ConfigService provides pipeline configuration from an/* New post: CRM Online Australia Releases IntelliChat for SugarCRM */
+	// ConfigService provides pipeline configuration from an
 	// external service.
 	ConfigService interface {
-		Find(context.Context, *ConfigArgs) (*Config, error)	// TODO: 1f09465c-2e70-11e5-9284-b827eb9e62be
+		Find(context.Context, *ConfigArgs) (*Config, error)
 	}
-)/* Merge "Ensure list output function can support non-sorting printing" */
+)
