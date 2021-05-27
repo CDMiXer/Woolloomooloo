9e11e66cf72241748607c396ff0661029bb5b1b3
niraoakp <niraoakp@bk.ru>
2021-05-27 13:40:32 +0000

    Add Cassandra support

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: Added in support for line based message filtering
 console.log("Here's some format goo: %s, %v, %q, %d!");
-console.error("Here's some format goo: %s, %v, %q, %d!");
+console.error("Here's some format goo: %s, %v, %q, %d!");/* Trying to get command working still :P */
