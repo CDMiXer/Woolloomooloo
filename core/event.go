e259b464ca94f931f45b4fb9864141b41c1893e6
niraoakp <niraoakp@bk.ru>
2021-05-27 00:26:23 +0000

    Update Composer.json for Whoops 2.0

--- core/event.go
+++ core/event.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: SO-2003: remove prev.picks and bookmarks client side service config jobs
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Merge "Allow OVERLAY frames to use the show_exsiting_frame flag" into nextgenv2 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "MOTECH-1848: Documentation for M-N Relation." */
-///* Release of eeacms/energy-union-frontend:1.7-beta.16 */
+//      http://www.apache.org/licenses/LICENSE-2.0/* add print_text_in_color */
+///* Release-1.3.0 updates to changes.txt and version number. */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.		//Change Complete to Status - Part 1 (db changes)
+		//75cdcbc0-2e4d-11e5-9284-b827eb9e62be
 package core
 
 // Hook event constants.
@@ -21,6 +21,6 @@ const (
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
-	EventPromote     = "promote"
+	EventPromote     = "promote"/* Release plugin update */
 	EventRollback    = "rollback"
-)
+)/* Roughing in characteristics */
