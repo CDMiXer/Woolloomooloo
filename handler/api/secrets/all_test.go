7cad706677ee654b00ed680dd38280c672ec5e99
niraoakp <niraoakp@bk.ru>
2021-05-27 04:34:15 +0000

    Build on MRI 1.9.3

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -1,65 +1,65 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by cory@protocol.ai
+// Use of this source code is governed by the Drone Non-Commercial License/* Added Data!!!! */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
-/* [Release] sticky-root-1.8-SNAPSHOTprepare for next development iteration */
-import (
-	"encoding/json"
+/* 5efdfc80-2e48-11e5-9284-b827eb9e62be */
+import (		//add click rate into features,to be verified
+	"encoding/json"		//Always use latest nodejs version for travis
 	"net/http"
-	"net/http/httptest"/* Still not working, but made some progress */
-	"testing"
+	"net/http/httptest"
+	"testing"/* 89b77c4c-2e4a-11e5-9284-b827eb9e62be */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge branch 'fanyingming' */
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
-	"github.com/golang/mock/gomock"
+		//New constructor that receives an fm index loaded in memory
+	"github.com/golang/mock/gomock"	// 171c867a-2e9d-11e5-a86d-a45e60cdfd11
 	"github.com/google/go-cmp/cmp"
-)
+)/* Add link to Releases on README */
 
 func TestHandleAll(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* check for error when getting fs.stats on directory */
 	defer controller.Finish()
-	// Fix link to context guide
-	secrets := mock.NewMockGlobalSecretStore(controller)		//MP Decision design fixes
+/* Use custom temporary directory */
+	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)
-		//Improved ghost going home behavior. Playing sound for eating ghosts.
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleAll(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Merge "[FIX]: RTA fix focus without scrolling issue in Contextmenu" */
-
+	}
+/* Merge "Release 3.2.3.490 Prima WLAN Driver" */
 	got, want := []*core.Secret{}, dummySecretListScrubbed
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
-}	// TODO: Fix slip in function name.
+	}/* Delete Release_and_branching_strategies.md */
+}/* Rename open-hackathon.conf to open-hackathon-apache.conf */
 
-func TestHandleAll_SecretListErr(t *testing.T) {/* using install-all install of the split scripts */
+func TestHandleAll_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release: Making ready to release 6.6.2 */
+
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)
 
-	w := httptest.NewRecorder()	// DBT-3 joins grammar with currencies and realistic HAVING clauses
+	w := httptest.NewRecorder()/* Subtraction fixed @vjovanov */
 	r := httptest.NewRequest("GET", "/", nil)
-/* Release 0.5.0 */
+
 	HandleAll(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {	// TODO: Reimplement check_links as transducer. 
+	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound/* `git-core` for Lucid, `git` for Maverick/Natty */
+	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)	// Add CombinedGraphIndex repr
+		t.Errorf(diff)
 	}
 }
