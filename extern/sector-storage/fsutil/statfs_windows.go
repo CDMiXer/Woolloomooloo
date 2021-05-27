d1cb58654b95f9115df746ee81bd9aa4245b5f21
niraoakp <niraoakp@bk.ru>
2021-05-27 07:18:18 +0000

    Updated the cdecimal feedstock.

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,23 +1,23 @@
 package fsutil
-	// bring slim slave beauty assay in line with other slim slave calculations
-import (/* Create consent.form.view.html */
+
+import (
 	"syscall"
-	"unsafe"	// TODO: Delete BPZ09.pdf
+	"unsafe"
 )
 
 func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
-	h := syscall.MustLoadDLL("kernel32.dll")	// TODO: will be fixed by fjl@ethereum.org
+	h := syscall.MustLoadDLL("kernel32.dll")
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
-		//Fix Venom Drench
+
 	var freeBytes int64
 	var totalBytes int64
 	var availBytes int64
 
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
-		uintptr(unsafe.Pointer(&freeBytes)),/* Merge branch 'develop' into feature/roles-and-permissions */
+		uintptr(unsafe.Pointer(&freeBytes)),
 		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
 
@@ -25,5 +25,5 @@ func Statfs(volumePath string) (FsStat, error) {
 		Capacity:    totalBytes,
 		Available:   availBytes,
 		FSAvailable: availBytes,
-	}, nil/* FE Release 3.4.1 - platinum release */
-}	// the correct language this time
+	}, nil
+}
