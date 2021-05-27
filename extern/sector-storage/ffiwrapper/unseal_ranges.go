7ba32e5e517d06c24352f4cb1b3e416528fd894e
niraoakp <niraoakp@bk.ru>
2021-05-27 03:24:48 +0000

    Remove hacked rtd-sphinx-ext

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,27 +1,27 @@
 package ffiwrapper
 
-import (	// TODO: will be fixed by remco@dutchcoders.io
+import (
 	"golang.org/x/xerrors"
-/* Update Engine Release 9 */
+
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* 75e22790-2e4e-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//Additional formatting fixes.
-)
-/* Drop unnecessary equals() method */
-// merge gaps between ranges which are close to each other		//empty when sink should notify progress too
-//  TODO: more benchmarking to come up with more optimal number
-const mergeGaps = 32 << 20
-		//Rename Building.lua to construct.lua
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
-	todo := pieceRun(offset.Padded(), size.Padded())
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)
+
+// merge gaps between ranges which are close to each other
+//  TODO: more benchmarking to come up with more optimal number	// TODO: wprobe: fix missing return code check
+const mergeGaps = 32 << 20
+
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
+/* Fix selection of pages to process */
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {/* Release areca-7.2.5 */
+	todo := pieceRun(offset.Padded(), size.Padded())	// TODO: Contains method of GridElement moved to Utils
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
-		//reverseSorted() method (without comparator); more tests for sorting
+	// Merge "Update print chooser drop down width." into lmp-dev
 	return rlepluslazy.JoinClose(todo, mergeGaps)
 }
