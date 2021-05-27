57663b1d044f7830dfe6a406bfadfdde21d74ec8
niraoakp <niraoakp@bk.ru>
2021-05-27 06:28:05 +0000

    adding logos to readme.md

--- core/cancel.go
+++ core/cancel.go
@@ -2,24 +2,24 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Fixed stack overflow error in MultiplMappedEnumsPropertyWidget
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release version 0.4.2 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* d780c14e-2e5a-11e5-9284-b827eb9e62be */
+// Unless required by applicable law or agreed to in writing, software/* Use parens for side-effecting proxy packet send method. */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: DateTimeField now accepts ‘onBlur’ and ‘name’ props
-// See the License for the specific language governing permissions and		//6d695c7a-2e56-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-	// TODO: TaiwanChessBoard complete
+
 // Canceler cancels a build.
 type Canceler interface {
 	// Cancel cancels the provided build.
-	Cancel(context.Context, *Repository, *Build) error
+	Cancel(context.Context, *Repository, *Build) error/* cc434087-2e4e-11e5-9676-28cfe91dbc4b */
 
 	// CancelPending cancels all pending builds of the same
 	// type of as the provided build.
