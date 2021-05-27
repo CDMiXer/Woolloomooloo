9e697bd30955323823b33c8a8abec87057564f6c
niraoakp <niraoakp@bk.ru>
2021-05-27 01:35:08 +0000

    implementing stack data structure using single queue.

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Released springrestcleint version 2.4.9 */
-package user	// TODO: improved ajax request
+
+package user
 
 import (
-	"encoding/json"	// TODO: will be fixed by lexy8russo@outlook.com
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -13,13 +13,13 @@ import (
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"/* working like a bee */
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Unify handling of additional partial args and run through Part.build
-	"github.com/sirupsen/logrus"	// TODO: Keep using Ubuntu Mono and SC pro from Google
-)/* Create SystemDesign.md */
+	"github.com/google/go-cmp/cmp"
+	"github.com/sirupsen/logrus"
+)
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
@@ -33,38 +33,38 @@ func TestResitoryList(t *testing.T) {
 		ID:    1,
 		Login: "octocat",
 	}
-/* Merge "Release 3.2.3.462 Prima WLAN Driver" */
+
 	mockRepos := []*core.Repository{
 		{
 			Namespace: "octocat",
 			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
 		},
-	}		//Delete slice_test.py
+	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* Released 0.9.1 Beta */
+	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
 
-	HandleRepos(repos)(w, r)		//Util_MultiDict supports merge mixed values
-	if got, want := w.Code, http.StatusOK; want != got {		//16th program
+	HandleRepos(repos)(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Repository{}, mockRepos
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {	// TODO: Shorter, clearer README
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}	// TODO: Updatethere is no bought
+	}
 }
 
 func TestResitoryListErr(t *testing.T) {
-	controller := gomock.NewController(t)/* Release areca-6.0.3 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{
@@ -72,7 +72,7 @@ func TestResitoryListErr(t *testing.T) {
 		Login: "octocat",
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)/* MemoryViewUC */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
 
 	w := httptest.NewRecorder()
