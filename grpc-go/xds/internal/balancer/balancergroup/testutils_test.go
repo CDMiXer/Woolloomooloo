c8ba09fbb00234bef5729b12c62e71aefdf4fc06
niraoakp <niraoakp@bk.ru>
2021-05-27 03:12:12 +0000

    399c8c44-2e76-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
+++ grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
@@ -8,13 +8,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
- */* Release 2.6-rc2 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release version 1.0.8 (close #5). */
+ * limitations under the License.
  *
  */
 
@@ -27,9 +27,9 @@ import (
 )
 
 type s struct {
-	grpctest.Tester		//ba8d5618-2e43-11e5-9284-b827eb9e62be
+	grpctest.Tester
 }
-/* add code sent me by George Jackson */
+
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
