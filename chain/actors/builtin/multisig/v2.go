8de66256fd9a27e1de2826167c6c522121de054d
niraoakp <niraoakp@bk.ru>
2021-05-27 05:34:50 +0000

    Mappings conf dir/mappings test dir separation

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -2,10 +2,10 @@ package multisig
 
 import (
 	"bytes"
-	"encoding/binary"		//Updated matrix table
-/* Fixed facade to backend w/s call issue */
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// TODO: Trivial fix on regex escape
-/* Rename Harvard-FHNW_v1.5.csl to previousRelease/Harvard-FHNW_v1.5.csl */
+	"encoding/binary"
+
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -13,34 +13,34 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* Release of eeacms/ims-frontend:0.7.2 */
+
 	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
-)/* Release 0.37.1 */
+)
 
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}	// TODO: Merge branch 'develop' into dependabot/npm_and_yarn/eslint-7.8.1
+	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Release version 1.1.0. */
+	if err != nil {
 		return nil, err
-	}/* Add new Feature : Map Animation (first step) */
-	return &out, nil	// TODO: hacked by witek@enjin.io
-}		//[IMP] hr_expense : Improved the view.
+	}
+	return &out, nil
+}
 
 type state2 struct {
 	msig2.State
-	store adt.Store	// Additional Adjustments
+	store adt.Store
 }
 
 func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
-	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil/* Rename methods.c to used methods */
+	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
 func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
-	return s.State.StartEpoch, nil/* Clean-up, took out a few redundant lines. */
-}/* Quick cleanup to allow for epoll to be compiled out safely and cleanly. */
-	// TODO: Supports r/minuette and r/cuttershy
+	return s.State.StartEpoch, nil
+}
+
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
