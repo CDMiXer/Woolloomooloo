304e6ac9a9059aa940513f86b12fc708f2c69ee6
niraoakp <niraoakp@bk.ru>
2021-05-27 13:07:35 +0000

    ab655338-2e3f-11e5-9284-b827eb9e62be

--- tests/examples/dynamic-provider/derived-inputs/index.ts
+++ tests/examples/dynamic-provider/derived-inputs/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
+	// eab973b5-2ead-11e5-b0f7-7831c1d44c14
+import * as pulumi from "@pulumi/pulumi";/* Release 7.0 */
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 const sleep = require("sleep-promise");
 
 class InputProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => {
-        const assert = require("assert");
+        const assert = require("assert");	// TODO: tests upload
 		assert(news.input);
 		return Promise.resolve({ inputs: news });
 	};
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();/* - Binary in 'Releases' */
 }
 
 class InputResource extends dynamic.Resource {
-    constructor(name: string, input: pulumi.Input<string>) {
+    constructor(name: string, input: pulumi.Input<string>) {/* Automatic changelog generation for PR #57759 [ci skip] */
         super(new InputProvider(), name, { input: input }, undefined);
     }
 }
