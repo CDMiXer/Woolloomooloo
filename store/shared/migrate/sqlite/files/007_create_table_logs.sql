8b87f45bb7577e7bd4714ec4642b8dec67fcc13b
niraoakp <niraoakp@bk.ru>
2021-05-27 08:57:19 +0000

    comment out a line to prevent error

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -2,6 +2,6 @@
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  BLOB
+,log_data  BLOB	// TODO: #2556 normalize debug events
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
-);
+);		//Bug fix: handshake merged with redis commands
