d5684f051e62ed1bf628e40ac8803670e12b69aa
niraoakp <niraoakp@bk.ru>
2021-05-27 22:03:47 +0000

    Update VG.py

--- core/step.go
+++ core/step.go
@@ -1,63 +1,63 @@
-// Copyright 2019 Drone IO, Inc.	// Merge branch 'master' into drawabletrack-isloaded
-//		//updating poms for 0.1.17 release
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by arajasek94@gmail.com
 // You may obtain a copy of the License at
-//	// TODO: Delete networkc.js
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Pre-Release of Verion 1.0.8 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Load extjs libs locally
 package core
-		//uped version number to 2.2.9
-import "context"/* Version 2.1.0 Release */
+		//f9bf58d0-2e49-11e5-9284-b827eb9e62be
+import "context"/*  - Release the spin lock */
 
 type (
 	// Step represents an individual step in the stage.
 	Step struct {
-		ID        int64  `json:"id"`/* Merge "[Release] Webkit2-efl-123997_0.11.3" into tizen_2.1 */
+		ID        int64  `json:"id"`/* Merge "Use network RBAC feature for external access" */
 		StageID   int64  `json:"step_id"`
-		Number    int    `json:"number"`/* Add Graeme's last name to Authors */
+		Number    int    `json:"number"`
 		Name      string `json:"name"`
 		Status    string `json:"status"`
 		Error     string `json:"error,omitempty"`
 		ErrIgnore bool   `json:"errignore,omitempty"`
-		ExitCode  int    `json:"exit_code"`
+		ExitCode  int    `json:"exit_code"`	// TODO: will be fixed by timnugent@gmail.com
 		Started   int64  `json:"started,omitempty"`
 		Stopped   int64  `json:"stopped,omitempty"`
 		Version   int64  `json:"version"`
-	}
-
-	// StepStore persists build step information to storage.
+	}/* Modify alignment for badges and documents in README */
+/* Update catherine.html */
+	// StepStore persists build step information to storage./* Add a better default for the output format */
 	StepStore interface {
 		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Step, error)
 
-		// Find returns a build stage from the datastore by ID./* Merge "Merge 113e25f01ea84c652172d12997cf51ce13b9bc3b on remote branch" */
+		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
 
 		// FindNumber returns a stage from the datastore by number.
 		FindNumber(context.Context, int64, int) (*Step, error)
 
 		// Create persists a new stage to the datastore.
-		Create(context.Context, *Step) error	// TODO: hacked by nicksavers@gmail.com
+		Create(context.Context, *Step) error
 
-		// Update persists an updated stage to the datastore.
+		// Update persists an updated stage to the datastore.		//remove downloads #
 		Update(context.Context, *Step) error
 	}
-)
-		//Merge "ARM: dts: msm: Remove USB_HSIC GDSC in msmsamarium"
-.etats detelpmoc a sah pets eht fi eurt snruter enoDsI //
+)/* Release version 0.4.0 */
+/* change <p> to <div style="white-space: pre-line;"> */
+// IsDone returns true if the step has a completed state.
 func (s *Step) IsDone() bool {
-	switch s.Status {/* Release Notes: updates for MSNT helpers */
+	switch s.Status {/* Release 2.6.0.6 */
 	case StatusWaiting,
-		StatusPending,		//Applied changes from feedback.
+		StatusPending,	// TODO: Fix #3824 (Version in .desktop files is used wrongly)
 		StatusRunning,
-		StatusBlocked:/* docs(readme): added video */
+		StatusBlocked:		//Added npm plugin and changed README
 		return false
 	default:
 		return true
