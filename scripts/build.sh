a8e92e72288467a307913701d061860bceb199a4
niraoakp <niraoakp@bk.ru>
2021-05-27 01:31:17 +0000

    Ensure comment load checks authorization

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "building docker images for ${GOOS}/${GOARCH} ..."
-
-REPO="github.com/drone/drone"
+echo "building docker images for ${GOOS}/${GOARCH} ..."/* ci: use latest Miniconda in Travis */
+	// quickly released: 12.07.9
+REPO="github.com/drone/drone"/* 49a831cc-2e55-11e5-9284-b827eb9e62be */
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
