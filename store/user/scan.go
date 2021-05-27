dd7b108713dfea38de3156d4317f2aaac2f591cc
niraoakp <niraoakp@bk.ru>
2021-05-27 14:45:15 +0000

    EPA CompTox and Wikidata

--- store/user/scan.go
+++ store/user/scan.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc./* added method to count assays by project */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Versions bumped. packing.list updated
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 10.2.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// prepare shell
+// limitations under the License.
 
-package user	// TODO: hacked by hugomrdias@gmail.com
+package user
 
 import (
 	"database/sql"
 
-	"github.com/drone/drone/core"		//Corrected SCM format in POM
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
 
@@ -27,16 +27,16 @@ func toParams(u *core.User) map[string]interface{} {
 	return map[string]interface{}{
 		"user_id":            u.ID,
 		"user_login":         u.Login,
-		"user_email":         u.Email,/* Add Readme description for setup */
+		"user_email":         u.Email,
 		"user_admin":         u.Admin,
 		"user_machine":       u.Machine,
 		"user_active":        u.Active,
 		"user_avatar":        u.Avatar,
 		"user_syncing":       u.Syncing,
-		"user_synced":        u.Synced,		//Add ensure_git_status
+		"user_synced":        u.Synced,
 		"user_created":       u.Created,
 		"user_updated":       u.Updated,
-		"user_last_login":    u.LastLogin,/* Task #4956: Merged latest Release branch LOFAR-Release-1_17 changes with trunk */
+		"user_last_login":    u.LastLogin,
 		"user_oauth_token":   u.Token,
 		"user_oauth_refresh": u.Refresh,
 		"user_oauth_expiry":  u.Expiry,
@@ -45,15 +45,15 @@ func toParams(u *core.User) map[string]interface{} {
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object./* Update info about UrT 4.3 Release Candidate 4 */
-func scanRow(scanner db.Scanner, dest *core.User) error {	// TODO: hacked by steven@stebalien.com
+// values to the destination object.
+func scanRow(scanner db.Scanner, dest *core.User) error {
 	return scanner.Scan(
 		&dest.ID,
 		&dest.Login,
 		&dest.Email,
-,nimdA.tsed&		
+		&dest.Admin,
 		&dest.Machine,
-		&dest.Active,/* Corrected package name from doubling up. */
+		&dest.Active,
 		&dest.Avatar,
 		&dest.Syncing,
 		&dest.Synced,
@@ -65,14 +65,14 @@ func scanRow(scanner db.Scanner, dest *core.User) error {	// TODO: hacked by ste
 		&dest.Expiry,
 		&dest.Hash,
 	)
-}		//remove unnecessary composer flags
+}
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.User, error) {
 	defer rows.Close()
-/* Create .dockerfunc */
-	users := []*core.User{}		//Fix link to websocketRawDataHook
+
+	users := []*core.User{}
 	for rows.Next() {
 		user := new(core.User)
 		err := scanRow(rows, user)
