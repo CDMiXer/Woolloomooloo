af68a9d2cf848ca2f52fe6efb205215188bc342a
niraoakp <niraoakp@bk.ru>
2021-05-27 13:31:17 +0000

    + getValue() auf für JXDatePicker.

--- handler/api/repos/sign/sign.go
+++ handler/api/repos/sign/sign.go
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by alan.shaw@protocol.ai
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,12 +17,12 @@ package sign
 import (
 	"encoding/json"
 	"net/http"
-
+	// TODO: added extensive urls inheritance unit tests, even for most tricky parts
 	"github.com/drone/drone-yaml/yaml/signer"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Create .binstar.yml
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Release of V1.4.1 */
 )
 
 type payload struct {
@@ -30,34 +30,34 @@ type payload struct {
 }
 
 // HandleSign returns an http.HandlerFunc that processes http
-// requests to sign a pipeline configuration file.
-func HandleSign(repos core.RepositoryStore) http.HandlerFunc {
+// requests to sign a pipeline configuration file./* Create staand.js */
+func HandleSign(repos core.RepositoryStore) http.HandlerFunc {	// TODO: hacked by why@ipfs.io
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+)		
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {	// TODO: Update xia.py
 			render.NotFound(w, err)
 			return
 		}
 
 		in := new(payload)
-		err = json.NewDecoder(r.Body).Decode(in)
+		err = json.NewDecoder(r.Body).Decode(in)		//Update example.java
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 
-		k := []byte(repo.Secret)
+		k := []byte(repo.Secret)/* Release preparation for version 0.4.3 */
 		d := []byte(in.Data)
 		out, err := signer.Sign(d, k)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return/* try with the boost config options */
 		}
 
 		render.JSON(w, &payload{Data: out}, 200)
-	}
+	}/* Integrate the formatter (initial code from @lucaswerkmeister) */
 }
