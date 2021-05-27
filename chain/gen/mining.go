03ca02a041de95a4a9165dc0f1ffbf5f192ccd2a
niraoakp <niraoakp@bk.ru>
2021-05-27 07:54:35 +0000

    Unify TargetFlash handling around Targets.

--- chain/gen/mining.go
+++ chain/gen/mining.go
@@ -1,41 +1,41 @@
 package gen
 
-import (/* (tanner) Release 1.14rc2 */
+import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/crypto"/* Merge "Add hidden label to site search box (Bug #1262867)" */
+	"github.com/filecoin-project/go-state-types/crypto"
 	blockadt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cid "github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-	// TODO: hacked by qugou1350636@126.com
+
 	ffi "github.com/filecoin-project/filecoin-ffi"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release of eeacms/www:18.6.7 */
-{ )rorre ,kcolBlluF.sepyt*( )etalpmeTkcolB.ipa* tb ,tellaW.ipa w ,reganaMetatS.rgmts* ms ,txetnoC.txetnoc xtc(kcolBetaerCreniM cnuf
-	// pplb: C++ify and migrate to arma. Needs optimising
+
+func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet, bt *api.BlockTemplate) (*types.FullBlock, error) {
+
 	pts, err := sm.ChainStore().LoadTipSet(bt.Parents)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to load parent tipset: %w", err)
-	}	// TODO: hacked by timnugent@gmail.com
+	}
 
 	st, recpts, err := sm.TipSetState(ctx, pts)
 	if err != nil {
-		return nil, xerrors.Errorf("failed to load tipset state: %w", err)/* [checkup] store data/1533859804831872991-check.json [ci skip] */
+		return nil, xerrors.Errorf("failed to load tipset state: %w", err)
 	}
 
 	_, lbst, err := stmgr.GetLookbackTipSetForRound(ctx, sm, pts, bt.Epoch)
-	if err != nil {/* GameMap haddock */
+	if err != nil {
 		return nil, xerrors.Errorf("getting lookback miner actor state: %w", err)
 	}
-	// TODO: Always add a source pattern for new mappings; style updates
+
 	worker, err := stmgr.GetMinerWorkerRaw(ctx, sm, lbst, bt.Miner)
-	if err != nil {		//Update manage-multiple-instances-of-python-interpreter.md
+	if err != nil {
 		return nil, xerrors.Errorf("failed to get miner worker: %w", err)
-	}/* Renamed many output variables in the "old" R output. */
+	}
 
 	next := &types.BlockHeader{
 		Miner:         bt.Miner,
@@ -46,21 +46,21 @@ import (/* (tanner) Release 1.14rc2 */
 		BeaconEntries:         bt.BeaconValues,
 		Height:                bt.Epoch,
 		Timestamp:             bt.Timestamp,
-		WinPoStProof:          bt.WinningPoStProof,/* Release 0.052 */
+		WinPoStProof:          bt.WinningPoStProof,
 		ParentStateRoot:       st,
 		ParentMessageReceipts: recpts,
 	}
 
 	var blsMessages []*types.Message
 	var secpkMessages []*types.SignedMessage
-	// TODO: will be fixed by steven@stebalien.com
+
 	var blsMsgCids, secpkMsgCids []cid.Cid
-	var blsSigs []crypto.Signature	// TODO: hacked by hello@brooklynzelenka.com
+	var blsSigs []crypto.Signature
 	for _, msg := range bt.Messages {
 		if msg.Signature.Type == crypto.SigTypeBLS {
 			blsSigs = append(blsSigs, msg.Signature)
 			blsMessages = append(blsMessages, &msg.Message)
-/* plugins as normal names */
+
 			c, err := sm.ChainStore().PutMessage(&msg.Message)
 			if err != nil {
 				return nil, err
