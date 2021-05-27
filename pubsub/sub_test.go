1b1ab7ea96173718e898e9fd3d12191c23f67424
niraoakp <niraoakp@bk.ru>
2021-05-27 21:36:41 +0000

    Delete user_activate_passwd.txt

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Add some fonts + Add dropFile when delete a Comic.
-// +build !oss/* Improved the Settings class */
+
+// +build !oss
 
 package pubsub
 
-import (	// Merge pull request #6758 from popcornmix/jpegtimeout
+import (
 	"testing"
 
 	"github.com/drone/drone/core"
-)/* Merge "wlan: Release 3.2.3.138" */
+)
 
 func nop(*core.Message) {}
 
 func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Message, 5),/* Project: Add travis badge to readme */
+		handler: make(chan *core.Message, 5),
 		quit:    make(chan struct{}),
-	}		//update accounts popup when accounts change
+	}
 
 	e := new(core.Message)
 	s.publish(e)
-	// Fixed duplicate options in font picker drop down.
-	if got, want := len(s.handler), 1; got != want {	// TODO: hacked by hugomrdias@gmail.com
+
+	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want event received from channel")
-	}	// TODO: LDEV-4746 Fix migration script for Talca issues
+	}
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-}		//Fixed import for SimpleFilter
-/* Addition of Constants class */
-func TestSubscription_buffer(t *testing.T) {	// TODO: will be fixed by jon@atack.com
-	s := &subscriber{	// TODO: hacked by praveen@minio.io
+}
+
+func TestSubscription_buffer(t *testing.T) {
+	s := &subscriber{
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
 	}
-		//Merge "Re-apply "Improve subobject handling in SMWSemanticData""
-sneppah tahw etalumis ot 1 si ezis reffub eht //	
+
+	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
 	// and the buffer fills up. In this case, events
 	// should be ignored until pending events are
