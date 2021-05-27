24ccd09e6c8423de138ce635bd02338ff62c449b
niraoakp <niraoakp@bk.ru>
2021-05-27 04:58:49 +0000

    Remove requirement to have is or seeks in NeedModelWrapper

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by fjl@ethereum.org
+/* Tests fixes. */
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Fixing mobile header missing content */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }/* Renamed len4caid into cam_common_len4caid (forgot to commit these files) */
+    }
 }
 
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
-const res1 = new Resource("newres1", {
-    aliases: [{ name: "res1" }],/* Release de la v2.0.1 */
-});/* Release build for API */
+const res1 = new Resource("newres1", {/* Release of eeacms/www:20.5.26 */
+    aliases: [{ name: "res1" }],
+});
