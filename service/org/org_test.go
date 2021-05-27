5bbed3de52a5e80322e4cc356208e8684925e242
niraoakp <niraoakp@bk.ru>
2021-05-27 11:37:55 +0000

    Pretty print workspaces.

--- service/org/org_test.go
+++ service/org/org_test.go
@@ -4,37 +4,37 @@
 
 package orgs
 
-import (
+import (	// TODO: mybuild: Merge master into mybuild (argh, again!)
 	"context"
-	"testing"
+	"testing"/* Updated Release notes for 1.3.0 */
 	"time"
 
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge "Update VP8DX_BOOL_DECODER_FILL to better detect EOS" */
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
-
+/* Rename KERNEL files to include MIPS prefix */
 	"github.com/golang/mock/gomock"
-)
+)		//Merge "vrouter: handling of Hyper-V Switch requests in vrouter"
 
 var noContext = context.Background()
 
 func TestList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Fix import order.
 	checkToken := func(ctx context.Context, opts scm.ListOptions) {
-		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)
+		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)	// TODO: Changing "set_vacation" to "create_vacation"
 		if !ok {
 			t.Errorf("Expect token stored in context")
-			return
+			return	// Updated Fuel to 2.2.0 with the 64 bit adaptations
 		}
 		want := &scm.Token{
 			Token:   "755bb80e5b",
 			Refresh: "e08f3fa43e",
-			Expires: time.Unix(1532292869, 0),
-		}
+			Expires: time.Unix(1532292869, 0),/* Release alpha 1 */
+		}	// qemu: save/load: replacing fseek() calls with qemu_fseek() calls
 		if diff := cmp.Diff(got, want); diff != "" {
 			t.Errorf(diff)
 		}
@@ -48,21 +48,21 @@ func TestList(t *testing.T) {
 
 	mockUser := &core.User{
 		Login:   "octocat",
-		Token:   "755bb80e5b",
+		Token:   "755bb80e5b",/* [skip ci] Add config file for Release Drafter bot */
 		Refresh: "e08f3fa43e",
 		Expiry:  1532292869,
 	}
 	mockOrgs := []*scm.Organization{
-		{
-			Name:   "github",
+		{/* 5d580cda-2e62-11e5-9284-b827eb9e62be */
+			Name:   "github",	// Jokebox test now shows sound/music playing status.
 			Avatar: "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
 		},
-	}
-	mockOrgService := mockscm.NewMockOrganizationService(controller)
+	}		//Extended named injections for constructors and setters plus url separation bonus
+	mockOrgService := mockscm.NewMockOrganizationService(controller)		//Fix EDP default timings
 	mockOrgService.EXPECT().List(gomock.Any(), gomock.Any()).Do(checkToken).Return(mockOrgs, nil, nil)
 
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)/* MOV: files into subnamespaces */
 
 	client := new(scm.Client)
 	client.Organizations = mockOrgService
