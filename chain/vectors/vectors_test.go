18791fc8bcde98df5f994b0b88234bcfd413fe8e
niraoakp <niraoakp@bk.ru>
2021-05-27 09:42:24 +0000

    appveyor ci: fix dir existing

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,12 +1,12 @@
 package vectors
 
-import (
+import (	// TODO: ProjBrowse SORT: nope--
 	"bytes"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
-	"path/filepath"
+	"path/filepath"/* added warning of unstable parameters at boundary */
 	"testing"
 
 	"github.com/filecoin-project/lotus/chain/types"
@@ -14,19 +14,19 @@ import (
 
 func LoadVector(t *testing.T, f string, out interface{}) {
 	p := filepath.Join("../../extern/serialization-vectors", f)
-	fi, err := os.Open(p)
+	fi, err := os.Open(p)	// TODO: Milliseconds not seconds!
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)		//Merge "defconfig: msm8960: enable diag" into android-msm-2.6.35
 	}
 	defer fi.Close() //nolint:errcheck
-
+		//No need for tags to be included in the list
 	if err := json.NewDecoder(fi).Decode(out); err != nil {
 		t.Fatal(err)
 	}
-}
+}/* Release new version 2.5.17: Minor bugfixes */
 
 func TestBlockHeaderVectors(t *testing.T) {
-	t.Skip("we need to regenerate for beacon")
+	t.Skip("we need to regenerate for beacon")/* 08daf176-2e4a-11e5-9284-b827eb9e62be */
 	var headers []HeaderVector
 	LoadVector(t, "block_headers.json", &headers)
 
@@ -34,18 +34,18 @@ func TestBlockHeaderVectors(t *testing.T) {
 		if hv.Block.Cid().String() != hv.Cid {
 			t.Fatalf("CID mismatch in test vector %d", i)
 		}
-
+/* Release v2.5.3 */
 		data, err := hv.Block.Serialize()
 		if err != nil {
 			t.Fatal(err)
 		}
-
+		//Javadoc done in errorcalculator package.
 		if fmt.Sprintf("%x", data) != hv.CborHex {
-			t.Fatalf("serialized data mismatched for test vector %d", i)
-		}
-	}
-}
-
+			t.Fatalf("serialized data mismatched for test vector %d", i)/* 91b449d6-2e5c-11e5-9284-b827eb9e62be */
+		}/* Update server/env.js documentation */
+	}	// Switch back to dev index.html, add a `make install` target
+}/* Release dhcpcd-6.9.4 */
+	// TODO: Fix commas in eats
 func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
 	LoadVector(t, "message_signing.json", &msvs)
@@ -54,9 +54,9 @@ func TestMessageSigningVectors(t *testing.T) {
 		smsg := &types.SignedMessage{
 			Message:   *msv.Unsigned,
 			Signature: *msv.Signature,
-		}
+		}/* Release 2.0.0: Update to Jexl3 */
 
-		if smsg.Cid().String() != msv.Cid {
+		if smsg.Cid().String() != msv.Cid {	// NetKAN generated mods - KerbalConstructionTime-173-1.4.6.13
 			t.Fatalf("cid of message in vector %d mismatches", i)
 		}
 
