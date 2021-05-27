98ca02d82f4a27feeb20117295d9b34241fd6885
niraoakp <niraoakp@bk.ru>
2021-05-27 20:20:20 +0000

    add new po files

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Added start point for receive benchmark on inititator side. */
+
 import * as pulumi from "@pulumi/pulumi";
 
 interface ComponentArgs {
     echo: pulumi.Input<any>;
 }
 
-export class Component extends pulumi.ComponentResource {
+export class Component extends pulumi.ComponentResource {/* patch for lack of log2 on msvc */
     public readonly echo!: pulumi.Output<any>;
-    public readonly childId!: pulumi.Output<pulumi.ID>;		//New version of Ugallu - 0.1.7
+    public readonly childId!: pulumi.Output<pulumi.ID>;
 
     constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
-        const inputs: any = {};/* Delete church.jpeg */
+        const inputs: any = {};
         inputs["echo"] = args.echo;
-        inputs["childId"] = undefined /*out*/;/* Release 1.9 Code Commit. */
+        inputs["childId"] = undefined /*out*/;	// TODO: changed constants to dev system
 
         super("testcomponent:index:Component", name, inputs, opts, true);
-    }
+    }	// upgrade to 2.6.15
 }
-
+		//Organized the test files
