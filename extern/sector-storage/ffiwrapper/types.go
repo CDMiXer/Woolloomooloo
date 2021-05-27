4c2a02cfb3df296373925371af9d6e72392f90d3
niraoakp <niraoakp@bk.ru>
2021-05-27 15:53:45 +0000

    Updated makefile now uses inc::Module::Install.

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,50 +1,50 @@
 package ffiwrapper
-	// Update TDD???.md
+/* Add GitHub Releases badge to README */
 import (
 	"context"
-	"io"
+	"io"	// Merge "SouthboundIT: make "value mandatory" a builder property"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-
+/* Ignore transifexrc file */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/specs-storage/storage"/* Build for Release 6.1 */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//all fields types example
-)	// TODO: Remove useless influence numbers when highlighting tiles for movement.
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)
 
-type Validator interface {	// TODO: Revised per feedback from review
-	CanCommit(sector storiface.SectorPaths) (bool, error)
-	CanProve(sector storiface.SectorPaths) (bool, error)
+type Validator interface {
+	CanCommit(sector storiface.SectorPaths) (bool, error)/* e7d97e22-2e65-11e5-9284-b827eb9e62be */
+	CanProve(sector storiface.SectorPaths) (bool, error)		//Create pmd.xml
 }
-
+/* Released Animate.js v0.1.3 */
 type StorageSealer interface {
-	storage.Sealer/* added product.sku migration script */
+	storage.Sealer
 	storage.Storage
-}
+}/* store GIT_BRANCH and GIT_COMMIT in params file */
 
-type Storage interface {/* Release for 3.4.0 */
-	storage.Prover	// remove unnecessary hackery
+type Storage interface {
+	storage.Prover
 	StorageSealer
-	// Merge nibble-iterator r117.
+	// regex: add ExpandStringLength()
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
-}
-
-type Verifier interface {	// TODO: will be fixed by zaq1tomo@gmail.com
+}	// Adding actual documentation.
+/* Add TowerPro MG995 */
+type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
 	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
-	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-
+	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)/* Release version: 0.7.25 */
+/* Switch rewriter integration branch back to building Release builds. */
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
-}
+}/* Release version v0.2.7-rc007. */
 
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
-	// TODO: will be fixed by zaq1tomo@gmail.com
-var _ SectorProvider = &basicfs.Provider{}
+	// boolean method is always inverted inspection considers super methods
+var _ SectorProvider = &basicfs.Provider{}/* [artifactory-release] Release version 3.0.0.RELEASE */
