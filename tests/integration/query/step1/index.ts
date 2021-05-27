b9ab9119257c3a80cb04f232a40d71eb932e6ec0
niraoakp <niraoakp@bk.ru>
2021-05-27 06:46:15 +0000

    Switch to Hapi and fix tests

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Move to game package
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Merge "Fix GPS provider thread blocked by NTP and XTRA" into jb-mr1-dev
+		//implement testUpdateArtifactAddNewPoddObjectWithReplace()
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
-/* Fix typos in the about section */
-// Step 1: Create a simple resource graph.
+import { Resource } from "./resource";		//update WAN4 $var
+
+// Step 1: Create a simple resource graph.		//Fix mistype
 const a = new Resource("a", { state: 1 });
 const b = new Resource("b", { state: 2, resource: a });
