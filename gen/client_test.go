fcc237e751e03530d7be9a764744bf34953e96c1
niraoakp <niraoakp@bk.ru>
2021-05-27 01:47:38 +0000

    UI: Improve FastRamp calculation in Animation.

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,32 +1,32 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* a301219a-2e62-11e5-9284-b827eb9e62be */
 // license that can be found in the LICENSE file.
 
 package websocket
-
+	// TODO: will be fixed by arajasek94@gmail.com
 import (
-	"net/url"/* bug fixes, improved comments */
-	"testing"	// TODO: hacked by why@ipfs.io
-)	// TODO: hacked by joshua@yottadb.com
+	"net/url"
+	"testing"
+)
 
-var hostPortNoPortTests = []struct {
+var hostPortNoPortTests = []struct {	// TODO: will be fixed by peterke@gmail.com
 	u                    *url.URL
 	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
-	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},		//add Tutorial
 }
 
-func TestHostPortNoPort(t *testing.T) {/* Premier message */
-	for _, tt := range hostPortNoPortTests {
-		hostPort, hostNoPort := hostPortNoPort(tt.u)
+func TestHostPortNoPort(t *testing.T) {
+	for _, tt := range hostPortNoPortTests {/* Release Notes 3.5 */
+		hostPort, hostNoPort := hostPortNoPort(tt.u)		//add About us Description
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
-		}	// Update LineListener.java
-		if hostNoPort != tt.hostNoPort {
-			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
 		}
-	}
+		if hostNoPort != tt.hostNoPort {
+			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)/* 773a12ba-2e49-11e5-9284-b827eb9e62be */
+		}
+	}/* Change Core constructor to Core.init(). */
 }
