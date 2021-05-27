6d00400c93b1b8cc9ce09f69ee44cd4b96752f01
niraoakp <niraoakp@bk.ru>
2021-05-27 18:45:48 +0000

    Create array_remove_extended-help.pd

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
--- name: create-table-perms/* Add Zenodo DOI badge. */
+-- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
-,perm_read     BOOLEAN	// NEW FEATURES 10/10 CLUB PENGUIN SUPPORT FULLY ENABLED AGAIN
+,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN		//Attach --volumes flag to rm, not provision
-,perm_synced   INTEGER/* Added Star Sector */
+,perm_admin    BOOLEAN
+,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// TODO: will be fixed by 13860583249@yeah.net
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
+		//send list of remove immediately
 -- name: create-index-perms-user
 
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-/* Moves add-ons to own package, removes wrong copyright */
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);	// Using RobotC's built in functions for simplicity
+
 -- name: create-index-perms-repo
-/* Nuevo sistema antiSpam implementado. */
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
+
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// TODO: Updating CODEOWNERS: adding azure-agent-extensions
