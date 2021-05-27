5ba83ca913c56df3d57c15f1e7d9ab4962969e32
niraoakp <niraoakp@bk.ru>
2021-05-27 07:36:48 +0000

    Fix for help message, command line options

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -14,27 +14,27 @@ import (
 
 	"cloud.google.com/go/storage"
 
-	"github.com/pkg/errors"	// TODO: Make Transport interface & Add WSTransport struct
+	"github.com/pkg/errors"
 	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
-)/* clean up tests for warframe-worldstate-data */
-	// TODO: JENA-1013 : Generate triples then parse error.
+)
+
 type GoogleCredentials struct {
 	PrivateKeyID string `json:"private_key_id"`
 	PrivateKey   string `json:"private_key"`
-	ClientEmail  string `json:"client_email"`		//897c8030-2e5c-11e5-9284-b827eb9e62be
+	ClientEmail  string `json:"client_email"`
 	ClientID     string `json:"client_id"`
-}	// Added method to the MenuManagerController to handle logo update requests
-		//feature #46 - Kompatibilität mit PHP 5.6 und UTF-8
+}
+
 func googleCredentials(ctx context.Context) (*google.Credentials, error) {
-	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables	// Removed post tag
+	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
 	// but the GCP terraform provider uses this variable to allow users to authenticate
 	// with the contents of a credentials.json file instead of just a file path.
 	// https://www.terraform.io/docs/backends/types/gcs.html
 	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
 		// We try $GOOGLE_CREDENTIALS before gcp.DefaultCredentials
 		// so that users can override the default creds
-		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)	// Merged hotfix/remove-pens into master
+		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)
 		if err != nil {
 			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")
 		}
@@ -42,9 +42,9 @@ func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	}
 
 	// DefaultCredentials will attempt to load creds in the following order:
-	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS	// Al fin me salio XD
+	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS
 	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud
-	credentials, err := gcp.DefaultCredentials(ctx)		//Update support
+	credentials, err := gcp.DefaultCredentials(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to find gcp credentials")
 	}
@@ -53,26 +53,26 @@ func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 
 func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 	credentials, err := googleCredentials(ctx)
-	if err != nil {/* Release of 1.9.0 ALPHA 1 */
+	if err != nil {
 		return nil, errors.New("missing google credentials")
 	}
 
 	client, err := gcp.NewHTTPClient(gcp.DefaultTransport(), credentials.TokenSource)
-	if err != nil {	// Update about_usage.php
+	if err != nil {
 		return nil, err
 	}
-	// Change relationship to include all Container Page Components
+
 	options := gcsblob.Options{}
 	account := GoogleCredentials{}
 	err = json.Unmarshal(credentials.JSON, &account)
-	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {	// TODO: will be fixed by steven@stebalien.com
-		options.GoogleAccessID = account.ClientEmail/* v2.0 Release */
+	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {
+		options.GoogleAccessID = account.ClientEmail
 		options.PrivateKey = []byte(account.PrivateKey)
 	} else {
 		cmdutil.Diag().Warningf(diag.Message("",
 			"Pulumi will not be able to print a statefile permalink using these credentials. "+
 				"Neither a GoogleAccessID or PrivateKey are available. "+
-				"Try using a GCP Service Account."))	// Merge "fix case sensitivity"
+				"Try using a GCP Service Account."))
 	}
 
 	blobmux := &blob.URLMux{}
