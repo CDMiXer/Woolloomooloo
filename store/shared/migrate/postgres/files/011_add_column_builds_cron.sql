70f60b99dabac0236fb41b6ba16787e149785d57
niraoakp <niraoakp@bk.ru>
2021-05-27 04:28:49 +0000

    Kaminari hinzugefügt

--- store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron/* Delete pic.JPG */
-	// TODO: hacked by cory@protocol.ai
+-- name: alter-table-builds-add-column-cron
+
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
