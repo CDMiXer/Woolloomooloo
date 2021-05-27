ec086a272ba2e19320a9f5a0dfab37dcb91c3c9b
niraoakp <niraoakp@bk.ru>
2021-05-27 20:38:44 +0000

    newSerial definition for Opt_ForwardTracing

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,37 +1,37 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Start working on 'annotate_flat' which conforms to the original spec. */
 // +build nodejs all
 
 package ints
-
+	// TODO: Fixed simple mistake on line 40
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-.stuptpuo ecruoser s'kcats eht revo yreuq a snur dna kcats a setaerc yreuQtseT //
+// TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		// Create Pulumi resources.
 		Dir:          "step1",
-		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
+		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",/* Remove createReleaseTag task dependencies */
 		Dependencies: []string{"@pulumi/pulumi"},
 		CloudURL:     "file://~", // Required; we hard-code the stack name
 		EditDirs: []integration.EditDir{
 			// Try to create resources during `pulumi query`. This should fail.
 			{
-				Dir:           "step2",/* Fix rest API.  */
-				Additive:      true,/* - Release to get a DOI */
-				QueryMode:     true,
+				Dir:           "step2",
+				Additive:      true,
+				QueryMode:     true,/* remove old basic function of log property of base class and use factory log */
 				ExpectFailure: true,
 			},
-			// Run a query during `pulumi query`. Should succeed.
+			// Run a query during `pulumi query`. Should succeed./* Release of version 1.2.3 */
 			{
 				Dir:           "step3",
-				Additive:      true,/* spawn/Prepared: Append() returns bool */
-				QueryMode:     true,/* Add useNio() and enableNaming() methods */
+				Additive:      true,
+				QueryMode:     true,/* updated Windows Release pipeline */
 				ExpectFailure: false,
 			},
-		},
-	})
-}
+		},/* Create Fit and predict for regression */
+	})/* Testing expanding and folding. */
+}		//delete old ttw.js
