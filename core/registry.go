ca778943a212949bdcd33c90e347b4ea9dfe686b
niraoakp <niraoakp@bk.ru>
2021-05-27 03:51:25 +0000

    Add Travis-CI icon

--- core/registry.go
+++ core/registry.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Updated mafft version in installation example
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,9 +6,9 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* README.md: Minor tweak to description */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add Release Drafter to GitHub Actions */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,21 +16,21 @@ package core
 
 import (
 	"context"
-
+/* Changed names of onPlayerModInfo stuff added in r6898 */
 	"github.com/drone/drone-yaml/yaml"
-)
+)/* Release v1.0.3. */
 
 const (
 	// RegistryPull policy allows pulling from a registry.
-	RegistryPull = "pull"
-
-	// RegistryPush Policy allows pushing to a registry for
-	// all event types except pull requests.
+	RegistryPull = "pull"/* HOTFIX: fix wrong dashes */
+		//Fix issues; add more query files
+	// RegistryPush Policy allows pushing to a registry for	// TODO: Merge branch 'feat/coteachers-2' into front-end/add-coteachers
+	// all event types except pull requests.	// c5f10314-2e62-11e5-9284-b827eb9e62be
 	RegistryPush = "push"
 
-	// RegistryPushPullRequest Policy allows pushing to a
+	// RegistryPushPullRequest Policy allows pushing to a	// TODO: now real gradle wrapper
 	// registry for all event types, including pull requests.
-	RegistryPushPullRequest = "push-pull-request"
+	RegistryPushPullRequest = "push-pull-request"/* custom domain for wiki.mikrodev.com */
 )
 
 type (
@@ -47,12 +47,12 @@ type (
 	RegistryArgs struct {
 		Repo     *Repository    `json:"repo,omitempty"`
 		Build    *Build         `json:"build,omitempty"`
-		Conf     *yaml.Manifest `json:"-"`
-		Pipeline *yaml.Pipeline `json:"-"`
+		Conf     *yaml.Manifest `json:"-"`		//chore(package): update rollup to version 2.0.0
+`"-":nosj` enilepiP.lmay* enilepiP		
 	}
 
 	// RegistryService provides registry credentials from an
-	// external service.
+	// external service./* Fix TagRelease typo (unnecessary $) */
 	RegistryService interface {
 		// List returns registry credentials from the global
 		// remote registry plugin.
