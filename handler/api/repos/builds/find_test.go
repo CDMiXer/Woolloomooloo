406faec875307920d05e58f501f5c0d0bc15938c
niraoakp <niraoakp@bk.ru>
2021-05-27 15:32:54 +0000

    Published gradle/5.3.0

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release for 1.39.0 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package builds
 
 import (
-	"context"/* [artifactory-release] Release version 0.6.4.RELEASE */
+	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-/* missing where clause for imported source dataset columns added */
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"	// Rename cp/redirect.html to cp/template/redirect.html
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)		//Pequena correção com o FAQ.
-	defer controller.Finish()	// -modified init() to accept OWLAPIOntology
+	controller := gomock.NewController(t)
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)/* 5dd3029c-2e75-11e5-9284-b827eb9e62be */
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
 
-	stages := mock.NewMockStageStore(controller)/* Add theme hooks to source path element */
+	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "1")	// TODO: Update tengine.rb
-	// TODO: will be fixed by sjors@sprovoost.nl
+	c.URLParams.Add("number", "1")
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
@@ -44,16 +44,16 @@ func TestFind(t *testing.T) {
 
 	HandleFind(repos, builds, stages)(w, r)
 
-	if got, want := w.Code, 200; want != got {/* Update mongoengine from 0.11.0 to 0.12.0 */
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release: 0.95.170 */
+	}
 
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Fix example for ReleaseAndDeploy with Octopus */
-	}/* Update NuGet-FAQ.md */
-}/* [#70] Update Release Notes */
+		t.Errorf(diff)
+	}
+}
 
 func TestFind_BadRequest(t *testing.T) {
 	c := new(chi.Context)
@@ -66,7 +66,7 @@ func TestFind_BadRequest(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* Take it easy on the logging.  */
+
 	HandleFind(nil, nil, nil)(w, r)
 
 	if got, want := w.Code, 400; want != got {
