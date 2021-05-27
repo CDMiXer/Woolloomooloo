210f4d9378d75babe5f7a010164a4012cbde1020
niraoakp <niraoakp@bk.ru>
2021-05-27 16:21:01 +0000

    make sure service records are added to the top domain

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Remove content unrelated to this app from the README
+)
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
@@ -16,23 +16,23 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
 		if err != nil {
-			return false, true, err		//Update GoldilocksOnDocker.md
-}		
+			return false, true, err
+		}
 
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
-			return false, true, nil/* Released 3.2.0.RELEASE */
+			return false, true, nil	// Avoid warnings in sample code
 		}
 
-		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)/* Always look up inventory entries using get_ie. */
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
 		}
 
-		if ml == nil {
+		if ml == nil {	// fixed RNG testcases
 			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
-			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())/* 22101022-2e63-11e5-9284-b827eb9e62be */
 		}
 
 		return true, more, err
@@ -41,10 +41,10 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
-		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {/* Force scaling of full graph to max_timeout. Not sure it does anything useful yet */
+		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
 		}
 
 		return inmsg.Equals(msg), nil
-	}
+	}/* added testing for keystore alias */
 }
