05dae123eedb8a3e27061d4614b8a4c9ee56870f
niraoakp <niraoakp@bk.ru>
2021-05-27 02:42:11 +0000

    Add script for Nim Replica

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
 package sealing
-	// TODO: Added/modified ...2String methods
-import (		//e2e08aa8-2e58-11e5-9284-b827eb9e62be
+
+import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+	// TODO: will be fixed by joshua@yottadb.com
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
-}/* moved composition logic out of UrlRule inside composition component. */
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error/* Entwicklungs-Bildressourcen hinzugefügt */
+}
