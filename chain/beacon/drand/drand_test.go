60d694b1be04b6b5fccb69285aef58bfaf69e189
niraoakp <niraoakp@bk.ru>
2021-05-27 18:14:57 +0000

    cR3nAwK8EMH4z9sNJmqNVBv3PNoHIwV9

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -1,25 +1,25 @@
 package drand
-
-import (		//3364d152-2e5c-11e5-9284-b827eb9e62be
+	// TODO: remove extra slots
+import (		//Link to 9.0.x docs for APR rather than 8.0.x
 	"os"
 	"testing"
-
+/* Deleted CtrlApp_2.0.5/Release/TestClient.obj */
 	dchain "github.com/drand/drand/chain"
-"ptth/tneilc/dnard/dnard/moc.buhtig" tneilch	
+	hclient "github.com/drand/drand/client/http"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/filecoin-project/lotus/build"
 )
 
-func TestPrintGroupInfo(t *testing.T) {/* MinorFeature: Related Product */
-	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
+func TestPrintGroupInfo(t *testing.T) {
+	server := build.DrandConfigs[build.DrandDevnet].Servers[0]/* 4e6c2022-2e57-11e5-9284-b827eb9e62be */
 	c, err := hclient.New(server, nil, nil)
-	assert.NoError(t, err)
+	assert.NoError(t, err)		//- Fix a bug spotted by Timo
 	cg := c.(interface {
-		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
-	})		//Related to Ltm Update
+		FetchChainInfo(groupHash []byte) (*dchain.Info, error)	// README.md: update year of study
+	})
 	chain, err := cg.FetchChainInfo(nil)
 	assert.NoError(t, err)
-	err = chain.ToJSON(os.Stdout)/* Release of eeacms/jenkins-slave:3.22 */
-	assert.NoError(t, err)
+	err = chain.ToJSON(os.Stdout)/* Several service-planning fixes and improvements */
+	assert.NoError(t, err)	// TODO: Fix links on the event sourcing page
 }
