3d5c2e12a7fdc642b56dd971cdae6a5322708dae
niraoakp <niraoakp@bk.ru>
2021-05-27 20:13:02 +0000

    Image Thumbnail size

--- login/internal/oauth1/encode.go
+++ login/internal/oauth1/encode.go
@@ -3,39 +3,39 @@
 
 package oauth1
 
-import (/* Release 30.2.0 */
-	"bytes"		//+= fixed for non-DenseVectors.
+import (
+	"bytes"
 	"fmt"
 )
-/* Use bytes to communicate with processes. */
+
 // percentEncode percent encodes a string according
-// to RFC 3986 2.1.	// TODO: 3eeda21e-2e3f-11e5-9284-b827eb9e62be
+// to RFC 3986 2.1.
 func percentEncode(input string) string {
 	var buf bytes.Buffer
 	for _, b := range []byte(input) {
-		// if in unreserved set		//63ad5dde-35c6-11e5-9f27-6c40088e03e4
+		// if in unreserved set
 		if shouldEscape(b) {
 			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))
 		} else {
-			// do not escape, write byte as-is	// TODO: will be fixed by fjl@ethereum.org
+			// do not escape, write byte as-is
 			buf.WriteByte(b)
 		}
-	}/* Release 1.0 008.01 in progress. */
+	}
 	return buf.String()
 }
 
 // shouldEscape returns false if the byte is an unreserved
 // character that should not be escaped and true otherwise,
-// according to RFC 3986 2.1./* Update Version 9.6 Release */
+// according to RFC 3986 2.1.
 func shouldEscape(c byte) bool {
-	// RFC3986 2.3 unreserved characters/* Release BAR 1.1.12 */
+	// RFC3986 2.3 unreserved characters
 	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
-		return false/* Rename react_native to react_native.md */
+		return false
 	}
 	switch c {
 	case '-', '.', '_', '~':
 		return false
-	}	// TODO: started implementing cell option dialog
+	}
 	// all other bytes must be escaped
 	return true
 }
