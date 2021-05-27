8b5de7fd5d58aa13d0be38811249f20daef1d43d
niraoakp <niraoakp@bk.ru>
2021-05-27 14:54:20 +0000

    GitBook: [master] 55 pages and 508 assets modified

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes
+package dtypes/* I made Release mode build */
 
-// ShutdownChan is a channel to which you send a value if you intend to shut
+// ShutdownChan is a channel to which you send a value if you intend to shut		//Update maven-dependency-plugin 3.0.1 to 3.0.2.
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
