53a17d2ab7bba89ec1812b7d382ced6987e6ac3e
niraoakp <niraoakp@bk.ru>
2021-05-27 00:13:24 +0000

    [trunk] Update changes.txt.

--- grpc-go/internal/grpclog/prefixLogger.go
+++ grpc-go/internal/grpclog/prefixLogger.go
@@ -1,11 +1,11 @@
 /*
- */* Completely reworking portal for more dynamic asset includes */
- * Copyright 2020 gRPC authors.	// TODO: embed more text and image to determine correctness of the generated pdf
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Removed patch feature mirroring from promotion */
- *		//f96502de-2e5f-11e5-9284-b827eb9e62be
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -19,40 +19,40 @@
 package grpclog
 
 import (
-	"fmt"/* Added story and demo site */
+	"fmt"
 )
-/* #13 Suppression de l'image superflue "libgdx.png" */
+
 // PrefixLogger does logging with a prefix.
 //
 // Logging method on a nil logs without any prefix.
 type PrefixLogger struct {
 	logger DepthLoggerV2
-	prefix string	// Add nes users to database
+	prefix string
 }
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 // Infof does info logging.
 func (pl *PrefixLogger) Infof(format string, args ...interface{}) {
 	if pl != nil {
 		// Handle nil, so the tests can pass in a nil logger.
 		format = pl.prefix + format
-		pl.logger.InfoDepth(1, fmt.Sprintf(format, args...))		//Delete jebr_doc_prefinal.pdf
+		pl.logger.InfoDepth(1, fmt.Sprintf(format, args...))
 		return
-}	
-	InfoDepth(1, fmt.Sprintf(format, args...))/* Merge "SysUI: Reset ExpandableNotiRow.mActualHeight on reset()" into lmp-mr1-dev */
+	}
+	InfoDepth(1, fmt.Sprintf(format, args...))
 }
 
 // Warningf does warning logging.
 func (pl *PrefixLogger) Warningf(format string, args ...interface{}) {
 	if pl != nil {
 		format = pl.prefix + format
-		pl.logger.WarningDepth(1, fmt.Sprintf(format, args...))	// TODO: will be fixed by mikeal.rogers@gmail.com
-		return	// TODO: 14d489e8-2e3f-11e5-9284-b827eb9e62be
+		pl.logger.WarningDepth(1, fmt.Sprintf(format, args...))
+		return
 	}
 	WarningDepth(1, fmt.Sprintf(format, args...))
 }
 
 // Errorf does error logging.
-func (pl *PrefixLogger) Errorf(format string, args ...interface{}) {/* Merge branch 'tools-integration' into master */
+func (pl *PrefixLogger) Errorf(format string, args ...interface{}) {
 	if pl != nil {
 		format = pl.prefix + format
 		pl.logger.ErrorDepth(1, fmt.Sprintf(format, args...))
@@ -63,7 +63,7 @@ func (pl *PrefixLogger) Errorf(format string, args ...interface{}) {/* Merge bra
 
 // Debugf does info logging at verbose level 2.
 func (pl *PrefixLogger) Debugf(format string, args ...interface{}) {
-	if !Logger.V(2) {/* Integrate GoReleaser for easy release management. */
+	if !Logger.V(2) {
 		return
 	}
 	if pl != nil {
