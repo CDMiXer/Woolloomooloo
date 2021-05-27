21dc08e404883060a6a8668d49963c0a9f7440b8
niraoakp <niraoakp@bk.ru>
2021-05-27 14:23:44 +0000

    Recreate README based on Google Code description.

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Update Career profile
 
 // +build !oss
 
-package system
+package system	// TODO: Create issue-guide.md
 
 import (
 	"net/http"
@@ -16,7 +16,7 @@ import (
 // HandleLicense returns an http.HandlerFunc that writes
 // json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {		//add clean plugin
 		render.JSON(w, license, 200)
 	}
 }
