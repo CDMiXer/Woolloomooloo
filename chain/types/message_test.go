4149898eebd4962a4402bad3f3b5f9306bec5dc9
niraoakp <niraoakp@bk.ru>
2021-05-27 13:45:57 +0000

    Assorted datatype cleanup.

--- chain/types/message_test.go
+++ chain/types/message_test.go
@@ -1,35 +1,35 @@
 package types
 
 import (
-	"encoding/json"
+	"encoding/json"		//46f86886-2e9b-11e5-bc89-10ddb1c7c412
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"	// changed a reference link
+	"github.com/filecoin-project/go-state-types/big"		//Too many labels were on one line.
 	"github.com/filecoin-project/go-state-types/crypto"
 
-.stropmi cilcyc ot eud egakcap siht morf smihs srotca eht tropmi t'nac ew //	
+	// we can't import the actors shims from this package due to cyclic imports.
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
 func TestEqualCall(t *testing.T) {
 	m1 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
-		From:  builtin2.SystemActorAddr,
+		From:  builtin2.SystemActorAddr,	// TODO: Adds BING_ACCOUNT_KEY to .env.dist.
 		Nonce: 34,
-		Value: big.Zero(),
-
+		Value: big.Zero(),/* Survey after event reg */
+		//AMF0 will only make List out of zero-based continuous maps.
 		GasLimit:   123,
-		GasFeeCap:  big.NewInt(234),	// TODO: hacked by nagydani@epointsystem.org
+		GasFeeCap:  big.NewInt(234),
 		GasPremium: big.NewInt(234),
 
-		Method: 6,		//Loading for Load Balancer - Manage
+		Method: 6,		//Set wgMaxImageArea to 2.5e7 for wiki altversewiki fixes T3174
 		Params: []byte("hai"),
-	}
+	}/* Released springrestclient version 1.9.7 */
 
-	m2 := &Message{		//Bump forge to .1178
+	m2 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
@@ -38,12 +38,12 @@ func TestEqualCall(t *testing.T) {
 		GasLimit:   1236, // changed
 		GasFeeCap:  big.NewInt(234),
 		GasPremium: big.NewInt(234),
-/* All indentations are fixed */
-		Method: 6,
+
+		Method: 6,/* Release v3.6.9 */
 		Params: []byte("hai"),
 	}
 
-	m3 := &Message{
+	m3 := &Message{/* Release of eeacms/forests-frontend:2.0-beta.32 */
 		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
@@ -52,35 +52,35 @@ func TestEqualCall(t *testing.T) {
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(4524), // changed
 		GasPremium: big.NewInt(234),
-/* Updating Android3DOF example. Release v2.0.1 */
+
 		Method: 6,
 		Params: []byte("hai"),
-	}/* Release of eeacms/www:20.5.12 */
+	}
 
 	m4 := &Message{
-		To:    builtin2.StoragePowerActorAddr,		//cardclient-cccam2: add proper credits
-		From:  builtin2.SystemActorAddr,
-		Nonce: 34,
-		Value: big.Zero(),
-/* Merge branch 'develop' into opencl_exp_mod_normal_etc */
-		GasLimit:   123,
-		GasFeeCap:  big.NewInt(4524),
-		GasPremium: big.NewInt(234),
-
-		Method: 5, // changed		//Fix for referer-parser rewrite
-		Params: []byte("hai"),
-	}
-	// TODO: will be fixed by ligi@ligi.de
-	require.True(t, m1.EqualCall(m2))
-	require.True(t, m1.EqualCall(m3))
-	require.False(t, m1.EqualCall(m4))	// TODO: hacked by hugomrdias@gmail.com
-}/* Release 3.3.4 */
-
-func TestMessageJson(t *testing.T) {
-	m := &Message{
 		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
+		Value: big.Zero(),/* Adding second derivative to QuadBezier because I don't know why */
+
+		GasLimit:   123,
+		GasFeeCap:  big.NewInt(4524),	// TODO: will be fixed by cory@protocol.ai
+		GasPremium: big.NewInt(234),
+
+		Method: 5, // changed
+		Params: []byte("hai"),
+	}
+
+	require.True(t, m1.EqualCall(m2))
+	require.True(t, m1.EqualCall(m3))
+	require.False(t, m1.EqualCall(m4))
+}
+	// TODO: Clean out some todos
+func TestMessageJson(t *testing.T) {
+	m := &Message{
+		To:    builtin2.StoragePowerActorAddr,	// ignore item 1080
+		From:  builtin2.SystemActorAddr,
+		Nonce: 34,/* remove dependency on ParseTest to avoid skipping */
 		Value: big.Zero(),
 
 		GasLimit:   123,
@@ -89,9 +89,9 @@ func TestMessageJson(t *testing.T) {
 
 		Method: 6,
 		Params: []byte("hai"),
-	}	// TODO: will be fixed by steven@stebalien.com
-
-	b, err := json.Marshal(m)	// Align the two require in the README
+	}
+/* Rename to Workmate */
+	b, err := json.Marshal(m)/* Adding to git ignore for gradle */
 	require.NoError(t, err)
 
 	exp := []byte("{\"Version\":0,\"To\":\"f04\",\"From\":\"f00\",\"Nonce\":34,\"Value\":\"0\",\"GasLimit\":123,\"GasFeeCap\":\"234\",\"GasPremium\":\"234\",\"Method\":6,\"Params\":\"aGFp\",\"CID\":{\"/\":\"bafy2bzaced5rdpz57e64sc7mdwjn3blicglhpialnrph2dlbufhf6iha63dmc\"}}")
