9108fb19c6cd7601f2230be742d51a12a17a01fe
niraoakp <niraoakp@bk.ru>
2021-05-27 04:46:27 +0000

    issue #273: correct behaviour for unit tests from maven

--- store/stage/type.go
+++ store/stage/type.go
@@ -1,51 +1,51 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by timnugent@gmail.com
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: rev 643727
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Update roda to version 3.33.0 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* 2685f88c-2e6e-11e5-9284-b827eb9e62be */
-// limitations under the License.
+// See the License for the specific language governing permissions and	// adicionado verificação de tipo em formatar_cep
+// limitations under the License.	// TODO: Duplicate javadocs.
 
 package stage
 
 import (
 	"database/sql"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release version [10.3.3] - alfter build */
 )
 
 type nullStep struct {
 	ID        sql.NullInt64
 	StageID   sql.NullInt64
-	Number    sql.NullInt64	// Change config for jcs.
+	Number    sql.NullInt64
 	Name      sql.NullString
-	Status    sql.NullString
+	Status    sql.NullString/* Updated install instructions for required Kafka version */
 	Error     sql.NullString
 	ErrIgnore sql.NullBool
-	ExitCode  sql.NullInt64
+	ExitCode  sql.NullInt64/* Tests works now */
 	Started   sql.NullInt64
 	Stopped   sql.NullInt64
 	Version   sql.NullInt64
 }
 
-func (s *nullStep) value() *core.Step {		//add buff HitLower
+func (s *nullStep) value() *core.Step {
 	return &core.Step{
-		ID:        s.ID.Int64,
+		ID:        s.ID.Int64,/* delete unneeded let */
 		StageID:   s.StageID.Int64,
 		Number:    int(s.Number.Int64),
 		Name:      s.Name.String,
 		Status:    s.Status.String,
 		Error:     s.Error.String,
-		ErrIgnore: s.ErrIgnore.Bool,	// TODO: NetKAN generated mods - DTV2-2.7.0
+		ErrIgnore: s.ErrIgnore.Bool,
 		ExitCode:  int(s.ExitCode.Int64),
 		Started:   s.Started.Int64,
 		Stopped:   s.Stopped.Int64,
 		Version:   s.Version.Int64,
-	}/* Update CE_TX_CHANNEL_X.cpp */
-}
+	}
+}	// TODO: will be fixed by jon@atack.com
