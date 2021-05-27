fb5c91dbb677ac00cfbfefc61494b084c00ed0b8
niraoakp <niraoakp@bk.ru>
2021-05-27 08:04:58 +0000

    print() function and raw_input() for Python 3

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// Try Python CGI
 import { Resource } from "./resource";
 
-// Allocate a new resource. When this exists, we should not allow	// TODO: Fixed NullPointerExceptions when file not found.
-// the stack holding it to be `rm`'d without `--force`.
-let a = new Resource("res", { state: 1 });
+// Allocate a new resource. When this exists, we should not allow
+// the stack holding it to be `rm`'d without `--force`.	// Lint happy
+let a = new Resource("res", { state: 1 });/* loan changes 2.56am(s) */
 
 export let o = a.state;
