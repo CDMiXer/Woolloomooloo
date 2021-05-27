cfad2feb773cfda0f5b8af37e3e72af7471e8adc
niraoakp <niraoakp@bk.ru>
2021-05-27 13:27:32 +0000

    Corrected initial values.

--- tests/examples/dynamic-provider/multiple-turns-2/index.ts
+++ tests/examples/dynamic-provider/multiple-turns-2/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Additions plus update setup for Service programs
+		//Merge branch 'master' into NNZ_recorderswap
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-
+/* hd44780_pinIO examples updated to support lcdkeypad on espduino32  */
 const sleep = require("sleep-promise");
 const assert = require("assert");
 
-class NullProvider implements dynamic.ResourceProvider {
+class NullProvider implements dynamic.ResourceProvider {/* Merge "Release note for scheduler rework" */
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
