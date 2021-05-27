703a01dd55c531996f24c00e81d2b1ac9b57d379
niraoakp <niraoakp@bk.ru>
2021-05-27 22:43:37 +0000

    Способ убрать выделение в списке участников конференции, также как в ростере

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
-/* Adding build status badge to README */
+
 for m in $*; do
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)/* Proper installer command in README.md */
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
