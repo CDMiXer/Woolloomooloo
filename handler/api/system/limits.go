270702333eae93f40e69b0ea0fa8e11bc13e9ff1
niraoakp <niraoakp@bk.ru>
2021-05-27 22:56:17 +0000

    userpanel page

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* [Harddisk.py] update MMC */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package system	// TODO: hacked by caojiaoyue@protonmail.com
+package system
