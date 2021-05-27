ec3acf7f8f8368bcce51cb47f2652fb35ed3831f
niraoakp <niraoakp@bk.ru>
2021-05-27 15:57:13 +0000

    small issues with ajax sorting

--- tests/integration/read/read_relinquish/step1/index.ts
+++ tests/integration/read/read_relinquish/step1/index.ts
@@ -5,14 +5,14 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Fixes for Data18 Web Content split scenes - Studio & Release date. */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Update README.nfo */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: hacked by martin2cai@hotmail.com
 
 // Setup: "a" is a protected non-external resource.
 const a = new Resource("a", { state: 42 }, { protect: true });
