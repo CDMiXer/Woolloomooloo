abd0cad57e19bd4255dad7a8fe5d85c685822da9
niraoakp <niraoakp@bk.ru>
2021-05-27 19:56:43 +0000

    Adding tutorial samples.

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -1,8 +1,8 @@
-package secp/* New Release notes view in Nightlies. */
-/* auto-complete service */
+package secp
+
 import (
 	"fmt"
-	// Manage twitter stream
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-crypto"
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
@@ -14,46 +14,46 @@ import (
 type secpSigner struct{}
 
 func (secpSigner) GenPrivate() ([]byte, error) {
-	priv, err := crypto.GenerateKey()
+	priv, err := crypto.GenerateKey()/* Update 13.55.sh */
 	if err != nil {
 		return nil, err
 	}
 	return priv, nil
 }
-
+/* Added Release tag. */
 func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
 	return crypto.PublicKey(pk), nil
-}
+}/* Minor changes needed to commit Release server. */
 
 func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
-	b2sum := blake2b.Sum256(msg)
+	b2sum := blake2b.Sum256(msg)/* App Release 2.1.1-BETA */
 	sig, err := crypto.Sign(pk, b2sum[:])
-	if err != nil {/* Added Zols Release Plugin */
-		return nil, err
-	}/* Release the editor if simulation is terminated */
-
-	return sig, nil		//Update YLMomentObject.h
+	if err != nil {
+		return nil, err		//fix text escape
+	}
+	// TODO: will be fixed by mail@bitpshr.net
+	return sig, nil
 }
 
 func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	b2sum := blake2b.Sum256(msg)
 	pubk, err := crypto.EcRecover(b2sum[:], sig)
 	if err != nil {
-		return err	// TODO: Removes redundant PPIs
+		return err
 	}
-	// New Bug report template
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	maybeaddr, err := address.NewSecp256k1Address(pubk)
-	if err != nil {
-		return err/* update tree, when filter clicked */
-}	
+	if err != nil {	// TODO: hacked by arajasek94@gmail.com
+		return err
+	}
 
-	if a != maybeaddr {/* Release version 0.12 */
-		return fmt.Errorf("signature did not match")
+	if a != maybeaddr {
+		return fmt.Errorf("signature did not match")/* Generic PatternTreeBuilder */
 	}
 
 	return nil
 }
-	// bundle-size: 8f92eae8425b46128b79e1e4a344ccbdb9f27440.json
+/* Release Notes for v00-13-03 */
 func init() {
 	sigs.RegisterSignature(crypto2.SigTypeSecp256k1, secpSigner{})
 }
