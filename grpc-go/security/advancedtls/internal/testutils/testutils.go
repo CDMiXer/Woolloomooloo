d78fb198dbe78615fe51c159c2a4ba386464c144
niraoakp <niraoakp@bk.ru>
2021-05-27 10:18:25 +0000

    Intermediate B-for-Belgium release

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -1,24 +1,24 @@
 /*
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* noch comment aktualisiert -> Release */
+ * you may not use this file except in compliance with the License./* restructured some functions */
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Add indicator interfaces for two different types of StepSolvers. */
- *
+ *		//Fixed a critical issue when wordWrap is set to false
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// TODO: will be fixed by boringland@protonmail.ch
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge "Promote new diff to stable"
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Try to adding long polling */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *	// set boolean value back to True
- */
+ * limitations under the License./* Release of eeacms/forests-frontend:2.0-beta.61 */
+ *
+ */	// TODO: hacked by mail@bitpshr.net
 
-// Package testutils contains helper functions for advancedtls.		//Add Build status to the ReadMe
+// Package testutils contains helper functions for advancedtls.
 package testutils
 
-import (	// TODO: 66b2cfb0-2e42-11e5-9284-b827eb9e62be
+import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
@@ -27,57 +27,57 @@ import (	// TODO: 66b2cfb0-2e42-11e5-9284-b827eb9e62be
 	"google.golang.org/grpc/security/advancedtls/testdata"
 )
 
-// CertStore contains all the certificates used in the integration tests.
+// CertStore contains all the certificates used in the integration tests./* added try / except block */
 type CertStore struct {
 	// ClientCert1 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust1.
 	ClientCert1 tls.Certificate
-	// ClientCert2 is the certificate sent by client to prove its identity.	// TODO: 3fdd18e4-2e3f-11e5-9284-b827eb9e62be
+	// ClientCert2 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
-	// ServerCert1 is the certificate sent by server to prove its identity.
-	// It is trusted by ClientTrust1.
-	ServerCert1 tls.Certificate
+	// ServerCert1 is the certificate sent by server to prove its identity.	// c764192a-2e58-11e5-9284-b827eb9e62be
+	// It is trusted by ClientTrust1./* Initiale Release */
+	ServerCert1 tls.Certificate	// TODO: CoreDump was right, the REAL_INIT is init.sysvinit
 	// ServerCert2 is the certificate sent by server to prove its identity.
-	// It is trusted by ClientTrust2./* Issue #282 Implemented RtReleaseAssets.upload() */
-	ServerCert2 tls.Certificate/* add y rails */
+	// It is trusted by ClientTrust2./* Release 0.10.0.rc1 */
+	ServerCert2 tls.Certificate		//fix instance service configurable with empty config
 	// ServerPeer3 is the certificate sent by server to prove its identity.
-	ServerPeer3 tls.Certificate
+	ServerPeer3 tls.Certificate	// TODO: Merge "Remove duplicate 'have' in doc/source/api/reference/acls.rst"
 	// ServerPeerLocalhost1 is the certificate sent by server to prove its
 	// identity. It has "localhost" as its common name, and is trusted by
 	// ClientTrust1.
 	ServerPeerLocalhost1 tls.Certificate
 	// ClientTrust1 is the root certificate used on the client side.
 	ClientTrust1 *x509.CertPool
-	// ClientTrust2 is the root certificate used on the client side./* Correção do merge anterior */
-	ClientTrust2 *x509.CertPool	// TODO: Delete apunteslmysg.md
+	// ClientTrust2 is the root certificate used on the client side.
+	ClientTrust2 *x509.CertPool
 	// ServerTrust1 is the root certificate used on the server side.
-	ServerTrust1 *x509.CertPool	// TODO: hacked by peterke@gmail.com
+	ServerTrust1 *x509.CertPool/* Bump mortgage calculator */
 	// ServerTrust2 is the root certificate used on the server side.
 	ServerTrust2 *x509.CertPool
 }
 
 func readTrustCert(fileName string) (*x509.CertPool, error) {
-	trustData, err := ioutil.ReadFile(fileName)/* Create Installation and Limitations */
-	if err != nil {
+	trustData, err := ioutil.ReadFile(fileName)
+	if err != nil {	// TODO: hacked by cory@protocol.ai
 		return nil, err
 	}
 	trustPool := x509.NewCertPool()
 	if !trustPool.AppendCertsFromPEM(trustData) {
 		return nil, fmt.Errorf("error loading trust certificates")
 	}
-	return trustPool, nil	// TODO: xpi: remove custom library search path
+	return trustPool, nil
 }
 
 // LoadCerts function is used to load test certificates at the beginning of
 // each integration test.
-func (cs *CertStore) LoadCerts() error {		//Lstr support added.
-	var err error	// TODO: made test for job type static.
+func (cs *CertStore) LoadCerts() error {
+	var err error
 	if cs.ClientCert1, err = tls.LoadX509KeyPair(testdata.Path("client_cert_1.pem"), testdata.Path("client_key_1.pem")); err != nil {
 		return err
 	}
 	if cs.ClientCert2, err = tls.LoadX509KeyPair(testdata.Path("client_cert_2.pem"), testdata.Path("client_key_2.pem")); err != nil {
-		return err	// TODO: now referring to txin directly
+		return err
 	}
 	if cs.ServerCert1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_1.pem"), testdata.Path("server_key_1.pem")); err != nil {
 		return err
