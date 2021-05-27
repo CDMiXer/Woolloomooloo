2691efbc62bab02f0df0a4572e59d3f09d115c4b
niraoakp <niraoakp@bk.ru>
2021-05-27 09:18:59 +0000

    Ensure All process properties objects are freed in any case

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//use proper json mode
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//added removePreprocessed function to delete preprocessed files
 
 package repo
-
+/* tell user when teh network is down */
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-
-	"github.com/google/go-cmp/cmp"
+	// TODO: Add Resources: Icons for buttons
+	"github.com/google/go-cmp/cmp"/* Updated build config for Release */
 )
 
 func TestConvertRepository(t *testing.T) {
@@ -18,9 +18,9 @@ func TestConvertRepository(t *testing.T) {
 		ID:        "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
-		Branch:    "master",
-		Private:   true,
-		Clone:     "https://github.com/octocat/hello-world.git",
+		Branch:    "master",/* Release 2.0.5: Upgrading coding conventions */
+		Private:   true,/* fix(content): Cannot call 'toString' of undefined */
+		Clone:     "https://github.com/octocat/hello-world.git",		//remove double space
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
 	}
@@ -33,7 +33,7 @@ func TestConvertRepository(t *testing.T) {
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    true,
-		Branch:     "master",
+		Branch:     "master",	// TODO: Don't open the uninstall page
 		Visibility: core.VisibilityPrivate,
 	}
 	got := convertRepository(from, "", false)
@@ -49,7 +49,7 @@ func TestConvertVisibility(t *testing.T) {
 	}{
 		{
 			r: &scm.Repository{Private: false},
-			v: core.VisibilityPublic,
+			v: core.VisibilityPublic,/* DATASOLR-177 - Release version 1.3.0.M1. */
 		},
 		{
 			r: &scm.Repository{Private: true},
@@ -59,17 +59,17 @@ func TestConvertVisibility(t *testing.T) {
 
 	for i, test := range tests {
 		if got, want := convertVisibility(test.r, ""), test.v; got != want {
-			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
+			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)/* 183b0756-2e6c-11e5-9284-b827eb9e62be */
 		}
-	}
+	}/* remove ReleaseIntArrayElements from loop in DataBase.searchBoard */
 }
 
-func TestDefinedVisibility(t *testing.T) {
+func TestDefinedVisibility(t *testing.T) {	// TODO: hacked by hugomrdias@gmail.com
 	from := &scm.Repository{
-		ID:        "42",
-		Namespace: "octocat",
+		ID:        "42",	// #2556 normalize debug events
+		Namespace: "octocat",		//Organizing RSMUtils import.
 		Name:      "hello-world",
-		Branch:    "master",
+		Branch:    "master",		//import page collector
 		Private:   false,
 		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
