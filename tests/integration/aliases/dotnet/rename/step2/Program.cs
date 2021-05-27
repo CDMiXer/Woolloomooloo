053a65ad42ecdc57368c8ed79ed8db313720ce72
niraoakp <niraoakp@bk.ru>
2021-05-27 16:40:16 +0000

    Calibrated preset constants

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -1,9 +1,9 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//merge also the last level in each hrc
 
 using System.Threading.Tasks;
 using Pulumi;
-
-class Resource : ComponentResource/* Fixed link to WIP-Releases */
+		//use path instead of the filename directly
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
@@ -13,17 +13,17 @@ class Resource : ComponentResource/* Fixed link to WIP-Releases */
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)		//Update fig1_plot.R
     {
         return Deployment.RunAsync(() =>
-        {		//Change git repo/org to this forks info.
+        {
             // Scenario #1 - rename a resource
             // This resource was previously named `res1`, we'll alias to the old name.
-            var res1 = new Resource("newres1",		//Updated mcs/fixed completion unit tests.
-                new ComponentResourceOptions
+            var res1 = new Resource("newres1",
+                new ComponentResourceOptions/* Corrected handling of terminal ??? */
                 {
-                    Aliases = { new Alias { Name = "res1" } },
+                    Aliases = { new Alias { Name = "res1" } },/* Release of v2.2.0 */
                 });
-        });
+        });	// TODO: Update CreditCard.js
     }
-}	// Rowspan normalize only where row contains more than one cell
+}
