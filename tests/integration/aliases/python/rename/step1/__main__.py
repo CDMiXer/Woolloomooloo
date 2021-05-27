fe73e959f775498a8dc1c54c69c5fc6f60fdc59e
niraoakp <niraoakp@bk.ru>
2021-05-27 18:54:56 +0000

    IMGAPI-58: Bugfix

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release version 3.2.0 build 5140 */
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-	// cambios sobre api
-class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+/* 9e37b9a0-2e74-11e5-9284-b827eb9e62be */
+class Resource1(ComponentResource):/* Merge "all: Windows fixes (don't listen on file descriptors in test.World, etc)" */
+    def __init__(self, name, opts=None):/* Release "1.1-SNAPSHOT" */
         super().__init__("my:module:Resource", name, None, opts)
-
+/* rebar magick in app */
 # Scenario #1 - rename a resource
 res1 = Resource1("res1")
