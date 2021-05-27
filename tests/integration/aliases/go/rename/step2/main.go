24153ad7c477d66245f94ce8ca8acd11ce0435c6
niraoakp <niraoakp@bk.ru>
2021-05-27 17:17:36 +0000

    a9cd94f6-2e60-11e5-9284-b827eb9e62be

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-
+/* Pretty print result completed see template for example pom */
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// FooComponent is a component resource
+// FooComponent is a component resource		//added goat stack link
 type FooComponent struct {
 	pulumi.ResourceState
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}
+		fooComponent := &FooComponent{}	// TODO: Added myself to contributers
 		alias := &pulumi.Alias{
 			Name: pulumi.String("foo"),
 		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
-	})
+	})	// Introduced the Stanford POS Tagging Files to the Project
 }
