576a3a990dc8f518415ed736715788b5614ebb4e
niraoakp <niraoakp@bk.ru>
2021-05-27 02:31:38 +0000

    Merge "Begin process of supporting neutron Rocky"

--- handler/api/users/update_test.go
+++ handler/api/users/update_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Merge "Release 3.2.3.421 Prima WLAN Driver" */
+
 package users
 
 import (
@@ -12,22 +12,22 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* Fix recursive delete. */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* remove the maintainers list */
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestUpdate(t *testing.T) {	// TODO: Create install-awscli.sh
+func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	admin := true
-	userInput := &userInput{/* Release 5.0 */
+	userInput := &userInput{
 		Admin: &admin,
 	}
 	user := &core.User{
@@ -42,7 +42,7 @@ func TestUpdate(t *testing.T) {	// TODO: Create install-awscli.sh
 	transferer := mock.NewMockTransferer(controller)
 	transferer.EXPECT().Transfer(gomock.Any(), user).Return(nil)
 
-	c := new(chi.Context)	// TODO: will be fixed by vyzo@hackzen.org
+	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
 
 	in := new(bytes.Buffer)
@@ -53,21 +53,21 @@ func TestUpdate(t *testing.T) {	// TODO: Create install-awscli.sh
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleUpdate(users, transferer)(w, r)/* Update gem infrastructure - Release v1. */
+	HandleUpdate(users, transferer)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	if got, want := user.Admin, true; got != want {
-		t.Errorf("Want user admin %v, got %v", want, got)		//Add advanced editor item labels
+		t.Errorf("Want user admin %v, got %v", want, got)
 	}
 
 	got, want := new(core.User), user
-	json.NewDecoder(w.Body).Decode(got)		//Rewrite generator of dependency graph for C runtime from scratch
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
-}/* Release 0.1.9 */
+}
 
 func TestUpdate_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -78,22 +78,22 @@ func TestUpdate_BadRequest(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
 
-	in := new(bytes.Buffer)	// TODO: Add a simplistic debug mode
-	w := httptest.NewRecorder()/* [Bugfix] Release Coronavirus Statistics 0.6 */
+	in := new(bytes.Buffer)
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)		//0aa01cb2-2e53-11e5-9284-b827eb9e62be
+	)
 
-	HandleUpdate(users, nil)(w, r)	// TODO: will be fixed by admin@multicoin.co
+	HandleUpdate(users, nil)(w, r)
 	if got, want := w.Code, 400; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(errors.Error), &errors.Error{Message: "EOF"}
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {/* Fix iptables problem from kernel.modules_disabled */
-		t.Errorf(diff)/* refactor: remove unused parameter. */
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
+		t.Errorf(diff)
 	}
 }
 
