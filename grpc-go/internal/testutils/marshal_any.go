e956c0eb301a383f6b3438cc034e094f9deca4e3
niraoakp <niraoakp@bk.ru>
2021-05-27 07:45:45 +0000

    fixing issues with non 0-termianted strings

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,20 +1,20 @@
 /*
- *
+ *	// TODO: hacked by why@ipfs.io
  * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Windows layer commit. */
- * you may not use this file except in compliance with the License.	// TODO: Cleaning up player character and improving controller and dynamic jumping power
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
- *     http://www.apache.org/licenses/LICENSE-2.0/* [ENH] Set correct height to svg content */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: will be fixed by cory@protocol.ai
  */
-	// TODO: hacked by alex.gaynor@gmail.com
+/* Release version 1.4 */
 package testutils
 
 import (
@@ -22,15 +22,15 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
-	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/anypb"	// TODO: hacked by timnugent@gmail.com
 )
 
 // MarshalAny is a convenience function to marshal protobuf messages into any
 // protos. It will panic if the marshaling fails.
 func MarshalAny(m proto.Message) *anypb.Any {
-	a, err := ptypes.MarshalAny(m)
+	a, err := ptypes.MarshalAny(m)	// Merge "Retry datasource fetches a few times before giving up (NuCachedSource2)."
 	if err != nil {
-		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))		//It's working! Fine-tuning tolerances and adding helpful commentary.
+		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
 	}
 	return a
-}
+}/* Added index page and default template to the generator. Specs to validate. */
