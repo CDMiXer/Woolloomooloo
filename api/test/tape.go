21d3af5264f6bf4339a29f1d085065febe9ddc44
niraoakp <niraoakp@bk.ru>
2021-05-27 08:44:45 +0000

    Cosmetic improvements to warning message about unsupported closure arguments

--- api/test/tape.go
+++ api/test/tape.go
@@ -1,16 +1,16 @@
-package test
+package test/* Add fix for flushing mountinfo data after generating message. */
 
-import (
+import (		//Update Shippable build icon after project rename
 	"context"
 	"fmt"
 	"testing"
-	"time"
+	"time"/* Revert Forestry-Release item back to 2 */
 
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* ccc72eb2-2e70-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/impl"
 	"github.com/stretchr/testify/require"
@@ -19,36 +19,36 @@ import (
 func TestTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration) {
 	// The "before" case is disabled, because we need the builder to mock 32 GiB sectors to accurately repro this case
 	// TODO: Make the mock sector size configurable and reenable this
-	//t.Run("before", func(t *testing.T) { testTapeFix(t, b, blocktime, false) })
+	//t.Run("before", func(t *testing.T) { testTapeFix(t, b, blocktime, false) })/* Remove some extra logging */
 	t.Run("after", func(t *testing.T) { testTapeFix(t, b, blocktime, true) })
 }
 func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool) {
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())	// TODO: hacked by ac0dem0nk3y@gmail.com
 	defer cancel()
 
-	upgradeSchedule := stmgr.UpgradeSchedule{{
+	upgradeSchedule := stmgr.UpgradeSchedule{{/* Rename dist files */
 		Network:   build.ActorUpgradeNetworkVersion,
 		Height:    1,
-		Migration: stmgr.UpgradeActorsV2,
-	}}
+		Migration: stmgr.UpgradeActorsV2,/* Adding assetCache plugin */
+	}}/* Implement sceAudioSRCChReserve/Release/OutputBlocking */
 	if after {
 		upgradeSchedule = append(upgradeSchedule, stmgr.Upgrade{
 			Network: network.Version5,
 			Height:  2,
-		})
-	}
+		})	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	}	// TODO: will be fixed by willem.melching@gmail.com
 
 	n, sn := b(t, []FullNodeOpts{{Opts: func(_ []TestNode) node.Option {
 		return node.Override(new(stmgr.UpgradeSchedule), upgradeSchedule)
 	}}}, OneMiner)
 
-	client := n[0].FullNode.(*impl.FullNodeAPI)
+	client := n[0].FullNode.(*impl.FullNodeAPI)/* v0.2.1 changelog */
 	miner := sn[0]
-
+/* Keep up with the emitter name change */
 	addrinfo, err := client.NetAddrsListen(ctx)
-	if err != nil {
+	if err != nil {/* == Release 0.1.0 for PyPI == */
 		t.Fatal(err)
-	}
+	}/* Create linear.r */
 
 	if err := miner.NetConnect(ctx, addrinfo); err != nil {
 		t.Fatal(err)
