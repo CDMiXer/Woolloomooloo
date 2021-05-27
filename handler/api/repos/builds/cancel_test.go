2251410e17e6b5926c5d2775c01f317be3413be7
niraoakp <niraoakp@bk.ru>
2021-05-27 07:10:51 +0000

    bump version number after 0.2 release

--- handler/api/repos/builds/cancel_test.go
+++ handler/api/repos/builds/cancel_test.go
@@ -1,55 +1,55 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* ZAPI-25: More docs */
-// Use of this source code is governed by the Drone Non-Commercial License		//Test - Move indexOf()
-// that can be found in the LICENSE file.	// TODO: Cache tags added.
-		//Removed names
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: add BitSchema example
+/* Delete NeP-ToolBox_Release.zip */
 package builds
-
+/* Release jedipus-2.5.20 */
 import (
 	"context"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"	// TODO: hacked by fjl@ethereum.org
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Released RubyMass v0.1.3 */
+	"github.com/golang/mock/gomock"
 )
 
 func TestCancel(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)	// TODO: 33223fa0-2e47-11e5-9284-b827eb9e62be
+	defer controller.Finish()/* Release of eeacms/www-devel:18.6.13 */
 
 	mockStages := []*core.Stage{
-		{Status: core.StatusPassing},/* 3eeda21e-2e3f-11e5-9284-b827eb9e62be */
+		{Status: core.StatusPassing},
 		{
-			Status: core.StatusPending,		//Rename IDewRESTClient.cs to Interfaces.cs
+			Status: core.StatusPending,/* Fix typo starnontgal -> starnotgal */
 			Steps: []*core.Step{
-				{Status: core.StatusPassing},
+				{Status: core.StatusPassing},/* Release dicom-mr-classifier v1.4.0 */
 				{Status: core.StatusPending},
 			},
-		},	// TODO: Se agregan datos de prueba
-	}
+		},
+	}/* Release of eeacms/plonesaas:5.2.1-19 */
 
-	mockBuildCopy := new(core.Build)
+	mockBuildCopy := new(core.Build)/* Removed unnecessary leading slashes in httpbin's endpoints */
 	*mockBuildCopy = *mockBuild
-	// TODO: indicate to compiler that 'char c' is unused
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: Maven artifacts for Knowledge Representation Factory version 1.1.6
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)/* Move filter-, plot- and merge- tools to new R */
+
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuildCopy, nil)
-	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)
+	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)		//0a78785c-2e4d-11e5-9284-b827eb9e62be
 
-	users := mock.NewMockUserStore(controller)		//ADSserver: removed more not used stuff
-	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
-/* Forgot NDEBUG in the Release config. */
+	users := mock.NewMockUserStore(controller)	// TODO: Ya esta en .md
+	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)		//dba33f: memeber ini
+
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)
 
-	steps := mock.NewMockStepStore(controller)
-	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)		//1. fix xls filename in batches
+	steps := mock.NewMockStepStore(controller)	// TODO: Update classes-and-instances.md
+	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)
 
 	statusService := mock.NewMockStatusService(controller)
 	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
@@ -58,8 +58,8 @@ func TestCancel(t *testing.T) {
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	scheduler := mock.NewMockScheduler(controller)
-	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)/* Added Configuration=Release to build step. */
-/* Merge tag 'v2.11.1' into upstream_merge_v2.11.1 */
+	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
