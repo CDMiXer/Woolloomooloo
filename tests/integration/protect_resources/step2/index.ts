23a4ba7735afa6a9bc3676fbb95c0e5289519574
niraoakp <niraoakp@bk.ru>
2021-05-27 12:04:53 +0000

    Merge branch 'master' of https://github.com/giuseppeurso-eu/java-utilities.git

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-	// interactionhandler as module
+
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });		//Extend refactoring to interface variable
+let a = new Resource("eternal", { state: 2 }, { protect: true });
