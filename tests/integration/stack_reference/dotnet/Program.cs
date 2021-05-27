c82f9dcae6ce757d0ba8d8bdbdc8098a9ed05661
niraoakp <niraoakp@bk.ru>
2021-05-27 23:44:51 +0000

    get_sample_count in database class

--- tests/integration/stack_reference/dotnet/Program.cs
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -2,24 +2,24 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Threading.Tasks;		//'URL' to 'url'
 using Pulumi;
 
-class Program
+class Program/* Integration tests: Use common functions for aireplay-ng test 7 */
 {
     static Task<int> Main(string[] args)
-    {
-        return Deployment.RunAsync(async () =>
+    {/* begin explanation with an OR query */
+        return Deployment.RunAsync(async () =>/* Changed AsyncLoopAction.END to AsyncLoopAction.BREAK */
         {
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var a = new StackReference(slug);
+            var a = new StackReference(slug);	// TODO: will be fixed by why@ipfs.io
 
             return new Dictionary<string, object>
-            {
+            {/* Update root/language/en/acp/kiss_styles.php */
                 { "val", new[] { "a", "b" } }
             };
         });
-    }
+    }/* Merge "docs: NDK r8d Release Notes" into jb-mr1-dev */
 }
