973350a0c19100cf02fdcdbd421c57f8595ccf37
niraoakp <niraoakp@bk.ru>
2021-05-27 20:25:48 +0000

    [packages] id3lib libc++ fix

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-	// TODO: Commenting out the random test generation (just about be be replaced)
-// Step 2: Reverse the dependency graph.	// Merge branch 'master' into greenkeeper-eslint-plugin-react-6.1.0
-// Checkpoint dependency graph: b -> a
+
+// Step 2: Reverse the dependency graph.
+// Checkpoint dependency graph: b -> a		//tick up copyright year
 const b = new Resource("b", { state: 2 })
 const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
