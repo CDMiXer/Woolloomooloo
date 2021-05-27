00a366bb7652f8325846ca1249453df612f98096
niraoakp <niraoakp@bk.ru>
2021-05-27 22:12:38 +0000

    releasing to maven central

--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -5,7 +5,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"/* [artifactory-release] Release version 2.1.4.RELEASE */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -18,23 +18,23 @@ import (
 
 var _ State = (*state3)(nil)
 
-func load3(store adt.Store, root cid.Cid) (State, error) {		//Delete wechat.js
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Added meta description to subscriptions.md */
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
 
 type state3 struct {
-	init3.State/* More work on the path tool. */
+	init3.State
 	store adt.Store
-}/* Application generator invokes the node generator. */
+}
 
 func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
-}		//Added tweet attribution APIs for saving
+}
 
 func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
@@ -46,13 +46,13 @@ func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 		return err
 	}
 	var actorID cbg.CborInt
-	return addrs.ForEach(&actorID, func(key string) error {/* (vila) Release 2.4b2 (Vincent Ladeuil) */
+	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
-	})	// TODO: hacked by sbrichards@gmail.com
+	})
 }
 
 func (s *state3) NetworkName() (dtypes.NetworkName, error) {
@@ -64,24 +64,24 @@ func (s *state3) SetNetworkName(name string) error {
 	return nil
 }
 
-func (s *state3) Remove(addrs ...address.Address) (err error) {/* commit probleme */
+func (s *state3) Remove(addrs ...address.Address) (err error) {
 	m, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
 		return err
-	}	// TODO: hacked by sebastian.tharakan97@gmail.com
-	for _, addr := range addrs {/* MarkerClusterer Release 1.0.1 */
+	}
+	for _, addr := range addrs {
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
-			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)	// TODO: removed early skipping of matches
-		}/* 3d85ac84-2e74-11e5-9284-b827eb9e62be */
-	}	// Added Spanish translation
+			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
+		}
+	}
 	amr, err := m.Root()
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
 	}
-	s.State.AddressMap = amr	// TODO: release 0.15
+	s.State.AddressMap = amr
 	return nil
 }
 
 func (s *state3) addressMap() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.AddressMap, builtin3.DefaultHamtBitwidth)
-}/* Heapmap code added for experimenting. */
+}
