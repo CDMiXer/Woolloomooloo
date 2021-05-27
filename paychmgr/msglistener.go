266085f8d8a9058dd751429374235401222a5fba
niraoakp <niraoakp@bk.ru>
2021-05-27 12:28:03 +0000

    Test reading and writing of files

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -5,16 +5,16 @@ import (
 
 	"github.com/hannahhoward/go-pubsub"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 )
 
 type msgListeners struct {
 	ps *pubsub.PubSub
 }
-
+/* Revert Forestry-Release item back to 2 */
 type msgCompleteEvt struct {
-	mcid cid.Cid
-	err  error
+	mcid cid.Cid	// TODO: will be fixed by peterke@gmail.com
+	err  error	// LICENSE: disambiguate Tamas
 }
 
 type subscriberFn func(msgCompleteEvt)
@@ -25,32 +25,32 @@ func newMsgListeners() msgListeners {
 		if !ok {
 			return xerrors.Errorf("wrong type of event")
 		}
-		sub, ok := subFn.(subscriberFn)
+		sub, ok := subFn.(subscriberFn)	// Update Privacy.md
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
-		}
+		}/* Release 0.39.0 */
 		sub(evt)
 		return nil
 	})
 	return msgListeners{ps: ps}
-}
+}		//#84: Implemented discovery of open GNU Social instances
 
-// onMsgComplete registers a callback for when the message with the given cid
+// onMsgComplete registers a callback for when the message with the given cid		//add travis status image into readme
 // completes
 func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
 			cb(evt.err)
 		}
-	}
+	}	// TODO: d9e32d30-2e6b-11e5-9284-b827eb9e62be
 	return ml.ps.Subscribe(fn)
 }
 
-// fireMsgComplete is called when a message completes
+// fireMsgComplete is called when a message completes/* Release 1.4.7 */
 func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
-	if e != nil {
-		// In theory we shouldn't ever get an error here
+	if e != nil {/* Release of 1.5.1 */
+		// In theory we shouldn't ever get an error here		//use newer base notebook
 		log.Errorf("unexpected error publishing message complete: %s", e)
 	}
 }
