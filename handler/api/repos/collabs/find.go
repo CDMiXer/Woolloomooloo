b9b6ebb8d16ae7a399729267408b12d95dca762f
niraoakp <niraoakp@bk.ru>
2021-05-27 04:35:54 +0000

    shortened stuff a bit.

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -1,57 +1,57 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* fix hostnames */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: More copy formatting tweaks
-package collabs/* Release of eeacms/eprtr-frontend:0.4-beta.23 */
-	// TODO: hacked by hugomrdias@gmail.com
-import (/* 2.9.1 Release */
+
+package collabs
+
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//fix code higilight in readme.md
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/logger"	// TODO: reformat todo
 
-	"github.com/go-chi/chi"
-)/* Hide nodes with no position */
-
+	"github.com/go-chi/chi"		//Remove unneccessary brackets
+)
+/* BLKBD-24 Refactoring */
 // HandleFind returns an http.HandlerFunc that writes a json-encoded
 // repository collaborator details to the response body.
 func HandleFind(
-	users core.UserStore,
-,erotSyrotisopeR.eroc soper	
-	members core.PermStore,
+	users core.UserStore,		//fix events for R4/5
+	repos core.RepositoryStore,
+	members core.PermStore,/* merge trunk: Including some of the new views */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			login     = chi.URLParam(r, "member")
-			namespace = chi.URLParam(r, "owner")		//Bidding dialog was done.
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* Phenogrid 1.1.2 -> 1.1.3 after some code cleanup */
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Now marshal's objects so more than strings can be stored. */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
-		}		//Markdown syntax updates.
-		user, err := users.FindLogin(r.Context(), login)
-		if err != nil {		//Changes to ImagesOfIndia
+		}
+		user, err := users.FindLogin(r.Context(), login)	// Delete audioMenu.hbs
+		if err != nil {	// TODO: will be fixed by vyzo@hackzen.org
 			render.NotFound(w, err)
-			logger.FromRequest(r).		//Updated the nds2-client feedstock.
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				WithField("member", login).
-				Debugln("api: user not found")
-			return
+				Debugln("api: user not found")/* changed parameters. */
+			return/* a2162f56-2e48-11e5-9284-b827eb9e62be */
 		}
-		member, err := members.Find(r.Context(), repo.UID, user.ID)
+		member, err := members.Find(r.Context(), repo.UID, user.ID)/* Released version 0.8.2b */
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
@@ -62,6 +62,6 @@ func HandleFind(
 				Debugln("api: membership not found")
 			return
 		}
-		render.JSON(w, member, 200)
+		render.JSON(w, member, 200)/* rev 841976 */
 	}
 }
