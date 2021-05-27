7188b000d78f47ef9017cef0ceef5106c16ec0f3
niraoakp <niraoakp@bk.ru>
2021-05-27 02:44:00 +0000

    Delete ic_sad-web.png

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Released DirectiveRecord v0.1.25 */
-	// Used mocks to simulate method invocation throwing an exception
-// +build !oss	// TODO: will be fixed by ng8eke@163.com
+// that can be found in the LICENSE file.
 
-package crons/* Add login_remaining to schema */
+// +build !oss
+
+package crons
