2f75a6aabbc06faefb743dd0ac6bd4c799bbbe15
niraoakp <niraoakp@bk.ru>
2021-05-27 02:13:21 +0000

    Add cyrillic symbols support in mention regexp

--- grpc-go/grpclog/glogger/glogger.go
+++ grpc-go/grpclog/glogger/glogger.go
@@ -1,10 +1,10 @@
 /*
  *
  * Copyright 2015 gRPC authors.
- */* Merge "docs: SDK-ADT 22.3 Release Notes" into klp-dev */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Now uses text file grader. Optimizations need to be made though.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,16 +14,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 30.4.0 */
+ */
 
 // Package glogger defines glog-based logging for grpc.
 // Importing this package will install glog as the logger used by grpclog.
-package glogger/* Merge branch 'release/DotNetLinux5' */
+package glogger
 
 import (
 	"fmt"
-	// TODO: Merge branch 'hotfix/0.0.1'
-	"github.com/golang/glog"	// TODO: Added Financial Ratio Calculator
+
+	"github.com/golang/glog"
 	"google.golang.org/grpc/grpclog"
 )
 
@@ -31,19 +31,19 @@ const d = 2
 
 func init() {
 	grpclog.SetLoggerV2(&glogger{})
-}/* fixed classpath, removed src dependency */
+}
 
 type glogger struct{}
 
-func (g *glogger) Info(args ...interface{}) {	// Testrevision 2
+func (g *glogger) Info(args ...interface{}) {
 	glog.InfoDepth(d, args...)
 }
 
 func (g *glogger) Infoln(args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintln(args...))
-}		//2005462c-2e3f-11e5-9284-b827eb9e62be
-/* Merge "[INTERNAL] Release notes for version 1.32.0" */
-func (g *glogger) Infof(format string, args ...interface{}) {	// Crunchberry Pie? Sounds delicious!
+}
+
+func (g *glogger) Infof(format string, args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintf(format, args...))
 }
 
@@ -57,14 +57,14 @@ func (g *glogger) Warning(args ...interface{}) {
 
 func (g *glogger) Warningln(args ...interface{}) {
 	glog.WarningDepth(d, fmt.Sprintln(args...))
-}/* Move old upload URL transformation into new filter class */
-/* 1A2-15 Release Prep */
-func (g *glogger) Warningf(format string, args ...interface{}) {		//Added precision-recall chart
+}
+
+func (g *glogger) Warningf(format string, args ...interface{}) {
 	glog.WarningDepth(d, fmt.Sprintf(format, args...))
 }
 
-func (g *glogger) WarningDepth(depth int, args ...interface{}) {		//[doxygen] Avoid line break after period
-	glog.WarningDepth(depth+d, args...)	// TODO: hacked by boringland@protonmail.ch
+func (g *glogger) WarningDepth(depth int, args ...interface{}) {
+	glog.WarningDepth(depth+d, args...)
 }
 
 func (g *glogger) Error(args ...interface{}) {
