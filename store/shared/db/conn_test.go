f15c72843407f15048146946919db93375e6ce7d
niraoakp <niraoakp@bk.ru>
2021-05-27 04:56:56 +0000

    Callers/callees context menu shown

--- store/shared/db/conn_test.go
+++ store/shared/db/conn_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
-// that can be found in the LICENSE file./* added auto-dependency generation to makefile */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package db
