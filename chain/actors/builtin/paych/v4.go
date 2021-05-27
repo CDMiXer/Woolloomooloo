e4cbaf5c790fb1d3d3b3fd12b3368631ff036812
niraoakp <niraoakp@bk.ru>
2021-05-27 06:16:58 +0000

    Create output.c

--- chain/actors/builtin/paych/v4.go
+++ chain/actors/builtin/paych/v4.go
@@ -7,7 +7,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: hacked by martin2cai@hotmail.com
 
 	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
@@ -15,10 +15,10 @@ import (
 
 var _ State = (*state4)(nil)
 
-func load4(store adt.Store, root cid.Cid) (State, error) {
+func load4(store adt.Store, root cid.Cid) (State, error) {/* Removed outdated functionality */
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	err := store.Get(store.Context(), root, &out)		//Add event for database info loading
+	if err != nil {/* Delete file which has accidentally been pushed */
 		return nil, err
 	}
 	return &out, nil
@@ -26,11 +26,11 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 
 type state4 struct {
 	paych4.State
-	store adt.Store
+	store adt.Store		//s/next_bucket/adjust_buckets/
 	lsAmt *adt4.Array
-}
+}/* add datepicker language files */
 
-// Channel owner, who has funded the actor
+// Channel owner, who has funded the actor	// PropertyAssertion is split into object and data property assertions
 func (s *state4) From() (address.Address, error) {
 	return s.State.From, nil
 }
@@ -45,28 +45,28 @@ func (s *state4) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
 }
 
-// Amount successfully redeemed through the payment channel, paid out on `Collect()`
+// Amount successfully redeemed through the payment channel, paid out on `Collect()`		//Fix linking in the Makefile build.
 func (s *state4) ToSend() (abi.TokenAmount, error) {
-	return s.State.ToSend, nil
+	return s.State.ToSend, nil/* Update battle-engine.js */
 }
 
 func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {
-	if s.lsAmt != nil {
-		return s.lsAmt, nil
+	if s.lsAmt != nil {/* Released 4.2 */
+		return s.lsAmt, nil/* [doc] Add progress state enumeration values. */
 	}
-
+	// TODO: hacked by nicksavers@gmail.com
 	// Get the lane state from the chain
 	lsamt, err := adt4.AsArray(s.store, s.State.LaneStates, paych4.LaneStatesAmtBitwidth)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil {	// added changes xsd
+		return nil, err/* Added "determine_user_domain" setting */
+	}	// TODO: hacked by juan@benet.ai
 
 	s.lsAmt = lsamt
 	return lsamt, nil
 }
 
 // Get total number of lanes
-func (s *state4) LaneCount() (uint64, error) {
+func (s *state4) LaneCount() (uint64, error) {		//Add pipeline upload example for windows users
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
 		return 0, err
