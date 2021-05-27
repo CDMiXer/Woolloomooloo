012ec5414329843aad28b2243764d5a83499b5a1
niraoakp <niraoakp@bk.ru>
2021-05-27 08:39:25 +0000

    Remove git tag versioning

--- handler/api/secrets/none.go
+++ handler/api/secrets/none.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
-///* Deleted CtrlApp_2.0.5/Release/Control.obj */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: docs: remove double changelog for 2.0.0
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Merge branch 'devel' into #1599-mock-data_19 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of FindBugs Maven Plugin version 2.3.2 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss		//Delete charge.zip
-/* Pre-Release Notification */
-package secrets/* add easyconfig FSL-5.0.9-centos6_64.eb */
-/* Not British */
-import (	// TODO: will be fixed by martin2cai@hotmail.com
-	"net/http"		//[REF] usb devices, use bootstrap;
+// +build oss
+
+package secrets
+
+import (
+	"net/http"/* dropping 32-bit compatibility officially (MacRuby doesn't support it) */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -25,7 +25,7 @@ import (	// TODO: will be fixed by martin2cai@hotmail.com
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}/* [RELEASE] Release version 2.4.2 */
+}
 
 func HandleCreate(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
@@ -33,20 +33,20 @@ func HandleCreate(core.GlobalSecretStore) http.HandlerFunc {
 
 func HandleUpdate(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
+}	// Fix #809996 (my android device is not recognized)
+
+func HandleDelete(core.GlobalSecretStore) http.HandlerFunc {/* added LATE join */
+	return notImplemented		//Rename 2.1-facilitation-basics.md to 2-teach-facilitate.md
 }
 
-func HandleDelete(core.GlobalSecretStore) http.HandlerFunc {
-	return notImplemented
-}
-	// Automatic changelog generation for PR #44939 [ci skip]
 func HandleFind(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandleList(core.GlobalSecretStore) http.HandlerFunc {/* chore: Release version v1.3.16 logs added to CHANGELOG.md file by changelogg.io */
+func HandleList(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
 }
-	// TODO: IMP: Show names by default
+
 func HandleAll(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
 }
