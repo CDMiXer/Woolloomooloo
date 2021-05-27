0730779ee1f6daac5e55b9fda514b20f00ab6f7d
niraoakp <niraoakp@bk.ru>
2021-05-27 10:38:19 +0000

    4ec3cae2-2e6c-11e5-9284-b827eb9e62be

--- service/repo/util.go
+++ service/repo/util.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* updating poms for branch'release/0.0.12' with non-snapshot versions */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update README for 2.1.0.Final Release */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Make sure the Schema's uri is passed through when creating new Schemas. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//6d8a67de-2e52-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Hex editor UI fixes */
 package repo
 
 import (
-	"github.com/drone/drone/core"		//use index rather than line to identify scenarios
-	"github.com/drone/go-scm/scm"/* Fixed images and update view */
+	"github.com/drone/drone/core"
+	"github.com/drone/go-scm/scm"
 )
 
-// convertRepository is a helper function that converts a/* Update Commerce_Center__rlaan.py */
+// convertRepository is a helper function that converts a
 // repository from the source code management system to the
 // local datastructure.
 func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
 	return &core.Repository{
 		UID:        src.ID,
-		Namespace:  src.Namespace,	// TODO: hacked by why@ipfs.io
-		Name:       src.Name,/* Release of eeacms/www:20.11.25 */
+		Namespace:  src.Namespace,
+		Name:       src.Name,
 		Slug:       scm.Join(src.Namespace, src.Name),
-		HTTPURL:    src.Clone,
+		HTTPURL:    src.Clone,		//Upload and update Spike .psd
 		SSHURL:     src.CloneSSH,
 		Link:       src.Link,
 		Private:    src.Private,
@@ -44,9 +44,9 @@ func convertVisibility(src *scm.Repository, visibility string) string {
 	switch {
 	case src.Private == true:
 		return core.VisibilityPrivate
-	case visibility == core.VisibilityInternal:
+	case visibility == core.VisibilityInternal:		//Add info re addTracks
 		return core.VisibilityInternal
 	default:
-		return core.VisibilityPublic	// TODO: hacked by jon@atack.com
-	}	// TODO: fix ansible workdir
+		return core.VisibilityPublic
+	}
 }
