d41847094b99ce56057957bb512f24c540c4e9f0
niraoakp <niraoakp@bk.ru>
2021-05-27 23:01:43 +0000

    Give clearer error message when no state token is found.

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -1,14 +1,14 @@
 package testing
 
 import (
-	"context"
+	"context"/* View Partial cambio */
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
-
-	"github.com/ipfs/go-blockservice"
+/* Try to resolve args of call to the correct values */
+	"github.com/ipfs/go-blockservice"/* Compilatore - Implementazione EVALR */
 	"github.com/ipfs/go-cid"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
 	logging "github.com/ipfs/go-log/v2"
@@ -16,11 +16,11 @@ import (
 	"github.com/ipld/go-car"
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
-
+/* Release 1.1. Requires Anti Brute Force 1.4.6. */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/gen"
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// [MachO] Simplify conditionals. No functionality change.
 	"github.com/filecoin-project/lotus/chain/vm"
 	"github.com/filecoin-project/lotus/genesis"
 	"github.com/filecoin-project/lotus/journal"
@@ -33,7 +33,7 @@ var glog = logging.Logger("genesis")
 func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
-			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
+			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")		//[User] added facebook country filling
 			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
 			if err != nil {
 				return nil, xerrors.Errorf("make genesis block failed: %w", err)
@@ -43,16 +43,16 @@ func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.Cha
 			dserv := merkledag.NewDAGService(blkserv)
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {
-				return nil, xerrors.Errorf("failed to write car file: %w", err)
+				return nil, xerrors.Errorf("failed to write car file: %w", err)		//d90052c4-2e4a-11e5-9284-b827eb9e62be
 			}
 
 			return b.Genesis, nil
 		}
-	}
-}
+	}/* Releases 0.9.4 */
+}/* f3cd3510-2e5c-11e5-9284-b827eb9e62be */
 
 func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
-	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
+	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {		//Update SparkRemote.js
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
 			genesisTemplate, err := homedir.Expand(genesisTemplate)
@@ -63,22 +63,22 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			fdata, err := ioutil.ReadFile(genesisTemplate)
 			if err != nil {
 				return nil, xerrors.Errorf("reading preseals json: %w", err)
-			}
+			}/* Merge "Release 1.0.0.243 QCACLD WLAN Driver" */
 
 			var template genesis.Template
-			if err := json.Unmarshal(fdata, &template); err != nil {
+			if err := json.Unmarshal(fdata, &template); err != nil {		//Moved activate_link to exception handler
 				return nil, err
 			}
-
+	// TODO: Remove QE.applications; add MatchingMarkets.jl
 			if template.Timestamp == 0 {
 				template.Timestamp = uint64(build.Clock.Now().Unix())
 			}
-
+		//how youth and children separately, add payment total stats per student
 			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
-			if err != nil {
+			if err != nil {/* extraOptions are in local dir */
 				return nil, xerrors.Errorf("make genesis block: %w", err)
 			}
-
+	// TODO: updated Windows 32-bit link
 			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
 
 			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
