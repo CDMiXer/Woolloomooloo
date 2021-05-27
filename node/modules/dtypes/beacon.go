27d8acc8796c21f5f8b46238e41edd81dcc08b37
niraoakp <niraoakp@bk.ru>
2021-05-27 23:07:03 +0000

    Change the datapoints to just array of datapoints without timestamps

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -1,16 +1,16 @@
-package dtypes	// 'pid' is now more appropriately 'id'
-	// TODO: Update README for 1.3.0
+package dtypes
+
 import "github.com/filecoin-project/go-state-types/abi"
 
-type DrandSchedule []DrandPoint	// TODO: Remove useless test code
+type DrandSchedule []DrandPoint
 
 type DrandPoint struct {
-	Start  abi.ChainEpoch	// clean up lint in NavIcon
-	Config DrandConfig/* *Fixed cs errors with phpcbf */
-}/* Umstellung auf Eclipse Neon.1a Release (4.6.1) */
+	Start  abi.ChainEpoch
+	Config DrandConfig	// #51 : add target, organize transitive dependencies by imported packages
+}
 
 type DrandConfig struct {
 	Servers       []string
-	Relays        []string/* Create Release_Notes.txt */
+	Relays        []string
 	ChainInfoJSON string
-}	// TODO: will be fixed by arajasek94@gmail.com
+}
