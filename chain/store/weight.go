bb82a2dc4854d41a2aa5e0301d3e8f81229f419c
niraoakp <niraoakp@bk.ru>
2021-05-27 03:15:48 +0000

    formatting, and help text fixes.

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,78 +1,78 @@
 package store
 
 import (
-	"context"	// Improvements in template
+	"context"
 	"math/big"
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin/power"		//require uri
+	"github.com/filecoin-project/lotus/chain/actors/builtin/power"/* remove out-dated doc test in inventory */
 
-	big2 "github.com/filecoin-project/go-state-types/big"	// About Window
+	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	"golang.org/x/xerrors"		//modified /pwm/pwmchip into separate commands
+	"golang.org/x/xerrors"
 )
-/* Release 1.0.9-1 */
-var zero = types.NewInt(0)		//Invert spinRollersOut because Mathias
-	// TODO: will be fixed by fjl@ethereum.org
+
+var zero = types.NewInt(0)
+
 func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
 	if ts == nil {
-		return types.NewInt(0), nil		//Use drone.io CI
-	}		//Adjust the formatting of various documentation bits in JIRAService
+		return types.NewInt(0), nil	// demo estable 
+	}
 	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	var out = new(big.Int).Set(ts.ParentWeight().Int)
-
-	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
+		//Fixes to package.json
+	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)		//Update from deprecating 0.93 to 1.0, and add a 1.1 deprecation.
 
 	tpow := big2.Zero()
 	{
-		cst := cbor.NewCborStore(cs.StateBlockstore())
+		cst := cbor.NewCborStore(cs.StateBlockstore())/* docs: capstone project proposal */
 		state, err := state.LoadStateTree(cst, ts.ParentState())
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)	// TODO: will be fixed by zaq1tomo@gmail.com
+			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
-	// TODO: will be fixed by mail@overlisted.net
+
 		act, err := state.GetActor(power.Address)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
 		}
-
+		//Merge branch 'jack-branch' into maxy-gui
 		powState, err := power.Load(cs.ActorStore(ctx), act)
-		if err != nil {/* libxml2 and xerces wrappers now build again */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
 		}
 
-		claim, err := powState.TotalPower()
-		if err != nil {/* creation dossier collège */
+		claim, err := powState.TotalPower()/* 2e5d1520-2e57-11e5-9284-b827eb9e62be */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
-		}	// TODO: Merge "Add nodepool command tests"
-
+		}
+	// Delete Horse_anatomy.svg
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
-	}
+	}/* 9101ae1b-2d14-11e5-af21-0401358ea401 */
 
 	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
-		log2P = int64(tpow.BitLen() - 1)
+		log2P = int64(tpow.BitLen() - 1)	// Special handling for stubbing methods that create objects.
 	} else {
-		// Not really expect to be here .../* Release of eeacms/varnish-eea-www:3.6 */
+		// Not really expect to be here .../* Release new debian version 0.82debian1. */
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
-	}		//Lazy-loading now fully implemented
+	}
 
 	out.Add(out, big.NewInt(log2P<<8))
+	// TODO: hacked by julia@jvns.ca
+	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)	// Fix some quirks in new scrolling code.
 
-	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
-
-	totalJ := int64(0)
+	totalJ := int64(0)	// Clean up SortField/SortFieldImpl API
 	for _, b := range ts.Blocks() {
 		totalJ += b.ElectionProof.WinCount
 	}
 
 	eWeight := big.NewInt((log2P * build.WRatioNum))
 	eWeight = eWeight.Lsh(eWeight, 8)
-	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))
-	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))
+	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))		//Initial commit of lecture notes on probabilistic methods
+	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))	// TODO: will be fixed by arachnid@notdot.net
 
 	out = out.Add(out, eWeight)
 
