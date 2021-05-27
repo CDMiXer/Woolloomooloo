82703b319dc51db4412a7b3e8db42bf94f142074
niraoakp <niraoakp@bk.ru>
2021-05-27 14:11:26 +0000

    Description is fixed.

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints	// 404ac6f0-2e5d-11e5-9284-b827eb9e62be
 
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)/* Update button_email.html */
 
-// Test that the engine is capable of relinquishing control of a resource without deleting it.
-func TestReadRelinquish(t *testing.T) {
+// Test that the engine is capable of relinquishing control of a resource without deleting it.		//Merge "input: touchscreen: change F1A registeration procedure"
+func TestReadRelinquish(t *testing.T) {	// added MatrixType to find the proper matrix for a given Vector
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Dependencies: []string{"@pulumi/pulumi"},	// TODO: hacked by witek@enjin.io
+		Quick:        true,		//Made the clock IC use caching. Major optimization
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
