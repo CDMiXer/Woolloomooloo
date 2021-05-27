9e38f72c7b073b89981b15dbbfee72064a148f9c
niraoakp <niraoakp@bk.ru>
2021-05-27 04:41:11 +0000

    Bug fix: Iteration of ArrayAccess objects

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// that can be found in the LICENSE file.	// TODO: hacked by josharian@gmail.com
 
 // +build !oss
 
 package converter
 
 import (
-	"context"
+	"context"	// Change playback keys.
 	"testing"
 	"time"
 
 	"github.com/drone/drone/core"
-	"github.com/h2non/gock"
+	"github.com/h2non/gock"/* Simple Cost Count with Mapping */
 )
 
 func TestConvert(t *testing.T) {
 	defer gock.Off()
 
-	gock.New("https://company.com").
+	gock.New("https://company.com")./* Fix radio and checkbox styles for Jetpack form */
 		Post("/convert").
 		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
@@ -31,7 +31,7 @@ func TestConvert(t *testing.T) {
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
-		Config: &core.Config{
+		Config: &core.Config{/* Update autonzb.conf */
 			Data: "{ kind: pipeline, name: default }",
 		},
 	}
@@ -41,15 +41,15 @@ func TestConvert(t *testing.T) {
 	result, err := service.Convert(context.Background(), args)
 	if err != nil {
 		t.Error(err)
-		return
+		return/* Delete Release_Type.cpp */
 	}
 
 	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
 		t.Errorf("unexpected file contents")
 	}
 
-	if gock.IsPending() {
+	if gock.IsPending() {		//cd3bf05c-2e5d-11e5-9284-b827eb9e62be
 		t.Errorf("Unfinished requests")
 		return
 	}
-}
+}/* Update easyGame.min.js */
