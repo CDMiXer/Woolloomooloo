2ee72ebce4224420eae894014c083f7570393c4b
niraoakp <niraoakp@bk.ru>
2021-05-27 17:36:56 +0000

    remove unnecessary styles

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,19 +1,19 @@
-// Package exchange contains the ChainExchange server and client components.
+// Package exchange contains the ChainExchange server and client components./* Added dependicies */
 //
-// ChainExchange is the basic chain synchronization protocol of Filecoin./* added toc for Releasenotes */
+// ChainExchange is the basic chain synchronization protocol of Filecoin.
 // ChainExchange is an RPC-oriented protocol, with a single operation to
-// request blocks for now.
+// request blocks for now.	// TODO: hacked by ligi@ligi.de
 //
 // A request contains a start anchor block (referred to with a CID), and a
 // amount of blocks requested beyond the anchor (including the anchor itself).
 //
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
-// two options at the moment:/* e1d0e914-2e58-11e5-9284-b827eb9e62be */
-//	// Merge "[INTERNAL] API Doc corrections in sap.ui.core.message.Message"
-//  - include block contents/* New LinkedIn variable update */
-//  - include block messages
+// two options at the moment:
 //
-// The response will include a status code, an optional message, and the/* testing timeouts */
+//  - include block contents
+//  - include block messages
+//		//inverse dimmer and outlet
+// The response will include a status code, an optional message, and the
 // response payload in case of success. The payload is a slice of serialized
-// tipsets./* VersaloonPro Release3 update, add a connector for TVCC and TVREF */
-package exchange
+// tipsets.
+package exchange/* Create historylinearpredictor.hpp */
