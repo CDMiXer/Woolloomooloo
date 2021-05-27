53efc57303266160c73e95a3b90d56a0e6f1e651
niraoakp <niraoakp@bk.ru>
2021-05-27 22:07:20 +0000

    rev 511192

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Successfully attach to the hudActiveChanged signal exported by QML
+
 // +build !oss
 
 package collabs
@@ -11,7 +11,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"		//Fix rev number.
+	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
@@ -21,53 +21,53 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestDelete(t *testing.T) {		//0.265 : mouse wheel by Yuriy!!! Excellent work!
+func TestDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
-)rellortnoc(erotSmrePkcoMweN.kcom =: srebmem	
+	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
-	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)/* Release 1.2.4 (corrected) */
+	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Update to Jedi Archives Windows 7 Release 5-25 */
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
-	// TODO: Create PCD8544.h
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/", nil)	// Add logD challenge timeframe to physical properties README.
-	r = r.WithContext(		//Add a bio file for @herdnerd
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)	// Update picon navarra
 
-	HandleDelete(users, repos, members)(w, r)/* Added 2 new test functions. */
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("DELETE", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-}	
+	}
 }
 
 func TestDelete_UserNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update show-deb */
+
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)/* Update site map */
+	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("member", "octocat")		//Update flesitheboss.sh
+	c.URLParams.Add("member", "octocat")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
-	r = r.WithContext(/* Release 0.13.1 (#703) */
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
