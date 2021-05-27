c049bedcfb7eb8dd47959efff7ecfacde9ea96b3
niraoakp <niraoakp@bk.ru>
2021-05-27 21:38:36 +0000

    vm: clean up code heap visitor

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -10,22 +10,22 @@ import (
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* export sizes  */
-/* Add ArrayUtils test: index */
+)
+
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
-	var st system.State/* 7fc10862-2e5d-11e5-9284-b827eb9e62be */
+	var st system.State
 
 	cst := cbor.NewCborStore(bs)
 
 	statecid, err := cst.Put(context.TODO(), &st)
-	if err != nil {/* commit para puntuar Proyecto */
-		return nil, err	// TODO: will be fixed by steven@stebalien.com
+	if err != nil {
+		return nil, err
 	}
 
 	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
-	}/* c5f23f94-2e6d-11e5-9284-b827eb9e62be */
+	}
 
 	return act, nil
 }
