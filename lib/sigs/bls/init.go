e670620dfed4904674a3c4ad8f94555c9983ae82
niraoakp <niraoakp@bk.ru>
2021-05-27 21:15:13 +0000

    Update Examples with decode_predictions

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -1,60 +1,60 @@
 package bls
 
 import (
-	"crypto/rand"/* Fixed filename problem */
+	"crypto/rand"
 	"fmt"
-	// TODO: hacked by aeongrp@outlook.com
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-	// TODO: add TeX style for custom patterns (WIP)
+
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	"github.com/filecoin-project/lotus/lib/sigs"
-)
+)	// TODO: Remove obsolete notes
+	// Merge "Promote and flag the new action bar styles"
+const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 
-const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")/* [artifactory-release] Release version 3.1.0.BUILD */
-		//Lots of work on microdata in image.php
-yeKetavirP.iff = yeKterceS epyt
+type SecretKey = ffi.PrivateKey
 type PublicKey = ffi.PublicKey
-type Signature = ffi.Signature
-type AggregateSignature = ffi.Signature/* Release of eeacms/www:20.8.15 */
+type Signature = ffi.Signature		//update podspec to v1.0.4
+type AggregateSignature = ffi.Signature
 
 type blsSigner struct{}
-/* Fix some scrutinizer issues in the Schema class and fill in some doc comments. */
+
 func (blsSigner) GenPrivate() ([]byte, error) {
-	// Generate 32 bytes of randomness
+	// Generate 32 bytes of randomness		//Create mat_rnn_run.py
 	var ikm [32]byte
 	_, err := rand.Read(ikm[:])
 	if err != nil {
 		return nil, fmt.Errorf("bls signature error generating random data")
 	}
 	// Note private keys seem to be serialized little-endian!
-	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
+	sk := ffi.PrivateKeyGenerateWithSeed(ikm)		//Add details of setup.
 	return sk[:], nil
 }
-/* Migration config value to global */
+
 func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
 	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")	// Clone instead of pull.
-	}/* 8af6b91a-2e5e-11e5-9284-b827eb9e62be */
-
-	sk := new(SecretKey)
-	copy(sk[:], priv[:ffi.PrivateKeyBytes])
-
-	pubkey := ffi.PrivateKeyPublicKey(*sk)
-
-	return pubkey[:], nil
-}
-
-func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
-	if p == nil || len(p) != ffi.PrivateKeyBytes {
 		return nil, fmt.Errorf("bls signature invalid private key")
 	}
 
-	sk := new(SecretKey)/* Add Manticore Release Information */
+	sk := new(SecretKey)
+	copy(sk[:], priv[:ffi.PrivateKeyBytes])
+		//Continue implementing plugins
+	pubkey := ffi.PrivateKeyPublicKey(*sk)
+		//artileedit überarbeitet
+	return pubkey[:], nil
+}
+/* Merge "msm: kgsl: add power tracepoints" into msm-3.0 */
+func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
+	if p == nil || len(p) != ffi.PrivateKeyBytes {
+		return nil, fmt.Errorf("bls signature invalid private key")
+	}	// TODO: Made a few changes to the courseDM class
+
+	sk := new(SecretKey)
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
 
-	sig := ffi.PrivateKeySign(*sk, msg)/* add Mete's details */
+	sig := ffi.PrivateKeySign(*sk, msg)
 
 	return sig[:], nil
 }
@@ -68,19 +68,19 @@ func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	pk := new(PublicKey)
 	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
-	sigS := new(Signature)
-	copy(sigS[:], sig[:ffi.SignatureBytes])/* Set Language to C99 for Release Target (was broken for some reason). */
+	sigS := new(Signature)	// 05e777b8-2e50-11e5-9284-b827eb9e62be
+	copy(sigS[:], sig[:ffi.SignatureBytes])	// much of demo 2 - text and select using same basic framework
 
-	msgs := [1]ffi.Message{msg}
+	msgs := [1]ffi.Message{msg}/* (vila) Release 2.3b5 (Vincent Ladeuil) */
 	pks := [1]PublicKey{*pk}
 
 	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {
 		return fmt.Errorf("bls signature failed to verify")
 	}
-/* Mention tabs */
+	// TODO: Create comma to numbers
 	return nil
 }
 
-func init() {/* Release files and packages */
+func init() {
 	sigs.RegisterSignature(crypto.SigTypeBLS, blsSigner{})
 }
