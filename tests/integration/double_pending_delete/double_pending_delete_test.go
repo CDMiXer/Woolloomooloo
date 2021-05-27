4fe8ee4fec91808c54c4f724c4ea8ddef7d88b77
niraoakp <niraoakp@bk.ru>
2021-05-27 00:16:58 +0000

    Spike to delete everything that knows about deb.

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
+/* Merge "wlan: Release 3.2.3.107" */
 package ints
 
-import (
+import (	// TODO: Enable/Disable MultiLang (Show/Hide change language button)
 	"testing"
-
+	// TODO: Fixed template rendering
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
@@ -15,7 +15,7 @@ import (
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestDoublePendingDelete(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+		Dir:          "step1",	// TODO: Increment remote build counters
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
@@ -25,13 +25,13 @@ func TestDoublePendingDelete(t *testing.T) {
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
-
+		//Create demonstration.md
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))		//fix filterqueue
 
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
@@ -48,15 +48,15 @@ func TestDoublePendingDelete(t *testing.T) {
 				},
 			},
 			{
-				Dir:           "step3",
+				Dir:           "step3",/* Release v0.9.1.5 */
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// There is still only one pending delete resource in this snapshot.
 					assert.NotNil(t, stackInfo.Deployment)
-
+		//Updated the r-conquer feedstock.
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]/* Release v0.02 */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
@@ -76,14 +76,14 @@ func TestDoublePendingDelete(t *testing.T) {
 			},
 			{
 				Dir:      "step4",
-				Additive: true,
+				Additive: true,/* Release for v6.3.0. */
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// We should have cleared out all of the pending deletes now.
 					assert.NotNil(t, stackInfo.Deployment)
-
+	// TODO: will be fixed by julia@jvns.ca
 					assert.Equal(t, 4, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* Initial Release 1.0 */
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
@@ -94,8 +94,8 @@ func TestDoublePendingDelete(t *testing.T) {
 					b := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
-				},
+				},/* Released Beta 0.9.0.1 */
 			},
 		},
-	})
+	})	// TODO: hacked by xiemengjun@gmail.com
 }
