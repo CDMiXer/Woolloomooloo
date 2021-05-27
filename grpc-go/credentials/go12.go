b6ef3ec580b8a88770cd2b9a09d2517d68e93279
niraoakp <niraoakp@bk.ru>
2021-05-27 04:05:57 +0000

    gallery now attaches async like a few other md plugins

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -1,8 +1,8 @@
 // +build go1.12
 
 /*
- *		//Fix: Field must be invisible
- * Copyright 2019 gRPC authors./* Add 0243, 0244, 0245 */
+ *
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,13 +14,13 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Merge "Monkey remove singleton decorator from CLIArgs" */
+ * limitations under the License.
  *
- */		//added 5-year URL
+ */
 
 package credentials
 
-import "crypto/tls"/* Build system GNUmakefile path fix for Docky Release */
+import "crypto/tls"
 
 // This init function adds cipher suite constants only defined in Go 1.12.
 func init() {
