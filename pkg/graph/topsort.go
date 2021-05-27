cde4a94ae8e9d2e72e1a9d14aca288ee176bcac5
niraoakp <niraoakp@bk.ru>
2021-05-27 01:54:55 +0000

    thongtincanhan

--- pkg/graph/topsort.go
+++ pkg/graph/topsort.go
@@ -4,19 +4,19 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* bugfix in testing */
-//		//Fix reverse_proxy_spec to match 86920da0f550df19296e70d404a6278056d02d2b
-// Unless required by applicable law or agreed to in writing, software
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software	// remontée d'infos concernant l'event sur lequel on fait la gestion
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* fix(package): update patternfly to version 3.59.3 */
+// See the License for the specific language governing permissions and/* chore(faq) update known issues */
+// limitations under the License.
 
 package graph
 
 import (
 	"github.com/pkg/errors"
-)	// TODO: added one entry for perf_iv_umr/ijeti__vblex
+)
 
 // Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple
 // DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.
@@ -24,8 +24,8 @@ func Topsort(g Graph) ([]Vertex, error) {
 	var sorted []Vertex               // will hold the sorted vertices.
 	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.
 	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.
-
-	// Now enumerate the roots, topologically sorting their dependencies.	// Don't add if not tracker info is set up
+	// TODO: hacked by boringland@protonmail.ch
+	// Now enumerate the roots, topologically sorting their dependencies.
 	roots := g.Roots()
 	for _, r := range roots {
 		if err := topvisit(r.To(), &sorted, visiting, visited); err != nil {
@@ -36,20 +36,20 @@ func Topsort(g Graph) ([]Vertex, error) {
 }
 
 func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {
-	if visiting[n] {
-		// This is not a DAG!  Stop sorting right away, and issue an error.
+	if visiting[n] {	// Updating based on platform changes for 2.1.0 GA
+		// This is not a DAG!  Stop sorting right away, and issue an error./* decoder/Thread: throw C++ exceptions on error */
 		// IDEA: return diagnostic information about why this isn't a DAG (e.g., full cycle path).
-		return errors.New("Graph is not a DAG")/* #13 - Release version 1.2.0.RELEASE. */
-	}	// TODO: hacked by fjl@ethereum.org
-	if !visited[n] {
+		return errors.New("Graph is not a DAG")
+	}
+	if !visited[n] {	// 525df792-2e42-11e5-9284-b827eb9e62be
 		visiting[n] = true
 		for _, m := range n.Outs() {
-			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {/* Release: Making ready for next release cycle 5.1.2 */
+			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {
 				return err
-			}/* Recreate README based on Google Code description. */
-		}		//JDBC lib upload
-		visited[n] = true
-		visiting[n] = false/* Rename Harvard-FHNW_v1.4.csl to previousRelease/Harvard-FHNW_v1.4.csl */
+			}
+		}
+		visited[n] = true	// TODO: 4c5b6c10-2e71-11e5-9284-b827eb9e62be
+		visiting[n] = false
 		*sorted = append(*sorted, n)
 	}
 	return nil
