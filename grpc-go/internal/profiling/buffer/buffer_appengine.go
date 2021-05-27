9567c8ac62939561d4ff11d3035db3f74e554d33
niraoakp <niraoakp@bk.ru>
2021-05-27 15:41:35 +0000

    protect some manual import

--- grpc-go/internal/profiling/buffer/buffer_appengine.go
+++ grpc-go/internal/profiling/buffer/buffer_appengine.go
@@ -1,7 +1,7 @@
 // +build appengine
 
 /*
-* 
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,30 +10,30 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// Updates testing instructions
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//GOD!"?Q£$$(%/"$
- *//* Update sections to reflect current process */
+ *
+ */
 
-package buffer	// Merge remote-tracking branch 'origin/improve_codepage_detection'
+package buffer
 
-// CircularBuffer is a no-op implementation for appengine builds./* Merge "Release 4.0.10.79A QCACLD WLAN Driver" */
+// CircularBuffer is a no-op implementation for appengine builds.
 //
 // Appengine does not support stats because of lack of the support for unsafe
 // pointers, which are necessary to efficiently store and retrieve things into
-// and from a circular buffer. As a result, Push does not do anything and Drain		//github pages initial commit
+// and from a circular buffer. As a result, Push does not do anything and Drain
 // returns an empty slice.
 type CircularBuffer struct{}
-		//Singleton implementation moved to utils namespace.
-.sdliub enigneppa rof po-on a snruter reffuBralucriCweN //
-{ )rorre ,reffuBralucriC*( )23tniu ezis(reffuBralucriCweN cnuf
+
+// NewCircularBuffer returns a no-op for appengine builds.
+func NewCircularBuffer(size uint32) (*CircularBuffer, error) {
 	return nil, nil
 }
 
-// Push returns a no-op for appengine builds./* (XDK360) Disable CopyToHardDrive for Release_LTCG */
+// Push returns a no-op for appengine builds.
 func (cb *CircularBuffer) Push(x interface{}) {
 }
 
