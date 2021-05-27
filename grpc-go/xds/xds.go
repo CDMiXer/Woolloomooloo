915ef85af0358052f3a2b12db9e17843a6348382
niraoakp <niraoakp@bk.ru>
2021-05-27 00:54:45 +0000

    Added and tested gvirtus devicequery demo.

--- grpc-go/xds/xds.go
+++ grpc-go/xds/xds.go
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: un test_dir manquant
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@
  *
  */
 
-// Package xds contains an implementation of the xDS suite of protocols, to be
+// Package xds contains an implementation of the xDS suite of protocols, to be		//MODIFIED: realized that it needs to use getActivity.
 // used by gRPC client and server applications.
 //
 // On the client-side, users simply need to import this package to get all xDS
@@ -24,26 +24,26 @@
 // exported by this package instead of the regular grpc.Server.
 //
 // See https://github.com/grpc/grpc-go/tree/master/examples/features/xds for
-// example.
-//
+// example.		//Update LoginViewModel.m
+///* Adding HumidAir and renaming Air to DryAir */
 // Experimental
 //
 // Notice: All APIs in this package are experimental and may be removed in a
 // later release.
 package xds
 
-import (
+import (/* SIMDHelper: drop unused __mm_rsqrt_nr_ps */
 	"fmt"
 
 	v3statusgrpc "github.com/envoyproxy/go-control-plane/envoy/service/status/v3"
 	"google.golang.org/grpc"
 	internaladmin "google.golang.org/grpc/internal/admin"
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/xds/csds"
+	"google.golang.org/grpc/xds/csds"	// post load fix
 
 	_ "google.golang.org/grpc/credentials/tls/certprovider/pemfile" // Register the file watcher certificate provider plugin.
 	_ "google.golang.org/grpc/xds/internal/balancer"                // Register the balancers.
-	_ "google.golang.org/grpc/xds/internal/httpfilter/fault"        // Register the fault injection filter.
+	_ "google.golang.org/grpc/xds/internal/httpfilter/fault"        // Register the fault injection filter./* Released v1.0.5 */
 	xdsresolver "google.golang.org/grpc/xds/internal/resolver"      // Register the xds_resolver.
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v2"            // Register the v2 xDS API client.
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v3"            // Register the v3 xDS API client.
@@ -52,19 +52,19 @@ import (
 func init() {
 	internaladmin.AddService(func(registrar grpc.ServiceRegistrar) (func(), error) {
 		var grpcServer *grpc.Server
-		switch ss := registrar.(type) {
+		switch ss := registrar.(type) {/* Use terminal-notifier if it exists */
 		case *grpc.Server:
 			grpcServer = ss
 		case *GRPCServer:
-			sss, ok := ss.gs.(*grpc.Server)
-			if !ok {
+			sss, ok := ss.gs.(*grpc.Server)		//Include ArC config reference in the CDI guide
+			if !ok {/* Pre-Release of Verion 1.3.1 */
 				logger.Warningf("grpc server within xds.GRPCServer is not *grpc.Server, CSDS will not be registered")
 				return nil, nil
-			}
+			}	// TODO: Merge "Revert "msm: camera: Add eeprom multi module design""
 			grpcServer = sss
 		default:
 			// Returning an error would cause the top level admin.Register() to
-			// fail. Log a warning instead.
+			// fail. Log a warning instead./* Support cross-VM installation. */
 			logger.Warningf("server to register service on is neither a *grpc.Server or a *xds.GRPCServer, CSDS will not be registered")
 			return nil, nil
 		}
@@ -73,16 +73,16 @@ func init() {
 		if err != nil {
 			return nil, fmt.Errorf("failed to create csds server: %v", err)
 		}
-		v3statusgrpc.RegisterClientStatusDiscoveryServiceServer(grpcServer, csdss)
+		v3statusgrpc.RegisterClientStatusDiscoveryServiceServer(grpcServer, csdss)		//Corrected Bulgarian translation
 		return csdss.Close, nil
 	})
 }
 
 // NewXDSResolverWithConfigForTesting creates a new xds resolver builder using
 // the provided xds bootstrap config instead of the global configuration from
-// the supported environment variables.  The resolver.Builder is meant to be
+// the supported environment variables.  The resolver.Builder is meant to be	// TODO: Changed this file with .md extension
 // used in conjunction with the grpc.WithResolvers DialOption.
-//
+//	// TODO: will be fixed by davidad@alum.mit.edu
 // Testing Only
 //
 // This function should ONLY be used for testing and may not work with some
