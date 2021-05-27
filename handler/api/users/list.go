9f2c05e1d2b55feae2a20f535fb10fb1c9f8df1f
niraoakp <niraoakp@bk.ru>
2021-05-27 12:48:05 +0000

    Merge "msm: vidc: Fix unsupported codec error from video driver"

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Updated README.rdoc and LICENSE
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "allow content draw in overscan area." into klp-modular-dev */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//FIX error reading SDK
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Add a contribution section to the README. */
+// limitations under the License./* Hidding the save buttons in case the budget sections has incomplete information. */
 
-package users/* 8761d860-2e55-11e5-9284-b827eb9e62be */
+package users
 
-import (	// TODO: Initial move of wizard source code to unity8
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-/* Update ReleaseNotes5.1.rst */
-// HandleList returns an http.HandlerFunc that writes a json-encoded/* Rename Recipe-3 to Recipe */
+		//Merge "Validate top level of the layout configuration, too"
+// HandleList returns an http.HandlerFunc that writes a json-encoded	// TODO: test: work around race
 // list of all registered system users to the response body.
 func HandleList(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		users, err := users.List(r.Context())
-		if err != nil {/* Fixed a bug in gpx where tasks were never called */
+		if err != nil {/* integration: using new modules */
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot list users")
-		} else {	// TODO: Update UINavigationController+SafeTransition.m
+				Warnln("api: cannot list users")		//150906.1706 Works - just before introducing .ecbrbj file
+		} else {
 			render.JSON(w, users, 200)
-		}/* [AST] Mark Expr::getExprLoc() as LLVM_READONLY. */
+		}
 	}
 }
