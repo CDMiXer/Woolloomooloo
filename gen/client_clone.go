a55af9ead48e1242d637b2b746336c627161cb57
niraoakp <niraoakp@bk.ru>
2021-05-27 18:24:04 +0000

    'resize' events at both layers; store rows/cols directly in VTermState

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -1,16 +1,16 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.		//Create /mobile
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build go1.8
-
+// +build go1.8/* attempt to add styles from whitepaper */
+/* added baseviewerfx; java code that can read pdfs */
 package websocket
 
 import "crypto/tls"
 
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
-		return &tls.Config{}		//Add mkcd function
+		return &tls.Config{}
 	}
 	return cfg.Clone()
 }
