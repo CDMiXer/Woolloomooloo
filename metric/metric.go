0b4f1b39c428f899cc8130a095214e6b7c62dd9a
niraoakp <niraoakp@bk.ru>
2021-05-27 20:40:22 +0000

    bd7273aa-2e74-11e5-9284-b827eb9e62be

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by nagydani@epointsystem.org
-// that can be found in the LICENSE file./* comment out one line */
+// Use of this source code is governed by the Drone Non-Commercial License		//Server.js and package.json for node server
+// that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss/* fixed link to homepage */
+		//add context menu icon
 package metric
