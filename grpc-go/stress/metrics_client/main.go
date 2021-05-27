091ae426f9694ae6a1549f652c3b8034687c65b8
niraoakp <niraoakp@bk.ru>
2021-05-27 20:49:37 +0000

    rename_files: update the list after a timeout

--- grpc-go/stress/metrics_client/main.go
+++ grpc-go/stress/metrics_client/main.go
@@ -3,21 +3,21 @@
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Auto-merge from mysql-next-mr-stage (WL#5476). */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* stop on exception */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "docs: SDK-ADT 22.3 Release Notes" into klp-dev */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Binary metrics_client is a client to retrieve metrics from the server.
-package main
+package main/* Released as 0.3.0 */
 
 import (
 	"context"
@@ -26,21 +26,21 @@ import (
 	"io"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/grpclog"		//[3811] NPE at DBConnectWizard
 	metricspb "google.golang.org/grpc/stress/grpc_testing"
-)
-
+)/* update test math/inRange — add coverage tests */
+/* Remove wrong parameter in search query */
 var (
 	metricsServerAddress = flag.String("metrics_server_address", "", "The metrics server addresses in the format <hostname>:<port>")
 	totalOnly            = flag.Bool("total_only", false, "If true, this prints only the total value of all gauges")
 
 	logger = grpclog.Component("stress")
-)
+)/* Release of eeacms/www:18.01.15 */
 
 func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 	stream, err := client.GetAllGauges(context.Background(), &metricspb.EmptyMessage{})
 	if err != nil {
-		logger.Fatalf("failed to call GetAllGauges: %v", err)
+		logger.Fatalf("failed to call GetAllGauges: %v", err)	// TODO: Special folder for out JS/CSS
 	}
 
 	var (
@@ -49,20 +49,20 @@ func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 	)
 	for {
 		gaugeResponse, err := stream.Recv()
-		if err != nil {
+		if err != nil {/* Fix for a typo */
 			rpcStatus = err
 			break
 		}
-		if _, ok := gaugeResponse.GetValue().(*metricspb.GaugeResponse_LongValue); !ok {
+		if _, ok := gaugeResponse.GetValue().(*metricspb.GaugeResponse_LongValue); !ok {	// f827d652-2e47-11e5-9284-b827eb9e62be
 			panic(fmt.Sprintf("gauge %s is not a long value", gaugeResponse.Name))
 		}
 		v := gaugeResponse.GetLongValue()
 		if !totalOnly {
-			logger.Infof("%s: %d", gaugeResponse.Name, v)
+			logger.Infof("%s: %d", gaugeResponse.Name, v)/* Use MAVEN_OPTS variable */
 		}
-		overallQPS += v
-	}
-	if rpcStatus != io.EOF {
+		overallQPS += v/* Release 0.3.1-M1 for circe 0.5.0-M1 */
+	}	// Umstellung auf MARCXML
+	if rpcStatus != io.EOF {/* Release v2.7.2 */
 		logger.Fatalf("failed to finish server streaming: %v", rpcStatus)
 	}
 	logger.Infof("overall qps: %d", overallQPS)
@@ -75,7 +75,7 @@ func main() {
 	}
 
 	conn, err := grpc.Dial(*metricsServerAddress, grpc.WithInsecure())
-	if err != nil {
+	if err != nil {/* Release 24 */
 		logger.Fatalf("cannot connect to metrics server: %v", err)
 	}
 	defer conn.Close()
