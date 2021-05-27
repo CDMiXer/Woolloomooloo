63aa1ef725290ca17aa910d6d67630a13da4a0ab
niraoakp <niraoakp@bk.ru>
2021-05-27 15:21:48 +0000

    fix a bug relating to pidgin opening chats with offline buddies.

--- handler/api/repos/secrets/find_test.go
+++ handler/api/repos/secrets/find_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Add Qt declarative module for marble */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release-Datum korrigiert */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -9,13 +9,13 @@ package secrets
 import (
 	"context"
 	"encoding/json"
-	"net/http"/* fix GeoID bug (forgotten \0) */
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"/* fixed Qt-related segfault */
-	"github.com/drone/drone/handler/api/errors"	// TODO: Merge "Remove obsolete comment from abusefilter.tables.pg.sql"
-	"github.com/drone/drone/mock"/* 67c2315e-2e40-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
@@ -28,7 +28,7 @@ func TestHandleFind(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-	// TODO: Updates for v0.4
+
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 
@@ -38,13 +38,13 @@ func TestHandleFind(t *testing.T) {
 	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Began OI revamp. */
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Release version: 0.1.4 */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleFind(repos, secrets).ServeHTTP(w, r)/* Update config.ini */
-	if got, want := w.Code, http.StatusOK; want != got {/* Create rh.ly */
+	HandleFind(repos, secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -55,9 +55,9 @@ func TestHandleFind(t *testing.T) {
 	}
 }
 
-func TestHandleFind_RepoNotFound(t *testing.T) {	// Create 45) THE MAX LINES
+func TestHandleFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* [artifactory-release] Release version 1.3.0.RC2 */
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
@@ -70,13 +70,13 @@ func TestHandleFind_RepoNotFound(t *testing.T) {	// Create 45) THE MAX LINES
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//Corrected wrong src path in COPYING
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* [TRAVIS] Minor fixes */
-	}	// TODO: move to swagger
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
