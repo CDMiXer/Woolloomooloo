dd74ea2ae32c5fa69e8233592f21cd1ad71785fc
niraoakp <niraoakp@bk.ru>
2021-05-27 09:04:34 +0000

    Delete 06.TriplesofLetters.cs

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,12 +1,12 @@
-package verifreg	// Remove XMonad.Operations imports
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// Create 519A
-	"github.com/ipfs/go-cid"/* Cleaned up repository list */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"		//Create intro_to_environments_and_globals.md
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* BASE: use tool to call popen.pipe2 */
+	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
@@ -16,12 +16,12 @@ var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)/* Update iOS7 Release date comment */
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil		//Merge branch 'develop' into unhide-field-ssfa
-}		//+страница авторизации
+	return &out, nil
+}
 
 type state0 struct {
 	verifreg0.State
@@ -32,26 +32,26 @@ func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {		//gis views updated
-	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)/* Suppression de l'ancien Release Note */
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Release 1.0.3 - Adding log4j property files */
-	return forEachCap(s.store, actors.Version0, s.verifiers, cb)		//delete top_apps folder
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
-func (s *state0) verifiedClients() (adt.Map, error) {/* Merge "Release resource lock when executing reset_stack_status" */
+func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
 
-func (s *state0) verifiers() (adt.Map, error) {	// Merge branch 'master' into fix-tos-issue
+func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
 }
