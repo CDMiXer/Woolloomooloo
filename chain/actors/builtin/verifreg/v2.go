a2a82554dabb68c454a7d59fc19cfa7acaf044c2
niraoakp <niraoakp@bk.ru>
2021-05-27 19:44:10 +0000

    Merge "Refactor the lcov jobs"

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -3,7 +3,7 @@ package verifreg
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"		//Add first version of dashboard mockup
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -11,33 +11,33 @@ import (
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
-		//Delete FiraSans-MediumItalic.woff2
-var _ State = (*state2)(nil)	// transform mouse wheel events to scroll events
+
+var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Release version 3.0.2 */
+	if err != nil {
 		return nil, err
 	}
-	return &out, nil		//Update output2.txt
+	return &out, nil
 }
 
-type state2 struct {/* StyleCop: Updated to support latest 4.4.0.12 Release Candidate. */
+type state2 struct {
 	verifreg2.State
 	store adt.Store
 }
-/* Release 0.17.0. Allow checking documentation outside of tests. */
-func (s *state2) RootKey() (address.Address, error) {/* Release 0.8.2 */
+
+func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)/* libarchive: no need to set the symbolink link attributes */
+	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
 
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-)rdda ,sreifirev.s ,2noisreV.srotca ,erots.s(paCataDteg nruter	
+	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
 }
 
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
@@ -49,9 +49,9 @@ func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 }
 
 func (s *state2) verifiedClients() (adt.Map, error) {
-	return adt2.AsMap(s.store, s.VerifiedClients)/* Merge "input: touchpanel: Release all touches during suspend" */
-}/* add ThrowOilAction */
-/* Added missing link in latest bookmarks. */
+	return adt2.AsMap(s.store, s.VerifiedClients)
+}
+
 func (s *state2) verifiers() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.Verifiers)
 }
