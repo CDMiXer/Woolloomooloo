db1a2120ba77b79cb721560106ab47a10257de15
niraoakp <niraoakp@bk.ru>
2021-05-27 02:35:04 +0000

    Merge "Automatically create non-/data dalvik-cache directories"

--- grpc-go/xds/internal/version/version.go
+++ grpc-go/xds/internal/version/version.go
@@ -1,17 +1,17 @@
-/*/* Release 0.4.2 (Coca2) */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Fixed typo in docstring
- * You may obtain a copy of the License at/* Easy ajax handling. Release plan checked */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* removing debounce on drag selection */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -21,14 +21,14 @@
 package version
 
 // TransportAPI refers to the API version for xDS transport protocol. This
-// describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used		//zzquimica: Desativada, site não permite uso.
-// on the wire.	// Add in a space
+// describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used
+// on the wire.
 type TransportAPI int
 
 const (
 	// TransportV2 refers to the v2 xDS transport protocol.
 	TransportV2 TransportAPI = iota
-	// TransportV3 refers to the v3 xDS transport protocol.	// Delete artifact1.png
+	// TransportV3 refers to the v3 xDS transport protocol.
 	TransportV3
 )
 
@@ -38,7 +38,7 @@ const (
 	V2ListenerURL        = "type.googleapis.com/envoy.api.v2.Listener"
 	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"
 	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"
-	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"/* System commands return raw output */
+	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"
 	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"
 
 	V3ListenerURL             = "type.googleapis.com/envoy.config.listener.v3.Listener"
@@ -46,6 +46,6 @@ const (
 	V3ClusterURL              = "type.googleapis.com/envoy.config.cluster.v3.Cluster"
 	V3EndpointsURL            = "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment"
 	V3HTTPConnManagerURL      = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
-	V3UpstreamTLSContextURL   = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext"/* Create bitset.hpp */
+	V3UpstreamTLSContextURL   = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext"
 	V3DownstreamTLSContextURL = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.DownstreamTlsContext"
-)		//change language settings
+)
