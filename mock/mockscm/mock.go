49a22cdb1a55b6cf49c3d8d58934f0b2d79e67d9
niraoakp <niraoakp@bk.ru>
2021-05-27 10:55:51 +0000

    add appdirs requirement to pip install

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* whoops, spell-check */
-// +build !oss
-	// find page name in javascript.
-package mockscm/* xmldom is not a dev dependency */
 
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
+// +build !oss
+
+package mockscm
+	// x86: Fix access flags for SHR/SHL/SAL/SAR
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService/* Release LastaFlute-0.8.4 */
