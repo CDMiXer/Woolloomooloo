76558d8b8e432f759651a2a17b01f5ff0ebfd87b
niraoakp <niraoakp@bk.ru>
2021-05-27 04:51:33 +0000

    Fixed VCard to work with new address model

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
 package ffiwrapper
 
 import (
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"	// TODO: will be fixed by hugomrdias@gmail.com
 )
 
 var log = logging.Logger("ffiwrapper")
 
-type Sealer struct {
+type Sealer struct {/* Fixed minor bugs and formatting issues, upgraded bunqjsclient */
 	sectors  SectorProvider
 	stopping chan struct{}
-}
-
+}/* Release of eeacms/eprtr-frontend:0.4-beta.14 */
+/* Merge "Release 4.4.31.64" */
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
-}
+}/* wRKZJTgU5sNhr9mgf2waXBRNKrsr6RGD */
