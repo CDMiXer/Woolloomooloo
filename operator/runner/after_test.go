04d190ae93969ee5a0fabd99e12b271de6c5273a
niraoakp <niraoakp@bk.ru>
2021-05-27 03:37:04 +0000

    fix reachability call

--- operator/runner/after_test.go
+++ operator/runner/after_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//NEW: Trashed variable definition in procedure
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 0467401a-2e75-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
 
 package runner
