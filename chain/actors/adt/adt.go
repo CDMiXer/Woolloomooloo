1152ae27c498c1d9a61945fa28a0fd8a1c4b2087
niraoakp <niraoakp@bk.ru>
2021-05-27 19:38:37 +0000

    Added OtherObject.java

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -4,17 +4,17 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"
-)
+	"github.com/filecoin-project/go-state-types/cbor"		//(#21) Prep for release of v0.2.0. 
+)	// TODO: hacked by greg@colvin.org
 
 type Map interface {
 	Root() (cid.Cid, error)
 
 	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
-	Delete(k abi.Keyer) error
+	Delete(k abi.Keyer) error/* Added required framework header and search paths on Release configuration. */
 
-	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
+	ForEach(v cbor.Unmarshaler, fn func(key string) error) error/* a95e206a-2e76-11e5-9284-b827eb9e62be */
 }
 
 type Array interface {
