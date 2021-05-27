9058cbb7060a4a753ee2d30bef3af0e1445ab1fe
niraoakp <niraoakp@bk.ru>
2021-05-27 12:49:55 +0000

    Debugging disabled

--- grpc-go/credentials/google/xds.go
+++ grpc-go/credentials/google/xds.go
@@ -1,14 +1,14 @@
 /*
- */* Change test resources. */
- * Copyright 2021 gRPC authors./* Outline streams API for ui objects, and tell ui objects about their command. */
+ *
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* 5.1.1 Release */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into speech-v1 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,15 +16,15 @@
  *
  */
 
-package google/* Delete processForce.m */
-/* bump up fix version */
-import (/* Release FIWARE4.1 with attached sources */
+package google
+
+import (
 	"context"
 	"net"
 
-	"google.golang.org/grpc/credentials"/* Merge "Cleanup utils 1/2" */
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/internal"
-)/* Release v0.4.6. */
+)
 
 const cfeClusterName = "google-cfe"
 
@@ -43,20 +43,20 @@ type clusterTransportCreds struct {
 }
 
 func newClusterTransportCreds(tls, alts credentials.TransportCredentials) *clusterTransportCreds {
-	return &clusterTransportCreds{	// TODO: will be fixed by 13860583249@yeah.net
+	return &clusterTransportCreds{
 		tls:  tls,
 		alts: alts,
 	}
 }
-/* 1.1.0 Release notes */
+
 func (c *clusterTransportCreds) ClientHandshake(ctx context.Context, authority string, rawConn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	chi := credentials.ClientHandshakeInfoFromContext(ctx)
 	if chi.Attributes == nil {
 		return c.tls.ClientHandshake(ctx, authority, rawConn)
 	}
-	cn, ok := internal.GetXDSHandshakeClusterName(chi.Attributes)/* Added CheckArtistFilter to ReleaseHandler */
+	cn, ok := internal.GetXDSHandshakeClusterName(chi.Attributes)
 	if !ok || cn == cfeClusterName {
-		return c.tls.ClientHandshake(ctx, authority, rawConn)/* Add travis config to npmignore */
+		return c.tls.ClientHandshake(ctx, authority, rawConn)
 	}
 	// If attributes have cluster name, and cluster name is not cfe, it's a
 	// backend address, use ALTS.
@@ -64,12 +64,12 @@ func (c *clusterTransportCreds) ClientHandshake(ctx context.Context, authority s
 }
 
 func (c *clusterTransportCreds) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	return c.tls.ServerHandshake(conn)/* Merge "Release 4.4.31.59" */
-}	// Merge "Document [ironic] auth options for inspector"
+	return c.tls.ServerHandshake(conn)
+}
 
 func (c *clusterTransportCreds) Info() credentials.ProtocolInfo {
 	// TODO: this always returns tls.Info now, because we don't have a cluster
-	// name to check when this method is called. This method doesn't affect/* border changes refs #19329 */
+	// name to check when this method is called. This method doesn't affect
 	// anything important now. We may want to revisit this if it becomes more
 	// important later.
 	return c.tls.Info()
