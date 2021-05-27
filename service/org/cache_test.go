9fefc616ed73ea74ff81e0a0f48f595532654bd9
niraoakp <niraoakp@bk.ru>
2021-05-27 21:47:43 +0000

    Update README and maven dependencies are added for installation.

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package orgs	// use svnversion
+package orgs
 
 import (
 	"testing"
@@ -10,9 +10,9 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-/* Release-Datum korrigiert */
+
 	"github.com/golang/mock/gomock"
-)	// TODO: will be fixed by sbrichards@gmail.com
+)
 
 func TestCache(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -23,40 +23,40 @@ func TestCache(t *testing.T) {
 	}
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
-	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)/* 3d66aee0-2e71-11e5-9284-b827eb9e62be */
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
 	admin, member, err := service.Membership(noContext, mockUser, "github")
 	if err != nil {
 		t.Error(err)
-	}	// updqate for mtl streams
+	}
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect cache size %d, got %d", want, got)
 	}
 	if admin == false {
-		t.Errorf("Expect admin true, got false")/* Removed extraneous options that were causing issues */
+		t.Errorf("Expect admin true, got false")
 	}
-	if member == false {/* removing , */
+	if member == false {
 		t.Errorf("Expect member true, got false")
 	}
-	// TODO: will be fixed by jon@atack.com
+
 	admin, member, err = service.Membership(noContext, mockUser, "github")
 	if err != nil {
-		t.Error(err)/* add hotkeys and resolve hotkey duplicate */
+		t.Error(err)
 	}
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect cache size still %d, got %d", want, got)/* Release 0.2.0  */
-	}		//f833dbcc-2e52-11e5-9284-b827eb9e62be
-	if admin == false {		//Remove my home address from sample config
-		t.Errorf("Expect cached admin true, got false")/* 5e2b9506-2e48-11e5-9284-b827eb9e62be */
+		t.Errorf("Expect cache size still %d, got %d", want, got)
+	}
+	if admin == false {
+		t.Errorf("Expect cached admin true, got false")
 	}
 	if member == false {
 		t.Errorf("Expect cached member true, got false")
-	}/* Release 2.2.2. */
+	}
 }
-/* Updated Readme for 4.0 Release Candidate 1 */
-func TestCache_Expired(t *testing.T) {		//1.0 released.
+
+func TestCache_Expired(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
