95eb8b20701299b01d8ea5a8ed669bded44938ff
niraoakp <niraoakp@bk.ru>
2021-05-27 05:47:30 +0000

    Merge "update octavia-lib to 1.2.0"

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Update integrate-your-tech@es.md
-// that can be found in the LICENSE file.	// Fixed Issue #2: Broken link to original project.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* tpm2_policyor.c: Removed remnants of debug code in the file */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Merge "msm: cpufreq: Release cpumask_var_t on all cases" into msm-3.0 */
 
 // +build !oss
-
+/* 4b0f8030-2e67-11e5-9284-b827eb9e62be */
 package config
