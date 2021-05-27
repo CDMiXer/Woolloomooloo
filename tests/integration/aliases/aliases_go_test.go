75614f83da83a3053bc9bfcf4e12652955cf45cb
niraoakp <niraoakp@bk.ru>
2021-05-27 14:07:37 +0000

    Flat theme

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -3,11 +3,11 @@
 
 package ints
 
-import (
+import (		//Adding Kixmpp websocket codec.
 	"path/filepath"
 	"testing"
-	// TODO: will be fixed by caojiaoyue@protonmail.com
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//Merge branch 'master' into bug/open-enrollments-no-service-167206215
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 var dirs = []string{
@@ -15,27 +15,27 @@ var dirs = []string{
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",
+	"rename_component",/* Change type of Composer package */
 }
-/* b59e1622-2e63-11e5-9284-b827eb9e62be */
+
 func TestGoAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("go", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),/* nunaliit2-js: Fixes to GridCanvas */
-				Dependencies: []string{/* Merge branch 'hotfix/3.0.1' into develop */
+				Dir: filepath.Join(d, "step1"),
+				Dependencies: []string{
 					"github.com/pulumi/pulumi/sdk/v2",
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
-					{
+					{/* 72d22e16-2e52-11e5-9284-b827eb9e62be */
 						Dir:             filepath.Join(d, "step2"),
 						ExpectNoChanges: true,
-						Additive:        true,
+						Additive:        true,/* Delete GitReleases.h */
 					},
-				},/* Alpha Release Nº1. */
+				},
 			})
 		})
 	}
-}	// TODO: Deleted unneeded files
+}
