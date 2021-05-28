6a68ee094f1fc49984f3338c55e708060c161524
niraoakp <niraoakp@bk.ru>
2021-05-28 19:43:37 +0000

    JsonClient: start secure and normal server

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//compatibility to Sage 5, SymPy 0.7, Cython 0.15, Django 1.2
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package rpc
+		//Catch Unoconv exception
+package rpc/* XPATH: Added Check for Trisotch BPMN  Modeler. */
 
 type serverError struct {
 	Status  int
