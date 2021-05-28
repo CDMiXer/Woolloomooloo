03defe026ffe82ec3209056e034f696e95fc228f
niraoakp <niraoakp@bk.ru>
2021-05-28 11:22:24 +0000

    Merge "Improve ANR dropbox reports" into nyc-dev

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-		//Merge branch 'master' into #52/custom-color-picker-layout
+
 // Step 1: Populate our dependency graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });	// TODO: Update OpenWisAuthorization.java
-// Dependency graph: b -> a
+const b = new Resource("b", { state: 2, resource: a });	// TODO: will be fixed by witek@enjin.io
+// Dependency graph: b -> a	// I don't like hardcoded links...
