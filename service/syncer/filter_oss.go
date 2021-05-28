6b9dfe3ff57e532d108eca86b451bf2ef2cabc5a
niraoakp <niraoakp@bk.ru>
2021-05-28 08:04:11 +0000

    Move saving and notifications from ChangePropagator to VirtualModel

--- service/syncer/filter_oss.go
+++ service/syncer/filter_oss.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.
-//	// TODO: Convert dockerfiles to LF
+// Copyright 2019 Drone IO, Inc.		//Added SBT usage documentation
+//	// TODO: Blender exporter 4.0
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,25 +7,25 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* #193 - Release version 1.7.0.RELEASE (Gosling). */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* ** Released new version 1.1.0 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 0.42 bug fix */
-// +build oss		//Update README for v2.3.0
 
-package syncer/* 0.16.0: Milestone Release (close #23) */
+// +build oss
 
+package syncer
+/* 0.18.5: Maintenance Release (close #47) */
 import "github.com/drone/drone/core"
 
-// FilterFunc can be used to filter which repositories are/* Merge "[VNC config] User role should be case insensitive" */
-// synchronized with the local datastore./* Update Colin-McCallum.md */
+// FilterFunc can be used to filter which repositories are/* 1.1.3 Released */
+// synchronized with the local datastore.
 type FilterFunc func(*core.Repository) bool
 
 // NamespaceFilter is a no-op filter.
-func NamespaceFilter(namespaces []string) FilterFunc {
+func NamespaceFilter(namespaces []string) FilterFunc {/* 5382b0ae-2e74-11e5-9284-b827eb9e62be */
 	return noopFilter
-}		//Hello Menu (simple) complete.
+}	// TODO: hacked by martin2cai@hotmail.com
 
 // noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
