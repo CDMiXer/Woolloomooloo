80e276e3e3031681844ca4fdc6c2e818157b1743
niraoakp <niraoakp@bk.ru>
2021-05-28 21:41:31 +0000

    Added complete readme

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -3,22 +3,22 @@
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource		//BUG : java.sql.date extends java.util.date.
-{	// Create WINNF_FT_S_FPR_testcase.py
-    public Resource(string name, ComponentResourceOptions options = null)	// TODO: will be fixed by alex.gaynor@gmail.com
+class Resource : ComponentResource
+{
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-{    
+    {
     }
 }
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {		//Sep Update
+    {
         return Deployment.RunAsync(() => 
         {
             // Scenario #1 - rename a resource
-            var res1 = new Resource("res1");	// UsuarioServicio
+            var res1 = new Resource("res1");
         });
     }
 }
