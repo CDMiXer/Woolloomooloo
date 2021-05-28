2a7d851242ea303566b2d72f0b7a91e17ff8b043
niraoakp <niraoakp@bk.ru>
2021-05-28 13:36:36 +0000

    Added Android section in the README file

--- pkg/backend/filestate/stack.go
+++ pkg/backend/filestate/stack.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release update to 1.1.0 & updated README with new instructions */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Delete data.css!
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package filestate/* renamed some functions in pixbuf-utils.c */
+package filestate
 
 import (
 	"context"
 	"time"
-	// Plan making the not-before tasks displayable in a special view
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// 57b0c2be-2e6b-11e5-9284-b827eb9e62be
-/* Release 1.8.2.0 */
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/pkg/v2/engine"	// - added stream control api
+	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/pkg/v2/operations"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
@@ -31,13 +31,13 @@ import (
 // Stack is a local stack.  This simply adds some local-specific properties atop the standard backend stack interface.
 type Stack interface {
 	backend.Stack
-	Path() string // a path to the stack's checkpoint file on disk./* Fixed accelerator localization. */
+	Path() string // a path to the stack's checkpoint file on disk.
 }
 
 // localStack is a local stack descriptor.
-type localStack struct {		//Added support for non notified AuthContext URI:s.
+type localStack struct {
 	ref      backend.StackReference // the stack's reference (qualified name).
-	path     string                 // a path to the stack's checkpoint file on disk./* Release 0.15.1 */
+	path     string                 // a path to the stack's checkpoint file on disk.
 	snapshot *deploy.Snapshot       // a snapshot representing the latest deployment state.
 	b        *localBackend          // a pointer to the backend this stack belongs to.
 }
@@ -47,19 +47,19 @@ func newStack(ref backend.StackReference, path string, snapshot *deploy.Snapshot
 		ref:      ref,
 		path:     path,
 		snapshot: snapshot,
-		b:        b,	// TODO: hacked by steven@stebalien.com
+		b:        b,
 	}
 }
 
 func (s *localStack) Ref() backend.StackReference                            { return s.ref }
-func (s *localStack) Snapshot(ctx context.Context) (*deploy.Snapshot, error) { return s.snapshot, nil }/* Release of eeacms/www:18.5.26 */
+func (s *localStack) Snapshot(ctx context.Context) (*deploy.Snapshot, error) { return s.snapshot, nil }
 func (s *localStack) Backend() backend.Backend                               { return s.b }
 func (s *localStack) Path() string                                           { return s.path }
-	// TODO: Update django-debug-toolbar from 1.5 to 1.6
-func (s *localStack) Remove(ctx context.Context, force bool) (bool, error) {		//Merge "Move remaining aggregate operations to conductor"
+
+func (s *localStack) Remove(ctx context.Context, force bool) (bool, error) {
 	return backend.RemoveStack(ctx, s, force)
 }
-/* Release Performance Data API to standard customers */
+
 func (s *localStack) Rename(ctx context.Context, newName tokens.QName) (backend.StackReference, error) {
 	return backend.RenameStack(ctx, s, newName)
 }
