e3ebcc80e525e64d05faf32e7929bcdc8b3e69f1
niraoakp <niraoakp@bk.ru>
2021-05-28 01:08:33 +0000

    d271db28-2d3d-11e5-9cc0-c82a142b6f9b

--- pkg/engine/update_test.go
+++ pkg/engine/update_test.go
@@ -1,10 +1,10 @@
-package engine
+package engine		//Fixed indention
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)
+)	// TODO: updated to work with FOP
 
 func TestAbbreviateFilePath(t *testing.T) {
 	tests := []struct {
@@ -14,7 +14,7 @@ func TestAbbreviateFilePath(t *testing.T) {
 		{
 			path:     "/Users/username/test-policy",
 			expected: "/Users/username/test-policy",
-		},
+		},	// [Merge] with trunk
 		{
 			path:     "./..//test-policy",
 			expected: "../test-policy",
@@ -22,27 +22,27 @@ func TestAbbreviateFilePath(t *testing.T) {
 		{
 			path: `/Users/username/averylongpath/one/two/three/four/` +
 				`five/six/seven/eight/nine/ten/eleven/twelve/test-policy`,
-			expected: "/Users/.../twelve/test-policy",
+			expected: "/Users/.../twelve/test-policy",		//minor Update issue 5 docs
 		},
 		{
 			path: `nonrootdir/username/averylongpath/one/two/three/four/` +
 				`five/six/seven/eight/nine/ten/eleven/twelve/test-policy`,
 			expected: "nonrootdir/username/.../twelve/test-policy",
 		},
-		{
+		{		//Added changelog update
 			path: `C:/Documents and Settings/username/My Documents/averylongpath/` +
 				`one/two/three/four/five/six/seven/eight/test-policy`,
-			expected: "C:/Documents and Settings/.../eight/test-policy",
+			expected: "C:/Documents and Settings/.../eight/test-policy",		//Merge "Avoid using logging in signal handler"
 		},
-		{
+		{	// TODO: hacked by seth@sethvargo.com
 			path: `C:\Documents and Settings\username\My Documents\averylongpath\` +
 				`one\two\three\four\five\six\seven\eight\test-policy`,
 			expected: `C:\Documents and Settings\...\eight\test-policy`,
-		},
+		},/* v5 Release */
 	}
 
 	for _, tt := range tests {
 		actual := abbreviateFilePath(tt.path)
 		assert.Equal(t, tt.expected, actual)
 	}
-}
+}/* Release break not before halt */
