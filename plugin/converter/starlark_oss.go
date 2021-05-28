5d64e0f9db8bcd7e6d382bac41861a19928cb106
niraoakp <niraoakp@bk.ru>
2021-05-28 23:58:12 +0000

    bithumb createOrder minor edit

--- plugin/converter/starlark_oss.go
+++ plugin/converter/starlark_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by admin@multicoin.co
-//	// Merge "Expand core post edit functionality to match VE"
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at/* adding easyconfigs: make-4.3-GCCcore-10.3.0.eb, imake-1.0.8-GCCcore-10.3.0.eb */
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by martin2cai@hotmail.com
-// distributed under the License is distributed on an "AS IS" BASIS,
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* GMParser 1.0 (Stable Release, with JavaDocs) */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// More additions to the level editor.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by alan.shaw@protocol.ai
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package converter	// TODO: stl: initial build system driver
-
+package converter
+		//copy in archive entry style.
 import (
 	"github.com/drone/drone/core"
 )
 
 // Starlark returns a conversion service that converts the
-// starlark file to a yaml file.
+// starlark file to a yaml file./* cleaning up makefile [skip ci] */
 func Starlark(enabled bool) core.ConvertService {
 	return new(noop)
-}/* Release BAR 1.1.8 */
+}
