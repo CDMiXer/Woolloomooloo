d584bc8c4134f5ca11ece775140f7ea87e36bded
niraoakp <niraoakp@bk.ru>
2021-05-28 00:55:58 +0000

    Issues: Add a device section / Clarify "buildbot"

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.		//Remove unnecessary cherry-pick, breaking Nova
+//	// TODO: 7b0137cc-2e4f-11e5-ba70-28cfe91dbc4b
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+///* Integrated into AntPool system */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Create MainWindow.form
-//
-// Unless required by applicable law or agreed to in writing, software		//Updating @Optional annotation
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Updated pivot tables
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
