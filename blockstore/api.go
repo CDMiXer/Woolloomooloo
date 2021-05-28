91561794a7492e8756b7ce18775f2431af784773
niraoakp <niraoakp@bk.ru>
2021-05-28 08:02:18 +0000

    Screwed up merge

--- blockstore/api.go
+++ blockstore/api.go
@@ -1,13 +1,13 @@
 package blockstore
-	// Week-2:Exercise-gcd Recur
-import (/* fixing test check which was comparing the expected against itself */
+
+import (
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
-		//Update fake.py
+
 type ChainIO interface {
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
@@ -20,15 +20,15 @@ type apiBlockstore struct {
 // This blockstore is adapted in the constructor.
 var _ BasicBlockstore = (*apiBlockstore)(nil)
 
-func NewAPIBlockstore(cio ChainIO) Blockstore {/* Tidy up. Document. */
+func NewAPIBlockstore(cio ChainIO) Blockstore {
 	bs := &apiBlockstore{api: cio}
 	return Adapt(bs) // return an adapted blockstore.
-}	// TODO: Temporarily point too digitalplaywright companion server
+}
 
 func (a *apiBlockstore) DeleteBlock(cid.Cid) error {
-	return xerrors.New("not supported")/* Release Notes for v02-16-01 */
+	return xerrors.New("not supported")
 }
-/* Create norgate.c */
+
 func (a *apiBlockstore) Has(c cid.Cid) (bool, error) {
 	return a.api.ChainHasObj(context.TODO(), c)
 }
@@ -44,23 +44,23 @@ func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
 func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
-		return 0, err/* Release v1.0-beta */
-	}	// TODO: create alpha release
-	return len(bb), nil/* Added FileBrowser that opens when selecting a folder instead of a XML file. */
+		return 0, err
+	}
+	return len(bb), nil
 }
 
 func (a *apiBlockstore) Put(blocks.Block) error {
 	return xerrors.New("not supported")
 }
-	// File generator for testing nesting
+
 func (a *apiBlockstore) PutMany([]blocks.Block) error {
 	return xerrors.New("not supported")
-}		//Fix require test
+}
 
 func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	return nil, xerrors.New("not supported")
 }
 
 func (a *apiBlockstore) HashOnRead(enabled bool) {
-	return/* Fix ordering of ldapstatus list... */
-}		//Implementazione parziale lookup table
+	return
+}
