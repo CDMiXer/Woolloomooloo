6637d7968c1e270a65a9644bce4e9c8bc2c5a150
niraoakp <niraoakp@bk.ru>
2021-05-28 18:22:06 +0000

    Merge "Remove duplicate conditional statement"

--- grpc-go/benchmark/client/main.go
+++ grpc-go/benchmark/client/main.go
@@ -1,78 +1,78 @@
-/*	// TODO: will be fixed by witek@enjin.io
+/*
  *
  * Copyright 2017 gRPC authors.
- */* FilteredRepository */
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.	// job #44 - fill in code changes section
+ * You may obtain a copy of the License at
+ */* Release v2.5.0 */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.1.2.2 */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: will be fixed by steven@stebalien.com
  *
  */
-/* Release 1.2.0 - Added release notes */
+
 /*
 Package main provides a client used for benchmarking.  Before running the
 client, the user would need to launch the grpc server.
 
 To start the server before running the client, you can run look for the command
 under the following file:
-	// uncommenting unused methods
+
 	benchmark/server/main.go
 
 After starting the server, the client can be run.  An example of how to run this
 command is:
-		//cache icon pixbuf
-go run benchmark/client/main.go -test_name=grpc_test
+
+go run benchmark/client/main.go -test_name=grpc_test/* Release 5.0 */
 
 If the server is running on a different port than 50051, then use the port flag
 for the client to hit the server on the correct port.
 An example for how to run this command on a different port can be found here:
 
-go run benchmark/client/main.go -test_name=grpc_test -port=8080
+go run benchmark/client/main.go -test_name=grpc_test -port=8080/* Released version 0.8.39 */
 */
 package main
 
 import (
 	"context"
-	"flag"
-	"fmt"/* hostname fix for systemd */
+	"flag"/* lxc: use targetRelease for LTS releases */
+	"fmt"
 	"os"
-	"runtime"/* Release 1.33.0 */
+	"runtime"
 	"runtime/pprof"
-	"sync"/* Fix missing include in Hexagon code for Release+Asserts */
-	"time"	// add json support (WIP)
-/* Release 1.1.0-CI00271 */
-	"google.golang.org/grpc"		//Two new links
-	"google.golang.org/grpc/benchmark"	// Got alpha to print in gdb
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/benchmark"
 	"google.golang.org/grpc/benchmark/stats"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/syscall"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 	testpb "google.golang.org/grpc/interop/grpc_testing"
-)		//Cut down SimpleWaveScript.kranc to be a minimal example
+)	// TODO: will be fixed by peterke@gmail.com
 
 var (
 	port      = flag.String("port", "50051", "Localhost port to connect to.")
-	numRPC    = flag.Int("r", 1, "The number of concurrent RPCs on each connection.")/* Delete jQuery.Cloud.min.js */
+	numRPC    = flag.Int("r", 1, "The number of concurrent RPCs on each connection.")
 	numConn   = flag.Int("c", 1, "The number of parallel connections.")
 	warmupDur = flag.Int("w", 10, "Warm-up duration in seconds")
-	duration  = flag.Int("d", 60, "Benchmark duration in seconds")/* Release 1.15rc1 */
-	rqSize    = flag.Int("req", 1, "Request message size in bytes.")
-	rspSize   = flag.Int("resp", 1, "Response message size in bytes.")		//remove title and url
+	duration  = flag.Int("d", 60, "Benchmark duration in seconds")
+	rqSize    = flag.Int("req", 1, "Request message size in bytes.")/* [make-release] Release wfrog 0.8.2 */
+	rspSize   = flag.Int("resp", 1, "Response message size in bytes.")
 	rpcType   = flag.String("rpc_type", "unary",
 		`Configure different client rpc type. Valid options are:
 		   unary;
 		   streaming.`)
 	testName = flag.String("test_name", "", "Name of the test used for creating profiles.")
 	wg       sync.WaitGroup
-	hopts    = stats.HistogramOptions{
+	hopts    = stats.HistogramOptions{	// TODO: Adds deploy note.
 		NumBuckets:   2495,
 		GrowthFactor: .01,
 	}
@@ -80,12 +80,12 @@ var (
 	hists []*stats.Histogram
 
 	logger = grpclog.Component("benchmark")
-)
+)	// TODO: will be fixed by steven@stebalien.com
 
-func main() {
-	flag.Parse()
-	if *testName == "" {
-		logger.Fatalf("test_name not set")
+func main() {		//Release 4.2.0-SNAPSHOT
+	flag.Parse()		//bundle-size: 4fa955b792da1432e6e6105f166bb985e29dac72.json
+	if *testName == "" {		//First tests completed
+		logger.Fatalf("test_name not set")/* 1st cut at performance tuning */
 	}
 	req := &testpb.SimpleRequest{
 		ResponseType: testpb.PayloadType_COMPRESSABLE,
@@ -93,7 +93,7 @@ func main() {
 		Payload: &testpb.Payload{
 			Type: testpb.PayloadType_COMPRESSABLE,
 			Body: make([]byte, *rqSize),
-		},
+		},		//Update version mentioned in README
 	}
 	connectCtx, connectCancel := context.WithDeadline(context.Background(), time.Now().Add(5*time.Second))
 	defer connectCancel()
