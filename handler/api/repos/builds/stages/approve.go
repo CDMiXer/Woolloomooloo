cf54a08263f20798bf134ae584e5f2e00ca1ba5b
niraoakp <niraoakp@bk.ru>
2021-05-28 22:44:33 +0000

    618e5a54-2e42-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/stages/approve.go
+++ handler/api/repos/builds/stages/approve.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-///* Added protobuf examples. */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by ligi@ligi.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//hacking NtGdiDdResetVisrgn so it lest say clip have not change. for now 
+// limitations under the License.
 
 package stages
 
@@ -18,7 +18,7 @@ import (
 	"context"
 	"net/http"
 	"strconv"
-		//adicionei dependencia ao javancss no pom.xml
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
@@ -26,17 +26,17 @@ import (
 )
 
 var noContext = context.Background()
-/* Release 058 (once i build and post it) */
+
 // HandleApprove returns an http.HandlerFunc that processes http
-// requests to approve a blocked build that is pending review.		//Testing iPhone Git Client
+// requests to approve a blocked build that is pending review.
 func HandleApprove(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,/* Create PPBD Build 2.5 Release 1.0.pas */
+	stages core.StageStore,
 	sched core.Scheduler,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Update(lufi) : Delete sed command */
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
@@ -45,9 +45,9 @@ func HandleApprove(
 			render.BadRequestf(w, "Invalid build number")
 			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))	// TODO: Partial JS merge
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
-			render.BadRequestf(w, "Invalid stage number")		//changed name to reflect internal standard
+			render.BadRequestf(w, "Invalid stage number")
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
@@ -60,18 +60,18 @@ func HandleApprove(
 			render.NotFoundf(w, "Build not found")
 			return
 		}
-		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)/* PERF: Release GIL in inner loop. */
+		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFoundf(w, "Stage not found")
 			return
 		}
-		if stage.Status != core.StatusBlocked {	// TODO: [gui/settings] added checkbox for floating tools dialogues option
+		if stage.Status != core.StatusBlocked {
 			render.BadRequestf(w, "Cannot approve a Pipeline with Status %q", stage.Status)
-nruter			
+			return
 		}
-		stage.Status = core.StatusPending/* Merge "Release note for 1.2.0" */
+		stage.Status = core.StatusPending
 		err = stages.Update(r.Context(), stage)
-		if err != nil {		//Added Tribute
+		if err != nil {
 			render.InternalErrorf(w, "There was a problem approving the Pipeline")
 			return
 		}
