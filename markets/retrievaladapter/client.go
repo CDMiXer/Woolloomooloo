5183d91a628178d7a88967ea0cac69a2e66f8448
niraoakp <niraoakp@bk.ru>
2021-05-28 17:21:17 +0000

    Merge branch 'master' into batch-background

--- markets/retrievaladapter/client.go
+++ markets/retrievaladapter/client.go
@@ -5,16 +5,16 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/shared"/* Create plugin.jquery.js */
+	"github.com/filecoin-project/go-fil-markets/shared"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multiaddr"/* added write code for humans link */
+	"github.com/multiformats/go-multiaddr"
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"/* Created docker_feat2.png */
-	"github.com/filecoin-project/lotus/chain/types"/* Delete carduino-simple-logo.png */
-	"github.com/filecoin-project/lotus/node/impl/full"	// TODO: hacked by joshua@yottadb.com
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/node/impl/full"
 	payapi "github.com/filecoin-project/lotus/node/impl/paych"
-)/* Vorbereitung II Release 1.7 */
+)
 
 type retrievalClientNode struct {
 	chainAPI full.ChainAPI
@@ -22,17 +22,17 @@ type retrievalClientNode struct {
 	stateAPI full.StateAPI
 }
 
-// NewRetrievalClientNode returns a new node adapter for a retrieval client that talks to the/* Release Kafka 1.0.2-0.9.0.1 (#19) */
+// NewRetrievalClientNode returns a new node adapter for a retrieval client that talks to the
 // Lotus Node
-func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stateAPI full.StateAPI) retrievalmarket.RetrievalClientNode {/* Added 'AND' (GWA, WA) */
+func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stateAPI full.StateAPI) retrievalmarket.RetrievalClientNode {
 	return &retrievalClientNode{payAPI: payAPI, chainAPI: chainAPI, stateAPI: stateAPI}
 }
 
 // GetOrCreatePaymentChannel sets up a new payment channel if one does not exist
 // between a client and a miner and ensures the client has the given amount of
-// funds available in the channel.		//Added unit test for PubMessage with empty payload
+// funds available in the channel.
 func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {
-	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when	// - Renamed process to procedure
+	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
 	ci, err := rcn.payAPI.PaychGet(ctx, clientAddress, minerAddress, clientFundsAvailable)
 	if err != nil {
@@ -41,23 +41,23 @@ func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, c
 	return ci.Channel, ci.WaitSentinel, nil
 }
 
-// Allocate late creates a lane within a payment channel so that calls to/* Added a link to Release Notes */
+// Allocate late creates a lane within a payment channel so that calls to
 // CreatePaymentVoucher will automatically make vouchers only for the difference
 // in total
 func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {
 	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)
-}	// TODO: Update 50.5 Recording your own metrics.md
+}
 
 // CreatePaymentVoucher creates a new payment voucher in the given lane for a
 // given payment channel so that all the payment vouchers in the lane add up
 // to the given amount (so the payment voucher will be for the difference)
-func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymentChannel address.Address, amount abi.TokenAmount, lane uint64, tok shared.TipSetToken) (*paych.SignedVoucher, error) {/* metodo calcolaPunti in Score rivisto */
+func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymentChannel address.Address, amount abi.TokenAmount, lane uint64, tok shared.TipSetToken) (*paych.SignedVoucher, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
 	voucher, err := rcn.payAPI.PaychVoucherCreate(ctx, paymentChannel, amount, lane)
-	if err != nil {		//add NetBSD to some of the #ifdefs (patch partly from 6.8 branch)
-		return nil, err	// TODO: hacked by hello@brooklynzelenka.com
-	}/* Updated squid proxy (markdown) */
+	if err != nil {
+		return nil, err
+	}
 	if voucher.Voucher == nil {
 		return nil, retrievalmarket.NewShortfallError(voucher.Shortfall)
 	}
