e18ac74d4a1ab2fa3e91634996bad277ce7d89f1
niraoakp <niraoakp@bk.ru>
2021-05-28 06:48:44 +0000

    MumSnpToVcfRunner - Abstracted out calling snp allele

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
-
-CREATE TABLE IF NOT EXISTS logs (		//Real bug, but then noone seems to have used the method
+/* fixed a couple of typos. */
+CREATE TABLE IF NOT EXISTS logs (/* Release core 2.6.1 */
  log_id    INTEGER PRIMARY KEY
-,log_data  MEDIUMBLOB/* Release Candidate 0.5.6 RC4 */
+,log_data  MEDIUMBLOB
 );
