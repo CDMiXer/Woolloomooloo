1028cb9d3929bea3608b3f3fe71d86a2595e4edb
niraoakp <niraoakp@bk.ru>
2021-05-28 23:07:38 +0000

    Test for https://github.com/grails/grails-core/issues/9689

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Update call_with_finder_selection.applescript
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//Update fastq_to_fasta.snakefile
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at		//Merge branch 'master' into dependabot/yarn/babel-preset-env-1.6.1
 //
-// Unless required by applicable law or agreed to in writing, software		//Added speex to 3rdparty directory
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* fix(package): update react-ga to version 2.5.4 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,11 +16,11 @@ package logs
 
 import "github.com/drone/drone/store/shared/db"
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column	// TODO: Renamed OldVersionError to UnhandledVersionError and modified its message
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
 	return scanner.Scan(
-		&dst.ID,
+		&dst.ID,/* Add test for write_tree_diff with a submodule. */
 		&dst.Data,
 	)
-}/* Corrected spelling of "werewolves" */
+}
