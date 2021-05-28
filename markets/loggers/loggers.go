ddc9457af82346f14942635bbb0ba1169547463f
niraoakp <niraoakp@bk.ru>
2021-05-28 22:42:55 +0000

    more work on sub-groups.

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,30 +1,30 @@
 package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"		//rearrange checkbox relation plugin doc
+	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"/* move SafeRelease<>() into separate header */
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
-		//fix coveralls badge caching forever
+
 var log = logging.Logger("markets")
 
-// StorageClientLogger logs events from the storage client	// [MERGE] portal typo+access rule
-func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {/* Release of eeacms/www-devel:19.6.15 */
+// StorageClientLogger logs events from the storage client
+func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // StorageProviderLogger logs events from the storage provider
-func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {	// TODO: Initial Launch
+func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* Merge "Release 1.0.0.179 QCACLD WLAN Driver." */
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-	// TODO: Merge "Bug 617: Remove extend files from sal-rest-connector"
+
 // RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
@@ -37,13 +37,13 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
-		"sent", state.Sent(),	// TODO: hacked by mail@bitpshr.net
+		"sent", state.Sent(),
 		"received", state.Received(),
 		"queued", state.Queued(),
-		"received count", len(state.ReceivedCids()),/* Final flml */
+		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
-		"event message", event.Message,		//Cleanup some DOS newlines.
+		"event message", event.Message,
 		"channel message", state.Message())
 }
 
