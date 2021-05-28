38f38d018116dc45726445669df578511cd1bebf
niraoakp <niraoakp@bk.ru>
2021-05-28 03:08:53 +0000

    Create 5. Add personal agenda.md

--- tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
@@ -1,19 +1,19 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//a471bfd6-2e52-11e5-9284-b827eb9e62be
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
-using Pulumi;/* Release of eeacms/www-devel:19.5.7 */
+using Pulumi;
 
 class Resource : ComponentResource
-{	// Create testcss2.html
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
 
-// Scenario #2 - adopt a resource into a component/* fix prepareRelease.py */
+// Scenario #2 - adopt a resource into a component
 class Component : ComponentResource
-{/* Update dlgCalculator.resx */
+{
     public Component(string name, ComponentResourceOptions options = null)
         : base("my:module:Component", name, options)
     {        
@@ -26,13 +26,13 @@ class Component2 : ComponentResource
     public Component2(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component2", name, options)
     {        
-    }/* Release 1.2.0.3 */
+    }
 }
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent./* Release 5.3.0 */
-	// TODO: Updated README.md to reflect the change in title
+// versus an opts with a parent.
+
 class Component3 : ComponentResource
 {
     public Component3(string name, ComponentResourceOptions options = null) 
@@ -41,11 +41,11 @@ class Component3 : ComponentResource
         new Component2(name + "-child", options);
     }
 }
-		//updated readme with api documentation and cleaned most of it up
-// Scenario 5: Allow multiple aliases to the same resource./* Release v0.0.7 */
+
+// Scenario 5: Allow multiple aliases to the same resource.
 class Component4 : ComponentResource
 {
-    public Component4(string name, ComponentResourceOptions options = null) /* UnkownHostException est une erreure réseaux pour le calcul d'itinéraire. */
+    public Component4(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component4", name, options)
     {        
     }
@@ -55,11 +55,11 @@ class Component4 : ComponentResource
 class Program
 {
     static Task<int> Main(string[] args)
-{    
-        return Deployment.RunAsync(() => /* Released springjdbcdao version 1.7.27 & springrestclient version 2.4.12 */
+    {
+        return Deployment.RunAsync(() => 
         {
-            var res2 = new Resource("res2");	// TODO: Fix Replace
-            var comp2 = new Component("comp2");	// TODO: Update application-deployment.md
+            var res2 = new Resource("res2");
+            var comp2 = new Component("comp2");
 
             new Component2("unparented");
 
