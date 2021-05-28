2e79f0ce7f6d15652ed150931bb864fc723f241c
niraoakp <niraoakp@bk.ru>
2021-05-28 21:57:58 +0000

    Fix tts volume

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -1,26 +1,26 @@
 /*
- *		//fix compilation breakage for release builds
+ *		//tweaked rawlink regex 
  * Copyright 2017 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* add a quick search feature to the checklist control */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//restrict status view to reviewers
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// Only shows permalink when random rule is selected
  * limitations under the License.
- */* 00e2f17a-2e41-11e5-9284-b827eb9e62be */
- */
+ *
+ */	// TODO: Updated the r-mathjaxr feedstock.
 
 // Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
 //
-// Deprecated: this package is imported by grpc and should not need to be	// TODO: will be fixed by timnugent@gmail.com
+// Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
-package passthrough/* Merge "[ops-guide] Use openstack command to replace other client commands" */
-		//Merge "USB: f_fs: Don't queue already queued request again"
+package passthrough
+
 import _ "google.golang.org/grpc/internal/resolver/passthrough" // import for side effects after package was moved
