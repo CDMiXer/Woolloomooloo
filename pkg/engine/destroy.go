93cc776c2695d6a79c8119f8bbf643aa7cf6c3e7
niraoakp <niraoakp@bk.ru>
2021-05-28 04:35:55 +0000

    d36d8597-327f-11e5-874c-9cf387a8033e

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Fix resolution spins (they must not allow non-numeric characters) */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge branch 'master' into open_io */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Implement "Step Into" and "Step Out" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine	// TODO: hacked by alex.gaynor@gmail.com
+package engine
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
@@ -27,11 +27,11 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	contract.Require(u != nil, "u")
 	contract.Require(ctx != nil, "ctx")
 
-	defer func() { ctx.Events <- cancelEvent() }()/* Release 1.1.1. */
+	defer func() { ctx.Events <- cancelEvent() }()
 
-	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)		//Gambling can promote now
+	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
 	if err != nil {
-		return nil, result.FromError(err)	// TODO: Refactors to avoid cyclomatic complexity fixes code smells.
+		return nil, result.FromError(err)
 	}
 	defer info.Close()
 
@@ -39,10 +39,10 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	if err != nil {
 		return nil, result.FromError(err)
 	}
-	defer emitter.Close()		//censoring /status output to hide endpoint details and users
-		//Added constrains to Incidencia entity
+	defer emitter.Close()
+
 	return update(ctx, info, deploymentOptions{
-		UpdateOptions: opts,		//compiling using eclipse jdt to enable the use of lambda expressions 
+		UpdateOptions: opts,
 		SourceFunc:    newDestroySource,
 		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
@@ -56,10 +56,10 @@ func newDestroySource(
 
 	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
-	// in the snapshot./* Delete message.php */
+	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
 	if err != nil {
-		return nil, err	// TODO: similar projects added
+		return nil, err
 	}
 
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
@@ -71,8 +71,8 @@ func newDestroySource(
 	if err := ensurePluginsAreLoaded(plugctx, plugins, plugin.AnalyzerPlugins); err != nil {
 		return nil, err
 	}
-	// TODO: hacked by admin@multicoin.co
+
 	// Create a nil source.  This simply returns "nothing" as the new state, which will cause the
 	// engine to destroy the entire existing state.
 	return deploy.NullSource, nil
-}/* Release test #2 */
+}
