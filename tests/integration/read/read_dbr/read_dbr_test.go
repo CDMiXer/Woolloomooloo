34872f665b0ea98acb9a239eaa188b3c85023faa
niraoakp <niraoakp@bk.ru>
2021-05-28 13:13:33 +0000

    Fix: Missing css style

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all/* Delete edgebox.py */
 
-package ints/* Release of 1.0.1 */
+package ints
 
 import (
 	"testing"
-	// TODO: bumped to version 10.1.17
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//Merge branch 'develop' into docs-0.4.2
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestReadDBR(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",		//Create Unique Number of Occurrences.java
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Fix README.md API example */
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,	// Delete .DS_Store files
-		EditDirs: []integration.EditDir{		//Atributo styleClass
+		Quick:        true,
+		EditDirs: []integration.EditDir{	// TODO: UI: Policy upload: Nicer button, proper multipart/form-data content-type
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-			{
+			{/* Merge "Release 1.0.0.183 QCACLD WLAN Driver" */
 				Dir:      "step3",
 				Additive: true,
 			},
 		},
 	})
-}		//Bug fix (issue 225 - code.google). Trouble with m4a.
+}
