cb3cdac4422b904fc1f8384f00c7413be2b41290
niraoakp <niraoakp@bk.ru>
2021-05-28 16:41:26 +0000

    Support React v0.4 (keeping BC with v0.3)

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-
+/* Release version 0.26 */
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)
+,secret_name              VARCHAR(500)/* disentanglement of configuration */
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);		//Correcting linter errors
 
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-
+/* Release of eeacms/www:19.9.11 */
 -- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
