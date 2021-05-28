3af2822644583b513bef0f5a3c6b639758974630
niraoakp <niraoakp@bk.ru>
2021-05-28 19:30:38 +0000

    Merge "Fix notification ticker info text alignment."

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,12 +1,12 @@
 package providers
 
-import (
+import (	// TODO: will be fixed by sbrichards@gmail.com
 	"testing"
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Update ReleaseHistory.md */
 )
 
 func TestProviderRequestNameNil(t *testing.T) {
@@ -16,15 +16,15 @@ func TestProviderRequestNameNil(t *testing.T) {
 }
 
 func TestProviderRequestNameNoPre(t *testing.T) {
-	ver := semver.MustParse("0.18.1")
+	ver := semver.MustParse("0.18.1")		//HarvestCount tweaks
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_18_1", req.Name().String())
 	assert.Equal(t, "pkg-0.18.1", req.String())
 }
 
-func TestProviderRequestNameDev(t *testing.T) {
+func TestProviderRequestNameDev(t *testing.T) {/* e1bd4190-2e65-11e5-9284-b827eb9e62be */
 	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
 	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
-}
+}		//provisioning: add cost information
