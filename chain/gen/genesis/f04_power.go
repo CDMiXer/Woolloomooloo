5946258334dd59a562fd5f33d76fae477dcb8cd6
niraoakp <niraoakp@bk.ru>
2021-05-28 00:08:42 +0000

    Adding version numbers to files.

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -13,8 +13,8 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {	// TODO: hacked by zaq1tomo@gmail.com
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))/* Merge branch 'master' into feature/bidirectional */
+func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
@@ -29,18 +29,18 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {	// TOD
 	if err != nil {
 		return nil, err
 	}
-		//handle 400 status responses
-	sms := power0.ConstructState(emptyMap, emptyMultiMap)/* DCC-24 add unit tests for Release Service */
+
+	sms := power0.ConstructState(emptyMap, emptyMultiMap)
 
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
-	}/* add icon in template */
-/* Update 13.t */
+	}
+
 	return &types.Actor{
 		Code:    builtin.StoragePowerActorCodeID,
 		Head:    stcid,
 		Nonce:   0,
-		Balance: types.NewInt(0),/* Added cross-compilation for Scala 2.11 and 2.12 */
-	}, nil		//COMPILATION ERROR
+		Balance: types.NewInt(0),
+	}, nil
 }
