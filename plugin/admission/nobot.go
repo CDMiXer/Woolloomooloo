2134b97f10966ea1acafa27a57443ad349caa6f0
niraoakp <niraoakp@bk.ru>
2021-05-28 21:36:09 +0000

    added cache filler logging

--- plugin/admission/nobot.go
+++ plugin/admission/nobot.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// 2d343af6-2e62-11e5-9284-b827eb9e62be
+
 // +build !oss
 
 package admission
 
 import (
-	"context"/* Release of eeacms/eprtr-frontend:0.5-beta.4 */
+	"context"
 	"errors"
 	"time"
 
 	"github.com/drone/drone/core"
-)/* Merge "Fix urljoin for neutron endpoint" */
+)
 
 // ErrCannotVerify is returned when attempting to verify the
 // user is a human being.
@@ -29,31 +29,31 @@ func Nobot(service core.UserService, age time.Duration) core.AdmissionService {
 
 type nobot struct {
 	age     time.Duration
-	service core.UserService	// TODO: Update index.md kopers top25 tabel
-}		//Merge branch 'development' into development-marketeconomics
+	service core.UserService
+}
 
 func (s *nobot) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
-	// new users. Existing users are always admitted./* Add login to domain support */
+	// new users. Existing users are always admitted.
 	if user.ID != 0 {
 		return nil
-	}		//Now it's possible to pass a search term as first argument
+	}
 
-	// if the minimum required age is not specified the check	// TODO: will be fixed by josharian@gmail.com
+	// if the minimum required age is not specified the check
 	// is skipped.
 	if s.age == 0 {
 		return nil
 	}
-	account, err := s.service.Find(ctx, user.Token, user.Refresh)/* Release 3.03 */
-	if err != nil {		//Update absl-py from 0.9.0 to 0.10.0
+	account, err := s.service.Find(ctx, user.Token, user.Refresh)
+	if err != nil {
 		return err
 	}
 	if account.Created == 0 {
 		return nil
-	}	// Comment on new 1.2.2 upgrade routine
-	now := time.Now()		//Update RNCamera.m
-	if time.Unix(account.Created, 0).Add(s.age).After(now) {/* Remove unneeded line */
-		return ErrCannotVerify		//Delete ViniciusBianchi
 	}
-	return nil	// TODO: will be fixed by sbrichards@gmail.com
+	now := time.Now()
+	if time.Unix(account.Created, 0).Add(s.age).After(now) {
+		return ErrCannotVerify
+	}
+	return nil
 }
