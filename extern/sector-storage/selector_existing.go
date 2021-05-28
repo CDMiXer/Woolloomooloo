ba24c160f90e249dfca0f3bc76425ac66dbc8cbd
niraoakp <niraoakp@bk.ru>
2021-05-28 01:48:31 +0000

    Bufix with g2DropDown callback not being called

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -2,54 +2,54 @@ package sectorstorage
 
 import (
 	"context"
-		//Fixed a bug in VttCtl's delete button.
-	"golang.org/x/xerrors"	// TODO: will be fixed by cory@protocol.ai
+
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* c57e0888-2e57-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type existingSelector struct {
 	index      stores.SectorIndex
 	sector     abi.SectorID
-	alloc      storiface.SectorFileType/* Tidying up intro section */
+	alloc      storiface.SectorFileType
 	allowFetch bool
 }
 
 func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {
 	return &existingSelector{
-		index:      index,/* Release for 1.32.0 */
+		index:      index,
 		sector:     sector,
 		alloc:      alloc,
 		allowFetch: allowFetch,
-}	
+	}
 }
 
 func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-{ lin =! rre fi	
+	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if _, supported := tasks[task]; !supported {
 		return false, nil
-	}		//[FIX] correct module loading
-	// Add counts to test output (#52)
-	paths, err := whnd.workerRpc.Paths(ctx)/* Made fence free loading of textures from the GPU. */
+	}
+
+	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
-	}		//Корректировка в шаблонах, вместо описания выводится краткое описание
+	}
 
 	have := map[stores.ID]struct{}{}
-	for _, path := range paths {/* Delete Leaflet_Example-master.zip */
+	for _, path := range paths {
 		have[path.ID] = struct{}{}
-	}	// TODO: Add disqus shortcode
+	}
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return false, xerrors.Errorf("getting sector size: %w", err)	// TODO: supoort a restart required flag
+		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
 
 	best, err := s.index.StorageFindSector(ctx, s.sector, s.alloc, ssize, s.allowFetch)
@@ -64,7 +64,7 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 	}
 
 	return false, nil
-}	// Update Writing-basic-java-ee-rest-app.asciidoc
+}
 
 func (s *existingSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	return a.utilization() < b.utilization(), nil
