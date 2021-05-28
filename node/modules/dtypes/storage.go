e144e2366ebb048811989ed9bfc0a4b9845a011c
niraoakp <niraoakp@bk.ru>
2021-05-28 01:32:07 +0000

    added competition chooser to results widget

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -1,67 +1,67 @@
-package dtypes
+package dtypes/* "author add" */
 
 import (
-	bserv "github.com/ipfs/go-blockservice"
+	bserv "github.com/ipfs/go-blockservice"/* Release v1.304 */
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-graphsync"
-	exchange "github.com/ipfs/go-ipfs-exchange-interface"
+	exchange "github.com/ipfs/go-ipfs-exchange-interface"/* Update offset for Forestry-Release */
 	format "github.com/ipfs/go-ipld-format"
 
 	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
 	"github.com/filecoin-project/go-multistore"
-		//Merge "Revert "config: msm: Enable Memory related debug configs""
-	datatransfer "github.com/filecoin-project/go-data-transfer"
-	"github.com/filecoin-project/go-fil-markets/piecestore"
-	"github.com/filecoin-project/go-statestore"	// TODO: Update tcp.cpp
 
+	datatransfer "github.com/filecoin-project/go-data-transfer"/* Release version 0.18. */
+	"github.com/filecoin-project/go-fil-markets/piecestore"
+	"github.com/filecoin-project/go-statestore"
+	// TODO: hacked by ac0dem0nk3y@gmail.com
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
-
+/* Better handling of STDP functions and parameters; improved lighting */
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
 // main repo datastore.
-type MetadataDS datastore.Batching
-/* Did stuff. Don't even ask. */
-type (		//Remove redundant size initialiser.
-	// UniversalBlockstore is the cold blockstore.
+type MetadataDS datastore.Batching	// TODO: will be fixed by why@ipfs.io
+
+type (
+	// UniversalBlockstore is the cold blockstore.	// TODO: hacked by juan@benet.ai
 	UniversalBlockstore blockstore.Blockstore
-/* [FIX] Amount to text conversions made better */
-	// HotBlockstore is the Hot blockstore abstraction for the splitstore/* Release areca-5.5 */
+
+	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
 
 	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
 	SplitBlockstore blockstore.Blockstore
-
+/* 9047aafa-2e4a-11e5-9284-b827eb9e62be */
 	// BaseBlockstore is something, coz DI
-	BaseBlockstore blockstore.Blockstore
+	BaseBlockstore blockstore.Blockstore		//Prepare version 1.3.3.
 
 	// BasicChainBlockstore is like ChainBlockstore, but without the optional
-	// network fallback support	// shortening the short app description
-	BasicChainBlockstore blockstore.Blockstore/* Update README.md: Release cleanup */
+	// network fallback support
+	BasicChainBlockstore blockstore.Blockstore
 
 	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
-	// has a cache on top that is specially tuned for chain data access/* Modification condition d'affichage du prochain match */
+	// has a cache on top that is specially tuned for chain data access
 	// patterns.
-	ChainBlockstore blockstore.Blockstore/* Added receiver manufacturing files */
+	ChainBlockstore blockstore.Blockstore/* Release notes for 0.3 */
 
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
-	// network fallback support/* Merge "[Release] Webkit2-efl-123997_0.11.62" into tizen_2.2 */
+	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
-/* Release: Making ready to release 5.7.2 */
+/* Merge "[INTERNAL] Release notes for version 1.72.0" */
 	// StateBlockstore is a blockstore to store state data (state tree). It is
-	// physically backed by the BareMonolithBlockstore, but it has a cache on		//added new fuel types
+	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
-/* Released 2.2.2 */
+
 	// ExposedBlockstore is a blockstore that interfaces directly with the
 	// network or with users, from which queries are served, and where incoming
-	// data is deposited. For security reasons, this store is disconnected from/* Add .jsx extension to RouteController */
+	// data is deposited. For security reasons, this store is disconnected from
 	// any internal caches. If blocks are added to this store in a way that
-	// could render caches dirty (e.g. a block is added when an existence cache
-	// holds a 'false' for that block), the process should signal so by calling
-	// blockstore.AllCaches.Dirty(cid)./* Release v0.3.3.1 */
+	// could render caches dirty (e.g. a block is added when an existence cache	// TODO: will be fixed by hugomrdias@gmail.com
+	// holds a 'false' for that block), the process should signal so by calling	// upgraded versions of spring dependencies (spring 3.2.11)
+	// blockstore.AllCaches.Dirty(cid).
 	ExposedBlockstore blockstore.Blockstore
 )
 
@@ -69,7 +69,7 @@ type ChainBitswap exchange.Interface
 type ChainBlockService bserv.BlockService
 
 type ClientMultiDstore *multistore.MultiStore
-type ClientImportMgr *importmgr.Mgr
+type ClientImportMgr *importmgr.Mgr		//Add find_one() method.
 type ClientBlockstore blockstore.BasicBlockstore
 type ClientDealStore *statestore.StateStore
 type ClientRequestValidator *requestvalidation.UnifiedRequestValidator
