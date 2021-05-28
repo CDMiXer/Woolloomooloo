f314524af8ae9529714513a6062b43cad91276c2
niraoakp <niraoakp@bk.ru>
2021-05-28 18:16:35 +0000

    Fixes for milestone 1.7.3 and updates to test automation.

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-		//Move file gcp-compute-engine-vms.png to images/gcp-compute-engine-vms.png
+
 import "github.com/filecoin-project/go-state-types/network"
 
 type NetworkVersion = network.Version
