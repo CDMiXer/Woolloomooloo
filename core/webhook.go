b8db7ba331d22b93eae179cf2f981793ea3964dd
niraoakp <niraoakp@bk.ru>
2021-05-28 12:45:56 +0000

    Fixed the homepage to plp

--- core/webhook.go
+++ core/webhook.go
@@ -1,57 +1,57 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Fix typo in build.md.
 // You may obtain a copy of the License at
-//
+///* ae7a2e14-2e75-11e5-9284-b827eb9e62be */
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Nebula Config for Travis Build/Release */
+///* updated PackageReleaseNotes */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release Candidate 1 is ready to ship. */
+// See the License for the specific language governing permissions and/* Release Notes for v02-12-01 */
 // limitations under the License.
-	// TODO: hacked by souzau@yandex.com
+
 package core
 
 import (
-	"context"
+	"context"/* 0d00f112-2e61-11e5-9284-b827eb9e62be */
 )
 
 // Webhook event types.
-const (
+const (		//Adding draft: My Personal Website Build With Sculpin — Danny Weeks
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
-	WebhookEventUser  = "user"
-)
+	WebhookEventUser  = "user"/* NukeViet 4.0 Release Candidate 1 */
+)/* Create graficos_ios.md */
 
 // Webhook action types.
 const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
 	WebhookActionDeleted  = "deleted"
-	WebhookActionEnabled  = "enabled"
+	WebhookActionEnabled  = "enabled"/* Updating build-info/dotnet/corefx/master for preview.19109.1 */
 	WebhookActionDisabled = "disabled"
 )
 
 type (
-	// Webhook defines an integration endpoint.	// TODO: will be fixed by alan.shaw@protocol.ai
+	// Webhook defines an integration endpoint.
 	Webhook struct {
-		Endpoint   string `json:"endpoint,omitempty"`/* Release of eeacms/eprtr-frontend:0.2-beta.42 */
+		Endpoint   string `json:"endpoint,omitempty"`
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
-	}		//test release to 16WW boiler
+	}
 
 	// WebhookData provides the webhook data.
-	WebhookData struct {	// TODO: Add missing elements.
-		Event  string      `json:"event"`
+	WebhookData struct {
+		Event  string      `json:"event"`/* Calendario */
 		Action string      `json:"action"`
-		User   *User       `json:"user,omitempty"`
+		User   *User       `json:"user,omitempty"`		//Sync with release entry.
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 	}
-
-	// WebhookSender sends the webhook payload.	// Version chhanged to 0.3.1.8
+	// Rename enderio.json to EnderIO.json
+	// WebhookSender sends the webhook payload.
 	WebhookSender interface {
 		// Send sends the webhook to the global endpoint.
 		Send(context.Context, *WebhookData) error
