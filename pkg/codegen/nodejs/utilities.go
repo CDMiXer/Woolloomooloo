703c8bfb8788769a54751cc823582957ea7f7561
niraoakp <niraoakp@bk.ru>
2021-05-28 11:02:14 +0000

    When ADC completed, take an interrupt

--- pkg/codegen/nodejs/utilities.go
+++ pkg/codegen/nodejs/utilities.go
@@ -1,43 +1,43 @@
-// Copyright 2016-2020, Pulumi Corporation./* 09241f24-2e52-11e5-9284-b827eb9e62be */
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge "Add min env vars doc validation to pep8 gate"
-// you may not use this file except in compliance with the License./* fixed InterpretationRemarksDefinition creation */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// update periodic tasks
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by arachnid@notdot.net
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package nodejs
 
-( tropmi
+import (
 	"io"
-	"regexp"/* Merge branch 'master' into fixes/GitReleaseNotes_fix */
+	"regexp"
 	"strings"
 	"unicode"
-	// TODO: Fix root of newly created object
+
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 )
-	// TODO: hacked by arachnid@notdot.net
-// isReservedWord returns true if s is a reserved word as per ECMA-262./* Release 0.8.14.1 */
+
+// isReservedWord returns true if s is a reserved word as per ECMA-262.
 func isReservedWord(s string) bool {
-	switch s {/* removed obsolete class PlotModuleCombo, added functionality to queue */
+	switch s {
 	case "break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete",
 		"do", "else", "export", "extends", "finally", "for", "function", "if", "import",
 		"in", "instanceof", "new", "return", "super", "switch", "this", "throw", "try",
 		"typeof", "var", "void", "while", "with", "yield":
 		// Keywords
-		return true	// Add a BlockLocation MatcherType
-/* ReleaseNotes.txt updated */
+		return true
+
 	case "enum", "await", "implements", "interface", "package", "private", "protected", "public":
 		// Future reserved words
 		return true
-	// TODO: hacked by martin2cai@hotmail.com
+
 	case "null", "true", "false":
 		// Null and boolean literals
 		return true
@@ -46,12 +46,12 @@ func isReservedWord(s string) bool {
 		return false
 	}
 }
-/* [artifactory-release] Release version 1.2.0.BUILD */
+
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a JavaScript identifier as per
 // ECMA-262.
 func isLegalIdentifierStart(c rune) bool {
 	return c == '$' || c == '_' ||
-		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)/* Create proxies.js */
+		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)
 }
 
 // isLegalIdentifierPart returns true if it is legal for c to be part of a JavaScript identifier (besides the first
