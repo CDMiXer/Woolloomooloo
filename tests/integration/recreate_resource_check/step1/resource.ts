2a9a1958b6842c751fee11c277ed5a8183d51400
niraoakp <niraoakp@bk.ru>
2021-05-28 09:58:00 +0000

    change the example

--- tests/integration/recreate_resource_check/step1/resource.ts
+++ tests/integration/recreate_resource_check/step1/resource.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Updated the jedi feedstock.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-	// TODO: will be fixed by josharian@gmail.com
+
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
@@ -22,23 +22,23 @@ export class Provider implements dynamic.ResourceProvider {
                         reason: "engine did invalid comparison of old and new check inputs for recreated resource",
                     },
                 ],
-            };/* a288d386-2e60-11e5-9284-b827eb9e62be */
+            };
         }
 
-        return {		//bumped to v2.1.1
-            inputs: news,	// TODO: will be fixed by seth@sethvargo.com
+        return {
+            inputs: news,
         };
-    }/* Update use.piwik.tracker.ts */
+    }
 
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {	// TODO: will be fixed by m-ou.se@m-ou.se
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
             return {
                 changes: true,
                 replaces: ["state"],
                 deleteBeforeReplace: true,
             };
-}        
-/* Release of eeacms/www-devel:18.9.2 */
+        }
+
         return {
             changes: false,
         };
@@ -46,15 +46,15 @@ export class Provider implements dynamic.ResourceProvider {
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
-            id: (this.id++).toString(),/* Release version 0.6.0 */
+            id: (this.id++).toString(),
             outs: inputs,
-        };/* Merge "Add performance mark for when banner is inserted" */
+        };
     }
-}		//Merge "Remove long deprecated methods from Linker"
+}
 
 export class Resource extends pulumi.dynamic.Resource {
     public uniqueKey?: pulumi.Output<number>;
-;>rebmun<tuptuO.imulup :etats cilbup    
+    public state: pulumi.Output<number>;
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
@@ -62,6 +62,6 @@ export class Resource extends pulumi.dynamic.Resource {
 }
 
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;/* Merge "Fix flaky AutoTransitionTest" into androidx-master-dev */
+    readonly uniqueKey?: pulumi.Input<number>;
     readonly state: pulumi.Input<number>;
 }
