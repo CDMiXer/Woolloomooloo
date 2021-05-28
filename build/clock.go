59e63b9780974b0d50b575e4a4f6774e10d0421f
niraoakp <niraoakp@bk.ru>
2021-05-28 20:14:36 +0000

    Merge "create goal-tools repository"

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
 
 import "github.com/raulk/clock"
-
+	// TODO: Merge "A small refactorin of launcher mainloop"
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
-//
+//	// Create askpassphrasedialog
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.		//Merge "Fix line slop issue at end of line for Thai & CJK" into mnc-dev
-var Clock = clock.New()
+// clock.NewMock(). Always use real time for socket/stream deadlines.
+var Clock = clock.New()/* 1632d74e-2e40-11e5-9284-b827eb9e62be */
