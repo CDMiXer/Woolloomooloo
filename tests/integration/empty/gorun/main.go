b963d3f56828e452ab4a7fb125c162411cbcee9c
niraoakp <niraoakp@bk.ru>
2021-05-28 20:29:02 +0000

    Trunk: add images and rename to SnAPhylApp.

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//847fa494-2e4e-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main		//float values for timings
+package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// 157800f4-2e72-11e5-9284-b827eb9e62be
+)		//#fix Исправлена инструкция
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* devops-edit --pipeline=dotnet/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
+	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: will be fixed by mikeal.rogers@gmail.com
 		return nil
-	})
+	})/* Create dkim_policy.pl */
 }
