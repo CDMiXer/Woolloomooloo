e1330f93d15912225c3335c25134d4157fffa9d9
niraoakp <niraoakp@bk.ru>
2021-05-28 10:30:27 +0000

    Update CalCentral specific commands

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -7,17 +7,17 @@ class Resource extends pulumi.ComponentResource {
         super("my:module:Resource", name, {}, opts);
     }
 }
-
+	// TODO: Create careers.yml
 // Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
     resource2: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentThree", name, {}, opts);	// Use shorter SSH ControlPath.
+        super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, {parent: this});
-        this.resource2 = new Resource("otherchild", {parent: this});	// Merge "Pass in `region_name` to get correct heat endpoint"
+        this.resource2 = new Resource("otherchild", {parent: this});
     }
 }
 const comp3 = new ComponentThree("comp3");
