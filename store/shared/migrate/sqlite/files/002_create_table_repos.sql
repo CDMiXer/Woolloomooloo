8a187cebd62a6a6c726763a1f9795278788d6e79
niraoakp <niraoakp@bk.ru>
2021-05-28 00:23:17 +0000

    1cf61682-2e49-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -1,34 +1,34 @@
--- name: create-table-repos/* Release 1.4.0.4 */
+-- name: create-table-repos
 
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
 ,repo_uid                   TEXT
-,repo_user_id               INTEGER
+,repo_user_id               INTEGER/* 0eaff640-2e69-11e5-9284-b827eb9e62be */
 ,repo_namespace             TEXT
 ,repo_name                  TEXT
-,repo_slug                  TEXT		//py3 dict_values -> list
+,repo_slug                  TEXT
 ,repo_scm                   TEXT
-,repo_clone_url             TEXT
-,repo_ssh_url               TEXT
+,repo_clone_url             TEXT		//Updated save to use *args,**kwargs syntax.
+,repo_ssh_url               TEXT/* Delete ModuloEstoques01.png */
 ,repo_html_url              TEXT
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            TEXT
-,repo_branch                TEXT	// TODO: Update category-parent-index.html
+,repo_branch                TEXT
 ,repo_counter               INTEGER
 ,repo_config                TEXT
 ,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN	// TODO: Arrumar Identação
+,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
-,repo_version               INTEGER
-,repo_signer                TEXT/* Merge "Modify midonet plugin to support the latest MidoNet" */
-,repo_secret                TEXT
-,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)
-);	// TODO: will be fixed by aeongrp@outlook.com
+,repo_version               INTEGER/* Embrace the moondragon :crescent_moon::dragon: */
+,repo_signer                TEXT
+,repo_secret                TEXT/* Delete hulu.py */
+,UNIQUE(repo_slug)	// TODO: will be fixed by aeongrp@outlook.com
+,UNIQUE(repo_uid)/* Release 3.2.0.M1 profiles */
+);
 
 -- name: alter-table-repos-add-column-no-fork
 
@@ -36,12 +36,12 @@ ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
 
 -- name: alter-table-repos-add-column-no-pulls
 
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;/* Fix a bug in getEmptyLocalSlot that would let us reuse the same ID over and over */
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;
 
--- name: alter-table-repos-add-column-cancel-pulls	// Updated member monthly fees
+-- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
-/* Remove duplicate large FOSSA badge */
+
 -- name: alter-table-repos-add-column-cancel-push
-	// add tabix for validation on chr22
+
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;
