8dcb5ba8e906c98cebdaa225c8c5af246361740d
niraoakp <niraoakp@bk.ru>
2021-05-28 14:28:45 +0000

    Merge branch 'master' into adfinis-sygroup-users-page

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -2,20 +2,20 @@
 
 package types
 
-import "bytes"	// TODO: Fix state parameter check typo
-		//README: Add RKE Vagrant
+import "bytes"
+
 func FuzzMessage(data []byte) int {
 	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		return 0
-	}/* Release version 1.2.4 */
+	}
 	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
 	}
 	var msg2 Message
-	err = msg2.UnmarshalCBOR(bytes.NewReader(data))		//[#433] Mirage: some template fixes
+	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		panic(err) // ok
 	}
@@ -24,7 +24,7 @@ func FuzzMessage(data []byte) int {
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
-		panic("reencoding not equal") // ok/* [artifactory-release] Release version 1.3.0.M6 */
+		panic("reencoding not equal") // ok
 	}
 	return 1
-}		//Merge "Unify render of interface/bond view header"
+}
