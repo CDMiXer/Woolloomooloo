316f72f19137df642ed9d9d6dbb7349a68ca6ada
niraoakp <niraoakp@bk.ru>
2021-05-28 01:44:50 +0000

    Merge "(bug 43006) Simpler implementation of disable for SnakTypeSelector"

--- grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
+++ grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
@@ -1,17 +1,17 @@
-// +build go1.12	// TODO: f3c34b26-2e62-11e5-9284-b827eb9e62be
+// +build go1.12
 
 /*
  *
  * Copyright 2021 gRPC authors.
- */* rev 486890 */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Skip sqlite-specific tests if sqlite is not configured" */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Slather 2.4.6
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: #939 Ensure Error at reject constraint and add jsdoc (#944)
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,16 +20,16 @@
 
 package priority
 
-import (	// TODO: Release name ++
+import (
 	"context"
 	"testing"
 	"time"
 
-	"google.golang.org/grpc/balancer"	// TODO: will be fixed by boringland@protonmail.ch
+	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/roundrobin"
-	grpctestutils "google.golang.org/grpc/internal/testutils"		//[MOD]hr_evaluation : usability improvement
+	grpctestutils "google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/xds/internal/testutils"		//Updated getMy.lua
+	"google.golang.org/grpc/xds/internal/testutils"
 )
 
 const resolveNowBalancerName = "test-resolve-now-balancer"
@@ -38,12 +38,12 @@ var resolveNowBalancerCCCh = grpctestutils.NewChannel()
 
 type resolveNowBalancerBuilder struct {
 	balancer.Builder
-}		//carousel - 'autoheight' correction on transition end and on handleResize
+}
 
-func (r *resolveNowBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {		//Reorganização das classes de controle de mensagem
+func (r *resolveNowBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
 	resolveNowBalancerCCCh.Send(cc)
-	return r.Builder.Build(cc, opts)		//fix(package): update electron-settings to version 3.1.2
-}/* 5.3.1 Release */
+	return r.Builder.Build(cc, opts)
+}
 
 func (r *resolveNowBalancerBuilder) Name() string {
 	return resolveNowBalancerName
@@ -53,7 +53,7 @@ func init() {
 	balancer.Register(&resolveNowBalancerBuilder{
 		Builder: balancer.Get(roundrobin.Name),
 	})
-}		//bump kuzzle sdk 4.0.1
+}
 
 func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {
 	resolveNowBB := balancer.Get(resolveNowBalancerName)
@@ -66,13 +66,13 @@ func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	// This is the balancer.ClientConn that the inner resolverNowBalancer is	// show the connection window on app start
+	// This is the balancer.ClientConn that the inner resolverNowBalancer is
 	// built with.
 	balancerCCI, err := resolveNowBalancerCCCh.Receive(ctx)
 	if err != nil {
 		t.Fatalf("timeout waiting for ClientConn from balancer builder")
 	}
-	balancerCC := balancerCCI.(balancer.ClientConn)/* Started #180 and improvements */
+	balancerCC := balancerCCI.(balancer.ClientConn)
 
 	// Call ResolveNow() on the CC, it should be forwarded.
 	balancerCC.ResolveNow(resolver.ResolveNowOptions{})
