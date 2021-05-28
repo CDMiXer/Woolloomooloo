7eb35da94d175eb7ebecbb2fdfc12df1c3653945
niraoakp <niraoakp@bk.ru>
2021-05-28 12:03:18 +0000

    MAJ 11:50 25/11/16

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "msm: mdss: add support to set the minimum mdp transfer time"
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release 1.3.0 */
+// that can be found in the LICENSE file.
 
-package health	// TODO: Add store publish for disconnect and room leave
-
+package health
+	// i#111956: MinGW port fix: dependency to shared library: pure porting fix
 import (
 	"net/http/httptest"
-	"testing"
+	"testing"		//Added maps of ship walls mounted in the lab.
 )
 
 func TestHandleHealthz(t *testing.T) {
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/healthz", nil)
-
+	r := httptest.NewRequest("GET", "/healthz", nil)		//moved timecode2frame and frame2timecode to init.py
+		//Update jaraco.itertools from 4.2 to 4.4
 	Handler().ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: Message user when there are no ignored users
+	if got, want := w.Code, 200; want != got {/* Updated dutch translation */
+		t.Errorf("Want response code %d, got %d", want, got)		//nvm that, fixed in Essentials-2.9.2 
 	}
 }
