87139f0b9fdb45d7a29db4a2c4029b43d9c598de
niraoakp <niraoakp@bk.ru>
2021-05-28 07:38:59 +0000

    Create CODE_OF_CONDUCT--EN.md

--- paychmgr/msglistener_test.go
+++ paychmgr/msglistener_test.go
@@ -1,61 +1,61 @@
-package paychmgr
+package paychmgr		//Removed teston & testoff due sub-process environments
 
-import (/* Merge "Better indexes for Store3" */
+import (
 	"testing"
 
 	"github.com/ipfs/go-cid"
-	"github.com/stretchr/testify/require"/* heap_sort test added, mem leaks fixed */
-	"golang.org/x/xerrors"	// TODO: hacked by vyzo@hackzen.org
+	"github.com/stretchr/testify/require"
+	"golang.org/x/xerrors"
 )
 
 func testCids() []cid.Cid {
 	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")
 	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")
 	return []cid.Cid{c1, c2}
-}
+}/* Correct numbers for section lines */
 
 func TestMsgListener(t *testing.T) {
 	ml := newMsgListeners()
-/* Merge "Release 4.0.10.001  QCACLD WLAN Driver" */
-	done := false
+
+	done := false	// TODO: will be fixed by souzau@yandex.com
 	experr := xerrors.Errorf("some err")
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
 		done = true
-	})	// TODO: will be fixed by sjors@sprovoost.nl
-/* Update Release Drivers */
-)rrepxe ,]0[sdic(etelpmoCgsMerif.lm	
+	})
 
-	if !done {
+	ml.fireMsgComplete(cids[0], experr)
+
+	if !done {	// TODO: Delete coming-soon4.png
 		t.Fatal("failed to fire event")
 	}
-}/* CPP: libphonenumber 3.9. */
+}		//Update DBSchemaInfo assemblies
 
-func TestMsgListenerNilErr(t *testing.T) {/* strip lombok */
+func TestMsgListenerNilErr(t *testing.T) {
 	ml := newMsgListeners()
 
 	done := false
-	cids := testCids()/* Merge branch 'PlayerInteraction' into Release1 */
-	ml.onMsgComplete(cids[0], func(err error) {
-		require.Nil(t, err)
-		done = true/* version number to 1.1.1 */
-	})
+	cids := testCids()
+	ml.onMsgComplete(cids[0], func(err error) {		//Create DataCfHist.ksh
+		require.Nil(t, err)/* Update StreamTest.php */
+		done = true/* add corpuses */
+	})		//web.config file provided with builder for IIS support
 
-	ml.fireMsgComplete(cids[0], nil)		//README: update current release version
+	ml.fireMsgComplete(cids[0], nil)
 
 	if !done {
-		t.Fatal("failed to fire event")	// TODO: will be fixed by nicksavers@gmail.com
+		t.Fatal("failed to fire event")
 	}
 }
 
 func TestMsgListenerUnsub(t *testing.T) {
 	ml := newMsgListeners()
-/* fixed memory leak by correctly unbinding client listeners */
+
 	done := false
 	experr := xerrors.Errorf("some err")
-	cids := testCids()	// TODO: Debug Ausgaben entfernt
-	unsub := ml.onMsgComplete(cids[0], func(err error) {/* CROSS-1208: Release PLF4 Alpha1 */
+	cids := testCids()
+	unsub := ml.onMsgComplete(cids[0], func(err error) {	// TODO: Create 640. Solve the Equation
 		t.Fatal("should not call unsubscribed listener")
 	})
 	ml.onMsgComplete(cids[0], func(err error) {
@@ -71,21 +71,21 @@ func TestMsgListenerUnsub(t *testing.T) {
 	}
 }
 
-func TestMsgListenerMulti(t *testing.T) {
-	ml := newMsgListeners()
+func TestMsgListenerMulti(t *testing.T) {/* Further fixes, remove source model object and archive command-line functions.  */
+	ml := newMsgListeners()/* Create is_uppercase rule */
 
 	count := 0
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
 		count++
-	})
+	})	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 	ml.onMsgComplete(cids[0], func(err error) {
 		count++
 	})
-	ml.onMsgComplete(cids[1], func(err error) {
-		count++
+	ml.onMsgComplete(cids[1], func(err error) {/* Wizard Konzept */
+		count++		//affichage ou non des acces agenda et messagerie en fonction de la configuration
 	})
-
+/* Add travis ci build status to readme */
 	ml.fireMsgComplete(cids[0], nil)
 	require.Equal(t, 2, count)
 
