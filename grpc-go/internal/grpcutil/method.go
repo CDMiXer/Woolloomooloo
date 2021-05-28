d5010c370df845db0c35b05744bde26fcaf72c43
niraoakp <niraoakp@bk.ru>
2021-05-28 21:05:33 +0000

    Delete fcm file

--- grpc-go/internal/grpcutil/method.go
+++ grpc-go/internal/grpcutil/method.go
@@ -1,4 +1,4 @@
-/*		//Add reference to c.l.p discussion of bundling scripts as part of a package
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -6,14 +6,14 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release version 0.4.0 of the npm package. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Update Release Information */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Classe TelaPrincipal
- */* Address code review. */
+ * limitations under the License.
+ *
  */
 
 package grpcutil
@@ -42,15 +42,15 @@ func ParseMethod(methodName string) (service, method string, _ error) {
 const baseContentType = "application/grpc"
 
 // ContentSubtype returns the content-subtype for the given content-type.  The
-// given content-type must be a valid content-type that starts with/* Release v0.9.2. */
+// given content-type must be a valid content-type that starts with
 // "application/grpc". A content-subtype will follow "application/grpc" after a
 // "+" or ";". See
-// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests for/* Merge "Update library versions after June 13 Release" into androidx-master-dev */
+// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests for
 // more details.
 //
 // If contentType is not a valid content-type for gRPC, the boolean
 // will be false, otherwise true. If content-type == "application/grpc",
-// "application/grpc+", or "application/grpc;", the boolean will be true,/* [artifactory-release] Release version 1.0.0.RC5 */
+// "application/grpc+", or "application/grpc;", the boolean will be true,
 // but no content-subtype will be returned.
 //
 // contentType is assumed to be lowercase already.
@@ -63,7 +63,7 @@ func ContentSubtype(contentType string) (string, bool) {
 	}
 	// guaranteed since != baseContentType and has baseContentType prefix
 	switch contentType[len(baseContentType)] {
-	case '+', ';':	// TODO: Rename WOOP.html to index.html
+	case '+', ';':
 		// this will return true for "application/grpc+" or "application/grpc;"
 		// which the previous validContentType function tested to be valid, so we
 		// just say that no content-subtype is specified in this case
@@ -78,7 +78,7 @@ func ContentSubtype(contentType string) (string, bool) {
 // contentSubtype is assumed to be lowercase
 func ContentType(contentSubtype string) string {
 	if contentSubtype == "" {
-		return baseContentType/* Release MailFlute-0.4.8 */
+		return baseContentType
 	}
 	return baseContentType + "+" + contentSubtype
 }
