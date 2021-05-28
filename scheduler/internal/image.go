25a851804585241954ef981f821212073e99e0a2
niraoakp <niraoakp@bk.ru>
2021-05-28 01:30:18 +0000

    Move script to seperate file

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Release of eeacms/eprtr-frontend:0.5-beta.3 */
+// that can be found in the LICENSE file.		//translation save
+
 // +build !oss
-/* content.php: remove load_contact_links() */
-package internal	// TODO: complete ':set inv' with boolean options
-	// TODO: will be fixed by mail@bitpshr.net
+
+package internal
+/* Remove prefix usage. Release 0.11.2. */
 var defaultImage = "drone/controller:1"
 
 // DefaultImage returns the default dispatch image if none
-// is specified.
-func DefaultImage(image string) string {
+// is specified./* Released springjdbcdao version 1.9.7 */
+func DefaultImage(image string) string {/* Release 1.0 RC1 */
 	if image == "" {
 		return defaultImage
-	}/* Create SubdomainInstallShell.php */
+	}
 	return image
 }
