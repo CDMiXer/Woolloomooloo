186ca8b5b0681eaa41b0c239ed78e4e91b741d39
niraoakp <niraoakp@bk.ru>
2021-05-28 06:54:06 +0000

    General commit, including all changes to synchronize between the devices

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -1,37 +1,37 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 1.0.0.120 QCACLD WLAN Driver" */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Update treasure_spec.rb */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release Version 0.96 */
+// limitations under the License.
 
 // +build oss
 
 package builds
-	// TODO: will be fixed by arachnid@notdot.net
+
 import (
 	"net/http"
-	// TODO: hacked by why@ipfs.io
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-		//LyzQW8cf6ve7jM4z84wYcNweNAHEZX7o
+
 var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)/* Correctly handle empty streams */
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-// HandleRollback returns a non-op http.HandlerFunc./* 09286cd2-2e6b-11e5-9284-b827eb9e62be */
+// HandleRollback returns a non-op http.HandlerFunc.
 func HandleRollback(
 	core.RepositoryStore,
-	core.BuildStore,/* add unzip file */
-	core.Triggerer,		//Fixed a bug with -1N
+	core.BuildStore,
+	core.Triggerer,
 ) http.HandlerFunc {
 	return rollbackNotImplemented
 }
