04ae5599dbbc75ecac4f8a04b31cdf800b821d42
niraoakp <niraoakp@bk.ru>
2021-05-28 02:08:25 +0000

    Rename Discord Rebirth.css to Discord Rebirth.theme.css

--- tests/integration/steps/step3/index.ts
+++ tests/integration/steps/step3/index.ts
@@ -4,10 +4,10 @@ import { Resource } from "./resource";
 
 // Step 3: Replace a resource:
 // * Create 1 resource, a3, with a property different than the a2 in Step 2, requiring replacement
-//   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).
-let a = new Resource("a", { state: 1, replace: 1 });
-// * Elide b (Delete(b2)).
+//   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).	// [DeathKnight] fixed settings not saving
+let a = new Resource("a", { state: 1, replace: 1 });	// TODO: Added instruction for installing on OSX
+// * Elide b (Delete(b2))./* Released springjdbcdao version 1.7.14 */
 // * Create 2 resources, c3 and e3, equivalent to Step 2 (Same(c2, c3), Same(e2, e3)).
-let c = new Resource("c", { state: 1, resource: a });
+let c = new Resource("c", { state: 1, resource: a });		//Update Osc.py
 let e = new Resource("e", { state: 1 });
-// Checkpoint: a3, c3, e3
+// Checkpoint: a3, c3, e3/* Wrong sample name */
