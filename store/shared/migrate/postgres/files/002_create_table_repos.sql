4274fb09fd64640f6c4676f1af02ea1ddd92da83
niraoakp <niraoakp@bk.ru>
2021-05-28 15:54:33 +0000

    Update ColorTest.php

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,5 +1,5 @@
--- name: create-table-repos/* 0.9.9 Release. */
-	// Merge mybuild branch into master
+-- name: create-table-repos
+
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    SERIAL PRIMARY KEY
 ,repo_uid                   VARCHAR(250)
@@ -7,27 +7,27 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)/* Release 0.5.3. */
-,repo_clone_url             VARCHAR(2000)	// TODO: Another formatting fix in the README
+,repo_scm                   VARCHAR(50)
+,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
-,repo_html_url              VARCHAR(2000)	// TODO: will be fixed by nicksavers@gmail.com
+,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER/* x11-plugins/pidgin-fetion: Initial import from my personal overlay. */
-,repo_trusted               BOOLEAN/* Release TomcatBoot-0.3.9 */
-,repo_protected             BOOLEAN		//Merge "Enables Py34 tests for unit.api.openstack.compute.test_server_tags"
+,repo_timeout               INTEGER
+,repo_trusted               BOOLEAN
+,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
-,repo_updated               INTEGER		//delete commented out code
+,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)/* [#1369] Allow zoom in sprite (and button) export */
+,UNIQUE(repo_uid)
 );
 
 -- name: alter-table-repos-add-column-no-fork
@@ -41,7 +41,7 @@ ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 -- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
-/* Don't throw exception when a root is "closed" */
+
 -- name: alter-table-repos-add-column-cancel-push
 
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
