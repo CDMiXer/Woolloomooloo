c657ca7ed6c993502b438c1dd6c768e9b502e1cc
niraoakp <niraoakp@bk.ru>
2021-05-28 15:14:35 +0000

    Added disqussion

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,23 +1,23 @@
-package store		//Merge branch 'master' into ISSUE_5875
+package store
 
 import (
 	"fmt"
-	"testing"		//implements the first simple test
+	"testing"/* Updated with functions for Labels */
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBaseFee(t *testing.T) {	// TODO: Improve mutation coverage (pitest)
+func TestBaseFee(t *testing.T) {
 	tests := []struct {
-		basefee             uint64	// Notificators: return Optional
+		basefee             uint64	// TODO: will be fixed by souzau@yandex.com
 		limitUsed           int64
 		noOfBlocks          int
-		preSmoke, postSmoke uint64
+		preSmoke, postSmoke uint64	// Delete Editor.java~
 	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
-		{100e6, 0, 5, 87.5e6, 87.5e6},
+		{100e6, 0, 5, 87.5e6, 87.5e6},	// default generated code
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
@@ -25,13 +25,13 @@ func TestBaseFee(t *testing.T) {	// TODO: Improve mutation coverage (pitest)
 	}
 
 	for _, test := range tests {
-		test := test/* Stubbed out Deploy Release Package #324 */
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {	// TODO: hacked by fjl@ethereum.org
-			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)/* Fix drawsegment bounding box and hittest issues in pcbnew. */
+		test := test
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
+			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)/* Panning of chart is enabled with Ctrl+Mouse, this closes #12 */
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
-
-			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
+/* Release version changed */
+			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)	// TODO: Update input_scanner.py
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
-		})
-	}
+		})		//Merge "Revert "add range_check for fdct in vp10""
+	}	// Added trExecutives.json to data
 }
