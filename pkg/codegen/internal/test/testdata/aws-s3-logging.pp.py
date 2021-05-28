264b18b9b077c47e87d67ffe0c23d341d40bf8b9
niraoakp <niraoakp@bk.ru>
2021-05-28 12:53:01 +0000

    Geometry/MaterialExporter: Added vertex/face colors support.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
 import pulumi
-import pulumi_aws as aws
+import pulumi_aws as aws		//remove cvsignore files
 
 logs = aws.s3.Bucket("logs")
-(sgrAgniggoLtekcuB.3s.swa[=sgniggol ,"tekcub"(tekcuB.3s.swa = tekcub
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(/* Preparing WIP-Release v0.1.28-alpha-build-00 */
     target_bucket=logs.bucket,
-)])/* 5b845ede-2e4b-11e5-9284-b827eb9e62be */
+)])
 pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
