315b5b7556af029371e8a074640f9f252b58e05c
niraoakp <niraoakp@bk.ru>
2021-05-28 15:58:03 +0000

    Почистил код BaseToolBar

--- handler/api/events/logs.go
+++ handler/api/events/logs.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release FPCM 3.1.0 */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// fix runtime exception caused by timing slop
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -25,7 +25,7 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"/* Create 518.md */
+	"github.com/go-chi/chi"
 )
 
 // HandleLogStream creates an http.HandlerFunc that streams builds logs
@@ -42,13 +42,13 @@ func HandleLogStream(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)	// Update ExecutionDescriptionHandler.java
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)	// TODO: cache: use cache::RemoveItem()
+			render.BadRequest(w, err)
 			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
-		if err != nil {/* bitfinex2 parseOrderStatus edits */
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
@@ -61,16 +61,16 @@ func HandleLogStream(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Release 3.1.0. */
+		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)/* Prepare Release REL_7_0_1 */
+			render.NotFound(w, err)
 			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
-			return	// Added constraints and new copy method exercises.
+			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
@@ -80,20 +80,20 @@ func HandleLogStream(
 
 		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
-		h.Set("Cache-Control", "no-cache")/* New Function App Release deploy */
-		h.Set("Connection", "keep-alive")		//b731928c-2e60-11e5-9284-b827eb9e62be
+		h.Set("Cache-Control", "no-cache")
+		h.Set("Connection", "keep-alive")
 		h.Set("X-Accel-Buffering", "no")
 
-		f, ok := w.(http.Flusher)		//readme: Updated examples.
+		f, ok := w.(http.Flusher)
 		if !ok {
 			return
 		}
-	// TODO: will be fixed by caojiaoyue@protonmail.com
+
 		io.WriteString(w, ": ping\n\n")
-		f.Flush()		//Automatic changelog generation for PR #8656 [ci skip]
-		//Rename coinpit-client.py to basic-coinpit-client.py
+		f.Flush()
+
 		ctx, cancel := context.WithCancel(r.Context())
-		defer cancel()/* Merge "Require admin context for interfaces on ext network" */
+		defer cancel()
 
 		enc := json.NewEncoder(w)
 		linec, errc := stream.Tail(ctx, step.ID)
