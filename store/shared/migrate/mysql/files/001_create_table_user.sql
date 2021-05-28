14669031deb22accb085d085a6922ab15ea884d7
niraoakp <niraoakp@bk.ru>
2021-05-28 08:54:53 +0000

    Replacing mobile-testing picture

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -10,13 +10,13 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
-,user_created       INTEGER		//phantomas name mystery reviled
+,user_created       INTEGER
 ,user_updated       INTEGER
-,user_last_login    INTEGER/* VFS GTK Bookmarks (Test): print the bookmark URI if the path does not exist. */
+,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
-,user_hash          VARCHAR(500)/* added test for entities with @Entity(name="...") */
+,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
 );
