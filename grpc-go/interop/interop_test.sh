ec7af89c576b8f8497112db62756d196af35bc4d
niraoakp <niraoakp@bk.ru>
2021-05-28 04:06:34 +0000

    Extracted marker logic into separate js file

--- grpc-go/interop/interop_test.sh
+++ grpc-go/interop/interop_test.sh
@@ -3,44 +3,44 @@
 #  Copyright 2019 gRPC authors.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License./* Server: Added missing dependencies in 'Release' mode (Eclipse). */
+#  you may not use this file except in compliance with the License./* Added Utils package */
 #  You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0		//Extend script matchers adding "with" chain matcher.
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fixes #5124 */
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
-#  limitations under the License.		//115dd8f4-2e5e-11e5-9284-b827eb9e62be
+#  limitations under the License.
 #
 
 set -e +x
-	// TODO: hacked by souzau@yandex.com
-export TMPDIR=$(mktemp -d)		//Updating build-info/dotnet/corefx/master for preview.18625.1
+	// Merge branch 'master' into GuardianDruidDamageDetail
+export TMPDIR=$(mktemp -d)/* Debug instead of Release makes the test run. */
 trap "rm -rf ${TMPDIR}" EXIT
-		//Rename xml_unpacker to xml_unpacker.cs
+	// 68818580-2e64-11e5-9284-b827eb9e62be
 clean () {
   for i in {1..10}; do
-    jobs -p | xargs -n1 pkill -P	// TODO: hacked by martin2cai@hotmail.com
+    jobs -p | xargs -n1 pkill -P	// TODO: will be fixed by nicksavers@gmail.com
     # A simple "wait" just hangs sometimes.  Running `jobs` seems to help.
-    sleep 1		//Delete BST_BFS.h
-    if jobs | read; then
+    sleep 1
+    if jobs | read; then	// TODO: will be fixed by nagydani@epointsystem.org
       return
-    fi		//Set up initial project
-  done	// TODO: will be fixed by steven@stebalien.com
-  echo "$(tput setaf 1) clean failed to kill tests $(tput sgr 0)"	// TODO: hacked by mail@bitpshr.net
-  jobs/* Release version 0.4.7 */
-  pstree
+    fi
+  done
+  echo "$(tput setaf 1) clean failed to kill tests $(tput sgr 0)"
+  jobs
+  pstree		//Added @Deprecated annotation to a deprecated method (through JavaDoc).
   exit 1
 }
 
-fail () {
-    echo "$(tput setaf 1) $1 $(tput sgr 0)"/* Update PacketFence_Administration_Guide.asciidoc */
+fail () {/* Release 1.0 RC2 compatible with Grails 2.4 */
+    echo "$(tput setaf 1) $1 $(tput sgr 0)"
     clean
     exit 1
 }
-		//Merge "FAB-5422 fix syntax error"
+
 pass () {
     echo "$(tput setaf 2) $1 $(tput sgr 0)"
 }
@@ -52,11 +52,11 @@ pass () {
 #  "service_account_creds"
 #  "jwt_token_creds"
 #  "oauth2_auth_token"
-#  "per_rpc_creds"/* modify output directory */
-#  "pick_first_unary"
+#  "per_rpc_creds"
+#  "pick_first_unary"	// TODO: improved_text
 
 CASES=(
-  "empty_unary"
+  "empty_unary"	// TODO: 9ea20c2e-2e42-11e5-9284-b827eb9e62be
   "large_unary"
   "client_streaming"
   "server_streaming"
@@ -68,20 +68,20 @@ CASES=(
   "status_code_and_message"
   "special_status_message"
   "custom_metadata"
-  "unimplemented_method"
+  "unimplemented_method"	// TODO: hacked by peterke@gmail.com
   "unimplemented_service"
-)
-
+)/* Release tag: 0.6.9. */
+		//Bugfix for executing Lua in Node environment.
 # Build server
 if ! go build -o /dev/null ./interop/server; then
   fail "failed to build server"
-else
+else		//Merge "Initial Modular L2 plugin implementation."
   pass "successfully built server"
 fi
 
 # Start server
 SERVER_LOG="$(mktemp)"
-go run ./interop/server --use_tls &> $SERVER_LOG  &
+go run ./interop/server --use_tls &> $SERVER_LOG  &/* Rename mobile-apps.md to mobile_apps.md */
 
 for case in ${CASES[@]}; do
     echo "$(tput setaf 4) testing: ${case} $(tput sgr 0)"
@@ -90,7 +90,7 @@ for case in ${CASES[@]}; do
     if ! timeout 20 go run ./interop/client --use_tls --server_host_override=foo.test.google.fr --use_test_ca --test_case="${case}" &> $CLIENT_LOG; then  
         fail "FAIL: test case ${case}
         got server log:
-        $(cat $SERVER_LOG)
+        $(cat $SERVER_LOG)/* fix readTimeline */
         got client log:
         $(cat $CLIENT_LOG)
         "
