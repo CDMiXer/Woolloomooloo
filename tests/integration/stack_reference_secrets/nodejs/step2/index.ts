af2dc70fbffbdac8fcefb889519eab1ad1594373
niraoakp <niraoakp@bk.ru>
2021-05-28 00:39:35 +0000

    updating poms for branch'release/10.2.0' with non-snapshot versions

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
-
-export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");
+/* Release for 1.32.0 */
+export const normal = pulumi.output("normal");/* error status codes added */
+export const secret = pulumi.secret("secret");/* eztelemeta: suppress warning caused by empty dc elements */
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
-const stack = pulumi.getStack();
+const stack = pulumi.getStack();	// TODO: Add 2.2.6 tests and allow onResolve and onReject to throw exceptions
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-
+/* check if entities parameter is exist in params array */
 export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");
+export const refSecret = sr.getOutput("secret");	// TODO: hacked by greg@colvin.org
