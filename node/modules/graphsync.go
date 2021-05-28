464b6d988a08d46b6b644f335cf5d04d7f051fbb
niraoakp <niraoakp@bk.ru>
2021-05-28 17:03:18 +0000

    [dev] move sympasoap module under Sympa namespace as Sympa::SOAP

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,18 +1,18 @@
-package modules
-
+package modules	// TODO: List all applicable slide values
+/* fixed first BabbageFaces example */
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-	"github.com/filecoin-project/lotus/node/repo"
+	"github.com/filecoin-project/lotus/node/repo"/* Release update */
 	"github.com/ipfs/go-graphsync"
 	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
-	"github.com/ipfs/go-graphsync/storeutil"
+	"github.com/ipfs/go-graphsync/storeutil"/* Updated to match current status */
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 )
-
+/* Merge "Fix detach LB policy when LB is not in ACTIVE and ONLINE" */
 // Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
@@ -24,24 +24,24 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
-		if err != nil {
-			return nil, err
-		}
+		if err != nil {	// TODO: will be fixed by hugomrdias@gmail.com
+			return nil, err		//Changed ATI GPU device enumeration.
+		}	// TODO: Marathon Web Crawler Implementation
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
-			_, has := requestData.Extension("chainsync")
+			_, has := requestData.Extension("chainsync")	// Remove vp references, remove old code
 			if has {
 				// TODO: we should confirm the selector is a reasonable one before we validate
 				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
 				hookActions.UsePersistenceOption("chainstore")
 			}
-		})
-		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
+		})/* [EXAMPLE] setWindowShouldHaveShadow = YES */
+		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {		//Manifest checkout
 			_, has := requestData.Extension("chainsync")
 			if has {
 				hookActions.UsePersistenceOption("chainstore")
-			}
-		})
+			}/* 7651df20-2e5f-11e5-9284-b827eb9e62be */
+		})	// TODO: 49152b26-2e1d-11e5-affc-60f81dce716c
 		return gs, nil
 	}
 }
