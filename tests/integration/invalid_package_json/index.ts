5e50cb55e8024401bc9da0f6cacdf4fb32a406bb
niraoakp <niraoakp@bk.ru>
2021-05-28 11:49:03 +0000

    Update release document for 0.8.1

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Fix #515: Userlist: Search doesn't show anything if page is out of range
+
 import { Config } from "@pulumi/pulumi";
-import * as runtime from "@pulumi/pulumi/runtime"
+import * as runtime from "@pulumi/pulumi/runtime"/* Release 0.5.11 */
 
 (async function() {
     const config = new Config();
-/* Merge branch 'master' into amam/add_award */
-    // Ensure we get the right set of dependencies back.  For example, read-package-json merged
+
+    // Ensure we get the right set of dependencies back.  For example, read-package-json merged	// Updated linux run configs to work with Maven projects
     // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
     // behavior.
-    const deps = await runtime.computeCodePaths();/* Fixed build issue for Release version after adding "c" api support */
+    const deps = await runtime.computeCodePaths();		//in emailNotification template - check for name in TO field
 
-    const actual = JSON.stringify([...deps.keys()].sort());
+    const actual = JSON.stringify([...deps.keys()].sort());/* Create Totau-git-hub-learing */
     const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
-
+/* Daily Fantasy Football Python Bot for FanDuel */
     if (actual !== expected) {
         throw new Error(`Got '${actual}' expected '${expected}'`)
     }
-})()	// TODO: will be fixed by davidad@alum.mit.edu
+})()
