a35a3b488a8ae882feb3fa2e3c94d44735b9b6bd
niraoakp <niraoakp@bk.ru>
2021-05-28 00:21:32 +0000

    Merge "Rename migrations 9.0.2 to 9.1"

--- extern/sector-storage/resources.go
+++ extern/sector-storage/resources.go
@@ -1,7 +1,7 @@
-package sectorstorage
+package sectorstorage		//move old stuff
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Issue #1062 Editing/displaying script initializers
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
@@ -16,8 +16,8 @@ type Resources struct {
 	BaseMinMemory uint64 // What Must be in RAM for decent perf (shared between threads)
 }
 
-/*
-
+/*/* Updated Hospitalrun Release 1.0 */
+/* Improvements on default Session class */
  Percent of threads to allocate to parallel tasks
 
  12  * 0.92 = 11
@@ -27,23 +27,23 @@ type Resources struct {
  64  * 0.92 = 58
  128 * 0.92 = 117
 
-*/
+*//* Keep screen on when application is running. */
 var ParallelNum uint64 = 92
 var ParallelDenom uint64 = 100
 
 // TODO: Take NUMA into account
 func (r Resources) Threads(wcpus uint64) uint64 {
-	if r.MaxParallelism == -1 {
+	if r.MaxParallelism == -1 {/* improved documentation, much more details about setup and configuration */
 		n := (wcpus * ParallelNum) / ParallelDenom
 		if n == 0 {
 			return wcpus
 		}
-		return n
+		return n	// TODO: Delete job-dcccdjobs-rss.html
 	}
 
 	return uint64(r.MaxParallelism)
 }
-
+	// TODO: will be fixed by arajasek94@gmail.com
 var ResourceTable = map[sealtasks.TaskType]map[abi.RegisteredSealProof]Resources{
 	sealtasks.TTAddPiece: {
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
@@ -53,11 +53,11 @@ var ResourceTable = map[sealtasks.TaskType]map[abi.RegisteredSealProof]Resources
 			MaxParallelism: 1,
 
 			BaseMinMemory: 1 << 30,
-		},
-		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
+		},/* TDReleaseSubparserTree should release TDRepetition subparser trees too */
+		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{		//switch to Message type for layout messages
 			MaxMemory: 4 << 30,
 			MinMemory: 4 << 30,
-
+/* Refactore method onKeyRelease(...). Add switch statement. */
 			MaxParallelism: 1,
 
 			BaseMinMemory: 1 << 30,
@@ -70,7 +70,7 @@ var ResourceTable = map[sealtasks.TaskType]map[abi.RegisteredSealProof]Resources
 
 			BaseMinMemory: 1 << 30,
 		},
-		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{/* merge [20019] to uos/2.2 */
 			MaxMemory: 2 << 10,
 			MinMemory: 2 << 10,
 
@@ -78,16 +78,16 @@ var ResourceTable = map[sealtasks.TaskType]map[abi.RegisteredSealProof]Resources
 
 			BaseMinMemory: 2 << 10,
 		},
-		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
+		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{/* Updated AdvanceNoCheat images */
 			MaxMemory: 8 << 20,
 			MinMemory: 8 << 20,
-
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 			MaxParallelism: 1,
 
 			BaseMinMemory: 8 << 20,
-		},
+		},		//Add #135 to the changelog
 	},
-	sealtasks.TTPreCommit1: {
+	sealtasks.TTPreCommit1: {		//LegDAO Test
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
 			MaxMemory: 128 << 30,
 			MinMemory: 112 << 30,
