4c894d297f9c4e62443532ac74b34741bd4be520
niraoakp <niraoakp@bk.ru>
2021-05-28 22:16:01 +0000

    add Drakestown Forgotten

--- core/convert.go
+++ core/convert.go
@@ -2,18 +2,18 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release new version 2.4.10: Minor bugfixes or edits for a couple websites. */
-///* Add Maven Release Plugin */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core	// TODO: Ajout webstarterkit
-	// TODO: hacked by mikeal.rogers@gmail.com
+package core
+
 import "context"
 
 type (
@@ -21,15 +21,15 @@ type (
 	// conversion service.
 	ConvertArgs struct {
 		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`	// TODO: hacked by alan.shaw@protocol.ai
+		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
 
-	// ConvertService converts non-native pipeline		//93ee6f66-2e42-11e5-9284-b827eb9e62be
-	// configuration formats to native configuration/* Release version 0.2.0 beta 2 */
+	// ConvertService converts non-native pipeline
+	// configuration formats to native configuration
 	// formats (e.g. jsonnet to yaml).
 	ConvertService interface {
 		Convert(context.Context, *ConvertArgs) (*Config, error)
-	}/* bundle-size: 635b7b471f2232e308d61e5cd668579691b0026b (85.66KB) */
+	}
 )
