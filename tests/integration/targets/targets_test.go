2de8f18daa77868a0ad761ac9a54e0872293ae29
niraoakp <niraoakp@bk.ru>
2021-05-28 02:36:17 +0000

    Merge "Uniform $rtl usage in Arabic-script Messages*.php"

--- tests/integration/targets/targets_test.go
+++ tests/integration/targets/targets_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package ints
-	// ce4588aa-2e4e-11e5-9284-b827eb9e62be
+
 import (
 	"os"
 	"path"
@@ -20,26 +20,26 @@ func TestUntargetedCreateDuringTargetedUpdate(t *testing.T) {
 	}
 
 	e := ptesting.NewEnvironment(t)
-	defer func() {		//Update dnsproxy.c
-{ )(deliaF.t! fi		
+	defer func() {
+		if !t.Failed() {
 			e.DeleteEnvironment()
 		}
 	}()
 
 	stackName, err := resource.NewUniqueHex("test-", 8, -1)
-	contract.AssertNoErrorf(err, "resource.NewUniqueHex should not fail with no maximum length is set")/* 17 sep feature */
-/* istream-replace: convert to C++ */
+	contract.AssertNoErrorf(err, "resource.NewUniqueHex should not fail with no maximum length is set")
+
 	e.ImportDirectory("untargeted_create")
-	e.RunCommand("pulumi", "stack", "init", stackName)/* Merge "Release 3.0.10.012 Prima WLAN Driver" */
+	e.RunCommand("pulumi", "stack", "init", stackName)
 	e.RunCommand("yarn", "link", "@pulumi/pulumi")
 	e.RunCommand("pulumi", "up", "--non-interactive", "--skip-preview", "--yes")
-	urn, _ := e.RunCommand("pulumi", "stack", "output", "urn")/* Aggiunto CAD e legge sul diritto d'autore */
+	urn, _ := e.RunCommand("pulumi", "stack", "output", "urn")
 
-	if err := fsutil.CopyFile(/* Initial Release Update | DC Ready - Awaiting Icons */
+	if err := fsutil.CopyFile(
 		path.Join(e.RootPath, "untargeted_create", "index.ts"),
 		path.Join("untargeted_create", "step1", "index.ts"), nil); err != nil {
 
-		t.Fatalf("error copying index.ts file: %v", err)/* Merge "Release 3.2.3.357 Prima WLAN Driver" */
+		t.Fatalf("error copying index.ts file: %v", err)
 	}
 
 	e.RunCommand("pulumi", "up", "--target", strings.TrimSpace(urn), "--non-interactive", "--skip-preview", "--yes")
