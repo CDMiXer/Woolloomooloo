17921faaa515bff8f92ade69b81b83d5974a4106
niraoakp <niraoakp@bk.ru>
2021-05-28 18:07:21 +0000

    If you build it, they will come

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,9 +1,9 @@
 -- name: create-table-cron
 
-CREATE TABLE IF NOT EXISTS cron (	// Merge "Delete unnecessary groupadd in multinode-lab"
- cron_id          SERIAL PRIMARY KEY/* Delete XPloadsion - XPloadsive Love [LDGM Release].mp3 */
-,cron_repo_id     INTEGER
-,cron_name        VARCHAR(50)	// TODO: Merge branch 'develop' into cithomas/tpondefaultsink
+CREATE TABLE IF NOT EXISTS cron (
+ cron_id          SERIAL PRIMARY KEY
+,cron_repo_id     INTEGER	// 7fbf6332-2e6b-11e5-9284-b827eb9e62be
+,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
@@ -13,15 +13,15 @@ CREATE TABLE IF NOT EXISTS cron (	// Merge "Delete unnecessary groupadd in multi
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER
+,cron_version     INTEGER	// TODO: Release jedipus-2.6.43
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
-
--- name: create-index-cron-repo
+);/* Made ReleaseUnknownCountry lazily loaded in Release. */
+/* Piston 0.5 Released */
+-- name: create-index-cron-repo/* display course class on the certificate */
 
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next
+-- name: create-index-cron-next/* Merge "Move Exifinterface to beta for July 2nd Release" into androidx-master-dev */
 
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
