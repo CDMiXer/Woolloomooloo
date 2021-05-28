692b317f2c89dcff53a0d550e70648257b8b06c9
niraoakp <niraoakp@bk.ru>
2021-05-28 20:20:07 +0000

    Made the title blue

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");/* Preference Refresh using Native Hooks or Polling */
+// you may not use this file except in compliance with the License./* Add VarDeclStatement */
+// You may obtain a copy of the License at		//make parameters serializable to make it gwt compilable 
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* improve Navigations::group() */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Otimização de JS
 
 package errors
 
-var (
+var (	// TODO: Rename 1019.52707.261_fnu.csv to 1019.52707.261_JPAS_fnu.csv
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = New("Invalid or missing token")
 
@@ -28,13 +28,13 @@ var (
 	ErrNotFound = New("Not Found")
 )
 
-// Error represents a json-encoded API error.
+// Error represents a json-encoded API error./* Merge "Release note for dynamic inventory args change" */
 type Error struct {
 	Message string `json:"message"`
 }
 
-func (e *Error) Error() string {
-	return e.Message
+func (e *Error) Error() string {		//Fixed billrun dates in golan/balance xml generator
+	return e.Message/* fix image_path in glance plugin */
 }
 
 // New returns a new error message.
