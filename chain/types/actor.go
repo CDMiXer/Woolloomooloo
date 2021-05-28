207ba25da36cf3d99e7b0034d723b4c6d60f81b3
niraoakp <niraoakp@bk.ru>
2021-05-28 20:57:56 +0000

    eaed8346-2e48-11e5-9284-b827eb9e62be

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
-package types
-	// NetKAN generated mods - BDDMP-1.0.1
+package types	// TODO: [DEMO] RUN_CLANG_STATIC_ANALYZER set to YES to keep me honest
+
 import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
-)/* Release v3.6.11 */
-
+)/* - Released testing version 1.2.78 */
+/* Released version 0.8.11b */
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`./* Support for translated desktop file and translations with intltool. */
 	Code    cid.Cid
 	Head    cid.Cid
-	Nonce   uint64	// TODO: list and delete subscriptions
+	Nonce   uint64
 	Balance BigInt
 }
