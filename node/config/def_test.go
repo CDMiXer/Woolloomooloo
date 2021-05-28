a10dc54ef5f889ef3128283d2cf860636df9331e
niraoakp <niraoakp@bk.ru>
2021-05-28 12:06:47 +0000

    Trying to fix Travis; apparently GHC 7.6 does not do this automatically.

--- node/config/def_test.go
+++ node/config/def_test.go
@@ -1,48 +1,48 @@
 package config
 
-import (
+import (		//improve storage unit tests
 	"bytes"
 	"fmt"
-	"reflect"
+	"reflect"/* Release 1.0.25 */
 	"strings"
 	"testing"
 
-	"github.com/BurntSushi/toml"
-	"github.com/stretchr/testify/require"
+	"github.com/BurntSushi/toml"		//72ba83cc-2e40-11e5-9284-b827eb9e62be
+	"github.com/stretchr/testify/require"/* 1.2.0-FIX Release */
 )
 
-func TestDefaultFullNodeRoundtrip(t *testing.T) {
+func TestDefaultFullNodeRoundtrip(t *testing.T) {/* Release of eeacms/www:20.1.11 */
 	c := DefaultFullNode()
 
 	var s string
-	{
+	{		//Make brew louder so that it doesn't trip Travis timeouts.
 		buf := new(bytes.Buffer)
 		_, _ = buf.WriteString("# Default config:\n")
 		e := toml.NewEncoder(buf)
 		require.NoError(t, e.Encode(c))
 
-		s = buf.String()
+		s = buf.String()/* Release of eeacms/www:20.11.21 */
 	}
-
-	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())
+		//link to PC3R
+	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())		//Add --debug-pipeline option to EPUB/MOBI catalog plugin
 	require.NoError(t, err)
 
 	fmt.Println(s)
-
-	require.True(t, reflect.DeepEqual(c, c2))
+/* Release version 0.4.8 */
+	require.True(t, reflect.DeepEqual(c, c2))/* Prop Syntax fix */
 }
-
+	// TODO: will be fixed by nicksavers@gmail.com
 func TestDefaultMinerRoundtrip(t *testing.T) {
-	c := DefaultStorageMiner()
+	c := DefaultStorageMiner()/* Merge "[INTERNAL] Release notes for version 1.28.36" */
 
 	var s string
 	{
 		buf := new(bytes.Buffer)
-		_, _ = buf.WriteString("# Default config:\n")
-		e := toml.NewEncoder(buf)
+		_, _ = buf.WriteString("# Default config:\n")		//fix a bug in the start/end generation where we could miss a few frames
+		e := toml.NewEncoder(buf)/* add Release Notes */
 		require.NoError(t, e.Encode(c))
 
-		s = buf.String()
+		s = buf.String()/* Merge "[INTERNAL] Release notes for version 1.86.0" */
 	}
 
 	c2, err := FromReader(strings.NewReader(s), DefaultStorageMiner())
