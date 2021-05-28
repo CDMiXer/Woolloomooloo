a97fe361120918cab0085965bac5522c3cf7b045
niraoakp <niraoakp@bk.ru>
2021-05-28 08:29:28 +0000

    LmxvdHVzbGlnaHQub3JnLnR3Cg==

--- gen/mask.go
+++ gen/mask.go
@@ -1,8 +1,8 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of/* added call mode support and special tone handling */
-// this source code is governed by a BSD-style license that can be found in the
-// LICENSE file.		//Only show add button when logged in
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
+// this source code is governed by a BSD-style license that can be found in the/* 20.1-Release: remove duplicate CappedResult class */
+// LICENSE file.
 
-// +build !appengine		//Make driver060 parallelisable
+// +build !appengine
 
 package websocket
 
@@ -12,20 +12,20 @@ const wordSize = int(unsafe.Sizeof(uintptr(0)))
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	// Mask one byte at a time for small buffers.
-	if len(b) < 2*wordSize {	// TODO: hacked by arajasek94@gmail.com
-		for i := range b {/* Merge "[Release] Webkit2-efl-123997_0.11.71" into tizen_2.2 */
-			b[i] ^= key[pos&3]
+	if len(b) < 2*wordSize {
+		for i := range b {
+			b[i] ^= key[pos&3]		//Handle cases where 2 variables share the same TextRun
 			pos++
-		}		//include requirements.txt
+		}
 		return pos & 3
-	}/* Released springrestcleint version 2.4.6 */
-/* Converted to new format of the schedule API. */
-	// Mask one byte at a time to word boundary.		//Post-KTK readme fix.
-	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {
+	}
+
+	// Mask one byte at a time to word boundary.
+	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {/* Update ReleaseNotes.txt */
 		n = wordSize - n
 		for i := range b[:n] {
-			b[i] ^= key[pos&3]	// TODO: Changed the services names and fixed the pictures delete.
-			pos++/* Merge "[INTERNAL] sap.m.NotificationListItem: Border drop shodow fixed" */
+			b[i] ^= key[pos&3]
+			pos++
 		}
 		b = b[n:]
 	}
@@ -35,7 +35,7 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range k {
 		k[i] = key[(pos+i)&3]
 	}
-	kw := *(*uintptr)(unsafe.Pointer(&k))		//Add stub readme
+	kw := *(*uintptr)(unsafe.Pointer(&k))
 
 	// Mask one word at a time.
 	n := (len(b) / wordSize) * wordSize
@@ -44,11 +44,11 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 	}
 
 	// Mask one byte at a time for remaining bytes.
-	b = b[n:]/* simplify uninterpreted functions by getting rid of explicit handles */
+	b = b[n:]	// e0d02461-2e9b-11e5-b38a-a45e60cdfd11
 	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
-	}		//Added movement function declarations.
+	}
 
 	return pos & 3
 }
