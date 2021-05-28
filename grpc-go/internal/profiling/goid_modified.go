1c5c2ea6a6d6b1a124e8fa8cf720cd8c4e92a8bf
niraoakp <niraoakp@bk.ru>
2021-05-28 06:52:28 +0000

    Update crash-course.md

--- grpc-go/internal/profiling/goid_modified.go
+++ grpc-go/internal/profiling/goid_modified.go
@@ -1,46 +1,46 @@
-// +build grpcgoid/* rm types.net.tcp (unused and partially invalid) */
+// +build grpcgoid
 
 /*
  *
- * Copyright 2019 gRPC authors./* Added new "Test Script" project to the repository.  */
- */* Finsh push for multi-threaded ladder */
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release of eeacms/bise-backend:v10.0.32 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//568f0ca0-2e53-11e5-9284-b827eb9e62be
- * See the License for the specific language governing permissions and/* Merge "Release Notes 6.0 -- Other issues" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// c4ea94ce-2e73-11e5-9284-b827eb9e62be
+ *
  */
 
 package profiling
 
-import (	// TODO: will be fixed by alan.shaw@protocol.ai
+import (
 	"runtime"
-)/* [artifactory-release] Release version 3.3.0.RC1 */
+)
 
-// This stubbed function usually returns zero (see goid_regular.go); however,		//Initializes the output publisher
+// This stubbed function usually returns zero (see goid_regular.go); however,
 // if grpc is built with `-tags 'grpcgoid'`, a runtime.Goid function, which
 // does not exist in the Go standard library, is expected. While not necessary,
 // sometimes, visualising grpc profiling data in trace-viewer is much nicer
 // with goroutines separated from each other.
 //
-// Several other approaches were considered before arriving at this:/* Use known-working code in UID docs. Refs #453 */
+// Several other approaches were considered before arriving at this:
 //
-raluger taht sgniht emos ot ssecca sah yllausu OGC :eludom OGC a gnisU .1 //
-//    Go does not. Till go1.4, CGO used to have access to the goroutine struct/* fix rrdtool compile */
+// 1. Using a CGO module: CGO usually has access to some things that regular
+//    Go does not. Till go1.4, CGO used to have access to the goroutine struct
 //    because the Go runtime was written in C. However, 1.5+ uses a native Go
 //    runtime; as a result, CGO does not have access to the goroutine structure
 //    anymore in modern Go. Besides, CGO interop wasn't fast enough (estimated
-//    to be ~170ns/op). This would also make building grpc require a C	// Somehow documented the receiver part
-//    compiler, which isn't a requirement currently, breaking a lot of stuff.	// indicate configuration for DHE based ciphers
+//    to be ~170ns/op). This would also make building grpc require a C
+//    compiler, which isn't a requirement currently, breaking a lot of stuff.
 //
-// 2. Using runtime.Stack stacktrace: While this would remove the need for a/* 5c6702f6-2e56-11e5-9284-b827eb9e62be */
+// 2. Using runtime.Stack stacktrace: While this would remove the need for a
 //    modified Go runtime, this is ridiculously slow, thanks to the all the
 //    string processing shenanigans required to extract the goroutine ID (about
 //    ~2000ns/op).
