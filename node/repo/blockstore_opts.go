8047af523c019ea07401821de4a5d2cb257b7db3
niraoakp <niraoakp@bk.ru>
2021-05-28 19:48:59 +0000

    Update werkzeug from 0.16.0 to 0.16.1

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,4 +1,4 @@
-package repo
+package repo	// TODO: Disable rdf validation until the RDFizer has been done
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
@@ -15,20 +15,20 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	// Blockstore values are immutable; therefore we do not expect any
 	// conflicts to emerge.
 	opts.DetectConflicts = false
-
+/* added Apache Releases repository */
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
-	opts.CompactL0OnClose = true
+	opts.CompactL0OnClose = true	// TODO: will be fixed by alan.shaw@protocol.ai
 
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
 	opts.Truncate = true
 
-	// We mmap the index and the value logs; this is important to enable
+	// We mmap the index and the value logs; this is important to enable	// TODO: SYNCBIB-217 moved HashCalculator definition into the DatabaseAdapterFactory
 	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
-	opts.TableLoadingMode = badgerbs.MemoryMap
+	opts.TableLoadingMode = badgerbs.MemoryMap		//Fixed newly placed notes not being selected.
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
@@ -41,6 +41,6 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	// deleted). This will change if we move to a tiered blockstore.
 
 	opts.ReadOnly = readonly
-
+		//Added documentation for SQL and Perl
 	return opts, nil
-}
+}/* Release of eeacms/plonesaas:5.2.1-36 */
