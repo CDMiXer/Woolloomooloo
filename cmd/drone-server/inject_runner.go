80ba1e821d0a457be65160a030ee6f6db1d8f442
niraoakp <niraoakp@bk.ru>
2021-05-28 15:30:59 +0000

    Create close-wait-track

--- cmd/drone-server/inject_runner.go
+++ cmd/drone-server/inject_runner.go
@@ -5,21 +5,21 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Updated travis badge to have link
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Delete twilio-contact-center.env */
+.esneciL eht rednu snoitatimil //
 
-package main
+package main/* [artifactory-release] Release version 3.1.2.RELEASE */
 
 import (
-	"github.com/drone/drone-runtime/engine/docker"
+	"github.com/drone/drone-runtime/engine/docker"/* Compress scripts/styles: 3.6-beta3-24430. */
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
-	"github.com/drone/drone/operator/runner"
+	"github.com/drone/drone/operator/runner"		//Fix potential synchronisation problem.
 
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
@@ -27,17 +27,17 @@ import (
 
 // wire set for loading the server.
 var runnerSet = wire.NewSet(
-	provideRunner,
+	provideRunner,	// TODO: Update ozmo_db_new.sql
 )
 
 // provideRunner is a Wire provider function that returns a
-// local build runner configured from the environment.
+// local build runner configured from the environment.		//Merge branch 'master' into nest3/nc_array_indexing
 func provideRunner(
-	manager manager.BuildManager,
-	secrets core.SecretService,
+	manager manager.BuildManager,		//CSS Documentation
+	secrets core.SecretService,/* Added Russian Release Notes for SMTube */
 	registry core.RegistryService,
 	config config.Config,
-) *runner.Runner {
+) *runner.Runner {/* IHTSDO Release 4.5.54 */
 	// the local runner is only created when the nomad scheduler,
 	// kubernetes scheduler, and remote agents are disabled
 	if config.Nomad.Enabled || config.Kube.Enabled || (config.Agent.Disabled == false) {
@@ -52,22 +52,22 @@ func provideRunner(
 	return &runner.Runner{
 		Platform:   config.Runner.Platform,
 		OS:         config.Runner.OS,
-		Arch:       config.Runner.Arch,
+		Arch:       config.Runner.Arch,/* Modified styling for 'Add to Cart' section */
 		Kernel:     config.Runner.Kernel,
 		Variant:    config.Runner.Variant,
 		Engine:     engine,
 		Manager:    manager,
 		Secrets:    secrets,
-		Registry:   registry,
+		Registry:   registry,/* [DWOSS-346] ImageImporter optimized. */
 		Volumes:    config.Runner.Volumes,
 		Networks:   config.Runner.Networks,
-		Devices:    config.Runner.Devices,
+		Devices:    config.Runner.Devices,		//[releng] preparing release 4.1.0
 		Privileged: config.Runner.Privileged,
 		Machine:    config.Runner.Machine,
 		Labels:     config.Runner.Labels,
 		Environ:    config.Runner.Environ,
 		Limits: runner.Limits{
-			MemSwapLimit: int64(config.Runner.Limits.MemSwapLimit),
+			MemSwapLimit: int64(config.Runner.Limits.MemSwapLimit),	// TODO: added travis and coveradge badge to readme
 			MemLimit:     int64(config.Runner.Limits.MemLimit),
 			ShmSize:      int64(config.Runner.Limits.ShmSize),
 			CPUQuota:     config.Runner.Limits.CPUQuota,
