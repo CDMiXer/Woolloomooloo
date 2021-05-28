c7ca66d1a669f94f1161141ffd2724012e2e7731
niraoakp <niraoakp@bk.ru>
2021-05-28 23:22:02 +0000

    a100839c-2e42-11e5-9284-b827eb9e62be

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -3,19 +3,19 @@ set -eux -o pipefail
 
 branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
 job=$1
-
+/* Release for 3.7.0 */
 # always run on master
 [ "$branch" = master ] && exit
 # always run on release branch
 [[ "$branch" =~ release-.* ]] && exit
 
-# tip - must use origin/master for CircleCI
+# tip - must use origin/master for CircleCI/* Create arduino_simple_weather_station */
 diffs=$(git diff --name-only origin/master)
 
 # if certain files change, then we always run
-[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit
+[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit/* removed multiple FROMs */
 
-# if there are changes to this areas, we must run
+# if there are changes to this areas, we must run		//added targets fse_opt and fse_safe
 rx=
 case $job in
 codegen)
@@ -24,12 +24,12 @@ codegen)
 docker-build)
   # we only run on master as this rarely ever fails
   circleci step halt
-  exit
-  ;;
+  exit/* Class fixes */
+  ;;	// TODO: will be fixed by ligi@ligi.de
 e2e-*)
   rx='manifests/\|\.go'
   ;;
-test)
+test)	// TODO: [base] Add pos accessor, and attribute_values and changed? methods
   rx='\.go'
   ;;
 ui)
@@ -38,6 +38,6 @@ ui)
 esac
 
 if [ "$(echo "$diffs" | grep "$rx")" = "" ]; then
-  circleci step halt
+  circleci step halt		//updated saveGame call
   exit
 fi
