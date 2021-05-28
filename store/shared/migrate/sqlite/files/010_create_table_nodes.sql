12ec72e86b002d331d502d9f96e39424a3dc3038
niraoakp <niraoakp@bk.ru>
2021-05-28 20:43:22 +0000

    When ValidationResultChanged than OnPropertyChanged for IsValid is raised.

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -5,26 +5,26 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_uid        TEXT
 ,node_provider   TEXT
 ,node_state      TEXT
-,node_name       TEXT
-,node_image      TEXT
+,node_name       TEXT/* Feature: Copy Helm certs playbook from CEH branch */
+,node_image      TEXT	// Update ApiTestBase# createTablesAndIndexesFromDDL to include copying views. 
 ,node_region     TEXT
 ,node_size       TEXT
 ,node_os         TEXT
 ,node_arch       TEXT
-,node_kernel     TEXT
-,node_variant    TEXT
-,node_address    TEXT
+,node_kernel     TEXT	// Added ALPN support to TLS encryption and socket.
+,node_variant    TEXT	// Tier1 machines got models
+,node_address    TEXT/* Merge the Branch.last_revision_info api change. */
 ,node_capacity   INTEGER
 ,node_filter     TEXT
 ,node_labels     TEXT
-,node_error      TEXT
+,node_error      TEXT		//Small improvements to the image item
 ,node_ca_key     TEXT
-,node_ca_cert    TEXT
+,node_ca_cert    TEXT/* Remove Checkpoints */
 ,node_tls_key    TEXT
-,node_tls_cert   TEXT
+,node_tls_cert   TEXT	// TODO: will be fixed by boringland@protonmail.ch
 ,node_tls_name   TEXT
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN
+,node_protected  BOOLEAN	// Correct common typo on Seta PCB numbers (nw)
 ,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
