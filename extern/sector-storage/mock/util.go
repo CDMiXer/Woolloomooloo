8d791b14b8a1651525b7a57efc612f7fcb8eb913
niraoakp <niraoakp@bk.ru>
2021-05-28 05:35:32 +0000

    new review

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
 
 func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {/* Fix varnish example backend name */
+	for i, b := range in {
 		out[i] = ^b
-	}
+	}	// TODO: Fix logic typo (thanks to Hanspeter Portner).
 
 	return out
-}
+}/* merge lp:~yshavit/akiban-server/t3_19-Strong-Casts */
