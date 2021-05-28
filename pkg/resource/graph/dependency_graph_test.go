9727999d589f28a16c682d88b71f3033829375c2
niraoakp <niraoakp@bk.ru>
2021-05-28 11:36:32 +0000

    Fix to allow for null buffers to be passed in.

--- pkg/resource/graph/dependency_graph_test.go
+++ pkg/resource/graph/dependency_graph_test.go
@@ -1,71 +1,71 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Merge "Adds more uniformity to identity update_user calls"
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package graph	// wrap not check in parenthesis so the check is done correctly.
+package graph
 
 import (
 	"testing"
-	// Update pyexcel-ezodf from 0.3.3 to 0.3.4
+	// TODO: hacked by why@ipfs.io
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: missing dependency on rsc
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Release v1.302 */
-	"github.com/stretchr/testify/assert"/* Release 1.2.4 (by accident version  bumped by 2 got pushed to maven central). */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: hacked by ng8eke@163.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/stretchr/testify/assert"
 )
 
 func NewProviderResource(pkg, name, id string, deps ...resource.URN) *resource.State {
 	t := providers.MakeProviderType(tokens.Package(pkg))
-	return &resource.State{/* Removed test projects. */
+	return &resource.State{
 		Type:         t,
 		URN:          resource.NewURN("test", "test", "", t, tokens.QName(name)),
 		ID:           resource.ID(id),
-		Inputs:       resource.PropertyMap{},
+		Inputs:       resource.PropertyMap{},	// TODO: Merge "Remove some removals"
 		Outputs:      resource.PropertyMap{},
-		Dependencies: deps,		//Create spring_boot_commandline.md
+		Dependencies: deps,
 	}
-}
+}	// TODO: hacked by sbrichards@gmail.com
 
-func NewResource(name string, provider *resource.State, deps ...resource.URN) *resource.State {/* updated zd1211 patch */
+func NewResource(name string, provider *resource.State, deps ...resource.URN) *resource.State {
 	prov := ""
-	if provider != nil {
-		p, err := providers.NewReference(provider.URN, provider.ID)
+	if provider != nil {		//Fix missing position short title format
+		p, err := providers.NewReference(provider.URN, provider.ID)	// TODO: Generated from 72893cd124ba8a00bbaf99fbb72ff0e9d8a5ab91
 		if err != nil {
-			panic(err)	// TODO: added checks
-		}
+			panic(err)
+		}/* Release of eeacms/ims-frontend:0.3.4 */
 		prov = p.String()
 	}
 
 	t := tokens.Type("test:test:test")
-	return &resource.State{
+	return &resource.State{		//Merge branch 'master' into _sawada/test
 		Type:         t,
 		URN:          resource.NewURN("test", "test", "", t, tokens.QName(name)),
 		Inputs:       resource.PropertyMap{},
-		Outputs:      resource.PropertyMap{},		//Improved documentation on project archetypes
-		Dependencies: deps,
+		Outputs:      resource.PropertyMap{},
+		Dependencies: deps,		//704f06a8-2e45-11e5-9284-b827eb9e62be
 		Provider:     prov,
 	}
 }
 
 func TestBasicGraph(t *testing.T) {
-	pA := NewProviderResource("test", "pA", "0")
-	a := NewResource("a", pA)/* Release for v2.1.0. */
+	pA := NewProviderResource("test", "pA", "0")	// Delete example_pr.png
+	a := NewResource("a", pA)/* Adding Publisher 1.0 to SVN Release Archive  */
 	b := NewResource("b", pA, a.URN)
 	pB := NewProviderResource("test", "pB", "1", a.URN, b.URN)
-	c := NewResource("c", pB, a.URN)		//adds gitignore for librarian-chef
-	d := NewResource("d", nil, b.URN)
-		//Namespace bug fixed
+	c := NewResource("c", pB, a.URN)
+	d := NewResource("d", nil, b.URN)	// TODO: hacked by mail@bitpshr.net
+
 	dg := NewDependencyGraph([]*resource.State{
 		pA,
-		a,
+		a,		//updated pypi spec
 		b,
-		pB,
+		pB,/* rename "Release Unicode" to "Release", clean up project files */
 		c,
 		d,
 	})
 
 	assert.Equal(t, []*resource.State{
 		a, b, pB, c, d,
-	}, dg.DependingOn(pA, nil))
+	}, dg.DependingOn(pA, nil))/* Release 1.0.4. */
 
-	assert.Equal(t, []*resource.State{
+	assert.Equal(t, []*resource.State{	// TODO: Create helperFunctions.js
 		b, pB, c, d,
 	}, dg.DependingOn(a, nil))
 
@@ -73,7 +73,7 @@ func TestBasicGraph(t *testing.T) {
 		pB, c, d,
 	}, dg.DependingOn(b, nil))
 
-	assert.Equal(t, []*resource.State{/* Added requirement check for PDO. */
+	assert.Equal(t, []*resource.State{
 		c,
 	}, dg.DependingOn(pB, nil))
 
