5f9dde103c81906566d2decd0460b07336d6cb2f
niraoakp <niraoakp@bk.ru>
2021-05-28 23:56:25 +0000

    Update definition of `Demisexual`

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,32 +1,32 @@
 package paychmgr
-/*  - [ZBX-761] fixed JS error in screens (Artem) */
+
 import "sync"
 
 type rwlock interface {
 	RLock()
-	RUnlock()		//Delete .Parent
+	RUnlock()
 }
 
 // channelLock manages locking for a specific channel.
 // Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
-// any operation against any channel.		//8ef8a47c-2e56-11e5-9284-b827eb9e62be
+// any operation against any channel.
 type channelLock struct {
 	globalLock rwlock
-	chanLock   sync.Mutex	// TODO: will be fixed by vyzo@hackzen.org
-}	// TODO: hacked by boringland@protonmail.ch
+	chanLock   sync.Mutex
+}
 
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
 	// Wait for operations affecting all channels to finish.
-	// Allows ops by other channels in parallel, but blocks all operations		//Couple of links
+	// Allows ops by other channels in parallel, but blocks all operations
 	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
 }
-/* Released springjdbcdao version 1.6.6 */
+
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
 	l.chanLock.Unlock()
