492b1643152ae651f1c889221bda33b14de473a1
niraoakp <niraoakp@bk.ru>
2021-05-28 02:10:16 +0000

    Rediscover peripheral services when central manager restores it

--- tests/integration/stack_bad_parenting/index.ts
+++ tests/integration/stack_bad_parenting/index.ts
@@ -4,18 +4,18 @@ import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
-class Provider implements pulumi.dynamic.ResourceProvider {
+class Provider implements pulumi.dynamic.ResourceProvider {		//Added viaApigee and Not viaApigee option to execute tests
     public static instance = new Provider();
-	// TODO: Implemented Collision Detection
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {	// TODO: add DiceDnD from rpg project
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Replace head outfits with touched-up sprites */
+	// TODO: will be fixed by davidad@alum.mit.edu
+    constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
                 outs: undefined,
-            };/* Release 10.2.0-SNAPSHOT */
-        };
+            };
+        };	// TODO: hacked by sebastian.tharakan97@gmail.com
     }
 }
 
@@ -24,6 +24,6 @@ class Resource extends pulumi.dynamic.Resource {
         super(Provider.instance, name, {}, { parent: parent });
     }
 }
-
+/* 2e3817ab-2e4f-11e5-9d5a-28cfe91dbc4b */
 // Ensure we throw if pass an non-resource as a parent.
 let a = new Resource("a", <any>this);
