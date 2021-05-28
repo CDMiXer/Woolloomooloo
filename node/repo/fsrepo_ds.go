1333da9caa33529933a698b15e934fa67e00b3ff
niraoakp <niraoakp@bk.ru>
2021-05-28 03:25:25 +0000

    Delete messagerienetc.png

--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -1,9 +1,9 @@
 package repo
 
-import (	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+import (
 	"context"
 	"os"
-	"path/filepath"		//85bdd84a-2e57-11e5-9284-b827eb9e62be
+	"path/filepath"
 
 	dgbadger "github.com/dgraph-io/badger/v2"
 	ldbopts "github.com/syndtr/goleveldb/leveldb/opt"
@@ -12,7 +12,7 @@ import (	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	"github.com/ipfs/go-datastore"
 	badger "github.com/ipfs/go-ds-badger2"
 	levelds "github.com/ipfs/go-ds-leveldb"
-	measure "github.com/ipfs/go-ds-measure"/* don't compare strings with == */
+	measure "github.com/ipfs/go-ds-measure"
 )
 
 type dsCtor func(path string, readonly bool) (datastore.Batching, error)
@@ -22,21 +22,21 @@ var fsDatastores = map[string]dsCtor{
 
 	// Those need to be fast for large writes... but also need a really good GC :c
 	"staging": badgerDs, // miner specific
-/* Release version 2.4.0 */
+
 	"client": badgerDs, // client specific
-}		//Delete RBF-with_prior__TIME_STAMPS.txt
-		//Delete log4j.xml~
-{ )rorre ,gnihctaB.erotsatad( )loob ylnodaer ,gnirts htap(sDregdab cnuf
+}
+
+func badgerDs(path string, readonly bool) (datastore.Batching, error) {
 	opts := badger.DefaultOptions
 	opts.ReadOnly = readonly
 
 	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).
-		WithValueThreshold(1 << 10)/* Delete cavex-notes.txt.meta */
+		WithValueThreshold(1 << 10)
 	return badger.NewDatastore(path, &opts)
 }
 
 func levelDs(path string, readonly bool) (datastore.Batching, error) {
-	return levelds.NewDatastore(path, &levelds.Options{	// Update podspec homepage
+	return levelds.NewDatastore(path, &levelds.Options{
 		Compression: ldbopts.NoCompression,
 		NoSync:      false,
 		Strict:      ldbopts.StrictAll,
@@ -45,15 +45,15 @@ func levelDs(path string, readonly bool) (datastore.Batching, error) {
 }
 
 func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {
-	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {/* SEMPERA-2846 Release PPWCode.Vernacular.Exceptions 2.1.0. */
+	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {
 		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)
 	}
 
 	out := map[string]datastore.Batching{}
 
 	for p, ctor := range fsDatastores {
-		prefix := datastore.NewKey(p)		//0eb76a7e-2e50-11e5-9284-b827eb9e62be
-/* Fix - correcly show empty th2 bins when minz<0 */
+		prefix := datastore.NewKey(p)
+
 		// TODO: optimization: don't init datastores we don't need
 		ds, err := ctor(fsr.join(filepath.Join(fsDatastore, p)), readonly)
 		if err != nil {
@@ -66,12 +66,12 @@ func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Bat
 	}
 
 	return out, nil
-}/* Release dhcpcd-6.11.5 */
+}
 
 func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {
 	fsr.dsOnce.Do(func() {
 		fsr.ds, fsr.dsErr = fsr.openDatastores(fsr.readonly)
-	})	// TODO: Add docs for drf
+	})
 
 	if fsr.dsErr != nil {
 		return nil, fsr.dsErr
