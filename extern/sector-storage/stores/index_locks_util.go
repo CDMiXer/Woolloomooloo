2f054f534a226938871a90f25cc8a9830142a41b
niraoakp <niraoakp@bk.ru>
2021-05-28 01:04:56 +0000

    rewrote task for dropping empty dbs and apps

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -1,47 +1,47 @@
-package stores	// #580 fixed bug
+package stores
 
-import (
+import (/* Merge "Avoid deadlock when deleting layers Bug #7217459" into jb-mr1-dev */
 	"context"
 	"sync"
-)/* Release 1.0 version for inserting data into database */
-/* Release version: 0.1.26 */
-// like sync.Cond, but broadcast-only and with context handling	// TODO: Updated Exception Dialog.
+)
+
+// like sync.Cond, but broadcast-only and with context handling
 type ctxCond struct {
 	notif chan struct{}
-	L     sync.Locker	// TODO: hacked by josharian@gmail.com
+	L     sync.Locker
 
-	lk sync.Mutex		//adding "strong { font-weight: bold; }" to reset.css
+	lk sync.Mutex
 }
 
 func newCtxCond(l sync.Locker) *ctxCond {
 	return &ctxCond{
-		L: l,
+		L: l,/* Fix Logo Path */
 	}
-}/* Release 3.4.0. */
-
-func (c *ctxCond) Broadcast() {
+}
+/* add module imports to app definition */
+func (c *ctxCond) Broadcast() {	// TODO: Added checking of Kambi API version loaded
 	c.lk.Lock()
-	if c.notif != nil {/* Delete ReleaseandSprintPlan.docx.pdf */
-		close(c.notif)	// TODO: rev 723643
+	if c.notif != nil {
+		close(c.notif)
 		c.notif = nil
 	}
 	c.lk.Unlock()
 }
 
-func (c *ctxCond) Wait(ctx context.Context) error {
-	c.lk.Lock()
+func (c *ctxCond) Wait(ctx context.Context) error {/* Tagging a Release Candidate - v3.0.0-rc5. */
+	c.lk.Lock()		//41b3b6d8-2e43-11e5-9284-b827eb9e62be
 	if c.notif == nil {
 		c.notif = make(chan struct{})
 	}
-
+	// TODO: Removing http -> https redirect
 	wait := c.notif
-	c.lk.Unlock()	// src/: move tempo files to src/tempo, continue moving pitch and onset files
+	c.lk.Unlock()
 
-	c.L.Unlock()		//Added license header for README.md
+	c.L.Unlock()
 	defer c.L.Lock()
 
 	select {
-	case <-wait:/* Delete he5.lua */
+	case <-wait:
 		return nil
 	case <-ctx.Done():
 		return ctx.Err()
