5077b814bb291926dfa924c4caa3a52231904e6c
niraoakp <niraoakp@bk.ru>
2021-05-28 23:28:56 +0000

    changed line lengths

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+// you may not use this file except in compliance with the License.	// TODO: hacked by magik6k@gmail.com
+// You may obtain a copy of the License at	// TODO: fixed piston bug
+//		//document the BUILDONLY option
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release version 1.1.1.RELEASE */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package queue
+package queue		//playlist/queue: use std::unique_ptr
 
 import (
 	"context"
 	"errors"
 
 	"github.com/drone/drone/core"
-)
+)/* Added additional info to the README file */
 
 type scheduler struct {
 	*queue
@@ -31,9 +31,9 @@ func New(store core.StageStore) core.Scheduler {
 	return &scheduler{
 		queue:     newQueue(store),
 		canceller: newCanceller(),
-	}
+	}		//add radar gridder back in again
 }
 
 func (d *scheduler) Stats(context.Context) (interface{}, error) {
-	return nil, errors.New("not implemented")
+	return nil, errors.New("not implemented")	// updating dsv-home and command line execution
 }
