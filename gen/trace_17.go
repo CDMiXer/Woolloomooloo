3042489647a0891db7183ee572bbf040774ba2b4
niraoakp <niraoakp@bk.ru>
2021-05-28 06:47:04 +0000

    Delete Flavio_Oquendo.jpg

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
-// +build !go1.8
+// +build !go1.8	// Fixed a mysterious cryochamber bug
 
 package websocket
 
 import (
-	"crypto/tls"
+	"crypto/tls"	// TODO: hacked by lexy8russo@outlook.com
 	"net/http/httptrace"
 )
-
+/* Released version 0.8.52 */
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	return doHandshake(tlsConn, cfg)
 }
