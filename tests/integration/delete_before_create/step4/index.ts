abd527b15f33ecbc99e09d5dd6c7da86a836b381
niraoakp <niraoakp@bk.ru>
2021-05-28 23:47:30 +0000

    Upgrade A* to v3.6

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";/* Implement subset and superset checks. */
+import { Resource } from "./resource";
 
 // Setup for the next test.
-const a = new Resource("base", { uniqueKey: 1, state: 100 });		//Cleaning up debug outputs
-const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
+const a = new Resource("base", { uniqueKey: 1, state: 100 });
+const b = new Resource("base-2", { uniqueKey: 2, state: 100 });	// TODO: Project werkt eindelijk goed synchroon met het DCD
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });	// TODO: hacked by vyzo@hackzen.org
