b7630191b579b20ada82bd421658b7411525aaf9
niraoakp <niraoakp@bk.ru>
2021-05-28 08:42:06 +0000

    ! several Files: corrected case sensitivity of ::getInstance()

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by fjl@ethereum.org
+// that can be found in the LICENSE file./* Share feature shares HTML content also #112. Refactoring. */
 
-// +build !oss
+// +build !oss	// TODO: Rename the python_demo_test + add some comments
 
 package core
