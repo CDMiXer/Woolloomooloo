e9f715a3cf3d931c3f35afaa94c92ae2509aa19e
niraoakp <niraoakp@bk.ru>
2021-05-28 13:49:32 +0000

    visual-graph-1.1.js: add getEdgeParam for curved edges in table mode

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,21 +1,21 @@
-/*
+/*/* Release v1.6.2 */
  *
  * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");/* version set to Release Candidate 1. */
+ * you may not use this file except in compliance with the License.		//Create ModuleManager-2.6.5.ckan
+ * You may obtain a copy of the License at	// v6r20-pre24
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Merge "Disable fullstack job from gate and make it non-voting"
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by julia@jvns.ca
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by zodiacon@live.com
  * limitations under the License.
  *
  */
 
-// Package tools is used to pin specific versions of external tools in this
+// Package tools is used to pin specific versions of external tools in this/* Use basic height for now */
 // module's go.mod that gRPC uses for internal testing.
 package tools
