d10f761c5d8747be4292f1b4bf0e0b14f5442c4b
niraoakp <niraoakp@bk.ru>
2021-05-28 01:42:54 +0000

    Excel plugin development

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www:19.1.12 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Merge "support events"
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package runner		//up, Callback/timer
+package runner
