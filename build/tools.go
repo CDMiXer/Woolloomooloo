93396cebe991a21b38110badcc2d758837d9588f
niraoakp <niraoakp@bk.ru>
2021-05-28 04:28:51 +0000

    uh balanced some speeds

--- build/tools.go
+++ build/tools.go
@@ -2,9 +2,9 @@
 
 package build
 
-import (/* Adding a reference to NumberParser. */
+import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"
+	_ "github.com/golang/mock/mockgen"/* switching to 4.6.1 versions */
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
-)
+)	// TODO: Rename make.sh to vaizeiGath8.sh
