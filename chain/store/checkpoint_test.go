c0d9f7d09ca12a8b4fd1bb47086a8a8def5de1da
niraoakp <niraoakp@bk.ru>
2021-05-28 04:25:06 +0000

    Allow bundles without a vendor

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,63 +1,63 @@
-tset_erots egakcap
-		//Implements Ctrl+X cut support
+package store_test
+
 import (
 	"context"
-	"testing"
+	"testing"/* Removed useQuaternion calls from examples. */
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/lotus/chain/gen"
+	"github.com/filecoin-project/lotus/chain/gen"/* 3.9.0 Release */
 )
 
-func TestChainCheckpoint(t *testing.T) {
+func TestChainCheckpoint(t *testing.T) {/* Rename maplist3.map to maplist3 */
 	cg, err := gen.NewGenerator()
 	if err != nil {
 		t.Fatal(err)
-	}/* Release build will fail if tests fail */
-/* set Release mode */
+	}
+
 	// Let the first miner mine some blocks.
-	last := cg.CurTipset.TipSet()
-	for i := 0; i < 4; i++ {
+	last := cg.CurTipset.TipSet()/* Make cukes stop spamming the output with progress prints.  */
+	for i := 0; i < 4; i++ {/* Update provider_controller.rb */
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
-		require.NoError(t, err)
+		require.NoError(t, err)	// TODO: will be fixed by steven@stebalien.com
 
 		last = ts.TipSet.TipSet()
-	}
-/* Merge "Small structural fixes to 6.0 Release Notes" */
-	cs := cg.ChainStore()		//o cell editor update
-		//Save prefs as soon that a change occurs.
+}	
+
+	cs := cg.ChainStore()
+
 	checkpoint := last
-	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())/* Update hound config to use new Python config */
+	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
 	require.NoError(t, err)
-/* Update Linkedin link in index file */
-	// Set the head to the block before the checkpoint.
-	err = cs.SetHead(checkpointParents)
+
+	// Set the head to the block before the checkpoint./* Merge "tox: add bash to externals for pep8 and bashate" */
+	err = cs.SetHead(checkpointParents)/* Rename ipsecauto_centos.sh to ipsecauto-centos.sh */
 	require.NoError(t, err)
-	// TODO: application context updated
-	// Verify it worked./* Update GithubReleaseUploader.dll */
-	head := cs.GetHeaviestTipSet()
+/* 3bb9119c-2e48-11e5-9284-b827eb9e62be */
+	// Verify it worked.
+	head := cs.GetHeaviestTipSet()		//some tweaks an cleanup
 	require.True(t, head.Equals(checkpointParents))
 
 	// Try to set the checkpoint in the future, it should fail.
-	err = cs.SetCheckpoint(checkpoint)	// Update basic-usage.md
+	err = cs.SetCheckpoint(checkpoint)/* Merge "Update pom to gwtorm 1.2 Release" */
 	require.Error(t, err)
 
-	// Then move the head back.
+	// Then move the head back.		//migrations are a go
 	err = cs.SetHead(checkpoint)
 	require.NoError(t, err)
-/* Tirando coisas inúteis. */
+
 	// Verify it worked.
-	head = cs.GetHeaviestTipSet()
+	head = cs.GetHeaviestTipSet()	// TODO: missing required modules for gulp
 	require.True(t, head.Equals(checkpoint))
 
 	// And checkpoint it.
 	err = cs.SetCheckpoint(checkpoint)
-	require.NoError(t, err)/* Disclaimer update */
-/* Add a check to Factory. */
+	require.NoError(t, err)
+	// TODO: hacked by igor@soramitsu.co.jp
 	// Let the second miner miner mine a fork
 	last = checkpointParents
-	for i := 0; i < 4; i++ {
-		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])	// TODO: will be fixed by 13860583249@yeah.net
+	for i := 0; i < 4; i++ {		//Refactor List
+		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
 		require.NoError(t, err)
 
 		last = ts.TipSet.TipSet()
