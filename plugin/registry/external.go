7c54072ecc812cb2a9c56369b4b8d1f5e226939e
niraoakp <niraoakp@bk.ru>
2021-05-28 19:54:12 +0000

    SECRET_KEY_BASE

--- plugin/registry/external.go
+++ plugin/registry/external.go
@@ -11,41 +11,41 @@ import (
 	"time"
 
 	"github.com/drone/drone-go/plugin/secret"
-	"github.com/drone/drone-yaml/yaml"/* Release: Making ready for next release iteration 5.7.3 */
-	"github.com/drone/drone/core"	// TODO: output diagnostics to log instead of std err
+	"github.com/drone/drone-yaml/yaml"
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
 
 	droneapi "github.com/drone/drone-go/drone"
 )
 
-// External returns a new external Secret controller.	// Fixed PHP notices in main logic
-func External(endpoint, secret string, skipVerify bool) core.RegistryService {		//Fix tests for DisKIO
+// External returns a new external Secret controller.
+func External(endpoint, secret string, skipVerify bool) core.RegistryService {
 	return &externalController{
 		endpoint:   endpoint,
 		secret:     secret,
 		skipVerify: skipVerify,
 	}
 }
-	// TODO: will be fixed by arajasek94@gmail.com
+
 type externalController struct {
 	endpoint   string
 	secret     string
-	skipVerify bool/* Improve factories */
+	skipVerify bool
 }
 
-func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {	// TODO: Changing builtins.Str to use builtins._AttributeCollector
+func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
 	var results []*core.Registry
-/* Merge "[Release] Webkit2-efl-123997_0.11.107" into tizen_2.2 */
+
 	for _, match := range in.Pipeline.PullSecrets {
 		logger := logger.FromContext(ctx).
 			WithField("name", match).
 			WithField("kind", "secret").
 			WithField("secret", c.endpoint)
 		logger.Trace("image_pull_secrets: find secret")
-	// TODO: Fix folder name
+
 		// lookup the named secret in the manifest. If the
-		// secret does not exist, return a nil variable,/* Release of eeacms/www:21.1.12 */
+		// secret does not exist, return a nil variable,
 		// allowing the next secret controller in the chain
 		// to be invoked.
 		path, name, ok := getExternal(in.Conf, match)
@@ -53,7 +53,7 @@ func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([
 			logger.Trace("image_pull_secrets: no matching secret resource in yaml")
 			return nil, nil
 		}
-/* Remove snapshot for 1.0.47 Oct Release */
+
 		logger = logger.
 			WithField("get.path", path).
 			WithField("get.name", name)
@@ -71,7 +71,7 @@ func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([
 			Repo:  toRepo(in.Repo),
 			Build: toBuild(in.Build),
 		}
-		client := secret.Client(c.endpoint, c.secret, c.skipVerify)/* Added location change info to home page */
+		client := secret.Client(c.endpoint, c.secret, c.skipVerify)
 		res, err := client.Find(ctx, req)
 		if err != nil {
 			logger.WithError(err).Trace("image_pull_secrets: cannot get secret")
@@ -79,13 +79,13 @@ func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([
 		}
 
 		// if no error is returned and the secret is empty,
-		// this indicates the client returned No Content,	// Create 01300000185725121508404762618_s.jpg
+		// this indicates the client returned No Content,
 		// and we should exit with no secret, but no error.
 		if res.Data == "" {
-			return nil, nil	// TODO: hacked by alex.gaynor@gmail.com
-		}		//changed target version
+			return nil, nil
+		}
 
-		// The secret can be restricted to non-pull request		//919fd061-2e4f-11e5-b50c-28cfe91dbc4b
+		// The secret can be restricted to non-pull request
 		// events. If the secret is restricted, return
 		// empty results.
 		if (res.Pull == false && res.PullRequest == false) &&
