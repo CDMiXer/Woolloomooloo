e5b0bb8dfee3042da215b618f0c86e82d869bb58
niraoakp <niraoakp@bk.ru>
2021-05-28 12:31:43 +0000

    install just libav-tools

--- core/renewer.go
+++ core/renewer.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Rename yacy-graphite-service to yacy-graphite.service
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by why@ipfs.io
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,9 +16,9 @@ package core
 
 import "context"
 
-// Renewer renews the user account authorization. If
-// successful, the user token and token expiry attributes/* Merge "Bump the size of the squid cache_dir." */
-// are updated, and persisted to the datastore./* Release 2.0.22 - Date Range toString and access token logging */
+// Renewer renews the user account authorization. If		//Create a MarkerView and use it rather than the selectionView
+// successful, the user token and token expiry attributes
+// are updated, and persisted to the datastore.
 type Renewer interface {
 	Renew(ctx context.Context, user *User, force bool) error
 }
