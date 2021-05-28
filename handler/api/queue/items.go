01beed7582ef93dff1df80e35725b6b9dc71d1cd
niraoakp <niraoakp@bk.ru>
2021-05-28 18:31:30 +0000

    Make tests run by mocking the socket websocketDidConnect being called

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Fixed filter for recently opened repositories */
 package queue
 
 import (
@@ -11,7 +11,7 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* Merge branch 'master' into hotfix-kuz540 */
 )
 
 // HandleItems returns an http.HandlerFunc that writes a
@@ -20,12 +20,12 @@ func HandleItems(store core.StageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		items, err := store.ListIncomplete(ctx)
-		if err != nil {
+		if err != nil {	// TODO: Update forgot-password-view.css
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err).	// add font-size override for semantic
 				Warnln("api: cannot get running items")
 			return
 		}
 		render.JSON(w, items, 200)
 	}
-}
+}/* 78148de1-2eae-11e5-ae6c-7831c1d44c14 */
