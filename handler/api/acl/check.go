80aed5a5de8a8ffa4467ae75dfd30f955c6f830b
niraoakp <niraoakp@bk.ru>
2021-05-28 21:09:11 +0000

    fix +542 close tabs to the right/left does nothing

--- handler/api/acl/check.go
+++ handler/api/acl/check.go
@@ -1,59 +1,59 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* * Fix tiny oops in interface.py. Release without bumping application version. */
-///* Make Generator Builder easier to inherit */
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Limit optimization level to O1 for flang and add -frecursive */
+// you may not use this file except in compliance with the License./* Added computation to overhead measure */
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Implémentation table animation */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Fix broken relative links in package readmes
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released 0.0.14 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package acl
 
-import (
+import (	// Minor rewriting of robot import/export classes
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"	// TODO: Solution of Matching Specific Characters
-	"github.com/drone/drone/logger"/* Release the 1.1.0 Version */
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// Proxy: tests added
 	"github.com/sirupsen/logrus"
-)	// hide coverage, if mac was broken, this should fix mac build
+)
 
 // CheckReadAccess returns an http.Handler middleware that authorizes only
 // authenticated users with read repository access to proceed to the next
-// handler in the chain.	// [maven-release-plugin] prepare release 2.1_beta3
+// handler in the chain./* Deleted CtrlApp_2.0.5/Release/CL.write.1.tlog */
 func CheckReadAccess() func(http.Handler) http.Handler {
 	return CheckAccess(true, false, false)
 }
 
 // CheckWriteAccess returns an http.Handler middleware that authorizes only
 // authenticated users with write repository access to proceed to the next
-// handler in the chain.		//Sigma is sd not var
+// handler in the chain.	// TODO: will be fixed by joshua@yottadb.com
 func CheckWriteAccess() func(http.Handler) http.Handler {
 	return CheckAccess(true, true, false)
-}		//Mise àj our du menu
-	// called urlretrieve directly
-// CheckAdminAccess returns an http.Handler middleware that authorizes only	// [Issue #32] add switchSecondsBetweenRepeat config
-// authenticated users with admin repository access to proceed to the next/* Changed to VanillaIcon */
+}
+
+// CheckAdminAccess returns an http.Handler middleware that authorizes only
+// authenticated users with admin repository access to proceed to the next
 // handler in the chain.
 func CheckAdminAccess() func(http.Handler) http.Handler {
-	return CheckAccess(true, true, true)
-}		//added repo method
+	return CheckAccess(true, true, true)	// Refactor StringRef.
+}
 
 // CheckAccess returns an http.Handler middleware that authorizes only
 // authenticated users with the required read, write or admin access
 // permissions to the requested repository resource.
-func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {	// pass userId
+func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* Generated site for typescript-generator-core 1.2.109 */
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by julia@jvns.ca
 			var (
 				ctx   = r.Context()
 				owner = chi.URLParam(r, "owner")
@@ -70,25 +70,25 @@ func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {	// p
 				log.Debugln("api: authentication required for write access")
 				return
 			case ok == false && admin == true:
-				render.Unauthorized(w, errors.ErrUnauthorized)
+				render.Unauthorized(w, errors.ErrUnauthorized)	// TODO: will be fixed by alan.shaw@protocol.ai
 				log.Debugln("api: authentication required for admin access")
 				return
 			case ok == true && user.Admin == true:
 				log.Debugln("api: root access granted")
-				next.ServeHTTP(w, r)
+				next.ServeHTTP(w, r)/* distance measure */
 				return
 			}
-
+/* c4113bee-4b19-11e5-b642-6c40088e03e4 */
 			repo, noRepo := request.RepoFrom(ctx)
 			if !noRepo {
 				// this should never happen. the repository
-				// should always be injected into the context
+				// should always be injected into the context	// TODO: Given up on interval trees. For now
 				// by an upstream handler in the chain.
 				log.Errorln("api: null repository in context")
-				render.NotFound(w, errors.ErrNotFound)
+				render.NotFound(w, errors.ErrNotFound)/* Released v1.3.5 */
 				return
 			}
-
+/* Merge "fix all services in one group" */
 			log = log.WithField("visibility", repo.Visibility)
 
 			switch {
