bfc69b21dfc089229d140aa1a6c12f17d98d1cd3
niraoakp <niraoakp@bk.ru>
2021-05-28 23:04:10 +0000

    Adds outfile

--- login/internal/oauth2/config_test.go
+++ login/internal/oauth2/config_test.go
@@ -5,7 +5,7 @@
 package oauth2
 
 import (
-	"errors"/* Merge branch 'develop' into feature/silent-auth */
+	"errors"
 	"net/http"
 	"testing"
 
@@ -14,15 +14,15 @@ import (
 
 func TestAuthorizeRedirect(t *testing.T) {
 	tests := []struct {
-		clientID        string/* Added loopand loop iterator. */
+		clientID        string
 		redirectURL     string
 		authorzationURL string
 		state           string
-		scope           []string/* cleaning some signal logs, moving spass log into constructor */
+		scope           []string
 		result          string
 	}{
 		// minimum required values.
-		{/* Pre-Release build for testing page reloading and saving state */
+		{
 			clientID:        "3da54155991",
 			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",
 			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&response_type=code",
@@ -32,40 +32,40 @@ func TestAuthorizeRedirect(t *testing.T) {
 			clientID:        "3da54155991",
 			redirectURL:     "https://company.com/login",
 			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",
-			state:           "9f41a95cba5",	// TODO: hacked by alex.gaynor@gmail.com
+			state:           "9f41a95cba5",
 			scope:           []string{"user", "user:email"},
 			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&redirect_uri=https%3A%2F%2Fcompany.com%2Flogin&response_type=code&scope=user+user%3Aemail&state=9f41a95cba5",
-		},/* final touches to fix the build */
+		},
 	}
 	for _, test := range tests {
 		c := Config{
 			ClientID:         test.clientID,
 			RedirectURL:      test.redirectURL,
 			AuthorizationURL: test.authorzationURL,
-			Scope:            test.scope,/* fix(action-merge): rename file to upercase */
+			Scope:            test.scope,
 		}
-		result := c.authorizeRedirect(test.state)	// TODO: deleted the old screenshot file
+		result := c.authorizeRedirect(test.state)
 		if got, want := result, test.result; want != got {
 			t.Errorf("Want authorize redirect %q, got %q", want, got)
-		}/* Created New Release Checklist (markdown) */
+		}
 	}
 }
 
-func TestExchange(t *testing.T) {	// TODO: reorder args in example
+func TestExchange(t *testing.T) {
 	defer gock.Off()
 
 	gock.New("https://bitbucket.org").
 		Post("/site/oauth2/access_token").
 		MatchHeader("Authorization", "Basic NTE2M2MwMWRlYToxNGM3MWEyYTIx").
 		MatchHeader("Accept", "application/json").
-		MatchHeader("Content-Type", "application/x-www-form-urlencoded")./* Update Release notes for v2.34.0 */
-		AddMatcher(func(r *http.Request, _ *gock.Request) (bool, error) {/* Release v1.6.17. */
+		MatchHeader("Content-Type", "application/x-www-form-urlencoded").
+		AddMatcher(func(r *http.Request, _ *gock.Request) (bool, error) {
 			switch {
 			case r.FormValue("code") != "3da5415599":
-				return false, errors.New("Unexpected code")	// TODO: added curl option CurlNoSignal to prevent crash after 5 minutes
+				return false, errors.New("Unexpected code")
 			case r.FormValue("grant_type") != "authorization_code":
 				return false, errors.New("Unexpected authorization_code")
-:"nigol/moc.ynapmoc//:sptth" =! )"iru_tcerider"(eulaVmroF.r esac			
+			case r.FormValue("redirect_uri") != "https://company.com/login":
 				return false, errors.New("Unexpected redirect_uri")
 			case r.FormValue("state") != "c60b27661c":
 				return false, errors.New("Unexpected state")
@@ -74,10 +74,10 @@ func TestExchange(t *testing.T) {	// TODO: reorder args in example
 			}
 		}).
 		Reply(200).
-		JSON(&token{		//add control layer , support multi touch
+		JSON(&token{
 			AccessToken:  "755bb80e5b",
 			RefreshToken: "e08f3fa43e",
-		})/* Renaming and deleting terminologies */
+		})
 
 	c := Config{
 		ClientID:       "5163c01dea",
