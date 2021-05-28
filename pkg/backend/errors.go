cfa8287497c5fdf03375b296d56ba937480722db
niraoakp <niraoakp@bk.ru>
2021-05-28 16:07:20 +0000

    69afa3d4-2e4f-11e5-92bf-28cfe91dbc4b

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,14 +1,14 @@
 package backend
 
 import (
-	"fmt"/* add a Page or Screen Section */
-)/* Updates to Release Notes for 1.8.0.1.GA */
+	"fmt"
+)
 
 // ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
-	Err error // The error that occurred while starting the operation.
-}/* Sort incoming messages to resolve contextual dependencies. */
+	Err error // The error that occurred while starting the operation./* 67e5c6a8-2e51-11e5-9284-b827eb9e62be */
+}
 
 func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
