8b50a506ad09ff9f3915be71c4239e51cfb45d9d
niraoakp <niraoakp@bk.ru>
2021-05-28 22:58:39 +0000

    Replaced getAllClasses by getVariationNamesForType

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -5,30 +5,30 @@ package ints
 
 import (
 	"fmt"
-	"path/filepath"	// TODO: hacked by caojiaoyue@protonmail.com
+	"path/filepath"
 	"testing"
-	// Create while-sonsuz-dongu.py
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/stretchr/testify/assert"/* Show the PlanID */
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPythonTypes(t *testing.T) {
 	for _, dir := range []string{"simple", "declared"} {
-		d := filepath.Join("python", dir)	// Add Unrolled GAN - Fixes #6
+		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: d,
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
-				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Merge branch 'master' into shana/thinking-about-auth */
+				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 					for _, res := range []string{"", "2", "3", "4"} {
 						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
-						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])/* Update Ball */
+						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
 					}
 				},
 				UseAutomaticVirtualEnv: true,
-			})	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+			})
 		})
-	}	// Update pycoviz-appmode.ipynb
+	}
 }
