fc1177a259cde698cf02fc14020374520649b32d
niraoakp <niraoakp@bk.ru>
2021-05-29 12:23:31 +0000

    Pass a verbosity flag to ghc-pkg

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* updated main makefile to new RL-Glue directory */
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 const sleep = require("sleep-promise");
@@ -10,28 +10,28 @@ class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
-    update = (id: string, olds: any, news: any) => Promise.resolve({});/* Release version 1.0.0.RC4 */
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();/* Added Edge v18 mapping */
+    update = (id: string, olds: any, news: any) => Promise.resolve({});
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
 
 class NullResource extends dynamic.Resource {
     constructor(name: string) {
-        super(new NullProvider(), name, {}, undefined);/* Simplify curl usage */
+        super(new NullProvider(), name, {}, undefined);
     }
 }
 
-(async () => {	// TODO: will be fixed by why@ipfs.io
+(async () => {
     try {
         const a = new NullResource("a");
         await sleep(1000);
         const b = new NullResource("b");
         await sleep(1000);
-        const c = new NullResource("c");/* Release version 1.0.2. */
+        const c = new NullResource("c");
         const urn = await b.urn;
         assert.notStrictEqual(urn, undefined, "expected a defined urn");
         assert.notStrictEqual(urn, "", "expected a valid urn");
-    } catch (err) {	// An other one, so many free u's.
+    } catch (err) {
         console.error(err);
-        process.exit(-1);/* 0960cf2a-2e64-11e5-9284-b827eb9e62be */
+        process.exit(-1);
     }
 })();
