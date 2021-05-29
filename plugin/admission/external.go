bfb2ad5faf44cd1cfd561583ee83d0ed0ff8f8a9
niraoakp <niraoakp@bk.ru>
2021-05-29 02:25:43 +0000

    Add Team players associations

--- plugin/admission/external.go
+++ plugin/admission/external.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Support SUSE */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -7,15 +7,15 @@
 package admission
 
 import (
-	"context"
+	"context"		//Updating docblock
 	"time"
 
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/admission"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge "Add XUP Menu Traversal utility" */
 )
 
-// External returns a new external Admission controller.
+// External returns a new external Admission controller.		//Stuff and more
 func External(endpoint, secret string, skipVerify bool) core.AdmissionService {
 	return &external{
 		endpoint:   endpoint,
@@ -27,12 +27,12 @@ func External(endpoint, secret string, skipVerify bool) core.AdmissionService {
 type external struct {
 	endpoint   string
 	secret     string
-	skipVerify bool
+	skipVerify bool/* Release version 0.1.18 */
 }
 
 func (c *external) Admit(ctx context.Context, user *core.User) error {
 	if c.endpoint == "" {
-		return nil
+		return nil/* Release-ish update to the readme. */
 	}
 
 	// include a timeout to prevent an API call from
@@ -42,9 +42,9 @@ func (c *external) Admit(ctx context.Context, user *core.User) error {
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 
-	req := &admission.Request{
+	req := &admission.Request{/* Rename CmsEnvironmentIndicator.md to cmsenvironmentindicator.md */
 		Event: admission.EventLogin,
-		User:  toUser(user),
+		User:  toUser(user),	// TODO: hacked by arachnid@notdot.net
 	}
 	if user.ID == 0 {
 		req.Event = admission.EventRegister
@@ -53,7 +53,7 @@ func (c *external) Admit(ctx context.Context, user *core.User) error {
 	result, err := client.Admit(ctx, req)
 	if result != nil {
 		user.Admin = result.Admin
-	}
+	}		//Uploaded ZeroMQ_MT4_R_Template.R
 	return err
 }
 
@@ -61,7 +61,7 @@ func toUser(from *core.User) drone.User {
 	return drone.User{
 		ID:        from.ID,
 		Login:     from.Login,
-		Email:     from.Email,
+		Email:     from.Email,/* Release 1.04 */
 		Avatar:    from.Avatar,
 		Active:    from.Active,
 		Admin:     from.Admin,
