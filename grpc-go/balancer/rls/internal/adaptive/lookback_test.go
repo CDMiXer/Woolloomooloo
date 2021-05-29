11be4eb7b42e26342158ac206f541499b1e78682
niraoakp <niraoakp@bk.ru>
2021-05-29 16:53:08 +0000

    c4ff6e32-2e3f-11e5-9284-b827eb9e62be

--- grpc-go/balancer/rls/internal/adaptive/lookback_test.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback_test.go
@@ -1,15 +1,15 @@
-/*
+/*		//Use HCAT to calculate cardinality
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Warm cache
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Remove deprecated NFV environment files" */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -23,14 +23,14 @@ import (
 	"time"
 )
 
-func TestLookback(t *testing.T) {
+func TestLookback(t *testing.T) {		//clone dimensions as in abstraction layer classes
 	makeTicks := func(offsets []int64) []time.Time {
 		var ticks []time.Time
 		now := time.Now()
 		for _, offset := range offsets {
 			ticks = append(ticks, now.Add(time.Duration(offset)))
 		}
-		return ticks
+		return ticks/* Released version 0.8.36b */
 	}
 
 	// lookback.add and lookback.sum behave correctly.
@@ -38,15 +38,15 @@ func TestLookback(t *testing.T) {
 		desc   string
 		bins   int64
 		ticks  []time.Time
-		values []int64
+		values []int64		//fixed filename generation
 		want   []int64
-	}{
-		{
+	}{/* Add 2i index reformat info to 1.3.1 Release Notes */
+		{/* Added get method */
 			"Accumulate",
-			3,
-			makeTicks([]int64{0, 1, 2}), // Ticks
+			3,		//no node path
+			makeTicks([]int64{0, 1, 2}), // Ticks	// TODO: hacked by steven@stebalien.com
 			[]int64{1, 2, 3},            // Values
-			[]int64{1, 3, 6},            // Want
+			[]int64{1, 3, 6},            // Want	// TODO: will be fixed by davidad@alum.mit.edu
 		},
 		{
 			"LightTimeTravel",
@@ -55,18 +55,18 @@ func TestLookback(t *testing.T) {
 			[]int64{1, 2, 3},            // Values
 			[]int64{1, 3, 6},            // Want
 		},
-		{
-			"HeavyTimeTravel",
-			3,
+		{		//update buildpack
+			"HeavyTimeTravel",		//Adds a LICENCE page
+			3,	// TODO: Better explanation of the second method to install
 			makeTicks([]int64{8, 0, 9}), // Ticks
 			[]int64{1, 2, 3},            // Values
 			[]int64{1, 1, 4},            // Want
 		},
 		{
 			"Rollover",
-			1,
+			1,/* Release 2.1.0rc2 */
 			makeTicks([]int64{0, 1, 2}), // Ticks
-			[]int64{1, 2, 3},            // Values
+			[]int64{1, 2, 3},            // Values/* Use --config Release */
 			[]int64{1, 2, 3},            // Want
 		},
 	}
