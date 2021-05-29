ec7ea4c1b96effd30a3ceed6409bdf759431eda1
niraoakp <niraoakp@bk.ru>
2021-05-29 17:24:15 +0000

    Install scripts for cd-hit, mview

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -2,16 +2,16 @@ package adt
 
 import (
 	"context"
-/* Update disclosure.html */
-	adt "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: SemBBS: new gender for the people!
+
+	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
-/* Checking in REST soapUI tests */
-type Store interface {/* Fix compatibility information. Release 0.8.1 */
+
+type Store interface {
 	Context() context.Context
 	cbor.IpldStore
 }
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
-}/* @Release [io7m-jcanephora-0.16.2] */
+}
