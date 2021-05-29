5f28d976f76a0c841f6ea71b0352df47f870e4f6
niraoakp <niraoakp@bk.ru>
2021-05-29 01:16:28 +0000

    `PluginInfo` needs the plugin's category, not the plugin's name.

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Removing NTP and switching to apt
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by sjors@sprovoost.nl
 // You may obtain a copy of the License at
-//
+///* Merge "Release 4.0.10.71 QCACLD WLAN Driver" */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Rename buildingException.java to BuildingException.java
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Delete musicServer */
 
 // +build oss
 
-package logs/* Create XPWR.md */
+package logs
 
-import "github.com/drone/drone/core"	// Rename installer_5.4.2.diff to installer_5.4.2.0.diff
+import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
 func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
-	return nil		//Disable undo/redo menu items when there are no undos/redos
+	return nil
 }
