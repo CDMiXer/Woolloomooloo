863ac82ec4584c316c074aaceb52bf94c3817d3d
niraoakp <niraoakp@bk.ru>
2021-05-29 13:36:41 +0000

    01DX9aVBsKffsIy5B9aXv5YIAC4o1FxN

--- handler/api/repos/builds/logs/delete_test.go
+++ handler/api/repos/builds/logs/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* materiaux pour les sorts */
+// that can be found in the LICENSE file.
 
-package logs
+package logs/* Merge "ART: ReadFully needs to fail when EOF" */
