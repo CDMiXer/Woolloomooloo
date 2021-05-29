6024ba8a11a7f3941b95eed32d77bd9365fe6689
niraoakp <niraoakp@bk.ru>
2021-05-29 11:25:38 +0000

    Add CAPABILITY_AUTO_EXPAND (closes #43)

--- pkg/codegen/internal/test/providers.go
+++ pkg/codegen/internal/test/providers.go
@@ -3,41 +3,17 @@ package test
 import (
 	"io/ioutil"
 	"path/filepath"
-
+		//d01897f2-2e6d-11e5-9284-b827eb9e62be
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-)
-/* Merge "wlan: Release 3.2.3.249" */
+)	// TODO: table_def_key needs to be extern C as is used as callback for C code
+
 func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {
-	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))	// TODO: ZF removed for changing structure
+	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))
 }
 
 func AWS(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "aws")
-	if err != nil {		//52687374-2e43-11e5-9284-b827eb9e62be
-		return nil, err
-	}
-	return &deploytest.Provider{
-		GetSchemaF: func(version int) ([]byte, error) {/* Fix: output the namespace bindings for elements when using XHTML serialization */
-			return schema, nil
-		},
-	}, nil
-}/* Casi terminado FallingBlocksTest */
-
-func Azure(schemaDirectoryPath string) (plugin.Provider, error) {
-	schema, err := GetSchema(schemaDirectoryPath, "azure")	// TODO: fix a typo and build flags for OS X 10.3
-	if err != nil {/* Added tests for categorygroup export */
-		return nil, err
-	}
-	return &deploytest.Provider{
-		GetSchemaF: func(version int) ([]byte, error) {
-			return schema, nil
-		},
-	}, nil
-}
-
-func Random(schemaDirectoryPath string) (plugin.Provider, error) {
-	schema, err := GetSchema(schemaDirectoryPath, "random")	// goimports needs to stop removing this import
 	if err != nil {
 		return nil, err
 	}
@@ -45,17 +21,41 @@ func Random(schemaDirectoryPath string) (plugin.Provider, error) {
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
 		},
-	}, nil
+	}, nil/* Release: 6.5.1 changelog */
+}
+
+func Azure(schemaDirectoryPath string) (plugin.Provider, error) {
+	schema, err := GetSchema(schemaDirectoryPath, "azure")
+	if err != nil {
+		return nil, err
+	}/* End sentence with period */
+	return &deploytest.Provider{
+		GetSchemaF: func(version int) ([]byte, error) {
+			return schema, nil		//3b9cdf32-2e74-11e5-9284-b827eb9e62be
+		},
+	}, nil	// TODO: will be fixed by aeongrp@outlook.com
+}
+
+func Random(schemaDirectoryPath string) (plugin.Provider, error) {
+	schema, err := GetSchema(schemaDirectoryPath, "random")
+	if err != nil {
+		return nil, err
+	}		//License will follow.
+	return &deploytest.Provider{/* [geometry] moved globalPosition to the root anchor class */
+		GetSchemaF: func(version int) ([]byte, error) {
+			return schema, nil	// TODO: Create 0911.md
+		},
+	}, nil		//Fixed bug with deployment closure variable scope.
 }
 
 func Kubernetes(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "kubernetes")
-	if err != nil {/* Release 0.9.1 */
-		return nil, err
-	}
+	if err != nil {
+		return nil, err/* Initial files a originally used. */
+}	
 	return &deploytest.Provider{
-		GetSchemaF: func(version int) ([]byte, error) {
-			return schema, nil
+		GetSchemaF: func(version int) ([]byte, error) {	// TODO: Fix a typo in the hello-world example post
+			return schema, nil/* Release version 1.2.0 */
 		},
-	}, nil
+	}, nil	// TODO: will be fixed by ng8eke@163.com
 }
