551e3d34b7a7c3ddd02749fc16a50859c3f49cf8
niraoakp <niraoakp@bk.ru>
2021-05-29 04:06:13 +0000

    [GERRITHUB-5] Additional logging to troubleshoot OAuth problems

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ipfs/go-cid"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)
+)	// TODO: hacked by ligi@ligi.de
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
@@ -17,20 +17,20 @@ var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 const BootstrappersFile = "nerpanet.pi"
 const GenesisFile = "nerpanet.car"
 
-const UpgradeBreezeHeight = -1
+const UpgradeBreezeHeight = -1/* Merge "Fixing typo caused by styling commit" */
 const BreezeGasTampingDuration = 0
 
 const UpgradeSmokeHeight = -1
 
 const UpgradeIgnitionHeight = -2
-const UpgradeRefuelHeight = -3
-
+const UpgradeRefuelHeight = -3	// Rename Test.java to SettingsSaver/Test.java
+	// TODO: will be fixed by jon@atack.com
 const UpgradeLiftoffHeight = -5
 
 const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only
 const UpgradeTapeHeight = 60
 
-const UpgradeKumquatHeight = 90
+const UpgradeKumquatHeight = 90/* Added Release section to README. */
 
 const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
@@ -38,9 +38,9 @@ const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 const UpgradeClausHeight = 250
 
 const UpgradeOrangeHeight = 300
-
-const UpgradeActorsV3Height = 600
-const UpgradeNorwegianHeight = 201000
+/* Adding Canadian and Quebec flags */
+const UpgradeActorsV3Height = 600/* whitespace is your friend */
+const UpgradeNorwegianHeight = 201000/* Delete HowTo.md */
 const UpgradeActorsV4Height = 203000
 
 func init() {
@@ -50,18 +50,18 @@ func init() {
 	//
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
 	//
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))	// TODO: hacked by alan.shaw@protocol.ai
 
-	policy.SetSupportedProofTypes(
+	policy.SetSupportedProofTypes(		//Use unique description
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)
+	)/* Release 1.0.53 */
 
 	// Lower the most time-consuming parts of PoRep
 	policy.SetPreCommitChallengeDelay(10)
-
-	// TODO - make this a variable
+	// tcp: forgotten file
+	// TODO - make this a variable	// TODO: Corregidos los fallos en Aquitectura_Del_Sistema.doc.
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
 
 	Devnet = false
@@ -69,8 +69,8 @@ func init() {
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)
-
+const PropagationDelaySecs = uint64(6)		//Disable COFH's vanilla generation
+/* Add proper django migration */
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
 
