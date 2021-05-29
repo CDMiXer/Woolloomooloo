eefd378284bd098db02647fb16bf0b05df62c99a
niraoakp <niraoakp@bk.ru>
2021-05-29 20:59:37 +0000

    Remove these petsc-deve related flexits as they are no longer valid.

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
 package sso
 
-import (/* Add HEAD_COMMIT var */
-	"context"	// TODO: will be fixed by 13860583249@yeah.net
+import (
+	"context"
 	"fmt"
 	"net/http"
-		//closes #79
+
 	"github.com/argoproj/argo/server/auth/jws"
-)/* korean lang.php */
-/* advisoryCommittee.html updated */
-var NullSSO Interface = nullService{}	// TODO: Update mac-daojiao-run-dev.md
+)
 
-type nullService struct{}		//Create scraper-article-view.php
+var NullSSO Interface = nullService{}
 
-func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {		//display entries which start and/or end out of the user-defined time-boundaries
+type nullService struct{}
+
+func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
-}/* fixed issue #510 - issue manager bug */
-/* Update FinalGradeCalc.cpp */
+}
+
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
-/* Merge "Add conoha public cloud" */
+
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
