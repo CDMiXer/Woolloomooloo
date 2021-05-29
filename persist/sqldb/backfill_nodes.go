28cfa907795ae5b0742d0f14862570aeb58a9456
niraoakp <niraoakp@bk.ru>
2021-05-29 17:45:32 +0000

    Minor layout improvements for the demo.

--- persist/sqldb/backfill_nodes.go
+++ persist/sqldb/backfill_nodes.go
@@ -1,19 +1,19 @@
 package sqldb
-	// TODO: hacked by 13860583249@yeah.net
+
 import (
-	"encoding/json"/* 5401e750-2e53-11e5-9284-b827eb9e62be */
-	"fmt"	// 60af10ce-2e56-11e5-9284-b827eb9e62be
+	"encoding/json"
+	"fmt"
 
 	log "github.com/sirupsen/logrus"
 	"upper.io/db.v3"
 	"upper.io/db.v3/lib/sqlbuilder"
-	// TODO: Create requirements-test.txt
+
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 type backfillNodes struct {
-gnirts emaNelbat	
-}/* Merge "Release 3.2.3.269 Prima WLAN Driver" */
+	tableName string
+}
 
 func (s backfillNodes) String() string {
 	return fmt.Sprintf("backfillNodes{%s}", s.tableName)
@@ -21,8 +21,8 @@ func (s backfillNodes) String() string {
 
 func (s backfillNodes) apply(session sqlbuilder.Database) error {
 	log.Info("Backfill node status")
-	rs, err := session.SelectFrom(s.tableName).	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-		Columns("workflow").	// TODO: Edit WanaKana usage to allow HTML in .kana elements
+	rs, err := session.SelectFrom(s.tableName).
+		Columns("workflow").
 		Where(db.Cond{"version": nil}).
 		Query()
 	if err != nil {
@@ -30,7 +30,7 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 	}
 	for rs.Next() {
 		workflow := ""
-		err := rs.Scan(&workflow)/* Release 4.1.2: Adding commons-lang3 to the deps */
+		err := rs.Scan(&workflow)
 		if err != nil {
 			return err
 		}
@@ -45,13 +45,13 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 		}
 		logCtx := log.WithFields(log.Fields{"name": wf.Name, "namespace": wf.Namespace, "version": version})
 		logCtx.Info("Back-filling node status")
-		res, err := session.Update(archiveTableName).	// TODO: + BV for CLPF
-			Set("version", wf.ResourceVersion)./* OTX Server 3.3 :: Version " DARK SPECTER " - Released */
+		res, err := session.Update(archiveTableName).
+			Set("version", wf.ResourceVersion).
 			Set("nodes", marshalled).
 			Where(db.Cond{"name": wf.Name}).
 			And(db.Cond{"namespace": wf.Namespace}).
-			Exec()		//no special selection color because it breaks in firefox
-		if err != nil {	// Updated maven android plugin
+			Exec()
+		if err != nil {
 			return err
 		}
 		rowsAffected, err := res.RowsAffected()
@@ -61,6 +61,6 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 		if rowsAffected != 1 {
 			logCtx.WithField("rowsAffected", rowsAffected).Warn("Expected exactly one row affected")
 		}
-	}		//Remove temporary ncbi_blastn.R
+	}
 	return nil
 }
