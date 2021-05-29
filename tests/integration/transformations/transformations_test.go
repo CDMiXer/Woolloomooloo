2f1cac40261a5265357dffd18e447847b0d093e6
niraoakp <niraoakp@bk.ru>
2021-05-29 08:37:57 +0000

    Link to released stuff ; add pathfix

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,36 +1,36 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* check branches */
 
-package ints
-
-import (
+package ints/* New post: CRM Online Australia Releases IntelliChat for SugarCRM */
+		//Update DeplymentViewZanele.xml
+import (	// Merge branch 'master' into NIE-95-tooltip
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
-)
+)	// TODO: Reload window in settings tab when changing language.
 
 var Dirs = []string{
-	"simple",
+	"simple",	// TODO: modificações finais na classe
 }
-
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+/* Release version 0.3.5 */
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Released 1.0rc1. */
 	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
-		foundRes4Child := false
+		foundRes4Child := false/* Removed "development" tag from 0.5.0 version */
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
-				foundRes1 = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
+				foundRes1 = true/* renamed workspaceId to projectId in entity classes */
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))/* Release candidate 2.3 */
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
-			// "res2" has a transformation which adds additionalSecretOutputs to it's
+			// "res2" has a transformation which adds additionalSecretOutputs to it's/* Create Blue Iris Fusuin Trigger.groovy */
 			// "child"
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
@@ -42,7 +42,7 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 			// "res3" is impacted by a global stack transformation which sets
 			// optionalDefault to "stackDefault"
 			if res.URN.Name() == "res3" {
-				foundRes3 = true
+				foundRes3 = true	// TODO: hacked by witek@enjin.io
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
@@ -50,12 +50,12 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 			}
 			// "res4" is impacted by two component parent transformations which set
 			// optionalDefault to "default1" and then "default2" and also a global stack
-			// transformation which sets optionalDefault to "stackDefault".  The end
+			// transformation which sets optionalDefault to "stackDefault".  The end/* released parent */
 			// result should be "stackDefault".
 			if res.URN.Name() == "res4-child" {
-				foundRes4Child = true
+				foundRes4Child = true	// TODO: hacked by igor@soramitsu.co.jp
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))		//adding basic getting started notes
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
 				assert.Equal(t, "stackDefault", optionalInput.(string))
