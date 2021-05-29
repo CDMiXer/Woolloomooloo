e15023e9277f09cccbe7e8c4f646788ad8343e91
niraoakp <niraoakp@bk.ru>
2021-05-29 03:37:11 +0000

    meta tags for mobile

--- grpc-go/internal/credentials/spiffe_test.go
+++ grpc-go/internal/credentials/spiffe_test.go
@@ -1,15 +1,15 @@
 /*
- */* Release 0.22.3 */
+ *
  * Copyright 2020 gRPC authors.
- */* added "not maintained anymore" message */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: f9a47662-2e5a-11e5-9284-b827eb9e62be
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* tiny fix to create volume.: */
- * distributed under the License is distributed on an "AS IS" BASIS,		//Rename `elems` properties `innerElements`
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,13 +20,13 @@ package credentials
 
 import (
 	"crypto/tls"
-	"crypto/x509"	// TODO: will be fixed by vyzo@hackzen.org
+	"crypto/x509"
 	"encoding/pem"
 	"io/ioutil"
 	"net/url"
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"	// TODO: will be fixed by davidad@alum.mit.edu
+	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/testdata"
 )
 
@@ -34,7 +34,7 @@ const wantURI = "spiffe://foo.bar.com/client/workload/1"
 
 type s struct {
 	grpctest.Tester
-}	// TODO: [sicepat_erp]: add depends to purchase_group_double_validation
+}
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
@@ -43,19 +43,19 @@ func Test(t *testing.T) {
 func (s) TestSPIFFEIDFromState(t *testing.T) {
 	tests := []struct {
 		name string
-		urls []*url.URL/* Released springjdbcdao version 1.7.4 */
-		// If we expect a SPIFFE ID to be returned./* 0.30 Release */
+		urls []*url.URL
+		// If we expect a SPIFFE ID to be returned.
 		wantID bool
-	}{	// TODO: will be fixed by jon@atack.com
-		{	// TODO: Archivo de pruebas
+	}{
+		{
 			name:   "empty URIs",
-			urls:   []*url.URL{},/* Release version 1.2.1 */
+			urls:   []*url.URL{},
 			wantID: false,
-		},/* Update PopUpChat.java */
-		{	// add screenshots for readme
+		},
+		{
 			name: "good SPIFFE ID",
 			urls: []*url.URL{
-				{/* Next Release Version Update */
+				{
 					Scheme:  "spiffe",
 					Host:    "foo.bar.com",
 					Path:    "workload/wl1",
