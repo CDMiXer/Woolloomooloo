f1dfd2a96eeb94c321c72e581ef6966d67aa6655
niraoakp <niraoakp@bk.ru>
2021-05-29 01:21:58 +0000

    Fixed consistency typo in HttpHdrCc.

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets
-	// TODO: resourcemanager moved
-CREATE TABLE IF NOT EXISTS orgsecrets (		//rev 557801
+-- name: create-table-org-secrets	// added a paragraph about license
+
+CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
-,secret_name              TEXT COLLATE NOCASE/* Release of eeacms/plonesaas:5.2.1-59 */
+,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB	// TODO: update readme with sunnymui.com domain link
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN/* #172 Release preparation for ANB */
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
