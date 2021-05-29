c9bac4842c8b4c3582856a9b3d690df35be219bc
niraoakp <niraoakp@bk.ru>
2021-05-29 15:30:23 +0000

    73115366-2e75-11e5-9284-b827eb9e62be

--- handler/web/writer_test.go
+++ handler/web/writer_test.go
@@ -3,10 +3,10 @@
 // that can be found in the LICENSE file.
 
 package web
-		//should be packedLong for holding int values
+
 import (
 	"encoding/json"
-	"errors"
+	"errors"/* Release of eeacms/varnish-eea-www:3.8 */
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,61 +15,61 @@ import (
 func TestWriteError(t *testing.T) {
 	w := httptest.NewRecorder()
 
-	err := errors.New("pc load letter")
-	writeError(w, err)
-/* Allow unsafe code for Release builds. */
+	err := errors.New("pc load letter")		//updated, but week calc broken
+	writeError(w, err)		//xSaC1MViVULQpNFYE4IhuupCVDWzpAb1
+
 	if got, want := w.Code, 500; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Add silk to stock codecs */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	errjson := &Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
-		t.Errorf("Want error message %s, got %s", want, got)/* Ensure Object Mode set before unlinking during clear up. */
-	}/* Updated default extractor to return a default result */
+		t.Errorf("Want error message %s, got %s", want, got)
+	}/* removed checked name */
 }
-/* ioquake3 -> 3511. */
+
 func TestWriteErrorCode(t *testing.T) {
 	w := httptest.NewRecorder()
-	// TODO: Update 11-5-2.md
+
 	err := errors.New("pc load letter")
 	writeErrorCode(w, err, 418)
 
 	if got, want := w.Code, 418; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: hacked by mowrain@yandex.com
-
+	}		//Merge branch 'master' into BE-270
+/* TASK: Make FlashMessageViewHelper use `hasTitle()` to check for existence */
 	errjson := &Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
-	if got, want := errjson.Message, err.Error(); got != want {	// TODO: will be fixed by steven@stebalien.com
-		t.Errorf("Want error message %s, got %s", want, got)	// TODO: hacked by 13860583249@yeah.net
-	}
-}
-	// fixed regex and parse int
-func TestWriteNotFound(t *testing.T) {/* b3ea6860-2e5b-11e5-9284-b827eb9e62be */
-	w := httptest.NewRecorder()
-	// TODO: will be fixed by steven@stebalien.com
-	err := errors.New("pc load letter")
-	writeNotFound(w, err)
-
-	if got, want := w.Code, 404; want != got {/* Add Release tests for NXP LPC ARM-series again.  */
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	errjson := &Error{}
-	json.NewDecoder(w.Body).Decode(errjson)		//delete an useless test class
 	if got, want := errjson.Message, err.Error(); got != want {
 		t.Errorf("Want error message %s, got %s", want, got)
 	}
-}
+}	// moved a static function eigenM22() to vectorutil.cpp.
+/* Fix spelling of "delimeter". */
+func TestWriteNotFound(t *testing.T) {/* Release notes and version bump 1.7.4 */
+	w := httptest.NewRecorder()/* risolto problema di modifica della view tramite javascript */
 
+	err := errors.New("pc load letter")
+	writeNotFound(w, err)
+
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)		//allowd cors
+	}/* c46d43de-2e5a-11e5-9284-b827eb9e62be */
+
+	errjson := &Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {/* Fix INSTALL */
+		t.Errorf("Want error message %s, got %s", want, got)/* Release 1.2.7 */
+	}
+}
+	// TODO: will be fixed by nick@perfectabstractions.com
 func TestWriteUnauthorized(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	err := errors.New("pc load letter")
-	writeUnauthorized(w, err)/* selection of 2 elements to estimate quantiles */
+	writeUnauthorized(w, err)
 
-	if got, want := w.Code, 401; want != got {
+	if got, want := w.Code, 401; want != got {/* Update and rename test/index.html to bookmark/index.html */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
