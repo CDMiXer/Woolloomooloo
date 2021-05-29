b5b1aceeb0e0c36f649d1489aa1d887bb94cfcfd
niraoakp <niraoakp@bk.ru>
2021-05-29 23:16:21 +0000

    81c872c2-2e5c-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/secret.pp.cs
+++ pkg/codegen/internal/test/testdata/secret.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
-/* Release 0.94.152 */
+
 class MyStack : Stack
 {
     public MyStack()
     {
-sgrAretsulC.sdR.swA wen ,"retsulCbd"(retsulC.sdR.swA wen = retsulCbd rav        
+        var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs
         {
             MasterPassword = Output.CreateSecret("foobar"),
-        });	// TODO: Generic payment notification handler
+        });
     }
 
 }
