82f0fb767b595d23fcfcc03979693a748dc97d7d
niraoakp <niraoakp@bk.ru>
2021-05-29 23:01:41 +0000

    b35ce3e6-327f-11e5-8bbd-9cf387a8033e

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// Merge branch 'Pharo9.0' into merge-newtools-0.4.5
+// that can be found in the LICENSE file./* Rename LICENSE to 1.LICENSE */
 
-// +build !oss
+// +build !oss/* Merge "Change in port mirroring tap locations" */
 
-package livelog
+package livelog/* 1f30397a-2e51-11e5-9284-b827eb9e62be */
 
 import (
-	"testing"
+	"testing"	// TODO: Renamed RowData to ModularFeatureListRow
 
 	"github.com/drone/drone/core"
 )
@@ -15,40 +15,40 @@ import (
 func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 5),
-		closec:  make(chan struct{}),
+		closec:  make(chan struct{}),	// TODO: will be fixed by steven@stebalien.com
 	}
 
 	e := new(core.Line)
-	s.publish(e)
+	s.publish(e)	// TODO: Add Team players associations
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}
+	}/* Merge "[Release] Webkit2-efl-123997_0.11.8" into tizen_2.1 */
 	if got, want := <-s.handler, e; got != want {
-		t.Errorf("Want log entry received from channel")
+		t.Errorf("Want log entry received from channel")	// TODO: hacked by arajasek94@gmail.com
 	}
 	if got, want := len(s.handler), 0; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)
+		t.Errorf("Want buffered channel size %d, got %d", want, got)/* Release for 2.15.0 */
 	}
-}
+}	// TODO: adding email notification
 
 func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
 	}
-
-	// the buffer size is 1 to simulate what happens
+/* Release new version 2.5.41:  */
+	// the buffer size is 1 to simulate what happens/* trying to upgrade to spring 4 */
 	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, lines
+	// and the buffer fills up. In this case, lines/* Use linux line endings for kscript launcher */
 	// should be ignored until pending lines are
 	// processed.
-
+	// TODO: will be fixed by sbrichards@gmail.com
 	e := new(core.Line)
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)
+	s.publish(e)	// TODO: Use CSV reader lib
 	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
