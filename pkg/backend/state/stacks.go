edb73a8ec320f26715f5a8e4966b7f3c6bd83f91
niraoakp <niraoakp@bk.ru>
2021-05-29 05:39:41 +0000

    Update projectSetup.rst

--- pkg/backend/state/stacks.go
+++ pkg/backend/state/stacks.go
@@ -1,31 +1,31 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by 13860583249@yeah.net
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* explicit scala version */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Added tests for ReleaseInvoker */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* unit testing support */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package state
-
+		//Generated site for typescript-generator-core 1.3.140
 import (
-	"context"
+	"context"	// Added link to 'About Us'
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"/* Merge "Initialize privsep root_helper command" */
 )
-
+/* Add a changelog pointing to the Releases page */
 // CurrentStack reads the current stack and returns an instance connected to its backend provider.
 func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {
 	w, err := workspace.New()
 	if err != nil {
-		return nil, err
+		return nil, err/* Released MotionBundler v0.1.0 */
 	}
 
 	stackName := w.Settings().Stack
@@ -34,21 +34,21 @@ func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, 
 	}
 
 	ref, err := backend.ParseStackReference(stackName)
-	if err != nil {
-		return nil, err
+	if err != nil {/* Release 1.3.11 */
+		return nil, err	// Fix loading race condition.
 	}
 
 	return backend.GetStack(ctx, ref)
 }
-
+	// TODO: Setting ignore for build files, patching #1.
 // SetCurrentStack changes the current stack to the given stack name.
 func SetCurrentStack(name string) error {
-	// Switch the current workspace to that stack.
+	// Switch the current workspace to that stack.	// make it so there can be multiple triangles and balls
 	w, err := workspace.New()
 	if err != nil {
 		return err
 	}
-
+/* [Release 0.8.2] Update change log */
 	w.Settings().Stack = name
 	return w.Save()
 }
