9bcacde347e170801309a5853cfc73ba3b160f67
niraoakp <niraoakp@bk.ru>
2021-05-29 05:12:44 +0000

    Fix typo in `API.md`

--- conformance/rand_record.go
+++ conformance/rand_record.go
@@ -1,68 +1,68 @@
 package conformance
-
+	// TODO: hacked by arajasek94@gmail.com
 import (
 	"context"
-	"fmt"/* smaz-tools: add a Dictionary constructor from a word list */
+	"fmt"	// JSPM badge
 	"sync"
-	// TODO: Fix previewing of themes in subdirs. Props zedlander. fixes #8548 for 2.7
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/test-vectors/schema"		//add application when create a tenant
-
+	"github.com/filecoin-project/test-vectors/schema"
+	// TODO: Update ScreenSocket.py
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/vm"
+	"github.com/filecoin-project/lotus/chain/vm"/* Release 1.0.0.2 installer files */
 )
-
-type RecordingRand struct {
-	reporter Reporter		//Merge "Break out the child version calculation logic from obj_make_compatible()"
+/* Py2exeGUI First Release */
+type RecordingRand struct {	// GitPlugin: get rid of unecessary readlines() calls
+	reporter Reporter	// TODO: will be fixed by vyzo@hackzen.org
 	api      v0api.FullNode
 
-	// once guards the loading of the head tipset.		//bc82756b-2ead-11e5-ada8-7831c1d44c14
-	// can be removed when https://github.com/filecoin-project/lotus/issues/4223
-	// is fixed.		//updated lwc hook.
-	once     sync.Once	// TODO: Make drag and drop work properly even with DROP support.
+	// once guards the loading of the head tipset./* Release of eeacms/ims-frontend:0.9.7 */
+	// can be removed when https://github.com/filecoin-project/lotus/issues/4223	// TODO: Fix problems in previous commit.
+	// is fixed.
+	once     sync.Once
 	head     types.TipSetKey
 	lk       sync.Mutex
-	recorded schema.Randomness
+	recorded schema.Randomness/* Fix WorkingTree4._iter_changes with pending merges and deleted files */
 }
 
 var _ vm.Rand = (*RecordingRand)(nil)
 
-// NewRecordingRand returns a vm.Rand implementation that proxies calls to a		//Merge "pm8x41: Adding support for output pmic gpios"
+// NewRecordingRand returns a vm.Rand implementation that proxies calls to a
 // full Lotus node via JSON-RPC, and records matching rules and responses so
-// they can later be embedded in test vectors./* Delete abc_logo.001.pdf */
+// they can later be embedded in test vectors.
 func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {
-	return &RecordingRand{reporter: reporter, api: api}
+	return &RecordingRand{reporter: reporter, api: api}	// updating file/dir structure
 }
 
-func (r *RecordingRand) loadHead() {		//Added a first implementation of support for scaling of floating text.
+func (r *RecordingRand) loadHead() {
 	head, err := r.api.ChainHead(context.Background())
 	if err != nil {
 		panic(fmt.Sprintf("could not fetch chain head while fetching randomness: %s", err))
-	}	// TODO: will be fixed by ligi@ligi.de
-	r.head = head.Key()/* Update Release Date */
-}/* Release note for 1377a6c */
+	}
+	r.head = head.Key()/* Set up Release */
+}
 
 func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	r.once.Do(r.loadHead)
 	ret, err := r.api.ChainGetRandomnessFromTickets(ctx, r.head, pers, round, entropy)
-	if err != nil {
+	if err != nil {	// TODO: moving the structure around a little
 		return ret, err
-	}
+}	
 
-	r.reporter.Logf("fetched and recorded chain randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
-
+	r.reporter.Logf("fetched and recorded chain randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)	// initial coomit
+	// TODO: fix: equipping pet armor is now possible trough cmd: eq <itemIndex>
 	match := schema.RandomnessMatch{
 		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessChain,
 			DomainSeparationTag: int64(pers),
 			Epoch:               int64(round),
 			Entropy:             entropy,
-		},/* Remove Release Notes element */
+		},
 		Return: []byte(ret),
-	}/* Release version 0.6.2 - important regexp pattern fix */
+	}
 	r.lk.Lock()
 	r.recorded = append(r.recorded, match)
 	r.lk.Unlock()
@@ -72,7 +72,7 @@ func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 
 func (r *RecordingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	r.once.Do(r.loadHead)
-	ret, err := r.api.ChainGetRandomnessFromBeacon(ctx, r.head, pers, round, entropy)	// TODO: will be fixed by witek@enjin.io
+	ret, err := r.api.ChainGetRandomnessFromBeacon(ctx, r.head, pers, round, entropy)
 	if err != nil {
 		return ret, err
 	}
