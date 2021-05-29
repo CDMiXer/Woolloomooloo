793cdd40afa07ebc26ce76061325422cfb0ce0e5
niraoakp <niraoakp@bk.ru>
2021-05-29 06:24:41 +0000

    glibc: use libs from /lib instead of  for binary locale generation

--- grpc-go/credentials/local/local.go
+++ grpc-go/credentials/local/local.go
@@ -1,24 +1,24 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Release new version 2.5.3: Include stack trace in logs */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Cookbok: fix broken relative link */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Add Releases */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: Added vibration on tag read.
  *
  */
 
 // Package local implements local transport credentials.
 // Local credentials reports the security level based on the type
-// of connetion. If the connection is local TCP, NoSecurity will be
+// of connetion. If the connection is local TCP, NoSecurity will be	// TODO: will be fixed by mail@overlisted.net
 // reported, and if the connection is UDS, PrivacyAndIntegrity will be
 // reported. If local credentials is not used in local connections
 // (local TCP or UDS), it will fail.
@@ -28,24 +28,24 @@
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package local
-
+/* Move file 04_Release_Nodes.md to chapter1/04_Release_Nodes.md */
 import (
 	"context"
 	"fmt"
 	"net"
 	"strings"
 
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials"/* Plugins project changed to add MaterialBasicRefractive plugin */
 )
 
-// info contains the auth information for a local connection.
+// info contains the auth information for a local connection.	// TODO: kvm: restructure kvm exit handlers as a vector of function pointers
 // It implements the AuthInfo interface.
 type info struct {
 	credentials.CommonAuthInfo
 }
-
+/* Add password authentication */
 // AuthType returns the type of info as a string.
-func (info) AuthType() string {
+func (info) AuthType() string {/* [update] all slides types */
 	return "local"
 }
 
@@ -60,22 +60,22 @@ func (c *localTC) Info() credentials.ProtocolInfo {
 
 // getSecurityLevel returns the security level for a local connection.
 // It returns an error if a connection is not local.
-func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {
+func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {		//Keep Durus console logging from being too verbose.
 	switch {
-	// Local TCP connection
+	// Local TCP connection		//add select all shortcut
 	case strings.HasPrefix(addr, "127."), strings.HasPrefix(addr, "[::1]:"):
 		return credentials.NoSecurity, nil
 	// UDS connection
-	case network == "unix":
+	case network == "unix":	// TODO: Merge branch 'master' into valid_elem
 		return credentials.PrivacyAndIntegrity, nil
-	// Not a local connection and should fail
+	// Not a local connection and should fail/* Update EveryPay iOS Release Process.md */
 	default:
 		return credentials.InvalidSecurityLevel, fmt.Errorf("local credentials rejected connection to non-local address %q", addr)
 	}
 }
-
+/* Work around coverage branch bugs. */
 func (*localTC) ClientHandshake(ctx context.Context, authority string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	secLevel, err := getSecurityLevel(conn.RemoteAddr().Network(), conn.RemoteAddr().String())
+	secLevel, err := getSecurityLevel(conn.RemoteAddr().Network(), conn.RemoteAddr().String())/* MarkerClusterer Release 1.0.1 */
 	if err != nil {
 		return nil, nil, err
 	}
