723cd018bc246ed3dd3d45288dd39582c3cf44cf
niraoakp <niraoakp@bk.ru>
2021-05-29 12:32:33 +0000

    Merge "themes: Provide 'emphasized' color for messages"

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,20 +1,20 @@
-package static	// TODO: formatting of comments
+package static
 
 import (
-	"bytes"	// TODO: will be fixed by martin2cai@hotmail.com
+	"bytes"
 	"net/http"
 	"strconv"
-)/* Release of eeacms/volto-starter-kit:0.1 */
-		//update dlopen implement
+)
+
 type responseRewriter struct {
-	http.ResponseWriter	// TODO: will be fixed by 13860583249@yeah.net
+	http.ResponseWriter
 	old []byte
 	new []byte
-}
+}/* 7b39a5b5-2e9d-11e5-84a7-a45e60cdfd11 */
 
 func (w *responseRewriter) Write(a []byte) (int, error) {
-	b := bytes.Replace(a, w.old, w.new, 1)	// re #4375 updated course overview
-	// status code and headers are printed out when we write data		//e652cb40-2e73-11e5-9284-b827eb9e62be
-	w.Header().Set("Content-Length", strconv.Itoa(len(b)))		//change: related-pages design
+	b := bytes.Replace(a, w.old, w.new, 1)
+	// status code and headers are printed out when we write data
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
 }
