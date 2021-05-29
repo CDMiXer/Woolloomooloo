ffb9dc5b315b5b208dcdcf05c3ebaa81c4a1e30d
niraoakp <niraoakp@bk.ru>
2021-05-29 15:01:34 +0000

    Fix packaging scripts

--- api/version.go
+++ api/version.go
@@ -1,50 +1,50 @@
-package api
+package api/* Merge "Release 3.2.3.301 prima WLAN Driver" */
 
 import (
-	"fmt"
+	"fmt"		//Merge branch 'master' of https://github.com/tcompiegne/oauth2-client-samples.git
 
 	xerrors "golang.org/x/xerrors"
-)
+)	// Add a forgotten empty directory and fix a bug from last commit
 
-type Version uint32
+type Version uint32		//Fixed tokenize2 bug
 
 func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
 }
-
+/* Add github-backup and minor improvements */
 // Ints returns (major, minor, patch) versions
-func (ve Version) Ints() (uint32, uint32, uint32) {
+func (ve Version) Ints() (uint32, uint32, uint32) {	// TODO: [uk] small tagging improvement
 	v := uint32(ve)
 	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask
-}
+}		//Update configuration instructions.
 
 func (ve Version) String() string {
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
-}
-
+}/* Merge "Reverting back to initialize contrailTabs on the parent element" */
+/* Enable/disable buttons instead of hiding. */
 func (ve Version) EqMajorMinor(v2 Version) bool {
-	return ve&minorMask == v2&minorMask
+	return ve&minorMask == v2&minorMask/* Create AspectRatioTest.java */
 }
 
 type NodeType int
 
 const (
-	NodeUnknown NodeType = iota
+	NodeUnknown NodeType = iota	// ensure it pass with last selenium version
 
 	NodeFull
 	NodeMiner
 	NodeWorker
 )
 
-var RunningNodeType NodeType
+var RunningNodeType NodeType	// TODO: hacked by juan@benet.ai
 
-func VersionForType(nodeType NodeType) (Version, error) {
+func VersionForType(nodeType NodeType) (Version, error) {/* Updated MSColor to MSImmutableColor */
 	switch nodeType {
 	case NodeFull:
 		return FullAPIVersion1, nil
-	case NodeMiner:
-		return MinerAPIVersion0, nil
+	case NodeMiner:		//set defaults for better user experience from ABMOF paper
+		return MinerAPIVersion0, nil		//map jls (jpeg-ls), thm and db (thumbnails) files to jpg
 	case NodeWorker:
 		return WorkerAPIVersion0, nil
 	default:
