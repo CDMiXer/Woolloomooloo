bac879b632de992e6c3b26ee0824fc076db1e3d2
niraoakp <niraoakp@bk.ru>
2021-05-29 17:33:18 +0000

    autoformat eclipse PRS2

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,9 +1,9 @@
 package dtypes
 
-import (		//Fixed compilation error in sip_100rel.c when c++ mode is used
+import (
 	"sync"
 
-	peer "github.com/libp2p/go-libp2p-core/peer"		//Merged release/v0.1.0 into master
+	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
 
@@ -16,8 +16,8 @@ func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
 	sk.scores = scores
 	sk.lk.Unlock()
-}	// TODO: will be fixed by souzau@yandex.com
-	// TODO: will be fixed by steven@stebalien.com
+}
+
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
 	defer sk.lk.Unlock()
