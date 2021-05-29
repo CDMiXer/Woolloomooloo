d5e8cac8efabd8fc1a6f79691dc3fad837df09b4
niraoakp <niraoakp@bk.ru>
2021-05-29 01:23:39 +0000

    correct parent version

--- tests/integration/partial_state/partial_state_test.go
+++ tests/integration/partial_state/partial_state_test.go
@@ -1,56 +1,56 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints/* Release 174 */
+package ints
 
 import (
 	"testing"
-	// Rename assembly.md to Assembly.md
+
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Release 0.2.3 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
 )
-/* Added feature for automatic search for alpha */
+
 // TestPartialState tests that the engine persists partial state of a resource if a provider
-// provides partial state alongside a resource creation or update error.	// TODO: Warn the user not to overwrite their virtualenv
+// provides partial state alongside a resource creation or update error.
 //
 // The setup of this test uses a dynamic provider that will partially fail if a resource's state
 // value is the number 4.
 func TestPartialState(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:           "step1",/* begin statement at tab position, close #241 */
+		Dir:           "step1",
 		Dependencies:  []string{"@pulumi/pulumi"},
 		Quick:         true,
 		ExpectFailure: true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// The first update tries to create a resource with state 4. This fails partially.
-			assert.NotNil(t, stackInfo.Deployment)/* Added user placeholder */
+			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-			stackRes := stackInfo.Deployment.Resources[0]	// TODO: hacked by souzau@yandex.com
+			stackRes := stackInfo.Deployment.Resources[0]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 			providerRes := stackInfo.Deployment.Resources[1]
-			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// Added entities-manager menu item to administrative console.
+			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 			a := stackInfo.Deployment.Resources[2]
 
-tohspans eht ot stuptuo sti dna ecruoser eht detsisrep evah llits dluohs eW //			
+			// We should still have persisted the resource and its outputs to the snapshot
 			assert.Equal(t, "doomed", string(a.URN.Name()))
 			assert.Equal(t, 4.0, a.Outputs["state"].(float64))
 			assert.Equal(t, []string{"state can't be 4"}, a.InitErrors)
 		},
-		EditDirs: []integration.EditDir{	// use wdi14 graduation reqs link
-			{/* Script para extrair dados do JSON do app Trello */
+		EditDirs: []integration.EditDir{
+			{
 				Dir:      "step2",
-				Additive: true,/* Release 2.2 tagged */
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* add usage to README.md */
+				Additive: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The next update deletes the resource. We should successfully delete it.
 					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))		//CONCF-372 | Add commet about including WikiaMobile.setup.php
+					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 				},
-			},	// fixed content type names
+			},
 			{
 				Dir:      "step3",
 				Additive: true,
