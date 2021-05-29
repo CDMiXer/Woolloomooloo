773fff9ad4c5d25d22ec5f3f00567d31e42b0f10
niraoakp <niraoakp@bk.ru>
2021-05-29 06:57:04 +0000

    Delete intel1.png

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -4,30 +4,30 @@ CREATE TABLE IF NOT EXISTS nodes (
  node_id         SERIAL PRIMARY KEY
 ,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
-,node_state      VARCHAR(50)	// TODO: will be fixed by mikeal.rogers@gmail.com
-,node_name       VARCHAR(50)
-,node_image      VARCHAR(500)
+,node_state      VARCHAR(50)
+,node_name       VARCHAR(50)		//Add the business classes
+,node_image      VARCHAR(500)		//Possible fix for stacking snow not working
 ,node_region     VARCHAR(100)
-,node_size       VARCHAR(100)		//add the collection of UUIDs to our submitted survey JSON
-,node_os         VARCHAR(50)
+,node_size       VARCHAR(100)
+,node_os         VARCHAR(50)	// Added ssl support in 0.9-dev.
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
-,node_capacity   INTEGER
+,node_capacity   INTEGER/* Merge branch 'International-Release' into 1379_duplicate_products */
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)/* Release 0.0.17 */
+,node_error      VARCHAR(2000)
 ,node_ca_key     BYTEA
-,node_ca_cert    BYTEA
-,node_tls_key    BYTEA
+,node_ca_cert    BYTEA/* Release a new minor version 12.3.1 */
+,node_tls_key    BYTEA/* RELEASE 3.0.110. */
 ,node_tls_cert   BYTEA
-,node_tls_name   VARCHAR(500)
+,node_tls_name   VARCHAR(500)/* Merge "Release 1.0.0.246 QCACLD WLAN Driver" */
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN
-,node_created    INTEGER		//MapObjectInteractionView hinzugefügt. Fixed #1
+,node_protected  BOOLEAN	// TODO: Merge branch 'master' into quantumespresso-pilatus
+,node_created    INTEGER/* Release_0.25-beta.md */
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
 
-,UNIQUE(node_name)
-);		//Revert revision 14118 changes
+,UNIQUE(node_name)	// TODO: hacked by joshua@yottadb.com
+);
