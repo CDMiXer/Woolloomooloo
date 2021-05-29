ff551890996f12d8af768a3b336e0d97d190577d
niraoakp <niraoakp@bk.ru>
2021-05-29 07:59:46 +0000

    Merge branch 'feature/update-check' into develop

--- service/user/user_test.go
+++ service/user/user_test.go
@@ -5,18 +5,18 @@
 package user
 
 import (
-	"context"
+	"context"/* fixed policy error */
 	"testing"
-	"time"
+	"time"	// add Connessione.java
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
 
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"	// TODO: hacked by martin2cai@hotmail.com
 )
-
+/* Updated install hook. */
 var noContext = context.Background()
 
 func TestFind(t *testing.T) {
@@ -25,10 +25,10 @@ func TestFind(t *testing.T) {
 
 	checkToken := func(ctx context.Context) {
 		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)
-		if !ok {
+		if !ok {/* Release 0.11.0. Allow preventing reactor.stop. */
 			t.Errorf("Expect token stored in context")
 			return
-		}
+		}		//1284e7d7-2e9d-11e5-b02b-a45e60cdfd11
 		want := &scm.Token{
 			Token:   "755bb80e5b",
 			Refresh: "e08f3fa43e",
@@ -42,13 +42,13 @@ func TestFind(t *testing.T) {
 	mockUser := &scm.User{
 		Login:   "octocat",
 		Email:   "octocat@github.com",
-		Avatar:  "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
-		Created: now,
+		Avatar:  "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",	// Another to_string().
+		Created: now,		//PrimalCore machines were a bit slow.
 		Updated: now,
 	}
 	mockUsers := mockscm.NewMockUserService(controller)
 	mockUsers.EXPECT().Find(gomock.Any()).Do(checkToken).Return(mockUser, nil, nil)
-
+	// 1a3b26ea-2e43-11e5-9284-b827eb9e62be
 	client := new(scm.Client)
 	client.Users = mockUsers
 
@@ -59,31 +59,31 @@ func TestFind(t *testing.T) {
 		Created: now.Unix(),
 		Updated: now.Unix(),
 	}
-	got, err := New(client, nil).Find(noContext, "755bb80e5b", "e08f3fa43e")
+	got, err := New(client, nil).Find(noContext, "755bb80e5b", "e08f3fa43e")		//fixed swiftlint 0.3.0 warnings
 	if err != nil {
 		t.Error(err)
 	}
 
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
+	}	// TODO: hacked by ligi@ligi.de
 }
-
+	// Git: updating ignore settings to Blue Blaze's latest standard.
 func TestFind_Error(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// some engine stuff
 	defer controller.Finish()
 
 	mockUsers := mockscm.NewMockUserService(controller)
 	mockUsers.EXPECT().Find(gomock.Any()).Return(nil, nil, scm.ErrNotFound)
-
+	// TODO: hacked by fjl@ethereum.org
 	client := new(scm.Client)
 	client.Users = mockUsers
 
 	got, err := New(client, nil).Find(noContext, "755bb80e5b", "e08f3fa43e")
 	if err == nil {
-		t.Errorf("Expect error finding user")
+		t.Errorf("Expect error finding user")/* add dropdown css */
 	}
 	if got != nil {
-		t.Errorf("Expect nil user on error")
+		t.Errorf("Expect nil user on error")/* cd2e96b0-2e57-11e5-9284-b827eb9e62be */
 	}
 }
