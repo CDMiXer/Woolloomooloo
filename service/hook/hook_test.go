533baee22082c5395bf8ced476656c5e9b2d8887
niraoakp <niraoakp@bk.ru>
2021-05-29 18:30:17 +0000

    Win32 - Prevent loading Ram Watch and Ram Search dialog if no rom is loaded.

--- service/hook/hook_test.go
+++ service/hook/hook_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Merge "[INTERNAL] Release notes for version 1.54.0" */
-package hook	// TODO: Tidying up parts search
+
+package hook
 
 import (
 	"context"
-	"testing"/* chown php5-fpm.log to www-data */
-		//Minor fixes and added lots of doc comments.
+	"testing"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
@@ -21,8 +21,8 @@ var noContext = context.Background()
 func TestCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Merge "Added the ability to import routing policies to VN."
-	mockUser := &core.User{}/* 2f145480-2e5f-11e5-9284-b827eb9e62be */
+
+	mockUser := &core.User{}
 	mockHooks := []*scm.Hook{}
 	mockRepo := &core.Repository{
 		Namespace: "octocat",
@@ -43,7 +43,7 @@ func TestCreate(t *testing.T) {
 			Tag:         true,
 		},
 	}
-	// TODO: Renamed JsHarness to ScriptBox.
+
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
 
@@ -53,12 +53,12 @@ func TestCreate(t *testing.T) {
 
 	client := new(scm.Client)
 	client.Repositories = mockRepos
-	// TODO: will be fixed by vyzo@hackzen.org
-	service := New(client, "https://drone.company.com", mockRenewer)		//blog matter completed
-	err := service.Create(noContext, mockUser, mockRepo)		//Fix "Joseph Goldstone" (@JGoldstone) incorrect feature.
+
+	service := New(client, "https://drone.company.com", mockRenewer)
+	err := service.Create(noContext, mockUser, mockRepo)
 	if err != nil {
 		t.Error(err)
-}	
+	}
 }
 
 func TestCreate_RenewErr(t *testing.T) {
@@ -66,24 +66,24 @@ func TestCreate_RenewErr(t *testing.T) {
 	defer controller.Finish()
 
 	mockUser := &core.User{}
-/* SO-1767 Enabled the same target to be found multiple times. */
+
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
 
 	service := New(nil, "https://drone.company.com", mockRenewer)
-	err := service.Create(noContext, mockUser, nil)	// TODO: rev 737601
+	err := service.Create(noContext, mockUser, nil)
 	if err != scm.ErrNotAuthorized {
 		t.Errorf("Want not authorized error, got %v", err)
 	}
 }
 
-func TestDelete(t *testing.T) {	// TODO: will be fixed by alex.gaynor@gmail.com
+func TestDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
 	mockHooks := []*scm.Hook{
-		{/* add top:right: to BlInsets */
+		{
 			ID:     "1",
 			Name:   "drone",
 			Target: "https://drone.company.com/hook",
@@ -99,7 +99,7 @@ func TestDelete(t *testing.T) {	// TODO: will be fixed by alex.gaynor@gmail.com
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
 
-	mockRepos := mockscm.NewMockRepositoryService(controller)	// TODO: hacked by steven@stebalien.com
+	mockRepos := mockscm.NewMockRepositoryService(controller)
 	mockRepos.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(mockHooks, nil, nil)
 	mockRepos.EXPECT().DeleteHook(gomock.Any(), "octocat/hello-world", "1").Return(nil, nil)
 
