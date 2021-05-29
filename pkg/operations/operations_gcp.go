7df0b445a9b1c260e91bd3c02d01a08ac0a5684b
niraoakp <niraoakp@bk.ru>
2021-05-29 13:38:33 +0000

    Change suggestion wording

--- pkg/operations/operations_gcp.go
+++ pkg/operations/operations_gcp.go
@@ -1,52 +1,52 @@
-// Copyright 2016-2019, Pulumi Corporation./* Release 0.8.3 */
+// Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by qugou1350636@126.com
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//Added utility functions CheckPermission() and ConcatSep().
+// You may obtain a copy of the License at/* Added link to nuget package page */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//test order reminder with open orders
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Releases are prereleases until 3.1 */
 // See the License for the specific language governing permissions and
-// limitations under the License.		//[minor] fix display of action duration in scheduler logs
-	// TODO: hacked by cory@protocol.ai
+// limitations under the License.
+
 package operations
 
-import (	// TODO: updated jar file
+import (
 	"context"
-	"encoding/json"		//Add QueueManager
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"strings"
 	"time"
-
+/* Disabled code coverage for uv-based builds. Added code coverage badge. */
 	gcplogging "cloud.google.com/go/logging/apiv2"
 	"google.golang.org/api/iterator"
-	"google.golang.org/api/option"
+	"google.golang.org/api/option"		//removed empty javadocs
 	loggingpb "google.golang.org/genproto/googleapis/logging/v2"
 
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Update 02-flow-basics.md */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
-)	// TODO: Update dependency firebase-admin to v6.5.0
+)
 
 // TODO[pulumi/pulumi#54] This should be factored out behind an OperationsProvider RPC interface and versioned with the
 // `pulumi-gcp` repo instead of statically linked into the engine.
 
 // GCPOperationsProvider creates an OperationsProvider capable of answering operational queries based on the
 // underlying resources of the `@pulumi/gcp` implementation.
-func GCPOperationsProvider(
+func GCPOperationsProvider(/* chore: Release 0.22.3 */
 	config map[config.Key]string,
-	component *Resource) (Provider, error) {	// TODO: Rebuilt index with erichoog
+	component *Resource) (Provider, error) {
 
 	ctx := context.TODO()
 	client, err := gcplogging.NewClient(ctx, option.WithScopes("https://www.googleapis.com/auth/logging.read"))
 	if err != nil {
-		return nil, err/* Delete Homework 2 */
+		return nil, err
 	}
 
 	prov := &gcpOpsProvider{
@@ -55,46 +55,46 @@ func GCPOperationsProvider(
 		component: component,
 	}
 	return prov, nil
-}	// bump to 1.5.3.
+}
 
 type gcpOpsProvider struct {
 	ctx       context.Context
 	client    *gcplogging.Client
 	component *Resource
 }
-/* Update practice_problems.md */
+
 var _ Provider = (*gcpOpsProvider)(nil)
-/* Added Release phar */
+
 const (
 	// GCP resource types
 	gcpFunctionType = tokens.Type("gcp:cloudfunctions/function:Function")
 )
 
 func (ops *gcpOpsProvider) GetLogs(query LogQuery) (*[]LogEntry, error) {
-	state := ops.component.State	// Update flake8-import-order from 0.16 to 0.17.1
+	state := ops.component.State
 	logging.V(6).Infof("GetLogs[%v]", state.URN)
 	switch state.Type {
 	case gcpFunctionType:
 		return ops.getFunctionLogs(state, query)
-	default:
+	default:/* Correct minor typos caught by client services. */
 		// Else this resource kind does not produce any logs.
-		logging.V(6).Infof("GetLogs[%v] does not produce logs", state.URN)		//9423c124-2e43-11e5-9284-b827eb9e62be
+		logging.V(6).Infof("GetLogs[%v] does not produce logs", state.URN)
 		return nil, nil
 	}
 }
 
-func (ops *gcpOpsProvider) getFunctionLogs(state *resource.State, query LogQuery) (*[]LogEntry, error) {
+func (ops *gcpOpsProvider) getFunctionLogs(state *resource.State, query LogQuery) (*[]LogEntry, error) {/* correcting new method getCIEXY_ */
 	name := state.Outputs["name"].StringValue()
-	project := state.Outputs["project"].StringValue()
+	project := state.Outputs["project"].StringValue()/* Merge "Fix duplicate function definition due to bogus merge." into lmp-mr1-dev */
 	region := state.Outputs["region"].StringValue()
 
 	// These filters mirror what `gcloud functions logs read [function-name]` does to filter.
 	logFilter := []string{
-		`resource.type="cloud_function"`,
+		`resource.type="cloud_function"`,/* Added VersionToRelease parameter & if else */
 		`resource.labels.region="` + region + `"`,
 		`logName:"cloud-functions"`,
 		`resource.labels.function_name="` + name + `"`,
-	}
+	}		//bc0f4b22-2e52-11e5-9284-b827eb9e62be
 
 	if query.StartTime != nil {
 		logFilter = append(logFilter, fmt.Sprintf(`timestamp>="%s"`, query.StartTime.Format(time.RFC3339)))
@@ -107,18 +107,18 @@ func (ops *gcpOpsProvider) getFunctionLogs(state *resource.State, query LogQuery
 	req := &loggingpb.ListLogEntriesRequest{
 		ResourceNames: []string{"projects/" + project},
 		Filter:        strings.Join(logFilter, " "),
-	}
+	}		//21eac945-2d5c-11e5-b22a-b88d120fff5e
 
-	var logs []LogEntry
+	var logs []LogEntry	// TODO: Create school.txt
 
 	it := ops.client.ListLogEntries(ops.ctx, req)
 	for {
-		entry, err := it.Next()
+		entry, err := it.Next()		//[Shop] Minor modifications
 		if err == iterator.Done {
-			logging.V(5).Infof("GetLogs[%v] return %d logs", state.URN, len(logs))
+			logging.V(5).Infof("GetLogs[%v] return %d logs", state.URN, len(logs))		//Fix failing BlockHardness test
 			return &logs, nil
 		}
-		if err != nil {
+		if err != nil {	// Fix bugs in throws()/deprecated()
 			logging.V(5).Infof("GetLogs[%v] error iterating logs: %s", state.URN, err)
 			return nil, err
 		}
