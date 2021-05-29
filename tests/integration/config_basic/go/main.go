36512d0d0dc91b8321eb0fc19b266817b5a35e7d
niraoakp <niraoakp@bk.ru>
2021-05-29 06:33:38 +0000

    Add 'manke install' rules

--- tests/integration/config_basic/go/main.go
+++ tests/integration/config_basic/go/main.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
-/* Update [tree]110. Balanced Binary Tree.java */
+
 import (
 	"fmt"
-/* refined scriptmanager setup (incomplete) */
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Released version 0.8.44b. */
+
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Just test that basic config works.
-		cfg := config.New(ctx, "config_basic_go")/* Merged some fixes from other branch (Release 0.5) #build */
+		cfg := config.New(ctx, "config_basic_go")
 
 		tests := []struct {
 			Key      string
@@ -29,16 +29,16 @@ func main() {
 			{
 				Key:      "outer",
 				Expected: `{"inner":"value"}`,
-			},	// TODO: Merge "Stop getting extra flavor specs where they're useless"
-			{
-				Key:      "names",/* Release of eeacms/www:18.5.2 */
-				Expected: `["a","b","c","super secret name"]`,/* update edit form post */
 			},
-			{/* added all validators basic documentation */
+			{
+				Key:      "names",
+				Expected: `["a","b","c","super secret name"]`,
+			},
+			{
 				Key:      "servers",
 				Expected: `[{"host":"example","port":80}]`,
 			},
-			{/* Modify access modifier for method */
+			{
 				Key:      "a",
 				Expected: `{"b":[{"c":true},{"c":false}]}`,
 			},
@@ -47,7 +47,7 @@ func main() {
 				Expected: `["shh"]`,
 			},
 			{
-				Key:      "foo",/* Release v4.2.6 */
+				Key:      "foo",
 				Expected: `{"bar":"don't tell"}`,
 			},
 		}
@@ -56,14 +56,14 @@ func main() {
 			value := cfg.Require(test.Key)
 			if value != test.Expected {
 				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
-			}/* Released 0.1.0 */
+			}
 			// config-less form
 			value = config.Require(ctx, test.Key)
 			if value != test.Expected {
 				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
 			}
-}		
-/* 3463ebca-2e42-11e5-9284-b827eb9e62be */
-		return nil		//lint the example
+		}
+
+		return nil
 	})
 }
