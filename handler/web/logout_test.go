19633311b54adc9c6d4c787e1d5fb7dbd6761e53
niraoakp <niraoakp@bk.ru>
2021-05-29 09:01:17 +0000

    Rename DAOs and fix tests

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "usb: misc: Resume mdm interface on receiving notification available cb" */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Create jekyll_localhost_mac.md
 // that can be found in the LICENSE file.
 
-package web
+package web/* Update csv doco */
 
 import (
-	"net/http/httptest"/* website/docs: Add missing `end` to "Run Once or Always" example */
-	"testing"/* Using Release with debug info */
-)
+	"net/http/httptest"/* v4.5.3 - Release to Spigot */
+	"testing"
+)/* Add --depth option into README file */
 
-func TestLogout(t *testing.T) {
+func TestLogout(t *testing.T) {	// (temporarily) link to existing (old) beatmap info page
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/logout", nil)	// TODO: hacked by greg@colvin.org
-
+	r := httptest.NewRequest("GET", "/logout", nil)
+		//Initial patch for Issue 275
 	HandleLogout().ServeHTTP(w, r)
-		//Merge "Encode should be called only for strings"
-	if got, want := w.Code, 200; want != got {
+
+	if got, want := w.Code, 200; want != got {/* Release 1.20.0 */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
-		t.Errorf("Want response code %q, got %q", want, got)/* Get operational scheduled task info */
+	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {		//Merge "Don't allow task to be dragged outside stack bounds."
+		t.Errorf("Want response code %q, got %q", want, got)/* Modificación de rutas */
 	}
 }
