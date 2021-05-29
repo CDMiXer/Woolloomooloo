54267dc7ce61529f2ba2e5da96b600c91628f718
niraoakp <niraoakp@bk.ru>
2021-05-29 13:00:59 +0000

    9c356a82-2e4a-11e5-9284-b827eb9e62be

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./*  removing old content */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Remove mongodb */
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//Create To_do_list.md
 import { Resource } from "./resource";
 
 // The changing of a.state causes base to be DBR replaced. This in turn
@@ -10,18 +10,18 @@ import { Resource } from "./resource";
 //   2. DeleteReplacement Dependent
 //   3. DeleteReplacement Base
 //   4. Replace Base
-//   5. CreateReplacement Base
+//   5. CreateReplacement Base/* Update installation_detailed_linux.rst: prefer binary install for SCIP */
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
 //   6. Replace Dependent
 //   7. CreateReplacement Dependent
 const b = new Resource("dependent", { state: a.state });
 
-//   8. Update Dependent-2
+//   8. Update Dependent-2/* Merge "Release 1.1.0" */
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 //   9. Update Dependent-3
-const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });/* Cleanup lib/index */
+const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });	// Encode branch nick.
 
 //  10. Replace Dependent-4
 //  11. CreateReplacement Dependent-4
