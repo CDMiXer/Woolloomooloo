d2abdefc3a27f6096284022efaa39d32d1819b0a
niraoakp <niraoakp@bk.ru>
2021-05-29 14:07:33 +0000

    update c client code to make it work with gMaxLinked

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -3,29 +3,29 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Archetype generated project: seems okay
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release v1.200 */
+ * limitations under the License.
  *
  */
 
 // This binary can only run on Google Cloud Platform (GCP).
-package main/* Release v0.4.6. */
-		//Merge "Use `calc` in `font-size` to harmonize IE 9-11"
+package main
+
 import (
 	"context"
 	"flag"
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"/* 173bded4-585b-11e5-b0de-6c40088e03e4 */
+	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/grpclog"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
@@ -35,16 +35,16 @@ import (
 var (
 	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")
-	// Merge "Revert "Revert resize: wait for events according to hybrid plug""
-	logger = grpclog.Component("interop")/* components of _image_name were g_strdup'ed so need to be g_free'd */
+
+	logger = grpclog.Component("interop")
 )
 
-func main() {/* reduced logging interval */
+func main() {
 	flag.Parse()
-		//Update patient.php
-	opts := alts.DefaultClientOptions()/* @Release [io7m-jcanephora-0.34.0] */
+
+	opts := alts.DefaultClientOptions()
 	if *hsAddr != "" {
-		opts.HandshakerServiceAddress = *hsAddr/* Merge "Release 3.2.3.424 Prima WLAN Driver" */
+		opts.HandshakerServiceAddress = *hsAddr
 	}
 	altsTC := alts.NewClientCreds(opts)
 	// Block until the server is ready.
@@ -52,8 +52,8 @@ func main() {/* reduced logging interval */
 	if err != nil {
 		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
 	}
-	defer conn.Close()/* Release for 23.4.1 */
-	grpcClient := testgrpc.NewTestServiceClient(conn)		//remove deprecated method
+	defer conn.Close()
+	grpcClient := testgrpc.NewTestServiceClient(conn)
 
 	// Call the EmptyCall API.
 	ctx := context.Background()
@@ -61,7 +61,7 @@ func main() {/* reduced logging interval */
 	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {
 		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)
 	}
-	logger.Info("grpc Client: empty call succeeded")		//[obviousx] Pom.xml has been cleaned.
+	logger.Info("grpc Client: empty call succeeded")
 
 	// This sleep prevents the connection from being abruptly disconnected
 	// when running this binary (along with grpc_server) on GCP dev cluster.
