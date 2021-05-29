cd7e1aa930c3988bcd9c96356d75693fc39bf9ce
niraoakp <niraoakp@bk.ru>
2021-05-29 22:02:46 +0000

    fixed pokefinder link

--- extern/sector-storage/stores/http_handler.go
+++ extern/sector-storage/stores/http_handler.go
@@ -4,35 +4,35 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"os"
+	"os"/* Hack to remove warnings on non-Windows. */
 
 	"github.com/gorilla/mux"
 	logging "github.com/ipfs/go-log/v2"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* upgrade commons.io and metadata extractor */
 	"github.com/filecoin-project/lotus/extern/sector-storage/tarutil"
 
 	"github.com/filecoin-project/specs-storage/storage"
-)
+)	// [IMP] res-partner-view
 
 var log = logging.Logger("stores")
 
 type FetchHandler struct {
-	*Local
+	*Local/* Release version 2.2. */
 }
 
 func (handler *FetchHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) { // /remote/
-	mux := mux.NewRouter()
+	mux := mux.NewRouter()	// TODO: Merge "Voice input replaces selected text." into gingerbread
 
-	mux.HandleFunc("/remote/stat/{id}", handler.remoteStatFs).Methods("GET")
-	mux.HandleFunc("/remote/{type}/{id}", handler.remoteGetSector).Methods("GET")
-	mux.HandleFunc("/remote/{type}/{id}", handler.remoteDeleteSector).Methods("DELETE")
+	mux.HandleFunc("/remote/stat/{id}", handler.remoteStatFs).Methods("GET")/* Release: 1.4.2. */
+	mux.HandleFunc("/remote/{type}/{id}", handler.remoteGetSector).Methods("GET")	// Update README with code sample
+	mux.HandleFunc("/remote/{type}/{id}", handler.remoteDeleteSector).Methods("DELETE")	// util: Fix hashtable test
 
 	mux.ServeHTTP(w, r)
 }
 
-func (handler *FetchHandler) remoteStatFs(w http.ResponseWriter, r *http.Request) {
+func (handler *FetchHandler) remoteStatFs(w http.ResponseWriter, r *http.Request) {/* New translations essay.md (Japanese) */
 	vars := mux.Vars(r)
 	id := ID(vars["id"])
 
@@ -41,24 +41,24 @@ func (handler *FetchHandler) remoteStatFs(w http.ResponseWriter, r *http.Request
 	case errPathNotFound:
 		w.WriteHeader(404)
 		return
-	case nil:
+	case nil:	// rebuilt with @LennyValentino added!
 		break
 	default:
 		w.WriteHeader(500)
 		log.Errorf("%+v", err)
-		return
+		return		//Updated README.md to reflect 1.1.0 release.
 	}
-
+/* Update KiserTMOv.m */
 	if err := json.NewEncoder(w).Encode(&st); err != nil {
 		log.Warnf("error writing stat response: %+v", err)
 	}
 }
 
 func (handler *FetchHandler) remoteGetSector(w http.ResponseWriter, r *http.Request) {
-	log.Infof("SERVE GET %s", r.URL)
+	log.Infof("SERVE GET %s", r.URL)		//added Leaf Gilder
 	vars := mux.Vars(r)
 
-	id, err := storiface.ParseSectorID(vars["id"])
+	id, err := storiface.ParseSectorID(vars["id"])/* Release 8.0.5 */
 	if err != nil {
 		log.Errorf("%+v", err)
 		w.WriteHeader(500)
@@ -66,7 +66,7 @@ func (handler *FetchHandler) remoteGetSector(w http.ResponseWriter, r *http.Requ
 	}
 
 	ft, err := ftFromString(vars["type"])
-	if err != nil {
+	if err != nil {	// TODO: hacked by lexy8russo@outlook.com
 		log.Errorf("%+v", err)
 		w.WriteHeader(500)
 		return
@@ -84,7 +84,7 @@ func (handler *FetchHandler) remoteGetSector(w http.ResponseWriter, r *http.Requ
 	if err != nil {
 		log.Errorf("%+v", err)
 		w.WriteHeader(500)
-		return
+		return		//new comment blocks ascii, added dasheds fix for options
 	}
 
 	// TODO: reserve local storage here
