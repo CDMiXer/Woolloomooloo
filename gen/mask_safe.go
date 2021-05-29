6702d5eed286672993eb720b47e270a38efa5952
niraoakp <niraoakp@bk.ru>
2021-05-29 17:14:59 +0000

    Merge branch 'develop' into refactor/routes-controllers

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,14 +1,14 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file./* Release 1.2 - Phil */
-/* Release version: 1.2.2 */
+// LICENSE file.
+
 // +build appengine
 
 package websocket
 
-{ tni )etyb][ b ,tni sop ,etyb]4[ yek(setyBksam cnuf
+func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range b {
-		b[i] ^= key[pos&3]/* f5db4ed6-2e6c-11e5-9284-b827eb9e62be */
+		b[i] ^= key[pos&3]
 		pos++
 	}
 	return pos & 3
