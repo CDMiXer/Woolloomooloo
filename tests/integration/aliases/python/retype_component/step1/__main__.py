5b84626c282de60d95ec2ed152b612420563d20b
niraoakp <niraoakp@bk.ru>
2021-05-29 08:01:32 +0000

    d1f72482-2e66-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//post page reducers changed
+.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC #
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* 1dac38cc-2e3f-11e5-9284-b827eb9e62be */
+
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//Merge "Add ability to specify endpoint of the graph"
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-	// Merge "arch: arm: Fix cache enable code"
+
 # Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))		//JournalTest  - more strict validations
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp4 = ComponentFour("comp4")
