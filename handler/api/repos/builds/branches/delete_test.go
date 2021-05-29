a7d56d5a72ba9cbbca960944ee626efb64a61a50
niraoakp <niraoakp@bk.ru>
2021-05-29 22:47:34 +0000

    Merge branch 'stretch-unstable' into remove-deprecated-helpers

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Update pgq_case.md
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package branches
+package branches		//Apk file of the software
