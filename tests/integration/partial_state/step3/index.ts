49c3ba42613b41a304680c4397e25066a8cf4504
niraoakp <niraoakp@bk.ru>
2021-05-29 02:38:22 +0000

    I want to see if I can use the Bouncy Castle jar.

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//Corrected SSAO random mode. It couldn't work properly with precomputed sin/cos
-import { Resource } from "./resource";
-/* Add content to the new file HowToRelease.md. */
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";		//PHP 5.3 version...
+
 // resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
-/* Página novo usuário */
+/* Release 0.23.6 */
 // "a" should be in the checkpoint.
