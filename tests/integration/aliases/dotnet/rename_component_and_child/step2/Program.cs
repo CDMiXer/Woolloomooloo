7bb2796c56feb7906e3317ce4dc6bd7557db88dd
niraoakp <niraoakp@bk.ru>
2021-05-29 21:16:16 +0000

    rgaa22 rule 12.1  -> add testcases and inhib 12.2

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -3,7 +3,7 @@
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource/* Release version [10.0.1] - alfter build */
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
@@ -12,31 +12,31 @@ class Resource : ComponentResource/* Release version [10.0.1] - alfter build */
 }
 
 // Scenario #5 - composing #1 and #3
-class ComponentFive : ComponentResource		//7ed72db4-2e3a-11e5-935e-c03896053bdd
-{		//added open in browser instructions
+class ComponentFive : ComponentResource
+{
     private Resource resource;
 
     public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
-        { 		//Update builder.py
+        { 
             Parent = this,
             Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
         });
-    }		//delete hyperlinks
+    }
 }
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {	// Added link to initial blockchain data file
-        return Deployment.RunAsync(() =>/* Released code under the MIT License */
+    {
+        return Deployment.RunAsync(() =>
         {
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp5" } },
-            });		//README: A CSS required for keyboard navigation
+            });
         });
     }
 }
