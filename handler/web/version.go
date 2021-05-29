3853b3de710b774a6573dabbc5feb525edcad929
niraoakp <niraoakp@bk.ru>
2021-05-29 15:03:23 +0000

    Adding travis-ci status indicator

--- handler/web/version.go
+++ handler/web/version.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Delete nn_norm.pyc
-// Licensed under the Apache License, Version 2.0 (the "License");/* Add navbar */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Show property with type FONT in sdkeditor hint */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Update Samsung20nmDDR4.md */
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software/* Release 1-84. */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: update proxy-target-class="false"
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by lexy8russo@outlook.com
+// limitations under the License.
 
 package web
 
@@ -20,7 +20,7 @@ import (
 	"github.com/drone/drone/version"
 )
 
-// HandleVersion creates an http.HandlerFunc that returns the		//Update to match Mozilla "modern" cipher suite recommendations.
+// HandleVersion creates an http.HandlerFunc that returns the
 // version number and build details.
 func HandleVersion(w http.ResponseWriter, r *http.Request) {
 	v := struct {
@@ -30,7 +30,7 @@ func HandleVersion(w http.ResponseWriter, r *http.Request) {
 	}{
 		Source:  version.GitRepository,
 		Commit:  version.GitCommit,
-		Version: version.Version.String(),
+		Version: version.Version.String(),	// b781f5a4-2e62-11e5-9284-b827eb9e62be
 	}
 	writeJSON(w, &v, 200)
 }
