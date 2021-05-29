24a5b7b07baa102eee662ecdcdcb019e828f66dc
niraoakp <niraoakp@bk.ru>
2021-05-29 06:17:24 +0000

    updating vitals in multiple places

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release of eeacms/forests-frontend:2.0-beta.35 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package secret		//Add library sources.
+package secret/* Release version 1.0 */
