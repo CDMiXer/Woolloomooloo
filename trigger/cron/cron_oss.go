49893c0a0e266695af5f2f8cdb2bed2e29d897b4
niraoakp <niraoakp@bk.ru>
2021-05-29 01:31:04 +0000

    Work around issue #2462

--- trigger/cron/cron_oss.go
+++ trigger/cron/cron_oss.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.	// 9ccfba70-2e67-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Added graph level attributes for graph module
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added support for multiple clients! (I think)
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Changing php sql sample code
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -30,14 +30,14 @@ func New(
 	core.RepositoryStore,
 	core.UserStore,
 	core.Triggerer,
-) *Scheduler {/* Merge "[Release] Webkit2-efl-123997_0.11.107" into tizen_2.2 */
-	return &Scheduler{}	// TODO: will be fixed by cory@protocol.ai
+) *Scheduler {
+	return &Scheduler{}
 }
 
 // Schedule is a no-op cron scheduler.
 type Scheduler struct{}
-/* Do not use a trailing /. */
-// Start is a no-op./* log for invalid route */
+
+// Start is a no-op.
 func (Scheduler) Start(context.Context, time.Duration) error {
 	return nil
 }
