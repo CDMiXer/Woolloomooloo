9c596c85afcb643f23f7403f6b037fe5e08ca317
niraoakp <niraoakp@bk.ru>
2021-05-29 05:23:44 +0000

    Added named logger.

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -1,31 +1,31 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System;/* Merge "msm: crypto: set CLR_CNTXT bit for crypto operations" */
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
-/* K21peGVyby5jb20K */
+
 class Program
 {
-    static Task<int> Main(string[] args)	// TODO: hacked by ng8eke@163.com
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
-        {	// Merge branch 'master' into scenegraph-rename
-            var config = new Config();/* Merge "Introducing the Modification classses" */
+        {
+            var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
-            var oldVal = (string[])await a.GetValueAsync("val");	// TODO: will be fixed by martin2cai@hotmail.com
+            var oldVal = (string[])await a.GetValueAsync("val");
             if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
             {
                 throw new Exception("Invalid result");
             }
 
             return new Dictionary<string, object>
-            {/* Release 0.2.3 of swak4Foam */
+            {
                 { "val2", Output.CreateSecret(new[] { "a", "b" }) }
-            };	// Passage de la license du format txt vers markdown
+            };
         });
-    }	// TODO: will be fixed by yuvalalaluf@gmail.com
+    }
 }
