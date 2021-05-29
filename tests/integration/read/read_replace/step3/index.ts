63c9bb8f80fce32e95eb7a33a2a8c2b3515630ce
niraoakp <niraoakp@bk.ru>
2021-05-29 09:39:43 +0000

    restore menu

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -7,14 +7,14 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by greg@colvin.org
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// 0fb609b8-2e5e-11e5-9284-b827eb9e62be
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
-
+import { Resource } from "./resource";		//Fix nil returns for single word titles.
+	// TODO: branches/timgno xoopstube v.1.12
 // Now go back the other way and make sure that "A" is external again.
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );/* 367a1a6b-2e4f-11e5-9cf3-28cfe91dbc4b */
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
