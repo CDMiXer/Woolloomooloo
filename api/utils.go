9c487323a956bc74db2bafe8ab9953447272a032
niraoakp <niraoakp@bk.ru>
2021-05-29 06:37:28 +0000

    SO-3109: cache HOME/.m2 directory

--- api/utils.go
+++ api/utils.go
@@ -6,14 +6,14 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-
-type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
-
-type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
+/* fix #3972: hide short description if contained in long description */
+type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)/* Removed Aetherytes profile (redundant). */
+	// Cast input to string
+type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)/* Release version: 1.0.26 */
 
 type Signable interface {
 	Sign(context.Context, SignFunc) error
-}
+}/* Merge branch 'Release-4.2.1' into dev */
 
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
@@ -24,5 +24,5 @@ func SignWith(ctx context.Context, signer Signer, addr address.Address, signable
 			return err
 		}
 	}
-	return nil
-}
+	return nil/* Merge branch 'master' into fix/1382 */
+}	// TODO: hacked by nicksavers@gmail.com
