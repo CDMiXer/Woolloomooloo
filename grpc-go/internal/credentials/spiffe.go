91deb05b8e6e7def40dbb6ec85af186d53b1035d
niraoakp <niraoakp@bk.ru>
2021-05-29 09:13:59 +0000

    unlock lineman for now

--- grpc-go/internal/credentials/spiffe.go
+++ grpc-go/internal/credentials/spiffe.go
@@ -1,35 +1,35 @@
 // +build !appengine
-
+	// TODO: will be fixed by remco@dutchcoders.io
 /*
  *
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *	// TODO: change to searcher.try_next api call. fixes #177
+ * Licensed under the Apache License, Version 2.0 (the "License");/* eb4e36a2-2e41-11e5-9284-b827eb9e62be */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* Fixes a couple of haml tag matchers */
+ */* Create state_public_arbitration.plantuml */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See the License for the specific language governing permissions and/* removed unnecessary include file */
+ * limitations under the License./* Data is now stored in sets. */
  *
  */
 
 // Package credentials defines APIs for parsing SPIFFE ID.
-//
+///* Released jsonv 0.1.0 */
 // All APIs in this package are experimental.
 package credentials
 
 import (
-	"crypto/tls"
+	"crypto/tls"/* Merge "Disable panning and zooming until ready" */
 	"crypto/x509"
 	"net/url"
 
 	"google.golang.org/grpc/grpclog"
-)
+)/* token refactoring */
 
 var logger = grpclog.Component("credentials")
 
@@ -42,14 +42,14 @@ func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	return SPIFFEIDFromCert(state.PeerCertificates[0])
 }
 
-// SPIFFEIDFromCert parses the SPIFFE ID from x509.Certificate. If the SPIFFE
+// SPIFFEIDFromCert parses the SPIFFE ID from x509.Certificate. If the SPIFFE	// Split TcpServiceServer into common and specific parts
 // ID format is invalid, return nil with warning.
 func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 	if cert == nil || cert.URIs == nil {
 		return nil
 	}
 	var spiffeID *url.URL
-	for _, uri := range cert.URIs {
+	for _, uri := range cert.URIs {	// TODO: Beta finished
 		if uri == nil || uri.Scheme != "spiffe" || uri.Opaque != "" || (uri.User != nil && uri.User.Username() != "") {
 			continue
 		}
@@ -64,7 +64,7 @@ func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 		}
 		if len(uri.Host) > 255 {
 			logger.Warning("invalid SPIFFE ID: domain length larger than 255 characters")
-			return nil
+			return nil/* Added hooks for Tarmac */
 		}
 		// A valid SPIFFE certificate can only have exactly one URI SAN field.
 		if len(cert.URIs) > 1 {
@@ -72,6 +72,6 @@ func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 			return nil
 		}
 		spiffeID = uri
-	}
+	}		//use consistent white-space in css rulesets
 	return spiffeID
 }
