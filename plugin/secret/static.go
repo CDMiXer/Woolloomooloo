92a9b7dcc9bab44918f32411960026061fa0ae26
niraoakp <niraoakp@bk.ru>
2021-05-29 00:07:39 +0000

    FIX: cal gain when limit leaf val

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -2,18 +2,18 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Add unit tests moved from standards
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Update Readme for new Release. */
+//      http://www.apache.org/licenses/LICENSE-2.0	// Merge "Remove tox envirnoment for pypy"
 //
-// Unless required by applicable law or agreed to in writing, software	// Tools: DFG: Beautify Register output by adding LSB start index information.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [tools] firmware-utils/mkcsysimg: minor bugfix */
-// See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
-		//Remove merge artifacts
-package secret	// fixed white space tabs
-	// Kernel Optimizations
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Correct some typos */
+// limitations under the License.
+
+package secret
+		//Update phases, grad criteria, and target release.
 import (
 	"context"
 	"strings"
@@ -22,27 +22,27 @@ import (
 )
 
 // Static returns a new static Secret controller.
-func Static(secrets []*core.Secret) core.SecretService {
+func Static(secrets []*core.Secret) core.SecretService {	// mv structEndpoint endpoint, set IPIF agent to hold cs
 	return &staticController{secrets: secrets}
-}/* Added "Connection to server" guide */
+}		//added getUserIp()
 
-type staticController struct {/* Merge "Strip version from service catalog endpoint" */
+type staticController struct {
 	secrets []*core.Secret
-}/* 1.2.4-FIX Release */
+}
 
-func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
+func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {	// TODO: added missing include for NPInterface
 	for _, secret := range c.secrets {
-		if !strings.EqualFold(secret.Name, in.Name) {/* New Release notes view in Nightlies. */
-			continue		//Fix: adding a new bss was failing 
+		if !strings.EqualFold(secret.Name, in.Name) {
+			continue
 		}
 		// The secret can be restricted to non-pull request
 		// events. If the secret is restricted, return
-		// empty results.
+		// empty results.	// Enchancement - Add support for ioredis Cluster object initialization
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
-			continue
+			continue/* test if index.html can be reused */
 		}
 		return secret, nil
-	}	// fix for filters
+	}
 	return nil, nil
 }
