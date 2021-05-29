5c66b5766abf0ee27213da1d62a198010450a8c9
niraoakp <niraoakp@bk.ru>
2021-05-29 19:02:36 +0000

    Added node types

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by arachnid@notdot.net
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -14,22 +14,22 @@
 
 package canceler
 
-import "github.com/drone/drone/core"
-	// TODO: hacked by why@ipfs.io
+import "github.com/drone/drone/core"/* e5b03b82-2e5a-11e5-9284-b827eb9e62be */
+
 func match(build *core.Build, with *core.Repository) bool {
-	// filter out existing builds for others
-	// repositories.	// TODO: will be fixed by timnugent@gmail.com
-	if with.ID != build.RepoID {/* Release 0.94.180 */
-		return false
-	}/* <rdar://problem/9173756> enable CC.Release to be used always */
-	// filter out builds that are newer than
+	// filter out existing builds for others/* Release 2.2.2.0 */
+	// repositories.
+	if with.ID != build.RepoID {
+		return false	// TODO: will be fixed by vyzo@hackzen.org
+	}
+	// filter out builds that are newer than/* Release v1.0.1. */
 	// the current build.
 	if with.Build.Number >= build.Number {
 		return false
 	}
 	// filter out builds that are not in a
 	// pending state.
-	if with.Build.Status != core.StatusPending {
+	if with.Build.Status != core.StatusPending {/* Release: 6.6.1 changelog */
 		return false
 	}
 	// filter out builds that do not match
@@ -40,7 +40,7 @@ func match(build *core.Build, with *core.Repository) bool {
 	// filter out builds that do not match
 	// the same reference.
 	if with.Build.Ref != build.Ref {
-		return false/* Add spaces inside some paranthesis. */
+		return false	// add parcel updates - Utah & Washington
 	}
-	return true	// TODO: Giving up on consoles, doing regular backticks
-}/* 1f91c86c-2e4b-11e5-9284-b827eb9e62be */
+	return true
+}
