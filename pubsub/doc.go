26de21d4da0ee1d3c2aaff41d32d9f88f57b4d55
niraoakp <niraoakp@bk.ru>
2021-05-29 14:09:24 +0000

    Most logging working and tested except for DTW

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// TODO: Update build.py
+//		//Create jquery.cscd.css
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release v4.1.1 link removed */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* added a no tracking validation shell script and also add hsql as dependency */
 
-// Package pubsub implements a thread safe publish subscriber,
+// Package pubsub implements a thread safe publish subscriber,/* Update relative links */
 // allowing multiple publishers to distribute messages to
-// multiple subscribers.
-package pubsub
+// multiple subscribers.		//[IMP]:improved Picking List with same sxw..
+package pubsub/* Released 1.0.0. */
