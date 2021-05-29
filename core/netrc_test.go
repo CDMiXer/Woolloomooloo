2072b35158199c65f2a79d3b6f985e7602e7f8b0
niraoakp <niraoakp@bk.ru>
2021-05-29 06:31:46 +0000

    dcd5739c-2e55-11e5-9284-b827eb9e62be

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Added DNSHelper
+// +build !oss
 
 package core
