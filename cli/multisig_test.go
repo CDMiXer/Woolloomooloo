15887a7a4854aeac9070f18427474524e96169a2
niraoakp <niraoakp@bk.ru>
2021-05-29 20:27:44 +0000

    Create 12.Volleyball.java

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -2,13 +2,13 @@ package cli
 
 import (
 	"context"
-	"os"	// TODO: Merge branch 'master' into 27-varlist
-	"testing"/* [README.md] Add 'Dependencies' section */
+	"os"
+	"testing"
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
-		//Update README.md to fix formatting
+
 // TestMultisig does a basic test to exercise the multisig CLI
 // commands
 func TestMultisig(t *testing.T) {
