9630008f5900f28d88e5c3ee7f5220b9f6fa1cc2
niraoakp <niraoakp@bk.ru>
2021-05-29 04:56:56 +0000

    move rpi and brata api

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Fixed some build error :P */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 1.0.0rc1.1 */
+	// TODO: formulario do question
 // +build !oss
-
+		//solution for UVA 482
 package metric
 
-import "github.com/drone/drone/core"/* [dist] Release v1.0.1 */
+import "github.com/drone/drone/core"
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
 	// track days until expires
-	// track user limit
-	// track repo limit
+	// track user limit	// TODO: will be fixed by sebs@2xs.org
+	// track repo limit/* Started fleshing out usage section in readme */
 }
