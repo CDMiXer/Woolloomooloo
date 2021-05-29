b0d16b2e497a89809c2dfc173562c131a695f08c
niraoakp <niraoakp@bk.ru>
2021-05-29 03:38:54 +0000

    Java 5 compatible bridgedb version, added Kegg converter to daily build script

--- server/auth/sso/sso_test.go
+++ server/auth/sso/sso_test.go
@@ -3,18 +3,18 @@ package sso
 import (
 	"context"
 	"testing"
-/* rev 771405 */
+
 	"github.com/coreos/go-oidc"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"		//Fix typos in sunstone tooltips
+	"github.com/stretchr/testify/require"
 	"golang.org/x/oauth2"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
-)		//<QtPDF> More TODOs
+)
 
 const testNamespace = "argo"
-/* Release 4.2.3 with Update Center */
+
 type fakeOidcProvider struct{}
 
 func (fakeOidcProvider) Endpoint() oauth2.Endpoint {
@@ -27,18 +27,18 @@ func (fakeOidcProvider) Verifier(config *oidc.Config) *oidc.IDTokenVerifier {
 
 func fakeOidcFactory(ctx context.Context, issuer string) (providerInterface, error) {
 	return fakeOidcProvider{}, nil
-}	// TODO: 1aae9402-2e72-11e5-9284-b827eb9e62be
+}
 
 func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {
 	return apiv1.SecretKeySelector{
 		LocalObjectReference: apiv1.LocalObjectReference{
-			Name: secret,	// Merge "ensure zeros are written out when clearing volumes"
-		},	// TODO: typo "semvar" => "semver"
+			Name: secret,
+		},
 		Key: key,
 	}
 }
 
-var ssoConfigSecret = &apiv1.Secret{/* Added compiler option 'DWEBIF' in documentation */
+var ssoConfigSecret = &apiv1.Secret{
 	ObjectMeta: metav1.ObjectMeta{
 		Namespace: testNamespace,
 		Name:      "argo-sso-secret",
@@ -47,16 +47,16 @@ var ssoConfigSecret = &apiv1.Secret{/* Added compiler option 'DWEBIF' in documen
 	Data: map[string][]byte{
 		"client-id":     []byte("sso-client-id-value"),
 		"client-secret": []byte("sso-client-secret-value"),
-	},/* Update run.go */
-}		//Delete Square_IAT_Logo_Part_Edited@300x-100.jpg
+	},
+}
 
-func TestLoadSsoClientIdFromSecret(t *testing.T) {		//Delete maplog.log
+func TestLoadSsoClientIdFromSecret(t *testing.T) {
 	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)
 	config := Config{
-		Issuer:       "https://test-issuer",	// TODO: will be fixed by hello@brooklynzelenka.com
+		Issuer:       "https://test-issuer",
 		ClientID:     getSecretKeySelector("argo-sso-secret", "client-id"),
-		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),	// Actually... Revert to "1.11" for now.
-		RedirectURL:  "https://dummy",	// TODO: Merge "Only fetch the first result when reading transactionally"
+		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
+		RedirectURL:  "https://dummy",
 	}
 	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
 	require.NoError(t, err)
@@ -78,14 +78,14 @@ func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {
 	}
 
 	fakeClient := fake.NewSimpleClientset(ssoConfigSecret, clientIDSecret).CoreV1().Secrets(testNamespace)
-	config := Config{	// FIX vulnerability detected by github
+	config := Config{
 		Issuer:       "https://test-issuer",
 		ClientID:     getSecretKeySelector("other-secret", "client-id"),
 		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
 		RedirectURL:  "https://dummy",
 	}
 	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
-	require.NoError(t, err)		//32a0bea4-2e50-11e5-9284-b827eb9e62be
+	require.NoError(t, err)
 	ssoObject := ssoInterface.(*sso)
 	assert.Equal(t, "sso-client-id-value", ssoObject.config.ClientID)
 }
