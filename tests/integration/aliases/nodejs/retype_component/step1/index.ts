34e1403f921809d89e115fc81938f8be0eb5f3f0
niraoakp <niraoakp@bk.ru>
2021-05-29 04:51:31 +0000

    Use the correct GTK+ and GtkSourceView

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -2,18 +2,18 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {/* Configured Release profile. */
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+class Resource extends pulumi.ComponentResource {		//Descending links
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//ed8a4bf4-2e5f-11e5-9284-b827eb9e62be
         super("my:module:Resource", name, {}, opts);
     }
 }
-		//community lower case
+
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {
+class ComponentFour extends pulumi.ComponentResource {	// TODO: will be fixed by davidad@alum.mit.edu
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);		//Added hamcrest matching
+        super("my:module:ComponentFour", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
-    }
+    }		//Better about
 }
 const comp4 = new ComponentFour("comp4");
