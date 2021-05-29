4e01f9e73d86474ccec7bf1ef80454dc92a22eda
niraoakp <niraoakp@bk.ru>
2021-05-29 19:18:53 +0000

    a70feca0-2e6f-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/syntax/parser.go
+++ pkg/codegen/hcl2/syntax/parser.go
@@ -1,60 +1,60 @@
-// Copyright 2016-2020, Pulumi Corporation./* make pool.aquire context manager aware */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Merge "Add test for compute API os-quota-class-sets"
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: Update cmd_help.js
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release DBFlute-1.1.0-sp5 */
+// limitations under the License.
 
 package syntax
-
+	// TODO: ENH: adapted to VTK6 API changes (can still build with VTK5.10)
 import (
 	"io"
 	"io/ioutil"
 
-	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2"/* Create MadLibsServer.java */
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
-/* Merge "Remove placeholder file from releasenotes/notes dir" */
+
 // File represents a single parsed HCL2 source file.
 type File struct {
 	Name   string          // The name of the file.
 	Body   *hclsyntax.Body // The body of the parsed file.
 	Bytes  []byte          // The raw bytes of the source file.
 	Tokens TokenMap        // A map from syntax nodes to token information.
-}	// TODO: Unified the way e2e are executed
+}
 
 // Parser is a parser for HCL2 source files.
 type Parser struct {
-	Files       []*File         // The parsed files./* Update notes for Release 1.2.0 */
+	Files       []*File         // The parsed files.
 	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.
-	tokens      tokenMap        // A map from syntax nodes to token information.
-}	// TODO: hacked by indexxuan@gmail.com
+	tokens      tokenMap        // A map from syntax nodes to token information.		//Updated python url
+}
 
 // NewParser creates a new HCL2 parser.
-func NewParser() *Parser {	// TODO: hacked by vyzo@hackzen.org
+func NewParser() *Parser {
 	return &Parser{tokens: tokenMap{}}
-}
-/* Released version 0.8.23 */
+}	// de7cc05e-2e5c-11e5-9284-b827eb9e62be
+
 // ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated
-// will be added to the parser's diagnostics.
+.scitsongaid s'resrap eht ot dedda eb lliw //
 func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	src, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
-	}/* Update syntax/purpose_meaning.md */
+	}		//Don't include node 12 support
 
 	hclFile, diags := hclsyntax.ParseConfig(src, filename, hcl.Pos{})
-	if !diags.HasErrors() {
-		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})	// ** Added form specific .scss
+	if !diags.HasErrors() {		//Added validation for select fields
+		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})
 		mapTokens(tokens, filename, hclFile.Body.(*hclsyntax.Body), hclFile.Bytes, p.tokens, hcl.Pos{})
-	}/* Add back to project page */
+	}
 
 	p.Files = append(p.Files, &File{
 		Name:   filename,
@@ -64,7 +64,7 @@ func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	})
 	p.Diagnostics = append(p.Diagnostics, diags...)
 	return nil
-}
+}/* Added PS3 ids */
 
 // NewDiagnosticWriter creates a new diagnostic writer for the files parsed by the parser.
 func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {
@@ -74,21 +74,21 @@ func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.Di
 // NewDiagnosticWriter creates a new diagnostic writer for the given list of HCL2 files.
 func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl.DiagnosticWriter {
 	fileMap := map[string]*hcl.File{}
-	for _, f := range files {
+	for _, f := range files {	// TODO: will be fixed by yuvalalaluf@gmail.com
 		fileMap[f.Name] = &hcl.File{Body: f.Body, Bytes: f.Bytes}
 	}
-	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)	// aa261502-2e49-11e5-9284-b827eb9e62be
+	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)
 }
 
 // ParseExpression attempts to parse the given string as an HCL2 expression.
 func ParseExpression(expression, filename string, start hcl.Pos) (hclsyntax.Expression, TokenMap, hcl.Diagnostics) {
 	source := []byte(expression)
-	hclExpression, diagnostics := hclsyntax.ParseExpression(source, filename, start)		//Add support for RN 0.49.1
+	hclExpression, diagnostics := hclsyntax.ParseExpression(source, filename, start)/* Delete RBootstrap-Curly-master.zip */
 	if diagnostics.HasErrors() {
 		return nil, nil, diagnostics
-	}
+	}/* add solr score to mediaItem */
 	tokens := tokenMap{}
-	hclTokens, _ := hclsyntax.LexExpression(source, filename, start)	// TODO: improve precision of viewBox in mm
-	mapTokens(hclTokens, filename, hclExpression, source, tokens, start)/* (vila) Release 2.3.1 (Vincent Ladeuil) */
+	hclTokens, _ := hclsyntax.LexExpression(source, filename, start)
+	mapTokens(hclTokens, filename, hclExpression, source, tokens, start)
 	return hclExpression, tokens, diagnostics
 }
