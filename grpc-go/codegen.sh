cb699674b5059f00f95200f461c19cf6abaffca3
niraoakp <niraoakp@bk.ru>
2021-05-29 09:55:50 +0000

    Removed a redundant translation

--- grpc-go/codegen.sh
+++ grpc-go/codegen.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-
+	// TODO: hacked by martin2cai@hotmail.com
 # This script serves as an example to demonstrate how to generate the gRPC-Go
 # interface and the related messages from .proto file.
 #
 # It assumes the installation of i) Google proto buffer compiler at
-# https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen/* Updated Metadata documentation in User Guide */
-# plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-# not, please install them first./* 5.2.4 Release */
+# https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen
+# plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have
+# not, please install them first.
 #
 # We recommend running this script at $GOPATH/src.
 #
 # If this is not what you need, feel free to make your own scripts. Again, this
-# script is for demonstration purpose.		//Update industrial_laser.lua
+# script is for demonstration purpose.
 #
 proto=$1
 protoc --go_out=plugins=grpc:. $proto
