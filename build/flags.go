7d5445addc89d48ba6c33d57c8207b7f3aa637c0
niraoakp <niraoakp@bk.ru>
2021-05-29 16:51:25 +0000

    Update ArvoreBinaria.h

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
-package build
-
+package build	// TODO: Updating the Prettify example with updated directive.
+	// TODO: Make jsex depend on jsx application
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
-// etc.	// TODO: hacked by timnugent@gmail.com
-///* Merge branch 'develop' into feature/IFS-108 */
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,	// TODO: fix a line in Plot_xyz
+// etc.
+//	// TODO: will be fixed by magik6k@gmail.com
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
 //
-// This is useful when you're using Lotus as a library, such as to orchestrate
+// This is useful when you're using Lotus as a library, such as to orchestrate/* ajout d'un lien vers le livre */
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
 //
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
-var DisableBuiltinAssets = false
+var DisableBuiltinAssets = false	// repackage Form>>#stride to Sparta-Core
