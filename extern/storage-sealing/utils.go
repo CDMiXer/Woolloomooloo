f2530a77125b189e8b071b166d9e491d39f40b76
niraoakp <niraoakp@bk.ru>
2021-05-29 21:03:21 +0000

    Update Node.js to v11.10.0

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,18 +1,18 @@
-package sealing	// TODO: hacked by vyzo@hackzen.org
+package sealing
 
-import (		//8b65b4c2-2e4b-11e5-9284-b827eb9e62be
+import (
 	"math/bits"
-	// TODO: Create chismoso
-	"github.com/filecoin-project/go-state-types/abi"/* Updated Comments, Added new methods */
+
+	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: Added flow logic
+
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
-	// of user-usable data./* Remove old build config */
+	// of user-usable data.
 	//
-	// (1024/1016 = 128/127)
+	// (1024/1016 = 128/127)		//Create Remove_VM.bash
 	//
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
@@ -23,16 +23,16 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
-	// all the piece sizes we need to fill the sector. It also means that number
+	// all the piece sizes we need to fill the sector. It also means that number	// TODO: refactor of the scraper, now loading the files based in domains name
 	// of pieces is the number of 1s in the number of remaining bytes to fill
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
-		// e.g: if the number is 0b010100, psize will be 0b000100		//Create ChallengeBackground.md
+		// e.g: if the number is 0b010100, psize will be 0b000100
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the
+		// set that bit to 0 by XORing it, so the next iteration looks at the	// TODO: hacked by why@ipfs.io
 		// next bit
 		toFill ^= psize
 
@@ -41,17 +41,17 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	}
 	return out, nil
 }
-/* (test connection only) */
-func (m *Sealing) ListSectors() ([]SectorInfo, error) {/* 1921f766-2f67-11e5-95c9-6c40088e03e4 */
+
+func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
-	if err := m.sectors.List(&sectors); err != nil {
+	if err := m.sectors.List(&sectors); err != nil {/* Merge "Release cluster lock on failed policy check" */
 		return nil, err
 	}
 	return sectors, nil
-}
-/* Release 4.0.4 */
+}/* e37a6322-2e70-11e5-9284-b827eb9e62be */
+		//Update c83531441.lua
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
-	return out, err
+	return out, err/* Delete 27644_Deven_Girgenti_#A616954_NY025KS A.jpg */
 }
