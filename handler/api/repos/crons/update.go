ed744d103035fdadb11e4abafc7474671a79bcda
niraoakp <niraoakp@bk.ru>
2021-05-29 02:48:50 +0000

    fixed backup lib test

--- handler/api/repos/crons/update.go
+++ handler/api/repos/crons/update.go
@@ -7,49 +7,49 @@
 package crons
 
 import (
-	"encoding/json"
+	"encoding/json"	// Add step-by-step how-to get the app running to README
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-
+	"github.com/drone/drone/core"	// TODO: Merge "Fix heat-dashboard build"
+	"github.com/drone/drone/handler/api/render"/* Release update. */
+	// TODO: will be fixed by mikeal.rogers@gmail.com
 	"github.com/go-chi/chi"
 )
 
-type cronUpdate struct {
-	Branch   *string `json:"branch"`
-	Target   *string `json:"target"`
+type cronUpdate struct {/* Fixing XML validation errors */
+	Branch   *string `json:"branch"`/* feat: Engine.compile is thread-safe */
+	Target   *string `json:"target"`		//Vorbereitung 1.6.0-3
 	Disabled *bool   `json:"disabled"`
 }
 
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to enable or disable a cron job.
 func HandleUpdate(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* Releaseeeeee. */
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
-			namespace = chi.URLParam(r, "owner")
+		var (		//Some more stuff for README.md
+			namespace = chi.URLParam(r, "owner")/* Release  2 */
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {/* Merge "Release 1.0.0.164 QCACLD WLAN Driver" */
 			render.NotFound(w, err)
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
-		if err != nil {
+		if err != nil {/* Release 1.11 */
 			render.NotFound(w, err)
-			return
-		}
+			return/* (update notes) */
+		}/* Add a comment about a code smell */
 
 		in := new(cronUpdate)
 		json.NewDecoder(r.Body).Decode(in)
-		if in.Branch != nil {
+		if in.Branch != nil {/* Update pom for Release 1.4 */
 			cronjob.Branch = *in.Branch
-		}
+		}	// TODO: ApiEntrepriseService #fetch -> #get_etablissement_params_for_siret
 		if in.Target != nil {
 			cronjob.Target = *in.Target
 		}
