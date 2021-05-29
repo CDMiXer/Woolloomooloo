b8176f0c1c5916616395468f3116e6ffa48a0a8b
niraoakp <niraoakp@bk.ru>
2021-05-29 06:35:30 +0000

    Remove HTTP base url setting

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release: Making ready for next release cycle 4.5.3 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,16 +10,16 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by witek@enjin.io
+// limitations under the License./* add token protect tag. */
 
-package webhook/* cb1a8d1c-2e9c-11e5-91a4-a45e60cdfd11 */
+package webhook
 
 import "github.com/drone/drone/core"
 
 // Config provides the webhook configuration.
 type Config struct {
-	Events   []string	// TODO: Create longestCommonPrefix.py
+	Events   []string	// backing up before first run
 	Endpoint []string
 	Secret   string
 	System   *core.System
-}/* 1.4.1 Release */
+}
