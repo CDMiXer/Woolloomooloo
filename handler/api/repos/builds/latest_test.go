7dd53a4a4f27c91481ffd84a445cdacfc0fec396
niraoakp <niraoakp@bk.ru>
2021-05-29 12:50:17 +0000

    Fixing failing failover tests

--- handler/api/repos/builds/latest_test.go
+++ handler/api/repos/builds/latest_test.go
@@ -1,64 +1,64 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: handle the 'metadata_path" deps
-// that can be found in the LICENSE file./* Release 8. */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+/* Task #3483: Merged Release 1.3 with trunk */
+package builds/* Create AHAOTU CHIAGORO 13CK015345 EEE */
 
-package builds
-
-import (/* Add version resolver to Release Drafter */
-	"context"	// TODO: will be fixed by steven@stebalien.com
+import (	// TODO: let glut application be an event object
+	"context"
 	"encoding/json"
-	"net/http/httptest"	// update collab design
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/handler/api/errors"
-/* Delete facebook_s.png */
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-)/* rzs bugfix: Anzeige im Stundenplan */
-/* Release 0.95 */
+	"github.com/google/go-cmp/cmp"/* Release 0.95.185 */
+)
+
 func TestLast(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: Create printnum.asm
+	controller := gomock.NewController(t)/* Release 1.3.0.0 Beta 2 */
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)/* Update Post “one-api-to-rule-them-all” */
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(mockBuild, nil)
 
-	stages := mock.NewMockStageStore(controller)
+	stages := mock.NewMockStageStore(controller)/* Release memory before each run. */
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")		//Metadata > Hashmap
 	c.URLParams.Add("name", "hello-world")
-
-	w := httptest.NewRecorder()
+/* Use $PWD instead of dot for readability. */
+	w := httptest.NewRecorder()	// Delete 6f1.png
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// rev 805594
 	)
 
-	HandleLast(repos, builds, stages)(w, r)	// TODO: - Modulo de competencia NagoJudge (90%)
+	HandleLast(repos, builds, stages)(w, r)
 
-	if got, want := w.Code, 200; want != got {/* Law chooser style (not actually used) */
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}		//Issue #11, don't log an error for manual orders
 
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
-	json.NewDecoder(w.Body).Decode(got)	// TODO: hacked by yuvalalaluf@gmail.com
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* minor change concerning legends of distribution operator  */
-		t.Errorf(diff)/* removed header info */
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
 	}
 }
 
 func TestLast_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: will be fixed by cory@protocol.ai
-	// Not fully working yet
-	repos := mock.NewMockRepositoryStore(controller)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)	// word mistake in data/config.desc
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
@@ -66,13 +66,13 @@ func TestLast_RepoNotFound(t *testing.T) {
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// TODO: hacked by nick@perfectabstractions.com
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)/* Editor: Remove Contact Form Feature Flag :triangular_flag_on_post: (#4545) */
 
-	HandleLast(repos, nil, nil)(w, r)
+	HandleLast(repos, nil, nil)(w, r)	// TODO: hacked by sebastian.tharakan97@gmail.com
 
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
