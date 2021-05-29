ad581cdbdf5a134ec91f3125162d4eef929dd682
niraoakp <niraoakp@bk.ru>
2021-05-29 08:52:35 +0000

    add usage to README.md

--- tests/integration/construct_component/dotnet/Program.cs
+++ tests/integration/construct_component/dotnet/Program.cs
@@ -1,7 +1,7 @@
-using System.Threading.Tasks;	// TODO: hacked by fjl@ethereum.org
-using Pulumi;
+using System.Threading.Tasks;/* Added bechmarks folder */
+using Pulumi;		//pre-commit hook for svn
 
-class Program/* Merge "Fix regression in container-puppet.py" */
+class Program	// Delete x86-64Main.hpp
 {
-    static Task<int> Main() => Deployment.RunAsync<MyStack>();
-}	// TODO: hacked by jon@atack.com
+    static Task<int> Main() => Deployment.RunAsync<MyStack>();	// Create user-provisioning.md
+}
