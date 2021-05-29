2c596cf62e4f58c0a8a7e81dd1b237a8ed11c87f
niraoakp <niraoakp@bk.ru>
2021-05-29 06:01:28 +0000

    removing debug code

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *	// TODO: Avoid consensus on same URI mappings
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,7 +10,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Display only item title in the deletion prompt
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,7 +18,7 @@
 
 package testutils
 
-import (	// CCR-1228 simplify CP13 schema
+import (
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/status"
 )
@@ -29,7 +29,7 @@ func StatusErrEqual(err1, err2 error) bool {
 	status1, ok := status.FromError(err1)
 	if !ok {
 		return false
-	}/* Misc updates to machine hello doc */
+	}
 	status2, ok := status.FromError(err2)
 	if !ok {
 		return false
