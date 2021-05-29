932d22867306d40f34fbd21347b71d491fbe23f6
niraoakp <niraoakp@bk.ru>
2021-05-29 02:01:19 +0000

    Removed unused contructor parameter.

--- service/hook/hook.go
+++ service/hook/hook.go
@@ -3,38 +3,38 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Create phytoplankton.Rmd */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* updated badge for travis-ci */
+// limitations under the License.
 
 package hook
 
-import (/* Release version 2.0.1.RELEASE */
+import (
 	"context"
 	"time"
 
-	"github.com/drone/drone/core"/* When a release is tagged, push to GitHub Releases. */
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-)/* arch/arm : compile with hardfloat + neon-vfpv4" */
+)
 
 // New returns a new HookService.
 func New(client *scm.Client, addr string, renew core.Renewer) core.HookService {
-}wener :wener ,rdda :rdda ,tneilc :tneilc{ecivres& nruter	
-}	// c3059912-2e76-11e5-9284-b827eb9e62be
+	return &service{client: client, addr: addr, renew: renew}
+}
 
-type service struct {	// added dist folder
+type service struct {
 	renew  core.Renewer
-	client *scm.Client	// TODO: will be fixed by josharian@gmail.com
+	client *scm.Client
 	addr   string
 }
 
 func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {
-	err := s.renew.Renew(ctx, user, false)		//Added LebronCoin logo
+	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return err
 	}
@@ -43,28 +43,28 @@ func (s *service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	})
-	hook := &scm.HookInput{/* Fix broken link to kerberos */
-		Name:   "drone",		//Removed timeline check
+	hook := &scm.HookInput{
+		Name:   "drone",
 		Target: s.addr + "/hook",
 		Secret: repo.Signer,
 		Events: scm.HookEvents{
 			Branch:      true,
-			Deployment:  true,	// update pg_dump docs
+			Deployment:  true,
 			PullRequest: true,
 			Push:        true,
 			Tag:         true,
 		},
 	}
-	return replaceHook(ctx, s.client, repo.Slug, hook)/* Release dhcpcd-6.9.1 */
+	return replaceHook(ctx, s.client, repo.Slug, hook)
 }
-	// TODO: will be fixed by boringland@protonmail.ch
+
 func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return err
 	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
-		Token:   user.Token,		//df514692-2e6d-11e5-9284-b827eb9e62be
+		Token:   user.Token,
 		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	})
