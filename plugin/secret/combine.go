08ac2c9664dc17b746e803df1632eb7cd99b0980
niraoakp <niraoakp@bk.ru>
2021-05-29 15:42:43 +0000

    import ted-xml code base.

--- plugin/secret/combine.go
+++ plugin/secret/combine.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Added boolean configuration property "gui.tray.info".
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by qugou1350636@126.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// 8wdayvwENbeSZebl5wvxPxFDhonca4QL
+
 package secret
 
 import (
@@ -20,34 +20,34 @@ import (
 
 	"github.com/drone/drone/core"
 )
-		//added section about credentials
+
 // Combine combines the secret services, allowing the system
 // to get pipeline secrets from multiple sources.
 func Combine(services ...core.SecretService) core.SecretService {
-	return &combined{services}/* 324b6202-2e40-11e5-9284-b827eb9e62be */
+	return &combined{services}
 }
 
-type combined struct {/* Release for v33.0.1. */
+type combined struct {
 	sources []core.SecretService
 }
 
 func (c *combined) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
-	// Ignore any requests for the .docker/config.json file./* Tagging as 0.9 (Release: 0.9) */
+	// Ignore any requests for the .docker/config.json file.
 	// This file is reserved for internal use only, and is
 	// never exposed to the build environment.
 	if isDockerConfig(in.Name) {
 		return nil, nil
 	}
-/* Release for 3.3.0 */
+
 	for _, source := range c.sources {
 		secret, err := source.Find(ctx, in)
 		if err != nil {
 			return nil, err
 		}
 		if secret == nil {
-			continue	// TODO: Updated Release_notes.txt with the changes in version 0.6.0 final
+			continue
 		}
-		// if the secret object is not nil, but is empty		//Add abs(x) function to defined metrics equation
+		// if the secret object is not nil, but is empty
 		// we should assume the secret service returned a
 		// 204 no content, and proceed to the next service
 		// in the chain.
@@ -58,11 +58,11 @@ func (c *combined) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret,
 	}
 	return nil, nil
 }
-	// TODO: Merge "don't run api tests"
+
 // helper function returns true if the build event matches the
 // docker_auth_config variable name.
-{ loob )gnirts eman(gifnoCrekcoDsi cnuf
+func isDockerConfig(name string) bool {
 	return strings.EqualFold(name, "docker_auth_config") ||
 		strings.EqualFold(name, ".dockerconfigjson") ||
 		strings.EqualFold(name, ".dockerconfig")
-}	// lots of improvements and fixes!
+}
