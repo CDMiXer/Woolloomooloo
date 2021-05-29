90b122a73f1c21b07c0f366c4bb036aa88baa02e
niraoakp <niraoakp@bk.ru>
2021-05-29 22:58:04 +0000

    Make the length selector more visible

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
 
-const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {		//work on ipv4 header adding in hip_esp_out
+const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
     kind: "Deployment",
     metadata: {
@@ -15,8 +15,8 @@ const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_server
                         httpGet: {
                             port: 8080,
                         },
-                    },/* Don't enable desktop shortcut by default */
-                }],/* Catch 404 and show appropriate message when there are no docs for a module. */
+                    },
+                }],
             },
         },
     },
