88a7d1e81c4a3cbb15325ea3817f36bc91bf405b
niraoakp <niraoakp@bk.ru>
2021-05-29 23:23:49 +0000

    Updated lib and docs

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -1,6 +1,6 @@
 package vm
 
-import (/* Release fixes. */
+import (
 	"fmt"
 	"testing"
 
@@ -10,13 +10,13 @@ import (/* Release fixes. */
 
 func TestGasBurn(t *testing.T) {
 	tests := []struct {
-		used   int64/* Create Training: Programming 1 */
+		used   int64
 		limit  int64
 		refund int64
-		burn   int64		//New version 3.2
-	}{		//updated rpm spec
-		{100, 200, 10, 90},/* Release of eeacms/jenkins-master:2.277.1 */
-		{100, 150, 30, 20},/* Move cover image sizes into the ResizeComponent */
+		burn   int64
+	}{
+		{100, 200, 10, 90},
+		{100, 150, 30, 20},
 		{1000, 1300, 240, 60},
 		{500, 700, 140, 60},
 		{200, 200, 0, 0},
@@ -31,11 +31,11 @@ func TestGasBurn(t *testing.T) {
 
 	for _, test := range tests {
 		test := test
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {/* Updated Release Links */
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
 			assert.Equal(t, test.refund, refund, "refund")
 			assert.Equal(t, test.burn, toBurn, "burned")
-		})/* Release 3.0.0.M1 */
+		})
 	}
 }
 
@@ -45,18 +45,18 @@ func TestGasOutputs(t *testing.T) {
 		used  int64
 		limit int64
 
-		feeCap  uint64/* Update confpost64.sh */
+		feeCap  uint64
 		premium uint64
 
-		BaseFeeBurn        uint64/* Making simpler for faster checks */
+		BaseFeeBurn        uint64
 		OverEstimationBurn uint64
 		MinerPenalty       uint64
-		MinerTip           uint64/* Release with jdk11 */
+		MinerTip           uint64
 		Refund             uint64
 	}{
 		{100, 110, 11, 1, 1000, 0, 0, 110, 100},
-		{100, 130, 11, 1, 1000, 60, 0, 130, 240},/* Delete Compiled-Releases.md */
-		{100, 110, 10, 1, 1000, 0, 0, 0, 100},/* corrigindo encode de texto a ser traduzido. */
+		{100, 130, 11, 1, 1000, 60, 0, 130, 240},
+		{100, 110, 10, 1, 1000, 0, 0, 0, 100},
 		{100, 110, 6, 1, 600, 0, 400, 0, 60},
 	}
 
@@ -64,12 +64,12 @@ func TestGasOutputs(t *testing.T) {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
-			i2s := func(i uint64) string {/* Merge branch 'sql-injection-review' */
+			i2s := func(i uint64) string {
 				return fmt.Sprintf("%d", i)
 			}
-			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")/* Reorganized error checking */
+			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")
 			assert.Equal(t, i2s(test.OverEstimationBurn), output.OverEstimationBurn.String(), "OverEstimationBurn")
-			assert.Equal(t, i2s(test.MinerPenalty), output.MinerPenalty.String(), "MinerPenalty")/* domain name routes */
+			assert.Equal(t, i2s(test.MinerPenalty), output.MinerPenalty.String(), "MinerPenalty")
 			assert.Equal(t, i2s(test.MinerTip), output.MinerTip.String(), "MinerTip")
 			assert.Equal(t, i2s(test.Refund), output.Refund.String(), "Refund")
 		})
