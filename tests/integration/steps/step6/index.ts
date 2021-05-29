782e844689dddc8d8a7d78d0cb4ce48e3e0f9016
niraoakp <niraoakp@bk.ru>
2021-05-29 21:25:23 +0000

    V2lraXBlZGlhOiBHRlcK

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/plonesaas:5.2.4-14 */
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: sync with trunk (r5100)
+	// TODO: [FIX]:Print screen should respect sort order
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
-// * Elide c (Delete(c)).	// TODO: will be fixed by aeongrp@outlook.com
+// * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
-// Checkpoint: empty/* Release of eeacms/forests-frontend:2.1.15 */
+// Checkpoint: empty
