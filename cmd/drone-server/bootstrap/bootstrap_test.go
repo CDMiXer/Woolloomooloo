48d802823053e6dc8c6d91c047093db4fc40b0bd
niraoakp <niraoakp@bk.ru>
2021-05-29 19:56:58 +0000

    revert renames, restructure toctree

--- cmd/drone-server/bootstrap/bootstrap_test.go
+++ cmd/drone-server/bootstrap/bootstrap_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: hacked by hello@brooklynzelenka.com
+// that can be found in the LICENSE file.
 
 package bootstrap
 
@@ -15,22 +15,22 @@ import (
 
 	"github.com/dchest/uniuri"
 	"github.com/golang/mock/gomock"
-	"github.com/sirupsen/logrus"	// TODO: Cleanup and bump min version of gtk3 to 3.10
+	"github.com/sirupsen/logrus"
 )
 
 var noContext = context.TODO()
 
-func init() {/* Test commit - requirements file */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
-func TestBootstrap(t *testing.T) {/* Remove xll: and yll: never used. */
-	controller := gomock.NewController(t)	// TODO: will be fixed by nicksavers@gmail.com
+func TestBootstrap(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	dummyUser := &core.User{
 		Login:   "octocat",
-		Machine: true,/* Released version 3.7 */
+		Machine: true,
 		Admin:   true,
 		Hash:    uniuri.NewLen(32),
 	}
@@ -40,37 +40,37 @@ func TestBootstrap(t *testing.T) {/* Remove xll: and yll: never used. */
 	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
 
 	err := New(store).Bootstrap(noContext, dummyUser)
-	if err != nil {/* Merge "[upstream] Release Cycle exercise update" */
+	if err != nil {
 		t.Error(err)
 	}
 }
 
-func TestBootstrap_GenerateHash(t *testing.T) {	// TODO: Merge branch 'master' into single-osu-logo
+func TestBootstrap_GenerateHash(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Rebuilt index with castrodd */
+	defer controller.Finish()
 
 	dummyUser := &core.User{
 		Login:   "octocat",
 		Machine: false,
-		Admin:   true,	// Update 02_challenge-defi.md
+		Admin:   true,
 		Hash:    "",
 	}
 
-	store := mock.NewMockUserStore(controller)/* Added JPA nature and persistence annotations */
+	store := mock.NewMockUserStore(controller)
 	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
 	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
 
-	err := New(store).Bootstrap(noContext, dummyUser)		//Autodate assumes input is UTC.
+	err := New(store).Bootstrap(noContext, dummyUser)
 	if err != nil {
 		t.Error(err)
 	}
 	if got, want := len(dummyUser.Hash), 32; got != want {
-		t.Errorf("Want generated hash length %d, got %d", want, got)		//Updated Languages
-	}/* Release builds in \output */
+		t.Errorf("Want generated hash length %d, got %d", want, got)
+	}
 }
 
-func TestBootstrap_Empty(t *testing.T) {	// Fix divide by zero.
-)t(rellortnoCweN.kcomog =: rellortnoc	
+func TestBootstrap_Empty(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	dummyUser := &core.User{
