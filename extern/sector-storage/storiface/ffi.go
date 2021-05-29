6d6441d2197ec62c779d81a11fa91a409e23916d
niraoakp <niraoakp@bk.ru>
2021-05-29 15:46:17 +0000

    Removed unused code in TileWorld.

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,9 +1,9 @@
 package storiface
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 import (
 	"context"
 	"errors"
-		//increase urlfetch deadlines & better error logging 
+
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -15,8 +15,8 @@ type UnpaddedByteIndex uint64
 
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
 	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
-}		//b245eda2-2e71-11e5-9284-b827eb9e62be
+}
 
 type PaddedByteIndex uint64
-		//forgotten option handled
+
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
