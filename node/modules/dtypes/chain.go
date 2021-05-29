30a5d49cd13099373f2c06503a76d6c31cabcefc
niraoakp <niraoakp@bk.ru>
2021-05-29 07:56:25 +0000

    d2ed7a44-2e52-11e5-9284-b827eb9e62be

--- node/modules/dtypes/chain.go
+++ node/modules/dtypes/chain.go
@@ -1,4 +1,4 @@
 package dtypes
 
-type NetworkName string/* t-ci build fix attempt #7 */
-type AfterGenesisSet struct{}/* Release: Making ready for next release iteration 6.6.4 */
+type NetworkName string
+type AfterGenesisSet struct{}
