b4dc8b9173f3432f98ec07528d736cf5185315f3
niraoakp <niraoakp@bk.ru>
2021-05-29 06:30:02 +0000

    don't treat comments as part of ledger transaction descriptions

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
-package ints
+/* [appveyor] Remove hack to create Release directory */
+package ints/* Fixup ReleaseDC and add information. */
 
 import (
 	"path/filepath"
@@ -9,14 +9,14 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
+	// Remove deprecated -e flag from docker login
 func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
 		d := filepath.Join("nodejs", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{
+			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Update MainInterface.java */
 				Dir:                    d,
-				Dependencies:           []string{"@pulumi/pulumi"},
+				Dependencies:           []string{"@pulumi/pulumi"},/* Add unit test for APSTUD-3694 */
 				Quick:                  true,
 				ExtraRuntimeValidation: Validator("nodejs"),
 			})
