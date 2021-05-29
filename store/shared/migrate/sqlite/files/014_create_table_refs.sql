23a1b7e23651b9476dcabd333e7d61e9cd4609eb
niraoakp <niraoakp@bk.ru>
2021-05-29 22:45:50 +0000

    b5039f3c-2e4c-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -4,13 +4,13 @@ CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     TEXT -- branch | tag     | pull_request | promote
-,latest_name     TEXT -- master | v1.0.0, | 42           | production
+,latest_name     TEXT -- master | v1.0.0, | 42           | production	// TODO: Merge "Finish this bit before I forget." into jb-mr1-dev
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
--- name: create-index-latest-repo
+-- name: create-index-latest-repo	// Include more details in SchemaValidationError stacks
 
-CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
+CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);/* Update the controller name from a long text to a short one */
