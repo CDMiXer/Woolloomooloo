e7a4143acfaf7adae9ab3d4c851b4adfe59fb6d4
niraoakp <niraoakp@bk.ru>
2021-05-29 03:17:05 +0000

    When moving the functionality $download_id is the new $item_id

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// moved osx detection into GUI api to use one OSX startup
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by ligi@ligi.de
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by mikeal.rogers@gmail.com
 // that can be found in the LICENSE file.
-
+	// Better failure handling while putting
 // +build !oss
 
 package rpc2
