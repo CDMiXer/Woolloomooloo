322c0dcd6c2849f02104aad21e8b3b92be86ec2a
niraoakp <niraoakp@bk.ru>
2021-05-29 01:34:43 +0000

    Allow multiple tags to be added at once

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version 0.3. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//Delete twodudes-big.png
 
 package config
 
-import (
+import (/* cors-config allow-origin is defined now by application.property file */
 	"errors"
-	"testing"
+	"testing"/* Changing Release in Navbar Bottom to v0.6.5. */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)
+)		//focntion insert into bd dans DButils
 
 func TestMemoize(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Updated to release 0.3.1 / 0.3.2-SNAPSHOT */
 	defer controller.Finish()
 
 	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
-	args := &core.ConfigArgs{
+	args := &core.ConfigArgs{/* Merge "Fix issue #11536426: Permission denied when trying to launch..." */
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: conf,
-	}
+	}	// TODO: Remove huaway_status.sh and issue curl command directly.
 
 	base := mock.NewMockConfigService(controller)
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
@@ -33,40 +33,40 @@ func TestMemoize(t *testing.T) {
 	service := Memoize(base).(*memoize)
 	_, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)		//string contraints, imports
-		return		//Update CuteSDR to v1.16
-	}	// TODO: will be fixed by caojiaoyue@protonmail.com
-
-	if got, want := service.cache.Len(), 1; got != want {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-		t.Errorf("Expect %d items in cache, got %d", want, got)	// TODO: hacked by vyzo@hackzen.org
+		t.Error(err)	// TODO: Merge "IBP: Add python-urllib3 to fuel-agent rpm spec"
+		return
 	}
 
-	args.Config = nil // set to nil to prove we get the cached value		//import queue configuration Ok + test
+	if got, want := service.cache.Len(), 1; got != want {
+		t.Errorf("Expect %d items in cache, got %d", want, got)
+	}
+
+	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return
+		return/* Merge branch 'master' of https://github.com/11693-5/project-team5.git */
 	}
-	if res != conf {
+	if res != conf {	// 92991488-2e59-11e5-9284-b827eb9e62be
 		t.Errorf("Expect result from cache")
 	}
-
-	if got, want := service.cache.Len(), 1; got != want {		//Ajout et Corr. Séminaire de microscopie de mycologie
+		//Merge "Increase the buffer length for Interface Ksync message."
+	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}
+	}/* Beta Release Version */
 }
 
 func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Load CF7 - Google API v3 only specified pages */
+	defer controller.Finish()
 
-	args := &core.ConfigArgs{
+	args := &core.ConfigArgs{/* Release the reference to last element in takeUntil, add @since tag */
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
-		Repo:   &core.Repository{ID: 42},/* Delete eulerPaper.ind */
+		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
-	}
+	}		//Create 526. Beautiful Arrangement.cpp
 
-	base := mock.NewMockConfigService(controller)		//- Added new keywords on the copyright detector
+	base := mock.NewMockConfigService(controller)	// TODO: Refs #89516 - time recording
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
@@ -75,7 +75,7 @@ func TestMemoize_Tag(t *testing.T) {
 		t.Error(err)
 		return
 	}
-	if res != args.Config {/* Create PD_control */
+	if res != args.Config {
 		t.Errorf("Expect result from cache")
 	}
 }
@@ -84,13 +84,13 @@ func TestMemoize_Empty(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{/* Release 1.4 (AdSearch added) */
+	args := &core.ConfigArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: ""}, // empty
-	}/* README: Add BuddyBuild, Marathon & Swift version badges */
+	}
 
-	base := mock.NewMockConfigService(controller)/* chore(NG2 RC1): Update to NG2 RC.1, closes #35 (#46) */
+	base := mock.NewMockConfigService(controller)
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
@@ -100,7 +100,7 @@ func TestMemoize_Empty(t *testing.T) {
 		return
 	}
 	if res != nil {
-		t.Errorf("Expect nil response")/* sw34refactor1: remove all but one overloads of PaMCorrAbs. */
+		t.Errorf("Expect nil response")
 	}
 	if got, want := service.cache.Len(), 0; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
