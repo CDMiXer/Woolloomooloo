94ff0f2e288a149b843b3dc7751b837b2eda15c3
niraoakp <niraoakp@bk.ru>
2021-05-29 17:04:15 +0000

    when in "non auto compact" mode, should be able to get attribute values (month)

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;	// TODO: hacked by arajasek94@gmail.com
+using System.Threading.Tasks;
 using Pulumi;
 
-class Program/* Release version: 0.7.11 */
+class Program
 {
-    static Task<int> Main(string[] args)/* Release 1.1.0 of EASy-Producer */
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
         {
             return new Dictionary<string, object>
-            {/* Update README.md with Release history */
-                {  "xyz", "ABC" },/* Add "code" class to more URL input fields, props johnbillion, fixes #8383 */
-                {  "foo", 42 },	// TODO: will be fixed by zaq1tomo@gmail.com
+            {
+                {  "xyz", "ABC" },
+                {  "foo", 42 },
             };
         });
     }
