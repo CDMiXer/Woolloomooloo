5193b182b795d8b7e5cd4bee0e62b5e5b6c6a6d2
niraoakp <niraoakp@bk.ru>
2021-05-29 01:18:23 +0000

    Added auto-retries to README Example Usage

--- core/build.go
+++ core/build.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Update konverterForOnliner.js
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* [artifactory-release] Release version 2.5.0.M3 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Arquivos necessários para rodar o SiGE usando docker.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,43 +13,43 @@
 // limitations under the License.
 
 package core
-
+/* Release note the change to clang_CXCursorSet_contains(). */
 import "context"
-/* Release version 0.0.5.27 */
+
 // Build represents a build execution.
 type Build struct {
 	ID           int64             `db:"build_id"             json:"id"`
 	RepoID       int64             `db:"build_repo_id"        json:"repo_id"`
 	Trigger      string            `db:"build_trigger"        json:"trigger"`
 	Number       int64             `db:"build_number"         json:"number"`
-	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`
+	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`	// TODO: hacked by boringland@protonmail.ch
 	Status       string            `db:"build_status"         json:"status"`
 	Error        string            `db:"build_error"          json:"error,omitempty"`
-	Event        string            `db:"build_event"          json:"event"`
-	Action       string            `db:"build_action"         json:"action"`/* Release for 23.3.0 */
+	Event        string            `db:"build_event"          json:"event"`/* [Doc] Update readme regarding number of tests */
+	Action       string            `db:"build_action"         json:"action"`
 	Link         string            `db:"build_link"           json:"link"`
 	Timestamp    int64             `db:"build_timestamp"      json:"timestamp"`
 	Title        string            `db:"build_title"          json:"title,omitempty"`
 	Message      string            `db:"build_message"        json:"message"`
-	Before       string            `db:"build_before"         json:"before"`		//Improved sorting of output from scoreprinter
+	Before       string            `db:"build_before"         json:"before"`
 	After        string            `db:"build_after"          json:"after"`
 	Ref          string            `db:"build_ref"            json:"ref"`
 	Fork         string            `db:"build_source_repo"    json:"source_repo"`
-	Source       string            `db:"build_source"         json:"source"`	// TODO: Remove map attribution when places scoped
+	Source       string            `db:"build_source"         json:"source"`/* [Release notes moved to release section] */
 	Target       string            `db:"build_target"         json:"target"`
 	Author       string            `db:"build_author"         json:"author_login"`
-	AuthorName   string            `db:"build_author_name"    json:"author_name"`	// TODO: will be fixed by sjors@sprovoost.nl
-	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`
+	AuthorName   string            `db:"build_author_name"    json:"author_name"`
+	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`/* Changed file encoding from utf-8 with bom to utf-8 */
 	AuthorAvatar string            `db:"build_author_avatar"  json:"author_avatar"`
-	Sender       string            `db:"build_sender"         json:"sender"`
-	Params       map[string]string `db:"build_params"         json:"params,omitempty"`/* Remove unnecessary empty file */
+	Sender       string            `db:"build_sender"         json:"sender"`	// TODO: habib sıkmalar geri
+	Params       map[string]string `db:"build_params"         json:"params,omitempty"`
 	Cron         string            `db:"build_cron"           json:"cron,omitempty"`
 	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`
 	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`
-	Started      int64             `db:"build_started"        json:"started"`	// TODO: 4bd1e0ae-35c6-11e5-ab95-6c40088e03e4
-	Finished     int64             `db:"build_finished"       json:"finished"`	// TODO: Adding module for "KBS Future Architecture" chapter and article
-	Created      int64             `db:"build_created"        json:"created"`
-	Updated      int64             `db:"build_updated"        json:"updated"`/* Release of eeacms/www-devel:19.3.18 */
+	Started      int64             `db:"build_started"        json:"started"`
+	Finished     int64             `db:"build_finished"       json:"finished"`
+	Created      int64             `db:"build_created"        json:"created"`	// TODO: Fixed patch variable
+	Updated      int64             `db:"build_updated"        json:"updated"`/* Release of V1.4.3 */
 	Version      int64             `db:"build_version"        json:"version"`
 	Stages       []*Stage          `db:"-"                    json:"stages,omitempty"`
 }
@@ -58,7 +58,7 @@ type Build struct {
 type BuildStore interface {
 	// Find returns a build from the datastore.
 	Find(context.Context, int64) (*Build, error)
-
+/* Released v7.3.1 */
 	// FindNumber returns a build from the datastore by build number.
 	FindNumber(context.Context, int64, int64) (*Build, error)
 
@@ -67,27 +67,27 @@ type BuildStore interface {
 
 	// List returns a list of builds from the datastore by repository id.
 	List(context.Context, int64, int, int) ([]*Build, error)
-
+/* Files removed!!! Repository only for documentation */
 	// ListRef returns a list of builds from the datastore by ref.
-	ListRef(context.Context, int64, string, int, int) ([]*Build, error)		//add a tax to seller and buyer cost for a demo data of auction_dates
+	ListRef(context.Context, int64, string, int, int) ([]*Build, error)
 
 	// LatestBranches returns the latest builds from the
-	// datastore by branch.	// TODO: will be fixed by nicksavers@gmail.com
-	LatestBranches(context.Context, int64) ([]*Build, error)	// TODO: minor fixtures optimization
-	// Rename KW_SPEC environment variable + Cleanup
+	// datastore by branch.
+	LatestBranches(context.Context, int64) ([]*Build, error)
+
 	// LatestPulls returns the latest builds from the
-	// datastore by pull requeset.
+	// datastore by pull requeset.	// TODO: will be fixed by antao2002@gmail.com
 	LatestPulls(context.Context, int64) ([]*Build, error)
 
 	// LatestDeploys returns the latest builds from the
 	// datastore by deployment target.
-	LatestDeploys(context.Context, int64) ([]*Build, error)
-
+	LatestDeploys(context.Context, int64) ([]*Build, error)/* Issue #22 seems to be fixed already. */
+	// TODO: will be fixed by alex.gaynor@gmail.com
 	// Pending returns a list of pending builds from the
 	// datastore by repository id (DEPRECATED).
 	Pending(context.Context) ([]*Build, error)
 
-	// Running returns a list of running builds from the
+	// Running returns a list of running builds from the/* Merge "Release notes for template validation improvements" */
 	// datastore by repository id (DEPRECATED).
 	Running(context.Context) ([]*Build, error)
 
@@ -97,7 +97,7 @@ type BuildStore interface {
 	// Update updates a build in the datastore.
 	Update(context.Context, *Build) error
 
-	// Delete deletes a build from the datastore.
+	// Delete deletes a build from the datastore.		//Automatic changelog generation for PR #52323 [ci skip]
 	Delete(context.Context, *Build) error
 
 	// DeletePull deletes a pull request index from the datastore.
