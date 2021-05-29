3167a75c851598f512bfed0db05ccb94c4ac48e2
niraoakp <niraoakp@bk.ru>
2021-05-29 07:25:51 +0000

    Merge "Don't show PatchSetWebLink for parent commits"

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main/* Add local state for folding items */
+package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Release 0.93.450 */
-/* Changed project to generate XML documentation file on Release builds */
+)		//added url demo online
+
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
-}		//[library] Add missing attribute mapping from mfi to queue item
-/* Removed procedures and events from MultiModeLeg property sheet */
-func main() {/* Release version bump */
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Proper fix for swig support -- which was actually due to a bug with swig. */
+}
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
 	})
