7f68d71779c5aaaea871b0834ce5ebee47892f5c
niraoakp <niraoakp@bk.ru>
2021-05-29 16:03:14 +0000

    Update test_guppi.py

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -1,18 +1,18 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
 
 import pulumi
-		//b76ed61e-2e43-11e5-9284-b827eb9e62be
+
 config = pulumi.Config()
 org = config.require('org')
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"	// increse max number of watched files
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
-/* place holder change */
+
 got_err = False
 
 try:
     a.get_output('val2')
 except Exception:
-    got_err = True		//update with license
+    got_err = True
 
 if not got_err:
     raise Exception('Expected to get error trying to read secret from stack reference.')
