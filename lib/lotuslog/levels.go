4736a7d92adc23826c073c9f80b387aa6ee1fca8
niraoakp <niraoakp@bk.ru>
2021-05-29 17:18:45 +0000

    Added Backend functionalities

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
-/* Release 1.3.0.1 */
+
 import (
-	"os"
-		//Making unit tests more readable
+	"os"	// TODO: Sort the files before iterating over them.
+/* fixed topic click action */
 	logging "github.com/ipfs/go-log/v2"
 )
 
-func SetupLogLevels() {	// TODO: will be fixed by steven@stebalien.com
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")/* moved constant back to GrailsScriptRunner */
+func SetupLogLevels() {
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {/* Hotfix Release 3.1.3. See CHANGELOG.md for details (#58) */
+		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")		//Disambiguate the namespace.
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")	// Make pkg-config dep a little less strict
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
