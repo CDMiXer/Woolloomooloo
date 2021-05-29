3c1c7259fac2da7495eabed2fbdcf9fd741637e5
niraoakp <niraoakp@bk.ru>
2021-05-29 03:53:33 +0000

    Convert sources to new config system.

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Automatic changelog generation for PR #49236 [ci skip] */
 
 package encrypt
-
+/* - putting commonly used visualizers into annis-utilsgui */
 import "testing"
-/* update to 0.2-SNAPSHOT */
+
 func TestNone(t *testing.T) {
-	n, _ := New("")/* Merge "Release 3.2.3.460 Prima WLAN Driver" */
-	ciphertext, err := n.Encrypt("correct-horse-batter-staple")/* give pointer to first and last step */
-	if err != nil {
-		t.Error(err)/* 20.1-Release: removing syntax error from cappedFetchResult */
-	}
-	plaintext, err := n.Decrypt(ciphertext)/* Release 0.3.3 (#46) */
+	n, _ := New("")
+	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
 	if err != nil {
 		t.Error(err)
 	}
-	if want, got := plaintext, "correct-horse-batter-staple"; got != want {	// Merge branch 'dev' into srk/pushnotifications
+	plaintext, err := n.Decrypt(ciphertext)	// fix upload error
+	if err != nil {
+		t.Error(err)
+	}
+	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
 }
