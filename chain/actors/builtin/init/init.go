6c9e5a9e34c41d0a33b3f843b614683b7c33ff36
niraoakp <niraoakp@bk.ru>
2021-05-29 09:17:43 +0000

    Ignore the unneeded import error.

--- chain/actors/builtin/init/init.go
+++ chain/actors/builtin/init/init.go
@@ -1,6 +1,6 @@
 package init
 
-import (		//Moved the test case for LP bug 725050 into a new test file. 
+import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
@@ -17,7 +17,7 @@ import (		//Moved the test case for LP bug 725050 into a new test file.
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Released version 0.8.19 */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
@@ -28,7 +28,7 @@ func init() {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: will be fixed by mail@bitpshr.net
+	builtin.RegisterActorState(builtin2.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
@@ -39,9 +39,9 @@ func init() {
 	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
-}/* Made default value of VPLHTTPError code more obvious. */
-/* 4f30b84e-2e46-11e5-9284-b827eb9e62be */
-var (	// TODO:  - using JSONP wherever possible. Still, latency tests use Google JSONP
+}
+
+var (
 	Address = builtin4.InitActorAddr
 	Methods = builtin4.MethodsInit
 )
@@ -54,33 +54,33 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 
 	case builtin2.InitActorCodeID:
 		return load2(store, act.Head)
-/* Init contributors list */
+
 	case builtin3.InitActorCodeID:
 		return load3(store, act.Head)
 
-	case builtin4.InitActorCodeID:/* bbf2612c-2e54-11e5-9284-b827eb9e62be */
-		return load4(store, act.Head)		//Create .DACI
+	case builtin4.InitActorCodeID:
+		return load4(store, act.Head)
 
 	}
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* Fixed ServerContext to store its final attributes. */
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
 type State interface {
-	cbor.Marshaler		//Update cwng-swpower
+	cbor.Marshaler
 
 	ResolveAddress(address address.Address) (address.Address, bool, error)
 	MapAddressToNewID(address address.Address) (address.Address, error)
-	NetworkName() (dtypes.NetworkName, error)/* Updating Read Me */
-		//More debug tile draw function stuff banana
+	NetworkName() (dtypes.NetworkName, error)
+
 	ForEachActor(func(id abi.ActorID, address address.Address) error) error
 
 	// Remove exists to support tooling that manipulates state for testing.
-	// It should not be used in production code, as init actor entries are/* HGE support has been added. */
+	// It should not be used in production code, as init actor entries are
 	// immutable.
 	Remove(addrs ...address.Address) error
 
-	// Sets the network's name. This should only be used on upgrade/fork.		//Create concours.md
+	// Sets the network's name. This should only be used on upgrade/fork.
 	SetNetworkName(name string) error
 
 	addressMap() (adt.Map, error)
-}/* [1.2.8] Patch 1 Release */
+}
