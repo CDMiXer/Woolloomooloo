99ec2b91fb6f36a16c3afe0ae48cbef3b75218b5
niraoakp <niraoakp@bk.ru>
2021-05-29 22:31:13 +0000

    added post nav part

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//http_code added to exceptions - getCode() suitable
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Latest Infection Unofficial Release */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: hacked by souzau@yandex.com
 
 package repos
 
@@ -24,11 +24,11 @@ import (
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		repo, _ := request.RepoFrom(ctx)
+	return func(w http.ResponseWriter, r *http.Request) {/* Release cascade method. */
+		ctx := r.Context()/* Create Chorus Pro */
+		repo, _ := request.RepoFrom(ctx)/* Release areca-7.3.5 */
 		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
-		render.JSON(w, repo, 200)
+		render.JSON(w, repo, 200)/* Fixed "Heat Vision" compositor to work in d3d11 rs. */
 	}
 }
