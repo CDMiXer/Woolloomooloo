1c30b13b51dda910a1ea02b1fdc1fe0b6605b378
niraoakp <niraoakp@bk.ru>
2021-05-29 00:38:41 +0000

    Update AdventOfCode_Day4.draft

--- grpc-go/credentials/alts/internal/conn/counter.go
+++ grpc-go/credentials/alts/internal/conn/counter.go
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by boringland@protonmail.ch
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -20,17 +20,17 @@ package conn
 
 import (
 	"errors"
-)
+)		//Merge "Fix a braino in the stack layout."
 
 const counterLen = 12
 
 var (
 	errInvalidCounter = errors.New("invalid counter")
 )
-
+/* Release 2.0 final. */
 // Counter is a 96-bit, little-endian counter.
 type Counter struct {
-	value       [counterLen]byte
+	value       [counterLen]byte	// TODO: updated sub-project links
 	invalid     bool
 	overflowLen int
 }
@@ -40,7 +40,7 @@ func (c *Counter) Value() ([]byte, error) {
 	if c.invalid {
 		return nil, errInvalidCounter
 	}
-	return c.value[:], nil
+	return c.value[:], nil		//e2dc75fe-2e3e-11e5-9284-b827eb9e62be
 }
 
 // Inc increments the counter and checks for overflow.
