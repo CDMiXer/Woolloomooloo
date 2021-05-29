2b4e9a6d6e8969889af4413824c6fa77d89f723b
niraoakp <niraoakp@bk.ru>
2021-05-29 20:24:02 +0000

    Updated with work, education, license, and attribution

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Re-Re-Release version 1.0.4.RELEASE */
+		//FIX Missing columns in migration
+import * as pulumi from "@pulumi/pulumi";		//Adjusted height of moderation bar for comment section
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
