b88690d7ab1ccbb535435c523bab7357de5fc64f
niraoakp <niraoakp@bk.ru>
2021-05-29 08:10:48 +0000

    fixed bug in propertytable: not refreshed after changing property

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Merge "Add "--version" parameters to cmd"
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* View for nodes too */
-//
-// Unless required by applicable law or agreed to in writing, software/* qt-pro: qt: Remove unsused flags from .*prf */
+//     http://www.apache.org/licenses/LICENSE-2.0
+///* Release of eeacms/eprtr-frontend:0.2-beta.27 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* 3935e546-2e50-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Presentation about virtual memory was added. */
-import { Resource } from "./resource";/* Rename Videos to Video Plug-ins, etc. */
+
+import { Resource } from "./resource";	// Time to get blogging!
 
 // The changes in this plan trigger replacement of both A and B.
 // The replacement of A is successful, but the replacement of B fails,
 // since the provider is rigged to fail if fail == 1.
 //
-// This leaves the previous instance of A in the snapshot, since the plan/* [artifactory-release] Release version 3.1.0.BUILD */
+// This leaves the previous instance of A in the snapshot, since the plan	// TODO: trigger new build for ruby-head-clang (1b94420)
 // failed before we got a chance to service the pending deletion.
 const a = new Resource("a", { fail: 2 });
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });/* Merge local/master */
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
 detaerC :A  //
 //  A: Pending Delete
-//  B: Created		//remove eol whitespace
+//  B: Created/* Updated the portaudio feedstock. */
 
