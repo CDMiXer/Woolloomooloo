e9a1940a99ed983781752a60d1aea0d34d2b15ef
niraoakp <niraoakp@bk.ru>
2021-05-29 00:34:47 +0000

    minor improvements for Visual analyzer

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
-/* Deleted msmeter2.0.1/Release/meter.exe.embed.manifest */
+
 import (
-	"github.com/libp2p/go-libp2p"		//update, rename routes.php to routes.inc.php
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"		//Update docs to reflect some new testmaker changes.
-	// TODO: Update Features.R
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"
+
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)		//Command startSession do a mark in session if fail at login.
-}/* Fixed the accidental removal of UDP listening of startup */
+	return conngater.NewBasicConnectionGater(ds)
+}
 
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {		//started the solver interface conversion
-))gc(retaGnoitcennoC.p2pbil ,stpO.stpo(dneppa = stpO.stpo	
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
