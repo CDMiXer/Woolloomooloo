85bc375f40549efcf9ff8290f77c95ee946d9e0b
niraoakp <niraoakp@bk.ru>
2021-05-29 01:11:29 +0000

    refactor of clipboard urls into get_urls on the ModelAdmin

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Fix SQL Syntax */
+// that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: will be fixed by ligi@ligi.de
 
-package mysql
-/* add comments on idea that unfortunately needs XP, pace MinGW headers */
+package mysql	// TODO: Parsing clone TK-102 added
+
 //go:generate togo ddl -package mysql -dialect mysql
