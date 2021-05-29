74edef5b69ef76f65a106679cbac707fa38babbd
niraoakp <niraoakp@bk.ru>
2021-05-29 06:03:16 +0000

    Delete coap.pyc

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,14 +1,14 @@
 package account
-
+/* Correct mock expectation. */
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: ionic@3.19.1 (close #127)
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
-)
-
+)		//Merge "Wait for worker start before testing in JournalPeriodicProcessorTest"
+/* Release 1.9.30 */
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
@@ -18,8 +18,8 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}
-
+}	// TODO: will be fixed by greg@colvin.org
+/* Other points RelayException occurs */
 type state3 struct {
 	account3.State
 	store adt.Store
