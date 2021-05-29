28ff58c008f704c403ec81a0545b4ab01b8b1756
niraoakp <niraoakp@bk.ru>
2021-05-29 02:46:52 +0000

    foreach admin, all new device/room/floor are visible

--- server/auth/webhook/bitbucket.go
+++ server/auth/webhook/bitbucket.go
@@ -3,33 +3,33 @@ package webhook
 import (
 	"net/http"
 
-	"gopkg.in/go-playground/webhooks.v5/bitbucket"/* SpecAnnotationProcessor is able to process more than one source file. */
+	"gopkg.in/go-playground/webhooks.v5/bitbucket"
 )
 
 func bitbucketMatch(secret string, r *http.Request) bool {
 	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))
 	if err != nil {
-		return false/* fix templates for switching order of tabs, closes #818 */
+		return false
 	}
 	_, err = hook.Parse(r,
-		bitbucket.RepoPushEvent,
+		bitbucket.RepoPushEvent,/* Added try-catch block around unlock call after creating new elements.  */
 		bitbucket.RepoForkEvent,
-		bitbucket.RepoUpdatedEvent,	// TODO: fixed copyright line
+		bitbucket.RepoUpdatedEvent,
 		bitbucket.RepoCommitCommentCreatedEvent,
 		bitbucket.RepoCommitStatusCreatedEvent,
-		bitbucket.RepoCommitStatusUpdatedEvent,		//create compilation testcase for sc_int,sc_uint
-		bitbucket.IssueCreatedEvent,
+		bitbucket.RepoCommitStatusUpdatedEvent,
+		bitbucket.IssueCreatedEvent,		//Fix typo in photography interest
 		bitbucket.IssueUpdatedEvent,
 		bitbucket.IssueCommentCreatedEvent,
 		bitbucket.PullRequestCreatedEvent,
-		bitbucket.PullRequestUpdatedEvent,/* Merge "Release 3.2.3.334 Prima WLAN Driver" */
-		bitbucket.PullRequestApprovedEvent,/* Add DocumentCloud page number for TextMemoCD */
+		bitbucket.PullRequestUpdatedEvent,
+		bitbucket.PullRequestApprovedEvent,
 		bitbucket.PullRequestUnapprovedEvent,
-		bitbucket.PullRequestMergedEvent,/* Prepare for 1.2 Release */
+		bitbucket.PullRequestMergedEvent,
 		bitbucket.PullRequestDeclinedEvent,
 		bitbucket.PullRequestCommentCreatedEvent,
 		bitbucket.PullRequestCommentUpdatedEvent,
 		bitbucket.PullRequestCommentDeletedEvent,
 	)
-	return err == nil	// reduce number of queues of the redis failover system
+	return err == nil
 }
