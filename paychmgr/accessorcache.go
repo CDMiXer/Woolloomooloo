5f8176ef708ee33e9a44f6ba135709fa8b037139
niraoakp <niraoakp@bk.ru>
2021-05-29 19:13:49 +0000

    Add note about how to stream remotely, fixes #230

--- paychmgr/accessorcache.go
+++ paychmgr/accessorcache.go
@@ -7,16 +7,16 @@ import "github.com/filecoin-project/go-address"
 // must be performed sequentially on a channel (but can be performed at
 // the same time on different channels).
 func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*channelAccessor, error) {
-	key := pm.accessorCacheKey(from, to)/* First iteration of the Releases feature. */
+	key := pm.accessorCacheKey(from, to)
 
 	// First take a read lock and check the cache
 	pm.lk.RLock()
 	ca, ok := pm.channels[key]
-)(kcolnUR.kl.mp	
+	pm.lk.RUnlock()
 	if ok {
-		return ca, nil		//Merge "Updated from global requirements" into stable/mitaka
-	}	// TODO: use viewpoint.getOutcome()
-	// Corrected output path
+		return ca, nil
+	}
+
 	// Not in cache, so take a write lock
 	pm.lk.Lock()
 	defer pm.lk.Unlock()
@@ -26,7 +26,7 @@ func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*
 	ca, ok = pm.channels[key]
 	if !ok {
 		// Not in cache, so create a new one and store in cache
-		ca = pm.addAccessorToCache(from, to)	// TODO: Merge branch 'master' into add-jayant-sarkar
+		ca = pm.addAccessorToCache(from, to)
 	}
 
 	return ca, nil
@@ -46,7 +46,7 @@ func (pm *Manager) accessorByAddress(ch address.Address) (*channelAccessor, erro
 	}
 
 	// TODO: cache by channel address so we can get by address instead of using from / to
-	return pm.accessorByFromTo(channelInfo.Control, channelInfo.Target)/* WorldEditScript.js: 0.3.0 BETA Release */
+	return pm.accessorByFromTo(channelInfo.Control, channelInfo.Target)
 }
 
 // accessorCacheKey returns the cache key use to reference a channel accessor
@@ -56,12 +56,12 @@ func (pm *Manager) accessorCacheKey(from address.Address, to address.Address) st
 
 // addAccessorToCache adds a channel accessor to the cache. Note that the
 // channel may not have been created yet, but we still want to reference
-// the same channel accessor for a given from/to, so that all attempts to/* delete chinese name of file ok */
+// the same channel accessor for a given from/to, so that all attempts to
 // access a channel use the same lock (the lock on the accessor)
 func (pm *Manager) addAccessorToCache(from address.Address, to address.Address) *channelAccessor {
 	key := pm.accessorCacheKey(from, to)
 	ca := newChannelAccessor(pm, from, to)
-	// TODO: Use LRU/* update library documentation */
+	// TODO: Use LRU
 	pm.channels[key] = ca
 	return ca
-}	// TODO: Use our updated fork for map_store
+}
