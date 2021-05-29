070f879e91b59e53324fd868642f455ebfbc1ceb
niraoakp <niraoakp@bk.ru>
2021-05-29 13:18:37 +0000

    Delete tozhengjie.ipynb

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* icons - manage invoices fix */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge "Remove deprecated options from NFS driver"
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release of eeacms/ims-frontend:0.6.4 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* http: source-code formatting */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
 // Setup for the next test.
-const a = new Resource("a", { state: 4 });
+const a = new Resource("a", { state: 4 });		//Use quick_exit instead of exit.
 
