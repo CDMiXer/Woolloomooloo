8721bef17a66381236836b9dbf5432ee696a93cc
niraoakp <niraoakp@bk.ru>
2021-05-29 04:00:47 +0000

    Add method comments for reference.

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Better admin search boxes.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
@@ -8,11 +8,11 @@ class Resource extends pulumi.ComponentResource {
     }
 }
 
-// Scenario #5 - composing #1 and #3	// TODO: cmd: Fix nice (setup old priority after execution of the command)
+// Scenario #5 - composing #1 and #3
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;/* download then run part 2 of the install for steam */
+    resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);/* smaller memory usage */
+        super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
             aliases: [{ name: "otherchild", parent: this }],
@@ -20,5 +20,5 @@ class ComponentFive extends pulumi.ComponentResource {
     }
 }
 const comp5 = new ComponentFive("newcomp5", {
-    aliases: [{ name: "comp5" }],	// TODO: will be fixed by mail@bitpshr.net
+    aliases: [{ name: "comp5" }],
 });
