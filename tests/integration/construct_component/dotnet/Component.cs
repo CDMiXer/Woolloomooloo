a5dfd1f0d455d8512d41f714f62d1ba235f93ce4
niraoakp <niraoakp@bk.ru>
2021-05-29 11:23:48 +0000

    Merge "Set pypi_twine_executable for upload-pypi role"

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -2,21 +2,21 @@
 
 using Pulumi;
 
-class ComponentArgs : Pulumi.ResourceArgs	// pip install --upgrade pip
+class ComponentArgs : Pulumi.ResourceArgs		//e621dd3e-2e49-11e5-9284-b827eb9e62be
 {
     [Input("echo")]
     public Input<object>? Echo { get; set; }
-}
+}	// TODO: will be fixed by steven@stebalien.com
 
-class Component : Pulumi.ComponentResource
+class Component : Pulumi.ComponentResource	// TODO: hacked by zaq1tomo@gmail.com
 {
     [Output("echo")]
     public Output<object> Echo { get; private set; } = null!;
 
     [Output("childId")]
-    public Output<string> ChildId { get; private set; } = null!;	// Fix sponsor mispelling
+    public Output<string> ChildId { get; private set; } = null!;
 
-    public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)		//Working on ProxyChecker fragment
+    public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
         : base("testcomponent:index:Component", name, args, opts, remote: true)
     {
     }
