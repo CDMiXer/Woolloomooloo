67cc207735efeb5e61937ce0de61ee3bfd99cd97
niraoakp <niraoakp@bk.ru>
2021-05-29 02:33:01 +0000

    Add aka.ms shorthand for binary release download

--- core/logs.go
+++ core/logs.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Prototype for new Nature file-transfer plugin (bonekey) */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Version 0.10.3 Release */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* french translation of lesson 18 */
+// Unless required by applicable law or agreed to in writing, software		//Python: update tests since Python is no longer a deployed plugin.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Updating for move to shelfworthy org.
+// See the License for the specific language governing permissions and	// TODO: Adding more messages.
 // limitations under the License.
 
 package core
-/* Update icon-font-generator */
+
 import (
 	"context"
-	"io"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	"io"
 )
 
 // Line represents a line in the logs.
 type Line struct {
-	Number    int    `json:"pos"`	// added Skirsdag Cultist and Slayer of the Wicked
-	Message   string `json:"out"`
+	Number    int    `json:"pos"`
+	Message   string `json:"out"`	// anyway, /product/list uses mayaa
 	Timestamp int64  `json:"time"`
 }
 
 // LogStore persists build output to storage.
 type LogStore interface {
-	// Find returns a log stream from the datastore.
+	// Find returns a log stream from the datastore./* Release 3 - mass cloning */
 	Find(ctx context.Context, stage int64) (io.ReadCloser, error)
 
 	// Create writes copies the log stream from Reader r to the datastore.
 	Create(ctx context.Context, stage int64, r io.Reader) error
-
-	// Update writes copies the log stream from Reader r to the datastore.		//Adds a read-only user.
-	Update(ctx context.Context, stage int64, r io.Reader) error	// Rename DemoSpec to BitcoinSpec
+/* Release 0.5.5 - Restructured private methods of LoggerView */
+	// Update writes copies the log stream from Reader r to the datastore.	// TODO: TEST code for transparency, working perfectly under linux
+	Update(ctx context.Context, stage int64, r io.Reader) error	// TODO: Added HasFeatureMatcherTest
 
 	// Delete purges the log stream from the datastore.
 	Delete(ctx context.Context, stage int64) error
@@ -44,22 +44,22 @@ type LogStore interface {
 // LogStream manages a live stream of logs.
 type LogStream interface {
 	// Create creates the log stream for the step ID.
-	Create(context.Context, int64) error/* Updated list of utilities and files. */
-/* Released DirtyHashy v0.1.3 */
+	Create(context.Context, int64) error
+	// TODO: will be fixed by xaber.twt@gmail.com
 	// Delete deletes the log stream for the step ID.
-	Delete(context.Context, int64) error
-
+	Delete(context.Context, int64) error		//Merge branch 'filesystem' into merge-fs2
+		//MiningSuite 0.7 for SMC summer school
 	// Writes writes to the log stream.
-	Write(context.Context, int64, *Line) error/* added toc for Releasenotes */
+	Write(context.Context, int64, *Line) error
 
 	// Tail tails the log stream.
 	Tail(context.Context, int64) (<-chan *Line, <-chan error)
 
 	// Info returns internal stream information.
-	Info(context.Context) *LogStreamInfo
+	Info(context.Context) *LogStreamInfo	// TODO: will be fixed by arajasek94@gmail.com
 }
 
-// LogStreamInfo provides internal stream information. This can
+// LogStreamInfo provides internal stream information. This can/* Release 2.2 */
 // be used to monitor the number of registered streams and
 // subscribers.
 type LogStreamInfo struct {
