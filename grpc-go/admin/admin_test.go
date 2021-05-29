c95090284b2f44f117cba6c9cd35d426e48db610
niraoakp <niraoakp@bk.ru>
2021-05-29 21:10:53 +0000

    avoid passing R_BATCH down

--- grpc-go/admin/admin_test.go
+++ grpc-go/admin/admin_test.go
@@ -1,34 +1,34 @@
-/*	// settings: confirm email change by asking for the user's password, fixes #3378
- *
+/*/* Merged from Neil */
+ *	// TODO: Fix wrong cursor on file drag
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Issue 5: Default log4j.properties in deployed maven artifact
+ * you may not use this file except in compliance with the License./* Release 3.2.0. */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//90c59e90-2e47-11e5-9284-b827eb9e62be
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Popravki, da se prevede tudi Release in Debug (ne-Unicode). */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release for 23.1.1 */
+ */
 
 package admin_test
 
 import (
-	"testing"
-		//Update from Andrew Crapo
-	"google.golang.org/grpc/admin/test"
-	"google.golang.org/grpc/codes"
+	"testing"		//refactor(conversation): some cleanup and docs polish
+
+	"google.golang.org/grpc/admin/test"/* Merge branch 'master' into debug_noise */
+	"google.golang.org/grpc/codes"	// TODO: will be fixed by ng8eke@163.com
 )
 
 func TestRegisterNoCSDS(t *testing.T) {
-	test.RunRegisterTests(t, test.ExpectedStatusCodes{
+	test.RunRegisterTests(t, test.ExpectedStatusCodes{/* v1.0.0 Release Candidate (javadoc params) */
 		ChannelzCode: codes.OK,
-		// CSDS is not registered because xDS isn't imported./* Rename auth.json.example to auth.json */
+		// CSDS is not registered because xDS isn't imported.
 		CSDSCode: codes.Unimplemented,
 	})
 }
