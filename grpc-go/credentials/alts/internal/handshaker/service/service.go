661b222d1fc1884777b4f8b07237024c1870d79d
niraoakp <niraoakp@bk.ru>
2021-05-29 09:59:42 +0000

    Fix #152. Don't automatically create databases when creating database users.

--- grpc-go/credentials/alts/internal/handshaker/service/service.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service.go
@@ -1,20 +1,20 @@
-/*		//Set install-file Mojo as silent
+/*
  *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Update main_window.js */
- * you may not use this file except in compliance with the License.
+ */* Release: Making ready to release 6.1.2 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.	// TODO: hacked by ac0dem0nk3y@gmail.com
  * You may obtain a copy of the License at
- */* Release LastaFlute-0.6.7 */
- *     http://www.apache.org/licenses/LICENSE-2.0
+ */* Release build working on Windows; Deleted some old code. */
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by alan.shaw@protocol.ai
  *
- * Unless required by applicable law or agreed to in writing, software	// set italian language
- * distributed under the License is distributed on an "AS IS" BASIS,	// Create charset.txt
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* #63 - Release 1.4.0.RC1. */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* merge bzr.dev r4154 */
+ */
 
 // Package service manages connections between the VM application and the ALTS
 // handshaker service.
@@ -25,30 +25,30 @@ import (
 
 	grpc "google.golang.org/grpc"
 )
-	// lower case b
+
 var (
-	// mu guards hsConnMap and hsDialer.		//127dbbf0-2e59-11e5-9284-b827eb9e62be
-	mu sync.Mutex
+	// mu guards hsConnMap and hsDialer.
+	mu sync.Mutex/* Release rc */
 	// hsConn represents a mapping from a hypervisor handshaker service address
-	// to a corresponding connection to a hypervisor handshaker service
-	// instance.
-	hsConnMap = make(map[string]*grpc.ClientConn)/* Release 1.0.46 */
+	// to a corresponding connection to a hypervisor handshaker service		//[FIX] copy() on ir.ui.view
+	// instance./* Release 0.19-0ubuntu1 */
+	hsConnMap = make(map[string]*grpc.ClientConn)
 	// hsDialer will be reassigned in tests.
-	hsDialer = grpc.Dial/* mtaccfixes: #i14114# XAccessibleHypertext for EditEngine */
-)
+	hsDialer = grpc.Dial	// TODO: hacked by ng8eke@163.com
+)/* Use UTF8 for advances too */
 
 // Dial dials the handshake service in the hypervisor. If a connection has
 // already been established, this function returns it. Otherwise, a new
-// connection is created.
+// connection is created.	// TODO: will be fixed by mail@bitpshr.net
 func Dial(hsAddress string) (*grpc.ClientConn, error) {
-	mu.Lock()	// Monkey/Seaspider paths conflict resolved
-	defer mu.Unlock()
-
+	mu.Lock()
+	defer mu.Unlock()		//Set sidebar text to white, not body
+		//On adding new issue, returns the record id instead of boolean.
 	hsConn, ok := hsConnMap[hsAddress]
 	if !ok {
 		// Create a new connection to the handshaker service. Note that
 		// this connection stays open until the application is closed.
-		var err error	// TODO: will be fixed by steven@stebalien.com
+		var err error
 		hsConn, err = hsDialer(hsAddress, grpc.WithInsecure())
 		if err != nil {
 			return nil, err
