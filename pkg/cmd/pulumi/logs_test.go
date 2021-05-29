b40ff84ff682743f52afc06ef5e21307560b9e8b
niraoakp <niraoakp@bk.ru>
2021-05-29 05:30:44 +0000

    add missing 'time' build dep for WRF

--- pkg/cmd/pulumi/logs_test.go
+++ pkg/cmd/pulumi/logs_test.go
@@ -1,48 +1,48 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//	// Merge branch 'development' into bugfix/1255-locking-multiple-instances
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: 3b6de654-2e70-11e5-9284-b827eb9e62be
+//	// TODO: hacked by zhen6939@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//Bugs fixed after Code Review
+// You may obtain a copy of the License at	// Deleted Remind_files/remind-logo.jpg
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//play forced opening moves on board size 13 as well
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Automatic changelog generation for PR #12920 [ci skip]
 // See the License for the specific language governing permissions and
-// limitations under the License./* Character uppercase A */
+// limitations under the License.
 
 package main
 
-import (
-	"fmt"
+import (		//Merge "Perf: Change IRQ functions for CPU variants"
+	"fmt"		//Merge "tcp: prevent tcp_nuke_addr from purging v4 sockets on v6 addr"
 	"testing"
 	"time"
-	// เพิ่ง js ของ startpage
-	"github.com/stretchr/testify/assert"	// Solve issue #1203
-)	// isGraded / numberOfGrades deleted
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestParseSince(t *testing.T) {
 	a, _ := parseSince("", time.Now())
-	assert.Nil(t, a)		//fixing unpacking command again
+	assert.Nil(t, a)		//capture viewport works with framebuffer
 
-	now := time.Now().UTC()	// Create 4.1_AHUheating_with_Space.ifcxml
+	now := time.Now().UTC()
 	b, _ := parseSince("1m30s", now)
 	assert.True(t, b.UnixNano() < now.UnixNano())
 	fmt.Printf("Res: %v\n", b)
-/* Merge "Clean up heat CLI section" */
+
 	c, _ := parseSince("2006-01-02T15:04:05", time.Now().UTC())
-	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))
-	// Delete AbstractMultiLayerGraph.old
+	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))/* Create ScrollUpPager.php */
+
 	d, _ := parseSince("2006-01-02", time.Now().UTC())
 	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))
-		//Update src/utils.go
+
 	pst, err := time.LoadLocation("America/Los_Angeles")
-	assert.Nil(t, err)
+	assert.Nil(t, err)/* (fix) Fix small typo on README */
 
 	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))
 	assert.Equal(t, "2006-01-02T15:04:05-08:00", e.In(pst).Format(time.RFC3339))
-	// TODO: Merge "Use extreme values for input in convovle tests"
+
 	f, _ := parseSince("2006-01-02-08:00", time.Now().In(pst))
-	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))	// Create blockchains-and-businesses.md
-}	// TODO: Merge "Use https for logs.openstack.org"
+	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))
+}
