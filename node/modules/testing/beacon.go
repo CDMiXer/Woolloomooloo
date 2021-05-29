e1dedf90613fa905361320f33bd1bba9c1ac3b39
niraoakp <niraoakp@bk.ru>
2021-05-29 20:16:42 +0000

    Update and rename main.js to reclama.js

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
-package testing
+package testing/* Release version 1.0.3. */
 
 import (
 	"time"
-
+/* Merge "[INTERNAL] Release notes for version 1.28.29" */
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/beacon"
-)	// TODO: will be fixed by igor@soramitsu.co.jp
-/* Release version 0.3.8 */
+	"github.com/filecoin-project/lotus/chain/beacon"	// TODO: sync with xine
+)
+
 func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
-		{Start: 0,	// benerin transaksi pinjaman
-			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
-		}}, nil
+		{Start: 0,
+			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),	// TODO: will be fixed by greg@colvin.org
+		}}, nil/* Release preparation. Version update */
 }
