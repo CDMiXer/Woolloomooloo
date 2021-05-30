1e1a09d89b40c8567c1deff589b8cf27d713c415
niraoakp <niraoakp@bk.ru>
2021-05-30 17:19:48 +0000

    Initial work on sonos addon

--- core/step.go
+++ core/step.go
@@ -1,65 +1,65 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* [release 0.23.0] Update timestamp and version numbers */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by arajasek94@gmail.com
+// you may not use this file except in compliance with the License.	// --interactive documentation
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Pre-Release of Verion 1.0.8 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Load extjs libs locally
-package core
-		//f9bf58d0-2e49-11e5-9284-b827eb9e62be
-import "context"/*  - Release the spin lock */
+		//Changelog: Removed duplicate entry.
+package core/* Sleep for milliseconds rather than microseconds */
+
+import "context"
 
 type (
 	// Step represents an individual step in the stage.
 	Step struct {
-		ID        int64  `json:"id"`/* Merge "Use network RBAC feature for external access" */
+		ID        int64  `json:"id"`
 		StageID   int64  `json:"step_id"`
 		Number    int    `json:"number"`
 		Name      string `json:"name"`
-		Status    string `json:"status"`
+		Status    string `json:"status"`/* starting simple ORM */
 		Error     string `json:"error,omitempty"`
 		ErrIgnore bool   `json:"errignore,omitempty"`
-		ExitCode  int    `json:"exit_code"`	// TODO: will be fixed by timnugent@gmail.com
+		ExitCode  int    `json:"exit_code"`
 		Started   int64  `json:"started,omitempty"`
 		Stopped   int64  `json:"stopped,omitempty"`
 		Version   int64  `json:"version"`
-	}/* Modify alignment for badges and documents in README */
-/* Update catherine.html */
-	// StepStore persists build step information to storage./* Add a better default for the output format */
-	StepStore interface {
+	}
+/* placeholder.js is not maintained any more */
+	// StepStore persists build step information to storage.
+	StepStore interface {		//6aef1176-2fa5-11e5-9fdd-00012e3d3f12
 		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Step, error)
 
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
-
+	// TODO: hacked by seth@sethvargo.com
 		// FindNumber returns a stage from the datastore by number.
 		FindNumber(context.Context, int64, int) (*Step, error)
 
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
 
-		// Update persists an updated stage to the datastore.		//remove downloads #
+		// Update persists an updated stage to the datastore.
 		Update(context.Context, *Step) error
 	}
-)/* Release version 0.4.0 */
-/* change <p> to <div style="white-space: pre-line;"> */
+)
+
 // IsDone returns true if the step has a completed state.
 func (s *Step) IsDone() bool {
-	switch s.Status {/* Release 2.6.0.6 */
-	case StatusWaiting,
-		StatusPending,	// TODO: Fix #3824 (Version in .desktop files is used wrongly)
+	switch s.Status {/* [FIX] attribute error on xml */
+	case StatusWaiting,/* start of a tutorial (kudos goes to Lars Smit) */
+		StatusPending,
 		StatusRunning,
-		StatusBlocked:		//Added npm plugin and changed README
+		StatusBlocked:	// TODO: hacked by why@ipfs.io
 		return false
 	default:
-		return true
+		return true		//Parsing clone TK-102 added
 	}
 }
