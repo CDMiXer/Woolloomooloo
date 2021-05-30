cd61dbc3f6e9d9533d7065740c0ef1cc64a018cb
niraoakp <niraoakp@bk.ru>
2021-05-30 05:56:16 +0000

    change output file names to format 000.html and 000.def.xml

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Delete GP_Content_Seg_Input_File_092115_Full_Data_weights.csv */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by alex.gaynor@gmail.com
-// Unless required by applicable law or agreed to in writing, software
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Catch exception trying to get announce room
+//
+// Unless required by applicable law or agreed to in writing, software/* Release v0.1.7 */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Add a key benefits section in Release Notes" */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Release '0.1~ppa11~loms~lucid'. */
 // limitations under the License.
-		//Rename compile-env.md to env.md
+
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning.
+// goconst linter's warning.		//Added ML.Net website
 //
 // nolint: lll, goconst
 package nodejs
-
+	// Re-organize code, a bit
 import (
 	"testing"
-
+/* move supported table to querying section */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
@@ -31,44 +31,44 @@ var testPackageSpec = schema.PackageSpec{
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
-	Types: map[string]schema.ComplexTypeSpec{/* GameState.released(key) & Press/Released constants */
-		"aws:s3/BucketCorsRule:BucketCorsRule": {
+	Types: map[string]schema.ComplexTypeSpec{
+		"aws:s3/BucketCorsRule:BucketCorsRule": {/* Update ReleaseNote-ja.md */
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
-				Properties: map[string]schema.PropertySpec{
+				Properties: map[string]schema.PropertySpec{/* Even more DEs! */
 					"stringProp": {
 						Description: "A string prop.",
-						TypeSpec: schema.TypeSpec{	// TODO: hacked by souzau@yandex.com
+						TypeSpec: schema.TypeSpec{
 							Type: "string",
-						},	// Added correct user ID
-					},
+						},
+					},/* windows date fix */
 				},
-			},
+			},/* Release 1.1.11 */
 		},
-	},
-	Resources: map[string]schema.ResourceSpec{
-		"aws:s3/bucket:Bucket": {
-			InputProperties: map[string]schema.PropertySpec{/* Trying remove .text */
-				"corsRules": {
+	},		//New hack PencilPreviewPlugin, created by lucid
+	Resources: map[string]schema.ResourceSpec{		//test Item::getMedias()
+		"aws:s3/bucket:Bucket": {	// TODO: Fixed breadthfirst and depthfirst traversal
+			InputProperties: map[string]schema.PropertySpec{
+				"corsRules": {/* Release of eeacms/www-devel:19.1.17 */
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
-					},
+					},	// TODO: added codemirror 4 version
 				},
-			},
-		},/* Update and rename 0000-00-00-Home.markdown to 0001-01-01-Home.markdown */
+			},/* Release of eeacms/jenkins-slave-dind:17.06-3.13 */
+		},
 	},
 }
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
-/* Fixed loading inventory of unavailable tech. Release 0.95.186 */
+
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
 }
 
-func TestDocLinkGenerationForPulumiTypes(t *testing.T) {/* Release notes for 0.43 are no longer preliminary */
+func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
@@ -81,18 +81,18 @@ func TestDocLinkGenerationForPulumiTypes(t *testing.T) {/* Release notes for 0.4
 		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")
 		assert.Equal(t, expected, link)
 	})
-}		//Improved formatting in readme.
+}
 
-func TestGetDocLinkForResourceType(t *testing.T) {/* update to secure version of jackson-databind */
+func TestGetDocLinkForResourceType(t *testing.T) {
 	pkg := getTestPackage(t)
-	d := DocLanguageHelper{}	// Adding space between hashtag and text
+	d := DocLanguageHelper{}
 	expected := "/docs/reference/pkg/nodejs/pulumi/aws/s3/#Bucket"
-	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")	// TODO: will be fixed by sjors@sprovoost.nl
+	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")
 	assert.Equal(t, expected, link)
 }
 
 func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
-	pkg := getTestPackage(t)		//Update lang-model-dataset.md
+	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	expected := "/docs/reference/pkg/nodejs/pulumi/aws/types/input/#BucketCorsRule"
 	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "s3", "BucketCorsRule", true)
