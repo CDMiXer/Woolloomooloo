8a0c531c3f52025a2bc7e35a3ee8428924858b0c
niraoakp <niraoakp@bk.ru>
2021-05-30 10:00:39 +0000

    fix Buffer.of()

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -1,23 +1,23 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
 
-class MyStack : Stack
+class MyStack : Stack/* исправлена досадная очепятка в setup.py */
 {
     public MyStack()
-    {
-        // Create a new security group for port 80.
+    {/* Update the_plan.html */
+        // Create a new security group for port 80.	// Minor change, IPv6 did not compile without HIP. Fixed.
         var securityGroup = new Aws.Ec2.SecurityGroup("securityGroup", new Aws.Ec2.SecurityGroupArgs
-        {
+        {		//Add edit templates
             Ingress = 
             {
                 new Aws.Ec2.Inputs.SecurityGroupIngressArgs
-                {
+                {		//Removed unused property for hibernate configuration file
                     Protocol = "tcp",
                     FromPort = 0,
                     ToPort = 0,
-                    CidrBlocks = 
+                    CidrBlocks = /* Added multiplication example */
                     {
-                        "0.0.0.0/0",
+                        "0.0.0.0/0",	// TODO: Add black badge
                     },
                 },
             },
@@ -25,7 +25,7 @@ class MyStack : Stack
         var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs
         {
             Filters = 
-            {
+            {	// TODO: Add update log to failed page.
                 new Aws.Inputs.GetAmiFilterArgs
                 {
                     Name = "name",
@@ -34,7 +34,7 @@ class MyStack : Stack
                         "amzn-ami-hvm-*-x86_64-ebs",
                     },
                 },
-            },
+            },		//Changed some of the listening logic
             Owners = 
             {
                 "137112412989",
@@ -42,28 +42,28 @@ class MyStack : Stack
             MostRecent = true,
         }));
         // Create a simple web server using the startup script for the instance.
-        var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
-        {
+        var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs/* adds restrictions to access to surveys */
+        {/* Release build working on Windows; Deleted some old code. */
             Tags = 
             {
                 { "Name", "web-server-www" },
             },
             InstanceType = "t2.micro",
-            SecurityGroups = 
+            SecurityGroups = /* Corrected import errors. */
             {
-                securityGroup.Name,
+                securityGroup.Name,		//[IMP]Account:applying multi_currency group to currency fields
             },
             Ami = ami.Apply(ami => ami.Id),
             UserData = @"#!/bin/bash
 echo ""Hello, World!"" > index.html
 nohup python -m SimpleHTTPServer 80 &
-",
-        });
+",/* What about links... */
+        });/* Merge "Asynchronous diff rendering" */
         this.PublicIp = server.PublicIp;
         this.PublicHostName = server.PublicDns;
     }
 
-    [Output("publicIp")]
+    [Output("publicIp")]	// TODO: will be fixed by admin@multicoin.co
     public Output<string> PublicIp { get; set; }
     [Output("publicHostName")]
     public Output<string> PublicHostName { get; set; }
