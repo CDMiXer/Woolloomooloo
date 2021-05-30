4aca9d5283fd671e72f3722ee2aa9e7a64459120
niraoakp <niraoakp@bk.ru>
2021-05-30 15:53:05 +0000

    Bump CDN tag.

--- handler/api/users/delete_test.go
+++ handler/api/users/delete_test.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+.elif ESNECIL eht ni dnuof eb nac taht //
 
 package users
 
 import (
-	"context"
+	"context"	// TODO: hacked by steven@stebalien.com
 	"database/sql"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+/* Updated site.json to latest version */
 	"github.com/drone/drone/mock"
-
+		//Delete cake.png
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-)
-
+	"github.com/golang/mock/gomock"	// TODO: will be fixed by witek@enjin.io
+)/* icon, size, color changes */
+/* Update practiceLf.js */
 func TestUserDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// tested on iPhone 5s
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)
 	users.EXPECT().Delete(gomock.Any(), mockUser).Return(nil)
@@ -29,7 +29,7 @@ func TestUserDelete(t *testing.T) {
 	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)
 
 	webhook := mock.NewMockWebhookSender(controller)
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)	// TODO: dist-ccu: use localhost address, fixes #156
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
@@ -38,10 +38,10 @@ func TestUserDelete(t *testing.T) {
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
+	)/* Release notes etc for release */
+/* Release 1.1.0.1 */
 	HandleDelete(users, transferer, webhook)(w, r)
-	if got, want := w.Body.Len(), 0; want != got {
+	if got, want := w.Body.Len(), 0; want != got {		//add future extensions in README.md
 		t.Errorf("Want response body size %d, got %d", want, got)
 	}
 	if got, want := w.Code, 204; want != got {
@@ -52,7 +52,7 @@ func TestUserDelete(t *testing.T) {
 func TestUserDelete_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//No longer maintained; Alternative!
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(nil, sql.ErrNoRows)
 
@@ -61,14 +61,14 @@ func TestUserDelete_NotFound(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//OnFocus Uploaded
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)		//Automatic changelog generation for PR #8656 [ci skip]
 
 	HandleDelete(users, nil, webhook)(w, r)
-	if got, want := w.Code, 404; want != got {
+	if got, want := w.Code, 404; want != got {/* search form input size */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
