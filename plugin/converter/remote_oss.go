58e5cd695d270b0d6188b3240369144d0b114840
niraoakp <niraoakp@bk.ru>
2021-05-30 14:19:05 +0000

    Various other build fixes

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Use C++ 11 (needed for node 4+) */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,21 +9,21 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* [Fix #161] Remove incorrect commas from Dossier#text_summary */
 // limitations under the License.
 
-// +build oss
+// +build oss/* fix: Procfile */
 
 package converter
 
 import (
 	"time"
 
-	"github.com/drone/drone/core"
-)
+	"github.com/drone/drone/core"	// TODO: Change PACE to set the ID (0) for the default session context
+)	// Merge "Clean up WorkManagerConfiguration" into flatfoot-background
 
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
-	return new(noop)
+	return new(noop)		//One more example for VersionReq::parse
 }
