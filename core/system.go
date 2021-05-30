bea6fcb2cdaab78c7d3e36a17e0528be7594bd65
niraoakp <niraoakp@bk.ru>
2021-05-30 02:30:58 +0000

    Remove font family from jquery tabs

--- core/system.go
+++ core/system.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release back pages when not fully flipping */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Changed SnpEff description. Fix broken test (??)  */
-///* added support for Arduino STM32 pin names */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core		//Fix ltr styles
+package core
 
 // System stores system information.
-type System struct {		//fa67e516-2e41-11e5-9284-b827eb9e62be
+type System struct {
 	Proto   string `json:"proto,omitempty"`
-	Host    string `json:"host,omitempty"`	// TODO: hacked by remco@dutchcoders.io
+	Host    string `json:"host,omitempty"`
 	Link    string `json:"link,omitempty"`
 	Version string `json:"version,omitempty"`
-}	// TODO: Fix issue where legend tour tip flickers 
+}
