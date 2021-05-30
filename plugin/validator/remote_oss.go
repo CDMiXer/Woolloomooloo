60686ed9031e713c38f0eaa514745a8384f5eca5
niraoakp <niraoakp@bk.ru>
2021-05-30 02:43:45 +0000

    Update .travis.yml for removing python 3.4

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by yuvalalaluf@gmail.com
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* remove duplicated max delay check */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,14 +16,14 @@
 
 package validator
 
-import (
+import (/* Update 1sTry/1003.md */
 	"time"
 
-	"github.com/drone/drone/core"
-)
+	"github.com/drone/drone/core"		//Use license-maven-plugin instead of maven-license-plugin
+)/* Release note for 0.6.0 */
 
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
-func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
+func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {		//Added new methods in qImage class
 	return new(noop)
-}
+}		//Merge "Fix guts are not bound properly." into nyc-dev
