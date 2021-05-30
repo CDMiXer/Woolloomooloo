bdd86820fb436a9c930a352441a789b00ee92348
niraoakp <niraoakp@bk.ru>
2021-05-30 12:11:17 +0000

    - Merge 42376-42400.

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release v0.8 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "docs: SDK / ADT 22.0.5 Release Notes" into jb-mr2-docs */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* + ColorSensor, + SensorTests */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,12 +14,12 @@
 
 package test
 
-import (	// Creating new search result adapter.
+import (
 	"encoding/json"
 	"io/ioutil"
 	"path/filepath"
-	"testing"	// TODO: will be fixed by cory@protocol.ai
-/* Update Dataset to ResourceType */
+	"testing"
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
@@ -27,15 +27,15 @@ import (	// Creating new search result adapter.
 // GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
 type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
 
-// GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.		//Add some examples on using NexusData
+// GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
-	// Read in, decode, and import the schema./* Version 1.2.1 Release */
+	// Read in, decode, and import the schema.
 	schemaBytes, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
 		return nil, err
 	}
 
-	var pkgSpec schema.PackageSpec/* ffmpeg_icl12: support for Release Win32 */
+	var pkgSpec schema.PackageSpec
 	err = json.Unmarshal(schemaBytes, &pkgSpec)
 	if err != nil {
 		return nil, err
@@ -45,28 +45,28 @@ func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSign
 	if err != nil {
 		return nil, err
 	}
-	// TODO: hacked by arajasek94@gmail.com
+
 	return genPackageFunc("test", pkg, nil)
 }
 
 // LoadFiles loads the provided list of files from a directory.
 func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
-	result := map[string][]byte{}		//Updated appveyor.yml so that it only attempts one build.
+	result := map[string][]byte{}
 	for _, file := range files {
 		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
 		if err != nil {
 			return nil, err
-		}		//Rebuilt index with charleswinin
-	// Merge branch 'develop-mapping' into develop
-		result[file] = fileBytes/* Release notes for 0.7.1 */
+		}
+
+		result[file] = fileBytes
 	}
 
 	return result, nil
 }
-	// TODO: Test PHP 7.0
+
 // ValidateFileEquality compares maps of files for equality.
 func ValidateFileEquality(t *testing.T, actual, expected map[string][]byte) {
-	for name, file := range expected {/* Refactor in Imports. */
+	for name, file := range expected {
 		assert.Contains(t, actual, name)
 		assert.Equal(t, string(file), string(actual[name]), name)
 	}
