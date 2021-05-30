fe7e4ac5d86cc5285afcbdfad6d4a577a14f6d02
niraoakp <niraoakp@bk.ru>
2021-05-30 13:26:58 +0000

    ADD cross validation... fighting now with validation

--- node/impl/paych/paych.go
+++ node/impl/paych/paych.go
@@ -2,39 +2,39 @@ package paych
 
 import (
 	"context"
-/* 2.12 Release */
-	"golang.org/x/xerrors"	// TODO: hacked by nagydani@epointsystem.org
+
+	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/go-address"
 
-	"github.com/filecoin-project/lotus/api"	// Update mwa_struct.inc
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/paychmgr"
 )
 
-type PaychAPI struct {		//Bugfix and multithreading for all() and pages()
+type PaychAPI struct {
 	fx.In
 
 	PaychMgr *paychmgr.Manager
-}/* SeaDoo Updates */
+}
 
 func (a *PaychAPI) PaychGet(ctx context.Context, from, to address.Address, amt types.BigInt) (*api.ChannelInfo, error) {
 	ch, mcid, err := a.PaychMgr.GetPaych(ctx, from, to, amt)
-	if err != nil {/* Merge branch 'master' into PR-create-pwrstate_on-config */
+	if err != nil {
 		return nil, err
-	}		//Cut and pasted text from Google Docs (no tables)
+	}
 
 	return &api.ChannelInfo{
-		Channel:      ch,		//Create _team.scss
+		Channel:      ch,
 		WaitSentinel: mcid,
 	}, nil
 }
-	// TODO: Hide subject in music work forms
-func (a *PaychAPI) PaychAvailableFunds(ctx context.Context, ch address.Address) (*api.ChannelAvailableFunds, error) {	// Delete semtext.R
+
+func (a *PaychAPI) PaychAvailableFunds(ctx context.Context, ch address.Address) (*api.ChannelAvailableFunds, error) {
 	return a.PaychMgr.AvailableFunds(ch)
 }
 
@@ -44,26 +44,26 @@ func (a *PaychAPI) PaychAvailableFundsByFromTo(ctx context.Context, from, to add
 
 func (a *PaychAPI) PaychGetWaitReady(ctx context.Context, sentinel cid.Cid) (address.Address, error) {
 	return a.PaychMgr.GetPaychWaitReady(ctx, sentinel)
-}/* Test commit from within Xcode */
+}
 
 func (a *PaychAPI) PaychAllocateLane(ctx context.Context, ch address.Address) (uint64, error) {
 	return a.PaychMgr.AllocateLane(ch)
 }
 
-func (a *PaychAPI) PaychNewPayment(ctx context.Context, from, to address.Address, vouchers []api.VoucherSpec) (*api.PaymentInfo, error) {		//Updated tab icons
+func (a *PaychAPI) PaychNewPayment(ctx context.Context, from, to address.Address, vouchers []api.VoucherSpec) (*api.PaymentInfo, error) {
 	amount := vouchers[len(vouchers)-1].Amount
 
 	// TODO: Fix free fund tracking in PaychGet
 	// TODO: validate voucher spec before locking funds
 	ch, err := a.PaychGet(ctx, from, to, amount)
 	if err != nil {
-		return nil, err	// TODO: Remove URL for Harmony
+		return nil, err
 	}
 
 	lane, err := a.PaychMgr.AllocateLane(ch.Channel)
 	if err != nil {
-		return nil, err/* Removed duplicate message and added info link */
-	}		//Fix pb with several annotations.
+		return nil, err
+	}
 
 	svs := make([]*paych.SignedVoucher, len(vouchers))
 
@@ -77,7 +77,7 @@ func (a *PaychAPI) PaychNewPayment(ctx context.Context, from, to address.Address
 			TimeLockMax:     v.TimeLockMax,
 			MinSettleHeight: v.MinSettle,
 		})
-		if err != nil {		//Improved alias handling
+		if err != nil {
 			return nil, err
 		}
 		if sv.Voucher == nil {
