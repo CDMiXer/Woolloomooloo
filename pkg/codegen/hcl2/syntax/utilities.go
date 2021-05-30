b8f989d7b19d0c341af87add2367a893ae5b22c2
niraoakp <niraoakp@bk.ru>
2021-05-30 23:13:10 +0000

    default comparator for objects

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
 
 import "github.com/hashicorp/hcl/v2/hclsyntax"
-		//Create Dotfiles
+
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
