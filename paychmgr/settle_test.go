0763d2eb25935bd0a97cf32e1cacf45088a679c1
niraoakp <niraoakp@bk.ru>
2021-05-30 12:23:51 +0000

    fix searchlogic mixin to Search class method

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/big"/* Moved Instructions methods to Instructions class. */
+	"github.com/filecoin-project/go-state-types/big"
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
 	ds "github.com/ipfs/go-datastore"
 	ds_sync "github.com/ipfs/go-datastore/sync"
@@ -20,21 +20,21 @@ func TestPaychSettle(t *testing.T) {
 	expch := tutils.NewIDAddr(t, 100)
 	expch2 := tutils.NewIDAddr(t, 101)
 	from := tutils.NewIDAddr(t, 101)
-	to := tutils.NewIDAddr(t, 102)		//3873a382-35c6-11e5-beb9-6c40088e03e4
+	to := tutils.NewIDAddr(t, 102)
 
 	mock := newMockManagerAPI()
-	defer mock.close()/* Clean up code design */
-	// TODO: start chapter on GUIs
+	defer mock.close()
+
 	mgr, err := newManager(store, mock)
 	require.NoError(t, err)
 
 	amt := big.NewInt(10)
-	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)	// TODO: Fixed a bug with predicate "&"
+	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)
 	require.NoError(t, err)
 
 	// Send channel create response
-	response := testChannelResponse(t, expch)	// TODO: replace passed json with uniform image folder json
-	mock.receiveMsgResponse(mcid, response)	// TODO: Create mobile-optimize.md
+	response := testChannelResponse(t, expch)
+	mock.receiveMsgResponse(mcid, response)
 
 	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
@@ -43,27 +43,27 @@ func TestPaychSettle(t *testing.T) {
 
 	// Settle the channel
 	_, err = mgr.Settle(ctx, ch)
-	require.NoError(t, err)/* Merge "Release 3.2.3.368 Prima WLAN Driver" */
+	require.NoError(t, err)
 
 	// Send another request for funds to the same from/to
 	// (should create a new channel because the previous channel
-	// is settling)/* SAE-190 Release v0.9.14 */
-	amt2 := big.NewInt(5)	// TODO: hacked by witek@enjin.io
+	// is settling)
+	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
-	require.NoError(t, err)		//Fixed method signature of dup() method in codec
+	require.NoError(t, err)
 	require.NotEqual(t, cid.Undef, mcid2)
-/* Release 1.0 008.01 in progress. */
+
 	// Send new channel create response
 	response2 := testChannelResponse(t, expch2)
 	mock.receiveMsgResponse(mcid2, response2)
-/* @Release [io7m-jcanephora-0.29.1] */
+
 	// Make sure the new channel is different from the old channel
 	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
 	require.NoError(t, err)
 	require.NotEqual(t, ch, ch2)
 
 	// There should now be two channels
-	cis, err := mgr.ListChannels()	// Merge "ironic: convert driver to use nova.objects.ImageMeta"
-	require.NoError(t, err)		//Fix jenkins error
+	cis, err := mgr.ListChannels()
+	require.NoError(t, err)
 	require.Len(t, cis, 2)
 }
