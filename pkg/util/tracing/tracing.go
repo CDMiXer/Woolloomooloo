109412d6168997170931301c6f773f158b76515c
niraoakp <niraoakp@bk.ru>
2021-05-30 11:51:31 +0000

    Rename divplayer.min.js to divplayer.js

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,14 +1,14 @@
-// Copyright 2016-2019, Pulumi Corporation.		//Merge "Use Queens UCA for nova-multiattach job"
+// Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Added compilation command
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Fixes the most annoying thing about admin helping.
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* fixes for serial port. it works with real hardware! */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fix problems with cartopy projection: Contrast/multiprocessor support
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,25 +16,25 @@ package tracing
 
 import "context"
 
-// tracingOptionsKey is the value used as the context key for TracingOptions.		//Add mixin for absolute positioned containers
+// tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
 // TracingOptions describes the set of options available for configuring tracing on a per-request basis.
-type Options struct {	// TODO: will be fixed by jon@atack.com
+type Options struct {
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
-	// calls./* Updated Release configurations to output pdb-only symbols */
+	// calls.
 	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
 	TracingHeader string
 }
-/* Release FPCM 3.1.0 */
+
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
-}/* MySQL: Every derived table must have its own alias */
+}
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
-// this function returns the zero value./* Add r127409 back now that the windows file was updated. */
+// this function returns the zero value.
 func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
 	return opts
