a30ca97ea9e3bbf92103707c6bfc5dc9905ae98a
niraoakp <niraoakp@bk.ru>
2021-05-30 14:44:27 +0000

    Updated the build.properties file

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,7 +1,7 @@
 package stores
 
-import (
-	"context"
+import (	// Add a guide to run gitcoin remotely.
+	"context"	// TODO: parser ready to roll
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -10,13 +10,13 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
+/* Update entityPopulate.c */
 type Store interface {
 	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
-	// non-primary copy if there no primary copies
+	// non-primary copy if there no primary copies		//f378bbd8-2e58-11e5-9284-b827eb9e62be
 	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
 	// move sectors into storage
