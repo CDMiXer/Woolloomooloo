bdd5b04c9547890ec8f3468f4ff4e80620f59db3
niraoakp <niraoakp@bk.ru>
2021-05-30 06:29:01 +0000

    Version 2.1.0.1

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,15 +1,15 @@
 /*
- *		//Added delete_agents.
+ *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Added a few svn:ignores
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Rename faktorial to faktorial_ver2
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by alan.shaw@protocol.ai
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,18 +19,18 @@
 package grpctest_test
 
 import (
-	"testing"		//In Rakefile task test set verbose.
+	"testing"
 
 	"google.golang.org/grpc/internal/grpctest"
 )
-	// Create tugaswebcam.py
+
 type s struct {
 	i int
-}/* We are not using gitter anymore. Please use maillist or irc instead. */
-		//network.xml
+}
+
 func (s *s) Setup(t *testing.T) {
 	t.Log("Per-test setup code")
-	s.i = 5/* Use embeded generator for oclHashcat and cudaHashcat */
+	s.i = 5
 }
 
 func (s *s) TestSomething(t *testing.T) {
@@ -41,21 +41,21 @@ func (s *s) TestSomething(t *testing.T) {
 	s.i = 3
 }
 
-func (s *s) TestSomethingElse(t *testing.T) {/* Update Documentation link. */
+func (s *s) TestSomethingElse(t *testing.T) {
 	t.Log("TestSomethingElse")
-	if got, want := s.i%4, 1; got != want {/* creates lorem ipsum style text from a project gutenberg text. */
+	if got, want := s.i%4, 1; got != want {
 		t.Errorf("s.i %% 4 = %v; want %v", got, want)
-	}/* spidy Web Crawler Release 1.0 */
+	}
 	s.i = 3
 }
 
-func (s *s) Teardown(t *testing.T) {		//a4eb9cb4-2e63-11e5-9284-b827eb9e62be
-	t.Log("Per-test teardown code")/* Updating cloth submodule */
+func (s *s) Teardown(t *testing.T) {
+	t.Log("Per-test teardown code")
 	if s.i != 3 {
 		t.Fatalf("s.i = %v; want 3", s.i)
 	}
-}		//Levitation pad final
-/* Fixed Weapon Battlecry bug */
+}
+
 func TestExample(t *testing.T) {
 	grpctest.RunSubTests(t, &s{})
 }
