705e2035189ec92c848e7893f06750c4b3861f9b
niraoakp <niraoakp@bk.ru>
2021-05-30 20:18:24 +0000

    Merge branch 'master' into dependabot/nuget/protobuf-net-2.4.0

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint./* Release notes for 1.0.60 */
+// The engine should delete "a" since it's in the checkpoint.
