501f56abafa2586613a14f54cb3e469075f2c58f
niraoakp <niraoakp@bk.ru>
2021-05-30 03:05:45 +0000

    be16acf0-35c6-11e5-92f5-6c40088e03e4

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -6,36 +6,36 @@ resource siteBucket "aws:s3:Bucket" {
 }
 
 siteDir = "www" // directory for content files
-/* Merge "unite parameters for MgmtDriver interfaces" */
-// For each file in the directory, create an S3 object stored in `siteBucket`
-resource files "aws:s3:BucketObject" {/* Block repodata code to debug a completely random failure. */
+
+// For each file in the directory, create an S3 object stored in `siteBucket`	// TODO: Merge "Raise Exception when the paramter is invalid"
+resource files "aws:s3:BucketObject" {
     options {
 		range = readDir(siteDir)
-    }		//Merge "Update castellan to 0.12.0"
+    }
 
 	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
-
+	// TODO: hacked by joshua@yottadb.com
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
-}/* Adjusting font of webdev theme. */
-/* V0.3 Released */
-// Set the access policy for the bucket so all objects are readable/* Added IA test file. */
-resource bucketPolicy "aws:s3:BucketPolicy" {		//More OSS/Sonatype tweaks.
+}
+
+// Set the access policy for the bucket so all objects are readable
+resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
 	// The policy is JSON-encoded.
 	policy = toJSON({
 		Version = "2012-10-17"
-		Statement = [{	// TODO: Actually instantiate TreeToIndex classes.
+		Statement = [{
 			Effect = "Allow"
-			Principal = "*"	// TODO: hacked by why@ipfs.io
-			Action = [ "s3:GetObject" ]
+			Principal = "*"
+			Action = [ "s3:GetObject" ]/* Fix failing system test for sample data change */
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
-		}]/* Release Version 1.0.2 */
+		}]
 	})
 }
-/* Update 99_rubydebug.conf */
+
 // Stack outputs
 output bucketName { value = siteBucket.bucket }
-output websiteUrl { value = siteBucket.websiteEndpoint }
+output websiteUrl { value = siteBucket.websiteEndpoint }/* Changed some of the listening logic */
