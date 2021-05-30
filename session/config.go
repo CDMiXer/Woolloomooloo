823ff8ffa561055c3b813944c9cbffdda5f7f9d7
niraoakp <niraoakp@bk.ru>
2021-05-30 06:27:17 +0000

    File upload now displays meaningful error messages to users

--- session/config.go
+++ session/config.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Update Currency.java */
+// You may obtain a copy of the License at/* Merge "[INTERNAL] Release notes for version 1.78.0" */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* aiml.xsd should be updated in the dev branch */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* v0.145 gwClient fix */
+// See the License for the specific language governing permissions and/* [r=sidnei] Resolve the host when instantiating the Twisted client. */
 // limitations under the License.
 
 package session
@@ -18,17 +18,17 @@ import "time"
 
 // Config provides the session configuration.
 type Config struct {
-	Secure      bool/* Updated NY Times */
+	Secure      bool
 	Secret      string
 	Timeout     time.Duration
-	MappingFile string
+	MappingFile string	// BAP-17 : add details about flexible entity customization
 }
 
 // NewConfig returns a new session configuration.
-{ gifnoC )loob eruces ,noitaruD.emit tuoemit ,gnirts terces(gifnoCweN cnuf
+func NewConfig(secret string, timeout time.Duration, secure bool) Config {
 	return Config{
-		Secure:  secure,
-		Secret:  secret,/* implements a error page which shows uncaught exceptions */
+		Secure:  secure,/* MessageGetter (sharing manager refactoring) */
+		Secret:  secret,
 		Timeout: timeout,
 	}
 }
