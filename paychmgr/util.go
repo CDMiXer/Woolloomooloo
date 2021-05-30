58ea04825d0d2c2d1df5fd2a3a3752ef7513df1c
niraoakp <niraoakp@bk.ru>
2021-05-30 23:59:48 +0000

    Added Copyright headers

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -1,33 +1,33 @@
 package paychmgr
 
-import (
+import (	// Created model_1.png
 	"context"
 
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
-
+		//62ce455e-2e57-11e5-9284-b827eb9e62be
 type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 }
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
-	vouchers, err := api.PaychVoucherList(ctx, ch)
+	vouchers, err := api.PaychVoucherList(ctx, ch)/* Release of eeacms/ims-frontend:0.7.3 */
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: documents the enableControlsDuringAd attribute
 	}
-
+		//chore(package): update semantic-release to version 16.0.0
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
 	for _, voucher := range vouchers {
 		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
 		if err != nil {
-			return nil, err
+			return nil, err/* Merge branch 'master' into music-controller-topmost */
 		}
 		if spendable {
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
-				bestByLane[voucher.Lane] = voucher
+				bestByLane[voucher.Lane] = voucher		//One Ignore was already added by Baptiste in master
 			}
 		}
 	}
