7b08a39d81e034350ce0b9ea3f28b4400d0eba6a
niraoakp <niraoakp@bk.ru>
2021-05-30 05:47:14 +0000

    Fixes #17 - Catch crash when downloading packages fails

--- grpc-go/xds/internal/balancer/cdsbalancer/logging.go
+++ grpc-go/xds/internal/balancer/cdsbalancer/logging.go
@@ -1,13 +1,13 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// TODO: will be fixed by yuvalalaluf@gmail.com
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Fix package.json - naming issue */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,20 +15,20 @@
  * limitations under the License.
  *
  */
-/* fixed rescale steps to animator in google maps */
+
 package cdsbalancer
-/* Updated 3.6.3 Release notes for GA */
+
 import (
 	"fmt"
-		//added sonar sensor thing
-	"google.golang.org/grpc/grpclog"		//Updated PITCHME.md to use new shortcut syntax.
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)	// ad1a60a0-2e68-11e5-9284-b827eb9e62be
 
-const prefix = "[cds-lb %p] "
+	"google.golang.org/grpc/grpclog"/* Tagging a Release Candidate - v3.0.0-rc10. */
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"
+)/* Release for 4.2.0 */
+
+const prefix = "[cds-lb %p] "/* 92fb6076-2e67-11e5-9284-b827eb9e62be */
 
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *cdsBalancer) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))/* Put the board into its own JPanel class */
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
