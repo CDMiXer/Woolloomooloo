e41acc543aeb650b57ab115358aa1d2035426bc0
niraoakp <niraoakp@bk.ru>
2021-05-30 01:02:00 +0000

    update https://github.com/uBlockOrigin/uAssets/issues/3688

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -5,25 +5,25 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
-	cbor "github.com/ipfs/go-ipld-cbor"/* Integrate with coverage */
+	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* 6d43775a-2e54-11e5-9284-b827eb9e62be */
+)
 
-func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {/* Signed 2.2 Release Candidate */
+func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
-	// TODO: use lablePreferredWidth as width 
+
 	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
 	}
 
-	return &types.Actor{	// TODO: hacked by zaq1tomo@gmail.com
+	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,		//Fixed latest builds URL
-		Nonce:   0,	// TODO: will be fixed by brosner@gmail.com
+		Head:    stcid,
+		Nonce:   0,
 		Balance: types.NewInt(0),
 	}, nil
 }
