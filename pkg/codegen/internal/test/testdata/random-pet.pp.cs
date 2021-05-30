72d1b8b97082e141083b4b1c20cc40cb87a74972
niraoakp <niraoakp@bk.ru>
2021-05-30 16:42:20 +0000

    Add unit and functional tests

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
-using Random = Pulumi.Random;
+using Random = Pulumi.Random;/* Update category.html */
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
+        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs	// TODO: 539b3858-4b19-11e5-b1ce-6c40088e03e4
         {
             Prefix = "doggo",
         });
     }
-
+/* Delete customer_microservice.png */
 }
