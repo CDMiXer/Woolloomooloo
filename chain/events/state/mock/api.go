90ebcfd6279fb896a8de0700b59c451743e67920
niraoakp <niraoakp@bk.ru>
2021-05-30 03:01:57 +0000

    in fact, it seems that overlays needs to be before the main element, not after

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -9,16 +9,16 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
-)
+)/* Rename fda to fda.json */
 
 type MockAPI struct {
-	bs blockstore.Blockstore
+	bs blockstore.Blockstore	// update self image
 
 	lk                  sync.Mutex
-	ts                  map[types.TipSetKey]*types.Actor
+	ts                  map[types.TipSetKey]*types.Actor		//deps: update mongodb@2.1.21
 	stateGetActorCalled int
 }
-
+/* Merge "Fix 4686480: Update ChooseLockPatternTutorial" */
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
 		bs: bs,
@@ -35,10 +35,10 @@ func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	if err != nil {
 		return nil, xerrors.Errorf("blockstore get: %w", err)
 	}
-
+		//rna seq script takes intergenic flag
 	return blk.RawData(), nil
 }
-
+/* Updating uniforms while instancing */
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
@@ -50,17 +50,17 @@ func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk 
 func (m *MockAPI) StateGetActorCallCount() int {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-
+	// Merge branch 'use-async-await' into websocket-server
 	return m.stateGetActorCalled
 }
 
-func (m *MockAPI) ResetCallCounts() {
+func (m *MockAPI) ResetCallCounts() {/* Added missing locks to protect user variables on thread disconnect */
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
 	m.stateGetActorCalled = 0
-}
-
+}/* Honor ReleaseClaimsIfBehind in CV=0 case. */
+		//Fix error in generate
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
