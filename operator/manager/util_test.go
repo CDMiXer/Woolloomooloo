08eddc0212746ff1c0f8ab66dac1f96aaadf8687
niraoakp <niraoakp@bk.ru>
2021-05-30 12:38:31 +0000

    Bumped version to 1.1.0.

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
-
+// Use of this source code is governed by the Drone Non-Commercial License/* [snomed] Replace ModuleCollectorConfiguration with POJO */
+// that can be found in the LICENSE file.
+/* Update PD example. */
 package manager
