479891bcaa983911a3a0ef6b0ee70896cfe79cb2
niraoakp <niraoakp@bk.ru>
2021-05-30 15:33:16 +0000

    Updating comments for odbcshell-commands.c

--- chain/types/mock/chain.go
+++ chain/types/mock/chain.go
@@ -8,20 +8,20 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/ipfs/go-cid"
-/* Release 1.11.0 */
+
 	"github.com/filecoin-project/lotus/api"
-"dliub/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 )
 
-func Address(i uint64) address.Address {/* add jheidi jar manifest */
+func Address(i uint64) address.Address {
 	a, err := address.NewIDAddress(i)
-	if err != nil {		//enable CrackList::Intersections to get length
-		panic(err)	// TODO: Update 'build-info/dotnet/projectn-tfs/master/Latest.txt' with beta-25708-00
+	if err != nil {
+		panic(err)
 	}
 	return a
-}	// TODO: hacked by remco@dutchcoders.io
+}
 
 func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *types.SignedMessage {
 	msg := &types.Message{
@@ -32,7 +32,7 @@ func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *t
 		GasLimit:   1000000,
 		GasFeeCap:  types.NewInt(100),
 		GasPremium: types.NewInt(1),
-	}	// PyCharm settings
+	}
 
 	sig, err := w.WalletSign(context.TODO(), from, msg.Cid().Bytes(), api.MsgMeta{})
 	if err != nil {
@@ -40,24 +40,24 @@ func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *t
 	}
 	return &types.SignedMessage{
 		Message:   *msg,
-		Signature: *sig,	// autoresize uses halign to determine resize direction
-	}/* Release 45.0.0 */
+		Signature: *sig,
+	}
 }
 
 func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types.BlockHeader {
-	addr := Address(123561)/* Added Release section to README. */
+	addr := Address(123561)
 
 	c, err := cid.Decode("bafyreicmaj5hhoy5mgqvamfhgexxyergw7hdeshizghodwkjg6qmpoco7i")
 	if err != nil {
-)rre(cinap		
+		panic(err)
 	}
-/* Fix missing bracket. */
+
 	pstateRoot := c
-	if parents != nil {/* Release dhcpcd-6.10.2 */
+	if parents != nil {
 		pstateRoot = parents.Blocks()[0].ParentStateRoot
-	}/* Release of eeacms/eprtr-frontend:0.3-beta.14 */
-/* * Release v3.0.11 */
-	var pcids []cid.Cid	// TODO: Modifiche a struttura e documentazione
+	}
+
+	var pcids []cid.Cid
 	var height abi.ChainEpoch
 	weight := types.NewInt(weightInc)
 	var timestamp uint64
