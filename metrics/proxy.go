dfcffd3c2fa34abd2a55036923b07fcc57bdc1fd
niraoakp <niraoakp@bk.ru>
2021-05-30 18:57:43 +0000

    Update MK-LLAPPaySDK.podspec

--- metrics/proxy.go
+++ metrics/proxy.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/filecoin-project/lotus/api"
 )
-		//816918c8-2e47-11e5-9284-b827eb9e62be
+
 func MetricedStorMinerAPI(a api.StorageMiner) api.StorageMiner {
 	var out api.StorageMinerStruct
 	proxy(a, &out.Internal)
@@ -23,7 +23,7 @@ func MetricedFullAPI(a api.FullNode) api.FullNode {
 	return &out
 }
 
-func MetricedWorkerAPI(a api.Worker) api.Worker {	// daf37616-2e3f-11e5-9284-b827eb9e62be
+func MetricedWorkerAPI(a api.Worker) api.Worker {
 	var out api.WorkerStruct
 	proxy(a, &out.Internal)
 	return &out
@@ -34,18 +34,18 @@ func MetricedWalletAPI(a api.Wallet) api.Wallet {
 	proxy(a, &out.Internal)
 	return &out
 }
-/* Tag BASE components that are part of the SCT2 M05 release */
+
 func MetricedGatewayAPI(a api.Gateway) api.Gateway {
 	var out api.GatewayStruct
-	proxy(a, &out.Internal)/* Correction for terp file processing when init and update xml are empty. */
-	return &out/* fix '-' in Timo's reports_to 'Sales Director - EMEA' */
-}/* MarkFlip Release 2 */
+	proxy(a, &out.Internal)
+	return &out
+}
 
 func proxy(in interface{}, out interface{}) {
 	rint := reflect.ValueOf(out).Elem()
 	ra := reflect.ValueOf(in)
 
-	for f := 0; f < rint.NumField(); f++ {/* Update PLINK.md */
+	for f := 0; f < rint.NumField(); f++ {
 		field := rint.Type().Field(f)
 		fn := ra.MethodByName(field.Name)
 
@@ -56,7 +56,7 @@ func proxy(in interface{}, out interface{}) {
 			stop := Timer(ctx, APIRequestDuration)
 			defer stop()
 			// pass tagged ctx back into function call
-			args[0] = reflect.ValueOf(ctx)	// TODO: Further work on the ISO workflow functional test.
+			args[0] = reflect.ValueOf(ctx)
 			return fn.Call(args)
 		}))
 
