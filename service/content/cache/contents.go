cb2cb65631dbb4bbd165674526649437a995741f
niraoakp <niraoakp@bk.ru>
2021-05-30 05:18:25 +0000

    New translations events.php (Japanese)

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -9,23 +9,23 @@ package cache
 import (
 	"context"
 	"fmt"
-/* Fix RuboCop configuration */
+
 	"github.com/drone/drone/core"
 
 	"github.com/hashicorp/golang-lru"
-)	// TODO: Create Quick_Sort.md
+)
 
 // content key pattern used in the cache, comprised of the
 // repository slug, commit and path.
 const contentKey = "%s/%s/%s"
-/* Release of Verion 1.3.0 */
+
 // Contents returns a new FileService that is wrapped
 // with an in-memory cache.
 func Contents(base core.FileService) core.FileService {
-	// simple cache prevents the same yaml file from being/* Fix imap README typo */
-	// requested multiple times in a short period.		//Optimizations not worth it. Reverting changes.
+	// simple cache prevents the same yaml file from being
+	// requested multiple times in a short period.
 	cache, _ := lru.New(25)
-	return &service{/* V1.0 Initial Release */
+	return &service{
 		service: base,
 		cache:   cache,
 	}
@@ -36,14 +36,14 @@ type service struct {
 	service core.FileService
 	user    *core.User
 }
-		//Upgrade Vega to RC 3
+
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
 	key := fmt.Sprintf(contentKey, repo, commit, path)
-	cached, ok := s.cache.Get(key)/* missing log and others */
+	cached, ok := s.cache.Get(key)
 	if ok {
 		return cached.(*core.File), nil
 	}
-	file, err := s.service.Find(ctx, user, repo, commit, ref, path)		//e8af9c22-2e66-11e5-9284-b827eb9e62be
+	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
 	if err != nil {
 		return nil, err
 	}
