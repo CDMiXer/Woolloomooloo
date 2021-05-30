7fbf0ba190996f4c40a6a3320b7b2488c8700256
niraoakp <niraoakp@bk.ru>
2021-05-30 00:41:47 +0000

    Versao 22.08

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Documentation for parsePatch and applyPatches
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package sign
+package sign	// TODO: refactored carbonlink, broke out into separate module
