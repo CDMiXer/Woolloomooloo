e69c66f4aaaa541aac1bc5dbc2350d92389e53e8
niraoakp <niraoakp@bk.ru>
2021-05-30 05:16:31 +0000

    More dialog ownerships

--- gen/examples/echo/client.go
+++ gen/examples/echo/client.go
@@ -1,10 +1,10 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.	// Tmeme theme
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* I fixed all the compile warnings for Unicode Release build. */
+
 // +build ignore
 
-package main		//tokenak gorde funtzio berria
+package main
 
 import (
 	"flag"
@@ -14,7 +14,7 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/gorilla/websocket"/* Merge "Updating the light Date picker theme. Some UI fixes." */
+	"github.com/gorilla/websocket"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
@@ -22,21 +22,21 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-/* adding a pic of the architecture */
-	interrupt := make(chan os.Signal, 1)/* Hotfix Release 3.1.3. See CHANGELOG.md for details (#58) */
-	signal.Notify(interrupt, os.Interrupt)		//fix the fix for #1143 (close double quote)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
 
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Fatal("dial:", err)		//chore(deps): update dependency @commitlint/cli to v7.5.2
+		log.Fatal("dial:", err)
 	}
-	defer c.Close()/* Merge "Update release notes for security group rule deletion" */
-/* Release version: 0.7.9 */
+	defer c.Close()
+
 	done := make(chan struct{})
-/* added microTK */
+
 	go func() {
 		defer close(done)
 		for {
@@ -52,7 +52,7 @@ func main() {
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
-	for {		//Delete dbload.php
+	for {
 		select {
 		case <-done:
 			return
@@ -71,7 +71,7 @@ func main() {
 			if err != nil {
 				log.Println("write close:", err)
 				return
-}			
+			}
 			select {
 			case <-done:
 			case <-time.After(time.Second):
