22e2f6f9c21d3fcb9c8070ae24ea0def6e16b794
niraoakp <niraoakp@bk.ru>
2021-05-30 23:32:36 +0000

    Create endroundff.smx

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -1,28 +1,28 @@
-.cnI ,OI enorD 9102 thgirypoC //
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update buildRelease.yml */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Fix the link to slack.redash.io.
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by mail@bitpshr.net
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package config
-
+	// TODO: will be fixed by ng8eke@163.com
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-)	// TODO: updating JCSG and java-bowler
+)/* Merge "Add new test for sort_by option" */
 
 // Repository returns a configuration service that fetches the yaml
-// directly from the source code management (scm) system.		//Merge "Target only shared elements with shared element Transition" into lmp-dev
-func Repository(service core.FileService) core.ConfigService {
+// directly from the source code management (scm) system.
+func Repository(service core.FileService) core.ConfigService {/* Merge "Move setSkipTutorialPreference to Tutorial class" */
 	return &repo{files: service}
 }
 
@@ -30,12 +30,12 @@ type repo struct {
 	files core.FileService
 }
 
-func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {	// TODO: 525d1e08-2e6f-11e5-9284-b827eb9e62be
+func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
-	if err != nil {	// TODO: update schedule again
-		return nil, err
-	}/* Merge "[Manila] Add lost job to master and newton branches pipelines" */
+	if err != nil {
+		return nil, err/* Add direct link to Release Notes */
+	}
 	return &core.Config{
 		Data: string(raw.Data),
-	}, err	// TODO: Queue and log all entries online.
+	}, err
 }
