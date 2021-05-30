19276e0a3826d5648f8b9bee26190e8040609e44
niraoakp <niraoakp@bk.ru>
2021-05-30 15:20:31 +0000

    Pin django to latest version 2.0.1

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-
+		//ARTEMIS-2540 Display LargeMessage column in message browser of admin UI
 import "github.com/filecoin-project/go-state-types/network"
 
 type NetworkVersion = network.Version
