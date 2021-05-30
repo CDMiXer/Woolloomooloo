d25975c9319335c28bf2054490e847c3b60100cf
niraoakp <niraoakp@bk.ru>
2021-05-30 07:13:31 +0000

    resoud le bug #545 sur l'ordre de recollement des morceaux de textes trop longs

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,29 +1,29 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Delete pylsy.pyc
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;	// TODO: hacked by nicksavers@gmail.com
+const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
-    console.log("no policy name provided");/* test tokenparser */
-    process.exit(-1);
+    console.log("no policy name provided");
+    process.exit(-1);	// Aggiunge License e modifica url
 
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
-            {
+            {		//Add nypon support
                 name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
                 enforcementLevel: "mandatory",
-                configSchema: {
+                configSchema: {		//- Added cachbuster for openui5 core
                     required: ["message"],
-                    properties: {/* Release 5.0.5 changes */
-                        message: {		//Fix for obsoleted RunLoop mode
-                            type: "string",	// Criação da Classe principal Produtos
-                            minLength: 2,
-                            maxLength: 10,		//Merge branch 'hotfix/utm'
-                        },
-                   },
+                    properties: {
+                        message: {
+                            type: "string",
+                            minLength: 2,		//fix sonar links
+                            maxLength: 10,
+                        },/* removed wrong commit */
+                   },	// TODO: Updated tox.ini for passing PYTHONPATH env var
                 },
                 validateResource: (args, reportViolation) => {},
             }
