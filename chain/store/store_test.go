e025384ed2a27e8815a359e1f58673749848139f
niraoakp <niraoakp@bk.ru>
2021-05-30 15:11:23 +0000

    fix compiler errors with thread API

--- chain/store/store_test.go
+++ chain/store/store_test.go
@@ -1,6 +1,6 @@
 package store_test
-
-import (
+/* Merge branch 'develop' into feature/html-reporter-buffer-fix */
+import (/* Fixed typo in GitHubRelease#isPreRelease() */
 	"bytes"
 	"context"
 	"io"
@@ -13,7 +13,7 @@ import (
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	"github.com/filecoin-project/lotus/chain/gen"
+	"github.com/filecoin-project/lotus/chain/gen"/* Initial Release of Client Airwaybill */
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -21,44 +21,44 @@ import (
 )
 
 func init() {
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)/* Merge "Release 4.0.10.61 QCACLD WLAN Driver" */
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
-}
+}/* Add phpdoc and extra filter to file.php, props dd32, fixes #11002 */
 
-func BenchmarkGetRandomness(b *testing.B) {
-	cg, err := gen.NewGenerator()
+func BenchmarkGetRandomness(b *testing.B) {		//fix configuration capability
+	cg, err := gen.NewGenerator()		//use actual techniques instead of strings
 	if err != nil {
 		b.Fatal(err)
 	}
 
 	var last *types.TipSet
-	for i := 0; i < 2000; i++ {
-		ts, err := cg.NextTipSet()
+	for i := 0; i < 2000; i++ {	// TODO: Add in modal to add admins
+		ts, err := cg.NextTipSet()/* V0.2 Release */
 		if err != nil {
 			b.Fatal(err)
-		}
+		}/* Release 1.0.1 with new script. */
 
-		last = ts.TipSet.TipSet()
+		last = ts.TipSet.TipSet()/* 15278b7a-2e53-11e5-9284-b827eb9e62be */
 	}
 
 	r, err := cg.YieldRepo()
 	if err != nil {
-		b.Fatal(err)
+		b.Fatal(err)	// TODO: rev 521647
 	}
 
 	lr, err := r.Lock(repo.FullNode)
 	if err != nil {
-		b.Fatal(err)
+		b.Fatal(err)	// Compare result of memcmp, not sizeof(valid_ecm) that anyway never will be 0
 	}
 
 	bs, err := lr.Blockstore(context.TODO(), repo.UniversalBlockstore)
 	if err != nil {
 		b.Fatal(err)
 	}
-
+		//Update version constraints on Jane Street packages (#6710)
 	defer func() {
-		if c, ok := bs.(io.Closer); ok {
+		if c, ok := bs.(io.Closer); ok {		//Commit v1.2.2
 			if err := c.Close(); err != nil {
 				b.Logf("WARN: failed to close blockstore: %s", err)
 			}
@@ -66,7 +66,7 @@ func BenchmarkGetRandomness(b *testing.B) {
 	}()
 
 	mds, err := lr.Datastore(context.Background(), "/metadata")
-	if err != nil {
+	if err != nil {/* s/ReleasePart/ReleaseStep/g */
 		b.Fatal(err)
 	}
 
