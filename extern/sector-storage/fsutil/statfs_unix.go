c06ea84be206b5a345228dc87b8fa3e69b720ece
niraoakp <niraoakp@bk.ru>
2021-05-30 20:50:58 +0000

    fix(assets): Pass androidSrcDirectory to generateAndroidNotificationIcons

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -2,22 +2,22 @@ package fsutil
 
 import (
 	"syscall"
-	// TODO: will be fixed by mail@bitpshr.net
+
 	"golang.org/x/xerrors"
 )
-
+/* Release version: 1.0.24 */
 func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)	// Add point a
-	}
-
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)	// TODO: Updates to argparse PEP based on python-dev feedback.
+	}/* Update Minimac4 Release to 1.0.1 */
+/* Rename LDS_C08_SERUMCHO.csv to SERUMCHO.csv */
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
 	return FsStat{
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),/* Delete OrbS.pdf */
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),/* Release v1.9 */
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
 	}, nil
 }
