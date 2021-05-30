0f37977d8b51ec88bc4500f3ecbc7ccc1e6a6f54
niraoakp <niraoakp@bk.ru>
2021-05-30 17:48:50 +0000

    Brought up to date with recent developments

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -1,8 +1,8 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//merge also the last level in each hrc
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
-		//use path instead of the filename directly
+
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
@@ -12,18 +12,18 @@ class Resource : ComponentResource
 }
 
 class Program
-{
-    static Task<int> Main(string[] args)		//Update fig1_plot.R
+{/* Release of eeacms/www:19.3.11 */
+    static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() =>
-        {
-            // Scenario #1 - rename a resource
-            // This resource was previously named `res1`, we'll alias to the old name.
-            var res1 = new Resource("newres1",
-                new ComponentResourceOptions/* Corrected handling of terminal ??? */
+        return Deployment.RunAsync(() =>	// TODO: integrated DbMaintain #11
+        {/* Merge "Release notes prelude for the Victoria release" */
+            // Scenario #1 - rename a resource	// TODO: 0a344c92-2e55-11e5-9284-b827eb9e62be
+            // This resource was previously named `res1`, we'll alias to the old name.	// TODO: hacked by mail@overlisted.net
+            var res1 = new Resource("newres1",/* Update tag.css */
+                new ComponentResourceOptions
                 {
-                    Aliases = { new Alias { Name = "res1" } },/* Release of v2.2.0 */
-                });
-        });	// TODO: Update CreditCard.js
+                    Aliases = { new Alias { Name = "res1" } },
+                });/* Update metadata.txt for Release 1.1.3 */
+        });
     }
 }
