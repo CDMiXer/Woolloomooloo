ed3b109493f471e58a1aa3cf65e82b3281142cfd
niraoakp <niraoakp@bk.ru>
2021-05-30 10:32:55 +0000

    Merge "rally: Add explicit sla sections"

--- pkg/codegen/dotnet/gen_program_test.go
+++ pkg/codegen/dotnet/gen_program_test.go
@@ -1,12 +1,12 @@
 package dotnet
 
 import (
-	"bytes"/* Shaping CH16 first example */
+	"bytes"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
 	"testing"
-	// TODO: will be fixed by arajasek94@gmail.com
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
 
@@ -17,7 +17,7 @@ import (
 
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
-func TestGenProgram(t *testing.T) {	// Fix oxAuth SCIM endpoint authentication
+func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
@@ -25,38 +25,38 @@ func TestGenProgram(t *testing.T) {	// Fix oxAuth SCIM endpoint authentication
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
-			continue/* As good as its going to get! */
+			continue
 		}
-	// c3536335-2ead-11e5-b0eb-7831c1d44c14
+
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
 		}
 
 		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())/* Releasing 0.7 (Release: 0.7) */
+			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			expected, err := ioutil.ReadFile(path + ".cs")		//adds opf metadata
+			expected, err := ioutil.ReadFile(path + ".cs")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".cs", err)
 			}
 
-			parser := syntax.NewParser()/* Release 2.7.3 */
+			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
-			if err != nil {		//f75093e8-2e3f-11e5-9284-b827eb9e62be
+			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
 
-			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))/* Add copyright notices and fix docstrings. */
+			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
-			}		//fixed wrong quations in previous commit
+			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
@@ -65,17 +65,17 @@ func TestGenProgram(t *testing.T) {	// Fix oxAuth SCIM endpoint authentication
 			assert.NoError(t, err)
 
 			if expectNYIDiags {
-				var tmpDiags hcl.Diagnostics		//chase memory leak in keep alive looper
+				var tmpDiags hcl.Diagnostics
 				for _, d := range diags {
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
-				}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-				diags = tmpDiags/* Switch unit karma runner to PhantomJS */
+				}
+				diags = tmpDiags
 			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to generate program: %v", diags)
-			}	// Stick to robots.txt specs
+			}
 			assert.Equal(t, string(expected), string(files["MyStack.cs"]))
 		})
 	}
