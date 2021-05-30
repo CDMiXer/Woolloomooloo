94a0738810e8d4248535ee13e46634abfc1aa628
niraoakp <niraoakp@bk.ru>
2021-05-30 11:01:14 +0000

    Remove `__dirname` usage from `loadAddon`

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -1,51 +1,51 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Docs: update the known issues. */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: ref: Gettext.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* set Release mode */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release: Making ready for next release cycle 3.1.5 */
+
 package python
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 const (
 // intrinsicDataSource is the name of the data source intrinsic.
-//	intrinsicDataSource = "__dataSource"
+//	intrinsicDataSource = "__dataSource"/* Release camera stream when finished */
 )
 
 //// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified
-//// data source function with the given input properties.	// Add query methods to return true on the type of the os family
-//func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {/* Add RootySand for Cacti */
-//	return &il.BoundCall{/* Create rand.c */
+//// data source function with the given input properties.	// 0502b9da-2e6d-11e5-9284-b827eb9e62be
+//func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
+//	return &il.BoundCall{
 //		Func:     intrinsicDataSource,
-//		ExprType: il.TypeMap,/* Release for 2.20.0 */
+//		ExprType: il.TypeMap,	// TODO: Fix endless recursion in BranchStatement.hashCode()
 //		Args: []il.BoundExpr{
-//			&il.BoundLiteral{/* Release 1.7.0 */
+//			&il.BoundLiteral{
 //				ExprType: il.TypeString,
-//				Value:    functionName,
-//			},/* Release 2.4.2 */
-//			&il.BoundPropertyValue{
-//				NodeType: il.TypeMap,/* Release 1.0.36 */
+//				Value:    functionName,/* 7a9113a6-2e3e-11e5-9284-b827eb9e62be */
+//			},
+//			&il.BoundPropertyValue{	// TODO: manage API calls return Call.
+//				NodeType: il.TypeMap,
 //				Value:    inputs,
-//			},/* Release v4.5.1 */
-//			&il.BoundLiteral{	// TODO: Use absolute paths to stop making Windows freak out.
-//				ExprType: il.TypeString,		//Flip anchor points so the dropdown menus don't go offscreen.
+//			},
+//			&il.BoundLiteral{
+//				ExprType: il.TypeString,/* Refer to FORMLOADER db user instead of FORMBUILDER */
 //				Value:    optionsBag,
-//			},/* correct first heading */
+//			},/* chore(CI): Softer limits on virtual cards */
 //		},
 //	}
 //}
 //
 //// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
 //// a call to the data source intrinsic.
-//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
-//	contract.Assert(c.Func == intrinsicDataSource)
+//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {/* Release pubmedView */
+//	contract.Assert(c.Func == intrinsicDataSource)/* Create ReleaseInstructions.md */
 //	function = c.Args[0].(*il.BoundLiteral).Value.(string)
 //	inputs = c.Args[1].(*il.BoundPropertyValue).Value
 //	optionsBag = c.Args[2].(*il.BoundLiteral).Value.(string)
