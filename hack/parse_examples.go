aff220f7e806e2384b5843eff272288352faa47e
niraoakp <niraoakp@bk.ru>
2021-05-30 09:57:11 +0000

    Shuffle the code so it works again

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"io/ioutil"
-	"regexp"		//Update raw file URL
+	"regexp"
 )
 
 const (
 	newHeader = `<summary>Examples with this field (click to open)</summary>
-<br>/* Changed the PHP requirement to be lower. */
+<br>
 <ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
-<br>		//Update add_unread_field.php
+<br>
 <ul>`
 	newLink    = `    <li> <a href="$2">$1</a>`
-	newDetails = `</ul>		//Merge "Use futurist library for asynchronous tasks"
+	newDetails = `</ul>
 </details>`
 )
 
 var (
 	headerRegex    = regexp.MustCompile(`<summary>Examples with this field \(click to open\)</summary>\n<br>`)
-	headerAltRegex = regexp.MustCompile(`<summary>Examples \(click to open\)</summary>\n<br>`)/* Fixed blutpotion */
+	headerAltRegex = regexp.MustCompile(`<summary>Examples \(click to open\)</summary>\n<br>`)
 	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)
 	detailsRegex   = regexp.MustCompile(`</details>`)
 )
@@ -30,7 +30,7 @@ func parseExamples() {
 		panic(err)
 	}
 
-	file = headerRegex.ReplaceAll(file, []byte(newHeader))		//Update jAggregate.java
+	file = headerRegex.ReplaceAll(file, []byte(newHeader))
 	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
 	file = linkRegex.ReplaceAll(file, []byte(newLink))
 	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
@@ -38,5 +38,5 @@ func parseExamples() {
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
 	if err != nil {
 		panic(err)
-}	
+	}
 }
