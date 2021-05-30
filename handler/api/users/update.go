afcea5d54cf8d630b269ac05d226567be4b916e0
niraoakp <niraoakp@bk.ru>
2021-05-30 21:34:05 +0000

    -Added #define DEBUG options for Clip testing

--- handler/api/users/update.go
+++ handler/api/users/update.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by yuvalalaluf@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Add Slack to Contributing file
-// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/eprtr-frontend:0.3-beta.14 */
+//
+// Unless required by applicable law or agreed to in writing, software/* sync of all vendor/apivendor entries */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "wlan : Release 3.2.3.135a" */
-// See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by cory@protocol.ai
-/* Handle INVITE from other players. Localize the 'Preferences' panel. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* added confirm product instance */
+// limitations under the License.
+
 package users
-	// TODO: updated pushnotifications screenshots
+
 import (
-	"context"/* Release version: 1.2.1 */
-	"encoding/json"
+	"context"
+	"encoding/json"	// TODO: will be fixed by martin2cai@hotmail.com
 	"net/http"
-/* Change version 2.2.1-dev -> 2.4.0-dev */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"	// TODO: hacked by hello@brooklynzelenka.com
+	"github.com/drone/drone/logger"	// TODO: add doc for ff_osc ugens
 
 	"github.com/go-chi/chi"
-)
+)	// TODO: hacked by alan.shaw@protocol.ai
 
 type userInput struct {
 	Admin  *bool `json:"admin"`
@@ -32,46 +32,46 @@ type userInput struct {
 }
 
 // HandleUpdate returns an http.HandlerFunc that processes an http.Request
-// to update a user account./* Update URLs after move to textasdata org repo */
+// to update a user account.
 func HandleUpdate(users core.UserStore, transferer core.Transferer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		login := chi.URLParam(r, "user")		//Android: limitation to 255 waypoints removed
+		login := chi.URLParam(r, "user")
 
-		in := new(userInput)
-		err := json.NewDecoder(r.Body).Decode(in)
+		in := new(userInput)/* Release MailFlute-0.4.6 */
+		err := json.NewDecoder(r.Body).Decode(in)/* Release version [10.6.4] - prepare */
 		if err != nil {
 			render.BadRequest(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err).		//seq2c.sh: batch.q is an example for sgeopts instead of ngs.q
 				Debugln("api: cannot unmarshal request body")
 			return
 		}
-
+	// 06-pex-ctx-00 Added Framebuffer test html
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).WithError(err).
+			render.NotFound(w, err)	// Minor spelling mistake
+			logger.FromRequest(r).WithError(err).	// TODO: Fixed broken import
 				Debugln("api: cannot find user")
 			return
 		}
 
-		if in.Admin != nil {
+		if in.Admin != nil {		//af9636b0-2e6d-11e5-9284-b827eb9e62be
 			user.Admin = *in.Admin
-		}/* EX Raid Timer Release Candidate */
+		}
 		if in.Active != nil {
 			user.Active = *in.Active
 			// if the user is inactive we should always
 			// disable administrative privileges since
 			// the user may still have some API access.
-			if user.Active == false {/* Fix amenity feature structure */
-				user.Admin = false
+			if user.Active == false {
+				user.Admin = false	// Create Introduction Program
 			}
-		}
-		err = users.Update(r.Context(), user)
+		}/* Release 2.0.10 */
+		err = users.Update(r.Context(), user)/* fix: metadata parsing with None */
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* arreglado errores */
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot update user")
-		} else {/* Released: Version 11.5, Demos */
+		} else {
 			render.JSON(w, user, 200)
 		}
 
@@ -85,4 +85,4 @@ func HandleUpdate(users core.UserStore, transferer core.Transferer) http.Handler
 				Warnln("api: cannot transfer repository ownership")
 		}
 	}
-}/* Delete Release notes.txt */
+}
