9b2aecfc197d730397f8c8c2b8763d41cf659ebd
niraoakp <niraoakp@bk.ru>
2021-05-30 21:00:27 +0000

    Added support for large graphs

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -1,43 +1,43 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version 0.3. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Delete twodudes-big.png
+// +build !oss/* SDD-826 SDD-901 increase poll timeout to 60s */
 
 package config
 
-import (/* cors-config allow-origin is defined now by application.property file */
+import (
 	"errors"
-	"testing"/* Changing Release in Navbar Bottom to v0.6.5. */
+	"testing"		//e3e387de-2e67-11e5-9284-b827eb9e62be
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-
+/* Links open in a new tab now */
 	"github.com/golang/mock/gomock"
-)		//focntion insert into bd dans DButils
+)
 
-func TestMemoize(t *testing.T) {
-	controller := gomock.NewController(t)/* Updated to release 0.3.1 / 0.3.2-SNAPSHOT */
+func TestMemoize(t *testing.T) {		//Add support for non-case-sensitive paths
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
-	args := &core.ConfigArgs{/* Merge "Fix issue #11536426: Permission denied when trying to launch..." */
+	args := &core.ConfigArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
-		Repo:   &core.Repository{ID: 42},
+		Repo:   &core.Repository{ID: 42},/* CI shifted */
 		Config: conf,
-	}	// TODO: Remove huaway_status.sh and issue curl command directly.
+	}	// TODO: Tidy grammar and spelling
 
 	base := mock.NewMockConfigService(controller)
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
 	_, err := service.Find(noContext, args)
-	if err != nil {
-		t.Error(err)	// TODO: Merge "IBP: Add python-urllib3 to fuel-agent rpm spec"
+	if err != nil {	// TODO: Add filter to eclipse .project [ci skip]
+		t.Error(err)
 		return
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {
+	if got, want := service.cache.Len(), 1; got != want {/* Released version 1.0.1. */
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 
@@ -45,28 +45,28 @@ func TestMemoize(t *testing.T) {
 	res, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return/* Merge branch 'master' of https://github.com/11693-5/project-team5.git */
+		return		//Merge "Get rid of convoluted getContent failsafe"
 	}
-	if res != conf {	// 92991488-2e59-11e5-9284-b827eb9e62be
-		t.Errorf("Expect result from cache")
+	if res != conf {
+		t.Errorf("Expect result from cache")	// Added dimension of DB by tablespace.
 	}
-		//Merge "Increase the buffer length for Interface Ksync message."
-	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}/* Beta Release Version */
+	// 5c939622-2e6a-11e5-9284-b827eb9e62be
+	if got, want := service.cache.Len(), 1; got != want {		//Move DefineConstants="MONO" into OpenRA.Game.csproj.
+		t.Errorf("Expect %d items in cache, got %d", want, got)/* ReleaseNotes: mention basic debug info and ASan support in the Windows blurb */
+	}
 }
 
 func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{/* Release the reference to last element in takeUntil, add @since tag */
-		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
-		Repo:   &core.Repository{ID: 42},
+	args := &core.ConfigArgs{	// TODO: Content base router remainds are removed.
+		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},/* Release of eeacms/www:20.10.17 */
+		Repo:   &core.Repository{ID: 42},/* Delete UMSI course recommender-checkpoint.ipynb */
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
-	}		//Create 526. Beautiful Arrangement.cpp
+	}
 
-	base := mock.NewMockConfigService(controller)	// TODO: Refs #89516 - time recording
+	base := mock.NewMockConfigService(controller)
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
