537f4362e803d8e0be83c34ed4f1c681e6ae9e25
niraoakp <niraoakp@bk.ru>
2021-05-30 22:01:16 +0000

    3aaefe1e-2e4e-11e5-9284-b827eb9e62be

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Add User Manual - pdf version
-// Use of this source code is governed by the Drone Non-Commercial License/* Added driver menu name in config file */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Runtime type checking on return types of functions and operations */
-// +build !oss
+
+// +build !oss/* Initial renames. */
 
 package token
