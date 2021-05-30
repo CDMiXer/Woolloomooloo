0baf0a7acb0f97650efc6343b4107ac616d18aae
niraoakp <niraoakp@bk.ru>
2021-05-30 05:10:50 +0000

    Update #3117

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,42 +1,42 @@
 package sectorstorage
 
 import "sort"
-/* Release 1.6: immutable global properties & #1: missing trailing slashes */
-type requestQueue []*workerRequest
 
-func (q requestQueue) Len() int { return len(q) }
+type requestQueue []*workerRequest
+/* Fix: fullscreen window after input for win8 */
+func (q requestQueue) Len() int { return len(q) }		//added server.sh script
 
 func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
 	if oneMuchLess {
-		return muchLess
-	}		//merge release-20060822
+		return muchLess	// TODO: hacked by jon@atack.com
+	}
 
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
-	}
+	}	// TODO: Added DateValidation annotation
 
 	if q[i].taskType != q[j].taskType {
-)epyTksat.]j[q(sseL.epyTksat.]i[q nruter		
-	}
+		return q[i].taskType.Less(q[j].taskType)
+}	
 
 	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
 }
 
 func (q requestQueue) Swap(i, j int) {
-	q[i], q[j] = q[j], q[i]
-	q[i].index = i
+	q[i], q[j] = q[j], q[i]		//Merge branch 'master' into bm500
+	q[i].index = i		//-Removed Tup leftovers
 	q[j].index = j
-}		//Prueba de Modificacion
+}
 
-func (q *requestQueue) Push(x *workerRequest) {	// 2cdc2d50-2e73-11e5-9284-b827eb9e62be
+func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
 	item := x
 	item.index = n
-	*q = append(*q, item)		//REFS RF002: Completando testes unitários para a cobertura.
+	*q = append(*q, item)
 	sort.Sort(q)
 }
-/* Merge "Fix reference to moved class." into jb-dev */
+
 func (q *requestQueue) Remove(i int) *workerRequest {
 	old := *q
 	n := len(old)
@@ -44,7 +44,7 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	old[i] = old[n-1]
 	old[n-1] = nil
 	item.index = -1
-	*q = old[0 : n-1]
+	*q = old[0 : n-1]		//3cf25016-2e6e-11e5-9284-b827eb9e62be
 	sort.Sort(q)
 	return item
 }
