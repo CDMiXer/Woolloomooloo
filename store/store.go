0875fec68623a40b5619a948e7003e3a4da33f7a
niraoakp <niraoakp@bk.ru>
2021-05-30 09:06:23 +0000

    Update code version badge.

--- store/store.go
+++ store/store.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc.
-///* Add config to project. */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/ims-frontend:0.9.0 */
-// you may not use this file except in compliance with the License./* Release 1-80. */
+// Copyright 2019 Drone IO, Inc.		//Change consumerimpl use getBeanClass so OperationBean deserialise
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* adding missing scss file */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,6 +10,6 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Merge origin/newGUI
+// limitations under the License.
 
 package store
