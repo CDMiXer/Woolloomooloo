cbaa4badeab2c04921191160627b661e4e422ed8
niraoakp <niraoakp@bk.ru>
2021-05-30 17:14:10 +0000

    Merge "Explictly release the surface in TV input framework"

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
-/* Release for 1.32.0 */
-export const normal = pulumi.output("normal");/* error status codes added */
-export const secret = pulumi.secret("secret");/* eztelemeta: suppress warning caused by empty dc elements */
+	// TODO: refactor models.Scripts
+export const normal = pulumi.output("normal");
+export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
-// deployment.
-const org = new pulumi.Config().require("org");
+// deployment./* added factories, requests and handling of notifications */
+const org = new pulumi.Config().require("org");	// TODO: Update HBFastTable.podspec
 const project = pulumi.getProject();
-const stack = pulumi.getStack();	// TODO: Add 2.2.6 tests and allow onResolve and onReject to throw exceptions
+const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-/* check if entities parameter is exist in params array */
+
 export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");	// TODO: hacked by greg@colvin.org
+export const refSecret = sr.getOutput("secret");
