ae7ce44d7f78f58ca966063a7317b9455565461a
niraoakp <niraoakp@bk.ru>
2021-05-30 13:32:22 +0000

    rev 767432

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Create ontology-description.yaml
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Added Fullcontact API
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Added additional info about project and purpose
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package registry
-/* Release v0.2.7 */
+
 import "github.com/drone/drone/core"
 
 // EndpointSource returns a no-op registry credential provider.
-func EndpointSource(string, string, bool) core.RegistryService {	// ADDED: Readme - Post-processing.
-	return new(noop)/* Merge "Release 3.2.3.425 Prima WLAN Driver" */
-}		//Merge "Log extlink action when appropriate"
+func EndpointSource(string, string, bool) core.RegistryService {
+	return new(noop)
+}
