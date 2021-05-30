7499a222dfeb7666cc46f05991d2975db7f223fc
niraoakp <niraoakp@bk.ru>
2021-05-30 11:56:55 +0000

    Updating build-info/dotnet/roslyn/dev16.7 for 1.20222.2

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2020, Pulumi Corporation.  All rights reserved./* Release version: 0.5.4 */
+# Copyright 2020, Pulumi Corporation.  All rights reserved.
+/* Release of eeacms/bise-frontend:1.29.3 */
+import pulumi/* Update test-runner.html */
 
-import pulumi
-
-config = pulumi.Config()/* Merge "Fix cinder-manage volume delete" */
+config = pulumi.Config()	// TODO: Merge "Recover from bad input event timestamps from the kernel." into jb-mr1-dev
 org = config.require('org')
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"	// TODO: will be fixed by arachnid@notdot.net
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
-/* Moved BigDecimal conversion to assignment level */
+
 oldVal = a.get_output('val')
 
 if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
     raise Exception('Invalid result')
-
+/* rev 604220 */
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
