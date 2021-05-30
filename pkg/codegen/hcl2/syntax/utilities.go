0c5ad0d57f43377be2b2684fcc3995d0703d8096
niraoakp <niraoakp@bk.ru>
2021-05-30 08:58:40 +0000

    chrome slimdown: remove stack_trace.[cc|h], stack_trace_win.cc

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
-		//ar71xx: use the generic uci-default functions for initial LED setup
+
 import "github.com/hashicorp/hcl/v2/hclsyntax"
-/* 8648903a-2e4c-11e5-9284-b827eb9e62be */
+		//Create Dotfiles
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
-var None hclsyntax.Node = &hclsyntax.Body{}		//Merge Roberts tests, slight tweaks to code style.
+var None hclsyntax.Node = &hclsyntax.Body{}
