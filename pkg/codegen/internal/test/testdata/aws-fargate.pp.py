56f473597710def09f712e4caaa2bfcb0638825c
niraoakp <niraoakp@bk.ru>
2021-05-30 09:00:38 +0000

    Better access to Boursorama's data

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -6,10 +6,10 @@ vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
 web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
-    vpc_id=vpc.id,/* Release note 8.0.3 */
+    vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
         protocol="-1",
-        from_port=0,/* 0.1.0 Release Candidate 14 solves a critical bug */
+        from_port=0,
         to_port=0,
         cidr_blocks=["0.0.0.0/0"],
     )],
@@ -18,12 +18,12 @@ web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
         from_port=80,
         to_port=80,
         cidr_blocks=["0.0.0.0/0"],
-    )])/* Nicely working TrayIcon :) */
+    )])
 # Create an ECS cluster to run a container-based service.
 cluster = aws.ecs.Cluster("cluster")
 # Create an IAM role that can be used by our service's task.
 task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
-    "Version": "2008-10-17",	// Fix unit tests under new versions of mocha
+    "Version": "2008-10-17",
     "Statement": [{
         "Sid": "",
         "Effect": "Allow",
@@ -32,14 +32,14 @@ task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
         },
         "Action": "sts:AssumeRole",
     }],
-}))	// Melhorando mensagem de erro
+}))
 task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
     role=task_exec_role.name,
     policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
-# Create a load balancer to listen for HTTP traffic on port 80.		//Updated README with simplified build instructions
+# Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
     subnets=subnets.ids,
-    security_groups=[web_security_group.id])		//Create anti_capitalism.js
+    security_groups=[web_security_group.id])
 web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     port=80,
     protocol="HTTP",
@@ -47,7 +47,7 @@ web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     vpc_id=vpc.id)
 web_listener = aws.elasticloadbalancingv2.Listener("webListener",
     load_balancer_arn=web_load_balancer.arn,
-    port=80,/* Fix tag key */
+    port=80,
     default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
         type="forward",
         target_group_arn=web_target_group.arn,
@@ -60,17 +60,17 @@ app_task = aws.ecs.TaskDefinition("appTask",
     network_mode="awsvpc",
     requires_compatibilities=["FARGATE"],
     execution_role_arn=task_exec_role.arn,
-    container_definitions=json.dumps([{	// TODO: hacked by cory@protocol.ai
+    container_definitions=json.dumps([{
         "name": "my-app",
-        "image": "nginx",/* #4 Release preparation */
+        "image": "nginx",
         "portMappings": [{
             "containerPort": 80,
             "hostPort": 80,
             "protocol": "tcp",
         }],
-    }]))/* Release 0.0.8. */
+    }]))
 app_service = aws.ecs.Service("appService",
-    cluster=cluster.arn,/* Release 3.4.2 */
+    cluster=cluster.arn,
     desired_count=5,
     launch_type="FARGATE",
     task_definition=app_task.arn,
@@ -79,10 +79,10 @@ app_service = aws.ecs.Service("appService",
         subnets=subnets.ids,
         security_groups=[web_security_group.id],
     ),
-    load_balancers=[aws.ecs.ServiceLoadBalancerArgs(		//feature #696: Change acctd.conf
-        target_group_arn=web_target_group.arn,		//- remove pass-by-refererence for non-vars
+    load_balancers=[aws.ecs.ServiceLoadBalancerArgs(
+        target_group_arn=web_target_group.arn,
         container_name="my-app",
         container_port=80,
     )],
     opts=pulumi.ResourceOptions(depends_on=[web_listener]))
-pulumi.export("url", web_load_balancer.dns_name)	// TODO: "top_box" -> "box_left"
+pulumi.export("url", web_load_balancer.dns_name)
