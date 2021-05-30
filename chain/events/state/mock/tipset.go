2a9b5a25b16590a58d27ab3aa959c2a1c05907e6
niraoakp <niraoakp@bk.ru>
2021-05-30 00:12:31 +0000

    Switch AsyncSeq to use the Async base class

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,27 +1,27 @@
-package test	// Rename ace-voip to ace-voip.md
+package test
 
-import (/* Update NEWS and README.txt for latest changes. */
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
+import (
+	"github.com/filecoin-project/go-address"	// TODO: docs: update README with details about deprecation
+	"github.com/filecoin-project/go-state-types/crypto"/* 7f618a84-2e5a-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
 
-var dummyCid cid.Cid/* Fix a loop if silliness */
+var dummyCid cid.Cid
 
 func init() {
 	dummyCid, _ = cid.Parse("bafkqaaa")
-}/* 250:  misfunction of Tab key  (Reset key states after executing action) */
+}
 
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {/* Merge "Remove keystone/common/cache/_memcache_pool.py" */
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
-		Miner:                 minerAddr,
+		Miner:                 minerAddr,/* updated default file name to gz */
 		Height:                5,
-		ParentStateRoot:       dummyCid,
-		Messages:              dummyCid,		//Update marshal tests for expect
+		ParentStateRoot:       dummyCid,/* Update test_server.c */
+		Messages:              dummyCid,	// TODO: will be fixed by juan@benet.ai
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
-	}})		//certdb: add missing `DESC` to get tail, not head
+	}})
 }
