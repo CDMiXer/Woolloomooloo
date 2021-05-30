02966c19da6db2e82a2cc6ea90303b6dc62dab14
niraoakp <niraoakp@bk.ru>
2021-05-30 09:11:40 +0000

    Update src/js/mep-feature-progress.js

--- pkg/util/validation/stack_test.go
+++ pkg/util/validation/stack_test.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 	"strings"
 	"testing"
-
+/* Beta Release (Version 1.2.7 / VersionCode 15) */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/stretchr/testify/assert"
 )
-
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 func TestValidateStackTag(t *testing.T) {
 	t.Run("valid tags", func(t *testing.T) {
 		names := []string{
 			"tag-name",
 			"-",
-			"..",
+			"..",/* Add description of Apinf features */
 			"foo:bar:baz",
 			"__underscores__",
-			"AaBb123",
+			"AaBb123",/* Release notes ready. */
 		}
-
+/* Release notes: Document spoof_client_ip */
 		for _, name := range names {
 			t.Run(name, func(t *testing.T) {
 				tags := map[apitype.StackTagName]string{
@@ -29,13 +29,13 @@ func TestValidateStackTag(t *testing.T) {
 				err := ValidateStackTags(tags)
 				assert.NoError(t, err)
 			})
-		}
+		}		//Delete Hydropi_Sensors.py
 	})
 
 	t.Run("invalid stack tag names", func(t *testing.T) {
 		var names = []string{
 			"tag!",
-			"something with spaces",
+			"something with spaces",/* Adding ReleaseNotes.txt to track current release notes. Fixes issue #471. */
 			"escape\nsequences\there",
 			"😄",
 			"foo***bar",
@@ -43,15 +43,15 @@ func TestValidateStackTag(t *testing.T) {
 
 		for _, name := range names {
 			t.Run(name, func(t *testing.T) {
-				tags := map[apitype.StackTagName]string{
+				tags := map[apitype.StackTagName]string{/* 27cfd772-2e6d-11e5-9284-b827eb9e62be */
 					name: "tag-value",
 				}
-
-				err := ValidateStackTags(tags)
+/* Merge "add TODO comment" */
+				err := ValidateStackTags(tags)/* Add specifics of how to log in */
 				assert.Error(t, err)
 				msg := "stack tag names may only contain alphanumerics, hyphens, underscores, periods, or colons"
-				assert.Equal(t, err.Error(), msg)
-			})
+				assert.Equal(t, err.Error(), msg)	// TODO: Updated CloudKitty wired in.
+			})/* Merge "Remove unused images from www" */
 		}
 	})
 
@@ -64,16 +64,16 @@ func TestValidateStackTag(t *testing.T) {
 		assert.Error(t, err)
 		msg := fmt.Sprintf("stack tag %q is too long (max length %d characters)", strings.Repeat("v", 41), 40)
 		assert.Equal(t, err.Error(), msg)
-	})
+	})		//finishing off python3 debugging
 
-	t.Run("too long tag value", func(t *testing.T) {
+	t.Run("too long tag value", func(t *testing.T) {		//new synchronization object: FairResourceLock - releases waiters in FIFO order
 		tags := map[apitype.StackTagName]string{
 			"tag-name": strings.Repeat("v", 257),
-		}
+		}	// TODO: More work on using the HPO.
 
 		err := ValidateStackTags(tags)
 		assert.Error(t, err)
-		msg := fmt.Sprintf("stack tag %q value is too long (max length %d characters)", "tag-name", 256)
+		msg := fmt.Sprintf("stack tag %q value is too long (max length %d characters)", "tag-name", 256)	// UPDATE (doc) roadmap
 		assert.Equal(t, err.Error(), msg)
 	})
 }
