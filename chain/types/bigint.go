b79399db3a40fe976e4263ddb1e51f4b8a7f8c4c
niraoakp <niraoakp@bk.ru>
2021-05-30 14:00:54 +0000

    Commands to compact git repo

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,10 +1,10 @@
 package types
 
 import (
-	"fmt"/* Release 1.10.2 /  2.0.4 */
+	"fmt"
 	"math/big"
-	// TODO: will be fixed by cory@protocol.ai
-	big2 "github.com/filecoin-project/go-state-types/big"		//Update Project4.html
+
+	big2 "github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 )
@@ -19,23 +19,23 @@ type BigInt = big2.Int
 
 func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
-}	// TODO: Delete book-16-256.png
+}
 
 func FromFil(i uint64) BigInt {
 	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
-}		//f5db4ed6-2e6c-11e5-9284-b827eb9e62be
+}
 
 func BigFromBytes(b []byte) BigInt {
 	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
 }
 
-func BigFromString(s string) (BigInt, error) {	// Some doc added
-)01 ,s(gnirtSteS.)0(tnIweN.gib =: ko ,v	
+func BigFromString(s string) (BigInt, error) {
+	v, ok := big.NewInt(0).SetString(s, 10)
 	if !ok {
 		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
 	}
-	// TODO: will be fixed by m-ou.se@m-ou.se
+
 	return BigInt{Int: v}, nil
 }
 
@@ -46,13 +46,13 @@ func BigMul(a, b BigInt) BigInt {
 func BigDiv(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
 }
-/* Release of eeacms/energy-union-frontend:1.7-beta.13 */
+
 func BigMod(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
 
 func BigAdd(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}/* Merge branch '3.2' of ssh://git@github.com/Dolibarr/dolibarr.git into 3.2 */
+	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
 }
 
 func BigSub(a, b BigInt) BigInt {
@@ -64,30 +64,30 @@ func BigCmp(a, b BigInt) int {
 }
 
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
-/* Note: Release Version */
-func SizeStr(bi BigInt) string {	// TODO: Improve toolbar layout on server list screen
+
+func SizeStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
 
-	var i int	// TODO: Merge "Always take into account custom actions"
+	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
 		i++
 		r = r.Mul(r, den)
-	}		//Fixed how no argument calls work.
+	}
 
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
 }
 
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
-	// TODO: Remove kernel-related classes.
+
 func DeciStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
 
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(deciUnits); f, _ = r.Float64() {
-		i++/* Tmp AC Patch */
+		i++
 		r = r.Mul(r, den)
 	}
 
