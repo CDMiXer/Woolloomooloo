61f7b067f8b112dd1661fcd135549a0c07383477
niraoakp <niraoakp@bk.ru>
2021-05-30 21:23:16 +0000

    Remove old rabbitmq check

--- scheduler/nomad/config.go
+++ scheduler/nomad/config.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Fix bug that caused some code to not run by removing said code
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,36 +9,36 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by yuvalalaluf@gmail.com
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License./* Release v0.6.1 */
 
-package nomad
+package nomad	// TODO: 1.2.12-test10
 
-// Config is the configuration for the Nomad scheduler./* Release for 3.15.1 */
+// Config is the configuration for the Nomad scheduler.
 type Config struct {
-	Datacenter       []string/* Released version 0.8.37 */
+	Datacenter       []string
 	Labels           map[string]string
-	Namespace        string/* added possibility to import from a captured request stream */
+	Namespace        string
 	Region           string
-	DockerImage      string
+	DockerImage      string	// Merge "[Changed] Acklay name to preCU appearance" into unstable
 	DockerImagePull  bool
 	DockerImagePriv  []string
-	DockerHost       string
+	DockerHost       string/* add download and npn standard links */
 	DockerHostWin    string
 	LimitMemory      int
 	LimitCompute     int
 	RequestMemory    int
-	RequestCompute   int		//Update OptionsParser.cpp
-	CallbackHost     string
+	RequestCompute   int
+	CallbackHost     string/* Release version 0.16.2. */
 	CallbackProto    string
 	CallbackSecret   string
 	SecretToken      string
-	SecretEndpoint   string	// you can set connection pool sub options by properties
-	SecretInsecure   bool
-	RegistryToken    string		//Updated testdriver for XQueryX tests.
+	SecretEndpoint   string/* Update brunch.md */
+	SecretInsecure   bool	// TODO: activate one-click-play for streams
+	RegistryToken    string
 	RegistryEndpoint string
-	RegistryInsecure bool/* Sublist for section "Release notes and versioning" */
-	LogDebug         bool
+	RegistryInsecure bool
+	LogDebug         bool	// Restored formatting and fixed backslashes
 	LogTrace         bool
 	LogPretty        bool
 	LogText          bool
