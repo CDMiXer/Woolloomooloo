4255f4348984acba92f4a1666b33dd1a814a2c35
niraoakp <niraoakp@bk.ru>
2021-05-30 01:27:33 +0000

    New translations 03_p01_ch06_02.md (Turkish)

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,40 +1,40 @@
 package full
-
+		//avoid memory leaks in test code
 import (
-	"testing"
+	"testing"	// -Fixed README.md
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* optimized warps scripts */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
-		{big.NewInt(5), build.BlockGasTarget},
+		{big.NewInt(5), build.BlockGasTarget},	// TODO: use map_meta_cap for multisite superadmins, props dd32, fixes #12109
 	}, 1))
-
+/* [artifactory-release] Release version 1.0.0.RC2 */
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},	// TODO: removed problematic recent pubs parameter
-		{big.NewInt(20), build.BlockGasTarget / 2},	// regenerate the NBPCG modelled code; update test to include getMany test;
-	}, 1))
-
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
+	}, 1))
+		//Add full stops at the end of paragraphs in the readme
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
+		{big.NewInt(10), build.BlockGasTarget / 2},/* fix a few defaults for aniso_magic_nb, #424 */
+		{big.NewInt(20), build.BlockGasTarget / 2},	// TODO: will be fixed by yuvalalaluf@gmail.com
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},		//Merge "Remove `Content-Type` from GET Request's Header"
+		{big.NewInt(10), build.BlockGasTarget / 2},	// fix itemmeta link on kit section
+		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 2))/* Release version 3.2.2 of TvTunes and 0.0.7 of VideoExtras */
+	}, 2))
 }
