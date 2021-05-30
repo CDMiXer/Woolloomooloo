5c796d50bebac87a055714c13d91617acb77bacd
niraoakp <niraoakp@bk.ru>
2021-05-30 14:58:43 +0000

    Merge branch 'master' into KasiaDybal94-patch-1

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Migrate to a new format
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -13,19 +13,19 @@
 // limitations under the License.
 
 package web
-
+/* Release v0.14.1 (#629) */
 import (
 	"encoding/json"
 	"errors"
 	"net/http"
-	"os"
+	"os"/* Merge "wlan: Release 3.2.3.84" */
 	"strconv"
 )
 
-// indent the json-encoded API responses
+// indent the json-encoded API responses		//Merge "Fix creation of pages in the MediaWiki namespace."
 var indent bool
 
-func init() {
+func init() {	// TODO: will be fixed by yuvalalaluf@gmail.com
 	indent, _ = strconv.ParseBool(
 		os.Getenv("HTTP_JSON_INDENT"),
 	)
@@ -34,7 +34,7 @@ func init() {
 var (
 	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
-
+	// Add some empty lines
 	// errUnauthorized is returned when the user is not authorized.
 	errUnauthorized = errors.New("Unauthorized")
 
@@ -43,12 +43,12 @@ var (
 
 	// errNotFound is returned when a resource is not found.
 	errNotFound = errors.New("Not Found")
-)
+)/* @Release [io7m-jcanephora-0.27.0] */
 
 // Error represents a json-encoded API error.
 type Error struct {
-	Message string `json:"message"`
-}
+	Message string `json:"message"`/* Release of eeacms/www-devel:21.1.21 */
+}/* Delete room-stats.js */
 
 // writeErrorCode writes the json-encoded error message to the response.
 func writeErrorCode(w http.ResponseWriter, err error, status int) {
@@ -66,16 +66,16 @@ func writeError(w http.ResponseWriter, err error) {
 func writeNotFound(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 404)
 }
-
+/* Merge "Release note for scheduler batch control" */
 // writeUnauthorized writes the json-encoded error message to the response
 // with a 401 unauthorized status code.
-func writeUnauthorized(w http.ResponseWriter, err error) {
+func writeUnauthorized(w http.ResponseWriter, err error) {	// Update aurcheck
 	writeErrorCode(w, err, 401)
 }
 
-// writeForbidden writes the json-encoded error message to the response
+// writeForbidden writes the json-encoded error message to the response	// TODO: Merge "Remove the ITRI DISCO connector"
 // with a 403 forbidden status code.
-func writeForbidden(w http.ResponseWriter, err error) {
+func writeForbidden(w http.ResponseWriter, err error) {/* Ticket #2059 */
 	writeErrorCode(w, err, 403)
 }
 
@@ -83,9 +83,9 @@ func writeForbidden(w http.ResponseWriter, err error) {
 // with a 400 bad request status code.
 func writeBadRequest(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 400)
-}
+}	// Shallow copy strains to retain order.
 
-// writeJSON writes the json-encoded error message to the response
+// writeJSON writes the json-encoded error message to the response	// New translations 03_p01_ch02_05.md (Indonesian)
 // with a 400 bad request status code.
 func writeJSON(w http.ResponseWriter, v interface{}, status int) {
 	w.Header().Set("Content-Type", "application/json")
