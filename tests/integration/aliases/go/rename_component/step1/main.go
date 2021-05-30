50f89210d34251db3830410b8cf15cd36617c87d
niraoakp <niraoakp@bk.ru>
2021-05-30 03:38:25 +0000

    added ampache.conf

--- tests/integration/aliases/go/rename_component/step1/main.go
+++ tests/integration/aliases/go/rename_component/step1/main.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-/* Change URL to www */
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// TODO: remove coveralls badge at least until the 0% coverage bug is fixed.
+
 // FooComponent is a component resource
-type FooResource struct {/* Release on Monday */
+type FooResource struct {
 	pulumi.ResourceState
-}/* Release: Updated latest.json */
+}
 
 type FooComponent struct {
 	pulumi.ResourceState
 }
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}	// TODO: hacked by ac0dem0nk3y@gmail.com
+	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return fooRes, nil
-}/* Load Google Maps asynchronously */
-/* Merge "Set instance host field after resource claim" */
+}
+
 // Scenario #3 - rename a component (and all it's children)
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
@@ -31,8 +31,8 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	if err != nil {
 		return nil, err
 	}
-	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit/* Release of eeacms/redmine:4.1-1.6 */
-	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// TODO: hacked by 13860583249@yeah.net
+	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
 	if err != nil {
@@ -40,18 +40,18 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	}
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
-		return nil, err/* Made Size-retrieval from IntRect and Rect work the same way. */
+		return nil, err
 	}
 	return fooComp, nil
 }
 
-func main() {/* Allow reinforcement mode with a group. */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp3")
 		if err != nil {
 			return err
 		}
-		//Update soal.txt
+
 		return nil
 	})
 }
