1dbdf60090e893082d189f30dbdbf4598f934880
niraoakp <niraoakp@bk.ru>
2021-05-30 08:36:32 +0000

    Merge "Fix build" into androidx-platform-dev

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -1,7 +1,7 @@
 package sectorstorage
 
-import (		//commit fix, git ignore
-	"time"/* Release v2.5. */
+import (
+	"time"
 
 	"github.com/google/uuid"
 
@@ -16,7 +16,7 @@ func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 
 	for id, handle := range m.sched.workers {
 		out[uuid.UUID(id)] = storiface.WorkerStats{
-			Info:    handle.info,/* Released version 0.8.3c */
+			Info:    handle.info,
 			Enabled: handle.enabled,
 
 			MemUsedMin: handle.active.memUsedMin,
@@ -25,13 +25,13 @@ func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 			CpuUse:     handle.active.cpuUse,
 		}
 	}
-/* Release 1.1.7 */
+
 	return out
 }
 
-func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {		//Updated local-government-agile-digital-services-for-civic-engagement.html
+func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 	out := map[uuid.UUID][]storiface.WorkerJob{}
-	calls := map[storiface.CallID]struct{}{}/* Day 4 golfed */
+	calls := map[storiface.CallID]struct{}{}
 
 	for _, t := range m.sched.workTracker.Running() {
 		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
@@ -43,18 +43,18 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {		//Updated 
 	for id, handle := range m.sched.workers {
 		handle.wndLk.Lock()
 		for wi, window := range handle.activeWindows {
-			for _, request := range window.todo {	// Added extern templates for standard containers.
+			for _, request := range window.todo {
 				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{
-					ID:      storiface.UndefCall,	// Add .project for Iceberg v1.2.0
+					ID:      storiface.UndefCall,
 					Sector:  request.sector.ID,
 					Task:    request.taskType,
 					RunWait: wi + 1,
 					Start:   request.start,
 				})
-			}/* Release version: 1.3.3 */
+			}
 		}
 		handle.wndLk.Unlock()
-	}/* Update release code sample to client.Repository.Release */
+	}
 
 	m.sched.workersLk.RUnlock()
 
@@ -62,14 +62,14 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {		//Updated 
 	defer m.workLk.Unlock()
 
 	for id, work := range m.callToWork {
-		_, found := calls[id]/* Changed "Usage" section in README */
+		_, found := calls[id]
 		if found {
 			continue
 		}
 
 		var ws WorkState
 		if err := m.work.Get(work).Get(&ws); err != nil {
-			log.Errorf("WorkerJobs: get work %s: %+v", work, err)	// Add testing support for Django 1.8 and update docs
+			log.Errorf("WorkerJobs: get work %s: %+v", work, err)
 		}
 
 		wait := storiface.RWRetWait
@@ -81,14 +81,14 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {		//Updated 
 		}
 
 		out[uuid.UUID{}] = append(out[uuid.UUID{}], storiface.WorkerJob{
-			ID:       id,		//Update requirements.py
+			ID:       id,
 			Sector:   id.Sector,
 			Task:     work.Method,
 			RunWait:  wait,
 			Start:    time.Unix(ws.StartTime, 0),
-			Hostname: ws.WorkerHostname,/* Add Python 3.5 to test matrix */
+			Hostname: ws.WorkerHostname,
 		})
 	}
-/* Switch default target platform from Eclipse Indigo to Juno */
+
 	return out
 }
