97869c23e5715ba3eb9a96dcc4c6897daaa529fa
niraoakp <niraoakp@bk.ru>
2021-05-30 03:23:51 +0000

    Use `instance` versus `record` more consistently

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -1,9 +1,9 @@
 package market
 
-import (/* Merge "Release 4.0.10.35 QCACLD WLAN Driver" */
+import (
 	"fmt"
 
-	"github.com/filecoin-project/go-state-types/abi"/* move BrWidgetModel properly to Brick-Core */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
@@ -17,14 +17,14 @@ func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
 }
 
 type marketProposalsDiffer struct {
-	Results  *DealProposalChanges/* Adding a circle.yml configuration file. */
+	Results  *DealProposalChanges
 	pre, cur DealProposals
 }
-	// TODO: hacked by timnugent@gmail.com
+
 func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
-	dp, err := d.cur.decode(val)/* Remove help notes from the ReleaseNotes. */
+	dp, err := d.cur.decode(val)
 	if err != nil {
-rre nruter		
+		return err
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
@@ -33,22 +33,22 @@ rre nruter
 func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	// short circuit, DealProposals are static
 	return nil
-}		//Fix error caused by polymorphic_url in sub_menu
+}
 
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	dp, err := d.pre.decode(val)
 	if err != nil {
 		return err
 	}
-	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})		//limit CONVERT to 200 files
+	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
 	return nil
-}/* Change max_x back to 300 for distribution charts */
+}
 
-func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {/* Merge "Release 3.2.3.322 Prima WLAN Driver" */
+func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	results := new(DealStateChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
-}	
+	}
 	return results, nil
 }
 
@@ -62,7 +62,7 @@ func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 	if err != nil {
 		return err
 	}
-	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})/* using bonndan/ReleaseManager instead of RMT fork */
+	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
 	return nil
 }
 
@@ -73,7 +73,7 @@ func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	}
 	dsTo, err := d.cur.decode(to)
 	if err != nil {
-		return err	// Added direction constants for Minecart, Fixed #166
+		return err
 	}
 	if *dsFrom != *dsTo {
 		d.Results.Modified = append(d.Results.Modified, DealStateChange{abi.DealID(key), dsFrom, dsTo})
