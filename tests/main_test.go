5092a653afda7af95e73752d62c5075dcabd9a2f
niraoakp <niraoakp@bk.ru>
2021-05-30 12:27:19 +0000

    Update UpdateSafariExtension.sh

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Note that Xiang Zhang received commit privileges
+
 package tests
-/* Changed Proposed Release Date on wiki to mid May. */
+
 import (
 	"fmt"
 	"os"
-	"testing"		//I needed this schema salad version
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 )
@@ -16,8 +16,8 @@ func TestMain(m *testing.M) {
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
 		os.Exit(1)
-	}	// TODO: will be fixed by vyzo@hackzen.org
+	}
 
-	code := m.Run()		//switched to roxygen2 documentation and devtools build cycle
+	code := m.Run()
 	os.Exit(code)
 }
