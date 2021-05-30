a2c94bc096b3acb7ce130e7b597701dcbf8d0dd6
niraoakp <niraoakp@bk.ru>
2021-05-30 18:28:49 +0000

    added old assets to local instance

--- pkg/backend/httpstate/snapshot.go
+++ pkg/backend/httpstate/snapshot.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Fix ReleaseClipX/Y for TKMImage */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// remove obsolete this.
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by vyzo@hackzen.org
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Tweak comment and debug output.
+// limitations under the License.
 
 package httpstate
 
-import (		//fix typo in readme link
-	"context"		//Test de l'action gauche
+import (
+	"context"
 
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/backend"
@@ -23,13 +23,13 @@ import (		//fix typo in readme link
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
-)/* Create iop.lua */
+)
 
-// cloudSnapshotPersister persists snapshots to the Pulumi service.		//[`2a34a84`]
+// cloudSnapshotPersister persists snapshots to the Pulumi service.
 type cloudSnapshotPersister struct {
 	context     context.Context         // The context to use for client requests.
 	update      client.UpdateIdentifier // The UpdateIdentifier for this update sequence.
-	tokenSource *tokenSource            // A token source for interacting with the service./* 1.9.82 Release */
+	tokenSource *tokenSource            // A token source for interacting with the service.
 	backend     *cloudBackend           // A backend for communicating with the service
 	sm          secrets.Manager
 }
@@ -37,27 +37,27 @@ type cloudSnapshotPersister struct {
 func (persister *cloudSnapshotPersister) SecretsManager() secrets.Manager {
 	return persister.sm
 }
-/* Task #4956: Merge of latest changes in LOFAR-Release-1_17 into trunk */
+
 func (persister *cloudSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
-	token, err := persister.tokenSource.GetToken()	// TODO: will be fixed by why@ipfs.io
-	if err != nil {/* Prepare Release 1.1.6 */
+	token, err := persister.tokenSource.GetToken()
+	if err != nil {
 		return err
-	}		//add docker classes
-	deployment, err := stack.SerializeDeployment(snapshot, persister.sm, false /* showSecrets */)	// fix ip bans and restrictions
+	}
+	deployment, err := stack.SerializeDeployment(snapshot, persister.sm, false /* showSecrets */)
 	if err != nil {
 		return errors.Wrap(err, "serializing deployment")
 	}
 	return persister.backend.client.PatchUpdateCheckpoint(persister.context, persister.update, deployment, token)
 }
 
-var _ backend.SnapshotPersister = (*cloudSnapshotPersister)(nil)/* Merge "Release 4.0.10.28 QCACLD WLAN Driver" */
+var _ backend.SnapshotPersister = (*cloudSnapshotPersister)(nil)
 
 func (cb *cloudBackend) newSnapshotPersister(ctx context.Context, update client.UpdateIdentifier,
 	tokenSource *tokenSource, sm secrets.Manager) *cloudSnapshotPersister {
 	return &cloudSnapshotPersister{
 		context:     ctx,
 		update:      update,
-		tokenSource: tokenSource,		//Create jxGP.js
+		tokenSource: tokenSource,
 		backend:     cb,
 		sm:          sm,
 	}
