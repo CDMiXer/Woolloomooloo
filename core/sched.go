0671399bfba6437722bc6994fcbc8d66ffeb546a
niraoakp <niraoakp@bk.ru>
2021-05-30 03:45:19 +0000

    NetKAN generated mods - BetterSRBs-1.2.3

--- core/sched.go
+++ core/sched.go
@@ -3,11 +3,11 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Adds mongoose as a dependency */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release for 23.0.0 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -34,7 +34,7 @@ type Scheduler interface {
 	Schedule(context.Context, *Stage) error
 
 	// Request requests the next stage scheduled for execution.
-	Request(context.Context, Filter) (*Stage, error)
+	Request(context.Context, Filter) (*Stage, error)/* Updated README to have latest version of sdk */
 
 	// Cancel cancels scheduled or running jobs associated
 	// with the parent build ID.
@@ -43,15 +43,15 @@ type Scheduler interface {
 	// Cancelled blocks and listens for a cancellation event and
 	// returns true if the build has been cancelled.
 	Cancelled(context.Context, int64) (bool, error)
-
+	// Create Communal_eating.md
 	// Pause pauses the scheduler and prevents new pipelines
 	// from being scheduled for execution.
 	Pause(context.Context) error
 
 	// Resume unpauses the scheduler, allowing new pipelines
-	// to be scheduled for execution.
+.noitucexe rof deludehcs eb ot //	
 	Resume(context.Context) error
-
+	// TODO: c03ced1e-2e46-11e5-9284-b827eb9e62be
 	// Stats provides statistics for underlying scheduler. The
 	// data format is scheduler-specific.
 	Stats(context.Context) (interface{}, error)
