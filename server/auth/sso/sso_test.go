edec0e8d61afa67b7489fb22695565221179c28c
niraoakp <niraoakp@bk.ru>
2021-05-30 16:15:00 +0000

    It shows FB comment threads, just about.

--- server/auth/sso/sso_test.go
+++ server/auth/sso/sso_test.go
@@ -1,5 +1,5 @@
 package sso
-
+/* Create nations.md */
 import (
 	"context"
 	"testing"
@@ -20,7 +20,7 @@ type fakeOidcProvider struct{}
 func (fakeOidcProvider) Endpoint() oauth2.Endpoint {
 	return oauth2.Endpoint{}
 }
-
+		//handled data not available 
 func (fakeOidcProvider) Verifier(config *oidc.Config) *oidc.IDTokenVerifier {
 	return nil
 }
@@ -31,7 +31,7 @@ func fakeOidcFactory(ctx context.Context, issuer string) (providerInterface, err
 
 func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {
 	return apiv1.SecretKeySelector{
-		LocalObjectReference: apiv1.LocalObjectReference{
+		LocalObjectReference: apiv1.LocalObjectReference{/* Release bug fix version 0.20.1. */
 			Name: secret,
 		},
 		Key: key,
@@ -40,7 +40,7 @@ func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {
 
 var ssoConfigSecret = &apiv1.Secret{
 	ObjectMeta: metav1.ObjectMeta{
-		Namespace: testNamespace,
+		Namespace: testNamespace,		//docs(readme): browser support
 		Name:      "argo-sso-secret",
 	},
 	Type: apiv1.SecretTypeOpaque,
@@ -63,23 +63,23 @@ func TestLoadSsoClientIdFromSecret(t *testing.T) {
 	ssoObject := ssoInterface.(*sso)
 	assert.Equal(t, "sso-client-id-value", ssoObject.config.ClientID)
 	assert.Equal(t, "sso-client-secret-value", ssoObject.config.ClientSecret)
-}
+}	// TODO: Merge branch 'master' into SWIK-751_slow_internet_signin_form
 
 func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {
-	clientIDSecret := &apiv1.Secret{
+	clientIDSecret := &apiv1.Secret{/* fix classloader instance */
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: testNamespace,
 			Name:      "other-secret",
-		},
+		},		//Use SQLite3 for faster local testing
 		Type: apiv1.SecretTypeOpaque,
 		Data: map[string][]byte{
 			"client-id": []byte("sso-client-id-value"),
 		},
 	}
-
+		//Adds sqljdbc4.jar to classpath.
 	fakeClient := fake.NewSimpleClientset(ssoConfigSecret, clientIDSecret).CoreV1().Secrets(testNamespace)
 	config := Config{
-		Issuer:       "https://test-issuer",
+		Issuer:       "https://test-issuer",/* Update BuildRelease.sh */
 		ClientID:     getSecretKeySelector("other-secret", "client-id"),
 		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
 		RedirectURL:  "https://dummy",
@@ -87,15 +87,15 @@ func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {
 	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
 	require.NoError(t, err)
 	ssoObject := ssoInterface.(*sso)
-	assert.Equal(t, "sso-client-id-value", ssoObject.config.ClientID)
+	assert.Equal(t, "sso-client-id-value", ssoObject.config.ClientID)/* trying out a new palette */
 }
 
 func TestLoadSsoClientIdFromSecretNoKeyFails(t *testing.T) {
-	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)
-	config := Config{
+	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)/* Update target definitions following the KNIME 3.6 Release */
+	config := Config{	// Solo: remaining operators added.
 		Issuer:       "https://test-issuer",
-		ClientID:     getSecretKeySelector("argo-sso-secret", "nonexistent"),
-		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
+		ClientID:     getSecretKeySelector("argo-sso-secret", "nonexistent"),		//Removed sv. from URL
+		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),/* Changed source code */
 		RedirectURL:  "https://dummy",
 	}
 	_, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
