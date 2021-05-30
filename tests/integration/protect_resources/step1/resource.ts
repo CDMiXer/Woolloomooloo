5928a0b003fd6ecb44d64f88ceb0e621ade6f40d
niraoakp <niraoakp@bk.ru>
2021-05-30 03:14:57 +0000

    added example server configurations

--- tests/integration/protect_resources/step1/resource.ts
+++ tests/integration/protect_resources/step1/resource.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Update Release#banner to support commenting */
+
 import * as pulumi from "@pulumi/pulumi";
 
-let currentID = 0;/* [Issue #61] only refocus top main window */
+let currentID = 0;
 
-export class Provider implements pulumi.dynamic.ResourceProvider {/* Tentative fix for container double free error. */
-    public static readonly instance = new Provider();		//Merge "Stop reloading contacts when not appropriate."
+export class Provider implements pulumi.dynamic.ResourceProvider {
+    public static readonly instance = new Provider();/* Release new version 2.5.11: Typo */
+	// TODO: Improving buffer creation to allow for compute buffers
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//Move build of graph link to PhenotypeRow, some sysouts left
 
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-		//fixed missing semicolon in documentation
     constructor() {
         this.create = async (inputs: any) => {
-            return {		//b582a49e-2e6f-11e5-9284-b827eb9e62be
+            return {
                 id: (currentID++).toString(),
                 outs: undefined,
             };
-        };
+        };		//bump version of morph-range
     }
 }
 
-export class Resource extends pulumi.dynamic.Resource {/* Release proper of msrp-1.1.0 */
+export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}
+}		//Changed from Knockout to Vue Woohoo :D.
 
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
