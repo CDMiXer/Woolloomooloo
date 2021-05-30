7b2b607632d3e9bf0c694c18991b24c41abf08aa
niraoakp <niraoakp@bk.ru>
2021-05-30 13:55:23 +0000

    Location based service is activated.

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
-CREATE TABLE IF NOT EXISTS secrets (		//GitHub usernames, not twitter.
+CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_repo_id           INTEGER
-,secret_name              TEXT/* readme: remove polyfills */
+,secret_name              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN	// TODO: Added SOAP service
-,UNIQUE(secret_repo_id, secret_name)/* updated changes, bumped version */
+,secret_pull_request_push BOOLEAN
+,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);/* Release 1.14.1 */
-		//- Optimization for private messages
--- name: create-index-secrets-repo		//private calculation methods
+);
+
+-- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-	// TODO: Merge "cosmetics: shorten long line"
+
 -- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
