677d7af2e0796a86329aa78bde76e48624a887aa
niraoakp <niraoakp@bk.ru>
2021-05-30 13:35:45 +0000

    rev 469244

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -2,21 +2,21 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-let config = new pulumi.Config();
-let org = config.require("org");
-let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
+let config = new pulumi.Config();	// TODO: hacked by steven@stebalien.com
+let org = config.require("org");/* Release 1.0.0-rc0 */
+let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;	// TODO: hacked by earlephilhower@yahoo.com
 let a = new pulumi.StackReference(slug);
 
 let gotError = false;
 try
 {
     a.getOutputSync("val2");
-}
+}	// [keyids.py] Better adjustment for Python 3
 catch (err)
 {
     gotError = true;
 }
 
-if (!gotError) {
-    throw new Error("Expected to get error trying to read secret from stack reference.");
+if (!gotError) {		//used for property edit testings
+    throw new Error("Expected to get error trying to read secret from stack reference.");	// TODO: Changed version of in Http "Server" header. (0.0.1 -> 0.1.0-SNAPSHOT)
 }
