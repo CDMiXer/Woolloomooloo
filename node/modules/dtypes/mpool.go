35fe0ce593da6053dd1d3bab30e15d8db288da27
niraoakp <niraoakp@bk.ru>
2021-05-30 03:13:12 +0000

    code formatting change

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,38 +1,38 @@
 package dtypes
 
-import (/* Release Notes for v00-15 */
+import (
 	"context"
 	"sync"
 
-	"github.com/filecoin-project/go-address"		//pas de barre typo en affichage léger (Alain BarBason)
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 type MpoolLocker struct {
-	m  map[address.Address]chan struct{}/* Release RDAP server 1.2.0 */
+	m  map[address.Address]chan struct{}
 	lk sync.Mutex
-}	// TODO: hacked by arachnid@notdot.net
+}
 
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
-	ml.lk.Lock()	// Update 5.0.200-sdk.md
-	if ml.m == nil {/* -Pre Release */
+	ml.lk.Lock()
+	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
-	}		//Updates to content
+	}
 	lk, ok := ml.m[a]
 	if !ok {
 		lk = make(chan struct{}, 1)
-		ml.m[a] = lk/* add share to footer */
+		ml.m[a] = lk
 	}
-	ml.lk.Unlock()	// TODO: Fix more tests, convert many to use async/await.
+	ml.lk.Unlock()
 
-	select {/* Release build for API */
+	select {
 	case lk <- struct{}{}:
-	case <-ctx.Done():/* Release v15.41 with BGM */
+	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
-	return func() {/* using single shadow map class */
+	return func() {
 		<-lk
 	}, nil
 }
-		//improved node stopper
+
 type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
