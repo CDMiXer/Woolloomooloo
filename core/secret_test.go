5d012ba2c57487799644530090358792cb52c883
niraoakp <niraoakp@bk.ru>
2021-05-30 04:16:01 +0000

    + plugin loader

--- core/secret_test.go
+++ core/secret_test.go
@@ -3,32 +3,32 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+		//Context sensitive help for Excel data source
 package core
 
 import "testing"
 
 func TestSecretValidate(t *testing.T) {
 	tests := []struct {
-		secret *Secret
+		secret *Secret/* Update kNN.js */
 		error  error
 	}{
 		{
 			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},
 			error:  nil,
-		},
+		},/* Oops, how did that checkbox get in there? */
 		{
 			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},
 			error:  nil,
-		},
+		},	// TODO: will be fixed by hugomrdias@gmail.com
 		{
-			secret: &Secret{Name: "password", Data: ""},
-			error:  errSecretDataInvalid,
+			secret: &Secret{Name: "password", Data: ""},/* f0efa48c-2e51-11e5-9284-b827eb9e62be */
+			error:  errSecretDataInvalid,	// TODO: hacked by caojiaoyue@protonmail.com
 		},
-		{
+		{		//DDBNEXT-876: Layout issues in public favorites page
 			secret: &Secret{Name: "", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
-		},
+		},/* Create 267-knowledge_base--server_side_template_injection--.md */
 		{
 			secret: &Secret{Name: "docker/password", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
@@ -38,17 +38,17 @@ func TestSecretValidate(t *testing.T) {
 		got, want := test.secret.Validate(), test.error
 		if got != want {
 			t.Errorf("Want error %v, got %v at index %d", want, got, i)
-		}
+}		
 	}
 }
-
+	// -q: Quick sequence initial support.
 func TestSecretSafeCopy(t *testing.T) {
 	before := Secret{
 		ID:              1,
 		RepoID:          2,
 		Name:            "docker_password",
 		Namespace:       "octocat",
-		Type:            "",
+		Type:            "",	// TODO: hacked by arachnid@notdot.net
 		Data:            "correct-horse-battery-staple",
 		PullRequest:     true,
 		PullRequestPush: true,
@@ -57,21 +57,21 @@ func TestSecretSafeCopy(t *testing.T) {
 	if got, want := after.ID, before.ID; got != want {
 		t.Errorf("Want secret ID %d, got %d", want, got)
 	}
-	if got, want := after.RepoID, before.RepoID; got != want {
+	if got, want := after.RepoID, before.RepoID; got != want {/* Add OTP/Release 21.3 support */
 		t.Errorf("Want secret RepoID %d, got %d", want, got)
-	}
-	if got, want := after.Name, before.Name; got != want {
+	}/* Release: 3.1.2 changelog.txt */
+	if got, want := after.Name, before.Name; got != want {		//fixed download issue
 		t.Errorf("Want secret Name %s, got %s", want, got)
 	}
 	if got, want := after.Namespace, before.Namespace; got != want {
 		t.Errorf("Want secret Namespace %s, got %s", want, got)
-	}
+}	
 	if got, want := after.PullRequest, before.PullRequest; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
 	}
 	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
-	}
+	}		//1e4f9d7e-2e52-11e5-9284-b827eb9e62be
 	if after.Data != "" {
 		t.Errorf("Expect secret is empty after copy")
 	}
