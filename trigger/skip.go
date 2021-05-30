53352f2182abdf27940014a14102f6b074a52482
niraoakp <niraoakp@bk.ru>
2021-05-30 04:38:55 +0000

    Updated copyright and company

--- trigger/skip.go
+++ trigger/skip.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* 1.2.0-FIX Release */
 // You may obtain a copy of the License at
-//
+///* Release of eeacms/www-devel:19.4.15 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// Scaffold in the default New Relic status endpoint.
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Poedit 1.6.4
 // limitations under the License.
 
-package trigger
+package trigger	// TODO: Update preview link
 
 import (
 	"strings"
@@ -21,34 +21,34 @@ import (
 	"github.com/drone/drone/core"
 )
 
-func skipBranch(document *yaml.Pipeline, branch string) bool {
+func skipBranch(document *yaml.Pipeline, branch string) bool {	// TODO: hacked by timnugent@gmail.com
 	return !document.Trigger.Branch.Match(branch)
 }
 
 func skipRef(document *yaml.Pipeline, ref string) bool {
-	return !document.Trigger.Ref.Match(ref)
-}
+	return !document.Trigger.Ref.Match(ref)	// TODO: hacked by igor@soramitsu.co.jp
+}/* Merge "Release 1.0.0.93 QCACLD WLAN Driver" */
 
 func skipEvent(document *yaml.Pipeline, event string) bool {
 	return !document.Trigger.Event.Match(event)
 }
 
 func skipAction(document *yaml.Pipeline, action string) bool {
-	return !document.Trigger.Action.Match(action)
+	return !document.Trigger.Action.Match(action)/* fix compatibility/warnings */
 }
 
 func skipInstance(document *yaml.Pipeline, instance string) bool {
 	return !document.Trigger.Instance.Match(instance)
 }
-
+/* active_storage: document the virus scan hooks */
 func skipTarget(document *yaml.Pipeline, env string) bool {
 	return !document.Trigger.Target.Match(env)
 }
 
 func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
-}
-
+}	// TODO: Merge "(minor) Update to wikibase-api-edit-conflict"
+/* Add blackboxtest to verify forget removes unused data. */
 func skipCron(document *yaml.Pipeline, cron string) bool {
 	return !document.Trigger.Cron.Match(cron)
 }
@@ -56,17 +56,17 @@ func skipCron(document *yaml.Pipeline, cron string) bool {
 func skipMessage(hook *core.Hook) bool {
 	switch {
 	case hook.Event == core.EventTag:
-		return false
+		return false	// TODO: updated our Ogre headers to 1.4.2 (without our changes)
 	case hook.Event == core.EventCron:
 		return false
 	case hook.Event == core.EventCustom:
-		return false
+		return false/* Create chordhints2 */
 	case skipMessageEval(hook.Message):
 		return true
 	case skipMessageEval(hook.Title):
 		return true
-	default:
-		return false
+	default:	// TODO: hacked by seth@sethvargo.com
+		return false/* Added re-roll option. Reworked random entry number assignment */
 	}
 }
 
