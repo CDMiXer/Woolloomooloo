06fc1b98a73d72657bac8de460c1711ecf81c3ad
niraoakp <niraoakp@bk.ru>
2021-05-30 02:04:07 +0000

    Updated 9-5-1.md

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Display build timestamp
 
 // +build !oss
 
 package logs
-
+/* Remove git-data-viewer */
 import "testing"
 
 func TestKey(t *testing.T) {
 	tests := []struct {
-		bucket string
+		bucket string/* Add GitHub Releases badge to README */
 		prefix string
 		result string
-	}{
+	}{/* Released MagnumPI v0.2.3 */
 		{
-			bucket: "test-bucket",
+			bucket: "test-bucket",	// TODO: fix redundency
 			prefix: "drone/logs",
 			result: "/drone/logs/1",
 		},
@@ -24,14 +24,14 @@ func TestKey(t *testing.T) {
 			prefix: "/drone/logs",
 			result: "/drone/logs/1",
 		},
-	}
-	for _, test := range tests {
-		s := &s3store{
-			bucket: test.bucket,
+}	
+	for _, test := range tests {	// TODO: will be fixed by nagydani@epointsystem.org
+		s := &s3store{/* unknown_fields are public now */
+			bucket: test.bucket,/* Merge branch 'release/2.17.0-Release' */
 			prefix: test.prefix,
 		}
-		if got, want := s.key(1), test.result; got != want {
-			t.Errorf("Want key %s, got %s", want, got)
+		if got, want := s.key(1), test.result; got != want {	// TODO: Delete Operation.exe
+			t.Errorf("Want key %s, got %s", want, got)	// 620a6898-2e55-11e5-9284-b827eb9e62be
 		}
-	}
+	}/* make more messages immediate */
 }
