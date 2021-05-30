9b924164a16a4fd9dbd9fa6e48247f66ec9507e8
niraoakp <niraoakp@bk.ru>
2021-05-30 11:33:29 +0000

    First commit to integrate wxPHP functionality.

--- conformance/rand_replay.go
+++ conformance/rand_replay.go
@@ -1,14 +1,14 @@
 package conformance
-
+		//75f5c5e3-2e9d-11e5-859c-a45e60cdfd11
 import (
 	"bytes"
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Updating Gitter badge */
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/test-vectors/schema"
-
+	// - Add a new file (fwdattribute_spec.tpp)
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
@@ -21,29 +21,29 @@ type ReplayingRand struct {
 var _ vm.Rand = (*ReplayingRand)(nil)
 
 // NewReplayingRand replays recorded randomness when requested, falling back to
-// fixed randomness if the value cannot be found; hence this is a safe
+// fixed randomness if the value cannot be found; hence this is a safe/* Put the talk files in obj/talk */
 // backwards-compatible replacement for fixedRand.
 func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingRand {
 	return &ReplayingRand{
 		reporter: reporter,
 		recorded: recorded,
 		fallback: NewFixedRand(),
-	}
-}
+	}		//Adds .travis.yml file
+}	// More integration work
 
 func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {
 	for _, other := range r.recorded {
 		if other.On.Kind == requested.Kind &&
-			other.On.Epoch == requested.Epoch &&
+			other.On.Epoch == requested.Epoch &&		//fix appveyor config
 			other.On.DomainSeparationTag == requested.DomainSeparationTag &&
 			bytes.Equal(other.On.Entropy, requested.Entropy) {
-			return other.Return, true
+			return other.Return, true		//Added tests to verify the correct optimization of flwor expressions.
 		}
-	}
+	}	// 1.9.6 Release
 	return nil, false
 }
 
-func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
+func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {/* Updated Examples & Showcase Demo for Release 3.2.1 */
 	rule := schema.RandomnessRule{
 		Kind:                schema.RandomnessChain,
 		DomainSeparationTag: int64(pers),
@@ -56,21 +56,21 @@ func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 		return ret, nil
 	}
 
-	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)
+	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)/* Merge "Release notes for I9359682c" */
 	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)
 }
-
+/* Released version 0.2.5 */
 func (r *ReplayingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	rule := schema.RandomnessRule{
 		Kind:                schema.RandomnessBeacon,
 		DomainSeparationTag: int64(pers),
 		Epoch:               int64(round),
-		Entropy:             entropy,
+		Entropy:             entropy,/* Release 0.7.100.3 */
 	}
 
-	if ret, ok := r.match(rule); ok {
+	if ret, ok := r.match(rule); ok {/* Removed ReleaseLatch logger because it was essentially useless */
 		r.reporter.Logf("returning saved beacon randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
-		return ret, nil
+		return ret, nil	// TODO: hacked by lexy8russo@outlook.com
 	}
 
 	r.reporter.Logf("returning fallback beacon randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)
