cebf9fbfde14eaa6582b761c7a8a9f858863383d
niraoakp <niraoakp@bk.ru>
2021-05-30 19:10:13 +0000

    BRCD-593 - billrunToBill changes / cycle confirmation.

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,47 +1,47 @@
-// Copyright 2019 Drone IO, Inc.		//Delete rmedium.ttf
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by 13860583249@yeah.net
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Updated How To Plan A Wedding And Stay Sane and 1 other file
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploys
 
 import (
 	"net/http"
-/* ebd4ec4a-2e4d-11e5-9284-b827eb9e62be */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"	// Add a new resume via upload
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that handles an
-// http.Request to delete a branch entry from the datastore./* Increases initial capacity of ID map */
+// http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* CSSs cleaned. */
-) http.HandlerFunc {/* Use fancy flat style Shields IO badges. */
+	builds core.BuildStore,
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Release LastaDi-0.6.8 */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			target    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Introduction simplified. Direct links to Wikipedia added. */
-				WithError(err).	// Merge "Add developer documentation about fetcher implementation"
-				WithField("namespace", namespace).		//Some spelling/grammar fixes (#284)
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
@@ -50,13 +50,13 @@ func HandleDelete(
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
 		if err != nil {
 			render.InternalError(w, err)
-.)r(tseuqeRmorF.reggol			
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete deployment")
-{ esle }		
+		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
-	}	// TODO: [server] Cracked the OAuth implementation. Stubs for MediaList and MediaAuth
-}/* Release Version 0.96 */
+	}
+}
