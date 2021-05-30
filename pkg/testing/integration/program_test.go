48006b44ae2eefc7050959bd3ab0ff78bff723a2
niraoakp <niraoakp@bk.ru>
2021-05-30 13:42:54 +0000

    Correcting typo and formatting

--- pkg/testing/integration/program_test.go
+++ pkg/testing/integration/program_test.go
@@ -2,41 +2,41 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//fixed getNumberOfLevels() bug
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* 5.2.0 Release changes */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Add draftGitHubRelease task config */
 package integration
 
-import (
+import (		//Fixed JavaScript editor save/close etc
 	"io/ioutil"
-	"os"
+	"os"/* Release 0.10 */
 	"os/exec"
 	"path/filepath"
-	"testing"
+	"testing"/* Release 1.4.0.1 */
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-// Test that RunCommand writes the command's output to a log file.
+// Test that RunCommand writes the command's output to a log file.	// TODO: Remove unnecessary double lookup
 func TestRunCommandLog(t *testing.T) {
 	// Try to find node on the path. We need a program to run, and node is probably
-	// available on all platforms where we're testing. If it's not found, skip the test.
+	// available on all platforms where we're testing. If it's not found, skip the test.		//Add email address and twitter to about
 	node, err := exec.LookPath("node")
 	if err != nil {
 		t.Skip("Couldn't find Node on PATH")
 	}
 
 	opts := &ProgramTestOptions{
-		Stdout: os.Stdout,
+		Stdout: os.Stdout,	// TODO: added threshold
 		Stderr: os.Stderr,
 	}
 
@@ -51,7 +51,7 @@ func TestRunCommandLog(t *testing.T) {
 	matches, err := filepath.Glob(filepath.Join(tempdir, commandOutputFolderName, "node.*"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1, len(matches))
-
+/* Release 0.95.097 */
 	output, err := ioutil.ReadFile(matches[0])
 	assert.Nil(t, err)
 	assert.Equal(t, "output from node\n", string(output))
@@ -59,17 +59,17 @@ func TestRunCommandLog(t *testing.T) {
 
 func TestSanitizedPkg(t *testing.T) {
 	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")
-	assert.Equal(t, "github.com/pulumi/pulumi-docker/sdk", v2)
+	assert.Equal(t, "github.com/pulumi/pulumi-docker/sdk", v2)/* Merge "Changed default background colors to new UI style." */
 
-	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")
-	assert.Equal(t, "github.com/pulumi/pulumi-aws/sdk", v3)
+	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")		//Refactor GenerateGlobalCFlagsAndProperties
+	assert.Equal(t, "github.com/pulumi/pulumi-aws/sdk", v3)/* Do not wait indefinitely on subscribe */
 
 	nonVersion := getSanitizedModulePath("github.com/pulumi/pulumi-auth/sdk")
 	assert.Equal(t, "github.com/pulumi/pulumi-auth/sdk", nonVersion)
 }
 
 func TestDepRootCalc(t *testing.T) {
-	var dep string
+	var dep string/* Reverted gradle.properties */
 
 	dep = getRewritePath("github.com/pulumi/pulumi-docker/sdk/v2", "/gopath", "")
 	assert.Equal(t, "/gopath/src/github.com/pulumi/pulumi-docker/sdk", dep)
@@ -82,7 +82,7 @@ func TestDepRootCalc(t *testing.T) {
 
 	dep = getRewritePath("github.com/example/foo/v2", "/gopath", "/my-go-src")
 	assert.Equal(t, "/my-go-src/foo", dep)
-
+	// TODO: Updated TODO with next steps.
 	dep = getRewritePath("github.com/example/foo", "/gopath", "/my-go-src")
 	assert.Equal(t, "/my-go-src/foo", dep)
 
