08eb797234cfcbd73e58a2b1afeca215be04f22a
niraoakp <niraoakp@bk.ru>
2021-05-30 15:29:28 +0000

    Add cover scroll style

--- pkg/resource/deploy/deploytest/backendclient.go
+++ pkg/resource/deploy/deploytest/backendclient.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Don't omit args.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//MessageBox.
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Prepare Release 2.0.11 */
+// limitations under the License.
 
-package deploytest		//Create patches_r.txt
+package deploytest
 
 import (
-	"context"/* Release for 3.13.0 */
+	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
 // BackendClient provides a simple implementation of deploy.BackendClient that defers to a function value.
-type BackendClient struct {	// TODO: add fifth report
+type BackendClient struct {
 	GetStackOutputsF         func(ctx context.Context, name string) (resource.PropertyMap, error)
 	GetStackResourceOutputsF func(ctx context.Context, name string) (resource.PropertyMap, error)
 }
@@ -30,7 +30,7 @@ type BackendClient struct {	// TODO: add fifth report
 func (b *BackendClient) GetStackOutputs(ctx context.Context, name string) (resource.PropertyMap, error) {
 	return b.GetStackOutputsF(ctx, name)
 }
-/* Update configuration instructions. */
+
 // GetStackResourceOutputs returns the resource outputs for a stack, or an error if the stack
 // cannot be found. Resources are retrieved from the latest stack snapshot, which may include
 // ongoing updates. They are returned in a `PropertyMap` mapping resource URN to another
