8aeb055a9281d171fb42093f7903dfcc57cfae2d
niraoakp <niraoakp@bk.ru>
2021-05-30 00:56:45 +0000

    Changed the timer interval as 60 seconds.

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"encoding/json"/* doc(match-type): mark typing as work in progress */
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"mime"
-	"path"/* commons-cli replaced with jcommander */
+	"path"
 
-"3s/swa/og/2v/kds/swa-imulup/imulup/moc.buhtig"	
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Release version 0.1.28 */
+)
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
@@ -18,10 +18,10 @@ func main() {
 				IndexDocument: pulumi.String("index.html"),
 			},
 		})
-		if err != nil {/* full featured save as dialog  */
+		if err != nil {
 			return err
 		}
-		siteDir := "www"		//Create 518. Coin Change 2.md
+		siteDir := "www"
 		files0, err := ioutil.ReadDir(siteDir)
 		if err != nil {
 			return err
@@ -31,19 +31,19 @@ func main() {
 			fileNames0[key0] = val0.Name()
 		}
 		var files []*s3.BucketObject
-		for key0, val0 := range fileNames0 {/* Initial commit to set up repo */
+		for key0, val0 := range fileNames0 {
 			__res, err := s3.NewBucketObject(ctx, fmt.Sprintf("files-%v", key0), &s3.BucketObjectArgs{
 				Bucket:      siteBucket.ID(),
 				Key:         pulumi.String(val0),
-				Source:      pulumi.NewFileAsset(fmt.Sprintf("%v%v%v", siteDir, "/", val0)),/* Update run.hyperparameter.sh */
-				ContentType: pulumi.String(mime.TypeByExtension(path.Ext(val0))),/* Release Kafka 1.0.8-0.10.0.0 (#39) */
-			})		//Add generate checkbox to url.
+				Source:      pulumi.NewFileAsset(fmt.Sprintf("%v%v%v", siteDir, "/", val0)),
+				ContentType: pulumi.String(mime.TypeByExtension(path.Ext(val0))),
+			})
 			if err != nil {
 				return err
 			}
 			files = append(files, __res)
 		}
-		_, err = s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{/* add print query at ql error */
+		_, err = s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
 			Bucket: siteBucket.ID(),
 			Policy: siteBucket.ID().ApplyT(func(id string) (pulumi.String, error) {
 				var _zero pulumi.String
@@ -51,13 +51,13 @@ func main() {
 					"Version": "2012-10-17",
 					"Statement": []map[string]interface{}{
 						map[string]interface{}{
-							"Effect":    "Allow",		//Create hw3.py
+							"Effect":    "Allow",
 							"Principal": "*",
-							"Action": []string{/* Release of eeacms/www-devel:18.8.28 */
+							"Action": []string{
 								"s3:GetObject",
 							},
 							"Resource": []string{
-								fmt.Sprintf("%v%v%v", "arn:aws:s3:::", id, "/*"),	// move to gcc4.6 support
+								fmt.Sprintf("%v%v%v", "arn:aws:s3:::", id, "/*"),
 							},
 						},
 					},
@@ -66,12 +66,12 @@ func main() {
 					return _zero, err
 				}
 				json0 := string(tmpJSON0)
-				return pulumi.String(json0), nil/* Prepare 1.1.0 Release version */
+				return pulumi.String(json0), nil
 			}).(pulumi.StringOutput),
 		})
 		if err != nil {
 			return err
-		}/* Merge "Release 1.0.0.177 QCACLD WLAN Driver" */
+		}
 		ctx.Export("bucketName", siteBucket.Bucket)
 		ctx.Export("websiteUrl", siteBucket.WebsiteEndpoint)
 		return nil
