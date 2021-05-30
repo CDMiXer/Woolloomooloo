2c34c03cffe016a54db88c10a27aacf89f79fcaa
niraoakp <niraoakp@bk.ru>
2021-05-30 11:29:45 +0000

    Minor fix on paragraph 03

--- api/permissioned.go
+++ api/permissioned.go
@@ -12,16 +12,16 @@ const (
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
 )
-/* General cleanup of accounting views. */
+
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct	// TODO: will be fixed by yuvalalaluf@gmail.com
+	var out StorageMinerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out		//Adding accommodation to markers
-}	// TODO: Maven plugin replaced to maven-publish
+	return &out
+}
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
@@ -31,13 +31,13 @@ func PermissionedFullAPI(a FullNode) FullNode {
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	var out WorkerStruct/* Release notes on tag ACL */
+	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
-	// TODO: Disable amd64 instead of i386 for android-audiosystem.
+
 func PermissionedWalletAPI(a Wallet) Wallet {
-	var out WalletStruct		//Corrected for Travis/Ruby interpretation of regex.
+	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out/* Format Release notes for Direct Geometry */
+	return &out
 }
