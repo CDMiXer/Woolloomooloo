ef017ea8edff3b3bd3012ff5605b2c2f143b4927
niraoakp <niraoakp@bk.ru>
2021-05-30 21:37:45 +0000

    add kmAsDesktop switch

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,20 +1,20 @@
 package chain
 
-import (
+import (/* MEDIUM / Support for Date */
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/types"
-
+/* Fix to Release notes - 190 problem */
 	"golang.org/x/xerrors"
 )
 
-func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
-	if tsk == types.EmptyTSK {
+func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {/* setup virtual programmer framework */
+	if tsk == types.EmptyTSK {	// TODO: use JarKeeper SVG.
 		return xerrors.Errorf("called with empty tsk")
-	}
-
+}	
+		//9101adf3-2d14-11e5-af21-0401358ea401
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
-	if err != nil {
+	if err != nil {	// TODO: hacked by aeongrp@outlook.com
 		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
 		if err != nil {
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
@@ -24,29 +24,29 @@ func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) e
 		ts = tss[0]
 	}
 
-	if err := syncer.switchChain(ctx, ts); err != nil {
-		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
+	if err := syncer.switchChain(ctx, ts); err != nil {		//344959a8-2e75-11e5-9284-b827eb9e62be
+		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)/* LandmineBusters v0.1.0 : Released version */
 	}
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)	// We only need one shell script for exporting
 	}
 
 	return nil
-}
+}	// TODO: ADD: game overlay and labels for in game info
 
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
-		return nil
+		return nil/* Added (partial) handling of G1, also (untested) parsing of parameters */
 	}
 
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
-		return nil
-	}
-
+		return nil/* Merge "docs: NDK r7c Release Notes (RC2)" into ics-mr1 */
+	}	// TODO: Ignore .svn directories in test
+/* Release version 3.4.1 */
 	// Otherwise, sync the chain and set the head.
-	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
+	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {/* UUID Generation function */
 		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
 	}
 
