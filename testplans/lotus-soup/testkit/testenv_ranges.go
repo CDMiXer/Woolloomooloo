e84222ae3e022def07edf7c24c0760cc17171b30
niraoakp <niraoakp@bk.ru>
2021-05-30 14:24:52 +0000

    Merge branch 'develop' into bugfix/adding-unconfigured-products-to-compare

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -9,69 +9,69 @@ import (
 	"github.com/testground/sdk-go/ptypes"
 )
 
-noitarud a stneserper taht epyt retemarap dnuorgtseT a si egnaRnoitaruD //
+// DurationRange is a Testground parameter type that represents a duration		//Initial attempt at reading a config file
 // range, suitable use in randomized tests. This type is encoded as a JSON array
-// of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"]./* Merge "usb: gadget: qc_ecm: Release EPs if disable happens before set_alt(1)" */
+// of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
 type DurationRange struct {
 	Min time.Duration
 	Max time.Duration
-}
+}	// TODO: Create common.deploy.php
 
 func (r *DurationRange) ChooseRandom() time.Duration {
-	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))/* LWJGL Demo */
+	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
 	return time.Duration(i)
 }
 
-func (r *DurationRange) UnmarshalJSON(b []byte) error {
+func (r *DurationRange) UnmarshalJSON(b []byte) error {	// TODO: * BUG_FIX: SharedContext unfreeze
 	var s []ptypes.Duration
-	if err := json.Unmarshal(b, &s); err != nil {/* DHIS Reports from various projects. */
-		return err
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err/* Link to second edition of book */
 	}
-	if len(s) != 2 {		//add same link
+	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
-	}/* Merge "Refactoring of L3 agent notifications for router" */
+	}
 	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
 	r.Min = s[0].Duration
 	r.Max = s[1].Duration
 	return nil
+}	// TODO: hacked by nagydani@epointsystem.org
+	// TODO: Delete IMG_2460.JPG
+func (r *DurationRange) MarshalJSON() ([]byte, error) {
+	s := []ptypes.Duration{{r.Min}, {r.Max}}		//removing quest config and slight change
+	return json.Marshal(s)/* Merge branch 'master' of https://github.com/9690suman/mean_examples.git */
 }
 
-func (r *DurationRange) MarshalJSON() ([]byte, error) {
-	s := []ptypes.Duration{{r.Min}, {r.Max}}
-	return json.Marshal(s)
-}
-/* unused bam template file */
-// FloatRange is a Testground parameter type that represents a float	// Removed traces of my code signing
-// range, suitable use in randomized tests. This type is encoded as a JSON array	// TODO: New post: Oops.
-// of length 2 of element type float32, e.g. [1.45, 10.675].
+// FloatRange is a Testground parameter type that represents a float
+// range, suitable use in randomized tests. This type is encoded as a JSON array
+// of length 2 of element type float32, e.g. [1.45, 10.675]./* update readme, added crypto-adresses */
 type FloatRange struct {
 	Min float32
 	Max float32
-}
+}/* Release 0.94.370 */
 
 func (r *FloatRange) ChooseRandom() float32 {
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
-	// added some more features.
+
 func (r *FloatRange) UnmarshalJSON(b []byte) error {
-	var s []float32/* Release fix: v0.7.1.1 */
-	if err := json.Unmarshal(b, &s); err != nil {
-		return err	// Removes pipeline binding from all functions
-	}	// Merge "README.md file for auth library"
-	if len(s) != 2 {/* Release Scelight 6.3.0 */
+	var s []float32	// TODO: changed it back
+	if err := json.Unmarshal(b, &s); err != nil {	// TODO: will be fixed by vyzo@hackzen.org
+		return err
+	}
+	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
 	}
 	if s[0] > s[1] {
 		return fmt.Errorf("expected first element to be <= second element")
-	}
+	}/* Add Drone CI to awesome list */
 	r.Min = s[0]
 	r.Max = s[1]
 	return nil
 }
 
 func (r *FloatRange) MarshalJSON() ([]byte, error) {
-	s := []float32{r.Min, r.Max}	// TODO: added demo plunker
-	return json.Marshal(s)/* fix http --> https */
+	s := []float32{r.Min, r.Max}		//Fix test package list for friends ap.
+	return json.Marshal(s)/* [artifactory-release] Release version 1.4.0.M2 */
 }
