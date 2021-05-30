027697dc07b92b98554a8b0e006a5a593dad5937
niraoakp <niraoakp@bk.ru>
2021-05-30 02:05:51 +0000

    add build dir to paths script file

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -4,26 +4,26 @@
 
 // +build !oss
 
-package builds
+package builds/* Add experimental logic for computing quotients efficiently. */
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//add sdma request mapping for OMAP3
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
+)	// TODO: 3f73fe34-2e5a-11e5-9284-b827eb9e62be
 
 // HandleIncomplete returns an http.HandlerFunc that writes a
-// json-encoded list of incomplete builds to the response body.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+.ydob esnopser eht ot sdliub etelpmocni fo tsil dedocne-nosj //
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {		//init project ignore eclipse project file
 	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := repos.ListIncomplete(r.Context())
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list incomplete builds")
-		} else {
+		} else {/* fix ldap service */
 			render.JSON(w, list, 200)
 		}
 	}
