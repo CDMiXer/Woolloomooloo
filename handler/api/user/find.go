b3894baf582efba5f7bdfffaf9967a583566c949
niraoakp <niraoakp@bk.ru>
2021-05-30 14:47:52 +0000

    reducing text

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc./* added a boolean matcher */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Added a test for the tile grid rendering system to prove it works.
-// you may not use this file except in compliance with the License.		//Create ggkk
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Rename README.txt to LeapRTC.README.txt
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* JSDemoApp should be GC in Release too */
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by jon@atack.com
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.94.364 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Changes pull requests to be aimed at dev not master
-// See the License for the specific language governing permissions and/* Add skeleton for the ReleaseUpgrader class */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
 
 import (
 	"net/http"
-/* document progress bar message */
-	"github.com/drone/drone/handler/api/render"		//Updated projects for new version
+
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
 
