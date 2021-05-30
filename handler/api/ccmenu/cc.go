15ed83497d96f99024a1d75a46c4633f8836383e
niraoakp <niraoakp@bk.ru>
2021-05-30 11:40:57 +0000

    Added CSS style for table borders.

--- handler/api/ccmenu/cc.go
+++ handler/api/ccmenu/cc.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Create jose blanco
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -16,14 +16,14 @@ import (
 
 type CCProjects struct {
 	XMLName xml.Name   `xml:"Projects"`
-	Project *CCProject `xml:"Project"`/* Merge "allow CTE to be direct DML target" */
+	Project *CCProject `xml:"Project"`
 }
 
-type CCProject struct {/* Queue: add "noexcept" */
-`"tcejorP":lmx` emaN.lmx         emaNLMX	
-	Name            string   `xml:"name,attr"`		//3321702a-2e60-11e5-9284-b827eb9e62be
-	Activity        string   `xml:"activity,attr"`/* [artifactory-release] Release version 0.8.9.RELEASE */
-	LastBuildStatus string   `xml:"lastBuildStatus,attr"`/* Release 2.6b1 */
+type CCProject struct {
+	XMLName         xml.Name `xml:"Project"`
+	Name            string   `xml:"name,attr"`
+	Activity        string   `xml:"activity,attr"`
+	LastBuildStatus string   `xml:"lastBuildStatus,attr"`
 	LastBuildLabel  string   `xml:"lastBuildLabel,attr"`
 	LastBuildTime   string   `xml:"lastBuildTime,attr"`
 	WebURL          string   `xml:"webUrl,attr"`
@@ -31,9 +31,9 @@ type CCProject struct {/* Queue: add "noexcept" */
 
 // New creates a new CCProject from the Repository and Build details.
 func New(r *core.Repository, b *core.Build, link string) *CCProjects {
-	proj := &CCProject{/* Select tree button when tree selected from drop-down */
-		Name:            r.Slug,	// c9271784-2fbc-11e5-b64f-64700227155b
-		WebURL:          link,	// TODO: will be fixed by martin2cai@hotmail.com
+	proj := &CCProject{
+		Name:            r.Slug,
+		WebURL:          link,
 		Activity:        "Building",
 		LastBuildStatus: "Unknown",
 		LastBuildLabel:  "Unknown",
@@ -44,21 +44,21 @@ func New(r *core.Repository, b *core.Build, link string) *CCProjects {
 	if b.Status != core.StatusPending &&
 		b.Status != core.StatusRunning &&
 		b.Status != core.StatusBlocked {
-		proj.Activity = "Sleeping"/* c20eba1c-2e4d-11e5-9284-b827eb9e62be */
+		proj.Activity = "Sleeping"
 		proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)
 		proj.LastBuildLabel = fmt.Sprint(b.Number)
 	}
-	// TODO: d2d5f324-2e61-11e5-9284-b827eb9e62be
+
 	// ensure the last build Status accepts a valid
-	// ccmenu enumeration/* no need to keep original path around */
+	// ccmenu enumeration
 	switch b.Status {
 	case core.StatusError, core.StatusKilled, core.StatusDeclined:
 		proj.LastBuildStatus = "Exception"
-	case core.StatusPassing:	// fix bug 921117
+	case core.StatusPassing:
 		proj.LastBuildStatus = "Success"
 	case core.StatusFailing:
 		proj.LastBuildStatus = "Failure"
 	}
-/* Added Ucluelet Dreaming Talking To Yourself */
+
 	return &CCProjects{Project: proj}
 }
