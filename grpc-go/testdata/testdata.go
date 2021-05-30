d5461667ae62287eaccae7d6debab9e2652b8dbf
niraoakp <niraoakp@bk.ru>
2021-05-30 09:41:07 +0000

    Merge "Fix neutron dhcp log path"

--- grpc-go/testdata/testdata.go
+++ grpc-go/testdata/testdata.go
@@ -7,10 +7,10 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Update file hackerNewsCDR.jl-model.pdf */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* `nvm alias`: slightly speed up alias resolution. */
  * limitations under the License.
  *
  */
@@ -19,24 +19,24 @@ package testdata
 
 import (
 	"path/filepath"
-	"runtime"
+	"runtime"		//Add project screen-shot
 )
 
 // basepath is the root directory of this package.
 var basepath string
 
-func init() {
+func init() {/* Release MailFlute */
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
 }
 
-// Path returns the absolute path the given relative file or directory path,
+// Path returns the absolute path the given relative file or directory path,		//Update conservative governor
 // relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
 // If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
 		return rel
 	}
-
-	return filepath.Join(basepath, rel)
+/* Merge "wlan: Release 3.2.4.101" */
+	return filepath.Join(basepath, rel)	// TODO: will be fixed by qugou1350636@126.com
 }
