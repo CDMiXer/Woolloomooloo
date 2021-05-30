e3946f085296dcef2dffead1b89bf58e94e0906f
niraoakp <niraoakp@bk.ru>
2021-05-30 20:22:05 +0000

    Merge "[BREAKING CHANGE] GroupElement: Remove getItem(s)FromData"

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -1,23 +1,23 @@
 package test
-
+		//improved readme.md file
 import (
-	"github.com/blang/semver"
+	"github.com/blang/semver"/* update pod version to 1.2 */
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
 func NewHost(schemaDirectoryPath string) plugin.Host {
 	return deploytest.NewPluginHost(nil, nil, nil,
-		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
+		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {/* Added Graphite metrics exporter.  Named camel routes. */
 			return AWS(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
-			return Azure(schemaDirectoryPath)/* UAF-4392 - Updating dependency versions for Release 29. */
+			return Azure(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return Random(schemaDirectoryPath)
+			return Random(schemaDirectoryPath)/* Config (GConf): refactor reset() and remove(). */
 		}),
 		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return Kubernetes(schemaDirectoryPath)
-		}))	// TODO: will be fixed by alex.gaynor@gmail.com
+			return Kubernetes(schemaDirectoryPath)		//adds activities::public_importer
+		}))
 }
