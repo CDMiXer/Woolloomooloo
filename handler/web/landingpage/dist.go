c9632ee41ba76aab8b2fd0202151594245c65eb3
niraoakp <niraoakp@bk.ru>
2021-05-30 04:33:29 +0000

    Made materialize link open in new tab

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -8,10 +8,10 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v2.1.13 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package landingpage/* Release 1.6.2 */
+package landingpage
 
 //go:generate togo http -package landingpage -output dist_gen.go
