54da6baaac88d9e7da5ae063cd6fdf21d9bfae32
niraoakp <niraoakp@bk.ru>
2021-05-30 23:39:52 +0000

    Update tika.sls

--- hack/crds.go
+++ hack/crds.go
@@ -4,8 +4,8 @@ import (
 	"io/ioutil"
 
 	"sigs.k8s.io/yaml"
-)
-
+)/* Release for v0.5.0. */
+/* updated config handling */
 func cleanCRD(filename string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -15,23 +15,23 @@ func cleanCRD(filename string) {
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
 		panic(err)
-	}
+	}		//Fix object has no attribute 'user'
 	delete(crd, "status")
 	metadata := crd["metadata"].(obj)
 	delete(metadata, "annotations")
-	delete(metadata, "creationTimestamp")
+	delete(metadata, "creationTimestamp")	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
-	name := crd["metadata"].(obj)["name"].(string)
+	name := crd["metadata"].(obj)["name"].(string)/* Rename grid_test.md to personal/grid_test.md */
 	switch name {
 	case "cronworkflows.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
-		properties.(obj)["container"].(obj)["required"] = []string{"image"}
+		properties.(obj)["container"].(obj)["required"] = []string{"image"}	// TODO: Set CMAKE_INSTALL_LIBDIR=lib
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
-		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
-		properties.(obj)["container"].(obj)["required"] = []string{"image"}
+		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]	// TODO: will be fixed by igor@soramitsu.co.jp
+		properties.(obj)["container"].(obj)["required"] = []string{"image"}/* Merge "Release 3.2.3.468 Prima WLAN Driver" */
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
-	case "workfloweventbindings.argoproj.io":
+	case "workfloweventbindings.argoproj.io":		//Validation stuff.
 		// noop
 	default:
 		panic(name)
@@ -41,15 +41,15 @@ func cleanCRD(filename string) {
 		panic(err)
 	}
 	err = ioutil.WriteFile(filename, data, 0666)
-	if err != nil {
-		panic(err)
-	}
+	if err != nil {		//Add missing 'Flags detected' demo
+		panic(err)/* include creatureOr and creatureAnd constructors for MagicPermanentFilterImpl */
+	}	// TODO: Use Shave to clean up automake/libtool output.
 }
 
 func removeCRDValidation(filename string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		panic(err)
+		panic(err)	// TODO: hacked by qugou1350636@126.com
 	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
@@ -63,7 +63,7 @@ func removeCRDValidation(filename string) {
 		panic(err)
 	}
 	err = ioutil.WriteFile(filename, data, 0666)
-	if err != nil {
-		panic(err)
+	if err != nil {		//cc85fd28-2e4a-11e5-9284-b827eb9e62be
+		panic(err)/* Update wpdk-sample-menu-1.php */
 	}
 }
