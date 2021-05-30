b0f545327eebeca8a9377264cfeaa67ee2cea186
niraoakp <niraoakp@bk.ru>
2021-05-30 05:54:38 +0000

    Update modelgen.rb

--- plugin/registry/combine_test.go
+++ plugin/registry/combine_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package registry
+package registry	// TODO: hacked by julia@jvns.ca
 
 import (
 	"os"
@@ -10,31 +10,31 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)
+)		//Add last list matcher
 
-func TestCombineSources(t *testing.T) {
+func TestCombineSources(t *testing.T) {/* Bugs fixed; Release 1.3rc2 */
 	source := Combine(
 		FileSource("./auths/testdata/config.json"),
 		FileSource("./auths/testdata/config2.json"),
 		FileSource(""), // no source file, must not error
-	)
+	)/* Updated to latest nancy and rc2 (#24) */
 	got, err := source.List(noContext, &core.RegistryArgs{})
-	if err != nil {
+	if err != nil {		//Issue #14 Fixed issues with headings h1 to h6
 		t.Error(err)
-		return
-	}
+		return		//s/problems/exercises/g
+	}	// Update bancospedal2002_15.csv
 	want := []*core.Registry{
 		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
 		},
-		{
+		{/* Update attribute display in popover */
 			Address:  "https://gcr.io",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
-	}
+		},/* Fixes #766 - Release tool: doesn't respect bnd -diffignore instruction */
+	}	// TODO: Create Low-write.md
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
