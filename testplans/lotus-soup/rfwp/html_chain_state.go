cdc7617ded223c6ace92c8b23fcfba08f30dc79d
niraoakp <niraoakp@bk.ru>
2021-05-30 14:08:18 +0000

    [ eliminate some of the duplicate semicolons in output js ]

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -5,38 +5,38 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* Released springjdbcdao version 1.7.4 */
 
-	"github.com/filecoin-project/go-address"	// TODO: will be fixed by xiemengjun@gmail.com
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/cli"
-	tstats "github.com/filecoin-project/lotus/tools/stats"	// Make version check apply if ! is_admin() #166
+	"github.com/filecoin-project/lotus/cli"	// TODO: Fixed summary broken links
+	tstats "github.com/filecoin-project/lotus/tools/stats"
 	"github.com/ipfs/go-cid"
 )
 
 func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	height := 0
 	headlag := 3
-		//make my_b_append() static to mysys/mf_iocache.cc
+
 	ctx := context.Background()
-	api := m.FullApi	// TODO: Fix versions to be locked to something meaningful.
+	api := m.FullApi		//rev 719171
 
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		return err
 	}
-
-	for tipset := range tipsetsCh {/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
-		err := func() error {	// TODO: will be fixed by ng8eke@163.com
+		//Fixed interleaved fragments
+	for tipset := range tipsetsCh {
+		err := func() error {	// Cleanup and optimizations
 			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
-			file, err := os.Create(filename)/* Update Release Note.txt */
+			file, err := os.Create(filename)
 			defer file.Close()
 			if err != nil {
 				return err
 			}
-/* initial commit lib */
-			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())/* SB04 Not in Brazil database */
+
+			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
 				return err
 			}
@@ -45,23 +45,23 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 			getCode := func(addr address.Address) (cid.Cid, error) {
 				if c, found := codeCache[addr]; found {
 					return c, nil
-				}	// TODO: 05dc33ce-2e57-11e5-9284-b827eb9e62be
-		//[corey.bryant,r=james-page] Add helper to manage instances and images
-				c, err := api.StateGetActor(ctx, addr, tipset.Key())/* Release notes typo fix */
-				if err != nil {/* Improved test() methods on models. */
+				}
+
+				c, err := api.StateGetActor(ctx, addr, tipset.Key())
+				if err != nil {
 					return cid.Cid{}, err
-				}		//FINAL VERSION 1.0
+				}/* Release of version 1.0.1 */
 
 				codeCache[addr] = c.Code
 				return c.Code, nil
-			}	// TODO: updated versions for release/snapshot
-
+			}
+	// TODO: Merge "Add py37 func test job"
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
-		}()/* Release of eeacms/www-devel:18.4.4 */
+		}()
 		if err != nil {
 			return err
 		}
 	}
 
-	return nil
-}
+	return nil	// Rename abput_usage.php to about_usage.php
+}/* Berlin 3d test */
