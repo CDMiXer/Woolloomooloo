eb2271c80dfde955072b07b82db4e47d7c524fb3
niraoakp <niraoakp@bk.ru>
2021-05-30 14:22:59 +0000

    Make sure new units created are visible

--- store/stage/type.go
+++ store/stage/type.go
@@ -1,43 +1,43 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by timnugent@gmail.com
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* Update roda to version 3.33.0 */
+///* Updating ChangeLog For 0.57 Alpha 2 Dev Release */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// adicionado verificação de tipo em formatar_cep
-// limitations under the License.	// TODO: Duplicate javadocs.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package stage
 
 import (
 	"database/sql"
 
-	"github.com/drone/drone/core"/* Release version [10.3.3] - alfter build */
+	"github.com/drone/drone/core"		//Add script to run development server
 )
 
-type nullStep struct {
+type nullStep struct {/* Merge "Release 1.0.0.204 QCACLD WLAN Driver" */
 	ID        sql.NullInt64
 	StageID   sql.NullInt64
-	Number    sql.NullInt64
-	Name      sql.NullString
-	Status    sql.NullString/* Updated install instructions for required Kafka version */
+	Number    sql.NullInt64	// TODO: hacked by ligi@ligi.de
+	Name      sql.NullString		//Fixes #2722 by changing an aspect
+	Status    sql.NullString
 	Error     sql.NullString
 	ErrIgnore sql.NullBool
-	ExitCode  sql.NullInt64/* Tests works now */
+	ExitCode  sql.NullInt64	// Real driver.
 	Started   sql.NullInt64
 	Stopped   sql.NullInt64
 	Version   sql.NullInt64
-}
+}	// TODO: Merge "Fix notification ticker info text alignment."
 
 func (s *nullStep) value() *core.Step {
 	return &core.Step{
-		ID:        s.ID.Int64,/* delete unneeded let */
-		StageID:   s.StageID.Int64,
+		ID:        s.ID.Int64,
+		StageID:   s.StageID.Int64,/* BFXDEV-491, missing argument to re.sub for the SINGLE_END STAR commends case */
 		Number:    int(s.Number.Int64),
 		Name:      s.Name.String,
 		Status:    s.Status.String,
@@ -45,7 +45,7 @@ func (s *nullStep) value() *core.Step {
 		ErrIgnore: s.ErrIgnore.Bool,
 		ExitCode:  int(s.ExitCode.Int64),
 		Started:   s.Started.Int64,
-		Stopped:   s.Stopped.Int64,
+		Stopped:   s.Stopped.Int64,	// TODO: will be fixed by hugomrdias@gmail.com
 		Version:   s.Version.Int64,
-	}
-}	// TODO: will be fixed by jon@atack.com
+	}/* ChangeLog and Release Notes updates */
+}
