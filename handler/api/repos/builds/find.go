d60148400af34e01c2253912ad40fcd603e29089
niraoakp <niraoakp@bk.ru>
2021-05-30 20:32:59 +0000

    Correctly handle non uniform bounding box volumes

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,61 +1,61 @@
-// Copyright 2019 Drone IO, Inc.	// removing the .apk ignore temporarily to commit the apk that I have
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/forests-frontend:2.0-beta.64 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// rename function cache member
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package builds/* o Release aspectj-maven-plugin 1.4. */
+package builds
 
 import (
 	"net/http"
-	"strconv"		//Merge "Support requesting claims as of a particular time"
+	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-/* update ber for fsk */
+
 	"github.com/go-chi/chi"
-)	// fixing conventions
+)
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// build details to the the response body.	// TODO: 240e467a-2e6b-11e5-9284-b827eb9e62be
+// build details to the the response body.
 func HandleFind(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-{ cnuFreldnaH.ptth )
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)	// TODO: hacked by qugou1350636@126.com
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}/* Issue #1115596 by joachim: Changed GUI to use sanity check. */
-		repo, err := repos.FindName(r.Context(), namespace, name)		//add command line mode to gfa2fastg.py
+		}
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* kvm: save and restore host/guest FP registers */
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)/* ARMv5 bot in Release mode */
+		}
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Updated Release Notes for the upcoming 0.9.10 release */
+			return
 		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}/* Ignore CDT Release directory */
+		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
 }
