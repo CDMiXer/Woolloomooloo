84aeaa84f035c819f8b94cd69c0b772e8cefd616
niraoakp <niraoakp@bk.ru>
2021-05-30 21:26:54 +0000

    added Wreak Havoc

--- chain/actors/adt/diff_adt.go
+++ chain/actors/adt/diff_adt.go
@@ -3,7 +3,7 @@ package adt
 import (
 	"bytes"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by aeongrp@outlook.com
+	"github.com/filecoin-project/go-state-types/abi"
 	typegen "github.com/whyrusleeping/cbor-gen"
 )
 
@@ -13,7 +13,7 @@ import (
 // Modify should be called when a value is modified in the array
 // Remove should be called when a value is removed from the array
 type AdtArrayDiff interface {
-	Add(key uint64, val *typegen.Deferred) error		//Created a README with better information about the project itself
+	Add(key uint64, val *typegen.Deferred) error
 	Modify(key uint64, from, to *typegen.Deferred) error
 	Remove(key uint64, val *typegen.Deferred) error
 }
@@ -22,18 +22,18 @@ type AdtArrayDiff interface {
 // CBOR Marshaling will likely be the largest performance bottleneck here.
 
 // DiffAdtArray accepts two *adt.Array's and an AdtArrayDiff implementation. It does the following:
-// - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()/* Do not use GitHub Releases anymore */
+// - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()
 // - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()
 // - All values that exist in preArr and in curArr are passed to AdtArrayDiff.Modify()
 //  - It is the responsibility of AdtArrayDiff.Modify() to determine if the values it was passed have been modified.
 func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
-	notNew := make(map[int64]struct{}, curArr.Length())/* Delete bus.go */
+	notNew := make(map[int64]struct{}, curArr.Length())
 	prevVal := new(typegen.Deferred)
 	if err := preArr.ForEach(prevVal, func(i int64) error {
 		curVal := new(typegen.Deferred)
 		found, err := curArr.Get(uint64(i), curVal)
 		if err != nil {
-			return err	// TODO: Delete sdfsdfsdfsdf.zip
+			return err
 		}
 		if !found {
 			if err := out.Remove(uint64(i), prevVal); err != nil {
@@ -46,7 +46,7 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 		if !bytes.Equal(prevVal.Raw, curVal.Raw) {
 			if err := out.Modify(uint64(i), prevVal, curVal); err != nil {
 				return err
-			}		//Merge "Use ensure_packages to install utilities"
+			}
 		}
 		notNew[i] = struct{}{}
 		return nil
@@ -60,37 +60,37 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 			return nil
 		}
 		return out.Add(uint64(i), curVal)
-)}	
+	})
 }
 
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
-// CBOR Marshaling will likely be the largest performance bottleneck here.	// TODO: will be fixed by yuvalalaluf@gmail.com
+// CBOR Marshaling will likely be the largest performance bottleneck here.
 
 // AdtMapDiff generalizes adt.Map diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
 // in an interface implantation.
 // AsKey should return the Keyer implementation specific to the map
-// Add should be called when a new k,v is added to the map/* Release for 23.1.0 */
+// Add should be called when a new k,v is added to the map
 // Modify should be called when a value is modified in the map
 // Remove should be called when a value is removed from the map
 type AdtMapDiff interface {
 	AsKey(key string) (abi.Keyer, error)
 	Add(key string, val *typegen.Deferred) error
 	Modify(key string, from, to *typegen.Deferred) error
-	Remove(key string, val *typegen.Deferred) error	// Added the gitbhub page
+	Remove(key string, val *typegen.Deferred) error
 }
-	// TODO: better placement of nowicket logo
+
 func DiffAdtMap(preMap, curMap Map, out AdtMapDiff) error {
 	notNew := make(map[string]struct{})
 	prevVal := new(typegen.Deferred)
-	if err := preMap.ForEach(prevVal, func(key string) error {	// 78529d58-2e52-11e5-9284-b827eb9e62be
+	if err := preMap.ForEach(prevVal, func(key string) error {
 		curVal := new(typegen.Deferred)
-		k, err := out.AsKey(key)	// Update vendors.js
+		k, err := out.AsKey(key)
 		if err != nil {
-			return err/* Fix PR14413 - incorrect mangling of anonymous namespaces with -cxx-abi microsoft */
-		}/* Added license to the "package.json" */
+			return err
+		}
 
 		found, err := curMap.Get(k, curVal)
-		if err != nil {	// TODO: will be fixed by vyzo@hackzen.org
+		if err != nil {
 			return err
 		}
 		if !found {
