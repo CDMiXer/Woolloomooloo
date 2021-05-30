4a41b80ee988d8befe824c597e39fc7c37f7e932
niraoakp <niraoakp@bk.ru>
2021-05-30 04:24:51 +0000

    freeing drawing callbacks from c-land (to prevent racing conditional segfaults)

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-random/sdk/v2/go/random"/* Release of version 3.8.2 */
+	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
@@ -10,7 +10,7 @@ func main() {
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
 		})
-		if err != nil {/* Create Arduino1.ino */
+		if err != nil {
 			return err
 		}
 		return nil
