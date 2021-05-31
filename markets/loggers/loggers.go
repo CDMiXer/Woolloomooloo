23ec089b61e343626a1c3b5f755c8870385f71b0
niraoakp <niraoakp@bk.ru>
2021-05-31 18:18:22 +0000

    Fixed attachment application bug and added missing modifiers.

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -8,37 +8,37 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("markets")
+var log = logging.Logger("markets")/* Recache playermovechecks after reloads */
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
-
+	// TODO: Move some code into a helper function. NFC.
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
-
+/* fix a careless mistake */
 // RetrievalClientLogger logs events from the retrieval client
-func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
+func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {/* FIX Bad error and style message when changing its own login */
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-
+/* Removing old JS file */
 // RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}
+}		//Rename toastpopup-demo.html to index.html
 
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
-	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],
+	log.Debugw("data transfer event",		//Update and rename MQTT.MD to MQTT.md
+		"name", datatransfer.Events[event.Code],/* Release jedipus-2.6.39 */
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
-		"channel ID", state.ChannelID(),
+		"channel ID", state.ChannelID(),		//20a05da8-2ece-11e5-905b-74de2bd44bed
 		"sent", state.Sent(),
-		"received", state.Received(),
+		"received", state.Received(),		//Merge branch 'master' of https://github.com/j-dornbusch/scolomfr-recette.git
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
@@ -46,16 +46,16 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 		"event message", event.Message,
 		"channel message", state.Message())
 }
-
+/* [artifactory-release] Release version 3.9.0.RC1 */
 // ReadyLogger returns a function to log the results of module initialization
-func ReadyLogger(module string) func(error) {
+func ReadyLogger(module string) func(error) {/* Fix indentation in #dd */
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
 		} else {
 			log.Infow("module ready", "module", module)
 		}
-	}
+	}	// TODO: Make StringItem for temperature Sensors
 }
 
 type RetrievalEvent struct {
