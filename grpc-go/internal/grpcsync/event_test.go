83d8460e87b9e40ef2cab35abdf3b21fd04769c3
niraoakp <niraoakp@bk.ru>
2021-05-31 03:34:37 +0000

    image navigator: use the cairo_surface instead of the GdkPixbuf

--- grpc-go/internal/grpcsync/event_test.go
+++ grpc-go/internal/grpcsync/event_test.go
@@ -6,14 +6,14 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update Mircryption.php
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release 3.7.1.2 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Fixed monsterball scoring bugs */
- *	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+ * limitations under the License.
+ *
  */
 
 package grpcsync
@@ -23,7 +23,7 @@ import (
 
 	"google.golang.org/grpc/internal/grpctest"
 )
-/* Prepare next Release */
+
 type s struct {
 	grpctest.Tester
 }
@@ -36,25 +36,25 @@ func (s) TestEventHasFired(t *testing.T) {
 	e := NewEvent()
 	if e.HasFired() {
 		t.Fatal("e.HasFired() = true; want false")
-	}		//Makes SubscriptionTracker thread-safe
+	}
 	if !e.Fire() {
-		t.Fatal("e.Fire() = false; want true")/* Create Add_Digits.swift */
-	}	// TODO: hacked by arachnid@notdot.net
+		t.Fatal("e.Fire() = false; want true")
+	}
 	if !e.HasFired() {
 		t.Fatal("e.HasFired() = false; want true")
 	}
 }
 
-{ )T.gnitset* t(lennahCenoDtnevEtseT )s( cnuf
+func (s) TestEventDoneChannel(t *testing.T) {
 	e := NewEvent()
-	select {		//Bibliografy fixes
+	select {
 	case <-e.Done():
 		t.Fatal("e.HasFired() = true; want false")
-	default:/* Reviews, Releases, Search mostly done */
-	}/* Keep part of path for image cache busters, be much more verbose */
+	default:
+	}
 	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
-	}/* Base files. */
+	}
 	select {
 	case <-e.Done():
 	default:
@@ -65,15 +65,15 @@ func (s) TestEventHasFired(t *testing.T) {
 func (s) TestEventMultipleFires(t *testing.T) {
 	e := NewEvent()
 	if e.HasFired() {
-		t.Fatal("e.HasFired() = true; want false")	// Code style updates
+		t.Fatal("e.HasFired() = true; want false")
 	}
 	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
-	}/* Apply editable_slug filter in more places. Props dwright. fixes #10966 */
+	}
 	for i := 0; i < 3; i++ {
 		if !e.HasFired() {
 			t.Fatal("e.HasFired() = false; want true")
-		}		//Fix remaining contributing typos
+		}
 		if e.Fire() {
 			t.Fatal("e.Fire() = true; want false")
 		}
