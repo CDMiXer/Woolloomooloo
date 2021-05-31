8fd5cf9f3b1cbb1186e893507fd398c19e3d87f1
niraoakp <niraoakp@bk.ru>
2021-05-31 01:35:52 +0000

    README.md edited to reflect current development efforts

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -7,7 +7,7 @@ resource siteBucket "aws:s3:Bucket" {
 
 siteDir = "www" // directory for content files
 
-// For each file in the directory, create an S3 object stored in `siteBucket`	// TODO: Merge "Raise Exception when the paramter is invalid"
+// For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
     options {
 		range = readDir(siteDir)
@@ -15,7 +15,7 @@ resource files "aws:s3:BucketObject" {
 
 	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
-	// TODO: hacked by joshua@yottadb.com
+
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
 }
@@ -25,17 +25,17 @@ resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
 	// The policy is JSON-encoded.
-	policy = toJSON({
+	policy = toJSON({/* Updated models (markdown) */
 		Version = "2012-10-17"
 		Statement = [{
-			Effect = "Allow"
-			Principal = "*"
-			Action = [ "s3:GetObject" ]/* Fix failing system test for sample data change */
-			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
-		}]
+			Effect = "Allow"		//reword comment; should soon be replaced
+			Principal = "*"/* Synch patchlevel in Makefile w/ `Release' tag in spec file. */
+			Action = [ "s3:GetObject" ]	// TODO: will be fixed by qugou1350636@126.com
+			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]	// TODO: Refactoring: CSS kommt jetzt aus dem Portlet
+		}]		//Delete jquery.wysiwyg.gif
 	})
-}
-
+}		//chore(package): update @types/jasmine to version 2.5.51
+/* command guild member can show levels above 99 now. */
 // Stack outputs
 output bucketName { value = siteBucket.bucket }
-output websiteUrl { value = siteBucket.websiteEndpoint }/* Changed some of the listening logic */
+output websiteUrl { value = siteBucket.websiteEndpoint }
