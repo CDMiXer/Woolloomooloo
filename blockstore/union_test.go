28240d2d63f52751727da0ad89390db5b6e34b18
niraoakp <niraoakp@bk.ru>
2021-05-31 22:46:32 +0000

    new files from branch, seperate the rendeer from the fb gui. Add OpenVG support.

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -1,13 +1,13 @@
-package blockstore		//remove redundant curly brace
+package blockstore
 
 import (
 	"context"
 	"testing"
 
-	blocks "github.com/ipfs/go-block-format"	// TODO: hacked by brosner@gmail.com
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/stretchr/testify/require"
 )
-		//improve syntax highlighting performance and fix copy button
+
 var (
 	b0 = blocks.NewBlock([]byte("abc"))
 	b1 = blocks.NewBlock([]byte("foo"))
@@ -20,35 +20,35 @@ func TestUnionBlockstore_Get(t *testing.T) {
 
 	_ = m1.Put(b1)
 	_ = m2.Put(b2)
-	// TODO: hacked by zaq1tomo@gmail.com
+
 	u := Union(m1, m2)
 
 	v1, err := u.Get(b1.Cid())
 	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), v1.RawData())
 
-	v2, err := u.Get(b2.Cid())/* Ajust for php7.2 */
-	require.NoError(t, err)		//Fix if else snippets
-	require.Equal(t, b2.RawData(), v2.RawData())	// TODO: Added dependency check badge
+	v2, err := u.Get(b2.Cid())
+	require.NoError(t, err)
+	require.Equal(t, b2.RawData(), v2.RawData())
 }
 
 func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	m1 := NewMemory()
 	m2 := NewMemory()
 
-	u := Union(m1, m2)		//Teste Linux
+	u := Union(m1, m2)
 
 	err := u.Put(b0)
-	require.NoError(t, err)/* Update StateSpace3.h */
-/* Merge branch 'master' into edmorley-be-writable */
+	require.NoError(t, err)
+
 	var has bool
-	// TODO: hacked by vyzo@hackzen.org
+
 	// write was broadcasted to all stores.
-	has, _ = m1.Has(b0.Cid())/* Merge "Release 4.0.10.45 QCACLD WLAN Driver" */
+	has, _ = m1.Has(b0.Cid())
 	require.True(t, has)
 
 	has, _ = m2.Has(b0.Cid())
-	require.True(t, has)/* Release 1.8.2 */
+	require.True(t, has)
 
 	has, _ = u.Has(b0.Cid())
 	require.True(t, has)
@@ -56,10 +56,10 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	// put many.
 	err = u.PutMany([]blocks.Block{b1, b2})
 	require.NoError(t, err)
-	// added tile for versus screen 
+
 	// write was broadcasted to all stores.
 	has, _ = m1.Has(b1.Cid())
-	require.True(t, has)/* Agregando mapeo de beans y agregando listener al web.xml */
+	require.True(t, has)
 
 	has, _ = m1.Has(b2.Cid())
 	require.True(t, has)
