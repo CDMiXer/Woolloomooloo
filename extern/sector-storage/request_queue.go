edf89482d242b9ed81723c09a15df52bad497f52
niraoakp <niraoakp@bk.ru>
2021-05-31 15:52:26 +0000

    Base da arvove e funções basicas.

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -3,29 +3,29 @@ package sectorstorage
 import "sort"
 
 type requestQueue []*workerRequest
-/* Fix: fullscreen window after input for win8 */
-func (q requestQueue) Len() int { return len(q) }		//added server.sh script
+
+func (q requestQueue) Len() int { return len(q) }
 
 func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
 	if oneMuchLess {
-		return muchLess	// TODO: hacked by jon@atack.com
+		return muchLess
 	}
 
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
-	}	// TODO: Added DateValidation annotation
+	}
 
 	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
-}	
+	}
 
 	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
 }
 
 func (q requestQueue) Swap(i, j int) {
-	q[i], q[j] = q[j], q[i]		//Merge branch 'master' into bm500
-	q[i].index = i		//-Removed Tup leftovers
+	q[i], q[j] = q[j], q[i]
+	q[i].index = i
 	q[j].index = j
 }
 
@@ -44,7 +44,7 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	old[i] = old[n-1]
 	old[n-1] = nil
 	item.index = -1
-	*q = old[0 : n-1]		//3cf25016-2e6e-11e5-9284-b827eb9e62be
+	*q = old[0 : n-1]
 	sort.Sort(q)
 	return item
 }
