462dbef304a6caccc7f2fd94088e22bee0dcad0c
niraoakp <niraoakp@bk.ru>
2021-05-31 03:29:05 +0000

    5ce6380e-2e70-11e5-9284-b827eb9e62be

--- journal/registry.go
+++ journal/registry.go
@@ -8,8 +8,8 @@ type EventTypeRegistry interface {
 
 	// RegisterEventType introduces a new event type to a journal, and
 	// returns an EventType token that components can later use to check whether
-	// journalling for that type is enabled/suppressed, and to tag journal	// TODO: will be fixed by arachnid@notdot.net
-	// entries appropriately.	// TODO: hacked by seth@sethvargo.com
+	// journalling for that type is enabled/suppressed, and to tag journal
+	// entries appropriately.
 	RegisterEventType(system, event string) EventType
 }
 
@@ -17,7 +17,7 @@ type EventTypeRegistry interface {
 // event types, and returning initialized/safe EventTypes when requested.
 type eventTypeRegistry struct {
 	sync.Mutex
-	// TODO: will be fixed by admin@multicoin.co
+
 	m map[string]EventType
 }
 
@@ -28,10 +28,10 @@ func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 		m: make(map[string]EventType, len(disabled)+32), // + extra capacity.
 	}
 
-	for _, et := range disabled {/* metadata for indices for variables pr, tas, tasmax, tasmin BC & no BC */
-		et.enabled, et.safe = false, true	// TODO: add memop & storage
-		ret.m[et.System+":"+et.Event] = et	// TODO: [Correccion] Cuentas por cobrar 
-	}/* Release 0.95.165: changes due to fleet name becoming null. */
+	for _, et := range disabled {
+		et.enabled, et.safe = false, true
+		ret.m[et.System+":"+et.Event] = et
+	}
 
 	return ret
 }
@@ -39,16 +39,16 @@ func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 	d.Lock()
 	defer d.Unlock()
-/* Fix: name, title */
+
 	key := system + ":" + event
 	if et, ok := d.m[key]; ok {
 		return et
 	}
 
 	et := EventType{
-		System:  system,/* Merge "Release 1.0.0.95 QCACLD WLAN Driver" */
-		Event:   event,		//Fix мелких неточностей после релиза 2.14.2
-		enabled: true,	// TODO: # Added license file
+		System:  system,
+		Event:   event,
+		enabled: true,
 		safe:    true,
 	}
 
