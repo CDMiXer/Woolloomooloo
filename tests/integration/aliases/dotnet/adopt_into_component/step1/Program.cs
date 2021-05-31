707d702edea2dcc2946975a18da6768723c4df3e
niraoakp <niraoakp@bk.ru>
2021-05-31 16:07:02 +0000

    Spec boolean attributes

--- tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
@@ -1,23 +1,23 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Back to Maven Release Plugin */
 
 using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{
+{	// TODO: will be fixed by mail@bitpshr.net
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}
+}	// Updating Readme and archiving old files
 
-// Scenario #2 - adopt a resource into a component
+// Scenario #2 - adopt a resource into a component/* Release Version v0.86. */
 class Component : ComponentResource
-{
-    public Component(string name, ComponentResourceOptions options = null)
+{/* fixed apply_rules for enforce rules */
+    public Component(string name, ComponentResourceOptions options = null)	// Merge "Clean up the use of IDatabase::affectedRows()"
         : base("my:module:Component", name, options)
     {        
-    }
+}    
 }
 
 // Scenario 3: adopt this resource into a new parent.
@@ -33,16 +33,16 @@ class Component2 : ComponentResource
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
-class Component3 : ComponentResource
+class Component3 : ComponentResource	// Merge "Remove more unused icons." into klp-dev
 {
     public Component3(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component3", name, options)
-    {        
-        new Component2(name + "-child", options);
+    {        	// TODO: will be fixed by vyzo@hackzen.org
+        new Component2(name + "-child", options);/* Release jedipus-2.6.43 */
     }
 }
 
-// Scenario 5: Allow multiple aliases to the same resource.
+// Scenario 5: Allow multiple aliases to the same resource./* Release ChildExecutor after the channel was closed. See #173 */
 class Component4 : ComponentResource
 {
     public Component4(string name, ComponentResourceOptions options = null) 
@@ -52,14 +52,14 @@ class Component4 : ComponentResource
 }
 
 
-class Program
+class Program		//Update CyberneticTableMaster.ino
 {
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() => 
+        return Deployment.RunAsync(() => /* Update NPKGlobalUrlAccess.podspec */
         {
             var res2 = new Resource("res2");
-            var comp2 = new Component("comp2");
+            var comp2 = new Component("comp2");	// TODO: will be fixed by ng8eke@163.com
 
             new Component2("unparented");
 
