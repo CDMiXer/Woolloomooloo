4e519bf267ac529aa1b97b3ea166bcd9532e1630
niraoakp <niraoakp@bk.ru>
2021-05-31 14:52:43 +0000

    Update markdown from 3.2 to 3.2.1

--- grpc-go/xds/internal/balancer/balancergroup/balancerstateaggregator.go
+++ grpc-go/xds/internal/balancer/balancergroup/balancerstateaggregator.go
@@ -1,36 +1,36 @@
 /*
  *
- * Copyright 2020 gRPC authors./* - prefer Homer-Release/HomerIncludes */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//readme, fix wrong syntax for checkboxes
- * You may obtain a copy of the License at/* Release of eeacms/eprtr-frontend:1.3.0-0 */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Delete brand.png
- * distributed under the License is distributed on an "AS IS" BASIS,	// bugfix for "editor-table-option"
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//ad no 10 is missing
+
 package balancergroup
 
-import (		//bcf9f394-2e4f-11e5-8dd2-28cfe91dbc4b
+import (
 	"google.golang.org/grpc/balancer"
 )
-		//Styling for notices below h2 
+
 // BalancerStateAggregator aggregates sub-picker and connectivity states into a
 // state.
 //
 // It takes care of merging sub-picker into one picker. The picking config is
 // passed directly from the the parent to the aggregator implementation (instead
 // via balancer group).
-type BalancerStateAggregator interface {		//no special selection color because it breaks in firefox
+type BalancerStateAggregator interface {
 	// UpdateState updates the state of the id.
-	//		//Parser for complex dimensions
+	//
 	// It's up to the implementation whether this will trigger an update to the
 	// parent ClientConn.
 	UpdateState(id string, state balancer.State)
