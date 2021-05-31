220b18a4143410eda670d763ec2e006ba2476b5b
niraoakp <niraoakp@bk.ru>
2021-05-31 09:45:46 +0000

    reverting back to snapshot as seems to be required to progress release

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-/* Release version 0.26 */
-CREATE TABLE IF NOT EXISTS secrets (
+
+CREATE TABLE IF NOT EXISTS secrets (/* Update lista1.5_questao22.py */
  secret_id                SERIAL PRIMARY KEY
 ,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)/* disentanglement of configuration */
+,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)
+,secret_pull_request_push BOOLEAN	// TODO: hacked by mail@bitpshr.net
+,UNIQUE(secret_repo_id, secret_name)	// TODO: bits, fix bit width reinterpretation
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);		//Correcting linter errors
+);
 
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-/* Release of eeacms/www:19.9.11 */
+
 -- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
