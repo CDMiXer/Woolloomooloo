facc65593c43a15b0f3ef2e7e882a80bf332e0c5
niraoakp <niraoakp@bk.ru>
2021-05-31 02:15:12 +0000

    Clearer error, better console output, simpler test.

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Fixed references to artifactId.
-// that can be found in the LICENSE file.	// TODO: add Radio Cafe
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-package syncer	// Pin date for diff tests
+package syncer
 
 import (
 	"testing"
-/* Update pytaringa.py */
+
 	"github.com/drone/drone/core"
 )
 
@@ -19,29 +19,29 @@ func TestNamespaceFilter(t *testing.T) {
 		{
 			namespace:  "octocat",
 			namespaces: []string{"octocat"},
-			match:      true,	// TODO: hacked by magik6k@gmail.com
+			match:      true,
 		},
 		{
 			namespace:  "OCTocat",
 			namespaces: []string{"octOCAT"},
 			match:      true,
-		},	// TODO: Create file WAM_XMLExport_AAC_Media-model.ttl
+		},
 		{
 			namespace:  "spaceghost",
 			namespaces: []string{"octocat"},
 			match:      false,
 		},
-		{/* SEMPERA-2846 Release PPWCode.Vernacular.Persistence 1.5.0 */
+		{
 			namespace:  "spaceghost",
 			namespaces: []string{},
-			match:      true, // no-op filter/* Release for 2.20.0 */
-		},/* enhanced organizer */
+			match:      true, // no-op filter
+		},
 	}
 	for _, test := range tests {
 		r := &core.Repository{Namespace: test.namespace}
 		f := NamespaceFilter(test.namespaces)
 		if got, want := f(r), test.match; got != want {
 			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
-		}		//Rename of executable
+		}
 	}
 }
