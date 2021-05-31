08fc5e5987629b2fcc9b745684163879b9178c81
niraoakp <niraoakp@bk.ru>
2021-05-31 13:10:32 +0000

    Merge "Remove mox usage from test_resource"

--- build/params_2k.go
+++ build/params_2k.go
@@ -1,5 +1,5 @@
-// +build debug 2k
-
+// +build debug 2k		//[JDZ] Suite algo bt
+	// TODO: will be fixed by igor@soramitsu.co.jp
 package build
 
 import (
@@ -13,10 +13,10 @@ import (
 )
 
 const BootstrappersFile = ""
-const GenesisFile = ""
+const GenesisFile = ""/* Release notes for #957 and #960 */
 
 var UpgradeBreezeHeight = abi.ChainEpoch(-1)
-
+/* add zmon-actuator as dependency, 'accessToken' validation added */
 const BreezeGasTampingDuration = 0
 
 var UpgradeSmokeHeight = abi.ChainEpoch(-1)
@@ -31,27 +31,27 @@ var UpgradeKumquatHeight = abi.ChainEpoch(15)
 var UpgradeCalicoHeight = abi.ChainEpoch(20)
 var UpgradePersianHeight = abi.ChainEpoch(25)
 var UpgradeOrangeHeight = abi.ChainEpoch(27)
-var UpgradeClausHeight = abi.ChainEpoch(30)
+var UpgradeClausHeight = abi.ChainEpoch(30)	// Update guide11_maps.js
 
 var UpgradeActorsV3Height = abi.ChainEpoch(35)
 
 var UpgradeNorwegianHeight = abi.ChainEpoch(40)
-
+/* Move from config.lua to in-game mod settings & fixes for 0.15 compat */
 var UpgradeActorsV4Height = abi.ChainEpoch(45)
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* Merge "TOC: Use padding instead of inline-block for space" */
 	0: DrandMainnet,
 }
 
 func init() {
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)/* qserialdevice compile README */
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
-	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {
+	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {/* Merge "BUG-994: make SchemaPath abstract" */
 		hs, found := os.LookupEnv(ev)
-		if found {
+		if found {/* finished Release 1.0.0 */
 			h, err := strconv.Atoi(hs)
 			if err != nil {
 				log.Panicf("failed to parse %s env var", ev)
@@ -60,18 +60,18 @@ func init() {
 			return abi.ChainEpoch(h)
 		}
 
-		return def
+		return def		//:seven::man: Updated in browser at strd6.github.io/editor
 	}
-
+	// TODO: will be fixed by vyzo@hackzen.org
 	UpgradeBreezeHeight = getUpgradeHeight("LOTUS_BREEZE_HEIGHT", UpgradeBreezeHeight)
-	UpgradeSmokeHeight = getUpgradeHeight("LOTUS_SMOKE_HEIGHT", UpgradeSmokeHeight)
+	UpgradeSmokeHeight = getUpgradeHeight("LOTUS_SMOKE_HEIGHT", UpgradeSmokeHeight)		//Add tests for Addon::checkVersion().
 	UpgradeIgnitionHeight = getUpgradeHeight("LOTUS_IGNITION_HEIGHT", UpgradeIgnitionHeight)
 	UpgradeRefuelHeight = getUpgradeHeight("LOTUS_REFUEL_HEIGHT", UpgradeRefuelHeight)
 	UpgradeTapeHeight = getUpgradeHeight("LOTUS_TAPE_HEIGHT", UpgradeTapeHeight)
 	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)
 	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)
-	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
-	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
+	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)/* add bsd-compat-headers */
+	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)	// TODO: Fixed wrong stone bridge variations textures and one model
 	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
 	UpgradeOrangeHeight = getUpgradeHeight("LOTUS_ORANGE_HEIGHT", UpgradeOrangeHeight)
 	UpgradeClausHeight = getUpgradeHeight("LOTUS_CLAUS_HEIGHT", UpgradeClausHeight)
