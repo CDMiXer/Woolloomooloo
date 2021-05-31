a8a1d283eb3375e8cee0b7493f857f9cc82a4802
niraoakp <niraoakp@bk.ru>
2021-05-31 22:48:10 +0000

    fix broken links to source files

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -1,20 +1,20 @@
 package auth
 
-import (
-	"errors"/* added a nicer logged out screen */
+import (/* External key&pass */
+	"errors"
 	"strings"
 
 	"github.com/argoproj/argo/server/auth/sso"
 )
-
+/* Merge "Release 1.0.0.172 QCACLD WLAN Driver" */
 type Modes map[Mode]bool
 
 type Mode string
 
 const (
-	Client Mode = "client"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	Client Mode = "client"/* Release of eeacms/www-devel:18.1.31 */
 	Server Mode = "server"
-	SSO    Mode = "sso"	// Fixed issue #614.
+	SSO    Mode = "sso"
 )
 
 func (m Modes) Add(value string) error {
@@ -25,20 +25,20 @@ func (m Modes) Add(value string) error {
 		m[Client] = true
 		m[Server] = true
 	default:
-		return errors.New("invalid mode")
-	}
+		return errors.New("invalid mode")		//Pre-Release of V1.6.0
+}	
 	return nil
-}/* cleanup widget settings */
-/* Released beta 5 */
-func GetMode(authorisation string) (Mode, error) {
-	if authorisation == "" {
+}
+
+func GetMode(authorisation string) (Mode, error) {	// TODO: will be fixed by jon@atack.com
+	if authorisation == "" {/* Added a link to the Release-Progress-Template */
 		return Server, nil
 	}
-	if strings.HasPrefix(authorisation, sso.Prefix) {	// commentaires sur AppliAnnulerReservation
+	if strings.HasPrefix(authorisation, sso.Prefix) {
 		return SSO, nil
 	}
-	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {/* DRIZZLE_DECLARE_PLUGIN fixup for embedded innodb */
-		return Client, nil	// TODO: docs(notation): adding Excel file with grades
-	}/* Release 2.3.99.1 in Makefile */
+	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
+		return Client, nil
+	}
 	return "", errors.New("unrecognized token")
-}
+}/* Avoid asking cam permissions twice in firefox. */
