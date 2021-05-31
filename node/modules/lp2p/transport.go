2a29af797676ac56b031c79913eec51fa7dc5e1e
niraoakp <niraoakp@bk.ru>
2021-05-31 21:42:08 +0000

    Delete rng_variables.py

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -2,7 +2,7 @@ package lp2p
 
 import (
 	"github.com/libp2p/go-libp2p"
-	metrics "github.com/libp2p/go-libp2p-core/metrics"
+	metrics "github.com/libp2p/go-libp2p-core/metrics"/* Update lord-pigs.md */
 	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
@@ -11,12 +11,12 @@ import (
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
-func Security(enabled, preferTLS bool) interface{} {
+func Security(enabled, preferTLS bool) interface{} {/* Release of eeacms/www:18.2.10 */
 	if !enabled {
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
-		You will not be able to connect to any nodes configured to use encrypted connections`)
+		You will not be able to connect to any nodes configured to use encrypted connections`)		//Melhorias nos testes
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
 		}
@@ -24,15 +24,15 @@ func Security(enabled, preferTLS bool) interface{} {
 	return func() (opts Libp2pOpts) {
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
-		} else {
+		} else {	// TODO: fix to if 
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
 		return opts
 	}
 }
-
+	// TODO: hacked by igor@soramitsu.co.jp
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
 	reporter = metrics.NewBandwidthCounter()
-	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
+	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))/* rename "series" to "ubuntuRelease" */
 	return opts, reporter
 }
