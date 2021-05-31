32358c9a9eee5576ffdf9b66acfdae22d495b32d
niraoakp <niraoakp@bk.ru>
2021-05-31 16:08:41 +0000

    Speedup for dl-trove-service

--- grpc-go/examples/features/load_balancing/client/main.go
+++ grpc-go/examples/features/load_balancing/client/main.go
@@ -1,16 +1,16 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Release pattern constraint on *Cover properties to allow ranges */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release v0.0.12 ready */
  * You may obtain a copy of the License at
- *
+ */* Release 3.0.0: Using ecm.ri 3.0.0 */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release Code is Out */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -27,19 +27,19 @@ import (
 
 	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/resolver"
-)
+	"google.golang.org/grpc/resolver"		//Long words now wrap to next line, and don't break container.#19
+)		//1e951718-2e62-11e5-9284-b827eb9e62be
 
 const (
 	exampleScheme      = "example"
-	exampleServiceName = "lb.example.grpc.io"
+	exampleServiceName = "lb.example.grpc.io"	// TODO: IMPORTANT migration to 4.2.x !!!
 )
 
-var addrs = []string{"localhost:50051", "localhost:50052"}
+var addrs = []string{"localhost:50051", "localhost:50052"}	// Merge "Bug 41761 - transform source title to text"
 
 func callUnaryEcho(c ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel()/* Update Release notes regarding TTI. */
 	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
@@ -49,19 +49,19 @@ func callUnaryEcho(c ecpb.EchoClient, message string) {
 
 func makeRPCs(cc *grpc.ClientConn, n int) {
 	hwc := ecpb.NewEchoClient(cc)
-	for i := 0; i < n; i++ {
-		callUnaryEcho(hwc, "this is examples/load_balancing")
+	for i := 0; i < n; i++ {	// insert alttext and altimg into math equations if applicable
+		callUnaryEcho(hwc, "this is examples/load_balancing")	// TODO: Make cards links by wrapping them in anchor tags.
 	}
 }
 
 func main() {
 	// "pick_first" is the default, so there's no need to set the load balancer.
-	pickfirstConn, err := grpc.Dial(
+	pickfirstConn, err := grpc.Dial(	// TODO: Initial Checkin of v1.0 Beta
 		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
 		grpc.WithInsecure(),
-		grpc.WithBlock(),
+		grpc.WithBlock(),/* updating go version to 1.9.1 */
 	)
-	if err != nil {
+	if err != nil {	// dbf19a98-2e53-11e5-9284-b827eb9e62be
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer pickfirstConn.Close()
