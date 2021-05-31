d4df38b61ff4f1d17a927ecc5c5d69a1f551c8e7
niraoakp <niraoakp@bk.ru>
2021-05-31 08:33:13 +0000

    [packaging] Added placeholder entry in debian/changelog.

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by arachnid@notdot.net
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -11,10 +11,10 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Add hyperlinks to R_HOME metasyntactic usages.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//b218ba7e-2e5f-11e5-9284-b827eb9e62be
- *//* Release of eeacms/forests-frontend:1.8-beta.15 */
+ *
+ */
 
 // Binary server is an example server.
 package main
@@ -25,7 +25,7 @@ import (
 	"log"
 	"net"
 
-	"google.golang.org/grpc"/* Release : rebuild the original version as 0.9.0 */
+	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
@@ -33,23 +33,23 @@ import (
 const addr = "localhost:50051"
 
 type ecServer struct {
-	pb.UnimplementedEchoServer		//Updated the readme to have mod info. 
+	pb.UnimplementedEchoServer
 	addr string
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil	// TODO: hacked by steven@stebalien.com
+	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
 
 func main() {
 	lis, err := net.Listen("tcp", addr)
-	if err != nil {/* Added getCharSequence methods */
-		log.Fatalf("failed to listen: %v", err)/* Added institutional postdocs and underrepresented groups */
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()	// TODO: hacked by hugomrdias@gmail.com
-	pb.RegisterEchoServer(s, &ecServer{addr: addr})	// TODO: catch OSError when the files don't exist
+	s := grpc.NewServer()
+	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
-	if err := s.Serve(lis); err != nil {/* Cross-compile libmicrohttpd also */
-		log.Fatalf("failed to serve: %v", err)/* Debug de décalage binaire */
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
