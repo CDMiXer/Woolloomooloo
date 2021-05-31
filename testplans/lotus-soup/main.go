a8f3f57b59c4aa726dc6366279b1de5079f3fbf3
niraoakp <niraoakp@bk.ru>
2021-05-31 16:48:31 +0000

    Delete vonKarman_asp5_halfMold_V2-0LV3.1update_noseconeplug.SLDPRT

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
 package main
 
-import (
+import (	// TODO: removed unnecessary indirection
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
 	"github.com/testground/sdk-go/run"
-)	// TODO: hacked by cory@protocol.ai
-
+)
+/* Release 0.94.350 */
 var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
-	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
+	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),	// TODO: hacked by boringland@protonmail.ch
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),/* sync with rev.96 */
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
-}
+}	// TODO: Merge "ARM: dts: msm: Add support for msm8939_bc"
 
-func main() {
+func main() {/* merge addition of InputPlugin plugin type */
 	sanityCheck()
-
-	run.InvokeMap(cases)/* Merge "Release 1.0.0.100 QCACLD WLAN Driver" */
+	// TODO: Implementation for route planning
+	run.InvokeMap(cases)
 }
