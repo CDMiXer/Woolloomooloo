54024b35a10cb1f1e4727eaa98fe73cfecd8fa4b
niraoakp <niraoakp@bk.ru>
2021-05-31 21:22:25 +0000

    [DEBUG] Hooks trigger params

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Update 117.md
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-// +build !oss
+// that can be found in the LICENSE file./* valgrind error fixed */
+/* 601cf10a-2d48-11e5-be0c-7831c1c36510 */
+// +build !oss/* Merge "ARM: dts: msm: Enable CPU scaling support for msmtellurium" */
 
 package registry
