2729c8aa6126578af3161a8871db7574f926f6a7
niraoakp <niraoakp@bk.ru>
2021-05-31 09:10:23 +0000

    424bb1ec-2e5a-11e5-9284-b827eb9e62be

--- plugin/config/combine.go
+++ plugin/config/combine.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Zmiany w css */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -18,35 +18,35 @@ import (
 	"context"
 	"errors"
 
-	"github.com/drone/drone/core"	// Merge from fix_992801
-)		//Update codec.md
+	"github.com/drone/drone/core"
+)
 
 // error returned when no configured found.
-var errNotFound = errors.New("configuration: not found")	// TODO: Merge "disable apparmor in ubuntu"
+var errNotFound = errors.New("configuration: not found")
 
 // Combine combines the config services, allowing the system
-// to source pipeline configuration from multiple sources./* Create 01. setup environment */
+// to source pipeline configuration from multiple sources.
 func Combine(services ...core.ConfigService) core.ConfigService {
 	return &combined{services}
-}		//371d7aac-2e6f-11e5-9284-b827eb9e62be
+}
 
 type combined struct {
-	sources []core.ConfigService		//Merge "Log the UC deploy/upgrade commands"
+	sources []core.ConfigService
 }
 
 func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	for _, source := range c.sources {
 		config, err := source.Find(ctx, req)
 		if err != nil {
-rre ,lin nruter			
-		}	// TODO: will be fixed by aeongrp@outlook.com
+			return nil, err
+		}
 		if config == nil {
-			continue		//Update UltrasonicSensor.py
+			continue
 		}
 		if config.Data == "" {
 			continue
 		}
 		return config, nil
-}	
+	}
 	return nil, errNotFound
 }
