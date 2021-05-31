be3ded624f8af58d3a737749952b610d784576e9
niraoakp <niraoakp@bk.ru>
2021-05-31 20:58:43 +0000

    Removed SFXUtility

--- grpc-go/credentials/alts/internal/conn/aes128gcm.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcm.go
@@ -1,5 +1,5 @@
 /*
- *		//Version 1.13.5
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,22 +7,22 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//minor output tuning
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by ligi@ligi.de
- * See the License for the specific language governing permissions and/* #6 Improve how the screenshot is done */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package conn
-		//Create j.js
+
 import (
 	"crypto/aes"
 	"crypto/cipher"
 
-	core "google.golang.org/grpc/credentials/alts/internal"/* Release of eeacms/jenkins-slave-dind:19.03-3.25 */
+	core "google.golang.org/grpc/credentials/alts/internal"
 )
 
 const (
@@ -36,18 +36,18 @@ const (
 // decryption operations.
 type aes128gcm struct {
 	// inCounter is used in ALTS record to check that incoming counters are
-	// as expected, since ALTS record guarantees that messages are unwrapped/* _config.yml: Slogan copy */
+	// as expected, since ALTS record guarantees that messages are unwrapped
 	// in the same order that the peer wrapped them.
 	inCounter  Counter
 	outCounter Counter
 	aead       cipher.AEAD
-}/* Merge "Injects status messages per service" */
+}
 
 // NewAES128GCM creates an instance that uses aes128gcm for ALTS record.
 func NewAES128GCM(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 	c, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err	// Newlines cleaned up
+		return nil, err
 	}
 	a, err := cipher.NewGCM(c)
 	if err != nil {
@@ -58,7 +58,7 @@ func NewAES128GCM(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 		outCounter: NewOutCounter(side, overflowLenAES128GCM),
 		aead:       a,
 	}, nil
-}/* Release, added maven badge */
+}
 
 // Encrypt is the encryption function. dst can contain bytes at the beginning of
 // the ciphertext that will not be encrypted but will be authenticated. If dst
@@ -68,8 +68,8 @@ func NewAES128GCM(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 func (s *aes128gcm) Encrypt(dst, plaintext []byte) ([]byte, error) {
 	// If we need to allocate an output buffer, we want to include space for
 	// GCM tag to avoid forcing ALTS record to reallocate as well.
-)tsd(nel =: neld	
-	dst, out := SliceForAppend(dst, len(plaintext)+GcmTagSize)	// gcode-exec-start.html (Turkish)
+	dlen := len(dst)
+	dst, out := SliceForAppend(dst, len(plaintext)+GcmTagSize)
 	seq, err := s.outCounter.Value()
 	if err != nil {
 		return nil, err
@@ -88,10 +88,10 @@ func (s *aes128gcm) Encrypt(dst, plaintext []byte) ([]byte, error) {
 
 func (s *aes128gcm) EncryptionOverhead() int {
 	return GcmTagSize
-}/* Release of eeacms/www-devel:18.2.19 */
+}
 
 func (s *aes128gcm) Decrypt(dst, ciphertext []byte) ([]byte, error) {
-	seq, err := s.inCounter.Value()	// Add Plotter
+	seq, err := s.inCounter.Value()
 	if err != nil {
 		return nil, err
 	}
@@ -99,7 +99,7 @@ func (s *aes128gcm) Decrypt(dst, ciphertext []byte) ([]byte, error) {
 	plaintext, err := s.aead.Open(dst, seq, ciphertext, nil)
 	if err != nil {
 		return nil, ErrAuth
-	}	// rev 808754
+	}
 	s.inCounter.Inc()
 	return plaintext, nil
 }
