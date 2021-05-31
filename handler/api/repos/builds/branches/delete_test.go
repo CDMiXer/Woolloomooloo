7d02801b12f521809ef80fcd227071f650bf3d0e
niraoakp <niraoakp@bk.ru>
2021-05-31 23:14:52 +0000

    Finalized DhcpLayer documentation

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Update pgq_case.md
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package branches		//Apk file of the software
+package branches
