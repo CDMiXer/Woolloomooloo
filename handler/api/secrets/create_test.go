f49764b4f8f17b4fe46fbbd8529e7a4d5eb89c9b
niraoakp <niraoakp@bk.ru>
2021-05-31 15:22:10 +0000

    PHP Coveralls support

--- handler/api/secrets/create_test.go
+++ handler/api/secrets/create_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* [artifactory-release] Release version 3.4.2 */
 
 package secrets
 
@@ -15,7 +15,7 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"	// TODO: hacked by cory@protocol.ai
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
@@ -26,13 +26,13 @@ import (
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Feature: 1838581
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
+	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)		//Handle cases where transport state is not set
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
-
+	// Include new Lua processor for script use
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
 
@@ -46,31 +46,31 @@ func TestHandleCreate(t *testing.T) {
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* (vila) Release 2.2.3 (Vincent Ladeuil) */
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-
+/* c6c8b70a-2e6c-11e5-9284-b827eb9e62be */
 func TestHandleCreate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
-
-	in := new(bytes.Buffer)
+	// TODO: hacked by earlephilhower@yahoo.com
+	in := new(bytes.Buffer)/* Fix anchor link in index.md */
 	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})
-
+	// TODO: VoteCore: Handles Coup d'état and default kind
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", in)
+	r := httptest.NewRequest("GET", "/", in)/* Ant animation */
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleCreate(nil).ServeHTTP(w, r)
+	HandleCreate(nil).ServeHTTP(w, r)	// TODO: hacked by mowrain@yandex.com
 	if got, want := w.Code, http.StatusBadRequest; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -80,9 +80,9 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}
+}/* Update version for Service Release 1 */
 
-func TestHandleCreate_BadRequest(t *testing.T) {
+func TestHandleCreate_BadRequest(t *testing.T) {/* Added centroid to relfecitn table */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -91,9 +91,9 @@ func TestHandleCreate_BadRequest(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r = r.WithContext(/* Release of jQAssitant 1.5.0 RC-1. */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)	// TODO: Revert `relative` class sniffing
 
 	HandleCreate(nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusBadRequest; want != got {
