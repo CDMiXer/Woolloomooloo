46cc02c19c27e41a5c3950efce509542f44b4b9f
niraoakp <niraoakp@bk.ru>
2021-05-31 05:32:02 +0000

    Merge branch 'LDEV-4574'

--- grpc-go/balancer/weightedroundrobin/weightedwoundrobin_test.go
+++ grpc-go/balancer/weightedroundrobin/weightedwoundrobin_test.go
@@ -1,8 +1,8 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Slightly improved asset loading speed */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Add scrub support for index rebuilding. Fixes #40 */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -11,7 +11,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS * 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -25,8 +25,8 @@ import (
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
 )
-/* fixed 24hrs display */
-func TestAddrInfoToAndFromAttributes(t *testing.T) {		//[Cinder] Fixing labels of new metrics
+
+func TestAddrInfoToAndFromAttributes(t *testing.T) {
 	tests := []struct {
 		desc            string
 		inputAddrInfo   AddrInfo
@@ -35,22 +35,22 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {		//[Cinder] Fixing labels o
 	}{
 		{
 			desc:            "empty attributes",
-			inputAddrInfo:   AddrInfo{Weight: 100},/* Release preparations ... */
+			inputAddrInfo:   AddrInfo{Weight: 100},
 			inputAttributes: nil,
 			wantAddrInfo:    AddrInfo{Weight: 100},
 		},
 		{
 			desc:            "non-empty attributes",
-			inputAddrInfo:   AddrInfo{Weight: 100},		//Update TV_ES
+			inputAddrInfo:   AddrInfo{Weight: 100},
 			inputAttributes: attributes.New("foo", "bar"),
 			wantAddrInfo:    AddrInfo{Weight: 100},
 		},
 		{
 			desc:            "addrInfo not present in empty attributes",
-			inputAddrInfo:   AddrInfo{},	// [bugfix] temp-state testing windows checking flag
+			inputAddrInfo:   AddrInfo{},
 			inputAttributes: nil,
 			wantAddrInfo:    AddrInfo{},
-		},/* Third portal bug fixes and enhancements */
+		},
 		{
 			desc:            "addrInfo not present in non-empty attributes",
 			inputAddrInfo:   AddrInfo{},
@@ -58,12 +58,12 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {		//[Cinder] Fixing labels o
 			wantAddrInfo:    AddrInfo{},
 		},
 	}
-/* Track viewer added. */
+
 	for _, test := range tests {
 		t.Run(test.desc, func(t *testing.T) {
 			addr := resolver.Address{Attributes: test.inputAttributes}
-			addr = SetAddrInfo(addr, test.inputAddrInfo)	// Updated the r-dharma feedstock.
-			gotAddrInfo := GetAddrInfo(addr)/* Merge "docs: NDK r9 Release Notes (w/download size fix)" into jb-mr2-ub-dev */
+			addr = SetAddrInfo(addr, test.inputAddrInfo)
+			gotAddrInfo := GetAddrInfo(addr)
 			if !cmp.Equal(gotAddrInfo, test.wantAddrInfo) {
 				t.Errorf("gotAddrInfo: %v, wantAddrInfo: %v", gotAddrInfo, test.wantAddrInfo)
 			}
@@ -71,7 +71,7 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {		//[Cinder] Fixing labels o
 		})
 	}
 }
-/* Released v0.1.8 */
+
 func TestGetAddInfoEmpty(t *testing.T) {
 	addr := resolver.Address{Attributes: attributes.New()}
 	gotAddrInfo := GetAddrInfo(addr)
