57b3f07c6e4389f9334725fa5c56af1713fe2dd7
niraoakp <niraoakp@bk.ru>
2021-05-31 02:29:22 +0000

    GPL3->BSD nit

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* use native ShareActionProvider on ICS */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,29 +9,29 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Add a contribution section to the README. */
-// limitations under the License./* Hidding the save buttons in case the budget sections has incomplete information. */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package users
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//QEArchiveInterface: use new QCaDateTime API
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-		//Merge "Validate top level of the layout configuration, too"
-// HandleList returns an http.HandlerFunc that writes a json-encoded	// TODO: test: work around race
+
+// HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of all registered system users to the response body.
 func HandleList(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		users, err := users.List(r.Context())
-		if err != nil {/* integration: using new modules */
-			render.InternalError(w, err)
+		if err != nil {	// TODO: will be fixed by sbrichards@gmail.com
+			render.InternalError(w, err)/* Updated the cgal-cpp feedstock. */
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot list users")		//150906.1706 Works - just before introducing .ecbrbj file
-		} else {
+				Warnln("api: cannot list users")
+		} else {	// no need to make a file
 			render.JSON(w, users, 200)
 		}
 	}
