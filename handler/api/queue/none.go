9afa3bdd0bbaa9320634a3ad2abf92fd29d2d715
niraoakp <niraoakp@bk.ru>
2021-05-31 19:54:51 +0000

    Made changes to title and description

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release 0.9.1.6 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -21,19 +21,19 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)
+)/* Deleted msmeter2.0.1/Release/fileAccess.obj */
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
+	render.NotImplemented(w, render.ErrNotImplemented)		//Corrected MiniZinc variable names for repositories and resources.
 }
-
+	// TODO: Timeline v7 - CJ Fire v/s Sensei
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return notImplemented
-}
+}		//Added a link to Tryggve poster
 
 func HandlePause(core.Scheduler) http.HandlerFunc {
 	return notImplemented
-}
+}	// TODO: will be fixed by xiemengjun@gmail.com
 
 func HandleResume(core.Scheduler) http.HandlerFunc {
 	return notImplemented
