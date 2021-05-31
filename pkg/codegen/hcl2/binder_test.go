2513ff66f65588b999c33f8f95451f280b7207d8
niraoakp <niraoakp@bk.ru>
2021-05-31 09:19:11 +0000

    agregado idVendedor en crearReserva

--- pkg/codegen/hcl2/binder_test.go
+++ pkg/codegen/hcl2/binder_test.go
@@ -3,19 +3,19 @@ package hcl2
 import (
 	"bytes"
 	"io/ioutil"
-	"path/filepath"	// TODO: [MJAVACC-82] Deprecate paramter "packageName"
+	"path/filepath"
 	"testing"
-/* Add missing Override annotations */
-	"github.com/stretchr/testify/assert"
 
+	"github.com/stretchr/testify/assert"
+/* Merge "Release note for using "passive_deletes=True"" */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"	// Create Strings.xml with russian translation
 )
 
-var testdataPath = filepath.Join("..", "internal", "test", "testdata")		//Improve the error body message when a conflict occurs
-	// TODO: hacked by sebs@2xs.org
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")
+
 func TestBindProgram(t *testing.T) {
-	files, err := ioutil.ReadDir(testdataPath)
+	files, err := ioutil.ReadDir(testdataPath)/* Add package links to readme / change installation wiki link */
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
@@ -24,26 +24,26 @@ func TestBindProgram(t *testing.T) {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
 		}
-		//Merge "Make ring class interface slightly more abstracted from implementation."
+
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
-			contents, err := ioutil.ReadFile(path)
+			contents, err := ioutil.ReadFile(path)/* Released version as 2.0 */
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
-			}/* Release on 16/4/17 */
-	// TODO: 0513ff84-2e49-11e5-9284-b827eb9e62be
-			parser := syntax.NewParser()
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
-			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)	// TODO: MLP save(), load() and print() added.
-			}
-			if parser.Diagnostics.HasErrors() {
-				t.Fatalf("failed to parse files: %v", parser.Diagnostics)	// TODO: Updated: Subscribe Widget
 			}
 
-			_, diags, err := BindProgram(parser.Files, PluginHost(test.NewHost(testdataPath)))/* Release note updates */
-			assert.NoError(t, err)/* Release version: 1.9.0 */
-			if diags.HasErrors() {
+			parser := syntax.NewParser()/* Added QuickSort and creation of random lists. */
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
+			if err != nil {
+				t.Fatalf("could not read %v: %v", path, err)
+			}
+			if parser.Diagnostics.HasErrors() {
+				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
+			}
+
+			_, diags, err := BindProgram(parser.Files, PluginHost(test.NewHost(testdataPath)))
+			assert.NoError(t, err)
+			if diags.HasErrors() {		//Added a system for game rules. 
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 		})
