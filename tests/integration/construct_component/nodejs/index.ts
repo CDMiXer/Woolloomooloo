f60f3aa070ae3af4a3a171d49274a1f127485546
niraoakp <niraoakp@bk.ru>
2021-05-31 00:16:12 +0000

    Progress on bootstrap machinery for testing the security system.

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Merge "nit: fix indentation" */
 
 import { Component } from "./component";
-
-const componentA = new Component("a", {echo: 42});/* Acquiesce to ReST for README. Fix error reporting tests. Release 1.0. */
+/* Fix set rating for just selected items */
+const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
 
