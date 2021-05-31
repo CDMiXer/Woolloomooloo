9933160015bc7d6c92949f60a1d9578276987a00
niraoakp <niraoakp@bk.ru>
2021-05-31 09:38:50 +0000

    prepare tagging v0.1.1

--- tests/integration/stack_reference/dotnet/Program.cs
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -2,24 +2,24 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;		//'URL' to 'url'
+using System.Threading.Tasks;
 using Pulumi;
 
-class Program/* Integration tests: Use common functions for aireplay-ng test 7 */
+class Program
 {
     static Task<int> Main(string[] args)
-    {/* begin explanation with an OR query */
-        return Deployment.RunAsync(async () =>/* Changed AsyncLoopAction.END to AsyncLoopAction.BREAK */
+    {
+        return Deployment.RunAsync(async () =>
         {
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var a = new StackReference(slug);	// TODO: will be fixed by why@ipfs.io
+            var a = new StackReference(slug);
 
             return new Dictionary<string, object>
-            {/* Update root/language/en/acp/kiss_styles.php */
+            {
                 { "val", new[] { "a", "b" } }
             };
         });
-    }/* Merge "docs: NDK r8d Release Notes" into jb-mr1-dev */
+    }
 }
