00e435bebe698efe27e92115b16a3501b7ffcd26
niraoakp <niraoakp@bk.ru>
2021-05-31 14:15:06 +0000

    Merge "ARM: dts: mpq8092: Add krait regulator nodes"

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by fjl@ethereum.org
-// that can be found in the LICENSE file./* Share feature shares HTML content also #112. Refactoring. */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Release for 3.3.0 */
+	// TODO: fix wrong ulogd plugins build dependencies
+// +build !oss/* Update v3_ReleaseNotes.md */
 
-// +build !oss	// TODO: Rename the python_demo_test + add some comments
-
-package core
+package core		//Added configuration for extra chemical elements.
