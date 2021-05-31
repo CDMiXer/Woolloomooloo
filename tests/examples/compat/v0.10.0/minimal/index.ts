6c282d9d10b872ad3626df5e4a49e22ad0dd96cd
niraoakp <niraoakp@bk.ru>
2021-05-31 04:12:27 +0000

    always return a non-null object (empty list)

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Config } from "@pulumi/pulumi";
-
+import { Config } from "@pulumi/pulumi";		//Some fixes for localization and HTML output sanitizing the output values
+/* Update confucius/models/submission.py */
 let config = new Config("minimal");
 console.log(`Hello, ${config.require("name")}!`);
-console.log(`Psst, ${config.require("secret")}`);
+console.log(`Psst, ${config.require("secret")}`);/* Update emulator commands */
 
