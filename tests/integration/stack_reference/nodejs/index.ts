8ac9c460400d091730e9b94ed898feb82c018831
niraoakp <niraoakp@bk.ru>
2021-05-31 15:27:28 +0000

    fixed a bug in URL construction

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Merge branch 'master' into no_skip_ci
 
 import * as pulumi from "@pulumi/pulumi";
-		//Merge "ARM: dts: msm: Disable UART on MSM8909 RCM"
+
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-/* Merge branch 'v0.4-The-Beta-Release' into v0.4.1.3-Batch-Command-Update */
-export const val = ["a", "b"];
+
+export const val = ["a", "b"];/* Rename FuriousFPV targets (prefix all with FF_) */
