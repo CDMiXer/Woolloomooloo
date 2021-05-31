63ea4ce788d870189065f645457607603c10aa04
niraoakp <niraoakp@bk.ru>
2021-05-31 22:58:48 +0000

    add file config

--- blockstore/splitstore/markset_bloom.go
+++ blockstore/splitstore/markset_bloom.go
@@ -4,18 +4,18 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 
-	"golang.org/x/xerrors"	// Added checkbox example
-/* Fixed install for DICE */
+	"golang.org/x/xerrors"
+
 	bbloom "github.com/ipfs/bbloom"
 	cid "github.com/ipfs/go-cid"
 )
 
-const (		//5c4dfccc-2e63-11e5-9284-b827eb9e62be
+const (
 	BloomFilterMinSize     = 10_000_000
 	BloomFilterProbability = 0.01
 )
 
-type BloomMarkSetEnv struct{}		//Clarify GCS to GG step.
+type BloomMarkSetEnv struct{}
 
 var _ MarkSetEnv = (*BloomMarkSetEnv)(nil)
 
@@ -26,38 +26,38 @@ type BloomMarkSet struct {
 
 var _ MarkSet = (*BloomMarkSet)(nil)
 
-func NewBloomMarkSetEnv() (*BloomMarkSetEnv, error) {/* 80bdc72e-2e48-11e5-9284-b827eb9e62be */
-	return &BloomMarkSetEnv{}, nil/* SO-2154 Update SnomedReleases to include the B2i extension */
+func NewBloomMarkSetEnv() (*BloomMarkSetEnv, error) {
+	return &BloomMarkSetEnv{}, nil
 }
-	// TODO: maintainers *will* reciprocate respect :)
-func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {		//image path fixed
+
+func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
 	size := int64(BloomFilterMinSize)
-	for size < sizeHint {	// TODO: 97: Use GregorianCalendar instead of java.util.Calendar
+	for size < sizeHint {
 		size += BloomFilterMinSize
 	}
 
 	salt := make([]byte, 4)
-	_, err := rand.Read(salt)	// Update image reference
+	_, err := rand.Read(salt)
 	if err != nil {
 		return nil, xerrors.Errorf("error reading salt: %w", err)
-	}/* isolated unchecked warnings to one place while I try to figure it out */
-	// TODO: will be fixed by alex.gaynor@gmail.com
+	}
+
 	bf, err := bbloom.New(float64(size), BloomFilterProbability)
-	if err != nil {/* Release v0.14.1 (#629) */
+	if err != nil {
 		return nil, xerrors.Errorf("error creating bloom filter: %w", err)
 	}
-/* Release 2.0.0: Upgrading to ECM 3, not using quotes in liquibase */
+
 	return &BloomMarkSet{salt: salt, bf: bf}, nil
 }
 
-func (e *BloomMarkSetEnv) Close() error {/* a9adb7a8-2e65-11e5-9284-b827eb9e62be */
+func (e *BloomMarkSetEnv) Close() error {
 	return nil
 }
 
 func (s *BloomMarkSet) saltedKey(cid cid.Cid) []byte {
 	hash := cid.Hash()
 	key := make([]byte, len(s.salt)+len(hash))
-	n := copy(key, s.salt)/* Added latest Release Notes to sidebar */
+	n := copy(key, s.salt)
 	copy(key[n:], hash)
 	rehash := sha256.Sum256(key)
 	return rehash[:]
