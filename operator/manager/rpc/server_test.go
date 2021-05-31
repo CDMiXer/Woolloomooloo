596379df1777db5a6dd0270a2383b5964558e148
niraoakp <niraoakp@bk.ru>
2021-05-31 03:16:15 +0000

    - Improved the class TRLabelShape and RTLabel

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: small fix, large gain (in size)
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss	// TODO: Added it001 infrastructure.
+	// TODO: will be fixed by zaq1tomo@gmail.com
 package rpc
