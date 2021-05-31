8661c28a9e2ee1f662d7e15a191d54b34ca6399f
niraoakp <niraoakp@bk.ru>
2021-05-31 15:29:25 +0000

    Add page break CSS info to the README

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Manifest Release Notes v2.1.18 */
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* New Feature: Release program updates via installer */
 
-class Resource extends pulumi.ComponentResource {		//Descending links
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//ed8a4bf4-2e5f-11e5-9284-b827eb9e62be
+class Resource extends pulumi.ComponentResource {/* [artifactory-release] Release version 3.3.7.RELEASE */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {	// TODO: will be fixed by davidad@alum.mit.edu
+class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);
+        super("my:module:ComponentFour", name, {}, opts);	// 53c1674e-2e4d-11e5-9284-b827eb9e62be
         this.resource = new Resource("otherchild", {parent: this});
-    }		//Better about
+    }
 }
 const comp4 = new ComponentFour("comp4");
