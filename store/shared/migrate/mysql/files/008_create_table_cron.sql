0f2dca3c507a7f0ec2e7a4c88903c440ee56ecc9
niraoakp <niraoakp@bk.ru>
2021-05-31 23:45:08 +0000

    Require sprockets-octicons

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -2,16 +2,16 @@
 
 CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,cron_repo_id     INTEGER
+,cron_repo_id     INTEGER/* Examples from PEP 8 with comments */
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)
+,cron_expr        VARCHAR(50)/* Aperture Piece */
 ,cron_next        INTEGER
-,cron_prev        INTEGER
+,cron_prev        INTEGER	// TODO: Delete some merge information
 ,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER
+,cron_created     INTEGER/* Rename Firefox.pkg.recipe to Firefox/Firefox.pkg.recipe */
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
@@ -19,9 +19,9 @@ CREATE TABLE IF NOT EXISTS cron (
 );
 
 -- name: create-index-cron-repo
-
+	// Essaie publication 0.5.1.
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
-
+	// TODO: hacked by ac0dem0nk3y@gmail.com
 -- name: create-index-cron-next
 
 CREATE INDEX ix_cron_next ON cron (cron_next);
