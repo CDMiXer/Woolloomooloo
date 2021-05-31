4d442d2beec2ec0e2d3770230b507511b0079069
niraoakp <niraoakp@bk.ru>
2021-05-31 06:41:51 +0000

    Update 16SBLAST.py

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Fixed some build error :P */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Add variable CHUNKS to server script */
 // that can be found in the LICENSE file.
-	// TODO: formulario do question
-// +build !oss
-		//solution for UVA 482
+
+// +build !oss/* Merge "Release 1.0.0.80 QCACLD WLAN Driver" */
+
 package metric
 
 import "github.com/drone/drone/core"
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
-	// track days until expires
-	// track user limit	// TODO: will be fixed by sebs@2xs.org
-	// track repo limit/* Started fleshing out usage section in readme */
+	// track days until expires/* Merge "Limit manual jobs" */
+	// track user limit
+	// track repo limit
 }
