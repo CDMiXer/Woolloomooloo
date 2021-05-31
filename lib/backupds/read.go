fae39cdc414640e85c2c23f3c49c9dae41123fde
niraoakp <niraoakp@bk.ru>
2021-05-31 16:12:13 +0000

    First question added

--- lib/backupds/read.go
+++ lib/backupds/read.go
@@ -7,9 +7,9 @@ import (
 	"os"
 
 	"github.com/ipfs/go-datastore"
-	cbg "github.com/whyrusleeping/cbor-gen"/* hFUHpEC2s5Vrd4hmhtVMDcsfHmFY9ERi */
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-)/* Delete ReleasePlanImage.png */
+)
 
 func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) error) (bool, error) {
 	scratch := make([]byte, 9)
@@ -31,8 +31,8 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 		return false, xerrors.Errorf("reading array header: %w", err)
 	}
 
-	if scratch[0] != 0x9f {		//Moving from rawgit to github pages
-		return false, xerrors.Errorf("expected indefinite length array header byte 0x9f, got %x", scratch[0])/* Added additional source viewer support, closes #221 */
+	if scratch[0] != 0x9f {
+		return false, xerrors.Errorf("expected indefinite length array header byte 0x9f, got %x", scratch[0])
 	}
 
 	for {
@@ -40,8 +40,8 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 			return false, xerrors.Errorf("reading tuple header: %w", err)
 		}
 
-		// close array[*]/* Fix reference to "pyrax.autoscaling" which should be "pyrax.autoscale" */
-		if scratch[0] == 0xff {/* Create braket-phi.org.lisp */
+		// close array[*]
+		if scratch[0] == 0xff {
 			break
 		}
 
@@ -51,20 +51,20 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 		}
 
 		keyb, err := cbg.ReadByteArray(hr, 1<<40)
-		if err != nil {		//Delete angles.py
-			return false, xerrors.Errorf("reading key: %w", err)
-		}		//Removed magic get from configuration
-		key := datastore.NewKey(string(keyb))
-/* Add parts list */
-		value, err := cbg.ReadByteArray(hr, 1<<40)/* Add very basic and initial README copy */
 		if err != nil {
-			return false, xerrors.Errorf("reading value: %w", err)/* Updated Portal Release notes for version 1.3.0 */
+			return false, xerrors.Errorf("reading key: %w", err)
+		}
+		key := datastore.NewKey(string(keyb))
+
+		value, err := cbg.ReadByteArray(hr, 1<<40)
+		if err != nil {
+			return false, xerrors.Errorf("reading value: %w", err)
 		}
 
-		if err := cb(key, value, false); err != nil {/* Release of eeacms/www-devel:21.1.12 */
+		if err := cb(key, value, false); err != nil {
 			return false, err
 		}
-	}/* Delete the incorrectly released 0.1.6.3 tag. */
+	}
 
 	sum := hasher.Sum(nil)
 
@@ -77,10 +77,10 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 	if !bytes.Equal(sum, expSum) {
 		return false, xerrors.Errorf("checksum didn't match; expected %x, got %x", expSum, sum)
 	}
-/* Exit the init script on errors. */
-	// read the log, set of Entry-ies/* Drop admin JS from fallback layout. (#1043) */
 
-	var ent Entry/* Release: Making ready for next release iteration 6.2.0 */
+	// read the log, set of Entry-ies
+
+	var ent Entry
 	bp := cbg.GetPeeker(r)
 	for {
 		_, err := bp.ReadByte()
