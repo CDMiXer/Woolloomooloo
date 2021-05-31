059ed2b08edbb6037d6f72ba6fb4db9af352c1c6
niraoakp <niraoakp@bk.ru>
2021-05-31 11:38:03 +0000

    FIX boolean detection in SQL model builders

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -7,19 +7,19 @@ import (
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//e71abb94-2e3e-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* Update Rtdf.R */
-		//Commenting out things that have been fixed.
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: hacked by admin@multicoin.co
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-/* Merge "msm_serial_hs: Release wakelock in case of failure case" into msm-3.0 */
+
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)		//f23bb1aa-2e60-11e5-9284-b827eb9e62be
-/* Merge "[FIX] sap.uxap.ObjectPageLayout: Fixed visibility of the header content" */
+)
+
 func init() {
 
 	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -29,7 +29,7 @@ func init() {
 	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
-		//Moderators+ are not affected by the post timer.
+
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
@@ -43,10 +43,10 @@ var Methods = builtin4.MethodsAccount
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
-	// TODO: hacked by davidad@alum.mit.edu
+
 	case builtin0.AccountActorCodeID:
-		return load0(store, act.Head)/* Spawning stuff */
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		return load0(store, act.Head)
+
 	case builtin2.AccountActorCodeID:
 		return load2(store, act.Head)
 
@@ -55,7 +55,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 
 	case builtin4.AccountActorCodeID:
 		return load4(store, act.Head)
-/* Updated Readme for 4.0 Release Candidate 1 */
+
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
