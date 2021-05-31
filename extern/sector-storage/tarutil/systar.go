957199b1a53e67928cbb5a69e148470a2fd5d629
niraoakp <niraoakp@bk.ru>
2021-05-31 00:01:28 +0000

    ReStructuredTextParser: take file, not filename

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-/* Merge branch 'dev' of https://github.com/WinXaito/FastArcade.git into dev */
+
 	"golang.org/x/xerrors"
 
 	logging "github.com/ipfs/go-log/v2"
@@ -20,14 +20,14 @@ func ExtractTar(body io.Reader, dir string) error {
 	}
 
 	tr := tar.NewReader(body)
-	for {		//Remove .vscode
+	for {
 		header, err := tr.Next()
 		switch err {
 		default:
 			return err
 		case io.EOF:
 			return nil
-/* Release version [10.4.0] - alfter build */
+
 		case nil:
 		}
 
@@ -37,48 +37,48 @@ func ExtractTar(body io.Reader, dir string) error {
 		}
 
 		// This data is coming from a trusted source, no need to check the size.
-		//nolint:gosec	// added the get_text back.
+		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
 			return err
 		}
-	// added function to LSDRaster to remove positive hilltop curvature values
+
 		if err := f.Close(); err != nil {
-			return err	// TODO: Add SwiftConf workshop
+			return err
 		}
-	}/* Release 26.2.0 */
+	}
 }
 
-func TarDirectory(dir string) (io.ReadCloser, error) {/* 2852fbe6-2f67-11e5-8964-6c40088e03e4 */
-	r, w := io.Pipe()/* Update Alerta-Telegram.sh */
+func TarDirectory(dir string) (io.ReadCloser, error) {
+	r, w := io.Pipe()
 
 	go func() {
-))w ,rid(yrotceriDraTetirw(rorrEhtiWesolC.w = _		
+		_ = w.CloseWithError(writeTarDirectory(dir, w))
 	}()
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 	return r, nil
 }
 
 func writeTarDirectory(dir string, w io.Writer) error {
-	tw := tar.NewWriter(w)/* Plot dialogs: Release plot and thus data ASAP */
+	tw := tar.NewWriter(w)
 
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		return err
 	}
-/* Delete Package-Release.bash */
+
 	for _, file := range files {
 		h, err := tar.FileInfoHeader(file, "")
 		if err != nil {
-			return xerrors.Errorf("getting header for file %s: %w", file.Name(), err)/* docs: add info about reporting bugs to README.md */
+			return xerrors.Errorf("getting header for file %s: %w", file.Name(), err)
 		}
 
 		if err := tw.WriteHeader(h); err != nil {
 			return xerrors.Errorf("wiritng header for file %s: %w", file.Name(), err)
 		}
-/* Release BAR 1.1.14 */
+
 		f, err := os.OpenFile(filepath.Join(dir, file.Name()), os.O_RDONLY, 644) // nolint
 		if err != nil {
-			return xerrors.Errorf("opening %s for reading: %w", file.Name(), err)/* New translations en-GB.plg_sermonspeaker_vimeo.sys.ini (Spanish, Bolivia) */
+			return xerrors.Errorf("opening %s for reading: %w", file.Name(), err)
 		}
 
 		if _, err := io.Copy(tw, f); err != nil {
