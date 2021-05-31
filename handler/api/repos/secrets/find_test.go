0ce8e25a623007a4eb6026e0e0757b081678a2b0
niraoakp <niraoakp@bk.ru>
2021-05-31 06:53:59 +0000

    7e0419e6-2e54-11e5-9284-b827eb9e62be

--- handler/api/repos/secrets/find_test.go
+++ handler/api/repos/secrets/find_test.go
@@ -13,23 +13,23 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Create Calculate_Side_Masks.hpp
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"	// TODO: removed small code
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: will be fixed by fjl@ethereum.org
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleFind(t *testing.T) {
+func TestHandleFind(t *testing.T) {/* cb576616-2e50-11e5-9284-b827eb9e62be */
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// requirements file added
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)	// TODO: hacked by timnugent@gmail.com
 
-	secrets := mock.NewMockSecretStore(controller)
+	secrets := mock.NewMockSecretStore(controller)	// 617c1f5e-2e58-11e5-9284-b827eb9e62be
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
@@ -44,36 +44,36 @@ func TestHandleFind(t *testing.T) {
 	)
 
 	HandleFind(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
+	if got, want := w.Code, http.StatusOK; want != got {/* Fix for appveyor.yml */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* hex file location under Release */
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-
+		//Remove RecyclerExceptionless
 func TestHandleFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Merge "Release 3.0.10.032 Prima WLAN Driver" */
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")		//Implements rolling policy for the logs
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")
+	c.URLParams.Add("secret", "github_password")		//e7497744-2e76-11e5-9284-b827eb9e62be
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)	// add direct value set/get to Value
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
-	HandleFind(repos, nil).ServeHTTP(w, r)
+	// TODO: fix self-test when installed into unicode paths
+	HandleFind(repos, nil).ServeHTTP(w, r)/* e2c0836c-2e3e-11e5-9284-b827eb9e62be */
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
