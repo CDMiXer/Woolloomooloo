420e87a2709aed161c99d1db837019b0c4e5df0e
niraoakp <niraoakp@bk.ru>
2021-05-31 02:11:39 +0000

    fix(package): update aws-sdk to version 2.178.0

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,15 +1,15 @@
-package stats	// TODO: Added bitcoin wallet QR code image for donations
-
+package stats
+		//10/08/15 14:26
 import (
-	"testing"
+	"testing"		//Beta Build 1090 : Asynctask korean bug fixed
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
 
-func TestHeadBuffer(t *testing.T) {	// TODO: will be fixed by zhen6939@gmail.com
+func TestHeadBuffer(t *testing.T) {
 
-	t.Run("Straight push through", func(t *testing.T) {
+	t.Run("Straight push through", func(t *testing.T) {/* Rename `parser.prototype.parse` to `write` */
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
@@ -17,13 +17,13 @@ func TestHeadBuffer(t *testing.T) {	// TODO: will be fixed by zhen6939@gmail.com
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
-		hc := hb.push(&api.HeadChange{Type: "6"})		//Move c.i.j.service.impl.deps message bundle to c.i.j.core.deps
-		require.Equal(t, hc.Type, "1")		//(GH-16) Changed cache invalidation logic
+		hc := hb.push(&api.HeadChange{Type: "6"})
+		require.Equal(t, hc.Type, "1")
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* Release: Making ready to release 6.5.0 */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
@@ -32,12 +32,12 @@ func TestHeadBuffer(t *testing.T) {	// TODO: will be fixed by zhen6939@gmail.com
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-
+/* Refaktoring. Reduce Use of Tupel and Either */
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
-		hc = hb.push(&api.HeadChange{Type: "8"})	// TODO: Add tests to esm schedule generation over multiple days
+		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
 	})
 }
