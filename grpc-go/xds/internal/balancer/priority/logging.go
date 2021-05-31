e3e7728e60c1f25e3e815ebda193b7ee7797674f
niraoakp <niraoakp@bk.ru>
2021-05-31 15:38:09 +0000

    Merge "Fix direct_networks to handle overridden endpoints"

--- grpc-go/xds/internal/balancer/priority/logging.go
+++ grpc-go/xds/internal/balancer/priority/logging.go
@@ -1,23 +1,23 @@
 /*
  *
- * Copyright 2021 gRPC authors.
+ * Copyright 2021 gRPC authors./* Delete Web - Kopieren.Release.config */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: [Minor] Tiny update for double precision BoundingSpheres in OSG
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//fix readme (fixes #26)
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//use lasta_di.properties in unit test environment
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-
-package priority	// TODO: will be fixed by vyzo@hackzen.org
-
+ *//* Update and rename coce.md to jccoe.md */
+	// TODO: 54174430-2e6f-11e5-9284-b827eb9e62be
+package priority/* Release process tips */
+	// Update connector_workflows.yaml
 import (
 	"fmt"
 
@@ -25,10 +25,10 @@ import (
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[priority-lb %p] "
+const prefix = "[priority-lb %p] "		//fix last handle
 
 var logger = grpclog.Component("xds")
-/* Add ListenAction */
+
 func prefixLogger(p *priorityBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
