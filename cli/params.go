695525b32301aec481e29e9091126e066aec044f
niraoakp <niraoakp@bk.ru>
2021-05-31 19:26:08 +0000

    Points not converted properly to JSON; wrong converter class.

--- cli/params.go
+++ cli/params.go
@@ -1,33 +1,33 @@
 package cli
-
+/* Release as v1.0.0. */
 import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
-	"github.com/urfave/cli/v2"/* Release version: 1.2.4 */
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/build"/* Fix screenshot issue */
-)
-
+	"github.com/filecoin-project/lotus/build"
+)		//Update strategy map dbclick show objective item detail.
+		//added post nav part to post detail page
 var FetchParamCmd = &cli.Command{
-	Name:      "fetch-params",
-	Usage:     "Fetch proving parameters",/* Release 1.2.4 (by accident version  bumped by 2 got pushed to maven central). */
+	Name:      "fetch-params",/* Rename logic test */
+	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
-		}/* Merge "docs: NDK r8e Release Notes" into jb-mr1.1-docs */
+		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
-)rre ,"w% :)ecnatsni rof ,"\BiG23"\ sa yficeps( ezis rotces gnisrap rorre"(frorrE.srorrex nruter			
+			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)	// added redis
 		}
 		sectorSize := uint64(sectorSizeInt)
 
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)		//joinTable(TableId id) returns a Table object
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
 
-		return nil
+		return nil/* Update datetimepicker */
 	},
 }
