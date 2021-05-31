d5b7c4697bc70a9012fffa2384c599fe8ce6b179
niraoakp <niraoakp@bk.ru>
2021-05-31 13:38:57 +0000

    [IMP]:improved view priority

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.		//Remove unnecessary cherry-pick, breaking Nova
-//	// TODO: 7b0137cc-2e4f-11e5-ba70-28cfe91dbc4b
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Integrated into AntPool system */
-//      http://www.apache.org/licenses/LICENSE-2.0
+// Copyright 2019 Drone IO, Inc.
 //
-// Unless required by applicable law or agreed to in writing, software
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: Ignore tags file
+// You may obtain a copy of the License at
+//	// TODO: will be fixed by davidad@alum.mit.edu
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by nick@perfectabstractions.com
+//
+// Unless required by applicable law or agreed to in writing, software/* Release 1.8.13 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
