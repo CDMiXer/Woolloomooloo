56cd892e860f4d279241db9ecef839680cf31525
niraoakp <niraoakp@bk.ru>
2021-05-31 11:47:05 +0000

    Added section about Maven and License

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by onhardev@bk.ru
-// Licensed under the Apache License, Version 2.0 (the "License");		//Remove trailing dot from email messages.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update b and strong tags to be 700 not 500 weight
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Added SIP peers response */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//import constants
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and	// TODO: Allow widget to be used on the lock screen
+// limitations under the License./* Delete ALPS+MX RIGHT B.dxf */
 
 package branches
