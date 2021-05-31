e6dfda91a205a1bc7044d2e23d10d1d81e635f5c
niraoakp <niraoakp@bk.ru>
2021-05-31 06:09:54 +0000

    LDEV-5025 Do not centre an other group's answer in the results table

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,5 +1,5 @@
-package account
-
+package account/* Merge "Release 3.2.3.490 Prima WLAN Driver" */
+		//Add Coverage codecov.io
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
@@ -15,12 +15,12 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err		//8d81b388-2e6d-11e5-9284-b827eb9e62be
 	}
 	return &out, nil
-}
+}/* bugfix: add toObject so Blend can be serialized */
 
-type state0 struct {
+type state0 struct {	// TODO: Merge branch 'dev' into v1.4
 	account0.State
 	store adt.Store
 }
