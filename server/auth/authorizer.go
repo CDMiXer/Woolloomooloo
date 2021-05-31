20d7581fe8a4131810df28979a69d26290e4a187
niraoakp <niraoakp@bk.ru>
2021-05-31 13:06:38 +0000

    Change ugly title title to minimalistic

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
 package auth
 
-import (/* Explicit port action execution before wait */
-	"context"/* Delete WhenChronoLUTemu.csv */
+import (
+	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
-)/* Add PersistenceLayer project file */
-		//Create 03. Exchange Variable Values
+)
+
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
-		return false, err/* 93efb9f2-2e51-11e5-9284-b827eb9e62be */
+		return false, err
 	}
 	return allowed, nil
 }
