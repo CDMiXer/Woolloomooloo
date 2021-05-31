0dd7be2f2e8192b9022ef3a3e652afd069f8ab87
niraoakp <niraoakp@bk.ru>
2021-05-31 08:12:04 +0000

    Restored info cards.

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* More preparatory work. Still nothing on the solve() method */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file.	// TODO: will be fixed by vyzo@hackzen.org
+/* bitmex omit _format from http body */
 // +build !oss
 
 package system
