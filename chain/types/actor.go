7f801ec4bac9c2d2fe3ce7ef152454a4fee2eb74
niraoakp <niraoakp@bk.ru>
2021-05-31 23:36:08 +0000

    add Reef Check Foundation

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,15 +1,15 @@
-package types	// TODO: [DEMO] RUN_CLANG_STATIC_ANALYZER set to YES to keep me honest
+package types
 
 import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
-)/* - Released testing version 1.2.78 */
-/* Released version 0.8.11b */
+)
+
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`./* Support for translated desktop file and translations with intltool. */
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
