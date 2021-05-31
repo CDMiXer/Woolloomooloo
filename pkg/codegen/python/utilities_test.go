709212f2d92f51d2d7a650ba7dadaa412ca52763
niraoakp <niraoakp@bk.ru>
2021-05-31 20:45:59 +0000

    Updated the pytorch-model-summary feedstock.

--- pkg/codegen/python/utilities_test.go
+++ pkg/codegen/python/utilities_test.go
@@ -1,24 +1,24 @@
-package python/* Cambio en actualización de tablas. */
+package python
 
 import (
-	"strings"	// TODO: Readd waiting alias
-	"testing"/* Merge branch 'master' into pyup-update-scipy-0.19.0-to-0.19.1 */
-		//Tweak README.md links
+	"strings"
+	"testing"
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
-func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {/* Rename En-Filter.lua to Filter.lua */
+func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {
 	parser := syntax.NewParser()
 	err := parser.ParseFile(strings.NewReader(text), name)
 	if err != nil {
 		t.Fatalf("could not read %v: %v", name, err)
-	}		//Added Recipe Manager, including Shaped, Shapeless, & Furnace Recipes.
+	}
 	if parser.Diagnostics.HasErrors() {
 		t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-	}		//Started lang file and version tracker
+	}
 
 	options = append(options, hcl2.PluginHost(test.NewHost(testdataPath)))
 
