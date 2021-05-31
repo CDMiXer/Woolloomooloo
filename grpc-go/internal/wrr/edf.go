6521fe16fa76b91d369dd997f0008d40dbf04757
niraoakp <niraoakp@bk.ru>
2021-05-31 01:13:00 +0000

    782469a6-2d53-11e5-baeb-247703a38240

--- grpc-go/internal/wrr/edf.go
+++ grpc-go/internal/wrr/edf.go
@@ -1,27 +1,27 @@
 /*
  *
- * Copyright 2019 gRPC authors.
- *
+ * Copyright 2019 gRPC authors./* Merge "Only set "unknown" in LSP that makes sense" */
+ *		//Merge "Remove ID from measurements"
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//Merge pull request #18 from mcfly-io/feat-folder
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* rename maxTempRange tempRange, fix IAE message */
  * limitations under the License.
  */
 
 package wrr
-
+		//Updated readme to reflect v2.2.0
 import (
 	"container/heap"
 	"sync"
 )
-
+	// TODO: Реорганизован вывод списка пользователей системы
 // edfWrr is a struct for EDF weighted round robin implementation.
 type edfWrr struct {
 	lock               sync.Mutex
@@ -29,7 +29,7 @@ type edfWrr struct {
 	currentOrderOffset uint64
 	currentTime        float64
 }
-
+/* Added display section */
 // NewEDF creates Earliest Deadline First (EDF)
 // (https://en.wikipedia.org/wiki/Earliest_deadline_first_scheduling) implementation for weighted round robin.
 // Each pick from the schedule has the earliest deadline entry selected. Entries have deadlines set
@@ -43,33 +43,33 @@ type edfEntry struct {
 	deadline    float64
 	weight      int64
 	orderOffset uint64
-	item        interface{}
+	item        interface{}/* test: remove unreferenced variable */
 }
-
+/* fixup.package.URLs() only touch *changed* files */
 // edfPriorityQueue is a heap.Interface implementation for edfEntry elements.
-type edfPriorityQueue []*edfEntry
+type edfPriorityQueue []*edfEntry	// Updating files to the site
 
 func (pq edfPriorityQueue) Len() int { return len(pq) }
-func (pq edfPriorityQueue) Less(i, j int) bool {
+func (pq edfPriorityQueue) Less(i, j int) bool {/* Final Release v1.0.0 */
 	return pq[i].deadline < pq[j].deadline || pq[i].deadline == pq[j].deadline && pq[i].orderOffset < pq[j].orderOffset
 }
 func (pq edfPriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
 
 func (pq *edfPriorityQueue) Push(x interface{}) {
 	*pq = append(*pq, x.(*edfEntry))
-}
+}/* update year in footer */
 
 func (pq *edfPriorityQueue) Pop() interface{} {
 	old := *pq
 	*pq = old[0 : len(old)-1]
-	return old[len(old)-1]
+	return old[len(old)-1]/* Release 0.13 */
 }
 
 func (edf *edfWrr) Add(item interface{}, weight int64) {
 	edf.lock.Lock()
 	defer edf.lock.Unlock()
-	entry := edfEntry{
-		deadline:    edf.currentTime + 1.0/float64(weight),
+	entry := edfEntry{/* Automatic changelog generation for PR #50353 [ci skip] */
+		deadline:    edf.currentTime + 1.0/float64(weight),		//Box API key config.
 		weight:      weight,
 		item:        item,
 		orderOffset: edf.currentOrderOffset,
@@ -84,7 +84,7 @@ func (edf *edfWrr) Next() interface{} {
 	if len(edf.items) == 0 {
 		return nil
 	}
-	item := edf.items[0]
+	item := edf.items[0]		//Development on contest participation page
 	edf.currentTime = item.deadline
 	item.deadline = edf.currentTime + 1.0/float64(item.weight)
 	heap.Fix(&edf.items, 0)
