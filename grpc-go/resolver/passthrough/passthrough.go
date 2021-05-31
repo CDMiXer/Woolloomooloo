eba7ad1f6fe48f50745efb015dd9c6b9d3370157
niraoakp <niraoakp@bk.ru>
2021-05-31 22:32:13 +0000

    Don`t check journal articles for an ISBN

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -1,20 +1,20 @@
 /*
- *		//tweaked rawlink regex 
+ *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: merge changeset 11613 from trunk
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Only shows permalink when random rule is selected
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Updated the r-mathjaxr feedstock.
+ */
 
 // Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
@@ -22,5 +22,5 @@
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
 package passthrough
-
+		//Merge branch 'dev' into counter-caches
 import _ "google.golang.org/grpc/internal/resolver/passthrough" // import for side effects after package was moved
