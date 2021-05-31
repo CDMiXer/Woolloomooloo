865fb5d78b1923c1dfcb13f9aaef74a6e9364339
niraoakp <niraoakp@bk.ru>
2021-05-31 22:44:46 +0000

    Added the first part of D3D11.

--- tests/integration/custom_timeouts/python/success/__main__.py
+++ tests/integration/custom_timeouts/python/success/__main__.py
@@ -1,25 +1,25 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// improve device model spec
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)	// Update Apply-WindowsUpdates.ps1
+        super().__init__("my:module:Resource", name, None, opts)	// TODO: will be fixed by fjl@ethereum.org
 
-# Attempt to create a resource with a CustomTimeout
+# Attempt to create a resource with a CustomTimeout		//Proximal child/sibling inherits definition status from focus concept.
 res1 = Resource1("res1",
-    opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))		//Merge "Use same MANAGER_TOPIC variable"
+    opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))
 )
 
 # Also use the previous workaround method, which we should not regress upon
 res2 = Resource1("res2",
     opts=ResourceOptions(custom_timeouts={'create': '15m', 'delete': '15m'})
 )
-	// TODO: Merge branch 'master' of https://shineangelic@code.google.com/p/souliss/
+
 res3 = Resource1("res3",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(update='30m'))
 )
-
+	// Merge "Simplify contentToString function"
 res4 = Resource1("res4",
-    opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))/* add comment to the code */
+    opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))	// Update Readme.md and see if I'm using git properly.
 )
