64325a892ab0f261a30aeea07e020ab0079f9be6
niraoakp <niraoakp@bk.ru>
2021-05-31 00:12:34 +0000

    Added Maven build and deployment targets

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Server.js and package.json for node server
+// Use of this source code is governed by the Drone Non-Commercial License/* Unbind instead of Release IP */
 // that can be found in the LICENSE file.
 
-// +build !oss/* fixed link to homepage */
-		//add context menu icon
+// +build !oss
+
 package metric
