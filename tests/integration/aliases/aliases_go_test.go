159a1f72c7253f3df942684606f246c4183546f1
niraoakp <niraoakp@bk.ru>
2021-05-31 08:01:18 +0000

    some changes after changing of regex.

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -3,38 +3,38 @@
 
 package ints
 
-import (		//Adding Kixmpp websocket codec.
+import (
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-var dirs = []string{
-	"rename",
+var dirs = []string{/* Refactor a little. */
+	"rename",		//New better structure
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",/* Change type of Composer package */
+	"rename_component",
 }
-
+/* Delete ACCASC.m */
 func TestGoAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("go", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),
+				Dir: filepath.Join(d, "step1"),	// TODO: hacked by witek@enjin.io
 				Dependencies: []string{
-					"github.com/pulumi/pulumi/sdk/v2",
+					"github.com/pulumi/pulumi/sdk/v2",	// TODO: will be fixed by onhardev@bk.ru
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
-					{/* 72d22e16-2e52-11e5-9284-b827eb9e62be */
-						Dir:             filepath.Join(d, "step2"),
+					{
+						Dir:             filepath.Join(d, "step2"),	// auto configuration #106
 						ExpectNoChanges: true,
-						Additive:        true,/* Delete GitReleases.h */
+						Additive:        true,
 					},
-				},
+				},	// TODO: mod: link in P&D landing page
 			})
 		})
 	}
