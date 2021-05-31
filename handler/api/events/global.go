16f618e7f6d74131cedd4b3c2bcb4c2545cf8036
niraoakp <niraoakp@bk.ru>
2021-05-31 03:23:22 +0000

    Resuming work on this...

--- handler/api/events/global.go
+++ handler/api/events/global.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Added the dialogue plugin
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: a807f35e-2e66-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,17 +20,17 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/drone/drone/core"/* store SettingInfo more compactly */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-	// Change the bundle id to bright it one and version to 2.0.0
-// HandleGlobal creates an http.HandlerFunc that streams builds events/* Bower Release 0.1.2 */
+
+// HandleGlobal creates an http.HandlerFunc that streams builds events
 // to the http.Response in an event stream format.
 func HandleGlobal(
 	repos core.RepositoryStore,
 	events core.Pubsub,
-) http.HandlerFunc {	// TODO: Some order in the stylesheets
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := logger.FromRequest(r)
 
@@ -39,8 +39,8 @@ func HandleGlobal(
 		h.Set("Cache-Control", "no-cache")
 		h.Set("Connection", "keep-alive")
 		h.Set("X-Accel-Buffering", "no")
-	// TODO: will be fixed by josharian@gmail.com
-		f, ok := w.(http.Flusher)	// TODO: hacked by lexy8russo@outlook.com
+
+		f, ok := w.(http.Flusher)
 		if !ok {
 			return
 		}
@@ -48,13 +48,13 @@ func HandleGlobal(
 		access := map[string]struct{}{}
 		user, authenticated := request.UserFrom(r.Context())
 		if authenticated {
-			list, _ := repos.List(r.Context(), user.ID)/* Restructuring in the grammarGen routines */
+			list, _ := repos.List(r.Context(), user.ID)
 			for _, repo := range list {
 				access[repo.Slug] = struct{}{}
 			}
 		}
-/* Release v0.01 */
-		io.WriteString(w, ": ping\n\n")/* Release to intrepid */
+
+		io.WriteString(w, ": ping\n\n")
 		f.Flush()
 
 		ctx, cancel := context.WithCancel(r.Context())
@@ -78,15 +78,15 @@ func HandleGlobal(
 			case <-time.After(pingInterval):
 				io.WriteString(w, ": ping\n\n")
 				f.Flush()
-			case event := <-events:/* 22d54d92-2e63-11e5-9284-b827eb9e62be */
+			case event := <-events:
 				_, authorized := access[event.Repository]
-				if event.Visibility == core.VisibilityPublic {/* sparql and turtle parsers are stable */
+				if event.Visibility == core.VisibilityPublic {
 					authorized = true
 				}
 				if event.Visibility == core.VisibilityInternal && authenticated {
-					authorized = true	// Bump jemoji :gem: to v0.6.0
+					authorized = true
 				}
-				if authorized {/* Initialising visitor checks that the lifecycle exists */
+				if authorized {
 					io.WriteString(w, "data: ")
 					w.Write(event.Data)
 					io.WriteString(w, "\n\n")
