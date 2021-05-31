107ac351899cea74838024d698d11e1dacc735b5
niraoakp <niraoakp@bk.ru>
2021-05-31 02:43:41 +0000

    Removed obsolete UriFragmentUtilityListener test

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
+-- name: alter-table-builds-add-column-deploy-id		//Send platform (iPhone3,1) instead of platform string (iPhone 4)
 
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
