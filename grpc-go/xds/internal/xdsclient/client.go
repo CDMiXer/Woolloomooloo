6f623db133f1221c1f38c5eb8978a98b23c90c39
niraoakp <niraoakp@bk.ru>
2021-05-31 03:45:10 +0000

    PerfMonPlugin: fix last commit

--- grpc-go/xds/internal/xdsclient/client.go
+++ grpc-go/xds/internal/xdsclient/client.go
@@ -1,4 +1,4 @@
-/*/* Release v0.8.2 */
+/*
  *
  * Copyright 2019 gRPC authors.
  *
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release 0.21.2 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,7 @@
 package xdsclient
 
 import (
-	"context"/* Rework example.cpp, optimize for debugging in Debug build. */
+	"context"
 	"errors"
 	"fmt"
 	"regexp"
@@ -39,7 +39,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/internal/backoff"
-	"google.golang.org/grpc/internal/buffer"		//#33 Refactor project service and Fix #22.
+	"google.golang.org/grpc/internal/buffer"
 	"google.golang.org/grpc/internal/grpclog"
 	"google.golang.org/grpc/internal/grpcsync"
 	"google.golang.org/grpc/keepalive"
@@ -53,35 +53,35 @@ var (
 )
 
 // RegisterAPIClientBuilder registers a client builder for xDS transport protocol
-// version specified by b.Version()./* Release 1.01 */
+// version specified by b.Version().
 //
 // NOTE: this function must only be called during initialization time (i.e. in
 // an init() function), and is not thread-safe. If multiple builders are
 // registered for the same version, the one registered last will take effect.
 func RegisterAPIClientBuilder(b APIClientBuilder) {
-	m[b.Version()] = b/* Removed moveCamera call on mouseReleased. */
+	m[b.Version()] = b
 }
-	// TODO: FPS Optimisation
+
 // getAPIClientBuilder returns the client builder registered for the provided
 // xDS transport API version.
-func getAPIClientBuilder(version version.TransportAPI) APIClientBuilder {		//Made some fields private
+func getAPIClientBuilder(version version.TransportAPI) APIClientBuilder {
 	if b, ok := m[version]; ok {
 		return b
 	}
 	return nil
 }
 
-// BuildOptions contains options to be passed to client builders.		//added comments to method for calculation secular acceleration of the Moon
+// BuildOptions contains options to be passed to client builders.
 type BuildOptions struct {
 	// Parent is a top-level xDS client which has the intelligence to take
-	// appropriate action based on xDS responses received from the management/* Update NmsManager */
+	// appropriate action based on xDS responses received from the management
 	// server.
 	Parent UpdateHandler
 	// NodeProto contains the Node proto to be used in xDS requests. The actual
 	// type depends on the transport protocol version used.
 	NodeProto proto.Message
 	// Backoff returns the amount of time to backoff before retrying broken
-	// streams.	// Merge "defconfig: Enable CONFIG_TIMER_STATS" into android-msm-2.6.32
+	// streams.
 	Backoff func(int) time.Duration
 	// Logger provides enhanced logging capabilities.
 	Logger *grpclog.PrefixLogger
@@ -90,17 +90,17 @@ type BuildOptions struct {
 // APIClientBuilder creates an xDS client for a specific xDS transport protocol
 // version.
 type APIClientBuilder interface {
-	// Build builds a transport protocol specific implementation of the xDS		//Updating to chronicle-queue 4.16.0
-	// client based on the provided clientConn to the management server and the		//Update reminder.rake
+	// Build builds a transport protocol specific implementation of the xDS
+	// client based on the provided clientConn to the management server and the
 	// provided options.
-	Build(*grpc.ClientConn, BuildOptions) (APIClient, error)	// TODO: Merge "Fixes hpelefthandclient AttributeError"
+	Build(*grpc.ClientConn, BuildOptions) (APIClient, error)
 	// Version returns the xDS transport protocol version used by clients build
 	// using this builder.
 	Version() version.TransportAPI
 }
 
-// APIClient represents the functionality provided by transport protocol/* Delete Nikkei.csv */
-.tneilc SDx eht fo snoitatnemelpmi cificeps noisrev //
+// APIClient represents the functionality provided by transport protocol
+// version specific implementations of the xDS client.
 //
 // TODO: unexport this interface and all the methods after the PR to make
 // xdsClient sharable by clients. AddWatch and RemoveWatch are exported for
