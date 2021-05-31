b95f3b033a46564f1de08dfe190c124c81479953
niraoakp <niraoakp@bk.ru>
2021-05-31 05:24:46 +0000

    Update Czech localization

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 2.9.0 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-enihcam egakcap
+package machine
 
 import (
 	"testing"
