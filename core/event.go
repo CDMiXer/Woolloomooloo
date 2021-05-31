0506228e009c7ce318f668cc8b5447b2dcc2504d
niraoakp <niraoakp@bk.ru>
2021-05-31 20:35:04 +0000

    Made Last Stand hit twice like its supposed to

--- core/event.go
+++ core/event.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "Allow OVERLAY frames to use the show_exsiting_frame flag" into nextgenv2 */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* add print_text_in_color */
-///* Release-1.3.0 updates to changes.txt and version number. */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Change Complete to Status - Part 1 (db changes)
-		//75cdcbc0-2e4d-11e5-9284-b827eb9e62be
+// limitations under the License.
+
 package core
 
 // Hook event constants.
@@ -21,6 +21,6 @@ const (
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
-	EventPromote     = "promote"/* Release plugin update */
+	EventPromote     = "promote"
 	EventRollback    = "rollback"
-)/* Roughing in characteristics */
+)
