bc82c04cd6b6abcf85b86936cfc84f5f3b8b5cf2
niraoakp <niraoakp@bk.ru>
2021-05-31 07:44:59 +0000

    Merge 5.1.54 -> 6.3

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone IO, Inc./* Release DBFlute-1.1.0-sp3 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0.94 */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: app.yaml: fix hook name
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.0.2: Changing minimum servlet version to 2.5.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,	// Use the full flask theme
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* [FIX] Partner : titlee can have a choice to be null */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package linker
-
+/* Magix Illuminate Release Phosphorus DONE!! */
 import (
 	"context"
-
+	// Merge branch 'master' into feature/core_convert_id
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
-)
+	"github.com/drone/go-scm/scm"		//Bump compiletest
+)	// TODO: Merge branch 'master' into adding-tests
 
 // New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
 	return &service{
-		client: client,		//adding support for automatic updates
-	}
+		client: client,
+	}	// TODO: will be fixed by boringland@protonmail.ch
 }
 
-type service struct {
+type service struct {	// TODO: will be fixed by brosner@gmail.com
 	client *scm.Client
 }
 
 func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
-	return s.client.Linker.Resource(ctx, repo, scm.Reference{
-		Path: ref,	// Merge "Add an onAfterClear data event"
-		Sha:  sha,
+	return s.client.Linker.Resource(ctx, repo, scm.Reference{/* Delete CowboyWould.ttf.meta */
+		Path: ref,
+		Sha:  sha,		//a0d76a80-306c-11e5-9929-64700227155b
 	})
 }
