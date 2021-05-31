4282c5515d3c8aee2fd682e49526add9f93359b8
niraoakp <niraoakp@bk.ru>
2021-05-31 06:00:54 +0000

    ne2k_pci: Add a check on infinite loop

--- gen/join_test.go
+++ gen/join_test.go
@@ -2,27 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket
-
+package websocket		//Follow rails convention of test:units
+	// TODO: will be fixed by zaq1tomo@gmail.com
 import (
-	"bytes"
-	"io"
+	"bytes"/* Merge "Clean up EventLoggingService as a singleton." */
+	"io"/* Merge "Release memory allocated by scandir in init_pqos_events function" */
 	"strings"
 	"testing"
-)
+)/* update some dev stuff */
 
 func TestJoinMessages(t *testing.T) {
-	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}
-	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
+	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}	// TODO: removes random_seed param when not using random order
+	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {/* Release 1-78. */
 		for _, term := range []string{"", ","} {
-			var connBuf bytes.Buffer
+			var connBuf bytes.Buffer/* Rename screenshots section to demo in README */
 			wc := newTestConn(nil, &connBuf, true)
 			rc := newTestConn(&connBuf, nil, false)
 			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
 			}
-
-			var result bytes.Buffer
+		//Removed tests and benchmarks databases from repo
+			var result bytes.Buffer		//7b4660e3-2e4f-11e5-800a-28cfe91dbc4b
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
 			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
