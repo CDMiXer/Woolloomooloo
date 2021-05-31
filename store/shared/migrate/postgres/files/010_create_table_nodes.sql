f4294d184a7e227968881ddeaee1c6900f056b41
niraoakp <niraoakp@bk.ru>
2021-05-31 18:00:17 +0000

    ### Create hyperlink to particular mail

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -1,33 +1,33 @@
--- name: create-table-nodes
+-- name: create-table-nodes	// TODO: will be fixed by joshua@yottadb.com
 
-CREATE TABLE IF NOT EXISTS nodes (
+CREATE TABLE IF NOT EXISTS nodes (/* Updated How To Stay On Budget While All Your Friends Get Married */
  node_id         SERIAL PRIMARY KEY
 ,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
 ,node_state      VARCHAR(50)
-,node_name       VARCHAR(50)		//Add the business classes
-,node_image      VARCHAR(500)		//Possible fix for stacking snow not working
+,node_name       VARCHAR(50)
+,node_image      VARCHAR(500)
 ,node_region     VARCHAR(100)
 ,node_size       VARCHAR(100)
-,node_os         VARCHAR(50)	// Added ssl support in 0.9-dev.
+,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
-,node_address    VARCHAR(500)
-,node_capacity   INTEGER/* Merge branch 'International-Release' into 1379_duplicate_products */
+,node_address    VARCHAR(500)	// TODO: Now blocks removed with mouse left-click are acquired as items.
+,node_capacity   INTEGER
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)
+,node_error      VARCHAR(2000)	// first attempt at evalargs.
 ,node_ca_key     BYTEA
-,node_ca_cert    BYTEA/* Release a new minor version 12.3.1 */
-,node_tls_key    BYTEA/* RELEASE 3.0.110. */
-,node_tls_cert   BYTEA
-,node_tls_name   VARCHAR(500)/* Merge "Release 1.0.0.246 QCACLD WLAN Driver" */
+,node_ca_cert    BYTEA	// TODO: hacked by aeongrp@outlook.com
+,node_tls_key    BYTEA	// TODO: stupid inclusions...
+,node_tls_cert   BYTEA	// TODO: will be fixed by fjl@ethereum.org
+,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN	// TODO: Merge branch 'master' into quantumespresso-pilatus
-,node_created    INTEGER/* Release_0.25-beta.md */
+,node_protected  BOOLEAN
+,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
-
-,UNIQUE(node_name)	// TODO: hacked by joshua@yottadb.com
+		//Update license with copyright info
+,UNIQUE(node_name)
 );
