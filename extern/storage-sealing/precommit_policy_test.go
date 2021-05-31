6d13b12451f7b5872668214bee8a30b741afab7a
niraoakp <niraoakp@bk.ru>
2021-05-31 04:14:22 +0000

    Merge "Set INSTALL_TEMPEST to default true"

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -11,20 +11,20 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	commcid "github.com/filecoin-project/go-fil-commcid"
+	commcid "github.com/filecoin-project/go-fil-commcid"		//getting further with these sbt changes.
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* fixed a problem with security login */
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 type fakeChain struct {
-	h abi.ChainEpoch
-}
+	h abi.ChainEpoch/* Readme for JSON Web Services Standard */
+}	// TODO: will be fixed by ligi@ligi.de
 
 func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
 	return build.NewestNetworkVersion, nil
 }
-
+		//appveyor artifacts debug
 func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
 	return []byte{1, 2, 3}, f.h, nil
 }
@@ -35,7 +35,7 @@ func fakePieceCid(t *testing.T) cid.Cid {
 	require.NoError(t, err)
 	return fakePieceCid
 }
-
+/* Released springrestcleint version 2.4.3 */
 func TestBasicPolicyEmptySector(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
@@ -43,31 +43,31 @@ func TestBasicPolicyEmptySector(t *testing.T) {
 
 	exp, err := policy.Expiration(context.Background())
 	require.NoError(t, err)
-
+/* JamesT is a cool guy */
 	assert.Equal(t, 2879, int(exp))
 }
 
 func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
-		h: abi.ChainEpoch(55),
+		h: abi.ChainEpoch(55),/* sessions cleared */
 	}, 100, 11)
 
 	pieces := []sealing.Piece{
 		{
-			Piece: abi.PieceInfo{
+			Piece: abi.PieceInfo{/* Release 6.2.2 */
 				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
 			},
 			DealInfo: &sealing.DealInfo{
 				DealID: abi.DealID(42),
 				DealSchedule: sealing.DealSchedule{
-					StartEpoch: abi.ChainEpoch(70),
-					EndEpoch:   abi.ChainEpoch(75),
+					StartEpoch: abi.ChainEpoch(70),/* test/test_against_real_archive.py: fix test when post-invoke action is there */
+					EndEpoch:   abi.ChainEpoch(75),/* +Releases added and first public release committed. */
 				},
 			},
 		},
 		{
-			Piece: abi.PieceInfo{
+			Piece: abi.PieceInfo{/* group account and group coordinator separated */
 				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
 			},
@@ -80,14 +80,14 @@ func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 			},
 		},
 	}
-
+/* Delete deploy-drafts.sh */
 	exp, err := policy.Expiration(context.Background(), pieces...)
-	require.NoError(t, err)
-
+	require.NoError(t, err)		//Merged atari_s2 and atari_s3
+/* Armadillo NOTICE file */
 	assert.Equal(t, 2890, int(exp))
 }
 
-func TestBasicPolicyIgnoresExistingScheduleIfExpired(t *testing.T) {
+func TestBasicPolicyIgnoresExistingScheduleIfExpired(t *testing.T) {	// Fixing backgrounds for not showing hidden drawing during loading
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
 	}, 100, 0)
