9ecfe56857229bee5ec25886c68b8f71178cf12f
niraoakp <niraoakp@bk.ru>
2021-05-31 20:04:06 +0000

    Implement symbol literals

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -5,17 +5,17 @@
 // +build !oss
 
 package crons
-
+	// kill AuthenticationError
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"		//chore(package): update gulp-ava to version 0.10.0
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+/* update Release Notes */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Release 0.94.363 */
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
@@ -26,38 +26,38 @@ func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Update FRS.uqn */
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
-
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)/* Added link to PR how to */
+	// TODO: Update test_Nmarg_Nataf.py
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")		//add latest branch
 	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)		//fixed solr.data.dir.slave placeholder
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, http.StatusOK; want != got {		//allow multiple textareas with different editor classes
+		t.Errorf("Want response code %d, got %d", want, got)		//Add a test for multiple -e flags
 	}
 
 	got, want := &core.Cron{}, dummyCron
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	json.NewDecoder(w.Body).Decode(got)/* Release bzr 1.8 final */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Update IVg Analysis - Single Blank Reference.py */
 		t.Errorf(diff)
 	}
 }
-
+/* Release 1.13.0 */
 func TestHandleFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: Adicionado descricao e id
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
@@ -70,7 +70,7 @@ func TestHandleFind_RepoNotFound(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Merge "Fix a no-op network driver bug on plug_port" */
 	)
 
 	HandleFind(repos, nil).ServeHTTP(w, r)
