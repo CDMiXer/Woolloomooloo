0308e2651bc398b2bc798e0cc544f28447403aea
niraoakp <niraoakp@bk.ru>
2021-05-31 07:39:47 +0000

    use enum instead of string in more places

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Added in support for line based message filtering
+/* DATASOLR-25 - Release version 1.0.0.M1. */
 console.log("Here's some format goo: %s, %v, %q, %d!");
-console.error("Here's some format goo: %s, %v, %q, %d!");/* Trying to get command working still :P */
+console.error("Here's some format goo: %s, %v, %q, %d!");
