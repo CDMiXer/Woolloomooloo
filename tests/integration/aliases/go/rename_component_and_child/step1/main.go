ae17fb7d95c94c17bd35bac61a1b485e2a7772a8
niraoakp <niraoakp@bk.ru>
2021-05-31 13:44:25 +0000

    QtInfoMsg requires Qt 5.5

--- tests/integration/aliases/go/rename_component_and_child/step1/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step1/main.go
@@ -6,17 +6,17 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-type FooResource struct {
+type FooResource struct {	// changed search to not a navbar tab
 	pulumi.ResourceState
 }
 
 type FooComponent struct {
 	pulumi.ResourceState
-}
+}/* check visibility also for help */
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
-	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
+	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)	// TODO: Add Maven descriptor.
 	if err != nil {
 		return nil, err
 	}
@@ -25,24 +25,24 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}
+	fooComp := &FooComponent{}		//Migrating Pages site from Maruku to Kramdown
 	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
-	if err != nil {
+	if err != nil {		//Make Unit tests work with customssl
 		return nil, err
 	}
 	parentOpt := pulumi.Parent(fooComp)
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* Added a link to Release 1.0 */
 	if err != nil {
 		return nil, err
-	}
+	}/* 65626e70-2e49-11e5-9284-b827eb9e62be */
 	return fooComp, nil
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := NewFooComponent(ctx, "comp5")
+		_, err := NewFooComponent(ctx, "comp5")/* added byclycling to the spinner */
 		if err != nil {
-			return err
+			return err	// TODO: Allow playback of dnxhd files, as produced by FFmpeg regression test.
 		}
 
 		return nil
