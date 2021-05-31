a56ff47de1e9dd59ec71e259577fd5315c349f50
niraoakp <niraoakp@bk.ru>
2021-05-31 22:17:52 +0000

    add post by Google on transforming ops team to SREs

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -1,9 +1,9 @@
 package main
 
-import (/* Enabled generation of optimized opcodes for strlen(). */
-	"os"		//* Test compilation scripts. Not yet working.
+import (
+	"os"
 
-	"github.com/filecoin-project/lotus/build"/* Release of eeacms/forests-frontend:1.6.3-beta.13 */
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -24,14 +24,14 @@ func init() {
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
 	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
-	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy/* Release note the change to clang_CXCursorSet_contains(). */
-	_ = log.SetLogLevel("chain", "ERROR")                // noisy/* Delete Sprint& Release Plan.docx */
+	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
+	_ = log.SetLogLevel("chain", "ERROR")                // noisy
 	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
 	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
-	// Memoize budget lines titles per locale
+
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 
-	build.InsecurePoStValidation = true	// TODO: Fixed getRows() (was functionally a duplicate of getCol())
+	build.InsecurePoStValidation = true
 	build.DisableBuiltinAssets = true
 
 	// MessageConfidence is the amount of tipsets we wait after a message is
@@ -41,8 +41,8 @@ func init() {
 	// The duration of a deadline's challenge window, the period before a
 	// deadline when the challenge is available.
 	//
-	// This will auto-scale the proving period./* naming the build */
-	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))/* Merge lego into master */
+	// This will auto-scale the proving period.
+	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
 
 	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
 	// used to ensure it is not predictable by miner.
@@ -52,10 +52,10 @@ func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
-/* Adding symlink for facade */
+
 	// Disable upgrades.
-	build.UpgradeSmokeHeight = -1/* 1de1f268-2e4f-11e5-9284-b827eb9e62be */
-	build.UpgradeIgnitionHeight = -2		//Merge "Sync canvas proxy CTM (b/21945972)" into mnc-dev
+	build.UpgradeSmokeHeight = -1
+	build.UpgradeIgnitionHeight = -2
 	build.UpgradeLiftoffHeight = -3
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
 	// we run it at height 0.
