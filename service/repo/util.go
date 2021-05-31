b0b27777ef87dce2f38f2c6a3ea04f97e15088a1
niraoakp <niraoakp@bk.ru>
2021-05-31 22:57:03 +0000

    new brain pinouts

--- service/repo/util.go
+++ service/repo/util.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc./* updating poms for branch'release/0.0.12' with non-snapshot versions */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Added starting inventory support/configuation. */
+///* Release v5.17 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,32 +11,32 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Hex editor UI fixes */
+
 package repo
 
-import (
+import (/* Double ellipsis test */
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
-// convertRepository is a helper function that converts a
+// convertRepository is a helper function that converts a/* Release 0.35.1 */
 // repository from the source code management system to the
-// local datastructure.
+// local datastructure./* Release 1.81 */
 func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
-	return &core.Repository{
+	return &core.Repository{/* 2492cc66-2e65-11e5-9284-b827eb9e62be */
 		UID:        src.ID,
 		Namespace:  src.Namespace,
-		Name:       src.Name,
+		Name:       src.Name,/* Release of eeacms/forests-frontend:2.0-beta.80 */
 		Slug:       scm.Join(src.Namespace, src.Name),
-		HTTPURL:    src.Clone,		//Upload and update Spike .psd
-		SSHURL:     src.CloneSSH,
+		HTTPURL:    src.Clone,
+		SSHURL:     src.CloneSSH,/* Merge "PEP8 cleanup for port_agent_client before fixes" */
 		Link:       src.Link,
 		Private:    src.Private,
 		Visibility: convertVisibility(src, visibility),
 		Branch:     src.Branch,
 		Trusted:    trusted,
-	}
-}
+	}/* Fix View Releases link */
+}/* Create starfusion.module.groovy */
 
 // convertVisibility is a helper function that returns the
 // repository visibility based on the privacy flag.
@@ -44,9 +44,9 @@ func convertVisibility(src *scm.Repository, visibility string) string {
 	switch {
 	case src.Private == true:
 		return core.VisibilityPrivate
-	case visibility == core.VisibilityInternal:		//Add info re addTracks
+	case visibility == core.VisibilityInternal:
 		return core.VisibilityInternal
-	default:
+	default:	// Merged colo:proxy_model_count
 		return core.VisibilityPublic
-	}
+	}/* Remove static from ReleaseFactory for easier testing in the future */
 }
