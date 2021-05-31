f5e4cba4626377c84aff989bab2785fcaeae1069
niraoakp <niraoakp@bk.ru>
2021-05-31 12:12:34 +0000

    Remove SayThanks badge

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -2,22 +2,22 @@
 
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
-,perm_repo_uid VARCHAR(250)
+,perm_repo_uid VARCHAR(250)	// TODO: will be fixed by witek@enjin.io
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
-,perm_created  INTEGER
+,perm_created  INTEGER	// TODO: Specify simulator in travis.yml
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-		//send list of remove immediately
+
 -- name: create-index-perms-user
 
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);	// Using RobotC's built in functions for simplicity
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
 
--- name: create-index-perms-repo
+-- name: create-index-perms-repo/* Add missing LICENSE file. */
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// TODO: Updating CODEOWNERS: adding azure-agent-extensions
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
