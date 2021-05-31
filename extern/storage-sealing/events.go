2c41e8f76ced33c11e4ff1353796978e77a9eab1
niraoakp <niraoakp@bk.ru>
2021-05-31 11:43:16 +0000

    use service.name instead of service._id

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
 package sealing
 
 import (
-	"context"
+	"context"/* Going to Release Candidate 1 */
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//80ccf5a6-2e4c-11e5-9284-b827eb9e62be
 )
-	// TODO: will be fixed by joshua@yottadb.com
+
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error/* Entwicklungs-Bildressourcen hinzugefügt */
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
