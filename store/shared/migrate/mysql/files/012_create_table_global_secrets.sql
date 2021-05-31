32527aa2435ebd7bb0205fe978270883d27964eb
niraoakp <niraoakp@bk.ru>
2021-05-31 06:24:00 +0000

    Updated MySQL configuration settings

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
-,secret_type              VARCHAR(50)
+,secret_type              VARCHAR(50)/* 0403a89e-2e60-11e5-9284-b827eb9e62be */
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN/* Update README.md with swagger java client */
-,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)/* Flat API in progress */
-);	// Falha no handshake SSL estava provocando um segfault
+,secret_pull_request      BOOLEAN
+,secret_pull_request_push BOOLEAN/* Use GBIF registry key for identifier */
+,UNIQUE(secret_namespace, secret_name)
+);
