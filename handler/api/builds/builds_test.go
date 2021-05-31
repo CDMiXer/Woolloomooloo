5bc6aac667d33bdf7cb3a61be16e40034b4e02f6
niraoakp <niraoakp@bk.ru>
2021-05-31 22:35:44 +0000

    rename src/temporal/resample.c to src/temporal/resampler.c

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,49 +1,49 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: chore(package): update rollup to version 0.61.0
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Merge "Clipboard service keeps separate clipboards per user." */
+/* Release under MIT license */
 // +build !oss
 
-package builds
+package builds	// 4f5346ee-2e4f-11e5-9284-b827eb9e62be
 
-import (
-	"encoding/json"
+import (/* Release Target */
+	"encoding/json"	// Document parameters to register
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
-
+/* relax all deps to ^ */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
+		//Update champagne-tower.cpp
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-	"github.com/sirupsen/logrus"
+	"github.com/google/go-cmp/cmp"	// TODO: will be fixed by hi@antfu.me
+	"github.com/sirupsen/logrus"/* Added validation for select fields */
 )
 
-func init() {
+func init() {/* Release new version 2.4.26: Revert style rules change, as it breaks GMail */
 	logrus.SetOutput(ioutil.Discard)
 }
-
+/* Release 0.30 */
 func TestHandleBuilds(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	want := []*core.Repository{
+	want := []*core.Repository{		//FIX AbstractQueryBuilder filename (part 1)
 		{ID: 1, Slug: "octocat/hello-world"},
-		{ID: 2, Slug: "octocat/spoon-fork"},
+		{ID: 2, Slug: "octocat/spoon-fork"},		//افزودن بخش های جدید
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Released v0.4.6 (bug fixes) */
 
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* CustomPacket PHAR Release */
 
 	got := []*core.Repository{}
 	json.NewDecoder(w.Body).Decode(&got)
