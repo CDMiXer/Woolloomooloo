a3fdd497c9cf69b8b8e79127d55c062be1944c6c
niraoakp <niraoakp@bk.ru>
2021-05-31 16:45:07 +0000

    Add the spec file

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone IO, Inc.		//http_code added to exceptions - getCode() suitable
+// Copyright 2019 Drone IO, Inc./* Update getRelease.Rd */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Latest Infection Unofficial Release */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* template def updated */
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by souzau@yandex.com
+// limitations under the License.
 
 package repos
 
@@ -20,15 +20,15 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
-
+	// Removing http -> https redirect
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Release cascade method. */
-		ctx := r.Context()/* Create Chorus Pro */
-		repo, _ := request.RepoFrom(ctx)/* Release areca-7.3.5 */
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		repo, _ := request.RepoFrom(ctx)
 		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
-		render.JSON(w, repo, 200)/* Fixed "Heat Vision" compositor to work in d3d11 rs. */
+		render.JSON(w, repo, 200)/* Eggdrop v1.8.1 Release Candidate 2 */
 	}
 }
