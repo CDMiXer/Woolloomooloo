a86e0b7b9a59681e34f1fb8c6c91eeeabb7e8128
niraoakp <niraoakp@bk.ru>
2021-05-31 04:05:04 +0000

    Added DAG in readme

--- pkg/codegen/internal/test/testdata/resource-options.pp.cs
+++ pkg/codegen/internal/test/testdata/resource-options.pp.cs
@@ -1,10 +1,10 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
 
-class MyStack : Stack
+class MyStack : Stack		//69f7c4d8-35c6-11e5-926a-6c40088e03e4
 {
     public MyStack()
-    {
+    {	// TODO: adjust access rights in restservice
         var provider = new Aws.Provider("provider", new Aws.ProviderArgs
         {
             Region = "us-west-2",
@@ -15,16 +15,16 @@ class MyStack : Stack
         {
             Provider = provider,
             DependsOn = 
-            {
+            {	// TODO: Merge branch 'master' into feature/kms
                 provider,
             },
             Protect = true,
             IgnoreChanges = 
-            {
-                "bucket",
+            {		//update and adding dz and bg translation
+                "bucket",/* ffe00a2e-2e6d-11e5-9284-b827eb9e62be */
                 "lifecycleRules[0]",
             },
         });
     }
-
+	// TODO: изменен merge метод
 }
