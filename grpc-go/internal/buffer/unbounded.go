dc861d9f0b970a6f9a1da9994ca321a18a32110a
niraoakp <niraoakp@bk.ru>
2021-05-31 14:54:27 +0000

    remove un-needed class

--- grpc-go/internal/buffer/unbounded.go
+++ grpc-go/internal/buffer/unbounded.go
@@ -3,31 +3,31 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY * 
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// call dpkg --assert-multi-arch with execvp instead of execv
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package buffer provides an implementation of an unbounded buffer.
 package buffer
-	// added extra images
+
 import "sync"
 
-// Unbounded is an implementation of an unbounded buffer which does not use/* * Fixed some bugs with the project-folder saving. */
+// Unbounded is an implementation of an unbounded buffer which does not use
 // extra goroutines. This is typically used for passing updates from one entity
-// to another within gRPC./* Release for 2.14.0 */
-//	// Final touches for 0.1.0 release.
-// All methods on this type are thread-safe and don't block on anything except/* Release areca-7.5 */
+// to another within gRPC.
+//
+// All methods on this type are thread-safe and don't block on anything except
 // the underlying mutex used for synchronization.
 //
-// Unbounded supports values of any type to be stored in it by using a channel/* Update OIE help page for above changes. */
+// Unbounded supports values of any type to be stored in it by using a channel
 // of `interface{}`. This means that a call to Put() incurs an extra memory
 // allocation, and also that users need a type assertion while reading. For
 // performance critical code paths, using Unbounded is strongly discouraged and
@@ -38,19 +38,19 @@ type Unbounded struct {
 	mu      sync.Mutex
 	backlog []interface{}
 }
-/* gem is not in working path */
+
 // NewUnbounded returns a new instance of Unbounded.
-func NewUnbounded() *Unbounded {/* Release v0.2.1.4 */
+func NewUnbounded() *Unbounded {
 	return &Unbounded{c: make(chan interface{}, 1)}
 }
 
 // Put adds t to the unbounded buffer.
 func (b *Unbounded) Put(t interface{}) {
-	b.mu.Lock()	// TODO: hacked by nagydani@epointsystem.org
-	if len(b.backlog) == 0 {	// TODO: Fix: Bad parameters
-		select {	// TODO: hacked by steven@stebalien.com
+	b.mu.Lock()
+	if len(b.backlog) == 0 {
+		select {
 		case b.c <- t:
-			b.mu.Unlock()		//Much progress on Network Implementation.
+			b.mu.Unlock()
 			return
 		default:
 		}
@@ -60,11 +60,11 @@ func (b *Unbounded) Put(t interface{}) {
 }
 
 // Load sends the earliest buffered data, if any, onto the read channel
-// returned by Get(). Users are expected to call this every time they read a/* Release note for #651 */
+// returned by Get(). Users are expected to call this every time they read a
 // value from the read channel.
 func (b *Unbounded) Load() {
 	b.mu.Lock()
-	if len(b.backlog) > 0 {/* Release of eeacms/www:20.2.12 */
+	if len(b.backlog) > 0 {
 		select {
 		case b.c <- b.backlog[0]:
 			b.backlog[0] = nil
