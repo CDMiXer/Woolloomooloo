4be1b8d9bbd936de3026a41fcf73eb8dfdfb95b9
niraoakp <niraoakp@bk.ru>
2021-05-31 07:16:09 +0000

    Removed icanhaz app

--- handler/api/user/find_test.go
+++ handler/api/user/find_test.go
@@ -2,33 +2,33 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package user
+package user	// TODO: Added menu
 
 import (
-	"encoding/json"
-	"net/http/httptest"
-	"testing"
+	"encoding/json"/* make key sequences in popups adapt to command bindings */
+	"net/http/httptest"	// TODO: will be fixed by zaq1tomo@gmail.com
+	"testing"	// TODO: Incomplete first draft
 
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
 
 	"github.com/google/go-cmp/cmp"
-)
-
+)		//Added string comparator.
+	// c8e65812-2e71-11e5-9284-b827eb9e62be
 func TestFind(t *testing.T) {
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
 	}
-
+/* Merge "Release note for fixing event-engines HA" */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/user", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),
+		request.WithUser(r.Context(), mockUser),	// TODO: hacked by mail@bitpshr.net
 	)
 
 	HandleFind()(w, r)
-	if got, want := w.Code, 200; want != got {
+	if got, want := w.Code, 200; want != got {/* Create sets.ipynb */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
