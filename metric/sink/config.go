684e51234e064b4365ba2552229d0537d8d4bd8d
niraoakp <niraoakp@bk.ru>
2021-05-31 02:34:51 +0000

    Delete trashed

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -1,35 +1,35 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by magik6k@gmail.com
-//	// TODO: Create nsmutableattribute_String.md
+// Copyright 2019 Drone IO, Inc./* Improved Logging In Debug+Release Mode */
+///* Release Notes for v01-00 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Nebula Config for Travis Build/Release */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Merge pull request #1 from ferhung-mtk/master
-// limitations under the License./* Merge "Release 1.0.0.235 QCACLD WLAN Driver" */
+// See the License for the specific language governing permissions and
+// limitations under the License./* added devtools for documentation */
 
 package sink
 
 // Config configures a Datadog sink.
 type Config struct {
 	Endpoint string
-	Token    string	// TODO: will be fixed by steven@stebalien.com
+	Token    string		//add counter for mapping categories
 
 	License          string
-	Licensor         string/* Release of eeacms/www:19.8.13 */
-	Subscription     string		//fix: Remove redundant import and import cint
-	EnableGithub     bool	// TODO: hacked by caojiaoyue@protonmail.com
+	Licensor         string
+	Subscription     string
+	EnableGithub     bool
 	EnableGithubEnt  bool
 	EnableGitlab     bool
-	EnableBitbucket  bool/* improve totalvi coverage */
+	EnableBitbucket  bool
 	EnableStash      bool
 	EnableGogs       bool
 	EnableGitea      bool
 	EnableAgents     bool
-	EnableNomad      bool
+	EnableNomad      bool	// Merge "Title: Always add title to LinkCache when necessary (in 3 methods)"
 	EnableKubernetes bool
 }
