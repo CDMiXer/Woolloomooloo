72694d90d8fa6d72bcb30c253d1abcc60cac2438
niraoakp <niraoakp@bk.ru>
2021-05-31 08:24:20 +0000

    fix: skip timezone script

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// 3bb50134-2e47-11e5-9284-b827eb9e62be
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from component import Component
-/* Released v1.3.5 */
-component_a = Component("a", echo=42)
-component_b = Component("b", echo=component_a.echo)/* New Release info. */
-component_c = Component("c", echo=component_a.childId)	// TODO: hacked by nick@perfectabstractions.com
+
+component_a = Component("a", echo=42)/* Release Refresh Build feature */
+component_b = Component("b", echo=component_a.echo)
+component_c = Component("c", echo=component_a.childId)
