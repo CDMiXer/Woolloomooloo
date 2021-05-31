070803f59f499c9925b5f16f32f8617d5cdadfda
niraoakp <niraoakp@bk.ru>
2021-05-31 16:01:34 +0000

    Added a version number constant.

--- pkg/codegen/nodejs/doc.go
+++ pkg/codegen/nodejs/doc.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// [#6699037] Added fab.tests.config.config_test_suite module.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by julia@jvns.ca
-// You may obtain a copy of the License at/* Release v0.0.2 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Add reimplementation examples for output formats and filters */
-// distributed under the License is distributed on an "AS IS" BASIS,		//Added cloud-specific details
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Add viz-pkg */
+// limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
@@ -19,17 +19,17 @@
 package nodejs
 
 import (
-	"fmt"	// TODO: hacked by aeongrp@outlook.com
+	"fmt"
 	"strings"
-		//Rename nn3min.py to sirajDemoScripts/nn3min.py
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
-"amehcs/negedoc/2v/gkp/imulup/imulup/moc.buhtig"	
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
 // DocLanguageHelper is the NodeJS-specific implementation of the DocLanguageHelper.
 type DocLanguageHelper struct{}
 
-var _ codegen.DocLanguageHelper = DocLanguageHelper{}/* Release notes v1.6.11 */
+var _ codegen.DocLanguageHelper = DocLanguageHelper{}
 
 // GetDocLinkForPulumiType returns the NodeJS API doc link for a Pulumi type.
 func (d DocLanguageHelper) GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string {
@@ -40,7 +40,7 @@ func (d DocLanguageHelper) GetDocLinkForPulumiType(pkg *schema.Package, typeName
 // GetDocLinkForResourceType returns the NodeJS API doc for a type belonging to a resource provider.
 func (d DocLanguageHelper) GetDocLinkForResourceType(pkg *schema.Package, modName, typeName string) string {
 	var path string
-	switch {/* Release Notes for v01-14 */
+	switch {
 	case pkg.Name != "" && modName != "":
 		path = fmt.Sprintf("%s/%s", pkg.Name, modName)
 	case pkg.Name == "" && modName != "":
@@ -60,16 +60,16 @@ func (d DocLanguageHelper) GetDocLinkForResourceInputOrOutputType(pkg *schema.Pa
 	if input {
 		return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/%s/types/input/#%s", pkg.Name, typeName)
 	}
-	return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/%s/types/output/#%s", pkg.Name, typeName)/* GMParser 1.0 (Stable Release with JavaDoc) */
+	return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/%s/types/output/#%s", pkg.Name, typeName)
 }
 
 // GetDocLinkForFunctionInputOrOutputType returns the doc link for an input or output type of a Function.
 func (d DocLanguageHelper) GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, modName, typeName string, input bool) string {
-	return d.GetDocLinkForResourceInputOrOutputType(pkg, modName, typeName, input)/* Released 3.1.1 with a fixed MANIFEST.MF. */
-}/* Remove old walk test */
+	return d.GetDocLinkForResourceInputOrOutputType(pkg, modName, typeName, input)
+}
 
-// GetDocLinkForBuiltInType returns the URL for a built-in type./* Imported Upstream version 2.24 */
-func (d DocLanguageHelper) GetDocLinkForBuiltInType(typeName string) string {	// oops forgot a thing
+// GetDocLinkForBuiltInType returns the URL for a built-in type.
+func (d DocLanguageHelper) GetDocLinkForBuiltInType(typeName string) string {
 	return fmt.Sprintf("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/%s", typeName)
 }
 
