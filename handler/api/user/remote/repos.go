75fd14d07a5941cf5588f6da37bccb996dc513fd
niraoakp <niraoakp@bk.ru>
2021-05-31 14:11:20 +0000

    e77ace58-2e5c-11e5-9284-b827eb9e62be

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
-//		//docs(@angular/cli): fix schema.json description for `lazyModules`
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Create HOWTO-CFP.md
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Add note about discontinued API keys */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v3.6.5 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* update google auth to not use plus api */
+// limitations under the License.
 
-package remote/* Aggiunto pezzo di CSS */
+package remote
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"		//Fixes for running test suite with HPC.
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-/* rename settings */
+
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())/* 0.16.0: Milestone Release (close #23) */
+		viewer, _ := request.UserFrom(r.Context())
 
 		list, err := repos.List(r.Context(), viewer)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).		//Added new packages
+			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list remote repositories")
 		} else {
 			render.JSON(w, list, 200)
