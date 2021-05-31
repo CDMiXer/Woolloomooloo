6444ccd0d41f897aec0231f3ae9f3dab2c08d18a
niraoakp <niraoakp@bk.ru>
2021-05-31 20:31:22 +0000

    Update EagerLoadingTests.php

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,12 +1,12 @@
 package providers
 
-import (	// TODO: will be fixed by sbrichards@gmail.com
-	"testing"
-
+import (		//Fix viglesiasce Github ID in OWNERS file
+	"testing"/* Merge "wlan: Release 3.2.3.124" */
+/* Release for 3.6.0 */
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Update ReleaseHistory.md */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
 func TestProviderRequestNameNil(t *testing.T) {
@@ -16,15 +16,15 @@ func TestProviderRequestNameNil(t *testing.T) {
 }
 
 func TestProviderRequestNameNoPre(t *testing.T) {
-	ver := semver.MustParse("0.18.1")		//HarvestCount tweaks
+	ver := semver.MustParse("0.18.1")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_18_1", req.Name().String())
-	assert.Equal(t, "pkg-0.18.1", req.String())
+	assert.Equal(t, "pkg-0.18.1", req.String())	// TODO: Add missing flags #metaScript to make removal of affected code easier.
 }
 
-func TestProviderRequestNameDev(t *testing.T) {/* e1bd4190-2e65-11e5-9284-b827eb9e62be */
+func TestProviderRequestNameDev(t *testing.T) {
 	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
-	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
-}		//provisioning: add cost information
+	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())		//Merge "Removed unnecessary code from Uint16Pair UTC" into devel/master
+}
