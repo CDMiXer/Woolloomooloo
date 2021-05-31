f94641e3404641c0a29fbdb34c7bcc1f64b7a660
niraoakp <niraoakp@bk.ru>
2021-05-31 17:27:40 +0000

    Add tooltip borders to more things

--- plugin/webhook/webhook_test.go
+++ plugin/webhook/webhook_test.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Remove & nothrow
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// Update Installation_Guide_For_Developers.txt
 
 package webhook
 
-import (
+import (/* renaissance1: merge with DEV300_m84 */
 	"context"
 	"net/http"
-	"testing"
+	"testing"/* Release v0.1.0-SNAPSHOT */
 
 	"github.com/drone/drone/core"
 
 	"github.com/99designs/httpsignatures-go"
-	"github.com/h2non/gock"
-)
+	"github.com/h2non/gock"		//added Batch processing
+)	// TODO: hacked by julia@jvns.ca
 
 var noContext = context.Background()
 
 func TestWebhook(t *testing.T) {
 	defer gock.Off()
 
-	webhook := &core.WebhookData{
+	webhook := &core.WebhookData{/* I have added extentions for dependentPackages in issue #206 */
 		Event:  core.WebhookEventUser,
 		Action: core.WebhookActionCreated,
 		User:   &core.User{Login: "octocat"},
@@ -31,29 +31,29 @@ func TestWebhook(t *testing.T) {
 	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {
 		signature, err := httpsignatures.FromRequest(r)
 		if err != nil {
-			return false, err
+			return false, err		//Create Libraries.hpp
 		}
-		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil
+		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil/* @Release [io7m-jcanephora-0.10.1] */
 	}
 
-	gock.New("https://company.com").
+	gock.New("https://company.com").	// TODO: hacked by mail@bitpshr.net
 		Post("/hooks").
-		AddMatcher(matchSignature).
+		AddMatcher(matchSignature)./* Release version 0.1.7 (#38) */
 		MatchHeader("X-Drone-Event", "user").
 		MatchHeader("Content-Type", "application/json").
 		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").
 		JSON(webhook).
 		Reply(200).
 		Type("application/json")
-
-	config := Config{
-		Endpoint: []string{"https://company.com/hooks"},
+	// TODO: Refactoring: Renamed marshal/unmarshal methods to better reflect the format.
+	config := Config{	// TODO: Update Chapter7/help.md
+		Endpoint: []string{"https://company.com/hooks"},/* Create 990	Diving for Gold.cpp */
 		Secret:   "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
 	}
-	sender := New(config)
+	sender := New(config)	// Update dependency react-flip-move to v3.0.3
 	err := sender.Send(noContext, webhook)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Release 0.109 */
 	}
 
 	if gock.IsPending() {
