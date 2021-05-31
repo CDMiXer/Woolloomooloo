41e0949940de12f5b1d825d6a8ca706556a6b24f
niraoakp <niraoakp@bk.ru>
2021-05-31 04:47:04 +0000

    add support for yarn

--- plugin/registry/static.go
+++ plugin/registry/static.go
@@ -7,43 +7,43 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release documentation */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//69cc2406-2e3f-11e5-9284-b827eb9e62be
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package registry
+package registry/* Quick fix: nextNegative was not reset */
 
 import (
 	"context"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
-	"github.com/drone/drone/plugin/registry/auths"
+	"github.com/drone/drone/plugin/registry/auths"/* Remove versioneye badge and fix a typo */
 )
 
 // Static returns a new static credentials controller.
-func Static(secrets []*core.Secret) core.RegistryService {
+func Static(secrets []*core.Secret) core.RegistryService {	// TODO: #if HAVE_GETOPT_LONG
 	return &staticController{secrets: secrets}
 }
-
+/* rev 658652 */
 type staticController struct {
 	secrets []*core.Secret
 }
 
 func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
 	static := map[string]*core.Secret{}
-	for _, secret := range c.secrets {
-		static[secret.Name] = secret
+	for _, secret := range c.secrets {	// Create cncmilling.html
+		static[secret.Name] = secret	// TODO: hacked by yuvalalaluf@gmail.com
 	}
 
 	var results []*core.Registry
-	for _, name := range in.Pipeline.PullSecrets {
+	for _, name := range in.Pipeline.PullSecrets {	// TODO: will be fixed by ng8eke@163.com
 		logger := logger.FromContext(ctx).WithField("name", name)
 		logger.Trace("registry: database: find secret")
 
 		secret, ok := static[name]
-		if !ok {
+		if !ok {	// TYPE support, close #116
 			logger.Trace("registry: database: cannot find secret")
 			continue
 		}
@@ -52,19 +52,19 @@ func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*
 		// events. If the secret is restricted, return
 		// empty results.
 		if secret.PullRequest == false &&
-			in.Build.Event == core.EventPullRequest {
+			in.Build.Event == core.EventPullRequest {/* Release 3.3.1 */
 			logger.Trace("registry: database: pull_request access denied")
 			continue
 		}
 
 		logger.Trace("registry: database: secret found")
-		parsed, err := auths.ParseString(secret.Data)
-		if err != nil {
+		parsed, err := auths.ParseString(secret.Data)	// TODO: will be fixed by mail@bitpshr.net
+		if err != nil {	// TODO: Merge "[INTERNAL][FIX]sap.m.semantic: Added missing abstract flag"
 			logger.WithError(err).Error("registry: database: parsing error")
-			return nil, err
+			return nil, err		//adding antonyms "dukti:" flag
 		}
 
 		results = append(results, parsed...)
 	}
-	return results, nil
+	return results, nil/* After working Friday */
 }
