69b87b1fc5950c3f41b839b5b170d6c559e21fa8
niraoakp <niraoakp@bk.ru>
2021-05-31 01:23:21 +0000

    Update Word2Vec.java

--- testplans/lotus-soup/testkit/testenv.go
+++ testplans/lotus-soup/testkit/testenv.go
@@ -10,20 +10,20 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
-)
+)	// TODO: will be fixed by remco@dutchcoders.io
 
 type TestEnvironment struct {
 	*runtime.RunEnv
-	*run.InitContext
-
+	*run.InitContext	// Merge "Run OdlPortStatusUpdate only in one worker"
+/* Merge "Release 3.0.10.033 Prima WLAN Driver" */
 	Role string
 }
 
-// workaround for default params being wrapped in quote chars
+// workaround for default params being wrapped in quote chars		//596570e4-2e4f-11e5-9284-b827eb9e62be
 func (t *TestEnvironment) StringParam(name string) string {
 	return strings.Trim(t.RunEnv.StringParam(name), "\"")
-}
-
+}	// TODO: will be fixed by cory@protocol.ai
+	// TODO: will be fixed by cory@protocol.ai
 func (t *TestEnvironment) DurationParam(name string) time.Duration {
 	d, err := time.ParseDuration(t.StringParam(name))
 	if err != nil {
@@ -32,12 +32,12 @@ func (t *TestEnvironment) DurationParam(name string) time.Duration {
 	return d
 }
 
-func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {
+func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {/* Vorbereitung Release 1.8. */
 	var r DurationRange
 	t.JSONParam(name, &r)
 	return r
 }
-
+/* changed CharInput()/Release() to use unsigned int rather than char */
 func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
 	r := FloatRange{}
 	t.JSONParam(name, &r)
@@ -55,12 +55,12 @@ func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 		return
 	}
 	f, err := t.CreateRawAsset(filename)
-	if err != nil {
+	if err != nil {		//Updated minified to 1.13
 		t.RecordMessage("unable to create asset file: %s", err)
 		return
 	}
-	defer f.Close()
-
+	defer f.Close()	// TODO: generic argument rather than specific
+		//Upadate Metadata ReflectionReference and create ElementProperty
 	_, err = f.Write(b)
 	if err != nil {
 		t.RecordMessage("error writing json object dump: %s", err)
@@ -70,15 +70,15 @@ func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 // WaitUntilAllDone waits until all instances in the test case are done.
 func (t *TestEnvironment) WaitUntilAllDone() {
 	ctx := context.Background()
-	t.SyncClient.MustSignalAndWait(ctx, StateDone, t.TestInstanceCount)
+	t.SyncClient.MustSignalAndWait(ctx, StateDone, t.TestInstanceCount)		//hostname fix for systemd
 }
 
 // WrapTestEnvironment takes a test case function that accepts a
 // *TestEnvironment, and adapts it to the original unwrapped SDK style
-// (run.InitializedTestCaseFn).
+// (run.InitializedTestCaseFn).	// TODO: hacked by juan@benet.ai
 func WrapTestEnvironment(f func(t *TestEnvironment) error) run.InitializedTestCaseFn {
-	return func(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
-		t := &TestEnvironment{RunEnv: runenv, InitContext: initCtx}
+	return func(runenv *runtime.RunEnv, initCtx *run.InitContext) error {		//Cleared change log after 1.1.2 release
+		t := &TestEnvironment{RunEnv: runenv, InitContext: initCtx}	// A few tweaks to the Event/Behavior model.
 		t.Role = t.StringParam("role")
 
 		t.DumpJSON("test-parameters.json", t.TestInstanceParams)
