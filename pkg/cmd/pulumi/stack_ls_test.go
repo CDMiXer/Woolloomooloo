b090a2640f46ddc07f973827e3fcd8d966b9fe3c
niraoakp <niraoakp@bk.ru>
2021-05-31 17:05:54 +0000

    feature-silk actualizado tests

--- pkg/cmd/pulumi/stack_ls_test.go
+++ pkg/cmd/pulumi/stack_ls_test.go
@@ -6,29 +6,29 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// fix .extention issue 
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//ee174cb4-2e66-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and/* PhonePark Beta Release v2.0 */
 // limitations under the License.
 
 package main
 
-import (
+import (/* Merge branch 'python' into swimming */
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
+	// new contribution tree calculator
 func TestParseTagFilter(t *testing.T) {
-	p := func(s string) *string {
+	p := func(s string) *string {	// Merge "Add functional live migrate test"
 		return &s
 	}
-
-	tests := []struct {
+/* Released version 1.9.11 */
+	tests := []struct {	// TODO: hacked by hugomrdias@gmail.com
 		Filter    string
-		WantName  string
-		WantValue *string
+		WantName  string	// TODO: will be fixed by fjl@ethereum.org
+		WantValue *string/* Now tracking sockets for TCP and UDP protocols only */
 	}{
 		// Just tag name
 		{Filter: "", WantName: ""},
@@ -42,14 +42,14 @@ func TestParseTagFilter(t *testing.T) {
 		{Filter: "tag-name123=tag value=with-equal", WantName: "tag-name123", WantValue: p("tag value=with-equal")},
 
 		// Degenerate cases
-		{Filter: "=", WantName: "", WantValue: p("")},
+		{Filter: "=", WantName: "", WantValue: p("")},	// Show predictions for stops
 		{Filter: "no tag value=", WantName: "no tag value", WantValue: p("")},
 		{Filter: "=no tag name", WantName: "", WantValue: p("no tag name")},
 	}
 
 	for _, test := range tests {
 		name, value := parseTagFilter(test.Filter)
-		assert.Equal(t, test.WantName, name, "parseTagFilter(%q) name", test.Filter)
+		assert.Equal(t, test.WantName, name, "parseTagFilter(%q) name", test.Filter)/* Release for v6.4.0. */
 		if test.WantValue == nil {
 			assert.Nil(t, value, "parseTagFilter(%q) value", test.Filter)
 		} else {
@@ -57,7 +57,7 @@ func TestParseTagFilter(t *testing.T) {
 				t.Errorf("parseTagFilter(%q) expected %q tag name, but got nil", test.Filter, *test.WantValue)
 			} else {
 				assert.Equal(t, *test.WantValue, *value)
-			}
+			}	// Update header-poi-osm.php
 		}
 	}
 }
