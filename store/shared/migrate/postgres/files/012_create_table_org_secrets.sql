4113fdc251b93bec103dc5553662eac07b1abf17
niraoakp <niraoakp@bk.ru>
2021-05-31 21:09:53 +0000

    Added equals and hashCode methods

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
-
+		//Merge "Remove unnecessary spaces in test data JSON file"
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                SERIAL PRIMARY KEY	// Update with new VPN address
+ secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
-,secret_name              VARCHAR(200)
+,secret_name              VARCHAR(200)/* Release 2.0.0: Update to Jexl3 */
 ,secret_type              VARCHAR(50)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN	// TODO: Update URL to main pancancer_launcher readme
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);
+);/* Set up GitHub action to compile the code */
