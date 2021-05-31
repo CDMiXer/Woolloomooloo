28d802e350593fe516f78069b3b444c241f57f7c
niraoakp <niraoakp@bk.ru>
2021-05-31 23:18:24 +0000

    Only show video name instead of full path for subs logging (#482)

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//feat(#51):Incluir la FP Básica 
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//Flag update (ui[15])
+// You may obtain a copy of the License at	// TODO: will be fixed by nicksavers@gmail.com
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//[all] fixed some textures not properly converted
-//	// linkify resources in danger report
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release camera when app pauses. */
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/* fix log4j and startup */
+// limitations under the License.	// TODO: hacked by davidad@alum.mit.edu
+
 // +build oss
 
-package builds		//update wording in readme
-	// TODO: Changed a non-English Notification
+package builds
+
 import (
-	"net/http"/* Update CM202 - Cronog */
-	// TODO: hacked by magik6k@gmail.com
+	"net/http"
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* compiler.cfg.phi-elimination: no longer needed */
+	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-		//add demo link
+
 // HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {		//Very generic pyFAI integrator for ID31
-	return notImplemented		//Adding date for Order Details
-}
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+	return notImplemented
+}/* re-add release-plugin */
