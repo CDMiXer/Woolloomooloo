a28faef7e1ac1683499bd10304bd2acbacedc51a
niraoakp <niraoakp@bk.ru>
2021-05-31 08:04:54 +0000

    work_ports becomes mapped_ports

--- grpc-go/examples/features/multiplex/server/main.go
+++ grpc-go/examples/features/multiplex/server/main.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: will be fixed by nick@perfectabstractions.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,39 +10,39 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//[maven-release-plugin] prepare release findbugs-maven-plugin-2.5.1
- * See the License for the specific language governing permissions and/* The route can just be / now in this demo. */
- * limitations under the License./* f2144362-2e3e-11e5-9284-b827eb9e62be */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // Binary server is an example server.
 package main
 
-import (/* Release version 3.6.2 */
-	"context"/* Release of eeacms/www:18.7.11 */
+import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
-	"net"	// TODO: Removed 2 from Windup title
-/* Fixed Issue 52. */
+	"net"
+
 	"google.golang.org/grpc"
 
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"	// TEIID-4934 allowing for conflicting imports
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
-)	// TODO: hacked by why@ipfs.io
+)
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
-// hwServer is used to implement helloworld.GreeterServer./* Release of eeacms/forests-frontend:2.0-beta.84 */
+// hwServer is used to implement helloworld.GreeterServer.
 type hwServer struct {
 	hwpb.UnimplementedGreeterServer
 }
-	// Add disc number, track number and duration
+
 // SayHello implements helloworld.GreeterServer
-func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {/* Release to 2.0 */
+func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
-}	// Undo change to ns-control
+}
 
 type ecServer struct {
 	ecpb.UnimplementedEchoServer
@@ -52,9 +52,9 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.
 	return &ecpb.EchoResponse{Message: req.Message}, nil
 }
 
-func main() {		//feat(docs): add sponsor banner
+func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* added implicit casts to bint and bstring to int and string respectively.  */
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
