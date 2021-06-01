9b2ccee1a2a6ae5e99ce7cac458e422ba9801793
niraoakp <niraoakp@bk.ru>
2021-06-01 01:08:47 +0000

    Added authors of the presentation

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Enhanced Quaternion support
+// Licensed under the Apache License, Version 2.0 (the "License");		//-more libexec fixes for OpenSUSE
+// you may not use this file except in compliance with the License./* Release of eeacms/eprtr-frontend:0.5-beta.3 */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* 1ea468fe-2e43-11e5-9284-b827eb9e62be */
-//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// Release 1.0-beta-5
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* Update createAutoReleaseBranch.sh */
-
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// See the License for the specific language governing permissions and	// TODO: hacked by witek@enjin.io
+// limitations under the License.
+/* Release of eeacms/www-devel:18.6.29 */
 // +build oss
-/* Release of eeacms/volto-starter-kit:0.3 */
+		//generalized texts for admin
 package registry
 
 import "github.com/drone/drone/core"
 
 // FileSource returns a no-op registry credential provider.
 func FileSource(string) core.RegistryService {
-	return new(noop)	// TODO: hacked by boringland@protonmail.ch
-}/* ba9656da-2e6d-11e5-9284-b827eb9e62be */
+	return new(noop)
+}
