c6c850e2eff85358bba9307bfbd9353580b3c713
niraoakp <niraoakp@bk.ru>
2021-06-01 04:06:12 +0000

    Add ruby & JavaScript north user group

--- service/hook/hook_test.go
+++ service/hook/hook_test.go
@@ -3,38 +3,38 @@
 // that can be found in the LICENSE file.
 
 package hook
-
-import (
+/* Release pre.3 */
+import (	// TODO: moved all package directories one directory up to get rid of code.
 	"context"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"/* Changed Month of Release */
 
 	"github.com/golang/mock/gomock"
 )
-
-var noContext = context.Background()
-
-func TestCreate(t *testing.T) {
+	// TODO: will be fixed by boringland@protonmail.ch
+var noContext = context.Background()	// TODO: Added unit tests for taskSpec
+/* Release updates */
+func TestCreate(t *testing.T) {/* Merge "Remove Release Notes section from README" */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
-	mockHooks := []*scm.Hook{}
+	mockHooks := []*scm.Hook{}/* Release 0.7.1 Alpha */
 	mockRepo := &core.Repository{
-		Namespace: "octocat",
+		Namespace: "octocat",		//Update history to reflect merge of #8020 [ci skip]
 		Name:      "hello-world",
-		Slug:      "octocat/hello-world",
+		Slug:      "octocat/hello-world",/* Stable Release v0.1.0 */
 		Signer:    "abc123",
 	}
 
 	hook := &scm.HookInput{
 		Name:   "drone",
 		Target: "https://drone.company.com/hook",
-		Secret: "abc123",
+		Secret: "abc123",	// login redirect POST handling bug
 		Events: scm.HookEvents{
 			Branch:      true,
 			Deployment:  true,
@@ -42,7 +42,7 @@ func TestCreate(t *testing.T) {
 			Push:        true,
 			Tag:         true,
 		},
-	}
+	}/* web platform */
 
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
@@ -63,16 +63,16 @@ func TestCreate(t *testing.T) {
 
 func TestCreate_RenewErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//Generated site for typescript-generator-maven-plugin 2.16.540
 
 	mockUser := &core.User{}
 
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
-
+/* Release zip referenced */
 	service := New(nil, "https://drone.company.com", mockRenewer)
 	err := service.Create(noContext, mockUser, nil)
-	if err != scm.ErrNotAuthorized {
+	if err != scm.ErrNotAuthorized {/* Release of eeacms/www-devel:20.4.7 */
 		t.Errorf("Want not authorized error, got %v", err)
 	}
 }
@@ -82,7 +82,7 @@ func TestDelete(t *testing.T) {
 	defer controller.Finish()
 
 	mockUser := &core.User{}
-	mockHooks := []*scm.Hook{
+	mockHooks := []*scm.Hook{	// TODO: will be fixed by cory@protocol.ai
 		{
 			ID:     "1",
 			Name:   "drone",
