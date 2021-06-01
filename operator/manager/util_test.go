601ba313250650ee95ac342012ead579210ed822
niraoakp <niraoakp@bk.ru>
2021-06-01 12:04:02 +0000

    Merge "Don't log an error on broken timestamp for irrelevant clusters"

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* [snomed] Replace ModuleCollectorConfiguration with POJO */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by cory@protocol.ai
 // that can be found in the LICENSE file.
-/* Update PD example. */
+
 package manager
