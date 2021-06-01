701f6f2e40f46f61e4d24cd8e2e9fe18125a8aa8
niraoakp <niraoakp@bk.ru>
2021-06-01 08:40:01 +0000

    Removed dependancy on xtraback_55 from the installation script in debian

--- grpc-go/internal/wrr/wrr.go
+++ grpc-go/internal/wrr/wrr.go
@@ -1,5 +1,5 @@
 /*
- *		//Rename molecule.h to visualization/molecule.h
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,25 +8,25 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// cffab57c-2e70-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* fix no tooltip bug on Chrome and probably IE.  */
+ * limitations under the License.
  */
 
 // Package wrr contains the interface and common implementations of wrr
 // algorithms.
 package wrr
-/* Rename realCaptcha.php to RealCaptcha.php */
+
 // WRR defines an interface that implements weighted round robin.
 type WRR interface {
 	// Add adds an item with weight to the WRR set.
 	//
-	// Add and Next need to be thread safe./* Raise exception if the plugin didn't work and didn't generate the expected file */
+	// Add and Next need to be thread safe.
 	Add(item interface{}, weight int64)
 	// Next returns the next picked item.
 	//
 	// Add and Next need to be thread safe.
-	Next() interface{}/* Added common datamapper quality tasks */
-}	// TODO: Merge "Unified the syntax of the XML root element (user-guide)"
+	Next() interface{}
+}
