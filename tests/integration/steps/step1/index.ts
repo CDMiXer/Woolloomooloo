76a5bb146711146537cc61c2f868d78aa90adea2
niraoakp <niraoakp@bk.ru>
2021-06-01 16:55:00 +0000

    Demodocs: Add data description

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by mikeal.rogers@gmail.com
 
 import { Resource } from "./resource";
-
-// Step 1: Populate the world:		//Fixed Cereal compilation bug on Android
-// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property./* Allow to define auxiliary variables in global models. */
+		//GUAC-1275: Netherlands keyboard layout filename replaced
+// Step 1: Populate the world:/* Update data.ino */
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
 let a = new Resource("a", { state: 1 });
-let b = new Resource("b", { state: 1 });/* Перед обновлением. */
-let c = new Resource("c", { state: 1, resource: a });
+let b = new Resource("b", { state: 1 });
+let c = new Resource("c", { state: 1, resource: a });/* added sag stack fix for webkit */
 let d = new Resource("d", { state: 1 });
 // Checkpoint: a1, b1, c1, d1
