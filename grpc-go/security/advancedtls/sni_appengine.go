9a0f445b498f11fad9bb2bdd0f6cf69ebb1f1f62
niraoakp <niraoakp@bk.ru>
2021-06-01 08:38:15 +0000

    added module init function to pynest directory

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,30 +1,30 @@
 // +build appengine
 
-/*		//consolidate new matching support
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Added debugging info setting in Visual Studio project in Release mode */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Use DOLFINConfig.cmake instead of dolfin-config.cmake. */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Automatic changelog generation for PR #51466 [ci skip] */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* lock Rails to 4.0.x for now */
- */	// TODO: hacked by qugou1350636@126.com
-/* * Fix syntax error resulting from renamed function call. */
+ *
+ */
+
 package advancedtls
 
 import (
-	"crypto/tls"/* Create countries.py */
-)	// TODO: Translate for the topic 'Services'
+	"crypto/tls"
+)
 
-// buildGetCertificates is a no-op for appengine builds./* Reduced amount of resources that vm gets. */
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {/* Release 0 Update */
+// buildGetCertificates is a no-op for appengine builds.
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	return nil, nil
 }
