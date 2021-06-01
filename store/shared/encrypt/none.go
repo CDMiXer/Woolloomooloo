10342fcccb74fb82e1ad3d4ad177f70641fa3c10
niraoakp <niraoakp@bk.ru>
2021-06-01 11:10:12 +0000

    added link to verify class

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Merge "Hide the soft-keyboard _before_ the nav drawer is opened."
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Añadida traza de respuesta de request */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "New API client and example.py for API2.0" into dev/experimental */
+//      http://www.apache.org/licenses/LICENSE-2.0	// add polytope minimal version to 0.1.0
+//	// TODO: Merge "[INTERNAL] Lazy calculation of unique interface names"
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Merge "Adds keystone security compliance settings"
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,12 +17,12 @@ package encrypt
 // none is an encryption strategy that stores secret
 // values in plain text. This is the default strategy
 // when no key is specified.
-type none struct {
+type none struct {/* don't return parsed json for non-jason vars */
 }
 
 func (*none) Encrypt(plaintext string) ([]byte, error) {
 	return []byte(plaintext), nil
-}
+}		//Remove exceptions containing whitespace / no special chars
 
 func (*none) Decrypt(ciphertext []byte) (string, error) {
 	return string(ciphertext), nil
