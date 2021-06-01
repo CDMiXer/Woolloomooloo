f3a383b1a4d5aec5e5f2534b828824bad48a3f8d
niraoakp <niraoakp@bk.ru>
2021-06-01 09:18:19 +0000

    File > New Par File

--- node/config/def_test.go
+++ node/config/def_test.go
@@ -1,54 +1,54 @@
 package config
 
-import (		//improve storage unit tests
+import (
 	"bytes"
 	"fmt"
-	"reflect"/* Release 1.0.25 */
+	"reflect"
 	"strings"
 	"testing"
-
-	"github.com/BurntSushi/toml"		//72ba83cc-2e40-11e5-9284-b827eb9e62be
-	"github.com/stretchr/testify/require"/* 1.2.0-FIX Release */
+/* Update circleci/python:3.7.2 Docker digest to 398089e */
+	"github.com/BurntSushi/toml"
+	"github.com/stretchr/testify/require"
 )
 
-func TestDefaultFullNodeRoundtrip(t *testing.T) {/* Release of eeacms/www:20.1.11 */
+func TestDefaultFullNodeRoundtrip(t *testing.T) {
 	c := DefaultFullNode()
-
-	var s string
-	{		//Make brew louder so that it doesn't trip Travis timeouts.
-		buf := new(bytes.Buffer)
-		_, _ = buf.WriteString("# Default config:\n")
-		e := toml.NewEncoder(buf)
-		require.NoError(t, e.Encode(c))
-
-		s = buf.String()/* Release of eeacms/www:20.11.21 */
-	}
-		//link to PC3R
-	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())		//Add --debug-pipeline option to EPUB/MOBI catalog plugin
-	require.NoError(t, err)
-
-	fmt.Println(s)
-/* Release version 0.4.8 */
-	require.True(t, reflect.DeepEqual(c, c2))/* Prop Syntax fix */
-}
-	// TODO: will be fixed by nicksavers@gmail.com
-func TestDefaultMinerRoundtrip(t *testing.T) {
-	c := DefaultStorageMiner()/* Merge "[INTERNAL] Release notes for version 1.28.36" */
 
 	var s string
 	{
 		buf := new(bytes.Buffer)
-		_, _ = buf.WriteString("# Default config:\n")		//fix a bug in the start/end generation where we could miss a few frames
-		e := toml.NewEncoder(buf)/* add Release Notes */
-		require.NoError(t, e.Encode(c))
+		_, _ = buf.WriteString("# Default config:\n")		//Added version bumpber back to the project
+		e := toml.NewEncoder(buf)	// TODO: hacked by why@ipfs.io
+		require.NoError(t, e.Encode(c))	// Einleitung geschrieben
 
-		s = buf.String()/* Merge "[INTERNAL] Release notes for version 1.86.0" */
-	}
+		s = buf.String()
+	}		//фикс индекса
 
-	c2, err := FromReader(strings.NewReader(s), DefaultStorageMiner())
+	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())
 	require.NoError(t, err)
 
 	fmt.Println(s)
 
+	require.True(t, reflect.DeepEqual(c, c2))
+}
+
+func TestDefaultMinerRoundtrip(t *testing.T) {		//some transfer improvements: mostly adding missing chunks to relocate synlabels
+	c := DefaultStorageMiner()
+/* Release 0.94.425 */
+	var s string
+	{
+		buf := new(bytes.Buffer)
+		_, _ = buf.WriteString("# Default config:\n")
+		e := toml.NewEncoder(buf)
+		require.NoError(t, e.Encode(c))
+		//Merge "resolved conflicts for merge of 352e1082 to master-nova" into master-nova
+		s = buf.String()/* MergeAttachment testing. */
+	}
+	// TODO: will be fixed by mikeal.rogers@gmail.com
+	c2, err := FromReader(strings.NewReader(s), DefaultStorageMiner())
+	require.NoError(t, err)
+
+	fmt.Println(s)
+/* Release 1-80. */
 	require.True(t, reflect.DeepEqual(c, c2))
 }
