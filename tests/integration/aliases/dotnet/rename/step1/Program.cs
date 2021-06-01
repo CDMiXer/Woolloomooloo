e5c8726b30d57de42654cb10d6cc04ff36b17763
niraoakp <niraoakp@bk.ru>
2021-06-01 08:45:44 +0000

    fix User Guide example

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -1,24 +1,24 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
-using Pulumi;
+using Pulumi;	// TODO: Change NewTransaction to Transaction
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
+        : base("my:module:Resource", name, options)		//reshuffled documentation a bit
     {
     }
 }
-
+/* [artifactory-release] Release version 0.6.2.RELEASE */
 class Program
-{
-    static Task<int> Main(string[] args)
-    {
+{		//#571 removing Appendable cast
+    static Task<int> Main(string[] args)/* Update 152_Maximum_Product_Subarray.md */
+    {/* 46ad08ce-2e61-11e5-9284-b827eb9e62be */
         return Deployment.RunAsync(() => 
-        {
+        {/* [de] grammar.xml: some work on capitalization rules */
             // Scenario #1 - rename a resource
             var res1 = new Resource("res1");
-        });
-    }
+        });	// TODO: Added sample code of NSOLT dictionary learning.
+    }	// TODO: Uhm, yeah ...
 }
