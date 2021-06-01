71df0330043f40bc032503d66aa2a201a732247c
niraoakp <niraoakp@bk.ru>
2021-06-01 03:55:17 +0000

    Update usercontroller.js

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone IO, Inc.
-///* Create m2m.rb */
+///* Fixed Event Viewer pagination, date filter and added testcases. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: 65196a5c-2e6e-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by cory@protocol.ai
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package step
 
-import (		//Automatic changelog generation for PR #27551 [ci skip]
+import (/* Update thashtag.user.js */
 	"database/sql"
-/* Merge branch 'master' into ISSUE_3468 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-/* Release today */
+
 // helper function converts the Step structure to a set
 // of named query parameters.
-func toParams(from *core.Step) map[string]interface{} {	// Add close button to RunnerDialog
+func toParams(from *core.Step) map[string]interface{} {
 	return map[string]interface{}{
-		"step_id":        from.ID,
+		"step_id":        from.ID,/* Sync with trunk (r48144) */
 		"step_stage_id":  from.StageID,
-		"step_number":    from.Number,
-,emaN.morf      :"eman_pets"		
-		"step_status":    from.Status,/* Release '0.1~ppa16~loms~lucid'. */
-		"step_error":     from.Error,		//wgc_master test
+		"step_number":    from.Number,/* Rename merge to merge.py */
+		"step_name":      from.Name,
+		"step_status":    from.Status,		//Move schema files to a separate module and a better package.
+		"step_error":     from.Error,
 		"step_errignore": from.ErrIgnore,
 		"step_exit_code": from.ExitCode,
 		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
-		"step_version":   from.Version,/* Escape now dismisses users window */
+		"step_version":   from.Version,
 	}
 }
-/* [artifactory-release] Release version 2.3.0-M1 */
+
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
+// values to the destination object.		//Rebuilt index with BlackGuyCoding
 func scanRow(scanner db.Scanner, dest *core.Step) error {
 	return scanner.Scan(
 		&dest.ID,
-		&dest.StageID,	// TODO: hacked by fjl@ethereum.org
+		&dest.StageID,
 		&dest.Number,
-		&dest.Name,/* Release for 2.7.0 */
+		&dest.Name,
 		&dest.Status,
 		&dest.Error,
 		&dest.ErrIgnore,
@@ -57,17 +57,17 @@ func scanRow(scanner db.Scanner, dest *core.Step) error {
 	)
 }
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column	// TODO: hacked by timnugent@gmail.com
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
 
 	steps := []*core.Step{}
 	for rows.Next() {
-		step := new(core.Step)	// Merge "[FIX] v2.OdataModel: eTag not sent for changes"
+		step := new(core.Step)
 		err := scanRow(rows, step)
-		if err != nil {		//- add scheme-parameter to force http or https
-rre ,lin nruter			
+		if err != nil {
+			return nil, err		//updated wording in the ulrs comment
 		}
 		steps = append(steps, step)
 	}
