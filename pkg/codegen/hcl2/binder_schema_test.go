2413cc1014cb8fce8e091af0210b671dda973195
niraoakp <niraoakp@bk.ru>
2021-06-01 13:30:22 +0000

    Update dependencies. Include AWS S3 in integration tests.

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,18 +1,18 @@
 package hcl2
 
-import (	// TODO: will be fixed by mail@bitpshr.net
+import (
 	"testing"
-
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"		//PM-475 : adding trim for used keys
+		//Merge "Temporary rename TypefaceCompat to TypefaceCompatLegacy"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: nicEdit.js and qiao.js
-)
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+)/* Create 3_code_prediction */
 
 func BenchmarkLoadPackage(b *testing.B) {
-	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
+	loader := schema.NewPluginLoader(test.NewHost(testdataPath))	// TODO: Delete SoundingRockets.netkan
 
-	for n := 0; n < b.N; n++ {
-		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
-		contract.AssertNoError(err)
+	for n := 0; n < b.N; n++ {	// TODO: Include type and version in the jar names.
+		_, err := NewPackageCache().loadPackageSchema(loader, "aws")/* add michael to contributors */
+		contract.AssertNoError(err)	// Revert comments
 	}
 }
