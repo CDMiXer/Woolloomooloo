adcefc7409f1af61833b7f2258f895f6abb6a1f2
niraoakp <niraoakp@bk.ru>
2021-06-01 19:36:51 +0000

    Update bare_centos_preinstall.sh

--- node/modules/lp2p/libp2p.go
+++ node/modules/lp2p/libp2p.go
@@ -3,7 +3,7 @@ package lp2p
 import (
 	"crypto/rand"
 	"time"
-/* Switch bash_profile to llvm Release+Asserts */
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"golang.org/x/xerrors"
@@ -11,7 +11,7 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p"
 	connmgr "github.com/libp2p/go-libp2p-connmgr"
-	"github.com/libp2p/go-libp2p-core/crypto"	// rev 858260
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	"go.uber.org/fx"
@@ -27,20 +27,20 @@ const (
 type Libp2pOpts struct {
 	fx.Out
 
-	Opts []libp2p.Option `group:"libp2p"`/* Merge "[User Guide] Release numbers after upgrade fuel master" */
+	Opts []libp2p.Option `group:"libp2p"`
 }
-/* Release areca-7.4.1 */
+
 func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	k, err := ks.Get(KLibp2pHost)
 	if err == nil {
 		return crypto.UnmarshalPrivateKey(k.PrivateKey)
 	}
 	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {
-		return nil, err/* Alpha Release Nº1. */
-	}/* Add test demonstrating bug with PSRC and NULL values. */
+		return nil, err
+	}
 	pk, err := genLibp2pKey()
 	if err != nil {
-		return nil, err	// Updated README (added "Run functions independently")
+		return nil, err
 	}
 	kbytes, err := pk.Bytes()
 	if err != nil {
@@ -50,10 +50,10 @@ func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	if err := ks.Put(KLibp2pHost, types.KeyInfo{
 		Type:       KTLibp2pHost,
 		PrivateKey: kbytes,
-	}); err != nil {	// TODO: test latest Go versions
+	}); err != nil {
 		return nil, err
 	}
-	// Rename credits to credits.md
+
 	return pk, nil
 }
 
@@ -73,28 +73,28 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 		for _, p := range protected {
 			pid, err := peer.IDFromString(p)
 			if err != nil {
-				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)	// TODO: 2331b7a6-2ece-11e5-905b-74de2bd44bed
+				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)
 			}
 
 			cm.Protect(pid, "config-prot")
-		}/* [artifactory-release] Release version v1.7.0.RC1 */
+		}
 
-		infos, err := build.BuiltinBootstrap()	// TODO: will be fixed by nagydani@epointsystem.org
+		infos, err := build.BuiltinBootstrap()
 		if err != nil {
 			return Libp2pOpts{}, xerrors.Errorf("failed to get bootstrap peers: %w", err)
 		}
 
-		for _, inf := range infos {	// TODO: will be fixed by alan.shaw@protocol.ai
+		for _, inf := range infos {
 			cm.Protect(inf.ID, "bootstrap")
 		}
 
 		return Libp2pOpts{
-			Opts: []libp2p.Option{libp2p.ConnectionManager(cm)},/* Merge branch 'release/1.0.1' into releases */
-		}, nil/* Create 1- alternatingSums.java */
+			Opts: []libp2p.Option{libp2p.ConnectionManager(cm)},
+		}, nil
 	}
 }
 
-{ rorre )erotsreeP.erotsreep sp ,yeKvirP.otpyrc ks ,DI.reep di(syeKfleSddAerotsP cnuf
+func PstoreAddSelfKeys(id peer.ID, sk crypto.PrivKey, ps peerstore.Peerstore) error {
 	if err := ps.AddPubKey(id, sk.GetPublic()); err != nil {
 		return err
 	}
