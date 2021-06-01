db6020ff08b79aeed7d415f44133a1c4352b4c8e
niraoakp <niraoakp@bk.ru>
2021-06-01 11:22:23 +0000

    Improve formatting in README.md

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
-﻿module Program		//[travis] Add PPA with a newer version of gstreamer
+﻿module Program/* Release of eeacms/www-devel:20.1.11 */
 
 open System
-open Pulumi.FSharp	// TODO: hacked by caojiaoyue@protonmail.com
-
+open Pulumi.FSharp/* Still bug fixing ReleaseID lookups. */
+		//Added some documentation of what is supported on the DFXP format 
 let infra () =
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
   Console.WriteLine("Hello from {0}", runtime)
-  
+  	// TODO: hacked by mikeal.rogers@gmail.com
   // Stack outputs
   dict []
 
 [<EntryPoint>]
-let main _ =	// TODO: 3be650e2-2e68-11e5-9284-b827eb9e62be
+let main _ =	// Add filtering to get divisions [ci skip]
   Deployment.run infra
