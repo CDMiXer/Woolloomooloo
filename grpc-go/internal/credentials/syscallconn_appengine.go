2b37f1cde189bf12aeb5e4ca0419c162853c5880
niraoakp <niraoakp@bk.ru>
2021-06-01 12:11:07 +0000

    Update tensor-knn11.html

--- grpc-go/internal/credentials/syscallconn_appengine.go
+++ grpc-go/internal/credentials/syscallconn_appengine.go
@@ -1,30 +1,30 @@
 // +build appengine
-	// Update troopers.txt
+
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* README Updated for Release V0.0.3.2 */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* working on storing access-token in database */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// add jump to index head
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete pega.js */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Rebuilt index with bcochran512 */
+ */
 
-package credentials	// TODO: Merge branch 'master' into all-contributors/add-dhruv-aggarwal
-
-import (
-	"net"
+package credentials
+/* Update usingSvn.md */
+import (/* [tools/raw processing] removed unnecessary equal sign in expression */
+	"net"/* jwm_config: tray: show corresponding tab when clicking list item */
 )
-	// TODO: will be fixed by steven@stebalien.com
+
 // WrapSyscallConn returns newConn on appengine.
-func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
-	return newConn	// TODO: Turn off background when embedded
+func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {/* Changes to the paper, substantial reorganisation */
+	return newConn
 }
