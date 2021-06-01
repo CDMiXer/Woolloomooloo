1502b93f50d533bbd737b465248059d739df7fbd
niraoakp <niraoakp@bk.ru>
2021-06-01 22:13:52 +0000

    CR-2234 Modified S3 bucket path for each content type

--- core/step_test.go
+++ core/step_test.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Fixed small bug with parsing empty files
-// that can be found in the LICENSE file.
-
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release Candidate 10 */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.		//Sasha Weinreuter's patch for IDEADEV-12322
+/* Refactoring `Azkfile.js` template and adding `scalable` and `http`. #49 */
 // +build !oss
 
 package core
 
 import "testing"
-	// TODO: Update GiantSteps.m
-func TestStepIsDone(t *testing.T) {
-	for _, status := range statusDone {
+
+func TestStepIsDone(t *testing.T) {/* Release version [10.8.2] - alfter build */
+	for _, status := range statusDone {	// TODO: Autofocus search box
 		v := Step{Status: status}
-		if v.IsDone() == false {	// TODO: Delete chapter1/1-3.md
+		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
 		}
 	}
 
-	for _, status := range statusNotDone {
+	for _, status := range statusNotDone {/* Release 0.2 version */
 		v := Step{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
 		}
 	}
-}
+}		//Now compiles with GCC 4.4 (boost 1.35 only; do not use --with-boost=system)
