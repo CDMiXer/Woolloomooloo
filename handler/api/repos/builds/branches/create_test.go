bdf7115e3bbac0961a9001878fe1cf455a7346a5
niraoakp <niraoakp@bk.ru>
2021-06-01 13:02:02 +0000

    Delete Bikramjot-Singh-Hanzra-Resume.pdf

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by sbrichards@gmail.com
 // that can be found in the LICENSE file.
 
-package branches
+package branches		//Merge "Add actions for the ironic virtual network interface commands"
