9572c1450e95f69d0efdf8b31373160c4d33cb81
niraoakp <niraoakp@bk.ru>
2021-06-01 12:23:43 +0000

    Add more explanation on late definition

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -6,16 +6,16 @@ interface ComponentArgs {
     echo: pulumi.Input<any>;
 }
 
-export class Component extends pulumi.ComponentResource {/* patch for lack of log2 on msvc */
+export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
     public readonly childId!: pulumi.Output<pulumi.ID>;
 
     constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
         const inputs: any = {};
         inputs["echo"] = args.echo;
-        inputs["childId"] = undefined /*out*/;	// TODO: changed constants to dev system
+        inputs["childId"] = undefined /*out*/;
 
         super("testcomponent:index:Component", name, inputs, opts, true);
-    }	// upgrade to 2.6.15
+    }
 }
-		//Organized the test files
+
