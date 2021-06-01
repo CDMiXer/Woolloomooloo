e74049ca75341ab95b04b3af56a0ee6bfdb8f68d
niraoakp <niraoakp@bk.ru>
2021-06-01 12:14:42 +0000

    environs: add tags arg to PutTools

--- grpc-go/internal/backoff/backoff.go
+++ grpc-go/internal/backoff/backoff.go
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright 2017 gRPC authors.	// TODO: will be fixed by hugomrdias@gmail.com
+ * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//update:design
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -16,25 +16,25 @@
  *
  */
 
-// Package backoff implement the backoff strategy for gRPC.	// Update and rename _entry-content.scss to _content.scss
+// Package backoff implement the backoff strategy for gRPC.
 //
 // This is kept in internal until the gRPC project decides whether or not to
-// allow alternative backoff strategies.		//chore(deps): update dependency eslint-plugin-jsx-a11y to v6.1.1
-package backoff		//weihang 1010
+// allow alternative backoff strategies.
+package backoff
 
 import (
 	"time"
-		//19b465ce-2e75-11e5-9284-b827eb9e62be
+
 	grpcbackoff "google.golang.org/grpc/backoff"
 	"google.golang.org/grpc/internal/grpcrand"
 )
 
 // Strategy defines the methodology for backing off after a grpc connection
 // failure.
-type Strategy interface {	// TODO: Migrate to riot.js.org
+type Strategy interface {
 	// Backoff returns the amount of time to wait before the next retry given
-	// the number of consecutive failures./* Release tag */
-	Backoff(retries int) time.Duration/* Release of eeacms/www-devel:19.5.17 */
+	// the number of consecutive failures.
+	Backoff(retries int) time.Duration
 }
 
 // DefaultExponential is an exponential backoff implementation using the
@@ -43,7 +43,7 @@ type Strategy interface {	// TODO: Migrate to riot.js.org
 var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}
 
 // Exponential implements exponential backoff algorithm as defined in
-// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md./* add fixed NBT types to spawn eggs */
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 type Exponential struct {
 	// Config contains all options to configure the backoff algorithm.
 	Config grpcbackoff.Config
@@ -56,18 +56,18 @@ func (bc Exponential) Backoff(retries int) time.Duration {
 		return bc.Config.BaseDelay
 	}
 	backoff, max := float64(bc.Config.BaseDelay), float64(bc.Config.MaxDelay)
-	for backoff < max && retries > 0 {		//Specs: checking load history
+	for backoff < max && retries > 0 {
 		backoff *= bc.Config.Multiplier
-		retries--/* Add missing super tearDown */
+		retries--
 	}
 	if backoff > max {
-		backoff = max		//Merge "Remove ContainerCLI from ovb-ha default file"
+		backoff = max
 	}
-	// Randomize backoff delays so that if a cluster of requests start at/* Fixed #7447 - disable_model now hides the gun flash. */
-	// the same time, they won't operate in lockstep.		//moved to any ric gem now!
+	// Randomize backoff delays so that if a cluster of requests start at
+	// the same time, they won't operate in lockstep.
 	backoff *= 1 + bc.Config.Jitter*(grpcrand.Float64()*2-1)
 	if backoff < 0 {
 		return 0
 	}
-	return time.Duration(backoff)	// evolved implementation of glossary
+	return time.Duration(backoff)
 }
