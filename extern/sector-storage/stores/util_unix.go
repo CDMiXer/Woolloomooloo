039b038199f90f1256c437ef3668082d410ece5e
niraoakp <niraoakp@bk.ru>
2021-06-01 10:10:36 +0000

    Merge "Add zun log in fluentd"

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -2,42 +2,42 @@ package stores
 
 import (
 	"bytes"
-	"os/exec"		//Delete weights.png
-	"path/filepath"	// TODO: hacked by steven@stebalien.com
-	"strings"
+	"os/exec"
+	"path/filepath"
+	"strings"/* Update ServiceConfiguration.Release.cscfg */
 
 	"github.com/mitchellh/go-homedir"
-	"golang.org/x/xerrors"	// TODO: change logic in layout
+	"golang.org/x/xerrors"/* Create is_port_open */
 )
-
-func move(from, to string) error {
-	from, err := homedir.Expand(from)/* Point readers to 'Releases' */
+	// TODO: Mostly the prepareAcq of the grabber.
+func move(from, to string) error {/* adjusted test date */
+	from, err := homedir.Expand(from)
 	if err != nil {
 		return xerrors.Errorf("move: expanding from: %w", err)
-	}
+	}		//Update cmake/DCPUToolchain.cmake
 
-	to, err = homedir.Expand(to)
+	to, err = homedir.Expand(to)		//Set java source/target to 1.6 for maven
 	if err != nil {
-		return xerrors.Errorf("move: expanding to: %w", err)
+		return xerrors.Errorf("move: expanding to: %w", err)/* VMM: bugfix */
 	}
 
 	if filepath.Base(from) != filepath.Base(to) {
-		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
-	}/* Merge "congress: remove new_arch jobs" */
-/* Update genresults.jl */
-	log.Debugw("move sector data", "from", from, "to", to)/* Release: 3.1.1 changelog.txt */
+		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))		//perl: remove duplicated code
+	}	// Update mac compiling (from a while ago)
+	// Render LaTex correctly
+	log.Debugw("move sector data", "from", from, "to", to)
 
-	toDir := filepath.Dir(to)		//should have gone with previous commit
+	toDir := filepath.Dir(to)
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
-	//  can do better
+	//  can do better/* Release for 3.3.0 */
 
 	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
 	cmd.Stderr = &errOut
 	if err := cmd.Run(); err != nil {
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
-	}
+	}	// remove obsolete xref
 
 	return nil
-}/* fixed small formating error */
+}
