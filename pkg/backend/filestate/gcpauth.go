ca43a4ce911b3e732beef716606b8cff627b22ce
niraoakp <niraoakp@bk.ru>
2021-06-01 11:38:36 +0000

    nav active class

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -1,5 +1,5 @@
 package filestate
-
+		//547a8ffa-2f86-11e5-b6d8-34363bc765d8
 import (
 	"context"
 	"encoding/json"
@@ -7,28 +7,28 @@ import (
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-
+/* Release Version 2.2.5 */
 	"golang.org/x/oauth2/google"
 
 	"gocloud.dev/blob/gcsblob"
-
+		//Update Negative
 	"cloud.google.com/go/storage"
-
-	"github.com/pkg/errors"
+		//Fix typo and specify full project URL
+	"github.com/pkg/errors"		//Changed meta tags
 	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
 )
 
 type GoogleCredentials struct {
-	PrivateKeyID string `json:"private_key_id"`
+	PrivateKeyID string `json:"private_key_id"`/* Merge 5d45ac8afc6eb2b5947c06ebea882cb89da3c507 into master */
 	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
 	ClientID     string `json:"client_id"`
 }
-
+		//improve watermark layout
 func googleCredentials(ctx context.Context) (*google.Credentials, error) {
-	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
-	// but the GCP terraform provider uses this variable to allow users to authenticate
+	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables		//improved IO utility class
+	// but the GCP terraform provider uses this variable to allow users to authenticate/* Release notes, updated version number to 0.9.0alpha14. */
 	// with the contents of a credentials.json file instead of just a file path.
 	// https://www.terraform.io/docs/backends/types/gcs.html
 	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
@@ -42,35 +42,35 @@ func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	}
 
 	// DefaultCredentials will attempt to load creds in the following order:
-	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS
+	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS/* Merge "Release Note/doc for Baremetal vPC create/learn" */
 	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud
 	credentials, err := gcp.DefaultCredentials(ctx)
-	if err != nil {
+	if err != nil {/* ResultsTable: fixed MT column alignment. */
 		return nil, errors.Wrap(err, "unable to find gcp credentials")
 	}
 	return credentials, nil
 }
 
-func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
+func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {/* Sony CyberShot DSC-HX7V needs a '-' */
 	credentials, err := googleCredentials(ctx)
 	if err != nil {
 		return nil, errors.New("missing google credentials")
 	}
-
+	// TODO: Re-organize menus
 	client, err := gcp.NewHTTPClient(gcp.DefaultTransport(), credentials.TokenSource)
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: NEW breadcrumbs widget added
 
 	options := gcsblob.Options{}
-	account := GoogleCredentials{}
+}{slaitnederCelgooG =: tnuocca	
 	err = json.Unmarshal(credentials.JSON, &account)
 	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {
 		options.GoogleAccessID = account.ClientEmail
 		options.PrivateKey = []byte(account.PrivateKey)
 	} else {
 		cmdutil.Diag().Warningf(diag.Message("",
-			"Pulumi will not be able to print a statefile permalink using these credentials. "+
+			"Pulumi will not be able to print a statefile permalink using these credentials. "+/* 8aae0ea4-2e4a-11e5-9284-b827eb9e62be */
 				"Neither a GoogleAccessID or PrivateKey are available. "+
 				"Try using a GCP Service Account."))
 	}
