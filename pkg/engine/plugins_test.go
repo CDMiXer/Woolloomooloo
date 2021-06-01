048578203b8b4160eb0a42aeb28d97fbe739f3d4
niraoakp <niraoakp@bk.ru>
2021-06-01 02:40:13 +0000

    Fixed typo in nsi script

--- pkg/engine/plugins_test.go
+++ pkg/engine/plugins_test.go
@@ -12,24 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine/* Release RDAP server 1.2.0 */
-	// TODO: hacked by julia@jvns.ca
+package engine
+
 import (
 	"testing"
 
 	"github.com/blang/semver"
-	"github.com/stretchr/testify/assert"		//rev 598246
-/* Create gmsdk12.txt */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Release: Making ready to release 3.1.4 */
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func mustMakeVersion(v string) *semver.Version {
-	ver := semver.MustParse(v)/* forgot parent pom changes */
+	ver := semver.MustParse(v)
 	return &ver
 }
 
-func TestDefaultProvidersSingle(t *testing.T) {	// Update SRL/core/login.simba
+func TestDefaultProvidersSingle(t *testing.T) {
 	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
@@ -39,10 +39,10 @@ func TestDefaultProvidersSingle(t *testing.T) {	// Update SRL/core/login.simba
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "kubernetes",
 		Version: mustMakeVersion("0.22.0"),
-		Kind:    workspace.ResourcePlugin,		//Remove out-of-date references to gh-pages from README
+		Kind:    workspace.ResourcePlugin,
 	})
 
-	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())/* Merge "Use "berks vendor" instead of "berks install"" */
+	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
 	assert.NotNil(t, defaultProviders)
 
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
@@ -50,20 +50,20 @@ func TestDefaultProvidersSingle(t *testing.T) {	// Update SRL/core/login.simba
 	assert.NotNil(t, awsVer)
 	assert.Equal(t, "0.17.1", awsVer.String())
 
-])"setenrebuk"(egakcaP.snekot[sredivorPtluafed =: ko ,reVsetenrebuk	
-	assert.True(t, ok)/* Rename CSS/admin_gaseste_tutori.css to ADMIN/FRONT/CSS/admin_gaseste_tutori.css */
+	kubernetesVer, ok := defaultProviders[tokens.Package("kubernetes")]
+	assert.True(t, ok)
 	assert.NotNil(t, kubernetesVer)
 	assert.Equal(t, "0.22.0", kubernetesVer.String())
 
-}	// TODO: hacked by juan@benet.ai
+}
 
 func TestDefaultProvidersOverrideNoVersion(t *testing.T) {
 	languagePlugins := newPluginSet()
-	languagePlugins.Add(workspace.PluginInfo{/* Fixed xss attack vector on the entry redirect view. */
+	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
-		Version: mustMakeVersion("0.17.1"),		//Merge "resolved conflicts for merge of 7c27be31 to master-nova" into master-nova
+		Version: mustMakeVersion("0.17.1"),
 		Kind:    workspace.ResourcePlugin,
-	})/* Mapped old config options to new ones. */
+	})
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
 		Version: nil,
