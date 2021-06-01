6685bc1cf4633b8604fe84e103f70c859d94bd0e
niraoakp <niraoakp@bk.ru>
2021-06-01 16:51:36 +0000

    Moving Email

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Changement du nom du script et début de traitement des options POSIX. */
-import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+import * as pulumi from "@pulumi/pulumi";
+	// Rename aspnetcore.md to aspnetcore.html
+class Resource extends pulumi.ComponentResource {	// TODO: Add member_role and rename sequence_id
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Release 0.26.0 */
+        super("my:module:Resource", name, {}, opts);
     }
-}/* Improved resolution-reasons display */
+}
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {
+class ComponentFour extends pulumi.ComponentResource {	// TODO: will be fixed by nagydani@epointsystem.org
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         // Add an alias that references the old type of this resource...
         const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
-        // ..and then make the super call with the new type of this resource and the added alias.	// new task json bugfix
-;)} sesaila ,stpo... { ,}{ ,eman ,"emaNepyTtnereffiDAhtiWruoFtnenopmoC:eludomtnereffid:ym"(repus        
-        // The child resource will also pick up an implicit alias due to the new type of the component it is parented	// Create mywebsite
+        // ..and then make the super call with the new type of this resource and the added alias.
+        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
+        // The child resource will also pick up an implicit alias due to the new type of the component it is parented
         // to.
-        this.resource = new Resource("otherchild", { parent: this });	// TODO: 7aa74144-2e75-11e5-9284-b827eb9e62be
+        this.resource = new Resource("otherchild", { parent: this });	// TODO: Add deployment documentation, and update API docs.
     }
 }
 const comp4 = new ComponentFour("comp4");
