4c07ee02deef21db04a2e79eb718004e919e1886
niraoakp <niraoakp@bk.ru>
2021-06-01 20:27:21 +0000

    Set execute permission on scripts extracted from the SDK

--- plugin/admission/account_oss.go
+++ plugin/admission/account_oss.go
@@ -5,20 +5,20 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// fixed incorrect description
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 3.2.3.316 Prima WLAN Driver" */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/* Release for v7.0.0. */
-// +build oss/* Release Candidate for setThermostatFanMode handling */
+// limitations under the License./* Merge "Implement some logic in abstract methods" */
+/* e16335ec-2e42-11e5-9284-b827eb9e62be */
+// +build oss
 
 package admission
 
 import "github.com/drone/drone/core"
 
-// Membership is a no-op admission controller	// remove global install [skip-ci]
-func Membership(core.OrganizationService, []string) core.AdmissionService {/* Add and use peakFontSize pref */
+// Membership is a no-op admission controller/* Suchliste: Release-Date-Spalte hinzugefügt */
+func Membership(core.OrganizationService, []string) core.AdmissionService {
 	return new(noop)
 }
