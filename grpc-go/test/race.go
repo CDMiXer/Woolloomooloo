a0a747ab4eb0af3db30d9c48b48bd93e51760bca
niraoakp <niraoakp@bk.ru>
2021-06-01 03:28:24 +0000

    Ensure we’re checking for the post image before displaying it.

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -1,8 +1,8 @@
 // +build race
 
-/*	// TODO: Merge "Consume floating_ip_source config value"
+/*
  * Copyright 2016 gRPC authors.
- *	// tolti degli apici
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,14 +11,14 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// remove old drafts
- * See the License for the specific language governing permissions and/* Released wffweb-1.1.0 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Create ChatListAdapter */
+ *
  */
-		//Set landscape-client-common as a dep.
+
 package test
 
-func init() {/* Updating build-info/dotnet/roslyn/dev16.9 for 4.21061.22 */
-	raceMode = true
+func init() {
+	raceMode = true	// Merge "Fix getBytesSize for YUV allocations."
 }
