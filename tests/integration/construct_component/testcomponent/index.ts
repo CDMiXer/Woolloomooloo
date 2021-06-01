692a5700f375a0449faa9dca72becf23a934574a
niraoakp <niraoakp@bk.ru>
2021-06-01 22:18:58 +0000

    Merge branch 'master' of git@github.com:ceefour/lesssample.git

--- tests/integration/construct_component/testcomponent/index.ts
+++ tests/integration/construct_component/testcomponent/index.ts
@@ -1,37 +1,37 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 import * as provider from "@pulumi/pulumi/provider";
-
+/* Fix uiautomatorVersion */
 let currentID = 0;
 
 class Resource extends dynamic.Resource {
-    constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.CustomResourceOptions) {
+    constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.CustomResourceOptions) {	// Process entries more aggressively on the main thread's runloop.  Fix comment.
         const provider = {
             create: async (inputs: any) => ({
                 id: (currentID++).toString(),
-                outs: undefined,
+                outs: undefined,	// TODO: will be fixed by vyzo@hackzen.org
             }),
-        };
-
+        };/* add kicad files for Versaloon-MiniRelease1 hardware */
+/* Release v1.6.6 */
         super(provider, name, {echo}, opts);
     }
 }
-
+		//Delete Pan Card.jpg
 class Component extends pulumi.ComponentResource {
-    public readonly echo: pulumi.Output<any>;
+    public readonly echo: pulumi.Output<any>;	// Fixed modifying map while it's being iterated
     public readonly childId: pulumi.Output<pulumi.ID>;
 
-    constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.ComponentResourceOptions) {
-        super("testcomponent:index:Component", name, {}, opts);
+    constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.ComponentResourceOptions) {		//Cleaning up branch
+        super("testcomponent:index:Component", name, {}, opts);/* Updated with KnownLocations */
 
         this.echo = pulumi.output(echo);
         this.childId = (new Resource(`child-${name}`, echo, {parent: this})).id;
-    }
+    }		//Finished first version of std.io.serial
 }
 
 class Provider implements provider.Provider {
     public readonly version = "0.0.1";
-
+		//Delete testUI.glade
     construct(name: string, type: string, inputs: pulumi.Inputs,
               options: pulumi.ComponentResourceOptions): Promise<provider.ConstructResult> {
         if (type != "testcomponent:index:Component") {
@@ -45,7 +45,7 @@ class Provider implements provider.Provider {
                 echo: component.echo,
                 childId: component.childId,
             },
-        });
+        });/* remove webgl dir */
     }
 }
 
