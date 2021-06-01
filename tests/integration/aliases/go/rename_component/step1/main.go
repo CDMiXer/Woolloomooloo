81d6af660f0867989adb66a0d53c90bdda5dc6f6
niraoakp <niraoakp@bk.ru>
2021-06-01 13:12:36 +0000

    Blog's sitemap

--- tests/integration/aliases/go/rename_component/step1/main.go
+++ tests/integration/aliases/go/rename_component/step1/main.go
@@ -1,29 +1,29 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Update 05-Create-update-manage-website.md */
 
-package main
+package main		//Tuturial cleanup -- @PetOfTheMonth
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+		//Added some features; working on Model injection
 // FooComponent is a component resource
 type FooResource struct {
 	pulumi.ResourceState
 }
-
+	// TODO: will be fixed by CoinCap@ShapeShift.io
 type FooComponent struct {
 	pulumi.ResourceState
-}
-
+}	// Use global write states
+/* ReadME-Open Source Release v1 */
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}
+	fooRes := &FooResource{}	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
-	}
+	}/* Old WebIf: Now we have footer at the bottom  */
 	return fooRes, nil
 }
-
+	// TODO: 3.0.1 release
 // Scenario #3 - rename a component (and all it's children)
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
@@ -37,13 +37,13 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: will be fixed by mikeal.rogers@gmail.com
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
-	}
-	return fooComp, nil
-}
+	}	// TODO: Merge "[Dummy driver] Add possibility to set delays for driver methods"
+	return fooComp, nil/* add linear-presets-metric-prefixes as related project */
+}	// TODO: hacked by arachnid@notdot.net
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
