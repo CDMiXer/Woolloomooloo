7fbad9a26da4f9645ed3a068385e1dda111ec67f
niraoakp <niraoakp@bk.ru>
2021-06-01 07:19:30 +0000

    Translated What I forgot

--- handler/api/repos/builds/logs/delete_test.go
+++ handler/api/repos/builds/logs/delete_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package logs/* Merge "ART: ReadFully needs to fail when EOF" */
+package logs
