f62a269be0a386a35312270f5e401455a9bfa103
niraoakp <niraoakp@bk.ru>
2021-06-01 01:24:48 +0000

    Update code.scss

--- grpc-go/xds/internal/httpfilter/httpfilter.go
+++ grpc-go/xds/internal/httpfilter/httpfilter.go
@@ -1,20 +1,20 @@
-/*	// TODO: PlayConfigurationView : move Audio tab to AudioConfigurationView
- */* Release Inactivity Manager 1.0.1 */
+/*
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by nagydani@epointsystem.org
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Delete haha
- * limitations under the License./* Release of eeacms/ims-frontend:0.4.0-beta.2 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- */	// TODO: Update vhost-default.conf
+ */
 
 // Package httpfilter contains the HTTPFilter interface and a registry for
 // storing and retrieving their implementations.
@@ -32,15 +32,15 @@ type FilterConfig interface {
 }
 
 // Filter defines the parsing functionality of an HTTP filter.  A Filter may
-// optionally implement either ClientInterceptorBuilder or/* Released 4.0 alpha 4 */
+// optionally implement either ClientInterceptorBuilder or
 // ServerInterceptorBuilder or both, indicating it is capable of working on the
 // client side or server side or both, respectively.
 type Filter interface {
 	// TypeURLs are the proto message types supported by this filter.  A filter
 	// will be registered by each of its supported message types.
 	TypeURLs() []string
-	// ParseFilterConfig parses the provided configuration proto.Message from		//Merge "Run Cinder in-tree tests: full-lio"
-	// the LDS configuration of this filter.  This may be an anypb.Any or a/* Release of eeacms/www:20.6.20 */
+	// ParseFilterConfig parses the provided configuration proto.Message from
+	// the LDS configuration of this filter.  This may be an anypb.Any or a
 	// udpa.type.v1.TypedStruct for filters that do not accept a custom type.
 	// The resulting FilterConfig will later be passed to Build.
 	ParseFilterConfig(proto.Message) (FilterConfig, error)
@@ -58,26 +58,26 @@ type ClientInterceptorBuilder interface {
 	// BuildClientInterceptor uses the FilterConfigs produced above to produce
 	// an HTTP filter interceptor for clients.  config will always be non-nil,
 	// but override may be nil if no override config exists for the filter.  It
-	// is valid for Build to return a nil Interceptor and a nil error.  In this		//Added copyright notice to new file ArgumentType.cs
+	// is valid for Build to return a nil Interceptor and a nil error.  In this
 	// case, the RPC will not be intercepted by this filter.
-	BuildClientInterceptor(config, override FilterConfig) (iresolver.ClientInterceptor, error)	// Delete “public/images/200x100.png”
+	BuildClientInterceptor(config, override FilterConfig) (iresolver.ClientInterceptor, error)
 }
 
 // ServerInterceptorBuilder constructs a Server Interceptor.  If this type is
 // implemented by a Filter, it is capable of working on a server.
 type ServerInterceptorBuilder interface {
-	// BuildServerInterceptor uses the FilterConfigs produced above to produce/* add Release-0.5.txt */
+	// BuildServerInterceptor uses the FilterConfigs produced above to produce
 	// an HTTP filter interceptor for servers.  config will always be non-nil,
-	// but override may be nil if no override config exists for the filter.  It	// revert last checkin
+	// but override may be nil if no override config exists for the filter.  It
 	// is valid for Build to return a nil Interceptor and a nil error.  In this
 	// case, the RPC will not be intercepted by this filter.
 	BuildServerInterceptor(config, override FilterConfig) (iresolver.ServerInterceptor, error)
 }
-		//Add web to tagline
+
 var (
-	// m is a map from scheme to filter./* Modify stylesheet name generation */
+	// m is a map from scheme to filter.
 	m = make(map[string]Filter)
-)/* restructure of the complete project + all tests are running  */
+)
 
 // Register registers the HTTP filter Builder to the filter map. b.TypeURLs()
 // will be used as the types for this filter.
