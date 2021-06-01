a1d48b18042256754f8aaaa5dd979f4550ace689
niraoakp <niraoakp@bk.ru>
2021-06-01 17:09:27 +0000

    Added timezone hack, to handle saving and rendering sessions in non-uk timezones

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "building docker images for ${GOOS}/${GOARCH} ..."/* ci: use latest Miniconda in Travis */
-	// quickly released: 12.07.9
-REPO="github.com/drone/drone"/* 49a831cc-2e55-11e5-9284-b827eb9e62be */
+echo "building docker images for ${GOOS}/${GOARCH} ..."
+
+REPO="github.com/drone/drone"		//Bump version, README
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled
+# compile the runners with gcc disabled/* Release of eeacms/plonesaas:5.2.1-66 */
 export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
 go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
