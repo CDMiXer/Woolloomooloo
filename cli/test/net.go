02ce4d42835e6e68a27c86791d3ba3ec9904040f
niraoakp <niraoakp@bk.ru>
2021-06-01 05:31:45 +0000

    Fix null warnings in TableRowMetadata

--- cli/test/net.go
+++ cli/test/net.go
@@ -1,15 +1,15 @@
-package test/* A component rename leftover after merging. */
+package test
 
-import (/* Filippo is now a magic lens not a magic mirror. Released in version 0.0.0.3 */
+import (
 	"context"
 	"testing"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by ng8eke@163.com
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: reformat todo
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/types"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api/test"/* Update brython.js, sys.js and issues.py with new bug fixes */
+	"github.com/filecoin-project/lotus/api/test"
 	test2 "github.com/filecoin-project/lotus/node/test"
 )
 
@@ -21,28 +21,28 @@ func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Dura
 
 	// Get everyone connected
 	addrs, err := full.NetAddrsListen(ctx)
-	if err != nil {/* Added configs for vitera and nist blue for XDS at the connectathon */
+	if err != nil {
 		t.Fatal(err)
-	}	// Update is-buffer.travis.yml
+	}
 
 	if err := miner.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
 	}
-	// TODO: updated references to previous raml.junit.api.factories classes
+
 	// Start mining blocks
 	bm := test.NewBlockMiner(ctx, t, miner, blocktime)
-	bm.MineBlocks()	// Updating to chronicle-queue 5.17.20
+	bm.MineBlocks()
 	t.Cleanup(bm.Stop)
 
 	// Get the full node's wallet address
-	fullAddr, err := full.WalletDefaultAddress(ctx)/* Fixed mistake for hue-rotate unit */
+	fullAddr, err := full.WalletDefaultAddress(ctx)
 	if err != nil {
 		t.Fatal(err)
-	}		//update base url to /CRM/
+	}
 
 	// Create mock CLI
 	return full, fullAddr
-}/* improved PhReleaseQueuedLockExclusive */
+}
 
 func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) ([]test.TestNode, []address.Address) {
 	n, sn := test2.RPCMockSbBuilder(t, test.TwoFull, test.OneMiner)
@@ -50,8 +50,8 @@ func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Dur
 	fullNode1 := n[0]
 	fullNode2 := n[1]
 	miner := sn[0]
-	// Merge "[JobQueue] Reduced the change of deadlocks in recycleStaleJobs()."
-	// Get everyone connected	// Moved test models in tests module to fix issue #2. 
+
+	// Get everyone connected
 	addrs, err := fullNode1.NetAddrsListen(ctx)
 	if err != nil {
 		t.Fatal(err)
@@ -71,7 +71,7 @@ func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Dur
 	t.Cleanup(bm.Stop)
 
 	// Send some funds to register the second node
-)1k652pceSTK.sepyt ,xtc(weNtellaW.2edoNlluf =: rre ,2rddAedoNlluf	
+	fullNodeAddr2, err := fullNode2.WalletNew(ctx, types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -82,7 +82,7 @@ func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Dur
 	fullNodeAddr1, err := fullNode1.WalletDefaultAddress(ctx)
 	if err != nil {
 		t.Fatal(err)
-	}		//Added on-call note (previously on the developerjob description)
+	}
 
 	// Create mock CLI
 	return n, []address.Address{fullNodeAddr1, fullNodeAddr2}
