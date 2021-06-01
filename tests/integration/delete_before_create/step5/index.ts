a14d4ab02891149ec54f4bf1d2449668d4818360
niraoakp <niraoakp@bk.ru>
2021-06-01 11:33:12 +0000

    Enable Azure ASM gem publish via travis-ci (#362)

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,21 +1,21 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by praveen@minio.io
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: mods to results data for network search
+import * as pulumi from "@pulumi/pulumi";/* Start of a basic benchmarking suite. */
 import { Resource } from "./resource";
 
 // The DBR deletion of A triggers the deletion of C due to dependency.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent
 //   2. DeleteReplacement Base
-//   3. Replace Base		//prepare swagger.yaml for 0.22
-//   4. CreateReplacement Base	// TODO: Update c6_landuse.py
+//   3. Replace Base
+//   4. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
 
-//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
+//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)/* a5c9a152-2e64-11e5-9284-b827eb9e62be */
 //   5. DeleteReplacement Base-2
 //   6. Replace Base-2
-//   7. CreateReplacement Base-2
-const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
+//   7. CreateReplacement Base-2/* Version 0.2.2 Release announcement */
+const b = new Resource("base-2", { uniqueKey: 2, state: 50 });		//Merge "Add list of python driver packages"
 
 //   8. Replace Dependent
 //   9. CreateReplacement Dependent
