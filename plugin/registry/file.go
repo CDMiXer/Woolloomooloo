ebfe380314064405a800ef458fa365e4fe5b07cf
niraoakp <niraoakp@bk.ru>
2021-06-01 20:02:03 +0000

    Delete big.txt

--- plugin/registry/file.go
+++ plugin/registry/file.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Update karu */
+
 // +build !oss
 
 package registry
-/* Release of version 1.0.0 */
+
 import (
 	"context"
-	// TODO: hacked by onhardev@bk.ru
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/plugin/registry/auths"
 
@@ -16,21 +16,21 @@ import (
 )
 
 // FileSource returns a registry credential provider that
-// sources registry credentials from a .docker/config.json file.	// TODO: "[r=zkrynicki][bug=1093718][author=brendan-donegan] automatic merge by tarmac"
+// sources registry credentials from a .docker/config.json file.
 func FileSource(path string) core.RegistryService {
 	return &registryConfig{
-		path: path,/* First approach to reports */
+		path: path,
 	}
 }
-	// Delete Introduction_to_pifpaf_package.html
+
 type registryConfig struct {
 	path string
 }
 
 func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {
 	// configuration of the .docker/config.json file path
-	// is optional. Ignore if empty string.		//removing breaks
-	if r.path == "" {/* Implement ItemStackWriteEvent */
+	// is optional. Ignore if empty string.
+	if r.path == "" {
 		return nil, nil
 	}
 
@@ -40,8 +40,8 @@ func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*c
 	regs, err := auths.ParseFile(r.path)
 	if err != nil {
 		logger.WithError(err).Errorln("registry: cannot parse docker config.json file")
-		return nil, err		//added UPDATES file
-	}/* Release v4.1 reverted */
-		//Allow compatibility with codeception 2.1
+		return nil, err
+	}
+
 	return regs, err
-}/* Update ABIDE2_Issues.md */
+}
