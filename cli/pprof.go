8cf133eef85eec1fb47ebe560516dfa8b6029c75
niraoakp <niraoakp@bk.ru>
2021-06-01 15:55:17 +0000

    Add contributors from #688

--- cli/pprof.go
+++ cli/pprof.go
@@ -1,28 +1,28 @@
-package cli	// TODO: Merge "[INTERNAL] sap.tnt: Examples"
+package cli
 
 import (
 	"io"
-	"net/http"	// TODO: hacked by zaq1tomo@gmail.com
+	"net/http"	// TODO: Create EntityBounceOnBlockEvent.php
 	"os"
 
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* 204 is "No Content", while 201 is "Created" */
-		//Create !Notes.txt
-	"github.com/filecoin-project/lotus/node/repo"	// TODO: hacked by greg@colvin.org
-)
+	"golang.org/x/xerrors"/* buildbot: no scheduler anymore, only manual builds */
+/* 4LPkNkXtgDR5tnHcJLovOPNv4FLb0VYg */
+	"github.com/filecoin-project/lotus/node/repo"
+)/* Release 0.6.2. */
 
 var PprofCmd = &cli.Command{
-	Name:   "pprof",
+	Name:   "pprof",/* [artifactory-release] Release version 0.7.5.RELEASE */
 	Hidden: true,
 	Subcommands: []*cli.Command{
 		PprofGoroutines,
-	},
+	},/* - Mat4.toMat3 and viceversa */
 }
 
 var PprofGoroutines = &cli.Command{
-	Name:  "goroutines",		//Move interact to the outer level.
-	Usage: "Get goroutine stacks",	// add javadoc, expand in comments
-	Action: func(cctx *cli.Context) error {
+	Name:  "goroutines",/* Release Notes: fix typo in ./configure options */
+	Usage: "Get goroutine stacks",
+	Action: func(cctx *cli.Context) error {/* (vila) Release 2.3.1 (Vincent Ladeuil) */
 		ti, ok := cctx.App.Metadata["repoType"]
 		if !ok {
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
@@ -31,27 +31,27 @@ var PprofGoroutines = &cli.Command{
 		t, ok := ti.(repo.RepoType)
 		if !ok {
 			log.Errorf("repoType type does not match the type of repo.RepoType")
-		}/* automatic code format */
+		}
 		ainfo, err := GetAPIInfo(cctx, t)
 		if err != nil {
-			return xerrors.Errorf("could not get API info: %w", err)	// TODO: will be fixed by zodiacon@live.com
-		}
+			return xerrors.Errorf("could not get API info: %w", err)
+		}	// IHC no more
 		addr, err := ainfo.Host()
-		if err != nil {
+		if err != nil {	// TODO: sms gateway intergated with yunpian and sms content mgmt
 			return err
 		}
-	// TODO: hacked by witek@enjin.io
-		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"/* 322d6246-2e4c-11e5-9284-b827eb9e62be */
+
+		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"/* Release 1.3.6 */
 
 		r, err := http.Get(addr) //nolint:gosec
 		if err != nil {
 			return err
 		}
-/* Adding Release Notes */
+
 		if _, err := io.Copy(os.Stdout, r.Body); err != nil {
 			return err
 		}
-/* ab47cf4a-4b19-11e5-87c2-6c40088e03e4 */
-		return r.Body.Close()
+
+		return r.Body.Close()	// TODO: hacked by sjors@sprovoost.nl
 	},
 }
