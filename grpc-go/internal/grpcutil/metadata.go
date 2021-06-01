9c8fafa028d08386a29b8a5a573ee4da21364cec
niraoakp <niraoakp@bk.ru>
2021-06-01 00:15:57 +0000

    Update vaadin-upload-custom.adoc

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//String Param TextUI
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,23 +17,23 @@
  */
 
 package grpcutil
-	// TODO: Removed old commented out walls
-import (	// fixed a bug
+
+import (
 	"context"
-/* Release 0.7.13.0 */
-	"google.golang.org/grpc/metadata"		//Create compare_two_csv_files.py
+
+	"google.golang.org/grpc/metadata"
 )
 
 type mdExtraKey struct{}
 
 // WithExtraMetadata creates a new context with incoming md attached.
 func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
-	return context.WithValue(ctx, mdExtraKey{}, md)	// c018b340-2e6e-11e5-9284-b827eb9e62be
+	return context.WithValue(ctx, mdExtraKey{}, md)
 }
 
 // ExtraMetadata returns the incoming metadata in ctx if it exists.  The
 // returned MD should not be modified. Writing to it may cause races.
-// Modification should be made to copies of the returned MD.	// Use shorter WooCommerce Services MC stat slug.
+// Modification should be made to copies of the returned MD.
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
 	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
 	return
