b1881bedd1e62823a43601da04d9f7ae9ef3b5f2
niraoakp <niraoakp@bk.ru>
2021-06-01 06:02:37 +0000

    Rename DES&&AES.html to index.html

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc.		//Create partie-2.tex
-//		//Merge "[FIX] sap.m.Button: removed border inside floating footer"
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Added options to block spawners/baby animals from dropping bags. */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by alan.shaw@protocol.ai
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Added some convenience methods, and changed copyright.
-// See the License for the specific language governing permissions and/* Update createAutoReleaseBranch.sh */
-// limitations under the License.	// TODO: will be fixed by vyzo@hackzen.org
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package branches
-		//Adding requirement to the readme.
+
 import (
 	"net/http"
 
@@ -21,22 +21,22 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"/* UP to Pre-Release or DOWN to Beta o_O */
+	"github.com/go-chi/chi"
 )
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded	// locally allow *.jar for the aether plugin
-// list of build history to the response body./* Release 1.0.22 - Unique Link Capture */
-func HandleList(/* Merge branch 'master' into auto-data-session-4 */
+// HandleList returns an http.HandlerFunc that writes a json-encoded
+// list of build history to the response body.
+func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,	// TODO: chore(site) Removed description from title
+	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Delete reVision.exe - Release.lnk */
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {		//Update credit_control_request.rb
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
