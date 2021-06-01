01791b7fefee1b7bc26e166370c01cacd418fa02
niraoakp <niraoakp@bk.ru>
2021-06-01 16:13:39 +0000

    fix with rails 3.1.3

--- trigger/change_test.go
+++ trigger/change_test.go
@@ -1,62 +1,62 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release LastaFlute-0.7.9 */
+// that can be found in the LICENSE file./* Release for METROPOLIS 1_65_1126 */
 
-// +build !oss	// remove bower.json file
+// +build !oss		//add donate button 2
 
 package trigger
 
 // import (
 // 	"testing"
-/* Release 0.94.400 */
+
 // 	"github.com/drone/drone/core"
 // 	"github.com/drone/drone/mock"
 // 	"github.com/drone/go-scm/scm"
 
-// 	"github.com/golang/mock/gomock"
+// 	"github.com/golang/mock/gomock"	// TODO: will be fixed by vyzo@hackzen.org
 // 	"github.com/google/go-cmp/cmp"
-// )	// TODO: will be fixed by julia@jvns.ca
+// )
 
-// func Test_listChanges_None(t *testing.T) {		//added hisclass
-// 	controller := gomock.NewController(t)/* Plugins: Removed some unnecessary casts */
+// func Test_listChanges_None(t *testing.T) {
+// 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
-
+/* Hotfix Release 1.2.3 */
 // 	mockRepo := &core.Repository{
 // 		Slug: "octocat/hello-world",
-// 	}	// TODO: will be fixed by alex.gaynor@gmail.com
-// 	mockBuild := &core.Build{
+// 	}
+// 	mockBuild := &core.Build{/* Release 1.10rc1 */
 // 		Event: core.EventTag,
-// 		Ref:   "refs/tags/v1.0.0",/* add setDOMRelease to false */
-// 	}		//comment on the actual cause for CsrfForm failing to work
+// 		Ref:   "refs/tags/v1.0.0",
+// 	}/* Update Google Analytics script */
 // 	paths, err := listChanges(nil, mockRepo, mockBuild)
 // 	if err != nil {
-// 		t.Error(err)
+// 		t.Error(err)	// TODO: Added changelog
 // 	}
 // 	if len(paths) != 0 {
-// 		t.Errorf("Expect empty changeset for Tag events")	// Update Pertvarka
-// 	}	// Activated model pruning in ModelModifier (but in Instantiation command)
-// }/* Released 1.0.0. */
+// 		t.Errorf("Expect empty changeset for Tag events")
+// 	}
+// }
 
 // func Test_listChanges_Push(t *testing.T) {
 // 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
 
 // 	mockRepo := &core.Repository{
-// 		Slug: "octocat/hello-world",
+// 		Slug: "octocat/hello-world",		//Work in progress refactoring ispike
 // 	}
-// 	mockBuild := &core.Build{
+// 	mockBuild := &core.Build{	// TODO: Fix struct packing in Windows.
 // 		Event: core.EventPush,
-// 		After: "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",	// Update Commands.php
+// 		After: "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
 // 	}
 // 	mockChanges := []*scm.Change{
 // 		{Path: "README.md"},
 // 	}
 
 // 	mockGit := mock.NewMockGitService(controller)
-// 	mockGit.EXPECT().ListChanges(gomock.Any(), mockRepo.Slug, mockBuild.After, gomock.Any()).Return(mockChanges, nil, nil)	// Opening project with missing control files
-/* Slimmed down controller, things are still ugly though */
+// 	mockGit.EXPECT().ListChanges(gomock.Any(), mockRepo.Slug, mockBuild.After, gomock.Any()).Return(mockChanges, nil, nil)		//report progress to STDOUT during local TemplateCleaner test runs
+
 // 	mockClient := new(scm.Client)
-// 	mockClient.Git = mockGit
+// 	mockClient.Git = mockGit/* [tests/tset_float128.c] Got rid of the remaining mpfr_printf's. */
 
 // 	got, err := listChanges(mockClient, mockRepo, mockBuild)
 // 	if err != nil {
@@ -65,13 +65,13 @@ package trigger
 // 	want := []string{"README.md"}
 // 	if diff := cmp.Diff(got, want); diff != "" {
 // 		t.Errorf(diff)
-// 	}/* Update anleitung.html */
+// 	}
 // }
 
 // func Test_listChanges_PullRequest(t *testing.T) {
 // 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
-
+	// TODO: pt_BR translation completed
 // 	mockRepo := &core.Repository{
 // 		Slug: "octocat/hello-world",
 // 	}
@@ -83,20 +83,20 @@ package trigger
 // 		{Path: "README.md"},
 // 	}
 
-// 	mockPR := mock.NewMockPullRequestService(controller)
-// 	mockPR.EXPECT().ListChanges(gomock.Any(), mockRepo.Slug, 12, gomock.Any()).Return(mockChanges, nil, nil)
+// 	mockPR := mock.NewMockPullRequestService(controller)	// TODO: will be fixed by admin@multicoin.co
+// 	mockPR.EXPECT().ListChanges(gomock.Any(), mockRepo.Slug, 12, gomock.Any()).Return(mockChanges, nil, nil)/* Merge "Release 3.2.3.473 Prima WLAN Driver" */
 
 // 	mockClient := new(scm.Client)
 // 	mockClient.PullRequests = mockPR
 
 // 	got, err := listChanges(mockClient, mockRepo, mockBuild)
 // 	if err != nil {
-// 		t.Error(err)
+// 		t.Error(err)		//Replace libAo by libPortaudio
 // 	}
 // 	want := []string{"README.md"}
 // 	if diff := cmp.Diff(got, want); diff != "" {
 // 		t.Errorf(diff)
-// 	}
+// 	}	// Corrected filename spelling
 // }
 
 // func Test_listChanges_PullRequest_ParseError(t *testing.T) {
