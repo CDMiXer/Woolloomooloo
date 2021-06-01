e6dc92b661b9c7853a2a987a3bca00f2e3aa1cd7
niraoakp <niraoakp@bk.ru>
2021-06-01 15:15:15 +0000

    Compute the difference between two images

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -3,25 +3,25 @@ package ffiwrapper
 import (
 	"golang.org/x/xerrors"
 
-	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
+	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"/* FIX: Use correct markdown syntax for the code */
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//[doc] Add links to the blueprint section
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
+		//Readme.md for tellstick plugin
 // merge gaps between ranges which are close to each other
-//  TODO: more benchmarking to come up with more optimal number	// TODO: wprobe: fix missing return code check
+//  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
+/* Release plugin added */
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests/* Merge "Release 1.0.0.108 QCACLD WLAN Driver" */
 
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
-/* Fix selection of pages to process */
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {/* Release areca-7.2.5 */
-	todo := pieceRun(offset.Padded(), size.Padded())	// TODO: Contains method of GridElement moved to Utils
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
+	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
-	// Merge "Update print chooser drop down width." into lmp-dev
+
 	return rlepluslazy.JoinClose(todo, mergeGaps)
 }
