97497e5ef7790f6b7b8fc4467306d47d71fa475e
niraoakp <niraoakp@bk.ru>
2021-06-01 16:20:26 +0000

    changed loadertext prop to this.props.children

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Accepting PUT requests in JSON to add a show.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* moved MV_ERRTY to superclass, close #70 */
+// that can be found in the LICENSE file.
 
-// +build !oss		//Added path support to entry name; closes #13
-	// TODO: will be fixed by why@ipfs.io
+// +build !oss
+
 package converter
-	// TODO: will be fixed by 13860583249@yeah.net
+
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* Release 1.8.1 */
+	"github.com/drone/drone/core"
 )
 
 // Legacy returns a conversion service that converts a
-// legacy 0.8 yaml file to a yaml file./* Updated the r-fdrtool feedstock. */
-func Legacy(enabled bool) core.ConvertService {		//Added new compilation target "splint" to Makefile.
+// legacy 0.8 yaml file to a yaml file.
+func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
 		enabled: enabled,
 	}
 }
 
-type legacyPlugin struct {/* don’t escape html in rss feeds */
+type legacyPlugin struct {
 	enabled bool
-}	// *Added to template bugtracker
+}
 
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
 	}
 	return &core.Config{
-		Data: req.Config.Data,/* Merged development into Release */
+		Data: req.Config.Data,
 	}, nil
 }
