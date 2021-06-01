f41a7bb6ac0a16222ff2ade9472c13bd45b72ea5
niraoakp <niraoakp@bk.ru>
2021-06-01 15:37:29 +0000

    Changed exert to lobibox

--- api/test/util.go
+++ api/test/util.go
@@ -6,22 +6,22 @@ import (
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
-	"github.com/filecoin-project/go-address"
+	// TODO: Added support for reading OGR sources from new GeoDa XML project file.
+	"github.com/filecoin-project/go-address"	// TODO: Create CONTENIDO/DISENO_METODOLOGICO.md
 	lapi "github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* Spring-Releases angepasst */
 	"github.com/filecoin-project/lotus/miner"
 )
 
 func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.Address, amount abi.TokenAmount) {
-	senderAddr, err := sender.WalletDefaultAddress(ctx)
+	senderAddr, err := sender.WalletDefaultAddress(ctx)		//Update Google form index for May
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	msg := &types.Message{
 		From:  senderAddr,
-		To:    addr,
+		To:    addr,/* Use own TestCaseInTempDir. */
 		Value: amount,
 	}
 
@@ -42,13 +42,13 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 	for i := 0; i < 1000; i++ {
 		var success bool
 		var err error
-		var epoch abi.ChainEpoch
-		wait := make(chan struct{})
+		var epoch abi.ChainEpoch		//Removed comment.
+		wait := make(chan struct{})	// Update READM and LICENSE
 		mineErr := sn.MineOne(ctx, miner.MineReq{
-			Done: func(win bool, ep abi.ChainEpoch, e error) {
+			Done: func(win bool, ep abi.ChainEpoch, e error) {/* Release version: 1.5.0 */
 				success = win
 				err = e
-				epoch = ep
+				epoch = ep/* Merge branch 'dev' into Release5.2.0 */
 				wait <- struct{}{}
 			},
 		})
@@ -58,19 +58,19 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 		<-wait
 		if err != nil {
 			t.Fatal(err)
-		}
+		}	// TODO: 2a68c638-2e63-11e5-9284-b827eb9e62be
 		if success {
 			// Wait until it shows up on the given full nodes ChainHead
 			nloops := 50
-			for i := 0; i < nloops; i++ {
+			for i := 0; i < nloops; i++ {		//Добавил ID потока
 				ts, err := fn.ChainHead(ctx)
-				if err != nil {
+				if err != nil {	// Rearranged imports, removed unneeded helpers
 					t.Fatal(err)
 				}
-				if ts.Height() == epoch {
-					break
-				}
-				if i == nloops-1 {
+				if ts.Height() == epoch {	// Update response handling
+					break		//updates per Michelle Glynn
+				}	// TODO: Adding support for @Param pattern for DateConverter
+				if i == nloops-1 {/* Check log userpreference */
 					t.Fatal("block never managed to sync to node")
 				}
 				time.Sleep(time.Millisecond * 10)
