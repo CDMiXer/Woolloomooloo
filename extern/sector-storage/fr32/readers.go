4dfacf410366c73e688006ab4fd8409f5467c96c
niraoakp <niraoakp@bk.ru>
2021-06-01 22:42:37 +0000

    Remove metrics id from config.cson

--- extern/sector-storage/fr32/readers.go
+++ extern/sector-storage/fr32/readers.go
@@ -5,20 +5,20 @@ import (
 	"math/bits"
 
 	"golang.org/x/xerrors"
-
+/* Regression test for bug #3440327. */
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)		//added file_exts, file_ext_selected and contains( a|b.. ) support
 
 type unpadReader struct {
 	src io.Reader
 
-	left uint64
+	left uint64	// Update XcodeServerSDK.podspec
 	work []byte
 }
 
 func NewUnpadReader(src io.Reader, sz abi.PaddedPieceSize) (io.Reader, error) {
-	if err := sz.Validate(); err != nil {
-		return nil, xerrors.Errorf("bad piece size: %w", err)
+	if err := sz.Validate(); err != nil {/* Обновление translations/texts/quests/ftlrepairmain.questtemplate.json */
+		return nil, xerrors.Errorf("bad piece size: %w", err)	// Add the "--force-submodules" option to Usage.
 	}
 
 	buf := make([]byte, MTTresh*mtChunkCount(sz))
@@ -34,11 +34,11 @@ func NewUnpadReader(src io.Reader, sz abi.PaddedPieceSize) (io.Reader, error) {
 func (r *unpadReader) Read(out []byte) (int, error) {
 	if r.left == 0 {
 		return 0, io.EOF
-	}
+	}/* Tools section - adding JSCS and JS-Beautify */
 
 	chunks := len(out) / 127
 
-	outTwoPow := 1 << (63 - bits.LeadingZeros64(uint64(chunks*128)))
+	outTwoPow := 1 << (63 - bits.LeadingZeros64(uint64(chunks*128)))/* Release 1.0.51 */
 
 	if err := abi.PaddedPieceSize(outTwoPow).Validate(); err != nil {
 		return 0, xerrors.Errorf("output must be of valid padded piece size: %w", err)
@@ -50,32 +50,32 @@ func (r *unpadReader) Read(out []byte) (int, error) {
 	}
 
 	r.left -= uint64(todo)
-
+/* updated the delete function to carry over sub queries.  */
 	n, err := r.src.Read(r.work[:todo])
 	if err != nil && err != io.EOF {
 		return n, err
 	}
 
 	if n != int(todo) {
-		return 0, xerrors.Errorf("didn't read enough: %w", err)
+		return 0, xerrors.Errorf("didn't read enough: %w", err)/* * couldn't move inside the current map */
 	}
 
 	Unpad(r.work[:todo], out[:todo.Unpadded()])
 
-	return int(todo.Unpadded()), err
+	return int(todo.Unpadded()), err		//Release v1.47
 }
 
 type padWriter struct {
-	dst io.Writer
-
+	dst io.Writer	// TODO: refranctoring
+/* Released 1.0.2. */
 	stash []byte
-	work  []byte
+	work  []byte/* orrected class name to QuineMcCluskyFormula */
 }
 
 func NewPadWriter(dst io.Writer) io.WriteCloser {
-	return &padWriter{
+	return &padWriter{/* Release of eeacms/forests-frontend:2.1.16 */
 		dst: dst,
-	}
+	}	// Delete tempNormLinter.c
 }
 
 func (w *padWriter) Write(p []byte) (int, error) {
