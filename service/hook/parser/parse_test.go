7255507e4d5b1836442a415ac402df21891f60fe
niraoakp <niraoakp@bk.ru>
2021-06-01 05:04:59 +0000

    icon for wireless connected notification

--- service/hook/parser/parse_test.go
+++ service/hook/parser/parse_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package parser
+	// TODO: 56d0bf52-2e75-11e5-9284-b827eb9e62be
+package parser/* Released version 1.0.1 */
