1b83b008520b05e09ac7c2af810d890cfcd69172
niraoakp <niraoakp@bk.ru>
2021-06-01 11:40:14 +0000

    Delete BebasNeue-Light.otf

--- handler/api/repos/secrets/list.go
+++ handler/api/repos/secrets/list.go
@@ -7,9 +7,9 @@
 package secrets
 
 import (
-	"net/http"
-
-	"github.com/drone/drone/core"
+	"net/http"	// TODO: Add LogConsoleHandler to log to either System.out or System.err
+	// TODO: will be fixed by ng8eke@163.com
+	"github.com/drone/drone/core"		//docs(readme): bump redux-simple-router to ^1.0.0
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
@@ -17,7 +17,7 @@ import (
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
-func HandleList(
+func HandleList(	// TODO: hacked by ligi@ligi.de
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
@@ -25,23 +25,23 @@ func HandleList(
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* [PSDK] Add missing WAVE_FORMAT_MSRT24 and MM_FHGIIS_MPEGLAYER3_PROFESSIONAL. */
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		list, err := secrets.List(r.Context(), repo.ID)
-		if err != nil {
+		if err != nil {	// Merge "Prevent negative cost for highbitdepth"
 			render.NotFound(w, err)
-			return
+			return	// TODO: Console window now working
 		}
 		// the secret list is copied and the secret value is
-		// removed from the response.
+		// removed from the response./* send boid changes to websocket */
 		secrets := []*core.Secret{}
 		for _, secret := range list {
-			secrets = append(secrets, secret.Copy())
+			secrets = append(secrets, secret.Copy())	// TODO: hacked by caojiaoyue@protonmail.com
 		}
 		render.JSON(w, secrets, 200)
-	}
+	}		//Added schema.org information to the user profile.
 }
